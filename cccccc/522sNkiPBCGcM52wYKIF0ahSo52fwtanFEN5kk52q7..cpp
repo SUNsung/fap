c3c278769eb1ab48c4fe07f72c2551f2acdbdc3d
SUNsung <weatherius@gmail.com>
2018-09-30 00:43:53 +0300

    Rewrite [522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp] File in: 2018-09-30 00:43:52

--- cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
+++ cccccc/522sNkiPBCGcM52wYKIF0ahSo52fwtanFEN5kk52q7..cpp
@@ -1,199 +1,184 @@
 
         
-        namespace internal {
-    }
-    
-    // Prints the given number of bytes in the given object to the given
-// ostream.
-GTEST_API_ void PrintBytesInObjectTo(const unsigned char* obj_bytes,
-                                     size_t count,
-                                     ::std::ostream* os);
-    
-    // Helper function for *_STRNE on wide strings.
-//
-// INTERNAL IMPLEMENTATION - DO NOT USE IN A USER PROGRAM.
-GTEST_API_ AssertionResult CmpHelperSTRNE(const char* s1_expression,
-                                          const char* s2_expression,
-                                          const wchar_t* s1,
-                                          const wchar_t* s2);
-    
-    // Makes sure this header is not included before gtest.h.
-#ifndef GTEST_INCLUDE_GTEST_GTEST_H_
-# error Do not include gtest_pred_impl.h directly.  Include gtest.h instead.
-#endif  // GTEST_INCLUDE_GTEST_GTEST_H_
-    
-     private:
-  class Iterator : public ParamIteratorInterface<ParamType> {
-   public:
-    Iterator(const ParamGeneratorInterface<ParamType>* base,
-      const ParamGenerator<T1>& g1,
-      const typename ParamGenerator<T1>::iterator& current1,
-      const ParamGenerator<T2>& g2,
-      const typename ParamGenerator<T2>::iterator& current2,
-      const ParamGenerator<T3>& g3,
-      const typename ParamGenerator<T3>::iterator& current3,
-      const ParamGenerator<T4>& g4,
-      const typename ParamGenerator<T4>::iterator& current4,
-      const ParamGenerator<T5>& g5,
-      const typename ParamGenerator<T5>::iterator& current5)
-        : base_(base),
-          begin1_(g1.begin()), end1_(g1.end()), current1_(current1),
-          begin2_(g2.begin()), end2_(g2.end()), current2_(current2),
-          begin3_(g3.begin()), end3_(g3.end()), current3_(current3),
-          begin4_(g4.begin()), end4_(g4.end()), current4_(current4),
-          begin5_(g5.begin()), end5_(g5.end()), current5_(current5)    {
-      ComputeCurrentValue();
-    }
-    virtual ~Iterator() {}
-    }
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26>
-struct Types26 {
-  typedef T1 Head;
-  typedef Types25<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
-      T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26> Tail;
-};
-    
-    template <GTEST_TEMPLATE_ T1>
-struct Templates1 {
-  typedef TemplateSel<T1> Head;
-  typedef Templates0 Tail;
-};
-    
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-    
-    #include 'sample2.h'
-    
-      /*
-   * @brief the uri used to begin POSTing carve data
-   *
-   * This endpoint should negotiate the details of the carve, as well
-   * as give the client a session id used to continue POSTing the data.
-   */
-  std::string startUri_;
-    
-    /// Root key to retrieve Kafka topic configurations.
-const std::string kKafkaTopicParserRootKey('kafka_topics');
-    
-    #pragma once
-    
-    TEST_F(DecoratorsConfigParserPluginTests, test_decorators_run_load_top_level) {
-  // Re-enable the decorators, then update the config.
-  // The 'load' decorator set should run every time the config is updated.
-  FLAGS_disable_decorators = false;
-  // enable top level decorations for the test
-  FLAGS_decorations_top_level = true;
-  Config::get().update(config_data_);
-    }
-    
-    
-    {
-    {  c.reset();
+        
+    {  RenderThread::Get()->Send(new ShellViewHostMsg_Allocate_Object(
+      routing_id,
+      object_id,
+      type,
+      *static_cast<base::DictionaryValue*>(value_option.get())));
+  return v8::Undefined(isolate);
 }
-}
-
     
-      fpack.platform_ = 'bad_value';
-  EXPECT_FALSE(fpack.checkPlatform());
+    // Tell browser to allocate a new object.
+// function AllocateObject(id, name, options);
+v8::Handle<v8::Value> AllocateObject(int routing_id,
+                                     int object_id,
+                                     const std::string& type,
+                                     v8::Handle<v8::Value> options);
     
-      free((char*)reply.message);
+      ~EventListener() override;
     
-        // Use the basic 'force' flag to check implicit SQL usage.
-    auto flags =
-        SQL('select default_value from osquery_flags where name = 'force'');
-    if (flags.rows().size() > 0) {
-      r['flag_test'] = flags.rows().back().at('default_value');
+    void PointMenuPositionFunc(GtkMenu* menu,
+                           int* x,
+                           int* y,
+                           gboolean* push_in,
+                           gpointer userdata) {
+  *push_in = TRUE;
     }
+    
+    #include <string.h>
+    
+            static bool IsUDF(const FunctionPtr& f);
+    
+        public:
+        template <typename ElementType>
+        PackedValue(const NDShape& sampleShape, const std::vector<Axis>& sampleDynamicAxes, const std::shared_ptr<Microsoft::MSR::CNTK::Matrix<ElementType>>& packedDataMatrix, const std::shared_ptr<Microsoft::MSR::CNTK::MBLayout>& packedDataLayout, bool isReadOnly)
+            : Value(nullptr), m_isPacked(true), m_sampleShape(sampleShape), m_sampleDynamicAxes(sampleDynamicAxes), m_packedData(nullptr), m_packedDataLayout(packedDataLayout), m_isReadOnly(isReadOnly)
+        {
+            NDShape packedMatrixShape({ packedDataMatrix->GetNumRows(), packedDataMatrix->GetNumCols() });
+            auto tensorView = new Microsoft::MSR::CNTK::TensorView<ElementType>(packedDataMatrix, AsTensorViewShape(packedMatrixShape));
+            m_packedData = MakeSharedObject<NDArrayView>(AsDataType<ElementType>(), AsDeviceDescriptor(packedDataMatrix->GetDeviceId()), AsStorageFormat(packedDataMatrix->GetFormat()), packedMatrixShape, m_isReadOnly, tensorView);
+    }
+    
+    bool DataReader::GetMinibatchCopy(
+    std::vector<std::vector<std::pair<wstring, size_t>>>& uttInfo,
+    StreamMinibatchInputs& matrices,
+    MBLayoutPtr pMBLayout)
+{
+    bool ans = false;
+    for (size_t i = 0; i < m_ioNames.size(); i++)
+        ans = (m_dataReaders[m_ioNames[i]]->GetMinibatchCopy(uttInfo, matrices, pMBLayout) || ans);
+    return ans;
+}
+    
+            fprintf(stderr, 'CloneFunction: Cloned %d nodes and relinked %d inputs.\n', (int)numCloned, (int)numRelinks);
+    
+    // -----------------------------------------------------------------------
+// SequenceDecoderNode (label, position_dependent_score, transition_score)
+// Decoder that matches CRF training.
+//  - label : output label vector of [0:T-1]
+//  - position_dependent_score : score from position dependent node,
+//    in the R-CRF case, it is the RNN output score before softmax
+//  - transition score : score from the transition node,
+//    in the R-CRF case, it is the transition probability between labels
+// -----------------------------------------------------------------------
+    
+    template <class ElemType>
+class InputValueBase : public ComputationNode<ElemType>, public NumInputs<0>, public ITakesDynamicAxis
+{
+    typedef ComputationNode<ElemType> Base;
+    UsingComputationNodeMembers;
+    }
+    
+    void IDCT1d(const double* in, int stride, double* out) {
+  for (int x = 0; x < 8; ++x) {
+    out[x * stride] = 0.0;
+    for (int u = 0; u < 8; ++u) {
+      out[x * stride] += kDCTMatrix[8 * u + x] * in[u * stride];
+    }
+  }
+}
+    
+    #ifndef GUETZLI_DCT_DOUBLE_H_
+#define GUETZLI_DCT_DOUBLE_H_
+    
+    #include 'guetzli/stats.h'
+    
+    const double* Srgb8ToLinearTable() {
+  static const double* const kSrgb8ToLinearTable = NewSrgb8ToLinearTable();
+  return kSrgb8ToLinearTable;
+}
+    
+    #include 'guetzli/fdct.h'
+    
+      // Returns the next byte and skips the 0xff/0x00 escape sequences.
+  uint8_t GetNextByte() {
+    if (pos_ >= next_marker_pos_) {
+      ++pos_;
+      return 0;
+    }
+    uint8_t c = data_[pos_++];
+    if (c == 0xff) {
+      uint8_t escape = data_[pos_];
+      if (escape == 0) {
+        ++pos_;
+      } else {
+        // 0xff was followed by a non-zero byte, which means that we found the
+        // start of the next marker segment.
+        next_marker_pos_ = pos_ - 1;
+      }
+    }
+    return c;
+  }
+    
+    // Builds a JPEG-style huffman code from the given bit depths.
+void BuildHuffmanCode(uint8_t* depth, int* counts, int* values) {
+  for (int i = 0; i < JpegHistogram::kSize; ++i) {
+    if (depth[i] > 0) {
+      ++counts[depth[i]];
+    }
+  }
+  int offset[kJpegHuffmanMaxBitLength + 1] = { 0 };
+  for (int i = 1; i <= kJpegHuffmanMaxBitLength; ++i) {
+    offset[i] = offset[i - 1] + counts[i - 1];
+  }
+  for (int i = 0; i < JpegHistogram::kSize; ++i) {
+    if (depth[i] > 0) {
+      values[offset[depth[i]]++] = i;
+    }
+  }
+}
+    
+    #include <vector>
+    
+    
+    {  if (static_cast<size_t>(*frame_num) != frames.size()) {
+    AERROR << 'frame num is incorrect.';
+    return ErrorCode::CAN_CLIENT_ERROR_FRAME_NUM;
+  }
+  for (size_t i = 0; i < frames.size(); ++i) {
+    ADEBUG << 'send frame i:' << i;
+    ADEBUG << frames[i].CanFrameString();
+    frame_info_ << frames[i].CanFrameString();
+  }
+  ++send_counter_;
+  return ErrorCode::OK;
+}
     
       /**
-   * @brief Initialize the CAN client by specified CAN card parameters.
+   * @brief Initialize the fake CAN client by specified CAN card parameters.
    * @param parameter CAN card parameters to initialize the CAN client.
    * @return If the initialization is successful.
    */
-  virtual bool Init(const CANCardParameter &parameter) = 0;
+  bool Init(const CANCardParameter &param) override;
     
-    TEST(EsdCanClientTest, simple_test) {
-  CANCardParameter param;
-  param.set_brand(CANCardParameter::ESD_CAN);
-  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
+    TEST(CanReceiverTest, ReceiveOne) {
+  can::FakeCanClient can_client;
+  MessageManager<::apollo::canbus::ChassisDetail> pm;
+  CanReceiver<::apollo::canbus::ChassisDetail> receiver;
     }
     
-    /**
- * @file
- * @brief Defines the FakeCanClient class which inherites CanClient.
- */
+    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
     
-    // buf size must be 8 bytes, every time, we receive only one frame
-const int RX_TIMEOUT = -7;
-apollo::common::ErrorCode HermesCanClient::Receive(
-    std::vector<CanFrame> *const frames, int32_t *const frame_num) {
-  if (!_is_init) {
-    AERROR << 'Hermes can client is not init! Please init first!';
-    return ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED;
-  }
-  if (*frame_num > MAX_CAN_RECV_FRAME_LEN || *frame_num < 0) {
-    AERROR << 'recv can frame num not in range[0, ' << MAX_CAN_RECV_FRAME_LEN
-           << '], frame_num:' << *frame_num;
-    return ErrorCode::CAN_CLIENT_ERROR_FRAME_NUM;
-  }
-    }
-    
-      virtual void Stop();
-    
-      /**
-   * @brief Stop the ESD CAN client.
-   */
-  void Stop() override;
-    
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-#include 'modules/common/proto/error_code.pb.h'
-#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
-#include 'modules/drivers/canbus/can_comm/protocol_data.h'
-    
-      /*
-   * @brief parse received data
-   * @param bytes a pointer to the input bytes
-   * @param length the length of the input bytes
-   * @param sensor_data the parsed sensor_data
-   */
-  virtual void Parse(const uint8_t *bytes, int32_t length,
-                     SensorType *sensor_data) const;
-    
-    std::string Byte::byte_to_hex(const uint8_t value) {
-  uint8_t high = value >> 4;
-  uint8_t low = value & 0x0F;
-  std::string result = '';
-  result += HEX[high];
-  result += HEX[low];
-  return result;
+    TEST(ByteTest, GetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  EXPECT_EQ(0x05, value.get_byte(1, 3));
+  EXPECT_EQ(0x01, value.get_byte(1, 1));
+  EXPECT_EQ(0x00, value.get_byte(8, 1));
+  EXPECT_EQ(0x00, value.get_byte(-1, 1));
+  EXPECT_EQ(0x1A, value.get_byte(0, 10));
 }
     
-      /**
-   * @brief Reset some consecutive bits starting from a specified position with
-   *        a certain length of another one-byte unsigned integer.
-   * @param value The one-byte unsigned integer whose certain bits are used
-   *        to set this Byte.
-   * @param start_pos The starting position (from the lowest) of the bits.
-   * @param length The length of the consecutive bits.
-   */
-  void set_value(const uint8_t value, const int32_t start_pos,
-                 const int32_t length);
-    
-    template <typename SensorType>
-class SensorCanbus : public apollo::common::ApolloApp {
- public:
-  // TODO(lizh): check whether we need a new msg item, say
-  // MonitorMessageItem::SENSORCANBUS
-  SensorCanbus()
-      : monitor_logger_(apollo::common::monitor::MonitorMessageItem::CANBUS) {}
+    /**
+ * @namespace apollo::drivers
+ * @brief apollo::drivers
+ */
+namespace apollo {
+namespace drivers {
+    }
     }
     
-    // data file
-DECLARE_string(sensor_conf_file);
+    // System gflags
+DECLARE_string(node_name);
+DECLARE_string(canbus_driver_name);
