b7464d657c4ec0eb2b21720be34920675c36548d
SUNsung <weatherius@gmail.com>
2018-09-30 02:38:46 +0300

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2018-09-30 02:38:46

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,38 +1,136 @@
 
         
-          def prepend(*paths)
-    @paths = parse(*paths, *@paths)
-    self
-  end
+                private
     
-      # Inserts new values or updates existing cached values to persistent storage
-  # mechanism
-  #
-  # @abstract
-  def update!(*)
-    raise NotImplementedError
-  end
+    def docs_name
+  '#{name}-docs'
+end
     
-    module Hbc
-  module Artifact
-    class AbstractFlightBlock < AbstractArtifact
-      def self.dsl_key
-        super.to_s.sub(/_block$/, '').to_sym
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+    $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+    
+    #
+    
+          def initialize(runtime, path_or_io, options)
+        @runtime = runtime
+        @snippets_input = []
+        @io = ensure_io(path_or_io)
+        @prefixes = options[:prefixes] || {}
+        @delayed_messages = []
+        @options = options
+        @exceptions = []
+        @indent = 0
+        @timings = {}
       end
     
-          def link(**options)
-        unless source.exist?
-          raise CaskError, 'It seems the #{self.class.link_type_english_name.downcase} source '#{source}' is not there.'
-        end
+    module Jekyll
+  module Commands
+    class Serve
+      # The LiveReload protocol requires the server to serve livereload.js over HTTP
+      # despite the fact that the protocol itself uses WebSockets.  This custom connection
+      # class addresses the dual protocols that the server needs to understand.
+      class HttpAwareConnection < EventMachine::WebSocket::Connection
+        attr_reader :reload_body, :reload_size
     
-        # Outputs the post.date as formatted html, with hooks for CSS styling.
-    #
-    #  +date+ is the date object to format as HTML.
-    #
-    # Returns string
-    def date_to_html_string(date)
-      result = '<span class='month'>' + date.strftime('%b').upcase + '</span> '
-      result << date.strftime('<span class='day'>%d</span> ')
-      result << date.strftime('<span class='year'>%Y</span> ')
-      result
-    end
+    # define charCodeAt on String
+class String
+  def charCodeAt(k)
+    # use scan, nil check, and unpack instead of ord for 1.8
+    # 1.9 can simply use self[k].ord
+    # http://stackoverflow.com/questions/7793177/split-utf8-string-regardless-of-ruby-version
+    c = self.scan(/./mu)[k]
+    return nil if c.nil?
+    c.unpack('U')[0]
+  end
+end
+    
+          def left_diff_line_number(id, line)
+        if line =~ /^@@/
+          m, li                  = *line.match(/\-(\d+)/)
+          @left_diff_line_number = li.to_i
+          @current_line_number   = @left_diff_line_number
+          ret                    = '...'
+        elsif line[0] == ?-
+          ret                    = @left_diff_line_number.to_s
+          @left_diff_line_number += 1
+          @current_line_number   = @left_diff_line_number - 1
+        elsif line[0] == ?+
+          ret = ' '
+        else
+          ret                    = @left_diff_line_number.to_s
+          @left_diff_line_number += 1
+          @current_line_number   = @left_diff_line_number - 1
+        end
+        ret
+      end
+    
+      teardown do
+    FileUtils.rm_r(File.join(File.dirname(__FILE__), *%w[examples test.git]))
+  end
+    
+        EMOJI_PATHNAME = Pathname.new(Gemojione.images_path).freeze
+    
+      s.test_files = s.files.select { |path| path =~ /^test\/test_.*\.rb/ }
+end
+
+    
+    require 'bundler/cli'
+require 'bundler/friendly_errors'
+    
+          # create Gemfile from template iff it does not exist
+      unless ::File.exists?(Environment::GEMFILE_PATH)
+        FileUtils.copy(
+          ::File.join(Environment::LOGSTASH_HOME, 'Gemfile.template'), Environment::GEMFILE_PATH
+        )
+      end
+      # create Gemfile.jruby-1.9.lock from template iff a template exists it itself does not exist
+      lock_template = ::File.join(ENV['LOGSTASH_HOME'], 'Gemfile.jruby-2.3.lock.release')
+      if ::File.exists?(lock_template) && !::File.exists?(Environment::LOCKFILE)
+        FileUtils.copy(lock_template, Environment::LOCKFILE)
+      end
+    
+        class << self
+      def elastic_pack_base_uri
+        env_url = ENV['LOGSTASH_PACK_URL']
+        (env_url.nil? || env_url.empty?) ? DEFAULT_PACK_URL : env_url
+      end
+    
+    project_versions_yaml_path = File.expand_path('../versions.yml', File.dirname(__FILE__))
+if File.exist?(project_versions_yaml_path)
+  # we need to copy the project level versions.yml into the gem root
+  # to be able to package it into the gems file structure
+  # as the require 'logstash-core-plugin-api/version' loads the yaml file from within the gem root.
+  #
+  # we ignore the copy in git and we overwrite an existing file
+  # each time we build the logstash-core gem
+  original_lines = IO.readlines(project_versions_yaml_path)
+  original_lines << ''
+  original_lines << '# This is a copy the project level versions.yml into this gem's root and it is created when the gemspec is evaluated.'
+  gem_versions_yaml_path = File.expand_path('./versions-gem-copy.yml', File.dirname(__FILE__))
+  File.open(gem_versions_yaml_path, 'w') do |new_file|
+    # create or overwrite
+    new_file.puts(original_lines)
+  end
+end
+    
+        let(:secret) { 'fancy pants' }
+    subject { klass.new('password' => secret) }
+    
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-dns')
+          end
+        end
