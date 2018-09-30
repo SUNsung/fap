5ee283055437187b377338e026b0b415bb1f501d
SUNsung <weatherius@gmail.com>
2018-09-30 04:40:49 +0300

    Rewrite [37SuwKCZsoKFk37P8.o4gjXBs37zvGTQFRuxS.37wfC.cpp] File in: 2018-09-30 04:40:48

--- cccccc/37SuwKCZsoKFk37P8.o4gjXBs37zvGTQFRuxS.37wfC.cpp
+++ cccccc/37SuwKCZsoKFk37P8.o4gjXBs37zvGTQFRuxS.37wfC.cpp
@@ -1,298 +1,171 @@
 
         
-        namespace tesseract {
-    }
+        AuthPropertyIterator::~AuthPropertyIterator() {}
     
-    // Computes part of matrix.vector v = Wu. Computes N=64 results.
-// The weights *must* be arranged so that consecutive reads from wi
-// provides (num_in/kNumInputsPerGroup groups of (N output dim groups of
-// (kNumInputsPerGroup inputs))). After that there must be N consecutive
-// bias weights, before continuing with any more weights.
-// u must be padded out with zeros to
-// kNumInputsPerGroup*ceil(num_in/kNumInputsPerGroup) elements.
-static void PartialMatrixDotVector64(const int8_t* wi, const double* scales,
-                                     const int8_t* u, int num_in, int num_out,
-                                     double* v) {
-  // Register containing 16-bit ones for horizontal add with 16->32 bit
-  // conversion.
-  __m256i ones =
-      _mm256_set_epi16(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
-  __m256i shift_id = _mm256_set_epi32(0, 7, 6, 5, 4, 3, 2, 1);
-  // Initialize all the results to 0.
-  __m256i result0 = _mm256_setzero_si256();
-  __m256i result1 = _mm256_setzero_si256();
-  __m256i result2 = _mm256_setzero_si256();
-  __m256i result3 = _mm256_setzero_si256();
-  __m256i result4 = _mm256_setzero_si256();
-  __m256i result5 = _mm256_setzero_si256();
-  __m256i result6 = _mm256_setzero_si256();
-  __m256i result7 = _mm256_setzero_si256();
-  // Iterate over the input (u), one registerful at a time.
-  for (int j = 0; j < num_in;) {
-    __m256i inputs =
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(u + j));
-    // Inputs are processed in groups of kNumInputsPerGroup, replicated
-    // kNumInputGroups times.
-    for (int ig = 0; ig < kNumInputGroups && j < num_in;
-         ++ig, j += kNumInputsPerGroup) {
-      // Replicate the low 32 bits (4 inputs) 8 times.
-      __m256i rep_input =
-          _mm256_broadcastd_epi32(_mm256_castsi256_si128(inputs));
-      // Rotate the inputs in groups of 4, so the next 4 inputs are ready.
-      inputs = _mm256_permutevar8x32_epi32(inputs, shift_id);
-      __m256i weights, reps;
-      // Mul-add, with horizontal add of the 4 inputs to each of the results.
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result0);
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result1);
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result2);
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result3);
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result4);
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result5);
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result6);
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result7);
-    }
-  }
-  ExtractResults(result0, shift_id, wi, scales, kNumOutputsPerRegister, v);
-  ExtractResults(result1, shift_id, wi, scales, kNumOutputsPerRegister, v);
-  ExtractResults(result2, shift_id, wi, scales, kNumOutputsPerRegister, v);
-  ExtractResults(result3, shift_id, wi, scales, kNumOutputsPerRegister, v);
-  ExtractResults(result4, shift_id, wi, scales, kNumOutputsPerRegister, v);
-  ExtractResults(result5, shift_id, wi, scales, kNumOutputsPerRegister, v);
-  ExtractResults(result6, shift_id, wi, scales, kNumOutputsPerRegister, v);
-  num_out -= kNumOutputsPerRegister * 7;
-  ExtractResults(result7, shift_id, wi, scales,
-                 std::min(kNumOutputsPerRegister, num_out), v);
-}
-    
-    IntSimdMatrixSSE::IntSimdMatrixSSE() {
-#ifdef __SSE4_1__
-  partial_funcs_ = {PartialMatrixDotVector1};
-#endif  // __SSE4_1__
-}
-    
-      // Check for a seed candidate using the foreground pixel density. And we
-  // return true if the density is below a certain threshold, because characters
-  // in equation regions usually are apart with more white spaces.
-  bool CheckSeedFgDensity(const float density_th, ColPartition* part);
-    
-      /**
-   * Returns the bounding rectangle of the current object at the given level.
-   * See comment on coordinate system above.
-   * Returns false if there is no such object at the current position.
-   * The returned bounding box is guaranteed to match the size and position
-   * of the image returned by GetBinaryImage, but may clip foreground pixels
-   * from a grey image. The padding argument to GetImage can be used to expand
-   * the image to include more foreground pixels. See GetImage below.
-   */
-  bool BoundingBox(PageIteratorLevel level,
-                   int* left, int* top, int* right, int* bottom) const;
-  bool BoundingBox(PageIteratorLevel level, const int padding,
-                   int* left, int* top, int* right, int* bottom) const;
-  /**
-   * Returns the bounding rectangle of the object in a coordinate system of the
-   * working image rectangle having its origin at (rect_left_, rect_top_) with
-   * respect to the original image and is scaled by a factor scale_.
-   */
-  bool BoundingBoxInternal(PageIteratorLevel level,
-                           int* left, int* top, int* right, int* bottom) const;
-    
-     private:
-  // The unique ID of this VC object.
-  int my_id_;
-  // Whether the parameter was changed_ and thus needs to be rewritten.
-  bool changed_;
-  // The actual ParamType of this VC object.
-  ParamType param_type_;
-    
-    
-    {  if (word->word->flag(W_DONT_CHOP)) {
-    saved_enable_assoc = wordrec_enable_assoc;
-    saved_chop_enable = chop_enable;
-    wordrec_enable_assoc.set_value(0);
-    chop_enable.set_value(0);
-  }
-  if (pass_n == 1)
-    set_pass1();
-  else
-    set_pass2();
-  recog_word(word);
-  if (word->best_choice == nullptr)
-    word->SetupFake(*word->uch_set);
-  if (word->word->flag(W_DONT_CHOP)) {
-    wordrec_enable_assoc.set_value(saved_enable_assoc);
-    chop_enable.set_value(saved_chop_enable);
-  }
-}
-    
-    #include <grpc/grpc.h>
-#include <grpc/grpc_posix.h>
-#include <grpcpp/channel.h>
-#include <grpcpp/create_channel.h>
-#include <grpcpp/impl/grpc_library.h>
-    
-    SecureAuthContext::~SecureAuthContext() {
-  if (take_ownership_) grpc_auth_context_release(ctx_);
-}
-    
-    void GenerateServerContext(absl::string_view tracing, absl::string_view stats,
-                           absl::string_view primary_role,
-                           absl::string_view method, CensusContext* context) {
-  GrpcTraceContext trace_ctxt;
-  TraceContextEncoding::Decode(tracing, &trace_ctxt);
-  SpanContext parent_ctx = trace_ctxt.ToSpanContext();
-  new (context) CensusContext(method, parent_ctx);
-}
-    
-      CensusContext(absl::string_view name,
-                const ::opencensus::trace::SpanContext& parent_ctxt)
-      : span_(::opencensus::trace::Span::StartSpanWithRemoteParent(
-            name, parent_ctxt)) {}
-    
-    #include 'src/cpp/ext/filters/census/rpc_encoding.h'
-    
-    
-    {  const protobuf::FileDescriptor* file_desc =
-      descriptor_pool_->FindFileByName(filename);
-  if (file_desc == nullptr) {
-    return Status(StatusCode::NOT_FOUND, 'File not found.');
-  }
-  std::unordered_set<grpc::string> seen_files;
-  FillFileDescriptorResponse(file_desc, response, &seen_files);
-  return Status::OK;
-}
-    
-            // Only support sequence without batch
-        if (numOfSequences != 1)
-            InvalidArgument('The Value cannot be copied to buffers in sparse format, since it contains multiple sequences. Only a single sequence is supported.');
-    
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-#include 'Sequences.h'
-#include 'TensorView.h'
-#include 'Utils.h'
-    
-        virtual bool OutputUsedInComputingInputNodesGradients() const override
-    {
-        return false;
-    }
-    virtual bool InputUsedInComputingInputNodesGradients(size_t /*childIndex*/) const override
-    {
-        return false;
-    }
-    
-        void Init(const TensorShape& sampleLayout, bool isSparse, const std::wstring axisName, float learningRateMultiplier = 0)
-    {
-        m_isSparse = isSparse;
-        Base::m_isValueSparse = isSparse;
-        MarkValueNonSharable();
-        if (isSparse)
-            ConvertToSparseMatrix();
-    }
-    
-      // Get initial measurement of the space we will be reading.
-  const int64_t initial_size = Size(Key(0), Key(n));
-  const int64_t initial_other_size = Size(Key(n), Key(kCount));
-    
-    
-    {  if (s.ok() && meta->file_size > 0) {
-    // Keep it
-  } else {
-    env->DeleteFile(fname);
-  }
-  return s;
-}
-    
-    // Maximum number of level-0 files.  We stop writes at this point.
-static const int kL0_StopWritesTrigger = 12;
-    
-    static std::string MakeFileName(const std::string& dbname, uint64_t number,
-                                const char* suffix) {
-  char buf[100];
-  snprintf(buf, sizeof(buf), '/%06llu.%s',
-           static_cast<unsigned long long>(number),
-           suffix);
-  return dbname + buf;
-}
-    
-    // If filename is a leveldb file, store the type of the file in *type.
-// The number encoded in the filename is stored in *number.  If the
-// filename was successfully parsed, returns true.  Else return false.
-bool ParseFileName(const std::string& filename,
-                   uint64_t* number,
-                   FileType* type);
-    
-      ~Reader();
-    
-    void MemTable::Add(SequenceNumber s, ValueType type,
-                   const Slice& key,
-                   const Slice& value) {
-  // Format of an entry is concatenation of:
-  //  key_size     : varint32 of internal_key.size()
-  //  key bytes    : char[internal_key.size()]
-  //  value_size   : varint32 of value.size()
-  //  value bytes  : char[value.size()]
-  size_t key_size = key.size();
-  size_t val_size = value.size();
-  size_t internal_key_size = key_size + 8;
-  const size_t encoded_len =
-      VarintLength(internal_key_size) + internal_key_size +
-      VarintLength(val_size) + val_size;
-  char* buf = arena_.Allocate(encoded_len);
-  char* p = EncodeVarint32(buf, internal_key_size);
-  memcpy(p, key.data(), key_size);
-  p += key_size;
-  EncodeFixed64(p, (s << 8) | type);
-  p += 8;
-  p = EncodeVarint32(p, val_size);
-  memcpy(p, value.data(), val_size);
-  assert(p + val_size == buf + encoded_len);
-  table_.Insert(buf);
-}
-    
-    #include <string>
-#include 'leveldb/db.h'
-#include 'db/dbformat.h'
-#include 'db/skiplist.h'
-#include 'util/arena.h'
-    
-    TEST(RecoveryTest, LogFileReuse) {
-  if (!CanAppend()) {
-    fprintf(stderr, 'skipping test because env does not support appending\n');
+    void ChannelArguments::SetSocketMutator(grpc_socket_mutator* mutator) {
+  if (!mutator) {
     return;
   }
-  for (int i = 0; i < 2; i++) {
-    ASSERT_OK(Put('foo', 'bar'));
-    if (i == 0) {
-      // Compact to ensure current log is empty
-      CompactMemTable();
+  grpc_arg mutator_arg = grpc_socket_mutator_to_arg(mutator);
+  bool replaced = false;
+  grpc_core::ExecCtx exec_ctx;
+  for (auto it = args_.begin(); it != args_.end(); ++it) {
+    if (it->type == mutator_arg.type &&
+        grpc::string(it->key) == grpc::string(mutator_arg.key)) {
+      GPR_ASSERT(!replaced);
+      it->value.pointer.vtable->destroy(it->value.pointer.p);
+      it->value.pointer = mutator_arg.value.pointer;
+      replaced = true;
     }
-    Close();
-    ASSERT_EQ(1, NumLogs());
-    uint64_t number = FirstLogFile();
-    if (i == 0) {
-      ASSERT_EQ(0, FileSize(LogName(number)));
-    } else {
-      ASSERT_LT(0, FileSize(LogName(number)));
-    }
-    Open();
-    ASSERT_EQ(1, NumLogs());
-    ASSERT_EQ(number, FirstLogFile()) << 'did not reuse log file';
-    ASSERT_EQ('bar', Get('foo'));
-    Open();
-    ASSERT_EQ(1, NumLogs());
-    ASSERT_EQ(number, FirstLogFile()) << 'did not reuse log file';
-    ASSERT_EQ('bar', Get('foo'));
   }
+    }
+    
+    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
+  for (unsigned int i = 0; i < args_.size(); i++) {
+    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
+      return args_[i].value.string;
+    }
+  }
+  return '';
 }
     
-      // No-barrier variants that can be safely used in a few locations.
-  Node* NoBarrier_Next(int n) {
-    assert(n >= 0);
-    return reinterpret_cast<Node*>(next_[n].NoBarrier_Load());
-  }
-  void NoBarrier_SetNext(int n, Node* x) {
-    assert(n >= 0);
-    next_[n].NoBarrier_Store(x);
-  }
+      static void OnDoneRecvInitialMetadataCb(void* user_data, grpc_error* error);
     
-        SnapshotImpl* snapshot = new SnapshotImpl(sequence_number);
+        template <typename ElementType, typename V1ElemType>
+    /*static*/ NDArrayViewPtr NDArrayView::_RandomUniform(const NDShape& shape, double rangeBegin, double rangeEnd, unsigned long seed, const DeviceDescriptor& device/* = DeviceDescriptor::UseDefaultDevice()*/)
+    {
+        auto matrixDims = GetMatrixDimensions(shape);
+        auto randomUniformMatrix = std::make_shared<Matrix<V1ElemType>>(Matrix<V1ElemType>::RandomUniform(matrixDims.first, matrixDims.second, AsCNTKImplDeviceId(device), (V1ElemType)rangeBegin, (V1ElemType)rangeEnd, seed));
+        auto tensorView = new TensorView<V1ElemType>(randomUniformMatrix, AsTensorViewShape(shape));
+    }
+    
+        void ProgressWriter::UpdateTest(size_t samples, const ValuePtr& accumulatedMetric)
+    {
+        m_test->Update(samples, nullptr, accumulatedMetric,
+            [this](const std::pair<size_t, size_t> samples, std::pair<size_t, size_t> updates,
+                   const std::pair<double, double> /*aggregateLoss*/, std::pair<double, double> aggregateMetric)
+            {
+                OnWriteTestUpdate(samples, updates, aggregateMetric);
+            });
+    }
+    
+            size_t MaskedCount() const override
+        {
+            if (m_isPacked)
+                // Compute the number of masked samples after the data will be unpacked
+                return m_packedDataLayout ? ((m_packedDataLayout->GetNumTimeSteps() * m_packedDataLayout->GetNumSequences()) - m_packedDataLayout->GetActualNumSamples()) : 0;
+            else
+                return Value::MaskedCount();
+        }
+    
+    public:
+    CrossProcessMutex(const std::string& name)
+        : m_fd(-1),
+          m_fileName('/var/lock/' + name)
+    {
+    }
+    
+    #include <string>
+    
+        virtual void /*ComputationNode::*/ BackpropTo(const size_t inputIndex, const FrameRange& fr) override
+    {
+        if (inputIndex == 0) // left derivative
+        {
+            Matrix<ElemType> sliceOutputGrad = MaskedGradientFor(fr); // use Masked- version since this is reducing over frames
+            Matrix<ElemType> sliceInput1Value = Input(1)->MaskedValueFor(fr);
+            m_innerproduct->AssignInnerProductOf(sliceOutputGrad, sliceInput1Value, false);
+            InputRef(0).GradientAsMatrix() += *m_innerproduct;
+        }
+        else // right derivative
+        {
+            Matrix<ElemType> sliceOutputGrad = GradientFor(fr);
+            Matrix<ElemType> sliceInput1Grad = Input(1)->GradientFor(fr);
+            m_rightGradient->SetValue(sliceOutputGrad);
+            m_rightGradient->ColumnElementMultiplyWith(InputRef(0).ValueAsMatrix());
+            sliceInput1Grad += *m_rightGradient;
+        }
+    }
+    
+    
+    {
+    {
+    { private:
+  /*! \brief input stream */
+  dmlc::Stream *strm_;
+  /*! \brief current buffer pointer */
+  size_t buffer_ptr_;
+  /*! \brief internal buffer */
+  std::string buffer_;
+};
+}  // namespace common
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_IO_H_
+
+    
+    
+    {
+    {}  // namespace common
+}  // namespace xgboost
+    
+    #include <xgboost/base.h>
+#include <xgboost/data.h>
+#include <dmlc/threadediter.h>
+#include <utility>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include '../common/common.h'
+#include './sparse_page_writer.h'
+    
+    /**
+ * @file
+ * @brief Defines the EsdCanClient class which inherits CanClient.
+ */
+    
+    #include <vector>
+    
+    namespace apollo {
+namespace drivers {
+namespace canbus {
+namespace can {
+    }
+    }
+    }
+    }
+    
+    class MockProtocolData : public ProtocolData<::apollo::canbus::ChassisDetail> {
+ public:
+  static const int32_t ID = 0x111;
+  MockProtocolData() {}
+};
+    
+    /**
+ * @class ProtocolData
+ *
+ * @brief This is the base class of protocol data.
+ */
+template <typename SensorType>
+class ProtocolData {
+ public:
+  /**
+   * @brief static function, used to calculate the checksum of input array.
+   * @param input the pointer to the start position of input array
+   * @param length the length of the input array
+   * @return the value of checksum
+   */
+  static std::uint8_t CalculateCheckSum(const uint8_t *input,
+                                        const uint32_t length);
+  /**
+   * @brief construct protocol data.
+   */
+  ProtocolData() = default;
+    }
+    
+    std::string Byte::byte_to_hex(const uint8_t value) {
+  uint8_t high = value >> 4;
+  uint8_t low = value & 0x0F;
+  std::string result = '';
+  result += HEX[high];
+  result += HEX[low];
+  return result;
+}
