75dc2e547f70241df20516023bf20da8d2f1e098
SUNsung <weatherius@gmail.com>
2018-09-30 17:48:57 +0300

    Create [56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp] File in: 2018-09-30 17:48:56

---
+++ cccccc/56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp
@@ -0,0 +1,63 @@
+  // Get original parameter name.
+  string GetName() const { return name_; }
+    
+    namespace tensorflow {
+    }
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    namespace tensorflow {
+    }
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    string AlgorithmConfig::ToString() const { return port::StrCat(algorithm_); }
+    
+    ScopedActivateExecutorContext::ScopedActivateExecutorContext(
+    CUDAExecutor *cuda_exec):
+      driver_scoped_activate_context_(
+          new ScopedActivateContext{ExtractCudaContext(cuda_exec)}) { }
+    
+      void wait(int n = 1) {
+    std::unique_lock<std::mutex> lock(m_);
+    while (n_ < n) {
+      cv_.wait(lock);
+    }
+    n_ -= n;
+  }
+    
+        for (int fd: to_add)
+      register_fd(fd);
+    to_add.clear();
+    
+    namespace caffe2 {
+std::vector<TensorShape> FCShapeInference(
+    const OperatorDef& def,
+    const vector<TensorShape>& in,
+    bool pretransposed_weight) {
+  vector<TensorShape> out(1);
+  ArgumentHelper helper(def);
+    }
+    }
+    
+    template <typename T, class Context>
+class FlexibleTopKOp : public Operator<Context> {
+ public:
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+    }
+    
+    
+    {  bool RunOnDevice() override {
+    for (Blob* output : OperatorBase::Outputs()) {
+      output->Reset();
+    }
+    return true;
+  }
+};
