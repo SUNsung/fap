d381a8f01f51041bd3a3576d213c607969aa2ccd
SUNsung <weatherius@gmail.com>
2018-09-30 16:40:53 +0300

    Rewrite [5386cZk0f9OEc53IGOAo0jrIXo53FDoPQRRt6NU53V1c.py] File in: 2018-09-30 16:40:53

--- pyton/5386cZk0f9OEc53IGOAo0jrIXo53FDoPQRRt6NU53V1c.py
+++ pyton/5386cZk0f9OEc53IGOAo0jrIXo53FDoPQRRt6NU53V1c.py
@@ -1,77 +1,131 @@
 
         
-        containers = (('thefuck/python3-fish',
-               u'''FROM python:3
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'),
-              ('thefuck/python2-fish',
-               u'''FROM python:2
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'))
+            @staticmethod
+    def import_windows_ca(certfile):
+        xlog.debug('Begin to import Windows CA')
+        with open(certfile, 'rb') as fp:
+            certdata = fp.read()
+            if certdata.startswith(b'-----'):
+                begin = b'-----BEGIN CERTIFICATE-----'
+                end = b'-----END CERTIFICATE-----'
+                certdata = base64.b64decode(b''.join(certdata[certdata.find(begin)+len(begin):certdata.find(end)].strip().splitlines()))
+        try:
+            common_name = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, certdata).get_subject().CN
+        except Exception as e:
+            logging.error('load_certificate(certfile=%r) 失败：%s', certfile, e)
+            return -1
+    
+                if fail or self.network_stat != 'OK':
+                # Fail or unknown
+                if time_now - self.last_check_time < 3:
+                    return
+            else:
+                if time_now - self.last_check_time < 10:
+                    return
+    
+        for qualified, server, _, _ in server_list:
+        if qualified:
+            best_server = server[0]
+            break
+    log = Log()
+    if best_server:
+        log.write('best server is: %s.' % best_server)
+    else:
+        xlog.warning('no server detected, return default: teredo.remlab.net.')
+        log.write('no server detected, return default: teredo.remlab.net.')
+        best_server = 'teredo.remlab.net'
+    log.close()
+    return best_server
+    
+    noarch_lib = os.path.abspath( os.path.join(python_path, 'lib', 'noarch'))
+sys.path.append(noarch_lib)
+    
+            p = random.choice(self.plus)
     
     
-@pytest.fixture(params=[(python_3, False),
-                        (python_3, True),
-                        (python_2, False)])
-def proc(request, spawnu, TIMEOUT):
-    container, instant_mode = request.param
-    proc = spawnu(*container)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(init_zshrc.format(
-        u'--enable-experimental-instant-mode' if instant_mode else ''))
-    proc.sendline(u'zsh')
-    if instant_mode:
-        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
-    return proc
+    def specialStateTransition(self, s, input):
+        return -1
+    
+    # Other resources excluded from --use=all:
+#
+# - extralagefile (ex: test_zipfile64): really too slow to be enabled
+#   'by default'
+# - tzdata: while needed to validate fully test_datetime, it makes
+#   test_datetime too slow (15-20 min on some buildbots) and so is disabled by
+#   default (see bpo-30822).
+RESOURCE_NAMES = ALL_RESOURCES + ('extralargefile', 'tzdata')
+    
+            if not self.ns.fromfile:
+            self.selected = self.tests or self.ns.args or alltests
+        else:
+            self.selected = self.tests
+        if self.ns.single:
+            self.selected = self.selected[:1]
+            try:
+                pos = alltests.index(self.selected[0])
+                self.next_single_test = alltests[pos + 1]
+            except IndexError:
+                pass
+    
+            - encoding_details: EncodingDetails for eager decoding
+        - stderr_lines: result of calling splitlines() on the stderr output
+    
+            out = self.get_output('-c', code, PYTHONUTF8='1')
+        self.assertEqual(out, '1')
+    
+    The special characters are:
+    '.'      Matches any character except a newline.
+    '^'      Matches the start of the string.
+    '$'      Matches the end of the string or just before the newline at
+             the end of the string.
+    '*'      Matches 0 or more (greedy) repetitions of the preceding RE.
+             Greedy means that it will match as many repetitions as possible.
+    '+'      Matches 1 or more (greedy) repetitions of the preceding RE.
+    '?'      Matches 0 or 1 (greedy) of the preceding RE.
+    *?,+?,?? Non-greedy versions of the previous three special characters.
+    {m,n}    Matches from m to n repetitions of the preceding RE.
+    {m,n}?   Non-greedy version of the above.
+    '\\'     Either escapes special characters or signals a special sequence.
+    []       Indicates a set of characters.
+             A '^' as the first character indicates a complementing set.
+    '|'      A|B, creates an RE that will match either A or B.
+    (...)    Matches the RE inside the parentheses.
+             The contents can be retrieved or matched later in the string.
+    (?aiLmsux) Set the A, I, L, M, S, U, or X flag for the RE (see below).
+    (?:...)  Non-grouping version of regular parentheses.
+    (?P<name>...) The substring matched by the group is accessible by name.
+    (?P=name)     Matches the text matched earlier by the group named name.
+    (?#...)  A comment; ignored.
+    (?=...)  Matches if ... matches next, but doesn't consume the string.
+    (?!...)  Matches if ... doesn't match next.
+    (?<=...) Matches if preceded by ... (must be fixed length).
+    (?<!...) Matches if not preceded by ... (must be fixed length).
+    (?(id/name)yes|no) Matches yes pattern if the group with id/name matched,
+                       the (optional) no pattern otherwise.
     
     
-@pytest.mark.parametrize('command', [
-    Command('apt list --upgradable', no_match_output),
-    Command('sudo apt list --upgradable', no_match_output)
-])
-def test_not_match(command):
-    assert not match(command)
+def split(s):
+    '''Split a pathname into two parts: the directory leading up to the final
+    bit, and the basename (the filename, without colons, in that directory).
+    The result (s, t) is such that join(s, t) yields the original argument.'''
     
-            self.assertEqual(jws, JWS.from_json(jws.to_json()))
+        '''
+    if not module or not isinstance(module, types.ModuleType):
+        raise TypeError('reload() argument must be a module')
+    try:
+        name = module.__spec__.name
+    except AttributeError:
+        name = module.__name__
     
+        return result
     
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
+        complete_apps = ['sentry']
+    symmetrical = True
+
     
-            with mock.patch('certbot.util.get_os_info') as mock_info:
-            for distro in entrypoint.OVERRIDE_CLASSES.keys():
-                mock_info.return_value = (distro, 'whatever')
-                self.assertEqual(entrypoint.get_configurator(),
-                                 entrypoint.OVERRIDE_CLASSES[distro])
-    
-        def _get_addrs(self, achall):
-        '''Return the Apache addresses needed for TLS-SNI-01.'''
-        # TODO: Checkout _default_ rules.
-        addrs = set()
-        default_addr = obj.Addr(('*', str(
-            self.configurator.config.tls_sni_01_port)))
-    
-                # if multiple releases have files, just rename them
-            # instead of trying to merge
-            if len(releases_with_files) > 1:
-                for release in releases:
-                    update_version(release, orm)
-                continue
-    
-            # Adding field 'ApiToken.scope_list'
+            # Adding field 'ApiAuthorization.scope_list'
         db.add_column(
-            'sentry_apitoken',
+            'sentry_apiauthorization',
             'scope_list',
             self.gf('sentry.db.models.fields.array.ArrayField')(
                 of=('django.db.models.fields.TextField', [], {})
@@ -79,5 +133,29 @@ sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
             keep_default=False
         )
     
-            # Adding unique constraint on 'VersionDSymFile', fields ['dsym_file', 'version', 'build']
-        db.create_unique('sentry_versiondsymfile', ['dsym_file_id', 'version', 'build'])
+    
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding model 'VersionDSymFile'
+        db.create_table(
+            'sentry_versiondsymfile', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'dsym_file', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.ProjectDSymFile'], null=True
+                    )
+                ), (
+                    'dsym_app', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.DSymApp']
+                    )
+                ), ('version', self.gf('django.db.models.fields.CharField')(max_length=32)),
+                ('build', self.gf('django.db.models.fields.CharField')(max_length=32, null=True)), (
+                    'date_added',
+                    self.gf('django.db.models.fields.DateTimeField')()
+                ),
+            )
+        )
+        db.send_create_signal('sentry', ['VersionDSymFile'])
