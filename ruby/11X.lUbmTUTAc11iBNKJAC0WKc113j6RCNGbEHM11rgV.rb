692868507e63239d0f8c68e86538fd346221cdec
SUNsung <weatherius@gmail.com>
2018-09-30 11:07:56 +0300

    Rewrite [11X.lUbmTUTAc11iBNKJAC0WKc113j6RCNGbEHM11rgV.rb] File in: 2018-09-30 11:07:55

--- ruby/11X.lUbmTUTAc11iBNKJAC0WKc113j6RCNGbEHM11rgV.rb
+++ ruby/11X.lUbmTUTAc11iBNKJAC0WKc113j6RCNGbEHM11rgV.rb
@@ -1,163 +1,69 @@
-  describe 'local topic link' do
-    
-    invalids = []
-Parallel.each(links, in_threads: 4) do |link|
-  href = link.attribute('href').to_s
-  begin
-    case check_link(URI.join(BASE_URI, href))
-    when (200...300)
-      putc('.')
-    when (300..302)
-      putc('w')
-    end
-  rescue => e
-    putc('F')
-    invalids << '#{href} (reason: #{e.message})'
-  end
-end
-    
-      def resource_params
-    params.fetch(resource_name, {})
-  end
-    
-                def current_#{group_name}(favourite=nil)
-              mappings = #{mappings}
-              mappings.unshift mappings.delete(favourite.to_sym) if favourite
-              mappings.each do |mapping|
-                current = warden.authenticate(scope: mapping)
-                return current if current
-              end
-              nil
-            end
-    
-    module Devise
-  module Controllers
-    # A module that may be optionally included in a controller in order
-    # to provide remember me behavior. Useful when signing in is done
-    # through a callback, like in OmniAuth.
-    module Rememberable
-      # Return default cookie values retrieved from session options.
-      def self.cookie_values
-        Rails.configuration.session_options.slice(:path, :domain, :secure)
-      end
-    
-          # Sign in a user bypassing the warden callbacks and stores the user
-      # straight in session. This option is useful in cases the user is already
-      # signed in, but we want to refresh the credentials in session.
-      #
-      # Examples:
-      #
-      #   bypass_sign_in @user, scope: :user
-      #   bypass_sign_in @user
-      def bypass_sign_in(resource, scope: nil)
-        scope ||= Devise::Mapping.find_scope!(resource)
-        expire_data_after_sign_in!
-        warden.session_serializer.store(resource, scope)
-      end
-    
-            routes.each do |module_name, actions|
-          [:path, :url].each do |path_or_url|
-            actions.each do |action|
-              action = action ? '#{action}_' : ''
-              method = :'#{action}#{module_name}_#{path_or_url}'
-    
-        # Try retrieving the URL options from the parent controller (usually
-    # ApplicationController). Instance methods are not supported at the moment,
-    # so only the class-level attribute is used.
-    def self.default_url_options(*args)
-      if defined?(Devise.parent_controller.constantize)
-        Devise.parent_controller.constantize.try(:default_url_options) || {}
-      else
-        {}
-      end
-    end
-    
-              private
-    
-                res = checksum + encrypted
-            res
-          end
-        end
-      end
+
+        
+          def search_taps(rx)
+    SEARCHABLE_TAPS.map do |user, repo|
+      Thread.new { search_tap(user, repo, rx) }
+    end.inject([]) do |results, t|
+      results.concat(t.value)
     end
   end
-end
     
-              # Decodes the etype field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Array<Integer>]
-          def decode_etype(input)
-            encs = []
-            input.value[0].value.each do |enc|
-              encs << enc.value.to_i
-            end
-            encs
-          end
+      DATA = :DATA
     
-              # Decodes the cusec field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_cusec(input)
-            input.value[0].value
-          end
+      # Use this method to generate standard caveats.
+  def standard_instructions(home_name, home_value = libexec)
+    <<-EOS.undent
+      Before you can use these tools you must export some variables to your $SHELL.
     
-              private
+      it 'raises a TypeError when passed nil' do
+    lambda { sleep(nil)   }.should raise_error(TypeError)
+  end
     
-    (allow file-read-metadata)
-(allow file-read*
-  ; This is currenly only added because using `xcodebuild` to build a resource
-  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
-  ; needed, but for now it doesn’t seem like a real problem.
-  (literal '/')
-  (regex
-    ; TODO see if we can restrict this more, but it's going to be hard
-    #'^/Users/[^.]+/*'
-    ;#'^/Users/[^.]+/.netrc'
-    ;#'^/Users/[^.]+/.gemrc'
-    ;#'^/Users/[^.]+/.gem/*'
-    ;#'^/Users/[^.]+/Library/.*'
-    #'^/Library/*'
-    #'^/System/Library/*'
-    #'^/usr/lib/*'
-    #'^/usr/share/*'
-    #'^/private/*'
-    #'^/dev/*'
-    #'^<%= ruby_prefix %>'
-    #'^<%= pod_prefix %>'
-    #'^<%= xcode_app_path %>'
-    #'^<%= Pod::Config.instance.repos_dir %>'
-<% prefixes.each do |prefix| %>
-    #'^<%= prefix %>/*'
-<% end %>
-  )
-)
+      it 'accepts and uses a seed of 0' do
+    srand(0)
+    srand.should == 0
+  end
     
-        %w[iOS macOS].each do |platform|
-        abstract_target '#{platform} Pods' do
-            project '#{platform} Modules.xcodeproj'
+      it 'returns true when passed ?e if the argument is a file' do
+    Kernel.test(?e, @file).should == true
+  end
     
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
+      it 'transfers control to the innermost catch block waiting for the same sympol' do
+    one = two = three = 0
+    catch :duplicate do
+      catch :duplicate do
+        catch :duplicate do
+          one = 1
+          throw :duplicate
         end
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
-        end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+        two = 2
+        throw :duplicate
       end
+      three = 3
+      throw :duplicate
+    end
+    [one, two, three].should == [1, 2, 3]
+  end
+    
+        if run? && ARGV.any?
+      require 'optparse'
+      OptionParser.new { |op|
+        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
+        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
+        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
+        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
+        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
+        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
+      }.parse!(ARGV.dup)
+    end
+  end
+    
+          def set_token(session)
+        session[:csrf] ||= self.class.random_token
+      end
+    
+      it 'allows for a custom authenticity token param' do
+    mock_app do
+      use Rack::Protection::AuthenticityToken, :authenticity_param => 'csrf_param'
+      run proc { |e| [200, {'Content-Type' => 'text/plain'}, ['hi']] }
     end
