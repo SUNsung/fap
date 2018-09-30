67259f917100bfde9fd91ae1305df1d34a28ab7f
SUNsung <weatherius@gmail.com>
2018-10-01 00:19:36 +0300

    Rewrite [india.rb] File in: 2018-10-01 00:19:35

--- ruby/india.rb
+++ ruby/india.rb
@@ -1,130 +1,75 @@
 
         
-            return if Language::Python.reads_brewed_pth_files?('python')
+                result = Fastlane::FastFile.new.parse('lane :test do
+          add_git_tag ({
+            tag: '#{tag}',
+            grouping: 'grouping',
+            build_number: 'build_number',
+            prefix: 'prefix',
+          })
+        end').runner.execute(:test)
     
-      def describe_ruby
-    ruby = which 'ruby'
-    return 'N/A' if ruby.nil?
-    ruby_binary = Utils.popen_read ruby, '-rrbconfig', '-e', \
-      'include RbConfig;print'#{CONFIG['bindir']}/#{CONFIG['ruby_install_name']}#{CONFIG['EXEEXT']}''
-    ruby_binary = Pathname.new(ruby_binary).realpath
-    if ruby == ruby_binary
-      ruby
-    else
-      '#{ruby} => #{ruby_binary}'
-    end
-  end
-    
-          if tap.core_tap?
-        new_full_name = new_name
-      else
-        new_full_name = '#{tap}/#{new_name}'
+            inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
+        # this is not really the command that would have been executed, but a 'fabricated' representation for tests (by Actions.sh) that includes both command that would have been run
+        pseudocommand = 'git log --pretty=\'%B\' #{inner_command.shellescape}...HEAD'
+        expect(result).to eq(pseudocommand)
       end
     
-        if filter.include? key
-      filter.delete key
-    else
-      filter << key
-    end
-    
-          def provider
-        oauth['provider']
+          context 'without parameters' do
+        it 'executes the correct git command' do
+          allow(Fastlane::Actions).to receive(:sh).with('git add .', anything).and_return('')
+          result = Fastlane::FastFile.new.parse('lane :test do
+            git_add
+          end').runner.execute(:test)
+        end
       end
     
-          def find_oauth_access_token
-        token = Doorkeeper::OAuth::Token.from_request(current_request, *Doorkeeper.configuration.access_token_methods)
-        return unless token
-    
-              builds = scope_relevant.select('count(*)').to_sql
-          created = scope_relevant.created.select('count(*)').to_sql
-          success = scope_relevant.success.select('count(*)').to_sql
-          manual = scope_relevant.manual.select('count(*)').to_sql
-          pending = scope_relevant.pending.select('count(*)').to_sql
-          running = scope_relevant.running.select('count(*)').to_sql
-          skipped = scope_relevant.skipped.select('count(*)').to_sql
-          canceled = scope_relevant.canceled.select('count(*)').to_sql
-          warnings = scope_warnings.select('count(*) > 0').to_sql
-    
-            MergeRequest
-          .where(id: start_id..stop_id)
-          .where(latest_merge_request_diff_id: nil)
-          .each_batch(of: BATCH_SIZE) do |relation|
-    
-            def metadata
-          @metadata ||= Pipeline::Metadata.new(self)
-        end
-    
-    group :docs do
-  gem 'typhoeus'
-  gem 'nokogiri'
-  gem 'html-pipeline'
-  gem 'image_optim'
-  gem 'image_optim_pack', platforms: :ruby
-  gem 'progress_bar', require: false
-  gem 'unix_utils', require: false
-  gem 'tty-pager', require: false
-end
-    
-        private
-    
-        def at_css(*args)
-      doc.at_css(*args)
-    end
-    
-        def path
-      @path ||= url.path
-    end
-    
-            css('.l-sub-section', '.alert', '.banner').each do |node|
-          node.name = 'blockquote'
-        end
-    
-            css('.multi-line-signature').each do |node|
-          node.name = 'pre'
-          node.content = node.content.strip
-        end
-    
-            name.prepend '#{breadcrumbs.join('.')}#' if breadcrumbs.present? && breadcrumbs[0] != name
-        name << '()' if %w(Function Method Constructor).include?(subtitle)
-        name
-      end
-    
-            # Get the machine
-        machine = nil
-        begin
-          machine = env.machine(self.name.to_sym, self.provider.to_sym)
-        rescue Errors::MachineNotFound
-          return false
-        end
-    
-                # Call the state method so that we update our index state. Don't
-            # worry about exceptions here, since we just care about updating
-            # the cache.
-            begin
-              # Called for side effects
-              machine.state
-            rescue Errors::VagrantError
-            end
+          context 'with valid path to compile_commands.json' do
+        context 'with no path to oclint' do
+          let(:result) do
+            Fastlane::FastFile.new.parse('lane :test do
+              oclint( compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json' )
+            end').runner.execute(:test)
           end
-        end
+          let(:command) { 'cd #{File.expand_path('.').shellescape} && oclint -report-type=html -o=oclint_report.html' }
     
-    When /^I reset Bundler environment variable$/ do
-  BUNDLE_ENV_VARS.each do |key|
-    ENV[key] = nil
-  end
-end
-    
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
-    
-        def names_for(klass)
-      @attachments[klass].keys
-    end
-    
-        def calculated_type_matches
-      possible_types.select do |content_type|
-        content_type == type_from_file_contents
+            expected = 'slather coverage
+                    --build-directory #{build_dir.shellescape}
+                    --source-directory #{source_dir.shellescape}
+                    --output-directory #{output_dir.shellescape}
+                    --ignore #{ignore.shellescape}
+                    --input-format bah
+                    --scheme #{scheme.shellescape}
+                    #{proj.shellescape}'.gsub(/\s+/, ' ')
+        expect(result).to eq(expected)
       end
+    
+        it 'adds an environment Hash at the beginning' do
+      message = 'A message'
+      command = command_from_args({ 'PATH' => '/usr/local/bin' }, 'git', 'commit', '-m', message)
+      expect(command).to eq('PATH=/usr/local/bin git commit -m #{message.shellescape}')
     end
+    
+        context 'string formats' do
+      it 'determines the maximum number of characters to be copied from the string' do
+        format('%.1p', [1]).should == '['
+        format('%.2p', [1]).should == '[1'
+        format('%.10p', [1]).should == '[1]'
+        format('%.0p', [1]).should == ''
+    
+      it 'accepts a Rational' do
+    sleep(Rational(1, 9)).should be_close(0, 2)
+  end
+    
+        it 'returns true when passed ?w if the argument is readable by the effective uid' do
+      Kernel.test(?w, @tmp_file).should be_true
+    end
+    
+        if other.respond_to?(:to_str)
+      return true if to_str == other.to_str
+    end
+    
+    require 'commands'
+    
+      prepend Compat
+end
