d16129dd263a431e2cfa15fd7c8e66823aaf465b
SUNsung <weatherius@gmail.com>
2018-09-30 08:58:24 +0300

    Rewrite [11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb] File in: 2018-09-30 08:58:23

--- ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
+++ ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
@@ -1,91 +1,30 @@
 
         
-        def check_link(uri)
-  HTTParty.head(uri, :verify => false).code.to_i.tap do |status|
-    if (400..422).include?(status)
-      if status != 403 && !uri.exclude?('udemy.com')
-        raise 'Request had status #{status}'
-      else
-        putc('S')
+              def failed?
+        error_message.present?
+      end
+    
+              if @job.blank?
+            @pipeline.coverage
+          else
+            @pipeline.builds
+              .find_by(name: @job)
+              .try(:coverage)
+          end
+        end
       end
     end
   end
 end
+
     
-            def self.normalize_dn(dn)
-          ::Gitlab::Auth::LDAP::DN.new(dn).to_normalized_s
-        rescue ::Gitlab::Auth::LDAP::DN::FormatError => e
-          Rails.logger.info('Returning original DN \'#{dn}\' due to error during normalization attempt: #{e.message}')
-    
-          def valid_access_token?(scopes: [])
-        validate_access_token!(scopes: scopes)
-    
-          def image_url
-        raise NotImplementedError
-      end
-    
-            def metadata
-          @metadata ||= Pipeline::Metadata.new(self)
+            def value_text
+          STATUS_RENAME[@status.to_s] || @status.to_s
         end
     
-    group :test do
-  gem 'minitest'
-  gem 'rr', require: false
-  gem 'rack-test', require: false
-end
+        set :csp, false
     
-          def store_meta(store)
-        json = as_json
-        json[:mtime] = Time.now.to_i
-        json[:db_size] = store.size(DB_FILENAME)
-        store.write(META_FILENAME, json.to_json)
-      end
-    end
+        html_filters.push 'clean_local_urls'
     
-        def absolute_url_string?(str)
-      str =~ SCHEME_RGX
-    end
-    
-        def mime_type
-      headers['Content-Type'] || 'text/plain'
-    end
-    
-        class << self
-      attr_accessor :dir
-    
-            css('img[style]').each do |node|
-          node['align'] ||= node['style'][/float:\s*(left|right)/, 1]
-          node['style'] = node['style'].split(';').map(&:strip).select { |s| s =~ /\Awidth|height/ }.join(';')
-        end
-    
-          def get_type
-        if slug.start_with?('guide/')
-          'Guide'
-        elsif slug.start_with?('cookbook/')
-          'Cookbook'
-        elsif slug == 'glossary'
-          'Guide'
-        else
-          type = at_css('.nav-title.is-selected').content.strip
-          type.remove! ' Reference'
-          type << ': #{mod}' if mod
-          type
-        end
-      end
-    
-      def internal_developer_commands
-    find_internal_commands HOMEBREW_LIBRARY_PATH/'dev-cmd'
-  end
-    
-        first_warning = true
-    methods.each do |method|
-      $stderr.puts 'Checking #{method}' if ARGV.debug?
-      unless checks.respond_to?(method)
-        Homebrew.failed = true
-        puts 'No check available by the name: #{method}'
-        next
-      end
-    
-      def framework_version
-    @framework_version ||= `rails -v`[/^Rails (.+)$/, 1]
-  end
+            subtitle = at_css('.hero-subtitle').try(:content)
+        breadcrumbs = css('.breadcrumbs li').map(&:content)[2..-2]
