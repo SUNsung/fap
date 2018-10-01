efa504fd562f701dc59a26b9c8ccffffa549a650
SUNsung <weatherius@gmail.com>
2018-10-01 07:59:00 +0300

    Rewrite [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2018-10-01 07:58:59

--- pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -1,199 +1,217 @@
 
         
-            * :class:`dict`
-    * :class:`tuple`
-    * :class:`bytes`
-    * :class:`~flask.Markup`
-    * :class:`~uuid.UUID`
-    * :class:`~datetime.datetime`
+        
+def check_format(filename):
     '''
-    
-        :param template_name_or_list: the name of the template to be
-                                  rendered, or an iterable with template names
-                                  the first one existing will be rendered
-    :param context: the variables that should be available in the
-                    context of the template.
+    validates that each line is formatted correctly,
+    appending to error list as needed
     '''
-    ctx = _app_ctx_stack.top
-    ctx.app.update_template_context(context)
-    return _render(ctx.app.jinja_env.get_or_select_template(template_name_or_list),
-                   context, ctx.app)
-    
-        # This test only works on CPython 2.7.
-    if sys.version_info >= (2, 7) and \
-            not hasattr(sys, 'pypy_translation_info'):
-        with assert_no_leak():
-            for x in range(10):
-                fire()
-    
-    try:
-    from greenlet import greenlet
-except ImportError:
-    greenlet = None
-    
-    pytestmark = pytest.mark.skipif(
-    blinker is None,
-    reason='Signals require the blinker library.'
-)
-    
-        h = history.history
-    assert_array_almost_equal(h['val_loss'], h['val_' + loss_full_name], decimal=decimal)
-    assert_array_almost_equal(h['val_loss'], h['val_weighted_' + loss_full_name], decimal=decimal)
-    
-        times = []
-    for use_cudnn in [True, False]:
-        start_time = time.time()
-        inputs = keras.layers.Input(shape=(None, input_size))
-        if use_cudnn:
-            if rnn_type == 'lstm':
-                layer = keras.layers.CuDNNLSTM(units)
+    with open(filename) as fp:
+        lines = list(line.rstrip() for line in fp)
+    check_alphabetical(lines)
+    # START Check Entries
+    num_in_category = min_entries_per_section + 1
+    category = ''
+    category_line = 0
+    for line_num, line in enumerate(lines):
+        if section_title_re.match(line):
+            title_links.append(section_title_re.match(line).group(1))
+        # check each section for the minimum number of entries
+        if line.startswith(anchor):
+            match = anchor_re.match(line)
+            if match:
+                if match.group(1) not in title_links:
+                    add_error(line_num, 'section header ({}) not added as a title link'.format(match.group(1)))
             else:
-                layer = keras.layers.CuDNNGRU(units)
-        else:
-            if rnn_type == 'lstm':
-                layer = keras.layers.LSTM(units)
-            else:
-                layer = keras.layers.GRU(units)
-        outputs = layer(inputs)
+                add_error(line_num, 'section header is not formatted correctly')
+            if num_in_category < min_entries_per_section:
+                add_error(category_line, '{} section does not have the minimum {} entries (only has {})'.format(
+                    category, min_entries_per_section, num_in_category))
+            category = line.split(' ')[1]
+            category_line = line_num
+            num_in_category = 0
+            continue
+        # skips lines that we do not care about
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        num_in_category += 1
+        segments = line.split('|')[1:-1]
+        if len(segments) < num_segments:
+            add_error(line_num, 'entry does not have all the required sections (have {}, need {})'.format(
+                len(segments), num_segments))
+            continue
+        # START Global
+        for segment in segments:
+            # every line segment should start and end with exactly 1 space
+            if len(segment) - len(segment.lstrip()) != 1 or len(segment) - len(segment.rstrip()) != 1:
+                add_error(line_num, 'each segment must start and end with exactly 1 space')
+        # END Global
+        segments = [seg.strip() for seg in segments]
+        check_entry(line_num, segments)
+    # END Check Entries
     
     
-@keras_test
-def test_max_norm():
-    array = get_example_array()
-    for m in get_test_values():
-        norm_instance = constraints.max_norm(m)
-        normed = norm_instance(K.variable(array))
-        assert(np.all(K.eval(normed) < m))
+class TrigramSimilar(PostgresSimpleLookup):
+    lookup_name = 'trigram_similar'
+    operator = '%%'
     
     
-# Artificial data generation:
-# Generate movies with 3 to 7 moving squares inside.
-# The squares are of shape 1x1 or 2x2 pixels,
-# which move linearly over time.
-# For convenience we first create movies with bigger width and height (80x80)
-# and at the end we select a 40x40 window.
+class ArrayMaxLengthValidator(MaxLengthValidator):
+    message = ngettext_lazy(
+        'List contains %(show_value)d item, it should contain no more than %(limit_value)d.',
+        'List contains %(show_value)d items, it should contain no more than %(limit_value)d.',
+        'limit_value')
     
-        x_train = np.array(x[:int(len(x) * (1 - test_split))])
-    y_train = np.array(y[:int(len(x) * (1 - test_split))])
-    x_test = np.array(x[int(len(x) * (1 - test_split)):])
-    y_test = np.array(y[int(len(x) * (1 - test_split)):])
-    return (x_train, y_train), (x_test, y_test)
+    # the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    
+        # Arguments
+        inputs (tensor): input tensor from input image or previous layer
+        num_filters (int): Conv2D number of filters
+        kernel_size (int): Conv2D square kernel dimensions
+        strides (int): Conv2D square stride dimensions
+        activation (string): activation name
+        batch_normalization (bool): whether to include batch normalization
+        conv_first (bool): conv-bn-activation (True) or
+            bn-activation-conv (False)
+    
+        fpath = os.path.join(path, 'test_batch')
+    x_test, y_test = load_batch(fpath)
+    
+        fpath = os.path.join(path, 'test')
+    x_test, y_test = load_batch(fpath, label_key=label_mode + '_labels')
+    
+    
+def print_summary(model, line_length=None, positions=None, print_fn=None):
+    '''Prints a summary of a model.
+    
+        # Test single image
+    x = np.random.uniform(0, 255, (10, 10, 3))
+    xint = x.astype('int32')
+    assert utils.preprocess_input(x).shape == x.shape
+    assert utils.preprocess_input(xint).shape == xint.shape
+    
+            if horizontal_flip:
+            coin = tf.less(tf.random_uniform([batch_size], 0, 1.0), 0.5)
+            shape = [-1., 0., width, 0., 1., 0., 0., 0.]
+            flip_transform = tf.convert_to_tensor(shape, dtype=tf.float32)
+            flip = tf.tile(tf.expand_dims(flip_transform, 0), [batch_size, 1])
+            noflip = tf.tile(tf.expand_dims(identity, 0), [batch_size, 1])
+            transforms.append(tf.where(coin, flip, noflip))
+    
+    Output after 4 epochs on CPU: ~0.8146
+Time per epoch on CPU (Core i7): ~150s.
+'''
+    
+        # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, num_decoder_tokens))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, target_token_index['\t']] = 1.
+    
+    
+(x_train, y_train), (x_test, y_test) = split_data(data_input, expected_output)
+print('x_train.shape: ', x_train.shape)
+print('y_train.shape: ', y_train.shape)
+print('x_test.shape: ', x_test.shape)
+print('y_test.shape: ', y_test.shape)
+    
+    model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
 
     
-        from sklearn.tree import DecisionTreeClassifier
     
-    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-TRAIN_FOLDER = '20news-bydate-train'
-TEST_FOLDER = '20news-bydate-test'
-    
-    In this examples we will use a movie review dataset.
-    
-    data, row_idx, col_idx = sg._shuffle(data, random_state=0)
-plt.matshow(data, cmap=plt.cm.Blues)
-plt.title('Shuffled dataset')
-    
-    acc_clf1, acc_clf2 = [], []
-n_features_range = range(1, n_features_max + 1, step)
-for n_features in n_features_range:
-    score_clf1, score_clf2 = 0, 0
-    for _ in range(n_averages):
-        X, y = generate_data(n_train, n_features)
-    
-        t0 = time()
-    scores = uniform_labelings_scores(score_func, n_samples, n_clusters_range)
-    print('done in %0.3fs' % (time() - t0))
-    plots.append(plt.errorbar(
-        n_clusters_range, np.median(scores, axis=1), scores.std(axis=1))[0])
-    names.append(score_func.__name__)
-    
-        plt.imshow(avg_dist, interpolation='nearest', cmap=plt.cm.gnuplot2,
-               vmin=0)
-    plt.xticks(range(n_clusters), labels, rotation=45)
-    plt.yticks(range(n_clusters), labels)
-    plt.colorbar()
-    plt.suptitle('Interclass %s distances' % metric, size=18)
-    plt.tight_layout()
+def CheckCall( args, **kwargs ):
+  try:
+    subprocess.check_call( args, **kwargs )
+  except subprocess.CalledProcessError as error:
+    sys.exit( error.returncode )
     
     
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
+def BuildServerConf():
+  '''Builds a dictionary mapping YCM Vim user options to values. Option names
+  don't have the 'ycm_' prefix.'''
+  # We only evaluate the keys of the vim globals and not the whole dictionary
+  # to avoid unicode issues.
+  # See https://github.com/Valloric/YouCompleteMe/pull/2151 for details.
+  keys = vimsupport.GetVimGlobalsKeys()
+  server_conf = {}
+  for key in keys:
+    if not key.startswith( YCM_VAR_PREFIX ):
+      continue
+    new_key = key[ len( YCM_VAR_PREFIX ): ]
+    new_value = vimsupport.VimExpressionToPythonType( 'g:' + key )
+    server_conf[ new_key ] = new_value
     
-            if infos:
-            writeln(' (%s)' % (', '.join(infos),))
-        else:
-            write('\n')
     
-    from . import Contract
+  def Done( self ):
+    return bool( self._response_future ) and self._response_future.done()
     
-    import six
-from twisted.internet import reactor, defer, task
+      return completion_data
     
-            path = self._schemes[scheme]
-        try:
-            dhcls = load_object(path)
-            dh = dhcls(self._crawler.settings)
-        except NotConfigured as ex:
-            self._notconfigured[scheme] = str(ex)
-            return None
-        except Exception as ex:
-            logger.error('Loading '%(clspath)s' for scheme '%(scheme)s'',
-                         {'clspath': path, 'scheme': scheme},
-                         exc_info=True,  extra={'crawler': self._crawler})
-            self._notconfigured[scheme] = str(ex)
-            return None
-        else:
-            self._handlers[scheme] = dh
-        return self._handlers[scheme]
     
-            self._signer = None
-        if is_botocore():
-            import botocore.auth
-            import botocore.credentials
-            kw.pop('anon', None)
-            if kw:
-                raise TypeError('Unexpected keyword arguments: %s' % kw)
-            if not self.anon:
-                SignerCls = botocore.auth.AUTH_TYPE_MAPS['s3']
-                self._signer = SignerCls(botocore.credentials.Credentials(
-                    aws_access_key_id, aws_secret_access_key))
-        else:
-            _S3Connection = _get_boto_connection()
-            try:
-                self.conn = _S3Connection(
-                    aws_access_key_id, aws_secret_access_key, **kw)
-            except Exception as ex:
-                raise NotConfigured(str(ex))
+def FormatDebugInfoResponse_Completer_ServerRunningWithHost_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Completer name completer debug information:\n'
+      '  Server name running at: http://127.0.0.1:1234\n'
+      '  Server name process ID: 12345\n'
+      '  Server name executable: /path/to/executable\n'
+      '  Server name logfiles:\n'
+      '    /path/to/stdout/logfile\n'
+      '    /path/to/stderr/logfile\n'
+      '  Server name key: value\n'
+      '  Key: value\n'
+    )
+  )
     
-        def download(self, download_func, request, spider):
-        @defer.inlineCallbacks
-        def process_request(request):
-            for method in self.methods['process_request']:
-                response = yield method(request=request, spider=spider)
-                assert response is None or isinstance(response, (Response, Request)), \
-                        'Middleware %s.process_request must return None, Response or Request, got %s' % \
-                        (six.get_method_self(method).__class__.__name__, response.__class__.__name__)
-                if response:
-                    defer.returnValue(response)
-            defer.returnValue((yield download_func(request=request,spider=spider)))
     
-        def _set_connection_attributes(self, request):
-        parsed = urlparse_cached(request)
-        self.scheme, self.netloc, self.host, self.port, self.path = _parsed_url_args(parsed)
-        proxy = request.meta.get('proxy')
-        if proxy:
-            self.scheme, _, self.host, self.port, _ = _parse(proxy)
-            self.path = self.url
+@patch( 'ycm.client.messages_request.PostVimMessage',
+        new_callable = ExtendedMock )
+def HandlePollResponse_MultipleMessages_test( post_vim_message ):
+  assert_that( _HandlePollResponse( [ { 'message': 'this is a message' },
+                                      { 'message': 'this is another one' } ] ,
+                                    None ),
+               equal_to( True ) )
     
-            namelist = zip_file.namelist()
-        body = zip_file.read(namelist[0])
-        respcls = responsetypes.from_args(filename=namelist[0], body=body)
-        return response.replace(body=body, cls=respcls)
     
-    from w3lib.http import basic_auth_header
+def ExtractKeywordsFromGroup_KeywordMiddle_test():
+  assert_that( syntax_parse._ExtractKeywordsFromGroup(
+                 syntax_parse.SyntaxGroup( '', [
+                   'foo contained bar',
+                   'zoo goo'
+                 ] ) ),
+               contains_inanyorder( 'foo', 'contained', 'bar', 'zoo', 'goo' ) )
     
-    class CoreStats(object):
+    
+__licence__ = 'BSD (3 clause)'
+    
+    # If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import sys
+import os
+import inspect
+    
+    
+async def async_setup_scanner(hass, config, async_see, discovery_info=None):
+    '''Set up the MySensors device scanner.'''
+    new_devices = mysensors.setup_mysensors_platform(
+        hass, DOMAIN, discovery_info, MySensorsDeviceScanner,
+        device_args=(async_see, ))
+    if not new_devices:
+        return False
+    
+            self.hosts = config[CONF_HOSTS]
+        self.exclude = config[CONF_EXCLUDE]
+        minutes = config[CONF_HOME_INTERVAL]
+        self._options = config[CONF_OPTIONS]
+        self.home_interval = timedelta(minutes=minutes)
