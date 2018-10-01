82c31098d483f5ec6132f6cea44c8d11b2a4771e
SUNsung <weatherius@gmail.com>
2018-10-01 08:20:20 +0300

    Rewrite [49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp] File in: 2018-10-01 08:20:20

--- cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
+++ cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
@@ -1,292 +1,253 @@
 
         
-        void SILLayout::Profile(llvm::FoldingSetNodeID &id,
-                        CanGenericSignature Generics,
-                        ArrayRef<SILField> Fields) {
-  id.AddPointer(Generics.getPointer());
-  for (auto &field : Fields) {
-    id.AddPointer(field.getLoweredType().getPointer());
-    id.AddBoolean(field.isMutable());
+        #if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef _module = {
+  PyModuleDef_HEAD_INIT,
+  kModuleName,
+  kModuleDocstring,
+  -1,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+#define INITFUNC PyInit__api_implementation
+#define INITFUNC_ERRORVAL NULL
+#else
+#define INITFUNC init_api_implementation
+#define INITFUNC_ERRORVAL
+#endif
+    
+      auto* message1_on_arena =
+      Arena::CreateMessage<protobuf_unittest::TestAllTypes>(&arena);
+  TestUtil::SetAllFields(message1_on_arena);
+  const auto* nested = &message1_on_arena->optional_nested_message();
+    
+    #include <google/protobuf/stubs/common.h>
+namespace google {
+namespace protobuf {
+namespace compiler {
+namespace csharp {
+    }
+    }
+    }
+    }
+    
+    #include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/compiler/plugin.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+    
+    template <typename DescriptorType>
+static void WriteDocCommentBody(
+    io::Printer* printer, const DescriptorType* descriptor) {
+  SourceLocation location;
+  if (descriptor->GetSourceLocation(&location)) {
+    WriteDocCommentBodyForLocation(printer, location);
   }
 }
-
     
-    using namespace swift;
-using namespace swift::syntax;
+    // Author: liujisi@google.com (Pherl Liu)
     
-      if (UnsupportedOS || UnsupportedArch)
-    return { UnsupportedOS, UnsupportedArch };
+        
     
-    DIRECTIONAL_PREPOSITION(above)
-DIRECTIONAL_PREPOSITION(after)
-DIRECTIONAL_PREPOSITION(along)
-DIRECTIONAL_PREPOSITION(alongside)
-DIRECTIONAL_PREPOSITION(as)
-DIRECTIONAL_PREPOSITION(at)
-DIRECTIONAL_PREPOSITION(before)
-DIRECTIONAL_PREPOSITION(below)
-DIRECTIONAL_PREPOSITION(by)
-DIRECTIONAL_PREPOSITION(following)
-DIRECTIONAL_PREPOSITION(for)
-DIRECTIONAL_PREPOSITION(from)
-DIRECTIONAL_PREPOSITION(given)
-DIRECTIONAL_PREPOSITION(in)
-DIRECTIONAL_PREPOSITION(including)
-DIRECTIONAL_PREPOSITION(inside)
-DIRECTIONAL_PREPOSITION(into)
-DIRECTIONAL_PREPOSITION(matching)
-DIRECTIONAL_PREPOSITION(of)
-DIRECTIONAL_PREPOSITION(on)
-DIRECTIONAL_PREPOSITION(passing)
-DIRECTIONAL_PREPOSITION(preceding)
-DIRECTIONAL_PREPOSITION(since)
-DIRECTIONAL_PREPOSITION(to)
-DIRECTIONAL_PREPOSITION(until)
-DIRECTIONAL_PREPOSITION(using)
-DIRECTIONAL_PREPOSITION(via)
-DIRECTIONAL_PREPOSITION(when)
-PREPOSITION(with)
-DIRECTIONAL_PREPOSITION(within)
-    
-    %# Ignore the following admonition; it applies to the resulting .cpp file only
-//// Automatically Generated From UnicodeExtendedGraphemeClusters.cpp.gyb.
-//// Do Not Edit Directly!
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
-    
-    #include 'llvm/ADT/PointerUnion.h'
-    
-    #endif // BITCOIN_QT_MACDOCKICONHANDLER_H
-
-    
-        QColor TextColor() const { return textColor; }
-    QColor SingleColor() const { return singleColor; }
-    
-    #endif // BITCOIN_REVERSELOCK_H
-
-    
-    static void secp256k1_gej_add_ge_var(secp256k1_gej *r, const secp256k1_gej *a, const secp256k1_ge *b, secp256k1_fe *rzr) {
-    /* 8 mul, 3 sqr, 4 normalize, 12 mul_int/add/negate */
-    secp256k1_fe z12, u1, u2, s1, s2, h, i, i2, h2, h3, t;
-    if (a->infinity) {
-        VERIFY_CHECK(rzr == NULL);
-        secp256k1_gej_set_ge(r, b);
-        return;
+        void ProgressWriter::UpdateDistributedSync(size_t samples, const ValuePtr& accumulatedMetric)
+    {
+        m_distributedSync->Update(samples, nullptr, accumulatedMetric,
+            [this](const std::pair<size_t, size_t> samples, std::pair<size_t, size_t> updates,
+                   const std::pair<double, double> /*aggregateLoss*/, std::pair<double, double> aggregateMetric)
+        {
+            OnWriteDistributedSyncUpdate(samples, updates, aggregateMetric);
+        });
     }
-    if (b->infinity) {
-        if (rzr != NULL) {
-            secp256k1_fe_set_int(rzr, 1);
+    
+    void ProgressWriter::WriteTrainingSummary(const ValuePtr& accumulatedLoss, const ValuePtr& accumulatedMetric)
+    {
+        m_training->WriteSummary(
+            accumulatedLoss, accumulatedMetric,
+            [this](size_t samples, size_t updates, size_t summaries, double aggregateLoss, double aggregateMetric,
+                   uint64_t elapsedMs)
+            {
+                OnWriteTrainingSummary(samples, updates, summaries, aggregateLoss, aggregateMetric, elapsedMs);
+            });
+    }
+    
+        template <typename ElementType>
+    void Value::CopyVariableValueToVector(const Variable& outputVariable, std::vector<std::vector<size_t>>& sequences)
+    {
+        if (outputVariable.Shape()[0] != outputVariable.Shape().TotalSize())
+            InvalidArgument('For sparse data, the outputVariable's leading axis dimensionality (%zu) must equal the total size (%zu) of the Variable '%S'.',
+                            outputVariable.Shape()[0], outputVariable.Shape().TotalSize(), outputVariable.AsString().c_str());
+    }
+    
+        void Variable::SetOwner(const std::weak_ptr<Function>& ownerFunction)
+    {
+        if (Kind() != VariableKind::Output)
+            LogicError('Variable '%S' SetOwner: Owner can only be set for Output Variables', AsString().c_str());
+    }
+    
+        auto& pMBLayout = minibatch.begin()->second.pMBLayout;
+    // This is only allowed for old readers, which support a single layout for all inputs.
+    for (const auto& iter : minibatch)
+    {
+        assert(iter.second.pMBLayout == pMBLayout);
+        // TODO: This should be a runtime check, not an assert() that only runs in Debug.
+        UNUSED(iter);
+    }
+    
+    
+    {    ~ScopeTimer()
+    {
+        if (m_verbosity > 2)
+        {
+            m_aggregateTimer.Stop();
+            double time = m_aggregateTimer.ElapsedSeconds();
+            fprintf(stderr, m_message.c_str(), time);
         }
-        *r = *a;
-        return;
     }
-    r->infinity = 0;
-    }
-    
-    
-    {    RejectDifficultyMismatch(difficulty, expected_difficulty);
-}
-    
-        // Escapes
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\ Baz\'', {
-            {'Foo', 'Bar Baz'},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\Baz\'', {
-            {'Foo', 'BarBaz'},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\@Baz\'', {
-            {'Foo', 'Bar@Baz'},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\\'Baz\' Spam=\'\\\'Eggs\\\'\'', {
-            {'Foo', 'Bar\'Baz'},
-            {'Spam', '\'Eggs\''},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar\\\\Baz\'', {
-            {'Foo', 'Bar\\Baz'},
-        });
-    
-    cog.outl()
-cog.outl('#define TINYFORMAT_FOREACH_ARGNUM(m) \\\n    ' +
-         ' '.join(['m(%d)' % (j,) for j in range(1,maxParams+1)]))
-]]]*/
-#define TINYFORMAT_ARGTYPES_1 class T1
-#define TINYFORMAT_ARGTYPES_2 class T1, class T2
-#define TINYFORMAT_ARGTYPES_3 class T1, class T2, class T3
-#define TINYFORMAT_ARGTYPES_4 class T1, class T2, class T3, class T4
-#define TINYFORMAT_ARGTYPES_5 class T1, class T2, class T3, class T4, class T5
-#define TINYFORMAT_ARGTYPES_6 class T1, class T2, class T3, class T4, class T5, class T6
-#define TINYFORMAT_ARGTYPES_7 class T1, class T2, class T3, class T4, class T5, class T6, class T7
-#define TINYFORMAT_ARGTYPES_8 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8
-#define TINYFORMAT_ARGTYPES_9 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9
-#define TINYFORMAT_ARGTYPES_10 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10
-#define TINYFORMAT_ARGTYPES_11 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class T11
-#define TINYFORMAT_ARGTYPES_12 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class T11, class T12
-#define TINYFORMAT_ARGTYPES_13 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class T11, class T12, class T13
-#define TINYFORMAT_ARGTYPES_14 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class T11, class T12, class T13, class T14
-#define TINYFORMAT_ARGTYPES_15 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class T11, class T12, class T13, class T14, class T15
-#define TINYFORMAT_ARGTYPES_16 class T1, class T2, class T3, class T4, class T5, class T6, class T7, class T8, class T9, class T10, class T11, class T12, class T13, class T14, class T15, class T16
-    
-        strKey = 'moon';
-    BOOST_CHECK(obj.pushKV(strKey, true));
-    
-      image_file.read(reinterpret_cast<char*>(&magic), 4);
-  magic = swap_endian(magic);
-  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
-  label_file.read(reinterpret_cast<char*>(&magic), 4);
-  magic = swap_endian(magic);
-  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
-  image_file.read(reinterpret_cast<char*>(&num_items), 4);
-  num_items = swap_endian(num_items);
-  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
-  num_labels = swap_endian(num_labels);
-  CHECK_EQ(num_items, num_labels);
-  image_file.read(reinterpret_cast<char*>(&rows), 4);
-  rows = swap_endian(rows);
-  image_file.read(reinterpret_cast<char*>(&cols), 4);
-  cols = swap_endian(cols);
-    
-      /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to the data.
-   *
-   * @param datum
-   *    Datum containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See data_layer.cpp for an example.
-   */
-  void Transform(const Datum& datum, Blob<Dtype>* transformed_blob);
-    
-    namespace caffe {
-    }
-    
-    
-    {  Blob<Dtype> col_buffer_;
-  Blob<Dtype> bias_multiplier_;
 };
     
-      vector<shared_ptr<Batch<Dtype> > > prefetch_;
-  BlockingQueue<Batch<Dtype>*> prefetch_free_;
-  BlockingQueue<Batch<Dtype>*> prefetch_full_;
-  Batch<Dtype>* prefetch_current_;
-    
-    /**
- * @brief Computes a sum of two input Blobs, with the shape of the latter Blob
- *        'broadcast' to match the shape of the former. Equivalent to tiling
- *        the latter Blob, then computing the elementwise sum.
- *
- * The second input may be omitted, in which case it's learned as a parameter
- * of the layer. Note: in case bias and scaling are desired, both operations can
- * be handled by `ScaleLayer` configured with `bias_term: true`.
- */
-template <typename Dtype>
-class BiasLayer : public Layer<Dtype> {
- public:
-  explicit BiasLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange& fr) override
+    {
+        auto sliceInputValue  = InputRef(0).ValueFor(fr);
+        auto sliceOutputValue =           ValueFor(fr); // row vector
     }
     
-    namespace caffe {
-    }
-    
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of ReLULayer.
- */
-template <typename Dtype>
-class CuDNNReLULayer : public ReLULayer<Dtype> {
- public:
-  explicit CuDNNReLULayer(const LayerParameter& param)
-      : ReLULayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNReLULayer();
-    }
+    #include 'Basics.h'
+#include 'ComputationNode.h'
+#include 'ScriptableObjects.h'
+#include 'TensorShape.h'
+#include 'Matrix.h'
     
     
-    {}  // namespace caffe
-    
-    std::unique_ptr<php::Unit> parse_unit(php::Program& prog,
-                                      std::unique_ptr<UnitEmitter> ue);
-    
-    void Assembler::nor(const Reg64& ra, const Reg64& rs, const Reg64& rb,
-                    bool rc) {
-  EmitXForm(31, rn(rs), rn(ra), rn(rb), 124, rc);
+    {  // Verify that the size of the key space not touched by the reads
+  // is pretty much unchanged.
+  const int64_t final_other_size = Size(Key(n), Key(kCount));
+  ASSERT_LE(final_other_size, initial_other_size + 1048576);
+  ASSERT_GE(final_other_size, initial_other_size/5 - 1048576);
 }
     
-    struct DataStreamWrapper final : Stream::Wrapper {
-  DataStreamWrapper() {
-    m_isLocal = true;
+        TableBuilder* builder = new TableBuilder(options, file);
+    meta->smallest.DecodeFrom(iter->key());
+    for (; iter->Valid(); iter->Next()) {
+      Slice key = iter->key();
+      meta->largest.DecodeFrom(key);
+      builder->Add(key, iter->value());
+    }
+    
+    // Number of bytes to use as a cache of uncompressed data.
+// Negative means use default settings.
+static int FLAGS_cache_size = -1;
+    
+        // Handle key/value, add to state, etc.
+    bool drop = false;
+    if (!ParseInternalKey(key, &ikey)) {
+      // Do not hide error keys
+      current_user_key.clear();
+      has_current_user_key = false;
+      last_sequence_for_key = kMaxSequenceNumber;
+    } else {
+      if (!has_current_user_key ||
+          user_comparator()->Compare(ikey.user_key,
+                                     Slice(current_user_key)) != 0) {
+        // First occurrence of this user key
+        current_user_key.assign(ikey.user_key.data(), ikey.user_key.size());
+        has_current_user_key = true;
+        last_sequence_for_key = kMaxSequenceNumber;
+      }
+    }
+    
+    
+    {  // No copying allowed
+  LookupKey(const LookupKey&);
+  void operator=(const LookupKey&);
+};
+    
+    class StdoutPrinter : public WritableFile {
+ public:
+  virtual Status Append(const Slice& data) {
+    fwrite(data.data(), 1, data.size(), stdout);
+    return Status::OK();
   }
-    }
+  virtual Status Close() { return Status::OK(); }
+  virtual Status Flush() { return Status::OK(); }
+  virtual Status Sync() { return Status::OK(); }
+};
     
-    inline const String& ExecutionContext::getSandboxId() const {
-  return m_sandboxId;
+      scratch->clear();
+  record->clear();
+  bool in_fragmented_record = false;
+  // Record offset of the logical record that we're reading
+  // 0 is a dummy value to make compilers happy
+  uint64_t prospective_record_offset = 0;
+    
+    
+    { private:
+  std::string dbname_;
+  Env* env_;
+  DB* db_;
+};
+    
+    template<typename Key, class Comparator>
+typename SkipList<Key,Comparator>::Node*
+SkipList<Key,Comparator>::NewNode(const Key& key, int height) {
+  char* mem = arena_->AllocateAligned(
+      sizeof(Node) + sizeof(port::AtomicPointer) * (height - 1));
+  return new (mem) Node(key);
 }
     
-    #include 'hphp/runtime/base/file-util.h'
-#include 'hphp/runtime/base/stream-wrapper-registry.h'
-#include 'hphp/runtime/base/unit-cache.h'
-#include 'hphp/runtime/ext/std/ext_std_file.h'
-#include 'hphp/util/logger.h'
+    //////////////////////////////////////////////////////////////////////////////
+// parse JSON from file
+//////////////////////////////////////////////////////////////////////////////
     
-    
-//--------------------------------------------------------------
-// The following part are API Registration of Simple Operators
-//--------------------------------------------------------------
-/*!
- * \brief Macro to register simple operator to both imperative and symbolic API.
- *
- * see src/operator/elementwise_unary_op-inl.h for example
- *
- * \code
- * // example of registering a sigmoid operator on GPU
- * // MySigmoid is of type UnaryFunction,
- * // MySigmoidGrad is of type UnaryGradFunctionT2
- *
- * MXNET_REGISTER_SIMPLE_OP(sigmoid, cpu)
- * .set_function(MySigmoid<gpu>, true)
- * .set_gradient(MySigmoidGrad<gpu>, true)
- * .describe('Sigmoid function');
- *
- * \endcode
- */
-#define MXNET_REGISTER_SIMPLE_OP(Name, DEV)                             \
-  static ::mxnet::op::SimpleOpRegEntry &                                \
-  __make_ ## SimpleOpRegEntry ## _ ## Name ## __ ## DEV ##__ =          \
-      ::mxnet::op::SimpleOpRegistry::Get()->__REGISTER_OR_FIND__(#Name)
-    
-    MXNET_REGISTER_OP_PROPERTY(CaffeOp, CaffeOpProp)
-.describe('Apply caffe operator')
-.add_argument('data', 'Symbol[]', 'List of tensors')
-.add_arguments(CaffeOpParam::__FIELDS__());
-    
-    #include <dmlc/base.h>
-#include <mxnet/base.h>
-#include <cstddef>
-#include <array>
+    #include <cmath>
+#include <iosfwd>
+#include <limits>
 #include <string>
-#include <mutex>
-#include '../common/cuda_utils.h'
+#include <vector>
     
-    #include './exec_pass.h'
+      BENCHMARK_NORETURN ~CheckHandler() BENCHMARK_NOEXCEPT_OP(false) {
+    log_ << std::endl;
+    CallAbortHandler();
+  }
+    
+      // Accumulators.
+  std::vector<int> n;
+  std::vector<double> real_time;
+  std::vector<double> cpu_time;
+    
+    inline int& LogLevel() {
+  static int log_level = 0;
+  return log_level;
+}
+    
+    
+    { private:
+  bool init_;
+// Underlying regular expression object
+#if defined(HAVE_STD_REGEX)
+  std::regex re_;
+#elif defined(HAVE_POSIX_REGEX) || defined(HAVE_GNU_POSIX_REGEX)
+  regex_t re_;
+#else
+#error No regular expression backend implementation available
+#endif
+};
+    
+      const CPUInfo &info = context.cpu_info;
+  Out << 'Run on (' << info.num_cpus << ' X '
+      << (info.cycles_per_second / 1000000.0) << ' MHz CPU '
+      << ((info.num_cpus > 1) ? 's' : '') << ')\n';
+  if (info.caches.size() != 0) {
+    Out << 'CPU Caches:\n';
+    for (auto &CInfo : info.caches) {
+      Out << '  L' << CInfo.level << ' ' << CInfo.type << ' '
+          << (CInfo.size / 1000) << 'K';
+      if (CInfo.num_sharing != 0)
+        Out << ' (x' << (info.num_cpus / CInfo.num_sharing) << ')';
+      Out << '\n';
+    }
+  }
