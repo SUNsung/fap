598724b69a659b50098b6acb26079b6ba9f7f1e8
SUNsung <weatherius@gmail.com>
2018-10-01 21:55:16 +0300

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2018-10-01 21:55:15

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,87 +1,114 @@
 
         
-        def global_require
-  JSON.pretty_generate(DATA)
+        def content_for(file)
+  contents = File.read(file)
+  case file
+  when 'History.markdown'
+    converted_history(contents)
+  else
+    contents.gsub(%r!\A# .*\n\n?!, '')
+  end
 end
     
-    require 'erb'
+    Nullam luctus fermentum est id blandit. Phasellus consectetur ullamcorper
+ligula, {% if author == 'Jane Doe' %} at finibus eros laoreet id. {% else %}
+Etiam sit amet est in libero efficitur.{% endif %}
+tristique. Ut nec magna augue. Quisque ut fringilla lacus, ac dictum enim.
+Aliquam vel ornare mauris. Suspendisse ornare diam tempor nulla facilisis
+aliquet. Sed ultrices placerat ultricies.
+LIQUID
     
-    class BuildEnvironment
-  def initialize(*settings)
-    @settings = Set.new(*settings)
-  end
+    def pre_pr(url)
+  url[-1, 1] == FORWARD_SLASH ? url : File.dirname(url)
+end
     
-      def app_caveats
-    if keg && keg.app_installed?
-      <<-EOS.undent
-        .app bundles were installed.
-        Run `brew linkapps #{keg.name}` to symlink these to /Applications.
-      EOS
-    end
-  end
+    require 'benchmark/ips'
+require 'jekyll'
+require 'json'
     
-        if ARGV.include? '--list-checks'
-      puts checks.all.sort
-      exit
-    end
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-      def search
-    if ARGV.empty?
-      puts_columns Formula.full_names
-    elsif ARGV.include? '--macports'
-      exec_browser 'https://www.macports.org/ports.php?by=name&substr=#{ARGV.next}'
-    elsif ARGV.include? '--fink'
-      exec_browser 'http://pdb.finkproject.org/pdb/browse.php?summary=#{ARGV.next}'
-    elsif ARGV.include? '--debian'
-      exec_browser 'https://packages.debian.org/search?keywords=#{ARGV.next}&searchon=names&suite=all&section=all'
-    elsif ARGV.include? '--opensuse'
-      exec_browser 'https://software.opensuse.org/search?q=#{ARGV.next}'
-    elsif ARGV.include? '--fedora'
-      exec_browser 'https://admin.fedoraproject.org/pkgdb/packages/%2A#{ARGV.next}%2A/'
-    elsif ARGV.include? '--ubuntu'
-      exec_browser 'http://packages.ubuntu.com/search?keywords=#{ARGV.next}&searchon=names&suite=all&section=all'
-    elsif ARGV.include? '--desc'
-      query = ARGV.next
-      rx = query_regexp(query)
-      Descriptions.search(rx, :desc).print
-    elsif ARGV.first =~ HOMEBREW_TAP_FORMULA_REGEX
-      query = ARGV.first
-      user, repo, name = query.split('/', 3)
+      Jekyll::Command.subclasses.each { |c| c.init_with_program(p) }
     
-      def migrate_tap_migration
-    report[:D].each do |full_name|
-      name = full_name.split('/').last
-      next unless (dir = HOMEBREW_CELLAR/name).exist? # skip if formula is not installed.
-      next unless new_tap_name = tap.tap_migrations[name] # skip if formula is not in tap_migrations list.
-      tabs = dir.subdirs.map { |d| Tab.for_keg(Keg.new(d)) }
-      next unless tabs.first.tap == tap # skip if installed formula is not from this tap.
-      new_tap = Tap.fetch(new_tap_name)
-      new_tap.install unless new_tap.installed?
-      # update tap for each Tab
-      tabs.each { |tab| tab.tap = new_tap }
-      tabs.each(&:write)
-    end
-  end
+              new_theme_name = args.join('_')
+          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
     
-            format('%*e', -20, 109.52).should == '1.095200e+02        '
-        format('%*E', -20, 109.52).should == '1.095200E+02        '
-        format('%*f', -20, 10.952).should == '10.952000           '
-        format('%*g', -20, 12.1234).should == '12.1234             '
-        format('%*G', -20, 12.1234).should == '12.1234             '
-        format('%*a', -20, 196).should == '0x1.88p+7           '
-        format('%*A', -20, 196).should == '0X1.88P+7           '
+            def connect(websocket, handshake)
+          @connections_count += 1
+          if @connections_count == 1
+            message = 'Browser connected'
+            message += ' over SSL/TLS' if handshake.secure?
+            Jekyll.logger.info 'LiveReload:', message
+          end
+          websocket.send(
+            JSON.dump(
+              :command    => 'hello',
+              :protocols  => ['http://livereload.com/protocols/official-7'],
+              :serverName => 'jekyll'
+            )
+          )
     
-        $?.should be_an_instance_of Process::Status
-    $?.success?.should == false
-    $?.exitstatus.should == 1
-  end
+    module Jekyll
+  module Converters
+    class Markdown
+      class KramdownParser
+        CODERAY_DEFAULTS = {
+          'css'               => 'style',
+          'bold_every'        => 10,
+          'line_numbers'      => 'inline',
+          'line_number_start' => 1,
+          'tab_width'         => 4,
+          'wrap'              => 'div',
+        }.freeze
     
-          it 'raises TypeError if passed not Integer' do
-        -> { warn '', uplevel: '' }.should raise_error(TypeError)
-        -> { warn '', uplevel: [] }.should raise_error(TypeError)
-        -> { warn '', uplevel: {} }.should raise_error(TypeError)
-        -> { warn '', uplevel: Object.new }.should raise_error(TypeError)
-      end
+        def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
     end
   end
 end
+
+    
+        if ARGV.ignore_deps?
+      @deps = []
+      @reqs = []
+    else
+      @deps = expand_deps
+      @reqs = expand_reqs
+    end
+  end
+    
+      def <<(o)
+    @settings << o
+    self
+  end
+    
+      # True if a {Formula} is being built with a specific option.
+  # <pre>args << '--i-want-spam' if build.with? 'spam'
+  #
+  # args << '--qt-gui' if build.with? 'qt' # '--with-qt' ==> build.with? 'qt'
+  #
+  # # If a formula presents a user with a choice, but the choice must be fulfilled:
+  # if build.with? 'example2'
+  #   args << '--with-example2'
+  # else
+  #   args << '--with-example1'
+  # end</pre>
+  def with?(val)
+    option_names = val.respond_to?(:option_names) ? val.option_names : [val]
