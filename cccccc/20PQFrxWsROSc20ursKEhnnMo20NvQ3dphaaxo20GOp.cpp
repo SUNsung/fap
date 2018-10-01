e56fb0712afd707184c18184494178656db85ba1
SUNsung <weatherius@gmail.com>
2018-10-02 01:28:35 +0300

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2018-10-02 01:28:34

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,140 +1,243 @@
 
         
-        namespace mxnet {
-    }
+        #endif  // TENSORFLOW_PYTHON_FRAMEWORK_CPP_SHAPE_INFERENCE_H_
+
     
-    #define CHECK_NEXT_TIMING
+    // Whether the op should be prefixed with underscore.
+bool IsOpWithUnderscorePrefix(const string& s);
     
-    void OpenMP::set_reserve_cores(int cores) {
-  CHECK_GE(cores, 0);
-  reserve_cores_ = cores;
-#ifdef _OPENMP
-  if (reserve_cores_ >= omp_thread_max_) {
-    omp_set_num_threads(1);
-  } else {
-    omp_set_num_threads(omp_thread_max_ - reserve_cores_);
+    void CostAnalyzer::PredictCosts(CostEstimator* cost_estimator,
+                                CostGraphDef* cost_graph, int64* total_time) {
+  TF_CHECK_OK(cost_estimator->Initialize(*item_));
+  Costs costs;
+  const Status status =
+      cost_estimator->PredictCosts(item_->graph, cost_graph, &costs);
+  *total_time = costs.execution_time.count();
+  if (!status.ok()) {
+    LOG(ERROR) << 'Could not estimate the cost for item ' << item_->id << ': '
+               << status.error_message();
+    return;
   }
-#endif
 }
     
+    ModelAnalyzer::ModelAnalyzer(const GrapplerItem& item) : item_(item) {}
     
-    { private:
-  /*! \brief Concurrency for thread pool */
-  static constexpr std::size_t kNumWorkingThreads = 16;
-  /*! \brief Maximum number of GPUs */
-  static constexpr std::size_t kMaxNumGpus = 16;
-  /*!\brief number of streams allocated for each GPU */
-  static constexpr std::size_t kNumStreamsPerGpu = 16;
-  /*!
-   * \brief Streams.
-   */
-  std::unique_ptr<StreamManager<kMaxNumGpus, kNumStreamsPerGpu>> streams_;
-  /*!
-   * \brief Task queues.
-   */
-  std::shared_ptr<dmlc::ConcurrentBlockingQueue<OprBlock*>> task_queue_;
-  std::shared_ptr<dmlc::ConcurrentBlockingQueue<OprBlock*>> io_task_queue_;
-  /*!
-   * \brief Thread pools.
-   */
-  std::unique_ptr<ThreadPool> thread_pool_;
-  std::unique_ptr<ThreadPool> io_thread_pool_;
-  /*!
-   * \brief Worker.
-   * \param task_queue Queue to work on.
-   *
-   * The method to pass to thread pool to parallelize.
-   */
-  void ThreadWorker(std::shared_ptr<dmlc::ConcurrentBlockingQueue<OprBlock*>> task_queue,
-                    const std::shared_ptr<dmlc::ManualEvent>& ready_event) {
-    OprBlock* opr_block;
-    ready_event->signal();
-    while (task_queue->Pop(&opr_block)) {
-      DoExecute(opr_block);
+    namespace tensorflow {
     }
+    
+    // Performs a NumPy array cast from type 'From' to 'To'.
+template <typename From, typename To>
+void NPyCast(void* from_void, void* to_void, npy_intp n, void* fromarr,
+             void* toarr) {
+  const From* from = reinterpret_cast<From*>(from_void);
+  To* to = reinterpret_cast<To*>(to_void);
+  for (npy_intp i = 0; i < n; ++i) {
+    to[i] = static_cast<To>(from[i]);
   }
-  /*!
-   * \brief Execute an operation.
-   * \param opr_block The operator block.
-   */
-  void DoExecute(OprBlock* opr_block) {
-    assert(opr_block->wait.load() == 0);
-    if (opr_block->ctx.dev_mask() == gpu::kDevMask) {
-      #if MXNET_USE_CUDA
-      CUDA_CALL(cudaSetDevice(opr_block->ctx.dev_id));
-      #else   // MXNET_USE_CUDA
-      LOG(FATAL) << 'Please compile with CUDA enabled';
-      #endif  // MXNET_USE_CUDA
-    }
-    bool is_copy = (opr_block->opr->prop == FnProperty::kCopyFromGPU ||
-                    opr_block->opr->prop == FnProperty::kCopyToGPU);
-    auto&& rctx = is_copy
-        ? streams_->GetIORunContext(opr_block->ctx)
-        : streams_->GetRunContext(opr_block->ctx);
-    this->ExecuteOprBlock(rctx, opr_block);
-  }
-  /*!
-   * \brief Push the operation to the queue.
-   * \param opr_block The operator block.
-   */
-  void DoPushToQueue(OprBlock* opr_block) {
-    switch (opr_block->opr->prop) {
-      case FnProperty::kCopyFromGPU:
-      case FnProperty::kCopyToGPU: {
-        io_task_queue_->Push(opr_block);
-        break;
-      }
-      default: {
-        task_queue_->Push(opr_block);
-        break;
-      }
-    }
-  }
-};
-    
-      // get the shape hints
-  std::string shape_hints_key = std::string(attr_name) + '_hints';
-  if (ret.attrs.count(shape_hints_key)) {
-    nnvm::NodeEntryMap<AttrType> shape_hints =
-      ret.GetAttr<nnvm::NodeEntryMap<AttrType>>(shape_hints_key);
-    for (const auto& kv : shape_hints) {
-      nnvm::NodeEntry e = kv.first;
-      if (idx.exist(e.node.get())) {
-        rshape[idx.entry_id(kv.first)] = kv.second;
-      }
-    }
-  }
-    
-    
-    {private:
-    CC_DISALLOW_COPY_AND_ASSIGN(Liquid);
-};
-    
-    
-    {    return nullptr;
 }
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
+// a copy.
+Status TensorToNdarray(const Tensor& t, PyObject** ret);
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    namespace tensorflow {
+    }
+    
+    namespace tensorflow {
+namespace detail {
+    }
+    }
+    
+    std::ostream& operator<<(std::ostream& os, ComputationType ty) {
+  return os << ComputationTypeString(ty);
+}
+    
+    class CUDAExecutor;
+class ScopedActivateContext;
+    
+         * For example:
+     * - 0 Means that the action just started.
+     * - 0.5 Means that the action is in the middle.
+     * - 1 Means that the action is over.
+     *
+     * @param time A value between 0 and 1.
+     */
+    virtual void update(float time);
+    /** Return certain target.
+     *
+     * @return A certain target.
+     */
+    Node* getTarget() const { return _target; }
+    /** The action will modify the target properties. 
+     *
+     * @param target A certain target.
+     */
+    void setTarget(Node *target) { _target = target; }
+    /** Return a original Target. 
+     *
+     * @return A original Target.
+     */
+    Node* getOriginalTarget() const { return _originalTarget; }
+    /** 
+     * Set the original target, since target can be nil.
+     * Is the target that were used to run the action. Unless you are doing something complex, like ActionManager, you should NOT call this method.
+     * The target is 'assigned', it is not 'retained'.
+     * @since v0.8.2
+     *
+     * @param originalTarget Is 'assigned', it is not 'retained'.
+     */
+    void setOriginalTarget(Node *originalTarget) { _originalTarget = originalTarget; }
+    /** Returns a tag that is used to identify the action easily. 
+     *
+     * @return A tag.
+     */
+    int getTag() const { return _tag; }
+    /** Changes the tag that is used to identify the action easily. 
+     *
+     * @param tag Used to identify the action easily.
+     */
+    void setTag(int tag) { _tag = tag; }
+    /** Returns a flag field that is used to group the actions easily.
+     *
+     * @return A tag.
+     */
+    unsigned int getFlags() const { return _flags; }
+    /** Changes the flag field that is used to group the actions easily.
+     *
+     * @param flags Used to group the actions easily.
+     */
+    void setFlags(unsigned int flags) { _flags = flags; }
+    
+    
+    {    points->autorelease();
+    return points;
+}
+    
+        /** Returns a new copy of the array reversed. User is responsible for releasing this copy.
+     *
+     * @js NA
+     * @return A new copy of the array reversed.
+     */
+    PointArray* reverse() const;
+    
+    CallFuncN * CallFuncN::clone() const
+{
+    // no copy constructor
+    auto a = new (std::nothrow) CallFuncN();
+    }
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __ACTION_CCPAGETURN3D_ACTION_H__
+#define __ACTION_CCPAGETURN3D_ACTION_H__
+    
+    bool ProgressFromTo::initWithDuration(float duration, float fromPercentage, float toPercentage)
+{
+    if (ActionInterval::initWithDuration(duration))
+    {
+        _to = toPercentage;
+        _from = fromPercentage;
+    }
+    }
+    
+        /** 
+     * @brief Initializes the action with the number of jumps, the sin amplitude, the grid size and the duration.
+     * @param duration Specify the duration of the JumpTiles3D action. It's a value in seconds.
+     * @param gridSize Specify the size of the grid.
+     * @param numberOfJumps Specify the jump tiles count.
+     * @param amplitude Specify the amplitude of the JumpTiles3D action.
+     * @return If the initialization success, return true; otherwise, return false.
+     */
+    bool initWithDuration(float duration, const Size& gridSize, unsigned int numberOfJumps, float amplitude);
     
     The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
     
-                // We scale z here to avoid the animation being
-            // too much bigger than the screen due to perspective transform
     
-    bool ShuffleTiles::initWithDuration(float duration, const Size& gridSize, unsigned int seed)
-{
-    if (TiledGrid3DAction::initWithDuration(duration, gridSize))
+    {// end of base_node group
+/// @}
+    
+      /**
+   * @brief Send messages
+   * @param frames The messages to send.
+   * @param frame_num The amount of messages to send.
+   * @return The status of the sending action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                 int32_t *const frame_num) override;
+    
+    TEST_F(FakeCanClientTest, SendMessage) {
+  std::vector<CanFrame> frames;
+  frames.resize(FRAME_LEN);
+  for (int32_t i = 0; i < FRAME_LEN; ++i) {
+    frames[i].id = 1 & 0x3FF;
+    frames[i].len = 8;
+    frames[i].data[7] = 1 % 256;
+    for (int32_t j = 0; j < 7; ++j) {
+      frames[i].data[j] = j;
+    }
+  }
+    }
+    
+      // 1. set baudrate to 500k
+  ret = bcan_set_baudrate(_dev_handler, BCAN_BAUDRATE_500K);
+  if (ret != ErrorCode::OK) {
+    AERROR << 'Set baudrate error Code: ' << ret;
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
+  }
+    
+    #endif  // MODULES_DRIVERS_CANBUS_CANCARD_CLIENT_SOCKET_CAN_CLIENT_RAW_H_
+
+    
+    #include 'gtest/gtest.h'
+    
+    
     {
-        _seed = seed;
-        _tilesOrder = nullptr;
-        _tiles = nullptr;
-    }
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    TEST(CanSenderTest, OneRunCase) {
+  CanSender<::apollo::canbus::ChassisDetail> sender;
+  can::FakeCanClient can_client;
+  sender.Init(&can_client, true);
     }
     
-    protected:
-    unsigned int _cols;
-    Size _winSize;
-    
-    void AnimationCache::parseVersion2(const ValueMap& animations)
-{
-    SpriteFrameCache *frameCache = SpriteFrameCache::getInstance();
-    }
+    /**
+ * @file
+ * @brief The class of ProtocolData
+ */
