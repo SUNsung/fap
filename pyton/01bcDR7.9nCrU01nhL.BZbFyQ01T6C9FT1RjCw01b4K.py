a0606efeb77be420810bf8c537d5c00e5ab1ef48
SUNsung <weatherius@gmail.com>
2018-10-01 06:41:01 +0300

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2018-10-01 06:41:01

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,125 +1,132 @@
 
         
-            states_t_bxn, outputs_t_bxn = sess.run([states_t, outputs_t],
-                                           feed_dict=feed_dict)
-    states_nxt = np.transpose(np.squeeze(np.asarray(states_t_bxn)))
-    outputs_t_bxn = np.squeeze(np.asarray(outputs_t_bxn))
-    r_sxt = np.dot(P_nxn, states_nxt)
-    
-          else:
-        to_fill_in = num_steps - len(example)
-        final_x = example + [EOS_INDEX] * to_fill_in
-        final_y = final_x[1:] + [EOS_INDEX]
-        w[i] = [1] * len(example) + [0] * to_fill_in
-    
-          for s in xrange(t, FLAGS.sequence_length):
-        cum_advantage += missing_list[s] * np.power(gamma,
-                                                    (s - t)) * rewards_list[s]
-      cum_advantage -= baselines[t]
-      # Clip advantages.
-      cum_advantage = tf.clip_by_value(cum_advantage, -FLAGS.advantage_clipping,
-                                       FLAGS.advantage_clipping)
-      advantages.append(missing_list[t] * cum_advantage)
-      final_gen_objective += tf.multiply(
-          log_probability, missing_list[t] * tf.stop_gradient(cum_advantage))
-    
-      def decoder_fn(time, cell_state, cell_input, cell_output, context_state):
-    '''Decoder function used in the `dynamic_rnn_decoder` for inference.
+        _codes = {
+    }
     
     
-class ExitStatus:
-    '''Exit status code constants.'''
-    OK = 0
-    ERROR = 1
-    PLUGIN_ERROR = 7
-    
-            if isinstance(headers, bytes):
-            # Python < 3
-            headers = headers.decode('utf8')
-        return headers
-    
-    
-setup(
-    name='httpie',
-    version=httpie.__version__,
-    description=httpie.__doc__.strip(),
-    long_description=long_description(),
-    url='http://httpie.org/',
-    download_url='https://github.com/jakubroztocil/httpie',
-    author=httpie.__author__,
-    author_email='jakub@roztocil.co',
-    license=httpie.__licence__,
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'http = httpie.__main__:main',
-        ],
-    },
-    extras_require=extras_require,
-    install_requires=install_requires,
-    tests_require=tests_require,
-    cmdclass={'test': PyTest},
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development',
-        'Topic :: System :: Networking',
-        'Topic :: Terminals',
-        'Topic :: Text Processing',
-        'Topic :: Utilities'
-    ],
-)
-
+@pytest.mark.parametrize(
+    'value, length', (
+        ('', 0),
+        ('T', 1),
+        ('Test', 4),
+        ('Cont', 0),
+        ('Other', -5),
+        ('Content', None),
+    ))
+def test_iter_slices(value, length):
+    if length is None or (length <= 0 and len(value) > 0):
+        # Reads all content at once
+        assert len(list(iter_slices(value, length))) == 1
+    else:
+        assert len(list(iter_slices(value, 1))) == length
     
     
-def test_auth_plugin_require_auth_false_and_auth_provided(httpbin):
+def check_compatibility(urllib3_version, chardet_version):
+    urllib3_version = urllib3_version.split('.')
+    assert urllib3_version != ['dev']  # Verify urllib3 isn't installed from git.
     
-        def test_POST_with_data_auto_JSON_headers(self, httpbin):
-        r = http('POST', httpbin.url + '/post', 'a=b')
-        assert HTTP_OK in r
-        assert r.json['headers']['Accept'] == JSON_ACCEPT
-        assert r.json['headers']['Content-Type'] == 'application/json'
+        :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
+    
+            start = datetime.now()
+        ridge = linear_model.Ridge(alpha=1.)
+        ridge.fit(X, Y)
+        time_ridge[i] = total_seconds(datetime.now() - start)
+    
+        if args.plot is not None:
+        max_val = getattr(args, args.plot)
+        if args.plot in ('classes', 'samples'):
+            min_val = 2
+        else:
+            min_val = 0
+        steps = np.linspace(min_val, max_val, num=args.n_steps + 1)[1:]
+        if args.plot in ('classes', 'samples'):
+            steps = np.unique(np.round(steps).astype(int))
+        setattr(args, args.plot, steps)
+    
+                print('Fast K-Means')
+            # let's prepare the data in small chunks
+            mbkmeans = MiniBatchKMeans(init='k-means++',
+                                       n_clusters=10,
+                                       batch_size=chunk)
+            tstart = time()
+            mbkmeans.fit(data)
+            delta = time() - tstart
+            print('Speed: %0.3fs' % delta)
+            print('Inertia: %f' % mbkmeans.inertia_)
+            print()
+            print()
+    
+            start = time.time()
+        func(X, n_jobs=1)
+        one_core.append(time.time() - start)
+    
+                gc.collect()
+            print('benchmarking scikit-learn randomized_svd: n_iter=0')
+            tstart = time()
+            randomized_svd(X, rank, n_iter=0)
+            results['scikit-learn randomized_svd (n_iter=0)'].append(
+                time() - tstart)
+    
+                print('=======================')
+            print('Round %d %d' % (i, j))
+            print('n_features:', n_features)
+            print('n_samples:', n_train)
+    
+    ROOT_URL = 'https://api.github.com/repos/scikit-learn/scikit-learn.github.io/contents/'  # noqa
+RAW_FMT = 'https://raw.githubusercontent.com/scikit-learn/scikit-learn.github.io/master/%s/documentation.html'  # noqa
+VERSION_RE = re.compile(r'\bVERSION:\s*'([^']+)'')
+NAMED_DIRS = ['dev', 'stable']
+    
+        if not app.config.edit_on_github_project:
+        warnings.warn('edit_on_github_project not specified')
+        return
+    if not doctree:
+        warnings.warn('doctree is None')
+        return
+    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
+    show_url = get_github_url(app, 'blob', path)
+    edit_url = get_github_url(app, 'edit', path)
+    
+    from . import flash_briefings, intent, smart_home
+from .const import (
+    CONF_AUDIO, CONF_DISPLAY_URL, CONF_TEXT, CONF_TITLE, CONF_UID, DOMAIN,
+    CONF_FILTER, CONF_ENTITY_CONFIG)
     
     
-filenames = list(rst_filenames())
-assert filenames
+@asyncio.coroutine
+def async_trigger(hass, config, action):
+    '''Listen for state changes based on configuration.'''
+    if CONF_AT in config:
+        at_time = config.get(CONF_AT)
+        hours, minutes, seconds = at_time.hour, at_time.minute, at_time.second
+    else:
+        hours = config.get(CONF_HOURS)
+        minutes = config.get(CONF_MINUTES)
+        seconds = config.get(CONF_SECONDS)
     
-    def acfun_download_by_vid(vid, title, output_dir='.', merge=True, info_only=False, **kwargs):
-    '''str, str, str, bool, bool ->None
+    For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/browser/
+'''
+import voluptuous as vol
     
+        def _get_value(self, hass, data, config_key):
+        '''Get value.'''
+        customize = hass.data.get(DATA_CUSTOMIZE, {}).get(config_key) or {}
+        return {'global': customize, 'local': data.get(config_key, {})}
     
-def get_file_path(merge, output_dir, title, url):
-    mime, ext, size = url_info(url)
-    file_name = get_output_filename([], title, ext, output_dir, merge)
-    file_path = os.path.join(output_dir, file_name)
-    return file_name, file_path
+        @asyncio.coroutine
+    def get(self, request):
+        '''Request suite status.'''
+        inp = yield from hassbian_status(request.app['hass'], self._test_mode)
     
-        for quality in ['1080','720','480','380','240','144','auto']:
-        try:
-            real_url = info[quality][1]['url']
-            if real_url:
-                break
-        except KeyError:
-            pass
-    
-                if not info_only:
-                try:
-                    download_urls([real_url], title, ext, size, output_dir, merge = merge)
-                except:
-                    pass
-    
-    from ..common import *
-import json
+            # At this point it is difficult to tell if a connection is established.
+        # So just check for null objects.
+        if self.fritz_box is None or not self.fritz_box.modelname:
+            self.success_init = False
