0cf6ea86bfa85a1de9806bb4ed62aaeb455909c8
SUNsung <weatherius@gmail.com>
2018-10-02 13:42:58 +0300

    Rewrite [02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb] File in: 2018-10-02 13:42:57

--- ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
+++ ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
@@ -1,19 +1,27 @@
 
         
-        Given /^(?:|[tT]hat )?following user[s]?(?: exist[s]?)?:$/ do |table|
-  table.hashes.each do |hash|
-    if hash.has_key? 'username' and hash.has_key? 'email'
-      step %{a user named '#{hash['username']}' with email '#{hash['email']}'}
-    elsif hash.has_key? 'username'
-      step %{a user with username '#{hash['username']}'}
-    elsif hash.has_key? 'email'
-      step %{a user with email '#{hash['email']}'}
+              private
+    
+            def template
+          @template ||= Coverage::Template.new(self)
+        end
+    
+    # It is very likely that we'll need these and as some of those paths will atm
+# result in a I18n deprecation warning, we load those here now so that we can
+# get rid of that warning.
+require 'active_support/core_ext/string/strip'
+require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/array/conversions'
+# TODO: check what this actually does by the time we're going to add support for
+# other locales.
+require 'i18n'
+if I18n.respond_to?(:enforce_available_locales=)
+  I18n.enforce_available_locales = false
+end
+    
+          def actual_path
+        $PROGRAM_NAME
+      end
     end
   end
 end
-    
-    require 'cucumber/api_steps'
-    
-        it 'generates the aspects_manage_contacts_json fixture', fixture: true do
-      # adds one not mutual contact
-      bob.share_with(FactoryGirl.create(:person), @aspect)
