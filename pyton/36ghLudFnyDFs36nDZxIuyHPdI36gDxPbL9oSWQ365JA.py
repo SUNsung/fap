2c52b30d3a1cd3cc0efd0f36f3327233b7b3cdb8
SUNsung <weatherius@gmail.com>
2018-10-02 11:07:07 +0300

    Rewrite [36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py] File in: 2018-10-02 11:07:06

--- pyton/36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py
+++ pyton/36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py
@@ -1,164 +1,86 @@
 
         
-        versions_info['signature'] = signature
-with open('update/versions.json', 'w') as versionsf:
-    json.dump(versions_info, versionsf, indent=4, sort_keys=True)
-
+            # Separating the 'table of contents' from the contents (blocks)
+    table_of_contents = ''.join(read_me.split('- - -')[0])
+    blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
+    for i in range(len(blocks)):
+        if i == 0:
+            blocks[i] = blocks[i] + '\n'
+        else:
+            blocks[i] = '# ' + blocks[i] + '\n'
+    
+        self.path_embeddings = path_embeddings
+    self.relata_embeddings = relata_embeddings
+    self.nc_embeddings = nc_embeddings
+    
+    if S < N:
+  # Note that this isn't necessary for this synthetic example, but
+  # it's useful to see how the input factor matrices were initialized
+  # for actual neurophysiology data.
+  datasets = add_alignment_projections(datasets, npcs=FLAGS.npcs)
+    
+      if not FLAGS.dis_share_embedding:
+    decoder_embedding = [
+        v for v in tf.trainable_variables()
+        if v.op.name == 'dis/decoder/rnn/embedding'
+    ][0]
+    
+            if not any(os.path.isfile('%s/certutil' % x) for x in os.environ['PATH'].split(os.pathsep)):
+            xlog.warn('please install *libnss3-tools* package to import GoAgent root ca')
+            return False
+    
+    log_file = os.path.join(data_path, 'ipv6_tunnel.log')
+    
+        def nat_type_probe(self):
+        print('Starting probe NAT type...')
+        self.nat_type = 'probing'
+        server_ip_list = self.server_ip_list.copy()
+        self.rs_cone_flag = 1
+        for server_ip in server_ip_list:
+            qualified = self.qualify_loop(server_ip)
+            if qualified:
+                break
+        if qualified is None:
+            self.rs_cone_flag = 0
+            while server_ip_list:
+                server_ip = server_ip_list.pop()
+                qualified = self.qualify_loop(server_ip)
+                if qualified:
+                    break
+        if qualified is None:
+            self.qualified = False
+            return 'offline'
+        ra_cone_flag, first_indicate = qualified
+        if ra_cone_flag:
+            self.qualified = True
+            return 'cone'
+        qualified = None
+        for server_ip in  server_ip_list:
+            qualified = self.qualify_loop(server_ip)
+            if qualified:
+                break
+        if qualified is None:
+            self.last_server_ip = server_ip
+            self.qualified = True
+            return 'unknown'
+        ra_cone_flag, second_indicate = qualified
+        if first_indicate == second_indicate:
+            self.qualified = True
+            return 'restricted'
+        else:
+            self.qualified = False
+            return 'symmetric'
+    
+    \note Please be warned that the line numbers in the API documentation do not
+match the real locations in the source code of the package. This is an
+unintended artifact of doxygen, which I could only convince to use the
+correct module names by concatenating all files from the package into a single
+module file...
+    
+    EOF = -1
     
     
-total_bytes = 0
-    
-    
-class AcademicEarthCourseIE(InfoExtractor):
-    _VALID_URL = r'^https?://(?:www\.)?academicearth\.org/playlists/(?P<id>[^?#/]+)'
-    IE_NAME = 'AcademicEarth:Course'
-    _TEST = {
-        'url': 'http://academicearth.org/playlists/laws-of-nature/',
-        'info_dict': {
-            'id': 'laws-of-nature',
-            'title': 'Laws of Nature',
-            'description': 'Introduce yourself to the laws of nature with these free online college lectures from Yale, Harvard, and MIT.',
-        },
-        'playlist_count': 3,
-    }
-    
-            return info_dict
-
-    
-    
-def basic_auth(header=BASIC_AUTH_HEADER_VALUE):
-    
-    from httpie.plugins import plugin_manager
-from httpie.context import Environment
-    
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
-    
-        # Auth
-    def get_auth_plugins(self):
-        return [plugin for plugin in self if issubclass(plugin, AuthPlugin)]
-    
-    
-def test_credentials_in_url_auth_flag_has_priority(httpbin_both):
-    '''When credentials are passed in URL and via -a at the same time,
-     then the ones from -a are used.'''
-    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
-                   auth='user:wrong')
-    r = http('--auth=user:password', 'GET', url)
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-        # a Model inside a Sequential
-    x = Input(shape=(1,))
-    y = Dense(2)(x)
-    inner_model = Model(x, y)
-    outer_model = Sequential()
-    outer_model.add(inner_model)
-    assert outer_model.trainable_weights == inner_model.trainable_weights
-    inner_model.trainable = False
-    assert outer_model.trainable_weights == []
-    inner_model.trainable = True
-    inner_model.layers[-1].trainable = False
-    assert outer_model.trainable_weights == []
-    
-    from keras.layers import Lambda, Input, Dense
-from keras.models import Model
-from keras.datasets import mnist
-from keras.losses import mse, binary_crossentropy
-from keras.utils import plot_model
-from keras import backend as K
-    
-    max_words = 1000
-batch_size = 32
-epochs = 5
-    
-    Compares the performance of a simple MLP using two
-different activation functions: RELU and SELU
-on the Reuters newswire topic classification task.
-    
-        # Arguments
-        path: path where to cache the dataset locally
-            (relative to ~/.keras/datasets).
-        test_split: fraction of the data to reserve as test set.
-        seed: Random seed for shuffling the data
-            before computing the test split.
-    
-        fpath = os.path.join(path, 'test_batch')
-    x_test, y_test = load_batch(fpath)
-    
-        dirname = 'cifar-100-python'
-    origin = 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
-    path = get_file(dirname, origin=origin, untar=True)
-    
-            if response.status_code != 200:
-            path += '.error'
-    
-            # global, resource
-        input_url = 'https://www.googleapis.com/compute/v1/projects/myproject/global/urlMaps'
-        actual = GCPUtils.parse_gcp_url(input_url)
-        self.assertEquals('compute', actual['service'])
-        self.assertEquals('v1', actual['api_version'])
-        self.assertEquals('myproject', actual['project'])
-        self.assertTrue('global' in actual)
-        self.assertTrue(actual['global'])
-        self.assertEquals('urlMaps', actual['resource_name'])
-    
-    from ansible.utils.vars import combine_vars
-    
-    AnsibleConstructor.add_constructor(
-    u'!unsafe',
-    AnsibleConstructor.construct_yaml_unsafe)
-    
-        def start_requests(self):
-        qargs = {'total': self.total, 'show': self.show}
-        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
-        return [scrapy.Request(url, dont_filter=True)]
-    
-        def adjust_request_args(self, args):
-        args['url'] = self.args[0]
-        return args
-    
-    
-class UserAgentMiddleware(object):
-    '''This middleware allows spiders to override the user_agent'''
-    
-    class ScrapyDeprecationWarning(Warning):
-    '''Warning category for deprecated features, since the default
-    DeprecationWarning is silenced on Python 2.7+
-    '''
-    pass
-    
-    from scrapy import signals
-    
-    
-class MissingNonceTest(unittest.TestCase):
-    '''Tests for acme.errors.MissingNonce.'''
-    
-    # Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'certbot-apache.tex', u'certbot-apache Documentation',
-     u'Certbot Project', 'manual'),
-]
-    
-        def __init__(self, args):
-        '''Initializes the plugin with the given command line args'''
-        self._temp_dir = tempfile.mkdtemp()
-        self.le_config = util.create_le_config(self._temp_dir)
-        config_dir = util.extract_configs(args.configs, self._temp_dir)
-        self._configs = [
-            os.path.join(config_dir, config)
-            for config in os.listdir(config_dir)]
-    
-    # Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
+    def __str__(self):
+        if self.inserted is not None and self.token is not None:
+            return 'MissingTokenException(inserted %r at %r)' % (
+                self.inserted, self.token.text)
