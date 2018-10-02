c84683fafe8b2bd1561237d94ef47985585c3b7b
SUNsung <weatherius@gmail.com>
2018-10-02 10:29:39 +0300

    Rewrite [37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py] File in: 2018-10-02 10:29:39

--- pyton/37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py
+++ pyton/37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py
@@ -1,214 +1,194 @@
 
         
-        
-def _check_cryptography(cryptography_version):
-    # cryptography < 1.3.4
-    try:
-        cryptography_version = list(map(int, cryptography_version.split('.')))
-    except ValueError:
-        return
+            def _escalate_call(self):
+        self.call.state = CallState.READY
+        call = self.call
+        self.call = None
+        self.call_center.notify_call_escalated(call)
     
-        def __call__(self, r):
-        # Initialize per-thread state, if needed
-        self.init_per_thread_state()
-        # If we have a saved nonce, skip the 401
-        if self._thread_local.last_nonce:
-            r.headers['Authorization'] = self.build_digest_header(r.method, r.url)
+        def get_person(self, person_id):
+        person_server = self.lookup[person_id]
+        return person_server.people[person_id]
+    
+        def parse_query(self, query):
+        '''Remove markup, break text into terms, deal with typos,
+        normalize capitalization, convert to use boolean operations.
+        '''
+        ...
+    
+        def add_link_to_crawl(self, url):
+        '''Add the given link to `links_to_crawl`.'''
+        pass
+    
+            RESULT = 'porn' in webpage.lower()
+    
+        def create_asset(self, release_id, asset):
+        asset_name = os.path.basename(asset)
+        url = self._UPLOADS_URL % (release_id, asset_name)
+        # Our files are small enough to be loaded directly into memory.
+        data = open(asset, 'rb').read()
+        req = sanitized_Request(url, data)
+        mime_type, _ = mimetypes.guess_type(asset_name)
+        req.add_header('Content-Type', mime_type or 'application/octet-stream')
+        return self._call(req)
+    
+    import json
+import sys
+import hashlib
+import os.path
+    
+    versions_info = json.load(open('update/versions.json'))
+if 'signature' in versions_info:
+    del versions_info['signature']
+    
+    
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
+    
+    
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
+    
+    
+FORM_CONTENT_TYPE = 'application/x-www-form-urlencoded; charset=utf-8'
+JSON_CONTENT_TYPE = 'application/json'
+JSON_ACCEPT = '{0}, */*'.format(JSON_CONTENT_TYPE)
+DEFAULT_UA = 'HTTPie/%s' % __version__
+    
+    from utils import http, add_auth, HTTP_OK, MockEnvironment
+import httpie.input
+import httpie.cli
+    
+    
+def test_DELETE(httpbin_both):
+    r = http('DELETE', httpbin_both + '/delete')
+    assert HTTP_OK in r
+    
+    
+class PostgresSimpleLookup(Lookup):
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s %s %s' % (lhs, self.operator, rhs), params
+    
+        def __init__(self):
+        self.name = 'citext'
+    
+    
+class ArrayMinLengthValidator(MinLengthValidator):
+    message = ngettext_lazy(
+        'List contains %(show_value)d item, it should contain no fewer than %(limit_value)d.',
+        'List contains %(show_value)d items, it should contain no fewer than %(limit_value)d.',
+        'limit_value')
+    
+        def save(self, must_create=False):
+        '''
+        Save the session data. If 'must_create' is True, create a new session
+        object (or raise CreateError). Otherwise, only update an existing
+        object and don't create one (raise UpdateError if needed).
+        '''
+        raise NotImplementedError('subclasses of SessionBase must provide a save() method')
+    
+        def _get_session_from_db(self):
         try:
-            self._thread_local.pos = r.body.tell()
-        except AttributeError:
-            # In the case of HTTPDigestAuth being reused and the body of
-            # the previous request was a file-like object, pos has the
-            # file position of the previous body. Ensure it's set to
-            # None.
-            self._thread_local.pos = None
-        r.register_hook('response', self.handle_401)
-        r.register_hook('response', self.handle_redirect)
-        self._thread_local.num_401_calls = 1
+            return self.model.objects.get(
+                session_key=self.session_key,
+                expire_date__gt=timezone.now()
+            )
+        except (self.model.DoesNotExist, SuspiciousOperation) as e:
+            if isinstance(e, SuspiciousOperation):
+                logger = logging.getLogger('django.security.%s' % e.__class__.__name__)
+                logger.warning(str(e))
+            self._session_key = None
     
-    
-@pytest.fixture
-def httpbin(httpbin):
-    return prepare_url(httpbin)
-    
-        @possible_keys
-    def test_getitem(self, key):
-        assert self.case_insensitive_dict[key] == 'application/json'
-    
-            p = PreparedRequest()
-        p.prepare(
-            method=request.method.upper(),
-            url=request.url,
-            files=request.files,
-            data=request.data,
-            json=request.json,
-            headers=merge_setting(request.headers, self.headers, dict_class=CaseInsensitiveDict),
-            params=merge_setting(request.params, self.params),
-            auth=merge_setting(auth, self.auth),
-            cookies=merged_cookies,
-            hooks=merge_hooks(request.hooks, self.hooks),
-        )
-        return p
-    
-        def test_session_hooks_are_used_with_no_request_hooks(self, httpbin):
-        hook = lambda x, *args, **kwargs: x
-        s = requests.Session()
-        s.hooks['response'].append(hook)
-        r = requests.Request('GET', httpbin())
-        prep = s.prepare_request(r)
-        assert prep.hooks['response'] != []
-        assert prep.hooks['response'] == [hook]
-    
-        def triger_check_network(self, fail=False, force=False):
-        time_now = time.time()
-        if not force:
-            if self._checking_num > 0:
-                return
-    
-            begin_recv = time.time()
-        while time.time() < self.timeout + begin_recv:
-            qualified = self.prober_dict[dst_ip]['ra_packets'].get()
-            if qualified:
-                return qualified
-            time.sleep(0.01)
-    
-                # Is there a bitwise operation to do this?
-            if v == 0xFFFF:
-                v = -1
-    
-    class BaseRecognizer(object):
-    '''
-    @brief Common recognizer functionality.
-    
-    A generic recognizer that can handle recognizers generated from
-    lexer, parser, and tree grammars.  This is all the parsing
-    support code essentially; most of it is error recovery stuff and
-    backtracking.
-    '''
-    
-        @classmethod
-    # pylint: disable=arguments-differ,too-many-arguments
-    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
-        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
-        # jwk field if kid is not provided.
-        include_jwk = kid is None
-        return super(JWS, cls).sign(payload, key=key, alg=alg,
-                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
-                                    nonce=nonce, url=url, kid=kid,
-                                    include_jwk=include_jwk)
+        def get_decoded(self):
+        session_store_class = self.get_session_store_class()
+        return session_store_class().decode(self.session_data)
 
     
-    
-class VirtualHostTest(unittest.TestCase):
-    '''Test the VirtualHost class.'''
-    
-    
-def main(path):
+        The Django sessions framework is entirely cookie-based. It does
+    not fall back to putting session IDs in URLs. This is an intentional
+    design decision. Not only does that behavior make URLs ugly, it makes
+    your site vulnerable to session-ID theft via the 'Referer' header.
     
     
-  def Start( self ):
-    self.PostDataToHandler( {},
-                            'shutdown',
-                            TIMEOUT_SECONDS,
-                            display_message = False )
-    
-      AddToGroupMap( 'Conditional', statement_group )
-  AddToGroupMap( 'Repeat'     , statement_group )
-  AddToGroupMap( 'Label'      , statement_group )
-  AddToGroupMap( 'Operator'   , statement_group )
-  AddToGroupMap( 'Keyword'    , statement_group )
-  AddToGroupMap( 'Exception'  , statement_group )
+def x_robots_tag(func):
+    @wraps(func)
+    def inner(request, *args, **kwargs):
+        response = func(request, *args, **kwargs)
+        response['X-Robots-Tag'] = 'noindex, noodp, noarchive'
+        return response
+    return inner
     
     
-def FormatDebugInfoResponse_Completer_ServerNotRunningWithNoLogfiles_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  response[ 'completer' ][ 'servers' ][ 0 ].update( {
-    'is_running': False,
-    'logfiles': []
-  } )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Completer name completer debug information:\n'
-      '  Server name not running\n'
-      '  Server name executable: /path/to/executable\n'
-      '  No logfiles available\n'
-      '  Server name key: value\n'
-      '  Key: value\n'
-    )
-  )
-
-    
-      # We don't want the requests to actually be sent to the server, just have it
-  # return success.
-  with patch( 'ycm.client.completer_available_request.'
-              'CompleterAvailableRequest.PostDataToHandler',
-              return_value = True ):
-    with patch( 'ycm.client.completion_request.CompletionRequest.'
-                'PostDataToHandlerAsync',
-                return_value = MagicMock( return_value=True ) ):
-    
-      return {
-    'extra_data': extra_data,
-    'insertion_text': insertion_text,
-    'menu_text': menu_text,
-    'extra_menu_info': extra_menu_info,
-    'kind': kind,
-    'detailed_info': detailed_info,
-  }
-    
-          assert_that( ycm.IsServerAlive(), equal_to( False ) )
-      post_vim_message.assert_called_once_with(
-        'Unable to start the ycmd server. Cannot find Python 2.7 or 3.4+. '
-        'Set the 'g:ycm_server_python_interpreter' option to a Python '
-        'interpreter path. '
-        'Correct the error then restart the server with ':YcmRestartServer'.' )
+def print_summary(model, line_length=None, positions=None, print_fn=None):
+    '''Prints a summary of a model.
     
     
-def run_test(setup, test, iterations_per_test=5, tests_to_run=10):
-    fastest_execution = min(timeit.Timer(test, setup=setup).repeat(tests_to_run, iterations_per_test))
-    execution_time = fastest_execution / iterations_per_test
-    fps = 1.0 / execution_time
-    return execution_time, fps
-    
-    # Note: This isn't exactly the same as a 'percent match'. The scale isn't linear. But you can assume that images with a
-# smaller distance are more similar to each other than ones with a larger distance.
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-        # If no valid image file was uploaded, show the file upload form:
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
+def test_objective_shapes_3d():
+    y_a = K.variable(np.random.random((5, 6, 7)))
+    y_b = K.variable(np.random.random((5, 6, 7)))
+    for obj in allobj:
+        objective_output = obj(y_a, y_b)
+        assert K.eval(objective_output).shape == (5, 6)
     
     
-@click.command()
-@click.argument('image_to_check')
-@click.option('--cpus', default=1, help='number of CPU cores to use in parallel. -1 means 'use all in system'')
-@click.option('--model', default='hog', help='Which face detection model to use. Options are 'hog' or 'cnn'.')
-def main(image_to_check, cpus, model):
-    # Multi-core processing only supported on Python 3.4 or greater
-    if (sys.version_info < (3, 4)) and cpus != 1:
-        click.echo('WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!')
-        cpus = 1
+def get_data():
+    (x_train, y_train), _ = test_utils.get_test_data(
+        num_train=batch_size,
+        num_test=batch_size,
+        input_shape=(data_dim,),
+        classification=True,
+        num_classes=num_classes)
+    y_train = np_utils.to_categorical(y_train, num_classes)
     
-                face_names.append(name)
+    # The default options which are only relevant to the client, not the server and
+# thus are not part of default_options.json, but are required for a working
+# YouCompleteMe object.
+DEFAULT_CLIENT_OPTIONS = {
+  'g:ycm_server_python_interpreter': '',
+  'g:ycm_log_level': 'info',
+  'g:ycm_keep_logfiles': 0,
+  'g:ycm_extra_conf_vim_data': [],
+  'g:ycm_show_diagnostics_ui': 1,
+  'g:ycm_echo_current_diagnostic': 1,
+  'g:ycm_enable_diagnostic_signs': 1,
+  'g:ycm_enable_diagnostic_highlighting': 0,
+  'g:ycm_always_populate_location_list': 0,
+}
     
-    # Get a reference to the Raspberry Pi camera.
-# If this fails, make sure you have a camera connected to the RPi and that you
-# enabled your camera in raspi-config and rebooted first.
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-output = np.empty((240, 320, 3), dtype=np.uint8)
+        with MockCurrentColumnAndLineContents( 5, 'føo ' ):
+      ok_( base.CurrentIdentifierFinished() )
     
-        # Every 128 frames (the default batch size), batch process the list of frames to find faces
-    if len(frames) == 128:
-        batch_of_face_locations = face_recognition.batch_face_locations(frames, number_of_times_to_upsample=0)
+      current_buffer = VimBuffer( 'buffer', filetype = 'cpp' )
+  with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
+    with patch( 'ycm.client.event_notification.EventNotification.Response',
+                return_value = {} ):
+      ycm.ShowDiagnostics()
+    
+    
+def _worker( executor_reference, work_queue ):
+  try:
+    while True:
+      work_item = work_queue.get( block=True )
+      if work_item is not None:
+        work_item.run()
+        continue
+      executor = executor_reference()
+      # Exit if:
+      #   - The executor that owns the worker has been collected OR
+      #   - The executor that owns the worker has been shutdown.
+      if executor is None or executor._shutdown:
+        # Notice other workers
+        work_queue.put( None )
+        return
+      del executor
+  except BaseException:
+    _base.LOGGER.critical( 'Exception in worker', exc_info=True )
