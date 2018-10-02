7156d75db77a7b9c74a0e4cf2aa16c2812cde133
SUNsung <weatherius@gmail.com>
2018-10-02 12:08:57 +0300

    Rewrite [564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb] File in: 2018-10-02 12:08:56

--- ruby/564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb
+++ ruby/564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb
@@ -1,245 +1,69 @@
 
         
-              if path.symlink? || path.directory?
-        next
-      elsif path.extname == '.la'
-        path.unlink
-      else
-        # Set permissions for executables and non-executables
-        perms = if path.mach_o_executable? || path.text_executable?
-          0555
-        else
-          0444
-        end
-        if ARGV.debug?
-          old_perms = path.stat.mode & 0777
-          if perms != old_perms
-            puts 'Fixing #{path} permissions from #{old_perms.to_s(8)} to #{perms.to_s(8)}'
-          end
-        end
-        path.chmod perms
-      end
-    end
-  end
-end
-
-    
-          # Find commands in Homebrew/dev-cmd
-      if ARGV.homebrew_developer?
-        puts
-        puts 'Built-in development commands'
-        puts_columns internal_development_commands
-      end
-    
-    group :app do
-  gem 'rack'
-  gem 'sinatra'
-  gem 'sinatra-contrib'
-  gem 'thin'
-  gem 'sprockets'
-  gem 'sprockets-helpers'
-  gem 'erubi'
-  gem 'browser'
-  gem 'sass'
-  gem 'coffee-script'
-end
-    
-        def docs
-      @docs ||= begin
-        cookie = memoized_cookies['docs']
-    
-      def self.generate_manifest
-    Manifest.new(store, all_versions).store
+          it 'accepts a Float' do
+    sleep(0.1).should be_close(0, 2)
   end
     
-        def build_page(id, &block)
-      raise NotImplementedError
-    end
+        path = Commands.path(cmd)
     
-        def version
-      context[:version]
-    end
+    module Homebrew
+  module_function
     
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
-      end
-    end
-    
-        def terminal_width
-      return @terminal_width if defined? @terminal_width
-    
-        def new
-      authorize :email_domain_block, :create?
-      @email_domain_block = EmailDomainBlock.new
-    end
-    
-      def maxheight_or_default
-    params[:maxheight].present? ? params[:maxheight].to_i : nil
-  end
-end
-
-    
-      def set_account
-    @account = Account.find(params[:id])
-  end
-end
-
-    
+          # If this importer is based on files on the local filesystem This method
+      # should return true if the file, when changed, should trigger a
+      # recompile.
       #
-  # The resource path relative to the root of a server mount point.
-  #
-  attr_accessor :relative_resource
-    
-      include Proto
-    
-    =begin
-   +-------------+---------------+-------------------------------------+
-   | VALUE       | Name          | Description                         |
-   +-------------+---------------+-------------------------------------+
-   | 0x01        | Hangup        | The call has been hungup at the     |
-   |             |               | remote end                          |
-   |             |               |                                     |
-   | 0x02        | Reserved      | Reserved for future use             |
-   |             |               |                                     |
-   | 0x03        | Ringing       | Remote end is ringing (ring-back)   |
-   |             |               |                                     |
-   | 0x04        | Answer        | Remote end has answered             |
-   |             |               |                                     |
-   | 0x05        | Busy          | Remote end is busy                  |
-   |             |               |                                     |
-   | 0x06        | Reserved      | Reserved for future use             |
-   |             |               |                                     |
-   | 0x07        | Reserved      | Reserved for future use             |
-   |             |               |                                     |
-   | 0x08        | Congestion    | The call is congested               |
-   |             |               |                                     |
-   | 0x09        | Flash Hook    | Flash hook                          |
-   |             |               |                                     |
-   | 0x0a        | Reserved      | Reserved for future use             |
-   |             |               |                                     |
-   | 0x0b        | Option        | Device-specific options are being   |
-   |             |               | transmitted                         |
-   |             |               |                                     |
-   | 0x0c        | Key Radio     | Key Radio                           |
-   |             |               |                                     |
-   | 0x0d        | Unkey Radio   | Unkey Radio                         |
-   |             |               |                                     |
-   | 0x0e        | Call Progress | Call is in progress                 |
-   |             |               |                                     |
-   | 0x0f        | Call          | Call is proceeding                  |
-   |             | Proceeding    |                                     |
-   |             |               |                                     |
-   | 0x10        | Hold          | Call is placed on hold              |
-   |             |               |                                     |
-   | 0x11        | Unhold        | Call is taken off hold              |
-   +-------------+---------------+-------------------------------------+
-=end
-    
-    module Rex
-module Proto
-module IPMI
-  require 'rex/proto/ipmi/channel_auth_reply'
-  require 'rex/proto/ipmi/open_session_reply'
-  require 'rex/proto/ipmi/rakp2'
-    
-      def self.checksum(data)
-    sum = 0
-    data.unpack('C*').each {|c| sum += c }
-    sum = ~sum + 1
-    sum & 0xff
-  end
-    
-            # Sends a Kerberos Request over a tcp connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [Integer] the number of bytes sent
-        # @raise [RuntimeError] if the request can't be encoded
-        def send_request_tcp(req)
-          data = req.encode
-          length = [data.length].pack('N')
-          connection.put(length + data)
-        end
-    
-                res = ''
-            case etype
-            when RC4_HMAC
-              res = decrypt_rc4_hmac(cipher, key, msg_type)
-              raise ::RuntimeError, 'EncryptedData failed to decrypt' if res.length < 8
-              res = res[8, res.length - 1]
-            else
-              raise ::NotImplementedError, 'EncryptedData schema is not supported'
-            end
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
-  end
-end
-    
-      class FetchWebfinger < Base
-    def perform(*_args)
-      # don't do real discovery in cucumber
+      # It is acceptable for non-sass files to be watched and trigger a recompile.
+      #
+      # @param filename [String] The absolute filename for a file that has changed.
+      # @return [Boolean] When the file changed should cause a recompile.
+      def watched_file?(filename)
+        false
+      end
     end
   end
 end
 
     
-      def fill_in_new_user_form
-    @username = 'ohai'
-    fill_in('user_email', with: '#{@username}@example.com')
-    fill_in('user_username', with: @username)
-    fill_in('user_password', with: 'secret')
-    fill_in('user_password_confirmation', with: 'secret')
+    profile = Profile.new
+# puts profile.generate
+command = ['/usr/bin/sandbox-exec', '-p', profile.generate, profile.pod_bin, *ARGV]
+exec(*command)
+
     
-    describe ConversationsController, :type => :controller do
-  describe '#index' do
-    before do
-      @person = alice.contacts.first.person
-      hash = {
-        :author => @person,
-        :participant_ids => [alice.person.id, @person.id],
-        :subject => 'not spam',
-        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
-      }
-      @conv1 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
-             .increase_unread(alice)
-      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
-             .increase_unread(alice)
-    
-        it 'returns an array of likes for a post' do
-      bob.like!(@message)
-      get :index, params: {post_id: @message.id}
-      expect(JSON.parse(response.body).map {|h| h['id'] }).to match_array(@message.likes.map(&:id))
+        # Checks that the lockfile exists.
+    #
+    # @raise  If the lockfile does not exists.
+    #
+    # @return [void]
+    #
+    def verify_lockfile_exists!
+      unless config.lockfile
+        raise Informative, 'No `Podfile.lock' found in the project directory, run `pod install'.'
+      end
     end
     
-    # Declares a dependency to the git repo of CocoaPods gem. This declaration is
-# compatible with the local git repos feature of Bundler.
-#
-def cp_gem(name, repo_name, branch = 'master', path: false)
-  return gem name if SKIP_UNRELEASED_VERSIONS
-  opts = if path
-           { :path => '../#{repo_name}' }
-         else
-           url = 'https://github.com/CocoaPods/#{repo_name}.git'
-           { :git => url, :branch => branch }
-         end
-  gem name, opts
-end
+          ::Bundler.settings[:path] = LogStash::Environment::BUNDLE_DIR
+      ::Bundler.settings[:gemfile] = LogStash::Environment::GEMFILE_PATH
+      ::Bundler.settings[:without] = options[:without].join(':')
+      ::Bundler.settings[:force] = options[:force]
     
-      desc 'Prepares for a release'
-  task :pre_release do
-    unless File.exist?('../Specs')
-      raise 'Ensure that the specs repo exits in the `../Specs` location'
-    end
-  end
+          explicit_plugins_specs = explicitly_declared_plugins_specs
     
-        # From asking people, it seems MacPorts does not have a `prefix` command, like
-    # Homebrew does, so make an educated guess:
-    if port_prefix = prefix_from_bin('port')
-      prefixes << port_prefix
+            if Utils::HttpClient.remote_file_exist?(uri)
+          PluginManager.ui.debug('Found package at: #{uri}')
+          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
+        else
+          PluginManager.ui.debug('Package not found at: #{uri}')
+          return nil
+        end
+      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
+        # This probably means there is a firewall in place of the proxy is not correctly configured.
+        # So lets skip this strategy but log a meaningful errors.
+        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
+    
+    shared_examples 'logstash update' do |logstash|
+  describe 'logstash-plugin update on #{logstash.hostname}' do
+    before :each do
+      logstash.install({:version => LOGSTASH_VERSION})
     end
