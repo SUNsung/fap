adcb05fd332d40d12d5bcd15afb75838c126b297
SUNsung <weatherius@gmail.com>
2018-10-02 03:46:06 +0300

    Rewrite [391I.XvGag99639Tt3qtrKqyCE3960AZQS78LVg39J1Y.py] File in: 2018-10-02 03:46:06

--- pyton/391I.XvGag99639Tt3qtrKqyCE3960AZQS78LVg39J1Y.py
+++ pyton/391I.XvGag99639Tt3qtrKqyCE3960AZQS78LVg39J1Y.py
@@ -1,164 +1,114 @@
 
         
-            # Path-based
-    if self.hparams.input in ['path', 'integrated', 'integrated-nc']:
-      network_input += self.path_dim
+            region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
+    client = boto3_conn(module, conn_type='client', resource='waf', region=region, endpoint=ec2_url, **aws_connect_kwargs)
     
-      if ntime is None:
-    ntime = dataset['train_data'].shape[1]
-  if nsamples is None:
-    nsamples = dataset['train_data'].shape[0]
+    - name: create an origin access identity
+  cloudfront_origin_access_identity:
+    state: present
+    caller_reference: this is an example reference
+    comment: this is an example comment
     
-      dir_name = os.path.dirname(data_fname)
-  if not os.path.exists(dir_name):
-    os.makedirs(dir_name)
+        def disable(self):
+        '''Disables the rule in AWS'''
+        try:
+            response = self.client.disable_rule(Name=self.name)
+        except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
+            self.module.fail_json_aws(e, msg='Could not disable rule %s' % self.name)
+        self.changed = True
+        return response
     
-        Args:
-      filepattern: Dataset file pattern.
-      vocab: Vocabulary.
-    '''
-    self._vocab = vocab
-    self._all_shards = tf.gfile.Glob(filepattern)
-    tf.logging.info('Found %d shards at %s', len(self._all_shards), filepattern)
+        exit_params = {'msg': 'AMI deregister operation complete.', 'changed': True}
     
-    import tensorflow as tf
+    try:
+    import boto3
+    HAS_BOTO3 = True
+except ImportError:
+    HAS_BOTO3 = False
     
-      # *Total* number of n-grams produced by the generator.
-  total_ngrams_produced = 0
-    
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
-    
-        def get_headers(self):
-        return self.formatting.format_headers(
-            self.msg.headers).encode(self.output_encoding)
-    
-        def __iter__(self):
-        return iter(self._plugins)
+        if limit:
+        results = results[:int(limit)]
     
     
-FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
-FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
-JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
-BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
-    
-    
-class Response(object):
-    # noinspection PyDefaultArgument
-    def __init__(self, url, headers={}, status_code=200):
-        self.url = url
-        self.headers = CaseInsensitiveDict(headers)
-        self.status_code = status_code
-    
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
-    
-            '''
-        env = MockEnvironment(colors=256)
-        r = http('--print=B', '--pretty=all', httpbin.url + '/post',
-                 'Content-Type:text/foo+json', 'a=b', env=env)
-        assert COLOR in r
-    
-    
-def test_max_redirects(httpbin):
-    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
-             error_exit_ok=True)
-    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
+if __name__ == '__main__':
+    main()
 
     
-        ratio = n_samples / opts.n_population
+    First, we fix a training set and increase the number of
+samples. Then we plot the computation time as function of
+the number of samples.
+    
+    Note that LocalOutlierFactor is not meant to predict on a test set and its
+performance is assessed in an outlier detection context:
+1. The model is trained on the whole dataset which is assumed to contain
+outliers.
+2. The ROC curve is computed on the same dataset using the knowledge of the
+labels.
+In this context there is no need to shuffle the dataset because the model
+is trained and tested on the whole dataset. The randomness of this benchmark
+is only caused by the random selection of anomalies in the SA dataset.
+    
+            ax.set_xlabel('n_samples')
+        ax.set_ylabel('n_features')
+        ax.set_zlabel('Time (s)')
+        ax.set_zlim3d(0.0, max_time * 1.1)
+        ax.set_title(label)
+        # ax.legend()
+        i += 1
+    plt.show()
+
+    
+    import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import ticker
+    
+        max_it = len(samples_range) * len(features_range)
+    for n_samples in samples_range:
+        for n_features in features_range:
+            it += 1
+            print('====================')
+            print('Iteration %03d of %03d' % (it, max_it))
+            print('====================')
+            X = make_low_rank_matrix(n_samples, n_features,
+                                  effective_rank=rank,
+                                  tail_strength=0.2)
+    
+    import time
+    
+        default_algorithms = 'custom-tracking-selection,custom-auto,' \
+                         'custom-reservoir-sampling,custom-pool,'\
+                         'python-core-sample,numpy-permutation'
     
     File: sparsity_benchmark.py
 Function: benchmark_sparse_predict at line 56
 Total time: 0.39274 s
     
+    for Vectorizer, (analyzer, ngram_range) in itertools.product(
+            [CountVectorizer, TfidfVectorizer, HashingVectorizer],
+            [('word', (1, 1)),
+             ('word', (1, 2)),
+             ('word', (1, 4)),
+             ('char', (4, 4)),
+             ('char_wb', (4, 4))
+             ]):
+    
+        # start time
+    tstart = datetime.now()
+    clf = DecisionTreeClassifier()
+    clf.fit(X, Y).predict(X)
+    delta = (datetime.now() - tstart)
+    # stop time
+    
+            :user:`sloria`
     '''
+    options = options or {}
+    content = content or []
+    has_explicit_title, title, target = split_explicit_title(text)
     
-      if not p.isfile( build_file ):
-    sys.exit(
-      'File {0} does not exist; you probably forgot to run:\n'
-      '\tgit submodule update --init --recursive\n'.format( build_file ) )
-    
-      if left_string == right_string:
-    return min( left_string_length, right_string_length )
-    
-    
-class MessagesPoll( BaseRequest ):
-  def __init__( self ):
-    super( MessagesPoll, self ).__init__()
-    self._request_data = BuildRequestData()
-    self._response_future = None
-    
-    
-def ConvertVimDataToCompletionData( vim_data ):
-  # see :h complete-items for a description of the dictionary fields
-  completion_data = {}
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-def AdjustCandidateInsertionText_MoreThanWordMatchingAfterCursor_test():
-  with MockTextAfterCursor( 'bar.h' ):
-    eq_( [ { 'abbr': 'foobar.h', 'word': 'foo' } ],
-         base.AdjustCandidateInsertionText( [ 'foobar.h' ] ) )
-    
-    
-  def Just_Detailed_Info_test( self ):
-    self._Check( 9999999999, {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'detailed_info':   'DETAILED INFO',
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DETAILED INFO',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '9999999999',
-    } )
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+    import os
+try:
+    # Python 2 compat
+    from urllib2 import Request, build_opener
+except ImportError:
+    # Python 3
+    from urllib.request import Request, build_opener
