c1a32ed6cbf84b6cb76eeec7db2efc8ca1910daa
SUNsung <weatherius@gmail.com>
2018-10-02 15:25:01 +0300

    Rewrite [42XUZSY3nAkZo420mg5kq6nfRU42oCEKhAEz6EA42p.8.cpp] File in: 2018-10-02 15:25:00

--- cccccc/42XUZSY3nAkZo420mg5kq6nfRU42oCEKhAEz6EA42p.8.cpp
+++ cccccc/42XUZSY3nAkZo420mg5kq6nfRU42oCEKhAEz6EA42p.8.cpp
@@ -1,162 +1,163 @@
 
         
-        // Tell browser we have an uncaughtException from node.
-IPC_MESSAGE_ROUTED1(ShellViewHostMsg_UncaughtException,
-                    std::string /* err */)
-    
-        // Ignore first non-switch arg if it's not a standalone package.
-    bool ignore_arg = !package->self_extract();
-    for (unsigned i = 1; i < argv.size(); ++i) {
-      if (ignore_arg && argv[i] == args[0]) {
-        ignore_arg = false;
-        continue;
-      }
-    }
-    
-     protected:
-  ~NwClipboardReadAvailableTypesFunction() override;
-    
-    struct grpc_channel;
-    
-    
-    {}  // namespace grpc
+        void SILLayout::Profile(llvm::FoldingSetNodeID &id,
+                        CanGenericSignature Generics,
+                        ArrayRef<SILField> Fields) {
+  id.AddPointer(Generics.getPointer());
+  for (auto &field : Fields) {
+    id.AddPointer(field.getLoweredType().getPointer());
+    id.AddBoolean(field.isMutable());
+  }
+}
 
     
-      std::shared_ptr<grpc::Channel> CreateChannel(
-      const string& target, const grpc::ChannelArguments& args) override {
-    grpc_channel_args channel_args;
-    args.SetChannelArgs(&channel_args);
-    return CreateChannelInternal(
-        '', grpc_cronet_secure_channel_create(engine_, target.c_str(),
-                                              &channel_args, nullptr));
-  }
-    
-    namespace grpc {
+    bool swift::parseASTSection(SerializedModuleLoader *SML, StringRef buf,
+                            SmallVectorImpl<std::string> &foundModules) {
+  if (!serialization::isSerializedAST(buf))
+    return false;
     }
     
-    void CensusClientCallData::OnDoneRecvMessageCb(void* user_data,
-                                               grpc_error* error) {
-  grpc_call_element* elem = reinterpret_cast<grpc_call_element*>(user_data);
-  CensusClientCallData* calld =
-      reinterpret_cast<CensusClientCallData*>(elem->call_data);
-  CensusChannelData* channeld =
-      reinterpret_cast<CensusChannelData*>(elem->channel_data);
-  GPR_ASSERT(calld != nullptr);
-  GPR_ASSERT(channeld != nullptr);
-  // Stream messages are no longer valid after receiving trailing metadata.
-  if ((*calld->recv_message_) != nullptr) {
-    calld->recv_message_count_++;
-  }
-  GRPC_CLOSURE_RUN(calld->initial_on_done_recv_message_, GRPC_ERROR_REF(error));
+    void CacheImpl::releaseValue(void *Value) {
+  cache_release_value(static_cast<cache_t*>(Impl), Value);
 }
     
+    void DiverseStackBase::pushNewStorageSlow(std::size_t needed) {
+  bool wasInline = isAllocatedInline();
+    }
     
-    { private:
-  CensusContext context_;
-  // Metadata elements for tracing and census stats data.
-  grpc_linked_mdelem stats_bin_;
-  grpc_linked_mdelem tracing_bin_;
-  // Client method.
-  absl::string_view method_;
-  std::string qualified_method_;
-  grpc_slice path_;
-  // The recv trailing metadata callbacks.
-  grpc_metadata_batch* recv_trailing_metadata_;
-  grpc_closure* initial_on_done_recv_trailing_metadata_;
-  grpc_closure on_done_recv_trailing_metadata_;
-  // recv message
-  grpc_closure* initial_on_done_recv_message_;
-  grpc_closure on_done_recv_message_;
-  // Start time (for measuring latency).
-  absl::Time start_time_;
-  // Server elapsed time in nanoseconds.
-  uint64_t elapsed_time_;
-  // The received message--may be null.
-  grpc_core::OrphanablePtr<grpc_core::ByteStream>* recv_message_;
-  // Number of messages in this RPC.
-  uint64_t recv_message_count_;
-  uint64_t sent_message_count_;
-  // Buffer needed for grpc_slice to reference when adding trace context
-  // metatdata to outgoing message.
-  char tracing_buf_[kMaxTraceContextLen];
+      bool IsFirstLine = true;
+    
+    uint64_t swift::unicode::getUTF16Length(StringRef Str) {
+  uint64_t Length;
+  // Transcode the string to UTF-16 to get its length.
+  SmallVector<llvm::UTF16, 128> buffer(Str.size() + 1); // +1 for ending nulls.
+  const llvm::UTF8 *fromPtr = (const llvm::UTF8 *) Str.data();
+  llvm::UTF16 *toPtr = &buffer[0];
+  llvm::ConversionResult Result =
+    ConvertUTF8toUTF16(&fromPtr, fromPtr + Str.size(),
+                       &toPtr, toPtr + Str.size(),
+                       llvm::strictConversion);
+  assert(Result == llvm::conversionOK &&
+         'UTF-8 encoded string cannot be converted into UTF-16 encoding');
+  (void)Result;
+    }
+    
+    
+    {  return '';
+}
+
+    
+    public:
+  static CFPointeeInfo classifyTypedef(const clang::TypedefNameDecl *decl);
+    
+    #if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef _module = {
+  PyModuleDef_HEAD_INIT,
+  kModuleName,
+  kModuleDocstring,
+  -1,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL
 };
-    
-    const ViewDescriptor& ServerReceivedMessagesPerRpcCumulative() {
-  const static ViewDescriptor descriptor =
-      ViewDescriptor()
-          .set_name('grpc.io/server/sent_messages_per_rpc/cumulative')
-          .set_measure(kRpcServerReceivedMessagesPerRpcMeasureName)
-          .set_aggregation(CountDistributionAggregation())
-          .add_column(ServerMethodTagKey());
-  return descriptor;
-}
-    
-    BENCHMARK(BENCHFUN(defaultCtor), iters) {
-  FOR_EACH_RANGE (i, 0, iters) {
-    VECTOR v;
-    doNotOptimizeAway(&v);
-  }
-}
-    
-    BENCHMARK_RELATIVE(sformat_short_string_safe, iters) {
-  BenchmarkSuspender suspender;
-  auto const& shortString = getShortString();
-  while (iters--) {
-    std::string out;
-    suspender.dismissing([&] { out = sformat('{}', shortString); });
-  }
-}
-    
-    TEST(FunctionRef, FunctionPtr) {
-  int (*funcptr)(int) = [](int v) { return v * v; };
-    }
-    
-    vector<detail::BenchmarkResult> resultsFromFile(const std::string& filename) {
-  string content;
-  readFile(filename.c_str(), content);
-  vector<detail::BenchmarkResult> ret;
-  benchmarkResultsFromDynamic(parseJson(content), ret);
-  return ret;
-}
-    
-    // Various helpers to throw appropriate std::system_error exceptions from C
-// library errors (returned in errno, as positive return values (many POSIX
-// functions), or as negative return values (Linux syscalls))
-//
-// The *Explicit functions take an explicit value for errno.
-    
-    // Empirically, this indicates if the runtime supports
-// std::exception_ptr, as not all (arm, for instance) do.
-#if defined(__GNUC__) && defined(__GCC_ATOMIC_INT_LOCK_FREE) && \
-    __GCC_ATOMIC_INT_LOCK_FREE > 1
-inline fbstring exceptionStr(std::exception_ptr ep) {
-  try {
-    std::rethrow_exception(ep);
-  } catch (const std::exception& e) {
-    return exceptionStr(e);
-  } catch (...) {
-    return '<unknown exception>';
-  }
-}
+#define INITFUNC PyInit__api_implementation
+#define INITFUNC_ERRORVAL NULL
+#else
+#define INITFUNC init_api_implementation
+#define INITFUNC_ERRORVAL
 #endif
     
-    fbstring exceptionStr(exception_wrapper const& ew) {
-  return ew.what();
+    #include <google/protobuf/python/python_protobuf.h>
+    
+      // True when a ScopedPyObjectPtr and a raw pointer refer to the same object.
+  // Comparison operators are non reflexive.
+  bool operator==(const PyObjectStruct* p) const { return ptr_ == p; }
+  bool operator!=(const PyObjectStruct* p) const { return ptr_ != p; }
+    
+    void upb_inttable_packedsize(const upb_inttable *t, size_t *size);
+void upb_strtable_packedsize(const upb_strtable *t, size_t *size);
+upb_inttable *upb_inttable_pack(const upb_inttable *t, void *p, size_t *ofs,
+                                size_t size);
+upb_strtable *upb_strtable_pack(const upb_strtable *t, void *p, size_t *ofs,
+                                size_t size);
+    
+    bool DecodeMetadata(const string& path, GeneratedCodeInfo* info) {
+  string data;
+  GOOGLE_CHECK_OK(File::GetContents(path, &data, true));
+  io::ArrayInputStream input(data.data(), data.size());
+  return info->ParseFromZeroCopyStream(&input);
 }
     
-    #include <stdexcept>
     
-    // Return the state size needed by RNG, expressed as a number of uint32_t
-// integers. Specialized for all templates specified in the C++11 standard.
-// For some (mersenne_twister_engine), this is exported as a state_size static
-// data member; for others, the standard shows formulas.
+    {  // Check if the optional_nested_message was actually moved (and not just
+  // copied).
+  EXPECT_EQ(nested, &message2.optional_nested_message());
+  EXPECT_NE(nested, &message1.optional_nested_message());
+}
     
-      /**
-   * Returns a random uint64_t in [0, max). If max == 0, returns 0.
-   */
-  template <class RNG = ThreadLocalPRNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static uint64_t rand64(uint64_t max, RNG&& rng) {
-    return rand64(0, max, rng);
+      printer->Print(
+    '/// <summary>Holder for reflection information generated from $file_name$</summary>\n'
+    '$access_level$ static partial class $reflection_class_name$ {\n'
+    '\n',
+    'file_name', file_->name(),
+    'access_level', class_access_level(),
+    'reflection_class_name', reflectionClassname_);
+  printer->Indent();
+}
+    
+    const Options* SourceGeneratorBase::options() {
+  return this->options_;
+}
+    
+    #include <google/protobuf/compiler/java/java_doc_comment.h>
+    
+    void ImmutableMapFieldGenerator::
+GenerateParsingCode(io::Printer* printer) const {
+  printer->Print(
+      variables_,
+      'if (!$get_mutable_bit_parser$) {\n'
+      '  $name$_ = com.google.protobuf.MapField.newMapField(\n'
+      '      $map_field_parameter$);\n'
+      '  $set_mutable_bit_parser$;\n'
+      '}\n');
+  if (!SupportUnknownEnumValue(descriptor_->file()) &&
+      GetJavaType(ValueField(descriptor_)) == JAVATYPE_ENUM) {
+    printer->Print(
+        variables_,
+        'com.google.protobuf.ByteString bytes = input.readBytes();\n'
+        'com.google.protobuf.MapEntry<$type_parameters$>\n'
+        '$name$__ = $default_entry$.getParserForType().parseFrom(bytes);\n');
+    printer->Print(
+        variables_,
+        'if ($value_enum_type$.forNumber($name$__.getValue()) == null) {\n'
+        '  unknownFields.mergeLengthDelimitedField($number$, bytes);\n'
+        '} else {\n'
+        '  $name$_.getMutableMap().put(\n'
+        '      $name$__.getKey(), $name$__.getValue());\n'
+        '}\n');
+  } else {
+    printer->Print(
+        variables_,
+        'com.google.protobuf.MapEntry<$type_parameters$>\n'
+        '$name$__ = input.readMessage(\n'
+        '    $default_entry$.getParserForType(), extensionRegistry);\n'
+        '$name$_.getMutableMap().put(\n'
+        '    $name$__.getKey(), $name$__.getValue());\n');
   }
+}
     
-    #pragma once
+      // Note: For the TextFormat decode info, we can't use the enum value as
+  // the key because protocol buffer enums have 'allow_alias', which lets
+  // a value be used more than once. Instead, the index into the list of
+  // enum value descriptions is used. Note: start with -1 so the first one
+  // will be zero.
+  TextFormatDecodeData text_format_decode_data;
+  int enum_value_description_key = -1;
+  string text_blob;
+    
+    class EnumFieldGenerator : public SingleFieldGenerator {
+  friend FieldGenerator* FieldGenerator::Make(const FieldDescriptor* field,
+                                              const Options& options);
+    }
