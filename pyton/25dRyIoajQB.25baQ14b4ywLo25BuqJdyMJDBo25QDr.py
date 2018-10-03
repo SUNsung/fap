b896b3e4986e5340256c20625e2f874a81d7353a
SUNsung <weatherius@gmail.com>
2018-10-03 15:13:32 +0300

    Rewrite [25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py] File in: 2018-10-03 15:13:31

--- pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
+++ pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
@@ -1,52 +1,131 @@
 
         
-            @staticmethod
-    def import_windows_ca(certfile):
-        xlog.debug('Begin to import Windows CA')
-        with open(certfile, 'rb') as fp:
-            certdata = fp.read()
-            if certdata.startswith(b'-----'):
-                begin = b'-----BEGIN CERTIFICATE-----'
-                end = b'-----END CERTIFICATE-----'
-                certdata = base64.b64decode(b''.join(certdata[certdata.find(begin)+len(begin):certdata.find(end)].strip().splitlines()))
-        try:
-            common_name = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, certdata).get_subject().CN
-        except Exception as e:
-            logging.error('load_certificate(certfile=%r) 失败：%s', certfile, e)
-            return -1
+                # Insert the include statement to MANIFEST.in if not present
+        with open(manifest_path, 'a+') as manifest:
+            manifest.seek(0)
+            manifest_content = manifest.read()
+            if not 'include fastentrypoints.py' in manifest_content:
+                manifest.write(('\n' if manifest_content else '')
+                               + 'include fastentrypoints.py')
     
-        if probe_nat:
-        pteredor_is_running = False
-        log = Log()
-        log.write('qualified: %s\nNAT type: %s' % (prober.qualified, prober.nat_type))
-        log.close()
-    return prober
     
-        def eval_servers(self):
-        if self.nat_type is 'null':
-            self.nat_type = self.nat_type_probe()
-        elif self.nat_type is 'probing':
-            print('Is probing NAT type now, pleace wait...')
-        while self.nat_type is 'probing':
-            time.sleep(0.1)
-        if not self.qualified:
-            print('This device can not use teredo tunnel, the NAT type is %s!' % prober.nat_type)
-            return []
-        print('Starting evaluate servers...')
-        self.clear()
-        eval_list = []
-        queue_obj = queue.Queue()
-        for server_ip in self.server_ip_list:
-            thread.start_new_thread(self._eval_servers, (server_ip, queue_obj))
-        for _ in self.server_ip_list:
-            eval_list.append(queue_obj.get())
-        return eval_list
+@pytest.mark.functional
+def test_without_confirmation(proc, TIMEOUT):
+    without_confirmation(proc, TIMEOUT)
+    history_changed(proc, TIMEOUT, u'echo test')
     
-    A tree.RewriteCardinalityException is raised, when the parsers hits a
-cardinality mismatch during AST construction. Although this is basically a
-bug in your grammar, it can only be detected at runtime.
     
-    # Predefined token types
-EOR_TOKEN_TYPE = 1
+@pytest.mark.parametrize('command, packages, which', [
+    (Command('a_bad_cmd', 'a_bad_cmd: command not found'),
+     [], None),
+    (Command('vim', ''), [], None),
+    (Command('', ''), [], None),
+    (Command('vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim'),
+    (Command('sudo vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim')])
+def test_not_match(mocker, command, packages, which):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=which)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
     
-            return [token.text for token in tokens]
+    
+@pytest.mark.parametrize('command', [
+    Command('apt-cache search foo', ''),
+    Command('aptitude search foo', ''),
+    Command('apt search foo', ''),
+    Command('apt-get install foo', ''),
+    Command('apt-get source foo', ''),
+    Command('apt-get clean', ''),
+    Command('apt-get remove', ''),
+    Command('apt-get update', ''),
+    Command('sudo apt update', no_match_output)
+])
+def test_not_match(command):
+    assert not match(command)
+    
+    
+@pytest.mark.parametrize('command', [
+    Command('apt list --upgradable', no_match_output),
+    Command('sudo apt list --upgradable', no_match_output)
+])
+def test_not_match(command):
+    assert not match(command)
+    
+    lr_reducer = ReduceLROnPlateau(factor=np.sqrt(0.1),
+                               cooldown=0,
+                               patience=5,
+                               min_lr=0.5e-6)
+    
+    
+def test_imdb():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = imdb.load_data()
+        (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=40)
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        word_index = imdb.get_word_index()
+        assert isinstance(word_index, dict)
+    
+    __Note on specifying the initial state of RNNs__
+    
+    
+max_features = 20000
+# cut texts after this number of words
+# (among top max_features most common words)
+maxlen = 100
+batch_size = 32
+    
+    for i, (input_text, target_text) in enumerate(zip(input_texts, target_texts)):
+    for t, char in enumerate(input_text):
+        encoder_input_data[i, t, input_token_index[char]] = 1.
+    for t, char in enumerate(target_text):
+        # decoder_target_data is ahead of decoder_input_data by one timestep
+        decoder_input_data[i, t, target_token_index[char]] = 1.
+        if t > 0:
+            # decoder_target_data will be ahead by one timestep
+            # and will not include the start character.
+            decoder_target_data[i, t - 1, target_token_index[char]] = 1.
+    
+        # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ''
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict(
+            [target_seq] + states_value)
+    
+    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    title = r1(r'<title>(.*)</title>', html)
+    assert title
+    id = r1(r'flvid\s*=\s*(\d+)', html)
+    assert id
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+    
+    
+def cleanup_files(files):
+    for file in files:
+        os.remove(file)
+    
+    __all__ = ['facebook_download']
+    
+    def get_single_photo_url(url):
+    page = get_html(url)
+    pid = get_photo_id(url, page)
+    title = match1(page, pattern_inline_title)
+    if match1(page, pattern_inline_video_mark):
+        api_key = get_api_key(page)
+        reply = get_content(tmpl_api_call_photo_info % (api_key, get_photo_id(url, page)))
+        secret = json.loads(reply)['photo']['secret']
+        return get_orig_video_source(api_key, pid, secret), title
+    #last match always has the best resolution
+    match = match1(page, pattern_inline_img_url)
+    return 'https:' + match.replace('\\', ''), title
