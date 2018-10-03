70d4bbb446f192b58fcaae6ca4637c0fea9090c3
SUNsung <weatherius@gmail.com>
2018-10-03 20:12:28 +0300

    Rewrite [02CckrCnlJnQ02D2MjMcikdEI02CZ3aabLKF5w02dpu.cpp] File in: 2018-10-03 20:12:28

--- cccccc/02CckrCnlJnQ02D2MjMcikdEI02CZ3aabLKF5w02dpu.cpp
+++ cccccc/02CckrCnlJnQ02D2MjMcikdEI02CZ3aabLKF5w02dpu.cpp
@@ -1,182 +1,343 @@
 
         
-        /*!
- * \brief Registry entry for linear updater.
- */
-struct LinearUpdaterReg
-    : public dmlc::FunctionRegEntryBase<LinearUpdaterReg,
-                                        std::function<LinearUpdater*()> > {};
+        #include 'tensorflow/core/framework/op.h'
     
-      virtual void PredictInstance(const SparsePage::Inst& inst,
-                               std::vector<bst_float>* out_preds,
-                               const gbm::GBTreeModel& model,
-                               unsigned ntree_limit = 0,
-                               unsigned root_index = 0) = 0;
-    
-    #include <dmlc/registry.h>
-#include <functional>
-#include <vector>
-#include <utility>
-#include <string>
-#include './base.h'
-#include './data.h'
-#include './tree_model.h'
-#include '../../src/common/host_device_vector.h'
-    
-    
-    {    for (size_t i = 0; i < batch.size; ++i) {
-      offset_[i + 1] = (i + 1) * num_col_;
-      Row<IndexType> row = batch[i];
-      for (uint32_t j = 0; j < num_col_; ++j) {
-        dense_index_[i * num_col_ + j] = j;
-      }
-      for (unsigned k = 0; k < row.length; ++k) {
-        uint32_t index = row.get_index(k);
-        CHECK_LT(index, num_col_)
-            << 'Featuere index larger than num_col';
-        dense_value_[i * num_col_ + index]  = row.get_value(k);
-      }
-    }
-    out_ = batch;
-    out_.index = dmlc::BeginPtr(dense_index_);
-    out_.value = dmlc::BeginPtr(dense_value_);
-    out_.offset = dmlc::BeginPtr(offset_);
-    return true;
+      for (const auto& node : item_.MainOpsFanin()) {
+    PrintNodeInfo(node, properties, debug, os);
+  }
+  for (const auto& node : item_.EnqueueOpsFanin()) {
+    PrintNodeInfo(node, properties, debug, os);
   }
     
-      // Taylor coefficients
-  const float P0expf = 1.f / 2.f;
-  const float P1expf = 1.f / 6.f;
-  const float P2expf = 1.f / 24.f;
-  const float P3expf = 1.f / 120.f;
-  const float P4expf = 1.f / 720.f;
-  const float P5expf = 1.f / 5040.f;
-    
-    #include <xgboost/logging.h>
-#include <cctype>
-#include <cstdio>
-#include <string>
-#include './io.h'
-    
-    
-    {
-    {
-    { private:
-  std::ifstream fi_;
-};
-}  // namespace common
-}  // namespace xgboost
-#endif  // XGBOOST_COMMON_CONFIG_H_
+    #endif  // TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
 
     
-    namespace xgboost {
-namespace data {
-/*!
- * \brief Format specification of SparsePage.
- */
-class SparsePageFormat {
- public:
-  /*! \brief virtual destructor */
-  virtual ~SparsePageFormat() = default;
-  /*!
-   * \brief Load all the segments into page, advance fi to end of the block.
-   * \param page The data to read page into.
-   * \param fi the input stream of the file
-   * \return true of the loading as successful, false if end of file was reached
-   */
-  virtual bool Read(SparsePage* page, dmlc::SeekStream* fi) = 0;
-  /*!
-   * \brief read only the segments we are interested in, advance fi to end of the block.
-   * \param page The page to load the data into.
-   * \param fi the input stream of the file
-   * \param sorted_index_set sorted index of segments we are interested in
-   * \return true of the loading as successful, false if end of file was reached
-   */
-  virtual bool Read(SparsePage* page,
-                    dmlc::SeekStream* fi,
-                    const std::vector<bst_uint>& sorted_index_set) = 0;
-  /*!
-   * \brief save the data to fo, when a page was written.
-   * \param fo output stream
-   */
-  virtual void Write(const SparsePage& page, dmlc::Stream* fo) = 0;
-  /*!
-   * \brief Create sparse page of format.
-   * \return The created format functors.
-   */
-  static SparsePageFormat* Create(const std::string& name);
-  /*!
-   * \brief decide the format from cache prefix.
-   * \return pair of row format, column format type of the cache prefix.
-   */
-  static std::pair<std::string, std::string> DecideFormat(const std::string& cache_prefix);
-};
-    }
-    }
-    
-            // Convert to freetype flags (nb: Bold and Oblique are processed separately)
-        UserFlags = cfg.RasterizerFlags | extra_user_flags;
-        FreetypeLoadFlags = FT_LOAD_NO_BITMAP;
-        if (UserFlags & ImGuiFreeType::NoHinting)      FreetypeLoadFlags |= FT_LOAD_NO_HINTING;
-        if (UserFlags & ImGuiFreeType::NoAutoHint)     FreetypeLoadFlags |= FT_LOAD_NO_AUTOHINT;
-        if (UserFlags & ImGuiFreeType::ForceAutoHint)  FreetypeLoadFlags |= FT_LOAD_FORCE_AUTOHINT;
-        if (UserFlags & ImGuiFreeType::LightHinting)   
-            FreetypeLoadFlags |= FT_LOAD_TARGET_LIGHT;
-        else if (UserFlags & ImGuiFreeType::MonoHinting)   
-            FreetypeLoadFlags |= FT_LOAD_TARGET_MONO;
-        else                                                
-            FreetypeLoadFlags |= FT_LOAD_TARGET_NORMAL;
-    
-    static void ImGui_ImplFreeGLUT_UpdateKeyboardMods()
-{
-    ImGuiIO& io = ImGui::GetIO();
-    int mods = glutGetModifiers();
-    io.KeyCtrl = (mods & GLUT_ACTIVE_CTRL) != 0;
-    io.KeyShift = (mods & GLUT_ACTIVE_SHIFT) != 0;
-    io.KeyAlt = (mods & GLUT_ACTIVE_ALT) != 0;
+    // Destructor passed to TF_NewTensor when it reuses a numpy buffer. Stores a
+// pointer to the pyobj in a buffer to be dereferenced later when we're actually
+// holding the GIL.
+void DelayedNumpyDecref(void* data, size_t len, void* obj) {
+  mutex_lock ml(*DelayedDecrefLock());
+  DecrefCache()->push_back(obj);
 }
     
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'CIwTexture*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-    
-        // Setup Dear ImGui binding
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    ImGui_Marmalade_Init(true);
-    
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-    HRESULT CreateDeviceD3D(HWND hWnd)
-{
-    // Setup swap chain
-    DXGI_SWAP_CHAIN_DESC sd;
-    ZeroMemory(&sd, sizeof(sd));
-    sd.BufferCount = 2;
-    sd.BufferDesc.Width = 0;
-    sd.BufferDesc.Height = 0;
-    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    sd.BufferDesc.RefreshRate.Numerator = 60;
-    sd.BufferDesc.RefreshRate.Denominator = 1;
-    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    sd.OutputWindow = hWnd;
-    sd.SampleDesc.Count = 1;
-    sd.SampleDesc.Quality = 0;
-    sd.Windowed = TRUE;
-    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    namespace tensorflow {
     }
     
-    #include 'imgui.h'
-#include 'imgui_impl_dx9.h'
-#include 'imgui_impl_win32.h'
-#include <d3d9.h>
-#define DIRECTINPUT_VERSION 0x0800
-#include <dinput.h>
-#include <tchar.h>
+    namespace tensorflow {
+    }
     
-    #endif // BOOST_ATOMIC_FENCES_HPP_INCLUDED_
+    namespace tensorflow {
+    }
+    
+    #include 'tensorflow/stream_executor/blas.h'
+    
+    ScopedActivateExecutorContext::~ScopedActivateExecutorContext() {
+  delete static_cast<ScopedActivateContext *>(driver_scoped_activate_context_);
+}
+    
+    // Activates a CUDA context within an enclosing scope.
+class ScopedActivateExecutorContext {
+ public:
+  // Form that takes a CUDA executor implementation.
+  explicit ScopedActivateExecutorContext(CUDAExecutor* cuda_exec);
+    }
+    
+    private:
+    NetworkStyle(const QString &appName, const int iconColorHueShift, const int iconColorSaturationReduction, const char *titleAddText);
+    
+    
+    {    Lock& lock;
+    Lock templock;
+};
+    
+    #endif
+
+    
+    #ifndef SECP256K1_MODULE_ECDH_MAIN_H
+#define SECP256K1_MODULE_ECDH_MAIN_H
+    
+    
+    {    secp256k1_ecdsa_recoverable_signature_load(ctx, &r, &s, &recid, sigin);
+    secp256k1_ecdsa_signature_save(sig, &r, &s);
+    return 1;
+}
+    
+    void run_recovery_tests(void) {
+    int i;
+    for (i = 0; i < count; i++) {
+        test_ecdsa_recovery_api();
+    }
+    for (i = 0; i < 64*count; i++) {
+        test_ecdsa_recovery_end_to_end();
+    }
+    test_ecdsa_recovery_edge_cases();
+}
+    
+      inline Dtype diff_at(const int n, const int c, const int h,
+      const int w) const {
+    return cpu_diff()[offset(n, c, h, w)];
+  }
+    
+    /// @brief Fills a Blob with Gaussian-distributed values @f$ x = a @f$.
+template <typename Dtype>
+class GaussianFiller : public Filler<Dtype> {
+ public:
+  explicit GaussianFiller(const FillerParameter& param)
+      : Filler<Dtype>(param) {}
+  virtual void Fill(Blob<Dtype>* blob) {
+    Dtype* data = blob->mutable_cpu_data();
+    CHECK(blob->count());
+    caffe_rng_gaussian<Dtype>(blob->count(), Dtype(this->filler_param_.mean()),
+        Dtype(this->filler_param_.std()), blob->mutable_cpu_data());
+    int sparse = this->filler_param_.sparse();
+    CHECK_GE(sparse, -1);
+    if (sparse >= 0) {
+      // Sparse initialization is implemented for 'weight' blobs; i.e. matrices.
+      // These have num == channels == 1; width is number of inputs; height is
+      // number of outputs.  The 'sparse' variable specifies the mean number
+      // of non-zero input weights for a given output.
+      CHECK_GE(blob->num_axes(), 1);
+      const int num_outputs = blob->shape(0);
+      Dtype non_zero_probability = Dtype(sparse) / Dtype(num_outputs);
+      rand_vec_.reset(new SyncedMemory(blob->count() * sizeof(int)));
+      int* mask = reinterpret_cast<int*>(rand_vec_->mutable_cpu_data());
+      caffe_rng_bernoulli(blob->count(), non_zero_probability, mask);
+      for (int i = 0; i < blob->count(); ++i) {
+        data[i] *= mask[i];
+      }
+    }
+  }
+    }
+    
+     protected:
+  /// @copydoc AbsValLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+    namespace caffe {
+    }
+    
+    namespace caffe {
+    }
+    
+    #endif  // CAFFE_CONTRASTIVE_LOSS_LAYER_HPP_
+
+    
+    
+    {}
+    
+    namespace parallel {
+    }
+    
+    namespace php {
+struct Program;
+}
+struct Index;
+    
+    #endif
+
+    
+    extern __thread int tl_exit_code;
+    
+        // skipping .  .. hidden files
+    if (ename[0] == '.' || !*ename) {
+      continue;
+    }
+    auto fe = fullPath + ename;
+    struct stat se;
+    if (stat(fe.c_str(), &se)) {
+      Logger::Error('FileUtil::find(): unable to stat %s', fe.c_str());
+      continue;
+    }
+    
+      // No copying allowed
+  DBImpl(const DBImpl&);
+  void operator=(const DBImpl&);
+    
+    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
+                                        std::string* dst) const {
+  // We rely on the fact that the code in table.cc does not mind us
+  // adjusting keys[].
+  Slice* mkey = const_cast<Slice*>(keys);
+  for (int i = 0; i < n; i++) {
+    mkey[i] = ExtractUserKey(keys[i]);
+    // TODO(sanjay): Suppress dups?
+  }
+  user_policy_->CreateFilter(keys, n, dst);
+}
+    
+    // Make the CURRENT file point to the descriptor file with the
+// specified number.
+Status SetCurrentFile(Env* env, const std::string& dbname,
+                      uint64_t descriptor_number);
+    
+    class StdoutPrinter : public WritableFile {
+ public:
+  virtual Status Append(const Slice& data) {
+    fwrite(data.data(), 1, data.size(), stdout);
+    return Status::OK();
+  }
+  virtual Status Close() { return Status::OK(); }
+  virtual Status Flush() { return Status::OK(); }
+  virtual Status Sync() { return Status::OK(); }
+};
+    
+    
+    {  // No copying allowed
+  MemTableIterator(const MemTableIterator&);
+  void operator=(const MemTableIterator&);
+};
+    
+      // Add an entry into memtable that maps key to value at the
+  // specified sequence number and with the specified type.
+  // Typically value will be empty if type==kTypeDeletion.
+  void Add(SequenceNumber seq, ValueType type,
+           const Slice& key,
+           const Slice& value);
+    
+    class ButteraugliComparator : public Comparator {
+ public:
+  ButteraugliComparator(const int width, const int height,
+                        const std::vector<uint8_t>* rgb,
+                        const float target_distance, ProcessStats* stats);
+    }
+    
+    #define GUETZLI_LOG(stats, ...)                                    \
+  do {                                                             \
+    char debug_string[1024];                                       \
+    int res = snprintf(debug_string, sizeof(debug_string),         \
+                       __VA_ARGS__);                               \
+    assert(res > 0 && 'expected successful printing');             \
+    (void)res;                                                     \
+    debug_string[sizeof(debug_string) - 1] = '\0';                 \
+    ::guetzli::PrintDebug(                      \
+         stats, std::string(debug_string));        \
+  } while (0)
+#define GUETZLI_LOG_QUANT(stats, q)                    \
+  for (int y = 0; y < 8; ++y) {                        \
+    for (int c = 0; c < 3; ++c) {                      \
+      for (int x = 0; x < 8; ++x)                      \
+        GUETZLI_LOG(stats, ' %2d', (q)[c][8 * y + x]); \
+      GUETZLI_LOG(stats, '   ');                       \
+    }                                                  \
+    GUETZLI_LOG(stats, '\n');                          \
+  }
+    
+    #include 'guetzli/jpeg_data.h'
+    
+      tmp0 = in[2 * stride];
+  tmp1 = kIDCTMatrix[ 2] * tmp0;
+  tmp2 = kIDCTMatrix[10] * tmp0;
+  out[0] += tmp1;
+  out[1] += tmp2;
+  out[2] -= tmp2;
+  out[3] -= tmp1;
+  out[4] -= tmp1;
+  out[5] -= tmp2;
+  out[6] += tmp2;
+  out[7] += tmp1;
+    
+    
+    {}  // namespace guetzli
+    
+    void InitJPEGDataForYUV444(int w, int h, JPEGData* jpg) {
+  jpg->width = w;
+  jpg->height = h;
+  jpg->max_h_samp_factor = 1;
+  jpg->max_v_samp_factor = 1;
+  jpg->MCU_rows = (h + 7) >> 3;
+  jpg->MCU_cols = (w + 7) >> 3;
+  jpg->quant.resize(3);
+  jpg->components.resize(3);
+  for (int i = 0; i < 3; ++i) {
+    JPEGComponent* c = &jpg->components[i];
+    c->id = i;
+    c->h_samp_factor = 1;
+    c->v_samp_factor = 1;
+    c->quant_idx = i;
+    c->width_in_blocks = jpg->MCU_cols;
+    c->height_in_blocks = jpg->MCU_rows;
+    c->num_blocks = c->width_in_blocks * c->height_in_blocks;
+    c->coeffs.resize(c->num_blocks * kDCTBlockSize);
+  }
+}
+    
+      // Compute YUV444 DCT coefficients.
+  int block_ix = 0;
+  for (int block_y = 0; block_y < jpg->MCU_rows; ++block_y) {
+    for (int block_x = 0; block_x < jpg->MCU_cols; ++block_x) {
+      coeff_t block[3 * kDCTBlockSize];
+      // RGB->YUV transform.
+      for (int iy = 0; iy < 8; ++iy) {
+        for (int ix = 0; ix < 8; ++ix) {
+          int y = std::min(h - 1, 8 * block_y + iy);
+          int x = std::min(w - 1, 8 * block_x + ix);
+          int p = y * w + x;
+          RGBToYUV16(&rgb[3 * p], &block[8 * iy + ix]);
+        }
+      }
+      // DCT
+      for (int i = 0; i < 3; ++i) {
+        ComputeBlockDCT(&block[i * kDCTBlockSize]);
+      }
+      // Quantization
+      for (int i = 0; i < 3 * 64; ++i) {
+        Quantize(&block[i], iquant[i]);
+      }
+      // Copy the resulting coefficients to *jpg.
+      for (int i = 0; i < 3; ++i) {
+        memcpy(&jpg->components[i].coeffs[block_ix * kDCTBlockSize],
+               &block[i * kDCTBlockSize], kDCTBlockSize * sizeof(block[0]));
+      }
+      ++block_ix;
+    }
+  }
+    
+    BENCHMARK_RELATIVE(format_long_string_safe, iters) {
+  BenchmarkSuspender suspender;
+  auto const& longString = getLongString();
+  while (iters--) {
+    fbstring out;
+    suspender.dismissing([&] { format(&out, '{}', longString); });
+  }
+}
+    
+        // variant 2 (const-overload of v1)
+    int operator()(int x) const {
+      return 100 + 2 * x;
+    }
+    
+    #define FOLLY_SDT_DECLARE_SEMAPHORE(provider, name)                            \
+  extern 'C' volatile unsigned short FOLLY_SDT_SEMAPHORE(provider, name)
+    
+    #include <boost/regex/pending/unicode_iterator.hpp>
+    
+    /**
+ * Reads sizeof(T) bytes, and returns false if not enough bytes are available.
+ * Returns true if the first n bytes are equal to prefix when interpreted as
+ * a little endian T.
+ */
+template <typename T>
+typename std::enable_if<std::is_unsigned<T>::value, bool>::type
+dataStartsWithLE(const IOBuf* data, T prefix, uint64_t n = sizeof(T)) {
+  DCHECK_GT(n, 0);
+  DCHECK_LE(n, sizeof(T));
+  T value;
+  Cursor cursor{data};
+  if (!cursor.tryReadLE(value)) {
+    return false;
+  }
+  const T mask = n == sizeof(T) ? T(-1) : (T(1) << (8 * n)) - 1;
+  return prefix == (value & mask);
+}
+    
+    CacheLocality CacheLocality::readFromSysfs() {
+  return readFromSysfsTree([](std::string name) {
+    std::ifstream xi(name.c_str());
+    std::string rv;
+    std::getline(xi, rv);
+    return rv;
+  });
+}
