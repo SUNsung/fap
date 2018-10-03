0f780183142ffb9c342b412d86bd400496238eea
SUNsung <weatherius@gmail.com>
2018-10-03 03:13:24 +0300

    Rewrite [40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp] File in: 2018-10-03 03:13:23

--- cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
+++ cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
@@ -1,318 +1,157 @@
 
         
-        std::unique_ptr<ScenarioResult> RunScenario(
-    const grpc::testing::ClientConfig& client_config, size_t num_clients,
-    const grpc::testing::ServerConfig& server_config, size_t num_servers,
-    int warmup_seconds, int benchmark_seconds, int spawn_local_worker_count,
-    const grpc::string& qps_server_target_override,
-    const grpc::string& credential_type, bool run_inproc);
-    
-    
-    {  return 0;
-}
-
-    
-    
-    {}  // namespace grpc_node_generator
-    
-    #include 'src/cpp/common/secure_auth_context.h'
-    
-    gpr_atm grpc::testing::interop::g_got_sigint;
-    
-    #ifndef TEST_QPS_USAGE_TIMER_H
-#define TEST_QPS_USAGE_TIMER_H
-    
-    
-    {
-    {}  // namespace log
-}  // namespace leveldb
-    
-    
-    {}  // namespace leveldb
-    
-    double Histogram::Percentile(double p) const {
-  double threshold = num_ * (p / 100.0);
-  double sum = 0;
-  for (int b = 0; b < kNumBuckets; b++) {
-    sum += buckets_[b];
-    if (sum >= threshold) {
-      // Scale linearly within this bucket
-      double left_point = (b == 0) ? 0 : kBucketLimit[b-1];
-      double right_point = kBucketLimit[b];
-      double left_sum = sum - buckets_[b];
-      double right_sum = sum;
-      double pos = (threshold - left_sum) / (right_sum - left_sum);
-      double r = left_point + (right_point - left_point) * pos;
-      if (r < min_) r = min_;
-      if (r > max_) r = max_;
-      return r;
-    }
-  }
-  return max_;
-}
-    
-    
-    { private:
-  port::Mutex *const mu_;
-  // No copying allowed
-  MutexLock(const MutexLock&);
-  void operator=(const MutexLock&);
-};
-    
-    #endif  // STORAGE_LEVELDB_UTIL_RANDOM_H_
-
-    
-    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
-                                        std::string* dst) const {
-  // We rely on the fact that the code in table.cc does not mind us
-  // adjusting keys[].
-  Slice* mkey = const_cast<Slice*>(keys);
-  for (int i = 0; i < n; i++) {
-    mkey[i] = ExtractUserKey(keys[i]);
-    // TODO(sanjay): Suppress dups?
-  }
-  user_policy_->CreateFilter(keys, n, dst);
-}
-    
-    #endif  // STORAGE_LEVELDB_DB_LOG_WRITER_H_
-
-    
-      // Return true iff no entries have been added since the last Reset()
-  bool empty() const {
-    return buffer_.empty();
-  }
-    
-    TEST_F(ViewsConfigParserPluginTests, test_swap_view) {
-  Config c;
-  std::vector<std::string> old_views_vec;
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  old_views_vec.clear();
-  auto s = c.update(getTestConfigMap('view_test.conf'));
-  EXPECT_TRUE(s.ok());
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  EXPECT_EQ(old_views_vec[0], 'config_views.kernel_hashes_new');
-    }
-    
-    TEST_F(PermissionsTests, test_nobody_drop) {
-  if (getuid() != 0) {
-    LOG(WARNING) << 'Not root, skipping (explicit) unprivileged testing';
+        void DocumentHook2(bool start, content::RenderFrame* frame, Dispatcher* dispatcher) {
+  // ignore the first invocation of this hook for iframe
+  // or we'll trigger creating a context with invalid type
+  // there will follow another one with valid url
+  blink::ScriptForbiddenScope::AllowUserAgentScript script;
+  blink::WebLocalFrame* web_frame = frame->GetWebFrame();
+  GURL frame_url = ScriptContext::GetDocumentLoaderURLForFrame(web_frame);
+  if (web_frame->Parent() && (!frame_url.is_valid() || frame_url.is_empty()))
     return;
-  }
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  v8::HandleScope scope(isolate);
+  blink::WebFrame* f = frame->GetRenderView()->GetWebView()->MainFrame();
+  if (!f->IsWebLocalFrame())
+    return;
+  blink::WebLocalFrame* local_frame = f->ToWebLocalFrame();
+  v8::Local<v8::Context> v8_context = local_frame->MainWorldScriptContext();
+  ScriptContext* script_context =
+      dispatcher->script_context_set().GetByV8Context(v8_context);
+  if (start)
+    TryInjectStartScript(web_frame, script_context ? script_context->extension() : nullptr, true);
+  if (!script_context)
+    return;
+  std::vector<v8::Handle<v8::Value> > arguments;
+  v8::Local<v8::Value> window =
+    web_frame->MainWorldScriptContext()->Global();
+  arguments.push_back(v8::Boolean::New(isolate, start));
+  arguments.push_back(window);
+  if (base::FeatureList::IsEnabled(::features::kNWNewWin)) {
+    content::RenderFrame* main_frame = frame->GetRenderView()->GetMainRenderFrame();
+    arguments.push_back(v8::Integer::New(isolate, main_frame->GetRoutingID()));
+    }
     }
     
-    /**
- * @brief Generate the separator string for query results
- *
- * @param lengths The data returned from computeQueryDataLengths
- * @param columns The order of the keys (since maps are unordered)
- *
- * @return A string, with a newline, representing your separator
- */
-std::string generateToken(const std::map<std::string, size_t>& lengths,
-                          const std::vector<std::string>& columns);
+        // Ignore first non-switch arg if it's not a standalone package.
+    bool ignore_arg = !package->self_extract();
+    for (unsigned i = 1; i < argv.size(); ++i) {
+      if (ignore_arg && argv[i] == args[0]) {
+        ignore_arg = false;
+        continue;
+      }
+    }
     
     
-    {  // Then compute lengths of column names.
-  computeRowLengths(q.front(), lengths, true);
-  expected = {{'name', 10}, {'age', 4}, {'food', 23}, {'number', 6}};
-  EXPECT_EQ(lengths, expected);
+    {  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method(
+      routing_id,
+      object_id,
+      type,
+      method,
+      *static_cast<base::ListValue*>(value_args.get())));
+  return v8::Undefined(isolate);
 }
     
-    template <typename EventType>
-using TypedKernelEventContextRef =
-    std::shared_ptr<TypedKernelEventContext<EventType>>;
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    }
+    
+    #include 'content/nw/src/api/menu/menu_delegate.h'
     
     
-    {TEST_F(SyslogTests, test_csv_separator) {
-  ASSERT_EQ(std::vector<std::string>({'', '', '', '', ''}), splitCsv(',,,,'));
-  ASSERT_EQ(std::vector<std::string>({' ', ' ', ' ', ' ', ' '}),
-            splitCsv(' , , , , '));
-  ASSERT_EQ(std::vector<std::string>({'foo', 'bar', 'baz'}),
-            splitCsv('foo,bar,baz'));
-  ASSERT_EQ(std::vector<std::string>({'foo', 'bar', 'baz'}),
-            splitCsv('\'foo\',\'bar\',\'baz\''));
-  ASSERT_EQ(std::vector<std::string>({',foo,', ',bar', 'baz,'}),
-            splitCsv('\',foo,\',\',bar\',\'baz,\''));
-  ASSERT_EQ(std::vector<std::string>({',f\\oo,', ',ba\\'r', 'baz\\,'}),
-            splitCsv('\',f\\oo,\',\',ba\\'r\',\'baz\\,\''));
-  ASSERT_EQ(std::vector<std::string>({'\',f\\o\'o,', '\',ba\\'r', 'baz\\,\''}),
-            splitCsv('\'\'\',f\\o\'\'o,\',\'\'\',ba\\'r\',\'baz\\,\'\'\''));
-  ASSERT_EQ(std::vector<std::string>({'\',f\\ø\'o,', '\',bá\\'r', 'baz\\,\''}),
-            splitCsv('\'\'\',f\\ø\'\'o,\',\'\'\',bá\\'r\',\'baz\\,\'\'\''));
+    {  is_menu_modified_ = true;
+  menu_item->menu_ = this;
+  menu_items_.insert(menu_items_.begin() + pos, menu_item);
 }
+    
+    class NwAppGetArgvSyncFunction : public NWSyncExtensionFunction {
+ public:
+  NwAppGetArgvSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    #include <vector>
+    
+    NwObjAllocateIdFunction::~NwObjAllocateIdFunction() {
 }
-
     
     
-    {
-    {  /// Protects the count of buffered logs
-  RecursiveMutex count_mutex_;
+    { protected:
+  int n_ = 0;
+  std::mutex m_;
+  std::condition_variable cv_;
 };
-}
-
     
+    OPERATOR_SCHEMA(Exp)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .IdenticalTypeAndShape()
+    .SetDoc(R'DOC(
+Calculates the exponential of the given input tensor ($exp(x)$), element-wise. This
+operation can be done in an in-place fashion too, by providing the same input
+and output blobs.
     
-    {        // [Internals]
-        FontInfo        Info;               // Font descriptor of the current font.
-        unsigned int    UserFlags;          // = ImFontConfig::RasterizerFlags
-        FT_Library      FreetypeLibrary;
-        FT_Face         FreetypeFace;
-        FT_Int32        FreetypeLoadFlags;
-    };
-    
-            static float f = 0.0f;
-        ImGui::Text('Hello, world!');
-        ImGui::SliderFloat('float', &f, 0.0f, 1.0f);
-        ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / io.Framerate, io.Framerate);
-        ImGui::ShowDemoWindow(NULL);
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'ALLEGRO_BITMAP*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-//  [X] Platform: Clipboard support (from Allegro 5.1.12)
-//  [X] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'.
-    
-    
-    {    return 0;
-}
-
-    
-        // Create Vulkan Instance
-    {
-        VkInstanceCreateInfo create_info = {};
-        create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-        create_info.enabledExtensionCount = extensions_count;
-        create_info.ppEnabledExtensionNames = extensions;
+    OpSchema::Cost CostInferenceForFC(
+    const OperatorDef& def,
+    const vector<TensorShape>& in) {
+  CAFFE_ENFORCE_EQ(in.size(), 3, 'FC requires three inputs');
+  struct OpSchema::Cost c;
+  ArgumentHelper helper(def);
     }
     
-        // Main loop
-    MSG msg;
-    ZeroMemory(&msg, sizeof(msg));
-    while (msg.message != WM_QUIT)
-    {
-        // Poll and handle messages (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-            continue;
-        }
+    namespace caffe2 {
     }
     
-    #endif
-
-    
-    
-    
-    class StringAppendOperator : public AssociativeMergeOperator {
- public:
-  // Constructor: specify delimiter
-  explicit StringAppendOperator(char delim_char);
+    namespace caffe2 {
+namespace {
+    }
     }
     
-      // Notify the underlying storage that no more items will be added.
-  // REQUIRES: external synchronization to prevent simultaneous
-  // operations on the same MemTable.
-  // After MarkImmutable() is called, you should not attempt to
-  // write anything to this MemTable().  (Ie. do not call Add() or Update()).
-  void MarkImmutable() {
-    table_->MarkReadOnly();
-    mem_tracker_.DoneAllocating();
+    /// Root key to retrieve Kafka topic configurations.
+const std::string kKafkaTopicParserRootKey('kafka_topics');
+    
+    Status PrometheusMetricsConfigParserPlugin::update(const std::string& source,
+                                                   const ParserConfig& config) {
+  auto prometheus_targets = config.find(kPrometheusParserRootKey);
+  if (prometheus_targets != config.end()) {
+    auto obj = data_.getObject();
+    data_.copyFrom(prometheus_targets->second.doc(), obj);
+    data_.add(kPrometheusParserRootKey, obj);
   }
-    
-    // A builder class to build a merging iterator by adding iterators one by one.
-class MergeIteratorBuilder {
- public:
-  // comparator: the comparator used in merging comparator
-  // arena: where the merging iterator needs to be allocated from.
-  explicit MergeIteratorBuilder(const InternalKeyComparator* comparator,
-                                Arena* arena, bool prefix_seek_mode = false);
-  ~MergeIteratorBuilder();
-    }
-    
-    int BaseComparatorJniCallback::Compare(const Slice& a, const Slice& b) const {
-  jboolean attached_thread = JNI_FALSE;
-  JNIEnv* env = getJniEnv(&attached_thread);
-  assert(env != nullptr);
-    }
-    
-      class LoggerJniCallback : public JniCallback, public Logger {
-   public:
-     LoggerJniCallback(JNIEnv* env, jobject jLogger);
-     ~LoggerJniCallback();
-    }
-    
-    double HistogramStat::StandardDeviation() const {
-  uint64_t cur_num = num();
-  uint64_t cur_sum = sum();
-  uint64_t cur_sum_squares = sum_squares();
-  if (cur_num == 0) return 0;
-  double variance =
-      static_cast<double>(cur_sum_squares * cur_num - cur_sum * cur_sum) /
-      static_cast<double>(cur_num * cur_num);
-  return sqrt(variance);
-}
-std::string HistogramStat::ToString() const {
-  uint64_t cur_num = num();
-  std::string r;
-  char buf[1650];
-  snprintf(buf, sizeof(buf),
-           'Count: %' PRIu64 ' Average: %.4f  StdDev: %.2f\n',
-           cur_num, Average(), StandardDeviation());
-  r.append(buf);
-  snprintf(buf, sizeof(buf),
-           'Min: %' PRIu64 '  Median: %.4f  Max: %' PRIu64 '\n',
-           (cur_num == 0 ? 0 : min()), Median(), (cur_num == 0 ? 0 : max()));
-  r.append(buf);
-  snprintf(buf, sizeof(buf),
-           'Percentiles: '
-           'P50: %.2f P75: %.2f P99: %.2f P99.9: %.2f P99.99: %.2f\n',
-           Percentile(50), Percentile(75), Percentile(99), Percentile(99.9),
-           Percentile(99.99));
-  r.append(buf);
-  r.append('------------------------------------------------------\n');
-  if (cur_num == 0) return r;   // all buckets are empty
-  const double mult = 100.0 / cur_num;
-  uint64_t cumulative_sum = 0;
-  for (unsigned int b = 0; b < num_buckets_; b++) {
-    uint64_t bucket_value = bucket_at(b);
-    if (bucket_value <= 0.0) continue;
-    cumulative_sum += bucket_value;
-    snprintf(buf, sizeof(buf),
-             '%c %7' PRIu64 ', %7' PRIu64 ' ] %8' PRIu64 ' %7.3f%% %7.3f%% ',
-             (b == 0) ? '[' : '(',
-             (b == 0) ? 0 : bucketMapper.BucketLimit(b-1),  // left
-              bucketMapper.BucketLimit(b),  // right
-              bucket_value,                   // count
-             (mult * bucket_value),           // percentage
-             (mult * cumulative_sum));       // cumulative percentage
-    r.append(buf);
-    }
     }
     
     
     {
+    {  c.reset();
 }
-    
-    // Unless required by applicable law or agreed to in writing, software distributed under the License is
-// distributed on an 'AS IS' basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-// either express or implied. See the License for the specific language governing permissions and
-// limitations under the License.
-    
-        void RemoveSpy(void* _this) {
-        m_thismap.erase(_this);
-    }
-    
-    
-Test_Spy_Sample::Test_Spy_Sample()
-{
-    SPY_ATTACH_CLASS(NULL);
 }
+
     
-    #ifndef COMM_HAS_MEMBER_H_
-#define COMM_HAS_MEMBER_H_
+      auto obj = data_.getObject();
+  data_.copyFrom(cv->second.doc(), obj);
+  data_.add('views', obj);
+    
+    #include <gtest/gtest.h>
+    
+      auto constraint = Constraint(EQUALS);
+  constraint.expr = 'some';
+  cl.add(constraint);
+    
+    
+    {/// Get a performance limit by name and optional level.
+size_t getWorkerLimit(WatchdogLimitType limit);
+}
+
+    
+    
+    {  EXPECT_EQ(reply.type, audit_event_record.type);
+  EXPECT_EQ('1440542781.644:403030', audit_event_record.audit_id);
+  EXPECT_EQ(audit_event_record.fields.size(), 4U);
+  EXPECT_EQ(audit_event_record.fields.count('argc'), 1U);
+  EXPECT_EQ(audit_event_record.fields['argc'], '3');
+  EXPECT_EQ(audit_event_record.fields['a0'], '\'H=1 \'');
+  EXPECT_EQ(audit_event_record.fields['a1'], '\'/bin/sh\'');
+  EXPECT_EQ(audit_event_record.fields['a2'], 'c');
+}
