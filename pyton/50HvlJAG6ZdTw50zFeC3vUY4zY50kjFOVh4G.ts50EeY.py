4f2084bd4502418c7dc2fe5f977d04b315e9fff9
SUNsung <weatherius@gmail.com>
2018-10-03 17:43:41 +0300

    Rewrite [50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py] File in: 2018-10-03 17:43:40

--- pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
+++ pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
@@ -1,58 +1,204 @@
 
         
-                    try:
-                self.working_appid_list.remove(appid)
-            except:
-                pass
+            # test that the user was inserted into the database
+    with app.app_context():
+        assert get_db().execute(
+            'select * from user where username = 'a'',
+        ).fetchone() is not None
     
-            if response.status == 503:
-            # out of quota
-            if 'gws' not in server_type and 'Google Frontend' not in server_type and 'GFE' not in server_type:
-                xlog.warn('503 but server type:%s', server_type)
-                return False
-            else:
-                return True
+        auth.login()
+    # current user can't modify other user's post
+    assert client.post('/1/update').status_code == 403
+    assert client.post('/1/delete').status_code == 403
+    # current user doesn't see edit link
+    assert b'href='/1/update'' not in client.get('/').data
+    
+        def record_once(self, func):
+        '''Works like :meth:`record` but wraps the function in another
+        function that will ensure the function is only called once.  If the
+        blueprint is registered a second time on the application, the
+        function passed is not called.
+        '''
+        def wrapper(state):
+            if state.first_registration:
+                func(state)
+        return self.record(update_wrapper(wrapper, func))
+    
+        Probably the most interesting way to load configurations is from an
+    environment variable pointing to a file::
+    
+            buf.append('  Make sure to directly send your %s-request to this URL '
+                   'since we can\'t make browsers or HTTP clients redirect '
+                   'with form data reliably or without user interaction.' %
+                   request.method)
+        buf.append('\n\nNote: this exception is only raised in debug mode')
+        AssertionError.__init__(self, ''.join(buf).encode('utf-8'))
+    
+    from functools import partial
+from werkzeug.local import LocalStack, LocalProxy
+    
+        if b'\x00' not in head:
+        return 'utf-8'
+    
+    
+#: Log messages to :func:`~flask.logging.wsgi_errors_stream` with the format
+#: ``[%(asctime)s] %(levelname)s in %(module)s: %(message)s``.
+default_handler = logging.StreamHandler(wsgi_errors_stream)
+default_handler.setFormatter(logging.Formatter(
+    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+))
+    
+        #: Some implementations can detect whether a session is newly
+    #: created, but that is not guaranteed. Use with caution. The mixin
+    # default is hard-coded ``False``.
+    new = False
+    
+        :copyright: © 2010 by the Pallets team.
+    :license: BSD, see LICENSE for more details.
+'''
+    
+    from ansible.module_utils.ec2 import get_aws_connection_info
+from ansible.module_utils.ec2 import ec2_argument_spec, boto3_conn
+from ansible.module_utils.ec2 import snake_dict_to_camel_dict
+from ansible.module_utils.ec2 import camel_dict_to_snake_dict
+from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.aws.cloudfront_facts import CloudFrontFactsServiceManager
+import datetime
+    
+    # Activate pipeline
+- data_pipeline:
+    name: test-dp
+    region: us-west-2
+    state: active
+    
+    # AMI Creation, with a custom root-device size and another EBS attached
+- ec2_ami:
+    instance_id: i-xxxxxx
+    name: newtest
+    device_mapping:
+        - device_name: /dev/sda1
+          size: XXX
+          delete_on_termination: true
+          volume_type: gp2
+        - device_name: /dev/sdb
+          size: YYY
+          delete_on_termination: false
+          volume_type: gp2
+    
+    import re
+    
+        if region:
+        try:
+            connection = connect_to_aws(boto.ec2.cloudwatch, region, **aws_connect_params)
+        except (NoAuthHandlerFound, AnsibleAWSError) as e:
+            module.fail_json(msg=str(e))
+    else:
+        module.fail_json(msg='region must be specified')
+    
+    
+def get_github_url(app, view, path):
+    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
+    return (
+        github_fmt.format(app.config.edit_on_github_project, view,
+                          app.config.edit_on_github_branch,
+                          app.config.edit_on_github_src_path, path))
+    
+        return '{}/blob/{}/{}{}'.format(GITHUB_URL, code_branch, fn, linespec)
+    
+        hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, _shutdown)
+    
+        def get_actiontec_data(self):
+        '''Retrieve data from Actiontec MI424WR and return parsed result.'''
+        try:
+            telnet = telnetlib.Telnet(self.host)
+            telnet.read_until(b'Username: ')
+            telnet.write((self.username + '\n').encode('ascii'))
+            telnet.read_until(b'Password: ')
+            telnet.write((self.password + '\n').encode('ascii'))
+            prompt = telnet.read_until(
+                b'Wireless Broadband Router> ').split(b'\n')[-1]
+            telnet.write('firewall mac_cache_dump\n'.encode('ascii'))
+            telnet.write('\n'.encode('ascii'))
+            telnet.read_until(prompt)
+            leases_result = telnet.read_until(prompt).split(b'\n')[1:-1]
+            telnet.write('exit\n'.encode('ascii'))
+        except EOFError:
+            _LOGGER.exception('Unexpected response from router')
+            return
+        except ConnectionRefusedError:
+            _LOGGER.exception('Connection refused by router. Telnet enabled?')
+            return None
     
         try:
-        if cookie:
-            if 'rc4' not in options:
-                metadata = zlib.decompress(base64.b64decode(cookie), -zlib.MAX_WBITS)
-                payload = input_data or ''
-            else:
-                metadata = zlib.decompress(rc4crypt(base64.b64decode(cookie), __password__), -zlib.MAX_WBITS)
-                payload = rc4crypt(input_data, __password__) if input_data else ''
-        else:
-            if 'rc4' in options:
-                input_data = rc4crypt(input_data, __password__)
-            metadata_length = struct.unpack('!h', input_data[:2])
-            metadata = zlib.decompress(input_data[2:2+metadata_length], -zlib.MAX_WBITS)
-            payload = input_data[2+metadata_length:]
-        headers = dict(x.split(':', 1) for x in metadata.splitlines() if x)
-        method = headers.pop('G-Method')
-        url = headers.pop('G-Url')
-    except (zlib.error, KeyError, ValueError):
-        import traceback
-        start_response('500 Internal Server Error', [('Content-Type', 'text/html')])
-        yield message_html('500 Internal Server Error', 'Bad Request (metadata) - Possible Wrong Password', '<pre>%s</pre>' % traceback.format_exc())
-        raise StopIteration
-    
-    - ANTLRStringStream: Reads from a string objects. The input should be a unicode
-  object, or ANTLR3 will have trouble decoding non-ascii data.
-- ANTLRFileStream: Opens a file and read the contents, with optional character
-  decoding.
-- ANTLRInputStream: Reads the date from a file-like object, with optional
-  character decoding.
+        res = requests.post(url, data=data, timeout=5, **kwargs)
+    except requests.exceptions.Timeout:
+        _LOGGER.exception('Connection to the router timed out')
+        return
+    if res.status_code == 200:
+        try:
+            result = res.json()
+        except ValueError:
+            # If json decoder could not parse the response
+            _LOGGER.exception('Failed to parse response from luci')
+            return
+        try:
+            return result['result']
+        except KeyError:
+            _LOGGER.exception('No result in response from luci')
+            return
+    elif res.status_code == 401:
+        # Authentication error
+        _LOGGER.exception(
+            'Failed to authenticate, check your username and password')
+        return
+    elif res.status_code == 403:
+        _LOGGER.error('Luci responded with a 403 Invalid token')
+        raise InvalidLuciTokenError
     
     
-class CommonTokenStream(TokenStream):
-    '''
-    @brief The most common stream of tokens
+def _arp(ip_address):
+    '''Get the MAC address for a given IP.'''
+    cmd = ['arp', '-n', ip_address]
+    arp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out, _ = arp.communicate()
+    match = re.search(r'(([0-9A-Fa-f]{1,2}\:){5}[0-9A-Fa-f]{1,2})', str(out))
+    if match:
+        return match.group(0)
+    _LOGGER.info('No MAC address found for %s', ip_address)
+    return None
     
-    The most common stream of tokens is one where every token is buffered up
-    and tokens are prefiltered for a certain channel (the parser will only
-    see these tokens and cannot change the filter channel number during the
-    parse).
-    '''
+        def see_vehicle(vehicle):
+        '''Handle the reporting of the vehicle position.'''
+        host_name = voc.vehicle_name(vehicle)
+        dev_id = 'volvo_{}'.format(slugify(host_name))
+        see(dev_id=dev_id,
+            host_name=host_name,
+            gps=(vehicle.position['latitude'],
+                 vehicle.position['longitude']),
+            icon='mdi:car')
     
-            You don't have to implement but it's nice to know where a Token
-        comes from if you have include files etc... on the input.'''
+        def __init__(self, hass, config):
+        '''Set up main service.'''
+        conf = config[DOMAIN]
+        from pizzapi import Address, Customer
+        from pizzapi.address import StoreException
+        self.hass = hass
+        self.customer = Customer(
+            conf.get(ATTR_FIRST_NAME),
+            conf.get(ATTR_LAST_NAME),
+            conf.get(ATTR_EMAIL),
+            conf.get(ATTR_PHONE),
+            conf.get(ATTR_ADDRESS))
+        self.address = Address(
+            *self.customer.address.split(','),
+            country=conf.get(ATTR_COUNTRY))
+        self.country = conf.get(ATTR_COUNTRY)
+        try:
+            self.closest_store = self.address.closest_store()
+        except StoreException:
+            self.closest_store = None
+    
+    
+def clean_socket_close(sock):
+    '''Close a socket connection and logs its closure.'''
+    _LOGGER.info('UPNP responder shutting down.')
