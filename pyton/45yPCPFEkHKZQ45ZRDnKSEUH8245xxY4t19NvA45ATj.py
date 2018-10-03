e8e873d47f144ca0dfbcc1403e297c3d253a392a
SUNsung <weatherius@gmail.com>
2018-10-03 18:18:38 +0300

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2018-10-03 18:18:37

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,125 +1,66 @@
 
         
-        
-def test_update(client, auth, app):
-    auth.login()
-    assert client.get('/1/update').status_code == 200
-    client.post('/1/update', data={'title': 'updated', 'body': ''})
-    
-    PY2 = sys.version_info[0] == 2
-_identity = lambda x: x
-    
-            .. versionadded:: 0.7
-           The `use_cookies` parameter was added as well as the ability
-           to override the client to be used by setting the
-           :attr:`test_client_class` attribute.
-    
-        def copy(self):
-        '''Creates a copy of this request context with the same request object.
-        This can be used to move a request context to a different greenlet.
-        Because the actual request object is the same this cannot be used to
-        move a request context to a different thread unless access to the
-        request object is locked.
-    
-            if triple is None:
-            detail = 'no match'
-        else:
-            detail = 'found (%r)' % (triple[1] or '<string>')
-            total_found += 1
-        info.append('       -> %s' % detail)
-    
-        def _fail(self, *args, **kwargs):
-        raise RuntimeError('The session is unavailable because no secret '
-                           'key was set.  Set the secret_key on the '
-                           'application to something unique and secret.')
-    __setitem__ = __delitem__ = clear = pop = popitem = \
-        update = setdefault = _fail
-    del _fail
+        #A Python script to generate a single PDF document with all the tldr pages. It works by generating 
+#intermediate HTML files from existing md files using Python-markdown, applying desired formating 
+#through CSS, and finally rendering them as PDF. There is no LaTeX dependency for generating the PDF.
     
     
-@pytest.fixture(autouse=True)
-def usage_tracker_io(usage_tracker):
-    io = StringIO()
-    usage_tracker.return_value \
-                 .open.return_value \
-                 .__enter__.return_value = io
-    return io
+def _ValidateResponseObject( response ):
+  our_hmac = CreateHmac( response.content, BaseRequest.hmac_secret )
+  their_hmac = ToBytes( b64decode( response.headers[ _HMAC_HEADER ] ) )
+  if not SecureBytesEqual( our_hmac, their_hmac ):
+    raise RuntimeError( 'Received invalid HMAC for response!' )
+  return True
     
-        new_command = get_new_command(Command('sudo apt list --upgradable', match_output))
-    assert new_command == 'sudo apt upgrade'
-
+        self._response_future = self.PostDataToHandlerAsync( request_data,
+                                                         'event_notification' )
     
-        def call(self, inputs):
-        inputs -= K.mean(inputs, axis=1, keepdims=True)
-        inputs = K.l2_normalize(inputs, axis=1)
-        pos = K.relu(inputs)
-        neg = K.relu(-inputs)
-        return K.concatenate([pos, neg], axis=1)
-    
-        def _canonical_to_params(self, weights, biases):
-        import tensorflow as tf
-        weights = [tf.reshape(x, (-1,)) for x in weights]
-        biases = [tf.reshape(x, (-1,)) for x in biases]
-        return tf.concat(weights + biases, 0)
-    
-    legacy_cropping2d_support = generate_legacy_interface(
-    allowed_positional_args=['cropping'],
-    conversions=[('dim_ordering', 'data_format')],
-    value_conversions={'dim_ordering': {'tf': 'channels_last',
-                                        'th': 'channels_first',
-                                        'default': None}})
-    
-        # Arguments
-        l1: Float; L1 regularization factor.
-        l2: Float; L2 regularization factor.
-    '''
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
     
     
-def test_boston_housing():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
+  def Start( self ):
+    self.PostDataToHandler( {},
+                            'shutdown',
+                            TIMEOUT_SECONDS,
+                            display_message = False )
     
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
     
-def get_example_array():
-    np.random.seed(3537)
-    example_array = np.random.random((100, 100)) * 100. - 50.
-    example_array[0, 0] = 0.  # 0 could possibly cause trouble
-    return example_array
+      # As a last resort, we search python in the PATH. We prefer Python 2 over 3
+  # for the sake of backwards compatibility with ycm_extra_conf.py files out
+  # there; few people wrote theirs to work on py3.
+  # So we check 'python2' before 'python' because on some distributions (Arch
+  # Linux for example), python refers to python3.
+  python_interpreter = utils.PathToFirstExistingExecutable( [ 'python2',
+                                                              'python',
+                                                              'python3' ] )
+  if python_interpreter:
+    return python_interpreter
     
-        def test_default_decoder(self):
-        from acme.fields import RFC3339Field
-        self.assertEqual(
-            self.decoded, RFC3339Field.default_decoder(self.encoded))
+        with MockCurrentColumnAndLineContents( 4, 'føo(' ):
+      ok_( base.LastEnteredCharIsIdentifierChar() )
     
-        # TODO: decoder/encoder should accept cls? Otherwise, subclassing
-    # JSONObjectWithFields is tricky...
-    header_cls = Header
-    header = jose.Field(
-        'header', omitempty=True, default=header_cls(),
-        decoder=header_cls.from_json)
-    
-            self.assertEqual(jws, JWS.from_json(jws.to_json()))
-    
-        '''
-    return _split_aug_path(vhost_path)[1]
-    
-            save_files = set()
-        if save_paths:
-            for path in save_paths:
-                save_files.add(self.aug.get(path)[6:])
-        return save_files
-    
-    
-UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
-'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
-    
-            :returns: If addresses conflicts with vhost
-        :rtype: bool
-    
-        def setUp(self):  # pylint: disable=arguments-differ
-        super(TlsSniPerformTest, self).setUp()
+      def _adjust_thread_count( self ):
+    # When the executor gets lost, the weakref callback will wake up
+    # the worker threads.
+    def weakref_cb( _, q=self._work_queue ):
+      q.put( None )
+    # TODO(bquinlan): Should avoid creating new threads if there are more
+    # idle threads than items in the work queue.
+    if len( self._threads ) < self._max_workers:
+      t = threading.Thread( target=_worker,
+                            args=( weakref.ref( self, weakref_cb ),
+                                   self._work_queue ) )
+      t.daemon = True
+      t.start()
+      self._threads.add( t )
