91fd7f8555bb6546688414589e3c4adea0ac0f43
SUNsung <weatherius@gmail.com>
2018-10-04 11:32:25 +0300

    Rewrite [386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb] File in: 2018-10-04 11:32:24

--- ruby/386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb
+++ ruby/386VqW3J45jbQ38NKlHIv6Q38638SYVGpi0mZ2k38bFe.rb
@@ -1,30 +1,287 @@
 
         
-              def merge!(other)
-        other.each do |key, value|
-          self[convert_key(key)] = value
-        end
-        self
+              open_dry_run_modal(agent)
+      find('.dry-run-event-sample').click
+      within(:css, '.modal .builder') do
+        expect(page).to have_text('http://xkcd.com/')
+      end
+      click_on('Dry Run')
+      expect(page).to have_text('Biologists play reverse')
+      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
+    end
+    
+      it 'asks to accept conflicts when the scenario was modified' do
+    DefaultScenarioImporter.seed(user)
+    agent = user.agents.where(name: 'Rain Notifier').first
+    agent.options['expected_receive_period_in_days'] = 9001
+    agent.save!
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
+    click_on 'Start Import'
+    expect(page).to have_text('This Scenario already exists in your system.')
+    expect(page).to have_text('9001')
+    check('I confirm that I want to import these Agents.')
+    click_on 'Finish Import'
+    expect(page).to have_text('Import successful!')
+  end
+    
+      describe '#nav_link' do
+    it 'returns a nav link' do
+      stub(self).current_page?('/things') { false }
+      nav = nav_link('Things', '/things')
+      a = Nokogiri(nav).at('li:not(.active) > a[href='/things']')
+      expect(a.text.strip).to eq('Things')
+    end
+    
+        it 'can be turned off' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
+      DefaultScenarioImporter.import(user)
+    end
+    
+    describe AgentRunner do
+  context 'without traps' do
+    before do
+      stub.instance_of(Rufus::Scheduler).every
+      stub.instance_of(AgentRunner).set_traps
+      @agent_runner = AgentRunner.new
+    end
+    
+        it 'strips leading and trailing dashes' do
+      expect(AgentsExporter.new(:name => ',foo,').filename).to eq('foo.json')
+    end
+    
+        it 'html_safes the output unless :skip_safe is passed in' do
+      expect(Utils.jsonify({:foo => 'bar'})).to be_html_safe
+      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => false)).to be_html_safe
+      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => true)).not_to be_html_safe
+    end
+  end
+    
+    gem 'activemodel-serializers-xml', github: 'rails/activemodel-serializers-xml'
+    
+        # Check if a reset_password_token is provided in the request
+    def assert_reset_token_passed
+      if params[:reset_password_token].blank?
+        set_flash_message(:alert, :no_token)
+        redirect_to new_session_path(resource_name)
+      end
+    end
+    
+        # The path used after unlocking the resource
+    def after_unlock_path_for(resource)
+      new_session_path(resource)  if is_navigational_format?
+    end
+    
+      # Request keys used when authenticating a user.
+  mattr_accessor :request_keys
+  @@request_keys = []
+    
+          # The default url to be used after signing in. This is used by all Devise
+      # controllers and you can overwrite it in your ApplicationController to
+      # provide a custom hook for a custom resource.
+      #
+      # By default, it first tries to find a valid resource_return_to key in the
+      # session, then it fallbacks to resource_root_path, otherwise it uses the
+      # root path. For a user scope, you can define the default url in
+      # the following way:
+      #
+      #   get '/users' => 'users#index', as: :user_root # creates user_root_path
+      #
+      #   namespace :user do
+      #     root 'users#index' # creates user_root_path
+      #   end
+      #
+      # If the resource root path is not defined, root_path is used. However,
+      # if this default is not enough, you can customize it, for example:
+      #
+      #   def after_sign_in_path_for(resource)
+      #     stored_location_for(resource) ||
+      #       if resource.is_a?(User) && resource.can_publish?
+      #         publisher_url
+      #       else
+      #         super
+      #       end
+      #   end
+      #
+      def after_sign_in_path_for(resource_or_scope)
+        stored_location_for(resource_or_scope) || signed_in_root_path(resource_or_scope)
       end
     
-    $stdout.puts res
+          # Remembers the given resource by setting up a cookie
+      def remember_me(resource)
+        return if request.env['devise.skip_storage']
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.remember_me!
+        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
+      end
+    
+          def expire_data_after_sign_in!
+        # session.keys will return an empty array if the session is not yet loaded.
+        # This is a bug in both Rack and Rails.
+        # A call to #empty? forces the session to be loaded.
+        session.empty?
+        session.keys.grep(/^devise\./).each { |k| session.delete(k) }
+      end
+    
+            I18n.t(:'#{scope}.#{message}', options)
+      else
+        message.to_s
+      end
+    end
+    
+      def set_filters
+    @filters = current_account.custom_filters
+  end
+    
+      def scope_for_collection
+    case params[:id]
+    when 'featured'
+      @account.statuses.permitted_for(@account, signed_request_account).tap do |scope|
+        scope.merge!(@account.pinned_statuses)
+      end
+    else
+      raise ActiveRecord::NotFound
+    end
+  end
+    
+        def set_account_moderation_note
+      @account_moderation_note = AccountModerationNote.find(params[:id])
+    end
+  end
+end
 
     
-            when :err
-          case s[:last]
-            when :pass
-              # Oops got a -ERR after a pass so its crap ignore the pass
-              # But report it, might be helpfull for guessing :-)
+        def set_report_note
+      @report_note = ReportNote.find(params[:id])
+    end
+  end
+end
+
     
-    		self.block[-1] << '    ''
+        def create
+      authorize :status, :update?
     
-          def report
-        <<-EOS
+      def process_push_request
+    case hub_mode
+    when 'subscribe'
+      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
+    when 'unsubscribe'
+      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
+    else
+      ['Unknown mode: #{hub_mode}', 422]
+    end
+  end
     
-          def update_if_necessary!
-        if @update && config.verbose?
-          UI.section('\nUpdating Spec Repositories\n'.yellow) do
-            Repo.new(ARGV.new(['update'])).run
+      def show
+    if subscription.valid?(params['hub.topic'])
+      @account.update(subscription_expires_at: future_expires)
+      render plain: encoded_challenge, status: 200
+    else
+      head 404
+    end
+  end
+    
+    # skip over blogs that aren't found
+unavailable = []
+fast_forwards = [
+  'Baidu Research',
+  'Booking.com',
+  'Fynd',
+  'Graphcool',
+  'LinkedIn',
+  'Medallia',
+  'OmniTI',
+  'Paperless Post',
+  'Pluralsight',
+  'Prolific Interactive',
+  'Quora',
+  'Robert Elder Software',
+  'Simple',
+  'SlideShare',
+  'SourceClear',
+  'Viget',
+  'Zalando',
+  'Zapier',
+  'Zynga',
+  'Dave Beazley',
+  'Edan Kwan',
+  'Grzegorz Gajos',
+  'Joe Armstrong',
+  'Kai Hendry',
+  'LiveOverflow'
+]
+    
+      def self.create_ipmi_rakp_1(bmc_session_id, console_random_id, username)
+    head = [
+      0x06, 0x00, 0xff, 0x07,  # RMCP Header
+      0x06,                    # RMCP+ Authentication Type
+      PAYLOAD_RAKP1,           # Payload Type
+      0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00,
+    ].pack('C*')
+    
+                res = ''
+            case etype
+            when RC4_HMAC
+              res = encrypt_rc4_hmac(data, key, 5)
+            else
+              raise ::NotImplementedError, 'EncryptedData schema is not supported'
+            end
+    
+              # Decodes the kvno from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_kvno(input)
+            input.value[0].value.to_i
+          end
+    
+              # Decodes the till field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Time]
+          def decode_till(input)
+            input.value[0].value
+          end
+    
+              # Decodes the ctime field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Time]
+          def decode_ctime(input)
+            input.value[0].value
+          end
+    
+            def image_params
+          params.require(:image).permit(permitted_image_attributes)
+        end
+    
+            def remove_coupon_code
+          find_order(true)
+          authorize! :update, @order, order_token
+          @handler = Spree::PromotionHandler::Coupon.new(@order).remove(params[:coupon_code])
+          status = @handler.successful? ? 200 : 404
+          render 'spree/api/v1/promotions/handler', status: status
+        end
+    
+            def update
+          if @property
+            authorize! :update, @property
+            @property.update_attributes(property_params)
+            respond_with(@property, status: 200, default_template: :show)
+          else
+            invalid_resource!(@property)
           end
         end
-      end
+    
+              @original_shipment.transfer_to_location(@variant, @quantity, @stock_location)
+          render json: { success: true, message: Spree.t(:shipment_transfer_success) }, status: 201
+        end
+    
+            def index
+          authorize! :read, StockMovement
+          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@stock_movements)
+        end
