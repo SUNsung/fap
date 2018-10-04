4196fb5fe623de18689b3c2eb7d37204802c4997
SUNsung <weatherius@gmail.com>
2018-10-04 05:11:10 +0300

    Rewrite [23xZHIUO8q2Fc23BmvZdnam3wU23rcnJrLSUUY23UaY.py] File in: 2018-10-04 05:11:09

--- pyton/23xZHIUO8q2Fc23BmvZdnam3wU23rcnJrLSUUY23UaY.py
+++ pyton/23xZHIUO8q2Fc23BmvZdnam3wU23rcnJrLSUUY23UaY.py
@@ -1,195 +1,215 @@
 
         
-        
-def create_app(test_config=None):
-    '''Create and configure an instance of the Flask application.'''
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY='dev',
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+            def reject_friend_request(self, from_user_id, to_user_id):
+        pass
+    
+    from mrjob.job import MRJob
     
     
-def _default_template_ctx_processor():
-    '''Default template context processor.  Injects `request`,
-    `session` and `g`.
-    '''
-    reqctx = _request_ctx_stack.top
-    appctx = _app_ctx_stack.top
-    rv = {}
-    if appctx is not None:
-        rv['g'] = appctx.g
-    if reqctx is not None:
-        rv['request'] = reqctx.request
-        rv['session'] = reqctx.session
-    return rv
+class Budget(object):
     
-        test_time = 0.0
-    refleak = False  # True if the test leaked references.
-    try:
-        abstest = get_abs_module(ns, test)
-        clear_caches()
-        with saved_test_environment(test, ns.verbose, ns.quiet, pgo=ns.pgo) as environment:
-            start_time = time.time()
-            the_module = importlib.import_module(abstest)
-            # If the test has a test_main, that will run the appropriate
-            # tests.  If not, use normal unittest test loading.
-            test_runner = getattr(the_module, 'test_main', None)
-            if test_runner is None:
-                def test_runner():
-                    loader = unittest.TestLoader()
-                    tests = loader.loadTestsFromModule(the_module)
-                    for error in loader.errors:
-                        print(error, file=sys.stderr)
-                    if loader.errors:
-                        raise Exception('errors while loading tests')
-                    support.run_unittest(tests)
-            if ns.huntrleaks:
-                refleak = dash_R(the_module, test, test_runner, ns.huntrleaks)
-            else:
-                test_runner()
-            test_time = time.time() - start_time
-        post_test_cleanup()
-    except support.ResourceDenied as msg:
-        if not ns.quiet and not ns.pgo:
-            print(test, 'skipped --', msg, flush=True)
-        return RESOURCE_DENIED, test_time
-    except unittest.SkipTest as msg:
-        if not ns.quiet and not ns.pgo:
-            print(test, 'skipped --', msg, flush=True)
-        return SKIPPED, test_time
-    except KeyboardInterrupt:
-        raise
-    except support.TestFailed as msg:
-        if not ns.pgo:
-            if display_failure:
-                print('test', test, 'failed --', msg, file=sys.stderr,
-                      flush=True)
-            else:
-                print('test', test, 'failed', file=sys.stderr, flush=True)
-        return FAILED, test_time
-    except:
-        msg = traceback.format_exc()
-        if not ns.pgo:
-            print('test', test, 'crashed --', msg, file=sys.stderr,
-                  flush=True)
-        return FAILED, test_time
-    else:
-        if refleak:
-            return FAILED, test_time
-        if environment.changed:
-            return ENV_CHANGED, test_time
-        return PASSED, test_time
-    
-        def run(self):
-        try:
-            stop = False
-            while not stop:
-                stop = self._runtest()
-        except BaseException:
-            self.output.put((None, None, None, None))
-            raise
-    
-        suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(TestTests))
-    stream = io.StringIO()
-    runner_cls = get_test_runner_class(sum(a == '-v' for a in sys.argv))
-    runner = runner_cls(sys.stdout)
-    result = runner.run(suite)
-    print('Output:', stream.getvalue())
-    print('XML: ', end='')
-    for s in ET.tostringlist(result.get_xml_element()):
-        print(s.decode(), end='')
-    print()
-
-    
-        # unpack non-capturing groups
-    for i in range(len(subpattern))[::-1]:
-        op, av = subpattern[i]
-        if op is SUBPATTERN:
-            group, add_flags, del_flags, p = av
-            if group is None and not add_flags and not del_flags:
-                subpattern[i: i+1] = p
-    
-    
-# Are two open files really referencing the same file?
-# (Not necessarily the same file descriptor!)
-def sameopenfile(fp1, fp2):
-    '''Test whether two open file objects reference the same file'''
-    s1 = os.fstat(fp1)
-    s2 = os.fstat(fp2)
-    return samestat(s1, s2)
-    
-        @unittest.skipIf(posix is None, 'Test requires posix module')
-    def test_ismount_different_device(self):
-        # Simulate the path being on a different device from its parent by
-        # mocking out st_dev.
-        save_lstat = os.lstat
-        def fake_lstat(path):
-            st_ino = 0
-            st_dev = 0
-            if path == ABSTFN:
-                st_dev = 1
-                st_ino = 1
-            return posix.stat_result((0, st_ino, st_dev, 0, 0, 0, 0, 0, 0, 0))
-        try:
-            os.lstat = fake_lstat
-            self.assertIs(posixpath.ismount(ABSTFN), True)
-        finally:
-            os.lstat = save_lstat
-    
-    
-class RadioTest(unittest.TestCase):
-    '''
-    Attention: Test case results depend on test case execution. The test cases
-    in this integration test class should be executed in an explicit order:
-    http://stackoverflow.com/questions/5387299/python-unittest-testcase-execution-order
-    '''
-    
-        def main_method(self):
-        '''will execute either _instance_method_1 or _instance_method_2
-    
-        def undo(self):
-        self.rename(self.dest, self.src)
-    
-        def attach(self, observer):
-        if observer not in self._observers:
-            self._observers.append(observer)
+        for asset in os.listdir(build_path):
+        compat_print('Uploading %s...' % asset)
+        releaser.create_asset(release_id, os.path.join(build_path, asset))
     
     
 def main():
-    message_center = Provider()
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
     
-    ### OUTPUT ###
-# We have a lovely Cat
-# It says meow
+        outfile, = args
+    
+    compat_print('total downloads traffic: %s' % format_size(total_bytes))
+
+    
+    # The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
 #
-# We have a lovely Dog
-# It says woof
-# ====================
-# We have a lovely Cat
-# It says meow
-# ====================
-# We have a lovely Cat
-# It says meow
-# ====================
+# The short X.Y version.
+from youtube_dl.version import __version__
+version = __version__
+# The full version, including alpha/beta/rc tags.
+release = version
+    
+    py2exe_options = {
+    'bundle_files': 1,
+    'compressed': 1,
+    'optimize': 2,
+    'dist_dir': '.',
+    'dll_excludes': ['w9xpopen.exe', 'crypt32.dll'],
+}
+    
+    
+class TestInfoExtractor(unittest.TestCase):
+    def setUp(self):
+        self.ie = TestIE(FakeYDL())
+    
+            password = intlist_to_bytes(self.key).decode('utf-8')
+        encrypted = base64.b64encode(
+            intlist_to_bytes(self.iv[:8]) +
+            b'\x0b\xe6\xa4\xd9z\x0e\xb8\xb9\xd0\xd4i_\x85\x1d\x99\x98_\xe5\x80\xe7.\xbf\xa5\x83'
+        ).decode('utf-8')
+        decrypted = (aes_decrypt_text(encrypted, password, 32))
+        self.assertEqual(decrypted, self.secret_msg)
+    
+        '''
+    is_windows = is_windows
+    config_dir = DEFAULT_CONFIG_DIR
+    stdin = sys.stdin
+    stdin_isatty = stdin.isatty()
+    stdin_encoding = None
+    stdout = sys.stdout
+    stdout_isatty = stdout.isatty()
+    stdout_encoding = None
+    stderr = sys.stderr
+    stderr_isatty = stderr.isatty()
+    colors = 256
+    if not is_windows:
+        if curses:
+            try:
+                curses.setupterm()
+                colors = curses.tigetnum('colors')
+            except curses.error:
+                pass
+    else:
+        # noinspection PyUnresolvedReferences
+        import colorama.initialise
+        stdout = colorama.initialise.wrap_stream(
+            stdout, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        stderr = colorama.initialise.wrap_stream(
+            stderr, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        del colorama
+    
+        def get_lexer(self, mime, body):
+        return get_lexer(
+            mime=mime,
+            explicit_json=self.explicit_json,
+            body=body,
+        )
+    
+    
+class DigestAuthPlugin(BuiltinAuthPlugin):
+    
+        def get_formatters_grouped(self):
+        groups = {}
+        for group_name, group in groupby(
+                self.get_formatters(),
+                key=lambda p: getattr(p, 'group_name', 'format')):
+            groups[group_name] = list(group)
+        return groups
+    
+    
+def test_headers_empty_value_with_value_gives_error(httpbin):
+    with pytest.raises(ParseError):
+        http('GET', httpbin + '/headers', 'Accept;SYNTAX_ERROR')
+    
+    
+@pytest.fixture(params=[(python_3, False),
+                        (python_3, True),
+                        (python_2, False)])
+def proc(request, spawnu, TIMEOUT):
+    container, instant_mode = request.param
+    proc = spawnu(*container)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(init_zshrc.format(
+        u'--enable-experimental-instant-mode' if instant_mode else ''))
+    proc.sendline(u'zsh')
+    if instant_mode:
+        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
+    return proc
+    
+    
+@pytest.mark.parametrize('command, packages, which', [
+    (Command('a_bad_cmd', 'a_bad_cmd: command not found'),
+     [], None),
+    (Command('vim', ''), [], None),
+    (Command('', ''), [], None),
+    (Command('vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim'),
+    (Command('sudo vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim')])
+def test_not_match(mocker, command, packages, which):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=which)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+    
+@pytest.mark.parametrize('before, after', [
+    ('brew install sshfs',
+     'brew cask install osxfuse && brew install sshfs')])
+def test_get_new_command(before, after):
+    command = Command(before, output)
+    assert get_new_command(command) == after
 
     
-    '''
-*What is this pattern about?
-This patterns aims to reduce the number of classes required by an
-application. Instead of relying on subclasses it creates objects by
-copying a prototypical instance at run-time.
-    
-        def now(self):
-        current_time_is_always_midnight = '24:01'
-        return current_time_is_always_midnight
+        if args.show_plot:
+        plt.plot(*zip(*timings))
+        plt.title('Average time taken running isotonic regression')
+        plt.xlabel('Number of observations')
+        plt.ylabel('Time (s)')
+        plt.axis('tight')
+        plt.loglog()
+        plt.show()
 
     
+        n_samples = 2000
+    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
+    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
+                                           list_n_features, precompute=False)
+    plt.subplot(212)
+    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
+    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
+    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
+    plt.legend(loc='upper left')
+    plt.xlabel('number of features')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
     
-class Scientist(AbstractExpert):
-    @property
-    def is_eager_to_contribute(self):
-        return random.randint(0, 1)
+                gc.collect()
+            print('benchmarking lasso_path (without Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            lasso_path(X, y, precompute=False)
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            results['lasso_path (without Gram)'].append(delta)
+    
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
+    
+            for iteration in xrange(opts.n_times):
+            print('\titer %s...' % iteration, end='')
+            time_to_fit, time_to_transform = bench_scikit_transformer(X_dense,
+              transformers[name])
+            time_fit[name].append(time_to_fit)
+            time_transform[name].append(time_to_transform)
+            print('done')
+    
+        This is called by sphinx.ext.linkcode
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    
+def main(path):
