d8ff4cdca0f0ad66e1f40aa6e1d6cdfba7f759e7
SUNsung <weatherius@gmail.com>
2018-10-04 06:21:20 +0300

    Rewrite [25ZVjDwQNCvWc25AMEE8mRYzTU252mvrzMw7eVI250.cpp] File in: 2018-10-04 06:21:19

--- cccccc/25ZVjDwQNCvWc25AMEE8mRYzTU252mvrzMw7eVI250.cpp
+++ cccccc/25ZVjDwQNCvWc25AMEE8mRYzTU252mvrzMw7eVI250.cpp
@@ -1,184 +1,320 @@
 
         
-        // A macro for testing Google Test assertions or code that's expected to
-// generate Google Test non-fatal failures.  It asserts that the given
-// statement will cause exactly one non-fatal Google Test failure with 'substr'
-// being part of the failure message.
-//
-// There are two different versions of this macro. EXPECT_NONFATAL_FAILURE only
-// affects and considers failures generated in the current thread and
-// EXPECT_NONFATAL_FAILURE_ON_ALL_THREADS does the same but for all threads.
-//
-// 'statement' is allowed to reference local variables and members of
-// the current object.
-//
-// The verification of the assertion is done correctly even when the statement
-// throws an exception or aborts the current function.
-//
-// Known restrictions:
-//   - You cannot stream a failure message to this macro.
-//
-// Note that even though the implementations of the following two
-// macros are much alike, we cannot refactor them to use a common
-// helper macro, due to some peculiarity in how the preprocessor
-// works.  If we do that, the code won't compile when the user gives
-// EXPECT_NONFATAL_FAILURE() a statement that contains a macro that
-// expands to code containing an unprotected comma.  The
-// AcceptsMacroThatExpandsToUnprotectedComma test in gtest_unittest.cc
-// catches that.
-//
-// For the same reason, we have to write
-//   if (::testing::internal::AlwaysTrue()) { statement; }
-// instead of
-//   GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement)
-// to avoid an MSVC warning on unreachable code.
-#define EXPECT_NONFATAL_FAILURE(statement, substr) \
-  do {\
-    ::testing::TestPartResultArray gtest_failures;\
-    ::testing::internal::SingleFailureChecker gtest_checker(\
-        &gtest_failures, ::testing::TestPartResult::kNonFatalFailure, \
-        (substr));\
-    {\
-      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
-          ::testing::ScopedFakeTestPartResultReporter:: \
-          INTERCEPT_ONLY_CURRENT_THREAD, &gtest_failures);\
-      if (::testing::internal::AlwaysTrue()) { statement; }\
-    }\
-  } while (::testing::internal::AlwaysFalse())
-    
-    // Internal macro for implementing {EXPECT|ASSERT}_PRED_FORMAT2.
-// Don't use this in your code.
-#define GTEST_PRED_FORMAT2_(pred_format, v1, v2, on_failure)\
-  GTEST_ASSERT_(pred_format(#v1, #v2, v1, v2), \
-                on_failure)
-    
-    // Names of the flags (needed for parsing Google Test flags).
-const char kDeathTestStyleFlag[] = 'death_test_style';
-const char kDeathTestUseFork[] = 'death_test_use_fork';
-const char kInternalRunDeathTestFlag[] = 'internal_run_death_test';
-    
-      explicit FilePath(const std::string& pathname) : pathname_(pathname) {
-    Normalize();
-  }
-    
-    APCHandle::Pair APCCollection::WrapArray(APCHandle::Pair inner,
-                                         CollectionType colType) {
-  auto const col = new APCCollection;
-  col->m_arrayHandle = inner.handle;
-  col->m_colType = colType;
-  return { &col->m_handle, inner.size + sizeof(APCCollection) };
-}
-    
-    public:
-  virtual ~IDebuggable() {}
-    
-    struct FatalErrorException : ExtendedException {
-  explicit FatalErrorException(const char *msg)
-    : ExtendedException('%s', msg)
-  {}
-  FatalErrorException(int, ATTRIBUTE_PRINTF_STRING const char *msg, ...)
-    ATTRIBUTE_PRINTF(3,4);
-  FatalErrorException(const std::string&, const Array& backtrace,
-                      bool isRecoverable = false);
-    }
-    
-    inline int ExecutionContext::getPageletTasksStarted() const {
-  return m_pageletTasksStarted;
-}
-    
-    
-    {}
-    
-    template<>
-void SetDataGradToBlob<mshadow::gpu, float>(caffeMemoryTypes memType,
-                            std::vector<::caffe::Blob<float>*>::iterator blob,
-                            std::vector<TBlob>::const_iterator itr) {
-  float *data_ptr = reinterpret_cast<float*>((*itr).dptr_);
-  if (memType == Data)
-    (*blob)->set_gpu_data(data_ptr);
-  else
-    MXCAFFEBLOB(*blob, float)->set_gpu_diff(data_ptr);
-}
-    
-    #endif  // PLUGIN_CAFFE_CAFFE_BLOB_H_
-
-    
-    #include <cstddef>
-#include <cstdlib>
-#include <sstream>
-#include <limits>
-#include <map>
-#include <set>
-#include <typeinfo>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <utility>
-    
-    MXNET_REGISTER_OP_PROPERTY(CaffeOp, CaffeOpProp)
-.describe('Apply caffe operator')
-.add_argument('data', 'Symbol[]', 'List of tensors')
-.add_arguments(CaffeOpParam::__FIELDS__());
-    
-    template <std::size_t kNumGpus, std::size_t kStreams>
-void StreamManager<kNumGpus, kStreams>::Finalize() {
-#if MXNET_USE_CUDA
-  for (std::size_t i = 0; i < kNumGpus; ++i) {
-    if (gpu_cnt_.at(i) != -1) {
-      for (auto&& j : gpu_streams_.at(i)) {
-        // Catch exception for CUDA driver shutdown
-        MSHADOW_CATCH_ERROR(mshadow::DeleteStream<gpu>(j));
-      }
-      gpu_cnt_.at(i) = -1;
-    }
-  }
-#endif  // MXNET_USE_CUDA
-}
-    
-    namespace mxnet {
-namespace engine {
-/*!
- * \brief ThreadedEngine using global thread pool across all devices.
- * The policy of this Engine:
- *  - Execute Async operation immediately if pushed from Pusher.
- *  - Use a common thread pool for normal operations on all devices.
- *  - Use special thread pool for copy operations.
- */
-class ThreadedEnginePooled : public ThreadedEngine {
- public:
-  ThreadedEnginePooled() {
-    this->Start();
+        PlatformKind swift::targetPlatform(LangOptions &LangOpts) {
+  if (LangOpts.Target.isMacOSX()) {
+    return (LangOpts.EnableAppExtensionRestrictions
+                ? PlatformKind::OSXApplicationExtension
+                : PlatformKind::OSX);
   }
     }
-    }
-    }
     
-    namespace mxnet {
-namespace io {
-/*! \return the parameter of default augmenter */
-std::vector<dmlc::ParamFieldInfo> ListDefaultAugParams();
-std::vector<dmlc::ParamFieldInfo> ListDefaultDetAugParams();
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_IO_IMAGE_AUGMENTER_H_
-
-    
-    bool get_png_size(const uint8_t* data, uint32_t data_size, int64_t *width, int64_t *height) {
-  if (data[0] == 0x89 && data[1] == 0x50 && data[2] ==0x4E && data[3] == 0x47) {
-    uint8_t const* p = data + 16;
-    *width = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
-    p += 4;
-    *height = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
-    return true;
-  } else {
+    /// When splitting based on a preposition, whether we should place the
+/// preposition on the argument label (vs. on the base name).
+static bool shouldPlacePrepositionOnArgLabel(StringRef beforePreposition,
+                                             StringRef preposition,
+                                             StringRef afterPreposition) {
+  // X/Y/Z often used as coordinates and should be the labels.
+  if (afterPreposition == 'X' ||
+      afterPreposition == 'Y' ||
+      afterPreposition == 'Z')
     return false;
+    }
+    
+    DummyTaskQueue::DummyTaskQueue(unsigned NumberOfParallelTasks)
+  : TaskQueue(NumberOfParallelTasks) {}
+    
+    
+    {  // The length of the transcoded string in UTF-16 code points.
+  Length = toPtr - &buffer[0];
+  return Length;
+}
+
+    
+    from GYBUnicodeDataUtils import GraphemeClusterBreakPropertyTable, get_extended_grapheme_cluster_rules_matrix
+    
+    namespace swift {
+namespace importer {
+    }
+    }
+    
+    SourceLoc ClangDiagnosticConsumer::resolveSourceLocation(
+    const clang::SourceManager &clangSrcMgr,
+    clang::SourceLocation clangLoc) {
+  SourceManager &swiftSrcMgr = ImporterImpl.SwiftContext.SourceMgr;
+  SourceLoc loc;
+    }
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26, typename T27, typename T28, typename T29, typename T30,
+    typename T31, typename T32, typename T33, typename T34, typename T35,
+    typename T36, typename T37, typename T38, typename T39, typename T40,
+    typename T41>
+internal::ValueArray41<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
+    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
+    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40,
+    T41> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+    T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
+    T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
+    T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
+    T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41) {
+  return internal::ValueArray41<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
+      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39,
+      T40, T41>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14,
+      v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28,
+      v29, v30, v31, v32, v33, v34, v35, v36, v37, v38, v39, v40, v41);
+}
+    
+    // Finally, you are free to instantiate the pattern with the types you
+// want.  If you put the above code in a header file, you can #include
+// it in multiple C++ source files and instantiate it multiple times.
+//
+// To distinguish different instances of the pattern, the first
+// argument to the INSTANTIATE_* macro is a prefix that will be added
+// to the actual test case name.  Remember to pick unique prefixes for
+// different instances.
+typedef testing::Types<char, int, unsigned int> MyTypes;
+INSTANTIATE_TYPED_TEST_CASE_P(My, FooTest, MyTypes);
+    
+    
+    {
+    {}  // namespace internal
+}  // namespace testing
+    
+    
+    {    // You can generate a failure in any event handler except
+    // OnTestPartResult. Just use an appropriate Google Test assertion to do
+    // it.
+    EXPECT_LE(difference, 0) << 'Leaked ' << difference << ' unit(s) of Water!';
+  }
+    
+    namespace grpc {
+    }
+    
+    
+    {  if (op->send_message() != nullptr) {
+    ++sent_message_count_;
+  }
+  if (op->recv_message() != nullptr) {
+    recv_message_ = op->op()->payload->recv_message.recv_message;
+    initial_on_done_recv_message_ =
+        op->op()->payload->recv_message.recv_message_ready;
+    op->op()->payload->recv_message.recv_message_ready = &on_done_recv_message_;
+  }
+  if (op->recv_trailing_metadata() != nullptr) {
+    recv_trailing_metadata_ = op->recv_trailing_metadata()->batch();
+    initial_on_done_recv_trailing_metadata_ =
+        op->op()->payload->recv_trailing_metadata.recv_trailing_metadata_ready;
+    op->op()->payload->recv_trailing_metadata.recv_trailing_metadata_ready =
+        &on_done_recv_trailing_metadata_;
+  }
+  // Call next op.
+  grpc_call_next_op(elem, op->op());
+}
+    
+    size_t ServerStatsDeserialize(const char* buf, size_t buf_size,
+                              uint64_t* server_elapsed_time) {
+  return RpcServerStatsEncoding::Decode(absl::string_view(buf, buf_size),
+                                        server_elapsed_time);
+}
+    
+    bool ProtoServerReflectionPlugin::has_async_methods() const {
+  if (reflection_service_) {
+    return reflection_service_->has_async_methods();
+  }
+  return false;
+}
+    
+    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
+    const grpc::string& name, int value) {
+  class IntOption final : public ServerBuilderOption {
+   public:
+    IntOption(const grpc::string& name, int value)
+        : name_(name), value_(value) {}
+    }
+    }
+    
+    ThreadPoolInterface* CreateDefaultThreadPoolImpl() {
+  int cores = gpr_cpu_num_cores();
+  if (!cores) cores = 4;
+  return new DynamicThreadPool(cores);
+}
+    
+    template <class charT, class traits>
+bool basic_regex_parser<charT, traits>::parse_match_any()
+{
+   //
+   // we have a '.' that can match any character:
+   //
+   ++m_position;
+   static_cast<re_dot*>(
+      this->append_state(syntax_element_wild, sizeof(re_dot))
+      )->mask = static_cast<unsigned char>(this->flags() & regbase::no_mod_s 
+      ? BOOST_REGEX_DETAIL_NS::force_not_newline 
+         : this->flags() & regbase::mod_s ?
+            BOOST_REGEX_DETAIL_NS::force_newline : BOOST_REGEX_DETAIL_NS::dont_care);
+   return true;
+}
+    
+     /*
+  *   LOCATION:    see http://www.boost.org for most recent version.
+  *   FILE         mem_block_cache.hpp
+  *   VERSION      see <boost/version.hpp>
+  *   DESCRIPTION: memory block cache used by the non-recursive matcher.
+  */
+    
+    class BOOST_REGEX_DECL abstract_protected_call
+{
+public:
+   bool BOOST_REGEX_CALL execute()const;
+   // this stops gcc-4 from complaining:
+   virtual ~abstract_protected_call(){}
+private:
+   virtual bool call()const = 0;
+};
+    
+    #ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable: 4103)
+#endif
+#ifdef BOOST_HAS_ABI_HEADERS
+#  include BOOST_ABI_PREFIX
+#endif
+#ifdef BOOST_MSVC
+#pragma warning(pop)
+#endif
+    
+    #ifndef BOOST_REGEX_V4_REGEX_REPLACE_HPP
+#define BOOST_REGEX_V4_REGEX_REPLACE_HPP
+    
+    #ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable: 4103)
+#endif
+#ifdef BOOST_HAS_ABI_HEADERS
+#  include BOOST_ABI_SUFFIX
+#endif
+#ifdef BOOST_MSVC
+#pragma warning(pop)
+#endif
+    
+    template <class BidirectionalIterator,
+          class charT,
+          class traits>
+class regex_token_iterator_implementation 
+{
+   typedef basic_regex<charT, traits> regex_type;
+   typedef sub_match<BidirectionalIterator>      value_type;
+    }
+    
+    /*! \brief small eps gap for minimum split decision. */
+const bst_float kRtEps = 1e-6f;
+    
+      virtual void PredictBatch(DMatrix* dmat, HostDeviceVector<bst_float>* out_preds,
+                            const gbm::GBTreeModel& model, int tree_begin,
+                            unsigned ntree_limit = 0) = 0;
+    
+    void DHTReplaceNodeTask::onTimeout(const std::shared_ptr<DHTNode>& node)
+{
+  ++numRetry_;
+  if (numRetry_ >= MAX_RETRY) {
+    A2_LOG_INFO(fmt('ReplaceNode: Ping failed %d times. Replace %s with %s.',
+                    numRetry_, node->toString().c_str(),
+                    newNode_->toString().c_str()));
+    node->markBad();
+    bucket_->addNode(newNode_);
+    setFinished(true);
+  }
+  else {
+    A2_LOG_INFO(fmt('ReplaceNode: Ping reply timeout from %s. Try once more.',
+                    node->toString().c_str()));
+    sendMessage();
   }
 }
     
-    #ifndef GUETZLI_COMPARATOR_H_
-#define GUETZLI_COMPARATOR_H_
+      std::vector<std::shared_ptr<DHTNode>> nodes;
+  // nodes
+  const int compactlen = bittorrent::getCompactLength(family_);
+  for (size_t i = 0; i < numNodes; ++i) {
+    // 1byte compact peer info length
+    uint8_t peerInfoLen;
+    READ_CHECK(fp, &peerInfoLen, sizeof(peerInfoLen));
+    if (peerInfoLen != compactlen) {
+      // skip this entry
+      readBytes(fp, buf, buf.size(), 7 + 48);
+      continue;
+    }
+    // 7bytes reserved
+    readBytes(fp, buf, buf.size(), 7);
+    // compactlen bytes compact peer info
+    readBytes(fp, buf, buf.size(), compactlen);
+    if (memcmp(zero, buf, compactlen) == 0) {
+      // skip this entry
+      readBytes(fp, buf, buf.size(), 48 - compactlen);
+      continue;
+    }
+    std::pair<std::string, uint16_t> peer =
+        bittorrent::unpackcompact(buf, family_);
+    if (peer.first.empty()) {
+      // skip this entry
+      readBytes(fp, buf, buf.size(), 48 - compactlen);
+      continue;
+    }
+    // 24-compactlen bytes reserved
+    readBytes(fp, buf, buf.size(), 24 - compactlen);
+    // node ID
+    readBytes(fp, buf, buf.size(), DHT_ID_LENGTH);
+    }
     
-    #endif  // GUETZLI_IDCT_H_
-
+    DHTSetup::~DHTSetup() = default;
     
-    #include 'guetzli/jpeg_data_decoder.h'
+    
+    {} // namespace aria2
+    
+      unsigned char secret_[2][SECRET_SIZE];
+    
+      /**
+   * @brief Create a pointer to a specified brand of CAN client. The brand is
+   *        set in the parameter.
+   * @param parameter The parameter to create the CAN client.
+   * @return A pointer to the created CAN client.
+   */
+  std::unique_ptr<CanClient> CreateCANClient(const CANCardParameter &parameter);
+    
+    #include 'gtest/gtest.h'
+    
+    
+    {
+    {
+    {
+    {}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+    
+    #include 'modules/common/log.h'
+#include 'modules/common/time/time.h'
+#include 'modules/drivers/canbus/common/byte.h'
+    
+    #include <cmath>
+#include <numeric>
+    
+    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    
+    /**
+ * @class Byte
+ * @brief The class of one byte, which is 8 bits.
+ *        It includes some operations on one byte.
+ */
+class Byte {
+ public:
+  /**
+   * @brief Constructor which takes a pointer to a one-byte unsigned integer.
+   * @param value The pointer to a one-byte unsigned integer for construction.
+   */
+  explicit Byte(const uint8_t *value);
+    }
