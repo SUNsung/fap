d525a26f34f740b9e6f0e6838d7ead2980f7b930
SUNsung <weatherius@gmail.com>
2018-10-05 21:18:31 +0300

    Rewrite [58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py] File in: 2018-10-05 21:18:30

--- pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
+++ pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
@@ -1,122 +1,17 @@
 
         
-          def __init__(self, master, component_spec):
-    self.master = master
-    self.spec = component_spec
-    self.name = component_spec.name
-    self.network = MockNetwork()
-    self.beam_size = 1
-    self.num_actions = 45
-    self._attrs = {}
+        from httpie.utils import repr_dict_nice
     
-        Raises:
-      RuntimeError: if fixed features are configured.
-    '''
-    logging.info('Building component: %s', self.spec.name)
-    if self.spec.fixed_feature:
-      raise RuntimeError(
-          'Fixed features are not compatible with bulk annotation. '
-          'Use the 'bulk-features' component instead.')
-    linked_embeddings = [
-        fetch_linked_embedding(self, network_states, spec)
-        for spec in self.spec.linked_feature
-    ]
+        if resp:
+        output.append(Stream(
+            msg=HTTPResponse(response),
+            with_headers=resp_h,
+            with_body=resp_b))
     
-      Args:
-    root: [S] vector of activations for the artificial root token.
-    tokens: [B,N,T] tensor of batched activations for root tokens.
-    weights_arc: [S,T] matrix of weights.
-    weights_source: [S] vector of weights.
+    from httpie.plugins.base import AuthPlugin
     
-      def testArcSourcePotentialsFromTokens(self):
-    with self.test_session():
-      tokens = tf.constant([[[4, 5, 6],
-                             [5, 6, 7],
-                             [6, 7, 8]],
-                            [[6, 7, 8],
-                             [5, 6, 7],
-                             [4, 5, 6]]], tf.float32)  # pyformat: disable
-      weights = tf.constant([2, 3, 5], tf.float32)
-    
-    flags.DEFINE_string('master_spec', None, 'Path to task context with '
-                    'inputs and parameters for feature extractors.')
-flags.DEFINE_string('params_path', None, 'Path to trained model parameters.')
-flags.DEFINE_string('export_path', '', 'Output path for exported servo model.')
-flags.DEFINE_bool('export_moving_averages', False,
-                  'Whether to export the moving average parameters.')
-flags.DEFINE_bool('build_runtime_graph', False,
-                  'Whether to build a graph for use by the runtime.')
-    
-            expected_sentences = [expected[i] for i in [0, 0, 1, 0]]
-    
-    
-class Environment(BaseEnvironment):
-    '''Works like a regular Jinja2 environment but has some additional
-    knowledge of how Flask's blueprint works so that it can prepend the
-    name of the blueprint to referenced templates if necessary.
-    '''
-    
-                for key in http_method_funcs:
-                if hasattr(cls, key):
-                    methods.add(key.upper())
-    
-    
-def test_main_module_paths(modules_tmpdir, purge_module):
-    app = modules_tmpdir.join('main_app.py')
-    app.write('import flask\n\napp = flask.Flask('__main__')')
-    purge_module('main_app')
-    
-    from flask._compat import StringIO
-    
-        rv = client.get('/')
-    assert rv.data == b'dcba'
-    
-    
-def patharg(path):
-    '''
-    Back slashes need to be escaped in ITEM args,
-    even in Windows paths.
-    
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
-    
-    
-def test_follow_all_output_options_used_for_redirects(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 3
-    assert HTTP_OK not in r
-    
-    
-def test_unicode_url_query_arg_item_verbose(httpbin):
-    r = http('--verbose', httpbin.url + '/get', u'test==%s' % UNICODE)
-    assert HTTP_OK in r
-    assert UNICODE in r
-    
-        # Used only when requested with --check-status:
-    ERROR_HTTP_3XX = 3
-    ERROR_HTTP_4XX = 4
-    ERROR_HTTP_5XX = 5
-    
-    
-def test_credentials_in_url_auth_flag_has_priority(httpbin_both):
-    '''When credentials are passed in URL and via -a at the same time,
-     then the ones from -a are used.'''
-    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
-                   auth='user:wrong')
-    r = http('--auth=user:password', 'GET', url)
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
+        def __iter__(self):
+        return iter(self._plugins)
     
         def test_binary_file_form(self, httpbin):
         env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
@@ -124,51 +19,185 @@ def test_credentials_in_url_auth_flag_has_priority(httpbin_both):
                  'test@' + BIN_FILE_PATH_ARG, env=env)
         assert bytes(BIN_FILE_CONTENT) in bytes(r)
     
-        def test_filter_gcp_fields(self):
-        input_data = {
-            u'kind': u'compute#httpsHealthCheck',
-            u'description': u'',
-            u'timeoutSec': 5,
-            u'checkIntervalSec': 5,
-            u'port': 443,
-            u'healthyThreshold': 2,
-            u'host': u'',
-            u'requestPath': u'/',
-            u'unhealthyThreshold': 2,
-            u'creationTimestamp': u'2017-05-16T15:09:36.546-07:00',
-            u'id': u'8727093129334146639',
-            u'selfLink': u'https://www.googleapis.com/compute/v1/projects/myproject/global/httpsHealthChecks/myhealthcheck',
-            u'name': u'myhealthcheck'}
     
-    from ansible.module_utils._text import to_bytes
-from ansible.parsing.yaml.objects import AnsibleMapping, AnsibleSequence, AnsibleUnicode
-from ansible.parsing.yaml.objects import AnsibleVaultEncryptedUnicode
-from ansible.utils.unsafe_proxy import wrap_var
-from ansible.parsing.vault import VaultLib
-    
-            raise NotImplementedError
+def test_version():
+    r = http('--version', error_exit_ok=True)
+    assert r.exit_status == httpie.ExitStatus.OK
+    # FIXME: py3 has version in stdout, py2 in stderr
+    assert httpie.__version__ == r.stderr.strip() + r.strip()
     
     
-    def getCharPositionInLine(self):
-        '''@brief Get the column of the tokens first character,
-        
-        Columns are numbered 0..n-1
-        
-        Using setter/getter methods is deprecated. Use o.charPositionInLine instead.'''
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
+    assert HTTP_OK in r
     
-            # Self test
-        self.assertTrue(self.addr.conflicts(self.addr))
-        self.assertTrue(self.addr1.conflicts(self.addr1))
-        # This is a tricky one...
-        self.assertTrue(self.addr1.conflicts(self.addr2))
+    apt-get is a simple command line interface for downloading and
+installing packages. The most frequently used commands are update
+and install.
     
-        @mock.patch('certbot_compatibility_test.validator.requests.get')
-    def test_succesful_redirect(self, mock_get_request):
-        mock_get_request.return_value = create_response(
-            301, {'location': 'https://test.com'})
-        self.assertTrue(self.validator.redirect('test.com'))
     
-        @mock.patch('certbot.display.enhancements.util')
-    def test_redirect(self, mock_util):
-        mock_util().menu.return_value = (display_util.OK, 1)
-        self.assertTrue(self._call('redirect'))
+@pytest.mark.parametrize('command', [
+    Command('apt list --upgradable', no_match_output),
+    Command('sudo apt list --upgradable', no_match_output)
+])
+def test_not_match(command):
+    assert not match(command)
+    
+    
+no_such_subcommand_old = '''No such subcommand
+    
+        def test_from_json_hashable(self):
+        hash(self.MockConstant.from_json('a'))
+    
+    
+def load_comparable_csr(*names):
+    '''Load ComparableX509 certificate request.'''
+    return jose.ComparableX509(load_csr(*names))
+    
+    latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
+    }
+    
+    REWRITE_HTTPS_ARGS = [
+    '^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[L,NE,R=permanent]']
+'''Apache version<2.3.9 rewrite rule arguments used for redirections to
+https vhost'''
+    
+            self.assertFalse(self.addr_defined.conflicts(self.addr1))
+        self.assertFalse(self.addr_defined.conflicts(self.addr2))
+        self.assertFalse(self.addr_defined.conflicts(self.addr))
+        self.assertFalse(self.addr_defined.conflicts(self.addr_default))
+    
+        title = r1(r'data-title='([^']+)'', html)
+    title = unescape_html(title)
+    title = escape_file_path(title)
+    assert title
+    if match1(url, r'_(\d+)$'): # current P
+        title = title + ' ' + r1(r'active'>([^<]*)', html)
+    
+                if stream_id not in self.streams:
+                log.e('[Error] Invalid video format.')
+                log.e('Run \'-i\' command with no specific video format to view all available formats.')
+                exit(2)
+        else:
+            # Extract stream with the best quality
+            stream_id = self.streams_sorted[0]['id']
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
+    
+        print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([video_url], title, ext, size, output_dir, merge=merge, headers = fake_headers)
+    
+    
+@unittest.skipUnless(hasattr(signal, 'setitimer'), 'requires setitimer()')
+# bpo-30320: Need pthread_sigmask() to block the signal, otherwise the test
+# is vulnerable to a race condition between the child and the parent processes.
+@unittest.skipUnless(hasattr(signal, 'pthread_sigmask'),
+                     'need signal.pthread_sigmask()')
+class SignalEINTRTest(EINTRBaseTest):
+    ''' EINTR tests for the signal module. '''
+    
+        def __init__(self, fileobj, mode, zipinfo, decrypter=None,
+                 close_fileobj=False):
+        self._fileobj = fileobj
+        self._decrypter = decrypter
+        self._close_fileobj = close_fileobj
+    
+        @unittest.skipUnless(support.FS_NONASCII, 'need support.FS_NONASCII')
+    def test_non_ascii(self):
+        # Test handling of non-ascii data
+        command = ('assert(ord(%r) == %s)'
+                   % (support.FS_NONASCII, ord(support.FS_NONASCII)))
+        assert_python_ok('-c', command)
+    
+            'isolated': 0,
+        'site_import': 1,
+        'bytes_warning': 0,
+        'inspect': 0,
+        'interactive': 0,
+        'optimization_level': 0,
+        'parser_debug': 0,
+        'write_bytecode': 1,
+        'verbose': 0,
+        'quiet': 0,
+        'user_site_directory': 1,
+        'buffered_stdio': 1,
+    
+    This module also defines an exception 'error'.
+    
+    
+class PyDecoratorMethod(PyDecoratorMixin, PyClassmember):
+    def run(self):
+        self.name = 'py:method'
+        return PyClassmember.run(self)
+    
+    if __name__ == '__main__':
+    unittest.main()
+
+    
+    
+def SendCompleterAvailableRequest( filetypes ):
+  request = CompleterAvailableRequest( filetypes )
+  # This is a blocking call.
+  request.Start()
+  return request.Response()
+
+    
+    
+class OmniCompletionRequest( CompletionRequest ):
+  def __init__( self, omni_completer, request_data ):
+    super( OmniCompletionRequest, self ).__init__( request_data )
+    self._omni_completer = omni_completer
+    
+    
+  def Start( self ):
+    self.PostDataToHandler( {},
+                            'shutdown',
+                            TIMEOUT_SECONDS,
+                            display_message = False )
+    
+      # Ignore 'contained' argument in first position.
+  if words[ 0 ] == 'contained':
+    words = words[ 1: ]
+    
+    
+def PathToTestFile( *args ):
+  dir_of_current_script = os.path.dirname( os.path.abspath( __file__ ) )
+  return os.path.join( dir_of_current_script, 'testdata', *args )
+    
+    
+def FormatDebugInfoResponse_NoExtraConf_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  response[ 'extra_conf' ].update( {
+    'is_loaded': False,
+    'path': None
+  } )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'No extra configuration file found\n'
+    )
+  )
+    
+      eq_( request.Response(), {
+    'completions': results,
+    'completion_start_column': 1
+  } )
+    
+    
+def ExtractKeywordsFromGroup_KeywordWithoutNextgroup_test():
+  assert_that( syntax_parse._ExtractKeywordsFromGroup(
+                 syntax_parse.SyntaxGroup( '', [
+                   'skipempty foo bar',
+                   'zoo goo',
+                 ] ) ),
+               contains_inanyorder( 'skipempty', 'foo', 'bar', 'zoo', 'goo' ) )
