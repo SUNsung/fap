7f10f31937f5032f61b297c1d642afa6af43973b
SUNsung <weatherius@gmail.com>
2018-10-05 07:25:04 +0300

    Rewrite [167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb] File in: 2018-10-05 07:25:03

--- ruby/167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb
+++ ruby/167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb
@@ -1,127 +1,96 @@
 
         
-          p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
-    
-            # Get the first part of the email address (before @)
-        # In addtion in removes illegal characters
-        def generate_username(email)
-          email.match(/^[^@]*/)[0].mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/, '').to_s
+                # Mounts a shared folder.
+        #
+        # This method should create, mount, and properly set permissions
+        # on the shared folder. This method should also properly
+        # adhere to any configuration values such as `shared_folder_uid`
+        # on `config.vm`.
+        #
+        # @param [String] name The name of the shared folder.
+        # @param [String] guestpath The path on the machine which the user
+        #   wants the folder mounted.
+        # @param [Hash] options Additional options for the shared folder
+        #   which can be honored.
+        def mount_shared_folder(name, guestpath, options)
+          raise BaseError, _key: :unsupported_shared_folder
         end
     
-            include ::EachBatch
+            # Registers a callback to be called when a specific action sequence
+        # is run. This allows plugin authors to hook into things like VM
+        # bootup, VM provisioning, etc.
+        #
+        # @param [Symbol] name Name of the action.
+        # @return [Array] List of the hooks for the given action.
+        def self.action_hook(name, &block)
+          # Get the list of hooks for the given hook name
+          data[:action_hooks] ||= {}
+          hooks = data[:action_hooks][name.to_sym] ||= []
+    
+            # Execute a command on the remote machine. The exact semantics
+        # of this method are up to the implementor, but in general the
+        # users of this class will expect this to be a shell.
+        #
+        # This method gives you no way to write data back to the remote
+        # machine, so only execute commands that don't expect input.
+        #
+        # @param [String] command Command to execute.
+        # @yield [type, data] Realtime output of the command being executed.
+        # @yieldparam [String] type Type of the output. This can be
+        #   `:stdout`, `:stderr`, etc. The exact types are up to the
+        #   implementor.
+        # @yieldparam [String] data Data for the given output.
+        # @return [Integer] Exit code of the command.
+        def execute(command, opts=nil)
+        end
+    
+            # This contains all the synced folder implementations by name.
+        #
+        # @return [Registry<Symbol, Array<Class, Integer>>]
+        attr_reader :synced_folders
+    
+              # Persist through the set of invalid methods
+          this_invalid  = @__invalid_methods || Set.new
+          other_invalid = other.instance_variable_get(:'@__invalid_methods') || Set.new
+          result.instance_variable_set(:'@__invalid_methods', this_invalid + other_invalid)
+    
+            # This is an internal initialize function that should never be
+        # overridden. It is used to initialize some common internal state
+        # that is used in a provider.
+        def _initialize(name, machine)
+          initialize_capabilities!(
+            name.to_sym,
+            { name.to_sym => [Class.new, nil] },
+            Vagrant.plugin('2').manager.provider_capabilities,
+            machine,
+          )
+        end
       end
-    
-          def to_markdown
-        '[![#{title}](#{image_url})](#{link_url})'
-      end
-    
-      private
-    
-      def process_push_request
-    case hub_mode
-    when 'subscribe'
-      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
-    when 'unsubscribe'
-      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
-    else
-      ['Unknown mode: #{hub_mode}', 422]
     end
   end
+end
+
     
-      def lease_seconds_or_default
-    (params['hub.lease_seconds'] || 1.day).to_i.seconds
+    Then(/^file symlinks are created in the new release$/) do
+  TestApp.linked_files.each do |file|
+    run_vagrant_command(test_symlink_exists(TestApp.current_path.join(file)))
+  end
+end
+    
+    Given(/^a custom task to generate a file$/) do
+  TestApp.copy_task_to_test_app('spec/support/tasks/database.rake')
+end
+    
+      def test_file_exists(path)
+    exists?('f', path)
   end
     
-    # Ensure we know the appservers port
-Capybara.server_port = AppConfig.pod_uri.port
-Rails.application.routes.default_url_options[:host] = AppConfig.pod_uri.host
-Rails.application.routes.default_url_options[:port] = AppConfig.pod_uri.port
-    
-      # delete all cookies, destroying the current session
-  def logout
-    page.driver.clear_cookies
-  end
-    
-      describe '#new' do
-    before do
-      sign_in alice, scope: :user
-    end
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
+            if echo?
+          $stdin.gets
         else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
+          $stdin.noecho(&:gets).tap { $stdout.print '\n' }
         end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
-          end
-        end
-      end
-      rtn
-    end
-  end
-    
-          def children(node)
-        node.children
-      end
-    
-      include_examples 'multiline literal brace layout' do
-    let(:open) { '[' }
-    let(:close) { ']' }
-  end
-    
-      it 'ignores empty hashes' do
-    expect_no_offenses('{}')
-  end
-    
-          # Checks whether any of the key value pairs in the `hash` literal are on
-      # the same line.
-      #
-      # @note A multiline `pair` is considered to be on the same line if it
-      #       shares any of its lines with another `pair`
-      #
-      # @return [Boolean] whether any `pair` nodes are on the same line
-      def pairs_on_same_line?
-        pairs.each_cons(2).any? { |first, second| first.same_line?(second) }
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `kwsplat` nodes. This will be used in place of a
-    # plain  node when the builder constructs the AST, making its methods
-    # available to all `kwsplat` nodes within RuboCop.
-    class KeywordSplatNode < Node
-      include HashElementNode
-    
-          # A shorthand for getting the last argument of the node.
-      # Equivalent to `arguments.last`.
-      #
-      # @return [Node, nil] the last argument of the node,
-      #                     or `nil` if there are no arguments
-      def last_argument
-        arguments[-1]
+      rescue Errno::EIO
+        # when stdio gets closed
+        return
       end
