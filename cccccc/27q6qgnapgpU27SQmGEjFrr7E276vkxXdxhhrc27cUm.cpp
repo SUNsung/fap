b3a2298e78ffd02b4ac56807711a79c6a38baf3d
SUNsung <weatherius@gmail.com>
2018-10-05 04:14:24 +0300

    Rewrite [27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp] File in: 2018-10-05 04:14:23

--- cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
+++ cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
@@ -1,173 +1,231 @@
 
         
-        Optional<PlatformKind> swift::platformFromString(StringRef Name) {
-  if (Name == '*')
-    return PlatformKind::none;
-  return llvm::StringSwitch<Optional<PlatformKind>>(Name)
-#define AVAILABILITY_PLATFORM(X, PrettyName) .Case(#X, PlatformKind::X)
-#include 'swift/AST/PlatformKinds.def'
-      .Case('macOS', PlatformKind::OSX)
-      .Case('macOSApplicationExtension', PlatformKind::OSXApplicationExtension)
-      .Default(Optional<PlatformKind>());
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
+    
+    
+    {  tensorflow::DeviceNameUtils::ParsedName parsed_name;
+  if (!tensorflow::DeviceNameUtils::ParseFullName(node_def.device(),
+                                                  &parsed_name)) {
+    LOG(WARNING) << 'Failed to parse device from node_def: '
+                 << node_def.ShortDebugString();
+    return '';
+  }
+  string class_name = '';
+  tensorflow::FindKernelDef(tensorflow::DeviceType(parsed_name.type.c_str()),
+                            node_def, nullptr /* kernel_def */, &class_name)
+      .IgnoreError();
+  return class_name;
 }
     
-    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
-  int Ret = cache_get_and_retain(static_cast<cache_t*>(Impl),
-                                 const_cast<void*>(Key), Value_out);
-  return Ret == 0;
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+        /** Colorize an icon (given object) with the icon color */
+    QIcon SingleColorIcon(const QIcon& icon) const;
+    
+    #endif // BITCOIN_QT_TRAFFICGRAPHWIDGET_H
+
+    
+    #include <script/script.h>
+    
+    static void CheckSplitTorReplyLine(std::string input, std::string command, std::string args)
+{
+    BOOST_TEST_MESSAGE(std::string('CheckSplitTorReplyLine(') + input + ')');
+    auto ret = SplitTorReplyLine(input);
+    BOOST_CHECK_EQUAL(ret.first, command);
+    BOOST_CHECK_EQUAL(ret.second, args);
 }
     
-      std::size_t capacity = End - Allocated;
-  std::size_t requiredCapacity = capacity + needed;
-  do {
-    capacity = 2 * capacity + 16;
-  } while (capacity < requiredCapacity);
+    #if defined(__GLIBCXX__) && __GLIBCXX__ < 20080201
+//  std::showpos is broken on old libstdc++ as provided with OSX.  See
+//  http://gcc.gnu.org/ml/libstdc++/2007-11/msg00075.html
+#   define TINYFORMAT_OLD_LIBSTDCPLUSPLUS_WORKAROUND
+#endif
     
-    std::pair<bool, bool> LangOptions::setTarget(llvm::Triple triple) {
-  clearAllPlatformConditionValues();
+            // TODO: make these functions friends of NDViewArray and move to Utils?
+        static bool HasNan(const NDArrayViewPtr& value, const char* name);
+        static void Print(const NDArrayViewPtr& value, const char* msg);
+    
+                m_packedData = nullptr;
+            m_packedDataLayout = nullptr;
+            m_isPacked = false;
+    
+    template<class ElemType>
+class ComputationNetworkFromFile : public ComputationNetwork
+{
+public:
+    ComputationNetworkFromFile(const IConfigRecordPtr configp) :
+        ComputationNetwork()
+    {
+        let& config = *configp;
+    }
     }
     
-    // Ternary predicate assertion macros.
-#define EXPECT_PRED_FORMAT3(pred_format, v1, v2, v3) \
-  GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, GTEST_NONFATAL_FAILURE_)
-#define EXPECT_PRED3(pred, v1, v2, v3) \
-  GTEST_PRED3_(pred, v1, v2, v3, GTEST_NONFATAL_FAILURE_)
-#define ASSERT_PRED_FORMAT3(pred_format, v1, v2, v3) \
-  GTEST_PRED_FORMAT3_(pred_format, v1, v2, v3, GTEST_FATAL_FAILURE_)
-#define ASSERT_PRED3(pred, v1, v2, v3) \
-  GTEST_PRED3_(pred, v1, v2, v3, GTEST_FATAL_FAILURE_)
     
-    #if GTEST_HAS_DEATH_TEST
+    {            // reshape Input(2)
+            Input(2)->SetDims(TensorShape(dimsA), false);
+            fprintf(stderr, '\n%ls %ls operation: For legacy compatibility, the sample layout of third input (%ls %ls operation) was patched to [%s] (from [%s])\n',
+                NodeName().c_str(), OperationName().c_str(), Input(2)->NodeName().c_str(), Input(2)->OperationName().c_str(), string(Input(2)->GetSampleLayout()).c_str(), dimsCstring.c_str());
+        }
     
-      template <typename T>
-  operator ParamGenerator<T>() const {
-    const T array[] = {static_cast<T>(v1_), static_cast<T>(v2_),
-        static_cast<T>(v3_), static_cast<T>(v4_), static_cast<T>(v5_),
-        static_cast<T>(v6_), static_cast<T>(v7_), static_cast<T>(v8_),
-        static_cast<T>(v9_), static_cast<T>(v10_), static_cast<T>(v11_),
-        static_cast<T>(v12_), static_cast<T>(v13_), static_cast<T>(v14_),
-        static_cast<T>(v15_), static_cast<T>(v16_), static_cast<T>(v17_),
-        static_cast<T>(v18_), static_cast<T>(v19_), static_cast<T>(v20_),
-        static_cast<T>(v21_), static_cast<T>(v22_), static_cast<T>(v23_)};
-    return ValuesIn(array);
+    typedef enum _match_flags
+{
+   match_default = 0,
+   match_not_bol = 1,                                /* first is not start of line */
+   match_not_eol = match_not_bol << 1,               /* last is not end of line */
+   match_not_bob = match_not_eol << 1,               /* first is not start of buffer */
+   match_not_eob = match_not_bob << 1,               /* last is not end of buffer */
+   match_not_bow = match_not_eob << 1,               /* first is not start of word */
+   match_not_eow = match_not_bow << 1,               /* last is not end of word */
+   match_not_dot_newline = match_not_eow << 1,       /* \n is not matched by '.' */
+   match_not_dot_null = match_not_dot_newline << 1,  /* '\0' is not matched by '.' */
+   match_prev_avail = match_not_dot_null << 1,       /* *--first is a valid expression */
+   match_init = match_prev_avail << 1,               /* internal use */
+   match_any = match_init << 1,                      /* don't care what we match */
+   match_not_null = match_any << 1,                  /* string can't be null */
+   match_continuous = match_not_null << 1,           /* each grep match must continue from */
+                                                     /* uninterupted from the previous one */
+   match_partial = match_continuous << 1,            /* find partial matches */
+   
+   match_stop = match_partial << 1,                  /* stop after first match (grep) V3 only */
+   match_not_initial_null = match_stop,              /* don't match initial null, V4 only */
+   match_all = match_stop << 1,                      /* must find the whole of input even if match_any is set */
+   match_perl = match_all << 1,                      /* Use perl matching rules */
+   match_posix = match_perl << 1,                    /* Use POSIX matching rules */
+   match_nosubs = match_posix << 1,                  /* don't trap marked subs */
+   match_extra = match_nosubs << 1,                  /* include full capture information for repeated captures */
+   match_single_line = match_extra << 1,             /* treat text as single line and ignor any \n's when matching ^ and $. */
+   match_unused1 = match_single_line << 1,           /* unused */
+   match_unused2 = match_unused1 << 1,               /* unused */
+   match_unused3 = match_unused2 << 1,               /* unused */
+   match_max = match_unused3,
+    }
+    
+    #include <new>
+#ifdef BOOST_HAS_THREADS
+#include <boost/regex/pending/static_mutex.hpp>
+#endif
+    
+     /*
+  *   LOCATION:    see http://www.boost.org for most recent version.
+  *   FILE         basic_regex_creator.cpp
+  *   VERSION      see <boost/version.hpp>
+  *   DESCRIPTION: Declares template class basic_regex_creator which fills in
+  *                the data members of a regex_data object.
+  */
+    
+    #ifdef BOOST_REGEX_USE_WIN32_LOCALE
+template <class charT, class implementationT = w32_regex_traits<charT> >
+struct regex_traits;
+#elif defined(BOOST_REGEX_USE_CPP_LOCALE)
+template <class charT, class implementationT = cpp_regex_traits<charT> >
+struct regex_traits;
+#else
+template <class charT, class implementationT = c_regex_traits<charT> >
+struct regex_traits;
+#endif
+    
+    
+    {} // namespace boost
+    
+    
+#ifndef BOOST_REGEX_MATCH_HPP
+#define BOOST_REGEX_MATCH_HPP
+    
+    template <class BidiIterator, class charT, class traits>
+bool regex_search(BidiIterator first, BidiIterator last, 
+                  const basic_regex<charT, traits>& e, 
+                  match_flag_type flags = match_default)
+{
+   if(e.flags() & regex_constants::failbit)
+      return false;
+    }
+    
+    
+
+    
+    void compareBenchmarkResults(const std::string& base, const std::string& test) {
+  printResultComparison(resultsFromFile(base), resultsFromFile(test));
+}
+    
+    static uint32_t arrayTestFunc() {
+  uint32_t v1 = folly::Random::rand32();
+  uint32_t v2 = folly::Random::rand32();
+  uint64_t v3 = v1 + v2;
+  uint32_t a[4] = {v1, v2, v1, v2};
+  FOLLY_SDT(folly, test_static_tracepoint_array, a, v1, v3);
+  return v1 + v2;
+}
+    
+      template <typename ExecutorT = Executor>
+  class KeepAlive {
+   public:
+    KeepAlive() = default;
+    }
+    
+    
+    {} // namespace folly
+
+    
+    
+    {    return FuncAndKeepAlive(std::move(f), this);
   }
     
-    #ifndef GTEST_INCLUDE_GTEST_INTERNAL_GTEST_STRING_H_
-#define GTEST_INCLUDE_GTEST_INTERNAL_GTEST_STRING_H_
+        // Test the upper boundary of conversion to int64_t microseconds
+    using usec_i64 = std::chrono::duration<int64_t, std::micro>;
+    ts.tv_sec = 9223372036854LL;
+    ts.tv_nsec = 775807000;
+    EXPECT_EQ(std::numeric_limits<int64_t>::max(), to<usec_i64>(ts).count());
     
-      GTEST_DECLARE_TUPLE_AS_FRIEND_
-    
-    // Tests the Set method.
-TEST(MyString, Set) {
-  MyString s;
-    }
-    
-    #include <folly/test/DeterministicSchedule.h>
-    
-    
-    {  dynamic d4 = dynamic::object('C', 'C++');
-  auto i4 = convertTo<std::pair<std::string, folly::fbstring>>(d4);
-  EXPECT_EQ(i4.first, 'C');
-  EXPECT_EQ(i4.second, 'C++');
-}
-    
-    // this test makes sure that the coroutine is destroyed properly
-TEST(Expected, CoroutineCleanedUp) {
-  int count_dest = 0;
-  auto r = [&]() -> Expected<int, Err> {
-    SCOPE_EXIT {
-      ++count_dest;
+    /// Wrapper around the makeCompressionCounterHandler() extension point.
+class CompressionCounter {
+ public:
+  CompressionCounter() {}
+  CompressionCounter(
+      folly::io::CodecType codecType,
+      folly::StringPiece codecName,
+      folly::Optional<int> level,
+      CompressionCounterKey key,
+      CompressionCounterType counterType) {
+    initialize_ = [=]() {
+      return makeCompressionCounterHandler(
+          codecType, codecName, level, key, counterType);
     };
-    auto x = co_await Expected<int, Err>(makeUnexpected(Err::badder()));
-    ADD_FAILURE() << 'Should not be resuming';
-    co_return x;
-  }();
-  EXPECT_FALSE(r.hasValue());
-  EXPECT_EQ(1, count_dest);
-}
-    
-    #include <boost/random.hpp>
-    
-    void BENCHFUN(pushBack)(int iters, int initialSize) {
-  BenchmarkSuspender braces;
-  auto const obj = randomObject<VECTOR::value_type>();
-  VECTOR v(initialSize, obj);
-  braces.dismissing([&]() {
-    FOR_EACH_RANGE (i, 0, iters) { v.push_back(obj); }
-  });
-}
-    
-      {
-    Lock lock(f, std::defer_lock);
-    EXPECT_TRUE(lock.try_lock());
+    DCHECK(!initialize_.hasAllocatedMemory());
   }
-    
-    struct Const {
-  void test() const;
-};
-    
-    TEST(CanClientFactoryTest, CreateCanClient) {
-  auto *can_factory = CanClientFactory::instance();
-  EXPECT_TRUE(can_factory != nullptr);
     }
     
-    /**
- * @file
- * @brief Defines the EsdCanClient class which inherits CanClient.
- */
-    
-    #include <cstring>
-    
-      if (_card_port > MAX_CAN_PORT || _card_port < 0) {
-    AERROR << 'can port number [' << _card_port << '] is out of the range [0,'
-           << MAX_CAN_PORT << ']';
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
-  }
-  // open device
-  int32_t ret = bcan_open(_card_port, 0,
-                          5,  // 5ms for rx timeout
-                          5,  // 5ms for tx timeout
-                          &_dev_handler);
-    
     
     {
     {
     {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
+    {} // namespace detail
+} // namespace compression
+} // namespace io
+} // namespace folly
 
     
-    
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-    }
-    }
-    }
-    
-    const char HEX[] = '0123456789ABCDEF';
-    
-      /**
-   * @brief Set the bit on a specified position to zero.
-   * @param pos The position of the bit to be set to zero.
-   */
-  void set_bit_0(const int32_t pos);
-    
-     private:
-  void PublishSensorData();
-  void OnTimer(const ros::TimerEvent &event);
-  void DataTrigger();
-  common::Status OnError(const std::string &error_msg);
-  void RegisterCanClients();
-    
-    // System gflags
-DECLARE_string(sensor_node_name);
+      explicit Options(
+      Format format_ = Format::ZLIB,
+      int windowSize_ = 15,
+      int memLevel_ = 8,
+      int strategy_ = Z_DEFAULT_STRATEGY)
+      : format(format_),
+        windowSize(windowSize_),
+        memLevel(memLevel_),
+        strategy(strategy_) {}
