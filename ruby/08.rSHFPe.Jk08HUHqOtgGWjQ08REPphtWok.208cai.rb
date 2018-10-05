9783fbe1a047f0d91fcec5b9326286819c77794a
SUNsung <weatherius@gmail.com>
2018-10-05 13:47:52 +0300

    Rewrite [08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb] File in: 2018-10-05 13:47:52

--- ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
+++ ruby/08.rSHFPe.Jk08HUHqOtgGWjQ08REPphtWok.208cai.rb
@@ -1,158 +1,125 @@
 
         
-            gu = GroupUser.find_by(user_id: moderator.id, group_id: group.id)
-    expect(gu.notification_level).to eq(NotificationLevels.all[:regular])
-  end
+            To install Clojure you should install Leiningen:
+      brew install leiningen
+    and then follow the tutorial:
+      https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md
+    EOS
+  when 'osmium' then <<-EOS.undent
+    The creator of Osmium requests that it not be packaged and that people
+    use the GitHub master branch instead.
+    EOS
+  when 'gfortran' then <<-EOS.undent
+    GNU Fortran is now provided as part of GCC, and can be installed with:
+      brew install gcc
+    EOS
+  when 'play' then <<-EOS.undent
+    Play 2.3 replaces the play command with activator:
+      brew install typesafe-activator
     
-    Badge.seed do |b|
-  b.id = Badge::FirstFlag
-  b.name = 'First Flag'
-  b.badge_type_id = BadgeType::Bronze
-  b.multiple_grant = false
-  b.target_posts = true
-  b.show_posts = false
-  b.query = BadgeQueries::FirstFlag
-  b.badge_grouping_id = BadgeGrouping::GettingStarted
-  b.default_badge_grouping_id = BadgeGrouping::GettingStarted
-  b.trigger = Badge::Trigger::PostAction
-  b.auto_revoke = false
-  b.system = true
-end
+    module Homebrew
+  module Cleanup
+    @@disk_cleanup_size = 0
     
-      smoke_user = User.seed do |u|
-    u.id = 0
-    u.name = 'smoke_user'
-    u.username = 'smoke_user'
-    u.username_lower = 'smoke_user'
-    u.password = 'P4ssw0rd'
-    u.active = true
-    u.approved = true
-    u.approved_at = Time.now
-    u.trust_level = TrustLevel[3]
-  end.first
-    
-            unless post && post.id
-          puts post.errors.full_messages if post
-          puts creator.errors.inspect
-          raise 'Failed to create description for trust level 3 lounge!'
-        end
-    
-            def value_width
-          54
-        end
-    
-              # Add the help option, which must be on every command.
-          opts.on_tail('-h', '--help', 'Print this help') do
-            safe_puts(opts.help)
-            return nil
-          end
-    
-            # This unregisters a plugin so that its components will no longer
-        # be used. Note that this should only be used for testing purposes.
-        def unregister(plugin)
-          if @registered.include?(plugin)
-            @logger.info('Unregistered: #{plugin.name}')
-            @registered.delete(plugin)
-          end
-        end
+            $stderr.puts
+        opoo out
+        Homebrew.failed = true
+        first_warning = false
       end
     end
-  end
-end
-
     
-            # This is the method called to 'prepare' the provisioner. This is called
-        # before any actions are run by the action runner (see {Vagrant::Actions::Runner}).
-        # This can be used to setup shared folders, forward ports, etc. Whatever is
-        # necessary on a 'meta' level.
-        #
-        # No return value is expected.
-        def prepare
-        end
-    
-                # Use the default provider if nothing else
-            provider_to_use ||= @env.default_provider(machine: name)
-    
-            # Execute a command on the remote machine. The exact semantics
-        # of this method are up to the implementor, but in general the
-        # users of this class will expect this to be a shell.
-        #
-        # This method gives you no way to write data back to the remote
-        # machine, so only execute commands that don't expect input.
-        #
-        # @param [String] command Command to execute.
-        # @yield [type, data] Realtime output of the command being executed.
-        # @yieldparam [String] type Type of the output. This can be
-        #   `:stdout`, `:stderr`, etc. The exact types are up to the
-        #   implementor.
-        # @yieldparam [String] data Data for the given output.
-        # @return [Integer] Exit code of the command.
-        def execute(command, opts=nil)
-        end
-    
-      #
-  # Brings in from an array like yo.
-  #
-  def from_a(ary)
-    ary.each { |e|
-      self[e[0]] = e[1]
-    }
+      def self.path(name)
+    Formulary.core_path(name)
   end
     
+            def username_and_email
+          @username_and_email ||= begin
+            username  = get_info(:username).presence || get_info(:nickname).presence
+            email     = get_info(:email).presence
     
-  #
-  # Payload types were copied from xCAT-server source code (IPMI.pm)
-  #
-  RMCP_ERRORS = {
-    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
-    2 => 'Invalid Session ID', #this shouldn't occur...
-    3 => 'Invalid payload type',#shouldn't occur..
-    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
-    5 => 'Invalid integrity algorithm', #same as above
-    6 => 'No matching authentication payload',
-    7 => 'No matching integrity payload',
-    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
-    9 => 'Invalid role',
-    0xa => 'Unauthorised role or privilege level requested',
-    0xb => 'Insufficient resources to create a session at the requested role',
-    0xc => 'Invalid username length',
-    0xd => 'Unauthorized name',
-    0xe => 'Unauthorized GUID',
-    0xf => 'Invalid integrity check value',
-    0x10 => 'Invalid confidentiality algorithm',
-    0x11 => 'No cipher suite match with proposed security algorithms',
-    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
-  }
+        private
     
-              # Decodes the req_body from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::KdcRequestBody]
-          def decode_asn1_req_body(input)
-            Rex::Proto::Kerberos::Model::KdcRequestBody.decode(input.value[0])
-          end
-        end
-      end
+        def log_status(status)
+      puts bold status
     end
+    
+    # The module that contains everything Sass-related:
+#
+# * {Sass::Engine} is the class used to render Sass/SCSS within Ruby code.
+# * {Sass::Plugin} is interfaces with web frameworks (Rails and Merb in particular).
+# * {Sass::SyntaxError} is raised when Sass encounters an error.
+# * {Sass::CSS} handles conversion of CSS to Sass.
+#
+# Also see the {file:SASS_REFERENCE.md full Sass reference}.
+module Sass
+  class << self
+    # @private
+    attr_accessor :tests_running
   end
+    
+        # Parses the command-line arguments and runs the executable.
+    # This does not handle exceptions or exit the program.
+    #
+    # @see #parse!
+    def parse
+      @opts = OptionParser.new(&method(:set_opts))
+      @opts.parse!(@args)
+    
+          opts.on('-C', '--no-cache', 'Don't cache to sassc files.') do
+        @options[:for_engine][:read_cache] = false
+      end
+    
+    class Profile
+  def pod_bin
+    File.expand_path('../pod', __FILE__)
+  end
+    
+    # It is very likely that we'll need these and as some of those paths will atm
+# result in a I18n deprecation warning, we load those here now so that we can
+# get rid of that warning.
+require 'active_support/core_ext/string/strip'
+require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/array/conversions'
+# TODO: check what this actually does by the time we're going to add support for
+# other locales.
+require 'i18n'
+if I18n.respond_to?(:enforce_available_locales=)
+  I18n.enforce_available_locales = false
+end
+    
+            self.description = <<-DESC
+          Shows the content of the pods cache as a YAML tree output, organized by pod.
+          If `NAME` is given, only the caches for that pod will be included in the output.
+        DESC
+    
+          private
+    
+      require ARGV.shift
+  exit_status = LogStash::Runner.run('bin/logstash', ARGV)
+  exit(exit_status || 0)
 end
 
     
-        # Paths
-    def gem_path
-      @gem_path ||= File.expand_path '..', File.dirname(__FILE__)
+      # We compare the before the update and after the update
+  def display_updated_plugins(previous_gem_specs_map)
+    update_count = 0
+    find_latest_gem_specs.values.each do |spec|
+      name = spec.name.downcase
+      if previous_gem_specs_map.has_key?(name)
+        if spec.version != previous_gem_specs_map[name].version
+          puts('Updated #{spec.name} #{previous_gem_specs_map[name].version.to_s} to #{spec.version.to_s}')
+          update_count += 1
+        end
+      else
+        puts('Installed #{spec.name} #{spec.version.to_s}')
+        update_count += 1
+      end
     end
     
-        @save_to.each { |_, v| FileUtils.mkdir_p(v) }
-    
-      # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-    
-          # Checks whether the `if` node has at least one `elsif` branch. Returns
-      # true if this `if` node itself is an `elsif`.
-      #
-      # @return [Boolean] whether the `if` node has at least one `elsif` branch
-      def elsif_conditional?
-        else_branch && else_branch.if_type? && else_branch.elsif?
+        context 'update a specific plugin' do
+      it 'has executed successfully' do
+        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
+        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
+        expect(logstash).not_to have_installed?(plugin_name, previous_version)
       end
+    end
