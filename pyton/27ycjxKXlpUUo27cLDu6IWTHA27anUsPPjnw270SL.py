b34cbd0fd5449544ce056640923e68f6222a8f37
SUNsung <weatherius@gmail.com>
2018-10-05 23:36:35 +0300

    Rewrite [27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py] File in: 2018-10-05 23:36:35

--- pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
+++ pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
@@ -1,37 +1,52 @@
 
         
-            elif not RESULT and ('info_dict' in test and 'age_limit' in test['info_dict'] and
-                         test['info_dict']['age_limit'] == 18):
-        print('\nPotential false negative: {0}'.format(test['name']))
+        DIR_OF_THIS_SCRIPT = p.dirname( p.abspath( __file__ ) )
+DIR_OF_OLD_LIBS = p.join( DIR_OF_THIS_SCRIPT, 'python' )
     
-    import json
-import sys
-import hashlib
-import os.path
-    
-    import io
-import optparse
-import os.path
-import re
-    
-    class build_lazy_extractors(Command):
-    description = 'Build the extractor lazy loading module'
-    user_options = []
-    
-        def test_timeout(self):
-        self.assertTrue(self.timeout.timeout)
-        self.assertFalse(self.invalid.timeout)
+      return server_conf
     
     
-class Signature(jose.Signature):
-    '''ACME-specific Signature. Uses ACME-specific Header for customer fields.'''
-    __slots__ = jose.Signature._orig_slots  # pylint: disable=no-member
+  def Start( self ):
+    request_data = BuildRequestData()
+    request_data.update( { 'filetypes': self.filetypes } )
+    self._response = self.PostDataToHandler( request_data,
+                                             'semantic_completion_available' )
     
-        This implements the basic functionality of unittest.skipUnless
-    which is only available on Python 2.7+.
     
-        def test_view_config_changes(self):
-        self.config.view_config_changes()
+def FormatDebugInfoResponse( response ):
+  if not response:
+    return 'Server errored, no debug info from server\n'
+  message = _FormatYcmdDebugInfo( response )
+  completer = response[ 'completer' ]
+  if completer:
+    message += _FormatCompleterDebugInfo( completer )
+  return message
     
-            self.vhost2 = VirtualHost(
-            'fp', 'vhp', set([self.addr2]), False, False, 'localhost')
+        self._cached_response = self.HandleFuture( self._response_future,
+                                               truncate_message = True )
+    
+    
+  def _UpdateSigns( self ):
+    signs_to_unplace = vimsupport.GetSignsInBuffer( self._bufnr )
+    
+      post_vim_message.assert_has_exact_calls( [
+    call( 'this is a message', warning=False, truncate=True )
+  ] )
+    
+      eq_( len( results ), len( expected_results ) )
+  for result, expected_result in zip( results, expected_results ):
+    assert_that( result, expected_result )
+
+    
+    
+@patch( 'ycm.vimsupport.GetVariableValue',
+        GetVariableValue_CompleteItemIs( 'Test' ) )
+@patch( 'ycm.vimsupport.ReplaceChunks' )
+def PostCompleteFixIt_ApplyFixIt_PickFirst_test( replace_chunks, *args ):
+  completions = [
+    BuildCompletionFixIt( [ { 'chunks': 'one' } ] ),
+    BuildCompletionFixIt( [ { 'chunks': 'two' } ] ),
+  ]
+  with _SetUpCompleteDone( completions ) as request:
+    request._OnCompleteDone_FixIt()
+    replace_chunks.assert_called_once_with( 'one', silent = True )
