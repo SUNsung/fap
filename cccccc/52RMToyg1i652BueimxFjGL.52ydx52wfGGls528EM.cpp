510d1aa2056d3f67b8f3e56d216ad48f1f27fcbf
SUNsung <weatherius@gmail.com>
2018-10-06 08:47:21 +0300

    Rewrite [52RMToyg1i652BueimxFjGL.52ydx52wfGGls528EM.cpp] File in: 2018-10-06 08:47:21

--- cccccc/52RMToyg1i652BueimxFjGL.52ydx52wfGGls528EM.cpp
+++ cccccc/52RMToyg1i652BueimxFjGL.52ydx52wfGGls528EM.cpp
@@ -1,267 +1,228 @@
 
         
-        
-    {}  // namespace tesseract.
+        namespace grpc {
+    }
     
-    #include <cstdint>      // for int32_t
+    #include 'src/cpp/ext/filters/census/measures.h'
     
-    // Number of outputs held in each register. 8 x 32 bit ints.
-constexpr int kNumOutputsPerRegister = 8;
-// Maximum number of registers that we will use.
-constexpr int kMaxOutputRegisters = 8;
-// Number of inputs in the inputs register.
-constexpr int kNumInputsPerRegister = 32;
-// Number of inputs in each weight group.
-constexpr int kNumInputsPerGroup = 4;
-// Number of groups of inputs to be broadcast.
-constexpr int kNumInputGroups = kNumInputsPerRegister / kNumInputsPerGroup;
+      // Field data sizes in bytes
+  enum FieldSize {
+    kTraceIdSize = 16,
+    kSpanIdSize = 8,
+    kTraceOptionsSize = 1,
+  };
     
-    #endif  // TESSERACT_CCMAIN_EQUATIONDETECT_H_
+    
+    {}  // namespace grpc
+    
+    Status ProtoServerReflection::ListService(ServerContext* context,
+                                          ListServiceResponse* response) {
+  if (services_ == nullptr) {
+    return Status(StatusCode::NOT_FOUND, 'Services not found.');
+  }
+  for (auto it = services_->begin(); it != services_->end(); ++it) {
+    ServiceResponse* service_response = response->add_service();
+    service_response->set_name(*it);
+  }
+  return Status::OK;
+}
+    
+    void SetCreateThreadPool(CreateThreadPoolFunc func) { g_ctp_impl = func; }
+    
+    
+    {	void set_flag(PhysicsServer::HingeJointFlag p_flag, bool p_value);
+	bool get_flag(PhysicsServer::HingeJointFlag p_flag) const;
+};
+#endif
 
     
-    class OrientationDetector {
- public:
-  OrientationDetector(const GenericVector<int>* allowed_scripts,
-                      OSResults* results);
-  bool detect_blob(BLOB_CHOICE_LIST* scores);
-  int get_orientation();
- private:
-  OSResults* osr_;
-  const GenericVector<int>* allowed_scripts_;
+    class JointBullet : public ConstraintBullet {
+    }
+    
+    class RIDBullet : public RID_Data {
+	RID self;
+	BulletPhysicsServer *physicsServer;
+    }
+    
+    jobject GDAPI godot_android_get_activity() {
+#ifdef __ANDROID__
+	JNIEnv *env = ThreadAndroid::get_env();
+    }
+    
+    	ERR_FAIL_NULL(p_obj);
+	id = p_obj->get_instance_id();
+}
+void FuncRef::set_function(const StringName &p_func) {
+    
+    
+    {
+    {			n = c - '0';
+		} else if (c >= 'a' && c <= 'f') {
+			n = 10 + (c - 'a');
+		} else if (c >= 'A' && c <= 'F') {
+			n = 10 + (c - 'A');
+		} else if (c == ':') {
+			break;
+		} else {
+			ERR_EXPLAIN('Invalid character in ipv6 address: ' + p_string);
+			ERR_FAIL();
+		};
+		ret = ret << 4;
+		ret += n;
+	};
+    
+    	if (cmderOptions.registerApp == true) {
+		RegisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_BACKGROUND);
+		RegisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_LISTITEM);
+	}
+	else if (cmderOptions.unRegisterApp == true)
+	{
+		UnregisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_BACKGROUND);
+		UnregisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_LISTITEM);
+	}
+	else if (cmderOptions.error == true)
+	{
+		return 1;
+	}
+	else
+	{
+		StartCmder(cmderOptions.cmderStart, cmderOptions.cmderSingle, cmderOptions.cmderTask, cmderOptions.cmderCfgRoot);
+	}
+    
+    
+    {  // Verify that the size of the key space not touched by the reads
+  // is pretty much unchanged.
+  const int64_t final_other_size = Size(Key(n), Key(kCount));
+  ASSERT_LE(final_other_size, initial_other_size + 1048576);
+  ASSERT_GE(final_other_size, initial_other_size/5 - 1048576);
+}
+    
+    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
+  result->append(key.user_key.data(), key.user_key.size());
+  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
+}
+    
+    #include <stdio.h>
+#include 'leveldb/dumpfile.h'
+#include 'leveldb/env.h'
+#include 'leveldb/status.h'
+    
+        // Advances to the previous position.
+    // REQUIRES: Valid()
+    void Prev();
+    
+      /*
+   * @brief a variable tracking all of the paths we attempt to carve
+   *
+   * This is a globbed set of file paths that we're expecting will be
+   * carved.
+   */
+  std::set<boost::filesystem::path> carvePaths_;
+    
+    namespace rj = rapidjson;
+    
+    #include <osquery/config.h>
+#include <osquery/logger.h>
+#include <osquery/registry_factory.h>
+    
+    
+    {  c.reset();
+}
+    
+    /**
+ * @brief Compute multiple hashes from a files contents simultaneously.
+ *
+ * @param mask Bitmask specifying target osquery-supported algorithms.
+ * @param path Filesystem path (the hash target).
+ * @return A struct containing string (hex) representations
+ *         of the hash digests.
+ */
+MultiHashes hashMultiFromFile(int mask, const std::string& path);
+    
+      SubscriptionVector delete_subscriptions;
+  {
+    WriteLock lock(subscription_lock_);
+    auto end = std::remove_if(
+        subscriptions_.begin(),
+        subscriptions_.end(),
+        [&delete_subscriptions](const SubscriptionRef& subscription) {
+          auto inotify_sc = getSubscriptionContext(subscription->context);
+          if (inotify_sc->mark_for_deletion == true) {
+            delete_subscriptions.push_back(subscription);
+            return true;
+          }
+          return false;
+        });
+    subscriptions_.erase(end, subscriptions_.end());
+  }
+    
+      // Perform the parsing.
+  AuditEventRecord audit_event_record = {};
+  bool parser_status =
+      AuditdNetlinkParser::ParseAuditReply(reply, audit_event_record);
+  EXPECT_EQ(parser_status, true);
+    
+    BENCHMARK_RELATIVE(hex_uintToHex, iters) {
+  while (iters--) {
+    detail::uintToHexLower(
+        bigBuf.data(), detail::kMaxHexLength, static_cast<unsigned int>(iters));
+  }
+}
+    
+    #include <folly/Benchmark.h>
+    
+    
+    {  std::string arguments;
+  ASSERT_TRUE(getTracepointArguments(
+      'folly',
+      'test_semaphore_local',
+      (uintptr_t)((void*)&FOLLY_SDT_SEMAPHORE(folly, test_semaphore_local)),
+      arguments));
+  std::array<int, 2> expected{{sizeof(long), sizeof(short)}};
+  checkTracepointArguments(arguments, expected);
+  EXPECT_FALSE(FOLLY_SDT_IS_ENABLED(folly, test_semaphore_local));
+}
+    
+    #include <folly/Range.h>
+    
+    template <>
+struct hash<folly::Uri> {
+  std::size_t operator()(const folly::Uri& k) const {
+    return std::hash<folly::uri_detail::UriTuple>{}(
+        folly::uri_detail::as_tuple(k));
+  }
 };
     
-    /**
- * Sets up the internal data for iterating the blobs of a new word, then
- * moves the iterator to the given offset.
- */
-void PageIterator::BeginWord(int offset) {
-  WERD_RES* word_res = it_->word();
-  if (word_res == nullptr) {
-    // This is a non-text block, so there is no word.
-    word_length_ = 0;
-    blob_index_ = 0;
-    word_ = nullptr;
-    return;
-  }
-  if (word_res->best_choice != nullptr) {
-    // Recognition has been done, so we are using the box_word, which
-    // is already baseline denormalized.
-    word_length_ = word_res->best_choice->length();
-    if (word_res->box_word != nullptr) {
-      if (word_res->box_word->length() != word_length_) {
-        tprintf('Corrupted word! best_choice[len=%d] = %s, box_word[len=%d]: ',
-                word_length_, word_res->best_choice->unichar_string().string(),
-                word_res->box_word->length());
-        word_res->box_word->bounding_box().print();
-      }
-      ASSERT_HOST(word_res->box_word->length() == word_length_);
+    namespace folly {
     }
-    word_ = nullptr;
-    // We will be iterating the box_word.
-    delete cblob_it_;
-    cblob_it_ = nullptr;
-  } else {
-    // No recognition yet, so a 'symbol' is a cblob.
-    word_ = word_res->word;
-    ASSERT_HOST(word_->cblob_list() != nullptr);
-    word_length_ = word_->cblob_list()->length();
-    if (cblob_it_ == nullptr) cblob_it_ = new C_BLOB_IT;
-    cblob_it_->set_to_list(word_->cblob_list());
+    
+    /// Wrapper around the makeCompressionCounterHandler() extension point.
+class CompressionCounter {
+ public:
+  CompressionCounter() {}
+  CompressionCounter(
+      folly::io::CodecType codecType,
+      folly::StringPiece codecName,
+      folly::Optional<int> level,
+      CompressionCounterKey key,
+      CompressionCounterType counterType) {
+    initialize_ = [=]() {
+      return makeCompressionCounterHandler(
+          codecType, codecName, level, key, counterType);
+    };
+    DCHECK(!initialize_.hasAllocatedMemory());
   }
-  for (blob_index_ = 0; blob_index_ < offset; ++blob_index_) {
-    if (cblob_it_ != nullptr)
-      cblob_it_->forward();
-  }
-}
-    
-      /**
-   * Moves the iterator to the beginning of the paragraph.
-   * This class implements this functionality by moving it to the zero indexed
-   * blob of the first (leftmost) word on the first row of the paragraph.
-   */
-  virtual void RestartParagraph();
+    }
     
     
-    {}  // namespace tesseract.
+    {
+    {
+    {
+    {} // namespace detail
+} // namespace compression
+} // namespace io
+} // namespace folly
 
-    
-      // Find all editable parameters used within tesseract and create a
-  // SVMenuNode tree from it.
-  SVMenuNode *BuildListOfAllLeaves(tesseract::Tesseract *tess);
-    
-    class Node;
-    
-    DeccelAmplitude* DeccelAmplitude::create(Action *action, float duration)
-{
-    DeccelAmplitude *ret = new (std::nothrow) DeccelAmplitude();
-    if (ret && ret->initWithAction(action, duration))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    }
-    
-    void ActionInstant::step(float /*dt*/)
-{
-    float updateDt = 1;
-#if CC_ENABLE_SCRIPT_BINDING
-    if (_scriptType == kScriptTypeJavascript)
-    {
-        if (ScriptEngineManager::sendActionEventToJS(this, kActionUpdate, (void *)&updateDt))
-            return;
-    }
-#endif
-    update(updateDt);
-}
-    
-        /** Helper constructor to create an array of spawned actions. 
-     *
-     * @param action1   The first sequenceable action.
-     * @param args  The va_list variable.
-     * @return  An autoreleased Spawn object.
-     * @js NA
-     */
-    static Spawn* createWithVariableList(FiniteTimeAction *action1, va_list args);
-    
-    #include '2d/CCActionGrid3D.h'
-    
-    /**
-@brief Progress from a percentage to another percentage.
-@since v0.99.1
-*/
-class CC_DLL ProgressFromTo : public ActionInterval
-{
-public:
-    /** 
-     * @brief Create and initializes the action with a duration, a 'from' percentage and a 'to' percentage.
-     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
-     * @param fromPercentage Specify the source percentage.
-     * @param toPercentage Specify the destination percentage.
-     * @return If the creation success, return a pointer of ProgressFromTo action; otherwise, return nil.
-     */
-    static ProgressFromTo* create(float duration, float fromPercentage, float toPercentage);
-    }
-    
-    void AnimationCache::parseVersion1(const ValueMap& animations)
-{
-    SpriteFrameCache *frameCache = SpriteFrameCache::getInstance();
-    }
-    
-    void TransformBlock(double block[64], Transform1d f) {
-  double tmp[64];
-  for (int x = 0; x < 8; ++x) {
-    f(&block[x], 8, &tmp[x]);
-  }
-  for (int y = 0; y < 8; ++y) {
-    f(&tmp[8 * y], 1, &block[8 * y]);
-  }
-}
-    
-    const double* NewSrgb8ToLinearTable() {
-  double* table = new double[256];
-  int i = 0;
-  for (; i < 11; ++i) {
-    table[i] = i / 12.92;
-  }
-  for (; i < 256; ++i) {
-    table[i] = 255.0 * std::pow(((i / 255.0) + 0.055) / 1.055, 2.4);
-  }
-  return table;
-}
-    
-    // Fills in 'result' with the inverse DCT of 'block'.
-// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
-// a row-by-row memory layout.
-void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
-    
-    #include 'guetzli/jpeg_data_decoder.h'
-    
-    namespace guetzli {
-    }
-    
-    
-    {}  // namespace guetzli
-    
-    void BENCHFUN(insertFront)(int iters, int initialSize) {
-  BenchmarkSuspender braces;
-  auto const obj = randomObject<VECTOR::value_type>();
-  VECTOR v(initialSize, obj);
-  braces.dismissing([&]() {
-    FOR_EACH_RANGE (i, 0, iters) { v.insert(v.begin(), obj); }
-  });
-}
-    
-    BENCHMARK_RELATIVE(format_nested_fbstrings, iters) {
-  BenchmarkSuspender suspender;
-  while (iters--) {
-    for (int i = 0; i < 1000; ++i) {
-      fbstring out;
-      suspender.dismissing([&] {
-        format(
-            &out,
-            '{} {}',
-            format('{} {}', i, i + 1).fbstr(),
-            format('{} {}', -i, -i - 1).fbstr());
-      });
-    }
-  }
-}
-    
-    // ============================================================================
-// folly/test/GLogBenchmark.cpp                    relative  time/iter  iters/s
-// ============================================================================
-// skip_overhead                                               36.37ns   27.49M
-// dev_null_log_overhead                                        2.61us  382.57K
-// ============================================================================
-    
-    // Structure of note section for the probe.
-#define FOLLY_SDT_NOTE_CONTENT(provider, name, has_semaphore, arg_template)    \
-  FOLLY_SDT_ASM_1(990: FOLLY_SDT_NOP)                                          \
-  FOLLY_SDT_ASM_3(     .pushsection .note.stapsdt,'','note')                   \
-  FOLLY_SDT_ASM_1(     .balign 4)                                              \
-  FOLLY_SDT_ASM_3(     .4byte 992f-991f, 994f-993f, FOLLY_SDT_NOTE_TYPE)       \
-  FOLLY_SDT_ASM_1(991: .asciz FOLLY_SDT_NOTE_NAME)                             \
-  FOLLY_SDT_ASM_1(992: .balign 4)                                              \
-  FOLLY_SDT_ASM_1(993: FOLLY_SDT_ASM_ADDR 990b)                                \
-  FOLLY_SDT_ASM_1(     FOLLY_SDT_ASM_ADDR 0) /*Reserved for Base Address*/     \
-  FOLLY_SDT_SEMAPHORE_NOTE_##has_semaphore(provider, name)                     \
-  FOLLY_SDT_ASM_STRING(provider)                                               \
-  FOLLY_SDT_ASM_STRING(name)                                                   \
-  FOLLY_SDT_ASM_STRING(arg_template)                                           \
-  FOLLY_SDT_ASM_1(994: .balign 4)                                              \
-  FOLLY_SDT_ASM_1(     .popsection)
-    
-    namespace detail {
-    }
-    
-    template <class String>
-String Uri::toString() const {
-  String str;
-  if (hasAuthority_) {
-    toAppend(scheme_, '://', &str);
-    if (!password_.empty()) {
-      toAppend(username_, ':', password_, '@', &str);
-    } else if (!username_.empty()) {
-      toAppend(username_, '@', &str);
-    }
-    toAppend(host_, &str);
-    if (port_ != 0) {
-      toAppend(':', port_, &str);
-    }
-  } else {
-    toAppend(scheme_, ':', &str);
-  }
-  toAppend(path_, &str);
-  if (!query_.empty()) {
-    toAppend('?', query_, &str);
-  }
-  if (!fragment_.empty()) {
-    toAppend('#', fragment_, &str);
-  }
-  return str;
-}
-    
-    #if FOLLY_HAVE_LIBZ
     
      private:
   // Matches packed_sync_pointer.  Must be > max number of local
