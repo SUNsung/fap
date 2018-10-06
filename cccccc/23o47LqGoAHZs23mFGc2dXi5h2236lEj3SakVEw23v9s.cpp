f40cd2f97aeba2e79ce5af948b39a7117a55bde4
SUNsung <weatherius@gmail.com>
2018-10-06 08:25:55 +0300

    Rewrite [23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp] File in: 2018-10-06 08:25:55

--- cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
+++ cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
@@ -1,278 +1,240 @@
 
         
-        #include <set>
-#include <string>
-#include <vector>
+        #include 'base/values.h'
+#include 'base/strings/utf_string_conversions.h'
+#include 'base/strings/string16.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'ui/base/clipboard/clipboard.h'
     
-    using atom::api::BoxLayout;
     
-    namespace atom {
+    {}
+    
+      keyval = GetKeycodeFromText(key);
+  if (keyval == ui::VKEY_UNKNOWN){
+    enable_shortcut_ = false;
+  } else {
+    enable_shortcut_ = true;
+    //only code for ctrl, shift, alt, super and meta modifiers
+    int modifiers_value = ui::EF_NONE;
+    modifiers = base::ToLowerASCII(modifiers);
+    if (modifiers.find('ctrl')!=std::string::npos){
+      modifiers_value |= ui::EF_CONTROL_DOWN;
+    }
+    if (modifiers.find('shift')!=std::string::npos){
+      modifiers_value |= ui::EF_SHIFT_DOWN ;
+    }
+    if (modifiers.find('alt')!=std::string::npos){
+      modifiers_value |= ui::EF_ALT_DOWN;
+    }
+    if (modifiers.find('super')!=std::string::npos
+     || modifiers.find('cmd')!=std::string::npos
+     || modifiers.find('command')!=std::string::npos){
+      modifiers_value |= ui::EF_COMMAND_DOWN;
+    }
+    if (modifiers.find('meta')!=std::string::npos){
+      meta_down_flag_ = true;
+    }
+    accelerator_ = ui::Accelerator(keyval,modifiers_value);
+  }
+    
+        bool ReadImage(ClipboardData& data) {
+      DCHECK(data.type == TYPE_PNG || data.type == TYPE_JPEG);
+      std::vector<unsigned char> encoded_image;
+      SkBitmap bitmap = clipboard_->ReadImage(ui::CLIPBOARD_TYPE_COPY_PASTE);
     }
     
-    // static
-void GlobalShortcut::BuildPrototype(v8::Isolate* isolate,
-                                    v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, 'GlobalShortcut'));
-  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
-      .SetMethod('register', &GlobalShortcut::Register)
-      .SetMethod('isRegistered', &GlobalShortcut::IsRegistered)
-      .SetMethod('unregister', &GlobalShortcut::Unregister)
-      .SetMethod('unregisterAll', &GlobalShortcut::UnregisterAll);
-}
-    
-      // Observable:
-  void OnMenuWillClose() override;
-  void OnMenuWillShow() override;
-    
-    #ifndef BITCOIN_QT_PLATFORMSTYLE_H
-#define BITCOIN_QT_PLATFORMSTYLE_H
-    
-    
-    {    secp256k1_scalar_get_b32(brx, sigr);
-    r = secp256k1_fe_set_b32(&fx, brx);
-    (void)r;
-    VERIFY_CHECK(r); /* brx comes from a scalar, so is less than the order; certainly less than p */
-    if (recid & 2) {
-        if (secp256k1_fe_cmp_var(&fx, &secp256k1_ecdsa_const_p_minus_order) >= 0) {
-            return 0;
-        }
-        secp256k1_fe_add(&fx, &secp256k1_ecdsa_const_order_as_fe);
-    }
-    if (!secp256k1_ge_set_xo_var(&x, &fx, recid & 1)) {
-        return 0;
-    }
-    secp256k1_gej_set_ge(&xj, &x);
-    secp256k1_scalar_inverse_var(&rn, sigr);
-    secp256k1_scalar_mul(&u1, &rn, message);
-    secp256k1_scalar_negate(&u1, &u1);
-    secp256k1_scalar_mul(&u2, &rn, sigs);
-    secp256k1_ecmult(ctx, &qj, &xj, &u2, &u1);
-    secp256k1_ge_set_gej_var(pubkey, &qj);
-    return !secp256k1_gej_is_infinity(&qj);
-}
-    
-    #endif /* SECP256K1_MODULE_RECOVERY_TESTS_H */
-
-    
-    
-/// Format a value into a stream, delegating to operator<< by default.
-///
-/// Users may override this for their own types.  When this function is called,
-/// the stream flags will have been modified according to the format string.
-/// The format specification is provided in the range [fmtBegin, fmtEnd).  For
-/// truncating conversions, ntrunc is set to the desired maximum number of
-/// characters, for example '%.7s' calls formatValue with ntrunc = 7.
-///
-/// By default, formatValue() uses the usual stream insertion operator
-/// operator<< to format the type T, with special cases for the %c and %p
-/// conversions.
-template<typename T>
-inline void formatValue(std::ostream& out, const char* /*fmtBegin*/,
-                        const char* fmtEnd, int ntrunc, const T& value)
-{
-#ifndef TINYFORMAT_ALLOW_WCHAR_STRINGS
-    // Since we don't support printing of wchar_t using '%ls', make it fail at
-    // compile time in preference to printing as a void* at runtime.
-    typedef typename detail::is_wchar<T>::tinyformat_wchar_is_not_supported DummyType;
-    (void) DummyType(); // avoid unused type warning with gcc-4.8
-#endif
-    // The mess here is to support the %c and %p conversions: if these
-    // conversions are active we try to convert the type to a char or const
-    // void* respectively and format that instead of the value itself.  For the
-    // %p conversion it's important to avoid dereferencing the pointer, which
-    // could otherwise lead to a crash when printing a dangling (const char*).
-    const bool canConvertToChar = detail::is_convertible<T,char>::value;
-    const bool canConvertToVoidPtr = detail::is_convertible<T, const void*>::value;
-    if(canConvertToChar && *(fmtEnd-1) == 'c')
-        detail::formatValueAsType<T, char>::invoke(out, value);
-    else if(canConvertToVoidPtr && *(fmtEnd-1) == 'p')
-        detail::formatValueAsType<T, const void*>::invoke(out, value);
-#ifdef TINYFORMAT_OLD_LIBSTDCPLUSPLUS_WORKAROUND
-    else if(detail::formatZeroIntegerWorkaround<T>::invoke(out, value)) /**/;
-#endif
-    else if(ntrunc >= 0)
-    {
-        // Take care not to overread C strings in truncating conversions like
-        // '%.4s' where at most 4 characters may be read.
-        detail::formatTruncated(out, value, ntrunc);
-    }
-    else
-        out << value;
-}
-    
-      bool is_started() const;
-    
-    /**
- * @brief Computes the classification accuracy for a one-of-many
- *        classification task.
- */
-template <typename Dtype>
-class AccuracyLayer : public Layer<Dtype> {
+    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
  public:
-  /**
-   * @param param provides AccuracyParameter accuracy_param,
-   *     with AccuracyLayer options:
-   *   - top_k (\b optional, default 1).
-   *     Sets the maximum rank @f$ k @f$ at which a prediction is considered
-   *     correct.  For example, if @f$ k = 5 @f$, a prediction is counted
-   *     correct if the correct label is among the top 5 predicted labels.
-   */
-  explicit AccuracyLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+  NwMenuGetNSStringFWithFixupFunction() {}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     
-     protected:
-  /// @copydoc BNLLLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+ protected:
+  ~NwMenuGetNSStringFWithFixupFunction() override {}
     
-    
-    {}  // namespace caffe
-    
-    namespace caffe {
-    }
-    
-    REGISTER_GRADIENT(Sub, GetSubGradient);
-    
-        auto oldSize = new_tensor->size();
-    auto maxElem = 1 +
-        *(std::max_element(
-            indices.template data<int>(),
-            indices.template data<int>() + indices.size()));
-    
-    //////////////////////////////////////////////////////////////////////
-    
-      bool operator!=(SrcLoc o) const { return !(*this == o); }
-    
-    struct ObjectData;
-struct Object;
-    
-    inline APCLocalArray* APCLocalArray::Make(const APCArray* aa) {
-  auto size = sizeof(APCLocalArray) + aa->size() * sizeof(TypedValue);
-  auto local = new (tl_heap->objMalloc(size)) APCLocalArray(aa);
-  assertx(local->heapSize() == size);
-  return local;
-}
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    #endif
-
-    
-    /*!
- * \brief Whether customize the logger outputs.
- */
-#ifndef XGBOOST_CUSTOMIZE_LOGGER
-#define XGBOOST_CUSTOMIZE_LOGGER XGBOOST_STRICT_R_MODE
-#endif
-    
-    inline Float8 polynomial_5(Float8 const& x, const float c0, const float c1,
-                           const float c2, const float c3, const float c4,
-                           const float c5) {
-  // calculates polynomial c5*x^5 + c4*x^4 + c3*x^3 + c2*x^2 + c1*x + c0
-  Float8 x2 = x * x;
-  Float8 x4 = x2 * x2;
-  return (Float8(c2) + Float8(c3) * x) * x2 +
-         ((Float8(c4) + Float8(c5) * x) * x4 + (Float8(c0) + Float8(c1) * x));
-}
-    
-    
-    { private:
-  std::istream &fin_;
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
 };
     
-    using folly::Function;
-using folly::FunctionRef;
+        std::string type;
+    switch(src.id.type) {
+    case content::DesktopMediaID::TYPE_WINDOW:
+      type = 'window';
+      break;
+    case content::DesktopMediaID::TYPE_SCREEN:
+      type = 'screen';
+      break;
+    case content::DesktopMediaID::TYPE_NONE:
+      type = 'none';
+      break;
+    default:
+      type = 'unknown';
+      break;
+    }
     
-    #include <folly/GLog.h>
+      // implement nw.Screen.getScreens()
+  class NwScreenGetScreensFunction: public NWSyncExtensionFunction {
+    public:
+      NwScreenGetScreensFunction();
+      bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
     
-    // Check a Linux kernel-style return code (>= 0 on success, negative error
-// number on error), throw on error.
-template <class... Args>
-void checkKernelError(ssize_t ret, Args&&... args) {
-  if (UNLIKELY(ret < 0)) {
-    throwSystemErrorExplicit(int(-ret), std::forward<Args>(args)...);
-  }
+    
+    {    struct KeypointStore {
+        virtual void push(f32 kpX, f32 kpY, f32 kpSize, f32 kpAngle=-1, f32 kpResponse=0, s32 kpOctave=0, s32 kpClass_id=-1) = 0;
+        virtual ~KeypointStore() {};
+    };
 }
     
-    // Empirically, this indicates if the runtime supports
-// std::exception_ptr, as not all (arm, for instance) do.
-#if defined(__GNUC__) && defined(__GCC_ATOMIC_INT_LOCK_FREE) && \
-    __GCC_ATOMIC_INT_LOCK_FREE > 1
-inline fbstring exceptionStr(std::exception_ptr ep) {
-  try {
-    std::rethrow_exception(ep);
-  } catch (const std::exception& e) {
-    return exceptionStr(e);
-  } catch (...) {
-    return '<unknown exception>';
-  }
-}
+    
+    {            vst1q_s16(dst + j, v_dst0);
+            vst1q_s16(dst + j + 8, v_dst1);
+        }
+        for (; j < roiw8; j += 8)
+        {
+            int16x8_t v_src = vreinterpretq_s16_u16(vmovl_u8(vld1_u8(src + j)));
+            int16x8_t v_dst = vld1q_s16(dst + j);
+            int16x4_t v_srclo = vget_low_s16(v_src), v_srchi = vget_high_s16(v_src);
+            v_dst = vcombine_s16(vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srclo, v_srclo), shift), vget_low_s16(v_dst))),
+                                 vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srchi, v_srchi), shift), vget_high_s16(v_dst))));
+            vst1q_s16(dst + j, v_dst);
+        }
+    
+    SPLIT(u, 8,2)
+SPLIT(u, 8,3)
+SPLIT(u, 8,4)
+SPLIT(u,16,2)
+SPLIT(u,16,3)
+SPLIT(u,16,4)
+SPLIT(s,32,2)
+SPLIT(s,32,3)
+SPLIT(s,32,4)
+    
+    void combineUYVY(const Size2D &size,
+                 const u8 * srcyBase, ptrdiff_t srcyStride,
+                 const u8 * srcuBase, ptrdiff_t srcuStride,
+                 const u8 * srcvBase, ptrdiff_t srcvStride,
+                 u8 * dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+#ifndef __ANDROID__
+    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
 #endif
-    
-    /// An Executor accepts units of work with add(), which should be
-/// threadsafe.
-class Executor {
- public:
-  // Workaround for a linkage problem with explicitly defaulted dtor t22914621
-  virtual ~Executor() {}
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
     }
     
-      /**
-   * Returns a random uint32_t given a specific RNG
-   */
-  template <class RNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static uint32_t rand32(RNG&& rng) {
-    return rng();
-  }
-    
-    
-    {  // Test for overflow.
-  // Use a custom hours type using time_t as the underlying storage type to
-  // guarantee that we can overflow.
-  using hours_timet = std::chrono::duration<time_t, std::ratio<3600>>;
-  EXPECT_THROW(
-      to<struct timespec>(hours_timet(std::numeric_limits<time_t>::max())),
-      std::range_error);
+    /*!
+ *  Aligns pointer by the certain number of bytes
+ *
+ *  This small inline function aligns the pointer by the certain number of bytes by shifting
+ *  it forward by 0 or a positive offset.
+ */
+template<typename T> inline T* alignPtr(T* ptr, size_t n=sizeof(T))
+{
+    return (T*)(((size_t)ptr + n-1) & -n);
 }
     
-    namespace folly {
-namespace io {
-enum class CodecType;
-} // namespace io
+        if (flipMode == FLIP_VERTICAL_MODE)
+    {
+        for (size_t y = 0; y < size.height; ++y)
+        {
+            const u8 * src_row = internal::getRowPtr(srcBase, srcStride, y);
+            u8 * dst_row = internal::getRowPtr(dstBase, dstStride, size.height - y - 1);
+    }
     }
     
-    template <typename T>
-typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type
-prefixToStringLE(T prefix, uint64_t n = sizeof(T)) {
-  DCHECK_GT(n, 0);
-  DCHECK_LE(n, sizeof(T));
-  prefix = Endian::little(prefix);
-  std::string result;
-  result.resize(n);
-  memcpy(&result[0], &prefix, n);
-  return result;
-}
+        // Setup Vulkan binding
+    ImGui_ImplVulkan_InitInfo init_info = {};
+    init_info.Instance = g_Instance;
+    init_info.PhysicalDevice = g_PhysicalDevice;
+    init_info.Device = g_Device;
+    init_info.QueueFamily = g_QueueFamily;
+    init_info.Queue = g_Queue;
+    init_info.PipelineCache = g_PipelineCache;
+    init_info.DescriptorPool = g_DescriptorPool;
+    init_info.Allocator = g_Allocator;
+    init_info.CheckVkResultFn = check_vk_result;
+    ImGui_ImplVulkan_Init(&init_info, wd->RenderPass);
     
-      std::sort(cpus.begin(), cpus.end(), [&](size_t lhs, size_t rhs) -> bool {
-    // sort first by equiv class of cache with highest index,
-    // direction doesn't matter.  If different cpus have
-    // different numbers of caches then this code might produce
-    // a sub-optimal ordering, but it won't crash
-    auto& lhsEquiv = equivClassesByCpu[lhs];
-    auto& rhsEquiv = equivClassesByCpu[rhs];
-    for (ssize_t i = ssize_t(std::min(lhsEquiv.size(), rhsEquiv.size())) - 1;
-         i >= 0;
-         --i) {
-      auto idx = size_t(i);
-      if (lhsEquiv[idx] != rhsEquiv[idx]) {
-        return lhsEquiv[idx] < rhsEquiv[idx];
-      }
+        glutMainLoop();
+    
+        {
+        D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+        desc.Type           = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+        desc.NumDescriptors = NUM_BACK_BUFFERS;
+        desc.Flags          = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+        desc.NodeMask       = 1;
+        if (g_pd3dDevice->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&g_pd3dRtvDescHeap)) != S_OK)
+            return E_FAIL;
     }
+    
+        // Create depth-stencil State
+    {
+        D3D11_DEPTH_STENCIL_DESC desc;
+        ZeroMemory(&desc, sizeof(desc));
+        desc.DepthEnable = false;
+        desc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+        desc.DepthFunc = D3D11_COMPARISON_ALWAYS;
+        desc.StencilEnable = false;
+        desc.FrontFace.StencilFailOp = desc.FrontFace.StencilDepthFailOp = desc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+        desc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+        desc.BackFace = desc.FrontFace;
+        g_pd3dDevice->CreateDepthStencilState(&desc, &g_pDepthStencilState);
     }
+    
+            D3D12_COMMAND_QUEUE_DESC queueDesc = {};
+        queueDesc.Type     = D3D12_COMMAND_LIST_TYPE_DIRECT;
+        queueDesc.Flags    = D3D12_COMMAND_QUEUE_FLAG_NONE;
+        queueDesc.NodeMask = 1;
+    
+    // About GLSL version:
+// The 'glsl_version' initialization parameter should be NULL (default) or a '#version XXX' string.
+// On computer platform the GLSL version default to '#version 130'. On OpenGL ES 3 platform it defaults to '#version 300 es'
+// Only override if your GL version doesn't handle this GLSL version. See GLSL version table at the top of imgui_impl_opengl3.cpp.
+    
+    static const int kCrToRedTable[256] = {
+  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
+  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
+  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
+  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
+  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
+   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
+   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
+   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
+   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
+   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
+   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
+     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
+    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
+    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
+    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
+    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
+    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
+   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
+   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
+   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
+   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
+   174,  175,  177,  178
+};
+    
+      // Returns the distance map between the baseline image and the image in the
+  // last Compare() call (or the baseline image, if Compare() was not called
+  // yet).
+  // The dimensions of the distance map are the same as the baseline image.
+  // The interpretation of the distance values depend on the comparator used.
+  virtual const std::vector<float> distmap() const = 0;
+    
+    #include 'guetzli/fdct.h'
+    
+    namespace guetzli {
+    }
+    
+    
+    {}  // namespace guetzli
+
+    
+    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
+// quantization table must have 3 * kDCTBlockSize values.
+bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
+                     const int* quant, JPEGData* jpg);
