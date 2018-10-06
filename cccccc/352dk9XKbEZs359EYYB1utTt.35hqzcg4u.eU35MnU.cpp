5032ef7e9c27c49f228582758117193c09bc0b0b
SUNsung <weatherius@gmail.com>
2018-10-06 23:35:35 +0300

    Rewrite [352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp] File in: 2018-10-06 23:35:35

--- cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
+++ cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
@@ -1,90 +1,171 @@
 
         
-        #ifdef BOOST_REGEX_HAS_OTHER_WCHAR_T
-template<>
-struct regex_iterator_traits<unsigned short*> : pointer_iterator_traits<unsigned short>{};
-template<>
-struct regex_iterator_traits<const unsigned short*> : const_pointer_iterator_traits<unsigned short>{};
-#endif
+        Q_SIGNALS:
+    void valueChanged();
     
-    typedef enum _match_flags
-{
-   match_default = 0,
-   match_not_bol = 1,                                /* first is not start of line */
-   match_not_eol = match_not_bol << 1,               /* last is not end of line */
-   match_not_bob = match_not_eol << 1,               /* first is not start of buffer */
-   match_not_eob = match_not_bob << 1,               /* last is not end of buffer */
-   match_not_bow = match_not_eob << 1,               /* first is not start of word */
-   match_not_eow = match_not_bow << 1,               /* last is not end of word */
-   match_not_dot_newline = match_not_eow << 1,       /* \n is not matched by '.' */
-   match_not_dot_null = match_not_dot_newline << 1,  /* '\0' is not matched by '.' */
-   match_prev_avail = match_not_dot_null << 1,       /* *--first is a valid expression */
-   match_init = match_prev_avail << 1,               /* internal use */
-   match_any = match_init << 1,                      /* don't care what we match */
-   match_not_null = match_any << 1,                  /* string can't be null */
-   match_continuous = match_not_null << 1,           /* each grep match must continue from */
-                                                     /* uninterupted from the previous one */
-   match_partial = match_continuous << 1,            /* find partial matches */
-   
-   match_stop = match_partial << 1,                  /* stop after first match (grep) V3 only */
-   match_not_initial_null = match_stop,              /* don't match initial null, V4 only */
-   match_all = match_stop << 1,                      /* must find the whole of input even if match_any is set */
-   match_perl = match_all << 1,                      /* Use perl matching rules */
-   match_posix = match_perl << 1,                    /* Use POSIX matching rules */
-   match_nosubs = match_posix << 1,                  /* don't trap marked subs */
-   match_extra = match_nosubs << 1,                  /* include full capture information for repeated captures */
-   match_single_line = match_extra << 1,             /* treat text as single line and ignor any \n's when matching ^ and $. */
-   match_unused1 = match_single_line << 1,           /* unused */
-   match_unused2 = match_unused1 << 1,               /* unused */
-   match_unused3 = match_unused2 << 1,               /* unused */
-   match_max = match_unused3,
+        std::tie(r_key_dest, preexisting_r_address) = build_address();
+    std::tie(s_key_dest, preexisting_s_address) = build_address();
+    std::tie(std::ignore, new_address) = build_address();
+    
+        explicit reverse_lock(Lock& _lock) : lock(_lock) {
+        _lock.unlock();
+        _lock.swap(templock);
     }
     
     
-    {
-    {}
-} // namespace boost
+    {    /* Try to multiply it by bad values */
+    CHECK(secp256k1_ecdh(ctx, output, &point, s_zero) == 0);
+    CHECK(secp256k1_ecdh(ctx, output, &point, s_overflow) == 0);
+    /* ...and a good one */
+    s_overflow[31] -= 1;
+    CHECK(secp256k1_ecdh(ctx, output, &point, s_overflow) == 1);
+}
+    
+    BOOST_AUTO_TEST_SUITE_END()
+
+    
+    static void RejectDifficultyMismatch(double difficulty, double expected_difficulty) {
+     BOOST_CHECK_MESSAGE(
+        DoubleEquals(difficulty, expected_difficulty, 0.00001),
+        'Difficulty was ' + std::to_string(difficulty)
+            + ' but was expected to be ' + std::to_string(expected_difficulty));
+}
+    
+    /// Make type-agnostic format list from list of template arguments.
+///
+/// The exact return type of this function is an implementation detail and
+/// shouldn't be relied upon.  Instead it should be stored as a FormatListRef:
+///
+///   FormatListRef formatList = makeFormatList( /*...*/ );
+template<typename... Args>
+detail::FormatListN<sizeof...(Args)> makeFormatList(const Args&... args)
+{
+    return detail::FormatListN<sizeof...(args)>(args...);
+}
     
     
-    {} // namespace boost
-    
-    #endif  // include
-    
-       typedef typename match_results<BidiIterator>::allocator_type match_allocator_type;
-    
-    
-    {public:
-   regex_iterator_implementation(const regex_type* p, BidirectionalIterator last, match_flag_type f)
-      : base(), end(last), re(*p), flags(f){}
-   bool init(BidirectionalIterator first)
-   {
-      base = first;
-      return regex_search(first, end, what, re, flags);
-   }
-   bool compare(const regex_iterator_implementation& that)
-   {
-      if(this == &that) return true;
-      return (&re.get_data() == &that.re.get_data()) && (end == that.end) && (flags == that.flags) && (what[0].first == that.what[0].first) && (what[0].second == that.what[0].second);
-   }
-   const match_results<BidirectionalIterator>& get()
-   { return what; }
-   bool next()
-   {
-      //if(what.prefix().first != what[0].second)
-      //   flags |= match_prev_avail;
-      BidirectionalIterator next_start = what[0].second;
-      match_flag_type f(flags);
-      if(!what.length() || (f & regex_constants::match_posix))
-         f |= regex_constants::match_not_initial_null;
-      //if(base != next_start)
-      //   f |= regex_constants::match_not_bob;
-      bool result = regex_search(next_start, end, what, re, f, base);
-      if(result)
-         what.set_base(base);
-      return result;
-   }
-private:
-   regex_iterator_implementation& operator=(const regex_iterator_implementation&);
+    { protected:
+  int n_ = 0;
+  std::mutex m_;
+  std::condition_variable cv_;
 };
     
+    **Result**
     
+    REGISTER_CPU_OPERATOR(
+    MergeSingleListFeatureTensors,
+    MergeSingleListFeatureTensorsOp<CPUContext>);
+OPERATOR_SCHEMA(MergeSingleListFeatureTensors)
+    .SetDoc(
+        'Merge given single-feature tensors with list features into one '
+        'multi-feature tensor.' +
+        doc)
+    .NumInputs([](int n) { return n >= 3 && n % 3 == 0; })
+    .NumOutputs(4)
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_values', '.values')
+    .Input(2, 'in1_presence', '.presence')
+    .Output(0, 'out_lengths', '.lengths')
+    .Output(1, 'out_keys', '.keys')
+    .Output(2, 'out_values_lengths', '.values.lengths')
+    .Output(3, 'out_values_values', '.values.values')
+    .Arg('feature_ids', 'feature ids');
+    
+    workspace.FeedBlob('data', np.array([8,2,1,1,7,8,1]).astype(np.float32))
+print('data:\n', workspace.FetchBlob('data'))
+    
+      template <typename T>
+  bool DoRunWithType() {
+    const auto& data = Input(0);
+    CAFFE_ENFORCE(data.ndim() == 1, 'data should be 1-D.');
+    }
+    
+    	static char (&test(B *))[1];
+	static char (&test(...))[2];
+    
+    #include 'core/math/matrix3.h'
+#include 'core/math/transform.h'
+#include 'core/math/vector3.h'
+#include 'core/typedefs.h'
+    
+    	const btDiscreteDynamicsWorld *m_world;
+	btPersistentManifold *m_manifoldPtr;
+	bool m_ownManifold;
+	bool m_isSwapped;
+    
+    
+    {	_FORCE_INLINE_ void _set_physics_server(BulletPhysicsServer *p_physicsServer) { physicsServer = p_physicsServer; }
+	_FORCE_INLINE_ BulletPhysicsServer *get_physics_server() const { return physicsServer; }
+};
+#endif
+
+    
+    void register_csg_types() {
+    }
+    
+    static ResourceFormatDDS *resource_loader_dds = NULL;
+    
+    	ADD_PROPERTY(PropertyInfo(Variant::BOOL, 'refuse_new_connections'), 'set_refuse_new_connections', 'is_refusing_new_connections');
+	ADD_PROPERTY(PropertyInfo(Variant::INT, 'transfer_mode', PROPERTY_HINT_ENUM, 'Unreliable,Unreliable Ordered,Reliable'), 'set_transfer_mode', 'get_transfer_mode');
+    
+      tmp0 = in[6 * stride];
+  tmp1 = kIDCTMatrix[ 6] * tmp0;
+  tmp2 = kIDCTMatrix[14] * tmp0;
+  out[0] += tmp1;
+  out[1] += tmp2;
+  out[2] -= tmp2;
+  out[3] -= tmp1;
+  out[4] -= tmp1;
+  out[5] -= tmp2;
+  out[6] += tmp2;
+  out[7] += tmp1;
+    
+    #ifndef GUETZLI_JPEG_DATA_READER_H_
+#define GUETZLI_JPEG_DATA_READER_H_
+    
+    class DHTRegistry {
+private:
+  struct Data {
+    bool initialized;
+    }
+    }
+    
+    namespace aria2 {
+    }
+    
+      virtual bool isReply() const CXX11_OVERRIDE;
+    
+    #include 'DHTNode.h'
+#include 'DHTConstants.h'
+#include 'bittorrent_helper.h'
+#include 'DlAbortEx.h'
+#include 'Logger.h'
+#include 'a2netcompat.h'
+#include 'fmt.h'
+#include 'util.h'
+#include 'array_fun.h'
+#include 'LogFactory.h'
+#include 'BufferedFile.h'
+    
+    #endif // D_DHT_ROUTING_TABLE_DESERIALIZER_H
+
+    
+      std::chrono::seconds timeout_;
+    
+      virtual void
+  addPeriodicTask1(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+    
+    #endif // D_DHT_TOKEN_TRACKER_H
+
+    
+      virtual void process() CXX11_OVERRIDE;
+    
+    void DNSCache::CacheEntry::markBad(const std::string& addr)
+{
+  auto i = find(addr);
+  if (i != addrEntries_.end()) {
+    i->good_ = false;
+  }
+}
+    
+      DNSCache& operator=(const DNSCache& c);
