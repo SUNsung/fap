6751b6bed64b83225a2921488408d2d08e48746b
SUNsung <weatherius@gmail.com>
2018-10-07 01:58:23 +0300

    Rewrite [06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp] File in: 2018-10-07 01:58:22

--- cccccc/06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp
+++ cccccc/06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp
@@ -1,275 +1,85 @@
 
         
-        void NwCurrentWindowInternalCapturePageInternalFunction::OnCaptureFailure(FailureReason reason) {
-  const char* reason_description = 'internal error';
-  switch (reason) {
-    case FAILURE_REASON_UNKNOWN:
-      reason_description = 'unknown error';
-      break;
-    case FAILURE_REASON_ENCODING_FAILED:
-      reason_description = 'encoding failed';
-      break;
-    case FAILURE_REASON_VIEW_INVISIBLE:
-      reason_description = 'view is invisible';
-      break;
-  }
-  error_ = ErrorUtils::FormatErrorMessage('Failed to capture tab: *',
-                                          reason_description);
-  Respond(Error(error_));
-}
-    
-    // Generate param traits read methods.
-#include 'ipc/param_traits_read_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
-    
-    
-    {    result->AppendBoolean(success);
-    return;
-  } else if (method == 'UnregisterGlobalHotKey') {
-    int object_id = -1;
-    arguments.GetInteger(0, &object_id);
-    Shortcut* shortcut =
-        static_cast<Shortcut*>(DispatcherHost::GetApiObject(object_id));
-    GlobalShortcutListener::GetInstance()->UnregisterAccelerator(
-        shortcut->GetAccelerator(), shortcut);
-    return;
-  } else if (method == 'SetProxyConfig') {
-    std::string proxy_config;
-    arguments.GetString(0, &proxy_config);
-    SetProxyConfig(GetRenderProcessHost(), proxy_config);
-    return;
-  } else if (method == 'DoneMenuShow') {
-    dispatcher_host->quit_run_loop();
-    return;
-  }
-    
-    
-    
-    Menu::Menu(int id,
-           const base::WeakPtr<ObjectManager>& object_manager,
-           const base::DictionaryValue& option,
-           const std::string& extension_id)
-  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
-  Create(option);
-}
-    
-       void ExecuteCommand(int command_id, int event_flags) override;
-    
-    MenuItem::MenuItem(int id,
-                   const base::WeakPtr<ObjectManager>& object_manager,
-                   const base::DictionaryValue& option,
-                   const std::string& extension_id)
-  : Base(id, object_manager, option, extension_id) {
-  Create(option);
-}
+          // NotificationDelegate:
+  void NotificationAction(int index) override;
+  void NotificationClick() override;
+  void NotificationReplied(const std::string& reply) override;
+  void NotificationDisplayed() override;
+  void NotificationDestroyed() override;
+  void NotificationClosed() override;
     
     
     {
-    {    std::string error_;
-    std::unique_ptr<ui::ScopedClipboardWriter> scw_;
-  };
+    {    if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Decl *>()) {
+      SemanticNode.get<Decl *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else {
+      llvm_unreachable('ASTNode has pointer to unknown thing!');
+    }
+    llvm::errs() << '\n=====================================================\n';
+  }
 }
-    
-    
-    {
-} // namespace extensions
-#endif
 
     
-    #include 'extensions/browser/extension_function.h'
+    bool swift::parseASTSection(SerializedModuleLoader *SML, StringRef buf,
+                            SmallVectorImpl<std::string> &foundModules) {
+  if (!serialization::isSerializedAST(buf))
+    return false;
+    }
+    
+      bool IsFirstLine = true;
+    
+      if (auto ptr = type->getAs<clang::PointerType>()) {
+    auto pointee = ptr->getPointeeType();
+    }
+    
+      // Quit the whole app.
+  static void Quit(content::RenderProcessHost* rph = NULL);
+    
+    std::string Clipboard::GetText() {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  base::string16 text;
+  clipboard->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
+  return base::UTF16ToUTF8(text);
+}
+    
+    #include 'ui/base/models/simple_menu_model.h'
+    
+    aura::Window* Menu::GetActiveNativeView(content::RenderFrameHost* rfh) {
+  content::WebContents* web_contents =
+    content::WebContents::FromRenderFrameHost(rfh);
+  if (!web_contents) {
+    LOG(ERROR) << 'Menu: couldn't find WebContents';
+    return NULL;
+  }
+  return web_contents->GetFullscreenRenderWidgetHostView()
+             ? web_contents->GetFullscreenRenderWidgetHostView()
+                   ->GetNativeView()
+             : web_contents->GetNativeView();
+}
+    
+    void MenuItem::SetLabel(const std::string& label) {
+  label_ = label;
+  gtk_menu_item_set_label(GTK_MENU_ITEM(menu_item_), label.c_str());
+}
+    
+      focus_manager_ = NULL;
+  menu_ = NULL;
+    
+    #include <vector>
     
      protected:
-  ~NwObjAllocateIdFunction() override;
+  ~NwClipboardReadAvailableTypesFunction() override;
     
-    OPERATOR_SCHEMA(ExpandDims)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .AllowInplace({{0, 0}})
-    .TensorInferenceFunction([](const OperatorDef& def,
-                                const vector<TensorShape>& in) {
-      ArgumentHelper helper(def);
-      auto dims = helper.template GetRepeatedArgument<int>('dims');
-      auto originalSize = dims.size();
-      CAFFE_ENFORCE(originalSize > 0, 'Parameter `dims` must be provided.');
+    bool NwObjCallObjectMethodFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
     }
-    
-    #include 'caffe2/core/context.h'
-#include 'caffe2/core/operator.h'
-#include 'caffe2/core/tensor.h'
-    
-    **Result**
-    
-    #endif // CAFFE2_OPERATORS_FLOOR_OP_H_
-
-    
-    /// Database domain where we store carve table entries
-const std::string kCarveDbDomain = 'carves';
-    
-    
-    {
-    {  Status update(const std::string& source, const ParserConfig& config) override;
-};
-} // namespace osquery
-
-    
-      // We use a restricted scope below to change the data structure from
-  // an array to a set. This lets us do deletes much more efficiently
-  std::vector<std::string> created_views;
-  std::set<std::string> erase_views;
-  {
-    std::vector<std::string> old_views_vec;
-    scanDatabaseKeys(kQueries, old_views_vec, kConfigViews);
-    for (const auto& view : old_views_vec) {
-      erase_views.insert(view.substr(kConfigViews.size()));
-    }
-  }
-    
-      /**
-   * @brief Update the internal context buffer with additional content
-   *
-   * This method allows you to chunk up large content so that it doesn't all
-   * have to be loaded into memory at the same time
-   *
-   * @param buffer The buffer to be hashed
-   * @param size The size of the buffer to be hashed
-   */
-  void update(const void* buffer, size_t size);
-    
-      // Now test inclusive bounds.
-  struct ConstraintList cl3;
-  constraint = Constraint(LESS_THAN_OR_EQUALS);
-  constraint.expr = '1000';
-  cl3.add(constraint);
-  constraint = Constraint(GREATER_THAN_OR_EQUALS);
-  constraint.expr = '1';
-  cl3.add(constraint);
-    
-        // Main loop
-    bool done = false;
-    while (!done)
-    {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                done = true;
-        }
-    }
-    
-        UINT64 fenceValue = frameCtxt->FenceValue;
-    if (fenceValue == 0)
-        return; // No fence was signaled
-    
-    
-    {  // Reorder to the correct output order.
-  // TODO(szabadka): Modify the above computation so that this is not needed.
-  Complex tmp = a[2];
-  a[2] = a[3];
-  a[3] = a[5];
-  a[5] = a[7];
-  a[7] = a[4];
-  a[4] = a[1];
-  a[1] = a[6];
-  a[6] = tmp;
-}
-    
-      // Returns true if the argument of the last Compare() call (or the baseline
-  // image, if Compare() was not called yet) meets the image acceptance
-  // criteria. The target_mul modifies the acceptance criteria used in this call
-  // the following way:
-  //    = 1.0 : the original acceptance criteria is used,
-  //    < 1.0 : a more strict acceptance criteria is used,
-  //    > 1.0 : a less strict acceptance criteria is used.
-  virtual bool DistanceOK(double target_mul) const = 0;
-    
-    #define GUETZLI_LOG(stats, ...)                                    \
-  do {                                                             \
-    char debug_string[1024];                                       \
-    int res = snprintf(debug_string, sizeof(debug_string),         \
-                       __VA_ARGS__);                               \
-    assert(res > 0 && 'expected successful printing');             \
-    (void)res;                                                     \
-    debug_string[sizeof(debug_string) - 1] = '\0';                 \
-    ::guetzli::PrintDebug(                      \
-         stats, std::string(debug_string));        \
-  } while (0)
-#define GUETZLI_LOG_QUANT(stats, q)                    \
-  for (int y = 0; y < 8; ++y) {                        \
-    for (int c = 0; c < 3; ++c) {                      \
-      for (int x = 0; x < 8; ++x)                      \
-        GUETZLI_LOG(stats, ' %2d', (q)[c][8 * y + x]); \
-      GUETZLI_LOG(stats, '   ');                       \
-    }                                                  \
-    GUETZLI_LOG(stats, '\n');                          \
-  }
-    
-        // The nodes are:
-    // [0, n): the sorted leaf nodes that we start with.
-    // [n]: we add a sentinel here.
-    // [n + 1, 2n): new parent nodes are added here, starting from
-    //              (n+1). These are naturally in ascending order.
-    // [2n]: we add a sentinel at the end as well.
-    // There will be (2n+1) elements at the end.
-    const HuffmanTree sentinel(~static_cast<uint32_t>(0), -1, -1);
-    tree[n] = sentinel;
-    tree[n + 1] = sentinel;
-    
-    inline int Log2FloorNonZero(uint32_t n) {
-#ifdef __GNUC__
-  return 31 ^ __builtin_clz(n);
-#else
-  unsigned int result = 0;
-  while (n >>= 1) result++;
-  return result;
-#endif
-}
-    
-    // An upper estimate of memory usage of Guetzli. The bound is
-// max(kLowerMemusaeMB * 1<<20, pixel_count * kBytesPerPixel)
-constexpr int kBytesPerPixel = 350;
-constexpr int kLowestMemusageMB = 100; // in MB
-    
-      tmp0 = in[stride];
-  tmp1 = kIDCTMatrix[ 1] * tmp0;
-  tmp2 = kIDCTMatrix[ 9] * tmp0;
-  tmp3 = kIDCTMatrix[17] * tmp0;
-  tmp4 = kIDCTMatrix[25] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] += tmp3;
-  out[3] += tmp4;
-  out[4] -= tmp4;
-  out[5] -= tmp3;
-  out[6] -= tmp2;
-  out[7] -= tmp1;
-    
-      // Read sampling factors and quant table index for each component.
-  std::vector<bool> ids_seen(256, false);
-  for (size_t i = 0; i < jpg->components.size(); ++i) {
-    const int id = ReadUint8(data, pos);
-    if (ids_seen[id]) {   // (cf. section B.2.2, syntax of Ci)
-      fprintf(stderr, 'Duplicate ID %d in SOF.\n', id);
-      jpg->error = JPEG_DUPLICATE_COMPONENT_ID;
-      return false;
-    }
-    ids_seen[id] = true;
-    jpg->components[i].id = id;
-    int factor = ReadUint8(data, pos);
-    int h_samp_factor = factor >> 4;
-    int v_samp_factor = factor & 0xf;
-    VERIFY_INPUT(h_samp_factor, 1, 15, SAMP_FACTOR);
-    VERIFY_INPUT(v_samp_factor, 1, 15, SAMP_FACTOR);
-    jpg->components[i].h_samp_factor = h_samp_factor;
-    jpg->components[i].v_samp_factor = v_samp_factor;
-    jpg->components[i].quant_idx = ReadUint8(data, pos);
-    jpg->max_h_samp_factor = std::max(jpg->max_h_samp_factor, h_samp_factor);
-    jpg->max_v_samp_factor = std::max(jpg->max_v_samp_factor, v_samp_factor);
-  }
