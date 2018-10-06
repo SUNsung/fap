495fd4c7c12325572633501e52d61d3ab1910c52
SUNsung <weatherius@gmail.com>
2018-10-07 02:39:33 +0300

    Rewrite [51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb] File in: 2018-10-07 02:39:32

--- ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
+++ ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
@@ -1,55 +1,12 @@
 
         
-                def initialize(object_name, method_name, template_object, content_or_options = nil, options = nil)
-          options ||= {}
-    
-    module ActionView
-  # = Action View Log Subscriber
-  #
-  # Provides functionality so that Rails can output logs from Action View.
-  class LogSubscriber < ActiveSupport::LogSubscriber
-    VIEWS_PATTERN = /^app\/views\//
-    
-      it 'accepts a Bignum as a seed' do
-    srand(0x12345678901234567890)
-    srand.should == 0x12345678901234567890
+          platform_is :windows do
+    it 'does expand shell variables when given multiples arguments' do
+      # See https://bugs.ruby-lang.org/issues/12231
+      lambda { @object.system('echo', @shell_var) }.should output_to_fd('foo\n')
+    end
   end
     
-    module LogStash
-  module PluginManager
-    class Error < StandardError; end
-    
-              it 'successfully install the plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-dns')
-          end
-        end
-    
-        context 'without a specific plugin' do
-      it 'display a list of plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
-    
-    When /^I rollback a migration$/ do
-  step %[I successfully run `rake db:rollback STEPS=1 --trace`]
-end
-    
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
-    
-        def add_active_record_callbacks
-      name = @name
-      @klass.send(:after_save) { send(name).send(:save) }
-      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
-      if @klass.respond_to?(:after_commit)
-        @klass.send(:after_commit, on: :destroy) do
-          send(name).send(:flush_deletes)
-        end
-      else
-        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
-      end
-    end
+      it 'is a private method' do
+    Kernel.should have_private_instance_method(:test)
+  end
