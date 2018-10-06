44ba79b3185eff86e6415a3a8ba772ab5c93b5e4
SUNsung <weatherius@gmail.com>
2018-10-06 14:40:58 +0300

    Rewrite [08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp] File in: 2018-10-06 14:40:58

--- cccccc/08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp
+++ cccccc/08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp
@@ -1,166 +1,85 @@
 
         
-          // add two new params for better error handling
-  errorObject->Set(mate::StringToV8(isolate(), 'code'),
-                   v8::Integer::New(isolate(), code));
-  errorObject->Set(mate::StringToV8(isolate(), 'domain'),
-                   mate::StringToV8(isolate(), domain));
+          // This random number generator facade hides boost and CUDA rng
+  // implementation from one another (for cross-platform compatibility).
+  class RNG {
+   public:
+    RNG();
+    explicit RNG(unsigned int seed);
+    explicit RNG(const RNG&);
+    RNG& operator=(const RNG&);
+    void* generator();
+   private:
+    class Generator;
+    shared_ptr<Generator> generator_;
+  };
     
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-    void Initialize(v8::Local<v8::Object> exports,
-                v8::Local<v8::Value> unused,
-                v8::Local<v8::Context> context,
-                void* priv) {
-  v8::Isolate* isolate = context->GetIsolate();
-  BrowserView::SetConstructor(isolate, base::Bind(&BrowserView::New));
+    namespace caffe {
     }
     
     
-    {}  // namespace atom
-
-    
-    // static
-mate::Handle<DownloadItem> DownloadItem::Create(v8::Isolate* isolate,
-                                                download::DownloadItem* item) {
-  auto* existing = TrackableObject::FromWrappedClass(isolate, item);
-  if (existing)
-    return mate::CreateHandle(isolate, static_cast<DownloadItem*>(existing));
-    }
-    
-    void Menu::SetRole(int index, const base::string16& role) {
-  model_->SetRole(index, role);
-}
-    
-    #include 'atom/browser/api/event_emitter.h'
-    
-      void Show();
-  void Close();
-    
-    // static
-void PowerMonitor::BuildPrototype(v8::Isolate* isolate,
-                                  v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, 'PowerMonitor'));
-    }
-    
-    namespace atom {
-    }
-    
-    int RenderProcessPreferences::AddEntry(const base::DictionaryValue& entry) {
-  return preferences_.AddEntry(entry);
-}
-    
-      /// Retrieve the array of protocol conformances, which line up with the
-  /// requirements of the generic signature.
-  ArrayRef<ProtocolConformanceRef> getConformances() const {
-    return llvm::makeArrayRef(getTrailingObjects<ProtocolConformanceRef>(),
-                              numConformanceRequirements);
-  }
-  MutableArrayRef<ProtocolConformanceRef> getConformances() {
-    return MutableArrayRef<ProtocolConformanceRef>(
-                              getTrailingObjects<ProtocolConformanceRef>(),
-                              numConformanceRequirements);
-  }
-    
-    
-    {  SyntaxMap[FromNode] = ToNode;
-}
-    
-    
-    {    buf = buf.substr(info.bytes);
-  }
-    
-      // Now, match from the first word up until the end of the type name.
-  auto typeWordIter = typeWordRevIter.base(),
-    typeWordIterEnd = typeWords.end();
-  ++nameWordIter;
-  while (typeWordIter != typeWordIterEnd &&
-         nameWordIter != nameWordIterEnd &&
-         matchNameWordToTypeWord(*nameWordIter, *typeWordIter)) {
-    ++typeWordIter;
-    ++nameWordIter;
-  }
-    
-    using namespace swift;
-    
-    
-    {  return forInvalid();
-}
-    
-    #include <intrin.h>
-#include <boost/memory_order.hpp>
-#include <boost/type_traits/make_signed.hpp>
-#include <boost/atomic/detail/config.hpp>
-#include <boost/atomic/detail/interlocked.hpp>
-#include <boost/atomic/detail/storage_type.hpp>
-#include <boost/atomic/detail/operations_fwd.hpp>
-#include <boost/atomic/capabilities.hpp>
-#include <boost/atomic/detail/ops_msvc_common.hpp>
-    
-      /**
-   * @brief Stop the CAN client.
+    { protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
+   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
+   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
+   *      the computed outputs @f$
+   *       y_n = \arg\max\limits_i x_{ni}
+   *      @f$ (for @f$ K = 1 @f$).
    */
-  virtual void Stop() = 0;
-    
-      void SendThreadFunc() {
-    using common::time::Clock;
-    using common::time::AsInt64;
-    using common::time::micros;
-    using common::ErrorCode;
-    AINFO << 'Send thread starting...';
-    TestCanParam *param = param_ptr();
-    CanClient *client = param->can_client;
-    std::vector<CanFrame> frames;
-    frames.resize(MAX_CAN_SEND_FRAME_LEN);
-    }
-    
-    
-    {
-    {
-    {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    // System gflags
-DEFINE_string(node_name, 'chassis', 'The chassis module name in proto');
-DEFINE_string(canbus_driver_name, 'canbus', 'Driver name.');
-    
-    
-    {
-    {
-    {      --(*argc);
-      --i;
-    } else if (IsFlag(argv[i], 'help')) {
-      PrintUsageAndExit();
-    }
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    NOT_IMPLEMENTED;
   }
-  for (auto const* flag :
-       {&FLAGS_benchmark_format, &FLAGS_benchmark_out_format})
-    if (*flag != 'console' && *flag != 'json' && *flag != 'csv') {
-      PrintUsageAndExit();
+  bool out_max_val_;
+  size_t top_k_;
+  bool has_axis_;
+  int axis_;
+};
+    
+    /**
+ * @brief Takes a Blob and crop it, to the shape specified by the second input
+ *  Blob, across all dimensions after the specified axis.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+    
+      bool handles_setup_;
+  cudnnHandle_t* handle_;
+  cudaStream_t*  stream_;
+    
+    #ifdef USE_CUDNN
+template <typename Dtype>
+class CuDNNLCNLayer : public LRNLayer<Dtype> {
+ public:
+  explicit CuDNNLCNLayer(const LayerParameter& param)
+      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
+        tempData1(NULL), tempData2(NULL) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNLCNLayer();
     }
-  if (FLAGS_benchmark_color.empty()) {
-    PrintUsageAndExit();
-  }
-}
     
-    Benchmark* Benchmark::ThreadPerCpu() {
-  thread_counts_.push_back(CPUInfo::Get().num_cpus);
-  return this;
-}
+      bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnLRNDescriptor_t norm_desc_;
+  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
     
     
-    {  *value = value_str;
-  return true;
-}
+    {
+    {}}
     
-    // Parses a bool/Int32/string from the environment variable
-// corresponding to the given Google Test flag.
-bool BoolFromEnv(const char* flag, bool default_val);
-int32_t Int32FromEnv(const char* flag, int32_t default_val);
-double DoubleFromEnv(const char* flag, double default_val);
-const char* StringFromEnv(const char* flag, const char* default_val);
+    #ifndef HPHP_DATA_STREAM_WRAPPER_H
+#define HPHP_DATA_STREAM_WRAPPER_H
+    
+    namespace HPHP {
+    }
