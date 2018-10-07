4dd2b602521ad427516c09b251c47d641f53538e
SUNsung <weatherius@gmail.com>
2018-10-07 07:21:33 +0300

    Rewrite [16E5eCalx28H216jG9Djsfv9616m48.3PNNQe.16OO..rb] File in: 2018-10-07 07:21:32

--- ruby/16E5eCalx28H216jG9Djsfv9616m48.3PNNQe.16OO..rb
+++ ruby/16E5eCalx28H216jG9Djsfv9616m48.3PNNQe.16OO..rb
@@ -1,103 +1,21 @@
 
         
-                def self.find_by_dn(dn, adapter)
-          adapter.user('dn', dn)
-        end
-    
-          def failed?
-        error_message.present?
-      end
-    
-          def perform(start_id, stop_id)
-        status_sql = Build
-          .where('ci_builds.commit_id = ci_stages.pipeline_id')
-          .where('ci_builds.stage = ci_stages.name')
-          .status_sql
-    
-              # Make sure we're only working with one VM if single target
-          if options[:single_target] && vms.length != 1
-            vm = @env.primary_vm
-            raise Errors::MultiVMTargetRequired if !vm
-            vms = [vm]
+                def location
+          location = get_info(:address)
+          if location.is_a?(Hash)
+            [location.locality.presence, location.country.presence].compact.join(', ')
+          else
+            location
           end
-    
-            # Checks if the target machine is ready for communication. If this
-        # returns true, then all the other methods for communicating with
-        # the machine are expected to be functional.
-        #
-        # @return [Boolean]
-        def ready?
-          false
         end
     
-            # This is the method called to when the system is being destroyed
-        # and allows the provisioners to engage in any cleanup tasks necessary.
-        def cleanup
-        end
-      end
-    end
-  end
-end
-
-    
-        describe 'A' do
-      it 'converts floating point argument as [-]0xh.hhhhp[+-]dd and use uppercase X and P' do
-        format('%A', 196).should == '0X1.88P+7'
-        format('%A', -196).should == '-0X1.88P+7'
-        format('%A', 196.1).should == '0X1.8833333333333P+7'
-        format('%A', 0.01).should == '0X1.47AE147AE147BP-7'
-        format('%A', -0.01).should == '-0X1.47AE147AE147BP-7'
+          def find_sessionless_user
+        find_user_from_access_token || find_user_from_feed_token
+      rescue Gitlab::Auth::AuthenticationError
+        nil
       end
     
-      it 'accepts a Rational' do
-    sleep(Rational(1, 9)).should be_close(0, 2)
-  end
-    
-      it 'returns true when passed ?d if the argument is a directory' do
-    Kernel.test(?d, @dir).should == true
-  end
-    
-      it 'sets the return value of the catch block to nil by default' do
-    res = catch :blah do
-      throw :blah
-    end
-    res.should == nil
-  end
-    
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-    require './plugins/pygments_code'
-require './plugins/raw'
-require 'pathname'
-    
-    ::Bundler.with_friendly_errors do
-  ::Bundler::CLI.start(ARGV, :debug => true)
-end
-
-    
-    module LogStash
-  module PluginManager
-    class Error < StandardError; end
-    
-    module LogStash module PluginManager module PackFetchStrategy
-  class Repository
-    DEFAULT_PACK_URL = 'https://artifacts.elastic.co/downloads/logstash-plugins'
-    PACK_EXTENSION = 'zip'
-    
-        platforms.types.each do |type|
-      desc 'Run acceptance test in #{type} machines'
-      task type do
-        ENV['LS_TEST_PLATFORM']=type
-        exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/*_spec.rb']]))
-      end
-    end
-    
-              it 'successfully install the plugin when verification is disabled' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
-          end
+            MergeRequest
+          .where(id: start_id..stop_id)
+          .where(latest_merge_request_diff_id: nil)
+          .each_batch(of: BATCH_SIZE) do |relation|
