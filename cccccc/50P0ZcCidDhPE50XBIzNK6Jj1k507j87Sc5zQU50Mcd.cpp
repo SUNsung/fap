eb312ce715a6316cbf31eaa1cb18645dd6859a60
SUNsung <weatherius@gmail.com>
2018-10-07 20:05:46 +0300

    Rewrite [50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp] File in: 2018-10-07 20:05:45

--- cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
+++ cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
@@ -1,211 +1,228 @@
 
         
-        PlatformKind swift::targetPlatform(LangOptions &LangOpts) {
-  if (LangOpts.Target.isMacOSX()) {
-    return (LangOpts.EnableAppExtensionRestrictions
-                ? PlatformKind::OSXApplicationExtension
-                : PlatformKind::OSX);
-  }
-    }
+         protected:
+  // content::ContentClient:
+  std::string GetProduct() const override;
+  std::string GetUserAgent() const override;
+  base::string16 GetLocalizedString(int message_id) const override;
+  void AddAdditionalSchemes(Schemes* schemes) override;
+  void AddPepperPlugins(
+      std::vector<content::PepperPluginInfo>* plugins) override;
+  void AddContentDecryptionModules(
+      std::vector<content::CdmInfo>* cdms,
+      std::vector<media::CdmHostFilePath>* cdm_host_file_paths) override;
     
-    void
-SyntaxASTMap::recordSyntaxMapping(RC<syntax::SyntaxData> FromNode,
-                                  ASTNode ToNode) {
-  if (FromNode->getKind() == SyntaxKind::Unknown) {
-    return;
-  }
-    }
     
-    void TypeWalker::anchor() {}
-    
-    bool swift::canBeMemberName(StringRef identifier) {
-  return llvm::StringSwitch<bool>(identifier)
-    .Case('init', false)
-    .Case('Protocol', false)
-    .Case('self', false)
-    .Case('Type', false)
-    .Default(true);
+    {  std::move(self->tasks_[timer]).Run();
+  self->tasks_.erase(timer);
+  uv_timer_stop(timer);
+  uv_close(reinterpret_cast<uv_handle_t*>(timer), UvTaskRunner::OnClose);
 }
     
-      while ((!QueuedTasks.empty() && !SubtaskFailed) ||
-         !ExecutingTasks.empty()) {
-    // Enqueue additional tasks if we have additional tasks, we aren't already
-    // at the parallel limit, and no earlier subtasks have failed.
-    while (!SubtaskFailed && !QueuedTasks.empty() &&
-           ExecutingTasks.size() < MaxNumberOfParallelTasks) {
-      std::unique_ptr<DummyTask> T(QueuedTasks.front().release());
-      QueuedTasks.pop();
-    }
+    class AutoUpdater : public mate::EventEmitter<AutoUpdater>,
+                    public auto_updater::Delegate,
+                    public WindowListObserver {
+ public:
+  static mate::Handle<AutoUpdater> Create(v8::Isolate* isolate);
     }
     
     
-<details>
-    
-    
-    { private:
-  const int kNumTensorsPerInput = 5;
+    {  DISALLOW_COPY_AND_ASSIGN(GlobalShortcut);
 };
     
-    #endif // CAFFE2_OPERATORS_FLEXIBLE_TOP_K_H_
+      URLRequest::SetConstructor(isolate, base::Bind(URLRequest::New));
+    
+    // Tell browser to allocate a new object.
+// function AllocateObject(id, name, options);
+v8::Handle<v8::Value> AllocateObject(int routing_id,
+                                     int object_id,
+                                     const std::string& type,
+                                     v8::Handle<v8::Value> options);
+    
+    #include 'content/nw/src/api/clipboard/clipboard.h'
+    
+    
+    {} // namespace nwapi
 
     
-    **Code**
     
-    ButteraugliComparator::ButteraugliComparator(const int width, const int height,
-                                             const std::vector<uint8_t>* rgb,
-                                             const float target_distance,
-                                             ProcessStats* stats)
-    : width_(width),
-      height_(height),
-      target_distance_(target_distance),
-      rgb_orig_(*rgb),
-      comparator_(LinearRgb(width_, height_, *rgb)),
-      distance_(0.0),
-      stats_(stats) {}
-    
-    // We don't really need to round before descaling, since we
-// still have 4 bits of precision left as final scaled output.
-#define DESCALE(a)  static_cast<coeff_t>((a) >> 16)
-    
-    namespace guetzli {
-    }
-    
-    // Fills in 'result' with the inverse DCT of 'block'.
-// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
-// a row-by-row memory layout.
-void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
-    
-    
-    {}  // namespace guetzli
-    
-      bool is_progressive = false;   // default
-  do {
-    // Read next marker.
-    size_t num_skipped = FindNextMarker(data, len, pos);
-    if (num_skipped > 0) {
-      // Add a fake marker to indicate arbitrary in-between-markers data.
-      jpg->marker_order.push_back(0xff);
-      jpg->inter_marker_data.push_back(
-          std::string(reinterpret_cast<const char*>(&data[pos]),
-                                      num_skipped));
-      pos += num_skipped;
-    }
-    EXPECT_MARKER();
-    marker = data[pos + 1];
-    pos += 2;
-    bool ok = true;
-    switch (marker) {
-      case 0xc0:
-      case 0xc1:
-      case 0xc2:
-        is_progressive = (marker == 0xc2);
-        ok = ProcessSOF(data, len, mode, &pos, jpg);
-        found_sof = true;
-        break;
-      case 0xc4:
-        ok = ProcessDHT(data, len, mode, &dc_huff_lut, &ac_huff_lut, &pos, jpg);
-        break;
-      case 0xd0:
-      case 0xd1:
-      case 0xd2:
-      case 0xd3:
-      case 0xd4:
-      case 0xd5:
-      case 0xd6:
-      case 0xd7:
-        // RST markers do not have any data.
-        break;
-      case 0xd9:
-        // Found end marker.
-        break;
-      case 0xda:
-        if (mode == JPEG_READ_ALL) {
-          ok = ProcessScan(data, len, dc_huff_lut, ac_huff_lut,
-                           scan_progression, is_progressive, &pos, jpg);
-        }
-        break;
-      case 0xdb:
-        ok = ProcessDQT(data, len, &pos, jpg);
-        break;
-      case 0xdd:
-        ok = ProcessDRI(data, len, &pos, jpg);
-        break;
-      case 0xe0:
-      case 0xe1:
-      case 0xe2:
-      case 0xe3:
-      case 0xe4:
-      case 0xe5:
-      case 0xe6:
-      case 0xe7:
-      case 0xe8:
-      case 0xe9:
-      case 0xea:
-      case 0xeb:
-      case 0xec:
-      case 0xed:
-      case 0xee:
-      case 0xef:
-        if (mode != JPEG_READ_TABLES) {
-          ok = ProcessAPP(data, len, &pos, jpg);
-        }
-        break;
-      case 0xfe:
-        if (mode != JPEG_READ_TABLES) {
-          ok = ProcessCOM(data, len, &pos, jpg);
-        }
-        break;
-      default:
-        fprintf(stderr, 'Unsupported marker: %d pos=%d len=%d\n',
-                marker, static_cast<int>(pos), static_cast<int>(len));
-        jpg->error = JPEG_UNSUPPORTED_MARKER;
-        ok = false;
-        break;
-    }
-    if (!ok) {
-      return false;
-    }
-    jpg->marker_order.push_back(marker);
-    if (mode == JPEG_READ_HEADER && found_sof) {
-      break;
-    }
-  } while (marker != 0xd9);
-    
-    // Parses the jpeg stream contained in data[*pos ... len) and fills in *jpg with
-// the parsed information.
-// If mode is JPEG_READ_HEADER, it fills in only the image dimensions in *jpg.
-// Returns false if the data is not valid jpeg, or if it contains an unsupported
-// jpeg feature.
-bool ReadJpeg(const uint8_t* data, const size_t len, JpegReadMode mode,
-              JPEGData* jpg);
-// string variant
-bool ReadJpeg(const std::string& data, JpegReadMode mode,
-              JPEGData* jpg);
-    
-    
-    {}  // namespace
-    
-        static BOOST_FORCEINLINE storage_type load(storage_type const volatile& storage, memory_order order) BOOST_NOEXCEPT
-    {
-        storage_type v = BOOST_ATOMIC_DETAIL_ARM_LOAD16(&storage);
-        base_type::fence_after_load(order);
-        return v;
-    }
-    
-    BOOST_FORCEINLINE void pause() BOOST_NOEXCEPT
-{
-#if defined(_MSC_VER) && (defined(_M_AMD64) || defined(_M_IX86))
-    _mm_pause();
-#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-    __asm__ __volatile__('pause;');
-#endif
+    {  *y = CalculateMenuYPosition(&screen_rect, &menu_req, NULL, *y);
 }
     
-        struct aligned
-    {
-        type value;
+    class NwAppCrashBrowserFunction : public UIThreadExtensionFunction {
+ public:
+  NwAppCrashBrowserFunction() {}
     }
     
-    #include <boost/memory_order.hpp>
-#include <boost/atomic/capabilities.hpp>
-#include <boost/atomic/detail/operations.hpp>
+    #include 'chrome/browser/devtools/devtools_window.h'
+#include 'chrome/browser/extensions/devtools_util.h'
+#include 'chrome/browser/extensions/extension_service.h'
+#include 'content/nw/src/api/menuitem/menuitem.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/public/browser/render_view_host.h'
+#include 'content/public/browser/web_contents.h'
+#include 'extensions/browser/extension_system.h'
+#include 'extensions/common/error_utils.h'
+    
+     protected:
+  ~NwObjAllocateIdFunction() override;
+    
+      // implement nw.Screen.getScreens()
+  class NwScreenGetScreensFunction: public NWSyncExtensionFunction {
+    public:
+      NwScreenGetScreensFunction();
+      bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    
+    {  if (s.ok() && meta->file_size > 0) {
+    // Keep it
+  } else {
+    env->DeleteFile(fname);
+  }
+  return s;
+}
+    
+    
+    {  int Compare(const InternalKey& a, const InternalKey& b) const;
+};
+    
+    // Return the name of a temporary file owned by the db named 'dbname'.
+// The result will be prefixed with 'dbname'.
+std::string TempFileName(const std::string& dbname, uint64_t number);
+    
+      class StringSource : public SequentialFile {
+   public:
+    Slice contents_;
+    bool force_error_;
+    bool returned_partial_;
+    StringSource() : force_error_(false), returned_partial_(false) { }
+    }
+    
+      // Create a writer that will append data to '*dest'.
+  // '*dest' must have initial length 'dest_length'.
+  // '*dest' must remain live while this Writer is in use.
+  Writer(WritableFile* dest, uint64_t dest_length);
+    
+    TEST(RecoveryTest, LogFileReuse) {
+  if (!CanAppend()) {
+    fprintf(stderr, 'skipping test because env does not support appending\n');
+    return;
+  }
+  for (int i = 0; i < 2; i++) {
+    ASSERT_OK(Put('foo', 'bar'));
+    if (i == 0) {
+      // Compact to ensure current log is empty
+      CompactMemTable();
+    }
+    Close();
+    ASSERT_EQ(1, NumLogs());
+    uint64_t number = FirstLogFile();
+    if (i == 0) {
+      ASSERT_EQ(0, FileSize(LogName(number)));
+    } else {
+      ASSERT_LT(0, FileSize(LogName(number)));
+    }
+    Open();
+    ASSERT_EQ(1, NumLogs());
+    ASSERT_EQ(number, FirstLogFile()) << 'did not reuse log file';
+    ASSERT_EQ('bar', Get('foo'));
+    Open();
+    ASSERT_EQ(1, NumLogs());
+    ASSERT_EQ(number, FirstLogFile()) << 'did not reuse log file';
+    ASSERT_EQ('bar', Get('foo'));
+  }
+}
+    
+        // Create builder.
+    std::string copy = TableFileName(dbname_, next_file_number_++);
+    WritableFile* file;
+    Status s = env_->NewWritableFile(copy, &file);
+    if (!s.ok()) {
+      return;
+    }
+    TableBuilder* builder = new TableBuilder(options_, file);
+    
+    static void RunConcurrent(int run) {
+  const int seed = test::RandomSeed() + (run * 100);
+  Random rnd(seed);
+  const int N = 1000;
+  const int kSize = 1000;
+  for (int i = 0; i < N; i++) {
+    if ((i % 100) == 0) {
+      fprintf(stderr, 'Run %d of %d\n', i, N);
+    }
+    TestState state(seed + 1);
+    Env::Default()->Schedule(ConcurrentReader, &state);
+    state.Wait(TestState::RUNNING);
+    for (int i = 0; i < kSize; i++) {
+      state.t_.WriteStep(&rnd);
+    }
+    state.quit_flag_.Release_Store(&state);  // Any non-null arg will do
+    state.Wait(TestState::DONE);
+  }
+}
+    
+    // Synchronous transmission of CAN messages
+ErrorCode EsdCanClient::Send(const std::vector<CanFrame> &frames,
+                             int32_t *const frame_num) {
+  CHECK_NOTNULL(frame_num);
+  CHECK_EQ(frames.size(), static_cast<size_t>(*frame_num));
+    }
+    
+    
+    {
+    {
+    {
+    {}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+    
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
+#include 'modules/drivers/canbus/can_comm/message_manager.h'
+    
+      ProtocolData<::apollo::canbus::ChassisDetail> mpd;
+  SenderMessage<::apollo::canbus::ChassisDetail> msg(1, &mpd);
+  EXPECT_FALSE(sender.NeedSend(msg, 1));
+  EXPECT_EQ(msg.message_id(), 1);
+  int32_t period = msg.curr_period();
+  msg.UpdateCurrPeriod(-50);
+  EXPECT_EQ(msg.curr_period(), period + 50);
+  EXPECT_EQ(msg.CanFrame().id, 1);
+    
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    
+    using ::apollo::canbus::ChassisDetail;
+    
+    // Canbus gflags
+DEFINE_double(sensor_freq, 100,
+              'Sensor feedback timer frequency -- 0 means event trigger.');
+    
+    #if defined(_MSC_VER)
+#include <intrin.h> // for _ReadWriteBarrier
+#endif
+    
+    namespace internal {
+    }
+    
+    
+    {  *value = double_value;
+  return true;
+}
+    
+    namespace benchmark {
+namespace internal {
+    }
+    }
+    
+      Out << run.GetAdjustedRealTime() << ',';
+  Out << run.GetAdjustedCPUTime() << ',';
+    
+    inline LogType& GetNullLogInstance() {
+  static LogType log(nullptr);
+  return log;
+}
