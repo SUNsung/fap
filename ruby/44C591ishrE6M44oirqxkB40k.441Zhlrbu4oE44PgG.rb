8d2d11b04d172e91722d003faa14a31eb866ec5b
SUNsung <weatherius@gmail.com>
2018-10-07 17:52:38 +0300

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2018-10-07 17:52:38

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,65 +1,103 @@
 
         
-        CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+                  def name_and_id_index(options)
+            if options.key?('index')
+              options.delete('index') || ''
+            elsif @generate_indexed_names
+              @auto_index || ''
+            end
+          end
     
-              @exceptions << exception
+            layout_definition = \
+          case _layout
+          when String
+            _layout.inspect
+          when Symbol
+            <<-RUBY
+              #{_layout}.tap do |layout|
+                return #{default_behavior} if layout.nil?
+                unless layout.is_a?(String) || !layout
+                  raise ArgumentError, 'Your layout method :#{_layout} returned \#{layout}. It ' \
+                    'should have returned a String, false, or nil'
+                end
+              end
+            RUBY
+          when Proc
+            define_method :_layout_from_proc, &_layout
+            protected :_layout_from_proc
+            <<-RUBY
+              result = _layout_from_proc(#{_layout.arity == 0 ? '' : 'self'})
+              return #{default_behavior} if result.nil?
+              result
+            RUBY
+          when false
+            nil
+          when true
+            raise ArgumentError, 'Layouts must be specified as a String, Symbol, Proc, false, or nil'
+          when nil
+            name_clause
+          end
+    
+    class BuildEnvironment
+  def initialize(*settings)
+    @settings = Set.new(*settings)
+  end
+    
+            file_is_stale = if PkgVersion === version
+          f.pkg_version > version
+        else
+          f.version > version
         end
     
-    module Jekyll
-  module Commands
-    class Build < Command
-      class << self
-        # Create the Mercenary command for the Jekyll CLI for this Command
-        def init_with_program(prog)
-          prog.command(:build) do |c|
-            c.syntax      'build [options]'
-            c.description 'Build your site'
-            c.alias :b
+    module Homebrew
+  def doctor
+    checks = Diagnostic::Checks.new
     
-        def self.cleanup_logs
-      return unless HOMEBREW_LOGS.directory?
-      HOMEBREW_LOGS.subdirs.each do |dir|
-        cleanup_path(dir) { dir.rmtree } if prune?(dir, :days_default => 14)
+      def filtered_list
+    names = if ARGV.named.empty?
+      Formula.racks
+    else
+      ARGV.named.map { |n| HOMEBREW_CELLAR+n }.select(&:exist?)
+    end
+    if ARGV.include? '--pinned'
+      pinned_versions = {}
+      names.each do |d|
+        keg_pin = (HOMEBREW_LIBRARY/'PinnedKegs'/d.basename.to_s)
+        if keg_pin.exist? || keg_pin.symlink?
+          pinned_versions[d] = keg_pin.readlink.basename.to_s
+        end
+      end
+      pinned_versions.each do |d, version|
+        puts '#{d.basename}'.concat(ARGV.include?('--versions') ? ' #{version}' : '')
+      end
+    else # --versions without --pinned
+      names.each do |d|
+        versions = d.subdirs.map { |pn| pn.basename.to_s }
+        next if ARGV.include?('--multiple') && versions.length < 2
+        puts '#{d.basename} #{versions*' '}'
       end
     end
+  end
+end
     
-        first_warning = true
-    methods.each do |method|
-      unless checks.respond_to?(method)
-        Homebrew.failed = true
-        puts 'No check available by the name: #{method}'
-        next
-      end
+    Badge.seed do |b|
+  b.id = Badge::FirstQuote
+  b.name = 'First Quote'
+  b.badge_type_id = BadgeType::Bronze
+  b.multiple_grant = false
+  b.target_posts = true
+  b.show_posts = true
+  b.query = BadgeQueries::FirstQuote
+  b.default_badge_grouping_id = BadgeGrouping::GettingStarted
+  b.trigger = Badge::Trigger::PostRevision
+  b.system = true
+end
     
-        # Unbrewed uses the PREFIX, which will exist
-    # Things below use the CELLAR, which doesn't until the first formula is installed.
-    unless HOMEBREW_CELLAR.exist?
-      raise NoSuchKegError.new(ARGV.named.first) if ARGV.named.any?
-      return
-    end
-    
-        def configure_sass
-      require 'sass'
-    
-        private
-    
-        # #gradient > { @mixin horizontal ... }
-    # to:
-    # @mixin gradient-horizontal
-    def flatten_mixins(file, container, prefix)
-      log_transform container, prefix
-      replace_rules file, Regexp.escape(container) do |mixins_css|
-        unindent unwrap_rule_block(mixins_css).gsub(/@mixin\s*([\w-]+)/, '@mixin #{prefix}-\\1')
-      end
-    end
+          if staff.topic_id.nil?
+        creator = PostCreator.new(Discourse.system_user,
+          raw: I18n.t('staff_category_description'),
+          title: I18n.t('category.topic_prefix', category: staff.name),
+          category: staff.name,
+          archetype: Archetype.default
+        )
+        post = creator.create
