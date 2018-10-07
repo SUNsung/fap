c51b84f83a5ecd6d095601da1540f339362c682f
SUNsung <weatherius@gmail.com>
2018-10-07 14:21:21 +0300

    Rewrite [52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py] File in: 2018-10-07 14:21:21

--- pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
+++ pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
@@ -1,50 +1,310 @@
 
         
-                title_string = 'Varying %s' % quantity
+            elif METHOD == 'LIST':
+        domain = compat_urllib_parse_urlparse(test['url']).netloc
+        if not domain:
+            print('\nFail: {0}'.format(test['name']))
+            continue
+        domain = '.'.join(domain.split('.')[-2:])
     
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (3 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
+    entry_template = textwrap.dedent('''
+    <entry>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
+        <title>New version @VERSION@</title>
+        <link href='http://rg3.github.io/youtube-dl' />
+        <content type='xhtml'>
+            <div xmlns='http://www.w3.org/1999/xhtml'>
+                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
+            </div>
+        </content>
+        <author>
+            <name>The youtube-dl maintainers</name>
+        </author>
+        <updated>@TIMESTAMP@</updated>
+    </entry>
+    ''')
     
-    import os
-import tarfile
-from contextlib import closing
     
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
+def main():
+    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
+        template = tmplf.read()
     
-    import matplotlib
-matplotlib.use('TkAgg')
+    import io
+import optparse
+import re
     
-    This example demonstrates how to generate a dataset and bicluster it
-using the Spectral Co-Clustering algorithm.
+    import io
+import optparse
+import os
+import sys
     
-    # plot shrinkage coefficient
-plt.subplot(2, 1, 2)
-plt.errorbar(n_samples_range, lw_shrinkage.mean(1), yerr=lw_shrinkage.std(1),
-             label='Ledoit-Wolf', color='navy', lw=2)
-plt.errorbar(n_samples_range, oa_shrinkage.mean(1), yerr=oa_shrinkage.std(1),
-             label='OAS', color='darkorange', lw=2)
-plt.xlabel('n_samples')
-plt.ylabel('Shrinkage')
-plt.legend(loc='lower right')
-plt.ylim(plt.ylim()[0], 1. + (plt.ylim()[1] - plt.ylim()[0]) / 10.)
-plt.xlim(5, 31)
+    sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     
-    print('The data was generated from (random_state=%d):' % RANDOM_SEED)
-print('Class', 'P(C)', 'P(w0|C)', 'P(w1|C)', sep='\t')
-for k, p, p_w in zip(['red', 'blue', 'yellow'], p_c, p_w_c.T):
-    print('%s\t%0.2f\t%0.2f\t%0.2f' % (k, p, p_w[0], p_w[1]))
+        params = {
+        'data_files': data_files,
+    }
+    if setuptools_available:
+        params['entry_points'] = {'console_scripts': ['youtube-dl = youtube_dl:main']}
+    else:
+        params['scripts'] = ['bin/youtube-dl']
+    
+                if info_dict_str:
+                info_dict_str += '\n'
+        info_dict_str += ''.join(
+            '    %s: %s,\n' % (_repr(k), _repr(test_info_dict[k]))
+            for k in missing_keys)
+        write_string(
+            '\n\'info_dict\': {\n' + info_dict_str + '},\n', out=sys.stderr)
+        self.assertFalse(
+            missing_keys,
+            'Missing keys in test definition: %s' % (
+                ', '.join(sorted(missing_keys))))
+    
+            if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ? WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    
+    import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+    
+    import pytest
+from flaskr.db import get_db
+    
+    
+def with_metaclass(meta, *bases):
+    '''Create a base class with a metaclass.'''
+    # This requires a bit of explanation: the basic idea is to make a
+    # dummy metaclass for one level of class instantiation that replaces
+    # itself with the actual metaclass.
+    class metaclass(type):
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+    return type.__new__(metaclass, 'temporary_class', (), {})
+    
+    This typically means that you attempted to use functionality that needed
+an active HTTP request.  Consult the documentation on testing for
+information about how to avoid this problem.\
+'''
+_app_ctx_err_msg = '''\
+Working outside of application context.
+    
+    
+def make_response(*args):
+    '''Sometimes it is necessary to set additional headers in a view.  Because
+    views do not have to return response objects but can return a value that
+    is converted into a response object by Flask itself, it becomes tricky to
+    add headers to it.  This function can be called instead of using a return
+    and you will get a response object which you can use to attach headers.
+    
+        if not has_level_handler(logger):
+        logger.addHandler(default_handler)
+    
+        #: A flag that indicates if the session interface is pickle based.
+    #: This can be used by Flask extensions to make a decision in regards
+    #: to how to deal with the session object.
+    #:
+    #: .. versionadded:: 0.10
+    pickle_based = False
+    
+            '''
+        available_plugins = plugin_manager.get_formatters_grouped()
+        self.enabled_plugins = []
+        for group in groups:
+            for cls in available_plugins[group]:
+                p = cls(env=env, **kwargs)
+                if p.enabled:
+                    self.enabled_plugins.append(p)
+    
+    
+ENTRY_POINT_NAMES = [
+    'httpie.plugins.auth.v1',
+    'httpie.plugins.formatter.v1',
+    'httpie.plugins.converter.v1',
+    'httpie.plugins.transport.v1',
+]
+    
+    
+install_requires = [
+    'requests>=2.18.4',
+    'Pygments>=2.1.3'
+]
+    
+    
+FILE_PATH_ARG = patharg(FILE_PATH)
+BIN_FILE_PATH_ARG = patharg(BIN_FILE_PATH)
+JSON_FILE_PATH_ARG = patharg(JSON_FILE_PATH)
+    
+    USERNAME = 'user'
+PASSWORD = 'password'
+# Basic auth encoded `USERNAME` and `PASSWORD`
+# noinspection SpellCheckingInspection
+BASIC_AUTH_HEADER_VALUE = 'Basic dXNlcjpwYXNzd29yZA=='
+BASIC_AUTH_URL = '/basic-auth/{0}/{1}'.format(USERNAME, PASSWORD)
+AUTH_OK = {'authenticated': True, 'user': USERNAME}
+    
+    
+def test_headers(httpbin_both):
+    r = http('GET', httpbin_both + '/headers', 'Foo:bar')
+    assert HTTP_OK in r
+    assert ''User-Agent': 'HTTPie' in r, r
+    assert ''Foo': 'bar'' in r
+    
+        def syntax(self):
+        return '[options] <spider>'
+    
+        def run(self, args, opts):
+        if len(args) != 1:
+            raise UsageError()
+        filename = args[0]
+        if not os.path.exists(filename):
+            raise UsageError('File not found: %s\n' % filename)
+        try:
+            module = _import_file(filename)
+        except (ImportError, ValueError) as e:
+            raise UsageError('Unable to load %r: %s\n' % (filename, e))
+        spclasses = list(iter_spider_classes(module))
+        if not spclasses:
+            raise UsageError('No spider found in file: %s\n' % filename)
+        spidercls = spclasses.pop()
+    
+        class ScrapyClientContextFactory(ClientContextFactory):
+        'A SSL context factory which is more permissive against SSL bugs.'
+        # see https://github.com/scrapy/scrapy/issues/82
+        # and https://github.com/scrapy/scrapy/issues/26
+        # and https://github.com/scrapy/scrapy/issues/981
+    
+                if res.h2:
+                self.write_ip(ip, res.domain, res.handshake_time)
+    
+    def remove_same_server(server_ip_list):
+    logger.debug('input ip: %s' % server_ip_list)
+    cleared_list = set()
+    for ip1 in server_ip_list:
+        _ip1 = ip2int(ip1)
+        for ip2 in server_ip_list:
+            b = _ip1 - ip2int(ip2)
+            if b in (1, -1):
+                cleared_list.add(ip1 if b < 0 else ip2)
+                break
+        if b not in (1, -1):
+            cleared_list.add(ip1)
+    logger.debug('cleared ip: %s' % cleared_list)
+    return cleared_list
+    
+        if __hostsdeny__ and netloc.endswith(__hostsdeny__):
+        start_response('403 Forbidden', [('Content-Type', 'text/html')])
+        yield message_html('403 Hosts Deny', 'Hosts Deny(%r)' % netloc, detail='共用appid因为资源有限，限制观看视频和文件下载等消耗资源过多的访问，请使用自己的appid <a href=' https://github.com/XX-net/XX-Net/wiki/Register-Google-appid' target='_blank'>帮助</a> ')
+        raise StopIteration
+    
+    MIN_TOKEN_TYPE = UP+1
+	
+INVALID_TOKEN_TYPE = 0
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+# This class can be used to keep the ycmd server alive for the duration of the
+# life of the client. By default, ycmd shuts down if it doesn't see a request in
+# a while.
+class YcmdKeepalive( object ):
+  def __init__( self, ping_interval_seconds = 60 * 10 ):
+    self._keepalive_thread = Thread( target = self._ThreadMain )
+    self._keepalive_thread.daemon = True
+    self._ping_interval_seconds = ping_interval_seconds
+    
+    
+def _CompileFilters( config ):
+  '''Given a filter config dictionary, return a list of compiled filters'''
+  filters = []
+    
+    
+def _EndsWithPython( path ):
+  '''Check if given path ends with a python 2.7 or 3.4+ name.'''
+  return path and PYTHON_BINARY_REGEX.search( path ) is not None
+    
+    
+def HandlePollResponse_MultipleDiagnostics_test():
+  diagnostics_handler = ExtendedMock()
+  messages = [
+    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER1' ] },
+    { 'filepath': 'bar', 'diagnostics': [ 'PLACEHOLDER2' ] },
+    { 'filepath': 'baz', 'diagnostics': [ 'PLACEHOLDER3' ] },
+    { 'filepath': 'foo', 'diagnostics': [ 'PLACEHOLDER4' ] },
+  ]
+  assert_that( _HandlePollResponse( messages, diagnostics_handler ),
+               equal_to( True ) )
+  diagnostics_handler.UpdateWithNewDiagnosticsForFile.assert_has_exact_calls( [
+    call( 'foo', [ 'PLACEHOLDER1' ] ),
+    call( 'bar', [ 'PLACEHOLDER2' ] ),
+    call( 'baz', [ 'PLACEHOLDER3' ] ),
+    call( 'foo', [ 'PLACEHOLDER4' ] )
+  ] )
+    
+      _assert_rejects( f, { 'text' : 'This is an unimportant taco',
+                        'kind' : 'WARNING' } )
+  _assert_accepts( f, { 'text' : 'This taco will be shown',
+                        'kind' : 'ERROR' } )
+    
+    
+def EndsWithPython_Bad( path ):
+  ok_( not _EndsWithPython( path ),
+       'Path {0} does end with a Python name.'.format( path ) )
+    
+    
+def _worker( executor_reference, work_queue ):
+  try:
+    while True:
+      work_item = work_queue.get( block=True )
+      if work_item is not None:
+        work_item.run()
+        continue
+      executor = executor_reference()
+      # Exit if:
+      #   - The executor that owns the worker has been collected OR
+      #   - The executor that owns the worker has been shutdown.
+      if executor is None or executor._shutdown:
+        # Notice other workers
+        work_queue.put( None )
+        return
+      del executor
+  except BaseException:
+    _base.LOGGER.critical( 'Exception in worker', exc_info=True )
+    
+        def do_action(self):
+        print(self.name, self.action.name, end=' ')
+        return self.action
+    
+    
+class Radio(object):
+    
+    ### OUTPUT ###
+# Strategy Example 0
+# Strategy Example 1 from execute 1
+# Strategy Example 2 from execute 2
 
     
+    # Execute them
+for template in templates:
+    template()
     
-def huaban_download(url, output_dir='.', **kwargs):
-    if re.match(r'http://huaban\.com/boards/\d+/', url):
-        huaban_download_board(url, output_dir, **kwargs)
-    else:
-        print('Only board (画板) pages are supported currently')
-        print('ex: http://huaban.com/boards/12345678/')
+            if not meth:
+            meth = self.generic_visit
+        return meth(node, *args, **kwargs)
+    
+        def now(self):
+        current_time = datetime.datetime.now()
+        current_time_formatted = '{}:{}'.format(current_time.hour, current_time.minute)
+        return current_time_formatted
