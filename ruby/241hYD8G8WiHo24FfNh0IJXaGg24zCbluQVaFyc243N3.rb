08d8baa807f057b10746aab26703a0b1ee467c21
SUNsung <weatherius@gmail.com>
2018-10-08 18:16:56 +0300

    Rewrite [241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb] File in: 2018-10-08 18:16:55

--- ruby/241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb
+++ ruby/241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb
@@ -1,71 +1,65 @@
 
         
-              # it 'raises an exception after retrying a failed request multiple times' do
-      #   stub_request(:get, 'https://appstoreconnect.apple.com/testflight/v2/providers/1234/apps/898536088/platforms/ios/trains').
-      #     to_return(status: 200, body: TunesStubbing.itc_read_fixture_file('build_trains_operation_failed.json'), headers: { 'Content-Type' => 'application/json' })
+            def create
+      authorize AccountModerationNote, :create?
     
-          stub_request(:get, 'https://appstoreconnect.apple.com/itc/static-resources/controllers/login_cntrl.js').
-        to_return(status: 200, body: itc_read_fixture_file('login_cntrl.js'))
-      stub_request(:get, 'https://appstoreconnect.apple.com/WebObjects/iTunesConnect.woa').
-        to_return(status: 200, body: '')
-      stub_request(:get, 'https://appstoreconnect.apple.com/WebObjects/iTunesConnect.woa/wa').
-        to_return(status: 200, body: '')
-      stub_request(:get, 'https://olympus.itunes.apple.com/v1/session').
-        to_return(status: 200, body: itc_read_fixture_file('olympus_session.json'))
-      stub_request(:get, 'https://olympus.itunes.apple.com/v1/app/config?hostname=itunesconnect.apple.com').
-        to_return(status: 200, body: { authServiceKey: 'e0abc' }.to_json, headers: { 'Content-Type' => 'application/json' })
+    module Admin
+  class ChangeEmailsController < BaseController
+    before_action :set_account
+    before_action :require_local_account!
     
-        def self.prevalidate
-      output_types = Scan.config[:output_types]
-      has_multiple_report_types = output_types && output_types.split(',').size > 1
-      if has_multiple_report_types && Scan.config[:custom_report_file_name]
-        UI.user_error!('Using a :custom_report_file_name with multiple :output_types (#{output_types}) will lead to unexpected results. Use :output_files instead.')
+          @domain_block = DomainBlock.new(resource_params)
+    
+      def lease_seconds_or_default
+    (params['hub.lease_seconds'] || 1.day).to_i.seconds
+  end
+    
+      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
+  gem.executables   = %w(cap capify)
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ['lib']
+    
+        def ask(key, default=nil, options={})
+      question = Question.new(key, default, options)
+      set(key, question)
+    end
+    
+            @response = (gets || '').chomp
       end
-    end
     
-    full_params = ARGV.shelljoin
-    
-        def assets_path
-      @assets_path ||= File.join gem_path, 'assets'
-    end
-    
-          spec['version'] = Bootstrap::VERSION
-    
-    module Sass
-  module CacheStores
-    # A backend for the Sass cache using the filesystem.
-    class Filesystem < Base
-      # The directory where the cached files will be stored.
-      #
-      # @return [String]
-      attr_accessor :cache_location
-    
-        def parse_warn_directive(parent, line, root, value, offset)
-      raise SyntaxError.new('Invalid warn directive '@warn': expected expression.') unless value
-      raise SyntaxError.new('Illegal nesting: Nothing may be nested beneath warn directives.',
-        :line => @line + 1) unless line.children.empty?
-      offset = line.offset + line.text.index(value).to_i
-      Tree::WarnNode.new(parse_script(value, :offset => offset))
-    end
-    
-              def _#{name}(name)
-            (@#{name}s && @#{name}s[name]) || @parent && @parent._#{name}(name)
-          end
-          protected :_#{name}
-    
-        # The name of the mixin in which the error occurred.
-    # This could be `nil` if the error occurred outside a mixin.
-    #
-    # @return [String]
-    def sass_mixin
-      sass_backtrace.first[:mixin]
-    end
-    
-          # If the importer is based on files on the local filesystem
-      # this method should return folders which should be watched
-      # for changes.
-      #
-      # @return [Array<String>] List of absolute paths of directories to watch
-      def directories_to_watch
-        []
+          def netssh_options
+        @netssh_options ||= super.merge(fetch(:ssh_options) || {})
       end
+    
+    if command? :kicker
+  desc 'Launch Kicker (like autotest)'
+  task :kicker do
+    puts 'Kicking... (ctrl+c to cancel)'
+    exec 'kicker -e rake test lib examples'
+  end
+end
+    
+          Resque.redis
+    end
+    
+          context 'when the whitelist is a single value' do
+        let(:test_file) { File.open(file_path('test.jpeg')) }
+    
+          it 'follows redirects but still respect the extension_whitelist' do
+        expect { uploader.download!('http://www.redirect.com/') }.to raise_error(CarrierWave::IntegrityError)
+      end
+    
+    When /^I delete the datamapper record$/ do
+  @instance.destroy
+end
+
+    
+    Then /^the file at '(.*?)' should be the reverse of the file at '(.*?)'$/ do |one, two|
+  File.read(file_path(one)).should == File.read(file_path(two)).reverse
+end
+
+    
+        def option(name)
+      self.uploader_options ||= {}
+      self.uploader_options[name] ||= record.class.uploader_option(column, name)
+    end
