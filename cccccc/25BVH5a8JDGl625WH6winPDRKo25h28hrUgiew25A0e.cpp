af00a74b8a38a85e90af51d8ba5cd3005fd62f19
SUNsung <weatherius@gmail.com>
2018-10-09 00:56:06 +0300

    Rewrite [25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp] File in: 2018-10-09 00:56:05

--- cccccc/25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp
+++ cccccc/25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp
@@ -1,231 +1,152 @@
 
         
-            /*
-        For each point `p` within `size`, do:
-        dst[p] = (1 - alpha) * dst[p] + alpha * src[p]
-    */
-    void accumulateWeighted(const Size2D &size,
-                            const u8 *srcBase, ptrdiff_t srcStride,
-                            u8 *dstBase, ptrdiff_t dstStride,
-                            f32 alpha);
-    
-    #ifdef CAROTENE_NEON
-    
-    
-    {            vst1q_s32(lanea + x, w0);
-            vst1q_s32(laneb + x, w1);
-        }
-        if(x < colsn)
-        {
-            x = colsn-4;
-            goto box3x3s32_vert;
-        }
-    
-    #define SPLIT_QUAD(sgn, bits, n) { \
-                                     internal::prefetch(src + sj); \
-                                     SPLIT_ASM##n(sgn, bits) \
-                                 }
-    
-    #include <carotene/functions.hpp>
-#include 'saturate_cast.hpp'
-    
-    #ifdef CAROTENE_NEON
-    if (shift >= 16u)
-    {
-        for (size_t i = 0; i < size.height; ++i)
-        {
-            s16 * dst = internal::getRowPtr(dstBase, dstStride, i);
-            std::memset(dst, 0, sizeof(s16) * size.width);
-        }
-        return;
-    }
-    
-    #define INRANGEFUNC(T)                                       \
-void inRange(const Size2D &_size,                            \
-             const T * srcBase, ptrdiff_t srcStride,         \
-             const T * rng1Base, ptrdiff_t rng1Stride,       \
-             const T * rng2Base, ptrdiff_t rng2Stride,       \
-             u8 * dstBase, ptrdiff_t dstStride)              \
-{                                                            \
-    internal::assertSupportedConfiguration();                \
-    inRangeCheck(_size, srcBase, srcStride,                  \
-                 rng1Base, rng1Stride, rng2Base, rng2Stride, \
-                 dstBase, dstStride);                        \
+        bool swift::isPlatformActive(PlatformKind Platform, LangOptions &LangOpts) {
+  if (Platform == PlatformKind::none)
+    return true;
+  
+  if (Platform == PlatformKind::OSXApplicationExtension ||
+      Platform == PlatformKind::iOSApplicationExtension)
+    if (!LangOpts.EnableAppExtensionRestrictions)
+      return false;
+  
+  // FIXME: This is an awful way to get the current OS.
+  switch (Platform) {
+    case PlatformKind::OSX:
+    case PlatformKind::OSXApplicationExtension:
+      return LangOpts.Target.isMacOSX();
+    case PlatformKind::iOS:
+    case PlatformKind::iOSApplicationExtension:
+      return LangOpts.Target.isiOS() && !LangOpts.Target.isTvOS();
+    case PlatformKind::tvOS:
+    case PlatformKind::tvOSApplicationExtension:
+      return LangOpts.Target.isTvOS();
+    case PlatformKind::watchOS:
+    case PlatformKind::watchOSApplicationExtension:
+      return LangOpts.Target.isWatchOS();
+    case PlatformKind::none:
+      llvm_unreachable('handled above');
+  }
+  llvm_unreachable('bad PlatformKind');
 }
-#else
-#define INRANGEFUNC(T)                                       \
-void inRange(const Size2D &,                                 \
-             const T *, ptrdiff_t,                           \
-             const T *, ptrdiff_t,                           \
-             const T *, ptrdiff_t,                           \
-             u8 *, ptrdiff_t)                                \
-{                                                            \
-    internal::assertSupportedConfiguration();                \
-}
+    
+    #ifndef PREPOSITION
+#  define PREPOSITION(Word)
 #endif
     
-      std::vector<std::string> filenames;
-  env_->GetChildren(dbname_, &filenames);  // Ignoring errors on purpose
-  uint64_t number;
-  FileType type;
-  for (size_t i = 0; i < filenames.size(); i++) {
-    if (ParseFileName(filenames[i], &number, &type)) {
-      bool keep = true;
-      switch (type) {
-        case kLogFile:
-          keep = ((number >= versions_->LogNumber()) ||
-                  (number == versions_->PrevLogNumber()));
-          break;
-        case kDescriptorFile:
-          // Keep my manifest file, and any newer incarnations'
-          // (in case there is a race that allows other incarnations)
-          keep = (number >= versions_->ManifestFileNumber());
-          break;
-        case kTableFile:
-          keep = (live.find(number) != live.end());
-          break;
-        case kTempFile:
-          // Any temp files that are currently being written to must
-          // be recorded in pending_outputs_, which is inserted into 'live'
-          keep = (live.find(number) != live.end());
-          break;
-        case kCurrentFile:
-        case kDBLockFile:
-        case kInfoLogFile:
-          keep = true;
-          break;
-      }
-    }
+      static ProcessId Pid = 0;
+    
+      static CFPointeeInfo forRecord(bool isConst, const clang::RecordDecl *decl) {
+    assert(decl);
+    CFPointeeInfo info;
+    info.IsValid = true;
+    info.IsConst = isConst;
+    info.Decl = decl;
+    return info;
+  }
+    
+    namespace rj = rapidjson;
+    
+    #include <iostream>
+    
+    TEST_F(OptionsConfigParserPluginTests, test_unknown_option) {
+  Config c;
+  std::map<std::string, std::string> update;
     }
     
-    bool GuessType(const std::string& fname, FileType* type) {
-  size_t pos = fname.rfind('/');
-  std::string basename;
-  if (pos == std::string::npos) {
-    basename = fname;
-  } else {
-    basename = std::string(fname.data() + pos + 1, fname.size() - pos - 1);
-  }
-  uint64_t ignored;
-  return ParseFileName(basename, &ignored, type);
+      /**
+   * @brief Compute the final hash and return it's result
+   *
+   * @return The final hash value
+   */
+  std::string digest();
+    
+      val = getEnvVar('GTEST_OSQUERY');
+  EXPECT_FALSE(!val);
+  EXPECT_TRUE(val.is_initialized());
+  EXPECT_EQ(*val, 'true');
+    
+    
+    {
+    {  // Stored query names is a factory method included alongside every query.
+  // It will include the set of query names with existing 'previous' results.
+  auto names = cf.getStoredQueryNames();
+  auto in_vector = std::find(names.begin(), names.end(), 'foobar');
+  EXPECT_NE(in_vector, names.end());
+}
+}
+
+    
+      // Test operator lower bounds.
+  EXPECT_FALSE(cl2.matches(0));
+  EXPECT_FALSE(cl2.matches(1));
+    
+    
+    {  // Set the last-most-recent query as the request, and delete it.
+  DistributedQueryRequest request;
+  const auto& next = queries.front();
+  request.id = next.substr(kDistributedQueryPrefix.size());
+  getDatabaseValue(kQueries, next, request.query);
+  deleteDatabaseValue(kQueries, next);
+  return request;
 }
     
-    // Return the name of the sstable with the specified number
-// in the db named by 'dbname'.  The result will be prefixed with
-// 'dbname'.
-std::string TableFileName(const std::string& dbname, uint64_t number);
-    
-      // Successful parses
-  static struct {
-    const char* fname;
-    uint64_t number;
-    FileType type;
-  } cases[] = {
-    { '100.log',            100,   kLogFile },
-    { '0.log',              0,     kLogFile },
-    { '0.sst',              0,     kTableFile },
-    { '0.ldb',              0,     kTableFile },
-    { 'CURRENT',            0,     kCurrentFile },
-    { 'LOCK',               0,     kDBLockFile },
-    { 'MANIFEST-2',         2,     kDescriptorFile },
-    { 'MANIFEST-7',         7,     kDescriptorFile },
-    { 'LOG',                0,     kInfoLogFile },
-    { 'LOG.old',            0,     kInfoLogFile },
-    { '18446744073709551615.log', 18446744073709551615ull, kLogFile },
-  };
-  for (int i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
-    std::string f = cases[i].fname;
-    ASSERT_TRUE(ParseFileName(f, &number, &type)) << f;
-    ASSERT_EQ(cases[i].type, type) << f;
-    ASSERT_EQ(cases[i].number, number) << f;
-  }
-    
-    class MemTableIterator: public Iterator {
- public:
-  explicit MemTableIterator(MemTable::Table* table) : iter_(table) { }
+    TEST_F(EventsTests, test_fire_event) {
+  auto pub = std::make_shared<BasicEventPublisher>();
+  pub->setName('BasicPublisher');
+  auto status = EventFactory::registerEventPublisher(pub);
+  ASSERT_TRUE(status.ok());
     }
-    
-      // Insert key into the list.
-  // REQUIRES: nothing that compares equal to key is currently in the list.
-  void Insert(const Key& key);
-    
-    // Performs in-place floating point 8x8 DCT on block[0..63].
-// Note that the DCT used here is the DCT-2 with the first term multiplied by
-// 1/sqrt(2) and the result scaled by 1/2.
-void ComputeBlockDCTDouble(double block[64]);
-    
-        // The nodes are:
-    // [0, n): the sorted leaf nodes that we start with.
-    // [n]: we add a sentinel here.
-    // [n + 1, 2n): new parent nodes are added here, starting from
-    //              (n+1). These are naturally in ascending order.
-    // [2n]: we add a sentinel at the end as well.
-    // There will be (2n+1) elements at the end.
-    const HuffmanTree sentinel(~static_cast<uint32_t>(0), -1, -1);
-    tree[n] = sentinel;
-    tree[n + 1] = sentinel;
-    
-    void SaveQuantTables(const int q[3][kDCTBlockSize], JPEGData* jpg) {
-  const size_t kTableSize = kDCTBlockSize * sizeof(q[0][0]);
-  jpg->quant.clear();
-  int num_tables = 0;
-  for (size_t i = 0; i < jpg->components.size(); ++i) {
-    JPEGComponent* comp = &jpg->components[i];
-    // Check if we have this quant table already.
-    bool found = false;
-    for (int j = 0; j < num_tables; ++j) {
-      if (memcmp(&q[i][0], &jpg->quant[j].values[0], kTableSize) == 0) {
-        comp->quant_idx = j;
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      JPEGQuantTable table;
-      memcpy(&table.values[0], &q[i][0], kTableSize);
-      table.precision = 0;
-      for (int k = 0; k < kDCTBlockSize; ++k) {
-        assert(table.values[k] >= 0);
-        assert(table.values[k] < (1 << 16));
-        if (table.values[k] > 0xff) {
-          table.precision = 1;
-        }
-      }
-      table.index = num_tables;
-      comp->quant_idx = num_tables;
-      jpg->quant.push_back(table);
-      ++num_tables;
-    }
-  }
-}
-    
-    static const int kDCTBlockSize = 64;
-static const int kMaxComponents = 4;
-static const int kMaxQuantTables = 4;
-static const int kMaxHuffmanTables = 4;
-static const int kJpegHuffmanMaxBitLength = 16;
-static const int kJpegHuffmanAlphabetSize = 256;
-static const int kJpegDCAlphabetSize = 12;
-static const int kMaxDHTMarkers = 512;
-    
-      // 2. set baudrate to 500k
-  ret = canSetBaudrate(dev_handler_, NTCAN_BAUD_500);
-  if (ret != NTCAN_SUCCESS) {
-    AERROR << 'set baudrate error code: ' << ret << ', ' << GetErrorString(ret);
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
-  }
     
     /**
- * @struct CheckIdArg
+ * @brief A more 'complex' example table is provided to assist with tests.
  *
- * @brief this struct include data for check ids.
+ * This table will access options and flags known to the extension.
+ * An extension should not assume access to any CLI flags- rather, access is
+ * provided via the osquery-meta table: osquery_flags.
+ *
+ * There is no API/C++ wrapper to provide seamless use of flags yet.
+ * We can force an implicit query to the manager though.
+ *
+ * Database access should be mediated by the *Database functions.
+ * Direct use of the 'database' registry will lead to undefined behavior.
  */
-struct CheckIdArg {
-  int64_t period = 0;
-  int64_t real_period = 0;
-  int64_t last_time = 0;
-  int32_t error_count = 0;
-};
+class ComplexExampleTable : public TablePlugin {
+ private:
+  TableColumns columns() const {
+    return {
+        std::make_tuple('flag_test', TEXT_TYPE, ColumnOptions::DEFAULT),
+        std::make_tuple('database_test', TEXT_TYPE, ColumnOptions::DEFAULT),
+    };
+  }
+    }
     
-    #ifndef MODULES_DRIVERS_CANBUS_SENSOR_GFLAGS_H_
-#define MODULES_DRIVERS_CANBUS_SENSOR_GFLAGS_H_
+    void IDCT1d(const double* in, int stride, double* out) {
+  for (int x = 0; x < 8; ++x) {
+    out[x * stride] = 0.0;
+    for (int u = 0; u < 8; ++u) {
+      out[x * stride] += kDCTMatrix[8 * u + x] * in[u * stride];
+    }
+  }
+}
+    
+    #include 'guetzli/jpeg_data.h'
+    
+    #endif  // GUETZLI_JPEG_DATA_H_
+
+    
+    // Parses the jpeg stream contained in data[*pos ... len) and fills in *jpg with
+// the parsed information.
+// If mode is JPEG_READ_HEADER, it fills in only the image dimensions in *jpg.
+// Returns false if the data is not valid jpeg, or if it contains an unsupported
+// jpeg feature.
+bool ReadJpeg(const uint8_t* data, const size_t len, JpegReadMode mode,
+              JPEGData* jpg);
+// string variant
+bool ReadJpeg(const std::string& data, JpegReadMode mode,
+              JPEGData* jpg);
+    
+    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo);
+void BuildACHistograms(const JPEGData& jpg, JpegHistogram* histo);
+size_t JpegHeaderSize(const JPEGData& jpg, bool strip_metadata);
+size_t EstimateJpegDataSize(const int num_components,
+                            const std::vector<JpegHistogram>& histograms);
