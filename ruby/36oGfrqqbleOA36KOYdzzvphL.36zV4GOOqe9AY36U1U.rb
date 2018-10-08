41f9d2172e4a9ec95007b1e1d7cbe5a8eb4037f1
SUNsung <weatherius@gmail.com>
2018-10-08 14:10:52 +0300

    Rewrite [36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb] File in: 2018-10-08 14:10:52

--- ruby/36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb
+++ ruby/36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb
@@ -1,23 +1,33 @@
 
         
-                def uid
-          @uid ||= Gitlab::Utils.force_utf8(auth_hash.uid.to_s)
+                def render(&block)
+          options = @options.stringify_keys
+          tag_value = options.delete('value')
+          name_and_id = options.dup
+    
+            class << self
+          def field_type
+            @field_type ||= name.split('::').last.sub('Field', '').downcase
+          end
         end
     
-          def to_markdown
-        '[![#{title}](#{image_url})](#{link_url})'
-      end
-    
-            def template
-          @template ||= Pipeline::Template.new(self)
-        end
-      end
+        initializer 'action_view.logger' do
+      ActiveSupport.on_load(:action_view) { self.logger ||= Rails.logger }
     end
+    
+      def set_statuses
+    return unless page_requested?
+    
+          @account_moderation_note = current_account.account_moderation_notes.new(resource_params)
+    
+          log_action :confirm, @user
+    
+      def maxheight_or_default
+    params[:maxheight].present? ? params[:maxheight].to_i : nil
   end
 end
 
     
-            def initialize(badge)
-          @entity = badge.entity
-          @status = badge.status
-        end
+      def initialize(*paths)
+    @paths = parse(*paths)
+  end
