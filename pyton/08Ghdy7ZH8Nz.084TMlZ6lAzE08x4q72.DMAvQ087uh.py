99442ead04485b29b5fe7e38d52ce310f973abaa
SUNsung <weatherius@gmail.com>
2018-10-08 10:22:40 +0300

    Rewrite [08Ghdy7ZH8Nz.084TMlZ6lAzE08x4q72.DMAvQ087uh.py] File in: 2018-10-08 10:22:39

--- pyton/08Ghdy7ZH8Nz.084TMlZ6lAzE08x4q72.DMAvQ087uh.py
+++ pyton/08Ghdy7ZH8Nz.084TMlZ6lAzE08x4q72.DMAvQ087uh.py
@@ -1,197 +1,128 @@
 
         
-            def _dispatch_call(self, call, employees):
-        for employee in employees:
-            if employee.call is None:
-                employee.take_call(call)
-                return employee
-        return None
+            def __init__(self, vehicle_size, license_plate, spot_size):
+        self.vehicle_size = vehicle_size
+        self.license_plate = license_plate
+        self.spot_size
+        self.spots_taken = []
     
-        def __init__(self, timestamp, seller, amount):
-        self.timestamp = timestamp
-        self.seller = seller
-        self.amount = amount
+        def __init__(self):
+        self.lookup = {}  # key: person_id, value: person_server
     
+        def get(self, key):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                return item.value
+        raise KeyError('Key not found')
     
-class HashTable(object):
+        def state_forwards(self, app_label, state):
+        pass
     
-        @pytest.mark.parametrize(
-        'other, result', (
-            ({'AccePT': 'application/json'}, True),
-            ({}, False),
-            (None, False)
-        )
-    )
-    def test_instance_equality(self, other, result):
-        assert (self.case_insensitive_dict == other) is result
+        def exists(self, session_key=None):
+        '''
+        This method makes sense when you're talking to a shared resource, but
+        it doesn't matter when you're storing the information in the client's
+        cookie.
+        '''
+        return False
     
+        req_protocol = request.scheme
+    req_site = get_current_site(request)
     
-def consume_socket_content(sock, timeout=0.5):
-    chunks = 65536
-    content = b''
+        def __call__(self, x):
+        regularization = 0.
+        if self.l1:
+            regularization += K.sum(self.l1 * K.abs(x))
+        if self.l2:
+            regularization += K.sum(self.l2 * K.square(x))
+        return regularization
     
-        # Client Error.
-    400: ('bad_request', 'bad'),
-    401: ('unauthorized',),
-    402: ('payment_required', 'payment'),
-    403: ('forbidden',),
-    404: ('not_found', '-o-'),
-    405: ('method_not_allowed', 'not_allowed'),
-    406: ('not_acceptable',),
-    407: ('proxy_authentication_required', 'proxy_auth', 'proxy_authentication'),
-    408: ('request_timeout', 'timeout'),
-    409: ('conflict',),
-    410: ('gone',),
-    411: ('length_required',),
-    412: ('precondition_failed', 'precondition'),
-    413: ('request_entity_too_large',),
-    414: ('request_uri_too_large',),
-    415: ('unsupported_media_type', 'unsupported_media', 'media_type'),
-    416: ('requested_range_not_satisfiable', 'requested_range', 'range_not_satisfiable'),
-    417: ('expectation_failed',),
-    418: ('im_a_teapot', 'teapot', 'i_am_a_teapot'),
-    421: ('misdirected_request',),
-    422: ('unprocessable_entity', 'unprocessable'),
-    423: ('locked',),
-    424: ('failed_dependency', 'dependency'),
-    425: ('unordered_collection', 'unordered'),
-    426: ('upgrade_required', 'upgrade'),
-    428: ('precondition_required', 'precondition'),
-    429: ('too_many_requests', 'too_many'),
-    431: ('header_fields_too_large', 'fields_too_large'),
-    444: ('no_response', 'none'),
-    449: ('retry_with', 'retry'),
-    450: ('blocked_by_windows_parental_controls', 'parental_controls'),
-    451: ('unavailable_for_legal_reasons', 'legal_reasons'),
-    499: ('client_closed_request',),
-    
-    EXAMPLES = '''
-  - name: create web ACL
-    aws_waf_web_acl:
-      name: my_web_acl
-      rules:
-        - name: my_rule
-          priority: 1
-          action: block
-      default_action: block
-      purge_rules: yes
-      state: present
-    
-    
-class CloudFrontInvalidationValidationManager(object):
+        # Arguments
+        model: target model for the conversion.
     '''
-    Manages Cloudfront validations for invalidation batches
-    '''
-    
-        def _sync_rule(self, enabled=True):
-        '''Syncs local rule state with AWS'''
-        if not self._rule_matches_aws():
-            self.rule.put(enabled)
-    
-    
-def describe_log_group(client, log_group_name, module):
-    params = {}
-    if log_group_name:
-        params['logGroupNamePrefix'] = log_group_name
-    try:
-        desc_log_group = client.describe_log_groups(**params)
-        return desc_log_group
-    except botocore.exceptions.ClientError as e:
-        module.fail_json(msg='Unable to describe log group {0}: {1}'.format(log_group_name, to_native(e)),
-                         exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
-    except botocore.exceptions.BotoCoreError as e:
-        module.fail_json(msg='Unable to describe log group {0}: {1}'.format(log_group_name, to_native(e)),
-                         exception=traceback.format_exc())
-    
-        if pipeline_field(client, dp_id, field='@pipelineState') in DP_INACTIVE_STATES:
-        changed = False
-    else:
-        client.deactivate_pipeline(pipelineId=dp_id)
-        try:
-            pipeline_status_timeout(client, dp_id, status=DP_INACTIVE_STATES,
-                                    timeout=timeout)
-        except TimeOutException:
-            module.fail_json(msg=('Data Pipeline {0} failed to deactivate'
-                                  'within timeout {1} seconds').format(dp_name, timeout))
-        changed = True
+    # Note: SeparableConvolution not included
+    # since only supported by TF.
+    conv_classes = {
+        'Conv1D',
+        'Conv2D',
+        'Conv3D',
+        'Conv2DTranspose',
+    }
+    to_assign = []
+    for layer in model.layers:
+        if layer.__class__.__name__ in conv_classes:
+            original_kernel = K.get_value(layer.kernel)
+            converted_kernel = convert_kernel(original_kernel)
+            to_assign.append((layer.kernel, converted_kernel))
+    K.batch_set_value(to_assign)
     
     
-if __name__ == '__main__':
-    main()
-
+def test_categorical_hinge():
+    y_pred = K.variable(np.array([[0.3, 0.2, 0.1],
+                                  [0.1, 0.2, 0.7]]))
+    y_true = K.variable(np.array([[0, 1, 0],
+                                  [1, 0, 0]]))
+    expected_loss = ((0.3 - 0.2 + 1) + (0.7 - 0.1 + 1)) / 2.0
+    loss = K.eval(losses.categorical_hinge(y_true, y_pred))
+    assert np.isclose(expected_loss, np.mean(loss))
     
-    '''
+        return _postprocess_conv3d_output(x, data_format)
     
-        if assign_public_ip is not None:
-        launch_config['AssociatePublicIpAddress'] = assign_public_ip
-    
-        # If we're just listing volumes then do nothing, else get the latest update for the volume
-    if state != 'list':
-        volume.update()
-    
-            X = np.random.randn(n_samples, n_features)
-        Y = np.random.randn(n_samples)
-    
-        Returns
-    -------
-    array of floats shaped like (metrics, formats, samples, classes, density)
-        Time in seconds.
-    '''
-    metrics = np.atleast_1d(metrics)
-    samples = np.atleast_1d(samples)
-    classes = np.atleast_1d(classes)
-    density = np.atleast_1d(density)
-    formats = np.atleast_1d(formats)
-    out = np.zeros((len(metrics), len(formats), len(samples), len(classes),
-                    len(density)), dtype=float)
-    it = itertools.product(samples, classes, density)
-    for i, (s, c, d) in enumerate(it):
-        _, y_true = make_multilabel_classification(n_samples=s, n_features=1,
-                                                   n_classes=c, n_labels=d * c,
-                                                   random_state=42)
-        _, y_pred = make_multilabel_classification(n_samples=s, n_features=1,
-                                                   n_classes=c, n_labels=d * c,
-                                                   random_state=84)
-        for j, f in enumerate(formats):
-            f_true = f(y_true)
-            f_pred = f(y_pred)
-            for k, metric in enumerate(metrics):
-                t = timeit(partial(metric, f_true, f_pred), number=n_times)
-    
-                gc.collect()
-            print('benchmarking lasso_path (without Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            lasso_path(X, y, precompute=False)
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            results['lasso_path (without Gram)'].append(delta)
-    
-            tick_vals = []
-        tick_labels = []
+            # create folders and subfolders
+        paths = []
+        for cl in range(num_classes):
+            class_directory = 'class-{}'.format(cl)
+            classpaths = [
+                class_directory,
+                os.path.join(class_directory, 'subfolder-1'),
+                os.path.join(class_directory, 'subfolder-2'),
+                os.path.join(class_directory, 'subfolder-1', 'sub-subfolder')
+            ]
+            for path in classpaths:
+                tmpdir.join(path).mkdir()
+            paths.append(classpaths)
     
     
-def rbf_kernels(X, n_jobs):
-    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
+def render_function(function, method=True):
+    subblocks = []
+    signature = get_function_signature(function, method=method)
+    if method:
+        signature = signature.replace(
+            clean_module_name(function.__module__) + '.', '')
+    subblocks.append('### ' + function.__name__ + '\n')
+    subblocks.append(code_snippet(signature))
+    docstring = function.__doc__
+    if docstring:
+        subblocks.append(process_docstring(docstring))
+    return '\n\n'.join(subblocks)
     
-        ratio = n_samples / opts.n_population
+        def report_out_of_quota(self, appid):
+        self.logger.warn('report_out_of_quota:%s', appid)
+        with self.lock:
+            if appid not in self.out_of_quota_appids:
+                self.out_of_quota_appids.append(appid)
+            try:
+                self.working_appid_list.remove(appid)
+            except:
+                pass
     
-    from __future__ import print_function
+    def test():
+    logging.basicConfig(level=logging.DEBUG)
+    blank_rs_packet = bytearray(
+        b'\x00\x01\x00\x00\x8a\xde\xb0\xd0\x2e\xea\x0b\xfc\x00'
+        b'\x60\x00\x00\x00\x00\x08\x3a\xff\xfe\x80\x00\x00\x00\x00\x00\x00'
+        b'\x00\x00\xff\xff\xff\xff\xff\xff\xff\x02\x00\x00\x00\x00\x00\x00'
+        b'\x00\x00\x00\x00\x00\x00\x00\x02\x85\x00\x7d\x37\x00\x00\x00\x00')
+    nonce = creat_rs_nonce()
+    blank_rs_packet[4:12] = nonce
+    assert(teredo_rs_packet(nonce).type_restricted == bytes(blank_rs_packet))
     
-        revision is a git commit reference (hash or name)
+              1. wipe out any insertions before op within that range.
+          2. Drop any replace op before that is contained completely within
+             that range.
+          3. Throw exception upon boundary overlap with any previous replace.
     
-      Args:
-    filename: The name of the current file.
-    clean_lines: A CleansedLines instance containing the file.
-    linenum: The number of the line to check.
-    file_extension: The extension (without the dot) of the filename.
-    include_state: An _IncludeState instance in which the headers are inserted.
-    nesting_state: A NestingState instance which maintains information about
-                   the current stack of nested blocks being parsed.
-    error: The function to call with any errors found.
-  '''
-  # If the line is empty or consists of entirely a comment, no need to
-  # check it.
-  line = clean_lines.elided[linenum]
-  if not line:
-    return
+      Call CheckNextIncludeOrder() once for each header in the file, passing
+  in the type constants defined above. Calls in an illegal order will
+  raise an _IncludeError with an appropriate error message.
