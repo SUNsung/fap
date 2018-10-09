a239e7985e4b5b5fe6c8bb967b4a7ec9f24a2377
SUNsung <weatherius@gmail.com>
2018-10-09 21:50:22 +0300

    Rewrite [32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb] File in: 2018-10-09 21:50:21

--- ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
+++ ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
@@ -1,152 +1,125 @@
 
         
-        BASE_URI = ENV['BASE_URI'] || 'https://github.com/jondot/awesome-react-native'
-    
-      protected
-    
-            if options[:bypass]
-          ActiveSupport::Deprecation.warn(<<-DEPRECATION.strip_heredoc, caller)
-          [Devise] bypass option is deprecated and it will be removed in future version of Devise.
-          Please use bypass_sign_in method instead.
-          Example:
-    
-          def _devise_route_context
-        @_devise_route_context ||= send(Devise.available_router_name)
+                case array.length
+        when 0
+          ''.html_safe
+        when 1
+          ERB::Util.html_escape(array[0])
+        when 2
+          safe_join([array[0], array[1]], options[:two_words_connector])
+        else
+          safe_join([safe_join(array[0...-1], options[:words_connector]), options[:last_word_connector], array[-1]], nil)
+        end
       end
     end
   end
 end
 
     
-        if record.timedout?(last_request_at) &&
-        !env['devise.skip_timeout'] &&
-        !proxy.remember_me_is_active?(record)
-      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
-      throw :warden, scope: scope, message: :timeout
-    end
+            private
     
-      private
-    
-      def command
-    abort 'This command requires a command argument' if ARGV.empty?
-    
-        if ARGV.named.empty?
-      slow_checks = %w[
-        check_for_broken_symlinks
-        check_missing_deps
-      ]
-      methods = (checks.all.sort - slow_checks) + slow_checks
-    else
-      methods = ARGV.named
-    end
-    
-    require 'formula'
-require 'system_config'
-require 'stringio'
-require 'socket'
-    
-        ff.each do |f|
-      missing = f.missing_dependencies(hide: ARGV.values('hide'))
-      next if missing.empty?
-    
-      #
-  # Returns a hash of variables that contain information about the request,
-  # such as the remote host information.
-  #
-  # TODO
-  #
-  def meta_vars
-  end
-    
-        head + [data.length].pack('v') + data
-  end
-    
-            end
-      end
-    end
-  end
-end
-
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an
-          # OpenSSL::ASN1::Sequence
-          #
-          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
-          def decode_asn1(input)
-            seq_values = input.value
-            self.type = decode_type(seq_values[0])
-            self.value = decode_value(seq_values[1])
-          end
-    
-              # Decodes the rtime field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_rtime(input)
-            input.value[0].value
-          end
-    
-              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_pvno(input)
-            input.value[0].value.to_i
-          end
-    
-    
-    {        def right_whole_line_range(loc_end)
-          if range_by_whole_lines(loc_end).source.strip =~ /}\s*,?\z/
-            range_by_whole_lines(loc_end, include_final_newline: true)
+          def typecast(paths)
+        paths.map do |path|
+          case path
+          when Pathname, String
+            OptimizedFileSystemResolver.new path.to_s
           else
-            loc_end
+            path
           end
         end
-    
-    RSpec.describe RuboCop::Cop::Layout::MultilineHashBraceLayout, :config do
-  subject(:cop) { described_class.new(config) }
-    
-        context 'opening brace on separate line from first element' do
-      it 'allows closing brace on same line as last element' do
-        expect_no_offenses(construct(true, false))
       end
-    
-          it 'does not autocorrect the closing brace' do
-        new_source = autocorrect_source(source)
-        expect(new_source).to eq([source].join($RS))
-      end
-    end
-    
-        context 'when using safe navigation operator' do
-      let(:ruby_version) { 2.3 }
-    
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
-      end
-    
-    
-    {      # Checks whether the `hash` literal is delimited by curly braces.
-      #
-      # @return [Boolean] whether the `hash` literal is enclosed in braces
-      def braces?
-        loc.end && loc.end.is?('}')
-      end
-    end
   end
 end
 
     
-    module RuboCop
-  module AST
-    # A node extension for `kwsplat` nodes. This will be used in place of a
-    # plain  node when the builder constructs the AST, making its methods
-    # available to all `kwsplat` nodes within RuboCop.
-    class KeywordSplatNode < Node
-      include HashElementNode
+      def key?(key)
+    @checksums.key?(key)
+  end
+    
+      def describe_x11
+    return 'N/A' unless MacOS::XQuartz.installed?
+    '#{MacOS::XQuartz.version} => #{describe_path(MacOS::XQuartz.prefix)}'
+  end
+    
+        first_warning = true
+    methods.each do |method|
+      unless checks.respond_to?(method)
+        Homebrew.failed = true
+        puts 'No check available by the name: #{method}'
+        next
+      end
+    
+            # Using the LDAP attributes configuration, find and return the first
+        # attribute with a value. For example, by default, when given 'email',
+        # this method looks for 'mail', 'email' and 'userPrincipalName' and
+        # returns the first with a value.
+        def attribute_value(attribute)
+          attributes = Array(config.attributes[attribute.to_s])
+          selected_attr = attributes.find { |attr| entry.respond_to?(attr) }
+    
+    module Gitlab
+  module BackgroundMigration
+    class PopulateMergeRequestsLatestMergeRequestDiffId
+      BATCH_SIZE = 1_000
+    
+            def entity
+          'pipeline'
+        end
+    
+            def key_width
+          62
+        end
+    
+        def generate(site)
+      site.write_category_indexes
+    end
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
+      end
+    end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
+    
+        def render(context)
+      includes_dir = File.join(context.registers[:site].source, '_includes')
+    
+    end
+    
+      require ARGV.shift
+  exit_status = LogStash::Runner.run('bin/logstash', ARGV)
+  exit(exit_status || 0)
+end
+
+    
+        class Main < Clamp::Command
+      subcommand 'list', 'List all installed Logstash plugins', LogStash::PluginManager::List
+      subcommand 'install', 'Install a Logstash plugin', LogStash::PluginManager::Install
+      subcommand 'remove', 'Remove a Logstash plugin', LogStash::PluginManager::Remove
+      subcommand 'update', 'Update a plugin', LogStash::PluginManager::Update
+      subcommand 'pack', 'Package currently installed plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Pack
+      subcommand 'unpack', 'Unpack packaged plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Unpack
+      subcommand 'generate', 'Create the foundation for a new plugin', LogStash::PluginManager::Generate
+      subcommand 'uninstall', 'Uninstall a plugin. Deprecated: Please use remove instead', LogStash::PluginManager::Remove
+      subcommand 'prepare-offline-pack', 'Create an archive of specified plugins to use for offline installation', LogStash::PluginManager::PrepareOfflinePack
+    end
+  end
+end
+    
+          # Try to add the gems to the current gemfile and lock file, if successful
+      # both of them will be updated. This injector is similar to Bundler's own injector class
+      # minus the support for additionals source and doing local resolution only.
+      ::Bundler::LogstashInjector.inject!(pack)
+    
+    class LogStash::PluginManager::Unpack < LogStash::PluginManager::PackCommand
+  option '--tgz', :flag, 'unpack a packaged tar.gz file', :default => !LogStash::Environment.windows?
+  option '--zip', :flag, 'unpack a packaged  zip file', :default => LogStash::Environment.windows?
