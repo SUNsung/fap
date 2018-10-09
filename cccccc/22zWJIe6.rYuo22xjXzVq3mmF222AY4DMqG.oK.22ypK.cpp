195f00c1b441a3f98f651777cd6410101e2f5735
SUNsung <weatherius@gmail.com>
2018-10-10 01:33:17 +0300

    Rewrite [22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp] File in: 2018-10-10 01:33:17

--- cccccc/22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp
+++ cccccc/22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp
@@ -1,236 +1,247 @@
 
         
-          virtual string Code();
+        #ifndef CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+#define CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
     
-    REGISTER_OP('RefOutput')
-    .Output('a: Ref(int32)')
-    .SetShapeFn(shape_inference::UnknownShape);
-    
-    namespace tensorflow {
+    void Menu::Call(const std::string& method,
+                const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  if (method == 'Append') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    Append(object_manager()->GetApiObject<MenuItem>(object_id));
+  } else if (method == 'Insert') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Remove') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Popup') {
+    int x = 0;
+    arguments.GetInteger(0, &x);
+    int y = 0;
+    arguments.GetInteger(1, &y);
+    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
+    DCHECK(web_contents);
+    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
+    }
     }
     
-    #endif  // TENSORFLOW_PYTHON_LIB_CORE_PY_FUNC_H_
-
+     private:
+  ObjectManager* object_manager_;
     
-      // Attempt to get the next record at 'current_offset()'. Populates status
-  // with OK on success, OUT_OF_RANGE for end of file, DATA_LOSS for some
-  // kinds of truncated reads, or another code for other errors
-  // (e.g., filesystem errors).
-  void GetNext(TF_Status* status);
-    
-    ScopedActivateExecutorContext::ScopedActivateExecutorContext(
-    CUDAExecutor *cuda_exec):
-      driver_scoped_activate_context_(
-          new ScopedActivateContext{ExtractCudaContext(cuda_exec)}) { }
-    
-    #endif  // ATOM_APP_COMMAND_LINE_ARGS_H_
-
-    
-    namespace mate {
+        bool ReadRTF(ClipboardData& data) {
+      DCHECK(data.type == TYPE_RTF);
+      std::string text;
+      clipboard_->ReadRTF(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
+      data.data.reset(new std::string(text));
+      return true;
     }
     
-    namespace api {
+    bool NwObjCallObjectMethodFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
     }
     
-    int Menu::GetIndexOfCommandId(int command_id) {
-  return model_->GetIndexOfCommandId(command_id);
+    #if GTEST_OS_SYMBIAN
+  // These are needed as the Nokia Symbian Compiler cannot decide between
+  // const T& and const T* in a function template. The Nokia compiler _can_
+  // decide between class template specializations for T and T*, so a
+  // tr1::type_traits-like is_pointer works, and we can overload on that.
+  template <typename T>
+  inline void StreamHelper(internal::true_type /*is_pointer*/, T* pointer) {
+    if (pointer == NULL) {
+      *ss_ << '(null)';
+    } else {
+      *ss_ << pointer;
+    }
+  }
+  template <typename T>
+  inline void StreamHelper(internal::false_type /*is_pointer*/,
+                           const T& value) {
+    // See the comments in Message& operator <<(const T&) above for why
+    // we need this using statement.
+    using ::operator <<;
+    *ss_ << value;
+  }
+#endif  // GTEST_OS_SYMBIAN
+    
+    // Values() allows generating tests from explicitly specified list of
+// parameters.
+//
+// Synopsis:
+// Values(T v1, T v2, ..., T vN)
+//   - returns a generator producing sequences with elements v1, v2, ..., vN.
+//
+// For example, this instantiates tests from test case BarTest each
+// with values 'one', 'two', and 'three':
+//
+// INSTANTIATE_TEST_CASE_P(NumSequence, BarTest, Values('one', 'two', 'three'));
+//
+// This instantiates tests from test case BazTest each with values 1, 2, 3.5.
+// The exact type of values will depend on the type of parameter in BazTest.
+//
+// INSTANTIATE_TEST_CASE_P(FloatingNumbers, BazTest, Values(1, 2, 3.5));
+//
+// Currently, Values() supports from 1 to $n parameters.
+//
+$range i 1..n
+$for i [[
+$range j 1..i
+    
+    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
+    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
+    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
+    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
+    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
+    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17, GTEST_TEMPLATE_ T18,
+    GTEST_TEMPLATE_ T19, GTEST_TEMPLATE_ T20, GTEST_TEMPLATE_ T21,
+    GTEST_TEMPLATE_ T22>
+struct Templates22 {
+  typedef TemplateSel<T1> Head;
+  typedef Templates21<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+      T15, T16, T17, T18, T19, T20, T21, T22> Tail;
+};
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    void Assembler::sradi(const Reg64& ra, const Reg64& rs, uint8_t sh, bool rc) {
+  EmitXSForm(31, rn(rs), rn(ra), sh, 413, rc);
 }
     
-    class Notification : public mate::TrackableObject<Notification>,
-                     public brightray::NotificationDelegate {
- public:
-  static mate::WrappableBase* New(mate::Arguments* args);
-  static bool IsSupported();
-    }
+    inline TypedValue* APCLocalArray::localCache() const {
+  return const_cast<TypedValue*>(
+    reinterpret_cast<const TypedValue*>(this + 1)
+  );
+}
     
-    /** Macintosh-specific dock icon handler.
+    /**
+ * Parts of the language can individually be made stricter, warning or
+ * erroring when there's dangerous/unintuive usage; for example,
+ * array_fill_keys() with non-int/string keys: Hack.Lang.StrictArrayFillKeys
  */
-class MacDockIconHandler : public QObject
-{
-    Q_OBJECT
-    }
-    
-        explicit reverse_lock(Lock& _lock) : lock(_lock) {
-        _lock.unlock();
-        _lock.swap(templock);
-    }
+enum class HackStrictOption {
+  OFF, // PHP5 behavior
+  WARN,
+  ON
+};
     
     
-static void secp256k1_gej_add_ge(secp256k1_gej *r, const secp256k1_gej *a, const secp256k1_ge *b) {
-    /* Operations: 7 mul, 5 sqr, 4 normalize, 21 mul_int/add/negate/cmov */
-    static const secp256k1_fe fe_1 = SECP256K1_FE_CONST(0, 0, 0, 0, 0, 0, 0, 1);
-    secp256k1_fe zz, u1, u2, s1, s2, t, tt, m, n, q, rr;
-    secp256k1_fe m_alt, rr_alt;
-    int infinity, degenerate;
-    VERIFY_CHECK(!b->infinity);
-    VERIFY_CHECK(a->infinity == 0 || a->infinity == 1);
-    }
-    
-    #define Ch(x,y,z) ((z) ^ ((x) & ((y) ^ (z))))
-#define Maj(x,y,z) (((x) & (y)) | ((z) & ((x) | (y))))
-#define Sigma0(x) (((x) >> 2 | (x) << 30) ^ ((x) >> 13 | (x) << 19) ^ ((x) >> 22 | (x) << 10))
-#define Sigma1(x) (((x) >> 6 | (x) << 26) ^ ((x) >> 11 | (x) << 21) ^ ((x) >> 25 | (x) << 7))
-#define sigma0(x) (((x) >> 7 | (x) << 25) ^ ((x) >> 18 | (x) << 14) ^ ((x) >> 3))
-#define sigma1(x) (((x) >> 17 | (x) << 15) ^ ((x) >> 19 | (x) << 13) ^ ((x) >> 10))
-    
-    BOOST_AUTO_TEST_CASE(get_difficulty_for_very_high_target)
-{
-    TestDifficulty(0x12345678, 5913134931067755359633408.0);
+    {///////////////////////////////////////////////////////////////////////////////
 }
     
-        // Other valid inputs
-    CheckParseTorReplyMapping(
-        'Foo=Bar=Baz Spam=Eggs', {
-            {'Foo', 'Bar=Baz'},
-            {'Spam', 'Eggs'},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar=Baz\'', {
-            {'Foo', 'Bar=Baz'},
-        });
-    CheckParseTorReplyMapping(
-        'Foo=\'Bar Baz\'', {
-            {'Foo', 'Bar Baz'},
-        });
+    const StaticString
+  s_wrapper_type('wrapper_type'),
+  s_stream_type('stream_type'),
+  s_mode('mode'),
+  s_unread_bytes('unread_bytes'),
+  s_seekable('seekable'),
+  s_timed_out('timed_out'),
+  s_blocked('blocked'),
+  s_eof('eof'),
+  s_plainfile('plainfile'),
+  s_dir('dir'),
+  s_r('r');
     
-    size_t num_threads = 31;
-size_t work_chunk  = 120;
+    /**
+ * Make dest directory look identical to src by copying files and directories,
+ * without copying identical files (so they keep the same timestamp as before).
+ */
+void syncdir(const std::string &dest, const std::string &src,
+             bool keepSrc = false);
     
-    void Assembler::andi(const Reg64& ra, const Reg64& rs, Immed imm) {
-  assert(imm.fits(HPHP::sz::word) && 'Immediate is too big');
-  EmitDForm(28, rn(rs), rn(ra), imm.w());
+    Array File::getMetaData() {
+  return make_map_array(
+    s_wrapper_type, getWrapperType(),
+    s_stream_type,  getStreamType(),
+    s_mode,         String(m_data->m_mode),
+    s_unread_bytes, 0,
+    s_seekable,     seekable(),
+    s_uri,          String(m_data->m_name),
+    s_timed_out,    false,
+    s_blocked,      true,
+    s_eof,          eof(),
+    s_wrapper_data, getWrapperMetaData()
+  );
 }
     
-    namespace HPHP {
+     private:
+  /*
+   * @brief A helper function to 'carve' files from disk
+   *
+   * This function performs a 'forensic carve' of a specified path to the
+   * users tmp directory
+   */
+  Status carve(const boost::filesystem::path& path);
+    
+      auto val4 = splayValue(100, 1);
+  EXPECT_GE(val4, 99U);
+  EXPECT_LE(val4, 101U);
+    
+    std::shared_ptr<PlatformProcess> PlatformProcess::launchTestPythonScript(
+    const std::string& args) {
+  std::string osquery_path;
+  auto osquery_path_option = getEnvVar('OSQUERY_DEPS');
+  if (osquery_path_option) {
+    osquery_path = *osquery_path_option;
+  } else {
+    if (!isPlatform(PlatformType::TYPE_FREEBSD)) {
+      osquery_path = '/usr/local/osquery';
+    } else {
+      osquery_path = '/usr/local';
     }
-    
-    #include <memory>
-    
-          if (section != ini_section) {
-        ini_section = section;
-        write(ini_fd, '[', 1);
-        write(ini_fd, section.c_str(), section.length());
-        write(ini_fd, ']\n', 2);
-      }
-    
-        size_t NDMask::MaskedCount() const
-    {
-        auto maskMatrix = GetMatrix();
-        std::unique_ptr<char[]> maskData(maskMatrix->CopyToArray());
-        return std::count_if(maskData.get(), maskData.get() + maskMatrix->GetNumElements(), [](const char& val) {
-            return val == (char)MaskKind::Invalid;
-        });
-    }
-    
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-#include 'Utils.h'
-    
-                // Validate that each of the dynamic axes are unique
-            std::unordered_set<Axis> uniqueDynamicAxis;
-            for (auto& currentDynamicAxis : dynamicAxes)
-            {
-                auto retVal = uniqueDynamicAxis.insert(currentDynamicAxis);
-                if (!retVal.second)
-                    InvalidArgument('Dynamic axis named %S is specified more than once for Variable '%S'', currentDynamicAxis.Name().c_str(), AsString().c_str());
-            }
-    
-    // Destroy - cleanup and remove this class
-// NOTE: this destroys the object, and it can't be used past this point
-void DataReader::Destroy()
-{
-    // newer code that explicitly place multiple streams for inputs
-    foreach_index (i, m_ioNames) // inputNames should map to node names
-    {
-        m_dataReaders[m_ioNames[i]]->Destroy();
-    }
-}
-    
-        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange&) override
-    {
-        RuntimeError('%ls is a special node only to be used as input to the Input() node.', NodeDescription().c_str());
-    }
-    
-    #ifndef ROCKSDB_VALGRIND_RUN
-INSTANTIATE_TEST_CASE_P(
-    DBTestCompactionFilterWithCompactOption,
-    DBTestCompactionFilterWithCompactParam,
-    ::testing::Values(DBTestBase::OptionConfig::kDefault,
-                      DBTestBase::OptionConfig::kUniversalCompaction,
-                      DBTestBase::OptionConfig::kUniversalCompactionMultiLevel,
-                      DBTestBase::OptionConfig::kLevelSubcompactions,
-                      DBTestBase::OptionConfig::kUniversalSubcompactions));
-#else
-// Run fewer cases in valgrind
-INSTANTIATE_TEST_CASE_P(DBTestCompactionFilterWithCompactOption,
-                        DBTestCompactionFilterWithCompactParam,
-                        ::testing::Values(DBTestBase::OptionConfig::kDefault));
-#endif  // ROCKSDB_VALGRIND_RUN
-    
-    
-    {  env_->SetBackgroundThreads(1, Env::LOW);
-  env_->SetBackgroundThreads(1, Env::HIGH);
-}
-    
-      void set_max_delayed_write_rate(uint64_t write_rate) {
-    // avoid divide 0
-    if (write_rate == 0) {
-      write_rate = 1u;
-    }
-    max_delayed_write_rate_ = write_rate;
-    // update delayed_write_rate_ as well
-    delayed_write_rate_ = write_rate;
   }
-    
-    Status PosixWritableFile::Fsync() {
-  if (fsync(fd_) < 0) {
-    return IOError('While fsync', filename_, errno);
-  }
-  return Status::OK();
-}
-    
-    namespace aria2 {
     }
     
-      void setTaskQueue(DHTTaskQueue* taskQueue);
+      /// Setter for an extension process.
+  void setExtension(const std::string& extension,
+                    const std::shared_ptr<PlatformProcess>& child);
     
-    #define READ_CHECK(fp, ptr, count)                                             \
-  if (fp.read((ptr), (count)) != (count)) {                                    \
-    throw DL_ABORT_EX('Failed to load DHT routing table.');                    \
-  }
     
-    std::pair<std::vector<std::unique_ptr<Command>>,
-          std::vector<std::unique_ptr<Command>>>
-DHTSetup::setup(DownloadEngine* e, int family)
-{
-  std::vector<std::unique_ptr<Command>> tempCommands;
-  std::vector<std::unique_ptr<Command>> tempRoutineCommands;
-  if ((family != AF_INET && family != AF_INET6) ||
-      (family == AF_INET && DHTRegistry::isInitialized()) ||
-      (family == AF_INET6 && DHTRegistry::isInitialized6())) {
-    return {};
-  }
-  try {
-    // load routing table and localnode id here
-    std::shared_ptr<DHTNode> localNode;
-    }
-    }
-    
-      void setCommonProperty(const std::shared_ptr<DHTAbstractTask>& task);
-    
-    void DHTTaskQueueImpl::addImmediateTask(const std::shared_ptr<DHTTask>& task)
-{
-  immediateTaskQueue_.addTask(task);
+    {  auto subscription = Subscription::create('TestSubscriber', mc);
+  auto status = EventFactory::addSubscription('inotify', subscription);
+  EXPECT_TRUE(status.ok());
+  EventFactory::deregisterEventPublisher('inotify');
 }
     
-    void DNSCache::markBad(const std::string& hostname, const std::string& ipaddr,
-                       uint16_t port)
-{
-  auto target = std::make_shared<CacheEntry>(hostname, port);
-  auto i = entries_.find(target);
-  if (i != entries_.end()) {
-    (*i)->markBad(ipaddr);
+    DEFINE_bool(benchmark_report_aggregates_only, false,
+            'Report the result of each benchmark repetitions. When 'true' is '
+            'specified only the mean, standard deviation, and other statistics '
+            'are reported for repeated benchmarks.');
+    
+    bool SameNames(UserCounters const& l, UserCounters const& r) {
+  if (&l == &r) return true;
+  if (l.size() != r.size()) {
+    return false;
   }
+  for (auto const& c : l) {
+    if (r.find(c.first) == r.end()) {
+      return false;
+    }
+  }
+  return true;
 }
     
-    #include 'common.h'
+    
+    {  // Print user counters
+  for (const auto &ucn : user_counter_names_) {
+    auto it = run.counters.find(ucn);
+    if(it == run.counters.end()) {
+      Out << ',';
+    } else {
+      Out << ',' << it->second;
+    }
+  }
+  Out << '\n';
+}
