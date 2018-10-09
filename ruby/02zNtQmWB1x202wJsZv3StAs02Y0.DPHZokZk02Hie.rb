d72df7e3f5a3be0879e02457c181a7b75fbacedb
SUNsung <weatherius@gmail.com>
2018-10-09 04:30:20 +0300

    Rewrite [02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb] File in: 2018-10-09 04:30:20

--- ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
+++ ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
@@ -1,26 +1,125 @@
 
         
-              private
+            group.add(moderator)
+    group.save
     
-            def template
-          @template ||= Coverage::Template.new(self)
+        def relative_url_string?(str)
+      str !~ SCHEME_RGX && !fragment_url_string?(str) && !data_url_string?(str)
+    end
+    
+        alias_method :insert_before, :insert
+    
+        def add(path, content)
+      @pages[path] = content
+    end
+    
+        def initialize(content)
+      @content = content
+      @html = document? ? parse_as_document : parse_as_fragment
+    end
+    
+        def self.run(urls, options = {}, &block)
+      urls = urls.dup
+      requester = new(options)
+      requester.on_response(&block) if block
+      requester.on_response do # cheap hack to ensure root page is processed first
+        if urls
+          requester.request(urls)
+          urls = nil
+        end
+      end
+      requester.request(urls.shift)
+      requester.run
+      requester
+    end
+    
+            css('br', 'hr', '.material-icons', '.header-link', '.breadcrumb').remove
+    
+        # This returns whether the guest is ready to work. If this returns
+    # `false`, then {#detect!} should be called in order to detect the
+    # guest OS.
+    #
+    # @return [Boolean]
+    def ready?
+      !!capability_host_chain
+    end
+  end
+end
+
+    
+                    if provider_to_use && provider_to_use != active_provider
+                  # We found an active machine with a provider that doesn't
+                  # match the requested provider. Show an error.
+                  raise Errors::ActiveMachineWithDifferentProvider,
+                    name: active_name.to_s,
+                    active_provider: active_provider.to_s,
+                    requested_provider: provider_to_use.to_s
+                else
+                  # Use this provider and exit out of the loop. One of the
+                  # invariants [for now] is that there shouldn't be machines
+                  # with multiple providers.
+                  @logger.info('Active machine found with name #{active_name}. ' +
+                               'Using provider: #{active_provider}')
+                  provider_to_use = active_provider
+                  break
+                end
+              end
+            end
+    
+            # This clears out all the registered plugins. This is only used by
+        # unit tests and should not be called directly.
+        def reset!
+          @registered.clear
         end
     
-    # It is very likely that we'll need these and as some of those paths will atm
-# result in a I18n deprecation warning, we load those here now so that we can
-# get rid of that warning.
-require 'active_support/core_ext/string/strip'
-require 'active_support/core_ext/string/inflections'
-require 'active_support/core_ext/array/conversions'
-# TODO: check what this actually does by the time we're going to add support for
-# other locales.
-require 'i18n'
-if I18n.respond_to?(:enforce_available_locales=)
-  I18n.enforce_available_locales = false
-end
+      def reject(&block)
+    self.class.new(@paths.reject(&block))
+  end
     
-          def actual_path
-        $PROGRAM_NAME
+    require 'sass/logger'
+require 'sass/util'
+    
+        # @comment
+    #   rubocop:disable MethodLength
+    def parse_import_arg(scanner, offset)
+      return if scanner.eos?
+    
+          # Creates a new filesystem importer that imports files relative to a given path.
+      #
+      # @param root [String] The root path.
+      #   This importer will import files relative to this path.
+      def initialize(root)
+        @root = File.expand_path(root)
+        @real_root = Sass::Util.realpath(@root).to_s
+        @same_name_warnings = Set.new
+      end
+    
+          if specs
+        title 'Running the specs'
+        sh 'bundle exec bacon #{specs('**/*')}'
+    
+    end
+    
+            # Prints the list of specs & pod cache dirs for a single pod name.
+        #
+        # This output is valid YAML so it can be parsed with 3rd party tools
+        #
+        # @param [Array<Hash>] cache_descriptors
+        #        The various infos about a pod cache. Keys are
+        #        :spec_file, :version, :release and :slug
+        #
+        def print_pod_cache_infos(pod_name, cache_descriptors)
+          UI.puts '#{pod_name}:'
+          cache_descriptors.each do |desc|
+            if @short_output
+              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
+            end
+            UI.puts('  - Version: #{desc[:version]}')
+            UI.puts('    Type:    #{pod_type(desc)}')
+            UI.puts('    Spec:    #{desc[:spec_file]}')
+            UI.puts('    Pod:     #{desc[:slug]}')
+          end
+        end
       end
     end
   end
