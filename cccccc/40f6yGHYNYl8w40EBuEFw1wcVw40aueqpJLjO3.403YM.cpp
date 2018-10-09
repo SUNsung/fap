ab03b46b739c52068b9b255ae1531cd9267ab24e
SUNsung <weatherius@gmail.com>
2018-10-09 22:22:46 +0300

    Rewrite [40f6yGHYNYl8w40EBuEFw1wcVw40aueqpJLjO3.403YM.cpp] File in: 2018-10-09 22:22:45

--- cccccc/40f6yGHYNYl8w40EBuEFw1wcVw40aueqpJLjO3.403YM.cpp
+++ cccccc/40f6yGHYNYl8w40EBuEFw1wcVw40aueqpJLjO3.403YM.cpp
@@ -1,249 +1,101 @@
 
         
-        public:
-  explicit Traversal(TypeWalker &walker) : Walker(walker) {}
-    
-    CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
-  llvm::SmallString<32> NameBuf(Name);
-  cache_attributes_t Attrs = {
-    CACHE_ATTRIBUTES_VERSION_2,
-    CBs.keyHashCB,
-    CBs.keyIsEqualCB,
-    nullptr,
-    CBs.keyDestroyCB,
-    CBs.valueReleaseCB,
-    nullptr,
-    nullptr,
-    CBs.UserData,
-    CBs.valueRetainCB,
-  };
+        namespace osquery {
     }
     
-    #undef VERB
-#undef DIRECTIONAL_PREPOSITION
-#undef PREPOSITION
-
+    const std::string kPrometheusParserRootKey('prometheus_targets');
     
-    void swift::printOpaquePrefixMap(raw_ostream &out, void *_root,
-                         void (*printNodeData)(raw_ostream &out, void *node)) {
-  auto root = reinterpret_cast<Node*>(_root);
-  if (!root) {
-    out << '(empty)\n';
-    return;
+      // This looks funky, because the parser is named 'options' and it claims
+  // ownership of a single top-level-key called 'options'.
+  const auto& doc = c.getParser('options')->getData().doc()['options'];
+    
+    
+    {
+    {    // Code should never reach this point
+    LOG(ERROR) << 'osqueryd could not start worker process';
+    Initializer::shutdown(EXIT_CATASTROPHIC);
+    return std::shared_ptr<PlatformProcess>();
   }
-  TreePrinter(out, *printNodeData).print(root, ChildKind::Root);
+  return std::make_shared<PlatformProcess>(worker_pid);
 }
     
-      if (localScratch.back() == 'y') {
-    // If the last letter is a 'y', try 'ies'.
-    localScratch.pop_back();
-    localScratch += 'ies';
-    if (allPropertyNames->contains(localScratch)) return true;
-  } else {
-    // Otherwise, add an 's' and try again.
-    localScratch += 's';
-    if (allPropertyNames->contains(localScratch)) return true;
+    TEST_F(ProcessTests, test_launchExtension) {
+  {
+    auto process =
+        PlatformProcess::launchExtension(kProcessTestExecPath.c_str(),
+                                         kExpectedExtensionArgs[3],
+                                         kExpectedExtensionArgs[5],
+                                         kExpectedExtensionArgs[7],
+                                         true);
+    EXPECT_NE(nullptr, process.get());
+    }
     }
     
-    unsigned swift::unicode::extractFirstUnicodeScalar(StringRef S) {
-  unsigned Scalar;
-  bool Result = extractFirstUnicodeScalarImpl(S, Scalar);
-  assert(Result && 'string does not consist of one Unicode scalar');
-  (void)Result;
-  return Scalar;
+    
+    {  // And of the column has constraints:
+  EXPECT_TRUE(cm['path'].notExistsOrMatches('some'));
+  EXPECT_FALSE(cm['path'].notExistsOrMatches('not_some'));
+  EXPECT_TRUE(cm['path'].exists());
+  EXPECT_TRUE(cm['path'].existsAndMatches('some'));
 }
     
-      if (auto subTypedef = type->getAs<clang::TypedefType>()) {
-    if (classifyTypedef(subTypedef->getDecl()))
-      return forTypedef(subTypedef->getDecl());
-    return forInvalid();
-  }
+      r.request = request;
+  r.results = results;
     
-    // If true, then AVX has been detected.
-bool SIMDDetect::avx_available_;
-bool SIMDDetect::avx2_available_;
-bool SIMDDetect::avx512F_available_;
-bool SIMDDetect::avx512BW_available_;
-// If true, then SSe4.1 has been detected.
-bool SIMDDetect::sse_available_;
+        // Store our identifier
+    io.Fonts->TexID = (ImTextureID)g_FontTexture;
     
-    struct BlobData {
-  BlobData() : blob(nullptr), choices(nullptr) {}
-  BlobData(int index, Tesseract* tess, const WERD_RES& word)
-    : blob(word.chopped_word->blobs[index]),
-      tesseract(tess),
-      choices(&(*word.ratings)(index, index)) {}
+    int32 ImGui_Marmalade_PointerButtonEventCallback(void* system_data, void* user_data)
+{
+    // pEvent->m_Button is of type s3ePointerButton and indicates which mouse
+    // button was pressed.  For touchscreen this should always have the value
+    // S3E_POINTER_BUTTON_SELECT
+    s3ePointerEvent* pEvent = (s3ePointerEvent*)system_data;
     }
     
-    // Main entry point for Paragraph Detection Algorithm.
-//
-// Given a set of equally spaced textlines (described by row_infos),
-// Split them into paragraphs.  See http://goto/paragraphstalk
-//
-// Output:
-//   row_owners - one pointer for each row, to the paragraph it belongs to.
-//   paragraphs - this is the actual list of PARA objects.
-//   models - the list of paragraph models referenced by the PARA objects.
-//            caller is responsible for deleting the models.
-void DetectParagraphs(int debug_level,
-                      GenericVector<RowInfo> *row_infos,
-                      GenericVector<PARA *> *row_owners,
-                      PARA_LIST *paragraphs,
-                      GenericVector<ParagraphModel *> *models);
+      // Run one instance of this benchmark concurrently in t threads.
+  Benchmark* Threads(int t);
     
-    // A rather hackish helper structure which can take any kind of parameter input
-// (defined by ParamType) and do a couple of common operations on them, like
-// comparisond or getting its value. It is used in the context of the
-// ParamsEditor as a bridge from the internal tesseract parameters to the
-// ones displayed by the ScrollView server.
-class ParamContent : public ELIST_LINK {
- public:
-  // Compare two VC objects by their name.
-  static int Compare(const void* v1, const void* v2);
-    }
+    // That gcc wants both of these prototypes seems mysterious. VC, for
+// its part, can't decide which to use (another mystery). Matching of
+// template overloads: the final frontier.
+#ifndef COMPILER_MSVC
+template <typename T, size_t N>
+char (&ArraySizeHelper(const T (&array)[N]))[N];
+#endif
     
-      size_t Read(void* dptr, size_t size) override {
-    size_t nbuffer = buffer_.length() - buffer_ptr_;
-    if (nbuffer == 0) return strm_->Read(dptr, size);
-    if (nbuffer < size) {
-      std::memcpy(dptr, dmlc::BeginPtr(buffer_) + buffer_ptr_, nbuffer);
-      buffer_ptr_ += nbuffer;
-      return nbuffer + strm_->Read(reinterpret_cast<char*>(dptr) + nbuffer,
-                                   size - nbuffer);
-    } else {
-      std::memcpy(dptr, dmlc::BeginPtr(buffer_) + buffer_ptr_, size);
-      buffer_ptr_ += size;
-      return size;
+    
+    {
+    {      // See how much iterations should be increased by
+      // Note: Avoid division by zero with max(seconds, 1ns).
+      double multiplier = min_time * 1.4 / std::max(seconds, 1e-9);
+      // If our last run was at least 10% of FLAGS_benchmark_min_time then we
+      // use the multiplier directly. Otherwise we use at most 10 times
+      // expansion.
+      // NOTE: When the last run was at least 10% of the min time the max
+      // expansion should be 14x.
+      bool is_significant = (seconds / min_time) > 0.1;
+      multiplier = is_significant ? multiplier : std::min(10.0, multiplier);
+      if (multiplier <= 1.0) multiplier = 2.0;
+      double next_iters = std::max(multiplier * iters, iters + 1.0);
+      if (next_iters > kMaxIterations) {
+        next_iters = kMaxIterations;
+      }
+      VLOG(3) << 'Next iters: ' << next_iters << ', ' << multiplier << '\n';
+      iters = static_cast<int>(next_iters + 0.5);
     }
   }
-    
-    
-    { private:
-  /*! \brief number of allocated pages */
-  size_t num_free_buffer_;
-  /*! \brief clock_pointer */
-  size_t clock_ptr_;
-  /*! \brief writer threads */
-  std::vector<std::unique_ptr<std::thread> > workers_;
-  /*! \brief recycler queue */
-  dmlc::ConcurrentBlockingQueue<std::shared_ptr<SparsePage> > qrecycle_;
-  /*! \brief worker threads */
-  std::vector<dmlc::ConcurrentBlockingQueue<std::shared_ptr<SparsePage> > > qworkers_;
-};
-#endif  // DMLC_ENABLE_STD_THREAD
-    
-    
-    {  // remove from column_family_set
-  column_family_set_->RemoveColumnFamily(this);
-}
-    
-      void SetFlushReason(FlushReason flush_reason) {
-    flush_reason_ = flush_reason;
-  }
-  FlushReason GetFlushReason() const { return flush_reason_; }
-  // thread-safe
-  const EnvOptions* soptions() const;
-  const ImmutableCFOptions* ioptions() const { return &ioptions_; }
-  // REQUIRES: DB mutex held
-  // This returns the MutableCFOptions used by current SuperVersion
-  // You should use this API to reference MutableCFOptions most of the time.
-  const MutableCFOptions* GetCurrentMutableCFOptions() const {
-    return &(super_version_->mutable_cf_options);
-  }
-  // REQUIRES: DB mutex held
-  // This returns the latest MutableCFOptions, which may be not in effect yet.
-  const MutableCFOptions* GetLatestMutableCFOptions() const {
-    return &mutable_cf_options_;
+  // Calculate additional statistics
+  auto stat_reports = ComputeStats(reports);
+  if ((b.complexity != oNone) && b.last_benchmark_instance) {
+    auto additional_run_stats = ComputeBigO(*complexity_reports);
+    stat_reports.insert(stat_reports.end(), additional_run_stats.begin(),
+                        additional_run_stats.end());
+    complexity_reports->clear();
   }
     
-    size_t CompactedDBImpl::FindFile(const Slice& key) {
-  size_t left = 0;
-  size_t right = files_.num_files - 1;
-  while (left < right) {
-    size_t mid = (left + right) >> 1;
-    const FdWithKeyRange& f = files_.files[mid];
-    if (user_comparator_->Compare(ExtractUserKey(f.largest_key), key) < 0) {
-      // Key at 'mid.largest' is < 'target'.  Therefore all
-      // files at or before 'mid' are uninteresting.
-      left = mid + 1;
-    } else {
-      // Key at 'mid.largest' is >= 'target'.  Therefore all files
-      // after 'mid' are uninteresting.
-      right = mid;
-    }
-  }
-  return right;
-}
+    // Function to return an string for the calculated complexity
+std::string GetBigOString(BigO complexity);
     
-      // Input statistics
-  // TODO(noetzli): The stats are incomplete. They are lacking everything
-  // consumed by MergeHelper.
-  uint64_t num_input_records = 0;
-  uint64_t num_input_deletion_records = 0;
-  uint64_t num_input_corrupt_records = 0;
-  uint64_t total_input_raw_key_bytes = 0;
-  uint64_t total_input_raw_value_bytes = 0;
-    
-    namespace rocksdb {
-class DBTestDynamicLevel : public DBTestBase {
- public:
-  DBTestDynamicLevel() : DBTestBase('/db_dynamic_level_test') {}
-};
-    }
-    
-      // Implementations of the DB interface
-  using DB::Get;
-  virtual Status Get(const ReadOptions& options,
-                     ColumnFamilyHandle* column_family, const Slice& key,
-                     PinnableSlice* value) override;
-    
-    namespace rocksdb {
-    }
-    
-    // open a file for sequential reading
-Status HdfsEnv::NewSequentialFile(const std::string& fname,
-                                  unique_ptr<SequentialFile>* result,
-                                  const EnvOptions& options) {
-  result->reset();
-  HdfsReadableFile* f = new HdfsReadableFile(fileSys_, fname);
-  if (f == nullptr || !f->isValid()) {
-    delete f;
-    *result = nullptr;
-    return IOError(fname, errno);
-  }
-  result->reset(dynamic_cast<SequentialFile*>(f));
-  return Status::OK();
-}
-    
-      // atomic write
-  WriteBatch batch;
-  batch.Put(handles[0], Slice('key2'), Slice('value2'));
-  batch.Put(handles[1], Slice('key3'), Slice('value3'));
-  batch.Delete(handles[0], Slice('key'));
-  s = db->Write(WriteOptions(), &batch);
-  assert(s.ok());
-    
-    int main() {
-  Options options;
-  options.create_if_missing = true;
-  // Disable RocksDB background compaction.
-  options.compaction_style = kCompactionStyleNone;
-  // Small slowdown and stop trigger for experimental purpose.
-  options.level0_slowdown_writes_trigger = 3;
-  options.level0_stop_writes_trigger = 5;
-  options.IncreaseParallelism(5);
-  options.listeners.emplace_back(new FullCompactor(options));
-    }
-    
-      int ret = system('rm -rf /tmp/rocksmergetest');
-  if (ret != 0) {
-    fprintf(stderr, 'Error deleting /tmp/rocksmergetest, code: %d\n', ret);
-    return ret;
-  }
-  rocksdb::Options options;
-  options.create_if_missing = true;
-  options.merge_operator.reset(new MyMerge);
-  options.compaction_filter = &filter;
-  status = rocksdb::DB::Open(options, '/tmp/rocksmergetest', &raw_db);
-  assert(status.ok());
-  std::unique_ptr<rocksdb::DB> db(raw_db);
+    #include 'benchmark/benchmark.h'
+#include 'internal_macros.h'
