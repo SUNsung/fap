ed0d7eafb6cb9dbe6da1183d573a70d36f563b27
SUNsung <weatherius@gmail.com>
2018-10-09 13:07:54 +0300

    Rewrite [50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp] File in: 2018-10-09 13:07:53

--- cccccc/50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp
+++ cccccc/50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp
@@ -1,142 +1,197 @@
 
         
-          /// Returns true on failure.
-  bool doIt(Type ty) {
-    // Do the pre-order visitation.  If it returns false, we just
-    // skip entering subnodes of this tree.
-    switch (Walker.walkToTypePre(ty)) {
-    case TypeWalker::Action::Continue:
-      break;
-    case TypeWalker::Action::SkipChildren:
-      return false;
-    case TypeWalker::Action::Stop:
-      return true;
-    }
-    }
-    
-        if (info.status == serialization::Status::Valid) {
-      assert(info.bytes != 0);
-      if (!info.name.empty()) {
-        StringRef moduleData = buf.substr(0, info.bytes);
-        std::unique_ptr<llvm::MemoryBuffer> bitstream(
-          llvm::MemoryBuffer::getMemBuffer(moduleData, info.name, false));
-    }
-    }
-    
-    void DiverseStackBase::pushNewStorageSlow(std::size_t needed) {
-  bool wasInline = isAllocatedInline();
+        class AutoUpdater : public mate::EventEmitter<AutoUpdater>,
+                    public auto_updater::Delegate,
+                    public WindowListObserver {
+ public:
+  static mate::Handle<AutoUpdater> Create(v8::Isolate* isolate);
     }
     
     
-    {} // end anonymous namespace
+    {}  // namespace api
     
-    static SourceLoc findEndOfLine(SourceManager &SM, SourceLoc loc,
-                               unsigned bufferID) {
-  CharSourceRange entireBuffer = SM.getRangeForBuffer(bufferID);
-  CharSourceRange rangeFromLoc{SM, loc, entireBuffer.getEnd()};
-  StringRef textFromLoc = SM.extractText(rangeFromLoc);
-  size_t newlineOffset = textFromLoc.find_first_of({'\r\n\0', 3});
-  if (newlineOffset == StringRef::npos)
-    return entireBuffer.getEnd();
-  return loc.getAdvancedLoc(newlineOffset);
+    #include 'atom/browser/native_window_views.h'
+    
+    void Initialize(v8::Local<v8::Object> exports,
+                v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context,
+                void* priv) {
+  auto controller = base::Unretained(TracingController::GetInstance());
+  mate::Dictionary dict(context->GetIsolate(), exports);
+  dict.SetMethod('getCategories',
+                 base::Bind(&TracingController::GetCategories, controller));
+  dict.SetMethod('startRecording',
+                 base::Bind(&TracingController::StartTracing, controller));
+  dict.SetMethod('stopRecording', &StopRecording);
+  dict.SetMethod(
+      'getTraceBufferUsage',
+      base::Bind(&TracingController::GetTraceBufferUsage, controller));
 }
     
     
-    {        Clear();
-    }
-    
-    #pragma once
-    
-        Variable::Variable(const NDShape& shape, VariableKind varType, CNTK::DataType dataType, const NDArrayViewPtr& value, bool needsGradient, const std::vector<Axis>& dynamicAxes, bool isSparse, const std::wstring& name, const std::wstring& uid)
-        : m_dataFields(MakeSharedObject<VariableFields>(shape, varType, dataType, std::weak_ptr<Function>(), value, needsGradient, dynamicAxes, isSparse, name, uid))
-    {}
-    
-            NDShape m_shape;
-        VariableKind m_varKind;
-        ::CNTK::DataType m_dataType;
-        std::weak_ptr<Function> m_ownerFunction;
-        std::unique_ptr<std::once_flag> m_initValueFlag;
-        NDArrayViewPtr m_value;
-        std::unique_ptr<ParameterInitializer> m_valueInitializer;
-        std::unique_ptr<DeviceDescriptor> m_valueInitializationDevice;
-        bool m_needsGradient;
-        std::wstring m_name;
-        std::vector<Axis> m_dynamicAxes;
-        bool m_isSparse;
-        std::wstring m_uid;
-        std::atomic<size_t> m_valueTimeStamp;
-        Variable m_blockFunctionVariableMapping;
-    
-        EditDistanceErrorNode(const ScriptableObjects::IConfigRecordPtr configp)
-        : EditDistanceErrorNode(configp->Get(L'deviceId'), L'<placeholder>', configp->Get(L'subPen'), configp->Get(L'delPen'), configp->Get(L'insPen'), configp->Get(L'squashInputs'), {})
-    {
-        AttachInputsFromConfig(configp, this->GetExpectedNumInputs());
-        m_tokensToIgnore = ScriptableObjects::ConfigArray::FlattenedVectorFrom<size_t>(configp->Get(L'tokensToIgnore'));
-    }
-    
-        std::unique_ptr<DHTPeerAnnounceStorage> peerAnnounceStorage;
-    
-    bool DHTRoutingTable::addNode(const std::shared_ptr<DHTNode>& node)
-{
-  return addNode(node, false);
+    {  // Reference this object in case it got garbage collected.
+  g_download_item_objects[handle->weak_map_id()] =
+      v8::Global<v8::Object>(isolate, handle.ToV8());
+  return handle;
 }
     
-      DHTTaskQueue* taskQueue_;
+     private:
+  base::FilePath save_path_;
+  download::DownloadItem* download_item_;
     
-        taskFactory->setLocalNode(localNode);
-    taskFactory->setRoutingTable(routingTable.get());
-    taskFactory->setMessageDispatcher(dispatcher.get());
-    taskFactory->setMessageFactory(factory.get());
-    taskFactory->setTaskQueue(taskQueue.get());
-    taskFactory->setTimeout(std::chrono::seconds(messageTimeout));
-    
-    void DHTTaskExecutor::update()
-{
-  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
-                                  std::mem_fn(&DHTTask::finished)),
-                   execTasks_.end());
-  int r;
-  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
-    r = numConcurrent_ - execTasks_.size();
+    bool GlobalShortcut::Register(const ui::Accelerator& accelerator,
+                              const base::Closure& callback) {
+  if (!GlobalShortcutListener::GetInstance()->RegisterAccelerator(accelerator,
+                                                                  this)) {
+    return false;
   }
-  else {
-    r = 0;
-  }
-  while (r && !queue_.empty()) {
-    std::shared_ptr<DHTTask> task = queue_.front();
-    queue_.pop_front();
-    task->startup();
-    if (!task->finished()) {
-      execTasks_.push_back(task);
-      --r;
-    }
-  }
-  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
-                   static_cast<unsigned int>(getExecutingTaskSize()),
-                   static_cast<unsigned int>(getQueueSize())));
-}
-    
-    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createBucketRefreshTask()
-{
-  auto task = std::make_shared<DHTBucketRefreshTask>();
-  setCommonProperty(task);
-  return task;
-}
-    
-    namespace aria2 {
     }
     
+    void InAppPurchase::OnTransactionsUpdated(
+    const std::vector<in_app_purchase::Transaction>& transactions) {
+  Emit('transactions-updated', transactions);
+}
+#endif
     
-    {} // namespace aria2
+    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_browser_net, Initialize)
 
     
-    DHTUnknownMessage::~DHTUnknownMessage() { delete[] data_; }
+    #ifndef ATOM_BROWSER_API_ATOM_API_NET_H_
+#define ATOM_BROWSER_API_ATOM_API_NET_H_
     
-    bool DNSCache::CacheEntry::operator<(const CacheEntry& e) const
-{
-  int r = hostname_.compare(e.hostname_);
-  if (r != 0) {
-    return r < 0;
-  }
-  return port_ < e.port_;
+    base::string16 Notification::GetBody() const {
+  return body_;
+}
+    
+    // static
+void RenderProcessPreferences::BuildPrototype(
+    v8::Isolate* isolate,
+    v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(
+      mate::StringToV8(isolate, 'RenderProcessPreferences'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod('addEntry', &RenderProcessPreferences::AddEntry)
+      .SetMethod('removeEntry', &RenderProcessPreferences::RemoveEntry);
+}
+    
+    #include 'atom/browser/render_process_preferences.h'
+#include 'native_mate/handle.h'
+#include 'native_mate/wrappable.h'
+    
+    #include 'atom/browser/api/atom_api_browser_window.h'
+#include 'atom/browser/browser.h'
+#include 'atom/common/native_mate_converters/gfx_converter.h'
+#include 'base/bind.h'
+#include 'native_mate/dictionary.h'
+#include 'native_mate/object_template_builder.h'
+#include 'ui/display/display.h'
+#include 'ui/display/screen.h'
+#include 'ui/gfx/geometry/point.h'
+    
+    
+std::vector<struct pollfd> pollfds;
+std::unordered_map<int, ClientSession> client_sessions;
+// TODO: check if objects have been freed from time to time
+std::set<std::string> used_objects;
+    
+    SHOULD_NOT_DO_GRADIENT(EnforceFinite);
+    
+      auto axis = helper.GetSingleArgument<int32_t>('axis', 1);
+  const auto canonical_axis = canonical_axis_index_(axis, in[0].dims().size());
+  auto axis_w = helper.GetSingleArgument<int32_t>('axis_w', 1);
+  const int canonical_axis_w =
+      canonical_axis_index_(axis_w, in[1].dims().size());
+  const int N = pretransposed_weight
+      ? size_from_dim_(canonical_axis_w, GetDimsVector(in[1]))
+      : size_to_dim_(canonical_axis_w, GetDimsVector(in[1]));
+    
+    **Result**
+    
+    namespace caffe2 {
+    }
+    
+      // Will be called while on the write thread before the write executes.  If
+  // this function returns a non-OK status, the write will be aborted and this
+  // status will be returned to the caller of DB::Write().
+  virtual Status Callback(DB* db) = 0;
+    
+    
+    {  // Add new data and corrupt it
+  ASSERT_OK(writable_file->Append(kCorrupted));
+  ASSERT_TRUE(writable_file->GetFileSize() == kGood.size() + kCorrupted.size());
+  result.clear();
+  ASSERT_OK(rand_file->Read(kGood.size(), kCorrupted.size(),
+            &result, &(scratch[0])));
+  ASSERT_EQ(result.compare(kCorrupted), 0);
+  // Corrupted
+  ASSERT_OK(dynamic_cast<MockEnv*>(env_)->CorruptBuffer(kFileName));
+  result.clear();
+  ASSERT_OK(rand_file->Read(kGood.size(), kCorrupted.size(),
+            &result, &(scratch[0])));
+  ASSERT_NE(result.compare(kCorrupted), 0);
+}
+    
+    int main() {
+  rocksdb::DB* raw_db;
+  rocksdb::Status status;
+    }
+    
+      // destroy and open DB
+  DB* db;
+  Status s = DestroyDB(kDBPath, Options(db_opt, cf_descs[0].options));
+  assert(s.ok());
+  s = DB::Open(Options(db_opt, cf_descs[0].options), kDBPath, &db);
+  assert(s.ok());
+    
+    
+BENCHMARK_MAIN();
+
+    
+      BENCHMARK_ALWAYS_INLINE
+  explicit StateIterator(State* st)
+      : cached_(st->error_occurred_ ? 0 : st->max_iterations), parent_(st) {}
+    
+    namespace benchmark {
+namespace internal {
+// The arraysize(arr) macro returns the # of elements in an array arr.
+// The expression is a compile-time constant, and therefore can be
+// used in defining new arrays, for example.  If you use arraysize on
+// a pointer by mistake, you will get a compile-time error.
+//
+    }
+    }
+    
+    // The CHECK macro returns a std::ostream object that can have extra information
+// written to it.
+#ifndef NDEBUG
+#define CHECK(b)                                                             \
+  (b ? ::benchmark::internal::GetNullLogInstance()                           \
+     : ::benchmark::internal::CheckHandler(#b, __FILE__, __func__, __LINE__) \
+           .GetLog())
+#else
+#define CHECK(b) ::benchmark::internal::GetNullLogInstance()
+#endif
+    
+    
+    {// Returns true unless value starts with one of: '0', 'f', 'F', 'n' or 'N', or
+// some non-alphanumeric character. As a special case, also returns true if
+// value is the empty string.
+bool IsTruthyFlagValue(const std::string& value);
+}  // end namespace benchmark
+    
+    #ifndef BENCHMARK_CYCLECLOCK_H_
+#define BENCHMARK_CYCLECLOCK_H_
+    
+    // Enable thread safety attributes only with clang.
+// The attributes can be safely erased when compiling with other compilers.
+#if defined(HAVE_THREAD_SAFETY_ATTRIBUTES)
+#define THREAD_ANNOTATION_ATTRIBUTE__(x) __attribute__((x))
+#else
+#define THREAD_ANNOTATION_ATTRIBUTE__(x)  // no-op
+#endif
+    
+    double BenchmarkReporter::Run::GetAdjustedRealTime() const {
+  double new_time = real_accumulated_time * GetTimeUnitMultiplier(time_unit);
+  if (iterations != 0) new_time /= static_cast<double>(iterations);
+  return new_time;
 }
