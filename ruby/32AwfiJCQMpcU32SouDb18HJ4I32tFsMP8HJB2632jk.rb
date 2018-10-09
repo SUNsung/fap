2fd93acd26059097251659b813328fa4fc0645b1
SUNsung <weatherius@gmail.com>
2018-10-09 07:25:49 +0300

    Rewrite [32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb] File in: 2018-10-09 07:25:49

--- ruby/32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb
+++ ruby/32AwfiJCQMpcU32SouDb18HJ4I32tFsMP8HJB2632jk.rb
@@ -1,3 +1,115 @@
 
         
-        
+            def initialize(analytics_ingester_client: AnalyticsIngesterClient.new(GA_TRACKING))
+      require 'securerandom'
+      @session_id = SecureRandom.uuid
+      @client = analytics_ingester_client
+      @threads = []
+      @launch_event_sent = false
+    end
+    
+            cmd << ['-am #{message.shellescape}']
+        cmd << '--force' if options[:force]
+        cmd << '-s' if options[:sign]
+        cmd << tag.shellescape
+        cmd << options[:commit].to_s if options[:commit]
+    
+          it 'Does not use pattern matching for tag name if so requested' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          changelog_from_git_commits()
+        end').runner.execute(:test)
+    
+            allow(File).to receive(:file?).and_return(false)
+        allow(File).to receive(:file?).with(keychain_path).and_return(true)
+        allow(File).to receive(:exist?).and_return(false)
+        expect(File).to receive(:exist?).with(cert_name).and_return(true)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_set_key_partition_list_command, print: false)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_security_import_command, print: false)
+    
+          it 'raises an exception when the default compile_commands.json is not present' do
+        expect do
+          Fastlane::FastFile.new.parse('lane :test do
+            oclint
+          end').runner.execute(:test)
+        end.to raise_error('Could not find json compilation database at path 'compile_commands.json'')
+      end
+    
+            context 'when slather version is 2.4.2' do
+          let(:version) { '2.4.2' }
+          it 'configuration option is available' do
+            expect(action.configuration_available?).to be_truthy
+          end
+        end
+      end
+    
+    module CrossplatformShellwords
+  # handle switching between implementations of shellescape
+  def shellescape(str)
+    if FastlaneCore::Helper.windows?
+      WindowsShellwords.shellescape(str)
+    else
+      # using `escape` instead of expected `shellescape` here
+      # which corresponds to Shellword's `String.shellescape` implementation
+      # https://github.com/ruby/ruby/blob/1cf2bb4b2085758112503e7da7414d1ef52d4f48/lib/shellwords.rb#L216
+      Shellwords.escape(str)
+    end
+  end
+  module_function :shellescape
+    
+          it 'should shell escape keychain names when checking for installation' do
+        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
+    
+          describe 'misc features' do
+        it 'makes it non optional by default' do
+          c = FastlaneCore::ConfigItem.new(key: :test,
+                                 default_value: '123')
+          expect(c.optional).to eq(false)
+        end
+    
+          # Forgets the given resource by deleting a cookie
+      def forget_me(resource)
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.forget_me!
+        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
+      end
+    
+    module Devise
+  module Controllers
+    # Provide the ability to store a location.
+    # Used to redirect back to a desired path after sign in.
+    # Included by default in all controllers.
+    module StoreLocation
+      # Returns and delete (if it's navigational format) the url stored in the session for
+      # the given scope. Useful for giving redirect backs after sign up:
+      #
+      # Example:
+      #
+      #   redirect_to stored_location_for(:user) || root_path
+      #
+      def stored_location_for(resource_or_scope)
+        session_key = stored_location_key_for(resource_or_scope)
+    
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+              workspace_path = 'Examples.xcworkspace'
+          workspace = Xcodeproj::Workspace.new_from_xcworkspace(workspace_path)
+          workspace.schemes.each do |scheme_name, project_path|
+            next if scheme_name == 'Pods'
+            next if project_path.end_with? 'Pods.xcodeproj'
+            puts '    Building scheme: #{scheme_name}'
+    
+          attr_reader :key, :default, :options
+    
+          # Internal use only.
+      def peek(key, default=nil, &block)
+        value = fetch_for(key, default, &block)
+        while callable_without_parameters?(value)
+          value = (values[key] = value.call)
+        end
+        value
+      end
