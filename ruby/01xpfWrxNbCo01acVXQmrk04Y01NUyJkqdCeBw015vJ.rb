c9880442f8648d85a370dc90d433fb2525dfbcfa
SUNsung <weatherius@gmail.com>
2018-10-09 20:09:18 +0300

    Rewrite [01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb] File in: 2018-10-09 20:09:17

--- ruby/01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb
+++ ruby/01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb
@@ -1,85 +1,404 @@
 
         
-                a_split.each_with_index { |s, i| a_split[i] = s.to_i unless i == a_length - 1 }
-        b_split.each_with_index { |s, i| b_split[i] = s.to_i unless i == b_length - 1 }
+                  def tag_id(index = nil)
+            # a little duplication to construct less strings
+            case
+            when @object_name.empty?
+              sanitized_method_name.dup
+            when index
+              '#{sanitized_object_name}_#{index}_#{sanitized_method_name}'
+            else
+              '#{sanitized_object_name}_#{sanitized_method_name}'
+            end
+          end
     
-          def process_response(response)
-        super.merge! response_url: response.url
+    module ActionView
+  module Helpers
+    module Tags # :nodoc:
+      class Label < Base # :nodoc:
+        class LabelBuilder # :nodoc:
+          attr_reader :object
+    
+            def render
+          options = @options.stringify_keys
+          options['size'] = options['maxlength'] unless options.key?('size')
+          options['type'] ||= field_type
+          options['value'] = options.fetch('value') { value_before_type_cast } unless field_type == 'file'
+          add_default_name_and_id(options)
+          tag('input', options)
+        end
+    
+        # Some actions have special handling in fast_file.rb, that means we can't directly call the action
+    # but we have to use the same logic that is in fast_file.rb instead.
+    # That's where this switch statement comes into play
+    def run_action_requiring_special_handling(command: nil, parameter_map: nil, action_return_type: nil)
+      action_return = nil
+      closure_argument_value = nil # only used if the action uses it
+    
+        def initialize(analytics_ingester_client: AnalyticsIngesterClient.new(GA_TRACKING))
+      require 'securerandom'
+      @session_id = SecureRandom.uuid
+      @client = analytics_ingester_client
+      @threads = []
+      @launch_event_sent = false
+    end
+    
+            # Stub out calls related to the execution environment
+        client = double('ingester_client')
+        session = FastlaneCore::AnalyticsSession.new(analytics_ingester_client: client)
+        expect(client).to receive(:post_event).with({
+            client_id: p_hash,
+            category: 'fastlane Client Langauge - ruby',
+            action: :launch,
+            label: nil,
+            value: nil
+        })
+    
+            [
+          'This will automatically tag your build with the following format: `<grouping>/<lane>/<prefix><build_number>`, where:'.markdown_preserve_newlines,
+          list,
+          'For example, for build 1234 in the 'appstore' lane, it will tag the commit with `builds/appstore/1234`.'
+        ].join('\n')
+      end
+    
+            message = '#{tag} (fastlane)'
+        expect(result).to eq('git tag -am #{message.shellescape} #{tag.shellescape}')
+      end
+    
+          it 'Does not accept a :commits_count < 1' do
+        expect do
+          Fastlane::FastFile.new.parse('lane :test do
+            changelog_from_git_commits(commits_count: -1)
+          end').runner.execute(:test)
+        end.to raise_error(':commits_count must be >= 1')
+      end
+    
+            expect(result.size).to eq(3)
+        expect(result[0]).to eq('security create-keychain -p testpassword ~/Library/Keychains/test.keychain')
+    
+          it 'handles the exclude_dirs parameter with multiple  elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: ['.bundle', 'Packages/'])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --exclude-dir .bundle --exclude-dir Packages/')
       end
     end
   end
 end
 
     
-        private
+          it 'works with select regex' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+              select_regex: /AppDelegate/
+            )
+          end').runner.execute(:test)
     
-        # Tells optparse how to parse the arguments.
-    #
-    # @param opts [OptionParser]
-    def set_opts(opts)
-      opts.banner = <<END
-Usage: sass-convert [options] [INPUT] [OUTPUT]
+        # wrap in double quotes if contains space
+    if str =~ /\s/
+      # double quotes have to be doubled if will be quoted
+      str.gsub!(''', '''')
+      return ''' + str + '''
+    else
+      return str
+    end
+  end
+  module_function :shellescape
+end
+
     
-        private
+    shellescape_testcases = [
+  # baseline
+  {
+    'it' => '(#1) on simple string',
+    'it_result' => {
+      'windows' => 'doesn't change it',
+      'other'   => 'doesn't change it'
+    },
+    'str' => 'normal_string_without_spaces',
+    'expect' => {
+      'windows' => 'normal_string_without_spaces',
+      'other'   => 'normal_string_without_spaces'
+    }
+  },
+  {
+    'it' => '(#2) on empty string',
+    'it_result' => {
+      'windows' => 'wraps it in double quotes',
+      'other'   => 'wraps it in single quotes'
+    },
+    'str' => '',
+    'expect' => {
+      'windows' => '''',
+      'other'   => '\'\''
+    }
+  },
+  # spaces
+  {
+    'it' => '(#3) on string with spaces',
+    'it_result' => {
+      'windows' => 'wraps it in double quotes',
+      'other'   => 'escapes spaces with <backslash>'
+    },
+    'str' => 'string with spaces',
+    'expect' => {
+      'windows' => ''string with spaces'',
+      'other'   => 'string\ with\ spaces'
+    }
+  },
+  # double quotes
+  {
+    'it' => '(#4) on simple string that is already wrapped in double quotes',
+    'it_result' => {
+      'windows' => 'doesn't touch it',
+      'other'   => 'escapes the double quotes with <backslash>'
+    },
+    'str' => ''normal_string_without_spaces'',
+    'expect' => {
+      'windows' => ''normal_string_without_spaces'',
+      'other'   => '\'normal_string_without_spaces\''
+    }
+  },
+  {
+    'it' => '(#5) on string with spaces that is already wrapped in double quotes',
+    'it_result' => {
+      'windows' => 'wraps in double quotes and duplicates existing double quotes',
+      'other'   => 'escapes the double quotes and spaces with <backslash>'
+    },
+    'str' => ''string with spaces already wrapped in double quotes'',
+    'expect' => {
+      'windows' => ''''string with spaces already wrapped in double quotes'''',
+      'other'   => '\'string\ with\ spaces\ already\ wrapped\ in\ double\ quotes\''
+    }
+  },
+  {
+    'it' => '(#6) on string with spaces and double quotes',
+    'it_result' => {
+      'windows' => 'wraps in double quotes and duplicates existing double quotes',
+      'other'   => 'escapes the double quotes and spaces with <backslash>'
+    },
+    'str' => 'string with spaces and 'double' quotes',
+    'expect' => {
+      'windows' => ''string with spaces and ''double'' quotes'',
+      'other'   => 'string\ with\ spaces\ and\ \'double\'\ quotes'
+    }
+  },
+  # https://github.com/ruby/ruby/blob/ac543abe91d7325ace7254f635f34e71e1faaf2e/test/test_shellwords.rb#L64-L65
+  {
+    'it' => '(#7) on simple int',
+    'it_result' => {
+      'windows' => 'doesn't change it',
+      'other'   => 'doesn't change it'
+    },
+    'str' => 3,
+    'expect' => {
+      'windows' => '3',
+      'other'   => '3'
+    }
+  },
+  # single quotes
+  {
+    'it' => '(#8) on simple string that is already wrapped in single quotes',
+    'it_result' => {
+      'windows' => 'doesn't touch it',
+      'other'   => 'escapes the single quotes with <backslash>'
+    },
+    'str' => ''normal_string_without_spaces'',
+    'expect' => {
+      'windows' => ''normal_string_without_spaces'',
+      'other'   => '\\'normal_string_without_spaces\\''
+    }
+  },
+  {
+    'it' => '(#9) on string with spaces that is already wrapped in single quotes',
+    'it_result' => {
+      'windows' => 'wraps in double quotes',
+      'other'   => 'escapes the single quotes and spaces with <backslash>'
+    },
+    'str' => ''string with spaces already wrapped in single quotes'',
+    'expect' => {
+      'windows' => '\''string with spaces already wrapped in single quotes'\'',
+      'other'   => '\\'string\\ with\\ spaces\\ already\\ wrapped\\ in\\ single\\ quotes\\''
+    }
+  },
+  {
+    'it' => '(#10) string with spaces and single quotes',
+    'it_result' => {
+      'windows' => 'wraps in double quotes and leaves single quotes',
+      'other'   => 'escapes the single quotes and spaces with <backslash>'
+    },
+    'str' => 'string with spaces and 'single' quotes',
+    'expect' => {
+      'windows' => '\'string with spaces and 'single' quotes\'',
+      'other'   => 'string\ with\ spaces\ and\ \\\'single\\\'\ quotes'
+    }
+  },
+  {
+    'it' => '(#11) string with spaces and <backslash>',
+    'it_result' => {
+      'windows' => 'wraps in double quotes and escapes the backslash with backslash',
+      'other'   => 'escapes the spaces and the backslash (which in results in quite a lot of them)'
+    },
+    'str' => 'string with spaces and \\ in it',
+    'expect' => {
+      'windows' => '\'string with spaces and \\ in it\'',
+      'other'   => 'string\\ with\\ spaces\\ and\\ \\\\\\ in\\ it'
+    }
+  },
+  {
+    'it' => '(#12) string with spaces and <slash>',
+    'it_result' => {
+      'windows' => 'wraps in double quotes',
+      'other'   => 'escapes the spaces'
+    },
+    'str' => 'string with spaces and / in it',
+    'expect' => {
+      'windows' =>  '\'string with spaces and / in it\'',
+      'other'   => 'string\\ with\\ spaces\\ and\\ /\\ in\\ it'
+    }
+  },
+  {
+    'it' => '(#13) string with spaces and parens',
+    'it_result' => {
+      'windows' => 'wraps in double quotes',
+      'other'   => 'escapes the spaces and parens'
+    },
+    'str' => 'string with spaces and (parens) in it',
+    'expect' => {
+      'windows' => '\'string with spaces and (parens) in it\'',
+      'other'   => 'string\\ with\\ spaces\\ and\\ \\(parens\\)\\ in\\ it'
+    }
+  },
+  {
+    'it' => '(#14) string with spaces, single quotes and parens',
+    'it_result' => {
+      'windows' => 'wraps in double quotes',
+      'other'   => 'escapes the spaces, single quotes and parens'
+    },
+    'str' => 'string with spaces and 'quotes' (and parens) in it',
+    'expect' => {
+      'windows' => '\'string with spaces and 'quotes' (and parens) in it\'',
+      'other'   => 'string\\ with\\ spaces\\ and\\ \\'quotes\\'\\ \\(and\\ parens\\)\\ in\\ it'
+    }
+  }
+]
     
-          # Get the cache key pair for the given Sass URI.
-      # The URI need not be checked for validity.
-      #
-      # The only strict requirement is that the returned pair of strings
-      # uniquely identify the file at the given URI.
-      # However, the first component generally corresponds roughly to the directory,
-      # and the second to the basename, of the URI.
-      #
-      # Note that keys must be unique *across importers*.
-      # Thus it's probably a good idea to include the importer name
-      # at the beginning of the first component.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [(String, String)] The key pair which uniquely identifies
-      #   the file at the given URI.
-      def key(uri, options)
-        Sass::Util.abstract(self)
+        it 'returns a label 'No' if a given agent is not working' do
+      stub(@agent).working? { false }
+      label = working(@agent)
+      expect(label).to be_html_safe
+      expect(Nokogiri(label).text).to eq 'No'
+    end
+  end
+    
+              expect(weather_agent.name).to eq('a weather agent')
+          expect(weather_agent.schedule).to eq('5pm')
+          expect(weather_agent.keep_events_for).to eq(14.days)
+          expect(weather_agent.propagate_immediately).to be_falsey
+          expect(weather_agent).to be_disabled
+          expect(weather_agent.memory).to be_empty
+          expect(weather_agent.options).to eq(weather_agent_options)
+    
+          it 'loads all workers' do
+        workers = @agent_runner.send(:load_workers)
+        expect(workers).to be_a(Hash)
+        expect(workers.keys).to eq(['HuginnScheduler', 'DelayedJobWorker'])
       end
     
-          # @see Base#to_s
-      def to_s
-        @root
-      end
+    describe HuginnScheduler do
+  before(:each) do
+    @rufus_scheduler = Rufus::Scheduler.new
+    @scheduler = HuginnScheduler.new
+    stub(@scheduler).setup {}
+    @scheduler.setup!(@rufus_scheduler, Mutex.new)
+  end
     
-            def run
-          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
-          if @pod_name.nil? # Print all
-            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
-              print_pod_cache_infos(pod_name, cache_descriptors)
-            end
-          else # Print only for the requested pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            else
-              print_pod_cache_infos(@pod_name, cache_descriptors)
+        it 'should ignore strings which just contain a JSONPath' do
+      expect(LiquidMigrator.convert_string('$.data')).to eq('$.data')
+      expect(LiquidMigrator.convert_string('$first_title')).to eq('$first_title')
+      expect(LiquidMigrator.convert_string(' $.data', true)).to eq(' $.data')
+      expect(LiquidMigrator.convert_string('lorem $.data', true)).to eq('lorem $.data')
+    end
+    it 'should raise an exception when encountering complex JSONPaths' do
+      expect { LiquidMigrator.convert_string('$.data.test.*', true) }.
+        to raise_error('JSONPath '$.data.test.*' is too complex, please check your migration.')
+    end
+  end
+    
+          expect {
+        Agent.receive!
+      }.not_to change { agent2.events.count }
+    end
+  end
+end
+    
+        def to_a
+      @filters.dup
+    end
+    
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
+    end
+    
+        def queue(request)
+      request.on_complete(&method(:handle_response))
+      super
+    end
+    
+          private
+    
+              hosts
+        end
+    
+            # This contains all the registered host capabilities.
+        #
+        # @return [Hash<Symbol, Registry>]
+        attr_reader :host_capabilities
+    
+            # This returns all registered pushes.
+        #
+        # @return [Registry]
+        def pushes
+          Registry.new.tap do |result|
+            @registered.each do |plugin|
+              result.merge!(plugin.components.pushes)
             end
           end
         end
     
-    module RailsCommandHelpers
-  def framework_version?(version_string)
-    framework_version =~ /^#{version_string}/
-  end
+      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=end
     
-      def generate_migration
-    migration_template('paperclip_migration.rb.erb',
-                       'db/migrate/#{migration_file_name}',
-                       migration_version: migration_version)
-  end
+      test 'frontend links for editing blocked' do
+    Precious::App.set(:wiki_options, { allow_editing: false })
+    get '/A'
     
-    # The base module that gets included in ActiveRecord::Base. See the
-# documentation for Paperclip::ClassMethods for more useful information.
-module Paperclip
-  extend Helpers
-  extend Logger
-  extend ProcessorHelpers
+      test 'displays_latest_changes' do
+    get('/latest_changes')
+    body = last_response.body
     
-        alias :empty? :empty_file?
+      parameter 'file', 'the package file name', :attribute_name => :package_file, :required => true
+    
+        # remove any version constrain from the Gemfile so the plugin(s) can be updated to latest version
+    # calling update without requirements will remove any previous requirements
+    plugins = plugins_to_update(previous_gem_specs_map)
+    # Skipping the major version validation when using a local cache as we can have situations
+    # without internet connection.
+    filtered_plugins = plugins.map { |plugin| gemfile.find(plugin) }
+      .compact
+      .reject { |plugin| REJECTED_OPTIONS.any? { |key| plugin.options.has_key?(key) } }
+      .each   { |plugin| gemfile.update(plugin.name) }
