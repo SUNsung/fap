c5308872a5d89fd0524c6744ed1e18d89f2fe3d3
SUNsung <weatherius@gmail.com>
2018-10-09 15:13:52 +0300

    Rewrite [33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb] File in: 2018-10-09 15:13:51

--- ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
+++ ruby/33U00zZbwC.Gw333BmZca1g6Kc33IO3cL2Vz2Tg33q6G.rb
@@ -1,71 +1,115 @@
 
         
-            # Add or remove new parameters to the permitted list of an +action+.
-    #
-    # === Arguments
-    #
-    # * +action+ - A +Symbol+ with the action that the controller is
-    #   performing, like +sign_up+, +sign_in+, etc.
-    # * +keys:+     - An +Array+ of keys that also should be permitted.
-    # * +except:+   - An +Array+ of keys that shouldn't be permitted.
-    # * +block+     - A block that should be used to permit the action
-    #   parameters instead of the +Array+ based approach. The block will be
-    #   called with an +ActionController::Parameters+ instance.
-    #
-    # === Examples
-    #
-    #   # Adding new parameters to be permitted in the `sign_up` action.
-    #   devise_parameter_sanitizer.permit(:sign_up, keys: [:subscribe_newsletter])
-    #
-    #   # Removing the `password` parameter from the `account_update` action.
-    #   devise_parameter_sanitizer.permit(:account_update, except: [:password])
-    #
-    #   # Using the block form to completely override how we permit the
-    #   # parameters for the `sign_up` action.
-    #   devise_parameter_sanitizer.permit(:sign_up) do |user|
-    #     user.permit(:email, :password, :password_confirmation)
-    #   end
-    #
-    #
-    # Returns nothing.
-    def permit(action, keys: nil, except: nil, &block)
-      if block_given?
-        @permitted[action] = block
+                array.flatten.map! { |i| ERB::Util.unwrapped_html_escape(i) }.join(sep).html_safe
       end
     
-      test 'error message is configurable by resource name' do
-    store_translations :en, devise: { failure: { admin: { invalid: 'Invalid credentials' } } } do
-      sign_in_as_admin do
-        fill_in 'password', with: 'abcdef'
-      end
+              def add_default_name_and_id(options)
+            index = name_and_id_index(options)
+            options['name'] = options.fetch('name') { tag_name(options['multiple'], index) }
     
-        def matching_username
-      Account.where(Account.arel_table[:username].lower.eq username.to_s.downcase)
-    end
+                content ||= Translator
+              .new(object, @object_name, method_and_value, scope: 'helpers.label')
+              .translate
+            content ||= @method_name.humanize
     
-    namespace :db do
-  namespace :migrate do
-    desc 'Setup the db or migrate depending on state of db'
-    task setup: :environment do
-      begin
-        if ActiveRecord::Migrator.current_version.zero?
-          Rake::Task['db:migrate'].invoke
-          Rake::Task['db:seed'].invoke
+            ActiveSupport::Notifications.instrument('render_#{name}.action_view', options) do |payload|
+          yield payload
         end
-      rescue ActiveRecord::NoDatabaseError
-        Rake::Task['db:setup'].invoke
-      else
-        Rake::Task['db:migrate'].invoke
       end
+    
+        context 'destroy' do
+      let!(:notification) { Fabricate(:notification) }
+    
+            unless post && post.id
+          puts post.errors.full_messages if post
+          puts creator.errors.inspect
+          raise 'Failed to create description for trust level 3 lounge!'
+        end
+    
+    When /^I post a status with the text '([^\']*)'$/ do |text|
+  @me.post(:status_message, :text => text, :public => true, :to => 'all')
+end
+    
+    # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
+# order to ease the transition to Capybara we set the default here. If you'd
+# prefer to use XPath just remove this line and adjust any selectors in your
+# steps to use the XPath syntax.
+Capybara.default_selector = :css
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
+    end
+  end
+end
+
+    
+        it 'lets a user destroy their like' do
+      current_user = controller.send(:current_user)
+      expect(current_user).to receive(:retract).with(@like)
+    
+    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
+
+    
+        # Initializes a new CategoryIndex.
+    #
+    #  +base+         is the String path to the <source>.
+    #  +category_dir+ is the String path between <source> and the category folder.
+    #  +category+     is the category currently being processed.
+    def initialize(site, base, category_dir, category)
+      @site = site
+      @base = base
+      @dir  = category_dir
+      @name = 'index.html'
+      self.process(@name)
+      # Read the YAML data from the layout page.
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.data['category']    = category
+      # Set the title for this page.
+      title_prefix             = site.config['category_title_prefix'] || 'Category: '
+      self.data['title']       = '#{title_prefix}#{category}'
+      # Set the meta-description for this page.
+      meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
+      self.data['description'] = '#{meta_description_prefix}#{category}'
+    end
+    
+    class ConfigTag < Liquid::Tag
+  def initialize(tag_name, options, tokens)
+    super
+    options = options.split(' ').map {|i| i.strip }
+    @key = options.slice!(0)
+    @tag = nil
+    @classname = nil
+    options.each do |option|
+      @tag = $1 if option =~ /tag:(\S+)/ 
+      @classname = $1 if option =~ /classname:(\S+)/
     end
   end
     
-    class ActivityPub::EmojiSerializer < ActiveModel::Serializer
-  include RoutingHelper
-    
-    class NotificationMailerPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/mention
-  def mention
-    m = Mention.last
-    NotificationMailer.mention(m.account, Notification.find_by(activity: m))
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
+          end
+        end
+      end
+      rtn
+    end
   end
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
