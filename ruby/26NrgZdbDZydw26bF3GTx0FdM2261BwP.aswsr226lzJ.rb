ff9debdbe9c8a962d650b4311decb4dbf6c25b9f
SUNsung <weatherius@gmail.com>
2018-10-10 11:56:21 +0300

    Rewrite [26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb] File in: 2018-10-10 11:56:20

--- ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
+++ ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
@@ -1,73 +1,145 @@
 
         
-            if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+            it 'shows the dry run pop up without previous events and selects the events tab when a event was created' do
+      open_dry_run_modal(agent)
+      click_on('Dry Run')
+      expect(page).to have_text('Biologists play reverse')
+      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
     end
-  end
     
-    if defined?(ActionMailer)
-  class Devise::Mailer < Devise.parent_mailer.constantize
-    include Devise::Mailers::Helpers
+    end
+
     
-    Rails.application.initialize!
-    
-      module Mailers
-    autoload :Helpers, 'devise/mailers/helpers'
-  end
-    
-      def prepend(*paths)
-    @paths = parse(*paths, *@paths)
-    self
-  end
-    
-        odie 'Unknown command: #{cmd}' unless path
-    puts path
+        it 'can not be turned off' do
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'true' }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
   end
 end
 
     
-      # add junk params
-  attr_accessor :junk_params
+          it 'loads only the workers specified in the :only option' do
+        agent_runner = AgentRunner.new(only: HuginnScheduler)
+        workers = agent_runner.send(:load_workers)
+        expect(workers.keys).to eq(['HuginnScheduler'])
+        agent_runner.stop
+      end
     
-    IAX_IE_CALLED_NUMBER  = 1
-IAX_IE_CALLING_NUMBER = 2
-IAX_IE_AUTH_METHODS   = 3
-IAX_IE_CALLING_NAME   = 4
-IAX_IE_USERNAME       = 6
-IAX_IE_DESIRED_CODEC  = 9
-IAX_IE_ORIGINAL_DID   = 10
-IAX_IE_ACTUAL_CODECS  = 8
-IAX_IE_PROTO_VERSION  = 11
-IAX_IE_REG_REFRESH    = 19
-IAX_IE_CHALLENGE_DATA = 15
-IAX_IE_CHALLENGE_RESP = 16
-IAX_IE_APPARENT_ADDR  = 18
-IAX_IE_REGREJ_CAUSE   = 22
-IAX_IE_HANGUP_CAUSE   = 42
+        it 'outputs control links to agents within the incoming set, but not outside it' do
+      agents(:jane_rain_notifier_agent).control_targets = [agents(:jane_weather_agent), agents(:jane_basecamp_agent)]
+      agents(:jane_rain_notifier_agent).save!
+    
+        it 'work with set FAILED_JOBS_TO_KEEP env variable' do
+      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(-1)
+      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(0)
+      expect(@keep.id).to eq(Delayed::Job.order(:failed_at)[0].id)
+    end
+    
+      it 'converts values to Float' do
+    expect(location.lat).to be_a Float
+    expect(location.lat).to eq 2.0
+    expect(location.lng).to be_a Float
+    expect(location.lng).to eq 3.0
+    expect(location.radius).to be_a Float
+    expect(location.radius).to eq 300.0
+    expect(location.speed).to be_a Float
+    expect(location.speed).to eq 2.0
+    expect(location.course).to be_a Float
+    expect(location.course).to eq 30.0
+  end
+    
+      let :valid_options do
+    {
+      'name' => 'XKCD',
+      'expected_update_period_in_days' => '2',
+      'type' => 'html',
+      'url' => '{{ url | default: 'http://xkcd.com/' }}',
+      'mode' => 'on_change',
+      'extract' => old_extract,
+      'template' => old_template
+    }
+  end
+    
+        context 'when .receive_web_request is defined with just request' do
+      before do
+        @agent = Agents::WebRequestReceiver.new(:name => 'something')
+        @agent.user = users(:bob)
+        @agent.save!
+    
+              it 'cuts fraction part to have only 6 digits at all' do
+            format('%#{f}', 1.1234567).should == '1.12346'
+            format('%#{f}', 12.1234567).should == '12.1235'
+            format('%#{f}', 123.1234567).should == '123.123'
+            format('%#{f}', 1234.1234567).should == '1234.12'
+            format('%#{f}', 12345.1234567).should == '12345.1'
+            format('%#{f}', 123456.1234567).should == '123456'
+          end
+        end
+    
+    describe 'Kernel#srand' do
+  it 'needs to be reviewed for spec completeness'
+end
+
+    
+      platform_is_not :windows do
+    it 'does not expand shell variables when given multiples arguments' do
+      lambda { @object.system('echo', @shell_var) }.should output_to_fd('#{@shell_var}\n')
+    end
+  end
+    
+      it 'creates a public method in TOPLEVEL_BINDING' do
+    eval @code, TOPLEVEL_BINDING
+    Object.should have_method :boom
+  end
+    
+        # ghettoooooo!
+    # If we don't have any newlines..., put one there.
+    if (header.size > 0 && header !~ /\r\n/)
+      header << '\r\n'
+    end
     
       #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
+  # Move these into an IPMI stack or mixin at some point
   #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
     
-              sent = 0
-          case protocol
-          when 'tcp'
-            sent = send_request_tcp(req)
-          when 'udp'
-            sent = send_request_udp(req)
-          else
-            raise ::RuntimeError, 'Kerberos Client: unknown transport protocol'
+            # Sends a kerberos request, and reads the response through the connection
+        #
+        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
+        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] The kerberos message
+        # @raise [RuntimeError] if the transport protocol is unknown or the response can't be parsed.
+        # @raise [NotImplementedError] if the transport protocol isn't supported
+        def send_recv(req)
+          send_request(req)
+          res = recv_response
+    
+                data_encrypt = Rex::Text::rand_text(8) + data
+    
+              # Rex::Proto::Kerberos::Model::ApReq decoding isn't supported
+          #
+          # @raise [NotImplementedError]
+          def decode(input)
+            raise ::NotImplementedError, 'AP-REQ decoding not supported'
+          end
+    
+              # Encodes the checksum field
+          #
+          # @return [OpenSSL::ASN1::OctetString]
+          def encode_checksum
+            OpenSSL::ASN1::OctetString.new(checksum)
+          end
+        end
+      end
+    end
+  end
+end
+    
+              # Decodes the kvno from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_kvno(input)
+            input.value[0].value.to_i
           end
     
               # Encodes the pvno field
@@ -77,43 +149,26 @@ IAX_IE_HANGUP_CAUSE   = 42
             bn = OpenSSL::BN.new(pvno.to_s)
             int = OpenSSL::ASN1::Integer.new(bn)
     
-              # Encodes the data
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_data(data)
-            OpenSSL::ASN1::OctetString.new(data)
+          def unprocessable_entity(message)
+        render plain: { exception: message }.to_json, status: 422
+      end
+    
+            def create
+          authorize! :create, ProductProperty
+          @product_property = @product.product_properties.new(product_property_params)
+          if @product_property.save
+            respond_with(@product_property, status: 201, default_template: :show)
+          else
+            invalid_resource!(@product_property)
           end
         end
-      end
-    end
-  end
-end
     
-              # @!attribute type
-          #   @return [Integer] The algorithm used to generate the checksum
-          attr_accessor :type
-          # @!attribute checksum
-          #   @return [String] The checksum itself
-          attr_accessor :checksum
-    
-              # Decodes the end_time field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_end_time(input)
-            input.value[0].value
+            def create
+          authorize! :create, StockMovement
+          @stock_movement = scope.new(stock_movement_params)
+          if @stock_movement.save
+            respond_with(@stock_movement, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_movement)
           end
-    
-              # Decodes the cname field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Type::PrincipalName]
-          def decode_cname(input)
-            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
-          end
-    
-        def install_plugin(plugin, load_hooks: true, load_immediately: false)
-      installer.install(plugin,
-                        load_hooks: load_hooks,
-                        load_immediately: load_immediately)
-    end
+        end
