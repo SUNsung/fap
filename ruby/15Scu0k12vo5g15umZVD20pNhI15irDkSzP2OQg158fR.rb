7ae9c31451782e0cc57b2c0584513e1b44720abd
SUNsung <weatherius@gmail.com>
2018-10-10 16:47:52 +0300

    Rewrite [15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb] File in: 2018-10-10 16:47:51

--- ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
+++ ruby/15Scu0k12vo5g15umZVD20pNhI15irDkSzP2OQg158fR.rb
@@ -1,85 +1,164 @@
 
         
-        def du_uploadtrailer_preview_correct_jpg
-  mock_jpg = double
-  allow(mock_jpg).to receive(:file_name).and_return('ftl_FAKEMD5_trailer-en-US_preview.jpg')
-  allow(mock_jpg).to receive(:file_size).and_return(12_345)
-  allow(mock_jpg).to receive(:content_type).and_return('image/jpg')
-  allow(mock_jpg).to receive(:bytes).and_return('trailer preview...')
-  mock_jpg
-end
+            def show_message
+      UI.message('Sending anonymous analytics information')
+      UI.message('Learn more at https://docs.fastlane.tools/#metrics')
+      UI.message('No personal or sensitive data is sent.')
+      UI.message('You can disable this by adding `opt_out_usage` at the top of your Fastfile')
+    end
     
-      context '#builds_for_group' do
-    it 'executes the request' do
-      MockAPI::TestFlightServer.get('/testflight/v2/providers/fake-team-id/apps/some-app-id/groups/fake-group-id/builds') {}
-      subject.builds_for_group(app_id: app_id, group_id: 'fake-group-id')
-      expect(WebMock).to have_requested(:get, 'https://appstoreconnect.apple.com/testflight/v2/providers/fake-team-id/apps/some-app-id/groups/fake-group-id/builds')
+              it '--log-path option is present' do
+            result = Fastlane::FastFile.new.parse('lane :test do
+                carthage(command: '#{command}', log_path: 'bla.log')
+              end').runner.execute(:test)
+            expect(result).to eq('carthage build --log-path bla.log')
+          end
+        end
+    
+              it 'uses system wide oclint' do
+            expect(result).to include(command)
+          end
+        end
+    
+          it 'should not be fooled by 10 local code signing identities available' do
+        allow(FastlaneCore::CertChecker).to receive(:wwdr_certificate_installed?).and_return(true)
+        allow(FastlaneCore::CertChecker).to receive(:list_available_identities).and_return('     10 valid identities found\n')
+        expect(FastlaneCore::UI).not_to(receive(:error))
+    
+        it 'returns a FontAwesome icon element' do
+      icon = icon_tag('fa-copy')
+      expect(icon).to be_html_safe
+      expect(Nokogiri(icon).at('i.fa.fa-copy')).to be_a Nokogiri::XML::Element
+    end
+    
+      describe 'DotHelper::DotDrawer' do
+    describe '#id' do
+      it 'properly escapes double quotaion and backslash' do
+        expect(DotHelper::DotDrawer.draw(foo: '') {
+          id('hello\\'')
+        }).to eq(''hello\\\\\\''')
+      end
+    end
+  end
+end
+
+    
+        it 'works for queued jobs' do
+      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
     end
   end
     
-          it 'raises an error if something is wrong' do
-        TunesStubbing.itc_stub_applications_broken_first_create
-        expect do
-          Spaceship::Tunes::Application.create!(name: 'My Name',
-                                                sku: 'SKU123',
-                                                bundle_id: 'net.sunapps.123')
-        end.to raise_error('You must provide a company name to use on the App Store. You must provide a company name to use on the App Store.')
+          describe '#import' do
+        it 'uses the existing scenario, updating its data' do
+          expect {
+            scenario_import.import(:skip_agents => true)
+            expect(scenario_import.scenario).to eq(existing_scenario)
+          }.not_to change { users(:bob).scenarios.count }
+    
+      it 'is droppable' do
+    {
+      '{{location.lat}}' => '2.0',
+      '{{location.latitude}}' => '2.0',
+      '{{location.lng}}' => '3.0',
+      '{{location.longitude}}' => '3.0',
+      '{{location.latlng}}' => '2.0,3.0',
+    }.each { |template, result|
+      expect(Liquid::Template.parse(template).render('location' => location.to_liquid)).to eq(result),
+        'expected #{template.inspect} to expand to #{result.inspect}'
+    }
+  end
+end
+
+    
+        def initialize(filters = nil)
+      @filters = filters ? filters.dup : []
+    end
+    
+        def initialize
+      @pages = {}
+    end
+    
+          def filters
+        html_filters.to_a + text_filters.to_a
+      end
+    
+          INDEX = Set.new
+    
+    Then(/^the invalid (.+) release is ignored$/) do |filename|
+  test = 'ls -g #{TestApp.releases_path} | grep #{filename}'
+  _, _, status = vagrant_cli_command('ssh -c #{test.shellescape}')
+  expect(status).to be_success
+end
+    
+    Given(/^the configuration is in a custom location$/) do
+  TestApp.move_configuration_to_custom_location('app')
+end
+    
+    World(VagrantHelpers)
+
+    
+          def fetch(key, default=nil, &block)
+        fetched_keys << key unless fetched_keys.include?(key)
+        peek(key, default, &block)
+      end
+    
+    # Disable the metadata feature
+$METADATA = false
+    
+      if wiki_options[:plantuml_url]
+    Gollum::Filter::PlantUML.configure do |config|
+      puts 'Using #{wiki_options[:plantuml_url]} as PlantUML endpoint'
+      config.url = wiki_options[:plantuml_url]
+    end
+  end
+    
+          it 'registers an offense for one hash parameter with braces and one ' \
+         'without' do
+        expect_offense(<<-RUBY.strip_indent)
+          where({ x: 1 }, y: 2)
+                          ^^^^ Missing curly braces around a hash parameter.
+        RUBY
       end
     end
     
-              zip_build_products_path = Scan.cache[:zip_build_products_path]
-          Actions.lane_context[SharedValues::SCAN_ZIP_BUILD_PRODUCTS_PATH] = zip_build_products_path if zip_build_products_path
+      context 'symmetrical style' do
+    let(:cop_config) { { 'EnforcedStyle' => 'symmetrical' } }
     
-        def self.regular_expression_for_split_on_whitespace_followed_by_parenthesized_version
-      # %r{
-      #   \s # a whitespace character
-      #   (?= # followed by -- using lookahead
-      #   \( # open parenthesis
-      #   [\d\.]+ # our version -- one or more digits or full stops
-      #   \) # close parenthesis
-      #   $ # end of line
-      #   ) # end of lookahead
-      # }
-      /\s(?=\([\d\.]+\)$)/
-    end
+    module RuboCop
+  module AST
+    # A node extension for `case` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `case` nodes within RuboCop.
+    class CaseNode < Node
+      include ConditionalNode
     
-    require 'colored'
-require 'shellwords'
+          # The name of the defined method as a symbol.
+      #
+      # @return [Symbol] the name of the defined method
+      def method_name
+        node_parts[2]
+      end
     
-          run!
-    end
-  end
-end
-
+    module RuboCop
+  module AST
+    # A node extension for `for` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `for` nodes within RuboCop.
+    class ForNode < Node
+      # Returns the keyword of the `for` statement as a string.
+      #
+      # @return [String] the keyword of the `until` statement
+      def keyword
+        'for'
+      end
     
-      def remove_duplicates
-    where = 'WHERE s1.user_id = s2.user_id AND s1.shareable_id = s2.shareable_id AND '\
-      's1.shareable_type = s2.shareable_type AND s1.id > s2.id'
-    if AppConfig.postgres?
-      execute('DELETE FROM share_visibilities AS s1 USING share_visibilities AS s2 #{where}')
-    else
-      execute('DELETE s1 FROM share_visibilities s1, share_visibilities s2 #{where}')
-    end
-  end
-end
-
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
+            [condition, true_branch, false_branch]
+      end
     end
   end
 end
 
     
-    Capybara.javascript_driver = :poltergeist
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-      describe '#create' do
-    let(:like_hash) {
-      {post_id: @target.id}
-    }
+            def key_delta(alignment = :left)
+          return 0 if first.same_line?(second)
+          return 0 if keyword_splat? && alignment == :right
