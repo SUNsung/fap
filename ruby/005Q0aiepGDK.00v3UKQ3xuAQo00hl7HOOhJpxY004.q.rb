523b54f3d333db73a71c199cc7be2e8f134c00ae
SUNsung <weatherius@gmail.com>
2018-10-10 20:08:02 +0300

    Rewrite [005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb] File in: 2018-10-10 20:08:01

--- ruby/005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb
+++ ruby/005Q0aiepGDK.00v3UKQ3xuAQo00hl7HOOhJpxY004.q.rb
@@ -1,178 +1,64 @@
 
         
-        # User for the smoke tests
-if ENV['SMOKE'] == '1'
-  UserEmail.seed do |ue|
-    ue.id = 0
-    ue.email = 'smoke_user@discourse.org'
-    ue.primary = true
-    ue.user_id = 0
-  end
+        class TestApp < Rails::Application
+  config.root = File.dirname(__FILE__)
+  config.session_store :cookie_store, key: 'cookie_store_key'
+  secrets.secret_token    = 'secret_token'
+  secrets.secret_key_base = 'secret_key_base'
+  config.eager_load = false
     
-      # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
-  end
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
+        end
     
-      # POST /resource/unlock
-  def create
-    self.resource = resource_class.send_unlock_instructions(resource_params)
-    yield resource if block_given?
-    
-        def respond
-      if http_auth?
-        http_auth
-      elsif warden_options[:recall]
-        recall
-      else
-        redirect
-      end
-    end
-    
-        def destroy
-      authorize @account_moderation_note, :destroy?
-      @account_moderation_note.destroy!
-      redirect_to admin_account_path(@account_moderation_note.target_account_id), notice: I18n.t('admin.account_moderation_notes.destroyed_msg')
-    end
-    
-            redirect_to admin_report_path(@report), notice: I18n.t('admin.report_notes.created_msg')
-      else
-        @report_notes = @report.notes.latest
-        @report_history = @report.history
-        @form = Form::StatusBatch.new
-    
-      def show
-    @status = status_finder.status
-    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
-  end
-    
-      def hub_topic_uri
-    @_hub_topic_uri ||= Addressable::URI.parse(hub_topic).normalize
-  end
-    
-      #
-  # HTTP server.
-  #
-  def alias
-    super || 'HTTP Server'
-  end
-    
-          # Look for IAX_AUTH_MD5 (2) as an available auth method
-      if res[2][14].unpack('n')[0] & 2 <= 0
-        dprint('REGAUTH: MD5 authentication is not enabled on the server')
-        return
-      end
-    
-      #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
-    
-    
-  def self.create_rakp_hmac_sha1_salt(con_sid, bmc_sid, con_rid, bmc_rid, bmc_gid, auth_level, username)
-    con_sid +
-    bmc_sid +
-    con_rid +
-    bmc_rid +
-    bmc_gid +
-    [ auth_level ].pack('C') +
-    [ username.length ].pack('C') +
-    username
-  end
-    
-              res = decode_kerb_response(data)
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
-          #
-          # @return [String] encoded principal
-          def encode
-            encoded = ''
-            encoded << encode_name_type
-            encoded << [components.length].pack('N')
-            encoded << encode_realm
-            encoded << encode_components
-    
-              # Rex::Proto::Kerberos::Model::ApReq decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'AP-REQ decoding not supported'
-          end
-    
-              # @!attribute type
-          #   @return [Integer] The algorithm used to generate the checksum
-          attr_accessor :type
-          # @!attribute checksum
-          #   @return [String] The checksum itself
-          attr_accessor :checksum
-    
-              def initialize(options = {})
-            self.class.attributes.each do |attr|
-              if options.has_key?(attr)
-                m = (attr.to_s + '=').to_sym
-                self.send(m, options[attr])
-              end
-            end
-          end
-    
-              # Encodes the type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_type
-            bn = OpenSSL::BN.new(type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-      def up_down(change)
-    change.up do
-      Mention.update_all(mentions_container_type: 'Post')
-      change_column :mentions, :mentions_container_type, :string, null: false
-      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
-    end
-    
-    When /^I have user with username '([^']*)' in an aspect called '([^']*)'$/ do |username, aspect|
-  user = User.find_by_username(username)
-  contact = @me.reload.contact_for(user.person)
-  contact.aspects << @me.aspects.find_by_name(aspect)
+    # Declares a dependency to the git repo of CocoaPods gem. This declaration is
+# compatible with the local git repos feature of Bundler.
+#
+def cp_gem(name, repo_name, branch = 'master', path: false)
+  return gem name if SKIP_UNRELEASED_VERSIONS
+  opts = if path
+           { :path => '../#{repo_name}' }
+         else
+           url = 'https://github.com/CocoaPods/#{repo_name}.git'
+           { :git => url, :branch => branch }
+         end
+  gem name, opts
 end
     
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
     end
-  end
-end
-
     
-        # The import/mixin stack.
-    #
-    # @return [Sass::Stack]
-    def stack
-      @stack || global_env.stack
-    end
-  end
-    
-        # Processes the options set by the command-line arguments,
-    # and runs the Sass compiler appropriately.
-    def process_result
-      require 'sass'
-    
-          # @see Base#watched_file?
-      def watched_file?(filename)
-        # Check against the root with symlinks resolved, since Listen
-        # returns fully-resolved paths.
-        filename =~ /\.s[ac]ss$/ && filename.start_with?(@real_root + File::SEPARATOR)
+          # Wraps page formatted data to Nokogiri::HTML document.
+      #
+      def build_document(content)
+        Nokogiri::HTML::fragment(%{<div id='gollum-root'>} + content.to_s + %{</div>}, 'UTF-8')
       end
+    
+        # Extract the 'page' name from the file_path
+    def extract_name(file_path)
+      if file_path[-1, 1] == '/'
+        return nil
+      end
+    
+      def for_each_gem
+    SPREE_GEMS.each do |gem_name|
+      yield 'pkg/spree_#{gem_name}-#{version}.gem'
+    end
+    yield 'pkg/spree-#{version}.gem'
+  end
+    
+          def load_user_roles
+        @current_user_roles = @current_api_user ? @current_api_user.spree_roles.pluck(:name) : []
+      end
+    
+            private
+    
+            def update
+          @option_value = scope.accessible_by(current_ability, :update).find(params[:id])
+          if @option_value.update_attributes(option_value_params)
+            render :show
+          else
+            invalid_resource!(@option_value)
+          end
+        end
