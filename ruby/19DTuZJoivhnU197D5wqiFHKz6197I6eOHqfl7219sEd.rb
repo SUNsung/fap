6a0098d6325392f44227893e175d8f28baf60432
SUNsung <weatherius@gmail.com>
2018-10-10 06:28:48 +0300

    Rewrite [19DTuZJoivhnU197D5wqiFHKz6197I6eOHqfl7219sEd.rb] File in: 2018-10-10 06:28:47

--- ruby/19DTuZJoivhnU197D5wqiFHKz6197I6eOHqfl7219sEd.rb
+++ ruby/19DTuZJoivhnU197D5wqiFHKz6197I6eOHqfl7219sEd.rb
@@ -1,90 +1,114 @@
 
         
-            context 'updating existing users' do
-      it 'follows the edit link' do
-        visit admin_users_path
-        click_on('bob')
-        expect(page).to have_text('Edit User')
+                def render
+          options = @options.stringify_keys
+          options['size'] = options['maxlength'] unless options.key?('size')
+          options['type'] ||= field_type
+          options['value'] = options.fetch('value') { value_before_type_cast } unless field_type == 'file'
+          add_default_name_and_id(options)
+          tag('input', options)
+        end
+    
+        def find(*args)
+      find_all(*args).first || raise(MissingTemplate.new(self, *args))
+    end
+    
+      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
+  p.option 'destination', '-d', '--destination [DIR]',
+    'Destination directory (defaults to ./_site)'
+  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
+  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
+    'Plugins directory (defaults to ./_plugins)'
+  p.option 'layouts_dir', '--layouts DIR', String,
+    'Layouts directory (defaults to ./_layouts)'
+  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+    
+          def before_step(step)
+        @current_step = step
       end
     
-      it 'allows to click on on the agent name in select2 tags' do
-    visit new_agent_path
-    select_agent_type('Website Agent scrapes')
-    select2('SF Weather', from: 'Sources')
-    click_on 'SF Weather'
-    expect(page).to have_content 'Editing your WeatherAgent'
-  end
+      s.rdoc_options = ['--charset=UTF-8']
+  s.extra_rdoc_files = %w(README.markdown LICENSE)
     
-        it 'sends escape characters correctly to the backend' do
-      emitter.events << Event.new(payload: {data: 'Line 1\nLine 2\nLine 3'})
-      formatting_agent.sources << emitter
-      formatting_agent.options.merge!('instructions' => {'data' => '{{data | newline_to_br | strip_newlines | split: '<br />' | join: ','}}'})
-      formatting_agent.save!
+            # Internal: Gets the filename of the sample post to be created
+        #
+        # Returns the filename of the sample post, as a String
+        def initialized_post_name
+          '_posts/#{Time.now.strftime('%Y-%m-%d')}-welcome-to-jekyll.markdown'
+        end
     
-        it 'returns a correct icon tag for GitHub' do
-      icon = omniauth_provider_icon(:github)
-      expect(icon).to be_html_safe
-      elem = Nokogiri(icon).at('i.fa.fa-github')
-      expect(elem).to be_a Nokogiri::XML::Element
-    end
+    module Jekyll
+  module Commands
+    class NewTheme < Jekyll::Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:'new-theme') do |c|
+            c.syntax 'new-theme NAME'
+            c.description 'Creates a new Jekyll theme scaffold'
+            c.option 'code_of_conduct', \
+                     '-c', '--code-of-conduct', \
+                     'Include a Code of Conduct. (defaults to false)'
     
-    describe DotHelper do
-  describe 'with example Agents' do
-    class Agents::DotFoo < Agent
-      default_schedule '2pm'
-    
-        it 'should work with nested arrays' do
-      @agent.options['array'] = ['one', '$.two']
-      LiquidMigrator.convert_all_agent_options(@agent)
-      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'array' => ['one', '{{two}}'], 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
-    end
-    
-      it 'provides hash-style access to its properties with both symbol and string keys' do
-    expect(location[:lat]).to be_a Float
-    expect(location[:lat]).to eq 2.0
-    expect(location['lat']).to be_a Float
-    expect(location['lat']).to eq 2.0
-  end
-    
-        it 'should revert extract and template options for an updated WebsiteAgent' do
-      expect(agent.options).to include('extract' => new_extract,
-                                       'template' => new_template)
-      ConvertWebsiteAgentTemplateForMerge.new.down
-      agent.reload
-      expect(agent.options).to include('extract' => reverted_extract,
-                                       'template' => reverted_template)
+                # stream_file_data would free us from keeping livereload.js in memory
+            # but JRuby blocks on that call and never returns
+            send_data(reload_body)
+            close_connection_after_writing
+          else
+            body = 'This port only serves livereload.js over HTTP.\n'
+            headers = [
+              'HTTP/1.1 400 Bad Request',
+              'Content-Type: text/plain',
+              'Content-Length: #{body.bytesize}',
+              '',
+              '',
+            ].join('\r\n')
+            send_data(headers)
+            send_data(body)
+            close_connection_after_writing
+          end
+        end
+        # rubocop:enable Metrics/MethodLength
+      end
     end
   end
 end
 
     
-              # Decodes the cname field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::PrincipalName]
-          def decode_cname(input)
-            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
+                if key != cleaned_key
+              Jekyll::Deprecator.deprecation_message(
+                'You are using '#{key}'. Normalizing to #{cleaned_key}.'
+              )
+            end
+    
+        def no_subcommand(args)
+      unless args.empty? ||
+          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
+        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
+                          Run `jekyll help` to find out more.'
+        abort
+      end
+    end
+    
+        # The path used after sending reset password instructions
+    def after_sending_reset_password_instructions_path_for(resource_name)
+      new_session_path(resource_name) if is_navigational_format?
+    end
+    
+    class Devise::SessionsController < DeviseController
+  prepend_before_action :require_no_authentication, only: [:new, :create]
+  prepend_before_action :allow_params_authentication!, only: :create
+  prepend_before_action :verify_signed_out_user, only: :destroy
+  prepend_before_action(only: [:create, :destroy]) { request.env['devise.skip_timeout'] = true }
+    
+            class_eval <<-METHODS, __FILE__, __LINE__ + 1
+          def authenticate_#{mapping}!(opts={})
+            opts[:scope] = :#{mapping}
+            warden.authenticate!(opts) if !devise_controller? || opts.delete(:force)
           end
     
-              # Decodes the value from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_value(input)
-            input.value[0].value
-          end
-        end
+          def _devise_route_context
+        @_devise_route_context ||= send(Devise.available_router_name)
       end
     end
   end
 end
-    
-                platform = target.platform_name
-            case platform
-            when :osx
-              execute_command 'xcodebuild -workspace '#{workspace_path}' -scheme '#{scheme_name}' clean build'
-            when :ios
-              test_flag = (scheme_name.start_with? 'Test') ? 'test' : ''
-    
-        not_matching_pipeline = described_class.new(source, pipeline_id, [], settings)
-    expect(subject).not_to eq(not_matching_pipeline)
