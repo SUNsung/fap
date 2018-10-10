9b16a7f0db80d8740c6ec63b0635b20151479279
SUNsung <weatherius@gmail.com>
2018-10-10 10:49:25 +0300

    Rewrite [42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb] File in: 2018-10-10 10:49:25

--- ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
+++ ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
@@ -1,39 +1,135 @@
 
         
-                def bundle_install(path)
+        STDOUT.sync = true
+    
+            def bundle_install(path)
           Jekyll.logger.info 'Running bundle install in #{path.cyan}...'
           Dir.chdir(path) do
             exe = Gem.bin_path('bundler', 'bundle')
             process, output = Jekyll::Utils::Exec.run('ruby', exe, 'install')
     
-            def start(opts)
-          @thread = Thread.new do
-            # Use epoll if the kernel supports it
-            EM.epoll
-            EM.run do
-              EM.error_handler { |e| log_error(e) }
+        def process(args)
+      arg_is_present? args, '--server', 'The --server command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
+                          use the 'build' subcommand.'
+      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
+                          '--watch'.'
+      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
+                          the '--watch' switch.'
+      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
+                          favour of 'highlighter'.'
+      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
+                          your config files.'
+      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
+                          config files.'
+      no_subcommand(args)
+    end
     
-          def output_ext(_ext)
-        '.html'
+    gem 'activemodel-serializers-xml', github: 'rails/activemodel-serializers-xml'
+    
+        # The path used after confirmation.
+    def after_confirmation_path_for(resource_name, resource)
+      if signed_in?(resource_name)
+        signed_in_root_path(resource)
+      else
+        new_session_path(resource_name)
+      end
+    end
+    
+      def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+    redirect_to after_omniauth_failure_path_for(resource_name)
+  end
+    
+      # GET /resource/password/new
+  def new
+    self.resource = resource_class.new
+  end
+    
+      # GET /resource/unlock?unlock_token=abcdef
+  def show
+    self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
+    yield resource if block_given?
+    
+      devise_scope :user do
+    get '/some/route' => 'some_devise_controller'
+  end
+    
+      # Default way to set up Devise. Run rails generate devise_install to create
+  # a fresh initializer with all configuration values.
+  def self.setup
+    yield self
+  end
+    
+          # Tell warden that params authentication is allowed for that specific page.
+      def allow_params_authentication!
+        request.env['devise.allow_params_authentication'] = true
       end
     
-          def uninstall_login_item(*login_items, command: nil, **_)
-        login_items.each do |name|
-          ohai 'Removing login item #{name}'
-          command.run!('/usr/bin/osascript',
-                        args: ['-e', %Q(tell application 'System Events' to delete every login item whose name is '#{name}')],
-                        sudo: false)
-          sleep 1
+          before_action :set_content_type
+      before_action :load_user
+      before_action :authorize_for_order, if: proc { order_token.present? }
+      before_action :authenticate_user
+      before_action :load_user_roles
+    
+              if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
+            if current_api_user.has_spree_role?('admin') && user_id.present?
+              @order.associate_user!(Spree.user_class.find(user_id))
+            end
+    
+            def image_params
+          params.require(:image).permit(permitted_image_attributes)
         end
-      end
     
-          def extract
-        mount do |mounts|
-          begin
-            raise CaskError, 'No mounts found in '#{@path}'; perhaps it is a bad DMG?' if mounts.empty?
-            mounts.each(&method(:extract_mount))
-          ensure
-            mounts.each(&method(:eject))
+              inventory_unit.transaction do
+            if inventory_unit.update_attributes(inventory_unit_params)
+              fire
+              render :show, status: 200
+            else
+              invalid_resource!(inventory_unit)
+            end
+          end
+        end
+    
+            def authorize
+          perform_payment_action(:authorize)
+        end
+    
+              if @product_property.update_attributes(product_property_params)
+            respond_with(@product_property, status: 200, default_template: :show)
+          else
+            invalid_resource!(@product_property)
+          end
+        end
+    
+            def new; end
+    
+              if error
+            unprocessable_entity('#{Spree.t(:shipment_transfer_errors_occured, scope: 'api')} \n#{error}')
+          else
+            @original_shipment.transfer_to_shipment(@variant, @quantity, @target_shipment)
+            render json: { success: true, message: Spree.t(:shipment_transfer_success) }, status: 201
+          end
+        end
+    
+            def scope
+          if params[:country_id]
+            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
+            @country.states.accessible_by(current_ability, :read).order('name ASC')
+          else
+            State.accessible_by(current_ability, :read).order('name ASC')
           end
         end
       end
+    end
+  end
+end
+
+    
+            def stock_location
+          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
+        end
