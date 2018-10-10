10784242bbc7d801907394fa247876905508be9b
SUNsung <weatherius@gmail.com>
2018-10-11 00:22:19 +0300

    Rewrite [11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb] File in: 2018-10-11 00:22:18

--- ruby/11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb
+++ ruby/11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb
@@ -1,56 +1,132 @@
 
         
-        # No trailing slash
-Benchmark.ips do |x|
-  path = '/some/very/very/long/path/to/a/file/i/like'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
-end
-    
-          #
-    
-      s.rdoc_options = ['--charset=UTF-8']
-  s.extra_rdoc_files = %w(README.markdown LICENSE)
-    
-                c.option 'force', '--force', 'Force creation even if PATH already exists'
-            c.option 'blank', '--blank', 'Creates scaffolding but with empty files'
-            c.option 'skip-bundle', '--skip-bundle', 'Skip 'bundle install''
-    
-    gem 'rake'
-gem 'thor'
-gem 'pry', '~> 0.11.0'
-gem 'activesupport', '~> 5.2', require: false
-gem 'yajl-ruby', require: false
-    
-        if version.present?
-      doc = doc.versions.find { |klass| klass.version == version || klass.version_slug == version }
-      raise DocNotFound.new(%(could not find version '#{version}' for doc '#{name}'), name) unless doc
-    elsif version != false
-      doc = doc.versions.first
+            after :each do
+      ENV['SHELL'] = @shell
     end
     
-        def to_json
-      JSON.generate(as_json)
-    end
-    
-        def self.run(urls, options = {}, &block)
-      urls = urls.dup
-      requester = new(options)
-      requester.on_response(&block) if block
-      requester.on_response do # cheap hack to ensure root page is processed first
-        if urls
-          requester.request(urls)
-          urls = nil
-        end
+      ruby_version_is '2.5' do
+    describe ':uplevel keyword argument' do
+      before :each do
+        $VERBOSE = true
       end
-      requester.request(urls.shift)
-      requester.run
-      requester
+    
+    script_binding = binding
+    
+      #
+  # Payload types were identified from xCAT-server source code (IPMI.pm)
+  #
+  PAYLOAD_IPMI = 0
+  PAYLOAD_SOL  = 1
+  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
+  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
+  PAYLOAD_RAKP1 = 0x12
+  PAYLOAD_RAKP2 = 0x13
+  PAYLOAD_RAKP3 = 0x14
+  PAYLOAD_RAKP4 = 0x15
+    
+                cipher = OpenSSL::Cipher.new('rc4')
+            cipher.encrypt
+            cipher.key = k3
+            encrypted = cipher.update(data_encrypt) + cipher.final
+    
+              # Encrypts the Rex::Proto::Kerberos::Model::AuthorizationData
+          #
+          # @param etype [Integer] the crypto schema to encrypt
+          # @param key [String] the key to encrypt
+          # @return [String] the encrypted result
+          # @raise [NotImplementedError] if encryption schema isn't supported
+          def encrypt(etype, key)
+            data = self.encode
+    
+              def initialize(options = {})
+            self.class.attributes.each do |attr|
+              if options.has_key?(attr)
+                m = (attr.to_s + '=').to_sym
+                self.send(m, options[attr])
+              end
+            end
+          end
+    
+              # Decodes the till field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Time]
+          def decode_till(input)
+            input.value[0].value
+          end
+    
+              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_pvno(input)
+            input.value[0].value.to_i
+          end
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
     end
     
-            subclass.class_eval do
-          extend AutoloadHelper
-          autoload_all 'docs/filters/#{to_s.demodulize.underscore}', 'filter'
-        end
+      end
+    
+        def render(context)
+      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
+        gist, file = parts[1].strip, parts[2].strip
+      else
+        gist, file = @text.strip, ''
+      end
+      if gist.empty?
+        ''
+      else
+        script_url = script_url_for gist, file
+        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        html_output_for script_url, code
+      end
+    end
+    
+          Dir.chdir(code_path) do
+        code = file.read
+        @filetype = file.extname.sub('.','') if @filetype.nil?
+        title = @title ? '#{@title} (#{file.basename})' : file.basename
+        url = '/#{code_dir}/#{@file}'
+        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
+        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
+        TemplateWrapper::safe_wrap(source)
+      end
+    end
+  end
+    
+      # Removes trailing forward slash from a string for easily appending url segments
+  def strip_slash(input)
+    if input =~ /(.+)\/$|^\/$/
+      input = $1
+    end
+    input
+  end
+    
+    module Jekyll
