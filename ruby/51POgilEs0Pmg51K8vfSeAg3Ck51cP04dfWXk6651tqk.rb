8f7b7eccb4e8c0302a0586be85d619eeff267672
SUNsung <weatherius@gmail.com>
2018-10-10 08:43:51 +0300

    Rewrite [51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb] File in: 2018-10-10 08:43:50

--- ruby/51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb
+++ ruby/51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb
@@ -1,101 +1,116 @@
 
         
-        describe GivenDailyLike do
+            You can read more about this change at:
+      https://www.playframework.com/documentation/2.3.x/Migration23
+      https://www.playframework.com/documentation/2.3.x/Highlights23
+    EOS
+  when 'haskell-platform' then <<-EOS.undent
+    We no longer package haskell-platform. Consider installing ghc
+    and cabal-install instead:
+      brew install ghc cabal-install
     
-    SiteSetting.refresh!
-uncat_id = SiteSetting.uncategorized_category_id
-uncat_id = -1 unless Numeric === uncat_id
-    
-          if staff.topic_id.nil?
-        creator = PostCreator.new(Discourse.system_user,
-          raw: I18n.t('staff_category_description'),
-          title: I18n.t('category.topic_prefix', category: staff.name),
-          category: staff.name,
-          archetype: Archetype.default
-        )
-        post = creator.create
-    
-    gem 'activemodel-serializers-xml', github: 'rails/activemodel-serializers-xml'
-    
-        if resource.errors.empty?
-      set_flash_message!(:notice, :confirmed)
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+        # Exclude cache, logs, and repository, if they are located under the prefix.
+    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
+      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
     end
+    dirs.delete 'etc'
+    dirs.delete 'var'
+    
+        names = @@remote_tap_formulae['#{user}/#{repo}']
+    user = user.downcase if user == 'Homebrew' # special handling for the Homebrew organization
+    names.select { |name| rx === name }.map { |name| '#{user}/#{repo}/#{name}' }
+  rescue GitHub::HTTPNotFoundError => e
+    opoo 'Failed to search tap: #{user}/#{repo}. Please run `brew update`'
+    []
+  rescue GitHub::Error => e
+    SEARCH_ERROR_QUEUE << e
+    []
   end
     
-      # POST /resource/unlock
-  def create
-    self.resource = resource_class.send_unlock_instructions(resource_params)
-    yield resource if block_given?
-    
-    require 'rack/test'
-require 'action_controller/railtie'
-require 'active_record'
-require 'devise/rails/routes'
-require 'devise/rails/warden_compat'
-    
-          def is_navigational_format?
-        Devise.navigational_formats.include?(request_format)
+        updated_taps = []
+    Tap.each do |tap|
+      next unless tap.git?
+      begin
+        reporter = Reporter.new(tap)
+      rescue Reporter::ReporterRevisionUnsetError => e
+        onoe e if ARGV.homebrew_developer?
+        next
       end
+      if reporter.updated?
+        updated_taps << tap.name
+        hub.add(reporter)
+      end
+    end
     
-              path
+      def std_cmake_parameters
+    '-DCMAKE_INSTALL_PREFIX='#{prefix}' -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev'
+  end
+    
+          class MergeRequest < ActiveRecord::Base
+        self.table_name = 'merge_requests'
+    
+            private
+    
+      not_found do
+    send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), {:status => 404})
+  end
+    
+    module Jekyll
+    
+          Dir.chdir(file_path) do
+        contents = file.read
+        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
+          contents = $1.lstrip
+        end
+        contents = pre_filter(contents)
+        if @raw
+          contents
+        else
+          partial = Liquid::Template.parse(contents)
+          context.stack do
+            partial.render(context)
+          end
         end
       end
-    
-        unless env['devise.skip_trackable']
-      warden.session(scope)['last_request_at'] = Time.now.utc.to_i
     end
   end
 end
-
     
-          it 'left-justifies the result if specified with $ argument is negative' do
-        format('%1$*2$b', 10, -10).should == '1010      '
-        format('%1$*2$B', 10, -10).should == '1010      '
-        format('%1$*2$d', 112, -10).should == '112       '
-        format('%1$*2$i', 112, -10).should == '112       '
-        format('%1$*2$o', 87, -10).should == '127       '
-        format('%1$*2$u', 112, -10).should == '112       '
-        format('%1$*2$x', 196, -10).should == 'c4        '
-        format('%1$*2$X', 196, -10).should == 'C4        '
-    
-    STDOUT.sync = true if ENV['CP_STDOUT_SYNC'] == 'TRUE'
-    
-      def prefixes
-    prefixes = ['/bin', '/usr/bin', '/usr/libexec', xcode_app_path]
-    prefixes << `brew --prefix`.strip unless `which brew`.strip.empty?
-    
-            def validate!
-          super
-          if @pod_name.nil? && !@wipe_all
-            # Security measure, to avoid removing the pod cache too agressively by mistake
-            help! 'You should either specify a pod name or use the --all flag'
+              unless hash[key] == value
+            hash[key] = value
+            hash.delete(key) if value.nil?
           end
-        end
     
-            self.description = <<-DESC
-          Shows the content of the pods cache as a YAML tree output, organized by pod.
-          If `NAME` is given, only the caches for that pod will be included in the output.
-        DESC
+        def initialize(local_file)
+      @local_file = local_file
+    end
     
-                respond_with(@order, default_template: :show, status: 201)
-          else
-            @order = Spree::Order.create!(user: current_api_user, store: current_store)
-            if Cart::Update.call(order: @order, params: order_params).success?
-              respond_with(@order, default_template: :show, status: 201)
-            else
-              invalid_resource!(@order)
-            end
-          end
-        end
+        # remove any version constrain from the Gemfile so the plugin(s) can be updated to latest version
+    # calling update without requirements will remove any previous requirements
+    plugins = plugins_to_update(previous_gem_specs_map)
+    # Skipping the major version validation when using a local cache as we can have situations
+    # without internet connection.
+    filtered_plugins = plugins.map { |plugin| gemfile.find(plugin) }
+      .compact
+      .reject { |plugin| REJECTED_OPTIONS.any? { |key| plugin.options.has_key?(key) } }
+      .each   { |plugin| gemfile.update(plugin.name) }
     
-            def perform_payment_action(action, *args)
-          authorize! action, Payment
-          @payment.send('#{action}!', *args)
-          respond_with(@payment, default_template: :show)
-        end
+          after do
+        ENV.delete('bar')
+        ENV.delete('f$$')
+      end
     
-            def index
-          @states = scope.ransack(params[:q]).result.includes(:country)
+    describe LogStash::Config::PipelineConfig do
+  let(:source) { LogStash::Config::Source::Local }
+  let(:pipeline_id) { :main }
+  let(:ordered_config_parts) do
+    [
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/1', 0, 0, 'input { generator1 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/2', 0, 0,  'input { generator2 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/3', 0, 0, 'input { generator3 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/4', 0, 0, 'input { generator4 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/5', 0, 0, 'input { generator5 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/6', 0, 0, 'input { generator6 }'),
+      org.logstash.common.SourceWithMetadata.new('string', 'config_string', 0, 0, 'input { generator1 }'),
+    ]
+  end
