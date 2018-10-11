a63ea4ec20e4f65c3c4fcebaa1e753ca5ce1572d
SUNsung <weatherius@gmail.com>
2018-10-11 21:19:12 +0300

    Rewrite [25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py] File in: 2018-10-11 21:19:11

--- pyton/25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py
+++ pyton/25GkZ0rV7GfYw252LLXVUMjkY256yMhrY5tQzo25Tol.py
@@ -1,264 +1,36 @@
 
         
-          # Four initially random vectors for the special tokens: <PAD>, <UNK>, <X>, <Y>
-  special_embeddings = np.random.normal(0, 0.1, (4, dim))
-  embeddings = np.vstack((special_embeddings, embeddings))
-  embeddings = embeddings.astype(np.float32)
+            # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
+    ERROR_CTRL_C = 130
     
-        # Note the use of the plural in all of these quantities.  This is intended
-    # to mark that even though a sample z_t from the posterior is thought of a
-    # single sample of a multidimensional gaussian, the prior is actually
-    # thought of as U AR(1) processes, where U is the dimension of the inferred
-    # input.
-    size_bx1 = tf.stack([batch_size, 1])
-    size__xu = [None, z_size]
-    # process variance, the variance at time t over all instantiations of AR(1)
-    # with these parameters.
-    log_evar_inits_1xu = tf.expand_dims(tf.log(noise_variances), 0)
-    self.logevars_1xu = logevars_1xu = \
-        tf.Variable(log_evar_inits_1xu, name=name+'/logevars', dtype=tf.float32,
-                    trainable=do_train_prior_ar_nvar)
-    self.logevars_bxu = logevars_bxu = tf.tile(logevars_1xu, size_bx1)
-    logevars_bxu.set_shape(size__xu) # tile loses shape
+        def get_auth_plugin_mapping(self):
+        return {plugin.auth_type: plugin for plugin in self.get_auth_plugins()}
     
     
-def build_hyperparameter_dict(flags):
-  '''Simple script for saving hyper parameters.  Under the hood the
-  flags structure isn't a dictionary, so it has to be simplified since we
-  want to be able to view file as text.
-    
-    # pick which RNN is used on each trial
-rnn_to_use = rng.randint(2, size=E)
-ext_input = np.repeat(np.expand_dims(rnn_to_use, axis=1), ntimesteps, axis=1)
-ext_input = np.expand_dims(ext_input, axis=2)  # these are 'a's' in the paper
+@pytest.mark.parametrize('argument_name', ['--auth-type', '-A'])
+def test_digest_auth(httpbin_both, argument_name):
+    r = http(argument_name + '=digest', '--auth=user:password',
+             'GET', httpbin_both.url + '/digest-auth/auth/user/password')
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
     
     
-def generate_data(rnn, T, E, x0s=None, P_sxn=None, input_magnitude=0.0,
-                  input_times=None):
-  ''' Generates data from an randomly initialized RNN.
-  Args:
-    rnn: the rnn
-    T: Time in seconds to run (divided by rnn['dt'] to get steps, rounded down.
-    E: total number of examples
-    S: number of samples (subsampling N)
-  Returns:
-    A list of length E of NxT tensors of the network being run.
-  '''
-  N = rnn['N']
-  def run_rnn(rnn, x0, ntime_steps, input_time=None):
-    rs = np.zeros([N,ntime_steps])
-    x_tm1 = x0
-    r_tm1 = np.tanh(x0)
-    tau = rnn['tau']
-    dt = rnn['dt']
-    alpha = (1.0-dt/tau)
-    W = dt/tau*rnn['W']*rnn['g']
-    Bin = dt/tau*rnn['Bin']
-    Bin2 = dt/tau*rnn['Bin2']
-    b = dt/tau*rnn['b']
+class Unaccent(Transform):
+    bilateral = True
+    lookup_name = 'unaccent'
+    function = 'UNACCENT'
     
-      for batch in range(num_batches):
-    x = np.zeros([batch_size, num_steps], dtype=np.int32)
-    y = np.zeros([batch_size, num_steps], dtype=np.int32)
-    w = np.zeros([batch_size, num_steps], dtype=np.float)
+        def __init__(self, get_response=None):
+        if not apps.is_installed('django.contrib.sites'):
+            raise ImproperlyConfigured(
+                'You cannot use RedirectFallbackMiddleware when '
+                'django.contrib.sites is not installed.'
+            )
+        super().__init__(get_response)
     
+        @cached_property
+    def model(self):
+        return self.get_model_class()
     
-def create_masked_cross_entropy_loss(targets, present, logits):
-  '''Calculate the cross entropy loss matrices for the masked tokens.'''
-  cross_entropy_losses = losses.cross_entropy_loss_matrix(targets, logits)
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-        # Compute quantities required for featurewise normalization
-    # (std, mean, and principal components if ZCA whitening is applied).
-    datagen.fit(x_train)
-    
-        toplot = track[i, ::, ::, 0]
-    
-    
-max_features = 20000
-# cut texts after this number of words
-# (among top max_features most common words)
-maxlen = 100
-batch_size = 32
-    
-    # ----------------------------
-    
-    # Read lines from the linkcheck output file
-try:
-    with open('build/linkcheck/output.txt') as out:
-        output_lines = out.readlines()
-except IOError:
-    print('linkcheck output not found; please run linkcheck first.')
-    exit(1)
-    
-            self.concurrent -= 1
-        return ''
-    
-        def run(self, args, opts):
-        '''
-        Entry point for running commands
-        '''
-        raise NotImplementedError
-
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-l', '--list', dest='list', action='store_true',
-            help='List available templates')
-        parser.add_option('-e', '--edit', dest='edit', action='store_true',
-            help='Edit spider after creating it')
-        parser.add_option('-d', '--dump', dest='dump', metavar='TEMPLATE',
-            help='Dump template to standard output')
-        parser.add_option('-t', '--template', dest='template', default='basic',
-            help='Uses a custom template.')
-        parser.add_option('--force', dest='force', action='store_true',
-            help='If the spider already exists, overwrite it with the template')
-    
-        name = 'returns'
-    objects = {
-        'request': Request,
-        'requests': Request,
-        'item': (BaseItem, dict),
-        'items': (BaseItem, dict),
-    }
-    
-        test_suite = 'tests',
-    
-        return yk_streams
-    
-    #----------------------------------------------------------------------
-def ckplayer_download_by_xml(ckinfo, output_dir = '.', merge = False, info_only = False, **kwargs):
-    #Info XML
-    video_info = ckplayer_get_info_by_xml(ckinfo)
-    
-    try:
-        title = kwargs['title']
-    except:
-        title = ''
-    type_ = ''
-    size = 0
-    
-    if len(video_info['links']) > 0:  #has link
-        type_, _ext, size = url_info(video_info['links'][0])  #use 1st to determine type, ext
-    
-    if 'size' in video_info:
-        size = int(video_info['size'])
-    else:
-        for i in video_info['links'][1:]:  #save 1st one
-            size += url_info(i)[2]
-    
-    print_info(site_info, title, type_, size)
-    if not info_only:
-        download_urls(video_info['links'], title, _ext, size, output_dir=output_dir, merge=merge)
-    
-    
-def write_loop_file(records_number, loop_file_path, file_name):
-    with open(loop_file_path, 'a') as file:
-        for i in range(records_number):
-            file.write('file '{}'\n'.format(file_name))
-    
-        for quality in ['1080','720','480','380','240','144','auto']:
-        try:
-            real_url = info[quality][1]['url']
-            if real_url:
-                break
-        except KeyError:
-            pass
-    
-        # Try harder, check all iframes
-    if 'recur_lv' not in kwargs or kwargs['recur_lv'] < recur_limit:
-        r = kwargs.get('recur_lv')
-        if r is None:
-            r = 1
-        else:
-            r += 1
-        iframes = matchall(content, [r'<iframe.+?src=(?:\'|\')(.+?)(?:\'|\')'])
-        for iframe in iframes:
-            if not iframe.startswith('http'):
-                src = urllib.parse.urljoin(url, iframe)
-            else:
-                src = iframe
-            found = embed_download(src, output_dir=output_dir, merge=merge, info_only=info_only, recur_lv=r, **kwargs)
-            if found:
-                return True
-    
-    
-    {
-    {pattern_inline_title = r'<title>([^<]*)</title>'
-pattern_inline_api_key = r'api\.site_key\s*=\s*'([^']+)''
-pattern_inline_img_url = r''url':'([^']+)','key':'[^']+'}}'
-pattern_inline_NSID = r''nsid'\s*:\s*'([^']+)''
-pattern_inline_video_mark = r'('mediaType':'video')'
-    
-        if ns.single and ns.fromfile:
-        parser.error('-s and -f don't go together!')
-    if ns.use_mp is not None and ns.trace:
-        parser.error('-T and -j don't go together!')
-    if ns.use_mp is not None and ns.findleaks:
-        parser.error('-l and -j don't go together!')
-    if ns.failfast and not (ns.verbose or ns.verbose3):
-        parser.error('-G/--failfast needs either -v or -W')
-    if ns.pgo and (ns.verbose or ns.verbose2 or ns.verbose3):
-        parser.error('--pgo/-v don't go together!')
-    
-        argument_signatures = [
-        Sig('-x', type=float),
-        Sig('-3', type=float, dest='y'),
-        Sig('z', nargs='*'),
-    ]
-    failures = ['-x', '-y2.5', '-xa', '-x -a',
-                '-x -3', '-x -3.5', '-3 -3.5',
-                '-x -2.5', '-x -2.5 a', '-3 -.5',
-                'a x -1', '-x -1 a', '-3 -1 a']
-    successes = [
-        ('', NS(x=None, y=None, z=[])),
-        ('-x 2.5', NS(x=2.5, y=None, z=[])),
-        ('-x 2.5 a', NS(x=2.5, y=None, z=['a'])),
-        ('-3.5', NS(x=None, y=0.5, z=[])),
-        ('-3-.5', NS(x=None, y=-0.5, z=[])),
-        ('-3 .5', NS(x=None, y=0.5, z=[])),
-        ('a -3.5', NS(x=None, y=0.5, z=['a'])),
-        ('a', NS(x=None, y=None, z=['a'])),
-        ('a -x 1', NS(x=1.0, y=None, z=['a'])),
-        ('-x 1 a', NS(x=1.0, y=None, z=['a'])),
-        ('-3 1 a', NS(x=None, y=1.0, z=['a'])),
-    ]
-    
-        def __repr__(self):
-        result = ['<%s.%s' % (self.__class__.__module__,
-                              self.__class__.__qualname__)]
-        if not self.closed:
-            result.append(' name=%r mode=%r' % (self.name, self.mode))
-            if self._compress_type != ZIP_STORED:
-                result.append(' compress_type=%s' %
-                              compressor_names.get(self._compress_type,
-                                                   self._compress_type))
-        else:
-            result.append(' [closed]')
-        result.append('>')
-        return ''.join(result)
-    
-        def assertFileNotFound(self, func, *args, **kwargs):
-        with self.assertRaises(FileNotFoundError) as cm:
-            func(*args, **kwargs)
-        self.assertEqual(cm.exception.errno, errno.ENOENT)
-    
-    def skip_if_ABSTFN_contains_backslash(test):
-    '''
-    On Windows, posixpath.abspath still returns paths with backslashes
-    instead of posix forward slashes. If this is the case, several tests
-    fail, so skip them.
-    '''
-    found_backslash = '\\' in ABSTFN
-    msg = 'ABSTFN is not a posix path - tests fail'
-    return [test, unittest.skip(msg)(test)][found_backslash]
-    
-      def __init__(self, linenum, seen_open_brace):
-    self.starting_linenum = linenum
-    self.seen_open_brace = seen_open_brace
-    self.open_parentheses = 0
-    self.inline_asm = _NO_ASM
-    self.check_namespace_indentation = False
+    def print_title(title, pattern = '-'):
+    print('\n'.join(('', title, pattern * len(title)))) 
