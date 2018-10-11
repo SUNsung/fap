ba98ac9bdd0249375372ee965a901797f96d56c6
SUNsung <weatherius@gmail.com>
2018-10-11 08:02:08 +0300

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2018-10-11 08:02:07

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,243 +1,144 @@
 
         
-        #endif  // TENSORFLOW_PYTHON_FRAMEWORK_CPP_SHAPE_INFERENCE_H_
-
-    
-    // Whether the op should be prefixed with underscore.
-bool IsOpWithUnderscorePrefix(const string& s);
-    
-    void CostAnalyzer::PredictCosts(CostEstimator* cost_estimator,
-                                CostGraphDef* cost_graph, int64* total_time) {
-  TF_CHECK_OK(cost_estimator->Initialize(*item_));
-  Costs costs;
-  const Status status =
-      cost_estimator->PredictCosts(item_->graph, cost_graph, &costs);
-  *total_time = costs.execution_time.count();
-  if (!status.ok()) {
-    LOG(ERROR) << 'Could not estimate the cost for item ' << item_->id << ': '
-               << status.error_message();
-    return;
-  }
-}
-    
-    ModelAnalyzer::ModelAnalyzer(const GrapplerItem& item) : item_(item) {}
-    
-    namespace tensorflow {
+        struct SrcLoc {
+  SrcLoc()
+    : start{0,0}
+    , past{0,0}
+  {}
     }
     
-    // Performs a NumPy array cast from type 'From' to 'To'.
-template <typename From, typename To>
-void NPyCast(void* from_void, void* to_void, npy_intp n, void* fromarr,
-             void* toarr) {
-  const From* from = reinterpret_cast<From*>(from_void);
-  To* to = reinterpret_cast<To*>(to_void);
-  for (npy_intp i = 0; i < n; ++i) {
-    to[i] = static_cast<To>(from[i]);
-  }
+    int64_t VMTOC::allocTOC(int64_t target) {
+  folly::MSLGuard g{s_TOC};
+  HPHP::Address addr = m_tocvector->frontier();
+  m_tocvector->qword(target);
+  return addr - (m_tocvector->base() + INT16_MAX + 1);
 }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
-// a copy.
-Status TensorToNdarray(const Tensor& t, PyObject** ret);
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    namespace tensorflow {
+    namespace HPHP {
     }
     
-    namespace tensorflow {
-namespace detail {
-    }
-    }
+    #ifndef incl_HPHP_CONFIG_H_
+#define incl_HPHP_CONFIG_H_
     
-    std::ostream& operator<<(std::ostream& os, ComputationType ty) {
-  return os << ComputationTypeString(ty);
+    
+    {  free(temp);
+  return ret;
 }
     
-    class CUDAExecutor;
-class ScopedActivateContext;
     
-         * For example:
-     * - 0 Means that the action just started.
-     * - 0.5 Means that the action is in the middle.
-     * - 1 Means that the action is over.
-     *
-     * @param time A value between 0 and 1.
-     */
-    virtual void update(float time);
-    /** Return certain target.
-     *
-     * @return A certain target.
-     */
-    Node* getTarget() const { return _target; }
-    /** The action will modify the target properties. 
-     *
-     * @param target A certain target.
-     */
-    void setTarget(Node *target) { _target = target; }
-    /** Return a original Target. 
-     *
-     * @return A original Target.
-     */
-    Node* getOriginalTarget() const { return _originalTarget; }
-    /** 
-     * Set the original target, since target can be nil.
-     * Is the target that were used to run the action. Unless you are doing something complex, like ActionManager, you should NOT call this method.
-     * The target is 'assigned', it is not 'retained'.
-     * @since v0.8.2
-     *
-     * @param originalTarget Is 'assigned', it is not 'retained'.
-     */
-    void setOriginalTarget(Node *originalTarget) { _originalTarget = originalTarget; }
-    /** Returns a tag that is used to identify the action easily. 
-     *
-     * @return A tag.
-     */
-    int getTag() const { return _tag; }
-    /** Changes the tag that is used to identify the action easily. 
-     *
-     * @param tag Used to identify the action easily.
-     */
-    void setTag(int tag) { _tag = tag; }
-    /** Returns a flag field that is used to group the actions easily.
-     *
-     * @return A tag.
-     */
-    unsigned int getFlags() const { return _flags; }
-    /** Changes the flag field that is used to group the actions easily.
-     *
-     * @param flags Used to group the actions easily.
-     */
-    void setFlags(unsigned int flags) { _flags = flags; }
+    {
+    {            m_currentTime[parameter] = 0;
+        }
+        return LearnerBase::CreateCheckpoint();
+    }
     
+        private:
+        std::unordered_set<StreamInformation> m_streamInfos;
+        bool m_epochEndReached;
+        size_t m_numWorkers;
+        size_t m_workerRank;
+        size_t m_prevMinibatchSize;
+        size_t m_maxNumSamplesToRead;
+        size_t m_maxNumSweepsToRead;
+        size_t m_truncationLength;
+        size_t m_maxErrors;
+        std::unordered_map<StreamInformation, MinibatchData> m_minibatchData;
     
-    {    points->autorelease();
-    return points;
-}
+            if (bufferSizeInBytes < (viewShape.TotalSize() * sizeof(V1ElemType)))
+            InvalidArgument('Size (%d) of the specified buffer for creating the NDArrayView is smaller than the specified view shape '%S'.',
+                            (int)bufferSizeInBytes, viewShape.AsString().c_str());
     
-        /** Returns a new copy of the array reversed. User is responsible for releasing this copy.
-     *
-     * @js NA
-     * @return A new copy of the array reversed.
-     */
-    PointArray* reverse() const;
+            if (sectionOffset.size() > m_maskShape.Rank())
+            LogicError('NDMask::MaskSection: The sectionOffset dimensionality (%d) must be <= rank (%d) of 'this' mask.', (int)sectionOffset.size(), (int)m_maskShape.Rank());
     
-    CallFuncN * CallFuncN::clone() const
+            static bool IsUDF(const FunctionPtr& f);
+    
+            void Erase() override
+        {
+            if (IsPacked())
+            {
+                m_packedData = nullptr;
+                m_packedDataLayout = nullptr;
+                m_isPacked = false;
+            }
+            else
+                Value::Erase();
+        }
+    
+    namespace CNTK
 {
-    // no copy constructor
-    auto a = new (std::nothrow) CallFuncN();
-    }
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __ACTION_CCPAGETURN3D_ACTION_H__
-#define __ACTION_CCPAGETURN3D_ACTION_H__
-    
-    bool ProgressFromTo::initWithDuration(float duration, float fromPercentage, float toPercentage)
-{
-    if (ActionInterval::initWithDuration(duration))
+    Variable::Variable(const FunctionPtr& function)
+        : Variable(function->Output())
     {
-        _to = toPercentage;
-        _from = fromPercentage;
     }
     }
     
-        /** 
-     * @brief Initializes the action with the number of jumps, the sin amplitude, the grid size and the duration.
-     * @param duration Specify the duration of the JumpTiles3D action. It's a value in seconds.
-     * @param gridSize Specify the size of the grid.
-     * @param numberOfJumps Specify the jump tiles count.
-     * @param amplitude Specify the amplitude of the JumpTiles3D action.
-     * @return If the initialization success, return true; otherwise, return false.
-     */
-    bool initWithDuration(float duration, const Size& gridSize, unsigned int numberOfJumps, float amplitude);
-    
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-    
-    
-    {// end of base_node group
-/// @}
-    
-      /**
-   * @brief Send messages
-   * @param frames The messages to send.
-   * @param frame_num The amount of messages to send.
-   * @return The status of the sending action which is defined by
-   *         apollo::common::ErrorCode.
-   */
-  apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
-                                 int32_t *const frame_num) override;
-    
-    TEST_F(FakeCanClientTest, SendMessage) {
-  std::vector<CanFrame> frames;
-  frames.resize(FRAME_LEN);
-  for (int32_t i = 0; i < FRAME_LEN; ++i) {
-    frames[i].id = 1 & 0x3FF;
-    frames[i].len = 8;
-    frames[i].data[7] = 1 % 256;
-    for (int32_t j = 0; j < 7; ++j) {
-      frames[i].data[j] = j;
-    }
-  }
+            VariableFields(const NDShape& shape, VariableKind varType, ::CNTK::DataType type, const std::weak_ptr<Function>& ownerFunction, const NDArrayViewPtr& value, bool needsGradient, const std::vector<Axis>& dynamicAxes, bool isSparse, const std::wstring& name, const std::wstring& uid)
+            : m_shape(shape), m_varKind(varType), m_dataType(type), m_ownerFunction(ownerFunction), m_value(value), m_needsGradient(needsGradient), m_dynamicAxes(dynamicAxes), m_isSparse(isSparse), m_name(name), m_uid(uid), m_valueTimeStamp(0)
+        {
+            if (value && (type != value->GetDataType()))
+                InvalidArgument('The DataType of the Parameter/Constant Variable '%S' does not match the DataType of the associated Value', AsString().c_str());
     }
     
-      // 1. set baudrate to 500k
-  ret = bcan_set_baudrate(_dev_handler, BCAN_BAUDRATE_500K);
-  if (ret != ErrorCode::OK) {
-    AERROR << 'Set baudrate error Code: ' << ret;
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
-  }
     
-    #endif  // MODULES_DRIVERS_CANBUS_CANCARD_CLIENT_SOCKET_CAN_CLIENT_RAW_H_
-
+    {    std::string GetCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
+};
     
-    #include 'gtest/gtest.h'
-    
-    
+        // return the randomized feature bounds for a time range
+    std::pair<size_t, size_t> Bounds(size_t ts, size_t te) const
     {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    TEST(CanSenderTest, OneRunCase) {
-  CanSender<::apollo::canbus::ChassisDetail> sender;
-  can::FakeCanClient can_client;
-  sender.Init(&can_client, true);
+        size_t tbegin = max(ts, randomizationrange / 2) - randomizationrange / 2;
+        size_t tend = min(te + randomizationrange / 2, map.size());
+        return std::make_pair<size_t, size_t>(std::move(tbegin), move(tend));
     }
     
-    /**
- * @file
- * @brief The class of ProtocolData
+        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange& fr) override
+    {
+        Matrix<ElemType> sliceInput1Value = Input(1)->ValueFor(fr);
+        Matrix<ElemType> sliceOutputValue = ValueFor(fr);
+    }
+    
+    
+    {/**
+ * @brief Start a file carve of the given paths
+ *
+ * @return A status returning if the carves were started successfully
  */
+Status carvePaths(const std::set<std::string>& paths);
+} // namespace osquery
+
+    
+    #pragma once
+    
+    REGISTER_INTERNAL(LoggerConfigParserPlugin, 'config_parser', 'logger');
+    
+      // We use a restricted scope below to change the data structure from
+  // an array to a set. This lets us do deletes much more efficiently
+  std::vector<std::string> created_views;
+  std::set<std::string> erase_views;
+  {
+    std::vector<std::string> old_views_vec;
+    scanDatabaseKeys(kQueries, old_views_vec, kConfigViews);
+    for (const auto& view : old_views_vec) {
+      erase_views.insert(view.substr(kConfigViews.size()));
+    }
+  }
+    
+    
+    {  ASSERT_FALSE(fpack.getPlatform().empty());
+  ASSERT_FALSE(fpack.getVersion().empty());
+  ASSERT_EQ(fpack.getShard(), 1U);
+}
+    
+    
+    {    // Code should never reach this point
+    VLOG(1) << 'Could not start extension process: ' << exec_path;
+    Initializer::shutdown(EXIT_FAILURE);
+    return std::shared_ptr<PlatformProcess>();
+  }
+    
+      // This class is the SAME, there was no type override.
+  auto another_basic_pub = std::make_shared<AnotherBasicEventPublisher>();
+  another_basic_pub->setName(basic_pub->getName());
+  status = EventFactory::registerEventPublisher(another_basic_pub);
+  EXPECT_FALSE(status.ok());
