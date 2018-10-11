893776cff6d0ae7e2e0d8fef1e3382d4b7ad8360
SUNsung <weatherius@gmail.com>
2018-10-11 23:42:28 +0300

    Rewrite [54IY.h2OgyP7.54zwx5FvpsNhI543Oiom0QpGS.54I83.cpp] File in: 2018-10-11 23:42:28

--- cccccc/54IY.h2OgyP7.54zwx5FvpsNhI543Oiom0QpGS.54I83.cpp
+++ cccccc/54IY.h2OgyP7.54zwx5FvpsNhI543Oiom0QpGS.54I83.cpp
@@ -1,182 +1,318 @@
 
         
-        
-    {
-    {    std::vector<T> ret(ret_index.size());
-    for (size_t i = 0; i < ret_index.size(); ++i) {
-      ret[i] = all_data[ret_index[i]];
+        void convert_dataset(const char* image_filename, const char* label_filename,
+        const char* db_filename) {
+  // Open files
+  std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
+  std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
+  CHECK(image_file) << 'Unable to open file ' << image_filename;
+  CHECK(label_file) << 'Unable to open file ' << label_filename;
+  // Read the magic and the meta data
+  uint32_t magic;
+  uint32_t num_items;
+  uint32_t num_labels;
+  uint32_t rows;
+  uint32_t cols;
     }
-    return ret;
-  }
-  /*!
-   * \brief create OperatorProperty
-   * \param type_name the type string of the OperatorProperty
-   * \return a new constructed OperatorProperty
-   */
-  static OperatorProperty *Create(const char* type_name);
-  /*! \return execution type of the operator */
-  virtual ExecType exec_type() const {
-    return ExecType::kSync;
-  }
-};
-    
-    template <typename Dtype>
-class LayerRegistry {
- public:
-  static ::caffe::Layer<Dtype> * CreateLayer(const ::caffe::LayerParameter& param) {
-    ::caffe::shared_ptr< ::caffe::Layer<Dtype> > ptr =
-      ::caffe::LayerRegistry<Dtype>::CreateLayer(param);
-    // avoid caffe::layer destructor, which deletes the weights layer owns
-    new ::caffe::shared_ptr< ::caffe::Layer<Dtype> >(ptr);
-    return ptr.get();
-  }
-};
-    
-    MXNET_REGISTER_OP_PROPERTY(CaffeOp, CaffeOpProp)
-.describe('Apply caffe operator')
-.add_argument('data', 'Symbol[]', 'List of tensors')
-.add_arguments(CaffeOpParam::__FIELDS__());
-    
-    template <std::size_t kNumGpus, std::size_t kStreams>
-void StreamManager<kNumGpus, kStreams>::Finalize() {
-#if MXNET_USE_CUDA
-  for (std::size_t i = 0; i < kNumGpus; ++i) {
-    if (gpu_cnt_.at(i) != -1) {
-      for (auto&& j : gpu_streams_.at(i)) {
-        // Catch exception for CUDA driver shutdown
-        MSHADOW_CATCH_ERROR(mshadow::DeleteStream<gpu>(j));
-      }
-      gpu_cnt_.at(i) = -1;
-    }
-  }
-#endif  // MXNET_USE_CUDA
-}
-    
-    namespace mxnet {
-namespace io {
-/*!
- * \brief OpenCV based Image augmenter,
- *  The augmenter can contain internal temp state.
- */
-class ImageAugmenter {
- public:
-  /*!
-   *  \brief Initialize the Operator by setting the parameters
-   *  This function need to be called before all other functions.
-   *  \param kwargs the keyword arguments parameters
-   */
-  virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) = 0;
-  /*!
-   * \brief augment src image.
-   *   this function is not thread safe, and will only be called by one thread
-   *   however, it will tries to re-use memory space as much as possible
-   * \param src the source image
-   * \param prnd pointer to random number generator.
-   * \return The processed image.
-   */
-  virtual cv::Mat Process(const cv::Mat &src, std::vector<float> *label,
-                          common::RANDOM_ENGINE *prnd) = 0;
-  // virtual destructor
-  virtual ~ImageAugmenter() {}
-  /*!
-   * \brief factory function
-   * \param name Name of the augmenter
-   * \return The created augmenter.
-   */
-  static ImageAugmenter* Create(const std::string& name);
-};
-    }
-    }
-    
-    #include <iostream>
-    
-    #include <string>
-#include <vector>
-    
-    
-    {
-    {  // Reset the configuration.
-  c.reset();
-}
-}
-
-    
-    class ViewsConfigParserPluginTests : public testing::Test {};
-    
-      fpack.platform_ = 'posix';
-  if (isPlatform(PlatformType::TYPE_POSIX) ||
-      isPlatform(PlatformType::TYPE_LINUX) ||
-      isPlatform(PlatformType::TYPE_OSX) ||
-      isPlatform(PlatformType::TYPE_FREEBSD)) {
-    EXPECT_TRUE(fpack.checkPlatform());
-  }
-    
-      /// Get state information for a worker or extension child.
-  PerformanceState& getState(const PlatformProcess& child);
-  PerformanceState& getState(const std::string& extension);
-    
-    void Initializer::platformSetup() {
-  // Initialize the COM libraries utilized by Windows WMI calls.
-  auto ret = ::CoInitializeEx(0, COINIT_MULTITHREADED);
-  if (ret != S_OK) {
-    ::CoUninitialize();
-  }
-}
-    
-    
-    {  return doc.toString(json);
-}
-    
-      /// The configuration finished loading or was updated.
-  void configure() override;
-    
-    #include <osquery/events.h>
-#include <osquery/flags.h>
-#include <osquery/tables.h>
-    
-    static std::string getNoteRawContent(const std::string& fileName) {
-  auto subProc = folly::Subprocess(
-      std::vector<std::string>{
-          'objdump',
-          '--full-content',
-          '--section=.note.' + kUSDTSubsectionName,
-          fileName,
-      },
-      folly::Subprocess::Options().pipeStdout().usePath());
-  auto output = subProc.communicate();
-  auto retCode = subProc.wait();
-  CHECK(retCode.exited());
-  CHECK(output.second.empty());
-  return output.first;
-}
-    
-    // Return the state size needed by RNG, expressed as a number of uint32_t
-// integers. Specialized for all templates specified in the C++11 standard.
-// For some (mersenne_twister_engine), this is exported as a state_size static
-// data member; for others, the standard shows formulas.
-    
-    #include <boost/regex/pending/unicode_iterator.hpp>
     
     /**
- * This functions is an extension point when FOLLY_HAVE_WEAK_SYMBOLS is true.
- * There is a default no-op implementation provided which can be overrided by
- * linking in a library which provides its own definition.
- *
- * @param codecType   The type of the codec for this counter.
- * @param codecName   The name of the codec for this counter. If the codecName
- *                    is empty it should be defaulted using the codecType.
- * @param level       Optionally the level used to construct the codec.
- * @param key         The key of the counter.
- * @param counterType The type of the counter.
- * @returns           A function to increment the counter for the given key and
- *                    type. It may be an empty folly::Function.
+ Forward declare boost::thread instead of including boost/thread.hpp
+ to avoid a boost/NVCC issues (#1009, #1010) on OSX.
  */
-folly::Function<void(double)> makeCompressionCounterHandler(
-    folly::io::CodecType codecType,
-    folly::StringPiece codecName,
-    folly::Optional<int> level,
-    CompressionCounterKey key,
-    CompressionCounterType counterType);
+namespace boost { class thread; }
     
-      // one cache shared by all cpus
-  rv.numCachesByLevel.push_back(numCpus);
+    
+ protected:
+  /** The protobuf that stores the layer parameters */
+  LayerParameter layer_param_;
+  /** The phase: TRAIN or TEST */
+  Phase phase_;
+  /** The vector that stores the learnable parameters as a set of blobs. */
+  vector<shared_ptr<Blob<Dtype> > > blobs_;
+  /** Vector indicating whether to compute the diff of each param blob. */
+  vector<bool> param_propagate_down_;
+    
+    #define REGISTER_LAYER_CLASS(type)                                             \
+  template <typename Dtype>                                                    \
+  shared_ptr<Layer<Dtype> > Creator_##type##Layer(const LayerParameter& param) \
+  {                                                                            \
+    return shared_ptr<Layer<Dtype> >(new type##Layer<Dtype>(param));           \
+  }                                                                            \
+  REGISTER_LAYER_CREATOR(type, Creator_##type##Layer)
+    
+    
+    {}  // namespace caffe
+    
+    #endif  // CAFFE_BASE_CONVOLUTION_LAYER_HPP_
+
+    
+      virtual inline const char* type() const { return 'BatchNorm'; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+    
+     protected:
+  /**
+   * @param bottom input Blob vector (length 2+)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_1 @f$
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_2 @f$
+   *   -# ...
+   *   - K @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_K @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (KN \times C \times H \times W) @f$ if axis == 0, or
+   *      @f$ (N \times KC \times H \times W) @f$ if axis == 1:
+   *      the concatenated output @f$
+   *        y = [\begin{array}{cccc} x_1 & x_2 & ... & x_K \end{array}]
+   *      @f$
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+    
+    {  Blob<Dtype> diff_;  // cached for backward pass
+  Blob<Dtype> dist_sq_;  // cached for backward pass
+  Blob<Dtype> diff_sq_;  // tmp storage for gpu forward pass
+  Blob<Dtype> summer_vec_;  // tmp storage for gpu forward pass
+};
+    
+    #ifdef USE_CUDNN
+/*
+ * @brief cuDNN implementation of ConvolutionLayer.
+ *        Fallback to ConvolutionLayer for CPU mode.
+ *
+ * cuDNN accelerates convolution through forward kernels for filtering and bias
+ * plus backward kernels for the gradient w.r.t. the filters, biases, and
+ * inputs. Caffe + cuDNN further speeds up the computation through forward
+ * parallelism across groups and backward parallelism across gradients.
+ *
+ * The CUDNN engine does not have memory overhead for matrix buffers. For many
+ * input and filter regimes the CUDNN engine is faster than the CAFFE engine,
+ * but for fully-convolutional models and large inputs the CAFFE engine can be
+ * faster as long as it fits in memory.
+*/
+template <typename Dtype>
+class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
+ public:
+  explicit CuDNNConvolutionLayer(const LayerParameter& param)
+      : ConvolutionLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNConvolutionLayer();
+    }
+    
+    // Next, associate a list of types with the test case, which will be
+// repeated for each type in the list.  The typedef is necessary for
+// the macro to parse correctly.
+typedef testing::Types<char, int, unsigned int> MyTypes;
+TYPED_TEST_CASE(FooTest, MyTypes);
+    
+    // We cannot use std::numeric_limits<T>::max() as it clashes with the max()
+// macro defined by <windows.h>.
+template <>
+inline float FloatingPoint<float>::Max() { return FLT_MAX; }
+template <>
+inline double FloatingPoint<double>::Max() { return DBL_MAX; }
+    
+      // Compares two wide C strings.  Returns true iff they have the same
+  // content.
+  //
+  // Unlike wcscmp(), this function can handle NULL argument(s).  A
+  // NULL C string is considered different to any non-NULL C string,
+  // including the empty string.
+  static bool WideCStringEquals(const wchar_t* lhs, const wchar_t* rhs);
+    
+    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
+    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
+    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
+    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
+    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
+    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17>
+struct Templates17 {
+  typedef TemplateSel<T1> Head;
+  typedef Templates16<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+      T15, T16, T17> Tail;
+};
+    
+      // Copy c'tor
+  MyString(const MyString& string) : c_string_(NULL) {
+    Set(string.c_string_);
+  }
+    
+    template <typename T>
+inline StringName __constant_get_enum_name(T param, const String &p_constant) {
+	if (GetTypeInfo<T>::VARIANT_TYPE == Variant::NIL)
+		ERR_PRINTS('Missing VARIANT_ENUM_CAST for constant's enum: ' + p_constant);
+	return GetTypeInfo<T>::get_class_info().class_name;
+}
+    
+    #include 'collision_object_bullet.h'
+    
+    	virtual void getAllContactManifolds(btManifoldArray &manifoldArray) {
+		///should we use m_ownManifold to avoid adding duplicates?
+		if (m_manifoldPtr && m_ownManifold)
+			manifoldArray.push_back(m_manifoldPtr);
+	}
+	struct CreateFunc : public btCollisionAlgorithmCreateFunc {
+    }
+    
+    	void set_param(PhysicsServer::PinJointParam p_param, real_t p_value);
+	real_t get_param(PhysicsServer::PinJointParam p_param) const;
+    
+    #ifndef SLIDER_JOINT_BULLET_H
+#define SLIDER_JOINT_BULLET_H
+    
+    	if (id == 0) {
+		r_error.error = Variant::CallError::CALL_ERROR_INSTANCE_IS_NULL;
+		return Variant();
+	}
+	Object *obj = ObjectDB::get_instance(id);
+    
+    
+    {	ERR_FAIL_COND(!zfile);
+	unzSeekCurrentFile(zfile, get_len() + p_position);
+};
+    
+    
+    { private:
+  friend class CarverTests;
+  FRIEND_TEST(CarverTests, test_carve_files_locally);
+};
+    
+    
+    {} // namespace osquery
+
+    
+      // This looks funky, because the parser is named 'options' and it claims
+  // ownership of a single top-level-key called 'options'.
+  const auto& doc = c.getParser('options')->getData().doc()['options'];
+    
+      /**
+   * @brief Update the internal context buffer with additional content
+   *
+   * This method allows you to chunk up large content so that it doesn't all
+   * have to be loaded into memory at the same time
+   *
+   * @param buffer The buffer to be hashed
+   * @param size The size of the buffer to be hashed
+   */
+  void update(const void* buffer, size_t size);
+    
+      /// Add extensions autoloadable paths.
+  void addExtensionPath(const std::string& path);
+    
+      /**
+   * Some decision making code refactored in needMonitoring before calling
+   * addMonitor in the context of monitorSubscription.
+   * Decision to call addMonitor from the context of monitorSubscription
+   * is done based on the status change time of file/directory, since
+   * creation time is not available on linux.
+   */
+  bool needMonitoring(const std::string& path,
+                      INotifySubscriptionContextRef& isc,
+                      uint32_t mask,
+                      bool recursive,
+                      bool add_watch);
+    
+    
+    {  // When the hex fails to decode the input value is returned as the result.
+  auto decoded_fail = DecodeAuditPathValues('7');
+  EXPECT_EQ(decoded_fail, '7');
+}
+    
+    
+    {}  //  namespace rocksdb
+
+    
+      env.now_micros_ += 100u;  // sleep credit 100
+  // 1000 used, 7240 left
+  ASSERT_EQ(static_cast<uint64_t>(0), controller.GetDelay(&env, 1000u));
+    
+    #pragma once
+    
+      // Truncate is necessary to trim the file to the correct size
+  // before closing. It is not always possible to keep track of the file
+  // size due to whole pages writes. The behavior is undefined if called
+  // with other writes to follow.
+  virtual Status Truncate(uint64_t size) override {
+    return file_->Truncate(size + prefixLength_);
+  }
+    
+      virtual Status LockFile(const std::string& fname, FileLock** flock) override;
+    
+    class MyMerge : public rocksdb::MergeOperator {
+ public:
+  virtual bool FullMergeV2(const MergeOperationInput& merge_in,
+                           MergeOperationOutput* merge_out) const override {
+    merge_out->new_value.clear();
+    if (merge_in.existing_value != nullptr) {
+      merge_out->new_value.assign(merge_in.existing_value->data(),
+                                  merge_in.existing_value->size());
+    }
+    for (const rocksdb::Slice& m : merge_in.operand_list) {
+      fprintf(stderr, 'Merge(%s)\n', m.ToString().c_str());
+      // the compaction filter filters out bad values
+      assert(m.ToString() != 'bad');
+      merge_out->new_value.assign(m.data(), m.size());
+    }
+    return true;
+  }
+    }
+    
+      delete db;
+    
+    
+    {
+    {}  // namespace
+}  // namespace internal
+    
+    // FIXME: This function is a hack so that benchmark.cc can access
+// `BenchmarkFamilies`
+bool FindBenchmarksInternal(const std::string& re,
+                            std::vector<Benchmark::Instance>* benchmarks,
+                            std::ostream* Err) {
+  return BenchmarkFamilies::GetInstance()->FindBenchmarks(re, benchmarks, Err);
+}
+    
+    std::string FormatString(const char* msg, va_list args) {
+  // we might need a second shot at this, so pre-emptivly make a copy
+  va_list args_cp;
+  va_copy(args_cp, args);
+    }
+    
+    void Increment(UserCounters *l, UserCounters const& r) {
+  // add counters present in both or just in *l
+  for (auto &c : *l) {
+    auto it = r.find(c.first);
+    if (it != r.end()) {
+      c.second.value = c.second + it->second;
+    }
+  }
+  // add counters present in r, but not in *l
+  for (auto const &tc : r) {
+    auto it = l->find(tc.first);
+    if (it == l->end()) {
+      (*l)[tc.first] = tc.second;
+    }
+  }
+}
+    
+        // print the header
+    for (auto B = elements.begin(); B != elements.end();) {
+      Out << *B++;
+      if (B != elements.end()) Out << ',';
+    }
+    for (auto B = user_counter_names_.begin(); B != user_counter_names_.end();) {
+      Out << ',\'' << *B++ << '\'';
+    }
+    Out << '\n';
+    
+    class Barrier {
+ public:
+  Barrier(int num_threads) : running_threads_(num_threads) {}
+    }
