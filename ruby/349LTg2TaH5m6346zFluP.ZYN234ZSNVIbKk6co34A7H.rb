69d748acef6d2d94b9ad96262ab40b54718c4dba
SUNsung <weatherius@gmail.com>
2018-10-13 01:45:35 +0300

    Create [349LTg2TaH5m6346zFluP.ZYN234ZSNVIbKk6co34A7H.rb] File in: 2018-10-13 01:45:34

---
+++ ruby/349LTg2TaH5m6346zFluP.ZYN234ZSNVIbKk6co34A7H.rb
@@ -0,0 +1,8 @@
+        reaction
+      end
+    
+        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      subject.options[:allow_empty_referrer] = false
+      expect(subject.referrer(env)).to be_nil
+    end
