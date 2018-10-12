788ce8d22212dd74ba380cbdfad5c4bebc108c8f
SUNsung <weatherius@gmail.com>
2018-10-12 14:28:33 +0300

    Rewrite [00zBLWtqMM9qY00QkuzCJdXjm60028UszKeGc200MSP.py] File in: 2018-10-12 14:28:32

--- pyton/00zBLWtqMM9qY00QkuzCJdXjm60028UszKeGc200MSP.py
+++ pyton/00zBLWtqMM9qY00QkuzCJdXjm60028UszKeGc200MSP.py
@@ -1,84 +1,146 @@
 
         
-                        elif char in '=!<':
-                    # lookahead assertions
-                    dir = 1
-                    if char == '<':
-                        char = sourceget()
-                        if char is None:
-                            raise source.error('unexpected end of pattern')
-                        if char not in '=!':
-                            raise source.error('unknown extension ?<' + char,
-                                               len(char) + 2)
-                        dir = -1 # lookbehind
-                        lookbehindgroups = state.lookbehindgroups
-                        if lookbehindgroups is None:
-                            state.lookbehindgroups = state.groups
-                    p = _parse_sub(source, state, verbose, nested + 1)
-                    if dir < 0:
-                        if lookbehindgroups is None:
-                            state.lookbehindgroups = None
-                    if not sourcematch(')'):
-                        raise source.error('missing ), unterminated subpattern',
-                                           source.tell() - start)
-                    if char == '=':
-                        subpatternappend((ASSERT, (dir, p)))
-                    else:
-                        subpatternappend((ASSERT_NOT, (dir, p)))
-                    continue
-    
-        def test_write_non_pyfile(self):
-        with TemporaryFile() as t, zipfile.PyZipFile(t, 'w') as zipfp:
-            with open(TESTFN, 'w') as f:
-                f.write('most definitely not a python file')
-            self.assertRaises(RuntimeError, zipfp.writepy, TESTFN)
-            unlink(TESTFN)
-    
-        If we give any reporting options when we set up the tests,
-    however:
+        
+def main():
+    if len(sys.argv) < 2:
+        print('No file passed (file should contain Markdown table syntax)')
+        sys.exit(1)
+    check_format(sys.argv[1])
+    if len(errors) > 0:
+        for err in errors:
+            print(err)
+        sys.exit(1)
     
     
-# Return the longest prefix of all list elements.
-def commonprefix(m):
-    'Given a list of pathnames, returns the longest common leading component'
-    if not m: return ''
-    # Some people pass in a list of pathname parts to operate in an OS-agnostic
-    # fashion; don't try to translate in that case as that's an abuse of the
-    # API and they are already doing what they need to be OS-agnostic and so
-    # they most likely won't be using an os.PathLike object in the sublists.
-    if not isinstance(m[0], (list, tuple)):
-        m = tuple(map(os.fspath, m))
-    s1 = min(m)
-    s2 = max(m)
-    for i, c in enumerate(s1):
-        if c != s2[i]:
-            return s1[:i]
-    return s1
+@functools.lru_cache()
+def get_citext_oids(connection_alias):
+    '''Return citext array OIDs.'''
+    with connections[connection_alias].cursor() as cursor:
+        cursor.execute('SELECT typarray FROM pg_type WHERE typname = 'citext'')
+        return tuple(row[0] for row in cursor)
     
-        def test_formats(self):
-        # let's create a command and make sure
-        # we can set the format
-        dist = self.create_dist()[1]
-        cmd = bdist(dist)
-        cmd.formats = ['msi']
-        cmd.ensure_finalized()
-        self.assertEqual(cmd.formats, ['msi'])
-    
-    @unittest.skipIf(getattr(bdist_wininst, '_unsupported', False),
-    'bdist_wininst is not supported in this install')
-class BuildWinInstTestCase(support.TempdirManager,
-                           support.LoggingSilencer,
-                           unittest.TestCase):
-    
-        def test_addsitedir(self):
-        # Same tests for test_addpackage since addsitedir() essentially just
-        # calls addpackage() for every .pth file in the directory
-        pth_file = PthFile()
-        pth_file.cleanup(prep=True) # Make sure that nothing is pre-existing
-                                    # that is tested for
+            r = None
         try:
-            pth_file.create()
-            site.addsitedir(pth_file.base_dir, set())
-            self.pth_file_tests(pth_file)
-        finally:
-            pth_file.cleanup()
+            r = Redirect.objects.get(site=current_site, old_path=full_path)
+        except Redirect.DoesNotExist:
+            pass
+        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
+            try:
+                r = Redirect.objects.get(
+                    site=current_site,
+                    old_path=request.get_full_path(force_append_slash=True),
+                )
+            except Redirect.DoesNotExist:
+                pass
+        if r is not None:
+            if r.new_path == '':
+                return self.response_gone_class()
+            return self.response_redirect_class(r.new_path)
+    
+        def load(self):
+        try:
+            session_data = self._cache.get(self.cache_key)
+        except Exception:
+            # Some backends (e.g. memcache) raise an exception on invalid
+            # cache keys. If this happens, reset the session. See #17810.
+            session_data = None
+        if session_data is not None:
+            return session_data
+        self._session_key = None
+        return {}
+    
+    def os_constant(key):
+    # XXX TODO: In the future, this could return different constants
+    #           based on what OS we are running under.  To see an
+    #           approach to how to handle different OSes, see the
+    #           apache version of this file.  Currently, we do not
+    #           actually have any OS-specific constants on Nginx.
+    '''
+    Get a constant value for operating system
+    
+        def test_encode(self):
+        self.assertEqual('x', self.field.encode('x'))
+    
+        def test_json_de_serializable(self):
+        self.assertTrue(isinstance(self.certr, jose.JSONDeSerializable))
+        from acme.messages import CertificateResource
+        self.assertEqual(
+            self.certr, CertificateResource.from_json(self.certr.to_json()))
+    
+        def test_bad_parse(self):
+        # pylint: disable=protected-access
+        self.config.parser.parse_file(os.path.join(
+            self.config.parser.root, 'conf-available', 'bad_conf_file.conf'))
+        self.assertRaises(
+            errors.PluginError, self.config.check_parsing_errors, 'httpd.aug')
+    
+        def test_nested(self):
+        self.assertEqual(len(self.parser.find_dir('NESTED_DIRECTIVE')), 3)
+        self.assertEqual(
+            len(self.parser.find_dir('INVALID_NESTED_DIRECTIVE')), 0)
+    
+        def test_nonexistent_like(self):
+        with mock.patch('certbot.util.get_os_info') as mock_info:
+            mock_info.return_value = ('nonexistent', 'irrelevant')
+            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
+                for like in entrypoint.OVERRIDE_CLASSES.keys():
+                    mock_like.return_value = [like]
+                    self.assertEqual(entrypoint.get_configurator(),
+                                     entrypoint.OVERRIDE_CLASSES[like])
+    
+        result = False
+    try:
+        ret = urlopen(url, timeout=2)
+        result = (ret.code == 200)
+    except HTTPError as e:
+        print(e, file=sys.stderr)
+    except URLError as e:
+        print(e, file=sys.stderr)
+    except timeout as e:
+        print(e, file=sys.stderr)
+    except Exception as e:
+        print(e, file=sys.stderr)
+    
+    *TL;DR80
+Traverses a container and accesses the container's elements.
+'''
+    
+        def publishReport(self):
+        self._db.update()
+        self._reporter.report()
+    
+    
+class UserSpecification(CompositeSpecification):
+    def is_satisfied_by(self, candidate):
+        return isinstance(candidate, User)
+    
+    
+# Create our template functions
+templates = [
+    make_template(s, g, a)
+    for g in (get_list, get_lists)
+    for a in (print_item, reverse_item)
+    for s in (iter_elements, rev_elements)
+]
+    
+    
+class A(Node):
+    pass
+    
+    *TL;DR80
+Delays the eval of an expr until its value is needed and avoids repeated evals.
+'''
+    
+    '''
+*What is this pattern about?
+This pattern is used when creating an object is costly (and they are
+created frequently) but only a few are used at a time. With a Pool we
+can manage those instances we have as of now by caching them. Now it
+is possible to skip the costly creation of an object if one is
+available in the pool.
+A pool allows to 'check out' an inactive object and then to return it.
+If none are available the pool creates one to provide without wait.
+    
+    
+if __name__ == '__main__':
+    blackboard = Blackboard()
