bd8f5fe22a4d6bd185acfd1777d281e656451a6a
SUNsung <weatherius@gmail.com>
2018-10-13 17:18:10 +0300

    Rewrite [57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb] File in: 2018-10-13 17:18:10

--- ruby/57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb
+++ ruby/57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb
@@ -1,112 +1,229 @@
 
         
-        def site
-  @site ||= Jekyll::Site.new(
-    Jekyll.configuration('source' => File.expand_path('../docs', __dir__))
-  ).tap(&:reset).tap(&:read)
+        SUITE.each do |key, text|
+  Benchmark.ips do |x|
+    x.report('regex-check   - #{key}') { check_with_regex(text) }
+    x.report('builtin-check - #{key}') { check_with_builtin(text) }
+    x.compare!
+  end
 end
+# ------------------------------------------------------------------------
     
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+    module Jekyll
+  module Commands
+    class Serve
+      # The LiveReload protocol requires the server to serve livereload.js over HTTP
+      # despite the fact that the protocol itself uses WebSockets.  This custom connection
+      # class addresses the dual protocols that the server needs to understand.
+      class HttpAwareConnection < EventMachine::WebSocket::Connection
+        attr_reader :reload_body, :reload_size
     
-          def initialize(runtime, path_or_io, options)
-        @runtime = runtime
-        @snippets_input = []
-        @io = ensure_io(path_or_io)
-        @prefixes = options[:prefixes] || {}
-        @delayed_messages = []
-        @options = options
-        @exceptions = []
-        @indent = 0
-        @timings = {}
-      end
+        DATA_URL = 'data:'.freeze
     
-            def site_template
-          File.expand_path('../../site_template', __dir__)
-        end
-    
-            def username
-          @username ||= username_and_email[:username].to_s
-        end
-    
-          def identity
-        @identity ||= current_user.identities
-                                  .with_extern_uid(provider, uid)
-                                  .first_or_initialize(extern_uid: uid)
-      end
-    
-    module Gitlab
-  module BackgroundMigration
-    class PopulateMergeRequestsLatestMergeRequestDiffId
-      BATCH_SIZE = 1_000
-    
-            def key_text
-          @entity.to_s
-        end
-    
-    puts 'Deduping #{links.size} links...'
-    
-      def set_filter
-    @filter = current_account.custom_filters.find(params[:id])
-  end
-    
-      def set_account
-    @account = Account.find_local!(params[:account_username]) if params[:account_username]
-  end
-    
-      private
-    
-    module Admin
-  class ReportedStatusesController < BaseController
-    before_action :set_report
-    
-      def account_from_topic
-    if hub_topic.present? && local_domain? && account_feed_path?
-      Account.find_local(hub_topic_params[:username])
+        def initialize(content)
+      @content = content
+      @html = document? ? parse_as_document : parse_as_fragment
     end
-  end
     
-      it 'no raises a RuntimeError on symbols' do
-    v = :sym
-    lambda { v.taint }.should_not raise_error(RuntimeError)
-    v.tainted?.should == false
-  end
-    
-          errors.each do |error|
-        summary << ' #{Formatter.error('-')} #{error}'
+        def request(urls, options = {}, &block)
+      requests = [urls].flatten.map do |url|
+        build_and_queue_request(url, options, &block)
       end
+      requests.length == 1 ? requests.first : requests
+    end
     
-        # Scrub the high bits out of the call IDs
-    src_call ^= 0x8000 if (src_call & 0x8000 != 0)
-    dst_call ^= 0x8000 if (dst_call & 0x8000 != 0)
+            css('.filetree').each do |node|
+          node.content = node.css('.file').map(&:inner_html).join('\n')
+          node.name = 'pre'
+          node.remove_attribute('class')
+        end
     
-              case protocol
-          when 'tcp'
-            self.connection = create_tcp_connection
-          when 'udp'
-            raise ::NotImplementedError, 'Kerberos Client: UDP not supported'
-          else
-            raise ::RuntimeError, 'Kerberos Client: unknown transport protocol'
+            # Remove ng-* attributes
+        css('*').each do |node|
+          node.attributes.each_key do |attribute|
+            node.remove_attribute(attribute) if attribute.start_with? 'ng-'
           end
+        end
     
-            end
-      end
+              # We split the arguments into two: One set containing any
+          # flags before a word, and then the rest. The rest are what
+          # get actually sent on to the subcommand.
+          argv.each_index do |i|
+            if !argv[i].start_with?('-')
+              # We found the beginning of the sub command. Split the
+              # args up.
+              main_args   = argv[0, i]
+              sub_command = argv[i]
+              sub_args    = argv[i + 1, argv.length - i + 1]
+    
+    module Vagrant
+  module Plugin
+    module V2
+      # Base class for a communicator in Vagrant. A communicator is
+      # responsible for communicating with a machine in some way. There
+      # are various stages of Vagrant that require things such as uploading
+      # files to the machine, executing shell commands, etc. Implementors
+      # of this class are expected to provide this functionality in some
+      # way.
+      #
+      # Note that a communicator must provide **all** of the methods
+      # in this base class. There is currently no way for one communicator
+      # to provide say a more efficient way of uploading a file, but not
+      # provide shell execution. This sort of thing will come in a future
+      # version.
+      class Communicator
+        # This returns true/false depending on if the given machine
+        # can be communicated with using this communicator. If this returns
+        # `true`, then this class will be used as the primary communication
+        # method for the machine.
+        #
+        # @return [Boolean]
+        def self.match?(machine)
+          true
+        end
+    
+            # This contains all the registered host capabilities.
+        #
+        # @return [Hash<Symbol, Registry>]
+        attr_reader :host_capabilities
+    
+        def stylesheets_path
+      File.join assets_path, 'stylesheets'
     end
-  end
-end
+    
+        def byte_to_str_pos(pos)
+      @s.string.byteslice(0, pos).length
+    end
+    
+      # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
+    
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+    # ECMA-262, section 15.1.3
+    def Encode(uri, unescape)
+      uriLength = uri.length;
+      # We are going to pass result to %StringFromCharCodeArray
+      # which does not expect any getters/setters installed
+      # on the incoming array.
+      result    = Array.new(uriLength);
+      index = 0;
+      k = -1;
+      while ((k+=1) < uriLength) do
+        cc1 = uri.charCodeAt(k);
+        next if cc1.nil?
+        if (self.send(unescape, cc1))
+          result[index] = cc1;
+          index += 1
+        else
+          if (cc1 >= 0xDC00 && cc1 <= 0xDFFF);
+            throw('URI malformed')
+          end
+          if (cc1 < 0xD800 || cc1 > 0xDBFF)
+            index = URIEncodeSingle(cc1, result, index);
+          else
+            k+=1;
+            if (k == uriLength);
+              throw('URI malformed')
+            end
+            cc2 = uri.charCodeAt(k);
+            if (cc2 < 0xDC00 || cc2 > 0xDFFF);
+              throw('URI malformed')
+            end
+            index = URIEncodePair(cc1, cc2, result, index);
+          end
+        end
+      end
+      # use .compact to get rid of nils from charCodeAt
+      # return %StringFromCharCodeArray(result);
+      # 'c' = 8 bit signed char
+      # http://www.ruby-doc.org/core-1.9.3/Array.html#method-i-pack
+      return result.compact.pack 'c*'
+    end
+  end # class << self
+end # module
 
     
-              # Decodes the realm field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_realm(input)
-            input.value[0].value
+          def footer
+        if @footer.nil?
+          if page = @page.footer
+            @footer = page.text_data
+          else
+            @footer = false
+          end
+        end
+        @footer
+      end
+    
+          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
+      def left_shift(int, shift)
+        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
+        # 1>>31, 2**32
+        (r & 2147483648) == 0 ? r : r - 4294967296
+      end
+    
+    module Precious
+  module Views
+    class Layout < Mustache
+      include Rack::Utils
+      alias_method :h, :escape_html
+    
+    # test/spec/mini 3
+# http://gist.github.com/25455
+# chris@ozmm.org
+# file:lib/test/spec/mini.rb
+def context(*args, &block)
+  return super unless (name = args.first) && block
+  require 'test/unit'
+  klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
+    def self.test(name, &block)
+      define_method('test_#{name.gsub(/\W/, '_')}', &block) if block
+    end
+    
+      test 'edits page' do
+    page_1 = @wiki.page('A')
+    post '/edit/A', :content => 'abc', :page => 'A',
+         :format             => page_1.format, :message => 'def'
+    follow_redirect!
+    assert last_response.ok?
+    
+      test 'clean path with leading slash' do
+    assert_equal '/Mordor', clean_path('/Mordor')
+  end
+    
+        page = @wiki.page('k')
+    assert_equal '한글 text', utf8(page.raw_data)
+    assert_equal 'def', page.version.message
+  end
+    
+    def date
+  Date.today.to_s
+end
+    
+            if Utils::HttpClient.remote_file_exist?(uri)
+          PluginManager.ui.debug('Found package at: #{uri}')
+          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
+        else
+          PluginManager.ui.debug('Package not found at: #{uri}')
+          return nil
+        end
+      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
+        # This probably means there is a firewall in place of the proxy is not correctly configured.
+        # So lets skip this strategy but log a meaningful errors.
+        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
+    
+          post_install_messages.compact.each do |message|
+        PluginManager.ui.info(message)
+      end
+    
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest')
           end
