08742a8c1dd0131bd51e4b4317aa32af871f1458
SUNsung <weatherius@gmail.com>
2018-10-13 06:18:13 +0300

    Rewrite [17eh5qMjjZ1os179amf.owjP4Q17rXUHbn69hbI17W2D.cpp] File in: 2018-10-13 06:18:13

--- cccccc/17eh5qMjjZ1os179amf.owjP4Q17rXUHbn69hbI17W2D.cpp
+++ cccccc/17eh5qMjjZ1os179amf.owjP4Q17rXUHbn69hbI17W2D.cpp
@@ -1,147 +1,356 @@
 
         
-        IPC_MESSAGE_ROUTED3(ShellViewMsg_Object_On_Event,
-                    int /* object id */,
-                    std::string /* event name */,
-                    base::ListValue /* arguments */)
-    
-    Base::~Base() {
-}
-    
-    
-    {}  // namespace nwapi
-    
-    // Call method of an object in browser and return the result.
-// function CallObjectMethod(id, type, method, args);
-v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
-                                           int object_id,
-                                           const std::string& type,
-                                           const std::string& method,
-                                           v8::Handle<v8::Value> args);
-    
-     protected:
-  ResponseAction Run() override;
-  ~NwObjCallObjectMethodAsyncFunction() override;
-    
-    
-    {    private:
-      DISALLOW_COPY_AND_ASSIGN(NwScreenInitEventListenersFunction);      
-  };
-    
-    cmderOptions GetOption()
+        /* Coin network-specific GUI style information */
+class NetworkStyle
 {
-	cmderOptions cmderOptions;
-	LPWSTR *szArgList;
-	int argCount;
+public:
+    /** Get style associated with provided BIP70 network id, or 0 if not known */
+    static const NetworkStyle *instantiate(const QString &networkId);
     }
     
-    TEST(CorruptionTest, TableFileRepair) {
-  options_.block_size = 2 * kValueSize;  // Limit scope of corruption
-  options_.paranoid_checks = true;
-  Reopen();
-  Build(100);
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
-  dbi->TEST_CompactMemTable();
-  dbi->TEST_CompactRange(0, nullptr, nullptr);
-  dbi->TEST_CompactRange(1, nullptr, nullptr);
-    }
+    #ifndef BITCOIN_QT_QVALUECOMBOBOX_H
+#define BITCOIN_QT_QVALUECOMBOBOX_H
     
     
-    {  delete iter;
-  delete table;
-  delete file;
-  return Status::OK();
-}
-    
-    std::string LogFileName(const std::string& dbname, uint64_t number) {
-  assert(number > 0);
-  return MakeFileName(dbname, number, 'log');
-}
-    
-    namespace leveldb {
-namespace log {
-    }
-    }
-    
-    uint64_t LogTest::initial_offset_last_record_offsets_[] =
-    {0,
-     kHeaderSize + 10000,
-     2 * (kHeaderSize + 10000),
-     2 * (kHeaderSize + 10000) +
-         (2 * log::kBlockSize - 1000) + 3 * kHeaderSize,
-     2 * (kHeaderSize + 10000) +
-         (2 * log::kBlockSize - 1000) + 3 * kHeaderSize
-         + kHeaderSize + 1,
-     3 * log::kBlockSize,
-    };
-    
-      struct KeyComparator {
-    const InternalKeyComparator comparator;
-    explicit KeyComparator(const InternalKeyComparator& c) : comparator(c) { }
-    int operator()(const char* a, const char* b) const;
-  };
-  friend class MemTableIterator;
-  friend class MemTableBackwardIterator;
-    
-    class CacheBench {
- public:
-  CacheBench() : num_threads_(FLAGS_threads) {
-    if (FLAGS_use_clock_cache) {
-      cache_ = NewClockCache(FLAGS_cache_size, FLAGS_num_shard_bits);
-      if (!cache_) {
-        fprintf(stderr, 'Clock cache not supported.\n');
-        exit(1);
-      }
-    } else {
-      cache_ = NewLRUCache(FLAGS_cache_size, FLAGS_num_shard_bits);
-    }
-  }
-    }
-    
-    class CompactedDBImpl : public DBImpl {
- public:
-  CompactedDBImpl(const DBOptions& options, const std::string& dbname);
-  virtual ~CompactedDBImpl();
-    }
-    
-      // truncated the value of the next key and output it without applying any
-  // compaction rules.  This is used for outputting a put after a single delete.
-  bool clear_and_output_next_key_ = false;
-    
-    
-    {  virtual const char* Name() const override { return 'DeleteFilter'; }
+    {private Q_SLOTS:
+    /* sign message */
+    void on_addressBookButton_SM_clicked();
+    void on_pasteButton_SM_clicked();
+    void on_signMessageButton_SM_clicked();
+    void on_copySignatureButton_SM_clicked();
+    void on_clearButton_SM_clicked();
+    /* verify message */
+    void on_addressBookButton_VM_clicked();
+    void on_verifyMessageButton_VM_clicked();
+    void on_clearButton_VM_clicked();
 };
     
-      using DBImpl::Flush;
-  virtual Status Flush(const FlushOptions& /*options*/,
-                       ColumnFamilyHandle* /*column_family*/) override {
-    return Status::NotSupported('Not supported operation in read only mode.');
-  }
+        {
+        LOCK(wallet->cs_wallet);
+        wallet->SetAddressBook(r_key_dest, r_label.toStdString(), 'receive');
+        wallet->SetAddressBook(s_key_dest, s_label.toStdString(), 'send');
+    }
     
-    Status WriteBatchBase::DeleteRange(ColumnFamilyHandle* column_family,
-                                   const SliceParts& begin_key,
-                                   const SliceParts& end_key) {
-  std::string begin_key_buf, end_key_buf;
-  Slice begin_key_slice(begin_key, &begin_key_buf);
-  Slice end_key_slice(end_key, &end_key_buf);
-  return DeleteRange(column_family, begin_key_slice, end_key_slice);
+    private:
+    void paintPath(QPainterPath &path, QQueue<float> &samples);
+    
+    
+    {
+    {        /* compute using ECDH function */
+        CHECK(secp256k1_ec_pubkey_create(ctx, &point[0], s_one) == 1);
+        CHECK(secp256k1_ecdh(ctx, output_ecdh, &point[0], s_b32) == 1);
+        /* compute 'explicitly' */
+        CHECK(secp256k1_ec_pubkey_create(ctx, &point[1], s_b32) == 1);
+        CHECK(secp256k1_ec_pubkey_serialize(ctx, point_ser, &point_ser_len, &point[1], SECP256K1_EC_COMPRESSED) == 1);
+        CHECK(point_ser_len == sizeof(point_ser));
+        secp256k1_sha256_initialize(&sha);
+        secp256k1_sha256_write(&sha, point_ser, point_ser_len);
+        secp256k1_sha256_finalize(&sha, output_ser);
+        /* compare */
+        CHECK(memcmp(output_ecdh, output_ser, sizeof(output_ser)) == 0);
+    }
 }
     
-    class DB;
-    
-    
-    {  std::unique_ptr<RateLimiter> low_pri_rate_limiter_;
-};
-    
-    
-    {  return 0;
-}
+    BOOST_AUTO_TEST_SUITE_END()
 
     
-    int main() {
-  // open DB
-  Options options;
-  options.create_if_missing = true;
-  DB* db;
-  OptimisticTransactionDB* txn_db;
+    grpc_completion_queue* CoreCodegen::grpc_completion_queue_create(
+    const grpc_completion_queue_factory* factory,
+    const grpc_completion_queue_attributes* attributes, void* reserved) {
+  return ::grpc_completion_queue_create(factory, attributes, reserved);
+}
+    
+    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
+#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
+    
+    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CLIENT_FILTER_H */
+
+    
+    // Client
+MeasureDouble RpcClientSentBytesPerRpc() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcClientSentBytesPerRpcMeasureName,
+      'Total bytes sent across all request messages per RPC', kUnitBytes);
+  return measure;
+}
+    
+    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
+::opencensus::stats::MeasureDouble RpcClientServerLatency();
+::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
+    
+        /*virtual*/ void LearnerBase::RestoreFromCheckpoint(const Dictionary& checkpoint) /*override*/
+    {
+        static const vector<std::wstring> s_requiredDictionaryKeys = { typeKey, sampleCountKey, minibatchCountKey, learningRateScheduleKey };
     }
+    
+        template <typename ElementType>
+    std::tuple<const ElementType *, const SparseIndexType*, const SparseIndexType*, size_t> NDArrayView::SparseCSCDataBuffers() const
+    {
+        return _SparseCSCDataBuffers<ElementType, ElementType>();
+    }
+    
+    
+    {        for (auto& progressWriter : m_progressWriters)
+        {
+            progressWriter->UpdateTraining(numSamples, m_aggregatedTrainingLossValue, m_aggregatedTrainingEvalCriterionValue);
+        }
+    }
+    
+                if (m_varKind == VariableKind::Input)
+            {
+                for (auto dim : m_shape.Dimensions())
+                {
+                    if (dim == 0)
+                        InvalidArgument('Variable '%S' has invalid shape '%S'.', AsString().c_str(), m_shape.AsString().c_str());
+                }
+            }
+    
+    
+    {    std::string GetCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
+};
+    
+    class CloneFunctionConfigLambda : public ConfigLambda
+{
+    // how we treat the parameters in the clone
+    enum class ParameterTreatment
+    {
+        learnable, // parameters are copied and kept trainable
+        constant,  // parameters are copied and made immutable (e.g. for use of this as a fixed feature extractor)
+        shared     // parameters are shared with where they came from (e.g. for parallel identical paths through a network)
+    };
+public:
+    // -----------------------------------------------------------------------
+    // construction
+    // -----------------------------------------------------------------------
+    }
+    
+      virtual ~DHTResponseMessage();
+    
+      std::vector<std::shared_ptr<DHTNode>> nodes_;
+    
+      std::vector<std::shared_ptr<DHTNode>> nodes_;
+    
+    
+    {
+    {    PrefPtr prefEntryPointHost = family == AF_INET ? PREF_DHT_ENTRY_POINT_HOST
+                                                   : PREF_DHT_ENTRY_POINT_HOST6;
+    if (!e->getOption()->get(prefEntryPointHost).empty()) {
+      {
+        PrefPtr prefEntryPointPort = family == AF_INET
+                                         ? PREF_DHT_ENTRY_POINT_PORT
+                                         : PREF_DHT_ENTRY_POINT_PORT6;
+        std::pair<std::string, uint16_t> addr(
+            e->getOption()->get(prefEntryPointHost),
+            e->getOption()->getAsInt(prefEntryPointPort));
+        std::vector<std::pair<std::string, uint16_t>> entryPoints;
+        entryPoints.push_back(addr);
+        auto command = make_unique<DHTEntryPointNameResolveCommand>(
+            e->newCUID(), e, family, entryPoints);
+        command->setBootstrapEnabled(true);
+        command->setTaskQueue(taskQueue.get());
+        command->setTaskFactory(taskFactory.get());
+        command->setRoutingTable(routingTable.get());
+        command->setLocalNode(localNode);
+        tempCommands.push_back(std::move(command));
+      }
+    }
+    else {
+      A2_LOG_INFO('No DHT entry point specified.');
+    }
+    {
+      auto command = make_unique<DHTInteractionCommand>(e->newCUID(), e);
+      command->setMessageDispatcher(dispatcher.get());
+      command->setMessageReceiver(receiver.get());
+      command->setTaskQueue(taskQueue.get());
+      command->setReadCheckSocket(connection->getSocket());
+      command->setConnection(std::move(connection));
+      command->setUDPTrackerClient(udpTrackerClient);
+      tempRoutineCommands.push_back(std::move(command));
+    }
+    {
+      auto command = make_unique<DHTTokenUpdateCommand>(
+          e->newCUID(), e, DHT_TOKEN_UPDATE_INTERVAL);
+      command->setTokenTracker(tokenTracker.get());
+      tempCommands.push_back(std::move(command));
+    }
+    {
+      auto command = make_unique<DHTBucketRefreshCommand>(
+          e->newCUID(), e, DHT_BUCKET_REFRESH_CHECK_INTERVAL);
+      command->setTaskQueue(taskQueue.get());
+      command->setRoutingTable(routingTable.get());
+      command->setTaskFactory(taskFactory.get());
+      tempCommands.push_back(std::move(command));
+    }
+    {
+      auto command = make_unique<DHTPeerAnnounceCommand>(
+          e->newCUID(), e, DHT_PEER_ANNOUNCE_CHECK_INTERVAL);
+      command->setPeerAnnounceStorage(peerAnnounceStorage.get());
+      tempCommands.push_back(std::move(command));
+    }
+    {
+      auto command =
+          make_unique<DHTAutoSaveCommand>(e->newCUID(), e, family, 30_min);
+      command->setLocalNode(localNode);
+      command->setRoutingTable(routingTable.get());
+      tempCommands.push_back(std::move(command));
+    }
+    // add deserialized nodes to routing table
+    auto& desnodes = deserializer.getNodes();
+    for (auto& node : desnodes) {
+      routingTable->addNode(node);
+    }
+    if (!desnodes.empty()) {
+      auto task = std::static_pointer_cast<DHTBucketRefreshTask>(
+          taskFactory->createBucketRefreshTask());
+      task->setForceRefresh(true);
+      taskQueue->addPeriodicTask1(task);
+    }
+    // assign them into DHTRegistry
+    if (family == AF_INET) {
+      DHTRegistry::getMutableData().localNode = localNode;
+      DHTRegistry::getMutableData().routingTable = std::move(routingTable);
+      DHTRegistry::getMutableData().taskQueue = std::move(taskQueue);
+      DHTRegistry::getMutableData().taskFactory = std::move(taskFactory);
+      DHTRegistry::getMutableData().peerAnnounceStorage =
+          std::move(peerAnnounceStorage);
+      DHTRegistry::getMutableData().tokenTracker = std::move(tokenTracker);
+      DHTRegistry::getMutableData().messageDispatcher = std::move(dispatcher);
+      DHTRegistry::getMutableData().messageReceiver = std::move(receiver);
+      DHTRegistry::getMutableData().messageFactory = std::move(factory);
+      e->getBtRegistry()->setUDPTrackerClient(udpTrackerClient);
+      DHTRegistry::setInitialized(true);
+    }
+    else {
+      DHTRegistry::getMutableData6().localNode = localNode;
+      DHTRegistry::getMutableData6().routingTable = std::move(routingTable);
+      DHTRegistry::getMutableData6().taskQueue = std::move(taskQueue);
+      DHTRegistry::getMutableData6().taskFactory = std::move(taskFactory);
+      DHTRegistry::getMutableData6().peerAnnounceStorage =
+          std::move(peerAnnounceStorage);
+      DHTRegistry::getMutableData6().tokenTracker = std::move(tokenTracker);
+      DHTRegistry::getMutableData6().messageDispatcher = std::move(dispatcher);
+      DHTRegistry::getMutableData6().messageReceiver = std::move(receiver);
+      DHTRegistry::getMutableData6().messageFactory = std::move(factory);
+      DHTRegistry::setInitialized6(true);
+    }
+    if (e->getBtRegistry()->getUdpPort() == 0) {
+      // We assign port last so that no exception gets in the way
+      e->getBtRegistry()->setUdpPort(port);
+    }
+  }
+  catch (RecoverableException& ex) {
+    A2_LOG_ERROR_EX(fmt('Exception caught while initializing DHT functionality.'
+                        ' DHT is disabled.'),
+                    ex);
+    tempCommands.clear();
+    tempRoutineCommands.clear();
+    if (family == AF_INET) {
+      DHTRegistry::clearData();
+      e->getBtRegistry()->setUDPTrackerClient(
+          std::shared_ptr<UDPTrackerClient>{});
+    }
+    else {
+      DHTRegistry::clearData6();
+    }
+  }
+  return std::make_pair(std::move(tempCommands),
+                        std::move(tempRoutineCommands));
+}
+    
+    
+    {  // Returns two vector of Commands.  First one contains regular
+  // commands.  Secod one contains so called routine commands, which
+  // executed once per event poll returns.
+  std::pair<std::vector<std::unique_ptr<Command>>,
+            std::vector<std::unique_ptr<Command>>>
+  setup(DownloadEngine* e, int family);
+};
+    
+      virtual std::shared_ptr<DHTTask>
+  createPeerAnnounceTask(const unsigned char* infoHash) CXX11_OVERRIDE;
+    
+      virtual void executeTask() = 0;
+    
+    void DHTTaskQueueImpl::addPeriodicTask1(const std::shared_ptr<DHTTask>& task)
+{
+  periodicTaskQueue1_.addTask(task);
+}
+    
+      virtual ~DHTTokenUpdateCommand();
+    
+    public:
+  // _remoteNode is always null
+  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                    const unsigned char* data, size_t length,
+                    const std::string& ipaddr, uint16_t port);
+    
+    // Represents a baseline image, a comparison metric and an image acceptance
+// criteria based on this metric.
+class Comparator {
+ public:
+  Comparator() {}
+  virtual ~Comparator() {}
+    }
+    
+    #define GUETZLI_LOG(stats, ...)                                    \
+  do {                                                             \
+    char debug_string[1024];                                       \
+    int res = snprintf(debug_string, sizeof(debug_string),         \
+                       __VA_ARGS__);                               \
+    assert(res > 0 && 'expected successful printing');             \
+    (void)res;                                                     \
+    debug_string[sizeof(debug_string) - 1] = '\0';                 \
+    ::guetzli::PrintDebug(                      \
+         stats, std::string(debug_string));        \
+  } while (0)
+#define GUETZLI_LOG_QUANT(stats, q)                    \
+  for (int y = 0; y < 8; ++y) {                        \
+    for (int c = 0; c < 3; ++c) {                      \
+      for (int x = 0; x < 8; ++x)                      \
+        GUETZLI_LOG(stats, ' %2d', (q)[c][8 * y + x]); \
+      GUETZLI_LOG(stats, '   ');                       \
+    }                                                  \
+    GUETZLI_LOG(stats, '\n');                          \
+  }
+    
+    // Sort the root nodes, least popular first.
+static inline bool SortHuffmanTree(const HuffmanTree& v0,
+                                   const HuffmanTree& v1) {
+  if (v0.total_count_ != v1.total_count_) {
+    return v0.total_count_ < v1.total_count_;
+  }
+  return v0.index_right_or_value_ > v1.index_right_or_value_;
+}
+    
+    namespace guetzli {
+    }
+    
+    #endif  // GUETZLI_JPEG_DATA_DECODER_H_
+
+    
+    #include <string>
+    
+    
+    {  int code = 0;
+  int si = huffsize[0];
+  p = 0;
+  while (huffsize[p]) {
+    while ((huffsize[p]) == si) {
+      huffcode[p++] = code;
+      code++;
+    }
+    code <<= 1;
+    si++;
+  }
+  for (p = 0; p < lastp; p++) {
+    int i = values[p];
+    table->depth[i] = huffsize[p];
+    table->code[i] = huffcode[p];
+  }
+}
+    
+    #if defined(BOOST_HAS_INT128)
