1de8b773a92ae4ee660140080db875b66c491791
SUNsung <weatherius@gmail.com>
2018-10-13 20:01:05 +0300

    Rewrite [04XhsIPC3XUc204slgs.MSMGwg04x9dU7mZvePI04BiC.rb] File in: 2018-10-13 20:01:04

--- ruby/04XhsIPC3XUc204slgs.MSMGwg04x9dU7mZvePI04BiC.rb
+++ ruby/04XhsIPC3XUc204slgs.MSMGwg04x9dU7mZvePI04BiC.rb
@@ -1,116 +1,35 @@
 
         
-                  def value
-            if @allow_method_names_outside_object
-              object.public_send @method_name if object && object.respond_to?(@method_name)
-            else
-              object.public_send @method_name if object
-            end
-          end
-    
-              def hidden_field_for_checkbox(options)
-            @unchecked_value ? tag('input', options.slice('name', 'disabled', 'form').merge!('type' => 'hidden', 'value' => @unchecked_value)) : ''.html_safe
-          end
-      end
+            def process_response(response)
+      data = {}
+      html, title = parse(response)
+      context = pipeline_context(response)
+      context[:html_title] = title
+      pipeline.call(html, context, data)
+      data
     end
+    
+        def relative_path_to(url)
+      url = self.class.parse(url)
+      return unless origin == url.origin
+    
+            css('.file').each do |node|
+          node.content = node.content.strip
+        end
+    
+            css('h1:not(:first-child)').each do |node|
+          node.name = 'h2'
+        end unless at_css('h2')
+    
+            # Remove root-level <div>
+        while div = at_css('h1 + div')
+          div.before(div.children)
+          div.remove
+        end
+    
+      puts '* [Sinatra](#sinatra)'
+  title = Regexp.new('(?<=\* )(.*)') # so Ruby 1.8 doesn't complain
+  File.binread(a.readme).scan(/^##.*/) do |line|
+    puts line.gsub(/#(?=#)/, '    ').gsub('#', '*').gsub(title) { '[#{$1}](##{link($1)})' }
   end
 end
-
-    
-          if _include_layout?(options)
-        layout = options.delete(:layout) { :default }
-        options[:layout] = _layout_for_option(layout)
-      end
-    end
-    
-        initializer 'action_view.caching' do |app|
-      ActiveSupport.on_load(:action_view) do
-        if app.config.action_view.cache_template_loading.nil?
-          ActionView::Resolver.caching = app.config.cache_classes
-        end
-      end
-    end
-    
-              details[key] = Array(value) if value
-        end
-      end
-    
-      # DELETE /resource/sign_out
-  def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :notice, :signed_out if signed_out
-    yield if block_given?
-    respond_to_on_destroy
-  end
-    
-    gemfile(true) do
-  source 'https://rubygems.org'
-  # Activate the gem you are reporting the issue against.
-  gem 'rails', '~> 4.2.0'
-  gem 'devise', '~> 4.0'
-  gem 'sqlite3'
-  gem 'byebug'
-end
-    
-        def xpath(*args)
-      doc.xpath(*args)
-    end
-    
-        def initialize_stubs
-      self.class.stubs.each do |path, block|
-        Typhoeus.stub(url_for(path)).and_return do
-          Typhoeus::Response.new \
-            effective_url: url_for(path),
-            code: 200,
-            headers: { 'Content-Type' => 'text/html' },
-            body: self.instance_exec(&block)
-        end
-      end
-    end
-    
-        private
-    
-        def format_path(path)
-      path.to_s.remove File.join(File.expand_path('.'), '')
-    end
-    
-        # See {CapabilityHost#capability}
-    def capability(*args)
-      super
-    rescue Errors::CapabilityNotFound => e
-      raise Errors::GuestCapabilityNotFound,
-        cap: e.extra_data[:cap],
-        guest: name
-    rescue Errors::CapabilityInvalid => e
-      raise Errors::GuestCapabilityInvalid,
-        cap: e.extra_data[:cap],
-        guest: name
-    end
-    
-            # Defines additional configuration keys to be available in the
-        # Vagrantfile. The configuration class should be returned by a
-        # block passed to this method. This is done to ensure that the class
-        # is lazy loaded, so if your class inherits from any classes that
-        # are specific to Vagrant 1.0, then the plugin can still be defined
-        # without breaking anything in future versions of Vagrant.
-        #
-        # @param [String] name Configuration key.
-        # @param [Boolean] upgrade_safe If this is true, then this configuration
-        #   key is safe to load during an upgrade, meaning that it depends
-        #   on NO Vagrant internal classes. Do _not_ set this to true unless
-        #   you really know what you're doing, since you can cause Vagrant
-        #   to crash (although Vagrant will output a user-friendly error
-        #   message if this were to happen).
-        def self.config(name=UNSET_VALUE, upgrade_safe=false, &block)
-          data[:config] ||= Registry.new
-    
-            # Initializes the communicator with the machine that we will be
-        # communicating with. This base method does nothing (it doesn't
-        # even store the machine in an instance variable for you), so you're
-        # expected to override this and do something with the machine if
-        # you care about it.
-        #
-        # @param [Machine] machine The machine this instance is expected to
-        #   communicate with.
-        def initialize(machine)
-        end
