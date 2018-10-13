7827412b0c8ad6d9edd096e355dcd8b64fdda846
SUNsung <weatherius@gmail.com>
2018-10-13 15:16:57 +0300

    Rewrite [37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb] File in: 2018-10-13 15:16:57

--- ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
+++ ruby/37fSkWDoOzcXs37FhLh3jwgmZ.37tvUblFthN237jps.rb
@@ -1,148 +1,57 @@
 
         
-                def self.find_by_dn(dn, adapter)
-          adapter.user('dn', dn)
-        end
-    
-          def perform(start_id, stop_id)
-        status_sql = Build
-          .where('ci_builds.commit_id = ci_stages.pipeline_id')
-          .where('ci_builds.stage = ci_stages.name')
-          .status_sql
-    
-            def metadata
-          @metadata ||= Coverage::Metadata.new(self)
-        end
-    
-            def initialize(badge)
-          @entity = badge.entity
-          @status = badge.status
-        end
-    
-            def value_width
-          54
-        end
-    
-          it 'requires the passwords to match when changing them' do
-        visit edit_admin_user_path(users(:bob))
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: 'no_match'
-        click_on 'Update User'
-        expect(page).to have_text('Password confirmation doesn't match')
-      end
-    end
-    
-      context 'successful dry runs' do
-    before do
-      stub_request(:get, 'http://xkcd.com/').
-        with(:headers => {'Accept-Encoding'=>'gzip,deflate', 'User-Agent'=>'Huginn - https://github.com/huginn/huginn'}).
-        to_return(:status => 200, :body => File.read(Rails.root.join('spec/data_fixtures/xkcd.html')), :headers => {})
-    end
-    
-      it 'schould clean up expired events' do
-    mock(Event).cleanup_expired!
-    stub.instance_of(IO).puts
-    @scheduler.send(:cleanup_expired_events!)
+          def initialize(params)
+    @params = if params
+                params.dup
+              else
+                [] # EventFilter.default_filter
+              end
   end
     
-      describe 'migrating an actual agent' do
-    before do
-      valid_params = {
-                        'auth_token' => 'token',
-                        'room_name' => 'test',
-                        'room_name_path' => '',
-                        'username' => 'Huginn',
-                        'username_path' => '$.username',
-                        'message' => 'Hello from Huginn!',
-                        'message_path' => '$.message',
-                        'notify' => false,
-                        'notify_path' => '',
-                        'color' => 'yellow',
-                        'color_path' => '',
-                      }
-    
-        it 'html_safes the output unless :skip_safe is passed in' do
-      expect(Utils.jsonify({:foo => 'bar'})).to be_html_safe
-      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => false)).to be_html_safe
-      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => true)).not_to be_html_safe
-    end
+        system_command '#{staged_path}/AdobePatchInstaller.app/Contents/MacOS/AdobePatchInstaller',
+                   args: [
+                           '--mode=silent',
+                         ],
+                   sudo: true
   end
     
-        ARGV.kegs.each do |keg|
-      if mode.dry_run
-        puts 'Would remove:'
-        keg.unlink(mode)
-        next
-      end
-    
-          old_name = name
-      old_path = path
-      old_remote = path.git_origin
-    
-    World(VagrantHelpers)
-
-    
-        def handle_options
-      options.rakelib = ['rakelib']
-      options.trace_output = $stderr
-    
-          def call
-        ask_question
-        value_or_default
-      end
-    
-    end
-    
-        def render(context)
-      quote = paragraphize(super)
-      author = '<strong>#{@by.strip}</strong>' if @by
-      if @source
-        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
-        parts = []
-        url.each do |part|
-          if (parts + [part]).join('/').length < 32
-            parts << part
+          def footer
+        if @footer.nil?
+          if page = @page.footer
+            @footer = page.text_data
+          else
+            @footer = false
           end
         end
-        source = parts.join('/')
-        source << '/&hellip;' unless source == @source
+        @footer
       end
-      if !@source.nil?
-        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
-      elsif !@title.nil?
-        cite = ' <cite>#{@title}</cite>'
+    
+          def title
+        'Home'
       end
-      blockquote = if @by.nil?
-        quote
-      elsif cite
-        '#{quote}<footer>#{author + cite}</footer>'
-      else
-        '#{quote}<footer>#{author}</footer>'
+    
+      test 'creating page is blocked' do
+    Precious::App.set(:wiki_options, { allow_editing: false})
+    post '/create', :content => 'abc', :page => 'D',
+         :format             => 'markdown', :message => 'def'
+    assert !last_response.ok?
+    
+      if cfg = options[:config]
+    # If the path begins with a '/' it will be considered an absolute path,
+    # otherwise it will be relative to the CWD
+    cfg = File.join(Dir.getwd, cfg) unless cfg.slice(0) == File::SEPARATOR
+    require cfg
+  end
+    
+      s.authors  = ['Tom Preston-Werner', 'Rick Olson']
+  s.email    = 'tom@github.com'
+  s.homepage = 'http://github.com/gollum/gollum'
+    
+      it 'ignores empty arrays' do
+    expect_no_offenses('[]')
+  end
+    
+            expect(cop.highlights).to eq([close])
+        expect(cop.messages)
+          .to eq([described_class::SAME_LINE_MESSAGE])
       end
-      '<blockquote>#{blockquote}</blockquote>'
-    end
-    
-    module Jekyll
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-    Liquid::Template.register_tag('include_code', Jekyll::IncludeCodeTag)
-
-    
-      class PostFilters < Octopress::Hooks::Post
-    def pre_render(post)
-      OctopressFilters::pre_filter(post)
-    end
-    
-    Liquid::Template.register_tag('video', Jekyll::VideoTag)
