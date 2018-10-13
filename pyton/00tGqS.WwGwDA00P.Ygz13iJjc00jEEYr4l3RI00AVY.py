81a5b631dc70714b52582a3f8172158f1fcdd7c5
SUNsung <weatherius@gmail.com>
2018-10-13 03:19:29 +0300

    Rewrite [00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py] File in: 2018-10-13 03:19:29

--- pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
+++ pyton/00tGqS.WwGwDA00P.Ygz13iJjc00jEEYr4l3RI00AVY.py
@@ -1,111 +1,147 @@
 
         
-        
-def CheckCall( args, **kwargs ):
-  try:
-    subprocess.check_call( args, **kwargs )
-  except subprocess.CalledProcessError as error:
-    sys.exit( error.returncode )
+            Stacks of 2 x (3 x 3) Conv2D-BN-ReLU
+    Last ReLU is after the shortcut connection.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filters is
+    doubled. Within each stage, the layers have the same number filters and the
+    same number of filters.
+    Features maps sizes:
+    stage 0: 32x32, 16
+    stage 1: 16x16, 32
+    stage 2:  8x8,  64
+    The Number of parameters is approx the same as Table 6 of [a]:
+    ResNet20 0.27M
+    ResNet32 0.46M
+    ResNet44 0.66M
+    ResNet56 0.85M
+    ResNet110 1.7M
     
-      AddToGroupMap( 'Function', identifier_group )
     
-    import contextlib
-import functools
+def load_data():
+    '''Loads CIFAR10 dataset.
+    
+    import gzip
 import os
-import requests
-import time
-import warnings
-    
-      results = request.RawResponse()[ 'completions' ]
     
     
-def EndsWithPython_BadPaths_test():
-  not_python_paths = [
-    None,
-    '',
-    '/opt/local/bin/vim',
-    r'C:\Program Files\Vim\vim74\gvim.exe',
-    '/usr/bin/python2.5',
-    '/home/user/.pyenv/shims/python3.2',
-  ]
+def test_fashion_mnist():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+        assert len(x_train) == len(y_train) == 60000
+        assert len(x_test) == len(y_test) == 10000
     
-    from ycm.tests.test_utils import MockVimModule
-MockVimModule()
     
-        requests *= 10000
-    client1_delegate = timeit(client1.delegate)
-    client2_delegate = timeit(client2.delegate)
-    with suppress_stdout():
-        client1_delegate(requests)
-        client2_delegate(requests)
-    # lets check which is faster
-    print(client1_delegate._time, client2_delegate._time)
+allobj = [losses.mean_squared_error,
+          losses.mean_absolute_error,
+          losses.mean_absolute_percentage_error,
+          losses.mean_squared_logarithmic_error,
+          losses.squared_hinge,
+          losses.hinge,
+          losses.categorical_crossentropy,
+          losses.binary_crossentropy,
+          losses.kullback_leibler_divergence,
+          losses.poisson,
+          losses.cosine_proximity,
+          losses.logcosh,
+          losses.categorical_hinge]
     
-    print('Counting to two...')
-for number in count_to_two():
-    print(number, end=' ')
+    from keras.models import Sequential, Model
+from keras.layers import Dense, Input, Average
+from keras.utils import np_utils
+from keras.utils import test_utils
+from keras import regularizers
+from keras import backend as K
     
-        def tearDown(self):
-        if not self._bProblem:
-            print('Tearing down')
-            time.sleep(0.1)
-            self._tm.publishReport()
+            if self.unit_forget_bias:
+            def bias_initializer(shape, *args, **kwargs):
+                return K.concatenate([
+                    self.bias_initializer((self.units * 5,), *args, **kwargs),
+                    initializers.Ones()((self.units,), *args, **kwargs),
+                    self.bias_initializer((self.units * 2,), *args, **kwargs),
+                ])
         else:
-            print('Test not executed. No tear down required.')
+            bias_initializer = self.bias_initializer
+        self.bias = self.add_weight(shape=(self.units * 8,),
+                                    name='bias',
+                                    initializer=bias_initializer,
+                                    regularizer=self.bias_regularizer,
+                                    constraint=self.bias_constraint)
+    
+        # Arguments
+        layers: list of layers to add to the model.
+    
+            start = datetime.now()
+        lasso = linear_model.LassoLars()
+        lasso.fit(X, Y)
+        time_lasso[i] = total_seconds(datetime.now() - start)
+    
+        max_it = len(samples_range) * len(features_range)
+    for n_samples in samples_range:
+        for n_features in features_range:
+            it += 1
+            print('==============================')
+            print('Iteration %03d of %03d' % (it, max_it))
+            print('==============================')
+            print()
+            data = nr.randint(-50, 51, (n_samples, n_features))
+    
+        ###########################################################################
+    # List sampling algorithm
+    ###########################################################################
+    # We assume that sampling algorithm has the following signature:
+    #   sample(n_population, n_sample)
+    #
+    sampling_algorithm = {}
+    
+        # Plot results
+    i = 0
+    m = len(list_n_features)
+    plt.figure('scikit-learn SGD regression benchmark results',
+               figsize=(5 * 2, 4 * m))
+    for j in range(m):
+        plt.subplot(m, 2, i + 1)
+        plt.plot(list_n_samples, np.sqrt(elnet_results[:, j, 0]),
+                 label='ElasticNet')
+        plt.plot(list_n_samples, np.sqrt(sgd_results[:, j, 0]),
+                 label='SGDRegressor')
+        plt.plot(list_n_samples, np.sqrt(asgd_results[:, j, 0]),
+                 label='A-SGDRegressor')
+        plt.plot(list_n_samples, np.sqrt(ridge_results[:, j, 0]),
+                 label='Ridge')
+        plt.legend(prop={'size': 10})
+        plt.xlabel('n_train')
+        plt.ylabel('RMSE')
+        plt.title('Test error - %d features' % list_n_features[j])
+        i += 1
+    
+    ###############################################################################
+clf = SGDRegressor(penalty='l1', alpha=.2, fit_intercept=True, max_iter=2000,
+                   tol=None)
+clf.fit(X_train, y_train)
+print('model sparsity: %f' % sparsity_ratio(clf.coef_))
+    
+    import os
+import tarfile
+from contextlib import closing
+    
+    from ..common import *
+    
+        CNTV().download_by_vid(rid, **kwargs)
+    
+    site_info = 'ehow.com'
+download = ehow_download
+download_playlist = playlist_not_supported('ehow')
+
     
     
-# Makes templates
-def make_template(skeleton, getter, action):
-    '''Instantiate a template method with getter and action'''
-    def template():
-        skeleton(getter, action)
-    return template
+def fantasy_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    if 'fantasy.tv' not in url:
+        raise Exception('Wrong place!')
     
-    
-class Dog(object):
-    
-        def __get__(self, obj, type_):
-        if obj is None:
-            return self
-        val = self.function(obj)
-        obj.__dict__[self.function.__name__] = val
-        return val
-    
-    
-class MidnightTimeProvider(object):
-    '''
-    Class implemented as hard-coded stub (in contrast to configurable stub).
-    '''
-    
-        def __getattr__(self, name):
-        def wrapper(*args, **kwargs):
-            if hasattr(self.delegate, name):
-                attr = getattr(self.delegate, name)
-                if callable(attr):
-                    return attr(*args, **kwargs)
-        return wrapper
-    
-        for obj in objects:
-        print('A {0} goes {1}'.format(obj.name, obj.make_noise()))
-    
-    # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named 'default.css' will overwrite the builtin 'default.css'.
-html_static_path = ['_static']
-    
-        hass.services.register(DOMAIN, SERVICE_BROWSE_URL,
-                           lambda service:
-                           webbrowser.open(service.data[ATTR_URL]),
-                           schema=SERVICE_BROWSE_URL_SCHEMA)
-    
-        def __init__(self, test_mode):
-        '''Initialize suite view.'''
-        self._test_mode = test_mode
-    
-        see(
-        dev_id='demo_home_boy',
-        host_name='Home Boy',
-        gps=[hass.config.latitude - 0.00002, hass.config.longitude + 0.00002],
-        gps_accuracy=20,
-        battery=53
-    )
+    site_info = 'FC2Video'
+download = fc2video_download
+download_playlist = playlist_not_supported('fc2video')
