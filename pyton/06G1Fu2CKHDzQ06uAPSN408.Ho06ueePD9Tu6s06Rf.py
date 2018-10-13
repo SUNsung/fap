245be0e1380c1f065b2541bcd015096bae7d1455
SUNsung <weatherius@gmail.com>
2018-10-13 11:18:22 +0300

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2018-10-13 11:18:21

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,50 +1,68 @@
 
         
-        
-def test_login(client, auth):
-    # test that viewing the page renders without template errors
-    assert client.get('/auth/login').status_code == 200
+            if RESULT and ('info_dict' not in test or 'age_limit' not in test['info_dict'] or
+                   test['info_dict']['age_limit'] != 18):
+        print('\nPotential missing age_limit check: {0}'.format(test['name']))
     
-        with app.app_context():
-        db = get_db()
-        post = db.execute('SELECT * FROM post WHERE id = 1').fetchone()
-        assert post is None
-
-    
-        :copyright: © 2010 by the Pallets team.
-    :license: BSD, see LICENSE for more details.
-'''
-    
-            try:
-            with open(filename) as json_file:
-                obj = json.loads(json_file.read())
-        except IOError as e:
-            if silent and e.errno in (errno.ENOENT, errno.EISDIR):
-                return False
-            e.strerror = 'Unable to load configuration file (%s)' % e.strerror
-            raise
-        return self.from_mapping(obj)
-    
-        def get_cookie_httponly(self, app):
-        '''Returns True if the session cookie should be httponly.  This
-        currently just returns the value of the ``SESSION_COOKIE_HTTPONLY``
-        config var.
-        '''
-        return app.config['SESSION_COOKIE_HTTPONLY']
-    
-            :return: a :class:`~click.testing.Result` object.
-        '''
-        if cli is None:
-            cli = self.app.cli
+    header = oldreadme[:oldreadme.index('# OPTIONS')]
+footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
     
     
-def test_on_first_run_from_current_shell(usage_tracker_io, shell_pid,
-                                         shell, logs):
-    shell.get_history.return_value = ['fuck']
-    shell_pid.return_value = 12
-    main()
-    _assert_tracker_updated(usage_tracker_io, 12)
-    logs.how_to_configure_alias.assert_called_once()
+total_bytes = 0
+    
+            f = match_filter_func('uploader != '變態妍字幕版 太妍 тест'')
+        res = get_videos(f)
+        self.assertEqual(res, ['2'])
+    
+        def test_pbs(self):
+        # https://github.com/rg3/youtube-dl/issues/2350
+        self.assertMatch('http://video.pbs.org/viralplayer/2365173446/', ['pbs'])
+        self.assertMatch('http://video.pbs.org/widget/partnerplayer/980042464/', ['pbs'])
+    
+    
+def get_db():
+    '''Connect to the application's configured database. The connection
+    is unique for each request and will be reused if this is called
+    again.
+    '''
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+    
+        def reraise(tp, value, tb=None):
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
+    
+    
+def _find_app():
+    top = _app_ctx_stack.top
+    if top is None:
+        raise RuntimeError(_app_ctx_err_msg)
+    return top.app
+    
+        By default it will try to guess the mimetype for you, but you can
+    also explicitly provide one.  For extra security you probably want
+    to send certain files as attachment (HTML for instance).  The mimetype
+    guessing requires a `filename` or an `attachment_filename` to be
+    provided.
+    
+        def _get_source_explained(self, environment, template):
+        attempts = []
+        trv = None
+    
+    
+@pytest.fixture(autouse=True)
+def shell_pid(mocker):
+    return mocker.patch('thefuck.entrypoints.not_configured._get_shell_pid',
+                        new_callable=MagicMock)
+    
+        proc.sendline(u'fuck')
+    assert proc.expect([TIMEOUT, u'echo test'])
+    assert proc.expect([TIMEOUT, u'test'])
     
     
 @pytest.fixture(params=containers)
@@ -56,14 +74,242 @@ def proc(request, spawnu, TIMEOUT):
     proc.sendline(u'fish')
     return proc
     
-    
-@pytest.mark.parametrize('script, output', [
-    ('brew link sshfs', output),
-    ('cat output', output),
-    ('brew install sshfs', '')])
-def test_not_match(script, output):
-    command = Command(script, output)
-    assert not match(command)
+        assert get_new_command(Command('brew install aa',
+                                   brew_no_available_formula))\
+        != 'brew install aha'
+
     
     
 no_such_subcommand_old = '''No such subcommand
+    
+    # Model parameter
+# ----------------------------------------------------------------------------
+#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
+# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
+#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
+# ----------------------------------------------------------------------------
+# ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
+# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
+# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
+# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
+# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
+# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
+# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
+# ---------------------------------------------------------------------------
+n = 3
+    
+        # Test single image
+    x = np.random.uniform(0, 255, (10, 10, 3))
+    inputs = Input(shape=x.shape)
+    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
+    model = Model(inputs, outputs)
+    assert model.predict(x[np.newaxis])[0].shape == x.shape
+    
+    
+def test_cce_one_hot():
+    y_a = K.variable(np.random.randint(0, 7, (5, 6)))
+    y_b = K.variable(np.random.random((5, 6, 7)))
+    objective_output = losses.sparse_categorical_crossentropy(y_a, y_b)
+    assert K.eval(objective_output).shape == (5, 6)
+    
+        # Input shape
+        3D tensor with shape `(batch_size, timesteps, input_dim)`.
+    
+            if self.stateful and batch_size:
+            # Check that for stateful networks, number of samples is a multiple
+            # of the static batch size.
+            if x[0].shape[0] % batch_size != 0:
+                raise ValueError('In a stateful network, '
+                                 'you should only pass inputs with '
+                                 'a number of samples that can be '
+                                 'divided by the batch size. Found: ' +
+                                 str(x[0].shape[0]) + ' samples')
+        return x, y, sample_weights
+    
+            return h, [h, c]
+    
+        def __init__(self,
+                 return_sequences=False,
+                 return_state=False,
+                 go_backwards=False,
+                 stateful=False,
+                 **kwargs):
+        if K.backend() != 'tensorflow':
+            raise RuntimeError('CuDNN RNNs are only available '
+                               'with the TensorFlow backend.')
+        super(RNN, self).__init__(**kwargs)
+        self.return_sequences = return_sequences
+        self.return_state = return_state
+        self.go_backwards = go_backwards
+        self.stateful = stateful
+        self.supports_masking = False
+        self.input_spec = [InputSpec(ndim=3)]
+        if hasattr(self.cell.state_size, '__len__'):
+            state_size = self.cell.state_size
+        else:
+            state_size = [self.cell.state_size]
+        self.state_spec = [InputSpec(shape=(None, dim))
+                           for dim in state_size]
+        self.constants_spec = None
+        self._states = None
+        self._num_constants = None
+    
+                    outs = to_list(outs)
+                for l, o in zip(out_labels, outs):
+                    batch_logs[l] = o
+    
+    def _run_print_help(parser, func, *a, **kw):
+    try:
+        func(*a, **kw)
+    except UsageError as e:
+        if str(e):
+            parser.error(str(e))
+        if e.print_help:
+            parser.print_help()
+        sys.exit(2)
+    
+        exitcode = 0
+    
+        def _print_response(self, response, opts):
+        if opts.headers:
+            self._print_headers(response.request.headers, b'>')
+            print('>')
+            self._print_headers(response.headers, b'<')
+        else:
+            self._print_bytes(response.body)
+    
+        return video_dict
+    
+    #----------------------------------------------------------------------
+def dilidili_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
+    global headers
+    re_str = r'http://www.dilidili.com/watch\S+'
+    if re.match(r'http://www.dilidili.wang', url):
+        re_str = r'http://www.dilidili.wang/watch\S+'
+        headers['Referer'] = 'http://www.dilidili.wang/'
+    elif re.match(r'http://www.dilidili.mobi', url):
+        re_str = r'http://www.dilidili.mobi/watch\S+'
+        headers['Referer'] = 'http://www.dilidili.mobi/'
+    
+    # [
+# (
+#   regex_match_url,
+#   remote_api_method,
+#   additional_query_parameter_for_method,
+#   parser_for_additional_parameter,
+#   field_where_photourls_are_saved
+# )
+# ]
+url_patterns = [
+    # www.flickr.com/photos/{username|NSID}/sets|albums/{album-id}
+    (
+        pattern_url_photoset,
+        'photosets.getPhotos',
+        'photoset_id',
+        get_photoset_id,
+        'photoset'
+    ),
+    # www.flickr.com/photos/{username|NSID}/{pageN}?
+    (
+        pattern_url_photostream,
+        # according to flickr api documentation, this method needs to be
+        # authenticated in order to filter photo visible to the calling user
+        # but it seems works fine anonymously as well
+        'people.getPhotos',
+        'user_id',
+        get_NSID,
+        'photos'
+    ),
+    # www.flickr.com/photos/{username|NSID}/galleries/{gallery-id}
+    (
+        pattern_url_gallery,
+        'galleries.getPhotos',
+        'gallery_id',
+        get_gallery_id,
+        'photos'
+    ),
+    # www.flickr.com/groups/{groupname|groupNSID}/
+    (
+        pattern_url_group,
+        'groups.pools.getPhotos',
+        'group_id',
+        get_NSID,
+        'photos'
+    ),
+    # www.flickr.com/photos/{username|NSID}/favorites/*
+    (
+        pattern_url_favorite,
+        'favorites.getList',
+        'user_id',
+        get_NSID,
+        'photos'
+    )
+]
+    
+    
+def get_localizer(language='English'):
+    '''The factory method'''
+    languages = dict(English=EnglishGetter, Greek=GreekGetter)
+    return languages[language]()
+    
+    
+class Delegator(object):
+    '''
+    >>> delegator = Delegator(Delegate())
+    >>> delegator.p1
+    123
+    >>> delegator.p2
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Delegate' object has no attribute 'p2'
+    >>> delegator.do_something('nothing')
+    'Doing nothing'
+    >>> delegator.do_anything()
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Delegate' object has no attribute 'do_anything'
+    '''
+    
+        @abc.abstractmethod
+    def _handle(self, request):
+        raise NotImplementedError('Must provide implementation in subclass.')
+    
+        def undo(self):
+        self.rename(self.dest, self.src)
+    
+    
+class DB:
+    def __init__(self):
+        self._tm = None
+    
+        def __init__(self, deep, *targets):
+        self.deep = deep
+        self.targets = targets
+        self.commit()
+    
+        def subscribe(self, msg, subscriber):
+        self.subscribers.setdefault(msg, []).append(subscriber)
+    
+        def scan(self):
+        '''Scan the dial to the next station'''
+        self.pos += 1
+        if self.pos == len(self.stations):
+            self.pos = 0
+        print(u'Scanning... Station is %s %s' % (self.stations[self.pos], self.name))
+    
+    '''
+http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+    
+    *TL;DR80
+Separates an algorithm from an object structure on which it operates.
+'''
+    
+    *What does this example do?
+In this example queue.Queue is used to create the pool (wrapped in a
+custom ObjectPool object to use with the with statement), and it is
+populated with strings.
+As we can see, the first string object put in 'yam' is USED by the
+with statement. But because it is released back into the pool
+afterwards it is reused by the explicit call to sample_queue.get().
+Same thing happens with 'sam', when the ObjectPool created insided the
+function is deleted (by the GC) and the object is returned.
