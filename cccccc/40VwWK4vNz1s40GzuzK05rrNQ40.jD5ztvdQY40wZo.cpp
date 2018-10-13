e57f188da5a3d0909d1e56283d6b9cbf79df587a
SUNsung <weatherius@gmail.com>
2018-10-13 14:29:22 +0300

    Rewrite [40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp] File in: 2018-10-13 14:29:21

--- cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
+++ cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
@@ -1,157 +1,370 @@
 
         
-        void DocumentHook2(bool start, content::RenderFrame* frame, Dispatcher* dispatcher) {
-  // ignore the first invocation of this hook for iframe
-  // or we'll trigger creating a context with invalid type
-  // there will follow another one with valid url
-  blink::ScriptForbiddenScope::AllowUserAgentScript script;
-  blink::WebLocalFrame* web_frame = frame->GetWebFrame();
-  GURL frame_url = ScriptContext::GetDocumentLoaderURLForFrame(web_frame);
-  if (web_frame->Parent() && (!frame_url.is_valid() || frame_url.is_empty()))
-    return;
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  v8::HandleScope scope(isolate);
-  blink::WebFrame* f = frame->GetRenderView()->GetWebView()->MainFrame();
-  if (!f->IsWebLocalFrame())
-    return;
-  blink::WebLocalFrame* local_frame = f->ToWebLocalFrame();
-  v8::Local<v8::Context> v8_context = local_frame->MainWorldScriptContext();
-  ScriptContext* script_context =
-      dispatcher->script_context_set().GetByV8Context(v8_context);
-  if (start)
-    TryInjectStartScript(web_frame, script_context ? script_context->extension() : nullptr, true);
-  if (!script_context)
-    return;
-  std::vector<v8::Handle<v8::Value> > arguments;
-  v8::Local<v8::Value> window =
-    web_frame->MainWorldScriptContext()->Global();
-  arguments.push_back(v8::Boolean::New(isolate, start));
-  arguments.push_back(window);
-  if (base::FeatureList::IsEnabled(::features::kNWNewWin)) {
-    content::RenderFrame* main_frame = frame->GetRenderView()->GetMainRenderFrame();
-    arguments.push_back(v8::Integer::New(isolate, main_frame->GetRoutingID()));
-    }
-    }
+        #endif  // CONTENT_NW_SRC_API_BASE_BASE_H_
+
     
-        // Ignore first non-switch arg if it's not a standalone package.
-    bool ignore_arg = !package->self_extract();
-    for (unsigned i = 1; i < argv.size(); ++i) {
-      if (ignore_arg && argv[i] == args[0]) {
-        ignore_arg = false;
-        continue;
-      }
+    namespace remote {
     }
     
     
-    {  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method(
-      routing_id,
-      object_id,
-      type,
-      method,
-      *static_cast<base::ListValue*>(value_args.get())));
-  return v8::Undefined(isolate);
-}
+    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
+};
     
-    EventListener::EventListener(int id,
-  const base::WeakPtr<DispatcherHost>& dispatcher_host,
-  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    #endif //CONTENT_NW_SRC_API_EVENT_EVENT_H_
+
+    
+       bool IsItemForCommandIdDynamic(int command_id) const override;
+   base::string16 GetLabelForCommandId(int command_id) const override;
+   bool GetIconForCommandId(int command_id,
+                                   gfx::Image* icon) const override;
+    
+    static KeyMap keymap = {
+  {'`'    , 'Backquote'},
+  {'\\'   , 'Backslash'},
+  {'['    , 'BracketLeft'},
+  {']'    , 'BracketRight'},
+  {','    , 'Comma'},
+  {'='    , 'Equal'},
+  {'-'    , 'Minus'},
+  {'.'    , 'Period'},
+  {'''    , 'Quote'},
+  {';'    , 'Semicolon'},
+  {'/'    , 'Slash'},
+  {'\n'   , 'Enter'},
+  {'\t'   , 'Tab'},
+  {'UP'   , 'ArrowUp'},
+  {'DOWN' , 'ArrowDown'},
+  {'LEFT' , 'ArrowLeft'},
+  {'RIGHT', 'ArrowRight'},
+  {'ESC'  , 'Escape'},
+  {'MEDIANEXTTRACK', 'MediaTrackNext'},
+  {'MEDIAPREVTRACK', 'MediaTrackPrevious'}
+};
+    
+    bool NwObjCallObjectMethodSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
     }
     
-    #include 'content/nw/src/api/menu/menu_delegate.h'
-    
-    
-    {  is_menu_modified_ = true;
-  menu_item->menu_ = this;
-  menu_items_.insert(menu_items_.begin() + pos, menu_item);
-}
-    
-    class NwAppGetArgvSyncFunction : public NWSyncExtensionFunction {
+    class NwObjCallObjectMethodSyncFunction : public NWSyncExtensionFunction {
  public:
-  NwAppGetArgvSyncFunction();
+  NwObjCallObjectMethodSyncFunction();
   bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-    #include <vector>
     
-    NwObjAllocateIdFunction::~NwObjAllocateIdFunction() {
+    {  // Nested types
+  if (descriptor->nested_type_count() > 0) {
+      // Need to specify array type explicitly here, as all elements may be null. 
+      printer->Print('new pbr::GeneratedClrTypeInfo[] { ');
+      for (int i = 0; i < descriptor->nested_type_count(); i++) {
+          WriteGeneratedCodeInfo(descriptor->nested_type(i), printer, i == descriptor->nested_type_count() - 1);
+      }
+      printer->Print('}');
+  }
+  else {
+      printer->Print('null');
+  }
+  printer->Print(last ? ')' : '),\n');
 }
     
+    RepeatedEnumFieldGenerator::~RepeatedEnumFieldGenerator() {}
     
-    { protected:
-  int n_ = 0;
-  std::mutex m_;
-  std::condition_variable cv_;
+    #include <map>
+#include <string>
+#include <google/protobuf/compiler/objectivec/objectivec_field.h>
+    
+    // The Message class works like an ostream repeater.
+//
+// Typical usage:
+//
+//   1. You stream a bunch of values to a Message object.
+//      It will remember the text in a stringstream.
+//   2. Then you stream the Message object to an ostream.
+//      This causes the text in the Message to be streamed
+//      to the ostream.
+//
+// For example;
+//
+//   testing::Message foo;
+//   foo << 1 << ' != ' << 2;
+//   std::cout << foo;
+//
+// will print '1 != 2'.
+//
+// Message is not intended to be inherited from.  In particular, its
+// destructor is not virtual.
+//
+// Note that stringstream behaves differently in gcc and in MSVC.  You
+// can stream a NULL char pointer to it in the former, but not in the
+// latter (it causes an access violation if you do).  The Message
+// class hides this difference by treating a NULL char pointer as
+// '(null)'.
+class GTEST_API_ Message {
+ private:
+  // The type of basic IO manipulators (endl, ends, and flush) for
+  // narrow streams.
+  typedef std::ostream& (*BasicNarrowIoManip)(std::ostream&);
+    }
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26, typename T27, typename T28, typename T29, typename T30,
+    typename T31, typename T32, typename T33, typename T34, typename T35,
+    typename T36, typename T37, typename T38, typename T39, typename T40,
+    typename T41, typename T42, typename T43, typename T44, typename T45,
+    typename T46, typename T47, typename T48, typename T49, typename T50>
+internal::ValueArray50<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
+    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
+    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42, T43,
+    T44, T45, T46, T47, T48, T49, T50> Values(T1 v1, T2 v2, T3 v3, T4 v4,
+    T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12, T13 v13,
+    T14 v14, T15 v15, T16 v16, T17 v17, T18 v18, T19 v19, T20 v20, T21 v21,
+    T22 v22, T23 v23, T24 v24, T25 v25, T26 v26, T27 v27, T28 v28, T29 v29,
+    T30 v30, T31 v31, T32 v32, T33 v33, T34 v34, T35 v35, T36 v36, T37 v37,
+    T38 v38, T39 v39, T40 v40, T41 v41, T42 v42, T43 v43, T44 v44, T45 v45,
+    T46 v46, T47 v47, T48 v48, T49 v49, T50 v50) {
+  return internal::ValueArray50<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
+      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39,
+      T40, T41, T42, T43, T44, T45, T46, T47, T48, T49, T50>(v1, v2, v3, v4,
+      v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19,
+      v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33,
+      v34, v35, v36, v37, v38, v39, v40, v41, v42, v43, v44, v45, v46, v47,
+      v48, v49, v50);
+}
+    
+      // Same as above, but you can choose the interception scope of this object.
+  ScopedFakeTestPartResultReporter(InterceptMode intercept_mode,
+                                   TestPartResultArray* result);
+    
+      // Appends the given TestPartResult to the array.
+  void Append(const TestPartResult& result);
+    
+    // Helper function for implementing {EXPECT|ASSERT}_PRED4.  Don't use
+// this in your code.
+template <typename Pred,
+          typename T1,
+          typename T2,
+          typename T3,
+          typename T4>
+AssertionResult AssertPred4Helper(const char* pred_text,
+                                  const char* e1,
+                                  const char* e2,
+                                  const char* e3,
+                                  const char* e4,
+                                  Pred pred,
+                                  const T1& v1,
+                                  const T2& v2,
+                                  const T3& v3,
+                                  const T4& v4) {
+  if (pred(v1, v2, v3, v4)) return AssertionSuccess();
+    }
+    
+    // Returns a newly created InternalRunDeathTestFlag object with fields
+// initialized from the GTEST_FLAG(internal_run_death_test) flag if
+// the flag is specified; otherwise returns NULL.
+InternalRunDeathTestFlag* ParseInternalRunDeathTestFlag();
+    
+    // A compile-time bool constant that is true if and only if x is a
+// null pointer literal (i.e. NULL or any 0-valued compile-time
+// integral constant).
+#ifdef GTEST_ELLIPSIS_NEEDS_POD_
+// We lose support for NULL detection where the compiler doesn't like
+// passing non-POD classes through ellipsis (...).
+# define GTEST_IS_NULL_LITERAL_(x) false
+#else
+# define GTEST_IS_NULL_LITERAL_(x) \
+    (sizeof(::testing::internal::IsNullLiteralHelper(x)) == 1)
+#endif  // GTEST_ELLIPSIS_NEEDS_POD_
+    
+       private:
+    Iterator(const Iterator& other)
+        : base_(other.base_),
+        begin1_(other.begin1_),
+        end1_(other.end1_),
+        current1_(other.current1_),
+        begin2_(other.begin2_),
+        end2_(other.end2_),
+        current2_(other.current2_),
+        begin3_(other.begin3_),
+        end3_(other.end3_),
+        current3_(other.current3_),
+        begin4_(other.begin4_),
+        end4_(other.end4_),
+        current4_(other.current4_),
+        begin5_(other.begin5_),
+        end5_(other.end5_),
+        current5_(other.current5_),
+        begin6_(other.begin6_),
+        end6_(other.end6_),
+        current6_(other.current6_) {
+      ComputeCurrentValue();
+    }
+    
+      // Converts a wide C string to a String using the UTF-8 encoding.
+  // NULL will be converted to '(null)'.  If an error occurred during
+  // the conversion, '(failed to convert from wide string)' is
+  // returned.
+  static std::string ShowWideCString(const wchar_t* wide_c_str);
+    
+    template <GTEST_10_TYPENAMES_(T)>
+struct TupleElement<true, 0, GTEST_10_TUPLE_(T) > {
+  typedef T0 type;
 };
     
-    OPERATOR_SCHEMA(Exp)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .AllowInplace({{0, 0}})
-    .IdenticalTypeAndShape()
-    .SetDoc(R'DOC(
-Calculates the exponential of the given input tensor ($exp(x)$), element-wise. This
-operation can be done in an in-place fashion too, by providing the same input
-and output blobs.
+    #ifndef GTEST_SAMPLES_PRIME_TABLES_H_
+#define GTEST_SAMPLES_PRIME_TABLES_H_
     
-    OpSchema::Cost CostInferenceForFC(
-    const OperatorDef& def,
-    const vector<TensorShape>& in) {
-  CAFFE_ENFORCE_EQ(in.size(), 3, 'FC requires three inputs');
-  struct OpSchema::Cost c;
-  ArgumentHelper helper(def);
+    // Sets the 0-terminated C string this MyString object
+// represents.
+void MyString::Set(const char* a_c_string) {
+  // Makes sure this works when c_string == c_string_
+  const char* const temp = MyString::CloneCString(a_c_string);
+  delete[] c_string_;
+  c_string_ = temp;
+}
+
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    public:
+  static void Add(InfoVec& out, const char* name, const std::string& value);
+  static void AddServerStats(InfoVec& out, const char* name,
+                             const char* statsName = nullptr);
+    
+    template <typename F>
+void find(const std::string &root, const std::string& path, bool php,
+          const F& callback) {
+  auto spath = path.empty() || !isDirSeparator(path[0]) ?
+    path : path.substr(1);
     }
     
-    namespace caffe2 {
+    static void ParseString(benchmark::State& state, const char* filename)
+{
+    std::ifstream f(filename);
+    std::string str((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
     }
     
-    namespace caffe2 {
-namespace {
-    }
-    }
+     private:
+  void PrintRunData(const Run& report);
     
-    /// Root key to retrieve Kafka topic configurations.
-const std::string kKafkaTopicParserRootKey('kafka_topics');
-    
-    Status PrometheusMetricsConfigParserPlugin::update(const std::string& source,
-                                                   const ParserConfig& config) {
-  auto prometheus_targets = config.find(kPrometheusParserRootKey);
-  if (prometheus_targets != config.end()) {
-    auto obj = data_.getObject();
-    data_.copyFrom(prometheus_targets->second.doc(), obj);
-    data_.add(kPrometheusParserRootKey, obj);
+      Mutex& GetBenchmarkMutex() const RETURN_CAPABILITY(benchmark_mutex_) {
+    return benchmark_mutex_;
   }
-    }
+    
+    PlatformColorCode GetPlatformColorCode(LogColor color) {
+#ifdef BENCHMARK_OS_WINDOWS
+  switch (color) {
+    case COLOR_RED:
+      return FOREGROUND_RED;
+    case COLOR_GREEN:
+      return FOREGROUND_GREEN;
+    case COLOR_YELLOW:
+      return FOREGROUND_RED | FOREGROUND_GREEN;
+    case COLOR_BLUE:
+      return FOREGROUND_BLUE;
+    case COLOR_MAGENTA:
+      return FOREGROUND_BLUE | FOREGROUND_RED;
+    case COLOR_CYAN:
+      return FOREGROUND_BLUE | FOREGROUND_GREEN;
+    case COLOR_WHITE:  // fall through to default
+    default:
+      return 0;
+  }
+#else
+  switch (color) {
+    case COLOR_RED:
+      return '1';
+    case COLOR_GREEN:
+      return '2';
+    case COLOR_YELLOW:
+      return '3';
+    case COLOR_BLUE:
+      return '4';
+    case COLOR_MAGENTA:
+      return '5';
+    case COLOR_CYAN:
+      return '6';
+    case COLOR_WHITE:
+      return '7';
+    default:
+      return nullptr;
+  };
+#endif
+}
     
     
     {
-    {  c.reset();
+    {
+    {  // Native Client does not provide any API to access cycle counter.
+  // Use clock_gettime(CLOCK_MONOTONIC, ...) instead of gettimeofday
+  // because is provides nanosecond resolution (which is noticable at
+  // least for PNaCl modules running on x86 Mac & Linux).
+  // Initialize to always return 0 if clock_gettime fails.
+  struct timespec ts = { 0, 0 };
+  clock_gettime(CLOCK_MONOTONIC, &ts);
+  return static_cast<int64_t>(ts.tv_sec) * 1000000000 + ts.tv_nsec;
+#elif defined(__aarch64__)
+  // System timer of ARMv8 runs at a different frequency than the CPU's.
+  // The frequency is fixed, typically in the range 1-50MHz.  It can be
+  // read at CNTFRQ special register.  We assume the OS has set up
+  // the virtual timer properly.
+  int64_t virtual_timer_value;
+  asm volatile('mrs %0, cntvct_el0' : '=r'(virtual_timer_value));
+  return virtual_timer_value;
+#elif defined(__ARM_ARCH)
+  // V6 is the earliest arch that has a standard cyclecount
+  // Native Client validator doesn't allow MRC instructions.
+#if (__ARM_ARCH >= 6)
+  uint32_t pmccntr;
+  uint32_t pmuseren;
+  uint32_t pmcntenset;
+  // Read the user mode perf monitor counter access permissions.
+  asm volatile('mrc p15, 0, %0, c9, c14, 0' : '=r'(pmuseren));
+  if (pmuseren & 1) {  // Allows reading perfmon counters for user mode code.
+    asm volatile('mrc p15, 0, %0, c9, c12, 1' : '=r'(pmcntenset));
+    if (pmcntenset & 0x80000000ul) {  // Is it counting?
+      asm volatile('mrc p15, 0, %0, c9, c13, 0' : '=r'(pmccntr));
+      // The counter is set up to count every 64th cycle
+      return static_cast<int64_t>(pmccntr) * 64;  // Should optimize to << 6
+    }
+  }
+#endif
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  return static_cast<int64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+#elif defined(__mips__)
+  // mips apparently only allows rdtsc for superusers, so we fall
+  // back to gettimeofday.  It's possible clock_gettime would be better.
+  struct timeval tv;
+  gettimeofday(&tv, nullptr);
+  return static_cast<int64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
+#else
+// The soft failover to a generic implementation is automatic only for ARM.
+// For other platforms the developer is expected to make an attempt to create
+// a fast implementation and use generic version if nothing better is available.
+#error You need to define CycleTimer for your OS and CPU
+#endif
 }
-}
-
+}  // end namespace cycleclock
+}  // end namespace benchmark
     
-      auto obj = data_.getObject();
-  data_.copyFrom(cv->second.doc(), obj);
-  data_.add('views', obj);
+    class LogType {
+  friend LogType& GetNullLogInstance();
+  friend LogType& GetErrorLogInstance();
+    }
     
-    #include <gtest/gtest.h>
-    
-      auto constraint = Constraint(EQUALS);
-  constraint.expr = 'some';
-  cl.add(constraint);
-    
-    
-    {/// Get a performance limit by name and optional level.
-size_t getWorkerLimit(WatchdogLimitType limit);
-}
-
-    
-    
-    {  EXPECT_EQ(reply.type, audit_event_record.type);
-  EXPECT_EQ('1440542781.644:403030', audit_event_record.audit_id);
-  EXPECT_EQ(audit_event_record.fields.size(), 4U);
-  EXPECT_EQ(audit_event_record.fields.count('argc'), 1U);
-  EXPECT_EQ(audit_event_record.fields['argc'], '3');
-  EXPECT_EQ(audit_event_record.fields['a0'], '\'H=1 \'');
-  EXPECT_EQ(audit_event_record.fields['a1'], '\'/bin/sh\'');
-  EXPECT_EQ(audit_event_record.fields['a2'], 'c');
-}
+      // Compile a regular expression matcher from spec.  Returns true on success.
+  //
+  // On failure (and if error is not nullptr), error is populated with a human
+  // readable error message if an error occurs.
+  bool Init(const std::string& spec, std::string* error);
