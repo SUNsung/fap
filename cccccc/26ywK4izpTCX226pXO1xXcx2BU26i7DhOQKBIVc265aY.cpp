eefa507a32c441dc2b79aecac2d20ac78148f6cf
SUNsung <weatherius@gmail.com>
2018-10-14 21:48:59 +0300

    Rewrite [26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp] File in: 2018-10-14 21:48:58

--- cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
+++ cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
@@ -1,302 +1,72 @@
 
         
-        #endif
-
-    
-      /**
-   * Returns true if the iterator is at the start of an object at the given
-   * level.
-   *
-   * For instance, suppose an iterator it is pointed to the first symbol of the
-   * first word of the third line of the second paragraph of the first block in
-   * a page, then:
-   *   it.IsAtBeginningOf(RIL_BLOCK) = false
-   *   it.IsAtBeginningOf(RIL_PARA) = false
-   *   it.IsAtBeginningOf(RIL_TEXTLINE) = true
-   *   it.IsAtBeginningOf(RIL_WORD) = true
-   *   it.IsAtBeginningOf(RIL_SYMBOL) = true
-   */
-  virtual bool IsAtBeginningOf(PageIteratorLevel level) const;
-    
-    struct BlobData {
-  BlobData() : blob(nullptr), choices(nullptr) {}
-  BlobData(int index, Tesseract* tess, const WERD_RES& word)
-    : blob(word.chopped_word->blobs[index]),
-      tesseract(tess),
-      choices(&(*word.ratings)(index, index)) {}
-    }
-    
-    NO_GRADIENT(GivenTensorFill);
-NO_GRADIENT(GivenTensorDoubleFill);
-NO_GRADIENT(GivenTensorBoolFill);
-NO_GRADIENT(GivenTensorIntFill);
-NO_GRADIENT(GivenTensorInt64Fill);
-NO_GRADIENT(GivenTensorStringFill);
-    
-    namespace dmlc {
-namespace data {
-    }
-    }
-    
-     private:
-  StreamBufferReader reader_;
-  int tmp_ch;
-  int num_prev;
-  unsigned char buf_prev[2];
-  // whether we need to do strict check
-  static const bool kStrictCheck = false;
-};
-/*! \brief the stream that write to base64, note we take from file pointers */
-class Base64OutStream: public dmlc::Stream {
- public:
-  explicit Base64OutStream(dmlc::Stream *fp) : fp(fp) {
-    buf_top = 0;
-  }
-  virtual void Write(const void *ptr, size_t size) {
-    using base64::EncodeTable;
-    size_t tlen = size;
-    const unsigned char *cptr = static_cast<const unsigned char*>(ptr);
-    while (tlen) {
-      while (buf_top < 3  && tlen != 0) {
-        buf[++buf_top] = *cptr++; --tlen;
-      }
-      if (buf_top == 3) {
-        // flush 4 bytes out
-        PutChar(EncodeTable[buf[1] >> 2]);
-        PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
-        PutChar(EncodeTable[((buf[2] << 2) | (buf[3] >> 6)) & 0x3F]);
-        PutChar(EncodeTable[buf[3] & 0x3F]);
-        buf_top = 0;
-      }
-    }
-  }
-  virtual size_t Read(void *ptr, size_t size) {
-    LOG(FATAL) << 'Base64OutStream do not support read';
-    return 0;
-  }
-  /*!
-   * \brief finish writing of all current base64 stream, do some post processing
-   * \param endch character to put to end of stream, if it is EOF, then nothing will be done
-   */
-  inline void Finish(char endch = EOF) {
-    using base64::EncodeTable;
-    if (buf_top == 1) {
-      PutChar(EncodeTable[buf[1] >> 2]);
-      PutChar(EncodeTable[(buf[1] << 4) & 0x3F]);
-      PutChar('=');
-      PutChar('=');
-    }
-    if (buf_top == 2) {
-      PutChar(EncodeTable[buf[1] >> 2]);
-      PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
-      PutChar(EncodeTable[(buf[2] << 2) & 0x3F]);
-      PutChar('=');
-    }
-    buf_top = 0;
-    if (endch != EOF) PutChar(endch);
-    this->Flush();
-  }
-    
-    /*!
- * \brief an iterator that iterates over a configure file and gets the configures
+        /*
+ * If Trace::hhbbc_time >= 1, print some stats about the program to a
+ * temporary file.  If it's greater than or equal to 2, also dump it
+ * to stdout.
  */
-class ConfigIterator: public ConfigStreamReader {
- public:
-  /*!
-   * \brief constructor
-   * \param fname name of configure file
-   */
-  explicit ConfigIterator(const char *fname) : ConfigStreamReader(fi_) {
-    fi_.open(fname);
-    if (fi_.fail()) {
-      LOG(FATAL) << 'cannot open file ' << fname;
-    }
-    ConfigReaderBase::Init();
+void print_stats(const Index&, const php::Program&);
+    
+      // The HHIRLICM runtime option is all capitals, so separation
+  // cannot be determined. Special case it.
+  boost::replace_first(out, 'hhirlicm', 'hhir_licm');
+  // The HHVM ini option becomes the standard PHP option.
+  boost::replace_first(out,
+                       'hhvm.server.upload.max_file_uploads',
+                       'max_file_uploads');
+  // Make sure IPv6 or IPv4 are handled correctly
+  boost::replace_first(out, '_i_pv', '_ipv');
+  boost::replace_first(out, '.i_pv', '.ipv');
+  // urls are special too. Let's not have 'ur_ls'
+  boost::replace_first(out, '_ur_ls', '_urls');
+  boost::replace_first(out, '.ur_ls', '.urls');
+  // No use of Eval in our ini strings
+  boost::replace_first(out, '.eval.', '.');
+  boost::replace_first(out, '.my_sql.', '.mysql.');
+  boost::replace_first(out, '.enable_hip_hop_syntax', '.force_hh');
+    
+    
+    {  auto entry = m_it.second();
+  assertx(entry.isString());
+  return HHVM_FN(dirname)(entry.toString());
+}
+    
+      char** newargv_array = (char**)alloca(sizeof(char*) * (newargv.size() + 1));
+  for (unsigned i = 0; i < newargv.size(); i++) {
+    // printf('%s\n', newargv[i].data());
+    newargv_array[i] = (char *)newargv[i].data();
   }
-  /*! \brief destructor */
-  ~ConfigIterator() {
-    fi_.close();
-  }
-    }
+  // NULL-terminate the argument array.
+  newargv_array[newargv.size()] = nullptr;
     
-    #include <xgboost/data.h>
-#include <algorithm>
-#include <vector>
-    
-    #endif  // DMLC_ENABLE_STD_THREAD
-
-    
-    /*!
- * \brief Registry entry for sparse page format.
+    /**
+ * Helper functions for use with FileUtil::isValidPath
  */
-struct SparsePageFormatReg
-    : public dmlc::FunctionRegEntryBase<SparsePageFormatReg,
-                                        std::function<SparsePageFormat* ()> > {
-};
+bool checkPathAndWarn(const String& path,
+                      const char* func_name,
+                      int param_pos);
+void checkPathAndError(const String& path,
+                       const char* func_name,
+                       int param_pos);
     
-    #include '../common/host_device_vector.h'
-    
-    
-    { private:
-  // synchronizer
-  std::unique_ptr<TreeUpdater> syncher_;
-  // training parameter
-  TrainParam param_;
-};
-    
-      // push all files to the highest level L2. This
-  // means that all keys should pass at least once
-  // via the compaction filter
-  cfilter_count = 0;
-  dbfull()->TEST_CompactRange(0, nullptr, nullptr, handles_[1]);
-  ASSERT_EQ(cfilter_count, 100000);
-  cfilter_count = 0;
-  dbfull()->TEST_CompactRange(1, nullptr, nullptr, handles_[1]);
-  ASSERT_EQ(cfilter_count, 100000);
-  ASSERT_EQ(NumTableFilesAtLevel(0, 1), 0);
-  ASSERT_EQ(NumTableFilesAtLevel(1, 1), 0);
-  ASSERT_NE(NumTableFilesAtLevel(2, 1), 0);
-    
-     public:
-  HdfsLogger(HdfsWritableFile* f, uint64_t (*gettid)())
-      : file_(f), gettid_(gettid) {
-    ROCKS_LOG_DEBUG(mylog, '[hdfs] HdfsLogger opened %s\n',
-                    file_->getName().c_str());
+    Status DistributedPlugin::call(const PluginRequest& request,
+                               PluginResponse& response) {
+  if (request.count('action') == 0) {
+    return Status(1, 'Distributed plugins require an action in PluginRequest');
   }
+    }
     
-      // Write a key in this transaction
-  txn->Put('abc', 'def');
-    
-      // Do some reads and writes to key 'y'
-  // Since the snapshot was advanced, the write done outside of the
-  // transaction does not conflict.
-  s = txn->GetForUpdate(read_options, 'y', &value);
-  txn->Put('y', 'y');
-    
-    DEFINE_string(benchmark_filter, '.',
-              'A regular expression that specifies the set of benchmarks '
-              'to execute.  If this flag is empty, no benchmarks are run.  '
-              'If this flag is the string \'all\', all benchmarks linked '
-              'into the process are run.');
-    
-    
-    {
-    {}  // end namespace internal
-}  // end namespace benchmark
-    
-    void ColorPrintf(std::ostream& out, LogColor color, const char* fmt,
-                 va_list args);
-void ColorPrintf(std::ostream& out, LogColor color, const char* fmt, ...);
-    
-      // The flag must start with '--'.
-  const std::string flag_str = std::string('--') + std::string(flag);
-  const size_t flag_len = flag_str.length();
-  if (strncmp(str, flag_str.c_str(), flag_len) != 0) return nullptr;
-    
-    // Parses a bool/Int32/string from the environment variable
-// corresponding to the given Google Test flag.
-bool BoolFromEnv(const char* flag, bool default_val);
-int32_t Int32FromEnv(const char* flag, int32_t default_val);
-double DoubleFromEnv(const char* flag, double default_val);
-const char* StringFromEnv(const char* flag, const char* default_val);
-    
-    // Source project : https://github.com/ismaelJimenez/cpp.leastsq
-// Adapted to be used with google benchmark
-    
-      // print results for each run
-  for (const auto& run : reports) {
-    PrintRunData(run);
+    class AuditTests : public testing::Test {
+ protected:
+  void SetUp() override {
+    Row().swap(row_);
   }
-    
-    namespace benchmark {
-// NOTE: only i386 and x86_64 have been well tested.
-// PPC, sparc, alpha, and ia64 are based on
-//    http://peter.kuscsik.com/wordpress/?p=14
-// with modifications by m3b.  See also
-//    https://setisvn.ssl.berkeley.edu/svn/lib/fftw-3.0.1/kernel/cycle.h
-namespace cycleclock {
-// This should return the number of cycles since power-on.  Thread-safe.
-inline BENCHMARK_ALWAYS_INLINE int64_t Now() {
-#if defined(BENCHMARK_OS_MACOSX)
-  // this goes at the top because we need ALL Macs, regardless of
-  // architecture, to return the number of 'mach time units' that
-  // have passed since startup.  See sysinfo.cc where
-  // InitializeSystemInfo() sets the supposed cpu clock frequency of
-  // macs to the number of mach time units per second, not actual
-  // CPU clock frequency (which can change in the face of CPU
-  // frequency scaling).  Also note that when the Mac sleeps, this
-  // counter pauses; it does not continue counting, nor does it
-  // reset to zero.
-  return mach_absolute_time();
-#elif defined(BENCHMARK_OS_EMSCRIPTEN)
-  // this goes above x86-specific code because old versions of Emscripten
-  // define __x86_64__, although they have nothing to do with it.
-  return static_cast<int64_t>(emscripten_get_now() * 1e+6);
-#elif defined(__i386__)
-  int64_t ret;
-  __asm__ volatile('rdtsc' : '=A'(ret));
-  return ret;
-#elif defined(__x86_64__) || defined(__amd64__)
-  uint64_t low, high;
-  __asm__ volatile('rdtsc' : '=a'(low), '=d'(high));
-  return (high << 32) | low;
-#elif defined(__powerpc__) || defined(__ppc__)
-  // This returns a time-base, which is not always precisely a cycle-count.
-  int64_t tbl, tbu0, tbu1;
-  asm('mftbu %0' : '=r'(tbu0));
-  asm('mftb  %0' : '=r'(tbl));
-  asm('mftbu %0' : '=r'(tbu1));
-  tbl &= -static_cast<int64_t>(tbu0 == tbu1);
-  // high 32 bits in tbu1; low 32 bits in tbl  (tbu0 is garbage)
-  return (tbu1 << 32) | tbl;
-#elif defined(__sparc__)
-  int64_t tick;
-  asm('.byte 0x83, 0x41, 0x00, 0x00');
-  asm('mov   %%g1, %0' : '=r'(tick));
-  return tick;
-#elif defined(__ia64__)
-  int64_t itc;
-  asm('mov %0 = ar.itc' : '=r'(itc));
-  return itc;
-#elif defined(COMPILER_MSVC) && defined(_M_IX86)
-  // Older MSVC compilers (like 7.x) don't seem to support the
-  // __rdtsc intrinsic properly, so I prefer to use _asm instead
-  // when I know it will work.  Otherwise, I'll use __rdtsc and hope
-  // the code is being compiled with a non-ancient compiler.
-  _asm rdtsc
-#elif defined(COMPILER_MSVC)
-  return __rdtsc();
-#elif defined(BENCHMARK_OS_NACL)
-  // Native Client validator on x86/x86-64 allows RDTSC instructions,
-  // and this case is handled above. Native Client validator on ARM
-  // rejects MRC instructions (used in the ARM-specific sequence below),
-  // so we handle it here. Portable Native Client compiles to
-  // architecture-agnostic bytecode, which doesn't provide any
-  // cycle counter access mnemonics.
-    }
-    }
     }
     
-    inline int& LogLevel() {
-  static int log_level = 0;
-  return log_level;
-}
-    
-    BenchmarkReporter::Context::Context() : cpu_info(CPUInfo::Get()) {}
-    
-    namespace benchmark {
-#ifdef BENCHMARK_OS_WINDOWS
-// Window's Sleep takes milliseconds argument.
-void SleepForMilliseconds(int milliseconds) { Sleep(milliseconds); }
-void SleepForSeconds(double seconds) {
-  SleepForMilliseconds(static_cast<int>(kNumMillisPerSecond * seconds));
-}
-#else   // BENCHMARK_OS_WINDOWS
-void SleepForMicroseconds(int microseconds) {
-  struct timespec sleep_time;
-  sleep_time.tv_sec = microseconds / kNumMicrosPerSecond;
-  sleep_time.tv_nsec = (microseconds % kNumMicrosPerSecond) * kNumNanosPerMicro;
-  while (nanosleep(&sleep_time, &sleep_time) != 0 && errno == EINTR)
-    ;  // Ignore signals and wait for the full interval to elapse.
-}
+    class INotifyTests : public testing::Test {
+ protected:
+  void SetUp() override {
+    // INotify will use data from the config and config parsers.
+    Registry::get().registry('config_parser')->setUp();
+    }
     }
