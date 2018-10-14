beb68ac7a58288f8c176e9fd2dbfa006733be146
SUNsung <weatherius@gmail.com>
2018-10-14 23:53:07 +0300

    Rewrite [540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py] File in: 2018-10-14 23:53:06

--- pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
+++ pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
@@ -1,184 +1,149 @@
 
         
-        versions_info = json.load(open('update/versions.json'))
-if 'signature' in versions_info:
-    del versions_info['signature']
+            def reject_friend_request(self, friend_id):
+        pass
     
-    with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
-    
-        flags = [opt.get_opt_string() for opt in opts]
-    
-    from __future__ import unicode_literals
-    
-        def test_youtube_show_matching(self):
-        self.assertMatch('http://www.youtube.com/show/airdisasters', ['youtube:show'])
-    
-    
-def deserialize(config, custom_objects=None):
-    return deserialize_keras_object(config,
-                                    module_objects=globals(),
-                                    custom_objects=custom_objects,
-                                    printable_module_name='regularizer')
-    
-        # Returns
-        List of input tensors.
-    '''
-    if not hasattr(tensor, '_keras_history'):
-        return tensor
-    
-    from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Embedding, LSTM, Bidirectional
-from keras.datasets import imdb
-    
-    
-def ensure_web_acl_present(client, module):
-    changed = False
-    result = None
-    name = module.params['name']
-    web_acl_id = get_web_acl_by_name(client, module, name)
-    if web_acl_id:
-        (changed, result) = find_and_update_web_acl(client, module, web_acl_id)
-    else:
-        metric_name = module.params['metric_name']
-        if not metric_name:
-            metric_name = re.sub(r'[^A-Za-z0-9]', '', module.params['name'])
-        default_action = module.params['default_action'].upper()
-        try:
-            params = {'Name': name, 'MetricName': metric_name, 'DefaultAction': {'Type': default_action}}
-            new_web_acl = run_func_with_change_token_backoff(client, module, params, client.create_web_acl)
-        except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-            module.fail_json_aws(e, msg='Could not create Web ACL')
-        (changed, result) = find_and_update_web_acl(client, module, new_web_acl['WebACL']['WebACLId'])
-    return changed, result
-    
-    RETURN = '''
-invalidation:
-  description: The invalidation's information.
-  returned: always
-  type: complex
-  contains:
-    create_time:
-      description: The date and time the invalidation request was first made.
-      returned: always
-      type: string
-      sample: '2018-02-01T15:50:41.159000+00:00'
-    id:
-      description: The identifier for the invalidation request.
-      returned: always
-      type: string
-      sample: I2G9MOWJZFV612
-    invalidation_batch:
-      description: The current invalidation information for the batch request.
-      returned: always
-      type: complex
-      contains:
-        caller_reference:
-          description: The value used to uniquely identify an invalidation request.
-          returned: always
-          type: string
-          sample: testing 123
-        paths:
-          description: A dict that contains information about the objects that you want to invalidate.
-          returned: always
-          type: complex
-          contains:
-            items:
-              description: A list of the paths that you want to invalidate.
-              returned: always
-              type: list
-              sample:
-              - /testpathtwo/test2.js
-              - /testpathone/test1.css
-              - /testpaththree/test3.ss
-            quantity:
-              description: The number of objects that you want to invalidate.
-              returned: always
-              type: int
-              sample: 3
-    status:
-      description: The status of the invalidation request.
-      returned: always
-      type: string
-      sample: Completed
-location:
-  description: The fully qualified URI of the distribution and invalidation batch request.
-  returned: always
-  type: string
-  sample: https://cloudfront.amazonaws.com/2017-03-25/distribution/E1ZID6KZJECZY7/invalidation/I2G9MOWJZFV622
-'''
-    
-    EXAMPLES = '''
+        HOUSING = 0
+    FOOD = 1
+    GAS = 2
+    SHOPPING = 3
+    # ...
     
     
 if __name__ == '__main__':
-    main()
+    RemoveDuplicateUrls.run()
 
     
-    - name: Gather facts about a filtered list of customer gateways, based on tags
-  ec2_customer_gateway_facts:
-    region: ap-southeast-2
-    filters:
-      'tag:Name': test-customer-gateway
-      'tag:AltName': test-customer-gateway-alt
-  register: cust_gw_facts
     
-        # disassociating address from instance
-    if device_id:
-        if isinstance:
-            return disassociate_ip_and_device(ec2, address, device_id,
-                                              check_mode)
-        else:
-            return disassociate_ip_and_device(ec2, address, device_id,
-                                              check_mode, isinstance=False)
-    # releasing address
-    else:
-        return release_address(ec2, address, check_mode)
+class Graph(object):
     
-        snapshot_ids = module.params.get('snapshot_ids')
-    owner_ids = [str(owner_id) for owner_id in module.params.get('owner_ids')]
-    restorable_by_user_ids = [str(user_id) for user_id in module.params.get('restorable_by_user_ids')]
-    filters = ansible_dict_to_boto3_filter_list(module.params.get('filters'))
+    # the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
     
-        Returns:
-        True if boto library has the named param as an argument on the request_spot_instances method, else False
+    from .cifar import load_batch
+from ..utils.data_utils import get_file
+from .. import backend as K
+import numpy as np
+import os
+    
+    
+def test_objective_shapes_2d():
+    y_a = K.variable(np.random.random((6, 7)))
+    y_b = K.variable(np.random.random((6, 7)))
+    for obj in allobj:
+        objective_output = obj(y_a, y_b)
+        assert K.eval(objective_output).shape == (6,)
+    
+    # Apply a dense layer to the every temporal slice of an input. For each of step
+# of the output sequence, decide which character should be chosen.
+model.add(layers.TimeDistributed(layers.Dense(len(chars), activation='softmax')))
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+model.summary()
+    
+                batches = make_batches(num_train_samples, batch_size)
+            for batch_index, (batch_start, batch_end) in enumerate(batches):
+                batch_ids = index_array[batch_start:batch_end]
+                try:
+                    if isinstance(fit_inputs[-1], float):
+                        # Do not slice the training phase flag.
+                        ins_batch = slice_arrays(
+                            fit_inputs[:-1], batch_ids) + [fit_inputs[-1]]
+                    else:
+                        ins_batch = slice_arrays(fit_inputs, batch_ids)
+                except TypeError:
+                    raise TypeError('TypeError while preparing batch. '
+                                    'If using HDF5 input data, '
+                                    'pass shuffle='batch'.')
+                batch_logs = {}
+                batch_logs['batch'] = batch_index
+                batch_logs['size'] = len(batch_ids)
+                callbacks.on_batch_begin(batch_index, batch_logs)
+                for i in indices_for_conversion_to_dense:
+                    ins_batch[i] = ins_batch[i].toarray()
+    
+    
+    {            'utf8_mode': 1,
+            'stdio_encoding': 'utf-8',
+            'stdio_errors': 'surrogateescape',
+            'filesystem_encoding': 'utf-8',
+            'filesystem_errors': self.UTF8_MODE_ERRORS,
+            'user_site_directory': 0,
+            '_frozen': 1,
+        }
+        self.check_config('init_global_config', config)
+    
+        def test_text_modes(self):
+        text = self.TEXT.decode('ascii')
+        text_native_eol = text.replace('\n', os.linesep)
+        for mode in ('wt', 'xt'):
+            if mode == 'xt':
+                unlink(self.filename)
+            with self.open(self.filename, mode) as f:
+                f.write(text)
+            with open(self.filename, 'rb') as f:
+                file_data = ext_decompress(f.read()).decode('ascii')
+                self.assertEqual(file_data, text_native_eol)
+            with self.open(self.filename, 'rt') as f:
+                self.assertEqual(f.read(), text)
+            with self.open(self.filename, 'at') as f:
+                f.write(text)
+            with open(self.filename, 'rb') as f:
+                file_data = ext_decompress(f.read()).decode('ascii')
+                self.assertEqual(file_data, text_native_eol * 2)
+    
+        # Load the records from the pickle data stream.
+    file.seek(0)
+    memos = DBUnpickler(file, conn).load()
+    
+    # Make a local copy of what we are going to send.
+with open('outgoing.msg', 'wb') as f:
+    f.write(bytes(msg))
+    
+    #
+# Function used to calculate result
+#
+    
+    # If not '', a 'Last updated on:' timestamp is inserted at every page
+# bottom, using the given strftime format.
+#html_last_updated_fmt = '%b %d, %Y'
+    
+        :param face_image: The image that contains one or more faces
+    :param known_face_locations: Optional - the bounding boxes of each face if you already know them.
+    :param num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
+    :return: A list of 128-dimensional face encodings (one for each face in the image)
     '''
-    return hasattr(boto, 'Version') and LooseVersion(boto.Version) >= LooseVersion('2.29.0')
+    raw_landmarks = _raw_face_landmarks(face_image, known_face_locations, model='small')
+    return [np.array(face_encoder.compute_face_descriptor(face_image, raw_landmark_set, num_jitters)) for raw_landmark_set in raw_landmarks]
     
-        def test_nonce_decoder(self):
-        from acme.jws import Header
-        nonce_field = Header._fields['nonce']
+            # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
     
-    # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.0'
+    # The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = __short_version__
+# The full version, including alpha/beta/rc tags.
+release = __version__
     
-        def tearDown(self):
-        shutil.rmtree(self.config_dir)
-        shutil.rmtree(self.work_dir)
-        shutil.rmtree(self.temp_dir)
+    import requests
+import voluptuous as vol
     
-            vhs = []
-        for match in vh_match:
-            # pylint: disable=protected-access
-            vhs.append(self.sni.configurator._create_vhost(match))
-        self.assertEqual(len(vhs), 2)
-        for vhost in vhs:
-            self.assertEqual(vhost.addrs, set([obj.Addr.fromstring('*:443')]))
-            names = vhost.get_names()
-            self.assertTrue(names in z_domains)
-    
-            Result: Apache config includes virtual servers for issued challs
-    
-            if state is None or state.state == STATE_UNKNOWN:
-            return
-    
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.host = config[CONF_HOST]
-        self.username = config[CONF_USERNAME]
-        self.password = config[CONF_PASSWORD]
-        self.last_results = []
-        data = self.get_actiontec_data()
-        self.success_init = data is not None
-        _LOGGER.info('canner initialized')
+        def _make_request(self):
+        # Weirdly enough, this doesn't seem to require authentication
+        data = [{
+            'request': {
+                'sinceRevision': 0
+            },
+            'action': 'http://linksys.com/jnap/devicelist/GetDevices'
+        }]
+        headers = {'X-JNAP-Action': 'http://linksys.com/jnap/core/Transaction'}
+        return requests.post('http://{}/JNAP/'.format(self.host),
+                             timeout=DEFAULT_TIMEOUT,
+                             headers=headers,
+                             json=data)
