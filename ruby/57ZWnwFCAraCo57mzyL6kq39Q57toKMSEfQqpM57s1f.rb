538b31cb3c740334a69e8ca31c8388914cd434ed
SUNsung <weatherius@gmail.com>
2018-10-14 10:27:58 +0300

    Rewrite [57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb] File in: 2018-10-14 10:27:58

--- ruby/57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb
+++ ruby/57ZWnwFCAraCo57mzyL6kq39Q57toKMSEfQqpM57s1f.rb
@@ -1,229 +1,55 @@
 
         
-        SUITE.each do |key, text|
-  Benchmark.ips do |x|
-    x.report('regex-check   - #{key}') { check_with_regex(text) }
-    x.report('builtin-check - #{key}') { check_with_builtin(text) }
-    x.compare!
+                  return_val = lane_obj.call(parameters) # by default no parameters
+    
+          new_path = File.join(FastlaneCore.fastlane_user_dir, file_name)
+      did_show = File.exist?(new_path)
+    
+            # Stub out calls related to the execution environment
+        client = double('ingester_client')
+        session = FastlaneCore::AnalyticsSession.new(analytics_ingester_client: client)
+        expect(client).to receive(:post_event).with({
+            client_id: p_hash,
+            category: 'fastlane Client Langauge - ruby',
+            action: :launch,
+            label: nil,
+            value: nil
+        })
+    
+            allow(File).to receive(:file?).and_return(false)
+        allow(File).to receive(:file?).with(keychain_path).and_return(true)
+        allow(File).to receive(:exist?).and_return(false)
+        expect(File).to receive(:exist?).with(cert_name).and_return(true)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_set_key_partition_list_command, print: false)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_security_import_command, print: false)
+    
+    module CrossplatformShellwords
+  # handle switching between implementations of shellescape
+  def shellescape(str)
+    if FastlaneCore::Helper.windows?
+      WindowsShellwords.shellescape(str)
+    else
+      # using `escape` instead of expected `shellescape` here
+      # which corresponds to Shellword's `String.shellescape` implementation
+      # https://github.com/ruby/ruby/blob/1cf2bb4b2085758112503e7da7414d1ef52d4f48/lib/shellwords.rb#L216
+      Shellwords.escape(str)
+    end
+  end
+  module_function :shellescape
+    
+      include_examples 'multiline literal brace layout trailing comma' do
+    let(:open) { '[' }
+    let(:close) { ']' }
   end
 end
-# ------------------------------------------------------------------------
-    
-    module Jekyll
-  module Commands
-    class Serve
-      # The LiveReload protocol requires the server to serve livereload.js over HTTP
-      # despite the fact that the protocol itself uses WebSockets.  This custom connection
-      # class addresses the dual protocols that the server needs to understand.
-      class HttpAwareConnection < EventMachine::WebSocket::Connection
-        attr_reader :reload_body, :reload_size
-    
-        DATA_URL = 'data:'.freeze
-    
-        def initialize(content)
-      @content = content
-      @html = document? ? parse_as_document : parse_as_fragment
-    end
-    
-        def request(urls, options = {}, &block)
-      requests = [urls].flatten.map do |url|
-        build_and_queue_request(url, options, &block)
-      end
-      requests.length == 1 ? requests.first : requests
-    end
-    
-            css('.filetree').each do |node|
-          node.content = node.css('.file').map(&:inner_html).join('\n')
-          node.name = 'pre'
-          node.remove_attribute('class')
-        end
-    
-            # Remove ng-* attributes
-        css('*').each do |node|
-          node.attributes.each_key do |attribute|
-            node.remove_attribute(attribute) if attribute.start_with? 'ng-'
-          end
-        end
-    
-              # We split the arguments into two: One set containing any
-          # flags before a word, and then the rest. The rest are what
-          # get actually sent on to the subcommand.
-          argv.each_index do |i|
-            if !argv[i].start_with?('-')
-              # We found the beginning of the sub command. Split the
-              # args up.
-              main_args   = argv[0, i]
-              sub_command = argv[i]
-              sub_args    = argv[i + 1, argv.length - i + 1]
-    
-    module Vagrant
-  module Plugin
-    module V2
-      # Base class for a communicator in Vagrant. A communicator is
-      # responsible for communicating with a machine in some way. There
-      # are various stages of Vagrant that require things such as uploading
-      # files to the machine, executing shell commands, etc. Implementors
-      # of this class are expected to provide this functionality in some
-      # way.
-      #
-      # Note that a communicator must provide **all** of the methods
-      # in this base class. There is currently no way for one communicator
-      # to provide say a more efficient way of uploading a file, but not
-      # provide shell execution. This sort of thing will come in a future
-      # version.
-      class Communicator
-        # This returns true/false depending on if the given machine
-        # can be communicated with using this communicator. If this returns
-        # `true`, then this class will be used as the primary communication
-        # method for the machine.
-        #
-        # @return [Boolean]
-        def self.match?(machine)
-          true
-        end
-    
-            # This contains all the registered host capabilities.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :host_capabilities
-    
-        def stylesheets_path
-      File.join assets_path, 'stylesheets'
-    end
-    
-        def byte_to_str_pos(pos)
-      @s.string.byteslice(0, pos).length
-    end
-    
-      # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-    
-      # The test environment is used exclusively to run your application's
-  # test suite. You never need to work with it otherwise. Remember that
-  # your test database is 'scratch space' for the test suite and is wiped
-  # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
-    
-    # ECMA-262, section 15.1.3
-    def Encode(uri, unescape)
-      uriLength = uri.length;
-      # We are going to pass result to %StringFromCharCodeArray
-      # which does not expect any getters/setters installed
-      # on the incoming array.
-      result    = Array.new(uriLength);
-      index = 0;
-      k = -1;
-      while ((k+=1) < uriLength) do
-        cc1 = uri.charCodeAt(k);
-        next if cc1.nil?
-        if (self.send(unescape, cc1))
-          result[index] = cc1;
-          index += 1
-        else
-          if (cc1 >= 0xDC00 && cc1 <= 0xDFFF);
-            throw('URI malformed')
-          end
-          if (cc1 < 0xD800 || cc1 > 0xDBFF)
-            index = URIEncodeSingle(cc1, result, index);
-          else
-            k+=1;
-            if (k == uriLength);
-              throw('URI malformed')
-            end
-            cc2 = uri.charCodeAt(k);
-            if (cc2 < 0xDC00 || cc2 > 0xDFFF);
-              throw('URI malformed')
-            end
-            index = URIEncodePair(cc1, cc2, result, index);
-          end
-        end
-      end
-      # use .compact to get rid of nils from charCodeAt
-      # return %StringFromCharCodeArray(result);
-      # 'c' = 8 bit signed char
-      # http://www.ruby-doc.org/core-1.9.3/Array.html#method-i-pack
-      return result.compact.pack 'c*'
-    end
-  end # class << self
-end # module
 
     
-          def footer
-        if @footer.nil?
-          if page = @page.footer
-            @footer = page.text_data
-          else
-            @footer = false
-          end
+          it 'detects closing brace on different line from last element' do
+        src = construct(false, true)
+        inspect_source(src)
+    
+    shared_examples_for 'multiline literal brace layout method argument' do
+  include MultilineLiteralBraceHelper
+    
+              raise ArgumentError unless valid_argument_types?
         end
-        @footer
-      end
-    
-          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
-      def left_shift(int, shift)
-        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
-        # 1>>31, 2**32
-        (r & 2147483648) == 0 ? r : r - 4294967296
-      end
-    
-    module Precious
-  module Views
-    class Layout < Mustache
-      include Rack::Utils
-      alias_method :h, :escape_html
-    
-    # test/spec/mini 3
-# http://gist.github.com/25455
-# chris@ozmm.org
-# file:lib/test/spec/mini.rb
-def context(*args, &block)
-  return super unless (name = args.first) && block
-  require 'test/unit'
-  klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
-    def self.test(name, &block)
-      define_method('test_#{name.gsub(/\W/, '_')}', &block) if block
-    end
-    
-      test 'edits page' do
-    page_1 = @wiki.page('A')
-    post '/edit/A', :content => 'abc', :page => 'A',
-         :format             => page_1.format, :message => 'def'
-    follow_redirect!
-    assert last_response.ok?
-    
-      test 'clean path with leading slash' do
-    assert_equal '/Mordor', clean_path('/Mordor')
-  end
-    
-        page = @wiki.page('k')
-    assert_equal '한글 text', utf8(page.raw_data)
-    assert_equal 'def', page.version.message
-  end
-    
-    def date
-  Date.today.to_s
-end
-    
-            if Utils::HttpClient.remote_file_exist?(uri)
-          PluginManager.ui.debug('Found package at: #{uri}')
-          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
-        else
-          PluginManager.ui.debug('Package not found at: #{uri}')
-          return nil
-        end
-      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
-        # This probably means there is a firewall in place of the proxy is not correctly configured.
-        # So lets skip this strategy but log a meaningful errors.
-        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
-    
-          post_install_messages.compact.each do |message|
-        PluginManager.ui.info(message)
-      end
-    
-              it 'successfully install the plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
-          end
