eaa811212174e5ced4441b37fdb1392178365f12
SUNsung <weatherius@gmail.com>
2018-10-14 20:38:47 +0300

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2018-10-14 20:38:46

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,112 +1,148 @@
 
         
-          it 'returns the previous seed value' do
-    srand(10)
-    srand(20).should == 10
-  end
+                      yield item, value, text, default_html_options.merge(additional_html_options)
+            end.join.html_safe
+          end
     
-        reset
-  end
+              content = if block_given?
+            @template_object.capture(builder, &block)
+          elsif @content.present?
+            @content.to_s
+          else
+            render_component(builder)
+          end
     
-            end
-      end
+    invalids = []
+Parallel.each(links, in_threads: 4) do |link|
+  href = link.attribute('href').to_s
+  begin
+    case check_link(URI.join(BASE_URI, href))
+    when (200...300)
+      putc('.')
+    when (300..302)
+      putc('w')
     end
+  rescue => e
+    putc('F')
+    invalids << '#{href} (reason: #{e.message})'
   end
 end
     
-                if OpenSSL::HMAC.digest('MD5', k1, decrypted) != checksum
-              raise ::RuntimeError, 'RC4-HMAC decryption failed, incorrect checksum verification'
-            end
+      private
     
-              include Rex::Proto::Kerberos::Crypto
-          include Rex::Proto::Kerberos::Model
+        def create
+      authorize :domain_block, :create?
     
-              # Decodes a Rex::Proto::Kerberos::Model::EncKdcResponse from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
+      def payload
+    @_payload ||= request.body.read
+  end
     
-              # Encodes the value field
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_value
-            OpenSSL::ASN1::OctetString.new(value)
+      it 'raises a TypeError when passed a String' do
+    lambda { sleep('2')   }.should raise_error(TypeError)
+  end
+    
+      it 'seeds the RNG correctly and repeatably' do
+    srand(10)
+    x = rand
+    srand(10)
+    rand.should == x
+  end
+    
+    describe 'Kernel#system' do
+  it 'is a private method' do
+    Kernel.should have_private_instance_method(:system)
+  end
+    
+      platform_is_not :windows do
+    it 'returns true when passed ?l if the argument is a symlink' do
+      link = tmp('file_symlink.lnk')
+      File.symlink(@file, link)
+      begin
+        Kernel.test(?l, link).should be_true
+      ensure
+        rm_r link
+      end
+    end
+  end
+    
+        trace_var :$Kernel_trace_var_global do |value|
+      captured = value
+    end
+    
+      def up_down(change)
+    change.up do
+      Mention.update_all(mentions_container_type: 'Post')
+      change_column :mentions, :mentions_container_type, :string, null: false
+      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
+    end
+    
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
+  end
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
+  end
+end
+    
+      class SendPrivate < Base
+    def perform(*_args)
+      # don't federate in cucumber
+    end
+  end
+    
+    describe ContactsController, :type => :controller do
+  describe '#index' do
+    before do
+      AppConfig.chat.enabled = true
+      @aspect = bob.aspects.create(:name => 'another aspect')
+      bob.share_with alice.person, @aspect
+      bob.share_with eve.person, @aspect
+      sign_in bob, scope: :user
+    end
+    
+          it 'registers an offense for two hash parameters with braces' do
+        expect_offense(<<-RUBY.strip_indent)
+          where({ x: 1 }, { y: 2 })
+                          ^^^^^^^^ Redundant curly braces around a hash parameter.
+        RUBY
+      end
+    end
+    
+          # An array containing the arguments of the method definition.
+      #
+      # @return [Array<Node>] the arguments of the method definition
+      def arguments
+        node_parts[1]
+      end
+    
+          # A shorthand for getting the last argument of the node.
+      # Equivalent to `arguments.last`.
+      #
+      # @return [Node, nil] the last argument of the node,
+      #                     or `nil` if there are no arguments
+      def last_argument
+        arguments[-1]
+      end
+    
+            def authorize
+          perform_payment_action(:authorize)
+        end
+    
+            def find_property
+          @property = Spree::Property.accessible_by(current_ability, :read).find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          @property = Spree::Property.accessible_by(current_ability, :read).find_by!(name: params[:id])
+        end
+    
+            def mine
+          if current_api_user.persisted?
+            @shipments = Spree::Shipment.
+                         reverse_chronological.
+                         joins(:order).
+                         where(spree_orders: { user_id: current_api_user.id }).
+                         includes(mine_includes).
+                         ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          else
+            render 'spree/api/errors/unauthorized', status: :unauthorized
           end
         end
-      end
-    end
-  end
-end
-    
-        def pos=(i)
-      @s.pos = str_to_byte_pos i
-      i
-    end
-    
-        # These mixins will get vararg definitions in SCSS (not supported by LESS):
-    VARARG_MIXINS               = %w(
-      scale transition transition-duration transition-property transition-transform box-shadow
-    )
-    
-        def log_processing(name)
-      puts yellow '  #{File.basename(name)}'
-    end
-    
-        def read_cached_files(path, files)
-      full_path = '#@cache_path/#@branch_sha/#{path}'
-      contents  = {}
-      if File.directory?(full_path)
-        files.each do |name|
-          path = '#{full_path}/#{name}'
-          contents[name] = File.read(path, mode: 'rb') if File.exists?(path)
-        end
-      end
-      contents
-    end
-    
-        remove_duplicates
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
-    
-    Then /^'([^']*)' should be post (\d+)$/ do |post_text, position|
-  stream_element_numbers_content(position).should have_content(post_text)
-end
-    
-    When /^I view '([^\']*)''s first post$/ do |email|
-  user = User.find_by_email(email)
-  post = user.posts.first
-  visit post_path(post)
-end
-    
-          delete :destroy, params: {post_id: @message.id, id: like2.id}, format: :json
-      expect(response.status).to eq(404)
-      expect(response.body).to eq(I18n.t('likes.destroy.error'))
-      expect(Like.count).to eq(like_count)
-    end
-  end
-end
-
-    
-      gem 'rb-fsevent'
-  gem 'kicker'
-  gem 'awesome_print'
-  gem 'ruby-prof', :platforms => [:ruby]
-end
-
-    
-        pod 'Alamofire', path: '../Alamofire Example/Alamofire'
-    
-      autoload :AggregateTarget,           'cocoapods/target/aggregate_target'
-  autoload :Command,                   'cocoapods/command'
-  autoload :Deintegrator,              'cocoapods_deintegrate'
-  autoload :Executable,                'cocoapods/executable'
-  autoload :ExternalSources,           'cocoapods/external_sources'
-  autoload :Installer,                 'cocoapods/installer'
-  autoload :HooksManager,              'cocoapods/hooks_manager'
-  autoload :PodTarget,                 'cocoapods/target/pod_target'
-  autoload :Project,                   'cocoapods/project'
-  autoload :Resolver,                  'cocoapods/resolver'
-  autoload :Sandbox,                   'cocoapods/sandbox'
-  autoload :Target,                    'cocoapods/target'
-  autoload :Validator,                 'cocoapods/validator'
