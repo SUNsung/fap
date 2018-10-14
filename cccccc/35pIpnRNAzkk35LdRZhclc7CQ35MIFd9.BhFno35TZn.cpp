db296f80c6acee3e8c9aff940e15826340977db4
SUNsung <weatherius@gmail.com>
2018-10-15 02:47:16 +0300

    Rewrite [35pIpnRNAzkk35LdRZhclc7CQ35MIFd9.BhFno35TZn.cpp] File in: 2018-10-15 02:47:16

--- cccccc/35pIpnRNAzkk35LdRZhclc7CQ35MIFd9.BhFno35TZn.cpp
+++ cccccc/35pIpnRNAzkk35LdRZhclc7CQ35MIFd9.BhFno35TZn.cpp
@@ -1,181 +1,195 @@
 
         
-        bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv) {
-  const base::CommandLine::StringType dashdash(2, '-');
-  bool block_args = false;
-  for (int i = 0; i < argc; ++i) {
-    if (argv[i] == dashdash)
-      break;
-    if (block_args) {
-      return false;
-    } else if (IsUrlArg(argv[i])) {
-      block_args = true;
-    }
-  }
-  return true;
-}
-    
-      void SetFlexForView(mate::Handle<View> view, int flex);
-    
-    #include 'atom/browser/api/trackable_object.h'
-#include 'base/files/file_path.h'
-#include 'components/download/public/common/download_item.h'
-#include 'native_mate/handle.h'
-#include 'url/gurl.h'
-    
-    #ifndef ATOM_BROWSER_API_ATOM_API_LABEL_BUTTON_H_
-#define ATOM_BROWSER_API_ATOM_API_LABEL_BUTTON_H_
-    
-    void ConstraintBullet::disable_collisions_between_bodies(const bool p_disabled) {
-	disabled_collisions_between_bodies = p_disabled;
-    }
-    
-    /**
-	@author AndreaCatania
-*/
-    
-    	void set_param(PhysicsServer::PinJointParam p_param, real_t p_value);
-	real_t get_param(PhysicsServer::PinJointParam p_param) const;
-    
-        template <typename ElementType>
-    void LearnerNesterov::Update(const Parameter& parameter, const NDArrayViewPtr& gradientValue, 
-                                 const NDArrayViewPtr& smoothedGradientValue, size_t trainingSampleCount) const
+            LearnerPtr AdaDeltaLearner(const vector<Parameter>& parameters,
+                               const LearningRateSchedule& learningRateSchedule,
+                               double rho, double epsilon,
+                               AdditionalLearningOptions additionalOptions /*= AdditionalLearningOptions()*/)
     {
-        GET_WRITABLE_MATRICES;
+        return MakeSharedObject<LearnerAdaDelta>(parameters, learningRateSchedule, rho, epsilon, additionalOptions);
     }
     
+        TrainerPtr CreateTrainer(const FunctionPtr& model, const FunctionPtr& lossFunction, const std::vector<LearnerPtr>& parameterLearners,
+                             const std::vector<ProgressWriterPtr>& progressWriters)
+    {
+        return MakeSharedObject<Trainer>(model, lossFunction, parameterLearners, progressWriters);
+    }
     
-    {        double m_smoothedCount;
-        MomentumSchedule m_varianceMomentumSchedule;
-    };
-    
-            template<typename OnWriteSummaryFunc>
-        void WriteSummary(const ValuePtr& accumulatedLoss, const ValuePtr& accumulatedMetric,
-                          OnWriteSummaryFunc callback)
+        bool hasMultipleReaders = config.Exists(L'readers');
+    // In case when deserializers are specified, use the new logic to compose them.
+    bool hasDeserializers = config.Exists(L'deserializers');
+    if (hasMultipleReaders)
+    {
+        vector<wstring> ioNames = config(L'readers', ConfigRecordType::Array(stringargvector()));
+        // newer code that explicitly place multiple streams for inputs
+        for (const auto& ioName : ioNames) // inputNames should map to node names
         {
-            if (accumulatedLoss && m_samples.second > 0)
-            {
-                m_loss.second = accumulatedLoss->AsScalar<double>();
-            }
-    }
-    
-            static bool IsUDF(const Dictionary& dict);
-    
-            VariableFields(const NDShape& shape, VariableKind varType, ::CNTK::DataType type, const std::weak_ptr<Function>& ownerFunction, const NDArrayViewPtr& value, bool needsGradient, const std::vector<Axis>& dynamicAxes, bool isSparse, const std::wstring& name, const std::wstring& uid)
-            : m_shape(shape), m_varKind(varType), m_dataType(type), m_ownerFunction(ownerFunction), m_value(value), m_needsGradient(needsGradient), m_dynamicAxes(dynamicAxes), m_isSparse(isSparse), m_name(name), m_uid(uid), m_valueTimeStamp(0)
-        {
-            if (value && (type != value->GetDataType()))
-                InvalidArgument('The DataType of the Parameter/Constant Variable '%S' does not match the DataType of the associated Value', AsString().c_str());
-    }
-    
-    // ---------------------------------------------------------------------------
-// RandomOrdering -- class to help manage randomization of input data
-// ---------------------------------------------------------------------------
-    
-    
-    {}  // namespace guetzli
-    
-    // This function will create a Huffman tree.
-//
-// The catch here is that the tree cannot be arbitrarily deep.
-// Brotli specifies a maximum depth of 15 bits for 'code trees'
-// and 7 bits for 'code length code trees.'
-//
-// count_limit is the value that is to be faked as the minimum value
-// and this minimum value is raised until the tree matches the
-// maximum length requirement.
-//
-// This algorithm is not of excellent performance for very long data blocks,
-// especially when population counts are longer than 2**tree_limit, but
-// we are not planning to use this with extremely long blocks.
-//
-// See http://en.wikipedia.org/wiki/Huffman_coding
-void CreateHuffmanTree(const uint32_t *data,
-                       const size_t length,
-                       const int tree_limit,
-                       HuffmanTree* tree,
-                       uint8_t *depth) {
-  // For block sizes below 64 kB, we never need to do a second iteration
-  // of this loop. Probably all of our block sizes will be smaller than
-  // that, so this loop is mostly of academic interest. If we actually
-  // would need this, we would be better off with the Katajainen algorithm.
-  for (uint32_t count_limit = 1; ; count_limit *= 2) {
-    size_t n = 0;
-    for (size_t i = length; i != 0;) {
-      --i;
-      if (data[i]) {
-        const uint32_t count = std::max<uint32_t>(data[i], count_limit);
-        tree[n++] = HuffmanTree(count, -1, static_cast<int16_t>(i));
-      }
-    }
+            const ConfigRecordType& thisIO = config(ioName);
+            wstring readerType = thisIO(L'readerType', L'Cntk.Deserializers.TextFormat');
     }
     }
     
-    namespace guetzli {
+        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange& fr) override
+    {
+        Matrix<ElemType> sliceInput1Value = Input(1)->ValueFor(fr);
+        Matrix<ElemType> sliceOutputValue = ValueFor(fr);
     }
     
-    #endif  // GUETZLI_JPEG_BIT_WRITER_H_
+    
+    {REGISTER_INTERNAL(KafkaTopicsConfigParserPlugin,
+                  'config_parser',
+                  'kafka_topics');
+} // namespace osquery
 
     
-      // Compute YUV444 DCT coefficients.
-  int block_ix = 0;
-  for (int block_y = 0; block_y < jpg->MCU_rows; ++block_y) {
-    for (int block_x = 0; block_x < jpg->MCU_cols; ++block_x) {
-      coeff_t block[3 * kDCTBlockSize];
-      // RGB->YUV transform.
-      for (int iy = 0; iy < 8; ++iy) {
-        for (int ix = 0; ix < 8; ++ix) {
-          int y = std::min(h - 1, 8 * block_y + iy);
-          int x = std::min(w - 1, 8 * block_x + ix);
-          int p = y * w + x;
-          RGBToYUV16(&rgb[3 * p], &block[8 * iy + ix]);
-        }
-      }
-      // DCT
-      for (int i = 0; i < 3; ++i) {
-        ComputeBlockDCT(&block[i * kDCTBlockSize]);
-      }
-      // Quantization
-      for (int i = 0; i < 3 * 64; ++i) {
-        Quantize(&block[i], iquant[i]);
-      }
-      // Copy the resulting coefficients to *jpg.
-      for (int i = 0; i < 3; ++i) {
-        memcpy(&jpg->components[i].coeffs[block_ix * kDCTBlockSize],
-               &block[i * kDCTBlockSize], kDCTBlockSize * sizeof(block[0]));
-      }
-      ++block_ix;
+    namespace rj = rapidjson;
+    
+    namespace osquery {
     }
+    
+      std::string log_line;
+  serializeQueryLogItemJSON(item, log_line);
+  std::string expected =
+      '{\'snapshot\':[],\'action\':\'snapshot\',\'name\':\'\','
+      '\'hostIdentifier\':\'\',\'calendarTime\':\'\',\'unixTime\':0,'
+      '\'epoch\':0,\'counter\':0,'
+      '\'decorations\':{\'internal_60_test\':\'test\',\'one\':\'1\'}}';
+  EXPECT_EQ(log_line, expected);
+    
+      // This looks funky, because the parser is named 'options' and it claims
+  // ownership of a single top-level-key called 'options'.
+  const auto& doc = c.getParser('options')->getData().doc()['options'];
+    
+    
+    {
+    {  c.reset();
+}
+}
+
+    
+      while ((ret = ::WaitForSingleObject(process.nativeHandle(), INFINITE)) !=
+             WAIT_FAILED &&
+         ret != WAIT_OBJECT_0)
+    ;
+  if (ret == WAIT_FAILED) {
+    return false;
   }
+    
+    #include <osquery/tables.h>
+    
+     private:
+  /// Fork and execute a worker process.
+  virtual void createWorker();
+    
+    void Initializer::platformSetup() {
+  // Initialize the COM libraries utilized by Windows WMI calls.
+  auto ret = ::CoInitializeEx(0, COINIT_MULTITHREADED);
+  if (ret != S_OK) {
+    ::CoUninitialize();
+  }
+}
+    
+    int main(int argc, char* argv[]) {
+  osquery::Initializer runner(argc, argv, ToolType::EXTENSION);
+    }
+    
+        std::unique_ptr<DHTMessageDispatcher> messageDispatcher;
+    
+    std::shared_ptr<DHTBucket>
+DHTRoutingTable::getBucketFor(const unsigned char* nodeID) const
+{
+  return dht::findBucketFor(root_.get(), nodeID);
+}
     
     #include <string>
 #include <vector>
 #include <memory>
     
-      int version;
+      char headerCompat[8];
+  memset(headerCompat, 0, sizeof(headerCompat));
+  // magic
+  headerCompat[0] = 0xa1u;
+  headerCompat[1] = 0xa2u;
+  // format ID
+  headerCompat[2] = 0x02u;
+  // version
+  headerCompat[6] = 0;
+  headerCompat[7] = 0x02u;
     
-        peerAnnounceStorage->setTaskQueue(taskQueue.get());
-    peerAnnounceStorage->setTaskFactory(taskFactory.get());
+    #include <algorithm>
     
+    #endif // D_DHT_TOKEN_UPDATE_COMMAND_H
+
     
-    {} // namespace aria2
+    ///////////////////////////////////////////////////////////////////////////////
+// visible FDCT callable functions
     
-      ~DHTTokenTracker();
+    // Fills in 'result' with the inverse DCT of 'block'.
+// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
+// a row-by-row memory layout.
+void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
     
-    void DHTTokenUpdateCommand::preProcess()
-{
-  if (getDownloadEngine()->getRequestGroupMan()->downloadFinished() ||
-      getDownloadEngine()->isHaltRequested()) {
-    enableExit();
-  }
+    #endif  // GUETZLI_JPEG_DATA_DECODER_H_
+
+    
+    void Quantize(coeff_t* v, int iquant) {
+  *v = (*v * iquant + kBias) >> kDCTBits;
 }
     
-    DNSCache::CacheEntry& DNSCache::CacheEntry::operator=(const CacheEntry& c)
-{
-  if (this != &c) {
-    hostname_ = c.hostname_;
-    port_ = c.port_;
-    addrEntries_ = c.addrEntries_;
+    // Reads the DRI marker and saved the restart interval into *jpg.
+bool ProcessDRI(const uint8_t* data, const size_t len, size_t* pos,
+                JPEGData* jpg) {
+  if (jpg->restart_interval > 0) {
+    fprintf(stderr, 'Duplicate DRI marker.\n');
+    jpg->error = JPEG_DUPLICATE_DRI;
+    return false;
   }
-  return *this;
+  const size_t start_pos = *pos;
+  VERIFY_LEN(4);
+  size_t marker_len = ReadUint16(data, pos);
+  int restart_interval = ReadUint16(data, pos);
+  jpg->restart_interval = restart_interval;
+  VERIFY_MARKER_END();
+  return true;
 }
+    
+    // Writes a string using the out callback.
+inline bool JPEGWrite(JPEGOutput out, const std::string& s) {
+  const uint8_t* data = reinterpret_cast<const uint8_t*>(&s[0]);
+  return JPEGWrite(out, data, s.size());
+}
+    
+    using json = nlohmann::json;
+    
+      // Specify if each repetition of the benchmark should be reported separately
+  // or if only the final statistics should be reported. If the benchmark
+  // is not repeated then the single result is always reported.
+  Benchmark* ReportAggregatesOnly(bool value = true);
+    
+    #endif  // BENCHMARK_API_INTERNAL_H
+
+    
+      const HANDLE stdout_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    
+    
+    {// Returns true unless value starts with one of: '0', 'f', 'F', 'n' or 'N', or
+// some non-alphanumeric character. As a special case, also returns true if
+// value is the empty string.
+bool IsTruthyFlagValue(const std::string& value);
+}  // end namespace benchmark
+    
+    bool SameNames(UserCounters const& l, UserCounters const& r) {
+  if (&l == &r) return true;
+  if (l.size() != r.size()) {
+    return false;
+  }
+  for (auto const& c : l) {
+    if (r.find(c.first) == r.end()) {
+      return false;
+    }
+  }
+  return true;
+}
+    
+    #include <iostream>
+#include <tuple>
+#include <vector>
