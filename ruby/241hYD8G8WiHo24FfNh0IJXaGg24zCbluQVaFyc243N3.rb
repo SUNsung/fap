cfadf237af9689aa69768a497d2e7407bf0a68e2
SUNsung <weatherius@gmail.com>
2018-10-14 10:53:43 +0300

    Rewrite [241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb] File in: 2018-10-14 10:53:42

--- ruby/241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb
+++ ruby/241hYD8G8WiHo24FfNh0IJXaGg24zCbluQVaFyc243N3.rb
@@ -1,65 +1,264 @@
 
         
-            def create
-      authorize AccountModerationNote, :create?
+                def initialize(object_name, method_name, template_object, checked_value, unchecked_value, options)
+          @checked_value   = checked_value
+          @unchecked_value = unchecked_value
+          super(object_name, method_name, template_object, options)
+        end
     
-    module Admin
-  class ChangeEmailsController < BaseController
-    before_action :set_account
-    before_action :require_local_account!
+    require 'action_view/helpers/tags/collection_helpers'
     
-          @domain_block = DomainBlock.new(resource_params)
+    require 'rubygems'
+require 'rake'
+require 'rdoc'
+require 'date'
+require 'yaml'
     
-      def lease_seconds_or_default
-    (params['hub.lease_seconds'] || 1.day).to_i.seconds
-  end
+    Nullam luctus fermentum est id blandit. Phasellus consectetur ullamcorper
+ligula, at finibus eros laoreet id. Etiam sit amet est in libero efficitur.
+tristique. Ut nec magna augue. {{ author }} Quisque ut fringilla lacus
+Aliquam vel ornare mauris. Suspendisse ornare diam tempor nulla facilisis
+aliquet. Sed ultrices placerat ultricies.
+LIQUID
     
-      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
-  gem.executables   = %w(cap capify)
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ['lib']
-    
-        def ask(key, default=nil, options={})
-      question = Question.new(key, default, options)
-      set(key, question)
-    end
-    
-            @response = (gets || '').chomp
-      end
-    
-          def netssh_options
-        @netssh_options ||= super.merge(fetch(:ssh_options) || {})
-      end
-    
-    if command? :kicker
-  desc 'Launch Kicker (like autotest)'
-  task :kicker do
-    puts 'Kicking... (ctrl+c to cancel)'
-    exec 'kicker -e rake test lib examples'
-  end
+    def graceful_require
+  Jekyll::External.require_with_graceful_fail('json')
+  JSON.pretty_generate(DATA)
 end
     
-          Resque.redis
-    end
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-          context 'when the whitelist is a single value' do
-        let(:test_file) { File.open(file_path('test.jpeg')) }
+    Given(%r!^I have the following (draft|page|post)s?(?: (in|under) '([^']+)')?:$!) do |status, direction, folder, table|
+  table.hashes.each do |input_hash|
+    title = slug(input_hash['title'])
+    ext = input_hash['type'] || 'markdown'
+    filename = '#{title}.#{ext}' if %w(draft page).include?(status)
+    before, after = location(folder, direction)
+    dest_folder = '_drafts' if status == 'draft'
+    dest_folder = '_posts'  if status == 'post'
+    dest_folder = '' if status == 'page'
     
-          it 'follows redirects but still respect the extension_whitelist' do
-        expect { uploader.download!('http://www.redirect.com/') }.to raise_error(CarrierWave::IntegrityError)
+      s.summary       = 'A simple, blog aware, static site generator.'
+  s.description   = 'Jekyll is a simple, blog aware, static site generator.'
+    
+            def setup
+          @config['syntax_highlighter'] ||= highlighter
+          @config['syntax_highlighter_opts'] ||= {}
+          @config['coderay'] ||= {} # XXX: Legacy.
+          modernize_coderay_config
+          make_accessible
+        end
+    
+              return [main_args, sub_command, sub_args]
+        end
       end
-    
-    When /^I delete the datamapper record$/ do
-  @instance.destroy
+    end
+  end
 end
 
     
-    Then /^the file at '(.*?)' should be the reverse of the file at '(.*?)'$/ do |one, two|
-  File.read(file_path(one)).should == File.read(file_path(two)).reverse
+              # Set all of our instance variables on the new class
+          [self, other].each do |obj|
+            obj.instance_variables.each do |key|
+              # Ignore keys that start with a double underscore. This allows
+              # configuration classes to still hold around internal state
+              # that isn't propagated.
+              if !key.to_s.start_with?('@__')
+                result.instance_variable_set(key, obj.instance_variable_get(key))
+              end
+            end
+          end
+    
+            # This contains all the guests and their parents.
+        #
+        # @return [Registry<Symbol, Array<Class, Symbol>>]
+        attr_reader :guests
+    
+      private
+    
+          @user.resend_confirmation_instructions
+    
+        def new
+      authorize :custom_emoji, :create?
+      @custom_emoji = CustomEmoji.new
+    end
+    
+          redirect_to admin_report_path(@report)
+    end
+    
+      def process_push_request
+    case hub_mode
+    when 'subscribe'
+      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
+    when 'unsubscribe'
+      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
+    else
+      ['Unknown mode: #{hub_mode}', 422]
+    end
+  end
+    
+          attr_reader :page, :diff, :versions, :message, :allow_editing
+    
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
+  end
+    
+    desc 'Validate lib files and version file'
+task :validate do
+  libfiles = Dir['lib/*'] - ['lib/#{name}.rb', 'lib/#{name}']
+  unless libfiles.empty?
+    puts 'Directory `lib` should only contain a `#{name}.rb` file and `#{name}` dir.'
+    exit!
+  end
+  unless Dir['VERSION*'].empty?
+    puts 'A `VERSION` file at root level violates Gem best practices.'
+    exit!
+  end
 end
 
     
-        def option(name)
-      self.uploader_options ||= {}
-      self.uploader_options[name] ||= record.class.uploader_option(column, name)
+        # make a backup of the option and sanitize it
+    base_path_original = base_path.dup
+    base_path = CGI.escape(base_path)
+    
+    # then let the user know if we changed the URL
+    unless base_path_original == base_path
+      puts <<MSG
+Warning: your base-path has been sanitized:
+  - original: '#{base_path_original}'
+  - sanitized: '#{base_path}'
+MSG
     end
+      
+    # and finally, let others enjoy our hard work:
+    wiki_options[:base_path] = base_path
+  end
+  opts.on('--page-file-dir [PATH]', 'Specify the subdirectory for all pages. Default: repository root.', 
+    'Example: setting this to 'pages' will make Gollum serve only pages at '<git-repo>/pages/*'.') do |path|
+    wiki_options[:page_file_dir] = path
+  end
+  opts.on('--css', 'Inject custom CSS into each page. The '<git-repo>/custom.css' file is used (must be committed).') do
+    wiki_options[:css] = true
+  end
+  opts.on('--js', 'Inject custom JavaScript into each page. The '<git-repo>/custom.js' file is used (must be committed).') do
+    wiki_options[:js] = true
+  end
+  opts.on('--emoji', 'Parse and interpret emoji tags (e.g. :heart:).') do
+    wiki_options[:emoji] = true
+  end
+  opts.on('--no-edit', 'Disable the feature of editing pages.')  do
+    wiki_options[:allow_editing] = false
+  end
+  opts.on('--live-preview', 'Enable the live preview feature in page editor.') do
+    wiki_options[:live_preview] = true
+  end
+  opts.on('--no-live-preview', 'Disable the live preview feature in page editor.') do
+    wiki_options[:live_preview] = false
+  end
+  opts.on('--allow-uploads [MODE]', [:dir, :page], 'Enable file uploads.',
+    'If set to 'dir', Gollum will store all uploads in the '<git-repo>/uploads/' directory.',
+    'If set to 'page', Gollum will store each upload at the currently edited page.') do |mode|
+    wiki_options[:allow_uploads]    = true
+    wiki_options[:per_page_uploads] = true if mode == :page
+  end
+  opts.on('--mathjax', 'Enable MathJax (renders mathematical equations).',
+    'By default, uses the 'TeX-AMS-MML_HTMLorMML' config with the 'autoload-all' extension.') do
+    wiki_options[:mathjax] = true
+  end
+  opts.on('--irb', 'Launch Gollum in 'console mode', with a predefined API.') do
+    options[:irb] = true
+  end
+  
+  opts.separator ''
+  opts.separator '  Minor:'
+  
+  opts.on('--h1-title', 'Use the first '<h1>' as page title.') do
+    wiki_options[:h1_title] = true
+  end
+  opts.on('--show-all', 'Also show files in the file view. By default, only valid pages are shown.') do
+    wiki_options[:show_all] = true
+  end
+  opts.on('--collapse-tree', 'Collapse the tree, when file view is opened. By default, the tree is expanded.') do
+    wiki_options[:collapse_tree] = true
+  end
+  opts.on('--user-icons [MODE]', [:gravatar, :identicon, :none], 'Use specific user-icons for history view.',
+    'Can be set to 'gravatar', 'identicon' or 'none'. Default: 'none'.') do |mode|
+    wiki_options[:user_icons] = mode
+  end
+  opts.on('--mathjax-config [FILE]', 'Specify path to a custom MathJax configuration.',
+    'If not specified, uses the '<git-repo>/mathjax.config.js' file.') do |file|
+    wiki_options[:mathjax_config] = file || 'mathjax.config.js'
+  end
+  opts.on('--plantuml-url [URL]', 'Sets the PlantUML server endpoint.') do |url|
+    wiki_options[:plantuml_url] = url
+  end
+  opts.on('--template-dir [PATH]', 'Specify custom mustache template directory.') do |path|
+    wiki_options[:template_dir] = path
+  end
+  
+  opts.separator ''
+  opts.separator '  Common:'
+  
+  opts.on('--help', 'Display this message.') do
+    puts opts
+    exit 0
+  end
+  opts.on('--version', 'Display the current version of Gollum.') do
+    puts 'Gollum ' + Gollum::VERSION
+    exit 0
+  end
+  
+  opts.separator ''
+end
+    
+                it 'detects closing brace on separate line from last element' do
+              inspect_source(source)
+    
+          it 'does not autocorrect the closing brace' do
+        new_source = autocorrect_source(source)
+        expect(new_source).to eq([source].join($RS))
+      end
+    end
+    
+            self
+      end
+    
+          # This is used for duck typing with `pair` nodes which also appear as
+      # `hash` elements.
+      #
+      # @return [false]
+      def hash_rocket?
+        false
+      end
+    
+    module RuboCop
+  module AST
+    # Common functionality for nodes that can be used as hash elements:
+    # `pair`, `kwsplat`
+    module HashElementNode
+      # Returns the key of this `hash` element.
+      #
+      # @note For keyword splats, this returns the whole node
+      #
+      # @return [Node] the key of the hash element
+      def key
+        node_parts[0]
+      end
