aaac80ca60386946f73b1fe2b22d5a41af10c82a
SUNsung <weatherius@gmail.com>
2018-10-14 08:02:21 +0300

    Rewrite [44IlpZC5VPp6Q446vn57aB1G644yTuS1N5lKRU44ys7.cpp] File in: 2018-10-14 08:02:20

--- cccccc/44IlpZC5VPp6Q446vn57aB1G644yTuS1N5lKRU44ys7.cpp
+++ cccccc/44IlpZC5VPp6Q446vn57aB1G644yTuS1N5lKRU44ys7.cpp
@@ -1,280 +1,154 @@
 
         
-          // brightray::MainDelegate:
-  std::unique_ptr<brightray::ContentClient> CreateContentClient() override;
-#if defined(OS_MACOSX)
-  void OverrideChildProcessPath() override;
-  void OverrideFrameworkBundlePath() override;
-#endif
+        class PlatformStyle;
+class WalletModel;
     
+        // Define a new address (which should add to the address book successfully).
+    QString new_address;
     
-    {}  // namespace atom
-    
-    namespace api {
+        secp256k1_pubkey_load(ctx, &pt, point);
+    secp256k1_scalar_set_b32(&s, scalar, &overflow);
+    if (overflow || secp256k1_scalar_is_zero(&s)) {
+        ret = 0;
+    } else {
+        unsigned char x[32];
+        unsigned char y[1];
+        secp256k1_sha256_t sha;
     }
     
     
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(InAppPurchase);
+    {
+    {        /* compute using ECDH function */
+        CHECK(secp256k1_ec_pubkey_create(ctx, &point[0], s_one) == 1);
+        CHECK(secp256k1_ecdh(ctx, output_ecdh, &point[0], s_b32) == 1);
+        /* compute 'explicitly' */
+        CHECK(secp256k1_ec_pubkey_create(ctx, &point[1], s_b32) == 1);
+        CHECK(secp256k1_ec_pubkey_serialize(ctx, point_ser, &point_ser_len, &point[1], SECP256K1_EC_COMPRESSED) == 1);
+        CHECK(point_ser_len == sizeof(point_ser));
+        secp256k1_sha256_initialize(&sha);
+        secp256k1_sha256_write(&sha, point_ser, point_ser_len);
+        secp256k1_sha256_finalize(&sha, output_ser);
+        /* compare */
+        CHECK(memcmp(output_ecdh, output_ser, sizeof(output_ser)) == 0);
+    }
+}
+    
+    
+    {    // These inputs are valid because PROTOCOLINFO accepts an OtherLine that is
+    // just an OptArguments, which enables multiple spaces to be present
+    // between the command and arguments.
+    CheckSplitTorReplyLine('COMMAND  ARGS', 'COMMAND', ' ARGS');
+    CheckSplitTorReplyLine('COMMAND   EVEN+more  ARGS', 'COMMAND', '  EVEN+more  ARGS');
+}
+    
+        BOOST_CHECK_EQUAL(obj.empty(), false);
+    BOOST_CHECK_EQUAL(obj.size(), 11);
+    
+            template <typename ElementType>
+        std::pair<std::shared_ptr<const Microsoft::MSR::CNTK::Matrix<ElementType>>, std::shared_ptr<Microsoft::MSR::CNTK::MBLayout>> PackedData()
+        {
+            if (!m_isPacked)
+                InvalidArgument('PackedValue::PackedData called on a Value object that has already been unpacked.');
+    }
+    
+                if (m_varKind == VariableKind::Input)
+            {
+                for (auto dim : m_shape.Dimensions())
+                {
+                    if (dim == 0)
+                        InvalidArgument('Variable '%S' has invalid shape '%S'.', AsString().c_str(), m_shape.AsString().c_str());
+                }
+            }
+    
+        double ElapsedSeconds();
+    
+            size_t rows, colsDummy;
+        fstream >> rows >> colsDummy;
+        TensorShape sampleLayout;
+        sampleLayout.Load(fstream, /*acceptLegacyFormat=*/true);
+        // some older files may have inconsistent tensor information
+        if (rows != 0 /*old file*/ && rows != sampleLayout.GetNumElements() /*even older file*/)
+        {
+            fprintf(stderr, 'WARNING: %ls InputValue has inconsistent serialized sample layout %s vs. number of rows %d. Resetting sample layout to vector.\n',
+                NodeName().c_str(), string(sampleLayout).c_str(), (int)rows);
+            sampleLayout = TensorShape(rows);
+        }
+    
+    //---- Don't implement some functions to reduce linkage requirements.
+//#define IMGUI_DISABLE_WIN32_DEFAULT_CLIPBOARD_FUNCTIONS   // [Win32] Don't implement default clipboard handler. Won't use and link with OpenClipboard/GetClipboardData/CloseClipboard etc.
+//#define IMGUI_DISABLE_WIN32_DEFAULT_IME_FUNCTIONS         // [Win32] Don't implement default IME handler. Won't use and link with ImmGetContext/ImmSetCompositionWindow.
+//#define IMGUI_DISABLE_FORMAT_STRING_FUNCTIONS             // Don't implement ImFormatString/ImFormatStringV so you can implement them yourself if you don't want to link with vsnprintf.
+//#define IMGUI_DISABLE_MATH_FUNCTIONS                      // Don't implement ImFabs/ImSqrt/ImPow/ImFmod/ImCos/ImSin/ImAcos/ImAtan2 wrapper so you can implement them yourself. Declare your prototypes in imconfig.h.
+//#define IMGUI_DISABLE_DEFAULT_ALLOCATORS                  // Don't implement default allocators calling malloc()/free() to avoid linking with them. You will need to call ImGui::SetAllocatorFunctions().
+    
+    #include 'imgui.h'
+#include 'imgui_impl_dx9.h'
+    
+            if (!font_face.Init(cfg, extra_flags))
+            return false;
+    
+    void ImGui_ImplFreeGLUT_SpecialUpFunc(int key, int x, int y)
+{
+    //printf('key_up_func %d\n', key);
+    ImGuiIO& io = ImGui::GetIO();
+    if (key + 256 < IM_ARRAYSIZE(io.KeysDown))
+        io.KeysDown[key + 256] = false;
+    ImGui_ImplFreeGLUT_UpdateKeyboardMods();
+    (void)x; (void)y; // Unused
+}
+    
+    class ButteraugliComparator : public Comparator {
+ public:
+  ButteraugliComparator(const int width, const int height,
+                        const std::vector<uint8_t>* rgb,
+                        const float target_distance, ProcessStats* stats);
+    }
+    
+    static const int kCbToBlueTable[256] = {
+  -227, -225, -223, -222, -220, -218, -216, -214, -213, -211, -209, -207,
+  -206, -204, -202, -200, -198, -197, -195, -193, -191, -190, -188, -186,
+  -184, -183, -181, -179, -177, -175, -174, -172, -170, -168, -167, -165,
+  -163, -161, -159, -158, -156, -154, -152, -151, -149, -147, -145, -144,
+  -142, -140, -138, -136, -135, -133, -131, -129, -128, -126, -124, -122,
+  -120, -119, -117, -115, -113, -112, -110, -108, -106, -105, -103, -101,
+   -99,  -97,  -96,  -94,  -92,  -90,  -89,  -87,  -85,  -83,  -82,  -80,
+   -78,  -76,  -74,  -73,  -71,  -69,  -67,  -66,  -64,  -62,  -60,  -58,
+   -57,  -55,  -53,  -51,  -50,  -48,  -46,  -44,  -43,  -41,  -39,  -37,
+   -35,  -34,  -32,  -30,  -28,  -27,  -25,  -23,  -21,  -19,  -18,  -16,
+   -14,  -12,  -11,   -9,   -7,   -5,   -4,   -2,    0,    2,    4,    5,
+     7,    9,   11,   12,   14,   16,   18,   19,   21,   23,   25,   27,
+    28,   30,   32,   34,   35,   37,   39,   41,   43,   44,   46,   48,
+    50,   51,   53,   55,   57,   58,   60,   62,   64,   66,   67,   69,
+    71,   73,   74,   76,   78,   80,   82,   83,   85,   87,   89,   90,
+    92,   94,   96,   97,   99,  101,  103,  105,  106,  108,  110,  112,
+   113,  115,  117,  119,  120,  122,  124,  126,  128,  129,  131,  133,
+   135,  136,  138,  140,  142,  144,  145,  147,  149,  151,  152,  154,
+   156,  158,  159,  161,  163,  165,  167,  168,  170,  172,  174,  175,
+   177,  179,  181,  183,  184,  186,  188,  190,  191,  193,  195,  197,
+   198,  200,  202,  204,  206,  207,  209,  211,  213,  214,  216,  218,
+   220,  222,  223,  225,
 };
     
-    #include 'atom/common/node_includes.h'
+    constexpr int kDefaultMemlimitMB = 6000; // in MB
     
-    // static
-mate::WrappableBase* LayoutManager::New(mate::Arguments* args) {
-  args->ThrowError('LayoutManager can not be created directly');
-  return nullptr;
-}
+    namespace guetzli {
+    }
     
-       unsigned short translate(unsigned short c) const 
-   { 
-      return c; 
-   }
-   unsigned short translate_nocase(unsigned short c) const 
-   { 
-      return (std::towlower)((wchar_t)c); 
-   }
+    // Writes DHT and SOS marker segments to out and fills in DC/AC Huffman tables
+// for each component of the image.
+bool BuildAndEncodeHuffmanCodes(const JPEGData& jpg, JPEGOutput out,
+                                std::vector<HuffmanCodeTable>* dc_huff_tables,
+                                std::vector<HuffmanCodeTable>* ac_huff_tables) {
+  const int ncomps = jpg.components.size();
+  dc_huff_tables->resize(ncomps);
+  ac_huff_tables->resize(ncomps);
+    }
     
-       // match procs, stored in s_match_vtable:
-   bool match_startmark();
-   bool match_endmark();
-   bool match_literal();
-   bool match_start_line();
-   bool match_end_line();
-   bool match_wild();
-   bool match_match();
-   bool match_word_boundary();
-   bool match_within_word();
-   bool match_word_start();
-   bool match_word_end();
-   bool match_buffer_start();
-   bool match_buffer_end();
-   bool match_backref();
-   bool match_long_set();
-   bool match_set();
-   bool match_jump();
-   bool match_alt();
-   bool match_rep();
-   bool match_combining();
-   bool match_soft_buffer_end();
-   bool match_restart_continue();
-   bool match_long_set_repeat();
-   bool match_set_repeat();
-   bool match_char_repeat();
-   bool match_dot_repeat_fast();
-   bool match_dot_repeat_slow();
-   bool match_dot_repeat_dispatch()
-   {
-      return ::boost::is_random_access_iterator<BidiIterator>::value ? match_dot_repeat_fast() : match_dot_repeat_slow();
-   }
-   bool match_backstep();
-   bool match_assert_backref();
-   bool match_toggle_case();
-#ifdef BOOST_REGEX_RECURSIVE
-   bool backtrack_till_match(std::size_t count);
-#endif
-   bool match_recursion();
-   bool match_fail();
-   bool match_accept();
-   bool match_commit();
-   bool match_then();
-   bool skip_until_paren(int index, bool match = true);
-    
-    template <class BidiIterator, class Allocator, class traits>
-bool perl_matcher<BidiIterator, Allocator, traits>::unwind_char_repeat(bool r)
+    template< bool Signed >
+struct operations< 2u, Signed > :
+    public msvc_arm_operations< typename make_storage_type< 2u, Signed >::type, operations< 2u, Signed > >
 {
-   saved_single_repeat<BidiIterator>* pmp = static_cast<saved_single_repeat<BidiIterator>*>(m_backup_state);
+    typedef msvc_arm_operations< typename make_storage_type< 2u, Signed >::type, operations< 2u, Signed > > base_type;
+    typedef typename base_type::storage_type storage_type;
+    typedef typename make_storage_type< 2u, Signed >::aligned aligned_storage_type;
     }
-    
-     /*
-  *   LOCATION:    see http://www.boost.org for most recent version.
-  *   FILE         regex_format.hpp
-  *   VERSION      see <boost/version.hpp>
-  *   DESCRIPTION: Provides formatting output routines for search and replace
-  *                operations.  Note this is an internal header file included
-  *                by regex.hpp, do not include on its own.
-  */
-    
-    template <class BidiIterator, class Allocator, class charT, class traits>
-bool regex_search(BidiIterator first, BidiIterator last, 
-                  match_results<BidiIterator, Allocator>& m, 
-                  const basic_regex<charT, traits>& e, 
-                  match_flag_type flags = match_default)
-{
-   return regex_search(first, last, m, e, flags, first);
-}
-    
-        //
-    // convert 'absolutes' to 'diffs'
-    //
-    Vec2 p = copyConfig->getControlPointAtIndex(0);
-    for (ssize_t i = 1; i < copyConfig->count(); ++i)
-    {
-        Vec2 current = copyConfig->getControlPointAtIndex(i);
-        Vec2 diff = current - p;
-        copyConfig->replaceControlPoint(diff, i);
-    }
-    
-    //
-// NOTE: Converting these macros into Templates is desirable, but please see
-// issue #16159 [https://github.com/cocos2d/cocos2d-x/pull/16159] for further info
-//
-#define EASERATE_TEMPLATE_IMPL(CLASSNAME, TWEEN_FUNC) \
-CLASSNAME* CLASSNAME::create(cocos2d::ActionInterval *action, float rate) \
-{ \
-    CLASSNAME *ease = new (std::nothrow) CLASSNAME(); \
-    if (ease) \
-    { \
-        if (ease->initWithAction(action, rate)) \
-            ease->autorelease(); \
-        else \
-            CC_SAFE_RELEASE_NULL(ease); \
-    } \
-    return ease; \
-} \
-CLASSNAME* CLASSNAME::clone() const \
-{ \
-    if(_inner) return CLASSNAME::create(_inner->clone(), _rate); \
-    return nullptr; \
-} \
-void CLASSNAME::update(float time) { \
-    _inner->update(TWEEN_FUNC(time, _rate)); \
-} \
-EaseRateAction* CLASSNAME::reverse() const { \
-    return CLASSNAME::create(_inner->reverse(), 1.f / _rate); \
-}
-    
-    FlipX * FlipX::clone() const
-{
-    // no copy constructor
-    return FlipX::create(_flipX);
-}
-//
-// FlipY
-//
-    
-            // fix for issue #1288, incorrect end value of repeat
-        if (std::abs(dt - 1.0f) < FLT_EPSILON && _total < _times)
-        {
-            if (!(sendUpdateEventToScript(1.0f, _innerAction)))
-                _innerAction->update(1.0f);
-            
-            _total++;
-        }
-    
-    
-    {        // only delete currentTarget if no actions were scheduled during the cycle (issue #481)
-        if (_currentTargetSalvaged && _currentTarget->actions->num == 0)
-        {
-            deleteHashElement(_currentTarget);
-        }
-        //if some node reference 'target', it's reference count >= 2 (issues #14050)
-        else if (_currentTarget->target->getReferenceCount() == 1)
-        {
-            deleteHashElement(_currentTarget);
-        }
-    }
-    
-        /** Removes all actions matching at least one bit in flags and the target.
-     *
-     * @param flags     The flag field to match the actions' flags based on bitwise AND.
-     * @param target    A certain target.
-     * @js NA
-     */
-    virtual void removeActionsByFlags(unsigned int flags, Node *target);
-    
-        if (action && action->initWithDuration(duration, gridSize, numberOfJumps, amplitude))
-    {
-        action->autorelease();
-        return action;
-    }
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-    
-    Animation* Animation::create(const Vector<AnimationFrame*>& arrayOfAnimationFrameNames, float delayPerUnit, unsigned int loops /* = 1 */)
-{
-    Animation *animation = new (std::nothrow) Animation();
-    animation->initWithAnimationFrames(arrayOfAnimationFrameNames, delayPerUnit, loops);
-    animation->autorelease();
-    return animation;
-}
-    
-        /** Gets the total Delay units of the Animation. 
-     *
-     * @return The total Delay units of the Animation.
-     */
-    float getTotalDelayUnits() const { return _totalDelayUnits; };
-    
-    /** Sets the delay in seconds of the 'delay unit'.
-     *
-     * @param delayPerUnit The delay in seconds of the 'delay unit'.
-     */
-    void setDelayPerUnit(float delayPerUnit) { _delayPerUnit = delayPerUnit; };
-    
-    /** Gets the delay in seconds of the 'delay unit'.
-     * 
-     * @return The delay in seconds of the 'delay unit'.
-     */
-    float getDelayPerUnit() const { return _delayPerUnit; };
-    
-        /** Adds a Animation with a name.
-     *
-     * @param animation An animation.
-     * @param name The name of animation.
-     */
-    void addAnimation(Animation *animation, const std::string& name);
-    
-    
-    { protected:
-  /// The CAN client is started.
-  bool is_started_ = false;
-};
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-namespace can {
-    }
-    }
-    }
-    }
-    
-    
-    {
-    {
-    {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-    
-    #include <cstring>
-    
-      dev_handler_ = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-  if (dev_handler_ < 0) {
-    AERROR << 'open device error code [' << dev_handler_ << ']: ';
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
-  }
-    
-    template <typename SensorType>
-void CanReceiver<SensorType>::RecvThreadFunc() {
-  AINFO << 'Can client receiver thread starts.';
-  CHECK_NOTNULL(can_client_);
-  CHECK_NOTNULL(pt_manager_);
-    }
-    
-      ProtocolData<::apollo::canbus::ChassisDetail> mpd;
-  SenderMessage<::apollo::canbus::ChassisDetail> msg(1, &mpd);
-  EXPECT_FALSE(sender.NeedSend(msg, 1));
-  EXPECT_EQ(msg.message_id(), 1);
-  int32_t period = msg.curr_period();
-  msg.UpdateCurrPeriod(-50);
-  EXPECT_EQ(msg.curr_period(), period + 50);
-  EXPECT_EQ(msg.CanFrame().id, 1);
