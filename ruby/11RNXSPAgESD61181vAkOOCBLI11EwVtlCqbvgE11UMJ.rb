9cf2bc268bdbe45503cca8928c1f5daa1e56b7cb
SUNsung <weatherius@gmail.com>
2018-10-15 17:31:07 +0300

    Rewrite [11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb] File in: 2018-10-15 17:31:06

--- ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
+++ ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
@@ -1,98 +1,42 @@
 
         
-            if successfully_sent?(resource)
-      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
-    end
+            puts 'Your system is ready to brew.' unless Homebrew.failed?
   end
+end
+
     
-        def email_changed(record, opts={})
-      devise_mail(record, :email_changed, opts)
-    end
-    
-    DeviseCreateUsers.migrate(:up)
-    
-            if options[:bypass]
-          ActiveSupport::Deprecation.warn(<<-DEPRECATION.strip_heredoc, caller)
-          [Devise] bypass option is deprecated and it will be removed in future version of Devise.
-          Please use bypass_sign_in method instead.
-          Example:
-    
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
-        end
-      end
-    
-            # Create an environment so we can determine the active
-        # machines...
-        found = false
-        env = vagrant_env(home_path)
-        env.active_machines.each do |name, provider|
-          if name.to_s == self.name.to_s &&
-            provider.to_s == self.provider.to_s
-            found = true
-            break
-          end
-        end
-    
-            # Upload a file to the remote machine.
-        #
-        # @param [String] from Path of the file locally to upload.
-        # @param [String] to Path of where to save the file on the remote
-        #   machine.
-        def upload(from, to)
-        end
-    
-            # Returns the instance variables as a hash of key-value pairs.
-        def instance_variables_hash
-          instance_variables.inject({}) do |acc, iv|
-            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
-            acc
-          end
-        end
+      DATA = :DATA
     
         remove_duplicates
     remove_index :share_visibilities, name: :shareable_and_user_id
     add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
     
-    When /^I toggle nsfw posts$/ do
-  find('.toggle_nsfw_state', match: :first).click
+    Given /^I have been invited by '([^\']+)'$/ do |email|
+  AppConfig.settings.enable_registrations = false
+  @inviter = User.find_by_email(email)
+  @inviter_invite_count = @inviter.invitation_code.count
+  i = EmailInviter.new('new_invitee@example.com', @inviter)
+  i.send!
 end
     
-      # creates a new user object from the factory with some default attributes
-  # and the given override attributes, adds the standard aspects to it
-  # and returns it
-  def create_user(overrides={})
-    default_attrs = {
-        :password => 'password',
-        :password_confirmation => 'password',
-        :getting_started => false
-    }
+    require 'coveralls'
+Coveralls.wear!('rails')
     
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
+      class SendPrivate < Base
+    def perform(*_args)
+      # don't federate in cucumber
     end
   end
-end
-
     
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
+      describe '#new' do
+    before do
+      sign_in alice, scope: :user
     end
-  end
-end
+    
+        context 'on my own post' do
+      it 'succeeds' do
+        @target = alice.post :status_message, text: 'AWESOME', to: @alices_aspect.id
+        post :create, params: like_hash, format: :json
+        expect(response.code).to eq('201')
+      end
+    end
