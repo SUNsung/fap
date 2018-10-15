3e90d09346544ba7b8c936e5735bf5113303968e
SUNsung <weatherius@gmail.com>
2018-10-15 03:43:49 +0300

    Rewrite [4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp] File in: 2018-10-15 03:43:48

--- cccccc/4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp
+++ cccccc/4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp
@@ -1,143 +1,81 @@
 
         
-        Q_SIGNALS:
-    void valueChanged();
-    
-        // Initialize relevant QT models.
-    std::unique_ptr<const PlatformStyle> platformStyle(PlatformStyle::instantiate('other'));
-    auto node = interfaces::MakeNode();
-    OptionsModel optionsModel(*node);
-    AddWallet(wallet);
-    WalletModel walletModel(std::move(node->getWallets()[0]), *node, platformStyle.get(), &optionsModel);
-    RemoveWallet(wallet);
-    EditAddressDialog editAddressDialog(EditAddressDialog::NewSendingAddress);
-    editAddressDialog.setModel(walletModel.getAddressTableModel());
+        #ifndef TESSERACT_ARCH_INTSIMDMATRIX_H_
+#define TESSERACT_ARCH_INTSIMDMATRIX_H_
     
     
-    {    count = 0;
-    for (i = 0; i < len; i++) {
-        r[i].infinity = a[i].infinity;
-        if (!a[i].infinity) {
-            secp256k1_ge_set_gej_zinv(&r[i], &a[i], &azi[count++]);
-        }
+    {}  // namespace tesseract.
+
+    
+    // Main entry point for Paragraph Detection Algorithm.
+//
+// Given a set of equally spaced textlines (described by row_infos),
+// Split them into paragraphs.  See http://goto/paragraphstalk
+//
+// Output:
+//   row_owners - one pointer for each row, to the paragraph it belongs to.
+//   paragraphs - this is the actual list of PARA objects.
+//   models - the list of paragraph models referenced by the PARA objects.
+//            caller is responsible for deleting the models.
+void DetectParagraphs(int debug_level,
+                      GenericVector<RowInfo> *row_infos,
+                      GenericVector<PARA *> *row_owners,
+                      PARA_LIST *paragraphs,
+                      GenericVector<ParagraphModel *> *models);
+    
+    
+    {  ScrollView* sv_window_;
+};
+    
+      /// Threshold the rectangle, taking everything except the src_pix
+  /// from the class, using thresholds/hi_values to the output pix.
+  /// NOTE that num_channels is the size of the thresholds and hi_values
+  // arrays and also the bytes per pixel in src_pix.
+  void ThresholdRectToPix(Pix* src_pix, int num_channels,
+                          const int* thresholds, const int* hi_values,
+                          Pix** pix) const;
+    
+    	// Now set the registry keys
+	HKEY root = GetRootKey(opt);
+    
+            template<typename OnWriteSummaryFunc>
+        void WriteSummary(const ValuePtr& accumulatedLoss, const ValuePtr& accumulatedMetric,
+                          OnWriteSummaryFunc callback)
+        {
+            if (accumulatedLoss && m_samples.second > 0)
+            {
+                m_loss.second = accumulatedLoss->AsScalar<double>();
+            }
     }
-    free(azi);
-}
     
-    #define Ch(x,y,z) ((z) ^ ((x) & ((y) ^ (z))))
-#define Maj(x,y,z) (((x) & (y)) | ((z) & ((x) | (y))))
-#define Sigma0(x) (((x) >> 2 | (x) << 30) ^ ((x) >> 13 | (x) << 19) ^ ((x) >> 22 | (x) << 10))
-#define Sigma1(x) (((x) >> 6 | (x) << 26) ^ ((x) >> 11 | (x) << 21) ^ ((x) >> 25 | (x) << 7))
-#define sigma0(x) (((x) >> 7 | (x) << 25) ^ ((x) >> 18 | (x) << 14) ^ ((x) >> 3))
-#define sigma1(x) (((x) >> 17 | (x) << 15) ^ ((x) >> 19 | (x) << 13) ^ ((x) >> 10))
-    
-    int secp256k1_ecdsa_recover(const secp256k1_context* ctx, secp256k1_pubkey *pubkey, const secp256k1_ecdsa_recoverable_signature *signature, const unsigned char *msg32) {
-    secp256k1_ge q;
-    secp256k1_scalar r, s;
-    secp256k1_scalar m;
-    int recid;
-    VERIFY_CHECK(ctx != NULL);
-    ARG_CHECK(secp256k1_ecmult_context_is_built(&ctx->ecmult_ctx));
-    ARG_CHECK(msg32 != NULL);
-    ARG_CHECK(signature != NULL);
-    ARG_CHECK(pubkey != NULL);
-    }
-    
-    
-    {    /* On the first run, return 0 to force a second run */
-    if (counter == 0) {
-        memset(nonce32, 0, 32);
-        return 1;
-    }
-    /* On the second run, return an overflow to force a third run */
-    if (counter == 1) {
-        memset(nonce32, 0xff, 32);
-        return 1;
-    }
-    /* On the next run, return a valid nonce, but flip a coin as to whether or not to fail signing. */
-    memset(nonce32, 1, 32);
-    return secp256k1_rand_bits(1);
-}
-    
-    BOOST_AUTO_TEST_CASE(bip173_testvectors_invalid)
-{
-    static const std::string CASES[] = {
-        ' 1nwldj5',
-        '\x7f''1axkwrx',
-        '\x80''1eym55h',
-        'an84characterslonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1569pvx',
-        'pzry9x0s0muk',
-        '1pzry9x0s0muk',
-        'x1b4n0q5v',
-        'li1dgmt3',
-        'de1lg7wt\xff',
-        'A1G7SGD8',
-        '10a06t8',
-        '1qzzfhee',
-        'a12UEL5L',
-        'A12uEL5L',
-    };
-    for (const std::string& str : CASES) {
-        auto ret = bech32::Decode(str);
-        BOOST_CHECK(ret.first.empty());
-    }
-}
-    
-    
-    {    RejectDifficultyMismatch(difficulty, expected_difficulty);
-}
-    
-    
-//------------------------------------------------------------------------------
-// Tools for emulating variadic templates in C++98.  The basic idea here is
-// stolen from the boost preprocessor metaprogramming library and cut down to
-// be just general enough for what we need.
-    
-    ExtensionFunction::ResponseAction
-NwCurrentWindowInternalSetShadowFunction::Run() {
-#if defined(OS_MACOSX)
-  EXTENSION_FUNCTION_VALIDATE(args_);
-  bool shadow;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetBoolean(0, &shadow));
-  AppWindow* window = getAppWindow(this);
-  SetShadowOnWindow(window->GetNativeWindow(), shadow);
+    #pragma once
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
 #endif
-  return RespondNow(NoArguments());
-}
-    
-    // The browser want to open a file.
-IPC_MESSAGE_CONTROL1(ShellViewMsg_Open,
-                     std::string /* file name */)
-    
-      static void ClearCache(content::RenderProcessHost* render_view_host);
-  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
-                             const std::string& proxy_config);
-    
-    #include <map>
+#ifdef _WIN32
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif // NOMINMAX
+#pragma comment(lib, 'Dbghelp.lib')
+#else
+#include <execinfo.h>
+#include <cxxabi.h>
+#endif
     
     
-bool MenuDelegate::GetAcceleratorForCommandId(
-      int command_id,
-      ui::Accelerator* accelerator) const {
-  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
-    return false;
+    {            // reshape Input(2)
+            Input(2)->SetDims(TensorShape(dimsA), false);
+            fprintf(stderr, '\n%ls %ls operation: For legacy compatibility, the sample layout of third input (%ls %ls operation) was patched to [%s] (from [%s])\n',
+                NodeName().c_str(), OperationName().c_str(), Input(2)->NodeName().c_str(), Input(2)->OperationName().c_str(), string(Input(2)->GetSampleLayout()).c_str(), dimsCstring.c_str());
+        }
+    
+    template <class RNG>
+struct SeedData {
+  SeedData() {
+    Random::secureRandom(seedData.data(), seedData.size() * sizeof(uint32_t));
+  }
     }
     
-    void Menu::Create(const base::DictionaryValue& option) {
-  is_menu_modified_ = true;
-  menu_delegate_.reset(new MenuDelegate(object_manager()));
-  menu_model_.reset(new ui::NwMenuModel(menu_delegate_.get()));
-    }
-    
-    typedef std::map<std::string,std::string> KeyMap;
-    
-      // Returns true iff the test part passed.
-  bool passed() const { return type_ == kSuccess; }
-    
-    int main(int argc, char **argv) {
-  InitGoogleTest(&argc, argv);
-    }
-    
-      // The default c'tor constructs a NULL string.
-  MyString() : c_string_(NULL) {}
+    #include <folly/io/Cursor.h>
+#include <folly/io/IOBuf.h>
+#include <folly/lang/Bits.h>
