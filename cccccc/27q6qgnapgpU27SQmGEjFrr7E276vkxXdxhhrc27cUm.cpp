02f0b4b356a3fc63acdd6531a2e378a722723c1a
SUNsung <weatherius@gmail.com>
2018-10-15 16:31:33 +0300

    Rewrite [27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp] File in: 2018-10-15 16:31:32

--- cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
+++ cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
@@ -1,231 +1,108 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#ifndef TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
+        
+    {  return render_process_host;
+}
     
     
-    {  tensorflow::DeviceNameUtils::ParsedName parsed_name;
-  if (!tensorflow::DeviceNameUtils::ParseFullName(node_def.device(),
-                                                  &parsed_name)) {
-    LOG(WARNING) << 'Failed to parse device from node_def: '
-                 << node_def.ShortDebugString();
-    return '';
+    { private:
+  App();
+  DISALLOW_COPY_AND_ASSIGN(App);
+};
+    
+    void Clipboard::Call(const std::string& method,
+                     const base::ListValue& arguments) {
+  if (method == 'Set') {
+    std::string text, type;
+    arguments.GetString(0, &text);
+    arguments.GetString(1, &type);
+    SetText(text);
+  } else if (method == 'Clear') {
+    Clear();
+  } else {
+    NOTREACHED() << 'Invalid call to Clipboard method:' << method
+                 << ' arguments:' << arguments;
   }
-  string class_name = '';
-  tensorflow::FindKernelDef(tensorflow::DeviceType(parsed_name.type.c_str()),
-                            node_def, nullptr /* kernel_def */, &class_name)
-      .IgnoreError();
-  return class_name;
 }
     
-        http://www.apache.org/licenses/LICENSE-2.0
     
-        http://www.apache.org/licenses/LICENSE-2.0
+    {  DISALLOW_COPY_AND_ASSIGN(MenuDelegate);
+};
     
-        /** Colorize an icon (given object) with the icon color */
-    QIcon SingleColorIcon(const QIcon& icon) const;
+    #define TegraUnaryOp_Invoker(name, func) TegraGenOp_Invoker(name, func, 1, 1, 0, \
+                                                            RANGE_DATA(ST, src1_data, src1_step), src1_step, \
+                                                            RANGE_DATA(DT, dst1_data, dst1_step), dst1_step )
     
-    #endif // BITCOIN_QT_TRAFFICGRAPHWIDGET_H
-
     
-    #include <script/script.h>
-    
-    static void CheckSplitTorReplyLine(std::string input, std::string command, std::string args)
-{
-    BOOST_TEST_MESSAGE(std::string('CheckSplitTorReplyLine(') + input + ')');
-    auto ret = SplitTorReplyLine(input);
-    BOOST_CHECK_EQUAL(ret.first, command);
-    BOOST_CHECK_EQUAL(ret.second, args);
+    {    struct KeypointStore {
+        virtual void push(f32 kpX, f32 kpY, f32 kpSize, f32 kpAngle=-1, f32 kpResponse=0, s32 kpOctave=0, s32 kpClass_id=-1) = 0;
+        virtual ~KeypointStore() {};
+    };
 }
     
-    #if defined(__GLIBCXX__) && __GLIBCXX__ < 20080201
-//  std::showpos is broken on old libstdc++ as provided with OSX.  See
-//  http://gcc.gnu.org/ml/libstdc++/2007-11/msg00075.html
-#   define TINYFORMAT_OLD_LIBSTDCPLUSPLUS_WORKAROUND
-#endif
+                uint8x16x2_t v_src = vld2q_u8(src + sj);
+            vst1q_u8(dst + dj, v_src.val[coi]);
     
-            // TODO: make these functions friends of NDViewArray and move to Utils?
-        static bool HasNan(const NDArrayViewPtr& value, const char* name);
-        static void Print(const NDArrayViewPtr& value, const char* msg);
-    
-                m_packedData = nullptr;
-            m_packedDataLayout = nullptr;
-            m_isPacked = false;
-    
-    template<class ElemType>
-class ComputationNetworkFromFile : public ComputationNetwork
+    inline uint8x8_t convertToGray(const uint16x8_t & v_r,
+                               const uint16x8_t & v_g,
+                               const uint16x8_t & v_b,
+                               const uint16x4_t & v_r2y,
+                               const uint16x4_t & v_g2y,
+                               const uint16x4_t & v_b2y)
 {
-public:
-    ComputationNetworkFromFile(const IConfigRecordPtr configp) :
-        ComputationNetwork()
-    {
-        let& config = *configp;
+    uint32x4_t v_dst0 = vmull_u16(vget_low_u16(v_g), v_g2y);
+    uint32x4_t v_dst1 = vmull_u16(vget_high_u16(v_g), v_g2y);
+    }
+    
+    namespace CAROTENE_NS { namespace internal {
     }
     }
     
+                for(; j < (ptrdiff_t)size.width - 16 - 3; j += 16, ptr += 16)
+            {
+                internal::prefetch(ptr);
+                internal::prefetch(ptr + pixel[0]);
+                internal::prefetch(ptr + pixel[2]);
+    }
     
-    {            // reshape Input(2)
-            Input(2)->SetDims(TensorShape(dimsA), false);
-            fprintf(stderr, '\n%ls %ls operation: For legacy compatibility, the sample layout of third input (%ls %ls operation) was patched to [%s] (from [%s])\n',
-                NodeName().c_str(), OperationName().c_str(), Input(2)->NodeName().c_str(), Input(2)->OperationName().c_str(), string(Input(2)->GetSampleLayout()).c_str(), dimsCstring.c_str());
+    
+    {            vec128 v_src = vld1q(src + js);
+            vec128 v_dst = vrev64q(v_src);
+            v_dst = vcombine(vget_high(v_dst), vget_low(v_dst));
+            vst1q(dst + jd - step_base, v_dst);
+        }
+        for (; js < roiw_tail; js += step_tail, jd -= step_tail)
+        {
+            vec64 v_src = vld1(src + js);
+            vst1(dst + jd - step_tail, vrev64(v_src));
         }
     
-    typedef enum _match_flags
-{
-   match_default = 0,
-   match_not_bol = 1,                                /* first is not start of line */
-   match_not_eol = match_not_bol << 1,               /* last is not end of line */
-   match_not_bob = match_not_eol << 1,               /* first is not start of buffer */
-   match_not_eob = match_not_bob << 1,               /* last is not end of buffer */
-   match_not_bow = match_not_eob << 1,               /* first is not start of word */
-   match_not_eow = match_not_bow << 1,               /* last is not end of word */
-   match_not_dot_newline = match_not_eow << 1,       /* \n is not matched by '.' */
-   match_not_dot_null = match_not_dot_newline << 1,  /* '\0' is not matched by '.' */
-   match_prev_avail = match_not_dot_null << 1,       /* *--first is a valid expression */
-   match_init = match_prev_avail << 1,               /* internal use */
-   match_any = match_init << 1,                      /* don't care what we match */
-   match_not_null = match_any << 1,                  /* string can't be null */
-   match_continuous = match_not_null << 1,           /* each grep match must continue from */
-                                                     /* uninterupted from the previous one */
-   match_partial = match_continuous << 1,            /* find partial matches */
-   
-   match_stop = match_partial << 1,                  /* stop after first match (grep) V3 only */
-   match_not_initial_null = match_stop,              /* don't match initial null, V4 only */
-   match_all = match_stop << 1,                      /* must find the whole of input even if match_any is set */
-   match_perl = match_all << 1,                      /* Use perl matching rules */
-   match_posix = match_perl << 1,                    /* Use POSIX matching rules */
-   match_nosubs = match_posix << 1,                  /* don't trap marked subs */
-   match_extra = match_nosubs << 1,                  /* include full capture information for repeated captures */
-   match_single_line = match_extra << 1,             /* treat text as single line and ignor any \n's when matching ^ and $. */
-   match_unused1 = match_single_line << 1,           /* unused */
-   match_unused2 = match_unused1 << 1,               /* unused */
-   match_unused3 = match_unused2 << 1,               /* unused */
-   match_max = match_unused3,
-    }
     
-    #include <new>
-#ifdef BOOST_HAS_THREADS
-#include <boost/regex/pending/static_mutex.hpp>
-#endif
-    
-     /*
-  *   LOCATION:    see http://www.boost.org for most recent version.
-  *   FILE         basic_regex_creator.cpp
-  *   VERSION      see <boost/version.hpp>
-  *   DESCRIPTION: Declares template class basic_regex_creator which fills in
-  *                the data members of a regex_data object.
-  */
-    
-    #ifdef BOOST_REGEX_USE_WIN32_LOCALE
-template <class charT, class implementationT = w32_regex_traits<charT> >
-struct regex_traits;
-#elif defined(BOOST_REGEX_USE_CPP_LOCALE)
-template <class charT, class implementationT = cpp_regex_traits<charT> >
-struct regex_traits;
-#else
-template <class charT, class implementationT = c_regex_traits<charT> >
-struct regex_traits;
-#endif
-    
-    
-    {} // namespace boost
-    
-    
-#ifndef BOOST_REGEX_MATCH_HPP
-#define BOOST_REGEX_MATCH_HPP
-    
-    template <class BidiIterator, class charT, class traits>
-bool regex_search(BidiIterator first, BidiIterator last, 
-                  const basic_regex<charT, traits>& e, 
-                  match_flag_type flags = match_default)
-{
-   if(e.flags() & regex_constants::failbit)
-      return false;
-    }
-    
-    
+    {} // namespace CAROTENE_NS
 
     
-    void compareBenchmarkResults(const std::string& base, const std::string& test) {
-  printResultComparison(resultsFromFile(base), resultsFromFile(test));
+    inline void APCLocalArray::scan(type_scan::Scanner& scanner) const {
+  scanner.scan(*localCache(), m_size * sizeof(TypedValue));
 }
     
-    static uint32_t arrayTestFunc() {
-  uint32_t v1 = folly::Random::rand32();
-  uint32_t v2 = folly::Random::rand32();
-  uint64_t v3 = v1 + v2;
-  uint32_t a[4] = {v1, v2, v1, v2};
-  FOLLY_SDT(folly, test_static_tracepoint_array, a, v1, v3);
-  return v1 + v2;
+    void Config::ParseIniString(const std::string &iniStr, IniSettingMap &ini,
+                            const bool constants_only /* = false */ ) {
+  Config::SetParsedIni(ini, iniStr, '', constants_only, true);
 }
     
-      template <typename ExecutorT = Executor>
-  class KeepAlive {
-   public:
-    KeepAlive() = default;
-    }
+          std::string line = argv[cnt+1];
+      std::string section = 'php';
+      int pos_period = line.find_first_of('.');
+      int pos_equals = line.find_first_of('=');
     
+    #include <string>
+#include <fstream>
     
-    {} // namespace folly
-
-    
-    
-    {    return FuncAndKeepAlive(std::move(f), this);
-  }
-    
-        // Test the upper boundary of conversion to int64_t microseconds
-    using usec_i64 = std::chrono::duration<int64_t, std::micro>;
-    ts.tv_sec = 9223372036854LL;
-    ts.tv_nsec = 775807000;
-    EXPECT_EQ(std::numeric_limits<int64_t>::max(), to<usec_i64>(ts).count());
-    
-    /// Wrapper around the makeCompressionCounterHandler() extension point.
-class CompressionCounter {
- public:
-  CompressionCounter() {}
-  CompressionCounter(
-      folly::io::CodecType codecType,
-      folly::StringPiece codecName,
-      folly::Optional<int> level,
-      CompressionCounterKey key,
-      CompressionCounterType counterType) {
-    initialize_ = [=]() {
-      return makeCompressionCounterHandler(
-          codecType, codecName, level, key, counterType);
-    };
-    DCHECK(!initialize_.hasAllocatedMemory());
-  }
-    }
-    
-    
-    {
-    {
-    {
-    {} // namespace detail
-} // namespace compression
-} // namespace io
-} // namespace folly
-
-    
-      explicit Options(
-      Format format_ = Format::ZLIB,
-      int windowSize_ = 15,
-      int memLevel_ = 8,
-      int strategy_ = Z_DEFAULT_STRATEGY)
-      : format(format_),
-        windowSize(windowSize_),
-        memLevel(memLevel_),
-        strategy(strategy_) {}
+    /**
+ * Search for PHP or non-PHP files under a directory, calling callback for
+ * each one found.
+ */
+template <typename F>
+void find(std::vector<std::string> &out,
+          const std::string &root, const std::string& path, bool php,
+          const F& callback);
