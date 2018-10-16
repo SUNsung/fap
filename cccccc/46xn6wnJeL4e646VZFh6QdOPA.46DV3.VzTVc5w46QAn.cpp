49763044d3ae592276565664e729c25fc550cb9e
SUNsung <weatherius@gmail.com>
2018-10-16 17:35:19 +0300

    Rewrite [46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp] File in: 2018-10-16 17:35:18

--- cccccc/46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp
+++ cccccc/46xn6wnJeL4e646VZFh6QdOPA.46DV3.VzTVc5w46QAn.cpp
@@ -1,198 +1,120 @@
 
         
-        template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8>
-internal::ValueArray8<T1, T2, T3, T4, T5, T6, T7, T8> Values(T1 v1, T2 v2,
-    T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8) {
-  return internal::ValueArray8<T1, T2, T3, T4, T5, T6, T7, T8>(v1, v2, v3, v4,
-      v5, v6, v7, v8);
+        // Prints the given value using the << operator if it has one;
+// otherwise prints the bytes in it.  This is what
+// UniversalPrinter<T>::Print() does when PrintTo() is not specialized
+// or overloaded for type T.
+//
+// A user can override this behavior for a class type Foo by defining
+// an overload of PrintTo() in the namespace where Foo is defined.  We
+// give the user this option as sometimes defining a << operator for
+// Foo is not desirable (e.g. the coding style may prevent doing it,
+// or there is already a << operator but it doesn't do what the user
+// wants).
+template <typename T>
+void PrintTo(const T& value, ::std::ostream* os) {
+  // DefaultPrintTo() is overloaded.  The type of its first two
+  // arguments determine which version will be picked.  If T is an
+  // STL-style container, the version for container will be called; if
+  // T is a pointer, the pointer version will be called; otherwise the
+  // generic version will be called.
+  //
+  // Note that we check for container types here, prior to we check
+  // for protocol message types in our operator<<.  The rationale is:
+  //
+  // For protocol messages, we want to give people a chance to
+  // override Google Mock's format by defining a PrintTo() or
+  // operator<<.  For STL containers, other formats can be
+  // incompatible with Google Mock's format for the container
+  // elements; therefore we check for container types here to ensure
+  // that our format is used.
+  //
+  // The second argument of DefaultPrintTo() is needed to bypass a bug
+  // in Symbian's C++ compiler that prevents it from picking the right
+  // overload between:
+  //
+  //   PrintTo(const T& x, ...);
+  //   PrintTo(T* x, ...);
+  DefaultPrintTo(IsContainerTest<T>(0), is_pointer<T>(), value, os);
 }
     
-    // The variables defined in the type-parameterized test macros are
-// static as typically these macros are used in a .h file that can be
-// #included in multiple translation units linked together.
-# define TYPED_TEST_CASE_P(CaseName) \
-  static ::testing::internal::TypedTestCasePState \
-      GTEST_TYPED_TEST_CASE_P_STATE_(CaseName)
+      // Gets the message associated with the test part.
+  const char* message() const { return message_.c_str(); }
     
-    // Makes sure this header is not included before gtest.h.
-#ifndef GTEST_INCLUDE_GTEST_GTEST_H_
-# error Do not include gtest_pred_impl.h directly.  Include gtest.h instead.
-#endif  // GTEST_INCLUDE_GTEST_GTEST_H_
-    
-      // RemoveFileName returns the directory path with the filename removed.
-  // Example: FilePath('path/to/file').RemoveFileName() returns 'path/to/'.
-  // If the FilePath is 'a_file' or '/a_file', RemoveFileName returns
-  // FilePath('./') or, on Windows, FilePath('.\\'). If the filepath does
-  // not have a file, like 'just/a/dir/', it returns the FilePath unmodified.
-  // On Windows platform, '\' is the path separator, otherwise it is '/'.
-  FilePath RemoveFileName() const;
-    
-    // The following family of struct and struct templates are used to
-// represent type lists.  In particular, TypesN<T1, T2, ..., TN>
-// represents a type list with N types (T1, T2, ..., and TN) in it.
-// Except for Types0, every struct in the family has two member types:
-// Head for the first type in the list, and Tail for the rest of the
-// list.
-    
-      // Called after a test ends.
-  virtual void OnTestEnd(const TestInfo& /* test_info */) {
-    int difference = Water::allocated() - initially_allocated_;
-    }
-    
-    #include <limits.h>
-#include 'sample1.h'
-#include 'gtest/gtest.h'
+      // Given directory = 'dir', base_name = 'test', number = 0,
+  // extension = 'xml', returns 'dir/test.xml'. If number is greater
+  // than zero (e.g., 12), returns 'dir/test_12.xml'.
+  // On Windows platform, uses \ as the separator rather than /.
+  static FilePath MakeFileName(const FilePath& directory,
+                               const FilePath& base_name,
+                               int number,
+                               const char* extension);
     
     
-    {} // namespace caffe2
-
+    {
+    {}  // namespace internal
+}  // namespace testing
     
-    REGISTER_CPU_OPERATOR(
-    MergeMultiMapFeatureTensors,
-    MergeMultiMapFeatureTensorsOp<CPUContext>);
-OPERATOR_SCHEMA(MergeMultiMapFeatureTensors)
-    .SetDoc(
-        'Merge given multi-feature tensors with map features into one.' + doc)
-    .NumInputs([](int n) { return n >= 5 && n % 5 == 0; })
-    .NumOutputs(5)
-    .Input(0, 'in1_lengths', '.lengths')
-    .Input(1, 'in1_keys', '.keys')
-    .Input(2, 'in1_values_lengths', '.values.lengths')
-    .Input(3, 'in1_values_keys', '.values.keys')
-    .Input(4, 'in1_values_values', '.values.values')
-    .Output(0, 'out_lengths', '.lengths')
-    .Output(1, 'out_keys', '.keys')
-    .Output(2, 'out_values_lengths', '.values_lengths')
-    .Output(3, 'out_values_keys', '.values.keys')
-    .Output(4, 'out_values_values', '.values.values');
-    
-    
-<details>
-    
-    op = core.CreateOperator(
-    'Floor',
-    ['X'],
-    ['X'],
-)
-    
-    
-    {} // namespace caffe2
-    
-    OPERATOR_SCHEMA(GivenTensorInt64Fill)
-    .NumInputs(0, 1)
-    .NumOutputs(1)
-    .AllowInplace({{0, 0}})
-    .Arg(
-        'values',
-        'The value for the elements of the output tensor.',
-        true /* required */)
-    .Arg(
-        'shape',
-        'The shape of the output tensor.'
-        'Cannot set the shape argument and pass in an input at the same time.')
-    .Arg(
-        'extra_shape',
-        'The additional dimensions appended at the end of the shape indicated'
-        'by the input blob.'
-        'Cannot set the extra_shape argument when there is no input blob.')
-    .Arg(
-        'input_as_shape',
-        '1D tensor containing the desired output shape. First input must be in CPU context.')
-    .TensorInferenceFunction(FillerTensorInference<TensorProto_DataType_INT64>);
-    
-    ChannelCredentials::~ChannelCredentials() {}
-    
-    grpc_slice CoreCodegen::grpc_slice_new_with_user_data(void* p, size_t len,
-                                                      void (*destroy)(void*),
-                                                      void* user_data) {
-  return ::grpc_slice_new_with_user_data(p, len, destroy, user_data);
-}
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
-    
-    void CensusClientCallData::Destroy(grpc_call_element* elem,
-                                   const grpc_call_final_info* final_info,
-                                   grpc_closure* then_call_closure) {
-  const uint64_t request_size = GetOutgoingDataSize(final_info);
-  const uint64_t response_size = GetIncomingDataSize(final_info);
-  double latency_ms = absl::ToDoubleMilliseconds(absl::Now() - start_time_);
-  ::opencensus::stats::Record(
-      {{RpcClientSentBytesPerRpc(), static_cast<double>(request_size)},
-       {RpcClientReceivedBytesPerRpc(), static_cast<double>(response_size)},
-       {RpcClientRoundtripLatency(), latency_ms},
-       {RpcClientServerLatency(),
-        ToDoubleMilliseconds(absl::Nanoseconds(elapsed_time_))},
-       {RpcClientSentMessagesPerRpc(), sent_message_count_},
-       {RpcClientReceivedMessagesPerRpc(), recv_message_count_}},
-      {{ClientMethodTagKey(), method_},
-       {ClientStatusTagKey(), StatusCodeToString(final_info->final_status)}});
-  grpc_slice_unref_internal(path_);
-  context_.EndSpan();
-}
-    
-    
-    {        return true;
-    }
-    
-    
-    {private:
-    CC_DISALLOW_COPY_AND_ASSIGN(Waves);
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9>
+struct Types9 {
+  typedef T1 Head;
+  typedef Types8<T2, T3, T4, T5, T6, T7, T8, T9> Tail;
 };
     
-    const std::string& DHTResponseMessage::getType() const { return R; }
+    $range i 2..n
     
-    public:
-  DHTResponseMessage(const std::shared_ptr<DHTNode>& localNode,
-                     const std::shared_ptr<DHTNode>& remoteNode,
-                     const std::string& transactionID);
-    
-    DHTRoutingTable::DHTRoutingTable(const std::shared_ptr<DHTNode>& localNode)
-    : localNode_(localNode),
-      root_(make_unique<DHTBucketTreeNode>(
-          std::make_shared<DHTBucket>(localNode_))),
-      numBucket_(1),
-      taskQueue_{nullptr},
-      taskFactory_{nullptr}
-{
-}
-    
-      void setCommonProperty(const std::shared_ptr<DHTAbstractTask>& task);
-    
-    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
-                                           const std::string& ipaddr,
-                                           uint16_t port) const
-{
-  return generateToken(infoHash, ipaddr, port, secret_[0]);
-}
-    
-        const std::string& getGoodAddr() const;
-    
-    #ifndef V8_BUILTINS_BUILTINS_ASYNC_GEN_H_
-#define V8_BUILTINS_BUILTINS_ASYNC_GEN_H_
-    
-      auto if_throw_undefined = [=](Node* const native_context, Node* const promise,
-                                Label* if_exception) { Goto(if_exception); };
-    
-    #include 'src/code-stub-assembler.h'
-    
-        BIND(&if_numisheapnumber);
-    {
-      // Check if {num} contains a NaN.
-      Node* num_value = LoadHeapNumberValue(num);
-      BranchIfFloat64IsNaN(num_value, &return_true, &return_false);
+    // Tests factorial of negative numbers.
+TEST(FactorialTest, Negative) {
+  // This test is named 'Negative', and belongs to the 'FactorialTest'
+  // test case.
+  EXPECT_EQ(1, Factorial(-5));
+  EXPECT_EQ(1, Factorial(-1));
+  EXPECT_GT(Factorial(-10), 0);
     }
     
-        // Check if the optimized code is marked for deopt. If it is, call the
-    // runtime to clear it.
-    Label found_deoptimized_code(this);
-    TNode<CodeDataContainer> code_data_container =
-        CAST(LoadObjectField(optimized_code, Code::kCodeDataContainerOffset));
+    NS_CC_END
+
     
+    /**
+@brief The delegate class for ActionTween.
+@details If you want to use ActionTween on a node.
+        You should implement the node follow these steps:
+        1. The node should be inherit from ActionTweenDelegate.
+        2. Override the virtual method updateTweenAction in the node.
     
-    {  Node* value = ToThisValue(context, receiver, PrimitiveType::kSymbol,
-                            'Symbol.prototype.description');
-  Node* result = LoadObjectField(value, Symbol::kNameOffset);
-  Return(result);
-}
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+      if (!is_started_) {
+    AERROR << 'Esd can client has not been initiated! Please init first!';
+    return ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED;
+  }
+  for (size_t i = 0; i < frames.size() && i < MAX_CAN_SEND_FRAME_LEN; ++i) {
+    send_frames_[i].id = frames[i].id;
+    send_frames_[i].len = frames[i].len;
+    std::memcpy(send_frames_[i].data, frames[i].data, frames[i].len);
+  }
+    
+    #include <sstream>
+#include <string>
+#include <vector>
+    
+    #include 'modules/drivers/canbus/can_comm/can_sender.h'
+    
+      /**
+   * @brief Set the bit on a specified position to zero.
+   * @param pos The position of the bit to be set to zero.
+   */
+  void set_bit_0(const int32_t pos);
+    
+    const int32_t CANBUS_MESSAGE_LENGTH = 8;  // according to ISO-11891-1
+const int32_t MAX_CAN_PORT = 3;
+    
+    #include 'modules/drivers/canbus/sensor_gflags.h'
+    
+    #include 'gflags/gflags.h'
