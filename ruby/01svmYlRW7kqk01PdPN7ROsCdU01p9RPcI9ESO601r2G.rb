e8f996bb439c22259d3c924a4f23570d5ea14d37
SUNsung <weatherius@gmail.com>
2018-10-16 22:32:09 +0300

    Rewrite [01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb] File in: 2018-10-16 22:32:08

--- ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
+++ ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
@@ -1,26 +1,54 @@
 
         
-                inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
-        pseudocommand = 'git log --pretty=\'%B\' #{inner_command.shellescape}...HEAD --merges'
-        expect(result).to eq(pseudocommand)
+                  @pipeline = @project.pipelines.latest_successful_for(@ref)
+        end
+    
+          def to_asciidoc
+        'image:#{image_url}[link=\'#{link_url}\',title=\'#{title}\']'
       end
     
-          it 'generates the correct git command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          git_commit(path: './fastlane/README.md', message: 'message')
-        end').runner.execute(:test)
-    
-      expect(Open3).to receive(:popen2e).with(*command).and_yield(mock_input, mock_output, mock_thread)
+            def template
+          @template ||= Pipeline::Template.new(self)
+        end
+      end
+    end
+  end
 end
 
     
-            keychain = 'keychain with spaces.keychain'
-        cmd = %r{curl -f -o (([A-Z]\:)?\/.+) https://developer\.apple\.com/certificationauthority/AppleWWDRCA.cer && security import \1 -k #{Regexp.escape(keychain.shellescape)}}
-        require 'open3'
-    
-    # test monkey patched method on both (simulated) OSes
-describe 'monkey patch of String.shellescape (via CrossplatformShellwords)' do
-  describe 'on Windows' do
-    before(:each) do
-      allow(FastlaneCore::Helper).to receive(:windows?).and_return(true)
+        def check_confirmation
+      if @user.confirmed?
+        flash[:error] = I18n.t('admin.accounts.resend_confirmation.already_confirmed')
+        redirect_to admin_accounts_path
+      end
     end
+  end
+end
+
+    
+        def ordered_instances
+      paginated_instances.map { |account| Instance.new(account) }
+    end
+    
+      def parse(*paths)
+    paths.flatten
+         .compact
+         .flat_map { |p| Pathname.new(p).to_path.split(File::PATH_SEPARATOR) }
+         .uniq
+  end
+end
+
+    
+      class Mention < ApplicationRecord
+  end
+    
+      class SendPrivate < Base
+    def perform(*_args)
+      # don't federate in cucumber
+    end
+  end
+    
+          get :index, params: {conversation_id: @conv1.id}
+      save_fixture(html_for('body'), 'conversations_read')
+    end
+  end
