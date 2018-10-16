f9d9da35215219a830e759d0aadc91abe32b5757
SUNsung <weatherius@gmail.com>
2018-10-16 19:37:29 +0300

    Rewrite [15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb] File in: 2018-10-16 19:37:29

--- ruby/15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb
+++ ruby/15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb
@@ -1,164 +1,76 @@
 
         
-        def gemspec_file
-  '#{name}.gemspec'
-end
+            # The stdlib recorded in the install receipt is used during dependency
+    # compatibility checks, so we only care about the stdlib that libraries
+    # link against.
+    keg.detect_cxx_stdlibs(:skip_executables => true)
+  end
     
-    #
-    
-    #
-    
-      all_files       = `git ls-files -z`.split('\x0')
-  s.files         = all_files.grep(%r!^(exe|lib|rubocop)/|^.rubocop.yml$!)
-  s.executables   = all_files.grep(%r!^exe/!) { |f| File.basename(f) }
-  s.bindir        = 'exe'
-  s.require_paths = ['lib']
-    
-              # WebSockets requests will have a Connection: Upgrade header
-          if parser.http_method != 'GET' || parser.upgrade?
-            super
-          elsif parser.request_url =~ %r!^\/livereload.js!
-            headers = [
-              'HTTP/1.1 200 OK',
-              'Content-Type: application/javascript',
-              'Content-Length: #{reload_size}',
-              '',
-              '',
-            ].join('\r\n')
-            send_data(headers)
-    
-    class DeviseCreateUsers < ActiveRecord::Migration
-  def change
-    create_table(:users) do |t|
-      t.string :email,              null: false
-      t.string :encrypted_password, null: true
-      t.timestamps null: false
-    end
-    
-          # Sign in a user bypassing the warden callbacks and stores the user
-      # straight in session. This option is useful in cases the user is already
-      # signed in, but we want to refresh the credentials in session.
-      #
-      # Examples:
-      #
-      #   bypass_sign_in @user, scope: :user
-      #   bypass_sign_in @user
-      def bypass_sign_in(resource, scope: nil)
-        scope ||= Devise::Mapping.find_scope!(resource)
-        expire_data_after_sign_in!
-        warden.session_serializer.store(resource, scope)
+        def self.rm_DS_Store
+      paths = Queue.new
+      %w[Cellar Frameworks Library bin etc include lib opt sbin share var].
+        map { |p| HOMEBREW_PREFIX/p }.each { |p| paths << p if p.exist? }
+      workers = (0...Hardware::CPU.cores).map do
+        Thread.new do
+          begin
+            while p = paths.pop(true)
+              quiet_system 'find', p, '-name', '.DS_Store', '-delete'
+            end
+          rescue ThreadError # ignore empty queue error
+          end
+        end
       end
-    
-    module Devise
-  module Controllers
-    # Provide the ability to store a location.
-    # Used to redirect back to a desired path after sign in.
-    # Included by default in all controllers.
-    module StoreLocation
-      # Returns and delete (if it's navigational format) the url stored in the session for
-      # the given scope. Useful for giving redirect backs after sign up:
-      #
-      # Example:
-      #
-      #   redirect_to stored_location_for(:user) || root_path
-      #
-      def stored_location_for(resource_or_scope)
-        session_key = stored_location_key_for(resource_or_scope)
-    
-        # Choose whether we should respond in an HTTP authentication fashion,
-    # including 401 and optional headers.
-    #
-    # This method allows the user to explicitly disable HTTP authentication
-    # on AJAX requests in case they want to redirect on failures instead of
-    # handling the errors on their own. This is useful in case your AJAX API
-    # is the same as your public API and uses a format like JSON (so you
-    # cannot mark JSON as a navigational format).
-    def http_auth?
-      if request.xhr?
-        Devise.http_authenticatable_on_xhr
-      else
-        !(request_format && is_navigational_format?)
-      end
+      workers.map(&:join)
     end
     
-      before_action :set_filters, only: :index
-  before_action :set_filter, only: [:edit, :update, :destroy]
+        @@remote_tap_formulae ||= Hash.new do |cache, key|
+      user, repo = key.split('/', 2)
+      tree = {}
     
-      private
-    
-      before_action :set_account
-  before_action :set_statuses
-    
-        def show
-      authorize @user, :change_email?
-    end
-    
-        def copy
-      authorize @custom_emoji, :copy?
-    
-        def set_domain_block
-      @domain_block = DomainBlock.find(params[:id])
-    end
-    
-          if @email_domain_block.save
-        log_action :create, @email_domain_block
-        redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.created_msg')
-      else
-        render :new
-      end
-    end
-    
-      private
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
+      def python(_options = {}, &block)
+    opoo 'Formula#python is deprecated and will go away shortly.'
+    block.call if block_given?
+    PythonRequirement.new
   end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
-  end
-end
-    
-      # create a new @me user, if not present, and log in using the
-  # integration_sessions controller (automatic)
-  def automatic_login
-    @me ||= FactoryGirl.create(:user_with_aspect, :getting_started => false)
-    visit(new_integration_sessions_path(user_id: @me.id))
-    click_button 'Login'
-  end
-    
-        sign_in(alice, scope: :user)
-  end
-    
-        # we assume that the first file that requires 'sinatra' is the
-    # app_file. all other path related options are calculated based
-    # on this path by default.
-    set :app_file, caller_files.first || $0
-    
-    begin
-  require 'bundler'
-  Bundler::GemHelper.install_tasks
-rescue LoadError => e
-  $stderr.puts e
-end
-    
-          def escape_string(str)
-        str = @escaper.escape_url(str)        if @url
-        str = @escaper.escape_html(str)       if @html
-        str = @escaper.escape_javascript(str) if @javascript
-        str
-      end
-    end
-  end
+  alias_method :python2, :python
+  alias_method :python3, :python
 end
 
     
-      it 'accepts post requests with masked X-CSRF-Token header' do
-    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => masked_token)
-    expect(last_response).to be_ok
-  end
+    desc 'Run all tests'
+task :test do
+  test_cases = [
+    {
+      'env'   => {'MATHN' => 'true'},
+      'tasks' => ['test:ruby', 'test:spec', :rubocop]
+    },
+    {
+      'env'   => {'MATHN' => 'false'},
+      'tasks' => ['test:ruby']
+    }
+  ]
     
-      describe '#referrer' do
-    it 'Reads referrer from Referer header' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
-      expect(subject.referrer(env)).to eq('bar.com')
+        desc 'Run the unit specs'
+    task :unit => 'fixture_tarballs:unpack' do
+      sh unit_specs_command
     end
+    
+        # From asking people, it seems MacPorts does not have a `prefix` command, like
+    # Homebrew does, so make an educated guess:
+    if port_prefix = prefix_from_bin('port')
+      prefixes << port_prefix
+    end
+    
+        # Checks that the podfile exists.
+    #
+    # @raise  If the podfile does not exists.
+    #
+    # @return [void]
+    #
+    def verify_podfile_exists!
+      unless config.podfile
+        raise Informative, 'No `Podfile' found in the project directory.'
+      end
+    end
+    
+          private
