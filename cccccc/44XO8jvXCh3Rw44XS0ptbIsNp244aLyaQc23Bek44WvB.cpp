667c5a6ce56aad53b5d1c06f792b15e68c535385
SUNsung <weatherius@gmail.com>
2018-10-16 07:19:52 +0300

    Rewrite [44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp] File in: 2018-10-16 07:19:52

--- cccccc/44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp
+++ cccccc/44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp
@@ -1,343 +1,108 @@
 
         
-          std::vector<string> output;
-  string input_tensors_needed_out;
-  tensorflow::Status status = RunCppShapeInferenceImpl(
-      graph_def_version, serialized_node_def, input_serialized_shapes,
-      input_constant_tensor_values_v, input_constant_tensor_as_shape_values,
-      &output, &input_tensors_needed_out);
-    
-    #include 'tensorflow/core/platform/env.h'
-#include 'tensorflow/core/platform/null_file_system.h'
-    
-    // Creates a tensor in 'ret' from the input Ndarray.
-Status NdarrayToTensor(PyObject* obj, Tensor* ret);
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    namespace tensorflow {
-namespace detail {
-    }
+        namespace atom {
     }
     
-    // Returns the kernel class name required to execute <node_def> on the device
-// type of <node_def.device>, or an empty string if the kernel class is not
-// found or the device name is invalid.
-string TryFindKernelClass(const string& serialized_node_def);
+    AutoUpdater::AutoUpdater(v8::Isolate* isolate) {
+  auto_updater::AutoUpdater::SetDelegate(this);
+  Init(isolate);
+}
+    
+    namespace api {
+    }
+    
+    namespace atom {
+    }
     
     
-    {
-    {}  // namespace cuda
-}  // namespace stream_executor
+    {  DISALLOW_COPY_AND_ASSIGN(PowerSaveBlocker);
+};
     
-    public:
-    ~MacDockIconHandler();
-    
-        QString getURI();
-    
-    #include <QDialog>
-    
-    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            rcx:rbx = d
- *            r10-r14 = a0-a4
- *            r15     = M (0xfffffffffffff)
- *            rdi     = r
- *            rsi     = a / t?
+    /*!
+ * \brief Registry entry for predictor.
  */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
-    'movq $0xfffffffffffff,%%r15\n'
+struct PredictorReg
+    : public dmlc::FunctionRegEntryBase<PredictorReg,
+                                        std::function<Predictor*()>> {};
+    
+      size_t PeekRead(void* dptr, size_t size) {
+    size_t nbuffer = buffer_.length() - buffer_ptr_;
+    if (nbuffer < size) {
+      buffer_ = buffer_.substr(buffer_ptr_, buffer_.length());
+      buffer_ptr_ = 0;
+      buffer_.resize(size);
+      size_t nadd = strm_->Read(dmlc::BeginPtr(buffer_) + nbuffer, size - nbuffer);
+      buffer_.resize(nbuffer + nadd);
+      std::memcpy(dptr, dmlc::BeginPtr(buffer_), buffer_.length());
+      return buffer_.length();
+    } else {
+      std::memcpy(dptr, dmlc::BeginPtr(buffer_) + buffer_ptr_, size);
+      return size;
+    }
+  }
+    
+    namespace xgboost {
     }
     
-    
-    {    rng->retry = 1;
-}
-    
-        s_one[31] = 1;
-    /* Check against pubkey creation when the basepoint is the generator */
-    for (i = 0; i < 100; ++i) {
-        secp256k1_sha256_t sha;
-        unsigned char s_b32[32];
-        unsigned char output_ecdh[32];
-        unsigned char output_ser[32];
-        unsigned char point_ser[33];
-        size_t point_ser_len = sizeof(point_ser);
-        secp256k1_scalar s;
-    }
-    
-    void test_ecdsa_recovery_api(void) {
-    /* Setup contexts that just count errors */
-    secp256k1_context *none = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
-    secp256k1_context *sign = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
-    secp256k1_context *vrfy = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
-    secp256k1_context *both = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
-    secp256k1_pubkey pubkey;
-    secp256k1_pubkey recpubkey;
-    secp256k1_ecdsa_signature normal_sig;
-    secp256k1_ecdsa_recoverable_signature recsig;
-    unsigned char privkey[32] = { 1 };
-    unsigned char message[32] = { 2 };
-    int32_t ecount = 0;
-    int recid = 0;
-    unsigned char sig[74];
-    unsigned char zero_privkey[32] = { 0 };
-    unsigned char over_privkey[32] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-    }
-    
-    namespace {
-    }
-    
-        void operator() (const typename VecTraits<f32>::vec64 & v_src0,
-                     const typename VecTraits<f32>::vec64 & v_src1,
-                     typename VecTraits<f32>::vec64 & v_dst) const
+    bool Shaky3D::initWithDuration(float duration, const Size& gridSize, int range, bool shakeZ)
+{
+    if (Grid3DAction::initWithDuration(duration, gridSize))
     {
-        float32x2_t vs1 = vmla_f32(vget_low(vgamma), v_src0, vget_low(valpha));
-        v_dst = vmla_f32(vs1, v_src1, vget_low(vbeta));
-    }
-    
-    #define YCRCB_CONSTS                                                        \
-    register int16x4_t vcYR  asm ('d31') = vmov_n_s16(4899);                \
-    register int16x4_t vcYG  asm ('d30') = vmov_n_s16(9617);                \
-    register int16x4_t vcYB  asm ('d29') = vmov_n_s16(1868);                \
-    register int16x4_t vcCrG asm ('d28') = vmov_n_s16(6860);                \
-    register int16x4_t vcCrB asm ('d27') = vmov_n_s16(1332);                \
-    register int16x4_t vcCbR asm ('d26') = vmov_n_s16(2765);                \
-    register int16x4_t vcCbG asm ('d25') = vmov_n_s16(5427);
-    
-        ptrdiff_t width = (ptrdiff_t)size.width, height = (ptrdiff_t)size.height;
-    static const vshrq_s32_func vshrq_s32_a[33] =
-    {
-        vshrq_s32<0>,
-        vshrq_s32<1>,
-        vshrq_s32<2>,
-        vshrq_s32<3>,
-        vshrq_s32<4>,
-        vshrq_s32<5>,
-        vshrq_s32<6>,
-        vshrq_s32<7>,
-        vshrq_s32<8>,
-        vshrq_s32<9>,
-        vshrq_s32<10>,
-        vshrq_s32<11>,
-        vshrq_s32<12>,
-        vshrq_s32<13>,
-        vshrq_s32<14>,
-        vshrq_s32<15>,
-        vshrq_s32<16>,
-        vshrq_s32<17>,
-        vshrq_s32<18>,
-        vshrq_s32<19>,
-        vshrq_s32<20>,
-        vshrq_s32<21>,
-        vshrq_s32<22>,
-        vshrq_s32<23>,
-        vshrq_s32<24>,
-        vshrq_s32<25>,
-        vshrq_s32<26>,
-        vshrq_s32<27>,
-        vshrq_s32<28>,
-        vshrq_s32<29>,
-        vshrq_s32<30>,
-        vshrq_s32<31>,
-        vshrq_s32<32>
-    };
-    vshrq_s32_func vshrq_s32_p = vshrq_s32_a[scale];
-    
-            result += (s[0] += s[1]);
-        if (s[0] < 0 || result < 0)//case of overflow ~ 16GB of non-zeros...
-        {
-            return 0x7fFFffFF;
-        }
-    
-    
-    {                uint32x4_t v_mask = vceqq_f32(v_src1,v_zero);
-                vst1q_f32(dst + j, vreinterpretq_f32_u32(vbicq_u32(
-                                   vreinterpretq_u32_f32(vmulq_f32(v_src0, internal::vrecpq_f32(v_src1))), v_mask)));
-            }
-    
-    
-    {        u32 buf[8];
-        vst1_u32(buf, vget_low_u32(el8shr01l));
-        vst1_u32(buf+2, el2l);
-        vst1_u32(buf+4, el2hl);
-        vst1_u32(buf+6, el2hh);
-        for(u32 k=0; k < 8; k++)
-            sqsum[j+k] = prev + buf[k];
-        prev += buf[7];
-    }
-    
-    TYPED_TEST_P(FooTest, HasPropertyA) { ... }
-    
-    // The following family of struct and struct templates are used to
-// represent type lists.  In particular, TypesN<T1, T2, ..., TN>
-// represents a type list with N types (T1, T2, ..., and TN) in it.
-// Except for Types0, every struct in the family has two member types:
-// Head for the first type in the list, and Tail for the rest of the
-// list.
-    
-      bool check_for_leaks = false;
-  if (argc > 1 && strcmp(argv[1], '--check_for_leaks') == 0 )
-    check_for_leaks = true;
-  else
-    printf('%s\n', 'Run this program with --check_for_leaks to enable '
-           'custom leak checking in the tests.');
-    
-    /*
- * If Trace::hhbbc_time >= 1, print some stats about the program to a
- * temporary file.  If it's greater than or equal to 2, also dump it
- * to stdout.
- */
-void print_stats(const Index&, const php::Program&);
-    
-      if (comma != data) {
-    // we have meta
-    ssize_t meta_len = comma - data;
-    data_len -= meta_len;
-    char* semi = (char*)memchr(data, ';', meta_len);
-    char* slash = (char*)memchr(data, '/', meta_len);
-    }
-    
-    #include 'hphp/runtime/base/thread-info.h'
-    
-      std::set<string> todelete;
-  while ((e = readdir(ddest))) {
-    if (strcmp(e->d_name, '.') == 0 ||
-        strcmp(e->d_name, '..') == 0) {
-      continue;
-    }
-    string fsrc = src + e->d_name;
-    string fdest = dest + e->d_name;
-    }
-    
-    
-    {    if (s.ok()) {
-      // Verify that the table is usable
-      Iterator* it = table_cache->NewIterator(ReadOptions(),
-                                              meta->number,
-                                              meta->file_size);
-      s = it->status();
-      delete it;
-    }
-  }
-    
-      void FinishedSingleOp() {
-    if (FLAGS_histogram) {
-      double now = g_env->NowMicros();
-      double micros = now - last_op_finish_;
-      hist_.Add(micros);
-      if (micros > 20000) {
-        fprintf(stderr, 'long op: %.1f micros%30s\r', micros, '');
-        fflush(stderr);
-      }
-      last_op_finish_ = now;
+        _randrange = range;
+        _shakeZ = shakeZ;
     }
     }
     
-        Slice key = input->key();
-    if (compact->compaction->ShouldStopBefore(key) &&
-        compact->builder != nullptr) {
-      status = FinishCompactionOutputFile(compact, input);
-      if (!status.ok()) {
-        break;
-      }
-    }
-    
-    namespace leveldb {
-    }
-    
-      int CountFiles() {
-    std::vector<std::string> files;
-    env_->GetChildren(dbname_, &files);
-    return static_cast<int>(files.size());
-  }
-    
-    bool HandleDumpCommand(Env* env, char** files, int num) {
-  StdoutPrinter printer;
-  bool ok = true;
-  for (int i = 0; i < num; i++) {
-    Status s = DumpFile(env, files[i], &printer);
-    if (!s.ok()) {
-      fprintf(stderr, '%s\n', s.ToString().c_str());
-      ok = false;
-    }
-  }
-  return ok;
-}
-    
-    #endif  // STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-
-    
-        // Parse the header
-    const char* header = buffer_.data();
-    const uint32_t a = static_cast<uint32_t>(header[4]) & 0xff;
-    const uint32_t b = static_cast<uint32_t>(header[5]) & 0xff;
-    const unsigned int type = header[6];
-    const uint32_t length = a | (b << 8);
-    if (kHeaderSize + length > buffer_.size()) {
-      size_t drop_size = buffer_.size();
-      buffer_.clear();
-      if (!eof_) {
-        ReportCorruption(drop_size, 'bad record length');
-        return kBadRecord;
-      }
-      // If the end of the file has been reached without reading |length| bytes
-      // of payload, assume the writer died in the middle of writing the record.
-      // Don't report a corruption.
-      return kEof;
-    }
-    
-    TEST(LogTest, ReadFourthMiddleBlock) {
-  CheckInitialOffsetRecord(log::kBlockSize + 1, 3);
+    bool AnimationCache::init()
+{
+    return true;
 }
     
     
-    {  // Write the header and the payload
-  Status s = dest_->Append(Slice(buf, kHeaderSize));
-  if (s.ok()) {
-    s = dest_->Append(Slice(ptr, n));
-    if (s.ok()) {
-      s = dest_->Flush();
-    }
+    {  // Synchronous transmission of CAN messages
+  int32_t ret = canWrite(dev_handler_, send_frames_, frame_num, nullptr);
+  if (ret != NTCAN_SUCCESS) {
+    AERROR << 'send message failed, error code: ' << ret << ', '
+           << GetErrorString(ret);
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
   }
-  block_offset_ += kHeaderSize + n;
-  return s;
+  return ErrorCode::OK;
 }
     
-    static void RunConcurrent(int run) {
-  const int seed = test::RandomSeed() + (run * 100);
-  Random rnd(seed);
-  const int N = 1000;
-  const int kSize = 1000;
-  for (int i = 0; i < N; i++) {
-    if ((i % 100) == 0) {
-      fprintf(stderr, 'Run %d of %d\n', i, N);
+      /**
+   * @brief Start the ESD CAN client.
+   * @return The status of the start action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  apollo::common::ErrorCode Start() override;
+    
+      ProtocolData<::apollo::canbus::ChassisDetail> mpd;
+  SenderMessage<::apollo::canbus::ChassisDetail> msg(1, &mpd);
+  EXPECT_FALSE(sender.NeedSend(msg, 1));
+  EXPECT_EQ(msg.message_id(), 1);
+  int32_t period = msg.curr_period();
+  msg.UpdateCurrPeriod(-50);
+  EXPECT_EQ(msg.curr_period(), period + 50);
+  EXPECT_EQ(msg.CanFrame().id, 1);
+    
+    TEST(MessageManagerTest, GetMutableProtocolDataById) {
+  uint8_t mock_data = 1;
+  MockMessageManager manager;
+  manager.Parse(MockProtocolData::ID, &mock_data, 8);
+  manager.ResetSendMessages();
+  EXPECT_TRUE(manager.GetMutableProtocolDataById(MockProtocolData::ID) !=
+              nullptr);
     }
-    TestState state(seed + 1);
-    Env::Default()->Schedule(ConcurrentReader, &state);
-    state.Wait(TestState::RUNNING);
-    for (int i = 0; i < kSize; i++) {
-      state.t_.WriteStep(&rnd);
-    }
-    state.quit_flag_.Release_Store(&state);  // Any non-null arg will do
-    state.Wait(TestState::DONE);
-  }
+    
+      /*
+   * @brief get the length of protocol data. The length is usually 8.
+   * @return the length of protocol data.
+   */
+  virtual int32_t GetLength() const;
+    
+    TEST(ProtocolDataTest, CheckSum) {
+  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
+  const uint8_t result =
+      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
+  EXPECT_EQ(0xE7, result);
 }
