369d3574301a3c6b410f7b0e73a8c78a7ddb8327
SUNsung <weatherius@gmail.com>
2018-10-17 09:40:06 +0300

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2018-10-17 09:40:06

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,76 +1,94 @@
 
         
-            plt.figure('scikit-learn GLM benchmark results')
-    plt.xlabel('Dimensions')
-    plt.ylabel('Time (s)')
-    plt.plot(dimensions, time_ridge, color='r')
-    plt.plot(dimensions, time_ols, color='g')
-    plt.plot(dimensions, time_lasso, color='b')
+            def describe_stack_events(self, stack_name):
+        try:
+            func = partial(self.client.describe_stack_events, StackName=stack_name)
+            return self.paginated_response(func, 'StackEvents')
+        except Exception as e:
+            self.module.fail_json(msg='Error describing stack events - ' + str(e), exception=traceback.format_exc())
     
-        plt.clf()
-    xx = range(0, n * step, step)
-    plt.title('Lasso regression on sample dataset (%d features)' % n_features)
-    plt.plot(xx, scikit_results, 'b-', label='scikit-learn')
-    plt.plot(xx, glmnet_results, 'r-', label='glmnet')
-    plt.legend()
-    plt.xlabel('number of samples to classify')
-    plt.ylabel('Time (s)')
-    plt.show()
+        def _rule_matches_aws(self):
+        '''Checks if the local rule data matches AWS'''
+        aws_rule_data = self.rule.describe()
     
-        max_it = len(samples_range) * len(features_range)
-    for i_s, n_samples in enumerate(samples_range):
-        for i_f, n_features in enumerate(features_range):
-            it += 1
-            n_informative = n_features / 10
-            print('====================')
-            print('Iteration %03d of %03d' % (it, max_it))
-            print('====================')
-            # dataset_kwargs = {
-            #     'n_train_samples': n_samples,
-            #     'n_test_samples': 2,
-            #     'n_features': n_features,
-            #     'n_informative': n_informative,
-            #     'effective_rank': min(n_samples, n_features) / 10,
-            #     #'effective_rank': None,
-            #     'bias': 0.0,
-            # }
-            dataset_kwargs = {
-                'n_samples': 1,
-                'n_components': n_features,
-                'n_features': n_samples,
-                'n_nonzero_coefs': n_informative,
-                'random_state': 0
-            }
-            print('n_samples: %d' % n_samples)
-            print('n_features: %d' % n_features)
-            y, X, _ = make_sparse_coded_signal(**dataset_kwargs)
-            X = np.asfortranarray(X)
+        for index in all_indexes:
+        validate_index(index, module)
     
-            plt.subplot(m, 2, i + 1)
-        plt.plot(list_n_samples, np.sqrt(elnet_results[:, j, 1]),
-                 label='ElasticNet')
-        plt.plot(list_n_samples, np.sqrt(sgd_results[:, j, 1]),
-                 label='SGDRegressor')
-        plt.plot(list_n_samples, np.sqrt(asgd_results[:, j, 1]),
-                 label='A-SGDRegressor')
-        plt.plot(list_n_samples, np.sqrt(ridge_results[:, j, 1]),
-                 label='Ridge')
-        plt.legend(prop={'size': 10})
-        plt.xlabel('n_train')
-        plt.ylabel('Time [sec]')
-        plt.title('Training time - %d features' % list_n_features[j])
-        i += 1
+        addresses = camel_dict_to_snake_dict(response)['addresses']
+    for address in addresses:
+        if 'tags' in address:
+            address['tags'] = boto3_tag_list_to_ansible_dict(address['tags'])
+    return addresses
     
-    In the second benchmark, we increase the number of dimensions of the
-training set, classify a sample and plot the time taken as a function
-of the number of dimensions.
+        module = AnsibleModule(
+        argument_spec=argument_spec,
+    )
+    
+    - name: create a VPC with dedicated tenancy and a couple of tags
+  ec2_vpc_net:
+    name: Module_dev2
+    cidr_block: 10.10.0.0/16
+    region: us-east-1
+    tags:
+      module: ec2_vpc_net
+      this: works
+    tenancy: dedicated
 '''
-import numpy as np
-import matplotlib.pyplot as plt
-import gc
-from datetime import datetime
     
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
+    - name: set-policy as string
+  ecs_ecr:
+    name: needs-policy-string
+    policy: '{{ lookup('template', 'policy.json.j2') }}'
+    
+    
+def main():
+    argument_spec = ec2_argument_spec()
+    argument_spec.update(
+        dict(
+            name=dict(required=True, type='str'),
+            state=dict(required=True, type='str', choices=['present', 'absent', 'copy']),
+            replication_id=dict(type='str'),
+            cluster_id=dict(type='str'),
+            target=dict(type='str'),
+            bucket=dict(type='str'),
+        )
+    )
+    
+        if state == 'present':
+        for required in ['name', 'description', 'subnets']:
+            if not module.params.get(required):
+                module.fail_json(msg=str('Parameter %s required for state='present'' % required))
+    else:
+        for not_allowed in ['description', 'subnets']:
+            if module.params.get(not_allowed):
+                module.fail_json(msg=str('Parameter %s not allowed for state='absent'' % not_allowed))
+    
+        cert_name = module.params.get('name')
+    results = get_server_certs(iam, cert_name)
+    module.exit_json(results=results)
+    
+        cognito = _cognito(cloud)
+    # Workaround for bug in Warrant. PR with fix:
+    # https://github.com/capless/warrant/pull/82
+    cognito.add_base_attributes()
+    try:
+        cognito.register(email, password)
+    except ClientError as err:
+        raise _map_aws_exception(err)
+    
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/device_tracker.actiontec/
+'''
+import logging
+import re
+import telnetlib
+from collections import namedtuple
+import voluptuous as vol
+    
+            self.last_results = {}
+        self.refresh_token()
+        self.mac2name = None
+        self.success_init = self.token is not None
+    
+        dispatcher_connect(hass, SIGNAL_VEHICLE_SEEN, see_vehicle)
+    dispatcher_send(hass, SIGNAL_VEHICLE_SEEN, vehicle)
