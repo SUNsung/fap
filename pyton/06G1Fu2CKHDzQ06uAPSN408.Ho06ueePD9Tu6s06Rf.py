44079fbc2c4d219259c8265256f18c631a90a15d
SUNsung <weatherius@gmail.com>
2018-10-17 13:44:50 +0300

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2018-10-17 13:44:49

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,315 +1,184 @@
 
         
-            if RESULT and ('info_dict' not in test or 'age_limit' not in test['info_dict'] or
-                   test['info_dict']['age_limit'] != 18):
-        print('\nPotential missing age_limit check: {0}'.format(test['name']))
+            def _dispatch_call(self, call, employees):
+        for employee in employees:
+            if employee.call is None:
+                employee.take_call(call)
+                return employee
+        return None
     
-    header = oldreadme[:oldreadme.index('# OPTIONS')]
-footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
+        def extract_year_month(self, timestamp):
+        '''Return the year and month portions of the timestamp.'''
+        ...
     
-    
-total_bytes = 0
-    
-            f = match_filter_func('uploader != '變態妍字幕版 太妍 тест'')
-        res = get_videos(f)
-        self.assertEqual(res, ['2'])
-    
-        def test_pbs(self):
-        # https://github.com/rg3/youtube-dl/issues/2350
-        self.assertMatch('http://video.pbs.org/viralplayer/2365173446/', ['pbs'])
-        self.assertMatch('http://video.pbs.org/widget/partnerplayer/980042464/', ['pbs'])
+        def mapper_sort(self, key, value):
+        '''Construct key to ensure proper sorting.
     
     
-def get_db():
-    '''Connect to the application's configured database. The connection
-    is unique for each request and will be reused if this is called
-    again.
-    '''
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-    
-        def reraise(tp, value, tb=None):
-        if value.__traceback__ is not tb:
-            raise value.with_traceback(tb)
-        raise value
+class Cache(object):
     
     
-def _find_app():
-    top = _app_ctx_stack.top
-    if top is None:
-        raise RuntimeError(_app_ctx_err_msg)
-    return top.app
+class PathBasedModel(object):
+  '''The LexNET path-based model for classifying semantic relations.'''
     
-        By default it will try to guess the mimetype for you, but you can
-    also explicitly provide one.  For extra security you probably want
-    to send certain files as attachment (HTML for instance).  The mimetype
-    guessing requires a `filename` or an `attachment_filename` to be
-    provided.
-    
-        def _get_source_explained(self, environment, template):
-        attempts = []
-        trv = None
+    if S < N:
+  # Note that this isn't necessary for this synthetic example, but
+  # it's useful to see how the input factor matrices were initialized
+  # for actual neurophysiology data.
+  datasets = add_alignment_projections(datasets, npcs=FLAGS.npcs)
     
     
-@pytest.fixture(autouse=True)
-def shell_pid(mocker):
-    return mocker.patch('thefuck.entrypoints.not_configured._get_shell_pid',
-                        new_callable=MagicMock)
+rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
+u_rng = np.random.RandomState(seed=FLAGS.synth_data_seed+1)
+T = FLAGS.T
+C = FLAGS.C
+N = FLAGS.N  # must be same N as in trained model (provided example is N = 50)
+nreplications = FLAGS.nreplications
+E = nreplications * C  # total number of trials
+train_percentage = FLAGS.train_percentage
+ntimesteps = int(T / FLAGS.dt)
+batch_size = 1  # gives one example per ntrial
     
-        proc.sendline(u'fuck')
-    assert proc.expect([TIMEOUT, u'echo test'])
-    assert proc.expect([TIMEOUT, u'test'])
+      _partial_score(scoring1, mask1)
+  _partial_score(scoring2, mask2)
     
     
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
-    proc.sendline(u'fish')
-    return proc
+def hash_function(input_tuple):
+  '''Hash function for a tuple.'''
+  return hash(input_tuple)
     
-        assert get_new_command(Command('brew install aa',
-                                   brew_no_available_formula))\
-        != 'brew install aha'
+        if model.__class__.__name__ == 'Sequential':
+        sequential_like = True
+    elif not model._is_graph_network:
+        # We treat subclassed models as a simple sequence of layers,
+        # for logging purposes.
+        sequential_like = True
+    else:
+        sequential_like = True
+        nodes_by_depth = model._nodes_by_depth.values()
+        nodes = []
+        for v in nodes_by_depth:
+            if (len(v) > 1) or (len(v) == 1 and len(v[0].inbound_layers) > 1):
+                # if the model has multiple nodes
+                # or if the nodes have multiple inbound_layers
+                # the model is no longer sequential
+                sequential_like = False
+                break
+            nodes += v
+        if sequential_like:
+            # search for shared layers
+            for layer in model.layers:
+                flag = False
+                for node in layer._inbound_nodes:
+                    if node in nodes:
+                        if flag:
+                            sequential_like = False
+                            break
+                        else:
+                            flag = True
+                if not sequential_like:
+                    break
+    
+    
+def _normalize_device_name(name):
+    name = '/' + ':'.join(name.lower().replace('/', '').split(':')[-2:])
+    return name
+    
+            model.fit_generator(generate_arrays_from_file('/my_file.txt'),
+                            steps_per_epoch=10000, epochs=10)
+        ```
+        '''
+        return training_generator.fit_generator(
+            self, generator,
+            steps_per_epoch=steps_per_epoch,
+            epochs=epochs,
+            verbose=verbose,
+            callbacks=callbacks,
+            validation_data=validation_data,
+            validation_steps=validation_steps,
+            class_weight=class_weight,
+            max_queue_size=max_queue_size,
+            workers=workers,
+            use_multiprocessing=use_multiprocessing,
+            shuffle=shuffle,
+            initial_epoch=initial_epoch)
+    
+            self._layers.pop()
+        self.built = False
+        if not self.layers:
+            self.outputs = None
+            self.inputs = None
+        elif self.outputs:
+            self.layers[-1]._outbound_nodes = []
+            self.outputs = [self.layers[-1].output]
+            self.build()
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+      _assert_rejects( f, 'This is a Taco' )
+  _assert_accepts( f, 'This is a Burrito' )
 
     
     
-no_such_subcommand_old = '''No such subcommand
+def EndsWithPython_Python2Paths_test():
+  python_paths = [
+    'python',
+    'python2',
+    '/usr/bin/python2.7',
+    '/home/user/.pyenv/shims/python2.7',
+    r'C:\Python27\python.exe',
+    '/Contents/MacOS/Python'
+  ]
     
-    # Model parameter
-# ----------------------------------------------------------------------------
-#           |      | 200-epoch | Orig Paper| 200-epoch | Orig Paper| sec/epoch
-# Model     |  n   | ResNet v1 | ResNet v1 | ResNet v2 | ResNet v2 | GTX1080Ti
-#           |v1(v2)| %Accuracy | %Accuracy | %Accuracy | %Accuracy | v1 (v2)
-# ----------------------------------------------------------------------------
-# ResNet20  | 3 (2)| 92.16     | 91.25     | -----     | -----     | 35 (---)
-# ResNet32  | 5(NA)| 92.46     | 92.49     | NA        | NA        | 50 ( NA)
-# ResNet44  | 7(NA)| 92.50     | 92.83     | NA        | NA        | 70 ( NA)
-# ResNet56  | 9 (6)| 92.71     | 93.03     | 93.01     | NA        | 90 (100)
-# ResNet110 |18(12)| 92.65     | 93.39+-.16| 93.15     | 93.63     | 165(180)
-# ResNet164 |27(18)| -----     | 94.07     | -----     | 94.54     | ---(---)
-# ResNet1001| (111)| -----     | 92.39     | -----     | 95.08+-.14| ---(---)
-# ---------------------------------------------------------------------------
-n = 3
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
+    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+    vol.Optional(CONF_DISCOVERY, default=True): cv.boolean,
+    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
+    vol.Optional(CONF_IGNORED_CLICK_TYPES):
+        vol.All(cv.ensure_list, [vol.In(CLICK_TYPES)])
+})
     
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    inputs = Input(shape=x.shape)
-    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
-    model = Model(inputs, outputs)
-    assert model.predict(x[np.newaxis])[0].shape == x.shape
+    _LOGGER = logging.getLogger(__name__)
     
+    from aiohttp.hdrs import CONTENT_TYPE
+import requests
+import voluptuous as vol
     
-def test_cce_one_hot():
-    y_a = K.variable(np.random.randint(0, 7, (5, 6)))
-    y_b = K.variable(np.random.random((5, 6, 7)))
-    objective_output = losses.sparse_categorical_crossentropy(y_a, y_b)
-    assert K.eval(objective_output).shape == (5, 6)
+            # Flag C stands for CONNECTED
+        active_clients = [client for client in data.values() if
+                          client['status'].find('C') != -1]
+        self.last_results = active_clients
+        return True
     
-        # Input shape
-        3D tensor with shape `(batch_size, timesteps, input_dim)`.
+    _LOGGER = logging.getLogger(__name__)
     
-            if self.stateful and batch_size:
-            # Check that for stateful networks, number of samples is a multiple
-            # of the static batch size.
-            if x[0].shape[0] % batch_size != 0:
-                raise ValueError('In a stateful network, '
-                                 'you should only pass inputs with '
-                                 'a number of samples that can be '
-                                 'divided by the batch size. Found: ' +
-                                 str(x[0].shape[0]) + ' samples')
-        return x, y, sample_weights
-    
-            return h, [h, c]
-    
-        def __init__(self,
-                 return_sequences=False,
-                 return_state=False,
-                 go_backwards=False,
-                 stateful=False,
-                 **kwargs):
-        if K.backend() != 'tensorflow':
-            raise RuntimeError('CuDNN RNNs are only available '
-                               'with the TensorFlow backend.')
-        super(RNN, self).__init__(**kwargs)
-        self.return_sequences = return_sequences
-        self.return_state = return_state
-        self.go_backwards = go_backwards
-        self.stateful = stateful
-        self.supports_masking = False
-        self.input_spec = [InputSpec(ndim=3)]
-        if hasattr(self.cell.state_size, '__len__'):
-            state_size = self.cell.state_size
-        else:
-            state_size = [self.cell.state_size]
-        self.state_spec = [InputSpec(shape=(None, dim))
-                           for dim in state_size]
-        self.constants_spec = None
-        self._states = None
-        self._num_constants = None
-    
-                    outs = to_list(outs)
-                for l, o in zip(out_labels, outs):
-                    batch_logs[l] = o
-    
-    def _run_print_help(parser, func, *a, **kw):
-    try:
-        func(*a, **kw)
-    except UsageError as e:
-        if str(e):
-            parser.error(str(e))
-        if e.print_help:
-            parser.print_help()
-        sys.exit(2)
-    
-        exitcode = 0
-    
-        def _print_response(self, response, opts):
-        if opts.headers:
-            self._print_headers(response.request.headers, b'>')
-            print('>')
-            self._print_headers(response.headers, b'<')
-        else:
-            self._print_bytes(response.body)
-    
-        return video_dict
-    
-    #----------------------------------------------------------------------
-def dilidili_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
-    global headers
-    re_str = r'http://www.dilidili.com/watch\S+'
-    if re.match(r'http://www.dilidili.wang', url):
-        re_str = r'http://www.dilidili.wang/watch\S+'
-        headers['Referer'] = 'http://www.dilidili.wang/'
-    elif re.match(r'http://www.dilidili.mobi', url):
-        re_str = r'http://www.dilidili.mobi/watch\S+'
-        headers['Referer'] = 'http://www.dilidili.mobi/'
-    
-    # [
-# (
-#   regex_match_url,
-#   remote_api_method,
-#   additional_query_parameter_for_method,
-#   parser_for_additional_parameter,
-#   field_where_photourls_are_saved
-# )
-# ]
-url_patterns = [
-    # www.flickr.com/photos/{username|NSID}/sets|albums/{album-id}
-    (
-        pattern_url_photoset,
-        'photosets.getPhotos',
-        'photoset_id',
-        get_photoset_id,
-        'photoset'
-    ),
-    # www.flickr.com/photos/{username|NSID}/{pageN}?
-    (
-        pattern_url_photostream,
-        # according to flickr api documentation, this method needs to be
-        # authenticated in order to filter photo visible to the calling user
-        # but it seems works fine anonymously as well
-        'people.getPhotos',
-        'user_id',
-        get_NSID,
-        'photos'
-    ),
-    # www.flickr.com/photos/{username|NSID}/galleries/{gallery-id}
-    (
-        pattern_url_gallery,
-        'galleries.getPhotos',
-        'gallery_id',
-        get_gallery_id,
-        'photos'
-    ),
-    # www.flickr.com/groups/{groupname|groupNSID}/
-    (
-        pattern_url_group,
-        'groups.pools.getPhotos',
-        'group_id',
-        get_NSID,
-        'photos'
-    ),
-    # www.flickr.com/photos/{username|NSID}/favorites/*
-    (
-        pattern_url_favorite,
-        'favorites.getList',
-        'user_id',
-        get_NSID,
-        'photos'
-    )
-]
-    
-    
-def get_localizer(language='English'):
-    '''The factory method'''
-    languages = dict(English=EnglishGetter, Greek=GreekGetter)
-    return languages[language]()
-    
-    
-class Delegator(object):
-    '''
-    >>> delegator = Delegator(Delegate())
-    >>> delegator.p1
-    123
-    >>> delegator.p2
-    Traceback (most recent call last):
-    ...
-    AttributeError: 'Delegate' object has no attribute 'p2'
-    >>> delegator.do_something('nothing')
-    'Doing nothing'
-    >>> delegator.do_anything()
-    Traceback (most recent call last):
-    ...
-    AttributeError: 'Delegate' object has no attribute 'do_anything'
-    '''
-    
-        @abc.abstractmethod
-    def _handle(self, request):
-        raise NotImplementedError('Must provide implementation in subclass.')
-    
-        def undo(self):
-        self.rename(self.dest, self.src)
-    
-    
-class DB:
-    def __init__(self):
-        self._tm = None
-    
-        def __init__(self, deep, *targets):
-        self.deep = deep
-        self.targets = targets
-        self.commit()
-    
-        def subscribe(self, msg, subscriber):
-        self.subscribers.setdefault(msg, []).append(subscriber)
-    
-        def scan(self):
-        '''Scan the dial to the next station'''
-        self.pos += 1
-        if self.pos == len(self.stations):
-            self.pos = 0
-        print(u'Scanning... Station is %s %s' % (self.stations[self.pos], self.name))
-    
-    '''
-http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
-    
-    *TL;DR80
-Separates an algorithm from an object structure on which it operates.
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/dominos/.
 '''
+import logging
+from datetime import timedelta
     
-    *What does this example do?
-In this example queue.Queue is used to create the pool (wrapped in a
-custom ObjectPool object to use with the with statement), and it is
-populated with strings.
-As we can see, the first string object put in 'yam' is USED by the
-with statement. But because it is released back into the pool
-afterwards it is reused by the explicit call to sample_queue.get().
-Same thing happens with 'sam', when the ObjectPool created insided the
-function is deleted (by the GC) and the object is returned.
+                    else:
+                    if filename is None and \
+                       'content-disposition' in req.headers:
+                        match = re.findall(r'filename=(\S+)',
+                                           req.headers['content-disposition'])
+    
+    REQUIREMENTS = ['libpurecoollink==0.4.2']
+    
+    
+class Watcher():
+    '''Class for starting Watchdog.'''
