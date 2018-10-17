d78fefabe5e88da99a4240965fee6b4d36ec93e7
SUNsung <weatherius@gmail.com>
2018-10-17 23:51:50 +0300

    Rewrite [01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py] File in: 2018-10-17 23:51:49

--- pyton/01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py
+++ pyton/01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py
@@ -1,121 +1,39 @@
 
         
-        for page in itertools.count(1):
-    releases = json.loads(compat_urllib_request.urlopen(
-        'https://api.github.com/repos/rg3/youtube-dl/releases?page=%s' % page
-    ).read().decode('utf-8'))
+            def reducer(self, key, values):
+        '''Sum values for each key.
     
-            # from https://www.pornoxo.com/videos/7564/striptease-from-sexy-secretary/
-        expect_dict(
-            self,
-            self.ie._extract_jwplayer_data(r'''
-<script type='text/javascript'>
-    jwplayer('mediaplayer').setup({
-        'videoid': '7564',
-        'width': '100%',
-        'aspectratio': '16:9',
-        'stretching': 'exactfit',
-        'autostart': 'false',
-        'flashplayer': 'https://t04.vipstreamservice.com/jwplayer/v5.10/player.swf',
-        'file': 'https://cdn.pornoxo.com/key=MF+oEbaxqTKb50P-w9G3nA,end=1489689259,ip=104.199.146.27/ip=104.199.146.27/speed=6573765/buffer=3.0/2009-12/4b2157147afe5efa93ce1978e0265289c193874e02597.flv',
-        'image': 'https://t03.vipstreamservice.com/thumbs/pxo-full/2009-12/14/a4b2157147afe5efa93ce1978e0265289c193874e02597.flv-full-13.jpg',
-        'filefallback': 'https://cdn.pornoxo.com/key=9ZPsTR5EvPLQrBaak2MUGA,end=1489689259,ip=104.199.146.27/ip=104.199.146.27/speed=6573765/buffer=3.0/2009-12/m_4b2157147afe5efa93ce1978e0265289c193874e02597.mp4',
-        'logo.hide': true,
-        'skin': 'https://t04.vipstreamservice.com/jwplayer/skin/modieus-blk.zip',
-        'plugins': 'https://t04.vipstreamservice.com/jwplayer/dock/dockableskinnableplugin.swf',
-        'dockableskinnableplugin.piclink': '/index.php?key=ajax-videothumbsn&vid=7564&data=2009-12--14--4b2157147afe5efa93ce1978e0265289c193874e02597.flv--17370',
-        'controlbar': 'bottom',
-        'modes': [
-            {type: 'flash', src: 'https://t04.vipstreamservice.com/jwplayer/v5.10/player.swf'}
-        ],
-        'provider': 'http'
-    });
-    //noinspection JSAnnotator
-    invideo.setup({
-        adsUrl: '/banner-iframe/?zoneId=32',
-        adsUrl2: '',
-        autostart: false
-    });
-</script>
-            ''', 'dummy', require_title=False),
-            {
-                'thumbnail': 'https://t03.vipstreamservice.com/thumbs/pxo-full/2009-12/14/a4b2157147afe5efa93ce1978e0265289c193874e02597.flv-full-13.jpg',
-                'formats': [{
-                    'url': 'https://cdn.pornoxo.com/key=MF+oEbaxqTKb50P-w9G3nA,end=1489689259,ip=104.199.146.27/ip=104.199.146.27/speed=6573765/buffer=3.0/2009-12/4b2157147afe5efa93ce1978e0265289c193874e02597.flv',
-                    'ext': 'flv'
-                }]
-            })
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
     
-        params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
-    }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
+        it = 0
     
-            if error is None:
-            # store the user id in a new session and return to the index
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            for i, alg in enumerate(algorithms):
+            xvals = 0.1 + i * (1 + len(vals)) + np.arange(len(vals))
+            width = 0.8
     
-            if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ? WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
+                gc.collect()
+            print('benchmarking lars_path (without Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            lars_path(X, y, Gram=None, max_iter=n_informative)
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            lars[i_f, i_s] = delta
     
-    This typically means that you attempted to use functionality that needed
-to interface with the current application object in some way. To solve
-this, set up an application context with app.app_context().  See the
-documentation for more information.\
+        scikit_classifier_results.append(
+        delta.seconds + delta.microseconds / mu_second)
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 '''
-    
-    :copyright: © 2010 by the Pallets team.
-:license: BSD, see LICENSE for more details.
-'''
-import codecs
-import io
-import uuid
-from datetime import date, datetime
-from flask.globals import current_app, request
-from flask._compat import text_type, PY2
-    
-    
-def test_when_already_configured(usage_tracker_io, shell_pid,
-                                 shell, shell_config, logs):
-    shell.get_history.return_value = ['fuck']
-    shell_pid.return_value = 12
-    _change_tracker(usage_tracker_io, 12)
-    shell_config.read.return_value = 'eval $(thefuck --alias)'
-    main()
-    logs.already_configured.assert_called_once()
-    
-    
-python_3 = ('thefuck/python3-zsh',
-            u'''FROM python:3
-                RUN apt-get update
-                RUN apt-get install -yy zsh''',
-            u'sh')
-    
-    
-@pytest.fixture
-def brew_install_no_argument():
-    return '''This command requires a formula argument'''
-    
-    import re, cgi
-TAG_REGEX = re.compile(r'(<!--.*?-->|<[^>]*>)')
-NAMED_A_TAG_REGEX = re.compile(r'.*name ?= ?'([^']*)'')
