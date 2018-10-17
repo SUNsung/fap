21ba80ef4d0e9dd67ec05a3116504fc9b5e5f2e9
SUNsung <weatherius@gmail.com>
2018-10-18 00:58:54 +0300

    Rewrite [35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb] File in: 2018-10-18 00:58:53

--- ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
+++ ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
@@ -1,88 +1,116 @@
 
         
-            gu = GroupUser.find_by(user_id: moderator.id, group_id: group.id)
-    expect(gu.notification_level).to eq(NotificationLevels.all[:watching])
+                default_connectors = {
+          words_connector: ', ',
+          two_words_connector: ' and ',
+          last_word_connector: ', and '
+        }
+        if defined?(I18n)
+          i18n_connectors = I18n.translate(:'support.array', locale: options[:locale], default: {})
+          default_connectors.merge!(i18n_connectors)
+        end
+        options = default_connectors.merge!(options)
     
-        # The category for users with trust level 3 has been created.
-    # Add initial permissions and description. They can be changed later.
+              def render_collection
+            @collection.map do |item|
+              value = value_for_collection(item, @value_method)
+              text  = value_for_collection(item, @text_method)
+              default_html_options = default_html_options_for_collection(item, value)
+              additional_html_options = option_html_attributes(item)
     
-        def finalize_session
-      @threads.map(&:join)
+          def _find_all(path, prefixes, args, outside_app)
+        prefixes = [prefixes] if String === prefixes
+        prefixes.each do |prefix|
+          paths.each do |resolver|
+            if outside_app
+              templates = resolver.find_all_anywhere(path, prefix, *args)
+            else
+              templates = resolver.find_all(path, prefix, *args)
+            end
+            return templates unless templates.empty?
+          end
+        end
+        []
+      end
+    
+        initializer 'action_view.default_enforce_utf8' do |app|
+      ActiveSupport.on_load(:action_view) do
+        default_enforce_utf8 = app.config.action_view.delete(:default_enforce_utf8)
+        unless default_enforce_utf8.nil?
+          ActionView::Helpers::FormTagHelper.default_enforce_utf8 = default_enforce_utf8
+        end
+      end
+    end
+    
+        # Main render entry point shared by Action View and Action Controller.
+    def render(context, options)
+      if options.key?(:partial)
+        render_partial(context, options)
+      else
+        render_template(context, options)
+      end
+    end
+    
+          def perform(start_id, stop_id)
+        status_sql = Build
+          .where('ci_builds.commit_id = ci_stages.pipeline_id')
+          .where('ci_builds.stage = ci_stages.name')
+          .status_sql
+    
+              def valid?
+            nonzero? && utf8?
+          end
+    
+            def valid?
+          @name.present?
+        end
+      end
     end
   end
 end
 
     
-          it 'does not add a verbose flag to command if verbose is set to false' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              verbose: false
-            )
-          end').runner.execute(:test)
+              entry :untracked, Entry::Boolean,
+            description: 'Cache all untracked files.'
     
-            expect(result.size).to eq(3)
-        expect(result[0]).to eq(%(security create-keychain -p #{password.shellescape} ~/Library/Keychains/test.keychain))
+        def stylesheets_path
+      File.join assets_path, 'stylesheets'
+    end
+    
+        # change Microsoft filters to Sass calling convention
+    def replace_ms_filters(file)
+      log_transform
+      file.gsub(
+          /filter: e\(%\('progid:DXImageTransform.Microsoft.gradient\(startColorstr='%d', endColorstr='%d', GradientType=(\d)\)',argb\(([\-$\w]+)\),argb\(([\-$\w]+)\)\)\);/,
+          %Q(filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='\#{ie-hex-str(\\2)}', endColorstr='\#{ie-hex-str(\\3)}', GradientType=\\1);)
+      )
+    end
+    
+    
+    def get_file(url)
+      uri = URI(url)
+      cache_path = './#@cache_path#{uri.path}#{uri.query.tr('?&=', '-') if uri.query}'
+      FileUtils.mkdir_p File.dirname(cache_path)
+      if File.exists?(cache_path)
+        log_http_get_file url, true
+        File.read(cache_path, mode: 'rb')
+      else
+        log_http_get_file url, false
+        content = open(url).read
+        File.open(cache_path, 'wb') { |f| f.write content }
+        content
       end
+    end
     
-          it 'handles the exclude_dirs parameter with no elements correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: [])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
-      end
+        remove_duplicates
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
     
-            FastlaneCore::CertChecker.wwdr_certificate_installed?
-      end
-    
-    When /^I (?:log|sign) out manually$/ do
-  manual_logout
+      def confirm_on_page(page_name)
+    if page_name == 'my profile page'
+      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
+    else
+      expect(page).to have_path(path_to(page_name))
+    end
+  end
 end
-    
-    # Capybara defaults to XPath selectors rather than Webrat's default of CSS3. In
-# order to ease the transition to Capybara we set the default here. If you'd
-# prefer to use XPath just remove this line and adjust any selectors in your
-# steps to use the XPath syntax.
-Capybara.default_selector = :css
-    
-          get :index, params: {a_id: @aspect.id, page: '1'}, format: :json
-      save_fixture(response.body, 'aspects_manage_contacts_json')
-    end
-    
-      get(/.+/) do
-    send_sinatra_file(request.path) {404}
-  end
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
-    
-      # Escapes CDATA sections in post content
-  def cdata_escape(input)
-    input.gsub(/<!\[CDATA\[/, '&lt;![CDATA[').gsub(/\]\]>/, ']]&gt;')
-  end
-    
-    module Jekyll
