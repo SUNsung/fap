1ea935c640ee6c5cd2cae6107642a0b99c19a097
SUNsung <weatherius@gmail.com>
2018-10-18 06:45:25 +0300

    Rewrite [35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py] File in: 2018-10-18 06:45:24

--- pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
+++ pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
@@ -1,122 +1,19 @@
 
         
-            # Separating the 'table of contents' from the contents (blocks)
-    table_of_contents = ''.join(read_me.split('- - -')[0])
-    blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
-    for i in range(len(blocks)):
-        if i == 0:
-            blocks[i] = blocks[i] + '\n'
-        else:
-            blocks[i] = '# ' + blocks[i] + '\n'
+        
+class BlackJackCard(Card):
     
+        def __init__(self):
+        self.head = None
+        self.tail = None
     
-def check_entry(line_num, segments):
-    # START Title
-    raw_title = segments[index_title]
-    title_re_match = link_re.match(raw_title)
-    # url should be wrapped in '[TITLE](LINK)' Markdown syntax
-    if not title_re_match:
-        add_error(line_num, 'Title syntax should be '[TITLE](LINK)'')
-    else:
-        # do not allow '... API' in the entry title
-        title = title_re_match.group(1)
-        if title.upper().endswith(' API'):
-            add_error(line_num, 'Title should not end with '... API'. Every entry is an API here!')
-        # do not allow duplicate links
-        link = title_re_match.group(2)
-        if link in previous_links:
-            add_error(line_num, 'Duplicate link - entries should only be included in one section')
-        else:
-            previous_links.append(link)
-    # END Title
-    # START Description
-    # first character should be capitalized
-    char = segments[index_desc][0]
-    if char.upper() != char:
-        add_error(line_num, 'first character of description is not capitalized')
-    # last character should not punctuation
-    char = segments[index_desc][-1]
-    if char in punctuation:
-        add_error(line_num, 'description should not end with {}'.format(char))
-    desc_length = len(segments[index_desc])
-    if desc_length > 100:
-        add_error(line_num, 'description should not exceed 100 characters (currently {})'.format(desc_length))
-    # END Description
-    # START Auth
-    # values should conform to valid options only
-    auth = segments[index_auth]
-    if auth != 'No' and (not auth.startswith('`') or not auth.endswith('`')):
-        add_error(line_num, 'auth value is not enclosed with `backticks`')
-    if auth.replace('`', '') not in auth_keys:
-        add_error(line_num, '{} is not a valid Auth option'.format(auth))
-    # END Auth
-    # START HTTPS
-    # values should conform to valid options only
-    https = segments[index_https]
-    if https not in https_keys:
-        add_error(line_num, '{} is not a valid HTTPS option'.format(https))
-    # END HTTPS
-    # START CORS
-    # values should conform to valid options only
-    cors = segments[index_cors]
-    if cors not in cors_keys:
-        add_error(line_num, '{} is not a valid CORS option'.format(cors))
-    # END CORS
-    
-    README_FILE = 'README.md'
-helptext = sys.stdin.read()
-    
-    DESCRIPTION = 'YouTube video downloader'
-LONG_DESCRIPTION = 'Command-line program to download videos from YouTube.com and other video sites'
-    
-    
-def get_params(override=None):
-    PARAMETERS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                   'parameters.json')
-    LOCAL_PARAMETERS_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                         'local_parameters.json')
-    with io.open(PARAMETERS_FILE, encoding='utf-8') as pf:
-        parameters = json.load(pf)
-    if os.path.exists(LOCAL_PARAMETERS_FILE):
-        with io.open(LOCAL_PARAMETERS_FILE, encoding='utf-8') as pf:
-            parameters.update(json.load(pf))
-    if override:
-        parameters.update(override)
-    return parameters
-    
-            self.assertEqual(ie._html_search_meta('a', html), '1')
-        self.assertEqual(ie._html_search_meta('b', html), '2')
-        self.assertEqual(ie._html_search_meta('c', html), '3')
-        self.assertEqual(ie._html_search_meta('d', html), '4')
-        self.assertEqual(ie._html_search_meta('e', html), '5')
-        self.assertEqual(ie._html_search_meta('f', html), '6')
-        self.assertEqual(ie._html_search_meta(('a', 'b', 'c'), html), '1')
-        self.assertEqual(ie._html_search_meta(('c', 'b', 'a'), html), '3')
-        self.assertEqual(ie._html_search_meta(('z', 'x', 'c'), html), '3')
-        self.assertRaises(RegexNotFoundError, ie._html_search_meta, 'z', html, None, fatal=True)
-        self.assertRaises(RegexNotFoundError, ie._html_search_meta, ('z', 'x'), html, None, fatal=True)
-    
-    
-def _download_restricted(url, filename, age):
-    ''' Returns true if the file has been downloaded '''
-    
-        def test_youtube_feeds(self):
-        self.assertMatch('https://www.youtube.com/feed/watch_later', ['youtube:watchlater'])
-        self.assertMatch('https://www.youtube.com/feed/subscriptions', ['youtube:subscriptions'])
-        self.assertMatch('https://www.youtube.com/feed/recommended', ['youtube:recommended'])
-        self.assertMatch('https://www.youtube.com/my_favorites', ['youtube:favorites'])
-    
-    site_info = 'AcFun.tv'
-download = acfun_download
-download_playlist = playlist_not_supported('acfun')
-
-    
-        if '_text' in dictified['flashvars'][0]:
-        video_dict['flashvars'] = dictified['flashvars'][0]['_text'].strip()
-    
-    dailymotion_embed_patterns = [ 'www\.dailymotion\.com/embed/video/(\w+)' ]
-    
-            print('Ordered results using pool.imap():')
-        for x in imap_it:
-            print('\t', x)
-        print()
+            (foo, p1), 2
+        (bar, p1), 2
+        (bar, p1), 1
+        (foo, p2), 3
+        (bar, p3), 10
+        (foo, p4), 1
+        '''
+        timestamp, product_id, category, quantity = line.split('\t')
+        if self.within_past_week(timestamp):
+            yield (category, product_id), quantity
