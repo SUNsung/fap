0b7b2bb74a0124d5e152e38ac9eb63389fe2e817
SUNsung <weatherius@gmail.com>
2018-10-18 14:09:54 +0300

    Rewrite [287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py] File in: 2018-10-18 14:09:53

--- pyton/287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py
+++ pyton/287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py
@@ -1,112 +1,47 @@
 
         
-        
-class CreateExtension(Operation):
-    reversible = True
-    
-    This module contains the set of Requests' exceptions.
-'''
-from urllib3.exceptions import HTTPError as BaseHTTPError
-    
-        def __getitem__(self, key):
-        return self._store[key.lower()][1]
-    
-            String:                    '#4e9a06',        # class: 's'
-        String.Backtick:           '#4e9a06',        # class: 'sb'
-        String.Char:               '#4e9a06',        # class: 'sc'
-        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
-        String.Double:             '#4e9a06',        # class: 's2'
-        String.Escape:             '#4e9a06',        # class: 'se'
-        String.Heredoc:            '#4e9a06',        # class: 'sh'
-        String.Interpol:           '#4e9a06',        # class: 'si'
-        String.Other:              '#4e9a06',        # class: 'sx'
-        String.Regex:              '#4e9a06',        # class: 'sr'
-        String.Single:             '#4e9a06',        # class: 's1'
-        String.Symbol:             '#4e9a06',        # class: 'ss'
-    
-    Some codes have multiple names, and both upper- and lower-case versions of
-the names are allowed. For example, ``codes.ok``, ``codes.OK``, and
-``codes.okay`` all correspond to the HTTP status code 200.
-'''
-    
-        def items(self):
-        '''Dict-like items() that returns a list of name-value tuples from the
-        jar. Allows client-code to call ``dict(RequestsCookieJar)`` and get a
-        vanilla python dict of key value pairs.
-    
-        close_server = threading.Event()
-    server = Server(response_handler, wait_to_close_event=close_server)
-    
-    # pyOpenSSL version 18.0.0 dropped support for Python 2.6
-if sys.version_info < (2, 7):
-    PYOPENSSL_VERSION = 'pyOpenSSL >= 0.14, < 18.0.0'
-else:
-    PYOPENSSL_VERSION = 'pyOpenSSL >= 0.14'
-    
-    def youku_acfun_proxy(vid, sign, ref):
-    endpoint = 'http://player.acfun.cn/flash_data?vid={}&ct=85&ev=3&sign={}&time={}'
-    url = endpoint.format(vid, sign, str(int(time.time() * 1000)))
-    json_data = json.loads(get_content(url, headers=dict(referer=ref)))['data']
-    enc_text = base64.b64decode(json_data)
-    dec_text = rc4(b'8bdc7e1a', enc_text).decode('utf8')
-    youku_json = json.loads(dec_text)
-    
-    from xml.etree import cElementTree as ET
-from copy import copy
-from ..common import *
-#----------------------------------------------------------------------
-def ckplayer_get_info_by_xml(ckinfo):
-    '''str->dict
-    Information for CKPlayer API content.'''
-    e = ET.XML(ckinfo)
-    video_dict = {'title': '',
-                  #'duration': 0,
-                  'links': [],
-                  'size': 0,
-                  'flashvars': '',}
-    dictified = dictify(e)['ckplayer']
-    if 'info' in dictified:
-        if '_text' in dictified['info'][0]['title'][0]:  #title
-            video_dict['title'] = dictified['info'][0]['title'][0]['_text'].strip()
+            def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        self.is_available = True
     
     
-def get_title_and_urls(json_data):
-    title = legitimize(re.sub('[\s*]', '_', json_data['title']))
-    video_info = json_data['file_versions']['html5']['video']
-    if 'high' not in video_info:
-        if 'med' not in video_info:
-            video_url = video_info['low']['url']
-        else:
-            video_url = video_info['med']['url']
-    else:
-        video_url = video_info['high']['url']
-    audio_info = json_data['file_versions']['html5']['audio']
-    if 'high' not in audio_info:
-        if 'med' not in audio_info:
-            audio_url = audio_info['low']['url']
-        else:
-            audio_url = audio_info['med']['url']
-    else:
-        audio_url = audio_info['high']['url']
-    return title, video_url, audio_url
+class Vehicle(metaclass=ABCMeta):
     
-    #----------------------------------------------------------------------
-def dilidili_parser_data_to_stream_types(typ ,vid ,hd2 ,sign, tmsign, ulk):
-    '''->list'''
-    another_url = 'https://newplayer.jfrft.com/parse.php?xmlurl=null&type={typ}&vid={vid}&hd={hd2}&sign={sign}&tmsign={tmsign}&userlink={ulk}'.format(typ = typ, vid = vid, hd2 = hd2, sign = sign, tmsign = tmsign, ulk = ulk)
-    parse_url = 'http://player.005.tv/parse.php?xmlurl=null&type={typ}&vid={vid}&hd={hd2}&sign={sign}&tmsign={tmsign}&userlink={ulk}'.format(typ = typ, vid = vid, hd2 = hd2, sign = sign, tmsign = tmsign, ulk = ulk)
-    html = get_content(another_url, headers=headers)
+        def __init__(self, seller_category_map, seller_category_overrides_map):
+        self.seller_category_map = seller_category_map
+        self.seller_category_overrides_map = seller_category_overrides_map
     
-    info = re.search(r'(\{[^{]+\})(\{[^{]+\})(\{[^{]+\})(\{[^{]+\})(\{[^{]+\})', html).groups()
-    info = [i.strip('{}').split('->') for i in info]
-    info = {i[0]: i [1] for i in info}
+    password = key + 16 * [0]
+new_key = aes_encrypt(password, key_expansion(password)) * (32 // 16)
+r = openssl_encode('aes-256-ctr', new_key, iv)
+print('aes_decrypt_text 32')
+print(repr(r))
+
     
-    stream_types = []
-    for i in zip(info['deft'].split('|'), info['defa'].split('|')):
-        stream_types.append({'id': str(i[1][-1]), 'container': 'mp4', 'video_profile': i[0]})
-    return stream_types
-    }
-    }
-    }
-    }
-    }
+    
+def build_lazy_ie(ie, name):
+    valid_url = getattr(ie, '_VALID_URL', None)
+    s = ie_template.format(
+        name=name,
+        bases=', '.join(map(get_base_name, ie.__bases__)),
+        valid_url=valid_url,
+        module=ie.__module__)
+    if ie.suitable.__func__ is not InfoExtractor.suitable.__func__:
+        s += '\n' + getsource(ie.suitable)
+    if hasattr(ie, '_make_valid_url'):
+        # search extractors
+        s += make_valid_template.format(valid_url=ie._make_valid_url())
+    return s
+    
+        return ret
+    
+    
+total_bytes = 0
+    
+        ydl.report_warning = _report_warning
+
+    
+    
+def _download_restricted(url, filename, age):
+    ''' Returns true if the file has been downloaded '''
