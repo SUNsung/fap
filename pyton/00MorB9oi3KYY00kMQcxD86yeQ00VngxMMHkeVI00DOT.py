5fd92553faa2c080dad6081beab0654abe0e4fee
SUNsung <weatherius@gmail.com>
2018-10-18 18:05:08 +0300

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2018-10-18 18:05:08

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,175 +1,148 @@
 
         
-        EXAMPLES = '''
-# Note: These examples do not set authentication details, see the AWS Guide for details.
+        with tf.Session() as sess:
+  # restore the latest model ckpt
+  if FLAGS.checkpoint_path == 'SAMPLE_CHECKPOINT':
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    model_checkpoint_path = os.path.join(dir_path, 'trained_itb/model-65000')
+  else:
+    model_checkpoint_path = FLAGS.checkpoint_path
+  try:
+    saver.restore(sess, model_checkpoint_path)
+    print ('Model restored from', model_checkpoint_path)
+  except:
+    assert False, ('No checkpoints to restore from, is the path %s correct?'
+                   %model_checkpoint_path)
     
-    notes:
-  - does not support check mode
+      Returns:
+    patches: A 2D matrix,
+      each entry is a matrix of shape (batch_size, num_timesteps).
+  '''
+  preprocessed = [['<S>']+sentence+['</S>'] for sentence in sentences]
+  max_len = max([len(sent) for sent in preprocessed])
     
-    
-def main():
-    argument_spec = ec2_argument_spec()
-    argument_spec.update(
-        dict(
-            name=dict(required=True),
-            schedule_expression=dict(),
-            event_pattern=dict(),
-            state=dict(choices=['present', 'disabled', 'absent'],
-                       default='present'),
-            description=dict(),
-            role_arn=dict(),
-            targets=dict(type='list', default=[]),
-        )
-    )
-    module = AnsibleAWSModule(argument_spec=argument_spec)
-    
-    try:
-    import botocore
-except ImportError:
-    pass  # will be detected by imported HAS_BOTO3
-    
-        connection = boto3_conn(module,
-                            resource='ec2', conn_type='client',
-                            region=region, endpoint=ec2_url, **aws_connect_params)
-    
-            last_snapshot_min_age = last_snapshot_min_age * 60  # Convert to seconds
-        snapshot = _get_most_recent_snapshot(current_snapshots,
-                                             max_snapshot_age_secs=last_snapshot_min_age)
-    try:
-        # Create a new snapshot if we didn't find an existing one to use
-        if snapshot is None:
-            snapshot = ec2.create_snapshot(volume_id, description=description)
-            changed = True
-        if wait:
-            if not _create_with_wait(snapshot, wait_timeout):
-                module.fail_json(msg='Timed out while creating snapshot.')
-        if snapshot_tags:
-            for k, v in snapshot_tags.items():
-                snapshot.add_tag(k, v)
-    except boto.exception.BotoServerError as e:
-        module.fail_json(msg='%s: %s' % (e.error_code, e.error_message))
+      data_len = len(raw_data)
+  batch_len = data_len // batch_size
+  data = np.full([batch_size, batch_len], EOS_INDEX, dtype=np.int32)
+  for i in range(batch_size):
+    data[i] = raw_data[batch_len * i:batch_len * (i + 1)]
     
     
-def get_tags(connection, load_balancer_name):
-    tags = connection.describe_tags(LoadBalancerNames=[load_balancer_name])['TagDescriptions']
-    if not tags:
-        return {}
-    return boto3_tag_list_to_ansible_dict(tags[0]['Tags'])
-    
-    RETURN = '''
-server_certificate_id:
-    description: The 21 character certificate id
-    returned: success
-    type: str
-    sample: 'ADWAJXWTZAXIPIMQHMJPO'
-certificate_body:
-    description: The asn1der encoded PEM string
-    returned: success
-    type: str
-    sample: '-----BEGIN CERTIFICATE-----\nbunch of random data\n-----END CERTIFICATE-----'
-server_certificate_name:
-    description: The name of the server certificate
-    returned: success
-    type: str
-    sample: 'server-cert-name'
-arn:
-    description: The Amazon resource name of the server certificate
-    returned: success
-    type: str
-    sample: 'arn:aws:iam::911277865346:server-certificate/server-cert-name'
-path:
-    description: The path of the server certificate
-    returned: success
-    type: str
-    sample: '/'
-expiration:
-    description: The date and time this server certificate will expire, in ISO 8601 format.
-    returned: success
-    type: str
-    sample: '2017-06-15T12:00:00+00:00'
-upload_date:
-    description: The date and time this server certificate was uploaded, in ISO 8601 format.
-    returned: success
-    type: str
-    sample: '2015-04-25T00:36:40+00:00'
-'''
+def write_unmasked_log(log, id_to_word, sequence_eval):
+  '''Helper function for logging evaluated sequences without mask.'''
+  indices_arr = np.asarray(sequence_eval)
+  samples = helper.convert_to_human_readable(id_to_word, indices_arr,
+                                             FLAGS.batch_size)
+  for sample in samples:
+    log.write(sample + '\n')
+  log.flush()
+  return samples
     
     
-def benchmark(estimator, data):
-    gc.collect()
-    print('Benching %s' % estimator)
-    t0 = time()
-    estimator.fit(data)
-    training_time = time() - t0
-    data_t = estimator.transform(data)
-    data_r = estimator.inverse_transform(data_t)
-    reconstruction_error = np.mean(np.abs(data - data_r))
-    return {'time': training_time, 'error': reconstruction_error}
+def create_gen_pretrain_op(hparams, cross_entropy_loss, global_step):
+  '''Create a train op for pretraining.'''
+  with tf.name_scope('pretrain_generator'):
+    optimizer = tf.train.AdamOptimizer(hparams.gen_pretrain_learning_rate)
+    gen_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
+    ]
+    gen_grads = tf.gradients(cross_entropy_loss, gen_vars)
+    gen_grads_clipped, _ = tf.clip_by_global_norm(gen_grads,
+                                                  FLAGS.grad_clipping)
+    gen_pretrain_op = optimizer.apply_gradients(
+        zip(gen_grads_clipped, gen_vars), global_step=global_step)
+    return gen_pretrain_op
     
-            tick_vals = []
-        tick_labels = []
-    
-    
-if __name__ == '__main__':
-    ###########################################################################
-    # Option parser
-    ###########################################################################
-    op = optparse.OptionParser()
-    op.add_option('--n-times',
-                  dest='n_times', default=5, type=int,
-                  help='Benchmark results are average over n_times experiments')
-    
-    input data sparsity: 0.050000
-true coef sparsity: 0.000100
-test data sparsity: 0.027400
-model sparsity: 0.000024
-r^2 on test data (dense model) : 0.233651
-r^2 on test data (sparse model) : 0.233651
-Wrote profile results to sparsity_benchmark.py.lprof
-Timer unit: 1e-06 s
-    
-        link = nodes.reference(text=text, refuri=ref, **options)
-    return [link], []
-    
-    try:
-    from urllib import urlopen
-except ImportError:
-    from urllib.request import urlopen
+        ## Load the Discriminator weights from the MaskGAN checkpoint if
+    # the weights are compatible.
+    if FLAGS.discriminator_model == 'seq2seq_vd':
+      dis_variable_maps = variable_mapping.dis_seq2seq_vd(hparams)
+      dis_init_saver = tf.train.Saver(var_list=dis_variable_maps)
+      init_savers['dis_init_saver'] = dis_init_saver
     
     
-# Join pathnames.
-# Ignore the previous parts if a part is absolute.
-# Insert a '/' unless the first part is empty or already ends in '/'.
+def percent_unique_ngrams_in_train(train_ngrams_dict, gen_ngrams_dict):
+  '''Compute the percent of ngrams generated by the model that are
+  present in the training text and are unique.'''
     
-            with self.restrict_walk_packages():
-            for url, title in requests:
-                self.call_url_handler(url, title)
+    # TODO: ensure that history changes.
+
     
-    # An imaginary module that would make this work and be safe.
-from imaginary import magic_html_parser
     
-    # Proxy type for generator objects
-class GeneratorProxy(BaseProxy):
-    _exposed_ = ['__next__']
-    def __iter__(self):
-        return self
-    def __next__(self):
-        return self._callmethod('__next__')
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
+    history_changed(proc, TIMEOUT, u'echo test')
     
-        # Get and print results
-    print('Unordered results:')
-    for i in range(len(TASKS1)):
-        print('\t', done_queue.get())
+        def __init__(self):
+        self.settings = None  # set in scrapy.cmdline
     
-        failures = []
-    while point is not None:
-        if point.name:
-            if re.search('h[1-2]', point.name):
-                if point.name == 'h1':
-                    h1_directory = os.path.join(output_directory, clean_text(point.text))
-                    current_directory = h1_directory
-                elif point.name == 'h2':
-                    current_directory = os.path.join(h1_directory, clean_text(point.text))  
-                if not os.path.exists(current_directory):
-                    os.makedirs(current_directory)
-                print_title(point.text)
+    
+class _BenchServer(object):
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
+    
+    
+    def run(self, args, opts):
+        # parse arguments
+        if not len(args) == 1 or not is_url(args[0]):
+            raise UsageError()
+        else:
+            url = args[0]
+    
+            if self.crawler_process.bootstrap_failed:
+            self.exitcode = 1
+
+    
+    import scrapy
+from scrapy.commands import ScrapyCommand
+from scrapy.utils.versions import scrapy_components_versions
+    
+    import urllib
+    
+    #----------------------------------------------------------------------
+def ckplayer_download(url, output_dir = '.', merge = False, info_only = False, is_xml = True, **kwargs):
+    if is_xml:  #URL is XML URL
+        try:
+            title = kwargs['title']
+        except:
+            title = ''
+        try:
+            headers = kwargs['headers']  #headers provided
+            ckinfo = get_content(url, headers = headers)
+        except NameError:
+            ckinfo = get_content(url)
+        
+        ckplayer_download_by_xml(ckinfo, output_dir, merge, 
+                                info_only, title = title)
+    
+    headers = {
+    'DNT': '1',
+    'Accept-Encoding': 'gzip, deflate, sdch, br',
+    'Accept-Language': 'en-CA,en;q=0.8,en-US;q=0.6,zh-CN;q=0.4,zh;q=0.2',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Referer': 'http://www.dilidili.com/',
+    'Connection': 'keep-alive',
+    'Save-Data': 'on',
+}
+    
+    
+def fantasy_download_by_id_channelId(id = 0, channelId = 0, output_dir = '.', merge = True, info_only = False,
+                                     **kwargs):
+    api_url = 'http://www.fantasy.tv/tv/playDetails.action?' \
+              'myChannelId=1&id={id}&channelId={channelId}&t={t}'.format(id = id,
+                                                                         channelId = channelId,
+                                                                         t = str(random.random())
+                                                                         )
+    html = get_content(api_url)
+    html = json.loads(html)
