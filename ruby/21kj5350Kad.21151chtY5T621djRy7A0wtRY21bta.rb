6eccb5ae9472eb8d5f453c6935e0fff159146f1c
SUNsung <weatherius@gmail.com>
2018-10-18 22:43:50 +0300

    Rewrite [21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb] File in: 2018-10-18 22:43:49

--- ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
+++ ruby/21kj5350Kad.21151chtY5T621djRy7A0wtRY21bta.rb
@@ -1,17 +1,9 @@
 
         
-            keys.each do |key|
-      value = env[key]
-      s = '#{key}: #{value}'
-      case key
-      when 'CC', 'CXX', 'LD'
-        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
-      end
-      f.puts s
-    end
+          def <<(o)
+    @settings << o
+    self
   end
-end
-
     
       # True if a {Formula} is being built with a specific option.
   # <pre>args << '--i-want-spam' if build.with? 'spam'
@@ -27,125 +19,138 @@ end
   def with?(val)
     option_names = val.respond_to?(:option_names) ? val.option_names : [val]
     
-    module Homebrew
-  def list
-    # Use of exec means we don't explicitly exit
-    list_unbrewed if ARGV.flag? '--unbrewed'
-    
-      def self.canonical_name(name)
-    Formulary.canonical_name(name)
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
+      end
+      cmds
+    end.sort
   end
     
-      before_action :set_account
-  before_action :set_size
-  before_action :set_statuses
+      def macports_or_fink
+    @ponk ||= MacOS.macports_or_fink
+    @ponk.join(', ') unless @ponk.empty?
+  end
     
-            @user.send_confirmation_instructions
+          paths = tree['Formula'] || tree['HomebrewFormula'] || tree['Casks'] || tree['.'] || []
+      cache[key] = paths.map { |path| File.basename(path, '.rb') }
+    end
+    
+          if !override && lanes[lane.platform][lane.name]
+        UI.user_error!('Lane '#{lane.name}' was defined multiple times!')
       end
     
-        def filter_params
-      params.permit(
-        :local,
-        :remote,
-        :by_domain,
-        :shortcode
-      )
-    end
-  end
-end
-
+            result = Fastlane::FastFile.new.parse('lane :test do
+          add_git_tag ({
+            tag: '#{tag}',
+            grouping: 'grouping',
+            build_number: 'build_number',
+            prefix: 'prefix',
+          })
+        end').runner.execute(:test)
     
-          if @email_domain_block.save
-        log_action :create, @email_domain_block
-        redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.created_msg')
+          it 'handles the extensions parameter with no elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', extensions: [])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
+      end
+    
+            it 'executes the correct git command' do
+          allow(Fastlane::Actions).to receive(:sh).with('git add #{path[0].shellescape} #{path[1].shellescape}', anything).and_return('')
+          result = Fastlane::FastFile.new.parse('lane :test do
+            git_add(path: #{path})
+          end').runner.execute(:test)
+        end
+      end
+    
+        it 'shell-escapes environment variable values' do
+      message = 'A message'
+      path = '/usr/my local/bin'
+      command = command_from_args({ 'PATH' => path }, 'git', 'commit', '-m', message)
+      expect(command).to eq('PATH=#{path.shellescape} git commit -m #{message.shellescape}')
+    end
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
       else
-        render :new
+        '#{quote}<footer>#{author}</footer>'
       end
+      '<blockquote>#{blockquote}</blockquote>'
     end
     
-        private
-    
-      def status_finder
-    StatusFinder.new(params[:url])
-  end
-    
-      it 'raises a TypeError when passed a String' do
-    lambda { srand('7') }.should raise_error(TypeError)
-  end
-end
-    
-        after :each do
-      @tmp_file.close
-      rm_r @tmp_file
-    end
-    
-        @save_to.each { |_, v| FileUtils.mkdir_p(v) }
-    
-        def pos=(i)
-      @s.pos = str_to_byte_pos i
-      i
-    end
-    
-        def log_http_get_files(files, from, cached = false)
-      return if files.empty?
-      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
-      if cached
-        puts dark green s
-      else
-        puts dark cyan s
+      class RenderPartialTag < Liquid::Tag
+    include OctopressFilters
+    def initialize(tag_name, markup, tokens)
+      @file = nil
+      @raw = false
+      if markup =~ /^(\S+)\s?(\w+)?/
+        @file = $1.strip
+        @raw = $2 == 'raw'
       end
+      super
     end
     
-        get page
-    # good html:
-    # <pre><code>one\ntwo\nthree\nfour\n</code></pre>\n
-    # broken html:
-    # <pre>\n  <code>one\ntwo\nthree\nfour\n</code>\n</pre>
-    assert_match /<pre><code>one\ntwo\nthree\nfour\n<\/code><\/pre>\n/m, last_response.body
-  end
-    
-        @wiki = Gollum::Wiki.new(@path)
-    page  = @wiki.page('PG')
-    assert_equal '바뀐 text', utf8(page.raw_data)
-    assert_equal 'ghi', page.version.message
-  end
-    
-      begin
-    require 'gollum-lib'
-    wiki = Gollum::Wiki.new(gollum_path, wiki_options)
-    if !wiki.exist? then
-      raise Gollum::InvalidGitRepositoryError
-    end
-    if wiki_options[:plantuml_url]
-      Gollum::Filter::PlantUML.configure do |config|
-        puts 'Using #{wiki_options[:plantuml_url]} as PlantUML endpoint'
-        config.url = wiki_options[:plantuml_url]
+          def create_worker_spec
+        template_file = File.join(
+            'spec/workers',
+            class_path,
+            '#{file_name}_worker_spec.rb'
+        )
+        template 'worker_spec.rb.erb', template_file
       end
+    
+        module PsychAutoload
+      def resolve_class(klass_name)
+        return nil if !klass_name || klass_name.empty?
+        # constantize
+        names = klass_name.split('::')
+        names.shift if names.empty? || names.first.empty?
+    
+        module ActionMailer
+      def sidekiq_delay(options={})
+        Proxy.new(DelayedMailer, self, options)
+      end
+      def sidekiq_delay_for(interval, options={})
+        Proxy.new(DelayedMailer, self, options.merge('at' => Time.now.to_f + interval.to_f))
+      end
+      def sidekiq_delay_until(timestamp, options={})
+        Proxy.new(DelayedMailer, self, options.merge('at' => timestamp.to_f))
+      end
+      alias_method :delay, :sidekiq_delay
+      alias_method :delay_for, :sidekiq_delay_for
+      alias_method :delay_until, :sidekiq_delay_until
     end
-    puts
-    puts 'Loaded Gollum wiki at:'
-    puts '#{File.expand_path(gollum_path).inspect}'
-    puts
-    puts 'Example API calls:'
-    puts %(    page = wiki.page('page-name'))
-    puts %(    # => <Gollum::Page>)
-    puts
-    puts %(    page.raw_data)
-    puts %(    # => '# My wiki page')
-    puts
-    puts %(    page.formatted_data)
-    puts %(    # => '<h1>My wiki page</h1>')
-    puts
-    puts 'Full API documentation at:'
-    puts 'https://github.com/gollum/gollum-lib'
-    puts
-    IRB.start_session(binding)
-  rescue Gollum::InvalidGitRepositoryError, Gollum::NoSuchPathError
-    puts 'Invalid Gollum wiki at #{File.expand_path(gollum_path).inspect}'
-    exit 0
-  end
-else
-  require 'gollum/app'
-  Precious::App.set(:gollum_path, gollum_path)
-  Precious::App.set(:wiki_options, wiki_options)
-  Precious::App.settings.mustache[:templates] = wiki_options[:template_dir] if wiki_options[:template_dir]
+    
+          def retrieve
+        map(&:make_new)
+      end
+    
+          # Drain all queued jobs across all workers
+      def drain_all
+        while jobs.any?
+          worker_classes = jobs.map { |job| job['class'] }.uniq
