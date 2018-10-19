ca25e521effe0ec9640643745682df97df48004e
SUNsung <weatherius@gmail.com>
2018-10-19 11:07:30 +0300

    Rewrite [352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp] File in: 2018-10-19 11:07:30

--- cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
+++ cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
@@ -1,171 +1,213 @@
 
         
-        Q_SIGNALS:
-    void valueChanged();
-    
-        std::tie(r_key_dest, preexisting_r_address) = build_address();
-    std::tie(s_key_dest, preexisting_s_address) = build_address();
-    std::tie(std::ignore, new_address) = build_address();
-    
-        explicit reverse_lock(Lock& _lock) : lock(_lock) {
-        _lock.unlock();
-        _lock.swap(templock);
+        // Computes and returns the dot product of the n-vectors u and v.
+// Uses Intel AVX intrinsics to access the SIMD instruction set.
+double DotProductAVX(const double* u, const double* v, int n) {
+  int max_offset = n - 4;
+  int offset = 0;
+  // Accumulate a set of 4 sums in sum, by loading pairs of 4 values from u and
+  // v, and multiplying them together in parallel.
+  __m256d sum = _mm256_setzero_pd();
+  if (offset <= max_offset) {
+    offset = 4;
+    // Aligned load is reputedly faster but requires 32 byte aligned input.
+    if ((reinterpret_cast<uintptr_t>(u) & 31) == 0 &&
+        (reinterpret_cast<uintptr_t>(v) & 31) == 0) {
+      // Use aligned load.
+      __m256d floats1 = _mm256_load_pd(u);
+      __m256d floats2 = _mm256_load_pd(v);
+      // Multiply.
+      sum = _mm256_mul_pd(floats1, floats2);
+      while (offset <= max_offset) {
+        floats1 = _mm256_load_pd(u + offset);
+        floats2 = _mm256_load_pd(v + offset);
+        offset += 4;
+        __m256d product = _mm256_mul_pd(floats1, floats2);
+        sum = _mm256_add_pd(sum, product);
+      }
+    } else {
+      // Use unaligned load.
+      __m256d floats1 = _mm256_loadu_pd(u);
+      __m256d floats2 = _mm256_loadu_pd(v);
+      // Multiply.
+      sum = _mm256_mul_pd(floats1, floats2);
+      while (offset <= max_offset) {
+        floats1 = _mm256_loadu_pd(u + offset);
+        floats2 = _mm256_loadu_pd(v + offset);
+        offset += 4;
+        __m256d product = _mm256_mul_pd(floats1, floats2);
+        sum = _mm256_add_pd(sum, product);
+      }
     }
-    
-    
-    {    /* Try to multiply it by bad values */
-    CHECK(secp256k1_ecdh(ctx, output, &point, s_zero) == 0);
-    CHECK(secp256k1_ecdh(ctx, output, &point, s_overflow) == 0);
-    /* ...and a good one */
-    s_overflow[31] -= 1;
-    CHECK(secp256k1_ecdh(ctx, output, &point, s_overflow) == 1);
-}
-    
-    BOOST_AUTO_TEST_SUITE_END()
-
-    
-    static void RejectDifficultyMismatch(double difficulty, double expected_difficulty) {
-     BOOST_CHECK_MESSAGE(
-        DoubleEquals(difficulty, expected_difficulty, 0.00001),
-        'Difficulty was ' + std::to_string(difficulty)
-            + ' but was expected to be ' + std::to_string(expected_difficulty));
-}
-    
-    /// Make type-agnostic format list from list of template arguments.
-///
-/// The exact return type of this function is an implementation detail and
-/// shouldn't be relied upon.  Instead it should be stored as a FormatListRef:
-///
-///   FormatListRef formatList = makeFormatList( /*...*/ );
-template<typename... Args>
-detail::FormatListN<sizeof...(Args)> makeFormatList(const Args&... args)
-{
-    return detail::FormatListN<sizeof...(args)>(args...);
-}
-    
-    
-    { protected:
-  int n_ = 0;
-  std::mutex m_;
-  std::condition_variable cv_;
-};
-    
-    **Result**
-    
-    REGISTER_CPU_OPERATOR(
-    MergeSingleListFeatureTensors,
-    MergeSingleListFeatureTensorsOp<CPUContext>);
-OPERATOR_SCHEMA(MergeSingleListFeatureTensors)
-    .SetDoc(
-        'Merge given single-feature tensors with list features into one '
-        'multi-feature tensor.' +
-        doc)
-    .NumInputs([](int n) { return n >= 3 && n % 3 == 0; })
-    .NumOutputs(4)
-    .Input(0, 'in1_lengths', '.lengths')
-    .Input(1, 'in1_values', '.values')
-    .Input(2, 'in1_presence', '.presence')
-    .Output(0, 'out_lengths', '.lengths')
-    .Output(1, 'out_keys', '.keys')
-    .Output(2, 'out_values_lengths', '.values.lengths')
-    .Output(3, 'out_values_values', '.values.values')
-    .Arg('feature_ids', 'feature ids');
-    
-    workspace.FeedBlob('data', np.array([8,2,1,1,7,8,1]).astype(np.float32))
-print('data:\n', workspace.FetchBlob('data'))
-    
-      template <typename T>
-  bool DoRunWithType() {
-    const auto& data = Input(0);
-    CAFFE_ENFORCE(data.ndim() == 1, 'data should be 1-D.');
-    }
-    
-    	static char (&test(B *))[1];
-	static char (&test(...))[2];
-    
-    #include 'core/math/matrix3.h'
-#include 'core/math/transform.h'
-#include 'core/math/vector3.h'
-#include 'core/typedefs.h'
-    
-    	const btDiscreteDynamicsWorld *m_world;
-	btPersistentManifold *m_manifoldPtr;
-	bool m_ownManifold;
-	bool m_isSwapped;
-    
-    
-    {	_FORCE_INLINE_ void _set_physics_server(BulletPhysicsServer *p_physicsServer) { physicsServer = p_physicsServer; }
-	_FORCE_INLINE_ BulletPhysicsServer *get_physics_server() const { return physicsServer; }
-};
-#endif
-
-    
-    void register_csg_types() {
-    }
-    
-    static ResourceFormatDDS *resource_loader_dds = NULL;
-    
-    	ADD_PROPERTY(PropertyInfo(Variant::BOOL, 'refuse_new_connections'), 'set_refuse_new_connections', 'is_refusing_new_connections');
-	ADD_PROPERTY(PropertyInfo(Variant::INT, 'transfer_mode', PROPERTY_HINT_ENUM, 'Unreliable,Unreliable Ordered,Reliable'), 'set_transfer_mode', 'get_transfer_mode');
-    
-      tmp0 = in[6 * stride];
-  tmp1 = kIDCTMatrix[ 6] * tmp0;
-  tmp2 = kIDCTMatrix[14] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] -= tmp2;
-  out[3] -= tmp1;
-  out[4] -= tmp1;
-  out[5] -= tmp2;
-  out[6] += tmp2;
-  out[7] += tmp1;
-    
-    #ifndef GUETZLI_JPEG_DATA_READER_H_
-#define GUETZLI_JPEG_DATA_READER_H_
-    
-    class DHTRegistry {
-private:
-  struct Data {
-    bool initialized;
-    }
-    }
-    
-    namespace aria2 {
-    }
-    
-      virtual bool isReply() const CXX11_OVERRIDE;
-    
-    #include 'DHTNode.h'
-#include 'DHTConstants.h'
-#include 'bittorrent_helper.h'
-#include 'DlAbortEx.h'
-#include 'Logger.h'
-#include 'a2netcompat.h'
-#include 'fmt.h'
-#include 'util.h'
-#include 'array_fun.h'
-#include 'LogFactory.h'
-#include 'BufferedFile.h'
-    
-    #endif // D_DHT_ROUTING_TABLE_DESERIALIZER_H
-
-    
-      std::chrono::seconds timeout_;
-    
-      virtual void
-  addPeriodicTask1(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
-    
-    #endif // D_DHT_TOKEN_TRACKER_H
-
-    
-      virtual void process() CXX11_OVERRIDE;
-    
-    void DNSCache::CacheEntry::markBad(const std::string& addr)
-{
-  auto i = find(addr);
-  if (i != addrEntries_.end()) {
-    i->good_ = false;
   }
+  // Add the 4 product sums together horizontally. Not so easy as with sse, as
+  // there is no add across the upper/lower 128 bit boundary, so permute to
+  // move the upper 128 bits to lower in another register.
+  __m256d sum2 = _mm256_permute2f128_pd(sum, sum, 1);
+  sum = _mm256_hadd_pd(sum, sum2);
+  sum = _mm256_hadd_pd(sum, sum);
+  double result;
+  // _mm256_extract_f64 doesn't exist, but resist the temptation to use an sse
+  // instruction, as that introduces a 70 cycle delay. All this casting is to
+  // fool the intrinsics into thinking we are extracting the bottom int64.
+  auto cast_sum = _mm256_castpd_si256(sum);
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored '-Wstrict-aliasing'
+  *(reinterpret_cast<int64_t*>(&result)) =
+#if defined(_WIN32) || defined(__i386__)
+      // This is a very simple workaround that is activated
+      // for all platforms that do not have _mm256_extract_epi64.
+      // _mm256_extract_epi64(X, Y) == ((uint64_t*)&X)[Y]
+      ((uint64_t*)&cast_sum)[0]
+#else
+      _mm256_extract_epi64(cast_sum, 0)
+#endif
+      ;
+#pragma GCC diagnostic pop
+  while (offset < n) {
+    result += u[offset] * v[offset];
+    ++offset;
+  }
+  return result;
 }
     
-      DNSCache& operator=(const DNSCache& c);
+    #ifndef DOCQUAL_H
+#define DOCQUAL_H
+    
+      // Search the nearest neighbor of part in one vertical direction as defined in
+  // search_bottom. It returns the neighbor found that major x overlap with it,
+  // or nullptr when not found.
+  ColPartition* SearchNNVertical(const bool search_bottom,
+                                 const ColPartition* part);
+    
+      // Gets the first n words (split by _) and puts them in t.
+  // For example, tesseract_foo_bar with N=2 will yield tesseract_foo_.
+  void GetFirstWords(const char *s,  // source string
+                     int n,          // number of words
+                     char *t);       // target string
+    
+        bool NDArrayView::IsSliceView()
+    {
+        switch (m_dataType)
+        {
+        case DataType::Float:
+        {
+            auto currentMatrix = GetMatrix<float>();
+            return currentMatrix->IsView();
+        }
+        case DataType::Double:
+        {
+            auto currentMatrix = GetMatrix<double>();
+            return currentMatrix->IsView();
+        }
+        case DataType::Float16:
+        {
+            auto currentMatrix = GetMatrix<half>();
+            return currentMatrix->IsView();
+        }
+        case DataType::Int8:
+        {
+            auto currentMatrix = GetMatrix<char>();
+            return currentMatrix->IsView();
+        }
+        case DataType::Int16:
+        {
+            auto currentMatrix = GetMatrix<short>();
+            return currentMatrix->IsView();
+        }
+        }
+        return false;
+    }
+    
+                Microsoft::MSR::CNTK::LearnableParameter<ElementType>::InitBilinear(*valueMatrix, AsTensorShape(shape), kernelWidth, kernelHeight, AsCNTKImplDeviceId(device));
+        }
+        else
+        {
+            auto randomSeed = (unsigned long)initConfig[RandomSeedAttributeName].Value<size_t>();
+            // using this in place on an assert, which is ignored in Release mode.
+            if (randomSeed == SentinelValueForAutoSelectRandomSeed) {
+                RuntimeError('Unexpected 'auto-select' placeholder. At this point the seed should have a fixed value.');
+            }
+    
+    // Exception wrapper to include native call stack string
+template <class E>
+class ExceptionWithCallStack : public E, public IExceptionWithCallStackBase
+{
+public:
+    ExceptionWithCallStack(const std::string& msg, const std::string& callstack) :
+        E(msg), m_callStack(callstack)
+    { }
+    }
+    
+    
+    {}  // namespace leveldb
+
+    
+      // Recover the descriptor from persistent storage.  May do a significant
+  // amount of work to recover recently logged updates.  Any changes to
+  // be made to the descriptor are added to *edit.
+  Status Recover(VersionEdit* edit, bool* save_manifest)
+      EXCLUSIVE_LOCKS_REQUIRED(mutex_);
+    
+      // Successful parses
+  static struct {
+    const char* fname;
+    uint64_t number;
+    FileType type;
+  } cases[] = {
+    { '100.log',            100,   kLogFile },
+    { '0.log',              0,     kLogFile },
+    { '0.sst',              0,     kTableFile },
+    { '0.ldb',              0,     kTableFile },
+    { 'CURRENT',            0,     kCurrentFile },
+    { 'LOCK',               0,     kDBLockFile },
+    { 'MANIFEST-2',         2,     kDescriptorFile },
+    { 'MANIFEST-7',         7,     kDescriptorFile },
+    { 'LOG',                0,     kInfoLogFile },
+    { 'LOG.old',            0,     kInfoLogFile },
+    { '18446744073709551615.log', 18446744073709551615ull, kLogFile },
+  };
+  for (int i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
+    std::string f = cases[i].fname;
+    ASSERT_TRUE(ParseFileName(f, &number, &type)) << f;
+    ASSERT_EQ(cases[i].type, type) << f;
+    ASSERT_EQ(cases[i].number, number) << f;
+  }
+    
+      // Add an entry into memtable that maps key to value at the
+  // specified sequence number and with the specified type.
+  // Typically value will be empty if type==kTypeDeletion.
+  void Add(SequenceNumber seq, ValueType type,
+           const Slice& key,
+           const Slice& value);
+    
+      void ScanTable(uint64_t number) {
+    TableInfo t;
+    t.meta.number = number;
+    std::string fname = TableFileName(dbname_, number);
+    Status status = env_->GetFileSize(fname, &t.meta.file_size);
+    if (!status.ok()) {
+      // Try alternate file name.
+      fname = SSTTableFileName(dbname_, number);
+      Status s2 = env_->GetFileSize(fname, &t.meta.file_size);
+      if (s2.ok()) {
+        status = Status::OK();
+      }
+    }
+    if (!status.ok()) {
+      ArchiveFile(TableFileName(dbname_, number));
+      ArchiveFile(SSTTableFileName(dbname_, number));
+      Log(options_.info_log, 'Table #%llu: dropped: %s',
+          (unsigned long long) t.meta.number,
+          status.ToString().c_str());
+      return;
+    }
+    }
+    
+      // Forward iteration test
+  for (int i = 0; i < R; i++) {
+    SkipList<Key, Comparator>::Iterator iter(&list);
+    iter.Seek(i);
+    }
+    
+    #endif  // STORAGE_LEVELDB_DB_SNAPSHOT_H_
