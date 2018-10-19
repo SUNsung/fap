1fdd88aacfbcefbab99093b170ab912b829a5bd5
SUNsung <weatherius@gmail.com>
2018-10-19 18:58:00 +0300

    Rewrite [11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb] File in: 2018-10-19 18:57:59

--- ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
+++ ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
@@ -1,30 +1,41 @@
 
         
-              def failed?
-        error_message.present?
-      end
+            it 'opens the dry run modal even when clicking on the refresh icon' do
+      visit edit_agent_path(agent)
+      find('.agent-dry-run-button span.glyphicon').click
+      expect(page).to have_text('Event to send (Optional)')
+    end
     
-              if @job.blank?
-            @pipeline.coverage
-          else
-            @pipeline.builds
-              .find_by(name: @job)
-              .try(:coverage)
-          end
+      describe '#scenario_label' do
+    it 'creates a scenario label with the scenario name' do
+      expect(scenario_label(scenario)).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Scene</span>'
+      )
+    end
+    
+      describe '.seed' do
+    it 'imports a set of agents to get the user going when they are first created' do
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
+    
+            end
+        it 'restarts dead workers' do
+          stub.instance_of(HuginnScheduler).thread { OpenStruct.new(alive?: false) }
+          mock.instance_of(HuginnScheduler).run!
+          @agent_runner.send(:restart_dead_workers)
         end
       end
     end
   end
-end
-
     
-            def value_text
-          STATUS_RENAME[@status.to_s] || @status.to_s
-        end
+      describe '#values_at' do
+    it 'returns arrays of matching values' do
+      expect(Utils.values_at({ :foo => { :bar => :baz }}, 'foo.bar')).to eq(%w[baz])
+      expect(Utils.values_at({ :foo => [ { :bar => :baz }, { :bar => :bing } ]}, 'foo[*].bar')).to eq(%w[baz bing])
+      expect(Utils.values_at({ :foo => [ { :bar => :baz }, { :bar => :bing } ]}, 'foo[*].bar')).to eq(%w[baz bing])
+    end
     
-        set :csp, false
-    
-        html_filters.push 'clean_local_urls'
-    
-            subtitle = at_css('.hero-subtitle').try(:content)
-        breadcrumbs = css('.breadcrumbs li').map(&:content)[2..-2]
+      it 'accepts post requests with masked X-CSRF-Token header' do
+    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => masked_token)
+    expect(last_response).to be_ok
+  end
