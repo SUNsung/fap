5cafd9740e3bb36981c2291242afe98538085bea
SUNsung <weatherius@gmail.com>
2018-10-20 01:43:53 +0300

    Rewrite [356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb] File in: 2018-10-20 01:43:53

--- ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
+++ ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
@@ -1,94 +1,93 @@
 
         
-          def destroy
-    @filter.destroy
-    redirect_to filters_path
-  end
+                      matches_path?(path, pipeline) &&
+                matches_pattern?(pattern, pipeline)
+            end
+          end
     
-        def set_user
-      @user = Account.find(params[:account_id]).user || raise(ActiveRecord::RecordNotFound)
-    end
-    
-          if @custom_emoji.update(resource_params)
-        log_action :update, @custom_emoji
-        flash[:notice] = I18n.t('admin.custom_emojis.updated_msg')
-      else
-        flash[:alert] =  I18n.t('admin.custom_emojis.update_failed_msg')
+          def store_page(store, id)
+        store.open(path) do
+          if page = new.build_page(id) and store_page?(page)
+            store.write page[:store_path], page[:output]
+            true
+          else
+            false
+          end
+        end
       end
-      redirect_to admin_custom_emojis_path(page: params[:page], **@filter_params)
+    
+        def insert(index, *names)
+      @filters.insert assert_index(index), *filter_const(names)
     end
     
-        def resource_params
-      params.require(:report_note).permit(
-        :content,
-        :report_id
+        def self.run(urls, options = {}, &block)
+      urls = urls.dup
+      requester = new(options)
+      requester.on_response(&block) if block
+      requester.on_response do # cheap hack to ensure root page is processed first
+        if urls
+          requester.request(urls)
+          urls = nil
+        end
+      end
+      requester.request(urls.shift)
+      requester.run
+      requester
+    end
+    
+          base_dir = Pathname.new(normalized_path)
+      base_dir = base_dir.parent unless path.end_with? '/'
+    
+          def mod
+        return @mod if defined?(@mod)
+        @mod = slug[/api\/([\w\-\.]+)\//, 1]
+        @mod.remove! 'angular2.' if @mod
+        @mod
+      end
+    end
+  end
+end
+
+    
+      def_delegators :@logger, :log, :log_status, :log_processing, :log_transform, :log_file_info, :log_processed, :log_http_get_file, :log_http_get_files, :silence_log
+    
+        def pos=(i)
+      @s.pos = str_to_byte_pos i
+      i
+    end
+    
+        # change Microsoft filters to Sass calling convention
+    def replace_ms_filters(file)
+      log_transform
+      file.gsub(
+          /filter: e\(%\('progid:DXImageTransform.Microsoft.gradient\(startColorstr='%d', endColorstr='%d', GradientType=(\d)\)',argb\(([\-$\w]+)\),argb\(([\-$\w]+)\)\)\);/,
+          %Q(filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='\#{ie-hex-str(\\2)}', endColorstr='\#{ie-hex-str(\\3)}', GradientType=\\1);)
       )
     end
     
-      def show
-    @status = status_finder.status
-    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
-  end
-    
-      private
-    
-      def subscription
-    @_subscription ||= @account.subscription(
-      api_subscription_url(@account.id)
-    )
-  end
-    
-        reversible(&method(:up_down))
-  end
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
-  end
+    Then(/^the shared path is created$/) do
+  run_vagrant_command(test_dir_exists(TestApp.shared_path))
 end
     
-        it 'returns an array of likes for a post' do
-      bob.like!(@message)
-      get :index, params: {post_id: @message.id}
-      expect(JSON.parse(response.body).map {|h| h['id'] }).to match_array(@message.likes.map(&:id))
-    end
-    
-      def developer_prefix
-    `xcode-select --print-path`.strip
-  end
-    
-      require 'cocoapods/gem_version'
-  require 'cocoapods-core'
-  require 'cocoapods/config'
-  require 'cocoapods/downloader'
-    
-        def initialize
-      clear
-    end
-    
-        # Gives a Geometry representing the given height and width
-    def initialize(width = nil, height = nil, modifier = nil)
-      if width.is_a?(Hash)
-        options = width
-        @height = options[:height].to_f
-        @width = options[:width].to_f
-        @modifier = options[:modifier]
-        @orientation = options[:orientation].to_i
-      else
-        @height = height.to_f
-        @width  = width.to_f
-        @modifier = modifier
+          def print_deprecation_warnings_if_applicable
+        if using_default_scm?
+          warn_add_git_to_capfile unless scm_plugin_installed?
+        elsif built_in_scm_name?
+          warn_set_scm_is_deprecated
+        elsif third_party_scm_name?
+          warn_third_party_scm_must_be_upgraded
+        end
       end
-    end
     
-        def path
-      @file.respond_to?(:path) ? @file.path : @file
-    end
-    
-        # Returns the pluralized form of the attachment name. e.g.
-    # 'avatars' for an attachment of :avatar
-    def attachment attachment, style_name
-      plural_cache.pluralize_symbol(attachment.name)
-    end
+            def set(key, value)
+          pval = @properties[key]
+          if pval.is_a?(Hash) && value.is_a?(Hash)
+            pval.merge!(value)
+          elsif pval.is_a?(Set) && value.is_a?(Set)
+            pval.merge(value)
+          elsif pval.is_a?(Array) && value.is_a?(Array)
+            pval.concat value
+          else
+            @properties[key] = value
+          end
+        end
