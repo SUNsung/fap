e779f3157ddd8d32d078ee920eb38efd0961f00f
SUNsung <weatherius@gmail.com>
2018-10-19 09:47:22 +0300

    Rewrite [179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py] File in: 2018-10-19 09:47:22

--- pyton/179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py
+++ pyton/179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py
@@ -1,110 +1,208 @@
 
         
-        from {0} import {1}
-    
-    
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
-    proc.sendline(u'fish')
-    return proc
+        
+def _change_tracker(usage_tracker_io, pid):
+    usage_tracker_io.truncate(0)
+    info = {'pid': pid, 'time': 0}
+    json.dump(info, usage_tracker_io)
+    usage_tracker_io.seek(0)
     
     
 @pytest.mark.functional
 def test_without_confirmation(proc, TIMEOUT):
     without_confirmation(proc, TIMEOUT)
-    
-        assert match(command)
-    
-    
-def test_get_new_command():
-    new_command = get_new_command(Command('apt list --upgradable', match_output))
-    assert new_command == 'apt upgrade'
+    history_changed(proc, TIMEOUT, u'echo test')
     
     
-@pytest.mark.parametrize('before, after', [
-    ('brew install sshfs',
-     'brew cask install osxfuse && brew install sshfs')])
-def test_get_new_command(before, after):
-    command = Command(before, output)
-    assert get_new_command(command) == after
+@pytest.mark.parametrize('command, packages', [
+    (Command('vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
+     [('vim', 'main'), ('vim-tiny', 'main')])])
+def test_match(mocker, command, packages):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+      aws help
+  aws <command> help
+  aws <command> <subcommand> help
+aws: error: argument command: Invalid choice, valid choices are:
+    
+    
+    {        Generic:                   '#000000',        # class: 'g'
+        Generic.Deleted:           '#a40000',        # class: 'gd'
+        Generic.Emph:              'italic #000000', # class: 'ge'
+        Generic.Error:             '#ef2929',        # class: 'gr'
+        Generic.Heading:           'bold #000080',   # class: 'gh'
+        Generic.Inserted:          '#00A000',        # class: 'gi'
+        Generic.Output:            '#888',           # class: 'go'
+        Generic.Prompt:            '#745334',        # class: 'gp'
+        Generic.Strong:            'bold #000000',   # class: 'gs'
+        Generic.Subheading:        'bold #800080',   # class: 'gu'
+        Generic.Traceback:         'bold #a40000',   # class: 'gt'
+    }
 
     
+        # Redirection.
+    300: ('multiple_choices',),
+    301: ('moved_permanently', 'moved', '\\o-'),
+    302: ('found',),
+    303: ('see_other', 'other'),
+    304: ('not_modified',),
+    305: ('use_proxy',),
+    306: ('switch_proxy',),
+    307: ('temporary_redirect', 'temporary_moved', 'temporary'),
+    308: ('permanent_redirect',
+          'resume_incomplete', 'resume',),  # These 2 to be removed in 3.0
     
-@pytest.mark.parametrize('command, new_command', [
-    (Command('cargo buid', no_such_subcommand_old), 'cargo build'),
-    (Command('cargo buils', no_such_subcommand), 'cargo build')])
-def test_get_new_command(command, new_command):
-    assert get_new_command(command) == new_command
-
+    # The scheme of the identifier. Typical schemes are ISBN or URL.
+#epub_scheme = ''
     
-        print('Classifier Training')
-    print('===================')
-    accuracy, train_time, test_time = {}, {}, {}
-    for name in sorted(args['estimators']):
-        clf = ESTIMATORS[name]
+        :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
+    
+        # Sometimes, urllib3 only reports its version as 16.1.
+    if len(urllib3_version) == 2:
+        urllib3_version.append('0')
+    
+        def __ne__(self, other):
+        return not self == other
+    
+    '''
+requests.hooks
+~~~~~~~~~~~~~~
+    
+    DEFAULT_REDIRECT_LIMIT = 30
+CONTENT_CHUNK_SIZE = 10 * 1024
+ITER_CHUNK_SIZE = 512
+    
+        # Bypass if not a dictionary (e.g. verify)
+    if not (
+            isinstance(session_setting, Mapping) and
+            isinstance(request_setting, Mapping)
+    ):
+        return request_setting
+    
+    
+def add_dict_to_cookiejar(cj, cookie_dict):
+    '''Returns a CookieJar from a key/value dictionary.
+    
+                self.ip_manager.report_connect_fail(ip, force_remove=True)
+    
+        global pteredor_is_running
+    pteredor_is_running = True
+    server_list = prober.eval_servers()
+    pteredor_is_running = False
+    
+    
+    def specialStateTransition(self, s, input):
+        return -1
+    
+            So, you cna see that the parser walks up call chain looking
+        for the token that was a member of the recovery set.
+    
+            raise NotImplementedError
+    
+    def setCharPositionInLine(self, pos):
+        '''@brief Set the column of the tokens first character,
+    
+    def dirname(s): return split(s)[0]
+def basename(s): return split(s)[1]
+    
+    # Join two paths, normalizing and eliminating any symbolic links
+# encountered in the second path.
+def _joinrealpath(path, rest, seen):
+    if isinstance(path, bytes):
+        sep = b'/'
+        curdir = b'.'
+        pardir = b'..'
+    else:
+        sep = '/'
+        curdir = '.'
+        pardir = '..'
+    
+        def test_isdir(self):
+        filename = support.TESTFN
+        bfilename = os.fsencode(filename)
+        self.assertIs(self.pathmodule.isdir(filename), False)
+        self.assertIs(self.pathmodule.isdir(bfilename), False)
+    
+                os.symlink(ABSTFN+'1', ABSTFN+'2')
+            os.symlink(ABSTFN+'2', ABSTFN+'1')
+            self.assertEqual(realpath(ABSTFN+'1'), ABSTFN+'1')
+            self.assertEqual(realpath(ABSTFN+'2'), ABSTFN+'2')
+    
+            if MS_WINDOWS:
+            # PYTHONLEGACYWINDOWSFSENCODING disables the UTF-8 mode
+            # and has the priority over PYTHONUTF8
+            out = self.get_output('-X', 'utf8', '-c', code, PYTHONUTF8='1',
+                                  PYTHONLEGACYWINDOWSFSENCODING='1')
+            self.assertEqual(out, '0')
+    
+        def test_synopsis_sourceless_empty_doc(self):
+        with test.support.temp_cwd() as test_dir:
+            init_path = os.path.join(test_dir, 'foomod42.py')
+            cached_path = importlib.util.cache_from_source(init_path)
+            with open(init_path, 'w') as fobj:
+                fobj.write('foo = 1')
+            py_compile.compile(init_path)
+            synopsis = pydoc.synopsis(init_path, {})
+            self.assertIsNone(synopsis)
+            synopsis_cached = pydoc.synopsis(cached_path, {})
+            self.assertIsNone(synopsis_cached)
+    
+        def testReadMonkeyMultiStream(self):
+        # Test BZ2File.read() on a multi-stream archive where a stream
+        # boundary coincides with the end of the raw read buffer.
+        buffer_size = _compression.BUFFER_SIZE
+        _compression.BUFFER_SIZE = len(self.DATA)
         try:
-            clf.set_params(random_state=0)
-        except (TypeError, ValueError):
-            pass
+            self.createTempFile(streams=5)
+            with BZ2File(self.filename) as bz2f:
+                self.assertRaises(TypeError, bz2f.read, float())
+                self.assertEqual(bz2f.read(), self.TEXT * 5)
+        finally:
+            _compression.BUFFER_SIZE = buffer_size
     
-        timings = []
-    for exponent in range(args.log_min_problem_size,
-                          args.log_max_problem_size):
-        n = 10 ** exponent
-        Y = DATASET_GENERATORS[args.dataset](n)
-        time_per_iteration = \
-            [bench_isotonic_regression(Y) for i in range(args.iterations)]
-        timing = (n, np.mean(time_per_iteration))
-        timings.append(timing)
-    
-    random_state = 2  # to control the random selection of anomalies in SA
-    
-    faces = fetch_lfw_people(resize=.2, min_faces_per_person=5)
-# limit dataset to 5000 people (don't care who they are!)
-X = faces.data[:5000]
-n_samples, h, w = faces.images.shape
-n_features = X.shape[1]
-    
-    from sklearn.linear_model import lars_path, orthogonal_mp
-from sklearn.datasets.samples_generator import make_sparse_coded_signal
-    
-    
-if __name__ == '__main__':
-    from mpl_toolkits.mplot3d import axes3d  # register the 3d projection
-    import matplotlib.pyplot as plt
-    
-        fig = plt.figure('scikit-learn sample w/o replacement benchmark results')
-    plt.title('n_population = %s, n_times = %s' %
-              (opts.n_population, opts.n_times))
-    ax = fig.add_subplot(111)
-    for name in sampling_algorithm:
-        ax.plot(ratio, time[name], label=name)
-    
-                std = X_train.std(axis=0)
-            mean = X_train.mean(axis=0)
-            X_train = (X_train - mean) / std
-            X_test = (X_test - mean) / std
-    
-    
-def benchmark_dense_predict():
-    for _ in range(300):
-        clf.predict(X_test)
-    
-                short_text_filename = os.path.join(short_text_lang_folder,
-                                               '%s_%04d.txt' % (lang, j))
-            print('Writing %s' % short_text_filename)
-            open(short_text_filename, 'wb').write(
-                small_content.encode('utf-8', 'ignore'))
-            j += 1
-            if j >= 1000:
+            it = pool.imap(f, list(range(10)))
+        for i in range(10):
+            try:
+                x = next(it)
+            except ZeroDivisionError:
+                if i == 5:
+                    pass
+            except StopIteration:
                 break
+            else:
+                if i == 5:
+                    raise AssertionError('expected ZeroDivisionError')
     
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (14 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
+        @staticmethod
+    def _static_method_2():
+        print('executed method 2!')
+    
+        def update(self):
+        print('Updating the test results in Database')
+        time.sleep(0.1)
+    
+    ###  OUTPUT ###
+# Before subclassing:
+# BaseRegisteredClass
+# After subclassing:
+# BaseRegisteredClass
+# ClassRegistree
+
+    
+    
+class Specification(object):
+    def and_specification(self, candidate):
+        raise NotImplementedError()
+    
+        def toggle_amfm(self):
+        self.state.toggle_amfm()
