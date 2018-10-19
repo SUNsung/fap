e96a33c952e8ed7acc23130d139025397b7de7da
SUNsung <weatherius@gmail.com>
2018-10-19 13:23:24 +0300

    Rewrite [288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py] File in: 2018-10-19 13:23:24

--- pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
+++ pyton/288QKMJSg0BE28y5l2IlotIfU280en1k8PXX3s28v.S.py
@@ -1,60 +1,215 @@
 
         
-            def __init__(self, *args, **kwargs):
-        '''Initialize RequestException with `request` and `response` objects.'''
-        response = kwargs.pop('response', None)
-        self.response = response
-        self.request = kwargs.pop('request', None)
-        if (response is not None and not self.request and
-                hasattr(response, 'request')):
-            self.request = self.response.request
-        super(RequestException, self).__init__(*args, **kwargs)
+              for n in xrange(FLAGS.number_epochs):
+        print('Epoch number: %d' % n)
+        # print('Percent done: %.2f' % float(n) / float(FLAGS.number_epochs))
+        iterator = get_iterator(data)
+        for x, y, _ in iterator:
+          if FLAGS.eval_language_model:
+            is_present_rate = 0.
+          else:
+            is_present_rate = FLAGS.is_present_rate
+          tf.logging.info(
+              'Evaluating on is_present_rate=%.3f.' % is_present_rate)
     
-            This attribute checks if the status code of the response is between
-        400 and 600 to see if there was a client error or a server error. If
-        the status code, is between 200 and 400, this will return True. This
-        is **not** a check to see if the response code is ``200 OK``.
-        '''
-        return self.ok
-    
-    # datasets available: ['http', 'smtp', 'SA', 'SF', 'shuttle', 'forestcover']
-datasets = ['http', 'smtp', 'SA', 'SF', 'shuttle', 'forestcover']
-    
-        max_it = len(samples_range) * len(features_range)
-    for n_samples in samples_range:
-        for n_features in features_range:
-            it += 1
-            print('==============================')
-            print('Iteration %03d of %03d' % (it, max_it))
-            print('==============================')
-            print()
-            data = nr.randint(-50, 51, (n_samples, n_features))
-    
-    X -= X.mean(axis=0)
-X /= X.std(axis=0)
+        # Maximize reward.
+    gen_grads = tf.gradients(-final_gen_reward, gen_vars)
+    gen_grads_clipped, _ = tf.clip_by_global_norm(gen_grads,
+                                                  FLAGS.grad_clipping)
+    maximize_op = gen_optimizer.apply_gradients(
+        zip(gen_grads_clipped, gen_vars), global_step=global_step)
     
     
-def compute_bench(samples_range, features_range):
+def hash_function(input_tuple):
+  '''Hash function for a tuple.'''
+  return hash(input_tuple)
+    
+            # Keyword arguments > stream.encoding > default utf8
+        if self.stdin_encoding is None:
+            self.stdin_encoding = getattr(
+                self.stdin, 'encoding', None) or 'utf8'
+        if self.stdout_encoding is None:
+            actual_stdout = self.stdout
+            if is_windows:
+                # noinspection PyUnresolvedReferences
+                from colorama import AnsiToWin32
+                if isinstance(self.stdout, AnsiToWin32):
+                    actual_stdout = self.stdout.wrapped
+            self.stdout_encoding = getattr(
+                actual_stdout, 'encoding', None) or 'utf8'
+    
+        @property
+    def content_type(self):
+        '''Return the message content type.'''
+        ct = self._orig.headers.get('Content-Type', '')
+        if not isinstance(ct, str):
+            ct = ct.decode('utf8')
+        return ct
+    
+        def format_headers(self, headers):
+        '''Return processed `headers`
     
     
-def plot_time_vs_s(time, norm, point_labels, title):
-    plt.figure()
-    colors = ['g', 'b', 'y']
-    for i, l in enumerate(sorted(norm.keys())):
-        if l != 'fbpca':
-            plt.plot(time[l], norm[l], label=l, marker='o', c=colors.pop())
-        else:
-            plt.plot(time[l], norm[l], label=l, marker='^', c='red')
+class BasicAuthPlugin(BuiltinAuthPlugin):
     
-    # Split data in train set and test set
-n_samples = X.shape[0]
-X_train, y_train = X[:n_samples // 2], y[:n_samples // 2]
-X_test, y_test = X[n_samples // 2:], y[n_samples // 2:]
-print('test data sparsity: %f' % sparsity_ratio(X_test))
     
-    print('='*80 + '\n#' + '    Text vectorizers benchmark' + '\n' + '='*80 + '\n')
-print('Using a subset of the 20 newsrgoups dataset ({} documents).'
-      .format(len(text)))
-print('This benchmarks runs in ~20 min ...')
+def test_headers_unset(httpbin_both):
+    r = http('GET', httpbin_both + '/headers')
+    assert 'Accept' in r.json['headers']  # default Accept present
     
-    mu_second = 0.0 + 10 ** 6  # number of microseconds in a second
+        deadline = URLFETCH_TIMEOUT
+    validate_certificate = bool(int(kwargs.get('validate', 0)))
+    accept_encoding = headers.get('Accept-Encoding', '')
+    errors = []
+    for i in xrange(int(kwargs.get('fetchmax', URLFETCH_MAX))):
+        try:
+            response = urlfetch.fetch(url, payload, fetchmethod, headers, allow_truncated=False, follow_redirects=False, deadline=deadline, validate_certificate=validate_certificate)
+            break
+        except apiproxy_errors.OverQuotaError as e:
+            time.sleep(5)
+        except urlfetch.DeadlineExceededError as e:
+            errors.append('%r, deadline=%s' % (e, deadline))
+            logging.error('DeadlineExceededError(deadline=%s, url=%r)', deadline, url)
+            time.sleep(1)
+            deadline = URLFETCH_TIMEOUT * 2
+        except urlfetch.DownloadError as e:
+            errors.append('%r, deadline=%s' % (e, deadline))
+            logging.error('DownloadError(deadline=%s, url=%r)', deadline, url)
+            time.sleep(1)
+            deadline = URLFETCH_TIMEOUT * 2
+        except urlfetch.ResponseTooLargeError as e:
+            errors.append('%r, deadline=%s' % (e, deadline))
+            response = e.response
+            logging.error('ResponseTooLargeError(deadline=%s, url=%r) response(%r)', deadline, url, response)
+            m = re.search(r'=\s*(\d+)-', headers.get('Range') or headers.get('range') or '')
+            if m is None:
+                headers['Range'] = 'bytes=0-%d' % int(kwargs.get('fetchmaxsize', URLFETCH_MAXSIZE))
+            else:
+                headers.pop('Range', '')
+                headers.pop('range', '')
+                start = int(m.group(1))
+                headers['Range'] = 'bytes=%s-%d' % (start, start+int(kwargs.get('fetchmaxsize', URLFETCH_MAXSIZE)))
+            deadline = URLFETCH_TIMEOUT * 2
+        except urlfetch.SSLCertificateError as e:
+            errors.append('%r, should validate=0 ?' % e)
+            logging.error('%r, deadline=%s', e, deadline)
+        except Exception as e:
+            errors.append(str(e))
+            if i == 0 and method == 'GET':
+                deadline = URLFETCH_TIMEOUT * 2
+    else:
+        start_response('500 Internal Server Error', [('Content-Type', 'text/html')])
+        error_string = '<br />\n'.join(errors)
+        if not error_string:
+            logurl = 'https://appengine.google.com/logs?&app_id=%s' % os.environ['APPLICATION_ID']
+            error_string = 'Internal Server Error. <p/>try <a href='javascript:window.location.reload(true);'>refresh</a> or goto <a href='%s' target='_blank'>appengine.google.com</a> for details' % logurl
+        yield message_html('502 Urlfetch Error', 'Python Urlfetch Error: %r' % method,  error_string)
+        raise StopIteration
+    
+    # begin[licence]
+#
+# [The 'BSD licence']
+# Copyright (c) 2005-2008 Terence Parr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# end[licence]
+    
+        :param bool condition: If ``False``, the test will be skipped
+    :param str reason: the reason for skipping the test
+    
+    
+from certbot import errors
+from certbot.plugins import common
+    
+            self.assertFalse(self.addr_defined.conflicts(self.addr1))
+        self.assertFalse(self.addr_defined.conflicts(self.addr2))
+        self.assertFalse(self.addr_defined.conflicts(self.addr))
+        self.assertFalse(self.addr_defined.conflicts(self.addr_default))
+    
+        #call extractor decided by sourceId
+    if sourceType == 'sina':
+        sina_download_by_vid(sourceId, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    elif sourceType == 'youku':
+        youku_download_by_vid(sourceId, title=title, output_dir=output_dir, merge=merge, info_only=info_only, **kwargs)
+    elif sourceType == 'tudou':
+        tudou_download_by_iid(sourceId, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    elif sourceType == 'qq':
+        qq_download_by_vid(sourceId, title, True, output_dir=output_dir, merge=merge, info_only=info_only)
+    elif sourceType == 'letv':
+        letvcloud_download_by_vu(sourceId, '2d8c027396', title, output_dir=output_dir, merge=merge, info_only=info_only)
+    elif sourceType == 'zhuzhan':
+        #As in Jul.28.2016, Acfun is using embsig to anti hotlink so we need to pass this
+#In Mar. 2017 there is a dedicated ``acfun_proxy'' in youku cloud player
+#old code removed
+        url = 'http://www.acfun.cn/v/ac' + vid
+        yk_streams = youku_acfun_proxy(info['sourceId'], info['encode'], url)
+        seq = ['mp4hd3', 'mp4hd2', 'mp4hd', 'flvhd']
+        for t in seq:
+            if yk_streams.get(t):
+                preferred = yk_streams[t]
+                break
+#total_size in the json could be incorrect(F.I. 0)
+        size = 0
+        for url in preferred[0]:
+            _, _, seg_size = url_info(url)
+            size += seg_size
+#fallback to flvhd is not quite possible
+        print_info(site_info, title, 'mp4', size)
+        if not info_only:
+            download_urls(preferred[0], title, 'mp4', size, output_dir=output_dir, merge=merge)
+    else:
+        raise NotImplementedError(sourceType)
+    
+    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
+    host = r1(r'host=([^&]*)', html)
+    assert host
+    type = r1(r'videofiletype=([^&]*)', html)
+    assert type
+    vid = r1(r'&stream_name=([^&]*)', html)
+    assert vid
+    dir_str = r1(r'&dir=([^&]*)', html).strip()
+    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
+    _, ext, size = url_info(url)
+    print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+        if '_text' in dictified['video'][0]['file'][0]:  #link exist
+        video_dict['links'] = [i['file'][0]['_text'].strip() for i in dictified['video']]
+    
+        html = get_content(rebuilt_url(url))
+    info = json.loads(match1(html, r'qualities':({.+?}),''))
+    title = match1(html, r''video_title'\s*:\s*'([^']+)'') or \
+            match1(html, r''title'\s*:\s*'([^']+)'')
+    title = unicodize(title)
+    
+    def ehow_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+	
+	assert re.search(r'http://www.ehow.com/video_', url), 'URL you entered is not supported'
+    
+    __all__ = ['facebook_download']
+    
+        title = match1(html, r'&title=([^&]+)')
