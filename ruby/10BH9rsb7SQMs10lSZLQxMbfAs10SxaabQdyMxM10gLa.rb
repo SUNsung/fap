fe06fb78e957ee1a7760ae2f87168ef20a914c4c
SUNsung <weatherius@gmail.com>
2018-10-20 23:36:57 +0300

    Rewrite [10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb] File in: 2018-10-20 23:36:56

--- ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
+++ ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
@@ -1,116 +1,35 @@
 
         
-            Pubsubhubbub::UnsubscribeWorker.perform_async(signed_request_account.id) if signed_request_account.subscribed?
-    DeliveryFailureTracker.track_inverse_success!(signed_request_account)
-  end
+            private
     
-          @form         = Form::StatusBatch.new(form_status_batch_params.merge(current_account: current_account, action: action_from_button))
-      flash[:alert] = I18n.t('admin.statuses.failed_to_execute') unless @form.save
-    
-      def status_finder
-    StatusFinder.new(params[:url])
-  end
-    
-      #
-  # Mounts a directory or resource as being serviced by the supplied handler.
-  #
-  def mount(root, handler, long_call = false, *args)
-    resources[root] = [ handler, long_call, args ]
-  end
-    
-    require 'rex/proto/ipmi/utils'
-    
-                encoded
-          end
-    
-              def initialize(options = {})
-            self.class.attributes.each do |attr|
-              if options.has_key?(attr)
-                m = (attr.to_s + '=').to_sym
-                self.send(m, options[attr])
-              end
-            end
-          end
-    
-              # Decodes the nonce field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_nonce(input)
-            input.value[0].value.to_i
-          end
-    
-              # Encodes the msg_type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_msg_type
-            bn = OpenSSL::BN.new(msg_type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-              # Decodes the Rex::Proto::Kerberos::Model::KdcResponse from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KdcResponse, invalid input'
-            end
-    
-    Then(/^the tasks folder is created$/) do
-  path = TestApp.test_app_path.join('lib/capistrano/tasks')
-  expect(Dir.exist?(path)).to be true
-end
-    
-    World(VagrantHelpers)
-
-    
-        def setup_filters
-      @filters = cmdline_filters
-      @filters += @custom_filters if @custom_filters
-      @filters << Filter.new(:role, ENV['ROLES']) if ENV['ROLES']
-      @filters << Filter.new(:host, ENV['HOSTS']) if ENV['HOSTS']
-      fh = fetch_for(:filter, {}) || {}
-      @filters << Filter.new(:host, fh[:hosts]) if fh[:hosts]
-      @filters << Filter.new(:role, fh[:roles]) if fh[:roles]
-      @filters << Filter.new(:host, fh[:host]) if fh[:host]
-      @filters << Filter.new(:role, fh[:role]) if fh[:role]
+        def shared_mixins
+      @shared_mixins ||= begin
+        log_status '  Reading shared mixins from mixins.less'
+        CLASSES_TO_MIXINS + read_mixins(read_files('less', bootstrap_less_files.grep(/mixins\//)).values.join('\n'),
+                                        nested: NESTED_MIXINS)
+      end
     end
     
-      # the install logic below support installing multiple plugins with each a version specification
-  # but the argument parsing does not support it for now so currently if specifying --version only
-  # one plugin name can be also specified.
-  def execute
-    # Turn off any jar dependencies lookup when running with `--local`
-    ENV['JARS_SKIP'] = 'true' if local?
+        def log_http_get_files(files, from, cached = false)
+      return if files.empty?
+      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
+      if cached
+        puts dark green s
+      else
+        puts dark cyan s
+      end
+    end
     
-        puts('Packaging plugins for offline usage')
-    
-      private
-    
-    class LogStash::PluginManager::Update < LogStash::PluginManager::Command
-  REJECTED_OPTIONS = [:path, :git, :github]
-  # These are local gems used by LS and needs to be filtered out of other plugin gems
-  NON_PLUGIN_LOCAL_GEMS = ['logstash-core', 'logstash-core-plugin-api']
-    
-    Gem::Specification.new do |gem|
-  gem.authors       = ['Elastic']
-  gem.email         = ['info@elastic.co']
-  gem.description   = %q{Logstash plugin API}
-  gem.summary       = %q{Define the plugin API that the plugin need to follow.}
-  gem.homepage      = 'http://www.elastic.co/guide/en/logstash/current/index.html'
-  gem.license       = 'Apache License (2.0)'
-    
-            it 'should raise a configuration error' do
-          expect { subject.required_strings }.to raise_error(LogStash::ConfigurationError)
-        end
+          # Checks whether the `if` node has at least one `elsif` branch. Returns
+      # true if this `if` node itself is an `elsif`.
+      #
+      # @return [Boolean] whether the `if` node has at least one `elsif` branch
+      def elsif_conditional?
+        else_branch && else_branch.if_type? && else_branch.elsif?
       end
     
-        not_same_pipeline_id = described_class.new(source, :another_pipeline, unordered_config_parts, settings)
-    expect(subject).not_to eq(not_same_pipeline_id)
-  end
+            def valid_argument_types?
+          [first, second].all? do |argument|
+            argument.pair_type? || argument.kwsplat_type?
+          end
+        end
