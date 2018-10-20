4d234aa700133e046c140fa8cea0339616843de0
SUNsung <weatherius@gmail.com>
2018-10-20 21:56:01 +0300

    Rewrite [40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp] File in: 2018-10-20 21:56:01

--- cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
+++ cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
@@ -1,315 +1,213 @@
 
         
-        namespace atom {
+        #include <cctype>
+    
+    // _fi_sep determines the directory separator, either '\\' or '/'
+BOOST_REGEX_DECL extern const char* _fi_sep;
+    
+    #ifdef BOOST_HAS_ABI_HEADERS
+#  include BOOST_ABI_SUFFIX
+#endif
+    
+    BOOST_MPL_HAS_XXX_TRAIT_DEF(const_iterator)
+    
+    
+namespace boost{
     }
     
     
-    {}  // namespace api
     
     
-    {}  // namespace atom
+    {} // namespace boost
     
-    
-    {}  // namespace api
-    
-      friend class SubstitutionMap;
-    
-      char *oldAllocation = Allocated;
-  char *oldBegin = Begin;
-  std::size_t oldSize = (std::size_t) (End - oldBegin);
-    
-        {
-      Out << Indent;
-      if (childKind == ChildKind::Root) {
-        Out << '+- ';
-      } else if (childKind == ChildKind::Left) {
-        Out << '/- ';
-      } else if (childKind == ChildKind::Right) {
-        Out << '\\- ';
-      } else if (childKind == ChildKind::Further) {
-        Out << '\\-> ';
-      }
-      PrintNodeData(Out, node);
-      Out << '\n';
-    }
-    
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-      bool SubtaskFailed = false;
-    
-      PyObject* as_pyobject() const { return reinterpret_cast<PyObject*>(ptr_); }
-    
-    AnyMetadata::AnyMetadata(UrlType* type_url, ValueType* value)
-    : type_url_(type_url), value_(value) {
-}
-    
-    bool RunProtoCompiler(const string& filename,
-                      const string& plugin_specific_args,
-                      CommandLineInterface* cli, FileDescriptorProto* file) {
-  cli->SetInputsAreProtoPathRelative(true);
-    }
-    
-    
-    {
-    {
-    {}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-    void MessageOneofFieldGenerator::GenerateParsingCode(io::Printer* printer) {
-  // TODO(jonskeet): We may be able to do better than this
-  printer->Print(
-    variables_,
-    '$type_name$ subBuilder = new $type_name$();\n'
-    'if ($has_property_check$) {\n'
-    '  subBuilder.MergeFrom($property_name$);\n'
-    '}\n'
-    'input.ReadMessage(subBuilder);\n' // No support of TYPE_GROUP
-    '$property_name$ = subBuilder;\n');
-}
-    
-    #include <google/protobuf/stubs/common.h>
-namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-    }
-    }
-    }
-    }
-    
-    void PrimitiveOneofFieldGenerator::WriteToString(io::Printer* printer) {
-  printer->Print(variables_,
-    'PrintField(\'$descriptor_name$\', $has_property_check$, $oneof_name$_, writer);\n');
-}
-    
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/plugin.h>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/descriptor.pb.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/stubs/strutil.h>
-    
-    #include <mxnet/op_attr_types.h>
-#include <mxnet/graph_attr_types.h>
-#include <mxnet/c_api.h>
-#include <nnvm/symbolic.h>
-#include <nnvm/op.h>
-#include <nnvm/graph.h>
-#include <vector>
-#include <atomic>
-#include <utility>
-#include <string>
-#include <unordered_map>
-    
-    struct CaffeDataParam : public dmlc::Parameter<CaffeDataParam> {
-  /*! \brief protobuf text */
-  ::caffe::LayerParameter prototxt;
-  /*! \brief number of iterations per epoch */
-  int num_examples;
-  /*! \brief data mode */
-  bool flat;
-    }
-    
-    
-    {
-    {/*! \brief typedef the factory function of data iterator */
-typedef std::function<ImageAugmenter *()> ImageAugmenterFactory;
-/*!
- * \brief Registry entry for DataIterator factory functions.
- */
-struct ImageAugmenterReg
-    : public dmlc::FunctionRegEntryBase<ImageAugmenterReg,
-                                        ImageAugmenterFactory> {
+    template <class BaseT>
+struct default_wrapper : public BaseT
+{
+   typedef typename BaseT::char_type char_type;
+   std::string error_string(::boost::regex_constants::error_type e)const
+   {
+      return ::boost::BOOST_REGEX_DETAIL_NS::get_default_error_string(e);
+   }
+   ::boost::regex_constants::syntax_type syntax_type(char_type c)const
+   {
+      return ((c & 0x7f) == c) ? get_default_syntax_type(static_cast<char>(c)) : ::boost::regex_constants::syntax_char;
+   }
+   ::boost::regex_constants::escape_syntax_type escape_syntax_type(char_type c)const
+   {
+      return ((c & 0x7f) == c) ? get_default_escape_syntax_type(static_cast<char>(c)) : ::boost::regex_constants::escape_type_identity;
+   }
+   int toi(const char_type*& p1, const char_type* p2, int radix)const
+   {
+      return ::boost::BOOST_REGEX_DETAIL_NS::global_toi(p1, p2, radix, *this);
+   }
+   char_type translate(char_type c, bool icase)const
+   {
+      return (icase ? this->translate_nocase(c) : this->translate(c));
+   }
+   char_type translate(char_type c)const
+   {
+      return BaseT::translate(c);
+   }
+   char_type tolower(char_type c)const
+   {
+      return ::boost::BOOST_REGEX_DETAIL_NS::global_lower(c);
+   }
+   char_type toupper(char_type c)const
+   {
+      return ::boost::BOOST_REGEX_DETAIL_NS::global_upper(c);
+   }
 };
-//--------------------------------------------------------------
-// The following part are API Registration of Iterators
-//--------------------------------------------------------------
-/*!
- * \brief Macro to register image augmenter
- *
- * \code
- * // example of registering a mnist iterator
- * REGISTER_IMAGE_AUGMENTER(aug_default)
- * .describe('default augmenter')
- * .set_body([]() {
- *     return new DefaultAugmenter();
- *   });
- * \endcode
- */
-#define MXNET_REGISTER_IMAGE_AUGMENTER(name)                            \
-  DMLC_REGISTRY_REGISTER(::mxnet::io::ImageAugmenterReg, ImageAugmenterReg, name)
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_USE_OPENCV
     
-    bool get_png_size(const uint8_t* data, uint32_t data_size, int64_t *width, int64_t *height) {
-  if (data[0] == 0x89 && data[1] == 0x50 && data[2] ==0x4E && data[3] == 0x47) {
-    uint8_t const* p = data + 16;
-    *width = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
-    p += 4;
-    *height = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
-    return true;
-  } else {
+      inline void SkipLine() {
+    do {
+      ch_buf_ = this->GetChar();
+    } while (ch_buf_ != EOF && ch_buf_ != '\n' && ch_buf_ != '\r');
+  }
+    
+    struct GBTreeModel {
+  explicit GBTreeModel(bst_float base_margin) : base_margin(base_margin) {}
+  void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) {
+    // initialize model parameters if not yet been initialized.
+    if (trees.size() == 0) {
+      param.InitAllowUnknown(cfg);
+    }
+  }
+    }
+    
+    
+    {    return false;
+}
+    
+    bool ActionInterval::sendUpdateEventToScript(float dt, Action *actionObject)
+{
+#if CC_ENABLE_SCRIPT_BINDING
+    if (_scriptType == kScriptTypeJavascript)
+    {
+        if (ScriptEngineManager::sendActionEventToJS(actionObject, kActionUpdate, (void *)&dt))
+            return true;
+    }
+#endif
+    return false;
+}
+    
+    /** @class BezierBy
+ * @brief An action that moves the target with a cubic Bezier curve by a certain distance.
+ */
+class CC_DLL BezierBy : public ActionInterval
+{
+public:
+    /** Creates the action with a duration and a bezier configuration.
+     * @param t Duration time, in seconds.
+     * @param c Bezier config.
+     * @return An autoreleased BezierBy object.
+     * @code
+     * When this function bound to js or lua,the input params are changed.
+     * in js: var create(var t,var table)
+     * in lua: local create(local t, local table)
+     * @endcode
+     */
+    static BezierBy* create(float t, const ccBezierConfig& c);
+    }
+    
+    void ProgressFromTo::update(float time)
+{
+    ((kProgressTimerCast)(_target))->setPercentage(_from + (_to - _from) * time);
+}
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    /**
+ * @file
+ * @brief Defines the CanClientFactory class.
+ */
+    
+    bool EsdCanClient::Init(const CANCardParameter &parameter) {
+  if (!parameter.has_channel_id()) {
+    AERROR << 'Init CAN failed: parameter does not have channel id. The '
+              'parameter is '
+           << parameter.DebugString();
     return false;
   }
+  port_ = parameter.channel_id();
+  return true;
 }
     
-        LearnerPtr MomentumSGDLearner(const vector<Parameter>& parameters,
-                                  const LearningRateSchedule& learningRateSchedule,
-                                  const MomentumSchedule& momentumSchedule,
-                                  bool unitGain,
-                                  AdditionalLearningOptions additionalOptions /*= AdditionalLearningOptions()*/)
-    {
-        return MakeSharedObject<LearnerMomentumSGD>(parameters, learningRateSchedule, momentumSchedule, unitGain, additionalOptions, 1);
+    #include <string>
+#include <vector>
+#include 'esd_can/include/ntcan.h'
+#include 'gflags/gflags.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/can_client.h'
+#include 'modules/drivers/canbus/common/canbus_consts.h'
+#include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
+    
+    TEST(HermesCanClient, receiver) {
+  CANCardParameter param;
+  param.set_brand(CANCardParameter::HERMES_CAN);
+  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
+  HermesCanClient hermes_can;
+  EXPECT_TRUE(hermes_can.Init(param));
     }
     
-            if (sectionShape.Rank() > m_maskShape.Rank())
-            LogicError('NDMask::MaskSection: The section shape '%S' rank (%d) must be <= rank (%d) of 'this' mask.', sectionShape.AsString().c_str(), (int)sectionShape.Rank(), (int)m_maskShape.Rank());
-    
-        void ProgressWriter::UpdateTraining(size_t samples, const ValuePtr& accumulatedLoss,
-                                        const ValuePtr& accumulatedMetric)
-    {
-        m_training->Update(samples, accumulatedLoss, accumulatedMetric,
-            [this](const std::pair<size_t, size_t> samples, std::pair<size_t, size_t> updates,
-                   const std::pair<double, double> aggregateLoss, std::pair<double, double> aggregateMetric)
-            {
-                OnWriteTrainingUpdate(samples, updates, aggregateLoss, aggregateMetric);
-            });
-        OnTrainingUpdateEnd();
+      for (int32_t i = 0; i < *frame_num && i < MAX_CAN_RECV_FRAME_LEN; ++i) {
+    CanFrame cf;
+    int ret = read(dev_handler_, &recv_frames_[i], sizeof(recv_frames_[i]));
     }
     
-    
-    {                unpackedShape = unpackedShape.AppendShape({ packedDataLayout->GetNumSequences() });
-            }
-            else if (!sampleDynamicAxes.empty())
-                LogicError('A PackedValue object that does not have a layout cannot have any dynamic axes.');
-    
-    // some older code uses this namespace
-namespace DebugUtil
-{
-    void PrintCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
+    namespace apollo {
+namespace drivers {
+namespace canbus {
+namespace can {
     }
-    
-    #endif // BOOST_NO_WREGEX
-    
-    namespace boost{
-   namespace BOOST_REGEX_DETAIL_NS{
+    }
     }
     }
     
+      /**
+   * @brief Initialize by a CAN client, message manager.
+   * @param can_client The CAN client to use for receiving messages.
+   * @param pt_manager The message manager which can parse and
+   *        get protocol data by message id.
+   * @param enable_log If log the essential information during running.
+   * @return An error code indicating the status of this initialization.
+   */
+  common::ErrorCode Init(CanClient *can_client,
+                         MessageManager<SensorType> *pt_manager,
+                         bool enable_log);
     
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
+#include 'modules/drivers/canbus/can_comm/protocol_data.h'
     
-    #ifdef BOOST_MSVC
-#pragma warning(push)
-#pragma warning(disable: 4103)
-#endif
-#ifdef BOOST_HAS_ABI_HEADERS
-#  include BOOST_ABI_PREFIX
-#endif
-#ifdef BOOST_MSVC
-#pragma warning(pop)
-#endif
+    /**
+ * @struct CheckIdArg
+ *
+ * @brief this struct include data for check ids.
+ */
+struct CheckIdArg {
+  int64_t period = 0;
+  int64_t real_period = 0;
+  int64_t last_time = 0;
+  int32_t error_count = 0;
+};
     
-       format_perl = 0,                                  /* perl style replacement */
-   format_default = 0,                               /* ditto. */
-   format_sed = match_max << 1,                      /* sed style replacement. */
-   format_all = format_sed << 1,                     /* enable all extentions to sytax. */
-   format_no_copy = format_all << 1,                 /* don't copy non-matching segments. */
-   format_first_only = format_no_copy << 1,          /* Only replace first occurance. */
-   format_is_if = format_first_only << 1,            /* internal use only. */
-   format_literal = format_is_if << 1                /* treat string as a literal */
+      /*
+   * @brief reset the protocol data
+   */
+  virtual void Reset();
     
-    template <class BidiIterator, class Allocator, class traits>
-bool perl_matcher<BidiIterator, Allocator, traits>::unwind_recursion_stopper(bool)
-{
-   boost::BOOST_REGEX_DETAIL_NS::inplace_destroy(m_backup_state++);
-   pstate = 0;   // nothing left to search
-   return false; // end of stack nothing more to search
+    TEST(ByteTest, GetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  EXPECT_EQ(0x05, value.get_byte(1, 3));
+  EXPECT_EQ(0x01, value.get_byte(1, 1));
+  EXPECT_EQ(0x00, value.get_byte(8, 1));
+  EXPECT_EQ(0x00, value.get_byte(-1, 1));
+  EXPECT_EQ(0x1A, value.get_byte(0, 10));
 }
-    
-    template <class BidiIterator, class Allocator, class traits>
-bool perl_matcher<BidiIterator, Allocator, traits>::backtrack_till_match(std::size_t count)
-{
-#ifdef BOOST_MSVC
-#pragma warning(push)
-#pragma warning(disable:4127)
-#endif
-   if(!m_can_backtrack)
-      return false;
-   if((m_match_flags & match_partial) && (position == last))
-      m_has_partial_match = true;
-    }
-    
-    template <class ST, class SA, class Allocator, class charT, class traits>
-inline bool regex_match(const std::basic_string<charT, ST, SA>& s, 
-                 match_results<typename std::basic_string<charT, ST, SA>::const_iterator, Allocator>& m, 
-                 const basic_regex<charT, traits>& e, 
-                 match_flag_type flags = match_default)
-{
-   return regex_match(s.begin(), s.end(), m, e, flags);
-}
-template <class charT, class traits>
-inline bool regex_match(const charT* str, 
-                        const basic_regex<charT, traits>& e, 
-                        match_flag_type flags = match_default)
-{
-   match_results<const charT*> m;
-   return regex_match(str, str + traits::length(str), m, e, flags | regex_constants::match_any);
-}
-    
-    
-
-    
-        // Cleanup
-    ImGui_ImplOpenGL2_Shutdown();
-    ImGui_ImplFreeGLUT_Shutdown();
-    ImGui::DestroyContext();
-    
-        // Create window with graphics context
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_DisplayMode current;
-    SDL_GetCurrentDisplayMode(0, &current);
-    SDL_Window* window = SDL_CreateWindow('Dear ImGui SDL2+OpenGL3 example', SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    SDL_GL_SetSwapInterval(1); // Enable vsync
-    
-        if (!g_pIB || g_IndexBufferSize < draw_data->TotalIdxCount)
-    {
-        if (g_pIB) { g_pIB->Release(); g_pIB = NULL; }
-        g_IndexBufferSize = draw_data->TotalIdxCount + 10000;
-        D3D10_BUFFER_DESC desc;
-        memset(&desc, 0, sizeof(D3D10_BUFFER_DESC));
-        desc.Usage = D3D10_USAGE_DYNAMIC;
-        desc.ByteWidth = g_IndexBufferSize * sizeof(ImDrawIdx);
-        desc.BindFlags = D3D10_BIND_INDEX_BUFFER;
-        desc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
-        if (ctx->CreateBuffer(&desc, NULL, &g_pIB) < 0)
-            return;
-    }
-    
-    bool    ImGui_ImplDX11_Init(ID3D11Device* device, ID3D11DeviceContext* device_context)
-{
-    // Get factory from device
-    IDXGIDevice* pDXGIDevice = NULL;
-    IDXGIAdapter* pDXGIAdapter = NULL;
-    IDXGIFactory* pFactory = NULL;
-    }
-    
-    // Render function.
-// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
-void ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data)
-{
-    // Avoid rendering when minimized
-    if (draw_data->DisplaySize.x <= 0.0f || draw_data->DisplaySize.y <= 0.0f)
-        return;
-    }
