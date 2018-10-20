01dcbf91ee9bbb3566e820572e3361ec714d35a6
SUNsung <weatherius@gmail.com>
2018-10-20 23:07:05 +0300

    Rewrite [27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py] File in: 2018-10-20 23:07:05

--- pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
+++ pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
@@ -1,52 +1,60 @@
 
         
-        DIR_OF_THIS_SCRIPT = p.dirname( p.abspath( __file__ ) )
-DIR_OF_OLD_LIBS = p.join( DIR_OF_THIS_SCRIPT, 'python' )
+                return True
     
-      return server_conf
-    
-    
-  def Start( self ):
-    request_data = BuildRequestData()
-    request_data.update( { 'filetypes': self.filetypes } )
-    self._response = self.PostDataToHandler( request_data,
-                                             'semantic_completion_available' )
-    
-    
-def FormatDebugInfoResponse( response ):
-  if not response:
-    return 'Server errored, no debug info from server\n'
-  message = _FormatYcmdDebugInfo( response )
-  completer = response[ 'completer' ]
-  if completer:
-    message += _FormatCompleterDebugInfo( completer )
-  return message
-    
-        self._cached_response = self.HandleFuture( self._response_future,
-                                               truncate_message = True )
-    
-    
-  def _UpdateSigns( self ):
-    signs_to_unplace = vimsupport.GetSignsInBuffer( self._bufnr )
-    
-      post_vim_message.assert_has_exact_calls( [
-    call( 'this is a message', warning=False, truncate=True )
-  ] )
-    
-      eq_( len( results ), len( expected_results ) )
-  for result, expected_result in zip( results, expected_results ):
-    assert_that( result, expected_result )
-
+        data = response.content
+    response_headers = response.headers
+    if 'content-encoding' not in response_headers and len(response.content) < URLFETCH_DEFLATE_MAXSIZE and response_headers.get('content-type', '').startswith(('text/', 'application/json', 'application/javascript')):
+        if 'gzip' in accept_encoding:
+            response_headers['Content-Encoding'] = 'gzip'
+            compressobj = zlib.compressobj(zlib.Z_DEFAULT_COMPRESSION, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, 0)
+            dataio = BytesIO()
+            dataio.write('\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\xff')
+            dataio.write(compressobj.compress(data))
+            dataio.write(compressobj.flush())
+            dataio.write(struct.pack('<LL', zlib.crc32(data) & 0xFFFFFFFFL, len(data) & 0xFFFFFFFFL))
+            data = dataio.getvalue()
+        elif 'deflate' in accept_encoding:
+            response_headers['Content-Encoding'] = 'deflate'
+            data = zlib.compress(data)[2:-4]
+    if data:
+         response_headers['Content-Length'] = str(len(data))
+    response_headers_data = zlib.compress('\n'.join('%s:%s' % (k.title(), v) for k, v in response_headers.items() if not k.startswith('x-google-')))[2:-4]
+    if 'rc4' not in options:
+        start_response('200 OK', [('Content-Type', __content_type__)])
+        yield struct.pack('!hh', int(response.status_code), len(response_headers_data))+response_headers_data
+        yield data
+    else:
+        start_response('200 OK', [('Content-Type', __content_type__), ('X-GOA-Options', 'rc4')])
+        yield struct.pack('!hh', int(response.status_code), len(response_headers_data))
+        yield rc4crypt(response_headers_data, __password__)
+        yield rc4crypt(data, __password__)
     
     
-@patch( 'ycm.vimsupport.GetVariableValue',
-        GetVariableValue_CompleteItemIs( 'Test' ) )
-@patch( 'ycm.vimsupport.ReplaceChunks' )
-def PostCompleteFixIt_ApplyFixIt_PickFirst_test( replace_chunks, *args ):
-  completions = [
-    BuildCompletionFixIt( [ { 'chunks': 'one' } ] ),
-    BuildCompletionFixIt( [ { 'chunks': 'two' } ] ),
-  ]
-  with _SetUpCompleteDone( completions ) as request:
-    request._OnCompleteDone_FixIt()
-    replace_chunks.assert_called_once_with( 'one', silent = True )
+    
+    *TL;DR80
+Describes a group of objects that is treated as a single instance.
+'''
+    
+    
+class ConcreteHandler2(Handler):
+    def _handle(self, request):
+        if 10 < request <= 20:
+            print('request {} handled in handler 2'.format(request))
+            return True
+    
+    
+class Person(object):
+    def __init__(self, name, action):
+        self.name = name
+        self.action = action
+    
+        @abstractmethod
+    def is_satisfied_by(self, candidate):
+        pass
+    
+    The first example achieves this by using an abstract base
+class for a building, where the initializer (__init__ method) specifies the
+steps needed, and the concrete subclasses implement these steps.
+    
+    class TimeDisplay(object):
