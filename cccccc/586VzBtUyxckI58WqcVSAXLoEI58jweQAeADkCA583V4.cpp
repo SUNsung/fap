a38780b5011ea0f0202208f117a37fffa17ee6f0
SUNsung <weatherius@gmail.com>
2018-10-22 00:28:26 +0300

    Rewrite [586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp] File in: 2018-10-22 00:28:26

--- cccccc/586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp
+++ cccccc/586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp
@@ -1,278 +1,128 @@
 
         
-         protected:
-  // content::ContentClient:
-  std::string GetProduct() const override;
-  std::string GetUserAgent() const override;
-  base::string16 GetLocalizedString(int message_id) const override;
-  void AddAdditionalSchemes(Schemes* schemes) override;
-  void AddPepperPlugins(
-      std::vector<content::PepperPluginInfo>* plugins) override;
-  void AddContentDecryptionModules(
-      std::vector<content::CdmInfo>* cdms,
-      std::vector<media::CdmHostFilePath>* cdm_host_file_paths) override;
+          auto* message1_on_arena =
+      Arena::CreateMessage<protobuf_unittest::TestAllTypes>(&arena);
+  TestUtil::SetAllFields(message1_on_arena);
+  const auto* nested = &message1_on_arena->optional_nested_message();
     
-    void AutoUpdater::OnCheckingForUpdate() {
-  Emit('checking-for-update');
-}
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
     
-      std::unique_ptr<NativeBrowserView> view_;
+    #include <google/protobuf/compiler/java/java_context.h>
+#include <google/protobuf/compiler/java/java_doc_comment.h>
+#include <google/protobuf/compiler/java/java_helpers.h>
+#include <google/protobuf/compiler/java/java_name_resolver.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/stubs/strutil.h>
     
-    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_browser_menu, Initialize)
-
-    
-    // static
-mate::WrappableBase* Notification::New(mate::Arguments* args) {
-  if (!Browser::Get()->is_ready()) {
-    args->ThrowError('Cannot create Notification before app is ready');
-    return nullptr;
-  }
-  return new Notification(args->isolate(), args->GetThis(), args);
-}
-    
-    #ifndef ATOM_BROWSER_API_ATOM_API_POWER_SAVE_BLOCKER_H_
-#define ATOM_BROWSER_API_ATOM_API_POWER_SAVE_BLOCKER_H_
-    
-    namespace api {
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace java {
+    }
+    }
+    }
     }
     
-    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_common_screen, Initialize)
-
-    
-      /// Retrieve the array of protocol conformances, which line up with the
-  /// requirements of the generic signature.
-  ArrayRef<ProtocolConformanceRef> getConformances() const {
-    return llvm::makeArrayRef(getTrailingObjects<ProtocolConformanceRef>(),
-                              numConformanceRequirements);
-  }
-  MutableArrayRef<ProtocolConformanceRef> getConformances() {
-    return MutableArrayRef<ProtocolConformanceRef>(
-                              getTrailingObjects<ProtocolConformanceRef>(),
-                              numConformanceRequirements);
-  }
-    
-    
-    {  SyntaxMap[FromNode] = ToNode;
+    string ClassNameResolver::GetClassName(const FileDescriptor* descriptor,
+                                       bool immutable) {
+  string result = FileJavaPackage(descriptor, immutable);
+  if (!result.empty()) result += '.';
+  result += GetFileClassName(descriptor, immutable);
+  return result;
 }
     
-    void CacheImpl::releaseValue(void *Value) {
-  cache_release_value(static_cast<cache_t*>(Impl), Value);
-}
+    // Author: xiaofeng@google.com (Feng Xiao)
     
-      // If the word preceding the preposition extends the preposition, it
-  // will never be dropped.
-  if (std::next(nameWordRevIter) != nameWordRevIterEnd &&
-      priorWordExtendsPreposition(*std::next(nameWordRevIter), preposition)) {
-    ++nameWordRevIter;
-    preposition = StringRef((*nameWordRevIter).begin(),
-                            preposition.size() + (*nameWordRevIter).size());
+      static const int kBytesPerLine = 40;  // allow for escaping
+  printer->Print(
+      '    static const char *valueNames =');
+  for (int i = 0; i < text_blob.size(); i += kBytesPerLine) {
+    printer->Print(
+        '\n        \'$data$\'',
+        'data', EscapeTrigraphs(CEscape(text_blob.substr(i, kBytesPerLine))));
   }
-    
-      bool isVoid() const {
-    assert(isValid());
-    return Decl.isNull();
+  printer->Print(
+      ';\n'
+      '    static const int32_t values[] = {\n');
+  for (int i = 0; i < all_values_.size(); i++) {
+    printer->Print('        $name$,\n',  'name', EnumValueName(all_values_[i]));
   }
+  printer->Print('    };\n');
     
-      bool isGetter() const {
-    return accessorKind == IAMAccessorKind::Getter;
-  }
-    
-    #ifdef __SSE4_1__
-// Computes part of matrix.vector v = Wu. Computes 1 result.
-static void PartialMatrixDotVector1(const int8_t* wi, const double* scales,
-                                    const int8_t* u, int num_in, int num_out,
-                                    double* v) {
-  int total = IntDotProductSSE(u, wi, num_in);
-  // Add in the bias and correct for integer values.
-  *v = (static_cast<double>(total) / INT8_MAX + wi[num_in]) * *scales;
-}
-#endif  // __SSE4_1__
-    
-      // Check if the neighbor with vertical distance of y_gap is a near and math
-  // block partition.
-  bool IsNearMathNeighbor(const int y_gap, const ColPartition *neighbor) const;
+    DHTResponseMessage::~DHTResponseMessage() = default;
     
     
-    {  UNICHARSET* unicharset;
-  OSBestResult best_result;
+    {  static const std::string R;
 };
     
-    struct BlobData {
-  BlobData() : blob(nullptr), choices(nullptr) {}
-  BlobData(int index, Tesseract* tess, const WERD_RES& word)
-    : blob(word.chopped_word->blobs[index]),
-      tesseract(tess),
-      choices(&(*word.ratings)(index, index)) {}
+    #include <cstring>
+#include <cstdio>
+    
+    DHTSetup::DHTSetup() = default;
+    
+    class DHTTask {
+public:
+  virtual ~DHTTask() = default;
     }
     
-     private:
-  // The unique ID of this VC object.
-  int my_id_;
-  // Whether the parameter was changed_ and thus needs to be rewritten.
-  bool changed_;
-  // The actual ParamType of this VC object.
-  ParamType param_type_;
-    
-    #include <dmlc/registry.h>
-#include <xgboost/base.h>
-#include <xgboost/data.h>
-#include <functional>
-#include <string>
-#include <utility>
-#include <vector>
-#include '../../src/gbm/gblinear_model.h'
-#include '../../src/common/host_device_vector.h'
-    
-     private:
-  // try to prune off current leaf
-  inline int TryPruneLeaf(RegTree &tree, int nid, int depth, int npruned) { // NOLINT(*)
-    if (tree[nid].IsRoot()) return npruned;
-    int pid = tree[nid].Parent();
-    RegTree::NodeStat &s = tree.Stat(pid);
-    ++s.leaf_child_cnt;
-    if (s.leaf_child_cnt >= 2 && param_.NeedPrune(s.loss_chg, depth - 1)) {
-      // need to be pruned
-      tree.ChangeToLeaf(pid, param_.learning_rate * s.base_weight);
-      // tail recursion
-      return this->TryPruneLeaf(tree, pid, depth - 1, npruned + 2);
-    } else {
-      return npruned;
-    }
-  }
-  /*! \brief do pruning of a tree */
-  inline void DoPrune(RegTree &tree) { // NOLINT(*)
-    int npruned = 0;
-    // initialize auxiliary statistics
-    for (int nid = 0; nid < tree.param.num_nodes; ++nid) {
-      tree.Stat(nid).leaf_child_cnt = 0;
-    }
-    for (int nid = 0; nid < tree.param.num_nodes; ++nid) {
-      if (tree[nid].IsLeaf()) {
-        npruned = this->TryPruneLeaf(tree, nid, tree.GetDepth(nid), npruned);
-      }
-    }
-    if (!param_.silent) {
-      LOG(INFO) << 'tree pruning end, ' << tree.param.num_roots << ' roots, '
-                << tree.NumExtraNodes() << ' extra nodes, ' << npruned
-                << ' pruned nodes, max_depth=' << tree.MaxDepth();
-    }
-  }
-    
-    namespace xgboost {
-namespace common {
-TEST(CompressedIterator, Test) {
-  ASSERT_TRUE(detail::SymbolBits(256) == 8);
-  ASSERT_TRUE(detail::SymbolBits(150) == 8);
-  std::vector<int> test_cases = {1, 3, 426, 21, 64, 256, 100000, INT32_MAX};
-  int num_elements = 1000;
-  int repetitions = 1000;
-  srand(9);
-    }
-    }
-    }
-    
-      ~Carver();
-    
-    namespace osquery {
-    }
-    
-    
-    {  c.reset();
-}
-    
-    Status ViewsConfigParserPlugin::update(const std::string& source,
-                                       const ParserConfig& config) {
-  auto cv = config.find('views');
-  if (cv == config.end()) {
-    return Status(1);
-  }
-    }
-    
-    namespace osquery {
-    }
-    
-      /// original path, read from config
-  std::string opath;
-    
-      // Assure we start from a base state.
-  EXPECT_EQ(sub->timesConfigured, 0U);
-  // Force the config into a loaded state.
-  Config::get().loaded_ = true;
-  Config::get().update({{'data', '{}'}});
-  EXPECT_EQ(sub->timesConfigured, 1U);
-    
-        std::unique_ptr<DHTTokenTracker> tokenTracker;
-    
-    
-    {} // namespace aria2
-
-    
-    class DHTRoutingTableSerializer {
-private:
-  int family_;
-    }
-    
-    
-    {} // namespace aria2
-    
-    void DHTTaskFactoryImpl::setMessageFactory(DHTMessageFactory* factory)
+    DHTTaskExecutor::DHTTaskExecutor(int numConcurrent)
+    : numConcurrent_(numConcurrent)
 {
-  factory_ = factory;
 }
     
-    void DHTTaskQueueImpl::addPeriodicTask2(const std::shared_ptr<DHTTask>& task)
-{
-  periodicTaskQueue2_.addTask(task);
-}
+      ~DHTTaskExecutor();
     
-    public:
-  DHTTaskQueueImpl();
+    namespace aria2 {
+    }
     
-    public:
-  // _remoteNode is always null
-  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
-                    const unsigned char* data, size_t length,
-                    const std::string& ipaddr, uint16_t port);
+      virtual void
+  addPeriodicTask2(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
     
-    DNSCache::~DNSCache() = default;
     
-      /**
-   * @brief Send a single message.
-   * @param frames A single-element vector containing only one message.
-   * @return The status of the sending single message action which is defined by
-   *         apollo::common::ErrorCode.
+    {  void setTokenTracker(DHTTokenTracker* tokenTracker);
+};
+    
+      FunctionRef<int(int, char const*)> variant5 = of;
+  EXPECT_EQ(100 + 5 * 19, variant5(19, 'foo'));
+  FunctionRef<int(int, char const*)> const cvariant5 = of;
+  EXPECT_EQ(100 + 5 * 19, cvariant5(19, 'foo'));
+    
+    #include <iostream>
+    
+    
+    {  /**
+   * Returns a double in [min, max), if min == max, returns 0.
    */
-  virtual apollo::common::ErrorCode SendSingleFrame(
-      const std::vector<CanFrame> &frames) {
-    CHECK_EQ(frames.size(), 1)
-        << 'frames size not equal to 1, actual frame size :' << frames.size();
-    int32_t n = 1;
-    return Send(frames, &n);
+  template <class RNG = ThreadLocalPRNG, class /* EnableIf */ = ValidRNG<RNG>>
+  static double randDouble(double min, double max, RNG&& rng) {
+    if (std::fabs(max - min) < std::numeric_limits<double>::epsilon()) {
+      return 0;
+    }
+    return std::uniform_real_distribution<double>(min, max)(rng);
   }
+};
     
-    #include 'modules/drivers/canbus/can_client/can_client_factory.h'
-    
-      /**
-   * @brief Destructor
+    struct Options {
+  /**
+   * ZLIB: default option -- write a zlib wrapper as documented in RFC 1950.
+   *
+   * GZIP: write a simple gzip header and trailer around the compressed data
+   * instead of a zlib wrapper.
+   *
+   * RAW: deflate will generate raw deflate data with no zlib header or
+   * trailer, and will not compute a check value.
+   *
+   * AUTO: enable automatic header detection for decoding gzip or zlib data.
+   * For deflation, ZLIB will be used.
    */
-  virtual ~EsdCanClient();
-    
-    using apollo::common::ErrorCode;
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-namespace can {
+  enum class Format { ZLIB, GZIP, RAW, AUTO };
     }
-    }
-    }
-    }
-    
-    /* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
-
-    
-    #include 'modules/drivers/canbus/can_client/socket/socket_can_client_raw.h'
-    
-    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
-    
-    #endif  // MODULES_DRIVERS_CANBUS_COMMON_CANBUS_CONSTS_H_
