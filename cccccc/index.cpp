0d0fb3354008d39bae53ce718d2bbfc0e3176de5
SUNsung <weatherius@gmail.com>
2018-10-21 22:09:58 +0300

    Rewrite [index.cpp] File in: 2018-10-21 22:09:57

--- cccccc/index.cpp
+++ cccccc/index.cpp
@@ -1,242 +1,74 @@
 
         
-          /**
-   * @brief Specifies whether the layer should compute gradients w.r.t. a
-   *        parameter at a particular index given by param_id.
-   *
-   * You can safely ignore false values and always compute gradients
-   * for all parameters, but possibly with wasteful computation.
-   */
-  inline bool param_propagate_down(const int param_id) {
-    return (param_propagate_down_.size() > param_id) ?
-        param_propagate_down_[param_id] : false;
-  }
-  /**
-   * @brief Sets whether the layer should compute gradients w.r.t. a
-   *        parameter at a particular index given by param_id.
-   */
-  inline void set_param_propagate_down(const int param_id, const bool value) {
-    if (param_propagate_down_.size() <= param_id) {
-      param_propagate_down_.resize(param_id + 1, true);
-    }
-    param_propagate_down_[param_id] = value;
-  }
-    
-     protected:
-  /// @copydoc BNLLLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    
-    {
-    {  // Recursive copy function: this is similar to crop_copy() but loops over all
-  // but the last two dimensions to allow for ND cropping while still relying on
-  // a CUDA kernel for the innermost two dimensions for performance reasons.  An
-  // alterantive implementation could rely on the kernel more by passing
-  // offsets, but this is problematic because of its variable length.
-  // Since in the standard (N,C,W,H) case N,C are usually not cropped a speedup
-  // could be achieved by not looping the application of the copy_kernel around
-  // these dimensions.
-  void crop_copy_gpu(const vector<Blob<Dtype>*>& bottom,
-                const vector<Blob<Dtype>*>& top,
-                const vector<int>& offsets,
-                vector<int> indices,
-                int cur_dim,
-                const Dtype* src_data,
-                Dtype* dest_data,
-                bool is_forward);
-};
-}  // namespace caffe
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    Action::~Action()
-{
-    CCLOGINFO('deallocing Action: %p - tag: %i', this, _tag);
+        float ButteraugliComparator::BlockErrorLimit() const {
+  return target_distance_;
 }
     
-        // 1st element (which should be 0,0) should be here too
     
-    EaseBezierAction* EaseBezierAction::create(cocos2d::ActionInterval* action)
-{
-    EaseBezierAction *ret = new (std::nothrow) EaseBezierAction();
-    if (ret && ret->initWithAction(action))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    }
+    {}  // namespace guetzli
     
-        /**
-    @brief Get the center position of twirl action.
-    @return The center position of twirl action.
-    */
-    const Vec2& getPosition() const { return _position; }
-    /**
-    @brief Set the center position of twirl action.
-    @param position The center position of twirl action will be set.
-    */
-    void setPosition(const Vec2& position);
+    #include <assert.h>
+#include <algorithm>
     
-    #ifndef __CCINSTANT_ACTION_H__
-#define __CCINSTANT_ACTION_H__
-    
-        /** Returns the numbers of actions that are running in a certain target. 
-     * Composable actions are counted as 1 action. Example:
-     * - If you are running 1 Sequence of 7 actions, it will return 1.
-     * - If you are running 7 Sequences of 2 actions, it will return 7.
-     *
-     * @param target    A certain target.
-     * @return  The numbers of actions that are running in a certain target.
-     * @js NA
-     */
-    virtual ssize_t getNumberOfRunningActionsInTarget(const Node *target) const;
-    
-    /** Returns the numbers of actions that are running in all targets.
-     * @return  The numbers of actions that are running in all target.
-     * @js NA
-     */
-    virtual ssize_t getNumberOfRunningActions() const;
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __ACTION_CCPAGETURN3D_ACTION_H__
-#define __ACTION_CCPAGETURN3D_ACTION_H__
-    
-    
-    {    // special opacity for premultiplied textures
-    if( _isOpacityModifyRGB )
-        this->setColor(_colorUnmodified);
+    void ComputeBlockDCT(coeff_t* coeffs) {
+  ColumnDct(coeffs);
+  RowDct(coeffs + 0 * 8, kTable04);
+  RowDct(coeffs + 1 * 8, kTable17);
+  RowDct(coeffs + 2 * 8, kTable26);
+  RowDct(coeffs + 3 * 8, kTable35);
+  RowDct(coeffs + 4 * 8, kTable04);
+  RowDct(coeffs + 5 * 8, kTable35);
+  RowDct(coeffs + 6 * 8, kTable26);
+  RowDct(coeffs + 7 * 8, kTable17);
 }
     
-    // Benchmark results on my dev server (20-core Intel Xeon E5-2660 v2 @ 2.20GHz)
-//
-// ============================================================================
-// folly/test/FormatBenchmark.cpp                  relative  time/iter  iters/s
-// ============================================================================
-// octal_snprintf                                              79.30ns   12.61M
-// octal_uintToOctal                               3452.19%     2.30ns  435.35M
-// ----------------------------------------------------------------------------
-// hex_snprintf                                                73.59ns   13.59M
-// hex_uintToHex                                   4507.53%     1.63ns  612.49M
-// ----------------------------------------------------------------------------
-// intAppend_snprintf                                         191.50us    5.22K
-// intAppend_to                                     552.46%    34.66us   28.85K
-// intAppend_format                                 215.76%    88.76us   11.27K
-// ----------------------------------------------------------------------------
-// bigFormat_snprintf                                         178.03us    5.62K
-// bigFormat_format                                  90.41%   196.91us    5.08K
-// ----------------------------------------------------------------------------
-// format_nested_strings                                      317.65us    3.15K
-// format_nested_fbstrings                           99.89%   318.01us    3.14K
-// format_nested_direct                             116.52%   272.62us    3.67K
-// ----------------------------------------------------------------------------
-// copy_short_string                                           28.33ns   35.30M
-// format_short_string_unsafe                        82.51%    34.33ns   29.13M
-// format_short_string_safe                          58.92%    48.08ns   20.80M
-// sformat_short_string_unsafe                       73.90%    38.33ns   26.09M
-// sformat_short_string_safe                         54.97%    51.53ns   19.41M
-// ----------------------------------------------------------------------------
-// copy_long_string                                            57.56ns   17.37M
-// format_long_string_unsafe                         68.79%    83.68ns   11.95M
-// format_long_string_safe                           69.44%    82.89ns   12.06M
-// sformat_long_string_unsafe                        65.58%    87.77ns   11.39M
-// sformat_long_string_safe                          68.14%    84.47ns   11.84M
-// ============================================================================
-    
-      FunctionRef<int(int, std::vector<int> const&)> variant6 = of;
-  EXPECT_EQ(100 + 6 * 20, variant6(20, {}));
-  EXPECT_EQ(100 + 6 * 20, variant6(20, {1, 2, 3}));
-  FunctionRef<int(int, std::vector<int> const&)> const cvariant6 = of;
-  EXPECT_EQ(100 + 6 * 20, cvariant6(20, {}));
-  EXPECT_EQ(100 + 6 * 20, cvariant6(20, {1, 2, 3}));
-    
-    // Empirically, this indicates if the runtime supports
-// std::exception_ptr, as not all (arm, for instance) do.
-#if defined(__GNUC__) && defined(__GCC_ATOMIC_INT_LOCK_FREE) && \
-    __GCC_ATOMIC_INT_LOCK_FREE > 1
-inline fbstring exceptionStr(std::exception_ptr ep) {
-  try {
-    std::rethrow_exception(ep);
-  } catch (const std::exception& e) {
-    return exceptionStr(e);
-  } catch (...) {
-    return '<unknown exception>';
-  }
-}
-#endif
+      tmp0 = in[5 * stride];
+  tmp1 = kIDCTMatrix[ 5] * tmp0;
+  tmp2 = kIDCTMatrix[13] * tmp0;
+  tmp3 = kIDCTMatrix[21] * tmp0;
+  tmp4 = kIDCTMatrix[29] * tmp0;
+  out[0] += tmp1;
+  out[1] += tmp2;
+  out[2] += tmp3;
+  out[3] += tmp4;
+  out[4] -= tmp4;
+  out[5] -= tmp3;
+  out[6] -= tmp2;
+  out[7] -= tmp1;
     
     
-    {void Executor::keepAliveRelease() {
-  LOG(FATAL) << __func__ << '() should not be called for folly::Executor types '
-             << 'which do not override keepAliveAcquire()';
-}
-} // namespace folly
+    {}  // namespace guetzli
 
     
-    #include <folly/Range.h>
     
-    /**
- * VirtualExecutor implements a light-weight view onto existing Executor.
- *
- * Multiple VirtualExecutors can be backed by a single Executor.
- *
- * VirtualExecutor's destructor blocks until all tasks scheduled through it are
- * complete. Executor's destructor also blocks until all VirtualExecutors
- * backed by it are released.
- */
-class VirtualExecutor : public DefaultKeepAliveExecutor {
-  auto wrapFunc(Func f) {
-    class FuncAndKeepAlive {
-     public:
-      FuncAndKeepAlive(Func&& f, VirtualExecutor* executor)
-          : keepAlive_(getKeepAliveToken(executor)), f_(std::move(f)) {}
-    }
-    }
-    }
+    {}  // namespace guetzli
     
-    /// Wrapper around the makeCompressionCounterHandler() extension point.
-class CompressionCounter {
- public:
-  CompressionCounter() {}
-  CompressionCounter(
-      folly::io::CodecType codecType,
-      folly::StringPiece codecName,
-      folly::Optional<int> level,
-      CompressionCounterKey key,
-      CompressionCounterType counterType) {
-    initialize_ = [=]() {
-      return makeCompressionCounterHandler(
-          codecType, codecName, level, key, counterType);
-    };
-    DCHECK(!initialize_.hasAllocatedMemory());
+    bool ReadJpeg(const uint8_t* data, const size_t len, JpegReadMode mode,
+              JPEGData* jpg) {
+  size_t pos = 0;
+  // Check SOI marker.
+  EXPECT_MARKER();
+  int marker = data[pos + 1];
+  pos += 2;
+  if (marker != 0xd8) {
+    fprintf(stderr, 'Did not find expected SOI marker, actual=%d\n', marker);
+    jpg->error = JPEG_SOI_NOT_FOUND;
+    return false;
   }
+  int lut_size = kMaxHuffmanTables * kJpegHuffmanLutSize;
+  std::vector<HuffmanTableEntry> dc_huff_lut(lut_size);
+  std::vector<HuffmanTableEntry> ac_huff_lut(lut_size);
+  bool found_sof = false;
+  uint16_t scan_progression[kMaxComponents][kDCTBlockSize] = { { 0 } };
     }
     
-    /**
- * Get a codec with the given options and compression level.
- *
- * If the windowSize is 15 and the format is Format::ZLIB or Format::GZIP, then
- * the type of the codec will be CodecType::ZLIB or CodecType::GZIP
- * respectively. Otherwise, the type will be CodecType::USER_DEFINED.
- *
- * Automatic uncompression is not supported with USER_DEFINED codecs.
- *
- * Levels supported: 0 = no compression, 1 = fast, ..., 9 = best; default = 6
- */
-std::unique_ptr<Codec> getCodec(
-    Options options = Options(),
-    int level = COMPRESSION_LEVEL_DEFAULT);
-std::unique_ptr<StreamCodec> getStreamCodec(
-    Options options = Options(),
-    int level = COMPRESSION_LEVEL_DEFAULT);
+      Mutex& GetBenchmarkMutex() const RETURN_CAPABILITY(benchmark_mutex_) {
+    return benchmark_mutex_;
+  }
+    
+    // Parses a string for an Int32 flag, in the form of
+// '--flag=value'.
+//
+// On success, stores the value of the flag in *value, and returns
+// true.  On failure, returns false without changing *value.
+bool ParseInt32Flag(const char* str, const char* flag, int32_t* value);
