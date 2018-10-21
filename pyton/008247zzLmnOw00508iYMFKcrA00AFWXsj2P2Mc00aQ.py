932fd8a49354d6262ed1cb9fbad6ef75cf61d550
SUNsung <weatherius@gmail.com>
2018-10-21 23:19:27 +0300

    Rewrite [008247zzLmnOw00508iYMFKcrA00AFWXsj2P2Mc00aQ.py] File in: 2018-10-21 23:19:27

--- pyton/008247zzLmnOw00508iYMFKcrA00AFWXsj2P2Mc00aQ.py
+++ pyton/008247zzLmnOw00508iYMFKcrA00AFWXsj2P2Mc00aQ.py
@@ -1,3 +1,147 @@
 
         
         
+if __name__ == '__main__':
+    
+    A test of LocalOutlierFactor on classical anomaly detection datasets.
+    
+                gc.collect()
+            print('benchmarking lars_path (with Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            G = np.dot(X.T, X)  # precomputed Gram matrix
+            Xy = np.dot(X.T, y)
+            lars_path(X, y, Xy=Xy, Gram=G, method='lasso')
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            results['lars_path (with Gram)'].append(delta)
+    
+    Typical output
+--------------
+    
+        xx = np.arange(start_dim, start_dim + n * step, step)
+    plt.subplot(212)
+    plt.title('Learning in high dimensional spaces')
+    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
+    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
+    plt.legend(loc='upper left')
+    plt.xlabel('number of dimensions')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
+    
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (3 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
+    
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (14 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
+    
+        def set_appid_not_exist(self, appid):
+        self.logger.warn('APPID_manager, set_appid_not_exist %s', appid)
+        with self.lock:
+            if appid not in self.not_exist_appids:
+                self.not_exist_appids.append(appid)
+            try:
+                self.config.GAE_APPIDS.remove(appid)
+            except:
+                pass
+    
+            if self.continue_fail_count > 10:
+            # don't set network_stat to 'unknown', wait for check
+            # network_stat = 'unknown'
+            xlog.debug('report_connect_fail %s continue_fail_count:%d',
+                       self.type, self.continue_fail_count)
+            self.triger_check_network(True)
+    
+    
+class SniManager(object):
+    plus = ['-', '', '.']
+    end = ['com', 'net', 'ml', 'org', 'us']
+    
+    try:
+    from io import BytesIO
+except ImportError:
+    from cStringIO import StringIO as BytesIO
+try:
+    from google.appengine.api import urlfetch
+    from google.appengine.runtime import apiproxy_errors
+except ImportError:
+    urlfetch = None
+try:
+    import sae
+except ImportError:
+    sae = None
+try:
+    import bae.core.wsgi
+except ImportError:
+    bae = None
+try:
+    import socket
+    import select
+except ImportError:
+    socket = None
+try:
+    import OpenSSL
+except ImportError:
+    OpenSSL = None
+    
+    # begin[licence]
+#
+# [The 'BSD licence']
+# Copyright (c) 2005-2008 Terence Parr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# end[licence]
+    
+    def os_constant(key):
+    # XXX TODO: In the future, this could return different constants
+    #           based on what OS we are running under.  To see an
+    #           approach to how to handle different OSes, see the
+    #           apache version of this file.  Currently, we do not
+    #           actually have any OS-specific constants on Nginx.
+    '''
+    Get a constant value for operating system
+    
+            :param str title: The title of the save. If a title is given, the
+            configuration will be saved as a new checkpoint and put in a
+            timestamped directory.
+    
+        def test_mod_config(self):
+        z_domains = []
+        for achall in self.achalls:
+            self.sni.add_chall(achall)
+            z_domain = achall.response(self.auth_key).z_domain
+            z_domains.append(set([z_domain.decode('ascii')]))
+    
+    # For 'manual' documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
