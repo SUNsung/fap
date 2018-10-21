ce29c8333ac797849d8282536a0ce131b1545d81
SUNsung <weatherius@gmail.com>
2018-10-22 02:09:15 +0300

    Rewrite [30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py] File in: 2018-10-22 02:09:14

--- pyton/30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py
+++ pyton/30Iz9hTHvadrw30E9WKiPVQgoU303rrAkujjJPU30i5S.py
@@ -1,187 +1,193 @@
 
         
-        
-def register_type_handlers(connection, **kwargs):
-    if connection.vendor != 'postgresql' or connection.alias == NO_DB_ALIAS:
-        return
+        from utils import write_datasets
+from synthetic_data_utils import add_alignment_projections, generate_data
+from synthetic_data_utils import generate_rnn, get_train_n_valid_inds
+from synthetic_data_utils import nparray_and_transpose
+from synthetic_data_utils import spikify_data, gaussify_data, split_list_by_inds
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.signal
+    
+      wname = (name + '/W') if name else '/W'
+    
+      for sequence in sequences:
+    indices = []
+    for embedding in sequence:
+      indices.append(np.argmax(embedding))
+    batch_of_indices.append(indices)
+  return batch_of_indices
     
     
-class RedirectFallbackMiddleware(MiddlewareMixin):
-    # Defined as class-level attributes to be subclassing-friendly.
-    response_gone_class = HttpResponseGone
-    response_redirect_class = HttpResponsePermanentRedirect
+def hash_function(input_tuple):
+  '''Hash function for a tuple.'''
+  return hash(input_tuple)
     
-        def get_expire_at_browser_close(self):
-        '''
-        Return ``True`` if the session is set to expire when the browser
-        closes, and ``False`` if there's an expiry date. Use
-        ``get_expiry_date()`` or ``get_expiry_age()`` to find the actual expiry
-        date/age, if there is one.
-        '''
-        if self.get('_session_expiry') is None:
-            return settings.SESSION_EXPIRE_AT_BROWSER_CLOSE
-        return self.get('_session_expiry') == 0
-    
-        def exists(self, session_key=None):
-        '''
-        This method makes sense when you're talking to a shared resource, but
-        it doesn't matter when you're storing the information in the client's
-        cookie.
-        '''
-        return False
-    
-        data_train = fetch_20newsgroups_vectorized(subset='train')
-    data_test = fetch_20newsgroups_vectorized(subset='test')
-    X_train = check_array(data_train.data, dtype=np.float32,
-                          accept_sparse='csc')
-    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
-    y_train = data_train.target
-    y_test = data_test.target
-    
-        plt.figure('scikit-learn LASSO benchmark results')
-    plt.subplot(211)
-    plt.plot(list_n_samples, lasso_results, 'b-',
-                            label='Lasso')
-    plt.plot(list_n_samples, lars_lasso_results, 'r-',
-                            label='LassoLars')
-    plt.title('precomputed Gram matrix, %d features, alpha=%s' % (n_features,
-                            alpha))
-    plt.legend(loc='upper left')
-    plt.xlabel('number of samples')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    
-        if dataset_name == 'SF':
-        lb = LabelBinarizer()
-        x1 = lb.fit_transform(X[:, 1].astype(str))
-        X = np.c_[X[:, :1], x1, X[:, 2:]]
-        y = (y != b'normal.').astype(int)
-    
-        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
-    plt.plot(sample_sizes, one_core, label='one core')
-    plt.plot(sample_sizes, multi_core, label='multi core')
-    plt.xlabel('n_samples')
-    plt.ylabel('Time (s)')
-    plt.title('Parallel %s' % func.__name__)
-    plt.legend()
+        def copy(self):
+        '''Creates a copy of this request context with the same request object.
+        This can be used to move a request context to a different greenlet.
+        Because the actual request object is the same this cannot be used to
+        move a request context to a different thread unless access to the
+        request object is locked.
     
     
-def scatter_time_vs_s(time, norm, point_labels, title):
-    plt.figure()
-    size = 100
-    for i, l in enumerate(sorted(norm.keys())):
-        if l != 'fbpca':
-            plt.scatter(time[l], norm[l], label=l, marker='o', c='b', s=size)
-            for label, x, y in zip(point_labels, list(time[l]), list(norm[l])):
-                plt.annotate(label, xy=(x, y), xytext=(0, -80),
-                             textcoords='offset points', ha='right',
-                             arrowprops=dict(arrowstyle='->',
-                                             connectionstyle='arc3'),
-                             va='bottom', size=11, rotation=90)
-        else:
-            plt.scatter(time[l], norm[l], label=l, marker='^', c='red', s=size)
-            for label, x, y in zip(point_labels, list(time[l]), list(norm[l])):
-                plt.annotate(label, xy=(x, y), xytext=(0, 30),
-                             textcoords='offset points', ha='right',
-                             arrowprops=dict(arrowstyle='->',
-                                             connectionstyle='arc3'),
-                             va='bottom', size=11, rotation=90)
-    
-    # add noise
-y += 0.01 * np.random.normal((n_samples,))
-    
-    html_folder = u'html'
-text_folder = u'paragraphs'
-short_text_folder = u'short_paragraphs'
-n_words_per_short_text = 5
-    
-    from __future__ import print_function
-from __future__ import division
-from __future__ import unicode_literals
-from __future__ import absolute_import
-    
-      # Start by looking for a single character match
-  # and increase length until no match is found.
-  best = 0
-  length = 1
-  while True:
-    pattern = left_string[ -length: ]
-    found = right_string.find( pattern )
-    if found < 0:
-      return best
-    length += found
-    if left_string[ -length: ] == right_string[ :length ]:
-      best = length
-      length += 1
+def explain_ignored_app_run():
+    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
+        warn(Warning('Silently ignoring app.run() because the '
+                     'application is run from the flask command line '
+                     'executable.  Consider putting app.run() behind an '
+                     'if __name__ == '__main__' guard to silence this '
+                     'warning.'), stacklevel=3)
 
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+            if head[::2] == b'\x00\x00':
+            return 'utf-16-be'
     
-      return FilterLevel
+                resp = app.response_class()
+            if not session_interface.is_null_session(sess):
+                session_interface.save_session(app, sess, resp)
+            headers = resp.get_wsgi_headers(c.request.environ)
+            self.cookie_jar.extract_wsgi(c.request.environ, headers)
     
-        for sign in signs_to_unplace:
-      vimsupport.UnplaceSign( sign )
+    KEY_PREFIX = 'django.contrib.sessions.cached_db'
     
-      # Ignore 'nextgroup=' argument in first position and the arguments
-  # 'skipwhite', 'skipnl', and 'skipempty' that immediately come after.
-  nextgroup_at_start = False
-  if words[ 0 ].startswith( 'nextgroup=' ):
-    nextgroup_at_start = True
-    words = words[ 1: ]
+    from sklearn.cluster.k_means_ import KMeans, MiniBatchKMeans
+    
+            ax.set_xlabel('n_samples')
+        ax.set_ylabel('n_features')
+        ax.set_zlabel('Time (s)')
+        ax.set_zlim3d(0.0, max_time * 1.1)
+        ax.set_title(label)
+        # ax.legend()
+        i += 1
+    plt.show()
+
+    
+        # Plot results
+    i = 0
+    m = len(list_n_features)
+    plt.figure('scikit-learn SGD regression benchmark results',
+               figsize=(5 * 2, 4 * m))
+    for j in range(m):
+        plt.subplot(m, 2, i + 1)
+        plt.plot(list_n_samples, np.sqrt(elnet_results[:, j, 0]),
+                 label='ElasticNet')
+        plt.plot(list_n_samples, np.sqrt(sgd_results[:, j, 0]),
+                 label='SGDRegressor')
+        plt.plot(list_n_samples, np.sqrt(asgd_results[:, j, 0]),
+                 label='A-SGDRegressor')
+        plt.plot(list_n_samples, np.sqrt(ridge_results[:, j, 0]),
+                 label='Ridge')
+        plt.legend(prop={'size': 10})
+        plt.xlabel('n_train')
+        plt.ylabel('RMSE')
+        plt.title('Test error - %d features' % list_n_features[j])
+        i += 1
+    
+    mu_second = 0.0 + 10 ** 6  # number of microseconds in a second
+    
+        def start_requests(self):
+        url = self.benchurl
+        if self.latency is not None:
+            url += '?latency={0}'.format(self.latency)
+    
+    def _get_commands_from_entry_points(inproject, group='scrapy.commands'):
+    cmds = {}
+    for entry_point in pkg_resources.iter_entry_points(group):
+        obj = entry_point.load()
+        if inspect.isclass(obj):
+            cmds[entry_point.name] = obj()
+        else:
+            raise Exception('Invalid entry point %s' % entry_point.name)
+    return cmds
+    
+        def prepare_request(self, spider, request, opts):
+        def callback(response):
+            # memorize first request
+            if not self.first_response:
+                self.first_response = response
+    
+    site_info = 'AcFun.tv'
+download = acfun_download
+download_playlist = playlist_not_supported('acfun')
+
+    
+    site_info = 'CBS.com'
+download = cbs_download
+download_playlist = playlist_not_supported('cbs')
+
+    
+        for quality in ['1080','720','480','380','240','144','auto']:
+        try:
+            real_url = info[quality][1]['url']
+            if real_url:
+                break
+        except KeyError:
+            pass
     
     
-def StopServer( ycm ):
-  try:
-    ycm.OnVimLeave()
-    WaitUntilProcessIsTerminated( ycm._server_popen )
-    CloseStandardStreams( ycm._server_popen )
-  except Exception:
-    pass
+site_info = 'fantasy.tv'
+download = fantasy_download
+download_playlist = playlist_not_supported('fantasy.tv')
+
+    
+                if item.get(CONF_TEXT) is not None:
+                if isinstance(item.get(CONF_TEXT), template.Template):
+                    output[ATTR_MAIN_TEXT] = item[CONF_TEXT].async_render()
+                else:
+                    output[ATTR_MAIN_TEXT] = item.get(CONF_TEXT)
+    
+            devices = {}
+        for lease in leases_result:
+            match = _LEASES_REGEX.search(lease.decode('utf-8'))
+            if match is not None:
+                devices[match.group('ip')] = {
+                    'ip': match.group('ip'),
+                    'mac': match.group('mac').upper(),
+                    'timevalid': int(match.group('timevalid'))
+                    }
+        return devices
+
+    
+            # If there's a home_id, we need a different API URL
+        if self.home_id is None:
+            self.tadoapiurl = 'https://my.tado.com/api/v2/me'
+        else:
+            self.tadoapiurl = 'https://my.tado.com/api/v2' \
+                              '/homes/{home_id}/mobileDevices'
+    
+        return scanner if scanner.success_init else None
+    
+        def see_vehicle(vehicle):
+        '''Handle the reporting of the vehicle position.'''
+        host_name = voc.vehicle_name(vehicle)
+        dev_id = 'volvo_{}'.format(slugify(host_name))
+        see(dev_id=dev_id,
+            host_name=host_name,
+            gps=(vehicle.position['latitude'],
+                 vehicle.position['longitude']),
+            icon='mdi:car')
+    
+        Adapted from Luci scanner.
+    '''
+    
+        @Throttle(MIN_TIME_BETWEEN_STORE_UPDATES)
+    def update_closest_store(self):
+        '''Update the shared closest store (if open).'''
+        from pizzapi.address import StoreException
+        try:
+            self.closest_store = self.address.closest_store()
+            return True
+        except StoreException:
+            self.closest_store = None
+            return False
+    
+    For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/emoncms_history/
+'''
+import logging
+from datetime import timedelta
     
     
-from __future__ import with_statement
-import threading
-import weakref
-import sys
-    
-        # commands are just pushed into the command stack
-    command_stack.append(MoveFileCommand('foo.txt', 'bar.txt'))
-    command_stack.append(MoveFileCommand('bar.txt', 'baz.txt'))
-    
-        def setTC(self, tc):
-        self._tc = tc
-    
-        def __get__(self, obj, T):
-        def transaction(*args, **kwargs):
-            state = memento(obj)
-            try:
-                return self.method(obj, *args, **kwargs)
-            except Exception as e:
-                state()
-                raise e
-    
-    
-# Makes templates
-def make_template(skeleton, getter, action):
-    '''Instantiate a template method with getter and action'''
-    def template():
-        skeleton(getter, action)
-    return template
-    
-    The idea is to abstract the creation of objects depending on business
-logic, platform choice, etc.
-    
-        def now(self):
-        current_time = datetime.datetime.now()
-        current_time_formatted = '{}:{}'.format(current_time.hour,
-                                                current_time.minute)
-        return current_time_formatted
-    
-    
-class BusinessLogic(object):
-    ''' Business logic holding data store instances '''
+class Watcher():
+    '''Class for starting Watchdog.'''
