4266afb24a35c6bfa85a33e91e67aa0b9869b630
SUNsung <weatherius@gmail.com>
2018-10-21 16:29:23 +0300

    Rewrite [50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp] File in: 2018-10-21 16:29:22

--- cccccc/50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp
+++ cccccc/50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp
@@ -1,125 +1,256 @@
 
         
-        // Internal macro for implementing {EXPECT|ASSERT}_PRED3.  Don't use
-// this in your code.
-#define GTEST_PRED3_(pred, v1, v2, v3, on_failure)\
-  GTEST_ASSERT_(::testing::AssertPred3Helper(#pred, \
-                                             #v1, \
-                                             #v2, \
-                                             #v3, \
-                                             pred, \
-                                             v1, \
-                                             v2, \
-                                             v3), on_failure)
+          // Since the basic IO manipulators are overloaded for both narrow
+  // and wide streams, we have to provide this specialized definition
+  // of operator <<, even though its body is the same as the
+  // templatized version above.  Without this definition, streaming
+  // endl or other basic IO manipulators to Message will confuse the
+  // compiler.
+  Message& operator <<(BasicNarrowIoManip val) {
+    *ss_ << val;
+    return *this;
+  }
     
-    // Removes const from a type if it is a const type, otherwise leaves
-// it unchanged.  This is the same as tr1::remove_const, which is not
-// widely available yet.
-template <typename T>
-struct RemoveConst { typedef T type; };  // NOLINT
-template <typename T>
-struct RemoveConst<const T> { typedef T type; };  // NOLINT
+    # define TYPED_TEST_P(CaseName, TestName) \
+  namespace GTEST_CASE_NAMESPACE_(CaseName) { \
+  template <typename gtest_TypeParam_> \
+  class TestName : public CaseName<gtest_TypeParam_> { \
+   private: \
+    typedef CaseName<gtest_TypeParam_> TestFixture; \
+    typedef gtest_TypeParam_ TypeParam; \
+    virtual void TestBody(); \
+  }; \
+  static bool gtest_##TestName##_defined_ GTEST_ATTRIBUTE_UNUSED_ = \
+      GTEST_TYPED_TEST_CASE_P_STATE_(CaseName).AddTestName(\
+          __FILE__, __LINE__, #CaseName, #TestName); \
+  } \
+  template <typename gtest_TypeParam_> \
+  void GTEST_CASE_NAMESPACE_(CaseName)::TestName<gtest_TypeParam_>::TestBody()
     
-    // We use our own TR1 tuple if we aren't sure the user has an
-// implementation of it already.  At this time, libstdc++ 4.0.0+ and
-// MSVC 2010 are the only mainstream standard libraries that come
-// with a TR1 tuple implementation.  NVIDIA's CUDA NVCC compiler
-// pretends to be GCC by defining __GNUC__ and friends, but cannot
-// compile GCC's tuple implementation.  MSVC 2008 (9.0) provides TR1
-// tuple in a 323 MB Feature Pack download, which we cannot assume the
-// user has.  QNX's QCC compiler is a modified GCC but it doesn't
-// support TR1 tuple.  libc++ only provides std::tuple, in C++11 mode,
-// and it can be used with some compilers that define __GNUC__.
-# if (defined(__GNUC__) && !defined(__CUDACC__) && (GTEST_GCC_VER_ >= 40000) \
-      && !GTEST_OS_QNX && !defined(_LIBCPP_VERSION)) || _MSC_VER >= 1600
-#  define GTEST_ENV_HAS_TR1_TUPLE_ 1
-# endif
     
-      tuple(const tuple& t) : f0_(t.f0_), f1_(t.f1_), f2_(t.f2_) {}
+    {  std::string pathname_;
+};  // class FilePath
     
-    TEST_F(UnicharcompressTest, DoesKannada) {
-  LOG(INFO) << 'Testing kan';
-  LoadUnicharset('kan.unicharset');
-  ExpectCorrect('kan');
-  SerializeAndUndo();
-  ExpectCorrect('kan');
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26, typename T27, typename T28, typename T29, typename T30,
+    typename T31, typename T32, typename T33, typename T34, typename T35,
+    typename T36, typename T37, typename T38, typename T39, typename T40,
+    typename T41, typename T42, typename T43, typename T44, typename T45>
+class ValueArray45 {
+ public:
+  ValueArray45(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+      T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
+      T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
+      T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
+      T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41,
+      T42 v42, T43 v43, T44 v44, T45 v45) : v1_(v1), v2_(v2), v3_(v3), v4_(v4),
+      v5_(v5), v6_(v6), v7_(v7), v8_(v8), v9_(v9), v10_(v10), v11_(v11),
+      v12_(v12), v13_(v13), v14_(v14), v15_(v15), v16_(v16), v17_(v17),
+      v18_(v18), v19_(v19), v20_(v20), v21_(v21), v22_(v22), v23_(v23),
+      v24_(v24), v25_(v25), v26_(v26), v27_(v27), v28_(v28), v29_(v29),
+      v30_(v30), v31_(v31), v32_(v32), v33_(v33), v34_(v34), v35_(v35),
+      v36_(v36), v37_(v37), v38_(v38), v39_(v39), v40_(v40), v41_(v41),
+      v42_(v42), v43_(v43), v44_(v44), v45_(v45) {}
+    }
+    
+      // Creates an ANSI string from the given wide string, allocating
+  // memory using new. The caller is responsible for deleting the return
+  // value using delete[]. Returns the ANSI string, or NULL if the
+  // input is NULL.
+  //
+  // The returned string is created using the ANSI codepage (CP_ACP) to
+  // match the behaviour of the ANSI versions of Win32 calls and the
+  // C runtime.
+  static const char* Utf16ToAnsi(LPCWSTR utf16_str);
+#endif
+    
+    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
+    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
+    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
+    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
+    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
+    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17, GTEST_TEMPLATE_ T18,
+    GTEST_TEMPLATE_ T19, GTEST_TEMPLATE_ T20, GTEST_TEMPLATE_ T21,
+    GTEST_TEMPLATE_ T22, GTEST_TEMPLATE_ T23, GTEST_TEMPLATE_ T24,
+    GTEST_TEMPLATE_ T25, GTEST_TEMPLATE_ T26, GTEST_TEMPLATE_ T27,
+    GTEST_TEMPLATE_ T28, GTEST_TEMPLATE_ T29, GTEST_TEMPLATE_ T30,
+    GTEST_TEMPLATE_ T31, GTEST_TEMPLATE_ T32, GTEST_TEMPLATE_ T33,
+    GTEST_TEMPLATE_ T34, GTEST_TEMPLATE_ T35, GTEST_TEMPLATE_ T36,
+    GTEST_TEMPLATE_ T37, GTEST_TEMPLATE_ T38, GTEST_TEMPLATE_ T39,
+    GTEST_TEMPLATE_ T40, GTEST_TEMPLATE_ T41, GTEST_TEMPLATE_ T42,
+    GTEST_TEMPLATE_ T43, GTEST_TEMPLATE_ T44, GTEST_TEMPLATE_ T45,
+    GTEST_TEMPLATE_ T46, GTEST_TEMPLATE_ T47>
+struct Templates47 {
+  typedef TemplateSel<T1> Head;
+  typedef Templates46<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+      T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
+      T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42,
+      T43, T44, T45, T46, T47> Tail;
+};
+    
+      virtual bool IsPrime(int n) const {
+    return 0 <= n && n < is_prime_size_ && is_prime_[n];
+  }
+    
+    
+// This sample shows how to write a simple unit test for a function,
+// using Google C++ testing framework.
+//
+// Writing a unit test using Google C++ testing framework is easy as 1-2-3:
+    
+    #ifndef GTEST_SAMPLES_SAMPLE3_INL_H_
+#define GTEST_SAMPLES_SAMPLE3_INL_H_
+    
+    	void disable_collisions_between_bodies(const bool p_disabled);
+	_FORCE_INLINE_ bool is_disabled_collisions_between_bodies() const { return disabled_collisions_between_bodies; }
+    
+    bool GodotCollisionDispatcher::needsResponse(const btCollisionObject *body0, const btCollisionObject *body1) {
+	if (body0->getUserIndex() == CASTED_TYPE_AREA || body1->getUserIndex() == CASTED_TYPE_AREA) {
+		// Avoide area narrow phase
+		return false;
+	}
+	return btCollisionDispatcher::needsResponse(body0, body1);
 }
-    
-    #endif  // THIRD_PARTY_TESSERACT_CCUTIL_DOUBLEPTR_H_
 
     
-    template <class R, class P1, class P2, class P3>
-inline typename _TessFunctionResultCallback_3_0<false,R,P1,P2,P3>::base*
-NewPermanentTessCallback(R (*function)(P1,P2,P3), typename Identity<P1>::type p1, typename Identity<P2>::type p2, typename Identity<P3>::type p3) {
-  return new _TessFunctionResultCallback_3_0<false,R,P1,P2,P3>(function, p1, p2, p3);
-}
+    public:
+	PinJointBullet(RigidBodyBullet *p_body_a, const Vector3 &p_pos_a, RigidBodyBullet *p_body_b, const Vector3 &p_pos_b);
+	~PinJointBullet();
     
-    const char *kUTF8LineSeparator = '\u2028';  // '\xe2\x80\xa8';
-const char *kUTF8ParagraphSeparator = '\u2029';  // '\xe2\x80\xa9';
-const char *kLRM = '\u200E';  // Left-to-Right Mark
-const char *kRLM = '\u200F';  // Right-to-Left Mark
-const char *kRLE = '\u202A';  // Right-to-Left Embedding
-const char *kPDF = '\u202C';  // Pop Directional Formatting
+    public:
+	_FORCE_INLINE_ void set_self(const RID &p_self) { self = p_self; }
+	_FORCE_INLINE_ RID get_self() const { return self; }
     
-    // Helper to expand a box in one of the 4 directions by the given pad,
-// provided it does not expand into any cell with a zero noise density.
-// If that is not possible, try expanding all round by a small constant.
-static TBOX AttemptBoxExpansion(const TBOX& box, const IntGrid& noise_density,
-                                int pad) {
-  TBOX expanded_box(box);
-  expanded_box.set_right(box.right() + pad);
-  if (!noise_density.AnyZeroInRect(expanded_box))
-    return expanded_box;
-  expanded_box = box;
-  expanded_box.set_left(box.left() - pad);
-  if (!noise_density.AnyZeroInRect(expanded_box))
-    return expanded_box;
-  expanded_box = box;
-  expanded_box.set_top(box.top() + pad);
-  if (!noise_density.AnyZeroInRect(expanded_box))
-    return expanded_box;
-  expanded_box = box;
-  expanded_box.set_bottom(box.bottom() + pad);
-  if (!noise_density.AnyZeroInRect(expanded_box))
-    return expanded_box;
-  expanded_box = box;
-  expanded_box.pad(kNoisePadding, kNoisePadding);
-  if (!noise_density.AnyZeroInRect(expanded_box))
-    return expanded_box;
-  return box;
+    
+    {#ifdef TOOLS_ENABLED
+	EditorPlugins::add_by_type<EditorPluginCSG>();
+#endif
+#endif
 }
     
     
-    {  // Max entry in noise_density_ before the cell is declared noisy.
-  int max_noise_count_;
-  // Completed noise density map, which we keep around to use for secondary
-  // noise detection.
-  IntGrid* noise_density_;
-};
+    {		String path;
+		String src_path;
+		int size;
+		uint64_t offset_offset;
+	};
+	Vector<File> files;
     
-      const auto result =
-      RunScenario(client_config, 1, server_config, 1, WARMUP, BENCHMARK, -2, '',
-                  kInsecureCredentialsType, false);
+    namespace CNTK
+{
+    const std::wstring versionKey = L'version';
+    const std::wstring typeKey = L'type';
+    const std::wstring uidKey = L'uid';
+    const std::wstring kindKey = L'kind';
+    const std::wstring dataTypeKey = L'data_type';
+    const std::wstring dynamicAxisKey = L'dynamic_axis';
+    const std::wstring isSparseKey = L'is_sparse';
+    const std::wstring nameKey = L'name';
+    const std::wstring needsGradientKey = L'needs_gradient';
+    const std::wstring shapeKey = L'shape';
+    const std::wstring valueKey = L'value';
+    const std::wstring opKey = L'op';
+    const std::wstring attributesKey = L'attributes';
+    const std::wstring inputsKey = L'inputs';
+    const std::wstring rootKey = L'root';
+    const std::wstring functionsKey = L'primitive_functions';
+    const std::wstring sampleCountKey = L'sample_count';
+    const std::wstring minibatchCountKey = L'minibatchCount'; // TODO: Python-style spelling
+    const std::wstring sweepCountKey = L'sweepCount';
+    const std::wstring unitKey = L'unit';
+    const std::wstring refMBSizeKey = L'ref_mb_size';
+    const std::wstring epochSizeKey = L'epoch_size';
+    const std::wstring scheduleKey = L'schedule';
+    const std::wstring learningRateScheduleKey = L'learnig_rate_schedule';
+    const std::wstring smoothedGradientsKey = L'smoothed_gradients';
+    const std::wstring noiseInjectionSeedKey = L'noise_injection_seed';
+    const std::wstring masterParameterUpdatedKey = L'master_parameter_updated';
+    const std::wstring smoothedCountKey = L'smoothed_count';
+    const std::wstring stateKey = L'state';
+    const std::wstring rngSeedKey = L'rng_seed';
+    const std::wstring rngOffsetKey = L'rng_offset';
+    const std::wstring blockFunctionCompositeKey = L'block_function_composite';
+    const std::wstring blockFunctionOpNameKey = L'block_function_op_name';
+    const std::wstring blockFunctionCompositeArgumentsMapKeysKey = L'block_function_composite_arguments_map_keys';
+    const std::wstring blockFunctionCompositeArgumentsMapValuesKey = L'block_function_composite_arguments_map_values';
+    const std::wstring internalWorkerStateKey = L'internal_worker_state';
+    const std::wstring externalWorkerStateKey = L'external_worker_state';
+    const std::wstring userDefinedStateKey = L'user_defined_state';
+    const std::wstring udfModuleNameKey = L'module';
+    const std::wstring udfFactoryMethodNameKey = L'deserialize_method';
+    const std::wstring nativeUDFKey = L'native';
+    }
     
-    // Get leading or trailing comments in a string. Comment lines start with '// '.
-// Leading detached comments are put in in front of leading comments.
-template <typename DescriptorType>
-inline grpc::string GetNodeComments(const DescriptorType* desc, bool leading) {
-  return grpc_generator::GetPrefixedComments(desc, leading, '//');
+    template <typename TVector>
+inline void RandomShuffleMT(TVector& v, std::mt19937_64& rng)
+{
+    foreach_index(currentLocation, v)
+    {
+        // Pick a random location and swap with current one
+        const size_t randomLocation = RandMT(0, v.size(), rng);
+        std::swap(v[currentLocation], v[randomLocation]);
+    }
 }
     
-    UsageTimer::Result UsageTimer::Sample() {
-  Result r;
-  r.wall = Now();
-  get_resource_usage(&r.user, &r.system);
-  r.total_cpu_time = 0;
-  r.idle_cpu_time = 0;
-  get_cpu_usage(&r.total_cpu_time, &r.idle_cpu_time);
-  return r;
+    #include <osquery/config.h>
+    
+      QueryLogItem item;
+  item.epoch = 0L;
+  item.counter = 0L;
+  getDecorations(item.decorations);
+  ASSERT_EQ(item.decorations.size(), 2U);
+  EXPECT_EQ(item.decorations.at('internal_60_test'), 'test');
+    
+      ASSERT_TRUE(data.doc().HasMember('events'));
+  ASSERT_TRUE(data.doc()['events'].HasMember('environment_variables'));
+  ASSERT_TRUE(data.doc()['events']['environment_variables'].IsArray());
+  for (const auto& var :
+       data.doc()['events']['environment_variables'].GetArray()) {
+    std::string value = var.GetString();
+    EXPECT_TRUE(value == 'foo' || value == 'bar');
+  }
+    
+    #include <osquery/core.h>
+#include <osquery/filesystem.h>
+#include <osquery/flags.h>
+#include <osquery/packs.h>
+    
+    /**
+ * @brief Compute a hash digest from the file content at a path.
+ *
+ * @param hash_type The osquery-supported hash algorithm.
+ * @param path Filesystem path (the hash target).
+ * @return A string (hex) representation of the hash digest.
+ */
+std::string hashFromFile(HashType hash_type, const std::string& path);
+    
+        // Call the diffing utility directly.
+    DiffResults expected = diff(previous_qd, result.second);
+    EXPECT_EQ(dr, expected);
+    
+    static int stb_not_crap(int best, int dist)
+{
+    return   ((best > 2  &&  dist <= 0x00100)     
+        || (best > 5  &&  dist <= 0x04000)
+        || (best > 7  &&  dist <= 0x80000));
 }
     
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+    }
     
-    {  const Result start_;
-};
+        // Setup Dear ImGui binding
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
     
-    #include 'grpc_tools/main.h'
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
