a87bd7773a31eab6b5dc319eff9a142153dbe3a3
SUNsung <weatherius@gmail.com>
2018-10-21 21:08:24 +0300

    Rewrite [23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp] File in: 2018-10-21 21:08:23

--- cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
+++ cccccc/23o47LqGoAHZs23mFGc2dXi5h2236lEj3SakVEw23v9s.cpp
@@ -1,240 +1,256 @@
 
         
-        #include 'base/values.h'
-#include 'base/strings/utf_string_conversions.h'
-#include 'base/strings/string16.h'
-#include 'content/nw/src/api/dispatcher_host.h'
-#include 'ui/base/clipboard/clipboard.h'
+        Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-    
-    {}
-    
-      keyval = GetKeycodeFromText(key);
-  if (keyval == ui::VKEY_UNKNOWN){
-    enable_shortcut_ = false;
-  } else {
-    enable_shortcut_ = true;
-    //only code for ctrl, shift, alt, super and meta modifiers
-    int modifiers_value = ui::EF_NONE;
-    modifiers = base::ToLowerASCII(modifiers);
-    if (modifiers.find('ctrl')!=std::string::npos){
-      modifiers_value |= ui::EF_CONTROL_DOWN;
+    namespace tensorflow {
+namespace swig {
     }
-    if (modifiers.find('shift')!=std::string::npos){
-      modifiers_value |= ui::EF_SHIFT_DOWN ;
-    }
-    if (modifiers.find('alt')!=std::string::npos){
-      modifiers_value |= ui::EF_ALT_DOWN;
-    }
-    if (modifiers.find('super')!=std::string::npos
-     || modifiers.find('cmd')!=std::string::npos
-     || modifiers.find('command')!=std::string::npos){
-      modifiers_value |= ui::EF_COMMAND_DOWN;
-    }
-    if (modifiers.find('meta')!=std::string::npos){
-      meta_down_flag_ = true;
-    }
-    accelerator_ = ui::Accelerator(keyval,modifiers_value);
-  }
-    
-        bool ReadImage(ClipboardData& data) {
-      DCHECK(data.type == TYPE_PNG || data.type == TYPE_JPEG);
-      std::vector<unsigned char> encoded_image;
-      SkBitmap bitmap = clipboard_->ReadImage(ui::CLIPBOARD_TYPE_COPY_PASTE);
     }
     
-    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
+    // Names that corresponds to a single input parameter.
+class ParamNames {
  public:
-  NwMenuGetNSStringFWithFixupFunction() {}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    
- protected:
-  ~NwMenuGetNSStringFWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
-};
-    
-        std::string type;
-    switch(src.id.type) {
-    case content::DesktopMediaID::TYPE_WINDOW:
-      type = 'window';
-      break;
-    case content::DesktopMediaID::TYPE_SCREEN:
-      type = 'screen';
-      break;
-    case content::DesktopMediaID::TYPE_NONE:
-      type = 'none';
-      break;
-    default:
-      type = 'unknown';
-      break;
+  // Create param based on Arg.
+  ParamNames(const string& name, const string& rename_to) : name_(name) {
+    rename_to_ = AvoidPythonReserved(rename_to);
+  }
     }
     
-      // implement nw.Screen.getScreens()
-  class NwScreenGetScreensFunction: public NWSyncExtensionFunction {
-    public:
-      NwScreenGetScreensFunction();
-      bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    
-    {    struct KeypointStore {
-        virtual void push(f32 kpX, f32 kpY, f32 kpSize, f32 kpAngle=-1, f32 kpResponse=0, s32 kpOctave=0, s32 kpClass_id=-1) = 0;
-        virtual ~KeypointStore() {};
-    };
-}
-    
-    
-    {            vst1q_s16(dst + j, v_dst0);
-            vst1q_s16(dst + j + 8, v_dst1);
-        }
-        for (; j < roiw8; j += 8)
-        {
-            int16x8_t v_src = vreinterpretq_s16_u16(vmovl_u8(vld1_u8(src + j)));
-            int16x8_t v_dst = vld1q_s16(dst + j);
-            int16x4_t v_srclo = vget_low_s16(v_src), v_srchi = vget_high_s16(v_src);
-            v_dst = vcombine_s16(vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srclo, v_srclo), shift), vget_low_s16(v_dst))),
-                                 vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srchi, v_srchi), shift), vget_high_s16(v_dst))));
-            vst1q_s16(dst + j, v_dst);
-        }
-    
-    SPLIT(u, 8,2)
-SPLIT(u, 8,3)
-SPLIT(u, 8,4)
-SPLIT(u,16,2)
-SPLIT(u,16,3)
-SPLIT(u,16,4)
-SPLIT(s,32,2)
-SPLIT(s,32,3)
-SPLIT(s,32,4)
-    
-    void combineUYVY(const Size2D &size,
-                 const u8 * srcyBase, ptrdiff_t srcyStride,
-                 const u8 * srcuBase, ptrdiff_t srcuStride,
-                 const u8 * srcvBase, ptrdiff_t srcvStride,
-                 u8 * dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-#ifndef __ANDROID__
-    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
-#endif
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
-    
-    /*!
- *  Aligns pointer by the certain number of bytes
- *
- *  This small inline function aligns the pointer by the certain number of bytes by shifting
- *  it forward by 0 or a positive offset.
- */
-template<typename T> inline T* alignPtr(T* ptr, size_t n=sizeof(T))
-{
-    return (T*)(((size_t)ptr + n-1) & -n);
-}
-    
-        if (flipMode == FLIP_VERTICAL_MODE)
-    {
-        for (size_t y = 0; y < size.height; ++y)
-        {
-            const u8 * src_row = internal::getRowPtr(srcBase, srcStride, y);
-            u8 * dst_row = internal::getRowPtr(dstBase, dstStride, size.height - y - 1);
+    void CostAnalyzer::PreprocessCosts() {
+  for (int i = 0; i < op_perf_.op_performance_size(); i++) {
+    OpPerformance* perf = op_perf_.mutable_op_performance(i);
+    const OpPerformance& analytical = op_perf_analytical_.op_performance(i);
+    perf->set_compute_time(analytical.compute_time());
+    perf->set_memory_time(analytical.memory_time());
+    double measured_cost = perf->compute_cost();
     }
     }
     
-        // Setup Vulkan binding
-    ImGui_ImplVulkan_InitInfo init_info = {};
-    init_info.Instance = g_Instance;
-    init_info.PhysicalDevice = g_PhysicalDevice;
-    init_info.Device = g_Device;
-    init_info.QueueFamily = g_QueueFamily;
-    init_info.Queue = g_Queue;
-    init_info.PipelineCache = g_PipelineCache;
-    init_info.DescriptorPool = g_DescriptorPool;
-    init_info.Allocator = g_Allocator;
-    init_info.CheckVkResultFn = check_vk_result;
-    ImGui_ImplVulkan_Init(&init_info, wd->RenderPass);
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-        glutMainLoop();
+    #ifndef TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
     
-        {
-        D3D12_DESCRIPTOR_HEAP_DESC desc = {};
-        desc.Type           = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
-        desc.NumDescriptors = NUM_BACK_BUFFERS;
-        desc.Flags          = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-        desc.NodeMask       = 1;
-        if (g_pd3dDevice->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&g_pd3dRtvDescHeap)) != S_OK)
-            return E_FAIL;
-    }
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/platform/mutex.h'
+#include 'tensorflow/python/lib/core/bfloat16.h'
+#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
     
-        // Create depth-stencil State
-    {
-        D3D11_DEPTH_STENCIL_DESC desc;
-        ZeroMemory(&desc, sizeof(desc));
-        desc.DepthEnable = false;
-        desc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
-        desc.DepthFunc = D3D11_COMPARISON_ALWAYS;
-        desc.StencilEnable = false;
-        desc.FrontFace.StencilFailOp = desc.FrontFace.StencilDepthFailOp = desc.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
-        desc.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
-        desc.BackFace = desc.FrontFace;
-        g_pd3dDevice->CreateDepthStencilState(&desc, &g_pDepthStencilState);
-    }
-    
-            D3D12_COMMAND_QUEUE_DESC queueDesc = {};
-        queueDesc.Type     = D3D12_COMMAND_LIST_TYPE_DIRECT;
-        queueDesc.Flags    = D3D12_COMMAND_QUEUE_FLAG_NONE;
-        queueDesc.NodeMask = 1;
-    
-    // About GLSL version:
-// The 'glsl_version' initialization parameter should be NULL (default) or a '#version XXX' string.
-// On computer platform the GLSL version default to '#version 130'. On OpenGL ES 3 platform it defaults to '#version 300 es'
-// Only override if your GL version doesn't handle this GLSL version. See GLSL version table at the top of imgui_impl_opengl3.cpp.
-    
-    static const int kCrToRedTable[256] = {
-  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
-  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
-  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
-  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
-  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
-   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
-   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
-   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
-   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
-   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
-   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
-     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
-    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
-    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
-    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
-    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
-    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
-   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
-   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
-   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
-   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
-   174,  175,  177,  178
-};
-    
-      // Returns the distance map between the baseline image and the image in the
-  // last Compare() call (or the baseline image, if Compare() was not called
-  // yet).
-  // The dimensions of the distance map are the same as the baseline image.
-  // The interpretation of the distance values depend on the comparator used.
-  virtual const std::vector<float> distmap() const = 0;
-    
-    #include 'guetzli/fdct.h'
-    
-    namespace guetzli {
-    }
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
     
-    {}  // namespace guetzli
+    {}  // namespace tensorflow
 
     
-    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
-// quantization table must have 3 * kDCTBlockSize values.
-bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     const int* quant, JPEGData* jpg);
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    ScopedActivateExecutorContext::ScopedActivateExecutorContext(
+    ScopedActivateExecutorContext &&other)
+    : driver_scoped_activate_context_(other.driver_scoped_activate_context_) {
+  other.driver_scoped_activate_context_ = nullptr;
+}
+    
+      CFDictionaryRef cuda_driver_info = nullptr;
+  if (CFDictionaryGetValueIfPresent(kext_infos, kDriverKextIdentifier,
+                                    (const void **)&cuda_driver_info)) {
+    bool started = CFBooleanGetValue((CFBooleanRef)CFDictionaryGetValue(
+        cuda_driver_info, CFSTR('OSBundleStarted')));
+    if (!started) {
+      LOG(INFO) << 'kernel driver is installed, but does not appear to be '
+                   'running on this host '
+                << '(' << port::Hostname() << ')';
+    }
+  } else {
+    LOG(INFO) << 'kernel driver does not appear to be installed on this host '
+              << '(' << port::Hostname() << ')';
+  }
+  CFRelease(kext_infos);
+#elif !defined(PLATFORM_WINDOWS)
+  if (access(kDriverVersionPath, F_OK) != 0) {
+    LOG(INFO) << 'kernel driver does not appear to be running on this host '
+              << '(' << port::Hostname() << '): '
+              << '/proc/driver/nvidia/version does not exist';
+    return;
+  }
+  auto dev0_path = GetDevNodePath(0);
+  if (access(dev0_path.c_str(), F_OK) != 0) {
+    LOG(INFO) << 'no NVIDIA GPU device is present: ' << dev0_path
+              << ' does not exist';
+    return;
+  }
+#endif
+    
+    class AutoUpdater : public mate::EventEmitter<AutoUpdater>,
+                    public auto_updater::Delegate,
+                    public WindowListObserver {
+ public:
+  static mate::Handle<AutoUpdater> Create(v8::Isolate* isolate);
+    }
+    
+    void BrowserView::SetBackgroundColor(const std::string& color_name) {
+  view_->SetBackgroundColor(ParseHexColor(color_name));
+}
+    
+    #include 'atom/common/native_mate_converters/callback.h'
+#include 'atom/common/native_mate_converters/file_path_converter.h'
+#include 'base/bind.h'
+#include 'base/files/file_util.h'
+#include 'content/public/browser/tracing_controller.h'
+#include 'native_mate/dictionary.h'
+    
+    #include 'atom/browser/api/atom_api_net.h'
+#include 'atom/browser/api/atom_api_url_request.h'
+#include 'atom/common/node_includes.h'
+#include 'native_mate/dictionary.h'
+    
+    namespace mate {
+template <>
+struct Converter<ui::IdleState> {
+  static v8::Local<v8::Value> ToV8(v8::Isolate* isolate,
+                                   const ui::IdleState& in) {
+    switch (in) {
+      case ui::IDLE_STATE_ACTIVE:
+        return mate::StringToV8(isolate, 'active');
+      case ui::IDLE_STATE_IDLE:
+        return mate::StringToV8(isolate, 'idle');
+      case ui::IDLE_STATE_LOCKED:
+        return mate::StringToV8(isolate, 'locked');
+      case ui::IDLE_STATE_UNKNOWN:
+      default:
+        return mate::StringToV8(isolate, 'unknown');
+    }
+  }
+};
+}  // namespace mate
+    
+      // The handle of the module that contains the window procedure of |window_|.
+  HMODULE instance_;
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(RenderProcessPreferences);
+};
+    
+    class Tray : public mate::TrackableObject<Tray>, public TrayIconObserver {
+ public:
+  static mate::WrappableBase* New(mate::Handle<NativeImage> image,
+                                  mate::Arguments* args);
+    }
+    
+    public:
+  /// Form storage for the given generic signature and its replacement
+  /// types and conformances.
+  static Storage *get(GenericSignature *genericSig,
+                      ArrayRef<Type> replacementTypes,
+                      ArrayRef<ProtocolConformanceRef> conformances);
+    
+    
+    {
+    {    if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Decl *>()) {
+      SemanticNode.get<Decl *>()->dump(llvm::errs());
+    } else if (SemanticNode.is<Expr *>()) {
+      SemanticNode.get<Expr *>()->dump(llvm::errs());
+    } else {
+      llvm_unreachable('ASTNode has pointer to unknown thing!');
+    }
+    llvm::errs() << '\n=====================================================\n';
+  }
+}
+
+    
+    /// Maintain a set of known CF types.
+static bool isKnownCFTypeName(StringRef name) {
+  return std::binary_search(KnownCFTypes, KnownCFTypes + NumKnownCFTypes,
+                            name, SortByLengthComparator());
+}
+    
+      bool isVoid() const {
+    assert(isValid());
+    return Decl.isNull();
+  }
+    
+      // Satisfy the default implementation (bookkeeping).
+  if (DumpToStderr)
+    TextDiagnosticPrinter::HandleDiagnostic(clangDiagLevel, clangDiag);
+  else
+    DiagnosticConsumer::HandleDiagnostic(clangDiagLevel, clangDiag);
+    
+    protected:
+	SpaceBullet *space;
+	btTypedConstraint *constraint;
+	bool disabled_collisions_between_bodies;
+    
+    #ifndef _3D_DISABLED
+    
+    #ifdef __ANDROID__
+#include 'platform/android/thread_jandroid.h'
+#else
+#define JNIEnv void
+#define jobject void *
+#endif
+    
+    size_t MemoryPool::total_memory = 0;
+size_t MemoryPool::max_memory = 0;
+    
+    static long godot_tell(voidpf opaque, voidpf stream) {
+    }
+    
+      port::Mutex* GetMutex() {
+    return &mu_;
+  }
+    
+      auto defaultEnv = Env::Default();
+  int hits = 0;
+  for (auto it = fileNames.begin() ; it != fileNames.end(); ++it) {
+    if ((*it == '..') || (*it == '.')) {
+      continue;
+    }
+    auto filePath = dbname_ + '/' + *it;
+    unique_ptr<SequentialFile> seqFile;
+    auto envOptions = EnvOptions(CurrentOptions());
+    status = defaultEnv->NewSequentialFile(filePath, &seqFile, envOptions);
+    ASSERT_OK(status);
+    }
+    
+      virtual Status FileExists(const std::string& fname) override {
+    auto status_and_enc_path = EncodePathWithNewBasename(fname);
+    if (!status_and_enc_path.first.ok()) {
+      return status_and_enc_path.first;
+    }
+    return EnvWrapper::FileExists(status_and_enc_path.second);
+  }
+    
+    #if !defined(ROCKSDB_LITE) && !defined(OS_WIN)
+    
+      // Use the returned alignment value to allocate
+  // aligned buffer for Direct I/O
+  virtual size_t GetRequiredBufferAlignment() const override { return file_->GetRequiredBufferAlignment(); } 
+    
+      int ret = system('rm -rf /tmp/rocksmergetest');
+  if (ret != 0) {
+    fprintf(stderr, 'Error deleting /tmp/rocksmergetest, code: %d\n', ret);
+    return ret;
+  }
+  rocksdb::Options options;
+  options.create_if_missing = true;
+  options.merge_operator.reset(new MyMerge);
+  options.compaction_filter = &filter;
+  status = rocksdb::DB::Open(options, '/tmp/rocksmergetest', &raw_db);
+  assert(status.ok());
+  std::unique_ptr<rocksdb::DB> db(raw_db);
