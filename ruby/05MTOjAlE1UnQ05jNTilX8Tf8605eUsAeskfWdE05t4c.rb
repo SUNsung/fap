e888167df44ae22e6b2e3a25fb53cbaef4cb7fc5
SUNsung <weatherius@gmail.com>
2018-10-21 15:41:44 +0300

    Rewrite [05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb] File in: 2018-10-21 15:41:43

--- ruby/05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb
+++ ruby/05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb
@@ -1,98 +1,200 @@
 
         
-          def collection_presenter
-    ActivityPub::CollectionPresenter.new(
-      id: account_collection_url(@account, params[:id]),
-      type: :ordered,
-      size: @size,
-      items: @statuses
-    )
-  end
+        # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
 end
-
     
-    class ActivityPub::InboxesController < Api::BaseController
-  include SignatureVerification
+    # -------------------------------------------------------------------
+# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
+# -------------------------------------------------------------------
     
-      def page_requested?
-    params[:page] == 'true'
-  end
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-        def filter_params
-      params.permit(
-        :local,
-        :remote,
-        :by_domain,
-        :silenced,
-        :recent,
-        :suspended,
-        :username,
-        :display_name,
-        :email,
-        :ip,
-        :staff
-      )
-    end
-  end
-end
-
+    STDOUT.sync = true
     
-        def create
-      authorize :custom_emoji, :create?
+    #
     
-      def status_finder
-    StatusFinder.new(params[:url])
-  end
-    
-        # we assume that the first file that requires 'sinatra' is the
-    # app_file. all other path related options are calculated based
-    # on this path by default.
-    set :app_file, caller_files.first || $0
-    
-          def remove_bad_cookies(request, response)
-        return if bad_cookies.empty?
-        paths = cookie_paths(request.path)
-        bad_cookies.each do |name|
-          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
-        end
+          def feature_element_timing_key(feature_element)
+        '\'#{feature_element.name}\' (#{feature_element.location})'
       end
     
-        it 'sets a session authenticity token if one does not exist' do
-      session = {}
-      allow(Rack::Protection::AuthenticityToken).to receive(:random_token).and_return(token)
-      allow_any_instance_of(Rack::Protection::AuthenticityToken).to receive(:mask_token).and_return(masked_token)
-      Rack::Protection::AuthenticityToken.token(session)
-      expect(session[:csrf]).to eq(token)
-    end
-  end
+    module Jekyll
+  module Commands
+    class NewTheme < Jekyll::Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:'new-theme') do |c|
+            c.syntax 'new-theme NAME'
+            c.description 'Creates a new Jekyll theme scaffold'
+            c.option 'code_of_conduct', \
+                     '-c', '--code-of-conduct', \
+                     'Include a Code of Conduct. (defaults to false)'
     
-        it 'Returns nil when Referer header is invalid' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
-      expect(subject.referrer(env)).to be_nil
+        brew cask install mactex
+    EOS
+  when 'pip' then <<-EOS.undent
+    Homebrew provides pip via: `brew install python`. However you will then
+    have two Pythons installed on your Mac, so alternatively you can install
+    pip via the instructions at:
+    
+    def bottle_tag
+  if MacOS.version >= :lion
+    MacOS.cat
+  elsif MacOS.version == :snow_leopard
+    Hardware::CPU.is_64_bit? ? :snow_leopard : :snow_leopard_32
+  else
+    # Return, e.g., :tiger_g3, :leopard_g5_64, :leopard_64 (which is Intel)
+    if Hardware::CPU.type == :ppc
+      tag = '#{MacOS.cat}_#{Hardware::CPU.family}'.to_sym
+    else
+      tag = MacOS.cat
     end
+    MacOS.prefer_64_bit? ? '#{tag}_64'.to_sym : tag
   end
 end
-
     
-          it 'autocorrects closing brace on same line from last element' do
-        new_source = autocorrect_source(construct(true, false))
+        # Get rid of any info 'dir' files, so they don't conflict at the link stage
+    info_dir_file = @f.info + 'dir'
+    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
+      observe_file_removal info_dir_file
+    end
     
-    shared_examples_for 'multiline literal brace layout method argument' do
-  include MultilineLiteralBraceHelper
+            if file_is_stale || ARGV.switch?('s') && !f.installed? || bottle_file_outdated?(f, file)
+          cleanup_path(file) { file.unlink }
+        end
+      end
+    end
     
-          # Calls the given block for each `when` node in the `case` statement.
-      # If no block is given, an `Enumerator` is returned.
+        # Unbrewed uses the PREFIX, which will exist
+    # Things below use the CELLAR, which doesn't until the first formula is installed.
+    unless HOMEBREW_CELLAR.exist?
+      raise NoSuchKegError.new(ARGV.named.first) if ARGV.named.any?
+      return
+    end
+    
+      def migrate_formula_rename
+    report[:R].each do |old_full_name, new_full_name|
+      old_name = old_full_name.split('/').last
+      next unless (dir = HOMEBREW_CELLAR/old_name).directory? && !dir.subdirs.empty?
+    
+    # See browser for an example
+class GithubGistFormula < ScriptFileFormula
+  def self.url(val)
+    super
+    version File.basename(File.dirname(val))[0, 6]
+  end
+end
+    
+    gem 'activemodel-serializers-xml', github: 'rails/activemodel-serializers-xml'
+    
+      def passthru
+    render status: 404, plain: 'Not found. Authentication passthru.'
+  end
+    
+        login_as User.create!(email: 'test@test.com', password: 'test123456', password_confirmation: 'test123456')
+    
+      # Constants which holds devise configuration for extensions. Those should
+  # not be modified by the 'end user' (this is why they are constants).
+  ALL         = []
+  CONTROLLERS = {}
+  ROUTES      = {}
+  STRATEGIES  = {}
+  URL_HELPERS = {}
+    
+        # Returns the specified or detected guest type name.
+    #
+    # @return [Symbol]
+    def name
+      capability_host_chain[0][0]
+    end
+    
+            # This is called as a last-minute hook that allows the configuration
+        # object to finalize itself before it will be put into use. This is
+        # a useful place to do some defaults in the case the user didn't
+        # configure something or so on.
+        #
+        # An example of where this sort of thing is used or has been used:
+        # the 'vm' configuration key uses this to make sure that at least
+        # one sub-VM has been defined: the default VM.
+        #
+        # The configuration object is expected to mutate itself.
+        def finalize!
+          # Default implementation is to do nothing.
+        end
+    
+    def cloned_testpath(path)
+  repo   = File.expand_path(testpath(path))
+  path   = File.dirname(repo)
+  cloned = File.join(path, self.class.name)
+  FileUtils.rm_rf(cloned)
+  Dir.chdir(path) do
+    %x{git clone #{File.basename(repo)} #{self.class.name} 2>/dev/null}
+  end
+  cloned
+end
+    
+      # replace name version and date
+  replace_header(head, :name)
+  replace_header(head, :version)
+  replace_header(head, :date)
+  #comment this out if your rubyforge_project has a different name
+  replace_header(head, :rubyforge_project)
+    
+            def check(arg, args)
+          if style == :braces && !arg.braces?
+            add_arg_offense(arg, :missing)
+          elsif style == :no_braces && arg.braces?
+            add_arg_offense(arg, :redundant)
+          elsif style == :context_dependent
+            check_context_dependent(arg, args)
+          end
+        end
+    
+      include_examples 'multiline literal brace layout' do
+    let(:open) { '[' }
+    let(:close) { ']' }
+  end
+    
+          # Checks whether this node body is a void context.
       #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_when
-        return when_branches.to_enum(__method__) unless block_given?
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
+      end
     
     module RuboCop
   module AST
-    # A node extension for `def` nodes. This will be used in place of a plain
+    # A node extension for `for` nodes. This will be used in place of a plain
     # node when the builder constructs the AST, making its methods available
-    # to all `def` nodes within RuboCop.
-    class DefNode < Node
-      include ParameterizedNode
-      include MethodIdentifierPredicates
+    # to all `for` nodes within RuboCop.
+    class ForNode < Node
+      # Returns the keyword of the `for` statement as a string.
+      #
+      # @return [String] the keyword of the `until` statement
+      def keyword
+        'for'
+      end
+    
+            def keyword_splat?
+          [first, second].any?(&:kwsplat_type?)
+        end
+      end
