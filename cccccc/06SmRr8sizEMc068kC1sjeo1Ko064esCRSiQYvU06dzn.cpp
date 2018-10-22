74f28aaf5cfc18037a62fdeb3c237175ff506620
SUNsung <weatherius@gmail.com>
2018-10-22 17:43:31 +0300

    Rewrite [06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp] File in: 2018-10-22 17:43:31

--- cccccc/06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp
+++ cccccc/06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp
@@ -1,279 +1,157 @@
 
         
-          // True when a ScopedPyObjectPtr and a raw pointer refer to the same object.
-  // Comparison operators are non reflexive.
-  bool operator==(const PyObjectStruct* p) const { return ptr_ == p; }
-  bool operator!=(const PyObjectStruct* p) const { return ptr_ != p; }
-    
-    namespace {
-// Whether two fields have conflicting accessors (assuming name1 and name2
-// are different). name1 and name2 are field1 and field2's camel-case name
-// respectively.
-bool IsConflicting(const FieldDescriptor* field1, const string& name1,
-                   const FieldDescriptor* field2, const string& name2,
-                   string* info) {
-  if (field1->is_repeated()) {
-    if (field2->is_repeated()) {
-      // Both fields are repeated.
-      return false;
-    } else {
-      // field1 is repeated, and field2 is not.
-      if (name1 + 'Count' == name2) {
-        *info = 'both repeated field \'' + field1->name() + '\' and singular ' +
-                'field \'' + field2->name() + '\' generate the method \'' +
-                'get' + name1 + 'Count()\'';
-        return true;
-      }
-      if (name1 + 'List' == name2) {
-        *info = 'both repeated field \'' + field1->name() + '\' and singular ' +
-                'field \'' + field2->name() + '\' generate the method \'' +
-                'get' + name1 + 'List()\'';
-        return true;
-      }
-      // Well, there are obviously many more conflicting cases, but it probably
-      // doesn't worth the effort to exhaust all of them because they rarely
-      // happen and as we are continuing adding new methods/changing existing
-      // methods the number of different conflicting cases will keep growing.
-      // We can just add more cases here when they are found in the real world.
-      return false;
-    }
-  } else {
-    if (field2->is_repeated()) {
-      return IsConflicting(field2, name2, field1, name1, info);
-    } else {
-      // None of the two fields are repeated.
-      return false;
-    }
-  }
-}
-}  // namespace
-    
-    #include <google/protobuf/compiler/java/java_context.h>
-#include <google/protobuf/compiler/java/java_doc_comment.h>
-#include <google/protobuf/compiler/java/java_helpers.h>
-#include <google/protobuf/compiler/java/java_name_resolver.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/stubs/strutil.h>
-    
-      // Limit the number of bytes per line.
-  static const int kBytesPerLine = 40;
-  // Limit the number of lines per string part.
-  static const int kLinesPerPart = 400;
-  // Every block of bytes, start a new string literal, in order to avoid the
-  // 64k length limit. Note that this value needs to be <64k.
-  static const int kBytesPerPart = kBytesPerLine * kLinesPerPart;
-  for (int i = 0; i < file_data.size(); i += kBytesPerLine) {
-    if (i > 0) {
-      if (i % kBytesPerPart == 0) {
-        printer->Print(',\n');
-      } else {
-        printer->Print(' +\n');
-      }
-    }
-    printer->Print('\'$data$\'',
-      'data', CEscape(file_data.substr(i, kBytesPerLine)));
-  }
-    
-    void Action::startWithTarget(Node *aTarget)
-{
-    _originalTarget = _target = aTarget;
+        DownloadItem::DownloadItem(v8::Isolate* isolate,
+                           download::DownloadItem* download_item)
+    : download_item_(download_item) {
+  download_item_->AddObserver(this);
+  Init(isolate);
+  AttachAsUserData(download_item);
 }
     
-    EaseBezierAction* EaseBezierAction::reverse() const
-{
-    EaseBezierAction* reverseAction = EaseBezierAction::create(_inner->reverse());
-    reverseAction->setBezierParamer(_p3,_p2,_p1,_p0);
-    return reverseAction;
-}
     
-    FlipX * FlipX::clone() const
-{
-    // no copy constructor
-    return FlipX::create(_flipX);
-}
-//
-// FlipY
-//
+    {}  // namespace api
     
-    class CC_DLL __CCCallFuncO : public CallFunc
-{
-public:
-    /** Creates the action with the callback.
-        typedef void (Ref::*SEL_CallFuncO)(Ref*);
-     *
-     * @param target    A certain target.
-     * @param selector  The callback need to be executed.
-     * @param object    An object as the callback's first argument.
-     * @return An autoreleased __CCCallFuncO object.
-     */
-    CC_DEPRECATED_ATTRIBUTE static __CCCallFuncO * create(Ref* target, SEL_CallFuncO selector, Ref* object);
-    //
-    // Overrides
-    //
-    virtual __CCCallFuncO* clone() const override;
-    virtual void execute() override;
-    
-    Ref* getObject() const;
-    void setObject(Ref* obj);
-    
-CC_CONSTRUCTOR_ACCESS:
-    __CCCallFuncO();
-    virtual ~__CCCallFuncO();
-    /** initializes the action with the callback
-    }
-    
-    void ScaleTo::startWithTarget(Node *target)
-{
-    ActionInterval::startWithTarget(target);
-    _startScaleX = target->getScaleX();
-    _startScaleY = target->getScaleY();
-    _startScaleZ = target->getScaleZ();
-    _deltaX = _endScaleX - _startScaleX;
-    _deltaY = _endScaleY - _startScaleY;
-    _deltaZ = _endScaleZ - _startScaleZ;
-}
-    
-    #endif // __ACTION_CCACTION_MANAGER_H__
+    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_browser_global_shortcut, Initialize)
 
     
+    #include 'atom/browser/api/trackable_object.h'
+#include 'base/callback.h'
+#include 'chrome/browser/extensions/global_shortcut_listener.h'
+#include 'native_mate/handle.h'
+#include 'ui/base/accelerators/accelerator.h'
     
-    {        return true;
+    void Initialize(v8::Local<v8::Object> exports,
+                v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context,
+                void* priv) {
+  v8::Isolate* isolate = context->GetIsolate();
+  Menu::SetConstructor(isolate, base::Bind(&Menu::New));
     }
     
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __ACTION_CCPROGRESS_TIMER_H__
-#define __ACTION_CCPROGRESS_TIMER_H__
+    #endif  // ATOM_BROWSER_API_ATOM_API_NET_H_
+
+    
+    #if defined(OS_LINUX)
+  // Private JS APIs.
+  void BlockShutdown();
+  void UnblockShutdown();
+#endif
+    
+    
+    {}  // namespace api
+    
+    namespace api {
+    }
+    
+      gfx::Point GetCursorScreenPoint();
+  display::Display GetPrimaryDisplay();
+  std::vector<display::Display> GetAllDisplays();
+  display::Display GetDisplayNearestPoint(const gfx::Point& point);
+  display::Display GetDisplayMatching(const gfx::Rect& match_rect);
+    
+    namespace xgboost {
+namespace data {
+/*!
+ * \brief Format specification of SparsePage.
+ */
+class SparsePageFormat {
+ public:
+  /*! \brief virtual destructor */
+  virtual ~SparsePageFormat() = default;
+  /*!
+   * \brief Load all the segments into page, advance fi to end of the block.
+   * \param page The data to read page into.
+   * \param fi the input stream of the file
+   * \return true of the loading as successful, false if end of file was reached
+   */
+  virtual bool Read(SparsePage* page, dmlc::SeekStream* fi) = 0;
+  /*!
+   * \brief read only the segments we are interested in, advance fi to end of the block.
+   * \param page The page to load the data into.
+   * \param fi the input stream of the file
+   * \param sorted_index_set sorted index of segments we are interested in
+   * \return true of the loading as successful, false if end of file was reached
+   */
+  virtual bool Read(SparsePage* page,
+                    dmlc::SeekStream* fi,
+                    const std::vector<bst_uint>& sorted_index_set) = 0;
+  /*!
+   * \brief save the data to fo, when a page was written.
+   * \param fo output stream
+   */
+  virtual void Write(const SparsePage& page, dmlc::Stream* fo) = 0;
+  /*!
+   * \brief Create sparse page of format.
+   * \return The created format functors.
+   */
+  static SparsePageFormat* Create(const std::string& name);
+  /*!
+   * \brief decide the format from cache prefix.
+   * \return pair of row format, column format type of the cache prefix.
+   */
+  static std::pair<std::string, std::string> DecideFormat(const std::string& cache_prefix);
+};
+    }
+    }
+    
+          ASSERT_TRUE(input == output);
+    
+    #include <folly/Executor.h>
+#include <folly/portability/GTest.h>
+    
+      fe([&](int x) { sum += x; });
+    
+    
+    {  FLAGS_minloglevel = prev;
+}
+    
+    static intptr_t getAddr(const std::vector<uint8_t>& v, size_t& pos) {
+  pos += kAddrWidth;
+  return folly::Endian::little(
+      folly::loadUnaligned<intptr_t>(v.data() + pos - kAddrWidth));
+}
+    
+    #include <folly/Executor.h>
     
     /**
-@brief FadeOutDownTiles action.
-@details Fades out the target node with many tiles from top to bottom.
+ * A PRNG with one instance per thread. This PRNG uses a mersenne twister random
+ * number generator and is seeded from /dev/urandom. It should not be used for
+ * anything which requires security, only for statistical randomness.
+ *
+ * An instance of this class represents the current threads PRNG. This means
+ * copying an instance of this class across threads will result in corruption
+ *
+ * Most users will use the Random class which implicitly creates this class.
+ * However, if you are worried about performance, you can memoize the TLS
+ * lookups that get the per thread state by manually using this class:
+ *
+ * ThreadLocalPRNG rng;
+ * for (...) {
+ *   Random::rand32(rng);
+ * }
  */
-class CC_DLL FadeOutDownTiles : public FadeOutUpTiles
-{
-public:
-    /** 
-    * @brief Create the action with the grid size and the duration.
-    * @param duration Specify the duration of the FadeOutDownTiles action. It's a value in seconds.
-    * @param gridSize Specify the size of the grid.
-    * @return If the creation success, return a pointer of FadeOutDownTiles action; otherwise, return nil.
-    */
-    static FadeOutDownTiles* create(float duration, const Size& gridSize);
+class ThreadLocalPRNG {
+ public:
+  using result_type = uint32_t;
     }
     
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __CCACTIONTWEEN_H__
-#define __CCACTIONTWEEN_H__
+      explicit VirtualExecutor(Executor* executor)
+      : VirtualExecutor(getKeepAliveToken(executor)) {}
     
-        /** @deprecated Use getInstance() instead. */
-    CC_DEPRECATED_ATTRIBUTE static AnimationCache* sharedAnimationCache() { return AnimationCache::getInstance(); }
-    
-        static BOOST_FORCEINLINE storage_type exchange(storage_type volatile& storage, storage_type v, memory_order order) BOOST_NOEXCEPT
-    {
-#if defined(__clang__)
-        // Clang cannot allocate eax:edx register pairs but it has sync intrinsics
-        storage_type old_val = storage;
-        while (true)
-        {
-            storage_type val = __sync_val_compare_and_swap(&storage, old_val, v);
-            if (val == old_val)
-                return val;
-            old_val = val;
-        }
-#elif !defined(BOOST_ATOMIC_DETAIL_NO_ASM_IMPLIED_ZERO_DISPLACEMENTS)
-#if defined(__PIC__)
-        uint32_t scratch;
-        __asm__ __volatile__
-        (
-            'movl %%ebx, %[scratch]\n\t'
-            'movl %%eax, %%ebx\n\t'
-            'movl %%edx, %%ecx\n\t'
-            'movl %[dest], %%eax\n\t'
-            'movl 4+%[dest], %%edx\n\t'
-            '.align 16\n\t'
-            '1: lock; cmpxchg8b %[dest]\n\t'
-            'jne 1b\n\t'
-            'movl %[scratch], %%ebx\n\t'
-            : '+A' (v), [scratch] '=m' (scratch), [dest] '+o' (storage)
-            :
-            : BOOST_ATOMIC_DETAIL_ASM_CLOBBER_CC_COMMA 'ecx', 'memory'
-        );
-        return v;
-#else // defined(__PIC__)
-        __asm__ __volatile__
-        (
-            'movl %[dest], %%eax\n\t'
-            'movl 4+%[dest], %%edx\n\t'
-            '.align 16\n\t'
-            '1: lock; cmpxchg8b %[dest]\n\t'
-            'jne 1b\n\t'
-            : '=A' (v), [dest] '+o' (storage)
-            : 'b' ((uint32_t)v), 'c' ((uint32_t)(v >> 32))
-            : BOOST_ATOMIC_DETAIL_ASM_CLOBBER_CC_COMMA 'memory'
-        );
-        return v;
-#endif // defined(__PIC__)
-#else // !defined(BOOST_ATOMIC_DETAIL_NO_ASM_IMPLIED_ZERO_DISPLACEMENTS)
-#if defined(__PIC__)
-        uint32_t scratch;
-        __asm__ __volatile__
-        (
-            'movl %%ebx, %[scratch]\n\t'
-            'movl %%eax, %%ebx\n\t'
-            'movl %%edx, %%ecx\n\t'
-            'movl 0(%[dest]), %%eax\n\t'
-            'movl 4(%[dest]), %%edx\n\t'
-            '.align 16\n\t'
-            '1: lock; cmpxchg8b 0(%[dest])\n\t'
-            'jne 1b\n\t'
-            'movl %[scratch], %%ebx\n\t'
-#if !defined(BOOST_ATOMIC_DETAIL_NO_ASM_CONSTRAINT_ALTERNATIVES)
-            : '+A,A' (v), [scratch] '=m,m' (scratch)
-            : [dest] 'D,S' (&storage)
-#else
-            : '+A' (v), [scratch] '=m' (scratch)
-            : [dest] 'D' (&storage)
-#endif
-            : BOOST_ATOMIC_DETAIL_ASM_CLOBBER_CC_COMMA 'ecx', 'memory'
-        );
-        return v;
-#else // defined(__PIC__)
-        __asm__ __volatile__
-        (
-            'movl 0(%[dest]), %%eax\n\t'
-            'movl 4(%[dest]), %%edx\n\t'
-            '.align 16\n\t'
-            '1: lock; cmpxchg8b 0(%[dest])\n\t'
-            'jne 1b\n\t'
-#if !defined(BOOST_ATOMIC_DETAIL_NO_ASM_CONSTRAINT_ALTERNATIVES)
-            : '=A,A' (v)
-            : 'b,b' ((uint32_t)v), 'c,c' ((uint32_t)(v >> 32)), [dest] 'D,S' (&storage)
-#else
-            : '=A' (v)
-            : 'b' ((uint32_t)v), 'c' ((uint32_t)(v >> 32)), [dest] 'D' (&storage)
-#endif
-            : BOOST_ATOMIC_DETAIL_ASM_CLOBBER_CC_COMMA 'memory'
-        );
-        return v;
-#endif // defined(__PIC__)
-#endif
-    }
-    
-    template< >
-struct make_storage_type< 1u, false >
-{
-    typedef mars_boost::uint8_t type;
+    /**
+ * This class creates core-local caches for a given shared_ptr, to
+ * mitigate contention when acquiring/releasing it.
+ *
+ * It has the same thread-safety guarantees as shared_ptr: it is safe
+ * to concurrently call get(), but reset()s must be synchronized with
+ * reads and other resets().
+ *
+ * @author Giuseppe Ottaviano <ott@fb.com>
+ */
+template <class T, size_t kNumSlots = 64>
+class CoreCachedSharedPtr {
+ public:
+  explicit CoreCachedSharedPtr(const std::shared_ptr<T>& p = nullptr) {
+    reset(p);
+  }
     }
