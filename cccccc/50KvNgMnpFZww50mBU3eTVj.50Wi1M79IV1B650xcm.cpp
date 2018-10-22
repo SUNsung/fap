52884d1ec9da5b39da1b5b61bb1a1ef5333c0871
SUNsung <weatherius@gmail.com>
2018-10-22 21:50:01 +0300

    Rewrite [50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp] File in: 2018-10-22 21:50:00

--- cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
+++ cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
@@ -1,436 +1,145 @@
 
         
-        Status RunCppShapeInferenceImpl(
-    int graph_def_version, const string& serialized_node_def,
-    const std::vector<string>& input_serialized_shapes,
-    const std::vector<PyObject*>& input_constant_tensor_values,
-    const std::vector<string>& input_constant_tensor_as_shape_values,
-    std::vector<string>* output_tensor_shape_protos,
-    string* input_tensors_needed_out) {
-  tensorflow::NodeDef node;
-  if (!node.ParseFromString(serialized_node_def)) {
-    return errors::InvalidArgument(
-        'Error parsing node_def during cpp shape inference');
-  }
-  DCHECK_EQ(output_tensor_shape_protos->size(), 0);
-    }
-    
-    #ifndef TENSORFLOW_PYTHON_FRAMEWORK_CPP_SHAPE_INFERENCE_H_
-#define TENSORFLOW_PYTHON_FRAMEWORK_CPP_SHAPE_INFERENCE_H_
-    
-    REGISTER_OP('Invalid')
-    .Attr('invalid attr: int32')  // invalid since the name has a space.
-    .Doc(R'doc(
-An op to test that invalid ops do not successfully generate invalid python code.
-)doc');
-    
-    // Register the bfloat16 numpy type.
-void RegisterNumpyBfloat16();
-    
-    Status TF_DataType_to_PyArray_TYPE(TF_DataType tf_datatype,
-                                   int* out_pyarray_type) {
-  switch (tf_datatype) {
-    case TF_HALF:
-      *out_pyarray_type = NPY_FLOAT16;
+        bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv) {
+  const base::CommandLine::StringType dashdash(2, '-');
+  bool block_args = false;
+  for (int i = 0; i < argc; ++i) {
+    if (argv[i] == dashdash)
       break;
-    case TF_FLOAT:
-      *out_pyarray_type = NPY_FLOAT32;
-      break;
-    case TF_DOUBLE:
-      *out_pyarray_type = NPY_FLOAT64;
-      break;
-    case TF_INT32:
-      *out_pyarray_type = NPY_INT32;
-      break;
-    case TF_UINT32:
-      *out_pyarray_type = NPY_UINT32;
-      break;
-    case TF_UINT8:
-      *out_pyarray_type = NPY_UINT8;
-      break;
-    case TF_UINT16:
-      *out_pyarray_type = NPY_UINT16;
-      break;
-    case TF_INT8:
-      *out_pyarray_type = NPY_INT8;
-      break;
-    case TF_INT16:
-      *out_pyarray_type = NPY_INT16;
-      break;
-    case TF_INT64:
-      *out_pyarray_type = NPY_INT64;
-      break;
-    case TF_UINT64:
-      *out_pyarray_type = NPY_UINT64;
-      break;
-    case TF_BOOL:
-      *out_pyarray_type = NPY_BOOL;
-      break;
-    case TF_COMPLEX64:
-      *out_pyarray_type = NPY_COMPLEX64;
-      break;
-    case TF_COMPLEX128:
-      *out_pyarray_type = NPY_COMPLEX128;
-      break;
-    case TF_STRING:
-      *out_pyarray_type = NPY_OBJECT;
-      break;
-    case TF_RESOURCE:
-      *out_pyarray_type = NPY_VOID;
-      break;
-    // TODO(keveman): These should be changed to NPY_VOID, and the type used for
-    // the resulting numpy array should be the custom struct types that we
-    // expect for quantized types.
-    case TF_QINT8:
-      *out_pyarray_type = NPY_INT8;
-      break;
-    case TF_QUINT8:
-      *out_pyarray_type = NPY_UINT8;
-      break;
-    case TF_QINT16:
-      *out_pyarray_type = NPY_INT16;
-      break;
-    case TF_QUINT16:
-      *out_pyarray_type = NPY_UINT16;
-      break;
-    case TF_QINT32:
-      *out_pyarray_type = NPY_INT32;
-      break;
-    case TF_BFLOAT16:
-      *out_pyarray_type = Bfloat16NumpyType();
-      break;
-    default:
-      return errors::Internal('Tensorflow type ', tf_datatype,
-                              ' not convertible to numpy dtype.');
-  }
-  return Status::OK();
-}
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      const tensorflow::OpRegistrationData* op_reg_data;
-  auto status =
-      tensorflow::OpRegistry::Global()->LookUp(node_def.op(), &op_reg_data);
-  if (!status.ok()) {
-    LOG(WARNING) << 'Op ' << node_def.op() << ' not found: ' << status;
-    return '';
-  }
-  AddDefaultsToNodeDef(op_reg_data->op_def, &node_def);
-    
-    string UpperLowerString(UpperLower ul) {
-  switch (ul) {
-    case UpperLower::kUpper:
-      return 'Upper';
-    case UpperLower::kLower:
-      return 'Lower';
-    default:
-      LOG(FATAL) << 'Unknown upperlower ' << static_cast<int32>(ul);
-  }
-}
-    
-    private Q_SLOTS:
-    void on_selectFileButton_clicked();
-    
-    
-    {private Q_SLOTS:
-    void handleSelectionChanged(int idx);
-};
-    
-        // Initialize relevant QT models.
-    std::unique_ptr<const PlatformStyle> platformStyle(PlatformStyle::instantiate('other'));
-    auto node = interfaces::MakeNode();
-    OptionsModel optionsModel(*node);
-    AddWallet(wallet);
-    WalletModel walletModel(std::move(node->getWallets()[0]), *node, platformStyle.get(), &optionsModel);
-    RemoveWallet(wallet);
-    EditAddressDialog editAddressDialog(EditAddressDialog::NewSendingAddress);
-    editAddressDialog.setModel(walletModel.getAddressTableModel());
-    
-    private:
-    reverse_lock(reverse_lock const&);
-    reverse_lock& operator=(reverse_lock const&);
-    
-    
-    {    /** In case a->infinity == 1, replace r with (b->x, b->y, 1). */
-    secp256k1_fe_cmov(&r->x, &b->x, a->infinity);
-    secp256k1_fe_cmov(&r->y, &b->y, a->infinity);
-    secp256k1_fe_cmov(&r->z, &fe_1, a->infinity);
-    r->infinity = infinity;
-}
-    
-    
-    {    /* Try to multiply it by bad values */
-    CHECK(secp256k1_ecdh(ctx, output, &point, s_zero) == 0);
-    CHECK(secp256k1_ecdh(ctx, output, &point, s_overflow) == 0);
-    /* ...and a good one */
-    s_overflow[31] -= 1;
-    CHECK(secp256k1_ecdh(ctx, output, &point, s_overflow) == 1);
-}
-    
-    BOOST_AUTO_TEST_CASE(get_difficulty_for_mid_target)
-{
-    TestDifficulty(0x1df88f6f, 0.004023);
-}
-    
-    #include 'intsimdmatrix.h'
-#include 'genericvector.h'      // for GenericVector
-#include 'intsimdmatrixavx2.h'  // for IntSimdMatrixAVX2
-#include 'intsimdmatrixsse.h'   // for IntSimdMatrixSSE
-#include 'matrix.h'             // for GENERIC_2D_ARRAY
-#include 'simddetect.h'         // for SIMDDetect
-    
-    // Base class for a SIMD function to multiply a matrix by a vector, with sources
-// of 8-bit signed integer, and result in a double, after appropriate scaling.
-// Assumes a specific method of multiplication that can be applied to any size
-// and number of SIMD registers as follows:
-// int32_t results are computed with num_outputs_per_register_ in each of
-// max_output_registers_ result registers, repeatedly until it would make too
-// many results, then the number of registers is halved, and so-on down to a
-// single result register. The last calculation only outputs the required number
-// of results instead of writing beyond the bounds. Eg: matrix has 75 outputs,
-//  num_outputs_per_register_ = 4, and max_output_registers_ = 8,
-// Step 1: 8x4=32 results are computed,
-// Step 2: 8x4=32 again, total 64,
-// Step 3: 2x4=8 (since 8x4 is too many, so is 4x4), total 72,
-// Step 4: 1x3, total 75.
-// Each step above is computed using a PartialFunc, which runs over the input
-// vector once. The input is read one registerful of num_inputs_per_register_
-// at a time (presumably 4x num_outputs_per_register_ since they are int8_t)
-// so the inputs MUST BE PADDED to a multiple of num_inputs_per_register_.
-// Since it is slow (on Intel at least) to horizontally add in a register,
-// provision is made to process num_inputs_per_group_ inputs at a time, with
-// the group being replicated num_input_groups_ times and multiplied by a
-// num_inputs_per_group_ by num_input_groups_ rectangle of the weights matrix.
-// This is most convenient if num_inputs_per_group_ is 4, and the product
-// sign-extends and sums 8x8=16 bit results to 32 bits, adding 4 adjacent
-// results in the process, but it doesn't have to be implemented that way.
-// The weights are re-ordered by Init() to be used sequentially by the above
-// algorithm, followed by the biases, so they can be added at the end.
-// The base class computes the base C++ implementation.
-// NOTE that, although the subclasses execute on different SIMD hardware, no
-// virtual methods are needed, as the constructor sets up everything that
-// is required to allow the base class implementation to do all the work.
-class IntSimdMatrix {
- public:
-  // Constructor should set the data members to indicate the sizes.
-  // NOTE: Base constructor public only for test purposes.
-  IntSimdMatrix()
-      : num_outputs_per_register_(1),
-        max_output_registers_(1),
-        num_inputs_per_register_(1),
-        num_inputs_per_group_(1),
-        num_input_groups_(1) {}
-    }
-    
-    IntSimdMatrixAVX2::IntSimdMatrixAVX2() {
-#ifdef __AVX2__
-  num_outputs_per_register_ = kNumOutputsPerRegister;
-  max_output_registers_ = kMaxOutputRegisters;
-  num_inputs_per_register_ = kNumInputsPerRegister;
-  num_inputs_per_group_ = kNumInputsPerGroup;
-  num_input_groups_ = kNumInputGroups;
-  partial_funcs_ = {PartialMatrixDotVector64, PartialMatrixDotVector32,
-                    PartialMatrixDotVector16, PartialMatrixDotVector8};
-#endif  // __AVX2__
-}
-    
-    // Constructor.
-// Tests the architecture in a system-dependent way to detect AVX, SSE and
-// any other available SIMD equipment.
-// __GNUC__ is also defined by compilers that include GNU extensions such as
-// clang.
-SIMDDetect::SIMDDetect() {
-#if defined(X86_BUILD)
-#if defined(__GNUC__)
-  unsigned int eax, ebx, ecx, edx;
-  if (__get_cpuid(1, &eax, &ebx, &ecx, &edx) != 0) {
-    // Note that these tests all use hex because the older compilers don't have
-    // the newer flags.
-    sse_available_ = (ecx & 0x00080000) != 0;
-    avx_available_ = (ecx & 0x10000000) != 0;
-    if (avx_available_) {
-      // There is supposed to be a __get_cpuid_count function, but this is all
-      // there is in my cpuid.h. It is a macro for an asm statement and cannot
-      // be used inside an if.
-      __cpuid_count(7, 0, eax, ebx, ecx, edx);
-      avx2_available_ = (ebx & 0x00000020) != 0;
-      avx512F_available_ = (ebx & 0x00010000) != 0;
-      avx512BW_available_ = (ebx & 0x40000000) != 0;
+    if (block_args) {
+      return false;
+    } else if (IsUrlArg(argv[i])) {
+      block_args = true;
     }
   }
-#elif defined(_WIN32)
-  int cpuInfo[4];
-  __cpuid(cpuInfo, 0);
-  if (cpuInfo[0] >= 1) {
-    __cpuid(cpuInfo, 1);
-    sse_available_ = (cpuInfo[2] & 0x00080000) != 0;
-    avx_available_ = (cpuInfo[2] & 0x10000000) != 0;
-  }
-#else
-#error 'I don't know how to test for SIMD with this compiler'
-#endif
-#endif  // X86_BUILD
+  return true;
 }
+    
+    namespace api {
+    }
+    
+    
+    {}  // namespace atom
+    
+    #include 'atom/browser/api/atom_api_browser_window.h'
+    
+    // static
+void GlobalShortcut::BuildPrototype(v8::Isolate* isolate,
+                                    v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'GlobalShortcut'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod('register', &GlobalShortcut::Register)
+      .SetMethod('isRegistered', &GlobalShortcut::IsRegistered)
+      .SetMethod('unregister', &GlobalShortcut::Unregister)
+      .SetMethod('unregisterAll', &GlobalShortcut::UnregisterAll);
+}
+    
+    namespace atom {
+    }
+    
+    using atom::api::Menu;
+    
+    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_browser_net, Initialize)
 
     
-    #include <string>
-#include <vector>
+    namespace atom {
+    }
     
-      const auto& X = in[0];
-  const auto& W = in[1];
-  const auto& b = in[2];
-  auto axis = helper.GetSingleArgument<int32_t>('axis', 1);
-  const auto canonical_axis = canonical_axis_index_(axis, in[0].dims().size());
-  const int M = size_to_dim_(canonical_axis, GetDimsVector(in[0]));
-  const int K = size_from_dim_(canonical_axis, GetDimsVector(in[0]));
-  auto axis_w = helper.GetSingleArgument<int32_t>('axis_w', 1);
-  const int canonical_axis_w =
-      canonical_axis_index_(axis_w, in[1].dims().size());
-  const int N = size_to_dim_(canonical_axis_w, GetDimsVector(in[1]));
+    #endif
     
-    #include 'caffe2/core/context.h'
+     private:
+  scoped_refptr<AtomBrowserContext> browser_context_;
     
-    ```
+    #include <QDialog>
+    
+    class TrafficGraphWidget : public QWidget
+{
+    Q_OBJECT
+    }
+    
+    static void secp256k1_gej_rescale(secp256k1_gej *r, const secp256k1_fe *s) {
+    /* Operations: 4 mul, 1 sqr */
+    secp256k1_fe zz;
+    VERIFY_CHECK(!secp256k1_fe_is_zero(s));
+    secp256k1_fe_sqr(&zz, s);
+    secp256k1_fe_mul(&r->x, &r->x, &zz);                /* r->x *= s^2 */
+    secp256k1_fe_mul(&r->y, &r->y, &zz);
+    secp256k1_fe_mul(&r->y, &r->y, s);                  /* r->y *= s^3 */
+    secp256k1_fe_mul(&r->z, &r->z, s);                  /* r->z *= s   */
+}
+    
+    void test_ecdh_api(void) {
+    /* Setup context that just counts errors */
+    secp256k1_context *tctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
+    secp256k1_pubkey point;
+    unsigned char res[32];
+    unsigned char s_one[32] = { 0 };
+    int32_t ecount = 0;
+    s_one[31] = 1;
+    }
+    
+    #include <BulletDynamics/ConstraintSolver/btTypedConstraint.h>
+    
+    #include 'register_types.h'
     
     
-    {} // namespace caffe2
+    {	ADD_SIGNAL(MethodInfo('peer_connected', PropertyInfo(Variant::INT, 'id')));
+	ADD_SIGNAL(MethodInfo('peer_disconnected', PropertyInfo(Variant::INT, 'id')));
+	ADD_SIGNAL(MethodInfo('server_disconnected'));
+	ADD_SIGNAL(MethodInfo('connection_succeeded'));
+	ADD_SIGNAL(MethodInfo('connection_failed'));
+}
+    
+    In a multithreaded test, it is guaranteed that none of the threads will start
+until all have reached the loop start, and all will have finished before any
+thread exits the loop body. As such, any global setup or teardown you want to
+do can be wrapped in a check against the thread index:
+    
+    void ConsoleReporter::PrintRunData(const Run& result) {
+  typedef void(PrinterFn)(std::ostream&, LogColor, const char*, ...);
+  auto& Out = GetOutputStream();
+  PrinterFn* printer = (output_options_ & OO_Color) ?
+                         (PrinterFn*)ColorPrintf : IgnoreColorPrint;
+  auto name_color =
+      (result.report_big_o || result.report_rms) ? COLOR_BLUE : COLOR_GREEN;
+  printer(Out, name_color, '%-*s ', name_field_width_,
+          result.benchmark_name.c_str());
+    }
+    
+    void Increment(UserCounters *l, UserCounters const& r) {
+  // add counters present in both or just in *l
+  for (auto &c : *l) {
+    auto it = r.find(c.first);
+    if (it != r.end()) {
+      c.second.value = c.second + it->second;
+    }
+  }
+  // add counters present in r, but not in *l
+  for (auto const &tc : r) {
+    auto it = l->find(tc.first);
+    if (it == l->end()) {
+      (*l)[tc.first] = tc.second;
+    }
+  }
+}
+    
+    
+    {}  // end namespace benchmark
 
     
-    template <>
-void GluOp<float, CPUContext>::ComputeGlu(
-    const int M,
-    const int split_dim,
-    const int N,
-    const float* Xdata,
-    float* Ydata) {
-  const int xStride = 2 * split_dim * N;
-  const int yStride = split_dim * N;
-  for (int i = 0; i < M; ++i) {
-    const int idx = i * xStride;
-    const int idy = i * yStride;
-    for (int j = 0; j < split_dim; ++j) {
-      const int jN = j * N;
-      const int jdx1 = idx + jN;
-      const int jdx2 = idx + (j + split_dim) * N;
-      const int jdy = idy + jN;
-      for (int k = 0; k < N; ++k) {
-        const float x1 = Xdata[jdx1 + k];
-        const float x2 = Xdata[jdx2 + k];
-        Ydata[jdy + k] = x1 * sigmoid(x2);
-      }
-    }
-  }
-}
+      CPUInfo const& info = context.cpu_info;
+  out << indent << FormatKV('num_cpus', static_cast<int64_t>(info.num_cpus))
+      << ',\n';
+  out << indent
+      << FormatKV('mhz_per_cpu',
+                  RoundDouble(info.cycles_per_second / 1000000.0))
+      << ',\n';
+  out << indent << FormatKV('cpu_scaling_enabled', info.scaling_enabled)
+      << ',\n';
     
-        //
-    // Override
-    //
-    virtual Follow* clone() const override;
-    virtual Follow* reverse() const override;
-    /**
-     * @param dt in seconds.
-     * @js NA
-     */
-    virtual void step(float dt) override;
-    virtual bool isDone() const override;
-    virtual void stop() override;
-    
-        p = reverse->getControlPointAtIndex(reverse->count()-1);
-    reverse->removeControlPointAtIndex(reverse->count()-1);
-    
-        /** Initializes the action with a duration and an array of points.
-     *
-     * @param dt In seconds.
-     * @param points An PointArray.
-     */
-    bool initWithDuration(float dt, PointArray* points);
-    
-    void ActionInstant::step(float /*dt*/)
-{
-    float updateDt = 1;
-#if CC_ENABLE_SCRIPT_BINDING
-    if (_scriptType == kScriptTypeJavascript)
-    {
-        if (ScriptEngineManager::sendActionEventToJS(this, kActionUpdate, (void *)&updateDt))
-            return;
-    }
+    // Prefer C regex libraries when compiling w/o exceptions so that we can
+// correctly report errors.
+#if defined(BENCHMARK_HAS_NO_EXCEPTIONS) && defined(HAVE_STD_REGEX) && \
+    (defined(HAVE_GNU_POSIX_REGEX) || defined(HAVE_POSIX_REGEX))
+#undef HAVE_STD_REGEX
 #endif
-    update(updateDt);
-}
-    
-        //
-    // Overrides
-    //
-    virtual BezierBy* clone() const override;
-    virtual BezierBy* reverse() const override;
-    virtual void startWithTarget(Node *target) override;
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    BezierBy() {}
-    virtual ~BezierBy() {}
-    
-        if (action && action->initWithDuration(duration, gridSize))
-    {
-        action->autorelease();
-        return action;
-    }
-    
-    // implementation of Animation
-    
-    class CompactedDBImpl : public DBImpl {
- public:
-  CompactedDBImpl(const DBOptions& options, const std::string& dbname);
-  virtual ~CompactedDBImpl();
-    }
-    
-      {
-    // Scan the entire database as of the snapshot to ensure
-    // that nothing is left
-    ReadOptions read_options;
-    read_options.snapshot = snapshot;
-    std::unique_ptr<Iterator> iter(db_->NewIterator(read_options));
-    iter->SeekToFirst();
-    int count = 0;
-    while (iter->Valid()) {
-      count++;
-      iter->Next();
-    }
-    ASSERT_EQ(count, 6);
-    read_options.snapshot = nullptr;
-    std::unique_ptr<Iterator> iter1(db_->NewIterator(read_options));
-    iter1->SeekToFirst();
-    count = 0;
-    while (iter1->Valid()) {
-      count++;
-      iter1->Next();
-    }
-    // We have deleted 10 keys from 40 using the compaction filter
-    //  Keys 6-9 before the snapshot and 100-105 after the snapshot
-    ASSERT_EQ(count, 30);
-  }
-    
-      ASSERT_OK(dbfull()->SetOptions({
-      {'disable_auto_compactions', 'false'},
-  }));
-    
-    class PosixRandomRWFile : public RandomRWFile {
- public:
-  explicit PosixRandomRWFile(const std::string& fname, int fd,
-                             const EnvOptions& options);
-  virtual ~PosixRandomRWFile();
-    }
-    
-    
-    {  std::atomic<int64_t> fake_sleep_micros_;
-};
-    
-    std::string kDBPath = '/tmp/rocksdb_column_families_example';
