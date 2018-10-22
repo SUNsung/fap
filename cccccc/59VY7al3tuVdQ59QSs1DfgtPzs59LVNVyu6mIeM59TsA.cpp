339b5a027d78bd46b3fa0599e55bc63e75bc9d81
SUNsung <weatherius@gmail.com>
2018-10-22 10:37:26 +0300

    Rewrite [59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp] File in: 2018-10-22 10:37:25

--- cccccc/59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp
+++ cccccc/59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp
@@ -1,318 +1,162 @@
 
         
-        #if !GTEST_OS_SYMBIAN
-# include <utility>
-#endif
+        bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv);
     
-    // A helper class for implementing EXPECT_FATAL_FAILURE() and
-// EXPECT_NONFATAL_FAILURE().  Its destructor verifies that the given
-// TestPartResultArray contains exactly one failure that has the given
-// type and contains the given substring.  If that's not the case, a
-// non-fatal failure will be generated.
-class GTEST_API_ SingleFailureChecker {
- public:
-  // The constructor remembers the arguments.
-  SingleFailureChecker(const TestPartResultArray* results,
-                       TestPartResult::Type type,
-                       const string& substr);
-  ~SingleFailureChecker();
- private:
-  const TestPartResultArray* const results_;
-  const TestPartResult::Type type_;
-  const string substr_;
+      // base::SingleThreadTaskRunner:
+  bool PostDelayedTask(const base::Location& from_here,
+                       base::OnceClosure task,
+                       base::TimeDelta delay) override;
+  bool RunsTasksInCurrentSequence() const override;
+  bool PostNonNestableDelayedTask(const base::Location& from_here,
+                                  base::OnceClosure task,
+                                  base::TimeDelta delay) override;
+    
+    void Initialize(v8::Local<v8::Object> exports,
+                v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context,
+                void* priv) {
+  v8::Isolate* isolate = context->GetIsolate();
+  mate::Dictionary dict(isolate, exports);
+  dict.Set('autoUpdater', AutoUpdater::Create(isolate));
+  dict.Set('AutoUpdater', AutoUpdater::GetConstructor(isolate)->GetFunction());
+}
+    
+    #endif  // ATOM_BROWSER_API_ATOM_API_BROWSER_VIEW_H_
+
+    
+    namespace atom {
     }
     
-    template <>
-class tuple<> {
- public:
-  tuple() {}
-  tuple(const tuple& /* t */)  {}
-  tuple& operator=(const tuple& /* t */) { return *this; }
-};
+    #include 'atom/common/native_mate_converters/callback.h'
+#include 'atom/common/native_mate_converters/file_path_converter.h'
+#include 'base/bind.h'
+#include 'base/files/file_util.h'
+#include 'content/public/browser/tracing_controller.h'
+#include 'native_mate/dictionary.h'
     
-      // Now, we have that n is odd and n >= 3.
-    
-    
-    {  return clone;
+    void GlobalShortcut::OnKeyPressed(const ui::Accelerator& accelerator) {
+  if (accelerator_callback_map_.find(accelerator) ==
+      accelerator_callback_map_.end()) {
+    // This should never occur, because if it does, GlobalGlobalShortcutListener
+    // notifes us with wrong accelerator.
+    NOTREACHED();
+    return;
+  }
+  accelerator_callback_map_[accelerator].Run();
 }
+    
+    
+    {}  // namespace atom
+    
+    void Menu::InsertItemAt(int index,
+                        int command_id,
+                        const base::string16& label) {
+  model_->InsertItemAt(index, command_id, label);
+}
+    
+      // The window used for processing events.
+  HWND window_;
+#endif
+    
+    void Screen::OnDisplayMetricsChanged(const display::Display& display,
+                                     uint32_t changed_metrics) {
+  Emit('display-metrics-changed', display, MetricsToArray(changed_metrics));
+}
+    
+    namespace tesseract {
+    }
     
     // Computes and returns the dot product of the n-vectors u and v.
 // Uses Intel SSE intrinsics to access the SIMD instruction set.
-double DotProductSSE(const double* u, const double* v, int n);
-// Computes and returns the dot product of the n-vectors u and v.
-// Uses Intel SSE intrinsics to access the SIMD instruction set.
-int32_t IntDotProductSSE(const int8_t* u, const int8_t* v, int n);
-    
-    IntSimdMatrixAVX2::IntSimdMatrixAVX2() {
-#ifdef __AVX2__
-  num_outputs_per_register_ = kNumOutputsPerRegister;
-  max_output_registers_ = kMaxOutputRegisters;
-  num_inputs_per_register_ = kNumInputsPerRegister;
-  num_inputs_per_group_ = kNumInputsPerGroup;
-  num_input_groups_ = kNumInputGroups;
-  partial_funcs_ = {PartialMatrixDotVector64, PartialMatrixDotVector32,
-                    PartialMatrixDotVector16, PartialMatrixDotVector8};
-#endif  // __AVX2__
+int32_t IntDotProductSSE(const int8_t* u, const int8_t* v, int n) {
+  int max_offset = n - 8;
+  int offset = 0;
+  // Accumulate a set of 4 32-bit sums in sum, by loading 8 pairs of 8-bit
+  // values, extending to 16 bit, multiplying to make 32 bit results.
+  __m128i sum = _mm_setzero_si128();
+  if (offset <= max_offset) {
+    offset = 8;
+    __m128i packed1 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(u));
+    __m128i packed2 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(v));
+    sum = _mm_cvtepi8_epi16(packed1);
+    packed2 = _mm_cvtepi8_epi16(packed2);
+    // The magic _mm_add_epi16 is perfect here. It multiplies 8 pairs of 16 bit
+    // ints to make 32 bit results, which are then horizontally added in pairs
+    // to make 4 32 bit results that still fit in a 128 bit register.
+    sum = _mm_madd_epi16(sum, packed2);
+    while (offset <= max_offset) {
+      packed1 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(u + offset));
+      packed2 = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(v + offset));
+      offset += 8;
+      packed1 = _mm_cvtepi8_epi16(packed1);
+      packed2 = _mm_cvtepi8_epi16(packed2);
+      packed1 = _mm_madd_epi16(packed1, packed2);
+      sum = _mm_add_epi32(sum, packed1);
+    }
+  }
+  // Sum the 4 packed 32 bit sums and extract the low result.
+  sum = _mm_hadd_epi32(sum, sum);
+  sum = _mm_hadd_epi32(sum, sum);
+  int32_t result = _mm_cvtsi128_si32(sum);
+  while (offset < n) {
+    result += u[offset] * v[offset];
+    ++offset;
+  }
+  return result;
 }
-    
-    // Constructor.
-// Tests the architecture in a system-dependent way to detect AVX, SSE and
-// any other available SIMD equipment.
-// __GNUC__ is also defined by compilers that include GNU extensions such as
-// clang.
-SIMDDetect::SIMDDetect() {
-#if defined(X86_BUILD)
-#if defined(__GNUC__)
-  unsigned int eax, ebx, ecx, edx;
-  if (__get_cpuid(1, &eax, &ebx, &ecx, &edx) != 0) {
-    // Note that these tests all use hex because the older compilers don't have
-    // the newer flags.
-    sse_available_ = (ecx & 0x00080000) != 0;
-    avx_available_ = (ecx & 0x10000000) != 0;
-    if (avx_available_) {
-      // There is supposed to be a __get_cpuid_count function, but this is all
-      // there is in my cpuid.h. It is a macro for an asm statement and cannot
-      // be used inside an if.
-      __cpuid_count(7, 0, eax, ebx, ecx, edx);
-      avx2_available_ = (ebx & 0x00000020) != 0;
-      avx512F_available_ = (ebx & 0x00010000) != 0;
-      avx512BW_available_ = (ebx & 0x40000000) != 0;
-    }
-  }
-#elif defined(_WIN32)
-  int cpuInfo[4];
-  __cpuid(cpuInfo, 0);
-  if (cpuInfo[0] >= 1) {
-    __cpuid(cpuInfo, 1);
-    sse_available_ = (cpuInfo[2] & 0x00080000) != 0;
-    avx_available_ = (cpuInfo[2] & 0x10000000) != 0;
-  }
-#else
-#error 'I don't know how to test for SIMD with this compiler'
-#endif
-#endif  // X86_BUILD
-}
-
-    
-    #ifndef TESSERACT_CCMAIN_OSDETECT_H_
-#define TESSERACT_CCMAIN_OSDETECT_H_
-    
-      // ============= Accessing data ==============.
-  // Coordinate system:
-  // Integer coordinates are at the cracks between the pixels.
-  // The top-left corner of the top-left pixel in the image is at (0,0).
-  // The bottom-right corner of the bottom-right pixel in the image is at
-  // (width, height).
-  // Every bounding box goes from the top-left of the top-left contained
-  // pixel to the bottom-right of the bottom-right contained pixel, so
-  // the bounding box of the single top-left pixel in the image is:
-  // (0,0)->(1,1).
-  // If an image rectangle has been set in the API, then returned coordinates
-  // relate to the original (full) image, rather than the rectangle.
-    
-      WERD_RES *word2 = nullptr;
-  BlamerBundle *orig_bb = nullptr;
-  split_word(word, split_index, &word2, &orig_bb);
-    
-      /// Threshold the rectangle, taking everything except the src_pix
-  /// from the class, using thresholds/hi_values to the output pix.
-  /// NOTE that num_channels is the size of the thresholds and hi_values
-  // arrays and also the bytes per pixel in src_pix.
-  void ThresholdRectToPix(Pix* src_pix, int num_channels,
-                          const int* thresholds, const int* hi_values,
-                          Pix** pix) const;
-    
-    bool AuthPropertyIterator::operator!=(const AuthPropertyIterator& rhs) const {
-  return !operator==(rhs);
-}
-    
-    #include <grpcpp/support/channel_arguments.h>
-    
-      const protobuf::Descriptor* desc =
-      descriptor_pool_->FindMessageTypeByName(type);
-  if (desc == nullptr) {
-    return Status(StatusCode::NOT_FOUND, 'Type not found.');
-  }
-    
-    // Read through the first n keys repeatedly and check that they get
-// compacted (verified by checking the size of the key space).
-void AutoCompactTest::DoReads(int n) {
-  std::string value(kValueSize, 'x');
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
-    }
-    
-      // Check for input iterator errors
-  if (!iter->status().ok()) {
-    s = iter->status();
-  }
-    
-      Status RecoverLogFile(uint64_t log_number, bool last_log, bool* save_manifest,
-                        VersionEdit* edit, SequenceNumber* max_sequence)
-      EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-    
-      // When start user key is prefix of limit user key
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foobar', 200, kTypeValue)));
-    
-    // Print contents of a log file. (*func)() is called on every record.
-Status PrintLogContents(Env* env, const std::string& fname,
-                        void (*func)(uint64_t, Slice, WritableFile*),
-                        WritableFile* dst) {
-  SequentialFile* file;
-  Status s = env->NewSequentialFile(fname, &file);
-  if (!s.ok()) {
-    return s;
-  }
-  CorruptionReporter reporter;
-  reporter.dst_ = dst;
-  log::Reader reader(file, &reporter, true, 0);
-  Slice record;
-  std::string scratch;
-  while (reader.ReadRecord(&record, &scratch)) {
-    (*func)(reader.LastRecordOffset(), record, dst);
-  }
-  delete file;
-  return Status::OK();
-}
-    
-    class StdoutPrinter : public WritableFile {
- public:
-  virtual Status Append(const Slice& data) {
-    fwrite(data.data(), 1, data.size(), stdout);
-    return Status::OK();
-  }
-  virtual Status Close() { return Status::OK(); }
-  virtual Status Flush() { return Status::OK(); }
-  virtual Status Sync() { return Status::OK(); }
-};
-    
-    namespace leveldb {
-namespace log {
-    }
-    }
-    
-      Status FindFiles() {
-    std::vector<std::string> filenames;
-    Status status = env_->GetChildren(dbname_, &filenames);
-    if (!status.ok()) {
-      return status;
-    }
-    if (filenames.empty()) {
-      return Status::IOError(dbname_, 'repair found no files');
-    }
-    }
-    
-    // Simple test that does single-threaded testing of the ConcurrentTest
-// scaffolding.
-TEST(SkipTest, ConcurrentWithoutThreads) {
-  ConcurrentTest test;
-  Random rnd(test::RandomSeed());
-  for (int i = 0; i < 10000; i++) {
-    test.ReadStep(&rnd);
-    test.WriteStep(&rnd);
-  }
-}
-    
-            // Get the function pointer (required for any extensions)
-        auto vkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(g_Instance, 'vkCreateDebugReportCallbackEXT');
-        IM_ASSERT(vkCreateDebugReportCallbackEXT != NULL);
-    
-        // Rendering
-    ImGui::Render();
-    ImGuiIO& io = ImGui::GetIO();
-    glViewport(0, 0, (GLsizei)io.DisplaySize.x, (GLsizei)io.DisplaySize.y);
-    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    glClear(GL_COLOR_BUFFER_BIT);
-    //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL3+ code.
-    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-    
-        // Setup Dear ImGui binding
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    ImGui_Marmalade_Init(true);
-    
-    // Render function.
-// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
-void ImGui_Marmalade_RenderDrawData(ImDrawData* draw_data)
-{
-    // Handle cases of screen coordinates != from framebuffer coordinates (e.g. retina displays)
-    ImGuiIO& io = ImGui::GetIO();
-    draw_data->ScaleClipRects(io.DisplayFramebufferScale);
-    }
-    
-    // OpenGL2 Render function.
-// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
-// Note that this implementation is little overcomplicated because we are saving/setting up/restoring every OpenGL state explicitly, in order to be able to run within any OpenGL engine that doesn't do so. 
-void ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data)
-{
-    // Avoid rendering when minimized, scale coordinates for retina displays (screen coordinates != framebuffer coordinates)
-    ImGuiIO& io = ImGui::GetIO();
-    int fb_width = (int)(draw_data->DisplaySize.x * io.DisplayFramebufferScale.x);
-    int fb_height = (int)(draw_data->DisplaySize.y * io.DisplayFramebufferScale.y);
-    if (fb_width == 0 || fb_height == 0)
-        return;
-    draw_data->ScaleClipRects(io.DisplayFramebufferScale);
-    }
-    
-    // About OpenGL function loaders: 
-// About OpenGL function loaders: modern OpenGL doesn't have a standard header file and requires individual function pointers to be loaded manually. 
-// Helper libraries are often used for this purpose! Here we are supporting a few common ones: gl3w, glew, glad. 
-// You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
-    
-    
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-    
-      // init config and state
-  // After a CAN handle is created with canOpen() the CAN-ID filter is
-  // cleared
-  // (no CAN messages
-  // will pass the filter). To receive a CAN message with a certain CAN-ID
-  // or an
-  // NTCAN-Event with
-  // a certain Event-ID it is required to enable this ID in the handle
-  // filter as
-  // otherwise a
-  // received  message or event is discarded by the driver for this handle.
-  // 1. set receive message_id filter, ie white list
-  int32_t id_count = 0x800;
-  ret = canIdRegionAdd(dev_handler_, 0, &id_count);
-  if (ret != NTCAN_SUCCESS) {
-    AERROR << 'add receive msg id filter error code: ' << ret << ', '
-           << GetErrorString(ret);
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
-  }
     
       /**
-   * @brief Stop the fake CAN client.
+   * Returns orientation for the block the iterator points to.
+   *   orientation, writing_direction, textline_order: see publictypes.h
+   *   deskew_angle: after rotating the block so the text orientation is
+   *                 upright, how many radians does one have to rotate the
+   *                 block anti-clockwise for it to be level?
+   *                   -Pi/4 <= deskew_angle <= Pi/4
    */
-  void Stop() override;
+  void Orientation(tesseract::Orientation *orientation,
+                   tesseract::WritingDirection *writing_direction,
+                   tesseract::TextlineOrder *textline_order,
+                   float *deskew_angle) const;
     
-    void HermesCanClient::Stop() {
-  if (_is_init) {
-    _is_init = false;
-    int32_t ret = bcan_close(_dev_handler);
-    if (ret != ErrorCode::OK) {
-      AERROR << 'close error code: ' << ret;
-    }
+    #define arraysize(array) (sizeof(::benchmark::internal::ArraySizeHelper(array)))
+    
+    // Internal function to calculate the different scalability forms
+BigOFunc* FittingCurve(BigO complexity) {
+  switch (complexity) {
+    case oN:
+      return [](int n) -> double { return n; };
+    case oNSquared:
+      return [](int n) -> double { return std::pow(n, 2); };
+    case oNCubed:
+      return [](int n) -> double { return std::pow(n, 3); };
+    case oLogN:
+      return [](int n) { return log2(n); };
+    case oNLogN:
+      return [](int n) { return n * log2(n); };
+    case o1:
+    default:
+      return [](int) { return 1.0; };
   }
 }
     
-    #include 'gtest/gtest.h'
+    // Return a vector containing the bigO and RMS information for the specified
+// list of reports. If 'reports.size() < 2' an empty vector is returned.
+std::vector<BenchmarkReporter::Run> ComputeBigO(
+    const std::vector<BenchmarkReporter::Run>& reports);
     
-    /**
- * @namespace apollo::drivers::canbus
- * @brief apollo::drivers::canbus
- */
-namespace apollo {
-namespace drivers {
-namespace canbus {
+    bool ConsoleReporter::ReportContext(const Context& context) {
+  name_field_width_ = context.name_field_width;
+  printed_header_ = false;
+  prev_counters_.clear();
     }
-    }
-    }
+    
+    
+    {
+    {} // end namespace internal
+} // end namespace benchmark
+
+    
+    inline int& LogLevel() {
+  static int log_level = 0;
+  return log_level;
+}
