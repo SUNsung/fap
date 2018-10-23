f1985b75e6030dfcfccc8b4c7de5c000a4535b96
SUNsung <weatherius@gmail.com>
2018-10-23 20:01:05 +0300

    Rewrite [46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py] File in: 2018-10-23 20:01:04

--- pyton/46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py
+++ pyton/46ztBjb7dXnOE46Zrz0c6Uk3M46M4uroKtzJqE46u2E.py
@@ -1,132 +1,77 @@
 
         
-            def test_distant_exception(self):
-        def f():
-            1/0
-        def g():
-            f()
-        def h():
-            g()
-        def i():
-            h()
-        def j(p):
-            i()
-        f_ident = ident(f)
-        g_ident = ident(g)
-        h_ident = ident(h)
-        i_ident = ident(i)
-        j_ident = ident(j)
-        self.check_events(j, [(1, 'call', j_ident),
-                              (2, 'call', i_ident),
-                              (3, 'call', h_ident),
-                              (4, 'call', g_ident),
-                              (5, 'call', f_ident),
-                              (5, 'return', f_ident),
-                              (4, 'return', g_ident),
-                              (3, 'return', h_ident),
-                              (2, 'return', i_ident),
-                              (1, 'return', j_ident),
-                              ])
+        
+@pytest.fixture(autouse=True)
+def shell(mocker):
+    shell = mocker.patch('thefuck.entrypoints.not_configured.shell',
+                         new_callable=MagicMock)
+    shell.get_history.return_value = []
+    shell.how_to_configure.return_value = ShellConfiguration(
+        content='eval $(thefuck --alias)',
+        path='/tmp/.bashrc',
+        reload='bash',
+        can_configure_automatically=True)
+    return shell
+    
+        proc.sendline(u'fuck')
+    assert proc.expect([TIMEOUT, u'git show'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git push'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git help'])
+    proc.send('\033[A')
+    assert proc.expect([TIMEOUT, u'git push'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git help'])
+    proc.send('\n')
     
     
-# AIX libc prints an empty string as '' rather than the string '(null)'
-NULL_STR = '' if platform.system() == 'AIX' else '(null)'
+@pytest.mark.functional
+def test_select_command_with_arrows(proc, TIMEOUT):
+    select_command_with_arrows(proc, TIMEOUT)
+    history_changed(proc, TIMEOUT, u'git help')
     
-            out = self.get_output('-X', 'utf8', '-c', code,
-                              PYTHONIOENCODING='')
-        self.assertEqual(out.splitlines(),
-                         ['stdin: utf-8/surrogateescape',
-                          'stdout: utf-8/surrogateescape',
-                          'stderr: utf-8/backslashreplace'])
-    
-    The special sequences consist of '\\' and a character from the list
-below.  If the ordinary character is not on the list, then the
-resulting RE will match the second character.
-    \number  Matches the contents of the group of the same number.
-    \A       Matches only at the start of the string.
-    \Z       Matches only at the end of the string.
-    \b       Matches the empty string, but only at the start or end of a word.
-    \B       Matches the empty string, but not at the start or end of a word.
-    \d       Matches any decimal digit; equivalent to the set [0-9] in
-             bytes patterns or string patterns with the ASCII flag.
-             In string patterns without the ASCII flag, it will match the whole
-             range of Unicode digits.
-    \D       Matches any non-digit character; equivalent to [^\d].
-    \s       Matches any whitespace character; equivalent to [ \t\n\r\f\v] in
-             bytes patterns or string patterns with the ASCII flag.
-             In string patterns without the ASCII flag, it will match the whole
-             range of Unicode whitespace characters.
-    \S       Matches any non-whitespace character; equivalent to [^\s].
-    \w       Matches any alphanumeric character; equivalent to [a-zA-Z0-9_]
-             in bytes patterns or string patterns with the ASCII flag.
-             In string patterns without the ASCII flag, it will match the
-             range of Unicode alphanumeric characters (letters plus digits
-             plus underscore).
-             With LOCALE, it will match the set [0-9_] plus characters defined
-             as letters for the current locale.
-    \W       Matches the complement of \w.
-    \\       Matches a literal backslash.
-    
-        return code
-    
-        colon = _get_colon(s)
-    if colon not in s: return s[:0], s
-    col = 0
-    for i in range(len(s)):
-        if s[i:i+1] == colon: col = i + 1
-    path, file = s[:col-1], s[col:]
-    if path and not colon in path:
-        path = path + colon
-    return path, file
-    
-        def test_is_reserved(self):
-        P = self.cls
-        self.assertIs(False, P('').is_reserved())
-        self.assertIs(False, P('/').is_reserved())
-        self.assertIs(False, P('/foo/bar').is_reserved())
-        self.assertIs(False, P('/dev/con/PRN/NUL').is_reserved())
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+    python_2 = ('thefuck/python2-zsh',
+            u'''FROM python:2
+                RUN apt-get update
+                RUN apt-get install -yy zsh''',
+            u'sh')
     
     
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
+def test_get_new_command():
+    new_command = get_new_command(Command('apt list --upgradable', match_output))
+    assert new_command == 'apt upgrade'
     
-      python_interpreter = _PathToPythonUsedDuringBuild()
-  if python_interpreter and utils.GetExecutable( python_interpreter ):
-    return python_interpreter
+        # Make the eyebrows into a nightmare
+    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
+    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
+    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
+    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
+    
+        # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ('unknown', loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+    
+        # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
+    
+        # Load the uploaded image file
+    img = face_recognition.load_image_file(file_stream)
+    # Get face encodings for any faces in the uploaded image
+    unknown_face_encodings = face_recognition.face_encodings(img)
     
     
-  def Extra_Info_No_Doc_String_test( self ):
-    self._Check( 0, {
-      'insertion_text':  'INSERTION TEXT',
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'extra_data': {
-      },
-    }, {
-      'word'     : 'INSERTION TEXT',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '0',
-    } )
+def load_image_file(file, mode='RGB'):
+    '''
+    Loads an image file (.jpg, .png, etc) into a numpy array
     
-    from hamcrest import assert_that, equal_to
-from mock import patch, call
+            face_names = []
+        for face_encoding in face_encodings:
+            # See if the face is a match for the known face(s)
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = 'Unknown'
     
-    from ycm.client.omni_completion_request import OmniCompletionRequest
+    with open('HISTORY.rst') as history_file:
+    history = history_file.read()
