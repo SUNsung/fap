409d1d21dd0bbd4ac22f09889a4346cb759bbe18
SUNsung <weatherius@gmail.com>
2018-10-23 09:02:31 +0300

    Rewrite [56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp] File in: 2018-10-23 09:02:30

--- cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
+++ cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
@@ -1,139 +1,262 @@
 
         
-        bool swift::isPlatformActive(PlatformKind Platform, LangOptions &LangOpts) {
-  if (Platform == PlatformKind::none)
-    return true;
-  
-  if (Platform == PlatformKind::OSXApplicationExtension ||
-      Platform == PlatformKind::iOSApplicationExtension)
-    if (!LangOpts.EnableAppExtensionRestrictions)
-      return false;
-  
-  // FIXME: This is an awful way to get the current OS.
-  switch (Platform) {
-    case PlatformKind::OSX:
-    case PlatformKind::OSXApplicationExtension:
-      return LangOpts.Target.isMacOSX();
-    case PlatformKind::iOS:
-    case PlatformKind::iOSApplicationExtension:
-      return LangOpts.Target.isiOS() && !LangOpts.Target.isTvOS();
-    case PlatformKind::tvOS:
-    case PlatformKind::tvOSApplicationExtension:
-      return LangOpts.Target.isTvOS();
-    case PlatformKind::watchOS:
-    case PlatformKind::watchOSApplicationExtension:
-      return LangOpts.Target.isWatchOS();
-    case PlatformKind::none:
-      llvm_unreachable('handled above');
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    string TransposeString(Transpose t) {
+  switch (t) {
+    case Transpose::kNoTranspose:
+      return 'NoTranspose';
+    case Transpose::kTranspose:
+      return 'Transpose';
+    case Transpose::kConjugateTranspose:
+      return 'ConjugateTranspose';
+    default:
+      LOG(FATAL) << 'Unknown transpose ' << static_cast<int32>(t);
   }
-  llvm_unreachable('bad PlatformKind');
+}
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+      // mutex that guards the cuBLAS handle for this device.
+  mutex mu_;
+    
+    class PAGE_RES_IT;
+class ROW;
+class WERD_RES;
+    
+    struct OSResults {
+  OSResults() : unicharset(nullptr) {
+    for (int i = 0; i < 4; ++i) {
+      for (int j = 0; j < kMaxNumberOfScripts; ++j)
+        scripts_na[i][j] = 0;
+      orientations[i] = 0;
+    }
+  }
+  void update_best_orientation();
+  // Set the estimate of the orientation to the given id.
+  void set_best_orientation(int orientation_id);
+  // Update/Compute the best estimate of the script assuming the given
+  // orientation id.
+  void update_best_script(int orientation_id);
+  // Return the index of the script with the highest score for this orientation.
+  TESS_API int get_best_script(int orientation_id) const;
+  // Accumulate scores with given OSResults instance and update the best script.
+  void accumulate(const OSResults& osr);
+    }
+    
+    namespace tesseract {
+    }
+    
+       void EmitAForm(const uint8_t op,
+                  const RegNumber rt,
+                  const RegNumber ra,
+                  const RegNumber rb,
+                  const RegNumber bc,
+                  const uint16_t xop,
+                  const bool rc = 0) {
+    }
+    
+    APCHandle::Pair APCCollection::Make(const ObjectData* obj,
+                                    APCHandleLevel level,
+                                    bool unserializeObj) {
+  auto bail = [&] {
+    return APCString::MakeSerializedObject(
+      apc_serialize(Variant(const_cast<ObjectData*>(obj)))
+    );
+  };
+    }
+    
+    #include 'hphp/runtime/base/apc-handle.h'
+    
+    inline APCLocalArray* APCLocalArray::Make(const APCArray* aa) {
+  auto size = sizeof(APCLocalArray) + aa->size() * sizeof(TypedValue);
+  auto local = new (tl_heap->objMalloc(size)) APCLocalArray(aa);
+  assertx(local->heapSize() == size);
+  return local;
+}
+    
+    std::string
+Config::IniName(const Hdf& config, bool /*prepend_hhvm*/ /* = true */) {
+  return Config::IniName(config.getFullPath());
+}
+    
+      private:
+    
+    public:
+  static void Add(InfoVec& out, const char* name, const std::string& value);
+  static void AddServerStats(InfoVec& out, const char* name,
+                             const char* statsName = nullptr);
+    
+    static int get_tempfile_if_not_exists(int ini_fd, char ini_path[]) {
+  if (ini_fd == -1) {
+#ifdef _MSC_VER
+    // MSVC doesn't require the characters to be the last
+    // 6 in the string.
+    ini_fd = open(mktemp(ini_path), O_RDWR | O_EXCL);
+#else
+    ini_fd = mkstemps(ini_path, 4); // keep the .ini suffix
+#endif
+    if (ini_fd == -1) {
+      fprintf(stderr, 'Error: unable to open temporary file');
+      exit(EXIT_FAILURE);
+    }
+  }
+  return ini_fd;
+}
+    
+    #include <string>
+#include <atomic>
+#include <utility>
+    
+    class ActionInterval;
+class RepeatForever;
+    
+    bool RotateTo::initWithDuration(float duration, const Vec3& dstAngle3D)
+{
+    if (ActionInterval::initWithDuration(duration))
+    {
+        _dstAngle = dstAngle3D;
+        _is3D = true;
+        
+        return true;
+    }
+    
+    return false;
 }
     
     
-    {    if (Finished) {
-      std::string Output = 'Output placeholder\n';
-      std::string Errors =
-          P.second->SeparateErrors ? 'Error placeholder\n' : '';
-      if (Finished(P.first, 0, Output, Errors, TaskProcessInformation(Pid),
-                   P.second->Context) == TaskFinishedResponse::StopExecution)
-        SubtaskFailed = true;
-    }
-  }
-    
-    #endif
-    
-             vline_f32 = vaddq_f32(vline_f32, vhalf);
-         vline_s32 = vcvtq_s32_f32(vline_f32);
-         vline_s16 = vqmovn_s32(vline_s32);
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(s32, u16, 8,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
+    {    if (element->actions->num == 0)
     {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.32 {d4-d5}, [%[src1]]                             \n\t'
-            'vld1.32 {d6-d7}, [%[src2]]                             \n\t'
-            'vcvt.f32.s32 q4, q2                                    \n\t'
-            'vcvt.f32.s32 q5, q3                                    \n\t'
-            'vmul.f32 q6, q4, q0                                    \n\t'
-            'vmul.f32 q7, q5, q0                                    \n\t'
-            'vadd.f32 q8, q6, q1                                    \n\t'
-            'vadd.f32 q9, q7, q1                                    \n\t'
-            'vcvt.s32.f32 q10, q8                                   \n\t'
-            'vcvt.s32.f32 q11, q9                                   \n\t'
-            'vqmovun.s32 d24, q10                                   \n\t'
-            'vqmovun.s32 d25, q11                                   \n\t'
-            'vst1.16 {d24-d25}, [%[dst]]                            \n\t'
-            : /*no output*/
-            : [src1] 'r' (_src + i + 0),
-              [src2] 'r' (_src + i + 4),
-              [dst] 'r' (_dst + i),
-              'w'  (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25'
-        );
-    }
-})
-#else
-CVTS_FUNC(s32, u16, 8,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        int32x4_t vline1_s32 = vld1q_s32(_src + i + 0);
-        int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
-        float32x4_t vline1_f32 = vcvtq_f32_s32(vline1_s32);
-        float32x4_t vline2_f32 = vcvtq_f32_s32(vline2_s32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        uint16x4_t vRes1 = vqmovun_s32(vline1_s32);
-        uint16x4_t vRes2 = vqmovun_s32(vline2_s32);
-        vst1q_u16(_dst + i, vcombine_u16(vRes1, vRes2));
-    }
-})
-#endif
-    
-    
-    {            vs1 = vqadd_s32(vs1, vnz1);
-            vs2 = vqadd_s32(vs2, vnz2);
-            vs3 = vqadd_s32(vs3, vnz3);
-            vs4 = vqadd_s32(vs4, vnz4);
+        if (_currentTarget == element)
+        {
+            _currentTargetSalvaged = true;
         }
+        else
+        {
+            deleteHashElement(element);
+        }
+    }
+}
+    
+    
+    {
+    {
+    {            p.z = (r * ( 1 - cosBeta ) * cosTheta);// '100' didn't work for
+            p.x = p.z * sinf(rotateByYAxis) + p.x * cosf(rotateByYAxis);
+            p.z = p.z * cosf(rotateByYAxis) - p.x * sinf(rotateByYAxis);
+            p.z/=7;
+            //    Stop z coord from dropping beneath underlying page in a transition
+            // issue #751
+            if( p.z < 0.5f )
+            {
+                p.z = 0.5f;
+            }
+            
+            // Set new coords
+            p.x += getGridRect().origin.x;
+            setVertex(Vec2(i, j), p);
+            
+        }
+    }
+}
+    
+    bool ProgressFromTo::initWithDuration(float duration, float fromPercentage, float toPercentage)
+{
+    if (ActionInterval::initWithDuration(duration))
+    {
+        _to = toPercentage;
+        _from = fromPercentage;
+    }
+    }
+    
+    // implementation AtlasNode
     
       /**
-   * @brief Create a pointer to a specified brand of CAN client. The brand is
-   *        set in the parameter.
-   * @param parameter The parameter to create the CAN client.
-   * @return A pointer to the created CAN client.
+   * @brief Send messages
+   * @param frames The messages to send.
+   * @param frame_num The amount of messages to send.
+   * @return The status of the sending action which is defined by
+   *         apollo::common::ErrorCode.
    */
-  std::unique_ptr<CanClient> CreateCANClient(const CANCardParameter &parameter);
+  apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                 int32_t *const frame_num) override;
     
-    #include 'modules/drivers/canbus/can_client/esd/esd_can_client.h'
+      /**
+   * @brief Destructor
+   */
+  virtual ~FakeCanClient() = default;
     
-    void HermesCanClient::SetInited(bool init) { _is_init = init; }
+    #include 'modules/drivers/canbus/can_client/hermes_can/hermes_can_client.h'
+#include 'gtest/gtest.h'
     
-    /*
-TEST(HermesCanClient, send) {
-  CANCardParameter param;
-  param.set_brand(CANCardParameter::HERMES_CAN);
-  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
-  HermesCanClient hermes_can;
-  EXPECT_TRUE(hermes_can.Init(param));
+        if (buf.size() != static_cast<size_t>(frame_num)) {
+      AERROR_EVERY(100) << 'Receiver buf size [' << buf.size()
+                        << '] does not match can_client returned length['
+                        << frame_num << '].';
     }
     
-    using apollo::common::ErrorCode;
-using apollo::common::time::Clock;
-using micros = std::chrono::microseconds;
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
+#include 'modules/drivers/canbus/can_comm/protocol_data.h'
     
-    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    template <typename SensorType>
+template <class T, bool need_check>
+void MessageManager<SensorType>::AddSendProtocolData() {
+  send_protocol_data_.emplace_back(new T());
+  auto *dt = send_protocol_data_.back().get();
+  if (dt == nullptr) {
+    return;
+  }
+  protocol_data_map_[T::ID] = dt;
+  if (need_check) {
+    check_ids_[T::ID].period = dt->GetPeriod();
+    check_ids_[T::ID].real_period = 0;
+    check_ids_[T::ID].last_time = 0;
+    check_ids_[T::ID].error_count = 0;
+  }
+}
     
-    #include <cstdint>
+    TEST(MessageManagerTest, GetMutableProtocolDataById) {
+  uint8_t mock_data = 1;
+  MockMessageManager manager;
+  manager.Parse(MockProtocolData::ID, &mock_data, 8);
+  manager.ResetSendMessages();
+  EXPECT_TRUE(manager.GetMutableProtocolDataById(MockProtocolData::ID) !=
+              nullptr);
+    }
+    
+    std::string Byte::byte_to_binary(const uint8_t value) {
+  return std::bitset<8 * sizeof(uint8_t)>(value).to_string();
+}
+    
+    /**
+ * @file
+ */
