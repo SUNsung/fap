44235301b00aac384bb20d2d40fa584a2fb0fa58
SUNsung <weatherius@gmail.com>
2018-10-23 10:14:33 +0300

    Rewrite [59DmcOAphNQrk5976dlBZotBM597Oomj5XjZs59LdD.cpp] File in: 2018-10-23 10:14:32

--- cccccc/59DmcOAphNQrk5976dlBZotBM597Oomj5XjZs59LdD.cpp
+++ cccccc/59DmcOAphNQrk5976dlBZotBM597Oomj5XjZs59LdD.cpp
@@ -1,76 +1,248 @@
 
-    {    return;
-  }
-    
-        // |-- bitsRemaining --|-------- ChunkSizeInBits --------|
-    // |     prevChunk     |             nextChunk           |
-    // |------ numBitsWanted ------|----- bitsRemaining' ----|
-    //                             |        prevChunk'       |
-    
-    void CacheImpl::setAndRetain(void *Key, void *Value, size_t Cost) {
-  cache_set_and_retain(static_cast<cache_t*>(Impl), Key, Value, Cost);
-}
-    
-      assert(capacity % 16 == 0 && 'not allocating multiple of alignment');
-    
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-      private:
-    /// Is this a diagnostic that doesn't do the user any good to show if it
-    /// is located in one of Swift's synthetic buffers? If so, returns true to
-    /// suppress it.
-    static bool shouldSuppressDiagInSwiftBuffers(clang::DiagOrStoredDiag info) {
-      if (info.isNull())
-        return false;
-    }
-    
-    BENCHMARK_RELATIVE(intAppend_to) {
-  fbstring out;
-  for (int i = -1000; i < 1000; i++) {
-    toAppend(i, &out);
-  }
-}
-    
-    TEST(StaticTracepoint, TestEmpty) {
-  emptyTestFunc();
-    }
-    
-    #pragma once
-    
-    
-    {void Executor::keepAliveRelease() {
-  LOG(FATAL) << __func__ << '() should not be called for folly::Executor types '
-             << 'which do not override keepAliveAcquire()';
-}
-} // namespace folly
-
-    
-    
-    {  static constexpr size_t stateSize = StateSizeT<RNG>::value;
-  std::array<uint32_t, stateSize> seedData;
-};
-    
-    using UriTuple = std::tuple<
-    const std::string&,
-    const std::string&,
-    const std::string&,
-    const std::string&,
-    uint16_t,
-    const std::string&,
-    const std::string&,
-    const std::string&>;
+        
+            /** Colorize an icon (given object) with the text color */
+    QIcon TextColorIcon(const QIcon& icon) const;
     
     /**
- * Interface for Zlib-specific codec initialization.
+ * Changelog:
+ * - March 2013, Diederik Huys:    original version
+ * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
+ * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
  */
-namespace folly {
-namespace io {
-namespace zlib {
+    
+    static void secp256k1_ge_set_xy(secp256k1_ge *r, const secp256k1_fe *x, const secp256k1_fe *y) {
+    r->infinity = 0;
+    r->x = *x;
+    r->y = *y;
+}
+    
+    #endif /* SECP256K1_MODULE_ECDH_MAIN_H */
+
+    
+    int secp256k1_ecdsa_recover(const secp256k1_context* ctx, secp256k1_pubkey *pubkey, const secp256k1_ecdsa_recoverable_signature *signature, const unsigned char *msg32) {
+    secp256k1_ge q;
+    secp256k1_scalar r, s;
+    secp256k1_scalar m;
+    int recid;
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(secp256k1_ecmult_context_is_built(&ctx->ecmult_ctx));
+    ARG_CHECK(msg32 != NULL);
+    ARG_CHECK(signature != NULL);
+    ARG_CHECK(pubkey != NULL);
+    }
+    
+    
+    {    // These inputs are valid because PROTOCOLINFO accepts an OtherLine that is
+    // just an OptArguments, which enables multiple spaces to be present
+    // between the command and arguments.
+    CheckSplitTorReplyLine('COMMAND  ARGS', 'COMMAND', ' ARGS');
+    CheckSplitTorReplyLine('COMMAND   EVEN+more  ARGS', 'COMMAND', '  EVEN+more  ARGS');
+}
+    
+        accumulateSquareConstFunc funcs[16] =
+    {
+        accumulateSquareConst<0>,
+        accumulateSquareConst<1>,
+        accumulateSquareConst<2>,
+        accumulateSquareConst<3>,
+        accumulateSquareConst<4>,
+        accumulateSquareConst<5>,
+        accumulateSquareConst<6>,
+        accumulateSquareConst<7>,
+        accumulateSquareConst<8>,
+        accumulateSquareConst<9>,
+        accumulateSquareConst<10>,
+        accumulateSquareConst<11>,
+        accumulateSquareConst<12>,
+        accumulateSquareConst<13>,
+        accumulateSquareConst<14>,
+        accumulateSquareConst<15>
+    }, func = funcs[shift];
+    
+    IMPL_ADDWEIGHTED(u8)
+IMPL_ADDWEIGHTED(s8)
+IMPL_ADDWEIGHTED(u16)
+IMPL_ADDWEIGHTED(s16)
+IMPL_ADDWEIGHTED(u32)
+IMPL_ADDWEIGHTED(s32)
+IMPL_ADDWEIGHTED(f32)
+    
+    #include 'common.hpp'
+    
+            size_t i = 0;
+        uint64x2_t ws = vmovq_n_u64(0);
+    
+    namespace {
+    }
+    
+    
+    {
+    {        for (--jd; js < size.width; ++js, --jd)
+            dst[jd] = src[js];
+    }
+}
+    
+      /**
+   * \fn  virtual void Predictor::PredictLeaf(DMatrix* dmat,
+   * std::vector<bst_float>* out_preds, const gbm::GBTreeModel& model, unsigned
+   * ntree_limit = 0) = 0;
+   *
+   * \brief predict the leaf index of each tree, the output will be nsample *
+   * ntree vector this is only valid in gbtree predictor.
+   *
+   * \param [in,out]  dmat        The input feature matrix.
+   * \param [in,out]  out_preds   The output preds.
+   * \param           model       Model to make predictions from.
+   * \param           ntree_limit (Optional) The ntree limit.
+   */
+    
+    namespace dmlc {
+namespace data {
     }
     }
+    
+    #if DMLC_ENABLE_STD_THREAD
+/*!
+ * \brief A threaded writer to write sparse batch page to sharded files.
+ */
+class SparsePageWriter {
+ public:
+  /*!
+   * \brief constructor
+   * \param name_shards name of shard files.
+   * \param format_shards format of each shard.
+   * \param extra_buffer_capacity Extra buffer capacity before block.
+   */
+  explicit SparsePageWriter(
+      const std::vector<std::string>& name_shards,
+      const std::vector<std::string>& format_shards,
+      size_t extra_buffer_capacity);
+  /*! \brief destructor, will close the files automatically */
+  ~SparsePageWriter();
+  /*!
+   * \brief Push a write job to the writer.
+   * This function won't block,
+   * writing is done by another thread inside writer.
+   * \param page The page to be written
+   */
+  void PushWrite(std::shared_ptr<SparsePage>&& page);
+  /*!
+   * \brief Allocate a page to store results.
+   *  This function can block when the writer is too slow and buffer pages
+   *  have not yet been recycled.
+   * \param out_page Used to store the allocated pages.
+   */
+  void Alloc(std::shared_ptr<SparsePage>* out_page);
+    }
+    
+    
+    {
+    {
+    {      // Test write Symbol
+      std::vector<unsigned char> buffer2(
+        CompressedBufferWriter::CalculateBufferSize(input.size(),
+          alphabet_size));
+      for (int i = 0; i < input.size(); i++) {
+        cbw.WriteSymbol(buffer2.data(), input[i], i);
+      }
+      CompressedIterator<int> ci2(buffer.data(), alphabet_size);
+      std::vector<int> output2(input.size());
+      for (int i = 0; i < input.size(); i++) {
+        output2[i] = ci2[i];
+      }
+      ASSERT_TRUE(input == output2);
+    }
+  }
+}
+    
+    public:
+  static const Data& getData() { return data_; }
+    
+    const std::string& DHTResponseMessage::getType() const { return R; }
+    
+      void moveBucketHead(const std::shared_ptr<DHTNode>& node);
+    
+      std::vector<std::shared_ptr<DHTNode>> nodes_;
+    
+    void DHTTaskQueueImpl::executeTask()
+{
+  A2_LOG_DEBUG('Updating periodicTaskQueue1');
+  periodicTaskQueue1_.update();
+  A2_LOG_DEBUG('Updating periodicTaskQueue2');
+  periodicTaskQueue2_.update();
+  A2_LOG_DEBUG('Updating immediateTaskQueue');
+  immediateTaskQueue_.update();
+}
+    
+    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
+                                             std::chrono::seconds interval)
+    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
+{
+}
+    
+      virtual void preProcess() CXX11_OVERRIDE;
+    
+      // returns 'unknown'
+  virtual const std::string& getMessageType() const CXX11_OVERRIDE;
+    
+        bool operator<(const CacheEntry& e) const;
+    
+    
+    {}  // namespace guetzli
+    
+      // Returns the combined score of the output image in the last Compare() call
+  // (or the baseline image, if Compare() was not called yet), based on output
+  // size and the similarity metric.
+  virtual double ScoreOutputSize(int size) const = 0;
+    
+    
+// these are the macro required by COLUMN_*
+#define LOAD_CST(dst, src) (dst) = (src)
+#define LOAD(dst, src) (dst) = (src)
+#define MULT(a, b)  (a) = (((a) * (b)) >> 16)
+#define ADD(a, b)   (a) = (a) + (b)
+#define SUB(a, b)   (a) = (a) - (b)
+#define LSHIFT(a, n) (a) = ((a) << (n))
+#define STORE16(a, b) (a) = (b)
+#define CORRECT_LSB(a) (a) += 1
+    
+    void SaveQuantTables(const int q[3][kDCTBlockSize], JPEGData* jpg) {
+  const size_t kTableSize = kDCTBlockSize * sizeof(q[0][0]);
+  jpg->quant.clear();
+  int num_tables = 0;
+  for (size_t i = 0; i < jpg->components.size(); ++i) {
+    JPEGComponent* comp = &jpg->components[i];
+    // Check if we have this quant table already.
+    bool found = false;
+    for (int j = 0; j < num_tables; ++j) {
+      if (memcmp(&q[i][0], &jpg->quant[j].values[0], kTableSize) == 0) {
+        comp->quant_idx = j;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      JPEGQuantTable table;
+      memcpy(&table.values[0], &q[i][0], kTableSize);
+      table.precision = 0;
+      for (int k = 0; k < kDCTBlockSize; ++k) {
+        assert(table.values[k] >= 0);
+        assert(table.values[k] < (1 << 16));
+        if (table.values[k] > 0xff) {
+          table.precision = 1;
+        }
+      }
+      table.index = num_tables;
+      comp->quant_idx = num_tables;
+      jpg->quant.push_back(table);
+      ++num_tables;
+    }
+  }
+}
+    
+    void Quantize(coeff_t* v, int iquant) {
+  *v = (*v * iquant + kBias) >> kDCTBits;
+}
+    
+    namespace guetzli {
     }
