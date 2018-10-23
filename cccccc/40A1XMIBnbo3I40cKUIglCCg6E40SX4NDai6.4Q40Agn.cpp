541cb08738cdcd59d52bba063fb989dfa4f29b81
SUNsung <weatherius@gmail.com>
2018-10-23 17:17:09 +0300

    Rewrite [40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp] File in: 2018-10-23 17:17:07

--- cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
+++ cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
@@ -1,150 +1,130 @@
 
         
-        #include <cctype>
-    
-    // _fi_sep determines the directory separator, either '\\' or '/'
-BOOST_REGEX_DECL extern const char* _fi_sep;
-    
-    #ifdef BOOST_HAS_ABI_HEADERS
-#  include BOOST_ABI_SUFFIX
-#endif
-    
-    BOOST_MPL_HAS_XXX_TRAIT_DEF(const_iterator)
-    
-    
-namespace boost{
-    }
-    
-    
-    
-    
-    {} // namespace boost
-    
-    template <class BaseT>
-struct default_wrapper : public BaseT
-{
-   typedef typename BaseT::char_type char_type;
-   std::string error_string(::boost::regex_constants::error_type e)const
-   {
-      return ::boost::BOOST_REGEX_DETAIL_NS::get_default_error_string(e);
-   }
-   ::boost::regex_constants::syntax_type syntax_type(char_type c)const
-   {
-      return ((c & 0x7f) == c) ? get_default_syntax_type(static_cast<char>(c)) : ::boost::regex_constants::syntax_char;
-   }
-   ::boost::regex_constants::escape_syntax_type escape_syntax_type(char_type c)const
-   {
-      return ((c & 0x7f) == c) ? get_default_escape_syntax_type(static_cast<char>(c)) : ::boost::regex_constants::escape_type_identity;
-   }
-   int toi(const char_type*& p1, const char_type* p2, int radix)const
-   {
-      return ::boost::BOOST_REGEX_DETAIL_NS::global_toi(p1, p2, radix, *this);
-   }
-   char_type translate(char_type c, bool icase)const
-   {
-      return (icase ? this->translate_nocase(c) : this->translate(c));
-   }
-   char_type translate(char_type c)const
-   {
-      return BaseT::translate(c);
-   }
-   char_type tolower(char_type c)const
-   {
-      return ::boost::BOOST_REGEX_DETAIL_NS::global_lower(c);
-   }
-   char_type toupper(char_type c)const
-   {
-      return ::boost::BOOST_REGEX_DETAIL_NS::global_upper(c);
-   }
-};
-    
-      inline void SkipLine() {
-    do {
-      ch_buf_ = this->GetChar();
-    } while (ch_buf_ != EOF && ch_buf_ != '\n' && ch_buf_ != '\r');
-  }
-    
-    struct GBTreeModel {
-  explicit GBTreeModel(bst_float base_margin) : base_margin(base_margin) {}
-  void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) {
-    // initialize model parameters if not yet been initialized.
-    if (trees.size() == 0) {
-      param.InitAllowUnknown(cfg);
-    }
-  }
-    }
-    
-    
-    {    return false;
+        Base::~Base() {
 }
     
-    bool ActionInterval::sendUpdateEventToScript(float dt, Action *actionObject)
-{
-#if CC_ENABLE_SCRIPT_BINDING
-    if (_scriptType == kScriptTypeJavascript)
+    
     {
-        if (ScriptEngineManager::sendActionEventToJS(actionObject, kActionUpdate, (void *)&dt))
-            return true;
-    }
-#endif
-    return false;
-}
-    
-    /** @class BezierBy
- * @brief An action that moves the target with a cubic Bezier curve by a certain distance.
- */
-class CC_DLL BezierBy : public ActionInterval
-{
-public:
-    /** Creates the action with a duration and a bezier configuration.
-     * @param t Duration time, in seconds.
-     * @param c Bezier config.
-     * @return An autoreleased BezierBy object.
-     * @code
-     * When this function bound to js or lua,the input params are changed.
-     * in js: var create(var t,var table)
-     * in lua: local create(local t, local table)
-     * @endcode
-     */
-    static BezierBy* create(float t, const ccBezierConfig& c);
+    {    if (zoom_controller) {
+      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
+      if (zoom_factor > content::kMaximumZoomFactor) {
+        zoom_factor = content::kMaximumZoomFactor;
+      }
+      if (zoom_factor < content::kMinimumZoomFactor) {
+        zoom_factor = content::kMinimumZoomFactor;
+      }
+      x *= zoom_factor;
+      y *= zoom_factor;
     }
     
-    void ProgressFromTo::update(float time)
-{
-    ((kProgressTimerCast)(_target))->setPercentage(_from + (_to - _from) * time);
-}
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-    /**
- * @file
- * @brief Defines the CanClientFactory class.
- */
-    
-    bool EsdCanClient::Init(const CANCardParameter &parameter) {
-  if (!parameter.has_channel_id()) {
-    AERROR << 'Init CAN failed: parameter does not have channel id. The '
-              'parameter is '
-           << parameter.DebugString();
-    return false;
+    Popup(x, y, rvh);
+  } else if (method == 'EnableShowEvent') {
+    arguments.GetBoolean(0, &enable_show_event_);
+  } else {
+    NOTREACHED() << 'Invalid call to Menu method:' << method
+                 << ' arguments:' << arguments;
   }
-  port_ = parameter.channel_id();
+}
+    
+        int menu_id;
+    if (option.GetInteger('submenu', &menu_id))
+      SetSubmenu(dispatcher_host()->GetApiObject<Menu>(menu_id));
+    std::string key;
+    if (option.GetString('key',&key)){
+      enable_shortcut = true;
+      std::string modifiers = '';
+      option.GetString('modifiers',&modifiers);
+      modifiers_mask = GdkModifierType(0);
+      if (modifiers.size() != 0){
+        if (modifiers.find('ctrl') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_CONTROL_MASK);
+        }
+        if (modifiers.find('alt') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_MOD1_MASK);
+        }
+        if (modifiers.find('super') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_SUPER_MASK);
+        }
+        if (modifiers.find('meta') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_META_MASK);
+        }
+        
+        if (modifiers.find('shift') != std::string::npos){
+          modifiers_mask = GdkModifierType(modifiers_mask|GDK_SHIFT_MASK);
+        }
+    }
+    }
+    
+    #include 'base/run_loop.h'
+#include 'content/public/browser/browsing_data_remover.h'
+#include 'extensions/browser/extension_function.h'
+    
+          std::string encoded_image_base64;
+      std::string encoded_image_str(encoded_image.data(), encoded_image.data() + encoded_image.size());
+      base::Base64Encode(encoded_image_str, &encoded_image_base64);
+    
+    
+    {  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
+  manager->OnDeallocateObject(id);
   return true;
 }
     
-    #include <string>
-#include <vector>
-#include 'esd_can/include/ntcan.h'
-#include 'gflags/gflags.h'
-#include 'modules/common/proto/error_code.pb.h'
-#include 'modules/drivers/canbus/can_client/can_client.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-#include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
+    Benchmark::Benchmark(const char* name)
+    : name_(name),
+      report_mode_(RM_Unspecified),
+      time_unit_(kNanosecond),
+      range_multiplier_(kRangeMultiplier),
+      min_time_(0),
+      iterations_(0),
+      repetitions_(0),
+      use_real_time_(false),
+      use_manual_time_(false),
+      complexity_(oNone),
+      complexity_lambda_(nullptr) {
+  ComputeStatistics('mean', StatisticsMean);
+  ComputeStatistics('median', StatisticsMedian);
+  ComputeStatistics('stddev', StatisticsStdDev);
+}
+    
+      bool term_supports_color = false;
+  for (const char* candidate : SUPPORTED_TERM_VALUES) {
+    if (term && 0 == strcmp(term, candidate)) {
+      term_supports_color = true;
+      break;
+    }
+  }
+    
+    // Function to return an string for the calculated complexity
+std::string GetBigOString(BigO complexity);
+    
+    #include 'benchmark/benchmark.h'
+#include 'internal_macros.h'
+    
+    #ifndef BENCHMARK_RE_H_
+#define BENCHMARK_RE_H_
+    
+      /**
+   * @brief Send messages
+   * @param frames The messages to send.
+   * @param frame_num The amount of messages to send.
+   * @return The status of the sending action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  virtual apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                         int32_t *const frame_num) = 0;
+    
+    /**
+ * @namespace apollo::drivers::canbus::can
+ * @brief apollo::drivers::canbus::can
+ */
+namespace apollo {
+namespace drivers {
+namespace canbus {
+namespace can {
+    }
+    }
+    }
+    }
     
     TEST(HermesCanClient, receiver) {
   CANCardParameter param;
@@ -154,60 +134,37 @@ furnished to do so, subject to the following conditions:
   EXPECT_TRUE(hermes_can.Init(param));
     }
     
-      for (int32_t i = 0; i < *frame_num && i < MAX_CAN_RECV_FRAME_LEN; ++i) {
-    CanFrame cf;
-    int ret = read(dev_handler_, &recv_frames_[i], sizeof(recv_frames_[i]));
-    }
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-namespace can {
-    }
-    }
-    }
-    }
-    
       /**
-   * @brief Initialize by a CAN client, message manager.
-   * @param can_client The CAN client to use for receiving messages.
-   * @param pt_manager The message manager which can parse and
-   *        get protocol data by message id.
-   * @param enable_log If log the essential information during running.
-   * @return An error code indicating the status of this initialization.
+   * @brief Stop the CAN receiver.
    */
-  common::ErrorCode Init(CanClient *can_client,
-                         MessageManager<SensorType> *pt_manager,
-                         bool enable_log);
+  void Stop();
     
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-#include 'modules/common/proto/error_code.pb.h'
-#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
-#include 'modules/drivers/canbus/can_comm/protocol_data.h'
     
-    /**
- * @struct CheckIdArg
- *
- * @brief this struct include data for check ids.
- */
-struct CheckIdArg {
-  int64_t period = 0;
-  int64_t real_period = 0;
-  int64_t last_time = 0;
-  int32_t error_count = 0;
-};
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
     
-      /*
-   * @brief reset the protocol data
-   */
-  virtual void Reset();
+    #ifndef MODULES_DRIVERS_CANBUS_CAN_COMM_PROTOCOL_DATA_H_
+#define MODULES_DRIVERS_CANBUS_CAN_COMM_PROTOCOL_DATA_H_
     
-    TEST(ByteTest, GetValue) {
-  unsigned char byte_value = 0x1A;
-  Byte value(&byte_value);
-  EXPECT_EQ(0x05, value.get_byte(1, 3));
-  EXPECT_EQ(0x01, value.get_byte(1, 1));
-  EXPECT_EQ(0x00, value.get_byte(8, 1));
-  EXPECT_EQ(0x00, value.get_byte(-1, 1));
-  EXPECT_EQ(0x1A, value.get_byte(0, 10));
+    TEST(ProtocolDataTest, CheckSum) {
+  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
+  const uint8_t result =
+      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
+  EXPECT_EQ(0xE7, result);
 }
+    
+    #include 'modules/drivers/canbus/sensor_gflags.h'
+    
+    // System gflags
+DECLARE_string(sensor_node_name);
+    
+    
+    {
+    {
+    {} // namespace detail
+} // namespace atomics
+} // namespace mars_boost
