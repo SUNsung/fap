2ff7192dfa97c1f0b81c0e447c906a60923e36a0
SUNsung <weatherius@gmail.com>
2018-10-23 09:52:36 +0300

    Rewrite [08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp] File in: 2018-10-23 09:52:35

--- cccccc/08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp
+++ cccccc/08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp
@@ -1,172 +1,119 @@
 
         
-        public:
-    explicit SignVerifyMessageDialog(const PlatformStyle *platformStyle, QWidget *parent);
-    ~SignVerifyMessageDialog();
-    
-    #endif
-
-    
-    int secp256k1_ecdsa_recoverable_signature_parse_compact(const secp256k1_context* ctx, secp256k1_ecdsa_recoverable_signature* sig, const unsigned char *input64, int recid) {
-    secp256k1_scalar r, s;
-    int ret = 1;
-    int overflow = 0;
-    }
-    
-        BOOST_CHECK(v.setStr('zum'));
-    BOOST_CHECK(v.isStr());
-    BOOST_CHECK_EQUAL(v.getValStr(), 'zum');
-    
-    #else  // !defined(__AVX__)
-// Implementation for avx capable archs.
-#include <immintrin.h>
-#include <cstdint>
-#include 'dotproductavx.h'
-    
-    #include <cstdint>      // for int32_t
-    
-    IntSimdMatrixAVX2::IntSimdMatrixAVX2() {
-#ifdef __AVX2__
-  num_outputs_per_register_ = kNumOutputsPerRegister;
-  max_output_registers_ = kMaxOutputRegisters;
-  num_inputs_per_register_ = kNumInputsPerRegister;
-  num_inputs_per_group_ = kNumInputsPerGroup;
-  num_input_groups_ = kNumInputGroups;
-  partial_funcs_ = {PartialMatrixDotVector64, PartialMatrixDotVector32,
-                    PartialMatrixDotVector16, PartialMatrixDotVector8};
-#endif  // __AVX2__
+        
+    {  // Expand the box.
+  *left = std::max(*left - padding, 0);
+  *top = std::max(*top - padding, 0);
+  right = std::min(right + padding, rect_width_);
+  bottom = std::min(bottom + padding, rect_height_);
+  Box* box = boxCreate(*left, *top, right - *left, bottom - *top);
+  Pix* grey_pix = pixClipRectangle(original_img, box, nullptr);
+  boxDestroy(&box);
+  if (level == RIL_BLOCK || level == RIL_PARA) {
+    // Clip to the block polygon as well.
+    TBOX mask_box;
+    Pix* mask = it_->block()->block->render_mask(&mask_box);
+    // Copy the mask registered correctly into an image the size of grey_pix.
+    int mask_x = *left - mask_box.left();
+    int mask_y = *top - (pixGetHeight(original_img) - mask_box.top());
+    int width = pixGetWidth(grey_pix);
+    int height = pixGetHeight(grey_pix);
+    Pix* resized_mask = pixCreate(width, height, 1);
+    pixRasterop(resized_mask, std::max(0, -mask_x), std::max(0, -mask_y), width, height,
+                PIX_SRC, mask, std::max(0, mask_x), std::max(0, mask_y));
+    pixDestroy(&mask);
+    pixDilateBrick(resized_mask, resized_mask, 2 * padding + 1,
+                   2 * padding + 1);
+    pixInvert(resized_mask, resized_mask);
+    pixSetMasked(grey_pix, resized_mask, UINT32_MAX);
+    pixDestroy(&resized_mask);
+  }
+  return grey_pix;
 }
     
-    // Constructor.
-// Tests the architecture in a system-dependent way to detect AVX, SSE and
-// any other available SIMD equipment.
-// __GNUC__ is also defined by compilers that include GNU extensions such as
-// clang.
-SIMDDetect::SIMDDetect() {
-#if defined(X86_BUILD)
-#if defined(__GNUC__)
-  unsigned int eax, ebx, ecx, edx;
-  if (__get_cpuid(1, &eax, &ebx, &ecx, &edx) != 0) {
-    // Note that these tests all use hex because the older compilers don't have
-    // the newer flags.
-    sse_available_ = (ecx & 0x00080000) != 0;
-    avx_available_ = (ecx & 0x10000000) != 0;
-    if (avx_available_) {
-      // There is supposed to be a __get_cpuid_count function, but this is all
-      // there is in my cpuid.h. It is a macro for an asm statement and cannot
-      // be used inside an if.
-      __cpuid_count(7, 0, eax, ebx, ecx, edx);
-      avx2_available_ = (ebx & 0x00000020) != 0;
-      avx512F_available_ = (ebx & 0x00010000) != 0;
-      avx512BW_available_ = (ebx & 0x40000000) != 0;
+      /** Are we positioned at the same location as other? */
+  bool PositionedAtSameWord(const PAGE_RES_IT* other) const;
+    
+     private:
+  // The unique ID of this VC object.
+  int my_id_;
+  // Whether the parameter was changed_ and thus needs to be rewritten.
+  bool changed_;
+  // The actual ParamType of this VC object.
+  ParamType param_type_;
+    
+      Status update(const std::string& source, const ParserConfig& config) override;
+    
+    #include 'osquery/config/parsers/prometheus_targets.h'
+    
+        // Construct a config map, the typical output from `Config::genConfig`.
+    config_data_['awesome'] = content_;
+    Config::get().reset();
+    clearDecorations('awesome');
+    
+      ASSERT_TRUE(data.doc().HasMember('events'));
+  ASSERT_TRUE(data.doc()['events'].HasMember('environment_variables'));
+  ASSERT_TRUE(data.doc()['events']['environment_variables'].IsArray());
+  for (const auto& var :
+       data.doc()['events']['environment_variables'].GetArray()) {
+    std::string value = var.GetString();
+    EXPECT_TRUE(value == 'foo' || value == 'bar');
+  }
+    
+    namespace osquery {
     }
+    
+    FLAG(bool,
+     disable_distributed,
+     true,
+     'Disable distributed queries (default true)');
+    
+      SCRef GetSubscription(const std::string& path,
+                        uint32_t mask = IN_ALL_EVENTS) {
+    auto mc = createSubscriptionContext();
+    mc->path = path;
+    mc->mask = mask;
+    return mc;
   }
-#elif defined(_WIN32)
-  int cpuInfo[4];
-  __cpuid(cpuInfo, 0);
-  if (cpuInfo[0] >= 1) {
-    __cpuid(cpuInfo, 1);
-    sse_available_ = (cpuInfo[2] & 0x00080000) != 0;
-    avx_available_ = (cpuInfo[2] & 0x10000000) != 0;
-  }
-#else
-#error 'I don't know how to test for SIMD with this compiler'
-#endif
-#endif  // X86_BUILD
+    
+    class DHTBucket;
+class DHTPingReplyMessage;
+    
+    void DHTResponseMessage::fillMessage(Dict* msgDict)
+{
+  msgDict->put(R, getResponse());
 }
-
     
-    struct OSBestResult {
-  OSBestResult() : orientation_id(0), script_id(0), sconfidence(0.0),
-                   oconfidence(0.0) {}
-  int orientation_id;
-  int script_id;
-  float sconfidence;
-  float oconfidence;
-};
+      std::shared_ptr<DHTNode> localNode_;
     
-    void PageIterator::Orientation(tesseract::Orientation *orientation,
-                               tesseract::WritingDirection *writing_direction,
-                               tesseract::TextlineOrder *textline_order,
-                               float *deskew_angle) const {
-  BLOCK* block = it_->block()->block;
+    
+    {} // namespace aria2
+    
+    bool DHTTokenTracker::validateToken(const std::string& token,
+                                    const unsigned char* infoHash,
+                                    const std::string& ipaddr,
+                                    uint16_t port) const
+{
+  for (auto& elem : secret_) {
+    if (generateToken(infoHash, ipaddr, port, elem) == token) {
+      return true;
     }
-    
-      /**
-   * Returns a binary image of the current object at the given level.
-   * The position and size match the return from BoundingBoxInternal, and so
-   * this could be upscaled with respect to the original input image.
-   * Use pixDestroy to delete the image after use.
-   */
-  Pix* GetBinaryImage(PageIteratorLevel level) const;
-    
-            // TODO: should we also save momentum schedule into the checkpoint?
-        // If that is the case, need to be able to override this method in subclasses.
-        std::vector<DictionaryValue> serializedSmoothedGradients(Parameters().size());
-        size_t i = 0;
-        for (const auto& parameter : Parameters())
-        {
-            const auto& smoothedGradientValue = m_smoothedGradientValues.at(parameter);
-            serializedSmoothedGradients[i++] = *smoothedGradientValue;
-        }
-    
-    
-    {            Reset();
-        }
-    
-    /**
- * @brief A simple ConfigParserPlugin for feature vector dictionary keys.
- */
-class FeatureVectorsConfigParserPlugin : public ConfigParserPlugin {
- public:
-  std::vector<std::string> keys() const override;
-    }
-    
-      /// Accessor for autoloadable extension paths.
-  const ExtensionMap& extensions() const {
-    return extensions_;
   }
+  return false;
+}
     
-    static const int kCbToBlueTable[256] = {
-  -227, -225, -223, -222, -220, -218, -216, -214, -213, -211, -209, -207,
-  -206, -204, -202, -200, -198, -197, -195, -193, -191, -190, -188, -186,
-  -184, -183, -181, -179, -177, -175, -174, -172, -170, -168, -167, -165,
-  -163, -161, -159, -158, -156, -154, -152, -151, -149, -147, -145, -144,
-  -142, -140, -138, -136, -135, -133, -131, -129, -128, -126, -124, -122,
-  -120, -119, -117, -115, -113, -112, -110, -108, -106, -105, -103, -101,
-   -99,  -97,  -96,  -94,  -92,  -90,  -89,  -87,  -85,  -83,  -82,  -80,
-   -78,  -76,  -74,  -73,  -71,  -69,  -67,  -66,  -64,  -62,  -60,  -58,
-   -57,  -55,  -53,  -51,  -50,  -48,  -46,  -44,  -43,  -41,  -39,  -37,
-   -35,  -34,  -32,  -30,  -28,  -27,  -25,  -23,  -21,  -19,  -18,  -16,
-   -14,  -12,  -11,   -9,   -7,   -5,   -4,   -2,    0,    2,    4,    5,
-     7,    9,   11,   12,   14,   16,   18,   19,   21,   23,   25,   27,
-    28,   30,   32,   34,   35,   37,   39,   41,   43,   44,   46,   48,
-    50,   51,   53,   55,   57,   58,   60,   62,   64,   66,   67,   69,
-    71,   73,   74,   76,   78,   80,   82,   83,   85,   87,   89,   90,
-    92,   94,   96,   97,   99,  101,  103,  105,  106,  108,  110,  112,
-   113,  115,  117,  119,  120,  122,  124,  126,  128,  129,  131,  133,
-   135,  136,  138,  140,  142,  144,  145,  147,  149,  151,  152,  154,
-   156,  158,  159,  161,  163,  165,  167,  168,  170,  172,  174,  175,
-   177,  179,  181,  183,  184,  186,  188,  190,  191,  193,  195,  197,
-   198,  200,  202,  204,  206,  207,  209,  211,  213,  214,  216,  218,
-   220,  222,  223,  225,
-};
+    class DHTTokenTracker;
     
-    // A node of a Huffman tree.
-struct HuffmanTree {
-  HuffmanTree() {}
-  HuffmanTree(uint32_t count, int16_t left, int16_t right)
-      : total_count_(count),
-        index_left_(left),
-        index_right_or_value_(right) {
+    std::string DHTUnknownMessage::toString() const
+{
+  size_t sampleLength = 8;
+  if (length_ < sampleLength) {
+    sampleLength = length_;
   }
-  uint32_t total_count_;
-  int16_t index_left_;
-  int16_t index_right_or_value_;
-};
+  return fmt('dht unknown Remote:%s(%u) length=%lu, first 8 bytes(hex)=%s',
+             ipaddr_.c_str(), port_, static_cast<unsigned long>(length_),
+             util::toHex(data_, sampleLength).c_str());
+}
     
-    #ifndef GUETZLI_JPEG_DATA_DECODER_H_
-#define GUETZLI_JPEG_DATA_DECODER_H_
-    
-    namespace guetzli {
-    }
+      void markBad(const std::string& hostname, const std::string& ipaddr,
+               uint16_t port);
