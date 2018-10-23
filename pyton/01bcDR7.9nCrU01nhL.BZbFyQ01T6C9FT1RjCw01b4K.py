07ed786a74b25ac4c0fa860c5c36d660ca638f61
SUNsung <weatherius@gmail.com>
2018-10-23 22:26:54 +0300

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2018-10-23 22:26:54

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,94 +1,173 @@
 
         
-            def describe_stack_events(self, stack_name):
-        try:
-            func = partial(self.client.describe_stack_events, StackName=stack_name)
-            return self.paginated_response(func, 'StackEvents')
-        except Exception as e:
-            self.module.fail_json(msg='Error describing stack events - ' + str(e), exception=traceback.format_exc())
+          with open(vocab_file) as f_in:
+    vocab = [line.strip() for line in f_in]
     
-        def _rule_matches_aws(self):
-        '''Checks if the local rule data matches AWS'''
-        aws_rule_data = self.rule.describe()
+      @property
+  def logvar(self):
+    return self.logvar_bxn
     
-        for index in all_indexes:
-        validate_index(index, module)
+      # Turn rates, noisy_data, and input into numpy arrays.
+  rates_train = nparray_and_transpose(rates_train)
+  rates_valid = nparray_and_transpose(rates_valid)
+  noisy_data_train = nparray_and_transpose(noisy_data_train)
+  noisy_data_valid = nparray_and_transpose(noisy_data_valid)
+  input_train = nparray_and_transpose(input_train)
+  inputs_valid = nparray_and_transpose(inputs_valid)
     
-        addresses = camel_dict_to_snake_dict(response)['addresses']
-    for address in addresses:
-        if 'tags' in address:
-            address['tags'] = boto3_tag_list_to_ansible_dict(address['tags'])
-    return addresses
+    rnn_a = generate_rnn(rnn_rngs[0], N, FLAGS.g, FLAGS.tau, FLAGS.dt,
+                     FLAGS.max_firing_rate)
+rnn_b = generate_rnn(rnn_rngs[1], N, FLAGS.g, FLAGS.tau, FLAGS.dt,
+                     FLAGS.max_firing_rate)
+rnns = [rnn_a, rnn_b]
     
-        module = AnsibleModule(
-        argument_spec=argument_spec,
-    )
+    import numpy as np
+from six.moves import xrange
+import tensorflow as tf
     
-    - name: create a VPC with dedicated tenancy and a couple of tags
-  ec2_vpc_net:
-    name: Module_dev2
-    cidr_block: 10.10.0.0/16
-    region: us-east-1
-    tags:
-      module: ec2_vpc_net
-      this: works
-    tenancy: dedicated
-'''
+      Args:
+    raw_data: one of the raw data outputs from imdb_raw_data.
+    batch_size: int, the batch size.
+    num_steps: int, the number of unrolls.
     
-    - name: set-policy as string
-  ecs_ecr:
-    name: needs-policy-string
-    policy: '{{ lookup('template', 'policy.json.j2') }}'
+                # Statefulness for the Generator.
+            for i, (c, h) in enumerate(model.fake_gen_initial_state):
+              eval_feed[c] = fake_gen_initial_state_eval[i].c
+              eval_feed[h] = fake_gen_initial_state_eval[i].h
     
     
-def main():
-    argument_spec = ec2_argument_spec()
-    argument_spec.update(
-        dict(
-            name=dict(required=True, type='str'),
-            state=dict(required=True, type='str', choices=['present', 'absent', 'copy']),
-            replication_id=dict(type='str'),
-            cluster_id=dict(type='str'),
-            target=dict(type='str'),
-            bucket=dict(type='str'),
-        )
-    )
+def convert_to_human_readable(id_to_word, arr, max_num_to_print):
+  '''Convert a np.array of indices into words using id_to_word dictionary.
+  Return max_num_to_print results.
+  '''
+  assert arr.ndim == 2
     
-        if state == 'present':
-        for required in ['name', 'description', 'subnets']:
-            if not module.params.get(required):
-                module.fail_json(msg=str('Parameter %s required for state='present'' % required))
+        # Group maintain averages op.
+    if averages_op:
+      gen_train_op = tf.group(maximize_op, averages_op)
     else:
-        for not_allowed in ['description', 'subnets']:
-            if module.params.get(not_allowed):
-                module.fail_json(msg=str('Parameter %s not allowed for state='absent'' % not_allowed))
+      gen_train_op = maximize_op
     
-        cert_name = module.params.get('name')
-    results = get_server_certs(iam, cert_name)
-    module.exit_json(results=results)
     
-        cognito = _cognito(cloud)
-    # Workaround for bug in Warrant. PR with fix:
-    # https://github.com/capless/warrant/pull/82
-    cognito.add_base_attributes()
+def hash_function(input_tuple):
+  '''Hash function for a tuple.'''
+  return hash(input_tuple)
+    
+        '''
+    # The value that should be passed to --auth-type
+    # to use this auth plugin. Eg. 'my-auth'
+    auth_type = None
+    
+            def get_auth(self, username=None, password=None):
+            assert username is None
+            assert password is None
+            assert self.raw_auth == BASIC_AUTH_HEADER_VALUE
+            return basic_auth(self.raw_auth)
+    
+    
+class Response(object):
+    # noinspection PyDefaultArgument
+    def __init__(self, url, headers={}, status_code=200):
+        self.url = url
+        self.headers = CaseInsensitiveDict(headers)
+        self.status_code = status_code
+    
+    
+def test_headers_empty_value(httpbin_both):
+    r = http('GET', httpbin_both + '/headers')
+    assert r.json['headers']['Accept']  # default Accept has value
+    
+    
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
+    
+    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    title = r1(r'<title>(.*)</title>', html)
+    assert title
+    id = r1(r'flvid\s*=\s*(\d+)', html)
+    assert id
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    
+    from .theplatform import theplatform_download_by_pid
+    
+    
+def cntv_download(url, **kwargs):
+    if re.match(r'http://tv\.cntv\.cn/video/(\w+)/(\w+)', url):
+        rid = match1(url, r'http://tv\.cntv\.cn/video/\w+/(\w+)')
+    elif re.match(r'http://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
+        rid = r1(r'var guid = '(\w+)'', get_content(url))
+    elif re.match(r'http://\w+\.cntv\.cn/(\w+/\w+/(classpage/video/)?)?\d+/\d+\.shtml', url) or \
+         re.match(r'http://\w+.cntv.cn/(\w+/)*VIDE\d+.shtml', url) or \
+         re.match(r'http://(\w+).cntv.cn/(\w+)/classpage/video/(\d+)/(\d+).shtml', url) or \
+         re.match(r'http://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
+         re.match(r'http://\w+.cntv.cn/\d+/\d+/\d+/\w+.shtml', url): 
+        page = get_content(url)
+        rid = r1(r'videoCenterId','(\w+)'', page)
+        if rid is None:
+            guid = re.search(r'guid\s*=\s*'([0-9a-z]+)'', page).group(1)
+            rid = guid
+    elif re.match(r'http://xiyou.cntv.cn/v-[\w-]+\.html', url):
+        rid = r1(r'http://xiyou.cntv.cn/v-([\w-]+)\.html', url)
+    else:
+        raise NotImplementedError(url)
+    
+    __all__ = ['dailymotion_download']
+    
+            #type_ = ''
+        #size = 0
+    
+    	type, ext, size = url_info(url)
+	print_info(site_info, title, type, size)
+	
+	if not info_only:
+		download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+    import os
+import warnings
+    
+    
+class FlicButton(BinarySensorDevice):
+    '''Representation of a flic button.'''
+    
+        def random_see(dev_id, name):
+        '''Randomize a sighting.'''
+        see(
+            dev_id=dev_id,
+            host_name=name,
+            gps=(hass.config.latitude + offset(),
+                 hass.config.longitude + offset()),
+            gps_accuracy=random.randrange(50, 150),
+            battery=random.randrange(10, 90)
+        )
+    
+        return scanner if scanner.success_init else None
+    
+    
+def get_scanner(hass, config):
+    '''Validate the configuration and return a Linksys AP scanner.'''
     try:
-        cognito.register(email, password)
-    except ClientError as err:
-        raise _map_aws_exception(err)
+        return LinksysSmartWifiDeviceScanner(config[DOMAIN])
+    except ConnectionError:
+        return None
     
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/device_tracker.actiontec/
-'''
-import logging
-import re
-import telnetlib
-from collections import namedtuple
-import voluptuous as vol
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_SSL, default=DEFAULT_SSL): cv.boolean
+})
     
-            self.last_results = {}
-        self.refresh_token()
-        self.mac2name = None
-        self.success_init = self.token is not None
+            try:
+            result = scanner.scan(hosts=' '.join(self.hosts),
+                                  arguments=options)
+        except PortScannerError:
+            return False
     
-        dispatcher_connect(hass, SIGNAL_VEHICLE_SEEN, see_vehicle)
-    dispatcher_send(hass, SIGNAL_VEHICLE_SEEN, vehicle)
+    _LOGGER = logging.getLogger(__name__)
