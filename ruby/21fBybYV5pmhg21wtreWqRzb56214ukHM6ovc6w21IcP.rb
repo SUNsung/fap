e9150223e8bb19e995cbc9f5d0b9e4056a9b8791
SUNsung <weatherius@gmail.com>
2018-10-24 15:14:12 +0300

    Rewrite [21fBybYV5pmhg21wtreWqRzb56214ukHM6ovc6w21IcP.rb] File in: 2018-10-24 15:14:11

--- ruby/21fBybYV5pmhg21wtreWqRzb56214ukHM6ovc6w21IcP.rb
+++ ruby/21fBybYV5pmhg21wtreWqRzb56214ukHM6ovc6w21IcP.rb
@@ -1,138 +1,43 @@
 
         
-                def test_url_sub_key_for_sqlite3
-          spec = resolve :production, 'production' => { 'url' => 'sqlite3:foo?encoding=utf8' }
-          assert_equal({
-            'adapter'  => 'sqlite3',
-            'database' => 'foo',
-            'encoding' => 'utf8',
-            'name'     => 'production' }, spec)
-        end
+              GivenDailyLike.decrement_for(user.id)
+      expect(value_for(user.id, dt)).to eq(0)
+      expect(limit_reached_for(user.id, dt)).to eq(false)
+    end
     
-      def test_where_by_serialized_attribute_with_array
-    settings = [ 'color' => 'green' ]
-    Topic.serialize(:content, Array)
-    topic = Topic.create!(content: settings)
-    assert_equal topic, Topic.where(content: settings).take
-  end
+        Category.transaction do
+      staff.group_names = ['staff']
+      unless staff.save
+        puts staff.errors.full_messages
+        raise 'Failed to set permissions on the Staff category!'
+      end
     
-      def test_find_scoped_grouped
-    assert_equal 1, companies(:first_firm).clients_grouped_by_firm_id.size
-    assert_equal 1, companies(:first_firm).clients_grouped_by_firm_id.length
-    assert_equal 3, companies(:first_firm).clients_grouped_by_name.size
-    assert_equal 3, companies(:first_firm).clients_grouped_by_name.length
-  end
+    module Admin
+  class ConfirmationsController < BaseController
+    before_action :set_user
+    before_action :check_confirmation, only: [:resend]
     
-    require 'models/topic'
+            redirect_to admin_report_path(@report), notice: I18n.t('admin.report_notes.created_msg')
+      else
+        @report_notes = @report.notes.latest
+        @report_history = @report.history
+        @form = Form::StatusBatch.new
+    
+          @form         = Form::StatusBatch.new(form_status_batch_params.merge(current_account: current_account, action: action_from_button))
+      flash[:alert] = I18n.t('admin.statuses.failed_to_execute') unless @form.save
     
       private
-    def open_connection
-      env = Rack::MockRequest.env_for '/test',
-        'HTTP_CONNECTION' => 'upgrade', 'HTTP_UPGRADE' => 'websocket',
-        'HTTP_HOST' => 'localhost', 'HTTP_ORIGIN' => 'http://rubyonrails.com'
-      env['rack.hijack'] = -> { env['rack.hijack_io'] = StringIO.new }
     
-          data = { 'content' => 'Hello World!', 'action' => 'speak' }
-      @subscriptions.execute_command 'command' => 'message', 'identifier' => @chat_identifier, 'data' => ActiveSupport::JSON.encode(data)
+      private
     
-        assert_called_with ::Redis, :new, [ config ] do
-      connect config.merge(other: 'unrelated', stuff: 'here')
-    end
-  end
-    
-      # Asynchronously send an email
-  class TestEmail < Jobs::Base
-    
-        if resource.errors.empty?
-      set_flash_message!(:notice, :confirmed)
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
-    
-        def email_changed(record, opts={})
-      devise_mail(record, :email_changed, opts)
-    end
-    
-          # Set up a subject doing an I18n lookup. At first, it attempts to set a subject
-      # based on the current mapping:
-      #
-      #   en:
-      #     devise:
-      #       mailer:
-      #         confirmation_instructions:
-      #           user_subject: '...'
-      #
-      # If one does not exist, it fallbacks to ActionMailer default:
-      #
-      #   en:
-      #     devise:
-      #       mailer:
-      #         confirmation_instructions:
-      #           subject: '...'
-      #
-      def subject_for(key)
-        I18n.t(:'#{devise_mapping.name}_subject', scope: [:devise, :mailer, key],
-          default: [:subject, key.to_s.humanize])
+            expect(new_source).to eq(construct(true, true))
       end
     end
   end
-end
-
     
-    module Devise
-  # Responsible for handling devise mappings and routes configuration. Each
-  # resource configured by devise_for in routes is actually creating a mapping
-  # object. You can refer to devise_for in routes for usage options.
-  #
-  # The required value in devise_for is actually not used internally, but it's
-  # inflected to find all other values.
-  #
-  #   map.devise_for :users
-  #   mapping = Devise.mappings[:user]
-  #
-  #   mapping.name #=> :user
-  #   # is the scope used in controllers and warden, given in the route as :singular.
-  #
-  #   mapping.as   #=> 'users'
-  #   # how the mapping should be search in the path, given in the route as :as.
-  #
-  #   mapping.to   #=> User
-  #   # is the class to be loaded from routes, given in the route as :class_name.
-  #
-  #   mapping.modules  #=> [:authenticatable]
-  #   # is the modules included in the class
-  #
-  class Mapping #:nodoc:
-    attr_reader :singular, :scoped_path, :path, :controllers, :path_names,
-                :class_name, :sign_out_via, :format, :used_routes, :used_helpers,
-                :failure_app, :router_name
-    
-          case Rails.env
-      when 'development'
-        config.eager_load = false
-      when 'test'
-        config.eager_load = false
-      when 'production'
-        config.eager_load = true
+          # Returns the operator for the `kwsplat` as a string.
+      #
+      # @return [String] the double splat operator
+      def operator
+        DOUBLE_SPLAT
       end
-    end
-  end
-end
-    
-          when :bye
-        sessions.delete(s[:session])
-    
-          when :login_pass
-    
-    unless STDIN.tty?
-	p = ::Parser.new('')
-	p.parse
-	p.dump_all
-else
-	print 'Tested with:\n'
-	print '\tGNU objdump 2.9-aix51-020209\n'
-	print '\tGNU objdump 2.15.92.0.2 20040927\n'
-	print 'Usage: objdump -dM suffix <file(s)> | ruby objdumptoc.rb\n'
-end
