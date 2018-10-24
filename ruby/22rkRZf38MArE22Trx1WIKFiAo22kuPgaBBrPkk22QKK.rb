811a409e7b9d65f9ce229deab7acaaf6ad456e61
SUNsung <weatherius@gmail.com>
2018-10-24 09:50:54 +0300

    Rewrite [22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb] File in: 2018-10-24 09:50:53

--- ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
+++ ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
@@ -1,102 +1,130 @@
 
         
-                  def render_collection_for(builder_class, &block)
-            options = @options.stringify_keys
-            rendered_collection = render_collection do |item, value, text, default_html_options|
-              builder = instantiate_builder(builder_class, item, value, text, default_html_options)
-    
-        initializer 'action_view.setup_action_pack' do |app|
-      ActiveSupport.on_load(:action_controller) do
-        ActionView::RoutingUrlFor.include(ActionDispatch::Routing::UrlFor)
-      end
-    end
-    
-              def satisfied_by?(pipeline, seed = nil)
-            @patterns.any? do |pattern|
-              pattern, path = pattern.split('@', 2)
-    
-    class ActivityPub::CollectionsController < Api::BaseController
-  include SignatureVerification
-    
-      def body
-    @body ||= request.body.read
-  end
-    
-        def destroy
-      authorize @account_moderation_note, :destroy?
-      @account_moderation_note.destroy!
-      redirect_to admin_account_path(@account_moderation_note.target_account_id), notice: I18n.t('admin.account_moderation_notes.destroyed_msg')
-    end
-    
-        def resubscribe
-      authorize :instance, :resubscribe?
-      params.require(:by_domain)
-      Pubsubhubbub::SubscribeWorker.push_bulk(subscribeable_accounts.pluck(:id))
-      redirect_to admin_instances_path
-    end
-    
-        def create
-      authorize ReportNote, :create?
-    
-      def process_salmon
-    SalmonWorker.perform_async(@account.id, payload.force_encoding('UTF-8'))
-  end
+        platforms :ruby do
+  gem 'sqlite3'
 end
-
     
-          root.children.each {|v| nest_seqs(v)}
+      def sign_in_params
+    devise_parameter_sanitizer.sanitize(:sign_in)
+  end
+    
+        def confirmation_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :confirmation_instructions, opts)
     end
     
-            line_tab_str = line[/^\s*/]
-        unless line_tab_str.empty?
-          if tab_str.nil?
-            comment_tab_str ||= line_tab_str
-            next if try_comment(line, lines.last, '', comment_tab_str, index)
-            comment_tab_str = nil
+              def current_#{mapping}
+            @current_#{mapping} ||= warden.authenticate(scope: :#{mapping})
           end
     
-        # variable
-    # Script::Value
-    inherited_hash_reader :var
+          def self.generate_helpers!(routes=nil)
+        routes ||= begin
+          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
+          Devise::URL_HELPERS.slice(*mappings)
+        end
     
-        def URIEncodeOctets(octets, result, index)
-      if (@@hexCharCodeArray == 0)
-        @@hexCharCodeArray = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
-                              65, 66, 67, 68, 69, 70];
-      end
-      index = URIAddEncodedOctetToBuffer(octets[0], result, index);
-      if (octets[1]);
-        index = URIAddEncodedOctetToBuffer(octets[1], result, index)
-      end
-      if (octets[2]);
-        index = URIAddEncodedOctetToBuffer(octets[2], result, index)
-      end
-      if (octets[3]);
-        index = URIAddEncodedOctetToBuffer(octets[3], result, index)
-      end
-      return index;
+        def inheritable_copy
+      self.class.new @filters
     end
     
-          def is_create_page
-        true
+        def blank?
+      body.blank?
+    end
+    
+          def initial_urls
+        super + self.class.base_urls[1..-1].deep_dup
       end
     
-          def header
-        if @header.nil?
-          if page = @page.header
-            @header = page.text_data
-          else
-            @header = false
+            css('header').each do |node|
+          node.before(node.children).remove
+        end
+    
+        ff.each do |f|
+      missing = f.missing_dependencies(hide: ARGV.values('hide'))
+      next if missing.empty?
+    
+      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
+    @logger     = Logger.new
+    @repo       = repo
+    @branch     = branch || 'master'
+    @branch_sha = get_branch_sha
+    @cache_path = cache_path
+    @repo_url   = 'https://github.com/#@repo'
+    @save_to    = {
+        js:    'assets/javascripts/bootstrap',
+        scss:  'assets/stylesheets/bootstrap',
+        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
+  end
+    
+        # advance scanner to pos after the next match of pattern and return the match
+    def scan_next(pattern)
+      return unless @s.scan_until(pattern)
+      @s.matched
+    end
+    
+          replace_rules file do |rule|
+        replace_properties rule do |props|
+          props.gsub /(?<!\w)([\w-]+):(.*?);/ do |m|
+            prop, vals = $1, split_prop_val.call($2)
+            next m unless vals.length >= 2 && vals.any? { |v| v =~ /^[\+\-]\$/ }
+            transformed = vals.map { |v| v.strip =~ %r(^\(.*\)$) ? v : '(#{v})' }
+            log_transform 'property #{prop}: #{transformed * ' '}', from: 'wrap_calculation'
+            '#{prop}: #{transformed * ' '};'
           end
         end
-        @header
+      end
+    end
+    
+        def puts(*args)
+      STDERR.puts *args unless @silence
+    end
+    
+        def open_file(filename, flag = 'r')
+      return if filename.nil?
+      flag = 'wb' if @options[:unix_newlines] && flag == 'w'
+      file = File.open(filename, flag)
+      return file unless block_given?
+      yield file
+      file.close
+    end
+    
+          # Returns the time the given Sass file was last modified.
+      #
+      # If the given file has been deleted or the time can't be accessed
+      # for some other reason, this should return nil.
+      #
+      # @param uri [String] The URI of the file to check.
+      #   Comes from a `:filename` option set on an engine returned by this importer.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` currently being checked.
+      # @return [Time, nil]
+      def mtime(uri, options)
+        Sass::Util.abstract(self)
       end
     
-    # Make sure we're in the test dir, the tests expect that to be the current
-# directory.
-TEST_DIR  = File.join(File.dirname(__FILE__), *%w[.])
+          # @see Base#find
+      def find(name, options)
+        _find(@root, name, options)
+      end
     
-        # Test page_header_from_content(@content)
-    actual = @view.title
-    assert_equal '1 & 2', actual
-  end
+          def unprocessable_entity(message)
+        render plain: { exception: message }.to_json, status: 422
+      end
+    
+              inventory_unit.transaction do
+            if inventory_unit.update_attributes(inventory_unit_params)
+              fire
+              render :show, status: 200
+            else
+              invalid_resource!(inventory_unit)
+            end
+          end
+        end
+    
+            def find_payment
+          @payment = @order.payments.find_by!(number: params[:id])
+        end
+    
+            def show
+          respond_with(stock_location)
+        end
