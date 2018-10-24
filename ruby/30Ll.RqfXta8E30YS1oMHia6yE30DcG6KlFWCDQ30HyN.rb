9de2931f6bda9e3d40b9f37d83cda0f8d64f5206
SUNsung <weatherius@gmail.com>
2018-10-24 03:02:04 +0300

    Rewrite [30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb] File in: 2018-10-24 03:02:03

--- ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
+++ ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
@@ -1,59 +1,77 @@
 
         
-        invalids = []
-Parallel.each(links, in_threads: 4) do |link|
-  href = link.attribute('href').to_s
-  begin
-    case check_link(URI.join(BASE_URI, href))
-    when (200...300)
-      putc('.')
-    when (300..302)
-      putc('w')
-    end
-  rescue => e
-    putc('F')
-    invalids << '#{href} (reason: #{e.message})'
-  end
+        # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+
+    
+    def local_require
+  require 'json'
+  JSON.pretty_generate(DATA)
 end
     
-        # Check if a reset_password_token is provided in the request
-    def assert_reset_token_passed
-      if params[:reset_password_token].blank?
-        set_flash_message(:alert, :no_token)
-        redirect_to new_session_path(resource_name)
+      s.authors       = ['Tom Preston-Werner']
+  s.email         = 'tom@mojombo.com'
+  s.homepage      = 'https://github.com/jekyll/jekyll'
+    
+          def remember_me_is_active?(resource)
+        return false unless resource.respond_to?(:remember_me)
+        scope = Devise::Mapping.find_scope!(resource)
+        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
+        resource.remember_me?(token, generated_at)
       end
-    end
     
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
+                bypass_sign_in(user)
+          DEPRECATION
+          warden.session_serializer.store(resource, scope)
+        elsif warden.user(scope) == resource && !options.delete(:force)
+          # Do nothing. User already signed in and we are not forcing it.
+          true
+        else
+          warden.set_user(resource, options.merge!(scope: scope))
+        end
+      end
     
-    Devise.setup do |config|
-  require 'devise/orm/active_record'
-  config.secret_key = 'secret_key_base'
-end
+          # Forcing colored to be included on String before Term::ANSIColor, so that Inch will work correctly.
+      require 'colored2'
+      ENV['GENERATE_COVERAGE'] = 'true'
+      puts '\033[0;32mUsing #{`ruby --version`}\033[0m'
     
-            routes.each do |module_name, actions|
-          [:path, :url].each do |path_or_url|
-            actions.each do |action|
-              action = action ? '#{action}_' : ''
-              method = :'#{action}#{module_name}_#{path_or_url}'
-    
-      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
-     options[:store] != false && !env['devise.skip_timeoutable']
-    last_request_at = warden.session(scope)['last_request_at']
-    
-        export LANG=en_US.UTF-8
-    DOC
-  else
+      if ARGV.include? '--no-ansi'
     STDERR.puts <<-DOC
-    \e[33mWARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
+    WARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
     Consider adding the following to ~/.profile:
     
-          def resolve
-        return if scm_name.nil?
-        set(:scm, :git) if using_default_scm?
+        %w[iOS macOS].each do |platform|
+        abstract_target '#{platform} Pods' do
+            project '#{platform} Modules.xcodeproj'
+    
+            # Prints the list of specs & pod cache dirs for a single pod name.
+        #
+        # This output is valid YAML so it can be parsed with 3rd party tools
+        #
+        # @param [Array<Hash>] cache_descriptors
+        #        The various infos about a pod cache. Keys are
+        #        :spec_file, :version, :release and :slug
+        #
+        def print_pod_cache_infos(pod_name, cache_descriptors)
+          UI.puts '#{pod_name}:'
+          cache_descriptors.each do |desc|
+            if @short_output
+              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
+            end
+            UI.puts('  - Version: #{desc[:version]}')
+            UI.puts('    Type:    #{pod_type(desc)}')
+            UI.puts('    Spec:    #{desc[:spec_file]}')
+            UI.puts('    Pod:     #{desc[:slug]}')
+          end
+        end
+      end
+    end
+  end
+end
