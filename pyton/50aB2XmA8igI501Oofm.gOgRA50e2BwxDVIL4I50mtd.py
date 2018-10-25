11f12119a2809ac845bde10c3d478cd81f7f1044
SUNsung <weatherius@gmail.com>
2018-10-25 20:02:27 +0300

    Rewrite [50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py] File in: 2018-10-25 20:02:26

--- pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
+++ pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
@@ -1,154 +1,243 @@
 
         
-            Checks that the id exists and optionally that the current user is
-    the author.
-    
-        if current_app.config['JSONIFY_PRETTYPRINT_REGULAR'] or current_app.debug:
-        indent = 2
-        separators = (', ', ': ')
-    
-            :param tag_class: tag class to register. Will be instantiated with this
-            serializer instance.
-        :param force: overwrite an existing tag. If false (default), a
-            :exc:`KeyError` is raised.
-        :param index: index to insert the new tag in the tag order. Useful when
-            the new tag is a special case of an existing tag. If ``None``
-            (default), the tag is appended to the end of the order.
-    
-        Implements signals based on blinker if available, otherwise
-    falls silently back to a noop.
-    
-            If the ``obj`` argument is not given, passes an instance of
-        :class:`~flask.cli.ScriptInfo` that knows how to load the Flask
-        app being tested.
-    
-    KEY_PREFIX = 'django.contrib.sessions.cached_db'
-    
-        def test_splitdrive(self):
-        # splitdrive for non-NT paths
-        splitdrive = self.pathmodule.splitdrive
-        self.assertEqual(splitdrive('/foo/bar'), ('', '/foo/bar'))
-        self.assertEqual(splitdrive('foo:bar'), ('', 'foo:bar'))
-        self.assertEqual(splitdrive(':foo:bar'), ('', ':foo:bar'))
-    
-        If a name, the module is imported.  If the passed or imported module
-    object is not a package, raise an exception.
+            Returns:
+      The likelihood of z under the model.
     '''
-    if hasattr(package, '__spec__'):
-        if package.__spec__.submodule_search_locations is None:
-            raise TypeError('{!r} is not a package'.format(
-                package.__spec__.name))
-        else:
-            return package
+    if z is None:
+      z = self.sample
+    
+    The main data structure being passed around is a dataset.  This is a dictionary
+of data dictionaries.
+    
+      W,b = init_linear(in_size, out_size, do_bias=do_bias, alpha=alpha,
+                    normalized=normalized, name=name, collections=collections)
+    
+      softmax_weights = sess.run(t['softmax_weights'])
+  fname = FLAGS.save_dir + '/embeddings_softmax.npy'
+  with tf.gfile.Open(fname, mode='w') as f:
+    np.save(f, softmax_weights)
+  sys.stderr.write('Finished softmax weights\n')
+    
+        ## Calculate the Advantages, A(s,a) = Q(s,a) - \hat{V}(s).
+    advantages = []
+    for t in xrange(FLAGS.sequence_length):
+      log_probability = log_probs_list[t]
+      cum_advantage = tf.zeros(shape=[FLAGS.batch_size])
+    
+          elif FLAGS.generator_model == 'seq2seq_nas':
+        # Encoder.
+        gen_encoder_variable_maps = variable_mapping.gen_encoder_seq2seq_nas(
+            hparams)
+        gen_encoder_init_saver = tf.train.Saver(
+            var_list=gen_encoder_variable_maps)
+        # Decoder.
+        gen_decoder_variable_maps = variable_mapping.gen_decoder_seq2seq_nas(
+            hparams)
+        gen_decoder_init_saver = tf.train.Saver(
+            var_list=gen_decoder_variable_maps)
+        init_savers['gen_encoder_init_saver'] = gen_encoder_init_saver
+        init_savers['gen_decoder_init_saver'] = gen_decoder_init_saver
+    
+        auto_json = args.data and not args.form
+    if args.json or auto_json:
+        default_headers['Accept'] = JSON_ACCEPT
+        if args.json or (auto_json and args.data):
+            default_headers['Content-Type'] = JSON_CONTENT_TYPE
+    
+        # Try to resolve the right lexer.
+    lexer = None
+    for mime_type in mime_types:
+        try:
+            lexer = pygments.lexers.get_lexer_for_mimetype(mime_type)
+            break
+        except ClassNotFound:
+            pass
     else:
-        module = import_module(package)
-        if module.__spec__.submodule_search_locations is None:
-            raise TypeError('{!r} is not a package'.format(package))
-        else:
-            return module
-    
-        @bigmemtest(size=_4G + 100, memuse=2)
-    def testCompress4G(self, size):
-        # 'Test BZ2Compressor.compress()/flush() with >4GiB input'
-        bz2c = BZ2Compressor()
-        data = b'x' * size
-        try:
-            compressed = bz2c.compress(data)
-            compressed += bz2c.flush()
-        finally:
-            data = None  # Release memory
-        data = bz2.decompress(compressed)
-        try:
-            self.assertEqual(len(data), size)
-            self.assertEqual(len(data.strip(b'x')), 0)
-        finally:
-            data = None
-    
-        # Initialize and populate our database.
-    conn = sqlite3.connect(':memory:')
-    cursor = conn.cursor()
-    cursor.execute('CREATE TABLE memos(key INTEGER PRIMARY KEY, task TEXT)')
-    tasks = (
-        'give food to fish',
-        'prepare group meeting',
-        'fight with a zebra',
-        )
-    for task in tasks:
-        cursor.execute('INSERT INTO memos VALUES(NULL, ?)', (task,))
-    
-    def handleSlideTitle(title):
-    print('<h2>%s</h2>' % getText(title.childNodes))
-    
-    # register the Foo class; make `f()` and `g()` accessible via proxy
-MyManager.register('Foo1', Foo)
-    
-            it = pool.imap(f, list(range(10)))
-        for i in range(10):
+        for name in lexer_names:
             try:
-                x = next(it)
-            except ZeroDivisionError:
-                if i == 5:
-                    pass
-            except StopIteration:
-                break
-            else:
-                if i == 5:
-                    raise AssertionError('expected ZeroDivisionError')
+                lexer = pygments.lexers.get_lexer_by_name(name)
+            except ClassNotFound:
+                pass
     
-        # Predict classes and remove classifications that aren't within the threshold
-    return [(pred, loc) if rec else ('unknown', loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
-    
-        # Load the uploaded image file
-    img = face_recognition.load_image_file(file_stream)
-    # Get face encodings for any faces in the uploaded image
-    unknown_face_encodings = face_recognition.face_encodings(img)
-    
-        :param faces: List of face encodings to compare
-    :param face_to_compare: A face encoding to compare against
-    :return: A numpy ndarray with the distance for each face in the same order as the 'faces' array
-    '''
-    if len(face_encodings) == 0:
-        return np.empty((0))
-    
-        fftv.publish('cartoon')
-    fftv.publish('music')
-    fftv.publish('ads')
-    fftv.publish('movie')
-    fftv.publish('cartoon')
-    fftv.publish('cartoon')
-    fftv.publish('movie')
-    fftv.publish('blank')
-    
-    
-a = A()
-b = B()
-c = C()
-visitor = Visitor()
-visitor.visit(a)
-visitor.visit(b)
-visitor.visit(c)
-    
-    
-# In some very complex cases, it might be desirable to pull out the building
-# logic into another function (or a method on another class), rather than being
-# in the base class '__init__'. (This leaves you in the strange situation where
-# a concrete class does not have a useful constructor)
-    
-    
-def main():
+    # sdist
+if 'bdist_wheel' not in sys.argv:
     try:
-        import queue
-    except ImportError:  # python 2.x compatibility
-        import Queue as queue
+        # noinspection PyUnresolvedReferences
+        import argparse
+    except ImportError:
+        install_requires.append('argparse>=1.2.1')
     
+        config['implicit_content_type'] = 'form'
+    config.save()
+    config.load()
+    assert 'implicit_content_type' not in config
+    assert config['default_options'] == ['--form']
+    
+    
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
+    
+        @pytest.mark.parametrize(
+        argnames=['mime', 'explicit_json', 'body', 'expected_lexer_name'],
+        argvalues=[
+            ('application/json',     False, None, 'JSON'),
+            ('application/json+foo', False, None, 'JSON'),
+            ('application/foo+json', False, None, 'JSON'),
+            ('application/json-foo', False, None, 'JSON'),
+            ('application/x-json',   False, None, 'JSON'),
+            ('foo/json',             False, None, 'JSON'),
+            ('foo/json+bar',         False, None, 'JSON'),
+            ('foo/bar+json',         False, None, 'JSON'),
+            ('foo/json-foo',         False, None, 'JSON'),
+            ('foo/x-json',           False, None, 'JSON'),
+            ('application/vnd.comverge.grid+hal+json', False, None, 'JSON'),
+            ('text/plain',           True, '{}', 'JSON'),
+            ('text/plain',           True, 'foo', 'Text only'),
+        ]
+    )
+    def test_get_lexer(self, mime, explicit_json, body, expected_lexer_name):
+        lexer = get_lexer(mime, body=body, explicit_json=explicit_json)
+        assert lexer is not None
+        assert lexer.name == expected_lexer_name
+    
+    ESTIMATORS = {
+    'dummy': DummyClassifier(),
+    'random_forest': RandomForestClassifier(n_estimators=100,
+                                            max_features='sqrt',
+                                            min_samples_split=10),
+    'extra_trees': ExtraTreesClassifier(n_estimators=100,
+                                        max_features='sqrt',
+                                        min_samples_split=10),
+    'logistic_regression': LogisticRegression(),
+    'naive_bayes': MultinomialNB(),
+    'adaboost': AdaBoostClassifier(n_estimators=10),
+}
+    
+    
+def plot_batch_errors(all_errors, n_features, all_batch_sizes, data):
+    plt.figure()
+    plot_results(all_batch_sizes, all_errors['pca'], label='PCA')
+    plot_results(all_batch_sizes, all_errors['ipca'], label='IncrementalPCA')
+    plt.legend(loc='lower left')
+    plt.suptitle('Algorithm error vs. batch_size for n_components %i\n \
+                 LFW, size %i x %i' % (
+                 n_features, data.shape[0], data.shape[1]))
+    plt.xlabel('Batch size')
+    plt.ylabel('Mean absolute error')
+    
+        for n_samples in sample_sizes:
+        X = random_state.rand(n_samples, 300)
+    
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
+
+    
+    n_samples, n_features = 5000, 300
+X = np.random.randn(n_samples, n_features)
+inds = np.arange(n_samples)
+np.random.shuffle(inds)
+X[inds[int(n_features / 1.2):]] = 0  # sparsify input
+print('input data sparsity: %f' % sparsity_ratio(X))
+coef = 3 * np.random.randn(n_features)
+inds = np.arange(n_features)
+np.random.shuffle(inds)
+coef[inds[n_features // 2:]] = 0  # sparsify coef
+print('true coef sparsity: %f' % sparsity_ratio(coef))
+y = np.dot(X, coef)
+    
+        An example with a long-untouched module that everyone has
+    >>> _linkcode_resolve('py', {'module': 'tty',
+    ...                          'fullname': 'setraw'},
+    ...                   package='tty',
+    ...                   url_fmt='http://hg.python.org/cpython/file/'
+    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
+    ...                   revision='xxxx')
+    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
     '''
-Port of the Java example of 'Parameter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
     
-        def contribute(self):
-        self.blackboard.common_state['problems'] += random.randint(1, 2)
-        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
-        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
-        self.blackboard.common_state['progress'] += random.randint(10, 100)
+    URL = ('http://people.csail.mit.edu/jrennie/'
+       '20Newsgroups/20news-bydate.tar.gz')
+    
+        def test_stop_iteration(self):
+        def f():
+            for i in range(2):
+                yield i
+        def g(p):
+            for i in f():
+                pass
+        f_ident = ident(f)
+        g_ident = ident(g)
+        self.check_events(g, [(1, 'call', g_ident),
+                              # call the iterator twice to generate values
+                              (2, 'call', f_ident),
+                              (2, 'return', f_ident),
+                              (2, 'call', f_ident),
+                              (2, 'return', f_ident),
+                              # once more to hit the raise:
+                              (2, 'call', f_ident),
+                              (2, 'return', f_ident),
+                              (1, 'return', g_ident),
+                              ])
+    
+        def __hash__(self):
+        try:
+            return self._hash
+        except AttributeError:
+            self._hash = hash(tuple(self._cparts))
+            return self._hash
+    
+    # Join two paths, normalizing and eliminating any symbolic links
+# encountered in the second path.
+def _joinrealpath(path, rest, seen):
+    if isinstance(path, bytes):
+        sep = b'/'
+        curdir = b'.'
+        pardir = b'..'
+    else:
+        sep = '/'
+        curdir = '.'
+        pardir = '..'
+    
+        def test_normpath_issue5827(self):
+        # Make sure normpath preserves unicode
+        for path in ('', '.', '/', '\\', '///foo/.//bar//'):
+            self.assertIsInstance(self.pathmodule.normpath(path), str)
+    
+    class EncodingDetails(_EncodingDetails):
+    # XXX (ncoghlan): Using JSON for child state reporting may be less fragile
+    CHILD_PROCESS_SCRIPT = ';'.join([
+        'import sys, os',
+        'print(sys.getfilesystemencoding())',
+        'print(sys.stdin.encoding + ':' + sys.stdin.errors)',
+        'print(sys.stdout.encoding + ':' + sys.stdout.errors)',
+        'print(sys.stderr.encoding + ':' + sys.stderr.errors)',
+        'print(os.environ.get('LANG', 'not set'))',
+        'print(os.environ.get('LC_CTYPE', 'not set'))',
+        'print(os.environ.get('LC_ALL', 'not set'))',
+    ])
+    
+            rc, out, err = assert_python_ok('-c', code, PYTHONHASHSEED='0')
+        self.assertIn(b'random is 0', out)
+    
+        def check_fsencoding(self, fs_encoding, expected=None):
+        self.assertIsNotNone(fs_encoding)
+        codecs.lookup(fs_encoding)
+        if expected:
+            self.assertEqual(fs_encoding, expected)
+    
+        print('Unpickled records:')
+    pprint.pprint(memos)
+    
+    def noop(x):
+    pass
