bc97fcafb817a50601d658bec3f44aa502f0350e
SUNsung <weatherius@gmail.com>
2018-10-25 08:52:00 +0300

    Rewrite [53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py] File in: 2018-10-25 08:51:59

--- pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
+++ pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
@@ -1,70 +1,39 @@
 
         
-                (2016-01, url0), 2
-        (2016-01, url1), 1
-        '''
-        yield key, sum(values)
+            def __init__(self, delegate):
+        self.delegate = delegate
     
-        def mapper_sort(self, key, value):
-        '''Construct key to ensure proper sorting.
+    '''
+*TL;DR80
+Encapsulates all information needed to perform an action or trigger an event.
+'''
     
+    print()
     
-if __name__ == '__main__':
-    RemoveDuplicateUrls.run()
-
+    *TL;DR80
+Separates an algorithm from an object structure on which it operates.
+'''
     
-        def get_people(self, ids):
-        results = []
-        for id in ids:
-            if id in self.people:
-                results.append(self.people[id])
-        return results
+    '''
+Port of the Java example of 'Parameter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
     
-        # L = -KL + log p(x|z), to maximize bound on likelihood
-    # -L = KL - log p(x|z), to minimize bound on NLL
-    # so 'KL cost' is postive KL divergence
-    z0_bxu = post_zs[0].sample
-    logq_bxu = post_zs[0].logp(z0_bxu)
-    logp_bxu = prior_z_process.logp_t(z0_bxu)
-    z_tm1_bxu = z0_bxu
-    for z_t in post_zs[1:]:
-      # posterior is independent in time, prior is not
-      z_t_bxu = z_t.sample
-      logq_bxu += z_t.logp(z_t_bxu)
-      logp_bxu += prior_z_process.logp_t(z_t_bxu, z_tm1_bxu)
-      z_tm1 = z_t_bxu
+        poll_again = _HandlePollResponse( response, diagnostics_handler )
+    if poll_again:
+      self._SendRequest()
+      return True
     
-            cur_pos = next_pos
-        cur_stream[i][0] = cur_stream[i][0][how_many:]
-        cur_stream[i][1] = cur_stream[i][1][how_many:]
-        cur_stream[i][2] = cur_stream[i][2][how_many:]
+    TIMEOUT_SECONDS = 0.1
     
-          else:
-        to_fill_in = num_steps - len(example)
-        final_x = example + [EOS_INDEX] * to_fill_in
-        final_y = final_x[1:] + [EOS_INDEX]
-        w[i] = [1] * len(example) + [0] * to_fill_in
-    
-        elif (FLAGS.discriminator_model == 'bidirectional_zaremba' or
-          FLAGS.discriminator_model == 'bidirectional_vd'):
-      assert FLAGS.language_model_ckpt_dir_reversed is not None, (
-          'Need a reversed directory to fill in the backward components.')
-      load_fwd_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-      load_bwd_ckpt = tf.train.latest_checkpoint(
-          FLAGS.language_model_ckpt_dir_reversed)
-      print('Restoring Discriminator from %s and %s.' % (load_fwd_ckpt,
-                                                         load_bwd_ckpt))
-      tf.logging.info('Restoring Discriminator from %s and %s.' %
-                      (load_fwd_ckpt, load_bwd_ckpt))
-      dis_fwd_init_saver = init_savers['dis_fwd_init_saver']
-      dis_bwd_init_saver = init_savers['dis_bwd_init_saver']
-      dis_fwd_init_saver.restore(sess, load_fwd_ckpt)
-      dis_bwd_init_saver.restore(sess, load_bwd_ckpt)
-    
-      Returns:
-    attention_score_fn: to compute similarity between key and target states.
-  '''
-  with tf.variable_scope(name, reuse=reuse):
-    if attention_option == 'bahdanau':
-      query_w = tf.get_variable('attnW', [num_units, num_units], dtype=dtype)
-      score_v = tf.get_variable('attnV', [num_units], dtype=dtype)
+      # As a last resort, we search python in the PATH. We prefer Python 2 over 3
+  # for the sake of backwards compatibility with ycm_extra_conf.py files out
+  # there; few people wrote theirs to work on py3.
+  # So we check 'python2' before 'python' because on some distributions (Arch
+  # Linux for example), python refers to python3.
+  python_interpreter = utils.PathToFirstExistingExecutable( [ 'python2',
+                                                              'python',
+                                                              'python3' ] )
+  if python_interpreter:
+    return python_interpreter
