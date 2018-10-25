8f8206745d04cf0091ec1b17f3a4220f2da52491
SUNsung <weatherius@gmail.com>
2018-10-26 00:07:35 +0300

    Rewrite [01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp] File in: 2018-10-26 00:07:34

--- cccccc/01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp
+++ cccccc/01mVk.4MhfM5Y01SnBvBGt078A01h3AO3stKdjM01pp8.cpp
@@ -1,461 +1,102 @@
 
         
-          scoped_ptr<base::Value> value_args(
-      converter->FromV8Value(args, isolate->GetCurrentContext()));
-  if (!value_args.get() ||
-      !value_args->IsType(base::Value::TYPE_LIST))
-    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-        'Unable to convert 'args' passed to CallObjectMethodSync')));
-    
-    // Call method of an object in browser and return the result.
-// function CallObjectMethod(id, type, method, args);
-v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
-                                           int object_id,
-                                           const std::string& type,
-                                           const std::string& method,
-                                           v8::Handle<v8::Value> args);
-    
-    
-    {}
-    
-    namespace nwapi {
+        namespace swift {
     }
     
-    #include 'base/values.h'
-#include 'components/zoom/zoom_controller.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menuitem/menuitem.h'
-#include 'content/public/browser/web_contents.h'
-#include 'content/public/common/page_zoom.h'
-#include 'ui/views/controls/menu/menu_runner.h'
+      char *oldBegin = Begin;
+  char *oldEnd = End;
+  std::size_t oldSize = (std::size_t) (oldEnd - oldBegin);
     
-    namespace ui {
+      // Update the argument label and base name.
+  argName = baseName.substr(startOfArgumentLabel);
+  baseName = newBaseName;
+    
+    
+    {    if (Finished) {
+      std::string Output = 'Output placeholder\n';
+      std::string Errors =
+          P.second->SeparateErrors ? 'Error placeholder\n' : '';
+      if (Finished(P.first, 0, Output, Errors, TaskProcessInformation(Pid),
+                   P.second->Context) == TaskFinishedResponse::StopExecution)
+        SubtaskFailed = true;
     }
-    
-    void MenuItem::Call(const std::string& method,
-                    const base::ListValue& arguments,
-                    content::RenderFrameHost* rvh) {
-  if (method == 'SetLabel') {
-    std::string label;
-    arguments.GetString(0, &label);
-    SetLabel(label);
-  } else if (method == 'SetIcon') {
-    std::string icon;
-    arguments.GetString(0, &icon);
-    SetIcon(icon);
-  } else if (method == 'SetIconIsTemplate') {
-    bool isTemplate;
-    arguments.GetBoolean(0, &isTemplate);
-    SetIconIsTemplate(isTemplate);
-  } else if (method == 'SetTooltip') {
-    std::string tooltip;
-    arguments.GetString(0, &tooltip);
-    SetTooltip(tooltip);
-  } else if (method == 'SetEnabled') {
-    bool enabled = true;
-    arguments.GetBoolean(0, &enabled);
-    SetEnabled(enabled);
-  } else if (method == 'SetChecked') {
-    bool checked = false;
-    arguments.GetBoolean(0, &checked);
-    SetChecked(checked);
-  } else if (method == 'SetSubmenu') {
-    int object_id = 0;
-    arguments.GetInteger(0, &object_id);
-    SetSubmenu(object_manager()->GetApiObject<Menu>(object_id));
-#if defined(OS_MACOSX)
-  } else if (method == 'SetKey') {
-    std::string key;
-    arguments.GetString(0, &key);
-    SetKey(key);
-  } else if (method == 'SetModifiers') {
-    std::string mod;
-    arguments.GetString(0, &mod);
-    SetModifiers(mod);
-#endif
-  } else {
-    NOTREACHED() << 'Invalid call to MenuItem method:' << method
-                 << ' arguments:' << arguments;
-  }
-}
-    
-    
-    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.clearSync', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwClipboardClearSyncFunction);
-};
-    
-    TegraCvtColor_Invoker(gray2rgb, gray2rgb, src_data + static_cast<size_t>(range.start) * src_step, src_step, \
-                                          dst_data + static_cast<size_t>(range.start) * dst_step, dst_step)
-TegraCvtColor_Invoker(gray2rgbx, gray2rgbx, src_data + static_cast<size_t>(range.start) * src_step, src_step, \
-                                            dst_data + static_cast<size_t>(range.start) * dst_step, dst_step)
-#define TEGRA_CVTGRAYTOBGR(src_data, src_step, dst_data, dst_step, width, height, depth, dcn) \
-( \
-    depth == CV_8U && CAROTENE_NS::isSupportedConfiguration() ? \
-        dcn == 3 ? \
-            parallel_for_(Range(0, height), \
-            TegraCvtColor_gray2rgb_Invoker(src_data, src_step, dst_data, dst_step, width, height), \
-            (width * height) / static_cast<double>(1<<16)), \
-            CV_HAL_ERROR_OK : \
-        dcn == 4 ? \
-            parallel_for_(Range(0, height), \
-            TegraCvtColor_gray2rgbx_Invoker(src_data, src_step, dst_data, dst_step, width, height), \
-            (width * height) / static_cast<double>(1<<16)), \
-            CV_HAL_ERROR_OK : \
-        CV_HAL_ERROR_NOT_IMPLEMENTED \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
-    
-        void operator() (const typename internal::VecTraits<T>::vec128 & v_src0,
-                     const typename internal::VecTraits<T>::vec128 & v_src1,
-                     typename internal::VecTraits<T>::vec128 & v_dst) const
-    {
-        v_dst = internal::vaddq(v_src0, v_src1);
-    }
-    
-    template <typename T> struct wAdd
-{
-    typedef T type;
-    }
-    
-        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0, const typename internal::VecTraits<T>::vec64 & v_src1,
-              typename internal::VecTraits<T>::unsign::vec64 & v_dst) const
-    {
-        v_dst = internal::vcge(v_src0, v_src1);
-    }
-    
-    
-    {                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t0_16s), kernelBase[8]);
-                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t1_16s), kernelBase[7]);
-                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t2_16s), kernelBase[6]);
-            }
-    
-    
-    {
-    {
-    {            // make shift
-            prevx = currx;
-            currx = nextx;
-        }
-    }
-#else
-    (void)srcBase;
-    (void)srcStride;
-    (void)dstBase;
-    (void)dstStride;
-    (void)borderValue;
-#endif
-}
-    
-    /////////////// Custom NEON intrinsics ///////////////////
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-      virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    {}  // namespace internal
-    
-    // This macro is for implementing ASSERT/EXPECT_DEBUG_DEATH when compiled in
-// NDEBUG mode. In this case we need the statements to be executed, the regex is
-// ignored, and the macro must accept a streamed message even though the message
-// is never printed.
-# define GTEST_EXECUTE_STATEMENT_(statement, regex) \
-  GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
-  if (::testing::internal::AlwaysTrue()) { \
-     GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
-  } else \
-    ::testing::Message()
-    
-    #if GTEST_HAS_EXCEPTIONS
-# include <stdexcept>
-#endif
-    
-      // Compares two wide C strings.  Returns true iff they have the same
-  // content.
-  //
-  // Unlike wcscmp(), this function can handle NULL argument(s).  A
-  // NULL C string is considered different to any non-NULL C string,
-  // including the empty string.
-  static bool WideCStringEquals(const wchar_t* lhs, const wchar_t* rhs);
-    
-    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
-    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
-    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
-    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
-    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
-    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17, GTEST_TEMPLATE_ T18,
-    GTEST_TEMPLATE_ T19, GTEST_TEMPLATE_ T20, GTEST_TEMPLATE_ T21,
-    GTEST_TEMPLATE_ T22, GTEST_TEMPLATE_ T23, GTEST_TEMPLATE_ T24,
-    GTEST_TEMPLATE_ T25, GTEST_TEMPLATE_ T26, GTEST_TEMPLATE_ T27,
-    GTEST_TEMPLATE_ T28, GTEST_TEMPLATE_ T29, GTEST_TEMPLATE_ T30,
-    GTEST_TEMPLATE_ T31, GTEST_TEMPLATE_ T32, GTEST_TEMPLATE_ T33,
-    GTEST_TEMPLATE_ T34, GTEST_TEMPLATE_ T35, GTEST_TEMPLATE_ T36,
-    GTEST_TEMPLATE_ T37, GTEST_TEMPLATE_ T38, GTEST_TEMPLATE_ T39,
-    GTEST_TEMPLATE_ T40, GTEST_TEMPLATE_ T41, GTEST_TEMPLATE_ T42,
-    GTEST_TEMPLATE_ T43, GTEST_TEMPLATE_ T44>
-struct Templates44 {
-  typedef TemplateSel<T1> Head;
-  typedef Templates43<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
-      T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-      T29, T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42,
-      T43, T44> Tail;
-};
-    
-    template <typename T>
-struct AssertTypeEq<T, T> {
-  typedef bool type;
-};
-    
-      // Called after a test ends.
-  virtual void OnTestEnd(const TestInfo& /* test_info */) {
-    int difference = Water::allocated() - initially_allocated_;
-    }
-    
-      // Constructs a MyString by cloning a 0-terminated C string.
-  explicit MyString(const char* a_c_string) : c_string_(NULL) {
-    Set(a_c_string);
   }
     
-            // TODO: make these functions friends of NDViewArray and move to Utils?
-        static bool HasNan(const NDArrayViewPtr& value, const char* name);
-        static void Print(const NDArrayViewPtr& value, const char* msg);
     
-        template <typename T> 
-    inline std::string GetVersionsString(size_t currentVersion, size_t dictVersion)
-    {
-        std::stringstream info;
-        info << 'Current ' << Typename<T>() << ' version = ' << currentVersion 
-             << ', Dictionary version = ' << dictVersion;
-        return info.str();
-    }
-    
-            void Erase() override
-        {
-            if (IsPacked())
-            {
-                m_packedData = nullptr;
-                m_packedDataLayout = nullptr;
-                m_isPacked = false;
-            }
-            else
-                Value::Erase();
-        }
-    
-    // base class that we can catch, independent of the type parameter
-struct /*interface*/ IExceptionWithCallStackBase
-{
-    virtual const char * CallStack() const = 0;
-    virtual ~IExceptionWithCallStackBase() noexcept = default;
-};
-    
-    class Clock
-{
-public:
-    static long long GetTimeStamp();
-    static long long GetTicksPerSecond();
-};
-    
-    void Twirl::update(float time)
-{
-    int i, j;
-    Vec2    c = _position;
-    
-    for (i = 0; i < (_gridSize.width+1); ++i)
-    {
-        for (j = 0; j < (_gridSize.height+1); ++j)
-        {
-            Vec3 v = getOriginalVertex(Vec2(i ,j));
-            
-            Vec2 avg(i-(_gridSize.width/2.0f), j-(_gridSize.height/2.0f));
-            float r = avg.getLength();
-            
-            float amp = 0.1f * _amplitude * _amplitudeRate;
-            float a = r * cosf( (float)M_PI/2.0f + time * (float)M_PI * _twirls * 2 ) * amp;
-            
-            Vec2 d(
-                sinf(a) * (v.y-c.y) + cosf(a) * (v.x-c.x),
-                cosf(a) * (v.y-c.y) - sinf(a) * (v.x-c.x));
-            
-            v.x = c.x + d.x;
-            v.y = c.y + d.y;
-    }
-    }
-    }
-    
-        /** Sets the amplitude rate, extension in GridAction
-     *
-     * @param amp   The amplitude rate.
-     */
-    void setAmplitudeRate(float amp);
-    
-    /** Gets the amplitude rate, extension in GridAction
-     *
-     * @return  The amplitude rate.
-     */
-    float getAmplitudeRate();
+    {  // The length of the transcoded string in UTF-16 code points.
+  Length = toPtr - &buffer[0];
+  return Length;
+}
+
     
     
-    {        // only delete currentTarget if no actions were scheduled during the cycle (issue #481)
-        if (_currentTargetSalvaged && _currentTarget->actions->num == 0)
-        {
-            deleteHashElement(_currentTarget);
-        }
-        //if some node reference 'target', it's reference count >= 2 (issues #14050)
-        else if (_currentTarget->target->getReferenceCount() == 1)
-        {
-            deleteHashElement(_currentTarget);
-        }
-    }
-    
-    /**
-@brief Progress from a percentage to another percentage.
-@since v0.99.1
-*/
-class CC_DLL ProgressFromTo : public ActionInterval
-{
-public:
-    /** 
-     * @brief Create and initializes the action with a duration, a 'from' percentage and a 'to' percentage.
-     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
-     * @param fromPercentage Specify the source percentage.
-     * @param toPercentage Specify the destination percentage.
-     * @return If the creation success, return a pointer of ProgressFromTo action; otherwise, return nil.
-     */
-    static ProgressFromTo* create(float duration, float fromPercentage, float toPercentage);
-    }
-    
-                if ( ((i+j) % 2) == 0 )
-            {
-                coords.bl.z += sinz;
-                coords.br.z += sinz;
-                coords.tl.z += sinz;
-                coords.tr.z += sinz;
-            }
-            else
-            {
-                coords.bl.z += sinz2;
-                coords.br.z += sinz2;
-                coords.tl.z += sinz2;
-                coords.tr.z += sinz2;
-            }
-    
-    ActionTween* ActionTween::create(float duration, const std::string& key, float from, float to)
-{
-    ActionTween* ret = new (std::nothrow) ActionTween();
-    if (ret && ret->initWithDuration(duration, key, from, to))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    
-    delete ret;
-    return nullptr;
+    {  return GraphemeClusterBreakProperty::Other;
 }
     
-          // Start all threads
-      shared.SetStart();
-      shared.GetCondVar()->SignalAll();
+      /**
+   * Returns orientation for the block the iterator points to.
+   *   orientation, writing_direction, textline_order: see publictypes.h
+   *   deskew_angle: after rotating the block so the text orientation is
+   *                 upright, how many radians does one have to rotate the
+   *                 block anti-clockwise for it to be level?
+   *                   -Pi/4 <= deskew_angle <= Pi/4
+   */
+  void Orientation(tesseract::Orientation *orientation,
+                   tesseract::WritingDirection *writing_direction,
+                   tesseract::TextlineOrder *textline_order,
+                   float *deskew_angle) const;
     
-      Put('0000000010', 'v10');
-  Put('0000000020', 'v20'); // skipped
-  Put('0000000050', 'v50');
-  Flush();
+      // Find all editable parameters used within tesseract and create a
+  // SVMenuNode tree from it.
+  SVMenuNode *BuildListOfAllLeaves(tesseract::Tesseract *tess);
     
-    Status WriteBatchBase::Merge(ColumnFamilyHandle* column_family,
-                             const SliceParts& key, const SliceParts& value) {
-  std::string key_buf, value_buf;
-  Slice key_slice(key, &key_buf);
-  Slice value_slice(value, &value_buf);
-    }
-    
-      // Need to refill more than one interval. Need to sleep longer. Check
-  // whether expiration will hit
-    
-      std::atomic<int> total_stopped_;
-  std::atomic<int> total_delayed_;
-  std::atomic<int> total_compaction_pressure_;
-  uint64_t bytes_left_;
-  uint64_t last_refill_time_;
-  // write rate set when initialization or by `DBImpl::SetDBOptions`
-  uint64_t max_delayed_write_rate_;
-  // current write rate
-  uint64_t delayed_write_rate_;
-    
-    #include 'db/write_controller.h'
-    
-      virtual Status NewWritableFile(const std::string& fname,
-                                 unique_ptr<WritableFile>* result,
-                                 const EnvOptions& env_options) override;
-    
-    #include 'env/mock_env.h'
-    
-      // close the db.
-  delete db;
-    
-      bool FilterMergeOperand(int level, const rocksdb::Slice& key,
-                          const rocksdb::Slice& existing_value) const override {
-    fprintf(stderr, 'FilterMerge(%s)\n', key.ToString().c_str());
-    ++merge_count_;
-    return existing_value == 'bad';
-  }
-    
-      ////////////////////////////////////////////////////////
-  //
-  // 'Repeatable Read' (Snapshot Isolation) Example
-  //   -- Using a single Snapshot
-  //
-  ////////////////////////////////////////////////////////
-    
-    
-    {} // namespace folly
-    
-    /**
- * Debug string for an exception: include type and what(), if
- * defined.
+    /*!
+ * \brief Registry entry for linear updater.
  */
-inline fbstring exceptionStr(const std::exception& e) {
-#ifdef FOLLY_HAS_RTTI
-  fbstring rv(demangle(typeid(e)));
-  rv += ': ';
-#else
-  fbstring rv('Exception (no RTTI available): ');
-#endif
-  rv += e.what();
-  return rv;
-}
+struct LinearUpdaterReg
+    : public dmlc::FunctionRegEntryBase<LinearUpdaterReg,
+                                        std::function<LinearUpdater*()> > {};
     
-      tv.tv_sec = 1;
-  tv.tv_usec = 10;
-  EXPECT_EQ(1000010000ns, to<nanoseconds>(tv));
-  EXPECT_EQ(1000010us, to<microseconds>(tv));
-  EXPECT_EQ(1000ms, to<milliseconds>(tv));
-  EXPECT_EQ(1s, to<seconds>(tv));
-  EXPECT_EQ(
-      createTimePoint<system_clock>(1000010000ns),
-      to<system_clock::time_point>(tv));
-  EXPECT_EQ(
-      createTimePoint<steady_clock>(1000010000ns),
-      to<steady_clock::time_point>(tv));
+    #include <dmlc/registry.h>
+#include <functional>
+#include <vector>
+#include <utility>
+#include <string>
+#include './base.h'
+#include './data.h'
+#include './tree_model.h'
+#include '../../src/common/host_device_vector.h'
     
-    #include <type_traits>
+        const size_t* begin = dmlc::BeginPtr(row_indices_);
+    const size_t* end = dmlc::BeginPtr(row_indices_) + row_indices_.size();
+    elem_of_each_node_.emplace_back(Elem(begin, end, 0));
+  }
+  // split rowset into two
+  inline void AddSplit(unsigned node_id,
+                       const std::vector<Split>& row_split_tloc,
+                       unsigned left_node_id,
+                       unsigned right_node_id) {
+    const Elem e = elem_of_each_node_[node_id];
+    const auto nthread = static_cast<bst_omp_uint>(row_split_tloc.size());
+    CHECK(e.begin != nullptr);
+    size_t* all_begin = dmlc::BeginPtr(row_indices_);
+    size_t* begin = all_begin + (e.begin - all_begin);
     
-      template <class, size_t>
-  friend class CoreCachedWeakPtr;
+    #if DMLC_ENABLE_STD_THREAD
+namespace xgboost {
+namespace data {
+    }
+    }
+    
+    #endif  // XGBOOST_OBJECTIVE_REGRESSION_LOSS_H_
+
+    
+    
+    {
+    {XGBOOST_REGISTER_TREE_UPDATER(TreePruner, 'prune')
+.describe('Pruner that prune the tree according to statistics.')
+.set_body([]() {
+    return new TreePruner();
+  });
+}  // namespace tree
+}  // namespace xgboost
