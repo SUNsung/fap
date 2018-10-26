8a1ae2db8c9369c9f4f81e8a6a0a37f9155af93a
SUNsung <weatherius@gmail.com>
2018-10-26 18:49:11 +0300

    Rewrite [179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py] File in: 2018-10-26 18:49:10

--- pyton/179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py
+++ pyton/179vSmi2tdSPs17Ed5AjURpkRg17Ko0ZHklS5Ps1774q.py
@@ -1,208 +1,73 @@
 
         
-        
-def _change_tracker(usage_tracker_io, pid):
-    usage_tracker_io.truncate(0)
-    info = {'pid': pid, 'time': 0}
-    json.dump(info, usage_tracker_io)
-    usage_tracker_io.seek(0)
+                flash(error)
     
+    import click
+from flask import current_app, g
+from flask.cli import with_appcontext
     
-@pytest.mark.functional
-def test_without_confirmation(proc, TIMEOUT):
-    without_confirmation(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'echo test')
+        yield app
     
-    
-@pytest.mark.parametrize('command, packages', [
-    (Command('vim', 'vim: command not found'),
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('sudo vim', 'vim: command not found'),
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
-     [('vim', 'main'), ('vim-tiny', 'main')])])
-def test_match(mocker, command, packages):
-    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
-    mocker.patch('thefuck.rules.apt_get._get_packages',
-                 create=True, return_value=packages)
-    
-      aws help
-  aws <command> help
-  aws <command> <subcommand> help
-aws: error: argument command: Invalid choice, valid choices are:
-    
-    
-    {        Generic:                   '#000000',        # class: 'g'
-        Generic.Deleted:           '#a40000',        # class: 'gd'
-        Generic.Emph:              'italic #000000', # class: 'ge'
-        Generic.Error:             '#ef2929',        # class: 'gr'
-        Generic.Heading:           'bold #000080',   # class: 'gh'
-        Generic.Inserted:          '#00A000',        # class: 'gi'
-        Generic.Output:            '#888',           # class: 'go'
-        Generic.Prompt:            '#745334',        # class: 'gp'
-        Generic.Strong:            'bold #000000',   # class: 'gs'
-        Generic.Subheading:        'bold #800080',   # class: 'gu'
-        Generic.Traceback:         'bold #a40000',   # class: 'gt'
-    }
-
-    
-        # Redirection.
-    300: ('multiple_choices',),
-    301: ('moved_permanently', 'moved', '\\o-'),
-    302: ('found',),
-    303: ('see_other', 'other'),
-    304: ('not_modified',),
-    305: ('use_proxy',),
-    306: ('switch_proxy',),
-    307: ('temporary_redirect', 'temporary_moved', 'temporary'),
-    308: ('permanent_redirect',
-          'resume_incomplete', 'resume',),  # These 2 to be removed in 3.0
-    
-    # The scheme of the identifier. Typical schemes are ISBN or URL.
-#epub_scheme = ''
-    
-        :param url: URL for the new :class:`Request` object.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    
-        # Sometimes, urllib3 only reports its version as 16.1.
-    if len(urllib3_version) == 2:
-        urllib3_version.append('0')
-    
-        def __ne__(self, other):
-        return not self == other
-    
-    '''
-requests.hooks
-~~~~~~~~~~~~~~
-    
-    DEFAULT_REDIRECT_LIMIT = 30
-CONTENT_CHUNK_SIZE = 10 * 1024
-ITER_CHUNK_SIZE = 512
-    
-        # Bypass if not a dictionary (e.g. verify)
-    if not (
-            isinstance(session_setting, Mapping) and
-            isinstance(request_setting, Mapping)
-    ):
-        return request_setting
-    
-    
-def add_dict_to_cookiejar(cj, cookie_dict):
-    '''Returns a CookieJar from a key/value dictionary.
-    
-                self.ip_manager.report_connect_fail(ip, force_remove=True)
-    
-        global pteredor_is_running
-    pteredor_is_running = True
-    server_list = prober.eval_servers()
-    pteredor_is_running = False
-    
-    
-    def specialStateTransition(self, s, input):
-        return -1
-    
-            So, you cna see that the parser walks up call chain looking
-        for the token that was a member of the recovery set.
-    
-            raise NotImplementedError
-    
-    def setCharPositionInLine(self, pos):
-        '''@brief Set the column of the tokens first character,
-    
-    def dirname(s): return split(s)[0]
-def basename(s): return split(s)[1]
-    
-    # Join two paths, normalizing and eliminating any symbolic links
-# encountered in the second path.
-def _joinrealpath(path, rest, seen):
-    if isinstance(path, bytes):
-        sep = b'/'
-        curdir = b'.'
-        pardir = b'..'
-    else:
-        sep = '/'
-        curdir = '.'
-        pardir = '..'
-    
-        def test_isdir(self):
-        filename = support.TESTFN
-        bfilename = os.fsencode(filename)
-        self.assertIs(self.pathmodule.isdir(filename), False)
-        self.assertIs(self.pathmodule.isdir(bfilename), False)
-    
-                os.symlink(ABSTFN+'1', ABSTFN+'2')
-            os.symlink(ABSTFN+'2', ABSTFN+'1')
-            self.assertEqual(realpath(ABSTFN+'1'), ABSTFN+'1')
-            self.assertEqual(realpath(ABSTFN+'2'), ABSTFN+'2')
-    
-            if MS_WINDOWS:
-            # PYTHONLEGACYWINDOWSFSENCODING disables the UTF-8 mode
-            # and has the priority over PYTHONUTF8
-            out = self.get_output('-X', 'utf8', '-c', code, PYTHONUTF8='1',
-                                  PYTHONLEGACYWINDOWSFSENCODING='1')
-            self.assertEqual(out, '0')
-    
-        def test_synopsis_sourceless_empty_doc(self):
-        with test.support.temp_cwd() as test_dir:
-            init_path = os.path.join(test_dir, 'foomod42.py')
-            cached_path = importlib.util.cache_from_source(init_path)
-            with open(init_path, 'w') as fobj:
-                fobj.write('foo = 1')
-            py_compile.compile(init_path)
-            synopsis = pydoc.synopsis(init_path, {})
-            self.assertIsNone(synopsis)
-            synopsis_cached = pydoc.synopsis(cached_path, {})
-            self.assertIsNone(synopsis_cached)
-    
-        def testReadMonkeyMultiStream(self):
-        # Test BZ2File.read() on a multi-stream archive where a stream
-        # boundary coincides with the end of the raw read buffer.
-        buffer_size = _compression.BUFFER_SIZE
-        _compression.BUFFER_SIZE = len(self.DATA)
-        try:
-            self.createTempFile(streams=5)
-            with BZ2File(self.filename) as bz2f:
-                self.assertRaises(TypeError, bz2f.read, float())
-                self.assertEqual(bz2f.read(), self.TEXT * 5)
-        finally:
-            _compression.BUFFER_SIZE = buffer_size
-    
-            it = pool.imap(f, list(range(10)))
-        for i in range(10):
             try:
-                x = next(it)
-            except ZeroDivisionError:
-                if i == 5:
-                    pass
-            except StopIteration:
-                break
-            else:
-                if i == 5:
-                    raise AssertionError('expected ZeroDivisionError')
+            with open(filename) as json_file:
+                obj = json.loads(json_file.read())
+        except IOError as e:
+            if silent and e.errno in (errno.ENOENT, errno.EISDIR):
+                return False
+            e.strerror = 'Unable to load configuration file (%s)' % e.strerror
+            raise
+        return self.from_mapping(obj)
     
-        @staticmethod
-    def _static_method_2():
-        print('executed method 2!')
+                def __init__(self, username, remote_addr=None):
+                self.username = username
+                if remote_addr is None and request:
+                    remote_addr = request.remote_addr
+                self.remote_addr = remote_addr
     
-        def update(self):
-        print('Updating the test results in Database')
-        time.sleep(0.1)
+            buf.append('  Make sure to directly send your %s-request to this URL '
+                   'since we can\'t make browsers or HTTP clients redirect '
+                   'with form data reliably or without user interaction.' %
+                   request.method)
+        buf.append('\n\nNote: this exception is only raised in debug mode')
+        AssertionError.__init__(self, ''.join(buf).encode('utf-8'))
     
-    ###  OUTPUT ###
-# Before subclassing:
-# BaseRegisteredClass
-# After subclassing:
-# BaseRegisteredClass
-# ClassRegistree
-
+        # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    '''
+    dirname = 'cifar-10-batches-py'
+    origin = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+    path = get_file(dirname, origin=origin, untar=True)
+    
+        out1 = utils.preprocess_input(x, 'channels_last')
+    out1int = utils.preprocess_input(xint, 'channels_last')
+    out2 = utils.preprocess_input(np.transpose(x, (0, 3, 1, 2)),
+                                  'channels_first')
+    out2int = utils.preprocess_input(np.transpose(xint, (0, 3, 1, 2)),
+                                     'channels_first')
+    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
+    assert_allclose(out1int, out2int.transpose(0, 2, 3, 1))
+    
+        def get_config(self):
+        return {'mse_fraction': self.mse_fraction}
     
     
-class Specification(object):
-    def and_specification(self, candidate):
-        raise NotImplementedError()
+def create_model(kernel_regularizer=None, activity_regularizer=None):
+    model = Sequential()
+    model.add(Dense(num_classes,
+                    kernel_regularizer=kernel_regularizer,
+                    activity_regularizer=activity_regularizer,
+                    input_shape=(data_dim,)))
+    return model
     
-        def toggle_amfm(self):
-        self.state.toggle_amfm()
+            i = self.recurrent_activation(x_i + h_i)
+        f = self.recurrent_activation(x_f + h_f)
+        c = f * c_tm1 + i * self.activation(x_c + h_c)
+        o = self.recurrent_activation(x_o + h_o)
+        h = o * self.activation(c)
+    
+        def predict_classes(self, x, batch_size=32, verbose=0):
+        '''Generate class predictions for the input samples.
+    
+        with open('README.md') as readme:
+        readme_html = mistune.markdown(readme.read())
+        readme_soup = BeautifulSoup.BeautifulSoup(readme_html, 'html.parser')
