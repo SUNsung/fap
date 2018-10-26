f74e589dbe85222c3df189c15ea78791bb1f3bec
SUNsung <weatherius@gmail.com>
2018-10-26 10:02:04 +0300

    Rewrite [47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp] File in: 2018-10-26 10:02:03

--- cccccc/47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp
+++ cccccc/47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp
@@ -1,294 +1,215 @@
 
         
-          /**
-   * @brief Compute the volume of a slice; i.e., the product of dimensions
-   *        among a range of axes.
-   *
-   * @param start_axis The first axis to include in the slice.
-   *
-   * @param end_axis The first axis to exclude from the slice.
-   */
-  inline int count(int start_axis, int end_axis) const {
-    CHECK_LE(start_axis, end_axis);
-    CHECK_GE(start_axis, 0);
-    CHECK_GE(end_axis, 0);
-    CHECK_LE(start_axis, num_axes());
-    CHECK_LE(end_axis, num_axes());
-    int count = 1;
-    for (int i = start_axis; i < end_axis; ++i) {
-      count *= shape(i);
+        IPC_SYNC_MESSAGE_ROUTED0_1(ShellViewHostMsg_AllocateId, int)
+    
+    // Tell browser to delete a object.
+// function DeallocateObject(id);
+v8::Handle<v8::Value> DeallocateObject(int routing_id,
+                                       int object_id);
+    
+    void Menu::Call(const std::string& method,
+                const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  if (method == 'Append') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    Append(object_manager()->GetApiObject<MenuItem>(object_id));
+  } else if (method == 'Insert') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Remove') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Popup') {
+    int x = 0;
+    arguments.GetInteger(0, &x);
+    int y = 0;
+    arguments.GetInteger(1, &y);
+    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
+    DCHECK(web_contents);
+    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
     }
-    return count;
-  }
-  /**
-   * @brief Compute the volume of a slice spanning from a particular first
-   *        axis to the final axis.
-   *
-   * @param start_axis The first axis to include in the slice.
-   */
-  inline int count(int start_axis) const {
-    return count(start_axis, num_axes());
-  }
-    
-      // Thread local context for Caffe. Moved to common.cpp instead of
-  // including boost/thread.hpp to avoid a boost/NVCC issues (#1009, #1010)
-  // on OSX. Also fails on Linux with CUDA 7.0.18.
-  static Caffe& Get();
-    
-      /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to the data.
-   *
-   * @param datum
-   *    Datum containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See data_layer.cpp for an example.
-   */
-  void Transform(const Datum& datum, Blob<Dtype>* transformed_blob);
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the reordered input.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient
-   *        with respect to the outputs
-   *   -# @f$ (M \times ...) @f$:
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to concatenated outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2):
-   *   - @f$ \frac{\partial E}{\partial y} @f$ is de-indexed (summing where
-   *     required) back to the input x_1
-   *   - This layer cannot backprop to x_2, i.e. propagate_down[1] must be
-   *     false.
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-      vector<cudnnTensorDescriptor_t> bottom_descs_, top_descs_;
-  cudnnTensorDescriptor_t bias_desc_;
-  cudnnFilterDescriptor_t filter_desc_;
-  vector<cudnnConvolutionDescriptor_t> conv_descs_;
-  int bottom_offset_, top_offset_, bias_offset_;
-    
-    #ifdef USE_CUDNN
-template <typename Dtype>
-class CuDNNLRNLayer : public LRNLayer<Dtype> {
- public:
-  explicit CuDNNLRNLayer(const LayerParameter& param)
-      : LRNLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNLRNLayer();
     }
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    // Computes and returns the dot product of the n-vectors u and v.
-// Uses Intel AVX intrinsics to access the SIMD instruction set.
-double DotProductAVX(const double* u, const double* v, int n) {
-  int max_offset = n - 4;
-  int offset = 0;
-  // Accumulate a set of 4 sums in sum, by loading pairs of 4 values from u and
-  // v, and multiplying them together in parallel.
-  __m256d sum = _mm256_setzero_pd();
-  if (offset <= max_offset) {
-    offset = 4;
-    // Aligned load is reputedly faster but requires 32 byte aligned input.
-    if ((reinterpret_cast<uintptr_t>(u) & 31) == 0 &&
-        (reinterpret_cast<uintptr_t>(v) & 31) == 0) {
-      // Use aligned load.
-      __m256d floats1 = _mm256_load_pd(u);
-      __m256d floats2 = _mm256_load_pd(v);
-      // Multiply.
-      sum = _mm256_mul_pd(floats1, floats2);
-      while (offset <= max_offset) {
-        floats1 = _mm256_load_pd(u + offset);
-        floats2 = _mm256_load_pd(v + offset);
-        offset += 4;
-        __m256d product = _mm256_mul_pd(floats1, floats2);
-        sum = _mm256_add_pd(sum, product);
-      }
-    } else {
-      // Use unaligned load.
-      __m256d floats1 = _mm256_loadu_pd(u);
-      __m256d floats2 = _mm256_loadu_pd(v);
-      // Multiply.
-      sum = _mm256_mul_pd(floats1, floats2);
-      while (offset <= max_offset) {
-        floats1 = _mm256_loadu_pd(u + offset);
-        floats2 = _mm256_loadu_pd(v + offset);
-        offset += 4;
-        __m256d product = _mm256_mul_pd(floats1, floats2);
-        sum = _mm256_add_pd(sum, product);
-      }
-    }
+    void MenuItem::SetSubmenu(Menu* sub_menu) {
+  submenu_ = sub_menu;
+  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    sub_menu->UpdateKeys(gtk_accel_group);
   }
-  // Add the 4 product sums together horizontally. Not so easy as with sse, as
-  // there is no add across the upper/lower 128 bit boundary, so permute to
-  // move the upper 128 bits to lower in another register.
-  __m256d sum2 = _mm256_permute2f128_pd(sum, sum, 1);
-  sum = _mm256_hadd_pd(sum, sum2);
-  sum = _mm256_hadd_pd(sum, sum);
-  double result;
-  // _mm256_extract_f64 doesn't exist, but resist the temptation to use an sse
-  // instruction, as that introduces a 70 cycle delay. All this casting is to
-  // fool the intrinsics into thinking we are extracting the bottom int64.
-  auto cast_sum = _mm256_castpd_si256(sum);
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored '-Wstrict-aliasing'
-  *(reinterpret_cast<int64_t*>(&result)) =
-#if defined(_WIN32) || defined(__i386__)
-      // This is a very simple workaround that is activated
-      // for all platforms that do not have _mm256_extract_epi64.
-      // _mm256_extract_epi64(X, Y) == ((uint64_t*)&X)[Y]
-      ((uint64_t*)&cast_sum)[0]
-#else
-      _mm256_extract_epi64(cast_sum, 0)
-#endif
-      ;
-#pragma GCC diagnostic pop
-  while (offset < n) {
-    result += u[offset] * v[offset];
-    ++offset;
-  }
-  return result;
+  if (sub_menu == NULL)
+    gtk_menu_item_remove_submenu(GTK_MENU_ITEM(menu_item_));
+  else
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item_), sub_menu->menu_);
 }
     
-    #include <emmintrin.h>
-#include <smmintrin.h>
-#include <cstdint>
-#include 'dotproductsse.h'
+    #undef cv_hal_add8u
+#define cv_hal_add8u TEGRA_ADD
+#undef cv_hal_add8s
+#define cv_hal_add8s TEGRA_ADD
+#undef cv_hal_add16u
+#define cv_hal_add16u TEGRA_ADD
+#undef cv_hal_add16s
+#define cv_hal_add16s TEGRA_ADD
+#undef cv_hal_add32s
+#define cv_hal_add32s TEGRA_ADD
+#undef cv_hal_add32f
+#define cv_hal_add32f TEGRA_ADDF
+//#undef cv_hal_add64f
+//#define cv_hal_add64f TEGRA_ADDF
+#undef cv_hal_sub8u
+#define cv_hal_sub8u TEGRA_SUB
+#undef cv_hal_sub8s
+#define cv_hal_sub8s TEGRA_SUB
+#undef cv_hal_sub16u
+#define cv_hal_sub16u TEGRA_SUB
+#undef cv_hal_sub16s
+#define cv_hal_sub16s TEGRA_SUB
+#undef cv_hal_sub32s
+#define cv_hal_sub32s TEGRA_SUB
+#undef cv_hal_sub32f
+#define cv_hal_sub32f TEGRA_SUBF
+//#undef cv_hal_sub64f
+//#define cv_hal_sub64f TEGRA_SUBF
+#undef cv_hal_max8u
+#define cv_hal_max8u TEGRA_MAX
+#undef cv_hal_max8s
+#define cv_hal_max8s TEGRA_MAX
+#undef cv_hal_max16u
+#define cv_hal_max16u TEGRA_MAX
+#undef cv_hal_max16s
+#define cv_hal_max16s TEGRA_MAX
+#undef cv_hal_max32s
+#define cv_hal_max32s TEGRA_MAX
+#undef cv_hal_max32f
+#define cv_hal_max32f TEGRA_MAX
+//#undef cv_hal_max64f
+//#define cv_hal_max64f TEGRA_MAX
+#undef cv_hal_min8u
+#define cv_hal_min8u TEGRA_MIN
+#undef cv_hal_min8s
+#define cv_hal_min8s TEGRA_MIN
+#undef cv_hal_min16u
+#define cv_hal_min16u TEGRA_MIN
+#undef cv_hal_min16s
+#define cv_hal_min16s TEGRA_MIN
+#undef cv_hal_min32s
+#define cv_hal_min32s TEGRA_MIN
+#undef cv_hal_min32f
+#define cv_hal_min32f TEGRA_MIN
+//#undef cv_hal_min64f
+//#define cv_hal_min64f TEGRA_MIN
+#undef cv_hal_absdiff8u
+#define cv_hal_absdiff8u TEGRA_ABSDIFF
+#undef cv_hal_absdiff8s
+#define cv_hal_absdiff8s TEGRA_ABSDIFF
+#undef cv_hal_absdiff16u
+#define cv_hal_absdiff16u TEGRA_ABSDIFF
+#undef cv_hal_absdiff16s
+#define cv_hal_absdiff16s TEGRA_ABSDIFF
+#undef cv_hal_absdiff32s
+#define cv_hal_absdiff32s TEGRA_ABSDIFF
+#undef cv_hal_absdiff32f
+#define cv_hal_absdiff32f TEGRA_ABSDIFF
+//#undef cv_hal_absdiff64f
+//#define cv_hal_absdiff64f TEGRA_ABSDIFF
+#undef cv_hal_and8u
+#define cv_hal_and8u TEGRA_AND
+#undef cv_hal_or8u
+#define cv_hal_or8u TEGRA_OR
+#undef cv_hal_xor8u
+#define cv_hal_xor8u TEGRA_XOR
+#undef cv_hal_not8u
+#define cv_hal_not8u TEGRA_NOT
     
-    // Computes and returns the dot product of the n-vectors u and v.
-// Uses Intel SSE intrinsics to access the SIMD instruction set.
-double DotProductSSE(const double* u, const double* v, int n);
-// Computes and returns the dot product of the n-vectors u and v.
-// Uses Intel SSE intrinsics to access the SIMD instruction set.
-int32_t IntDotProductSSE(const int8_t* u, const int8_t* v, int n);
+    #include <cstring>
     
-    // Extracts and converts 8x32-bit results from result, adding the bias from wi
-// and scaling by scales, before storing in *v. Note that wi, scales and v are
-// expected to contain 8 consecutive elements or num_out if less.
-inline void ExtractResults(__m256i& result, __m256i& shift_id,
-                           const int8_t*& wi, const double*& scales,
-                           int num_out, double*& v) {
-  for (int out = 0; out < num_out; ++out) {
-    int32_t res =
-#ifndef _MSC_VER
-        _mm256_extract_epi32(result, 0)
+    void add(const Size2D &size,
+         const f32 * src0Base, ptrdiff_t src0Stride,
+         const f32 * src1Base, ptrdiff_t src1Stride,
+         f32 * dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride,
+                         AddWrap<f32, f32>());
 #else
-        // Workaround MSVC's ICE
-        // _mm256_extract_epi32(X, Y) == ((int32_t*)&X)[Y]
-        ((int32_t*)&result)[0]
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
 #endif
-        ;
-    *v++ = (static_cast<double>(res) / INT8_MAX + *wi++) * *scales++;
-    // Rotate the results in int32_t units, so the next result is ready.
-    result = _mm256_permutevar8x32_epi32(result, shift_id);
-  }
 }
     
-    struct OSResults {
-  OSResults() : unicharset(nullptr) {
-    for (int i = 0; i < 4; ++i) {
-      for (int j = 0; j < kMaxNumberOfScripts; ++j)
-        scripts_na[i][j] = 0;
-      orientations[i] = 0;
-    }
-  }
-  void update_best_orientation();
-  // Set the estimate of the orientation to the given id.
-  void set_best_orientation(int orientation_id);
-  // Update/Compute the best estimate of the script assuming the given
-  // orientation id.
-  void update_best_script(int orientation_id);
-  // Return the index of the script with the highest score for this orientation.
-  TESS_API int get_best_script(int orientation_id) const;
-  // Accumulate scores with given OSResults instance and update the best script.
-  void accumulate(const OSResults& osr);
+    struct BitwiseAnd
+{
+    typedef u8 type;
     }
     
-    struct BlobData {
-  BlobData() : blob(nullptr), choices(nullptr) {}
-  BlobData(int index, Tesseract* tess, const WERD_RES& word)
-    : blob(word.chopped_word->blobs[index]),
-      tesseract(tess),
-      choices(&(*word.ratings)(index, index)) {}
+    void blur3x3(const Size2D &size, s32 cn,
+             const u8 * srcBase, ptrdiff_t srcStride,
+             u8 * dstBase, ptrdiff_t dstStride,
+             BORDER_MODE borderType, u8 borderValue)
+{
+    internal::assertSupportedConfiguration(isBlurU8Supported(size, cn, borderType));
+#ifdef CAROTENE_NEON
+//#define FLOAT_VARIANT_1_9
+#ifdef FLOAT_VARIANT_1_9
+    float32x4_t v1_9 = vdupq_n_f32 (1.0/9.0);
+    float32x4_t v0_5 = vdupq_n_f32 (.5);
+#else
+    const int16x8_t vScale = vmovq_n_s16(3640);
+#endif
     }
     
-      // Constructors for the various ParamTypes.
-  ParamContent() = default;
-  explicit ParamContent(tesseract::StringParam* it);
-  explicit ParamContent(tesseract::IntParam* it);
-  explicit ParamContent(tesseract::BoolParam* it);
-  explicit ParamContent(tesseract::DoubleParam* it);
-    
-    #include 'blread.h'
-#include <cstdio>       // for fclose, fopen, FILE
-#include 'host.h'       // for TRUE
-#include 'ocrblock.h'   // for BLOCK_IT, BLOCK, BLOCK_LIST (ptr only)
-#include 'scanutils.h'  // for tfscanf
-    
-      /**
-   * @brief the distributed work ID of a carve
-   *
-   * This value should be used by the TLS endpoints where carve data is
-   * aggregated, to tie together a distributed query with the carve data
-   */
-  std::string requestId_;
-    
-    namespace osquery {
+            mag_buf[1][0] = mag_buf[1][size.width+1] = 0;
+        if (borderyt == 0)
+        {
+            //sobelH row #-1
+            _src = internal::getRowPtr(srcBase, srcStride, -1);
+            sobelRow(_src, ((s16*)mag_buf[2]) + shxOffset, ((s16*)mag_buf[2]) + shyOffset, size.width);
     }
     
     
-    {/**
- * @brief Compute a hash digest from the contents of a buffer.
- *
- * @param hash_type The osquery-supported hash algorithm.
- * @param buffer A caller-controlled buffer (already allocated).
- * @param size The length of buffer in bytes.
- * @return A string (hex) representation of the hash digest.
- */
-std::string hashFromBuffer(HashType hash_type, const void* buffer, size_t size);
-} // namespace osquery
-
+    {                currx[0] = srow0 ? srow0[x3] : borderValue;
+                currx[1] =         srow1[x3]              ;
+                currx[2] = srow2 ? srow2[x3] : borderValue;
+            }
     
-    TEST_F(ProcessTests, test_getpid) {
-  int pid = -1;
+      static Data data_;
+  static Data data6_;
+    
+    class DHTMessageCallback;
+    
+    #include 'common.h'
+    
+    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createPeerLookupTask(
+    const std::shared_ptr<DownloadContext>& ctx, uint16_t tcpPort,
+    const std::shared_ptr<PeerStorage>& peerStorage)
+{
+  auto task = std::make_shared<DHTPeerLookupTask>(ctx, tcpPort);
+  // TODO this may be not freed by RequestGroup::releaseRuntimeResource()
+  task->setPeerStorage(peerStorage);
+  setCommonProperty(task);
+  return task;
+}
+    
+    class DHTTokenTracker {
+private:
+  static const size_t SECRET_SIZE = 4;
     }
     
-    #include <boost/filesystem/operations.hpp>
-    
-     private:
-  /// Inform the watcher that the worker restarted without cause.
-  void workerRestarted() {
-    worker_restarts_++;
-  }
-    
-      QueryData generate(QueryContext& request) {
-    Row r;
-    }
-    
-        if (frame_num == 0) {
-      LOG_IF_EVERY_N(ERROR, receive_none_count++ > ERROR_COUNT_MAX,
-                     ERROR_COUNT_MAX)
-          << 'Received ' << receive_none_count << ' empty messages.';
-      std::this_thread::sleep_for(default_period);
-      continue;
-    }
-    receive_none_count = 0;
-    
-      /*
-   * @brief get interval period for canbus messages
-   * @return the interval period in us (1e-6s)
-   */
-  virtual uint32_t GetPeriod() const;
-    
-    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    #include 'DHTNode.h'
+#include 'util.h'
+#include 'a2functional.h'
