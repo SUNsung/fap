2215cf6fa06c98fc4aa5e68831bea2b8b3d09c35
SUNsung <weatherius@gmail.com>
2018-10-26 20:00:05 +0300

    Rewrite [06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py] File in: 2018-10-26 20:00:04

--- pyton/06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py
+++ pyton/06oDN48gdyecc06ZBJhZ3yukUk06K36tDC.QAU061ao.py
@@ -1,120 +1,292 @@
 
         
-        
-class HasKey(PostgresSimpleLookup):
-    lookup_name = 'has_key'
-    operator = '?'
-    prepare_rhs = False
+            # L = -KL + log p(x|z), to maximize bound on likelihood
+    # -L = KL - log p(x|z), to minimize bound on NLL
+    # so 'KL cost' is postive KL divergence
+    z0_bxu = post_zs[0].sample
+    logq_bxu = post_zs[0].logp(z0_bxu)
+    logp_bxu = prior_z_process.logp_t(z0_bxu)
+    z_tm1_bxu = z0_bxu
+    for z_t in post_zs[1:]:
+      # posterior is independent in time, prior is not
+      z_t_bxu = z_t.sample
+      logq_bxu += z_t.logp(z_t_bxu)
+      logp_bxu += prior_z_process.logp_t(z_t_bxu, z_tm1_bxu)
+      z_tm1 = z_t_bxu
     
-        def load(self):
-        try:
-            session_data = self._cache.get(self.cache_key)
-        except Exception:
-            # Some backends (e.g. memcache) raise an exception on invalid
-            # cache keys. If this happens, reset the session. See #17810.
-            session_data = None
-        if session_data is not None:
-            return session_data
-        self._session_key = None
-        return {}
+      This version is specialized for the generator, but isn't as fast, so
+  we have two.  Note this allows for l2 regularization on the recurrent
+  weights, but also implicitly rescales the inputs via the 1/sqrt(input)
+  scaling in the linear helper routine to be large magnitude, if there are
+  fewer inputs than recurrent state.
     
-        def exists(self, session_key):
-        return session_key and (self.cache_key_prefix + session_key) in self._cache or super().exists(session_key)
+    def get_data_batch(batch_size, T, rng, u_std):
+  u_bxt = rng.randn(batch_size, T) * u_std
+  running_sum_b = np.zeros([batch_size])
+  labels_bxt = np.zeros([batch_size, T])
+  for t in xrange(T):
+    running_sum_b += u_bxt[:, t]
+    labels_bxt[:, t] += running_sum_b
+  labels_bxt = np.clip(labels_bxt, -1, 1)
+  return u_bxt, labels_bxt
     
-        req_protocol = request.scheme
-    req_site = get_current_site(request)
+      # Compute the PCs.
+  all_data_mean_nx1 = np.mean(all_data_nxtc, axis=1, keepdims=True)
+  all_data_zm_nxtc = all_data_nxtc - all_data_mean_nx1
+  corr_mat_nxn = np.dot(all_data_zm_nxtc, all_data_zm_nxtc.T)
+  evals_n, evecs_nxn = np.linalg.eigh(corr_mat_nxn)
+  sidxs = np.flipud(np.argsort(evals_n)) # sort such that 0th is highest
+  evals_n = evals_n[sidxs]
+  evecs_nxn = evecs_nxn[:,sidxs]
     
-            print('Training %s ... ' % name, end='')
-        t0 = time()
-        clf.fit(X_train, y_train)
-        train_time[name] = time() - t0
-        t0 = time()
-        y_pred = clf.predict(X_test)
-        test_time[name] = time() - t0
-        accuracy[name] = accuracy_score(y_test, y_pred)
-        print('done')
-    
-        it = 0
-    results = defaultdict(lambda: [])
-    chunk = 100
-    
-        for i, kk in enumerate(krange):
-        print('k = %i (%i out of %i)' % (kk, i + 1, len(krange)))
-        for algorithm in algorithms:
-            nbrs = neighbors.NearestNeighbors(n_neighbors=kk,
-                                              algorithm=algorithm,
-                                              leaf_size=leaf_size)
-            t0 = time()
-            nbrs.fit(X)
-            t1 = time()
-            nbrs.kneighbors(X)
-            t2 = time()
-    
-    for i, n in enumerate(n_samples):
-    for j, p in enumerate(n_features):
-        X = np.random.normal(size=(n, p))
-        t0 = time.time()
-        ward.fit(X)
-        scikits_time[j, i] = time.time() - t0
-        t0 = time.time()
-        hierarchy.ward(X)
-        scipy_time[j, i] = time.time() - t0
-    
-    print('='*80 + '\n#' + '    Text vectorizers benchmark' + '\n' + '='*80 + '\n')
-print('Using a subset of the 20 newsrgoups dataset ({} documents).'
-      .format(len(text)))
-print('This benchmarks runs in ~20 min ...')
-    
-        def test_class(self):
-        class C: 'New-style class'
-        c = C()
-    
-        def persistent_id(self, obj):
-        # Instead of pickling MemoRecord as a regular class instance, we emit a
-        # persistent ID.
-        if isinstance(obj, MemoRecord):
-            # Here, our persistent ID is simply a tuple, containing a tag and a
-            # key, which refers to a specific record in the database.
-            return ('MemoRecord', obj.key)
-        else:
-            # If obj does not have a persistent ID, return None. This means obj
-            # needs to be pickled as usual.
-            return None
+        if np.isnan(log_perp):
+      sys.stderr.error('log_perplexity is Nan.\n')
+    else:
+      sum_num += log_perp * weights.mean()
+      sum_den += weights.mean()
+    if sum_den > 0:
+      perplexity = np.exp(sum_num / sum_den)
     
     
-def main():
-    parser = ArgumentParser(description='''\
-Send the contents of a directory as a MIME message.
-Unless the -o option is given, the email is sent by forwarding to your local
-SMTP server, which then does the normal delivery process.  Your local machine
-must be running an SMTP server.
-''')
-    parser.add_argument('-d', '--directory',
-                        help='''Mail the contents of the specified directory,
-                        otherwise use the current directory.  Only the regular
-                        files in the directory are sent, and we don't recurse to
-                        subdirectories.''')
-    parser.add_argument('-o', '--output',
-                        metavar='FILE',
-                        help='''Print the composed message to FILE instead of
-                        sending the message to the SMTP server.''')
-    parser.add_argument('-s', '--sender', required=True,
-                        help='The value of the From: header (required)')
-    parser.add_argument('-r', '--recipient', required=True,
-                        action='append', metavar='RECIPIENT',
-                        default=[], dest='recipients',
-                        help='A To: header value (at least one required)')
-    args = parser.parse_args()
-    directory = args.directory
-    if not directory:
-        directory = '.'
-    # Create the message
-    msg = EmailMessage()
-    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
-    msg['To'] = ', '.join(args.recipients)
-    msg['From'] = args.sender
-    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+def imdb_raw_data(data_path=None):
+  '''Load IMDB raw data from data directory 'data_path'.
+  Reads IMDB tf record files containing integer ids,
+  and performs mini-batching of the inputs.
+  Args:
+    data_path: string path to the directory where simple-examples.tgz has
+      been extracted.
+  Returns:
+    tuple (train_data, valid_data)
+    where each of the data objects can be passed to IMDBIterator.
+  '''
     
-    ans = input('View full message?')
-if ans.lower()[0] == 'n':
-    sys.exit()
+    
+# TODO(adai): IMDB labels placeholder to model.
+def create_generator(hparams,
+                     inputs,
+                     targets,
+                     present,
+                     is_training,
+                     is_validating,
+                     reuse=None):
+  '''Create the Generator model specified by the FLAGS and hparams.
+    
+        # Critic loss calculated from the estimated value function \hat{V}(s)
+    # versus the true value function V*(s).
+    critic_loss = create_critic_loss(cumulative_rewards, estimated_values,
+                                     present)
+    
+          elif FLAGS.generator_model.startswith('seq2seq'):
+        load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
+        print('Restoring Generator from %s.' % load_ckpt)
+        tf.logging.info('Restoring Generator from %s.' % load_ckpt)
+        gen_encoder_init_saver = init_savers['gen_encoder_init_saver']
+        gen_decoder_init_saver = init_savers['gen_decoder_init_saver']
+        gen_encoder_init_saver.restore(sess, load_ckpt)
+        gen_decoder_init_saver.restore(sess, load_ckpt)
+    
+    
+@pytest.fixture
+def source_root():
+    return Path(__file__).parent.parent.resolve()
+    
+    
+def test_on_run_after_other_commands(usage_tracker_io, shell_pid, shell, logs):
+    shell_pid.return_value = 12
+    shell.get_history.return_value = ['fuck', 'ls']
+    _change_tracker(usage_tracker_io, 12)
+    main()
+    logs.how_to_configure_alias.assert_called_once()
+    
+    
+def how_to_configure(proc, TIMEOUT):
+    proc.sendline(u'fuck')
+    assert proc.expect([TIMEOUT, u'alias isn't configured'])
+
+    
+    python_2 = ('thefuck/python2-zsh',
+            u'''FROM python:2
+                RUN apt-get update
+                RUN apt-get install -yy zsh''',
+            u'sh')
+    
+     edit-sources - edit the source information file
+'''
+apt_operations = ['list', 'search', 'show', 'update', 'install', 'remove',
+                  'upgrade', 'full-upgrade', 'edit-sources']
+    
+    
+def test_get_new_command():
+    new_command = get_new_command(Command('apt list --upgradable', match_output))
+    assert new_command == 'apt upgrade'
+    
+    
+def test_not_match():
+    assert not match(Command('aws dynamodb invalid', no_suggestions))
+    
+                gc.collect()
+            print('- benchmarking LassoLars')
+            clf = LassoLars(alpha=alpha, fit_intercept=False,
+                            normalize=False, precompute=precompute)
+            tstart = time()
+            clf.fit(X, Y)
+            lars_lasso_results.append(time() - tstart)
+    
+    import numpy as np
+from numpy import random as nr
+    
+            # Create flat baselines to compare the variation over batch size
+        all_times['pca'].extend([results_dict['pca']['time']] *
+                                len(batch_sizes))
+        all_errors['pca'].extend([results_dict['pca']['error']] *
+                                 len(batch_sizes))
+        all_times['rpca'].extend([results_dict['rpca']['time']] *
+                                 len(batch_sizes))
+        all_errors['rpca'].extend([results_dict['rpca']['error']] *
+                                  len(batch_sizes))
+        for batch_size in batch_sizes:
+            ipca = IncrementalPCA(n_components=n_components,
+                                  batch_size=batch_size)
+            results_dict = {k: benchmark(est, data) for k, est in [('ipca',
+                                                                   ipca)]}
+            all_times['ipca'].append(results_dict['ipca']['time'])
+            all_errors['ipca'].append(results_dict['ipca']['error'])
+    
+        (opts, args) = op.parse_args()
+    if len(args) > 0:
+        op.error('this script takes no arguments.')
+        sys.exit(1)
+    opts.n_components = type_auto_or_int(opts.n_components)
+    opts.density = type_auto_or_float(opts.density)
+    selected_transformers = opts.selected_transformers.split(',')
+    
+        fn = os.path.relpath(fn,
+                         start=os.path.dirname(__import__(package).__file__))
+    try:
+        lineno = inspect.getsourcelines(obj)[1]
+    except Exception:
+        lineno = ''
+    return url_fmt.format(revision=revision, package=package,
+                          path=fn, lineno=lineno)
+    
+    
+if not os.path.exists(DATA_FOLDER):
+    
+    # Scrapy version
+import pkgutil
+__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
+version_info = tuple(int(v) if v.isdigit() else v
+                     for v in __version__.split('.'))
+del pkgutil
+    
+        def _list_templates(self):
+        print('Available templates:')
+        for filename in sorted(os.listdir(self.templates_dir)):
+            if filename.endswith('.tmpl'):
+                print('  %s' % splitext(filename)[0])
+    
+            if opts.meta:
+            try:
+                opts.meta = json.loads(opts.meta)
+            except ValueError:
+                raise UsageError('Invalid -m/--meta value, pass a valid json string to -m or --meta. ' \
+                                'Example: --meta='{\'foo\' : \'bar\'}'', print_help=False)
+    
+        def syntax(self):
+        return '[options] <spider_file>'
+    
+            def getContext(self, hostname=None, port=None):
+            ctx = ClientContextFactory.getContext(self)
+            # Enable all workarounds to SSL bugs as documented by
+            # https://www.openssl.org/docs/manmaster/man3/SSL_CTX_set_options.html
+            ctx.set_options(SSL.OP_ALL)
+            return ctx
+
+    
+    The matching from server ftp command return codes to html response codes is defined in the
+CODE_MAPPING attribute of the handler class. The key 'default' is used for any code
+that is not explicitly present among the map keys. You may need to overwrite this
+mapping if want a different behaviour than default.
+    
+        def __init__(self, resource_type, *args, **kwargs):
+        self.resource_type = resource_type
+        super(Resource, self).__init__(
+            'resource', default=resource_type, *args, **kwargs)
+    
+            self.assertRaises(
+            jose.DeserializationError, nonce_field.decode, self.wrong_nonce)
+        self.assertEqual(b'foo', nonce_field.decode(self.good_nonce))
+    
+    
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
+    
+    
+DIRECTORY_URL = 'https://acme-staging.api.letsencrypt.org/directory'
+BITS = 2048  # minimum for Boulder
+DOMAIN = 'example1.com'  # example.com is ignored by Boulder
+    
+            # Self test
+        self.assertTrue(self.addr.conflicts(self.addr))
+        self.assertTrue(self.addr1.conflicts(self.addr1))
+        # This is a tricky one...
+        self.assertTrue(self.addr1.conflicts(self.addr2))
+    
+            mock_exists.return_value = True
+        self.sni.configurator.parser.update_runtime_variables = mock.Mock()
+    
+    from __future__ import print_function
+    
+    '''
+http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+Implementation of the iterator pattern with a generator
+    
+    ### OUTPUT ###
+# Setting Data 1 = 10
+# DecimalViewer: Subject Data 1 has data 10
+# HexViewer: Subject Data 1 has data 0xa
+# Setting Data 2 = 15
+# HexViewer: Subject Data 2 has data 0xf
+# DecimalViewer: Subject Data 2 has data 15
+# Setting Data 1 = 3
+# DecimalViewer: Subject Data 1 has data 3
+# HexViewer: Subject Data 1 has data 0x3
+# Setting Data 2 = 5
+# HexViewer: Subject Data 2 has data 0x5
+# DecimalViewer: Subject Data 2 has data 5
+# Detach HexViewer from data1 and data2.
+# Setting Data 1 = 10
+# DecimalViewer: Subject Data 1 has data 10
+# Setting Data 2 = 15
+# DecimalViewer: Subject Data 2 has data 15
+
+    
+    
+class RegistryHolder(type):
+    
+    '''
+@author: Gordeev Andrey <gordeev.and.and@gmail.com>
+    
+    
+class Visitor(object):
+    def visit(self, node, *args, **kwargs):
+        meth = None
+        for cls in node.__class__.__mro__:
+            meth_name = 'visit_' + cls.__name__
+            meth = getattr(self, meth_name, None)
+            if meth:
+                break
+    
+    
+class ComplexHouse(ComplexBuilding):
+    def build_floor(self):
+        self.floor = 'One'
+    
+        if not sample_queue.empty():
+        print(sample_queue.get())
+    
+    
+class UnsupportedMessageType(BaseException):
+    pass
