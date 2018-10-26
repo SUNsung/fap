ec7fd6d76e24dfd4eaf371c96046cc0d9870ce2d
SUNsung <weatherius@gmail.com>
2018-10-26 22:16:25 +0300

    Rewrite [24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb] File in: 2018-10-26 22:16:24

--- ruby/24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb
+++ ruby/24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb
@@ -1,63 +1,184 @@
 
         
-              # we readlink because this path probably doesn't exist since caveats
-      # occurs before the link step of installation
-      # Yosemite security measures mildly tighter rules:
-      # https://github.com/Homebrew/homebrew/issues/33815
-      if !plist_path.file? || !plist_path.symlink?
-        if f.plist_startup
-          s << 'To have launchd start #{f.full_name} at startup:'
-          s << '  sudo mkdir -p #{destination}' unless destination_path.directory?
-          s << '  sudo cp -fv #{f.opt_prefix}/*.plist #{destination}'
-          s << '  sudo chown root #{plist_link}'
-        else
-          s << 'To have launchd start #{f.full_name} at login:'
-          s << '  mkdir -p #{destination}' unless destination_path.directory?
-          s << '  ln -sfv #{f.opt_prefix}/*.plist #{destination}'
-        end
-        s << 'Then to load #{f.full_name} now:'
-        if f.plist_startup
-          s << '  sudo launchctl load #{plist_link}'
-        else
-          s << '  launchctl load #{plist_link}'
-        end
-      # For startup plists, we cannot tell whether it's running on launchd,
-      # as it requires for `sudo launchctl list` to get real result.
-      elsif f.plist_startup
-        s << 'To reload #{f.full_name} after an upgrade:'
-        s << '  sudo launchctl unload #{plist_link}'
-        s << '  sudo cp -fv #{f.opt_prefix}/*.plist #{destination}'
-        s << '  sudo chown root #{plist_link}'
-        s << '  sudo launchctl load #{plist_link}'
-      elsif Kernel.system '/bin/launchctl list #{plist_domain} &>/dev/null'
-        s << 'To reload #{f.full_name} after an upgrade:'
-        s << '  launchctl unload #{plist_link}'
-        s << '  launchctl load #{plist_link}'
-      else
-        s << 'To load #{f.full_name}:'
-        s << '  launchctl load #{plist_link}'
-      end
+              # This method returns an HTML safe string similar to what <tt>Array#join</tt>
+      # would return. The array is flattened, and all items, including
+      # the supplied separator, are HTML escaped unless they are HTML
+      # safe, and the returned string is marked as HTML safe.
+      #
+      #   safe_join([raw('<p>foo</p>'), '<p>bar</p>'], '<br />')
+      #   # => '<p>foo</p>&lt;br /&gt;&lt;p&gt;bar&lt;/p&gt;'
+      #
+      #   safe_join([raw('<p>foo</p>'), raw('<p>bar</p>')], raw('<br />'))
+      #   # => '<p>foo</p><br /><p>bar</p>'
+      #
+      def safe_join(array, sep = $,)
+        sep = ERB::Util.unwrapped_html_escape(sep)
     
-      # Clean the keg of formula @f
-  def clean
-    ObserverPathnameExtension.reset_counts!
+              def checked?(value)
+            case value
+            when TrueClass, FalseClass
+              value == !!@checked_value
+            when NilClass
+              false
+            when String
+              value == @checked_value
+            else
+              if value.respond_to?(:include?)
+                value.include?(@checked_value)
+              else
+                value.to_i == @checked_value.to_i
+              end
+            end
+          end
     
-      SEARCHABLE_TAPS = OFFICIAL_TAPS.map { |tap| ['Homebrew', tap] } + [
-    %w[Caskroom cask],
-    %w[Caskroom versions]
-  ]
+          def prepend_formats(formats) # :doc:
+        formats = Array(formats)
+        return if formats.empty? || @lookup_context.html_fallback_for_js
     
-      def std_cmake_parameters
-    '-DCMAKE_INSTALL_PREFIX='#{prefix}' -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev'
+              @bar3 = Agents::DotBar.new(name: 'bar3').tap { |agent|
+            agent.user = users(:bob)
+            agent.sources << @bar2
+            agent.save!
+          },
+        ]
+        @foo.reload
+        @bar2.reload
+    
+        it 'in the future' do
+      expect(relative_distance_of_time_in_words(Time.now+5.minutes)).to eq('in 5m')
+    end
+  end
+end
+
+    
+      it 'converts values to Float' do
+    expect(location.lat).to be_a Float
+    expect(location.lat).to eq 2.0
+    expect(location.lng).to be_a Float
+    expect(location.lng).to eq 3.0
+    expect(location.radius).to be_a Float
+    expect(location.radius).to eq 300.0
+    expect(location.speed).to be_a Float
+    expect(location.speed).to eq 2.0
+    expect(location.course).to be_a Float
+    expect(location.course).to eq 30.0
   end
     
-        if other.respond_to?(:to_str)
-      return true if to_str == other.to_str
+    gem 'activemodel-serializers-xml', github: 'rails/activemodel-serializers-xml'
+    
+        def unlock_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :unlock_instructions, opts)
     end
     
-    module Homebrew
-  module_function
+      # Sets warden configuration using a block that will be invoked on warden
+  # initialization.
+  #
+  #  Devise.setup do |config|
+  #    config.allow_unconfirmed_access_for = 2.days
+  #
+  #    config.warden do |manager|
+  #      # Configure warden to use other strategies, like oauth.
+  #      manager.oauth(:twitter)
+  #    end
+  #  end
+  def self.warden(&block)
+    @@warden_config_blocks << block
+  end
     
-          new_name = name
-      new_path = path
-      new_remote = default_remote
+        def current_url
+      context[:url]
+    end
+    
+        def root?
+      path == 'index'
+    end
+    
+        def add(path, content)
+      @pages[path] = content
+    end
+    
+          def additional_options
+        if self.class.internal_urls
+          super.merge! \
+            only: self.class.internal_urls.to_set,
+            only_patterns: nil,
+            skip: nil,
+            skip_patterns: nil,
+            skip_links: nil,
+            fixed_internal_urls: true
+        else
+          super
+        end
+      end
+    
+              # Default opts to a blank optionparser if none is given
+          opts ||= OptionParser.new
+    
+            # Executes a command on the remote machine with administrative
+        # privileges. See {#execute} for documentation, as the API is the
+        # same.
+        #
+        # @see #execute
+        def sudo(command, opts=nil)
+        end
+    
+            # This method is automatically called when the system is available (when
+        # Vagrant can successfully SSH into the machine) to give the system a chance
+        # to determine the distro and return a distro-specific system.
+        #
+        # If this method returns nil, then this instance is assumed to be
+        # the most specific guest implementation.
+        def distro_dispatch
+        end
+    
+            # This returns all the registered guests.
+        #
+        # @return [Hash]
+        def guests
+          result = {}
+    
+            # Defines additional configuration keys to be available in the
+        # Vagrantfile. The configuration class should be returned by a
+        # block passed to this method. This is done to ensure that the class
+        # is lazy loaded, so if your class inherits from any classes that
+        # are specific to Vagrant 1.0, then the plugin can still be defined
+        # without breaking anything in future versions of Vagrant.
+        #
+        # @param [String] name Configuration key.
+        # @param [Boolean] upgrade_safe If this is true, then this configuration
+        #   key is safe to load during an upgrade, meaning that it depends
+        #   on NO Vagrant internal classes. Do _not_ set this to true unless
+        #   you really know what you're doing, since you can cause Vagrant
+        #   to crash (although Vagrant will output a user-friendly error
+        #   message if this were to happen).
+        def self.config(name=UNSET_VALUE, upgrade_safe=false, &block)
+          data[:config] ||= Registry.new
+    
+            # Defines a capability for the given provider. The block should return
+        # a class/module that has a method with the capability name, ready
+        # to be executed. This means that if it is an instance method,
+        # the block should return an instance of the class.
+        #
+        # @param [String] provider The name of the provider
+        # @param [String] cap The name of the capability
+        def self.provider_capability(provider, cap, &block)
+          components.provider_capabilities[provider.to_sym].register(cap.to_sym, &block)
+          nil
+        end
+    
+    module Admin
+  class InstancesController < BaseController
+    def index
+      authorize :instance, :index?
+      @instances = ordered_instances
+    end
+    
+    __END__
+    
+          def has_vector?(request, headers)
+        return false if request.xhr?
+        return false if options[:allow_if] && options[:allow_if].call(request.env)
+        return false unless headers['Content-Type'].to_s.split(';', 2).first =~ /^\s*application\/json\s*$/
+        origin(request.env).nil? and referrer(request.env) != request.host
+      end
