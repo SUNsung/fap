be09b40fcb5c3dddd17d31c218c215339483578a
SUNsung <weatherius@gmail.com>
2018-10-29 14:13:14 +0300

    Rewrite [486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb] File in: 2018-10-29 13:13:13

--- ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
+++ ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
@@ -1,125 +1,64 @@
 
         
-        links.each do |link|
-  href = link.attribute('href').to_s
-  uri = URI.join(BASE_URI, href)
-  if map[uri]
-    dups <<  href
-  end
-  map[uri] = href
-end
-    
-      def set_account
-    @account = Account.find_local!(params[:account_username])
+            @statuses = @account.statuses.permitted_for(@account, signed_request_account)
+    @statuses = params[:min_id].present? ? @statuses.paginate_by_min_id(LIMIT, params[:min_id]).reverse : @statuses.paginate_by_max_id(LIMIT, params[:max_id])
+    @statuses = cache_collection(@statuses, Status)
   end
     
-        def destroy
-      authorize @account_moderation_note, :destroy?
-      @account_moderation_note.destroy!
-      redirect_to admin_account_path(@account_moderation_note.target_account_id), notice: I18n.t('admin.account_moderation_notes.destroyed_msg')
-    end
+          @account_moderation_note = current_account.account_moderation_notes.new(resource_params)
     
-        def require_local_account!
-      redirect_to admin_account_path(@account.id) unless @account.local? && @account.user.present?
-    end
-    
-        def check_confirmation
-      if @user.confirmed?
-        flash[:error] = I18n.t('admin.accounts.resend_confirmation.already_confirmed')
-        redirect_to admin_accounts_path
+            @user.send_confirmation_instructions
       end
-    end
+    
+            redirect_to admin_report_path(@report), notice: I18n.t('admin.report_notes.created_msg')
+      else
+        @report_notes = @report.notes.latest
+        @report_history = @report.history
+        @form = Form::StatusBatch.new
+    
+      def show
+    @status = status_finder.status
+    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
+  end
+    
+      def account_feed_path?
+    hub_topic_params[:controller] == 'accounts' && hub_topic_params[:action] == 'show' && hub_topic_params[:format] == 'atom'
   end
 end
 
     
-      def show
-    if subscription.valid?(params['hub.topic'])
-      @account.update(subscription_expires_at: future_expires)
-      render plain: encoded_challenge, status: 200
-    else
-      head 404
-    end
+      def subscription
+    @_subscription ||= @account.subscription(
+      api_subscription_url(@account.id)
+    )
   end
     
-        # Extract the command string
-    self.cmd_string = header.slice!(/.+\r\n/)
+            private
     
-    IAX_IE_CALLED_NUMBER  = 1
-IAX_IE_CALLING_NUMBER = 2
-IAX_IE_AUTH_METHODS   = 3
-IAX_IE_CALLING_NAME   = 4
-IAX_IE_USERNAME       = 6
-IAX_IE_DESIRED_CODEC  = 9
-IAX_IE_ORIGINAL_DID   = 10
-IAX_IE_ACTUAL_CODECS  = 8
-IAX_IE_PROTO_VERSION  = 11
-IAX_IE_REG_REFRESH    = 19
-IAX_IE_CHALLENGE_DATA = 15
-IAX_IE_CHALLENGE_RESP = 16
-IAX_IE_APPARENT_ADDR  = 18
-IAX_IE_REGREJ_CAUSE   = 22
-IAX_IE_HANGUP_CAUSE   = 42
+            def destroy
+          @option_type = Spree::OptionType.accessible_by(current_ability, :destroy).find(params[:id])
+          @option_type.destroy
+          render plain: nil, status: 204
+        end
     
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
-          #
-          # @return [String] encoded principal
-          def encode
-            encoded = ''
-            encoded << encode_name_type
-            encoded << [components.length].pack('N')
-            encoded << encode_realm
-            encoded << encode_components
+            def load_transfer_params
+          @original_shipment         = Spree::Shipment.find_by!(number: params[:original_shipment_number])
+          @variant                   = Spree::Variant.find(params[:variant_id])
+          @quantity                  = params[:quantity].to_i
+          authorize! :read, @original_shipment
+          authorize! :create, Shipment
+        end
     
-                cipher = OpenSSL::Cipher.new('rc4')
-            cipher.encrypt
-            cipher.key = k3
-            encrypted = cipher.update(data_encrypt) + cipher.final
+              state = @states.last
+          respond_with(@states) if stale?(state)
+        end
     
-              # Decodes the key_expiration field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_key_expiration(input)
-            input.value[0].value
+            def create
+          authorize! :create, StockLocation
+          @stock_location = StockLocation.new(stock_location_params)
+          if @stock_location.save
+            respond_with(@stock_location, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_location)
           end
-    
-          # private
-    
-    module Precious
-  module Views
-    class Layout < Mustache
-      include Rack::Utils
-      alias_method :h, :escape_html
-    
-          def upload_dest
-        @upload_dest
-      end
-    
-      # replace name version and date
-  replace_header(head, :name)
-  replace_header(head, :version)
-  replace_header(head, :date)
-  #comment this out if your rubyforge_project has a different name
-  replace_header(head, :rubyforge_project)
-    
-    desc 'preview the site in a web browser'
-task :preview do
-  raise '### You haven't set anything up yet. First run `rake install` to set up an Octopress theme.' unless File.directory?(source_dir)
-  puts 'Starting to watch source with Jekyll and Compass. Starting Rack on port #{server_port}'
-  system 'compass compile --css-dir #{source_dir}/stylesheets' unless File.exist?('#{source_dir}/stylesheets/screen.css')
-  jekyllPid = Process.spawn({'OCTOPRESS_ENV'=>'preview'}, 'jekyll build --watch')
-  compassPid = Process.spawn('compass watch')
-  rackupPid = Process.spawn('rackup --port #{server_port}')
-    
-        # Creates an instance of CategoryIndex for each category page, renders it, and
-    # writes the output to a file.
-    #
-    #  +category_dir+ is the String path to the category folder.
-    #  +category+     is the category currently being processed.
-    def write_category_index(category_dir, category)
-      index = CategoryIndex.new(self, self.source, category_dir, category)
-      index.render(self.layouts, site_payload)
-      index.write(self.dest)
-      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
-      self.pages << index
+        end
