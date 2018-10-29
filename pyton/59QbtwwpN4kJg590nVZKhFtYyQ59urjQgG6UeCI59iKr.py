f14d8004113a255152f51b26bb29ff70f7aa1c37
SUNsung <weatherius@gmail.com>
2018-10-30 00:02:51 +0300

    Rewrite [59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py] File in: 2018-10-29 23:02:50

--- pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
+++ pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
@@ -1,108 +1,188 @@
 
         
-        
-def check_entry(line_num, segments):
-    # START Title
-    raw_title = segments[index_title]
-    title_re_match = link_re.match(raw_title)
-    # url should be wrapped in '[TITLE](LINK)' Markdown syntax
-    if not title_re_match:
-        add_error(line_num, 'Title syntax should be '[TITLE](LINK)'')
-    else:
-        # do not allow '... API' in the entry title
-        title = title_re_match.group(1)
-        if title.upper().endswith(' API'):
-            add_error(line_num, 'Title should not end with '... API'. Every entry is an API here!')
-        # do not allow duplicate links
-        link = title_re_match.group(2)
-        if link in previous_links:
-            add_error(line_num, 'Duplicate link - entries should only be included in one section')
+            # register the database commands
+    from flaskr import db
+    db.init_app(app)
+    
+            if error is None:
+            # the name is available, store it in the database and go to
+            # the login page
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+    
+            .. versionadded:: 0.5
+        '''
+        return self.static_folder is not None
+    
+        def setdefault(self, key, default=None):
+        self.accessed = True
+        return super(SecureCookieSession, self).setdefault(key, default)
+    
+        def __exit__(self, exc_type, exc_value, traceback):
+        self.proc.kill()
+        self.proc.wait()
+        time.sleep(0.2)
+    
+        def run(self, args, opts):
+        if len(args) < 1:
+            raise UsageError()
+        elif len(args) > 1:
+            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
+        spname = args[0]
+    
+        def run(self, args, opts):
+        if len(args) != 1 or not is_url(args[0]):
+            raise UsageError()
+        cb = lambda x: self._print_response(x, opts)
+        request = Request(args[0], callback=cb, dont_filter=True)
+        # by default, let the framework handle redirects,
+        # i.e. command handles all codes expect 3xx
+        if not opts.no_redirect:
+            request.meta['handle_httpstatus_list'] = SequenceExclude(range(300, 400))
         else:
-            previous_links.append(link)
-    # END Title
-    # START Description
-    # first character should be capitalized
-    char = segments[index_desc][0]
-    if char.upper() != char:
-        add_error(line_num, 'first character of description is not capitalized')
-    # last character should not punctuation
-    char = segments[index_desc][-1]
-    if char in punctuation:
-        add_error(line_num, 'description should not end with {}'.format(char))
-    desc_length = len(segments[index_desc])
-    if desc_length > 100:
-        add_error(line_num, 'description should not exceed 100 characters (currently {})'.format(desc_length))
-    # END Description
-    # START Auth
-    # values should conform to valid options only
-    auth = segments[index_auth]
-    if auth != 'No' and (not auth.startswith('`') or not auth.endswith('`')):
-        add_error(line_num, 'auth value is not enclosed with `backticks`')
-    if auth.replace('`', '') not in auth_keys:
-        add_error(line_num, '{} is not a valid Auth option'.format(auth))
-    # END Auth
-    # START HTTPS
-    # values should conform to valid options only
-    https = segments[index_https]
-    if https not in https_keys:
-        add_error(line_num, '{} is not a valid HTTPS option'.format(https))
-    # END HTTPS
-    # START CORS
-    # values should conform to valid options only
-    cors = segments[index_cors]
-    if cors not in cors_keys:
-        add_error(line_num, '{} is not a valid CORS option'.format(cors))
-    # END CORS
+            request.meta['handle_httpstatus_all'] = True
     
-        # Determine how many neighbors to use for weighting in the KNN classifier
-    if n_neighbors is None:
-        n_neighbors = int(round(math.sqrt(len(X))))
-        if verbose:
-            print('Chose n_neighbors automatically:', n_neighbors)
+        def print_items(self, lvl=None, colour=True):
+        if lvl is None:
+            items = [item for lst in self.items.values() for item in lst]
+        else:
+            items = self.items.get(lvl, [])
     
-    # Find all the faces in the image using the default HOG-based model.
-# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# See also: find_faces_in_picture_cnn.py
-face_locations = face_recognition.face_locations(image)
+        requires_project = False
+    default_settings = {'LOG_ENABLED': False,
+                        'SPIDER_LOADER_WARN_ONLY': True}
     
-    # This code finds all faces in a list of images using the CNN model.
-#
-# This demo is for the _special case_ when you need to find faces in LOTS of images very quickly and all the images
-# are the exact same size. This is common in video processing applications where you have lots of video frames
-# to process.
-#
-# If you are processing a lot of images and using a GPU with CUDA, batch processing can be ~3x faster then processing
-# single images at a time. But if you aren't using a GPU, then batch processing isn't going to be very helpful.
-#
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read the video file.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+        def short_desc(self):
+        return 'Print Scrapy version'
     
-    # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file('obama.jpg')
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    The matching from server ftp command return codes to html response codes is defined in the
+CODE_MAPPING attribute of the handler class. The key 'default' is used for any code
+that is not explicitly present among the map keys. You may need to overwrite this
+mapping if want a different behaviour than default.
+    
+            pemfile = '/etc/ssl/certs/CA.pem'
+        new_certfile = '/usr/local/share/ca-certificates/CA.crt'
+        if not os.path.exists(pemfile) or not CertUtil.file_is_same(certfile, new_certfile):
+            if os.system('cp '%s' '%s' && update-ca-certificates' % (certfile, new_certfile)) != 0:
+                xlog.warning('install root certificate failed, Please run as administrator/root/sudo')
+    
+        if len(sys.argv) > 2:
+        top_domain = sys.argv[2]
+    else:
+        top_domain = None
+    
+        CertUtil.init_ca()
+    
+    ## All tokens go to the parser (unless skip() is called in that rule)
+# on a particular 'channel'.  The parser tunes to a particular channel
+# so that whitespace etc... can go to the parser on a 'hidden' channel.
+DEFAULT_CHANNEL = 0
+    
+            self.error(nvae)
+        raise nvae
     
     
-class RadioTest(unittest.TestCase):
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
+
+    
+        def test_decode_wrong(self):
+        self.assertRaises(jose.DeserializationError, self.field.decode, 'y')
+    
+    
+class Header(jose.Header):
+    '''ACME-specific JOSE Header. Implements nonce, kid, and url.
     '''
-    Attention: Test case results depend on test case execution. The test cases
-    in this integration test class should be executed in an explicit order:
-    http://stackoverflow.com/questions/5387299/python-unittest-testcase-execution-order
-    '''
+    nonce = jose.Field('nonce', omitempty=True, encoder=jose.encode_b64jose)
+    kid = jose.Field('kid', omitempty=True)
+    url = jose.Field('url', omitempty=True)
     
+    # There are two options for replacing |today|: either, you set today to some
+# non-false value, then it is used:
+#today = ''
+# Else, today_fmt is used as the format for a strftime call.
+#today_fmt = '%B %d, %Y'
     
-def main():
-    command_stack = []
+    # generate_private_key requires cryptography>=0.5
+key = jose.JWKRSA(key=rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=BITS,
+    backend=default_backend()))
+acme = client.Client(DIRECTORY_URL, key)
     
-        print('After subclassing: ')
-    for k in RegistryHolder.REGISTRY:
-        print(k)
+        @mock.patch('certbot_apache.display_ops.display_util')
+    @certbot_util.patch_get_utility()
+    @mock.patch('certbot_apache.display_ops.logger')
+    def test_small_display(self, mock_logger, mock_util, mock_display_util):
+        mock_display_util.WIDTH = 20
+        mock_util().menu.return_value = (display_util.OK, 0)
+        self._call(self.vhosts)
     
-            path.append(start)
-        if start == end:
-            return path
-        for node in self.graph.get(start, []):
-            if node not in path:
-                newpath = self.find_path(node, end, path[:])
-                if newpath:
-                    return newpath
+    import mock
+    
+        def perform(self):
+        '''Perform a TLS-SNI-01 challenge.'''
+        if not self.achalls:
+            return []
+        # Save any changes to the configuration as a precaution
+        # About to make temporary changes to the config
+        self.configurator.save('Changes before challenge setup', True)
+    
+    # If true, 'Created using Sphinx' is shown in the HTML footer. Default is True.
+#html_show_sphinx = True
+    
+        def test_realpath(self):
+        self.assertIn('foo', self.pathmodule.realpath('foo'))
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore', DeprecationWarning)
+            self.assertIn(b'foo', self.pathmodule.realpath(b'foo'))
+    
+            self.assertEqual(posixpath.normpath(b''), b'.')
+        self.assertEqual(posixpath.normpath(b'/'), b'/')
+        self.assertEqual(posixpath.normpath(b'//'), b'//')
+        self.assertEqual(posixpath.normpath(b'///'), b'/')
+        self.assertEqual(posixpath.normpath(b'///foo/.//bar//'), b'/foo/bar')
+        self.assertEqual(posixpath.normpath(b'///foo/.//bar//.//..//.//baz'),
+                         b'/foo/baz')
+        self.assertEqual(posixpath.normpath(b'///..//./foo/.//bar'),
+                         b'/foo/bar')
+    
+            base_var_dict = {
+            'LANG': '',
+            'LC_CTYPE': '',
+            'LC_ALL': '',
+            'PYTHONCOERCECLOCALE': '',
+        }
+        base_var_dict.update(extra_vars)
+        if coerce_c_locale is not None:
+            base_var_dict['PYTHONCOERCECLOCALE'] = coerce_c_locale
+    
+            opts = get_xoptions()
+        self.assertEqual(opts, {})
+    
+        def test_initialize_twice(self):
+        '''
+        bpo-33932: Calling Py_Initialize() twice should do nothing (and not
+        crash!).
+        '''
+        out, err = self.run_embedded_interpreter('initialize_twice')
+        self.assertEqual(out, '')
+        self.assertEqual(err, '')
+    
+            out = self.get_output('-X', 'utf8', '-c', code,
+                              PYTHONIOENCODING='')
+        self.assertEqual(out.splitlines(),
+                         ['stdin: utf-8/surrogateescape',
+                          'stdout: utf-8/surrogateescape',
+                          'stderr: utf-8/backslashreplace'])
+    
+    --Pepé
+''')
+    
+    if __name__ == '__main__':
+    freeze_support()
+    test()
