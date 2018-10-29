d8c22c2769bb6c7036e8b6bf02a7fab28ea794af
SUNsung <weatherius@gmail.com>
2018-10-29 14:28:53 +0300

    Rewrite [233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py] File in: 2018-10-29 13:28:53

--- pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
+++ pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
@@ -1,68 +1,95 @@
 
         
-            def __init__(self):
-        self.name = 'pgcrypto'
-    
-        def load(self):
-        try:
-            session_data = self._cache.get(self.cache_key)
-        except Exception:
-            # Some backends (e.g. memcache) raise an exception on invalid
-            # cache keys. If this happens, reset the session. See #17810.
-            session_data = None
-        if session_data is not None:
-            return session_data
-        self._session_key = None
-        return {}
-    
-    KEY_PREFIX = 'django.contrib.sessions.cached_db'
-    
-        objects = BaseSessionManager()
-    
-        class Meta(AbstractBaseSession.Meta):
-        db_table = 'django_session'
+        # TODO: ensure that history changes.
 
     
-            if getattr(self, 'i18n', False):
-            urls = []
-            current_lang_code = translation.get_language()
-            for lang_code, lang_name in settings.LANGUAGES:
-                translation.activate(lang_code)
-                urls += self._urls(page, protocol, domain)
-            translation.activate(current_lang_code)
-        else:
-            urls = self._urls(page, protocol, domain)
     
-    from django.contrib.sites.shortcuts import get_current_site
-from django.core.paginator import EmptyPage, PageNotAnInteger
-from django.http import Http404
-from django.template.response import TemplateResponse
-from django.urls import reverse
-from django.utils.http import http_date
+def test_get_new_command():
+    new_command = get_new_command(Command('sudo apt update', match_output))
+    assert new_command == 'sudo apt list --upgradable'
     
-        def test_non_str_name(self):
-        # issue14638
-        # Treat illegal (non-str) name like no name
-        class A:
-            __name__ = 42
-        class B:
-            pass
-        adoc = pydoc.render_doc(A())
-        bdoc = pydoc.render_doc(B())
-        self.assertEqual(adoc.replace('A', 'B'), bdoc)
+    match_output = '''
+Listing... Done
+heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
+resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
+squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
+unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
+'''
     
-    ##
+    PROJ_NAME = 'you-get'
+PACKAGE_NAME = 'you_get'
     
-    def plus(a, b):
-    time.sleep(0.5*random.random())
-    return a + b
+        # Get options and arguments.
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], _short_options, _options)
+    except getopt.GetoptError as e:
+        log.wtf('''
+    [Fatal] {}.
+    Try '{} --help' for more options.'''.format(e, script_name))
     
-    # Register the adapter
-sqlite3.register_adapter(Point, adapt_point)
+    import json
+import re
+import base64
+import time
     
-    DB_FILE = 'mydb'
+    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
+    host = r1(r'host=([^&]*)', html)
+    assert host
+    type = r1(r'videofiletype=([^&]*)', html)
+    assert type
+    vid = r1(r'&stream_name=([^&]*)', html)
+    assert vid
+    dir_str = r1(r'&dir=([^&]*)', html).strip()
+    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
+    _, ext, size = url_info(url)
+    print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge = merge)
     
-        #Merging all the documents into a single PDF
-    for doc in group:
-        for p in doc.pages:
-            ap.append(p)
+            link_list = []
+    
+    site_info = 'Douban.com'
+download = douban_download
+download_playlist = playlist_not_supported('douban')
+
+    
+        sd_urls = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'sd_src_no_ratelimit:'([^']*)'', html)
+    ]))
+    hd_urls = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'hd_src_no_ratelimit:'([^']*)'', html)
+    ]))
+    urls = hd_urls if hd_urls else sd_urls
+    
+        #Writing names of all directories inside 'pages' to a list
+    for os_dir in os.listdir(loc):
+        oslist.append(os_dir)
+    
+            for project in RangeQuerySetWrapperWithProgressBar(orm.Project.objects.all()):
+            orm.Environment.objects.filter(
+                project_id=project.id, organization_id__isnull=True
+            ).update(organization_id=project.organization_id)
+    
+            # Changing field 'Environment.organization_id'
+        db.alter_column(
+            'sentry_environment',
+            'organization_id',
+            self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)
+        )
+    
+            for model in (
+            orm['sentry.ApiAuthorization'], orm['sentry.ApiGrant'], orm['sentry.ApiKey'],
+            orm['sentry.ApiToken'],
+        ):
+            queryset = model.objects.all()
+            for obj in RangeQuerySetWrapperWithProgressBar(queryset):
+                if not obj.scope_list:
+                    handler = make_handler(obj.scopes)
+                    model.objects.filter(
+                        id=obj.id,
+                    ).update(
+                        scope_list=[k for k, v in six.iteritems(handler) if v],
+                    )
