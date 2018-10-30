81b40d3acc9fc7ef27f74f16ea2b4e6146aa4649
SUNsung <weatherius@gmail.com>
2018-10-30 03:01:58 +0300

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2018-10-30 02:01:57

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,18 +1,137 @@
 
         
-        puts 'Deduping #{links.size} links...'
+        def graceful_require
+  Jekyll::External.require_with_graceful_fail('json')
+  JSON.pretty_generate(DATA)
+end
     
-      #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
+      all_files       = `git ls-files -z`.split('\x0')
+  s.files         = all_files.grep(%r!^(exe|lib|rubocop)/|^.rubocop.yml$!)
+  s.executables   = all_files.grep(%r!^exe/!) { |f| File.basename(f) }
+  s.bindir        = 'exe'
+  s.require_paths = ['lib']
+    
+              theme.create!
+          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
+                             ' is ready for you in #{theme.path.to_s.cyan}!'
+          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
+        end
+        # rubocop:enable Metrics/AbcSize
+      end
+    end
+  end
+end
+
+    
+        if f.keg_only?
+      keg_site_packages = f.opt_prefix/'lib/python2.7/site-packages'
+      unless Language::Python.in_sys_path?('python', keg_site_packages)
+        s = <<-EOS.undent
+          If you need Python to find bindings for this keg-only formula, run:
+            echo #{keg_site_packages} >> #{homebrew_site_packages/f.name}.pth
+        EOS
+        s += instructions unless Language::Python.reads_brewed_pth_files?('python')
+      end
+      return s
+    end
+    
+          open_dry_run_modal(agent)
+      find('.dry-run-event-sample').click
+      within(:css, '.modal .builder') do
+        expect(page).to have_text('http://xkcd.com/')
+      end
+      click_on('Dry Run')
+      expect(page).to have_text('Biologists play reverse')
+      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
+    end
+    
+      it 'renders the import form' do
+    visit new_scenario_imports_path
+    expect(page).to have_text('Import a Public Scenario')
+  end
+    
+          it 'generates a DOT script' do
+        expect(agents_dot(@agents)).to match(%r{
+          \A
+          digraph \x20 'Agent \x20 Event \x20 Flow' \{
+            node \[ [^\]]+ \];
+            edge \[ [^\]]+ \];
+            (?<foo>\w+) \[label=foo\];
+            \k<foo> -> (?<bar1>\w+) \[style=dashed\];
+            \k<foo> -> (?<bar2>\w+) \[color='\#999999'\];
+            \k<bar1> \[label=bar1\];
+            \k<bar2> \[label=bar2,style='rounded,dashed',color='\#999999',fontcolor='\#999999'\];
+            \k<bar2> -> (?<bar3>\w+) \[style=dashed,color='\#999999'\];
+            \k<bar3> \[label=bar3\];
+          \}
+          \z
+        }x)
+      end
+    
+      describe '#style_colors' do
+    it 'returns a css style-formated version of the scenario foreground and background colors' do
+      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
+    end
+    
+          expect(data[:agents][guid_order(agent_list, :jane_weather_agent)]).not_to have_key(:propagate_immediately) # can't receive events
+      expect(data[:agents][guid_order(agent_list, :jane_rain_notifier_agent)]).not_to have_key(:schedule) # can't be scheduled
+    end
+    
+        private
+    
+        def build_page(path)
+      response = request_one url_for(path)
+      result = handle_response(response)
+      yield result if block_given?
+      result
+    end
+    
+        def process_response?(response)
+      if response.error?
+        raise <<~ERROR
+          Error status code (#{response.code}): #{response.return_message}
+          #{response.url}
+          #{JSON.pretty_generate(response.headers).slice(2..-3)}
+        ERROR
+      elsif response.blank?
+        raise 'Empty response body: #{response.url}'
+      end
+    
+              node['data-language'] = 'typescript' if node['path'].try(:ends_with?, '.ts')
+          node['data-language'] = 'html' if node['path'].try(:ends_with?, '.html')
+          node['data-language'] = 'css' if node['path'].try(:ends_with?, '.css')
+          node['data-language'] = 'js' if node['path'].try(:ends_with?, '.js')
+          node['data-language'] = 'json' if node['path'].try(:ends_with?, '.json')
+          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip if node['language']
+          node['data-language'] ||= 'typescript' if node.content.start_with?('@')
+    
+      # add junk directories
+  attr_accessor :junk_directories
+    
+          dprint( ['RECV', phdr, stype, info].inspect )
+      case stype
+      when IAX_SUBTYPE_HANGUP
+        self.state = :hangup
+        self.client.send_ack(self)
+      when IAX_SUBTYPE_LAGRQ
+        # Lagrps echo the timestamp
+        self.client.send_lagrp(self, tstamp)
+      when IAX_SUBTYPE_ACK
+        # Nothing to do here
+      when IAX_SUBTYPE_PING
+        # Pongs echo the timestamp
+        self.client.send_pong(self, tstamp)
+      when IAX_SUBTYPE_PONG
+        self.client.send_ack(self)
+      else
+        dprint( ['RECV-QUEUE', phdr, stype, info].inspect )
+        self.queue.push( [phdr, stype, info ] )
+      end
+    
+              data = connection.get_once(length, timeout)
+          unless data && data.length == length
+            raise ::RuntimeError, 'Kerberos Client: failed to read response'
+          end
     
     module Rex
   module Proto
@@ -27,34 +146,45 @@
           #   @option [String] :data
           attr_accessor :elements
     
-              # @!attribute type
-          #   @return [Integer] The algorithm used to generate the checksum
-          attr_accessor :type
-          # @!attribute checksum
-          #   @return [String] The checksum itself
-          attr_accessor :checksum
+              # Encodes a Rex::Proto::Kerberos::Model::EncryptionKey into an
+          # ASN.1 String
+          #
+          # @return [String]
+          def encode
+            elems = []
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_type], 0, :CONTEXT_SPECIFIC)
+            elems << OpenSSL::ASN1::ASN1Data.new([encode_value], 1, :CONTEXT_SPECIFIC)
+            seq = OpenSSL::ASN1::Sequence.new(elems)
     
-                self
-          end
+      gem 'bacon'
+  gem 'mocha'
+  gem 'mocha-on-bacon'
+  gem 'prettybacon'
+  gem 'webmock'
     
-            opts.on_tail('-h', '--help', '-H', 'Display this help message.') do
-          puts opts
-          exit
-        end
+    ENV['COCOAPODS_DISABLE_STATS'] = 'true'
+
     
-          attr_reader :key, :default, :options
-    
-          # Internal use only.
-      def peek(key, default=nil, &block)
-        value = fetch_for(key, default, &block)
-        while callable_without_parameters?(value)
-          value = (values[key] = value.call)
-        end
-        value
+          # Checks whether this case statement has an `else` branch.
+      #
+      # @return [Boolean] whether the `case` statement has an `else` branch
+      def else?
+        loc.else
       end
-    
-        def pattern_path(path)
-      return ::File.join(LOGSTASH_HOME, 'patterns', path)
     end
+  end
+end
+
     
-    require 'bootstrap/environment'
+    module RuboCop
+  module AST
+    # A node extension for `for` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `for` nodes within RuboCop.
+    class ForNode < Node
+      # Returns the keyword of the `for` statement as a string.
+      #
+      # @return [String] the keyword of the `until` statement
+      def keyword
+        'for'
+      end
