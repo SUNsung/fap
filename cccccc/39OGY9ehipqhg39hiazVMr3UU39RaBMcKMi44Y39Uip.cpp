95a710f64c4690f1dfea0327068ad2e9a45801ab
SUNsung <weatherius@gmail.com>
2018-10-30 22:14:12 +0300

    Rewrite [39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp] File in: 2018-10-30 21:14:12

--- cccccc/39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp
+++ cccccc/39OGY9ehipqhg39hiazVMr3UU39RaBMcKMi44Y39Uip.cpp
@@ -1,243 +1,166 @@
 
         
-        
-    {    RNG &rng = ts->get_rng();
-    int modelPoints = cvtest::randInt(rng);
-    CvSize modelSize = cvSize(2, modelPoints);
-    int maxBasicSolutions = cvtest::randInt(rng);
-    BareModelEstimator modelEstimator(modelPoints, modelSize, maxBasicSolutions);
-    checkSubsetResult = modelEstimator.checkSubsetPublic(&_input, usedPointsCount, checkPartialSubsets);
-}
-    
-    #ifndef OPENCV_CUDA_WARP_REDUCE_HPP__
-#define OPENCV_CUDA_WARP_REDUCE_HPP__
-    
-    CV_EXPORTS_W void absdiff(InputArray src1, Scalar src2, OutputArray dst);
-    
-    #ifndef OPENGL_NOLOAD_STYLE_HPP
-#define OPENGL_NOLOAD_STYLE_HPP
-    
-    #endif // __OPENCV_CORE_OCL_RUNTIME_COMMON_HPP__
-
-    
-    #include <THPP/tensors/THTensor.hpp>
-    
-      template<typename T>
-  void allGatherT(std::vector<at::Tensor>& output,
-                  at::Tensor& input, THDGroup group_id);
-    
-    #define THCPStorage TH_CONCAT_3(THCP,Real,Storage)
-#define THCPStorageStr TH_CONCAT_STRING_3(torch.cuda.,Real,Storage)
-#define THCPStorageClass TH_CONCAT_3(THCP,Real,StorageClass)
-#define THCPStorage_(NAME) TH_CONCAT_4(THCP,Real,Storage_,NAME)
-    
-    // WorkloadStats is used to track per request timing for different states
-// of the VM.  At the entrypoint to a change of vm state a WorkloadStats object
-// should be made to guard the state change with appropriate timers and
-// counters.
-//
-// The states tracked are:
-//  - In a request (this is a superset of the interpreter state)
-//  - In the interpreter through Dispatch, or DispatchBB (interpOne disregarded)
-//  - In the JIT (currently tracks time inside the translate routine)
-//
-// Note the time in the TC is not tracked.  This is roughly:
-//   Time in request - Time in interp
-//
-// This gives us the relative interp time formula of:
-//   Relative interp time = Time in interp / Time in request
-struct WorkloadStats final {
-  enum State {
-    InRequest,
-    // -> InInterp   Okay (entering Dispatch loop)
-    // -> InTrans    Okay (entering translate)
-    InInterp,
-    // -> InRequest  Okay (leaving the dispatch loop)
-    // -> InTrans    Okay (entering translate)
-    InTrans,
-    // -> InRequest  Okay (leaving translate)
-    // -> InInterp   Okay (leaving translate)
-  };
+        void CostAnalyzer::PreprocessCosts() {
+  for (int i = 0; i < op_perf_.op_performance_size(); i++) {
+    OpPerformance* perf = op_perf_.mutable_op_performance(i);
+    const OpPerformance& analytical = op_perf_analytical_.op_performance(i);
+    perf->set_compute_time(analytical.compute_time());
+    perf->set_memory_time(analytical.memory_time());
+    double measured_cost = perf->compute_cost();
+    }
     }
     
-      auto const available = dst.thisAvailable && src.thisAvailable;
-  if (available != dst.thisAvailable) {
-    changed = true;
-    dst.thisAvailable = available;
+    REGISTER_OP('Invalid')
+    .Attr('invalid attr: int32')  // invalid since the name has a space.
+    .Doc(R'doc(
+An op to test that invalid ops do not successfully generate invalid python code.
+)doc');
+    
+    #endif  // TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_H_
+
+    
+    
+    {
+    {}  // namespace cuda
+}  // namespace stream_executor
+
+    
+    namespace atom {
+    }
+    
+      // WindowListObserver:
+  void OnWindowAllClosed() override;
+    
+    
+    {}  // namespace api
+    
+    namespace atom {
+    }
+    
+    template <>
+struct Converter<base::trace_event::TraceConfig> {
+  static bool FromV8(v8::Isolate* isolate,
+                     v8::Local<v8::Value> val,
+                     base::trace_event::TraceConfig* out) {
+    Dictionary options;
+    if (!ConvertFromV8(isolate, val, &options))
+      return false;
+    std::string category_filter, trace_options;
+    if (!options.Get('categoryFilter', &category_filter) ||
+        !options.Get('traceOptions', &trace_options))
+      return false;
+    *out = base::trace_event::TraceConfig(category_filter, trace_options);
+    return true;
   }
-    
-    
-    {private:
-  bool m_skipTop{false};
-  bool m_skipInlined{false};
-  bool m_withSelf{false};
-  bool m_withThis{false};
-  bool m_withMetadata{false};
-  bool m_withPseudoMain{false};
-  bool m_withArgValues{true};
-  bool m_withArgNames{false};
-  int m_limit{0};
-  VMParserFrame* m_parserFrame{nullptr};
-  c_WaitableWaitHandle* m_fromWaitHandle{nullptr};
 };
     
-    thread_local std::atomic<size_t> g_req_num;
-__thread size_t t_req_num; // snapshot thread-local copy of g_req_num;
-__thread size_t t_gc_num; // nth collection in this request.
-__thread bool t_enable_samples;
-__thread int64_t t_trigger;
-__thread int64_t t_trigger_allocated;
-__thread int64_t t_req_age;
-__thread MemoryUsageStats t_pre_stats;
-    
-    	if (streqi(opt.c_str(), L'ALL'))
-	{
-		userOnly = false;
-	}
-	else if (streqi(opt.c_str(), L'USER'))
-	{
-		userOnly = true;
-	}
-	else
-	{
-		MessageBox(NULL, L'Unrecognized option for /REGISTER or /UNREGISTER. Must be either ALL or USER.', MB_TITLE, MB_OK);
-		exit(1);
-	}
-    
-    
-    {    if (options->Has(String::NewSymbol('target_path_id')) ||
-        options->Get(String::NewSymbol('target_path_id'))->IsInt32()) {
-      target_path_id = (int)(options->Get(
-          String::NewSymbol('target_path_id'))->ToInt32()->Value());
-    }
-  }
-    
-      StatisticsJni::StatisticsJni(std::shared_ptr<Statistics> stats,
-      const std::set<uint32_t> ignore_histograms) : StatisticsImpl(stats, false),
-      m_ignore_histograms(ignore_histograms) {
-  }
-    
-    void SyncPoint::EnableProcessing() {
-  impl_->EnableProcessing();
+    GlobalShortcut::GlobalShortcut(v8::Isolate* isolate) {
+  Init(isolate);
 }
     
-         using Logger::SetInfoLogLevel;
-     using Logger::GetInfoLogLevel;
-     // Write an entry to the log file with the specified format.
-     virtual void Logv(const char* format, va_list ap);
-     // Write an entry to the log file with the specified log level
-     // and format.  Any log with level under the internal log level
-     // of *this (see @SetInfoLogLevel and @GetInfoLogLevel) will not be
-     // printed.
-     virtual void Logv(const InfoLogLevel log_level,
-         const char* format, va_list ap);
+    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_browser_in_app_purchase, Initialize)
+
+    
+    #include 'atom/browser/api/event_emitter.h'
+#include 'atom/browser/mac/in_app_purchase.h'
+#include 'atom/browser/mac/in_app_purchase_observer.h'
+#include 'atom/browser/mac/in_app_purchase_product.h'
+#include 'native_mate/handle.h'
     
     
-    {    // Do it
-    std::string contents;
-    Status s = ReadFileToString(Env::Default(), fname, &contents);
-    ASSERT_TRUE(s.ok()) << s.ToString();
-    for (int i = 0; i < bytes_to_corrupt; i++) {
-      contents[i + offset] ^= 0x80;
-    }
-    s = WriteStringToFile(Env::Default(), contents, fname);
-    ASSERT_TRUE(s.ok()) << s.ToString();
-    Options options;
-    EnvOptions env_options;
-    ASSERT_NOK(VerifySstFileChecksum(options, env_options, fname));
-  }
+    {  delegate.Get('isCommandIdChecked', &is_checked_);
+  delegate.Get('isCommandIdEnabled', &is_enabled_);
+  delegate.Get('isCommandIdVisible', &is_visible_);
+  delegate.Get('getAcceleratorForCommandId', &get_accelerator_);
+  delegate.Get('executeCommand', &execute_command_);
+  delegate.Get('menuWillShow', &menu_will_show_);
+}
     
-      /** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** ** **
-   * Finds the last occurrence of any character in `that` in this string.
-   * \note Equivalent to `find_last_of(that.data(), size(), that.size())`
+    // static
+void Net::BuildPrototype(v8::Isolate* isolate,
+                         v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'Net'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetProperty('URLRequest', &Net::URLRequest);
+}
+    
+    using atom::api::PowerMonitor;
+    
+    #include 'atom/browser/api/trackable_object.h'
+#include 'atom/browser/ui/tray_icon.h'
+#include 'atom/browser/ui/tray_icon_observer.h'
+#include 'native_mate/handle.h'
+    
+      /*
+   * @brief the uri used to begin POSTing carve data
+   *
+   * This endpoint should negotiate the details of the carve, as well
+   * as give the client a session id used to continue POSTing the data.
    */
-  template <std::size_t M>
-  constexpr std::size_t find_last_of(
-      const BasicFixedString<Char, M>& that) const noexcept {
-    return find_last_of(that, size_);
+  std::string startUri_;
+    
+      auto it = doc.FindMember(kLoggerKey);
+  if (it != doc.MemberEnd()) {
+    doc.EraseMember(it);
   }
     
-      ~HazptrSWMRSet() {
-    auto p = head_.load();
-    while (p) {
-      auto next = p->next_.load();
-      delete p;
-      p = next;
-    }
-  }
+      // Generate content to update/add to the config.
+  std::string content;
+  auto s = readFile(kTestDataPath + 'test_parse_items.conf', content);
+  EXPECT_TRUE(s.ok());
+  std::map<std::string, std::string> config;
+  config['awesome'] = content;
     
-    namespace folly {
-    }
     
-    #ifndef HAZPTR_ONE_DOMAIN
-#define HAZPTR_ONE_DOMAIN false
-#endif
-    
-    #include <string>
-    
-      virtual void truncate(int64_t length) CXX11_OVERRIDE;
-    
-    #endif // D_ABSTRACT_OPTION_HANDLER_H
+    {
+    {  EXPECT_TRUE(doc.HasMember('custom_nested_json'));
+  EXPECT_FALSE(Flag::getValue('custom_nested_json').empty());
+  EXPECT_EQ(R'raw({'foo':1,'bar':'baz'})raw',
+            Flag::getValue('custom_nested_json'));
+}
+}
 
     
-    void AnnounceList::shuffle()
-{
-  for (const auto& tier : tiers_) {
-    auto& urls = tier->urls;
-    std::shuffle(std::begin(urls), std::end(urls),
-                 *SimpleRandomizer::getInstance());
-  }
-}
+    #include 'osquery/tests/test_util.h'
     
-      /**
-   * Returns announce event, such as started, stopped, completed, etc.
-   */
-  const char* getEventString() const;
-    
-    void AnnounceTier::nextEventIfAfterStarted()
-{
-  switch (event) {
-  case STOPPED:
-    event = HALTED;
-    break;
-  case COMPLETED:
-    event = SEEDING;
-    break;
-  default:
-    break;
-  }
-}
-    
-    #include <string>
-#include <deque>
-    
-    // DiskwriterFactory class template to create DiskWriter derived
-// object, ignoring filename.
-template <class DiskWriterType>
-class AnonDiskWriterFactory : public DiskWriterFactory {
-public:
-  AnonDiskWriterFactory() = default;
-  virtual ~AnonDiskWriterFactory() = default;
+    TEST_F(QueryTests, test_add_and_get_current_results) {
+  // Test adding a 'current' set of results to a scheduled query instance.
+  auto query = getOsqueryScheduledQuery();
+  auto cf = Query('foobar', query);
+  uint64_t counter = 128;
+  auto status = cf.addNewResults(getTestDBExpectedResults(), 0, counter);
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(status.toString(), 'OK');
+  EXPECT_EQ(counter, 0UL);
     }
     
-    #endif // DOWNLOAD_EVENT_LISTENER_H
-
-    
-    class AuthConfig;
-    
-    
-    {};
-
-    
-        if (nodePtr == nullptr)
-        return nullptr;
-    
-    namespace facebook {
+    /**
+ * @brief A performance state structure for an autoloaded extension or worker.
+ *
+ * A watcher thread will continue to check the performance state, and keep a
+ * last-checked snapshot for each autoloaded extension and worker process.
+ */
+struct PerformanceState {
+  /// A counter of how many intervals the process exceeded performance limits.
+  size_t sustained_latency;
+  /// The last checked user CPU time.
+  size_t user_time;
+  /// The last checked system CPU time.
+  size_t system_time;
+  /// A timestamp when the process/worker was last created.
+  size_t last_respawn_time;
     }
     
-    // Creates a strong reference from a raw pointer, assuming that it points to a
-// freshly-created object. See the documentation for RefPtr for usage.
-template <typename T>
-static inline RefPtr<T> adoptRef(T* ptr) {
-  return RefPtr<T>::adoptRef(ptr);
-}
+        if (queries.IsObject()) {
+      for (const auto& query_entry : queries.GetObject()) {
+        if (!query_entry.name.IsString() || !query_entry.value.IsString()) {
+          return Status(1, 'Distributed query is not a string');
+        }
+    }
+    }
+    
+    static const size_t kINotifyMaxEvents = 512;
+static const size_t kINotifyEventSize =
+    sizeof(struct inotify_event) + (NAME_MAX + 1);
+static const size_t kINotifyBufferSize =
+    (kINotifyMaxEvents * kINotifyEventSize);
