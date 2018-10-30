90be1ca29a8bc31059acd82b44cbdbdfe682e7c6
SUNsung <weatherius@gmail.com>
2018-10-31 02:40:30 +0300

    Rewrite [35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb] File in: 2018-10-31 01:40:30

--- ruby/35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb
+++ ruby/35.7EyvTrymmo35ALpQz4ed5gM35.e2rv4fCm.w35XER.rb
@@ -1,167 +1,146 @@
 
         
-                      # Notify blocked threads that EventMachine has started or shutdown
-              EM.schedule { @started_event.set }
-              EM.add_shutdown_hook { @stopped_event.set }
-    
-    module Kramdown
-  module Parser
-    class SmartyPants < Kramdown::Parser::Kramdown
-      def initialize(source, options)
-        super
-        @block_parsers = [:block_html, :content]
-        @span_parsers =  [:smart_quotes, :html_entity, :typographic_syms, :span_html]
+            describe 'A' do
+      it 'converts floating point argument as [-]0xh.hhhhp[+-]dd and use uppercase X and P' do
+        format('%A', 196).should == '0X1.88P+7'
+        format('%A', -196).should == '-0X1.88P+7'
+        format('%A', 196.1).should == '0X1.8833333333333P+7'
+        format('%A', 0.01).should == '0X1.47AE147AE147BP-7'
+        format('%A', -0.01).should == '-0X1.47AE147AE147BP-7'
       end
     
-        def arg_is_present?(args, deprecated_argument, message)
-      deprecation_message(message) if args.include?(deprecated_argument)
-    end
+      it 'calls #to_int on seed' do
+    srand(3.8)
+    srand.should == 3
     
-    module Jekyll
-  module Drops
-    class UnifiedPayloadDrop < Drop
-      mutable true
-    
-        def backup?(entry)
-      entry[-1..-1] == '~'
-    end
-    
-    desc 'Increment the version number of this gem, after generating new Swift API'
-lane :bump do |options|
-  verify_env_variables
-  ensure_git_branch(branch: 'master')
-  ensure_git_status_clean
-    
-        options[:attribution] = <<-HTML
-      &copy; 2018 The TensorFlow Authors. All rights reserved.<br>
-      Licensed under the Creative Commons Attribution License 3.0.<br>
-      Code samples licensed under the Apache 2.0 License.
-    HTML
-    
-            if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-        def relative_path_from(url)
-      self.class.parse(url).relative_path_to(self)
-    end
+      it 'returns true when passed ?e if the argument is a file' do
+    Kernel.test(?e, @file).should == true
   end
-end
-
     
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
+      # More readable inspect that only shows the url and resources
+  # @return [String]
+  def inspect
+    resources_str = resources.keys.map{|r| r.inspect }.join ', '
     
-              entries << [name, node['id'], type]
-        end
+    =begin
+   +-------------+---------------+-------------------------------------+
+   | VALUE       | Name          | Description                         |
+   +-------------+---------------+-------------------------------------+
+   | 0x01        | Hangup        | The call has been hungup at the     |
+   |             |               | remote end                          |
+   |             |               |                                     |
+   | 0x02        | Reserved      | Reserved for future use             |
+   |             |               |                                     |
+   | 0x03        | Ringing       | Remote end is ringing (ring-back)   |
+   |             |               |                                     |
+   | 0x04        | Answer        | Remote end has answered             |
+   |             |               |                                     |
+   | 0x05        | Busy          | Remote end is busy                  |
+   |             |               |                                     |
+   | 0x06        | Reserved      | Reserved for future use             |
+   |             |               |                                     |
+   | 0x07        | Reserved      | Reserved for future use             |
+   |             |               |                                     |
+   | 0x08        | Congestion    | The call is congested               |
+   |             |               |                                     |
+   | 0x09        | Flash Hook    | Flash hook                          |
+   |             |               |                                     |
+   | 0x0a        | Reserved      | Reserved for future use             |
+   |             |               |                                     |
+   | 0x0b        | Option        | Device-specific options are being   |
+   |             |               | transmitted                         |
+   |             |               |                                     |
+   | 0x0c        | Key Radio     | Key Radio                           |
+   |             |               |                                     |
+   | 0x0d        | Unkey Radio   | Unkey Radio                         |
+   |             |               |                                     |
+   | 0x0e        | Call Progress | Call is in progress                 |
+   |             |               |                                     |
+   | 0x0f        | Call          | Call is proceeding                  |
+   |             | Proceeding    |                                     |
+   |             |               |                                     |
+   | 0x10        | Hold          | Call is placed on hold              |
+   |             |               |                                     |
+   | 0x11        | Unhold        | Call is taken off hold              |
+   +-------------+---------------+-------------------------------------+
+=end
     
-          class << self
-        # Mark a given block of code as a 'busy' block of code, which will
-        # register a SIGINT handler for the duration of the block. When a
-        # SIGINT occurs, the `sig_callback` proc will be called. It is up
-        # to the callback to behave properly and exit the application.
-        def busy(sig_callback)
-          register(sig_callback)
-          return yield
-        ensure
-          unregister(sig_callback)
-        end
-    
-              # Parse the options
-          argv = parse_options(opts)
-          return if !argv
-          if argv.empty? || argv.length > 2
-            raise Vagrant::Errors::CLIInvalidUsage,
-              help: opts.help.chomp
+              # Encodes the auth_time field
+          #
+          # @return [String]
+          def encode_auth_time
+            [auth_time].pack('N')
           end
     
-        if extension_pathname.readable?
-      ENV['BUNDLE_GEMFILE'] = extension_pathname.to_path
-      break
+                checksum = cipher[0, 16]
+            data = cipher[16, cipher.length - 1]
+    
+              # Encodes the data
+          #
+          # @return [OpenSSL::ASN1::OctetString]
+          def encode_data(data)
+            OpenSSL::ASN1::OctetString.new(data)
+          end
+        end
+      end
     end
   end
 end
     
-    # Copyright (C) 2008 Rapid7, Inc.
+              # Rex::Proto::Kerberos::Model::Checksum decoding isn't supported
+          #
+          # @raise [NotImplementedError]
+          def decode(input)
+            raise ::NotImplementedError, 'Checksum decoding not supported'
+          end
     
+              # Encodes the Rex::Proto::Kerberos::Model::Element into an ASN.1 String. This
+          # method has been designed to be overridden by subclasses.
+          #
+          # @raise [NoMethodError]
+          def encode
+            raise ::NoMethodError, 'Method designed to be overridden'
+          end
+        end
+      end
+    end
+  end
+end
     
-    {	if ln =~ /;(read|write)_(handle|filename)=/
-		parts = ln.split(' ')
-		if (parts[0] == 'mov')
-			parts2 = parts[2].split('=')
-			label = parts2[0]
-			label.slice!(0,1)
-			old = parts2[1]
-			new = addrs[label]
-			#puts '%32s: %s -> %x' % [label, old, new]
-			replaces << [label, old, new.to_s(16)]
-		end
-	end
-}
+              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_asn1_pvno(input)
+            input.value[0].value.to_i
+          end
     
-    vers.each do |ver|
-  case ver
-  when '6.1.4'
-    __NR_execve      = 7
-    __NR_getpeername = 211
-    __NR_accept      = 237
-    __NR_listen      = 240
-    __NR_bind        = 242
-    __NR_socket      = 243
-    __NR_connect     = 244
-    __NR_close       = 278
-    __NR_kfcntl      = 658
+            def add_arg_offense(arg, type)
+          add_offense(arg.parent, location: arg.source_range,
+                                  message: format(MSG,
+                                                  type: type.to_s.capitalize))
+        end
     
-    class Parser
+            expect(cop.highlights).to eq([close])
+        expect(cop.messages)
+          .to eq([described_class::SAME_LINE_MESSAGE])
+      end
     
-    module RuboCop
-  module Cop
-    module Lint
-      # This cop checks that there are no repeated conditions
-      # used in case 'when' expressions.
+          # Checks whether the `if` is an `elsif`. Parser handles these by nesting
+      # `if` nodes in the `else` branch.
       #
-      # @example
-      #
-      #   # bad
-      #
-      #   case x
-      #   when 'first'
-      #     do_something
-      #   when 'first'
-      #     do_something_else
-      #   end
-      #
-      # @example
-      #
-      #   # good
-      #
-      #   case x
-      #   when 'first'
-      #     do_something
-      #   when 'second'
-      #     do_something_else
-      #   end
-      class DuplicateCaseCondition < Cop
-        MSG = 'Duplicate `when` condition detected.'.freeze
+      # @return [Boolean] whether the node is an `elsif`
+      def elsif?
+        keyword == 'elsif'
+      end
     
-            def on_block(node)
-          on_body_of_reduce(node) do |body|
-            void_next = body.each_node(:next).find do |n|
-              n.children.empty? && parent_block_node(n) == node
-            end
-    
-    module RuboCop
-  module AST
-    # A node extension for `def` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `def` nodes within RuboCop.
-    class DefNode < Node
-      include ParameterizedNode
-      include MethodIdentifierPredicates
+          # Custom destructuring method. This is used to normalize the branches
+      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
+      #
+      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
+      def node_parts
+        [self, self]
+      end
+    end
+  end
+end
