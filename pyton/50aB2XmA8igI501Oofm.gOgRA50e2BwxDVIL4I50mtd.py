32ca18bda19ddc57e6a3398c7e07f4321110ed76
SUNsung <weatherius@gmail.com>
2018-10-30 20:20:13 +0300

    Rewrite [50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py] File in: 2018-10-30 19:20:12

--- pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
+++ pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
@@ -1,243 +1,128 @@
 
         
-            Returns:
-      The likelihood of z under the model.
-    '''
-    if z is None:
-      z = self.sample
-    
-    The main data structure being passed around is a dataset.  This is a dictionary
-of data dictionaries.
-    
-      W,b = init_linear(in_size, out_size, do_bias=do_bias, alpha=alpha,
-                    normalized=normalized, name=name, collections=collections)
-    
-      softmax_weights = sess.run(t['softmax_weights'])
-  fname = FLAGS.save_dir + '/embeddings_softmax.npy'
-  with tf.gfile.Open(fname, mode='w') as f:
-    np.save(f, softmax_weights)
-  sys.stderr.write('Finished softmax weights\n')
-    
-        ## Calculate the Advantages, A(s,a) = Q(s,a) - \hat{V}(s).
-    advantages = []
-    for t in xrange(FLAGS.sequence_length):
-      log_probability = log_probs_list[t]
-      cum_advantage = tf.zeros(shape=[FLAGS.batch_size])
-    
-          elif FLAGS.generator_model == 'seq2seq_nas':
-        # Encoder.
-        gen_encoder_variable_maps = variable_mapping.gen_encoder_seq2seq_nas(
-            hparams)
-        gen_encoder_init_saver = tf.train.Saver(
-            var_list=gen_encoder_variable_maps)
-        # Decoder.
-        gen_decoder_variable_maps = variable_mapping.gen_decoder_seq2seq_nas(
-            hparams)
-        gen_decoder_init_saver = tf.train.Saver(
-            var_list=gen_decoder_variable_maps)
-        init_savers['gen_encoder_init_saver'] = gen_encoder_init_saver
-        init_savers['gen_decoder_init_saver'] = gen_decoder_init_saver
-    
-        auto_json = args.data and not args.form
-    if args.json or auto_json:
-        default_headers['Accept'] = JSON_ACCEPT
-        if args.json or (auto_json and args.data):
-            default_headers['Content-Type'] = JSON_CONTENT_TYPE
-    
-        # Try to resolve the right lexer.
-    lexer = None
-    for mime_type in mime_types:
-        try:
-            lexer = pygments.lexers.get_lexer_for_mimetype(mime_type)
-            break
-        except ClassNotFound:
-            pass
-    else:
-        for name in lexer_names:
-            try:
-                lexer = pygments.lexers.get_lexer_by_name(name)
-            except ClassNotFound:
-                pass
-    
-    # sdist
-if 'bdist_wheel' not in sys.argv:
-    try:
-        # noinspection PyUnresolvedReferences
-        import argparse
-    except ImportError:
-        install_requires.append('argparse>=1.2.1')
-    
-        config['implicit_content_type'] = 'form'
-    config.save()
-    config.load()
-    assert 'implicit_content_type' not in config
-    assert config['default_options'] == ['--form']
-    
-    
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
-    
-        @pytest.mark.parametrize(
-        argnames=['mime', 'explicit_json', 'body', 'expected_lexer_name'],
-        argvalues=[
-            ('application/json',     False, None, 'JSON'),
-            ('application/json+foo', False, None, 'JSON'),
-            ('application/foo+json', False, None, 'JSON'),
-            ('application/json-foo', False, None, 'JSON'),
-            ('application/x-json',   False, None, 'JSON'),
-            ('foo/json',             False, None, 'JSON'),
-            ('foo/json+bar',         False, None, 'JSON'),
-            ('foo/bar+json',         False, None, 'JSON'),
-            ('foo/json-foo',         False, None, 'JSON'),
-            ('foo/x-json',           False, None, 'JSON'),
-            ('application/vnd.comverge.grid+hal+json', False, None, 'JSON'),
-            ('text/plain',           True, '{}', 'JSON'),
-            ('text/plain',           True, 'foo', 'Text only'),
-        ]
-    )
-    def test_get_lexer(self, mime, explicit_json, body, expected_lexer_name):
-        lexer = get_lexer(mime, body=body, explicit_json=explicit_json)
-        assert lexer is not None
-        assert lexer.name == expected_lexer_name
-    
-    ESTIMATORS = {
-    'dummy': DummyClassifier(),
-    'random_forest': RandomForestClassifier(n_estimators=100,
-                                            max_features='sqrt',
-                                            min_samples_split=10),
-    'extra_trees': ExtraTreesClassifier(n_estimators=100,
-                                        max_features='sqrt',
-                                        min_samples_split=10),
-    'logistic_regression': LogisticRegression(),
-    'naive_bayes': MultinomialNB(),
-    'adaboost': AdaBoostClassifier(n_estimators=10),
+        
+    {    # Options that need a file parameter
+    'download-archive': ['--require-parameter'],
+    'cookies': ['--require-parameter'],
+    'load-info': ['--require-parameter'],
+    'batch-file': ['--require-parameter'],
 }
     
+    versions_info = json.load(open('update/versions.json'))
+if 'signature' in versions_info:
+    del versions_info['signature']
     
-def plot_batch_errors(all_errors, n_features, all_batch_sizes, data):
-    plt.figure()
-    plot_results(all_batch_sizes, all_errors['pca'], label='PCA')
-    plot_results(all_batch_sizes, all_errors['ipca'], label='IncrementalPCA')
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. batch_size for n_components %i\n \
-                 LFW, size %i x %i' % (
-                 n_features, data.shape[0], data.shape[1]))
-    plt.xlabel('Batch size')
-    plt.ylabel('Mean absolute error')
+        with open('supportedsites.html', 'w', encoding='utf-8') as sitesf:
+        sitesf.write(template)
     
-        for n_samples in sample_sizes:
-        X = random_state.rand(n_samples, 300)
+    import io
+import sys
+import re
     
-    plt.figure('scikit-learn Ward's method benchmark results')
-plt.imshow(np.log(ratio), aspect='auto', origin='lower')
-plt.colorbar()
-plt.contour(ratio, levels=[1, ], colors='k')
-plt.yticks(range(len(n_features)), n_features.astype(np.int))
-plt.ylabel('N features')
-plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
-plt.xlabel('N samples')
-plt.title('Scikit's time, in units of scipy time (log)')
-plt.show()
+    DESCRIPTION = 'YouTube video downloader'
+LONG_DESCRIPTION = 'Command-line program to download videos from YouTube.com and other video sites'
+    
+        def to_screen(self, s, skip_eol=None):
+        print(s)
+    
+        argument_spec.update(dict(
+        state=dict(choices=['present', 'absent'], default='present'),
+        origin_access_identity_id=dict(),
+        caller_reference=dict(),
+        comment=dict(),
+    ))
+    
+    
+def main():
+    argument_spec = ec2_argument_spec()
+    argument_spec.update(dict(
+        name=dict(required=True),
+        registry_id=dict(required=False),
+        state=dict(required=False, choices=['present', 'absent'],
+                   default='present'),
+        force_set_policy=dict(required=False, type='bool', default=False),
+        policy=dict(required=False, type='json'),
+        delete_policy=dict(required=False, type='bool')))
+    
+    # Basic rename of existing certificate
+- iam_cert:
+    name: very_ssl
+    new_name: new_very_ssl
+    state: present
+    
+        state = module.params.get('state')
+    
+                appid = random.choice(self.working_appid_list)
+            return str(appid)
+        else:
+            for _ in xrange(0, 10):
+                appid = self.public_appid.get()
+                if appid in self.out_of_quota_appids or appid in self.not_exist_appids:
+                    continue
+                else:
+                    return appid
+            return None
+    
+        if sys.platform == 'win32':
+        win32_lib = os.path.abspath( os.path.join(python_path, 'lib', 'win32'))
+        sys.path.append(win32_lib)
+    elif sys.platform.startswith('linux'):
+        linux_lib = os.path.abspath( os.path.join(python_path, 'lib', 'linux'))
+        sys.path.append(linux_lib)
+    
+        for qualified, server, _, _ in server_list:
+        if qualified:
+            best_server = server[0]
+            break
+    log = Log()
+    if best_server:
+        log.write('best server is: %s.' % best_server)
+    else:
+        xlog.warning('no server detected, return default: teredo.remlab.net.')
+        log.write('no server detected, return default: teredo.remlab.net.')
+        best_server = 'teredo.remlab.net'
+    log.close()
+    return best_server
+    
+    __file__ = os.path.abspath(__file__)
+if os.path.islink(__file__):
+    __file__ = getattr(os, 'readlink', lambda x: x)(__file__)
+work_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(work_path)
+    
+            Using setter/getter methods is deprecated. Use o.line instead.'''
+    
+    
+# Are two filenames really pointing to the same file?
+def samefile(f1, f2):
+    '''Test whether two pathnames reference the same actual file'''
+    s1 = os.stat(f1)
+    s2 = os.stat(f2)
+    return samestat(s1, s2)
+    
+        @classmethod
+    def _from_parts(cls, args, init=True):
+        # We need to call _parse_args on the instance, so as to get the
+        # right flavour.
+        self = object.__new__(cls)
+        drv, root, parts = self._parse_args(args)
+        self._drv = drv
+        self._root = root
+        self._parts = parts
+        if init:
+            self._init()
+        return self
+    
+        def test_run_code(self):
+        # Test expected operation of the '-c' switch
+        # Switch needs an argument
+        assert_python_failure('-c')
+        # Check we get an error for an uncaught exception
+        assert_python_failure('-c', 'raise Exception')
+        # All good if execution is successful
+        assert_python_ok('-c', 'pass')
+    
+    handleSlideshow(dom)
 
     
-    n_samples, n_features = 5000, 300
-X = np.random.randn(n_samples, n_features)
-inds = np.arange(n_samples)
-np.random.shuffle(inds)
-X[inds[int(n_features / 1.2):]] = 0  # sparsify input
-print('input data sparsity: %f' % sparsity_ratio(X))
-coef = 3 * np.random.randn(n_features)
-inds = np.arange(n_features)
-np.random.shuffle(inds)
-coef[inds[n_features // 2:]] = 0  # sparsify coef
-print('true coef sparsity: %f' % sparsity_ratio(coef))
-y = np.dot(X, coef)
-    
-        An example with a long-untouched module that everyone has
-    >>> _linkcode_resolve('py', {'module': 'tty',
-    ...                          'fullname': 'setraw'},
-    ...                   package='tty',
-    ...                   url_fmt='http://hg.python.org/cpython/file/'
-    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
-    ...                   revision='xxxx')
-    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
-    '''
-    
-    URL = ('http://people.csail.mit.edu/jrennie/'
-       '20Newsgroups/20news-bydate.tar.gz')
-    
-        def test_stop_iteration(self):
-        def f():
-            for i in range(2):
-                yield i
-        def g(p):
-            for i in f():
-                pass
-        f_ident = ident(f)
-        g_ident = ident(g)
-        self.check_events(g, [(1, 'call', g_ident),
-                              # call the iterator twice to generate values
-                              (2, 'call', f_ident),
-                              (2, 'return', f_ident),
-                              (2, 'call', f_ident),
-                              (2, 'return', f_ident),
-                              # once more to hit the raise:
-                              (2, 'call', f_ident),
-                              (2, 'return', f_ident),
-                              (1, 'return', g_ident),
-                              ])
-    
-        def __hash__(self):
-        try:
-            return self._hash
-        except AttributeError:
-            self._hash = hash(tuple(self._cparts))
-            return self._hash
-    
-    # Join two paths, normalizing and eliminating any symbolic links
-# encountered in the second path.
-def _joinrealpath(path, rest, seen):
-    if isinstance(path, bytes):
-        sep = b'/'
-        curdir = b'.'
-        pardir = b'..'
-    else:
-        sep = '/'
-        curdir = '.'
-        pardir = '..'
-    
-        def test_normpath_issue5827(self):
-        # Make sure normpath preserves unicode
-        for path in ('', '.', '/', '\\', '///foo/.//bar//'):
-            self.assertIsInstance(self.pathmodule.normpath(path), str)
-    
-    class EncodingDetails(_EncodingDetails):
-    # XXX (ncoghlan): Using JSON for child state reporting may be less fragile
-    CHILD_PROCESS_SCRIPT = ';'.join([
-        'import sys, os',
-        'print(sys.getfilesystemencoding())',
-        'print(sys.stdin.encoding + ':' + sys.stdin.errors)',
-        'print(sys.stdout.encoding + ':' + sys.stdout.errors)',
-        'print(sys.stderr.encoding + ':' + sys.stderr.errors)',
-        'print(os.environ.get('LANG', 'not set'))',
-        'print(os.environ.get('LC_CTYPE', 'not set'))',
-        'print(os.environ.get('LC_ALL', 'not set'))',
-    ])
-    
-            rc, out, err = assert_python_ok('-c', code, PYTHONHASHSEED='0')
-        self.assertIn(b'random is 0', out)
-    
-        def check_fsencoding(self, fs_encoding, expected=None):
-        self.assertIsNotNone(fs_encoding)
-        codecs.lookup(fs_encoding)
-        if expected:
-            self.assertEqual(fs_encoding, expected)
-    
-        print('Unpickled records:')
-    pprint.pprint(memos)
-    
-    def noop(x):
-    pass
+    ##
