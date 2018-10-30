f104aedcf7d902cc06fbd8954622f61bcf1d15dd
SUNsung <weatherius@gmail.com>
2018-10-30 17:25:35 +0300

    Rewrite [21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp] File in: 2018-10-30 16:25:34

--- cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
+++ cccccc/21PbSD5gwXBaw21xOPWGRhGqio215pJQ3154w8I21sKP.cpp
@@ -1,129 +1,112 @@
 
         
-        namespace {
+        
+    {}  // namespace atom
+    
+    
+    {}  // namespace atom
+    
+    template <>
+struct Converter<atom::AutoResizeFlags> {
+  static bool FromV8(v8::Isolate* isolate,
+                     v8::Local<v8::Value> val,
+                     atom::AutoResizeFlags* auto_resize_flags) {
+    mate::Dictionary params;
+    if (!ConvertFromV8(isolate, val, &params)) {
+      return false;
+    }
+    }
+    }
+    
+      v8::Local<v8::Value> GetWebContents();
+    
+    void Initialize(v8::Local<v8::Object> exports,
+                v8::Local<v8::Value> unused,
+                v8::Local<v8::Context> context,
+                void* priv) {
+  v8::Isolate* isolate = context->GetIsolate();
+  Menu::SetConstructor(isolate, base::Bind(&Menu::New));
     }
     
     
-    {
-    {        for (; dj < size.width; sj += 4, ++dj)
+    {}  // namespace atom
+    
+    // static
+v8::Local<v8::Value> Screen::Create(v8::Isolate* isolate) {
+  if (!Browser::Get()->is_ready()) {
+    isolate->ThrowException(v8::Exception::Error(mate::StringToV8(
+        isolate, 'Cannot require \'screen\' module before app is ready')));
+    return v8::Null(isolate);
+  }
+    }
+    
+            dict[dynamicAxisKey] = dictionaryValueVector;
+        dict[isSparseKey] = IsSparse();
+        if (!Name().empty())
+            dict[nameKey] = Name();
+        dict[needsGradientKey] = NeedsGradient();
+        dict[shapeKey] = Shape();
+        if (IsParameter() || IsConstant())
         {
-            dst[dj] = src[sj + coi];
-        }
-    }
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)dstBase;
-    (void)dstStride;
-    (void)coi;
-#endif
-}
-    
-        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0, const typename internal::VecTraits<T>::vec64 & v_src1,
-              typename internal::VecTraits<T>::unsign::vec64 & v_dst) const
-    {
-        v_dst = internal::vcgt(v_src0, v_src1);
+            NDArrayView* value = Value().get();
+            if (value == nullptr)
+                LogicError('Uninitialized Parameter variable '%S' cannot be saved.', AsString().c_str());
     }
     
-    ptrdiff_t borderInterpolate(ptrdiff_t _p, size_t _len, BORDER_MODE borderType, size_t startMargin, size_t endMargin)
-{
-    ptrdiff_t p = _p + (ptrdiff_t)startMargin;
-    size_t len = _len + startMargin + endMargin;
-    if( (size_t)p < len )
-        return _p;
-    else if( borderType == BORDER_MODE_REPLICATE )
-        p = p < 0 ? 0 : (ptrdiff_t)len - 1;
-    else if( borderType == BORDER_MODE_REFLECT || borderType == BORDER_MODE_REFLECT101 )
+        // this returns the map directly (read-only) and will lazily initialize it for a given seed
+    const std::vector<INDEXTYPE>& operator()(size_t seed) // throw()
     {
-        s32 delta = borderType == BORDER_MODE_REFLECT101;
-        if( len == 1 )
-            return 0;
-        do
+        // if wrong seed then lazily recache the sequence
+        if (seed != currentseed && randomizationrange != randomizeDisable)
         {
-            if( p < 0 )
-                p = -p - 1 + delta;
-            else
-                p = (ptrdiff_t)len - 1 - (p - (ptrdiff_t)len) - delta;
-        }
-        while( (size_t)p >= len );
+            // test for numeric overflow
+            if (map.size() - 1 != (INDEXTYPE)(map.size() - 1))
+                RuntimeError('RandomOrdering: INDEXTYPE has too few bits for this corpus');
+            // 0, 1, 2...
+            foreach_index (t, map)
+                map[t] = (INDEXTYPE) t;
     }
-    else if( borderType == BORDER_MODE_WRAP )
-    {
-        if( p < 0 )
-            p -= ((p-(ptrdiff_t)len+1)/(ptrdiff_t)len)*(ptrdiff_t)len;
-        if( p >= (ptrdiff_t)len )
-            p %= (ptrdiff_t)len;
     }
-    else if( borderType == BORDER_MODE_CONSTANT )
-        p = -1;
-    else
-        internal::assertSupportedConfiguration(false);
-    return p - (ptrdiff_t)startMargin;
+    
+    #pragma once
+    
+    Status LoggerConfigParserPlugin::update(const std::string& /* source */,
+                                        const ParserConfig& config) {
+  rj::Document& doc = data_.doc();
+    }
+    
+    namespace osquery {
+    }
+    
+    #include <iostream>
+    
+    
+    {REGISTER_INTERNAL(ViewsConfigParserPlugin, 'config_parser', 'views');
+}
+
+    
+      size_t pack_count = 0U;
+  c.packs(([&pack_count, query_attemts](const Pack& p) {
+    pack_count++;
+    // There is one pack without a discovery query.
+    EXPECT_EQ(p.getStats().total, query_attemts + 1);
+    EXPECT_EQ(p.getStats().hits, query_attemts);
+    EXPECT_EQ(p.getStats().misses, 1U);
+  }));
+    
+    bool PlatformProcess::operator==(const PlatformProcess& process) const {
+  return (nativeHandle() == process.nativeHandle());
 }
     
-    void assertSupportedConfiguration(bool parametersSupported = true);
+    #include <Objbase.h>
+#include <Windows.h>
     
-        uint8x8_t tprev[3] = { v_zero_u8, v_zero_u8, v_zero_u8 },
-              tcurr[3] = { v_zero_u8, v_zero_u8, v_zero_u8 },
-              tnext[3] = { v_zero_u8, v_zero_u8, v_zero_u8 };
-    uint8x8_t t0 = v_zero_u8, t1 = v_zero_u8, t2 = v_zero_u8;
-    
-    inline float32x4_t vroundq(const float32x4_t& v)
-{
-    const int32x4_t signMask = vdupq_n_s32(1 << 31), half = vreinterpretq_s32_f32(vdupq_n_f32(0.5f));
-    float32x4_t v_addition = vreinterpretq_f32_s32(vorrq_s32(half, vandq_s32(signMask, vreinterpretq_s32_f32(v))));
-    return vaddq_f32(v, v_addition);
-}
-    
-    #ifdef CAROTENE_NEON
-    
-            ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-        ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-                ImGui::Begin('Hello, world!');                          // Create a window called 'Hello, world!' and append into it.
-    
-        // Setup Dear ImGui binding
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    
-        // Copy and convert all vertices into a single contiguous buffer
-    ImDrawVert* vtx_dst = NULL;
-    ImDrawIdx* idx_dst = NULL;
-    g_pVB->Map(D3D10_MAP_WRITE_DISCARD, 0, (void**)&vtx_dst);
-    g_pIB->Map(D3D10_MAP_WRITE_DISCARD, 0, (void**)&idx_dst);
-    for (int n = 0; n < draw_data->CmdListsCount; n++)
-    {
-        const ImDrawList* cmd_list = draw_data->CmdLists[n];
-        memcpy(vtx_dst, cmd_list->VtxBuffer.Data, cmd_list->VtxBuffer.Size * sizeof(ImDrawVert));
-        memcpy(idx_dst, cmd_list->IdxBuffer.Data, cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx));
-        vtx_dst += cmd_list->VtxBuffer.Size;
-        idx_dst += cmd_list->IdxBuffer.Size;
+    TEST_F(INotifyTests, test_inotify_match_subscription) {
+  event_pub_ = std::make_shared<INotifyEventPublisher>(true);
+  addMonitor('/etc', IN_ALL_EVENTS, false, false);
+  EXPECT_EQ(event_pub_->path_descriptors_.count('/etc'), 1U);
+  // This will fail because there is no trailing '/' at the end.
+  // The configure component should take care of these paths.
+  EXPECT_FALSE(event_pub_->isPathMonitored('/etc/passwd'));
+  event_pub_->path_descriptors_.clear();
     }
-    g_pVB->Unmap();
-    g_pIB->Unmap();
-    
-            D3D12_STATIC_SAMPLER_DESC staticSampler = {};
-        staticSampler.Filter = D3D12_FILTER_MIN_MAG_MIP_LINEAR;
-        staticSampler.AddressU = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-        staticSampler.AddressV = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-        staticSampler.AddressW = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
-        staticSampler.MipLODBias = 0.f;
-        staticSampler.MaxAnisotropy = 0;
-        staticSampler.ComparisonFunc = D3D12_COMPARISON_FUNC_ALWAYS;
-        staticSampler.BorderColor = D3D12_STATIC_BORDER_COLOR_TRANSPARENT_BLACK;
-        staticSampler.MinLOD = 0.f;
-        staticSampler.MaxLOD = 0.f;
-        staticSampler.ShaderRegister = 0;
-        staticSampler.RegisterSpace = 0;
-        staticSampler.ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
-    
-    // CHANGELOG 
-// (minor and older changes stripped away, please see git history for details)
-//  2018-06-08: Misc: Extracted imgui_impl_dx9.cpp/.h away from the old combined DX9+Win32 example.
-//  2018-06-08: DirectX9: Use draw_data->DisplayPos and draw_data->DisplaySize to setup projection matrix and clipping rectangle.
-//  2018-05-07: Render: Saving/restoring Transform because they don't seem to be included in the StateBlock. Setting shading mode to Gouraud.
-//  2018-02-16: Misc: Obsoleted the io.RenderDrawListsFn callback and exposed ImGui_ImplDX9_RenderDrawData() in the .h file so you can call it yourself.
-//  2018-02-06: Misc: Removed call to ImGui::Shutdown() which is not available from 1.60 WIP, user needs to call CreateContext/DestroyContext themselves.
