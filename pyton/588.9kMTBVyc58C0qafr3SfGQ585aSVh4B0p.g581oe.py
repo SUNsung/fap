63055b9f11eff7bc1df37712e6a5f9613368441c
SUNsung <weatherius@gmail.com>
2018-10-30 17:49:10 +0300

    Rewrite [588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py] File in: 2018-10-30 16:49:10

--- pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
+++ pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
@@ -1,72 +1,61 @@
 
         
-            # Print the location of each face in this image
+        _options = [
+    'help',
+    'version',
+    'gui',
+    'force',
+    'playlists',
+]
+_short_options = 'hVgfl'
+    
+        vid = r1('data-vid='(\d+)'', html)
+    up = r1('data-name='([^']+)'', html)
+    p_title = r1('active'>([^<]+)', html)
+    title = '%s (%s)' % (title, up)
+    if p_title: title = '%s - %s' % (title, p_title)
+    acfun_download_by_vid(vid, title,
+                          output_dir=output_dir,
+                          merge=merge,
+                          info_only=info_only,
+                          **kwargs)
+    
+        if re.match(re_str, url):
+        html = get_content(url)
+        title = match1(html, r'<title>(.+)丨(.+)</title>')  #title
+        
+        # player loaded via internal iframe
+        frame_url = re.search(r'<iframe src=\'(.+?)\'', html).group(1)
+        logging.debug('dilidili_download: %s' % frame_url)
+        
+        #https://player.005.tv:60000/?vid=a8760f03fd:a04808d307&v=yun&sign=a68f8110cacd892bc5b094c8e5348432
+        html = get_content(frame_url, headers=headers, decoded=False).decode('utf-8')
+        
+        match = re.search(r'(.+?)var video =(.+?);', html)
+        vid = match1(html, r'var vid='(.+)'')
+        hd2 = match1(html, r'var hd2='(.+)'')
+        typ = match1(html, r'var typ='(.+)'')
+        sign = match1(html, r'var sign='(.+)'')
+        tmsign = match1(html, r'tmsign=([A-Za-z0-9]+)')
+        ulk =  match1(html, r'var ulk='(.+)'')
+    
+        print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([video_url], title, ext, size, output_dir, merge = merge, headers = headers)
+    
+    # Often instead of just checking if two faces match or not (True or False), it's helpful to see how similar they are.
+# You can do that by using the face_distance function.
+    
+    Algorithm Description:
+The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
+in an unknown image by finding the k most similar faces (images with closet face-features under eucledian distance)
+in its training set, and performing a majority vote (possibly weighted) on their label.
+    
+        # Print the location of each face in this image
     top, right, bottom, left = face_location
     print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
-    
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    
-    # This is a demo of running face recognition on a video file and saving the results to a new video file.
-#
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-        def __init__(self, hass, client, address, timeout, ignored_click_types):
-        '''Initialize the flic button.'''
-        import pyflic
-    
-            try:
-            data = [
-                ('user', self._username),
-                (self._type, self._password),
-            ]
-            res = requests.post(self._loginurl, data=data, timeout=10)
-        except requests.exceptions.Timeout:
-            _LOGGER.error(
-                'Connection to the router timed out at URL %s', self._url)
-            return False
-        if res.status_code != 200:
-            _LOGGER.error(
-                'Connection failed with http code %s', res.status_code)
-            return False
-        try:
-            self._userid = res.cookies['userid']
-            return True
-        except KeyError:
-            _LOGGER.error('Failed to log in to router')
-            return False
-    
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/device_tracker.mysensors/
-'''
-from homeassistant.components import mysensors
-from homeassistant.components.device_tracker import DOMAIN
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
-from homeassistant.util import slugify
-    
-            self.last_results = last_results
-    
-        def see_vehicle(vehicle):
-        '''Handle the reporting of the vehicle position.'''
-        host_name = voc.vehicle_name(vehicle)
-        dev_id = 'volvo_{}'.format(slugify(host_name))
-        see(dev_id=dev_id,
-            host_name=host_name,
-            gps=(vehicle.position['latitude'],
-                 vehicle.position['longitude']),
-            icon='mdi:car')
-    
-    ATTR_FILENAME = 'filename'
-ATTR_SUBDIR = 'subdir'
-ATTR_URL = 'url'
-ATTR_OVERWRITE = 'overwrite'
-    
-    from homeassistant.const import (
-    CONF_NAME, CONF_WHITELIST, EVENT_STATE_CHANGED, STATE_UNKNOWN)
-import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers import state as state_helper
-from homeassistant.util import Throttle
+    # To run this, you need a Raspberry Pi 2 (or greater) with face_recognition and
+# the picamera[array] module installed.
+# You can follow this installation instructions to get your RPi set up:
+# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
