f5d6e2e4a151ae9d1457302d58ec8b890b54a09d
SUNsung <weatherius@gmail.com>
2018-10-31 18:40:55 +0300

    Rewrite [27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp] File in: 2018-10-31 17:40:54

--- cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
+++ cccccc/27q6qgnapgpU27SQmGEjFrr7E276vkxXdxhhrc27cUm.cpp
@@ -1,108 +1,92 @@
 
         
-        
-    {  return render_process_host;
-}
+        public:
+    ~MacDockIconHandler();
     
     
-    { private:
-  App();
-  DISALLOW_COPY_AND_ASSIGN(App);
+    {    QString appName;
+    QIcon appIcon;
+    QIcon trayAndWindowIcon;
+    QString titleAddText;
 };
-    
-    void Clipboard::Call(const std::string& method,
-                     const base::ListValue& arguments) {
-  if (method == 'Set') {
-    std::string text, type;
-    arguments.GetString(0, &text);
-    arguments.GetString(1, &type);
-    SetText(text);
-  } else if (method == 'Clear') {
-    Clear();
-  } else {
-    NOTREACHED() << 'Invalid call to Clipboard method:' << method
-                 << ' arguments:' << arguments;
-  }
-}
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(MenuDelegate);
-};
-    
-    #define TegraUnaryOp_Invoker(name, func) TegraGenOp_Invoker(name, func, 1, 1, 0, \
-                                                            RANGE_DATA(ST, src1_data, src1_step), src1_step, \
-                                                            RANGE_DATA(DT, dst1_data, dst1_step), dst1_step )
-    
-    
-    {    struct KeypointStore {
-        virtual void push(f32 kpX, f32 kpY, f32 kpSize, f32 kpAngle=-1, f32 kpResponse=0, s32 kpOctave=0, s32 kpClass_id=-1) = 0;
-        virtual ~KeypointStore() {};
-    };
-}
-    
-                uint8x16x2_t v_src = vld2q_u8(src + sj);
-            vst1q_u8(dst + dj, v_src.val[coi]);
-    
-    inline uint8x8_t convertToGray(const uint16x8_t & v_r,
-                               const uint16x8_t & v_g,
-                               const uint16x8_t & v_b,
-                               const uint16x4_t & v_r2y,
-                               const uint16x4_t & v_g2y,
-                               const uint16x4_t & v_b2y)
-{
-    uint32x4_t v_dst0 = vmull_u16(vget_low_u16(v_g), v_g2y);
-    uint32x4_t v_dst1 = vmull_u16(vget_high_u16(v_g), v_g2y);
-    }
-    
-    namespace CAROTENE_NS { namespace internal {
-    }
-    }
-    
-                for(; j < (ptrdiff_t)size.width - 16 - 3; j += 16, ptr += 16)
-            {
-                internal::prefetch(ptr);
-                internal::prefetch(ptr + pixel[0]);
-                internal::prefetch(ptr + pixel[2]);
-    }
-    
-    
-    {            vec128 v_src = vld1q(src + js);
-            vec128 v_dst = vrev64q(v_src);
-            v_dst = vcombine(vget_high(v_dst), vget_low(v_dst));
-            vst1q(dst + jd - step_base, v_dst);
-        }
-        for (; js < roiw_tail; js += step_tail, jd -= step_tail)
-        {
-            vec64 v_src = vld1(src + js);
-            vst1(dst + jd - step_tail, vrev64(v_src));
-        }
-    
-    
-    {} // namespace CAROTENE_NS
-
-    
-    inline void APCLocalArray::scan(type_scan::Scanner& scanner) const {
-  scanner.scan(*localCache(), m_size * sizeof(TypedValue));
-}
-    
-    void Config::ParseIniString(const std::string &iniStr, IniSettingMap &ini,
-                            const bool constants_only /* = false */ ) {
-  Config::SetParsedIni(ini, iniStr, '', constants_only, true);
-}
-    
-          std::string line = argv[cnt+1];
-      std::string section = 'php';
-      int pos_period = line.find_first_of('.');
-      int pos_equals = line.find_first_of('=');
-    
-    #include <string>
-#include <fstream>
     
     /**
- * Search for PHP or non-PHP files under a directory, calling callback for
- * each one found.
+ * An RAII-style reverse lock. Unlocks on construction and locks on destruction.
  */
-template <typename F>
-void find(std::vector<std::string> &out,
-          const std::string &root, const std::string& path, bool php,
-          const F& callback);
+template<typename Lock>
+class reverse_lock
+{
+public:
+    }
+    
+    static void secp256k1_ge_clear(secp256k1_ge *r) {
+    r->infinity = 0;
+    secp256k1_fe_clear(&r->x);
+    secp256k1_fe_clear(&r->y);
+}
+    
+        secp256k1_pubkey_load(ctx, &pt, point);
+    secp256k1_scalar_set_b32(&s, scalar, &overflow);
+    if (overflow || secp256k1_scalar_is_zero(&s)) {
+        ret = 0;
+    } else {
+        unsigned char x[32];
+        unsigned char y[1];
+        secp256k1_sha256_t sha;
+    }
+    
+    
+    {    secp256k1_ecdsa_recoverable_signature_load(ctx, &r, &s, &recid, sigin);
+    secp256k1_ecdsa_signature_save(sig, &r, &s);
+    return 1;
+}
+    
+    #include <bech32.h>
+#include <test/test_bitcoin.h>
+    
+    BOOST_AUTO_TEST_CASE(univalue_set)
+{
+    UniValue v(UniValue::VSTR, 'foo');
+    v.clear();
+    BOOST_CHECK(v.isNull());
+    BOOST_CHECK_EQUAL(v.getValStr(), '');
+    }
+    
+    void DHTReplaceNodeTask::onReceived(const DHTPingReplyMessage* message)
+{
+  A2_LOG_INFO(fmt('ReplaceNode: Ping reply received from %s.',
+                  message->getRemoteNode()->toString().c_str()));
+  setFinished(true);
+}
+    
+    namespace aria2 {
+    }
+    
+      void setNodes(const std::vector<std::shared_ptr<DHTNode>>& nodes);
+    
+    #include 'common.h'
+    
+      virtual std::shared_ptr<DHTTask> createBucketRefreshTask() = 0;
+    
+    void DHTTaskQueueImpl::executeTask()
+{
+  A2_LOG_DEBUG('Updating periodicTaskQueue1');
+  periodicTaskQueue1_.update();
+  A2_LOG_DEBUG('Updating periodicTaskQueue2');
+  periodicTaskQueue2_.update();
+  A2_LOG_DEBUG('Updating immediateTaskQueue');
+  immediateTaskQueue_.update();
+}
+    
+      std::string generateToken(const unsigned char* infoHash,
+                            const std::string& ipaddr, uint16_t port,
+                            const unsigned char* secret) const;
+    
+    DNSCache::AddrEntry::AddrEntry(const AddrEntry& c) = default;
+    
+        static BOOST_FORCEINLINE storage_type load(storage_type const volatile& storage, memory_order order) BOOST_NOEXCEPT
+    {
+        storage_type v = BOOST_ATOMIC_DETAIL_ARM_LOAD8(&storage);
+        base_type::fence_after_load(order);
+        return v;
+    }
