ac6849652cebb4a9be763069409a865e5834eb35
SUNsung <weatherius@gmail.com>
2018-10-31 19:26:54 +0300

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2018-10-31 18:26:53

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,91 +1,25 @@
 
         
-                  def checked?(value)
-            case value
-            when TrueClass, FalseClass
-              value == !!@checked_value
-            when NilClass
-              false
-            when String
-              value == @checked_value
-            else
-              if value.respond_to?(:include?)
-                value.include?(@checked_value)
-              else
-                value.to_i == @checked_value.to_i
-              end
-            end
-          end
-    
-              def render_collection
-            @collection.map do |item|
-              value = value_for_collection(item, @value_method)
-              text  = value_for_collection(item, @text_method)
-              default_html_options = default_html_options_for_collection(item, value)
-              additional_html_options = option_html_attributes(item)
-    
-              content_is_options = content_or_options.is_a?(Hash)
-          if content_is_options
-            options.merge! content_or_options
-            @content = nil
-          else
-            @content = content_or_options
-          end
-    
-              # Determines whether the current action has a layout definition by
-          # checking the action name against the :only and :except conditions
-          # set by the <tt>layout</tt> method.
-          #
-          # ==== Returns
-          # * <tt>Boolean</tt> - True if the action has a layout definition, false otherwise.
-          def _conditional_layout?
-            return unless super
-    
-          def typecast(paths)
-        paths.map do |path|
-          case path
-          when Pathname, String
-            OptimizedFileSystemResolver.new path.to_s
-          else
-            path
-          end
+                def render
+          error_wrapping(datetime_selector(@options, @html_options).send('select_#{select_type}').html_safe)
         end
-      end
-  end
-end
-
     
-    # Mimic how the proposed change would first execute a couple of checks and
-# proceed to process with Liquid if necessary
-def conditional_liquid(content)
-  return content if content.nil? || content.empty?
-  return content unless content.include?('{%') || content.include?('{{')
-  always_liquid(content)
-end
-    }
-    }
-    }
+        raise UsageError unless ARGV.named.empty?
     
-    if pathutil_relative == native_relative
-  Benchmark.ips do |x|
-    x.report('pathutil') { pathutil_relative }
-    x.report('native')   { native_relative }
-    x.compare!
-  end
-else
-  print 'PATHUTIL: '
-  puts pathutil_relative
-  print 'NATIVE:   '
-  puts native_relative
-end
-
+      attr_reader :name, :full_name, :owner
+  attr_reader :build, :resources, :patches, :options
+  attr_reader :deprecated_flags, :deprecated_options
+  attr_reader :dependency_collector
+  attr_reader :bottle_specification
+  attr_reader :compiler_failures
     
-      def self.status_file; test_dir.join('jekyll_status.txt'); end
-    
-            def log_error(error)
-          Jekyll.logger.error 'LiveReload experienced an error. ' \
-            'Run with --trace for more information.'
-          raise error
+            keys.find do |key|
+          key_tag_version = tag_without_or_later(key)
+          begin
+            MacOS::Version.from_symbol(key_tag_version) <= tag_version
+          rescue ArgumentError
+            false
+          end
         end
       end
     end
@@ -93,173 +27,91 @@ end
 end
 
     
-        def effective_path
-      @effective_path ||= effective_url.path
-    end
-  end
-end
-
-    
-        def request_all(urls)
-      queue = [urls].flatten
-      until queue.empty?
-        result = yield request_one(queue.shift)
-        queue.concat(result) if result.is_a? Array
+          def use_identicon
+        @page.wiki.user_icons == 'identicon'
       end
-    end
     
-        def justify(str)
-      return str unless terminal_width
-      str = str.dup
+          def sidebar_content
+        has_sidebar && @sidebar.formatted_data
+      end
     
-            css('h1[class]').remove_attr('class')
-        css('table[class]').remove_attr('class')
-        css('table[width]').remove_attr('width')
-        css('tr[style]').remove_attr('style')
-    
-      it 'returns false when the command exits with a non-zero exit status' do
-    @object.system(ruby_cmd('exit 1')).should == false
-    
-      it 'creates a public method in script binding' do
-    eval @code, script_binding
-    Object.should have_method :boom
+      test 'extract destination file name in case of path renaming' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
+    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
   end
     
-      # Setup a color scheme called 'bright' than can be used to add color codes
-  # to the pattern layout. Color schemes should only be used with appenders
-  # that write to STDOUT or STDERR; inserting terminal color codes into a file
-  # is generally considered bad form.
-  Logging.color_scheme('bright',
-                       levels:  {
-                         info:  :green,
-                         warn:  :yellow,
-                         error: :red,
-                         fatal: %i(white on_red)
-                       },
-                       date:    :blue,
-                       logger:  :cyan,
-                       message: :magenta
-                      )
+      test 'h1 title can be disabled' do
+    title = 'H1'
+    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
+    page = @wiki.page(title)
     
-      def down
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id
-  end
+        EMOJI_PATHNAME = Pathname.new(Gemojione.images_path).freeze
     
-    # Ensure we know the appservers port
-Capybara.server_port = AppConfig.pod_uri.port
-Rails.application.routes.default_url_options[:host] = AppConfig.pod_uri.host
-Rails.application.routes.default_url_options[:port] = AppConfig.pod_uri.port
-    
-      class FetchWebfinger < Base
-    def perform(*_args)
-      # don't do real discovery in cucumber
+        mkdir_p page_dir
+    file = '#{page_dir}/#{filename}.#{extension}'
+    if File.exist?(file)
+      abort('rake aborted!') if ask('#{file} already exists. Do you want to overwrite?', ['y', 'n']) == 'n'
     end
-  end
-end
-
-    
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
+    puts 'Creating new page: #{file}'
+    open(file, 'w') do |page|
+      page.puts '---'
+      page.puts 'layout: page'
+      page.puts 'title: \'#{title}\''
+      page.puts 'date: #{Time.now.strftime('%Y-%m-%d %H:%M')}'
+      page.puts 'comments: true'
+      page.puts 'sharing: true'
+      page.puts 'footer: true'
+      page.puts '---'
     end
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
-    end
-  end
-end
-
-    
-      # Compile a file on disk to CSS.
-  #
-  # @raise [Sass::SyntaxError] if there's an error in the document
-  # @raise [Encoding::UndefinedConversionError] if the source encoding
-  #   cannot be converted to UTF-8
-  # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
-  #
-  # @overload compile_file(filename, options = {})
-  #   Return the compiled CSS rather than writing it to a file.
-  #
-  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
-  #   @param options [{Symbol => Object}] An options hash;
-  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
-  #   @return [String] The compiled CSS.
-  #
-  # @overload compile_file(filename, css_filename, options = {})
-  #   Write the compiled CSS to a file.
-  #
-  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
-  #   @param options [{Symbol => Object}] An options hash;
-  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
-  #   @param css_filename [String] The location to which to write the compiled CSS.
-  def self.compile_file(filename, *args)
-    options = args.last.is_a?(Hash) ? args.pop : {}
-    css_filename = args.shift
-    result = Sass::Engine.for_file(filename, options).render
-    if css_filename
-      options[:css_filename] ||= css_filename
-      open(css_filename, 'w') {|css_file| css_file.write(result)}
-      nil
-    else
-      result
-    end
+  else
+    puts 'Syntax error: #{args.filename} contains unsupported characters'
   end
 end
     
-    module Sass
-  module CacheStores
-    # A backend for the Sass cache using the filesystem.
-    class Filesystem < Base
-      # The directory where the cached files will be stored.
-      #
-      # @return [String]
-      attr_accessor :cache_location
-    
-        # Helper for \{#dependencies}.
+        # Creates an instance of CategoryIndex for each category page, renders it, and
+    # writes the output to a file.
     #
-    # @private
-    def _dependencies(seen, engines)
-      key = [@options[:filename], @options[:importer]]
-      return if seen.include?(key)
-      seen << key
-      engines << self
-      to_tree.grep(Tree::ImportNode) do |n|
-        next if n.css_import?
-        n.imported_file._dependencies(seen, engines)
-      end
+    #  +category_dir+ is the String path to the category folder.
+    #  +category+     is the category currently being processed.
+    def write_category_index(category_dir, category)
+      index = CategoryIndex.new(self, self.source, category_dir, category)
+      index.render(self.layouts, site_payload)
+      index.write(self.dest)
+      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
+      self.pages << index
+    
+    Liquid::Template.register_tag('config_tag', ConfigTag)
+    
+      # Summary is used on the Archive pages to return the first block of content from a post.
+  def summary(input)
+    if input.index(/\n\n/)
+      input.split(/\n\n/)[0]
+    else
+      input
+    end
+  end
+    
+    require 'pathname'
+require './plugins/octopress_filters'
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
     end
     
-        # function
-    # Sass::Callable
-    inherited_hash_reader :function
+            def next
+          authorize! :update, @order, order_token
+          @order.next!
+          respond_with(@order, default_template: 'spree/api/v1/orders/show', status: 200)
+        rescue StateMachines::InvalidTransition
+          respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
+        end
     
-          output = input_path if @options[:in_place]
-      write_output(out, output)
-    rescue Sass::SyntaxError => e
-      raise e if @options[:trace]
-      file = ' of #{e.sass_filename}' if e.sass_filename
-      raise 'Error on line #{e.sass_line}#{file}: #{e.message}\n  Use --trace for backtrace'
-    rescue LoadError => err
-      handle_load_error(err)
-    end
-    
-    # This is the version that ships with OS X 10.10, so be sure we test against it.
-# At the same time, the 1.7.7 version won't install cleanly on Ruby > 2.2,
-# so we use a fork that makes a trivial change to a macro invocation.
-gem 'json', :git => 'https://github.com/segiddins/json.git', :branch => 'seg-1.7.7-ruby-2.2'
-    
-                case platform
-            when 'iOS' then self.platform :ios, '10.0'
-            when 'macOS' then self.platform :macos, '10.10'
-            end
+            def destroy
+          @option_type = Spree::OptionType.accessible_by(current_ability, :destroy).find(params[:id])
+          @option_type.destroy
+          render plain: nil, status: 204
+        end
