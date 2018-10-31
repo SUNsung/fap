89ebc6171fd85e3f564f122ae787a9ce816e08ca
SUNsung <weatherius@gmail.com>
2018-10-31 05:31:37 +0300

    Rewrite [50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb] File in: 2018-10-31 04:31:36

--- ruby/50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb
+++ ruby/50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb
@@ -1,175 +1,57 @@
 
         
-                  def initialize(template_object, object_name, method_name, object,
-                         sanitized_attribute_name, text, value, input_html_options)
-            @template_object = template_object
-            @object_name = object_name
-            @method_name = method_name
-            @object = object
-            @sanitized_attribute_name = sanitized_attribute_name
-            @text = text
-            @value = value
-            @input_html_options = input_html_options
-          end
+              GivenDailyLike.increment_for(user.id)
+      expect(value_for(user.id, dt)).to eq(2)
+      expect(limit_reached_for(user.id, dt)).to eq(true)
     
-            private
+    # pulling this out cause I don't want an observer
+describe Notification do
+  describe '#recent_report' do
+    let(:user) { Fabricate(:user) }
+    let(:post) { Fabricate(:post) }
     
-            class << self
-          def field_type
-            @field_type ||= name.split('::').last.sub('Field', '').downcase
-          end
-        end
+    BASE_URI = ENV['BASE_URI'] || 'https://github.com/jondot/awesome-react-native'
     
-        def initialize_copy(other)
-      @paths = other.paths.dup
-      self
+      def set_statuses
+    return unless page_requested?
+    
+        def destroy
+      authorize @report_note, :destroy?
+      @report_note.destroy!
+      redirect_to admin_report_path(@report_note.report_id), notice: I18n.t('admin.report_notes.destroyed_msg')
     end
     
-    html_readme = '<html>#{Kramdown::Document.new(open('README.md').read).to_html}</html>'
-readme_doctree = REXML::Document.new(html_readme)
-links = REXML::XPath.match(readme_doctree, '//a')
-    
-        it 'should ignore strings which just contain a JSONPath' do
-      expect(LiquidMigrator.convert_string('$.data')).to eq('$.data')
-      expect(LiquidMigrator.convert_string('$first_title')).to eq('$first_title')
-      expect(LiquidMigrator.convert_string(' $.data', true)).to eq(' $.data')
-      expect(LiquidMigrator.convert_string('lorem $.data', true)).to eq('lorem $.data')
-    end
-    it 'should raise an exception when encountering complex JSONPaths' do
-      expect { LiquidMigrator.convert_string('$.data.test.*', true) }.
-        to raise_error('JSONPath '$.data.test.*' is too complex, please check your migration.')
-    end
-  end
-    
-            receiver = Agents::CannotBeScheduled.new(
-           :name => 'Receiving Agent',
-        )
-        receiver.propagate_immediately = true
-        receiver.user = users(:bob)
-        receiver.sources << sender
-        receiver.save!
-    
-    gemspec
-    
-        def translation_scope
-      'devise.unlocks'
-    end
-end
-
-    
-      # Defines which strategy can be used to unlock an account.
-  # Values: :email, :time, :both
-  mattr_accessor :unlock_strategy
-  @@unlock_strategy = :both
-    
-          # Sign in a user and tries to redirect first to the stored location and
-      # then to the url specified by after_sign_in_path_for. It accepts the same
-      # parameters as the sign_in method.
-      def sign_in_and_redirect(resource_or_scope, *args)
-        options  = args.extract_options!
-        scope    = Devise::Mapping.find_scope!(resource_or_scope)
-        resource = args.last || resource_or_scope
-        sign_in(scope, resource, options)
-        redirect_to after_sign_in_path_for(resource)
-      end
-    
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-            # This is the method called to when the system is being destroyed
-        # and allows the provisioners to engage in any cleanup tasks necessary.
-        def cleanup
-        end
-      end
-    end
+      def maxheight_or_default
+    params[:maxheight].present? ? params[:maxheight].to_i : nil
   end
 end
 
     
-            # Converts this configuration object to JSON.
-        def to_json(*a)
-          instance_variables_hash.to_json(*a)
-        end
+    module Sass
+  module CacheStores
+    # A backend for the Sass cache using the filesystem.
+    class Filesystem < Base
+      # The directory where the cached files will be stored.
+      #
+      # @return [String]
+      attr_accessor :cache_location
     
-      private
+        # variable
+    # Script::Value
+    inherited_hash_writer :var
     
-      def create
-    if signed_request_account
-      upgrade_account
-      process_payload
-      head 202
-    else
-      render plain: signature_verification_failure_reason, status: 401
-    end
-  end
+        # Wraps the given string in terminal escapes
+    # causing it to have the given color.
+    # If terminal escapes aren't supported on this platform,
+    # just returns the string instead.
+    #
+    # @param color [Symbol] The name of the color to use.
+    #   Can be `:red`, `:green`, or `:yellow`.
+    # @param str [String] The string to wrap in the given color.
+    # @return [String] The wrapped string.
+    def color(color, str)
+      raise '[BUG] Unrecognized color #{color}' unless COLORS[color]
     
-          if @account_moderation_note.save
-        redirect_to admin_account_path(@account_moderation_note.target_account_id), notice: I18n.t('admin.account_moderation_notes.created_msg')
-      else
-        @account          = @account_moderation_note.target_account
-        @moderation_notes = @account.targeted_moderation_notes.latest
-    
-    module Admin
-  class ChangeEmailsController < BaseController
-    before_action :set_account
-    before_action :require_local_account!
-    
-      expect(status).to be_success
-end
-    
-            # rubocop:disable Style/MethodMissing
-        def method_missing(key, value=nil)
-          if value
-            set(lvalue(key), value)
-          else
-            fetch(key)
-          end
-        end
-        # rubocop:enable Style/MethodMissing
-    
-          attr_reader :validators
-    
-          def keys
-        values.keys
+            # JRuby chokes when trying to import files from JARs when the path starts with './'.
+        ret.map {|f, s| [f.sub(%r{^\./}, ''), s]}
       end
-    
-    module Jekyll
-    
-        def html_output_for(script_url, code)
-      code = CGI.escapeHTML code
-      <<-HTML
-<div><script src='#{script_url}'></script>
-<noscript><pre><code>#{code}</code></pre></noscript></div>
-      HTML
-    end
-    
-          if File.symlink?(includes_dir)
-        return 'Includes directory '#{includes_dir}' cannot be a symlink'
-      end
-    
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
-      end
-    end
-  end
-    
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
-    end
