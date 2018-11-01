072fabe4daf73d216e935823a59f55c104801657
SUNsung <weatherius@gmail.com>
2018-11-01 16:31:10 +0300

    Rewrite [55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp] File in: 2018-11-01 15:31:10

--- cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
+++ cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
@@ -1,125 +1,49 @@
 
         
-        QT_BEGIN_NAMESPACE
-class QIcon;
-class QMenu;
-class QWidget;
-QT_END_NAMESPACE
+        
     
-        QColor TextColor() const { return textColor; }
-    QColor SingleColor() const { return singleColor; }
-    
-    
-    {    check_addbook_size(3);
-}
-    
-    private:
-    reverse_lock(reverse_lock const&);
-    reverse_lock& operator=(reverse_lock const&);
-    
-    /** Check bounds on a command line confirm target */
-unsigned int ParseConfirmTarget(const UniValue& value);
-    
-    SECP256K1_INLINE static void secp256k1_fe_mul_inner(uint64_t *r, const uint64_t *a, const uint64_t * SECP256K1_RESTRICT b) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            r15:rcx = d
- *            r10-r14 = a0-a4
- *            rbx     = b
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
+    struct APCCollection {
+  static APCHandle::Pair Make(const ObjectData*,
+                              APCHandleLevel level,
+                              bool unserializeObj);
+  static void Delete(APCHandle*);
     }
     
-        s_one[31] = 1;
-    /* Check against pubkey creation when the basepoint is the generator */
-    for (i = 0; i < 100; ++i) {
-        secp256k1_sha256_t sha;
-        unsigned char s_b32[32];
-        unsigned char output_ecdh[32];
-        unsigned char output_ser[32];
-        unsigned char point_ser[33];
-        size_t point_ser_len = sizeof(point_ser);
-        secp256k1_scalar s;
+    // This method must return a char* which is owned by the IniSettingMap
+// to avoid issues with the lifetime of the char*
+const char* Config::Get(const IniSettingMap &ini, const Hdf& config,
+                        const std::string& name /* = '' */,
+                        const char *defValue /* = nullptr */,
+                        const bool prepend_hhvm /* = true */) {
+  auto ini_name = IniName(name, prepend_hhvm);
+  Hdf hdf = name != '' ? config[name] : config;
+  auto value = ini_iterate(ini, ini_name);
+  if (value.isString()) {
+    // See generic Get##METHOD below for why we are doing this
+    // Note that value is a string, so value.toString() is not
+    // a temporary.
+    const char* ini_ret = value.toString().data();
+    const char* hdf_ret = hdf.configGet(ini_ret);
+    if (hdf_ret != ini_ret) {
+      ini_ret = hdf_ret;
+      IniSetting::SetSystem(ini_name, ini_ret);
     }
-    
-    BOOST_AUTO_TEST_CASE(bip173_testvectors_valid)
-{
-    static const std::string CASES[] = {
-        'A12UEL5L',
-        'a12uel5l',
-        'an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs',
-        'abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw',
-        '11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j',
-        'split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w',
-        '?1ezyfcl',
-    };
-    for (const std::string& str : CASES) {
-        auto ret = bech32::Decode(str);
-        BOOST_CHECK(!ret.first.empty());
-        std::string recode = bech32::Encode(ret.first, ret.second);
-        BOOST_CHECK(!recode.empty());
-        BOOST_CHECK(CaseInsensitiveEqual(str, recode));
-    }
-}
-    
-    /* Equality between doubles is imprecise. Comparison should be done
- * with a small threshold of tolerance, rather than exact equality.
- */
-static bool DoubleEquals(double a, double b, double epsilon)
-{
-    return std::abs(a - b) < epsilon;
-}
-    
-    #ifdef __APPLE__
-// Workaround OSX linker warning: Xcode uses different default symbol
-// visibilities for static libs vs executables (see issue #25)
-#   define TINYFORMAT_HIDDEN __attribute__((visibility('hidden')))
-#else
-#   define TINYFORMAT_HIDDEN
-#endif
-    
-      bool Started() const {
-    return start_;
+    return ini_ret;
   }
-    
-    
-    {  // verify that all keys now have the new value that
-  // was set by the compaction process.
-  for (int i = 0; i < 100001; i++) {
-    char key[100];
-    snprintf(key, sizeof(key), 'B%010d', i);
-    std::string newvalue = Get(1, key);
-    ASSERT_EQ(newvalue.compare(NEW_VALUE), 0);
-  }
+  return hdf.configGet(defValue);
 }
     
-      // Make a set of all of the live *.sst files
-  std::vector<FileDescriptor> live;
-  for (auto cfd : *versions_->GetColumnFamilySet()) {
-    if (cfd->IsDropped()) {
-      continue;
-    }
-    cfd->current()->AddLiveFiles(&live);
-  }
+    #ifndef HPHP_DATA_STREAM_WRAPPER_H
+#define HPHP_DATA_STREAM_WRAPPER_H
     
-      using DBImpl::NewIterator;
-  virtual Iterator* NewIterator(const ReadOptions&,
-                                ColumnFamilyHandle* column_family) override;
+     private:
+  friend class DB;
+  inline size_t FindFile(const Slice& key);
+  Status Init(const Options& options);
     
-    #include <inttypes.h>
-#include <stdio.h>
-#include <string>
-#include <algorithm>
+    #include 'db/db_impl.h'
 #include <vector>
+#include <string>
     
     Status WriteBatchBase::SingleDelete(ColumnFamilyHandle* column_family,
                                     const SliceParts& key) {
@@ -128,64 +52,21 @@ static bool DoubleEquals(double a, double b, double epsilon)
   return SingleDelete(column_family, key_slice);
 }
     
-      virtual Status NewSequentialFile(const std::string& fname,
-                                   std::unique_ptr<SequentialFile>* result,
-                                   const EnvOptions& options) override {
-    auto status_and_enc_path = EncodePathWithNewBasename(fname);
+      virtual Status GetAbsolutePath(const std::string& db_path,
+                                 std::string* output_path) override {
+    auto status_and_enc_path = EncodePath(db_path);
     if (!status_and_enc_path.first.ok()) {
       return status_and_enc_path.first;
     }
-    return EnvWrapper::NewSequentialFile(status_and_enc_path.second, result,
-                                         options);
+    return EnvWrapper::GetAbsolutePath(status_and_enc_path.second, output_path);
   }
     
-    Status PosixSequentialFile::InvalidateCache(size_t offset, size_t length) {
-#ifndef OS_LINUX
-  (void)offset;
-  (void)length;
-  return Status::OK();
-#else
-  if (!use_direct_io()) {
-    // free OS pages
-    int ret = Fadvise(fd_, offset, length, POSIX_FADV_DONTNEED);
-    if (ret != 0) {
-      return IOError('While fadvise NotNeeded offset ' + ToString(offset) +
-                         ' len ' + ToString(length),
-                     filename_, errno);
-    }
-  }
-  return Status::OK();
-#endif
-}
+    PosixDirectory::~PosixDirectory() { close(fd_); }
     
-    
-    {  // Need to implement this so the file is truncated correctly
-  // with direct I/O
-  virtual Status Truncate(uint64_t size) override;
-  virtual Status Close() override;
-  virtual Status Append(const Slice& data) override;
-  virtual Status PositionedAppend(const Slice& data, uint64_t offset) override;
-  virtual Status Flush() override;
-  virtual Status Sync() override;
-  virtual Status Fsync() override;
-  virtual bool IsSyncThreadSafe() const override;
-  virtual bool use_direct_io() const override { return use_direct_io_; }
-  virtual void SetWriteLifeTimeHint(Env::WriteLifeTimeHint hint) override;
-  virtual uint64_t GetFileSize() override;
-  virtual Status InvalidateCache(size_t offset, size_t length) override;
-  virtual size_t GetRequiredBufferAlignment() const override {
-    return logical_sector_size_;
-  }
-#ifdef ROCKSDB_FALLOCATE_PRESENT
-  virtual Status Allocate(uint64_t offset, uint64_t len) override;
-#endif
-#ifdef ROCKSDB_RANGESYNC_PRESENT
-  virtual Status RangeSync(uint64_t offset, uint64_t nbytes) override;
-#endif
-#ifdef OS_LINUX
-  virtual size_t GetUniqueId(char* id, size_t max_size) const override;
-#endif
-};
+     public:
+  PosixMmapFile(const std::string& fname, int fd, size_t page_size,
+                const EnvOptions& options);
+  ~PosixMmapFile();
     
     
     {  MockEnvTest()
@@ -196,7 +77,18 @@ static bool DoubleEquals(double a, double b, double epsilon)
   }
 };
     
-      // create column family
-  ColumnFamilyHandle* cf;
-  s = db->CreateColumnFamily(ColumnFamilyOptions(), 'new_cf', &cf);
-  assert(s.ok());
+      bool FilterMergeOperand(int level, const rocksdb::Slice& key,
+                          const rocksdb::Slice& existing_value) const override {
+    fprintf(stderr, 'FilterMerge(%s)\n', key.ToString().c_str());
+    ++merge_count_;
+    return existing_value == 'bad';
+  }
+    
+    int main() {
+  DBOptions db_opt;
+  db_opt.create_if_missing = true;
+    }
+    
+    
+    {  return 0;
+}
