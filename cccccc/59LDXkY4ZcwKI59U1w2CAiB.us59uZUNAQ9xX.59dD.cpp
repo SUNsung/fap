0c521f9dcdbf580a5d98d67abf7ce30e9f5b60b5
SUNsung <weatherius@gmail.com>
2018-11-01 17:23:39 +0300

    Rewrite [59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp] File in: 2018-11-01 16:23:38

--- cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
+++ cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
@@ -1,138 +1,55 @@
 
         
-        Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    #if GOOGLE_CUDA
-    
-      auto scale_broadcasted = add(HloInstruction::CreateBroadcast(
-      activation_shape, scale, {feature_index}));
-  auto variance_broadcasted = add(HloInstruction::CreateBroadcast(
-      activation_shape, variance, {feature_index}));
-    
-    
-    {  return kTfLiteOk;
-}
-    
-    namespace {
-// TODO(suharshs): Move this to a common location to allow other part of the
-// repo to use it.
-template <typename T, typename... Args>
-std::unique_ptr<T> MakeUnique(Args&&... args) {
-  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
-}  // namespace
-    
-    TEST(PartialRunMgr, PartialRunRemoved) {
-  // Test that PartialRunMgr ensures that the PartialRun is deleted after
-  // ExecutorDone and PartialRunDone are called.
-  PartialRunMgr partial_run_mgr;
-  int step_id = 1;
-  CancellationManager* cancellation_manager;
-  partial_run_mgr.FindOrCreate(step_id, &cancellation_manager);
-    }
-    
-    // TODO(zongheng): this should be a general functor that powers SparseAdd and
-// ScatterNd ops.  It should be moved to its own head file, once the other ops
-// are implemented.
-template <typename Device, typename T, typename Index, int NDIMS,
-          scatter_op::UpdateOp op>
-struct ScatterNdFunctor {
-  // Returns -1 on success or a nonnegative i s.t. indices[i] is a bad index.
-  Index operator()(const Device& d, typename TTypes<Index>::ConstMatrix indices,
-                   typename TTypes<T>::ConstFlat updates,
-                   typename TTypes<T, NDIMS>::Tensor out);
-};
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    TEST_F(GrpcDebugTest, SendDebugTensorWithLargeStringAtIndex1ViaGrpcTest) {
-  Tensor tensor(DT_STRING, TensorShape({1, 2}));
-  tensor.flat<string>()(0) = 'A';
-  tensor.flat<string>()(1) = string(5000 * 1024, 'A');
-  const DebugNodeKey kDebugNodeKey('/job:localhost/replica:0/task:0/cpu:0',
-                                   'foo_tensor', 0, 'DebugIdentity');
-  const Status status = DebugIO::PublishDebugTensor(
-      kDebugNodeKey, tensor, Env::Default()->NowMicros(), {server_data_.url});
-  ASSERT_FALSE(status.ok());
-  ASSERT_NE(status.error_message().find('string value at index 1 from debug '
-                                        'node foo_tensor:0:DebugIdentity does '
-                                        'not fit gRPC message size limit'),
-            string::npos);
-  TF_ASSERT_OK(DebugIO::CloseDebugURL(server_data_.url));
-}
-    
-    /**
- * @brief Compute the index of the @f$ K @f$ max values for each datum across
- *        all dimensions @f$ (C \times H \times W) @f$.
- *
- * Intended for use after a classification layer to produce a prediction.
- * If parameter out_max_val is set to true, output is a vector of pairs
- * (max_ind, max_val) for each image. The axis parameter specifies an axis
- * along which to maximise.
- *
- * NOTE: does not implement Backwards operation.
- */
-template <typename Dtype>
-class ArgMaxLayer : public Layer<Dtype> {
- public:
-  /**
-   * @param param provides ArgMaxParameter argmax_param,
-   *     with ArgMaxLayer options:
-   *   - top_k (\b optional uint, default 1).
-   *     the number @f$ K @f$ of maximal items to output.
-   *   - out_max_val (\b optional bool, default false).
-   *     if set, output a vector of pairs (max_ind, max_val) unless axis is set then
-   *     output max_val along the specified axis.
-   *   - axis (\b optional int).
-   *     if set, maximise along the specified axis else maximise the flattened
-   *     trailing dimensions for each index of the first / num dimension.
+          /**
+   * @brief Applies the transformation defined in the data layer's
+   * transform_param block to the data.
+   *
+   * @param datum
+   *    Datum containing the data to be transformed.
+   * @param transformed_blob
+   *    This is destination blob. It can be part of top blob's data if
+   *    set_cpu_data() is used. See data_layer.cpp for an example.
    */
-  explicit ArgMaxLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+  void Transform(const Datum& datum, Blob<Dtype>* transformed_blob);
+    
+    namespace caffe {
     }
     
-      int num_spatial_axes_;
-  int bottom_dim_;
-  int top_dim_;
+      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+    
+    {  /**
+   * @brief Computes the error gradient w.r.t. the BNLL inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x}
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
     
     #include 'caffe/blob.hpp'
 #include 'caffe/layer.hpp'
 #include 'caffe/proto/caffe.pb.h'
     
-    #ifdef USE_CUDNN
-/*
- * @brief cuDNN implementation of ConvolutionLayer.
- *        Fallback to ConvolutionLayer for CPU mode.
- *
- * cuDNN accelerates convolution through forward kernels for filtering and bias
- * plus backward kernels for the gradient w.r.t. the filters, biases, and
- * inputs. Caffe + cuDNN further speeds up the computation through forward
- * parallelism across groups and backward parallelism across gradients.
- *
- * The CUDNN engine does not have memory overhead for matrix buffers. For many
- * input and filter regimes the CUDNN engine is faster than the CAFFE engine,
- * but for fully-convolutional models and large inputs the CAFFE engine can be
- * faster as long as it fits in memory.
-*/
-template <typename Dtype>
-class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
- public:
-  explicit CuDNNConvolutionLayer(const LayerParameter& param)
-      : ConvolutionLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNConvolutionLayer();
-    }
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
     
      protected:
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
@@ -140,42 +57,164 @@ class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
   virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    namespace caffe {
+    class JointBullet : public ConstraintBullet {
     }
     
-    TEST_F(ViewsConfigParserPluginTests, test_swap_view) {
-  Config c;
-  std::vector<std::string> old_views_vec;
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  old_views_vec.clear();
-  auto s = c.update(getTestConfigMap('view_test.conf'));
-  EXPECT_TRUE(s.ok());
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  EXPECT_EQ(old_views_vec[0], 'config_views.kernel_hashes_new');
+    public:
+	/// Reference frame is A
+	SliderJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Transform &frameInA, const Transform &frameInB);
+    
+    #include 'register_types.h'
+    
+    void unregister_etc_types() {
     }
     
-    class TLSConfigPlugin : public ConfigPlugin,
-                        public std::enable_shared_from_this<TLSConfigPlugin> {
- public:
-  Status setUp() override;
-  Status genConfig(std::map<std::string, std::string>& config) override;
+    jobject GDAPI godot_android_get_activity() {
+#ifdef __ANDROID__
+	JNIEnv *env = ThreadAndroid::get_env();
     }
     
     
-    {  status_ = Status(0);
+    {	ERR_EXPLAINC('There are still MemoryPool allocs in use at exit!');
+	ERR_FAIL_COND(allocs_used > 0);
+}
+
+    
+    void FuncRef::_bind_methods() {
+    }
+    
+    size_t num_threads = 31;
+size_t work_chunk  = 120;
+    
+    /*
+ * If Trace::hhbbc_time >= 1, print some stats about the program to a
+ * temporary file.  If it's greater than or equal to 2, also dump it
+ * to stdout.
+ */
+void print_stats(const Index&, const php::Program&);
+    
+    void Config::ParseIniFile(const std::string &filename,
+                          const bool is_system /* = true */) {
+  IniSettingMap ini = IniSettingMap();
+  Config::ParseIniFile(filename, ini, false, is_system);
 }
     
-    FLAG(string, aws_kinesis_stream, '', 'Name of Kinesis stream for logging')
+    #include <folly/ScopeGuard.h>
     
+      const char* from_start = from_dir + 1;
+  const char* to_start = to_file + 1;
     
+    std::string expandUser(const std::string& path,
+                       const std::string& sysUser = '');
+/**
+ * Makes sure there is ending slash by changing 'path/name' to 'path/name/'.
+ */
+std::string normalizeDir(const std::string &dirname);
+    
+      int64_t avail = bufferedLen();
+  if (m_data->m_buffer == nullptr) {
+    m_data->m_buffer = (char *)malloc(m_data->m_chunkSize * 3);
+    m_data->m_bufferSize = m_data->m_chunkSize * 3;
+  } else if (m_data->m_bufferSize < m_data->m_chunkSize * 3) {
+    auto newbuf = malloc(m_data->m_chunkSize * 3);
+    memcpy(newbuf, m_data->m_buffer, m_data->m_bufferSize);
+    free(m_data->m_buffer);
+    m_data->m_buffer = (char*) newbuf;
+    m_data->m_bufferSize = m_data->m_chunkSize * 3;
+  }
+    
+    // Build a Table file from the contents of *iter.  The generated file
+// will be named according to meta->number.  On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+Status BuildTable(const std::string& dbname,
+                  Env* env,
+                  const Options& options,
+                  TableCache* table_cache,
+                  Iterator* iter,
+                  FileMetaData* meta);
+    
+      Corrupt(kDescriptorFile, 0, 1000);
+  Status s = TryReopen();
+  ASSERT_TRUE(!s.ok());
+    
+      void MaybeIgnoreError(Status* s) const;
+    
+    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
+#define STORAGE_LEVELDB_DB_DB_ITER_H_
+    
+      iter->SeekToFirst();
+  ASSERT_EQ(IterStatus(iter), '(invalid)');
+    
+    #include <stdio.h>
+#include 'db/dbformat.h'
+#include 'port/port.h'
+#include 'util/coding.h'
+    
+      const Comparator* user_comparator() const { return user_comparator_; }
+    
+    bool GuessType(const std::string& fname, FileType* type) {
+  size_t pos = fname.rfind('/');
+  std::string basename;
+  if (pos == std::string::npos) {
+    basename = fname;
+  } else {
+    basename = std::string(fname.data() + pos + 1, fname.size() - pos - 1);
+  }
+  uint64_t ignored;
+  return ParseFileName(basename, &ignored, type);
+}
+    
+    #include 'db/dbformat.h'
+#include 'port/port.h'
+#include 'util/logging.h'
+#include 'util/testharness.h'
+    
+      ~Reader();
+    
+    TEST(LogTest, ErrorJoinsRecords) {
+  // Consider two fragmented records:
+  //    first(R1) last(R1) first(R2) last(R2)
+  // where the middle two fragments disappear.  We do not want
+  // first(R1),last(R2) to get joined and returned as a valid record.
+    }
+    
+        // Make sure that the dictionary contains all required keys, and if it does, return version value
+    // from the dictionary.
+    template <typename T>
+    inline size_t ValidateDictionary(const Dictionary& dict, const std::vector<std::wstring>& requiredKeys, const std::wstring& typeValue, size_t currentVersion)
+    { 
+        const auto& version = GetVersion(dict);
+    }
+    
+        // now pass that to concurrent reader so we can read ahead
+    // m_DataReader = new ConcurrentReader<ElemType>(m_DataReader);
+    // NOW we can init
+    // TODO: merge with the code above, but we first need to get the nbrUttPerMinibatch initialized inside each reader
+    for (const auto& ioName : m_ioNames)
     {
-    { private:
-  std::shared_ptr<KinesisLogForwarder> forwarder_{nullptr};
+        const ConfigRecordType& thisIO = hasMultipleReaders ? config(ioName) : config /*legacy*/;
+        m_dataReaders[ioName]->Init(thisIO);
+    }
+    
+    
+    {private:
+    long long m_start;
+    long long m_end;
 };
-}
+    
+        // release gradient and temp matrices that no longer needed after all the children's gradients are computed.
+    virtual void ReleaseMatricesAfterBackprop(MatrixPool& matrixPool)
+    {
+        Base::ReleaseMatricesAfterBackprop(matrixPool);
+        ReleaseMatrixToPool(m_innerproduct, matrixPool);
+        ReleaseMatrixToPool(m_rightGradient, matrixPool);
+    }
+    
+        // called from ComputationNode::ValidateInferInputDimsFrom()
+    // In case of an error, this function just backs out without updating.
+    // The caller must verify the dimensions.
+    // This is a bit weird since it is called after this node has been Validated once.
+    // BUGBUG: This will clear out any random initialization to 0. So currently this is not usable for most cases.
+    void InferInputDimsFrom(const TensorShape& otherShape);
