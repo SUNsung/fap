1a1e04acbc89881f9cbc064d32754b89598aed09
SUNsung <weatherius@gmail.com>
2018-11-01 21:52:17 +0200

    Rewrite [37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb] File in: 2018-11-01 21:52:16

--- ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
+++ ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
@@ -1,72 +1,84 @@
 
         
-        When /^I submit the password reset form$/ do
-  submit_password_reset_form
-end
+            # @return [Hash] All the lanes available, first the platform, then the lane
+    attr_accessor :lanes
     
-    Given /^I have been invited by '([^\']+)'$/ do |email|
-  AppConfig.settings.enable_registrations = false
-  @inviter = User.find_by_email(email)
-  @inviter_invite_count = @inviter.invitation_code.count
-  i = EmailInviter.new('new_invitee@example.com', @inviter)
-  i.send!
-end
+          return did_show if did_show
     
-      def confirm_on_page(page_name)
-    if page_name == 'my profile page'
-      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
-    else
-      expect(page).to have_path(path_to(page_name))
-    end
-  end
-end
+          def self.details
+        list = <<-LIST.markdown_list
+          `grouping` is just to keep your tags organised under one 'folder', defaults to 'builds'
+          `lane` is the name of the current fastlane lane
+          `prefix` is anything you want to stick in front of the version number, e.g. 'v'
+          `postfix` is anything you want to stick at the end of the version number, e.g. '-RC1'
+          `build_number` is the build number, which defaults to the value emitted by the `increment_build_number` action
+        LIST
     
-      class SendPrivate < Base
-    def perform(*_args)
-      # don't federate in cucumber
-    end
-  end
-    
-          @conv2 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
-             .increase_unread(alice)
-    
-      test_cases.each do |test_case|
-    env = test_case['env']
-    tasks = test_case['tasks']
-    
-      # An environment that can write to in-scope global variables, but doesn't
-  # create new variables in the global scope. Useful for top-level control
-  # directives.
-  class SemiGlobalEnvironment < Environment
-    def try_set_var(name, value)
-      @vars ||= {}
-      if @vars.include?(name)
-        @vars[name] = value
-        true
-      elsif @parent
-        @parent.try_set_var(name, value)
-      else
-        false
+            inner_command = 'git describe `git rev-list --tags --max-count=1`'
+        pseudocommand = 'git log --pretty=\'%B\' #{inner_command.shellescape}...HEAD'
+        expect(result).to eq(pseudocommand)
       end
-    end
+    
+            it 'executes the correct git command' do
+          allow(Fastlane::Actions).to receive(:sh).with('git add #{path}', anything).and_return('')
+          result = Fastlane::FastFile.new.parse('lane :test do
+            git_add(path: '#{path}')
+          end').runner.execute(:test)
+        end
+      end
+    
+            keychain_path = File.expand_path(File.join('~', 'Library', 'Keychains', keychain))
+        expected_command = 'security import #{cert_name} -k '#{keychain_path}' -P #{password} -T /usr/bin/codesign -T /usr/bin/security &> /dev/null'
+    
+          it 'works with multiple ignore patterns' do
+        pattern1 = 'Pods/*'
+        pattern2 = '../**/*/Xcode*'
+        result = Fastlane::FastFile.new.parse('lane :test do
+          slather({
+            ignore: ['#{pattern1}', '#{pattern2}'],
+            proj: 'foo.xcodeproj'
+          })
+        end').runner.execute(:test)
+    
+        # execute bundle install and capture any $stdout output. any raised exception in the process will be trapped
+    # and returned. logs errors to $stdout.
+    # @param [Hash] options invoke options with default values, :max_tries => 10, :clean => false, :install => false, :update => false
+    # @option options [Boolean] :max_tries The number of times bundler is going to try the installation before failing (default: 10)
+    # @option options [Boolean] :clean It cleans the unused gems (default: false)
+    # @option options [Boolean] :install Run the installation of a set of gems defined in a Gemfile (default: false)
+    # @option options [Boolean, String, Array] :update Update the current environment, must be either false or a String or an Array of String (default: false)
+    # @option options [Boolean] :local Do not attempt to fetch gems remotely and use the gem cache instead (default: false)
+    # @option options [Boolean] :package Locks and then caches all dependencies to be reused later on (default: false)
+    # @option options [Boolean] :all It packages dependencies defined with :git or :path (default: false)
+    # @option options [Array] :without  Exclude gems that are part of the specified named group (default: [:development])
+    # @return [String, Exception] the installation captured output and any raised exception or nil if none
+    def invoke!(options = {})
+      options = {:max_tries => 10, :clean => false, :install => false, :update => false, :local => false,
+                 :jobs => 12, :all => false, :package => false, :without => [:development]}.merge(options)
+      options[:without] = Array(options[:without])
+      options[:update] = Array(options[:update]) if options[:update]
+    
+        context 'when an environment variable is set' do
+      before do
+        ENV['FunString'] = 'fancy'
+        ENV['FunBool'] = 'true'
+        ENV['SERVER_LS_TEST_ADDRESS'] = 'some.host.address.tld'
+      end
+    
+      it 'records when the config was read' do
+    expect(subject.read_at).to be <= Time.now
   end
-end
-
     
-        # Tells optparse how to parse the arguments
-    # available for all executables.
-    #
-    # This is meant to be overridden by subclasses
-    # so they can add their own options.
-    #
-    # @param opts [OptionParser]
-    def set_opts(opts)
-      Sass::Util.abstract(this)
-    end
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-dns')
+          end
+        end
     
-          if @options[:update]
-        Sass::Plugin.update_stylesheets(files)
-        exit 1 if had_error
-        return
+        context 'with a specific plugin' do
+      let(:plugin_name) { 'logstash-input-stdin' }
+      it 'list the plugin and display the plugin name' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
       end
