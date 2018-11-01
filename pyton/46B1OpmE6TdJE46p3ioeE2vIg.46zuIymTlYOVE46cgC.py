9e43c13fbf656e22ac88af0c29a3d2bc405539a9
SUNsung <weatherius@gmail.com>
2018-11-01 05:43:35 +0300

    Rewrite [46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py] File in: 2018-11-01 04:43:35

--- pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
+++ pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
@@ -1,112 +1,98 @@
 
         
-            def __init__(self):
-        self.users_by_id = {}  # key: user id, value: User
+        
+@pytest.fixture(autouse=True)
+def functional(request):
+    if request.node.get_marker('functional') \
+            and not request.config.getoption('enable_functional'):
+        pytest.skip('functional tests are disabled')
     
     
-if __name__ == '__main__':
-    HitCounts.run()
-
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
+    proc.sendline(u'fish')
+    return proc
     
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
+    python_2 = ('thefuck/python2-zsh',
+            u'''FROM python:2
+                RUN apt-get update
+                RUN apt-get install -yy zsh''',
+            u'sh')
     
-        def set(self, key, value):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                item.value = value
-                return
-        self.table[hash_index].append(Item(key, value))
-    
-    
-    
-    # This flag is used for an experiment where one wants to know if the dynamics
-# learned by the generator generalize across conditions. In that case, you might
-# train up a model on one set of data, and then only further train the encoder
-# on another set of data (the conditions to be tested) so that the model is
-# forced to use the same dynamics to describe that data. If you don't care about
-# that particular experiment, this flag should always be false.
-flags.DEFINE_boolean('do_train_encoder_only', DO_TRAIN_ENCODER_ONLY,
-                     'Train only the encoder weights.')
-    
-    # write out the dataset
-write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
-print ('Saved to ', os.path.join(FLAGS.save_dir,
-                                 FLAGS.datafile_name + '_' + dataset_name))
+        new_command = get_new_command(Command('apt update', match_output))
+    assert new_command == 'apt list --upgradable'
 
     
     
-  Args:
-    hparams:  MaskGAN hyperparameters.
-    log_probs:  tf.float32 Tensor of log probailities of the tokens selected by
-      the Generator.  Shape [batch_size, sequence_length].
-    dis_predictions:  tf.float32 Tensor of the predictions from the
-      Discriminator.  Shape [batch_size, sequence_length].
-    present:  tf.bool Tensor indicating which tokens are present.  Shape
-      [batch_size, sequence_length].
-    estimated_values:  tf.float32 Tensor of estimated state values of tokens.
-      Shape [batch_size, sequence_length]
+def test_match():
+    assert match(Command('apt list --upgradable', match_output))
+    assert match(Command('sudo apt list --upgradable', match_output))
     
-        clip_ops.append(tf.assign(var, clipped_var))
-  return tf.group(*clip_ops)
+      * dynamodb
+'''
     
-        # apply the blueprints to the app
-    from flaskr import auth, blog
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+    from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
     
     
-def with_metaclass(meta, *bases):
-    '''Create a base class with a metaclass.'''
-    # This requires a bit of explanation: the basic idea is to make a
-    # dummy metaclass for one level of class instantiation that replaces
-    # itself with the actual metaclass.
-    class metaclass(type):
-        def __new__(cls, name, this_bases, d):
-            return meta(name, bases, d)
-    return type.__new__(metaclass, 'temporary_class', (), {})
+METRICS = {
+    'f1': partial(f1_score, average='micro'),
+    'f1-by-sample': partial(f1_score, average='samples'),
+    'accuracy': accuracy_score,
+    'hamming': hamming_loss,
+    'jaccard': jaccard_similarity_score,
+}
     
-    
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
+    plot(euclidean_distances)
+plot(rbf_kernels)
+plt.show()
 
     
-        @classmethod
-    def get_session_store_class(cls):
-        from django.contrib.sessions.backends.db import SessionStore
-        return SessionStore
+        ###########################################################################
+    # Set transformer input
+    ###########################################################################
+    transformers = {}
     
-    # datasets available: ['http', 'smtp', 'SA', 'SF', 'shuttle', 'forestcover']
-datasets = ['http', 'smtp', 'SA', 'SF', 'shuttle', 'forestcover']
+        # Print results
+    ###########################################################################
+    print('Script arguments')
+    print('===========================')
+    arguments = vars(opts)
+    print('%s \t | %s ' % ('Arguments'.ljust(16),
+                           'Value'.center(12),))
+    print(25 * '-' + ('|' + '-' * 14) * 1)
+    for key, value in arguments.items():
+        print('%s \t | %s ' % (str(key).ljust(16),
+                               str(value).strip().center(12)))
+    print('')
     
-                gc.collect()
-            print('benchmarking lars_path (with Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            G = np.dot(X.T, X)  # precomputed Gram matrix
-            Xy = np.dot(X.T, y)
-            lars_path(X, y, Xy=Xy, Gram=G, method='lasso')
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            results['lars_path (with Gram)'].append(delta)
     
-            plt.text(1.01, 0.5, title_string,
-                 transform=ax.transAxes, rotation=-90,
-                 ha='left', va='center', fontsize=20)
+def abspath(path):
+    '''Return an absolute path.'''
+    path = os.fspath(path)
+    if not isabs(path):
+        if isinstance(path, bytes):
+            cwd = os.getcwdb()
+        else:
+            cwd = os.getcwd()
+        path = join(cwd, path)
+    return normpath(path)
     
-    ###############################################################################
-clf = SGDRegressor(penalty='l1', alpha=.2, fit_intercept=True, max_iter=2000,
-                   tol=None)
-clf.fit(X_train, y_train)
-print('model sparsity: %f' % sparsity_ratio(clf.coef_))
+            out = self.check_warnings_filters('once::UserWarning',
+                                          'always::UserWarning',
+                                          use_pywarning=True)
+        self.assertEqual(out, expected_filters)
+    
+        def test_subinterps_distinct_state(self):
+        for run in self.run_repeated_init_and_subinterpreters():
+            main, *subs, _ = run
+    
+    # Of course, there are lots of email messages that could break this simple
+# minded program, but it will handle the most common ones.
