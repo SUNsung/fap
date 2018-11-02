036d04535f1602d7fee9dfc0b2837f69a337501b
SUNsung <weatherius@gmail.com>
2018-11-02 18:51:07 +0200

    Rewrite [50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py] File in: 2018-11-02 18:51:06

--- pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
+++ pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
@@ -1,247 +1,237 @@
 
         
-          dim = embeddings.shape[1]
-    
-        h = tf.matmul(self.input_vec, self.weights1)
-    self.output = h
-    
-        Returns:
-      the labels.
-    '''
-    return session.run(self.labels_to_load,
-                       feed_dict={self.instances_to_load: batch_instances})
-    
-      LFADS generates a number of outputs for each examples, and these are all
-  saved.  They are:
-    The mean and variance of the prior of g0.
-    The mean and variance of approximate posterior of g0.
-    The control inputs (if enabled)
-    The initial conditions, g0, for all examples.
-    The generator states for all time.
-    The factors for all time.
-    The rates for all time.
-    
-      Args:
-    hparams:  MaskGAN hyperparameters.
-    learning_rate:  tf.Variable scalar learning rate.
-    final_gen_objective:  Scalar final REINFORCE objective for the sequence.
-    averages_op:  ExponentialMovingAverage apply average op to
-      maintain the baseline.
-    global_step:  global_step tf.Variable.
-    
-                # >leading or trailing LWS MAY be removed without
-            # >changing the semantics of the field value'
-            # -https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
-            # Also, requests raises `InvalidHeader` for leading spaces.
-            value = value.strip()
-    
-        def iter_body(self, chunk_size=1):
-        return self._orig.iter_content(chunk_size=chunk_size)
-    
-        def __iter__(self):
-        return iter(self._plugins)
-    
-    
-@mock.patch('httpie.input.AuthCredentials._getpass',
-            new=lambda self, prompt: 'password')
-def test_password_prompt(httpbin):
-    r = http('--auth', 'user',
-             'GET', httpbin.url + '/basic-auth/user/password')
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-    
-def test_default_headers_case_insensitive(httpbin):
-    '''
-    <https://github.com/jakubroztocil/httpie/issues/644>
-    '''
-    r = http(
-        '--debug',
-        '--print=H',
-        httpbin.url + '/post',
-        'CONTENT-TYPE:application/json-patch+json',
-        'a=b',
-    )
-    assert 'CONTENT-TYPE: application/json-patch+json' in r
-    assert 'Content-Type' not in r
-    
-    
-@pytest.mark.skip('unimplemented')
-def test_unset_host_header(httpbin_both):
-    r = http('GET', httpbin_both + '/headers')
-    assert 'Host' in r.json['headers']  # default Host present
-    
-    - name: create a batch of invalidations using an alias as a reference and one path using a wildcard match
-  cloudfront_invalidation:
-    alias: alias.test.com
-    caller_reference: testing 123
-    target_paths:
-      - /testpathone/test4.css
-      - /testpathtwo/test5.js
-      - /testpaththree/*
-    
-    import re
-    
-        def tag_cgw_name(self, gw_id, name):
-        response = self.ec2.create_tags(
-            DryRun=False,
-            Resources=[
-                gw_id,
-            ],
-            Tags=[
-                {
-                    'Key': 'Name',
-                    'Value': name
-                },
-            ]
-        )
-        return response
-    
-    
-def main():
-    argument_spec = ec2_argument_spec()
-    argument_spec.update(
-        dict(
-            name=dict(required=False, default=[], type='list'),
-            sort=dict(required=False, default=None,
-                      choices=['launch_configuration_name', 'image_id', 'created_time', 'instance_type', 'kernel_id', 'ramdisk_id', 'key_name']),
-            sort_order=dict(required=False, default='ascending',
-                            choices=['ascending', 'descending']),
-            sort_start=dict(required=False),
-            sort_end=dict(required=False),
-        )
-    )
-    
-        result = dict(gateway_id=eigw_id, vpc_id=vpc_id)
-    changed = False
-    
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-    
-            try:
-            matching_groups = conn.describe_cache_subnet_groups(group_name, max_records=100)
-            exists = len(matching_groups) > 0
-        except BotoServerError as e:
-            if e.error_code != 'CacheSubnetGroupNotFoundFault':
-                module.fail_json(msg=e.error_message)
-    
-            try:
-            lambda_facts.update(versions=client.list_versions_by_function(FunctionName=function_name, **params)['Versions'])
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                lambda_facts.update(versions=[])
-            else:
-                module.fail_json_aws(e, msg='Trying to get {0} versions'.format(function_name))
-    else:
-        module.fail_json(msg='Parameter function_name required for query=versions.')
-    
-    # The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-version = '0'
-# The full version, including alpha/beta/rc tags.
-release = '0'
-    
-    from acme import client
-from acme import messages
-    
-    
-def get_internal_aug_path(vhost_path):
-    '''Get the Augeas path for a vhost with the file path removed.
-    
-            Examples:
-    
-        @certbot_util.patch_get_utility()
-    def test_multiple_names(self, mock_util):
-        mock_util().menu.return_value = (display_util.OK, 5)
-    
-            mock_exists.return_value = True
-        self.sni.configurator.parser.update_runtime_variables = mock.Mock()
-    
-    # -- Options for LaTeX output ---------------------------------------------
-    
-    
-def get_github_url(app, view, path):
-    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
-    return (
-        github_fmt.format(app.config.edit_on_github_project, view,
-                          app.config.edit_on_github_branch,
-                          app.config.edit_on_github_src_path, path))
-    
-        def scan_completed_callback(scan_wizard, result, address, name):
-        '''Restart scan wizard to constantly check for new buttons.'''
-        if result == pyflic.ScanWizardResult.WizardSuccess:
-            _LOGGER.info('Found new button %s', address)
-        elif result != pyflic.ScanWizardResult.WizardFailedTimeout:
-            _LOGGER.warning(
-                'Failed to connect to button %s. Reason: %s', address, result)
-    
-        def random_see(dev_id, name):
-        '''Randomize a sighting.'''
-        see(
-            dev_id=dev_id,
-            host_name=name,
-            gps=(hass.config.latitude + offset(),
-                 hass.config.longitude + offset()),
-            gps_accuracy=random.randrange(50, 150),
-            battery=random.randrange(10, 90)
-        )
-    
-    
-def get_scanner(hass, config):
-    '''Validate the configuration and return a Linksys AP scanner.'''
-    try:
-        return LinksysSmartWifiDeviceScanner(config[DOMAIN])
-    except ConnectionError:
-        return None
-    
-        return True
-
-    
-            self.last_results = {}
-        self.token = _get_token(self.host, self.username, self.password)
-    
-    ATTR_FILENAME = 'filename'
-ATTR_SUBDIR = 'subdir'
-ATTR_URL = 'url'
-ATTR_OVERWRITE = 'overwrite'
-    
-    For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/emoncms_history/
-'''
-import logging
-from datetime import timedelta
+            def get_prep_lookup(self):
+        return [str(item) for item in self.rhs]
     
         def __init__(self):
-        self.trans = dict(dog='σκύλος', cat='γάτα')
+        self.name = 'pgcrypto'
     
-        def main_method(self):
-        '''will execute either _static_method_1 or _static_method_2
+        try:
+        oids, array_oids = get_hstore_oids(connection.alias)
+        register_hstore(connection.connection, globally=True, oid=oids, array_oid=array_oids)
+    except ProgrammingError:
+        # Hstore is not available on the database.
+        #
+        # If someone tries to create an hstore field it will error there.
+        # This is necessary as someone may be using PSQL without extensions
+        # installed but be using other features of contrib.postgres.
+        #
+        # This is also needed in order to create the connection in order to
+        # install the hstore extension.
+        pass
     
-        def amount(self, val):
-        print(val, end=' ')
-        return self
+        def __call__(self, value):
+        keys = set(value)
+        missing_keys = self.keys - keys
+        if missing_keys:
+            raise ValidationError(
+                self.messages['missing_keys'],
+                code='missing_keys',
+                params={'keys': ', '.join(missing_keys)},
+            )
+        if self.strict:
+            extra_keys = keys - self.keys
+            if extra_keys:
+                raise ValidationError(
+                    self.messages['extra_keys'],
+                    code='extra_keys',
+                    params={'keys': ', '.join(extra_keys)},
+                )
     
-    ### OUTPUT ###
-# jim got cartoon
-# jack got music
-# gee got movie
-# jim got cartoon
-# jim got cartoon
-# gee got movie
-
+        def test_lower_items(self):
+        assert list(self.case_insensitive_dict.lower_items()) == [('accept', 'application/json')]
     
-        sample_queue.put('sam')
-    test_object(sample_queue)
-    print('Outside func: {}'.format(sample_queue.get()))
+            String:                    '#4e9a06',        # class: 's'
+        String.Backtick:           '#4e9a06',        # class: 'sb'
+        String.Char:               '#4e9a06',        # class: 'sc'
+        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
+        String.Double:             '#4e9a06',        # class: 's2'
+        String.Escape:             '#4e9a06',        # class: 'se'
+        String.Heredoc:            '#4e9a06',        # class: 'sh'
+        String.Interpol:           '#4e9a06',        # class: 'si'
+        String.Other:              '#4e9a06',        # class: 'sx'
+        String.Regex:              '#4e9a06',        # class: 'sr'
+        String.Single:             '#4e9a06',        # class: 's1'
+        String.Symbol:             '#4e9a06',        # class: 'ss'
+    
+       >>> import requests
+   >>> r = requests.get('https://www.python.org')
+   >>> r.status_code
+   200
+   >>> 'Python is a programming language' in r.content
+   True
+    
+    from requests.help import info
     
     
-class MidnightTimeProvider(object):
+class CheckIp(front_base.check_ip.CheckIp):
+    def check_response(self, response):
+        server_type = response.headers.get('Server', '')
+        self.logger.debug('status:%d', response.status)
+        self.logger.debug('Server type:%s', server_type)
+    
+    from cert_util import CertUtil
+import simple_http_server
+import proxy_handler
+import env_info
+from front import front, direct_front
+    
+    # begin[licence]
+#
+# [The 'BSD licence']
+# Copyright (c) 2005-2008 Terence Parr
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# end[licensc]
+    
+    class MismatchedTokenException(RecognitionException):
+    '''@brief A mismatched char or Token or tree node.'''
+    
+    def __init__(self, expecting, input):
+        RecognitionException.__init__(self, input)
+        self.expecting = expecting
+        
+    
+        def getTokenIndex(self):
+        return self.index
+    
+    def setTokenIndex(self, index):
+        self.index = index
+    
+            video_id = match1(html, r'data-brightcove-id='(\d+)'')
+        
+        assert account_number, video_id
+    
+                if stream_id not in self.streams:
+                log.e('[Error] Invalid video format.')
+                log.e('Run \'-i\' command with no specific video format to view all available formats.')
+                exit(2)
+        else:
+            # Extract stream with the best quality
+            stream_id = self.streams_sorted[0]['id']
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
+    
+    
+def fix_coub_video_file(file_path):
+    with open(file_path, 'r+b') as file:
+        file.seek(0)
+        file.write(bytes(2))
+    
+        if re.match(r'https?://movie', url):
+        title = match1(html, 'name='description' content='([^']+)')
+        tid = match1(url, 'trailer/(\d+)')
+        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
+        type, ext, size = url_info(real_url)
+    
+        title = r1(r'<title id='pageTitle'>(.+)</title>', html)
+    
+    tmpl_api_call_photo_info = (
+    'https://api.flickr.com/services/rest?'
+    '&format=json&nojsoncallback=1'
+    '&method=flickr.photos.getInfo'
+    '&api_key=%s'
+    '&photo_id=%s'
+)
+    
+                if item.get(CONF_TEXT) is not None:
+                if isinstance(item.get(CONF_TEXT), template.Template):
+                    output[ATTR_MAIN_TEXT] = item[CONF_TEXT].async_render()
+                else:
+                    output[ATTR_MAIN_TEXT] = item.get(CONF_TEXT)
+    
+            _LOGGER.debug('Sent event %s', event.data.get('entity_id'))
+    
+    
+def setup_scanner(hass, config, see, discovery_info=None):
+    '''Set up the demo tracker.'''
+    def offset():
+        '''Return random offset.'''
+        return (random.randrange(500, 2000)) / 2e5 * random.choice((-1, 1))
+    
+        return scanner if scanner.success_init else None
+    
+    # entity attributes
+ATTR_ACCOUNTNAME = 'account_name'
+ATTR_INTERVAL = 'interval'
+ATTR_DEVICENAME = 'device_name'
+ATTR_BATTERY = 'battery'
+ATTR_DISTANCE = 'distance'
+ATTR_DEVICESTATUS = 'device_status'
+ATTR_LOWPOWERMODE = 'low_power_mode'
+ATTR_BATTERYSTATUS = 'battery_status'
+    
+    
+def _arp(ip_address):
+    '''Get the MAC address for a given IP.'''
+    cmd = ['arp', '-n', ip_address]
+    arp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    out, _ = arp.communicate()
+    match = re.search(r'(([0-9A-Fa-f]{1,2}\:){5}[0-9A-Fa-f]{1,2})', str(out))
+    if match:
+        return match.group(0)
+    _LOGGER.info('No MAC address found for %s', ip_address)
+    return None
+    
+                    overwrite = service.data.get(ATTR_OVERWRITE)
+    
+        hass.bus.listen(EVENT_STATE_CHANGED, dweet_event_listener)
+    
+            if self.upnp_bind_multicast:
+            ssdp_socket.bind(('', 1900))
+        else:
+            ssdp_socket.bind((self.host_ip_addr, 1900))
+    
+        @classmethod
+    def _class_method_2(cls):
+        print('Value {}'.format(cls.x2))
+    
+    *TL;DR80
+Traverses a container and accesses the container's elements.
+'''
+    
+        def insert(self):
+        print('Inserting the execution begin status in the Database')
+        time.sleep(0.1)
+        # Following code is to simulate a communication from DB to TC
+        if random.randrange(1, 4) == 3:
+            return -1
+    
+        This is, in fact, just syntactic sugar around a memento closure.
     '''
-    Class implemented as hard-coded stub (in contrast to configurable stub).
-    '''
+    
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        '''
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
+    
+        sample_queue = queue.Queue()
     
     
 class ProductionCodeTimeProvider(object):
@@ -251,4 +241,6 @@ class ProductionCodeTimeProvider(object):
     '''
     
     
-class GraphSearch:
+print(graph1.find_path('A', 'D'))
+print(graph1.find_all_path('A', 'D'))
+print(graph1.find_shortest_path('A', 'D'))
