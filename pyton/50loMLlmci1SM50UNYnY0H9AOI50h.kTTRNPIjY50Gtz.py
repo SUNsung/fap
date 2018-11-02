6d857b8e7323c3cd832f34291d0d401544b16910
SUNsung <weatherius@gmail.com>
2018-11-02 04:22:04 +0200

    Rewrite [50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py] File in: 2018-11-02 04:22:03

--- pyton/50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py
+++ pyton/50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py
@@ -1,112 +1,89 @@
 
         
-            for group in opt_parser.option_groups:
-        for option in group.option_list:
-            long_option = option.get_opt_string().strip('-')
-            complete_cmd = ['complete', '--command', 'youtube-dl', '--long-option', long_option]
-            if option._short_opts:
-                complete_cmd += ['--short-option', option._short_opts[0].strip('-')]
-            if option.help != optparse.SUPPRESS_HELP:
-                complete_cmd += ['--description', option.help]
-            complete_cmd.extend(EXTRA_ARGS.get(long_option, []))
-            commands.append(shell_quote(complete_cmd))
+        # Ignore noisy twisted deprecation warnings
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
+del warnings
     
-    secret_msg = b'Secret message goes here'
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('--get', dest='get', metavar='SETTING',
+            help='print raw setting value')
+        parser.add_option('--getbool', dest='getbool', metavar='SETTING',
+            help='print setting value, interpreted as a boolean')
+        parser.add_option('--getint', dest='getint', metavar='SETTING',
+            help='print setting value, interpreted as an integer')
+        parser.add_option('--getfloat', dest='getfloat', metavar='SETTING',
+            help='print setting value, interpreted as a float')
+        parser.add_option('--getlist', dest='getlist', metavar='SETTING',
+            help='print setting value, interpreted as a list')
     
-    entries = []
-for v in versions:
-    fields = v.split('.')
-    year, month, day = map(int, fields[:3])
-    faked = 0
-    patchlevel = 0
-    while True:
-        try:
-            datetime.date(year, month, day)
-        except ValueError:
-            day -= 1
-            faked += 1
-            assert day > 0
-            continue
-        break
-    if len(fields) >= 4:
-        try:
-            patchlevel = int(fields[3])
-        except ValueError:
-            patchlevel = 1
-    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
-    
-    
-def get_base_name(base):
-    if base is InfoExtractor:
-        return 'LazyLoadExtractor'
-    elif base is SearchInfoExtractor:
-        return 'LazyLoadSearchExtractor'
-    else:
-        return base.__name__
-    
-    for page in itertools.count(1):
-    releases = json.loads(compat_urllib_request.urlopen(
-        'https://api.github.com/repos/rg3/youtube-dl/releases?page=%s' % page
-    ).read().decode('utf-8'))
-    
-    from youtube_dl.aes import aes_decrypt, aes_encrypt, aes_cbc_decrypt, aes_cbc_encrypt, aes_decrypt_text
-from youtube_dl.utils import bytes_to_intlist, intlist_to_bytes
-import base64
-    
-    
-def runtest(ns, test):
-    '''Run a single test.
-    
-    import xml.etree.ElementTree as ET
-    
-            self.abcd_parent = ErrorRaisingArgumentParser(add_help=False)
-        self.abcd_parent.add_argument('a')
-        self.abcd_parent.add_argument('-b')
-        c_group = self.abcd_parent.add_argument_group('c')
-        c_group.add_argument('--d')
-    
-            class FloatSizeof:
-            def __sizeof__(self):
-                return 4.5
-        self.assertRaises(TypeError, sys.getsizeof, FloatSizeof())
-        self.assertIs(sys.getsizeof(FloatSizeof(), sentinel), sentinel)
-    
-        def get_output(self, *args, failure=False, **kw):
-        kw = dict(self.DEFAULT_ENV, **kw)
-        if failure:
-            out = assert_python_failure(*args, **kw)
-            out = out[2]
+        def run(self, args, opts):
+        if opts.verbose:
+            versions = scrapy_components_versions()
+            width = max(len(n) for (n, _) in versions)
+            patt = '%-{}s : %s'.format(width)
+            for name, version in versions:
+                print(patt % (name, version))
         else:
-            out = assert_python_ok(*args, **kw)
-            out = out[1]
-        return out.decode().rstrip('\n\r')
+            print('Scrapy %s' % scrapy.__version__)
     
-        # compile the pattern
-    _compile(code, p.data, flags)
+    # A list of ignored prefixes for module index sorting.
+# modindex_common_prefix = []
+    
+        hass.services.register(DOMAIN, SERVICE_BROWSE_URL,
+                           lambda service:
+                           webbrowser.open(service.data[ATTR_URL]),
+                           schema=SERVICE_BROWSE_URL_SCHEMA)
+    
+            _LOGGER.debug('Sent metric %s: %s (tags: %s)', metric, value, tags)
+    
+    For more details about this platform, please refer to the documentation
+https://home-assistant.io/components/demo/
+'''
+import random
     
     
-# Split a path in a drive specification (a drive letter followed by a
-# colon) and the path specification.
-# It is always true that drivespec + pathspec == p
-def splitdrive(p):
-    '''Split a pathname into drive/UNC sharepoint and relative path specifiers.
-    Returns a 2-tuple (drive_or_unc, path); either part may be empty.
+def _retrieve_list(host, token, **kwargs):
+    '''Get device list for the given host.'''
+    url = 'http://{}/cgi-bin/luci/;stok={}/api/misystem/devicelist'
+    url = url.format(host, token)
+    try:
+        res = requests.get(url, timeout=5, **kwargs)
+    except requests.exceptions.Timeout:
+        _LOGGER.exception(
+            'Connection to the router timed out at URL %s', url)
+        return
+    if res.status_code != 200:
+        _LOGGER.exception(
+            'Connection failed with http code %s', res.status_code)
+        return
+    try:
+        result = res.json()
+    except ValueError:
+        # If json decoder could not parse the response
+        _LOGGER.exception('Failed to parse response from mi router')
+        return
+    try:
+        xiaomi_code = result['code']
+    except KeyError:
+        _LOGGER.exception(
+            'No field code in response from mi router. %s', result)
+        return
+    if xiaomi_code == 0:
+        try:
+            return result['list']
+        except KeyError:
+            _LOGGER.exception('No list in response from mi router. %s', result)
+            return
+    else:
+        _LOGGER.info(
+            'Receive wrong Xiaomi code %s, expected 0 in response %s',
+            xiaomi_code, result)
+        return
     
-        def read_text(self, encoding=None, errors=None):
-        '''
-        Open the file in text mode, read it, and close the file.
-        '''
-        with self.open(mode='r', encoding=encoding, errors=errors) as f:
-            return f.read()
-    
-        def test_str_common(self):
-        # Canonicalized paths roundtrip
-        for pathstr in ('a', 'a/b', 'a/b/c', '/', '/a/b', '/a/b/c'):
-            self._check_str(pathstr, (pathstr,))
-        # Special case for the empty path
-        self._check_str('.', ('',))
-        # Other tests for str() are in test_equivalences()
-    
-    import unittest
-import sys
-from ctypes import *
+                    if req.status_code != 200:
+                    _LOGGER.warning(
+                        'downloading '%s' failed, status_code=%d',
+                        url,
+                        req.status_code)
