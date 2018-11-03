0cf8d95e39727373f12397c01fe11fc404360520
SUNsung <weatherius@gmail.com>
2018-11-03 22:03:54 +0200

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2018-11-03 22:03:53

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,108 +1,29 @@
 
         
-                  @object_name.sub!(/\[\]$/, '') || @object_name.sub!(/\[\]\]$/, ']')
-          @object = retrieve_object(options.delete(:object))
-          @skip_default_ids = options.delete(:skip_default_ids)
-          @allow_method_names_outside_object = options.delete(:allow_method_names_outside_object)
-          @options = options
-    
-          def instrument(name, **options) # :doc:
-        options[:identifier] ||= (@template && @template.identifier) || @path
-    
-    unless invalids.empty?
-  puts '\n\nFailed links:'
-  invalids.each do |link|
-    puts '- #{link}'
-  end
-  puts 'Done with errors.'
-  exit(1)
-end
-    
-      # Check if there is no signed in user before doing the sign out.
-  #
-  # If there is no signed in user, it will set the flash message and redirect
-  # to the after_sign_out path.
-  def verify_signed_out_user
-    if all_signed_out?
-      set_flash_message! :notice, :already_signed_out
-    
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
-    
-        include Rails.application.routes.url_helpers
-    include Rails.application.routes.mounted_helpers
-    
-    class Keg
-  class AlreadyLinkedError < RuntimeError
-    def initialize(keg)
-      super <<~EOS
-        Cannot link #{keg.name}
-        Another version is already linked: #{keg.linked_keg_record.resolved_path}
-      EOS
-    end
-  end
-    
-        if GitHub.api_credentials_type == :none
-      puts <<~EOS
-        You can create a new personal access token:
-          #{GitHub::ALL_SCOPES_URL}
-        #{Utils::Shell.set_variable_in_profile('HOMEBREW_GITHUB_API_TOKEN', 'your_token_here')}
-    
-      def append(*paths)
-    @paths = parse(*@paths, *paths)
-    self
-  end
-    
-    if defined?(Gem)
-  GEMS_AND_ROOT_DIRECTORIES = {
-    'sinatra' => '.',
-    'sinatra-contrib' => './sinatra-contrib',
-    'rack-protection' => './rack-protection'
-  }
-    
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
-    end
-  end
-    
-      # insert data
-  fields.each do |field, values|
-    updated = '  s.#{field} = ['
-    updated << values.map { |v| '\n    %p' % v }.join(',')
-    updated << '\n  ]'
-    content.sub!(/  s\.#{field} = \[\n(    .*\n)*  \]/, updated)
-  end
-    
-          def drop_session(env)
-        session(env).clear if session? env
+              def version_slug
+        return if version.blank?
+        slug = version.downcase
+        slug.gsub! '+', 'p'
+        slug.gsub! '#', 's'
+        slug.gsub! %r{[^a-z0-9\_\.]}, '_'
+        slug
       end
     
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   XSS
-    # Supported browsers:: all
-    # More infos::         http://en.wikipedia.org/wiki/Cross-site_scripting
-    #
-    # Automatically escapes Rack::Request#params so they can be embedded in HTML
-    # or JavaScript without any further issues. Calls +html_safe+ on the escaped
-    # strings if defined, to avoid double-escaping in Rails.
-    #
-    # Options:
-    # escape:: What escaping modes to use, should be Symbol or Array of Symbols.
-    #          Available: :html (default), :javascript, :url
-    class EscapedParams < Base
-      extend Rack::Utils
+        def subpath_to(url)
+      base_url.subpath_to url, ignore_case: true
+    end
+    
+        def load_capybara_selenium
+      require 'capybara/dsl'
+      require 'selenium/webdriver'
+      Capybara.register_driver :chrome do |app|
+        options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+        Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+      end
+      Capybara.javascript_driver = :chrome
+      Capybara.current_driver = :chrome
+      Capybara.run_server = false
+      Capybara
+    end
+    
+              node.before(%(<div class='pre-title'>#{node['title']}</div>)) if node['title']
