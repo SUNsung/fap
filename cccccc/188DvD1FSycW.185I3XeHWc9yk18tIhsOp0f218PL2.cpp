b934883e674bba1cbdafbf29fa84ec9bff82d39c
SUNsung <weatherius@gmail.com>
2018-11-04 00:32:34 +0200

    Rewrite [188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp] File in: 2018-11-04 00:32:34

--- cccccc/188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp
+++ cccccc/188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp
@@ -1,76 +1,244 @@
 
         
-        Base::Base(int id,
-           const base::WeakPtr<ObjectManager>& object_manager,
-           const base::DictionaryValue& option,
-	   const std::string& extension_id)
-    : extension_id_(extension_id),
-      id_(id),
-      delay_destruction_(false),
-      pending_destruction_(false),
-      object_manager_(object_manager) {
-}
+        // Godot TO Bullet
+extern void G_TO_B(Vector3 const &inVal, btVector3 &outVal);
+extern void INVERT_G_TO_B(Vector3 const &inVal, btVector3 &outVal);
+extern void G_TO_B(Basis const &inVal, btMatrix3x3 &outVal);
+extern void INVERT_G_TO_B(Basis const &inVal, btMatrix3x3 &outVal);
+extern void G_TO_B(Transform const &inVal, btTransform &outVal);
     
+    #include 'bullet_utilities.h'
+#include 'rid_bullet.h'
     
-    {}  // namespace remote
+    public:
+	HingeJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Transform &frameA, const Transform &frameB);
+	HingeJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Vector3 &pivotInA, const Vector3 &pivotInB, const Vector3 &axisInA, const Vector3 &axisInB);
     
+    jobject GDAPI godot_android_get_activity() {
+#ifdef __ANDROID__
+	JNIEnv *env = ThreadAndroid::get_env();
+    }
     
-    {} // namespace nwapi
+    	for (uint32_t i = 0; i < alloc_count - 1; i++) {
+    }
+    
+    std::unique_ptr<php::Unit> parse_unit(php::Program& prog,
+                                      std::unique_ptr<UnitEmitter> ue);
+    
+    Object APCCollection::createObject() const {
+  if (m_arrayHandle->isTypedValue()) {
+    Variant local(m_arrayHandle->toLocal());
+    assertx(local.isArray());
+    return Object::attach(
+      collections::alloc(m_colType, local.getArrayData())
+    );
+  }
+    }
+    
+    #endif // HPHP_DATA_STREAM_WRAPPER_H
 
     
-      focus_manager_ = NULL;
-  window_ = NULL;
     
-    void MenuItem::OnClick(GtkWidget* widget) {
-  if (block_active_)
-    return;
+    {
+    {      write(ini_fd, line.c_str(), line.length());
+      write(ini_fd, '\n', 1);
+      cnt += 2;
+      continue;
     }
-    
-      focus_manager_ = NULL;
-  menu_ = NULL;
-    
-    #ifndef DOCQUAL_H
-#define DOCQUAL_H
-    
-    namespace tesseract {
+    if (argv[cnt][0] != '-') {
+      if (show) {
+        newargv.push_back('-w');
+      } else {
+        newargv.push_back(lint ? '-l' : '-f');
+      }
+      newargv.push_back(argv[cnt++]);
+      need_file = false;
+      break;
     }
+    if (strcmp(argv[cnt], '--') == 0) {
+      break;
+    }
+    cnt++; // skip unknown options
+  }
     
-      /// SetImage makes a copy of all the image data, so it may be deleted
-  /// immediately after this call.
-  /// Greyscale of 8 and color of 24 or 32 bits per pixel may be given.
-  /// Palette color images will not work properly and must be converted to
-  /// 24 bit.
-  /// Binary images of 1 bit per pixel may also be given but they must be
-  /// byte packed with the MSB of the first byte being the first pixel, and a
-  /// one pixel is WHITE. For binary images set bytes_per_pixel=0.
-  void SetImage(const unsigned char* imagedata, int width, int height,
-                int bytes_per_pixel, int bytes_per_line);
-    
-    void Config::Bind(HackStrictOption& loc, const IniSettingMap& ini,
-                  const Hdf& config, const std::string& name /* = '' */,
-                  HackStrictOption def) {
-  // Currently this doens't bind to ini_get since it is hard to thread through
-  // an enum
-  loc = GetHackStrictOption(ini, config, name, def);
+    inline VarEnv* ExecutionContext::hasVarEnv(int frame) {
+  auto const fp = getFrameAtDepth(frame);
+  if (fp && (fp->func()->attrs() & AttrMayUseVV)) {
+    if (fp->hasVarEnv()) return fp->getVarEnv();
+  }
+  return nullptr;
 }
     
-    #include 'hphp/runtime/base/mem-file.h'
-#include 'hphp/runtime/base/runtime-error.h'
-#include 'hphp/runtime/base/zend-url.h'
-#include 'hphp/runtime/base/zend-string.h'
+    #endif
+
     
-    #include 'hphp/runtime/base/file.h'
-#include 'hphp/runtime/base/stream-wrapper.h'
-#include 'hphp/runtime/base/stream-wrapper-registry.h'
+      req::ptr<Directory> opendir(const String& path) override;
     
-    struct FatalErrorException : ExtendedException {
-  explicit FatalErrorException(const char *msg)
-    : ExtendedException('%s', msg)
-  {}
-  FatalErrorException(int, ATTRIBUTE_PRINTF_STRING const char *msg, ...)
-    ATTRIBUTE_PRINTF(3,4);
-  FatalErrorException(const std::string&, const Array& backtrace,
-                      bool isRecoverable = false);
+            if (sectionShape.Rank() > m_maskShape.Rank())
+            LogicError('NDMask::MaskSection: The section shape '%S' rank (%d) must be <= rank (%d) of 'this' mask.', sectionShape.AsString().c_str(), (int)sectionShape.Rank(), (int)m_maskShape.Rank());
+    
+    
+    {            // Arithmetic schedule - write at every m_frequency steps or if the update is one of the first m_firstN
+            // updates.
+            return update % m_frequency == 0 || update <= m_firstN;
+        }
+    
+    
+    {        // TODO: Why Backward signature does not take Parameter instead of Variable for gradients?
+        m_combinedTrainingFunction->Backward(backPropSate, { { m_aggregatedLossFunction, m_rootGradientValue } }, parameterGradients);
+        m_prevMinibatchNumSamples = GetSampleCount(m_trainingSampleCountVar, outputs[m_trainingSampleCountVar]);
     }
     
-    struct Directory;
+            static bool IsUDF(const Dictionary& dict);
+    
+                    if (outputRank == SentinelValueForInferParamInitRank)
+                    outputRank = DefaultParamInitOutputRank;
+    
+    // GetData - Gets metadata from the specified section (into CPU memory)
+// sectionName - section name to retrieve data from
+// numRecords - number of records to read
+// data - pointer to data buffer, if NULL, dataBufferSize will be set to size of required buffer to accomidate request
+// dataBufferSize - [in] size of the databuffer in bytes
+//                  [out] size of buffer filled with data
+// recordStart - record to start reading from, defaults to zero (start of data)
+// returns: true if data remains to be read, false if the end of data was reached
+bool DataReader::GetData(const std::wstring& sectionName, size_t numRecords, void* data, size_t& dataBufferSize, size_t recordStart)
+{
+    bool bRet = true;
+    for (size_t i = 0; i < m_ioNames.size(); i++)
+        bRet &= m_dataReaders[m_ioNames[i]]->GetData(sectionName, numRecords, data, dataBufferSize, recordStart);
+    return bRet;
+}
+    
+    
+    {    std::string GetCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
+};
+    
+        // replace if requested
+    // This happens for model editing.
+    // workList operates on mapped nodes.
+    size_t numRelinked = 0;
+    for (auto& nodeRef : workList)
+    {
+        let iter = replacements.find(nodeRef);
+        if (iter != replacements.end())
+        {
+            assert(nodeRef->GetEnvironmentPtr()); // must be in some network if mapped
+            nodeRef = iter->second; // nodeRef is a reference, so this patches the workList in-place
+            numRelinked++;
+        }
+    }
+    
+        virtual void CopyTo(ComputationNodeBasePtr  nodeP, const std::wstring& newName, const CopyNodeFlags flags) const override
+    {
+        Base::CopyTo(nodeP, newName, flags);
+    }
+    
+            if (cols0 * wordsInEachSample != rows1)
+            LogicError('LookupTableNode: rows of input 1 is not a multiple of cols of input 0. This usually happens when the feature dimension is not specified as that in the network definition of look-up-table dimension size.');
+    
+    // Forward declarations
+namespace xgboost {
+class TreeUpdater;
+}
+    
+    
+    { private:
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
+};
+    
+        const size_t* begin = dmlc::BeginPtr(row_indices_);
+    const size_t* end = dmlc::BeginPtr(row_indices_) + row_indices_.size();
+    elem_of_each_node_.emplace_back(Elem(begin, end, 0));
+  }
+  // split rowset into two
+  inline void AddSplit(unsigned node_id,
+                       const std::vector<Split>& row_split_tloc,
+                       unsigned left_node_id,
+                       unsigned right_node_id) {
+    const Elem e = elem_of_each_node_[node_id];
+    const auto nthread = static_cast<bst_omp_uint>(row_split_tloc.size());
+    CHECK(e.begin != nullptr);
+    size_t* all_begin = dmlc::BeginPtr(row_indices_);
+    size_t* begin = all_begin + (e.begin - all_begin);
+    
+    namespace dmlc {
+DMLC_REGISTRY_ENABLE(::xgboost::TreeUpdaterReg);
+}  // namespace dmlc
+    
+      /**
+   * @brief Constructor
+   */
+  CanFrame() : id(0), len(0), timestamp{0} {
+    std::memset(data, 0, sizeof(data));
+  }
+    
+      /**
+   * @brief Create a pointer to a specified brand of CAN client. The brand is
+   *        set in the parameter.
+   * @param parameter The parameter to create the CAN client.
+   * @return A pointer to the created CAN client.
+   */
+  std::unique_ptr<CanClient> CreateCANClient(const CANCardParameter &parameter);
+    
+    #endif  // MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_ESD_CAN_CLIENT_H_
+
+    
+    
+    {  EsdCanClient esd_can_client;
+  EXPECT_TRUE(esd_can_client.Init(param));
+  EXPECT_EQ(esd_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
+  std::vector<CanFrame> frames;
+  int32_t num = 0;
+  EXPECT_EQ(esd_can_client.Send(frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  EXPECT_EQ(esd_can_client.Receive(&frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
+  CanFrame can_frame;
+  frames.push_back(can_frame);
+  EXPECT_EQ(esd_can_client.SendSingleFrame(frames),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  esd_can_client.Stop();
+}
+    
+    
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    
+      /**
+   * @brief Transform an integer with the size of 4 bytes to its hexadecimal
+   *        represented by a string.
+   * @param value The target integer to transform.
+   * @return Hexadecimal representing the target integer.
+   */
+  static std::string byte_to_hex(const uint32_t value);
+    
+    const int32_t CAN_FRAME_SIZE = 8;
+const int32_t MAX_CAN_SEND_FRAME_LEN = 1;
+const int32_t MAX_CAN_RECV_FRAME_LEN = 10;
+    
+    using apollo::common::adapter::AdapterConfig;
+using apollo::common::adapter::AdapterManager;
+using apollo::common::monitor::MonitorMessageItem;
+using apollo::common::Status;
+using apollo::common::ErrorCode;
+using apollo::common::time::Clock;
+using apollo::drivers::canbus::CanClientFactory;
+using apollo::drivers::canbus::CanClient;
+using apollo::drivers::canbus::CanReceiver;
+using apollo::drivers::canbus::SensorCanbusConf;
+    
+    #include 'modules/drivers/canbus/sensor_gflags.h'
