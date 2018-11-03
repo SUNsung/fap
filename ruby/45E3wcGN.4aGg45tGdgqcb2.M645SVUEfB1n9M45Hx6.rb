a734801ebac9981760a862fad4f295a8adf273f2
SUNsung <weatherius@gmail.com>
2018-11-03 21:46:41 +0200

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2018-11-03 21:46:41

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,145 +1,159 @@
 
         
-              File.write(new_path, '1')
-      false
+            it 'shows the dry run pop up with previous events and allows use previously received event' do
+      emitter.events << Event.new(payload: {url: 'http://xkcd.com/'})
+      agent.sources << emitter
+      agent.options.merge!('url' => '', 'url_from_event' => '{{url}}')
+      agent.save!
+    
+        it 'interpolates jsonpath expressions between matching <>'s' do
+      expect(Utils.interpolate_jsonpaths('hello <$.there.world> this <escape works>', payload)).to eq('hello WORLD this should+work')
     end
     
-        def run!
-      require_program(:version, :description)
-      trap('INT') { abort(program(:int_message)) } if program(:int_message)
-      trap('INT') { program(:int_block).call } if program(:int_block)
-      global_option('-h', '--help', 'Display help documentation') do
-        args = @args - %w(-h --help)
-        command(:help).run(*args)
-        return
-      end
-      global_option('-v', '--version', 'Display version information') do
-        say(version)
-        return
-      end
-      parse_global_options
-      remove_global_options(options, @args)
+          AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 4, :outbound_event => events(:jane_website_agent_event))
+      expect(agents(:jane_website_agent).reload.last_error_log_at.to_i).to be_within(2).of(Time.now.to_i)
+    end
     
-            expect(result).to eq('git commit -m message ./fastlane/README.md ./LICENSE ' + './fastlane/spec/fixtures/git_commit/A FILE WITH SPACE'.shellescape)
-      end
+        def format_url(url)
+      url.to_s.remove %r{\Ahttps?://}
+    end
     
-            it 'escapes spaces in list of files to process' do
-          file = 'path/to/my project/source code/AppDelegate.swift'
-          result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-                files: ['#{file}']
-            )
-          end').runner.execute(:test)
-    
-        os = 'other'
-    shellescape_testcases.each do |testcase|
-      it testcase['it'] + ': ' + testcase['it_result'][os] do
-        str = testcase['str'].to_s
-        expect_correct_implementation_to_be_called(str, :shellescape, os)
-        escaped = str.shellescape
-        expect(escaped).to eq(testcase['expect'][os])
+            css('ul.methods', 'ul.properties', 'ul.events').add_class('defs').each do |node|
+          node.css('> li > h3').each do |h3|
+            next if h3.content.present?
+            h3.content = h3.next_element.content
+            h3.next_element.remove
+          end
+        end
       end
     end
   end
 end
+
     
-          dir.find do |src|
-        dst = HOMEBREW_PREFIX + src.relative_path_from(path)
-        dst.extend(ObserverPathnameExtension)
-    
-    class GitHubGitDownloadStrategy < GitDownloadStrategy
-  def initialize(url, name, version, **meta)
-    super
-    
-    module Homebrew
-  module_function
-    
-      # Default log-level (development=debug, production=info)
-  Logging.logger.root.level = config.log_level
-    
-        execute 'INSERT INTO share_visibilities (user_id, shareable_id, shareable_type) ' \
-            'SELECT post_visibility.user_id, photos.id, 'Photo' FROM photos ' \
-            'INNER JOIN posts ON posts.guid = photos.status_message_guid AND posts.type = 'StatusMessage' ' \
-            'LEFT OUTER JOIN share_visibilities ON share_visibilities.shareable_id = photos.id ' \
-            'INNER JOIN share_visibilities AS post_visibility ON post_visibility.shareable_id = posts.id ' \
-            'WHERE photos.public = false AND share_visibilities.shareable_id IS NULL ' \
-            'AND post_visibility.shareable_type = 'Post''
-  end
-    
-    Then /^'([^']*)' should be post (\d+)$/ do |post_text, position|
-  stream_element_numbers_content(position).should have_content(post_text)
-end
-    
-    When /^I should see one less invite$/ do
-  step 'I should see \'#{@inviter_invite_count -1} invites left\''
-end
-    
-      # submit the password reset form
-  def submit_password_reset_form
-    find('.btn').click
-  end
-    
-          @conv2 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
-             .increase_unread(alice)
-    
-    # Get the version string. If this is being installed from Git,
-# this includes the proper prerelease version.
-def get_version
-  File.read(scope('VERSION').strip)
-end
-    
-    require 'sass/logger'
-require 'sass/util'
-    
-      # A read-only wrapper for a lexical environment for SassScript.
-  class ReadOnlyEnvironment < BaseEnvironment
-    def initialize(parent = nil, options = nil)
-      super
-      @content_cached = nil
-    end
-    # The read-only environment of the caller of this environment's mixin or function.
-    #
-    # @see BaseEnvironment#caller
-    # @return {ReadOnlyEnvironment}
-    def caller
-      return @caller if @caller
-      env = super
-      @caller ||= env.is_a?(ReadOnlyEnvironment) ? env : ReadOnlyEnvironment.new(env, env.options)
+        def destroy
+      authorize @custom_emoji, :destroy?
+      @custom_emoji.destroy!
+      log_action :destroy, @custom_emoji
+      flash[:notice] = I18n.t('admin.custom_emojis.destroyed_msg')
+      redirect_to admin_custom_emojis_path(page: params[:page], **@filter_params)
     end
     
-        # Set an option for specifying `Encoding.default_external`.
-    #
-    # @param opts [OptionParser]
-    def encoding_option(opts)
-      encoding_desc = 'Specify the default encoding for input files.'
-      opts.on('-E', '--default-encoding ENCODING', encoding_desc) do |encoding|
-        Encoding.default_external = encoding
+        def resubscribe
+      authorize :instance, :resubscribe?
+      params.require(:by_domain)
+      Pubsubhubbub::SubscribeWorker.push_bulk(subscribeable_accounts.pluck(:id))
+      redirect_to admin_instances_path
+    end
+    
+      path = 'assets/stylesheets'
+  css_path = args.with_defaults(css_path: 'tmp')[:css_path]
+  puts Term::ANSIColor.bold 'Compiling SCSS in #{path}'
+  Dir.mkdir(css_path) unless File.directory?(css_path)
+  %w(_bootstrap bootstrap/_theme).each do |file|
+    save_path = '#{css_path}/#{file.sub(/(^|\/)?_+/, '\1').sub('/', '-')}.css'
+    puts Term::ANSIColor.cyan('  #{save_path}') + '...'
+    engine    = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
+    css       = engine.render
+    File.open(save_path, 'w') { |f| f.write css }
+  end
+end
+    
+        def configure_sass
+      require 'sass'
+    
+      # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+    
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+    World(VagrantHelpers)
+
+    
+          def assert_value_or_block_not_both(value, block)
+        return if value.nil? || block.nil?
+        raise Capistrano::ValidationError,
+              'Value and block both passed to Configuration#set'
+      end
+    
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
+    
+      class OfflinePluginPackager
+    LOGSTASH_DIR = 'logstash'
+    DEPENDENCIES_DIR = ::File.join(LOGSTASH_DIR, 'dependencies')
+    
+        puts('Generated at #{target_file}')
+  end
+    
+        # remove 'system' local gems used by LS
+    local_gems = gemfile.locally_installed_gems.map(&:name) - NON_PLUGIN_LOCAL_GEMS
+    
+    project_versions_yaml_path = File.expand_path('../versions.yml', File.dirname(__FILE__))
+if File.exist?(project_versions_yaml_path)
+  # we need to copy the project level versions.yml into the gem root
+  # to be able to package it into the gems file structure
+  # as the require 'logstash-core-plugin-api/version' loads the yaml file from within the gem root.
+  #
+  # we ignore the copy in git and we overwrite an existing file
+  # each time we build the logstash-core gem
+  original_lines = IO.readlines(project_versions_yaml_path)
+  original_lines << ''
+  original_lines << '# This is a copy the project level versions.yml into this gem's root and it is created when the gemspec is evaluated.'
+  gem_versions_yaml_path = File.expand_path('./versions-gem-copy.yml', File.dirname(__FILE__))
+  File.open(gem_versions_yaml_path, 'w') do |new_file|
+    # create or overwrite
+    new_file.puts(original_lines)
+  end
+end
+    
+          it 'should return the scalar value as a single element array' do
+        expect(subject.strings).to match_array([strings])
       end
     end
     
-          output = @options[:output] = @args.shift
-      raise 'Error: --from required when using --recursive.' unless @options[:from]
-      raise 'Error: --to required when using --recursive.' unless @options[:to]
-      unless File.directory?(@options[:input])
-        raise 'Error: '#{@options[:input]}' is not a directory'
+            def scope
+          if params[:product_id]
+            Spree::Product.friendly.find(params[:product_id])
+          elsif params[:variant_id]
+            Spree::Variant.find(params[:variant_id])
+          end
+        end
       end
-      if @options[:output] && File.exist?(@options[:output]) &&
-        !File.directory?(@options[:output])
-        raise 'Error: '#{@options[:output]}' is not a directory'
-      end
-      @options[:output] ||= @options[:input]
-    
-    # The project root directory
-$root = ::File.dirname(__FILE__)
-    
-      class ImageTag < Liquid::Tag
-    @img = nil
-    
-      # Escapes CDATA sections in post content
-  def cdata_escape(input)
-    input.gsub(/<!\[CDATA\[/, '&lt;![CDATA[').gsub(/\]\]>/, ']]&gt;')
+    end
   end
+end
+
     
-    require 'pathname'
-require './plugins/octopress_filters'
+            def update
+          @option_value = scope.accessible_by(current_ability, :update).find(params[:id])
+          if @option_value.update_attributes(option_value_params)
+            render :show
+          else
+            invalid_resource!(@option_value)
+          end
+        end
+    
+            def update
+          authorize! :update, stock_location
+          if stock_location.update_attributes(stock_location_params)
+            respond_with(stock_location, status: 200, default_template: :show)
+          else
+            invalid_resource!(stock_location)
+          end
+        end
