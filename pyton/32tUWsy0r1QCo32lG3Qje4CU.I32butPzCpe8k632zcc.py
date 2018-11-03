b2e245db36e6d2053850c0aba77466cde65d1fae
SUNsung <weatherius@gmail.com>
2018-11-03 23:15:46 +0200

    Rewrite [32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py] File in: 2018-11-03 23:15:45

--- pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
+++ pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
@@ -1,225 +1,43 @@
 
         
-        '''
-This script employs a VERY basic heuristic ('porn' in webpage.lower()) to check
-if we are not 'age_limit' tagging some porn site
+        
+class TestCaseInsensitiveDict:
     
-    password = key
-new_key = aes_encrypt(password, key_expansion(password))
-r = openssl_encode('aes-128-ctr', new_key, iv)
-print('aes_decrypt_text 16')
-print(repr(r))
+    # -------
+# Pythons
+# -------
     
-        infile, outfile = args
+    import copy
+import time
+import calendar
     
-    with io.open(README_FILE, 'w', encoding='utf-8') as f:
-    f.write(header)
-    f.write(options)
-    f.write(footer)
+    
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or {}
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
 
     
     
-if __name__ == '__main__':
-    main()
-
+def test_digestauth_401_count_reset_on_redirect():
+    '''Ensure we correctly reset num_401_calls after a successful digest auth,
+    followed by a 302 redirect to another digest auth prompt.
     
-        :param str u_string: unicode string to check. Must be unicode
-        and not Python 2 `str`.
-    :rtype: bool
-    '''
-    assert isinstance(u_string, str)
-    try:
-        u_string.encode('ascii')
-        return True
-    except UnicodeEncodeError:
-        return False
-
+        @pytest.mark.skip(reason='this fails non-deterministically under pytest-xdist')
+    def test_request_recovery(self):
+        '''can check the requests content'''
+        # TODO: figure out why this sometimes fails when using pytest-xdist.
+        server = Server.basic_response_server(requests_to_handle=2)
+        first_request = b'put your hands up in the air'
+        second_request = b'put your hand down in the floor'
     
-    
-class ReadTimeout(Timeout):
-    '''The server did not send any data in the allotted amount of time.'''
-    
-            Keyword:                   'bold #004461',   # class: 'k'
-        Keyword.Constant:          'bold #004461',   # class: 'kc'
-        Keyword.Declaration:       'bold #004461',   # class: 'kd'
-        Keyword.Namespace:         'bold #004461',   # class: 'kn'
-        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
-        Keyword.Reserved:          'bold #004461',   # class: 'kr'
-        Keyword.Type:              'bold #004461',   # class: 'kt'
-    
-                # Allow self-specified cert location.
-            if verify is not True:
-                cert_loc = verify
-    
-    
-def default_hooks():
-    return {event: [] for event in HOOKS}
-    
-    
-def test_idna_with_version_attribute(mocker):
-    '''Verify we're actually setting idna version when it should be available.'''
-    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
-    assert info()['idna'] == {'version': '2.6'}
-
-    
-            assert status_line == b'GET /path/to/thing/ HTTP/1.1'
-        for frag in (b'view', b'edit', b'token', b'hunter2'):
-            assert frag not in headers
-            assert frag not in body
-    
-            # Send request and simulate redirect
-        resp = s.send(prep_req)
-        resp.status_code = 302
-        resp.headers['location'] = httpbin('get')
-        redirects = s.resolve_redirects(resp, prep_req)
-        resp = next(redirects)
-    
-            with Server.basic_response_server(wait_to_close_event=block_server) as (host, port):
-            sock = socket.socket()
-            sock.connect((host, port))
-            sock.sendall(b'send something')
-            time.sleep(2.5)
-            sock.sendall(b'still alive')
-            block_server.set()  # release server block
-    
-        print('Classifier Training')
-    print('===================')
-    accuracy, train_time, test_time = {}, {}, {}
-    for name in sorted(args['estimators']):
-        clf = ESTIMATORS[name]
-        try:
-            clf.set_params(random_state=0)
-        except (TypeError, ValueError):
-            pass
-    
-    
-def plot_feature_errors(all_errors, batch_size, all_components, data):
-    plt.figure()
-    plot_results(all_components, all_errors['pca'], label='PCA')
-    plot_results(all_components, all_errors['ipca'],
-                 label='IncrementalPCA, bsize=%i' % batch_size)
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. n_components\n'
-                 'LFW, size %i x %i' % data.shape)
-    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
-    plt.ylabel('Mean absolute error')
-    
-                gc.collect()
-            print('benchmarking orthogonal_mp (with Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            orthogonal_mp(X, y, precompute=True,
-                          n_nonzero_coefs=n_informative)
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            omp_gram[i_f, i_s] = delta
-    
-    
-def benchmark_sparse_predict():
-    X_test_sparse = csr_matrix(X_test)
-    for _ in range(300):
-        clf.predict(X_test_sparse)
-    
-        dim = start_dim
-    for i in range(0, n):
-        print('============================================')
-        print('Entering iteration %s of %s' % (i, n))
-        print('============================================')
-        dim += step
-        X = np.random.randn(100, dim)
-        Y = np.random.randint(0, n_classes, (100,))
-        bench_scikit_tree_classifier(X, Y)
-        Y = np.random.randn(100)
-        bench_scikit_tree_regressor(X, Y)
-    
-        try:
-        fn = inspect.getsourcefile(obj)
-    except Exception:
-        fn = None
-    if not fn:
-        try:
-            fn = inspect.getsourcefile(sys.modules[obj.__module__])
-        except Exception:
-            fn = None
-    if not fn:
-        return
-    
-        opener = build_opener()
-    html_filename = os.path.join(html_folder, lang + '.html')
-    if not os.path.exists(html_filename):
-        print('Downloading %s' % page)
-        request = Request(page)
-        # change the User Agent to avoid being blocked by Wikipedia
-        # downloading a couple of articles should not be considered abusive
-        request.add_header('User-Agent', 'OpenAnything/1.0')
-        html_content = opener.open(request).read()
-        open(html_filename, 'wb').write(html_content)
-    
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
-
-    
-    
-class FixedTest(unittest.TestCase):
-    '''Tests for acme.fields.Fixed.'''
-    
-    
-class Header(jose.Header):
-    '''ACME-specific JOSE Header. Implements nonce, kid, and url.
-    '''
-    nonce = jose.Field('nonce', omitempty=True, encoder=jose.encode_b64jose)
-    kid = jose.Field('kid', omitempty=True)
-    url = jose.Field('url', omitempty=True)
-    
-        '''
-    return_vars = {}
-    # Get list of words in the variable
-    a_opts = util.get_var_from_file(varname, filepath).split()
-    for i, v in enumerate(a_opts):
-        # Handle Define statements and make sure it has an argument
-        if v == '-D' and len(a_opts) >= i+2:
-            var_parts = a_opts[i+1].partition('=')
-            return_vars[var_parts[0]] = var_parts[2]
-        elif len(v) > 2 and v.startswith('-D'):
-            # Found var with no whitespace separator
-            var_parts = v[2:].partition('=')
-            return_vars[var_parts[0]] = var_parts[2]
-    return return_vars
-    
-        @certbot_util.patch_get_utility()
-    def test_select_cancel(self, mock_util):
-        mock_util().checklist.return_value = (display_util.CANCEL, 'whatever')
-        vhs = select_vhost_multiple([self.vhosts[2], self.vhosts[3]])
-        self.assertFalse(vhs)
-    
-    TIMEOUT_SECONDS = 0.1
-    
-    
-  def _ThreadMain( self ):
-    while True:
-      time.sleep( self._ping_interval_seconds )
-    
-    from ycm.tests.test_utils import MockVimModule
-MockVimModule()
-    
-    
-def EndsWithPython_BadPaths_test():
-  not_python_paths = [
-    None,
-    '',
-    '/opt/local/bin/vim',
-    r'C:\Program Files\Vim\vim74\gvim.exe',
-    '/usr/bin/python2.5',
-    '/home/user/.pyenv/shims/python3.2',
-  ]
-    
-    
-def ExtractKeywordsFromGroup_KeywordMiddle_test():
-  assert_that( syntax_parse._ExtractKeywordsFromGroup(
-                 syntax_parse.SyntaxGroup( '', [
-                   'foo contained bar',
-                   'zoo goo'
-                 ] ) ),
-               contains_inanyorder( 'foo', 'contained', 'bar', 'zoo', 'goo' ) )
+    # The master toctree document.
+master_doc = 'index'
