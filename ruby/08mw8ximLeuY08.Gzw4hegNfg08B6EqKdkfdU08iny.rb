66f2a163fa52df83e9406534b5888422df8b6924
SUNsung <weatherius@gmail.com>
2018-11-03 17:21:25 +0200

    Rewrite [08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb] File in: 2018-11-03 17:21:25

--- ruby/08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb
+++ ruby/08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb
@@ -1,59 +1,102 @@
 
         
-          attr_accessor :listen_port, :listen_host, :server_name, :context, :comm
-  attr_accessor :ssl, :ssl_cert, :ssl_compression, :ssl_cipher
-  attr_accessor :listener, :resources
+        # -------------------------------------------------------------------
+# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
+# -------------------------------------------------------------------
     
-      def hangup
-    self.client.send_hangup(self)
-    self.state = :hangup
-    true
+          #
+    
+      File.write(Paths.status_file, p.exitstatus)
+  File.open(Paths.output_file, 'wb') do |f|
+    f.print '$ '
+    f.puts args.join(' ')
+    f.puts output
+    f.puts 'EXIT STATUS: #{p.exitstatus}'
   end
     
-    module Rex
-  module Proto
-    module Kerberos
-      module Crypto
-        module Rc4Hmac
-          # Decrypts the cipher using RC4-HMAC schema
-          #
-          # @param cipher [String] the data to decrypt
-          # @param key [String] the key to decrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the decrypted cipher
-          # @raise [RuntimeError] if decryption doesn't succeed
-          def decrypt_rc4_hmac(cipher, key, msg_type)
-            unless cipher && cipher.length > 16
-              raise ::RuntimeError, 'RC4-HMAC decryption failed'
-            end
+    module Jekyll
+  module Converters
+    class Markdown
+      class KramdownParser
+        CODERAY_DEFAULTS = {
+          'css'               => 'style',
+          'bold_every'        => 10,
+          'line_numbers'      => 'inline',
+          'line_number_start' => 1,
+          'tab_width'         => 4,
+          'wrap'              => 'div',
+        }.freeze
     
-              # Encodes a Rex::Proto::Kerberos::Model::AuthorizationData into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            seqs = []
-            elements.each do |elem|
-              elems = []
-              type_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_type(elem[:type])], 0, :CONTEXT_SPECIFIC)
-              elems << type_asn1
-              data_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_data(elem[:data])], 1, :CONTEXT_SPECIFIC)
-              elems << data_asn1
-              seqs << OpenSSL::ASN1::Sequence.new(elems)
-            end
+          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent)).to eq([@agent2])
+    end
     
-              # Encodes the msg_type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_msg_type
-            bn = OpenSSL::BN.new(msg_type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
+      let :reverted_template do
+    old_template.merge('url' => '{{ url }}')
+  end
     
-              # Decodes the key_type from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_type(input)
-            input.value[0].value.to_i
-          end
+        private
     
-      s.executables = ['gollum']
+            # Remove examples
+        css('.runnable-example').each do |node|
+          node.parent.remove
+        end
+    
+      def remove_linked_keg_record
+    linked_keg_record.unlink
+    linked_keg_record.parent.rmdir_if_possible
+  end
+    
+        if GitHub.api_credentials_type == :none
+      puts <<~EOS
+        You can create a new personal access token:
+          #{GitHub::ALL_SCOPES_URL}
+        #{Utils::Shell.set_variable_in_profile('HOMEBREW_GITHUB_API_TOKEN', 'your_token_here')}
+    
+        ff = if ARGV.named.empty?
+      Formula.installed.sort
+    else
+      ARGV.resolved_formulae.sort
+    end
+    
+    # Get the version string. If this is being installed from Git,
+# this includes the proper prerelease version.
+def get_version
+  File.read(scope('VERSION').strip)
+end
+    
+        # Converts the CSS template into Sass or SCSS code.
+    #
+    # @param fmt [Symbol] `:sass` or `:scss`, designating the format to return.
+    # @return [String] The resulting Sass or SCSS code
+    # @raise [Sass::SyntaxError] if there's an error parsing the CSS template
+    def render(fmt = :sass)
+      check_encoding!
+      build_tree.send('to_#{fmt}', @options).strip + '\n'
+    rescue Sass::SyntaxError => err
+      err.modify_backtrace(:filename => @options[:filename] || '(css)')
+      raise err
+    end
+    
+    module RuboCop
+  module AST
+    # A node extension for `hash` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `hash` nodes within RuboCop.
+    class HashNode < Node
+      # Returns an array of all the key value pairs in the `hash` literal.
+      #
+      # @return [Array<PairNode>] an array of `pair` nodes
+      def pairs
+        each_pair.to_a
+      end
+    
+          # Custom destructuring method. This is used to normalize the branches
+      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
+      #
+      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
+      def node_parts
+        [self, self]
+      end
+    end
+  end
+end
