ff9b999a20adca0bbe04d25e052903de4ce5c14c
SUNsung <weatherius@gmail.com>
2018-11-04 09:46:06 +0200

    Rewrite [12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb] File in: 2018-11-04 09:46:05

--- ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
+++ ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
@@ -1,82 +1,28 @@
 
         
-            it 'does not output links to other agents outside of the incoming set' do
-      Link.create!(:source_id => agents(:jane_weather_agent).id, :receiver_id => agents(:jane_website_agent).id)
-      Link.create!(:source_id => agents(:jane_website_agent).id, :receiver_id => agents(:jane_rain_notifier_agent).id)
+              if @account_moderation_note.save
+        redirect_to admin_account_path(@account_moderation_note.target_account_id), notice: I18n.t('admin.account_moderation_notes.created_msg')
+      else
+        @account          = @account_moderation_note.target_account
+        @moderation_notes = @account.targeted_moderation_notes.latest
     
-      it 'considers a location empty if either latitude or longitude is missing' do
-    expect(Location.new.empty?).to be_truthy
-    expect(Location.new(lat: 2, radius: 1).present?).to be_falsy
-    expect(Location.new(lng: 3, radius: 1).present?).to be_falsy
+      def hub_secret
+    params['hub.secret']
   end
     
-          @log.level = 4
-      expect(@log).to be_valid
+          doc = Nokogiri::HTML(File.read('doc/file.SASS_REFERENCE.html'))
     
-      describe '.active/inactive' do
-    let(:agent) { agents(:jane_website_agent) }
-    
-    platforms :jruby do
-  gem 'activerecord-jdbc-adapter'
-  gem 'activerecord-jdbcsqlite3-adapter'
-  gem 'jruby-openssl'
-end
-    
-      def sign_in_params
-    devise_parameter_sanitizer.sanitize(:sign_in)
-  end
-    
-      # Sets flash message if is_flashing_format? equals true
-  def set_flash_message!(key, kind, options = {})
-    if is_flashing_format?
-      set_flash_message(key, kind, options)
-    end
-  end
-    
-        def email_changed(record, opts={})
-      devise_mail(record, :email_changed, opts)
-    end
-    
-          # Stores the provided location to redirect the user after signing in.
-      # Useful in combination with the `stored_location_for` helper.
-      #
-      # Example:
-      #
-      #   store_location_for(:user, dashboard_path)
-      #   redirect_to user_facebook_omniauth_authorize_path
-      #
-      def store_location_for(resource_or_scope, location)
-        session_key = stored_location_key_for(resource_or_scope)
-        
-        path = extract_path_from_location(location)
-        session[session_key] = path if path
-      end
-    
-        def recall_app(app)
-      controller, action = app.split('#')
-      controller_name  = ActiveSupport::Inflector.camelize(controller)
-      controller_klass = ActiveSupport::Inflector.constantize('#{controller_name}Controller')
-      controller_klass.action(action)
-    end
-    
-          # @see Base#\_retrieve
-      def _retrieve(key, version, sha)
-        return unless File.readable?(path_to(key))
-        begin
-          File.open(path_to(key), 'rb') do |f|
-            if f.readline('\n').strip == version && f.readline('\n').strip == sha
-              return f.read
+              def try_set_#{name}(name, value)
+            @#{name}s ||= {}
+            if @#{name}s.include?(name)
+              @#{name}s[name] = value
+              true
+            elsif @parent && !@parent.global?
+              @parent.try_set_#{name}(name, value)
+            else
+              false
             end
           end
-          File.unlink path_to(key)
-        rescue Errno::ENOENT
-          # Already deleted. Race condition?
-        end
-        nil
-      rescue EOFError, TypeError, ArgumentError => e
-        Sass::Util.sass_warn 'Warning. Error encountered while reading cache #{path_to(key)}: #{e}'
-      end
+          protected :try_set_#{name}
     
-          opts.on('-q', '--quiet', 'Silence warnings and status messages during conversion.') do |bool|
-        @options[:for_engine][:quiet] = bool
-      end
+          encoding_option(opts)
