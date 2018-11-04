eb421e41641156d52c16cce6ee2936c165b010e3
SUNsung <weatherius@gmail.com>
2018-11-04 21:54:58 +0200

    Rewrite [45q6b9HgJKd8Q4595R3H7EpbzI45IdpOHldHyPM456ko.rb] File in: 2018-11-04 21:54:58

--- ruby/45q6b9HgJKd8Q4595R3H7EpbzI45IdpOHldHyPM456ko.rb
+++ ruby/45q6b9HgJKd8Q4595R3H7EpbzI45IdpOHldHyPM456ko.rb
@@ -1,56 +1,197 @@
 
         
-            initializer 'action_view.form_with_generates_ids' do |app|
-      ActiveSupport.on_load(:action_view) do
-        form_with_generates_ids = app.config.action_view.delete(:form_with_generates_ids)
-        unless form_with_generates_ids.nil?
-          ActionView::Helpers::FormHelper.form_with_generates_ids = form_with_generates_ids
+                  def tag_id(index = nil)
+            # a little duplication to construct less strings
+            case
+            when @object_name.empty?
+              sanitized_method_name.dup
+            when index
+              '#{sanitized_object_name}_#{index}_#{sanitized_method_name}'
+            else
+              '#{sanitized_object_name}_#{sanitized_method_name}'
+            end
+          end
+    
+              if include_hidden
+            hidden = hidden_field_for_checkbox(options)
+            hidden + checkbox
+          else
+            checkbox
+          end
+        end
+    
+              if name_and_id['for']
+            name_and_id['id'] = name_and_id['for']
+          else
+            name_and_id.delete('id')
+          end
+    
+            def render
+          options = @options.stringify_keys
+          options['size'] = options['maxlength'] unless options.key?('size')
+          options['type'] ||= field_type
+          options['value'] = options.fetch('value') { value_before_type_cast } unless field_type == 'file'
+          add_default_name_and_id(options)
+          tag('input', options)
+        end
+    
+        delegate :[], :include?, :pop, :size, :each, to: :paths
+    
+            keys.find do |key|
+          key_tag_version = tag_without_or_later(key)
+          begin
+            MacOS::Version.from_symbol(key_tag_version) <= tag_version
+          rescue ArgumentError
+            false
+          end
         end
       end
     end
-    
-        # Direct access to template rendering.
-    def render_template(context, options) #:nodoc:
-      TemplateRenderer.new(@lookup_context).render(context, options)
-    end
-    
-          @options = options.merge(:_convert => true)
-      # Backwards compatibility
-      @options[:old] = true if @options[:alternate] == false
-      @template = template
-      @checked_encoding = false
-    end
-    
-          # Almost any real Unix terminal will support color,
-      # so we just filter for Windows terms (which don't set TERM)
-      # and not-real terminals, which aren't ttys.
-      return str if ENV['TERM'].nil? || ENV['TERM'].empty? || !STDOUT.tty?
-      '\e[#{COLORS[color]}m#{str}\e[0m'
-    end
-    
-          def left_diff_line_number(id, line)
-        if line =~ /^@@/
-          m, li                  = *line.match(/\-(\d+)/)
-          @left_diff_line_number = li.to_i
-          @current_line_number   = @left_diff_line_number
-          ret                    = '...'
-        elsif line[0] == ?-
-          ret                    = @left_diff_line_number.to_s
-          @left_diff_line_number += 1
-          @current_line_number   = @left_diff_line_number - 1
-        elsif line[0] == ?+
-          ret = ' '
-        else
-          ret                    = @left_diff_line_number.to_s
-          @left_diff_line_number += 1
-          @current_line_number   = @left_diff_line_number - 1
-        end
-        ret
-      end
-    
-    require 'rake/testtask'
-Rake::TestTask.new(:test) do |test|
-  test.libs << 'lib' << 'test' << '.'
-  test.pattern = 'test/**/test_*.rb'
-  test.verbose = true
+  end
 end
+
+    
+      config.include(FileUtils)
+    
+        it 'with universal' do
+      expect_offense(<<~RUBY)
+        class Foo < Formula
+          url 'https://example.com/foo-1.0.tgz'
+          option :universal
+          ^^^^^^^^^^^^^^^^^ macOS has been 64-bit only since 10.6 so universal options are deprecated.
+        end
+      RUBY
+    end
+    
+      def to_str
+    @paths.join(File::PATH_SEPARATOR)
+  end
+  alias to_s to_str
+    
+        self.store(key, value) if (stored == false)
+    self.dcase_hash[key.downcase] = value
+  end
+    
+          send_e404(cli, request)
+    end
+    
+                components.each do |c|
+              encoded << [c.length].pack('N')
+              encoded << c
+            end
+    
+              # Encodes the renew_time field
+          #
+          # @return [String]
+          def encode_renew_time
+            [renew_till].pack('N')
+          end
+    
+              # Decodes the key from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [EncryptionKey]
+          def decode_key(input)
+            Rex::Proto::Kerberos::Model::EncryptionKey.decode(input.value[0])
+          end
+    
+      def safely_remove_file(_path)
+    run_vagrant_command('rm #{test_file}')
+  rescue
+    VagrantHelpers::VagrantSSHCommandError
+  end
+end
+    
+          # rubocop:disable Security/MarshalLoad
+      def add_role(role, hosts, options={})
+        options_deepcopy = Marshal.dump(options.merge(roles: role))
+        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
+      end
+      # rubocop:enable Security/MarshalLoad
+    
+    module Capistrano
+  class Configuration
+    # Decorates a Variables object to additionally perform an optional set of
+    # user-supplied validation rules. Each rule for a given key is invoked
+    # immediately whenever `set` is called with a value for that key.
+    #
+    # If `set` is called with a callable value or a block, validation is not
+    # performed immediately. Instead, the validation rules are invoked the first
+    # time `fetch` is used to access the value.
+    #
+    # A rule is simply a block that accepts two arguments: key and value. It is
+    # up to the rule to raise an exception when it deems the value is invalid
+    # (or just print a warning).
+    #
+    # Rules can be registered using the DSL like this:
+    #
+    #   validate(:my_key) do |key, value|
+    #     # rule goes here
+    #   end
+    #
+    class ValidatedVariables < SimpleDelegator
+      include Capistrano::ProcHelpers
+    
+        # @return [String] major.minor ruby version, ex 1.9
+    def ruby_abi_version
+      RUBY_VERSION[/(\d+\.\d+)(\.\d+)*/, 1]
+    end
+    
+    if $0 == __FILE__
+  begin
+    LogStash::PluginManager::Main.run('bin/logstash-plugin', ARGV)
+  rescue LogStash::PluginManager::Error => e
+    $stderr.puts(e.message)
+    exit(1)
+  end
+end
+
+    
+      private
+    
+          PluginManager.ui.info('Installing file: #{local_file}')
+      uncompressed_path = uncompress(local_file)
+      PluginManager.ui.debug('Pack uncompressed to #{uncompressed_path}')
+      pack = LogStash::PluginManager::PackInstaller::Pack.new(uncompressed_path)
+      raise PluginManager::InvalidPackError, 'The pack must contains at least one plugin' unless pack.valid?
+    
+      gem.files         = Dir.glob(['logstash-core-plugin-api.gemspec', 'lib/**/*.rb', 'spec/**/*.rb'])
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.name          = 'logstash-core-plugin-api'
+  gem.require_paths = ['lib']
+  gem.version       = LOGSTASH_CORE_PLUGIN_API
+    
+        it 'should raise configuration error when provided with too many spaces' do
+      expect {
+        Class.new(LogStash::Filters::Base) do
+          include LogStash::Config::Mixin
+          config_name 'test'
+          milestone 1
+          config :size_file, :validate => :bytes
+        end.new({'size_file' => '10  kib'})
+      }.to raise_error(LogStash::ConfigurationError)
+    end
+  end
+    
+        def raw_push(payloads)
+      @redis_pool.with do |conn|
+        conn.multi do
+          atomic_push(conn, payloads)
+        end
+      end
+      true
+    end
+    
+      end
+end
+
+    
+        def self.with_context(msg)
+      Thread.current[:sidekiq_context] ||= []
+      Thread.current[:sidekiq_context] << msg
+      yield
+    ensure
+      Thread.current[:sidekiq_context].pop
+    end
+    
+        EXPIRY = 60 * 60 * 24
