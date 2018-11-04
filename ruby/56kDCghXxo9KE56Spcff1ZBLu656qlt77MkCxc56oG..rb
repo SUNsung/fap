3af0f1d64847cf91fddd3e89064e0874260eb3a1
SUNsung <weatherius@gmail.com>
2018-11-04 10:31:39 +0200

    Rewrite [56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb] File in: 2018-11-04 10:31:39

--- ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
+++ ruby/56kDCghXxo9KE56Spcff1ZBLu656qlt77MkCxc56oG..rb
@@ -1,136 +1,99 @@
 
         
-        group :production do
-  gem 'uglifier'
-  gem 'newrelic_rpm'
-end
-    
-        def as_json
-      @pages
+            def xpath(*args)
+      doc.xpath(*args)
     end
     
-        def parse_as_document
-      document = Nokogiri::HTML.parse @content, nil, 'UTF-8'
-      @title = document.at_css('title').try(:content)
-      document
+        def insert_after(index, *names)
+      insert assert_index(index) + 1, *names
     end
     
-            css('pre.no-bg-with-indent').each do |node|
-          node.content = '  ' + node.content.gsub('\n', '\n  ')
-        end
+        private
     
-          def get_type
-        if slug.start_with?('guide/')
-          'Guide'
-        elsif slug.start_with?('cookbook/')
-          'Cookbook'
-        elsif slug == 'glossary'
-          'Guide'
-        else
-          type = at_css('.nav-title.is-selected').content.strip
-          type.remove! ' Reference'
-          type << ': #{mod}' if mod
-          type
-        end
-      end
-    
-            # Remove dead links (e.g. ngRepeat)
-        css('a.type-hint').each do |node|
-          node.name = 'code'
-          node.remove_attribute 'href'
-        end
-    
-    describe 'Kernel.srand' do
-  it 'is a private method' do
-    Kernel.should have_private_instance_method(:srand)
-  end
-    
-        # Get rid of any info 'dir' files, so they don't conflict at the link stage
-    info_dir_file = @f.info + 'dir'
-    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
-      observe_file_removal info_dir_file
+        def html?
+      mime_type.include? 'html'
     end
     
-      def missing
-    return unless HOMEBREW_CELLAR.exist?
+        def root_url
+      @root_url ||= root_path? ? URL.parse(File.join(base_url.to_s, root_path)) : base_url.normalize
+    end
     
-    require 'ostruct'
+            css('h1').each_with_index do |node, i|
+          next if i == 0
+          node.name = 'h2'
+        end
     
-              data = connection.get_once(length, timeout)
-          unless data && data.length == length
-            raise ::RuntimeError, 'Kerberos Client: failed to read response'
-          end
+    module Vagrant
+  # This class handles guest-OS specific interactions with a machine.
+  # It is primarily responsible for detecting the proper guest OS
+  # implementation and then delegating capabilities.
+  #
+  # Vagrant has many tasks which require specific guest OS knowledge.
+  # These are implemented using a guest/capability system. Various plugins
+  # register as 'guests' which determine the underlying OS of the system.
+  # Then, 'guest capabilities' register themselves for a specific OS (one
+  # or more), and these capabilities are called.
+  #
+  # Example capabilities might be 'mount_virtualbox_shared_folder' or
+  # 'configure_networks'.
+  #
+  # This system allows for maximum flexibility and pluginability for doing
+  # guest OS specific operations.
+  class Guest
+    include CapabilityHost
     
-              # Encodes the name_type field
-          #
-          # @return [String]
-          def encode_name_type
-            [name_type].pack('N')
-          end
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a KRB_AP_REQ definition, containing the Kerberos protocol version number,
-        # the message type KRB_AP_REQ, an options field to indicate any options in use, and the ticket and authenticator
-        # themselves
-        class ApReq < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of the protocol message
-          attr_accessor :msg_type
-          # @!attribute options
-          #   @return [Integer] request options, affects processing
-          attr_accessor :options
-          # @!attribute ticket
-          #   @return [Rex::Proto::Kerberos::Model::Ticket] The ticket authenticating the client to the server
-          attr_accessor :ticket
-          # @!attribute authenticator
-          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] This contains the authenticator, which includes the
-          #   client's choice of a subkey
-          attr_accessor :authenticator
-    
-                int
-          end
-    
-              # Encodes the Rex::Proto::Kerberos::Model::Element into an ASN.1 String. This
-          # method has been designed to be overridden by subclasses.
-          #
-          # @raise [NoMethodError]
-          def encode
-            raise ::NoMethodError, 'Method designed to be overridden'
-          end
+              # Release access on this machine
+          unlocked_release(entry.id)
         end
       end
+    
+    module Vagrant
+  module Plugin
+    module V1
+      # This is the superclass for all V1 plugins.
+      class Plugin
+        # Special marker that can be used for action hooks that matches
+        # all action sequences.
+        ALL_ACTIONS = :__all_actions__
+    
+                # If we have this machine in our index, load that.
+            entry = @env.machine_index.get(name.to_s)
+            if entry
+              @env.machine_index.release(entry)
+    
+            # Helper method that will set a value if a value is given, or otherwise
+        # return the already set value.
+        #
+        # @param [Symbol] key Key for the data
+        # @param [Object] value Value to store.
+        # @return [Object] Stored value.
+        def self.get_or_set(key, value=UNSET_VALUE)
+          # If no value is to be set, then return the value we have already set
+          return data[key] if value.eql?(UNSET_VALUE)
+    
+      def scope_for_collection
+    case params[:id]
+    when 'featured'
+      @account.statuses.permitted_for(@account, signed_request_account).tap do |scope|
+        scope.merge!(@account.pinned_statuses)
+      end
+    else
+      raise ActiveRecord::NotFound
     end
   end
-end
     
-              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_pvno(input)
-            input.value[0].value.to_i
-          end
+        @statuses = @account.statuses.permitted_for(@account, signed_request_account)
+    @statuses = params[:min_id].present? ? @statuses.paginate_by_min_id(LIMIT, params[:min_id]).reverse : @statuses.paginate_by_max_id(LIMIT, params[:max_id])
+    @statuses = cache_collection(@statuses, Status)
+  end
     
-              # Decodes the cusec field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_cusec(input)
-            input.value[0].value
-          end
+        def update
+      authorize @user, :change_email?
+    
+        def enable
+      authorize @custom_emoji, :enable?
+      @custom_emoji.update!(disabled: false)
+      log_action :enable, @custom_emoji
+      flash[:notice] = I18n.t('admin.custom_emojis.enabled_msg')
+      redirect_to admin_custom_emojis_path(page: params[:page], **@filter_params)
+    end
