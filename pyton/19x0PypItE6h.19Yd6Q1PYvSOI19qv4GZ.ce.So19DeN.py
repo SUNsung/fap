5a26e6daece05ceb7dc6099e21c6c62f57a0ee90
SUNsung <weatherius@gmail.com>
2018-11-06 00:01:36 +0200

    Rewrite [19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py] File in: 2018-11-06 00:01:35

--- pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
+++ pyton/19x0PypItE6h.19Yd6Q1PYvSOI19qv4GZ.ce.So19DeN.py
@@ -1,14 +1,147 @@
 
         
-        DOCUMENTATION = '''
----
-inventory: openshift
-short_description: Openshift gears external inventory script
-description:
-  - Generates inventory of Openshift gears using the REST interface
-  - this permit to reuse playbook to setup an Openshift gear
-version_added: None
-author: Michael Scherer
-'''
+        
+def all_plot(d, full_name='', exclude='', nspaces=0):
+  '''Recursively plot all the LFADS model parameters in the nested
+  dictionary.'''
+  for k, v in d.iteritems():
+    this_name = full_name+'/'+k
+    if isinstance(v, dict):
+      all_plot(v, full_name=this_name, exclude=exclude, nspaces=nspaces+4)
+    else:
+      if exclude == '' or exclude not in this_name:
+        _plot_item(v, name=k, full_name=full_name+'/'+k, nspaces=nspaces+4)
     
-        args = parser.parse_args()
+      # Turn rates, noisy_data, and input into numpy arrays.
+  rates_train = nparray_and_transpose(rates_train)
+  rates_valid = nparray_and_transpose(rates_valid)
+  noisy_data_train = nparray_and_transpose(noisy_data_train)
+  noisy_data_valid = nparray_and_transpose(noisy_data_valid)
+  input_train = nparray_and_transpose(input_train)
+  inputs_valid = nparray_and_transpose(inputs_valid)
+    
+    
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+    
+_SPECIAL_CHAR_MAP = {
+    '\xe2\x80\x98': '\'',
+    '\xe2\x80\x99': '\'',
+    '\xe2\x80\x9c': ''',
+    '\xe2\x80\x9d': ''',
+    '\xe2\x80\x93': '-',
+    '\xe2\x80\x94': '-',
+    '\xe2\x88\x92': '-',
+    '\xce\x84': '\'',
+    '\xc2\xb4': '\'',
+    '`': '\''
+}
+    
+    
+def build_vocab(vocab_file):
+  word_to_id = {}
+    
+                # Statefulness for the Generator.
+            for i, (c, h) in enumerate(model.fake_gen_initial_state):
+              eval_feed[c] = fake_gen_initial_state_eval[i].c
+              eval_feed[h] = fake_gen_initial_state_eval[i].h
+    
+      samples = []
+  for sequence_id in xrange(min(len(arr), max_num_to_print)):
+    buffer_str = ' '.join(
+        [str(id_to_word[index]) for index in arr[sequence_id, :]])
+    samples.append(buffer_str)
+  return samples
+    
+    from models import bidirectional_zaremba
+from models import cnn
+from models import critic_vd
+from models import feedforward
+from models import rnn
+from models import rnn_nas
+from models import rnn_vd
+from models import rnn_zaremba
+from models import seq2seq
+from models import seq2seq_nas
+from models import seq2seq_vd
+from models import seq2seq_zaremba
+    
+    
+  Args:
+    hparams:  MaskGAN hyperparameters.
+    log_probs:  tf.float32 Tensor of log probailities of the tokens selected by
+      the Generator.  Shape [batch_size, sequence_length].
+    dis_predictions:  tf.float32 Tensor of the predictions from the
+      Discriminator.  Shape [batch_size, sequence_length].
+    present:  tf.bool Tensor indicating which tokens are present.  Shape
+      [batch_size, sequence_length].
+    estimated_values:  tf.float32 Tensor of estimated state values of tokens.
+      Shape [batch_size, sequence_length]
+    
+    
+@pytest.fixture(params=[(python_3, False),
+                        (python_3, True),
+                        (python_2, False)])
+def proc(request, spawnu, TIMEOUT):
+    container, instant_mode = request.param
+    proc = spawnu(*container)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(init_bashrc.format(
+        u'--enable-experimental-instant-mode' if instant_mode else ''))
+    proc.sendline(u'bash')
+    if instant_mode:
+        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
+    return proc
+    
+    
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
+    
+    
+@pytest.mark.parametrize('command, packages', [
+    (Command('vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
+     [('vim', 'main'), ('vim-tiny', 'main')])])
+def test_match(mocker, command, packages):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+    
+@pytest.mark.parametrize('command', [
+    Command('aws dynamdb scan', misspelled_command),
+    Command('aws dynamodb scn', misspelled_subcommand),
+    Command('aws dynamodb t-item',
+            misspelled_subcommand_with_multiple_options)])
+def test_match(command):
+    assert match(command)
+    
+        By default, it represents the actual environment.
+    All of the attributes can be overwritten though, which
+    is used by the test suite to simulate various scenarios.
+    
+    from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+    
+    
+with codecs.open(JSON_FILE_PATH, encoding='utf8') as f:
+    JSON_FILE_CONTENT = f.read()
+    
+    
+def test_credentials_in_url_auth_flag_has_priority(httpbin_both):
+    '''When credentials are passed in URL and via -a at the same time,
+     then the ones from -a are used.'''
+    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
+                   auth='user:wrong')
+    r = http('--auth=user:password', 'GET', url)
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+    from utils import TESTS_ROOT
