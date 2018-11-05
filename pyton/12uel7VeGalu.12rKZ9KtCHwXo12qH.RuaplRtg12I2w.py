9e0c18fcd644403a5534e8ad5a859ee26281f803
SUNsung <weatherius@gmail.com>
2018-11-05 09:09:34 +0200

    Rewrite [12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py] File in: 2018-11-05 09:09:33

--- pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
+++ pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
@@ -1,14 +1,23 @@
 
         
-            def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
+            outputs1 = Lambda(lambda x: utils.preprocess_input(x, 'channels_last'),
+                      output_shape=x.shape)(inputs)
+    model1 = Model(inputs, outputs1)
+    out1 = model1.predict(x[np.newaxis])[0]
+    x2 = np.transpose(x, (2, 0, 1))
+    inputs2 = Input(shape=x2.shape)
+    outputs2 = Lambda(lambda x: utils.preprocess_input(x, 'channels_first'),
+                      output_shape=x2.shape)(inputs2)
+    model2 = Model(inputs2, outputs2)
+    out2 = model2.predict(x2[np.newaxis])[0]
+    assert_allclose(out1, out2.transpose(1, 2, 0))
     
-    
-class SalesRanker(MRJob):
-    
-        def _hash_function(self, key):
-        return key % self.size
+        # Arguments
+        model: Keras model instance.
+        f: Keras function returning a list of tensors.
+        ins: list of tensors to be fed to `f`.
+        batch_size: integer batch size.
+        verbose: verbosity mode.
+        steps: Total number of steps (batches of samples)
+            before declaring `predict_loop` finished.
+            Ignored with the default value of `None`.
