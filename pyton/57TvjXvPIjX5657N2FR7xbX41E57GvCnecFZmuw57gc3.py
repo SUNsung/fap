6ac9345498d414b474c6b8b392fa601e097ac7db
SUNsung <weatherius@gmail.com>
2018-11-05 02:16:37 +0200

    Rewrite [57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py] File in: 2018-11-05 02:16:36

--- pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
+++ pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
@@ -1,174 +1,160 @@
 
         
-                # Insert the import statement to setup.py if not present
-        with open(setup_path, 'a+') as setup:
-            setup.seek(0)
-            setup_content = setup.read()
-            if not 'import fastentrypoints' in setup_content:
-                setup.seek(0)
-                setup.truncate()
-                setup.write('import fastentrypoints\n' + setup_content)
+        
+class PostgresSimpleLookup(Lookup):
+    def as_sql(self, qn, connection):
+        lhs, lhs_params = self.process_lhs(qn, connection)
+        rhs, rhs_params = self.process_rhs(qn, connection)
+        params = lhs_params + rhs_params
+        return '%s %s %s' % (lhs, self.operator, rhs), params
     
-    install_requires = ['psutil', 'colorama', 'six', 'decorator', 'pyte']
-extras_require = {':python_version<'3.4'': ['pathlib2'],
-                  ':python_version<'3.3'': ['backports.shutil_get_terminal_size'],
-                  ':sys_platform=='win32'': ['win_unicode_console']}
+        def __init__(self):
+        self.name = 'pgcrypto'
     
     
-@pytest.mark.parametrize('script, output, help_text, result', [
-    ('apt-get isntall vim', invalid_operation('isntall'),
-     apt_get_help, 'apt-get install vim'),
-    ('apt saerch vim', invalid_operation('saerch'),
-     apt_help, 'apt search vim'),
-])
-def test_get_new_command(set_help, output, script, help_text, result):
-    set_help(help_text)
-    assert get_new_command(Command(script, output))[0] == result
-
+class ArrayMinLengthValidator(MinLengthValidator):
+    message = ngettext_lazy(
+        'List contains %(show_value)d item, it should contain no fewer than %(limit_value)d.',
+        'List contains %(show_value)d items, it should contain no fewer than %(limit_value)d.',
+        'limit_value')
     
-        new_command = get_new_command(Command('sudo apt list --upgradable', match_output))
-    assert new_command == 'sudo apt upgrade'
-
-    
-      * dynamodb
-'''
-    
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_match(brew_no_available_formula, brew_already_installed,
-               brew_install_no_argument):
-    assert match(Command('brew install elsticsearch',
-                         brew_no_available_formula))
-    assert not match(Command('brew install git',
-                             brew_already_installed))
-    assert not match(Command('brew install', brew_install_no_argument))
-    
-        def process_lhs(self, qn, connection):
-        if not isinstance(self.lhs.output_field, SearchVectorField):
-            self.lhs = SearchVector(self.lhs)
-        lhs, lhs_params = super().process_lhs(qn, connection)
-        return lhs, lhs_params
-    
-            r = None
+        def save(self, must_create=False):
+        '''
+        Save the current session data to the database. If 'must_create' is
+        True, raise a database error if the saving operation doesn't create a
+        new entry (as opposed to possibly updating an existing entry).
+        '''
+        if self.session_key is None:
+            return self.create()
+        data = self._get_session(no_load=must_create)
+        obj = self.create_model_instance(data)
+        using = router.db_for_write(self.model, instance=obj)
         try:
-            r = Redirect.objects.get(site=current_site, old_path=full_path)
-        except Redirect.DoesNotExist:
-            pass
-        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
-            try:
-                r = Redirect.objects.get(
-                    site=current_site,
-                    old_path=request.get_full_path(force_append_slash=True),
-                )
-            except Redirect.DoesNotExist:
-                pass
-        if r is not None:
-            if r.new_path == '':
-                return self.response_gone_class()
-            return self.response_redirect_class(r.new_path)
-    
-        def delete(self, session_key=None):
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        self._cache.delete(self.cache_key_prefix + session_key)
-    
-        def load(self):
-        try:
-            data = self._cache.get(self.cache_key)
-        except Exception:
-            # Some backends (e.g. memcache) raise an exception on invalid
-            # cache keys. If this happens, reset the session. See #17810.
-            data = None
-    
-        @classmethod
-    def get_model_class(cls):
-        # Avoids a circular import and allows importing SessionStore when
-        # django.contrib.sessions is not in INSTALLED_APPS.
-        from django.contrib.sessions.models import Session
-        return Session
+            with transaction.atomic(using=using):
+                obj.save(force_insert=must_create, force_update=not must_create, using=using)
+        except IntegrityError:
+            if must_create:
+                raise CreateError
+            raise
+        except DatabaseError:
+            if not must_create:
+                raise UpdateError
+            raise
     
     
-class Session(AbstractBaseSession):
-    '''
-    Django provides full support for anonymous sessions. The session
-    framework lets you store and retrieve arbitrary data on a
-    per-site-visitor basis. It stores data on the server side and
-    abstracts the sending and receiving of cookies. Cookies contain a
-    session ID -- not the data itself.
+class AbstractBaseSession(models.Model):
+    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
+    session_data = models.TextField(_('session data'))
+    expire_date = models.DateTimeField(_('expire date'), db_index=True)
     
-    from django.contrib.sites.shortcuts import get_current_site
-from django.core.paginator import EmptyPage, PageNotAnInteger
-from django.http import Http404
-from django.template.response import TemplateResponse
-from django.urls import reverse
-from django.utils.http import http_date
-    
-        This is a private API.
-    '''
-    
-    PY_MAJOR, PY_MINOR, PY_PATCH = sys.version_info[ 0 : 3 ]
-if not ( ( PY_MAJOR == 2 and PY_MINOR == 7 and PY_PATCH >= 1 ) or
-         ( PY_MAJOR == 3 and PY_MINOR >= 4 ) or
-         PY_MAJOR > 3 ):
-  sys.exit( 'YouCompleteMe requires Python >= 2.7.1 or >= 3.4; '
-            'your version of Python is ' + sys.version )
+        fpath = os.path.join(path, 'test')
+    x_test, y_test = load_batch(fpath, label_key=label_mode + '_labels')
     
     
-  def _SendRequest( self ):
-    self._response_future = self.PostDataToHandlerAsync(
-      self._request_data,
-      'receive_messages',
-      timeout = TIMEOUT_SECONDS )
-    return
-    
-    TIMEOUT_SECONDS = 0.1
+# Aliases.
     
     
-def AdjustCandidateInsertionText_ParenInTextAfterCursor_test():
-  with MockTextAfterCursor( 'bar(zoo' ):
-    eq_( [ { 'abbr': 'foobar', 'word': 'foo' } ],
-         base.AdjustCandidateInsertionText( [ 'foobar' ] ) )
+class MSE_MAE_loss:
+    '''Loss function with internal state, for testing serialization code.'''
+    def __init__(self, mse_fraction):
+        self.mse_fraction = mse_fraction
     
-      opts = { 'filter_diagnostics' : {
-    'java,c,cs' : { 'regex' : '.*taco.*' } } }
+            h_tm1 = states[0]  # previous memory state
+        c_tm1 = states[1]  # previous carry state
     
-        # Error match is added after warning matches.
-    assert_that(
-      test_utils.VIM_MATCHES_FOR_WINDOW,
-      has_entries( {
-        1: contains(
-          VimMatch( 'YcmWarningSection', '\%3l\%5c\_.\{-}\%3l\%7c' ),
-          VimMatch( 'YcmWarningSection', '\%3l\%3c\_.\{-}\%3l\%9c' ),
-          VimMatch( 'YcmErrorSection', '\%3l\%8c' )
+            params = self._canonical_to_params(
+            weights=[
+                self.kernel_r,
+                self.kernel_z,
+                self.kernel_h,
+                self.recurrent_kernel_r,
+                self.recurrent_kernel_z,
+                self.recurrent_kernel_h,
+            ],
+            biases=[
+                self.bias_r_i,
+                self.bias_z_i,
+                self.bias_h_i,
+                self.bias_r,
+                self.bias_z,
+                self.bias_h,
+            ],
         )
-      } )
-    )
+        outputs, h = self._cudnn_gru(
+            inputs,
+            input_h=input_h,
+            params=params,
+            is_training=True)
     
-      def _adjust_thread_count( self ):
-    # When the executor gets lost, the weakref callback will wake up
-    # the worker threads.
-    def weakref_cb( _, q=self._work_queue ):
-      q.put( None )
-    # TODO(bquinlan): Should avoid creating new threads if there are more
-    # idle threads than items in the work queue.
-    if len( self._threads ) < self._max_workers:
-      t = threading.Thread( target=_worker,
-                            args=( weakref.ref( self, weakref_cb ),
-                                   self._work_queue ) )
-      t.daemon = True
-      t.start()
-      self._threads.add( t )
+    from . import network
+from .training import Model
+from .base_layer import Layer
+from .input_layer import Input
+from .input_layer import InputLayer
+from .. import backend as K
+from .. import layers as layer_module
     
-            # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+    # Score trained model.
+scores = model.evaluate(x_test, y_test, verbose=1)
+print('Test loss:', scores[0])
+print('Test accuracy:', scores[1])
+
     
-            face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-            name = 'Unknown'
+        plt.figure('scikit-learn GLM benchmark results')
+    plt.xlabel('Dimensions')
+    plt.ylabel('Time (s)')
+    plt.plot(dimensions, time_ridge, color='r')
+    plt.plot(dimensions, time_ols, color='g')
+    plt.plot(dimensions, time_lasso, color='b')
+    
+    import numpy as np
+import gc
+from datetime import datetime
+from sklearn.isotonic import isotonic_regression
+from sklearn.utils.bench import total_seconds
+import matplotlib.pyplot as plt
+import argparse
+    
+                gc.collect()
+            print('benchmarking lars_path (without Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            lars_path(X, y, Gram=None, max_iter=n_informative)
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            lars[i_f, i_s] = delta
+    
+    def plot(func):
+    random_state = check_random_state(0)
+    one_core = []
+    multi_core = []
+    sample_sizes = range(1000, 6000, 1000)
+    
+    
+df = pd.DataFrame(res).set_index(['analyzer', 'ngram_range', 'vectorizer'])
+    
+        dim = start_dim
+    for i in range(0, n):
+        print('============================================')
+        print('Entering iteration %s of %s' % (i, n))
+        print('============================================')
+        dim += step
+        X = np.random.randn(100, dim)
+        Y = np.random.randint(0, n_classes, (100,))
+        bench_scikit_tree_classifier(X, Y)
+        Y = np.random.randn(100)
+        bench_scikit_tree_regressor(X, Y)
+    
+    
+print(':orphan:')
+print()
+heading = 'Available documentation for Scikit-learn'
+print(heading)
+print('=' * len(heading))
+print()
+print('Web-based documentation is available for versions listed below:')
+print()
+    
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+TRAIN_FOLDER = '20news-bydate-train'
+TEST_FOLDER = '20news-bydate-test'
