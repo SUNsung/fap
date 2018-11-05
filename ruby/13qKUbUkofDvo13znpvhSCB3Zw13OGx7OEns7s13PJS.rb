23fbebe069681909942d55f8f6ec5da78952eab3
SUNsung <weatherius@gmail.com>
2018-11-05 13:54:35 +0200

    Rewrite [13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb] File in: 2018-11-05 13:54:34

--- ruby/13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb
+++ ruby/13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb
@@ -1,31 +1,13 @@
 
         
-              redirect_to admin_account_path(@account.id), notice: I18n.t('admin.accounts.change_email.changed_msg')
+        class SinatraWorker
+  include Sidekiq::Worker
+    
+        def initialize(options)
+      @strictly_ordered_queues = !!options[:strict]
+      @queues = options[:queues].map { |q| 'queue:#{q}' }
+      if @strictly_ordered_queues
+        @queues = @queues.uniq
+        @queues << TIMEOUT
+      end
     end
-    
-        def create
-      authorize ReportNote, :create?
-    
-      private
-    
-    When /^I (?:sign|log) in manually as '([^']*)' with password '([^']*)'( on the mobile website)?$/ \
-do |username, password, mobile|
-  @me = User.find_by_username(username)
-  @me.password ||= password
-  manual_login
-  confirm_login mobile
-end
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-        def sort_options(options)
-      not_applicable_to_capistrano = %w(quiet silent verbose)
-      options.reject! do |(switch, *)|
-        switch =~ /--#{Regexp.union(not_applicable_to_capistrano)}/
-      end
-    
-          def has_role?(role)
-        roles.include? role.to_sym
-      end
