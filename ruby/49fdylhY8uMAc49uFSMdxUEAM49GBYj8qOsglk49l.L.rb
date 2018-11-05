e23e08e9c59b5dfe83d72af59debf03c714d0c63
SUNsung <weatherius@gmail.com>
2018-11-05 12:32:02 +0200

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2018-11-05 12:32:01

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,104 +1,95 @@
 
         
-                    if placeholder_required?(html_options)
-              raise ArgumentError, 'include_blank cannot be false for a required field.' if options[:include_blank] == false
-              options[:include_blank] ||= true unless options[:prompt]
-            end
-    
-            def render(&block)
-          render_collection_for(RadioButtonBuilder, &block)
-        end
-    
-          # Specify the layout to use for this class.
-      #
-      # If the specified layout is a:
-      # String:: the String is the template name
-      # Symbol:: call the method specified by the symbol
-      # Proc::   call the passed Proc
-      # false::  There is no layout
-      # true::   raise an ArgumentError
-      # nil::    Force default layout behavior with inheritance
-      #
-      # Return value of +Proc+ and +Symbol+ arguments should be +String+, +false+, +true+ or +nil+
-      # with the same meaning as described above.
-      # ==== Parameters
-      # * <tt>layout</tt> - The layout to use.
-      #
-      # ==== Options (conditions)
-      # * :only   - A list of actions to apply this layout to.
-      # * :except - Apply this layout to all actions but this one.
-      def layout(layout, conditions = {})
-        include LayoutConditions unless conditions.empty?
-    
-      gem 'danger'
-end
-    
-      autoload :AggregateTarget,           'cocoapods/target/aggregate_target'
-  autoload :Command,                   'cocoapods/command'
-  autoload :Deintegrator,              'cocoapods_deintegrate'
-  autoload :Executable,                'cocoapods/executable'
-  autoload :ExternalSources,           'cocoapods/external_sources'
-  autoload :Installer,                 'cocoapods/installer'
-  autoload :HooksManager,              'cocoapods/hooks_manager'
-  autoload :PodTarget,                 'cocoapods/target/pod_target'
-  autoload :Project,                   'cocoapods/project'
-  autoload :Resolver,                  'cocoapods/resolver'
-  autoload :Sandbox,                   'cocoapods/sandbox'
-  autoload :Target,                    'cocoapods/target'
-  autoload :Validator,                 'cocoapods/validator'
-    
-          super(argv)
-    ensure
-      UI.print_warnings
-    end
-    
-      gem.required_ruby_version = '>= 2.0'
-  gem.add_dependency 'airbrussh', '>= 1.0.0'
-  gem.add_dependency 'i18n'
-  gem.add_dependency 'rake', '>= 10.0.0'
-  gem.add_dependency 'sshkit', '>= 1.9.0'
-    
-        require 'capistrano/scm/#{scm_name}'
-    install_plugin #{built_in_scm_plugin_class_name}
-    
-            def lvalue(key)
-          key.to_s.chomp('=').to_sym
-        end
-      end
+            # This returns whether the guest is ready to work. If this returns
+    # `false`, then {#detect!} should be called in order to detect the
+    # guest OS.
+    #
+    # @return [Boolean]
+    def ready?
+      !!capability_host_chain
     end
   end
 end
 
     
-          private
+              name = name.to_s
+          name = name[0...-1] if name.end_with?('=')
     
-            def update
-          authorize! :update, @order, order_token
-          @address = find_address
+      #
+  # Closes the supplied client, if valid.
+  #
+  def close_client(cli)
+    listener.close_client(cli)
+  end
     
-              inventory_unit.transaction do
-            if inventory_unit.update_attributes(inventory_unit_params)
-              fire
-              render :show, status: 200
-            else
-              invalid_resource!(inventory_unit)
+    module Rex
+  module Proto
+    module Kerberos
+      module Crypto
+        module Rc4Hmac
+          # Decrypts the cipher using RC4-HMAC schema
+          #
+          # @param cipher [String] the data to decrypt
+          # @param key [String] the key to decrypt
+          # @param msg_type [Integer] the message type
+          # @return [String] the decrypted cipher
+          # @raise [RuntimeError] if decryption doesn't succeed
+          def decrypt_rc4_hmac(cipher, key, msg_type)
+            unless cipher && cipher.length > 16
+              raise ::RuntimeError, 'RC4-HMAC decryption failed'
             end
+    
+              # Encodes the authenticator field
+          #
+          # @return [String]
+          def encode_authenticator
+            authenticator.encode
           end
         end
+      end
+    end
+  end
+end
     
-              if Spree::Cart::Update.call(order: @order, params: line_items_attributes).success?
-            @line_item.reload
-            respond_with(@line_item, default_template: :show)
-          else
-            invalid_resource!(@line_item)
-          end
-        end
+                seq = OpenSSL::ASN1::Sequence.new(elems)
     
-            def new; end
+    # This file provides the following methods:
+#   encodeURIComponent(componentString)
+#   string.charCodeAt(k)
     
-              @line_item = Spree::Cart::AddItem.call(order: @order,
-                                                 variant: variant,
-                                                 quantity: quantity,
-                                                 options: { shipment: @shipment }).value
+          def css # custom css
+        @css
+      end
     
-            private
+        @wiki.clear_cache
+    page_2 = @wiki.page(page_1.name)
+    assert_equal 'abc', page_2.raw_data
+    assert_equal '[no message]', page_2.version.message
+    assert_not_equal page_1.version.sha, page_2.version.sha
+  end
+    
+        @wiki = Gollum::Wiki.new(@path)
+    page  = @wiki.page('k')
+    assert_equal '바뀐 text', utf8(page.raw_data)
+    assert_equal 'ghi', page.version.message
+  end
+    
+    module RuboCop
+  module AST
+    # A node extension for `hash` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `hash` nodes within RuboCop.
+    class HashNode < Node
+      # Returns an array of all the key value pairs in the `hash` literal.
+      #
+      # @return [Array<PairNode>] an array of `pair` nodes
+      def pairs
+        each_pair.to_a
+      end
+    
+          # A helper class for comparing the positions of different parts of a
+      # `pair` node.
+      class HashElementDelta
+        def initialize(first, second)
+          @first = first
+          @second = second
