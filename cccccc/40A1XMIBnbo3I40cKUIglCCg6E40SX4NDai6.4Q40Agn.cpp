c93e253c087e43b07ca63a47ae9901ecd7fc5010
SUNsung <weatherius@gmail.com>
2018-11-05 22:25:37 +0200

    Rewrite [40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp] File in: 2018-11-05 22:25:36

--- cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
+++ cccccc/40A1XMIBnbo3I40cKUIglCCg6E40SX4NDai6.4Q40Agn.cpp
@@ -1,82 +1,182 @@
 
         
-          class ConstOneofAccessor {
-   public:
-    explicit ConstOneofAccessor(const MessageDef* msg) : msg_(msg) {}
-    const_oneof_iterator begin() { return msg_->oneof_begin(); }
-    const_oneof_iterator end() { return msg_->oneof_end(); }
-   private:
-    const MessageDef* msg_;
-  };
+        std::vector<string> RunCppShapeInference(
+    int graph_def_version, const string& serialized_node_def,
+    const std::vector<string>& input_serialized_shapes,
+    PyObject* input_constant_tensor_values,
+    const std::vector<string>& input_constant_tensor_as_shape_values,
+    TF_Status* out_status) {
+  if (!PyList_Check(input_constant_tensor_values)) {
+    TF_SetStatus(out_status, TF_INVALID_ARGUMENT, 'Invalid python value');
+    return std::vector<string>();
+  }
+    }
     
-    #include <google/protobuf/compiler/java/java_extension_lite.h>
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    REGISTER_OP('Invalid')
+    .Attr('invalid attr: int32')  // invalid since the name has a space.
+    .Doc(R'doc(
+An op to test that invalid ops do not successfully generate invalid python code.
+)doc');
+    
+      int type_num = -1;
+  Status s =
+      TF_DataType_to_PyArray_TYPE(static_cast<TF_DataType>(dtype), &type_num);
+  if (!s.ok()) {
+    return s;
+  }
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Safe containers for an owned TFE_TensorHandle. On destruction, the handle
+// will be deleted by TFE_DeleteTensorHandle.
+using Safe_TFE_TensorHandlePtr =
+    std::unique_ptr<TFE_TensorHandle, detail::TFETensorHandleDeleter>;
+Safe_TFE_TensorHandlePtr make_safe(TFE_TensorHandle* handle);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include 'tensorflow/python/util/kernel_registry.h'
     
     
-    {  (*variables)['default_entry'] = (*variables)['capitalized_name'] +
-      'DefaultEntryHolder.defaultEntry';
-  (*variables)['map_field_parameter'] = (*variables)['default_entry'];
-  (*variables)['descriptor'] =
-      name_resolver->GetImmutableClassName(descriptor->file()) +
-      '.internal_' + UniqueFileScopeIdentifier(descriptor->message_type()) +
-      '_descriptor, ';
-  (*variables)['ver'] = GeneratedCodeVersionSuffix();
+    {
+    {}  // namespace cuda
+}  // namespace stream_executor
+    
+    
+    {
+    {}  // namespace cuda
+}  // namespace stream_executor
+    
+    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
+  result->append(key.user_key.data(), key.user_key.size());
+  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
 }
     
-    // Strip package name from a descriptor's full name.
-// For example:
-//   Full name   : foo.Bar.Baz
-//   Package name: foo
-//   After strip : Bar.Baz
-string StripPackageName(const string& full_name,
-                        const FileDescriptor* file) {
-  if (file->package().empty()) {
-    return full_name;
-  } else {
-    // Strip package name
-    return full_name.substr(file->package().size() + 1);
+    TEST(FormatTest, InternalKey_EncodeDecode) {
+  const char* keys[] = { '', 'k', 'hello', 'longggggggggggggggggggggg' };
+  const uint64_t seq[] = {
+    1, 2, 3,
+    (1ull << 8) - 1, 1ull << 8, (1ull << 8) + 1,
+    (1ull << 16) - 1, 1ull << 16, (1ull << 16) + 1,
+    (1ull << 32) - 1, 1ull << 32, (1ull << 32) + 1
+  };
+  for (int k = 0; k < sizeof(keys) / sizeof(keys[0]); k++) {
+    for (int s = 0; s < sizeof(seq) / sizeof(seq[0]); s++) {
+      TestKey(keys[k], seq[s], kTypeValue);
+      TestKey('hello', 1, kTypeDeletion);
+    }
   }
 }
     
-    #endif  // CAFFE_DATA_TRANSFORMER_HPP_
+    
+    {  delete iter;
+  delete table;
+  delete file;
+  return Status::OK();
+}
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
 
     
-    #include <vector>
-    
-    #endif  // CAFFE_ARGMAX_LAYER_HPP_
-
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/data_transformer.hpp'
-#include 'caffe/internal_thread.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-#include 'caffe/util/blocking_queue.hpp'
-    
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+    TEST(RecoveryTest, MultipleLogFiles) {
+  ASSERT_OK(Put('foo', 'bar'));
+  Close();
+  ASSERT_EQ(1, NumLogs());
     }
     
-    namespace caffe {
+    // We want to make sure that with a single writer and multiple
+// concurrent readers (with no synchronization other than when a
+// reader's iterator is created), the reader always observes all the
+// data that was present in the skip list when the iterator was
+// constructor.  Because insertions are happening concurrently, we may
+// also observe new values that were inserted since the iterator was
+// constructed, but we should never miss any values that were present
+// at iterator construction time.
+//
+// We generate multi-part keys:
+//     <key,gen,hash>
+// where:
+//     key is in range [0..K-1]
+//     gen is a generation number for key
+//     hash is hash(key,gen)
+//
+// The insertion code picks a random key, sets gen to be 1 + the last
+// generation number inserted for that key, and sets hash to Hash(key,gen).
+//
+// At the beginning of a read, we snapshot the last inserted
+// generation number for each key.  We then iterate, including random
+// calls to Next() and Seek().  For every key we encounter, we
+// check that it is either expected given the initial snapshot or has
+// been concurrently added since the iterator started.
+class ConcurrentTest {
+ private:
+  static const uint32_t K = 4;
     }
     
-    namespace mars_boost {} namespace boost = mars_boost; namespace mars_boost {
-namespace atomics {
-namespace detail {
+    REGISTER_INTERNAL(LoggerConfigParserPlugin, 'config_parser', 'logger');
+    
+    TEST_F(PacksTests, test_discovery_zero_state) {
+  Pack pack('discovery_pack', getPackWithDiscovery().doc());
+  auto stats = pack.getStats();
+  EXPECT_EQ(stats.total, 0U);
+  EXPECT_EQ(stats.hits, 0U);
+  EXPECT_EQ(stats.misses, 0U);
+}
+    
+    TEST_F(ProcessTests, test_launchWorker) {
+  {
+    std::vector<char*> argv;
+    for (size_t i = 0; i < kExpectedWorkerArgsCount; i++) {
+      char* entry = new char[strlen(kExpectedWorkerArgs[i]) + 1];
+      EXPECT_NE(entry, nullptr);
+      memset(entry, '\0', strlen(kExpectedWorkerArgs[i]) + 1);
+      memcpy(entry, kExpectedWorkerArgs[i], strlen(kExpectedWorkerArgs[i]));
+      argv.push_back(entry);
     }
+    argv.push_back(nullptr);
     }
     }
     
-    namespace atomics {
+    TEST_F(QueryTests, test_get_query_results) {
+  // Grab an expected set of query data and add it as the previous result.
+  auto encoded_qd = getSerializedQueryDataJSON();
+  auto query = getOsqueryScheduledQuery();
+  auto status = setDatabaseValue(kQueries, 'foobar', encoded_qd.first);
+  EXPECT_TRUE(status.ok());
+    }
+    
+    bool INotifyEventPublisher::monitorSubscription(
+    INotifySubscriptionContextRef& sc, bool add_watch) {
+  std::string discovered = sc->path;
+  if (sc->path.find('**') != std::string::npos) {
+    sc->recursive = true;
+    discovered = sc->path.substr(0, sc->path.find('**'));
+    sc->path = discovered;
+  }
+    }
+    
+    
+    {  auto subscription = Subscription::create('TestSubscriber', mc);
+  auto status = EventFactory::addSubscription('inotify', subscription);
+  EXPECT_TRUE(status.ok());
+  EventFactory::deregisterEventPublisher('inotify');
+}
+    
+    TEST_F(EventsTests, test_create_custom_event_publisher) {
+  auto basic_pub = std::make_shared<BasicEventPublisher>();
+  basic_pub->setName('BasicPublisher');
+  EventFactory::registerEventPublisher(basic_pub);
     }
