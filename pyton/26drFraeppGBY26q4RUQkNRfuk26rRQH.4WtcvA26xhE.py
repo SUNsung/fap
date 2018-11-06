25dc92219abf0bf63942dc9fa3e9a7c827aa8b63
SUNsung <weatherius@gmail.com>
2018-11-06 14:32:00 +0200

    Rewrite [26drFraeppGBY26q4RUQkNRfuk26rRQH.4WtcvA26xhE.py] File in: 2018-11-06 14:32:00

--- pyton/26drFraeppGBY26q4RUQkNRfuk26rRQH.4WtcvA26xhE.py
+++ pyton/26drFraeppGBY26q4RUQkNRfuk26rRQH.4WtcvA26xhE.py
@@ -1,71 +1,208 @@
 
         
-        
-DIRECTORY_URL = 'https://acme-staging.api.letsencrypt.org/directory'
-BITS = 2048  # minimum for Boulder
-DOMAIN = 'example1.com'  # example.com is ignored by Boulder
+        if is_py2:
+    from urllib import (
+        quote, unquote, quote_plus, unquote_plus, urlencode, getproxies,
+        proxy_bypass, proxy_bypass_environment, getproxies_environment)
+    from urlparse import urlparse, urlunparse, urljoin, urlsplit, urldefrag
+    from urllib2 import parse_http_list
+    import cookielib
+    from Cookie import Morsel
+    from StringIO import StringIO
+    from collections import Callable, Mapping, MutableMapping, OrderedDict
     
-            with mock.patch('certbot.util.get_os_info') as mock_info:
-            for distro in entrypoint.OVERRIDE_CLASSES.keys():
-                mock_info.return_value = (distro, 'whatever')
-                self.assertEqual(entrypoint.get_configurator(),
-                                 entrypoint.OVERRIDE_CLASSES[distro])
+    import idna
+import urllib3
+import chardet
     
-          # We always go for the first diagnostic on the line because diagnostics
-      # are sorted by errors in priority and Vim can only display one sign by
-      # line.
-      name = 'YcmError' if _DiagnosticIsError( diags[ 0 ] ) else 'YcmWarning'
-      sign = vimsupport.CreateSign( line, name, self._bufnr )
+    RETURN = '''
+cloud_front_origin_access_identity:
+  description: The origin access identity's information.
+  returned: always
+  type: complex
+  contains:
+    cloud_front_origin_access_identity_config:
+      description: describes a url specifying the origin access identity.
+      returned: always
+      type: complex
+      contains:
+        caller_reference:
+          description: a caller reference for the oai
+          returned: always
+          type: string
+        comment:
+          description: a comment describing the oai
+          returned: always
+          type: string
+    id:
+      description: a unique identifier of the oai
+      returned: always
+      type: string
+    s3_canonical_user_id:
+      description: the cannonical user id of the user who created the oai
+      returned: always
+      type: string
+e_tag:
+  description: The current version of the origin access identity created.
+  returned: always
+  type: string
+location:
+  description: The fully qualified URI of the new origin access identity just created.
+  returned: when initially created
+  type: string
     
-    SYNTAX_ARGUMENT_REGEX = re.compile(
-  r'^\w+=.*$' )
+        return matched_asgs
+    
+    EXAMPLES = '''
+# Note: These examples do not set authentication details, see the AWS Guide for details.
+    
+        connection = boto3_conn(module,
+                            resource='ec2', conn_type='client',
+                            region=region, endpoint=ec2_url, **aws_connect_params)
+    
+        name = module.params.get('name')
+    state = module.params.get('state')
+    replication_id = module.params.get('replication_id')
+    cluster_id = module.params.get('cluster_id')
+    target = module.params.get('target')
+    bucket = module.params.get('bucket')
+    
+        orig_cert_names = [ctb['server_certificate_name'] for ctb in
+                       iam.get_all_server_certs().list_server_certificates_result.server_certificate_metadata_list]
+    orig_cert_bodies = [iam.get_server_certificate(thing).get_server_certificate_result.certificate_body
+                        for thing in orig_cert_names]
+    if new_name == name:
+        new_name = None
+    if new_path == path:
+        new_path = None
     
     
-  def No_Insertion_Text_test( self ):
-    self._Check( 0, {
-      'menu_text':       'MENU TEXT',
-      'extra_menu_info': 'EXTRA MENU INFO',
-      'kind':            'K',
-      'detailed_info':   'DETAILED INFO',
-      'extra_data': {
-        'doc_string':    'DOC STRING',
-      },
-    }, {
-      'word'     : '',
-      'abbr'     : 'MENU TEXT',
-      'menu'     : 'EXTRA MENU INFO',
-      'kind'     : 'k',
-      'info'     : 'DETAILED INFO\nDOC STRING',
-      'dup'      : 1,
-      'empty'    : 1,
-      'user_data': '0'
-    } )
+DOCUMENTATION = '''
+---
+module: iam_server_certificate_facts
+short_description: Retrieve the facts of a server certificate
+description:
+  - Retrieve the attributes of a server certificate
+version_added: '2.2'
+author: 'Allen Sanabria (@linuxdynasty)'
+requirements: [boto3, botocore]
+options:
+  name:
+    description:
+      - The name of the server certificate you are retrieving attributes for.
+    required: true
+extends_documentation_fragment:
+    - aws
+    - ec2
+'''
+    
+        :param module: Ansible module reference
+    :param aws: AWS client connection
+    :return dict:
+    '''
+    client = aws.client('lambda')
+    results = dict()
+    changed = False
+    current_state = 'absent'
+    state = module.params['state']
+    
+        lambda_facts = dict()
+    
+    test_encode_face = '''
+encoding = face_recognition.face_encodings(image, known_face_locations=face_locations)[0]
+'''
+    
+    # Load an image with an unknown face
+unknown_image = face_recognition.load_image_file('two_people.jpg')
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+class TestManager:
+    def __init__(self):
+        self._reporter = None
+        self._db = None
+        self._tc = None
+    
+    
+class BaseRegisteredClass(object):
+    __metaclass__ = RegistryHolder
+    '''
+        Any class that will inherits from BaseRegisteredClass will be included
+        inside the dict RegistryHolder.REGISTRY, the key being the name of the
+        class and the associated value, the class itself.
+    '''
+    pass
+    
+        def __init__(self):
+        '''We have an AM state and an FM state'''
+        self.amstate = AmState(self)
+        self.fmstate = FmState(self)
+        self.state = self.amstate
+    
+    ### OUTPUT ###
+# spam
+# ----------
+# eggs
+# ----------
+# apple
+# ----------
+# apple
+# ----------
+# eggs
+# ----------
+# spam
+# ----------
+# maps
+# ----------
+# sgge
+# ----------
+# elppa
+# ----------
+# elppa
+# ----------
+# sgge
+# ----------
+# maps
+# ----------
+# ['s', 'p', 'a', 'm']
+# ----------
+# ['e', 'g', 'g', 's']
+# ----------
+# ['a', 'p', 'p', 'l', 'e']
+# ----------
+# ['a', 'p', 'p', 'l', 'e']
+# ----------
+# ['e', 'g', 'g', 's']
+# ----------
+# ['s', 'p', 'a', 'm']
+# ----------
+# ['m', 'a', 'p', 's']
+# ----------
+# ['s', 'g', 'g', 'e']
+# ----------
+# ['e', 'l', 'p', 'p', 'a']
+# ----------
+# ['e', 'l', 'p', 'p', 'a']
+# ----------
+# ['s', 'g', 'g', 'e']
+# ----------
+# ['m', 'a', 'p', 's']
+# ----------
 
     
-      def ServerResponse( *args ):
-    return {
-      'completions': [ {
-        'insertion_text': 'insertion_text',
-        'menu_text': 'menu_text',
-        'extra_menu_info': 'extra_menu_info',
-        'detailed_info': 'detailed_info',
-        'kind': 'kind',
-        'extra_data': {
-           'doc_string': 'doc_string'
-        }
-      } ],
-      'completion_start_column': 3,
-      'errors': [ {
-        'exception': {
-           'TYPE': 'Exception'
-        },
-        'message': 'message',
-        'traceback': 'traceback'
-      } ]
-    }
+        def __str__(self):
+        return 'Cat'
+    
+        def contribute(self):
+        self.blackboard.common_state['problems'] += random.randint(1, 2)
+        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
+        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
+        self.blackboard.common_state['progress'] += random.randint(10, 100)
     
     
-def FilterToCompletedCompletions_ShortTextDoesntRaise_test():
-  completions = [ BuildCompletion( insertion_text = 'AAA' ) ]
-  result = _FilterToMatchingCompletions( CompleteItemIs( 'A' ), completions )
-  eq_( list( result ), [] )
+class Suspect(OutOfService):
+    def __init__(self, HierachicalStateMachine):
+        self._hsm = HierachicalStateMachine
