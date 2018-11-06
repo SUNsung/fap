d2c45c43b0913705bb6707b573df4ab02e0946d3
SUNsung <weatherius@gmail.com>
2018-11-06 15:59:46 +0200

    Rewrite [062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py] File in: 2018-11-06 15:59:45

--- pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
+++ pyton/062ntJTQT1O6A065RJmINIknuE06hdngPWcSu6606vEl.py
@@ -1,171 +1,213 @@
 
         
-            # Then we cluster the lines together as blocks
-    # Each block represents a collection of lines that should be sorted
-    # This was done by assuming only links ([...](...)) are meant to be sorted
-    # Clustering is done by indentation
-    blocks = []
-    last_indent = None
-    for line in read_me:
-        s_line = line.lstrip()
-        indent = len(line) - len(s_line)
-    
-            Tests on MNIST show that Antirectifier allows to train networks
-        with twice less parameters yet with comparable
-        classification accuracy as an equivalent ReLU-based network.
-    '''
-    
-    The VAE has a modular design. The encoder, decoder and VAE
-are 3 models that share weights. After training the VAE model,
-the encoder can be used to  generate latent vectors.
-The decoder can be used to generate MNIST digits by sampling the
-latent vector from a Gaussian distribution with mean=0 and std=1.
-    
-    
-# MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
-        return (x_train, y_train), (x_test, y_test)
-
-    
-    
-# Aliases
-    
-        Returns
-    -------
-    array of floats shaped like (metrics, formats, samples, classes, density)
-        Time in seconds.
-    '''
-    metrics = np.atleast_1d(metrics)
-    samples = np.atleast_1d(samples)
-    classes = np.atleast_1d(classes)
-    density = np.atleast_1d(density)
-    formats = np.atleast_1d(formats)
-    out = np.zeros((len(metrics), len(formats), len(samples), len(classes),
-                    len(density)), dtype=float)
-    it = itertools.product(samples, classes, density)
-    for i, (s, c, d) in enumerate(it):
-        _, y_true = make_multilabel_classification(n_samples=s, n_features=1,
-                                                   n_classes=c, n_labels=d * c,
-                                                   random_state=42)
-        _, y_pred = make_multilabel_classification(n_samples=s, n_features=1,
-                                                   n_classes=c, n_labels=d * c,
-                                                   random_state=84)
-        for j, f in enumerate(formats):
-            f_true = f(y_true)
-            f_pred = f(y_pred)
-            for k, metric in enumerate(metrics):
-                t = timeit(partial(metric, f_true, f_pred), number=n_times)
-    
-        results = defaultdict(lambda: [])
-    
-    The input data is mostly low rank but is a fat infinite tail.
-'''
-from __future__ import print_function
-    
-    The quality of the approximation is measured by the spectral norm discrepancy
-between the original input matrix and the reconstructed one (by multiplying
-the randomized_svd's outputs). The spectral norm is always equivalent to the
-largest singular value of a matrix. (3) justifies this choice. However, one can
-notice in these experiments that Frobenius and spectral norms behave
-very similarly in a qualitative sense. Therefore, we suggest to run these
-benchmarks with `enable_spectral_norm = False`, as Frobenius' is MUCH faster to
-compute.
-    
-        op.add_option('--n-step',
-                  dest='n_steps', default=5, type=int,
-                  help='Number of step interval between 0 and n_population.')
-    
-    
-def sparsity_ratio(X):
-    return np.count_nonzero(X) / float(n_samples * n_features)
-    
-    def os_constant(key):
-    # XXX TODO: In the future, this could return different constants
-    #           based on what OS we are running under.  To see an
-    #           approach to how to handle different OSes, see the
-    #           apache version of this file.  Currently, we do not
-    #           actually have any OS-specific constants on Nginx.
-    '''
-    Get a constant value for operating system
-    
-        @nonce.decoder
-    def nonce(value):  # pylint: disable=missing-docstring,no-self-argument
         try:
-            return jose.decode_b64jose(value)
-        except jose.DeserializationError as error:
-            # TODO: custom error
-            raise jose.DeserializationError('Invalid nonce: {0}'.format(error))
+    input = raw_input
+except NameError:
+    pass
+    
+        with io.open(infile, encoding='utf-8') as inf:
+        readme = inf.read()
+    
+                        # Pandoc's definition_lists. See http://pandoc.org/README.html
+                    # for more information.
+                    ret += '\n%s\n:   %s\n' % (option, description)
+                    continue
+            ret += line.lstrip() + '\n'
+        else:
+            ret += line + '\n'
+    
+        with open(ZSH_COMPLETION_TEMPLATE) as f:
+        template = f.read()
+    
+            def get_ids(params):
+            ydl = YDL(params)
+            # make a copy because the dictionary can be modified
+            ydl.process_ie_result(playlist.copy())
+            return [int(v['id']) for v in ydl.downloaded_info_dicts]
+    
+            password = intlist_to_bytes(self.key).decode('utf-8')
+        encrypted = base64.b64encode(
+            intlist_to_bytes(self.iv[:8]) +
+            b'\x0b\xe6\xa4\xd9z\x0e\xb8\xb9\xd0\xd4i_\x85\x1d\x99\x98_\xe5\x80\xe7.\xbf\xa5\x83'
+        ).decode('utf-8')
+        decrypted = (aes_decrypt_text(encrypted, password, 32))
+        self.assertEqual(decrypted, self.secret_msg)
     
     
-UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
-'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
+class TestCache(unittest.TestCase):
+    def setUp(self):
+        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
+        _mkdir(TESTDATA_DIR)
+        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
+        self.tearDown()
     
-        def tearDown(self):
-        shutil.rmtree(self.config_dir)
-        shutil.rmtree(self.work_dir)
-        shutil.rmtree(self.temp_dir)
+        def test_all_present(self):
+        import youtube_dl.compat
+        all_names = youtube_dl.compat.__all__
+        present_names = set(filter(
+            lambda c: '_' in c and not c.startswith('_'),
+            dir(youtube_dl.compat))) - set(['unicode_literals'])
+        self.assertEqual(all_names, sorted(present_names))
     
-        def _get_config_text(self, achall, ip_addrs):
-        '''Chocolate virtual server configuration text
     
-        if len(sys.argv) == 2:
-        main(sys.argv[1])
-    else:
-        print('Choose one path as argument one')
+def test_current_version():
+    version = Environment().config['__meta__']['httpie']
+    assert version == __version__
 
     
-    
-@coroutine
-def coroutine2(target):
-    while True:
-        request = yield
-        if 10 < request <= 20:
-            print('request {} handled in coroutine 2'.format(request))
-        else:
-            target.send(request)
-    
-        fftv.publish('cartoon')
-    fftv.publish('music')
-    fftv.publish('ads')
-    fftv.publish('movie')
-    fftv.publish('cartoon')
-    fftv.publish('cartoon')
-    fftv.publish('movie')
-    fftv.publish('blank')
-    
-        print('After subclassing: ')
-    for k in RegistryHolder.REGISTRY:
-        print(k)
+        def __init__(self, get_response=None):
+        if not apps.is_installed('django.contrib.sites'):
+            raise ImproperlyConfigured(
+                'You cannot use RedirectFallbackMiddleware when '
+                'django.contrib.sites is not installed.'
+            )
+        super().__init__(get_response)
     
     
-def lazy_property2(fn):
-    attr = '_lazy__' + fn.__name__
-    
-        def get_objects(self):
-        '''Get all objects'''
-        return self._objects
-    
-        def __getattr__(self, name):
-        def wrapper(*args, **kwargs):
-            if hasattr(self.delegate, name):
-                attr = getattr(self.delegate, name)
-                if callable(attr):
-                    return attr(*args, **kwargs)
-        return wrapper
+def x_robots_tag(func):
+    @wraps(func)
+    def inner(request, *args, **kwargs):
+        response = func(request, *args, **kwargs)
+        response['X-Robots-Tag'] = 'noindex, noodp, noarchive'
+        return response
+    return inner
     
     
-class Professor(AbstractExpert):
+@pytest.fixture
+def httpbin(httpbin):
+    return prepare_url(httpbin)
     
-    # Load some sample pictures and learn how to recognize them.
-lmm_image = face_recognition.load_image_file('lin-manuel-miranda.png')
-lmm_face_encoding = face_recognition.face_encodings(lmm_image)[0]
+    import pytest
     
-                face_names.append(name)
+    from . import utils
+from . import packages
+from .models import Request, Response, PreparedRequest
+from .api import request, get, head, post, patch, put, delete, options
+from .sessions import session, Session
+from .status_codes import codes
+from .exceptions import (
+    RequestException, Timeout, URLRequired,
+    TooManyRedirects, HTTPError, ConnectionError,
+    FileModeWarning, ConnectTimeout, ReadTimeout
+)
     
-            # 0.6 is the default face distance match threshold. So we'll spot-check that the numbers returned
-        # are above or below that based on if they should match (since the exact numbers could vary).
-        self.assertEqual(type(distance_results), np.ndarray)
-        self.assertLessEqual(distance_results[0], 0.6)
-        self.assertLessEqual(distance_results[1], 0.6)
-        self.assertGreater(distance_results[2], 0.6)
+            A1 = '%s:%s:%s' % (self.username, realm, self.password)
+        A2 = '%s:%s' % (method, path)
+    
+    author:
+  - Mike Mochan (@mmochan)
+  - Will Thames (@willthames)
+extends_documentation_fragment:
+  - aws
+  - ec2
+options:
+    name:
+        description: Name of the Web Application Firewall rule
+        required: yes
+    metric_name:
+        description:
+        - A friendly name or description for the metrics for the rule
+        - The name can contain only alphanumeric characters (A-Z, a-z, 0-9); the name can't contain whitespace.
+        - You can't change metric_name after you create the rule
+        - Defaults to the same as name with disallowed characters removed
+    state:
+        description: whether the rule should be present or absent
+        choices:
+        - present
+        - absent
+        default: present
+    conditions:
+        description: >
+          list of conditions used in the rule. Each condition should
+          contain I(type): which is one of [C(byte), C(geo), C(ip), C(size), C(sql) or C(xss)]
+          I(negated): whether the condition should be negated, and C(condition),
+          the name of the existing condition. M(aws_waf_condition) can be used to
+          create new conditions
+    purge_conditions:
+        description:
+          - Whether or not to remove conditions that are not passed when updating `conditions`.
+            Defaults to false.
+'''
+    
+    - name: create a batch of invalidations using an alias as a reference and one path using a wildcard match
+  cloudfront_invalidation:
+    alias: alias.test.com
+    caller_reference: testing 123
+    target_paths:
+      - /testpathone/test4.css
+      - /testpathtwo/test5.js
+      - /testpaththree/*
+    
+    
+def describe_log_group(client, log_group_name, module):
+    params = {}
+    if log_group_name:
+        params['logGroupNamePrefix'] = log_group_name
+    try:
+        desc_log_group = client.describe_log_groups(**params)
+        return desc_log_group
+    except botocore.exceptions.ClientError as e:
+        module.fail_json(msg='Unable to describe log group {0}: {1}'.format(log_group_name, to_native(e)),
+                         exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
+    except botocore.exceptions.BotoCoreError as e:
+        module.fail_json(msg='Unable to describe log group {0}: {1}'.format(log_group_name, to_native(e)),
+                         exception=traceback.format_exc())
+    
+    version_added: '2.2'
+author: 'Jose Armesto (@fiunchinho)'
+options:
+  region:
+    description:
+      - The AWS region to use.
+    required: true
+    aliases: ['aws_region', 'ec2_region']
+  name_regex:
+    description:
+      - A Launch Configuration to match
+      - It'll be compiled as regex
+    required: True
+  sort_order:
+    description:
+      - Order in which to sort results.
+    choices: ['ascending', 'descending']
+    default: 'ascending'
+  limit:
+    description:
+      - How many results to show.
+      - Corresponds to Python slice notation like list[:limit].
+requirements:
+  - 'python >= 2.6'
+  - boto3
+extends_documentation_fragment:
+    - aws
+'''
+    
+    - debug: msg='{{ specific_ec2_placement_groups | json_query(\'[?name=='my-cluster']\') }}'
+    
+        module       : AnsibleModule object
+    conn         : boto3 client connection object
+    vpc_id       : ID of the VPC we are operating on
+    '''
+    gateway_id = None
+    changed = False
+    
+    
+def run(ecr, params, verbosity):
+    # type: (EcsEcr, dict, int) -> Tuple[bool, dict]
+    result = {}
+    try:
+        name = params['name']
+        state = params['state']
+        policy_text = params['policy']
+        delete_policy = params['delete_policy']
+        registry_id = params['registry_id']
+        force_set_policy = params['force_set_policy']
+    
+    
+def stream_action(client, stream_name, shard_count=1, action='create',
+                  timeout=300, check_mode=False):
+    '''Create or Delete an Amazon Kinesis Stream.
+    Args:
+        client (botocore.client.EC2): Boto3 client.
+        stream_name (str): The name of the kinesis stream.
