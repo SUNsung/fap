2d400a9637e16cc603f06084f45ac91c88f1f73e
SUNsung <weatherius@gmail.com>
2018-11-06 09:07:34 +0200

    Rewrite [21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb] File in: 2018-11-06 09:07:33

--- ruby/21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb
+++ ruby/21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb
@@ -1,6 +1,18 @@
 
         
-          it 'transfers control to the innermost catch block waiting for the same sympol' do
+            it 'ignores SHELL env var and always uses `sh`' do
+      ENV['SHELL'] = '/bin/fakeshell'
+      lambda { @object.system('echo $0') }.should output_to_fd('sh\n')
+    end
+  end
+    
+      it 'sets the tainted bit' do
+    o = Object.new
+    o.taint
+    o.tainted?.should == true
+  end
+    
+      it 'transfers control to the innermost catch block waiting for the same sympol' do
     one = two = three = 0
     catch :duplicate do
       catch :duplicate do
@@ -17,70 +29,25 @@
     [one, two, three].should == [1, 2, 3]
   end
     
-          # rubocop:disable Security/MarshalLoad
-      def add_role(role, hosts, options={})
-        options_deepcopy = Marshal.dump(options.merge(roles: role))
-        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
-      end
-      # rubocop:enable Security/MarshalLoad
+    script_binding = binding
     
-      include_examples 'multiline literal brace layout trailing comma' do
-    let(:open) { '[' }
-    let(:close) { ']' }
-  end
-end
-
+        if GitHub.api_credentials_type == :none
+      puts <<~EOS
+        You can create a new personal access token:
+          #{GitHub::ALL_SCOPES_URL}
+        #{Utils::Shell.set_variable_in_profile('HOMEBREW_GITHUB_API_TOKEN', 'your_token_here')}
     
-      include_examples 'multiline literal brace layout' do
-    let(:open) { '{' }
-    let(:close) { '}' }
-    let(:a) { 'a: 1' }
-    let(:b) { 'b: 2' }
-    let(:multi_prefix) { 'b: ' }
-    let(:multi) do
-      <<-RUBY.strip_indent.chomp
-        [
-        1
-        ]
+        it 'with universal' do
+      expect_offense(<<~RUBY)
+        class Foo < Formula
+          url 'https://example.com/foo-1.0.tgz'
+          option :universal
+          ^^^^^^^^^^^^^^^^^ macOS has been 64-bit only since 10.6 so universal options are deprecated.
+        end
       RUBY
     end
-  end
     
-          # Calls the given block for each `value` node in the `hash` literal.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_value
-        return pairs.map(&:value).to_enum unless block_given?
-    
-          def current_ability
-        Spree::Ability.new(current_api_user)
-      end
-    
-            def update
-          @image = scope.images.accessible_by(current_ability, :update).find(params[:id])
-          if @image.update_attributes(image_params)
-            respond_with(@image, default_template: :show)
-          else
-            invalid_resource!(@image)
-          end
-        end
-    
-            def show
-          @inventory_unit = inventory_unit
-          respond_with(@inventory_unit)
-        end
-    
-            def update
-          @shipment = Spree::Shipment.accessible_by(current_ability, :update).readonly(false).find_by!(number: params[:id])
-          @shipment.update_attributes_and_order(shipment_params)
-    
-              @stock_item = scope.new(stock_item_params)
-          if @stock_item.save
-            @stock_item.adjust_count_on_hand(count_on_hand)
-            respond_with(@stock_item, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_item)
-          end
-        end
+        def pos=(i)
+      @s.pos = str_to_byte_pos i
+      i
+    end
