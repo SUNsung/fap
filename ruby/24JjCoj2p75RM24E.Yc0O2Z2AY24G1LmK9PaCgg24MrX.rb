74dfc199525d590ce07e8239b06351034c65b1ce
SUNsung <weatherius@gmail.com>
2018-11-06 11:34:58 +0200

    Rewrite [24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb] File in: 2018-11-06 11:34:57

--- ruby/24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb
+++ ruby/24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb
@@ -1,151 +1,102 @@
 
         
-            def path
-      @path ||= url.path
-    end
+        @@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset='utf-8' />
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+  </head>
+  <body><%= yield %></body>
+</html>
     
-              node['data-language'] = 'typescript' if node['path'].try(:ends_with?, '.ts')
-          node['data-language'] = 'html' if node['path'].try(:ends_with?, '.html')
-          node['data-language'] = 'css' if node['path'].try(:ends_with?, '.css')
-          node['data-language'] = 'js' if node['path'].try(:ends_with?, '.js')
-          node['data-language'] = 'json' if node['path'].try(:ends_with?, '.json')
-          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip if node['language']
-          node['data-language'] ||= 'typescript' if node.content.start_with?('@')
-    
-          def run
-        odisabled '`brew cask --version`', '`brew --version`'
-      end
-    
-        it 'with misc deprecated options' do
-      expect_offense(<<~RUBY)
-        class Foo < Formula
-          url 'https://example.com/foo-1.0.tgz'
-          option 'without-check'
-          ^^^^^^^^^^^^^^^^^^^^^^ Use '--without-test' instead of '--without-check'. Migrate '--without-check' with `deprecated_option`.
-        end
-      RUBY
-    end
-  end
-end
-    
-          # bootstrap requires minimum precision of 8, see https://github.com/twbs/bootstrap-sass/issues/409
-      ::Sass::Script::Number.precision = [8, ::Sass::Script::Number.precision].max
-    end
-    
-        # next matching brace for brace at from
-    def close_brace_pos(css, from, depth = 0)
-      s = CharStringScanner.new(css[from..-1])
-      while (b = s.scan_next(BRACE_RE))
-        depth += (b == '}' ? -1 : +1)
-        break if depth.zero?
-      end
-      raise 'match not found for {' unless depth.zero?
-      from + s.pos - 1
-    end
-    
-      # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-    
-      config.active_support.deprecation = :stderr
-end
-
-    
-      def up_down(change)
-    change.up do
-      Mention.update_all(mentions_container_type: 'Post')
-      change_column :mentions, :mentions_container_type, :string, null: false
-      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
-    end
-    
-      def login_page
-    path_to 'the new user session page'
-  end
-    
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
-    end
-    
-          @conv2 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
-             .increase_unread(alice)
-    
-    describe StatusMessagesController, :type => :controller do
-  describe '#bookmarklet' do
-    before do
-      sign_in bob, scope: :user
-    end
-    
-        it 'marks a notification as unread if it is told to' do
-      note = FactoryGirl.create(:notification)
-      expect(Notification).to receive(:where).and_return([note])
-      expect(note).to receive(:set_read_state).with(false)
-      get :update, params: {id: note.id, set_unread: 'true'}, format: :json
-    end
-    
-      class IncludeCodeTag < Liquid::Tag
-    def initialize(tag_name, markup, tokens)
-      @title = nil
-      @file = nil
-      if markup.strip =~ /\s*lang:(\S+)/i
-        @filetype = $1
-        markup = markup.strip.sub(/lang:\S+/i,'')
-      end
-      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
-        @title = $1 || nil
-        @file = $3
-      end
-      super
-    end
-    
-      # Returns a title cased string based on John Gruber's title case http://daringfireball.net/2008/08/title_case_update
-  def titlecase(input)
-    input.titlecase
-  end
-    
-    module RuboCop
-  module AST
-    # A node extension for `case` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `case` nodes within RuboCop.
-    class CaseNode < Node
-      include ConditionalNode
-    
-          # Returns the collection the `for` loop is iterating over.
-      #
-      # @return [Node] The collection the `for` loop is iterating over
-      def collection
-        node_parts[1]
-      end
-    
-          # Custom destructuring method. This is used to normalize the branches
-      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
-      #
-      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
-      def node_parts
-        [self, self]
-      end
+        it 'Returns nil when Referer header is invalid' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
+      expect(subject.referrer(env)).to be_nil
     end
   end
 end
 
     
-              delta(first.loc.operator, second.loc.operator)
+          def line_class(line)
+        if line =~ /^@@/
+          'gc'
+        elsif line =~ /^\+/
+          'gi'
+        elsif line =~ /^\-/
+          'gd'
+        else
+          ''
         end
-    
-          # Whether the last argument of the node is a block pass,
-      # i.e. `&block`.
-      #
-      # @return [Boolean] whether the last argument of the node is a block pass
-      def block_argument?
-        arguments? &&
-          (last_argument.block_pass_type? || last_argument.blockarg_type?)
       end
-    end
+    
+          def title
+        '#{@page.title}'
+      end
+    
+    module Precious
+  module Views
+    class Layout < Mustache
+      include Rack::Utils
+      alias_method :h, :escape_html
+    
+        get '/compare/A/fc66539528eb96f21b2bbdbf557788fe8a1196ac..b26b791cb7917c4f37dd9cb4d1e0efb24ac4d26f'
+    
+      test 'create sets the correct path for a relative path subdirectory with the page file directory set' do
+    Precious::App.set(:wiki_options, { :page_file_dir => 'foo' })
+    dir  = 'bardir'
+    name = '#{dir}/baz'
+    get '/create/foo/#{name}'
+    assert_match(/\/#{dir}/, last_response.body)
+    assert_no_match(/[^\/]#{dir}/, last_response.body)
+    # reset page_file_dir
+    Precious::App.set(:wiki_options, { :page_file_dir => nil })
   end
+    
+      test 'h1 title can be disabled' do
+    title = 'H1'
+    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
+    page = @wiki.page(title)
+    
+    
+    
+    require 'rake/testtask'
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'lib' << 'test' << '.'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
 end
+    
+    if options[:irb]
+  require 'irb'
+  # http://jameskilton.com/2009/04/02/embedding-irb-into-your-ruby-application/
+  module IRB # :nodoc:
+    def self.start_session(binding)
+      unless @__initialized
+        args = ARGV
+        ARGV.replace(ARGV.dup)
+        IRB.setup(nil)
+        ARGV.replace(args)
+        @__initialized = true
+      end
+    
+      class Error < StandardError;
+  end
+    
+    # Quiet some warnings we see when running in warning mode:
+# RUBYOPT=-w bundle exec sidekiq
+$TESTING = false
+    
+        execute 'ensure-sidekiq-is-setup-with-monit' do 
+      command %Q{ 
+        monit reload 
+      } 
+    end
+    
+        def self.with_context(msg)
+      Thread.current[:sidekiq_context] ||= []
+      Thread.current[:sidekiq_context] << msg
+      yield
+    ensure
+      Thread.current[:sidekiq_context].pop
+    end
