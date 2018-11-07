a80c6e858001fb5cd305ef8a58406789160e8c0e
SUNsung <weatherius@gmail.com>
2018-11-07 10:33:49 +0200

    Rewrite [16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb] File in: 2018-11-07 10:33:48

--- ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
+++ ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
@@ -1,147 +1,17 @@
 
         
-        module Gitlab
-  module BackgroundMigration
-    class MigrateStageStatus
-      STATUSES = { created: 0, pending: 1, running: 2, success: 3,
-                   failed: 4, canceled: 5, skipped: 6, manual: 7 }.freeze
+          # set version
+  content.sub! /(s\.version.*=\s+).*/, '\\1\'#{Rack::Protection::VERSION}\''
     
-          class MergeRequest < ActiveRecord::Base
-        self.table_name = 'merge_requests'
+            begin
+          token = decode_token(token)
+        rescue ArgumentError # encoded_masked_token is invalid Base64
+          return false
+        end
     
-      private
-    
-    module Admin
-  class EmailDomainBlocksController < BaseController
-    before_action :set_email_domain_block, only: [:show, :destroy]
-    
-        def filtered_instances
-      InstanceFilter.new(filter_params).results
-    end
-    
-      task :build do
-    title 'Building the gem'
-  end
-    
-        # Ensure that the master spec repo exists
-    #
-    # @return [void]
-    #
-    def ensure_master_spec_repo_exists!
-      unless config.sources_manager.master_repo_functional?
-        Setup.new(CLAide::ARGV.new([])).run
+          def has_vector?(request, headers)
+        return false if request.xhr?
+        return false if options[:allow_if] && options[:allow_if].call(request.env)
+        return false unless headers['Content-Type'].to_s.split(';', 2).first =~ /^\s*application\/json\s*$/
+        origin(request.env).nil? and referrer(request.env) != request.host
       end
-    end
-    
-          def plugins_string
-        UI::ErrorReport.plugins_string
-      end
-    
-      def safely_remove_file(_path)
-    run_vagrant_command('rm #{test_file}')
-  rescue
-    VagrantHelpers::VagrantSSHCommandError
-  end
-end
-    
-        def configure_scm
-      Capistrano::Configuration::SCMResolver.new.resolve
-    end
-    
-            if echo?
-          $stdin.gets
-        else
-          $stdin.noecho(&:gets).tap { $stdout.print '\n' }
-        end
-      rescue Errno::EIO
-        # when stdio gets closed
-        return
-      end
-    
-          def call(env)
-        status, headers, body = super
-        response = Rack::Response.new(body, status, headers)
-        request = Rack::Request.new(env)
-        remove_bad_cookies(request, response)
-        response.finish
-      end
-    
-      describe '#referrer' do
-    it 'Reads referrer from Referer header' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
-      expect(subject.referrer(env)).to eq('bar.com')
-    end
-    
-            if Utils::HttpClient.remote_file_exist?(uri)
-          PluginManager.ui.debug('Found package at: #{uri}')
-          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
-        else
-          PluginManager.ui.debug('Package not found at: #{uri}')
-          return nil
-        end
-      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
-        # This probably means there is a firewall in place of the proxy is not correctly configured.
-        # So lets skip this strategy but log a meaningful errors.
-        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
-    
-      private
-  def update_all?
-    plugins_arg.size == 0
-  end
-    
-          it 'should not support $ in environment variable name' do
-        expect(subject.oneString).to(be == '${f$$:val}')
-      end
-    end
-  end
-end
-
-    
-          it 'display a list of installed plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
-    
-    task default: :test
-    
-              if @address.update_attributes(address_params)
-            respond_with(@address, default_template: :show)
-          else
-            invalid_resource!(@address)
-          end
-        end
-    
-              if Spree::Cart::Update.call(order: @order, params: line_items_attributes).success?
-            @line_item.reload
-            respond_with(@line_item, default_template: :show)
-          else
-            invalid_resource!(@line_item)
-          end
-        end
-    
-            def update
-          authorize! params[:action], @payment
-          if !@payment.editable?
-            render 'update_forbidden', status: 403
-          elsif @payment.update_attributes(payment_params)
-            respond_with(@payment, default_template: :show)
-          else
-            invalid_resource!(@payment)
-          end
-        end
-    
-            private
-    
-              state = @states.last
-          respond_with(@states) if stale?(state)
-        end
-    
-            def create
-          authorize! :create, StockMovement
-          @stock_movement = scope.new(stock_movement_params)
-          if @stock_movement.save
-            respond_with(@stock_movement, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_movement)
-          end
-        end
