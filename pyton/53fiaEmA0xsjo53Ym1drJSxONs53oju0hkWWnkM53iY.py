8fd25f0d066a6d1ed4aec4150fd6e4e5545b426f
SUNsung <weatherius@gmail.com>
2018-11-07 21:43:15 +0200

    Rewrite [53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py] File in: 2018-11-07 21:43:15

--- pyton/53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py
+++ pyton/53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py
@@ -1,97 +1,49 @@
 
         
-        # tree.RewriteRuleElementStream
-# tree.RewriteRuleSubtreeStream
-# tree.RewriteRuleTokenStream
-# CharStream
-# DFA
-# TokenSource
+        signals_available = False
+try:
+    from blinker import Namespace
+    signals_available = True
+except ImportError:
+    class Namespace(object):
+        def signal(self, name, doc=None):
+            return _FakeSignal(name, doc)
     
-                    # EOF Transition to accept state?
-                if c == EOF and self.eof[s] >= 0:
-                    #print 'EOF Transition to accept state %d' \
-                    #  % self.accept[self.eof[s]]
-                    return self.accept[self.eof[s]]
+        def get_source(self, environment, template):
+        if self.app.config['EXPLAIN_TEMPLATE_LOADING']:
+            return self._get_source_explained(environment, template)
+        return self._get_source_fast(environment, template)
     
-    Instead of importing this module directly, import os and refer to
-this module as os.path.  The 'os.path' name is an alias for this
-module on Posix systems; on other systems (e.g. Mac, Windows),
-os.path provides the same operations in a manner specific to that
-platform, and is an alias to another module (e.g. macpath, ntpath).
+        def __init__(self, *a, **kw):
+        super(QPSSpider, self).__init__(*a, **kw)
+        if self.qps is not None:
+            self.qps = float(self.qps)
+            self.download_delay = 1 / self.qps
+        elif self.download_delay is not None:
+            self.download_delay = float(self.download_delay)
     
-        def test_default(self):
-        size = test.support.calcvobjsize
-        self.assertEqual(sys.getsizeof(True), size('') + self.longdigit)
-        self.assertEqual(sys.getsizeof(True, -1), size('') + self.longdigit)
+    __all__ = ['__version__', 'version_info', 'twisted_version',
+           'Spider', 'Request', 'FormRequest', 'Selector', 'Item', 'Field']
     
-    # In a real program you'd get the filename from the arguments.
-with open('outgoing.msg', 'rb') as fp:
-    msg = BytesParser(policy=policy.default).parse(fp)
+            if opts.logfile:
+            self.settings.set('LOG_ENABLED', True, priority='cmdline')
+            self.settings.set('LOG_FILE', opts.logfile, priority='cmdline')
     
-        counter = 1
-    for part in msg.walk():
-        # multipart/* are just containers
-        if part.get_content_maintype() == 'multipart':
-            continue
-        # Applications should really sanitize the given filename so that an
-        # email message can't be used to overwrite important files
-        filename = part.get_filename()
-        if not filename:
-            ext = mimetypes.guess_extension(part.get_content_type())
-            if not ext:
-                # Use a generic bag-of-bits extension
-                ext = '.bin'
-            filename = 'part-%03d%s' % (counter, ext)
-        counter += 1
-        with open(os.path.join(args.directory, filename), 'wb') as fp:
-            fp.write(part.get_payload(decode=True))
+        requires_project = False
     
-        function_parameters = zip(
-        images_to_check,
-        itertools.repeat(model),
-    )
+        requires_project = False
+    default_settings = {'LOG_ENABLED': False,
+                        'SPIDER_LOADER_WARN_ONLY': True}
     
-    # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file('obama.jpg')
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+            def getCertificateOptions(self):
+            # setting verify=True will require you to provide CAs
+            # to verify against; in other words: it's not that simple
     
-            print('I see someone named {}!'.format(name))
-
-    
-      # As a last resort, we search python in the PATH. We prefer Python 2 over 3
-  # for the sake of backwards compatibility with ycm_extra_conf.py files out
-  # there; few people wrote theirs to work on py3.
-  # So we check 'python2' before 'python' because on some distributions (Arch
-  # Linux for example), python refers to python3.
-  python_interpreter = utils.PathToFirstExistingExecutable( [ 'python2',
-                                                              'python',
-                                                              'python3' ] )
-  if python_interpreter:
-    return python_interpreter
-    
-      opts = { 'filter_diagnostics' : {
-    'java,c,cs' : { 'regex' : '.*taco.*' } } }
-    
-    
-@patch( 'ycm.vimsupport.GetVariableValue',
-        GetVariableValue_CompleteItemIs( 'Test' ) )
-@patch( 'ycm.vimsupport.ReplaceChunks' )
-def PostCompleteFixIt_ApplyFixIt_EmptyFixIt_test( replace_chunks, *args ):
-  completions = [
-    BuildCompletionFixIt( [ { 'chunks': [] } ] )
-  ]
-  with _SetUpCompleteDone( completions ) as request:
-    request._OnCompleteDone_FixIt()
-    replace_chunks.assert_called_once_with( [], silent = True )
-    
-    import os
-import sys
-from hamcrest import ( assert_that, contains, empty, equal_to, has_entries,
-                       is_in, is_not, matches_regexp )
-from mock import call, MagicMock, patch
-    
-      if parsed_args.coverage:
-    nosetests_args += [ '--with-coverage',
-                        '--cover-erase',
-                        '--cover-package=ycm',
-                        '--cover-html' ]
+    import logging
+from twisted.internet import defer
+import six
+from scrapy.exceptions import NotSupported, NotConfigured
+from scrapy.utils.httpobj import urlparse_cached
+from scrapy.utils.misc import load_object
+from scrapy.utils.python import without_none_values
+from scrapy import signals
