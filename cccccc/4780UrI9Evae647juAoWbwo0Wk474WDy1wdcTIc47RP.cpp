7e73b0b9c5039e4e5176bb4256f044edaf68c380
SUNsung <weatherius@gmail.com>
2018-11-07 03:26:51 +0200

    Rewrite [4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp] File in: 2018-11-07 03:26:50

--- cccccc/4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp
+++ cccccc/4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp
@@ -1,81 +1,290 @@
 
         
-        #ifndef TESSERACT_ARCH_INTSIMDMATRIX_H_
-#define TESSERACT_ARCH_INTSIMDMATRIX_H_
+        // Common functions and classes from std that caffe often uses.
+using std::fstream;
+using std::ios;
+using std::isnan;
+using std::isinf;
+using std::iterator;
+using std::make_pair;
+using std::map;
+using std::ostringstream;
+using std::pair;
+using std::set;
+using std::string;
+using std::stringstream;
+using std::vector;
+    
+      bool is_started() const;
+    
+    #include <algorithm>
+#include <string>
+#include <vector>
+    
+    /**
+ * @brief Takes at least two Blob%s and concatenates them along either the num
+ *        or channel dimension, outputting the result.
+ */
+template <typename Dtype>
+class ConcatLayer : public Layer<Dtype> {
+ public:
+  explicit ConcatLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
     
     
-    {}  // namespace tesseract.
-
-    
-    // Main entry point for Paragraph Detection Algorithm.
-//
-// Given a set of equally spaced textlines (described by row_infos),
-// Split them into paragraphs.  See http://goto/paragraphstalk
-//
-// Output:
-//   row_owners - one pointer for each row, to the paragraph it belongs to.
-//   paragraphs - this is the actual list of PARA objects.
-//   models - the list of paragraph models referenced by the PARA objects.
-//            caller is responsible for deleting the models.
-void DetectParagraphs(int debug_level,
-                      GenericVector<RowInfo> *row_infos,
-                      GenericVector<PARA *> *row_owners,
-                      PARA_LIST *paragraphs,
-                      GenericVector<ParagraphModel *> *models);
-    
-    
-    {  ScrollView* sv_window_;
+    {  bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnTensorDescriptor_t bottom_desc_;
+  cudnnTensorDescriptor_t top_desc_;
+  cudnnActivationDescriptor_t activ_desc_;
 };
+#endif
     
-      /// Threshold the rectangle, taking everything except the src_pix
-  /// from the class, using thresholds/hi_values to the output pix.
-  /// NOTE that num_channels is the size of the thresholds and hi_values
-  // arrays and also the bytes per pixel in src_pix.
-  void ThresholdRectToPix(Pix* src_pix, int num_channels,
-                          const int* thresholds, const int* hi_values,
-                          Pix** pix) const;
+    #else  // !defined(__AVX__)
+// Implementation for avx capable archs.
+#include <immintrin.h>
+#include <cstdint>
+#include 'dotproductavx.h'
     
-    	// Now set the registry keys
-	HKEY root = GetRootKey(opt);
-    
-            template<typename OnWriteSummaryFunc>
-        void WriteSummary(const ValuePtr& accumulatedLoss, const ValuePtr& accumulatedMetric,
-                          OnWriteSummaryFunc callback)
-        {
-            if (accumulatedLoss && m_samples.second > 0)
-            {
-                m_loss.second = accumulatedLoss->AsScalar<double>();
-            }
+    namespace tesseract {
     }
     
-    #pragma once
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
-#endif
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif // NOMINMAX
-#pragma comment(lib, 'Dbghelp.lib')
-#else
-#include <execinfo.h>
-#include <cxxabi.h>
-#endif
+    /** Return that there is no such object at a given level. */
+bool PageIterator::Empty(PageIteratorLevel level) const {
+  if (it_->block() == nullptr) return true;  // Already at the end!
+  if (it_->word() == nullptr && level != RIL_BLOCK) return true;  // image block
+  if (level == RIL_SYMBOL && blob_index_ >= word_length_)
+    return true;  // Zero length word, or already at the end of it.
+  return false;
+}
     
+    #include 'platform.h'
+#include 'publictypes.h'
     
-    {            // reshape Input(2)
-            Input(2)->SetDims(TensorShape(dimsA), false);
-            fprintf(stderr, '\n%ls %ls operation: For legacy compatibility, the sample layout of third input (%ls %ls operation) was patched to [%s] (from [%s])\n',
-                NodeName().c_str(), OperationName().c_str(), Input(2)->NodeName().c_str(), Input(2)->OperationName().c_str(), string(Input(2)->GetSampleLayout()).c_str(), dimsCstring.c_str());
-        }
+    ChannelCredentials::~ChannelCredentials() {}
     
-    template <class RNG>
-struct SeedData {
-  SeedData() {
-    Random::secureRandom(seedData.data(), seedData.size() * sizeof(uint32_t));
+    // A CallData class will be created for every grpc call within a channel. It is
+// used to store data and methods specific to that call. CensusClientCallData is
+// thread-compatible, however typically only 1 thread should be interacting with
+// a call at a time.
+class CensusClientCallData : public CallData {
+ public:
+  // Maximum size of trace context is sent on the wire.
+  static constexpr uint32_t kMaxTraceContextLen = 64;
+  // Maximum size of tags that are sent on the wire.
+  static constexpr uint32_t kMaxTagsLen = 2048;
+    }
+    
+    ViewDescriptor HourDescriptor() {
+  auto descriptor = ViewDescriptor();
+  SetAggregationWindow(AggregationWindow::Interval(absl::Hours(1)),
+                       &descriptor);
+  return descriptor;
+}
+    
+    // Build a Table file from the contents of *iter.  The generated file
+// will be named according to meta->number.  On success, the rest of
+// *meta will be filled with metadata about the generated table.
+// If no data is present in *iter, meta->file_size will be set to
+// zero, and no Table file will be produced.
+Status BuildTable(const std::string& dbname,
+                  Env* env,
+                  const Options& options,
+                  TableCache* table_cache,
+                  Iterator* iter,
+                  FileMetaData* meta);
+    
+      // Collect together all needed child iterators
+  std::vector<Iterator*> list;
+  list.push_back(mem_->NewIterator());
+  mem_->Ref();
+  if (imm_ != nullptr) {
+    list.push_back(imm_->NewIterator());
+    imm_->Ref();
   }
+  versions_->current()->AddIterators(options, &list);
+  Iterator* internal_iter =
+      NewMergingIterator(&internal_comparator_, &list[0], list.size());
+  versions_->current()->Ref();
+    
+    // Value types encoded as the last component of internal keys.
+// DO NOT CHANGE THESE ENUM VALUES: they are embedded in the on-disk
+// data structures.
+enum ValueType {
+  kTypeDeletion = 0x0,
+  kTypeValue = 0x1
+};
+// kValueTypeForSeek defines the ValueType that should be passed when
+// constructing a ParsedInternalKey object for seeking to a particular
+// sequence number (since we sort sequence numbers in decreasing order
+// and the value type is embedded as the low 8 bits in the sequence
+// number in internal keys, we need to use the highest-numbered
+// ValueType, not the lowest).
+static const ValueType kValueTypeForSeek = kTypeValue;
+    
+    #include 'db/dbformat.h'
+#include 'db/filename.h'
+#include 'db/log_reader.h'
+#include 'db/version_edit.h'
+#include 'db/write_batch_internal.h'
+#include 'leveldb/env.h'
+#include 'leveldb/iterator.h'
+#include 'leveldb/options.h'
+#include 'leveldb/status.h'
+#include 'leveldb/table.h'
+#include 'leveldb/write_batch.h'
+#include 'util/logging.h'
+    
+    
+    {}  // namespace leveldb
+    
+      fname = InfoLogFileName('foo');
+  ASSERT_EQ('foo/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(0, number);
+  ASSERT_EQ(kInfoLogFile, type);
+    
+      // Return an iterator that yields the contents of the memtable.
+  //
+  // The caller must ensure that the underlying MemTable remains live
+  // while the returned iterator is live.  The keys returned by this
+  // iterator are internal keys encoded by AppendInternalKey in the
+  // db/format.{h,cc} module.
+  Iterator* NewIterator();
+    
+    
+    {    if (counter > 0 && s.ok()) {
+      std::string orig = TableFileName(dbname_, t.meta.number);
+      s = env_->RenameFile(copy, orig);
+      if (s.ok()) {
+        Log(options_.info_log, 'Table #%llu: %d entries repaired',
+            (unsigned long long) t.meta.number, counter);
+        tables_.push_back(t);
+      }
+    }
+    if (!s.ok()) {
+      env_->DeleteFile(copy);
+    }
+  }
+    
+    bool AccelAmplitude::initWithAction(Action *action, float duration)
+{
+    if (ActionInterval::initWithDuration(duration))
+    {
+        _rate = 1.0f;
+        _other = (ActionInterval*)(action);
+        action->retain();
+    }
     }
     
-    #include <folly/io/Cursor.h>
-#include <folly/io/IOBuf.h>
-#include <folly/lang/Bits.h>
+    Liquid* Liquid::clone() const
+{
+    // no copy constructor
+    auto a = new (std::nothrow) Liquid();
+    a->initWithDuration(_duration, _gridSize, _waves, _amplitude);
+    a->autorelease();
+    return a;
+}
+    
+    void Hide::update(float time)
+{
+    ActionInstant::update(time);
+    _target->setVisible(false);
+}
+    
+    void ActionTween::startWithTarget(Node *target)
+{
+    CCASSERT(dynamic_cast<ActionTweenDelegate*>(target), 'target must implement ActionTweenDelegate');
+    ActionInterval::startWithTarget(target);
+    _delta = _to - _from;
+}
+    
+        /** Deletes a Animation from the cache.
+     *
+     * @param name The name of animation.
+     */
+    void removeAnimation(const std::string& name);
+    /** @deprecated. Use removeAnimation() instead
+     * @js NA
+     * @lua NA
+     */
+    CC_DEPRECATED_ATTRIBUTE void removeAnimationByName(const std::string& name){ removeAnimation(name);}
+    
+    void DHTReplaceNodeTask::onReceived(const DHTPingReplyMessage* message)
+{
+  A2_LOG_INFO(fmt('ReplaceNode: Ping reply received from %s.',
+                  message->getRemoteNode()->toString().c_str()));
+  setFinished(true);
+}
+    
+    bool DHTRoutingTable::addNode(const std::shared_ptr<DHTNode>& node, bool good)
+{
+  A2_LOG_DEBUG(fmt('Trying to add node:%s', node->toString().c_str()));
+  if (*localNode_ == *node) {
+    A2_LOG_DEBUG('Adding node with the same ID with localnode is not allowed.');
+    return false;
+  }
+  auto treeNode = dht::findTreeNodeFor(root_.get(), node->getID());
+  while (1) {
+    const std::shared_ptr<DHTBucket>& bucket = treeNode->getBucket();
+    if (bucket->addNode(node)) {
+      A2_LOG_DEBUG('Added DHTNode.');
+      return true;
+    }
+    else if (bucket->splitAllowed()) {
+      A2_LOG_DEBUG(fmt('Splitting bucket. Range:%s-%s',
+                       util::toHex(bucket->getMinID(), DHT_ID_LENGTH).c_str(),
+                       util::toHex(bucket->getMaxID(), DHT_ID_LENGTH).c_str()));
+      treeNode->split();
+      ++numBucket_;
+      if (treeNode->getLeft()->isInRange(node->getID())) {
+        treeNode = treeNode->getLeft();
+      }
+      else {
+        treeNode = treeNode->getRight();
+      }
+    }
+    else {
+      if (good) {
+        bucket->cacheNode(node);
+        A2_LOG_DEBUG(fmt('Cached node=%s', node->toString().c_str()));
+      }
+      return false;
+    }
+  }
+  return false;
+}
+    
+      std::shared_ptr<DHTNode> localNode_;
+    
+      char zero[18];
+  memset(zero, 0, sizeof(zero));
+    
+      virtual std::shared_ptr<DHTTask>
+  createPingTask(const std::shared_ptr<DHTNode>& remoteNode,
+                 int numRetry = 0) = 0;
+    
+    const std::string DHTUnknownMessage::E('e');
+    
+    public:
+  // _remoteNode is always null
+  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                    const unsigned char* data, size_t length,
+                    const std::string& ipaddr, uint16_t port);
+    
+    void DNSCache::remove(const std::string& hostname, uint16_t port)
+{
+  auto target = std::make_shared<CacheEntry>(hostname, port);
+  entries_.erase(target);
+}
