6f45c7351102631876fe0c7eb298474eb6e9bc07
SUNsung <weatherius@gmail.com>
2018-11-08 19:50:00 +0200

    Rewrite [58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp] File in: 2018-11-08 19:49:59

--- cccccc/58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp
+++ cccccc/58I6pfbcMDyu658YJBPnmI4zqw58CUqVCUlbnLU58Jek.cpp
@@ -1,141 +1,288 @@
 
         
-        using extensions::GlobalShortcutListener;
+        IMPL_ADDWEIGHTED(u8)
+IMPL_ADDWEIGHTED(s8)
+IMPL_ADDWEIGHTED(u16)
+IMPL_ADDWEIGHTED(s16)
+IMPL_ADDWEIGHTED(u32)
+IMPL_ADDWEIGHTED(s32)
+IMPL_ADDWEIGHTED(f32)
     
-    namespace api {
+        if (src0Stride == src1Stride && src0Stride == dstStride &&
+        src0Stride == (ptrdiff_t)(size.width * sizeof(type)))
+    {
+        size.width *= size.height;
+        size.height = 1;
     }
     
-    #endif  // ATOM_BROWSER_API_ATOM_API_NET_H_
-
+    #include <cstdlib>
+#include <iostream>
     
-    bool PowerMonitor::ShouldShutdown() {
-  return !Emit('shutdown');
+    /*!
+ *  Aligns pointer by the certain number of bytes
+ *
+ *  This small inline function aligns the pointer by the certain number of bytes by shifting
+ *  it forward by 0 or a positive offset.
+ */
+template<typename T> inline T* alignPtr(T* ptr, size_t n=sizeof(T))
+{
+    return (T*)(((size_t)ptr + n-1) & -n);
 }
     
     
-    {}  // namespace atom
-    
-      // this.emit(name, event, args...);
-  template <typename... Args>
-  bool EmitCustomEvent(const base::StringPiece& name,
-                       v8::Local<v8::Object> event,
-                       const Args&... args) {
-    return EmitWithEvent(
-        name, internal::CreateCustomEvent(isolate(), GetWrapper(), event),
-        args...);
-  }
-    
-    
-    {}  // namespace tesseract
-
-    
-      // Rounds the size up to a multiple of the input register size (in int8_t).
-  int RoundInputs(int size) const {
-    return Roundup(size, num_inputs_per_register_);
-  }
-  // Rounds the size up to a multiple of the output register size (in int32_t).
-  int RoundOutputs(int size) const {
-    return Roundup(size, num_outputs_per_register_);
-  }
-    
-    namespace tesseract {
-    }
-    
-    template <typename T> class GenericVector;
-    
-    // A rather hackish helper structure which can take any kind of parameter input
-// (defined by ParamType) and do a couple of common operations on them, like
-// comparisond or getting its value. It is used in the context of the
-// ParamsEditor as a bridge from the internal tesseract parameters to the
-// ones displayed by the ScrollView server.
-class ParamContent : public ELIST_LINK {
- public:
-  // Compare two VC objects by their name.
-  static int Compare(const void* v1, const void* v2);
-    }
-    
-    ActionCamera* ActionCamera::clone() const
-{
-    auto action = new (std::nothrow) ActionCamera();
-    if (action)
     {
-        action->autorelease();
-        return action;
-    }
-    
-    delete action;
-    return nullptr;
-}
-    
-    PointArray* PointArray::create(ssize_t capacity)
-{
-    PointArray* pointArray = new (std::nothrow) PointArray();
-    if (pointArray && pointArray->initWithCapacity(capacity))
-    {
-        pointArray->autorelease();
-        return pointArray;
-    }
-    }
-    
-        //
-    // Override
-    //
-    virtual RotateBy* clone() const override;
-    virtual RotateBy* reverse() const override;
-    virtual void startWithTarget(Node *target) override;
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    RotateBy();
-    virtual ~RotateBy() {}
-    
-        if (element)
-    {
-        if (element->actions != nullptr)
-        {
-            auto limit = element->actions->num;
-            for (int i = 0; i < limit; ++i)
-            {
-                Action *action = static_cast<Action*>(element->actions->arr[i]);
-    }
-    }
-    }
+    {         vst1q_s32(_dst + i, vline1_s32);
+         vst1q_s32(_dst + i + 4, vline2_s32);
+         vst1q_s32(_dst + i + 8, vline3_s32);
+         vst1q_s32(_dst + i + 12, vline4_s32);
+     }
+})
+#endif
     
     
     {
     {
-    {            p.z = (r * ( 1 - cosBeta ) * cosTheta);// '100' didn't work for
-            p.x = p.z * sinf(rotateByYAxis) + p.x * cosf(rotateByYAxis);
-            p.z = p.z * cosf(rotateByYAxis) - p.x * sinf(rotateByYAxis);
-            p.z/=7;
-            //    Stop z coord from dropping beneath underlying page in a transition
-            // issue #751
-            if( p.z < 0.5f )
-            {
-                p.z = 0.5f;
-            }
-            
-            // Set new coords
-            p.x += getGridRect().origin.x;
-            setVertex(Vec2(i, j), p);
-            
+    {             vec128  vs = internal::vld1q( src + i);
+             vec128 vr1 = internal::vld1q(rng1 + i);
+             vec128 vr2 = internal::vld1q(rng2 + i);
+            uvec128 vd1 = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
+                     vs = internal::vld1q( src + i + 16/sizeof(T));
+                    vr1 = internal::vld1q(rng1 + i + 16/sizeof(T));
+                    vr2 = internal::vld1q(rng2 + i + 16/sizeof(T));
+            uvec128 vd2 = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
+            vnst(dst + i, vd1, vd2);
         }
+        vtail<T, sizeof(T)>::inRange(src, rng1, rng2, dst, i, size.width);
+        for( ; i < size.width; i++ )
+            dst[i] = (u8)(-(rng1[i] <= src[i] && src[i] <= rng2[i]));
     }
 }
     
-        /**
-    @brief Hide the tile at specified position.
-    @param pos The position index of the tile should be hide.
-    */
-    void turnOffTile(const Vec2& pos);
     
-        static BOOST_FORCEINLINE storage_type fetch_add(storage_type volatile& storage, storage_type v, memory_order) BOOST_NOEXCEPT
-    {
-        return static_cast< storage_type >(BOOST_ATOMIC_INTERLOCKED_EXCHANGE_ADD16(&storage, v));
+    { private:
+  LogType& log_;
+};
+    
+      // Gets the current text color.
+  CONSOLE_SCREEN_BUFFER_INFO buffer_info;
+  GetConsoleScreenBufferInfo(stdout_handle, &buffer_info);
+  const WORD old_color_attrs = buffer_info.wAttributes;
+    
+    namespace benchmark {
+enum LogColor {
+  COLOR_DEFAULT,
+  COLOR_RED,
+  COLOR_GREEN,
+  COLOR_YELLOW,
+  COLOR_BLUE,
+  COLOR_MAGENTA,
+  COLOR_CYAN,
+  COLOR_WHITE
+};
     }
     
-    namespace mars_boost {} namespace boost = mars_boost; namespace mars_boost {
+    // Macro for referencing flags.
+#define FLAG(name) FLAGS_##name
+    
+    
+    {    // Compute all possible fitting curves and stick to the best one
+    for (const auto& fit : fit_curves) {
+      LeastSq current_fit = MinimalLeastSq(n, time, FittingCurve(fit));
+      if (current_fit.rms < best_fit.rms) {
+        best_fit = current_fit;
+        best_fit.complexity = fit;
+      }
     }
+  } else {
+    best_fit = MinimalLeastSq(n, time, FittingCurve(complexity));
+    best_fit.complexity = complexity;
+  }
+    
+    namespace benchmark {
+// NOTE: only i386 and x86_64 have been well tested.
+// PPC, sparc, alpha, and ia64 are based on
+//    http://peter.kuscsik.com/wordpress/?p=14
+// with modifications by m3b.  See also
+//    https://setisvn.ssl.berkeley.edu/svn/lib/fftw-3.0.1/kernel/cycle.h
+namespace cycleclock {
+// This should return the number of cycles since power-on.  Thread-safe.
+inline BENCHMARK_ALWAYS_INLINE int64_t Now() {
+#if defined(BENCHMARK_OS_MACOSX)
+  // this goes at the top because we need ALL Macs, regardless of
+  // architecture, to return the number of 'mach time units' that
+  // have passed since startup.  See sysinfo.cc where
+  // InitializeSystemInfo() sets the supposed cpu clock frequency of
+  // macs to the number of mach time units per second, not actual
+  // CPU clock frequency (which can change in the face of CPU
+  // frequency scaling).  Also note that when the Mac sleeps, this
+  // counter pauses; it does not continue counting, nor does it
+  // reset to zero.
+  return mach_absolute_time();
+#elif defined(BENCHMARK_OS_EMSCRIPTEN)
+  // this goes above x86-specific code because old versions of Emscripten
+  // define __x86_64__, although they have nothing to do with it.
+  return static_cast<int64_t>(emscripten_get_now() * 1e+6);
+#elif defined(__i386__)
+  int64_t ret;
+  __asm__ volatile('rdtsc' : '=A'(ret));
+  return ret;
+#elif defined(__x86_64__) || defined(__amd64__)
+  uint64_t low, high;
+  __asm__ volatile('rdtsc' : '=a'(low), '=d'(high));
+  return (high << 32) | low;
+#elif defined(__powerpc__) || defined(__ppc__)
+  // This returns a time-base, which is not always precisely a cycle-count.
+  int64_t tbl, tbu0, tbu1;
+  asm('mftbu %0' : '=r'(tbu0));
+  asm('mftb  %0' : '=r'(tbl));
+  asm('mftbu %0' : '=r'(tbu1));
+  tbl &= -static_cast<int64_t>(tbu0 == tbu1);
+  // high 32 bits in tbu1; low 32 bits in tbl  (tbu0 is garbage)
+  return (tbu1 << 32) | tbl;
+#elif defined(__sparc__)
+  int64_t tick;
+  asm('.byte 0x83, 0x41, 0x00, 0x00');
+  asm('mov   %%g1, %0' : '=r'(tick));
+  return tick;
+#elif defined(__ia64__)
+  int64_t itc;
+  asm('mov %0 = ar.itc' : '=r'(itc));
+  return itc;
+#elif defined(COMPILER_MSVC) && defined(_M_IX86)
+  // Older MSVC compilers (like 7.x) don't seem to support the
+  // __rdtsc intrinsic properly, so I prefer to use _asm instead
+  // when I know it will work.  Otherwise, I'll use __rdtsc and hope
+  // the code is being compiled with a non-ancient compiler.
+  _asm rdtsc
+#elif defined(COMPILER_MSVC)
+  return __rdtsc();
+#elif defined(BENCHMARK_OS_NACL)
+  // Native Client validator on x86/x86-64 allows RDTSC instructions,
+  // and this case is handled above. Native Client validator on ARM
+  // rejects MRC instructions (used in the ARM-specific sequence below),
+  // so we handle it here. Portable Native Client compiles to
+  // architecture-agnostic bytecode, which doesn't provide any
+  // cycle counter access mnemonics.
+    }
+    }
+    }
+    
+    namespace guetzli {
+    }
+    
+    // kDCTMatrix[8*u+x] = 0.5*alpha(u)*cos((2*x+1)*u*M_PI/16),
+// where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
+static const double kDCTMatrix[64] = {
+  0.3535533906,  0.3535533906,  0.3535533906,  0.3535533906,
+  0.3535533906,  0.3535533906,  0.3535533906,  0.3535533906,
+  0.4903926402,  0.4157348062,  0.2777851165,  0.0975451610,
+ -0.0975451610, -0.2777851165, -0.4157348062, -0.4903926402,
+  0.4619397663,  0.1913417162, -0.1913417162, -0.4619397663,
+ -0.4619397663, -0.1913417162,  0.1913417162,  0.4619397663,
+  0.4157348062, -0.0975451610, -0.4903926402, -0.2777851165,
+  0.2777851165,  0.4903926402,  0.0975451610, -0.4157348062,
+  0.3535533906, -0.3535533906, -0.3535533906,  0.3535533906,
+  0.3535533906, -0.3535533906, -0.3535533906,  0.3535533906,
+  0.2777851165, -0.4903926402,  0.0975451610,  0.4157348062,
+ -0.4157348062, -0.0975451610,  0.4903926402, -0.2777851165,
+  0.1913417162, -0.4619397663,  0.4619397663, -0.1913417162,
+ -0.1913417162,  0.4619397663, -0.4619397663,  0.1913417162,
+  0.0975451610, -0.2777851165,  0.4157348062, -0.4903926402,
+  0.4903926402, -0.4157348062,  0.2777851165, -0.0975451610,
+};
+    
+    inline int Log2FloorNonZero(uint32_t n) {
+#ifdef __GNUC__
+  return 31 ^ __builtin_clz(n);
+#else
+  unsigned int result = 0;
+  while (n >>= 1) result++;
+  return result;
+#endif
+}
+    
+    #include <cmath>
+    
+      void JumpToByteBoundary() {
+    while (put_bits <= 56) {
+      int c = (put_buffer >> 56) & 0xff;
+      EmitByte(c);
+      put_buffer <<= 8;
+      put_bits += 8;
+    }
+    if (put_bits < 64) {
+      int padmask = 0xff >> (64 - put_bits);
+      int c = ((put_buffer >> 56) & ~padmask) | padmask;
+      EmitByte(c);
+    }
+    put_buffer = 0;
+    put_bits = 64;
+  }
+    
+    void InitJPEGDataForYUV444(int w, int h, JPEGData* jpg) {
+  jpg->width = w;
+  jpg->height = h;
+  jpg->max_h_samp_factor = 1;
+  jpg->max_v_samp_factor = 1;
+  jpg->MCU_rows = (h + 7) >> 3;
+  jpg->MCU_cols = (w + 7) >> 3;
+  jpg->quant.resize(3);
+  jpg->components.resize(3);
+  for (int i = 0; i < 3; ++i) {
+    JPEGComponent* c = &jpg->components[i];
+    c->id = i;
+    c->h_samp_factor = 1;
+    c->v_samp_factor = 1;
+    c->quant_idx = i;
+    c->width_in_blocks = jpg->MCU_cols;
+    c->height_in_blocks = jpg->MCU_rows;
+    c->num_blocks = c->width_in_blocks * c->height_in_blocks;
+    c->coeffs.resize(c->num_blocks * kDCTBlockSize);
+  }
+}
+    
+    // Saves the APP marker segment as a string to *jpg.
+bool ProcessAPP(const uint8_t* data, const size_t len, size_t* pos,
+                JPEGData* jpg) {
+  VERIFY_LEN(2);
+  size_t marker_len = ReadUint16(data, pos);
+  VERIFY_INPUT(marker_len, 2, 65535, MARKER_LEN);
+  VERIFY_LEN(marker_len - 2);
+  // Save the marker type together with the app data.
+  std::string app_str(reinterpret_cast<const char*>(
+      &data[*pos - 3]), marker_len + 1);
+  *pos += marker_len - 2;
+  jpg->app_data.push_back(app_str);
+  return true;
+}
+    
+    bool EncodeSOF(const JPEGData& jpg, JPEGOutput out) {
+  const size_t ncomps = jpg.components.size();
+  const size_t marker_len = 8 + 3 * ncomps;
+  std::vector<uint8_t> data(marker_len + 2);
+  size_t pos = 0;
+  data[pos++] = 0xff;
+  data[pos++] = 0xc1;
+  data[pos++] = static_cast<uint8_t>(marker_len >> 8);
+  data[pos++] = marker_len & 0xff;
+  data[pos++] = kJpegPrecision;
+  data[pos++] = jpg.height >> 8;
+  data[pos++] = jpg.height & 0xff;
+  data[pos++] = jpg.width >> 8;
+  data[pos++] = jpg.width & 0xff;
+  data[pos++] = static_cast<uint8_t>(ncomps);
+  for (size_t i = 0; i < ncomps; ++i) {
+    data[pos++] = jpg.components[i].id;
+    data[pos++] = ((jpg.components[i].h_samp_factor << 4) |
+                      (jpg.components[i].v_samp_factor));
+    const size_t quant_idx = jpg.components[i].quant_idx;
+    if (quant_idx >= jpg.quant.size()) {
+      return false;
+    }
+    data[pos++] = jpg.quant[quant_idx].index;
+  }
+  return JPEGWrite(out, &data[0], pos);
+}
+    
+    
+    {}  // namespace guetzli
