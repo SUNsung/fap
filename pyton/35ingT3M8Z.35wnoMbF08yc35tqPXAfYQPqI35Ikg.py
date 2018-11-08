8e3f99812ae98f728029d7bbd321841e3fc423b0
SUNsung <weatherius@gmail.com>
2018-11-08 02:48:08 +0200

    Rewrite [35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py] File in: 2018-11-08 02:48:08

--- pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
+++ pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
@@ -1,19 +1,3 @@
 
         
         
-class BlackJackCard(Card):
-    
-        def __init__(self):
-        self.head = None
-        self.tail = None
-    
-            (foo, p1), 2
-        (bar, p1), 2
-        (bar, p1), 1
-        (foo, p2), 3
-        (bar, p3), 10
-        (foo, p4), 1
-        '''
-        timestamp, product_id, category, quantity = line.split('\t')
-        if self.within_past_week(timestamp):
-            yield (category, product_id), quantity
