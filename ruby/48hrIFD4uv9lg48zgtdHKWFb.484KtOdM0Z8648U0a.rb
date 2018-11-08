368bff90b8559b3a8cafae6a8e57d48cd611c09c
SUNsung <weatherius@gmail.com>
2018-11-08 12:25:14 +0200

    Rewrite [48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb] File in: 2018-11-08 12:25:13

--- ruby/48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb
+++ ruby/48hrIFD4uv9lg48zgtdHKWFb.484KtOdM0Z8648U0a.rb
@@ -1,137 +1,48 @@
 
         
-            if superenv?
-      ENV.keg_only_deps = keg_only_deps
-      ENV.deps = formula_deps
-      ENV.x11 = reqs.any? { |rq| rq.is_a?(X11Requirement) }
-      ENV.setup_build_environment(formula)
-      post_superenv_hacks
-      reqs.each(&:modify_build_environment)
-      deps.each(&:modify_build_environment)
-    else
-      ENV.setup_build_environment(formula)
-      reqs.each(&:modify_build_environment)
-      deps.each(&:modify_build_environment)
-    
-    module BuildEnvironmentDSL
-  def env(*settings)
-    @env ||= BuildEnvironment.new
-    @env.merge(settings)
-  end
+        def remove_head_from_history(markdown)
+  index = markdown =~ %r!^##\s+\d+\.\d+\.\d+!
+  markdown[index..-1]
 end
     
-      def initialize(f)
-    @f = f
-  end
+                  EM.start_server(
+                opts['host'],
+                opts['livereload_port'],
+                HttpAwareConnection,
+                opts
+              ) do |ws|
+                handle_websockets_event(ws)
+              end
     
-        def self.cleanup_lockfiles
-      return unless HOMEBREW_CACHE_FORMULA.directory?
-      candidates = HOMEBREW_CACHE_FORMULA.children
-      lockfiles  = candidates.select { |f| f.file? && f.extname == '.brewing' }
-      lockfiles.each do |file|
-        next unless file.readable?
-        file.open.flock(File::LOCK_EX | File::LOCK_NB) && file.unlink
-      end
+        # Returns the specified or detected guest type name.
+    #
+    # @return [Symbol]
+    def name
+      capability_host_chain[0][0]
     end
     
-        it 'creates an agent with a control target' do
-      visit '/'
-      page.find('a', text: 'Agents').trigger(:mouseover)
-      click_on('New Agent')
+            protected
     
-        it 'defauls foreground and background colors' do
-      scenario.tag_fg_color = nil
-      scenario.tag_bg_color = nil
-      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
-    end
-  end
-    
-              valid_parsed_trigger_agent_data.each do |key, value|
-            if key == :type
-              value = value.split('::').last
-            end
-            expect(trigger_agent_diff).to respond_to(key)
-            field = trigger_agent_diff.send(key)
-            expect(field).to be_a(ScenarioImport::AgentDiff::FieldDiff)
-            expect(field.incoming).to eq(value)
-            expect(field.updated).to eq(value)
-            expect(field.current).to be_nil
-          end
-          expect(trigger_agent_diff).not_to respond_to(:schedule)
+            # Executes a command and returns true if the command succeeded,
+        # and false otherwise. By default, this executes as a normal user,
+        # and it is up to the communicator implementation if they expose an
+        # option for running tests as an administrator.
+        #
+        # @see #execute
+        def test(command, opts=nil)
         end
       end
     end
-    
-    describe ConvertWebsiteAgentTemplateForMerge do
-  let :old_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
-    }
   end
-    
-    # TODO:
-# group :mongoid do
-#   gem 'mongoid', '~> 4.0.0'
-# end
+end
 
     
-    require 'minitest/autorun'
+    module Vagrant
+  module Plugin
+    module V2
+      # This is the base class for a CLI command.
+      class Command
+        include Util::SafePuts
     
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
-      end
-    
-          private
-    
-      def self.create_ipmi_getchannel_probe
-    [   # Get Channel Authentication Capabilities
-      0x06, 0x00, 0xff, 0x07, # RMCP Header
-      0x00, 0x00, 0x00, 0x00,
-      0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x20, 0x18,
-      0xc8, 0x81, 0x00, 0x38, 0x8e, 0x04, 0xb5
-    ].pack('C*')
-  end
-    
-            # Sends a kerberos request, and reads the response through the connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] The kerberos message
-        # @raise [RuntimeError] if the transport protocol is unknown or the response can't be parsed.
-        # @raise [NotImplementedError] if the transport protocol isn't supported
-        def send_recv(req)
-          send_request(req)
-          res = recv_response
-    
-                cipher = OpenSSL::Cipher.new('rc4')
-            cipher.decrypt
-            cipher.key = k3
-            decrypted = cipher.update(data) + cipher.final
-    
-              # Rex::Proto::Kerberos::Model::Checksum decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'Checksum decoding not supported'
-          end
-    
-          def is_edit_page
-        false
-      end
-    
-        def self.teardown(&block)
-      define_method(:teardown, &block)
-    end
-  end
-  (
-  class << klass;
-    self
-  end).send(:define_method, :name) { name.gsub(/\W/, '_') }
-  $contexts << klass
-  klass.class_eval &block
-end
+              nil
+        end
