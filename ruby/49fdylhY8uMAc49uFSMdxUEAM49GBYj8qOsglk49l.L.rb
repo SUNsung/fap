27d4091b0233153f860972a6867edd1e68b43684
SUNsung <weatherius@gmail.com>
2018-11-08 06:16:16 +0200

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2018-11-08 06:16:15

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,78 +1,215 @@
 
         
-            # This returns whether the guest is ready to work. If this returns
-    # `false`, then {#detect!} should be called in order to detect the
-    # guest OS.
+            def show_message
+      UI.message('Sending anonymous analytics information')
+      UI.message('Learn more at https://docs.fastlane.tools/#metrics')
+      UI.message('No personal or sensitive data is sent.')
+      UI.message('You can disable this by adding `opt_out_usage` at the top of your Fastfile')
+    end
+    
+            result = Fastlane::FastFile.new.parse('lane :test do
+          add_git_tag ({
+            tag: '#{tag}',
+          })
+        end').runner.execute(:test)
+    
+          it 'bootstraps with multiple dependencies' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            carthage(
+              command: 'bootstrap',
+              dependencies: ['TestDependency1', 'TestDependency2']
+            )
+          end').runner.execute(:test)
+    
+            expect(result[1]).to eq('security default-keychain -s ~/Library/Keychains/test.keychain')
+        expect(result[2]).to eq('security unlock-keychain -p testpassword ~/Library/Keychains/test.keychain')
+    
+          it 'handles the extensions parameter with no elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', extensions: [])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
+      end
+    
+            context 'when disabled' do
+          it 'should raise if swiftlint completes with a non-zero exit status' do
+            allow(FastlaneCore::UI).to receive(:important)
+            expect(FastlaneCore::UI).to receive(:important).with(/If you want fastlane to continue anyway/)
+            # This is simulating the exception raised if the return code is non-zero
+            expect(Fastlane::Actions).to receive(:sh).and_raise('fake error')
+            expect(FastlaneCore::UI).to receive(:user_error!).with(/SwiftLint finished with errors/).and_call_original
+    
+            allow(File).to receive(:file?).and_return(false)
+        expect(File).to receive(:file?).with('#{Dir.home}/Library/Keychains/login.keychain-db').and_return(true)
+        allow(File).to receive(:exist?).and_return(false)
+        expect(File).to receive(:exist?).with('item.path').and_return(true)
+    
+    describe JobsHelper do
+  let(:job) { Delayed::Job.new }
+    
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
+  end
+    
+      it 'schould register the schedules with the rufus scheduler and run' do
+    mock(@rufus_scheduler).join
+    scheduler = HuginnScheduler.new
+    scheduler.setup!(@rufus_scheduler, Mutex.new)
+    scheduler.run
+  end
+    
+      describe 'down' do
+    let :valid_options do
+      super().merge('extract' => new_extract,
+                    'template' => new_template)
+    end
+    
+            mock(Rails.logger).warn('DEPRECATED: The .receive_webhook method is deprecated, please switch your Agent to use .receive_web_request.')
+        @agent.trigger_web_request(request)
+        expect(@agent.reload.memory['last_webhook_request']).to eq({ 'some_param' => 'some_value' })
+        expect(@agent.last_web_request_at.to_i).to be_within(1).of(Time.now.to_i)
+      end
+    end
+  end
+    
+        # This will detect the proper guest OS for the machine and set up
+    # the class to actually execute capabilities.
+    def detect!
+      guest_name = @machine.config.vm.guest
+      initialize_capabilities!(guest_name, @guests, @capabilities, @machine)
+    rescue Errors::CapabilityHostExplicitNotDetected => e
+      raise Errors::GuestExplicitNotDetected, value: e.extra_data[:value]
+    rescue Errors::CapabilityHostNotDetected
+      raise Errors::GuestNotDetected
+    end
+    
+            # This returns all the registered communicators.
+        #
+        # @return [Hash]
+        def communicators
+          result = {}
+    
+                    raise Errors::VMNoMatchError if machines.empty?
+              else
+                # String name, just look for a specific VM
+                @logger.debug('Finding machine that match name: #{name}')
+                machines << get_machine.call(name.to_sym)
+                raise Errors::VMNotFoundError, name: name if !machines[0]
+              end
+            end
+          else
+            # No name was given, so we return every VM in the order
+            # configured.
+            @logger.debug('Loading all machines...')
+            machines = @env.machine_names.map do |machine_name|
+              get_machine.call(machine_name)
+            end
+          end
+    
+      it 'raises a TypeError when passed nil' do
+    lambda { sleep(nil)   }.should raise_error(TypeError)
+  end
+    
+    module Sass
+  module CacheStores
+    # A backend for the Sass cache using the filesystem.
+    class Filesystem < Base
+      # The directory where the cached files will be stored.
+      #
+      # @return [String]
+      attr_accessor :cache_location
+    
+        # Returns a string representation of the Sass backtrace.
     #
-    # @return [Boolean]
-    def ready?
-      !!capability_host_chain
+    # @param default_filename [String] The filename to use for unknown files
+    # @see #sass_backtrace
+    # @return [String]
+    def sass_backtrace_str(default_filename = 'an unknown file')
+      lines = message.split('\n')
+      msg = lines[0] + lines[1..-1].
+        map {|l| '\n' + (' ' * 'Error: '.size) + l}.join
+      'Error: #{msg}' +
+        sass_backtrace.each_with_index.map do |entry, i|
+          '\n        #{i == 0 ? 'on' : 'from'} line #{entry[:line]}' +
+            ' of #{entry[:filename] || default_filename}' +
+            (entry[:mixin] ? ', in `#{entry[:mixin]}'' : '')
+        end.join
+    end
+    
+          # Polling is used by default on Windows.
+      unless Sass::Util.windows?
+        opts.on('--poll', 'Check for file changes manually, rather than relying on the OS.',
+                          'Only meaningful for --watch.') do
+          @options[:poll] = true
+        end
+      end
+    
+        <div id='cookies'>
+      <h3 id='cookie-info'>COOKIES</h3>
+      <% unless req.cookies.empty? %>
+        <table class='req'>
+          <tr>
+            <th>Variable</th>
+            <th>Value</th>
+          </tr>
+          <% req.cookies.each { |key, val| %>
+            <tr>
+              <td><%=h key %></td>
+              <td class='code'><div><%=h val.inspect %></div></td>
+            </tr>
+          <% } %>
+        </table>
+      <% else %>
+        <p class='no-data'>No cookie data.</p>
+      <% end %>
+      <div class='clear'></div>
+    </div> <!-- /COOKIES -->
+    
+            # Set these key values to boolean 'true' to include in policy
+        NO_ARG_DIRECTIVES.each do |d|
+          if options.key?(d) && options[d].is_a?(TrueClass)
+            directives << d.to_s.sub(/_/, '-')
+          end
+        end
+    
+            expect(new_source)
+          .to eq('#{prefix}#{open}#{a}, # a\n#{b}#{close} # b\n#{suffix}')
+      end
+    
+            expect(new_source).to eq(['#{prefix}#{open}#{a},',
+                                  '#{b}#{close}',
+                                  suffix].join($RS))
+      end
     end
   end
 end
 
     
-              name = name.to_s
-          name = name[0...-1] if name.end_with?('=')
-    
+          # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
       #
-  # Closes the supplied client, if valid.
-  #
-  def close_client(cli)
-    listener.close_client(cli)
-  end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Crypto
-        module Rc4Hmac
-          # Decrypts the cipher using RC4-HMAC schema
-          #
-          # @param cipher [String] the data to decrypt
-          # @param key [String] the key to decrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the decrypted cipher
-          # @raise [RuntimeError] if decryption doesn't succeed
-          def decrypt_rc4_hmac(cipher, key, msg_type)
-            unless cipher && cipher.length > 16
-              raise ::RuntimeError, 'RC4-HMAC decryption failed'
-            end
-    
-              # Encodes the authenticator field
-          #
-          # @return [String]
-          def encode_authenticator
-            authenticator.encode
-          end
-        end
+      # In this case, the `def` node destructures into:
+      #
+      #   `method_name, arguments, body`
+      #
+      # while the `defs` node destructures into:
+      #
+      #   `receiver, method_name, arguments, body`
+      #
+      # so we reverse the destructured array to get the optional receiver
+      # at the end, where it can be discarded.
+      #
+      # @return [Array] the different parts of the `def` or `defs` node
+      def node_parts
+        to_a.reverse
       end
     end
   end
 end
-    
-                seq = OpenSSL::ASN1::Sequence.new(elems)
-    
-    # This file provides the following methods:
-#   encodeURIComponent(componentString)
-#   string.charCodeAt(k)
-    
-          def css # custom css
-        @css
-      end
-    
-        @wiki.clear_cache
-    page_2 = @wiki.page(page_1.name)
-    assert_equal 'abc', page_2.raw_data
-    assert_equal '[no message]', page_2.version.message
-    assert_not_equal page_1.version.sha, page_2.version.sha
-  end
-    
-        @wiki = Gollum::Wiki.new(@path)
-    page  = @wiki.page('k')
-    assert_equal '바뀐 text', utf8(page.raw_data)
-    assert_equal 'ghi', page.version.message
-  end
+
     
     module RuboCop
   module AST
@@ -87,9 +224,14 @@ end
         each_pair.to_a
       end
     
-          # A helper class for comparing the positions of different parts of a
-      # `pair` node.
-      class HashElementDelta
-        def initialize(first, second)
-          @first = first
-          @second = second
+          # Whether the last argument of the node is a block pass,
+      # i.e. `&block`.
+      #
+      # @return [Boolean] whether the last argument of the node is a block pass
+      def block_argument?
+        arguments? &&
+          (last_argument.block_pass_type? || last_argument.blockarg_type?)
+      end
+    end
+  end
+end
