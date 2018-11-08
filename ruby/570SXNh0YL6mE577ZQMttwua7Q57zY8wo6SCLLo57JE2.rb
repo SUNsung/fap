3e52f10ffb3fb96a813298c934774a6c223642f1
SUNsung <weatherius@gmail.com>
2018-11-08 06:43:27 +0200

    Rewrite [570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb] File in: 2018-11-08 06:43:27

--- ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
+++ ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
@@ -1,111 +1,172 @@
 
         
-            Pubsubhubbub::UnsubscribeWorker.perform_async(signed_request_account.id) if signed_request_account.subscribed?
-    DeliveryFailureTracker.track_inverse_success!(signed_request_account)
-  end
+          url = File.dirname(url)
+  url == FORWARD_SLASH ? url : '#{url}/'
+end
     
-        def destroy
-      authorize @custom_emoji, :destroy?
-      @custom_emoji.destroy!
-      log_action :destroy, @custom_emoji
-      flash[:notice] = I18n.t('admin.custom_emojis.destroyed_msg')
-      redirect_to admin_custom_emojis_path(page: params[:page], **@filter_params)
+    def graceful_require
+  Jekyll::External.require_with_graceful_fail('json')
+  JSON.pretty_generate(DATA)
+end
+    
+    require 'benchmark/ips'
+require 'pathutil'
+    
+    # For this pull request, which changes Page#dir
+# https://github.com/jekyll/jekyll/pull/4403
+    
+    module Jekyll
+  module Commands
+    class NewTheme < Jekyll::Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:'new-theme') do |c|
+            c.syntax 'new-theme NAME'
+            c.description 'Creates a new Jekyll theme scaffold'
+            c.option 'code_of_conduct', \
+                     '-c', '--code-of-conduct', \
+                     'Include a Code of Conduct. (defaults to false)'
+    
+                  Jekyll.logger.info 'LiveReload address:',
+                                 'http://#{opts['host']}:#{opts['livereload_port']}'
+            end
+          end
+          @thread.abort_on_exception = true
+        end
+    
+            def initialize(_opts)
+          # If EventMachine SSL support on Windows ever gets better, the code below will
+          # set up the reactor to handle SSL
+          #
+          # @ssl_enabled = opts['ssl_cert'] && opts['ssl_key']
+          # if @ssl_enabled
+          #   em_opts[:tls_options] = {
+          #   :private_key_file => Jekyll.sanitized_path(opts['source'], opts['ssl_key']),
+          #   :cert_chain_file  => Jekyll.sanitized_path(opts['source'], opts['ssl_cert'])
+          #   }
+          #   em_opts[:secure] = true
+          # end
+    
+          it 'Does not match lightweight tags when searching for the last one if so requested' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          changelog_from_git_commits(match_lightweight_tag: false)
+        end').runner.execute(:test)
+    
+          it 'handles the extensions parameter with no elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', extensions: [])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
+      end
+    
+            it 'escapes spaces in list of files to process' do
+          file = 'path/to/my project/source code/AppDelegate.swift'
+          result = Fastlane::FastFile.new.parse('lane :test do
+            swiftlint(
+                files: ['#{file}']
+            )
+          end').runner.execute(:test)
+    
+          self.default_value
     end
-    
-      def maxwidth_or_default
-    (params[:maxwidth].presence || 400).to_i
   end
-    
-    class Api::SubscriptionsController < Api::BaseController
-  before_action :set_account
-  respond_to :txt
-    
-        def replace_vars(less)
-      less = less.dup
-      # skip header comment
-      less =~ %r(\A/\*(.*?)\*/)m
-      from           = $~ ? $~.to_s.length : 0
-      less[from..-1] = less[from..-1].
-          gsub(/(?!@mixin|@media|@page|@keyframes|@font-face|@-\w)@/, '$').
-          # variables that would be ignored by gsub above: e.g. @page-header-border-color
-          gsub(/@(page[\w-]+)/, '$\1')
-      less
-    end
-    
-      # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-    
-      config.active_support.deprecation = :stderr
 end
 
     
-    Given /^(?:|[tT]hat )?following user[s]?(?: exist[s]?)?:$/ do |table|
-  table.hashes.each do |hash|
-    if hash.has_key? 'username' and hash.has_key? 'email'
-      step %{a user named '#{hash['username']}' with email '#{hash['email']}'}
-    elsif hash.has_key? 'username'
-      step %{a user with username '#{hash['username']}'}
-    elsif hash.has_key? 'email'
-      step %{a user with email '#{hash['email']}'}
+    # test monkey patched method on both (simulated) OSes
+describe 'monkey patch of String.shellescape (via CrossplatformShellwords)' do
+  describe 'on Windows' do
+    before(:each) do
+      allow(FastlaneCore::Helper).to receive(:windows?).and_return(true)
+    end
+    
+            allow(File).to receive(:file?).and_return(false)
+        expect(File).to receive(:file?).with('#{Dir.home}/Library/Keychains/login.keychain').and_return(true)
+        allow(File).to receive(:exist?).and_return(false)
+        expect(File).to receive(:exist?).with('item.path').and_return(true)
+    
+      def scope_for_collection
+    case params[:id]
+    when 'featured'
+      @account.statuses.permitted_for(@account, signed_request_account).tap do |scope|
+        scope.merge!(@account.pinned_statuses)
+      end
+    else
+      raise ActiveRecord::NotFound
     end
   end
-end
     
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
+          @form         = Form::StatusBatch.new(form_status_batch_params.merge(current_account: current_account, action: action_from_button))
+      flash[:alert] = I18n.t('admin.statuses.failed_to_execute') unless @form.save
     
-        def split_colon_path(path)
-      one, two = path.split(':', 2)
-      if one && two && Sass::Util.windows? &&
-          one =~ /\A[A-Za-z]\Z/ && two =~ %r{\A[/\\]}
-        # If we're on Windows and we were passed a drive letter path,
-        # don't split on that colon.
-        one2, two = two.split(':', 2)
-        one = one + ':' + one2
-      end
-      return one, two
+          weeks << {
+        week: week.to_time.to_i.to_s,
+        statuses: Redis.current.get('activity:statuses:local:#{week_id}') || '0',
+        logins: Redis.current.pfcount('activity:logins:#{week_id}').to_s,
+        registrations: Redis.current.get('activity:accounts:local:#{week_id}') || '0',
+      }
     end
     
-    Then(/^the task fails$/) do
-  expect(@success).to be_falsey
-end
+        private
     
-    Given(/^a linked file '(.*?)'$/) do |file|
-  # ignoring other linked files
-  TestApp.append_to_deploy_file('set :linked_files, ['#{file}']')
-end
+      it 'does object equality on config_hash and pipeline_id' do
+    another_exact_pipeline = described_class.new(source, pipeline_id, ordered_config_parts, settings)
+    expect(subject).to eq(another_exact_pipeline)
     
-          # Runs all validation rules registered for the given key against the
-      # user-supplied value for that variable. If no validator raises an
-      # exception, the value is assumed to be valid.
-      def assert_valid_now(key, value)
-        validators[key].each do |validator|
-          validator.call(key, value)
+        while readable_io = IO.select([self_read])
+      signal = readable_io.first[0].gets.strip
+      handle_signal(launcher, signal)
+    end
+  #end
+rescue SystemExit => e
+  #Sidekiq.logger.error('Profiling...')
+  #result = RubyProf.stop
+  #printer = RubyProf::GraphHtmlPrinter.new(result)
+  #printer.print(File.new('output.html', 'w'), :min_percent => 1)
+  # normal
+rescue => e
+  raise e if $DEBUG
+  STDERR.puts e.message
+  STDERR.puts e.backtrace.join('\n')
+  exit 1
+end
+
+    
+        worker_count.times do |count|
+      template '/data/#{app}/shared/config/sidekiq_#{count}.yml' do
+        owner node[:owner_name]
+        group node[:owner_name]
+        mode 0644
+        source 'sidekiq.yml.erb'
+        variables({
+          :require => '/data/#{app}/current'
+        })
+      end
+    end
+    
+        module ActionMailer
+      def sidekiq_delay(options={})
+        Proxy.new(DelayedMailer, self, options)
+      end
+      def sidekiq_delay_for(interval, options={})
+        Proxy.new(DelayedMailer, self, options.merge('at' => Time.now.to_f + interval.to_f))
+      end
+      def sidekiq_delay_until(timestamp, options={})
+        Proxy.new(DelayedMailer, self, options.merge('at' => timestamp.to_f))
+      end
+      alias_method :delay, :sidekiq_delay
+      alias_method :delay_for, :sidekiq_delay_for
+      alias_method :delay_until, :sidekiq_delay_until
+    end
+    
+          Sidekiq.redis do |conn|
+        conn.pipelined do
+          jobs_to_requeue.each do |queue, jobs|
+            conn.rpush('queue:#{queue}', jobs)
+          end
         end
       end
-    
-          !File.exists?(File.join(LogStash::Environment::LOGSTASH_HOME, 'x-pack'))
+      Sidekiq.logger.info('Pushed #{inprogress.size} jobs back to Redis')
+    rescue => ex
+      Sidekiq.logger.warn('Failed to requeue #{inprogress.size} jobs: #{ex.message}')
     end
-    
-        # To make sure we have the maximum compatibility
-    # we will ignore theses gems and they won't be included in the pack
-    IGNORE_GEMS_IN_PACK = %w(
-      logstash-core
-      logstash-core-plugin-api
-      jar-dependencies
-    )
-    
-              it 'successfully install the plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
-          end
-    
-        context 'with a specific plugin' do
-      let(:plugin_name) { 'logstash-input-stdin' }
-      it 'list the plugin and display the plugin name' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
-        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
-      end
