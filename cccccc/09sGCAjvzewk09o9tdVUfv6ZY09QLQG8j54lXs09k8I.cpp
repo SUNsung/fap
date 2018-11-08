cf34b9fba8bebce07d269d7abb33e7ad23a23015
SUNsung <weatherius@gmail.com>
2018-11-08 23:31:09 +0200

    Rewrite [09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp] File in: 2018-11-08 23:31:09

--- cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
+++ cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
@@ -1,107 +1,247 @@
 
         
-        
-    {
-    { private:
-  /*! \brief corresponding DLTensor of this TBlob */
-  DLTensor dltensor_;
-};
-}  // namespace mxnet
+        #include 'base/command_line.h'
     
-    /*!
- * Copyright (c) 2016 by Contributors
- * \file caffe_blob.cc
- * \brief Implementations of SetDataGradToBlob given various device/dimension
- * \author Haoran Wang
-*/
-#include 'caffe_blob.h'
-namespace mxnet {
-namespace op {
-namespace caffe {
-    }
-    }
-    }
+    #include 'atom/browser/native_window_views.h'
     
-    #include './exec_pass.h'
-    
-    void StopGrid::startWithTarget(Node *target)
-{
-    ActionInstant::startWithTarget(target);
-    cacheTargetAsGridNode();
-    GridBase *grid = _gridNodeTarget->getGrid();
-    if (grid && grid->isActive())
-    {
-        grid->setActive(false);
-    }
+    // static
+void InAppPurchase::BuildPrototype(v8::Isolate* isolate,
+                                   v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'InAppPurchase'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod('canMakePayments', &in_app_purchase::CanMakePayments)
+      .SetMethod('getReceiptURL', &in_app_purchase::GetReceiptURL)
+      .SetMethod('purchaseProduct', &InAppPurchase::PurchaseProduct)
+      .SetMethod('finishAllTransactions',
+                 &in_app_purchase::FinishAllTransactions)
+      .SetMethod('finishTransactionByDate',
+                 &in_app_purchase::FinishTransactionByDate)
+      .SetMethod('getProducts', &in_app_purchase::GetProducts);
 }
     
-        /**
-    @brief Initializes the action with center position, number of twirls, amplitude, a grid size and duration.
-    @param duration Specify the duration of the Twirl action. It's a value in seconds.
-    @param gridSize Specify the size of the grid.
-    @param position Specify the center position of the twirl action.
-    @param twirls Specify the twirls count of the Twirl action.
-    @param amplitude Specify the amplitude of the Twirl action.
-    @return If the initialization success, return true; otherwise, return false.
-    */
-    bool initWithDuration(float duration, const Size& gridSize, const Vec2& position, unsigned int twirls, float amplitude);
+    #ifndef ATOM_BROWSER_API_ATOM_API_IN_APP_PURCHASE_H_
+#define ATOM_BROWSER_API_ATOM_API_IN_APP_PURCHASE_H_
     
-    /** @class BezierBy
- * @brief An action that moves the target with a cubic Bezier curve by a certain distance.
- */
-class CC_DLL BezierBy : public ActionInterval
-{
-public:
-    /** Creates the action with a duration and a bezier configuration.
-     * @param t Duration time, in seconds.
-     * @param c Bezier config.
-     * @return An autoreleased BezierBy object.
-     * @code
-     * When this function bound to js or lua,the input params are changed.
-     * in js: var create(var t,var table)
-     * in lua: local create(local t, local table)
-     * @endcode
-     */
-    static BezierBy* create(float t, const ccBezierConfig& c);
+    // static
+v8::Local<v8::Value> PowerMonitor::Create(v8::Isolate* isolate) {
+  if (!Browser::Get()->is_ready()) {
+    isolate->ThrowException(v8::Exception::Error(mate::StringToV8(
+        isolate,
+        'Cannot require \'powerMonitor\' module before app is ready')));
+    return v8::Null(isolate);
+  }
     }
     
-        tHashElement *element = nullptr;
-    HASH_FIND_PTR(_targets, &target, element);
-    if (element)
-    {
-        if (ccArrayContainsObject(element->actions, element->currentAction) && (! element->currentActionSalvaged))
+    #ifndef ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
+#define ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
+    
+    SavePageHandler::SavePageHandler(content::WebContents* web_contents,
+                                 const SavePageCallback& callback)
+    : web_contents_(web_contents), callback_(callback) {}
+    
+      // Finally, add the generic parameters from the requirement.
+  for (auto genericParam : reqSig->getGenericParams().slice(1)) {
+    // The only depth that makes sense is depth == 1, the generic parameters
+    // of the requirement itself. Anything else is from invalid code.
+    if (genericParam->getDepth() != 1) {
+      return;
+    }
+    }
+    
+    using namespace swift::sys;
+using llvm::StringRef;
+    
+      // Set the 'os' platform condition.
+  if (Target.isMacOSX())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'OSX');
+  else if (Target.isTvOS())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'tvOS');
+  else if (Target.isWatchOS())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'watchOS');
+  else if (Target.isiOS())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'iOS');
+  else if (Target.isAndroid())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'Android');
+  else if (Target.isOSLinux())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'Linux');
+  else if (Target.isOSFreeBSD())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'FreeBSD');
+  else if (Target.isOSWindows())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'Windows');
+  else if (Target.isWindowsCygwinEnvironment())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'Cygwin');
+  else if (Target.isPS4())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'PS4');
+  else if (Target.isOSHaiku())
+    addPlatformConditionValue(PlatformConditionKind::OS, 'Haiku');
+  else
+    UnsupportedOS = true;
+    
+    Optional<swift::UUID> swift::UUID::fromString(const char *s) {
+#if defined(_WIN32)
+  RPC_CSTR t = const_cast<RPC_CSTR>(reinterpret_cast<const unsigned char*>(s));
+    }
+    
+    
+    {  return GraphemeClusterBreakProperty::Other;
+}
+    
+            if(cn > 1)
         {
-            element->currentAction->retain();
-            element->currentActionSalvaged = true;
+            for(size_t j = 0, jn = 0; j < size.width; ++j, jn += cn)
+            {
+                size_t maxIdx = jn;
+                for(s32 k = 1; k < cn; ++k)
+                    if(_norm[jn + k] > _norm[maxIdx]) maxIdx = jn + k;
+                _norm[j] = _norm[maxIdx];
+                _dx[j] = _dx[maxIdx];
+                _dy[j] = _dy[maxIdx];
+            }
         }
+    
+    #endif //CAROTENE_NEON
+    
+    
+    {            vst1q_s16(dst + j, v_dst0);
+            vst1q_s16(dst + j + 8, v_dst1);
+        }
+        for (; j < roiw8; j += 8)
+        {
+            int16x8_t v_dst = vreinterpretq_s16_u16(vmovl_u8(vld1_u8(src + j)));
+            vst1q_s16(dst + j, v_dst);
+        }
+    
+    
+    {} // namespace CAROTENE_NS
+
+    
+    
+    {        for (; i < size.width; ++i)
+            result += s32(src0[i]) * s32(src1[i]);
+    }
+    return result;
+#else
+    (void)_size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    
+            for (; j < roiw_tail; j += step_tail, js += step_tail3, jd -= step_tail3)
+        {
+            vec64 v_src = vld3(src + js), v_dst;
+            v_dst.val[0] = vrev64(v_src.val[0]);
+            v_dst.val[1] = vrev64(v_src.val[1]);
+            v_dst.val[2] = vrev64(v_src.val[2]);
     }
     
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+    #include 'common.hpp'
     
-    protected:
-    float _to;
-    float _from;
+      rocksdb::SyncPoint::GetInstance()->LoadDependency({
+      {'CompactionJob::Run():Start', 'DynamicLevelMaxBytesBase2:0'},
+      {'DynamicLevelMaxBytesBase2:1', 'CompactionJob::Run():End'},
+      {'DynamicLevelMaxBytesBase2:compact_range_finish',
+       'FlushJob::WriteLevel0Table'},
+  });
+  rocksdb::SyncPoint::GetInstance()->EnableProcessing();
     
-     @since v0.99.2
- */
-class CC_DLL ActionTween : public ActionInterval
-{
-public:
-    /** 
-     * @brief Create and initializes the action with the property name (key), and the from and to parameters.
-     * @param duration The duration of the ActionTween. It's a value in seconds.
-     * @param key The key of property which should be updated.
-     * @param from The value of the specified property when the action begin.
-     * @param to The value of the specified property when the action end.
-     * @return If the creation success, return a pointer of ActionTween; otherwise, return nil.
-     */
-    static ActionTween* create(float duration, const std::string& key, float from, float to);
+    class DBEncryptionTest : public DBTestBase {
+ public:
+  DBEncryptionTest() : DBTestBase('/db_encryption_test') {}
+};
+    
+        // We try twice: the first time with a fixed-size stack allocated buffer,
+    // and the second time with a much larger dynamically allocated buffer.
+    char buffer[500];
+    for (int iter = 0; iter < 2; iter++) {
+      char* base;
+      int bufsize;
+      if (iter == 0) {
+        bufsize = sizeof(buffer);
+        base = buffer;
+      } else {
+        bufsize = 30000;
+        base = new char[bufsize];
+      }
+      char* p = base;
+      char* limit = base + bufsize;
     }
     
+      // Results of these can be affected by FakeSleepForMicroseconds()
+  virtual Status GetCurrentTime(int64_t* unix_time) override;
+  virtual uint64_t NowMicros() override;
+  virtual uint64_t NowNanos() override;
     
-    {    // update duration
-    _totalDelayUnits++;
+    std::string kDBPath = '/tmp/rocksdb_column_families_example';
+    
+    
+    {}  // namespace
+    
+    // move-only type
+Expected<std::unique_ptr<int>, Err> f3(int x, double y) {
+  return std::make_unique<int>(int(x + y));
 }
     
-        bool init(void);
+    void BENCHFUN(fillCtor)(int iters, int size) {
+  FOR_EACH_RANGE (i, 0, iters) {
+    VECTOR v(size_t(size), randomObject<VECTOR::value_type>());
+    doNotOptimizeAway(&v);
+  }
+}
+BENCHMARK_PARAM(BENCHFUN(fillCtor), 16)
+BENCHMARK_PARAM(BENCHFUN(fillCtor), 128)
+BENCHMARK_PARAM(BENCHFUN(fillCtor), 1024)
+    
+    template <size_t... Indexes>
+int snprintf20Numbers(int i, index_sequence<Indexes...>) {
+  static_assert(20 == sizeof...(Indexes), 'Must have exactly 20 indexes');
+  return snprintf(
+      bigBuf.data(),
+      bigBuf.size(),
+      '%d %d %d %d %d'
+      '%d %d %d %d %d'
+      '%d %d %d %d %d'
+      '%d %d %d %d %d',
+      (i + static_cast<int>(Indexes))...);
+}
+    
+    int main(int argc, char** argv) {
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  folly::runBenchmarks();
+  return 0;
+}
+
+    
+    static uint32_t pointerTestFunc() {
+  uint32_t v1 = folly::Random::rand32();
+  uint32_t v2 = folly::Random::rand32();
+  std::string str = 'test string';
+  const char* a = str.c_str();
+  FOLLY_SDT(folly, test_static_tracepoint_pointer, a, v2, &v1);
+  return v1 + v2;
+}
+    
+    // Empirically, this indicates if the runtime supports
+// std::exception_ptr, as not all (arm, for instance) do.
+#if defined(__GNUC__) && defined(__GCC_ATOMIC_INT_LOCK_FREE) && \
+    __GCC_ATOMIC_INT_LOCK_FREE > 1
+inline fbstring exceptionStr(std::exception_ptr ep) {
+  try {
+    std::rethrow_exception(ep);
+  } catch (const std::exception& e) {
+    return exceptionStr(e);
+  } catch (...) {
+    return '<unknown exception>';
+  }
+}
+#endif
+    
+    template <
+    typename T,
+    template <typename> class Atom = std::atomic,
+    typename CountedDetail = detail::shared_ptr_internals>
+class atomic_shared_ptr {
+  using SharedPtr = typename CountedDetail::template CountedPtr<T>;
+  using BasePtr = typename CountedDetail::counted_base;
+  using PackedPtr = folly::PackedSyncPtr<BasePtr>;
+    }
