e8c97f88fd92c9fbddede8ffbfc88a30b13d3473
SUNsung <weatherius@gmail.com>
2018-11-09 17:58:38 +0200

    Rewrite [08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp] File in: 2018-11-09 17:58:37

--- cccccc/08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp
+++ cccccc/08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp
@@ -1,192 +1,246 @@
 
         
-        void read_image(std::ifstream* image_file, std::ifstream* label_file,
-        uint32_t index, uint32_t rows, uint32_t cols,
-        char* pixels, char* label) {
-  image_file->seekg(index * rows * cols + 16);
-  image_file->read(pixels, rows * cols);
-  label_file->seekg(index + 8);
-  label_file->read(label, 1);
+        
+    {}  // namespace api
+    
+    namespace IPC {
+class Message;
 }
     
-    namespace caffe {
-    }
-    
-    
-    { protected:
-  shared_ptr<SyncedMemory> rand_vec_;
-};
-    
-    namespace caffe {
-    }
-    
-    /**
- * @brief Normalizes the input to have 0-mean and/or unit (1) variance across
- *        the batch.
- *
- * This layer computes Batch Normalization as described in [1]. For each channel
- * in the data (i.e. axis 1), it subtracts the mean and divides by the variance,
- * where both statistics are computed across both spatial dimensions and across
- * the different examples in the batch.
- *
- * By default, during training time, the network is computing global
- * mean/variance statistics via a running average, which is then used at test
- * time to allow deterministic outputs for each input. You can manually toggle
- * whether the network is accumulating or using the statistics via the
- * use_global_stats option. For reference, these statistics are kept in the
- * layer's three blobs: (0) mean, (1) variance, and (2) moving average factor.
- *
- * Note that the original paper also included a per-channel learned bias and
- * scaling factor. To implement this in Caffe, define a `ScaleLayer` configured
- * with `bias_term: true` after each `BatchNormLayer` to handle both the bias
- * and scaling factor.
- *
- * [1] S. Ioffe and C. Szegedy, 'Batch Normalization: Accelerating Deep Network
- *     Training by Reducing Internal Covariate Shift.' arXiv preprint
- *     arXiv:1502.03167 (2015).
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class BatchNormLayer : public Layer<Dtype> {
- public:
-  explicit BatchNormLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-      virtual inline const char* type() const { return 'Bias'; }
-  virtual inline int MinBottomBlobs() const { return 1; }
-  virtual inline int MaxBottomBlobs() const { return 2; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    
-      virtual inline const char* type() const { return 'Crop'; }
-  virtual inline int ExactNumBottomBlobs() const { return 2; }
-  virtual inline int ExactNumTopBlobs() const { return 1; }
-    
-    #include 'collision_object_bullet.h'
-#include 'space_bullet.h'
-    
-    bool GodotCollisionDispatcher::needsResponse(const btCollisionObject *body0, const btCollisionObject *body1) {
-	if (body0->getUserIndex() == CASTED_TYPE_AREA || body1->getUserIndex() == CASTED_TYPE_AREA) {
-		// Avoide area narrow phase
-		return false;
-	}
-	return btCollisionDispatcher::needsResponse(body0, body1);
-}
+    #endif  // ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
 
     
-    class RigidBodyBullet;
     
-    			if (i == 0) {
-				continue; // next must be a ':'
-			};
-			if (!part_found) {
-				part_skip = true;
-				parts[parts_idx++] = -1;
-			};
-			part_found = false;
-		} else if (c == '.') {
+    {  DefaultCacheKey CKey(const_cast<void*>(Key), &DCache.CBs);
+  auto Entry = DCache.Entries.find(CKey);
+  if (Entry != DCache.Entries.end()) {
+    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
+    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
+    DCache.Entries.erase(Entry);
+    return true;
+  }
+  return false;
+}
     
-        LearnerPtr FSAdaGradLearner(const vector<Parameter>& parameters,
-                                const LearningRateSchedule& learningRateSchedule,
-                                const MomentumSchedule& momentumSchedule,
-                                bool unitGain, /*=true*/
-                                const MomentumSchedule& varianceMomentumSchedule, /*= MomentumAsTimeConstantSchedulePerSample(2 * 3600 * 100)*/
-                                AdditionalLearningOptions additionalOptions /*= AdditionalLearningOptions()*/)
-    {
-        return MakeSharedObject<LearnerFSAdaGrad>(parameters, learningRateSchedule, momentumSchedule, unitGain, varianceMomentumSchedule, additionalOptions);
-    }
-    
-        // Releases the mutex
-    void Release()
-    {
-        assert(m_fd != -1);
-        // removing file
-        unlink(m_fileName.c_str());
-        // Note: file is intentionally removed *before* releasing the lock
-        // to ensure that locked file isn't deleted by the non-owner of the lock
-        m_lock.l_type = F_UNLCK;
-        // Now removing the lock and closing the file descriptor
-        // waiting processes will be notified
-        int rc = fcntl(m_fd, F_SETLKW, &m_lock);
-        if (rc == FCNTL_ERROR)
         {
-            RuntimeError('Mutex Release: Failed to release mutex %s', m_fileName.c_str());
-        }
-        close(m_fd);
-        m_fd = -1;
+      Out << Indent;
+      if (childKind == ChildKind::Root) {
+        Out << '+- ';
+      } else if (childKind == ChildKind::Left) {
+        Out << '/- ';
+      } else if (childKind == ChildKind::Right) {
+        Out << '\\- ';
+      } else if (childKind == ChildKind::Further) {
+        Out << '\\-> ';
+      }
+      PrintNodeData(Out, node);
+      Out << '\n';
     }
     
-        // release gradient and temp matrices that no longer needed after all the children's gradients are computed.
-    virtual void ReleaseMatricesAfterBackprop(MatrixPool& matrixPool)
-    {
-        Base::ReleaseMatricesAfterBackprop(matrixPool);
-        ReleaseMatrixToPool(m_innerproduct, matrixPool);
-        ReleaseMatrixToPool(m_rightGradient, matrixPool);
+    void
+swift::trimLeadingWhitespaceFromLines(StringRef RawText,
+                                      unsigned WhitespaceToTrim,
+                                      SmallVectorImpl<StringRef> &OutLines) {
+  SmallVector<StringRef, 8> Lines;
+    }
+    
+    namespace swift {
+namespace sys {
+void TaskProcessInformation::ResourceUsage::provideMapping(json::Output &out) {
+  out.mapRequired('utime', Utime);
+  out.mapRequired('stime', Stime);
+  out.mapRequired('maxrss', Maxrss);
+}
+    }
     }
     
     
-    {    virtual void Load(File& fstream, size_t modelVersion) override
-    {
-        Base::Load(fstream, modelVersion);
-        fstream >> m_isOutputSparse;
-    }
-private:
-    bool m_isOutputSparse;
+    {  return forInvalid();
+}
+    
+    struct IAMOptions {
+  // TODO: fine-grained control over how we infer
+  static IAMOptions getDefault();
 };
     
-        // Overrides
-    virtual void startWithTarget(Node *target) override;
-    virtual ActionCamera * reverse() const override;
-    virtual ActionCamera *clone() const override;
-    
-    PointArray* PointArray::clone() const
+    template <typename Op>
+void vcompare(Size2D size,
+              const typename Op::type * src0Base, ptrdiff_t src0Stride,
+              const typename Op::type * src1Base, ptrdiff_t src1Stride,
+              u8 * dstBase, ptrdiff_t dstStride, const Op & op)
 {
-    vector<Vec2> newArray = _controlPoints;
-    
-    PointArray *points = new (std::nothrow) PointArray();
-    points->initWithCapacity(10);
-    points->setControlPoints(std::move(newArray));
+    typedef typename Op::type type;
+    typedef typename internal::VecTraits<type>::vec128 vec128;
+    typedef typename internal::VecTraits<type>::unsign::vec128 uvec128;
     }
     
-        EaseRateAction *easeRateAction = new (std::nothrow) EaseRateAction();
-    if (easeRateAction && easeRateAction->initWithAction(action, rate))
+    void assertSupportedConfiguration(bool parametersSupported = true);
+    
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
+CVTS_FUNC(s32, f32, 8,
+    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
+    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta);,
+{
+    for (size_t i = 0; i < w; i += 8)
     {
-        easeRateAction->autorelease();
-        return easeRateAction;
+        internal::prefetch(_src + i);
+        __asm__ (
+            'vld1.32 {d4-d5}, [%[src1]]                             \n\t'
+            'vld1.32 {d6-d7}, [%[src2]]                             \n\t'
+            'vcvt.f32.s32 q4, q2                                    \n\t'
+            'vcvt.f32.s32 q5, q3                                    \n\t'
+            'vmul.f32 q6, q4, q0                                    \n\t'
+            'vmul.f32 q7, q5, q0                                    \n\t'
+            'vadd.f32 q8, q6, q1                                    \n\t'
+            'vadd.f32 q9, q7, q1                                    \n\t'
+            'vst1.32 {d16-d17}, [%[dst1]]                           \n\t'
+            'vst1.32 {d18-d19}, [%[dst2]]                           \n\t'
+            : /*no output*/
+            : [src1] 'r' (_src + i),
+              [src2] 'r' (_src + i + 4),
+              [dst1] 'r' (_dst + i),
+              [dst2] 'r' (_dst + i + 4),
+              'w'  (vscale), 'w' (vshift)
+           : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19'
+        );
     }
-    
-        r.endPosition = -_config.endPosition;
-    r.controlPoint_1 = _config.controlPoint_2 + (-_config.endPosition);
-    r.controlPoint_2 = _config.controlPoint_1 + (-_config.endPosition);
-    
-        /** Gets an action given its tag an a target.
-     *
-     * @param tag       The action's tag.
-     * @param target    A certain target.
-     * @return  The Action the with the given tag.
-     */
-    virtual Action* getActionByTag(int tag, const Node *target) const;
-    
-    /**
-@brief Progress from a percentage to another percentage.
-@since v0.99.1
-*/
-class CC_DLL ProgressFromTo : public ActionInterval
+})
+#else
+CVTS_FUNC(s32, f32, 8,
+    float32x4_t vscale = vdupq_n_f32((f32)alpha);
+    float32x4_t vshift = vdupq_n_f32((f32)beta);,
 {
-public:
-    /** 
-     * @brief Create and initializes the action with a duration, a 'from' percentage and a 'to' percentage.
-     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
-     * @param fromPercentage Specify the source percentage.
-     * @param toPercentage Specify the destination percentage.
-     * @return If the creation success, return a pointer of ProgressFromTo action; otherwise, return nil.
-     */
-    static ProgressFromTo* create(float duration, float fromPercentage, float toPercentage);
+    for (size_t i = 0; i < w; i += 8)
+    {
+        internal::prefetch(_src + i);
+        int32x4_t vline1_s32 = vld1q_s32(_src + i + 0);
+        int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
+        float32x4_t vline1_f32 = vcvtq_f32_s32(vline1_s32);
+        float32x4_t vline2_f32 = vcvtq_f32_s32(vline2_s32);
+        vline1_f32 = vmulq_f32(vline1_f32, vscale);
+        vline2_f32 = vmulq_f32(vline2_f32, vscale);
+        vline1_f32 = vaddq_f32(vline1_f32, vshift);
+        vline2_f32 = vaddq_f32(vline2_f32, vshift);
+        vst1q_f32(_dst + i + 0, vline1_f32);
+        vst1q_f32(_dst + i + 4, vline2_f32);
+    }
+})
+#endif
+    
+    f64 dotProduct(const Size2D &_size,
+               const f32 * src0Base, ptrdiff_t src0Stride,
+               const f32 * src1Base, ptrdiff_t src1Stride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    Size2D size(_size);
+    if (src0Stride == src1Stride &&
+        src0Stride == (ptrdiff_t)(size.width * sizeof(f32)))
+    {
+        size.width *= size.height;
+        size.height = 1;
+    }
     }
     
+                    ptrdiff_t* lidx0 = lane + x - 2*4;
+                ptrdiff_t* lidx1 = lane + x - 1*4;
+                ptrdiff_t* lidx3 = lane + x + 1*4;
+                ptrdiff_t* lidx4 = lane + x + 2*4;
     
+    namespace tesseract {
+    }
+    
+    #endif  // TESSERACT_ARCH_INTSIMDMATRIX_H_
+
+    
+    int os_detect(TO_BLOCK_LIST* port_blocks,
+              OSResults* osr,
+              tesseract::Tesseract* tess);
+    
+      /**
+   * Moves to the start of the next object at the given level in the
+   * page hierarchy, and returns false if the end of the page was reached.
+   * NOTE that RIL_SYMBOL will skip non-text blocks, but all other
+   * PageIteratorLevel level values will visit each non-text block once.
+   * Think of non text blocks as containing a single para, with a single line,
+   * with a single imaginary word.
+   * Calls to Next with different levels may be freely intermixed.
+   * This function iterates words in right-to-left scripts correctly, if
+   * the appropriate language has been loaded into Tesseract.
+   */
+  virtual bool Next(PageIteratorLevel level);
+    
+    struct BlobData {
+  BlobData() : blob(nullptr), choices(nullptr) {}
+  BlobData(int index, Tesseract* tess, const WERD_RES& word)
+    : blob(word.chopped_word->blobs[index]),
+      tesseract(tess),
+      choices(&(*word.ratings)(index, index)) {}
+    }
+    
+    #define VARDIR        'configs/' /*parameters files */
+#define MAX_ITEMS_IN_SUBMENU 30
+    
+    REGISTER_CPU_OPERATOR(EnforceFinite, EnforceFiniteOp<CPUContext>);
+    
+    workspace.FeedBlob('X', (np.random.uniform(-10, 10, (5,5))).astype(np.float32))
+print('X before running op:', workspace.FetchBlob('X'))
+workspace.RunOperatorOnce(op)
+print('X after running op:', workspace.FetchBlob('X'))
+    
+    REGISTER_CPU_OPERATOR(GatherRangesToDense, GatherRangesToDenseOp<CPUContext>);
+NO_GRADIENT(GatherRangesToDense);
+    
+    
+    {          return out;
+        });
+OPERATOR_SCHEMA(Float16ConstantFill)
+    .NumInputs(0)
+    .NumOutputs(1)
+    .TensorInferenceFunction(Float16FillerTensorInference)
+    .Arg('value', 'The value for the elements of the output tensor.')
+    .Arg('shape', 'The shape of the output tensor.')
+    .Output(
+        0,
+        'output',
+        'Output tensor of constant values specified by 'value'');
+    
+    grpc::string SecureAuthContext::GetPeerIdentityPropertyName() const {
+  if (!ctx_) {
+    return '';
+  }
+  const char* name = grpc_auth_context_peer_identity_property_name(ctx_);
+  return name == nullptr ? '' : name;
+}
+    
+    
+    { private:
+  grpc_auth_context* ctx_;
+  bool take_ownership_;
+};
+    
+    void ChannelArguments::SetSslTargetNameOverride(const grpc::string& name) {
+  SetString(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG, name);
+}
+    
+      static void OnDoneRecvMessageCb(void* user_data, grpc_error* error);
+    
+    inline absl::string_view GetMethod(const grpc_slice* path) {
+  if (GRPC_SLICE_IS_EMPTY(*path)) {
+    return '';
+  }
+  // Check for leading '/' and trim it if present.
+  return absl::StripPrefix(absl::string_view(reinterpret_cast<const char*>(
+                                                 GRPC_SLICE_START_PTR(*path)),
+                                             GRPC_SLICE_LENGTH(*path)),
+                           '/');
+}
+    
+    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
+    const grpc::string& name, int value) {
+  class IntOption final : public ServerBuilderOption {
+   public:
+    IntOption(const grpc::string& name, int value)
+        : name_(name), value_(value) {}
+    }
+    }
