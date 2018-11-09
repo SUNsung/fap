c27e7d8dcae96166d3893bdde7961e5294e881e0
SUNsung <weatherius@gmail.com>
2018-11-09 02:43:20 +0200

    Rewrite [58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb] File in: 2018-11-09 02:43:20

--- ruby/58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb
+++ ruby/58yyJlKlvbREg58UmTVuSHhf.582FWBMyACIM58mQF.rb
@@ -1,211 +1,26 @@
 
         
-                def initialize(object_name, method_name, template_object, checked_value, unchecked_value, options)
-          @checked_value   = checked_value
-          @unchecked_value = unchecked_value
-          super(object_name, method_name, template_object, options)
-        end
-    
-              def render_component(builder)
-            builder.radio_button + builder.label
-          end
+                [
+          'This will automatically tag your build with the following format: `<grouping>/<lane>/<prefix><build_number>`, where:'.markdown_preserve_newlines,
+          list,
+          'For example, for build 1234 in the 'appstore' lane, it will tag the commit with `builds/appstore/1234`.'
+        ].join('\n')
       end
+    
+        if ARGV.include?('--new-issue') || ARGV.switch?('n')
+      url = create_issue(f.tap, '#{f.name} failed to build on #{MacOS.full_version}', url)
+    end
+    
+            Generate Homebrew's manpages.
+      EOS
+      switch '--fail-if-changed',
+        description: 'Return a failing status code if changes are detected in the manpage outputs. This '\
+                     'can be used for CI to be notified when the manpages are out of date. Additionally, '\
+                     'the date used in new manpages will match those in the existing manpages (to allow '\
+                     'comparison without factoring in the date).'
+      switch '--link',
+        description: 'This is now done automatically by `brew update`.'
     end
   end
-end
-
     
-    module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class DateSelect < Base # :nodoc:
-        def initialize(object_name, method_name, template_object, options, html_options)
-          @html_options = html_options
-    
-            def render
-          options = @options.stringify_keys
-          options['size'] = options['maxlength'] unless options.key?('size')
-          options['type'] ||= field_type
-          options['value'] = options.fetch('value') { value_before_type_cast } unless field_type == 'file'
-          add_default_name_and_id(options)
-          tag('input', options)
-        end
-    
-        def render_partial(event)
-      info do
-        message = '  Rendered #{from_rails_root(event.payload[:identifier])}'.dup
-        message << ' within #{from_rails_root(event.payload[:layout])}' if event.payload[:layout]
-        message << ' (#{event.duration.round(1)}ms)'
-        message << ' #{cache_message(event.payload)}' unless event.payload[:cache_hit].nil?
-        message
-      end
-    end
-    
-    gem 'rails', '~> 5.2'
-gem 'omniauth', '~> 1.3'
-gem 'oauth2'
-gem 'omniauth-oauth2'
-gem 'rdoc'
-    
-      def translation_scope
-    'devise.omniauth_callbacks'
-  end
-end
-
-    
-    module Devise
-  module Controllers
-    # Provide the ability to store a location.
-    # Used to redirect back to a desired path after sign in.
-    # Included by default in all controllers.
-    module StoreLocation
-      # Returns and delete (if it's navigational format) the url stored in the session for
-      # the given scope. Useful for giving redirect backs after sign up:
-      #
-      # Example:
-      #
-      #   redirect_to stored_location_for(:user) || root_path
-      #
-      def stored_location_for(resource_or_scope)
-        session_key = stored_location_key_for(resource_or_scope)
-    
-          private
-    
-        export LANG=en_US.UTF-8
-    DOC
-  else
-    STDERR.puts <<-DOC
-    \e[33mWARNING: CocoaPods requires your terminal to be using UTF-8 encoding.
-    Consider adding the following to ~/.profile:
-    
-            def run
-          if @pod_name.nil?
-            # Note: at that point, @wipe_all is always true (thanks to `validate!`)
-            # Remove all
-            clear_cache
-          else
-            # Remove only cache for this pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            elsif cache_descriptors.count > 1 && !@wipe_all
-              # Ask which to remove
-              choices = cache_descriptors.map { |c| '#{@pod_name} v#{c[:version]} (#{pod_type(c)})' }
-              index = UI.choose_from_array(choices, 'Which pod cache do you want to remove?')
-              remove_caches([cache_descriptors[index]])
-            else
-              # Remove all found cache of this pod
-              remove_caches(cache_descriptors)
-            end
-          end
-        end
-    
-            private
-    
-    #{stack}
-#{executable_path}
-### Plugins
-    
-    Then(/^the failure task will not run$/) do
-  failed = TestApp.shared_path.join('failed')
-  expect { run_vagrant_command(test_file_exists(failed)) }
-    .to raise_error(VagrantHelpers::VagrantSSHCommandError)
-end
-    
-    module Capistrano
-  class ValidationError < RuntimeError; end
-    
-          def set(key, value=nil, &block)
-        @trusted_keys << key if trusted? && !@trusted_keys.include?(key)
-        remember_location(key)
-        values[key] = block || value
-        trace_set(key)
-        values[key]
-      end
-    
-      # install_list will be an array of [plugin name, version, options] tuples, version it
-  # can be nil at this point we know that plugins_arg is not empty and if the
-  # --version is specified there is only one plugin in plugins_arg
-  #
-  def install_gems_list!(install_list)
-    # If something goes wrong during the installation `LogStash::Gemfile` will restore a backup version.
-    install_list = LogStash::PluginManager.merge_duplicates(install_list)
-    
-        # remove 'system' local gems used by LS
-    local_gems = gemfile.locally_installed_gems.map(&:name) - NON_PLUGIN_LOCAL_GEMS
-    
-        context 'when is not a system pipeline' do
-      it 'returns false if the pipeline is not a system pipeline' do
-        expect(subject.system?).to be_falsey
-      end
-    end
-  end
-end
-
-    
-        platforms.types.each do |type|
-      desc 'Run acceptance test in #{type} machines'
-      task type do
-        ENV['LS_TEST_PLATFORM']=type
-        exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/*_spec.rb']]))
-      end
-    end
-    
-            def remove_braces_with_whitespace(corrector, node, space)
-          if node.multiline?
-            remove_braces_with_range(corrector,
-                                     left_whole_line_range(node.loc.begin),
-                                     right_whole_line_range(node.loc.end))
-          else
-            right_brace_and_space = right_brace_and_space(node.loc.end, space)
-            left_brace_and_space = left_brace_and_space(node.loc.begin, space)
-            remove_braces_with_range(corrector,
-                                     left_brace_and_space,
-                                     right_brace_and_space)
-          end
-        end
-    
-      it 'ignores empty hashes' do
-    expect_no_offenses('{}')
-  end
-    
-            def value_delta
-          return 0 if first.same_line?(second)
-          return 0 if keyword_splat?
-    
-    require 'erb'
-require 'digest'
-require 'tempfile'
-require 'paperclip/version'
-require 'paperclip/geometry_parser_factory'
-require 'paperclip/geometry_detector_factory'
-require 'paperclip/geometry'
-require 'paperclip/processor'
-require 'paperclip/processor_helpers'
-require 'paperclip/tempfile'
-require 'paperclip/thumbnail'
-require 'paperclip/interpolations/plural_cache'
-require 'paperclip/interpolations'
-require 'paperclip/tempfile_factory'
-require 'paperclip/style'
-require 'paperclip/attachment'
-require 'paperclip/storage'
-require 'paperclip/callbacks'
-require 'paperclip/file_command_content_type_detector'
-require 'paperclip/media_type_spoof_detector'
-require 'paperclip/content_type_detector'
-require 'paperclip/glue'
-require 'paperclip/errors'
-require 'paperclip/missing_attachment_styles'
-require 'paperclip/validators'
-require 'paperclip/logger'
-require 'paperclip/helpers'
-require 'paperclip/has_attached_file'
-require 'paperclip/attachment_registry'
-require 'paperclip/filename_cleaner'
-require 'paperclip/rails_environment'
-    
-        def make
-      geometry = GeometryParser.new(geometry_string.strip).make
-      geometry || raise(Errors::NotIdentifiedByImageMagickError.new)
-    end
+      def_delegator :@paths, :each
