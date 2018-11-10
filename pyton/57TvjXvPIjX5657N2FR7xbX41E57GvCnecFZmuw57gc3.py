17a4d199e0d7ae9b71b5f896b99f844659498b39
SUNsung <weatherius@gmail.com>
2018-11-10 20:11:21 +0200

    Rewrite [57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py] File in: 2018-11-10 20:11:20

--- pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
+++ pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
@@ -1,160 +1,81 @@
 
         
-        
-class PostgresSimpleLookup(Lookup):
-    def as_sql(self, qn, connection):
-        lhs, lhs_params = self.process_lhs(qn, connection)
-        rhs, rhs_params = self.process_rhs(qn, connection)
-        params = lhs_params + rhs_params
-        return '%s %s %s' % (lhs, self.operator, rhs), params
+            For complete documentation on using Sessions in your code, consult
+    the sessions documentation that is shipped with Django (also available
+    on the Django Web site).
+    '''
+    objects = SessionManager()
     
-        def __init__(self):
-        self.name = 'pgcrypto'
+        def close_all(self):
+        for alias in self:
+            try:
+                connection = getattr(self._connections, alias)
+            except AttributeError:
+                continue
+            connection.close()
     
+    # Read lines from the linkcheck output file
+try:
+    with open('build/linkcheck/output.txt') as out:
+        output_lines = out.readlines()
+except IOError:
+    print('linkcheck output not found; please run linkcheck first.')
+    exit(1)
     
-class ArrayMinLengthValidator(MinLengthValidator):
-    message = ngettext_lazy(
-        'List contains %(show_value)d item, it should contain no fewer than %(limit_value)d.',
-        'List contains %(show_value)d items, it should contain no fewer than %(limit_value)d.',
-        'limit_value')
+    # Check minimum required Python version
+import sys
+if sys.version_info < (2, 7):
+    print('Scrapy %s requires Python 2.7' % __version__)
+    sys.exit(1)
     
-        def save(self, must_create=False):
-        '''
-        Save the current session data to the database. If 'must_create' is
-        True, raise a database error if the saving operation doesn't create a
-        new entry (as opposed to possibly updating an existing entry).
-        '''
-        if self.session_key is None:
-            return self.create()
-        data = self._get_session(no_load=must_create)
-        obj = self.create_model_instance(data)
-        using = router.db_for_write(self.model, instance=obj)
-        try:
-            with transaction.atomic(using=using):
-                obj.save(force_insert=must_create, force_update=not must_create, using=using)
-        except IntegrityError:
-            if must_create:
-                raise CreateError
-            raise
-        except DatabaseError:
-            if not must_create:
-                raise UpdateError
-            raise
+        def print_items(self, lvl=None, colour=True):
+        if lvl is None:
+            items = [item for lst in self.items.values() for item in lst]
+        else:
+            items = self.items.get(lvl, [])
     
     
-class AbstractBaseSession(models.Model):
-    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
-    session_data = models.TextField(_('session data'))
-    expire_date = models.DateTimeField(_('expire date'), db_index=True)
-    
-        fpath = os.path.join(path, 'test')
-    x_test, y_test = load_batch(fpath, label_key=label_mode + '_labels')
-    
-    
-# Aliases.
-    
-    
-class MSE_MAE_loss:
-    '''Loss function with internal state, for testing serialization code.'''
-    def __init__(self, mse_fraction):
-        self.mse_fraction = mse_fraction
-    
-            h_tm1 = states[0]  # previous memory state
-        c_tm1 = states[1]  # previous carry state
-    
-            params = self._canonical_to_params(
-            weights=[
-                self.kernel_r,
-                self.kernel_z,
-                self.kernel_h,
-                self.recurrent_kernel_r,
-                self.recurrent_kernel_z,
-                self.recurrent_kernel_h,
-            ],
-            biases=[
-                self.bias_r_i,
-                self.bias_z_i,
-                self.bias_h_i,
-                self.bias_r,
-                self.bias_z,
-                self.bias_h,
-            ],
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding model 'ApiGrant'
+        db.create_table(
+            'sentry_apigrant', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'user', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.User']
+                    )
+                ), (
+                    'application', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.ApiApplication']
+                    )
+                ), (
+                    'code',
+                    self.gf('django.db.models.fields.CharField')(max_length=64, db_index=True)
+                ), ('expires_at', self.gf('django.db.models.fields.DateTimeField')(db_index=True)),
+                ('redirect_uri', self.gf('django.db.models.fields.CharField')(max_length=255)),
+                ('scopes', self.gf('django.db.models.fields.BigIntegerField')(default=None)),
+            )
         )
-        outputs, h = self._cudnn_gru(
-            inputs,
-            input_h=input_h,
-            params=params,
-            is_training=True)
+        db.send_create_signal('sentry', ['ApiGrant'])
     
-    from . import network
-from .training import Model
-from .base_layer import Layer
-from .input_layer import Input
-from .input_layer import InputLayer
-from .. import backend as K
-from .. import layers as layer_module
     
-    # Score trained model.
-scores = model.evaluate(x_test, y_test, verbose=1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
+class Migration(DataMigration):
+    def forwards(self, orm):
+        from sentry.utils.query import RangeQuerySetWrapperWithProgressBar
+    
+        complete_apps = ['sentry']
 
     
-        plt.figure('scikit-learn GLM benchmark results')
-    plt.xlabel('Dimensions')
-    plt.ylabel('Time (s)')
-    plt.plot(dimensions, time_ridge, color='r')
-    plt.plot(dimensions, time_ols, color='g')
-    plt.plot(dimensions, time_lasso, color='b')
-    
-    import numpy as np
-import gc
-from datetime import datetime
-from sklearn.isotonic import isotonic_regression
-from sklearn.utils.bench import total_seconds
-import matplotlib.pyplot as plt
-import argparse
-    
-                gc.collect()
-            print('benchmarking lars_path (without Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            lars_path(X, y, Gram=None, max_iter=n_informative)
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            lars[i_f, i_s] = delta
-    
-    def plot(func):
-    random_state = check_random_state(0)
-    one_core = []
-    multi_core = []
-    sample_sizes = range(1000, 6000, 1000)
-    
-    
-df = pd.DataFrame(res).set_index(['analyzer', 'ngram_range', 'vectorizer'])
-    
-        dim = start_dim
-    for i in range(0, n):
-        print('============================================')
-        print('Entering iteration %s of %s' % (i, n))
-        print('============================================')
-        dim += step
-        X = np.random.randn(100, dim)
-        Y = np.random.randint(0, n_classes, (100,))
-        bench_scikit_tree_classifier(X, Y)
-        Y = np.random.randn(100)
-        bench_scikit_tree_regressor(X, Y)
-    
-    
-print(':orphan:')
-print()
-heading = 'Available documentation for Scikit-learn'
-print(heading)
-print('=' * len(heading))
-print()
-print('Web-based documentation is available for versions listed below:')
-print()
-    
-    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-TRAIN_FOLDER = '20news-bydate-train'
-TEST_FOLDER = '20news-bydate-test'
+            # Adding field 'ReleaseFile.dist'
+        db.add_column(
+            'sentry_releasefile',
+            'dist',
+            self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['sentry.Distribution'], null=True
+            ),
+            keep_default=False
+        )
