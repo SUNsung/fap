5b12b1867da84d5d02f965bb62e51049c41dfb4c
SUNsung <weatherius@gmail.com>
2018-11-10 09:26:38 +0200

    Rewrite [22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb] File in: 2018-11-10 09:26:38

--- ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
+++ ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
@@ -1,174 +1,120 @@
 
         
-          protected
-    def after_resetting_password_path_for(resource)
-      Devise.sign_in_after_reset_password ? after_sign_in_path_for(resource) : new_session_path(resource_name)
-    end
-    
-            store.replace(path) do
-          new.build_pages do |page|
-            next unless store_page?(page)
-            store.write page[:store_path], page[:output]
-            index.add page[:entries]
-            pages.add page[:path], page[:output]
-          end
-    
-            if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-        def path
-      @path ||= url.path
-    end
-    
-          request_all initial_urls do |response|
-        next unless data = handle_response(response)
-        yield data
-        next unless data[:internal_urls].present?
-        next_urls = data[:internal_urls].select { |url| history.add?(url.downcase) }
-        instrument 'queued.scraper', urls: next_urls
-        next_urls
-      end
-    end
-    
-        def relative_path_to(url)
-      url = self.class.parse(url)
-      return unless origin == url.origin
-    
-            css('.filetree').each do |node|
-          node.content = node.css('.file').map(&:inner_html).join('\n')
-          node.name = 'pre'
-          node.remove_attribute('class')
-        end
-    
-      def page_params
-    { page: true, max_id: params[:max_id], min_id: params[:min_id] }.compact
-  end
+        platforms :ruby do
+  gem 'sqlite3'
 end
-
     
-          @user.resend_confirmation_instructions
-    
-      # Compile a Sass or SCSS string to CSS.
-  # Defaults to SCSS.
-  #
-  # @param contents [String] The contents of the Sass file.
-  # @param options [{Symbol => Object}] An options hash;
-  #   see {file:SASS_REFERENCE.md#Options the Sass options documentation}
-  # @raise [Sass::SyntaxError] if there's an error in the document
-  # @raise [Encoding::UndefinedConversionError] if the source encoding
-  #   cannot be converted to UTF-8
-  # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
-  def self.compile(contents, options = {})
-    options[:syntax] ||= :scss
-    Engine.new(contents, options).to_css
+        users.all?(&:blank?)
   end
     
-      # The lexical environment for SassScript.
-  # This keeps track of variable, mixin, and function definitions.
-  #
-  # A new environment is created for each level of Sass nesting.
-  # This allows variables to be lexically scoped.
-  # The new environment refers to the environment in the upper scope,
-  # so it has access to variables defined in enclosing scopes,
-  # but new variables are defined locally.
-  #
-  # Environment also keeps track of the {Engine} options
-  # so that they can be made available to {Sass::Script::Functions}.
-  class Environment < BaseEnvironment
-    # The enclosing environment,
-    # or nil if this is the global environment.
-    #
-    # @return [Environment]
-    attr_reader :parent
+      # Set the OmniAuth path prefix so it can be overridden when
+  # Devise is used in a mountable engine
+  mattr_accessor :omniauth_path_prefix
+  @@omniauth_path_prefix = nil
     
-        def path_for(file)
-      return file.path if file.is_a?(File)
-      return file if file.is_a?(String)
+              # Encodes the pvno field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_pvno
+            bn = OpenSSL::BN.new(pvno.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+                seq.to_der
+          end
+    
+              def self.attr_accessor(*vars)
+            @attributes ||= []
+            @attributes.concat vars
+            super(*vars)
+          end
+    
+              # Decodes the Rex::Proto::Kerberos::Model::EncKdcResponse from an input
+          #
+          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [self] if decoding succeeds
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode(input)
+            case input
+            when String
+              decode_string(input)
+            when OpenSSL::ASN1::ASN1Data
+              decode_asn1(input)
+            else
+              raise ::RuntimeError, 'Failed to decode EncKdcResponse, invalid input'
+            end
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of an encrypted message.
+        class EncryptedData < Element
+          # @!attribute name_type
+          #   @return [Integer] The encryption algorithm
+          attr_accessor :etype
+          # @!attribute kvno
+          #   @return [Integer] The version number of the key
+          attr_accessor :kvno
+          # @!attribute cipher
+          #   @return [String] The enciphered text
+          attr_accessor :cipher
+    
+                res = ''
+            case etype
+            when RSA_MD5
+              res = checksum_rsa_md5(data)
+            else
+              raise ::NotImplementedError, 'EncryptedData schema is not supported'
+            end
+    
+        def URIEncodePair(cc1, cc2, result, index)
+      u = ((cc1 >> 6) & 0xF) + 1;
+      w = (cc1 >> 2) & 0xF;
+      x = cc1 & 3;
+      y = (cc2 >> 6) & 0xF;
+      z = cc2 & 63;
+      octets = Array.new(4);
+      octets[0] = (u >> 2) + 240;
+      octets[1] = (((u & 3) << 4) | w) + 128;
+      octets[2] = ((x << 4) | y) + 128;
+      octets[3] = z + 128;
+      return URIEncodeOctets(octets, result, index);
     end
     
-          # If a full uri is passed, this removes the root from it
-      # otherwise returns the name unchanged
-      def remove_root(name)
-        if name.index(@root + '/') == 0
-          name[(@root.length + 1)..-1]
-        else
-          name
+          def string_to_code string
+        # sha bytes
+        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
+        # Thanks donpark's IdenticonUtil.java for this.
+        # Match the following Java code
+        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
+        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
+    
+          def remove_page_extentions(page_path)
+        Gollum::Markup.formats.values.each do |format|
+          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
         end
+        return page_path
       end
     
-        # From asking people, it seems MacPorts does not have a `prefix` command, like
-    # Homebrew does, so make an educated guess:
-    if port_prefix = prefix_from_bin('port')
-      prefixes << port_prefix
-    end
-    
+          def css # custom css
+        @css
       end
     
-      # Summary is used on the Archive pages to return the first block of content from a post.
-  def summary(input)
-    if input.index(/\n\n/)
-      input.split(/\n\n/)[0]
-    else
-      input
-    end
+        # TODO: Remove to_url once write_page changes are merged.
+    @wiki.write_page('ééééé'.to_url, :markdown, '한글 text', commit_details)
+    page = @wiki.page('eeeee')
+    assert_equal '한글 text', utf8(page.raw_data)
   end
     
-            def order_id
-          super || params[:id]
-        end
-      end
-    end
-  end
+    # Read command line options into `options` hash
+begin
+  opts.parse!
+rescue OptionParser::InvalidOption
+  puts 'gollum: #{$!.message}'
+  puts 'gollum: try 'gollum --help' for more information'
+  exit
 end
-
     
-            def update
-          @image = scope.images.accessible_by(current_ability, :update).find(params[:id])
-          if @image.update_attributes(image_params)
-            respond_with(@image, default_template: :show)
-          else
-            invalid_resource!(@image)
-          end
-        end
-    
-            def update
-          @option_type = Spree::OptionType.accessible_by(current_ability, :update).find(params[:id])
-          if @option_type.update_attributes(option_type_params)
-            render :show
-          else
-            invalid_resource!(@option_type)
-          end
-        end
-    
-            def new; end
-    
-            def current
-          @order = find_current_order
-          if @order
-            respond_with(@order, default_template: :show, locals: { root_object: @order })
-          else
-            head :no_content
-          end
-        end
-    
-            def update
-          if @property
-            authorize! :update, @property
-            @property.update_attributes(property_params)
-            respond_with(@property, status: 200, default_template: :show)
-          else
-            invalid_resource!(@property)
-          end
-        end
-    
-            def update
-          @return_authorization = order.return_authorizations.accessible_by(current_ability, :update).find(params[:id])
-          if @return_authorization.update_attributes(return_authorization_params)
-            respond_with(@return_authorization, default_template: :show)
-          else
-            invalid_resource!(@return_authorization)
-          end
-        end
+    # external
+require 'github/markup'
+require 'sanitize'
