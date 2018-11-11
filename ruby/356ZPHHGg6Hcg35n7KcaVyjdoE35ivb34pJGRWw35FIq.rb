f2357b74c47a5f36507cd0cd95ac1f73d872859e
SUNsung <weatherius@gmail.com>
2018-11-11 12:28:56 +0200

    Rewrite [356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb] File in: 2018-11-11 12:28:55

--- ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
+++ ruby/356ZPHHGg6Hcg35n7KcaVyjdoE35ivb34pJGRWw35FIq.rb
@@ -1,16 +1,44 @@
 
         
-                # Set the name of the plugin. The moment that this is called, the
-        # plugin will be registered and available. Before this is called, a
-        # plugin does not exist. The name must be unique among all installed
-        # plugins.
+        end
+
+    
+      describe '.seed' do
+    it 'imports a set of agents to get the user going when they are first created' do
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
+    
+        it 'outputs a structure containing name, description, the date, all agents & their links' do
+      data = exporter.as_json
+      expect(data[:name]).to eq(name)
+      expect(data[:description]).to eq(description)
+      expect(data[:source_url]).to eq(source_url)
+      expect(data[:guid]).to eq(guid)
+      expect(data[:schema_version]).to eq(1)
+      expect(data[:tag_fg_color]).to eq(tag_fg_color)
+      expect(data[:tag_bg_color]).to eq(tag_bg_color)
+      expect(data[:icon]).to eq(icon)
+      expect(Time.parse(data[:exported_at])).to be_within(2).of(Time.now.utc)
+      expect(data[:links]).to eq([{ :source => guid_order(agent_list, :jane_weather_agent), :receiver => guid_order(agent_list, :jane_rain_notifier_agent)}])
+      expect(data[:control_links]).to eq([])
+      expect(data[:agents]).to eq(agent_list.sort_by{|a| a.guid}.map { |agent| exporter.agent_as_json(agent) })
+      expect(data[:agents].all? { |agent_json| agent_json[:guid].present? && agent_json[:type].present? && agent_json[:name].present? }).to be_truthy
+    
+        it 'ignores unknown types' do
+      Agent.where(id: agents(:bob_weather_agent).id).update_all type: 'UnknownTypeAgent'
+      mock(Agents::WeatherAgent).bulk_check('midnight').once
+      mock(Agents::WebsiteAgent).bulk_check('midnight').once
+      Agent.run_schedule('midnight')
+    end
+    
+            # Checks if the target machine is ready for communication. If this
+        # returns true, then all the other methods for communicating with
+        # the machine are expected to be functional.
         #
-        # @param [String] name Name of the plugin.
-        # @return [String] The name of the plugin.
-        def self.name(name=UNSET_VALUE)
-          # Get or set the value first, so we have a name for logging when
-          # we register.
-          result = get_or_set(:name, name)
+        # @return [Boolean]
+        def ready?
+          false
+        end
     
             # This is the method called to when the system is being destroyed
         # and allows the provisioners to engage in any cleanup tasks necessary.
@@ -22,43 +50,101 @@
 end
 
     
-    describe 'Kernel.sleep' do
-  it 'needs to be reviewed for spec completeness'
+            # This is an internal initialize function that should never be
+        # overridden. It is used to initialize some common internal state
+        # that is used in a provider.
+        def _initialize(name, machine)
+          initialize_capabilities!(
+            name.to_sym,
+            { name.to_sym => [Class.new, nil] },
+            Vagrant.plugin('2').manager.provider_capabilities,
+            machine,
+          )
+        end
+      end
+    end
+  end
 end
 
     
-      it 'sets the return value of the catch block to a value specified as second parameter' do
-    res = catch :blah do
-      throw :blah, :return_value
+        def URIAddEncodedOctetToBuffer(octet, result, index)
+      result[index] = 37; # Char code of '%'.
+      index         += 1
+      result[index] = @@hexCharCodeArray[octet >> 4];
+      index         += 1
+      result[index] = @@hexCharCodeArray[octet & 0x0F];
+      index += 1
+      return index;
     end
-    res.should == :return_value
-  end
     
-      it 'creates a public method in script binding' do
-    eval @code, script_binding
-    Object.should have_method :boom
-  end
-    
-    require 'sass/engine'
-require 'sass/plugin' if defined?(Merb::Plugins)
-require 'sass/railtie'
-require 'sass/features'
+    # Commit file to wiki, overwriting previous versions of that file
+def commit_test_file(wiki, dir, filename, ext, content)
+  committer = Gollum::Committer.new(wiki, :message => 'Added testfile', :parent  => wiki.repo.head.commit)
+  committer.add_to_index(dir, filename, ext, content, true)
+    committer.after_commit do |committer, sha|
+      wiki.clear_cache
+      committer.update_working_dir(dir, filename, ext)
+    end
+  committer.commit
+end
 
     
-          private
+        # must clear or create_msg will be returned
+    @wiki.clear_cache
     
-        # Parses the command-line arguments and runs the executable.
-    # Calls `Kernel#exit` at the end, so it never returns.
-    #
-    # @see #parse
-    def parse!
-      # rubocop:disable RescueException
-      begin
-        parse
-      rescue Exception => e
-        # Exit code 65 indicates invalid data per
-        # http://www.freebsd.org/cgi/man.cgi?query=sysexits. Setting it via
-        # at_exit is a bit of a hack, but it allows us to rethrow when --trace
-        # is active and get both the built-in exception formatting and the
-        # correct exit code.
-        at_exit {exit Sass::Util.windows? ? 13 : 65} if e.is_a?(Sass::SyntaxError)
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
+  end
+    
+        EMOJI_PATHNAME = Pathname.new(Gemojione.images_path).freeze
+    
+      s.add_dependency 'gollum-lib', '~> 4.2', '>= 4.2.10'
+  s.add_dependency 'kramdown', '~> 1.9.0'
+  s.add_dependency 'sinatra', '~> 1.4', '>= 1.4.4'
+  s.add_dependency 'mustache', ['>= 0.99.5', '< 1.0.0']
+  s.add_dependency 'useragent', '~> 0.16.2'
+  s.add_dependency 'gemojione', '~> 3.2'
+    
+        context 'but no comment after the last element' do
+      it 'autocorrects the closing brace' do
+        new_source = autocorrect_source(source)
+    
+          # Checks whether this case statement has an `else` branch.
+      #
+      # @return [Boolean] whether the `case` statement has an `else` branch
+      def else?
+        loc.else
+      end
+    end
+  end
+end
+
+    
+          # Checks whether this node body is a void context.
+      #
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
+      end
+    
+          # Checks whether this node is an `unless` statement. (This is not true
+      # of ternary operators and `if` statements.)
+      #
+      # @return [Boolean] whether the node is an `unless` statement
+      def unless?
+        keyword == 'unless'
+      end
+    
+          # Checks whether this `hash` element is on the same line as `other`.
+      #
+      # @note A multiline element is considered to be on the same line if it
+      #       shares any of its lines with `other`
+      #
+      # @return [Boolean] whether this element is on the same line as `other`
+      def same_line?(other)
+        loc.last_line == other.loc.line || loc.line == other.loc.last_line
+      end
