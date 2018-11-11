be2665f0785703e6db915cce552c581140b3c94e
SUNsung <weatherius@gmail.com>
2018-11-12 00:29:27 +0200

    Rewrite [275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb] File in: 2018-11-12 00:29:27

--- ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
+++ ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
@@ -1,43 +1,150 @@
 
         
-                      Time.utc(
-                default[:year], default[:month], default[:day],
-                default[:hour], default[:min], default[:sec]
-              )
-            end
-          end
+        Then(%r!^I should see exactly '(.*)' in '(.*)'$!) do |text, file|
+  step %(the '#{file}' file should exist)
+  expect(file_contents(file).strip).to eq text
+end
+    
+              theme.create!
+          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
+                             ' is ready for you in #{theme.path.to_s.cyan}!'
+          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
+        end
+        # rubocop:enable Metrics/AbcSize
       end
     end
   end
 end
 
     
-        # Main render entry point shared by Action View and Action Controller.
-    def render(context, options)
-      if options.key?(:partial)
-        render_partial(context, options)
-      else
-        render_template(context, options)
-      end
+    module Jekyll
+  module Converters
+    class Markdown
+      class KramdownParser
+        CODERAY_DEFAULTS = {
+          'css'               => 'style',
+          'bold_every'        => 10,
+          'line_numbers'      => 'inline',
+          'line_number_start' => 1,
+          'tab_width'         => 4,
+          'wrap'              => 'div',
+        }.freeze
+    
+            cmd << ['-am #{message.shellescape}']
+        cmd << '--force' if options[:force]
+        cmd << '-s' if options[:sign]
+        cmd << tag.shellescape
+        cmd << options[:commit].to_s if options[:commit]
+    
+            context 'when command is bootstrap' do
+          let(:command) { 'bootstrap' }
+    
+          it 'works with single quote in rule name' do
+        rule = 'CoveredSwitchStatementsDon'tNeedDefault'
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+              enable_rules: [\'#{rule}\'],
+              disable_rules: [\'#{rule}\']
+            )
+          end').runner.execute(:test)
+    
+              # Override the already overridden swiftlint_version method to check
+          # that the correct exectuable is being passed in as a parameter.
+          allow(Fastlane::Actions::SwiftlintAction).to receive(:swiftlint_version) { |params|
+            expect(params[:executable]).to eq(CUSTOM_EXECUTABLE_NAME)
+            swiftlint_gem_version
+          }
+    
+              expect(value).to eq(nil)
+        end
+    
+    shelljoin_testcases = [
+  {
+    'it' => '(#1) on array with entry with space',
+    'it_result' => {
+      'windows' => 'wraps this entry in double quotes',
+      'other'   => 'escapes the space in this entry'
+    },
+    'input' => ['a', 'b c', 'd'],
+    'expect' => {
+      'windows' => 'a 'b c' d',
+      'other'   => 'a b\ c d'
+    }
+  },
+  {
+    'it' => '(#2) on array with entry with string wrapped in double quotes and space',
+    'it_result' => {
+      'windows' => 'wraps the entry with space in quote, and doubles the double quotes',
+      'other'   => 'escapes the double quotes and escapes the space'
+    },
+    'input' => ['a', ''b' c', 'd'],
+    'expect' => {
+      'windows' => 'a '''b'' c' d',
+      'other'   => 'a \'b\'\ c d'
+    }
+  },
+  {
+    'it' => '(#3) on array with entry with string wrapped in single quotes and space',
+    'it_result' => {
+      'windows' => 'no changes',
+      'other'   => 'escapes the single quotes and space'
+    },
+    'input' => ['a', ''b' c', 'd'],
+    'expect' => {
+      'windows' => 'a \''b' c\' d',
+      'other'   => 'a \\'b\\'\\ c d'
+    }
+  },
+  # https://github.com/ruby/ruby/blob/ac543abe91d7325ace7254f635f34e71e1faaf2e/test/test_shellwords.rb#L67-L68
+  {
+    'it' => '(#4) on array with entry that is `$$`',
+    'it_result' => {
+      'windows' => 'the result includes the process id',
+      'other'   => 'the result includes the process id'
+    },
+    'input' => ['ps', '-p', $$],
+    'expect' => {
+      'windows' => 'ps -p #{$$}',
+      'other'   => 'ps -p #{$$}'
+    }
+  }
+]
+    
+      def_delegators :@resource, :stage, :fetch, :verify_download_integrity, :source_modified_time
+  def_delegators :@resource, :download_name, :cached_download, :clear_cache
+  def_delegators :@resource, :checksum, :mirrors, :specs, :using
+  def_delegators :@resource, :version, :mirror, *Checksum::TYPES
+  def_delegators :@resource, :downloader
+    
+      config.around(:each) do |example|
+    def find_files
+      Find.find(TEST_TMPDIR)
+          .reject { |f| File.basename(f) == '.DS_Store' }
+          .map { |f| f.sub(TEST_TMPDIR, '') }
     end
     
-      def resource_params
-    params.require(:custom_filter).permit(:phrase, :expires_in, :irreversible, :whole_word, context: [])
-  end
-end
-
-    
-    module Admin
-  class ConfirmationsController < BaseController
-    before_action :set_user
-    before_action :check_confirmation, only: [:resend]
-    
-        def destroy
-      authorize @report_note, :destroy?
-      @report_note.destroy!
-      redirect_to admin_report_path(@report_note.report_id), notice: I18n.t('admin.report_notes.destroyed_msg')
+      def up_down(change)
+    change.up do
+      Mention.update_all(mentions_container_type: 'Post')
+      change_column :mentions, :mentions_container_type, :string, null: false
+      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
     end
     
-      def hub_callback
-    params['hub.callback']
-  end
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+      describe '#new' do
+    before do
+      sign_in alice, scope: :user
+    end
+    
+          delete :destroy, params: {post_id: @message.id, id: @like.id}, format: :json
+      expect(response.status).to eq(204)
+    end
+    
+          expect(Notification.where(unread: true).count).to eq(2)
+      get :read_all
+      expect(Notification.where(unread: true).count).to eq(0)
+    end
