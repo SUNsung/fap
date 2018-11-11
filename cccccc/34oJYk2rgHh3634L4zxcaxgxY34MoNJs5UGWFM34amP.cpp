6f2338493c71f2dc6a079b8aa383d77aac08350f
SUNsung <weatherius@gmail.com>
2018-11-11 18:35:26 +0200

    Rewrite [34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp] File in: 2018-11-11 18:35:25

--- cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
+++ cccccc/34oJYk2rgHh3634L4zxcaxgxY34MoNJs5UGWFM34amP.cpp
@@ -1,250 +1,213 @@
 
         
-        // Base class for a SIMD function to multiply a matrix by a vector, with sources
-// of 8-bit signed integer, and result in a double, after appropriate scaling.
-// Assumes a specific method of multiplication that can be applied to any size
-// and number of SIMD registers as follows:
-// int32_t results are computed with num_outputs_per_register_ in each of
-// max_output_registers_ result registers, repeatedly until it would make too
-// many results, then the number of registers is halved, and so-on down to a
-// single result register. The last calculation only outputs the required number
-// of results instead of writing beyond the bounds. Eg: matrix has 75 outputs,
-//  num_outputs_per_register_ = 4, and max_output_registers_ = 8,
-// Step 1: 8x4=32 results are computed,
-// Step 2: 8x4=32 again, total 64,
-// Step 3: 2x4=8 (since 8x4 is too many, so is 4x4), total 72,
-// Step 4: 1x3, total 75.
-// Each step above is computed using a PartialFunc, which runs over the input
-// vector once. The input is read one registerful of num_inputs_per_register_
-// at a time (presumably 4x num_outputs_per_register_ since they are int8_t)
-// so the inputs MUST BE PADDED to a multiple of num_inputs_per_register_.
-// Since it is slow (on Intel at least) to horizontally add in a register,
-// provision is made to process num_inputs_per_group_ inputs at a time, with
-// the group being replicated num_input_groups_ times and multiplied by a
-// num_inputs_per_group_ by num_input_groups_ rectangle of the weights matrix.
-// This is most convenient if num_inputs_per_group_ is 4, and the product
-// sign-extends and sums 8x8=16 bit results to 32 bits, adding 4 adjacent
-// results in the process, but it doesn't have to be implemented that way.
-// The weights are re-ordered by Init() to be used sequentially by the above
-// algorithm, followed by the biases, so they can be added at the end.
-// The base class computes the base C++ implementation.
-// NOTE that, although the subclasses execute on different SIMD hardware, no
-// virtual methods are needed, as the constructor sets up everything that
-// is required to allow the base class implementation to do all the work.
-class IntSimdMatrix {
- public:
-  // Constructor should set the data members to indicate the sizes.
-  // NOTE: Base constructor public only for test purposes.
-  IntSimdMatrix()
-      : num_outputs_per_register_(1),
-        max_output_registers_(1),
-        num_inputs_per_register_(1),
-        num_inputs_per_group_(1),
-        num_input_groups_(1) {}
-    }
-    
-    #endif
-
-    
-    
-/**********************************************************************
- * split_and_recog_word
- *
- * Split the word into 2 smaller pieces at the largest gap.
- * Recognize the pieces and stick the results back together.
- **********************************************************************/
-void Tesseract::split_and_recog_word(WERD_RES *word) {
-  // Find the biggest blob gap in the chopped_word.
-  int bestgap = -INT32_MAX;
-  int split_index = 0;
-  for (int b = 1; b < word->chopped_word->NumBlobs(); ++b) {
-    TBOX prev_box = word->chopped_word->blobs[b - 1]->bounding_box();
-    TBOX blob_box = word->chopped_word->blobs[b]->bounding_box();
-    int gap = blob_box.left() - prev_box.right();
-    if (gap > bestgap) {
-      bestgap = gap;
-      split_index = b;
-    }
+        
+    {  std::vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'SubGradient',
+        '',
+        std::vector<std::string>{GO(0), I(0), I(1)},
+        std::vector<std::string>{GI(0), GI(1)});
   }
-  ASSERT_HOST(split_index > 0);
+};
+    
+    Example 1:
+  DATA  = [1, 2, 3, 4, 5, 6, 7, 8]
+  RANGES = [
+    [
+      [2, 4],
+      [0, 2],
+    ],
+    [
+      [0, 0],
+      [6, 2],
+    ]
+  ]
+  lengths = [4, 2]
+  OUTPUT[0] = [[3, 4, 5, 6], [0, 0, 0, 0]]
+  OUTPUT[1] = [[1, 2], [7, 8]]
+    
+        /** World boundaries. */
+    float _leftBoundary;
+    float _rightBoundary;
+    float _topBoundary;
+    float _bottomBoundary;
+    
+    /** Horizontal (x) and vertical (y) offset values. */
+    float _offsetX;
+    float _offsetY;
+    
+    Rect _worldRect;
+    
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+    /** @class ActionInstant
+* @brief Instant actions are immediate actions. They don't have a duration like the IntervalAction actions.
+**/
+class CC_DLL ActionInstant : public FiniteTimeAction
+{
+public:
+    //
+    // Overrides
+    //
+    virtual ActionInstant* clone() const override
+    {
+        CC_ASSERT(0);
+        return nullptr;
     }
     
-      // Gets a pix that contains an 8 bit threshold value at each pixel. The
-  // returned pix may be an integer reduction of the binary image such that
-  // the scale factor may be inferred from the ratio of the sizes, even down
-  // to the extreme of a 1x1 pixel thresholds image.
-  // Ideally the 8 bit threshold should be the exact threshold used to generate
-  // the binary image in ThresholdToPix, but this is not a hard constraint.
-  // Returns nullptr if the input is binary. PixDestroy after use.
-  virtual Pix* GetPixRectThresholds();
-    
-    // Gets the content of the stringstream's buffer as an std::string.  Each '\0'
-// character in the buffer is replaced with '\\0'.
-GTEST_API_ std::string StringStreamToString(::std::stringstream* stream);
-    
-      explicit tuple(GTEST_BY_REF_(T0) f0, GTEST_BY_REF_(T1) f1,
-      GTEST_BY_REF_(T2) f2, GTEST_BY_REF_(T3) f3, GTEST_BY_REF_(T4) f4,
-      GTEST_BY_REF_(T5) f5, GTEST_BY_REF_(T6) f6, GTEST_BY_REF_(T7) f7,
-      GTEST_BY_REF_(T8) f8, GTEST_BY_REF_(T9) f9) : f0_(f0), f1_(f1), f2_(f2),
-      f3_(f3), f4_(f4), f5_(f5), f6_(f6), f7_(f7), f8_(f8), f9_(f9) {}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-      std::vector<std::thread> workers;
-  for (auto worker = size_t{0}; worker < num_threads; ++worker) {
-    workers.push_back(std::thread([&] {
-      try {
-        hphp_thread_init();
-        hphp_session_init(Treadmill::SessionKind::HHBBC);
-        SCOPE_EXIT {
-          hphp_context_exit();
-          hphp_session_exit();
-          hphp_thread_exit();
-        };
-    }
+    virtual ActionInstant * reverse() const override
+    {
+        CC_ASSERT(0);
+        return nullptr;
     }
     }
     
-    
-    {}
-    
-    void Assembler::lhz(const Reg64& rt, MemoryRef m) {
-  assertx(Reg64(-1) == m.r.index);  // doesn't support base+index
-  EmitDForm(40, rn(rt), rn(m.r.base), m.r.disp);
-}
-    
-      /**
-   * Dump detailed information to return string.
-   */
-  virtual String debuggerDump() {
-    return String();
-  }
-    
-    /*
- * ExtendedException is the exception type for C++ exceptions that carry PHP
- * stack traces, but do not represent user-visible PHP exception objects.
- *
- * This class should probably eventually be merged with FatalErrorException;
- * for now it's still here for historical reasons, though.
- *
- * You generally should not have to add new subclasses of these Exception types
- * in extension code---normally you want to go through the raise_error
- * machinery.
- */
-struct ExtendedException : Exception {
-  enum class SkipFrame {};
+    void TintBy::startWithTarget(Node *target)
+{
+    ActionInterval::startWithTarget(target);
     }
     
-    #define ERROR_RAISE_WARNING(exp)        \
-  int ret = (exp);                      \
-  if (ret != 0) {                       \
-    raise_warning(                      \
-      '%s(): %s',                       \
-      __FUNCTION__,                     \
-      folly::errnoStr(errno).c_str()    \
-    );                                  \
-  }                                     \
-    
-    #endif
-
-    
-    
-    {///////////////////////////////////////////////////////////////////////////////
-}
-
-    
-    #include <string>
-#include <vector>
-    
+        //
+    // Overrides
+    //
+    virtual TintTo* clone() const override;
+    virtual TintTo* reverse() const override;
+    virtual void startWithTarget(Node *target) override;
     /**
- * @brief Parser plugin for logger configurations.
- */
-class LoggerConfigParserPlugin : public ConfigParserPlugin {
- public:
-  std::vector<std::string> keys() const override {
-    return {kLoggerKey};
-  }
-    }
+     * @param time In seconds.
+     */
+    virtual void update(float time) override;
     
-    #include <osquery/config.h>
-#include <osquery/registry.h>
+CC_CONSTRUCTOR_ACCESS:
+    TintTo() {}
+    virtual ~TintTo() {}
     
-    std::shared_ptr<PlatformProcess> PlatformProcess::launchWorker(
-    const std::string& exec_path, int argc /* unused */, char** argv) {
-  auto worker_pid = ::fork();
-  if (worker_pid < 0) {
-    return std::shared_ptr<PlatformProcess>();
-  } else if (worker_pid == 0) {
-    setEnvVar('OSQUERY_WORKER', std::to_string(::getpid()).c_str());
-    ::execve(exec_path.c_str(), argv, ::environ);
-    }
-    }
-    
-    TEST_F(QueryTests, test_get_query_results) {
-  // Grab an expected set of query data and add it as the previous result.
-  auto encoded_qd = getSerializedQueryDataJSON();
-  auto query = getOsqueryScheduledQuery();
-  auto status = setDatabaseValue(kQueries, 'foobar', encoded_qd.first);
-  EXPECT_TRUE(status.ok());
-    }
+        /** Removes all actions matching at least one bit in flags and the target.
+     *
+     * @param flags     The flag field to match the actions' flags based on bitwise AND.
+     * @param target    A certain target.
+     * @js NA
+     */
+    virtual void removeActionsByFlags(unsigned int flags, Node *target);
     
     
-    {  for (const auto& category : doc.doc()['exclude_paths'].GetObject()) {
-    for (const auto& excl_path : category.value.GetArray()) {
-      std::string pattern = excl_path.GetString();
-      if (pattern.empty()) {
-        continue;
-      }
-      exclude_paths_.insert(pattern);
-    }
-  }
+    {    return false;
 }
     
-      QueryData generate(QueryContext& request) {
-    QueryData results;
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __CCACTIONTWEEN_H__
+#define __CCACTIONTWEEN_H__
+    
+    
+    {// end of sprite_nodes group
+/// @}
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    
+    {        // Create texture view
+        D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc;
+        ZeroMemory(&srvDesc, sizeof(srvDesc));
+        srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+        srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+        srvDesc.Texture2D.MipLevels = desc.MipLevels;
+        srvDesc.Texture2D.MostDetailedMip = 0;
+        g_pd3dDevice->CreateShaderResourceView(pTexture, &srvDesc, &g_pFontTextureView);
+        pTexture->Release();
     }
     
-    #define BENCHMARK_PRIVATE_DECLARE_F(BaseClass, Method)        \
-  class BaseClass##_##Method##_Benchmark : public BaseClass { \
-   public:                                                    \
-    BaseClass##_##Method##_Benchmark() : BaseClass() {        \
-      this->SetName(#BaseClass '/' #Method);                  \
-    }                                                         \
-                                                              \
-   protected:                                                 \
-    virtual void BenchmarkCase(::benchmark::State&);          \
-  };
+        // Bind shader and vertex buffers
+    unsigned int stride = sizeof(ImDrawVert);
+    unsigned int offset = 0;
+    D3D12_VERTEX_BUFFER_VIEW vbv;
+    memset(&vbv, 0, sizeof(D3D12_VERTEX_BUFFER_VIEW));
+    vbv.BufferLocation = g_pVB->GetGPUVirtualAddress() + offset;
+    vbv.SizeInBytes = g_VertexBufferSize * stride;
+    vbv.StrideInBytes = stride;
+    ctx->IASetVertexBuffers(0, 1, &vbv);
+    D3D12_INDEX_BUFFER_VIEW ibv;
+    memset(&ibv, 0, sizeof(D3D12_INDEX_BUFFER_VIEW));
+    ibv.BufferLocation = g_pIB->GetGPUVirtualAddress();
+    ibv.SizeInBytes = g_IndexBufferSize * sizeof(ImDrawIdx);
+    ibv.Format = sizeof(ImDrawIdx) == 2 ? DXGI_FORMAT_R16_UINT : DXGI_FORMAT_R32_UINT;
+    ctx->IASetIndexBuffer(&ibv);
+    ctx->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+    ctx->SetPipelineState(g_pPipelineState);
+    ctx->SetGraphicsRootSignature(g_pRootSignature);
+    ctx->SetGraphicsRoot32BitConstants(0, 16, &vertex_constant_buffer, 0);
     
-    // That gcc wants both of these prototypes seems mysterious. VC, for
-// its part, can't decide which to use (another mystery). Matching of
-// template overloads: the final frontier.
-#ifndef COMPILER_MSVC
-template <typename T, size_t N>
-char (&ArraySizeHelper(const T (&array)[N]))[N];
-#endif
+        ALLEGRO_MOUSE_STATE mouse;
+    if (keys.display == g_Display)
+    {
+        al_get_mouse_state(&mouse);
+        io.MousePos = ImVec2((float)mouse.x, (float)mouse.y);
+    }
+    else
+    {
+        io.MousePos = ImVec2(-FLT_MAX, -FLT_MAX);
+    }
     
-    #include <cstdarg>
-#include <iostream>
-#include <string>
+    */
     
-      // Calculate least square fitting parameter
-  for (size_t i = 0; i < n.size(); ++i) {
-    double gn_i = fitting_curve(n[i]);
-    sigma_gn += gn_i;
-    sigma_gn_squared += gn_i * gn_i;
-    sigma_time += time[i];
-    sigma_time_gn += time[i] * gn_i;
-  }
+    // If you get an error please report on github. You may try different GL context version or GLSL version. See GL<>GLSL version table at the top of this file.
+static bool CheckShader(GLuint handle, const char* desc)
+{
+    GLint status = 0, log_length = 0;
+    glGetShaderiv(handle, GL_COMPILE_STATUS, &status);
+    glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &log_length);
+    if ((GLboolean)status == GL_FALSE)
+        fprintf(stderr, 'ERROR: ImGui_ImplOpenGL3_CreateDeviceObjects: failed to compile %s!\n', desc);
+    if (log_length > 0)
+    {
+        ImVector<char> buf;
+        buf.resize((int)(log_length + 1));
+        glGetShaderInfoLog(handle, log_length, NULL, (GLchar*)buf.begin());
+        fprintf(stderr, '%s\n', buf.begin());
+    }
+    return (GLboolean)status == GL_TRUE;
+}
     
-    #ifdef BENCHMARK_OS_EMSCRIPTEN
-#include <emscripten.h>
-#endif
     
-    #endif
-
+    {    // [Internal] Masks
+    ImGuiColorEditFlags__InputsMask     = ImGuiColorEditFlags_RGB|ImGuiColorEditFlags_HSV|ImGuiColorEditFlags_HEX,
+    ImGuiColorEditFlags__DataTypeMask   = ImGuiColorEditFlags_Uint8|ImGuiColorEditFlags_Float,
+    ImGuiColorEditFlags__PickerMask     = ImGuiColorEditFlags_PickerHueWheel|ImGuiColorEditFlags_PickerHueBar,
+    ImGuiColorEditFlags__OptionsDefault = ImGuiColorEditFlags_Uint8|ImGuiColorEditFlags_RGB|ImGuiColorEditFlags_PickerHueBar    // Change application default using SetColorEditOptions()
+};
     
-      // Compile a regular expression matcher from spec.  Returns true on success.
-  //
-  // On failure (and if error is not nullptr), error is populated with a human
-  // readable error message if an error occurs.
-  bool Init(const std::string& spec, std::string* error);
+        void    Draw(const char* title, bool* p_open)
+    {
+        ImGui::SetNextWindowSize(ImVec2(520,600), ImGuiCond_FirstUseEver);
+        if (!ImGui::Begin(title, p_open))
+        {
+            ImGui::End();
+            return;
+        }
+    }
+    
+    // The aim of imgui_impl_vulkan.h/.cpp is to be usable in your engine without any modification. 
+// IF YOU FEEL YOU NEED TO MAKE ANY CHANGE TO THIS CODE, please share them and your feedback at https://github.com/ocornut/imgui/
+    
+    IMGUI_IMPL_API bool     ImGui_ImplDX10_Init(ID3D10Device* device);
+IMGUI_IMPL_API void     ImGui_ImplDX10_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplDX10_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data);
+    
+    // Callbacks (installed by default if you enable 'install_callbacks' during initialization)
+// You can also handle inputs yourself and use those as a reference.
+IMGUI_IMPL_API int32    ImGui_Marmalade_PointerButtonEventCallback(void* system_data, void* user_data);
+IMGUI_IMPL_API int32    ImGui_Marmalade_KeyCallback(void* system_data, void* user_data);
+IMGUI_IMPL_API int32    ImGui_Marmalade_CharCallback(void* system_data, void* user_data);
