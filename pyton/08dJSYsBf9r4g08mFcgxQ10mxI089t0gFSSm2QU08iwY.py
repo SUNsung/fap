48d11551320962b26f472c81c705ce6007f354f5
SUNsung <weatherius@gmail.com>
2018-11-11 22:52:57 +0200

    Rewrite [08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py] File in: 2018-11-11 22:52:56

--- pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
+++ pyton/08dJSYsBf9r4g08mFcgxQ10mxI089t0gFSSm2QU08iwY.py
@@ -1,146 +1,146 @@
 
         
-            def __init__(self, categorizer):
-        self.categorizer = categorizer
-        ...
+        FISH_COMPLETION_FILE = 'youtube-dl.fish'
+FISH_COMPLETION_TEMPLATE = 'devscripts/fish-completion.in'
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
+        out = bug_text + dev_text
     
-        def iter_lines(self, chunk_size):
-        '''Return an iterator over the body yielding (`line`, `line_feed`).'''
-        raise NotImplementedError()
+    import itertools
+import json
+import os
+import re
+import sys
     
-        def format_headers(self, headers):
-        '''Return processed `headers`
+        params = {
+        'age_limit': age,
+        'skip_download': True,
+        'writeinfojson': True,
+        'outtmpl': '%(id)s.%(ext)s',
+    }
+    ydl = YoutubeDL(params)
+    ydl.add_default_info_extractors()
+    json_filename = os.path.splitext(filename)[0] + '.info.json'
+    try_rm(json_filename)
+    ydl.download([url])
+    res = os.path.exists(json_filename)
+    try_rm(json_filename)
+    return res
     
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
+        def test_youtube_feeds(self):
+        self.assertMatch('https://www.youtube.com/feed/watch_later', ['youtube:watchlater'])
+        self.assertMatch('https://www.youtube.com/feed/subscriptions', ['youtube:subscriptions'])
+        self.assertMatch('https://www.youtube.com/feed/recommended', ['youtube:recommended'])
+        self.assertMatch('https://www.youtube.com/my_favorites', ['youtube:favorites'])
     
-    
-def test_follow_redirect_output_options(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=h',
-             '--history-print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 2
-    assert 'HTTP/1.1 302 FOUND' not in r
-    assert HTTP_OK in r
+    - name: update an existing origin access identity using caller_reference as an identifier
+  cloudfront_origin_access_identity:
+     origin_access_identity_id: E17DRN9XUOAHZX
+     caller_reference: this is an example reference
+     comment: this is a new comment
     
     RETURN = '''
-web_acl:
-  description: contents of the Web ACL
-  returned: always
-  type: complex
-  contains:
-    default_action:
-      description: Default action taken by the Web ACL if no rules match
-      returned: always
-      type: dict
-      sample:
-        type: BLOCK
-    metric_name:
-      description: Metric name used as an identifier
-      returned: always
-      type: string
-      sample: mywebacl
-    name:
-      description: Friendly name of the Web ACL
-      returned: always
-      type: string
-      sample: my web acl
-    rules:
-      description: List of rules
-      returned: always
-      type: complex
-      contains:
-        action:
-          description: Action taken by the WAF when the rule matches
-          returned: always
-          type: complex
-          sample:
-            type: ALLOW
-        priority:
-          description: priority number of the rule (lower numbers are run first)
-          returned: always
-          type: int
-          sample: 2
-        rule_id:
-          description: Rule ID
-          returned: always
-          type: string
-          sample: a6fc7ab5-287b-479f-8004-7fd0399daf75
-        type:
-          description: Type of rule (either REGULAR or RATE_BASED)
-          returned: always
-          type: string
-          sample: REGULAR
-    web_acl_id:
-      description: Unique identifier of Web ACL
-      returned: always
-      type: string
-      sample: 10fff965-4b6b-46e2-9d78-24f6d2e2d21c
+block_device_mapping:
+    description: Block device mapping for the instances of launch configuration
+    type: list
+    returned: always
+    sample: '[{
+        'device_name': '/dev/xvda':,
+        'ebs': {
+            'delete_on_termination': true,
+            'volume_size': 8,
+            'volume_type': 'gp2'
+    }]'
+classic_link_vpc_security_groups:
+    description: IDs of one or more security groups for the VPC specified in classic_link_vpc_id
+    type: string
+    returned: always
+    sample:
+created_time:
+    description: The creation date and time for the launch configuration
+    type: string
+    returned: always
+    sample: '2016-05-27T13:47:44.216000+00:00'
+ebs_optimized:
+    description: EBS I/O optimized (true ) or not (false )
+    type: bool
+    returned: always
+    sample: true,
+image_id:
+    description: ID of the Amazon Machine Image (AMI)
+    type: string
+    returned: always
+    sample: 'ami-12345678'
+instance_monitoring:
+    description: Launched with detailed monitoring or not
+    type: dict
+    returned: always
+    sample: '{
+        'enabled': true
+    }'
+instance_type:
+    description: Instance type
+    type: string
+    returned: always
+    sample: 't2.micro'
+kernel_id:
+    description: ID of the kernel associated with the AMI
+    type: string
+    returned: always
+    sample:
+key_name:
+    description: Name of the key pair
+    type: string
+    returned: always
+    sample: 'user_app'
+launch_configuration_arn:
+    description: Amazon Resource Name (ARN) of the launch configuration
+    type: string
+    returned: always
+    sample: 'arn:aws:autoscaling:us-east-1:666612345678:launchConfiguration:ba785e3a-dd42-6f02-4585-ea1a2b458b3d:launchConfigurationName/lc-app'
+launch_configuration_name:
+    description: Name of the launch configuration
+    type: string
+    returned: always
+    sample: 'lc-app'
+ramdisk_id:
+    description: ID of the RAM disk associated with the AMI
+    type: string
+    returned: always
+    sample:
+security_groups:
+    description: Security groups to associated
+    type: list
+    returned: always
+    sample: '[
+        'web'
+    ]'
+user_data:
+    description: User data available
+    type: string
+    returned: always
+    sample:
 '''
+    }
     
-    import traceback
-from ansible.module_utils._text import to_native
-from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import HAS_BOTO3, camel_dict_to_snake_dict, boto3_conn, ec2_argument_spec, get_aws_connection_info
     
-        if pipeline_field(client, dp_id, field='@pipelineState') in DP_ACTIVE_STATES:
-        changed = False
-    else:
-        try:
-            client.activate_pipeline(pipelineId=dp_id)
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'InvalidRequestException':
-                module.fail_json(msg='You need to populate your pipeline before activation.')
-        try:
-            pipeline_status_timeout(client, dp_id, status=DP_ACTIVE_STATES,
-                                    timeout=timeout)
-        except TimeOutException:
-            if pipeline_field(client, dp_id, field='@pipelineState') == 'FINISHED':
-                # activated but completed more rapidly than it was checked
-                pass
-            else:
-                module.fail_json(msg=('Data Pipeline {0} failed to activate '
-                                      'within timeout {1} seconds').format(dp_name, timeout))
-        changed = True
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
     
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import (get_aws_connection_info, boto3_conn, ec2_argument_spec,
-                                      camel_dict_to_snake_dict, HAS_BOTO3)
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
     
-        module = AnsibleModule(argument_spec=argument_spec)
+        state = module.params.get('state')
+    group_name = module.params.get('name').lower()
+    group_description = module.params.get('description')
+    group_subnets = module.params.get('subnets') or {}
     
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import (ansible_dict_to_boto3_filter_list,
-                                      boto3_conn, boto3_tag_list_to_ansible_dict, camel_dict_to_snake_dict,
-                                      ec2_argument_spec, get_aws_connection_info)
-    
-    '''
-*What is this pattern about?
-This pattern aims to encapsulate each algorithm and allow them to be
-interchangeable. Separating algorithms allows the client to scale
-with larger and more complex algorithms, since the client and the
-strategies are kept independent of each other.
-    
-    class TimeDisplay(object):
-    
-        def __init__(self, delegate):
-        self.delegate = delegate
-    
-        from pprint import pprint
+        xx = range(0, n * step, step)
+    plt.figure('scikit-learn tree benchmark results')
+    plt.subplot(211)
+    plt.title('Learning with varying number of samples')
+    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
+    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
+    plt.legend(loc='upper left')
+    plt.xlabel('number of samples')
+    plt.ylabel('Time (s)')
