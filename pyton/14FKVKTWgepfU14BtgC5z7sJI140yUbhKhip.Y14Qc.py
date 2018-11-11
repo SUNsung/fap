36cf9542792437aac7288c72fbbc6e92684169bb
SUNsung <weatherius@gmail.com>
2018-11-11 05:51:02 +0200

    Rewrite [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2018-11-11 05:51:01

--- pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -1,267 +1,152 @@
 
         
-        
-def openssl_encode(algo, key, iv):
-    cmd = ['openssl', 'enc', '-e', '-' + algo, '-K', hex_str(key), '-iv', hex_str(iv)]
-    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, _ = prog.communicate(secret_msg)
-    return out
+          Args:
+    output: the LSTM outputs - a tensor with shape
+    [num_paths, output_dim, max_path_len]
+    seq_lengths: the sequences length per instance
     
-    versions_info['signature'] = signature
-with open('update/versions.json', 'w') as versionsf:
-    json.dump(versions_info, versionsf, indent=4, sort_keys=True)
+        total_cost = total_recon_cost = total_kl_cost = 0.0
+    # normalizing by batch done in distributions.py
+    epoch_size = len(collected_op_values)
+    for op_values in collected_op_values:
+      total_cost += op_values[0]
+      total_recon_cost += op_values[1]
+      total_kl_cost += op_values[2]
+    
+    import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import numpy as np
+import tensorflow as tf
+    
+        if len(free_ids) < 5:
+      raise ValueError('Not enough free char ids: %d' % len(free_ids))
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+    from six.moves import xrange
+    
+        with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    
+    
+def test_update(client, auth, app):
+    auth.login()
+    assert client.get('/1/update').status_code == 200
+    client.post('/1/update', data={'title': 'updated', 'body': ''})
+    
+    
+def _find_app():
+    top = _app_ctx_stack.top
+    if top is None:
+        raise RuntimeError(_app_ctx_err_msg)
+    return top.app
+    
+        if head[:2] in (codecs.BOM_UTF16_BE, codecs.BOM_UTF16_LE):
+        return 'utf-16'
+    
+        def check(self, value):
+        '''Check if the given value should be tagged by this tag.'''
+        raise NotImplementedError
+    
+    import logging
+import sys
+    
+    
+def _render(template, context, app):
+    '''Renders the template and fires the signal'''
+    
+        By default this will get the strings from the blns.txt file
+    
+    
+class Signature(jose.Signature):
+    '''ACME-specific Signature. Uses ACME-specific Header for customer fields.'''
+    __slots__ = jose.Signature._orig_slots  # pylint: disable=no-member
+    
+    csr = OpenSSL.crypto.load_certificate_request(
+    OpenSSL.crypto.FILETYPE_ASN1, pkg_resources.resource_string(
+        'acme', os.path.join('testdata', 'csr.der')))
+try:
+    acme.request_issuance(jose.util.ComparableX509(csr), (authzr,))
+except messages.Error as error:
+    print ('This script is doomed to fail as no authorization '
+           'challenges are ever solved. Error from server: {0}'.format(error))
 
     
+    REWRITE_HTTPS_ARGS_WITH_END = [
+    '^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[END,NE,R=permanent]']
+'''Apache version >= 2.3.9 rewrite rule arguments used for redirections to
+    https vhost'''
     
-if __name__ == '__main__':
-    main()
-
+            self.assertRaises(errors.PluginError, self.config.save)
     
-    compat_print('total downloads traffic: %s' % format_size(total_bytes))
-
+            self.assertTrue(self.addr_default.conflicts(self.addr))
+        self.assertTrue(self.addr_default.conflicts(self.addr1))
+        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
     
-        diropts = []
-    for opt in opts_dir:
-        if opt._short_opts:
-            diropts.extend(opt._short_opts)
-        if opt._long_opts:
-            diropts.extend(opt._long_opts)
+        def test_2nd_am_station_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1380'
+        self.assertEqual(station, expected_station)
+    
+    *Where is the pattern used practically?
+    
+        def get_objects(self):
+        '''Get all objects'''
+        return self._objects
     
     
-def create_app(test_config=None):
-    '''Create and configure an instance of the Flask application.'''
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY='dev',
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+class Controller(object):
+    def __init__(self, blackboard):
+        self.blackboard = blackboard
+    
+        def find_all_path(self, start, end, path=None):
+        path = path or []
+        path.append(start)
+        if start == end:
+            return [path]
+        paths = []
+        for node in self.graph.get(start, []):
+            if node not in path:
+                newpaths = self.find_all_path(node, end, path[:])
+                paths.extend(newpaths)
+        return paths
+    
+    import time
+from threading import Thread
+from ycm.client.base_request import BaseRequest
+    
+    
+def FormatDebugInfoResponse_Completer_ServerRunningWithHost_test():
+  response = deepcopy( GENERIC_RESPONSE )
+  assert_that(
+    FormatDebugInfoResponse( response ),
+    contains_string(
+      'Completer name completer debug information:\n'
+      '  Server name running at: http://127.0.0.1:1234\n'
+      '  Server name process ID: 12345\n'
+      '  Server name executable: /path/to/executable\n'
+      '  Server name logfiles:\n'
+      '    /path/to/stdout/logfile\n'
+      '    /path/to/stderr/logfile\n'
+      '  Server name key: value\n'
+      '  Key: value\n'
     )
+  )
     
-        return render_template('blog/create.html')
+      # Other non-False responses mean the same thing
+  assert_that( _HandlePollResponse( '', None ), equal_to( True ) )
+  assert_that( _HandlePollResponse( 1, None ), equal_to( True ) )
+  assert_that( _HandlePollResponse( {}, None ), equal_to( True ) )
     
-    # read in SQL for populating test data
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
-    
-        with client:
-        auth.logout()
-        assert 'user_id' not in session
-
-    
-        Defines all the global objects that are proxies to the current
-    active context.
-    
-        If `conditional=True` and `filename` is provided, this method will try to
-    upgrade the response stream to support range requests.  This will allow
-    the request to be answered with partial content response.
+      return request
     
     
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
-
+def _assert_rejects( filter, text ):
+  _assert_accept_equals( filter, text, False )
     
-            return list(result)
-    
-            # We attach the view class to the view function for two reasons:
-        # first of all it allows us to easily figure out what class-based
-        # view this thing came from, secondly it's also used for instantiating
-        # the view class so you can actually replace it with something else
-        # for testing purposes and debugging.
-        view.view_class = cls
-        view.__name__ = name
-        view.__doc__ = cls.__doc__
-        view.__module__ = cls.__module__
-        view.methods = cls.methods
-        view.provide_automatic_options = cls.provide_automatic_options
-        return view
-    
-        @property
-    def headers(self):
-        '''Return a `str` with the message's headers.'''
-        raise NotImplementedError()
-    
-            if env.stdout_isatty:
-            # Use the encoding supported by the terminal.
-            output_encoding = env.stdout_encoding
-        else:
-            # Preserve the message encoding.
-            output_encoding = self.msg.encoding
-    
-    
-def test_migrate_implicit_content_type():
-    config = MockEnvironment().config
-    
-    
-class Response(object):
-    # noinspection PyDefaultArgument
-    def __init__(self, url, headers={}, status_code=200):
-        self.url = url
-        self.headers = CaseInsensitiveDict(headers)
-        self.status_code = status_code
-    
-        def test_request_body_from_file_by_path_with_explicit_content_type(
-            self, httpbin):
-        r = http('--verbose',
-                 'POST', httpbin.url + '/post', '@' + FILE_PATH_ARG,
-                 'Content-Type:text/plain; charset=utf8')
-        assert HTTP_OK in r
-        assert FILE_CONTENT in r
-        assert 'Content-Type: text/plain; charset=utf8' in r
-    
-        def inner(*suffix):
-        return urljoin(httpbin_url, '/'.join(suffix))
-    
-        def _accept_connection(self):
-        try:
-            ready, _, _ = select.select([self.server_sock], [], [], self.WAIT_EVENT_TIMEOUT)
-            if not ready:
-                return None
-    
-    # TODO: response is the only one
-    
-    setup(
-    name=about['__title__'],
-    version=about['__version__'],
-    description=about['__description__'],
-    long_description=readme,
-    long_description_content_type='text/markdown',
-    author=about['__author__'],
-    author_email=about['__author_email__'],
-    url=about['__url__'],
-    packages=packages,
-    package_data={'': ['LICENSE', 'NOTICE'], 'requests': ['*.pem']},
-    package_dir={'requests': 'requests'},
-    include_package_data=True,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    install_requires=requires,
-    license=about['__license__'],
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    cmdclass={'test': PyTest},
-    tests_require=test_requirements,
-    extras_require={
-        'security': ['pyOpenSSL >= 0.14', 'cryptography>=1.3.4', 'idna>=2.0.0'],
-        'socks': ['PySocks>=1.5.6, !=1.5.7'],
-        'socks:sys_platform == 'win32' and python_version == '2.7'': ['win_inet_pton'],
-    },
-)
-
-    
-    
-@pytest.mark.parametrize(
-    'url, expected', (
-        ('http://u:p@example.com/path?a=1#test', 'http://example.com/path?a=1'),
-        ('http://example.com/path', 'http://example.com/path'),
-        ('//u:p@example.com/path', '//example.com/path'),
-        ('//example.com/path', '//example.com/path'),
-        ('example.com/path', '//example.com/path'),
-        ('scheme:u:p@example.com/path', 'scheme://example.com/path'),
-    ))
-def test_urldefragauth(url, expected):
-    assert urldefragauth(url) == expected
-    
-    Author: dufferzafar
-'''
-    
-    # Apply monkey patches to fix issues in external libraries
-from . import _monkeypatches
-del _monkeypatches
-    
-        def add_options(self, parser):
-        '''
-        Populate option parse with options available for this command
-        '''
-        group = OptionGroup(parser, 'Global Options')
-        group.add_option('--logfile', metavar='FILE',
-            help='log file. if omitted stderr will be used')
-        group.add_option('-L', '--loglevel', metavar='LEVEL', default=None,
-            help='log level (default: %s)' % self.settings['LOG_LEVEL'])
-        group.add_option('--nolog', action='store_true',
-            help='disable logging completely')
-        group.add_option('--profile', metavar='FILE', default=None,
-            help='write python cProfile stats to FILE')
-        group.add_option('--pidfile', metavar='FILE',
-            help='write process ID to FILE')
-        group.add_option('-s', '--set', action='append', default=[], metavar='NAME=VALUE',
-            help='set/override setting (may be repeated)')
-        group.add_option('--pdb', action='store_true', help='enable pdb on failure')
-    
-        def long_desc(self):
-        return 'Run the spider defined in the given file'
-    
-    class Command(ScrapyCommand):
-    
-            if not assertion:
-            if self.min_bound == self.max_bound:
-                expected = self.min_bound
-            else:
-                expected = '%s..%s' % (self.min_bound, self.max_bound)
-    
-        def gotClient(self, client, request, filepath):
-        self.client = client
-        protocol = ReceivedDataProtocol(request.meta.get('ftp_local_filename'))
-        return client.retrieveFile(filepath, protocol)\
-                .addCallbacks(callback=self._build_response,
-                        callbackArgs=(request, protocol),
-                        errback=self._failed,
-                        errbackArgs=(request,))
-    
-        def get(self, msgid):
-        '''We'll punt if we don't have a translation'''
-        return self.trans.get(msgid, str(msgid))
-    
-    
-def memento(obj, deep=False):
-    state = deepcopy(obj.__dict__) if deep else copy(obj.__dict__)
-    
-        for action in actions:
-        action()
-    
-    *References:
-bottle
-https://github.com/bottlepy/bottle/blob/cafc15419cbb4a6cb748e6ecdccf92893bb25ce5/bottle.py#L270
-django
-https://github.com/django/django/blob/ffd18732f3ee9e6f0374aff9ccf350d85187fac2/django/utils/functional.py#L19
-pip
-https://github.com/pypa/pip/blob/cb75cca785629e15efb46c35903827b3eae13481/pip/utils/__init__.py#L821
-pyramimd
-https://github.com/Pylons/pyramid/blob/7909e9503cdfc6f6e84d2c7ace1d3c03ca1d8b73/pyramid/decorator.py#L4
-werkzeug
-https://github.com/pallets/werkzeug/blob/5a2bf35441006d832ab1ed5a31963cbc366c99ac/werkzeug/utils.py#L35
-    
-    *What does this example do?
-When the number of prototypes in an application can vary, it can be
-useful to keep a Dispatcher (aka, Registry or Manager). This allows
-clients to query the Dispatcher for a prototype before cloning a new
-instance.
-    
-        def now(self):
-        current_time = datetime.datetime.now()
-        current_time_formatted = '{}:{}'.format(current_time.hour, current_time.minute)
-        return current_time_formatted
+    from ycm.tests.test_utils import MockVimModule
+MockVimModule()
