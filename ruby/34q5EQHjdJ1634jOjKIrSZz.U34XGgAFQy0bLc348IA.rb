0289c1eb6bd3fd913e67f81d418ad82334e18024
SUNsung <weatherius@gmail.com>
2018-11-11 23:46:12 +0200

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-11-11 23:46:12

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,161 +1,157 @@
 
         
-            def set_after_all(platform, block)
-      unless after_all_blocks[platform].nil?
-        UI.error('You defined multiple `after_all` blocks in your `Fastfile`. The last one being set will be used.')
-      end
-      after_all_blocks[platform] = block
-    end
+                format('%2$e', 0, 109.52).should == '1.095200e+02'
+        format('%2$E', 0, 109.52).should == '1.095200E+02'
+        format('%2$f', 0, 10.952).should == '10.952000'
+        format('%2$g', 0, 12.1234).should == '12.1234'
+        format('%2$G', 0, 12.1234).should == '12.1234'
+        format('%2$a', 0, 196).should == '0x1.88p+7'
+        format('%2$A', 0, 196).should == '0X1.88P+7'
     
-            # Stub out calls related to the execution environment
-        client = double('ingester_client')
-        session = FastlaneCore::AnalyticsSession.new(analytics_ingester_client: client)
-        expect(client).to receive(:post_event).with({
-            client_id: p_hash,
-            category: 'fastlane Client Langauge - ruby',
-            action: :launch,
-            label: nil,
-            value: nil
-        })
-    
-          it 'handles the exclude_dirs parameter with a single element correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: ['.bundle'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --exclude-dir .bundle')
-      end
-    
-          it 'works with multiple ignore patterns' do
-        pattern1 = 'Pods/*'
-        pattern2 = '../**/*/Xcode*'
-        result = Fastlane::FastFile.new.parse('lane :test do
-          slather({
-            ignore: ['#{pattern1}', '#{pattern2}'],
-            proj: 'foo.xcodeproj'
-          })
-        end').runner.execute(:test)
-    
-          if conflicting_options
-        conflicting_options.each do |conflicting_option_key|
-          UI.user_error!('Conflicting option key must be a symbol') unless conflicting_option_key.kind_of?(Symbol)
-        end
-      end
-    
-              config = FastlaneCore::Configuration.create([name, other, platform], {})
-          config.set(:name, 'name1')
-          config.set(:other, 'other')
-          config.push_values!
-    
-    invalids = []
-Parallel.each(links, in_threads: 4) do |link|
-  href = link.attribute('href').to_s
-  begin
-    case check_link(URI.join(BASE_URI, href))
-    when (200...300)
-      putc('.')
-    when (300..302)
-      putc('w')
-    end
-  rescue => e
-    putc('F')
-    invalids << '#{href} (reason: #{e.message})'
-  end
-end
-    
-    if defined?(ActionMailer)
-  class Devise::Mailer < Devise.parent_mailer.constantize
-    include Devise::Mailers::Helpers
-    
-        protected
-    
-            # This is called to upgrade this V1 config to V2. The parameter given
-        # is the full V2 configuration object, so you can do anything to it
-        # that you want.
-        #
-        # No return value is expected, modifications should be made directly
-        # to the new V2 object.
-        #
-        # @param [V2::Root] new
-        def upgrade(new)
-        end
-    
-            include Vagrant::Util
-    
-              providers
-        end
-    
-              # This is a helper that gets a single machine with the proper
-          # provider. The 'proper provider' in this case depends on what was
-          # given:
-          #
-          #   * If a provider was explicitly specified, then use that provider.
-          #     But if an active machine exists with a DIFFERENT provider,
-          #     then throw an error (for now), since we don't yet support
-          #     bringing up machines with different providers.
-          #
-          #   * If no provider was specified, then use the active machine's
-          #     provider if it exists, otherwise use the default provider.
-          #
-          get_machine = lambda do |name|
-            # Check for an active machine with the same name
-            provider_to_use = options[:provider]
-            provider_to_use = provider_to_use.to_sym if provider_to_use
-    
-            # This contains all the synced folder implementations by name.
-        #
-        # @return [Registry<Symbol, Array<Class, Integer>>]
-        attr_reader :synced_folders
-    
-    module Vagrant
-  module Plugin
-    module V2
-      # This class maintains a list of all the registered plugins as well
-      # as provides methods that allow querying all registered components of
-      # those plugins as a single unit.
-      class Manager
-        attr_reader :registered
-    
-      it 'raises a TypeError when passed nil' do
-    lambda { srand(nil) }.should raise_error(TypeError)
-  end
-    
-        it 'returns the last access time for the provided file when passed ?A' do
-      Kernel.test(?A, @tmp_file).should == @tmp_file.atime
-    end
-    
-      # String arguments should be evaluated in the context of the caller.
-  it 'accepts a String argument instead of a Proc or block' do
-    trace_var :$Kernel_trace_var_global, '$Kernel_trace_var_extra = true'
-    
-    if profile_filename = ENV['PROFILE']
-  require 'ruby-prof'
-  reporter =
-    case (profile_extname = File.extname(profile_filename))
-    when '.txt'
-      RubyProf::FlatPrinterWithLineNumbers
-    when '.html'
-      RubyProf::GraphHtmlPrinter
-    when '.callgrind'
-      RubyProf::CallTreePrinter
-    else
-      raise 'Unknown profiler format indicated by extension: #{profile_extname}'
-    end
-  File.open(profile_filename, 'w') do |io|
-    reporter.new(RubyProf.profile { Pod::Command.run(ARGV) }).print(io)
-  end
-else
-  Pod::Command.run(ARGV)
+    describe 'Kernel.system' do
+  it_behaves_like :kernel_system, :system, Kernel
 end
 
     
-        # Checks that the podfile exists.
-    #
-    # @raise  If the podfile does not exists.
-    #
-    # @return [void]
-    #
-    def verify_podfile_exists!
-      unless config.podfile
-        raise Informative, 'No `Podfile' found in the project directory.'
+    describe 'Kernel.throw' do
+  it 'transfers control to the end of the active catch block waiting for symbol' do
+    catch(:blah) do
+      :value
+      throw :blah
+      fail('throw didn't transfer the control')
+    end.should be_nil
+  end
+    
+        $Kernel_trace_var_global = 'foo'
+    
+      ruby_version_is '2.5' do
+    describe ':uplevel keyword argument' do
+      before :each do
+        $VERBOSE = true
+      end
+    
+          File.open('bower.json', 'w') do |f|
+        f.puts JSON.pretty_generate(spec)
       end
     end
+  end
+end
+
+    
+      # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+    
+          # Find a Sass file, if it exists.
+      #
+      # This is the primary entry point of the Importer.
+      # It corresponds directly to an `@import` statement in Sass.
+      # It should do three basic things:
+      #
+      # * Determine if the URI is in this importer's format.
+      #   If not, return nil.
+      # * Determine if the file indicated by the URI actually exists and is readable.
+      #   If not, return nil.
+      # * Read the file and place the contents in a {Sass::Engine}.
+      #   Return that engine.
+      #
+      # If this importer's format allows for file extensions,
+      # it should treat them the same way as the default {Filesystem} importer.
+      # If the URI explicitly has a `.sass` or `.scss` filename,
+      # the importer should look for that exact file
+      # and import it as the syntax indicated.
+      # If it doesn't exist, the importer should return nil.
+      #
+      # If the URI doesn't have either of these extensions,
+      # the importer should look for files with the extensions.
+      # If no such files exist, it should return nil.
+      #
+      # The {Sass::Engine} to be returned should be passed `options`,
+      # with a few modifications. `:syntax` should be set appropriately,
+      # `:filename` should be set to `uri`,
+      # and `:importer` should be set to this importer.
+      #
+      # @param uri [String] The URI to import.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` that's currently being resolved.
+      #   This is safe for subclasses to modify destructively.
+      #   Callers should only pass in a value they don't mind being destructively modified.
+      # @return [Sass::Engine, nil] An Engine containing the imported file,
+      #   or nil if it couldn't be found or was in the wrong format.
+      def find(uri, options)
+        Sass::Util.abstract(self)
+      end
+    
+        desc 'Release all gems as packages'
+    task :all => [:test, :commit_version] + GEMS_AND_ROOT_DIRECTORIES.keys
+  end
+end
+
+    
+        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      subject.options[:allow_empty_referrer] = false
+      expect(subject.referrer(env)).to be_nil
+    end
+    
+    ::Bundler.with_friendly_errors do
+  ::Bundler::CLI.start(ARGV, :debug => true)
+end
+
+    
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
+    
+        FileUtils.rm_rf(LogStash::Environment::CACHE_PATH)
+    validate_cache_location
+    archive_manager.extract(package_file, LogStash::Environment::CACHE_PATH)
+    puts('Unpacked at #{LogStash::Environment::CACHE_PATH}')
+    puts('The unpacked plugins can now be installed in local-only mode using bin/logstash-plugin install --local [plugin name]')
+  end
+    
+    namespace :qa do
+    
+        context 'update a specific plugin' do
+      it 'has executed successfully' do
+        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
+        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
+        expect(logstash).not_to have_installed?(plugin_name, previous_version)
+      end
+    end
+    
+      include_examples 'multiline literal brace layout' do
+    let(:open) { '[' }
+    let(:close) { ']' }
+  end
+    
+          expect(new_source)
+        .to eq(construct(false, a, make_multi(safe_heredoc), false))
+    end
+  end
+    
+    module RuboCop
+  module AST
+    # A node extension for `def` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `def` nodes within RuboCop.
+    class DefNode < Node
+      include ParameterizedNode
+      include MethodIdentifierPredicates
+    
+          # Checks whether the `hash` node contains any `pair`- or `kwsplat` nodes.
+      #
+      # @return[Boolean] whether the `hash` is empty
+      def empty?
+        children.empty?
+      end
