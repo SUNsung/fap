c8796d7bbddbaf565b1be41dcfb22731ec089ed4
SUNsung <weatherius@gmail.com>
2018-11-11 15:52:26 +0200

    Rewrite [23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py] File in: 2018-11-11 15:52:25

--- pyton/23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py
+++ pyton/23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py
@@ -1,121 +1,203 @@
 
         
-            # This is needed to make sure that the gradients are simple.
-    # The value of the function shouldn't change.
-    if z == self.sample:
-      return gaussian_pos_log_likelihood(self.mean, self.logvar, self.noise)
+          LFADS generates a number of outputs for each examples, and these are all
+  saved.  They are:
+    The mean and variance of the prior of g0.
+    The control inputs (if enabled)
+    The initial conditions, g0, for all examples.
+    The generator states for all time.
+    The factors for all time.
+    The output distribution parameters (e.g. rates) for all time.
     
+    rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
+rnn_rngs = [np.random.RandomState(seed=FLAGS.synth_data_seed+1),
+            np.random.RandomState(seed=FLAGS.synth_data_seed+2)]
+T = FLAGS.T
+C = FLAGS.C
+N = FLAGS.N
+nreplications = FLAGS.nreplications
+E = nreplications * C
+train_percentage = FLAGS.train_percentage
+ntimesteps = int(T / FLAGS.dt)
     
-flags.DEFINE_float('prior_ar_atau',  PRIOR_AR_AUTOCORRELATION,
-                   'Initial autocorrelation of AR(1) priors.')
-flags.DEFINE_float('prior_ar_nvar', PRIOR_AR_PROCESS_VAR,
-                   'Initial noise variance for AR(1) priors.')
-flags.DEFINE_boolean('do_train_prior_ar_atau', DO_TRAIN_PRIOR_AR_ATAU,
-                     'Is the value for atau an init, or the constant value?')
-flags.DEFINE_boolean('do_train_prior_ar_nvar', DO_TRAIN_PRIOR_AR_NVAR,
-                     'Is the value for noise variance an init, or the constant \
-                     value?')
+      Returns:
+    A length T list of numpy tensors with shape BxT.
+  '''
     
-      def call(self, h_tm1_bxn, u_bx1):
-    act_t_bxn = tf.matmul(h_tm1_bxn, self.Wh_nxn) + self.b_1xn + u_bx1 * self.Bu_1xn
-    h_t_bxn = tf.nn.tanh(act_t_bxn)
-    z_t = tf.nn.xw_plus_b(h_t_bxn, self.Wro_nxo, self.bro_o)
-    return z_t, h_t_bxn
+      def encode_chars(self, sentence):
+    chars_ids = [self.word_to_char_ids(cur_word)
+                 for cur_word in sentence.split()]
+    return np.vstack([self.bos_chars] + chars_ids + [self.eos_chars])
     
-          # Cut sentences into patches of shape processable by the LM.
-      batch_size, num_timesteps = self.shape
-      word_patches = utils.cut_to_patches(sentences, batch_size, num_timesteps)
-      probs = self._score_patches(word_patches)
+    import collections
+import os
+# Dependency imports
+import numpy as np
     
-      # Two consecutive substitutions always belong to the same question
-  question_ids = [qid for i, qid in enumerate(question_ids) if i % 2 == 0]
-  assert len(question_ids) == len(prediction_correctness)
-  num_questions = len(set(question_ids))
-    
-          else:
-        to_fill_in = num_steps - len(example)
-        final_x = example + [EOS_INDEX] * to_fill_in
-        final_y = final_x[1:] + [EOS_INDEX]
-        w[i] = [1] * len(example) + [0] * to_fill_in
-    
-            if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-    
-    import click
-from flask import current_app, g
-from flask.cli import with_appcontext
-    
-        def before_app_first_request(self, f):
-        '''Like :meth:`Flask.before_first_request`.  Such a function is
-        executed before the first request to the application.
-        '''
-        self.record_once(lambda s: s.app.before_first_request_funcs.append(f))
-        return f
-    
-    
-def _endpoint_from_view_func(view_func):
-    '''Internal helper that returns the default endpoint for a given
-    function.  This always is the function name.
-    '''
-    assert view_func is not None, 'expected view func if endpoint ' \
-                                  'is not provided.'
-    return view_func.__name__
-    
-    signals_available = False
-try:
-    from blinker import Namespace
-    signals_available = True
-except ImportError:
-    class Namespace(object):
-        def signal(self, name, doc=None):
-            return _FakeSignal(name, doc)
-    
-    
-def _render(template, context, app):
-    '''Renders the template and fires the signal'''
-    
-            if 'obj' not in kwargs:
-            kwargs['obj'] = ScriptInfo(create_app=lambda: self.app)
-    
-        exitcode = 0
-    
-    
-class _BenchSpider(scrapy.Spider):
-    '''A spider that follows all links'''
-    name = 'follow'
-    total = 10000
-    show = 20
-    baseurl = 'http://localhost:8998'
-    link_extractor = LinkExtractor()
-    
-        def syntax(self):
-        return '[options] <spider>'
-    
-        def run(self, args, opts):
-        settings = self.crawler_process.settings
-        if opts.get:
-            s = settings.get(opts.get)
-            if isinstance(s, BaseSettings):
-                print(json.dumps(s.copy_to_dict()))
-            else:
-                print(s)
-        elif opts.getbool:
-            print(settings.getbool(opts.getbool))
-        elif opts.getint:
-            print(settings.getint(opts.getint))
-        elif opts.getfloat:
-            print(settings.getfloat(opts.getfloat))
-        elif opts.getlist:
-            print(settings.getlist(opts.getlist))
+      for key, _ in gen_ngrams_dict.iteritems():
+    if key in train_ngrams_dict:
+      unique_ngrams_in_train += 1
+  return float(unique_ngrams_in_train) / float(total_ngrams_produced)
 
     
-        def run(self, args, opts):
-        url = args[0] if args else None
-        if url:
-            # first argument may be a local file
-            url = guess_scheme(url)
+    
+def dis_fwd_bidirectional(hparams):
+  '''Returns the *forward* PTB Variable name to MaskGAN Variable dictionary
+  mapping.  This is a highly restrictive function just for testing. This is for
+  the bidirectional_zaremba discriminator.
     
     
-class Command(ScrapyCommand):
+@pytest.fixture
+def no_memoize(monkeypatch):
+    monkeypatch.setattr('thefuck.utils.memoize.disabled', True)
+    
+    
+def how_to_configure(proc, TIMEOUT):
+    proc.sendline(u'fuck')
+    assert proc.expect([TIMEOUT, u'alias isn't configured'])
+
+    
+    You can download from:
+  https://osxfuse.github.io/
+Error: An unsatisfied requirement failed this build.'''
+    
+        def __init__(self, mime):
+        self.mime = mime
+    
+    
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
+    
+    
+def test_unicode_basic_auth(httpbin):
+    # it doesn't really authenticate us because httpbin
+    # doesn't interpret the utf8-encoded auth
+    http('--verbose', '--auth', u'test:%s' % UNICODE,
+         httpbin.url + u'/basic-auth/test/' + UNICODE)
+    
+    # Run training, with or without data augmentation.
+if not data_augmentation:
+    print('Not using data augmentation.')
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_data=(x_test, y_test),
+              shuffle=True,
+              callbacks=callbacks)
+else:
+    print('Using real-time data augmentation.')
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        # set input mean to 0 over the dataset
+        featurewise_center=False,
+        # set each sample mean to 0
+        samplewise_center=False,
+        # divide inputs by std of dataset
+        featurewise_std_normalization=False,
+        # divide each input by its std
+        samplewise_std_normalization=False,
+        # apply ZCA whitening
+        zca_whitening=False,
+        # epsilon for ZCA whitening
+        zca_epsilon=1e-06,
+        # randomly rotate images in the range (deg 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally
+        width_shift_range=0.1,
+        # randomly shift images vertically
+        height_shift_range=0.1,
+        # set range for random shear
+        shear_range=0.,
+        # set range for random zoom
+        zoom_range=0.,
+        # set range for random channel shifts
+        channel_shift_range=0.,
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        # value used for fill_mode = 'constant'
+        cval=0.,
+        # randomly flip images
+        horizontal_flip=True,
+        # randomly flip images
+        vertical_flip=False,
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either 'channels_first' or 'channels_last'
+        data_format=None,
+        # fraction of images reserved for validation (strictly between 0 and 1)
+        validation_split=0.0)
+    
+        fpath = os.path.join(path, 'train')
+    x_train, y_train = load_batch(fpath, label_key=label_mode + '_labels')
+    
+        model._check_trainable_weights_consistency()
+    if hasattr(model, '_collected_trainable_weights'):
+        trainable_count = count_params(model._collected_trainable_weights)
+    else:
+        trainable_count = count_params(model.trainable_weights)
+    
+    
+def test_decode_predictions():
+    x = np.zeros((2, 1000))
+    x[0, 372] = 1.0
+    x[1, 549] = 1.0
+    outs = utils.decode_predictions(x, top=1)
+    scores = [out[0][2] for out in outs]
+    assert scores[0] == scores[1]
+    
+    
+def test_regularization_shared_layer():
+    dense_layer = Dense(num_classes,
+                        kernel_regularizer=regularizers.l1(),
+                        activity_regularizer=regularizers.l1())
+    
+    
+def to_categorical(y, num_classes=None, dtype='float32'):
+    '''Converts a class vector (integers) to binary class matrix.
+    
+        def _process_batch(self, inputs, initial_state):
+        import tensorflow as tf
+        inputs = tf.transpose(inputs, (1, 0, 2))
+        input_h = initial_state[0]
+        input_h = tf.expand_dims(input_h, axis=0)
+    
+            def get_linux_ca_sha1(nss_path):
+            commonname = 'GoAgent XX-Net - GoAgent' #TODO: here should be GoAgent - XX-Net
+    
+                except NoViableAltException, re:
+                self.reportError(re)
+                self.recover(re) # throw out current char and try again
+    
+            Using setter/getter methods is deprecated. Use o.type instead.'''
+    
+            :raises .errors.PluginError: If there is a problem with the input or
+            the function is unable to correctly revert the configuration
+    
+            achall = self.achalls[0]
+        self.sni.add_chall(achall)
+        response = self.achalls[0].response(self.auth_key)
+        mock_setup_cert = mock.MagicMock(return_value=response)
+        # pylint: disable=protected-access
+        self.sni._setup_challenge_cert = mock_setup_cert
+    
+    # If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
+    
+        Snippets without code (only comments) or containing lines starting with ??? should not yeld files,
+    but the counter for naming snippets should still increment.
+    '''
+    parser = argparse.ArgumentParser(description='Split md file into plain text and code blocks')
+    parser.add_argument('sourcefile',
+                        help='which file to read')
+    parser.add_argument('targetfile',
+                        help='where to put plain text')
+    parser.add_argument('codedir',
+                        help='where to put codeblocks')
+    args = parser.parse_args()
