eaf1c7116e1c761e5bdce78eddbd7772557f1adb
SUNsung <weatherius@gmail.com>
2018-11-12 17:56:41 +0200

    Rewrite [03Sn0wIovFC.039lmxs9MTc1U03o9LL2Uv1Jk03Wp.py] File in: 2018-11-12 17:56:40

--- pyton/03Sn0wIovFC.039lmxs9MTc1U03o9LL2Uv1Jk03Wp.py
+++ pyton/03Sn0wIovFC.039lmxs9MTc1U03o9LL2Uv1Jk03Wp.py
@@ -1,51 +1,108 @@
 
         
-            for n_components in [i.astype(int) for i in
-                         np.linspace(data.shape[1] // 10,
-                                     data.shape[1], num=4)]:
-        all_times = defaultdict(list)
-        all_errors = defaultdict(list)
-        pca = PCA(n_components=n_components)
-        rpca = PCA(n_components=n_components, svd_solver='randomized',
-                   random_state=1999)
-        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
-                                                               ('rpca', rpca)]}
+        '''Eval pre-trained 1 billion word language model.
+'''
+import os
+import sys
+    
+      Raises:
+    ValueError: if batch_size or num_steps are too high.
+  '''
+  raw_data = np.array(raw_data, dtype=np.int32)
+    
+        elif (FLAGS.discriminator_model == 'bidirectional_zaremba' or
+          FLAGS.discriminator_model == 'bidirectional_vd'):
+      dis_fwd_variable_maps = variable_mapping.dis_fwd_bidirectional(hparams)
+      dis_bwd_variable_maps = variable_mapping.dis_bwd_bidirectional(hparams)
+      # Savers for the forward/backward Discriminator components.
+      dis_fwd_init_saver = tf.train.Saver(var_list=dis_fwd_variable_maps)
+      dis_bwd_init_saver = tf.train.Saver(var_list=dis_bwd_variable_maps)
+      init_savers['dis_fwd_init_saver'] = dis_fwd_init_saver
+      init_savers['dis_bwd_init_saver'] = dis_bwd_init_saver
+    
+      # Output variables only for the Generator.  Discriminator output weights
+  # and biases will begin randomly initialized.
+  if model == 'gen':
+    softmax_w = [
+        v for v in tf.trainable_variables() if v.op.name == 'gen/rnn/softmax_w'
+    ][0]
+    softmax_b = [
+        v for v in tf.trainable_variables() if v.op.name == 'gen/rnn/softmax_b'
+    ][0]
+    
+      Args:
+    name: to label variables.
+    num_units: hidden state dimension.
+    attention_score_fn: to compute similarity between key and target states.
+    reuse: whether to reuse variable scope.
+    
+    from test.helper import gettestcases
+from youtube_dl.utils import compat_urllib_parse_urlparse
+from youtube_dl.utils import compat_urllib_request
+    
+    filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
+build_dir = os.path.join('..', '..', 'build', version)
+for key, filename in filenames.items():
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    fn = os.path.join(build_dir, filename)
+    with open(fn, 'rb') as f:
+        data = f.read()
+    if not data:
+        raise ValueError('File %s is empty!' % fn)
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
     
     
-def compute_bench(samples_range, features_range):
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
     
-        # start time
-    t_start = datetime.now()
-    clf.fit(X)
-    delta = (datetime.now() - t_start)
-    # stop time
-    time_to_fit = compute_time(t_start, delta)
+    with io.open(README_FILE, encoding='utf-8') as f:
+    oldreadme = f.read()
     
-    # Gather data for each version directory, including symlinks
-dirs = {}
-symlinks = {}
-root_listing = json_urlread(ROOT_URL)
-for path_details in root_listing:
-    name = path_details['name']
-    if not (name[:1].isdigit() or name in NAMED_DIRS):
-        continue
-    if path_details['type'] == 'dir':
-        html = urlopen(RAW_FMT % name).read().decode('utf8')
-        version_num = VERSION_RE.search(html).group(1)
-        pdf_size = get_pdf_size(name)
-        dirs[name] = (version_num, pdf_size)
+    import io
+import optparse
+import os.path
+import re
     
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (14 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
     
-    # Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'certbot-apache', u'certbot-apache Documentation',
-     author, 'certbot-apache', 'One line description of project.',
-     'Miscellaneous'),
-]
+class TestAgeRestriction(unittest.TestCase):
+    def _assert_restricted(self, url, filename, age, old_age=None):
+        self.assertTrue(_download_restricted(url, filename, old_age))
+        self.assertFalse(_download_restricted(url, filename, age))
+    
+    
+if __name__ == '__main__':
+    unittest.main()
+
+    
+    
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
+    
+        def test_print_only_body_when_stdout_redirected_by_default(self, httpbin):
+        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
+        r = http('GET', httpbin.url + '/get', env=env)
+        assert 'HTTP/' not in r
+    
+    #----------------------------------------------------------------------
+def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    '''wrapper'''
+    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
+    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
+    hostname = urlparse(url).hostname
+    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
+        return False
+    upid = match1(url, r'.+/content/(\w+)')
+    
+    from ..common import *
