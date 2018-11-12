7a193542d6b15152becac8c37e80a922177fda7f
SUNsung <weatherius@gmail.com>
2018-11-12 04:30:31 +0200

    Rewrite [188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp] File in: 2018-11-12 04:30:31

--- cccccc/188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp
+++ cccccc/188DvD1FSycW.185I3XeHWc9yk18tIhsOp0f218PL2.cpp
@@ -1,244 +1,272 @@
 
         
-        // Godot TO Bullet
-extern void G_TO_B(Vector3 const &inVal, btVector3 &outVal);
-extern void INVERT_G_TO_B(Vector3 const &inVal, btVector3 &outVal);
-extern void G_TO_B(Basis const &inVal, btMatrix3x3 &outVal);
-extern void INVERT_G_TO_B(Basis const &inVal, btMatrix3x3 &outVal);
-extern void G_TO_B(Transform const &inVal, btTransform &outVal);
-    
-    #include 'bullet_utilities.h'
-#include 'rid_bullet.h'
-    
-    public:
-	HingeJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Transform &frameA, const Transform &frameB);
-	HingeJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Vector3 &pivotInA, const Vector3 &pivotInB, const Vector3 &axisInA, const Vector3 &axisInB);
-    
-    jobject GDAPI godot_android_get_activity() {
-#ifdef __ANDROID__
-	JNIEnv *env = ThreadAndroid::get_env();
+        // FreeOp frees the content of the output blob. We allow it to take in input
+// blobs purely for the reason that it can 'wait' on the input blobs to be
+// produced by some of the earlier operators before a free is called.
+template <class Context>
+class FreeOp : public Operator<Context> {
+ public:
+  FreeOp(const OperatorDef& def, Workspace* ws) : Operator<Context>(def, ws) {}
     }
     
-    	for (uint32_t i = 0; i < alloc_count - 1; i++) {
-    }
+      ~SecureAuthContext() override;
     
-    std::unique_ptr<php::Unit> parse_unit(php::Program& prog,
-                                      std::unique_ptr<UnitEmitter> ue);
-    
-    Object APCCollection::createObject() const {
-  if (m_arrayHandle->isTypedValue()) {
-    Variant local(m_arrayHandle->toLocal());
-    assertx(local.isArray());
-    return Object::attach(
-      collections::alloc(m_colType, local.getArrayData())
-    );
-  }
-    }
-    
-    #endif // HPHP_DATA_STREAM_WRAPPER_H
-
-    
-    
-    {
-    {      write(ini_fd, line.c_str(), line.length());
-      write(ini_fd, '\n', 1);
-      cnt += 2;
-      continue;
-    }
-    if (argv[cnt][0] != '-') {
-      if (show) {
-        newargv.push_back('-w');
-      } else {
-        newargv.push_back(lint ? '-l' : '-f');
-      }
-      newargv.push_back(argv[cnt++]);
-      need_file = false;
-      break;
-    }
-    if (strcmp(argv[cnt], '--') == 0) {
-      break;
-    }
-    cnt++; // skip unknown options
-  }
-    
-    inline VarEnv* ExecutionContext::hasVarEnv(int frame) {
-  auto const fp = getFrameAtDepth(frame);
-  if (fp && (fp->func()->attrs() & AttrMayUseVV)) {
-    if (fp->hasVarEnv()) return fp->getVarEnv();
-  }
-  return nullptr;
-}
-    
-    #endif
-
-    
-      req::ptr<Directory> opendir(const String& path) override;
-    
-            if (sectionShape.Rank() > m_maskShape.Rank())
-            LogicError('NDMask::MaskSection: The section shape '%S' rank (%d) must be <= rank (%d) of 'this' mask.', sectionShape.AsString().c_str(), (int)sectionShape.Rank(), (int)m_maskShape.Rank());
-    
-    
-    {            // Arithmetic schedule - write at every m_frequency steps or if the update is one of the first m_firstN
-            // updates.
-            return update % m_frequency == 0 || update <= m_firstN;
-        }
-    
-    
-    {        // TODO: Why Backward signature does not take Parameter instead of Variable for gradients?
-        m_combinedTrainingFunction->Backward(backPropSate, { { m_aggregatedLossFunction, m_rootGradientValue } }, parameterGradients);
-        m_prevMinibatchNumSamples = GetSampleCount(m_trainingSampleCountVar, outputs[m_trainingSampleCountVar]);
-    }
-    
-            static bool IsUDF(const Dictionary& dict);
-    
-                    if (outputRank == SentinelValueForInferParamInitRank)
-                    outputRank = DefaultParamInitOutputRank;
-    
-    // GetData - Gets metadata from the specified section (into CPU memory)
-// sectionName - section name to retrieve data from
-// numRecords - number of records to read
-// data - pointer to data buffer, if NULL, dataBufferSize will be set to size of required buffer to accomidate request
-// dataBufferSize - [in] size of the databuffer in bytes
-//                  [out] size of buffer filled with data
-// recordStart - record to start reading from, defaults to zero (start of data)
-// returns: true if data remains to be read, false if the end of data was reached
-bool DataReader::GetData(const std::wstring& sectionName, size_t numRecords, void* data, size_t& dataBufferSize, size_t recordStart)
-{
-    bool bRet = true;
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-        bRet &= m_dataReaders[m_ioNames[i]]->GetData(sectionName, numRecords, data, dataBufferSize, recordStart);
-    return bRet;
-}
-    
-    
-    {    std::string GetCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
+    class CensusChannelData : public ChannelData {
+ public:
+  grpc_error* Init(grpc_channel_element* elem,
+                   grpc_channel_element_args* args) override;
 };
     
-        // replace if requested
-    // This happens for model editing.
-    // workList operates on mapped nodes.
-    size_t numRelinked = 0;
-    for (auto& nodeRef : workList)
-    {
-        let iter = replacements.find(nodeRef);
-        if (iter != replacements.end())
-        {
-            assert(nodeRef->GetEnvironmentPtr()); // must be in some network if mapped
-            nodeRef = iter->second; // nodeRef is a reference, so this patches the workList in-place
-            numRelinked++;
-        }
-    }
     
-        virtual void CopyTo(ComputationNodeBasePtr  nodeP, const std::wstring& newName, const CopyNodeFlags flags) const override
-    {
-        Base::CopyTo(nodeP, newName, flags);
-    }
+    {}  // namespace grpc
+
     
-            if (cols0 * wordsInEachSample != rows1)
-            LogicError('LookupTableNode: rows of input 1 is not a multiple of cols of input 0. This usually happens when the feature dimension is not specified as that in the network definition of look-up-table dimension size.');
-    
-    // Forward declarations
-namespace xgboost {
-class TreeUpdater;
-}
+    #include 'opencensus/stats/stats.h'
+#include 'src/cpp/ext/filters/census/grpc_plugin.h'
     
     
     { private:
-  /*! \brief the underlying stream */
-  dmlc::Stream *stream_;
-  /*! \brief buffer to hold data */
-  std::string buffer_;
-  /*! \brief length of valid data in buffer */
-  size_t read_len_;
-  /*! \brief pointer in the buffer */
-  size_t read_ptr_;
+  CensusContext context_;
+  // server method
+  absl::string_view method_;
+  std::string qualified_method_;
+  grpc_slice path_;
+  // Pointer to the grpc_call element
+  grpc_call* gc_;
+  // Authorization context for the call.
+  grpc_auth_context* auth_context_;
+  // Metadata element for census stats.
+  grpc_linked_mdelem census_bin_;
+  // recv callback
+  grpc_metadata_batch* recv_initial_metadata_;
+  grpc_closure* initial_on_done_recv_initial_metadata_;
+  grpc_closure on_done_recv_initial_metadata_;
+  // recv message
+  grpc_closure* initial_on_done_recv_message_;
+  grpc_closure on_done_recv_message_;
+  absl::Time start_time_;
+  absl::Duration elapsed_time_;
+  grpc_core::OrphanablePtr<grpc_core::ByteStream>* recv_message_;
+  uint64_t recv_message_count_;
+  uint64_t sent_message_count_;
+  // Buffer needed for grpc_slice to reference it when adding metatdata to
+  // response.
+  char stats_buf_[kMaxServerStatsLen];
 };
     
-        const size_t* begin = dmlc::BeginPtr(row_indices_);
-    const size_t* end = dmlc::BeginPtr(row_indices_) + row_indices_.size();
-    elem_of_each_node_.emplace_back(Elem(begin, end, 0));
+    const ViewDescriptor& ServerReceivedMessagesPerRpcMinute() {
+  const static ViewDescriptor descriptor =
+      MinuteDescriptor()
+          .set_name('grpc.io/server/received_messages_per_rpc/minute')
+          .set_measure(kRpcServerReceivedMessagesPerRpcMeasureName)
+          .set_aggregation(CountDistributionAggregation())
+          .add_column(ServerMethodTagKey());
+  return descriptor;
+}
+    
+    
+    {
+    {   private:
+    const grpc::string name_;
+    const int value_;
+  };
+  return std::unique_ptr<ServerBuilderOption>(new IntOption(name, value));
+}
+    
+    DynamicThreadPool::~DynamicThreadPool() {
+  std::unique_lock<std::mutex> lock(mu_);
+  shutdown_ = true;
+  cv_.notify_all();
+  while (nthreads_ != 0) {
+    shutdown_cv_.wait(lock);
   }
-  // split rowset into two
-  inline void AddSplit(unsigned node_id,
-                       const std::vector<Split>& row_split_tloc,
-                       unsigned left_node_id,
-                       unsigned right_node_id) {
-    const Elem e = elem_of_each_node_[node_id];
-    const auto nthread = static_cast<bst_omp_uint>(row_split_tloc.size());
-    CHECK(e.begin != nullptr);
-    size_t* all_begin = dmlc::BeginPtr(row_indices_);
-    size_t* begin = all_begin + (e.begin - all_begin);
+  ReapThreads(&dead_threads_);
+}
     
-    namespace dmlc {
-DMLC_REGISTRY_ENABLE(::xgboost::TreeUpdaterReg);
-}  // namespace dmlc
+    #ifndef GRPC_INTERNAL_CPP_DYNAMIC_THREAD_POOL_H
+#define GRPC_INTERNAL_CPP_DYNAMIC_THREAD_POOL_H
     
-      /**
-   * @brief Constructor
-   */
-  CanFrame() : id(0), len(0), timestamp{0} {
-    std::memset(data, 0, sizeof(data));
+    /**
+ * @brief Parser plugin for logger configurations.
+ */
+class LoggerConfigParserPlugin : public ConfigParserPlugin {
+ public:
+  std::vector<std::string> keys() const override {
+    return {kLoggerKey};
   }
+    }
     
-      /**
-   * @brief Create a pointer to a specified brand of CAN client. The brand is
-   *        set in the parameter.
-   * @param parameter The parameter to create the CAN client.
-   * @return A pointer to the created CAN client.
-   */
-  std::unique_ptr<CanClient> CreateCANClient(const CANCardParameter &parameter);
+      // Generate content to update/add to the config.
+  std::string content;
+  auto s = readFile(kTestDataPath + 'test_parse_items.conf', content);
+  EXPECT_TRUE(s.ok());
+  std::map<std::string, std::string> config;
+  config['awesome'] = content;
     
-    #endif  // MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_ESD_CAN_CLIENT_H_
+    class ViewsConfigParserPluginTests : public testing::Test {};
+    
+    TEST_F(QueryTests, test_get_query_results) {
+  // Grab an expected set of query data and add it as the previous result.
+  auto encoded_qd = getSerializedQueryDataJSON();
+  auto query = getOsqueryScheduledQuery();
+  auto status = setDatabaseValue(kQueries, 'foobar', encoded_qd.first);
+  EXPECT_TRUE(status.ok());
+    }
+    
+    void Initializer::platformSetup() {
+  // Initialize the COM libraries utilized by Windows WMI calls.
+  auto ret = ::CoInitializeEx(0, COINIT_MULTITHREADED);
+  if (ret != S_OK) {
+    ::CoUninitialize();
+  }
+}
+    
+    const uint32_t kFileDefaultMasks = IN_MOVED_TO | IN_MOVED_FROM | IN_MODIFY |
+                                   IN_DELETE | IN_CREATE | IN_CLOSE_WRITE |
+                                   IN_ATTRIB;
+const uint32_t kFileAccessMasks = IN_OPEN | IN_ACCESS;
+    
+    
+    {        D3DCompile(pixelShader, strlen(pixelShader), NULL, NULL, NULL, 'main', 'ps_4_0', 0, 0, &g_pPixelShaderBlob, NULL);
+        if (g_pPixelShaderBlob == NULL)  // NB: Pass ID3D10Blob* pErrorBlob to D3DCompile() to get error showing in (const char*)pErrorBlob->GetBufferPointer(). Make sure to Release() the blob!
+            return false;
+        if (g_pd3dDevice->CreatePixelShader((DWORD*)g_pPixelShaderBlob->GetBufferPointer(), g_pPixelShaderBlob->GetBufferSize(), NULL, &g_pPixelShader) != S_OK)
+            return false;
+    }
+    
+    
+    {    switch (ev->type)
+    {
+    case ALLEGRO_EVENT_MOUSE_AXES:
+        io.MouseWheel += ev->mouse.dz;
+        io.MouseWheelH += ev->mouse.dw;
+        return true;
+    case ALLEGRO_EVENT_KEY_CHAR:
+        if (ev->keyboard.display == g_Display)
+            if (ev->keyboard.unichar > 0 && ev->keyboard.unichar < 0x10000)
+                io.AddInputCharacter((unsigned short)ev->keyboard.unichar);
+        return true;
+    case ALLEGRO_EVENT_KEY_DOWN:
+    case ALLEGRO_EVENT_KEY_UP:
+        if (ev->keyboard.display == g_Display)
+            io.KeysDown[ev->keyboard.keycode] = (ev->type == ALLEGRO_EVENT_KEY_DOWN);
+        return true;
+    }
+    return false;
+}
+    
+        const char* s = text_begin;
+    while (s < text_end)
+    {
+        if (word_wrap_enabled)
+        {
+            // Calculate how far we can render. Requires two passes on the string data but keeps the code simple and not intrusive for what's essentially an uncommon feature.
+            if (!word_wrap_eol)
+            {
+                word_wrap_eol = CalcWordWrapPositionA(scale, s, text_end, wrap_width - line_width);
+                if (word_wrap_eol == s) // Wrap_width is too small to fit anything. Force displaying 1 character to minimize the height discontinuity.
+                    word_wrap_eol++;    // +1 may not be a character start point in UTF-8 but it's ok because we use s >= word_wrap_eol below
+            }
+    }
+    }
+    
+        // Stateful path API, add points then finish with PathFillConvex() or PathStroke()
+    inline    void  PathClear()                                                 { _Path.resize(0); }
+    inline    void  PathLineTo(const ImVec2& pos)                               { _Path.push_back(pos); }
+    inline    void  PathLineToMergeDuplicate(const ImVec2& pos)                 { if (_Path.Size == 0 || memcmp(&_Path[_Path.Size-1], &pos, 8) != 0) _Path.push_back(pos); }
+    inline    void  PathFillConvex(ImU32 col)                                   { AddConvexPolyFilled(_Path.Data, _Path.Size, col); PathClear(); }  // Note: Anti-aliased filling requires points to be in clockwise order.
+    inline    void  PathStroke(ImU32 col, bool closed, float thickness = 1.0f)  { AddPolyline(_Path.Data, _Path.Size, col, closed, thickness); PathClear(); }
+    IMGUI_API void  PathArcTo(const ImVec2& centre, float radius, float a_min, float a_max, int num_segments = 10);
+    IMGUI_API void  PathArcToFast(const ImVec2& centre, float radius, int a_min_of_12, int a_max_of_12);                                            // Use precomputed angles for a 12 steps circle
+    IMGUI_API void  PathBezierCurveTo(const ImVec2& p1, const ImVec2& p2, const ImVec2& p3, int num_segments = 0);
+    IMGUI_API void  PathRect(const ImVec2& rect_min, const ImVec2& rect_max, float rounding = 0.0f, int rounding_corners_flags = ImDrawCornerFlags_All);
+    
+            ImGui::Text('Color button with Picker:');
+        ImGui::SameLine(); ShowHelpMarker('With the ImGuiColorEditFlags_NoInputs flag you can hide all the slider/text inputs.\nWith the ImGuiColorEditFlags_NoLabel flag you can pass a non-empty label which will only be used for the tooltip and picker popup.');
+        ImGui::ColorEdit4('MyColor##3', (float*)&color, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel | misc_flags);
+    
+        VkPipelineVertexInputStateCreateInfo vertex_info = {};
+    vertex_info.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+    vertex_info.vertexBindingDescriptionCount = 1;
+    vertex_info.pVertexBindingDescriptions = binding_desc;
+    vertex_info.vertexAttributeDescriptionCount = 3;
+    vertex_info.pVertexAttributeDescriptions = attribute_desc;
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    IMGUI_IMPL_API bool     ImGui_Marmalade_Init(bool install_callbacks);
+IMGUI_IMPL_API void     ImGui_Marmalade_Shutdown();
+IMGUI_IMPL_API void     ImGui_Marmalade_NewFrame();
+IMGUI_IMPL_API void     ImGui_Marmalade_RenderDrawData(ImDrawData* draw_data);
+    
+    // Called by Init/NewFrame/Shutdown
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateFontsTexture();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyFontsTexture();
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyDeviceObjects();
 
     
+    class ButteraugliComparator : public Comparator {
+ public:
+  ButteraugliComparator(const int width, const int height,
+                        const std::vector<uint8_t>* rgb,
+                        const float target_distance, ProcessStats* stats);
+    }
     
-    {  EsdCanClient esd_can_client;
-  EXPECT_TRUE(esd_can_client.Init(param));
-  EXPECT_EQ(esd_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
-  std::vector<CanFrame> frames;
-  int32_t num = 0;
-  EXPECT_EQ(esd_can_client.Send(frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  EXPECT_EQ(esd_can_client.Receive(&frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
-  CanFrame can_frame;
-  frames.push_back(can_frame);
-  EXPECT_EQ(esd_can_client.SendSingleFrame(frames),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  esd_can_client.Stop();
+    void DCT1d(const double* in, int stride, double* out) {
+  for (int x = 0; x < 8; ++x) {
+    out[x * stride] = 0.0;
+    for (int u = 0; u < 8; ++u) {
+      out[x * stride] += kDCTMatrix[8 * x + u] * in[u * stride];
+    }
+  }
 }
     
     
     {
     {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
+    {      // Add back the last sentinel node.
+      tree[j_end + 1] = sentinel;
+    }
+    if (SetDepth(static_cast<int>(2 * n - 1), &tree[0], depth, tree_limit)) {
+      /* We need to pack the Huffman tree in tree_limit bits. If this was not
+         successful, add fake entities to the lowest values and retry. */
+      break;
+    }
+  }
+}
     
-    #include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    void ComputeBlockIDCT(const coeff_t* block, uint8_t* out) {
+  coeff_t colidcts[kDCTBlockSize];
+  const int kColScale = 11;
+  const int kColRound = 1 << (kColScale - 1);
+  for (int x = 0; x < 8; ++x) {
+    int colbuf[8] = { 0 };
+    Compute1dIDCT(&block[x], 8, colbuf);
+    for (int y = 0; y < 8; ++y) {
+      colidcts[8 * y + x] = (colbuf[y] + kColRound) >> kColScale;
+    }
+  }
+  const int kRowScale = 18;
+  const int kRowRound = 257 << (kRowScale - 1);  // includes offset by 128
+  for (int y = 0; y < 8; ++y) {
+    const int rowidx = 8 * y;
+    int rowbuf[8] = { 0 };
+    Compute1dIDCT(&colidcts[rowidx], 1, rowbuf);
+    for (int x = 0; x < 8; ++x) {
+      out[rowidx + x] =
+          std::max(0, std::min(255, (rowbuf[x] + kRowRound) >> kRowScale));
+    }
+  }
+}
     
-      /**
-   * @brief Transform an integer with the size of 4 bytes to its hexadecimal
-   *        represented by a string.
-   * @param value The target integer to transform.
-   * @return Hexadecimal representing the target integer.
-   */
-  static std::string byte_to_hex(const uint32_t value);
+    #include 'guetzli/jpeg_data.h'
     
-    const int32_t CAN_FRAME_SIZE = 8;
-const int32_t MAX_CAN_SEND_FRAME_LEN = 1;
-const int32_t MAX_CAN_RECV_FRAME_LEN = 10;
+    #include 'guetzli/jpeg_data_decoder.h'
     
-    using apollo::common::adapter::AdapterConfig;
-using apollo::common::adapter::AdapterManager;
-using apollo::common::monitor::MonitorMessageItem;
-using apollo::common::Status;
-using apollo::common::ErrorCode;
-using apollo::common::time::Clock;
-using apollo::drivers::canbus::CanClientFactory;
-using apollo::drivers::canbus::CanClient;
-using apollo::drivers::canbus::CanReceiver;
-using apollo::drivers::canbus::SensorCanbusConf;
-    
-    #include 'modules/drivers/canbus/sensor_gflags.h'
+    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
+// quantization table must have 3 * kDCTBlockSize values.
+bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
+                     const int* quant, JPEGData* jpg);
