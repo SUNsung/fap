27e87cac5da1b21dc1784292380d5476fb81dca6
SUNsung <weatherius@gmail.com>
2018-11-12 05:43:44 +0200

    Rewrite [4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb] File in: 2018-11-12 05:43:44

--- ruby/4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb
+++ ruby/4438ApNC1QfUk447svL6EbHYQk44DizFzEMbT1644egQ.rb
@@ -1,214 +1,78 @@
 
         
-                def initialize(object_name, method_name, template_object, checked_value, unchecked_value, options)
-          @checked_value   = checked_value
-          @unchecked_value = unchecked_value
-          super(object_name, method_name, template_object, options)
-        end
-    
-        initializer 'action_view.default_enforce_utf8' do |app|
-      ActiveSupport.on_load(:action_view) do
-        default_enforce_utf8 = app.config.action_view.delete(:default_enforce_utf8)
-        unless default_enforce_utf8.nil?
-          ActionView::Helpers::FormTagHelper.default_enforce_utf8 = default_enforce_utf8
-        end
+            # Check if a reset_password_token is provided in the request
+    def assert_reset_token_passed
+      if params[:reset_password_token].blank?
+        set_flash_message(:alert, :no_token)
+        redirect_to new_session_path(resource_name)
       end
     end
     
-        s = nil
-    homebrew_site_packages = Language::Python.homebrew_site_packages
-    user_site_packages = Language::Python.user_site_packages 'python'
-    pth_file = user_site_packages/'homebrew.pth'
-    instructions = <<-EOS.undent.gsub(/^/, '  ')
-      mkdir -p #{user_site_packages}
-      echo 'import site; site.addsitedir('#{homebrew_site_packages}')' >> #{pth_file}
-    EOS
-    
-      def dump_verbose_config(f = $stdout)
-    f.puts 'HOMEBREW_VERSION: #{HOMEBREW_VERSION}'
-    f.puts 'ORIGIN: #{origin}'
-    f.puts 'HEAD: #{head}'
-    f.puts 'Last commit: #{last_commit}'
-    if CoreTap.instance.installed?
-      f.puts 'Core tap ORIGIN: #{core_tap_origin}'
-      f.puts 'Core tap HEAD: #{core_tap_head}'
-      f.puts 'Core tap last commit: #{core_tap_last_commit}'
-    else
-      f.puts 'Core tap: N/A'
-    end
-    f.puts 'HOMEBREW_PREFIX: #{HOMEBREW_PREFIX}'
-    f.puts 'HOMEBREW_REPOSITORY: #{HOMEBREW_REPOSITORY}'
-    f.puts 'HOMEBREW_CELLAR: #{HOMEBREW_CELLAR}'
-    f.puts 'HOMEBREW_BOTTLE_DOMAIN: #{BottleSpecification::DEFAULT_DOMAIN}'
-    f.puts hardware
-    f.puts 'OS X: #{MacOS.full_version}-#{kernel}'
-    f.puts 'Xcode: #{xcode ? xcode : 'N/A'}'
-    f.puts 'CLT: #{clt ? clt : 'N/A'}'
-    f.puts 'GCC-4.0: build #{gcc_40}' if gcc_40
-    f.puts 'GCC-4.2: build #{gcc_42}' if gcc_42
-    f.puts 'LLVM-GCC: build #{llvm}'  if llvm
-    f.puts 'Clang: #{clang ? '#{clang} build #{clang_build}' : 'N/A'}'
-    f.puts 'MacPorts/Fink: #{macports_or_fink}' if macports_or_fink
-    f.puts 'X11: #{describe_x11}'
-    f.puts 'System Ruby: #{describe_system_ruby}'
-    f.puts 'Perl: #{describe_perl}'
-    f.puts 'Python: #{describe_python}'
-    f.puts 'Ruby: #{describe_ruby}'
-    f.puts 'Java: #{describe_java}'
-  end
-end
-
-    
-        cd HOMEBREW_PREFIX
-    exec 'find', *args
-  end
-    
-      def search_tap(user, repo, rx)
-    if (HOMEBREW_LIBRARY/'Taps/#{user.downcase}/homebrew-#{repo.downcase}').directory? && \
-       user != 'Caskroom'
-      return []
+        def email_changed(record, opts={})
+      devise_mail(record, :email_changed, opts)
     end
     
-          renamed_formulae << [old_full_name, new_full_name] if @report[:A].include? new_full_name
+    class DeviseCreateUsers < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
+      t.string :email,              null: false
+      t.string :encrypted_password, null: true
+      t.timestamps null: false
     end
     
-          export JAVA_HOME='$(/usr/libexec/java_home)'
-      export AWS_ACCESS_KEY='<Your AWS Access ID>'
-      export AWS_SECRET_KEY='<Your AWS Secret Key>'
-      export #{home_name}='#{home_value}'
-    EOS
-  end
-end
-
+      # Defines which strategy can be used to lock an account.
+  # Values: :failed_attempts, :none
+  mattr_accessor :lock_strategy
+  @@lock_strategy = :failed_attempts
     
-          def as_json
-        json = { name: name, slug: slug, type: type }
-        json[:links] = links if links.present?
-        json[:version] = version if version.present? || defined?(@version)
-        json[:release] = release if release.present?
-        json
+    module Devise
+  module Controllers
+    # A module that may be optionally included in a controller in order
+    # to provide remember me behavior. Useful when signing in is done
+    # through a callback, like in OmniAuth.
+    module Rememberable
+      # Return default cookie values retrieved from session options.
+      def self.cookie_values
+        Rails.configuration.session_options.slice(:path, :domain, :secure)
       end
     
-        def to_a
-      @filters.dup
-    end
-    
-        def build_and_queue_request(url, options, &block)
-      request = Request.new(url, request_options.merge(options))
-      request.on_complete(&block) if block
-      queue(request)
-      request
-    end
-    
-        def request_all(urls, &block)
-      if options[:rate_limit]
-        if @@rate_limiter
-          @@rate_limiter.limit = options[:rate_limit]
-        else
-          @@rate_limiter = RateLimiter.new(options[:rate_limit])
-          Typhoeus.before(&@@rate_limiter.to_proc)
+        # @mixin transition($transition) {
+    # to:
+    # @mixin transition($transition...) {
+    def varargify_mixin_definitions(scss, *mixins)
+      scss = scss.dup
+      replaced = []
+      mixins.each do |mixin|
+        if scss.gsub! /(@mixin\s*#{Regexp.quote(mixin)})\((#{SCSS_MIXIN_DEF_ARGS_RE})\)/, '\1(\2...)'
+          replaced << mixin
         end
       end
+      log_transform *replaced unless replaced.empty?
+      scss
+    end
+    }
+    }
     
-        def origin
-      if scheme && host
-        origin = '#{scheme}://#{host}'
-        origin.downcase!
-        origin << ':#{port}' if port
-        origin
-      else
-        nil
+      # Set to :debug to see everything in the log.
+  config.log_level = :info
+    
+      namespace :release do
+    GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
+      desc 'Release #{gem} as a package'
+      task gem => 'package:#{gem}' do
+        sh <<-SH
+          gem install #{package(gem, '.gem')} --local &&
+          gem push #{package(gem, '.gem')}
+        SH
       end
     end
     
-          def include_default_entry?
-        INDEX.add?([name, type].join(';')) ? true : false # ¯\_(ツ)_/¯
+          def initialize(app, options = {})
+        @app, @options = app, default_options.merge(options)
       end
     
-      # Loaded immediately after dependencies to ensure proper override of their
-  # UI methods.
-  #
-  require 'cocoapods/user_interface'
-    
-      if options.respond_to? 'keys'
-    options.each do |k,v|
-      unless v.nil?
-        v = v.join ',' if v.respond_to? 'join'
-        v = v.to_json if v.respond_to? 'keys'
-        output += ' data-#{k.sub'_','-'}='#{v}''
-      end
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
     end
-  elsif options.respond_to? 'join'
-    output += ' data-value='#{config[key].join(',')}''
-  else
-    output += ' data-value='#{config[key]}''
-  end
-  output += '></#{tag}>'
-end
-    
-        def get_gist_url_for(gist, file)
-      'https://gist.githubusercontent.com/raw/#{gist}/#{file}'
-    end
-    
-      class IncludeArrayTag < Liquid::Tag
-    Syntax = /(#{Liquid::QuotedFragment}+)/
-    def initialize(tag_name, markup, tokens)
-      if markup =~ Syntax
-        @array_name = $1
-      else
-        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
-      end
-    
-          # Custom destructuring method. This can be used to normalize
-      # destructuring for different variations of the node.
-      #
-      # In this case, the `def` node destructures into:
-      #
-      #   `method_name, arguments, body`
-      #
-      # while the `defs` node destructures into:
-      #
-      #   `receiver, method_name, arguments, body`
-      #
-      # so we reverse the destructured array to get the optional receiver
-      # at the end, where it can be discarded.
-      #
-      # @return [Array] the different parts of the `def` or `defs` node
-      def node_parts
-        to_a.reverse
-      end
-    end
-  end
-end
-
-    
-          # Calls the given block for each `value` node in the `hash` literal.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_value
-        return pairs.map(&:value).to_enum unless block_given?
-    
-    module RuboCop
-  module AST
-    # Common functionality for nodes that can be used as hash elements:
-    # `pair`, `kwsplat`
-    module HashElementNode
-      # Returns the key of this `hash` element.
-      #
-      # @note For keyword splats, this returns the whole node
-      #
-      # @return [Node] the key of the hash element
-      def key
-        node_parts[0]
-      end
-    
-          # A shorthand for getting the first argument of the node.
-      # Equivalent to `arguments.first`.
-      #
-      # @return [Node, nil] the first argument of the node,
-      #                     or `nil` if there are no arguments
-      def first_argument
-        arguments[0]
-      end
