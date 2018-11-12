9352df97430558342fc1979a78da9e77f768da69
SUNsung <weatherius@gmail.com>
2018-11-12 23:37:32 +0200

    Rewrite [219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb] File in: 2018-11-12 23:37:32

--- ruby/219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb
+++ ruby/219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb
@@ -1,82 +1,25 @@
 
         
-              it 'requires the passwords to match when changing them' do
-        visit edit_admin_user_path(users(:bob))
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: 'no_match'
-        click_on 'Update User'
-        expect(page).to have_text('Password confirmation doesn't match')
+          preflight do
+    processes = system_command '/bin/launchctl', args: ['list']
+    
+          def html?(headers)
+        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
+        options[:html_types].include? header.last[/^\w+\/\w+/]
       end
     end
-    
-      it 'imports a scenario that does not exist yet' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
-    expect(page).not_to have_text('This Scenario already exists in your system.')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
   end
+end
+
     
-    describe AgentRunner do
-  context 'without traps' do
-    before do
-      stub.instance_of(Rufus::Scheduler).every
-      stub.instance_of(AgentRunner).set_traps
-      @agent_runner = AgentRunner.new
+        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      subject.options[:allow_empty_referrer] = false
+      expect(subject.referrer(env)).to be_nil
     end
     
-      def failed_strategy
-    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
-  end
-    
-          private
-    
-        delegate :flash, to: :request
-    
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
-    
-              # Require that names be an array
-          names ||= []
-          names = [names] if !names.is_a?(Array)
-    
-              @registered.each do |plugin|
-            providers.merge!(plugin.provider.to_hash)
-          end
-    
-              # Return the result
-          result
-        end
-    
-            # This contains all the provider plugins by name, and returns
-        # the provider class and options.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :providers
-    
-              # Return the dummy object so that anything else works
-          ::Vagrant::Config::V2::DummyConfig.new
-        end
-    
-              results
-        end
-    
-        def resource_params
-      params.require(:account_moderation_note).permit(
-        :content,
-        :target_account_id
-      )
-    end
-    
-      def encoded_challenge
-    HTMLEntities.new.encode(params['hub.challenge'])
+      def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
   end
