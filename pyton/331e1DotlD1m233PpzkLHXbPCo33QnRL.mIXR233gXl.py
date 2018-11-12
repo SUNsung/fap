a61e289fcaf93f54abdd9075562d62416608c900
SUNsung <weatherius@gmail.com>
2018-11-12 17:09:56 +0200

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2018-11-12 17:09:56

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,3 +1,222 @@
 
         
-        
+            def cycle_key(self):
+        '''
+        Keep the same data but with a new key. Call save() and it will
+        automatically save a cookie with a new key at the end of the request.
+        '''
+        self.save()
+    
+       >>> payload = dict(key1='value1', key2='value2')
+   >>> r = requests.post('https://httpbin.org/post', data=payload)
+   >>> print(r.text)
+   {
+     ...
+     'form': {
+       'key2': 'value2',
+       'key1': 'value1'
+     },
+     ...
+   }
+    
+        By default this will get the strings from the blns.txt file
+    
+    
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
+
+    
+    
+class Fixed(jose.Field):
+    '''Fixed field.'''
+    
+    
+class FixedTest(unittest.TestCase):
+    '''Tests for acme.fields.Fixed.'''
+    
+    regr = acme.register()
+logging.info('Auto-accepting TOS: %s', regr.terms_of_service)
+acme.agree_to_tos(regr)
+logging.debug(regr)
+    
+    ALL_SSL_OPTIONS_HASHES = [
+    '2086bca02db48daf93468332543c60ac6acdb6f0b58c7bfdf578a5d47092f82a',
+    '4844d36c9a0f587172d9fa10f4f1c9518e3bcfa1947379f155e16a70a728c21a',
+    '5a922826719981c0a234b1fbcd495f3213e49d2519e845ea0748ba513044b65b',
+    '4066b90268c03c9ba0201068eaa39abbc02acf9558bb45a788b630eb85dadf27',
+    'f175e2e7c673bd88d0aff8220735f385f916142c44aa83b09f1df88dd4767a88',
+    'cfdd7c18d2025836ea3307399f509cfb1ebf2612c87dd600a65da2a8e2f2797b',
+    '80720bd171ccdc2e6b917ded340defae66919e4624962396b992b7218a561791',
+    'c0c022ea6b8a51ecc8f1003d0a04af6c3f2bc1c3ce506b3c2dfc1f11ef931082',
+]
+'''SHA256 hashes of the contents of previous versions of all versions of MOD_SSL_CONF_SRC'''
+    
+        def test_include_variable(self):
+        self.verify_fnmatch('../complex_parsing/${fnmatch_filename}')
+    
+        def test_nonexistent_like(self):
+        with mock.patch('certbot.util.get_os_info') as mock_info:
+            mock_info.return_value = ('nonexistent', 'irrelevant')
+            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
+                for like in entrypoint.OVERRIDE_CLASSES.keys():
+                    mock_like.return_value = [like]
+                    self.assertEqual(entrypoint.get_configurator(),
+                                     entrypoint.OVERRIDE_CLASSES[like])
+    
+    
+class VirtualHostTest(unittest.TestCase):
+    '''Test the VirtualHost class.'''
+    
+    
+def getctime(filename):
+    '''Return the metadata change time of a file, reported by os.stat().'''
+    return os.stat(filename).st_ctime
+    
+        def test_init_dev_mode(self):
+        config = {
+            'dev_mode': 1,
+            'faulthandler': 1,
+            'allocator': 'debug',
+        }
+        self.check_config('init_dev_mode', config)
+    
+            if not encoding:
+            encoding = 'UTF-8'
+        if not errors:
+            errors = 'strict'
+        self.assertEqual(out, f'{encoding}/{errors}')
+    
+        def testThreading(self):
+        # Issue #7205: Using a BZ2File from several threads shouldn't deadlock.
+        data = b'1' * 2**20
+        nthreads = 10
+        with BZ2File(self.filename, 'wb') as f:
+            def comp():
+                for i in range(5):
+                    f.write(data)
+            threads = [threading.Thread(target=comp) for i in range(nthreads)]
+            with support.start_threads(threads):
+                pass
+    
+    from argparse import ArgumentParser
+    
+    from multiprocessing import Process, Queue, current_process, freeze_support
+    
+    '''
+Reference: https://en.wikipedia.org/wiki/Delegation_pattern
+Author: https://github.com/IuryAlves
+    
+        def update(self):
+        print('Updating the test results in Database')
+        time.sleep(0.1)
+    
+        def __init__(self, one, other):
+        self._one = one
+        self._other = other
+    
+      # 0 is not False
+  assert_that( _HandlePollResponse( 0, None ), equal_to( True ) )
+    
+      eq_( request.Response(), {
+    'completions': results,
+    'completion_start_column': 1
+  } )
+    
+    
+@patch( 'ycm.vimsupport.CurrentFiletypes', return_value = [ 'cs' ] )
+def GetCompleteDoneHooks_ResultOnCsharp_test( *args ):
+  request = CompletionRequest( None )
+  result = list( request._GetCompleteDoneHooks() )
+  eq_( result, [ request._OnCompleteDone_Csharp ] )
+    
+      # Ordered to ensure that the calls to update are deterministic
+  diagnostics_per_file = [
+    ( '/current', [ {
+        'kind': 'ERROR',
+        'text': 'error text in current buffer',
+        'location': {
+          'filepath': '/current',
+          'line_num': 1,
+          'column_num': 1
+        },
+        'location_extent': {
+          'start': {
+            'filepath': '/current',
+            'line_num': 1,
+            'column_num': 1,
+          },
+          'end': {
+            'filepath': '/current',
+            'line_num': 1,
+            'column_num': 1,
+          }
+        },
+        'ranges': [],
+      } ] ),
+    ( '/separate_window', [ {
+        'kind': 'ERROR',
+        'text': 'error text in a buffer open in a separate window',
+        'location': {
+          'filepath': '/separate_window',
+          'line_num': 3,
+          'column_num': 3
+        },
+        'location_extent': {
+          'start': {
+            'filepath': '/separate_window',
+            'line_num': 3,
+            'column_num': 3,
+          },
+          'end': {
+            'filepath': '/separate_window',
+            'line_num': 3,
+            'column_num': 3,
+          }
+        },
+        'ranges': []
+      } ] ),
+    ( '/hidden', [ {
+        'kind': 'ERROR',
+        'text': 'error text in hidden buffer',
+        'location': {
+          'filepath': '/hidden',
+          'line_num': 4,
+          'column_num': 2
+        },
+        'location_extent': {
+          'start': {
+            'filepath': '/hidden',
+            'line_num': 4,
+            'column_num': 2,
+          },
+          'end': {
+            'filepath': '/hidden',
+            'line_num': 4,
+            'column_num': 2,
+          }
+        },
+        'ranges': []
+      } ] ),
+    ( '/not_open', [ {
+        'kind': 'ERROR',
+        'text': 'error text in buffer not open in Vim',
+        'location': {
+          'filepath': '/not_open',
+          'line_num': 8,
+          'column_num': 4
+        },
+        'location_extent': {
+          'start': {
+            'filepath': '/not_open',
+            'line_num': 8,
+            'column_num': 4,
+          },
+          'end': {
+            'filepath': '/not_open',
+            'line_num': 8,
+            'column_num': 4,
+          }
+        },
+        'ranges': []
+      } ] )
+  ]
