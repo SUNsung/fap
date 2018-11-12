bd977cd6dc8540c2e66d33b02db6aca1d7e572e5
SUNsung <weatherius@gmail.com>
2018-11-12 21:52:58 +0200

    Rewrite [435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb] File in: 2018-11-12 21:52:57

--- ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
+++ ruby/435kO3PoUjRHk430wZQF0OUujw43slTJBTU0i5E43L2P.rb
@@ -1,27 +1,53 @@
 
         
-        group :docs do
-  gem 'typhoeus'
-  gem 'nokogiri'
-  gem 'html-pipeline'
-  gem 'image_optim'
-  gem 'image_optim_pack', platforms: :ruby
-  gem 'progress_bar', require: false
-  gem 'unix_utils', require: false
-  gem 'tty-pager', require: false
-end
+          def internal_commands
+    find_internal_commands HOMEBREW_LIBRARY_PATH/'cmd'
+  end
     
-      class DocNotFound < NameError; end
+              if File.extname(file) == '.rb'
+            tree[subtree] ||= []
+            tree[subtree] << file
+          end
+        end
+      end
     
-        def initialize(name = nil, path = nil, type = nil)
-      self.name = name
-      self.path = path
-      self.type = type
+          def cookie_paths(path)
+        path = '/' if path.to_s.empty?
+        paths = []
+        Pathname.new(path).descend { |p| paths << p.to_s }
+        paths
+      end
     
-        def format_url(url)
-      url.to_s.remove %r{\Ahttps?://}
+          def escape(object)
+        case object
+        when Hash   then escape_hash(object)
+        when Array  then object.map { |o| escape(o) }
+        when String then escape_string(object)
+        when Tempfile then object
+        else nil
+        end
+      end
+    
+        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      subject.options[:allow_empty_referrer] = false
+      expect(subject.referrer(env)).to be_nil
     end
     
-      # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-end
+          subject do
+        plugin_class.new(
+          'oneString' => '${FunString:foo}',
+          'oneBoolean' => '${FunBool:false}',
+          'oneArray' => [ 'first array value', '${FunString:foo}' ],
+          'oneHash' => { 'key1' => '${FunString:foo}', 'key2' => '${FunString} is ${FunBool}', 'key3' => '${FunBool:false} or ${funbool:false}' },
+          'nestedHash' => { 'level1' => { 'key1' => 'http://${FunString}:8080/blah.txt' } },
+          'nestedArray' => { 'level1' => [{ 'key1' => 'http://${FunString}:8080/blah.txt' }, { 'key2' => 'http://${FunString}:8080/foo.txt' }] },
+          'deepHash' => { 'level1' => { 'level2' => {'level3' => { 'key1' => 'http://${FunString}:8080/blah.txt' } } } }
+        )
+      end
+    
+              it 'successfully install the plugin when verification is disabled' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest')
+          end
