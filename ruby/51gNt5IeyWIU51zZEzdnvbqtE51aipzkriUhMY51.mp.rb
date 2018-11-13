62c47e386565cc39cecc6b4e1eec5d0701cdad2d
SUNsung <weatherius@gmail.com>
2018-11-13 08:09:39 +0200

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2018-11-13 08:09:38

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,134 +1,69 @@
 
         
-          def self.installed
-    Dir['#{store_path}/**/index.json'].
-      map { |file| file[%r{/([^/]*)/index\.json\z}, 1] }.
-      sort!.
-      map { |path| all_versions.find { |doc| doc.path == path } }.
-      compact
-  end
+              it 'Converts a string value for :commits_count' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          changelog_from_git_commits(commits_count: '10')
+        end').runner.execute(:test)
     
-          def as_json
-        json = { name: name, slug: slug, type: type }
-        json[:links] = links if links.present?
-        json[:version] = version if version.present? || defined?(@version)
-        json[:release] = release if release.present?
-        json
-      end
+          it 'works given the path to compile_commands.json' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json'
+            )
+          end').runner.execute(:test)
     
-        SPLIT_INTS = /(?<=\d)\.(?=[\s\d])/.freeze
+        str = str.dup
     
-        def at_xpath(*args)
-      doc.at_xpath(*args)
-    end
+    (allow file-ioctl)
+(allow sysctl-read)
+(allow mach-lookup)
+(allow ipc-posix-shm)
+(allow process-fork)
+(allow system-socket)
     
-        def inheritable_copy
-      self.class.new @filters
-    end
+        %w[iOS macOS].each do |platform|
+        abstract_target '#{platform} Pods' do
+            project '#{platform} Modules.xcodeproj'
     
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
-      end
-    end
+    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
+Gem.use_paths(LogStash::Environment.logstash_gem_home)
     
-        def effective_url
-      @effective_url ||= URL.parse super
-    end
+    module LogStash module PluginManager module PackFetchStrategy
+  class Repository
+    DEFAULT_PACK_URL = 'https://artifacts.elastic.co/downloads/logstash-plugins'
+    PACK_EXTENSION = 'zip'
     
-          str.truncate(max_length).ljust(max_length) << tag.to_s
-    end
-    
-          base = path
-      dest = url.path
-    
-      private
-    
-        def set_account_moderation_note
-      @account_moderation_note = AccountModerationNote.find(params[:id])
+        def valid_format?(local_file)
+      ::File.extname(local_file).downcase == PACK_EXTENSION
     end
   end
-end
+end end end
 
     
-        def destroy
-      authorize @custom_emoji, :destroy?
-      @custom_emoji.destroy!
-      log_action :destroy, @custom_emoji
-      flash[:notice] = I18n.t('admin.custom_emojis.destroyed_msg')
-      redirect_to admin_custom_emojis_path(page: params[:page], **@filter_params)
-    end
-    
-          @domain_block = DomainBlock.new(resource_params)
-    
-          @email_domain_block = EmailDomainBlock.new(resource_params)
-    
-        private
-    
-        def pos
-      byte_to_str_pos @s.pos
-    end
-    
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
-    
-        # @todo If a command is run inside another one some settings which where
-    #       true might return false.
-    #
-    # @todo We should probably not even load colored unless needed.
-    #
-    # @todo Move silent flag to CLAide.
-    #
-    # @note It is important that the commands don't override the default
-    #       settings if their flag is missing (i.e. their value is nil)
-    #
-    def initialize(argv)
-      super
-      config.silent = argv.flag?('silent', config.silent)
-      config.verbose = self.verbose? unless verbose.nil?
-      unless self.ansi_output?
-        Colored2.disable!
-        String.send(:define_method, :colorize) { |string, _| string }
-      end
-    end
-    
-        it 'registers an offense for one hash parameter with braces and multiple ' \
-       'keys' do
-      expect_offense(<<-RUBY.strip_indent)
-        where({ x: 1, foo: 'bar' })
-              ^^^^^^^^^^^^^^^^^^^^ Redundant curly braces around a hash parameter.
-      RUBY
-    end
-  end
-    
-      include_examples 'multiline literal brace layout' do
-    let(:open) { '[' }
-    let(:close) { ']' }
-  end
-    
-          it 'allows closing brace on different line from multi-line element' do
-        expect_no_offenses(construct(false, a, make_multi(multi), true))
+        context 'without a specific plugin' do
+      it 'display a list of plugins' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list')
+        expect(result.stdout.split('\n').size).to be > 1
       end
     
-          it 'detects closing brace on separate line from last element' do
-        inspect_source(source)
+    @@ layout
+<html>
+  <head>
+    <title>Sinatra + Sidekiq</title>
+    <body>
+      <%= yield %>
+    </body>
+</html>
     
-    module RuboCop
-  module AST
-    # A node extension for `def` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `def` nodes within RuboCop.
-    class DefNode < Node
-      include ParameterizedNode
-      include MethodIdentifierPredicates
-    
-          # Checks whether this node is an `if` statement. (This is not true of
-      # ternary operators and `unless` statements.)
-      #
-      # @return [Boolean] whether the node is an `if` statement
-      def if?
-        keyword == 'if'
+          def queue_name
+        queue.sub(/.*queue:/, '')
       end
+    
+          def add(klass, *args)
+        remove(klass) if exists?(klass)
+        entries << Entry.new(klass, *args)
+      end
+    
+        def identity
+      @@identity ||= '#{hostname}:#{$$}:#{process_nonce}'
+    end
