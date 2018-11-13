2cc62d9290ae259aa5235084273a27fab8497395
SUNsung <weatherius@gmail.com>
2018-11-13 09:53:32 +0200

    Rewrite [02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py] File in: 2018-11-13 09:53:32

--- pyton/02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py
+++ pyton/02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py
@@ -1,149 +1,185 @@
 
         
-        
-def main():
-    parser = optparse.OptionParser(usage='%prog CHANGELOG VERSION BUILDPATH')
-    options, args = parser.parse_args()
-    if len(args) != 3:
-        parser.error('Expected a version and a build directory')
+            monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
+
     
-        with open(FISH_COMPLETION_TEMPLATE) as f:
-        template = f.read()
-    filled_template = template.replace('{{commands}}', '\n'.join(commands))
-    with open(FISH_COMPLETION_FILE, 'w') as f:
-        f.write(filled_template)
-    
-    signature = hexlify(rsa.pkcs1.sign(json.dumps(versions_info, sort_keys=True).encode('utf-8'), privkey, 'SHA-256')).decode()
-print('signature: ' + signature)
-    
-    README_FILE = 'README.md'
-helptext = sys.stdin.read()
+    This typically means that you attempted to use functionality that needed
+an active HTTP request.  Consult the documentation on testing for
+information about how to avoid this problem.\
+'''
+_app_ctx_err_msg = '''\
+Working outside of application context.
     
     
-def report_warning(message):
+def get_load_dotenv(default=True):
+    '''Get whether the user has disabled loading dotenv files by setting
+    :envvar:`FLASK_SKIP_DOTENV`. The default is ``True``, load the
+    files.
+    
+        * :class:`dict`
+    * :class:`tuple`
+    * :class:`bytes`
+    * :class:`~flask.Markup`
+    * :class:`~uuid.UUID`
+    * :class:`~datetime.datetime`
     '''
-    Print the message to stderr, it will be prefixed with 'WARNING:'
-    If stderr is a tty file the 'WARNING:' will be colored
-    '''
-    if sys.stderr.isatty() and compat_os_name != 'nt':
-        _msg_header = '\033[0;33mWARNING:\033[0m'
-    else:
-        _msg_header = 'WARNING:'
-    output = '%s %s\n' % (_msg_header, message)
-    if 'b' in getattr(sys.stderr, 'mode', '') or sys.version_info[0] < 3:
-        output = output.encode(preferredencoding())
-    sys.stderr.write(output)
     
-        def test_parse_xspf(self):
-        _TEST_CASES = [
-            (
-                'foo_xspf',
-                'https://example.org/src/foo_xspf.xspf',
-                [{
-                    'id': 'foo_xspf',
-                    'title': 'Pandemonium',
-                    'description': 'Visit http://bigbrother404.bandcamp.com',
-                    'duration': 202.416,
-                    'formats': [{
-                        'manifest_url': 'https://example.org/src/foo_xspf.xspf',
-                        'url': 'https://example.org/src/cd1/track%201.mp3',
-                    }],
-                }, {
-                    'id': 'foo_xspf',
-                    'title': 'Final Cartridge (Nichico Twelve Remix)',
-                    'description': 'Visit http://bigbrother404.bandcamp.com',
-                    'duration': 255.857,
-                    'formats': [{
-                        'manifest_url': 'https://example.org/src/foo_xspf.xspf',
-                        'url': 'https://example.org/%E3%83%88%E3%83%A9%E3%83%83%E3%82%AF%E3%80%80%EF%BC%92.mp3',
-                    }],
-                }, {
-                    'id': 'foo_xspf',
-                    'title': 'Rebuilding Nightingale',
-                    'description': 'Visit http://bigbrother404.bandcamp.com',
-                    'duration': 287.915,
-                    'formats': [{
-                        'manifest_url': 'https://example.org/src/foo_xspf.xspf',
-                        'url': 'https://example.org/src/track3.mp3',
-                    }, {
-                        'manifest_url': 'https://example.org/src/foo_xspf.xspf',
-                        'url': 'https://example.com/track3.mp3',
-                    }]
-                }]
-            ),
-        ]
+        if not has_level_handler(logger):
+        logger.addHandler(default_handler)
+    
+            status_line = 'HTTP/{version} {status} {reason}'.format(
+            version=version,
+            status=original.status,
+            reason=original.reason
+        )
+        headers = [status_line]
+        try:
+            # `original.msg` is a `http.client.HTTPMessage` on Python 3
+            # `_headers` is a 2-tuple
+            headers.extend(
+                '%s: %s' % header for header in original.msg._headers)
+        except AttributeError:
+            # and a `httplib.HTTPMessage` on Python 2.x
+            # `headers` is a list of `name: val<CRLF>`.
+            headers.extend(h.strip() for h in original.msg.headers)
+    
+            '''
+        available_plugins = plugin_manager.get_formatters_grouped()
+        self.enabled_plugins = []
+        for group in groups:
+            for cls in available_plugins[group]:
+                p = cls(env=env, **kwargs)
+                if p.enabled:
+                    self.enabled_plugins.append(p)
     
     
-class TestAES(unittest.TestCase):
-    def setUp(self):
-        self.key = self.iv = [0x20, 0x15] + 14 * [0]
-        self.secret_msg = b'Secret message goes here'
-    
-    
-class TestAgeRestriction(unittest.TestCase):
-    def _assert_restricted(self, url, filename, age, old_age=None):
-        self.assertTrue(_download_restricted(url, filename, old_age))
-        self.assertFalse(_download_restricted(url, filename, age))
-    
-    
-class ExitStatus:
-    '''Exit status code constants.'''
-    OK = 0
-    ERROR = 1
-    PLUGIN_ERROR = 7
-    
-        '''
-    name = 'HTTP'
-    aliases = ['http']
-    filenames = ['*.http']
-    tokens = {
-        'root': [
-            # Request-Line
-            (r'([A-Z]+)( +)([^ ]+)( +)(HTTP)(/)(\d+\.\d+)',
-             pygments.lexer.bygroups(
-                 pygments.token.Name.Function,
-                 pygments.token.Text,
-                 pygments.token.Name.Namespace,
-                 pygments.token.Text,
-                 pygments.token.Keyword.Reserved,
-                 pygments.token.Operator,
-                 pygments.token.Number
-             )),
-            # Response Status-Line
-            (r'(HTTP)(/)(\d+\.\d+)( +)(\d{3})( +)(.+)',
-             pygments.lexer.bygroups(
-                 pygments.token.Keyword.Reserved,  # 'HTTP'
-                 pygments.token.Operator,  # '/'
-                 pygments.token.Number,  # Version
-                 pygments.token.Text,
-                 pygments.token.Number,  # Status code
-                 pygments.token.Text,
-                 pygments.token.Name.Exception,  # Reason
-             )),
-            # Header
-            (r'(.*?)( *)(:)( *)(.+)', pygments.lexer.bygroups(
-                pygments.token.Name.Attribute,  # Name
-                pygments.token.Text,
-                pygments.token.Operator,  # Colon
-                pygments.token.Text,
-                pygments.token.String  # Value
-            ))
-        ]
-    }
-    
-            Return a ``requests.auth.AuthBase`` subclass instance.
-    
-    
-def test_default_options_overwrite(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['json'] == {'foo': 'bar'}
-    
-        def test_verbose_implies_all(self, httpbin):
-        r = http('--verbose', '--follow', httpbin + '/redirect/1')
-        assert 'GET /redirect/1 HTTP/1.1' in r
-        assert 'HTTP/1.1 302 FOUND' in r
-        assert 'GET /get HTTP/1.1' in r
+class TestImplicitHTTPMethod:
+    def test_implicit_GET(self, httpbin):
+        r = http(httpbin.url + '/get')
         assert HTTP_OK in r
+    
+    
+@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
+@pytest.mark.parametrize('filename', filenames)
+def test_rst_file_syntax(filename):
+    p = subprocess.Popen(
+        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
+        stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE
+    )
+    err = p.communicate()[1]
+    assert p.returncode == 0, err.decode('utf8')
+
+    
+    
+class TestDownloads:
+    # TODO: more tests
+    
+    from httpie import ExitStatus
+from httpie.core import main
+    
+        def test_cert_pem(self, httpbin_secure):
+        r = http(httpbin_secure + '/get',
+                 '--cert', CLIENT_PEM)
+        assert HTTP_OK in r
+    
+    
+def test_unicode_basic_auth(httpbin):
+    # it doesn't really authenticate us because httpbin
+    # doesn't interpret the utf8-encoded auth
+    http('--verbose', '--auth', u'test:%s' % UNICODE,
+         httpbin.url + u'/basic-auth/test/' + UNICODE)
+    
+        Stacks of (1 x 1)-(3 x 3)-(1 x 1) BN-ReLU-Conv2D or also known as
+    bottleneck layer
+    First shortcut connection per layer is 1 x 1 Conv2D.
+    Second and onwards shortcut connection is identity.
+    At the beginning of each stage, the feature map size is halved (downsampled)
+    by a convolutional layer with strides=2, while the number of filter maps is
+    doubled. Within each stage, the layers have the same number filters and the
+    same filter map sizes.
+    Features maps sizes:
+    conv1  : 32x32,  16
+    stage 0: 32x32,  64
+    stage 1: 16x16, 128
+    stage 2:  8x8,  256
+    
+        paths = []
+    for fname in files:
+        paths.append(get_file(fname,
+                              origin=base + fname,
+                              cache_subdir=dirname))
+    
+    
+def test_fashion_mnist():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+        assert len(x_train) == len(y_train) == 60000
+        assert len(x_test) == len(y_test) == 10000
+    
+            self.kernel = self.add_weight(shape=(input_dim, self.units * 3),
+                                      name='kernel',
+                                      initializer=self.kernel_initializer,
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units * 3),
+            name='recurrent_kernel',
+            initializer=self.recurrent_initializer,
+            regularizer=self.recurrent_regularizer,
+            constraint=self.recurrent_constraint)
+    
+        @property
+    def recurrent_activation(self):
+        return self.cell.recurrent_activation
+    
+            if rotation > 0:
+            angle_rad = rotation * 3.141592653589793 / 180.0
+            angles = tf.random_uniform([batch_size], -angle_rad, angle_rad)
+            f = tf.contrib.image.angles_to_projective_transforms(angles,
+                                                                 height, width)
+            transforms.append(f)
+    
+    def get_srt_json(id):
+    url = 'http://danmu.aixifan.com/V2/%s' % id
+    return get_content(url)
+    
+        stream_types = [  #this is just a sample. Will make it in prepare()
+        # {'id': '1080'},
+        # {'id': '720'},
+        # {'id': '360'},
+        # {'id': '288'},
+        # {'id': '190'},
+        # {'id': '180'},
+        
+    ]
+    
+    
+    def extract(self, **kwargs):
+        for i in self.streams:
+            s = self.streams[i]
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
+        if 'stream_id' in kwargs and kwargs['stream_id']:
+            # Extract the stream
+            stream_id = kwargs['stream_id']
+    
+        print_info(site_info, title, mime, size)
+    if not info_only:
+        download_urls([real_url], title, ext, size, output_dir=output_dir, merge=merge)
+    
+    	for video in tab.childNodes:
+		if re.search(contentid, video.attributes['link'].value):
+			url = video.attributes['flv'].value
+			break
+    
+        urls = matchall(content, netease_embed_patterns)
+    for url in urls:
+        found = True
+        netease_download(url, output_dir=output_dir, merge=merge, info_only=info_only)
