d0b8648513695a7ff1b15c45c8b9cf8e9f025131
SUNsung <weatherius@gmail.com>
2018-11-13 03:41:35 +0200

    Rewrite [25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py] File in: 2018-11-13 03:41:34

--- pyton/25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py
+++ pyton/25MPd3hN3i30U25lIK0wa0muZ.25quzdMCjiS7s25YgO.py
@@ -1,56 +1,86 @@
 
         
-            with io.open(changelog_file, encoding='utf-8') as inf:
-        changelog = inf.read()
-    
-    import sys
-import os
-import textwrap
-    
-    
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
-    
-    with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
-    
-            if in_options:
-            if line.lstrip().startswith('-'):
-                split = re.split(r'\s{2,}', line.lstrip())
-                # Description string may start with `-` as well. If there is
-                # only one piece then it's a description bit not an option.
-                if len(split) > 1:
-                    option, description = split
-                    split_option = option.split(' ')
-    
-    # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = [
-    'sphinx.ext.autodoc',
-]
-    
-        requests_session = get_requests_session(ssl_version)
-    requests_session.max_redirects = args.max_redirects
+                # Insert the import statement to setup.py if not present
+        with open(setup_path, 'a+') as setup:
+            setup.seek(0)
+            setup_content = setup.read()
+            if not 'import fastentrypoints' in setup_content:
+                setup.seek(0)
+                setup.truncate()
+                setup.write('import fastentrypoints\n' + setup_content)
     
     
-tests_require = [
-    # Pytest needs to come last.
-    # https://bitbucket.org/pypa/setuptools/issue/196/
-    'pytest-httpbin',
-    'pytest',
-    'mock',
-]
+def test_on_run_after_other_commands(usage_tracker_io, shell_pid, shell, logs):
+    shell_pid.return_value = 12
+    shell.get_history.return_value = ['fuck', 'ls']
+    _change_tracker(usage_tracker_io, 12)
+    main()
+    logs.how_to_configure_alias.assert_called_once()
     
     
-with codecs.open(FILE_PATH, encoding='utf8') as f:
-    # Strip because we don't want new lines in the data so that we can
-    # easily count occurrences also when embedded in JSON (where the new
-    # line would be escaped).
-    FILE_CONTENT = f.read().strip()
+@pytest.fixture(params=[(python_3, False),
+                        (python_3, True),
+                        (python_2, False)])
+def proc(request, spawnu, TIMEOUT):
+    container, instant_mode = request.param
+    proc = spawnu(*container)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(init_bashrc.format(
+        u'--enable-experimental-instant-mode' if instant_mode else ''))
+    proc.sendline(u'bash')
+    if instant_mode:
+        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
+    return proc
+    
+    
+@pytest.mark.functional
+def test_select_command_with_arrows(proc, TIMEOUT):
+    select_command_with_arrows(proc, TIMEOUT)
+    
+    
+def test_match():
+    assert match(Command('apt list --upgradable', match_output))
+    assert match(Command('sudo apt list --upgradable', match_output))
+    
+    
+def write_stream(stream, outfile, flush):
+    '''Write the output stream.'''
+    try:
+        # Writing bytes so we use the buffer interface (Python 3).
+        buf = outfile.buffer
+    except AttributeError:
+        buf = outfile
+    
+        # Adapters
+    def get_transport_plugins(self):
+        return [plugin for plugin in self
+                if issubclass(plugin, TransportPlugin)]
+
+    
+    
+class PyTest(TestCommand):
+    # `$ python setup.py test' simply installs minimal requirements
+    # and runs the tests with no fancy stuff like parallel execution.
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '--doctest-modules', '--verbose',
+            './httpie', './tests'
+        ]
+        self.test_suite = True
+    
+    
+def test_missing_auth(httpbin):
+    r = http(
+        '--auth-type=basic',
+        'GET',
+        httpbin + '/basic-auth/user/password',
+        error_exit_ok=True
+    )
+    assert HTTP_OK not in r
+    assert '--auth required' in r.stderr
+
     
         def test_binary_included_and_correct_when_suitable(self):
         env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
@@ -59,323 +89,70 @@ with codecs.open(FILE_PATH, encoding='utf8') as f:
 
     
     
-def test_default_options(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http(httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['form'] == {'foo': 'bar'}
-    
-    
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
-    
-        exc = ConnectionError('Connection aborted')
+@mock.patch('httpie.core.get_response')
+def test_error_traceback(get_response):
+    exc = ConnectionError('Connection aborted')
     exc.request = Request(method='GET', url='http://www.google.com')
     get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
+    with raises(ConnectionError):
+        main(['--ignore-stdin', '--traceback', 'www.google.com'])
     
+        with gzip.open(paths[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
     
-class UnaccentExtension(CreateExtension):
+        def get_config(self):
+        return {'mse_fraction': self.mse_fraction}
     
-        def __init__(self, get_response=None):
-        if not apps.is_installed('django.contrib.sites'):
-            raise ImproperlyConfigured(
-                'You cannot use RedirectFallbackMiddleware when '
-                'django.contrib.sites is not installed.'
-            )
-        super().__init__(get_response)
+                    outs = fit_function(ins_batch)
+                outs = to_list(outs)
+                for l, o in zip(out_labels, outs):
+                    batch_logs[l] = o
     
-        def create_model_instance(self, data):
-        '''
-        Return a new instance of the session model object, which represents the
-        current session state. Intended to be used for saving the session data
-        to the database.
-        '''
-        return self.model(
-            session_key=self._get_or_create_session_key(),
-            session_data=self.encode(data),
-            expire_date=self.get_expiry_date(),
-        )
+        np.random.seed(args.seed)
     
-        def __init__(self, name=None):
-        self.name = name
-        super(LookupDict, self).__init__()
-    
-        @classmethod
-    def text_response_server(cls, text, request_timeout=0.5, **kwargs):
-        def text_response_handler(sock):
-            request_content = consume_socket_content(sock, timeout=request_timeout)
-            sock.send(text.encode('utf-8'))
-    
-    elif is_py3:
-    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
-    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
-    from http import cookiejar as cookielib
-    from http.cookies import Morsel
-    from io import StringIO
-    from collections import OrderedDict
-    from collections.abc import Callable, Mapping, MutableMapping
-    
-    # TODO: response is the only one
-    
-        def test_multiple_requests(self):
-        '''multiple requests can be served'''
-        requests_to_handle = 5
-    
-        # Informational.
-    100: ('continue',),
-    101: ('switching_protocols',),
-    102: ('processing',),
-    103: ('checkpoint',),
-    122: ('uri_too_long', 'request_uri_too_long'),
-    200: ('ok', 'okay', 'all_ok', 'all_okay', 'all_good', '\\o/', '✓'),
-    201: ('created',),
-    202: ('accepted',),
-    203: ('non_authoritative_info', 'non_authoritative_information'),
-    204: ('no_content',),
-    205: ('reset_content', 'reset'),
-    206: ('partial_content', 'partial'),
-    207: ('multi_status', 'multiple_status', 'multi_stati', 'multiple_stati'),
-    208: ('already_reported',),
-    226: ('im_used',),
-    
-        def describe_stacks(self, stack_name=None):
-        try:
-            kwargs = {'StackName': stack_name} if stack_name else {}
-            func = partial(self.client.describe_stacks, **kwargs)
-            response = self.paginated_response(func, 'Stacks')
-            if response is not None:
-                return response
-            self.module.fail_json(msg='Error describing stack(s) - an empty response was returned')
-        except Exception as e:
-            if 'does not exist' in e.response['Error']['Message']:
-                # missing stack, don't bail.
-                return {}
-            self.module.fail_json(msg='Error describing stack - ' + to_native(e), exception=traceback.format_exc())
-    
-        def list_targets(self):
-        '''Lists the existing targets for the rule in AWS'''
-        try:
-            targets = self.client.list_targets_by_rule(Rule=self.name)
-        except botocore.exceptions.ClientError as e:
-            error_code = e.response.get('Error', {}).get('Code')
-            if error_code == 'ResourceNotFoundException':
-                return []
-            self.module.fail_json_aws(e, msg='Could not find target for rule %s' % self.name)
-        except botocore.exceptions.BotoCoreError as e:
-            self.module.fail_json_aws(e, msg='Could not find target for rule %s' % self.name)
-        return self._snakify(targets)['targets']
-    
-    
-def describe_log_group(client, log_group_name, module):
-    params = {}
-    if log_group_name:
-        params['logGroupNamePrefix'] = log_group_name
-    try:
-        desc_log_group = client.describe_log_groups(**params)
-        return desc_log_group
-    except botocore.exceptions.ClientError as e:
-        module.fail_json(msg='Unable to describe log group {0}: {1}'.format(log_group_name, to_native(e)),
-                         exception=traceback.format_exc(), **camel_dict_to_snake_dict(e.response))
-    except botocore.exceptions.BotoCoreError as e:
-        module.fail_json(msg='Unable to describe log group {0}: {1}'.format(log_group_name, to_native(e)),
-                         exception=traceback.format_exc())
-    
-    
-def main():
-    module = AnsibleAWSModule(
-        argument_spec=dict(
-            filters=dict(type='dict', default={})
-        ),
-        supports_check_mode=True
-    )
-    
-    
-RETURN = '''
-placement_groups:
-  description: Placement group attributes
-  returned: always
-  type: complex
-  contains:
-    name:
-      description: PG name
-      type: string
-      sample: my-cluster
-    state:
-      description: PG state
-      type: string
-      sample: 'available'
-    strategy:
-      description: PG strategy
-      type: string
-      sample: 'cluster'
-    
-    RETURN = '''
-vpc:
-  description: info about the VPC that was created or deleted
-  returned: always
-  type: complex
-  contains:
-    cidr_block:
-      description: The CIDR of the VPC
-      returned: always
-      type: string
-      sample: 10.0.0.0/16
-    cidr_block_association_set:
-      description: IPv4 CIDR blocks associated with the VPC
-      returned: success
-      type: list
-      sample:
-        'cidr_block_association_set': [
-            {
-                'association_id': 'vpc-cidr-assoc-97aeeefd',
-                'cidr_block': '20.0.0.0/24',
-                'cidr_block_state': {
-                    'state': 'associated'
-                }
-            }
-        ]
-    classic_link_enabled:
-      description: indicates whether ClassicLink is enabled
-      returned: always
-      type: NoneType
-      sample: null
-    dhcp_options_id:
-      description: the id of the DHCP options assocaited with this VPC
-      returned: always
-      type: string
-      sample: dopt-0fb8bd6b
-    id:
-      description: VPC resource id
-      returned: always
-      type: string
-      sample: vpc-c2e00da5
-    instance_tenancy:
-      description: indicates whether VPC uses default or dedicated tenancy
-      returned: always
-      type: string
-      sample: default
-    is_default:
-      description: indicates whether this is the default VPC
-      returned: always
-      type: bool
-      sample: false
-    state:
-      description: state of the VPC
-      returned: always
-      type: string
-      sample: available
-    tags:
-      description: tags attached to the VPC, includes name
-      returned: always
-      type: complex
-      contains:
-        Name:
-          description: name tag for the VPC
-          returned: always
-          type: string
-          sample: pk_vpc4
-'''
-    
-        :param registry_id: Optional string containing the registryId.
-    :return: kwargs dict with registryId, if given
+        Returns
+    -------
+    array of floats shaped like (metrics, formats, samples, classes, density)
+        Time in seconds.
     '''
-    if not registry_id:
-        return dict()
-    else:
-        return dict(registryId=registry_id)
+    metrics = np.atleast_1d(metrics)
+    samples = np.atleast_1d(samples)
+    classes = np.atleast_1d(classes)
+    density = np.atleast_1d(density)
+    formats = np.atleast_1d(formats)
+    out = np.zeros((len(metrics), len(formats), len(samples), len(classes),
+                    len(density)), dtype=float)
+    it = itertools.product(samples, classes, density)
+    for i, (s, c, d) in enumerate(it):
+        _, y_true = make_multilabel_classification(n_samples=s, n_features=1,
+                                                   n_classes=c, n_labels=d * c,
+                                                   random_state=42)
+        _, y_pred = make_multilabel_classification(n_samples=s, n_features=1,
+                                                   n_classes=c, n_labels=d * c,
+                                                   random_state=84)
+        for j, f in enumerate(formats):
+            f_true = f(y_true)
+            f_pred = f(y_pred)
+            for k, metric in enumerate(metrics):
+                t = timeit(partial(metric, f_true, f_pred), number=n_times)
     
-        module.exit_json(elasticache_clusters=get_elasticache_clusters(client, module, region))
+                gc.collect()
+            print('benchmarking lars_path (without Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            lars_path(X, y, Gram=None, max_iter=n_informative)
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            lars[i_f, i_s] = delta
     
+        samples_range = np.linspace(2, 1000, 4).astype(np.int)
+    features_range = np.linspace(2, 1000, 4).astype(np.int)
+    results = compute_bench(samples_range, features_range)
     
-DOCUMENTATION = '''
----
-module: elasticache_snapshot
-short_description: Manage cache snapshots in Amazon Elasticache.
-description:
-  - Manage cache snapshots in Amazon Elasticache.
-  - Returns information about the specified snapshot.
-version_added: '2.3'
-author: 'Sloane Hertel (@s-hertel)'
-extends_documentation_fragment:
-  - aws
-  - ec2
-requirements: [ boto3, botocore ]
-options:
-  name:
-    description:
-      - The name of the snapshot we want to create, copy, delete
-    required: yes
-  state:
-    description:
-      - Actions that will create, destroy, or copy a snapshot.
-    choices: ['present', 'absent', 'copy']
-  replication_id:
-    description:
-      - The name of the existing replication group to make the snapshot.
-  cluster_id:
-    description:
-      - The name of an existing cache cluster in the replication group to make the snapshot.
-  target:
-    description:
-      - The name of a snapshot copy
-  bucket:
-    description:
-      - The s3 bucket to which the snapshot is exported
-'''
-    
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-    
-    ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-    
-        def test_invalid_paths(self):
-        for attr in GenericTest.common_attributes:
-            # os.path.commonprefix doesn't raise ValueError
-            if attr == 'commonprefix':
-                continue
-            func = getattr(self.pathmodule, attr)
-            with self.subTest(attr=attr):
-                if attr in ('exists', 'isdir', 'isfile'):
-                    func('/tmp\udfffabcds')
-                    func(b'/tmp\xffabcds')
-                    func('/tmp\x00abcds')
-                    func(b'/tmp\x00abcds')
-                else:
-                    with self.assertRaises((OSError, UnicodeEncodeError)):
-                        func('/tmp\udfffabcds')
-                    with self.assertRaises((OSError, UnicodeDecodeError)):
-                        func(b'/tmp\xffabcds')
-                    with self.assertRaisesRegex(ValueError, 'embedded null'):
-                        func('/tmp\x00abcds')
-                    with self.assertRaisesRegex(ValueError, 'embedded null'):
-                        func(b'/tmp\x00abcds')
-    
-            # The output from _testembed looks like this:
-        # --- Pass 0 ---
-        # interp 0 <0x1cf9330>, thread state <0x1cf9700>: id(modules) = 139650431942728
-        # interp 1 <0x1d4f690>, thread state <0x1d35350>: id(modules) = 139650431165784
-        # interp 2 <0x1d5a690>, thread state <0x1d99ed0>: id(modules) = 139650413140368
-        # interp 3 <0x1d4f690>, thread state <0x1dc3340>: id(modules) = 139650412862200
-        # interp 0 <0x1cf9330>, thread state <0x1cf9700>: id(modules) = 139650431942728
-        # --- Pass 1 ---
-        # ...
-    
-    # Proxy type for generator objects
-class GeneratorProxy(BaseProxy):
-    _exposed_ = ['__next__']
-    def __iter__(self):
-        return self
-    def __next__(self):
-        return self._callmethod('__next__')
+        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
+                                   'blob/{revision}/{package}/'
+                                   '{path}#L{lineno}')
+    '''
+    revision = _get_git_revision()
+    return partial(_linkcode_resolve, revision=revision, package=package,
+                   url_fmt=url_fmt)
