3093f248c326f1f00a444883b80cc81a347e3e6c
SUNsung <weatherius@gmail.com>
2018-11-14 00:47:06 +0200

    Rewrite [46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py] File in: 2018-11-14 00:47:05

--- pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
+++ pyton/46B1OpmE6TdJE46p3ioeE2vIg.46zuIymTlYOVE46cgC.py
@@ -1,98 +1,134 @@
 
         
-        
-@pytest.fixture(autouse=True)
-def functional(request):
-    if request.node.get_marker('functional') \
-            and not request.config.getoption('enable_functional'):
-        pytest.skip('functional tests are disabled')
+                if any([s_line.startswith(s) for s in ['* [', '- [']]):
+            if indent == last_indent:
+                blocks[-1].append(line)
+            else:
+                blocks.append([line])
+            last_indent = indent
+        else:
+            blocks.append([line])
+            last_indent = None
     
     
-@pytest.fixture(params=containers)
+class Call(object):
+    
+            Emit key value pairs of the form:
+    
+    
+class UserGraphService(object):
+    
+    
+class Cache(object):
+    
+        if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+    
+    
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/update',
+))
+def test_create_update_validate(client, auth, path):
+    auth.login()
+    response = client.post(path, data={'title': '', 'body': ''})
+    assert b'Title is required.' in response.data
+    
+            .. versionadded:: 0.11
+        '''
+        rv = {}
+        for k, v in iteritems(self):
+            if not k.startswith(namespace):
+                continue
+            if trim_namespace:
+                key = k[len(namespace):]
+            else:
+                key = k
+            if lowercase:
+                key = key.lower()
+            rv[key] = v
+        return rv
+    
+    
+    .. versionchanged:: 0.11
+       Added support for serializing top-level arrays. This introduces a
+       security risk in ancient browsers. See :ref:`json-security` for details.
+    
+            httponly = self.get_cookie_httponly(app)
+        secure = self.get_cookie_secure(app)
+        samesite = self.get_cookie_samesite(app)
+        expires = self.get_expiration_time(app, session)
+        val = self.get_signing_serializer(app).dumps(dict(session))
+        response.set_cookie(
+            app.session_cookie_name,
+            val,
+            expires=expires,
+            httponly=httponly,
+            domain=domain,
+            path=path,
+            secure=secure,
+            samesite=samesite
+        )
+
+    
+    
+@pytest.fixture(params=[(python_3, False),
+                        (python_3, True),
+                        (python_2, False)])
 def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
+    container, instant_mode = request.param
+    proc = spawnu(*container)
     proc.sendline(u'pip install /src')
     assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
-    proc.sendline(u'fish')
+    proc.sendline(init_zshrc.format(
+        u'--enable-experimental-instant-mode' if instant_mode else ''))
+    proc.sendline(u'zsh')
+    if instant_mode:
+        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
     return proc
     
-    python_2 = ('thefuck/python2-zsh',
-            u'''FROM python:2
-                RUN apt-get update
-                RUN apt-get install -yy zsh''',
-            u'sh')
     
-        new_command = get_new_command(Command('apt update', match_output))
-    assert new_command == 'apt list --upgradable'
+@pytest.mark.parametrize('command', [
+    Command('apt-cache search foo', ''),
+    Command('aptitude search foo', ''),
+    Command('apt search foo', ''),
+    Command('apt-get install foo', ''),
+    Command('apt-get source foo', ''),
+    Command('apt-get clean', ''),
+    Command('apt-get remove', ''),
+    Command('apt-get update', ''),
+    Command('sudo apt update', no_match_output)
+])
+def test_not_match(command):
+    assert not match(command)
+    
+    if __name__ == '__main__':
+    try:
+        execute()
+    finally:
+        # Twisted prints errors in DebugInfo.__del__, but PyPy does not run gc.collect()
+        # on exit: http://doc.pypy.org/en/latest/cpython_differences.html?highlight=gc.collect#differences-related-to-garbage-collection-strategies
+        garbage_collect()
 
     
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
     
-def test_match():
-    assert match(Command('apt list --upgradable', match_output))
-    assert match(Command('sudo apt list --upgradable', match_output))
-    
-      * dynamodb
-'''
-    
-    from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
+        def short_desc(self):
+        return 'Get settings values'
     
     
-METRICS = {
-    'f1': partial(f1_score, average='micro'),
-    'f1-by-sample': partial(f1_score, average='samples'),
-    'accuracy': accuracy_score,
-    'hamming': hamming_loss,
-    'jaccard': jaccard_similarity_score,
-}
-    
-    plot(euclidean_distances)
-plot(rbf_kernels)
-plt.show()
-
-    
-        ###########################################################################
-    # Set transformer input
-    ###########################################################################
-    transformers = {}
-    
-        # Print results
-    ###########################################################################
-    print('Script arguments')
-    print('===========================')
-    arguments = vars(opts)
-    print('%s \t | %s ' % ('Arguments'.ljust(16),
-                           'Value'.center(12),))
-    print(25 * '-' + ('|' + '-' * 14) * 1)
-    for key, value in arguments.items():
-        print('%s \t | %s ' % (str(key).ljust(16),
-                               str(value).strip().center(12)))
-    print('')
-    
-    
-def abspath(path):
-    '''Return an absolute path.'''
-    path = os.fspath(path)
-    if not isabs(path):
-        if isinstance(path, bytes):
-            cwd = os.getcwdb()
-        else:
-            cwd = os.getcwd()
-        path = join(cwd, path)
-    return normpath(path)
-    
-            out = self.check_warnings_filters('once::UserWarning',
-                                          'always::UserWarning',
-                                          use_pywarning=True)
-        self.assertEqual(out, expected_filters)
-    
-        def test_subinterps_distinct_state(self):
-        for run in self.run_repeated_init_and_subinterpreters():
-            main, *subs, _ = run
-    
-    # Of course, there are lots of email messages that could break this simple
-# minded program, but it will handle the most common ones.
+class ScrapesContract(Contract):
+    ''' Contract to check presence of fields in scraped items
+        @scrapes page_name page_body
+    '''
