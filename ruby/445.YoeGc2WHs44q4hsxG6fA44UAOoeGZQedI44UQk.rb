c189429ac126f690841b82954bd24127fe74d342
SUNsung <weatherius@gmail.com>
2018-11-15 01:43:32 +0200

    Rewrite [445.YoeGc2WHs44q4hsxG6fA44UAOoeGZQedI44UQk.rb] File in: 2018-11-15 01:43:31

--- ruby/445.YoeGc2WHs44q4hsxG6fA44UAOoeGZQedI44UQk.rb
+++ ruby/445.YoeGc2WHs44q4hsxG6fA44UAOoeGZQedI44UQk.rb
@@ -1,38 +1,110 @@
 
         
-          </body>
-</html>
-HTML
+            def handle_ssl_error!(e)
+      # SSL errors are very common when the Ruby or OpenSSL installation is somehow broken
+      # We want to show a nice error message to the user here
+      # We have over 20 GitHub issues just for this one error:
+      #   https://github.com/fastlane/fastlane/search?q=errno%3D0+state%3DSSLv3+read+server&type=Issues
+      suggest_ruby_reinstall(e)
+      display_user_error!(e, e.to_s)
+    end
     
-          it 'ignores '-' sign' do
-        format('%2$d', 1, 2, 3).should == '2'
-        format('%-2$d', 1, 2, 3).should == '2'
+          def self.example_code
+        [
+          'add_git_tag # simple tag with default values',
+          'add_git_tag(
+            grouping: 'fastlane-builds',
+            prefix: 'v',
+            postfix: '-RC1',
+            build_number: 123
+          )',
+          '# Alternatively, you can specify your own tag. Note that if you do specify a tag, all other arguments are ignored.
+          add_git_tag(
+            tag: 'my_custom_tag'
+          )'
+        ]
       end
     
-        trace_var :$Kernel_trace_var_global, proc {|value| captured = value}
-    
-    require 'commands'
-    
-          new_name = name
-      new_path = path
-      new_remote = default_remote
-    
-    public_dir      = 'public'    # compiled site directory
-source_dir      = 'source'    # source file directory
-blog_index_dir  = 'source'    # directory for your blog's index page (if you put your index in source/blog/index.html, set this to 'source/blog')
-deploy_dir      = '_deploy'   # deploy directory (for Github pages deployment)
-stash_dir       = '_stash'    # directory to stash posts for speedy generation
-posts_dir       = '_posts'    # directory for blog files
-themes_dir      = '.themes'   # directory for blog files
-new_post_ext    = 'markdown'  # default new post file extension when using the new_post task
-new_page_ext    = 'markdown'  # default new page file extension when using the new_page task
-server_port     = '4000'      # port for preview server eg. localhost:4000
-    
-          unless file.file?
-        return 'File #{file} could not be found'
+            expect(result).to eq('git tag -am #{message.shellescape} #{tag.shellescape}')
       end
     
-      # Escapes CDATA sections in post content
-  def cdata_escape(input)
-    input.gsub(/<!\[CDATA\[/, '&lt;![CDATA[').gsub(/\]\]>/, ']]&gt;')
+          context 'when specify output_file options' do
+        it 'adds redirect file to command' do
+          result = Fastlane::FastFile.new.parse('lane :test do
+            swiftlint(
+              output_file: '#{output_file}'
+            )
+          end').runner.execute(:test)
+    
+          EOS
+      login!
+    end
+    
+          return unless user == 'caskroom'
+    
+        # 100 continue counter
+    self.count_100 = 0
   end
+    
+              # Encrypts the Rex::Proto::Kerberos::Model::AuthorizationData
+          #
+          # @param etype [Integer] the crypto schema to encrypt
+          # @param key [String] the key to encrypt
+          # @return [String] the encrypted result
+          # @raise [NotImplementedError] if encryption schema isn't supported
+          def encrypt(etype, key)
+            data = self.encode
+    
+              # Decodes the key_expiration field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Time]
+          def decode_key_expiration(input)
+            input.value[0].value
+          end
+    
+        def lotus?
+      defined?(::Lotus)
+    end
+    
+        @save_to.each { |_, v| FileUtils.mkdir_p(v) }
+    
+        def str_to_byte_pos(pos)
+      @s.string.slice(0, pos).bytesize
+    end
+  end
+end
+    
+        # opening brace position from +from+ (search backwards)
+    def open_brace_pos(css, from, depth = 0)
+      s = CharStringScanner.new(css[0..from].reverse)
+      while (b = s.scan_next(BRACE_RE_REVERSE))
+        depth += (b == '{' ? +1 : -1)
+        break if depth.zero?
+      end
+      raise 'matching { brace not found' unless depth.zero?
+      from - s.pos + 1
+    end
+    }
+    }
+    
+        def log_status(status)
+      puts bold status
+    end
+    
+    # include would include the module in Object
+# extend only extends the `main` object
+extend Sinatra::Delegator
+    
+      # set version
+  content.sub! /(s\.version.*=\s+).*/, '\\1\'#{Rack::Protection::VERSION}\''
+    
+            # See if it's actually a masked token or not. We should be able
+        # to handle any unmasked tokens that we've issued without error.
+    
+            private
+    
+              Spree::Cart::AddItem.call(order: @shipment.order,
+                                    variant: variant,
+                                    quantity: quantity,
+                                    options: { shipment: @shipment })
