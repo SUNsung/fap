1ad9db0facdab56091fd5525086541d7224bd689
SUNsung <weatherius@gmail.com>
2018-11-14 15:35:37 +0200

    Rewrite [30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py] File in: 2018-11-14 15:35:36

--- pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
+++ pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
@@ -1,67 +1,250 @@
 
         
-        
-def plot_lfads(train_bxtxd, train_model_vals,
-               train_ext_input_bxtxi=None, train_truth_bxtxd=None,
-               valid_bxtxd=None, valid_model_vals=None,
-               valid_ext_input_bxtxi=None, valid_truth_bxtxd=None,
-               bidx=None, cf=1.0, output_dist='poisson'):
+            This could be extended by having nested blocks, sorting them recursively
+    and flattening the end structure into a list of lines. Revision 2 maybe ^.^.
+'''
     
-      This generates batch_size pointers into the raw PTB data, and allows
-  minibatch iteration along these pointers.
+        return app
+
     
-    
-def convert_to_human_readable(id_to_word, arr, p, max_num_to_print):
-  '''Convert a np.array of indices into words using id_to_word dictionary.
-  Return max_num_to_print results.
-  '''
-    
-      # If there are no real or fake entries in the batch, we assign an average
-  # value of zero.
-  real_avg = tf.where(tf.equal(real_count, 0), zero_tensor, real_avg)
-  fake_avg = tf.where(tf.equal(fake_count, 0), zero_tensor, fake_avg)
-    
-        a_transaction = Transaction(True, num_obj)
-    try:
-        for i in range(3):
-            num_obj.increment()
-            print(num_obj)
-        a_transaction.commit()
-        print('-- committed')
-    
-        def attach(self, observer):
-        if observer not in self._observers:
-            self._observers.append(observer)
+            if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
     
     
-class Visitor(object):
-    def visit(self, node, *args, **kwargs):
-        meth = None
-        for cls in node.__class__.__mro__:
-            meth_name = 'visit_' + cls.__name__
-            meth = getattr(self, meth_name, None)
-            if meth:
-                break
-    
-    *What does this example do?
-This particular implementation abstracts the creation of a pet and
-does so depending on the factory we chose (Dog or Cat, or random_animal)
-This works because both Dog/Cat and random_animal respect a common
-interface (callable for creation and .speak()).
-Now my application can create pets abstractly and decide later,
-based on my own criteria, dogs over cats.
-    
+def init_app(app):
+    '''Register database functions with the Flask app. This is called by
+    the application factory.
     '''
-*What is this pattern about?
-This pattern is used when creating an object is costly (and they are
-created frequently) but only a few are used at a time. With a Pool we
-can manage those instances we have as of now by caching them. Now it
-is possible to skip the costly creation of an object if one is
-available in the pool.
-A pool allows to 'check out' an inactive object and then to return it.
-If none are available the pool creates one to provide without wait.
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
     
-    class TimeDisplay(object):
+        # login request set the user_id in the session
+    # check that the user is loaded from the session
+    with client:
+        client.get('/')
+        assert session['user_id'] == 1
+        assert g.user['username'] == 'test'
+    
+        assert 'closed' in str(e)
+    
+    
+# Certain versions of pypy have a bug where clearing the exception stack
+# breaks the __exit__ function in a very peculiar way.  The second level of
+# exception blocks is necessary because pypy seems to forget to check if an
+# exception happened until the next bytecode instruction?
+#
+# Relevant PyPy bugfix commit:
+# https://bitbucket.org/pypy/pypy/commits/77ecf91c635a287e88e60d8ddb0f4e9df4003301
+# According to ronan on #pypy IRC, it is released in PyPy2 2.3 and later
+# versions.
+#
+# Ubuntu 14.04 has PyPy 2.2.1, which does exhibit this bug.
+BROKEN_PYPY_CTXMGR_EXIT = False
+if hasattr(sys, 'pypy_version_info'):
+    class _Mgr(object):
+        def __enter__(self):
+            return self
+        def __exit__(self, *args):
+            if hasattr(sys, 'exc_clear'):
+                # Python 3 (PyPy3) doesn't have exc_clear
+                sys.exc_clear()
+    try:
+        try:
+            with _Mgr():
+                raise AssertionError()
+        except:
+            raise
+    except TypeError:
+        BROKEN_PYPY_CTXMGR_EXIT = True
+    except AssertionError:
+        pass
+
+    
+    
+# context locals
+_request_ctx_stack = LocalStack()
+_app_ctx_stack = LocalStack()
+current_app = LocalProxy(_find_app)
+request = LocalProxy(partial(_lookup_req_object, 'request'))
+session = LocalProxy(partial(_lookup_req_object, 'session'))
+g = LocalProxy(partial(_lookup_app_object, 'g'))
+
+    
+    import logging
+import sys
+    
+    
+# Core signals.  For usage examples grep the source code or consult
+# the API documentation in docs/api.rst as well as docs/signals.rst
+template_rendered = _signals.signal('template-rendered')
+before_render_template = _signals.signal('before-render-template')
+request_started = _signals.signal('request-started')
+request_finished = _signals.signal('request-finished')
+request_tearing_down = _signals.signal('request-tearing-down')
+got_request_exception = _signals.signal('got-request-exception')
+appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
+appcontext_pushed = _signals.signal('appcontext-pushed')
+appcontext_popped = _signals.signal('appcontext-popped')
+message_flashed = _signals.signal('message-flashed')
+
+    
+        def __init__(self, *args, **kwargs):
+        super(FlaskClient, self).__init__(*args, **kwargs)
+        self.environ_base = {
+            'REMOTE_ADDR': '127.0.0.1',
+            'HTTP_USER_AGENT': 'werkzeug/' + werkzeug.__version__
+        }
+    
+        When you want to decorate a pluggable view you will have to either do that
+    when the view function is created (by wrapping the return value of
+    :meth:`as_view`) or you can use the :attr:`decorators` attribute::
+    
+    # build the model
+model = Sequential()
+model.add(layers.Dense(256, input_shape=(784,)))
+model.add(Antirectifier())
+model.add(layers.Dropout(0.1))
+model.add(layers.Dense(256))
+model.add(Antirectifier())
+model.add(layers.Dropout(0.1))
+model.add(layers.Dense(num_classes))
+model.add(layers.Activation('softmax'))
+    
+        return (x_train, y_train), (x_test, y_test)
+
+    
+    
+class L1L2(Regularizer):
+    '''Regularizer for L1 and L2 regularization.
+    
+    
+def test_cce_one_hot():
+    y_a = K.variable(np.random.randint(0, 7, (5, 6)))
+    y_b = K.variable(np.random.random((5, 6, 7)))
+    objective_output = losses.sparse_categorical_crossentropy(y_a, y_b)
+    assert K.eval(objective_output).shape == (5, 6)
+    
+        E.g. for use with categorical_crossentropy.
+    
+            self.kernel_initializer = initializers.get(kernel_initializer)
+        self.recurrent_initializer = initializers.get(recurrent_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.unit_forget_bias = unit_forget_bias
+    
+    if K.backend() != 'tensorflow':
+    raise RuntimeError('This example can only run with the '
+                       'TensorFlow backend, '
+                       'because it requires TF-native augmentation APIs')
+    
+    for j in range(16):
+    new_pos = seq.predict(track[np.newaxis, ::, ::, ::, ::])
+    new = new_pos[::, -1, ::, ::, ::]
+    track = np.concatenate((track, new), axis=0)
+    
+    
+FILEPATH = os.path.join(
+    os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'blns.txt')
+'''Path to the file'''
+    
+        def test_default_decoder_raises_deserialization_error(self):
+        from acme.fields import RFC3339Field
+        self.assertRaises(
+            jose.DeserializationError, RFC3339Field.default_decoder, '')
+    
+    # The encoding of source files.
+#source_encoding = 'utf-8-sig'
+    
+    from certbot import util
+    
+            :param str port: Desired port
+    
+    from certbot import errors
+    
+        def test_perform2(self):
+        # Avoid load module
+        self.sni.configurator.parser.modules.add('ssl_module')
+        self.sni.configurator.parser.modules.add('socache_shmcb_module')
+        acme_responses = []
+        for achall in self.achalls:
+            self.sni.add_chall(achall)
+            acme_responses.append(achall.response(self.auth_key))
+    
+    # Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
+    
+        @classmethod
+    def setUpClass(self):
+        self.radio = Radio()
+    
+    
+class Delegate(object):
+    def __init__(self):
+        self.p1 = 123
+    
+        _instance_method_choices = {'param_value_1': _instance_method_1, 'param_value_2': _instance_method_2}
+    
+    
+if __name__ == '__main__':
+    
+        # verify that none of the target files exist
+    assert not lexists('foo.txt')
+    assert not lexists('bar.txt')
+    assert not lexists('baz.txt')
+    try:
+        with open('foo.txt', 'w'):  # Creating the file
+            pass
+    
+        def subscribe(self, msg, subscriber):
+        self.subscribers.setdefault(msg, []).append(subscriber)
+    
+            if not meth:
+            meth = self.generic_visit
+        return meth(node, *args, **kwargs)
+    
+    ### OUTPUT ###
+# We have a lovely Cat
+# It says meow
+#
+# We have a lovely Dog
+# It says woof
+# ====================
+# We have a lovely Cat
+# It says meow
+# ====================
+# We have a lovely Cat
+# It says meow
+# ====================
+
+    
+    
+# In some very complex cases, it might be desirable to pull out the building
+# logic into another function (or a method on another class), rather than being
+# in the base class '__init__'. (This leaves you in the strange situation where
+# a concrete class does not have a useful constructor)
+    
+    
+def main():
+    Jhon = Person('Jhon', 'Coder')
+    print(u'Name: {0}    Occupation: {1}'.format(Jhon.name, Jhon.occupation))
+    print(u'Before we access `relatives`:')
+    print(Jhon.__dict__)
+    print(u'Jhon's relatives: {0}'.format(Jhon.relatives))
+    print(u'After we've accessed `relatives`:')
+    print(Jhon.__dict__)
+    print(Jhon.parents)
+    print(Jhon.__dict__)
+    print(Jhon.parents)
+    print(Jhon.call_count2)
+    
+        def __enter__(self):
+        if self.item is None:
+            self.item = self._queue.get()
+        return self.item
     
         def get_current_time_as_html_fragment(self):
         current_time = self.time_provider.now()
@@ -69,11 +252,11 @@ If none are available the pool creates one to provide without wait.
         return current_time_as_html_fragment
 '''
     
-        def now(self):
-        current_time_is_always_midnight = '24:01'
-        return current_time_is_always_midnight
-
-    
-    
-class Failed(OutOfService):
-    '''No need to override any method.'''
+            path.append(start)
+        if start == end:
+            return path
+        for node in self.graph.get(start, []):
+            if node not in path:
+                newpath = self.find_path(node, end, path[:])
+                if newpath:
+                    return newpath
