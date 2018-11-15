82e37ece3aa7b9ea8b0d508f4b201db9a61c6014
SUNsung <weatherius@gmail.com>
2018-11-15 19:45:55 +0200

    Rewrite [52zcj3MHIQDUo52QHG9ZEK9E0I5271qAIETH9ms52d2a.py] File in: 2018-11-15 19:45:55

--- pyton/52zcj3MHIQDUo52QHG9ZEK9E0I5271qAIETH9ms52d2a.py
+++ pyton/52zcj3MHIQDUo52QHG9ZEK9E0I5271qAIETH9ms52d2a.py
@@ -1,99 +1,238 @@
 
         
-        from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
+          plt.subplot(nrows,2,7+subplot_cidx)
+  plot_time_series(model_vals['factors'], bidx, n_to_plot=n_to_plot, color='b',
+                   scale=2.0, title=col_title + ' Factors')
+    
+        tf.global_variables_initializer().run()
+    
+      # First generate all firing rates. in the next loop, generate all
+  # replications this allows the random state for rate generation to be
+  # independent of n_replications.
+  dataset_name = 'dataset_N' + str(N) + '_S' + str(S)
+  if S < N:
+    dataset_name += '_n' + str(n+1)
     
     
-def generate_pathological_dataset(size):
-    # Triggers O(n^2) complexity on the original implementation.
-    return np.r_[np.arange(size),
-                 np.arange(-(size - 1), size),
-                 np.arange(-(size - 1), 1)]
+class SingleRecurrentLanguageModel(object):
+  '''Single Recurrent Language Model.'''
     
-            for k in sorted(results_dict.keys()):
-            all_times[k].append(results_dict[k]['time'])
-            all_errors[k].append(results_dict[k]['error'])
+      Yields:
+    Pairs of the batched data, each a matrix of shape [batch_size, num_steps].
+    The second element of the tuple is the same data time-shifted to the
+    right by one.
+    
+      Args:
+    hparams:  MaskGAN hyperparameters.
+    learning_rate:  tf.Variable scalar learning rate.
+    final_gen_objective:  Scalar final REINFORCE objective for the sequence.
+    averages_op:  ExponentialMovingAverage apply average op to
+      maintain the baseline.
+    global_step:  global_step tf.Variable.
+    
+    
+def construct_ngrams_dict(ngrams_list):
+  '''Construct a ngram dictionary which maps an ngram tuple to the number
+  of times it appears in the text.'''
+  counts = {}
+    
+        '''
+    is_windows = is_windows
+    config_dir = DEFAULT_CONFIG_DIR
+    stdin = sys.stdin
+    stdin_isatty = stdin.isatty()
+    stdin_encoding = None
+    stdout = sys.stdout
+    stdout_isatty = stdout.isatty()
+    stdout_encoding = None
+    stderr = sys.stderr
+    stderr_isatty = stderr.isatty()
+    colors = 256
+    if not is_windows:
+        if curses:
+            try:
+                curses.setupterm()
+                colors = curses.tigetnum('colors')
+            except curses.error:
+                pass
+    else:
+        # noinspection PyUnresolvedReferences
+        import colorama.initialise
+        stdout = colorama.initialise.wrap_stream(
+            stdout, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        stderr = colorama.initialise.wrap_stream(
+            stderr, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        del colorama
+    
+    
+install_requires = [
+    'requests>=2.18.4',
+    'Pygments>=2.1.3'
+]
+    
+        def test_print_overridable_when_stdout_redirected(self, httpbin):
+        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
+        r = http('--print=h', 'GET', httpbin.url + '/get', env=env)
+        assert HTTP_OK in r
+
+    
+    import pytest
+import mock
+from requests.structures import CaseInsensitiveDict
+    
+    
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
+    assert HTTP_OK in r
+    
+            self.tail.appendleft(delta)
+        self.lasttime = now
+        self.concurrent += 1
+    
+        def short_desc(self):
+        return 'Run quick benchmark test'
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
+    
+        def _print_response(self, response, opts):
+        if opts.headers:
+            self._print_headers(response.request.headers, b'>')
+            print('>')
+            self._print_headers(response.headers, b'<')
+        else:
+            self._print_bytes(response.body)
+    
+        if len(sys.argv) > 1:
+        ip = sys.argv[1]
+    else:
+        ip = '46.134.208.94'
+        ip = '2001:ee0:3203:a::12'
+        print('Usage: check_ip.py [ip] [top_domain] [wait_time=0]')
+    print('test ip:%s' % ip)
+    
+    
+    def reduceToSingleOperationPerIndex(self, rewrites):
+        '''
+        We need to combine operations and report invalid operations (like
+        overlapping replaces that are not completed nested).  Inserts to
+        same index need to be combined etc...   Here are the cases:
+    
+                for name in rest.split(sep):
+                if not name or name == '.':
+                    # current dir
+                    continue
+                if name == '..':
+                    # parent dir
+                    path, _, _ = path.rpartition(sep)
+                    continue
+                newpath = path + sep + name
+                if newpath in seen:
+                    # Already seen this path
+                    path = seen[newpath]
+                    if path is not None:
+                        # use cached value
+                        continue
+                    # The symlink is not resolved, so we must have a symlink loop.
+                    raise RuntimeError('Symlink loop from %r' % newpath)
+                # Resolve the symbolic link
+                try:
+                    target = accessor.readlink(newpath)
+                except OSError as e:
+                    if e.errno != EINVAL and strict:
+                        raise
+                    # Not a symlink, or non-strict mode. We just leave the path
+                    # untouched.
+                    path = newpath
+                else:
+                    seen[newpath] = None # not resolved symlink
+                    path = _resolve(path, target)
+                    seen[newpath] = path # resolved symlink
+    
+        def test_as_uri(self):
+        P = self.cls
+        with self.assertRaises(ValueError):
+            P('/a/b').as_uri()
+        with self.assertRaises(ValueError):
+            P('c:a/b').as_uri()
+        self.assertEqual(P('c:/').as_uri(), 'file:///c:/')
+        self.assertEqual(P('c:/a/b.c').as_uri(), 'file:///c:/a/b.c')
+        self.assertEqual(P('c:/a/b%#c').as_uri(), 'file:///c:/a/b%25%23c')
+        self.assertEqual(P('c:/a/b\xe9').as_uri(), 'file:///c:/a/b%C3%A9')
+        self.assertEqual(P('//some/share/').as_uri(), 'file://some/share/')
+        self.assertEqual(P('//some/share/a/b.c').as_uri(),
+                         'file://some/share/a/b.c')
+        self.assertEqual(P('//some/share/a/b%#c\xe9').as_uri(),
+                         'file://some/share/a/b%25%23c%C3%A9')
+    
+        def testDecompressUnusedData(self):
+        bz2d = BZ2Decompressor()
+        unused_data = b'this is unused data'
+        text = bz2d.decompress(self.DATA+unused_data)
+        self.assertEqual(text, self.TEXT)
+        self.assertEqual(bz2d.unused_data, unused_data)
+    
+        # Initialize and populate our database.
+    conn = sqlite3.connect(':memory:')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE memos(key INTEGER PRIMARY KEY, task TEXT)')
+    tasks = (
+        'give food to fish',
+        'prepare group meeting',
+        'fight with a zebra',
+        )
+    for task in tasks:
+        cursor.execute('INSERT INTO memos VALUES(NULL, ?)', (task,))
+    
+    # Now add the related image to the html part.
+with open('roasted-asparagus.jpg', 'rb') as img:
+    msg.get_payload()[1].add_related(img.read(), 'image', 'jpeg',
+                                     cid=asparagus_cid)
+    
+    if __name__ == '__main__':
+    main()
+
     
     
 if __name__ == '__main__':
-    from mpl_toolkits.mplot3d import axes3d  # register the 3d projection
-    import matplotlib.pyplot as plt
+    
+    def generateKey(keySize):
+    print('Generating prime p...')
+    p = rabinMiller.generateLargePrime(keySize)
+    print('Generating prime q...')
+    q = rabinMiller.generateLargePrime(keySize)
+    n = p * q
+    
+    '''
+The number of partitions of a number n into at least k parts equals the number of partitions into exactly k parts
+plus the number of partitions into at least k-1 parts. Subtracting 1 from each part of a partition of n into k parts
+gives a partition of n-k into k parts. These two facts together are used for this algorithm.
+'''
+def partition(m):
+	memo = [[0 for _ in xrange(m)] for _ in xrange(m+1)]
+	for i in xrange(m+1):
+		memo[i][0] = 1
     
     
-def user_role(name, rawtext, text, lineno,
-              inliner, options=None, content=None):
-    '''Sphinx role for linking to a user profile. Defaults to linking to
-    GitHub profiles, but the profile URIS can be configured via the
-    ``issues_user_uri`` config value.
-    
-                short_text_filename = os.path.join(short_text_lang_folder,
-                                               '%s_%04d.txt' % (lang, j))
-            print('Writing %s' % short_text_filename)
-            open(short_text_filename, 'wb').write(
-                small_content.encode('utf-8', 'ignore'))
-            j += 1
-            if j >= 1000:
-                break
-    
-        if sys.platform == 'win32':
-        win32_lib = os.path.abspath( os.path.join(python_path, 'lib', 'win32'))
-        sys.path.append(win32_lib)
-    elif sys.platform.startswith('linux'):
-        linux_lib = os.path.abspath( os.path.join(python_path, 'lib', 'linux'))
-        sys.path.append(linux_lib)
-    elif sys.platform == 'darwin':
-        darwin_lib = os.path.abspath( os.path.join(python_path, 'lib', 'darwin'))
-        sys.path.append(darwin_lib)
-        extra_lib = '/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python'
-        sys.path.append(extra_lib)
-    
-        server = ''
-    if probe_server:
-        server = ' and the best server is %s.' % best_server(probe_nat=probe_nat)
-    else:
-        new_pteredor()
-    
-    
-
-    
-              1. for any inserts to same index, combine even if not adjacent.
-          2. for any prior replace with same left boundary, combine this
-             insert with replace and delete this replace.
-          3. throw exception if index in same range as previous replace
-    
-    # If true, keep warnings as 'system message' paragraphs in the built documents.
-# keep_warnings = False
-    
-    DEVICE_SCHEMA = vol.Schema({
-    vol.Required(CONF_INCLUDE):
-        vol.All(cv.ensure_list, [vol.In(AXIS_INCLUDE)]),
-    vol.Optional(CONF_NAME): cv.string,
-    vol.Optional(CONF_HOST, default=AXIS_DEFAULT_HOST): cv.string,
-    vol.Optional(CONF_USERNAME, default=AXIS_DEFAULT_USERNAME): cv.string,
-    vol.Optional(CONF_PASSWORD, default=AXIS_DEFAULT_PASSWORD): cv.string,
-    vol.Optional(CONF_TRIGGER_TIME, default=0): cv.positive_int,
-    vol.Optional(CONF_PORT, default=80): cv.positive_int,
-    vol.Optional(ATTR_LOCATION, default=''): cv.string,
-})
-    
-            try:
-            result = _req_json_rpc(
-                url, 'net.arptable', params={'auth': self.token})
-        except InvalidLuciTokenError:
-            _LOGGER.info('Refreshing token')
-            self.refresh_token()
-            return False
-    
-        def scan_devices(self):
-        '''Scan for new devices and return a list with found device IDs.'''
-        self._update_info()
-        return [client['mac'] for client in self.last_results]
-    
-            # Flag C stands for CONNECTED
-        active_clients = [client for client in data.values() if
-                          client['status'].find('C') != -1]
-        self.last_results = active_clients
-        return True
+def b_expo(a, b):
+    res = 1
+    while b > 0:
+        if b&1:
+            res *= a
