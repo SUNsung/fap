9fcda3f51009c37e4a47546508188089a17b3035
SUNsung <weatherius@gmail.com>
2018-11-15 08:34:23 +0200

    Rewrite [03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp] File in: 2018-11-15 08:34:22

--- cccccc/03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp
+++ cccccc/03MQ2A1dHTXo03cjgLyGIypM03CNUNgxTUirs03OJp.cpp
@@ -1,3 +1,248 @@
 
         
-        
+        #ifndef BITCOIN_QT_QVALUECOMBOBOX_H
+#define BITCOIN_QT_QVALUECOMBOBOX_H
+    
+        void showTab_SM(bool fShow);
+    void showTab_VM(bool fShow);
+    
+    /** Generate blocks (mine) */
+UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbaseScript, int nGenerate, uint64_t nMaxTries, bool keepScript);
+    
+    #ifndef SECP256K1_MODULE_ECDH_MAIN_H
+#define SECP256K1_MODULE_ECDH_MAIN_H
+    
+      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    #ifdef USE_CUDNN
+template <typename Dtype>
+class CuDNNLRNLayer : public LRNLayer<Dtype> {
+ public:
+  explicit CuDNNLRNLayer(const LayerParameter& param)
+      : LRNLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNLRNLayer();
+    }
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    class ConstraintBullet : public RIDBullet {
+    }
+    
+    bool GodotCollisionDispatcher::needsResponse(const btCollisionObject *body0, const btCollisionObject *body1) {
+	if (body0->getUserIndex() == CASTED_TYPE_AREA || body1->getUserIndex() == CASTED_TYPE_AREA) {
+		// Avoide area narrow phase
+		return false;
+	}
+	return btCollisionDispatcher::needsResponse(body0, body1);
+}
+
+    
+    class GodotRayWorldAlgorithm : public btActivatingCollisionAlgorithm {
+    }
+    
+    #include 'register_types.h'
+    
+    	} else if (p_string.get_slice_count('.') == 4) {
+		// IPv4 (mapped to IPv6 internally)
+		field16[5] = 0xffff;
+		_parse_ipv4(p_string, 0, &field8[12]);
+		valid = true;
+    
+        private:
+        std::unordered_set<StreamInformation> m_streamInfos;
+        bool m_epochEndReached;
+        size_t m_numWorkers;
+        size_t m_workerRank;
+        size_t m_prevMinibatchSize;
+        size_t m_maxNumSamplesToRead;
+        size_t m_maxNumSweepsToRead;
+        size_t m_truncationLength;
+        size_t m_maxErrors;
+        std::unordered_map<StreamInformation, MinibatchData> m_minibatchData;
+    
+    
+    {        auto matrixDims = GetMatrixDimensions(viewShape);
+        std::shared_ptr<Matrix<V1ElemType>> matrix = std::make_shared<Matrix<V1ElemType>>(matrixDims.first, matrixDims.second, (V1ElemType*)dataBuffer, AsCNTKImplDeviceId(device), matrixFlagDontOwnBuffer);
+        return new TensorView<V1ElemType>(matrix, AsTensorViewShape(viewShape));
+    }
+    
+        void ProgressWriter::WriteTestSummary(const ValuePtr& accumulatedMetric)
+    {
+        m_test->WriteSummary(
+            nullptr, accumulatedMetric,
+            [this](size_t samples, size_t updates, size_t summaries, double /*aggregateLoss*/, double aggregateMetric,
+                uint64_t elapsedMs)
+            {
+                OnWriteTestSummary(samples, updates, summaries, aggregateMetric, elapsedMs);
+            });
+    }
+    
+            void CopyFrom(const Value& /*source*/) override
+        {
+            LogicError('Value::CopyFrom is currently unsupported for PackedValue objects');
+        }
+    
+    public:
+    ScopeTimer(size_t verbosity, const std::string& message)
+        : m_verbosity(verbosity), m_message(message)
+    {
+        if (m_verbosity > 2)
+        {
+            m_aggregateTimer.Start();
+        }
+    }
+    
+        SetTraceLevel(config[L'traceLevel']);
+    DEVICEID_TYPE deviceId = (DEVICEID_TYPE)(int)config[L'deviceId'];
+    
+    /*!
+ * \brief Check if alignas(*) keyword is supported. (g++ 4.8 or higher)
+ */
+#if defined(__GNUC__) && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 8) || __GNUC__ > 4)
+#define XGBOOST_ALIGNAS(X) alignas(X)
+#else
+#define XGBOOST_ALIGNAS(X)
+#endif
+    
+    /*!
+ * \brief Registry entry for predictor.
+ */
+struct PredictorReg
+    : public dmlc::FunctionRegEntryBase<PredictorReg,
+                                        std::function<Predictor*()>> {};
+    
+    namespace xgboost {
+namespace common {
+/*! \brief buffer reader of the stream that allows you to get */
+class StreamBufferReader {
+ public:
+  explicit StreamBufferReader(size_t buffer_size)
+      :stream_(NULL),
+       read_len_(1), read_ptr_(1) {
+    buffer_.resize(buffer_size);
+  }
+  /*!
+   * \brief set input stream
+   */
+  inline void set_stream(dmlc::Stream *stream) {
+    stream_ = stream;
+    read_len_ = read_ptr_ = 1;
+  }
+  /*!
+   * \brief allows quick read using get char
+   */
+  inline char GetChar(void) {
+    while (true) {
+      if (read_ptr_ < read_len_) {
+        return buffer_[read_ptr_++];
+      } else {
+        read_len_ = stream_->Read(&buffer_[0], buffer_.length());
+        if (read_len_ == 0) return EOF;
+        read_ptr_ = 0;
+      }
+    }
+  }
+  /*! \brief whether we are reaching the end of file */
+  inline bool AtEnd(void) const {
+    return read_len_ == 0;
+  }
+    }
+    }
+    }
+    
+    /*!
+ * \brief an iterator that iterates over a configure file and gets the configures
+ */
+class ConfigIterator: public ConfigStreamReader {
+ public:
+  /*!
+   * \brief constructor
+   * \param fname name of configure file
+   */
+  explicit ConfigIterator(const char *fname) : ConfigStreamReader(fi_) {
+    fi_.open(fname);
+    if (fi_.fail()) {
+      LOG(FATAL) << 'cannot open file ' << fname;
+    }
+    ConfigReaderBase::Init();
+  }
+  /*! \brief destructor */
+  ~ConfigIterator() {
+    fi_.close();
+  }
+    }
+    
+    
+    {  virtual void AddSplit(bst_uint nodeid,
+                        bst_uint leftid,
+                        bst_uint rightid,
+                        bst_uint featureid,
+                        bst_float leftweight,
+                        bst_float rightweight);
+};
+    
+    
+    {} // namespace folly
+
+    
+    BENCHMARK(BENCHFUN(defaultCtor), iters) {
+  FOR_EACH_RANGE (i, 0, iters) {
+    VECTOR v;
+    doNotOptimizeAway(&v);
+  }
+}
+    
+    
+    {} // namespace folly
+    
+        explicit operator bool() const {
+      return executorAndDummyFlag_;
+    }
+    
+     private:
+  void performLazyInit() {
+    if (!initialized_) {
+      initialized_ = true;
+      increment_ = initialize_();
+      initialize_ = {};
+    }
+  }
+    
+    
+    {  /**
+   * The strategy parameter is used to tune the compression algorithm.
+   * Supported values:
+   * - Z_DEFAULT_STRATEGY: normal data
+   * - Z_FILTERED: data produced by a filter (or predictor)
+   * - Z_HUFFMAN_ONLY: force Huffman encoding only (no string match)
+   * - Z_RLE: limit match distances to one
+   * - Z_FIXED: prevents the use of dynamic Huffman codes
+   *
+   * The strategy parameter only affects the compression ratio but not the
+   * correctness of the compressed output.
+   */
+  int strategy;
+};
+    
+      std::weak_ptr<T> get() const {
+    return slots_[AccessSpreader<>::current(kNumSlots)];
+  }
