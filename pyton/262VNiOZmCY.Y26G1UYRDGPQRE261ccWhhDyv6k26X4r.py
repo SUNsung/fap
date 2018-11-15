6ba1b5a55fb7f999bfdc2977370760cff1e0979c
SUNsung <weatherius@gmail.com>
2018-11-15 04:02:18 +0200

    Rewrite [262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py] File in: 2018-11-15 04:02:17

--- pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
+++ pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
@@ -1,231 +1,121 @@
 
         
-            # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
-    ERROR_CTRL_C = 130
+        FORMATS = {
+    'sequences': lambda y: [list(np.flatnonzero(s)) for s in y],
+    'dense': lambda y: y,
+    'csr': lambda y: sp.csr_matrix(y),
+    'csc': lambda y: sp.csc_matrix(y),
+}
     
-        '''
-)
-positional.add_argument(
-    'items',
-    metavar='REQUEST_ITEM',
-    nargs=ZERO_OR_MORE,
-    default=None,
-    type=KeyValueArgType(*SEP_GROUP_ALL_ITEMS),
-    help=r'''
-    Optional key-value pairs to be included in the request. The separator used
-    determines the type:
+    from sklearn.cluster.k_means_ import KMeans, MiniBatchKMeans
     
-        def __init__(self, **kwargs):
-        '''
-        Use keyword arguments to overwrite
-        any of the class attributes for this instance.
-    
-            status_line = 'HTTP/{version} {status} {reason}'.format(
-            version=version,
-            status=original.status,
-            reason=original.reason
-        )
-        headers = [status_line]
-        try:
-            # `original.msg` is a `http.client.HTTPMessage` on Python 3
-            # `_headers` is a 2-tuple
-            headers.extend(
-                '%s: %s' % header for header in original.msg._headers)
-        except AttributeError:
-            # and a `httplib.HTTPMessage` on Python 2.x
-            # `headers` is a list of `name: val<CRLF>`.
-            headers.extend(h.strip() for h in original.msg.headers)
-    
-            '''
-        assert with_headers or with_body
-        self.msg = msg
-        self.with_headers = with_headers
-        self.with_body = with_body
-        self.on_body_chunk_downloaded = on_body_chunk_downloaded
-    
-    
-class AuthPlugin(BasePlugin):
-    '''
-    Base auth plugin class.
-    
-        def load_installed_plugins(self):
-        for entry_point_name in ENTRY_POINT_NAMES:
-            for entry_point in iter_entry_points(entry_point_name):
-                plugin = entry_point.load()
-                plugin.package_name = entry_point.dist.key
-                self.register(entry_point.load())
-    
-    
-def humanize_bytes(n, precision=2):
-    # Author: Doug Latornell
-    # Licence: MIT
-    # URL: http://code.activestate.com/recipes/577081/
-    '''Return a humanized string representation of a number of bytes.
-    
-    
-def test_POST_stdin(httpbin_both):
-    with open(FILE_PATH) as f:
-        env = MockEnvironment(stdin=f, stdin_isatty=False)
-        r = http('--form', 'POST', httpbin_both + '/post', env=env)
-    assert HTTP_OK in r
-    assert FILE_CONTENT in r
-    
-            # Insert the import statement to setup.py if not present
-        with open(setup_path, 'a+') as setup:
-            setup.seek(0)
-            setup_content = setup.read()
-            if not 'import fastentrypoints' in setup_content:
-                setup.seek(0)
-                setup.truncate()
-                setup.write('import fastentrypoints\n' + setup_content)
-    
-    
-def test_when_cant_configure_automatically(shell_pid, shell, logs):
-    shell_pid.return_value = 12
-    shell.how_to_configure.return_value = ShellConfiguration(
-        content='eval $(thefuck --alias)',
-        path='/tmp/.bashrc',
-        reload='bash',
-        can_configure_automatically=False)
-    main()
-    logs.how_to_configure_alias.assert_called_once()
-    
-    
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
-    proc.sendline(u'fish')
-    return proc
-    
-    
-init_zshrc = u'''echo '
-export SHELL=/usr/bin/zsh
-export HISTFILE=~/.zsh_history
-echo > $HISTFILE
-export SAVEHIST=100
-export HISTSIZE=100
-eval $(thefuck --alias {})
-setopt INC_APPEND_HISTORY
-echo 'instant mode ready: $THEFUCK_INSTANT_MODE'
-' > ~/.zshrc'''
-    
-        new_command = get_new_command(Command('sudo apt list --upgradable', match_output))
-    assert new_command == 'sudo apt upgrade'
+            ax.set_xlabel('n_samples')
+        ax.set_ylabel('n_features')
+        ax.set_zlabel('Time (s)')
+        ax.set_zlim3d(0.0, max_time * 1.1)
+        ax.set_title(label)
+        # ax.legend()
+        i += 1
+    plt.show()
 
     
     
-@pytest.mark.parametrize('command', [
-    Command('cargo buid', no_such_subcommand_old),
-    Command('cargo buils', no_such_subcommand)])
-def test_match(command):
-    assert match(command)
+def euclidean_distances(X, n_jobs):
+    return pairwise_distances(X, metric='euclidean', n_jobs=n_jobs)
     
-        strings = []
-    with open(filepath, 'r') as f:
-    
-    from .le import letvcloud_download_by_vu
-from .qq import qq_download_by_vid
-from .sina import sina_download_by_vid
-from .tudou import tudou_download_by_iid
-from .youku import youku_download_by_vid
-    
-    __all__ = ['cbs_download']
-    
-    #----------------------------------------------------------------------
-def ckplayer_download_by_xml(ckinfo, output_dir = '.', merge = False, info_only = False, **kwargs):
-    #Info XML
-    video_info = ckplayer_get_info_by_xml(ckinfo)
-    
-    try:
-        title = kwargs['title']
-    except:
-        title = ''
-    type_ = ''
-    size = 0
-    
-    if len(video_info['links']) > 0:  #has link
-        type_, _ext, size = url_info(video_info['links'][0])  #use 1st to determine type, ext
-    
-    if 'size' in video_info:
-        size = int(video_info['size'])
-    else:
-        for i in video_info['links'][1:]:  #save 1st one
-            size += url_info(i)[2]
-    
-    print_info(site_info, title, type_, size)
-    if not info_only:
-        download_urls(video_info['links'], title, _ext, size, output_dir=output_dir, merge=merge)
+        ###########################################################################
+    # Set custom reservoir based method
+    sampling_algorithm['custom-pool'] = \
+        lambda n_population, n_samples, random_state=None: \
+            sample_without_replacement(n_population,
+                                       n_samples,
+                                       method='pool',
+                                       random_state=random_state)
     
     
-def get_title_and_urls(json_data):
-    title = legitimize(re.sub('[\s*]', '_', json_data['title']))
-    video_info = json_data['file_versions']['html5']['video']
-    if 'high' not in video_info:
-        if 'med' not in video_info:
-            video_url = video_info['low']['url']
+class settingslist_node(nodes.General, nodes.Element):
+    pass
+    
+    def _iter_command_classes(module_name):
+    # TODO: add `name` attribute to commands and and merge this function with
+    # scrapy.utils.spider.iter_spider_classes
+    for module in walk_modules(module_name):
+        for obj in vars(module).values():
+            if inspect.isclass(obj) and \
+                    issubclass(obj, ScrapyCommand) and \
+                    obj.__module__ == module.__name__ and \
+                    not obj == ScrapyCommand:
+                yield obj
+    
+        def start_requests(self):
+        qargs = {'total': self.total, 'show': self.show}
+        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
+        return [scrapy.Request(url, dont_filter=True)]
+    
+        def run(self, args, opts):
+        if len(args) != 1 or not is_url(args[0]):
+            raise UsageError()
+        cb = lambda x: self._print_response(x, opts)
+        request = Request(args[0], callback=cb, dont_filter=True)
+        # by default, let the framework handle redirects,
+        # i.e. command handles all codes expect 3xx
+        if not opts.no_redirect:
+            request.meta['handle_httpstatus_list'] = SequenceExclude(range(300, 400))
         else:
-            video_url = video_info['med']['url']
-    else:
-        video_url = video_info['high']['url']
-    audio_info = json_data['file_versions']['html5']['audio']
-    if 'high' not in audio_info:
-        if 'med' not in audio_info:
-            audio_url = audio_info['low']['url']
-        else:
-            audio_url = audio_info['med']['url']
-    else:
-        audio_url = audio_info['high']['url']
-    return title, video_url, audio_url
+            request.meta['handle_httpstatus_all'] = True
+    
+        def update_vars(self, vars):
+        '''You can use this function to update the Scrapy objects that will be
+        available in the shell
+        '''
+        pass
     
     
-def fantasy_download_by_id_channelId(id = 0, channelId = 0, output_dir = '.', merge = True, info_only = False,
-                                     **kwargs):
-    api_url = 'http://www.fantasy.tv/tv/playDetails.action?' \
-              'myChannelId=1&id={id}&channelId={channelId}&t={t}'.format(id = id,
-                                                                         channelId = channelId,
-                                                                         t = str(random.random())
-                                                                         )
-    html = get_content(api_url)
-    html = json.loads(html)
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
+
     
-    from ..common import *
-from hashlib import md5
-from urllib.parse import urlparse
-import re
+    regr = acme.register()
+logging.info('Auto-accepting TOS: %s', regr.terms_of_service)
+acme.agree_to_tos(regr)
+logging.debug(regr)
     
-      Args:
-    filename: The name of the current file.
-    clean_lines: A CleansedLines instance containing the file.
-    linenum: The number of the line to check.
-    nesting_state: A NestingState instance which maintains information about
-                   the current stack of nested blocks being parsed.
-    error: The function to call with any errors found.
-  '''
+        .. note:: This does not need to be accurate in order for the client to
+        run.  This simply keeps things clean if the user decides to revert
+        changes.
+    .. warning:: If all deps are not included, it may cause incorrect parsing
+        behavior, due to enable_mod's shortcut for updating the parser's
+        currently defined modules (`.ApacheParser.add_mod`)
+        This would only present a major problem in extremely atypical
+        configs that use ifmod for the missing deps.
     
-        def not_specification(self):
-        raise NotImplementedError()
+        def _get_addrs(self, achall):
+        '''Return the Apache addresses needed for TLS-SNI-01.'''
+        # TODO: Checkout _default_ rules.
+        addrs = set()
+        default_addr = obj.Addr(('*', str(
+            self.configurator.config.tls_sni_01_port)))
     
-    http://stackoverflow.com/questions/963965/how-is-this-strategy-pattern
- -written-in-python-the-sample-in-wikipedia
-In most of other languages Strategy pattern is implemented via creating some
-base strategy interface/abstract class and subclassing it with a number of
-concrete strategies (as we can see at
-http://en.wikipedia.org/wiki/Strategy_pattern), however Python supports
-higher-order functions and allows us to have only one class and inject
-functions into it's instances, as shown in this example.
+        def do_something(self, something):
+        return 'Doing %s' % something
     
-        def build_size(self):
-        self.size = 'Small'
     
-    *TL;DR80
-Stores a set of initialized objects kept ready to use.
-'''
+class ConcreteHandler2(Handler):
+    def _handle(self, request):
+        if 10 < request <= 20:
+            print('request {} handled in handler 2'.format(request))
+            return True
     
-        def register_object(self, name, obj):
-        '''Register an object'''
-        self._objects[name] = obj
+    In other programming languages, a more complex arrangement is sometimes
+necessary. In particular, you cannot have polymorphic behaviour in a constructor in C++ -
+see https://stackoverflow.com/questions/1453131/how-can-i-get-polymorphic-behavior-in-a-c-constructor
+- which means this Python technique will not work. The polymorphism
+required has to be provided by an external, already constructed
+instance of a different class.
     
-    class TimeDisplay(object):
+    production code which is untestable:
+    
+    
+class Controller(object):
+    def __init__(self, blackboard):
+        self.blackboard = blackboard
