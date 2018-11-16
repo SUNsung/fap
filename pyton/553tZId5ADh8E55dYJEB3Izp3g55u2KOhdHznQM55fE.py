592e33f06512d4f8915542c7a798de16cdbbd488
SUNsung <weatherius@gmail.com>
2018-11-16 09:21:16 +0200

    Rewrite [553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py] File in: 2018-11-16 09:21:15

--- pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
+++ pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
@@ -1,169 +1,190 @@
 
         
-            if check_author and post['author_id'] != g.user['id']:
-        abort(403)
+        from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
     
-        # test that successful login redirects to the index page
-    response = auth.login()
-    assert response.headers['Location'] == 'http://localhost/'
+        plt.figure('scikit-learn GLM benchmark results')
+    plt.xlabel('Dimensions')
+    plt.ylabel('Time (s)')
+    plt.plot(dimensions, time_ridge, color='r')
+    plt.plot(dimensions, time_ols, color='g')
+    plt.plot(dimensions, time_lasso, color='b')
     
-        The following characters are escaped in strings:
+        return results
     
+    import numpy as np
+from scipy.cluster import hierarchy
+import matplotlib.pyplot as plt
     
-class HasKey(PostgresSimpleLookup):
-    lookup_name = 'has_key'
-    operator = '?'
-    prepare_rhs = False
+    if __name__ == '__main__':
+    list_n_samples = np.linspace(100, 10000, 5).astype(np.int)
+    list_n_features = [10, 100, 1000]
+    n_test = 1000
+    max_iter = 1000
+    noise = 0.1
+    alpha = 0.01
+    sgd_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
+    elnet_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
+    ridge_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
+    asgd_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
+    for i, n_train in enumerate(list_n_samples):
+        for j, n_features in enumerate(list_n_features):
+            X, y, coef = make_regression(
+                n_samples=n_train + n_test, n_features=n_features,
+                noise=noise, coef=True)
     
-    
-class CITextExtension(CreateExtension):
-    
-        def save(self, session_key, session_dict, expire_date):
-        s = self.model(session_key, self.encode(session_dict), expire_date)
-        if session_dict:
-            s.save()
-        else:
-            s.delete()  # Clear sessions with no data.
-        return s
-    
-        def test_distant_exception(self):
-        def f():
-            1/0
-        def g():
-            f()
-        def h():
-            g()
-        def i():
-            h()
-        def j(p):
-            i()
-        f_ident = ident(f)
-        g_ident = ident(g)
-        h_ident = ident(h)
-        i_ident = ident(i)
-        j_ident = ident(j)
-        self.check_events(j, [(1, 'call', j_ident),
-                              (2, 'call', i_ident),
-                              (3, 'call', h_ident),
-                              (4, 'call', g_ident),
-                              (5, 'call', f_ident),
-                              (5, 'return', f_ident),
-                              (4, 'return', g_ident),
-                              (3, 'return', h_ident),
-                              (2, 'return', i_ident),
-                              (1, 'return', j_ident),
-                              ])
-    
-        return p[:0], p
-    
-            * Some platforms report ASCII as ANSI_X3.4-1968
-        * Some platforms report ASCII as US-ASCII
-        * Some platforms report UTF-8 instead of utf-8
-        '''
-        data = data.replace(b'ANSI_X3.4-1968', b'ascii')
-        data = data.replace(b'US-ASCII', b'ascii')
-        data = data.lower()
-        return data
-    
-            if MS_WINDOWS:
-            expected = 'utf-8/surrogatepass'
-        else:
-            expected = 'utf-8/surrogateescape'
-    
-        def testSeekPostEnd(self):
-        self.createTempFile()
-        with BZ2File(self.filename) as bz2f:
-            bz2f.seek(150000)
-            self.assertEqual(bz2f.tell(), len(self.TEXT))
-            self.assertEqual(bz2f.read(), b'')
-    
-        def persistent_load(self, pid):
-        # This method is invoked whenever a persistent ID is encountered.
-        # Here, pid is the tuple returned by DBPickler.
-        cursor = self.connection.cursor()
-        type_tag, key_id = pid
-        if type_tag == 'MemoRecord':
-            # Fetch the referenced record from the database and return it.
-            cursor.execute('SELECT * FROM memos WHERE key=?', (str(key_id),))
-            key, task = cursor.fetchone()
-            return MemoRecord(key, task)
-        else:
-            # Always raises an error if you cannot return the correct object.
-            # Otherwise, the unpickler will think None is the object referenced
-            # by the persistent ID.
-            raise pickle.UnpicklingError('unsupported persistent object')
-    
-        with open(args.msgfile, 'rb') as fp:
-        msg = email.message_from_binary_file(fp, policy=default)
-    
-    <slide><title>Another demo slide</title>
-<point>It is important</point>
-<point>To have more than</point>
-<point>one slide</point>
-</slide>
-</slideshow>
-'''
-    
-        it = manager.baz()
-    for i in it:
-        print('<%d>' % i, end=' ')
-    print()
-    
-    '''
-Reference: https://en.wikipedia.org/wiki/Delegation_pattern
-Author: https://github.com/IuryAlves
-    
-        requests *= 10000
-    client1_delegate = timeit(client1.delegate)
-    client2_delegate = timeit(client2.delegate)
-    with suppress_stdout():
-        client1_delegate(requests)
-        client2_delegate(requests)
-    # lets check which is faster
-    print(client1_delegate._time, client2_delegate._time)
-    
-        def amount(self, val):
-        print(val, end=' ')
-        return self
-    
-    print()
-    
-        def __new__(cls, name, bases, attrs):
-        new_cls = type.__new__(cls, name, bases, attrs)
-        '''
-            Here the name of the class is used as key but it could be any class
-            parameter.
-        '''
-        cls.REGISTRY[new_cls.__name__] = new_cls
-        return new_cls
-    
-        strat2 = StrategyExample(execute_replacement2)
-    strat2.name = 'Strategy Example 2'
-    
-        # A Shop that sells only cats
-    cat_shop = PetShop(Cat)
-    cat_shop.show_pet()
-    print('')
-    
-    In other programming languages, a more complex arrangement is sometimes
-necessary. In particular, you cannot have polymorphic behaviour in a constructor in C++ -
-see https://stackoverflow.com/questions/1453131/how-can-i-get-polymorphic-behavior-in-a-c-constructor
-- which means this Python technique will not work. The polymorphism
-required has to be provided by an external, already constructed
-instance of a different class.
+    Line #      Hits         Time  Per Hit   % Time  Line Contents
+==============================================================
+    51                                           @profile
+    52                                           def benchmark_dense_predict():
+    53       301          640      2.1      0.1      for _ in range(300):
+    54       300       532339   1774.5     99.9          clf.predict(X_test)
     
     
-def lazy_property2(fn):
-    attr = '_lazy__' + fn.__name__
+print(':orphan:')
+print()
+heading = 'Available documentation for Scikit-learn'
+print(heading)
+print('=' * len(heading))
+print()
+print('Web-based documentation is available for versions listed below:')
+print()
     
-        def find_all_path(self, start, end, path=None):
-        path = path or []
-        path.append(start)
-        if start == end:
-            return [path]
-        paths = []
-        for node in self.graph.get(start, []):
-            if node not in path:
-                newpaths = self.find_all_path(node, end, path[:])
-                paths.extend(newpaths)
-        return paths
+    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
+TRAIN_FOLDER = '20news-bydate-train'
+TEST_FOLDER = '20news-bydate-test'
+    
+        if sys.platform == 'win32':
+        win32_lib = os.path.abspath( os.path.join(python_path, 'lib', 'win32'))
+        sys.path.append(win32_lib)
+    elif sys.platform.startswith('linux'):
+        linux_lib = os.path.abspath( os.path.join(python_path, 'lib', 'linux'))
+        sys.path.append(linux_lib)
+    elif sys.platform == 'darwin':
+        darwin_lib = os.path.abspath( os.path.join(python_path, 'lib', 'darwin'))
+        sys.path.append(darwin_lib)
+        extra_lib = '/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python'
+        sys.path.append(extra_lib)
+    
+    ## Anything on different channel than DEFAULT_CHANNEL is not parsed
+# by parser.
+HIDDEN_CHANNEL = 99
+    
+        def __init__(
+        self, grammarDecisionDescription, decisionNumber, stateNumber, input
+        ):
+        RecognitionException.__init__(self, input)
+    
+            if args:
+            if 'gui' in conf and conf['gui']:
+                # Enter GUI mode.
+                from .gui import gui_main
+                gui_main(*args, **conf)
+            else:
+                # Enter console mode.
+                from .console import console_main
+                console_main(*args, **conf)
+    
+    class VideoExtractor():
+    def __init__(self, *args):
+        self.url = None
+        self.title = None
+        self.vid = None
+        self.m3u8_url = None
+        self.streams = {}
+        self.streams_sorted = []
+        self.audiolang = None
+        self.password_protected = False
+        self.dash_streams = {}
+        self.caption_tracks = {}
+        self.out = False
+        self.ua = None
+        self.referer = None
+        self.danmuku = None
+    
+            for i in html_json['sources']:
+            if 'src' in i:  #to avoid KeyError
+                if i['src'].startswith('https'):
+                    link_list.append((str(i['height']), i['src']))
+    
+            assert vid
+    
+    
+def embed_download(url, output_dir = '.', merge = True, info_only = False ,**kwargs):
+    content = get_content(url, headers=fake_headers)
+    found = False
+    title = match1(content, '<title>([^<>]+)</title>')
+    
+    
+site_info = 'fantasy.tv'
+download = fantasy_download
+download_playlist = playlist_not_supported('fantasy.tv')
+
+    
+        print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([video_url], title, ext, size, output_dir, merge=merge, headers = fake_headers)
+    
+        if results.overwrite and os.path.exists(output_directory):
+        shutil.rmtree(output_directory)
+    
+            # The following code is provided here to aid in writing a correct migration
+        # Changing field 'ReleaseEnvironment.project_id'
+        db.alter_column(
+            'sentry_environmentrelease', 'project_id',
+            self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
+        )
+    
+            for release_project in RangeQuerySetWrapperWithProgressBar(
+            orm.ReleaseProject.objects.all()
+        ):
+            orm.ReleaseProject.objects.filter(id=release_project.id).update(
+                new_groups=orm.Group.objects.filter(
+                    project_id=release_project.project_id,
+                    first_release_id=release_project.release_id,
+                ).count()
+            )
+    
+    
+def generateKey(keySize):
+    print('Generating prime p...')
+    p = rabinMiller.generateLargePrime(keySize)  # select large prime number.
+    e_1 = primitiveRoot(p)  # one primitive root on modulo p.
+    d = random.randrange(3, p)  # private_key -> have to be greater than 2 for safety.
+    e_2 = cryptoMath.findModInverse(pow(e_1, d, p), p)
+    
+        if num in lowPrimes:
+        return True
+    
+            while self.values[new_key] is not None \
+                and self.values[new_key] != key:
+            i += 1
+            new_key = self.hash_function(key + i*i) if not \
+                self.balanced_factor() >= self.lim_charge else None
+    
+    try:
+	xrange		#Python 2
+except NameError:
+	xrange = range	#Python 3
+    
+    Inputs:
+  - X , a 2D numpy array of features.
+  - k , number of clusters to create.
+  - initial_centroids , initial centroid values generated by utility function(mentioned in usage).
+  - maxiter , maximum number of iterations to process.
+  - heterogeneity , empty list that will be filled with hetrogeneity values if passed to kmeans func.
+    
+            a += a
+        b >>= 1
+    
+    def loadDictionary():
+    path = os.path.split(os.path.realpath(__file__))
+    dictionaryFile = open(path[0] + '/Dictionary.txt')
+    englishWords = {}
+    for word in dictionaryFile.read().split('\n'):
+        englishWords[word] = None
+    dictionaryFile.close()
+    return englishWords
