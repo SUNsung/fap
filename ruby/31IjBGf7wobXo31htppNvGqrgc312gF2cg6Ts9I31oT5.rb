90a0828777795d283bf79256593d8827d77356ca
SUNsung <weatherius@gmail.com>
2018-11-16 03:40:09 +0200

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2018-11-16 03:40:08

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,90 +1,79 @@
 
         
-            def rescue_file_error(e)
-      # We're also printing the new-lines, as otherwise the message is not very visible in-between the error and the stack trace
-      puts('')
-      FastlaneCore::UI.important('Error accessing file, this might be due to fastlane's directory handling')
-      FastlaneCore::UI.important('Check out https://docs.fastlane.tools/advanced/#directory-behavior for more details')
-      puts('')
-      raise e
-    end
-    
-          it 'logs the command if verbose' do
-        with_verbose(true) do
-          allow(Fastlane::Actions).to receive(:sh).with(anything, { log: true }).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: 'foo.bar')
-          end').runner.execute(:test)
+              it 'does not try to convert with to_str' do
+        obj = BasicObject.new
+        def obj.to_str
+          'abc'
         end
-      end
     
-          # Verify that value is the type that we're expecting, if we are expecting a type
-      if data_type == Fastlane::Boolean
-        ensure_boolean_type_passes_validation(value)
-      else
-        ensure_generic_type_passes_validation(value)
-      end
+    describe 'Kernel.sleep' do
+  it 'needs to be reviewed for spec completeness'
+end
+
     
-            # this command is also sent on macOS Sierra and we need to allow it or else the test will fail
-        allowed_command = 'security set-key-partition-list -S apple-tool:,apple: -k #{''.shellescape} #{Dir.home}/Library/Keychains/login.keychain &> /dev/null'
+    describe :kernel_system, shared: true do
+  it 'executes the specified command in a subprocess' do
+    lambda { @object.system('echo a') }.should output_to_fd('a\n')
     
-        # /%3faaa=bbbbb
-    # which could possibly decode to '/?aaa=bbbbb', which if the IDS normalizes first, then splits the URI on ?, then it can be bypassed
-    if self.junk_param_start
-      str.sub!(/\//, '/%3f' + Rex::Text.rand_text_alpha(rand(5) + 1) + '=' + Rex::Text.rand_text_alpha(rand(10) + 1) + '/../')
-    end
-    
-        # If keep-alive isn't enabled for this client, close the connection
-    if (cli.keepalive == false)
-      close_client(cli)
-    end
+      it 'returns true when passed ?e if the argument is a file' do
+    Kernel.test(?e, @file).should == true
   end
     
-          when IAX_CTRL_BUSY
-        dprint('BUSY')
-        self.busy  = true
-        self.state = :hangup
-        self.client.send_ack(self)
+      # String arguments should be evaluated in the context of the caller.
+  it 'accepts a String argument instead of a Proc or block' do
+    trace_var :$Kernel_trace_var_global, '$Kernel_trace_var_extra = true'
     
-                res = checksum + encrypted
-            res
+            private
+    
+            # Takes besides the products attributes either an array of variants or
+        # an array of option types.
+        #
+        # By submitting an array of variants the option types will be created
+        # using the *name* key in options hash. e.g
+        #
+        #   product: {
+        #     ...
+        #     variants: {
+        #       price: 19.99,
+        #       sku: 'hey_you',
+        #       options: [
+        #         { name: 'size', value: 'small' },
+        #         { name: 'color', value: 'black' }
+        #       ]
+        #     }
+        #   }
+        #
+        # Or just pass in the option types hash:
+        #
+        #   product: {
+        #     ...
+        #     option_types: ['size', 'color']
+        #   }
+        #
+        # By passing the shipping category name you can fetch or create that
+        # shipping category on the fly. e.g.
+        #
+        #   product: {
+        #     ...
+        #     shipping_category: 'Free Shipping Items'
+        #   }
+        #
+        def new; end
+    
+            def scope
+          if params[:country_id]
+            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
+            @country.states.accessible_by(current_ability, :read).order('name ASC')
+          else
+            State.accessible_by(current_ability, :read).order('name ASC')
           end
         end
       end
     end
   end
 end
+
     
-                seq = OpenSSL::ASN1::Sequence.new(seqs)
-    
-              # @!attribute key
-          #   @return [Integer] The type of encryption key
-          attr_accessor :type
-          # @!attribute value
-          #   @return [String] the key itself
-          attr_accessor :value
-    
-        set :run, Proc.new { File.expand_path($0) == File.expand_path(app_file) }
-    
-        <div id='cookies'>
-      <h3 id='cookie-info'>COOKIES</h3>
-      <% unless req.cookies.empty? %>
-        <table class='req'>
-          <tr>
-            <th>Variable</th>
-            <th>Value</th>
-          </tr>
-          <% req.cookies.each { |key, val| %>
-            <tr>
-              <td><%=h key %></td>
-              <td class='code'><div><%=h val.inspect %></div></td>
-            </tr>
-          <% } %>
-        </table>
-      <% else %>
-        <p class='no-data'>No cookie data.</p>
-      <% end %>
-      <div class='clear'></div>
-    </div> <!-- /COOKIES -->
-    
-          alias react deny
+            def stock_location
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:id])
+        end
