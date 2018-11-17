38b6d73563050decae31cfbcb8eaa0568185de2d
SUNsung <weatherius@gmail.com>
2018-11-17 03:14:52 +0200

    Rewrite [12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp] File in: 2018-11-17 03:14:52

--- cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
+++ cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
@@ -1,215 +1,200 @@
 
         
-        #include 'base/stl_util.h'
-    
-    #include <string>
-    
-    #include 'atom/browser/api/trackable_object.h'
-#include 'atom/browser/native_browser_view.h'
-#include 'native_mate/handle.h'
-    
-    void StopRecording(const base::FilePath& path,
-                   const CompletionCallback& callback) {
-  TracingController::GetInstance()->StopTracing(
-      GetTraceDataEndpoint(path, callback));
+        template <>
+template <typename T>
+bool EnforceFiniteOp<CPUContext>::DoRunWithType() {
+  EnforceOnCPU<T>(Input(0));
+  return true;
 }
     
-    using extensions::GlobalShortcutListener;
+      const auto& X = in[0];
+  const auto& W = in[1];
+  const auto& b = in[2];
+  auto axis = helper.GetSingleArgument<int32_t>('axis', 1);
+  const auto canonical_axis = canonical_axis_index_(axis, in[0].dims().size());
+  const int M = size_to_dim_(canonical_axis, GetDimsVector(in[0]));
+  const int K = size_from_dim_(canonical_axis, GetDimsVector(in[0]));
+  auto axis_w = helper.GetSingleArgument<int32_t>('axis_w', 1);
+  const int canonical_axis_w =
+      canonical_axis_index_(axis_w, in[1].dims().size());
+  const int N = size_to_dim_(canonical_axis_w, GetDimsVector(in[1]));
     
-    #endif  // ATOM_BROWSER_API_ATOM_API_IN_APP_PURCHASE_H_
-
+    ```
     
-    namespace mate {
-    }
-    
-    #define FARG_LINE(type, n) , type * dst##n##Base, ptrdiff_t dst##n##Stride
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s32, s8, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src1]]                              \n\t'
-             'vld1.32 {d2-d3}, [%[src2]]                              \n\t'
-             'vqmovn.s32 d4, q0                                       \n\t'
-             'vqmovn.s32 d5, q1                                       \n\t'
-             'vqmovn.s16  d6, q2                                      \n\t'
-             'vst1.8 {d6}, [%[dst]]                                   \n\t'
-             : /*no output*/
-             : [src1] 'r' (_src + i + 0),
-               [src2] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i)
-             : 'd0','d1','d2','d3','d4','d5','d6'
-         );
-     }
-})
-#else
-CVT_FUNC(s32, s8, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         int32x4_t vline1_s32 = vld1q_s32(_src + i);
-         int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
-    }
-    }
-    
-    typedef void (* rshiftConstFunc)(const Size2D &size,
-                                const s16 * srcBase, ptrdiff_t srcStride,
-                                u8 * dstBase, ptrdiff_t dstStride,
-                                CONVERT_POLICY cpolicy);
-    
-                    vSum_0_4 = vmlaq_u16(vSum_0_4, vLane2.val[0], vc6u16);
-                vSum_1_5 = vmlaq_u16(vSum_1_5, vLane2.val[1], vc6u16);
-                vSum_2_6 = vmlaq_u16(vSum_2_6, vLane2.val[2], vc6u16);
+    			if (szArgList[i + 1] != NULL && szArgList[i + 1][0] != '/')
+			{
+				cmderOptions.cmderCfgRoot = szArgList[i + 1];
+				i++;
+			}
+		}
+		else if (_wcsicmp(L'/start', szArgList[i]) == 0)
+		{
+			int len = wcslen(szArgList[i + 1]);
+			if (wcscmp(&szArgList[i + 1][len - 1], L'\'') == 0)
+			{
+				szArgList[i + 1][len - 1] = '\0';
+			}
     
     
-    { protected:
-  int n_ = 0;
-  std::mutex m_;
-  std::condition_variable cv_;
+    { private:
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
 };
     
-    namespace caffe2 {
-    }
-    
-    namespace caffe2 {
-    }
-    
-    NO_GRADIENT(GivenTensorFill);
-NO_GRADIENT(GivenTensorDoubleFill);
-NO_GRADIENT(GivenTensorBoolFill);
-NO_GRADIENT(GivenTensorIntFill);
-NO_GRADIENT(GivenTensorInt64Fill);
-NO_GRADIENT(GivenTensorStringFill);
-    
-    
-    {     public:
-      DataFile(SpecialEnv* env, WritableFile* base)
-          : env_(env),
-            base_(base) {
-      }
-      ~DataFile() { delete base_; }
-      Status Append(const Slice& data) {
-        if (env_->no_space_.Acquire_Load() != nullptr) {
-          // Drop writes on the floor
-          return Status::OK();
-        } else {
-          return base_->Append(data);
-        }
-      }
-      Status Close() { return base_->Close(); }
-      Status Flush() { return base_->Flush(); }
-      Status Sync() {
-        if (env_->data_sync_error_.Acquire_Load() != nullptr) {
-          return Status::IOError('simulated data sync error');
-        }
-        while (env_->delay_data_sync_.Acquire_Load() != nullptr) {
-          DelayMilliseconds(100);
-        }
-        return base_->Sync();
-      }
-    };
-    class ManifestFile : public WritableFile {
-     private:
-      SpecialEnv* env_;
-      WritableFile* base_;
-     public:
-      ManifestFile(SpecialEnv* env, WritableFile* b) : env_(env), base_(b) { }
-      ~ManifestFile() { delete base_; }
-      Status Append(const Slice& data) {
-        if (env_->manifest_write_error_.Acquire_Load() != nullptr) {
-          return Status::IOError('simulated writer error');
-        } else {
-          return base_->Append(data);
-        }
-      }
-      Status Close() { return base_->Close(); }
-      Status Flush() { return base_->Flush(); }
-      Status Sync() {
-        if (env_->manifest_sync_error_.Acquire_Load() != nullptr) {
-          return Status::IOError('simulated sync error');
-        } else {
-          return base_->Sync();
-        }
-      }
-    };
-    
-    inline bool ParseInternalKey(const Slice& internal_key,
-                             ParsedInternalKey* result) {
-  const size_t n = internal_key.size();
-  if (n < 8) return false;
-  uint64_t num = DecodeFixed64(internal_key.data() + n - 8);
-  unsigned char c = num & 0xff;
-  result->sequence = num >> 8;
-  result->type = static_cast<ValueType>(c);
-  result->user_key = Slice(internal_key.data(), n - 8);
-  return (c <= static_cast<unsigned char>(kTypeValue));
-}
-    
-    Status SetCurrentFile(Env* env, const std::string& dbname,
-                      uint64_t descriptor_number) {
-  // Remove leading 'dbname/' and add newline to manifest file name
-  std::string manifest = DescriptorFileName(dbname, descriptor_number);
-  Slice contents = manifest;
-  assert(contents.starts_with(dbname + '/'));
-  contents.remove_prefix(dbname.size() + 1);
-  std::string tmp = TempFileName(dbname, descriptor_number);
-  Status s = WriteStringToFileSync(env, contents.ToString() + '\n', tmp);
-  if (s.ok()) {
-    s = env->RenameFile(tmp, CurrentFileName(dbname));
+    SparsePageWriter::~SparsePageWriter() {
+  for (auto& queue : qworkers_) {
+    // use nullptr to signal termination.
+    std::shared_ptr<SparsePage> sig(nullptr);
+    queue.Push(std::move(sig));
   }
-  if (!s.ok()) {
-    env->DeleteFile(tmp);
-  }
-  return s;
-}
-    
-    
-    {    // Some corruption was detected.  'size' is the approximate number
-    // of bytes dropped due to the corruption.
-    virtual void Corruption(size_t bytes, const Status& status) = 0;
-  };
-    
-      // Compute the crc of the record type and the payload.
-  uint32_t crc = crc32c::Extend(type_crc_[t], ptr, n);
-  crc = crc32c::Mask(crc);                 // Adjust for storage
-  EncodeFixed32(buf, crc);
-    
-      // crc32c values for all supported record types.  These are
-  // pre-computed to reduce the overhead of computing the crc of the
-  // record type stored in the header.
-  uint32_t type_crc_[kMaxRecordType + 1];
-    
-    void IDCT1d(const double* in, int stride, double* out) {
-  for (int x = 0; x < 8; ++x) {
-    out[x * stride] = 0.0;
-    for (int u = 0; u < 8; ++u) {
-      out[x * stride] += kDCTMatrix[8 * u + x] * in[u * stride];
-    }
+  for (auto& thread : workers_) {
+    thread->join();
   }
 }
     
     
-    {}  // namespace guetzli
+    {
+    {
+    {  // base margin
+  bst_float base_margin;
+  // model parameter
+  GBTreeModelParam param;
+  /*! \brief vector of trees stored in the model */
+  std::vector<std::unique_ptr<RegTree> > trees;
+  /*! \brief for the update process, a place to keep the initial trees */
+  std::vector<std::unique_ptr<RegTree> > trees_to_update;
+  /*! \brief some information indicator of the tree, reserved */
+  std::vector<int> tree_info;
+};
+}  // namespace gbm
+}  // namespace xgboost
+
     
-    #ifndef GUETZLI_IDCT_H_
-#define GUETZLI_IDCT_H_
+    namespace xgboost {
+namespace obj {
+    }
+    }
     
-    #ifndef GUETZLI_JPEG_DATA_ENCODER_H_
-#define GUETZLI_JPEG_DATA_ENCODER_H_
+      virtual bst_float ComputeSplitScore(bst_uint nodeid,
+                                      bst_uint featureid,
+                                      const GradStats& left_stats,
+                                      const GradStats& right_stats) const;
     
-    void BuildHuffmanCodeTable(const int* counts, const int* values,
-                           HuffmanCodeTable* table) {
-  int huffcode[256];
-  int huffsize[256];
-  int p = 0;
-  for (int l = 1; l <= kJpegHuffmanMaxBitLength; ++l) {
-    int i = counts[l];
-    while (i--) huffsize[p++] = l;
+      std::vector<int> range_;
+    
+    // Source project : https://github.com/ismaelJimenez/cpp.leastsq
+// Adapted to be used with google benchmark
+    
+      for (auto& c : result.counters) {
+    const std::size_t cNameLen = std::max(std::string::size_type(10),
+                                          c.first.length());
+    auto const& s = HumanReadableNumber(c.second.value, 1000);
+    if (output_options_ & OO_Tabular) {
+      if (c.second.flags & Counter::kIsRate) {
+        printer(Out, COLOR_DEFAULT, ' %*s/s', cNameLen - 2, s.c_str());
+      } else {
+        printer(Out, COLOR_DEFAULT, ' %*s', cNameLen, s.c_str());
+      }
+    } else {
+      const char* unit = (c.second.flags & Counter::kIsRate) ? '/s' : '';
+      printer(Out, COLOR_DEFAULT, ' %s=%s%s', c.first.c_str(), s.c_str(),
+              unit);
+    }
+  }
+    
+    namespace {
+std::exception const* get_std_exception_(std::exception_ptr eptr) noexcept {
+  try {
+    std::rethrow_exception(eptr);
+  } catch (const std::exception& ex) {
+    return &ex;
+  } catch (...) {
+    return nullptr;
+  }
+}
+} // namespace
+    
+    template <class UIntType, UIntType a, UIntType c, UIntType m>
+struct StateSize<std::linear_congruential_engine<UIntType, a, c, m>> {
+  // From the standard [rand.eng.lcong], this is ceil(log2(m) / 32) + 3,
+  // which is the same as ceil(ceil(log2(m) / 32) + 3, and
+  // ceil(log2(m)) <= std::numeric_limits<UIntType>::digits
+  using type = std::integral_constant<
+      size_t,
+      (std::numeric_limits<UIntType>::digits + 31) / 32 + 3>;
+};
+    
+    inline UriTuple as_tuple(const folly::Uri& k) {
+  return UriTuple(
+      k.scheme(),
+      k.username(),
+      k.password(),
+      k.host(),
+      k.port(),
+      k.path(),
+      k.query(),
+      k.fragment());
+}
+    
+    namespace folly {
+    }
+    
+      ~atomic_shared_ptr() {
+    store(SharedPtr(nullptr));
+  }
+  void operator=(SharedPtr desired) /* noexcept */ {
+    store(std::move(desired));
+  }
+  void operator=(const atomic_shared_ptr<T>&) = delete;
+    
+    /// Returns the best real CacheLocality information available
+static CacheLocality getSystemLocalityInfo() {
+  if (kIsLinux) {
+    try {
+      return CacheLocality::readFromSysfs();
+    } catch (...) {
+      // keep trying
+    }
   }
     }
+    
+    ErrorCode FakeCanClient::Receive(std::vector<CanFrame> *const frames,
+                                 int32_t *const frame_num) {
+  if (frame_num == nullptr || frames == nullptr) {
+    AERROR << 'frames or frame_num pointer is null';
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
+  }
+  frames->resize(*frame_num);
+  const int MOCK_LEN = 8;
+  for (size_t i = 0; i < frames->size(); ++i) {
+    for (int j = 0; j < MOCK_LEN; ++j) {
+      (*frames)[i].data[j] = j;
+    }
+    (*frames)[i].id = i;
+    (*frames)[i].len = MOCK_LEN;
+    ADEBUG << (*frames)[i].CanFrameString() << 'frame_num[' << i << ']';
+  }
+  usleep(USLEEP_INTERVAL);
+  ++recv_counter_;
+  return ErrorCode::OK;
+}
+    
+    
+    {
+    {
+    {
+    {}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    // Canbus gflags
+DEFINE_double(sensor_freq, 100,
+              'Sensor feedback timer frequency -- 0 means event trigger.');
