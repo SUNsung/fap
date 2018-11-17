cad43521ce6b47a8f65ba07d2582d0a703f3b86d
SUNsung <weatherius@gmail.com>
2018-11-17 21:54:03 +0200

    Rewrite [46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp] File in: 2018-11-17 21:54:02

--- cccccc/46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp
+++ cccccc/46gbMqLjGjv2k460IRBxsCRmWw46VUABisZDLMQ46t60.cpp
@@ -1,163 +1,140 @@
 
         
-        Optional<PlatformKind> swift::platformFromString(StringRef Name) {
-  if (Name == '*')
-    return PlatformKind::none;
-  return llvm::StringSwitch<Optional<PlatformKind>>(Name)
-#define AVAILABILITY_PLATFORM(X, PrettyName) .Case(#X, PlatformKind::X)
-#include 'swift/AST/PlatformKinds.def'
-      .Case('macOS', PlatformKind::OSX)
-      .Case('macOSApplicationExtension', PlatformKind::OSXApplicationExtension)
-      .Default(Optional<PlatformKind>());
-}
+                    int16x8_t tail02 = vreinterpretq_s16_u16(vaddl_u8(tail2, tail0));
+            int16x8_t tail1x2 = vreinterpretq_s16_u16(vshll_n_u8(tail1, 1));
+            int16x8_t taildx = vreinterpretq_s16_u16(vsubl_u8(tail2, tail0));
+            int16x8_t taildy = vqaddq_s16(tail02, tail1x2);
     
-    bool swift::parseASTSection(SerializedModuleLoader *SML, StringRef buf,
-                            SmallVectorImpl<std::string> &foundModules) {
-  if (!serialization::isSerializedAST(buf))
-    return false;
+    #endif
+    
+    #ifdef CAROTENE_NEON
+    if (shift >= 16u)
+    {
+        for (size_t i = 0; i < size.height; ++i)
+        {
+            s16 * dst = internal::getRowPtr(dstBase, dstStride, i);
+            std::memset(dst, 0, sizeof(s16) * size.width);
+        }
+        return;
     }
-    
-      // Set the '_endian' platform condition.
-  switch (Target.getArch()) {
-  case llvm::Triple::ArchType::arm:
-  case llvm::Triple::ArchType::thumb:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::aarch64:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::ppc64:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'big');
-    break;
-  case llvm::Triple::ArchType::ppc64le:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::x86:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::x86_64:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::systemz:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'big');
-    break;
-  default:
-    llvm_unreachable('undefined architecture endianness');
-  }
-    
-    unsigned swift::measureNewline(const char *BufferPtr, const char *BufferEnd) {
-  if (BufferPtr == BufferEnd)
-    return 0;
-    }
-    
-    TaskQueue::~TaskQueue() = default;
     
     
     {
-    {}}
-    
-    int64_t VMTOC::allocTOC(int64_t target) {
-  folly::MSLGuard g{s_TOC};
-  HPHP::Address addr = m_tocvector->frontier();
-  m_tocvector->qword(target);
-  return addr - (m_tocvector->base() + INT16_MAX + 1);
+    {        for (--jd; js < size.width; ++js, --jd)
+            dst[jd] = src[js];
+    }
 }
     
-          XS_form_t xs_formater {{
-                            rc,
-                            static_cast<uint32_t>(sh >> 5),
-                            xop,
-                            static_cast<uint32_t>(sh & 0x1F),
-                            static_cast<uint32_t>(ra),
-                            static_cast<uint32_t>(rt),
-                            op
-                          }};
-    
-    inline APCLocalArray::APCLocalArray(const APCArray* source)
-  : ArrayData(kApcKind)
-  , m_arr(source)
-{
-  m_size = m_arr->size();
-  source->reference();
-  tl_heap->addApcArray(this);
-  memset(localCache(), static_cast<data_type_t>(KindOfUninit),
-         m_size * sizeof(TypedValue));
-  assertx(hasExactlyOneRef());
-}
+        // the others
+    for (size_t i = 1; i < size.height ; ++i)
+    {
+        src = internal::getRowPtr(srcBase, srcStride, i);
+        u32 * prevSum = internal::getRowPtr(sumBase, sumStride, i - 1);
+        sum = internal::getRowPtr(sumBase, sumStride, i);
+    }
     
     
-    {  static void
-  StringInsert(std::vector<std::string>& values, const std::string& /*key*/,
-               const std::string& value) {
-    values.push_back(value);
-  }
-  static void
-  StringInsert(boost::container::flat_set<std::string>& values,
-               const std::string& /*key*/, const std::string& value) {
-    values.insert(value);
-  }
-  static void
-  StringInsert(std::set<std::string, stdltistr>& values,
-               const std::string& /*key*/, const std::string& value) {
-    values.insert(value);
-  }
-  static void
-  StringInsert(std::set<std::string>& values, const std::string& /*key*/,
-               const std::string& value) {
-    values.insert(value);
-  }
-  static void StringInsert(std::map<std::string, std::string> &values,
-                           const std::string &key,
-                           const std::string &value) {
-    values[key] = value;
-  }
-  static void StringInsert(std::map<std::string, std::string,
-                           stdltistr> &values,
-                           const std::string &key,
-                           const std::string &value) {
-    values[key] = value;
-  }
-  static void StringInsert(hphp_string_imap<std::string> &values,
-                           const std::string &key,
-                           const std::string &value) {
-    values[key] = value;
-  }
-  static void ReplaceIncludesWithIni(const std::string& original_ini_filename,
-                                     const std::string& iniStr,
-                                     std::string& with_includes);
+    {            vst1q_s16(drow + x - 8, t0);
+        }
+    
+    
+    {	_FORCE_INLINE_ btTypedConstraint *get_bt_constraint() { return constraint; }
+};
+#endif
+
+    
+    void unregister_dds_types() {
+    }
+    
+    
+    {	ERR_FAIL_COND(!zfile);
+	unzSeekCurrentFile(zfile, get_len() + p_position);
 };
     
-      /**
-   * Dump detailed information to return string.
+    
+    {  /*!
+   * \brief Create a tree updater given name
+   * \param name Name of the tree updater.
    */
-  virtual String debuggerDump() {
-    return String();
+  static TreeUpdater* Create(const std::string& name);
+};
+    
+    
+    {
+    {
+    {  inline void PutChar(char ch) {
+    out_buf += ch;
+    if (out_buf.length() >= kBufferSize) Flush();
   }
-    
-    #ifndef incl_HPHP_CPP_BASE_EXCEPTIONS_H_
-#define incl_HPHP_CPP_BASE_EXCEPTIONS_H_
-    
-    inline void ExecutionContext::setTimeZone(const String& timezone) {
-  m_timezone = timezone;
-}
-    
-      CLASSNAME_IS('OutputFile');
-  // overriding ResourceData
-  const String& o_getClassNameHook() const override { return classnameof(); }
-    
-    TEST_F(PacksTests, test_discovery_zero_state) {
-  Pack pack('discovery_pack', getPackWithDiscovery().doc());
-  auto stats = pack.getStats();
-  EXPECT_EQ(stats.total, 0U);
-  EXPECT_EQ(stats.hits, 0U);
-  EXPECT_EQ(stats.misses, 0U);
-}
-    
-    
-    {  WriteLock lock(scratch_mutex_);
-  scratch_ = (char*)malloc(kINotifyBufferSize);
-  if (scratch_ == nullptr) {
-    return Status(1, 'Could not allocate scratch space');
+  inline void Flush(void) {
+    if (out_buf.length() != 0) {
+      fp->Write(&out_buf[0], out_buf.length());
+      out_buf.clear();
+    }
   }
-  return Status(0, 'OK');
+};
+}  // namespace common
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_BASE64_H_
+
+    
+    #include <xgboost/data.h>
+#include <algorithm>
+#include <vector>
+    
+    
+    {
+    {void SparsePageWriter::Alloc(std::shared_ptr<SparsePage>* out_page) {
+  CHECK(*out_page == nullptr);
+  if (num_free_buffer_ != 0) {
+    out_page->reset(new SparsePage());
+    --num_free_buffer_;
+  } else {
+    CHECK(qrecycle_.Pop(out_page));
+  }
 }
+}  // namespace data
+}  // namespace xgboost
+    
+    SEXP XGBoosterPredict_R(SEXP handle, SEXP dmat, SEXP option_mask, SEXP ntree_limit) {
+  SEXP ret;
+  R_API_BEGIN();
+  bst_ulong olen;
+  const float *res;
+  CHECK_CALL(XGBoosterPredict(R_ExternalPtrAddr(handle),
+                            R_ExternalPtrAddr(dmat),
+                            asInteger(option_mask),
+                            asInteger(ntree_limit),
+                            &olen, &res));
+  ret = PROTECT(allocVector(REALSXP, olen));
+  for (size_t i = 0; i < olen; ++i) {
+    REAL(ret)[i] = res[i];
+  }
+  R_API_END();
+  UNPROTECT(1);
+  return ret;
+}
+    
+    
+    {  *value = result;
+  return true;
+}
+    
+    double Finish(Counter const& c, double cpu_time, double num_threads) {
+  double v = c.value;
+  if (c.flags & Counter::kIsRate) {
+    v /= cpu_time;
+  }
+  if (c.flags & Counter::kAvgThreads) {
+    v /= num_threads;
+  }
+  return v;
+}
+    
+    namespace benchmark {
+    }
+    
+    #ifndef BENCHMARK_OS_WINDOWS
+#include <sys/time.h>
+#include <time.h>
+#endif
