f2632486498d214537de80e0e62e1a802a378428
SUNsung <weatherius@gmail.com>
2018-11-17 04:04:49 +0200

    Rewrite [06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py] File in: 2018-11-17 04:04:49

--- pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
+++ pyton/06G1Fu2CKHDzQ06uAPSN408.Ho06ueePD9Tu6s06Rf.py
@@ -1,184 +1,157 @@
 
         
-            def _dispatch_call(self, call, employees):
-        for employee in employees:
-            if employee.call is None:
-                employee.take_call(call)
-                return employee
-        return None
-    
-        def extract_year_month(self, timestamp):
-        '''Return the year and month portions of the timestamp.'''
-        ...
-    
-        def mapper_sort(self, key, value):
-        '''Construct key to ensure proper sorting.
-    
-    
-class Cache(object):
-    
-    
-class PathBasedModel(object):
-  '''The LexNET path-based model for classifying semantic relations.'''
-    
-    if S < N:
-  # Note that this isn't necessary for this synthetic example, but
-  # it's useful to see how the input factor matrices were initialized
-  # for actual neurophysiology data.
-  datasets = add_alignment_projections(datasets, npcs=FLAGS.npcs)
-    
-    
-rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
-u_rng = np.random.RandomState(seed=FLAGS.synth_data_seed+1)
-T = FLAGS.T
-C = FLAGS.C
-N = FLAGS.N  # must be same N as in trained model (provided example is N = 50)
-nreplications = FLAGS.nreplications
-E = nreplications * C  # total number of trials
-train_percentage = FLAGS.train_percentage
-ntimesteps = int(T / FLAGS.dt)
-batch_size = 1  # gives one example per ntrial
-    
-      _partial_score(scoring1, mask1)
-  _partial_score(scoring2, mask2)
-    
-    
-def hash_function(input_tuple):
-  '''Hash function for a tuple.'''
-  return hash(input_tuple)
-    
-        if model.__class__.__name__ == 'Sequential':
-        sequential_like = True
-    elif not model._is_graph_network:
-        # We treat subclassed models as a simple sequence of layers,
-        # for logging purposes.
-        sequential_like = True
-    else:
-        sequential_like = True
-        nodes_by_depth = model._nodes_by_depth.values()
-        nodes = []
-        for v in nodes_by_depth:
-            if (len(v) > 1) or (len(v) == 1 and len(v[0].inbound_layers) > 1):
-                # if the model has multiple nodes
-                # or if the nodes have multiple inbound_layers
-                # the model is no longer sequential
-                sequential_like = False
-                break
-            nodes += v
-        if sequential_like:
-            # search for shared layers
-            for layer in model.layers:
-                flag = False
-                for node in layer._inbound_nodes:
-                    if node in nodes:
-                        if flag:
-                            sequential_like = False
-                            break
-                        else:
-                            flag = True
-                if not sequential_like:
-                    break
-    
-    
-def _normalize_device_name(name):
-    name = '/' + ':'.join(name.lower().replace('/', '').split(':')[-2:])
-    return name
-    
-            model.fit_generator(generate_arrays_from_file('/my_file.txt'),
-                            steps_per_epoch=10000, epochs=10)
-        ```
-        '''
-        return training_generator.fit_generator(
-            self, generator,
-            steps_per_epoch=steps_per_epoch,
-            epochs=epochs,
-            verbose=verbose,
-            callbacks=callbacks,
-            validation_data=validation_data,
-            validation_steps=validation_steps,
-            class_weight=class_weight,
-            max_queue_size=max_queue_size,
-            workers=workers,
-            use_multiprocessing=use_multiprocessing,
-            shuffle=shuffle,
-            initial_epoch=initial_epoch)
-    
-            self._layers.pop()
-        self.built = False
-        if not self.layers:
-            self.outputs = None
-            self.inputs = None
-        elif self.outputs:
-            self.layers[-1]._outbound_nodes = []
-            self.outputs = [self.layers[-1].output]
-            self.build()
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-      _assert_rejects( f, 'This is a Taco' )
-  _assert_accepts( f, 'This is a Burrito' )
+        versions_info['signature'] = signature
+with open('update/versions.json', 'w') as versionsf:
+    json.dump(versions_info, versionsf, indent=4, sort_keys=True)
 
     
+    **youtube-dl** \[OPTIONS\] URL [URL...]
     
-def EndsWithPython_Python2Paths_test():
-  python_paths = [
-    'python',
-    'python2',
-    '/usr/bin/python2.7',
-    '/home/user/.pyenv/shims/python2.7',
-    r'C:\Python27\python.exe',
-    '/Contents/MacOS/Python'
-  ]
+    # Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+]
     
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
-    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-    vol.Optional(CONF_DISCOVERY, default=True): cv.boolean,
-    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
-    vol.Optional(CONF_IGNORED_CLICK_TYPES):
-        vol.All(cv.ensure_list, [vol.In(CLICK_TYPES)])
-})
+        def test_cbc_decrypt(self):
+        data = bytes_to_intlist(
+            b'\x97\x92+\xe5\x0b\xc3\x18\x91ky9m&\xb3\xb5@\xe6'\xc2\x96.\xc8u\x88\xab9-[\x9e|\xf1\xcd'
+        )
+        decrypted = intlist_to_bytes(aes_cbc_decrypt(data, self.key, self.iv))
+        self.assertEqual(decrypted.rstrip(b'\x08'), self.secret_msg)
+    
+      # Long type names may be broken across multiple lines, usually in one
+  # of these forms:
+  #   LongType
+  #       ::LongTypeContinued &identifier
+  #   LongType::
+  #       LongTypeContinued &identifier
+  #   LongType<
+  #       ...>::LongTypeContinued &identifier
+  #
+  # If we detected a type split across two lines, join the previous
+  # line to current line so that we can match const references
+  # accordingly.
+  #
+  # Note that this only scans back one line, since scanning back
+  # arbitrary number of lines would be expensive.  If you have a type
+  # that spans more than 2 lines, please use a typedef.
+  if linenum > 1:
+    previous = None
+    if Match(r'\s*::(?:[\w<>]|::)+\s*&\s*\S', line):
+      # previous_line\n + ::current_line
+      previous = Search(r'\b((?:const\s*)?(?:[\w<>]|::)+[\w<>])\s*$',
+                        clean_lines.elided[linenum - 1])
+    elif Match(r'\s*[a-zA-Z_]([\w<>]|::)+\s*&\s*\S', line):
+      # previous_line::\n + current_line
+      previous = Search(r'\b((?:const\s*)?(?:[\w<>]|::)+::)\s*$',
+                        clean_lines.elided[linenum - 1])
+    if previous:
+      line = previous.group(1) + line.lstrip()
+    else:
+      # Check for templated parameter that is split across multiple lines
+      endpos = line.rfind('>')
+      if endpos > -1:
+        (_, startline, startpos) = ReverseCloseExpression(
+            clean_lines, linenum, endpos)
+        if startpos > -1 and startline < linenum:
+          # Found the matching < on an earlier line, collect all
+          # pieces up to current line.
+          line = ''
+          for i in xrange(startline, linenum + 1):
+            line += clean_lines.elided[i].strip()
+    
+        if not app.config.edit_on_github_project:
+        warnings.warn('edit_on_github_project not specified')
+        return
+    if not doctree:
+        warnings.warn('doctree is None')
+        return
+    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
+    show_url = get_github_url(app, 'blob', path)
+    edit_url = get_github_url(app, 'edit', path)
+    
+    import homeassistant.helpers.config_validation as cv
+import homeassistant.util.dt as dt_util
+from homeassistant.components.device_tracker import (
+    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+    
+                last_results.append(Device(mac.upper(), name))
+    
+    
+def get_scanner(hass, config):
+    '''Return the Swisscom device scanner.'''
+    scanner = SwisscomDeviceScanner(config[DOMAIN])
     
     _LOGGER = logging.getLogger(__name__)
     
-    from aiohttp.hdrs import CONTENT_TYPE
-import requests
-import voluptuous as vol
+            Return the list if successful.
+        '''
+        _LOGGER.info('Refreshing device list')
+        result = _retrieve_list(self.host, self.token)
+        if result:
+            return result
     
-            # Flag C stands for CONNECTED
-        active_clients = [client for client in data.values() if
-                          client['status'].find('C') != -1]
-        self.last_results = active_clients
-        return True
+        @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        '''Update the order state and refreshes the store.'''
+        from pizzapi.address import StoreException
+        try:
+            self.dominos.update_closest_store()
+        except StoreException:
+            self._orderable = False
+            return
     
-    _LOGGER = logging.getLogger(__name__)
+    DOMAIN = 'downloader'
+DOWNLOAD_FAILED_EVENT = 'download_failed'
+DOWNLOAD_COMPLETED_EVENT = 'download_completed'
     
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/dominos/.
-'''
-import logging
-from datetime import timedelta
+        entities = []
+    core = None
+    heating = None
+    hotwater = None
+    light = None
+    sensor = None
+    switch = None
+    weather = None
+    attributes = None
     
-                    else:
-                    if filename is None and \
-                       'content-disposition' in req.headers:
-                        match = re.findall(r'filename=(\S+)',
-                                           req.headers['content-disposition'])
+        def __call__(self, method):
+        '''Decorate a function.'''
+        @wraps(method)
+        async def wrapper(view, request, *args, **kwargs):
+            '''Wrap a request handler with data validation.'''
+            data = None
+            try:
+                data = await request.json()
+            except ValueError:
+                if not self._allow_empty or \
+                   (await request.content.read()) != b'':
+                    _LOGGER.error('Invalid JSON received.')
+                    return view.json_message('Invalid JSON.', 400)
+                data = {}
     
-    REQUIREMENTS = ['libpurecoollink==0.4.2']
     
+def main():
+    print('Making key files...')
+    makeKeyFiles('elgamal', 2048)
+    print('Key files generation successful')
     
-class Watcher():
-    '''Class for starting Watchdog.'''
+        def _set_value(self, key, data):
+        self.values[key] = data
+        self._keys[key] = data
+    
+                return self.dp[x][y]
+    
+        return res
+    
+        >>> isEnglish('llold HorWd')
+    False
+    '''
+    wordsMatch = getEnglishCount(message) * 100 >= wordPercentage
+    numLetters = len(removeNonLetters(message))
+    messageLettersPercentage = (float(numLetters) / len(message)) * 100
+    lettersMatch = messageLettersPercentage >= letterPercentage
+    return wordsMatch and lettersMatch
