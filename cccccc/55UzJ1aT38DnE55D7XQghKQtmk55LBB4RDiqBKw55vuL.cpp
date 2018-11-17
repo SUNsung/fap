5209dfb1d193c22245455fb1e216cda259596aaf
SUNsung <weatherius@gmail.com>
2018-11-17 07:39:23 +0200

    Rewrite [55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp] File in: 2018-11-17 07:39:23

--- cccccc/55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp
+++ cccccc/55UzJ1aT38DnE55D7XQghKQtmk55LBB4RDiqBKw55vuL.cpp
@@ -1,326 +1,230 @@
 
         
-            void operator() (const uint8x8_t & _v_src0, const uint8x8_t & _v_src1,
-                     uint8x8_t & v_dst) const
-    {
-        uint16x8_t v_src0 = vmovl_u8(_v_src0), v_src1 = vmovl_u8(_v_src1);
+        
+    {}  // namespace mate
+    
+    namespace api {
     }
     
-        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0,
-                     const typename internal::VecTraits<T>::vec64 & v_src1,
-                     typename internal::VecTraits<T>::vec64 & v_dst) const
-    {
-        v_dst = internal::vadd(v_src0, v_src1);
+    scoped_refptr<TracingController::TraceDataEndpoint> GetTraceDataEndpoint(
+    const base::FilePath& path,
+    const CompletionCallback& callback) {
+  base::FilePath result_file_path = path;
+  if (result_file_path.empty() && !base::CreateTemporaryFile(&result_file_path))
+    LOG(ERROR) << 'Creating temporary file failed';
     }
     
-            const f32* ln0 = idx_rm1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm1) : tmp;
-        const f32* ln1 = internal::getRowPtr(srcBase, srcStride, i);
-        const f32* ln2 = internal::getRowPtr(srcBase, srcStride, i + 1);
-        const f32* ln3 = idx_rp2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp2) : tmp;
+    void InAppPurchase::OnTransactionsUpdated(
+    const std::vector<in_app_purchase::Transaction>& transactions) {
+  Emit('transactions-updated', transactions);
+}
+#endif
     
-    s32 countNonZero(const Size2D &_size,
-                 const u16 * srcBase, ptrdiff_t srcStride)
+    // static
+void Net::BuildPrototype(v8::Isolate* isolate,
+                         v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'Net'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetProperty('URLRequest', &Net::URLRequest);
+}
+    
+    Screen::~Screen() {
+  screen_->RemoveObserver(this);
+}
+    
+    
+    {}  // namespace api
+    
+    #ifndef ATOM_BROWSER_API_EVENT_H_
+#define ATOM_BROWSER_API_EVENT_H_
+    
+    void SavePageHandler::Destroy(download::DownloadItem* item) {
+  item->RemoveObserver(this);
+  delete this;
+}
+    
+    
+    {  // We need to handle SIGTERM, because that is how many POSIX-based distros ask
+  // processes to quit gracefully at shutdown time.
+  struct sigaction action;
+  memset(&action, 0, sizeof(action));
+  action.sa_handler = SIGTERMHandler;
+  CHECK_EQ(sigaction(SIGTERM, &action, nullptr), 0);
+  // Also handle SIGINT - when the user terminates the browser via Ctrl+C. If
+  // the browser process is being debugged, GDB will catch the SIGINT first.
+  action.sa_handler = SIGINTHandler;
+  CHECK_EQ(sigaction(SIGINT, &action, nullptr), 0);
+  // And SIGHUP, for when the terminal disappears. On shutdown, many Linux
+  // distros send SIGHUP, SIGTERM, and then SIGKILL.
+  action.sa_handler = SIGHUPHandler;
+  CHECK_EQ(sigaction(SIGHUP, &action, nullptr), 0);
+}
+    
+    AtomQuotaPermissionContext::AtomQuotaPermissionContext() {}
+    
+    CC_CONSTRUCTOR_ACCESS:
+    Action();
+    virtual ~Action();
+    
+    void Waves3D::update(float time)
 {
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    Size2D size(_size);
-    if (srcStride == (ptrdiff_t)(size.width))
+    int i, j;
+    for (i = 0; i < _gridSize.width + 1; ++i)
     {
-        size.width *= size.height;
-        size.height = 1;
+        for (j = 0; j < _gridSize.height + 1; ++j)
+        {
+            Vec3 v = getOriginalVertex(Vec2(i ,j));
+            v.z += (sinf((float)M_PI * time * _waves * 2 + (v.y+v.x) * 0.01f) * _amplitude * _amplitudeRate);
+            //CCLOG('v.z offset is %f\n', (sinf((float)M_PI * time * _waves * 2 + (v.y+v.x) * .01f) * _amplitude * _amplitudeRate));
+            setVertex(Vec2(i, j), v);
+        }
     }
-    size_t roiw8 = size.width & ~7u;
-    s32 result = 0;
-    for(size_t k = 0; k < size.height; ++k)
-    {
-        const u16* src = internal::getRowPtr( srcBase,  srcStride, k);
-        size_t i = 0;
-    }
-    }
-    
-            size_t i = 0;
-        uint64x2_t ws = vmovq_n_u64(0);
-    
-        v0k0 = vextq_s16(d0_7, d8_15, 7);
-    ak0 = vminq_s16(ak0, v0k0);
-    bk0 = vmaxq_s16(bk0, v0k0);
-    
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const s32 * src = internal::getRowPtr(srcBase, srcStride, i);
-        size_t j = 0;
-    }
-    
-    INRANGEFUNC(u8)
-INRANGEFUNC(s8)
-INRANGEFUNC(u16)
-INRANGEFUNC(s16)
-INRANGEFUNC(s32)
-INRANGEFUNC(f32)
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38>
-internal::ValueArray38<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-    T29, T30, T31, T32, T33, T34, T35, T36, T37, T38> Values(T1 v1, T2 v2,
-    T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9, T10 v10, T11 v11, T12 v12,
-    T13 v13, T14 v14, T15 v15, T16 v16, T17 v17, T18 v18, T19 v19, T20 v20,
-    T21 v21, T22 v22, T23 v23, T24 v24, T25 v25, T26 v26, T27 v27, T28 v28,
-    T29 v29, T30 v30, T31 v31, T32 v32, T33 v33, T34 v34, T35 v35, T36 v36,
-    T37 v37, T38 v38) {
-  return internal::ValueArray38<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26, T27, T28, T29, T30, T31, T32, T33, T34, T35, T36, T37, T38>(v1, v2,
-      v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18,
-      v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32,
-      v33, v34, v35, v36, v37, v38);
 }
     
-    template <typename T1, typename T2, typename T3, typename T4, typename T5>
-void PrintTo(const ::std::tr1::tuple<T1, T2, T3, T4, T5>& t,
-             ::std::ostream* os) {
-  PrintTupleTo(t, os);
-}
+                // We scale z here to avoid the animation being
+            // too much bigger than the screen due to perspective transform
     
-    namespace internal {
+    #include '2d/CCActionGrid3D.h'
+    
+    bool SplitRows::initWithDuration(float duration, unsigned int rows)
+{
+    _rows = rows;
     }
     
-      // Formats a byte as '%02X'.
-  static std::string FormatByte(unsigned char value);
     
-    
-    {
-    {    return -1;
-  }
+    {  // Single-Delete diagnostics for exceptional situations
+  uint64_t num_single_del_fallthru = 0;
+  uint64_t num_single_del_mismatch = 0;
 };
-    
-    int Water::allocated_ = 0;
-    
-     public:
-  // Clones a 0-terminated C string, allocating memory using new.
-  static const char* CloneCString(const char* a_c_string);
-    
-      // Adds an element to the end of the queue.  A copy of the element is
-  // created using the copy constructor, and then stored in the queue.
-  // Changes made to the element in the queue doesn't affect the source
-  // object, and vice versa.
-  void Enqueue(const E& element) {
-    QueueNode<E>* new_node = new QueueNode<E>(element);
-    }
-    
-    			cmderOptions.cmderCfgRoot = cmderCfgRoot;
-    
-    
-    {} // namespace osquery
 
     
-    namespace osquery {
+    class DB;
+    
+    int main() {
+  // open DB
+  Options options;
+  options.create_if_missing = true;
+  DB* db;
+  Status s = DB::Open(options, kDBPath, &db);
+  assert(s.ok());
     }
     
-    void INotifyEventPublisher::removeSubscriptions(const std::string& subscriber) {
-  WriteLock lock(subscription_lock_);
-  std::for_each(subscriptions_.begin(),
-                subscriptions_.end(),
-                [&subscriber](const SubscriptionRef& sub) {
-                  if (sub->subscriber_name == subscriber) {
-                    getSubscriptionContext(sub->context)->mark_for_deletion =
-                        true;
-                  }
-                });
-}
-    
-      // Perform the parsing.
-  AuditEventRecord audit_event_record = {};
-  bool parser_status =
-      AuditdNetlinkParser::ParseAuditReply(reply, audit_event_record);
-  EXPECT_EQ(parser_status, true);
-    
-    int main(int argc, char* argv[]) {
-  osquery::Initializer runner(argc, argv, ToolType::EXTENSION);
-    }
-    
-    namespace rocksdb {
-    }
-    
-      uint64_t single_refill_amount =
-      delayed_write_rate_ * kRefillInterval / kMicrosPerSecond;
-  if (bytes_left_ + single_refill_amount >= num_bytes) {
-    // Wait until a refill interval
-    // Never trigger expire for less than one refill interval to avoid to get
-    // time.
-    bytes_left_ = bytes_left_ + single_refill_amount - num_bytes;
-    last_refill_time_ = time_now + kRefillInterval;
-    return kRefillInterval + sleep_debt;
+    class MyFilter : public rocksdb::CompactionFilter {
+ public:
+  bool Filter(int level, const rocksdb::Slice& key,
+              const rocksdb::Slice& existing_value, std::string* new_value,
+              bool* value_changed) const override {
+    fprintf(stderr, 'Filter(%s)\n', key.ToString().c_str());
+    ++count_;
+    assert(*value_changed == false);
+    return false;
   }
-    
-    
-    {  std::unique_ptr<RateLimiter> low_pri_rate_limiter_;
-};
-    
-      env.now_micros_ += 200u;  // sleep debt 624
-  // Out of bound sleep, still 10480 left
-  ASSERT_EQ(static_cast<uint64_t>(3000624u),
-            controller.GetDelay(&env, 30000000u));
-    
-      // Positioned Read for direct I/O
-  // If Direct I/O enabled, offset, n, and scratch should be properly aligned
-  virtual Status PositionedRead(uint64_t offset, size_t n, Slice* result, char* scratch) override {
-    assert(scratch);
-    offset += prefixLength_; // Skip prefix
-    auto status = file_->PositionedRead(offset, n, result, scratch);
-    if (!status.ok()) {
-      return status;
     }
-    offset_ = offset + result->size();
-    status = stream_->Decrypt(offset, (char*)result->data(), result->size());
-    return status;
-  }
     
     
-    {  char* rid = id;
-  rid = EncodeVarint64(rid, buf.st_dev);
-  rid = EncodeVarint64(rid, buf.st_ino);
-  rid = EncodeVarint64(rid, buf.st_gen);
-  assert(rid >= id);
-  return static_cast<size_t>(rid - id);
-}
-#endif
-/*
- * PosixRandomAccessFile
- *
- * pread() based random-access
+    {// Factor method to create a new persistent cache
+Status NewPersistentCache(Env* const env, const std::string& path,
+                          const uint64_t size,
+                          const std::shared_ptr<Logger>& log,
+                          const bool optimized_for_nvm,
+                          std::shared_ptr<PersistentCache>* cache);
+}  // namespace rocksdb
+
+    
+      /**
+   * @brief Send messages
+   * @param frames The messages to send.
+   * @param frame_num The amount of messages to send.
+   * @return The status of the sending action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  virtual apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                         int32_t *const frame_num) = 0;
+    
+    TEST(SocketCanClientRawTest, simple_test) {
+  CANCardParameter param;
+  param.set_brand(CANCardParameter::SOCKET_CAN_RAW);
+  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
+    }
+    
+      /**
+   * @brief Transform an integer with the size of one byte to its binary
+   *        represented by a string.
+   * @param value The target integer to transform.
+   * @return Binary representing the target integer.
+   */
+  static std::string byte_to_binary(const uint8_t value);
+    
+    /**
+ * @file
  */
-PosixRandomAccessFile::PosixRandomAccessFile(const std::string& fname, int fd,
-                                             const EnvOptions& options)
-    : filename_(fname),
-      fd_(fd),
-      use_direct_io_(options.use_direct_reads),
-      logical_sector_size_(GetLogicalBufferSize(fd_)) {
-  assert(!options.use_direct_reads || !options.use_mmap_reads);
-  assert(!options.use_mmap_reads || sizeof(void*) < 8);
-}
     
-    class PosixMmapFile : public WritableFile {
- private:
-  std::string filename_;
-  int fd_;
-  size_t page_size_;
-  size_t map_size_;       // How much extra memory to map at a time
-  char* base_;            // The mapped region
-  char* limit_;           // Limit of the mapped region
-  char* dst_;             // Where to write next  (in range [base_,limit_])
-  char* last_sync_;       // Where have we synced up to
-  uint64_t file_offset_;  // Offset of base_ in file
-#ifdef ROCKSDB_FALLOCATE_PRESENT
-  bool allow_fallocate_;  // If false, fallocate calls are bypassed
-  bool fallocate_with_keep_size_;
-#endif
-    }
-    
-      virtual Status GetTestDirectory(std::string* path) override;
-    
-    TEST_F(MockEnvTest, Corrupt) {
-  const std::string kGood = 'this is a good string, synced to disk';
-  const std::string kCorrupted = 'this part may be corrupted';
-  const std::string kFileName = '/dir/f';
-  unique_ptr<WritableFile> writable_file;
-  ASSERT_OK(env_->NewWritableFile(kFileName, &writable_file, soptions_));
-  ASSERT_OK(writable_file->Append(kGood));
-  ASSERT_TRUE(writable_file->GetFileSize() == kGood.size());
-    }
-    
-      bool FilterMergeOperand(int level, const rocksdb::Slice& key,
-                          const rocksdb::Slice& existing_value) const override {
-    fprintf(stderr, 'FilterMerge(%s)\n', key.ToString().c_str());
-    ++merge_count_;
-    return existing_value == 'bad';
+      if (can_receiver_.Init(can_client_.get(), sensor_message_manager_.get(),
+                         canbus_conf_.enable_receiver_log()) != ErrorCode::OK) {
+    return OnError('Failed to init can receiver.');
   }
+  AINFO << 'The can receiver is successfully initialized.';
     
-      // Initialize pointer options for each column family
-  for (size_t i = 0; i < loaded_cf_descs.size(); ++i) {
-    auto* loaded_bbt_opt = reinterpret_cast<BlockBasedTableOptions*>(
-        loaded_cf_descs[0].options.table_factory->GetOptions());
-    // Expect the same as BlockBasedTableOptions will be loaded form file.
-    assert(loaded_bbt_opt->block_size == bbt_opts.block_size);
-    // However, block_cache needs to be manually initialized as documented
-    // in rocksdb/utilities/options_util.h.
-    loaded_bbt_opt->block_cache = cache;
-  }
-  // In addition, as pointer options are initialized with default value,
-  // we need to properly initialized all the pointer options if non-defalut
-  // values are used before calling DB::Open().
-  assert(loaded_cf_descs[0].options.compaction_filter == nullptr);
-  loaded_cf_descs[0].options.compaction_filter = compaction_filter.get();
+    DEFINE_string(adapter_config_filename, 'modules/canbus/conf/adapter.conf',
+              'The adapter config file');
     
-      s = db->Get(ReadOptions(), 'key1', &value);
-  assert(s.IsNotFound());
+      t6 = t1 - t4;
+  t8 = kSqrtHalf;
+  t6 *= t8;
+  a[5].real = a[4].real - t6;
+  t1 += t4;
+  t1 *= t8;
+  a[5].imag = a[4].imag - t1;
+  t6 += a[4].real;
+  a[4].real = t6;
+  t1 += a[4].imag;
+  a[4].imag = t1;
+    
+      // Compares the 8x8 block with offsets (off_x, off_y) within the current
+  // macro-block of the baseline image with the same block of img and returns
+  // the resulting per-block distance. The interpretation of the returned
+  // distance depends on the comparator used.
+  virtual double CompareBlock(const OutputImage& img,
+                              int off_x, int off_y) const = 0;
+    
+    // Performs in-place floating point 8x8 inverse DCT on block[0..63].
+void ComputeBlockIDCTDouble(double block[64]);
     
     
     {}  // namespace guetzli
-    
-    void PrintDebug(ProcessStats* stats, std::string s);
-    
-    
-// these are the macro required by COLUMN_*
-#define LOAD_CST(dst, src) (dst) = (src)
-#define LOAD(dst, src) (dst) = (src)
-#define MULT(a, b)  (a) = (((a) * (b)) >> 16)
-#define ADD(a, b)   (a) = (a) + (b)
-#define SUB(a, b)   (a) = (a) - (b)
-#define LSHIFT(a, n) (a) = ((a) << (n))
-#define STORE16(a, b) (a) = (b)
-#define CORRECT_LSB(a) (a) += 1
-    
-    #endif  // GUETZLI_IDCT_H_
 
     
-    static const uint8_t kDefaultQuantMatrix[2][64] = {
-  { 16,  11,  10,  16,  24,  40,  51,  61,
-    12,  12,  14,  19,  26,  58,  60,  55,
-    14,  13,  16,  24,  40,  57,  69,  56,
-    14,  17,  22,  29,  51,  87,  80,  62,
-    18,  22,  37,  56,  68, 109, 103,  77,
-    24,  35,  55,  64,  81, 104, 113,  92,
-    49,  64,  78,  87, 103, 121, 120, 101,
-    72,  92,  95,  98, 112, 100, 103,  99 },
-  { 17,  18,  24,  47,  99,  99,  99,  99,
-    18,  21,  26,  66,  99,  99,  99,  99,
-    24,  26,  56,  99,  99,  99,  99,  99,
-    47,  66,  99,  99,  99,  99,  99,  99,
-    99,  99,  99,  99,  99,  99,  99,  99,
-    99,  99,  99,  99,  99,  99,  99,  99,
-    99,  99,  99,  99,  99,  99,  99,  99,
-    99,  99,  99,  99,  99,  99,  99,  99 }
-};
+    #include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <exception>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <string.h>
+#include 'png.h'
+#include 'guetzli/jpeg_data.h'
+#include 'guetzli/jpeg_data_reader.h'
+#include 'guetzli/processor.h'
+#include 'guetzli/quality.h'
+#include 'guetzli/stats.h'
     
-    std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg) {
-  if (jpg.components.size() == 1 ||
-      (jpg.components.size() == 3 &&
-       HasYCbCrColorSpace(jpg) && (jpg.Is420() || jpg.Is444()))) {
-    OutputImage img(jpg.width, jpg.height);
-    img.CopyFromJpegData(jpg);
-    return img.ToSRGB();
+    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
+// quantization table must have 3 * kDCTBlockSize values.
+bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
+                     const int* quant, JPEGData* jpg);
+    
+    void BuildHuffmanCodeTable(const int* counts, const int* values,
+                           HuffmanCodeTable* table) {
+  int huffcode[256];
+  int huffsize[256];
+  int p = 0;
+  for (int l = 1; l <= kJpegHuffmanMaxBitLength; ++l) {
+    int i = counts[l];
+    while (i--) huffsize[p++] = l;
   }
-  return std::vector<uint8_t>();
-}
-    
-        static BOOST_FORCEINLINE bool compare_exchange_weak(
-        storage_type volatile& storage, storage_type& expected, storage_type desired, memory_order success_order, memory_order failure_order) BOOST_NOEXCEPT
-    {
-        return compare_exchange_strong(storage, expected, desired, success_order, failure_order);
     }
     
-    namespace mars_boost {} namespace boost = mars_boost; namespace mars_boost {
-namespace atomics {
-namespace detail {
-    }
-    }
-    }
     
-    #endif // BOOST_ATOMIC_FENCES_HPP_INCLUDED_
+    {  uint32_t counts[kSize];
+};
