7d6c981c90bc85e2e5fb6a38f5b2de27c7c5c273
SUNsung <weatherius@gmail.com>
2018-11-17 11:51:32 +0200

    Rewrite [34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb] File in: 2018-11-17 11:51:31

--- ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
+++ ruby/34q5EQHjdJ1634jOjKIrSZz.U34XGgAFQy0bLc348IA.rb
@@ -1,157 +1,117 @@
 
         
-                format('%2$e', 0, 109.52).should == '1.095200e+02'
-        format('%2$E', 0, 109.52).should == '1.095200E+02'
-        format('%2$f', 0, 10.952).should == '10.952000'
-        format('%2$g', 0, 12.1234).should == '12.1234'
-        format('%2$G', 0, 12.1234).should == '12.1234'
-        format('%2$a', 0, 196).should == '0x1.88p+7'
-        format('%2$A', 0, 196).should == '0X1.88P+7'
+                # This is called to upgrade this V1 config to V2. The parameter given
+        # is the full V2 configuration object, so you can do anything to it
+        # that you want.
+        #
+        # No return value is expected, modifications should be made directly
+        # to the new V2 object.
+        #
+        # @param [V2::Root] new
+        def upgrade(new)
+        end
     
-    describe 'Kernel.system' do
-  it_behaves_like :kernel_system, :system, Kernel
-end
-
+            # This returns all registered host classes.
+        #
+        # @return [Hash]
+        def hosts
+          hosts = {}
     
-    describe 'Kernel.throw' do
-  it 'transfers control to the end of the active catch block waiting for symbol' do
-    catch(:blah) do
-      :value
-      throw :blah
-      fail('throw didn't transfer the control')
-    end.should be_nil
+              # Setup the options hash
+          options ||= {}
+    
+          spec['main'] =
+          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
+          find_files.(Bootstrap.fonts_path) +
+          %w(assets/javascripts/bootstrap.js)
+    
+        process_font_assets
+    process_stylesheet_assets
+    process_javascript_assets
+    store_version
   end
     
-        $Kernel_trace_var_global = 'foo'
     
-      ruby_version_is '2.5' do
-    describe ':uplevel keyword argument' do
-      before :each do
-        $VERBOSE = true
-      end
+    {    # unwraps topmost rule block
+    # #sel { a: b; }
+    # to:
+    # a: b;
+    def unwrap_rule_block(css)
+      css[(css =~ RULE_OPEN_BRACE_RE) + 1..-1].sub(/\n?}\s*\z/m, '')
+    end
     
-          File.open('bower.json', 'w') do |f|
-        f.puts JSON.pretty_generate(spec)
+        def log_status(status)
+      puts bold status
+    end
+    
+      # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both thread web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+    
+                  <% if frame.post_context %>
+              <ol start='<%=h frame.lineno + 1 %>' class='post-context'
+                  id='post-<%= id %>' onclick='toggle(<%= id %>);'>
+                <% frame.post_context.each do |line| %>
+                <li class='post-context-line'><code><%=h line %></code></li>
+                <% end %>
+              </ol>
+              <% end %>
+              <div class='clear'></div>
+            </li>
+    
+          def csp_policy
+        directives = []
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   XSS
+    # Supported browsers:: all
+    # More infos::         http://en.wikipedia.org/wiki/Cross-site_scripting
+    #
+    # Automatically escapes Rack::Request#params so they can be embedded in HTML
+    # or JavaScript without any further issues. Calls +html_safe+ on the escaped
+    # strings if defined, to avoid double-escaping in Rails.
+    #
+    # Options:
+    # escape:: What escaping modes to use, should be Symbol or Array of Symbols.
+    #          Available: :html (default), :javascript, :url
+    class EscapedParams < Base
+      extend Rack::Utils
+    
+          def react_and_close(env, body)
+        reaction = react(env)
+    
+            def scope
+          if params[:product_id]
+            Spree::Product.friendly.find(params[:product_id])
+          elsif params[:variant_id]
+            Spree::Variant.find(params[:variant_id])
+          end
+        end
       end
     end
   end
 end
 
     
-      # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+              @stock_item = scope.new(stock_item_params)
+          if @stock_item.save
+            @stock_item.adjust_count_on_hand(count_on_hand)
+            respond_with(@stock_item, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_item)
+          end
+        end
     
-          # Find a Sass file, if it exists.
-      #
-      # This is the primary entry point of the Importer.
-      # It corresponds directly to an `@import` statement in Sass.
-      # It should do three basic things:
-      #
-      # * Determine if the URI is in this importer's format.
-      #   If not, return nil.
-      # * Determine if the file indicated by the URI actually exists and is readable.
-      #   If not, return nil.
-      # * Read the file and place the contents in a {Sass::Engine}.
-      #   Return that engine.
-      #
-      # If this importer's format allows for file extensions,
-      # it should treat them the same way as the default {Filesystem} importer.
-      # If the URI explicitly has a `.sass` or `.scss` filename,
-      # the importer should look for that exact file
-      # and import it as the syntax indicated.
-      # If it doesn't exist, the importer should return nil.
-      #
-      # If the URI doesn't have either of these extensions,
-      # the importer should look for files with the extensions.
-      # If no such files exist, it should return nil.
-      #
-      # The {Sass::Engine} to be returned should be passed `options`,
-      # with a few modifications. `:syntax` should be set appropriately,
-      # `:filename` should be set to `uri`,
-      # and `:importer` should be set to this importer.
-      #
-      # @param uri [String] The URI to import.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` that's currently being resolved.
-      #   This is safe for subclasses to modify destructively.
-      #   Callers should only pass in a value they don't mind being destructively modified.
-      # @return [Sass::Engine, nil] An Engine containing the imported file,
-      #   or nil if it couldn't be found or was in the wrong format.
-      def find(uri, options)
-        Sass::Util.abstract(self)
-      end
-    
-        desc 'Release all gems as packages'
-    task :all => [:test, :commit_version] + GEMS_AND_ROOT_DIRECTORIES.keys
-  end
-end
-
-    
-        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      subject.options[:allow_empty_referrer] = false
-      expect(subject.referrer(env)).to be_nil
-    end
-    
-    ::Bundler.with_friendly_errors do
-  ::Bundler::CLI.start(ARGV, :debug => true)
-end
-
-    
-    require 'clamp'
-require 'pluginmanager/util'
-require 'pluginmanager/gemfile'
-require 'pluginmanager/install'
-require 'pluginmanager/remove'
-require 'pluginmanager/list'
-require 'pluginmanager/update'
-require 'pluginmanager/pack'
-require 'pluginmanager/unpack'
-require 'pluginmanager/generate'
-require 'pluginmanager/prepare_offline_pack'
-require 'pluginmanager/proxy_support'
-configure_proxy
-    
-        FileUtils.rm_rf(LogStash::Environment::CACHE_PATH)
-    validate_cache_location
-    archive_manager.extract(package_file, LogStash::Environment::CACHE_PATH)
-    puts('Unpacked at #{LogStash::Environment::CACHE_PATH}')
-    puts('The unpacked plugins can now be installed in local-only mode using bin/logstash-plugin install --local [plugin name]')
-  end
-    
-    namespace :qa do
-    
-        context 'update a specific plugin' do
-      it 'has executed successfully' do
-        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
-        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
-        expect(logstash).not_to have_installed?(plugin_name, previous_version)
-      end
-    end
-    
-      include_examples 'multiline literal brace layout' do
-    let(:open) { '[' }
-    let(:close) { ']' }
-  end
-    
-          expect(new_source)
-        .to eq(construct(false, a, make_multi(safe_heredoc), false))
-    end
-  end
-    
-    module RuboCop
-  module AST
-    # A node extension for `def` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `def` nodes within RuboCop.
-    class DefNode < Node
-      include ParameterizedNode
-      include MethodIdentifierPredicates
-    
-          # Checks whether the `hash` node contains any `pair`- or `kwsplat` nodes.
-      #
-      # @return[Boolean] whether the `hash` is empty
-      def empty?
-        children.empty?
-      end
+            def create
+          authorize! :create, StockLocation
+          @stock_location = StockLocation.new(stock_location_params)
+          if @stock_location.save
+            respond_with(@stock_location, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_location)
+          end
+        end
