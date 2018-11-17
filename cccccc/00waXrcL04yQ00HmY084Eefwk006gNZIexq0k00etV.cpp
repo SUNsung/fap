8739f6beeff70167229e4fc2e8e33dfbab68f9b3
SUNsung <weatherius@gmail.com>
2018-11-17 15:25:53 +0200

    Rewrite [00waXrcL04yQ00HmY084Eefwk006gNZIexq0k00etV.cpp] File in: 2018-11-17 15:25:52

--- cccccc/00waXrcL04yQ00HmY084Eefwk006gNZIexq0k00etV.cpp
+++ cccccc/00waXrcL04yQ00HmY084Eefwk006gNZIexq0k00etV.cpp
@@ -1,172 +1,155 @@
 
         
-            void operator() (const typename internal::VecTraits<T>::vec128 & v_src0,
-                     const typename internal::VecTraits<T>::vec128 & v_src1,
-                     typename internal::VecTraits<T>::vec128 & v_dst) const
-    {
-        typename internal::VecTraits<T>::vec128 v_min = internal::vminq(v_src0, v_src1);
-        typename internal::VecTraits<T>::vec128 v_max = internal::vmaxq(v_src0, v_src1);
-        v_dst = internal::vqsubq(v_max, v_min);
-    }
-    
-                uint8x16x2_t v_y = vld2q_u8(srcy + syj);
-            uint8x16x4_t v_dst;
-            v_dst.val[0] = v_y.val[0];
-            v_dst.val[1] = vld1q_u8(srcu + sj);
-            v_dst.val[2] = v_y.val[1];
-            v_dst.val[3] = vld1q_u8(srcv + sj);
-            vst4q_u8(dst + dj, v_dst);
-    
-        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0, const typename internal::VecTraits<T>::vec64 & v_src1,
-              typename internal::VecTraits<T>::unsign::vec64 & v_dst) const
-    {
-        v_dst = internal::vcgt(v_src0, v_src1);
-    }
-    
-    typedef void (* rshiftConstFunc)(const Size2D &size,
-                                const s16 * srcBase, ptrdiff_t srcStride,
-                                u8 * dstBase, ptrdiff_t dstStride,
-                                CONVERT_POLICY cpolicy);
-    
-                    if (border == BORDER_MODE_CONSTANT && x4 < 0)
-                    prevx[0] = prevx[1] = prevx[2] = borderValue;
-                else
-                {
-                    prevx[0] = srow0 ? srow0[x4] : borderValue;
-                    prevx[1] =         srow1[x4]              ;
-                    prevx[2] = srow2 ? srow2[x4] : borderValue;
-                }
-    
-    #include <vector>
-#include <cstring>
-    
-    #ifdef CAROTENE_NEON
-    
-                v_dst.val[0] = vcombine(vget_high(v_src.val[0]), vget_low(v_src.val[0]));
-            v_dst.val[1] = vcombine(vget_high(v_src.val[1]), vget_low(v_src.val[1]));
-            v_dst.val[2] = vcombine(vget_high(v_src.val[2]), vget_low(v_src.val[2]));
-    
-        for(size_t j = 0; j < size.height; ++j)
-    {
-        const T *  src = internal::getRowPtr( srcBase,  srcStride, j);
-        const T * rng1 = internal::getRowPtr(rng1Base, rng1Stride, j);
-        const T * rng2 = internal::getRowPtr(rng2Base, rng2Stride, j);
-             u8 *  dst = internal::getRowPtr( dstBase,  dstStride, j);
-        size_t i = 0;
-        for( ; i < width; i += 32/sizeof(T) )
+                for (; j < roiw16; j += 16)
         {
-            internal::prefetch(src + i);
-            internal::prefetch(rng1 + i);
-            internal::prefetch(rng2 + i);
+            internal::prefetch(src + j);
+            internal::prefetch(dst + j);
+            uint8x16_t v_src = vld1q_u8(src + j);
+            int16x8_t v_dst0 = vld1q_s16(dst + j);
+            int16x8_t v_dst1 = vld1q_s16(dst + j + 8);
+            int16x8_t v_src0 = vreinterpretq_s16_u16(vmovl_u8(vget_low_u8(v_src)));
+            int16x8_t v_src1 = vreinterpretq_s16_u16(vmovl_u8(vget_high_u8(v_src)));
+            v_dst0 = vqaddq_s16(v_dst0, v_src0);
+            v_dst1 = vqaddq_s16(v_dst1, v_src1);
+            vst1q_s16(dst + j, v_dst0);
+            vst1q_s16(dst + j + 8, v_dst1);
+        }
+        for (; j < roiw8; j += 8)
+        {
+            uint8x8_t v_src = vld1_u8(src + j);
+            int16x8_t v_src16 = vreinterpretq_s16_u16(vmovl_u8(v_src));
+            int16x8_t v_dst = vld1q_s16(dst + j);
+            v_dst = vqaddq_s16(v_dst, v_src16);
+            vst1q_s16(dst + j, v_dst);
+        }
+    
+    
+    {    void operator() (const T * src0, const T * src1, T * dst) const
+    {
+        dst[0] = saturate_cast<T>(alpha*src0[0] + beta*src1[0] + gamma);
     }
-    }
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    
-    {  if (base64) {
-    decoded = string_base64_decode(data, data_len, true);
-    if (decoded.isNull()) {
-      raise_warning('unable to decode base64 data');
-      return nullptr;
-    }
-  } else {
-    decoded = url_decode(data, data_len);
-  }
-  return req::make<MemFile>(decoded.data(), decoded.size());
-}
-    
-    struct ExecutionProfiler {
-  ExecutionProfiler(ThreadInfo *info, bool builtin) : m_info(info) {
-    m_executing = m_info->m_executing;
-    m_info->m_executing =
-      builtin ? ThreadInfo::ExtensionFunctions : ThreadInfo::UserFunctions;
-  }
-  explicit ExecutionProfiler(ThreadInfo::Executing executing) {
-    m_info = ThreadInfo::s_threadInfo.getNoCheck();
-    m_executing = m_info->m_executing;
-    m_info->m_executing = executing;
-  }
-  ~ExecutionProfiler() {
-    m_info->m_executing = m_executing;
-  }
-private:
-  ThreadInfo *m_info;
-  ThreadInfo::Executing m_executing;
 };
     
-    #endif // HPHP_FILE_STREAM_WRAPPER_H
-
     
-      // Given a sequence number, return the sequence number of the
-  // earliest snapshot that this sequence number is visible in.
-  // The snapshots themselves are arranged in ascending order of
-  // sequence numbers.
-  // Employ a sequential search because the total number of
-  // snapshots are typically small.
-  inline SequenceNumber findEarliestVisibleSnapshot(
-      SequenceNumber in, SequenceNumber* prev_snapshot);
+    {
+    {        for (; dj < size.width; sj += 2, ++dj)
+        {
+            dst[dj] = src[sj + coi];
+        }
+    }
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)coi;
+#endif
+}
     
-    TEST_F(DBTestDynamicLevel, DynamicLevelMaxBytesBaseInc) {
-  Options options = CurrentOptions();
-  options.create_if_missing = true;
-  options.write_buffer_size = 2048;
-  options.max_write_buffer_number = 2;
-  options.level0_file_num_compaction_trigger = 2;
-  options.level0_slowdown_writes_trigger = 2;
-  options.level0_stop_writes_trigger = 2;
-  options.target_file_size_base = 2048;
-  options.level_compaction_dynamic_level_bytes = true;
-  options.max_bytes_for_level_base = 10240;
-  options.max_bytes_for_level_multiplier = 4;
-  options.soft_rate_limit = 1.1;
-  options.max_background_compactions = 2;
-  options.num_levels = 5;
-  options.max_compaction_bytes = 100000000;
+                    uint16x8_t vSum_3_1 = vaddq_u16(vLane3.val[0], vLane1.val[0]);
+                uint16x8_t vSum_4_2 = vaddq_u16(vLane3.val[1], vLane1.val[1]);
+                uint16x8_t vSum_5_6 = vaddq_u16(vLane3.val[2], vLane1.val[2]);
+    
+                uint32x2_t el2l = vadd_u32(vget_low_u32(el8shr01l), vget_high_u32(el8shr01l));
+            uint32x2_t el2hl = vadd_u32(vget_low_u32(el4h), vget_high_u32(el8shr01l));
+            uint32x2_t el2hh = vadd_u32(vget_low_u32(el4h), vget_high_u32(el4h));
+    
+        std::vector<u8> _tmp;
+    u8 *tmp = 0;
+    if (border == BORDER_MODE_CONSTANT)
+    {
+        _tmp.assign(cols + 4,borderValue);
+        tmp = &_tmp[2];
     }
     
-    #pragma once
+              vector<TensorShape> out(1);
+          switch (order) {
+            case StorageOrder::NCHW:
+              out[0] = CreateTensorShape(
+                  vector<int>{N, C * kernel_h * kernel_w, out_h, out_w},
+                  TensorProto::FLOAT);
+              break;
+            case StorageOrder::NHWC:
+              out[0] = CreateTensorShape(
+                  vector<int>{N, out_h, out_w, kernel_h * kernel_w * C},
+                  TensorProto::FLOAT);
+              break;
+            default:
+              CAFFE_THROW('Unknown storage order: ', order);
+          }
     
-      env.now_micros_ += 1999900u;  // sleep debt 1000
+    std::unique_ptr<php::Unit> parse_unit(php::Program& prog,
+                                      std::unique_ptr<UnitEmitter> ue);
     
-    // EncryptedEnv implements an Env wrapper that adds encryption to files stored on disk.
-class EncryptedEnv : public EnvWrapper {
- public:
-  EncryptedEnv(Env* base_env, EncryptionProvider *provider)
-      : EnvWrapper(base_env) {
-    provider_ = provider;
-  }
+    APCCollection::~APCCollection() {
+  // Zero for size is correct, because when this APCCollection was unreferenced
+  // it already included the size of the inner handle.
+  m_arrayHandle->unreferenceRoot(0);
+}
+    
+    #define CONFIG_BODY(T, METHOD) \
+T Config::Get##METHOD(const IniSetting::Map &ini, const Hdf& config, \
+                      const std::string &name /* = '' */, \
+                      const T defValue /* = 0ish */, \
+                      const bool prepend_hhvm /* = true */) { \
+  auto ini_name = IniName(name, prepend_hhvm); \
+  /* If we don't pass a name, then we just use the raw config as-is. */ \
+  /* This could happen when we are at a known leaf of a config node. */ \
+  Hdf hdf = name != '' ? config[name] : config; \
+  auto value = ini_iterate(ini, ini_name); \
+  if (value.isString()) { \
+    T ini_ret, hdf_ret; \
+    ini_on_update(value.toString(), ini_ret); \
+    /* I don't care what the ini_ret was if it isn't equal to what  */ \
+    /* is returned back from from an HDF get call, which it will be */ \
+    /* if the call just passes back ini_ret because either they are */ \
+    /* the same or the hdf option associated with this name does    */ \
+    /* not exist.... REMEMBER HDF WINS OVER INI UNTIL WE WIPE HDF   */ \
+    hdf_ret = hdf.configGet##METHOD(ini_ret); \
+    if (hdf_ret != ini_ret) { \
+      ini_ret = hdf_ret; \
+      IniSetting::SetSystem(ini_name, variant_init(ini_ret)); \
+    } \
+    return ini_ret; \
+  } \
+  /* If there is a value associated with this setting in the hdf config */ \
+  /* then return it; otherwise the defValue will be returned as it is   */ \
+  /* assigned to the return value for this call when nothing exists     */ \
+  return hdf.configGet##METHOD(defValue); \
+} \
+void Config::Bind(T& loc, const IniSetting::Map &ini, const Hdf& config, \
+                  const std::string& name /* = '' */, \
+                  const T defValue /* = 0ish */, \
+                  const bool prepend_hhvm /* = true */) { \
+  loc = Get##METHOD(ini, config, name, defValue, prepend_hhvm); \
+  IniSetting::Bind(IniSetting::CORE, IniSetting::PHP_INI_SYSTEM, \
+                   IniName(name, prepend_hhvm), &loc); \
+}
+    
+    
+    {  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+};
+    
+    #include <string>
+#include <utility>
+#include <vector>
+    
+    IMPLEMENT_RESOURCE_ALLOCATION(PlainDirectory)
+    
+    #ifndef HPHP_FILE_STREAM_WRAPPER_H
+#define HPHP_FILE_STREAM_WRAPPER_H
+    
+    template <typename F>
+void find(const std::string &root, const std::string& path, bool php,
+          const F& callback) {
+  auto spath = path.empty() || !isDirSeparator(path[0]) ?
+    path : path.substr(1);
     }
     
-      // Results of these can be affected by FakeSleepForMicroseconds()
-  virtual Status GetCurrentTime(int64_t* unix_time) override;
-  virtual uint64_t NowMicros() override;
-  virtual uint64_t NowNanos() override;
-    
-    #include 'env/mock_env.h'
-    
-      DB* db = nullptr;
-  DestroyDB(kDBPath, options);
-  Status s = DB::Open(options, kDBPath, &db);
-  assert(s.ok());
-  assert(db);
-    
-      int ret = system('rm -rf /tmp/rocksmergetest');
-  if (ret != 0) {
-    fprintf(stderr, 'Error deleting /tmp/rocksmergetest, code: %d\n', ret);
-    return ret;
-  }
-  rocksdb::Options options;
-  options.create_if_missing = true;
-  options.merge_operator.reset(new MyMerge);
-  options.compaction_filter = &filter;
-  status = rocksdb::DB::Open(options, '/tmp/rocksmergetest', &raw_db);
-  assert(status.ok());
-  std::unique_ptr<rocksdb::DB> db(raw_db);
-    
-      ////////////////////////////////////////////////////////
-  //
-  // 'Repeatable Read' (Snapshot Isolation) Example
-  //   -- Using a single Snapshot
-  //
-  ////////////////////////////////////////////////////////
+    req::ptr<Directory> GlobStreamWrapper::opendir(const String& path) {
+  const char* prefix = 'glob://';
+  const char* path_str = path.data();
+  int path_len = path.length();
+    }
