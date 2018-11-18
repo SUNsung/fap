62c4e2564304a537f4a9bbcc9c7a77c3660432f6
SUNsung <weatherius@gmail.com>
2018-11-18 20:26:33 +0200

    Rewrite [563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp] File in: 2018-11-18 20:26:33

--- cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
+++ cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
@@ -1,309 +1,169 @@
 
         
-        
-    {  args_.push_back(arg);
-}
+        /// Determine whether the given text matches a property name.
+static bool textMatchesPropertyName(StringRef text,
+                                    const InheritedNameSet *allPropertyNames) {
+  if (!allPropertyNames) return false;
+    }
     
     
-    {  if (op->send_message() != nullptr) {
-    ++sent_message_count_;
+    {    if (Finished) {
+      std::string Output = 'Output placeholder\n';
+      std::string Errors =
+          P.second->SeparateErrors ? 'Error placeholder\n' : '';
+      if (Finished(P.first, 0, Output, Errors, TaskProcessInformation(Pid),
+                   P.second->Context) == TaskFinishedResponse::StopExecution)
+        SubtaskFailed = true;
+    }
   }
-  if (op->recv_message() != nullptr) {
-    recv_message_ = op->op()->payload->recv_message.recv_message;
-    initial_on_done_recv_message_ =
-        op->op()->payload->recv_message.recv_message_ready;
-    op->op()->payload->recv_message.recv_message_ready = &on_done_recv_message_;
+    
+    
+    {  memcpy(Value, &uuid, Size);
+#else
+  uuid_generate_random(Value);
+#endif
+}
+    
+      bool isVoid() const {
+    assert(isValid());
+    return Decl.isNull();
   }
-  if (op->recv_trailing_metadata() != nullptr) {
-    recv_trailing_metadata_ = op->recv_trailing_metadata()->batch();
-    initial_on_done_recv_trailing_metadata_ =
-        op->op()->payload->recv_trailing_metadata.recv_trailing_metadata_ready;
-    op->op()->payload->recv_trailing_metadata.recv_trailing_metadata_ready =
-        &on_done_recv_trailing_metadata_;
+    
+    const Message* GetCProtoInsidePyProto(PyObject* msg) {
+  return GetCProtoInsidePyProtoPtr(msg);
+}
+Message* MutableCProtoInsidePyProto(PyObject* msg) {
+  return MutableCProtoInsidePyProtoPtr(msg);
+}
+    
+    bool AnyMetadata::InternalIs(const Descriptor* descriptor) const {
+  const string type_url = type_url_->GetNoArena();
+  string full_name;
+  if (!ParseAnyTypeUrl(type_url, &full_name)) {
+    return false;
   }
-  // Call next op.
-  grpc_call_next_op(elem, op->op());
+  return full_name == descriptor->full_name();
 }
     
-    size_t StatsContextSerialize(size_t max_tags_len, grpc_slice* tags) {
-  // TODO: Add implementation. Waiting on stats tagging to be added.
-  return 0;
+    const GeneratedCodeInfo::Annotation* FindAnnotationOnPath(
+    const GeneratedCodeInfo& info, const string& source_file,
+    const std::vector<int>& path) {
+  std::vector<const GeneratedCodeInfo::Annotation*> annotations;
+  FindAnnotationsOnPath(info, source_file, path, &annotations);
+  if (annotations.empty()) {
+    return NULL;
+  }
+  return annotations[0];
 }
     
-      // Deserializes a GrpcTraceContext from the incoming buffer. Returns the
-  // number of bytes deserialized from the buffer. If the incoming buffer is
-  // empty or the encoding version is not supported it will return 0 bytes,
-  // currently only version 0 is supported. If an unknown field ID is
-  // encountered it will return immediately without parsing the rest of the
-  // buffer. Inlined for performance reasons.
-  static size_t Decode(absl::string_view buf, GrpcTraceContext* tc) {
-    if (buf.empty()) {
-      return kEncodeDecodeFailure;
-    }
-    uint8_t version = buf[kVersionIdOffset];
-    // TODO: Support other versions later. Only support version 0 for
-    // now.
-    if (version != kVersionId) {
-      return kEncodeDecodeFailure;
-    }
-    }
+    void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enumDescriptor) {
+    WriteDocCommentBody(printer, enumDescriptor);
+}
+void WriteEnumValueDocComment(io::Printer* printer, const EnumValueDescriptor* value) {
+    WriteDocCommentBody(printer, value);
+}
     
-    #include <grpc/support/port_platform.h>
-    
-    // These measure definitions should be kept in sync across opencensus
-// implementations.
-    
-      // Add the full names of registered services
-  void SetServiceList(const std::vector<grpc::string>* services);
-    
-    #ifndef GRPC_CUSTOM_DEFAULT_THREAD_POOL
-    
-    DynamicThreadPool::DynamicThreadPool(int reserve_threads)
-    : shutdown_(false),
-      reserve_threads_(reserve_threads),
-      nthreads_(0),
-      threads_waiting_(0) {
-  for (int i = 0; i < reserve_threads_; i++) {
-    std::lock_guard<std::mutex> lock(mu_);
-    nthreads_++;
-    new DynamicThread(this);
+    // Strip package name from a descriptor's full name.
+// For example:
+//   Full name   : foo.Bar.Baz
+//   Package name: foo
+//   After strip : Bar.Baz
+string StripPackageName(const string& full_name,
+                        const FileDescriptor* file) {
+  if (file->package().empty()) {
+    return full_name;
+  } else {
+    // Strip package name
+    return full_name.substr(file->package().size() + 1);
   }
 }
     
-    std::string show(SrcLoc);
+      ObjectiveCType objc_type = GetObjectiveCType(descriptor_);
+  string singular_type;
+  if (objc_type == OBJECTIVECTYPE_MESSAGE) {
+    vars['type'] = string('GPBStringifySymbol(') +
+                   ClassName(descriptor_->message_type()) + ')';
+  } else {
+    vars['type'] = 'NULL';
+  }
     
-    void Assembler::bcctr(uint8_t bo, uint8_t bi, uint16_t bh) {
-  EmitXLForm(19, bo, bi, (bh & 0x3), 528);
-}
+    #define TEGRA_UNARYOPSCALE(type, op, src1, sz1, dst, sz, w, h, scales) \
+( \
+    CAROTENE_NS::isSupportedConfiguration() ? \
+    parallel_for_(Range(0, h), \
+    TegraGenOp_##op##_Invoker<const type, type>(src1, sz1, dst, sz, w, h, scales), \
+    (w * h) / static_cast<double>(1<<16)), \
+    CV_HAL_ERROR_OK \
+    : CV_HAL_ERROR_NOT_IMPLEMENTED \
+)
     
-      auto const array = collections::asArray(obj);
-  if (!array) return bail();
-    
-    private:
-  APCCollection();
-  ~APCCollection();
-  static APCHandle::Pair WrapArray(APCHandle::Pair, CollectionType);
-    
-    CONFIG_BODY(bool, Bool)
-CONFIG_BODY(char, Byte)
-CONFIG_BODY(unsigned char, UByte)
-CONFIG_BODY(int16_t, Int16)
-CONFIG_BODY(uint16_t, UInt16)
-CONFIG_BODY(int32_t, Int32)
-CONFIG_BODY(uint32_t, UInt32)
-CONFIG_BODY(int64_t, Int64)
-CONFIG_BODY(uint64_t, UInt64)
-CONFIG_BODY(double, Double)
-CONFIG_BODY(std::string, String)
-    
-      if (comma != data) {
-    // we have meta
-    ssize_t meta_len = comma - data;
-    data_len -= meta_len;
-    char* semi = (char*)memchr(data, ';', meta_len);
-    char* slash = (char*)memchr(data, '/', meta_len);
+            map = (u8*)(mag_buf[2] + magstep);
+        memset(map, 1, mapstep);
+        memset(map + mapstep*(size.height + 1), 1, mapstep);
+        borderyt = std::max<ptrdiff_t>(0, offsetk - (ptrdiff_t)borderMargin.top);
+        borderyb = std::max<ptrdiff_t>(0, offsetk - (ptrdiff_t)borderMargin.bottom);
     }
-    
-      req::ptr<Directory> opendir(const String& path) override;
-    
-        // skipping .  .. hidden files
-    if (ename[0] == '.' || !*ename) {
-      continue;
-    }
-    auto fe = fullPath + ename;
-    struct stat se;
-    if (stat(fe.c_str(), &se)) {
-      Logger::Error('FileUtil::find(): unable to stat %s', fe.c_str());
-      continue;
-    }
-    
-    bool File::flush() {
-  return true;
-}
-    
-    TEST(CorruptionTest, TableFile) {
-  Build(100);
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
-  dbi->TEST_CompactMemTable();
-  dbi->TEST_CompactRange(0, nullptr, nullptr);
-  dbi->TEST_CompactRange(1, nullptr, nullptr);
-    }
-    
-    
+    inline void firstRow(const Size2D &size, s32,
+                         const u8 *srcBase, ptrdiff_t srcStride,
+                         s16*, ptrdiff_t,
+                         s16*, ptrdiff_t,
+                         s32** mag_buf)
     {
-    {
-    {      default: {
-        char buf[40];
-        snprintf(buf, sizeof(buf), 'unknown record type %u', record_type);
-        ReportCorruption(
-            (fragment.size() + (in_fragmented_record ? scratch->size() : 0)),
-            buf);
-        in_fragmented_record = false;
-        scratch->clear();
-        break;
-      }
-    }
-  }
-  return false;
+        //sobelH row #0
+        const u8* _src = internal::getRowPtr(srcBase, srcStride, 0);
+        sobelRow(_src, ((s16*)mag_buf[0]) + shxOffset, ((s16*)mag_buf[0]) + shyOffset, size.width);
+        //sobelH row #1
+        _src = internal::getRowPtr(srcBase, srcStride, 1);
+        sobelRow(_src, ((s16*)mag_buf[1]) + shxOffset, ((s16*)mag_buf[1]) + shyOffset, size.width);
+    
+    /*!
+ *  Aligns pointer by the certain number of bytes
+ *
+ *  This small inline function aligns the pointer by the certain number of bytes by shifting
+ *  it forward by 0 or a positive offset.
+ */
+template<typename T> inline T* alignPtr(T* ptr, size_t n=sizeof(T))
+{
+    return (T*)(((size_t)ptr + n-1) & -n);
 }
     
-        if (s.ok()) {
-      s = file->Close();
-    }
-    delete file;
-    file = nullptr;
-    
-        // Advance to the first entry with a key >= target
-    void Seek(const Key& target);
-    
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-    int32 ImGui_Marmalade_KeyCallback(void* system_data, void* user_data)
+    void sqrIntegral(const Size2D &size,
+                 const u8 * srcBase, ptrdiff_t srcStride,
+                 f64 * sqsumBase, ptrdiff_t sqsumStride)
 {
-    ImGuiIO& io = ImGui::GetIO();
-    s3eKeyboardEvent* e = (s3eKeyboardEvent*)system_data;
-    if (e->m_Pressed == 1)
-        io.KeysDown[e->m_Key] = true;
-    if (e->m_Pressed == 0)
-        io.KeysDown[e->m_Key] = false;
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    uint16x8_t v_zero8 = vmovq_n_u16(0u);
     }
     
-    // Changelog:
-// - v0.50: (2017/08/16) imported from https://github.com/Vuhdo/imgui_freetype into http://www.github.com/ocornut/imgui_club, updated for latest changes in ImFontAtlas, minor tweaks.
-// - v0.51: (2017/08/26) cleanup, optimizations, support for ImFontConfig::RasterizerFlags, ImFontConfig::RasterizerMultiply.
-// - v0.52: (2017/09/26) fixes for imgui internal changes
-// - v0.53: (2017/10/22) minor inconsequential change to match change in master (removed an unnecessary statement)
-// - v0.54: (2018/01/22) fix for addition of ImFontAtlas::TexUvscale member
-// - v0.55: (2018/02/04) moved to main imgui repository (away from http://www.github.com/ocornut/imgui_club)
-// - v0.56: (2018/06/08) added support for ImFontConfig::GlyphMinAdvanceX, GlyphMaxAdvanceX
+    #include <carotene/definitions.hpp>
     
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
+      void StartBlockComparisons() override;
+  void FinishBlockComparisons() override;
     
-        // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    
-    const ImFontGlyph* ImFont::FindGlyphNoFallback(ImWchar c) const
-{
-    if (c >= IndexLookup.Size)
-        return NULL;
-    const ImWchar i = IndexLookup[c];
-    if (i == (ImWchar)-1)
-        return NULL;
-    return &Glyphs.Data[i];
+    inline int Log2FloorNonZero(uint32_t n) {
+#ifdef __GNUC__
+  return 31 ^ __builtin_clz(n);
+#else
+  unsigned int result = 0;
+  while (n >>= 1) result++;
+  return result;
+#endif
 }
     
+    #include 'guetzli/gamma_correct.h'
     
-    { private:
-  const int width_;
-  const int height_;
-  const float target_distance_;
-  const std::vector<uint8_t>& rgb_orig_;
-  int block_x_;
-  int block_y_;
-  int factor_x_;
-  int factor_y_;
-  std::vector<::butteraugli::ImageF> mask_xyz_;
-  std::vector<std::vector<std::vector<float>>> per_block_pregamma_;
-  ::butteraugli::ButteraugliComparator comparator_;
-  float distance_;
-  std::vector<float> distmap_;
-  ProcessStats* stats_;
-};
+    #include 'guetzli/output_image.h'
     
-    #define GUETZLI_LOG(stats, ...)                                    \
-  do {                                                             \
-    char debug_string[1024];                                       \
-    int res = snprintf(debug_string, sizeof(debug_string),         \
-                       __VA_ARGS__);                               \
-    assert(res > 0 && 'expected successful printing');             \
-    (void)res;                                                     \
-    debug_string[sizeof(debug_string) - 1] = '\0';                 \
-    ::guetzli::PrintDebug(                      \
-         stats, std::string(debug_string));        \
-  } while (0)
-#define GUETZLI_LOG_QUANT(stats, q)                    \
-  for (int y = 0; y < 8; ++y) {                        \
-    for (int c = 0; c < 3; ++c) {                      \
-      for (int x = 0; x < 8; ++x)                      \
-        GUETZLI_LOG(stats, ' %2d', (q)[c][8 * y + x]); \
-      GUETZLI_LOG(stats, '   ');                       \
-    }                                                  \
-    GUETZLI_LOG(stats, '\n');                          \
-  }
-    
-        // The nodes are:
-    // [0, n): the sorted leaf nodes that we start with.
-    // [n]: we add a sentinel here.
-    // [n + 1, 2n): new parent nodes are added here, starting from
-    //              (n+1). These are naturally in ascending order.
-    // [2n]: we add a sentinel at the end as well.
-    // There will be (2n+1) elements at the end.
-    const HuffmanTree sentinel(~static_cast<uint32_t>(0), -1, -1);
-    tree[n] = sentinel;
-    tree[n + 1] = sentinel;
-    
-    #endif  // GUETZLI_FAST_LOG_H_
-
-    
-    namespace guetzli {
-    }
-    
-    // Fills in 'result' with the inverse DCT of 'block'.
-// The arguments 'block' and 'result' point to 8x8 arrays that are arranged in
-// a row-by-row memory layout.
-void ComputeBlockIDCT(const coeff_t* block, uint8_t* result);
-    
-    namespace guetzli {
-    }
+    void Quantize(coeff_t* v, int iquant) {
+  *v = (*v * iquant + kBias) >> kDCTBits;
+}
     
     // Creates a JPEG from the rgb pixel data. Returns true on success. The given
 // quantization table must have 3 * kDCTBlockSize values.
 bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
                      const int* quant, JPEGData* jpg);
     
-    
-    {  // We have checked above that none of the sampling factors are 0, so the max
-  // sampling factors can not be 0.
-  jpg->MCU_rows = DivCeil(jpg->height, jpg->max_v_samp_factor * 8);
-  jpg->MCU_cols = DivCeil(jpg->width, jpg->max_h_samp_factor * 8);
-  // Compute the block dimensions for each component.
-  if (mode == JPEG_READ_ALL) {
-    for (size_t i = 0; i < jpg->components.size(); ++i) {
-      JPEGComponent* c = &jpg->components[i];
-      if (jpg->max_h_samp_factor % c->h_samp_factor != 0 ||
-          jpg->max_v_samp_factor % c->v_samp_factor != 0) {
-        fprintf(stderr, 'Non-integral subsampling ratios.\n');
-        jpg->error = JPEG_INVALID_SAMPLING_FACTORS;
-        return false;
-      }
-      c->width_in_blocks = jpg->MCU_cols * c->h_samp_factor;
-      c->height_in_blocks = jpg->MCU_rows * c->v_samp_factor;
-      const uint64_t num_blocks =
-          static_cast<uint64_t>(c->width_in_blocks) * c->height_in_blocks;
-      if (num_blocks > (1ull << 21)) {
-        // Refuse to allocate more than 1 GB of memory for the coefficients,
-        // that is 2M blocks x 64 coeffs x 2 bytes per coeff x max 4 components.
-        // TODO(user) Add this limit to a GuetzliParams struct.
-        fprintf(stderr, 'Image too large.\n');
-        jpg->error = JPEG_IMAGE_TOO_LARGE;
-        return false;
-      }
-      c->num_blocks = static_cast<int>(num_blocks);
-      c->coeffs.resize(c->num_blocks * kDCTBlockSize);
-    }
-  }
-  VERIFY_MARKER_END();
-  return true;
-}
-    
-    void BuildDCHistograms(const JPEGData& jpg, JpegHistogram* histo);
-void BuildACHistograms(const JPEGData& jpg, JpegHistogram* histo);
-size_t JpegHeaderSize(const JPEGData& jpg, bool strip_metadata);
-size_t EstimateJpegDataSize(const int num_components,
-                            const std::vector<JpegHistogram>& histograms);
+    #include <assert.h>
+#include <cstdlib>
+#include <string.h>
