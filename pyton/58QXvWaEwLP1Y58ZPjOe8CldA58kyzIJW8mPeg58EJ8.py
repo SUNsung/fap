ef86996b98a4ae47fa44901eb272ec5d9ab6c682
SUNsung <weatherius@gmail.com>
2018-11-18 17:09:05 +0200

    Rewrite [58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py] File in: 2018-11-18 17:09:04

--- pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
+++ pyton/58QXvWaEwLP1Y58ZPjOe8CldA58kyzIJW8mPeg58EJ8.py
@@ -1,21 +1,3 @@
 
         
         
-def l1(l=0.01):
-    return L1L2(l1=l)
-    
-    
-def test_cifar():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
-        (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine')
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
-        (x_train, y_train), (x_test, y_test) = cifar100.load_data('coarse')
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
