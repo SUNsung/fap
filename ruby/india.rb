2813aa2cf38018e65aec7e82a2f62e2c48f5c6db
SUNsung <weatherius@gmail.com>
2018-11-18 14:37:07 +0200

    Rewrite [india.rb] File in: 2018-11-18 14:37:06

--- ruby/india.rb
+++ ruby/india.rb
@@ -1,93 +1,17 @@
 
         
-          def self.path(name)
-    Formulary.core_path(name)
-  end
+            def subpath
+      @subpath ||= subpath_to(current_url)
+    end
     
-      before(:each) do
-    # This value needs to be set or our event fixtures will not match
-    allow(FastlaneCore::Helper).to receive(:ci?).and_return(false)
-    allow(FastlaneCore::Helper).to receive(:operating_system).and_return('macOS')
-  end
+        DOCUMENT_RGX = /\A(?:\s|(?:<!--.*?-->))*<(?:\!doctype|html)/i
     
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            tag: '#{tag}',
-            grouping: 'grouping',
-            build_number: 'build_number',
-            prefix: 'prefix',
-          })
-        end').runner.execute(:test)
+        def path
+      @path ||= url.path
+    end
     
-          it 'Does not use pattern matching for tag name if so requested' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          changelog_from_git_commits()
-        end').runner.execute(:test)
-    
-          it 'handles the extension parameter correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', extension: 'rb')
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --include=\\*.rb')
-      end
-    
-            it 'executes the correct git command' do
-          allow(Fastlane::Actions).to receive(:sh).with('git add #{path[0].shellescape} #{path[1].shellescape}', anything).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: #{path})
-          end').runner.execute(:test)
+          def call(*)
+        if @minute != Time.now.min
+          @minute = Time.now.min
+          @counter = 0
         end
-      end
-    
-          it 'works given the path to compile_commands.json' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json'
-            )
-          end').runner.execute(:test)
-    
-        describe 'shell escaping' do
-      let(:keychain_name) { 'keychain with spaces.keychain' }
-      let(:shell_escaped_name) { keychain_name.shellescape }
-      let(:name_regex) { Regexp.new(Regexp.escape(shell_escaped_name)) }
-    
-        odie 'Unknown command: #{cmd}' unless path
-    puts path
-  end
-end
-
-    
-        def URIEncodeOctets(octets, result, index)
-      if (@@hexCharCodeArray == 0)
-        @@hexCharCodeArray = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
-                              65, 66, 67, 68, 69, 70];
-      end
-      index = URIAddEncodedOctetToBuffer(octets[0], result, index);
-      if (octets[1]);
-        index = URIAddEncodedOctetToBuffer(octets[1], result, index)
-      end
-      if (octets[2]);
-        index = URIAddEncodedOctetToBuffer(octets[2], result, index)
-      end
-      if (octets[3]);
-        index = URIAddEncodedOctetToBuffer(octets[3], result, index)
-      end
-      return index;
-    end
-    
-    module Gollum
-end
-Gollum::GIT_ADAPTER = ENV['GIT_ADAPTER'] if ENV['GIT_ADAPTER']
-    
-      if base_path.nil?
-    Precious::App.run!(options)
-  else
-    require 'rack'
-    
-        def emoji(name)
-      if emoji = Gemojione.index.find_by_name(name)
-        IO.read(EMOJI_PATHNAME.join('#{emoji['unicode']}.png'))
-      else
-        fail ArgumentError, 'emoji `#{name}' not found'
-      end
-    end
