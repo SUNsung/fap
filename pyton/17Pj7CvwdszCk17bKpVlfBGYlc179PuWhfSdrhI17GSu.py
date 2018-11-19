ff4d8ccf4091acd9179647037767e1438c921c4e
SUNsung <weatherius@gmail.com>
2018-11-19 12:24:46 +0200

    Rewrite [17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py] File in: 2018-11-19 12:24:45

--- pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
+++ pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
@@ -1,100 +1,54 @@
 
         
-        Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
-'''
+        DOCUMENTATION = '''
+module: aws_waf_facts
+short_description: Retrieve facts for WAF ACLs, Rule , Conditions and Filters.
+description:
+  - Retrieve facts for WAF ACLs, Rule , Conditions and Filters.
+version_added: '2.4'
+requirements: [ boto3 ]
+options:
+  name:
+    description:
+      - The name of a Web Application Firewall
     
-        All keys are expected to be strings. The structure remembers the
-    case of the last key to be set, and ``iter(instance)``,
-    ``keys()``, ``items()``, ``iterkeys()``, and ``iteritems()``
-    will contain case-sensitive keys. However, querying and contains
-    testing is case insensitive::
+    # Get summary information about a stack
+- cloudformation_facts:
+    stack_name: my-cloudformation-stack
     
-        def test_list(self):
-        assert list(self.case_insensitive_dict) == ['Accept']
-    
-        def get_dict(self, domain=None, path=None):
-        '''Takes as an argument an optional domain and path and returns a plain
-        old Python dict of name-value pairs of cookies that meet the
-        requirements.
-    
-    Available hooks:
-    
-            assert status_line == b'GET /path/to/thing/ HTTP/1.1'
-        for frag in (b'view', b'edit', b'token', b'hunter2'):
-            assert frag not in headers
-            assert frag not in body
-    
-        def test_text_response(self):
-        '''the text_response_server sends the given text'''
-        server = Server.text_response_server(
-            'HTTP/1.1 200 OK\r\n' +
-            'Content-Length: 6\r\n' +
-            '\r\nroflol'
-        )
-    
-    # A sequence of (type, uri, title) tuples for the guide element of content.opf.
-#epub_guide = ()
-    
-        @staticmethod
-    def import_ubuntu_system_ca(common_name, certfile):
-        import platform
-        platform_distname = platform.dist()[0]
-        if platform_distname != 'Ubuntu':
-            return
-    
-        if len(sys.argv) > 1:
-        ip = sys.argv[1]
-    else:
-        ip = '46.134.208.94'
-        ip = '2001:ee0:3203:a::12'
-        print('Usage: check_ip.py [ip] [top_domain] [wait_time=0]')
-    print('test ip:%s' % ip)
+            # Identify targets that need to be added or updated on AWS
+        targets_to_put = self._targets_to_put()
+        if targets_to_put:
+            self.rule.put_targets(targets_to_put)
     
     
-class SniManager(object):
-    plus = ['-', '', '.']
-    end = ['com', 'net', 'ml', 'org', 'us']
+# Find the most recent snapshot
+def _get_snapshot_starttime(snap):
+    return datetime.datetime.strptime(snap.start_time, '%Y-%m-%dT%H:%M:%S.000Z')
     
-        major = int(m.group(1))
-    minor = int(m.group(2))
-    patch = int(m.group(4) or 0)
-    beta = int(m.group(6) or sys.maxint)
+                        if original_policy:
+                        original_policy = sort_json_policy_dict(original_policy)
     
-                elif opt in ('-g', '--gui'):
-                # Run using GUI.
-                conf['gui'] = True
+        def describe_services(self, cluster, services):
+        fn_args = dict()
+        if cluster and cluster is not None:
+            fn_args['cluster'] = cluster
+        fn_args['services'] = services
+        try:
+            response = self.describe_services_with_backoff(**fn_args)
+        except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
+            self.module.fail_json_aws(e, msg='Couldn't describe ECS services')
+        running_services = [self.extract_service_from(service) for service in response.get('services', [])]
+        services_not_running = response.get('failures', [])
+        return running_services, services_not_running
     
-        stream_types = [  # we do now know for now, as we have to check the
-                      # output from the API
-    ]
-    
-    __all__ = ['cbs_download']
-    
-        def prepare(self, **kwargs):
-        self.api_data = json.loads(get_content(self.__class__.ep.format(self.vid)))
-        self.title = self.api_data['title']
-        for s in self.api_data['video']:
-            for st in self.__class__.stream_types:
-                if st['map_to'] == s:
-                    urls = self.api_data['video'][s]
-                    src = [u['url'] for u in urls]
-                    stream_data = dict(src=src, size=0, container='mp4', video_profile=st['video_profile'])
-                    self.streams[st['id']] = stream_data
+        changed = False
+    try:
+        cert_action(module, iam, name, path, new_name, new_path, state,
+                    cert, key, cert_chain, orig_cert_names, orig_cert_bodies, dup_ok)
+    except boto.exception.BotoServerError as err:
+        module.fail_json(changed=changed, msg=str(err), debug=[cert, key])
     
     
-def write_loop_file(records_number, loop_file_path, file_name):
-    with open(loop_file_path, 'a') as file:
-        for i in range(records_number):
-            file.write('file '{}'\n'.format(file_name))
-    
-        if re.match(r'https?://movie', url):
-        title = match1(html, 'name='description' content='([^']+)')
-        tid = match1(url, 'trailer/(\d+)')
-        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
-        type, ext, size = url_info(real_url)
-    
-        urls = matchall(content, netease_embed_patterns)
-    for url in urls:
-        found = True
-        netease_download(url, output_dir=output_dir, merge=merge, info_only=info_only)
+def IsInitializerList(clean_lines, linenum):
+  '''Check if current line is inside constructor initializer list.
