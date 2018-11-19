2a985565a305b944b61abb3b317614e429a2a9fe
SUNsung <weatherius@gmail.com>
2018-11-19 08:17:09 +0200

    Rewrite [39X2Z6xSK2yak39PeEdgxltx4U392FPbLfNSmKs39x.Y.cpp] File in: 2018-11-19 08:17:08

--- cccccc/39X2Z6xSK2yak39PeEdgxltx4U392FPbLfNSmKs39x.Y.cpp
+++ cccccc/39X2Z6xSK2yak39PeEdgxltx4U392FPbLfNSmKs39x.Y.cpp
@@ -1,92 +1,274 @@
 
         
-        void CacheImpl::releaseValue(void *Value) {
-  // FIXME: Implementation.
-}
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+#define TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
     
-      char *oldAllocation = Allocated;
-  char *oldBegin = Begin;
-  std::size_t oldSize = (std::size_t) (End - oldBegin);
-    
-    bool LangOptions::isCustomConditionalCompilationFlagSet(StringRef Name) const {
-  return std::find(CustomConditionalCompilationFlags.begin(),
-                   CustomConditionalCompilationFlags.end(), Name)
-      != CustomConditionalCompilationFlags.end();
-}
-    
-    #if !defined(PREPOSITION) && !defined(VERB)
-#  error define one or more of PREPOSITION, VERB
-#endif
-    
-      ::UUID uuid2;
-  memcpy(&uuid2, y.Value, Size);
+    REGISTER_OP('Invalid')
+    .Attr('invalid attr: int32')  // invalid since the name has a space.
+    .Doc(R'doc(
+An op to test that invalid ops do not successfully generate invalid python code.
+)doc');
     
     
-    {  std::vector<OperatorDef> GetGradientDefs() override {
-    return SingleGradientDef(
-        'SubGradient',
-        '',
-        std::vector<std::string>{GO(0), I(0), I(1)},
-        std::vector<std::string>{GI(0), GI(1)});
-  }
-};
+    {}  // namespace
     
-    workspace.ResetWorkspace()
+    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
+// a copy.
+Status TensorToNdarray(const Tensor& t, PyObject** ret);
     
-    template <>
-void GluOp<float, CPUContext>::ComputeGlu(
-    const int M,
-    const int split_dim,
-    const int N,
-    const float* Xdata,
-    float* Ydata) {
-  const int xStride = 2 * split_dim * N;
-  const int yStride = split_dim * N;
-  for (int i = 0; i < M; ++i) {
-    const int idx = i * xStride;
-    const int idy = i * yStride;
-    for (int j = 0; j < split_dim; ++j) {
-      const int jN = j * N;
-      const int jdx1 = idx + jN;
-      const int jdx2 = idx + (j + split_dim) * N;
-      const int jdy = idy + jN;
-      for (int k = 0; k < N; ++k) {
-        const float x1 = Xdata[jdx1 + k];
-        const float x2 = Xdata[jdx2 + k];
-        Ydata[jdy + k] = x1 * sigmoid(x2);
-      }
+    
+    {}  // namespace tensorflow
+
+    
+    #include <Python.h>
+    
+    #include 'tensorflow/core/framework/node_def.pb.h'
+#include 'tensorflow/core/framework/node_def_util.h'
+#include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+#include 'tensorflow/core/framework/types.h'
+#include 'tensorflow/core/lib/core/status.h'
+#include 'tensorflow/core/util/device_name_utils.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    namespace stream_executor {
+namespace cuda {
+    }
+    }
+    
+        static AGInfo& Create(const nnvm::NodePtr& node) {
+      node->info.construct<AGInfo>();
+      return Get(node);
+    }
+    
+    #include <dmlc/base.h>
+#include <dmlc/json.h>
+#include <dmlc/logging.h>
+#include <dmlc/registry.h>
+#include <nnvm/node.h>
+#include <vector>
+#include <map>
+#include <string>
+#include <utility>
+#include './base.h'
+#include './resource.h'
+#include './op_attr_types.h'
+    
+     public:
+  /*! \brief cuda kernel argument descriptor */
+  struct ArgType {
+    /*! \brief whether argument is NDArray */
+    bool is_ndarray;
+    /*! \brief whether argument is constant (input) */
+    bool is_const;
+    /*! \brief data type of argument */
+    mshadow::TypeFlag dtype;
+  };
+  /*! \brief Cuda kernel */
+  class Kernel {
+   public:
+    /*! \brief Launch the kernel */
+    void Launch(const Context& ctx, const std::vector<dmlc::any>& args,
+                uint32_t grid_dim_x, uint32_t grid_dim_y, uint32_t grid_dim_z,
+                uint32_t block_dim_x, uint32_t block_dim_y, uint32_t block_dim_z,
+                uint32_t shared_mem);
+    /*! \brief kernel interface signature */
+    const std::vector<ArgType>& signature() { return signature_; }
+    }
+    
+    class TBlobContainer : public TBlob {
+ public:
+  TBlobContainer(void)
+    : TBlob(), tensor_container_(nullptr) {}
+  ~TBlobContainer() {
+    if (tensor_container_) {
+      release();
     }
   }
-}
-    
-    #include 'absl/strings/string_view.h'
-#include 'absl/time/time.h'
-#include 'src/cpp/ext/filters/census/channel_filter.h'
-#include 'src/cpp/ext/filters/census/context.h'
-    
-    
-    {}  // namespace grpc
-    
-    MeasureDouble RpcClientReceivedBytesPerRpc() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcClientReceivedBytesPerRpcMeasureName,
-      'Total bytes received across all response messages per RPC', kUnitBytes);
-  return measure;
-}
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_MEASURES_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_MEASURES_H
-    
-    bool ProtoServerReflectionPlugin::has_async_methods() const {
-  if (reflection_service_) {
-    return reflection_service_->has_async_methods();
+  void resize(const TShape &shape, int type_flag) {
+    if (tensor_container_) {
+      CHECK_EQ(this->type_flag_, type_flag);
+      this->shape_ = shape;
+      resize();
+    } else {
+      this->type_flag_ = type_flag;
+      this->shape_ = shape;
+      create();
+    }
   }
-  return false;
+    }
+    
+    TEST(ExecutorTest, KeepAliveBasic) {
+  KeepAliveTestExecutor exec;
+    }
+    
+    TEST(Expected, CoroutineSuccess) {
+  auto r0 = []() -> Expected<int, Err> {
+    auto x = co_await f1();
+    EXPECT_EQ(7, x);
+    auto y = co_await f2(x);
+    EXPECT_EQ(2.0 * 7, y);
+    auto z = co_await f3(x, y);
+    EXPECT_EQ(int(2.0 * 7 + 7), *z);
+    co_return* z;
+  }();
+  EXPECT_TRUE(r0.hasValue());
+  EXPECT_EQ(21, *r0);
 }
     
-    #ifndef GRPC_INTERNAL_CPP_DYNAMIC_THREAD_POOL_H
-#define GRPC_INTERNAL_CPP_DYNAMIC_THREAD_POOL_H
+    // ============================================================================
+// folly/test/GLogBenchmark.cpp                    relative  time/iter  iters/s
+// ============================================================================
+// skip_overhead                                               36.37ns   27.49M
+// dev_null_log_overhead                                        2.61us  382.57K
+// ============================================================================
     
-    	wchar_t message[1024];
-	swprintf_s(message, L'%s\nFunction: %s\nLine: %d', buffer, func, line);
-	LocalFree(buffer);
+    
+    {  std::string arguments;
+  ASSERT_TRUE(getTracepointArguments(
+      'folly', 'test_static_tracepoint_struct', 0, arguments));
+  std::array<int, 2> expected{{sizeof(testStruct), sizeof(testStruct)}};
+  checkTracepointArguments(arguments, expected);
+}
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    template <class String>
+String Uri::toString() const {
+  String str;
+  if (hasAuthority_) {
+    toAppend(scheme_, '://', &str);
+    if (!password_.empty()) {
+      toAppend(username_, ':', password_, '@', &str);
+    } else if (!username_.empty()) {
+      toAppend(username_, '@', &str);
+    }
+    toAppend(host_, &str);
+    if (port_ != 0) {
+      toAppend(':', port_, &str);
+    }
+  } else {
+    toAppend(scheme_, ':', &str);
+  }
+  toAppend(path_, &str);
+  if (!query_.empty()) {
+    toAppend('?', query_, &str);
+  }
+  if (!fragment_.empty()) {
+    toAppend('#', fragment_, &str);
+  }
+  return str;
+}
+    
+    struct Options {
+  /**
+   * ZLIB: default option -- write a zlib wrapper as documented in RFC 1950.
+   *
+   * GZIP: write a simple gzip header and trailer around the compressed data
+   * instead of a zlib wrapper.
+   *
+   * RAW: deflate will generate raw deflate data with no zlib header or
+   * trailer, and will not compute a check value.
+   *
+   * AUTO: enable automatic header detection for decoding gzip or zlib data.
+   * For deflation, ZLIB will be used.
+   */
+  enum class Format { ZLIB, GZIP, RAW, AUTO };
+    }
+    
+    /**
+ * @file
+ * @brief Defines the CanFrame struct and CanClient interface.
+ */
+    
+    TEST(CanClientFactoryTest, CreateCanClient) {
+  auto *can_factory = CanClientFactory::instance();
+  EXPECT_TRUE(can_factory != nullptr);
+    }
+    
+      // 2. set baudrate to 500k
+  ret = canSetBaudrate(dev_handler_, NTCAN_BAUD_500);
+  if (ret != NTCAN_SUCCESS) {
+    AERROR << 'set baudrate error code: ' << ret << ', ' << GetErrorString(ret);
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
+  }
+    
+      /**
+   * @brief Send messages
+   * @param frames The messages to send.
+   * @param frame_num The amount of messages to send.
+   * @return The status of the sending action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                 int32_t *const frame_num) override;
+    
+    ErrorCode FakeCanClient::Send(const std::vector<CanFrame> &frames,
+                              int32_t *const frame_num) {
+  if (frame_num == nullptr) {
+    AERROR << 'frame_num pointer is null';
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
+  }
+    }
+    
+      /**
+   * @brief Receive messages
+   * @param frames The messages to receive.
+   * @param frame_num The amount of messages to receive.
+   * @return The status of the receiving action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  apollo::common::ErrorCode Receive(std::vector<CanFrame> *frames,
+                                    int32_t *const frame_num) override;
+    
+      /**
+   * @brief Start the ESD CAN client.
+   * @return The status of the start action which is defined by
+   *         apollo::common::ErrorCode.
+   */
+  bool Init(const CANCardParameter &parameter) override;
+    
+      if (*frame_num > MAX_CAN_RECV_FRAME_LEN || *frame_num < 0) {
+    AERROR << 'recv can frame num not in range[0, ' << MAX_CAN_RECV_FRAME_LEN
+           << '], frame_num:' << *frame_num;
+    // TODO(Authors): check the difference of returning frame_num/error_code
+    return ErrorCode::CAN_CLIENT_ERROR_FRAME_NUM;
+  }
+    
+    template <typename SensorType>
+void CanReceiver<SensorType>::RecvThreadFunc() {
+  AINFO << 'Can client receiver thread starts.';
+  CHECK_NOTNULL(can_client_);
+  CHECK_NOTNULL(pt_manager_);
+    }
+    
+      /**
+   * @brief get chassis detail. used lock_guard in this function to avoid
+   * concurrent read/write issue.
+   * @param chassis_detail chassis_detail to be filled.
+   */
+  common::ErrorCode GetSensorData(SensorType *const sensor_data);
+    
+    
+    { private:
+  const int32_t data_length_ = CANBUS_MESSAGE_LENGTH;
+};
+    
+    TEST(ProtocolDataTest, CheckSum) {
+  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
+  const uint8_t result =
+      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
+  EXPECT_EQ(0xE7, result);
+}
