af38dbdbe9db1505470b30b9e8e8e565c79445cd
SUNsung <weatherius@gmail.com>
2018-11-19 06:02:08 +0200

    Rewrite [03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py] File in: 2018-11-19 06:02:08

--- pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
+++ pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
@@ -1,194 +1,138 @@
 
         
-            if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.update(test_config)
-    
-        # test that successful registration redirects to the login page
-    response = client.post(
-        '/auth/register', data={'username': 'a', 'password': 'a'}
-    )
-    assert 'http://localhost/auth/login' == response.headers['Location']
-    
-    
-def attach_enctype_error_multidict(request):
-    '''Since Flask 0.8 we're monkeypatching the files object in case a
-    request is detected that does not use multipart form data but the files
-    object is accessed.
-    '''
-    oldcls = request.files.__class__
-    class newcls(oldcls):
-        def __getitem__(self, key):
-            try:
-                return oldcls.__getitem__(self, key)
-            except KeyError:
-                if key not in request.form:
-                    raise
-                raise DebugFilesKeyError(request, key)
-    newcls.__name__ = oldcls.__name__
-    newcls.__module__ = oldcls.__module__
-    request.files.__class__ = newcls
-    
-    This typically means that you attempted to use functionality that needed
-an active HTTP request.  Consult the documentation on testing for
-information about how to avoid this problem.\
-'''
-_app_ctx_err_msg = '''\
-Working outside of application context.
-    
-        app.session_interface.serializer.register(TagOrderedDict, index=0)
-    
-            for blueprint in self.app.iter_blueprints():
-            loader = blueprint.jinja_loader
-            if loader is not None:
-                for template in loader.list_templates():
-                    result.add(template)
-    
-        .. versionchanged:: 0.12
-       `app.test_client()` includes preset default environment, which can be
-       set after instantiation of the `app.test_client()` object in
-       `client.environ_base`.
-    
-    version = sys.version_info[:2]
-if version < (2, 7):
-    print('thefuck requires Python version 2.7 or later' +
-          ' ({}.{} detected).'.format(*version))
-    sys.exit(-1)
-elif (3, 0) < version < (3, 4):
-    print('thefuck requires Python version 3.4 or later' +
-          ' ({}.{} detected).'.format(*version))
-    sys.exit(-1)
-    
-        assert match(command)
-    
-    
-@pytest.mark.parametrize('command', [
-    Command('aws dynamdb scan', misspelled_command),
-    Command('aws dynamodb scn', misspelled_subcommand),
-    Command('aws dynamodb t-item',
-            misspelled_subcommand_with_multiple_options)])
-def test_match(command):
-    assert match(command)
-    
-    
-@pytest.fixture
-def brew_install_no_argument():
-    return '''This command requires a formula argument'''
-    
-    \tDid you mean `build`?
-'''
-    
-        def disable(self):
-        '''Disables the rule in AWS'''
+            def create_origin_access_identity(self, caller_reference, comment):
         try:
-            response = self.client.disable_rule(Name=self.name)
-        except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
-            self.module.fail_json_aws(e, msg='Could not disable rule %s' % self.name)
-        self.changed = True
-        return response
+            return self.client.create_cloud_front_origin_access_identity(
+                CloudFrontOriginAccessIdentityConfig={
+                    'CallerReference': caller_reference,
+                    'Comment': comment
+                }
+            )
+        except (ClientError, BotoCoreError) as e:
+            self.module.fail_json_aws(e, msg='Error creating cloud front origin access identity.')
     
-        if state == 'present':
-        for required in ['name', 'description', 'subnets']:
-            if not module.params.get(required):
-                module.fail_json(msg=str('Parameter %s required for state='present'' % required))
+    import traceback
+from ansible.module_utils._text import to_native
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.ec2 import HAS_BOTO3, camel_dict_to_snake_dict, boto3_conn, ec2_argument_spec, get_aws_connection_info
+    
+        try:
+        images = ec2_client.describe_images(ImageIds=image_ids, Filters=filters, Owners=owner_param, ExecutableUsers=executable_users)
+        images = [camel_dict_to_snake_dict(image) for image in images['Images']]
+    except (ClientError, BotoCoreError) as err:
+        module.fail_json_aws(err, msg='error describing images')
+    for image in images:
+        try:
+            image['tags'] = boto3_tag_list_to_ansible_dict(image.get('tags', []))
+            if module.params.get('describe_image_attributes'):
+                launch_permissions = ec2_client.describe_image_attribute(Attribute='launchPermission', ImageId=image['image_id'])['LaunchPermissions']
+                image['launch_permissions'] = [camel_dict_to_snake_dict(perm) for perm in launch_permissions]
+        except (ClientError, BotoCoreError) as err:
+            # describing launch permissions of images owned by others is not permitted, but shouldn't cause failures
+            pass
+    
+        argument_spec = ec2_argument_spec()
+    argument_spec.update(
+        dict(
+            name=dict(type='str'),
+            tags=dict(type='dict'),
+        )
+    )
+    module = AnsibleModule(argument_spec=argument_spec)
+    
+    
+if __name__ == '__main__':
+    main()
+
+    
+                if delete_policy:
+                original_policy = ecr.get_repository_policy(registry_id, name)
+    
+    
+def get_elasticache_clusters(client, module, region):
+    try:
+        clusters = describe_cache_clusters_with_backoff(client, cluster_id=module.params.get('name'))
+    except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
+        module.fail_json_aws(e, msg='Couldn't obtain cache cluster info')
+    
+                # check if alias has changed -- only version and description can change
+            alias_params = ('function_version', 'description')
+            for param in alias_params:
+                if module.params.get(param) != facts.get(pc(param)):
+                    changed = True
+                    break
+    
+            if module.params.get('next_marker'):
+            params['Marker'] = module.params.get('next_marker')
+        try:
+            lambda_facts.update(aliases=client.list_aliases(FunctionName=function_name, **params)['Aliases'])
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                lambda_facts.update(aliases=[])
+            else:
+                module.fail_json_aws(e, msg='Trying to get aliases')
     else:
-        for not_allowed in ['description', 'subnets']:
-            if module.params.get(not_allowed):
-                module.fail_json(msg=str('Parameter %s not allowed for state='absent'' % not_allowed))
+        module.fail_json(msg='Parameter function_name required for query=aliases.')
     
-        except botocore.exceptions.ClientError:
-        pass
+            print('Training %s ... ' % name, end='')
+        t0 = time()
+        clf.fit(X_train, y_train)
+        train_time[name] = time() - t0
+        t0 = time()
+        y_pred = clf.predict(X_test)
+        test_time[name] = time() - t0
+        accuracy[name] = accuracy_score(y_test, y_pred)
+        print('done')
     
-        state = module.params.get('state')
+            X = np.random.randn(n_samples, n_features)
+        Y = np.random.randn(n_samples)
     
-        Basic Usage:
-        >>> client = boto3.client('kinesis')
-        >>> stream_name = 'test-stream'
-        >>> shard_count = 20
-        >>> stream_action(client, stream_name, shard_count, action='create', encryption_type='KMS',key_id='alias/aws')
+    We generate a synthetic dataset of size 10^n, for n in [min, max], and
+examine the time taken to run isotonic regression over the dataset.
     
-        aws = AWSConnection(module, ['lambda'])
+        if dataset_name == 'forestcover':
+        dataset = fetch_covtype()
+        X = dataset.data
+        y = dataset.target
+        # normal data are those with attribute 2
+        # abnormal those with attribute 4
+        s = (y == 2) + (y == 4)
+        X = X[s, :]
+        y = y[s]
+        y = (y != 2).astype(int)
     
-    # If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-    
-    test_locate_faces = '''
-face_locations = face_recognition.face_locations(image)
-'''
-    
-        # Make the eyebrows into a nightmare
-    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
-    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
-    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    
-        if len(unknown_face_encodings) > 0:
-        face_found = True
-        # See if the first face in the uploaded image matches the known face of Obama
-        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
-        if match_results[0]:
-            is_obama = True
-    
-        :param file: image file name or file object to load
-    :param mode: format to convert the image to. Only 'RGB' (8-bit RGB, 3 channels) and 'L' (black and white) are supported.
-    :return: image contents as numpy array
-    '''
-    im = PIL.Image.open(file)
-    if mode:
-        im = im.convert(mode)
-    return np.array(im)
-    
-        # Quit when the input video file ends
-    if not ret:
-        break
-    
-            # Clear the frames array to start the next batch
-        frames = []
-
-    
-    # Find all the faces and face encodings in the unknown image
-face_locations = face_recognition.face_locations(unknown_image)
-face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
-    
-    test_requirements = [
-    'tox',
-    'flake8==2.6.0'
-]
+    import numpy as np
+from numpy import random as nr
     
     
-def get_localizer(language='English'):
-    '''The factory method'''
-    languages = dict(English=EnglishGetter, Greek=GreekGetter)
-    return languages[language]()
+def compute_bench(samples_range, features_range):
     
-        doctest.testmod()
-
+        if netloc.startswith(('127.0.0.', '::1', 'localhost')):
+        start_response('400 Bad Request', [('Content-Type', 'text/html')])
+        html = ''.join('<a href='https://%s/'>%s</a><br/>' % (x, x) for x in ('google.com', 'mail.google.com'))
+        yield message_html('GoAgent %s is Running' % __version__, 'Now you can visit some websites', html)
+        raise StopIteration
     
+        if not app.config.edit_on_github_project:
+        warnings.warn('edit_on_github_project not specified')
+        return
+    if not doctree:
+        warnings.warn('doctree is None')
+        return
+    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
+    show_url = get_github_url(app, 'blob', path)
+    edit_url = get_github_url(app, 'edit', path)
     
-def main():
-    command_stack = []
+    ATTR_URL = 'url'
+ATTR_URL_DEFAULT = 'https://www.google.com'
     
-        def scan(self):
-        '''Scan the dial to the next station'''
-        self.pos += 1
-        if self.pos == len(self.stations):
-            self.pos = 0
-        print(u'Scanning... Station is %s %s' % (self.stations[self.pos], self.name))
+        def _update_info(self):
+        '''Ensure the information from the router is up to date.
     
-        def get_current_time_as_html_fragment(self, time_provider):
-        current_time = time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
+                        with open(final_path, 'wb') as fil:
+                        for chunk in req.iter_content(1024):
+                            fil.write(chunk)
     
-        def now(self):
-        current_time_is_always_midnight = '24:01'
-        return current_time_is_always_midnight
-
-    
-    
-class Controller(object):
-    def __init__(self, blackboard):
-        self.blackboard = blackboard
+            def on_created(self, event):
+            '''File created.'''
+            self.process(event)
