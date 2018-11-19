dedb945edd6c0941d589ddd7549a57f0a85b91d2
SUNsung <weatherius@gmail.com>
2018-11-19 14:37:17 +0200

    Rewrite [486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp] File in: 2018-11-19 14:37:17

--- cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
+++ cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
@@ -1,225 +1,86 @@
 
         
-        #endif  // TENSORFLOW_PYTHON_FRAMEWORK_CPP_SHAPE_INFERENCE_H_
-
+        class Semaphore {
+ public:
+  void post(int n = 1) {
+    std::unique_lock<std::mutex> lock(m_);
+    n_ += n;
+    cv_.notify_all();
+  }
+    }
     
-    // An example Op.
+    REGISTER_CPU_OPERATOR(
+    MergeMultiScalarFeatureTensorsGradient,
+    MergeMultiScalarFeatureTensorsGradientOp<CPUContext>);
+OPERATOR_SCHEMA(MergeMultiScalarFeatureTensorsGradient)
+    .SetDoc(
+        'Explode given multi-feature tensors with scalar features into many.' +
+        doc)
+    .NumInputs([](int n) { return n >= 2; })
+    .NumOutputs([](int n) { return n >= 1; })
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'out_values_grad', '.values_grad')
+    .Output(0, 'in1_values_grad', '.values_grad');
+REGISTER_GRADIENT(
+    MergeMultiScalarFeatureTensors,
+    GetMergeMultiScalarFeatureTensorsGradient);
     
+    <summary> <b>Example</b> </summary>
     
-    {}  // namespace tensorflow
-
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    namespace tensorflow {
-namespace swig {
+    namespace caffe2 {
+OPERATOR_SCHEMA(FloatToHalf)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .TensorInferenceFunction(
+        [](const OperatorDef& def, const vector<TensorShape>& in) {
+          vector<TensorShape> out;
+          const TensorShape& X = in[0];
+          out.push_back(X);
+          out[0].set_data_type(TensorProto_DataType_FLOAT16);
     }
     }
     
-    #include 'tensorflow/stream_executor/cuda/cuda_activation.h'
-    
-      // Gets a specific integer-valued property about the given device.
-  //
-  // http://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__DEVICE.html#group__CUDA__DEVICE_1g9c3e1414f0ad901d3278a4d6645fc266
-  static port::StatusOr<int> GetDeviceAttribute(CUdevice_attribute attribute,
-                                                CUdevice device);
-    
-       const_reference get_last_closed_paren()const
-   {
-      if(m_is_singular)
-         raise_logic_error();
-      return m_last_closed_paren == 0 ? m_null : (*this)[m_last_closed_paren];
-   }
-    
-    private:
-   // final result structure to be filled in:
-   match_results<BidiIterator, Allocator>& m_result;
-   // temporary result for POSIX matches:
-   scoped_ptr<match_results<BidiIterator, Allocator> > m_temp_match;
-   // pointer to actual result structure to fill in:
-   match_results<BidiIterator, Allocator>* m_presult;
-   // start of sequence being searched:
-   BidiIterator base;
-   // end of sequence being searched:
-   BidiIterator last; 
-   // current character being examined:
-   BidiIterator position;
-   // where to restart next search after failed match attempt:
-   BidiIterator restart;
-   // where the current search started from, acts as base for $` during grep:
-   BidiIterator search_base;
-   // how far we can go back when matching lookbehind:
-   BidiIterator backstop;
-   // the expression being examined:
-   const basic_regex<char_type, traits>& re;
-   // the expression's traits class:
-   const ::boost::regex_traits_wrapper<traits>& traits_inst;
-   // the next state in the machine being matched:
-   const re_syntax_base* pstate;
-   // matching flags in use:
-   match_flag_type m_match_flags;
-   // how many states we have examined so far:
-   std::ptrdiff_t state_count;
-   // max number of states to examine before giving up:
-   std::ptrdiff_t max_state_count;
-   // whether we should ignore case or not:
-   bool icase;
-   // set to true when (position == last), indicates that we may have a partial match:
-   bool m_has_partial_match;
-   // set to true whenever we get a match:
-   bool m_has_found_match;
-   // set to true whenever we're inside an independent sub-expression:
-   bool m_independent;
-   // the current repeat being examined:
-   repeater_count<BidiIterator>* next_count;
-   // the first repeat being examined (top of linked list):
-   repeater_count<BidiIterator> rep_obj;
-   // the mask to pass when matching word boundaries:
-   typename traits::char_class_type m_word_mask;
-   // the bitmask to use when determining whether a match_any matches a newline or not:
-   unsigned char match_any_mask;
-   // recursion information:
-   std::vector<recursion_info<results_type> > recursion_stack;
-#ifdef BOOST_REGEX_RECURSIVE
-   // Set to false by a (*COMMIT):
-   bool m_can_backtrack;
-   bool m_have_accept;
-   bool m_have_then;
-#endif
-#ifdef BOOST_REGEX_NON_RECURSIVE
-   //
-   // additional members for non-recursive version:
-   //
-   typedef bool (self_type::*unwind_proc_type)(bool);
-    
-    #endif
-    
-    template <class T>
-class concrete_protected_call
-   : public abstract_protected_call
-{
-public:
-   typedef bool (T::*proc_type)();
-   concrete_protected_call(T* o, proc_type p)
-      : obj(o), proc(p) {}
-private:
-   virtual bool call()const;
-   T* obj;
-   proc_type proc;
+    template <typename T>
+struct EnableIf<false, T> {
 };
     
-    template <class traits, class charT, class Formatter>
-std::basic_string<charT> regex_replace(const std::basic_string<charT>& s,
-                         const basic_regex<charT, traits>& e, 
-                         Formatter fmt,
-                         match_flag_type flags = match_default)
-{
-   std::basic_string<charT> result;
-   BOOST_REGEX_DETAIL_NS::string_out_iterator<std::basic_string<charT> > i(result);
-   regex_replace(i, s.begin(), s.end(), e, fmt, flags);
-   return result;
+    bool GodotCollisionDispatcher::needsResponse(const btCollisionObject *body0, const btCollisionObject *body1) {
+	if (body0->getUserIndex() == CASTED_TYPE_AREA || body1->getUserIndex() == CASTED_TYPE_AREA) {
+		// Avoide area narrow phase
+		return false;
+	}
+	return btCollisionDispatcher::needsResponse(body0, body1);
 }
+
     
-      void onReceived(const DHTPingReplyMessage* message);
+    public:
+	GodotRayWorldAlgorithm(const btDiscreteDynamicsWorld *world, btPersistentManifold *mf, const btCollisionAlgorithmConstructionInfo &ci, const btCollisionObjectWrapper *body0Wrap, const btCollisionObjectWrapper *body1Wrap, bool isSwapped);
+	virtual ~GodotRayWorldAlgorithm();
     
-    bool DHTRoutingTable::addNode(const std::shared_ptr<DHTNode>& node)
-{
-  return addNode(node, false);
+    #include 'joint_bullet.h'
+    
+    	const RigidBodyBullet *getRigidBodyA() const;
+	const RigidBodyBullet *getRigidBodyB() const;
+	const Transform getCalculatedTransformA() const;
+	const Transform getCalculatedTransformB() const;
+	const Transform getFrameOffsetA() const;
+	const Transform getFrameOffsetB() const;
+	Transform getFrameOffsetA();
+	Transform getFrameOffsetB();
+	real_t getLowerLinLimit() const;
+	void setLowerLinLimit(real_t lowerLimit);
+	real_t getUpperLinLimit() const;
+	void setUpperLinLimit(real_t upperLimit);
+	real_t getLowerAngLimit() const;
+	void setLowerAngLimit(real_t lowerLimit);
+	real_t getUpperAngLimit() const;
+	void setUpperAngLimit(real_t upperLimit);
+    
+    	ERR_FAIL_NULL(p_obj);
+	id = p_obj->get_instance_id();
 }
+void FuncRef::set_function(const StringName &p_func) {
     
-    class DHTNode;
-class DHTRoutingTable;
-class DHTMessageDispatcher;
-class DHTMessageFactory;
-class DHTTaskQueue;
-class DHTAbstractTask;
     
-    DHTTaskQueueImpl::~DHTTaskQueueImpl() = default;
-    
-      ~DHTTokenTracker();
-    
-    class DHTTokenUpdateCommand : public TimeBasedCommand {
-private:
-  DHTTokenTracker* tokenTracker_;
-    }
-    
-    std::string DHTUnknownMessage::toString() const
-{
-  size_t sampleLength = 8;
-  if (length_ < sampleLength) {
-    sampleLength = length_;
-  }
-  return fmt('dht unknown Remote:%s(%u) length=%lu, first 8 bytes(hex)=%s',
-             ipaddr_.c_str(), port_, static_cast<unsigned long>(length_),
-             util::toHex(data_, sampleLength).c_str());
+    {	return singleton;
 }
-    
-      // do nothing; we don't use this message as outgoing message.
-  virtual bool send() CXX11_OVERRIDE;
-    
-    DNSCache::AddrEntry::AddrEntry(const AddrEntry& c) = default;
-    
-    // Return the epilogue of generated mock file.
-grpc::string GetMockEpilogue(grpc_generator::File *file,
-                             const Parameters &params);
-    
-      // Instead, we're going to access it immediately, as if we just recieved this.
-    
-      // parse schema first, so we can use it to parse the data after
-  flatbuffers::Parser parser;
-  const char *include_directories[] = { 'samples', nullptr };
-  ok = parser.Parse(schemafile.c_str(), include_directories) &&
-       parser.Parse(jsonfile.c_str(), include_directories);
-  assert(ok);
-    
-    #include <stdio.h>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include 'flatbuffers/hash.h'
-    
-    #include <grpc++/impl/codegen/async_stream.h>
-#include <grpc++/impl/codegen/async_unary_call.h>
-#include <grpc++/impl/codegen/channel_interface.h>
-#include <grpc++/impl/codegen/client_unary_call.h>
-#include <grpc++/impl/codegen/method_handler_impl.h>
-#include <grpc++/impl/codegen/rpc_service_method.h>
-#include <grpc++/impl/codegen/service_type.h>
-#include <grpc++/impl/codegen/sync_stream.h>
-namespace MyGame {
-namespace Example {
-    }
-    }
-    
-    #include <grpc++/impl/codegen/async_stream.h>
-#include <grpc++/impl/codegen/async_unary_call.h>
-#include <grpc++/impl/codegen/method_handler_impl.h>
-#include <grpc++/impl/codegen/proto_utils.h>
-#include <grpc++/impl/codegen/rpc_method.h>
-#include <grpc++/impl/codegen/service_type.h>
-#include <grpc++/impl/codegen/status.h>
-#include <grpc++/impl/codegen/stub_options.h>
-#include <grpc++/impl/codegen/sync_stream.h>
-    
-    struct Parameters {
-  //Defines the custom parameter types for methods
-  //eg: flatbuffers uses flatbuffers.Builder as input for the client and output for the server
-  grpc::string custom_method_io_type;
-    }
-    
-      template <class T> bool operator==(const unique_ptr<T>& x, intptr_t y) {
-    return reinterpret_cast<intptr_t>(x.get()) == y;
-  }
-#endif  // !FLATBUFFERS_CPP98_STL
