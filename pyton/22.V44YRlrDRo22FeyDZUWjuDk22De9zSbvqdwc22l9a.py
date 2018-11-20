61ecd7b0fbbb68da30c70cb64098bd480e13859c
SUNsung <weatherius@gmail.com>
2018-11-20 05:17:23 +0200

    Rewrite [22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py] File in: 2018-11-20 05:17:23

--- pyton/22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py
+++ pyton/22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py
@@ -1,279 +1,171 @@
 
         
-                Accessing a node updates its position to the front of the LRU list.
-        '''
-        node = self.lookup.get(query)
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
+            changelog_file, version, build_path = args
     
+    now = datetime.datetime.now()
+now_iso = now.isoformat() + 'Z'
     
-class AddRequest(object):
+    MOCK_MODULES = ['face_recognition_models', 'Click', 'dlib', 'numpy', 'PIL']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+    
+    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
+# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
+# positive matches at the risk of more false negatives.
+    
+    # To run this, you need a Raspberry Pi 2 (or greater) with face_recognition and
+# the picamera[array] module installed.
+# You can follow this installation instructions to get your RPi set up:
+# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
+    
+                for face_location in face_locations:
+                # Print the location of each face in this frame
+                top, right, bottom, left = face_location
+                print(' - A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
     
 if __name__ == '__main__':
-    HitCounts.run()
+    main()
 
     
+        # Print the location of each facial feature in this image
+    for facial_feature in face_landmarks.keys():
+        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
     
-class RemoveDuplicateUrls(MRJob):
+            if match[0]:
+            name = 'Barack Obama'
     
+        # 讲识别结果以json键值对的数据结构输出
+    result = {
+        'face_found_in_image': face_found,
+        'is_picture_of_obama': is_obama
+    }
+    return jsonify(result)
     
-class Crawler(object):
+        # string conversion routines
+    def _str_header(self, name, symbol='`'):
+        return ['.. rubric:: ' + name, '']
     
-    
-def load_word_embeddings(word_embeddings_dir, word_embeddings_file):
-  '''Loads pretrained word embeddings from a binary file and returns the matrix.
-    
-    
-  if truth_bxtxn is not None:
-    plot_time_series(truth_bxtxn, bidx, n_to_plot=n_to_plot, color='k',
-                     scale=scale)
-    
-    
-class IntegrationToBoundModel:
-  def __init__(self, N):
-    scale = 0.8 / float(N**0.5)
-    self.N = N
-    self.Wh_nxn = tf.Variable(tf.random_normal([N, N], stddev=scale))
-    self.b_1xn = tf.Variable(tf.zeros([1, N]))
-    self.Bu_1xn = tf.Variable(tf.zeros([1, N]))
-    self.Wro_nxo = tf.Variable(tf.random_normal([N, 1], stddev=scale))
-    self.bro_o = tf.Variable(tf.zeros([1]))
-    
-      Args:
-    gen_logits:  Generator logits.
-    gen_labels:  Labels for the correct token.
-    dis_values:  Discriminator values Tensor of shape [batch_size,
-      sequence_length].
-    is_real_input:  Tensor indicating whether the label is present.
+    max_colwidth_doc = '''
+: int
+    The maximum width in characters of a column in the repr of
+    a pandas data structure. When the column overflows, a '...'
+    placeholder is embedded in the output.
+'''
     
     
-def recursive_length(item):
-  '''Recursively determine the total number of elements in nested list.'''
-  if type(item) == list:
-    return sum(recursive_length(subitem) for subitem in item)
-  else:
-    return 1.
+class UnpackValueError(UnpackException, ValueError):
+    pass
     
-    
-# TODO(adai): IMDB labels placeholder to model.
-def create_generator(hparams,
-                     inputs,
-                     targets,
-                     present,
-                     is_training,
-                     is_validating,
-                     reuse=None):
-  '''Create the Generator model specified by the FLAGS and hparams.
-    
-      # Common elements to Generator and Discriminator.
-  embedding = [
-      v for v in tf.trainable_variables()
-      if v.op.name == str(model) + '/rnn/embedding'
-  ][0]
-  lstm_w_0 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      str(model) + '/rnn/GenericMultiRNNCell/Cell0/Alien/rnn_builder/big_h_mat'
-  ][0]
-  lstm_b_0 = [
-      v for v in tf.trainable_variables()
-      if v.op.name == str(model) +
-      '/rnn/GenericMultiRNNCell/Cell0/Alien/rnn_builder/big_inputs_mat'
-  ][0]
-  lstm_w_1 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      str(model) + '/rnn/GenericMultiRNNCell/Cell1/Alien/rnn_builder/big_h_mat'
-  ][0]
-  lstm_b_1 = [
-      v for v in tf.trainable_variables()
-      if v.op.name == str(model) +
-      '/rnn/GenericMultiRNNCell/Cell1/Alien/rnn_builder/big_inputs_mat'
-  ][0]
-    
-        Args:
-      time: positive integer constant reflecting the current timestep.
-      cell_state: state of RNNCell.
-      cell_input: input provided by `dynamic_rnn_decoder`.
-      cell_output: output of RNNCell.
-      context_state: context state provided by `dynamic_rnn_decoder`.
-    
-    
-def _assert_tracker_updated(usage_tracker_io, pid):
-    usage_tracker_io.seek(0)
-    info = json.load(usage_tracker_io)
-    assert info['pid'] == pid
-    
-        assert proc.expect([TIMEOUT, u'usage'])
-    
-    containers = (('thefuck/python3-fish',
-               u'''FROM python:3
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'),
-              ('thefuck/python2-fish',
-               u'''FROM python:2
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'))
-    
-        assert not match(command)
-    
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_match(brew_no_available_formula, brew_already_installed,
-               brew_install_no_argument):
-    assert match(Command('brew install elsticsearch',
-                         brew_no_available_formula))
-    assert not match(Command('brew install git',
-                             brew_already_installed))
-    assert not match(Command('brew install', brew_install_no_argument))
-    
-            print('Iteration %s of %s' % (i, n_iter))
-    
-    
-def generate_logistic_dataset(size):
-    X = np.sort(np.random.normal(size=size))
-    return np.random.random(size=size) < 1.0 / (1.0 + np.exp(-X))
-    
-        alpha = 0.01  # regularization parameter
-    
-    Note that LocalOutlierFactor is not meant to predict on a test set and its
-performance is assessed in an outlier detection context:
-1. The model is trained on the whole dataset which is assumed to contain
-outliers.
-2. The ROC curve is computed on the same dataset using the knowledge of the
-labels.
-In this context there is no need to shuffle the dataset because the model
-is trained and tested on the whole dataset. The randomness of this benchmark
-is only caused by the random selection of anomalies in the SA dataset.
-    
-        max_it = len(samples_range) * len(features_range)
-    for n_samples in samples_range:
-        for n_features in features_range:
-            it += 1
-            print('====================')
-            print('Iteration %03d of %03d' % (it, max_it))
-            print('====================')
-            dataset_kwargs = {
-                'n_samples': n_samples,
-                'n_features': n_features,
-                'n_informative': n_features // 10,
-                'effective_rank': min(n_samples, n_features) / 10,
-                #'effective_rank': None,
-                'bias': 0.0,
-            }
-            print('n_samples: %d' % n_samples)
-            print('n_features: %d' % n_features)
-            X, y = make_regression(**dataset_kwargs)
-    
-    plt.figure('scikit-learn Ward's method benchmark results')
-plt.imshow(np.log(ratio), aspect='auto', origin='lower')
-plt.colorbar()
-plt.contour(ratio, levels=[1, ], colors='k')
-plt.yticks(range(len(n_features)), n_features.astype(np.int))
-plt.ylabel('N features')
-plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
-plt.xlabel('N samples')
-plt.title('Scikit's time, in units of scipy time (log)')
-plt.show()
+        if engine == 'numexpr':
+        try:
+            import numexpr as ne
+        except ImportError:
+            pytest.skip('no numexpr')
+        else:
+            if (LooseVersion(ne.__version__) <
+                    LooseVersion(_MIN_NUMEXPR_VERSION)):
+                with pytest.raises(ImportError):
+                    testit()
+            else:
+                testit()
+    else:
+        testit()
 
     
-        ###########################################################################
-    # Print results
-    ###########################################################################
-    print('Script arguments')
-    print('===========================')
-    arguments = vars(opts)
-    print('%s \t | %s ' % ('Arguments'.ljust(16),
-                           'Value'.center(12),))
-    print(25 * '-' + ('|' + '-' * 14) * 1)
-    for key, value in arguments.items():
-        print('%s \t | %s ' % (str(key).ljust(16),
-                               str(value).strip().center(12)))
-    print('')
-    
-                gc.collect()
-            print('- benchmarking RidgeRegression')
-            clf = Ridge(alpha=alpha, fit_intercept=False)
-            tstart = time()
-            clf.fit(X_train, y_train)
-            ridge_results[i, j, 0] = mean_squared_error(clf.predict(X_test),
-                                                        y_test)
-            ridge_results[i, j, 1] = time() - tstart
-    
-    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-TRAIN_FOLDER = '20news-bydate-train'
-TEST_FOLDER = '20news-bydate-test'
-    
-        def decode(self, value):
-        if value != self.value:
-            raise jose.DeserializationError('Expected {0!r}'.format(self.value))
-        return self.value
-    
-        # TODO: decoder/encoder should accept cls? Otherwise, subclassing
-    # JSONObjectWithFields is tricky...
-    header_cls = Header
-    header = jose.Field(
-        'header', omitempty=True, default=header_cls(),
-        decoder=header_cls.from_json)
+            index = getattr(tm, 'make{}'.format(index))
+        df = DataFrame({'a': list('abc'),
+                        'b': list(range(1, 4)),
+                        'c': np.arange(3, 6).astype('u1'),
+                        'd': np.arange(4.0, 7.0, dtype='float64'),
+                        'e': [True, False, True],
+                        'f': pd.Categorical(list('abc')),
+                        'g': pd.date_range('20130101', periods=3),
+                        'h': pd.date_range('20130101',
+                                           periods=3,
+                                           tz='US/Eastern')}
+                       )
     
     
-class JWSTest(unittest.TestCase):
-    '''Tests for acme.jws.JWS.'''
+def test_match():
+    cases = [
+        (None, b'\xc0'),
+        (False, b'\xc2'),
+        (True, b'\xc3'),
+        (0, b'\x00'),
+        (127, b'\x7f'),
+        (128, b'\xcc\x80'),
+        (256, b'\xcd\x01\x00'),
+        (-1, b'\xff'),
+        (-33, b'\xd0\xdf'),
+        (-129, b'\xd1\xff\x7f'),
+        ({1: 1}, b'\x81\x01\x01'),
+        (1.0, b'\xcb\x3f\xf0\x00\x00\x00\x00\x00\x00'),
+        ((), b'\x90'),
+        (tuple(range(15)), (b'\x9f\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09'
+                            b'\x0a\x0b\x0c\x0d\x0e')),
+        (tuple(range(16)), (b'\xdc\x00\x10\x00\x01\x02\x03\x04\x05\x06\x07'
+                            b'\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f')),
+        ({}, b'\x80'),
+        ({x: x for x in range(15)},
+         (b'\x8f\x00\x00\x01\x01\x02\x02\x03\x03\x04\x04\x05\x05\x06\x06\x07'
+          b'\x07\x08\x08\t\t\n\n\x0b\x0b\x0c\x0c\r\r\x0e\x0e')),
+        ({x: x for x in range(16)},
+         (b'\xde\x00\x10\x00\x00\x01\x01\x02\x02\x03\x03\x04\x04\x05\x05\x06'
+          b'\x06\x07\x07\x08\x08\t\t\n\n\x0b\x0b\x0c\x0c\r\r\x0e\x0e'
+          b'\x0f\x0f')),
+    ]
     
-        '''
-    # Strip off /files
-    file_path = vhost_path[6:]
-    internal_path = []
+    def quat_rot_vec(q, v0):
+    q_v0 = np.array([0, v0[0], v0[1], v0[2]])
+    q_v = quat_mul(q, quat_mul(q_v0, quat_conjugate(q)))
+    v = q_v[1:]
+    return v
     
-            # Make sure calls made to mocked function were correct
-        self.assertEqual(
-            mock_setup_cert.call_args_list[0], mock.call(self.achalls[0]))
-        self.assertEqual(
-            mock_setup_cert.call_args_list[1], mock.call(self.achalls[1]))
+            row, col = self.s // self.ncol, self.s % self.ncol
+        desc = self.desc.tolist()
+        desc = [[c.decode('utf-8') for c in line] for line in desc]
+        desc[row][col] = utils.colorize(desc[row][col], 'red', highlight=True)
+        if self.lastaction is not None:
+            outfile.write('  ({})\n'.format(['Left','Down','Right','Up'][self.lastaction]))
+        else:
+            outfile.write('\n')
+        outfile.write('\n'.join(''.join(line) for line in desc)+'\n')
     
-        lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
-                 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
-                 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
-                 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
-                 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
-                 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
-                 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,
-                 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
-                 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631,
-                 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709,
-                 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
-                 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877,
-                 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967,
-                 971, 977, 983, 991, 997]
+        The purpose is to have agents optimise their exploration parameters (e.g. how far to
+    explore from previous actions) based on previous experience. Because the goal changes
+    each episode a state-value or action-value function isn't able to provide any additional
+    benefit apart from being able to tell whether to increase or decrease the next guess.
     
-        print('Calculating d that is mod inverse of e...')
-    d = cryptoMath.findModInverse(e, (p - 1) * (q - 1))
+        def step(self, action):
+        assert self.action_space.contains(action)
+        if self.np_random.rand() < self.slip:
+            action = not action  # agent slipped, reverse action taken
+        if action:  # 'backwards': go back to the beginning, get small reward
+            reward = self.small
+            self.state = 0
+        elif self.state < self.n - 1:  # 'forwards': go up along the chain
+            reward = 0
+            self.state += 1
+        else:  # 'forwards': stay at the end of the chain, collect large reward
+            reward = self.large
+        done = False
+        return self.state, reward, done, {}
     
+    # We keep the actual reraising in different modules, since the
+# reraising code uses syntax mutually exclusive to Python 2/3.
+if sys.version_info[0] < 3:
+    from .reraise_impl_py2 import reraise_impl #pylint: disable=E0401
+else:
+    from .reraise_impl_py3 import reraise_impl
     
-class QuadraticProbing(HashTable):
-    '''
-        Basic Hash Table example with open addressing using Quadratic Probing 
-    '''
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    	for n in xrange(m+1):
-		for k in xrange(1, m):
-			memo[n][k] += memo[n][k-1]
-			if n-k > 0:
-				memo[n][k] += memo[n-k-1][k]
-    
-    print(longestSub([4,8,7,5,1,12,2,3,9]))
-print(longestSub([9,8,7,6,5,7]))
+    # TODO: don't hardcode sizeof_int here
+def _bigint_from_bytes(bytes):
+    sizeof_int = 4
+    padding = sizeof_int - len(bytes) % sizeof_int
+    bytes += b'\0' * padding
+    int_count = int(len(bytes) / sizeof_int)
+    unpacked = struct.unpack('{}I'.format(int_count), bytes)
+    accum = 0
+    for i, val in enumerate(unpacked):
+        accum += 2 ** (sizeof_int * 8 * i) * val
+    return accum
