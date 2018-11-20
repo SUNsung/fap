60e98396c4e6ecc3a893af8cdc2b0eb1eddb43c5
SUNsung <weatherius@gmail.com>
2018-11-20 17:57:28 +0200

    Rewrite [57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb] File in: 2018-11-20 17:57:28

--- ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
+++ ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
@@ -1,246 +1,127 @@
 
         
-            # User defines a new private lane, which can't be called from the CLI
-    def private_lane(lane_name, &block)
-      UI.user_error!('You have to pass a block using 'do' for lane '#{lane_name}'. Make sure you read the docs on GitHub.') unless block
-    
-        private_constant :GA_TRACKING
-    attr_accessor :session_id
-    attr_accessor :client
-    
-        def handle_ssl_error!(e)
-      # SSL errors are very common when the Ruby or OpenSSL installation is somehow broken
-      # We want to show a nice error message to the user here
-      # We have over 20 GitHub issues just for this one error:
-      #   https://github.com/fastlane/fastlane/search?q=errno%3D0+state%3DSSLv3+read+server&type=Issues
-      suggest_ruby_reinstall(e)
-      display_user_error!(e, e.to_s)
-    end
-    
-            expect(result[1]).to start_with('security set-keychain-settings')
-        expect(result[1]).to include('-t 600')
-        expect(result[1]).to include('-l')
-        expect(result[1]).to include('-u')
-        expect(result[1]).to include('~/Library/Keychains/test.keychain')
-      end
-    
-            context 'when enabled' do
-          it 'should not raise if swiftlint completes with a non-zero exit status' do
-            allow(FastlaneCore::UI).to receive(:important)
-            expect(FastlaneCore::UI).to receive(:important).with(/fastlane will continue/)
-            # This is simulating the exception raised if the return code is non-zero
-            expect(Fastlane::Actions).to receive(:sh).and_raise('fake error')
-            expect(FastlaneCore::UI).to_not(receive(:user_error!))
-    
-      context 'as an admin' do
-    before :each do
-      login_as(users(:jane))
-    end
-    
-      it 'requires a URL or file uplaod' do
-    visit new_scenario_imports_path
-    click_on 'Start Import'
-    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
-  end
-    
-    end
-
-    
-        it 'should convert the 'escape' method correctly' do
-      expect(LiquidMigrator.convert_string('Escaped: <escape $.content.name>\nNot escaped: <$.content.name>')).to eq(
-                                    'Escaped: {{content.name | uri_escape}}\nNot escaped: {{content.name}}'
-      )
-    end
-    
-      it 'replaces invalid byte sequences in a message' do
-    log = AgentLog.new(:agent => agents(:jane_website_agent), level: 3)
-    log.message = '\u{3042}\xffA\x95'
-    expect { log.save! }.not_to raise_error
-    expect(log.message).to eq('\u{3042}<ff>A\<95>')
-  end
-    
-          it 'should not propagate to disabled Agents' do
-        Agent.async_check(agents(:bob_weather_agent).id)
-        agents(:bob_rain_notifier_agent).update_attribute :disabled, true
-        mock(Agent).async_receive(agents(:bob_rain_notifier_agent).id, anything).times(0)
-        Agent.receive!
-      end
-    
-    module Docs
-  require 'docs/core/autoload_helper'
-  extend AutoloadHelper
-    
-        def types_as_json
-      @types.values.sort! { |a, b| sort_fn(a.name, b.name) }.map(&:as_json)
-    end
-    
-        def assert_index(index)
-      i = index.is_a?(Integer) ? index : @filters.index(filter_const(index))
-      raise 'No such filter to insert: #{index}' unless i
-      i
-    end
-  end
+        task :spec => :test
+require 'rake/testtask'
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
 end
-
     
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
-      end
-    end
+    Jekyll::Deprecator.process(ARGV)
     
-            self.base_url.scheme = effective_base_url.scheme
-        self.base_url.host = effective_base_url.host
-        self.base_url.path = effective_base_url.path
-        super
-      ensure
-        self.base_url.scheme = original_scheme
-        self.base_url.host = original_host
-        self.base_url.path = original_path
-      end
-    end
+    def run_rubygem(args)
+  run_in_shell('gem', *args.strip.split(' '))
+end
     
-          max_length = if tag = str.slice!(/ \[.+\]\z/)
-        terminal_width - tag.length
+          plist_filename = if f.plist
+        f.plist_path.basename
       else
-        terminal_width
+        File.basename Dir['#{keg}/*.plist'].first
       end
+      plist_link = '#{destination}/#{plist_filename}'
+      plist_domain = f.plist_path.basename('.plist')
+      destination_path = Pathname.new File.expand_path destination
+      plist_path = destination_path/plist_filename
     
-        def relative_path_from(url)
-      self.class.parse(url).relative_path_to(self)
-    end
+        puts 'Your system is ready to brew.' unless Homebrew.failed?
   end
 end
 
     
-            doc
+          GitHub.open 'https://api.github.com/repos/#{user}/homebrew-#{repo}/git/trees/HEAD?recursive=1' do |json|
+        json['tree'].each do |object|
+          next unless object['type'] == 'blob'
+    
+        def initialize(content)
+      @content = content
+      @html = document? ? parse_as_document : parse_as_fragment
+    end
+    
+          if base == dest
+        ''
+      elsif dest.start_with? File.join(base, '')
+        url.path[(path.length)..-1]
       end
     end
-  end
-end
-
     
-            css('pre.prettyprint').each do |node|
-          node.content = node.content.strip
-          node['data-language'] = 'dart' if node['class'].include?('dart')
-          node['data-language'] = 'html' if node.content.start_with?('<')
-          node.remove_attribute('class')
+            css('h1 + code').each do |node|
+          node.before('<p></p>')
+          while node.next_element.name == 'code'
+            node.previous_element << ' '
+            node.previous_element << node.next_element
+          end
+          node.previous_element.prepend_child(node)
         end
     
-      before_action :set_filters, only: :index
-  before_action :set_filter, only: [:edit, :update, :destroy]
-    
-      def collection_presenter
-    ActivityPub::CollectionPresenter.new(
-      id: account_collection_url(@account, params[:id]),
-      type: :ordered,
-      size: @size,
-      items: @statuses
-    )
-  end
+    puts '\nUnable to find an RSS feed for the following blogs:'
+puts '==================================================='
+unavailable.each do |b|
+  puts '#{b.name} | #{b.web_url}'
 end
+puts '==================================================='
 
     
-        private
-    
-      def show
-    if subscription.valid?(params['hub.topic'])
-      @account.update(subscription_expires_at: future_expires)
-      render plain: encoded_challenge, status: 200
-    else
-      head 404
-    end
-  end
-    
-        # ghettoooooo!
-    # If we don't have any newlines..., put one there.
-    if (header.size > 0 && header !~ /\r\n/)
-      header << '\r\n'
-    end
-    
-              private
-    
-                checksum = cipher[0, 16]
-            data = cipher[16, cipher.length - 1]
-    
-              # Decodes a Rex::Proto::Kerberos::Model::KdcRequest from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-    desc 'run specs'
-task(:spec) { ruby '-S rspec spec' }
-    
-            if unmasked_token?(token)
-          compare_with_real_token token, session
-    
-          def secure_compare(a, b)
-        Rack::Utils.secure_compare(a.to_s, b.to_s)
+          # Checks whether the `for` node has a `do` keyword.
+      #
+      # @return [Boolean] whether the `for` node has a `do` keyword
+      def do?
+        loc.begin && loc.begin.is?('do')
       end
     
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
+    module RuboCop
+  module AST
+    # A node extension for `kwsplat` nodes. This will be used in place of a
+    # plain  node when the builder constructs the AST, making its methods
+    # available to all `kwsplat` nodes within RuboCop.
+    class KeywordSplatNode < Node
+      include HashElementNode
+    
+      SPREE_GEMS.each do |gem_name|
+    rm_f  '#{gem_name}/Gemfile.lock'
+    rm_rf '#{gem_name}/pkg'
+    rm_rf '#{gem_name}/spec/dummy'
+  end
+end
+    
+          def gateway_error(exception)
+        @order.errors.add(:base, exception.message)
+        invalid_resource!(@order)
+      end
+    
+              unless inventory_unit.respond_to?(can_event) &&
+              inventory_unit.send(can_event)
+            render plain: { exception: 'cannot transition to #{@event}' }.to_json,
+                   status: 200
+            false
           end
         end
-        bad_cookies.empty?
-      end
     
-          def escape(object)
-        case object
-        when Hash   then escape_hash(object)
-        when Array  then object.map { |o| escape(o) }
-        when String then escape_string(object)
-        when Tempfile then object
-        else nil
+            def show
+          respond_with(@payment)
         end
-      end
     
-    # when launched as a script, not require'd, (currently from bin/logstash and bin/logstash-plugin) the first
-# argument is the path of a Ruby file to require and a LogStash::Runner class is expected to be
-# defined and exposing the LogStash::Runner#main instance method which will be called with the current ARGV
-# currently lib/logstash/runner.rb and lib/pluginmanager/main.rb are called using this.
-if $0 == __FILE__
-  LogStash::Bundler.setup!({:without => [:build, :development]})
-  require_relative 'patches/jar_dependencies'
-    
-            if explicit_plugins.any? { |spec| filename =~ /^#{spec.name}/ }
-          FileUtils.mv(gem_file, ::File.join(explicit_path, filename))
-        else
-          FileUtils.mv(gem_file, ::File.join(dependencies_path, filename))
+            def update
+          if @property
+            authorize! :update, @property
+            @property.update_attributes(property_params)
+            respond_with(@property, status: 200, default_template: :show)
+          else
+            invalid_resource!(@property)
+          end
         end
-      end
-    end
     
-            if Utils::HttpClient.remote_file_exist?(uri)
-          PluginManager.ui.debug('Found package at: #{uri}')
-          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
-        else
-          PluginManager.ui.debug('Package not found at: #{uri}')
-          return nil
+            def index
+          authorize! :read, StockMovement
+          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@stock_movements)
         end
-      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
-        # This probably means there is a firewall in place of the proxy is not correctly configured.
-        # So lets skip this strategy but log a meaningful errors.
-        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
     
-          post_install_messages.compact.each do |message|
-        PluginManager.ui.info(message)
-      end
-    
-        puts('Unpacking #{package_file}')
-    
-        desc 'Run one single machine acceptance test'
-    task :single, :machine do |t, args|
-      ENV['LS_VAGRANT_HOST']  = args[:machine]
-      exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/**/**/*_spec.rb']]))
-    end
-  end
-end
+            def update
+          authorize! :update, taxonomy
+          if taxonomy.update_attributes(taxonomy_params)
+            respond_with(taxonomy, status: 200, default_template: :show)
+          else
+            invalid_resource!(taxonomy)
+          end
+        end
