98431aad4e779750684b9c39d49d3771e6f2b184
SUNsung <weatherius@gmail.com>
2018-11-21 01:37:09 +0200

    Rewrite [28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp] File in: 2018-11-21 01:37:08

--- cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
+++ cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
@@ -1,350 +1,111 @@
 
         
-        Status RunCppShapeInferenceImpl(
-    int graph_def_version, const string& serialized_node_def,
-    const std::vector<string>& input_serialized_shapes,
-    const std::vector<PyObject*>& input_constant_tensor_values,
-    const std::vector<string>& input_constant_tensor_as_shape_values,
-    std::vector<string>* output_tensor_shape_protos,
-    string* input_tensors_needed_out) {
-  tensorflow::NodeDef node;
-  if (!node.ParseFromString(serialized_node_def)) {
-    return errors::InvalidArgument(
-        'Error parsing node_def during cpp shape inference');
+        TEST(MovableMessageTest, MoveDifferentArenas) {
+  Arena arena1, arena2;
+    }
+    
+      // Nested enums
+  if (descriptor->enum_type_count() > 0) {
+      std::vector<std::string> enums;
+      for (int i = 0; i < descriptor->enum_type_count(); i++) {
+          enums.push_back(GetClassName(descriptor->enum_type(i)));
+      }
+      printer->Print('new[]{ typeof($enums$) }, ', 'enums', JoinStrings(enums, '), typeof('));
   }
-  DCHECK_EQ(output_tensor_shape_protos->size(), 0);
-    }
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-      // A helper function that calls the real cuBLAS function together with error
-  // handling.
-  //
-  // cublas_func:        cuBLAS function pointer.
-  // cublas_name:        cuBLAS function name.
-  // stream:             Stream to enqueue the BLAS operation onto.
-  // pointer_mode_host:  Indicate if the pointer to a scalar value is from host
-  //                     (true) or device (false).
-  // err_on_failure:     Whether to print an error if the cublas function fails.
-  // args:               Arguments of cuBLAS function.
-  template <typename FuncT, typename... Args>
-  bool DoBlasInternalImpl(FuncT cublas_func, Stream *stream,
-                          bool pointer_mode_host, bool err_on_failure,
-                          bool use_tensor_op_math, Args... args);
-    
-    namespace stream_executor {
-namespace cuda {
-    }
-    }
-    
-    template <> struct wAdd<u32>
-{
-    typedef u32 type;
-    }
-    
-    void extract4(const Size2D &size,
-              const u8 * srcBase, ptrdiff_t srcStride,
-              u8 * dstBase, ptrdiff_t dstStride,
-              u32 coi)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-#ifndef __ANDROID__
-    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
-#endif
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
-    
-    
-    {        for (; i < size.width; ++i)
-            result += src0[i] * src1[i];
-    }
-    return result;
-#else
-    (void)_size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    
-            uint32x2_t el2l = vadd_u32(vget_low_u32(el8shr01l), vget_high_u32(el8shr01l));
-        uint32x2_t el2hl = vadd_u32(vget_low_u32(el4h), vget_high_u32(el8shr01l));
-        uint32x2_t el2hh = vadd_u32(vget_low_u32(el4h), vget_high_u32(el4h));
-    
-    ChannelArguments::~ChannelArguments() {
-  grpc_core::ExecCtx exec_ctx;
-  for (auto it = args_.begin(); it != args_.end(); ++it) {
-    if (it->type == GRPC_ARG_POINTER) {
-      it->value.pointer.vtable->destroy(it->value.pointer.p);
-    }
+  else {
+      printer->Print('null, ');
   }
+    
+    SourceGeneratorBase::SourceGeneratorBase(const FileDescriptor* descriptor,
+                                         const Options *options)
+    : descriptor_(descriptor), options_(options) {
 }
     
-    
-    {}  // namespace grpc
-
-    
-    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
-  for (unsigned int i = 0; i < args_.size(); i++) {
-    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
-      return args_[i].value.string;
-    }
-  }
-  return '';
+    int ImmutableExtensionLiteGenerator::GenerateRegistrationCode(
+    io::Printer* printer) {
+  printer->Print(
+    'registry.add($scope$.$name$);\n',
+    'scope', scope_,
+    'name', UnderscoresToCamelCase(descriptor_));
+  return 7;
 }
     
-    // A CallData class will be created for every grpc call within a channel. It is
-// used to store data and methods specific to that call. CensusServerCallData is
-// thread-compatible, however typically only 1 thread should be interacting with
-// a call at a time.
-class CensusServerCallData : public CallData {
- public:
-  // Maximum size of server stats that are sent on the wire.
-  static constexpr uint32_t kMaxServerStatsLen = 16;
-    }
+    // Author: liujisi@google.com (Pherl Liu)
     
-    void ProtoServerReflectionPlugin::InitServer(grpc::ServerInitializer* si) {
-  si->RegisterService(reflection_service_);
+    // Checks if the file contains any extensions definitions (at the root or
+// nested under a message).
+bool FileContainsExtensions(const FileDescriptor* file) {
+  if (file->extension_count() > 0) {
+    return true;
+  }
+  for (int i = 0; i < file->message_type_count(); i++) {
+    if (MessageContainsExtensions(file->message_type(i))) {
+      return true;
+    }
+  }
+  return false;
 }
     
-        virtual void UpdateArguments(ChannelArguments* args) override {
-      args->SetString(name_, value_);
-    }
-    virtual void UpdatePlugins(
-        std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {}
-    
-      // Make multiple inputs so we need to compact.
-  for (int i = 0; i < 2; i++) {
-    Build(10);
-    dbi->TEST_CompactMemTable();
-    Corrupt(kTableFile, 100, 1);
-    env_.SleepForMicroseconds(100000);
-  }
-  dbi->CompactRange(nullptr, nullptr);
-    
-      void Compact(ThreadState* thread) {
-    db_->CompactRange(nullptr, nullptr);
-  }
-    
-    class DBImpl;
-    
-    TEST(FormatTest, InternalKeyShortSeparator) {
-  // When user keys are same
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foo', 99, kTypeValue)));
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foo', 101, kTypeValue)));
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foo', 100, kTypeValue)));
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foo', 100, kTypeDeletion)));
-    }
-    
-    #endif  // STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-
-    
-    #ifndef STORAGE_LEVELDB_DB_LOG_READER_H_
-#define STORAGE_LEVELDB_DB_LOG_READER_H_
-    
-    namespace leveldb {
-    }
-    
-      // Test that previous recovery produced recoverable state.
-  Open();
-  ASSERT_LE(1, NumTables());
-  ASSERT_EQ(1, NumLogs());
-  if (CanAppend()) {
-    ASSERT_EQ(new_log, FirstLogFile());
-  }
-  ASSERT_EQ('bar2', Get('foo'));
-  ASSERT_EQ('world', Get('hello'));
-  ASSERT_EQ('there', Get('hi'));
-    
-    
-    { private:
-  port::Mutex mu_;
-  ReaderState state_ GUARDED_BY(mu_);
-  port::CondVar state_cv_ GUARDED_BY(mu_);
-};
-    
-      XGBOOST_DEVICE GradientPairInternal<T> operator-(
-      const GradientPairInternal<T> &rhs) const {
-    GradientPairInternal<T> g;
-    g.grad_ = grad_ - rhs.grad_;
-    g.hess_ = hess_ - rhs.hess_;
-    return g;
-  }
-    
-    namespace xgboost {
+    /*!
+ * \brief Shape inference function to get the correct shape given source shapes.
+ * \param lhs The shape of left operand.
+ * \param rhs The shape of right operand.
+ * \param env The Environment arguments.
+ * \return The inferred result shape.
+ */
+typedef TShape (*BinaryShapeFunction)(const TShape& lhs,
+                                      const TShape& rhs,
+                                      const EnvArguments& env);
 /*!
- * \brief Learner class that does training and prediction.
- *  This is the user facing module of xgboost training.
- *  The Load/Save function corresponds to the model used in python/R.
- *  \code
- *
- *  std::unique_ptr<Learner> learner(new Learner::Create(cache_mats));
- *  learner.Configure(configs);
- *
- *  for (int iter = 0; iter < max_iter; ++iter) {
- *    learner->UpdateOneIter(iter, train_mat);
- *    LOG(INFO) << learner->EvalOneIter(iter, data_sets, data_names);
- *  }
- *
- *  \endcode
+ * \brief Gradient function that takes only output gradient and computes gradient wrt to input.
+ *  We support total gradient as a whole to make it easy to combine a few ops.
+ * \param out_grad the gradient wrt to output of the function.
+ * \param env The Environment arguments.
+ * \param lhs_grad The container to store result of lhs gradient.
+ * \param rhs_grad The container to store result of lhs gradient.
+ * \param req_lhs_grad The requirement to store the lhs_grad
+ * \param req_rhs_grad The requirement to store the rhs_grad
+ * \param ctx Runtime context to execute the function.
  */
-class Learner : public rabit::Serializable {
- public:
-  /*! \brief virtual destructor */
-  ~Learner() override = default;
-  /*!
-   * \brief set configuration from pair iterators.
-   * \param begin The beginning iterator.
-   * \param end The end iterator.
-   * \tparam PairIter iterator<std::pair<std::string, std::string> >
-   */
-  template<typename PairIter>
-  inline void Configure(PairIter begin, PairIter end);
-  /*!
-   * \brief Set the configuration of gradient boosting.
-   *  User must call configure once before InitModel and Training.
-   *
-   * \param cfg configurations on both training and model parameters.
-   */
-  virtual void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) = 0;
-  /*!
-   * \brief Initialize the model using the specified configurations via Configure.
-   *  An model have to be either Loaded or initialized before Update/Predict/Save can be called.
-   */
-  virtual void InitModel() = 0;
-  /*!
-   * \brief load model from stream
-   * \param fi input stream.
-   */
-  void Load(dmlc::Stream* fi) override = 0;
-  /*!
-   * \brief save model to stream.
-   * \param fo output stream
-   */
-  void Save(dmlc::Stream* fo) const override = 0;
-  /*!
-   * \brief update the model for one iteration
-   *  With the specified objective function.
-   * \param iter current iteration number
-   * \param train reference to the data matrix.
-   */
-  virtual void UpdateOneIter(int iter, DMatrix* train) = 0;
-  /*!
-   * \brief Do customized gradient boosting with in_gpair.
-   *  in_gair can be mutated after this call.
-   * \param iter current iteration number
-   * \param train reference to the data matrix.
-   * \param in_gpair The input gradient statistics.
-   */
-  virtual void BoostOneIter(int iter,
-                            DMatrix* train,
-                            HostDeviceVector<GradientPair>* in_gpair) = 0;
-  /*!
-   * \brief evaluate the model for specific iteration using the configured metrics.
-   * \param iter iteration number
-   * \param data_sets datasets to be evaluated.
-   * \param data_names name of each dataset
-   * \return a string corresponding to the evaluation result
-   */
-  virtual std::string EvalOneIter(int iter,
-                                  const std::vector<DMatrix*>& data_sets,
-                                  const std::vector<std::string>& data_names) = 0;
-  /*!
-   * \brief get prediction given the model.
-   * \param data input data
-   * \param output_margin whether to only predict margin value instead of transformed prediction
-   * \param out_preds output vector that stores the prediction
-   * \param ntree_limit limit number of trees used for boosted tree
-   *   predictor, when it equals 0, this means we are using all the trees
-   * \param pred_leaf whether to only predict the leaf index of each tree in a boosted tree predictor
-   * \param pred_contribs whether to only predict the feature contributions
-   * \param approx_contribs whether to approximate the feature contributions for speed
-   * \param pred_interactions whether to compute the feature pair contributions
-   */
-  virtual void Predict(DMatrix* data,
-                       bool output_margin,
-                       HostDeviceVector<bst_float> *out_preds,
-                       unsigned ntree_limit = 0,
-                       bool pred_leaf = false,
-                       bool pred_contribs = false,
-                       bool approx_contribs = false,
-                       bool pred_interactions = false) const = 0;
-    }
-    }
-    
-    /*!
- * \brief Registry entry for linear updater.
+typedef void (*BinaryGradFunctionT0)(const OutputGrad& out_grad,
+                                     const EnvArguments& env,
+                                     TBlob* lhs_grad,
+                                     TBlob* rhs_grad,
+                                     OpReqType req_lhs_grad,
+                                     OpReqType req_rhs_grad,
+                                     RunContext ctx);
+/*!
+ * \brief Gradient function that takes inputs of function anod computes gradient wrt to input.
+ * \param out_grad the gradient wrt to output of the function.
+ * \param lhs The left operand to the function.
+ * \param rhs The right operand to the function.
+ * \param env The Environment arguments.
+ * \param lhs_grad The container to store result of lhs gradient.
+ * \param rhs_grad The container to store result of lhs gradient.
+ * \param req_lhs_grad The requirement to store the lhs_grad
+ * \param req_rhs_grad The requirement to store the rhs_grad
+ * \param ctx Runtime context to execute the function.
  */
-struct LinearUpdaterReg
-    : public dmlc::FunctionRegEntryBase<LinearUpdaterReg,
-                                        std::function<LinearUpdater*()> > {};
+typedef void (*BinaryGradFunctionT1)(const OutputGrad& out_grad,
+                                     const Input0& lhs,
+                                     const Input1& rhs,
+                                     const EnvArguments& env,
+                                     TBlob* lhs_grad,
+                                     TBlob* rhs_grad,
+                                     OpReqType req_lhs_grad,
+                                     OpReqType req_rhs_grad,
+                                     RunContext ctx);
     
+      Operator* CreateOperator(Context ctx) const override {
+    LOG(FATAL) << 'Not Implemented.';
+    return NULL;
+  }
     
-    {template<typename IndexType, typename DType = real_t>
-Parser<IndexType> *
-CreateDenseLibSVMParser(const std::string& path,
-                        const std::map<std::string, std::string>& args,
-                        unsigned part_index,
-                        unsigned num_parts) {
-  CHECK_NE(args.count('num_col'), 0) << 'expect num_col in dense_libsvm';
-  return new DensifyParser<IndexType>(
-            Parser<IndexType>::Create(path.c_str(), part_index, num_parts, 'libsvm'),
-           uint32_t(atoi(args.at('num_col').c_str())));
-}
-}  // namespace data
-    
-    /*!
- * \brief Input stream that support additional PeekRead
- *  operation, besides read.
- */
-class PeekableInStream : public dmlc::Stream {
- public:
-  explicit PeekableInStream(dmlc::Stream* strm)
-      : strm_(strm), buffer_ptr_(0) {}
-    }
-    
-    void ComputeBlockDCTDouble(double block[64]) {
-  TransformBlock(block, DCT1d);
-}
-    
-    // Performs in-place floating point 8x8 inverse DCT on block[0..63].
-void ComputeBlockIDCTDouble(double block[64]);
-    
-    #include 'guetzli/stats.h'
-    
-    #endif  // GUETZLI_FAST_LOG_H_
-
-    
-    // Contains information that is used in one scan.
-struct JPEGScanInfo {
-  // Parameters used for progressive scans (named the same way as in the spec):
-  //   Ss : Start of spectral band in zig-zag sequence.
-  //   Se : End of spectral band in zig-zag sequence.
-  //   Ah : Successive approximation bit position, high.
-  //   Al : Successive approximation bit position, low.
-  int Ss;
-  int Se;
-  int Ah;
-  int Al;
-  std::vector<JPEGComponentScanInfo> components;
-};
-    
-    
-    {}  // namespace guetzli
-
-    
-    // Mimic libjpeg's heuristics to guess jpeg color space.
-// Requires that the jpg has 3 components.
-bool HasYCbCrColorSpace(const JPEGData& jpg);
+    namespace mxnet {
+namespace io {
+/*! \return the parameter of default augmenter */
+std::vector<dmlc::ParamFieldInfo> ListDefaultAugParams();
+std::vector<dmlc::ParamFieldInfo> ListDefaultDetAugParams();
+}  // namespace io
+}  // namespace mxnet
+#endif  // MXNET_IO_IMAGE_AUGMENTER_H_
