4b23cc0b3d2f1b734fb071d3619e33faede5ae6a
SUNsung <weatherius@gmail.com>
2018-11-20 04:41:17 +0200

    Rewrite [22Zc2SYvqfYkg22ITVUJdHX9Fk22DcyEtMAkEB622IcI.cpp] File in: 2018-11-20 04:41:17

--- cccccc/22Zc2SYvqfYkg22ITVUJdHX9Fk22DcyEtMAkEB622IcI.cpp
+++ cccccc/22Zc2SYvqfYkg22ITVUJdHX9Fk22DcyEtMAkEB622IcI.cpp
@@ -1,211 +1,156 @@
 
         
-        // Generate param traits read methods.
-#include 'ipc/param_traits_read_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
+        #endif  // ATOM_APP_COMMAND_LINE_ARGS_H_
+
     
     
-    {}  // namespace nwapi
+    {}  // namespace atom
     
-      template<typename T> T* AddListener() {
-    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
-    if (i==listerners_.end()) {
-      T* listener_object = new T(this);
-      listerners_[T::id] = listener_object;
-      return listener_object;
-    }
-    return NULL;
+    #include 'atom/browser/api/atom_api_screen.h'
+    
+    #include <vector>
+    
+    void SavePageHandler::Destroy(download::DownloadItem* item) {
+  item->RemoveObserver(this);
+  delete this;
+}
+    
+      // Finds out the TrackableObject from the class it wraps.
+  static T* FromWrappedClass(v8::Isolate* isolate,
+                             base::SupportsUserData* wrapped) {
+    int32_t id = GetIDFromWrappedClass(wrapped);
+    if (!id)
+      return nullptr;
+    return FromWeakMapID(isolate, id);
   }
     
-    void Menu::Remove(MenuItem* menu_item, int pos) {
-  std::vector<MenuItem*>::iterator begin = menu_items.begin();
-  menu_items.erase(begin+pos);
-  gtk_container_remove(GTK_CONTAINER(menu_), menu_item->menu_item_);
-}
-    
-        /*
-        For each point `p` within `size`, do:
-        dst[p] = max(src0[p], src1[p])
-    */
-    void max(const Size2D &size,
-             const u8 *src0Base, ptrdiff_t src0Stride,
-             const u8 *src1Base, ptrdiff_t src1Stride,
-             u8 *dstBase, ptrdiff_t dstStride);
-    
-    #ifdef CAROTENE_NEON
-    
-    
-    {            v_src = vld2q_u8(src + sj + 32);
-            vst1q_u8(dst + dj + 16, v_src.val[coi]);
-        }
-#endif
-    
-    
-    {        for (; i < size.width; ++i)
-            result += src0[i] * src1[i];
+      if (!BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, task)) {
+    // Without a UI thread to post the exit task to, there aren't many
+    // options.  Raise the signal again.  The default handler will pick it up
+    // and cause an ungraceful exit.
+    RAW_LOG(WARNING, 'No UI thread, exiting ungracefully.');
+    kill(getpid(), signal);
     }
-    return result;
-#else
-    (void)_size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
     
-                    if( mask[0] == 0 )
-                {
-                    if (mask[1] != 0)
-                    {
-                        j -= 8;
-                        ptr -= 8;
-                    }
-                    continue;
-                }
+    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
+ public:
+  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
+  ~AtomJavaScriptDialogManager() override;
+    }
     
     
+    {}  // namespace atom
+
     
-    template <class T>
-struct pointer_iterator_traits
+    #endif // BITCOIN_QT_OPENURIDIALOG_H
+
+    
+        void showTab_SM(bool fShow);
+    void showTab_VM(bool fShow);
+    
+    #endif // BITCOIN_QT_TRANSACTIONDESCDIALOG_H
+
+    
+    /**
+ * An RAII-style reverse lock. Unlocks on construction and locks on destruction.
+ */
+template<typename Lock>
+class reverse_lock
 {
-   typedef std::ptrdiff_t difference_type;
-   typedef T value_type;
-   typedef T* pointer;
-   typedef T& reference;
-   typedef std::random_access_iterator_tag iterator_category;
-};
-template <class T>
-struct const_pointer_iterator_traits
-{
-   typedef std::ptrdiff_t difference_type;
-   typedef T value_type;
-   typedef const T* pointer;
-   typedef const T& reference;
-   typedef std::random_access_iterator_tag iterator_category;
-};
+public:
+    }
     
     
-    {   ~mem_block_cache()
-   {
-      while(next)
-      {
-         mem_block_node* old = next;
-         next = next->next;
-         ::operator delete(old);
-      }
-   }
-   void* get()
-   {
-#ifdef BOOST_HAS_THREADS
-      boost::static_mutex::scoped_lock g(mut);
-#endif
-     if(next)
-      {
-         mem_block_node* result = next;
-         next = next->next;
-         --cached_blocks;
-         return result;
-      }
-      return ::operator new(BOOST_REGEX_BLOCKSIZE);
-   }
-   void put(void* p)
-   {
-#ifdef BOOST_HAS_THREADS
-      boost::static_mutex::scoped_lock g(mut);
-#endif
-      if(cached_blocks >= BOOST_REGEX_MAX_CACHE_BLOCKS)
-      {
-         ::operator delete(p);
-      }
-      else
-      {
-         mem_block_node* old = static_cast<mem_block_node*>(p);
-         old->next = next;
-         next = old;
-         ++cached_blocks;
-      }
-   }
-};
-    
-    #endif   // BOOST_REGEX_MATCH_HPP
-    
-    template <class traits, class charT>
-inline std::basic_string<charT> regex_merge(const std::basic_string<charT>& s,
-                         const basic_regex<charT, traits>& e, 
-                         const charT* fmt,
-                         match_flag_type flags = match_default)
-{
-   return regex_replace(s, e, fmt, flags);
+    {    secp256k1_scalar_clear(&s);
+    return ret;
 }
     
-    template <class OutputIterator, class charT, class Traits1, class Alloc1, class Traits2>
-inline std::size_t regex_split(OutputIterator out,
-                   std::basic_string<charT, Traits1, Alloc1>& s, 
-                   const basic_regex<charT, Traits2>& e,
-                   match_flag_type flags = match_default)
+    // Verify that difficulty is 1.0 for an empty chain.
+BOOST_AUTO_TEST_CASE(get_difficulty_for_null_tip)
 {
-   return regex_split(out, s, e, flags, UINT_MAX);
+    double difficulty = GetDifficulty(nullptr);
+    RejectDifficultyMismatch(difficulty, 1.0);
 }
     
-      /*
-   * @brief a variable to keep track of the temp fs used in carving
-   *
-   * This variable represents the location in which we store all of our carved
-   * files. When a carve has completed all of the desired files, as well
-   * as the tar archive should reside in this directory
-   */
-  boost::filesystem::path carveDir_;
+        obj.clear();
+    BOOST_CHECK(obj.empty());
+    BOOST_CHECK_EQUAL(obj.size(), 0);
+    BOOST_CHECK_EQUAL(obj.getType(), UniValue::VNULL);
+    
+    const std::string LoggerConfigParserPlugin::kLoggerKey{'logger'};
     
     namespace osquery {
     }
     
-    /**
- * @brief Parser plugin for logger configurations.
- */
-class LoggerConfigParserPlugin : public ConfigParserPlugin {
- public:
-  std::vector<std::string> keys() const override {
-    return {kLoggerKey};
+      // Make sure a subscription cannot be added for a non-existent event type.
+  // Note: It normally would not make sense to create a blank subscription.
+  auto subscription = Subscription::create('FakeSubscriber');
+  auto status = EventFactory::addSubscription('FakePublisher', subscription);
+  EXPECT_FALSE(status.ok());
+    
+      for (int32_t i = 0; i < *frame_num && i < MAX_CAN_RECV_FRAME_LEN; ++i) {
+    CanFrame cf;
+    cf.id = recv_frames_[i].id;
+    cf.len = recv_frames_[i].len;
+    std::memcpy(cf.data, recv_frames_[i].data, recv_frames_[i].len);
+    frames->push_back(cf);
   }
-    }
-    
-    #include <osquery/config.h>
-#include <osquery/database.h>
-#include <osquery/registry.h>
-    
-      size_t pack_count = 0U;
-  c.packs(([&pack_count, query_attemts](const Pack& p) {
-    pack_count++;
-    // There is one pack without a discovery query.
-    EXPECT_EQ(p.getStats().total, query_attemts + 1);
-    EXPECT_EQ(p.getStats().hits, query_attemts);
-    EXPECT_EQ(p.getStats().misses, 1U);
-  }));
-    
-      /// Get row data from the processes table for a given pid.
-  virtual QueryData getProcessRow(pid_t pid) const;
     
     
     {
-    {    DistributedQueryResult result(
-        request, sql.rows(), sql.columns(), sql.getStatus());
-    addResult(result);
-  }
-  return flushCompleted();
+    {
+    {
+    {}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+    
+    /*
+TEST(HermesCanClient, send) {
+  CANCardParameter param;
+  param.set_brand(CANCardParameter::HERMES_CAN);
+  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
+  HermesCanClient hermes_can;
+  EXPECT_TRUE(hermes_can.Init(param));
+    }
+    
+    
+    {  SocketCanClientRaw socket_can_client;
+  EXPECT_TRUE(socket_can_client.Init(param));
+  EXPECT_EQ(socket_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
+  std::vector<CanFrame> frames;
+  int32_t num = 0;
+  EXPECT_EQ(socket_can_client.Send(frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  EXPECT_EQ(socket_can_client.Receive(&frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
+  CanFrame can_frame;
+  frames.push_back(can_frame);
+  EXPECT_EQ(socket_can_client.SendSingleFrame(frames),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  socket_can_client.Stop();
 }
     
-      /// The calling for beginning the thread's run loop.
-  Status run() override;
+    class MockProtocolData : public ProtocolData<::apollo::canbus::ChassisDetail> {
+ public:
+  static const int32_t ID = 0x111;
+  MockProtocolData() {}
+};
     
-    class INotifyTests : public testing::Test {
- protected:
-  void SetUp() override {
-    // INotify will use data from the config and config parsers.
-    Registry::get().registry('config_parser')->setUp();
+    namespace apollo {
+namespace drivers {
+namespace canbus {
     }
     }
+    }
     
-      // Normal subscribers will be enabled.
-  auto sub2 = std::make_shared<SubFakeEventSubscriber>();
-  EXPECT_FALSE(sub2->disabled);
+      /**
+   * @brief Transform an integer with the size of one byte to its hexadecimal
+   *        represented by a string.
+   * @param value The target integer to transform.
+   * @return Hexadecimal representing the target integer.
+   */
+  static std::string byte_to_hex(const uint8_t value);
+    
+    // data file
+DEFINE_string(sensor_conf_file, '', 'Sensor conf file');
