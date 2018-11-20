9146c9b04950e199ea5a5ab5fc90123b17a80357
SUNsung <weatherius@gmail.com>
2018-11-20 06:13:47 +0200

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-11-20 06:13:46

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,41 +1,120 @@
 
         
-        group :test do
-  gem 'omniauth-facebook'
-  gem 'omniauth-openid'
-  gem 'webrat', '0.7.3', require: false
-  gem 'mocha', '~> 1.1', require: false
+          it 'has no effect on immediate values' do
+    [nil, true, false].each do |v|
+      v.taint
+      v.tainted?.should == false
+    end
+  end
+    
+      it 'calls #to_path on second argument when passed ?d and a directory' do
+    p = mock('path')
+    p.should_receive(:to_path).and_return @dir
+    Kernel.test(?d, p)
+  end
 end
     
-      def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to after_omniauth_failure_path_for(resource_name)
-  end
-    
-      # GET /resource/password/new
-  def new
-    self.resource = resource_class.new
-  end
-    
-      # Email regex used to validate email formats. It asserts that there are no
-  # @ symbols or whitespaces in either the localpart or the domain, and that
-  # there is a single @ symbol separating the localpart and the domain.
-  mattr_accessor :email_regexp
-  @@email_regexp = /\A[^@\s]+@[^@\s]+\z/
-    
-    module Devise
-  module Controllers
-    # Those helpers are convenience methods added to ApplicationController.
-    module Helpers
-      extend ActiveSupport::Concern
-      include Devise::Controllers::SignInOut
-      include Devise::Controllers::StoreLocation
-    
-              path
+      it 'transfers control to the innermost catch block waiting for the same sympol' do
+    one = two = three = 0
+    catch :duplicate do
+      catch :duplicate do
+        catch :duplicate do
+          one = 1
+          throw :duplicate
         end
+        two = 2
+        throw :duplicate
+      end
+      three = 3
+      throw :duplicate
+    end
+    [one, two, three].should == [1, 2, 3]
+  end
+    
+        if run? && ARGV.any?
+      require 'optparse'
+      OptionParser.new { |op|
+        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
+        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
+        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
+        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
+        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
+        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
+      }.parse!(ARGV.dup)
+    end
+  end
+    
+        <div id='rack'>
+      <h3 id='env-info'>Rack ENV</h3>
+      <table class='req'>
+        <tr>
+          <th>Variable</th>
+          <th>Value</th>
+        </tr>
+         <% env.sort_by { |k, v| k.to_s }.each { |key, val| %>
+         <tr>
+           <td><%=h key %></td>
+           <td class='code'><div><%=h val %></div></td>
+         </tr>
+         <% } %>
+      </table>
+      <div class='clear'></div>
+    </div> <!-- /RACK ENV -->
+    
+          # Essentially the inverse of +mask_token+.
+      def unmask_token(masked_token)
+        # Split the token into the one-time pad and the encrypted
+        # value and decrypt it
+        token_length = masked_token.length / 2
+        one_time_pad = masked_token[0...token_length]
+        encrypted_token = masked_token[token_length..-1]
+        xor_byte_strings(one_time_pad, encrypted_token)
       end
     
-      def show
-    @status = status_finder.status
-    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
+          def session(env)
+        return env[options[:session_key]] if session? env
+        fail 'you need to set up a session middleware *before* #{self.class}'
+      end
+    
+      describe '.random_token' do
+    it 'generates a base64 encoded 32 character string' do
+      expect(Base64.strict_decode64(token).length).to eq(32)
+    end
   end
+end
+
+    
+      SPREE_GEMS.each do |gem_name|
+    rm_f  '#{gem_name}/Gemfile.lock'
+    rm_rf '#{gem_name}/pkg'
+    rm_rf '#{gem_name}/spec/dummy'
+  end
+end
+    
+            def create
+          authorize! :create, StockMovement
+          @stock_movement = scope.new(stock_movement_params)
+          if @stock_movement.save
+            respond_with(@stock_movement, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_movement)
+          end
+        end
+    
+            def update
+          authorize! :update, @store
+          if @store.update_attributes(store_params)
+            respond_with(@store, status: 200, default_template: :show)
+          else
+            invalid_resource!(@store)
+          end
+        end
+    
+            def update
+          authorize! :update, user
+          if user.update_attributes(user_params)
+            respond_with(user, status: 200, default_template: :show)
+          else
+            invalid_resource!(user)
+          end
+        end
