02f6d9e975492913756673385ceddbabc9a5b2ee
SUNsung <weatherius@gmail.com>
2018-11-21 22:58:58 +0200

    Rewrite [56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp] File in: 2018-11-21 22:58:57

--- cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
+++ cccccc/56e92EXg3qd6Y561DoNtdAd5VI56K.5IUyllCNU56OwE.cpp
@@ -1,259 +1,138 @@
 
         
-          // Gets the text streamed to this object so far as an std::string.
-  // Each '\0' character in the buffer is replaced with '\\0'.
-  //
-  // INTERNAL IMPLEMENTATION - DO NOT USE IN A USER PROGRAM.
-  std::string GetString() const;
-    
-    // ValuesIn() function allows generation of tests with parameters coming from
-// a container.
-//
-// Synopsis:
-// ValuesIn(const T (&array)[N])
-//   - returns a generator producing sequences with elements from
-//     a C-style array.
-// ValuesIn(const Container& container)
-//   - returns a generator producing sequences with elements from
-//     an STL-style container.
-// ValuesIn(Iterator begin, Iterator end)
-//   - returns a generator producing sequences with elements from
-//     a range [begin, end) defined by a pair of STL-style iterators. These
-//     iterators can also be plain C pointers.
-//
-// Please note that ValuesIn copies the values from the containers
-// passed in and keeps them to generate tests in RUN_ALL_TESTS().
-//
-// Examples:
-//
-// This instantiates tests from test case StringTest
-// each with C-string values of 'foo', 'bar', and 'baz':
-//
-// const char* strings[] = {'foo', 'bar', 'baz'};
-// INSTANTIATE_TEST_CASE_P(StringSequence, SrtingTest, ValuesIn(strings));
-//
-// This instantiates tests from test case StlStringTest
-// each with STL strings with values 'a' and 'b':
-//
-// ::std::vector< ::std::string> GetParameterStrings() {
-//   ::std::vector< ::std::string> v;
-//   v.push_back('a');
-//   v.push_back('b');
-//   return v;
-// }
-//
-// INSTANTIATE_TEST_CASE_P(CharSequence,
-//                         StlStringTest,
-//                         ValuesIn(GetParameterStrings()));
-//
-//
-// This will also instantiate tests from CharTest
-// each with parameter values 'a' and 'b':
-//
-// ::std::list<char> GetParameterChars() {
-//   ::std::list<char> list;
-//   list.push_back('a');
-//   list.push_back('b');
-//   return list;
-// }
-// ::std::list<char> l = GetParameterChars();
-// INSTANTIATE_TEST_CASE_P(CharSequence2,
-//                         CharTest,
-//                         ValuesIn(l.begin(), l.end()));
-//
-template <typename ForwardIterator>
-internal::ParamGenerator<
-  typename ::testing::internal::IteratorTraits<ForwardIterator>::value_type>
-ValuesIn(ForwardIterator begin, ForwardIterator end) {
-  typedef typename ::testing::internal::IteratorTraits<ForwardIterator>
-      ::value_type ParamType;
-  return internal::ParamGenerator<ParamType>(
-      new internal::ValuesInIteratorRangeGenerator<ParamType>(begin, end));
-}
-    
-    // A macro for testing Google Test assertions or code that's expected to
-// generate Google Test non-fatal failures.  It asserts that the given
-// statement will cause exactly one non-fatal Google Test failure with 'substr'
-// being part of the failure message.
-//
-// There are two different versions of this macro. EXPECT_NONFATAL_FAILURE only
-// affects and considers failures generated in the current thread and
-// EXPECT_NONFATAL_FAILURE_ON_ALL_THREADS does the same but for all threads.
-//
-// 'statement' is allowed to reference local variables and members of
-// the current object.
-//
-// The verification of the assertion is done correctly even when the statement
-// throws an exception or aborts the current function.
-//
-// Known restrictions:
-//   - You cannot stream a failure message to this macro.
-//
-// Note that even though the implementations of the following two
-// macros are much alike, we cannot refactor them to use a common
-// helper macro, due to some peculiarity in how the preprocessor
-// works.  If we do that, the code won't compile when the user gives
-// EXPECT_NONFATAL_FAILURE() a statement that contains a macro that
-// expands to code containing an unprotected comma.  The
-// AcceptsMacroThatExpandsToUnprotectedComma test in gtest_unittest.cc
-// catches that.
-//
-// For the same reason, we have to write
-//   if (::testing::internal::AlwaysTrue()) { statement; }
-// instead of
-//   GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement)
-// to avoid an MSVC warning on unreachable code.
-#define EXPECT_NONFATAL_FAILURE(statement, substr) \
-  do {\
-    ::testing::TestPartResultArray gtest_failures;\
-    ::testing::internal::SingleFailureChecker gtest_checker(\
-        &gtest_failures, ::testing::TestPartResult::kNonFatalFailure, \
-        (substr));\
-    {\
-      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
-          ::testing::ScopedFakeTestPartResultReporter:: \
-          INTERCEPT_ONLY_CURRENT_THREAD, &gtest_failures);\
-      if (::testing::internal::AlwaysTrue()) { statement; }\
-    }\
-  } while (::testing::internal::AlwaysFalse())
-    
-      // Returns the number of TestPartResult objects in the array.
-  int size() const;
-    
-      // Returns a pointer to the last occurence of a valid path separator in
-  // the FilePath. On Windows, for example, both '/' and '\' are valid path
-  // separators. Returns NULL if no path separator was found.
-  const char* FindLastPathSeparator() const;
-    
-    #if GTEST_OS_WINDOWS_MOBILE
-  // Windows CE does not have the 'ANSI' versions of Win32 APIs. To be
-  // able to pass strings to Win32 APIs on CE we need to convert them
-  // to 'Unicode', UTF-16.
-    
-    namespace internal {
-    }
-    
-    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
-int Factorial(int n) {
-  int result = 1;
-  for (int i = 1; i <= n; i++) {
-    result *= i;
+          /**
+   * @brief Specifies whether the layer should compute gradients w.r.t. a
+   *        parameter at a particular index given by param_id.
+   *
+   * You can safely ignore false values and always compute gradients
+   * for all parameters, but possibly with wasteful computation.
+   */
+  inline bool param_propagate_down(const int param_id) {
+    return (param_propagate_down_.size() > param_id) ?
+        param_propagate_down_[param_id] : false;
   }
+  /**
+   * @brief Sets whether the layer should compute gradients w.r.t. a
+   *        parameter at a particular index given by param_id.
+   */
+  inline void set_param_propagate_down(const int param_id, const bool value) {
+    if (param_propagate_down_.size() <= param_id) {
+      param_propagate_down_.resize(param_id + 1, true);
     }
-    
-    int main(int argc, char **argv) {
-  InitGoogleTest(&argc, argv);
-    }
-    
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-    
-    using LineNumber = uint32_t;
-using ColNumber  = uint32_t;
-using LineRange  = std::tuple<LineNumber,LineNumber>;
-    
-      if (HPHP::jit::deltaFits(imm64, HPHP::sz::word)) {
-    // immediate has only low 16 bits set, use simple load immediate
-    li(rt, static_cast<int16_t>(imm64));
-    if (imm64 & (1ULL << 15) && !(imm64 & (1ULL << 16))) {
-      // clear extended sign that should not be set
-      // (32bits number. Sets the 16th bit but not the 17th, it's not negative!)
-      clrldi(rt, rt, 48);
-      missing = kLi64Len - 2 * instr_size_in_bytes;
-    } else {
-      missing = kLi64Len - 1 * instr_size_in_bytes;
-    }
-  } else if (HPHP::jit::deltaFits(imm64, HPHP::sz::dword)) {
-    // immediate has only low 32 bits set
-    lis(rt, static_cast<int16_t>(imm64 >> 16));
-    ori(rt, rt, static_cast<int16_t>(imm64 & UINT16_MAX));
-    if (imm64 & (1ULL << 31) && !(imm64 & (1ULL << 32))) {
-      // clear extended sign
-      // (64bits number. Sets the 32th bit but not the 33th, it's not negative!)
-      clrldi(rt, rt, 32);
-      missing = kLi64Len - 3 * instr_size_in_bytes;
-    } else {
-      missing = kLi64Len - 2 * instr_size_in_bytes;
-    }
-  } else if (imm64 >> 48 == 0) {
-    // immediate has only low 48 bits set
-    lis(rt, static_cast<int16_t>(imm64 >> 32));
-    ori(rt, rt, static_cast<int16_t>((imm64 >> 16) & UINT16_MAX));
-    sldi(rt,rt,16);
-    ori(rt, rt, static_cast<int16_t>(imm64 & UINT16_MAX));
-    if (imm64 & (1ULL << 47)) {
-      // clear extended sign
-      clrldi(rt, rt, 16);
-    } else {
-      missing = kLi64Len - 4 * instr_size_in_bytes;
-    }
-  } else {
-    // load all 64 bits
-    lis(rt, static_cast<int16_t>(imm64 >> 48));
-    ori(rt, rt, static_cast<int16_t>((imm64 >> 32) & UINT16_MAX));
-    sldi(rt,rt,32);
-    oris(rt, rt, static_cast<int16_t>((imm64 >> 16) & UINT16_MAX));
-    ori(rt, rt, static_cast<int16_t>(imm64 & UINT16_MAX));
+    param_propagate_down_[param_id] = value;
   }
     
-      // generic template, for CodeAddress and Label
-  template <typename T>
-  void call(T& target, CallArg ca = CallArg::Internal) {
-    if (CallArg::Internal == ca) {
-      // tries best performance possible
-      branchAuto(target, BranchConditions::Always, LinkReg::Save, true);
-    } else {
-      // branchFar is not only smashable but also it will use r12 so an
-      // external branch can correctly set its TOC address appropriately.
-      branchFar(target, BranchConditions::Always, LinkReg::Save,
-                ImmType::TocOnly, true);
+    #include 'caffe/layers/neuron_layer.hpp'
+    
+    
+    {  Blob<Dtype> col_buffer_;
+  Blob<Dtype> bias_multiplier_;
+};
+    
+      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #endif  // CAFFE_BNLL_LAYER_HPP_
+
+    
+    /**
+ * @brief Takes at least two Blob%s and concatenates them along either the num
+ *        or channel dimension, outputting the result.
+ */
+template <typename Dtype>
+class ConcatLayer : public Layer<Dtype> {
+ public:
+  explicit ConcatLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     }
-    callEpilogue(ca);
-  }
     
-    inline APCLocalArray::APCLocalArray(const APCArray* source)
-  : ArrayData(kApcKind)
-  , m_arr(source)
-{
-  m_size = m_arr->size();
-  source->reference();
-  tl_heap->addApcArray(this);
-  memset(localCache(), static_cast<data_type_t>(KindOfUninit),
-         m_size * sizeof(TypedValue));
-  assertx(hasExactlyOneRef());
-}
-    
-    #ifndef HPHP_DATA_STREAM_WRAPPER_H
-#define HPHP_DATA_STREAM_WRAPPER_H
-    
-      // a silent exception does not have its exception message logged
-  bool isSilent() const { return m_silent; }
-  void setSilent(bool s = true) { m_silent = s; }
-  void recomputeBacktraceFromWH(c_WaitableWaitHandle* wh);
-    
-      assertx(chunk_size > 0);
+     protected:
+  /// @copydoc ContrastiveLossLayer
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     
     
-    {  fflush(stdout);
-  // Restores the text color.
-  SetConsoleTextAttribute(stdout_handle, old_color_attrs);
-#else
-  const char* color_code = GetPlatformColorCode(color);
-  if (color_code) out << FormatString('\033[0;3%sm', color_code);
-  out << FormatString(fmt, args) << '\033[m';
-#endif
-}
+    {
+    {  // Recursive copy function: this is similar to crop_copy() but loops over all
+  // but the last two dimensions to allow for ND cropping while still relying on
+  // a CUDA kernel for the innermost two dimensions for performance reasons.  An
+  // alterantive implementation could rely on the kernel more by passing
+  // offsets, but this is problematic because of its variable length.
+  // Since in the standard (N,C,W,H) case N,C are usually not cropped a speedup
+  // could be achieved by not looping the application of the copy_kernel around
+  // these dimensions.
+  void crop_copy_gpu(const vector<Blob<Dtype>*>& bottom,
+                const vector<Blob<Dtype>*>& top,
+                const vector<int>& offsets,
+                vector<int> indices,
+                int cur_dim,
+                const Dtype* src_data,
+                Dtype* dest_data,
+                bool is_forward);
+};
+}  // namespace caffe
     
-    // Parses a bool/Int32/string from the environment variable
-// corresponding to the given Google Test flag.
-bool BoolFromEnv(const char* flag, bool default_val);
-int32_t Int32FromEnv(const char* flag, int32_t default_val);
-double DoubleFromEnv(const char* flag, double default_val);
-const char* StringFromEnv(const char* flag, const char* default_val);
+      bool handles_setup_;
+  cudnnHandle_t* handle_;
+  cudaStream_t*  stream_;
     
-    #include 'string_util.h'
-#include 'timers.h'
-    
-    namespace benchmark {
+    #ifdef USE_CUDNN
+/**
+ * @brief cuDNN implementation of SoftmaxLayer.
+ *        Fallback to SoftmaxLayer for CPU mode.
+ */
+template <typename Dtype>
+class CuDNNSoftmaxLayer : public SoftmaxLayer<Dtype> {
+ public:
+  explicit CuDNNSoftmaxLayer(const LayerParameter& param)
+      : SoftmaxLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNSoftmaxLayer();
     }
+    
+    #include <condition_variable>
+#include <mutex>
+#include <string>
+#include <system_error>
+#include <vector>
+    
+    Github Links:
+    
+                if (smoothedGradientValue->GetDataType() != checkpointedValue.GetDataType())
+                LogicError('DataType of the smoothed gradient value restored from checkpoint for the parameter '%S' (uid = %ls) does not match the expected value.',
+                            parameter.AsString().c_str(), uid.c_str());
+    
+    
+    {        return CreateMask(sequenceLengths, sequenceStartFlags, device);
+    }
+    
+            NDShape m_shape;
+        VariableKind m_varKind;
+        ::CNTK::DataType m_dataType;
+        std::weak_ptr<Function> m_ownerFunction;
+        std::unique_ptr<std::once_flag> m_initValueFlag;
+        NDArrayViewPtr m_value;
+        std::unique_ptr<ParameterInitializer> m_valueInitializer;
+        std::unique_ptr<DeviceDescriptor> m_valueInitializationDevice;
+        bool m_needsGradient;
+        std::wstring m_name;
+        std::vector<Axis> m_dynamicAxes;
+        bool m_isSparse;
+        std::wstring m_uid;
+        std::atomic<size_t> m_valueTimeStamp;
+        Variable m_blockFunctionVariableMapping;
