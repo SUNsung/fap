d4b5b8f8560f11aa1c49dabf99ed381884d1cc14
SUNsung <weatherius@gmail.com>
2018-11-21 09:45:33 +0200

    Rewrite [03BG5JZaIwY0M03K5.zc2I0jbo030ngMDl8Aong03M3D.cpp] File in: 2018-11-21 09:45:32

--- cccccc/03BG5JZaIwY0M03K5.zc2I0jbo030ngMDl8Aong03M3D.cpp
+++ cccccc/03BG5JZaIwY0M03K5.zc2I0jbo030ngMDl8Aong03M3D.cpp
@@ -1,108 +1,218 @@
 
         
-        bool IsUrlArg(const base::CommandLine::CharType* arg) {
-  // the first character must be a letter for this to be a URL
-  auto c = *arg;
-  if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
-    for (auto* p = arg + 1; *p; ++p) {
-      c = *p;
+        #ifndef ATOM_BROWSER_API_ATOM_API_AUTO_UPDATER_H_
+#define ATOM_BROWSER_API_ATOM_API_AUTO_UPDATER_H_
+    
+      // The window used for processing events.
+  HWND window_;
+#endif
+    
+    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_common_screen, Initialize)
+
+    
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
+    
+      // Finds out the TrackableObject from the class it wraps.
+  static T* FromWrappedClass(v8::Isolate* isolate,
+                             base::SupportsUserData* wrapped) {
+    int32_t id = GetIDFromWrappedClass(wrapped);
+    if (!id)
+      return nullptr;
+    return FromWeakMapID(isolate, id);
+  }
+    
+      // This is a substitution function from the generic parameters of the
+  // conforming type to the synthetic environment.
+  //
+  // For structs, enums and protocols, this is a 1:1 mapping; for classes,
+  // we increase the depth of each generic parameter by 1 so that we can
+  // introduce a class-bound 'Self' parameter.
+  //
+  // This is a raw function rather than a substitution map because we need to
+  // keep generic parameters as generic, even if the conformanceSig (the best
+  // way to create the substitution map) equates them to concrete types.
+  auto conformanceToSyntheticTypeFn = [&](SubstitutableType *type) {
+    auto *genericParam = cast<GenericTypeParamType>(type);
+    if (covariantSelf) {
+      return GenericTypeParamType::get(genericParam->getDepth() + 1,
+                                       genericParam->getIndex(), ctx);
     }
     }
-    }
     
-    #if defined(OS_MACOSX)
-// static
-mate::Handle<InAppPurchase> InAppPurchase::Create(v8::Isolate* isolate) {
-  return mate::CreateHandle(isolate, new InAppPurchase(isolate));
-}
-    
-    
-    {}  // namespace api
-    
-     protected:
-  WebRequest(v8::Isolate* isolate, AtomBrowserContext* browser_context);
-  ~WebRequest() override;
-    
-    bool Event::SendReply(const base::ListValue& result) {
-  if (message_ == nullptr || sender_ == nullptr)
+    bool swift::parseASTSection(SerializedModuleLoader *SML, StringRef buf,
+                            SmallVectorImpl<std::string> &foundModules) {
+  if (!serialization::isSerializedAST(buf))
     return false;
     }
     
-    void PreventDefault(mate::Arguments* args) {
-  mate::Dictionary self(args->isolate(), args->GetThis());
-  self.Set('defaultPrevented', true);
+      auto mirrorIter = mirroredBuffers.find(buffer);
+  if (mirrorIter != mirroredBuffers.end()) {
+    mirrorID = mirrorIter->second;
+  } else {
+    std::unique_ptr<llvm::MemoryBuffer> mirrorBuffer{
+      llvm::MemoryBuffer::getMemBuffer(buffer->getBuffer(),
+                                       buffer->getBufferIdentifier(),
+                                       /*RequiresNullTerminator=*/true)
+    };
+    mirrorID = swiftSrcMgr.addNewSourceBuffer(std::move(mirrorBuffer));
+    mirroredBuffers[buffer] = mirrorID;
+  }
+  loc = swiftSrcMgr.getLocForOffset(mirrorID, decomposedLoc.second);
+    
+    
+    {  // Moving messages on two different arenas should lead to a copy.
+  *message2_on_arena = std::move(*message1_on_arena);
+  EXPECT_NE(nested, &message2_on_arena->optional_nested_message());
+  TestUtil::ExpectAllFieldsSet(*message1_on_arena);
+  TestUtil::ExpectAllFieldsSet(*message2_on_arena);
 }
     
-      // Returns a closure that can destroy the native class.
-  base::OnceClosure GetDestroyClosure();
+    void SourceGeneratorBase::WriteGeneratedCodeAttributes(io::Printer* printer) {
+  printer->Print('[global::System.Diagnostics.DebuggerNonUserCodeAttribute]\n');
+}
     
-    EaseBezierAction* EaseBezierAction::clone() const
-{
-    // no copy constructor
-    if (_inner)
+    static void WriteDocCommentBodyForLocation(
+    io::Printer* printer, const SourceLocation& location) {
+  string comments = location.leading_comments.empty() ?
+      location.trailing_comments : location.leading_comments;
+  if (!comments.empty()) {
+    // TODO(kenton):  Ideally we should parse the comment text as Markdown and
+    //   write it back as HTML, but this requires a Markdown parser.  For now
+    //   we just use <pre> to get fixed-width text formatting.
+    }
+    }
+    
+    void ImmutableMapFieldGenerator::
+GenerateHashCode(io::Printer* printer) const {
+  printer->Print(
+      variables_,
+      'if (!internalGet$capitalized_name$().getMap().isEmpty()) {\n'
+      '  hash = (37 * hash) + $constant_name$;\n'
+      '  hash = (53 * hash) + internalGet$capitalized_name$().hashCode();\n'
+      '}\n');
+}
+    
+    
     {
-        auto ret = EaseBezierAction::create(_inner->clone());
-        if (ret)
-        {
-            ret->setBezierParamer(_p0,_p1,_p2,_p3);
-        }
-        return ret;
-    }
-    }
-    
-        /** Removes all actions matching at least one bit in flags and the target.
-     *
-     * @param flags     The flag field to match the actions' flags based on bitwise AND.
-     * @param target    A certain target.
-     * @js NA
-     */
-    virtual void removeActionsByFlags(unsigned int flags, Node *target);
-    
-    // AtlasNode - RGBA protocol
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-    
-    typedef void (*Transform1d)(const double* in, int stride, double* out);
-    
-          // The sentinel node becomes the parent node.
-      size_t j_end = 2 * n - k;
-      tree[j_end].total_count_ =
-          tree[left].total_count_ + tree[right].total_count_;
-      tree[j_end].index_left_ = static_cast<int16_t>(left);
-      tree[j_end].index_right_or_value_ = static_cast<int16_t>(right);
-    
-      tmp0 = in[6 * stride];
-  tmp1 = kIDCTMatrix[ 6] * tmp0;
-  tmp2 = kIDCTMatrix[14] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] -= tmp2;
-  out[3] -= tmp1;
-  out[4] -= tmp1;
-  out[5] -= tmp2;
-  out[6] += tmp2;
-  out[7] += tmp1;
-    
-        static BOOST_FORCEINLINE storage_type load(storage_type const volatile& storage, memory_order order) BOOST_NOEXCEPT
     {
-        storage_type v = BOOST_ATOMIC_DETAIL_ARM_LOAD16(&storage);
-        base_type::fence_after_load(order);
-        return v;
-    }
-    
-        struct aligned
     {
-        type value;
+    {}  // namespace java
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
+    
+    #include <iostream>
+#include <set>
+    
+    #include 'caffe/layers/neuron_layer.hpp'
+    
+    
+    {}  // namespace caffe
+    
+     protected:
+  /**
+   * @param bottom input Blob vector (length 2+)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_1 @f$
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_2 @f$
+   *   -# ...
+   *   - K @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x_K @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (KN \times C \times H \times W) @f$ if axis == 0, or
+   *      @f$ (N \times KC \times H \times W) @f$ if axis == 1:
+   *      the concatenated output @f$
+   *        y = [\begin{array}{cccc} x_1 & x_2 & ... & x_K \end{array}]
+   *      @f$
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    
+    #ifdef USE_CUDNN
+template <typename Dtype>
+class CuDNNLCNLayer : public LRNLayer<Dtype> {
+ public:
+  explicit CuDNNLCNLayer(const LayerParameter& param)
+      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
+        tempData1(NULL), tempData2(NULL) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNLCNLayer();
     }
     
-    /*
- * IMPLEMENTATION NOTE: All interface functions MUST be declared with BOOST_FORCEINLINE,
- *                      see comment for convert_memory_order_to_gcc in ops_gcc_atomic.hpp.
- */
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include <vector>
+    
+    
+    {  ~TemporaryFile() {
+    unlink(path.c_str());
+  }
+};
+    
+    Status DBImpl::NewDB() {
+  VersionEdit new_db;
+  new_db.SetComparatorName(user_comparator()->Name());
+  new_db.SetLogNumber(0);
+  new_db.SetNextFile(2);
+  new_db.SetLastSequence(0);
+    }
+    
+    static void TestKey(const std::string& key,
+                    uint64_t seq,
+                    ValueType vt) {
+  std::string encoded = IKey(key, seq, vt);
+    }
+    
+    // Called on every item found in a WriteBatch.
+class WriteBatchItemPrinter : public WriteBatch::Handler {
+ public:
+  WritableFile* dst_;
+  virtual void Put(const Slice& key, const Slice& value) {
+    std::string r = '  put '';
+    AppendEscapedStringTo(&r, key);
+    r += '' '';
+    AppendEscapedStringTo(&r, value);
+    r += ''\n';
+    dst_->Append(r);
+  }
+  virtual void Delete(const Slice& key) {
+    std::string r = '  del '';
+    AppendEscapedStringTo(&r, key);
+    r += ''\n';
+    dst_->Append(r);
+  }
+};
+    
+    class SequentialFile;
+    
+    
+    {  ASSERT_EQ('correct', Read());
+  ASSERT_EQ('EOF', Read());
+  const size_t dropped = DroppedBytes();
+  ASSERT_LE(dropped, 2*kBlockSize + 100);
+  ASSERT_GE(dropped, 2*kBlockSize);
+}
+    
+    
+    {  // No copying allowed
+  MemTableIterator(const MemTableIterator&);
+  void operator=(const MemTableIterator&);
+};
+    
+      int NumLogs() {
+    return GetFiles(kLogFile).size();
+  }
+    
+    #endif  // STORAGE_LEVELDB_DB_SKIPLIST_H_
