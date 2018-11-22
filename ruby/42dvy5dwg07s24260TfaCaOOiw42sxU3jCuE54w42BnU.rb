bcdcd06380e4459e40ba8584bdba2148e07f1c59
SUNsung <weatherius@gmail.com>
2018-11-22 13:08:28 +0200

    Rewrite [42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb] File in: 2018-11-22 13:08:27

--- ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
+++ ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
@@ -1,56 +1,33 @@
 
         
-        def converted_history(markdown)
-  remove_head_from_history(
-    custom_release_header_anchors(
-      liquid_escape(
-        linkify(
-          normalize_bullets(markdown)
-        )
-      )
-    )
-  )
-end
+        DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
+COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
     
-    FORWARD_SLASH = '/'.freeze
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-    def graceful_require
-  Jekyll::External.require_with_graceful_fail('json')
-  JSON.pretty_generate(DATA)
-end
+    Mercenary.program(:jekyll) do |p|
+  p.version Jekyll::VERSION
+  p.description 'Jekyll is a blog-aware, static site generator in Ruby'
+  p.syntax 'jekyll <subcommand> [options]'
     
-    def all_steps_to_path(path)
-  source = source_dir
-  dest = Pathname.new(path).expand_path
-  paths = []
-    
-      all_files       = `git ls-files -z`.split('\x0')
-  s.files         = all_files.grep(%r!^(exe|lib|rubocop)/|^.rubocop.yml$!)
-  s.executables   = all_files.grep(%r!^exe/!) { |f| File.basename(f) }
-  s.bindir        = 'exe'
-  s.require_paths = ['lib']
-    
-        def arg_is_present?(args, deprecated_argument, message)
-      deprecation_message(message) if args.include?(deprecated_argument)
-    end
-    
-        it 'defauls foreground and background colors' do
-      scenario.tag_fg_color = nil
-      scenario.tag_bg_color = nil
-      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
-    end
-  end
-    
-          describe '#import' do
-        it 'should import' do
-          services_scenario_import.merges = {
-            '0' => {
-              'service_id' => '0',
-            }
-          }
-          expect {
-            expect(services_scenario_import.import).to eq(true)
-          }.to change { users(:bob).agents.count }.by(2)
+            # attributes - A Hash containing the raw lfs_object details. The keys of this
+        #              Hash must be Symbols.
+        def initialize(attributes)
+          @attributes = attributes
         end
       end
     end
@@ -58,85 +35,93 @@ end
 end
 
     
-      def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to after_omniauth_failure_path_for(resource_name)
-  end
-    
-      # Defines which strategy can be used to unlock an account.
-  # Values: :email, :time, :both
-  mattr_accessor :unlock_strategy
-  @@unlock_strategy = :both
-    
-    # Declares a dependency to the git repo of CocoaPods gem. This declaration is
-# compatible with the local git repos feature of Bundler.
-#
-def cp_gem(name, repo_name, branch = 'master', path: false)
-  return gem name if SKIP_UNRELEASED_VERSIONS
-  opts = if path
-           { :path => '../#{repo_name}' }
-         else
-           url = 'https://github.com/CocoaPods/#{repo_name}.git'
-           { :git => url, :branch => branch }
-         end
-  gem name, opts
-end
-    
-    # This bin wrapper runs the `pod` command in a OS X sandbox. The reason for this
-# is to ensure that people can’t use malicious code from pod specifications.
-#
-# It does this by creating a ‘seatbelt’ profile on the fly and executing the
-# given command through `/usr/bin/sandbox-exec`. This profile format is an
-# undocumented format, which uses TinyScheme to implement its DSL.
-#
-# Even though it uses a undocumented format, it’s actually very self-explanatory.
-# Because we use a whitelist approach, `(deny default)`, any action that is
-# denied is logged to `/var/log/system.log`. So tailing that should provide
-# enough information on steps that need to be take to get something to work.
-#
-# For more information see:
-#
-# * https://github.com/CocoaPods/CocoaPods/issues/939
-# * http://reverse.put.as/wp-content/uploads/2011/08/The-Apple-Sandbox-BHDC2011-Slides.pdf
-# * http://reverse.put.as/wp-content/uploads/2011/08/The-Apple-Sandbox-BHDC2011-Paper.pdf
-# * https://github.com/s7ephen/OSX-Sandbox--Seatbelt--Profiles
-# * `$ man sandbox-exec`
-# * `$ ls /usr/share/sandbox`
-    
-    module Pod
-  class Command
-    class Env < Command
-      self.summary = 'Display pod environment'
-      self.description = 'Display pod environment.'
-    
-      context 'when arguments to a method' do
-    let(:prefix) { 'bar(' }
-    let(:suffix) { ')' }
-    let(:source) { construct(false, true) }
-    
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
+        if self.junk_headers
+      while str.length < 4096
+        if self.fold
+          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}:\r\n\t#{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
+        else
+          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}: #{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
+        end
       end
+    end
     
-          # Calls the given block for each `pair` node in the `hash` literal.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_pair
-        return each_child_node(:pair).to_enum unless block_given?
+              # Decodes the Rex::Proto::Kerberos::Model::Element from the input. This
+          # method has been designed to be overridden by subclasses.
+          #
+          # @raise [NoMethodError]
+          def decode(input)
+            raise ::NoMethodError, 'Method designed to be overridden'
+          end
     
-          # Checks whether this node is an `if` statement. (This is not true of
-      # ternary operators and `unless` statements.)
-      #
-      # @return [Boolean] whether the node is an `if` statement
-      def if?
-        keyword == 'if'
-      end
+              # Decodes a Rex::Proto::Kerberos::Model::KrbError
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode_asn1(input)
+            input.value[0].value.each do |val|
+              case val.tag
+              when 0
+                self.pvno = decode_pvno(val)
+              when 1
+                self.msg_type = decode_msg_type(val)
+              when 2
+                self.ctime = decode_ctime(val)
+              when 3
+                self.cusec = decode_cusec(val)
+              when 4
+                self.stime = decode_stime(val)
+              when 5
+                self.susec = decode_susec(val)
+              when 6
+                self.error_code = decode_error_code(val)
+              when 7
+                self.crealm = decode_crealm(val)
+              when 8
+                self.cname = decode_cname(val)
+              when 9
+                self.realm = decode_realm(val)
+              when 10
+                self.sname = decode_sname(val)
+              when 12
+                self.e_data = decode_e_data(val)
+              else
+                raise ::RuntimeError, 'Failed to decode KRB-ERROR SEQUENCE'
+              end
+            end
+          end
+    
+            def find_order
+          @order = Spree::Order.find_by!(number: order_id)
+          authorize! :read, @order, order_token
+        end
+    
+              @properties = @properties.page(params[:page]).per(params[:per_page])
+          respond_with(@properties)
+        end
+    
+            def create
+          authorize! :create, Store
+          @store = Store.new(store_params)
+          @store.code = params[:store][:code]
+          if @store.save
+            respond_with(@store, status: 201, default_template: :show)
+          else
+            invalid_resource!(@store)
+          end
+        end
+    
+            def create
+          authorize! :create, Taxonomy
+          @taxonomy = Taxonomy.new(taxonomy_params)
+          if @taxonomy.save
+            respond_with(@taxonomy, status: 201, default_template: :show)
+          else
+            invalid_resource!(@taxonomy)
+          end
+        end
+    
+            def destroy
+          authorize! :destroy, taxon
+          taxon.destroy
+          respond_with(taxon, status: 204)
+        end
