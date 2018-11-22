195574c7e06f6419e3753cda12b9376f00acdedb
SUNsung <weatherius@gmail.com>
2018-11-22 23:03:49 +0200

    Rewrite [55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py] File in: 2018-11-22 23:03:49

--- pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
+++ pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
@@ -1,202 +1,83 @@
 
         
-        - name: obtain all facts for a single WAF
-  aws_waf_facts:
-    name: test_waf
-'''
+        from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
     
-    - cloudwatchevent_rule:
-    name: MyDisabledCronTask
-    schedule_expression: 'rate(5 minutes)'
-    description: Run my disabled scheduled task
-    state: disabled
-    targets:
-      - id: MyOtherTargetId
-        arn: arn:aws:lambda:us-east-1:123456789012:function:MyFunction
-        input: '{'foo': 'bar'}'
+    Does two benchmarks
+    
+        formats : array-like of callables (1d or 0d)
+        These may transform a dense indicator matrix into multilabel
+        representation.
     
     
-DOCUMENTATION = '''
----
-module: ec2_customer_gateway
-short_description: Manage an AWS customer gateway
-description:
-    - Manage an AWS customer gateway
-version_added: '2.2'
-author: Michael Baydoun (@MichaelBaydoun)
-requirements: [ botocore, boto3 ]
-notes:
-    - You cannot create more than one customer gateway with the same IP address. If you run an identical request more than one time, the
-      first request creates the customer gateway, and subsequent requests return information about the existing customer gateway. The subsequent
-      requests do not create new customer gateway resources.
-    - Return values contain customer_gateway and customer_gateways keys which are identical dicts. You should use
-      customer_gateway. See U(https://github.com/ansible/ansible-modules-extras/issues/2773) for details.
-options:
-  bgp_asn:
-    description:
-      - Border Gateway Protocol (BGP) Autonomous System Number (ASN), required when state=present.
-  ip_address:
-    description:
-      - Internet-routable IP address for customers gateway, must be a static address.
-    required: true
-  name:
-    description:
-      - Name of the customer gateway.
-    required: true
-  routing:
-    description:
-      - The type of routing.
-    choices: ['static', 'dynamic']
-    default: dynamic
-    version_added: '2.4'
-  state:
-    description:
-      - Create or terminate the Customer Gateway.
-    default: present
-    choices: [ 'present', 'absent' ]
-extends_documentation_fragment:
-    - aws
-    - ec2
-'''
+def benchmark(estimator, data):
+    gc.collect()
+    print('Benching %s' % estimator)
+    t0 = time()
+    estimator.fit(data)
+    training_time = time() - t0
+    data_t = estimator.transform(data)
+    data_r = estimator.inverse_transform(data_t)
+    reconstruction_error = np.mean(np.abs(data - data_r))
+    return {'time': training_time, 'error': reconstruction_error}
     
-    
-if __name__ == '__main__':
-    main()
+            ax.set_xlabel('n_samples')
+        ax.set_ylabel('n_features')
+        ax.set_zlabel('Time (s)')
+        ax.set_zlim3d(0.0, max_time * 1.1)
+        ax.set_title(label)
+        # ax.legend()
+        i += 1
+    plt.show()
 
     
-        create_snapshot(
-        module=module,
-        state=state,
-        description=description,
-        wait=wait,
-        wait_timeout=wait_timeout,
-        ec2=ec2,
-        volume_id=volume_id,
-        instance_id=instance_id,
-        snapshot_id=snapshot_id,
-        device_name=device_name,
-        snapshot_tags=snapshot_tags,
-        last_snapshot_min_age=last_snapshot_min_age
-    )
+            start = time.time()
+        func(X, n_jobs=-1)
+        multi_core.append(time.time() - start)
     
-        for eigw in response.get('EgressOnlyInternetGateways', []):
-        for attachment in eigw.get('Attachments', []):
-            if attachment.get('VpcId') == vpc_id and attachment.get('State') in ('attached', 'attaching'):
-                gateway_id = eigw.get('EgressOnlyInternetGatewayId')
+    import six
     
-        show_details = module.params.get('details')
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
     
-        Basic Usage:
-        >>> client = boto3.client('kinesis')
-        >>> stream_name = 'test-stream'
-        >>> number_of_shards = 10
-        >>> tags = {'env': 'test'}
-        >>> create_stream(client, stream_name, number_of_shards, tags=tags)
+    n_samples, n_features = 5000, 300
+X = np.random.randn(n_samples, n_features)
+inds = np.arange(n_samples)
+np.random.shuffle(inds)
+X[inds[int(n_features / 1.2):]] = 0  # sparsify input
+print('input data sparsity: %f' % sparsity_ratio(X))
+coef = 3 * np.random.randn(n_features)
+inds = np.arange(n_features)
+np.random.shuffle(inds)
+coef[inds[n_features // 2:]] = 0  # sparsify coef
+print('true coef sparsity: %f' % sparsity_ratio(coef))
+y = np.dot(X, coef)
     
-        :param client: AWS API client reference (boto3)
-    :param module: Ansible module reference
-    :return dict:
+        mem_usage = memory_usage(run_vectorizer(Vectorizer, text, **params))
+    
+        An example with a long-untouched module that everyone has
+    >>> _linkcode_resolve('py', {'module': 'tty',
+    ...                          'fullname': 'setraw'},
+    ...                   package='tty',
+    ...                   url_fmt='http://hg.python.org/cpython/file/'
+    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
+    ...                   revision='xxxx')
+    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
     '''
     
-        for node in doctree.traverse(settingslist_node):
-        settings_list = nodes.bullet_list()
-        settings_list.extend([make_setting_element(d, app, fromdocname)
-                              for d in sorted(env.scrapy_all_settings,
-                                              key=itemgetter('setting_name'))
-                              if fromdocname != d['docname']])
-        node.replace_self(settings_list)
     
-            infos = []
-        if not self.wasSuccessful():
-            write('FAILED')
-            failed, errored = map(len, (self.failures, self.errors))
-            if failed:
-                infos.append('failures=%d' % failed)
-            if errored:
-                infos.append('errors=%d' % errored)
-        else:
-            write('OK')
-    
-        requires_project = True
-    
-    def get_extension(link):
-    extension = os.path.splitext(link)[1][1:]
-    if extension in ['pdf', 'html']:
-        return extension
-    if 'pdf' in extension:
-        return 'pdf'    
-    return 'pdf'    
-    
-            for release_project in RangeQuerySetWrapperWithProgressBar(
-            orm.ReleaseProject.objects.all()
-        ):
-            orm.ReleaseProject.objects.filter(id=release_project.id).update(
-                new_groups=orm.Group.objects.filter(
-                    project_id=release_project.project_id,
-                    first_release_id=release_project.release_id,
-                ).count()
-            )
-    
-            # Adding field 'ApiToken.scope_list'
-        db.add_column(
-            'sentry_apitoken',
-            'scope_list',
-            self.gf('sentry.db.models.fields.array.ArrayField')(
-                of=('django.db.models.fields.TextField', [], {})
-            ),
-            keep_default=False
-        )
-    
-            # Adding model 'DSymApp'
-        db.create_table(
-            'sentry_dsymapp', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.Project']
-                    )
-                ), ('app_id', self.gf('django.db.models.fields.CharField')(max_length=64)),
-                ('sync_id', self.gf('django.db.models.fields.CharField')(max_length=64, null=True)),
-                ('data', self.gf('jsonfield.fields.JSONField')(default={})), (
-                    'platform',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(
-                        default=0
-                    )
-                ), (
-                    'last_synced',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['DSymApp'])
-    
-            # Deleting field 'ReleaseFile.dist'
-        db.delete_column('sentry_releasefile', 'dist_id')
-    
-    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-        # Load image file and find face locations
-    X_img = face_recognition.load_image_file(X_img_path)
-    X_face_locations = face_recognition.face_locations(X_img)
-    
-            # Draw a box around the face
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-    
-        # 将每一个人脸与已知样本图片比对
-    for face_encoding in face_encodings:
-        # 看是否属于奥巴马或者拜登
-        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
-        name = '<Unknown Person>'
-    
-        return score
-    
-            a += a
-        b >>= 1
+def user_role(name, rawtext, text, lineno,
+              inliner, options=None, content=None):
+    '''Sphinx role for linking to a user profile. Defaults to linking to
+    GitHub profiles, but the profile URIS can be configured via the
+    ``issues_user_uri`` config value.
