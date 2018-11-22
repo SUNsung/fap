47cf753a338d686fca8f87765a6b5b7edc573c4b
SUNsung <weatherius@gmail.com>
2018-11-22 14:19:39 +0200

    Rewrite [03pqLpWrdlAAo03HOEnAKrTqs.035MF7my1eqJM03i91.cpp] File in: 2018-11-22 14:19:39

--- cccccc/03pqLpWrdlAAo03HOEnAKrTqs.035MF7my1eqJM03i91.cpp
+++ cccccc/03pqLpWrdlAAo03HOEnAKrTqs.035MF7my1eqJM03i91.cpp
@@ -1,301 +1,191 @@
 
         
-        // Instantiate a class with float and double specifications.
-#define INSTANTIATE_CLASS(classname) \
-  char gInstantiationGuard##classname; \
-  template class classname<float>; \
-  template class classname<double>
+        #include 'base/basictypes.h'
+#include '../dispatcher_host.h'
     
-    /**
- * @brief Fills a Blob with values @f$ x \sim U(-a, +a) @f$ where @f$ a @f$ is
- *        set inversely proportional to number of incoming nodes, outgoing
- *        nodes, or their average.
- *
- * A Filler based on the paper [Bengio and Glorot 2010]: Understanding
- * the difficulty of training deep feedforward neuralnetworks.
- *
- * It fills the incoming matrix by randomly sampling uniform data from [-scale,
- * scale] where scale = sqrt(3 / n) where n is the fan_in, fan_out, or their
- * average, depending on the variance_norm option. You should make sure the
- * input blob has shape (num, a, b, c) where a * b * c = fan_in and num * b * c
- * = fan_out. Note that this is currently not the case for inner product layers.
- *
- * TODO(dox): make notation in above comment consistent with rest & use LaTeX.
- */
-template <typename Dtype>
-class XavierFiller : public Filler<Dtype> {
- public:
-  explicit XavierFiller(const FillerParameter& param)
-      : Filler<Dtype>(param) {}
-  virtual void Fill(Blob<Dtype>* blob) {
-    CHECK(blob->count());
-    int fan_in = blob->count() / blob->shape(0);
-    // Compatibility with ND blobs
-    int fan_out = blob->num_axes() > 1 ?
-                  blob->count() / blob->shape(1) :
-                  blob->count();
-    Dtype n = fan_in;  // default to fan_in
-    if (this->filler_param_.variance_norm() ==
-        FillerParameter_VarianceNorm_AVERAGE) {
-      n = (fan_in + fan_out) / Dtype(2);
-    } else if (this->filler_param_.variance_norm() ==
-        FillerParameter_VarianceNorm_FAN_OUT) {
-      n = fan_out;
-    }
-    Dtype scale = sqrt(Dtype(3) / n);
-    caffe_rng_uniform<Dtype>(blob->count(), -scale, scale,
-        blob->mutable_cpu_data());
-    CHECK_EQ(this->filler_param_.sparse(), -1)
-         << 'Sparsity not supported by this Filler.';
-  }
-};
-    
-      /** @brief Using the CPU device, compute the layer output. */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) = 0;
-  /**
-   * @brief Using the GPU device, compute the layer output.
-   *        Fall back to Forward_cpu() if unavailable.
-   */
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {
-    // LOG(WARNING) << 'Using CPU code as backup.';
-    return Forward_cpu(bottom, top);
-  }
-    
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    namespace caffe {
-    }
-    
-      // Streams a pointer value to this object.
-  //
-  // This function is an overload of the previous one.  When you
-  // stream a pointer to a Message, this definition will be used as it
-  // is more specialized.  (The C++ Standard, section
-  // [temp.func.order].)  If you stream a non-pointer, then the
-  // previous definition will be used.
-  //
-  // The reason for this overload is that streaming a NULL pointer to
-  // ostream is undefined behavior.  Depending on the compiler, you
-  // may get '0', '(nil)', '(null)', or an access violation.  To
-  // ensure consistent result across compilers, we always treat NULL
-  // as '(null)'.
-  template <typename T>
-  inline Message& operator <<(T* const& pointer) {  // NOLINT
-    if (pointer == NULL) {
-      *ss_ << '(null)';
-    } else {
-      *ss_ << pointer;
-    }
-    return *this;
-  }
-#endif  // GTEST_OS_SYMBIAN
-    
-    
-// Value-parameterized tests allow you to test your code with different
-// parameters without writing multiple copies of the same test.
-//
-// Here is how you use value-parameterized tests:
-    
-    // This flag controls whether Google Test includes Google Test internal
-// stack frames in failure stack traces.
-GTEST_DECLARE_bool_(show_internal_stack_frames);
-    
-      // # of fraction bits in a number.
-  static const size_t kFractionBitCount =
-    std::numeric_limits<RawType>::digits - 1;
-    
-    #endif  // GTEST_HAS_TYPED_TEST || GTEST_HAS_TYPED_TEST_P
-    
-      // Set should work when the input pointer is the same as the one
-  // already in the MyString object.
-  s.Set(s.c_string());
-  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
-    
-    
-    {}  // namespace grpc
-
-    
-    void CensusClientCallData::OnDoneRecvTrailingMetadataCb(void* user_data,
-                                                        grpc_error* error) {
-  grpc_call_element* elem = reinterpret_cast<grpc_call_element*>(user_data);
-  CensusClientCallData* calld =
-      reinterpret_cast<CensusClientCallData*>(elem->call_data);
-  GPR_ASSERT(calld != nullptr);
-  if (error == GRPC_ERROR_NONE) {
-    GPR_ASSERT(calld->recv_trailing_metadata_ != nullptr);
-    FilterTrailingMetadata(calld->recv_trailing_metadata_,
-                           &calld->elapsed_time_);
-  }
-  GRPC_CLOSURE_RUN(calld->initial_on_done_recv_trailing_metadata_,
-                   GRPC_ERROR_REF(error));
+    Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
 }
     
-      grpc_error* Init(grpc_call_element* elem,
-                   const grpc_call_element_args* args) override;
+    #include 'base/logging.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/api_messages.h'
+#include 'content/public/renderer/render_view.h'
+#include 'content/public/renderer/render_thread.h'
+#include 'content/public/renderer/v8_value_converter.h'
+#include 'third_party/WebKit/public/web/WebView.h'
+#include 'third_party/WebKit/public/web/WebLocalFrame.h'
+#include 'ui/base/resource/resource_bundle.h'
     
-    namespace grpc {
-    }
+    void Clipboard::SetText(std::string& text) {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  ui::Clipboard::ObjectMap map;
+  map[ui::Clipboard::CBF_TEXT].push_back(
+      std::vector<char>(text.begin(), text.end()));
+  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
+}
     
-    #include <grpc/support/cpu.h>
-    
-      // Delete everything
-  for (int i = 0; i < kCount; i++) {
-    ASSERT_OK(db_->Delete(WriteOptions(), Key(i)));
-  }
-  ASSERT_OK(dbi->TEST_CompactMemTable());
-    
-    struct Options;
-struct FileMetaData;
-    
-    
-    {}  // namespace leveldb
-    
-    
-    { private:
-  class ModelIter: public Iterator {
-   public:
-    ModelIter(const KVMap* map, bool owned)
-        : map_(map), owned_(owned), iter_(map_->end()) {
-    }
-    ~ModelIter() {
-      if (owned_) delete map_;
-    }
-    virtual bool Valid() const { return iter_ != map_->end(); }
-    virtual void SeekToFirst() { iter_ = map_->begin(); }
-    virtual void SeekToLast() {
-      if (map_->empty()) {
-        iter_ = map_->end();
-      } else {
-        iter_ = map_->find(map_->rbegin()->first);
-      }
-    }
-    virtual void Seek(const Slice& k) {
-      iter_ = map_->lower_bound(k.ToString());
-    }
-    virtual void Next() { ++iter_; }
-    virtual void Prev() { --iter_; }
-    virtual Slice key() const { return iter_->first; }
-    virtual Slice value() const { return iter_->second; }
-    virtual Status status() const { return Status::OK(); }
-   private:
-    const KVMap* const map_;
-    const bool owned_;  // Do we own map_
-    KVMap::const_iterator iter_;
-  };
-  const Options options_;
-  KVMap map_;
-};
-    
-    
-    {}  // namespace leveldb
+    #endif  // CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
 
     
-      // Successful parses
-  static struct {
-    const char* fname;
-    uint64_t number;
-    FileType type;
-  } cases[] = {
-    { '100.log',            100,   kLogFile },
-    { '0.log',              0,     kLogFile },
-    { '0.sst',              0,     kTableFile },
-    { '0.ldb',              0,     kTableFile },
-    { 'CURRENT',            0,     kCurrentFile },
-    { 'LOCK',               0,     kDBLockFile },
-    { 'MANIFEST-2',         2,     kDescriptorFile },
-    { 'MANIFEST-7',         7,     kDescriptorFile },
-    { 'LOG',                0,     kInfoLogFile },
-    { 'LOG.old',            0,     kInfoLogFile },
-    { '18446744073709551615.log', 18446744073709551615ull, kLogFile },
-  };
-  for (int i = 0; i < sizeof(cases) / sizeof(cases[0]); i++) {
-    std::string f = cases[i].fname;
-    ASSERT_TRUE(ParseFileName(f, &number, &type)) << f;
-    ASSERT_EQ(cases[i].type, type) << f;
-    ASSERT_EQ(cases[i].number, number) << f;
-  }
-    
-    
-    {  // For fragments
-  kFirstType = 2,
-  kMiddleType = 3,
-  kLastType = 4
-};
-static const int kMaxRecordType = kLastType;
-    
-      std::string LogName(uint64_t number) {
-    return LogFileName(dbname_, number);
-  }
-    
-        // Create builder.
-    std::string copy = TableFileName(dbname_, next_file_number_++);
-    WritableFile* file;
-    Status s = env_->NewWritableFile(copy, &file);
-    if (!s.ok()) {
-      return;
+    void MenuItem::OnClick(GtkWidget* widget) {
+  if (block_active_)
+    return;
     }
-    TableBuilder* builder = new TableBuilder(options_, file);
     
-    class SnapshotList;
+    namespace extensions {
+    }
+    
+    class NwObjCreateFunction : public NWSyncExtensionFunction {
+ public:
+  NwObjCreateFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    
+    {
+    {
+    {}  // namespace internal
+}  // namespace protobuf
+}  // namespace google
+
+    
+    int ImmutableExtensionLiteGenerator::GenerateRegistrationCode(
+    io::Printer* printer) {
+  printer->Print(
+    'registry.add($scope$.$name$);\n',
+    'scope', scope_,
+    'name', UnderscoresToCamelCase(descriptor_));
+  return 7;
+}
+    
+      EXPECT_EXIT(decode_data.AddString(1, '', ''),
+              ::testing::KilledBySignal(SIGABRT),
+              'error: got empty string for making TextFormat data, input:');
+  EXPECT_EXIT(decode_data.AddString(1, 'a', ''),
+              ::testing::KilledBySignal(SIGABRT),
+              'error: got empty string for making TextFormat data, input:');
+  EXPECT_EXIT(decode_data.AddString(1, '', 'a'),
+              ::testing::KilledBySignal(SIGABRT),
+              'error: got empty string for making TextFormat data, input:');
+    
+    OneofGenerator::OneofGenerator(const OneofDescriptor* descriptor)
+    : descriptor_(descriptor) {
+  variables_['enum_name'] = OneofEnumName(descriptor_);
+  variables_['name'] = OneofName(descriptor_);
+  variables_['capitalized_name'] = OneofNameCapitalized(descriptor_);
+  variables_['raw_index'] = SimpleItoa(descriptor_->index());
+  const Descriptor* msg_descriptor = descriptor_->containing_type();
+  variables_['owning_message_class'] = ClassName(msg_descriptor);
+    }
+    
+    ErrorCode SocketCanClientRaw::Start() {
+  if (is_started_) {
+    return ErrorCode::OK;
+  }
+  struct sockaddr_can addr;
+  struct ifreq ifr;
+    }
+    
+    
+    {
+    {
+    {
+    {}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+        if (buf.size() != static_cast<size_t>(frame_num)) {
+      AERROR_EVERY(100) << 'Receiver buf size [' << buf.size()
+                        << '] does not match can_client returned length['
+                        << frame_num << '].';
+    }
+    
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
+#include 'modules/drivers/canbus/can_comm/message_manager.h'
+    
+      ProtocolData<::apollo::canbus::ChassisDetail> mpd;
+  SenderMessage<::apollo::canbus::ChassisDetail> msg(1, &mpd);
+  EXPECT_FALSE(sender.NeedSend(msg, 1));
+  EXPECT_EQ(msg.message_id(), 1);
+  int32_t period = msg.curr_period();
+  msg.UpdateCurrPeriod(-50);
+  EXPECT_EQ(msg.curr_period(), period + 50);
+  EXPECT_EQ(msg.CanFrame().id, 1);
     
       /*
-   * @brief a variable tracking all of the paths we attempt to carve
-   *
-   * This is a globbed set of file paths that we're expecting will be
-   * carved.
+   * @brief parse received data
+   * @param bytes a pointer to the input bytes
+   * @param length the length of the input bytes
+   * @param sensor_data the parsed sensor_data
    */
-  std::set<boost::filesystem::path> carvePaths_;
+  virtual void Parse(const uint8_t *bytes, int32_t length,
+                     SensorType *sensor_data) const;
     
-    Status LoggerConfigParserPlugin::update(const std::string& /* source */,
-                                        const ParserConfig& config) {
-  rj::Document& doc = data_.doc();
-    }
-    
-    TEST_F(ViewsConfigParserPluginTests, test_swap_view) {
-  Config c;
-  std::vector<std::string> old_views_vec;
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  old_views_vec.clear();
-  auto s = c.update(getTestConfigMap('view_test.conf'));
-  EXPECT_TRUE(s.ok());
-  scanDatabaseKeys(kQueries, old_views_vec, 'config_views.');
-  EXPECT_EQ(old_views_vec.size(), 1U);
-  EXPECT_EQ(old_views_vec[0], 'config_views.kernel_hashes_new');
-    }
-    
-      /// The buffer used to maintain the context and state of the hashing
-  /// operations
-  void* ctx_;
-    
-    
-    {  query.query += ' LIMIT 1';
-  counter = 128;
-  auto cf2 = Query('will_update_query', query);
-  EXPECT_TRUE(cf2.isQueryNameInDatabase());
-  EXPECT_TRUE(cf2.isNewQuery());
-  cf2.addNewResults(results, 0, counter, dr);
-  EXPECT_FALSE(cf2.isNewQuery());
-  EXPECT_EQ(counter, 0UL);
+    TEST(ProtocolDataTest, CheckSum) {
+  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
+  const uint8_t result =
+      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
+  EXPECT_EQ(0xE7, result);
 }
     
-      /// Access the Inofity response scratch space.
-  mutable Mutex scratch_mutex_;
+    const uint8_t BIT_MASK_1[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
+const uint8_t BIT_MASK_0[] = {0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F};
+const uint8_t RANG_MASK_1_L[] = {0x01, 0x03, 0x07, 0x0F,
+                                 0x1F, 0x3F, 0x7F, 0xFF};
+const uint8_t RANG_MASK_0_L[] = {0xFE, 0XFC, 0xF8, 0xF0,
+                                 0xE0, 0xC0, 0x80, 0x00};
     
-      // Once more, now deregistering all event types.
-  status = EventFactory::registerEventPublisher(pub);
-  EXPECT_TRUE(status.ok());
+      /**
+   * @brief Transform to its hexadecimal represented by a string.
+   * @return Hexadecimal representing the Byte.
+   */
+  std::string to_hex_string() const;
+    
+    TEST(ByteTest, SetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  value.set_value(0x06, 3, 3);
+  EXPECT_EQ(0x32, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 0, 8);
+  EXPECT_EQ(0x06, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 0, 10);
+  EXPECT_EQ(0x06, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 1, 7);
+  EXPECT_EQ(0x0C, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x07, 1, 1);
+  EXPECT_EQ(0x1A, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x07, -1, 1);
+  EXPECT_EQ(0x1A, value.get_byte());
+}
+    
+      // Init can client
+  auto *can_factory = CanClientFactory::instance();
+  can_factory->RegisterCanClients();
+  can_client_ = can_factory->CreateCANClient(canbus_conf_.can_card_parameter());
+  if (!can_client_) {
+    return OnError('Failed to create can client.');
+  }
+  AINFO << 'Can client is successfully created.';
+    
+    #include 'modules/drivers/canbus/sensor_gflags.h'
+    
+    // System gflags
+DECLARE_string(node_name);
+DECLARE_string(canbus_driver_name);
