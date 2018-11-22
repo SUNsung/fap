c9fbdbd48deefe4c3a7c2a2bf3339ec0a41f50c4
SUNsung <weatherius@gmail.com>
2018-11-22 12:37:55 +0200

    Rewrite [25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py] File in: 2018-11-22 12:37:54

--- pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
+++ pyton/25dRyIoajQB.25baQ14b4ywLo25BuqJdyMJDBo25QDr.py
@@ -1,125 +1,215 @@
 
         
-        secret_msg = b'Secret message goes here'
-    
-    for page in itertools.count(1):
-    releases = json.loads(compat_urllib_request.urlopen(
-        'https://api.github.com/repos/rg3/youtube-dl/releases?page=%s' % page
-    ).read().decode('utf-8'))
-    
-        # Are checkable fields missing from the test case definition?
-    test_info_dict = dict((key, value if not isinstance(value, compat_str) or len(value) < 250 else 'md5:' + md5(value))
-                          for key, value in got_dict.items()
-                          if value and key in ('id', 'title', 'description', 'uploader', 'upload_date', 'timestamp', 'uploader_id', 'location', 'age_limit'))
-    missing_keys = set(test_info_dict.keys()) - set(expected_dict.keys())
-    if missing_keys:
-        def _repr(v):
-            if isinstance(v, compat_str):
-                return ''%s'' % v.replace('\\', '\\\\').replace(''', '\\'').replace('\n', '\\n')
+        
+def check_format(filename):
+    '''
+    validates that each line is formatted correctly,
+    appending to error list as needed
+    '''
+    with open(filename) as fp:
+        lines = list(line.rstrip() for line in fp)
+    check_alphabetical(lines)
+    # START Check Entries
+    num_in_category = min_entries_per_section + 1
+    category = ''
+    category_line = 0
+    for line_num, line in enumerate(lines):
+        if section_title_re.match(line):
+            title_links.append(section_title_re.match(line).group(1))
+        # check each section for the minimum number of entries
+        if line.startswith(anchor):
+            match = anchor_re.match(line)
+            if match:
+                if match.group(1) not in title_links:
+                    add_error(line_num, 'section header ({}) not added as a title link'.format(match.group(1)))
             else:
-                return repr(v)
-        info_dict_str = ''
-        if len(missing_keys) != len(expected_dict):
-            info_dict_str += ''.join(
-                '    %s: %s,\n' % (_repr(k), _repr(v))
-                for k, v in test_info_dict.items() if k not in missing_keys)
+                add_error(line_num, 'section header is not formatted correctly')
+            if num_in_category < min_entries_per_section:
+                add_error(category_line, '{} section does not have the minimum {} entries (only has {})'.format(
+                    category, min_entries_per_section, num_in_category))
+            category = line.split(' ')[1]
+            category_line = line_num
+            num_in_category = 0
+            continue
+        # skips lines that we do not care about
+        if not line.startswith('|') or line.startswith('|---'):
+            continue
+        num_in_category += 1
+        segments = line.split('|')[1:-1]
+        if len(segments) < num_segments:
+            add_error(line_num, 'entry does not have all the required sections (have {}, need {})'.format(
+                len(segments), num_segments))
+            continue
+        # START Global
+        for segment in segments:
+            # every line segment should start and end with exactly 1 space
+            if len(segment) - len(segment.lstrip()) != 1 or len(segment) - len(segment.rstrip()) != 1:
+                add_error(line_num, 'each segment must start and end with exactly 1 space')
+        # END Global
+        segments = [seg.strip() for seg in segments]
+        check_entry(line_num, segments)
+    # END Check Entries
+    
+    if os.environ.get('CONVERT_README'):
+    import pypandoc
+    
+        proc.sendline(u'fuck')
+    assert proc.expect([TIMEOUT, u'git show'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git push'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git help'])
+    proc.send('\033[A')
+    assert proc.expect([TIMEOUT, u'git push'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git help'])
+    proc.send('\n')
     
     
-FORM_CONTENT_TYPE = 'application/x-www-form-urlencoded; charset=utf-8'
-JSON_CONTENT_TYPE = 'application/json'
-JSON_ACCEPT = '{0}, */*'.format(JSON_CONTENT_TYPE)
-DEFAULT_UA = 'HTTPie/%s' % __version__
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
     
-        def __init__(self, msg, with_headers=True, with_body=True,
-                 on_body_chunk_downloaded=None):
-        '''
-        :param msg: a :class:`models.HTTPMessage` subclass
-        :param with_headers: if `True`, headers will be included
-        :param with_body: if `True`, body will be included
+        return _set_text
     
-        # Auth
-    def get_auth_plugins(self):
-        return [plugin for plugin in self if issubclass(plugin, AuthPlugin)]
+    match_output = '''
+Listing... Done
+heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
+resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
+squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
+unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
+'''
     
     
-with codecs.open(FILE_PATH, encoding='utf8') as f:
-    # Strip because we don't want new lines in the data so that we can
-    # easily count occurrences also when embedded in JSON (where the new
-    # line would be escaped).
-    FILE_CONTENT = f.read().strip()
+output = '''sshfs: OsxfuseRequirement unsatisfied!
     
     
-class TestBinaryRequestData:
+@pytest.mark.parametrize('command', [
+    Command('cargo buid', no_such_subcommand_old),
+    Command('cargo buils', no_such_subcommand)])
+def test_match(command):
+    assert match(command)
     
+        if dataset_name == 'SA':
+        lb = LabelBinarizer()
+        x1 = lb.fit_transform(X[:, 1].astype(str))
+        x2 = lb.fit_transform(X[:, 2].astype(str))
+        x3 = lb.fit_transform(X[:, 3].astype(str))
+        X = np.c_[X[:, :1], x1, x2, x3, X[:, 4:]]
+        y = (y != b'normal.').astype(int)
     
-def test_default_options(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http(httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['form'] == {'foo': 'bar'}
+        for n_components in [i.astype(int) for i in
+                         np.linspace(data.shape[1] // 10,
+                                     data.shape[1], num=4)]:
+        all_times = defaultdict(list)
+        all_errors = defaultdict(list)
+        pca = PCA(n_components=n_components)
+        rpca = PCA(n_components=n_components, svd_solver='randomized',
+                   random_state=1999)
+        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
+                                                               ('rpca', rpca)]}
     
-        @pytest.mark.parametrize('header, expected_filename', [
-        ('attachment; filename=hello-WORLD_123.txt', 'hello-WORLD_123.txt'),
-        ('attachment; filename='.hello-WORLD_123.txt'', 'hello-WORLD_123.txt'),
-        ('attachment; filename='white space.txt'', 'white space.txt'),
-        (r'attachment; filename='\'quotes\'.txt'', ''quotes'.txt'),
-        ('attachment; filename=/etc/hosts', 'hosts'),
-        ('attachment; filename=', None)
-    ])
-    def test_Content_Disposition_parsing(self, header, expected_filename):
-        assert filename_from_content_disposition(header) == expected_filename
+        import matplotlib.pyplot as plt
+    fig = plt.figure('scikit-learn OMP vs. LARS benchmark results')
+    for i, (label, timings) in enumerate(sorted(six.iteritems(results))):
+        ax = fig.add_subplot(1, 2, i+1)
+        vmax = max(1 - timings.min(), -1 + timings.max())
+        plt.matshow(timings, fignum=False, vmin=1 - vmax, vmax=1 + vmax)
+        ax.set_xticklabels([''] + [str(each) for each in samples_range])
+        ax.set_yticklabels([''] + [str(each) for each in features_range])
+        plt.xlabel('n_samples')
+        plt.ylabel('n_features')
+        plt.title(label)
     
+        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
+    plt.plot(sample_sizes, one_core, label='one core')
+    plt.plot(sample_sizes, multi_core, label='multi core')
+    plt.xlabel('n_samples')
+    plt.ylabel('Time (s)')
+    plt.title('Parallel %s' % func.__name__)
+    plt.legend()
     
-def test_help():
-    r = http('--help', error_exit_ok=True)
-    assert r.exit_status == httpie.ExitStatus.OK
-    assert 'https://github.com/jakubroztocil/httpie/issues' in r
+        print('============================================')
+    print('Warning: this is going to take a looong time')
+    print('============================================')
     
-    
-class TestVerboseFlag:
-    def test_verbose(self, httpbin):
-        r = http('--verbose',
-                 'GET', httpbin.url + '/get', 'test-header:__test__')
-        assert HTTP_OK in r
-        assert r.count('__test__') == 2
-    
-    
-def test_max_redirects(httpbin):
-    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
-             error_exit_ok=True)
-    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
+    # Output in order: dev, stable, decreasing other version
+seen = set()
+for name in (NAMED_DIRS +
+             sorted((k for k in dirs if k[:1].isdigit()),
+                    key=LooseVersion, reverse=True)):
+    version_num, pdf_size = dirs[name]
+    if version_num in seen:
+        # symlink came first
+        continue
+    else:
+        seen.add(version_num)
+    name_display = '' if name[:1].isdigit() else ' (%s)' % name
+    path = 'http://scikit-learn.org/%s' % name
+    out = ('* `Scikit-learn %s%s documentation <%s/documentation.html>`_'
+           % (version_num, name_display, path))
+    if pdf_size is not None:
+        out += (' (`PDF %s <%s/_downloads/scikit-learn-docs.pdf>`_)'
+                % (pdf_size, path))
+    print(out)
 
     
+    def dirname(p):
+    '''Returns the directory component of a pathname'''
+    p = os.fspath(p)
+    sep = _get_sep(p)
+    i = p.rfind(sep) + 1
+    head = p[:i]
+    if head and head != sep*len(head):
+        head = head.rstrip(sep)
+    return head
     
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'GroupCommitResolution'
-        db.create_table(
-            'sentry_groupcommitresolution', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'group_id',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
-                ), (
-                    'commit_id',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
-                ), (
-                    'datetime', self.gf('django.db.models.fields.DateTimeField')(
-                        db_index=True
-                    )
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['GroupCommitResolution'])
     
-        complete_apps = ['sentry']
-
+class DBUnpickler(pickle.Unpickler):
     
-            # User chose to not deal with backwards NULL issues for 'Environment.project_id'
-        raise RuntimeError(
-            'Cannot reverse this migration. 'Environment.project_id' and its values cannot be restored.'
-        )
+    # Create the base text message.
+msg = EmailMessage()
+msg['Subject'] = 'Ayons asperges pour le déjeuner'
+msg['From'] = Address('Pepé Le Pew', 'pepe', 'example.com')
+msg['To'] = (Address('Penelope Pussycat', 'penelope', 'example.com'),
+             Address('Fabrette Pussycat', 'fabrette', 'example.com'))
+msg.set_content('''\
+Salut!
+    
+    # We can extract the richest alternative in order to display it:
+richest = msg.get_body()
+partfiles = {}
+if richest['content-type'].maintype == 'text':
+    if richest['content-type'].subtype == 'plain':
+        for line in richest.get_content().splitlines():
+            print(line)
+        sys.exit()
+    elif richest['content-type'].subtype == 'html':
+        body = richest
+    else:
+        print('Don't know how to display {}'.format(richest.get_content_type()))
+        sys.exit()
+elif richest['content-type'].content_type == 'multipart/related':
+    body = richest.get_body(preferencelist=('html'))
+    for part in richest.iter_attachments():
+        fn = part.get_filename()
+        if fn:
+            extension = os.path.splitext(part.get_filename())[1]
+        else:
+            extension = mimetypes.guess_extension(part.get_content_type())
+        with tempfile.NamedTemporaryFile(suffix=extension, delete=False) as f:
+            f.write(part.get_content())
+            # again strip the <> to go from email form of cid to html form.
+            partfiles[part['content-id'][1:-1]] = f.name
+else:
+    print('Don't know how to display {}'.format(richest.get_content_type()))
+    sys.exit()
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
+    # The magic_html_parser has to rewrite the href='cid:....' attributes to
+    # point to the filenames in partfiles.  It also has to do a safety-sanitize
+    # of the html.  It could be written using html.parser.
+    f.write(magic_html_parser(body.get_content(), partfiles))
+webbrowser.open(f.name)
+os.remove(f.name)
+for fn in partfiles.values():
+    os.remove(fn)
