0cf047c768d9461b415605642ae27cfc72781643
SUNsung <weatherius@gmail.com>
2018-11-22 16:49:39 +0200

    Rewrite [56tAPqe4KWvPk56a4Lc90YyEQU56g24Phe4HjCY56U9h.cpp] File in: 2018-11-22 16:49:39

--- cccccc/56tAPqe4KWvPk56a4Lc90YyEQU56g24Phe4HjCY56U9h.cpp
+++ cccccc/56tAPqe4KWvPk56a4Lc90YyEQU56g24Phe4HjCY56U9h.cpp
@@ -1,100 +1,119 @@
 
         
-        
-    {}  // namespace
+        // Whether the op should be prefixed with underscore.
+bool IsOpWithUnderscorePrefix(const string& s);
     
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
+    void CostAnalyzer::PrintAnalysis(std::ostream& os, bool per_node_report,
+                                 bool verbose) const {
+  os << std::endl;
+  os << std::left << std::setw(50)
+     << 'Total time measured in ns (serialized): ' << std::right
+     << std::setw(20) << total_time_measured_serialized_ << std::endl;
+  os << std::left << std::setw(50)
+     << 'Total time measured in ns (actual): ' << std::right << std::setw(20)
+     << total_time_measured_ << std::endl;
+  os << std::left << std::setw(50)
+     << 'Total time analytical in ns (upper bound): ' << std::right
+     << std::setw(20) << total_time_analytical_upper_ << std::endl;
+  os << std::left << std::setw(50)
+     << 'Total time analytical in ns (lower bound): ' << std::right
+     << std::setw(20) << total_time_analytical_lower_ << std::endl;
+  double efficiency_upper = static_cast<double>(total_time_analytical_upper_) /
+                            static_cast<double>(total_time_measured_);
+  os << std::left << std::setw(50)
+     << 'Overall efficiency (analytical upper/actual): ' << std::right
+     << std::setw(20) << efficiency_upper << std::endl;
+  double efficiency_lower = static_cast<double>(total_time_analytical_lower_) /
+                            static_cast<double>(total_time_measured_);
+  os << std::left << std::setw(50)
+     << 'Overall efficiency (analytical lower/actual): ' << std::right
+     << std::setw(20) << efficiency_lower << std::endl;
+  os << std::endl;
+    }
     
     
-    {}  // namespace atom
+    {}  // namespace tensorflow
+
     
-      // content::DownloadManager::Observer:
-  void OnDownloadCreated(content::DownloadManager* manager,
-                         download::DownloadItem* item) override;
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
     
-    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    #include 'tensorflow/core/framework/node_def.pb.h'
+#include 'tensorflow/core/framework/node_def_util.h'
+#include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+#include 'tensorflow/core/framework/types.h'
+#include 'tensorflow/core/lib/core/status.h'
+#include 'tensorflow/core/util/device_name_utils.h'
+    
+    enum RecordType {
+  // Zero is reserved for preallocated files
+  kZeroType = 0,
+    }
+    
+        // Check crc
+    if (checksum_) {
+      uint32_t expected_crc = crc32c::Unmask(DecodeFixed32(header));
+      uint32_t actual_crc = crc32c::Value(header + 6, 1 + length);
+      if (actual_crc != expected_crc) {
+        // Drop the rest of the buffer since 'length' itself may have
+        // been corrupted and if we trust it, we could find some
+        // fragment of a real log record that just happens to look
+        // like a valid log record.
+        size_t drop_size = buffer_.size();
+        buffer_.clear();
+        ReportCorruption(drop_size, 'checksum mismatch');
+        return kBadRecord;
+      }
+    }
+    
+    /**
+ * @class CanClientFactory
+ * @brief CanClientFactory inherites apollo::common::util::Facotory.
+ */
+class CanClientFactory
+    : public apollo::common::util::Factory<CANCardParameter::CANCardBrand,
+                                           CanClient> {
  public:
-  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
-  ~AtomJavaScriptDialogManager() override;
+  /**
+   * @brief Register the CAN clients of all brands. This function call the
+   *        Function apollo::common::util::Factory::Register() for all of the
+   *        CAN clients.
+   */
+  void RegisterCanClients();
     }
     
-    IPC_SYNC_MESSAGE_ROUTED3_1(ShellViewHostMsg_Call_Static_Method_Sync,
-                           std::string /* type name */,
-                           std::string /* method name */,
-                           base::ListValue /* arguments */,
-                           base::ListValue /* result */)
-    
-    RenderView* GetCurrentRenderView() {
-  v8::Isolate* isolate = v8::Isolate::GetCurrent();
-  v8::Local<v8::Context> ctx = isolate->GetCurrentContext();
-  return GetRenderView(ctx);
-}
-    
-    bool MenuDelegate::IsCommandIdChecked(int command_id) const {
-  if (command_id < 0)
-    return false;
-    }
-    
-    class NwClipboardGetListSyncFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardGetListSyncFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
- public:
-  NwMenuGetNSStringWithFixupFunction(){}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    
- protected:
-  ~NwMenuGetNSStringWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
-};
-    
-    #include 'modules/common/macro.h'
-#include 'modules/common/util/factory.h'
-#include 'modules/drivers/canbus/can_client/can_client.h'
-    
-      CANCardParameter can_client_conf_b;
-  std::unique_ptr<CanClient> client_b;
-  if (!FLAGS_only_one_send) {
-    if (!apollo::common::util::GetProtoFromFile(FLAGS_can_client_conf_file_b,
-                                                &can_client_conf_b)) {
-      AERROR << 'Unable to load canbus conf file: '
-             << FLAGS_can_client_conf_file_b;
-      return 1;
-    } else {
-      AINFO << 'Conf file is loaded: ' << FLAGS_can_client_conf_file_b;
-    }
-    AINFO << can_client_conf_b.ShortDebugString();
-    client_b = can_client_factory->CreateObject(can_client_conf_b.brand());
-    if (!client_b || !client_b->Init(can_client_conf_b) ||
-        client_b->Start() != ErrorCode::OK) {
-      AERROR << 'Create can client b failed.';
-      return 1;
-    }
-    param_ptr_b->can_client = client_b.get();
-    param_ptr_b->conf = can_client_conf_b;
+      // 2. set baudrate to 500k
+  ret = canSetBaudrate(dev_handler_, NTCAN_BAUD_500);
+  if (ret != NTCAN_SUCCESS) {
+    AERROR << 'set baudrate error code: ' << ret << ', ' << GetErrorString(ret);
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
   }
     
-    #ifdef NTCAN_ERROR_FORMAT_LONG
-  {
-    NTCAN_RESULT res;
-    char sz_error_text[60];
-    }
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-namespace can {
-    }
-    }
-    }
-    }
+    #include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
     
     
     {  if (static_cast<size_t>(*frame_num) != frames.size()) {
@@ -110,93 +129,30 @@ namespace can {
   return ErrorCode::OK;
 }
     
-    #endif  // MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_HERMES_CAN_CLIENT_H
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
+#include 'modules/drivers/canbus/can_comm/message_manager.h'
     
-      if (ret < 0) {
-    AERROR << 'bind socket to network interface error code: ' << ret;
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
-  }
     
-      /**
-   * @brief Send messages
-   * @param frames The messages to send.
-   * @param frame_num The amount of messages to send.
-   * @return The status of the sending action which is defined by
-   *         apollo::common::ErrorCode.
-   */
-  apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
-                                 int32_t *const frame_num) override;
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
     
       /**
-   * @brief Constructor which takes a reference to a one-byte unsigned integer.
-   * @param value The reference to a one-byte unsigned integer for construction.
+   * @brief destruct protocol data.
    */
-  Byte(const Byte &value);
+  virtual ~ProtocolData() = default;
     
-    TEST(ByteTest, ByteToString) {
-  unsigned char value = 0x34;
-  EXPECT_EQ('34', Byte::byte_to_hex(value));
-  EXPECT_EQ('00110100', Byte::byte_to_binary(value));
-  uint32_t int_value = 0xE13A;
-  EXPECT_EQ('E13A', Byte::byte_to_hex(int_value));
+    TEST(ByteTest, GetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  EXPECT_EQ(0x05, value.get_byte(1, 3));
+  EXPECT_EQ(0x01, value.get_byte(1, 1));
+  EXPECT_EQ(0x00, value.get_byte(8, 1));
+  EXPECT_EQ(0x00, value.get_byte(-1, 1));
+  EXPECT_EQ(0x1A, value.get_byte(0, 10));
 }
-    
-      if (can_receiver_.Init(can_client_.get(), sensor_message_manager_.get(),
-                         canbus_conf_.enable_receiver_log()) != ErrorCode::OK) {
-    return OnError('Failed to init can receiver.');
-  }
-  AINFO << 'The can receiver is successfully initialized.';
-    
-    namespace v8 {
-namespace internal {
-    }
-    }
-    
-    void Builtins::Generate_InterpreterPushArgsThenCallWithFinalSpread(
-    MacroAssembler* masm) {
-  return Generate_InterpreterPushArgsThenCallImpl(
-      masm, ConvertReceiverMode::kAny,
-      InterpreterPushArgsMode::kWithFinalSpread);
-}
-    
-    // ES6 #sec-math.sqrt
-TF_BUILTIN(MathSqrt, MathBuiltinsAssembler) {
-  Node* context = Parameter(Descriptor::kContext);
-  Node* x = Parameter(Descriptor::kX);
-  MathUnaryOperation(context, x, &CodeStubAssembler::Float64Sqrt);
-}
-    
-    #ifndef V8_BUILTINS_BUILTINS_MATH_GEN_H_
-#define V8_BUILTINS_BUILTINS_MATH_GEN_H_
-    
-      BIND(&u8);
-  Return(SmiFromInt32((this->*function)(MachineType::Uint8(), backing_store,
-                                        index_word, value_word32)));
-    
-    class TestBuiltinsAssembler : public BaseBuiltinsFromDSLAssembler {
- public:
-  explicit TestBuiltinsAssembler(compiler::CodeAssemblerState* state)
-      : BaseBuiltinsFromDSLAssembler(state) {}
-};
-    
-    QStringList ListLikeKeyModel::getColumnNames()
-{
-    return QStringList() << 'row'  << 'value';
-}
-    
-    class StringKeyModel : public KeyModel<QByteArray>
-{    
-public:
-    StringKeyModel(QSharedPointer<RedisClient::Connection> connection,
-                   QByteArray fullPath, int dbIndex, long long ttl);
-    }
-    
-            public:
-            enum class State { READY, RUNNING, FINISHED };
-        public:
-            CurrentOperation(QSharedPointer<RedisClient::Connection> connection, int dbIndex,
-                             Manager::Operation op=Manager::Operation::DELETE_KEYS,
-                             QRegExp keyPattern=QRegExp('*', Qt::CaseSensitive, QRegExp::Wildcard));
-    
-        QObject::connect(manager, SIGNAL(finished(QNetworkReply*)),
-        this, SLOT(requestFinished(QNetworkReply*)));
