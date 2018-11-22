867a2e36fc57ba24ea934347f69d65d0dde42b0e
SUNsung <weatherius@gmail.com>
2018-11-22 14:44:27 +0200

    Rewrite [563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp] File in: 2018-11-22 14:44:26

--- cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
+++ cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
@@ -1,169 +1,233 @@
 
         
-        /// Determine whether the given text matches a property name.
-static bool textMatchesPropertyName(StringRef text,
-                                    const InheritedNameSet *allPropertyNames) {
-  if (!allPropertyNames) return false;
-    }
+        #endif  // TESSERACT_ARCH_DOTPRODUCTSSE_H_
+
     
-    
-    {    if (Finished) {
-      std::string Output = 'Output placeholder\n';
-      std::string Errors =
-          P.second->SeparateErrors ? 'Error placeholder\n' : '';
-      if (Finished(P.first, 0, Output, Errors, TaskProcessInformation(Pid),
-                   P.second->Context) == TaskFinishedResponse::StopExecution)
-        SubtaskFailed = true;
-    }
-  }
-    
-    
-    {  memcpy(Value, &uuid, Size);
+    // Extracts and converts 8x32-bit results from result, adding the bias from wi
+// and scaling by scales, before storing in *v. Note that wi, scales and v are
+// expected to contain 8 consecutive elements or num_out if less.
+inline void ExtractResults(__m256i& result, __m256i& shift_id,
+                           const int8_t*& wi, const double*& scales,
+                           int num_out, double*& v) {
+  for (int out = 0; out < num_out; ++out) {
+    int32_t res =
+#ifndef _MSC_VER
+        _mm256_extract_epi32(result, 0)
 #else
-  uuid_generate_random(Value);
+        // Workaround MSVC's ICE
+        // _mm256_extract_epi32(X, Y) == ((int32_t*)&X)[Y]
+        ((int32_t*)&result)[0]
 #endif
-}
-    
-      bool isVoid() const {
-    assert(isValid());
-    return Decl.isNull();
-  }
-    
-    const Message* GetCProtoInsidePyProto(PyObject* msg) {
-  return GetCProtoInsidePyProtoPtr(msg);
-}
-Message* MutableCProtoInsidePyProto(PyObject* msg) {
-  return MutableCProtoInsidePyProtoPtr(msg);
-}
-    
-    bool AnyMetadata::InternalIs(const Descriptor* descriptor) const {
-  const string type_url = type_url_->GetNoArena();
-  string full_name;
-  if (!ParseAnyTypeUrl(type_url, &full_name)) {
-    return false;
-  }
-  return full_name == descriptor->full_name();
-}
-    
-    const GeneratedCodeInfo::Annotation* FindAnnotationOnPath(
-    const GeneratedCodeInfo& info, const string& source_file,
-    const std::vector<int>& path) {
-  std::vector<const GeneratedCodeInfo::Annotation*> annotations;
-  FindAnnotationsOnPath(info, source_file, path, &annotations);
-  if (annotations.empty()) {
-    return NULL;
-  }
-  return annotations[0];
-}
-    
-    void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enumDescriptor) {
-    WriteDocCommentBody(printer, enumDescriptor);
-}
-void WriteEnumValueDocComment(io::Printer* printer, const EnumValueDescriptor* value) {
-    WriteDocCommentBody(printer, value);
-}
-    
-    // Strip package name from a descriptor's full name.
-// For example:
-//   Full name   : foo.Bar.Baz
-//   Package name: foo
-//   After strip : Bar.Baz
-string StripPackageName(const string& full_name,
-                        const FileDescriptor* file) {
-  if (file->package().empty()) {
-    return full_name;
-  } else {
-    // Strip package name
-    return full_name.substr(file->package().size() + 1);
+        ;
+    *v++ = (static_cast<double>(res) / INT8_MAX + *wi++) * *scales++;
+    // Rotate the results in int32_t units, so the next result is ready.
+    result = _mm256_permutevar8x32_epi32(result, shift_id);
   }
 }
     
-      ObjectiveCType objc_type = GetObjectiveCType(descriptor_);
-  string singular_type;
-  if (objc_type == OBJECTIVECTYPE_MESSAGE) {
-    vars['type'] = string('GPBStringifySymbol(') +
-                   ClassName(descriptor_->message_type()) + ')';
-  } else {
-    vars['type'] = 'NULL';
-  }
+      // Array holding scores for each orientation id [0,3].
+  // Orientation ids [0..3] map to [0, 270, 180, 90] degree orientations of the
+  // page respectively, where the values refer to the amount of clockwise
+  // rotation to be applied to the page for the text to be upright and readable.
+  float orientations[4];
+  // Script confidence scores for each of 4 possible orientations.
+  float scripts_na[4][kMaxNumberOfScripts];
     
-    #define TEGRA_UNARYOPSCALE(type, op, src1, sz1, dst, sz, w, h, scales) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-    parallel_for_(Range(0, h), \
-    TegraGenOp_##op##_Invoker<const type, type>(src1, sz1, dst, sz, w, h, scales), \
-    (w * h) / static_cast<double>(1<<16)), \
-    CV_HAL_ERROR_OK \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
     
-            map = (u8*)(mag_buf[2] + magstep);
-        memset(map, 1, mapstep);
-        memset(map + mapstep*(size.height + 1), 1, mapstep);
-        borderyt = std::max<ptrdiff_t>(0, offsetk - (ptrdiff_t)borderMargin.top);
-        borderyb = std::max<ptrdiff_t>(0, offsetk - (ptrdiff_t)borderMargin.bottom);
+    {  TBLOB* blob;
+  Tesseract* tesseract;
+  BLOB_CHOICE_LIST** choices;
+};
+    
+    #endif  // GRAPHICS_DISABLED
+#endif  // TESSERACT_CCMAIN_PARAMSD_H_
+
+    
+      bool Next() override {
+    if (!parser_->Next()) return false;
+    const RowBlock<IndexType>& batch = parser_->Value();
+    LOG(INFO) << batch.size;
+    dense_index_.resize(num_col_ * batch.size);
+    dense_value_.resize(num_col_ * batch.size);
+    std::fill(dense_value_.begin(), dense_value_.end(), 0.0);
+    offset_.resize(batch.size + 1);
+    offset_[0] = 0;
     }
-    inline void firstRow(const Size2D &size, s32,
-                         const u8 *srcBase, ptrdiff_t srcStride,
-                         s16*, ptrdiff_t,
-                         s16*, ptrdiff_t,
-                         s32** mag_buf)
-    {
-        //sobelH row #0
-        const u8* _src = internal::getRowPtr(srcBase, srcStride, 0);
-        sobelRow(_src, ((s16*)mag_buf[0]) + shxOffset, ((s16*)mag_buf[0]) + shyOffset, size.width);
-        //sobelH row #1
-        _src = internal::getRowPtr(srcBase, srcStride, 1);
-        sobelRow(_src, ((s16*)mag_buf[1]) + shxOffset, ((s16*)mag_buf[1]) + shyOffset, size.width);
+    
+      inline void ParseStr(std::string *tok) {
+    while ((ch_buf_ = this->GetChar()) != EOF) {
+      switch (ch_buf_) {
+        case '\\': *tok += this->GetChar(); break;
+        case '\'': return;
+        case '\r':
+        case '\n': LOG(FATAL)<< 'ConfigReader: unterminated string';
+        default: *tok += ch_buf_;
+      }
+    }
+    LOG(FATAL) << 'ConfigReader: unterminated string';
+  }
+  inline void ParseStrML(std::string *tok) {
+    while ((ch_buf_ = this->GetChar()) != EOF) {
+      switch (ch_buf_) {
+        case '\\': *tok += this->GetChar(); break;
+        case '\'': return;
+        default: *tok += ch_buf_;
+      }
+    }
+    LOG(FATAL) << 'unterminated string';
+  }
+  // return newline
+  inline bool GetNextToken(std::string *tok) {
+    tok->clear();
+    bool new_line = false;
+    while (ch_buf_ != EOF) {
+      switch (ch_buf_) {
+        case '#' : SkipLine(); new_line = true; break;
+        case '\'':
+          if (tok->length() == 0) {
+            ParseStr(tok); ch_buf_ = this->GetChar(); return new_line;
+          } else {
+            LOG(FATAL) << 'ConfigReader: token followed directly by string';
+          }
+        case '\'':
+          if (tok->length() == 0) {
+            ParseStrML(tok); ch_buf_ = this->GetChar(); return new_line;
+          } else {
+            LOG(FATAL) << 'ConfigReader: token followed directly by string';
+          }
+        case '=':
+          if (tok->length() == 0) {
+            ch_buf_ = this->GetChar();
+            *tok = '=';
+          }
+          return new_line;
+        case '\r':
+        case '\n':
+          if (tok->length() == 0) new_line = true;
+        case '\t':
+        case ' ' :
+          ch_buf_ = this->GetChar();
+          if (tok->length() != 0) return new_line;
+          break;
+        default:
+          *tok += ch_buf_;
+          ch_buf_ = this->GetChar();
+          break;
+      }
+    }
+    if (tok->length() == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+/*!
+ * \brief an iterator use stream base, allows use all types of istream
+ */
+class ConfigStreamReader: public ConfigReaderBase {
+ public:
+  /*!
+   * \brief constructor
+   * \param fin istream input stream
+   */
+  explicit ConfigStreamReader(std::istream &fin) : fin_(fin) {}
+    
+    // common regressions
+// linear regression
+struct LinearSquareLoss {
+  // duplication is necessary, as __device__ specifier
+  // cannot be made conditional on template parameter
+  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
+  XGBOOST_DEVICE static bool CheckLabel(bst_float x) { return true; }
+  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
+    return predt - label;
+  }
+  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
+    return 1.0f;
+  }
+  template <typename T>
+  static T PredTransform(T x) { return x; }
+  template <typename T>
+  static T FirstOrderGradient(T predt, T label) { return predt - label; }
+  template <typename T>
+  static T SecondOrderGradient(T predt, T label) { return T(1.0f); }
+  static bst_float ProbToMargin(bst_float base_score) { return base_score; }
+  static const char* LabelErrorMsg() { return ''; }
+  static const char* DefaultEvalMetric() { return 'rmse'; }
+};
+    
+          ASSERT_TRUE(input == output);
     
     /*!
- *  Aligns pointer by the certain number of bytes
- *
- *  This small inline function aligns the pointer by the certain number of bytes by shifting
- *  it forward by 0 or a positive offset.
+ * \brief macro to check the call.
  */
-template<typename T> inline T* alignPtr(T* ptr, size_t n=sizeof(T))
-{
-    return (T*)(((size_t)ptr + n-1) & -n);
-}
+#define CHECK_CALL(x)                           \
+  if ((x) != 0) {                               \
+    error(XGBGetLastError());                   \
+  }
     
-    void sqrIntegral(const Size2D &size,
-                 const u8 * srcBase, ptrdiff_t srcStride,
-                 f64 * sqsumBase, ptrdiff_t sqsumStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    uint16x8_t v_zero8 = vmovq_n_u16(0u);
+      /// The inotify file descriptor handle.
+  std::atomic<int> inotify_handle_{-1};
+    
+    #include <gtest/gtest.h>
+    
+      int num_greetings = 10;
+  greeter.SayManyHellos(name, num_greetings, [](const std::string &message) {
+    std::cerr << 'Greeter received: ' << message << std::endl;
+  });
+    
+      builder.Finish(orc);  // Serialize the root of the object.
+    
+    namespace flatbuffers {
     }
     
-    #include <carotene/definitions.hpp>
+    #include 'monster_test_generated.h'
+#include 'flatbuffers/grpc.h'
     
-      void StartBlockComparisons() override;
-  void FinishBlockComparisons() override;
-    
-    inline int Log2FloorNonZero(uint32_t n) {
-#ifdef __GNUC__
-  return 31 ^ __builtin_clz(n);
-#else
-  unsigned int result = 0;
-  while (n >>= 1) result++;
-  return result;
-#endif
+    template<typename T> const char *EnumName(T tval, const TypeTable *type_table) {
+  if (!type_table || !type_table->names) return nullptr;
+  auto i = LookupEnum(static_cast<int64_t>(tval), type_table->values,
+                      type_table->num_elems);
+  if (i >= 0 && i < static_cast<int64_t>(type_table->num_elems)) {
+    return type_table->names[i];
+  }
+  return nullptr;
 }
     
-    #include 'guetzli/gamma_correct.h'
+    #endif // STATICDISPATCH_H
+
     
-    #include 'guetzli/output_image.h'
     
-    void Quantize(coeff_t* v, int iquant) {
-  *v = (*v * iquant + kBias) >> kDCTBits;
-}
+    {        /* Drain the queue */
+        for (auto &x : loopData->deferQueues[oldDeferQueue]) {
+            x();
+        }
+        loopData->deferQueues[oldDeferQueue].clear();
+    }
     
-    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
-// quantization table must have 3 * kDCTBlockSize values.
-bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     const int* quant, JPEGData* jpg);
+                /* Remove onAborted function if we reach the end */
+            if (httpResponseData->offset == totalSize) {
+                httpResponseData->onAborted = nullptr;
+                /* Also remove onWritable so that we do not emit when draining behind the scenes. */
+                httpResponseData->onWritable = nullptr;
+            }
     
-    #include <assert.h>
-#include <cstdlib>
-#include <string.h>
+    
+    {            return s;
+        });
+    
+    namespace uWS {
+    }
+    
+        static inline bool consumeContinuation(char *&src, unsigned int &length, WebSocketState<isServer> *wState, void *user) {
+        if (wState->remainingBytes <= length) {
+            if (isServer) {
+                int n = wState->remainingBytes >> 2;
+                unmaskInplace(src, src + n * 4, wState->mask);
+                for (int i = 0, s = wState->remainingBytes % 4; i < s; i++) {
+                    src[n * 4 + i] ^= wState->mask[i];
+                }
+            }
+    }
+    }
