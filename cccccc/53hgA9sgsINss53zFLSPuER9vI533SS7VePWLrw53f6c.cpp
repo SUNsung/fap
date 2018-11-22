ee50a16c4efbf0a51f31b0a7e124909ec66f1db2
SUNsung <weatherius@gmail.com>
2018-11-23 01:48:56 +0200

    Rewrite [53hgA9sgsINss53zFLSPuER9vI533SS7VePWLrw53f6c.cpp] File in: 2018-11-23 01:48:56

--- cccccc/53hgA9sgsINss53zFLSPuER9vI533SS7VePWLrw53f6c.cpp
+++ cccccc/53hgA9sgsINss53zFLSPuER9vI533SS7VePWLrw53f6c.cpp
@@ -1,220 +1,206 @@
 
         
-        #endif // BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
-
-    
-    QT_BEGIN_NAMESPACE
-class QModelIndex;
-QT_END_NAMESPACE
-    
-    SECP256K1_INLINE static void secp256k1_fe_mul_inner(uint64_t *r, const uint64_t *a, const uint64_t * SECP256K1_RESTRICT b) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            r15:rcx = d
- *            r10-r14 = a0-a4
- *            rbx     = b
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
-    }
-    
-    string ClassNameResolver::GetClassName(const Descriptor* descriptor,
-                                       bool immutable) {
-  return GetClassFullName(ClassNameWithoutPackage(descriptor, immutable),
-                          descriptor->file(), immutable,
-                          MultipleJavaFiles(descriptor->file(), immutable));
+        Optional<PlatformKind> swift::platformFromString(StringRef Name) {
+  if (Name == '*')
+    return PlatformKind::none;
+  return llvm::StringSwitch<Optional<PlatformKind>>(Name)
+#define AVAILABILITY_PLATFORM(X, PrettyName) .Case(#X, PlatformKind::X)
+#include 'swift/AST/PlatformKinds.def'
+      .Case('macOS', PlatformKind::OSX)
+      .Case('macOSApplicationExtension', PlatformKind::OSXApplicationExtension)
+      .Default(Optional<PlatformKind>());
 }
     
-    #include <string>
-#include <set>
-#include <vector>
-#include <google/protobuf/descriptor.h>
+      friend class SubstitutionMap;
     
-    class RepeatedEnumFieldGenerator : public RepeatedFieldGenerator {
-  friend FieldGenerator* FieldGenerator::Make(const FieldDescriptor* field,
-                                              const Options& options);
+    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
     }
     
-    #define SPLIT_QUAD(sgn, bits, n) { \
-                                     internal::prefetch(src + sj); \
-                                     SPLIT_ASM##n(sgn, bits) \
-                                 }
+      if (!wasInline) delete[] oldBegin;
     
-    #include 'common.hpp'
-#include 'vtransform.hpp'
+        if (node->Left) {
+      IndentScope ms(this, (childKind == ChildKind::Left ||
+                            childKind == ChildKind::Root) ? '  ' : '| ');
+      print(node->Left, ChildKind::Left);
+    }
+    
+        StringRef Line = RawText.substr(0, Pos);
+    Lines.push_back(Line);
+    if (!IsFirstLine) {
+      size_t NonWhitespacePos = RawText.find_first_not_of(' ');
+      if (NonWhitespacePos != StringRef::npos)
+        WhitespaceToTrim =
+            std::min(WhitespaceToTrim,
+                     static_cast<unsigned>(NonWhitespacePos));
+    }
+    IsFirstLine = false;
+    
+    #include 'swift/Demangling/StandardTypesMangling.def'
+    
+    #ifndef BITCOIN_REVERSELOCK_H
+#define BITCOIN_REVERSELOCK_H
+    
+    
+    {    /** In case a->infinity == 1, replace r with (b->x, b->y, 1). */
+    secp256k1_fe_cmov(&r->x, &b->x, a->infinity);
+    secp256k1_fe_cmov(&r->y, &b->y, a->infinity);
+    secp256k1_fe_cmov(&r->z, &fe_1, a->infinity);
+    r->infinity = infinity;
+}
+    
+    void test_bad_scalar(void) {
+    unsigned char s_zero[32] = { 0 };
+    unsigned char s_overflow[32] = {
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe,
+        0xba, 0xae, 0xdc, 0xe6, 0xaf, 0x48, 0xa0, 0x3b,
+        0xbf, 0xd2, 0x5e, 0x8c, 0xd0, 0x36, 0x41, 0x41
+    };
+    unsigned char s_rand[32] = { 0 };
+    unsigned char output[32];
+    secp256k1_scalar rand;
+    secp256k1_pubkey point;
+    }
+    
+    static bool CaseInsensitiveEqual(const std::string &s1, const std::string &s2)
+{
+    if (s1.size() != s2.size()) return false;
+    for (size_t i = 0; i < s1.size(); ++i) {
+        char c1 = s1[i];
+        if (c1 >= 'A' && c1 <= 'Z') c1 -= ('A' - 'a');
+        char c2 = s2[i];
+        if (c2 >= 'A' && c2 <= 'Z') c2 -= ('A' - 'a');
+        if (c1 != c2) return false;
+    }
+    return true;
+}
+    
+    static void CheckSplitTorReplyLine(std::string input, std::string command, std::string args)
+{
+    BOOST_TEST_MESSAGE(std::string('CheckSplitTorReplyLine(') + input + ')');
+    auto ret = SplitTorReplyLine(input);
+    BOOST_CHECK_EQUAL(ret.first, command);
+    BOOST_CHECK_EQUAL(ret.second, args);
+}
+    
+        BOOST_CHECK(!obj.exists('nyuknyuknyuk'));
+    
+    /*!
+ * \brief Registry entry for tree updater.
+ */
+struct TreeUpdaterReg
+    : public dmlc::FunctionRegEntryBase<TreeUpdaterReg,
+                                        std::function<TreeUpdater* ()> > {
+};
     
     
     {
-    {} // namespace internal
-} // namespace CAROTENE_NS
-
-    
-                {
-                // combine 3 'shifted' vectors
-                t0 = vext_u8(tprev[0], tcurr[0], 7);
-                t1 = tcurr[0];
-                t2 = vext_u8(tcurr[0], tnext[0], 1);
-    }
-    
-    void makeOffsets(ptrdiff_t pixel[], ptrdiff_t row_stride)
-{
-    pixel[0] = 0 + row_stride * 3;
-    pixel[1] = 1 + row_stride * 3;
-    pixel[2] = 2 + row_stride * 2;
-    pixel[3] = 3 + row_stride * 1;
-    pixel[4] = 3 + row_stride * 0;
-    pixel[5] = 3 + row_stride * -1;
-    pixel[6] = 2 + row_stride * -2;
-    pixel[7] = 1 + row_stride * -3;
-    pixel[8] = 0 + row_stride * -3;
-    pixel[9] = -1 + row_stride * -3;
-    pixel[10] = -2 + row_stride * -2;
-    pixel[11] = -3 + row_stride * -1;
-    pixel[12] = -3 + row_stride * 0;
-    pixel[13] = -3 + row_stride * 1;
-    pixel[14] = -2 + row_stride * 2;
-    pixel[15] = -1 + row_stride * 3;
-}
-    
-    /// @brief Fills a Blob with Gaussian-distributed values @f$ x = a @f$.
-template <typename Dtype>
-class GaussianFiller : public Filler<Dtype> {
- public:
-  explicit GaussianFiller(const FillerParameter& param)
-      : Filler<Dtype>(param) {}
-  virtual void Fill(Blob<Dtype>* blob) {
-    Dtype* data = blob->mutable_cpu_data();
-    CHECK(blob->count());
-    caffe_rng_gaussian<Dtype>(blob->count(), Dtype(this->filler_param_.mean()),
-        Dtype(this->filler_param_.std()), blob->mutable_cpu_data());
-    int sparse = this->filler_param_.sparse();
-    CHECK_GE(sparse, -1);
-    if (sparse >= 0) {
-      // Sparse initialization is implemented for 'weight' blobs; i.e. matrices.
-      // These have num == channels == 1; width is number of inputs; height is
-      // number of outputs.  The 'sparse' variable specifies the mean number
-      // of non-zero input weights for a given output.
-      CHECK_GE(blob->num_axes(), 1);
-      const int num_outputs = blob->shape(0);
-      Dtype non_zero_probability = Dtype(sparse) / Dtype(num_outputs);
-      rand_vec_.reset(new SyncedMemory(blob->count() * sizeof(int)));
-      int* mask = reinterpret_cast<int*>(rand_vec_->mutable_cpu_data());
-      caffe_rng_bernoulli(blob->count(), non_zero_probability, mask);
-      for (int i = 0; i < blob->count(); ++i) {
-        data[i] *= mask[i];
-      }
+    {
+    {  inline void PutChar(char ch) {
+    out_buf += ch;
+    if (out_buf.length() >= kBufferSize) Flush();
+  }
+  inline void Flush(void) {
+    if (out_buf.length() != 0) {
+      fp->Write(&out_buf[0], out_buf.length());
+      out_buf.clear();
     }
   }
-    }
+};
+}  // namespace common
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_BASE64_H_
+
+    
+    #include <iostream>
+    
+        // Construct a config map, the typical output from `Config::genConfig`.
+    config_data_['awesome'] = content_;
+    Config::get().reset();
+    clearDecorations('awesome');
     
     /**
- * @brief Normalizes the input to have 0-mean and/or unit (1) variance across
- *        the batch.
+ * @brief The supported hashing algorithms in osquery
  *
- * This layer computes Batch Normalization as described in [1]. For each channel
- * in the data (i.e. axis 1), it subtracts the mean and divides by the variance,
- * where both statistics are computed across both spatial dimensions and across
- * the different examples in the batch.
- *
- * By default, during training time, the network is computing global
- * mean/variance statistics via a running average, which is then used at test
- * time to allow deterministic outputs for each input. You can manually toggle
- * whether the network is accumulating or using the statistics via the
- * use_global_stats option. For reference, these statistics are kept in the
- * layer's three blobs: (0) mean, (1) variance, and (2) moving average factor.
- *
- * Note that the original paper also included a per-channel learned bias and
- * scaling factor. To implement this in Caffe, define a `ScaleLayer` configured
- * with `bias_term: true` after each `BatchNormLayer` to handle both the bias
- * and scaling factor.
- *
- * [1] S. Ioffe and C. Szegedy, 'Batch Normalization: Accelerating Deep Network
- *     Training by Reducing Internal Covariate Shift.' arXiv preprint
- *     arXiv:1502.03167 (2015).
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ * These are usually used as a constructor argument to osquery::Hash
  */
-template <typename Dtype>
-class BatchNormLayer : public Layer<Dtype> {
- public:
-  explicit BatchNormLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    
-    {}  // namespace caffe
-    
-    
-    {}  // namespace grpc
-    
-    Status ProtoServerReflection::GetFileContainingSymbol(
-    ServerContext* context, const grpc::string& symbol,
-    ServerReflectionResponse* response) {
-  if (descriptor_pool_ == nullptr) {
-    return Status::CANCELLED;
-  }
-    }
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_PROTO_SERVER_REFLECTION_H
-#define GRPC_INTERNAL_CPP_EXT_PROTO_SERVER_REFLECTION_H
-    
-    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
-    const grpc::string& name, int value) {
-  class IntOption final : public ServerBuilderOption {
-   public:
-    IntOption(const grpc::string& name, int value)
-        : name_(name), value_(value) {}
-    }
-    }
-    
-    #include <condition_variable>
-#include <list>
-#include <memory>
-#include <mutex>
-#include <queue>
-    
-    
-    { private:
-  bool is_receiving_ = false;
-  bool is_sending_finish_ = false;
-  CanAgent *other_agent_ = nullptr;
-  TestCanParam *param_ptr_ = nullptr;
-  std::unique_ptr<std::thread> thread_recv_;
-  std::unique_ptr<std::thread> thread_send_;
+enum HashType {
+  HASH_TYPE_MD5 = 2,
+  HASH_TYPE_SHA1 = 4,
+  HASH_TYPE_SHA256 = 8,
 };
     
+      if (!::GetExitCodeProcess(process.nativeHandle(), &code)) {
+    return false;
+  }
+    
     
     {
-    {
-    {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
+    {  // Stored query names is a factory method included alongside every query.
+  // It will include the set of query names with existing 'previous' results.
+  auto names = cf.getStoredQueryNames();
+  auto in_vector = std::find(names.begin(), names.end(), 'foobar');
+  EXPECT_NE(in_vector, names.end());
+}
+}
 
     
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-#include 'modules/common/proto/error_code.pb.h'
-#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
-#include 'modules/drivers/canbus/can_comm/message_manager.h'
+      // Now test inclusive bounds.
+  struct ConstraintList cl3;
+  constraint = Constraint(LESS_THAN_OR_EQUALS);
+  constraint.expr = '1000';
+  cl3.add(constraint);
+  constraint = Constraint(GREATER_THAN_OR_EQUALS);
+  constraint.expr = '1';
+  cl3.add(constraint);
     
-    class MockMessageManager
-    : public MessageManager<::apollo::canbus::ChassisDetail> {
- public:
-  MockMessageManager() {
-    AddRecvProtocolData<MockProtocolData, true>();
-    AddSendProtocolData<MockProtocolData, true>();
+    void Distributed::addResult(const DistributedQueryResult& result) {
+  results_.push_back(result);
+}
+    
+    
+    {  if (sc_time != file_dir_stat.st_ctime) {
+    if ((rc = addMonitor(path, isc, isc->mask, isc->recursive, add_watch))) {
+      isc->path_sc_time_[path] = file_dir_stat.st_ctime;
+    }
   }
-};
+  return rc;
+}
+    
+    namespace fs = boost::filesystem;
+    
+    
+    {
+    {  auto status = EventFactory::deregisterEventSubscriber(sub->getName());
+  EXPECT_TRUE(status.ok());
+}
+}
+
+    
+      void moveBucketTail(const std::shared_ptr<DHTNode>& node);
+    
+    void DHTTaskFactoryImpl::setCommonProperty(
+    const std::shared_ptr<DHTAbstractTask>& task)
+{
+  task->setRoutingTable(routingTable_);
+  task->setMessageDispatcher(dispatcher_);
+  task->setMessageFactory(factory_);
+  task->setTaskQueue(taskQueue_);
+  task->setLocalNode(localNode_);
+}
+    
+    DHTTaskQueueImpl::~DHTTaskQueueImpl() = default;
+    
+    
+    {} // namespace aria2
+    
+    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
+                                             std::chrono::seconds interval)
+    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
+{
+}
+    
+      // do nothing; we don't use this message as outgoing message.
+  virtual bool send() CXX11_OVERRIDE;
