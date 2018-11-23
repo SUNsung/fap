792272e572dd9e20b427ada154d76c19385266df
SUNsung <weatherius@gmail.com>
2018-11-23 09:16:39 +0200

    Rewrite [45q6b9HgJKd8Q4595R3H7EpbzI45IdpOHldHyPM456ko.rb] File in: 2018-11-23 09:16:38

--- ruby/45q6b9HgJKd8Q4595R3H7EpbzI45IdpOHldHyPM456ko.rb
+++ ruby/45q6b9HgJKd8Q4595R3H7EpbzI45IdpOHldHyPM456ko.rb
@@ -1,12 +1,35 @@
 
         
-                    value = options.fetch(:selected) { value() }
-            select = content_tag('select', add_options(option_tags, options, value), html_options)
+        describe 'Dry running an Agent', js: true do
+  let(:agent)   { agents(:bob_website_agent) }
+  let(:formatting_agent) { agents(:bob_formatting_agent) }
+  let(:user)    { users(:bob) }
+  let(:emitter) { agents(:bob_weather_agent) }
     
-            class RadioButtonBuilder < Builder # :nodoc:
-          def radio_button(extra_html_options = {})
-            html_options = extra_html_options.merge(@input_html_options)
-            html_options[:skip_default_ids] = false
-            @template_object.radio_button(@object_name, @method_name, @value, html_options)
-          end
+    describe DotHelper do
+  describe 'with example Agents' do
+    class Agents::DotFoo < Agent
+      default_schedule '2pm'
+    
+        def log_file_info(s)
+      puts '    #{magenta s}'
+    end
+    
+        def read_cached_files(path, files)
+      full_path = '#@cache_path/#@branch_sha/#{path}'
+      contents  = {}
+      if File.directory?(full_path)
+        files.each do |name|
+          path = '#{full_path}/#{name}'
+          contents[name] = File.read(path, mode: 'rb') if File.exists?(path)
         end
+      end
+      contents
+    end
+    
+      class Notification < ApplicationRecord
+  end
+    
+    Then /^I should have (\d+) email delivery$/ do |n|
+  ActionMailer::Base.deliveries.length.should == n.to_i
+end
