1abf790dc46722f3430fb3a34d3917de183b0482
SUNsung <weatherius@gmail.com>
2018-11-23 06:04:43 +0200

    Rewrite [56gUUz5FoLsOI567JBdNyvQZBY56xojVacDP5JQ566xg.cpp] File in: 2018-11-23 06:04:42

--- cccccc/56gUUz5FoLsOI567JBdNyvQZBY56xojVacDP5JQ566xg.cpp
+++ cccccc/56gUUz5FoLsOI567JBdNyvQZBY56xojVacDP5JQ566xg.cpp
@@ -1,359 +1,151 @@
 
         
-        /* This macro must be used in C++98 for calls from inside a template.  But we
- * define this variant in all cases; code that wants to be compatible with both
- * C++98 and C++11 should always use this macro when calling from a template. */
-#define UpbMakeHandlerT(f) upb::MatchFunc(f).template GetFunc<f>()
-    
-    #include <sstream>
-    
-    const FieldGeneratorInfo* Context::GetFieldGeneratorInfo(
-    const FieldDescriptor* field) const {
-  const FieldGeneratorInfo* result =
-      FindOrNull(field_generator_info_map_, field);
-  if (result == NULL) {
-    GOOGLE_LOG(FATAL) << 'Can not find FieldGeneratorInfo for field: '
-               << field->full_name();
+        
+    {  PyObject* np_array =
+      PyArray_SimpleNewFromData(dim_size, dims, type_num, data);
+  if (PyType_Ready(&TensorReleaserType) == -1) {
+    return errors::Unknown('Python type initialization failed.');
   }
-  return result;
+  TensorReleaser* releaser = reinterpret_cast<TensorReleaser*>(
+      TensorReleaserType.tp_alloc(&TensorReleaserType, 0));
+  releaser->destructor = new std::function<void()>(std::move(destructor));
+  if (PyArray_SetBaseObject(reinterpret_cast<PyArrayObject*>(np_array),
+                            reinterpret_cast<PyObject*>(releaser)) == -1) {
+    Py_DECREF(releaser);
+    return errors::Unknown('Python array refused to use memory.');
+  }
+  *result = PyArray_Return(reinterpret_cast<PyArrayObject*>(np_array));
+  return Status::OK();
 }
     
-    ExtensionGenerator* ImmutableGeneratorFactory::NewExtensionGenerator(
-    const FieldDescriptor* descriptor) const {
-  if (HasDescriptorMethods(descriptor->file(), context_->EnforceLite())) {
-    return new ImmutableExtensionGenerator(descriptor, context_);
+    // Must be included first
+#include 'tensorflow/python/lib/core/numpy.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    class InAppPurchase : public mate::EventEmitter<InAppPurchase>,
+                      public in_app_purchase::TransactionObserver {
+ public:
+  static mate::Handle<InAppPurchase> Create(v8::Isolate* isolate);
+    }
+    
+    
+    {}  // namespace api
+    
+    #if defined(OS_LINUX)
+void PowerMonitor::BlockShutdown() {
+  PowerObserverLinux::BlockShutdown();
+}
+    
+     private:
+  v8::Global<v8::Object> menu_;
+  std::unique_ptr<TrayIcon> tray_icon_;
+    
+    namespace atom {
+    }
+    
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
+    
+      // this.emit(name, event, args...);
+  template <typename... Args>
+  bool EmitCustomEvent(const base::StringPiece& name,
+                       v8::Local<v8::Object> event,
+                       const Args&... args) {
+    return EmitWithEvent(
+        name, internal::CreateCustomEvent(isolate(), GetWrapper(), event),
+        args...);
+  }
+    
+      can_factory->RegisterCanClients();
+    
+      void WaitForFinish() {
+    if (thread_send_ != nullptr && thread_send_->joinable()) {
+      thread_send_->join();
+      thread_send_.reset();
+      AINFO << 'Send thread stopped. conf:'
+            << param_ptr_->conf.ShortDebugString();
+    }
+    if (thread_recv_ != nullptr && thread_recv_->joinable()) {
+      thread_recv_->join();
+      thread_recv_.reset();
+      AINFO << 'Recv thread stopped. conf:'
+            << param_ptr_->conf.ShortDebugString();
+    }
+  }
+    
+    /**
+ * @class EsdCanClient
+ * @brief The class which defines a ESD CAN client which inherits CanClient.
+ */
+class EsdCanClient : public CanClient {
+ public:
+  /**
+   * @brief Initialize the ESD CAN client by specified CAN card parameters.
+   * @param parameter CAN card parameters to initialize the CAN client.
+   * @return If the initialization is successful.
+   */
+  bool Init(const CANCardParameter &parameter) override;
+    }
+    
+    bool FakeCanClient::Init(const CANCardParameter &param) { return true; }
+    
+    // buf size must be 8 bytes, every time, we receive only one frame
+const int RX_TIMEOUT = -7;
+apollo::common::ErrorCode HermesCanClient::Receive(
+    std::vector<CanFrame> *const frames, int32_t *const frame_num) {
+  if (!_is_init) {
+    AERROR << 'Hermes can client is not init! Please init first!';
+    return ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED;
+  }
+  if (*frame_num > MAX_CAN_RECV_FRAME_LEN || *frame_num < 0) {
+    AERROR << 'recv can frame num not in range[0, ' << MAX_CAN_RECV_FRAME_LEN
+           << '], frame_num:' << *frame_num;
+    return ErrorCode::CAN_CLIENT_ERROR_FRAME_NUM;
+  }
+    }
+    
+    TEST(HermesCanClient, receiver) {
+  CANCardParameter param;
+  param.set_brand(CANCardParameter::HERMES_CAN);
+  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
+  HermesCanClient hermes_can;
+  EXPECT_TRUE(hermes_can.Init(param));
+    }
+    
+      /**
+   * @brief Get the error string.
+   * @param status The status to get the error string.
+   */
+  std::string GetErrorString(const int32_t status) override;
+    
+    template <typename SensorType>
+void CanReceiver<SensorType>::Stop() {
+  if (IsRunning()) {
+    AINFO << 'Stopping can client receiver ...';
+    is_running_ = false;
+    if (thread_ != nullptr && thread_->joinable()) {
+      thread_->join();
+    }
+    thread_.reset();
   } else {
-    return new ImmutableExtensionLiteGenerator(descriptor, context_);
+    AINFO << 'Can client receiver is not running.';
   }
+  AINFO << 'Can client receiver stopped [ok].';
 }
     
-    void ImmutableMapFieldGenerator::
-GenerateBuilderClearCode(io::Printer* printer) const {
-  printer->Print(
-      variables_,
-      'internalGetMutable$capitalized_name$().clear();\n');
-}
-    
-    void SharedCodeGenerator::Generate(GeneratorContext* context,
-                                   std::vector<string>* file_list,
-                                   std::vector<string>* annotation_file_list) {
-  string java_package = FileJavaPackage(file_);
-  string package_dir = JavaPackageToDir(java_package);
-    }
-    
-    #include <google/protobuf/compiler/objectivec/objectivec_enum_field.h>
-#include <google/protobuf/stubs/common.h>
-#include <google/protobuf/compiler/objectivec/objectivec_helpers.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/wire_format.h>
-#include <google/protobuf/stubs/strutil.h>
-    
-    inline bool operator < (const directory_iterator&, const directory_iterator&)
-{
-   return false;
-}
-    
-    namespace BOOST_REGEX_DETAIL_NS{
-template BOOST_REGEX_DECL void perl_matcher<BOOST_REGEX_CHAR_T const *, match_results< const BOOST_REGEX_CHAR_T* >::allocator_type BOOST_REGEX_TRAITS_T >::construct_init(
-      const basic_regex<BOOST_REGEX_CHAR_T BOOST_REGEX_TRAITS_T >& e, match_flag_type f);
-template BOOST_REGEX_DECL bool perl_matcher<BOOST_REGEX_CHAR_T const *, match_results< const BOOST_REGEX_CHAR_T* >::allocator_type BOOST_REGEX_TRAITS_T >::match();
-template BOOST_REGEX_DECL bool perl_matcher<BOOST_REGEX_CHAR_T const *, match_results< const BOOST_REGEX_CHAR_T* >::allocator_type BOOST_REGEX_TRAITS_T >::find();
-} // namespace
-    
-    
-    {}
-    
-    template <class BidiIterator, class Allocator, class traits>
-bool perl_matcher<BidiIterator, Allocator, traits>::match_then()
-{
-   pstate = pstate->next.p;
-   if(match_all_states())
-      return true;
-   m_can_backtrack = false;
-   m_have_then = true;
-   return false;
-}
-    
-    
-    
-    #ifdef BOOST_MSVC
-#pragma warning(push)
-#pragma warning(disable: 4103)
-#endif
-#ifdef BOOST_HAS_ABI_HEADERS
-#  include BOOST_ABI_PREFIX
-#endif
-#ifdef BOOST_MSVC
-#pragma warning(pop)
-#endif
-    
-    #endif // BOOST_NO_WREGEX
-    
-        // Create the pixel shader
-    {
-        static const char* pixelShader =
-            'struct PS_INPUT\
-            {\
-            float4 pos : SV_POSITION;\
-            float4 col : COLOR0;\
-            float2 uv  : TEXCOORD0;\
-            };\
-            sampler sampler0;\
-            Texture2D texture0;\
-            \
-            float4 main(PS_INPUT input) : SV_Target\
-            {\
-            float4 out_col = input.col * texture0.Sample(sampler0, input.uv); \
-            return out_col; \
-            }';
-    }
-    
-        // Setup display size (every frame to accommodate for window resizing)
-    int w = IwGxGetScreenWidth(), h = IwGxGetScreenHeight();
-    io.DisplaySize = ImVec2((float)w, (float)h);
-     // For retina display or other situations where window coordinates are different from framebuffer coordinates. User storage only, presently not used by ImGui.
-    io.DisplayFramebufferScale = g_scale;
-    
-    // Functions
-bool    ImGui_ImplWin32_Init(void* hwnd)
-{
-    if (!::QueryPerformanceFrequency((LARGE_INTEGER *)&g_TicksPerSecond))
-        return false;
-    if (!::QueryPerformanceCounter((LARGE_INTEGER *)&g_Time))
-        return false;
-    }
-    
-    
-    {    InputTextCallback_UserData cb_user_data;
-    cb_user_data.Str = str;
-    cb_user_data.ChainCallback = callback;
-    cb_user_data.ChainCallbackUserData = user_data;
-    return InputTextMultiline(label, (char*)str->c_str(), str->capacity() + 1, size, flags, InputTextCallback, &cb_user_data);
-}
-
-    
-        // Upload to Buffer:
-    {
-        char* map = NULL;
-        err = vkMapMemory(g_Device, g_UploadBufferMemory, 0, upload_size, 0, (void**)(&map));
-        check_vk_result(err);
-        memcpy(map, pixels, upload_size);
-        VkMappedMemoryRange range[1] = {};
-        range[0].sType = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
-        range[0].memory = g_UploadBufferMemory;
-        range[0].size = upload_size;
-        err = vkFlushMappedMemoryRanges(g_Device, 1, range);
-        check_vk_result(err);
-        vkUnmapMemory(g_Device, g_UploadBufferMemory);
-    }
-    
-    IMGUI_IMPL_API bool     ImGui_ImplDX9_Init(IDirect3DDevice9* device);
-IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
-    
-    IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_Init(const char* glsl_version = NULL);
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
-    
-        float scroll_max = ImMax(1.0f, win_size_contents_v - win_size_avail_v);
-    float scroll_ratio = ImSaturate(scroll_v / scroll_max);
-    float grab_v_norm = scroll_ratio * (scrollbar_size_v - grab_h_pixels) / scrollbar_size_v;
-    if (held && grab_h_norm < 1.0f)
-    {
-        float scrollbar_pos_v = horizontal ? bb.Min.x : bb.Min.y;
-        float mouse_pos_v = horizontal ? g.IO.MousePos.x : g.IO.MousePos.y;
-        float* click_delta_to_grab_center_v = horizontal ? &g.ScrollbarClickDeltaToGrabCenter.x : &g.ScrollbarClickDeltaToGrabCenter.y;
-    }
-    
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-    
-      bool Started() const {
-    return start_;
-  }
-    
-      using DBImpl::Put;
-  virtual Status Put(const WriteOptions& /*options*/,
-                     ColumnFamilyHandle* /*column_family*/,
-                     const Slice& /*key*/, const Slice& /*value*/) override {
-    return Status::NotSupported('Not supported in compacted db mode.');
-  }
-  using DBImpl::Merge;
-  virtual Status Merge(const WriteOptions& /*options*/,
-                       ColumnFamilyHandle* /*column_family*/,
-                       const Slice& /*key*/, const Slice& /*value*/) override {
-    return Status::NotSupported('Not supported in compacted db mode.');
-  }
-  using DBImpl::Delete;
-  virtual Status Delete(const WriteOptions& /*options*/,
-                        ColumnFamilyHandle* /*column_family*/,
-                        const Slice& /*key*/) override {
-    return Status::NotSupported('Not supported in compacted db mode.');
-  }
-  virtual Status Write(const WriteOptions& /*options*/,
-                       WriteBatch* /*updates*/) override {
-    return Status::NotSupported('Not supported in compacted db mode.');
-  }
-  using DBImpl::CompactRange;
-  virtual Status CompactRange(const CompactRangeOptions& /*options*/,
-                              ColumnFamilyHandle* /*column_family*/,
-                              const Slice* /*begin*/,
-                              const Slice* /*end*/) override {
-    return Status::NotSupported('Not supported in compacted db mode.');
-  }
-    
-    #include <cstdio>
-#include <string>
-    
-      // Do some reads and writes to key 'y'
-  // Since the snapshot was advanced, the write done outside of the
-  // transaction does not conflict.
-  s = txn->GetForUpdate(read_options, 'y', &value);
-  txn->Put('y', 'y');
-    
-    void DHTReplaceNodeTask::sendMessage()
-{
-  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
-  if (!questionableNode) {
-    setFinished(true);
-  }
-  else {
-    getMessageDispatcher()->addMessageToQueue(
-        getMessageFactory()->createPingMessage(questionableNode), timeout_,
-        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
-  }
-}
-    
-    void DHTRoutingTable::showBuckets() const
-{
-  /*
-    for(std::deque<std::shared_ptr<DHTBucket> >::const_iterator itr =
-    buckets_.begin(); itr != buckets_.end(); ++itr) {
-    cerr << 'prefix = ' << (*itr)->getPrefixLength() << ', '
-    << 'nodes = ' << (*itr)->countNode() << endl;
-    }
+      /**
+  * @brief module initialization function
+  * @return initialization status
   */
-}
+  apollo::common::Status Init() override;
     
-      void setTaskQueue(DHTTaskQueue* taskQueue);
-    
-    class DownloadEngine;
-class Command;
-    
-    void DHTTaskExecutor::update()
-{
-  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
-                                  std::mem_fn(&DHTTask::finished)),
-                   execTasks_.end());
-  int r;
-  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
-    r = numConcurrent_ - execTasks_.size();
-  }
-  else {
-    r = 0;
-  }
-  while (r && !queue_.empty()) {
-    std::shared_ptr<DHTTask> task = queue_.front();
-    queue_.pop_front();
-    task->startup();
-    if (!task->finished()) {
-      execTasks_.push_back(task);
-      --r;
-    }
-  }
-  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
-                   static_cast<unsigned int>(getExecutingTaskSize()),
-                   static_cast<unsigned int>(getQueueSize())));
-}
-    
-    void DHTTaskQueueImpl::addImmediateTask(const std::shared_ptr<DHTTask>& task)
-{
-  immediateTaskQueue_.addTask(task);
-}
-    
-    #endif // D_DHT_TASK_QUEUE_IMPL_H
-
-    
-      ~DHTTokenTracker();
-    
-      // do nothing; we don't use this message as outgoing message.
-  virtual bool send() CXX11_OVERRIDE;
-    
-    static const int kCrToRedTable[256] = {
-  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
-  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
-  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
-  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
-  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
-   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
-   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
-   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
-   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
-   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
-   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
-     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
-    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
-    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
-    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
-    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
-    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
-   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
-   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
-   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
-   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
-   174,  175,  177,  178
-};
-    
-    ///////////////////////////////////////////////////////////////////////////////
-// Constants (15bit precision) and C macros for IDCT vertical pass
-    
-    #include 'guetzli/gamma_correct.h'
-    
-    #ifndef GUETZLI_IDCT_H_
-#define GUETZLI_IDCT_H_
-    
-      void JumpToByteBoundary() {
-    while (put_bits <= 56) {
-      int c = (put_buffer >> 56) & 0xff;
-      EmitByte(c);
-      put_buffer <<= 8;
-      put_bits += 8;
-    }
-    if (put_bits < 64) {
-      int padmask = 0xff >> (64 - put_bits);
-      int c = ((put_buffer >> 56) & ~padmask) | padmask;
-      EmitByte(c);
-    }
-    put_buffer = 0;
-    put_bits = 64;
-  }
-    
-    bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     const int* quant, JPEGData* jpg) {
-  if (w < 0 || w >= 1 << 16 || h < 0 || h >= 1 << 16 ||
-      rgb.size() != 3 * w * h) {
-    return false;
-  }
-  InitJPEGDataForYUV444(w, h, jpg);
-  AddApp0Data(jpg);
-    }
-    
-      // Compute DHT and SOS marker data sizes and start emitting DHT marker.
-  int num_histo = num_dc_histo + num_ac_histo;
-  histograms.resize(num_histo);
-  int total_count = 0;
-  for (size_t i = 0; i < histograms.size(); ++i) {
-    total_count += histograms[i].NumSymbols();
-  }
-  const size_t dht_marker_len =
-      2 + num_histo * (kJpegHuffmanMaxBitLength + 1) + total_count;
-  const size_t sos_marker_len = 6 + 2 * ncomps;
-  std::vector<uint8_t> data(dht_marker_len + sos_marker_len + 4);
-  size_t pos = 0;
-  data[pos++] = 0xff;
-  data[pos++] = 0xc4;
-  data[pos++] = static_cast<uint8_t>(dht_marker_len >> 8);
-  data[pos++] = dht_marker_len & 0xff;
+    // Sensor gflags
+DECLARE_double(sensor_freq);
