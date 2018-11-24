e9b4811da653641f2eed4a3330edb69842f84db8
SUNsung <weatherius@gmail.com>
2018-11-24 19:34:53 +0200

    Rewrite [02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb] File in: 2018-11-24 19:34:52

--- ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
+++ ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
@@ -1,65 +1,24 @@
 
         
-              def _find_all(path, prefixes, args, outside_app)
-        prefixes = [prefixes] if String === prefixes
-        prefixes.each do |prefix|
-          paths.each do |resolver|
-            if outside_app
-              templates = resolver.find_all_anywhere(path, prefix, *args)
-            else
-              templates = resolver.find_all(path, prefix, *args)
-            end
-            return templates unless templates.empty?
-          end
+                unless post && post.id
+          puts post.errors.full_messages if post
+          puts creator.errors.inspect
+          raise 'Failed to create description for Staff category!'
         end
-        []
-      end
     
-    map = {}
-dups = []
-    
-    unless invalids.empty?
-  puts '\n\nFailed links:'
-  invalids.each do |link|
-    puts '- #{link}'
-  end
-  puts 'Done with errors.'
-  exit(1)
+    post '/msg' do
+  SinatraWorker.perform_async params[:msg]
+  redirect to('/')
 end
     
-        select_agent_type('Trigger Agent')
-    fill_in(:agent_name, with: 'Test Trigger Agent')
-    click_on 'Save'
-    
-          it 'handles signals' do
-        @agent_runner.instance_variable_set(:@signal_queue, ['TERM'])
-        @agent_runner.run
+        UnitOfWork = Struct.new(:queue, :job) do
+      def acknowledge
+        # nothing to do
       end
-    end
     
-      it 'transfers control to the innermost catch block waiting for the same sympol' do
-    one = two = three = 0
-    catch :duplicate do
-      catch :duplicate do
-        catch :duplicate do
-          one = 1
-          throw :duplicate
+            names.inject(Object) do |constant, name|
+          constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
         end
-        two = 2
-        throw :duplicate
       end
-      three = 3
-      throw :duplicate
     end
-    [one, two, three].should == [1, 2, 3]
-  end
-    
-            -> { w.f4(0.1) }.should output(nil, %r|classes.rb:#{w.warn_call_lineno}:|)
-        -> { w.f4(Rational(1, 2)) }.should output(nil, %r|classes.rb:#{w.warn_call_lineno}:|)
-      end
-    
-          alias react deny
-    
-        post('/', {'csrf_param' => token}, 'rack.session' => {:csrf => token})
-    expect(last_response).to be_ok
   end
