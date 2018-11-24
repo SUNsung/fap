890b54d1e659c269c450b34a15b700f6726dc183
SUNsung <weatherius@gmail.com>
2018-11-24 22:41:32 +0200

    Rewrite [153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py] File in: 2018-11-24 22:41:32

--- pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
+++ pyton/153Kg81.5Z5wc156Yz0q.ZBvCY15ek5eiuSEXRY15UkL.py
@@ -1,135 +1,102 @@
 
         
-        DEPLABEL_TO_ID = {label: lid for lid, label in enumerate(DEPLABELS)}
+        '''
+    The approach taken is explained below. I decided to do it simply.
+    Initially I was considering parsing the data into some sort of
+    structure and then generating an appropriate README. I am still
+    considering doing it - but for now this should work. The only issue
+    I see is that it only sorts the entries at the lowest level, and that
+    the order of the top-level contents do not match the order of the actual
+    entries.
     
-      def load_labels(self, session, instances):
-    '''Loads the labels for these instances.
+        if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
     
-    if S < N:
-  # Note that this isn't necessary for this synthetic example, but
-  # it's useful to see how the input factor matrices were initialized
-  # for actual neurophysiology data.
-  datasets = add_alignment_projections(datasets, npcs=FLAGS.npcs)
+        out1 = utils.preprocess_input(x, 'channels_last')
+    out1int = utils.preprocess_input(xint, 'channels_last')
+    out2 = utils.preprocess_input(np.transpose(x, (0, 3, 1, 2)),
+                                  'channels_first')
+    out2int = utils.preprocess_input(np.transpose(xint, (0, 3, 1, 2)),
+                                     'channels_first')
+    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
+    assert_allclose(out1int, out2int.transpose(0, 2, 3, 1))
     
-    from synthetic_data_utils import generate_data, generate_rnn
-from synthetic_data_utils import get_train_n_valid_inds
-from synthetic_data_utils import nparray_and_transpose
-from synthetic_data_utils import spikify_data, split_list_by_inds
-import tensorflow as tf
-from utils import write_datasets
+            self.kernel_z = self.kernel[:, :self.units]
+        self.recurrent_kernel_z = self.recurrent_kernel[:, :self.units]
+        self.kernel_r = self.kernel[:, self.units: self.units * 2]
+        self.recurrent_kernel_r = self.recurrent_kernel[:,
+                                                        self.units:
+                                                        self.units * 2]
+        self.kernel_h = self.kernel[:, self.units * 2:]
+        self.recurrent_kernel_h = self.recurrent_kernel[:, self.units * 2:]
     
-        import pdb
-    pdb.set_trace()
+        with tf.name_scope('augmentation'):
+        shp = tf.shape(inputs)
+        batch_size, height, width = shp[0], shp[1], shp[2]
+        width = tf.cast(width, tf.float32)
+        height = tf.cast(height, tf.float32)
     
-        yield (x, y, w)
+        def _forwards(self, orm):
+        'Write your forwards methods here.'
+        for release in RangeQuerySetWrapperWithProgressBar(
+            orm.Release.objects.exclude(new_groups=0)
+        ):
+            projects = list(release.projects.values_list('id', flat=True))
+            if len(projects) > 1:
+                # do something fancy where we look at Group.first_release
+                # to calculate ReleaseProject.new_group
+                for p_id in projects:
+                    new_groups = orm.Group.objects.filter(
+                        first_release=release, project_id=p_id
+                    ).count()
+                    if not new_groups:
+                        continue
+                    orm.ReleaseProject.objects.filter(
+                        release_id=release.id, project_id=p_id
+                    ).update(new_groups=new_groups)
+            elif len(projects) == 1:
+                # copy Release.new_groups to ReleaseProject.new_group
+                orm.ReleaseProject.objects.filter(
+                    release_id=release.id, project_id=projects[0]
+                ).update(new_groups=release.new_groups)
+    
+            # Deleting model 'DSymApp'
+        db.delete_table('sentry_dsymapp')
+    
+    
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding model 'ReleaseHeadCommit'
+        db.create_table(
+            'sentry_releaseheadcommit', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'organization_id',
+                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(
+                        db_index=True
+                    )
+                ), (
+                    'repository_id',
+                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
+                ), (
+                    'release', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.Release']
+                    )
+                ), (
+                    'commit', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.Commit']
+                    )
+                ),
+            )
+        )
+        db.send_create_signal('sentry', ['ReleaseHeadCommit'])
+    
+        complete_apps = ['sentry']
+    symmetrical = True
 
     
-    
-def calculate_reinforce_objective(hparams,
-                                  log_probs,
-                                  dis_predictions,
-                                  present,
-                                  estimated_values=None):
-  '''Calculate the REINFORCE objectives.  The REINFORCE objective should
-  only be on the tokens that were missing.  Specifically, the final Generator
-  reward should be based on the Discriminator predictions on missing tokens.
-  The log probaibilities should be only for missing tokens and the baseline
-  should be calculated only on the missing tokens.
-    
-      Returns:
-    gen_train_op: Generator training op.
-  '''
-  del hparams
-  with tf.name_scope('train_generator'):
-    if FLAGS.generator_optimizer == 'sgd':
-      gen_optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    elif FLAGS.generator_optimizer == 'adam':
-      gen_optimizer = tf.train.AdamOptimizer(learning_rate)
-    else:
-      raise NotImplementedError
-    gen_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
-    ]
-    print('\nOptimizing Generator vars:')
-    for v in gen_vars:
-      print(v)
-    
-        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
-    logs = boto3_conn(module, conn_type='client', resource='logs', region=region, endpoint=ec2_url, **aws_connect_kwargs)
-    
-        snaked_launch_configs = []
-    for launch_config in launch_configs['LaunchConfigurations']:
-        snaked_launch_configs.append(camel_dict_to_snake_dict(launch_config))
-    
-    
-RETURN = '''
-placement_groups:
-  description: Placement group attributes
-  returned: always
-  type: complex
-  contains:
-    name:
-      description: PG name
-      type: string
-      sample: my-cluster
-    state:
-      description: PG state
-      type: string
-      sample: 'available'
-    strategy:
-      description: PG strategy
-      type: string
-      sample: 'cluster'
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-    
-                if delete_policy:
-                original_policy = ecr.get_repository_policy(registry_id, name)
-    
-        except BotoServerError as e:
-        if e.error_message != 'No modifications were requested.':
-            module.fail_json(msg=e.error_message)
-        else:
-            changed = False
-    
-    
-@AWSRetry.exponential_backoff()
-def list_iam_roles_with_backoff(client, **kwargs):
-    paginator = client.get_paginator('list_roles')
-    return paginator.paginate(**kwargs).build_full_result()
-    
-        Basic Usage:
-        >>> client = boto3.client('kinesis')
-        >>> stream_name = 'test-stream'
-        >>> number_of_shards = 10
-        >>> tags = {'env': 'test'}
-        >>> create_stream(client, stream_name, number_of_shards, tags=tags)
-    
-    
-# -- Options for Texinfo output -------------------------------------------
-    
-        def check_parsing_errors(self, lens):
-        '''Verify Augeas can parse all of the lens files.
-    
-    
-UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
-'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
-    
-        def test_include_missing(self):
-        # This should miss
-        self.verify_fnmatch('test_*.onf', False)
-    
-        def test_nonexistent_generic(self):
-        with mock.patch('certbot.util.get_os_info') as mock_info:
-            mock_info.return_value = ('nonexistent', 'irrelevant')
-            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
-                mock_like.return_value = ['unknonwn']
-                self.assertEqual(entrypoint.get_configurator(),
-                                 configurator.ApacheConfigurator)
+    import markdown
