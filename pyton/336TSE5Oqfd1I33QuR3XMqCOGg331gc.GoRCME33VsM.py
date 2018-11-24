008bb6ea204f8287ca6f8bc8cb91bd8ac97b83e6
SUNsung <weatherius@gmail.com>
2018-11-24 12:26:34 +0200

    Create [336TSE5Oqfd1I33QuR3XMqCOGg331gc.GoRCME33VsM.py] File in: 2018-11-24 12:26:33

---
+++ pyton/336TSE5Oqfd1I33QuR3XMqCOGg331gc.GoRCME33VsM.py
@@ -0,0 +1,199 @@
+    n_samples = 2000
+    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
+    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
+                                           list_n_features, precompute=False)
+    plt.subplot(212)
+    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
+    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
+    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
+    plt.legend(loc='upper left')
+    plt.xlabel('number of features')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
+    
+    from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.metrics import roc_curve, auc
+from sklearn.datasets import fetch_kddcup99, fetch_covtype, fetch_mldata
+from sklearn.preprocessing import LabelBinarizer
+    
+            results['MiniBatchKMeans Speed'].append(delta)
+        results['MiniBatchKMeans Quality'].append(mbkmeans.inertia_)
+    
+    Benchmarks for IncrementalPCA
+    
+        max_it = len(samples_range) * len(features_range)
+    for i_s, n_samples in enumerate(samples_range):
+        for i_f, n_features in enumerate(features_range):
+            it += 1
+            n_informative = n_features / 10
+            print('====================')
+            print('Iteration %03d of %03d' % (it, max_it))
+            print('====================')
+            # dataset_kwargs = {
+            #     'n_train_samples': n_samples,
+            #     'n_test_samples': 2,
+            #     'n_features': n_features,
+            #     'n_informative': n_informative,
+            #     'effective_rank': min(n_samples, n_features) / 10,
+            #     #'effective_rank': None,
+            #     'bias': 0.0,
+            # }
+            dataset_kwargs = {
+                'n_samples': 1,
+                'n_components': n_features,
+                'n_features': n_samples,
+                'n_nonzero_coefs': n_informative,
+                'random_state': 0
+            }
+            print('n_samples: %d' % n_samples)
+            print('n_features: %d' % n_features)
+            y, X, _ = make_sparse_coded_signal(**dataset_kwargs)
+            X = np.asfortranarray(X)
+    
+        (opts, args) = op.parse_args()
+    if len(args) > 0:
+        op.error('this script takes no arguments.')
+        sys.exit(1)
+    opts.n_components = type_auto_or_int(opts.n_components)
+    opts.density = type_auto_or_float(opts.density)
+    selected_transformers = opts.selected_transformers.split(',')
+    
+                gc.collect()
+            print('max_iter', max_iter)
+            print('- benchmarking A-SGD')
+            clf = SGDRegressor(alpha=alpha / n_train, fit_intercept=False,
+                               max_iter=max_iter, learning_rate='invscaling',
+                               eta0=.002, power_t=0.05, tol=1e-3,
+                               average=(max_iter * n_train // 2))
+    
+        if revision is None:
+        return
+    if domain not in ('py', 'pyx'):
+        return
+    if not info.get('module') or not info.get('fullname'):
+        return
+    
+    
+def setup(app):
+    app.add_crossref_type(
+        directivename = 'setting',
+        rolename      = 'setting',
+        indextemplate = 'pair: %s; setting',
+    )
+    app.add_crossref_type(
+        directivename = 'signal',
+        rolename      = 'signal',
+        indextemplate = 'pair: %s; signal',
+    )
+    app.add_crossref_type(
+        directivename = 'command',
+        rolename      = 'command',
+        indextemplate = 'pair: %s; command',
+    )
+    app.add_crossref_type(
+        directivename = 'reqmeta',
+        rolename      = 'reqmeta',
+        indextemplate = 'pair: %s; reqmeta',
+    )
+    app.add_role('source', source_role)
+    app.add_role('commit', commit_role)
+    app.add_role('issue', issue_role)
+    app.add_role('rev', rev_role)
+    
+        def start_parsing(self, url, opts):
+        self.crawler_process.crawl(self.spidercls, **opts.spargs)
+        self.pcrawler = list(self.crawler_process.crawlers)[0]
+        self.crawler_process.start()
+    
+        def run(self, args, opts):
+        if len(args) != 1:
+            raise UsageError()
+        filename = args[0]
+        if not os.path.exists(filename):
+            raise UsageError('File not found: %s\n' % filename)
+        try:
+            module = _import_file(filename)
+        except (ImportError, ValueError) as e:
+            raise UsageError('Unable to load %r: %s\n' % (filename, e))
+        spclasses = list(iter_spider_classes(module))
+        if not spclasses:
+            raise UsageError('No spider found in file: %s\n' % filename)
+        spidercls = spclasses.pop()
+    
+    
+class ReturnsContract(Contract):
+    ''' Contract to check the output of a callback
+    
+        long_description = README,
+    
+        def p(self, stream_id=None):
+        maybe_print('site:                %s' % self.__class__.name)
+        maybe_print('title:               %s' % self.title)
+        if stream_id:
+            # Print the stream
+            print('stream:')
+            self.p_stream(stream_id)
+    
+        Extension is everything from the last dot to the end, ignoring
+    leading dots.  Returns '(root, ext)'; ext may be empty.'''
+    # NOTE: This code must work for text and bytes strings.
+    
+        def test_ismount(self):
+        self.assertIs(posixpath.ismount('/'), True)
+        self.assertIs(posixpath.ismount(b'/'), True)
+    
+        If the resource does not already exist on its own on the file system,
+    a temporary file will be created. If the file was created, the file
+    will be deleted upon exiting the context manager (no exception is
+    raised if the file was deleted prior to the context manager
+    exiting).
+    '''
+    resource = _normalize_path(resource)
+    package = _get_package(package)
+    reader = _get_resource_reader(package)
+    if reader is not None:
+        try:
+            yield Path(reader.resource_path(resource))
+            return
+        except FileNotFoundError:
+            pass
+    else:
+        _check_location(package)
+    # Fall-through for both the lack of resource_path() *and* if
+    # resource_path() raises FileNotFoundError.
+    package_directory = Path(package.__spec__.origin).parent
+    file_path = package_directory / resource
+    if file_path.exists():
+        yield file_path
+    else:
+        with open_binary(package, resource) as fp:
+            data = fp.read()
+        # Not using tempfile.NamedTemporaryFile as it leads to deeper 'try'
+        # blocks due to the need to close the temporary file to work on
+        # Windows properly.
+        fd, raw_path = tempfile.mkstemp()
+        try:
+            os.write(fd, data)
+            os.close(fd)
+            yield Path(raw_path)
+        finally:
+            try:
+                os.remove(raw_path)
+            except FileNotFoundError:
+                pass
+    
+    import pickle
+import sqlite3
+from collections import namedtuple
+    
+    from argparse import ArgumentParser
+    
+        #Required strings to create intermediate HTML files
+    header = '<html><head><link rel=stylesheet type=text/css href=' + colorscheme + '.css></head><body>\n'
+    footer = '</body></html>'
+    title_content = '<h1 class=titlemain>tldr pages</h1><h4 class=titlesub>Simplified and community driven man pages</h4></body></html>'
