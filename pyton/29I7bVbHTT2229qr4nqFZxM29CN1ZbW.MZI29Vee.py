5f4c2bae53468eaa0f9fabf4b77af3d8a5432528
SUNsung <weatherius@gmail.com>
2018-11-24 19:16:21 +0200

    Rewrite [29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py] File in: 2018-11-24 19:16:21

--- pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
+++ pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
@@ -1,207 +1,100 @@
 
         
         
-def ensure_rule_present(client, module):
+with open(BIN_FILE_PATH, 'rb') as f:
+    BIN_FILE_CONTENT = f.read()
+    
+    
+def test_default_options(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http(httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['form'] == {'foo': 'bar'}
+    
+    import pytest
+    
+    error_msg = None
+    
+    
+def ensure_web_acl_present(client, module):
+    changed = False
+    result = None
     name = module.params['name']
-    rule_id = get_rule_by_name(client, module, name)
-    params = dict()
-    if rule_id:
-        return find_and_update_rule(client, module, rule_id)
+    web_acl_id = get_web_acl_by_name(client, module, name)
+    if web_acl_id:
+        (changed, result) = find_and_update_web_acl(client, module, web_acl_id)
     else:
-        params['Name'] = module.params['name']
         metric_name = module.params['metric_name']
         if not metric_name:
-            metric_name = re.sub(r'[^a-zA-Z0-9]', '', module.params['name'])
-        params['MetricName'] = metric_name
+            metric_name = re.sub(r'[^A-Za-z0-9]', '', module.params['name'])
+        default_action = module.params['default_action'].upper()
         try:
-            new_rule = run_func_with_change_token_backoff(client, module, params, client.create_rule)['Rule']
+            params = {'Name': name, 'MetricName': metric_name, 'DefaultAction': {'Type': default_action}}
+            new_web_acl = run_func_with_change_token_backoff(client, module, params, client.create_web_acl)
         except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-            module.fail_json_aws(e, msg='Could not create rule')
-        return find_and_update_rule(client, module, new_rule['RuleId'])
+            module.fail_json_aws(e, msg='Could not create Web ACL')
+        (changed, result) = find_and_update_web_acl(client, module, new_web_acl['WebACL']['WebACLId'])
+    return changed, result
     
-        images.sort(key=lambda e: e.get('creation_date', ''))  # it may be possible that creation_date does not always exist
-    module.exit_json(images=images)
-    
-        results = dict(changed=False)
-    if module.params['state'] == 'present':
-        if existing['CustomerGateways']:
-            existing['CustomerGateway'] = existing['CustomerGateways'][0]
-            results['gateway'] = existing
-            if existing['CustomerGateway']['Tags']:
-                tag_array = existing['CustomerGateway']['Tags']
-                for key, value in enumerate(tag_array):
-                    if value['Key'] == 'Name':
-                        current_name = value['Value']
-                        if current_name != name:
-                            results['name'] = gw_mgr.tag_cgw_name(
-                                results['gateway']['CustomerGateway']['CustomerGatewayId'],
-                                module.params['name'],
-                            )
-                            results['changed'] = True
-        else:
-            if not module.check_mode:
-                results['gateway'] = gw_mgr.ensure_cgw_present(
-                    module.params['bgp_asn'],
-                    module.params['ip_address'],
-                )
-                results['name'] = gw_mgr.tag_cgw_name(
-                    results['gateway']['CustomerGateway']['CustomerGatewayId'],
-                    module.params['name'],
-                )
-            results['changed'] = True
-    
-    
-RETURN = '''
-placement_groups:
-  description: Placement group attributes
-  returned: always
-  type: complex
-  contains:
-    name:
-      description: PG name
-      type: string
-      sample: my-cluster
-    state:
-      description: PG state
-      type: string
-      sample: 'available'
-    strategy:
-      description: PG strategy
-      type: string
-      sample: 'cluster'
-    
-        if not region:
-        module.fail_json(msg=str('Either region or AWS_REGION or EC2_REGION environment variable or boto config aws_region or ec2_region must be set.'))
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import (HAS_BOTO3, boto3_conn, camel_dict_to_snake_dict, ec2_argument_spec,
-                                      get_aws_connection_info)
-    
-    import os, json, imp
-here = os.path.abspath(os.path.dirname(__file__))
-proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
-try:
-    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
-except:
-    README = ''
-CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
-VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
-    
-    from ..common import *
-    
-            link_list = self.get_streams_by_id(account_number, video_id)
-    
-    __all__ = ['cntv_download', 'cntv_download_by_id']
-    
-    
-def download_url(url, merge, output_dir, title, info_only):
-    mime, ext, size = url_info(url)
-    print_info(site_info, title, mime, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge)
-    
-        title = r1(r'<title id='pageTitle'>(.+)</title>', html)
-    
-        title = html['data']['tv']['title']
-    
-    class norm_error(Exception):
-    '''Path cannot be normalized'''
-    
-        @property
-    def suffixes(self):
-        '''A list of the final component's suffixes, if any.'''
-        name = self.name
-        if name.endswith('.'):
-            return []
-        name = name.lstrip('.')
-        return ['.' + suffix for suffix in name.split('.')[1:]]
-    
-        @unittest.skipUnless(support.can_symlink(),
-                         'Test requires symlink support')
-    def test_ismount_symlinks(self):
-        # Symlinks are never mountpoints.
+        def get_template(self, stack_name):
         try:
-            os.symlink('/', ABSTFN)
-            self.assertIs(posixpath.ismount(ABSTFN), False)
-        finally:
-            os.unlink(ABSTFN)
+            response = self.client.get_template(StackName=stack_name)
+            return response.get('TemplateBody')
+        except Exception as e:
+            self.module.fail_json(msg='Error getting stack template - ' + str(e), exception=traceback.format_exc())
     
-    
-def read_text(package: Package,
-              resource: Resource,
-              encoding: str = 'utf-8',
-              errors: str = 'strict') -> str:
-    '''Return the decoded string of the resource.
-    
-    # Set our expectation for the default encoding used in the C locale
-# for the filesystem encoding and the standard streams
-EXPECTED_C_LOCALE_STREAM_ENCODING = 'ascii'
-EXPECTED_C_LOCALE_FS_ENCODING = 'ascii'
+        def create_client(self, resource):
+        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(self.module, boto3=True)
+        self.client = boto3_conn(self.module, conn_type='client', resource=resource, region=region, endpoint=ec2_url, **aws_connect_kwargs)
     
     
 if __name__ == '__main__':
     main()
 
     
-    # We can extract the richest alternative in order to display it:
-richest = msg.get_body()
-partfiles = {}
-if richest['content-type'].maintype == 'text':
-    if richest['content-type'].subtype == 'plain':
-        for line in richest.get_content().splitlines():
-            print(line)
-        sys.exit()
-    elif richest['content-type'].subtype == 'html':
-        body = richest
-    else:
-        print('Don't know how to display {}'.format(richest.get_content_type()))
-        sys.exit()
-elif richest['content-type'].content_type == 'multipart/related':
-    body = richest.get_body(preferencelist=('html'))
-    for part in richest.iter_attachments():
-        fn = part.get_filename()
-        if fn:
-            extension = os.path.splitext(part.get_filename())[1]
-        else:
-            extension = mimetypes.guess_extension(part.get_content_type())
-        with tempfile.NamedTemporaryFile(suffix=extension, delete=False) as f:
-            f.write(part.get_content())
-            # again strip the <> to go from email form of cid to html form.
-            partfiles[part['content-id'][1:-1]] = f.name
-else:
-    print('Don't know how to display {}'.format(richest.get_content_type()))
-    sys.exit()
-with tempfile.NamedTemporaryFile(mode='w', delete=False) as f:
-    # The magic_html_parser has to rewrite the href='cid:....' attributes to
-    # point to the filenames in partfiles.  It also has to do a safety-sanitize
-    # of the html.  It could be written using html.parser.
-    f.write(magic_html_parser(body.get_content(), partfiles))
-webbrowser.open(f.name)
-os.remove(f.name)
-for fn in partfiles.values():
-    os.remove(fn)
+    - name: create a VPC with dedicated tenancy and a couple of tags
+  ec2_vpc_net:
+    name: Module_dev2
+    cidr_block: 10.10.0.0/16
+    region: us-east-1
+    tags:
+      module: ec2_vpc_net
+      this: works
+    tenancy: dedicated
+'''
     
+    - name: delete-policy
+  ecs_ecr:
+    name: needs-no-policy
+    delete_policy: yes
+'''
     
-def main():
-    parser = ArgumentParser(description='''\
-Unpack a MIME message into a directory of files.
-''')
-    parser.add_argument('-d', '--directory', required=True,
-                        help='''Unpack the MIME message into the named
-                        directory, which will be created if it doesn't already
-                        exist.''')
-    parser.add_argument('msgfile')
-    args = parser.parse_args()
+        def extract_service_from(self, service):
+        # some fields are datetime which is not JSON serializable
+        # make them strings
+        if 'deployments' in service:
+            for d in service['deployments']:
+                if 'createdAt' in d:
+                    d['createdAt'] = str(d['createdAt'])
+                if 'updatedAt' in d:
+                    d['updatedAt'] = str(d['updatedAt'])
+        if 'events' in service:
+            if not self.module.params['events']:
+                del service['events']
+            else:
+                for e in service['events']:
+                    if 'createdAt' in e:
+                        e['createdAt'] = str(e['createdAt'])
+        return service
     
-    document = '''\
-<slideshow>
-<title>Demo slideshow</title>
-<slide><title>Slide title</title>
-<point>This is a demo</point>
-<point>Of a program for processing slides</point>
-</slide>
+    # Fail if the server certificate name was not found
+- iam_server_certificate_facts:
+    name: production-cert
+  register: server_cert
+  failed_when: '{{ server_cert.results | length == 0 }}'
+'''
+    
+        lambda_facts = dict()
+    params = dict()
+    function_name = module.params.get('function_name')
