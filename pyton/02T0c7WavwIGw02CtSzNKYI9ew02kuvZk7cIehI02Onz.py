7f9e124e9b06cec46da8e78c70ce110c2d6c6432
SUNsung <weatherius@gmail.com>
2018-11-24 05:02:59 +0200

    Rewrite [02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py] File in: 2018-11-24 05:02:59

--- pyton/02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py
+++ pyton/02T0c7WavwIGw02CtSzNKYI9ew02kuvZk7cIehI02Onz.py
@@ -1,173 +1,187 @@
 
         
-              for iii in zip(idxs[0], shuffle_tidxs, idxs[2]):
-        S_bxtxd[iii] += 1
+        # Allow direct execution
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    
+        with open(FISH_COMPLETION_TEMPLATE) as f:
+        template = f.read()
+    filled_template = template.replace('{{commands}}', '\n'.join(commands))
+    with open(FISH_COMPLETION_FILE, 'w') as f:
+        f.write(filled_template)
     
     
-def plot_lfads_timeseries(data_bxtxn, model_vals, ext_input_bxtxi=None,
-                          truth_bxtxn=None, bidx=None, output_dist='poisson',
-                          conversion_factor=1.0, subplot_cidx=0,
-                          col_title=None):
+def get_base_name(base):
+    if base is InfoExtractor:
+        return 'LazyLoadExtractor'
+    elif base is SearchInfoExtractor:
+        return 'LazyLoadSearchExtractor'
+    else:
+        return base.__name__
     
-    def get_train_n_valid_inds(num_trials, train_fraction, nreplications):
-  '''Split the numbers between 0 and num_trials-1 into two portions for
-  training and validation, based on the train fraction.
-  Args:
-    num_trials: the number of trials
-    train_fraction: (e.g. .80)
-    nreplications: the number of spiking trials per initial condition
-  Returns:
-    a 2-tuple of two lists: the training indices and validation indices
+        if not releases:
+        break
+    
+        def test_youtube_extract(self):
+        assertExtractId = lambda url, id: self.assertEqual(YoutubeIE.extract_id(url), id)
+        assertExtractId('http://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
+        assertExtractId('https://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
+        assertExtractId('https://www.youtube.com/watch?feature=player_embedded&v=BaW_jenozKc', 'BaW_jenozKc')
+        assertExtractId('https://www.youtube.com/watch_popup?v=BaW_jenozKc', 'BaW_jenozKc')
+        assertExtractId('http://www.youtube.com/watch?v=BaW_jenozKcsharePLED17F32AD9753930', 'BaW_jenozKc')
+        assertExtractId('BaW_jenozKc', 'BaW_jenozKc')
+    
+        if state == 'present':
+        cwe_rule_manager.ensure_present()
+    elif state == 'disabled':
+        cwe_rule_manager.ensure_disabled()
+    elif state == 'absent':
+        cwe_rule_manager.ensure_absent()
+    else:
+        module.fail_json(msg='Invalid state '{0}' provided'.format(state))
+    
+    EXAMPLES = '''
+# Simple snapshot of volume using volume_id
+- ec2_snapshot:
+    volume_id: vol-abcdef12
+    description: snapshot of /data from DB123 taken 2013/11/28 12:18:32
+    
+    
+def delete_eigw(module, conn, eigw_id):
     '''
-  train_inds = []
-  valid_inds = []
-  for i in range(num_trials):
-    # This line divides up the trials so that within one initial condition,
-    # the randomness of spikifying the condition is shared among both
-    # training and validation data splits.
-    if (i % nreplications)+1 > train_fraction * nreplications:
-      valid_inds.append(i)
-    else:
-      train_inds.append(i)
+    Delete EIGW.
     
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+        argument_spec = ec2_argument_spec()
+    argument_spec.update(dict(
+        details=dict(type='bool', default=False),
+        events=dict(type='bool', default=True),
+        cluster=dict(),
+        service=dict(type='list')
+    ))
     
+    from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import boto3_conn, ec2_argument_spec, get_aws_connection_info
+from ansible.module_utils.ec2 import camel_dict_to_snake_dict, AWSRetry
+from ansible.module_utils.ec2 import boto3_tag_list_to_ansible_dict
     
-def write_masked_log(log, id_to_word, sequence_eval, present_eval):
-  indices_arr = np.asarray(sequence_eval)
-  samples = convert_to_human_readable(id_to_word, indices_arr, present_eval,
-                                      FLAGS.batch_size)
-  for sample in samples:
-    log.write(sample + '\n')
-  log.flush()
-  return samples
+        :param key:
+    :return:
+    '''
     
-      # Averages for real and fake token values.
-  real = tf.mul(values, present)
-  fake = tf.mul(values, missing)
-  real_avg = tf.reduce_sum(real) / real_count
-  fake_avg = tf.reduce_sum(fake) / fake_count
+    DOCUMENTATION = '''
+---
+module: rds_snapshot_facts
+version_added: '2.6'
+short_description: obtain facts about one or more RDS snapshots
+description:
+  - obtain facts about one or more RDS snapshots. These can be for unclustered snapshots or snapshots of clustered DBs (Aurora)
+  - Aurora snapshot facts may be obtained if no identifier parameters are passed or if one of the cluster parameters are passed.
+options:
+  db_snapshot_identifier:
+    description:
+      - Name of an RDS (unclustered) snapshot. Mutually exclusive with I(db_instance_identifier), I(db_cluster_identifier), I(db_cluster_snapshot_identifier)
+    required: false
+    aliases:
+      - snapshot_name
+  db_instance_identifier:
+    description:
+      - RDS instance name for which to find snapshots. Mutually exclusive with I(db_snapshot_identifier), I(db_cluster_identifier),
+        I(db_cluster_snapshot_identifier)
+    required: false
+  db_cluster_identifier:
+    description:
+      - RDS cluster name for which to find snapshots. Mutually exclusive with I(db_snapshot_identifier), I(db_instance_identifier),
+        I(db_cluster_snapshot_identifier)
+    required: false
+  db_cluster_snapshot_identifier:
+    description:
+      - Name of an RDS cluster snapshot. Mutually exclusive with I(db_instance_identifier), I(db_snapshot_identifier), I(db_cluster_identifier)
+    required: false
+  snapshot_type:
+    description:
+      - Type of snapshot to find. By default both automated and manual
+        snapshots will be returned.
+    required: false
+    choices: ['automated', 'manual', 'shared', 'public']
+requirements:
+    - 'python >= 2.6'
+    - 'boto3'
+author:
+    - 'Will Thames (@willthames)'
+extends_documentation_fragment:
+    - aws
+    - ec2
+'''
     
-        # Cumulative Discounted Returns.  The true value function V*(s).
-    cumulative_rewards = []
-    for t in xrange(FLAGS.sequence_length):
-      cum_value = tf.zeros(shape=[FLAGS.batch_size / 2])
-      for s in xrange(t, FLAGS.sequence_length):
-        cum_value += reward_missing_list[s] * np.power(gamma, (
-            s - t)) * rewards_list[s]
-      cumulative_rewards.append(cum_value)
-    cumulative_rewards = tf.stack(cumulative_rewards, axis=1)
+    # Copyright 2014 Jens Carl, Hothead Games Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
     
-      Returns:
-    gen_train_op: Generator training op.
-  '''
-  del hparams
-  with tf.name_scope('train_generator'):
-    if FLAGS.generator_optimizer == 'sgd':
-      gen_optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    elif FLAGS.generator_optimizer == 'adam':
-      gen_optimizer = tf.train.AdamOptimizer(learning_rate)
-    else:
-      raise NotImplementedError
-    gen_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
-    ]
-    print('\nOptimizing Generator vars:')
-    for v in gen_vars:
-      print(v)
-    
-        ## Discriminator Variables/Savers.
-    if FLAGS.discriminator_model == 'rnn_nas':
-      dis_variable_maps = variable_mapping.rnn_nas(hparams, model='dis')
-      dis_init_saver = tf.train.Saver(var_list=dis_variable_maps)
-      init_savers['dis_init_saver'] = dis_init_saver
-    
-          next context state: `context_state`, this decoder function does not
-      modify the given context state. The context state could be modified when
-      applying e.g. beam search.
-    
-        releaser = GitHubReleaser()
-    
-        print('20 newsgroups')
-    print('=============')
-    print('X_train.shape = {0}'.format(X_train.shape))
-    print('X_train.format = {0}'.format(X_train.format))
-    print('X_train.dtype = {0}'.format(X_train.dtype))
-    print('X_train density = {0}'
-          ''.format(X_train.nnz / np.product(X_train.shape)))
-    print('y_train {0}'.format(y_train.shape))
-    print('X_test {0}'.format(X_test.shape))
-    print('X_test.format = {0}'.format(X_test.format))
-    print('X_test.dtype = {0}'.format(X_test.dtype))
-    print('y_test {0}'.format(y_test.shape))
-    print()
-    
-            X = np.random.randn(n_samples, n_features)
-        Y = np.random.randn(n_samples)
-    
-                gc.collect()
-            print('benchmarking lars_path (with Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            G = np.dot(X.T, X)  # precomputed Gram matrix
-            Xy = np.dot(X.T, y)
-            lars_path(X, y, Xy=Xy, Gram=G, method='lasso')
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            results['lars_path (with Gram)'].append(delta)
-    
-                gc.collect()
-            print('benchmarking scikit-learn randomized_svd: n_iter=%d '
-                  % n_iter)
-            tstart = time()
-            randomized_svd(X, rank, n_iter=n_iter)
-            results['scikit-learn randomized_svd (n_iter=%d)'
-                    % n_iter].append(time() - tstart)
-    
-        ###########################################################################
-    # Set transformer input
-    ###########################################################################
-    transformers = {}
-    
-        ###########################################################################
-    # Remove unspecified algorithm
-    sampling_algorithm = dict((key, value)
-                              for key, value in sampling_algorithm.items()
-                              if key in selected_algorithm)
-    
-    Invoke with
------------
+            print('Training %s ... ' % name, end='')
+        t0 = time()
+        clf.fit(X_train, y_train)
+        train_time[name] = time() - t0
+        t0 = time()
+        y_pred = clf.predict(X_test)
+        test_time[name] = time() - t0
+        accuracy[name] = accuracy_score(y_test, y_pred)
+        print('done')
     
     
-def run_vectorizer(Vectorizer, X, **params):
-    def f():
-        vect = Vectorizer(**params)
-        vect.fit_transform(X)
-    return f
+def generate_logistic_dataset(size):
+    X = np.sort(np.random.normal(size=size))
+    return np.random.random(size=size) < 1.0 / (1.0 + np.exp(-X))
     
-            self.http_client = simple_http_client.Client(self.proxy, timeout=10)
+            # dummy point plot to stick the legend to since surface plot do not
+        # support legends (yet?)
+        # ax.plot([1], [1], [1], color=c, label=label)
+    
+        for n_samples in sample_sizes:
+        X = random_state.rand(n_samples, 300)
+    
+        res.append(bench)
     
         try:
-        # hide console in MS windows
-        startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-        startupinfo.wShowWindow = subprocess.SW_HIDE
+        fn = inspect.getsourcefile(obj)
+    except Exception:
+        fn = None
+    if not fn:
+        try:
+            fn = inspect.getsourcefile(sys.modules[obj.__module__])
+        except Exception:
+            fn = None
+    if not fn:
+        return
     
-    
-    def getLastRewriteTokenIndex(self, programName=DEFAULT_PROGRAM_NAME):
-        return self.lastRewriteTokenIndexes.get(programName, -1)
-    
-    HEADER_ARGS = {'Strict-Transport-Security': HSTS_ARGS}
+        print('Decompressing %s' % ARCHIVE_NAME)
+    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
+        archive.extractall(path='.')
+    os.remove(ARCHIVE_NAME)
 
     
+    ##
+# imaginary tree navigation type; traverse 'get child' link
+DOWN = 2
+##
+#imaginary tree navigation type; finish with a child list
+UP = 3
     
-    def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return (self.filep == other.filep and self.path == other.path and
-                    self.addrs == other.addrs and
-                    self.get_names() == other.get_names() and
-                    self.ssl == other.ssl and
-                    self.enabled == other.enabled and
-                    self.modmacro == other.modmacro)
+            Terence implemented packed table initializers, because Java has a
+        size restriction on .class files and the lookup tables can grow
+        pretty large. The generated JavaLexer.java of the Java.g example
+        would be about 15MB with uncompressed array initializers.
+    
+    
+    def getSourceName(self):
+        return self.name
+    
+        def get_names(self):
+        '''Return a set of all names.'''
+        all_names = set()  # type: Set[str]
+        all_names.update(self.aliases)
+        # Strip out any scheme:// and <port> field from servername
+        if self.name is not None:
+            all_names.add(VirtualHost.strip_name.findall(self.name)[0])
+    
+    class EntryPointTest(unittest.TestCase):
+    '''Entrypoint tests'''
