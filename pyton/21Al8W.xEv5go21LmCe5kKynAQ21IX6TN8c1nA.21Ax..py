71038398a74699b8c8d423d99791e8d60f859d8d
SUNsung <weatherius@gmail.com>
2018-11-24 18:17:00 +0200

    Rewrite [21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py] File in: 2018-11-24 18:17:00

--- pyton/21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py
+++ pyton/21Al8W.xEv5go21LmCe5kKynAQ21IX6TN8c1nA.21Ax..py
@@ -1,150 +1,48 @@
 
         
-                RESULT = ('.' + domain + '\n' in LIST or '\n' + domain + '\n' in LIST)
+                self.handler = handler or consume_socket_content
+        self.handler_results = []
     
+    # ---------
+# Specifics
+# ---------
     
-parser = youtube_dl.parseOpts()[0]
-build_completion(parser)
-
-    
-    
-def openssl_encode(algo, key, iv):
-    cmd = ['openssl', 'enc', '-e', '-' + algo, '-K', hex_str(key), '-iv', hex_str(iv)]
-    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, _ = prog.communicate(secret_msg)
-    return out
-    
-    signature = hexlify(rsa.pkcs1.sign(json.dumps(versions_info, sort_keys=True).encode('utf-8'), privkey, 'SHA-256')).decode()
-print('signature: ' + signature)
-    
-        bug_text = re.search(
-        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
-    dev_text = re.search(
-        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
-        readme).group(1)
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    # -- General configuration ------------------------------------------------
-    
-    from youtube_dl.aes import aes_decrypt, aes_encrypt, aes_cbc_decrypt, aes_cbc_encrypt, aes_decrypt_text
-from youtube_dl.utils import bytes_to_intlist, intlist_to_bytes
-import base64
-    
-    
-class TestAgeRestriction(unittest.TestCase):
-    def _assert_restricted(self, url, filename, age, old_age=None):
-        self.assertTrue(_download_restricted(url, filename, old_age))
-        self.assertFalse(_download_restricted(url, filename, age))
-    
-    
-from test.helper import FakeYDL
-from youtube_dl.cache import Cache
-    
-        if layer is None or node_index:
-        layer, node_index, _ = tensor._keras_history
-    if not layer._inbound_nodes:
-        return [tensor]
-    else:
-        node = layer._inbound_nodes[node_index]
-        if not node.inbound_layers:
-            # Reached an Input layer, stop recursion.
-            return node.input_tensors
-        else:
-            source_tensors = []
-            for i in range(len(node.inbound_layers)):
-                x = node.input_tensors[i]
-                layer = node.inbound_layers[i]
-                node_index = node.node_indices[i]
-                previous_sources = get_source_inputs(x,
-                                                     layer,
-                                                     node_index)
-                # Avoid input redundancy.
-                for x in previous_sources:
-                    if x not in source_tensors:
-                        source_tensors.append(x)
-            return source_tensors
-
-    
-        # Output shape
-        - if `return_sequences`
-             - if data_format='channels_first'
-                5D tensor with shape:
-                `(samples, time, filters, output_row, output_col)`
-             - if data_format='channels_last'
-                5D tensor with shape:
-                `(samples, time, output_row, output_col, filters)`
-        - else
-            - if data_format='channels_first'
-                4D tensor with shape:
-                `(samples, filters, output_row, output_col)`
-            - if data_format='channels_last'
-                4D tensor with shape:
-                `(samples, output_row, output_col, filters)`
-            where o_row and o_col depend on the shape of the filter and
-            the padding
-    
-        def __init__(self, units,
-                 kernel_initializer='glorot_uniform',
-                 recurrent_initializer='orthogonal',
-                 bias_initializer='zeros',
-                 kernel_regularizer=None,
-                 recurrent_regularizer=None,
-                 bias_regularizer=None,
-                 activity_regularizer=None,
-                 kernel_constraint=None,
-                 recurrent_constraint=None,
-                 bias_constraint=None,
-                 return_sequences=False,
-                 return_state=False,
-                 stateful=False,
-                 **kwargs):
-        self.units = units
-        super(CuDNNGRU, self).__init__(
-            return_sequences=return_sequences,
-            return_state=return_state,
-            stateful=stateful,
-            **kwargs)
+    from requests.help import info
     
     Loosely based on https://github.com/astropy/astropy/pull/347
 '''
     
-        def random_see(dev_id, name):
-        '''Randomize a sighting.'''
-        see(
-            dev_id=dev_id,
-            host_name=name,
-            gps=(hass.config.latitude + offset(),
-                 hass.config.longitude + offset()),
-            gps_accuracy=random.randrange(50, 150),
-            battery=random.randrange(10, 90)
-        )
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_TYPE, default=DEFAULT_TYPE): cv.string,
+})
+    
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+})
+    
+        def see_vehicle(vehicle):
+        '''Handle the reporting of the vehicle position.'''
+        host_name = voc.vehicle_name(vehicle)
+        dev_id = 'volvo_{}'.format(slugify(host_name))
+        see(dev_id=dev_id,
+            host_name=host_name,
+            gps=(vehicle.position['latitude'],
+                 vehicle.position['longitude']),
+            icon='mdi:car')
+    
+                    if req.status_code != 200:
+                    _LOGGER.warning(
+                        'downloading '%s' failed, status_code=%d',
+                        url,
+                        req.status_code)
     
     
-def get_scanner(_hass, config):
-    '''Validate the configuration and return a Nmap scanner.'''
-    scanner = HitronCODADeviceScanner(config[DOMAIN])
-    
-            _LOGGER.info('Scanner initialized')
-    
-            try:
-            with async_timeout.timeout(10):
-                # Format the URL here, so we can log the template URL if
-                # anything goes wrong without exposing username and password.
-                url = self.tadoapiurl.format(
-                    home_id=self.home_id, username=self.username,
-                    password=self.password)
-    
-            Return boolean if scanning successful.
-        '''
-        if not self.success_init:
-            return False
-    
-        dispatcher_connect(hass, SIGNAL_VEHICLE_SEEN, see_vehicle)
-    dispatcher_send(hass, SIGNAL_VEHICLE_SEEN, vehicle)
+def create_event_handler(patterns, hass):
+    '''Return the Watchdog EventHandler object.'''
+    from watchdog.events import PatternMatchingEventHandler
     
     *Where is the pattern used practically?
 In graphics editors a shape can be basic or complex. An example of a
@@ -154,35 +52,39 @@ such as rendering the shape to screen, and since shapes follow a
 part-whole hierarchy, composite pattern can be used to enable the
 program to deal with all shapes uniformly.
     
-            def wrapper(*args, **kwargs):
-            return attr(*args, **kwargs)
-        return wrapper
+        def test_3rd_am_station_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1510'
+        self.assertEqual(station, expected_station)
     
-        def _instance_method_1(self):
-        print('Value {}'.format(self.x1))
+            depending on self.param value
+        '''
+        self._instance_method_choices[self.param].__get__(self)()
     
+    
+class Handler(object):
+    __metaclass__ = abc.ABCMeta
+    
+        def prepare(self):
+        print('Reporter Class is preparing to report the results')
+        time.sleep(0.1)
+    
+        def subscribe(self, msg):
+        self.provider.subscribe(msg, self)
+    
+        @property
+    def _lazy_property(self):
+        if not hasattr(self, attr):
+            setattr(self, attr, fn(self))
+        return getattr(self, attr)
+    
+    
+class MidnightTimeProvider(object):
     '''
-http://web.archive.org/web/20120309135549/http://dpip.testingperspective.com/?p=28
+    Class implemented as hard-coded stub (in contrast to configurable stub).
+    '''
     
-    
-class NumObj(object):
-    def __init__(self, value):
-        self.value = value
-    
-        fftv.publish('cartoon')
-    fftv.publish('music')
-    fftv.publish('ads')
-    fftv.publish('movie')
-    fftv.publish('cartoon')
-    fftv.publish('cartoon')
-    fftv.publish('movie')
-    fftv.publish('blank')
-    
-    
-# Abstract Building
-class Building(object):
-    def __init__(self):
-        self.build_floor()
-        self.build_size()
-    
-    class TimeDisplay(object):
+        def on_fault_trigger(self):
+        super(Active, self).perform_switchover()
+        super(Active, self).on_fault_trigger()
