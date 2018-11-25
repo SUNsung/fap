03f5f294f31ca94d791de3f1442bb538fdc5e2ea
SUNsung <weatherius@gmail.com>
2018-11-25 23:31:22 +0200

    Rewrite [16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp] File in: 2018-11-25 23:31:22

--- cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
+++ cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
@@ -1,301 +1,86 @@
 
         
-        #include <string>
-#include <vector>
-#include 'tensorflow/core/framework/op_def.pb.h'
-#include 'tensorflow/core/framework/op_gen_lib.h'
-#include 'tensorflow/core/platform/types.h'
-    
-      int width = 35;
-  int width_narrow = 15;
-  int width_wide = 20;
-  os << std::setw(width + 1) << 'Op,';
-  os << std::setw(width_narrow + 1) << 'Count,';
-  os << std::setw(width_wide + 1) << 'Measured time (ns),';
-  os << std::setw(width_narrow + 2) << 'Time percent,';
-  os << std::setw(width_narrow + 2) << 'Acc percent,';
-  os << std::setw(width_wide + 1) << 'Analytical upper,';
-  os << std::setw(width_wide + 1) << 'Analytical lower,';
-  os << std::setw(width_narrow + 2) << 'Overall eff';
-  os << std::setw(width_narrow + 2) << 'Compute eff';
-  os << std::setw(width_narrow + 2) << 'Memory eff' << std::endl;
-  float acc_percent = 0;
-  for (const auto& op : ops_) {
-    double percent = static_cast<double>(op.time) /
-                     static_cast<double>(total_time_measured_serialized_);
-    double eff =
-        static_cast<double>(op.time_upper) / static_cast<double>(op.time);
-    double compute_eff =
-        static_cast<double>(op.compute_time) / static_cast<double>(op.time);
-    double memory_eff =
-        static_cast<double>(op.memory_time) / static_cast<double>(op.time);
-    os << std::setw(width) << op.name << ',';
-    os << std::setw(width_narrow) << op.count << ',';
-    os << std::setw(width_wide) << op.time << ',';
-    os << std::setw(width_narrow) << std::setprecision(2) << percent * 100
-       << '%,';
-    acc_percent += percent;
-    os << std::setw(width_narrow) << std::setprecision(2) << acc_percent * 100
-       << '%,';
-    os << std::setw(width_wide) << op.time_upper << ',';
-    os << std::setw(width_wide) << op.time_lower << ',';
-    os << std::setw(width_narrow) << std::setprecision(2) << eff * 100 << '%,';
-    os << std::setw(width_narrow) << std::setprecision(2) << compute_eff * 100
-       << '%,';
-    os << std::setw(width_narrow) << std::setprecision(2) << memory_eff * 100
-       << '%,';
-    os << std::endl;
+        template <>
+struct Converter<in_app_purchase::Payment> {
+  static v8::Local<v8::Value> ToV8(v8::Isolate* isolate,
+                                   const in_app_purchase::Payment& payment) {
+    mate::Dictionary dict = mate::Dictionary::CreateEmpty(isolate);
+    dict.SetHidden('simple', true);
+    dict.Set('productIdentifier', payment.productIdentifier);
+    dict.Set('quantity', payment.quantity);
+    return dict.GetHandle();
   }
-  os << std::endl;
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
-    
-    #include <map>
-    
-    
-    {
-    {}  // namespace blas
-}  // namespace stream_executor
-
-    
-    #include 'tensorflow/stream_executor/cuda/cuda_driver.h'
-#include 'tensorflow/stream_executor/stream_executor.h'
-#include 'tensorflow/stream_executor/stream_executor_internal.h'
-    
-    // Version constant.
-// This is either 0 for python, 1 for CPP V1, 2 for CPP V2.
-//
-// 0 is default and is equivalent to
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-//
-// 1 is set with -DPYTHON_PROTO2_CPP_IMPL_V1 and is equivalent to
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
-// and
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=1
-//
-// 2 is set with -DPYTHON_PROTO2_CPP_IMPL_V2 and is equivalent to
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
-// and
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
-#ifdef PYTHON_PROTO2_CPP_IMPL_V1
-#error 'PYTHON_PROTO2_CPP_IMPL_V1 is no longer supported.'
-#else
-#ifdef PYTHON_PROTO2_CPP_IMPL_V2
-static int kImplVersion = 2;
-#else
-#ifdef PYTHON_PROTO2_PYTHON_IMPL
-static int kImplVersion = 0;
-#else
-    
-    bool AnyMetadata::InternalIs(const Descriptor* descriptor) const {
-  const string type_url = type_url_->GetNoArena();
-  string full_name;
-  if (!ParseAnyTypeUrl(type_url, &full_name)) {
-    return false;
-  }
-  return full_name == descriptor->full_name();
-}
-    
-    bool DecodeMetadata(const string& path, GeneratedCodeInfo* info) {
-  string data;
-  GOOGLE_CHECK_OK(File::GetContents(path, &data, true));
-  io::ArrayInputStream input(data.data(), data.size());
-  return info->ParseFromZeroCopyStream(&input);
-}
-    
-    CodeGenerator::~CodeGenerator() {}
-    
-    
-    {  // Moving messages on two different arenas should lead to a copy.
-  *message2_on_arena = std::move(*message1_on_arena);
-  EXPECT_NE(nested, &message2_on_arena->optional_nested_message());
-  TestUtil::ExpectAllFieldsSet(*message1_on_arena);
-  TestUtil::ExpectAllFieldsSet(*message2_on_arena);
-}
-    
-    EnumGenerator::EnumGenerator(const EnumDescriptor* descriptor, const Options* options) :
-    SourceGeneratorBase(descriptor->file(), options),
-    descriptor_(descriptor) {
-}
-    
-    void FieldGeneratorBase::SetCommonOneofFieldVariables(
-    std::map<string, string>* variables) {
-  (*variables)['oneof_name'] = oneof_name();
-  (*variables)['has_property_check'] =
-    oneof_name() + 'Case_ == ' + oneof_property_name() +
-    'OneofCase.' + property_name();
-  (*variables)['oneof_property_name'] = oneof_property_name();
-}
-    
-    
-    { private:
-  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(MessageFieldGenerator);
 };
     
     
-    {}  // namespace tesseract.
+    {  AtomFrameHostMsg_Message_Sync::WriteReplyParams(message_, result);
+  bool success = sender_->Send(message_);
+  message_ = nullptr;
+  sender_ = nullptr;
+  return success;
+}
+    
+    bool SavePageHandler::Handle(const base::FilePath& full_path,
+                             const content::SavePageType& save_type) {
+  auto* download_manager = content::BrowserContext::GetDownloadManager(
+      web_contents_->GetBrowserContext());
+  download_manager->AddObserver(this);
+  // Chromium will create a 'foo_files' directory under the directory of saving
+  // page 'foo.html' for holding other resource files of 'foo.html'.
+  base::FilePath saved_main_directory_path = full_path.DirName().Append(
+      full_path.RemoveExtension().BaseName().value() +
+      FILE_PATH_LITERAL('_files'));
+  bool result =
+      web_contents_->SavePage(full_path, saved_main_directory_path, save_type);
+  download_manager->RemoveObserver(this);
+  // If initialization fails which means fail to create |DownloadItem|, we need
+  // to delete the |SavePageHandler| instance to avoid memory-leak.
+  if (!result)
+    delete this;
+  return result;
+}
+    
+    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
+ public:
+  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
+  ~AtomJavaScriptDialogManager() override;
+    }
+    
+    class DHTMessageCallback;
+    
+    std::shared_ptr<DHTNode> DHTRoutingTable::getNode(const unsigned char* nodeID,
+                                                  const std::string& ipaddr,
+                                                  uint16_t port) const
+{
+  std::shared_ptr<DHTBucket> bucket = getBucketFor(nodeID);
+  return bucket->getNode(nodeID, ipaddr, port);
+}
     
     
-    {}  // namespace tesseract.
+    {} // namespace aria2
 
     
-    #ifdef __SSE4_1__
-// Computes part of matrix.vector v = Wu. Computes 1 result.
-static void PartialMatrixDotVector1(const int8_t* wi, const double* scales,
-                                    const int8_t* u, int num_in, int num_out,
-                                    double* v) {
-  int total = IntDotProductSSE(u, wi, num_in);
-  // Add in the bias and correct for integer values.
-  *v = (static_cast<double>(total) / INT8_MAX + wi[num_in]) * *scales;
-}
-#endif  // __SSE4_1__
+      DHTTaskExecutor immediateTaskQueue_;
     
-    SIMDDetect SIMDDetect::detector;
-    
-      // Staring from the seed center, we do radius search. And for partitions that
-  // have large overlaps with seed, we remove them from part_grid_ and add into
-  // parts_overlap. Note: this function may update the part_grid_, so if the
-  // caller is also running ColPartitionGridSearch, use the RepositionIterator
-  // to continue.
-  void SearchByOverlap(ColPartition* seed,
-                       GenericVector<ColPartition*>* parts_overlap);
-    
-    
-    {  bool rword_indicates_list_item;
-  bool rword_likely_starts_idea;
-  bool rword_likely_ends_idea;
-};
-    
-    // Integrate the parameters editor as popupmenu into the existing scrollview
-// window (usually the pg editor). If sv == null, create a new empty
-// empty window and attach the parameters editor to that window (ugly).
-ParamsEditor::ParamsEditor(tesseract::Tesseract* tess,
-                                 ScrollView* sv) {
-  if (sv == nullptr) {
-    const char* name = 'ParamEditorMAIN';
-    sv = new ScrollView(name, 1, 1, 200, 200, 300, 200);
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port,
+                                           const unsigned char* secret) const
+{
+  unsigned char src[DHT_ID_LENGTH + COMPACT_LEN_IPV6 + SECRET_SIZE];
+  memset(src, 0, sizeof(src));
+  int compactlen = bittorrent::packcompact(src + DHT_ID_LENGTH, ipaddr, port);
+  if (compactlen == 0) {
+    throw DL_ABORT_EX(fmt('Token generation failed: ipaddr=%s, port=%u',
+                          ipaddr.c_str(), port));
   }
-    }
-    
-    #endif  // GRAPHICS_DISABLED
-#endif  // TESSERACT_CCMAIN_PARAMSD_H_
-
-    
-    namespace osquery {
-    }
-    
-    
-    {  return Status();
-}
-    
-      std::string log_line;
-  serializeQueryLogItemJSON(item, log_line);
-  std::string expected =
-      '{\'snapshot\':[],\'action\':\'snapshot\',\'name\':\'\','
-      '\'hostIdentifier\':\'\',\'calendarTime\':\'\',\'unixTime\':0,'
-      '\'epoch\':0,\'counter\':0,'
-      '\'decorations\':{\'internal_60_test\':\'test\',\'one\':\'1\'}}';
-  EXPECT_EQ(log_line, expected);
-    
-      // The word 'custom_' must be a prefix.
-  EXPECT_TRUE(doc.HasMember('fake_custom_fake'));
-  EXPECT_TRUE(Flag::getValue('fake_custom_fake').empty());
-    
-    
-    {  val = getEnvVar('GTEST_OSQUERY');
-  EXPECT_FALSE(val);
-  EXPECT_FALSE(val.is_initialized());
+  memcpy(src, infoHash, DHT_ID_LENGTH);
+  memcpy(src + DHT_ID_LENGTH + COMPACT_LEN_IPV6, secret, SECRET_SIZE);
+  unsigned char md[20];
+  message_digest::digest(md, sizeof(md), MessageDigest::sha1().get(), src,
+                         sizeof(src));
+  return std::string(&md[0], &md[sizeof(md)]);
 }
     
     
-    {  // Use the Query retrieval API to check the now 'previous' result.
-  QueryDataSet previous_qd;
-  auto cf = Query('foobar', query);
-  status = cf.getPreviousQueryResults(previous_qd);
-  EXPECT_TRUE(status.ok());
-}
-    
-    
-    {/// Get a performance limit by name and optional level.
-size_t getWorkerLimit(WatchdogLimitType limit);
-}
-
-    
-    size_t Distributed::getCompletedCount() {
-  return results_.size();
-}
-    
-      for (char* p = scratch_; p < scratch_ + record_num;) {
-    // Cast the inotify struct, make shared pointer, and append to contexts.
-    auto event = reinterpret_cast<struct inotify_event*>(p);
-    if (event->mask & IN_Q_OVERFLOW) {
-      // The inotify queue was overflown (try to recieve more events from OS).
-      handleOverflow();
-    } else if (event->mask & IN_IGNORED) {
-      // This inotify watch was removed.
-      removeMonitor(event->wd, false);
-    } else if (event->mask & IN_MOVE_SELF) {
-      // This inotify path was moved, but is still watched.
-      removeMonitor(event->wd, true);
-    } else if (event->mask & IN_DELETE_SELF) {
-      // A file was moved to replace the watched path.
-      removeMonitor(event->wd, false);
-    } else {
-      auto ec = createEventContextFrom(event);
-      if (!ec->action.empty()) {
-        fire(ec);
-      }
-    }
-    // Continue to iterate
-    p += (sizeof(struct inotify_event)) + event->len;
-  }
-    
-        // Create a basic path trigger, this is a file path.
-    real_test_path = kTestWorkingDirectory + 'inotify-trigger' +
-                     std::to_string(rand() % 10000 + 10000);
-    // Create a similar directory for embedded paths and directories.
-    real_test_dir = kTestWorkingDirectory + 'inotify-triggers' +
-                    std::to_string(rand() % 10000 + 10000);
-    
-    #include <atomic>
-    
-    bool Executor::keepAliveAcquire() {
-  return false;
-}
-    
-    #pragma once
-    
-    
-    {} // namespace folly
-    
-      // We don't use std::chrono_literals suffixes here since older
-  // gcc versions silently truncate the literals to 32-bits.
-  ts = to<struct timespec>(nanoseconds(987654321012LL));
-  EXPECT_EQ(987, ts.tv_sec);
-  EXPECT_EQ(654321012, ts.tv_nsec);
-    
-    /**
- * Helper functions for compression codecs.
- */
-namespace folly {
-namespace io {
-namespace compression {
-namespace detail {
-    }
-    }
-    }
-    }
+    {} // namespace aria2
