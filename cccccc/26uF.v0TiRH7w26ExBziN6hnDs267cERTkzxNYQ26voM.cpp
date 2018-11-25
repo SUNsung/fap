17410d88d7bbadfa82f9dd57223c787d33491507
SUNsung <weatherius@gmail.com>
2018-11-26 00:52:12 +0200

    Rewrite [26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp] File in: 2018-11-26 00:52:11

--- cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
+++ cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
@@ -1,201 +1,126 @@
 
         
-        #define TEGRA_RECIP(src2, sz2, dst, sz, w, h, scale) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-    CAROTENE_NS::reciprocal(CAROTENE_NS::Size2D(w, h), \
-                            src2, sz2, \
-                            dst, sz, \
-                            scale, \
-                            CAROTENE_NS::CONVERT_POLICY_SATURATE), \
-    CV_HAL_ERROR_OK \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
+        
+    {} // namespace
     
-            float32x4_t v_dst0f = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(v_src1))), v_beta),
-                                        v_alpha, vcvtq_f32_u32(vmovl_u16(vget_low_u16(v_src0))));
-        float32x4_t v_dst1f = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_high_u16(v_src1))), v_beta),
-                                        v_alpha, vcvtq_f32_u32(vmovl_u16(vget_high_u16(v_src0))));
-        uint16x8_t _v_dst = vcombine_u16(vmovn_u32(vcvtq_u32_f32(v_dst0f)),
-                                        vmovn_u32(vcvtq_u32_f32(v_dst1f)));
-    
-            if (policy == CONVERT_POLICY_SATURATE)
+        inline void operator()(const u8* src, s16* dstx, s16* dsty, ptrdiff_t width)
+    {
+        uint8x8_t l = vtbl1_u8(vld1_u8(src - lookLeft), vfmask);
+        ptrdiff_t i = 0;
+        for (; i < width - 8 + lookRight; i += 8)
         {
-            for (; j < roiw16; j += 16)
-            {
-                internal::prefetch(src0 + j);
-                internal::prefetch(src1 + j);
-                uint8x16_t v_src0 = vld1q_u8(src0 + j);
-                int16x8_t v_src00 = vreinterpretq_s16_u16(vmovl_u8(vget_low_u8(v_src0)));
-                int16x8_t v_src01 = vreinterpretq_s16_u16(vmovl_u8(vget_high_u8(v_src0)));
-                int16x8_t v_src10 = vld1q_s16(src1 + j), v_src11 = vld1q_s16(src1 + j + 8);
-                int16x8_t v_dst0 = vqaddq_s16(v_src00, v_src10);
-                int16x8_t v_dst1 = vqaddq_s16(v_src01, v_src11);
-                vst1q_s16(dst + j, v_dst0);
-                vst1q_s16(dst + j + 8, v_dst1);
-            }
-            for (; j < roiw8; j += 8)
-            {
-                int16x8_t v_src0 = vreinterpretq_s16_u16(vmovl_u8(vld1_u8(src0 + j)));
-                int16x8_t v_src1 = vld1q_s16(src1 + j);
-                int16x8_t v_dst = vqaddq_s16(v_src0, v_src1);
-                vst1q_s16(dst + j, v_dst);
-            }
+            internal::prefetch(src + i);
+            uint8x8_t l18u = vld1_u8(src + i + 1);
+    }
     }
     
-            size_t x = 0;
-        for (; x <= colsn - 4; x += 4)
-        {
-            internal::prefetch(internal::getRowPtr(ln2 + x, srcStride, x % 5 - 2));
-            int16x4_t v0 = vld1_s16(ln0+x);
-            int16x4_t v1 = vld1_s16(ln1+x);
-            int16x4_t v2 = vld1_s16(ln2+x);
-            int16x4_t v3 = vld1_s16(ln3+x);
-            int16x4_t v4 = vld1_s16(ln4+x);
-    }
+                uint8x16x3_t v_src = vld3q_u8(src + sj);
+            vst1q_u8(dst + dj, v_src.val[coi]);
     
-    inline float32x2_t vrsqrt_f32(float32x2_t val)
+    IMPL_CMPOP(GE, u8)
+IMPL_CMPOP(GE, s8)
+IMPL_CMPOP(GE, u16)
+IMPL_CMPOP(GE, s16)
+IMPL_CMPOP(GE, u32)
+IMPL_CMPOP(GE, s32)
+IMPL_CMPOP(GE, f32)
+    
+    void assertSupportedConfiguration(bool parametersSupported)
 {
-    float32x2_t e = vrsqrte_f32(val);
-    e = vmul_f32(vrsqrts_f32(vmul_f32(e, e), val), e);
-    e = vmul_f32(vrsqrts_f32(vmul_f32(e, e), val), e);
-    return e;
-}
-    
-    namespace tesseract {
-double DotProductAVX(const double* u, const double* v, int n) {
-  fprintf(stderr, 'DotProductAVX can't be used on Android\n');
-  abort();
-}
-}  // namespace tesseract
-    
-    namespace tesseract {
-double DotProductSSE(const double* u, const double* v, int n) {
-  fprintf(stderr, 'DotProductSSE can't be used on Android\n');
-  abort();
-}
-int32_t IntDotProductSSE(const int8_t* u, const int8_t* v, int n) {
-  fprintf(stderr, 'IntDotProductSSE can't be used on Android\n');
-  abort();
-}
-}  // namespace tesseract
-    
-    
-    {}  // namespace tesseract
-
-    
-     protected:
-  // Function to compute part of a matrix.vector multiplication. The weights
-  // are in a very specific order (see above) in w, which is multiplied by
-  // u of length num_in, to produce output v after scaling the integer results
-  // by the corresponding member of scales.
-  // The amount of w and scales consumed is fixed and not available to the
-  // caller. The number of outputs written to v will be at most num_out.
-  typedef void (*PartialFunc)(const int8_t* w, const double* scales,
-                              const int8_t* u, int num_in, int num_out,
-                              double* v);
-    
-    #undef X86_BUILD
-#if defined(__x86_64__) || defined(__i386__) || defined(_WIN32)
-#if !defined(ANDROID_BUILD)
-#define X86_BUILD 1
-#endif  // !ANDROID_BUILD
-#endif  // x86 target
-    
-    /**
- * Moves to the start of the next object at the given level in the
- * page hierarchy, and returns false if the end of the page was reached.
- * NOTE (CHANGED!) that ALL PageIteratorLevel level values will visit each
- * non-text block at least once.
- * Think of non text blocks as containing a single para, with at least one
- * line, with a single imaginary word, containing a single symbol.
- * The bounding boxes mark out any polygonal nature of the block, and
- * PTIsTextType(BLockType()) is false for non-text blocks.
- * Calls to Next with different levels may be freely intermixed.
- * This function iterates words in right-to-left scripts correctly, if
- * the appropriate language has been loaded into Tesseract.
- */
-bool PageIterator::Next(PageIteratorLevel level) {
-  if (it_->block() == nullptr) return false;  // Already at the end!
-  if (it_->word() == nullptr)
-    level = RIL_BLOCK;
+    if (!isSupportedConfiguration()) {
+        std::cerr << 'internal error: attempted to use an unavailable function' << std::endl;
+        std::abort();
+    }
     }
     
-      // ============= Accessing data ==============.
-  // Coordinate system:
-  // Integer coordinates are at the cracks between the pixels.
-  // The top-left corner of the top-left pixel in the image is at (0,0).
-  // The bottom-right corner of the bottom-right pixel in the image is at
-  // (width, height).
-  // Every bounding box goes from the top-left of the top-left contained
-  // pixel to the bottom-right of the bottom-right contained pixel, so
-  // the bounding box of the single top-left pixel in the image is:
-  // (0,0)->(1,1).
-  // If an image rectangle has been set in the API, then returned coordinates
-  // relate to the original (full) image, rather than the rectangle.
+             int32x4_t vline1_s32 = vmovl_s16(vget_low_s16(vline1_s16));
+         int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline1_s16));
+         int32x4_t vline3_s32 = vmovl_s16(vget_low_s16(vline2_s16));
+         int32x4_t vline4_s32 = vmovl_s16(vget_high_s16(vline2_s16));
     
-    // A rather hackish helper structure which can take any kind of parameter input
-// (defined by ParamType) and do a couple of common operations on them, like
-// comparisond or getting its value. It is used in the context of the
-// ParamsEditor as a bridge from the internal tesseract parameters to the
-// ones displayed by the ScrollView server.
-class ParamContent : public ELIST_LINK {
- public:
-  // Compare two VC objects by their name.
-  static int Compare(const void* v1, const void* v2);
+                for (; i <= lim; i+= 16)
+            {
+                internal::prefetch(src + i);
+                uint8x16_t vln = vld1q_u8(src + i);
+                uint8x16_t vnz = vminq_u8(vln, vc1);
+                vs = vaddq_u8(vs, vnz);
+            }
+    
+      // Input statistics
+  // TODO(noetzli): The stats are incomplete. They are lacking everything
+  // consumed by MergeHelper.
+  uint64_t num_input_records = 0;
+  uint64_t num_input_deletion_records = 0;
+  uint64_t num_input_corrupt_records = 0;
+  uint64_t total_input_raw_key_bytes = 0;
+  uint64_t total_input_raw_value_bytes = 0;
+    
+      // True if the iterator has already returned a record for the current key.
+  bool has_outputted_key_ = false;
+    
+      // Will be called while on the write thread before the write executes.  If
+  // this function returns a non-OK status, the write will be aborted and this
+  // status will be returned to the caller of DB::Write().
+  virtual Status Callback(DB* db) = 0;
+    
+      // When an actor (column family) requests a stop token, all writes will be
+  // stopped until the stop token is released (deleted)
+  std::unique_ptr<WriteControllerToken> GetStopToken();
+  // When an actor (column family) requests a delay token, total delay for all
+  // writes to the DB will be controlled under the delayed write rate. Every
+  // write needs to call GetDelay() with number of bytes writing to the DB,
+  // which returns number of microseconds to sleep.
+  std::unique_ptr<WriteControllerToken> GetDelayToken(
+      uint64_t delayed_write_rate);
+  // When an actor (column family) requests a moderate token, compaction
+  // threads will be increased
+  std::unique_ptr<WriteControllerToken> GetCompactionPressureToken();
+    
+    Status PosixWritableFile::Append(const Slice& data) {
+  if (use_direct_io()) {
+    assert(IsSectorAligned(data.size(), GetRequiredBufferAlignment()));
+    assert(IsSectorAligned(data.data(), GetRequiredBufferAlignment()));
+  }
+  const char* src = data.data();
+  size_t left = data.size();
+  while (left != 0) {
+    ssize_t done = write(fd_, src, left);
+    if (done < 0) {
+      if (errno == EINTR) {
+        continue;
+      }
+      return IOError('While appending to file', filename_, errno);
     }
+    left -= done;
+    src += done;
+  }
+  filesize_ += data.size();
+  return Status::OK();
+}
     
-    #include 'blamer.h'
-#include 'errcode.h'
-#include 'ratngs.h'
-#include 'reject.h'
-#include 'tesseractclass.h'
-#include 'werd.h'
+      // open DB with two column families
+  std::vector<ColumnFamilyDescriptor> column_families;
+  // have to open default column family
+  column_families.push_back(ColumnFamilyDescriptor(
+      kDefaultColumnFamilyName, ColumnFamilyOptions()));
+  // open the new one, too
+  column_families.push_back(ColumnFamilyDescriptor(
+      'new_cf', ColumnFamilyOptions()));
+  std::vector<ColumnFamilyHandle*> handles;
+  s = DB::Open(DBOptions(), kDBPath, column_families, &handles, &db);
+  assert(s.ok());
     
-    class BulletPhysicsServer;
+      // Set a new snapshot in the transaction
+  txn->SetSnapshot();
+  read_options.snapshot = db->GetSnapshot();
     
-    void IP_Address::_parse_ipv6(const String &p_string) {
-    }
+    using namespace rocksdb;
     
-    
-    {		String path;
-		String src_path;
-		int size;
-		uint64_t offset_offset;
-	};
-	Vector<File> files;
-    
-        //
-    // Overrides
-    //
-    virtual Spawn* clone() const override;
-    virtual Spawn* reverse() const override;
-    virtual void startWithTarget(Node *target) override;
-    virtual void stop() override;
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    Spawn();
-    virtual ~Spawn();
-    
-        // actions
-    
-    /** Adds an action with a target. 
-     If the target is already present, then the action will be added to the existing target.
-     If the target is not present, a new instance of this target will be created either paused or not, and the action will be added to the newly created target.
-     When the target is paused, the queued actions won't be 'ticked'.
-     *
-     * @param action    A certain action.
-     * @param target    The target which need to be added an action.
-     * @param paused    Is the target paused or not.
-     */
-    virtual void addAction(Action *action, Node *target, bool paused);
-    
-     @code
-     // scaleA and scaleB are equivalents
-     auto scaleA = ScaleTo::create(2, 3);                 // (duration, to)
-     auto scaleB = ActionTween::create(2, 'scale', 1, 3); // (duration, key, from, to)
- @endcode
+    // Move all L0 files to target_level skipping compaction.
+// This operation succeeds only if the files in L0 have disjoint ranges; this
+// is guaranteed to happen, for instance, if keys are inserted in sorted
+// order. Furthermore, all levels between 1 and target_level must be empty.
+// If any of the above condition is violated, InvalidArgument will be
+// returned.
+Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
+                 int target_level = 1);
