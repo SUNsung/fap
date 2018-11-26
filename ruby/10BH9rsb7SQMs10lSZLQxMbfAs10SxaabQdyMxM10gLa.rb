f0d6d2ebe21450d14d7c1f76b39662a79f99222e
SUNsung <weatherius@gmail.com>
2018-11-27 00:51:26 +0200

    Rewrite [10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb] File in: 2018-11-27 00:51:25

--- ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
+++ ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
@@ -1,13 +1,126 @@
 
         
-            def check_confirmation
-      if @user.confirmed?
-        flash[:error] = I18n.t('admin.accounts.resend_confirmation.already_confirmed')
-        redirect_to admin_accounts_path
+            def blank?
+      body.blank?
+    end
+    
+            if root_path?
+          (options[:skip] ||= []).concat ['', '/']
+        end
+    
+    Given(/^a linked file '(.*?)'$/) do |file|
+  # ignoring other linked files
+  TestApp.append_to_deploy_file('set :linked_files, ['#{file}']')
+end
+    
+        def version
+      ['--version', '-V',
+       'Display the program version.',
+       lambda do |_value|
+         puts 'Capistrano Version: #{Capistrano::VERSION} (Rake Version: #{Rake::VERSION})'
+         exit
+       end]
+    end
+    
+    # include would include the module in Object
+# extend only extends the `main` object
+extend Sinatra::Delegator
+    
+        <div id='cookies'>
+      <h3 id='cookie-info'>COOKIES</h3>
+      <% unless req.cookies.empty? %>
+        <table class='req'>
+          <tr>
+            <th>Variable</th>
+            <th>Value</th>
+          </tr>
+          <% req.cookies.each { |key, val| %>
+            <tr>
+              <td><%=h key %></td>
+              <td class='code'><div><%=h val.inspect %></div></td>
+            </tr>
+          <% } %>
+        </table>
+      <% else %>
+        <p class='no-data'>No cookie data.</p>
+      <% end %>
+      <div class='clear'></div>
+    </div> <!-- /COOKIES -->
+    
+    namespace :doc do
+  task :readmes do
+    Dir.glob 'lib/rack/protection/*.rb' do |file|
+      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
+      next if excluded_files.include?(file)
+      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
+      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
+      Dir.mkdir 'doc' unless File.directory? 'doc'
+      puts 'writing #{file}'
+      File.open(file, 'w') { |f| f << doc }
+    end
+  end
+    
+          def default_options
+        DEFAULT_OPTIONS
+      end
+    
+      describe '#random_string' do
+    it 'outputs a string of 32 characters' do
+      expect(subject.random_string.length).to eq(32)
+    end
+  end
+    
+            def next
+          authorize! :update, @order, order_token
+          @order.next!
+          respond_with(@order, default_template: 'spree/api/v1/orders/show', status: 200)
+        rescue StateMachines::InvalidTransition
+          respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
+        end
+    
+            def show
+          @option_type = Spree::OptionType.accessible_by(current_ability, :read).find(params[:id])
+          respond_with(@option_type)
+        end
+    
+            def property_params
+          params.require(:property).permit(permitted_property_attributes)
+        end
       end
     end
   end
 end
 
     
-        private
+            def scope
+          if params[:country_id]
+            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
+            @country.states.accessible_by(current_ability, :read).order('name ASC')
+          else
+            State.accessible_by(current_ability, :read).order('name ASC')
+          end
+        end
+      end
+    end
+  end
+end
+
+    
+            def user_params
+          params.require(:user).permit(permitted_user_attributes |
+                                         [bill_address_attributes: permitted_address_attributes,
+                                          ship_address_attributes: permitted_address_attributes])
+        end
+      end
+    end
+  end
+end
+
+    
+        def erb(content, options = {})
+      if content.kind_of? Symbol
+        unless respond_to?(:'_erb_#{content}')
+          src = ERB.new(File.read('#{Web.settings.views}/#{content}.erb')).src
+          WebAction.class_eval('def _erb_#{content}\n#{src}\n end')
+        end
+      end
