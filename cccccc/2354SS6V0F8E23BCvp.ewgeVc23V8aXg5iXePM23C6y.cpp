0bf948a9cc1e0f05a56aca2b89b4acec9235a4f8
SUNsung <weatherius@gmail.com>
2018-11-26 11:28:32 +0200

    Rewrite [2354SS6V0F8E23BCvp.ewgeVc23V8aXg5iXePM23C6y.cpp] File in: 2018-11-26 11:28:32

--- cccccc/2354SS6V0F8E23BCvp.ewgeVc23V8aXg5iXePM23C6y.cpp
+++ cccccc/2354SS6V0F8E23BCvp.ewgeVc23V8aXg5iXePM23C6y.cpp
@@ -1,495 +1,83 @@
 
         
-        public:
-  /// Form storage for the given generic signature and its replacement
-  /// types and conformances.
-  static Storage *get(GenericSignature *genericSig,
-                      ArrayRef<Type> replacementTypes,
-                      ArrayRef<ProtocolConformanceRef> conformances);
+          image_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
+  label_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
+  image_file.read(reinterpret_cast<char*>(&num_items), 4);
+  num_items = swap_endian(num_items);
+  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
+  num_labels = swap_endian(num_labels);
+  CHECK_EQ(num_items, num_labels);
+  image_file.read(reinterpret_cast<char*>(&rows), 4);
+  rows = swap_endian(rows);
+  image_file.read(reinterpret_cast<char*>(&cols), 4);
+  cols = swap_endian(cols);
     
-    #include 'swift/ASTSectionImporter/ASTSectionImporter.h'
-#include 'swift/Basic/Dwarf.h'
-#include 'swift/Serialization/SerializedModuleLoader.h'
-#include 'swift/Serialization/Validation.h'
-#include 'llvm/Support/Debug.h'
-#include 'llvm/Support/raw_ostream.h'
+      bool handles_setup_;
+  cudnnHandle_t* handle_;
+  cudaStream_t*  stream_;
     
-    //  This file implements a default caching implementation that never evicts
-//  its entries.
-    
-    void CacheImpl::setAndRetain(void *Key, void *Value, size_t Cost) {
-  cache_set_and_retain(static_cast<cache_t*>(Impl), Key, Value, Cost);
-}
-    
-    // We'd like the dump routine to be present in all builds, but it's
-// a pretty large amount of code, most of which is not sensitive to the
-// actual key and value data.  If we try to have a common implementation,
-// we're left with the problem of describing the layout of a node when
-// that's technically instantiation-specific.  Redefining the struct here
-// is technically an aliasing violation, but we can just tell the compilers
-// that actually use TBAA that this is okay.
-typedef struct _Node Node LLVM_MAY_ALIAS;
-struct _Node {
-  // If you change the layout in the header, you'll need to change it here.
-  // (This comment is repeated there.)
-  Node *Left, *Right, *Further;
-};
-    
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-    /// Match a word within a name to a word within a type.
-static bool matchNameWordToTypeWord(StringRef nameWord, StringRef typeWord) {
-  // If the name word is longer, there's no match.
-  if (nameWord.size() > typeWord.size()) return false;
-    }
-    
-    break_table = GraphemeClusterBreakPropertyTable(unicodeGraphemeBreakPropertyFile)
-    
-    // stdarg.h types.
-// Note: this does not catch va_list on platforms where va_list is an array.
-// There's an explicit workaround in ImportType.cpp's VisitDecayedType for that.
-MAP_STDLIB_TYPE('va_list', VaList, 0, 'CVaListPointer', false, DoNothing)
-MAP_STDLIB_TYPE('__gnuc_va_list', VaList, 0, 'CVaListPointer', false, DoNothing)
-MAP_STDLIB_TYPE('__va_list', VaList, 0, 'CVaListPointer', false, DoNothing)
-    
-    #ifndef BITCOIN_QT_PLATFORMSTYLE_H
-#define BITCOIN_QT_PLATFORMSTYLE_H
-    
-    /** Check bounds on a command line confirm target */
-unsigned int ParseConfirmTarget(const UniValue& value);
-    
-    
-    {    secp256k1_ecdsa_recoverable_signature_load(ctx, &r, &s, &recid, sigin);
-    secp256k1_ecdsa_signature_save(sig, &r, &s);
-    return 1;
-}
-    
-    
-    {    RejectDifficultyMismatch(difficulty, expected_difficulty);
-}
-    
-    // Bool() allows generating tests with parameters in a set of (false, true).
-//
-// Synopsis:
-// Bool()
-//   - returns a generator producing sequences with elements {false, true}.
-//
-// It is useful when testing code that depends on Boolean flags. Combinations
-// of multiple flags can be tested when several Bool()'s are combined using
-// Combine() function.
-//
-// In the following example all tests in the test case FlagDependentTest
-// will be instantiated twice with parameters false and true.
-//
-// class FlagDependentTest : public testing::TestWithParam<bool> {
-//   virtual void SetUp() {
-//     external_flag = GetParam();
-//   }
-// }
-// INSTANTIATE_TEST_CASE_P(BoolSequence, FlagDependentTest, Bool());
-//
-inline internal::ParamGenerator<bool> Bool() {
-  return Values(false, true);
-}
-    
-    // Initializes Google Test.  This must be called before calling
-// RUN_ALL_TESTS().  In particular, it parses a command line for the
-// flags that Google Test recognizes.  Whenever a Google Test flag is
-// seen, it is removed from argv, and *argc is decremented.
-//
-// No value is returned.  Instead, the Google Test flag variables are
-// updated.
-//
-// Calling the function for the second time has no user-visible effect.
-GTEST_API_ void InitGoogleTest(int* argc, char** argv);
-    
-      void depart() {
-    if (link_.depart()) delete value_;
-  }
-    
-    
-    {    static bool IsVariable(const nnvm::NodePtr& node) {
-      AGInfo& info = Get(node);
-      return info.grad_req != kNullOp && info.outputs.size() == 1
-             && info.out_grads.size() == 1;
-    }
-  };
-  /*! \brief whether operator recording is on. */
-  bool is_training() const {
-    return is_train_;
-  }
-  /*! \brief turn on or turn off operator recording for autograd. */
-  bool set_is_training(bool is_train) {
-      bool old = is_train_;
-      is_train_ = is_train;
-      return old;
-  }
-  /*! \brief whether operator recording is on. */
-  bool is_recording() const {
-    return is_recording_;
-  }
-  /*! \brief turn on or turn off operator recording for autograd. */
-  bool set_is_recording(bool is_recording) {
-      bool old = is_recording_;
-      is_recording_ = is_recording;
-      return old;
-  }
-  /*! \brief to record operator, return corresponding node. */
-  void RecordOp(nnvm::NodeAttrs&& attrs,
-                const std::vector<NDArray*>& inputs,
-                const std::vector<NDArray*>& outputs,
-                const OpStatePtr& state = OpStatePtr(),
-                std::vector<bool>* p_save_inputs = nullptr,
-                std::vector<bool>* p_save_outputs = nullptr);
-  /*! \brief */
-  OpStatePtr Invoke(const Context& default_ctx,
-                    const nnvm::NodeAttrs& attrs,
-                    const std::vector<NDArray*>& inputs,
-                    const std::vector<NDArray*>& outputs);
-  /*! \brief */
-  OpStatePtr InvokeOp(const Context& ctx,
-                      const nnvm::NodeAttrs& attrs,
-                      const std::vector<NDArray*>& inputs,
-                      const std::vector<NDArray*>& outputs,
-                      const std::vector<OpReqType>& req,
-                      const DispatchMode dispatch_mode,
-                      OpStatePtr state = OpStatePtr());
-  /*! \brief mark variables for computing gradients. */
-  void MarkVariables(const std::vector<NDArray*>& variables,
-                     const std::vector<mx_uint>& grad_reqs,
-                     const std::vector<NDArray*>& gradients);
-  /*! \brief compute the gradient of outputs w.r.t variables. */
-  std::vector<NDArray*> Backward(const std::vector<NDArray*>& outputs,
-                                 const std::vector<NDArray*>& ograds,
-                                 const std::vector<NDArray*>& variables,
-                                 bool is_train, bool retain_graph,
-                                 bool create_graph);
-  /*! \return AutogradRuntime singleton */
-  static Imperative* Get();
-    
-    /*! \brief Cuda runtime compile module. */
-class CudaModule {
- private:
-  /*! \brief Structure for holding internal info. */
-  struct Chunk {
-    /*!
-     * \brief Constructs cuda module.
-     * \param source cuda source code.
-     * \param exports export symbols before mangling.
-     */
-    Chunk(const char* source,
-          const std::vector<std::string>& options,
-          const std::vector<std::string>& exports);
-    /*! \brief deconstrutor */
-    ~Chunk();
-    /*!
-     * \brief Get handle to cuda kernel from loaded module
-     * \param mangled_name mangled kernel name
-     * \param ctx context to run kernel on
-     * \return loaded function handle
-     */
-    CUfunction GetFunction(const std::string& mangled_name, const Context& ctx);
-    /*! \brief nvrtc program handle. */
-    nvrtcProgram prog_;
-    /*! \brief compiled cuda PTX */
-    char* ptx_;
-    /*! \brief lazily loaded cuda module */
-    std::unordered_map<int, CUmodule> mod_;
-    /*! \brief exported names */
-    std::unordered_set<std::string> exports_;
-  };
-  /*! \brief pointer to Chunk */
-  std::shared_ptr<Chunk> ptr_;
-    }
-    
-    // DO_BIND_DISPATCH comes from static_operator_common.h
-Operator *CaffeLossProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
-                                     std::vector<int> *in_type) const {
-  std::vector<int> out_type, aux_type;
-  std::vector<TShape> out_shape, aux_shape;
-  out_type.resize(this->ListOutputs().size());
-  out_shape.resize(this->ListOutputs().size());
-  aux_type.resize(this->ListAuxiliaryStates().size());
-  aux_shape.resize(this->ListAuxiliaryStates().size());
-  CHECK(InferType(in_type, &out_type, &aux_type));
-  CHECK(InferShape(in_shape, &out_shape, &aux_shape));
-  DO_BIND_DISPATCH(CreateOp, param_, (*in_type)[0]);
-}
-    
-      /*!
-   * \brief Constructor takes function to run.
-   * \param size size of the thread pool.
-   * \param func the function to run on the thread pool.
+      /**
+   * @brief Send messages
+   * @param frames The messages to send.
+   * @param frame_num The amount of messages to send.
+   * @return The status of the sending action which is defined by
+   *         apollo::common::ErrorCode.
    */
-  explicit ThreadPool(size_t size, std::function<void()> func)
-      : worker_threads_(size) {
-    CHECK_GT(size, 0);
-    for (auto& i : worker_threads_) {
-      i = std::thread(func);
-    }
-  }
-  explicit ThreadPool(size_t size,
-                      std::function<void(std::shared_ptr<dmlc::ManualEvent> ready)> func,
-                      const bool wait)
-      : worker_threads_(size) {
-    CHECK_GT(size, 0);
-    for (auto& i : worker_threads_) {
-      std::shared_ptr<dmlc::ManualEvent> ptr = std::make_shared<dmlc::ManualEvent>();
-      ready_events_.emplace_back(ptr);
-      i = std::thread(func, ptr);
-    }
-    if (wait) {
-      WaitForReady();
-    }
-  }
-  ~ThreadPool() noexcept(false) {
-    for (auto&& i : worker_threads_) {
-      i.join();
-    }
+  virtual apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
+                                         int32_t *const frame_num) = 0;
+    
+      std::string can_name('can' + std::to_string(port_));
+  std::strncpy(ifr.ifr_name, can_name.c_str(), IFNAMSIZ);
+  if (ioctl(dev_handler_, SIOCGIFINDEX, &ifr) < 0) {
+    AERROR << 'ioctl error';
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
   }
     
-     protected:
-  void PushToExecute(OprBlock *opr_block, bool pusher_thread) override {
-    if (opr_block->opr->prop == FnProperty::kAsync && pusher_thread) {
-      DoExecute(opr_block);
-    } else {
-      DoPushToQueue(opr_block);
-    }
-  }
+    using apollo::common::ErrorCode;
     
-    namespace mxnet {
-namespace exec {
-    }
-    }
+      ProtocolData<::apollo::canbus::ChassisDetail> mpd;
+  SenderMessage<::apollo::canbus::ChassisDetail> msg(1, &mpd);
+  EXPECT_FALSE(sender.NeedSend(msg, 1));
+  EXPECT_EQ(msg.message_id(), 1);
+  int32_t period = msg.curr_period();
+  msg.UpdateCurrPeriod(-50);
+  EXPECT_EQ(msg.curr_period(), period + 50);
+  EXPECT_EQ(msg.CanFrame().id, 1);
     
-    /*!
- * \file iter_libsvm.cc
- * \brief define a LibSVM Reader to read in arrays
- */
-#include <mxnet/io.h>
-#include <dmlc/base.h>
-#include <dmlc/logging.h>
-#include <dmlc/parameter.h>
-#include <dmlc/data.h>
-#include './iter_sparse_prefetcher.h'
-#include './iter_sparse_batchloader.h'
-    
-    ssize_t PointArray::count() const
-{
-    return _controlPoints.size();
-}
-    
-    /** @class CardinalSplineTo
- * Cardinal Spline path.
- * http://en.wikipedia.org/wiki/Cubic_Hermite_spline#Cardinal_spline
- * @ingroup Actions
- */
-class CC_DLL CardinalSplineTo : public ActionInterval
-{
-public:
-    }
-    
-    NS_CC_END
-    
-    // FIXME: Passing 'const O *' instead of 'const O&' because HASH_FIND_IT requires the address of a pointer
-// and, it is not possible to get the address of a reference
-size_t ActionManager::getNumberOfRunningActionsInTargetByTag(const Node *target,
-                                                             int tag)
-{
-    CCASSERT(tag != Action::INVALID_TAG, 'Invalid tag value!');
-    }
-    
-    /** @class ActionManager
- @brief ActionManager is a singleton that manages all the actions.
- Normally you won't need to use this singleton directly. 99% of the cases you will use the Node interface,
- which uses this singleton.
- But there are some cases where you might need to use this singleton.
- Examples:
-    - When you want to run an action where the target is different from a Node. 
-    - When you want to pause / resume the actions.
- 
- @since v0.8
- */
-class CC_DLL ActionManager : public Ref
-{
-public:
     /**
-     * @js ctor
-     */
-    ActionManager();
-    }
-    
-    
-    {
-    {
-    {            if (_shakeZ)
-            {
-                coords.bl.z += ( rand() % (_randrange*2) ) - _randrange;
-                coords.br.z += ( rand() % (_randrange*2) ) - _randrange;
-                coords.tl.z += ( rand() % (_randrange*2) ) - _randrange;
-                coords.tr.z += ( rand() % (_randrange*2) ) - _randrange;
-            }
-                        
-            setTile(Vec2(i, j), coords);
-        }
-    }
-}
-    
-    #include 'base/CCRef.h'
-#include 'base/CCMap.h'
-#include 'base/CCValue.h'
-#include '2d/CCAnimation.h'
-    
-    
-    {  void setTimeout(std::chrono::seconds timeout)
-  {
-    timeout_ = std::move(timeout);
-  }
-};
-    
-    class DownloadEngine;
-class Command;
-    
-    void DHTTaskExecutor::update()
-{
-  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
-                                  std::mem_fn(&DHTTask::finished)),
-                   execTasks_.end());
-  int r;
-  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
-    r = numConcurrent_ - execTasks_.size();
-  }
-  else {
-    r = 0;
-  }
-  while (r && !queue_.empty()) {
-    std::shared_ptr<DHTTask> task = queue_.front();
-    queue_.pop_front();
-    task->startup();
-    if (!task->finished()) {
-      execTasks_.push_back(task);
-      --r;
-    }
-  }
-  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
-                   static_cast<unsigned int>(getExecutingTaskSize()),
-                   static_cast<unsigned int>(getQueueSize())));
-}
-    
-    #include <vector>
-#include <deque>
-#include <memory>
-    
-    
-    {} // namespace aria2
-
-    
-    const std::string& DHTUnknownMessage::getMessageType() const { return UNKNOWN; }
-    
-        PrintEnv();
-    SharedState shared(this);
-    std::vector<ThreadState*> threads(num_threads_);
-    for (uint32_t i = 0; i < num_threads_; i++) {
-      threads[i] = new ThreadState(i, &shared);
-      env->StartThread(ThreadBody, threads[i]);
-    }
-    {
-      MutexLock l(shared.GetMutex());
-      while (!shared.AllInitialized()) {
-        shared.GetCondVar()->Wait();
-      }
-      // Record start time
-      uint64_t start_time = env->NowMicros();
-    }
-    
-    struct CompactionIterationStats {
-  // Compaction statistics
-    }
-    
-    Status PosixRandomAccessFile::Prefetch(uint64_t offset, size_t n) {
-  Status s;
-  if (!use_direct_io()) {
-    ssize_t r = 0;
-#ifdef OS_LINUX
-    r = readahead(fd_, offset, n);
-#endif
-#ifdef OS_MACOSX
-    radvisory advice;
-    advice.ra_offset = static_cast<off_t>(offset);
-    advice.ra_count = static_cast<int>(n);
-    r = fcntl(fd_, F_RDADVISE, &advice);
-#endif
-    if (r == -1) {
-      s = IOError('While prefetching offset ' + ToString(offset) + ' len ' +
-                      ToString(n),
-                  filename_, errno);
-    }
-  }
-  return s;
-}
-    
-    class MyMerge : public rocksdb::MergeOperator {
+ * @class ProtocolData
+ *
+ * @brief This is the base class of protocol data.
+ */
+template <typename SensorType>
+class ProtocolData {
  public:
-  virtual bool FullMergeV2(const MergeOperationInput& merge_in,
-                           MergeOperationOutput* merge_out) const override {
-    merge_out->new_value.clear();
-    if (merge_in.existing_value != nullptr) {
-      merge_out->new_value.assign(merge_in.existing_value->data(),
-                                  merge_in.existing_value->size());
-    }
-    for (const rocksdb::Slice& m : merge_in.operand_list) {
-      fprintf(stderr, 'Merge(%s)\n', m.ToString().c_str());
-      // the compaction filter filters out bad values
-      assert(m.ToString() != 'bad');
-      merge_out->new_value.assign(m.data(), m.size());
-    }
-    return true;
-  }
+  /**
+   * @brief static function, used to calculate the checksum of input array.
+   * @param input the pointer to the start position of input array
+   * @param length the length of the input array
+   * @return the value of checksum
+   */
+  static std::uint8_t CalculateCheckSum(const uint8_t *input,
+                                        const uint32_t length);
+  /**
+   * @brief construct protocol data.
+   */
+  ProtocolData() = default;
     }
     
-      // In this example, we set the snapshot multiple times.  This is probably
-  // only necessary if you have very strict isolation requirements to
-  // implement.
+    TEST(ProtocolDataTest, CheckSum) {
+  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
+  const uint8_t result =
+      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
+  EXPECT_EQ(0xE7, result);
+}
     
-    // Take a string representation of option names and  values, apply them into the
-// base_options, and return the new options as a result. The string has the
-// following format:
-//   'write_buffer_size=1024;max_write_buffer_number=2'
-// Nested options config is also possible. For example, you can define
-// BlockBasedTableOptions as part of the string for block-based table factory:
-//   'write_buffer_size=1024;block_based_table_factory={block_size=4k};'
-//   'max_write_buffer_num=2'
-Status GetColumnFamilyOptionsFromString(
-    const ColumnFamilyOptions& base_options,
-    const std::string& opts_str,
-    ColumnFamilyOptions* new_options);
     
-    #include 'rocksdb/db.h'
-    
-    #include 'rocksdb/db.h'
-#include 'rocksdb/status.h'
-    
-    #include <string>
-#include 'rocksdb/table.h'
-    
-    #include 'rocksdb/env.h'
-#include 'rocksdb/slice.h'
-#include 'rocksdb/statistics.h'
-#include 'rocksdb/status.h'
-    
-      // Deprecated. New RateLimiter derived classes should override
-  // Request(const int64_t, const Env::IOPriority, Statistics*) or
-  // Request(const int64_t, const Env::IOPriority, Statistics*, OpType)
-  // instead.
-  //
-  // Request for token for bytes. If this request can not be satisfied, the call
-  // is blocked. Caller is responsible to make sure
-  // bytes <= GetSingleBurstBytes()
-  virtual void Request(const int64_t /*bytes*/, const Env::IOPriority /*pri*/) {
-    assert(false);
-  }
+    { private:
+  uint8_t *value_;
+};
