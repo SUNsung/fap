691a99d325c618f373ea53de3b072f2e481df71b
SUNsung <weatherius@gmail.com>
2018-11-26 22:05:08 +0200

    Rewrite [405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb] File in: 2018-11-26 22:05:07

--- ruby/405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb
+++ ruby/405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb
@@ -1,113 +1,80 @@
 
         
-              def store_page?(page)
-        page[:entries].present?
-      end
+                  # Default opts to a blank optionparser if none is given
+          opts ||= OptionParser.new
     
-        private
+              # Register a new provisioner class only if a name was given
+          data[:provisioners].register(name.to_sym, &block) if name != UNSET_VALUE
     
-        private
+            # This method is expected to return a class that is used for
+        # configuring the provisioner. This return value is expected to be
+        # a subclass of {Config}.
+        #
+        # @return [Config]
+        def self.config_class
+        end
     
-        def on_response(&block)
-      @on_response ||= []
-      @on_response << block if block
-      @on_response
+            # Called after the configuration is finalized and loaded to validate
+        # this object.
+        #
+        # @param [Machine] machine Access to the machine that is being
+        #   validated.
+        # @return [Hash]
+        def validate(machine)
+          return { self.to_s => _detected_errors }
+        end
+    
+                [block.call, parent]
+          end
+          nil
+        end
+    
+            # This should return the state of the machine within this provider.
+        # The state must be an instance of {MachineState}. Please read the
+        # documentation of that class for more information.
+        #
+        # @return [MachineState]
+        def state
+          nil
+        end
+    
+        # Get a value by the given key.
+    #
+    # This will evaluate the block given to `register` and return the
+    # resulting value.
+    def get(key)
+      return nil if !@items.key?(key)
+      return @results_cache[key] if @results_cache.key?(key)
+      @results_cache[key] = @items[key].call
     end
+    alias :[] :get
     
-          @terminal_width = if !tty?
-        nil
-      elsif ENV['COLUMNS']
-        ENV['COLUMNS'].to_i
-      else
-        `stty size`.scan(/\d+/).last.to_i
-      end
-    rescue
-      @terminal_width = nil
-    end
+        odie 'Unknown command: #{cmd}' unless path
+    puts path
   end
 end
 
     
-            css('pre.prettyprint').each do |node|
-          node.content = node.content.strip
-          node['data-language'] = 'dart' if node['class'].include?('dart')
-          node['data-language'] = 'html' if node.content.start_with?('<')
-          node.remove_attribute('class')
-        end
-    
-        def initialize(machine, guests, capabilities)
-      @capabilities = capabilities
-      @guests       = guests
-      @machine      = machine
+        # Returns the standard exception backtrace,
+    # including the Sass backtrace.
+    #
+    # @return [Array<String>]
+    def backtrace
+      return nil if super.nil?
+      return super if sass_backtrace.all? {|h| h.empty?}
+      sass_backtrace.map do |h|
+        '#{h[:filename] || '(sass)'}:#{h[:line]}' +
+          (h[:mixin] ? ':in `#{h[:mixin]}'' : '')
+      end + super
     end
     
-              # Make sure we're only working with one VM if single target
-          if options[:single_target] && vms.length != 1
-            vm = @env.primary_vm
-            raise Errors::MultiVMTargetRequired if !vm
-            vms = [vm]
-          end
-    
-            # This registers a plugin. This should _NEVER_ be called by the public
-        # and should only be called from within Vagrant. Vagrant will
-        # automatically register V1 plugins when a name is set on the
-        # plugin.
-        def register(plugin)
-          if !@registered.include?(plugin)
-            @logger.info('Registered plugin: #{plugin.name}')
-            @registered << plugin
-          end
+            unless File.directory?(File.dirname(output))
+          puts_action :directory, :green, File.dirname(output)
+          FileUtils.mkdir_p(File.dirname(output))
         end
-    
-            def initialize
-          # The action hooks hash defaults to []
-          @action_hooks = Hash.new { |h, k| h[k] = [] }
-    
-            # This returns all the registered guests.
-        #
-        # @return [Hash]
-        def guests
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.guests)
-            end
-          end
+        puts_action :convert, :green, f
+        if File.exist?(output)
+          puts_action :overwrite, :yellow, output
+        else
+          puts_action :create, :green, output
         end
-    
-            # Returns the internal data associated with this plugin. This
-        # should NOT be called by the general public.
-        #
-        # @return [Hash]
-        def self.data
-          @data ||= {}
-        end
-    
-          def allow_uploads
-        @allow_uploads
-      end
-    
-    Sidekiq.redis {|c| c.flushdb}
-def handle_signal(launcher, sig)
-  Sidekiq.logger.debug 'Got #{sig} signal'
-  case sig
-  when 'INT'
-    # Handle Ctrl-C in JRuby like MRI
-    # http://jira.codehaus.org/browse/JRUBY-4637
-    raise Interrupt
-  when 'TERM'
-    # Heroku sends TERM and then waits 10 seconds for process to exit.
-    raise Interrupt
-  when 'TSTP'
-    Sidekiq.logger.info 'Received TSTP, no longer accepting new work'
-    launcher.quiet
-  when 'TTIN'
-    Thread.list.each do |thread|
-      Sidekiq.logger.warn 'Thread TID-#{(thread.object_id ^ ::Process.pid).to_s(36)} #{thread['label']}'
-      if thread.backtrace
-        Sidekiq.logger.warn thread.backtrace.join('\n')
-      else
-        Sidekiq.logger.warn '<no backtrace available>'
-      end
-    end
-  end
-end
