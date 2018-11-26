1747ce73f97c8fcdcfdccddbfd97aeda2b3f6b14
SUNsung <weatherius@gmail.com>
2018-11-26 15:11:39 +0200

    Rewrite [08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp] File in: 2018-11-26 15:11:38

--- cccccc/08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp
+++ cccccc/08TVVFyPmFhgM08F7P01yrxQ208yqR8aUlMmk08KXV.cpp
@@ -1,246 +1,150 @@
 
         
-        
-    {}  // namespace api
-    
-    namespace IPC {
-class Message;
-}
-    
-    #endif  // ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
-
-    
-    
-    {  DefaultCacheKey CKey(const_cast<void*>(Key), &DCache.CBs);
-  auto Entry = DCache.Entries.find(CKey);
-  if (Entry != DCache.Entries.end()) {
-    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
-    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
-    DCache.Entries.erase(Entry);
-    return true;
+        SILLayout::SILLayout(CanGenericSignature Sig,
+                     ArrayRef<SILField> Fields)
+  : GenericSigAndFlags(Sig, getFlagsValue(anyMutable(Fields))),
+    NumFields(Fields.size())
+{
+#ifndef NDEBUG
+  verifyFields(Sig, Fields);
+#endif
+  auto FieldsMem = getTrailingObjects<SILField>();
+  for (unsigned i : indices(Fields)) {
+    new (FieldsMem + i) SILField(Fields[i]);
   }
-  return false;
 }
     
-        {
-      Out << Indent;
-      if (childKind == ChildKind::Root) {
-        Out << '+- ';
-      } else if (childKind == ChildKind::Left) {
-        Out << '/- ';
-      } else if (childKind == ChildKind::Right) {
-        Out << '\\- ';
-      } else if (childKind == ChildKind::Further) {
-        Out << '\\-> ';
-      }
-      PrintNodeData(Out, node);
-      Out << '\n';
-    }
-    
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-    namespace swift {
-namespace sys {
-void TaskProcessInformation::ResourceUsage::provideMapping(json::Output &out) {
-  out.mapRequired('utime', Utime);
-  out.mapRequired('stime', Stime);
-  out.mapRequired('maxrss', Maxrss);
+    bool CacheImpl::remove(const void *Key) {
+  int Ret = cache_remove(static_cast<cache_t*>(Impl), const_cast<void*>(Key));
+  return Ret == 0;
 }
-    }
-    }
     
     
     {  return forInvalid();
 }
     
-    struct IAMOptions {
-  // TODO: fine-grained control over how we infer
-  static IAMOptions getDefault();
-};
+      StringRef presumedFile = presumedLoc.getFilename();
+  SourceLoc startOfLine = loc.getAdvancedLoc(-presumedLoc.getColumn() + 1);
+  bool isNewVirtualFile =
+    swiftSrcMgr.openVirtualFile(startOfLine, presumedFile,
+                                presumedLoc.getLine() - bufferLineNumber);
+  if (isNewVirtualFile) {
+    SourceLoc endOfLine = findEndOfLine(swiftSrcMgr, loc, mirrorID);
+    swiftSrcMgr.closeVirtualFile(endOfLine);
+  }
     
-    template <typename Op>
-void vcompare(Size2D size,
-              const typename Op::type * src0Base, ptrdiff_t src0Stride,
-              const typename Op::type * src1Base, ptrdiff_t src1Stride,
-              u8 * dstBase, ptrdiff_t dstStride, const Op & op)
-{
-    typedef typename Op::type type;
-    typedef typename internal::VecTraits<type>::vec128 vec128;
-    typedef typename internal::VecTraits<type>::unsign::vec128 uvec128;
-    }
-    
-    void assertSupportedConfiguration(bool parametersSupported = true);
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(s32, f32, 8,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.32 {d4-d5}, [%[src1]]                             \n\t'
-            'vld1.32 {d6-d7}, [%[src2]]                             \n\t'
-            'vcvt.f32.s32 q4, q2                                    \n\t'
-            'vcvt.f32.s32 q5, q3                                    \n\t'
-            'vmul.f32 q6, q4, q0                                    \n\t'
-            'vmul.f32 q7, q5, q0                                    \n\t'
-            'vadd.f32 q8, q6, q1                                    \n\t'
-            'vadd.f32 q9, q7, q1                                    \n\t'
-            'vst1.32 {d16-d17}, [%[dst1]]                           \n\t'
-            'vst1.32 {d18-d19}, [%[dst2]]                           \n\t'
-            : /*no output*/
-            : [src1] 'r' (_src + i),
-              [src2] 'r' (_src + i + 4),
-              [dst1] 'r' (_dst + i),
-              [dst2] 'r' (_dst + i + 4),
-              'w'  (vscale), 'w' (vshift)
-           : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19'
-        );
-    }
-})
-#else
-CVTS_FUNC(s32, f32, 8,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        int32x4_t vline1_s32 = vld1q_s32(_src + i + 0);
-        int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
-        float32x4_t vline1_f32 = vcvtq_f32_s32(vline1_s32);
-        float32x4_t vline2_f32 = vcvtq_f32_s32(vline2_s32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        vst1q_f32(_dst + i + 0, vline1_f32);
-        vst1q_f32(_dst + i + 4, vline2_f32);
-    }
-})
-#endif
-    
-    f64 dotProduct(const Size2D &_size,
-               const f32 * src0Base, ptrdiff_t src0Stride,
-               const f32 * src1Base, ptrdiff_t src1Stride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    Size2D size(_size);
-    if (src0Stride == src1Stride &&
-        src0Stride == (ptrdiff_t)(size.width * sizeof(f32)))
-    {
-        size.width *= size.height;
-        size.height = 1;
-    }
-    }
-    
-                    ptrdiff_t* lidx0 = lane + x - 2*4;
-                ptrdiff_t* lidx1 = lane + x - 1*4;
-                ptrdiff_t* lidx3 = lane + x + 1*4;
-                ptrdiff_t* lidx4 = lane + x + 2*4;
-    
-    namespace tesseract {
-    }
-    
-    #endif  // TESSERACT_ARCH_INTSIMDMATRIX_H_
+    #undef MAP_STDLIB_TYPE
+#undef MAP_TYPE
 
     
-    int os_detect(TO_BLOCK_LIST* port_blocks,
-              OSResults* osr,
-              tesseract::Tesseract* tess);
     
-      /**
-   * Moves to the start of the next object at the given level in the
-   * page hierarchy, and returns false if the end of the page was reached.
-   * NOTE that RIL_SYMBOL will skip non-text blocks, but all other
-   * PageIteratorLevel level values will visit each non-text block once.
-   * Think of non text blocks as containing a single para, with a single line,
-   * with a single imaginary word.
-   * Calls to Next with different levels may be freely intermixed.
-   * This function iterates words in right-to-left scripts correctly, if
-   * the appropriate language has been loaded into Tesseract.
-   */
-  virtual bool Next(PageIteratorLevel level);
-    
-    struct BlobData {
-  BlobData() : blob(nullptr), choices(nullptr) {}
-  BlobData(int index, Tesseract* tess, const WERD_RES& word)
-    : blob(word.chopped_word->blobs[index]),
-      tesseract(tess),
-      choices(&(*word.ratings)(index, index)) {}
-    }
-    
-    #define VARDIR        'configs/' /*parameters files */
-#define MAX_ITEMS_IN_SUBMENU 30
-    
-    REGISTER_CPU_OPERATOR(EnforceFinite, EnforceFiniteOp<CPUContext>);
-    
-    workspace.FeedBlob('X', (np.random.uniform(-10, 10, (5,5))).astype(np.float32))
-print('X before running op:', workspace.FetchBlob('X'))
-workspace.RunOperatorOnce(op)
-print('X after running op:', workspace.FetchBlob('X'))
-    
-    REGISTER_CPU_OPERATOR(GatherRangesToDense, GatherRangesToDenseOp<CPUContext>);
-NO_GRADIENT(GatherRangesToDense);
-    
-    
-    {          return out;
-        });
-OPERATOR_SCHEMA(Float16ConstantFill)
-    .NumInputs(0)
-    .NumOutputs(1)
-    .TensorInferenceFunction(Float16FillerTensorInference)
-    .Arg('value', 'The value for the elements of the output tensor.')
-    .Arg('shape', 'The shape of the output tensor.')
-    .Output(
-        0,
-        'output',
-        'Output tensor of constant values specified by 'value'');
-    
-    grpc::string SecureAuthContext::GetPeerIdentityPropertyName() const {
-  if (!ctx_) {
-    return '';
-  }
-  const char* name = grpc_auth_context_peer_identity_property_name(ctx_);
-  return name == nullptr ? '' : name;
+    {  return 0;
 }
     
+    using ::opencensus::stats::MeasureDouble;
+using ::opencensus::stats::MeasureInt64;
     
-    { private:
-  grpc_auth_context* ctx_;
-  bool take_ownership_;
+    constexpr size_t RpcServerStatsEncoding::kRpcServerStatsSize;
+constexpr size_t RpcServerStatsEncoding::kEncodeDecodeFailure;
+constexpr size_t RpcServerStatsEncoding::kVersionIdSize;
+constexpr size_t RpcServerStatsEncoding::kFieldIdSize;
+constexpr size_t RpcServerStatsEncoding::kVersionIdOffset;
+constexpr size_t RpcServerStatsEncoding::kVersionId;
+    
+    #include 'absl/strings/string_view.h'
+#include 'absl/time/clock.h'
+#include 'absl/time/time.h'
+#include 'include/grpc/grpc_security.h'
+#include 'src/cpp/ext/filters/census/channel_filter.h'
+#include 'src/cpp/ext/filters/census/context.h'
+    
+    #include <unordered_set>
+#include <vector>
+    
+    #include 'common.h'
+    
+    #define WRITE_CHECK(fp, ptr, count)                                            \
+  if (fp.write((ptr), (count)) != (count)) {                                   \
+    throw DL_ABORT_EX(                                                         \
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));     \
+  }
+    
+      ~DHTTaskExecutor();
+    
+    class DownloadContext;
+class PeerStorage;
+class DHTTask;
+class DHTNode;
+class DHTBucket;
+    
+    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createReplaceNodeTask(
+    const std::shared_ptr<DHTBucket>& bucket,
+    const std::shared_ptr<DHTNode>& newNode)
+{
+  auto task = std::make_shared<DHTReplaceNodeTask>(bucket, newNode);
+  task->setTimeout(timeout_);
+  setCommonProperty(task);
+  return task;
+}
+    
+    #include 'DHTTaskQueue.h'
+#include 'DHTTaskExecutor.h'
+    
+    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
+                                             std::chrono::seconds interval)
+    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
+{
+}
+    
+      virtual void process() CXX11_OVERRIDE;
+    
+    DHTUnknownMessage::DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                                     const unsigned char* data, size_t length,
+                                     const std::string& ipaddr, uint16_t port)
+    : DHTMessage(localNode, std::shared_ptr<DHTNode>()),
+      length_(length),
+      ipaddr_(ipaddr),
+      port_(port)
+{
+  if (length_ == 0) {
+    data_ = nullptr;
+  }
+  else {
+    data_ = new unsigned char[length];
+    memcpy(data_, data, length);
+  }
+}
+    
+      // show some sample bytes
+  virtual std::string toString() const CXX11_OVERRIDE;
+    
+        const std::string& getGoodAddr() const;
+    
+    
+    {private:
 };
     
-    void ChannelArguments::SetSslTargetNameOverride(const grpc::string& name) {
-  SetString(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG, name);
-}
+          /** Unlocks the wallet.
+       *
+       * The wallet remain unlocked until the \c lock is called
+       * or the program exits.
+       * @param password the password previously set with \c set_password()
+       * @ingroup Wallet Management
+       */
+      virtual void    unlock(string password) = 0;
     
-      static void OnDoneRecvMessageCb(void* user_data, grpc_error* error);
     
-    inline absl::string_view GetMethod(const grpc_slice* path) {
-  if (GRPC_SLICE_IS_EMPTY(*path)) {
-    return '';
-  }
-  // Check for leading '/' and trim it if present.
-  return absl::StripPrefix(absl::string_view(reinterpret_cast<const char*>(
-                                                 GRPC_SLICE_START_PTR(*path)),
-                                             GRPC_SLICE_LENGTH(*path)),
-                           '/');
-}
+    {}
+
     
-    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
-    const grpc::string& name, int value) {
-  class IntOption final : public ServerBuilderOption {
-   public:
-    IntOption(const grpc::string& name, int value)
-        : name_(name), value_(value) {}
+                for( const auto& value : values ) {
+               auto idx = certs.template get_index<N(bytuple)>();
+               if (value.confidence) {
+                  eosio_assert(value.type.size() <= 32, 'certrow::type should be not longer than 32 bytes');
+                  auto itr = idx.lower_bound( certrow::key(value.property, trusted, certifier) );
     }
     }
+    
+    
+    {         add_balance( from, to, quantity );
+      }
