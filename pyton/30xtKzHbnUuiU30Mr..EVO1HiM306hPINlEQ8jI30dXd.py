d38de01c31ceac099f69408d0f9f55c57032c38c
SUNsung <weatherius@gmail.com>
2018-11-27 15:25:47 +0200

    Rewrite [30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py] File in: 2018-11-27 15:25:46

--- pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
+++ pyton/30xtKzHbnUuiU30Mr..EVO1HiM306hPINlEQ8jI30dXd.py
@@ -1,262 +1,90 @@
 
         
-            This could be extended by having nested blocks, sorting them recursively
-    and flattening the end structure into a list of lines. Revision 2 maybe ^.^.
-'''
-    
-        return app
-
-    
-            if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-    
-    
-def init_app(app):
-    '''Register database functions with the Flask app. This is called by
-    the application factory.
-    '''
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
-
-    
-        # login request set the user_id in the session
-    # check that the user is loaded from the session
-    with client:
-        client.get('/')
-        assert session['user_id'] == 1
-        assert g.user['username'] == 'test'
-    
-        assert 'closed' in str(e)
+        
+@bp.route('/')
+def index():
+    '''Show all the posts, most recent first.'''
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
     
     
-# Certain versions of pypy have a bug where clearing the exception stack
-# breaks the __exit__ function in a very peculiar way.  The second level of
-# exception blocks is necessary because pypy seems to forget to check if an
-# exception happened until the next bytecode instruction?
-#
-# Relevant PyPy bugfix commit:
-# https://bitbucket.org/pypy/pypy/commits/77ecf91c635a287e88e60d8ddb0f4e9df4003301
-# According to ronan on #pypy IRC, it is released in PyPy2 2.3 and later
-# versions.
-#
-# Ubuntu 14.04 has PyPy 2.2.1, which does exhibit this bug.
-BROKEN_PYPY_CTXMGR_EXIT = False
-if hasattr(sys, 'pypy_version_info'):
-    class _Mgr(object):
-        def __enter__(self):
-            return self
-        def __exit__(self, *args):
-            if hasattr(sys, 'exc_clear'):
-                # Python 3 (PyPy3) doesn't have exc_clear
-                sys.exc_clear()
-    try:
-        try:
-            with _Mgr():
-                raise AssertionError()
-        except:
-            raise
-    except TypeError:
-        BROKEN_PYPY_CTXMGR_EXIT = True
-    except AssertionError:
-        pass
-
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/update',
+    '/1/delete',
+))
+def test_login_required(client, path):
+    response = client.post(path)
+    assert response.headers['Location'] == 'http://localhost/auth/login'
+    
+    import os
+from warnings import warn
+    
+        If `conditional=True` and `filename` is provided, this method will try to
+    upgrade the response stream to support range requests.  This will allow
+    the request to be answered with partial content response.
+    
+            :param tag_class: tag class to register. Will be instantiated with this
+            serializer instance.
+        :param force: overwrite an existing tag. If false (default), a
+            :exc:`KeyError` is raised.
+        :param index: index to insert the new tag in the tag order. Useful when
+            the new tag is a special case of an existing tag. If ``None``
+            (default), the tag is appended to the end of the order.
+    
+        def get_cookie_samesite(self, app):
+        '''Return ``'Strict'`` or ``'Lax'`` if the cookie should use the
+        ``SameSite`` attribute. This currently just returns the value of
+        the :data:`SESSION_COOKIE_SAMESITE` setting.
+        '''
+        return app.config['SESSION_COOKIE_SAMESITE']
     
     
-# context locals
-_request_ctx_stack = LocalStack()
-_app_ctx_stack = LocalStack()
-current_app = LocalProxy(_find_app)
-request = LocalProxy(partial(_lookup_req_object, 'request'))
-session = LocalProxy(partial(_lookup_req_object, 'session'))
-g = LocalProxy(partial(_lookup_app_object, 'g'))
-
+class ArrayMinLengthValidator(MinLengthValidator):
+    message = ngettext_lazy(
+        'List contains %(show_value)d item, it should contain no fewer than %(limit_value)d.',
+        'List contains %(show_value)d items, it should contain no fewer than %(limit_value)d.',
+        'limit_value')
     
-    import logging
-import sys
+            if data is None:
+            s = self._get_session_from_db()
+            if s:
+                data = self.decode(s.session_data)
+                self._cache.set(self.cache_key, data, self.get_expiry_age(expiry=s.expire_date))
+            else:
+                data = {}
+        return data
     
-    
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
-
-    
-        def __init__(self, *args, **kwargs):
-        super(FlaskClient, self).__init__(*args, **kwargs)
-        self.environ_base = {
-            'REMOTE_ADDR': '127.0.0.1',
-            'HTTP_USER_AGENT': 'werkzeug/' + werkzeug.__version__
-        }
-    
-        When you want to decorate a pluggable view you will have to either do that
-    when the view function is created (by wrapping the return value of
-    :meth:`as_view`) or you can use the :attr:`decorators` attribute::
-    
-    # build the model
-model = Sequential()
-model.add(layers.Dense(256, input_shape=(784,)))
-model.add(Antirectifier())
-model.add(layers.Dropout(0.1))
-model.add(layers.Dense(256))
-model.add(Antirectifier())
-model.add(layers.Dropout(0.1))
-model.add(layers.Dense(num_classes))
-model.add(layers.Activation('softmax'))
-    
-        return (x_train, y_train), (x_test, y_test)
-
+        def create(self):
+        '''
+        To create a new key, set the modified flag so that the cookie is set
+        on the client for the current request.
+        '''
+        self.modified = True
     
     
-class L1L2(Regularizer):
-    '''Regularizer for L1 and L2 regularization.
+class AbstractBaseSession(models.Model):
+    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
+    session_data = models.TextField(_('session data'))
+    expire_date = models.DateTimeField(_('expire date'), db_index=True)
     
     
-def test_cce_one_hot():
-    y_a = K.variable(np.random.randint(0, 7, (5, 6)))
-    y_b = K.variable(np.random.random((5, 6, 7)))
-    objective_output = losses.sparse_categorical_crossentropy(y_a, y_b)
-    assert K.eval(objective_output).shape == (5, 6)
-    
-        E.g. for use with categorical_crossentropy.
-    
-            self.kernel_initializer = initializers.get(kernel_initializer)
-        self.recurrent_initializer = initializers.get(recurrent_initializer)
-        self.bias_initializer = initializers.get(bias_initializer)
-        self.unit_forget_bias = unit_forget_bias
-    
-    if K.backend() != 'tensorflow':
-    raise RuntimeError('This example can only run with the '
-                       'TensorFlow backend, '
-                       'because it requires TF-native augmentation APIs')
-    
-    for j in range(16):
-    new_pos = seq.predict(track[np.newaxis, ::, ::, ::, ::])
-    new = new_pos[::, -1, ::, ::, ::]
-    track = np.concatenate((track, new), axis=0)
-    
-    
-FILEPATH = os.path.join(
-    os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'blns.txt')
-'''Path to the file'''
-    
-        def test_default_decoder_raises_deserialization_error(self):
-        from acme.fields import RFC3339Field
-        self.assertRaises(
-            jose.DeserializationError, RFC3339Field.default_decoder, '')
-    
-    # The encoding of source files.
-#source_encoding = 'utf-8-sig'
-    
-    from certbot import util
-    
-            :param str port: Desired port
-    
-    from certbot import errors
-    
-        def test_perform2(self):
-        # Avoid load module
-        self.sni.configurator.parser.modules.add('ssl_module')
-        self.sni.configurator.parser.modules.add('socache_shmcb_module')
-        acme_responses = []
-        for achall in self.achalls:
-            self.sni.add_chall(achall)
-            acme_responses.append(achall.response(self.auth_key))
-    
-    # Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named 'default.css' will overwrite the builtin 'default.css'.
-html_static_path = ['_static']
-    
-        @classmethod
-    def setUpClass(self):
-        self.radio = Radio()
-    
-    
-class Delegate(object):
-    def __init__(self):
-        self.p1 = 123
-    
-        _instance_method_choices = {'param_value_1': _instance_method_1, 'param_value_2': _instance_method_2}
-    
-    
-if __name__ == '__main__':
-    
-        # verify that none of the target files exist
-    assert not lexists('foo.txt')
-    assert not lexists('bar.txt')
-    assert not lexists('baz.txt')
-    try:
-        with open('foo.txt', 'w'):  # Creating the file
-            pass
-    
-        def subscribe(self, msg, subscriber):
-        self.subscribers.setdefault(msg, []).append(subscriber)
-    
-            if not meth:
-            meth = self.generic_visit
-        return meth(node, *args, **kwargs)
-    
-    ### OUTPUT ###
-# We have a lovely Cat
-# It says meow
-#
-# We have a lovely Dog
-# It says woof
-# ====================
-# We have a lovely Cat
-# It says meow
-# ====================
-# We have a lovely Cat
-# It says meow
-# ====================
-
-    
-    
-# In some very complex cases, it might be desirable to pull out the building
-# logic into another function (or a method on another class), rather than being
-# in the base class '__init__'. (This leaves you in the strange situation where
-# a concrete class does not have a useful constructor)
-    
-    
-def main():
-    Jhon = Person('Jhon', 'Coder')
-    print(u'Name: {0}    Occupation: {1}'.format(Jhon.name, Jhon.occupation))
-    print(u'Before we access `relatives`:')
-    print(Jhon.__dict__)
-    print(u'Jhon's relatives: {0}'.format(Jhon.relatives))
-    print(u'After we've accessed `relatives`:')
-    print(Jhon.__dict__)
-    print(Jhon.parents)
-    print(Jhon.__dict__)
-    print(Jhon.parents)
-    print(Jhon.call_count2)
-    
-        def __enter__(self):
-        if self.item is None:
-            self.item = self._queue.get()
-        return self.item
-    
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-'''
-    
-            path.append(start)
-        if start == end:
-            return path
-        for node in self.graph.get(start, []):
-            if node not in path:
-                newpath = self.find_path(node, end, path[:])
-                if newpath:
-                    return newpath
+def test_cifar():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine')
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('coarse')
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
