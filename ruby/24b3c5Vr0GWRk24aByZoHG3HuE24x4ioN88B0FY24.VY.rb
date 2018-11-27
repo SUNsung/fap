39d44b12cd375a17ce89283604bea7a8d38fd50a
SUNsung <weatherius@gmail.com>
2018-11-27 02:32:30 +0200

    Rewrite [24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb] File in: 2018-11-27 02:32:30

--- ruby/24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb
+++ ruby/24b3c5Vr0GWRk24aByZoHG3HuE24x4ioN88B0FY24.VY.rb
@@ -1,190 +1,85 @@
 
         
-            if ARGV.include?('--pinned') || ARGV.include?('--versions')
-      filtered_list
-    elsif ARGV.named.empty?
-      if ARGV.include? '--full-name'
-        full_names = Formula.installed.map(&:full_name).sort do |a, b|
-          if a.include?('/') && !b.include?('/')
-            1
-          elsif !a.include?('/') && b.include?('/')
-            -1
-          else
-            a <=> b
+        # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+    
+          def step_name(_keyword, _step_match, status, _source_indent, _background, _file_colon_line)
+        @io.print CHARS[status]
+        @io.print ' '
+      end
+      # rubocop:enable Metrics/ParameterLists
+    
+              pipelines.each do |pipeline|
+            self.new(pipeline).tap do |preloader|
+              preloader.preload_commit_authors
+              preloader.preload_pipeline_warnings
+              preloader.preload_stages_warnings
+            end
           end
         end
-        puts_columns full_names
-      else
-        ENV['CLICOLOR'] = nil
-        exec 'ls', *ARGV.options_only << HOMEBREW_CELLAR
+    
+            def id_for_already_imported_cache(note)
+          note.id
+        end
       end
-    elsif ARGV.verbose? || !$stdout.tty?
-      exec 'find', *ARGV.kegs.map(&:to_s) + %w[-not -type d -print]
+    end
+  end
+end
+
+    
+            # Builds a user from a GitHub API response.
+        #
+        # user - An instance of `Sawyer::Resource` containing the user details.
+        def self.from_api_response(user)
+          new(id: user.id, login: user.login)
+        end
+    
+      def scope_for_collection
+    case params[:id]
+    when 'featured'
+      @account.statuses.permitted_for(@account, signed_request_account).tap do |scope|
+        scope.merge!(@account.pinned_statuses)
+      end
     else
-      ARGV.kegs.each { |keg| PrettyListing.new keg }
+      raise ActiveRecord::NotFound
     end
   end
     
-        if !updated
-      puts 'Already up-to-date.'
-    elsif hub.empty?
-      puts 'No changes to formulae.'
-    else
-      hub.dump
-      hub.reporters.each(&:migrate_tap_migration)
-      hub.reporters.each(&:migrate_formula_rename)
-      Descriptions.update_cache(hub)
-    end
-    
-            ActiveRecord::Base.connection.execute(sql)
+            render template: 'admin/accounts/show'
       end
     end
-  end
-end
-
     
-          def arranged_checks
-        return [@skips, @runs] if @skips && @runs
-    
-          def initialize(actions, positive_condition, negative_condition, skip)
-        @actions = actions
-        @positive_condition = positive_condition
-        @negative_condition = negative_condition
-        @skip = skip
-      end
-    
-          it 'creates a new user' do
-        visit new_admin_user_path
-        fill_in 'Email', with: 'test@test.com'
-        fill_in 'Username', with: 'usertest'
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: '12345678'
-        click_on 'Create User'
-        expect(page).to have_text('User 'usertest' was successfully created.')
-        expect(page).to have_text('test@test.com')
-      end
-    
-      it 'imports a scenario that does not exist yet' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
-    expect(page).not_to have_text('This Scenario already exists in your system.')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
-    
-    describe ApplicationHelper do
-  describe '#icon_tag' do
-    it 'returns a Glyphicon icon element' do
-      icon = icon_tag('glyphicon-help')
-      expect(icon).to be_html_safe
-      expect(Nokogiri(icon).at('span.glyphicon.glyphicon-help')).to be_a Nokogiri::XML::Element
+        def require_local_account!
+      redirect_to admin_account_path(@account.id) unless @account.local? && @account.user.present?
     end
     
-        it 'works for queued jobs' do
-      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
-    end
-  end
-    
-      describe '#style_colors' do
-    it 'returns a css style-formated version of the scenario foreground and background colors' do
-      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
+        def index
+      authorize :email_domain_block, :index?
+      @email_domain_blocks = EmailDomainBlock.page(params[:page])
     end
     
-          it 'loads only the workers specified in the :only option' do
-        agent_runner = AgentRunner.new(only: HuginnScheduler)
-        workers = agent_runner.send(:load_workers)
-        expect(workers.keys).to eq(['HuginnScheduler'])
-        agent_runner.stop
-      end
-    
-    describe AgentsExporter do
-  describe '#as_json' do
-    let(:name) { 'My set of Agents' }
-    let(:description) { 'These Agents work together nicely!' }
-    let(:guid) { 'some-guid' }
-    let(:tag_fg_color) { '#ffffff' }
-    let(:tag_bg_color) { '#000000' }
-    let(:icon) { 'Camera' }
-    let(:source_url) { 'http://yourhuginn.com/scenarios/2/export.json' }
-    let(:agent_list) { [agents(:jane_weather_agent), agents(:jane_rain_notifier_agent)] }
-    let(:exporter) { AgentsExporter.new(
-      agents: agent_list, name: name, description: description,
-      source_url: source_url, guid: guid, tag_fg_color: tag_fg_color,
-      tag_bg_color: tag_bg_color, icon: icon) }
-    
-      describe 'up' do
-    it 'should update extract and template options for an existing WebsiteAgent' do
-      expect(agent.options).to include('extract' => old_extract,
-                                       'template' => old_template)
-      ConvertWebsiteAgentTemplateForMerge.new.up
-      agent.reload
-      expect(agent.options).to include('extract' => new_extract,
-                                       'template' => new_template)
-    end
+      def update
+    response, status = process_push_request
+    render plain: response, status: status
   end
     
-      gem.licenses      = ['MIT']
-    
-    Then(/^it will not recreate the file$/) do
-  #
-end
-    
-    Liquid::Template.register_tag('img', Jekyll::ImageTag)
-
-    
-    # This is basically a copy of the original bundler 'bundle' shim
-# with the addition of the loading of our Bundler patches that
-# modify Bundler's caching behaviour.
-    
-        BUNDLE_DIR = ::File.join(LOGSTASH_HOME, 'vendor', 'bundle')
-    GEMFILE_PATH = ::File.join(LOGSTASH_HOME, 'Gemfile')
-    LOCAL_GEM_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'local_gems')
-    CACHE_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'cache')
-    LOCKFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile.lock'))
-    GEMFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile'))
-    
-      def delete_target_file?
-    return true if overwrite?
-    puts('File #{target_file} exist, do you want to overwrite it? (Y/N)')
-    ( 'y' == STDIN.gets.strip.downcase ? true : false)
-  end
-    
-          # Try to add the gems to the current gemfile and lock file, if successful
-      # both of them will be updated. This injector is similar to Bundler's own injector class
-      # minus the support for additionals source and doing local resolution only.
-      ::Bundler::LogstashInjector.inject!(pack)
-    
-      def validate_cache_location
-    cache_location = LogStash::Environment::CACHE_PATH
-    if File.exist?(cache_location)
-      puts('Directory #{cache_location} is going to be overwritten, do you want to continue? (Y/N)')
-      override = ( 'y' == STDIN.gets.strip.downcase ? true : false)
-      if override
-        FileUtils.rm_rf(cache_location)
-      else
-        puts('Unpack cancelled: file #{cache_location} already exists, please delete or move it')
-        exit
-      end
-    end
-  end
-end
-
-    
-      private
-  def update_all?
-    plugins_arg.size == 0
-  end
-    
-              it 'fails when installing a non logstash plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
-            expect(command).not_to install_successfully
-          end
-    
-    shared_examples 'logstash list' do |logstash|
-  describe 'logstash-plugin list on #{logstash.hostname}' do
-    before(:all) do
-      logstash.install({:version => LOGSTASH_VERSION})
+        s = StringIO.new
+    SystemConfig.dump_verbose_config s
+    # Dummy summary file, asciibetically first, to control display title of gist
+    files['# #{f.name} - #{timestamp}.txt'] = { content: brief_build_info(f) }
+    files['00.config.out'] = { content: s.string }
+    files['00.doctor.out'] = { content: Utils.popen_read('#{HOMEBREW_PREFIX}/bin/brew', 'doctor', err: :out) }
+    unless f.core_formula?
+      tap = <<~EOS
+        Formula: #{f.name}
+        Tap: #{f.tap}
+        Path: #{f.path}
+      EOS
+      files['00.tap.out'] = { content: tap }
     end
