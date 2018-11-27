ccf467313f46c5d099ac0ff478da1b89083cb038
SUNsung <weatherius@gmail.com>
2018-11-27 10:21:53 +0200

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2018-11-27 10:21:53

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,15 +1,146 @@
 
         
-            def decode(self, value):
-        if value != self.resource_type:
-            raise jose.DeserializationError(
-                'Wrong resource type: {0} instead of {1}'.format(
-                    value, self.resource_type))
-        return value
+        # Part of speech tags used in the paths.
+POSTAGS = [
+    'PAD',   'VERB',   'CONJ',   'NOUN',   'PUNCT',
+    'ADP',   'ADJ',    'DET',    'ADV',    'PART',
+    'NUM',   'X',      'INTJ',   'SYM',
+]
+    
+      truth_data_e = normalize_rates(data_e, E, N)
+    
+    rnn_a = generate_rnn(rnn_rngs[0], N, FLAGS.g, FLAGS.tau, FLAGS.dt,
+                     FLAGS.max_firing_rate)
+rnn_b = generate_rnn(rnn_rngs[1], N, FLAGS.g, FLAGS.tau, FLAGS.dt,
+                     FLAGS.max_firing_rate)
+rnns = [rnn_a, rnn_b]
+    
+    
+def _SampleModel(prefix_words, vocab):
+  '''Predict next words using the given prefix words.
+    
+      def __init__(self, test_data_name='wsc273'):
+    vocab_file = os.path.join(FLAGS.data_dir, 'vocab.txt')
+    self.vocab = utils.CharsVocabulary(vocab_file, 50)
+    assert test_data_name in ['pdp60', 'wsc273'], (
+        'Test data must be pdp60 or wsc273, got {}'.format(test_data_name))
+    self.test_data_name = test_data_name
+    
+      # Attention score function
+  attention_score_fn = _create_attention_score_fn('attention_score', num_units,
+                                                  attention_option, reuse)
+  # Attention construction function
+  attention_construct_fn = _create_attention_construct_fn(
+      'attention_construct', num_units, attention_score_fn, reuse)
+    
+    
+if __name__ == '__main__':
+    main()
 
     
-        return file_path, '/'.join(reversed(internal_path))
+        if state == 'present':
+        if not all((replication_id, cluster_id)):
+            module.fail_json(msg='The state 'present' requires options: 'replication_id' and 'cluster_id'')
+        response, changed = create(module, connection, replication_id, cluster_id, name)
+    elif state == 'absent':
+        response, changed = delete(module, connection, name)
+    elif state == 'copy':
+        if not all((target, bucket)):
+            module.fail_json(msg='The state 'copy' requires options: 'target' and 'bucket'.')
+        response, changed = copy(module, connection, name, target, bucket)
     
-        def test_rollback_checkpoints(self):
-        mock_load = mock.Mock()
-        self.config.aug.load = mock_load
+            try:
+            matching_groups = conn.describe_cache_subnet_groups(group_name, max_records=100)
+            exists = len(matching_groups) > 0
+        except BotoServerError as e:
+            if e.error_code != 'CacheSubnetGroupNotFoundFault':
+                module.fail_json(msg=e.error_message)
+    
+        Returns:
+        List (bool, str)
+    '''
+    success = False
+    err_msg = ''
+    params = {
+        'StreamName': stream_name
+    }
+    try:
+        if not check_mode:
+            if action == 'start_encryption':
+                params['EncryptionType'] = encryption_type
+                params['KeyId'] = key_id
+                client.start_stream_encryption(**params)
+                success = True
+            elif action == 'stop_encryption':
+                params['EncryptionType'] = encryption_type
+                params['KeyId'] = key_id
+                client.stop_stream_encryption(**params)
+                success = True
+            else:
+                err_msg = 'Invalid encryption action {0}'.format(action)
+        else:
+            if action == 'start_encryption':
+                success = True
+            elif action == 'stop_encryption':
+                success = True
+            else:
+                err_msg = 'Invalid encryption action {0}'.format(action)
+    
+        aws = AWSConnection(module, ['lambda'])
+    
+        try:
+        lambda_facts.update(mappings=client.list_event_source_mappings(**params)['EventSourceMappings'])
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'ResourceNotFoundException':
+            lambda_facts.update(mappings=[])
+        else:
+            module.fail_json_aws(e, msg='Trying to get source event mappings')
+    
+        params = dict()
+    if snapshot_name:
+        params['DBClusterSnapshotIdentifier'] = snapshot_name
+    if instance_name:
+        params['DBClusterInstanceIdentifier'] = instance_name
+    if snapshot_type:
+        params['SnapshotType'] = snapshot_type
+        if snapshot_type == 'public':
+            params['IsPublic'] = True
+        elif snapshot_type == 'shared':
+            params['IsShared'] = True
+    
+    
+class Ellipse(Graphic):
+    def __init__(self, name):
+        self.name = name
+    
+        def test_3rd_am_station_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1510'
+        self.assertEqual(station, expected_station)
+    
+    
+class Delegator(object):
+    '''
+    >>> delegator = Delegator(Delegate())
+    >>> delegator.p1
+    123
+    >>> delegator.p2
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Delegate' object has no attribute 'p2'
+    >>> delegator.do_something('nothing')
+    'Doing nothing'
+    >>> delegator.do_anything()
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Delegate' object has no attribute 'do_anything'
+    '''
+    
+        def __str__(self):
+        return 'Dog'
+    
+        def get_current_time_as_html_fragment(self, time_provider):
+        current_time = time_provider.now()
+        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
+        return current_time_as_html_fragment
