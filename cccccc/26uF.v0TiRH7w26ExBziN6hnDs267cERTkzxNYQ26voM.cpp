5bcc6707cd100708a91d49fe022f479f46049f06
SUNsung <weatherius@gmail.com>
2018-11-27 05:38:06 +0200

    Rewrite [26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp] File in: 2018-11-27 05:38:06

--- cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
+++ cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
@@ -1,126 +1,155 @@
 
         
-        
-    {} // namespace
+            if (info.bytes == 0)
+      return false;
     
-        inline void operator()(const u8* src, s16* dstx, s16* dsty, ptrdiff_t width)
-    {
-        uint8x8_t l = vtbl1_u8(vld1_u8(src - lookLeft), vfmask);
-        ptrdiff_t i = 0;
-        for (; i < width - 8 + lookRight; i += 8)
-        {
-            internal::prefetch(src + i);
-            uint8x8_t l18u = vld1_u8(src + i + 1);
-    }
-    }
-    
-                uint8x16x3_t v_src = vld3q_u8(src + sj);
-            vst1q_u8(dst + dj, v_src.val[coi]);
-    
-    IMPL_CMPOP(GE, u8)
-IMPL_CMPOP(GE, s8)
-IMPL_CMPOP(GE, u16)
-IMPL_CMPOP(GE, s16)
-IMPL_CMPOP(GE, u32)
-IMPL_CMPOP(GE, s32)
-IMPL_CMPOP(GE, f32)
-    
-    void assertSupportedConfiguration(bool parametersSupported)
-{
-    if (!isSupportedConfiguration()) {
-        std::cerr << 'internal error: attempted to use an unavailable function' << std::endl;
-        std::abort();
-    }
+    CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
+  llvm::SmallString<32> NameBuf(Name);
+  cache_attributes_t Attrs = {
+    CACHE_ATTRIBUTES_VERSION_2,
+    CBs.keyHashCB,
+    CBs.keyIsEqualCB,
+    nullptr,
+    CBs.keyDestroyCB,
+    CBs.valueReleaseCB,
+    nullptr,
+    nullptr,
+    CBs.UserData,
+    CBs.valueRetainCB,
+  };
     }
     
-             int32x4_t vline1_s32 = vmovl_s16(vget_low_s16(vline1_s16));
-         int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline1_s16));
-         int32x4_t vline3_s32 = vmovl_s16(vget_low_s16(vline2_s16));
-         int32x4_t vline4_s32 = vmovl_s16(vget_high_s16(vline2_s16));
+    using namespace swift;
+using namespace swift::sys;
     
-                for (; i <= lim; i+= 16)
-            {
-                internal::prefetch(src + i);
-                uint8x16_t vln = vld1q_u8(src + i);
-                uint8x16_t vnz = vminq_u8(vln, vc1);
-                vs = vaddq_u8(vs, vnz);
-            }
-    
-      // Input statistics
-  // TODO(noetzli): The stats are incomplete. They are lacking everything
-  // consumed by MergeHelper.
-  uint64_t num_input_records = 0;
-  uint64_t num_input_deletion_records = 0;
-  uint64_t num_input_corrupt_records = 0;
-  uint64_t total_input_raw_key_bytes = 0;
-  uint64_t total_input_raw_value_bytes = 0;
-    
-      // True if the iterator has already returned a record for the current key.
-  bool has_outputted_key_ = false;
-    
-      // Will be called while on the write thread before the write executes.  If
-  // this function returns a non-OK status, the write will be aborted and this
-  // status will be returned to the caller of DB::Write().
-  virtual Status Callback(DB* db) = 0;
-    
-      // When an actor (column family) requests a stop token, all writes will be
-  // stopped until the stop token is released (deleted)
-  std::unique_ptr<WriteControllerToken> GetStopToken();
-  // When an actor (column family) requests a delay token, total delay for all
-  // writes to the DB will be controlled under the delayed write rate. Every
-  // write needs to call GetDelay() with number of bytes writing to the DB,
-  // which returns number of microseconds to sleep.
-  std::unique_ptr<WriteControllerToken> GetDelayToken(
-      uint64_t delayed_write_rate);
-  // When an actor (column family) requests a moderate token, compaction
-  // threads will be increased
-  std::unique_ptr<WriteControllerToken> GetCompactionPressureToken();
-    
-    Status PosixWritableFile::Append(const Slice& data) {
-  if (use_direct_io()) {
-    assert(IsSectorAligned(data.size(), GetRequiredBufferAlignment()));
-    assert(IsSectorAligned(data.data(), GetRequiredBufferAlignment()));
-  }
-  const char* src = data.data();
-  size_t left = data.size();
-  while (left != 0) {
-    ssize_t done = write(fd_, src, left);
-    if (done < 0) {
-      if (errno == EINTR) {
-        continue;
-      }
-      return IOError('While appending to file', filename_, errno);
+    swift::unicode::GraphemeClusterBreakProperty
+swift::unicode::getGraphemeClusterBreakProperty(uint32_t C) {
+  // FIXME: replace linear search with a trie lookup.
     }
-    left -= done;
-    src += done;
-  }
-  filesize_ += data.size();
-  return Status::OK();
+    
+    bool importer::isCFTypeDecl(
+       const clang::TypedefNameDecl *Decl) {
+  if (CFPointeeInfo::classifyTypedef(Decl))
+    return true;
+  return false;
 }
     
-      // open DB with two column families
-  std::vector<ColumnFamilyDescriptor> column_families;
-  // have to open default column family
-  column_families.push_back(ColumnFamilyDescriptor(
-      kDefaultColumnFamilyName, ColumnFamilyOptions()));
-  // open the new one, too
-  column_families.push_back(ColumnFamilyDescriptor(
-      'new_cf', ColumnFamilyOptions()));
-  std::vector<ColumnFamilyHandle*> handles;
-  s = DB::Open(DBOptions(), kDBPath, column_families, &handles, &db);
-  assert(s.ok());
+    /// Determine whether this typedef is a CF type.
+bool isCFTypeDecl(const clang::TypedefNameDecl *Decl);
     
-      // Set a new snapshot in the transaction
-  txn->SetSnapshot();
-  read_options.snapshot = db->GetSnapshot();
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace cpp {
+    }
+    }
+    }
+    }
     
-    using namespace rocksdb;
+    void WriteDocCommentBodyImpl(io::Printer* printer, SourceLocation location) {
+    string comments = location.leading_comments.empty() ?
+        location.trailing_comments : location.leading_comments;
+    if (comments.empty()) {
+        return;
+    }
+    // XML escaping... no need for apostrophes etc as the whole text is going to be a child
+    // node of a summary element, not part of an attribute.
+    comments = StringReplace(comments, '&', '&amp;', true);
+    comments = StringReplace(comments, '<', '&lt;', true);
+    std::vector<string> lines = Split(comments, '\n', false /* skip_empty */);
+    // TODO: We really should work out which part to put in the summary and which to put in the remarks...
+    // but that needs to be part of a bigger effort to understand the markdown better anyway.
+    printer->Print('/// <summary>\n');
+    bool last_was_empty = false;
+    // We squash multiple blank lines down to one, and remove any trailing blank lines. We need
+    // to preserve the blank lines themselves, as this is relevant in the markdown.
+    // Note that we can't remove leading or trailing whitespace as *that's* relevant in markdown too.
+    // (We don't skip 'just whitespace' lines, either.)
+    for (std::vector<string>::iterator it = lines.begin(); it != lines.end(); ++it) {
+        string line = *it;
+        if (line.empty()) {
+            last_was_empty = true;
+        } else {
+            if (last_was_empty) {
+                printer->Print('///\n');
+            }
+            last_was_empty = false;
+            printer->Print('///$line$\n', 'line', *it);
+        }
+    }
+    printer->Print('/// </summary>\n');
+}
     
-    // Move all L0 files to target_level skipping compaction.
-// This operation succeeds only if the files in L0 have disjoint ranges; this
-// is guaranteed to happen, for instance, if keys are inserted in sorted
-// order. Furthermore, all levels between 1 and target_level must be empty.
-// If any of the above condition is violated, InvalidArgument will be
-// returned.
-Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
-                 int target_level = 1);
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace csharp {
+namespace {
+    }
+    }
+    }
+    }
+    }
+    
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
+    
+      ObjectiveCType objc_type = GetObjectiveCType(descriptor_);
+  string singular_type;
+  if (objc_type == OBJECTIVECTYPE_MESSAGE) {
+    vars['type'] = string('GPBStringifySymbol(') +
+                   ClassName(descriptor_->message_type()) + ')';
+  } else {
+    vars['type'] = 'NULL';
+  }
+    
+    #include 'joint_bullet.h'
+    
+    class JointBullet : public ConstraintBullet {
+    }
+    
+    
+    {	_FORCE_INLINE_ void _set_physics_server(BulletPhysicsServer *p_physicsServer) { physicsServer = p_physicsServer; }
+	_FORCE_INLINE_ BulletPhysicsServer *get_physics_server() const { return physicsServer; }
+};
+#endif
+
+    
+    Speed* Speed::create(ActionInterval* action, float speed)
+{
+    Speed *ret = new (std::nothrow) Speed();
+    if (ret && ret->initWithAction(action, speed))
+    {
+        ret->autorelease();
+        return ret;
+    }
+    CC_SAFE_DELETE(ret);
+    return nullptr;
+}
+    
+    ReuseGrid* ReuseGrid::reverse() const
+{
+    // no reverse, just clone it
+    return this->clone();
+}
+    
+        /** Removes all actions from all the targets.
+     */
+    virtual void removeAllActions();
+    
+    
+    {        for(const auto &value : spritesheets) {
+            std::string path = FileUtils::getInstance()->fullPathFromRelativeFile(value.asString(),plist);
+            SpriteFrameCache::getInstance()->addSpriteFramesWithFile(path);
+        }
+    }
+    
+    NS_CC_END
