7d6ce7de4aff489feef0b0839a15ebde7ec68a1d
SUNsung <weatherius@gmail.com>
2018-11-28 03:10:20 +0200

    Rewrite [55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb] File in: 2018-11-28 03:10:20

--- ruby/55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb
+++ ruby/55riqu5Bbq95w55YC4lfYmkM9M55sMfssyFUTyQ5567.rb
@@ -1,73 +1,160 @@
 
         
-          before(:each) do
-    # This value needs to be set or our event fixtures will not match
-    allow(FastlaneCore::Helper).to receive(:ci?).and_return(false)
-    allow(FastlaneCore::Helper).to receive(:operating_system).and_return('macOS')
-  end
+              #
     
-          it 'can be set to multiple the platforms' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              platform: 'iOS,Mac'
-            )
-          end').runner.execute(:test)
+    module Jekyll
+  module Deprecator
+    extend self
     
-      def test_file_exists(path)
-    exists?('f', path)
-  end
-    
-          # Returns the branch of the `if` node that gets evaluated when its
-      # condition is falsey.
-      #
-      # @note This is normalized for `unless` nodes.
-      #
-      # @return [Node] the falsey branch node of the `if` node
-      # @return [nil] when there is no else branch
-      def else_branch
-        node_parts[2]
+          it 'updates the notification count on destroy' do
+        Notification.any_instance.expects(:refresh_notification_count).returns(nil)
+        notification.destroy!
       end
     
-          # Whether the last argument of the node is a block pass,
-      # i.e. `&block`.
-      #
-      # @return [Boolean] whether the last argument of the node is a block pass
-      def block_argument?
-        arguments? &&
-          (last_argument.block_pass_type? || last_argument.blockarg_type?)
+      smoke_user = User.seed do |u|
+    u.id = 0
+    u.name = 'smoke_user'
+    u.username = 'smoke_user'
+    u.username_lower = 'smoke_user'
+    u.password = 'P4ssw0rd'
+    u.active = true
+    u.approved = true
+    u.approved_at = Time.now
+    u.trust_level = TrustLevel[3]
+  end.first
+    
+        def suggest_ruby_reinstall(e)
+      ui = FastlaneCore::UI
+      ui.error('-----------------------------------------------------------------------')
+      ui.error(e.to_s)
+      ui.error('')
+      ui.error('SSL errors can be caused by various components on your local machine.')
+      if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.1')
+        ui.error('Apple has recently changed their servers to require TLS 1.2, which may')
+        ui.error('not be available to your system installed Ruby (#{RUBY_VERSION})')
+      end
+      ui.error('')
+      ui.error('The best solution is to use the self-contained fastlane version.')
+      ui.error('Which ships with a bundled OpenSSL,ruby and all gems - so you don't depend on system libraries')
+      ui.error(' - Use Homebrew')
+      ui.error('    - update brew with `brew update`')
+      ui.error('    - install fastlane using:')
+      ui.error('      - `brew cask install fastlane`')
+      ui.error(' - Use One-Click-Installer:')
+      ui.error('    - download fastlane at https://download.fastlane.tools')
+      ui.error('    - extract the archive and double click the `install`')
+      ui.error('-----------------------------------------------------------')
+      ui.error('for more details on ways to install fastlane please refer the documentation:')
+      ui.error('-----------------------------------------------------------')
+      ui.error('        🚀       https://docs.fastlane.tools          🚀   ')
+      ui.error('-----------------------------------------------------------')
+      ui.error('')
+      ui.error('You can also install a new version of Ruby')
+      ui.error('')
+      ui.error('- Make sure OpenSSL is installed with Homebrew: `brew update && brew upgrade openssl`')
+      ui.error('- If you use system Ruby:')
+      ui.error('  - Run `brew update && brew install ruby`')
+      ui.error('- If you use rbenv with ruby-build:')
+      ui.error('  - Run `brew update && brew upgrade ruby-build && rbenv install 2.3.1`')
+      ui.error('  - Run `rbenv global 2.3.1` to make it the new global default Ruby version')
+      ui.error('- If you use rvm:')
+      ui.error('  - First run `rvm osx-ssl-certs update all`')
+      ui.error('  - Then run `rvm reinstall ruby-2.3.1 --with-openssl-dir=/usr/local`')
+      ui.error('')
+      ui.error('If that doesn't fix your issue, please google for the following error message:')
+      ui.error('  '#{e}'')
+      ui.error('-----------------------------------------------------------------------')
+    end
+    
+          it 'generates the correct git command with a shell-escaped message' do
+        message = 'message with 'quotes' (and parens)'
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: './fastlane/README.md', message: \'#{message}\')
+        end').runner.execute(:test)
+        expect(result).to eq('git commit -m #{message.shellescape} ./fastlane/README.md')
       end
     end
   end
 end
 
     
-    Given /^I comment out lines that contain '([^']+)' in '([^']+)'$/ do |contains, glob|
-  cd('.') do
-    Dir.glob(glob).each do |file|
-      transform_file(file) do |content|
-        content.gsub(/^(.*?#{contains}.*?)$/) { |line| '# #{line}' }
+        def document?
+      @content =~ DOCUMENT_RGX
+    end
+    
+        def html?
+      mime_type.include? 'html'
+    end
+    
+    module Docs
+  class URL < URI::Generic
+    PARSER = URI::Parser.new
+    
+        # use Feedbag as a backup to Google Feeds Api
+    if rss_url.nil?
+      rss_url = Feedbag.find(web_url).first
+      if rss_url.nil?
+        suggested_paths = ['/rss', '/feed', '/feeds', '/atom.xml', '/feed.xml', '/rss.xml', '.atom']
+        suggested_paths.each do |suggested_path|
+          rss_url = Feedbag.find('#{web_url.chomp('/')}#{suggested_path}').first
+          break if rss_url
+        end
       end
     end
   end
-end
     
-    When /^(?:|I )uncheck '([^']*)'$/ do |field|
-  uncheck(field)
-end
+      people = []
+  contacts = []
+  aspect_memberships = []
     
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
+        it 'generates the aspects_manage_contacts_json fixture', fixture: true do
+      # adds one not mutual contact
+      bob.share_with(FactoryGirl.create(:person), @aspect)
     
-        def blank_name?
-      @filepath.nil? || @filepath.empty?
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+        it 'returns a 404 for a post not visible to the user' do
+      sign_in eve
+      expect {
+        get :index, params: {post_id: @message.id}
+      }.to raise_error(ActiveRecord::RecordNotFound)
     end
     
-        def cropping dst, ratio, scale
-      if ratio.horizontal? || ratio.square?
-        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
-      else
-        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
-      end
+        it 'should redirect back in the mobile version if it has > 0 notifications' do
+      FactoryGirl.create(:notification, recipient: alice, type: 'Notifications::StartedSharing')
+      get :read_all, params: {type: 'liked'}, format: :mobile
+      expect(response).to redirect_to(notifications_path)
     end
+    
+      def run_vagrant_command(command)
+    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
+    return [stdout, stderr] if status.success?
+    raise VagrantSSHCommandError, status
+  end
+end
+    
+    module Capistrano
+  class Configuration
+    # Holds the variables assigned at Capistrano runtime via `set` and retrieved
+    # with `fetch`. Does internal bookkeeping to help identify user mistakes
+    # like spelling errors or unused variables that may lead to unexpected
+    # behavior.
+    class Variables
+      CAPISTRANO_LOCATION = File.expand_path('../..', __FILE__).freeze
+      IGNORED_LOCATIONS = [
+        '#{CAPISTRANO_LOCATION}/configuration/variables.rb:',
+        '#{CAPISTRANO_LOCATION}/configuration.rb:',
+        '#{CAPISTRANO_LOCATION}/dsl/env.rb:',
+        '/dsl.rb:',
+        '/forwardable.rb:'
+      ].freeze
+      private_constant :CAPISTRANO_LOCATION, :IGNORED_LOCATIONS
+    
+        def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
+    
+    
