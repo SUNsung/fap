d523a48c7586afd4e99abc401a3769056caf7885
SUNsung <weatherius@gmail.com>
2018-11-28 04:16:28 +0200

    Rewrite [51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp] File in: 2018-11-28 04:16:27

--- cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
+++ cccccc/51RLXBNTKJ74A51cpD16pBf3Os51LVgvWqTbjWQ51Zxb.cpp
@@ -1,89 +1,42 @@
 
         
-          size_t numTrailingObjects(OverloadToken<ProtocolConformanceRef>) const {
-    return numConformanceRequirements;
-  }
+        Q_SIGNALS:
+    void valueChanged();
     
     
-    {  }
-  bool visitSILTokenType(SILTokenType *ty) { return false; }
+    {private:
+    Ui::TransactionDescDialog *ui;
+};
     
-        if (info.bytes == 0)
-      return false;
+    /**
+ * Changelog:
+ * - March 2013, Diederik Huys:    original version
+ * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
+ * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
+ */
     
-    DIRECTIONAL_PREPOSITION(above)
-DIRECTIONAL_PREPOSITION(after)
-DIRECTIONAL_PREPOSITION(along)
-DIRECTIONAL_PREPOSITION(alongside)
-DIRECTIONAL_PREPOSITION(as)
-DIRECTIONAL_PREPOSITION(at)
-DIRECTIONAL_PREPOSITION(before)
-DIRECTIONAL_PREPOSITION(below)
-DIRECTIONAL_PREPOSITION(by)
-DIRECTIONAL_PREPOSITION(following)
-DIRECTIONAL_PREPOSITION(for)
-DIRECTIONAL_PREPOSITION(from)
-DIRECTIONAL_PREPOSITION(given)
-DIRECTIONAL_PREPOSITION(in)
-DIRECTIONAL_PREPOSITION(including)
-DIRECTIONAL_PREPOSITION(inside)
-DIRECTIONAL_PREPOSITION(into)
-DIRECTIONAL_PREPOSITION(matching)
-DIRECTIONAL_PREPOSITION(of)
-DIRECTIONAL_PREPOSITION(on)
-DIRECTIONAL_PREPOSITION(passing)
-DIRECTIONAL_PREPOSITION(preceding)
-DIRECTIONAL_PREPOSITION(since)
-DIRECTIONAL_PREPOSITION(to)
-DIRECTIONAL_PREPOSITION(until)
-DIRECTIONAL_PREPOSITION(using)
-DIRECTIONAL_PREPOSITION(via)
-DIRECTIONAL_PREPOSITION(when)
-PREPOSITION(with)
-DIRECTIONAL_PREPOSITION(within)
-    
-    swift::unicode::GraphemeClusterBreakProperty
-swift::unicode::getGraphemeClusterBreakProperty(uint32_t C) {
-  // FIXME: replace linear search with a trie lookup.
-    }
-    
-    protected:
-    bool eventFilter(QObject *object, QEvent *event);
-    
-    /** Dialog showing transaction details. */
-class TransactionDescDialog : public QDialog
-{
-    Q_OBJECT
-    }
-    
-    #include <script/script.h>
-    
-    
-    {        secp256k1_sha256_initialize(&sha);
-        secp256k1_sha256_write(&sha, y, sizeof(y));
-        secp256k1_sha256_write(&sha, x, sizeof(x));
-        secp256k1_sha256_finalize(&sha, result);
-        ret = 1;
-    }
-    
-    int secp256k1_ecdsa_sign_recoverable(const secp256k1_context* ctx, secp256k1_ecdsa_recoverable_signature *signature, const unsigned char *msg32, const unsigned char *seckey, secp256k1_nonce_function noncefp, const void* noncedata) {
-    secp256k1_scalar r, s;
-    secp256k1_scalar sec, non, msg;
-    int recid;
-    int ret = 0;
-    int overflow = 0;
-    VERIFY_CHECK(ctx != NULL);
-    ARG_CHECK(secp256k1_ecmult_gen_context_is_built(&ctx->ecmult_gen_ctx));
-    ARG_CHECK(msg32 != NULL);
-    ARG_CHECK(signature != NULL);
-    ARG_CHECK(seckey != NULL);
-    if (noncefp == NULL) {
-        noncefp = secp256k1_nonce_function_default;
-    }
-    }
-    
-    BOOST_AUTO_TEST_SUITE_END()
-
+    const int CURVE_B = 4;
+#  elif EXHAUSTIVE_TEST_ORDER == 13
+const secp256k1_ge secp256k1_ge_const_g = SECP256K1_GE_CONST(
+    0xedc60018, 0xa51a786b, 0x2ea91f4d, 0x4c9416c0,
+    0x9de54c3b, 0xa1316554, 0x6cf4345c, 0x7277ef15,
+    0x54cb1b6b, 0xdc8c1273, 0x087844ea, 0x43f4603e,
+    0x0eaf9a43, 0xf6effe55, 0x939f806d, 0x37adf8ac
+);
+const int CURVE_B = 2;
+#  else
+#    error No known generator for the specified exhaustive test group order.
+#  endif
+#else
+/** Generator for secp256k1, value 'g' defined in
+ *  'Standards for Efficient Cryptography' (SEC2) 2.7.1.
+ */
+static const secp256k1_ge secp256k1_ge_const_g = SECP256K1_GE_CONST(
+    0x79BE667EUL, 0xF9DCBBACUL, 0x55A06295UL, 0xCE870B07UL,
+    0x029BFCDBUL, 0x2DCE28D9UL, 0x59F2815BUL, 0x16F81798UL,
+    0x483ADA77UL, 0x26A3C465UL, 0x5DA4FBFCUL, 0x0E1108A8UL,
+    0xFD17B448UL, 0xA6855419UL, 0x9C47D08FUL, 0xFB10D4B8UL
+);
     
     /* Equality between doubles is imprecise. Comparison should be done
  * with a small threshold of tolerance, rather than exact equality.
@@ -93,114 +46,361 @@ static bool DoubleEquals(double a, double b, double epsilon)
     return std::abs(a - b) < epsilon;
 }
     
-    // Added for Bitcoin Core
-template<typename... Args>
-std::string format(const std::string &fmt, const Args&... args)
-{
-    std::ostringstream oss;
-    format(oss, fmt.c_str(), args...);
-    return oss.str();
+    
+    {  if (!endseq) return true;
+  hd = upb_handlers_gethandlerdata(s->handlers, sel);
+  return endseq(s->closure, hd);
 }
     
-        enum FLIP_MODE
-    {
-        FLIP_HORIZONTAL_MODE = 1,
-        FLIP_VERTICAL_MODE = 2,
-        FLIP_BOTH_MODE = FLIP_HORIZONTAL_MODE | FLIP_VERTICAL_MODE
-    };
     
-        for (size_t i = 0; i < size.height; ++i)
     {
-        const u8 * src0 = internal::getRowPtr(src0Base, src0Stride, i);
-        const s16 * src1 = internal::getRowPtr(src1Base, src1Stride, i);
-        s16 * dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t j = 0;
+    {
+    {
+    {}  // namespace csharp
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
+    
+    
+    {
+    {  if (!namespace_.empty()) {
+    printer->Outdent();
+    printer->Print('}\n');
+  }
+  printer->Print('\n');
+  printer->Print('#endregion Designer generated code\n');
+}
+    
+    TEST(JavaDocCommentTest, Escaping) {
+  EXPECT_EQ('foo /&#42; bar *&#47; baz', EscapeJavadoc('foo /* bar */ baz'));
+  EXPECT_EQ('foo /&#42;&#47; baz', EscapeJavadoc('foo /*/ baz'));
+  EXPECT_EQ('{&#64;foo}', EscapeJavadoc('{@foo}'));
+  EXPECT_EQ('&lt;i&gt;&amp;&lt;/i&gt;', EscapeJavadoc('<i>&</i>'));
+  EXPECT_EQ('foo&#92;u1234bar', EscapeJavadoc('foo\\u1234bar'));
+  EXPECT_EQ('&#64;deprecated', EscapeJavadoc('@deprecated'));
+}
+    
+    void FileGenerator::GenerateSource(io::Printer *printer) {
+  // #import the runtime support.
+  std::set<string> headers;
+  headers.insert('GPBProtocolBuffers_RuntimeSupport.h');
+  PrintFileRuntimePreamble(printer, headers);
     }
     
-            for (; j < roiw32; j += 32)
-        {
-            internal::prefetch(src + j);
-            uint8x16_t v_src0 = vld1q_u8(src + j), v_src1 = vld1q_u8(src + j + 16);
-            uint8x16_t v_dst0 = vmvnq_u8(v_src0), v_dst1 = vmvnq_u8(v_src1);
-            vst1q_u8(dst + j, v_dst0);
-            vst1q_u8(dst + j + 16, v_dst1);
-        }
-        for (; j < roiw8; j += 8)
-        {
-            uint8x8_t v_src = vld1_u8(src + j);
-            uint8x8_t v_dst = vmvn_u8(v_src);
-            vst1_u8(dst + j, v_dst);
-        }
-    
-            vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        float32x4_t vline1Shifted_f32 = vaddq_f32(vline1_f32, vshift);
-        float32x4_t vline2Shifted_f32 = vaddq_f32(vline2_f32, vshift);
-        uint32x4_t vline1_u32 = vcvtq_u32_f32(vline1Shifted_f32);
-        uint32x4_t vline2_u32 = vcvtq_u32_f32(vline2Shifted_f32);
-        uint32x4_t vline1Mask = vbicq_u32(vmask, vreinterpretq_u32_f32(vline2_f32));
-        uint32x4_t vline2Mask = vbicq_u32(vmask, vreinterpretq_u32_f32(vline1Shifted_f32));
-        vline1Mask = vshrq_n_u32(vline1Mask, 16);
-        vline2Mask = vshrq_n_u32(vline2Mask, 16);
-        vline1_u32 = vqsubq_u32(vline1_u32, vline1Mask);
-        vline2_u32 = vqsubq_u32(vline2_u32, vline2Mask);
-        uint16x4_t vRes1 = vqrshrn_n_u32(vline1_u32, 16);
-        uint16x4_t vRes2 = vqrshrn_n_u32(vline2_u32, 16);
-        uint8x8_t vRes = vqmovn_u16(vcombine_u16(vRes1, vRes2));
+    // Death tests do not work on Windows as of yet.
+#ifdef PROTOBUF_HAS_DEATH_TEST
+TEST(ObjCHelperDeathTest, TextFormatDecodeData_DecodeDataForString_Failures) {
+  // Empty inputs.
+    }
     
     
-    {                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t0_16s), kernelBase[5]);
-                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t1_16s), kernelBase[4]);
-                v_dst1 = vmlal_n_s16(v_dst1, vget_high_s16(t2_16s), kernelBase[3]);
-            }
+    {
+    {
+    {}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
     
-                for (; i <= lim; i+= 16)
+    #endif 
+    
+        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0, const typename internal::VecTraits<T>::vec64 & v_src1,
+              typename internal::VecTraits<T>::unsign::vec64 & v_dst) const
+    {
+        v_dst = internal::vceq(v_src0, v_src1);
+    }
+    
+    #if defined WITH_NEON && (defined __ARM_NEON__ || defined __ARM_NEON)
+#define CAROTENE_NEON
+#endif
+    
+    
+    {
+    {
+    {            for (; j < size.width; j++)
             {
-                internal::prefetch(src + i);
-                uint8x16_t vln = vld1q_u8(src + i);
-                uint8x16_t vnz = vminq_u8(vln, vc1);
-                vs = vaddq_u8(vs, vnz);
+                dst[j] = (u8)src[j];
             }
-    
-    
-    {            ws = vpadalq_u32(ws, s1);
-            ws = vpadalq_u32(ws, s2);
         }
+    }
+}
     
     
     {
-    {}}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    #include <cstdint>
-#include <tuple>
-    
-    Variant PlainDirectory::read() {
-  struct dirent entry;
-  struct dirent *result;
-  int ret = readdir_r(m_dir, &entry, &result);
-  if (ret != 0 || !result) {
-    return false;
-  }
-  return String(entry.d_name, CopyString);
+    {
+    {            vst1_u16(dst + x, ls);
+        }
+        for (s32 h = 0; h < cn; ++h)
+        {
+            u32* ln = lane + h;
+            u16* dt = dst + h;
+            for (size_t k = x; k < colsn; k += cn)
+            {
+                dt[k] = (u16)((ln[k-2*cn] + ln[k+2*cn] + 4*(ln[k-cn] + ln[k+cn]) + 6*ln[k] + (1<<7))>>8);
+            }
+        }
+    }
+#else
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)borderValue;
+    (void)borderMargin;
+#endif
 }
     
-      char** newargv_array = (char**)alloca(sizeof(char*) * (newargv.size() + 1));
-  for (unsigned i = 0; i < newargv.size(); i++) {
-    // printf('%s\n', newargv[i].data());
-    newargv_array[i] = (char *)newargv[i].data();
+    template <typename T, int elsize> struct vtail
+{
+    static inline void inRange(const T *, const T *, const T *,
+                               u8 *, size_t &, size_t)
+    {
+        //do nothing since there couldn't be enough data
+    }
+};
+template <typename T> struct vtail<T, 2>
+{
+    static inline void inRange(const T * src, const T * rng1, const T * rng2,
+                               u8 * dst, size_t &x, size_t width)
+    {
+        typedef typename internal::VecTraits<T>::vec128 vec128;
+        typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
+        //There no more than 15 elements in the tail, so we could handle 8 element vector only once
+        if( x + 8 < width)
+        {
+             vec128  vs = internal::vld1q( src + x);
+             vec128 vr1 = internal::vld1q(rng1 + x);
+             vec128 vr2 = internal::vld1q(rng2 + x);
+            uvec128  vd = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
+            internal::vst1(dst + x, internal::vmovn(vd));
+            x+=8;
+        }
+    }
+};
+template <typename T> struct vtail<T, 1>
+{
+    static inline void inRange(const T * src, const T * rng1, const T * rng2,
+                               u8 * dst, size_t &x, size_t width)
+    {
+        typedef typename internal::VecTraits<T>::vec128 vec128;
+        typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
+        typedef typename internal::VecTraits<T>::vec64 vec64;
+        typedef typename internal::VecTraits<T>::unsign::vec64 uvec64;
+        //There no more than 31 elements in the tail, so we could handle once 16+8 or 16 or 8 elements
+        if( x + 16 < width)
+        {
+             vec128  vs = internal::vld1q( src + x);
+             vec128 vr1 = internal::vld1q(rng1 + x);
+             vec128 vr2 = internal::vld1q(rng2 + x);
+            uvec128  vd = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
+            internal::vst1q(dst + x, vd);
+            x+=16;
+        }
+        if( x + 8 < width)
+        {
+             vec64  vs = internal::vld1( src + x);
+             vec64 vr1 = internal::vld1(rng1 + x);
+             vec64 vr2 = internal::vld1(rng2 + x);
+            uvec64  vd = internal::vand(internal::vcge(vs, vr1), internal::vcge(vr2, vs));
+            internal::vst1(dst + x, vd);
+            x+=8;
+        }
+    }
+};
+    
+    ChannelArguments::~ChannelArguments() {
+  grpc_core::ExecCtx exec_ctx;
+  for (auto it = args_.begin(); it != args_.end(); ++it) {
+    if (it->type == GRPC_ARG_POINTER) {
+      it->value.pointer.vtable->destroy(it->value.pointer.p);
+    }
   }
-  // NULL-terminate the argument array.
-  newargv_array[newargv.size()] = nullptr;
+}
     
-    //////////////////////////////////////////////////////////////////////
+    std::vector<grpc::string_ref> SecureAuthContext::FindPropertyValues(
+    const grpc::string& name) const {
+  if (!ctx_) {
+    return std::vector<grpc::string_ref>();
+  }
+  grpc_auth_property_iterator iter =
+      grpc_auth_context_find_properties_by_name(ctx_, name.c_str());
+  const grpc_auth_property* property = nullptr;
+  std::vector<grpc::string_ref> values;
+  while ((property = grpc_auth_property_iterator_next(&iter))) {
+    values.push_back(grpc::string_ref(property->value, property->value_length));
+  }
+  return values;
+}
     
-      while (true) {
-    int seg_len = 0;
-    char cur = from_start[0];
-    while (cur && !isDirSeparator(cur)) {
-      ++seg_len;
-      cur = from_start[seg_len];
+    #include <grpc/support/port_platform.h>
+    
+    
+    {}  // namespace
+    
+     public:
+  /*! \brief cuda kernel argument descriptor */
+  struct ArgType {
+    /*! \brief whether argument is NDArray */
+    bool is_ndarray;
+    /*! \brief whether argument is constant (input) */
+    bool is_const;
+    /*! \brief data type of argument */
+    mshadow::TypeFlag dtype;
+  };
+  /*! \brief Cuda kernel */
+  class Kernel {
+   public:
+    /*! \brief Launch the kernel */
+    void Launch(const Context& ctx, const std::vector<dmlc::any>& args,
+                uint32_t grid_dim_x, uint32_t grid_dim_y, uint32_t grid_dim_z,
+                uint32_t block_dim_x, uint32_t block_dim_y, uint32_t block_dim_z,
+                uint32_t shared_mem);
+    /*! \brief kernel interface signature */
+    const std::vector<ArgType>& signature() { return signature_; }
+    }
+    
+    /*!
+ * Copyright (c) 2016 by Contributors
+ * \file caffe_blob.cc
+ * \brief Implementations of SetDataGradToBlob given various device/dimension
+ * \author Haoran Wang
+*/
+#include 'caffe_blob.h'
+namespace mxnet {
+namespace op {
+namespace caffe {
     }
     }
+    }
+    
+    
+    {    return true;
+  }
+    
+      virtual void BeforeFirst() {
+    data_parser_->BeforeFirst();
+    if (label_parser_.get() != nullptr) {
+      label_parser_->BeforeFirst();
+    }
+    data_ptr_ = label_ptr_ = 0;
+    data_size_ = label_size_ = 0;
+    inst_counter_ = 0;
+    end_ = false;
+  }
+    
+    
+    {  // Write must fail because of corrupted table
+  std::string tmp1, tmp2;
+  Status s = db_->Put(WriteOptions(), Key(5, &tmp1), Value(5, &tmp2));
+  ASSERT_TRUE(!s.ok()) << 'write did not fail in corrupted paranoid db';
+}
+    
+    #endif  // STORAGE_LEVELDB_DB_DB_ITER_H_
+
+    
+    TEST(FormatTest, InternalKeyShortestSuccessor) {
+  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
+            ShortSuccessor(IKey('foo', 100, kTypeValue)));
+  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
+            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
+}
+    
+    
+    {  // Errors
+  static const char* errors[] = {
+    '',
+    'foo',
+    'foo-dx-100.log',
+    '.log',
+    '',
+    'manifest',
+    'CURREN',
+    'CURRENTX',
+    'MANIFES',
+    'MANIFEST',
+    'MANIFEST-',
+    'XMANIFEST-3',
+    'MANIFEST-3x',
+    'LOC',
+    'LOCKx',
+    'LO',
+    'LOGx',
+    '18446744073709551616.log',
+    '184467440737095516150.log',
+    '100',
+    '100.',
+    '100.lop'
+  };
+  for (int i = 0; i < sizeof(errors) / sizeof(errors[0]); i++) {
+    std::string f = errors[i];
+    ASSERT_TRUE(!ParseFileName(f, &number, &type)) << f;
+  }
+}
+    
+    #include <set>
+    
+    std::shared_ptr<PlatformProcess> PlatformProcess::launchExtension(
+    const std::string& exec_path,
+    const std::string& extensions_socket,
+    const std::string& extensions_timeout,
+    const std::string& extensions_interval,
+    bool verbose) {
+  auto ext_pid = ::fork();
+  if (ext_pid < 0) {
+    return std::shared_ptr<PlatformProcess>();
+  } else if (ext_pid == 0) {
+    setEnvVar('OSQUERY_EXTENSION', std::to_string(::getpid()).c_str());
+    }
+    }
+    
+    #ifndef WIN32
+#include <unistd.h>
+#endif
+    
+    class INotifyTests : public testing::Test {
+ protected:
+  void SetUp() override {
+    // INotify will use data from the config and config parsers.
+    Registry::get().registry('config_parser')->setUp();
+    }
+    }
+    
+    static int kBellHathTolled = 0;
+    
+        Row r;
+    r['example_text'] = 'example';
+    r['example_integer'] = INTEGER(1);
+    
+    NS_CC_BEGIN
+//
+// CameraAction
+//
+ActionCamera::ActionCamera()
+: _center(0, 0, 0)
+, _eye(0, 0, FLT_EPSILON)
+, _up(0, 1, 0)
+{
+}
+void ActionCamera::startWithTarget(Node *target)
+{
+    ActionInterval::startWithTarget(target);
+}
+    
+    // CatmullRom Spline formula:
+Vec2 ccCardinalSplineAt(const Vec2 &p0, const Vec2 &p1, const Vec2 &p2, const Vec2 &p3, float tension, float t)
+{
+    float t2 = t * t;
+    float t3 = t2 * t;
+    
+    /*
+     * Formula: s(-ttt + 2tt - t)P1 + s(-ttt + tt)P2 + (2ttt - 3tt + 1)P2 + s(ttt - 2tt + t)P3 + (-2ttt + 3tt)P3 + s(ttt - tt)P4
+     */
+    float s = (1 - tension) / 2;
+    }
+    
+    
+    {        return true;
+    }
+    
+                // We scale z here to avoid the animation being
+            // too much bigger than the screen due to perspective transform
