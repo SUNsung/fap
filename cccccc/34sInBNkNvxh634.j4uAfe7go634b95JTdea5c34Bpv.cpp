5e592f71dc9131da8c4f2ab2b751d6eee649ecbf
SUNsung <weatherius@gmail.com>
2018-11-28 07:12:32 +0200

    Rewrite [34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp] File in: 2018-11-28 07:12:32

--- cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
+++ cccccc/34sInBNkNvxh634.j4uAfe7go634b95JTdea5c34Bpv.cpp
@@ -1,333 +1,40 @@
 
         
-         protected:
-   /**
-   * @brief Generates a random integer from Uniform({0, 1, ..., n-1}).
-   *
-   * @param n
-   *    The upperbound (exclusive) value of the random number.
-   * @return
-   *    A uniformly random integer value from ({0, 1, ..., n-1}).
-   */
-  virtual int Rand(int n);
-    
-    
-    {  /**
-   * @brief Computes the error gradient w.r.t. the absolute value inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x} =
-   *            \mathrm{sign}(x) \frac{\partial E}{\partial y}
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-};
-    
-    
-    {  /// Whether to ignore instances with a certain label.
-  bool has_ignore_label_;
-  /// The label indicating that an instance should be ignored.
-  int ignore_label_;
-  /// Keeps counts of the number of samples per class.
-  Blob<Dtype> nums_buffer_;
-};
-    
-    /**
- * @brief Compute the index of the @f$ K @f$ max values for each datum across
- *        all dimensions @f$ (C \times H \times W) @f$.
- *
- * Intended for use after a classification layer to produce a prediction.
- * If parameter out_max_val is set to true, output is a vector of pairs
- * (max_ind, max_val) for each image. The axis parameter specifies an axis
- * along which to maximise.
- *
- * NOTE: does not implement Backwards operation.
- */
-template <typename Dtype>
-class ArgMaxLayer : public Layer<Dtype> {
- public:
-  /**
-   * @param param provides ArgMaxParameter argmax_param,
-   *     with ArgMaxLayer options:
-   *   - top_k (\b optional uint, default 1).
-   *     the number @f$ K @f$ of maximal items to output.
-   *   - out_max_val (\b optional bool, default false).
-   *     if set, output a vector of pairs (max_ind, max_val) unless axis is set then
-   *     output max_val along the specified axis.
-   *   - axis (\b optional int).
-   *     if set, maximise along the specified axis else maximise the flattened
-   *     trailing dimensions for each index of the first / num dimension.
-   */
-  explicit ArgMaxLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    /**
- * @brief Abstract base class that factors out the BLAS code common to
- *        ConvolutionLayer and DeconvolutionLayer.
- */
-template <typename Dtype>
-class BaseConvolutionLayer : public Layer<Dtype> {
- public:
-  explicit BaseConvolutionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    /**
- * @brief Normalizes the input to have 0-mean and/or unit (1) variance across
- *        the batch.
- *
- * This layer computes Batch Normalization as described in [1]. For each channel
- * in the data (i.e. axis 1), it subtracts the mean and divides by the variance,
- * where both statistics are computed across both spatial dimensions and across
- * the different examples in the batch.
- *
- * By default, during training time, the network is computing global
- * mean/variance statistics via a running average, which is then used at test
- * time to allow deterministic outputs for each input. You can manually toggle
- * whether the network is accumulating or using the statistics via the
- * use_global_stats option. For reference, these statistics are kept in the
- * layer's three blobs: (0) mean, (1) variance, and (2) moving average factor.
- *
- * Note that the original paper also included a per-channel learned bias and
- * scaling factor. To implement this in Caffe, define a `ScaleLayer` configured
- * with `bias_term: true` after each `BatchNormLayer` to handle both the bias
- * and scaling factor.
- *
- * [1] S. Ioffe and C. Szegedy, 'Batch Normalization: Accelerating Deep Network
- *     Training by Reducing Internal Covariate Shift.' arXiv preprint
- *     arXiv:1502.03167 (2015).
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-template <typename Dtype>
-class BatchNormLayer : public Layer<Dtype> {
- public:
-  explicit BatchNormLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    namespace caffe {
-    }
-    
-     protected:
-  /// @copydoc BNLLLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    #include 'caffe/layers/base_conv_layer.hpp'
-    
-    #ifdef USE_CUDNN
-template <typename Dtype>
-class CuDNNLCNLayer : public LRNLayer<Dtype> {
- public:
-  explicit CuDNNLCNLayer(const LayerParameter& param)
-      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
-        tempData1(NULL), tempData2(NULL) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNLCNLayer();
-    }
-    
-      virtual void PredictInteractionContributions(DMatrix* dmat,
-                                   std::vector<bst_float>* out_contribs,
-                                   const gbm::GBTreeModel& model,
-                                   unsigned ntree_limit = 0,
-                                   bool approximate = false) = 0;
-    
-      void BeforeFirst() override {
-    parser_->BeforeFirst();
+          auto cv = config.find(kLoggerKey);
+  if (cv != config.end()) {
+    auto obj = data_.getObject();
+    data_.copyFrom(cv->second.doc(), obj);
+    data_.add(kLoggerKey, obj);
   }
     
-    #if DMLC_ENABLE_STD_THREAD
-/*!
- * \brief A threaded writer to write sparse batch page to sharded files.
- */
-class SparsePageWriter {
- public:
-  /*!
-   * \brief constructor
-   * \param name_shards name of shard files.
-   * \param format_shards format of each shard.
-   * \param extra_buffer_capacity Extra buffer capacity before block.
-   */
-  explicit SparsePageWriter(
-      const std::vector<std::string>& name_shards,
-      const std::vector<std::string>& format_shards,
-      size_t extra_buffer_capacity);
-  /*! \brief destructor, will close the files automatically */
-  ~SparsePageWriter();
-  /*!
-   * \brief Push a write job to the writer.
-   * This function won't block,
-   * writing is done by another thread inside writer.
-   * \param page The page to be written
-   */
-  void PushWrite(std::shared_ptr<SparsePage>&& page);
-  /*!
-   * \brief Allocate a page to store results.
-   *  This function can block when the writer is too slow and buffer pages
-   *  have not yet been recycled.
-   * \param out_page Used to store the allocated pages.
-   */
-  void Alloc(std::shared_ptr<SparsePage>* out_page);
-    }
+    #include <osquery/config.h>
+#include <osquery/registry.h>
     
     
-    {
-    {
-    {  // base margin
-  bst_float base_margin;
-  // model parameter
-  GBTreeModelParam param;
-  /*! \brief vector of trees stored in the model */
-  std::vector<std::unique_ptr<RegTree> > trees;
-  /*! \brief for the update process, a place to keep the initial trees */
-  std::vector<std::unique_ptr<RegTree> > trees_to_update;
-  /*! \brief some information indicator of the tree, reserved */
-  std::vector<int> tree_info;
-};
-}  // namespace gbm
-}  // namespace xgboost
-
-    
-    bool SetDepth(int p0, HuffmanTree *pool, uint8_t *depth, int max_depth) {
-  int stack[17];
-  int level = 0;
-  int p = p0;
-  assert(max_depth <= 16);
-  stack[0] = -1;
-  while (true) {
-    if (pool[p].index_left_ >= 0) {
-      level++;
-      if (level > max_depth) return false;
-      stack[level] = pool[p].index_right_or_value_;
-      p = pool[p].index_left_;
-      continue;
-    } else {
-      depth[pool[p].index_right_or_value_] = static_cast<uint8_t>(level);
-    }
-    while (level >= 0 && stack[level] == -1) level--;
-    if (level < 0) return true;
-    p = stack[level];
-    stack[level] = -1;
+    {  // Any views left are views that don't exist in the new configuration file
+  // so we tear them down and remove them from the database.
+  for (const auto& old_view : erase_views) {
+    osquery::query('DROP VIEW ' + old_view, r);
+    deleteDatabaseValue(kQueries, kConfigViews + old_view);
   }
+  return Status(0, 'OK');
 }
     
-    std::string ReadFileOrDie(const char* filename) {
-  bool read_from_stdin = strncmp(filename, '-', 2) == 0;
+    Status Distributed::runQueries() {
+  while (getPendingQueryCount() > 0) {
+    auto request = popRequest();
+    LOG(INFO) << 'Executing distributed query: ' << request.id << ': '
+              << request.query;
+    }
     }
     
+      auto socket_flag = FLAGS_audit_allow_unix;
+  FLAGS_audit_allow_unix = true;
+  Row r4;
+  std::string msg3 = '01002F746D702F6F7371756572792E656D0000';
+  parseSockAddr(msg3, r4, unix_socket);
+  ASSERT_FALSE(r4['socket'].empty());
+  EXPECT_EQ(r4['socket'], '/tmp/osquery.em');
     
-    {}  // namespace guetzli
-    
-    // Huffman code and decoding lookup table used for DC and AC coefficients.
-struct JPEGHuffmanCode {
-  JPEGHuffmanCode() : counts(kJpegHuffmanMaxBitLength + 1),
-                      values(kJpegHuffmanAlphabetSize + 1),
-                      slot_id(0),
-                      is_last(true) {}
-    }
-    
-      void AddOtherAgent(CanAgent *agent) { other_agent_ = agent; }
-    
-    
-    {
-    {
-    {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    #include 'gflags/gflags.h'
-#include 'modules/common/proto/error_code.pb.h'
-#include 'modules/drivers/canbus/can_client/can_client.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-#include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
-    
-    
-    {
-    {
-    {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    #include <vector>
-    
-    /**
- * @class MessageManager
- *
- * @brief message manager manages protocols. It supports parse and can get
- * protocol data by message id.
- */
-template <typename SensorType>
-class MessageManager {
- public:
-  /*
-  * @brief constructor function
-  */
-  MessageManager() {}
-  /*
-   * @brief destructor function
-   */
-  virtual ~MessageManager() = default;
-    }
-    
-    #include 'gtest/gtest.h'
-    
-    // (SUM(input))^0xFF
-template <typename SensorType>
-uint8_t ProtocolData<SensorType>::CalculateCheckSum(const uint8_t *input,
-                                                    const uint32_t length) {
-  return std::accumulate(input, input + length, 0) ^ 0xFF;
-}
-    
-    std::string Byte::to_binary_string() const { return byte_to_binary(*value_); }
-    
-      /**
-   * @brief Reset the lower 4 bits as the lower 4 bits of a specified one-byte
-   *        unsigned integer.
-   * @param value The one-byte unsigned integer whose lower 4 bits are used to
-   *        set this Byte's lower 4 bits.
-   */
-  void set_value_low_4_bits(const uint8_t value);
-    
-      // last step: publish monitor messages
-  common::monitor::MonitorLogBuffer buffer(&monitor_logger_);
-  buffer.INFO('Canbus is started.');
-    
-    // System gflags
-DEFINE_string(sensor_node_name, '', 'Sensor node name.');
+    #include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/path.hpp>
