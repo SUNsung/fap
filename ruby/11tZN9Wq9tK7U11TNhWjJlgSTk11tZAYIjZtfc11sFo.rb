2f2402004a6ae74a4b89ddb823e05cbf0e50dd57
SUNsung <weatherius@gmail.com>
2018-11-29 00:55:45 +0200

    Rewrite [11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb] File in: 2018-11-29 00:55:45

--- ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
+++ ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
@@ -1,135 +1,63 @@
 
         
-          it 'shows the dry run pop up without previous events and selects the log tab when no event was created' do
-    stub_request(:get, 'http://xkcd.com/').
-      with(:headers => {'Accept-Encoding'=>'gzip,deflate', 'User-Agent'=>'Huginn - https://github.com/huginn/huginn'}).
-      to_return(:status => 200, :body => '', :headers => {})
-    
-      it 'imports a scenario which requires a service' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
-    click_on 'Start Import'
-    check('I confirm that I want to import these Agents.')
-    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
-    expect(page).to have_text('Import successful!')
+          if tap.nil? || tap.core_tap?
+    full_name = name
+  else
+    full_name = '#{tap}/#{name}'
   end
-end
-
     
-        it 'in the future' do
-      expect(relative_distance_of_time_in_words(Time.now+5.minutes)).to eq('in 5m')
-    end
-  end
-end
-
-    
-    require 'vagrant/capability_host'
-    
-            @id               = id
-        @local_data_path  = raw['local_data_path']
-        @name             = raw['name']
-        @provider         = raw['provider']
-        @state            = raw['state']
-        @vagrantfile_name = raw['vagrantfile_name']
-        @vagrantfile_path = raw['vagrantfile_path']
-        # TODO(mitchellh): parse into a proper datetime
-        @updated_at       = raw['updated_at']
-        @extra_data       = raw['extra_data'] || {}
-    
-            # Capture all bad configuration calls and save them for an error
-        # message later during validation.
-        def method_missing(name, *args, &block)
-          return super if @__finalized
-    
-          if !@options[:update] && !@options[:watch] &&
-          @args.first && colon_path?(@args.first)
-        if @args.size == 1
-          @args = split_colon_path(@args.first)
-        else
-          @fake_update = true
-          @options[:update] = true
+            if file_is_stale || ARGV.switch?('s') && !f.installed? || bottle_file_outdated?(f, file)
+          cleanup_path(file) { file.unlink }
         end
       end
-      load_compass if @options[:compass]
-      return interactive if @options[:interactive]
-      return watch_or_update if @options[:watch] || @options[:update]
-      super
+    end
     
-          # If this importer is based on files on the local filesystem This method
-      # should return true if the file, when changed, should trigger a
-      # recompile.
-      #
-      # It is acceptable for non-sass files to be watched and trigger a recompile.
-      #
-      # @param filename [String] The absolute filename for a file that has changed.
-      # @return [Boolean] When the file changed should cause a recompile.
-      def watched_file?(filename)
-        false
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
       end
-    end
+      cmds
+    end.sort
   end
-end
-
     
-          # Create an Atom-feed for each index.
-      feed = CategoryFeed.new(self, self.source, category_dir, category)
-      feed.render(self.layouts, site_payload)
-      feed.write(self.dest)
-      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
-      self.pages << feed
-    end
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+        if ARGV.include?('--pinned') || ARGV.include?('--versions')
+      filtered_list
+    elsif ARGV.named.empty?
+      if ARGV.include? '--full-name'
+        full_names = Formula.installed.map(&:full_name).sort do |a, b|
+          if a.include?('/') && !b.include?('/')
+            1
+          elsif !a.include?('/') && b.include?('/')
+            -1
+          else
+            a <=> b
+          end
         end
-        video += '</video>'
+        puts_columns full_names
       else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+        ENV['CLICOLOR'] = nil
+        exec 'ls', *ARGV.options_only << HOMEBREW_CELLAR
       end
+    elsif ARGV.verbose? || !$stdout.tty?
+      exec 'find', *ARGV.kegs.map(&:to_s) + %w[-not -type d -print]
+    else
+      ARGV.kegs.each { |keg| PrettyListing.new keg }
     end
-    
-    module LogStash module PluginManager module PackFetchStrategy
-  class Repository
-    DEFAULT_PACK_URL = 'https://artifacts.elastic.co/downloads/logstash-plugins'
-    PACK_EXTENSION = 'zip'
-    
-        # any errors will be logged to $stderr by invoke!
-    # Bundler cannot update and clean gems in one operation so we have to call the CLI twice.
-    options = {:update => plugins, :rubygems_source => gemfile.gemset.sources}
-    options[:local] = true if local?
-    output = LogStash::Bundler.invoke!(options)
-    # We currently dont removed unused gems from the logstash installation
-    # see: https://github.com/elastic/logstash/issues/6339
-    # output = LogStash::Bundler.invoke!(:clean => true)
-    display_updated_plugins(previous_gem_specs_map)
-  rescue => exception
-    gemfile.restore!
-    report_exception('Updated Aborted', exception)
-  ensure
-    display_bundler_output(output)
   end
     
-          include_examples('safe URI', :exclude_password_specs => true)
+            output = String.new
+        output << %Q|<link rel='next' href='#{next_page}'></link>| if next_page
+        output << %Q|<link rel='prev' href='#{prev_page}'></link>| if prev_page
+        output.html_safe
+      end
     end
+  end
+end
+
     
-    shared_examples 'logstash update' do |logstash|
-  describe 'logstash-plugin update on #{logstash.hostname}' do
-    before :each do
-      logstash.install({:version => LOGSTASH_VERSION})
-    end
-    
-            def left_brace_and_space(loc_begin, space)
-          range_with_surrounding_space(range: loc_begin,
-                                       side: :right,
-                                       newlines: space[:newlines],
-                                       whitespace: space[:left])
-        end
+          def params_for(page)
+        page_params = Rack::Utils.parse_nested_query('#{@param_name}=#{page}')
+        page_params = @params.deep_merge(page_params)
