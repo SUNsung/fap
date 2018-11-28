a447fd3a31d6da461066b46bccc0f800b686d579
SUNsung <weatherius@gmail.com>
2018-11-28 09:43:44 +0200

    Rewrite [20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb] File in: 2018-11-28 09:43:44

--- ruby/20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb
+++ ruby/20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb
@@ -1,165 +1,115 @@
 
         
-              check_value =
-        case check_value
-        when Integer
-          check_value
-        when Symbol
-          record.public_send(check_value) # rubocop:disable GitlabSecurity/PublicSend
-        end
+        desc 'Open an irb session preloaded with this library'
+task :console do
+  sh 'irb -rubygems -r ./lib/#{name}.rb'
+end
+
     
-          def initialize(current_user, oauth)
-        @current_user = current_user
-        @oauth = oauth
-        @changed = false
+      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
+    cmd = g.split('-').last
+    p.command(cmd.to_sym) do |c|
+      c.syntax cmd
+      c.action do
+        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
+          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
       end
-    
-          def find_sessionless_user
-        find_user_from_access_token || find_user_from_feed_token
-      rescue Gitlab::Auth::AuthenticationError
-        nil
-      end
-    
-            def status
-          @coverage ||= raw_coverage
-          return unless @coverage
-    
-          def initialize(badge)
-        @badge = badge
-      end
-    
-      def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to after_omniauth_failure_path_for(resource_name)
+    end
   end
     
-      # The parent mailer all Devise mailers inherit from.
-  # Defaults to ActionMailer::Base. This should be set early
-  # in the initialization process and should be set to a string.
-  mattr_accessor :parent_mailer
-  @@parent_mailer = 'ActionMailer::Base'
+            # rubocop:disable Metrics/AbcSize
+        def process(args, opts)
+          if !args || args.empty?
+            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
+          end
     
-    require 'uri'
+            def initialize(_opts)
+          # If EventMachine SSL support on Windows ever gets better, the code below will
+          # set up the reactor to handle SSL
+          #
+          # @ssl_enabled = opts['ssl_cert'] && opts['ssl_key']
+          # if @ssl_enabled
+          #   em_opts[:tls_options] = {
+          #   :private_key_file => Jekyll.sanitized_path(opts['source'], opts['ssl_key']),
+          #   :cert_chain_file  => Jekyll.sanitized_path(opts['source'], opts['ssl_cert'])
+          #   }
+          #   em_opts[:secure] = true
+          # end
+    
+        def no_subcommand(args)
+      unless args.empty? ||
+          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
+        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
+                          Run `jekyll help` to find out more.'
+        abort
+      end
+    end
     
           private
     
-        # Try retrieving the URL options from the parent controller (usually
-    # ApplicationController). Instance methods are not supported at the moment,
-    # so only the class-level attribute is used.
-    def self.default_url_options(*args)
-      if defined?(Devise.parent_controller.constantize)
-        Devise.parent_controller.constantize.try(:default_url_options) || {}
-      else
-        {}
-      end
+          private
+    
+    if defined?(::ActionDispatch::Request::Session) &&
+    !::ActionDispatch::Request::Session.method_defined?(:each)
+  # mperham/sidekiq#2460
+  # Rack apps can't reuse the Rails session store without
+  # this monkeypatch, fixed in Rails 5.
+  class ActionDispatch::Request::Session
+    def each(&block)
+      hash = self.to_hash
+      hash.each(&block)
+    end
+  end
+end
+
+    
+        def initialize(env, block)
+      @_erb = false
+      @env = env
+      @block = block
+      @@files ||= {}
     end
     
-        if record.timedout?(last_request_at) &&
-        !env['devise.skip_timeout'] &&
-        !proxy.remember_me_is_active?(record)
-      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
-      throw :warden, scope: scope, message: :timeout
-    end
-    
-            # Yields a VM for each target VM for the command.
-        #
-        # This is a convenience method for easily implementing methods that
-        # take a target VM (in the case of multi-VM) or every VM if no
-        # specific VM name is specified.
-        #
-        # @param [String] name The name of the VM. Nil if every VM.
-        # @param [Boolean] single_target If true, then an exception will be
-        #   raised if more than one target is found.
-        def with_target_vms(names=nil, options=nil)
-          # Using VMs requires a Vagrant environment to be properly setup
-          raise Errors::NoEnvironmentError if !@env.root_path
-    
-            def initialize(argv, env)
-          @argv = argv
-          @env  = env
-          @logger = Log4r::Logger.new('vagrant::command::#{self.class.to_s.downcase}')
-        end
-    
-            # A default to_s implementation.
-        def to_s
-          self.class.to_s
-        end
-    
-            # This returns all the registered guests.
-        #
-        # @return [Hash]
-        def hosts
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.hosts)
-            end
-          end
-        end
-    
-            # Returns the internal data associated with this plugin. This
-        # should NOT be called by the general public.
-        #
-        # @return [Hash]
-        def self.data
-          @data ||= {}
-        end
-    
-      def up
-    Photo.joins('INNER JOIN posts ON posts.guid = photos.status_message_guid')
-         .where(posts: {type: 'StatusMessage', public: true}).update_all(public: true)
-    
-    class PolymorphicMentions < ActiveRecord::Migration[4.2]
-  def change
-    remove_index :mentions, column: %i(post_id)
-    remove_index :mentions, column: %i(person_id post_id), unique: true
-    rename_column :mentions, :post_id, :mentions_container_id
-    add_column :mentions, :mentions_container_type, :string
-    add_index :mentions,
-              %i(mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_mc_id_and_mc_type',
-              length: {mentions_container_type: 191}
-    add_index :mentions,
-              %i(person_id mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_person_and_mc_id_and_mc_type',
-              length: {mentions_container_type: 191},
-              unique: true
-    
-    # If you set this to false, any error raised from within your app will bubble
-# up to your step definition and out to cucumber unless you catch it somewhere
-# on the way. You can make Rails rescue errors and render error pages on a
-# per-scenario basis by tagging a scenario or feature with the @allow-rescue tag.
-#
-# If you set this to true, Rails will rescue all errors and render error
-# pages, more or less in the same way your application would behave in the
-# default production environment. It's not recommended to do this for all
-# of your scenarios, as this makes it hard to discover errors in your application.
-ActionController::Base.allow_rescue = false
-    
-    module Pod
-  require 'pathname'
-  require 'tmpdir'
-    
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
+    Given /^I comment out lines that contain '([^']+)' in '([^']+)'$/ do |contains, glob|
+  cd('.') do
+    Dir.glob(glob).each do |file|
+      transform_file(file) do |content|
+        content.gsub(/^(.*?#{contains}.*?)$/) { |line| '# #{line}' }
       end
     end
   end
 end
+    
+    
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'paths'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'selectors'))
+    
+    After do
+  ORIGINAL_BUNDLE_VARS.each_pair do |key, value|
+    ENV[key] = value
+  end
+end
+    
+      def generate_migration
+    migration_template('paperclip_migration.rb.erb',
+                       'db/migrate/#{migration_file_name}',
+                       migration_version: migration_version)
+  end
+    
+        def self.register(klass, attachment_name, attachment_options)
+      instance.register(klass, attachment_name, attachment_options)
+    end
+    
+        def define_query
+      name = @name
+      @klass.send :define_method, '#{@name}?' do
+        send(name).file?
+      end
+    end
+    
+        # Returns the fingerprint of the instance.
+    def fingerprint attachment, style_name
+      attachment.fingerprint
+    end
