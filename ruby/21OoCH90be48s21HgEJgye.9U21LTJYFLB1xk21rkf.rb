bf563ff6311982598e6c8605cf1f91ef427c79c2
SUNsung <weatherius@gmail.com>
2018-11-29 21:15:56 +0200

    Rewrite [21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb] File in: 2018-11-29 21:15:55

--- ruby/21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb
+++ ruby/21OoCH90be48s21HgEJgye.9U21LTJYFLB1xk21rkf.rb
@@ -1,52 +1,160 @@
 
         
-            it 'shows the dry run pop up without previous events and selects the events tab when a event was created' do
-      open_dry_run_modal(agent)
-      click_on('Dry Run')
-      expect(page).to have_text('Biologists play reverse')
-      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
-    end
+                  def render_collection
+            @collection.map do |item|
+              value = value_for_collection(item, @value_method)
+              text  = value_for_collection(item, @text_method)
+              default_html_options = default_html_options_for_collection(item, value)
+              additional_html_options = option_html_attributes(item)
     
-          it 'loads all workers' do
-        workers = @agent_runner.send(:load_workers)
-        expect(workers).to be_a(Hash)
-        expect(workers.keys).to eq(['HuginnScheduler', 'DelayedJobWorker'])
-      end
-    
-        it 'requires a valid log level' do
-      @log.level = nil
-      expect(@log).not_to be_valid
-      expect(@log).to have(1).error_on(:level)
-    
-      context 'when arguments to a method' do
-    let(:prefix) { 'bar(' }
-    let(:suffix) { ')' }
-    let(:source) { construct(false, true) }
-    
-          # Checks whether this node body is a void context.
-      # Always `true` for `for`.
-      #
-      # @return [true] whether the `for` node body is a void context
-      def void_context?
-        true
-      end
-    
-          # This is used for duck typing with `pair` nodes which also appear as
-      # `hash` elements.
-      #
-      # @return [false]
-      def colon?
-        false
-      end
-    
-          # Whether the last argument of the node is a block pass,
-      # i.e. `&block`.
-      #
-      # @return [Boolean] whether the last argument of the node is a block pass
-      def block_argument?
-        arguments? &&
-          (last_argument.block_pass_type? || last_argument.blockarg_type?)
+              def render_component(builder)
+            builder.radio_button + builder.label
+          end
       end
     end
   end
 end
+
+    
+            private
+    
+        initializer 'action_view.embed_authenticity_token_in_remote_forms' do |app|
+      ActiveSupport.on_load(:action_view) do
+        ActionView::Helpers::FormTagHelper.embed_authenticity_token_in_remote_forms =
+          app.config.action_view.delete(:embed_authenticity_token_in_remote_forms)
+      end
+    end
+    
+        # Direct access to partial rendering.
+    def render_partial(context, options, &block) #:nodoc:
+      PartialRenderer.new(@lookup_context).render(context, options, block)
+    end
+    
+      url = File.dirname(url)
+  url == FORWARD_SLASH ? url : '#{url}/'
+end
+    
+      config.on_event :test_case_started do |event|
+    f.print_feature_element_name(event.test_case)
+    f.before_feature_element(event.test_case)
+  end
+    
+            def convert(content)
+          document = Kramdown::Document.new(content, @config)
+          html_output = document.to_html
+          if @config['show_warnings']
+            document.warnings.each do |warning|
+              Jekyll.logger.warn 'Kramdown warning:', warning
+            end
+          end
+          html_output
+        end
+    
+        def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
+    end
+  end
+end
+
+    
+    Group.user_trust_level_change!(-1, TrustLevel[4])
+    
+    module Homebrew
+  module_function
+    
+      gem.licenses      = ['MIT']
+    
+    Then(/^the failure task will not run$/) do
+  failed = TestApp.shared_path.join('failed')
+  expect { run_vagrant_command(test_file_exists(failed)) }
+    .to raise_error(VagrantHelpers::VagrantSSHCommandError)
+end
+    
+        def print_config_variables
+      ['--print-config-variables', '-p',
+       'Display the defined config variables before starting the deployment tasks.',
+       lambda do |_value|
+         Configuration.env.set(:print_config_variables, true)
+       end]
+    end
+  end
+end
+
+    
+        def set_if_empty(key, value=nil, &block)
+      set(key, value, &block) unless keys.include?(key)
+    end
+    
+          def add_host(host, properties={})
+        new_host = Server[host]
+        new_host.port = properties[:port] if properties.key?(:port)
+        # This matching logic must stay in sync with `Server#matches?`.
+        key = ServerKey.new(new_host.hostname, new_host.port)
+        existing = servers_by_key[key]
+        if existing
+          existing.user = new_host.user if new_host.user
+          existing.with(properties)
+        else
+          servers_by_key[key] = new_host.with(properties)
+        end
+      end
+    
+          def assert_value_or_block_not_both(value, block)
+        return if value.nil? || block.nil?
+        raise Capistrano::ValidationError,
+              'Value and block both passed to Configuration#set'
+      end
+    
+    module Capistrano
+  class Configuration
+    # Holds the variables assigned at Capistrano runtime via `set` and retrieved
+    # with `fetch`. Does internal bookkeeping to help identify user mistakes
+    # like spelling errors or unused variables that may lead to unexpected
+    # behavior.
+    class Variables
+      CAPISTRANO_LOCATION = File.expand_path('../..', __FILE__).freeze
+      IGNORED_LOCATIONS = [
+        '#{CAPISTRANO_LOCATION}/configuration/variables.rb:',
+        '#{CAPISTRANO_LOCATION}/configuration.rb:',
+        '#{CAPISTRANO_LOCATION}/dsl/env.rb:',
+        '/dsl.rb:',
+        '/forwardable.rb:'
+      ].freeze
+      private_constant :CAPISTRANO_LOCATION, :IGNORED_LOCATIONS
+    
+        context 'and a comment after the last element' do
+      let(:b_comment) { ' # comment b' }
+    
+    module RuboCop
+  module AST
+    # A node extension for `case` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `case` nodes within RuboCop.
+    class CaseNode < Node
+      include ConditionalNode
+    
+          # The body of the method definition.
+      #
+      # @note this can be either a `begin` node, if the method body contains
+      #       multiple expressions, or any other node, if it contains a single
+      #       expression.
+      #
+      # @return [Node] the body of the method definition
+      def body
+        node_parts[0]
+      end
+    
+    module RuboCop
+  module AST
+    # A node extension for `hash` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `hash` nodes within RuboCop.
+    class HashNode < Node
+      # Returns an array of all the key value pairs in the `hash` literal.
+      #
+      # @return [Array<PairNode>] an array of `pair` nodes
+      def pairs
+        each_pair.to_a
+      end
