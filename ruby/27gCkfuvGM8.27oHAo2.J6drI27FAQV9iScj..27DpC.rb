5ba6fba6f058472d9e3dd0c820614d8310c45c8b
SUNsung <weatherius@gmail.com>
2018-11-30 01:22:57 +0200

    Rewrite [27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb] File in: 2018-11-30 01:22:56

--- ruby/27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb
+++ ruby/27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb
@@ -1,196 +1,27 @@
 
         
-                default_connectors = {
-          words_connector: ', ',
-          two_words_connector: ' and ',
-          last_word_connector: ', and '
-        }
-        if defined?(I18n)
-          i18n_connectors = I18n.translate(:'support.array', locale: options[:locale], default: {})
-          default_connectors.merge!(i18n_connectors)
-        end
-        options = default_connectors.merge!(options)
-    
-              def sanitized_object_name
-            @sanitized_object_name ||= @object_name.gsub(/\]\[|[^-a-zA-Z0-9:.]/, '_').sub(/_$/, '')
-          end
-    
-            class RadioButtonBuilder < Builder # :nodoc:
-          def radio_button(extra_html_options = {})
-            html_options = extra_html_options.merge(@input_html_options)
-            html_options[:skip_default_ids] = false
-            @template_object.radio_button(@object_name, @method_name, @value, html_options)
-          end
-        end
-    
-              def select_type
-            self.class.select_type
-          end
-    
-            private
-    
-        initializer 'action_view.caching' do |app|
-      ActiveSupport.on_load(:action_view) do
-        if app.config.action_view.cache_template_loading.nil?
-          ActionView::Resolver.caching = app.config.cache_classes
-        end
-      end
-    end
-    
-        # Render but returns a valid Rack body. If fibers are defined, we return
-    # a streaming body that renders the template piece by piece.
-    #
-    # Note that partials are not supported to be rendered with streaming,
-    # so in such cases, we just wrap them in an array.
-    def render_body(context, options)
-      if options.key?(:partial)
-        [render_partial(context, options)]
-      else
-        StreamingTemplateRenderer.new(@lookup_context).render(context, options)
-      end
-    end
-    
-        @statuses = @account.statuses.permitted_for(@account, signed_request_account)
-    @statuses = params[:min_id].present? ? @statuses.paginate_by_min_id(LIMIT, params[:min_id]).reverse : @statuses.paginate_by_max_id(LIMIT, params[:max_id])
-    @statuses = cache_collection(@statuses, Status)
-  end
-    
-        def show
-      authorize @user, :change_email?
-    end
-    
-        def new
-      authorize :email_domain_block, :create?
-      @email_domain_block = EmailDomainBlock.new
-    end
-    
-        def resubscribe
-      authorize :instance, :resubscribe?
-      params.require(:by_domain)
-      Pubsubhubbub::SubscribeWorker.push_bulk(subscribeable_accounts.pluck(:id))
-      redirect_to admin_instances_path
-    end
-    
-        def form_status_batch_params
-      params.require(:form_status_batch).permit(status_ids: [])
-    end
-    
-    module Rex
-module Proto
-module Http
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncKdcResponse from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-              # Decodes the msg_type from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_msg_type(input)
-            input.value[0].value.to_i
-          end
-    
-        desc 'Run the functional specs'
-    task :functional, [:spec] => 'fixture_tarballs:unpack' do |_t, args|
-      args.with_defaults(:spec => '**/*')
-      sh 'bundle exec bacon #{specs('functional/#{args[:spec]}')}'
-    end
-    
-      def ruby_prefix
-    RbConfig::CONFIG['prefix']
-  end
-    
-        pod 'Alamofire', path: '../Alamofire Example/Alamofire'
-    
-        # Checks that the git version is at least 1.8.5
-    #
-    # @raise If the git version is older than 1.8.5
-    #
-    # @return [void]
-    #
-    def self.verify_minimum_git_version!
-      if git_version < Gem::Version.new('1.8.5')
-        raise Informative, 'You need at least git version 1.8.5 to use CocoaPods'
-      end
-    end
-    
-            # Prints the list of specs & pod cache dirs for a single pod name.
-        #
-        # This output is valid YAML so it can be parsed with 3rd party tools
-        #
-        # @param [Array<Hash>] cache_descriptors
-        #        The various infos about a pod cache. Keys are
-        #        :spec_file, :version, :release and :slug
-        #
-        def print_pod_cache_infos(pod_name, cache_descriptors)
-          UI.puts '#{pod_name}:'
-          cache_descriptors.each do |desc|
-            if @short_output
-              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
-            end
-            UI.puts('  - Version: #{desc[:version]}')
-            UI.puts('    Type:    #{pod_type(desc)}')
-            UI.puts('    Spec:    #{desc[:spec_file]}')
-            UI.puts('    Pod:     #{desc[:slug]}')
-          end
-        end
-      end
-    end
+          def run_vagrant_command(command)
+    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
+    return [stdout, stderr] if status.success?
+    raise VagrantSSHCommandError, status
   end
 end
-
     
-      gem.required_ruby_version = '>= 2.0'
-  gem.add_dependency 'airbrussh', '>= 1.0.0'
-  gem.add_dependency 'i18n'
-  gem.add_dependency 'rake', '>= 10.0.0'
-  gem.add_dependency 'sshkit', '>= 1.9.0'
+          OptionParser.new do |opts|
+        opts.banner = 'See full documentation at http://capistranorb.com/.'
+        opts.separator ''
+        opts.separator 'Install capistrano in a project:'
+        opts.separator '    bundle exec cap install [STAGES=qa,staging,production,...]'
+        opts.separator ''
+        opts.separator 'Show available tasks:'
+        opts.separator '    bundle exec cap -T'
+        opts.separator ''
+        opts.separator 'Invoke (or simulate invoking) a task:'
+        opts.separator '    bundle exec cap [--dry-run] STAGE TASK'
+        opts.separator ''
+        opts.separator 'Advanced options:'
     
-      def test_file_exists(path)
-    exists?('f', path)
-  end
-    
-      def execute
-    signal_deprecation_warning_for_pack
-    
-        puts('Unpacking #{package_file}')
-    
-        it 'should raise configuration error when provided with too many spaces' do
-      expect {
-        Class.new(LogStash::Filters::Base) do
-          include LogStash::Config::Mixin
-          config_name 'test'
-          milestone 1
-          config :size_file, :validate => :bytes
-        end.new({'size_file' => '10  kib'})
-      }.to raise_error(LogStash::ConfigurationError)
-    end
-  end
-    
-        context 'when is not a system pipeline' do
-      it 'returns false if the pipeline is not a system pipeline' do
-        expect(subject.system?).to be_falsey
+            expect(cop.highlights).to eq([close])
+        expect(cop.messages)
+          .to eq([described_class::SAME_LINE_MESSAGE])
       end
-    end
-  end
-end
-
-    
-          options = {:debug => ENV['LS_QA_DEBUG']}
-      puts 'Destroying #{machines}'
-      LogStash::VagrantHelpers.destroy(machines, options)
-      puts 'Bootstrapping #{machines}'
-      LogStash::VagrantHelpers.bootstrap(machines, options)
-    end
-    
-        context 'update all the plugins' do
-      it 'has executed successfully' do
-        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
-        expect(logstash).to have_installed?(plugin_name, '0.1.1')
-      end
-    end
-  end
-end
