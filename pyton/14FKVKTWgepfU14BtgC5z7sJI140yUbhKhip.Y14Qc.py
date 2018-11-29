ad15d6f1400d0bb1f1433d9188165c4969db8e2f
SUNsung <weatherius@gmail.com>
2018-11-29 19:18:50 +0200

    Rewrite [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2018-11-29 19:18:49

--- pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -1,152 +1,55 @@
 
         
-          Args:
-    output: the LSTM outputs - a tensor with shape
-    [num_paths, output_dim, max_path_len]
-    seq_lengths: the sequences length per instance
+            Args:
+      z_t_bxu: sample to compute likelihood for at time t.
+      z_tm1_bxu (optional): sample condition probability of z_t upon.
     
-        total_cost = total_recon_cost = total_kl_cost = 0.0
-    # normalizing by batch done in distributions.py
-    epoch_size = len(collected_op_values)
-    for op_values in collected_op_values:
-      total_cost += op_values[0]
-      total_recon_cost += op_values[1]
-      total_kl_cost += op_values[2]
+      n_to_plot = 10
+  scale = 1.0
+  nrows = 7
+  plt.subplot(nrows,2,1+subplot_cidx)
     
-    import matplotlib
-matplotlib.use('Agg')
-from matplotlib import pyplot as plt
-import numpy as np
-import tensorflow as tf
+    # pick which RNN is used on each trial
+rnn_to_use = rng.randint(2, size=E)
+ext_input = np.repeat(np.expand_dims(rnn_to_use, axis=1), ntimesteps, axis=1)
+ext_input = np.expand_dims(ext_input, axis=2)  # these are 'a's' in the paper
     
-        if len(free_ids) < 5:
-      raise ValueError('Not enough free char ids: %d' % len(free_ids))
+    import os
+import h5py
+import json
     
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-    from six.moves import xrange
-    
-        with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+      if sentence.find('<S>') != 0:
+    sentence = '<S> ' + sentence
     
     
-def test_update(client, auth, app):
-    auth.login()
-    assert client.get('/1/update').status_code == 200
-    client.post('/1/update', data={'title': 'updated', 'body': ''})
+def wasserstein_discriminator_loss(real_values, fake_values):
+  '''Wasserstein discriminator loss.
+    
+    # Dependency imports
+    
+      real_labels = tf.ones([FLAGS.batch_size, FLAGS.sequence_length])
+  dis_loss_real = tf.losses.sigmoid_cross_entropy(
+      real_labels, real_predictions, weights=missing)
+  dis_loss_fake = tf.losses.sigmoid_cross_entropy(
+      targets_present, fake_predictions, weights=missing)
     
     
-def _find_app():
-    top = _app_ctx_stack.top
-    if top is None:
-        raise RuntimeError(_app_ctx_err_msg)
-    return top.app
-    
-        if head[:2] in (codecs.BOM_UTF16_BE, codecs.BOM_UTF16_LE):
-        return 'utf-16'
-    
-        def check(self, value):
-        '''Check if the given value should be tagged by this tag.'''
-        raise NotImplementedError
-    
-    import logging
-import sys
-    
-    
-def _render(template, context, app):
-    '''Renders the template and fires the signal'''
-    
-        By default this will get the strings from the blns.txt file
-    
-    
-class Signature(jose.Signature):
-    '''ACME-specific Signature. Uses ACME-specific Header for customer fields.'''
-    __slots__ = jose.Signature._orig_slots  # pylint: disable=no-member
-    
-    csr = OpenSSL.crypto.load_certificate_request(
-    OpenSSL.crypto.FILETYPE_ASN1, pkg_resources.resource_string(
-        'acme', os.path.join('testdata', 'csr.der')))
-try:
-    acme.request_issuance(jose.util.ComparableX509(csr), (authzr,))
-except messages.Error as error:
-    print ('This script is doomed to fail as no authorization '
-           'challenges are ever solved. Error from server: {0}'.format(error))
-
-    
-    REWRITE_HTTPS_ARGS_WITH_END = [
-    '^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[END,NE,R=permanent]']
-'''Apache version >= 2.3.9 rewrite rule arguments used for redirections to
-    https vhost'''
-    
-            self.assertRaises(errors.PluginError, self.config.save)
-    
-            self.assertTrue(self.addr_default.conflicts(self.addr))
-        self.assertTrue(self.addr_default.conflicts(self.addr1))
-        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
-    
-        def test_2nd_am_station_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1380'
-        self.assertEqual(station, expected_station)
-    
-    *Where is the pattern used practically?
-    
-        def get_objects(self):
-        '''Get all objects'''
-        return self._objects
-    
-    
-class Controller(object):
-    def __init__(self, blackboard):
-        self.blackboard = blackboard
-    
-        def find_all_path(self, start, end, path=None):
-        path = path or []
-        path.append(start)
-        if start == end:
-            return [path]
-        paths = []
-        for node in self.graph.get(start, []):
-            if node not in path:
-                newpaths = self.find_all_path(node, end, path[:])
-                paths.extend(newpaths)
-        return paths
-    
-    import time
-from threading import Thread
-from ycm.client.base_request import BaseRequest
-    
-    
-def FormatDebugInfoResponse_Completer_ServerRunningWithHost_test():
-  response = deepcopy( GENERIC_RESPONSE )
-  assert_that(
-    FormatDebugInfoResponse( response ),
-    contains_string(
-      'Completer name completer debug information:\n'
-      '  Server name running at: http://127.0.0.1:1234\n'
-      '  Server name process ID: 12345\n'
-      '  Server name executable: /path/to/executable\n'
-      '  Server name logfiles:\n'
-      '    /path/to/stdout/logfile\n'
-      '    /path/to/stderr/logfile\n'
-      '  Server name key: value\n'
-      '  Key: value\n'
-    )
-  )
-    
-      # Other non-False responses mean the same thing
-  assert_that( _HandlePollResponse( '', None ), equal_to( True ) )
-  assert_that( _HandlePollResponse( 1, None ), equal_to( True ) )
-  assert_that( _HandlePollResponse( {}, None ), equal_to( True ) )
-    
-      return request
-    
-    
-def _assert_rejects( filter, text ):
-  _assert_accept_equals( filter, text, False )
-    
-    from ycm.tests.test_utils import MockVimModule
-MockVimModule()
+def create_dis_pretrain_op(hparams, dis_loss, global_step):
+  '''Create a train op for pretraining.'''
+  with tf.name_scope('pretrain_generator'):
+    optimizer = tf.train.AdamOptimizer(hparams.dis_pretrain_learning_rate)
+    dis_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('dis')
+    ]
+    if FLAGS.dis_update_share_embedding and FLAGS.dis_share_embedding:
+      shared_embedding = [
+          v for v in tf.trainable_variables()
+          if v.op.name == 'gen/decoder/rnn/embedding'
+      ][0]
+      dis_vars.append(shared_embedding)
+    dis_grads = tf.gradients(dis_loss, dis_vars)
+    dis_grads_clipped, _ = tf.clip_by_global_norm(dis_grads,
+                                                  FLAGS.grad_clipping)
+    dis_pretrain_op = optimizer.apply_gradients(
+        zip(dis_grads_clipped, dis_vars), global_step=global_step)
+    return dis_pretrain_op
