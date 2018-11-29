867647efd343d25e0419c83d498764efe16f2a8c
SUNsung <weatherius@gmail.com>
2018-11-29 07:31:19 +0200

    Rewrite [30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb] File in: 2018-11-29 07:31:18

--- ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
+++ ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
@@ -1,73 +1,274 @@
 
         
-          it 'sets the tainted bit' do
-    o = Object.new
-    o.taint
-    o.tainted?.should == true
-  end
-    
-      it 'transfers control to the innermost catch block waiting for the same sympol' do
-    one = two = three = 0
-    catch :duplicate do
-      catch :duplicate do
-        catch :duplicate do
-          one = 1
-          throw :duplicate
+              if class_ref
+        if class_ref.respond_to?(:run)
+          # Action is available, now execute it
+          return self.execute_action(method_sym, class_ref, arguments, custom_dir: custom_dir, from_action: from_action)
+        else
+          UI.user_error!('Action '#{method_sym}' of class '#{class_name}' was found, but has no `run` method.')
         end
-        two = 2
-        throw :duplicate
       end
-      three = 3
-      throw :duplicate
+    
+        private_constant :GA_TRACKING
+    attr_accessor :session_id
+    attr_accessor :client
+    
+            return_value = run_active_command
+    
+          it 'adds docset_bundle_name param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            docset_bundle_name: 'Bundle name'
+          )
+        end').runner.execute(:test)
+    
+          it 'logs the command if verbose' do
+        with_verbose(true) do
+          allow(Fastlane::Actions).to receive(:sh).with(anything, { log: true }).and_return('')
+          result = Fastlane::FastFile.new.parse('lane :test do
+            git_add(path: 'foo.bar')
+          end').runner.execute(:test)
+        end
+      end
+    
+          it 'generates the correct git command with a shell-escaped message' do
+        message = 'message with 'quotes' (and parens)'
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: './fastlane/README.md', message: \'#{message}\')
+        end').runner.execute(:test)
+        expect(result).to eq('git commit -m #{message.shellescape} ./fastlane/README.md')
+      end
     end
-    [one, two, three].should == [1, 2, 3]
+  end
+end
+
+    
+          it 'works with exclude regex' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+              exclude_regex: /Test/
+            )
+          end').runner.execute(:test)
+    
+        it 'recognizes an array as the first element of a command' do
+      message = 'A message'
+      command = command_from_args(['/usr/local/bin/git', 'git'], 'commit', '-m', message)
+      expect(command).to eq('/usr/local/bin/git commit -m #{message.shellescape}')
+    end
+  end
+end
+    
+        def help_default_value
+      return '#{self.default_value} *'.strip if self.default_value_dynamic
+      return '' if self.default_value.nil?
+      return '''' if self.default_value.instance_of?(String) && self.default_value.empty?
+      return ':#{self.default_value}' if self.default_value.instance_of?(Symbol)
+    
+            FastlaneCore::CertChecker.installed_identies
+      end
+    end
+    
+            allow(FastlaneCore::Helper).to receive(:backticks).with(allowed_command, print: FastlaneCore::Globals.verbose?)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_command, print: FastlaneCore::Globals.verbose?)
+    
+            def preload_pipeline_warnings
+          # This preloads the number of warnings for every pipeline, ensuring
+          # that Ci::Pipeline#has_warnings? doesn't execute any additional
+          # queries.
+          @pipeline.number_of_warnings
+        end
+    
+            def sidekiq_worker_class
+          ImportIssueWorker
+        end
+    
+            def sidekiq_worker_class
+          ImportLfsObjectWorker
+        end
+    
+          def check
+        create_event :payload => {}
+      end
+    end
+    
+        it 'outputs control links to agents within the incoming set, but not outside it' do
+      agents(:jane_rain_notifier_agent).control_targets = [agents(:jane_weather_agent), agents(:jane_basecamp_agent)]
+      agents(:jane_rain_notifier_agent).save!
+    
+        it 'for the afternoon' do
+      expect(@scheduler.send(:hour_to_schedule_name, 17)).to eq('5pm')
+    end
   end
     
-      it 'writes each array element on a line when passes an array' do
-    lambda {
-      $VERBOSE = true
-      warn(['line 1', 'line 2'])
-    }.should output(nil, 'line 1\nline 2\n')
+        it 'should raise an exception when encountering complex JSONPaths' do
+      @agent.options['username_path'] = '$.very.complex[*]'
+      expect { LiquidMigrator.convert_all_agent_options(@agent) }.
+        to raise_error('JSONPath '$.very.complex[*]' is too complex, please check your migration.')
+    end
+    
+        it 'interpolates jsonpath expressions between matching <>'s' do
+      expect(Utils.interpolate_jsonpaths('hello <$.there.world> this <escape works>', payload)).to eq('hello WORLD this should+work')
+    end
+    
+    group :test do
+  gem 'omniauth-facebook'
+  gem 'omniauth-openid'
+  gem 'webrat', '0.7.3', require: false
+  gem 'mocha', '~> 1.1', require: false
+end
+    
+        if successfully_sent?(resource)
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
   end
     
-        s = StringIO.new
-    SystemConfig.dump_verbose_config s
-    # Dummy summary file, asciibetically first, to control display title of gist
-    files['# #{f.name} - #{timestamp}.txt'] = { content: brief_build_info(f) }
-    files['00.config.out'] = { content: s.string }
-    files['00.doctor.out'] = { content: Utils.popen_read('#{HOMEBREW_PREFIX}/bin/brew', 'doctor', err: :out) }
-    unless f.core_formula?
-      tap = <<~EOS
-        Formula: #{f.name}
-        Tap: #{f.tap}
-        Path: #{f.path}
-      EOS
-      files['00.tap.out'] = { content: tap }
+    class Devise::UnlocksController < DeviseController
+  prepend_before_action :require_no_authentication
+    
+        def unlock_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :unlock_instructions, opts)
     end
     
-          clear_cache
-      super(new_user, new_repo)
+    gemfile(true) do
+  source 'https://rubygems.org'
+  # Activate the gem you are reporting the issue against.
+  gem 'rails', '~> 4.2.0'
+  gem 'devise', '~> 4.0'
+  gem 'sqlite3'
+  gem 'byebug'
+end
     
-        def assets_path
-      @assets_path ||= File.join gem_path, 'assets'
+          @sign_out_via = options[:sign_out_via] || Devise.sign_out_via
+      @format = options[:format]
+    
+        SCHEME_RGX = /\A[^:\/?#]+:/
+    
+        def name=(value)
+      @name = value.try :strip
     end
     
-      find_files = ->(path) {
-    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
-      path if File.file?(path)
-    end.compact
-  }
+        def initialize
+      @pages = {}
+    end
     
-      # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+        def error?
+      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
+    end
     
-    Gem::Specification.new do |gem|
-  gem.name          = 'capistrano'
-  gem.version       = Capistrano::VERSION
-  gem.authors       = ['Tom Clements', 'Lee Hambley']
-  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
-  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
-  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
-  gem.homepage      = 'http://capistranorb.com/'
+          def get_type
+        if slug.start_with?('guide/')
+          'Guide'
+        elsif slug.start_with?('cookbook/')
+          'Cookbook'
+        elsif slug == 'glossary'
+          'Guide'
+        else
+          type = at_css('.nav-title.is-selected').content.strip
+          type.remove! ' Reference'
+          type << ': #{mod}' if mod
+          type
+        end
+      end
     
-          attr_reader :key, :default, :options
+            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
+          node.before(node.children).remove
+        end
+    
+        # Creates/updates an entry object and returns the resulting entry.
+    #
+    # If the entry was new (no UUID), then the UUID will be set on the
+    # resulting entry and can be used. Additionally, the a lock will
+    # be created for the resulting entry, so you must {#release} it
+    # if you want others to be able to access it.
+    #
+    # If the entry isn't new (has a UUID). then this process must hold
+    # that entry's lock or else this set will fail.
+    #
+    # @param [Entry] entry
+    # @return [Entry]
+    def set(entry)
+      # Get the struct and update the updated_at attribute
+      struct = entry.to_json_struct
+    
+              # Go through each VM and yield it!
+          vms.each do |old_vm|
+            # We get a new VM from the environment here to avoid potentially
+            # stale VMs (if there was a config reload on the environment
+            # or something).
+            vm = @env.vms[old_vm.name]
+            yield vm
+          end
+        end
+    
+            # This is the method called to 'prepare' the provisioner. This is called
+        # before any actions are run by the action runner (see {Vagrant::Actions::Runner}).
+        # This can be used to setup shared folders, forward ports, etc. Whatever is
+        # necessary on a 'meta' level.
+        #
+        # No return value is expected.
+        def prepare
+        end
+    
+            # This registers a plugin. This should _NEVER_ be called by the public
+        # and should only be called from within Vagrant. Vagrant will
+        # automatically register V2 plugins when a name is set on the
+        # plugin.
+        def register(plugin)
+          if !@registered.include?(plugin)
+            @logger.info('Registered plugin: #{plugin.name}')
+            @registered << plugin
+          end
+        end
+    
+            # Registers a callback to be called when a specific action sequence
+        # is run. This allows plugin authors to hook into things like VM
+        # bootup, VM provisioning, etc.
+        #
+        # @param [String] name Name of the action.
+        # @param [Symbol] hook_name The location to hook. If this isn't
+        #   set, every middleware action is hooked.
+        # @return [Array] List of the hooks for the given action.
+        def self.action_hook(name, hook_name=nil, &block)
+          # The name is currently not used but we want it for the future.
+    
+    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
+  file_shared_path = TestApp.shared_path.join(file)
+  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
+  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
+end
+    
+        def remove(key, *values)
+      set(key, Array(fetch(key)) - values)
+    end
+    
+          ServerKey = Struct.new(:hostname, :port)
+    
+    module Capistrano
+  class Configuration
+    # Decorates a Variables object to additionally perform an optional set of
+    # user-supplied validation rules. Each rule for a given key is invoked
+    # immediately whenever `set` is called with a value for that key.
+    #
+    # If `set` is called with a callable value or a block, validation is not
+    # performed immediately. Instead, the validation rules are invoked the first
+    # time `fetch` is used to access the value.
+    #
+    # A rule is simply a block that accepts two arguments: key and value. It is
+    # up to the rule to raise an exception when it deems the value is invalid
+    # (or just print a warning).
+    #
+    # Rules can be registered using the DSL like this:
+    #
+    #   validate(:my_key) do |key, value|
+    #     # rule goes here
+    #   end
+    #
+    class ValidatedVariables < SimpleDelegator
+      include Capistrano::ProcHelpers
