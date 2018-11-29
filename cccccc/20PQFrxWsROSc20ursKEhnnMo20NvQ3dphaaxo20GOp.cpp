489b48d271a41514519d52b8d7aa197b6347e865
SUNsung <weatherius@gmail.com>
2018-11-29 16:59:54 +0200

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2018-11-29 16:59:54

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,144 +1,182 @@
 
         
-        struct SrcLoc {
-  SrcLoc()
-    : start{0,0}
-    , past{0,0}
-  {}
+        namespace google {
+namespace protobuf {
+namespace compiler {
+namespace annotation_test_util {
+namespace {
+    }
+    }
+    }
+    }
     }
     
-    int64_t VMTOC::allocTOC(int64_t target) {
-  folly::MSLGuard g{s_TOC};
-  HPHP::Address addr = m_tocvector->frontier();
-  m_tocvector->qword(target);
-  return addr - (m_tocvector->base() + INT16_MAX + 1);
+    void WriteServiceDocComment(io::Printer* printer,
+                            const ServiceDescriptor* service) {
+  printer->Print('/**\n');
+  WriteDocCommentBody(printer, service);
+  printer->Print(
+    ' * Protobuf service {@code $fullname$}\n'
+    ' */\n',
+    'fullname', EscapeJavadoc(service->full_name()));
 }
     
-    namespace HPHP {
-    }
-    
-    #ifndef incl_HPHP_CONFIG_H_
-#define incl_HPHP_CONFIG_H_
-    
-    
-    {  free(temp);
-  return ret;
+    ServiceGenerator* ImmutableGeneratorFactory::NewServiceGenerator(
+    const ServiceDescriptor* descriptor) const {
+  return new ImmutableServiceGenerator(descriptor, context_);
 }
     
-    
-    {
-    {            m_currentTime[parameter] = 0;
-        }
-        return LearnerBase::CreateCheckpoint();
+    namespace google {
+namespace protobuf {
+namespace compiler {
+namespace java {
+    }
+    }
+    }
     }
     
-        private:
-        std::unordered_set<StreamInformation> m_streamInfos;
-        bool m_epochEndReached;
-        size_t m_numWorkers;
-        size_t m_workerRank;
-        size_t m_prevMinibatchSize;
-        size_t m_maxNumSamplesToRead;
-        size_t m_maxNumSweepsToRead;
-        size_t m_truncationLength;
-        size_t m_maxErrors;
-        std::unordered_map<StreamInformation, MinibatchData> m_minibatchData;
+    #include <google/protobuf/compiler/objectivec/objectivec_extension.h>
+#include <google/protobuf/compiler/objectivec/objectivec_helpers.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/stubs/strutil.h>
+#include <google/protobuf/io/printer.h>
     
-            if (bufferSizeInBytes < (viewShape.TotalSize() * sizeof(V1ElemType)))
-            InvalidArgument('Size (%d) of the specified buffer for creating the NDArrayView is smaller than the specified view shape '%S'.',
-                            (int)bufferSizeInBytes, viewShape.AsString().c_str());
+    // Like ASSERT_EXIT, but continues on to successive tests in the
+// test case, if any:
+# define EXPECT_EXIT(statement, predicate, regex) \
+    GTEST_DEATH_TEST_(statement, predicate, regex, GTEST_NONFATAL_FAILURE_)
     
-            if (sectionOffset.size() > m_maskShape.Rank())
-            LogicError('NDMask::MaskSection: The sectionOffset dimensionality (%d) must be <= rank (%d) of 'this' mask.', (int)sectionOffset.size(), (int)m_maskShape.Rank());
     
-            static bool IsUDF(const FunctionPtr& f);
+    {}  // namespace testing
     
-            void Erase() override
-        {
-            if (IsPacked())
-            {
-                m_packedData = nullptr;
-                m_packedDataLayout = nullptr;
-                m_isPacked = false;
-            }
-            else
-                Value::Erase();
-        }
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+          typename T6, typename T7, typename T8, typename T9>
+void PrintTo(const ::std::tr1::tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9>& t,
+             ::std::ostream* os) {
+  PrintTupleTo(t, os);
+}
     
-    namespace CNTK
+    // Helper function for implementing {EXPECT|ASSERT}_PRED5.  Don't use
+// this in your code.
+template <typename Pred,
+          typename T1,
+          typename T2,
+          typename T3,
+          typename T4,
+          typename T5>
+AssertionResult AssertPred5Helper(const char* pred_text,
+                                  const char* e1,
+                                  const char* e2,
+                                  const char* e3,
+                                  const char* e4,
+                                  const char* e5,
+                                  Pred pred,
+                                  const T1& v1,
+                                  const T2& v2,
+                                  const T3& v3,
+                                  const T4& v4,
+                                  const T5& v5) {
+  if (pred(v1, v2, v3, v4, v5)) return AssertionSuccess();
+    }
+    
+      // Creates directories so that path exists. Returns true if successful or if
+  // the directories already exist; returns false if unable to create
+  // directories for any reason. Will also return false if the FilePath does
+  // not represent a directory (that is, it doesn't end with a path separator).
+  bool CreateDirectoriesRecursively() const;
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22, typename T23, typename T24, typename T25,
+    typename T26, typename T27, typename T28, typename T29, typename T30,
+    typename T31, typename T32, typename T33, typename T34, typename T35,
+    typename T36, typename T37, typename T38, typename T39, typename T40,
+    typename T41, typename T42, typename T43, typename T44, typename T45,
+    typename T46>
+class ValueArray46 {
+ public:
+  ValueArray46(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+      T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
+      T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
+      T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
+      T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41,
+      T42 v42, T43 v43, T44 v44, T45 v45, T46 v46) : v1_(v1), v2_(v2), v3_(v3),
+      v4_(v4), v5_(v5), v6_(v6), v7_(v7), v8_(v8), v9_(v9), v10_(v10),
+      v11_(v11), v12_(v12), v13_(v13), v14_(v14), v15_(v15), v16_(v16),
+      v17_(v17), v18_(v18), v19_(v19), v20_(v20), v21_(v21), v22_(v22),
+      v23_(v23), v24_(v24), v25_(v25), v26_(v26), v27_(v27), v28_(v28),
+      v29_(v29), v30_(v30), v31_(v31), v32_(v32), v33_(v33), v34_(v34),
+      v35_(v35), v36_(v36), v37_(v37), v38_(v38), v39_(v39), v40_(v40),
+      v41_(v41), v42_(v42), v43_(v43), v44_(v44), v45_(v45), v46_(v46) {}
+    }
+    
+    // A sample program demonstrating using Google C++ testing framework.
+//
+// Author: wan@google.com (Zhanyong Wan)
+    
+            NDShape m_shape;
+        VariableKind m_varKind;
+        ::CNTK::DataType m_dataType;
+        std::weak_ptr<Function> m_ownerFunction;
+        std::unique_ptr<std::once_flag> m_initValueFlag;
+        NDArrayViewPtr m_value;
+        std::unique_ptr<ParameterInitializer> m_valueInitializer;
+        std::unique_ptr<DeviceDescriptor> m_valueInitializationDevice;
+        bool m_needsGradient;
+        std::wstring m_name;
+        std::vector<Axis> m_dynamicAxes;
+        bool m_isSparse;
+        std::wstring m_uid;
+        std::atomic<size_t> m_valueTimeStamp;
+        Variable m_blockFunctionVariableMapping;
+    
+    // GetLabelMapping - Gets the label mapping from integer index to label type
+// returns - a map from numeric datatype to native label type
+const std::map<typename DataReader::LabelIdType, typename DataReader::LabelType>& DataReader::GetLabelMapping(const std::wstring&)
 {
-    Variable::Variable(const FunctionPtr& function)
-        : Variable(function->Output())
-    {
-    }
-    }
-    
-            VariableFields(const NDShape& shape, VariableKind varType, ::CNTK::DataType type, const std::weak_ptr<Function>& ownerFunction, const NDArrayViewPtr& value, bool needsGradient, const std::vector<Axis>& dynamicAxes, bool isSparse, const std::wstring& name, const std::wstring& uid)
-            : m_shape(shape), m_varKind(varType), m_dataType(type), m_ownerFunction(ownerFunction), m_value(value), m_needsGradient(needsGradient), m_dynamicAxes(dynamicAxes), m_isSparse(isSparse), m_name(name), m_uid(uid), m_valueTimeStamp(0)
-        {
-            if (value && (type != value->GetDataType()))
-                InvalidArgument('The DataType of the Parameter/Constant Variable '%S' does not match the DataType of the associated Value', AsString().c_str());
-    }
-    
-    
-    {    std::string GetCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
-};
-    
-        // return the randomized feature bounds for a time range
-    std::pair<size_t, size_t> Bounds(size_t ts, size_t te) const
-    {
-        size_t tbegin = max(ts, randomizationrange / 2) - randomizationrange / 2;
-        size_t tend = min(te + randomizationrange / 2, map.size());
-        return std::make_pair<size_t, size_t>(std::move(tbegin), move(tend));
-    }
-    
-        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange& fr) override
-    {
-        Matrix<ElemType> sliceInput1Value = Input(1)->ValueFor(fr);
-        Matrix<ElemType> sliceOutputValue = ValueFor(fr);
-    }
-    
-    
-    {/**
- * @brief Start a file carve of the given paths
- *
- * @return A status returning if the carves were started successfully
- */
-Status carvePaths(const std::set<std::string>& paths);
-} // namespace osquery
-
-    
-    #pragma once
-    
-    REGISTER_INTERNAL(LoggerConfigParserPlugin, 'config_parser', 'logger');
-    
-      // We use a restricted scope below to change the data structure from
-  // an array to a set. This lets us do deletes much more efficiently
-  std::vector<std::string> created_views;
-  std::set<std::string> erase_views;
-  {
-    std::vector<std::string> old_views_vec;
-    scanDatabaseKeys(kQueries, old_views_vec, kConfigViews);
-    for (const auto& view : old_views_vec) {
-      erase_views.insert(view.substr(kConfigViews.size()));
-    }
-  }
-    
-    
-    {  ASSERT_FALSE(fpack.getPlatform().empty());
-  ASSERT_FALSE(fpack.getVersion().empty());
-  ASSERT_EQ(fpack.getShard(), 1U);
+    NOT_IMPLEMENTED;
 }
     
+    private:
+    // -----------------------------------------------------------------------
+    // the cloning operation itself
+    // -----------------------------------------------------------------------
     
-    {    // Code should never reach this point
-    VLOG(1) << 'Could not start extension process: ' << exec_path;
-    Initializer::shutdown(EXIT_FAILURE);
-    return std::shared_ptr<PlatformProcess>();
+        virtual void /*ComputationNodeBase::*/ Validate(bool isFinalValidationPass) override
+    {
+        Base::Validate(isFinalValidationPass);
+        InferMBLayoutFromInputsForStandardCase(isFinalValidationPass);
+    }
+    
+    #include <osquery/config.h>
+    
+    #include <osquery/config.h>
+#include <osquery/database.h>
+#include <osquery/registry.h>
+    
+      std::shared_ptr<PlatformProcess> process;
+  int process_pid = ::fork();
+  if (process_pid == 0) {
+    // Start a Python script
+    ::execlp('sh', 'sh', '-c', argv.c_str(), nullptr);
+    ::exit(0);
+  } else if (process_pid > 0) {
+    process.reset(new PlatformProcess(process_pid));
   }
     
-      // This class is the SAME, there was no type override.
-  auto another_basic_pub = std::make_shared<AnotherBasicEventPublisher>();
-  another_basic_pub->setName(basic_pub->getName());
-  status = EventFactory::registerEventPublisher(another_basic_pub);
-  EXPECT_FALSE(status.ok());
+    void Initializer::platformSetup() {
+  // Initialize the COM libraries utilized by Windows WMI calls.
+  auto ret = ::CoInitializeEx(0, COINIT_MULTITHREADED);
+  if (ret != S_OK) {
+    ::CoUninitialize();
+  }
+}
+    
+    Status Distributed::pullUpdates() {
+  auto distributed_plugin = RegistryFactory::get().getActive('distributed');
+  if (!RegistryFactory::get().exists('distributed', distributed_plugin)) {
+    return Status(1, 'Missing distributed plugin: ' + distributed_plugin);
+  }
+    }
