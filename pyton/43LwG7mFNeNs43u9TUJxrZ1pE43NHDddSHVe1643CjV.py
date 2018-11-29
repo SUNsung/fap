a8896be27935a0d105c61466e508d3ce9b9de126
SUNsung <weatherius@gmail.com>
2018-11-29 11:11:11 +0200

    Rewrite [43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py] File in: 2018-11-29 11:11:10

--- pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
+++ pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
@@ -1,155 +1,69 @@
 
         
-            # Then we cluster the lines together as blocks
-    # Each block represents a collection of lines that should be sorted
-    # This was done by assuming only links ([...](...)) are meant to be sorted
-    # Clustering is done by indentation
-    blocks = []
-    last_indent = None
-    for line in read_me:
-        s_line = line.lstrip()
-        indent = len(line) - len(s_line)
+            def escalate_call(self):
+        raise NotImplemented('Directors must be able to handle any call')
     
-        def __eq__(self, other):
-        return (
-            isinstance(other, self.__class__) and
-            self.keys == other.keys and
-            self.messages == other.messages and
-            self.strict == other.strict
-        )
-    
-        def save(self, must_create=False):
-        '''
-        Save the current session data to the database. If 'must_create' is
-        True, raise a database error if the saving operation doesn't create a
-        new entry (as opposed to possibly updating an existing entry).
-        '''
-        if self.session_key is None:
-            return self.create()
-        data = self._get_session(no_load=must_create)
-        obj = self.create_model_instance(data)
-        using = router.db_for_write(self.model, instance=obj)
-        try:
-            with transaction.atomic(using=using):
-                obj.save(force_insert=must_create, force_update=not must_create, using=using)
-        except IntegrityError:
-            if must_create:
-                raise CreateError
-            raise
-        except DatabaseError:
-            if not must_create:
-                raise UpdateError
-            raise
-    
-        def create(self):
-        '''
-        To create a new key, set the modified flag so that the cookie is set
-        on the client for the current request.
-        '''
-        self.modified = True
-    
-        def _router_func(action):
-        def _route_db(self, model, **hints):
-            chosen_db = None
-            for router in self.routers:
-                try:
-                    method = getattr(router, action)
-                except AttributeError:
-                    # If the router doesn't have a method, skip to the next one.
-                    pass
-                else:
-                    chosen_db = method(model, **hints)
-                    if chosen_db:
-                        return chosen_db
-            instance = hints.get('instance')
-            if instance is not None and instance._state.db:
-                return instance._state.db
-            return DEFAULT_DB_ALIAS
-        return _route_db
-    
-        def setUp(self):
-        from acme.errors import BadNonce
-        self.error = BadNonce(nonce='xxx', error='error')
+        def __init__(self, num_levels):
+        self.num_levels = num_levels
+        self.levels = []  # List of Levels
     
     
-class Header(jose.Header):
-    '''ACME-specific JOSE Header. Implements nonce, kid, and url.
+class Cache(object):
+    
+    
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/update',
+    '/1/delete',
+))
+def test_login_required(client, path):
+    response = client.post(path)
+    assert response.headers['Location'] == 'http://localhost/auth/login'
+    
+    PY2 = sys.version_info[0] == 2
+_identity = lambda x: x
+    
+    from .helpers import _PackageBoundObject, _endpoint_from_view_func
+    
+            If you want to open the :file:`schema.sql` file you would do the
+        following::
+    
+        If you configure your own :class:`logging.StreamHandler`, you may want to
+    use this for the stream. If you are using file or dict configuration and
+    can't import this directly, you can refer to it as
+    ``ext://flask.logging.wsgi_errors_stream``.
     '''
-    nonce = jose.Field('nonce', omitempty=True, encoder=jose.encode_b64jose)
-    kid = jose.Field('kid', omitempty=True)
-    url = jose.Field('url', omitempty=True)
+    return request.environ['wsgi.errors'] if request else sys.stderr
     
-        :returns: Dict of Define:Value pairs
-    :rtype: `dict`
     
-            return False
-    
-        def test_bad_parse(self):
-        # pylint: disable=protected-access
-        self.config.parser.parse_file(os.path.join(
-            self.config.parser.root, 'conf-available', 'bad_conf_file.conf'))
-        self.assertRaises(
-            errors.PluginError, self.config.check_parsing_errors, 'httpd.aug')
-    
-        @certbot_util.patch_get_utility()
-    def test_select_correct(self, mock_util):
-        mock_util().checklist.return_value = (
-            display_util.OK, [self.vhosts[3].display_repr(),
-                              self.vhosts[2].display_repr()])
-        vhs = select_vhost_multiple([self.vhosts[3],
-                                     self.vhosts[2],
-                                     self.vhosts[1]])
-        self.assertTrue(self.vhosts[2] in vhs)
-        self.assertTrue(self.vhosts[3] in vhs)
-        self.assertFalse(self.vhosts[1] in vhs)
-    
-            self.sni._mod_config()  # pylint: disable=protected-access
-        self.sni.configurator.save()
-    
-        :param train_dir: directory that contains a sub-directory for each known person, with its name.
-    
-        # You can access the actual face itself like this:
-    face_image = image[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
-    pil_image.show()
-
-    
-            # If a match was found in known_face_encodings, just use the first one.
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
-    
-    # Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-    
+class SecureCookieSessionInterface(SessionInterface):
+    '''The default session interface that stores sessions in signed cookies
+    through the :mod:`itsdangerous` module.
     '''
-http://code.activestate.com/recipes/413838-memento-closure/
+    #: the salt that should be applied on top of the secret key for the
+    #: signing of cookie based sessions.
+    salt = 'cookie-session'
+    #: the hash function to use for the signature.  The default is sha1
+    digest_method = staticmethod(hashlib.sha1)
+    #: the name of the itsdangerous supported key derivation.  The default
+    #: is hmac.
+    key_derivation = 'hmac'
+    #: A python serializer for the payload.  The default is a compact
+    #: JSON derived serializer with support for some extra Python types
+    #: such as datetime objects or tuples.
+    serializer = session_json_serializer
+    session_class = SecureCookieSession
     
-    ### OUTPUT ###
-# jim got cartoon
-# jack got music
-# gee got movie
-# jim got cartoon
-# jim got cartoon
-# gee got movie
-
     
-        def scan(self):
-        self.state.scan()
+class RC4FileObject(object):
+    '''fileobj for rc4'''
+    def __init__(self, stream, key):
+        self.__stream = stream
+        self.__cipher = _Crypto_Cipher_ARC4_new(key) if key else lambda x:x
+    def __getattr__(self, attr):
+        if attr not in ('__stream', '__cipher'):
+            return getattr(self.__stream, attr)
+    def read(self, size=-1):
+        return self.__cipher.encrypt(self.__stream.read(size))
     
-    class TimeDisplay(object):
-    
-        def contribute(self):
-        self.blackboard.common_state['problems'] += random.randint(10, 20)
-        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
-        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
-        self.blackboard.common_state['progress'] += random.randint(10, 30)
-    
-        def find_path(self, start, end, path=None):
-        path = path or []
-    
-        def _send_diagnostics_pass_report(self):
-        return 'send diagnostics pass report'
+    import urllib
