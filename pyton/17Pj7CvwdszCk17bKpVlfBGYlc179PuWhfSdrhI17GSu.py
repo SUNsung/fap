3f0da83f7585175d3305ba225a0e24f2c4c2a711
SUNsung <weatherius@gmail.com>
2018-11-29 14:30:19 +0200

    Rewrite [17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py] File in: 2018-11-29 14:30:18

--- pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
+++ pyton/17Pj7CvwdszCk17bKpVlfBGYlc179PuWhfSdrhI17GSu.py
@@ -1,54 +1,187 @@
 
         
-        DOCUMENTATION = '''
-module: aws_waf_facts
-short_description: Retrieve facts for WAF ACLs, Rule , Conditions and Filters.
-description:
-  - Retrieve facts for WAF ACLs, Rule , Conditions and Filters.
-version_added: '2.4'
-requirements: [ boto3 ]
-options:
-  name:
-    description:
-      - The name of a Web Application Firewall
+            log_var_init = np.log(var_init)
+    if var_max > var_min:
+      var_is_trainable = True
+    else:
+      var_is_trainable = False
     
-    # Get summary information about a stack
-- cloudformation_facts:
-    stack_name: my-cloudformation-stack
+        if hps.output_dist == 'poisson':
+      out_dist_params = np.zeros([E_to_process, T, D])
+    elif hps.output_dist == 'gaussian':
+      out_dist_params = np.zeros([E_to_process, T, D+D])
+    else:
+      assert False, 'NIY'
     
-            # Identify targets that need to be added or updated on AWS
-        targets_to_put = self._targets_to_put()
-        if targets_to_put:
-            self.rule.put_targets(targets_to_put)
+      input_title = ''
+  if 'controller_outputs' in model_vals.keys():
+    input_title += ' Controller Output'
+    plt.subplot(nrows,2,3+subplot_cidx)
+    u_t = model_vals['controller_outputs'][0:-1]
+    plot_time_series(u_t, bidx, n_to_plot=n_to_plot, color='c', scale=1.0,
+                     title=col_title + input_title)
+    
+    # not the best way to do this but E is small enough
+rates = []
+spikes = []
+for trial in xrange(E):
+  if rnn_to_use[trial] == 0:
+    rates.append(rates_a[trial])
+    spikes.append(spikes_a[trial])
+  else:
+    rates.append(rates_b[trial])
+    spikes.append(spikes_b[trial])
+    
+    _START_SPECIAL_CHARS = ['.', ',', '?', '!', ';', ':', '[', ']', '\'', '+', '/',
+                        '\xc2\xa3', '$', '~', '*', '%', '{', '}', '#', '&', '-',
+                        ''', '(', ')', '='] + list(_SPECIAL_CHAR_MAP.keys())
+_SPECIAL_CHARS = _START_SPECIAL_CHARS + [
+    '\'s', '\'m', '\'t', '\'re', '\'d', '\'ve', '\'ll']
+    
+    '''Losses for Generator and Discriminator.'''
     
     
-# Find the most recent snapshot
-def _get_snapshot_starttime(snap):
-    return datetime.datetime.strptime(snap.start_time, '%Y-%m-%dT%H:%M:%S.000Z')
+def convert_to_indices(sequences):
+  '''Convert a list of size [batch_size, sequence_length, vocab_size] to
+  a list of size [batch_size, sequence_length] where the vocab element is
+  denoted by the index.
+  '''
+  batch_of_indices = []
     
-                        if original_policy:
-                        original_policy = sort_json_policy_dict(original_policy)
+      Args:
+    logits:  tf.float32 Tensor of the logits of shape [batch_size,
+      sequence_length, vocab_size].
+    targets:  tf.int32 Tensor of the sequence target of shape [batch_size,
+      sequence_length].
+    present:  tf.bool Tensor indicating the presence or absence of the token
+      of shape [batch_size, sequence_length].
     
-        def describe_services(self, cluster, services):
-        fn_args = dict()
-        if cluster and cluster is not None:
-            fn_args['cluster'] = cluster
-        fn_args['services'] = services
+    ESTIMATORS = {
+    'dummy': DummyClassifier(),
+    'random_forest': RandomForestClassifier(n_estimators=100,
+                                            max_features='sqrt',
+                                            min_samples_split=10),
+    'extra_trees': ExtraTreesClassifier(n_estimators=100,
+                                        max_features='sqrt',
+                                        min_samples_split=10),
+    'logistic_regression': LogisticRegression(),
+    'naive_bayes': MultinomialNB(),
+    'adaboost': AdaBoostClassifier(n_estimators=10),
+}
+    
+    
+def bench(factory, X, Y, X_test, Y_test, ref_coef):
+    gc.collect()
+    
+        for n_samples in sample_sizes:
+        X = random_state.rand(n_samples, 300)
+    
+        max_it = len(samples_range) * len(features_range)
+    for n_samples in samples_range:
+        for n_features in features_range:
+            it += 1
+            print('====================')
+            print('Iteration %03d of %03d' % (it, max_it))
+            print('====================')
+            X = make_low_rank_matrix(n_samples, n_features,
+                                  effective_rank=rank,
+                                  tail_strength=0.2)
+    
+    import gc
+    
+        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
+                                   'blob/{revision}/{package}/'
+                                   '{path}#L{lineno}')
+    '''
+    revision = _get_git_revision()
+    return partial(_linkcode_resolve, revision=revision, package=package,
+                   url_fmt=url_fmt)
+
+    
+    
+if not os.path.exists(DATA_FOLDER):
+    
+        def test_with(self):
+        p = self.cls(BASE)
+        it = p.iterdir()
+        it2 = p.iterdir()
+        next(it2)
+        with p:
+            pass
+        # I/O operation on closed path
+        self.assertRaises(ValueError, next, it)
+        self.assertRaises(ValueError, next, it2)
+        self.assertRaises(ValueError, p.open)
+        self.assertRaises(ValueError, p.resolve)
+        self.assertRaises(ValueError, p.absolute)
+        self.assertRaises(ValueError, p.__enter__)
+    
+    from argparse import ArgumentParser
+    
+        counter = 1
+    for part in msg.walk():
+        # multipart/* are just containers
+        if part.get_content_maintype() == 'multipart':
+            continue
+        # Applications should really sanitize the given filename so that an
+        # email message can't be used to overwrite important files
+        filename = part.get_filename()
+        if not filename:
+            ext = mimetypes.guess_extension(part.get_content_type())
+            if not ext:
+                # Use a generic bag-of-bits extension
+                ext = '.bin'
+            filename = 'part-%03d%s' % (counter, ext)
+        counter += 1
+        with open(os.path.join(args.directory, filename), 'wb') as fp:
+            fp.write(part.get_payload(decode=True))
+    
+    import homeassistant.helpers.config_validation as cv
+from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
+from homeassistant.util import Throttle
+from homeassistant.components.device_tracker import (
+    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.helpers.aiohttp_client import async_create_clientsession
+    
+        def get_thomson_data(self):
+        '''Retrieve data from THOMSON and return parsed result.'''
         try:
-            response = self.describe_services_with_backoff(**fn_args)
-        except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-            self.module.fail_json_aws(e, msg='Couldn't describe ECS services')
-        running_services = [self.extract_service_from(service) for service in response.get('services', [])]
-        services_not_running = response.get('failures', [])
-        return running_services, services_not_running
+            telnet = telnetlib.Telnet(self.host)
+            telnet.read_until(b'Username : ')
+            telnet.write((self.username + '\r\n').encode('ascii'))
+            telnet.read_until(b'Password : ')
+            telnet.write((self.password + '\r\n').encode('ascii'))
+            telnet.read_until(b'=>')
+            telnet.write(('hostmgr list\r\n').encode('ascii'))
+            devices_result = telnet.read_until(b'=>').split(b'\r\n')
+            telnet.write('exit\r\n'.encode('ascii'))
+        except EOFError:
+            _LOGGER.exception('Unexpected response from router')
+            return
+        except ConnectionRefusedError:
+            _LOGGER.exception(
+                'Connection refused by router. Telnet enabled?')
+            return
     
-        changed = False
-    try:
-        cert_action(module, iam, name, path, new_name, new_path, state,
-                    cert, key, cert_chain, orig_cert_names, orig_cert_bodies, dup_ok)
-    except boto.exception.BotoServerError as err:
-        module.fail_json(changed=changed, msg=str(err), debug=[cert, key])
+                    if subdir:
+                    subdir = sanitize_filename(subdir)
     
+        def startup(self, event):
+        '''Start the watcher.'''
+        self._observer.start()
     
-def IsInitializerList(clean_lines, linenum):
-  '''Check if current line is inside constructor initializer list.
+    '''
+The number of partitions of a number n into at least k parts equals the number of partitions into exactly k parts
+plus the number of partitions into at least k-1 parts. Subtracting 1 from each part of a partition of n into k parts
+gives a partition of n-k into k parts. These two facts together are used for this algorithm.
+'''
+def partition(m):
+	memo = [[0 for _ in xrange(m)] for _ in xrange(m+1)]
+	for i in xrange(m+1):
+		memo[i][0] = 1
+    
+        difference = predict - actual
+    square_diff = np.square(difference)
+    mean_square_diff = square_diff.mean()
+    score = np.sqrt(mean_square_diff)
+    return score
