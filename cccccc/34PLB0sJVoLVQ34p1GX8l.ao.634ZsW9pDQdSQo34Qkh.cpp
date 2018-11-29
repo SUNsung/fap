9127090ae82a66187ffc9c07f5118ed853300b7a
SUNsung <weatherius@gmail.com>
2018-11-29 22:24:11 +0200

    Rewrite [34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp] File in: 2018-11-29 22:24:10

--- cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
+++ cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
@@ -1,94 +1,283 @@
 
         
-            void operator() (const typename VecTraits<u32>::vec128 & v_src0,
-                     const typename VecTraits<u32>::vec128 & v_src1,
-                     typename VecTraits<u32>::vec128 & v_dst) const
-    {
-        float32x4_t vs1 = vcvtq_f32_u32(v_src0);
-        float32x4_t vs2 = vcvtq_f32_u32(v_src1);
-    }
+          explicit tuple(GTEST_BY_REF_(T0) f0, GTEST_BY_REF_(T1) f1,
+      GTEST_BY_REF_(T2) f2, GTEST_BY_REF_(T3) f3, GTEST_BY_REF_(T4) f4,
+      GTEST_BY_REF_(T5) f5, GTEST_BY_REF_(T6) f6, GTEST_BY_REF_(T7) f7,
+      GTEST_BY_REF_(T8) f8, GTEST_BY_REF_(T9) f9) : f0_(f0), f1_(f1), f2_(f2),
+      f3_(f3), f4_(f4), f5_(f5), f6_(f6), f7_(f7), f8_(f8), f9_(f9) {}
     
+    // This provides interface PrimeTable that determines whether a number is a
+// prime and determines a next prime number. This interface is used
+// in Google Test samples demonstrating use of parameterized tests.
     
-    {} //namespace
-#endif
+      // Gets the 0-terminated C string this MyString object represents.
+  const char* c_string() const { return c_string_; }
     
-                uint16x8_t v_mask0 = vorrq_u16(vceqq_s16(v_src0, v_maxval8), vceqq_s16(v_src0, v_minval8));
-            uint16x8_t v_mask1 = vorrq_u16(vceqq_s16(v_src1, v_maxval8), vceqq_s16(v_src1, v_minval8));
+    public:
+	virtual ~ConstraintBullet() {
+		bulletdelete(constraint);
+		constraint = NULL;
+	}
     
-                tnext4OldOldOld = tnext4Old;
-            tnext5OldOldOld = tnext5Old;
-            tnext1Old = tnext1OldOld;
-            tnext2Old = tnext2OldOld;
-            tnext3Old = tnext3OldOld;
-            tnext4Old = tnext4OldOld;
-            tnext5Old = tnext5OldOld;
-    
-    ConstraintBullet::ConstraintBullet() :
-		space(NULL),
-		constraint(NULL),
-		disabled_collisions_between_bodies(true) {}
-    
-    bool GodotCollisionDispatcher::needsResponse(const btCollisionObject *body0, const btCollisionObject *body1) {
+    bool GodotCollisionDispatcher::needsCollision(const btCollisionObject *body0, const btCollisionObject *body1) {
 	if (body0->getUserIndex() == CASTED_TYPE_AREA || body1->getUserIndex() == CASTED_TYPE_AREA) {
 		// Avoide area narrow phase
 		return false;
 	}
-	return btCollisionDispatcher::needsResponse(body0, body1);
+	return btCollisionDispatcher::needsCollision(body0, body1);
+}
+    
+    #include 'joint_bullet.h'
+    
+    #include 'csg_gizmos.h'
+#include 'csg_shape.h'
+    
+    
+    {	memdelete(resource_loader_dds);
 }
 
     
-    	const btDiscreteDynamicsWorld *m_world;
-	btPersistentManifold *m_manifoldPtr;
-	bool m_ownManifold;
-	bool m_isSwapped;
+    #include 'dvector.h'
     
-    public:
-	Error pck_start(const String &p_file, int p_alignment);
-	Error add_file(const String &p_file, const String &p_src);
-	Error flush(bool p_verbose = false);
-    
-    Shell::Shell() {
+    class PCKPacker : public Reference {
     }
     
-    void compareBenchmarkResults(const std::string& base, const std::string& test) {
-  printResultComparison(resultsFromFile(base), resultsFromFile(test));
+    Shell::~Shell() {
 }
     
-    exception_wrapper::exception_wrapper(std::exception_ptr ptr) noexcept
-    : exception_wrapper{} {
-  if (ptr) {
-    if (auto e = get_std_exception_(ptr)) {
-      LOG(DFATAL)
-          << 'Performance error: Please construct exception_wrapper with a '
-             'reference to the std::exception along with the '
-             'std::exception_ptr.';
-      *this = exception_wrapper{std::move(ptr), *e};
-    } else {
-      Unknown uk;
-      *this = exception_wrapper{ptr, uk};
+    	if (!streqi(cmderStart.c_str(), L''))
+	{
+		swprintf_s(args, L'%s /dir \'%s\'', args, cmderStart.c_str());
+	}
+    
+    /*!
+* \brief Maximum ndim supported for special operators like broadcasting with non contiguous lhs/rhs
+*/
+#define MXNET_SPECIAL_MAX_NDIM 5
+    
+        std::string type = param_.prototxt.type();
+    caffe_data_layer_ = caffe::LayerRegistry<Dtype>::CreateLayer(param_.prototxt);
+    CHECK(caffe_data_layer_ != nullptr) << 'Failed creating caffe data layer';
+    const size_t top_size = param_.prototxt.top_size();
+    if (top_size > 0) {
+      if (top_size > NR_SUPPORTED_TOP_ITEMS) {
+        LOG(WARNING)
+          << 'Too may \'top\' items, only two (one data, one label) are currently supported';
+      }
+      top_.reserve(top_size);
+      for (size_t x = 0; x < top_size; ++x) {
+        ::caffe::Blob<Dtype> *blob = new ::caffe::Blob<Dtype>();
+        cleanup_blobs_.push_back(std::unique_ptr<::caffe::Blob<Dtype>>(blob));
+        top_.push_back(blob);
+      }
+      caffe_data_layer_->SetUp(bottom_, top_);
+      const std::vector<int> &shape = top_[DATA]->shape();
+      const size_t shapeDimCount = shape.size();
+      if (shapeDimCount > 0) {
+        batch_size_ = shape[0];
+        if (shapeDimCount > 1) {
+          channels_ = shape[1];
+          if (shapeDimCount > 2) {
+            width_ = shape[2];
+            if (shapeDimCount > 3) {
+              height_ = shape[3];
+            }
+          }
+        }
+      }
     }
+    
+    /*!
+ * Copyright (c) 2016 by Contributors
+ * \file caffe_fieldentry.h
+ * \brief Implement FieldEntry<caffe::LayerParameter>
+ * \author Haoran Wang
+ */
+#ifndef PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
+#define PLUGIN_CAFFE_CAFFE_FIELDENTRY_H_
+    
+    template<>
+Operator* CreateOp<cpu>(CaffeOpParam param, int dtype) {
+  Operator *op = NULL;
+  switch (dtype) {
+  case mshadow::kFloat32:
+    op = new CaffeOp<cpu, float>(param);
+    break;
+  case mshadow::kFloat64:
+    op = new CaffeOp<cpu, double>(param);
+    break;
+  case mshadow::kFloat16:
+    LOG(FATAL) << 'float16 layer is not supported by caffe';
+    break;
+  default:
+    LOG(FATAL) << 'Unsupported type ' << dtype;
   }
+  return op;
 }
     
-    template <class UIntType, size_t w, size_t s, size_t r>
-struct StateSize<std::subtract_with_carry_engine<UIntType, w, s, r>> {
-  // [rand.eng.sub]: r * ceil(w / 32)
-  using type = std::integral_constant<size_t, r*((w + 31) / 32)>;
-};
+    /*!
+ * \brief Check if alignas(*) keyword is supported. (g++ 4.8 or higher)
+ */
+#if defined(__GNUC__) && ((__GNUC__ == 4 && __GNUC_MINOR__ >= 8) || __GNUC__ > 4)
+#define XGBOOST_ALIGNAS(X) alignas(X)
+#else
+#define XGBOOST_ALIGNAS(X)
+#endif
     
-      /**
-   * Returns a secure random uint64_t
+    namespace xgboost {
+namespace gbm {
+/*! \brief model parameters */
+struct GBTreeModelParam : public dmlc::Parameter<GBTreeModelParam> {
+  /*! \brief number of trees */
+  int num_trees;
+  /*! \brief number of roots */
+  int num_roots;
+  /*! \brief number of features to be used by trees */
+  int num_feature;
+  /*! \brief pad this space, for backward compatibility reason.*/
+  int pad_32bit;
+  /*! \brief deprecated padding space. */
+  int64_t num_pbuffer_deprecated;
+  /*!
+   * \brief how many output group a single instance can produce
+   *  this affects the behavior of number of output we have:
+   *    suppose we have n instance and k group, output will be k * n
    */
-  static uint64_t secureRand64() {
-    return secureRandom<uint64_t>();
+  int num_output_group;
+  /*! \brief size of leaf vector needed in tree */
+  int size_leaf_vector;
+  /*! \brief reserved parameters */
+  int reserved[32];
+  /*! \brief constructor */
+  GBTreeModelParam() {
+    std::memset(this, 0, sizeof(GBTreeModelParam));
+    static_assert(sizeof(GBTreeModelParam) == (4 + 2 + 2 + 32) * sizeof(int),
+                  '64/32 bit compatibility issue');
   }
+  // declare parameters, only declare those that need to be set.
+  DMLC_DECLARE_PARAMETER(GBTreeModelParam) {
+    DMLC_DECLARE_FIELD(num_output_group)
+        .set_lower_bound(1)
+        .set_default(1)
+        .describe(
+            'Number of output groups to be predicted,'
+            ' used for multi-class classification.');
+    DMLC_DECLARE_FIELD(num_roots).set_lower_bound(1).set_default(1).describe(
+        'Tree updater sequence.');
+    DMLC_DECLARE_FIELD(num_feature)
+        .set_lower_bound(0)
+        .describe('Number of features used for training and prediction.');
+    DMLC_DECLARE_FIELD(size_leaf_vector)
+        .set_lower_bound(0)
+        .set_default(0)
+        .describe('Reserved option for vector tree.');
+  }
+};
+    }
+    }
     
-      explicit Options(
-      Format format_ = Format::ZLIB,
-      int windowSize_ = 15,
-      int memLevel_ = 8,
-      int strategy_ = Z_DEFAULT_STRATEGY)
-      : format(format_),
-        windowSize(windowSize_),
-        memLevel(memLevel_),
-        strategy(strategy_) {}
+    void DHTReplaceNodeTask::onTimeout(const std::shared_ptr<DHTNode>& node)
+{
+  ++numRetry_;
+  if (numRetry_ >= MAX_RETRY) {
+    A2_LOG_INFO(fmt('ReplaceNode: Ping failed %d times. Replace %s with %s.',
+                    numRetry_, node->toString().c_str(),
+                    newNode_->toString().c_str()));
+    node->markBad();
+    bucket_->addNode(newNode_);
+    setFinished(true);
+  }
+  else {
+    A2_LOG_INFO(fmt('ReplaceNode: Ping reply timeout from %s. Try once more.',
+                    node->toString().c_str()));
+    sendMessage();
+  }
+}
+    
+      virtual void startup() CXX11_OVERRIDE;
+    
+    bool DHTRoutingTable::addNode(const std::shared_ptr<DHTNode>& node, bool good)
+{
+  A2_LOG_DEBUG(fmt('Trying to add node:%s', node->toString().c_str()));
+  if (*localNode_ == *node) {
+    A2_LOG_DEBUG('Adding node with the same ID with localnode is not allowed.');
+    return false;
+  }
+  auto treeNode = dht::findTreeNodeFor(root_.get(), node->getID());
+  while (1) {
+    const std::shared_ptr<DHTBucket>& bucket = treeNode->getBucket();
+    if (bucket->addNode(node)) {
+      A2_LOG_DEBUG('Added DHTNode.');
+      return true;
+    }
+    else if (bucket->splitAllowed()) {
+      A2_LOG_DEBUG(fmt('Splitting bucket. Range:%s-%s',
+                       util::toHex(bucket->getMinID(), DHT_ID_LENGTH).c_str(),
+                       util::toHex(bucket->getMaxID(), DHT_ID_LENGTH).c_str()));
+      treeNode->split();
+      ++numBucket_;
+      if (treeNode->getLeft()->isInRange(node->getID())) {
+        treeNode = treeNode->getLeft();
+      }
+      else {
+        treeNode = treeNode->getRight();
+      }
+    }
+    else {
+      if (good) {
+        bucket->cacheNode(node);
+        A2_LOG_DEBUG(fmt('Cached node=%s', node->toString().c_str()));
+      }
+      return false;
+    }
+  }
+  return false;
+}
+    
+      std::shared_ptr<DHTNode> localNode_;
+    
+    void DHTTaskExecutor::update()
+{
+  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
+                                  std::mem_fn(&DHTTask::finished)),
+                   execTasks_.end());
+  int r;
+  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
+    r = numConcurrent_ - execTasks_.size();
+  }
+  else {
+    r = 0;
+  }
+  while (r && !queue_.empty()) {
+    std::shared_ptr<DHTTask> task = queue_.front();
+    queue_.pop_front();
+    task->startup();
+    if (!task->finished()) {
+      execTasks_.push_back(task);
+      --r;
+    }
+  }
+  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
+                   static_cast<unsigned int>(getExecutingTaskSize()),
+                   static_cast<unsigned int>(getQueueSize())));
+}
+    
+    DHTTaskQueueImpl::~DHTTaskQueueImpl() = default;
+    
+    #include 'DHTTaskQueue.h'
+#include 'DHTTaskExecutor.h'
+    
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port) const
+{
+  return generateToken(infoHash, ipaddr, port, secret_[0]);
+}
+    
+    namespace aria2 {
+    }
+    
+      // do nothing; we don't use this message as outgoing message.
+  virtual bool send() CXX11_OVERRIDE;
+    
+    DNSCache::CacheEntry::~CacheEntry() = default;
