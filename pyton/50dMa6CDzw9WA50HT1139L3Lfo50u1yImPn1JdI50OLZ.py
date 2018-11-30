d6b2723d114be85003343b7846ac6ebc02847fbf
SUNsung <weatherius@gmail.com>
2018-11-30 03:44:31 +0200

    Rewrite [50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py] File in: 2018-11-30 03:44:31

--- pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
+++ pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
@@ -1,64 +1,144 @@
 
         
-        from youtube_dl.compat import (
-    compat_basestring,
-    compat_input,
-    compat_getpass,
-    compat_print,
-    compat_urllib_request,
-)
-from youtube_dl.utils import (
-    make_HTTPS_handler,
-    sanitized_Request,
-)
-    
-    sys.path.insert(0, dirn(dirn((os.path.abspath(__file__)))))
-import youtube_dl
-from youtube_dl.utils import shell_quote
+        versions_info['signature'] = signature
+with open('update/versions.json', 'w') as versionsf:
+    json.dump(versions_info, versionsf, indent=4, sort_keys=True)
+
     
     
-iv = key = [0x20, 0x15] + 14 * [0]
-    
-    import io
-import optparse
-    
-    
-class TestAgeRestriction(unittest.TestCase):
-    def _assert_restricted(self, url, filename, age, old_age=None):
-        self.assertTrue(_download_restricted(url, filename, old_age))
-        self.assertFalse(_download_restricted(url, filename, age))
+def main():
+    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
+        template = tmplf.read()
     
     
-def issue_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    ref = 'https://github.com/scrapy/scrapy/issues/' + text
-    set_classes(options)
-    node = nodes.reference(rawtext, 'issue ' + text, refuri=ref, **options)
-    return [node], []
+def filter_options(readme):
+    ret = ''
+    in_options = False
+    for line in readme.split('\n'):
+        if line.startswith('# '):
+            if line[2:].startswith('OPTIONS'):
+                in_options = True
+            else:
+                in_options = False
     
-            self.crawler_process.crawl(spname, **opts.spargs)
-        self.crawler_process.start()
+        if not releases:
+        break
     
-        def _list_templates(self):
-        print('Available templates:')
-        for filename in sorted(os.listdir(self.templates_dir)):
-            if filename.endswith('.tmpl'):
-                print('  %s' % splitext(filename)[0])
+    # The suffix of source filenames.
+source_suffix = '.rst'
     
-        def syntax(self):
-        return '[options] <url>'
     
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('--get', dest='get', metavar='SETTING',
-            help='print raw setting value')
-        parser.add_option('--getbool', dest='getbool', metavar='SETTING',
-            help='print setting value, interpreted as a boolean')
-        parser.add_option('--getint', dest='getint', metavar='SETTING',
-            help='print setting value, interpreted as an integer')
-        parser.add_option('--getfloat', dest='getfloat', metavar='SETTING',
-            help='print setting value, interpreted as a float')
-        parser.add_option('--getlist', dest='getlist', metavar='SETTING',
-            help='print setting value, interpreted as a list')
+def _is_empty(d):
+    return not bool(os.listdir(d))
     
-        def short_desc(self):
-        return 'Print Scrapy version'
+        # close and remove the temporary database
+    os.close(db_fd)
+    os.unlink(db_path)
+    
+            :param name: the optional name of the filter, otherwise the
+                     function name will be used.
+        '''
+        def decorator(f):
+            self.add_app_template_filter(f, name=name)
+            return f
+        return decorator
+    
+                app.config['IMAGE_STORE_TYPE'] = 'fs'
+            app.config['IMAGE_STORE_PATH'] = '/var/app/images'
+            app.config['IMAGE_STORE_BASE_URL'] = 'http://img.website.com'
+            image_store_config = app.config.get_namespace('IMAGE_STORE_')
+    
+        def get_cookie_secure(self, app):
+        '''Returns True if the cookie should be secure.  This currently
+        just returns the value of the ``SESSION_COOKIE_SECURE`` setting.
+        '''
+        return app.config['SESSION_COOKIE_SECURE']
+    
+        def state_forwards(self, app_label, state):
+        pass
+    
+    KEY_PREFIX = 'django.contrib.sessions.cached_db'
+    
+    
+@x_robots_tag
+def index(request, sitemaps,
+          template_name='sitemap_index.xml', content_type='application/xml',
+          sitemap_url_name='django.contrib.sitemaps.views.sitemap'):
+    
+            try:
+            create_file(filename)
+            self.assertIs(self.pathmodule.isfile(filename), True)
+            self.assertIs(self.pathmodule.isfile(bfilename), True)
+        finally:
+            support.unlink(filename)
+    
+            self.assertEqual(posixpath.join(b'/foo', b'bar', b'/bar', b'baz'),
+                         b'/bar/baz')
+        self.assertEqual(posixpath.join(b'/foo', b'bar', b'baz'),
+                         b'/foo/bar/baz')
+        self.assertEqual(posixpath.join(b'/foo/', b'bar/', b'baz/'),
+                         b'/foo/bar/baz/')
+    
+        def run_xdev(self, *args, check_exitcode=True, xdev=True):
+        env = dict(os.environ)
+        env.pop('PYTHONWARNINGS', None)
+        env.pop('PYTHONDEVMODE', None)
+        env.pop('PYTHONMALLOC', None)
+    
+        def testReadMultiStream(self):
+        self.createTempFile(streams=5)
+        with BZ2File(self.filename) as bz2f:
+            self.assertRaises(TypeError, bz2f.read, float())
+            self.assertEqual(bz2f.read(), self.TEXT * 5)
+    
+    # Now add the related image to the html part.
+with open('roasted-asparagus.jpg', 'rb') as img:
+    msg.get_payload()[1].add_related(img.read(), 'image', 'jpeg',
+                                     cid=asparagus_cid)
+    
+    
+def main():
+    parser = ArgumentParser(description='''\
+Send the contents of a directory as a MIME message.
+Unless the -o option is given, the email is sent by forwarding to your local
+SMTP server, which then does the normal delivery process.  Your local machine
+must be running an SMTP server.
+''')
+    parser.add_argument('-d', '--directory',
+                        help='''Mail the contents of the specified directory,
+                        otherwise use the current directory.  Only the regular
+                        files in the directory are sent, and we don't recurse to
+                        subdirectories.''')
+    parser.add_argument('-o', '--output',
+                        metavar='FILE',
+                        help='''Print the composed message to FILE instead of
+                        sending the message to the SMTP server.''')
+    parser.add_argument('-s', '--sender', required=True,
+                        help='The value of the From: header (required)')
+    parser.add_argument('-r', '--recipient', required=True,
+                        action='append', metavar='RECIPIENT',
+                        default=[], dest='recipients',
+                        help='A To: header value (at least one required)')
+    args = parser.parse_args()
+    directory = args.directory
+    if not directory:
+        directory = '.'
+    # Create the message
+    msg = EmailMessage()
+    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
+    msg['To'] = ', '.join(args.recipients)
+    msg['From'] = args.sender
+    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+    
+    # If we want to print a preview of the message content, we can extract whatever
+# the least formatted payload is and print the first three lines.  Of course,
+# if the message has no plain text part printing the first three lines of html
+# is probably useless, but this is just a conceptual example.
+simplest = msg.get_body(preferencelist=('plain', 'html'))
+print()
+print(''.join(simplest.get_content().splitlines(keepends=True)[:3]))
+    
+    ##
+    
+    def noop(x):
+    pass
