9a482a12bbc1d58306b920e940d5628a68d5584e
SUNsung <weatherius@gmail.com>
2018-11-30 20:37:58 +0200

    Rewrite [44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb] File in: 2018-11-30 20:37:58

--- ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
+++ ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
@@ -1,93 +1,50 @@
 
         
-        Nullam luctus fermentum est id blandit. Phasellus consectetur ullamcorper
-ligula, {% if author == 'Jane Doe' %} at finibus eros laoreet id. {% else %}
-Etiam sit amet est in libero efficitur.{% endif %}
-tristique. Ut nec magna augue. Quisque ut fringilla lacus, ac dictum enim.
-Aliquam vel ornare mauris. Suspendisse ornare diam tempor nulla facilisis
-aliquet. Sed ultrices placerat ultricies.
-LIQUID
-    
-    # No trailing slash
-Benchmark.ips do |x|
-  path = '/some/very/very/long/path/to/a/file/i/like/'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
-end
-
-    
-    DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
-    
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-          def after_background(_background)
-        @in_background = nil
-      end
-    
-    Gem::Specification.new do |s|
-  s.specification_version = 2 if s.respond_to? :specification_version=
-  s.required_rubygems_version = Gem::Requirement.new('>= 0') if s.respond_to? :required_rubygems_version=
-  s.rubygems_version = '2.2.2'
-  s.required_ruby_version = '>= 2.3.0'
-    
-            # This is called as a last-minute hook that allows the configuration
-        # object to finalize itself before it will be put into use. This is
-        # a useful place to do some defaults in the case the user didn't
-        # configure something or so on.
-        #
-        # An example of where this sort of thing is used or has been used:
-        # the 'vm' configuration key uses this to make sure that at least
-        # one sub-VM has been defined: the default VM.
-        #
-        # The configuration object is expected to mutate itself.
-        def finalize!
-          # Default implementation is to do nothing.
-        end
-    
-            # This returns all registered provisioners.
-        #
-        # @return [Hash]
-        def provisioners
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.provisioner)
-            end
-          end
-        end
-    
-      def show
-    render json: collection_presenter,
-           serializer: ActivityPub::CollectionSerializer,
-           adapter: ActivityPub::Adapter,
-           content_type: 'application/activity+json',
-           skip_activities: true
+            reversible(&method(:up_down))
   end
     
-        def destroy
-      authorize @account_moderation_note, :destroy?
-      @account_moderation_note.destroy!
-      redirect_to admin_account_path(@account_moderation_note.target_account_id), notice: I18n.t('admin.account_moderation_notes.destroyed_msg')
+    Then /^I should see an image in the publisher$/ do
+  photo_in_publisher.should be_present
+end
+    
+    Given /^a user with email '([^\']*)' is tagged '([^\']*)'$/ do |email, tags|
+  user = User.find_by_email(email)
+  user.profile.tag_string = tags
+  user.profile.build_tags
+  user.profile.save!
+end
+    
+        it 'generates the aspects_manage_contacts_json fixture', fixture: true do
+      # adds one not mutual contact
+      bob.share_with(FactoryGirl.create(:person), @aspect)
+    
+    class LogStash::PluginManager::Pack < LogStash::PluginManager::PackCommand
+  option '--tgz', :flag, 'compress package as a tar.gz file', :default => !LogStash::Environment.windows?
+  option '--zip', :flag, 'compress package as a zip file', :default => LogStash::Environment.windows?
+  option '--[no-]clean', :flag, 'clean up the generated dump of plugins', :default => true
+  option '--overwrite', :flag, 'Overwrite a previously generated package file', :default => false
+    
+    class LogStash::PluginManager::Unpack < LogStash::PluginManager::PackCommand
+  option '--tgz', :flag, 'unpack a packaged tar.gz file', :default => !LogStash::Environment.windows?
+  option '--zip', :flag, 'unpack a packaged  zip file', :default => LogStash::Environment.windows?
+    
+        def user_feedback_string_for(action, platform, machines, options={})
+      experimental_string = options['experimental'] ? 'experimental' : 'non experimental'
+      message  = '#{action} all #{experimental_string} VM's defined in acceptance/Vagrantfile'
+      '#{message} for #{platform}: #{machines}' if !platform.nil?
     end
     
-          @email_domain_block = EmailDomainBlock.new(resource_params)
+              it 'successfully install the plugin when verification is disabled' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest')
+          end
     
-        private
-    
-    ```
-Executable Path: #{actual_path}
-```
-EOS
+        context 'update all the plugins' do
+      it 'has executed successfully' do
+        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
+        expect(logstash).to have_installed?(plugin_name, '0.1.1')
       end
+    end
+  end
+end
