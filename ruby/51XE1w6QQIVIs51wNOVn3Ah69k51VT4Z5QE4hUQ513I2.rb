607b710e8e57127925bbab17bf91ba32df5058c3
SUNsung <weatherius@gmail.com>
2018-11-30 18:03:29 +0200

    Rewrite [51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb] File in: 2018-11-30 18:03:28

--- ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
+++ ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
@@ -1,148 +1,102 @@
 
         
-        #############################################################################
-#
-# Standard tasks
-#
-#############################################################################
-    
-    # Just a slash
-Benchmark.ips do |x|
-  path = '/'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
+        def version
+  Jekyll::VERSION
 end
     
-    #
+            def print_message(json_message)
+          msg = JSON.parse(json_message)
+          # Not sure what the 'url' command even does in LiveReload.  The spec is silent
+          # on its purpose.
+          Jekyll.logger.info 'LiveReload:', 'Browser URL: #{msg['url']}' if msg['command'] == 'url'
+        end
     
-    require 'fileutils'
-require 'jekyll'
-require 'time'
-require 'safe_yaml/load'
-    
-        def initialize(*args)
-      if args.empty?
-        super(*Array.new(9))
-      elsif args.length == 1 && args.first.is_a?(Hash)
-        args.first.assert_valid_keys URI::Generic::COMPONENT
-        super(*args.first.values_at(*URI::Generic::COMPONENT))
-      else
-        super
+        keys.each do |key|
+      value = env[key]
+      s = '#{key}: #{value}'
+      case key
+      when 'CC', 'CXX', 'LD'
+        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
       end
+      f.puts s
     end
-    
-            css('*[layout]').remove_attr('layout')
-        css('*[layout-xs]').remove_attr('layout-xs')
-        css('*[flex]').remove_attr('flex')
-        css('*[flex-xs]').remove_attr('flex-xs')
-        css('*[ng-class]').remove_attr('ng-class')
-        css('*[align]').remove_attr('align')
-        css('h1, h2, h3').remove_attr('class')
-    
-          INDEX = Set.new
-    
-            ohai 'Removing directories if empty:'
-        each_resolved_path(:rmdir, directories) do |path, resolved_paths|
-          puts path
-          resolved_paths.select(&:directory?).each do |resolved_path|
-            if (ds_store = resolved_path.join('.DS_Store')).exist?
-              command.run!('/bin/rm', args: ['-f', '--', ds_store], sudo: true, print_stderr: false)
-            end
-    
-      def format_usage_banner(usage_banner)
-    usage_banner.sub(/^/, '### ')
   end
 end
 
     
-    # A base class for non-formula requirements needed by formulae.
-# A 'fatal' requirement is one that will fail the build if it is not present.
-# By default, Requirements are non-fatal.
-class Requirement
-  include Dependable
+      def print_dir(root)
+    dirs = []
+    remaining_root_files = []
+    other = ''
     
-    desc 'LESS to stdin -> Sass to stdout'
-task :less_to_scss, :branch do |t, args|
-  require './tasks/converter'
-  puts Converter.new(branch: args[:branch]).convert_less(STDIN.read)
-end
-    
-        def register_lotus
-      Lotus::Assets.sources << assets_path
+      def remove_duplicates
+    where = 'WHERE s1.user_id = s2.user_id AND s1.shareable_id = s2.shareable_id AND '\
+      's1.shareable_type = s2.shareable_type AND s1.id > s2.id'
+    if AppConfig.postgres?
+      execute('DELETE FROM share_visibilities AS s1 USING share_visibilities AS s2 #{where}')
+    else
+      execute('DELETE s1 FROM share_visibilities s1, share_visibilities s2 #{where}')
     end
-    
-    
-    def get_file(url)
-      uri = URI(url)
-      cache_path = './#@cache_path#{uri.path}#{uri.query.tr('?&=', '-') if uri.query}'
-      FileUtils.mkdir_p File.dirname(cache_path)
-      if File.exists?(cache_path)
-        log_http_get_file url, true
-        File.read(cache_path, mode: 'rb')
-      else
-        log_http_get_file url, false
-        content = open(url).read
-        File.open(cache_path, 'wb') { |f| f.write content }
-        content
-      end
-    end
-    
-      # Disable Rails's static asset server (Apache or nginx will already do this).
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
   end
-    
-      # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
-  end
-  config.static_cache_control = 'public, max-age=3600'
-    
-    # Declares a dependency to the git repo of CocoaPods gem. This declaration is
-# compatible with the local git repos feature of Bundler.
-#
-def cp_gem(name, repo_name, branch = 'master', path: false)
-  return gem name if SKIP_UNRELEASED_VERSIONS
-  opts = if path
-           { :path => '../#{repo_name}' }
-         else
-           url = 'https://github.com/CocoaPods/#{repo_name}.git'
-           { :git => url, :branch => branch }
-         end
-  gem name, opts
-end
-    
-        pod 'ObjCPod', path: 'ObjCPod'
-    pod 'SwiftPod', path: 'SwiftPod'
-    pod 'MixedPod', path: 'MixedPod'
-    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
-    
-            def self.options
-          [[
-            '--short', 'Only print the path relative to the cache root'
-          ]].concat(super)
-        end
-    
-    class Rack::Builder
-  include Sinatra::Delegator
 end
 
     
-          attr_reader :app, :options
+      def up_down(change)
+    change.up do
+      Mention.update_all(mentions_container_type: 'Post')
+      change_column :mentions, :mentions_container_type, :string, null: false
+      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
+    end
     
-            # Set these key values to boolean 'true' to include in policy
-        NO_ARG_DIRECTIVES.each do |d|
-          if options.key?(d) && options[d].is_a?(TrueClass)
-            directives << d.to_s.sub(/_/, '-')
-          end
-        end
+    Then /^'([^']*)' should be post (\d+)$/ do |post_text, position|
+  stream_element_numbers_content(position).should have_content(post_text)
+end
+    
+      class FetchWebfinger < Base
+    def perform(*_args)
+      # don't do real discovery in cucumber
+    end
+  end
+end
+
+    
+          it 'should remove participation' do
+        delete :destroy, params: {post_id: post.id}
+        expect(alice.participations.where(:target_id => post.id)).not_to exist
+        expect(response.code).to eq('200')
+      end
+    end
+    
+              code = InvitationCode.create(user: bob)
+    
+    # This is the version that ships with OS X 10.10, so be sure we test against it.
+# At the same time, the 1.7.7 version won't install cleanly on Ruby > 2.2,
+# so we use a fork that makes a trivial change to a macro invocation.
+gem 'json', :git => 'https://github.com/segiddins/json.git', :branch => 'seg-1.7.7-ruby-2.2'
+    
+          if specs
+        title 'Running the specs'
+        sh 'bundle exec bacon #{specs('**/*')}'
+    
+          def actual_path
+        $PROGRAM_NAME
+      end
+    end
+  end
+end
+
+    
+          it 'allows closing brace on different line from multi-line element' do
+        expect_no_offenses(construct(false, a, make_multi(multi), true))
+      end
+    
+          # Checks whether this node body is a void context.
+      #
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
+      end
+    
+            def delimiter_delta
+          return 0 if first.same_line?(second)
+          return 0 if first.delimiter != second.delimiter
