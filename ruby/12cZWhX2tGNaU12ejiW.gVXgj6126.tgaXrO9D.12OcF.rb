5fd859f934a81f0178624838ab12846c49154beb
SUNsung <weatherius@gmail.com>
2018-12-01 06:51:59 +0200

    Rewrite [12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb] File in: 2018-12-01 06:51:59

--- ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
+++ ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
@@ -1,30 +1,122 @@
 
         
-              def identity
-        @identity ||= current_user.identities
-                                  .with_extern_uid(provider, uid)
-                                  .first_or_initialize(extern_uid: uid)
-      end
+        def slugify(header)
+  header.delete('#').strip.downcase.gsub(%r!\s+!, '-')
+end
     
-              <<-SQL.strip_heredoc
-            (CASE
-              WHEN (#{builds}) = (#{skipped}) AND (#{warnings}) THEN #{STATUSES[:success]}
-              WHEN (#{builds}) = (#{skipped}) THEN #{STATUSES[:skipped]}
-              WHEN (#{builds}) = (#{success}) THEN #{STATUSES[:success]}
-              WHEN (#{builds}) = (#{created}) THEN #{STATUSES[:created]}
-              WHEN (#{builds}) = (#{success}) + (#{skipped}) THEN #{STATUSES[:success]}
-              WHEN (#{builds}) = (#{success}) + (#{skipped}) + (#{canceled}) THEN #{STATUSES[:canceled]}
-              WHEN (#{builds}) = (#{created}) + (#{skipped}) + (#{pending}) THEN #{STATUSES[:pending]}
-              WHEN (#{running}) + (#{pending}) > 0 THEN #{STATUSES[:running]}
-              WHEN (#{manual}) > 0 THEN #{STATUSES[:manual]}
-              WHEN (#{created}) > 0 THEN #{STATUSES[:running]}
-              ELSE #{STATUSES[:failed]}
-            END)
-          SQL
+    # Mimic how Jekyll's LiquidRenderer would process a non-static file, with
+# some dummy payload
+def always_liquid(content)
+  Liquid::Template.error_mode = :warn
+  Liquid::Template.parse(content, :line_numbers => true).render(
+    'author' => 'John Doe',
+    'title'  => 'FooBar'
+  )
+end
+    
+    # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+    
+          #
+    
+      p
+end
+    
+            def start(opts)
+          @thread = Thread.new do
+            # Use epoll if the kernel supports it
+            EM.epoll
+            EM.run do
+              EM.error_handler { |e| log_error(e) }
+    
+            def initialize(config)
+          @main_fallback_highlighter = config['highlighter'] || 'rouge'
+          @config = config['kramdown'] || {}
+          @highlighter = nil
+          setup
         end
+    
+      #
+  # The method being used for the request (e.g. GET).
+  #
+  attr_accessor :method
+  #
+  # The raw URI being requested, before any mucking gets to it
+  #
+  attr_accessor :raw_uri
+    
+              # Encodes the pvno field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_pvno
+            bn = OpenSSL::BN.new(pvno.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of an encrypted message.
+        class EncryptedData < Element
+          # @!attribute name_type
+          #   @return [Integer] The encryption algorithm
+          attr_accessor :etype
+          # @!attribute kvno
+          #   @return [Integer] The version number of the key
+          attr_accessor :kvno
+          # @!attribute cipher
+          #   @return [String] The enciphered text
+          attr_accessor :cipher
+    
+          if rails?
+        register_rails_engine
+      elsif lotus?
+        register_lotus
+      elsif sprockets?
+        register_sprockets
       end
     
-              relation.update_all(update)
+    When /^I (?:like|unlike) the post '([^']*)' in the stream$/ do |post_text|
+  like_stream_post(post_text)
+end
+    
+    Given /^I have (\d+) contacts$/ do |n|
+  count = n.to_i - @me.contacts.count
+    
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+            def perform_payment_action(action, *args)
+          authorize! action, Payment
+          @payment.send('#{action}!', *args)
+          respond_with(@payment, default_template: :show)
+        end
+    
+            def create
+          authorize! :create, Property
+          @property = Spree::Property.new(property_params)
+          if @property.save
+            respond_with(@property, status: 201, default_template: :show)
+          else
+            invalid_resource!(@property)
+          end
+        end
+    
+            def destroy
+          authorize! :destroy, stock_location
+          stock_location.destroy
+          respond_with(stock_location, status: 204)
+        end
+    
+            def stock_movement_params
+          params.require(:stock_movement).permit(permitted_stock_movement_attributes)
         end
       end
     end
@@ -32,23 +124,22 @@
 end
 
     
-            def key_text
-          @entity.to_s
-        end
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
     
-        file package(gem, '.tar.gz') => ['pkg/'] do |f|
-      sh <<-SH
-        git archive \
-          --prefix=#{gem}-#{source_version}/ \
-          --format=tar \
-          HEAD -- #{directory} | gzip > #{f.name}
-      SH
+        def render(context)
+      includes_dir = File.join(context.registers[:site].source, '_includes')
+    
+          expect('.border-color-alternate').to have_rule(rule)
     end
   end
     
-          def report(env)
-        warn env, 'attack reported by #{self.class}'
-        env[options[:report_key]] = true
-      end
-    
-            close_body(body) if reaction
+      context 'called with null values' do
+    it 'writes rules for other three' do
+      ruleset = 'margin-top: 11px; ' +
+                'margin-right: 12px; ' +
+                'margin-left: 13px;'
+      bad_rule = 'margin-bottom: null;'
