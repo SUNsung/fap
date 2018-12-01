f26630e809208f2f86a1c78c1af5170f8ae132b0
SUNsung <weatherius@gmail.com>
2018-12-01 06:13:42 +0200

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2018-12-01 06:13:41

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,237 +1,79 @@
 
         
-            def show_message
-      UI.message('Sending anonymous analytics information')
-      UI.message('Learn more at https://docs.fastlane.tools/#metrics')
-      UI.message('No personal or sensitive data is sent.')
-      UI.message('You can disable this by adding `opt_out_usage` at the top of your Fastfile')
-    end
+        lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'capistrano/version'
     
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            tag: '#{tag}',
-          })
-        end').runner.execute(:test)
+          def warn_set_scm_is_deprecated
+        $stderr.puts(<<-MESSAGE)
+[Deprecation Notice] `set :scm, #{scm_name.inspect}` is deprecated.
+To ensure your project is compatible with future versions of Capistrano,
+remove the :scm setting and instead add these lines to your Capfile after
+`require 'capistrano/deploy'`:
     
-          it 'bootstraps with multiple dependencies' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              command: 'bootstrap',
-              dependencies: ['TestDependency1', 'TestDependency2']
-            )
-          end').runner.execute(:test)
-    
-            expect(result[1]).to eq('security default-keychain -s ~/Library/Keychains/test.keychain')
-        expect(result[2]).to eq('security unlock-keychain -p testpassword ~/Library/Keychains/test.keychain')
-    
-          it 'handles the extensions parameter with no elements correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', extensions: [])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
+            true
       end
     
-            context 'when disabled' do
-          it 'should raise if swiftlint completes with a non-zero exit status' do
-            allow(FastlaneCore::UI).to receive(:important)
-            expect(FastlaneCore::UI).to receive(:important).with(/If you want fastlane to continue anyway/)
-            # This is simulating the exception raised if the return code is non-zero
-            expect(Fastlane::Actions).to receive(:sh).and_raise('fake error')
-            expect(FastlaneCore::UI).to receive(:user_error!).with(/SwiftLint finished with errors/).and_call_original
-    
-            allow(File).to receive(:file?).and_return(false)
-        expect(File).to receive(:file?).with('#{Dir.home}/Library/Keychains/login.keychain-db').and_return(true)
-        allow(File).to receive(:exist?).and_return(false)
-        expect(File).to receive(:exist?).with('item.path').and_return(true)
-    
-    describe JobsHelper do
-  let(:job) { Delayed::Job.new }
-    
-        it 'creates a scenario label with the given text' do
-      expect(scenario_label(scenario, 'Other')).to eq(
-        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
-      )
-    end
-  end
-    
-      it 'schould register the schedules with the rufus scheduler and run' do
-    mock(@rufus_scheduler).join
-    scheduler = HuginnScheduler.new
-    scheduler.setup!(@rufus_scheduler, Mutex.new)
-    scheduler.run
-  end
-    
-      describe 'down' do
-    let :valid_options do
-      super().merge('extract' => new_extract,
-                    'template' => new_template)
-    end
-    
-            mock(Rails.logger).warn('DEPRECATED: The .receive_webhook method is deprecated, please switch your Agent to use .receive_web_request.')
-        @agent.trigger_web_request(request)
-        expect(@agent.reload.memory['last_webhook_request']).to eq({ 'some_param' => 'some_value' })
-        expect(@agent.last_web_request_at.to_i).to be_within(1).of(Time.now.to_i)
-      end
-    end
-  end
-    
-        # This will detect the proper guest OS for the machine and set up
-    # the class to actually execute capabilities.
-    def detect!
-      guest_name = @machine.config.vm.guest
-      initialize_capabilities!(guest_name, @guests, @capabilities, @machine)
-    rescue Errors::CapabilityHostExplicitNotDetected => e
-      raise Errors::GuestExplicitNotDetected, value: e.extra_data[:value]
-    rescue Errors::CapabilityHostNotDetected
-      raise Errors::GuestNotDetected
-    end
-    
-            # This returns all the registered communicators.
-        #
-        # @return [Hash]
-        def communicators
-          result = {}
-    
-                    raise Errors::VMNoMatchError if machines.empty?
-              else
-                # String name, just look for a specific VM
-                @logger.debug('Finding machine that match name: #{name}')
-                machines << get_machine.call(name.to_sym)
-                raise Errors::VMNotFoundError, name: name if !machines[0]
-              end
-            end
-          else
-            # No name was given, so we return every VM in the order
-            # configured.
-            @logger.debug('Loading all machines...')
-            machines = @env.machine_names.map do |machine_name|
-              get_machine.call(machine_name)
-            end
-          end
-    
-      it 'raises a TypeError when passed nil' do
-    lambda { sleep(nil)   }.should raise_error(TypeError)
-  end
-    
-    module Sass
-  module CacheStores
-    # A backend for the Sass cache using the filesystem.
-    class Filesystem < Base
-      # The directory where the cached files will be stored.
-      #
-      # @return [String]
-      attr_accessor :cache_location
-    
-        # Returns a string representation of the Sass backtrace.
-    #
-    # @param default_filename [String] The filename to use for unknown files
-    # @see #sass_backtrace
-    # @return [String]
-    def sass_backtrace_str(default_filename = 'an unknown file')
-      lines = message.split('\n')
-      msg = lines[0] + lines[1..-1].
-        map {|l| '\n' + (' ' * 'Error: '.size) + l}.join
-      'Error: #{msg}' +
-        sass_backtrace.each_with_index.map do |entry, i|
-          '\n        #{i == 0 ? 'on' : 'from'} line #{entry[:line]}' +
-            ' of #{entry[:filename] || default_filename}' +
-            (entry[:mixin] ? ', in `#{entry[:mixin]}'' : '')
-        end.join
-    end
-    
-          # Polling is used by default on Windows.
-      unless Sass::Util.windows?
-        opts.on('--poll', 'Check for file changes manually, rather than relying on the OS.',
-                          'Only meaningful for --watch.') do
-          @options[:poll] = true
-        end
+          def roles_for(names)
+        options = extract_options(names)
+        s = Filter.new(:role, names).filter(servers_by_key.values)
+        s.select { |server| server.select?(options) }
       end
     
-        <div id='cookies'>
-      <h3 id='cookie-info'>COOKIES</h3>
-      <% unless req.cookies.empty? %>
-        <table class='req'>
-          <tr>
-            <th>Variable</th>
-            <th>Value</th>
-          </tr>
-          <% req.cookies.each { |key, val| %>
-            <tr>
-              <td><%=h key %></td>
-              <td class='code'><div><%=h val.inspect %></div></td>
-            </tr>
-          <% } %>
-        </table>
-      <% else %>
-        <p class='no-data'>No cookie data.</p>
-      <% end %>
-      <div class='clear'></div>
-    </div> <!-- /COOKIES -->
+    # This is basically a copy of the original bundler 'bundle' shim
+# with the addition of the loading of our Bundler patches that
+# modify Bundler's caching behaviour.
     
-            # Set these key values to boolean 'true' to include in policy
-        NO_ARG_DIRECTIVES.each do |d|
-          if options.key?(d) && options[d].is_a?(TrueClass)
-            directives << d.to_s.sub(/_/, '-')
-          end
-        end
+    require 'bootstrap/environment'
     
-            expect(new_source)
-          .to eq('#{prefix}#{open}#{a}, # a\n#{b}#{close} # b\n#{suffix}')
-      end
+        def execute
+      temp_path = generate_temporary_path
+      packet_gem = Paquet::Gem.new(temp_path, LogStash::Environment::CACHE_PATH)
     
-            expect(new_source).to eq(['#{prefix}#{open}#{a},',
-                                  '#{b}#{close}',
-                                  suffix].join($RS))
+      def validate_cache_location
+    cache_location = LogStash::Environment::CACHE_PATH
+    if File.exist?(cache_location)
+      puts('Directory #{cache_location} is going to be overwritten, do you want to continue? (Y/N)')
+      override = ( 'y' == STDIN.gets.strip.downcase ? true : false)
+      if override
+        FileUtils.rm_rf(cache_location)
+      else
+        puts('Unpack cancelled: file #{cache_location} already exists, please delete or move it')
+        exit
       end
     end
   end
 end
 
     
-          # Custom destructuring method. This can be used to normalize
-      # destructuring for different variations of the node.
-      #
-      # In this case, the `def` node destructures into:
-      #
-      #   `method_name, arguments, body`
-      #
-      # while the `defs` node destructures into:
-      #
-      #   `receiver, method_name, arguments, body`
-      #
-      # so we reverse the destructured array to get the optional receiver
-      # at the end, where it can be discarded.
-      #
-      # @return [Array] the different parts of the `def` or `defs` node
-      def node_parts
-        to_a.reverse
-      end
-    end
-  end
+      namespace :vm do
+    
+    desc 'Clean up files.'
+task :clean do |t|
+  FileUtils.rm_rf 'doc'
+  FileUtils.rm_rf 'tmp'
+  FileUtils.rm_rf 'pkg'
+  FileUtils.rm_rf 'public'
+  FileUtils.rm 'test/debug.log' rescue nil
+  FileUtils.rm 'test/paperclip.db' rescue nil
+  Dir.glob('paperclip-*.gem').each{|f| FileUtils.rm f }
 end
 
     
-    module RuboCop
-  module AST
-    # A node extension for `hash` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `hash` nodes within RuboCop.
-    class HashNode < Node
-      # Returns an array of all the key value pairs in the `hash` literal.
-      #
-      # @return [Array<PairNode>] an array of `pair` nodes
-      def pairs
-        each_pair.to_a
-      end
     
-          # Whether the last argument of the node is a block pass,
-      # i.e. `&block`.
-      #
-      # @return [Boolean] whether the last argument of the node is a block pass
-      def block_argument?
-        arguments? &&
-          (last_argument.block_pass_type? || last_argument.blockarg_type?)
-      end
-    end
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'paths'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'selectors'))
+    
+    After do
+  ORIGINAL_BUNDLE_VARS.each_pair do |key, value|
+    ENV[key] = value
   end
 end
+    
+        private
+    
+        def register_new_attachment
+      Paperclip::AttachmentRegistry.register(@klass, @name, @options)
+    end
