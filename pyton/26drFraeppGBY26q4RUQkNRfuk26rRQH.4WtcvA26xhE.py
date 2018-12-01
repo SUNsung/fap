033dd2bcc7fc1979de7e6f8d0c494e3bce1d7583
SUNsung <weatherius@gmail.com>
2018-12-01 19:09:35 +0200

    Rewrite [26drFraeppGBY26q4RUQkNRfuk26rRQH.4WtcvA26xhE.py] File in: 2018-12-01 19:09:34

--- pyton/26drFraeppGBY26q4RUQkNRfuk26rRQH.4WtcvA26xhE.py
+++ pyton/26drFraeppGBY26q4RUQkNRfuk26rRQH.4WtcvA26xhE.py
@@ -1,70 +1,38 @@
 
         
-            if z is None:
-      z = self.sample
+                for i in html_json['sources']:
+            if 'src' in i:  #to avoid KeyError
+                if i['src'].startswith('https'):
+                    link_list.append((str(i['height']), i['src']))
     
-      def call(self, h_tm1_bxn, u_bx1):
-    act_t_bxn = tf.matmul(h_tm1_bxn, self.Wh_nxn) + self.b_1xn + u_bx1 * self.Bu_1xn
-    h_t_bxn = tf.nn.tanh(act_t_bxn)
-    z_t = tf.nn.xw_plus_b(h_t_bxn, self.Wro_nxo, self.bro_o)
-    return z_t, h_t_bxn
+    from xml.etree import cElementTree as ET
+from copy import copy
+from ..common import *
+#----------------------------------------------------------------------
+def ckplayer_get_info_by_xml(ckinfo):
+    '''str->dict
+    Information for CKPlayer API content.'''
+    e = ET.XML(ckinfo)
+    video_dict = {'title': '',
+                  #'duration': 0,
+                  'links': [],
+                  'size': 0,
+                  'flashvars': '',}
+    dictified = dictify(e)['ckplayer']
+    if 'info' in dictified:
+        if '_text' in dictified['info'][0]['title'][0]:  #title
+            video_dict['title'] = dictified['info'][0]['title'][0]['_text'].strip()
     
-          x[i] = final_x
-      y[i] = final_y
+    #----------------------------------------------------------------------
+def dilidili_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
+    global headers
+    re_str = r'http://www.dilidili.com/watch\S+'
+    if re.match(r'http://www.dilidili.wang', url):
+        re_str = r'http://www.dilidili.wang/watch\S+'
+        headers['Referer'] = 'http://www.dilidili.wang/'
+    elif re.match(r'http://www.dilidili.mobi', url):
+        re_str = r'http://www.dilidili.mobi/watch\S+'
+        headers['Referer'] = 'http://www.dilidili.mobi/'
     
-          elif FLAGS.generator_model.startswith('seq2seq'):
-        load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-        print('Restoring Generator from %s.' % load_ckpt)
-        tf.logging.info('Restoring Generator from %s.' % load_ckpt)
-        gen_encoder_init_saver = init_savers['gen_encoder_init_saver']
-        gen_decoder_init_saver = init_savers['gen_decoder_init_saver']
-        gen_encoder_init_saver.restore(sess, load_ckpt)
-        gen_decoder_init_saver.restore(sess, load_ckpt)
-    
-        #Checking correctness of path
-    if not os.path.isdir(loc):
-        print('Invalid directory. Please try again!', file = sys.stderr)
-        sys.exit(1)
-    
-    
-class GreekGetter(object):
-    
-        requests *= 10000
-    client1_delegate = timeit(client1.delegate)
-    client2_delegate = timeit(client2.delegate)
-    with suppress_stdout():
-        client1_delegate(requests)
-        client2_delegate(requests)
-    # lets check which is faster
-    print(client1_delegate._time, client2_delegate._time)
-    
-    from __future__ import print_function
-    
-    print()
-    
-    
-class Subject(object):
-    def __init__(self):
-        self._observers = []
-    
-        def notify(self, msg):
-        self.msg_queue.append(msg)
-    
-    '''
-@author: Gordeev Andrey <gordeev.and.and@gmail.com>
-    
-    
-class Dog(object):
-    def speak(self):
-        return 'woof'
-    
-    
-def main():
-    dispatcher = PrototypeDispatcher()
-    prototype = Prototype()
-    
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-'''
+        if int(html['status']) != 100000:
+        raise Exception('API error!')
