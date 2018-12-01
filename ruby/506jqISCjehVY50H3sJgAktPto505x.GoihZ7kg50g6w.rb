b9aec3f7762ed5fa9dd98af1c6d8e8c35413fc12
SUNsung <weatherius@gmail.com>
2018-12-01 15:25:48 +0200

    Rewrite [506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb] File in: 2018-12-01 15:25:47

--- ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
+++ ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
@@ -1,139 +1,134 @@
 
         
-                sql = <<-SQL
-          UPDATE ci_stages SET status = (#{status_sql})
-            WHERE ci_stages.status IS NULL
-            AND ci_stages.id BETWEEN #{start_id.to_i} AND #{stop_id.to_i}
-        SQL
+        module ActionView #:nodoc:
+  # = Action View Raw Output Helper
+  module Helpers #:nodoc:
+    module OutputSafetyHelper
+      # This method outputs without escaping a string. Since escaping tags is
+      # now default, this can be used when you don't want Rails to automatically
+      # escape tags. This is not recommended if the data is coming from the user's
+      # input.
+      #
+      # For example:
+      #
+      #  raw @user.name
+      #  # => 'Jimmy <alert>Tables</alert>'
+      def raw(stringish)
+        stringish.to_s.html_safe
+      end
     
-          class MergeRequest < ActiveRecord::Base
-        self.table_name = 'merge_requests'
+    def local_require
+  require 'json'
+  JSON.pretty_generate(DATA)
+end
     
-          def path
-        safe_name.gsub(/[\s]/, '_')
+      Jekyll::Command.subclasses.each { |c| c.init_with_program(p) }
+    
+            # Setup and normalize the configuration:
+        #   * Create Kramdown if it doesn't exist.
+        #   * Set syntax_highlighter, detecting enable_coderay and merging
+        #       highlighter if none.
+        #   * Merge kramdown[coderay] into syntax_highlighter_opts stripping coderay_.
+        #   * Make sure `syntax_highlighter_opts` exists.
+    
+        def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
+    end
+  end
+end
+
+    
+        it 'opens the dry run modal even when clicking on the refresh icon' do
+      visit edit_agent_path(agent)
+      find('.agent-dry-run-button span.glyphicon').click
+      expect(page).to have_text('Event to send (Optional)')
+    end
+    
+        it 'unregisters deleted SchedulerAgents' do
+      @scheduler.schedule_scheduler_agents
+    
+        def relative_url_string?(str)
+      str !~ SCHEME_RGX && !fragment_url_string?(str) && !data_url_string?(str)
+    end
+    
+        private
+    
+    module Docs
+  class Entry
+    class Invalid < StandardError; end
+    
+        def url
+      @url ||= URL.parse request.base_url
+    end
+    
+          def process_response(response)
+        original_scheme = self.base_url.scheme
+        original_host = self.base_url.host
+        original_path = self.base_url.path
+    
+            css('h1 + code').each do |node|
+          node.before('<p></p>')
+          while node.next_element.name == 'code'
+            node.previous_element << ' '
+            node.previous_element << node.next_element
+          end
+          node.previous_element.prepend_child(node)
+        end
+    
+          def mod
+        return @mod if defined?(@mod)
+        @mod = slug[/api\/([\w\-\.]+)\//, 1]
+        @mod.remove! 'angular2.' if @mod
+        @mod
       end
     end
   end
 end
 
     
-              # Input: '-foo'
-          if chunk.start_with?(dash)
-            key = chunk.byteslice(1, chunk.length - 1)
-            value = false
+          # Calls the given block for each `when` node in the `case` statement.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_when
+        return when_branches.to_enum(__method__) unless block_given?
     
-          def initialize(raw_change)
-        if raw_change.is_a?(Gitaly::GetRawChangesResponse::RawChange)
-          @blob_id = raw_change.blob_id
-          @blob_size = raw_change.size
-          @old_path = raw_change.old_path.presence
-          @new_path = raw_change.new_path.presence
-          @operation = raw_change.operation&.downcase || :unknown
-        else
-          parse(raw_change)
-        end
+          # Checks whether this node body is a void context.
+      #
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
       end
     
-      context 'with associated agents' do
-    let!(:bob_scheduler_agent) {
-      Agents::SchedulerAgent.create!(
-        user: users(:bob),
-        name: 'Example Scheduler',
-        options: {
-          'action' => 'run',
-          'schedule' => '0 * * * *'
-        },
-      )
-    }
+    module RuboCop
+  module AST
+    # A node extension for `if` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `if` nodes within RuboCop.
+    class IfNode < Node
+      include ConditionalNode
+      include ModifierNode
     
-              @bar3 = Agents::DotBar.new(name: 'bar3').tap { |agent|
-            agent.user = users(:bob)
-            agent.sources << @bar2
-            agent.save!
-          },
-        ]
-        @foo.reload
-        @bar2.reload
+          DOUBLE_SPLAT = '**'.freeze
     
-        it 'in the future' do
-      expect(relative_distance_of_time_in_words(Time.now+5.minutes)).to eq('in 5m')
-    end
-  end
-end
-
+          # A helper class for comparing the positions of different parts of a
+      # `pair` node.
+      class HashElementDelta
+        def initialize(first, second)
+          @first = first
+          @second = second
     
-      describe '#style_colors' do
-    it 'returns a css style-formated version of the scenario foreground and background colors' do
-      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
-    end
-    
-      describe 'validations' do
-    subject do
-      _import = ScenarioImport.new
-      _import.set_user(user)
-      _import
-    end
-    
-      context '#set_traps' do
-    it 'sets traps for INT TERM and QUIT' do
-      agent_runner = AgentRunner.new
-      mock(Signal).trap('INT')
-      mock(Signal).trap('TERM')
-      mock(Signal).trap('QUIT')
-      agent_runner.set_traps
-    
-          it 'should not run disabled Agents' do
-        mock(Agent).find(agents(:bob_weather_agent).id) { agents(:bob_weather_agent) }
-        do_not_allow(agents(:bob_weather_agent)).check
-        agents(:bob_weather_agent).update_attribute :disabled, true
-        Agent.async_check(agents(:bob_weather_agent).id)
-      end
-    end
-    
-    class Devise::SessionsController < DeviseController
-  prepend_before_action :require_no_authentication, only: [:new, :create]
-  prepend_before_action :allow_params_authentication!, only: :create
-  prepend_before_action :verify_signed_out_user, only: :destroy
-  prepend_before_action(only: [:create, :destroy]) { request.env['devise.skip_timeout'] = true }
-    
-      private
-    
-          # Forgets the given resource by deleting a cookie
-      def forget_me(resource)
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.forget_me!
-        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
-      end
-    
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-    Given(/^(\d+) valid existing releases$/) do |num|
-  a_day = 86_400 # in seconds
-  offset = -(a_day * num.to_i)
-  num.to_i.times do
-    run_vagrant_command('mkdir -p #{TestApp.release_path(TestApp.timestamp(offset))}')
-    offset += a_day
-  end
-end
-    
-          def fetch_primary(role)
-        hosts = roles_for([role])
-        hosts.find(&:primary) || hosts.first
-      end
-    
-      SPREE_GEMS.each do |gem_name|
-    rm_f  '#{gem_name}/Gemfile.lock'
-    rm_rf '#{gem_name}/pkg'
-    rm_rf '#{gem_name}/spec/dummy'
-  end
-end
-    
-            def inventory_unit_params
-          params.require(:inventory_unit).permit(permitted_inventory_unit_attributes)
+            def find_address
+          if @order.bill_address_id == params[:id].to_i
+            @order.bill_address
+          elsif @order.ship_address_id == params[:id].to_i
+            @order.ship_address
+          else
+            raise CanCan::AccessDenied
+          end
         end
       end
     end
@@ -141,44 +136,13 @@ end
 end
 
     
-              if Spree::Cart::Update.call(order: @order, params: line_items_attributes).success?
-            @line_item.reload
-            respond_with(@line_item, default_template: :show)
-          else
-            invalid_resource!(@line_item)
-          end
-        end
-    
-            def update
-          @option_type = Spree::OptionType.accessible_by(current_ability, :update).find(params[:id])
-          if @option_type.update_attributes(option_type_params)
-            render :show
-          else
-            invalid_resource!(@option_type)
-          end
-        end
-    
-            def create
-          authorize! :create, Spree::OptionValue
-          @option_value = scope.new(option_value_params)
-          if @option_value.save
-            render :show, status: 201
-          else
-            invalid_resource!(@option_value)
-          end
+            def capture
+          perform_payment_action(:capture)
         end
     
             def show
-          respond_with(@product_property)
+          respond_with(stock_location)
         end
     
-              @properties = if params[:ids]
-                          @properties.where(id: params[:ids].split(',').flatten)
-                        else
-                          @properties.ransack(params[:q]).result
-                        end
-    
-            def stock_location
-          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
-          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
-        end
+    # The project root directory
+$root = ::File.dirname(__FILE__)
