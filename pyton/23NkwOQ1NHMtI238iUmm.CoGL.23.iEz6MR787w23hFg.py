39487cfd318230b6f3aceb2780f67fea7efd2588
SUNsung <weatherius@gmail.com>
2018-12-02 22:31:34 +0200

    Rewrite [23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py] File in: 2018-12-02 22:31:34

--- pyton/23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py
+++ pyton/23NkwOQ1NHMtI238iUmm.CoGL.23.iEz6MR787w23hFg.py
@@ -1,73 +1,100 @@
 
         
-        
-class Rank(Enum):
+            auto_json = args.data and not args.form
+    if args.json or auto_json:
+        default_headers['Accept'] = JSON_ACCEPT
+        if args.json or (auto_json and args.data):
+            default_headers['Content-Type'] = JSON_CONTENT_TYPE
     
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
-    
-    
-def build_completion(opt_parser):
-    commands = []
-    
-    import rsa
-import json
-from binascii import hexlify
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    header = oldreadme[:oldreadme.index('# OPTIONS')]
-footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
-    
-        def matching_ies(self, url):
-        return [ie.IE_NAME for ie in self.ies if ie.suitable(url) and ie.IE_NAME != 'generic']
-    
-                if is_playlist:
-                self.assertTrue(res_dict['_type'] in ['playlist', 'multi_video'])
-                self.assertTrue('entries' in res_dict)
-                expect_info_dict(self, res_dict, test_case.get('info_dict', {}))
-    
-    
-class CheckIp(front_base.check_ip.CheckIp):
-    def check_response(self, response):
-        server_type = response.headers.get('Server', '')
-        self.logger.debug('status:%d', response.status)
-        self.logger.debug('Server type:%s', server_type)
-    
-        def get_stat(self):
-        return self.network_stat
-    
-            Terence implemented packed table initializers, because Java has a
-        size restriction on .class files and the lookup tables can grow
-        pretty large. The generated JavaLexer.java of the Java.g example
-        would be about 15MB with uncompressed array initializers.
-    
-    
-    def getTokens(self, start=None, stop=None, types=None):
-        '''
-        Given a start and stop index, return a list of all tokens in
-        the token type set.  Return None if no tokens were found.  This
-        method looks at both on and off channel tokens.
         '''
     
-        def __init__(self, type=None, text=None, channel=DEFAULT_CHANNEL,
-                 oldToken=None
-                 ):
-        Token.__init__(self)
-        
-        if oldToken is not None:
-            self.text = oldToken.text
-            self.type = oldToken.type
-            self.line = oldToken.line
-            self.charPositionInLine = oldToken.charPositionInLine
-            self.channel = oldToken.channel
-            
-        self.text = text
-        self.type = type
-        self.line = None
-        self.charPositionInLine = None
-        self.channel = channel
-        self.index = None
+        @staticmethod
+    def make_header(username, password):
+        credentials = u'%s:%s' % (username, password)
+        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
+        return 'Basic %s' % token
+    
+        def get_auth_plugin(self, auth_type):
+        return self.get_auth_plugin_mapping()[auth_type]
+    
+        >>> humanize_bytes(1)
+    '1 B'
+    >>> humanize_bytes(1024, precision=1)
+    '1.0 kB'
+    >>> humanize_bytes(1024 * 123, precision=1)
+    '123.0 kB'
+    >>> humanize_bytes(1024 * 12342, precision=1)
+    '12.1 MB'
+    >>> humanize_bytes(1024 * 12342, precision=2)
+    '12.05 MB'
+    >>> humanize_bytes(1024 * 1234, precision=2)
+    '1.21 MB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
+    '1.31 GB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
+    '1.3 GB'
+    
+            self.assertRaises(TypeError, self.pathmodule.samefile)
+    
+    
+class CompressDecompressTest(BaseTest):
+    def testCompress(self):
+        data = bz2.compress(self.TEXT)
+        self.assertEqual(ext_decompress(data), self.TEXT)
+    
+    
+def main():
+    parser = ArgumentParser(description='''\
+Send the contents of a directory as a MIME message.
+Unless the -o option is given, the email is sent by forwarding to your local
+SMTP server, which then does the normal delivery process.  Your local machine
+must be running an SMTP server.
+''')
+    parser.add_argument('-d', '--directory',
+                        help='''Mail the contents of the specified directory,
+                        otherwise use the current directory.  Only the regular
+                        files in the directory are sent, and we don't recurse to
+                        subdirectories.''')
+    parser.add_argument('-o', '--output',
+                        metavar='FILE',
+                        help='''Print the composed message to FILE instead of
+                        sending the message to the SMTP server.''')
+    parser.add_argument('-s', '--sender', required=True,
+                        help='The value of the From: header (required)')
+    parser.add_argument('-r', '--recipient', required=True,
+                        action='append', metavar='RECIPIENT',
+                        default=[], dest='recipients',
+                        help='A To: header value (at least one required)')
+    args = parser.parse_args()
+    directory = args.directory
+    if not directory:
+        directory = '.'
+    # Create the message
+    msg = EmailMessage()
+    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
+    msg['To'] = ', '.join(args.recipients)
+    msg['From'] = args.sender
+    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+    
+    while True:
+    line = input()
+    if line == '':
+        break
+    buffer += line
+    if sqlite3.complete_statement(buffer):
+        try:
+            buffer = buffer.strip()
+            cur.execute(buffer)
+    
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+    
+        CNTV().download_by_vid(rid, **kwargs)
+    
+                    moz_ec_name = search_dict(sym_to_name, 'mozEcName')
+                push = search_dict(sym_to_name, 'push')
+                patt = '{}\.{}\('(.+?)'\)'.format(moz_ec_name, push)
+                ec_list = re.findall(patt, code)
+                [magic_list.append(sym_to_name[ec]) for ec in ec_list]
+        return magic_list
