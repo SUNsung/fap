6607a9bbc4d435b395559685cd7c533b99717ca0
SUNsung <weatherius@gmail.com>
2018-12-02 21:50:02 +0200

    Rewrite [35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb] File in: 2018-12-02 21:50:01

--- ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
+++ ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
@@ -1,41 +1,67 @@
 
         
-                klass = Class.new(self)
-        klass.name = name
-        klass.slug = slug
-        klass.version = version
-        klass.release = release
-        klass.links = links
-        klass.class_exec(&block)
-        @versions ||= []
-        @versions << klass
-        klass
-      end
+            A binary installer is available:
+      https://www.haskell.org/platform/mac.html
+    EOS
+  when 'mysqldump-secure' then <<-EOS.undent
+    The creator of mysqldump-secure tried to game our popularity metrics.
+    EOS
+  when 'ngrok' then <<-EOS.undent
+    Upstream sunsetted 1.x in March 2016 and 2.x is not open-source.
     
-        def inheritable_copy
-      self.class.new @filters
+      # Use this method to generate standard caveats.
+  def standard_instructions(home_name, home_value = libexec)
+    <<-EOS.undent
+      Before you can use these tools you must export some variables to your $SHELL.
+    
+          open_dry_run_modal(formatting_agent)
+      find('.dry-run-event-sample').click
+      within(:css, '.modal .builder') do
+        expect(page).to have_text('Line 1\nLine 2\nLine 3')
+      end
+      click_on('Dry Run')
+      expect(page).to have_text('Line 1,Line 2,Line 3')
+      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
+    end
+  end
+    
+      it 'requires a URL or file uplaod' do
+    visit new_scenario_imports_path
+    click_on 'Start Import'
+    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
+  end
+    
+          context '#run_workers' do
+        it 'runs all the workers' do
+          mock.instance_of(HuginnScheduler).run!
+          mock.instance_of(DelayedJobWorker).run!
+          @agent_runner.send(:run_workers)
+        end
+    
+      it 'converts values to Float' do
+    expect(location.lat).to be_a Float
+    expect(location.lat).to eq 2.0
+    expect(location.lng).to be_a Float
+    expect(location.lng).to eq 3.0
+    expect(location.radius).to be_a Float
+    expect(location.radius).to eq 300.0
+    expect(location.speed).to be_a Float
+    expect(location.speed).to eq 2.0
+    expect(location.course).to be_a Float
+    expect(location.course).to eq 30.0
+  end
+    
+        it 'accepts objects as well as strings' do
+      log = AgentLog.log_for_agent(agents(:jane_website_agent), events(:bob_website_agent_event).payload)
+      expect(log.message).to include(''title'=>'foo'')
+    end
+  end
+    
+          flatten_rules(rule)
     end
     
-        def self.run(urls, options = {}, &block)
-      urls = urls.dup
-      requester = new(options)
-      requester.on_response(&block) if block
-      requester.on_response do # cheap hack to ensure root page is processed first
-        if urls
-          requester.request(urls)
-          urls = nil
-        end
-      end
-      requester.request(urls.shift)
-      requester.run
-      requester
-    end
-    
-            css('.example-title + pre').each do |node|
-          node['name'] = node.previous_element.content.strip
-          node.previous_element.remove
-        end
-    
-          def include_default_entry?
-        INDEX.add?([name, type].join(';')) ? true : false # ¯\_(ツ)_/¯
+          opts.on('-s', '--stdin', :NONE,
+              'Read input from standard input instead of an input file.',
+              'This is the default if no input file is specified. Requires --from.') do
+        @options[:input] = $stdin
       end
