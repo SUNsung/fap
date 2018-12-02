04b22ac5f2ed3faace2268c6541bbba149405055
SUNsung <weatherius@gmail.com>
2018-12-02 02:56:09 +0200

    Rewrite [507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py] File in: 2018-12-02 02:56:08

--- pyton/507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py
+++ pyton/507sgbC1dhKA50D0jVstiz3fg50F7Ucc.ZRc50TbL.py
@@ -1,140 +1,138 @@
 
         
-            @property
-    def config(self):
-        if not hasattr(self, '_config'):
-            self._config = Config(directory=self.config_dir)
-            if self._config.is_new():
-                self._config.save()
-            else:
-                self._config.load()
-        return self._config
-    
-        @property
-    def body(self):
-        '''Return a `bytes` with the message's body.'''
-        raise NotImplementedError()
-    
-        def get_auth_plugin(self, auth_type):
-        return self.get_auth_plugin_mapping()[auth_type]
-    
-        def test_GET_no_data_no_auto_headers(self, httpbin):
-        # https://github.com/jakubroztocil/httpie/issues/62
-        r = http('GET', httpbin.url + '/headers')
-        assert HTTP_OK in r
-        assert r.json['headers']['Accept'] == '*/*'
-        assert 'Content-Type' not in r.json['headers']
+            return _set
     
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
+def test_on_run_after_other_commands(usage_tracker_io, shell_pid, shell, logs):
+    shell_pid.return_value = 12
+    shell.get_history.return_value = ['fuck', 'ls']
+    _change_tracker(usage_tracker_io, 12)
+    main()
+    logs.how_to_configure_alias.assert_called_once()
+    
+    # TODO: ensure that history changes.
 
     
     
-def test_max_redirects(httpbin):
-    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
-             error_exit_ok=True)
-    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
+@pytest.mark.functional
+def test_how_to_configure_alias(proc, TIMEOUT):
+    proc.sendline(u'unfunction fuck')
+    how_to_configure(proc, TIMEOUT)
 
     
-        time_ridge = np.empty(n_iter)
-    time_ols = np.empty(n_iter)
-    time_lasso = np.empty(n_iter)
+     install - install packages
+ remove  - remove packages
     
-    # datasets available: ['http', 'smtp', 'SA', 'SF', 'shuttle', 'forestcover']
-datasets = ['http', 'smtp', 'SA', 'SF', 'shuttle', 'forestcover']
     
-        samples : array-like of ints (1d or 0d)
-        The number of samples to generate as input.
+@pytest.mark.parametrize('command', [
+    Command('apt list --upgradable', no_match_output),
+    Command('sudo apt list --upgradable', no_match_output)
+])
+def test_not_match(command):
+    assert not match(command)
     
-        i = 0
-    for c, (label, timings) in zip('br',
-                                   sorted(six.iteritems(results_2))):
-        i += 1
-        ax = fig.add_subplot(2, 2, i + 2)
-        y = np.asarray(timings)
-        ax.plot(chunks, y, color=c, alpha=0.8)
-        ax.set_xlabel('Chunks')
-        ax.set_ylabel(label)
+    You can download from:
+  https://osxfuse.github.io/
+Error: An unsatisfied requirement failed this build.'''
     
-                gc.collect()
-            print('benchmarking orthogonal_mp (without Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            orthogonal_mp(X, y, precompute=False,
-                          n_nonzero_coefs=n_informative)
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            omp[i_f, i_s] = delta
+                # >leading or trailing LWS MAY be removed without
+            # >changing the semantics of the field value'
+            # -https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
+            # Also, requests raises `InvalidHeader` for leading spaces.
+            value = value.strip()
     
-    ward = AgglomerativeClustering(n_clusters=3, linkage='ward')
+        def iter_body(self, chunk_size):
+        yield self.body
     
-    del sys
+    
+class Conversion(object):
+    
+        @staticmethod
+    def make_header(username, password):
+        credentials = u'%s:%s' % (username, password)
+        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
+        return 'Basic %s' % token
+    
+    
+with codecs.open(JSON_FILE_PATH, encoding='utf8') as f:
+    JSON_FILE_CONTENT = f.read()
+    
+    
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
+    
+        By default this will get the strings from the blns.txt file
+    
+    HEADER_ARGS = {'Strict-Transport-Security': HSTS_ARGS}
 
     
-        def run(self, args, opts):
-        if len(args) != 1:
-            raise UsageError()
-    
-                # trustRoot set to platformTrust() will use the platform's root CAs.
-            #
-            # This means that a website like https://www.cacert.org will be rejected
-            # by default, since CAcert.org CA certificate is seldom shipped.
-            return optionsForClientTLS(hostname.decode('ascii'),
-                                       trustRoot=platformTrust(),
-                                       extraCertificateOptions={
-                                            'method': self._ssl_method,
-                                       })
-    
-            crawler.signals.connect(self._close, signals.engine_stopped)
-    
-    site_info = 'baomihua.com'
-download = baomihua_download
-download_playlist = playlist_not_supported('baomihua')
-
-    
-    from ..common import *
-    
-    #----------------------------------------------------------------------
-def ckplayer_download(url, output_dir = '.', merge = False, info_only = False, is_xml = True, **kwargs):
-    if is_xml:  #URL is XML URL
-        try:
-            title = kwargs['title']
-        except:
-            title = ''
-        try:
-            headers = kwargs['headers']  #headers provided
-            ckinfo = get_content(url, headers = headers)
-        except NameError:
-            ckinfo = get_content(url)
-        
-        ckplayer_download_by_xml(ckinfo, output_dir, merge, 
-                                info_only, title = title)
-    
-        ep = 'http://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={}'
+        def setUp(self):
+        from acme.errors import PollError
+        self.timeout = PollError(
+            exhausted=set([mock.sentinel.AR]),
+            updated={})
+        self.invalid = PollError(exhausted=set(), updated={
+            mock.sentinel.AR: mock.sentinel.AR2})
     
     
-def coub_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_content(url)
+class Fixed(jose.Field):
+    '''Fixed field.'''
     
-        print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([video_url], title, ext, size, output_dir, merge = merge, headers = headers)
     
-        @classmethod
-    def get_coeff(cls, magic_list):
-        magic_set = set(magic_list)
-        no_dup = []
-        for item in magic_list:
-            if item in magic_set:
-                magic_set.remove(item)
-                no_dup.append(item)
-        # really necessary?
+class JWS(jose.JWS):
+    '''ACME-specific JWS. Includes none, url, and kid in protected header.'''
+    signature_cls = Signature
+    __slots__ = jose.JWS._orig_slots  # pylint: disable=no-member
+    
+    class SelectVhostTest(unittest.TestCase):
+    '''Tests for certbot_apache.display_ops.select_vhost.'''
+    
+            # Adding unique constraint on 'ReprocessingReport', fields ['project', 'event_id']
+        db.create_unique('sentry_reprocessingreport', ['project_id', 'event_id'])
+    
+            # Deleting model 'ApiAuthorization'
+        db.delete_table('sentry_apiauthorization')
+    
+            # Deleting model 'VersionDSymFile'
+        db.delete_table('sentry_versiondsymfile')
+    
+    
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding model 'ReleaseHeadCommit'
+        db.create_table(
+            'sentry_releaseheadcommit', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'organization_id',
+                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(
+                        db_index=True
+                    )
+                ), (
+                    'repository_id',
+                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
+                ), (
+                    'release', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.Release']
+                    )
+                ), (
+                    'commit', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.Commit']
+                    )
+                ),
+            )
+        )
+        db.send_create_signal('sentry', ['ReleaseHeadCommit'])
+    
+        def backwards(self, orm):
+        # Removing unique constraint on 'UserOption', fields ['user', 'organization', 'key']
+        db.delete_unique('sentry_useroption', ['user_id', 'organization_id', 'key'])
