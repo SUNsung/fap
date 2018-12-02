3b150752dac3f07e1f4b82f256be032ef3300380
SUNsung <weatherius@gmail.com>
2018-12-02 19:10:43 +0200

    Rewrite [369Jxba1ZJyMk36Ru2YzTzYTc236.s8QdbzPoFY36rMH.cpp] File in: 2018-12-02 19:10:43

--- cccccc/369Jxba1ZJyMk36Ru2YzTzYTc236.s8QdbzPoFY36rMH.cpp
+++ cccccc/369Jxba1ZJyMk36Ru2YzTzYTc236.s8QdbzPoFY36rMH.cpp
@@ -1,132 +1,149 @@
 
         
         
-    {    result->AppendBoolean(success);
-    return;
-  } else if (method == 'UnregisterGlobalHotKey') {
-    int object_id = -1;
-    arguments.GetInteger(0, &object_id);
-    Shortcut* shortcut =
-        static_cast<Shortcut*>(DispatcherHost::GetApiObject(object_id));
-    GlobalShortcutListener::GetInstance()->UnregisterAccelerator(
-        shortcut->GetAccelerator(), shortcut);
-    return;
-  } else if (method == 'SetProxyConfig') {
-    std::string proxy_config;
-    arguments.GetString(0, &proxy_config);
-    SetProxyConfig(GetRenderProcessHost(), proxy_config);
-    return;
-  } else if (method == 'DoneMenuShow') {
-    dispatcher_host->quit_run_loop();
-    return;
+    {  ~TemporaryFile() {
+    unlink(path.c_str());
   }
-    
-    
-void Base::Call(const std::string& method, const base::ListValue& arguments,
-                content::RenderFrameHost* rvh) {
-  NOTREACHED() << 'Uncatched call in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
-}
-    
-      scoped_ptr<base::Value> value_args(
-      converter->FromV8Value(args, isolate->GetCurrentContext()));
-  if (!value_args.get() ||
-      !value_args->IsType(base::Value::TYPE_LIST))
-    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-        'Unable to convert 'args' passed to CallObjectMethod')));
-    
-      ~EventListener() override;
-    
-    class MenuDelegate : public ui::SimpleMenuModel::Delegate {
- public:
-  MenuDelegate(ObjectManager* object_manager);
-  ~MenuDelegate() override;
-    }
-    
-    
-    {  focus_manager_ = NULL;
-}
-    
-     protected:
-  ~NwAppGetArgvSyncFunction() override;
-    
-      bool NwScreenInitEventListenersFunction::RunNWSync(base::ListValue* response, std::string* error) {
-    NwScreenDisplayObserver::GetInstance();
-    return true;
-  }
-    
-    template <class I>
-struct is_random_imp
-{
-#ifndef BOOST_NO_STD_ITERATOR_TRAITS
-private:
-   typedef typename std::iterator_traits<I>::iterator_category cat;
-public:
-   BOOST_STATIC_CONSTANT(bool, value = (::boost::is_convertible<cat*, std::random_access_iterator_tag*>::value));
-#else
-   BOOST_STATIC_CONSTANT(bool, value = false);
-#endif
 };
     
-       // find out which of these two alternatives we need to take:
-   bool take_first, take_second;
-   if(position == last)
-   {
-      take_first = rep->can_be_null & mask_take;
-      take_second = rep->can_be_null & mask_skip;
-   }
-   else
-   {
-      take_first = can_start(*position, rep->_map, (unsigned char)mask_take);
-      take_second = can_start(*position, rep->_map, (unsigned char)mask_skip);
-   }
+    REGISTER_CPU_OPERATOR(EnforceFinite, EnforceFiniteOp<CPUContext>);
     
-    #ifndef BOOST_NO_WREGEX
-#define BOOST_REGEX_CHAR_T wchar_t
-#ifdef BOOST_REGEX_WIDE_INSTANTIATE
-#  define BOOST_REGEX_INSTANTIATE
-#endif
-#include <boost/regex/v4/instances.hpp>
-#undef BOOST_REGEX_CHAR_T
-#ifdef BOOST_REGEX_INSTANTIATE
-#  undef BOOST_REGEX_INSTANTIATE
-#endif
-#endif
-    
-    static void Dump(benchmark::State& state, const char* filename, int indent)
-{
-    std::ifstream f(filename);
-    std::string str((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
-    json j = json::parse(str);
+    class GetMergeMultiScalarFeatureTensorsGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    vector<string> input_blob_names{};
+    vector<string> output_blob_names{};
+    }
     }
     
-    // Macros for defining flags.
-#define DEFINE_bool(name, default_val, doc) bool FLAG(name) = (default_val)
-#define DEFINE_int32(name, default_val, doc) int32_t FLAG(name) = (default_val)
-#define DEFINE_int64(name, default_val, doc) int64_t FLAG(name) = (default_val)
-#define DEFINE_double(name, default_val, doc) double FLAG(name) = (default_val)
-#define DEFINE_string(name, default_val, doc) \
-  std::string FLAG(name) = (default_val)
+    OPERATOR_SCHEMA(GatherRangesToDense)
+    .NumInputs(2, 3)
+    .NumOutputs(1, INT_MAX)
+    .SetDoc(R'DOC(
+Given DATA tensor of rank 1, and RANGES tensor of rank 3, gather values
+corresponding to each range into a separate output tensor. If the optional input
+KEY tensor is also given, the output will be sorted by KEY for each example.
     
     
-    {    printed_header_ = true;
-  } else {
-    // check that all the current counters are saved in the name set
-    for (const auto& run : reports) {
-      for (const auto& cnt : run.counters) {
-        CHECK(user_counter_names_.find(cnt.first) != user_counter_names_.end())
-              << 'All counters must be present in each run. '
-              << 'Counter named \'' << cnt.first
-              << '\' was not in a run after being added to the header';
-      }
-    }
+    {/**
+ * @brief Start a file carve of the given paths
+ *
+ * @return A status returning if the carves were started successfully
+ */
+Status carvePaths(const std::set<std::string>& paths);
+} // namespace osquery
+
+    
+    
+    { private:
+  static const std::string kLoggerKey;
+};
+    
+      // Generate content to update/add to the config.
+  std::string content;
+  auto s = readFile(kTestDataPath + 'test_parse_items.conf', content);
+  EXPECT_TRUE(s.ok());
+  std::map<std::string, std::string> config;
+  config['awesome'] = content;
+    
+    class ViewsConfigParserPluginTests : public testing::Test {};
+    
+    /**
+ * @brief The supported hashing algorithms in osquery
+ *
+ * These are usually used as a constructor argument to osquery::Hash
+ */
+enum HashType {
+  HASH_TYPE_MD5 = 2,
+  HASH_TYPE_SHA1 = 4,
+  HASH_TYPE_SHA256 = 8,
+};
+    
+      if (!::GetExitCodeProcess(process.nativeHandle(), &code)) {
+    return false;
   }
     
-    // Enable thread safety attributes only with clang.
-// The attributes can be safely erased when compiling with other compilers.
-#if defined(HAVE_THREAD_SAFETY_ATTRIBUTES)
-#define THREAD_ANNOTATION_ATTRIBUTE__(x) __attribute__((x))
-#else
-#define THREAD_ANNOTATION_ATTRIBUTE__(x)  // no-op
-#endif
+      /**
+   * Some decision making code refactored in needMonitoring before calling
+   * addMonitor in the context of monitorSubscription.
+   * Decision to call addMonitor from the context of monitorSubscription
+   * is done based on the status change time of file/directory, since
+   * creation time is not available on linux.
+   */
+  bool needMonitoring(const std::string& path,
+                      INotifySubscriptionContextRef& isc,
+                      uint32_t mask,
+                      bool recursive,
+                      bool add_watch);
+    
+      // Don't overload the normal `init` Subscription member.
+  sub->lateInit();
+  pub->configure();
+  EXPECT_EQ(pub->numSubscriptions(), 1U);
+    
+      t7 = a[5].imag;
+  t4 = a[1].imag - t7;
+  t7 += a[1].imag;
+  a[1].imag = t7;
+    
+    static const int kCrToRedTable[256] = {
+  -179, -178, -177, -175, -174, -172, -171, -170, -168, -167, -165, -164,
+  -163, -161, -160, -158, -157, -156, -154, -153, -151, -150, -149, -147,
+  -146, -144, -143, -142, -140, -139, -137, -136, -135, -133, -132, -130,
+  -129, -128, -126, -125, -123, -122, -121, -119, -118, -116, -115, -114,
+  -112, -111, -109, -108, -107, -105, -104, -102, -101, -100,  -98,  -97,
+   -95,  -94,  -93,  -91,  -90,  -88,  -87,  -86,  -84,  -83,  -81,  -80,
+   -79,  -77,  -76,  -74,  -73,  -72,  -70,  -69,  -67,  -66,  -64,  -63,
+   -62,  -60,  -59,  -57,  -56,  -55,  -53,  -52,  -50,  -49,  -48,  -46,
+   -45,  -43,  -42,  -41,  -39,  -38,  -36,  -35,  -34,  -32,  -31,  -29,
+   -28,  -27,  -25,  -24,  -22,  -21,  -20,  -18,  -17,  -15,  -14,  -13,
+   -11,  -10,   -8,   -7,   -6,   -4,   -3,   -1,    0,    1,    3,    4,
+     6,    7,    8,   10,   11,   13,   14,   15,   17,   18,   20,   21,
+    22,   24,   25,   27,   28,   29,   31,   32,   34,   35,   36,   38,
+    39,   41,   42,   43,   45,   46,   48,   49,   50,   52,   53,   55,
+    56,   57,   59,   60,   62,   63,   64,   66,   67,   69,   70,   72,
+    73,   74,   76,   77,   79,   80,   81,   83,   84,   86,   87,   88,
+    90,   91,   93,   94,   95,   97,   98,  100,  101,  102,  104,  105,
+   107,  108,  109,  111,  112,  114,  115,  116,  118,  119,  121,  122,
+   123,  125,  126,  128,  129,  130,  132,  133,  135,  136,  137,  139,
+   140,  142,  143,  144,  146,  147,  149,  150,  151,  153,  154,  156,
+   157,  158,  160,  161,  163,  164,  165,  167,  168,  170,  171,  172,
+   174,  175,  177,  178
+};
+    
+    namespace guetzli {
+    }
+    
+      png_infop info_ptr = png_create_info_struct(png_ptr);
+  if (!info_ptr) {
+    png_destroy_read_struct(&png_ptr, nullptr, nullptr);
+    return false;
+  }
+    
+    // Mimic libjpeg's heuristics to guess jpeg color space.
+// Requires that the jpg has 3 components.
+bool HasYCbCrColorSpace(const JPEGData& jpg) {
+  bool has_Adobe_marker = false;
+  uint8_t Adobe_transform = 0;
+  for (const std::string& app : jpg.app_data) {
+    if (static_cast<uint8_t>(app[0]) == 0xe0) {
+      return true;
+    } else if (static_cast<uint8_t>(app[0]) == 0xee && app.size() >= 15) {
+      has_Adobe_marker = true;
+      Adobe_transform = app[14];
+    }
+  }
+  if (has_Adobe_marker) {
+    return (Adobe_transform != 0);
+  }
+  const int cid0 = jpg.components[0].id;
+  const int cid1 = jpg.components[1].id;
+  const int cid2 = jpg.components[2].id;
+  return (cid0 != 'R' || cid1 != 'G' || cid2 != 'B');
+}
+    
+    #include 'guetzli/jpeg_data.h'
+    
+    
+    {  uint32_t counts[kSize];
+};
