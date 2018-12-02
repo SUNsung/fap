917616f03ee85f6f7219a6313995c246a0ff6edc
SUNsung <weatherius@gmail.com>
2018-12-02 15:26:29 +0200

    Rewrite [03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb] File in: 2018-12-02 15:26:28

--- ruby/03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb
+++ ruby/03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb
@@ -1,76 +1,120 @@
 
         
-          def remove_duplicates
-    where = 'WHERE s1.user_id = s2.user_id AND s1.shareable_id = s2.shareable_id AND '\
-      's1.shareable_type = s2.shareable_type AND s1.id > s2.id'
-    if AppConfig.postgres?
-      execute('DELETE FROM share_visibilities AS s1 USING share_visibilities AS s2 #{where}')
-    else
-      execute('DELETE s1 FROM share_visibilities s1, share_visibilities s2 #{where}')
+              # Converts the array to a comma-separated sentence where the last element is
+      # joined by the connector word. This is the html_safe-aware version of
+      # ActiveSupport's {Array#to_sentence}[http://api.rubyonrails.org/classes/Array.html#method-i-to_sentence].
+      #
+      def to_sentence(array, options = {})
+        options.assert_valid_keys(:words_connector, :two_words_connector, :last_word_connector, :locale)
+    
+              if include_hidden
+            hidden = hidden_field_for_checkbox(options)
+            hidden + checkbox
+          else
+            checkbox
+          end
+        end
+    
+          _write_layout_method
+    end
+    
+        def exists?(path, prefixes, *args)
+      find_all(path, prefixes, *args).any?
+    end
+    
+    describe 'Dry running an Agent', js: true do
+  let(:agent)   { agents(:bob_website_agent) }
+  let(:formatting_agent) { agents(:bob_formatting_agent) }
+  let(:user)    { users(:bob) }
+  let(:emitter) { agents(:bob_weather_agent) }
+    
+      it 'asks to accept conflicts when the scenario was modified' do
+    DefaultScenarioImporter.seed(user)
+    agent = user.agents.where(name: 'Rain Notifier').first
+    agent.options['expected_receive_period_in_days'] = 9001
+    agent.save!
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
+    click_on 'Start Import'
+    expect(page).to have_text('This Scenario already exists in your system.')
+    expect(page).to have_text('9001')
+    check('I confirm that I want to import these Agents.')
+    click_on 'Finish Import'
+    expect(page).to have_text('Import successful!')
+  end
+    
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
+  end
+    
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
+    end
+    
+      let :reverted_extract do
+    old_extract
+  end
+    
+            @receiver = Agents::CannotBeScheduled.new(
+          name: 'Agent',
+          options: { foo: 'bar3' },
+          keep_events_for: 3.days,
+          propagate_immediately: true)
+        @receiver.user = users(:bob)
+        @receiver.sources << @sender
+        @receiver.memory[:test] = 1
+        @receiver.save!
+      end
+    
+          attr_reader :page, :name
+    
+          def format
+        @format = (@page.format || false) if @format.nil?
+        @format.to_s.downcase
+      end
     end
   end
 end
 
     
-    When /^I (?:log|sign) out$/ do
-  logout
-  step 'I go to the root page'
-end
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
-  end
-end
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-      end
-    
-          if scanner.match?(/url\(/i)
-        script_parser = Sass::Script::Parser.new(scanner, @line, to_parser_offset(offset), @options)
-        str = script_parser.parse_string
-    
-        COLORS = {:red => 31, :green => 32, :yellow => 33}
-    
-      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
-  gem.executables   = %w(cap capify)
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ['lib']
-    
-          super
-    end
-    
-          def load_built_in_scm
-        require 'capistrano/scm/#{scm_name}'
-        scm_class = Object.const_get(built_in_scm_plugin_class_name)
-        # We use :load_immediately because we are initializing the SCM plugin
-        # late in the load process and therefore can't use the standard
-        # load:defaults technique.
-        install_plugin(scm_class, load_immediately: true)
-      end
-    
-          def fetch(key, default=nil, &block)
-        fetched_keys << key unless fetched_keys.include?(key)
-        peek(key, default, &block)
-      end
-    
-    # The project root directory
-$root = ::File.dirname(__FILE__)
-    
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+          def next_link
+        label = 'Next &raquo;'
+        if @versions.size == Gollum::Page.per_page
+          link = '/history/#{@page.name}?page=#{@page_num+1}'
+          %(<a href='#{link}' hotkey='l'>#{label}</a>)
+        else
+          %(<span class='disabled'>#{label}</span>)
+        end
       end
     end
   end
 end
+
     
+      test 'extracting paths from URLs' do
+    assert_nil extract_path('Eye-Of-Sauron')
+    assert_equal 'Mordor', extract_path('Mordor/Sauron')
+    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
+  end
+    
+      test 'remove page extentions' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'page', view.remove_page_extentions('page.wiki')
+    assert_equal 'page-wiki', view.remove_page_extentions('page-wiki.md')
+    assert_equal 'file.any_extention', view.remove_page_extentions('file.any_extention')
+  end
+    
+        def initialize(dir, existing, attempted, message = nil)
+      @dir            = dir
+      @existing_path  = existing
+      @attempted_path = attempted
+      super(message || 'Cannot write #{@dir}/#{@attempted_path}, found #{@dir}/#{@existing_path}.')
     end
-Liquid::Template.register_filter OctopressLiquidFilters
+  end
+end
