a6260d8f34139854b80197da54242c6c803ba69c
SUNsung <weatherius@gmail.com>
2018-12-03 13:58:41 +0200

    Rewrite [29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp] File in: 2018-12-03 13:58:41

--- cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
+++ cccccc/29WFHGGOZJeWc29Knv2.Z42uao29CnTSf2ETLGg29pCD.cpp
@@ -1,186 +1,6 @@
 
         
-        namespace caffe2 {
-    }
-    
-    template <class charT, class traits>
-bool basic_regex_parser<charT, traits>::parse_inner_set(basic_char_set<charT, traits>& char_set)
-{
-   static const char* incomplete_message = 'Character class declaration starting with [ terminated prematurely - either no ] was found or the set had no content.';
-   //
-   // we have either a character class [:name:]
-   // a collating element [.name.]
-   // or an equivalence class [=name=]
-   //
-   if(m_end == ++m_position)
-   {
-      fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-      return false;
-   }
-   switch(this->m_traits.syntax_type(*m_position))
-   {
-   case regex_constants::syntax_dot:
-      //
-      // a collating element is treated as a literal:
-      //
-      --m_position;
-      parse_set_literal(char_set);
-      return true;
-   case regex_constants::syntax_colon:
-      {
-      // check that character classes are actually enabled:
-      if((this->flags() & (regbase::main_option_type | regbase::no_char_classes)) 
-         == (regbase::basic_syntax_group  | regbase::no_char_classes))
-      {
-         --m_position;
-         parse_set_literal(char_set);
-         return true;
-      }
-      // skip the ':'
-      if(m_end == ++m_position)
-      {
-         fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-         return false;
-      }
-      const charT* name_first = m_position;
-      // skip at least one character, then find the matching ':]'
-      if(m_end == ++m_position)
-      {
-         fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-         return false;
-      }
-      while((m_position != m_end) 
-         && (this->m_traits.syntax_type(*m_position) != regex_constants::syntax_colon)) 
-         ++m_position;
-      const charT* name_last = m_position;
-      if(m_end == m_position)
-      {
-         fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-         return false;
-      }
-      if((m_end == ++m_position) 
-         || (this->m_traits.syntax_type(*m_position) != regex_constants::syntax_close_set))
-      {
-         fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-         return false;
-      }
-      //
-      // check for negated class:
-      //
-      bool negated = false;
-      if(this->m_traits.syntax_type(*name_first) == regex_constants::syntax_caret)
-      {
-         ++name_first;
-         negated = true;
-      }
-      typedef typename traits::char_class_type m_type;
-      m_type m = this->m_traits.lookup_classname(name_first, name_last);
-      if(m == 0)
-      {
-         if(char_set.empty() && (name_last - name_first == 1))
-         {
-            // maybe a special case:
-            ++m_position;
-            if( (m_position != m_end) 
-               && (this->m_traits.syntax_type(*m_position) 
-                  == regex_constants::syntax_close_set))
-            {
-               if(this->m_traits.escape_syntax_type(*name_first) 
-                  == regex_constants::escape_type_left_word)
-               {
-                  ++m_position;
-                  this->append_state(syntax_element_word_start);
-                  return false;
-               }
-               if(this->m_traits.escape_syntax_type(*name_first) 
-                  == regex_constants::escape_type_right_word)
-               {
-                  ++m_position;
-                  this->append_state(syntax_element_word_end);
-                  return false;
-               }
-            }
-         }
-         fail(regex_constants::error_ctype, name_first - m_base);
-         return false;
-      }
-      if(negated == false)
-         char_set.add_class(m);
-      else
-         char_set.add_negated_class(m);
-      ++m_position;
-      break;
-   }
-   case regex_constants::syntax_equal:
-      {
-      // skip the '='
-      if(m_end == ++m_position)
-      {
-         fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-         return false;
-      }
-      const charT* name_first = m_position;
-      // skip at least one character, then find the matching '=]'
-      if(m_end == ++m_position)
-      {
-         fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-         return false;
-      }
-      while((m_position != m_end) 
-         && (this->m_traits.syntax_type(*m_position) != regex_constants::syntax_equal)) 
-         ++m_position;
-      const charT* name_last = m_position;
-      if(m_end == m_position)
-      {
-         fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-         return false;
-      }
-      if((m_end == ++m_position) 
-         || (this->m_traits.syntax_type(*m_position) != regex_constants::syntax_close_set))
-      {
-         fail(regex_constants::error_brack, m_position - m_base, incomplete_message);
-         return false;
-      }
-      string_type m = this->m_traits.lookup_collatename(name_first, name_last);
-      if((0 == m.size()) || (m.size() > 2))
-      {
-         fail(regex_constants::error_collate, name_first - m_base);
-         return false;
-      }
-      digraph<charT> d;
-      d.first = m[0];
-      if(m.size() > 1)
-         d.second = m[1];
-      else
-         d.second = 0;
-      char_set.add_equivalent(d);
-      ++m_position;
-      break;
-   }
-   default:
-      --m_position;
-      parse_set_literal(char_set);
-      break;
-   }
-   return true;
-}
-    
-    
-    {private:
-   // this type is not copyable:
-   c_regex_traits(const c_regex_traits&);
-   c_regex_traits& operator=(const c_regex_traits&);
-};
-    
-    namespace BOOST_REGEX_DETAIL_NS{
-template BOOST_REGEX_DECL void perl_matcher<std::basic_string<BOOST_REGEX_CHAR_T>::const_iterator, match_results< std::basic_string<BOOST_REGEX_CHAR_T>::const_iterator >::allocator_type, boost::regex_traits<BOOST_REGEX_CHAR_T > >::construct_init(
-      const basic_regex<BOOST_REGEX_CHAR_T>& e, match_flag_type f);
-template BOOST_REGEX_DECL bool perl_matcher<std::basic_string<BOOST_REGEX_CHAR_T>::const_iterator, match_results< std::basic_string<BOOST_REGEX_CHAR_T>::const_iterator >::allocator_type, boost::regex_traits<BOOST_REGEX_CHAR_T > >::match();
-template BOOST_REGEX_DECL bool perl_matcher<std::basic_string<BOOST_REGEX_CHAR_T>::const_iterator, match_results< std::basic_string<BOOST_REGEX_CHAR_T>::const_iterator >::allocator_type, boost::regex_traits<BOOST_REGEX_CHAR_T > >::find();
-} // namespace
-#endif
-    
-       format_perl = 0,                                  /* perl style replacement */
+           format_perl = 0,                                  /* perl style replacement */
    format_default = 0,                               /* ditto. */
    format_sed = match_max << 1,                      /* sed style replacement. */
    format_all = format_sed << 1,                     /* enable all extentions to sytax. */
@@ -189,86 +9,164 @@ template BOOST_REGEX_DECL bool perl_matcher<std::basic_string<BOOST_REGEX_CHAR_T
    format_is_if = format_first_only << 1,            /* internal use only. */
    format_literal = format_is_if << 1                /* treat string as a literal */
     
-          typedef typename BOOST_REGEX_DETAIL_NS::compute_functor_type<Functor, match_results<BidiIterator, Allocator>, BOOST_REGEX_DETAIL_NS::string_out_iterator<std::basic_string<char_type> > >::type F;
-      F func(fmt);
+    template <class Results>
+struct recursion_info
+{
+   typedef typename Results::value_type value_type;
+   typedef typename value_type::iterator iterator;
+   int idx;
+   const re_syntax_base* preturn_address;
+   Results results;
+   repeater_count<iterator>* repeater_stack;
+};
+    
+    template <class BidiIterator, class Allocator, class traits>
+bool perl_matcher<BidiIterator, Allocator, traits>::match_rep()
+{
+#ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable:4127 4244)
+#endif
+   const re_repeat* rep = static_cast<const re_repeat*>(pstate);
+   //
+   // Always copy the repeat count, so that the state is restored
+   // when we exit this scope:
+   //
+   repeater_count<BidiIterator> r(rep->state_id, &next_count, position, this->recursion_stack.size() ? this->recursion_stack.back().idx : INT_MIN + 3);
+   //
+   // If we've had at least one repeat already, and the last one 
+   // matched the NULL string then set the repeat count to
+   // maximum:
+   //
+   next_count->check_null_repeat(position, rep->max);
+    }
+    
+    template <class T>
+class concrete_protected_call
+   : public abstract_protected_call
+{
+public:
+   typedef bool (T::*proc_type)();
+   concrete_protected_call(T* o, proc_type p)
+      : obj(o), proc(p) {}
+private:
+   virtual bool call()const;
+   T* obj;
+   proc_type proc;
+};
     
     
     {
-    {#if defined(BOOST_REGEX_NON_RECURSIVE) && !defined(BOOST_NO_EXCEPTIONS)
-   }
-   catch(...)
+      basic = basic_syntax_group | collate | no_escape_in_lists,
+      extended = no_bk_refs | collate | no_perl_ex | no_escape_in_lists,
+      normal = 0,
+      emacs = basic_syntax_group | collate | emacs_ex | bk_vbar,
+      awk = no_bk_refs | collate | no_perl_ex,
+      grep = basic | newline_alt,
+      egrep = extended | newline_alt,
+      sed = basic,
+      perl = normal,
+      ECMAScript = normal,
+      JavaScript = normal,
+      JScript = normal
+   };
+   typedef unsigned int flag_type;
+    
+    template <class OutputIterator, class Results, class traits, class ForwardIter>
+void basic_regex_formatter<OutputIterator, Results, traits, ForwardIter>::put(const sub_match_type& sub)
+{
+   typedef typename sub_match_type::iterator iterator_type;
+   iterator_type i = sub.first;
+   while(i != sub.second)
    {
-      // unwind all pushed states, apart from anything else this
-      // ensures that all the states are correctly destructed
-      // not just the memory freed.
-      while(unwind(true)){}
-      throw;
+      put(*i);
+      ++i;
    }
-#endif
 }
     
-    template <class BidiIterator, class Allocator, class traits>
-bool perl_matcher<BidiIterator, Allocator, traits>::match_commit()
+    template <class traits, class charT>
+inline std::basic_string<charT> regex_merge(const std::basic_string<charT>& s,
+                         const basic_regex<charT, traits>& e, 
+                         const charT* fmt,
+                         match_flag_type flags = match_default)
 {
-   // Ideally we would just junk all the states that are on the stack,
-   // however we might not unwind correctly in that case, so for now,
-   // just mark that we don't backtrack into whatever is left (or rather
-   // we'll unwind it unconditionally without pausing to try other matches).
+   return regex_replace(s, e, fmt, flags);
+}
+    
+    #ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable: 4103)
+#endif
+#ifdef BOOST_HAS_ABI_HEADERS
+#  include BOOST_ABI_PREFIX
+#endif
+#ifdef BOOST_MSVC
+#pragma warning(pop)
+#endif
+    
+    TEST_F(FakeCanClientTest, SendMessage) {
+  std::vector<CanFrame> frames;
+  frames.resize(FRAME_LEN);
+  for (int32_t i = 0; i < FRAME_LEN; ++i) {
+    frames[i].id = 1 & 0x3FF;
+    frames[i].len = 8;
+    frames[i].data[7] = 1 % 256;
+    for (int32_t j = 0; j < 7; ++j) {
+      frames[i].data[j] = j;
+    }
+  }
     }
     
-    #endif
-    
-    
-    {public:
-   regex_iterator_implementation(const regex_type* p, BidirectionalIterator last, match_flag_type f)
-      : base(), end(last), re(*p), flags(f){}
-   bool init(BidirectionalIterator first)
-   {
-      base = first;
-      return regex_search(first, end, what, re, flags);
-   }
-   bool compare(const regex_iterator_implementation& that)
-   {
-      if(this == &that) return true;
-      return (&re.get_data() == &that.re.get_data()) && (end == that.end) && (flags == that.flags) && (what[0].first == that.what[0].first) && (what[0].second == that.what[0].second);
-   }
-   const match_results<BidirectionalIterator>& get()
-   { return what; }
-   bool next()
-   {
-      //if(what.prefix().first != what[0].second)
-      //   flags |= match_prev_avail;
-      BidirectionalIterator next_start = what[0].second;
-      match_flag_type f(flags);
-      if(!what.length() || (f & regex_constants::match_posix))
-         f |= regex_constants::match_not_initial_null;
-      //if(base != next_start)
-      //   f |= regex_constants::match_not_bob;
-      bool result = regex_search(next_start, end, what, re, f, base);
-      if(result)
-         what.set_base(base);
-      return result;
-   }
-private:
-   regex_iterator_implementation& operator=(const regex_iterator_implementation&);
-};
-    
-     /*
-  *   LOCATION:    see http://www.boost.org for most recent version.
-  *   FILE         regex_raw_buffer.hpp
-  *   VERSION      see <boost/version.hpp>
-  *   DESCRIPTION: Raw character buffer for regex code.
-  *                Note this is an internal header file included
-  *                by regex.hpp, do not include on its own.
+    // Synchronous transmission of CAN messages
+apollo::common::ErrorCode HermesCanClient::Send(
+    const std::vector<CanFrame> &frames, int32_t *const frame_num) {
+  /*
+  typedef struct bcan_msg {
+      uint32_t bcan_msg_id;        // source CAN node id
+      uint8_t  bcan_msg_datalen;   // message data length
+      uint8_t  bcan_msg_rsv[3];    // reserved
+      uint8_t  bcan_msg_data[8];   // message data
+      uint64_t bcan_msg_timestamp; // TBD
+  } bcan_msg_t;
   */
+  CHECK_NOTNULL(frame_num);
+  CHECK_EQ(frames.size(), static_cast<size_t>(*frame_num));
+    }
     
-    template <class BidiIterator, class Allocator, class charT, class traits>
-bool regex_search(BidiIterator first, BidiIterator last, 
-                  match_results<BidiIterator, Allocator>& m, 
-                  const basic_regex<charT, traits>& e, 
-                  match_flag_type flags,
-                  BidiIterator base)
-{
-   if(e.flags() & regex_constants::failbit)
-      return false;
+    #include 'gflags/gflags.h'
+#include 'modules/common/proto/error_code.pb.h'
+#include 'modules/drivers/canbus/can_client/can_client.h'
+#include 'modules/drivers/canbus/common/canbus_consts.h'
+#include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
+    
+    TEST(HermesCanClient, receiver) {
+  CANCardParameter param;
+  param.set_brand(CANCardParameter::HERMES_CAN);
+  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
+  HermesCanClient hermes_can;
+  EXPECT_TRUE(hermes_can.Init(param));
+    }
+    
+    template <typename SensorType>
+template <class T, bool need_check>
+void MessageManager<SensorType>::AddRecvProtocolData() {
+  recv_protocol_data_.emplace_back(new T());
+  auto *dt = recv_protocol_data_.back().get();
+  if (dt == nullptr) {
+    return;
+  }
+  protocol_data_map_[T::ID] = dt;
+  if (need_check) {
+    check_ids_[T::ID].period = dt->GetPeriod();
+    check_ids_[T::ID].real_period = 0;
+    check_ids_[T::ID].last_time = 0;
+    check_ids_[T::ID].error_count = 0;
+  }
+}
+    
+    namespace apollo {
+namespace drivers {
+namespace canbus {
+    }
+    }
     }
