747779d89c654908211c023605b7337ca17a820c
SUNsung <weatherius@gmail.com>
2018-12-03 16:10:25 +0200

    Rewrite [56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb] File in: 2018-12-03 16:10:25

--- ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
+++ ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
@@ -1,172 +1,61 @@
 
         
-            if record.timedout?(last_request_at) &&
-        !env['devise.skip_timeout'] &&
-        !proxy.remember_me_is_active?(record)
-      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
-      throw :warden, scope: scope, message: :timeout
-    end
+        module Commander
+  # This class override the run method with our custom stack trace handling
+  # In particular we want to distinguish between user_error! and crash! (one with, one without stack trace)
+  class Runner
+    # Code taken from https://github.com/commander-rb/commander/blob/master/lib/commander/runner.rb#L50
     
-            # This is called to upgrade this V1 config to V2. The parameter given
-        # is the full V2 configuration object, so you can do anything to it
-        # that you want.
-        #
-        # No return value is expected, modifications should be made directly
-        # to the new V2 object.
-        #
-        # @param [V2::Root] new
-        def upgrade(new)
-        end
+          it 'allows you to specify your own message' do
+        tag = '2.0.0'
+        message = 'message'
     
-            # This returns all the registered communicators.
-        #
-        # @return [Hash]
-        def communicators
-          result = {}
+          it 'Does not include merge commits in the list of commits' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          changelog_from_git_commits(include_merges: false)
+        end').runner.execute(:test)
     
-            # Download a file from the remote machine to the local machine.
-        #
-        # @param [String] from Path of the file on the remote machine.
-        # @param [String] to Path of where to save the file locally.
-        def download(from, to)
-        end
-    
-              # Persist through the set of invalid methods
-          this_invalid  = @__invalid_methods || Set.new
-          other_invalid = other.instance_variable_get(:'@__invalid_methods') || Set.new
-          result.instance_variable_set(:'@__invalid_methods', this_invalid + other_invalid)
-    
-        reset
+    # remove (double and single) quote pairs
+# un-double-double quote resulting string
+def simulate_windows_shell_unwrapping(string)
+  regex = /^('|')(([^'])(\S*)([^']))('|')$/
+  unless string.to_s.match(regex).nil?
+    string = string.to_s.match(regex)[2] # get only part in quotes
+    string.to_s.gsub!('''', ''') # remove double double quotes
   end
-    
-          # If the handler class requires a relative resource...
-      if (handler.relative_resource_required?)
-        # Substituted the mount point root in the request to make things
-        # relative to the mount point.
-        request.relative_resource = request.resource.gsub(/^#{root}/, '')
-        request.relative_resource = '/' + request.relative_resource if (request.relative_resource !~ /^\//)
-      end
-    
-            # Creates a connection through a Rex socket
-        #
-        # @return [Rex::Socket::Tcp]
-        # @raise [RuntimeError] if the connection can not be created
-        def connect
-          return connection if connection
-    
-              # Rex::Proto::Kerberos::Model::Checksum decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'Checksum decoding not supported'
-          end
-    
-                decode_asn1(asn1)
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::LastRequest
-          #
-          # @param input [String, OpenSSL::ASN1::Sequence] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::Sequence
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode LastRequest, invalid input'
-            end
-    
-    When /^I (?:log|sign) out manually on the mobile website$/ do
-  manual_logout_mobile
+  return string
 end
+    
+    # Show a warning for PRs that are Work In Progress
+if (github.pr_body + github.pr_title).include?('WIP')
+  warn('Pull Request is Work in Progress')
+end
+    
+    class FixPhotosShareVisibilities < ActiveRecord::Migration[4.2]
+  class Photo < ApplicationRecord
+  end
     
       failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
+    'expected #{actual.inspect} to have path #{expected.inspect} but was #{actual.current_path.inspect}'
   end
   failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
+    'expected #{actual.inspect} to not have path #{expected.inspect} but it had'
   end
 end
     
-      def navigate_to(page_name)
-    path = path_to(page_name)
-    if path.is_a?(Hash)
-      visit(path[:path])
-      await_elem = path[:special_elem]
-      find(await_elem.delete(:selector), await_elem)
-    else
-      visit(path)
-    end
-  end
-    
-    end
-    
-    class ConfigTag < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-    options = options.split(' ').map {|i| i.strip }
-    @key = options.slice!(0)
-    @tag = nil
-    @classname = nil
-    options.each do |option|
-      @tag = $1 if option =~ /tag:(\S+)/ 
-      @classname = $1 if option =~ /classname:(\S+)/
-    end
-  end
-    
-        def get_cached_gist(gist, file)
-      return nil if @cache_disabled
-      cache_file = get_cache_file_for gist, file
-      File.read cache_file if File.exist? cache_file
-    end
-    
-      # Extracts raw content DIV from template, used for page description as {{ content }}
-  # contains complete sub-template code on main page level
-  def raw_content(input)
-    /<div class='entry-content'>(?<content>[\s\S]*?)<\/div>\s*<(footer|\/article)>/ =~ input
-    return (content.nil?) ? input : content
-  end
-    
-            private
-    
-              if Cart::Update.call(order: @order, params: order_params).success?
-            user_id = params[:order][:user_id]
-            if current_api_user.has_spree_role?('admin') && user_id
-              @order.associate_user!(Spree.user_class.find(user_id))
-            end
-            respond_with(@order, default_template: :show)
-          else
-            invalid_resource!(@order)
-          end
-        end
-    
-            def payment_params
-          params.require(:payment).permit(permitted_payment_attributes)
-        end
-      end
+        it 'generates a jasmine fixture', fixture: true do
+      session[:mobile_view] = true
+      get :new, format: :mobile
+      save_fixture(html_for('body'), 'conversations_new_mobile')
     end
   end
 end
 
     
-            def update
-          if @property
-            authorize! :update, @property
-            @property.update_attributes(property_params)
-            respond_with(@property, status: 200, default_template: :show)
-          else
-            invalid_resource!(@property)
-          end
-        end
-    
-            def update
-          authorize! :update, stock_location
-          if stock_location.update_attributes(stock_location_params)
-            respond_with(stock_location, status: 200, default_template: :show)
-          else
-            invalid_resource!(stock_location)
-          end
-        end
+          it 'should not create the participation' do
+        post :create, params: {post_id: @post.id}
+        expect(alice.participations.where(:target_id => @post.id)).not_to exist
+        expect(response.code).to eq('403')
+      end
+    end
+  end
