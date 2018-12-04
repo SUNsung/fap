b06e78651431d53b43c63f7f22bb55a09f6c9372
SUNsung <weatherius@gmail.com>
2018-12-04 23:28:53 +0200

    Rewrite [10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp] File in: 2018-12-04 23:28:52

--- cccccc/10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp
+++ cccccc/10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp
@@ -1,191 +1,127 @@
 
         
-          static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
+        
     
+    #ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable: 4103)
+#endif
+#ifdef BOOST_HAS_ABI_HEADERS
+#  include BOOST_ABI_PREFIX
+#endif
+#ifdef BOOST_MSVC
+#pragma warning(pop)
+#endif
     
-    {}  // namespace mate
+     /*
+  *   LOCATION:    see http://www.boost.org for most recent version.
+  *   FILE         regex_raw_buffer.hpp
+  *   VERSION      see <boost/version.hpp>
+  *   DESCRIPTION: Raw character buffer for regex code.
+  *                Note this is an internal header file included
+  *                by regex.hpp, do not include on its own.
+  */
     
-      // Override download::DownloadItem::Observer methods
-  void OnDownloadUpdated(download::DownloadItem* download) override;
-  void OnDownloadDestroyed(download::DownloadItem* download) override;
-    
-    namespace atom {
-    }
-    
-    
-    {}  // namespace atom
-    
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-    namespace api {
-    }
-    
-     protected:
-  Tray(v8::Isolate* isolate,
-       v8::Local<v8::Object> wrapper,
-       mate::Handle<NativeImage> image);
-  ~Tray() override;
-    
-    namespace api {
-    }
-    
-    v8::Local<v8::Object> CreateJSEvent(v8::Isolate* isolate,
-                                    v8::Local<v8::Object> object,
-                                    content::RenderFrameHost* sender,
-                                    IPC::Message* message);
-v8::Local<v8::Object> CreateCustomEvent(v8::Isolate* isolate,
-                                        v8::Local<v8::Object> object,
-                                        v8::Local<v8::Object> event);
-v8::Local<v8::Object> CreateEventFromFlags(v8::Isolate* isolate, int flags);
-    
-    #endif // SWIFT_AST_SUBSTITUTION_MAP_STORAGE_H
-
-    
-      while ((!QueuedTasks.empty() && !SubtaskFailed) ||
-         !ExecutingTasks.empty()) {
-    // Enqueue additional tasks if we have additional tasks, we aren't already
-    // at the parallel limit, and no earlier subtasks have failed.
-    while (!SubtaskFailed && !QueuedTasks.empty() &&
-           ExecutingTasks.size() < MaxNumberOfParallelTasks) {
-      std::unique_ptr<DummyTask> T(QueuedTasks.front().release());
-      QueuedTasks.pop();
-    }
-    }
-    
-    class SignVerifyMessageDialog : public QDialog
+    template <class traits, class charT, class Formatter>
+std::basic_string<charT> regex_replace(const std::basic_string<charT>& s,
+                         const basic_regex<charT, traits>& e, 
+                         Formatter fmt,
+                         match_flag_type flags = match_default)
 {
-    Q_OBJECT
-    }
-    
-    /** Check bounds on a command line confirm target */
-unsigned int ParseConfirmTarget(const UniValue& value);
-    
-        while (outlen > 0) {
-        secp256k1_hmac_sha256_t hmac;
-        int now = outlen;
-        secp256k1_hmac_sha256_initialize(&hmac, rng->k, 32);
-        secp256k1_hmac_sha256_write(&hmac, rng->v, 32);
-        secp256k1_hmac_sha256_finalize(&hmac, rng->v);
-        if (now > 32) {
-            now = 32;
-        }
-        memcpy(out, rng->v, now);
-        out += now;
-        outlen -= now;
-    }
-    
-    
-    {
-    {}  // namespace internal
-}  // namespace testing
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26>
-internal::ValueArray26<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13,
-    T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-    T26> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
-    T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
-    T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
-    T26 v26) {
-  return internal::ValueArray26<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
-      T12, T13, T14, T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25,
-      T26>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15,
-      v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26);
-}
-    
-    // IsAProtocolMessage<T>::value is a compile-time bool constant that's
-// true iff T is type ProtocolMessage, proto2::Message, or a subclass
-// of those.
-template <typename T>
-struct IsAProtocolMessage
-    : public bool_constant<
-  ImplicitlyConvertible<const T*, const ::ProtocolMessage*>::value ||
-  ImplicitlyConvertible<const T*, const ::proto2::Message*>::value> {
-};
-    
-      // Converts a wide C string to a String using the UTF-8 encoding.
-  // NULL will be converted to '(null)'.  If an error occurred during
-  // the conversion, '(failed to convert from wide string)' is
-  // returned.
-  static std::string ShowWideCString(const wchar_t* wide_c_str);
-    
-    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
-    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
-    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
-    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
-    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
-    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17, GTEST_TEMPLATE_ T18,
-    GTEST_TEMPLATE_ T19, GTEST_TEMPLATE_ T20, GTEST_TEMPLATE_ T21,
-    GTEST_TEMPLATE_ T22, GTEST_TEMPLATE_ T23, GTEST_TEMPLATE_ T24,
-    GTEST_TEMPLATE_ T25, GTEST_TEMPLATE_ T26, GTEST_TEMPLATE_ T27,
-    GTEST_TEMPLATE_ T28, GTEST_TEMPLATE_ T29, GTEST_TEMPLATE_ T30,
-    GTEST_TEMPLATE_ T31, GTEST_TEMPLATE_ T32, GTEST_TEMPLATE_ T33,
-    GTEST_TEMPLATE_ T34, GTEST_TEMPLATE_ T35, GTEST_TEMPLATE_ T36>
-struct Templates36 {
-  typedef TemplateSel<T1> Head;
-  typedef Templates35<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
-      T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
-      T29, T30, T31, T32, T33, T34, T35, T36> Tail;
-};
-    
-    // The template 'selector' struct TemplateSel<Tmpl> is used to
-// represent Tmpl, which must be a class template with one type
-// parameter, as a type.  TemplateSel<Tmpl>::Bind<T>::type is defined
-// as the type Tmpl<T>.  This allows us to actually instantiate the
-// template 'selected' by TemplateSel<Tmpl>.
-//
-// This trick is necessary for simulating typedef for class templates,
-// which C++ doesn't support directly.
-template <GTEST_TEMPLATE_ Tmpl>
-struct TemplateSel {
-  template <typename T>
-  struct Bind {
-    typedef Tmpl<T> type;
-  };
-};
-    
-      // Try to divide n by every odd number i, starting from 3
-  for (int i = 3; ; i += 2) {
-    // We only have to try i up to the squre root of n
-    if (i > n/i) break;
-    }
-    
-      // If we are given the --check_for_leaks command line flag, installs the
-  // leak checker.
-  if (check_for_leaks) {
-    TestEventListeners& listeners = UnitTest::GetInstance()->listeners();
-    }
-    
-    
-    {  // Sets the 0-terminated C string this MyString object represents.
-  void Set(const char* c_string);
-};
-    
-    void ConstraintBullet::destroy_internal_constraint() {
-	space->remove_constraint(this);
-}
-    
-    #ifndef HINGE_JOINT_BULLET_H
-#define HINGE_JOINT_BULLET_H
-    
-    	void setPivotInA(const Vector3 &p_pos);
-	void setPivotInB(const Vector3 &p_pos);
-    
-    
-    {	resource_loader_dds = memnew(ResourceFormatDDS);
-	ResourceLoader::add_resource_format_loader(resource_loader_dds);
+   std::basic_string<charT> result;
+   BOOST_REGEX_DETAIL_NS::string_out_iterator<std::basic_string<charT> > i(result);
+   regex_replace(i, s.begin(), s.end(), e, fmt, flags);
+   return result;
 }
     
     
-    {		allocs[i].free_list = &allocs[i + 1];
+    {   match_results<BidiIterator> m;
+   typedef typename match_results<BidiIterator>::allocator_type match_alloc_type;
+   BOOST_REGEX_DETAIL_NS::perl_matcher<BidiIterator, match_alloc_type, traits> matcher(first, last, m, e, flags | regex_constants::match_any, first);
+   return matcher.find();
+}
+    
+    		virtual Future<Reference<DirectorySubspace>> move(Reference<Transaction> const& tr, Path const& oldPath, Path const& newPath);
+		virtual Future<Reference<DirectorySubspace>> moveTo(Reference<Transaction> const& tr, Path const& newAbsolutePath);
+    
+    #include 'Subspace.h'
+    
+    		virtual Future<bool> exists(Reference<Transaction> const& tr, Path const& path = Path()) = 0;
+		virtual Future<Standalone<VectorRef<StringRef>>> list(Reference<Transaction> const& tr, Path const& path = Path()) = 0;
+    
+    
+    {		return *n;
 	}
     
-    	if (mode & ZLIB_FILEFUNC_MODE_WRITE) {
-		return NULL;
+    	bool Tuple::getBool(size_t index) const {
+		if(index >= offsets.size()) {
+			throw invalid_tuple_index();
+		}
+		ASSERT(offsets[index] < data.size());
+		uint8_t code = data[offsets[index]];
+		if(code == FALSE_CODE) {
+			return false;
+		} else if(code == TRUE_CODE) {
+			return true;
+		} else {
+			throw invalid_tuple_data_type();
+		}
+	}
+    
+    
+    {    Reference<class IThreadPool> threadPool;
+    struct LineNoiseReader* reader;
+};
+    
+    
+    {		void logEvent(std::string id) const {
+			TraceEvent('TransactionTrace_GetRange').detail('TransactionID', id).detail('Latency', latency).detail('RangeSizeBytes', rangeSize).detail('StartKey', printable(startKey)).detail('EndKey', printable(endKey));
+		}
 	};
+    
+    	void setMappedFuture(ErrorOr<ThreadFuture<T>> f) {
+		if(f.isError()) {
+			sendResult(f.getError());
+		}
+		else {
+			lock.enter();
+			mappedFuture = f.get();
+			bool doCancel = cancelled;
+			bool doRelease = released;
+			lock.leave();
+    }
+    }
+    
+            void output( const String_type& s )
+        {
+            os_ << ''' << add_esc_chars( s, raw_utf8_, esc_nonascii_ ) << ''';
+        }
+    
+    #include 'flow/Platform.h'
+#include 'flow/IRandom.h'
+#include <vector>
+#include <algorithm>
+    
+    
+    {	GlobalCounters() : 
+		conflictTime('Conflict detection time', doubles),
+		conflictBatches('Conflict batches', ints),
+		conflictKeys('Conflict keys', ints),
+		conflictTransactions('Conflict transactions', ints)
+	{
+	}
+	void clear() {
+		for(int i=0; i<ints.size(); i++)
+			ints[i]->clear();
+		for(int i=0; i<doubles.size(); i++)
+			doubles[i]->clear();
+	}
+};
+    
+    // Returns the absolute platform-dependant path for server-based files
+std::string getDefaultConfigPath();
+    
+    
+    {	double eFoldingTime;
+	double time, total, estimate;
+};
