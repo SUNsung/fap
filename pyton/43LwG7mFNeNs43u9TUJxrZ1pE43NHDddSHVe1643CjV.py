7573d5e57796674b15c7d88c1def56742bcc95fe
SUNsung <weatherius@gmail.com>
2018-12-04 18:03:29 +0200

    Rewrite [43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py] File in: 2018-12-04 18:03:28

--- pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
+++ pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
@@ -1,69 +1,26 @@
 
         
-            def escalate_call(self):
-        raise NotImplemented('Directors must be able to handle any call')
-    
-        def __init__(self, num_levels):
-        self.num_levels = num_levels
-        self.levels = []  # List of Levels
-    
-    
-class Cache(object):
+            with open('README.md', 'w+') as sorted_file:
+        # Then all of the blocks are sorted individually
+        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
+        # And the result is written back to README.md
+        sorted_file.write(''.join(blocks))
     
     
-@pytest.mark.parametrize('path', (
-    '/create',
-    '/1/update',
-    '/1/delete',
-))
-def test_login_required(client, path):
-    response = client.post(path)
-    assert response.headers['Location'] == 'http://localhost/auth/login'
+class DefaultCategories(Enum):
     
-    PY2 = sys.version_info[0] == 2
-_identity = lambda x: x
+            (foo, p1), 2
+        (bar, p1), 2
+        (bar, p1), 1
+        (foo, p2), 3
+        (bar, p3), 10
+        (foo, p4), 1
+        '''
+        timestamp, product_id, category, quantity = line.split('\t')
+        if self.within_past_week(timestamp):
+            yield (category, product_id), quantity
     
-    from .helpers import _PackageBoundObject, _endpoint_from_view_func
-    
-            If you want to open the :file:`schema.sql` file you would do the
-        following::
-    
-        If you configure your own :class:`logging.StreamHandler`, you may want to
-    use this for the stream. If you are using file or dict configuration and
-    can't import this directly, you can refer to it as
-    ``ext://flask.logging.wsgi_errors_stream``.
-    '''
-    return request.environ['wsgi.errors'] if request else sys.stderr
-    
-    
-class SecureCookieSessionInterface(SessionInterface):
-    '''The default session interface that stores sessions in signed cookies
-    through the :mod:`itsdangerous` module.
-    '''
-    #: the salt that should be applied on top of the secret key for the
-    #: signing of cookie based sessions.
-    salt = 'cookie-session'
-    #: the hash function to use for the signature.  The default is sha1
-    digest_method = staticmethod(hashlib.sha1)
-    #: the name of the itsdangerous supported key derivation.  The default
-    #: is hmac.
-    key_derivation = 'hmac'
-    #: A python serializer for the payload.  The default is a compact
-    #: JSON derived serializer with support for some extra Python types
-    #: such as datetime objects or tuples.
-    serializer = session_json_serializer
-    session_class = SecureCookieSession
-    
-    
-class RC4FileObject(object):
-    '''fileobj for rc4'''
-    def __init__(self, stream, key):
-        self.__stream = stream
-        self.__cipher = _Crypto_Cipher_ARC4_new(key) if key else lambda x:x
-    def __getattr__(self, attr):
-        if attr not in ('__stream', '__cipher'):
-            return getattr(self.__stream, attr)
-    def read(self, size=-1):
-        return self.__cipher.encrypt(self.__stream.read(size))
-    
-    import urllib
+        if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print('Choose one path as argument one')
