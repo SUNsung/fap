de66f82cf1a6ebaf9ee67adfc5e5dcb2f6a57f75
SUNsung <weatherius@gmail.com>
2018-12-04 08:43:23 +0200

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2018-12-04 08:43:22

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,226 +1,110 @@
 
         
         
-class RedirectFallbackMiddleware(MiddlewareMixin):
-    # Defined as class-level attributes to be subclassing-friendly.
-    response_gone_class = HttpResponseGone
-    response_redirect_class = HttpResponsePermanentRedirect
-    
-    from django.conf import settings
-from django.contrib.sessions.backends.db import SessionStore as DBStore
-from django.core.cache import caches
-    
-        def exists(self, session_key):
-        return self.model.objects.filter(session_key=session_key).exists()
-    
-        def load(self):
-        '''
-        Load the data from the key itself instead of fetching from some
-        external data store. Opposite of _get_session_key(), raise BadSignature
-        if signature fails.
-        '''
-        try:
-            return signing.loads(
-                self.session_key,
-                serializer=self.serializer,
-                # This doesn't handle non-default expiry dates, see #19201
-                max_age=settings.SESSION_COOKIE_AGE,
-                salt='django.contrib.sessions.backends.signed_cookies',
-            )
-        except Exception:
-            # BadSignature, ValueError, or unpickling exceptions. If any of
-            # these happen, reset the session.
-            self.create()
-        return {}
-    
-            volume_id = volumes[0].id
-    
-        vpc_id = module.params.get('vpc_id')
-    state = module.params.get('state')
-    
-    
-def get_elasticache_clusters(client, module, region):
+# Is a path a directory?
+# This follows symbolic links, so both islink() and isdir()
+# can be true for the same path on systems that support symlinks
+def isdir(s):
+    '''Return true if the pathname refers to an existing directory.'''
     try:
-        clusters = describe_cache_clusters_with_backoff(client, cluster_id=module.params.get('name'))
-    except (botocore.exceptions.ClientError, botocore.exceptions.BotoCoreError) as e:
-        module.fail_json_aws(e, msg='Couldn't obtain cache cluster info')
+        st = os.stat(s)
+    except (OSError, ValueError):
+        return False
+    return stat.S_ISDIR(st.st_mode)
     
-        function_name = module.params.get('function_name')
-    if function_name:
-        try:
-            # get_policy returns a JSON string so must convert to dict before reassigning to its key
-            lambda_facts.update(policy=json.loads(client.get_policy(FunctionName=function_name)['Policy']))
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                lambda_facts.update(policy={})
-            else:
-                module.fail_json_aws(e, msg='Trying to get {0} policy'.format(function_name))
-    else:
-        module.fail_json(msg='Parameter function_name required for query=policy.')
-    
-    DOCUMENTATION = '''
----
-module: rds_snapshot_facts
-version_added: '2.6'
-short_description: obtain facts about one or more RDS snapshots
-description:
-  - obtain facts about one or more RDS snapshots. These can be for unclustered snapshots or snapshots of clustered DBs (Aurora)
-  - Aurora snapshot facts may be obtained if no identifier parameters are passed or if one of the cluster parameters are passed.
-options:
-  db_snapshot_identifier:
-    description:
-      - Name of an RDS (unclustered) snapshot. Mutually exclusive with I(db_instance_identifier), I(db_cluster_identifier), I(db_cluster_snapshot_identifier)
-    required: false
-    aliases:
-      - snapshot_name
-  db_instance_identifier:
-    description:
-      - RDS instance name for which to find snapshots. Mutually exclusive with I(db_snapshot_identifier), I(db_cluster_identifier),
-        I(db_cluster_snapshot_identifier)
-    required: false
-  db_cluster_identifier:
-    description:
-      - RDS cluster name for which to find snapshots. Mutually exclusive with I(db_snapshot_identifier), I(db_instance_identifier),
-        I(db_cluster_snapshot_identifier)
-    required: false
-  db_cluster_snapshot_identifier:
-    description:
-      - Name of an RDS cluster snapshot. Mutually exclusive with I(db_instance_identifier), I(db_snapshot_identifier), I(db_cluster_identifier)
-    required: false
-  snapshot_type:
-    description:
-      - Type of snapshot to find. By default both automated and manual
-        snapshots will be returned.
-    required: false
-    choices: ['automated', 'manual', 'shared', 'public']
-requirements:
-    - 'python >= 2.6'
-    - 'boto3'
-author:
-    - 'Will Thames (@willthames)'
-extends_documentation_fragment:
-    - aws
-    - ec2
-'''
-    
-        client = module.client('route53')
-    
-        try:
-        client_connection.get_bucket_website(Bucket=bucket_name)
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'NoSuchWebsiteConfiguration':
-            module.exit_json(changed=changed)
+        def splitroot(self, part, sep=sep):
+        first = part[0:1]
+        second = part[1:2]
+        if (second == sep and first == sep):
+            # XXX extended paths should also disable the collapsing of '.'
+            # components (according to MSDN docs).
+            prefix, part = self._split_extended_path(part)
+            first = part[0:1]
+            second = part[1:2]
         else:
-            module.fail_json(msg=e.message, **camel_dict_to_snake_dict(e.response))
+            prefix = ''
+        third = part[2:3]
+        if (second == sep and first == sep and third != sep):
+            # is a UNC path:
+            # vvvvvvvvvvvvvvvvvvvvv root
+            # \\machine\mountpoint\directory\etc\...
+            #            directory ^^^^^^^^^^^^^^
+            index = part.find(sep, 2)
+            if index != -1:
+                index2 = part.find(sep, index + 1)
+                # a UNC path can't have two slashes in a row
+                # (after the initial two)
+                if index2 != index + 1:
+                    if index2 == -1:
+                        index2 = len(part)
+                    if prefix:
+                        return prefix + part[1:index2], sep, part[index2+1:]
+                    else:
+                        return part[:index2], sep, part[index2+1:]
+        drv = root = ''
+        if second == ':' and first in self.drive_letters:
+            drv = part[:2]
+            part = part[2:]
+            first = third
+        if first == sep:
+            root = first
+            part = part.lstrip(sep)
+        return prefix + drv, root, part
     
-        def get_item(self):
-        '''Get a single Azure Kubernetes Service'''
+    def islink(path):
+    '''Test whether a path is a symbolic link'''
+    try:
+        st = os.lstat(path)
+    except (OSError, ValueError, AttributeError):
+        return False
+    return stat.S_ISLNK(st.st_mode)
     
-        def report_out_of_quota(self, appid):
-        self.logger.warn('report_out_of_quota:%s', appid)
-        with self.lock:
-            if appid not in self.out_of_quota_appids:
-                self.out_of_quota_appids.append(appid)
-            try:
-                self.working_appid_list.remove(appid)
-            except:
-                pass
-    
-    
-class SniManager(object):
-    plus = ['-', '', '.']
-    end = ['com', 'net', 'ml', 'org', 'us']
-    
-    ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
-    
-        def __str__(self):
-        return 'MismatchedRangeException(%r not in [%r..%r])' % (
-            self.getUnexpectedType(), self.a, self.b
-            )
-    __repr__ = __str__
-    
-    
-        
-    def __init__(self, data):
-        '''
-        @param data This should be a unicode string holding the data you want
-           to parse. If you pass in a byte string, the Lexer will choke on
-           non-ascii data.
-           
-        '''
-        
-        CharStream.__init__(self)
-        
-  	# The data being scanned
-        self.strdata = unicode(data)
-        self.data = [ord(c) for c in self.strdata]
-        
-	# How many characters are actually in the buffer
-        self.n = len(data)
-    
-    
-    def __str__(self):
-        if self.type == EOF:
-            return '<EOF>'
-    
-    __all__ = ['normcase','isabs','join','splitdrive','split','splitext',
-           'basename','dirname','commonprefix','getsize','getmtime',
-           'getatime','getctime','islink','exists','lexists','isdir','isfile',
-           'ismount', 'expanduser','expandvars','normpath','abspath',
-           'samefile','sameopenfile','samestat',
-           'curdir','pardir','sep','pathsep','defpath','altsep','extsep',
-           'devnull','realpath','supports_unicode_filenames','relpath',
-           'commonpath']
-    
-        def test_normcase(self):
-        normcase = self.pathmodule.normcase
-        # check that normcase() is idempotent
-        for p in ['FoO/./BaR', b'FoO/./BaR']:
-            p = normcase(p)
-            self.assertEqual(p, normcase(p))
-    
-            self.assertIs(posixpath.isabs(b''), False)
-        self.assertIs(posixpath.isabs(b'/'), True)
-        self.assertIs(posixpath.isabs(b'/foo'), True)
-        self.assertIs(posixpath.isabs(b'/foo/bar'), True)
-        self.assertIs(posixpath.isabs(b'foo/bar'), False)
+            self.assertRaises(TypeError, posixpath.commonpath,
+                          [b'/usr/lib/', '/usr/lib/python3'])
+        self.assertRaises(TypeError, posixpath.commonpath,
+                          [b'/usr/lib/', 'usr/lib/python3'])
+        self.assertRaises(TypeError, posixpath.commonpath,
+                          [b'usr/lib/', '/usr/lib/python3'])
+        self.assertRaises(TypeError, posixpath.commonpath,
+                          ['/usr/lib/', b'/usr/lib/python3'])
+        self.assertRaises(TypeError, posixpath.commonpath,
+                          ['/usr/lib/', b'usr/lib/python3'])
+        self.assertRaises(TypeError, posixpath.commonpath,
+                          ['usr/lib/', b'/usr/lib/python3'])
     
     
-class BZ2CompressorTest(BaseTest):
-    def testCompress(self):
-        bz2c = BZ2Compressor()
-        self.assertRaises(TypeError, bz2c.compress)
-        data = bz2c.compress(self.TEXT)
-        data += bz2c.flush()
-        self.assertEqual(ext_decompress(data), self.TEXT)
+if __name__ == '__main__':
+    main()
+
     
-    # Add the html version.  This converts the message into a multipart/alternative
-# container, with the original text message as the first part and the new html
-# message as the second part.
-asparagus_cid = make_msgid()
-msg.add_alternative('''\
-<html>
-  <head></head>
-  <body>
-    <p>Salut!</p>
-    <p>Cela ressemble à un excellent
-        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
-            recipie
-        </a> déjeuner.
-    </p>
-    <img src='cid:{asparagus_cid}' />
-  </body>
-</html>
-'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
-# note that we needed to peel the <> off the msgid for use in the html.
+    ans = input('View full message?')
+if ans.lower()[0] == 'n':
+    sys.exit()
     
-    # If we want to print a preview of the message content, we can extract whatever
-# the least formatted payload is and print the first three lines.  Of course,
-# if the message has no plain text part printing the first three lines of html
-# is probably useless, but this is just a conceptual example.
-simplest = msg.get_body(preferencelist=('plain', 'html'))
-print()
-print(''.join(simplest.get_content().splitlines(keepends=True)[:3]))
+    
+class Action(object):
+    def __init__(self, name):
+        self.name = name
+    
+        def tearDown(self):
+        if not self._bProblem:
+            print('Tearing down')
+            time.sleep(0.1)
+            self._tm.publishReport()
+        else:
+            print('Test not executed. No tear down required.')
+    
+    *References:
+http://stackoverflow.com/questions/1514120/python-implementation-of-the-object-pool-design-pattern
+https://sourcemaking.com/design_patterns/object_pool
+    
+        def get_current_time_as_html_fragment(self):
+        current_time = self.time_provider.now()
+        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
+        return current_time_as_html_fragment
+    
+        def get_current_time_as_html_fragment(self):
+        current_time = self.time_provider.now()
+        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
+        return current_time_as_html_fragment
+'''
