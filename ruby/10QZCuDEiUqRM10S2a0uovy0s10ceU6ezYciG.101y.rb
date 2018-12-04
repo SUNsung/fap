6bd8a89b7450a858cf1403c9ad51306dd4fa5110
SUNsung <weatherius@gmail.com>
2018-12-04 05:46:10 +0200

    Rewrite [10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb] File in: 2018-12-04 05:46:09

--- ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
+++ ruby/10QZCuDEiUqRM10S2a0uovy0s10ceU6ezYciG.101y.rb
@@ -1,6 +1,66 @@
 
         
-            odie 'Unknown command: #{cmd}' unless path
-    puts path
+        def prev_version
+  return prev_feature + '.0' if source_version.end_with? '.0'
+  source_version.gsub(/\d+$/) { |s| s.to_i - 1 }
+end
+    
+      table td.code       {width:750px}
+  table td.code div   {width:750px;overflow:hidden}
+</style>
+</head>
+<body>
+  <div id='wrap'>
+    <div id='header'>
+      <img src='<%= env['SCRIPT_NAME'] %>/__sinatra__/500.png' alt='application error' height='161' width='313' />
+      <div id='summary'>
+        <h1><strong><%=h exception.class %></strong> at <strong><%=h path %>
+          </strong></h1>
+        <h2><%=h exception.message %></h2>
+        <ul>
+          <li class='first'><strong>file:</strong> <code>
+            <%=h frames.first.filename.split('/').last %></code></li>
+          <li><strong>location:</strong> <code><%=h frames.first.function %>
+            </code></li>
+          <li class='last'><strong>line:
+            </strong> <%=h frames.first.lineno %></li>
+        </ul>
+      </div>
+      <div class='clear'></div>
+    </div>
+    
+          def has_vector?(request, headers)
+        return false if request.xhr?
+        return false if options[:allow_if] && options[:allow_if].call(request.env)
+        return false unless headers['Content-Type'].to_s.split(';', 2).first =~ /^\s*application\/json\s*$/
+        origin(request.env).nil? and referrer(request.env) != request.host
+      end
+    
+      def validate_cache_location
+    cache_location = LogStash::Environment::CACHE_PATH
+    if File.exist?(cache_location)
+      puts('Directory #{cache_location} is going to be overwritten, do you want to continue? (Y/N)')
+      override = ( 'y' == STDIN.gets.strip.downcase ? true : false)
+      if override
+        FileUtils.rm_rf(cache_location)
+      else
+        puts('Unpack cancelled: file #{cache_location} already exists, please delete or move it')
+        exit
+      end
+    end
+  end
+end
+
+    
+    class LogStash::PluginManager::Update < LogStash::PluginManager::Command
+  REJECTED_OPTIONS = [:path, :git, :github]
+  # These are local gems used by LS and needs to be filtered out of other plugin gems
+  NON_PLUGIN_LOCAL_GEMS = ['logstash-core', 'logstash-core-plugin-api']
+    
+        context 'when is not a system pipeline' do
+      it 'returns false if the pipeline is not a system pipeline' do
+        expect(subject.system?).to be_falsey
+      end
+    end
   end
 end
