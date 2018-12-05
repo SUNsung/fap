54d2a561ee5c38dc79e1724d13ff2a10c44a359f
SUNsung <weatherius@gmail.com>
2018-12-05 13:22:12 +0200

    Rewrite [06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp] File in: 2018-12-05 13:22:11

--- cccccc/06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp
+++ cccccc/06SmRr8sizEMc068kC1sjeo1Ko064esCRSiQYvU06dzn.cpp
@@ -1,266 +1,72 @@
 
         
-        #ifndef NDEBUG
-/// Verify that the types of fields are valid within a given generic signature.
-static void verifyFields(CanGenericSignature Sig, ArrayRef<SILField> Fields) {
-  for (auto &field : Fields) {
-    auto ty = field.getLoweredType();
-    // Layouts should never refer to archetypes, since they represent an
-    // abstract generic type layout.
-    assert(!ty->hasArchetype()
-           && 'SILLayout field cannot have an archetype type');
-    assert(!ty->hasTypeVariable()
-           && 'SILLayout cannot contain constraint system type variables');
-    if (!ty->hasTypeParameter())
-      continue;
-    field.getLoweredType().findIf([Sig](Type t) -> bool {
-      if (auto gpt = t->getAs<GenericTypeParamType>()) {
-        // Check that the generic param exists in the generic signature.
-        assert(Sig && 'generic param in nongeneric layout?');
-        assert(std::find(Sig.getGenericParams().begin(),
-                         Sig.getGenericParams().end(),
-                         gpt->getCanonicalType()) != Sig.getGenericParams().end()
-               && 'generic param not declared in generic signature?!');
-      }
-      return false;
-    });
-  }
-}
-#endif
-    
-      size_t numTrailingObjects(OverloadToken<Type>) const {
-    return getNumReplacementTypes();
+          // Deletes the current owned object, if any.
+  // Then takes ownership of a new object without incrementing the reference
+  // count.
+  // This function must be called with a reference that you own.
+  //   this->reset(this->get()) is wrong!
+  //   this->reset(this->release()) is OK.
+  PyObjectStruct* reset(PyObjectStruct* p = NULL) {
+    Py_XDECREF(ptr_);
+    ptr_ = p;
+    return ptr_;
   }
     
-      if (triple.getOS() == llvm::Triple::Darwin &&
-      triple.getVendor() == llvm::Triple::Apple) {
-    // Rewrite darwinX.Y triples to macosx10.X'.Y ones.
-    // It affects code generation on our platform.
-    llvm::SmallString<16> osxBuf;
-    llvm::raw_svector_ostream osx(osxBuf);
-    osx << llvm::Triple::getOSTypeName(llvm::Triple::MacOSX);
-    }
+      template<class T>
+  OneofDef* FindOneofByName(const T& str) {
+    return FindOneofByName(str.c_str(), str.size());
+  }
+  template<class T>
+  const OneofDef* FindOneofByName(const T& str) const {
+    return FindOneofByName(str.c_str(), str.size());
+  }
     
-    bool swift::canBeMemberName(StringRef identifier) {
-  return llvm::StringSwitch<bool>(identifier)
-    .Case('init', false)
-    .Case('Protocol', false)
-    .Case('self', false)
-    .Case('Type', false)
-    .Default(true);
+    void AnyMetadata::PackFrom(const Message& message) {
+  PackFrom(message, kTypeGoogleApisComPrefix);
 }
     
-    #if PY_MAJOR_VERSION >= 3
-static struct PyModuleDef _module = {
-  PyModuleDef_HEAD_INIT,
-  kModuleName,
-  kModuleDocstring,
-  -1,
-  NULL,
-  NULL,
-  NULL,
-  NULL,
-  NULL
-};
-#define INITFUNC PyInit__api_implementation
-#define INITFUNC_ERRORVAL NULL
-#else
-#define INITFUNC init_api_implementation
-#define INITFUNC_ERRORVAL
-#endif
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
     
-      // True when a ScopedPyObjectPtr and a raw pointer refer to the same object.
-  // Comparison operators are non reflexive.
-  bool operator==(const PyObjectStruct* p) const { return ptr_ == p; }
-  bool operator!=(const PyObjectStruct* p) const { return ptr_ != p; }
-    
-    #include <google/protobuf/compiler/csharp/csharp_doc_comment.h>
-#include <google/protobuf/compiler/csharp/csharp_enum.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_options.h>
-    
-    void Context::InitializeFieldGeneratorInfoForMessage(
-    const Descriptor* message) {
-  for (int i = 0; i < message->nested_type_count(); ++i) {
-    InitializeFieldGeneratorInfoForMessage(message->nested_type(i));
-  }
-  std::vector<const FieldDescriptor*> fields;
-  for (int i = 0; i < message->field_count(); ++i) {
-    fields.push_back(message->field(i));
-  }
-  InitializeFieldGeneratorInfoForFields(fields);
+      if (GenerateCode(request, *generator, &response, &error_msg)) {
+    if (!response.SerializeToFileDescriptor(STDOUT_FILENO)) {
+      std::cerr << argv[0] << ': Error writing to stdout.' << std::endl;
+      return 1;
     }
-    
-      string::size_type pos = result.find_first_of('\n');
-  if (pos != string::npos) {
-    result.erase(pos);
-  }
-    
-    #include <google/protobuf/compiler/java/java_generator_factory.h>
-    
-    string ClassNameResolver::GetFileDefaultImmutableClassName(
-    const FileDescriptor* file) {
-  string basename;
-  string::size_type last_slash = file->name().find_last_of('/');
-  if (last_slash == string::npos) {
-    basename = file->name();
   } else {
-    basename = file->name().substr(last_slash + 1);
+    if (!error_msg.empty()) {
+      std::cerr << argv[0] << ': ' << error_msg << std::endl;
+    }
+    return 1;
   }
-  return UnderscoresToCamelCase(StripProto(basename), true);
-}
     
-    #ifndef GOOGLE_PROTOBUF_COMPILER_OBJECTIVEC_EXTENSION_H__
-#define GOOGLE_PROTOBUF_COMPILER_OBJECTIVEC_EXTENSION_H__
+      // Will be called while on the write thread before the write executes.  If
+  // this function returns a non-OK status, the write will be aborted and this
+  // status will be returned to the caller of DB::Write().
+  virtual Status Callback(DB* db) = 0;
     
-    std::string tmppath() {
-  // TMPFILE is for manual test execution during which the user will specify
-  // the full temp file path using the environmental variable TMPFILE
-  const char* tmpfile = getenv('TMPFILE');
-  if (tmpfile) {
-    return std::string(tmpfile);
-  }
-    }
+      Status s = OptimisticTransactionDB::Open(options, kDBPath, &txn_db);
+  assert(s.ok());
+  db = txn_db->GetBaseDB();
     
-    SHOULD_NOT_DO_GRADIENT(EnforceFinite);
+      // initialize column families options
+  std::unique_ptr<CompactionFilter> compaction_filter;
+  compaction_filter.reset(new DummyCompactionFilter());
+  cf_descs[0].options.table_factory.reset(NewBlockBasedTableFactory(bbt_opts));
+  cf_descs[0].options.compaction_filter = compaction_filter.get();
+  cf_descs[1].options.table_factory.reset(NewBlockBasedTableFactory(bbt_opts));
     
-    ```
+    // Supported only for Leveled compaction
+Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
+                           const Slice* begin, const Slice* end);
+Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
     
-    namespace caffe2 {
-    }
-    
-    
-    {} // namespace caffe2
-    
-    ```
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    APCHandle::Pair APCCollection::WrapArray(APCHandle::Pair inner,
-                                         CollectionType colType) {
-  auto const col = new APCCollection;
-  col->m_arrayHandle = inner.handle;
-  col->m_colType = colType;
-  return { &col->m_handle, inner.size + sizeof(APCCollection) };
-}
-    
-    
-    {private:
-  APCHandle m_handle;
-  APCHandle* m_arrayHandle;
-  CollectionType m_colType;
-};
-    
-    struct FatalErrorException : ExtendedException {
-  explicit FatalErrorException(const char *msg)
-    : ExtendedException('%s', msg)
-  {}
-  FatalErrorException(int, ATTRIBUTE_PRINTF_STRING const char *msg, ...)
-    ATTRIBUTE_PRINTF(3,4);
-  FatalErrorException(const std::string&, const Array& backtrace,
-                      bool isRecoverable = false);
-    }
-    
-    inline TypedValue ExecutionContext::invokeMethod(
-  ObjectData* obj,
-  const Func* meth,
-  InvokeArgs args,
-  bool dynamic
-) {
-  return invokeFuncFew(
-    meth,
-    ActRec::encodeThis(obj),
-    nullptr /* invName */,
-    args.size(),
-    args.start(),
-    dynamic
-  );
-}
-    
-        // skipping 'tags' files
-    if (strcmp(ename, 'tags') == 0) {
-      continue;
-    }
-    
-    
-    {///////////////////////////////////////////////////////////////////////////////
-}
-    
-    
-    {
-    {
-} // namespace BOOST_REGEX_DETAIL_NS
-using boost::BOOST_REGEX_DETAIL_NS::directory_iterator;
-using boost::BOOST_REGEX_DETAIL_NS::file_iterator;
-using boost::BOOST_REGEX_DETAIL_NS::mapfile;
-} // namespace boost
-    
-       format_perl = 0,                                  /* perl style replacement */
-   format_default = 0,                               /* ditto. */
-   format_sed = match_max << 1,                      /* sed style replacement. */
-   format_all = format_sed << 1,                     /* enable all extentions to sytax. */
-   format_no_copy = format_all << 1,                 /* don't copy non-matching segments. */
-   format_first_only = format_no_copy << 1,          /* Only replace first occurance. */
-   format_is_if = format_first_only << 1,            /* internal use only. */
-   format_literal = format_is_if << 1                /* treat string as a literal */
-    
-    template <class BidiIterator, class Allocator, class traits>
-inline void perl_matcher<BidiIterator, Allocator, traits>::push_repeater_count(int i, repeater_count<BidiIterator>** s)
-{
-   saved_repeater<BidiIterator>* pmp = static_cast<saved_repeater<BidiIterator>*>(m_backup_state);
-   --pmp;
-   if(pmp < m_stack_base)
-   {
-      extend_stack();
-      pmp = static_cast<saved_repeater<BidiIterator>*>(m_backup_state);
-      --pmp;
-   }
-   (void) new (pmp)saved_repeater<BidiIterator>(i, s, position, this->recursion_stack.size() ? this->recursion_stack.back().idx : (INT_MIN + 3));
-   m_backup_state = pmp;
-}
-    
-    
-    {
-    {}
-} // namespace boost
-    
-     /*
-  *   LOCATION:    see http://www.boost.org for most recent version.
-  *   FILE         regex_format.hpp
-  *   VERSION      see <boost/version.hpp>
-  *   DESCRIPTION: Provides formatting output routines for search and replace
-  *                operations.  Note this is an internal header file included
-  *                by regex.hpp, do not include on its own.
-  */
-    
-    #ifdef BOOST_MSVC
-#  pragma warning(pop)
-#endif
-    
-    typedef regex_token_iterator<const char*> cregex_token_iterator;
-typedef regex_token_iterator<std::string::const_iterator> sregex_token_iterator;
-#ifndef BOOST_NO_WREGEX
-typedef regex_token_iterator<const wchar_t*> wcregex_token_iterator;
-typedef regex_token_iterator<std::wstring::const_iterator> wsregex_token_iterator;
-#endif
-    
-    #ifndef BOOST_ATOMIC_DETAIL_PAUSE_HPP_INCLUDED_
-#define BOOST_ATOMIC_DETAIL_PAUSE_HPP_INCLUDED_
-    
-    template< >
-struct make_storage_type< 8u, true >
-{
-    typedef mars_boost::int64_t type;
-    }
-    
-    #if BOOST_ATOMIC_SIGNAL_FENCE > 0
-BOOST_FORCEINLINE void atomic_signal_fence(memory_order order) BOOST_NOEXCEPT
-{
-    detail::signal_fence(order);
-}
-#else
-BOOST_FORCEINLINE void atomic_signal_fence(memory_order) BOOST_NOEXCEPT
-{
-    detail::lockpool::signal_fence();
-}
-#endif
+      std::string ToString(bool exclude_zero_counters = false) const;
