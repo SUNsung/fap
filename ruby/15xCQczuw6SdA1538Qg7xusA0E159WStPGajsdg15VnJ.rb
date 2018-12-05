199e0221ddf61b073e8d16e9c13aa2699cd60ad6
SUNsung <weatherius@gmail.com>
2018-12-05 19:38:01 +0200

    Rewrite [15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb] File in: 2018-12-05 19:38:00

--- ruby/15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb
+++ ruby/15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb
@@ -1,100 +1,33 @@
 
         
-              class MergeRequest < ActiveRecord::Base
-        self.table_name = 'merge_requests'
-    
-    
-    def initialize
-      raise NotImplementedError, '#{self.class} is an abstract class and cannot be instantiated.' if self.class.abstract
-    end
-    
-        def subpath
-      @subpath ||= subpath_to(current_url)
-    end
-    
-        def as_json
-      @pages
-    end
-    
-        def parse_as_fragment
-      Nokogiri::HTML.fragment @content, 'UTF-8'
-    end
+        class Array
+  def shelljoin
+    CrossplatformShellwords.shelljoin(self)
   end
 end
-
     
-        def self.run(urls, options = {}, &block)
-      urls = urls.dup
-      requester = new(options)
-      requester.on_response(&block) if block
-      requester.on_response do # cheap hack to ensure root page is processed first
-        if urls
-          requester.request(urls)
-          urls = nil
-        end
-      end
-      requester.request(urls.shift)
-      requester.run
-      requester
+            allow(File).to receive(:file?).and_return(false)
+        expect(File).to receive(:file?).with('#{Dir.home}/Library/Keychains/login.keychain').and_return(true)
+        allow(File).to receive(:exist?).and_return(false)
+        expect(File).to receive(:exist?).with('item.path').and_return(true)
+    
+    module Homebrew
+  module_function
+    
+          clear_cache
+      super(new_user, new_repo)
+    
+          PluginManager.ui.info('Installing file: #{local_file}')
+      uncompressed_path = uncompress(local_file)
+      PluginManager.ui.debug('Pack uncompressed to #{uncompressed_path}')
+      pack = LogStash::PluginManager::PackInstaller::Pack.new(uncompressed_path)
+      raise PluginManager::InvalidPackError, 'The pack must contains at least one plugin' unless pack.valid?
+    
+          warn_local_gems(plugins_with_path) if plugins_with_path.size > 0
     end
-    
-            css('h1').each_with_index do |node, i|
-          next if i == 0
-          node.name = 'h2'
-        end
-    
-      it 'returns true when passed ?e if the argument is a file' do
-    Kernel.test(?e, @file).should == true
+    update_gems!
   end
     
-      it 'does not append line-end if last character is line-end' do
-    lambda {
-      $VERBOSE = true
-      warn('this is some simple text with line-end\n')
-    }.should output(nil, 'this is some simple text with line-end\n')
-  end
-    
-        def stylesheets_path
-      File.join assets_path, 'stylesheets'
+        after :each do
+      logstash.uninstall
     end
-    
-        def log_http_get_file(url, cached = false)
-      s = '  #{'CACHED ' if cached}GET #{url}...'
-      if cached
-        puts dark green s
-      else
-        puts dark cyan s
-      end
-    end
-    
-      # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-    
-        SPREE_GEMS.each do |gem_name|
-      Dir.chdir(gem_name) do
-        sh 'gem build spree_#{gem_name}.gemspec'
-        mv 'spree_#{gem_name}-#{version}.gem', pkgdir
-      end
-    end
-    
-              if Spree::Cart::Update.call(order: @order, params: line_items_attributes).success?
-            @line_item.reload
-            respond_with(@line_item, default_template: :show)
-          else
-            invalid_resource!(@line_item)
-          end
-        end
-    
-            def scope
-          if params[:country_id]
-            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
-            @country.states.accessible_by(current_ability, :read).order('name ASC')
-          else
-            State.accessible_by(current_ability, :read).order('name ASC')
-          end
-        end
-      end
-    end
-  end
-end
