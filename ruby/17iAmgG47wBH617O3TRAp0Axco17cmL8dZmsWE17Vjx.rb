9c6a86997a47d81c193ec1f2117c931307223387
SUNsung <weatherius@gmail.com>
2018-12-06 22:38:07 +0200

    Rewrite [17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb] File in: 2018-12-06 22:38:07

--- ruby/17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb
+++ ruby/17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb
@@ -1,257 +1,68 @@
 
         
-        # skip over blogs that aren't found
-unavailable = []
-fast_forwards = [
-  'Baidu Research',
-  'Booking.com',
-  'Fynd',
-  'Graphcool',
-  'LinkedIn',
-  'Medallia',
-  'OmniTI',
-  'Paperless Post',
-  'Pluralsight',
-  'Prolific Interactive',
-  'Quora',
-  'Robert Elder Software',
-  'Simple',
-  'SlideShare',
-  'SourceClear',
-  'Viget',
-  'Zalando',
-  'Zapier',
-  'Zynga',
-  'Dave Beazley',
-  'Edan Kwan',
-  'Grzegorz Gajos',
-  'Joe Armstrong',
-  'Kai Hendry',
-  'LiveOverflow'
-]
+              def parallel?
+        @parallel
+      end
     
-      #
-  # Constructage of the HTTP response with the supplied code, message, and
-  # protocol.
-  #
-  def initialize(code = 200, message = 'OK', proto = DefaultProtocol)
-    super()
+            def representation_class
+          Representation::Issue
+        end
     
+            def sidekiq_worker_class
+          ImportLfsObjectWorker
+        end
     
-  #
-  # Payload types were copied from xCAT-server source code (IPMI.pm)
-  #
-  RMCP_ERRORS = {
-    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
-    2 => 'Invalid Session ID', #this shouldn't occur...
-    3 => 'Invalid payload type',#shouldn't occur..
-    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
-    5 => 'Invalid integrity algorithm', #same as above
-    6 => 'No matching authentication payload',
-    7 => 'No matching integrity payload',
-    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
-    9 => 'Invalid role',
-    0xa => 'Unauthorised role or privilege level requested',
-    0xb => 'Insufficient resources to create a session at the requested role',
-    0xc => 'Invalid username length',
-    0xd => 'Unauthorized name',
-    0xe => 'Unauthorized GUID',
-    0xf => 'Invalid integrity check value',
-    0x10 => 'Invalid confidentiality algorithm',
-    0x11 => 'No cipher suite match with proposed security algorithms',
-    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
-  }
+            def sidekiq_worker_class
+          ImportNoteWorker
+        end
     
-    end
-end
-end
-end
-
+    module Gitlab
+  module GithubImport
+    # IssuableFinder can be used for caching and retrieving database IDs for
+    # issuable objects such as issues and pull requests. By caching these IDs we
+    # remove the need for running a lot of database queries when importing
+    # GitHub projects.
+    class IssuableFinder
+      attr_reader :project, :object
     
-              res = decode_kerb_response(data)
+    module Gitlab
+  module GithubImport
+    module Representation
+      class Note
+        include ToHash
+        include ExposeAttribute
     
-                checksum = OpenSSL::HMAC.digest('MD5', k1, data_encrypt)
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a KRB_AP_REQ definition, containing the Kerberos protocol version number,
-        # the message type KRB_AP_REQ, an options field to indicate any options in use, and the ticket and authenticator
-        # themselves
-        class ApReq < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of the protocol message
-          attr_accessor :msg_type
-          # @!attribute options
-          #   @return [Integer] request options, affects processing
-          attr_accessor :options
-          # @!attribute ticket
-          #   @return [Rex::Proto::Kerberos::Model::Ticket] The ticket authenticating the client to the server
-          attr_accessor :ticket
-          # @!attribute authenticator
-          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] This contains the authenticator, which includes the
-          #   client's choice of a subkey
-          attr_accessor :authenticator
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        class EncKdcResponse < Element
-          # @!attribute key
-          #   @return [Rex::Proto::Kerberos::Model::EncryptionKey] The session key
-          attr_accessor :key
-          # @!attribute last_req
-          #   @return [Array<Rex::Proto::Kerberos::Model::LastRequest>] This field is returned by the KDC and specifies the time(s)
-          #   of the last request by a principal
-          attr_accessor :last_req
-          # @!attribute nonce
-          #   @return [Integer] random number
-          attr_accessor :nonce
-          # @!attribute key_expiration
-          #   @return [Time] The key-expiration field is part of the response from the
-          #   KDC and specifies the time that the client's secret key is due to expire
-          attr_accessor :key_expiration
-          # @!attribute flags
-          #   @return [Integer] This field indicates which of various options were used or
-          #   requested when the ticket was issued
-          attr_accessor :flags
-          # @!attribute auth_time
-          #   @return [Time] the time of initial authentication for the named principal
-          attr_accessor :auth_time
-          # @!attribute start_time
-          #   @return [Time] Specifies the time after which the ticket is valid
-          attr_accessor :start_time
-          # @!attribute end_time
-          #   @return [Time] This field contains the time after which the ticket will
-          #   not be honored (its expiration time)
-          attr_accessor :end_time
-          # @!attribute renew_till
-          #   @return [Time] This field is only present in tickets that have the
-          #   RENEWABLE flag set in the flags field.  It indicates the maximum
-          #   endtime that may be included in a renewal
-          attr_accessor :renew_till
-          # @!attribute srealm
-          #   @return [String] The realm part of the server's principal identifier
-          attr_accessor :srealm
-          # @!attribute sname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
-          attr_accessor :sname
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptedData from an
-          # OpenSSL::ASN1::Sequence
-          #
-          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode_asn1(input)
-            seq_values = input.value
-    
-              # Decodes the Rex::Proto::Kerberos::Model::KdcResponse from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KdcResponse, invalid input'
-            end
-    
-              # Decodes the e_data from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_e_data(input)
-            input.value[0].value
+            def state
+          if merged_at
+            :merged
+          else
+            attributes[:state]
           end
         end
-      end
-    end
-  end
-end
     
-              # Rex::Proto::Kerberos::Model::LastRequest encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'LastRequest encoding not supported'
-          end
+            # Builds a user from a GitHub API response.
+        #
+        # user - An instance of `Sawyer::Resource` containing the user details.
+        def self.from_api_response(user)
+          new(id: user.id, login: user.login)
+        end
     
-    # usage rake isolate[my-post]
-desc 'Move all other posts than the one currently being worked on to a temporary stash location (stash) so regenerating the site happens much more quickly.'
-task :isolate, :filename do |t, args|
-  stash_dir = '#{source_dir}/#{stash_dir}'
-  FileUtils.mkdir(stash_dir) unless File.exist?(stash_dir)
-  Dir.glob('#{source_dir}/#{posts_dir}/*.*') do |post|
-    FileUtils.mv post, stash_dir unless post.include?(args.filename)
-  end
-end
-    
-    class ConfigTag < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-    options = options.split(' ').map {|i| i.strip }
-    @key = options.slice!(0)
-    @tag = nil
-    @classname = nil
-    options.each do |option|
-      @tag = $1 if option =~ /tag:(\S+)/ 
-      @classname = $1 if option =~ /classname:(\S+)/
-    end
-  end
-    
-      # Improved version of Liquid's truncate:
-  # - Doesn't cut in the middle of a word.
-  # - Uses typographically correct ellipsis (…) insted of '...'
-  def truncate(input, length)
-    if input.length > length && input[0..(length-1)] =~ /(.+)\b.+$/im
-      $1.strip + ' &hellip;'
-    else
-      input
-    end
-  end
-    
-        def sizes
-      attrs = 'width='#{@sizes[0]}'' if @sizes[0]
-      attrs += ' height='#{@sizes[1]}'' if @sizes[1]
-      attrs
-    end
-  end
-end
-    
-    require 'clamp'
-require 'pluginmanager/util'
-require 'pluginmanager/gemfile'
-require 'pluginmanager/install'
-require 'pluginmanager/remove'
-require 'pluginmanager/list'
-require 'pluginmanager/update'
-require 'pluginmanager/pack'
-require 'pluginmanager/unpack'
-require 'pluginmanager/generate'
-require 'pluginmanager/prepare_offline_pack'
-require 'pluginmanager/proxy_support'
-configure_proxy
-    
-        def self.find_by_name_with_wildcards(pattern)
-      re = transform_pattern_into_re(pattern)
-      ::Gem::Specification.find_all.select do |specification|
-        specification.name =~ re
+          it 'does not autocorrect the closing brace' do
+        new_source = autocorrect_source(source)
+        expect(new_source).to eq([source].join($RS))
       end
     end
     
-        context 'update a specific plugin' do
-      it 'has executed successfully' do
-        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
-        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
-        expect(logstash).not_to have_installed?(plugin_name, previous_version)
+            self
       end
-    end
+    
+    Given /^I run a rails generator to generate a '([^']*)' scaffold with '([^']*)'$/ do |model_name, attributes|
+  step %[I successfully run `rails generate scaffold #{model_name} #{attributes}`]
+end
+    
+    module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
