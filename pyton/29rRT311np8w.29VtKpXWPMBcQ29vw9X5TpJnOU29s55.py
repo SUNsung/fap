d9ff229e22907d638c4a26c28bb656a9d553503f
SUNsung <weatherius@gmail.com>
2018-12-06 02:43:21 +0200

    Rewrite [29rRT311np8w.29VtKpXWPMBcQ29vw9X5TpJnOU29s55.py] File in: 2018-12-06 02:43:20

--- pyton/29rRT311np8w.29VtKpXWPMBcQ29vw9X5TpJnOU29s55.py
+++ pyton/29rRT311np8w.29VtKpXWPMBcQ29vw9X5TpJnOU29s55.py
@@ -1,226 +1,77 @@
 
         
-            def is_face_card(self):
-        '''Jack = 11, Queen = 12, King = 13'''
-        return True if 10 < self._value <= 13 else False
+            def __str__(self):
+        defaults = dict(type(self).__dict__)
+        actual = dict(defaults)
+        actual.update(self.__dict__)
+        actual['config'] = self.config
+        return repr_dict_nice(dict(
+            (key, value)
+            for key, value in actual.items()
+            if not key.startswith('_'))
+        )
     
+        @property
+    def encoding(self):
+        '''Return a `str` with the message's encoding, if known.'''
+        raise NotImplementedError()
     
-class HitCounts(MRJob):
+            '''
+        return content
+
     
-      return -0.5 * (logvar + np.log(2*np.pi) + \
-                 tf.square((z-mu)/tf.exp(0.5*logvar)))
+            '''
+        r.headers['Authorization'] = type(self).make_header(
+            self.username, self.password).encode('latin1')
+        return r
     
-        gen_cell = gen_cell_class(hps.gen_dim,
-                              input_weight_scale=hps.gen_cell_input_weight_scale,
-                              rec_weight_scale=hps.gen_cell_rec_weight_scale,
-                              clip_value=hps.cell_clip_value,
-                              recurrent_collections=['l2_gen_reg'])
-    with tf.variable_scope('gen', reuse=False):
-      if ic_dim == 0:
-        self.gen_ics = tf.tile(
-              tf.Variable(tf.zeros([1, gen_cell.state_size]), name='g0'),
-              tf.stack([batch_size, 1]))
-      else:
-        self.gen_ics = linear(self.g0s_val, gen_cell.state_size,
-                              identity_if_possible=True,
-                              name='g0_2_gen_ic')
-    
-    
-# OPTIMIZATION
-flags.DEFINE_integer('batch_size', BATCH_SIZE,
-                     'Batch size to use during training.')
-flags.DEFINE_float('learning_rate_init', LEARNING_RATE_INIT,
-                   'Learning rate initial value')
-flags.DEFINE_float('learning_rate_decay_factor', LEARNING_RATE_DECAY_FACTOR,
-                   'Learning rate decay, decay by this fraction every so \
-                   often.')
-flags.DEFINE_float('learning_rate_stop', LEARNING_RATE_STOP,
-                   'The lr is adaptively reduced, stop training at this value.')
-# Rather put the learning rate on an exponentially decreasiong schedule,
-# the current algorithm pays attention to the learning rate, and if it
-# isn't regularly decreasing, it will decrease the learning rate.  So far,
-# it works fine, though it is not perfect.
-flags.DEFINE_integer('learning_rate_n_to_compare', LEARNING_RATE_N_TO_COMPARE,
-                     'Number of previous costs current cost has to be worse \
-                     than, to lower learning rate.')
-    
-    # just one dataset here
-datasets = {}
-dataset_name = 'dataset_N' + str(N)
-datasets[dataset_name] = data
-    
-      Args:
-    vocab: Vocabulary. Contains vocabulary size and converts word to ids.
-  '''
-  assert FLAGS.save_dir, 'Must specify FLAGS.save_dir for dump_emb.'
-  inputs = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
-  targets = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
-  weights = np.ones([BATCH_SIZE, NUM_TIMESTEPS], np.float32)
-    
-        if FLAGS.prefix_label and use_prefix:
-      label = sequence_example.context.feature['class'].int64_list.value[0]
-      review_words = [EOS_INDEX + 1 + label]
-    else:
-      review_words = []
-    review_words.extend([
-        f.int64_list.value[0]
-        for f in sequence_example.feature_lists.feature_list['token_id'].feature
-    ])
-    all_words.append(review_words)
-  return all_words
-    
-              [gen_initial_state_eval, fake_gen_initial_state_eval, _] = sess.run(
-              [
-                  model.eval_final_state, model.fake_gen_final_state,
-                  model.global_step
-              ],
-              feed_dict=eval_feed)
-    
-          for s in xrange(t, FLAGS.sequence_length):
-        cum_advantage += missing_list[s] * np.power(gamma,
-                                                    (s - t)) * rewards_list[s]
-      cum_advantage -= baselines[t]
-      # Clip advantages.
-      cum_advantage = tf.clip_by_value(cum_advantage, -FLAGS.advantage_clipping,
-                                       FLAGS.advantage_clipping)
-      advantages.append(missing_list[t] * cum_advantage)
-      final_gen_objective += tf.multiply(
-          log_probability, missing_list[t] * tf.stop_gradient(cum_advantage))
-    
-      decoder_embedding = [
-      v for v in tf.trainable_variables()
-      if v.op.name == 'gen/decoder/rnn/embedding'
-  ][0]
-  decoder_lstm_w_0 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      'gen/decoder/rnn/GenericMultiRNNCell/Cell0/Alien/rnn_builder/big_h_mat'
-  ][0]
-  decoder_lstm_b_0 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      'gen/decoder/rnn/GenericMultiRNNCell/Cell0/Alien/rnn_builder/big_inputs_mat'
-  ][0]
-  decoder_lstm_w_1 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      'gen/decoder/rnn/GenericMultiRNNCell/Cell1/Alien/rnn_builder/big_h_mat'
-  ][0]
-  decoder_lstm_b_1 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      'gen/decoder/rnn/GenericMultiRNNCell/Cell1/Alien/rnn_builder/big_inputs_mat'
-  ][0]
-    
-            # init attention
-        attention = _init_attention(encoder_state)
-      else:
-        # construct attention
-        attention = attention_construct_fn(cell_output, attention_keys,
-                                           attention_values)
-        cell_output = attention
-    
-        def process_rhs(self, compiler, connection):
-        result = super().process_rhs(compiler, connection)
-        # Treat None lookup values as null.
-        return (''null'', []) if result == ('%s', [None]) else result
+        # Adapters
+    def get_transport_plugins(self):
+        return [plugin for plugin in self
+                if issubclass(plugin, TransportPlugin)]
 
     
     
-class RangeMaxValueValidator(MaxValueValidator):
-    def compare(self, a, b):
-        return a.upper is None or a.upper > b
-    message = _('Ensure that this range is completely less than or equal to %(limit_value)s.')
-    
-        def save(self, must_create=False):
-        '''
-        Save the current session data to the database. If 'must_create' is
-        True, raise a database error if the saving operation doesn't create a
-        new entry (as opposed to possibly updating an existing entry).
-        '''
-        if self.session_key is None:
-            return self.create()
-        data = self._get_session(no_load=must_create)
-        obj = self.create_model_instance(data)
-        using = router.db_for_write(self.model, instance=obj)
-        try:
-            with transaction.atomic(using=using):
-                obj.save(force_insert=must_create, force_update=not must_create, using=using)
-        except IntegrityError:
-            if must_create:
-                raise CreateError
-            raise
-        except DatabaseError:
-            if not must_create:
-                raise UpdateError
-            raise
+class PyTest(TestCommand):
+    # `$ python setup.py test' simply installs minimal requirements
+    # and runs the tests with no fancy stuff like parallel execution.
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '--doctest-modules', '--verbose',
+            './httpie', './tests'
+        ]
+        self.test_suite = True
     
     
-class Error(Exception):
-    pass
+@mock.patch('httpie.core.get_response')
+def test_error(get_response):
+    def error(msg, *args, **kwargs):
+        global error_msg
+        error_msg = msg % args
     
-    CLI_DEFAULTS = dict(
-    server_root=server_root_tmp,
-    ctl='nginx',
-)
-'''CLI defaults.'''
     
-        Handles decoding/encoding between RFC3339 strings and aware (not
-    naive) `datetime.datetime` objects
-    (e.g. ``datetime.datetime.now(pytz.utc)``).
+class Config(BaseConfigDict):
     
-        def ensure_augeas_state(self):
-        '''Makes sure that all Augeas dom changes are written to files to avoid
-        loss of configuration directives when doing additional augeas parsing,
-        causing a possible augeas.load() resulting dom reset
-        '''
+        hass.bus.listen(EVENT_LOGBOOK_ENTRY, logbook_entry_listener)
+    hass.bus.listen(EVENT_STATE_CHANGED, state_changed_listener)
     
-    ALL_SSL_OPTIONS_HASHES = [
-    '2086bca02db48daf93468332543c60ac6acdb6f0b58c7bfdf578a5d47092f82a',
-    '4844d36c9a0f587172d9fa10f4f1c9518e3bcfa1947379f155e16a70a728c21a',
-    '5a922826719981c0a234b1fbcd495f3213e49d2519e845ea0748ba513044b65b',
-    '4066b90268c03c9ba0201068eaa39abbc02acf9558bb45a788b630eb85dadf27',
-    'f175e2e7c673bd88d0aff8220735f385f916142c44aa83b09f1df88dd4767a88',
-    'cfdd7c18d2025836ea3307399f509cfb1ebf2612c87dd600a65da2a8e2f2797b',
-    '80720bd171ccdc2e6b917ded340defae66919e4624962396b992b7218a561791',
-    'c0c022ea6b8a51ecc8f1003d0a04af6c3f2bc1c3ce506b3c2dfc1f11ef931082',
-]
-'''SHA256 hashes of the contents of previous versions of all versions of MOD_SSL_CONF_SRC'''
+        def get_device_name(self, device):
+        '''Return the name of the device with the given MAC address.'''
+        name = next((
+            result.name for result in self.last_results
+            if result.mac == device), None)
+        return name
     
-            :returns: 2 - FQ, 1 - wildcard, 0 - _default_
-        :rtype: int
+    SERVICE_SCHEMA = vol.Schema({
+    vol.Optional(ATTR_ACCOUNTNAME): vol.All(cv.ensure_list, [cv.slugify]),
+    vol.Optional(ATTR_DEVICENAME): cv.slugify,
+    vol.Optional(ATTR_INTERVAL): cv.positive_int
+})
     
-        def test_nonexistent_generic(self):
-        with mock.patch('certbot.util.get_os_info') as mock_info:
-            mock_info.return_value = ('nonexistent', 'irrelevant')
-            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
-                mock_like.return_value = ['unknonwn']
-                self.assertEqual(entrypoint.get_configurator(),
-                                 configurator.ApacheConfigurator)
+            # The API URL always needs a username and password
+        self.tadoapiurl += '?username={username}&password={password}'
     
-            self.assertEqual(mock_setup_cert.call_count, 2)
     
-    TEST_IMAGES = [
-    'obama-240p.jpg',
-    'obama-480p.jpg',
-    'obama-720p.jpg',
-    'obama-1080p.jpg'
-]
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-    app = Flask(__name__)
-    
-        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
-    context = multiprocessing
-    if 'forkserver' in multiprocessing.get_all_start_methods():
-        context = multiprocessing.get_context('forkserver')
-    
-        # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-    # This is an example of running face recognition on a single image
-# and drawing a box around each person that was identified.
+class ThomsonDeviceScanner(DeviceScanner):
+    '''This class queries a router running THOMSON firmware.'''
