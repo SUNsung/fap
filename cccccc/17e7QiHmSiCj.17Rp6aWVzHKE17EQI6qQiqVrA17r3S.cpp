dd3a55d57894082d08b7ee13d216a95a698fe67a
SUNsung <weatherius@gmail.com>
2018-12-08 01:07:30 +0200

    Rewrite [17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp] File in: 2018-12-08 01:07:30

--- cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
+++ cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
@@ -1,111 +1,79 @@
 
         
-        #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/plugin.h>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/descriptor.pb.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/stubs/strutil.h>
+        
+    {    elem_of_each_node_[left_node_id] = Elem(begin, split_pt, left_node_id);
+    elem_of_each_node_[right_node_id] = Elem(split_pt, e.end, right_node_id);
+    elem_of_each_node_[node_id] = Elem(nullptr, nullptr, -1);
+  }
     
-    // Get the name of a message's Java class without package name prefix.
-string ClassNameWithoutPackage(const Descriptor* descriptor,
-                               bool immutable) {
-  return StripPackageName(descriptor->full_name(),
-                          descriptor->file());
+    /**
+ * @brief Hash is a general utility class for hashing content
+ *
+ * @code{.cpp}
+ *   Hash my_hash(HASH_TYPE_SHA256);
+ *   my_hash.update(my_buffer, my_buffer_size);
+ *   std::cout << my_hash.digest();
+ * @endcode
+ *
+ */
+class Hash : private boost::noncopyable {
+ public:
+  /**
+   * @brief Hash constructor
+   *
+   * The hash class should be initialized with one of osquery::HashType as a
+   * constructor argument.
+   *
+   * @param algorithm The hashing algorithm which will be used to compute the
+   * hash
+   */
+  explicit Hash(HashType algorithm);
+    }
+    
+    bool PlatformProcess::operator==(const PlatformProcess& process) const {
+  return (nativeHandle() == process.nativeHandle());
 }
     
-    EnumFieldGenerator::~EnumFieldGenerator() {}
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace objectivec {
-    }
-    }
-    }
-    }
-    
-    #include <grpc/grpc_security.h>
-    
-    MeasureDouble RpcServerServerLatency() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcServerServerLatencyMeasureName,
-      'Time between first byte of request received to last byte of response '
-      'sent, or terminal error',
-      kUnitMilliseconds);
-  return measure;
-}
-    
-    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
-::opencensus::stats::MeasureDouble RpcClientServerLatency();
-::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
-    
-    Aggregation MillisDistributionAggregation() {
-  return Aggregation::Distribution(BucketBoundaries::Explicit(
-      {0,   0.01, 0.05, 0.1,  0.3,   0.6,   0.8,   1,     2,   3,   4,
-       5,   6,    8,    10,   13,    16,    20,    25,    30,  40,  50,
-       65,  80,   100,  130,  160,   200,   250,   300,   400, 500, 650,
-       800, 1000, 2000, 5000, 10000, 20000, 50000, 100000}));
-}
-    
-    #ifdef BOOST_REGEX_FI_WIN32_MAP // win32 mapfile
-    
-    
-    {   if(greedy)
-   {
-      if((rep->leading) && (count < rep->max))
-         restart = position;
-      // push backtrack info if available:
-      if(count - rep->min)
-         push_single_repeat(count, rep, position, saved_state_greedy_single_repeat);
-      // jump to next state:
-      pstate = rep->alt.p;
-      return true;
-   }
-   else
-   {
-      // non-greedy, push state and return true if we can skip:
-      if(count < rep->max)
-         push_single_repeat(count, rep, position, saved_state_rep_short_set);
-      pstate = rep->alt.p;
-      return (position == last) ? (rep->can_be_null & mask_skip) : can_start(*position, rep->_map, mask_skip);
-   }
-#ifdef __BORLANDC__
-#pragma option pop
+    #ifdef WIN32
+  pid = (int)::GetCurrentProcessId();
+#else
+  pid = getpid();
 #endif
-#ifdef BOOST_MSVC
-#pragma warning(pop)
-#endif
+    
+    #include <gtest/gtest.h>
+    
+    
+    {  EXPECT_EQ(constraint.op, EQUALS);
+  EXPECT_EQ(constraint.expr, 'none');
 }
     
-    template <class F, class M, class O>
-struct format_traits_imp
-{
-private:
-   //
-   // F must be a pointer, a function, or a class with a function call operator:
-   //
-   BOOST_STATIC_ASSERT((::boost::is_pointer<F>::value || ::boost::is_function<F>::value || ::boost::is_class<F>::value));
-   static formatter_wrapper<typename unwrap_reference<F>::type> f;
-   static M m;
-   static O out;
-   static boost::regex_constants::match_flag_type flags;
-public:
-   BOOST_STATIC_CONSTANT(int, value = sizeof(check_is_formatter(f(m), f(m, out), f(m, out, flags))));
-};
+    #include <Objbase.h>
+#include <Windows.h>
     
-    template<>
-struct padding3<4>
-{
-   enum{
-      padding_size = 4,
-      padding_mask = 3
-   };
-};
+      // exclude paths should be applied at last
+  auto path = ec->path.substr(0, ec->path.rfind('/'));
+  // Need to have two finds,
+  // what if somebody excluded an individual file inside a directory
+  if (!exclude_paths_.empty() &&
+      (exclude_paths_.find(path) || exclude_paths_.find(ec->path))) {
+    return false;
+  }
     
-    #endif  // BOOST_REGEX_V4_REGEX_SEARCH_HPP
+    #include <vector>
+#include <string>
+#include <memory>
+    
+    #include 'DHTNode.h'
+#include 'DlAbortEx.h'
+#include 'DHTConstants.h'
+#include 'bittorrent_helper.h'
+#include 'Logger.h'
+#include 'a2netcompat.h'
+#include 'util.h'
+#include 'TimeA2.h'
+#include 'fmt.h'
+#include 'File.h'
+#include 'LogFactory.h'
+#include 'BufferedFile.h'
+    
+    DHTTokenUpdateCommand::~DHTTokenUpdateCommand() = default;
