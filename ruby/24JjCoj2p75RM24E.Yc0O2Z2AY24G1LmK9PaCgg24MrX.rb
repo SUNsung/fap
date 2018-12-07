8ee4f88797763b891664b057475e03eb7308aabf
SUNsung <weatherius@gmail.com>
2018-12-08 00:01:09 +0200

    Rewrite [24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb] File in: 2018-12-08 00:01:08

--- ruby/24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb
+++ ruby/24JjCoj2p75RM24E.Yc0O2Z2AY24G1LmK9PaCgg24MrX.rb
@@ -1,109 +1,65 @@
 
         
-            # This returns whether the guest is ready to work. If this returns
-    # `false`, then {#detect!} should be called in order to detect the
-    # guest OS.
-    #
-    # @return [Boolean]
-    def ready?
-      !!capability_host_chain
+                include ::EachBatch
+      end
+    
+            def preload_stages_warnings
+          # This preloads the number of warnings for every stage, ensuring
+          # that Ci::Stage#has_warnings? doesn't execute any additional
+          # queries.
+          @pipeline.stages.each { |stage| stage.number_of_warnings }
+        end
+      end
     end
   end
 end
 
     
-            # Executes a command and returns true if the command succeeded,
-        # and false otherwise. By default, this executes as a normal user,
-        # and it is up to the communicator implementation if they expose an
-        # option for running tests as an administrator.
+          # Reads a cache key.
+      #
+      # If the key exists and has a non-empty value its TTL is refreshed
+      # automatically.
+      #
+      # raw_key - The cache key to read.
+      # timeout - The new timeout of the key if the key is to be refreshed.
+      def self.read(raw_key, timeout: TIMEOUT)
+        key = cache_key_for(raw_key)
+        value = Redis::Cache.with { |redis| redis.get(key) }
+    
+            # Builds a user from a GitHub API response.
         #
-        # @see #execute
-        def test(command, opts=nil)
+        # user - An instance of `Sawyer::Resource` containing the user details.
+        def self.from_api_response(user)
+          new(id: user.id, login: user.login)
         end
-      end
-    end
-  end
-end
-
     
-              # Set all of our instance variables on the new class
-          [self, other].each do |obj|
-            obj.instance_variables.each do |key|
-              # Ignore keys that start with a double underscore. This allows
-              # configuration classes to still hold around internal state
-              # that isn't propagated.
-              if !key.to_s.start_with?('@__')
-                result.instance_variable_set(key, obj.instance_variable_get(key))
-              end
-            end
-          end
-    
-    module Vagrant
-  module Plugin
-    module V2
-      # This is the base class for a provider for the V2 API. A provider
-      # is responsible for creating compute resources to match the needs
-      # of a Vagrant-configured system.
-      class Provider
-        include CapabilityHost
-    
-      it 'accepts a Rational' do
-    sleep(Rational(1, 9)).should be_close(0, 2)
-  end
-    
-      it 'accepts and uses a seed of 0' do
-    srand(0)
-    srand.should == 0
-  end
-    
-        def emoji(name)
-      if emoji = Gemojione.index.find_by_name(name)
-        IO.read(EMOJI_PATHNAME.join('#{emoji['unicode']}.png'))
-      else
-        fail ArgumentError, 'emoji `#{name}' not found'
-      end
+        def type=(value)
+      @type = value.try :strip
     end
     
-      s.name              = 'gollum'
-  s.version           = '4.1.4'
-  s.date              = '2018-10-01'
-  s.rubyforge_project = 'gollum'
-  s.license           = 'MIT'
+        delegate :empty?, :blank?, to: :pages
     
-          def load_user_roles
-        @current_user_roles = @current_api_user ? @current_api_user.spree_roles.pluck(:name) : []
+        def url
+      @url ||= URL.parse request.base_url
+    end
+    
+        self.initial_paths = []
+    self.options = {}
+    self.stubs = {}
+    
+          def fetch_redirections
+        result = {}
+        with_filters 'apply_base_url', 'container', 'normalize_urls', 'internal_urls' do
+          build_pages do |page|
+            next if page[:response_effective_path] == page[:response_path]
+            result[page[:response_path].downcase] = page[:response_effective_path]
+          end
+        end
+        result
       end
     
-            def update
-          authorize! :update, @order, order_token
-          @address = find_address
-    
-              unless inventory_unit.respond_to?(can_event) &&
-              inventory_unit.send(can_event)
-            render plain: { exception: 'cannot transition to #{@event}' }.to_json,
-                   status: 200
-            false
-          end
+            css('.filetree').each do |node|
+          node.content = node.css('.file').map(&:inner_html).join('\n')
+          node.name = 'pre'
+          node.remove_attribute('class')
         end
-    
-            def create
-          authorize! :create, Spree::OptionType
-          @option_type = Spree::OptionType.new(option_type_params)
-          if @option_type.save
-            render :show, status: 201
-          else
-            invalid_resource!(@option_type)
-          end
-        end
-    
-            def index
-          authorize! :admin, ReturnAuthorization
-          @return_authorizations = order.return_authorizations.accessible_by(current_ability, :read).
-                                   ransack(params[:q]).result.
-                                   page(params[:page]).per(params[:per_page])
-          respond_with(@return_authorizations)
-        end
-    
-              if params[:page] || params[:per_page]
-            @states = @states.page(params[:page]).per(params[:per_page])
-          end
