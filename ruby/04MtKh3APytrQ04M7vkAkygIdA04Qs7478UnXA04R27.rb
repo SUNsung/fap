78f9758542c70da6f545dfb6827cfd5ea8c73bea
SUNsung <weatherius@gmail.com>
2018-12-08 07:51:39 +0200

    Rewrite [04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb] File in: 2018-12-08 07:51:38

--- ruby/04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb
+++ ruby/04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb
@@ -1,127 +1,124 @@
 
         
-              AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 4, :outbound_event => events(:jane_website_agent_event))
-      expect(agents(:jane_website_agent).reload.last_error_log_at.to_i).to be_within(2).of(Time.now.to_i)
-    end
+                    DateTimeSelector.new(datetime, options, html_options)
+          end
     
-        reversible(&method(:up_down))
-  end
+              super(object_name, method_name, template_object, options)
+        end
     
-    Then /^I should have (\d) contacts? in '([^']*)'$/ do |n_contacts, aspect_name|
-  @me.aspects.where(:name => aspect_name).first.contacts.count.should == n_contacts.to_i
-end
-    
-    module NavigationHelpers
-  def path_to(page_name)
-    case page_name
-    when /^person_photos page$/
-      person_photos_path(@me.person)
-    when /^the home(?: )?page$/
-      stream_path
-    when /^the mobile path$/
-      force_mobile_path
-    when /^the user applications page$/
-      api_openid_connect_user_applications_path
-    when /^the tag page for '([^\']*)'$/
-      tag_path(Regexp.last_match(1))
-    when /^its ([\w ]+) page$/
-      send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path', @it)
-    when /^the mobile ([\w ]+) page$/
-      public_send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path', format: 'mobile')
-    when /^the ([\w ]+) page$/
-      public_send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path')
-    when /^my edit profile page$/
-      edit_profile_path
-    when /^my profile page$/
-      person_path(@me.person)
-    when /^my acceptance form page$/
-      invite_code_path(InvitationCode.first)
-    when /^the requestors profile$/
-      person_path(Request.where(recipient_id: @me.person.id).first.sender)
-    when /^'([^\']*)''s page$/
-      p = User.find_by_email(Regexp.last_match(1)).person
-      {path:         person_path(p),
-       # '#diaspora_handle' on desktop, '.description' on mobile
-       special_elem: {selector: '#diaspora_handle, .description', text: p.diaspora_handle}
-      }
-    when /^'([^\']*)''s photos page$/
-      p = User.find_by_email(Regexp.last_match(1)).person
-      person_photos_path p
-    when /^my account settings page$/
-      edit_user_path
-    when /^forgot password page$/
-      new_user_password_path
-    when %r{^'(/.*)'}
-      Regexp.last_match(1)
-    else
-      raise 'Can't find mapping from \'#{page_name}\' to a path.'
-    end
-  end
-    
-      let(:valid_params) {
-    {
-      user: {
-        username:              'jdoe',
-        email:                 'jdoe@example.com',
-        password:              'password',
-        password_confirmation: 'password'
-      }
-    }
-  }
-    
-        if rbenv_prefix = prefix_from_bin('rbenv')
-      prefixes << rbenv_prefix
-    end
-    
-    desc 'list of authors'
-task :authors, [:commit_range, :format, :sep] do |t, a|
-  a.with_defaults :format => '%s (%d)', :sep => ', ', :commit_range => '--all'
-  authors = Hash.new(0)
-  blake   = 'Blake Mizerany'
-  overall = 0
-  mapping = {
-    'blake.mizerany@gmail.com' => blake, 'bmizerany' => blake,
-    'a_user@mac.com' => blake, 'ichverstehe' => 'Harry Vangberg',
-    'Wu Jiang (nouse)' => 'Wu Jiang' }
-  `git shortlog -s #{a.commit_range}`.lines.map do |line|
-    line = line.force_encoding 'binary' if line.respond_to? :force_encoding
-    num, name = line.split('\t', 2).map(&:strip)
-    authors[mapping[name] || name] += num.to_i
-    overall += num.to_i
-  end
-  puts '#{overall} commits by #{authors.count} authors:'
-  puts authors.sort_by { |n,c| -c }.map { |e| a.format % e }.join(a.sep)
-end
-    
-          def html?(headers)
-        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
-        options[:html_types].include? header.last[/^\w+\/\w+/]
+        # Determine the layout for a given name, taking into account the name type.
+    #
+    # ==== Parameters
+    # * <tt>name</tt> - The name of the template
+    def _layout_for_option(name)
+      case name
+      when String     then _normalize_layout(name)
+      when Proc       then name
+      when true       then Proc.new { |formats| _default_layout(formats, true)  }
+      when :default   then Proc.new { |formats| _default_layout(formats, false) }
+      when false, nil then nil
+      else
+        raise ArgumentError,
+          'String, Proc, :default, true, or false, expected for `layout'; you passed #{name.inspect}'
       end
     end
-  end
-end
-
     
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   CSRF
-    # Supported browsers:: all
-    # More infos::         http://flask.pocoo.org/docs/0.10/security/#json-security
-    #                      http://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx
-    #
-    # JSON GET APIs are vulnerable to being embedded as JavaScript when the
-    # Array prototype has been patched to track data. Checks the referrer
-    # even on GET requests if the content type is JSON.
-    #
-    # If request includes Origin HTTP header, defers to HttpOrigin to determine
-    # if the request is safe. Please refer to the documentation for more info.
-    #
-    # The `:allow_if` option can be set to a proc to use custom allow/deny logic.
-    class JsonCsrf < Base
-      default_options :allow_if => nil
+        # Direct access to template rendering.
+    def render_template(context, options) #:nodoc:
+      TemplateRenderer.new(@lookup_context).render(context, options)
+    end
     
-        EMPTY_TYPE = 'inode/x-empty'
-    SENSIBLE_DEFAULT = 'application/octet-stream'
+    class TestController < ApplicationController
+  include Rails.application.routes.url_helpers
     
-        private
+          def mailer_from(mapping)
+        mailer_sender(mapping, :from)
+      end
+    
+        # Include the chosen devise modules in your model:
+    #
+    #   devise :database_authenticatable, :confirmable, :recoverable
+    #
+    # You can also give any of the devise configuration values in form of a hash,
+    # with specific values for this model. Please check your Devise initializer
+    # for a complete description on those values.
+    #
+    def devise(*modules)
+      options = modules.extract_options!.dup
+    
+          # Overwrites active_for_authentication? from Devise::Models::Activatable for locking purposes
+      # by verifying whether a user is active to sign in or not based on locked?
+      def active_for_authentication?
+        super && !access_locked?
+      end
+    
+        def mime_type
+      headers['Content-Type'] || 'text/plain'
+    end
+    
+        def initialize(machine, guests, capabilities)
+      @capabilities = capabilities
+      @guests       = guests
+      @machine      = machine
+    end
+    
+              # Set all of our instance variables on the new class
+          [self, other].each do |obj|
+            obj.instance_variables.each do |key|
+              # Ignore keys that start with a double underscore. This allows
+              # configuration classes to still hold around internal state
+              # that isn't propagated.
+              if !key.to_s.start_with?('@__')
+                result.instance_variable_set(key, obj.instance_variable_get(key))
+              end
+            end
+          end
+    
+            def initialize
+          @logger = Log4r::Logger.new('vagrant::plugin::v1::manager')
+          @registered = []
+        end
+    
+            # This contains all the registered provider capabilities.
+        #
+        # @return [Hash<Symbol, Registry>]
+        attr_reader :provider_capabilities
+    
+              components.providers.register(name.to_sym) do
+            [block.call, options]
+          end
+    
+      gem.required_ruby_version = '>= 2.0'
+  gem.add_dependency 'airbrussh', '>= 1.0.0'
+  gem.add_dependency 'i18n'
+  gem.add_dependency 'rake', '>= 10.0.0'
+  gem.add_dependency 'sshkit', '>= 1.9.0'
+    
+        def load_imports
+      if options.show_tasks && Rake::Task.task_defined?('load:defaults')
+        invoke 'load:defaults'
+        set(:stage, '')
+        Dir[deploy_config_path].each { |f| add_import f }
+      end
+    
+          def string_to_code string
+        # sha bytes
+        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
+        # Thanks donpark's IdenticonUtil.java for this.
+        # Match the following Java code
+        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
+        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
+    
+      s.add_development_dependency 'rack-test', '~> 0.6.2'
+  s.add_development_dependency 'shoulda', '~> 3.5.0'
+  s.add_development_dependency 'minitest-reporters', '~> 0.14.16'
+  s.add_development_dependency 'twitter_cldr', '~> 3.2.0'
+  s.add_development_dependency 'mocha', '~> 1.1.0'
+  s.add_development_dependency 'test-unit', '~> 3.1.0'
+  s.add_development_dependency 'webrick', '~> 1.3.1'
+    
+          if data.code.to_i != 200
+        raise RuntimeError, 'Gist replied with #{data.code} for #{gist_url}'
+      end
+    
+    module Jekyll
