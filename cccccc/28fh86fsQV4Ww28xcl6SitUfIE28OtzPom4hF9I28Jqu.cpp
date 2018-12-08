9a71133f6a5de1ff4bfa0901ed451b9e1d276aad
SUNsung <weatherius@gmail.com>
2018-12-08 02:33:31 +0200

    Rewrite [28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp] File in: 2018-12-08 02:33:31

--- cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
+++ cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
@@ -1,111 +1,359 @@
 
         
-        TEST(MovableMessageTest, MoveDifferentArenas) {
-  Arena arena1, arena2;
+        Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
+}
+    
+    #endif  // CONTENT_NW_SRC_API_BASE_BASE_H_
+
+    
+    bool MenuDelegate::GetIconForCommandId(int command_id,
+                                       gfx::Image* icon) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  if (item->icon_.IsEmpty())
+    return false;
     }
     
-      // Nested enums
-  if (descriptor->enum_type_count() > 0) {
-      std::vector<std::string> enums;
-      for (int i = 0; i < descriptor->enum_type_count(); i++) {
-          enums.push_back(GetClassName(descriptor->enum_type(i)));
-      }
-      printer->Print('new[]{ typeof($enums$) }, ', 'enums', JoinStrings(enums, '), typeof('));
-  }
-  else {
-      printer->Print('null, ');
-  }
+      // Ignore first non-switch arg if it's not a standalone package.
+  bool ignore_arg = !package->self_extract();
+  for (unsigned i = 1; i < argv.size(); ++i) {
+    if (ignore_arg && args.size() && argv[i] == args[0]) {
+      ignore_arg = false;
+      continue;
+    }
+    }
     
-    SourceGeneratorBase::SourceGeneratorBase(const FileDescriptor* descriptor,
-                                         const Options *options)
-    : descriptor_(descriptor), options_(options) {
+    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringFWithFixupFunction() {}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringFWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
+};
+    
+      // implement nw.Screen.stopMonitor()
+  class NwScreenStopMonitorFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenStopMonitorFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    #include 'gtest/internal/gtest-death-test-internal.h'
+    
+    #if GTEST_OS_SYMBIAN
+  // These are needed as the Nokia Symbian Compiler cannot decide between
+  // const T& and const T* in a function template. The Nokia compiler _can_
+  // decide between class template specializations for T and T*, so a
+  // tr1::type_traits-like is_pointer works, and we can overload on that.
+  template <typename T>
+  inline void StreamHelper(internal::true_type /*is_pointer*/, T* pointer) {
+    if (pointer == NULL) {
+      *ss_ << '(null)';
+    } else {
+      *ss_ << pointer;
+    }
+  }
+  template <typename T>
+  inline void StreamHelper(internal::false_type /*is_pointer*/,
+                           const T& value) {
+    // See the comments in Message& operator <<(const T&) above for why
+    // we need this using statement.
+    using ::operator <<;
+    *ss_ << value;
+  }
+#endif  // GTEST_OS_SYMBIAN
+    
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12>
+internal::ValueArray12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+    T12> Values(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
+    T10 v10, T11 v11, T12 v12) {
+  return internal::ValueArray12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11,
+      T12>(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12);
 }
     
-    int ImmutableExtensionLiteGenerator::GenerateRegistrationCode(
-    io::Printer* printer) {
-  printer->Print(
-    'registry.add($scope$.$name$);\n',
-    'scope', scope_,
-    'name', UnderscoresToCamelCase(descriptor_));
-  return 7;
+    template <>
+class UniversalTersePrinter<wchar_t*> {
+ public:
+  static void Print(wchar_t* str, ::std::ostream* os) {
+    UniversalTersePrinter<const wchar_t*>::Print(str, os);
+  }
+};
+    
+    
+    {}  // namespace internal
+    
+      // Trivial case 2: even numbers
+  if (n % 2 == 0) return n == 2;
+    
+      static int allocated() { return allocated_; }
+    
+    /*!
+ * \file graph_attr_types.h
+ * \brief Data structures that can appear in graph attributes.
+ */
+#ifndef MXNET_GRAPH_ATTR_TYPES_H_
+#define MXNET_GRAPH_ATTR_TYPES_H_
+    
+    /*! \brief Cuda runtime compile module. */
+class CudaModule {
+ private:
+  /*! \brief Structure for holding internal info. */
+  struct Chunk {
+    /*!
+     * \brief Constructs cuda module.
+     * \param source cuda source code.
+     * \param exports export symbols before mangling.
+     */
+    Chunk(const char* source,
+          const std::vector<std::string>& options,
+          const std::vector<std::string>& exports);
+    /*! \brief deconstrutor */
+    ~Chunk();
+    /*!
+     * \brief Get handle to cuda kernel from loaded module
+     * \param mangled_name mangled kernel name
+     * \param ctx context to run kernel on
+     * \return loaded function handle
+     */
+    CUfunction GetFunction(const std::string& mangled_name, const Context& ctx);
+    /*! \brief nvrtc program handle. */
+    nvrtcProgram prog_;
+    /*! \brief compiled cuda PTX */
+    char* ptx_;
+    /*! \brief lazily loaded cuda module */
+    std::unordered_map<int, CUmodule> mod_;
+    /*! \brief exported names */
+    std::unordered_set<std::string> exports_;
+  };
+  /*! \brief pointer to Chunk */
+  std::shared_ptr<Chunk> ptr_;
+    }
+    
+    template<>
+void SetDataGradToBlob<mshadow::gpu, double>(caffeMemoryTypes memType,
+                            std::vector<::caffe::Blob<double>*>::iterator blob,
+                            std::vector<TBlob>::const_iterator itr) {
+  double *data_ptr = reinterpret_cast<double*>((*itr).dptr_);
+  if (memType == Data)
+    (*blob)->set_gpu_data(data_ptr);
+  else
+    MXCAFFEBLOB(*blob, double)->set_gpu_diff(data_ptr);
 }
     
-    // Author: liujisi@google.com (Pherl Liu)
     
-    // Checks if the file contains any extensions definitions (at the root or
-// nested under a message).
-bool FileContainsExtensions(const FileDescriptor* file) {
-  if (file->extension_count() > 0) {
+    {  /*! \brief milliseconds spent in Next() */
+  std::atomic<uint64_t> next_time_;
+};  // class CaffeDataIterWrapper
+    
+    bool get_png_size(const unsigned char* data, mx_uint data_size, mx_uint *width, mx_uint *height) {
+  if (data[0] == 0x89 && data[1] == 0x50 && data[2] ==0x4E && data[3] == 0x47) {
+    unsigned char const* p = data + 16;
+    *width = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
+    p += 4;
+    *height = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
     return true;
+  } else {
+    return false;
   }
-  for (int i = 0; i < file->message_type_count(); i++) {
-    if (MessageContainsExtensions(file->message_type(i))) {
-      return true;
-    }
-  }
-  return false;
 }
     
     /*!
- * \brief Shape inference function to get the correct shape given source shapes.
- * \param lhs The shape of left operand.
- * \param rhs The shape of right operand.
- * \param env The Environment arguments.
- * \return The inferred result shape.
+ * \brief Thread pool.
  */
-typedef TShape (*BinaryShapeFunction)(const TShape& lhs,
-                                      const TShape& rhs,
-                                      const EnvArguments& env);
-/*!
- * \brief Gradient function that takes only output gradient and computes gradient wrt to input.
- *  We support total gradient as a whole to make it easy to combine a few ops.
- * \param out_grad the gradient wrt to output of the function.
- * \param env The Environment arguments.
- * \param lhs_grad The container to store result of lhs gradient.
- * \param rhs_grad The container to store result of lhs gradient.
- * \param req_lhs_grad The requirement to store the lhs_grad
- * \param req_rhs_grad The requirement to store the rhs_grad
- * \param ctx Runtime context to execute the function.
- */
-typedef void (*BinaryGradFunctionT0)(const OutputGrad& out_grad,
-                                     const EnvArguments& env,
-                                     TBlob* lhs_grad,
-                                     TBlob* rhs_grad,
-                                     OpReqType req_lhs_grad,
-                                     OpReqType req_rhs_grad,
-                                     RunContext ctx);
-/*!
- * \brief Gradient function that takes inputs of function anod computes gradient wrt to input.
- * \param out_grad the gradient wrt to output of the function.
- * \param lhs The left operand to the function.
- * \param rhs The right operand to the function.
- * \param env The Environment arguments.
- * \param lhs_grad The container to store result of lhs gradient.
- * \param rhs_grad The container to store result of lhs gradient.
- * \param req_lhs_grad The requirement to store the lhs_grad
- * \param req_rhs_grad The requirement to store the rhs_grad
- * \param ctx Runtime context to execute the function.
- */
-typedef void (*BinaryGradFunctionT1)(const OutputGrad& out_grad,
-                                     const Input0& lhs,
-                                     const Input1& rhs,
-                                     const EnvArguments& env,
-                                     TBlob* lhs_grad,
-                                     TBlob* rhs_grad,
-                                     OpReqType req_lhs_grad,
-                                     OpReqType req_rhs_grad,
-                                     RunContext ctx);
+class ThreadPool {
+ public:
+  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
+  struct SetReadyOnDestroy {
+    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
+      : event_(event) {
+    }
+    inline ~SetReadyOnDestroy() {
+      if (event_) {
+        event_->signal();
+      }
+    }
+    std::shared_ptr<dmlc::ManualEvent>  event_;
+  };
+    }
     
-      Operator* CreateOperator(Context ctx) const override {
-    LOG(FATAL) << 'Not Implemented.';
-    return NULL;
+      void StopNoWait() {
+    streams_->Finalize();
+    task_queue_->SignalForKill();
+    io_task_queue_->SignalForKill();
+    task_queue_ = nullptr;
+    io_task_queue_ = nullptr;
+    thread_pool_ = nullptr;
+    io_thread_pool_ = nullptr;
+    streams_ = nullptr;
   }
     
-    namespace mxnet {
-namespace io {
-/*! \return the parameter of default augmenter */
-std::vector<dmlc::ParamFieldInfo> ListDefaultAugParams();
-std::vector<dmlc::ParamFieldInfo> ListDefaultDetAugParams();
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_IO_IMAGE_AUGMENTER_H_
+    /*!
+ *  Copyright (c) 2015 by Contributors
+ * \file image_augmenter.h
+ * \brief Interface of opencv based image augmenter
+ */
+#ifndef MXNET_IO_IMAGE_AUGMENTER_H_
+#define MXNET_IO_IMAGE_AUGMENTER_H_
+    
+    /*! \brief create a batch iterator from single instance iterator */
+class BatchLoader : public IIterator<TBlobBatch> {
+ public:
+  explicit BatchLoader(IIterator<DataInst> *base):
+    head_(1), num_overflow_(0), base_(base) {
+  }
+    }
+    
+    void DHTReplaceNodeTask::onTimeout(const std::shared_ptr<DHTNode>& node)
+{
+  ++numRetry_;
+  if (numRetry_ >= MAX_RETRY) {
+    A2_LOG_INFO(fmt('ReplaceNode: Ping failed %d times. Replace %s with %s.',
+                    numRetry_, node->toString().c_str(),
+                    newNode_->toString().c_str()));
+    node->markBad();
+    bucket_->addNode(newNode_);
+    setFinished(true);
+  }
+  else {
+    A2_LOG_INFO(fmt('ReplaceNode: Ping reply timeout from %s. Try once more.',
+                    node->toString().c_str()));
+    sendMessage();
+  }
+}
+    
+    void DHTRoutingTableDeserializer::deserialize(const std::string& filename)
+{
+  A2_LOG_INFO(fmt('Loading DHT routing table from %s.', filename.c_str()));
+  BufferedFile fp(filename.c_str(), BufferedFile::READ);
+  if (!fp) {
+    throw DL_ABORT_EX(
+        fmt('Failed to load DHT routing table from %s', filename.c_str()));
+  }
+  char header[8];
+  memset(header, 0, sizeof(header));
+  // magic
+  header[0] = 0xa1u;
+  header[1] = 0xa2u;
+  // format ID
+  header[2] = 0x02u;
+  // version
+  header[6] = 0;
+  header[7] = 0x03u;
+    }
+    
+      std::vector<std::shared_ptr<DHTNode>> nodes_;
+    
+    
+    {} // namespace aria2
+
+    
+    
+    {  virtual bool finished() = 0;
+};
+    
+      virtual std::shared_ptr<DHTTask>
+  createNodeLookupTask(const unsigned char* targetID) = 0;
+    
+    #include 'common.h'
+    
+      DHTTaskExecutor periodicTaskQueue2_;
+    
+      bool validateToken(const std::string& token, const unsigned char* infoHash,
+                     const std::string& ipaddr, uint16_t port) const;
+    
+      // show some sample bytes
+  virtual std::string toString() const CXX11_OVERRIDE;
+    
+    
+    {  EsdCanClient esd_can_client;
+  EXPECT_TRUE(esd_can_client.Init(param));
+  EXPECT_EQ(esd_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
+  std::vector<CanFrame> frames;
+  int32_t num = 0;
+  EXPECT_EQ(esd_can_client.Send(frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  EXPECT_EQ(esd_can_client.Receive(&frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
+  CanFrame can_frame;
+  frames.push_back(can_frame);
+  EXPECT_EQ(esd_can_client.SendSingleFrame(frames),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  esd_can_client.Stop();
+}
+    
+    ErrorCode FakeCanClient::Send(const std::vector<CanFrame> &frames,
+                              int32_t *const frame_num) {
+  if (frame_num == nullptr) {
+    AERROR << 'frame_num pointer is null';
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
+  }
+    }
+    
+      if (!_is_init) {
+    AERROR << 'Hermes can client is not init! Please init first!';
+    return ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED;
+  }
+  //    if (*frame_num > MAX_CAN_SEND_FRAME_LEN || *frame_num < 0) {
+  //       AERROR << 'send can frame num not in range[0, '
+  //         << MAX_CAN_SEND_FRAME_LEN << '], frame_num:' << *frame_num;
+  //       return ErrorCode::CAN_CLIENT_ERROR_FRAME_NUM;
+  //    }
+  for (int i = 0; i < *frame_num; ++i) {
+    _send_frames[i].bcan_msg_id = frames[i].id;
+    _send_frames[i].bcan_msg_datalen = frames[i].len;
+    memcpy(_send_frames[i].bcan_msg_data, frames[i].data, frames[i].len);
+  }
+    
+    using ::apollo::canbus::ChassisDetail;
+    
+      /**
+   * @brief Check if the bit on a specified position is one.
+   * @param pos The position of the bit to check.
+   * @return If the bit on a specified position is one.
+   */
+  bool is_bit_1(const int32_t pos) const;
+    
+    // data file
+DECLARE_string(sensor_conf_file);
+    
+    #endif  // GUETZLI_ENTROPY_ENCODE_H_
+
+    
+    std::string ReadFileOrDie(const char* filename) {
+  bool read_from_stdin = strncmp(filename, '-', 2) == 0;
+    }
+    
+    bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
+                     JPEGData* jpg) {
+  static const int quant[3 * kDCTBlockSize] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  };
+  return EncodeRGBToJpeg(rgb, w, h, quant, jpg);
+}
