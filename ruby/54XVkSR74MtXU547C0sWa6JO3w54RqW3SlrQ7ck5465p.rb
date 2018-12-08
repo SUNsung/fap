1e624fa9b9431cfbe50970feff26e3a6d5285cfc
SUNsung <weatherius@gmail.com>
2018-12-09 01:10:27 +0200

    Rewrite [54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb] File in: 2018-12-09 01:10:27

--- ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
+++ ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
@@ -1,218 +1,84 @@
 
         
-        Given(%r!^I have the following (draft|post)s? within the '(.*)' directory:$!) do |type, folder, table|
-  table.hashes.each do |input_hash|
-    title = slug(input_hash['title'])
-    parsed_date = Time.xmlschema(input_hash['date']) rescue Time.parse(input_hash['date'])
+              puts 'Moving #{old_path} to #{new_path}...' if $stdout.tty?
+      path.dirname.mkpath
+      FileUtils.mv old_path, new_path
+    end
+  end
     
-    module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
+    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
+  file_shared_path = TestApp.shared_path.join(file)
+  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
+  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
+end
     
-    module Jekyll
-  module Converters
-    class Markdown
-      class KramdownParser
-        CODERAY_DEFAULTS = {
-          'css'               => 'style',
-          'bold_every'        => 10,
-          'line_numbers'      => 'inline',
-          'line_number_start' => 1,
-          'tab_width'         => 4,
-          'wrap'              => 'div',
-        }.freeze
+      def run_vagrant_command(command)
+    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
+    return [stdout, stderr] if status.success?
+    raise VagrantSSHCommandError, status
+  end
+end
     
-          action_launched('import')
+        def setup_filters
+      @filters = cmdline_filters
+      @filters += @custom_filters if @custom_filters
+      @filters << Filter.new(:role, ENV['ROLES']) if ENV['ROLES']
+      @filters << Filter.new(:host, ENV['HOSTS']) if ENV['HOSTS']
+      fh = fetch_for(:filter, {}) || {}
+      @filters << Filter.new(:host, fh[:hosts]) if fh[:hosts]
+      @filters << Filter.new(:role, fh[:roles]) if fh[:roles]
+      @filters << Filter.new(:host, fh[:host]) if fh[:host]
+      @filters << Filter.new(:role, fh[:role]) if fh[:role]
+    end
     
-    module Commander
-  # This class override the run method with our custom stack trace handling
-  # In particular we want to distinguish between user_error! and crash! (one with, one without stack trace)
-  class Runner
-    # Code taken from https://github.com/commander-rb/commander/blob/master/lib/commander/runner.rb#L50
+            # Skip validation behavior if no validators are registered for this key
+        return super unless validators.key?(key)
     
-            session.action_launched(launch_context: launch_context)
-      end
+    Given 'I allow the attachment to be submitted' do
+  cd('.') do
+    transform_file('app/controllers/users_controller.rb') do |content|
+      content.gsub('params.require(:user).permit(:name)',
+                   'params.require(:user).permit!')
+    end
+  end
+end
+    
+    When /^(?:|I )press '([^']*)'$/ do |button|
+  click_button(button)
+end
+    
+      def migration_version
+    if Rails.version.start_with? '5'
+      '[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]'
     end
   end
 end
 
     
-            cmd << ['-am #{message.shellescape}']
-        cmd << '--force' if options[:force]
-        cmd << '-s' if options[:sign]
-        cmd << tag.shellescape
-        cmd << options[:commit].to_s if options[:commit]
+    # This stuff needs to be run after Paperclip is defined.
+require 'paperclip/io_adapters/registry'
+require 'paperclip/io_adapters/abstract_adapter'
+require 'paperclip/io_adapters/empty_string_adapter'
+require 'paperclip/io_adapters/identity_adapter'
+require 'paperclip/io_adapters/file_adapter'
+require 'paperclip/io_adapters/stringio_adapter'
+require 'paperclip/io_adapters/data_uri_adapter'
+require 'paperclip/io_adapters/nil_adapter'
+require 'paperclip/io_adapters/attachment_adapter'
+require 'paperclip/io_adapters/uploaded_file_adapter'
+require 'paperclip/io_adapters/uri_adapter'
+require 'paperclip/io_adapters/http_url_proxy_adapter'
+
     
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            tag: '#{tag}',
-            message: '#{message}',
-            commit: '#{commit}'
-          })
-        end').runner.execute(:test)
-    
-          it 'adds docset_package_url param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            docset_package_url: 'http://docset-package-url.com'
-          )
-        end').runner.execute(:test)
-    
-          it 'use custom executable' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              executable: 'custom_carthage'
-            )
-          end').runner.execute(:test)
-    
-            inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
-        pseudocommand = 'git log --pretty=\'%B\' #{inner_command.shellescape}...HEAD'
-        expect(result).to eq(pseudocommand)
-      end
-    
-          it 'works with name and password that contain spaces or `\'`' do
-        password = '\'test password\''
-        result = Fastlane::FastFile.new.parse('lane :test do
-          create_keychain ({
-            name: 'test.keychain',
-            password: '#{password}',
-          })
-        end').runner.execute(:test)
-    
-            Fastlane::FastFile.new.parse('lane :test do
-          import_certificate ({
-            keychain_name: '#{keychain}',
-            certificate_path: '#{cert_name}',
-            certificate_password: '#{password}',
-            log_output: true
-          })
-        end').runner.execute(:test)
-      end
+        def type_from_file_contents
+      type_from_mime_magic || type_from_file_command
+    rescue Errno::ENOENT => e
+      Paperclip.log('Error while determining content type: #{e}')
+      SENSIBLE_DEFAULT
     end
-  end
-end
-
     
-          it 'works with select regex' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              select_regex: /AppDelegate/
-            )
-          end').runner.execute(:test)
-    
-          describe '#configuration_available?' do
-        let(:param) { { use_bundle_exec: false } }
-        let(:version) { '' }
-        before do
-          allow(action).to receive(:slather_version).and_return(Gem::Version.create(version))
-        end
-    
-          context 'when specify reporter' do
-        it 'adds reporter option' do
-          result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-              reporter: 'json'
-            )
-          end').runner.execute(:test)
-    
-      expect(Open3).to receive(:popen2e).with(*command).and_yield(mock_input, mock_output, mock_thread)
-end
-
-    
-        def retroactive_unblock?
-      ActiveRecord::Type.lookup(:boolean).cast(resource_params[:retroactive])
+        # Returns an integer timestamp that is time zone-neutral, so that paths
+    # remain valid even if a server's time zone changes.
+    def updated_at attachment, style_name
+      attachment.updated_at
     end
-  end
-end
-
-    
-    module Admin
-  class ReportedStatusesController < BaseController
-    before_action :set_report
-    
-      private
-    
-    class PolymorphicMentions < ActiveRecord::Migration[4.2]
-  def change
-    remove_index :mentions, column: %i(post_id)
-    remove_index :mentions, column: %i(person_id post_id), unique: true
-    rename_column :mentions, :post_id, :mentions_container_id
-    add_column :mentions, :mentions_container_type, :string
-    add_index :mentions,
-              %i(mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_mc_id_and_mc_type',
-              length: {mentions_container_type: 191}
-    add_index :mentions,
-              %i(person_id mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_person_and_mc_id_and_mc_type',
-              length: {mentions_container_type: 191},
-              unique: true
-    
-    Then /^I should see an image in the publisher$/ do
-  photo_in_publisher.should be_present
-end
-    
-          def page_name
-        @name.gsub('-', ' ')
-      end
-    
-          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
-      def left_shift int, shift
-        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
-        # 1>>31, 2**32
-        (r & 2147483648) == 0 ? r : r - 4294967296
-      end
-    
-          def title
-        'Home'
-      end
-    
-      test 'edit returns nil for non-existant page' do
-    # post '/edit' fails. post '/edit/' works.
-    page = 'not-real-page'
-    path = '/'
-    post '/edit/', :content => 'edit_msg',
-         :page              => page, :path => path, :message => ''
-    page_e = @wiki.paged(page, path)
-    assert_equal nil, page_e
-  end
-    
-        @wiki = Gollum::Wiki.new(@path)
-    page  = @wiki.page('k')
-    assert_equal '바뀐 text', utf8(page.raw_data)
-    assert_equal 'ghi', page.version.message
-  end
-    
-    if options[:irb]
-  require 'irb'
-  # http://jameskilton.com/2009/04/02/embedding-irb-into-your-ruby-application/
-  module IRB # :nodoc:
-    def self.start_session(binding)
-      unless @__initialized
-        args = ARGV
-        ARGV.replace(ARGV.dup)
-        IRB.setup(nil)
-        ARGV.replace(args)
-        @__initialized = true
-      end
-    
-      s.name              = 'gollum'
-  s.version           = '4.1.4'
-  s.date              = '2018-10-01'
-  s.rubyforge_project = 'gollum'
-  s.license           = 'MIT'
-    
-      class Error < StandardError;
-  end
