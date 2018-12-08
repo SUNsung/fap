47d084abe3d6aa3d39bf7bb51278b8870a34065d
SUNsung <weatherius@gmail.com>
2018-12-08 05:37:58 +0200

    Rewrite [575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp] File in: 2018-12-08 05:37:58

--- cccccc/575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp
+++ cccccc/575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp
@@ -1,350 +1,139 @@
 
         
-          // The handle of the module that contains the window procedure of |window_|.
-  HMODULE instance_;
+        #include <cstdint>
+#include <vector>
     
-    #ifndef ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
-#define ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
+      // Constructors for the various ParamTypes.
+  ParamContent() = default;
+  explicit ParamContent(tesseract::StringParam* it);
+  explicit ParamContent(tesseract::IntParam* it);
+  explicit ParamContent(tesseract::BoolParam* it);
+  explicit ParamContent(tesseract::DoubleParam* it);
     
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
+    #ifndef TESSERACT_CSTRUCT_BOXWORD_H_
+#define TESSERACT_CSTRUCT_BOXWORD_H_
     
-    
-    {  if (use_native_event) {
-    mate::Handle<mate::Event> native_event = mate::Event::Create(isolate);
-    native_event->SetSenderAndMessage(sender, message);
-    event = v8::Local<v8::Object>::Cast(native_event.ToV8());
-  } else {
-    event = CreateEventObject(isolate);
+      // Sets the destination filename and enables images to be written to a PDF
+  // on destruction.
+  void WritePDF(const char* filename) {
+    if (pixaGetCount(pixa_) > 0) {
+      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
+      pixaClear(pixa_);
+    }
   }
-  mate::Dictionary(isolate, event).Set('sender', object);
-  return event;
-}
     
-      SavePageHandler(content::WebContents* web_contents,
-                  const SavePageCallback& callback);
-  ~SavePageHandler() override;
+    protected:
+	SpaceBullet *space;
+	btTypedConstraint *constraint;
+	bool disabled_collisions_between_bodies;
     
-    #include <QDialog>
+    #include 'collision_object_bullet.h'
     
-    public:
-    explicit SignVerifyMessageDialog(const PlatformStyle *platformStyle, QWidget *parent);
-    ~SignVerifyMessageDialog();
+    	ClassDB::register_virtual_class<CSGShape>();
+	ClassDB::register_virtual_class<CSGPrimitive>();
+	ClassDB::register_class<CSGMesh>();
+	ClassDB::register_class<CSGSphere>();
+	ClassDB::register_class<CSGBox>();
+	ClassDB::register_class<CSGCylinder>();
+	ClassDB::register_class<CSGTorus>();
+	ClassDB::register_class<CSGPolygon>();
+	ClassDB::register_class<CSGCombiner>();
     
-    private:
-    reverse_lock(reverse_lock const&);
-    reverse_lock& operator=(reverse_lock const&);
-    
-    /** Check bounds on a command line confirm target */
-unsigned int ParseConfirmTarget(const UniValue& value);
-    
-    void test_bad_scalar(void) {
-    unsigned char s_zero[32] = { 0 };
-    unsigned char s_overflow[32] = {
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe,
-        0xba, 0xae, 0xdc, 0xe6, 0xaf, 0x48, 0xa0, 0x3b,
-        0xbf, 0xd2, 0x5e, 0x8c, 0xd0, 0x36, 0x41, 0x41
-    };
-    unsigned char s_rand[32] = { 0 };
-    unsigned char output[32];
-    secp256k1_scalar rand;
-    secp256k1_pubkey point;
+    void register_etc_types() {
     }
     
-    int secp256k1_ecdsa_recoverable_signature_serialize_compact(const secp256k1_context* ctx, unsigned char *output64, int *recid, const secp256k1_ecdsa_recoverable_signature* sig) {
-    secp256k1_scalar r, s;
+    FileAccessZip::~FileAccessZip() {
     }
     
-    static bool CaseInsensitiveEqual(const std::string &s1, const std::string &s2)
-{
-    if (s1.size() != s2.size()) return false;
-    for (size_t i = 0; i < s1.size(); ++i) {
-        char c1 = s1[i];
-        if (c1 >= 'A' && c1 <= 'Z') c1 -= ('A' - 'a');
-        char c2 = s2[i];
-        if (c2 >= 'A' && c2 <= 'Z') c2 -= ('A' - 'a');
-        if (c1 != c2) return false;
-    }
-    return true;
-}
-    
-    /* Given a BlockIndex with the provided nbits,
- * verify that the expected difficulty results.
- */
-static void TestDifficulty(uint32_t nbits, double expected_difficulty)
-{
-    CBlockIndex* block_index = CreateBlockIndexWithNbits(nbits);
-    double difficulty = GetDifficulty(block_index);
-    delete block_index;
-    }
-    
-    bool ParseInt32(const std::string& str, int32_t *out)
-{
-    if (!ParsePrechecks(str))
-        return false;
-    char *endp = NULL;
-    errno = 0; // strtol will not set errno if valid
-    long int n = strtol(str.c_str(), &endp, 10);
-    if(out) *out = (int32_t)n;
-    // Note that strtol returns a *long int*, so even if strtol doesn't report a over/underflow
-    // we still have to check that the returned value is within the range of an *int32_t*. On 64-bit
-    // platforms the size of these types may be different.
-    return endp && *endp == 0 && !errno &&
-        n >= std::numeric_limits<int32_t>::min() &&
-        n <= std::numeric_limits<int32_t>::max();
-}
-    
-    /** @brief Fills a Blob with values @f$ x \in [0, 1] @f$
- *         such that @f$ \forall i \sum_j x_{ij} = 1 @f$.
- */
-template <typename Dtype>
-class PositiveUnitballFiller : public Filler<Dtype> {
- public:
-  explicit PositiveUnitballFiller(const FillerParameter& param)
-      : Filler<Dtype>(param) {}
-  virtual void Fill(Blob<Dtype>* blob) {
-    Dtype* data = blob->mutable_cpu_data();
-    DCHECK(blob->count());
-    caffe_rng_uniform<Dtype>(blob->count(), 0, 1, blob->mutable_cpu_data());
-    // We expect the filler to not be called very frequently, so we will
-    // just use a simple implementation
-    int dim = blob->count() / blob->shape(0);
-    CHECK(dim);
-    for (int i = 0; i < blob->shape(0); ++i) {
-      Dtype sum = 0;
-      for (int j = 0; j < dim; ++j) {
-        sum += data[i * dim + j];
-      }
-      for (int j = 0; j < dim; ++j) {
-        data[i * dim + j] /= sum;
-      }
-    }
-    CHECK_EQ(this->filler_param_.sparse(), -1)
-         << 'Sparsity not supported by this Filler.';
+    Status KafkaTopicsConfigParserPlugin::update(const std::string& source,
+                                             const ParserConfig& config) {
+  auto topics = config.find(kKafkaTopicParserRootKey);
+  if (topics != config.end()) {
+    auto obj = data_.getObject();
+    data_.copyFrom(topics->second.doc(), obj);
+    data_.add(kKafkaTopicParserRootKey, obj);
   }
-};
+  return Status();
+}
     
+      auto it = doc.FindMember(kLoggerKey);
+  if (it != doc.MemberEnd()) {
+    doc.EraseMember(it);
+  }
     
-    {  /**
-   * @brief Computes the error gradient w.r.t. the absolute value inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to computed outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x @f$; Backward fills their diff with
-   *      gradients @f$
-   *        \frac{\partial E}{\partial x} =
-   *            \mathrm{sign}(x) \frac{\partial E}{\partial y}
-   *      @f$ if propagate_down[0]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-};
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the reordered input.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient
-   *        with respect to the outputs
-   *   -# @f$ (M \times ...) @f$:
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to concatenated outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2):
-   *   - @f$ \frac{\partial E}{\partial y} @f$ is de-indexed (summing where
-   *     required) back to the input x_1
-   *   - This layer cannot backprop to x_2, i.e. propagate_down[1] must be
-   *     false.
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-      virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    /**
- * @brief Convolves the input image with a bank of learned filters,
- *        and (optionally) adds biases.
- *
- *   Caffe convolves by reduction to matrix multiplication. This achieves
- *   high-throughput and generality of input and filter dimensions but comes at
- *   the cost of memory for matrices. This makes use of efficiency in BLAS.
- *
- *   The input is 'im2col' transformed to a channel K' x H x W data matrix
- *   for multiplication with the N x K' x H x W filter matrix to yield a
- *   N' x H x W output matrix that is then 'col2im' restored. K' is the
- *   input channel * kernel height * kernel width dimension of the unrolled
- *   inputs so that the im2col matrix has a column for each input region to
- *   be filtered. col2im restores the output spatial structure by rolling up
- *   the output channel N' columns of the output matrix.
- */
-template <typename Dtype>
-class ConvolutionLayer : public BaseConvolutionLayer<Dtype> {
- public:
-  /**
-   * @param param provides ConvolutionParameter convolution_param,
-   *    with ConvolutionLayer options:
-   *  - num_output. The number of filters.
-   *  - kernel_size / kernel_h / kernel_w. The filter dimensions, given by
-   *  kernel_size for square filters or kernel_h and kernel_w for rectangular
-   *  filters.
-   *  - stride / stride_h / stride_w (\b optional, default 1). The filter
-   *  stride, given by stride_size for equal dimensions or stride_h and stride_w
-   *  for different strides. By default the convolution is dense with stride 1.
-   *  - pad / pad_h / pad_w (\b optional, default 0). The zero-padding for
-   *  convolution, given by pad for equal dimensions or pad_h and pad_w for
-   *  different padding. Input padding is computed implicitly instead of
-   *  actually padding.
-   *  - dilation (\b optional, default 1). The filter
-   *  dilation, given by dilation_size for equal dimensions for different
-   *  dilation. By default the convolution has dilation 1.
-   *  - group (\b optional, default 1). The number of filter groups. Group
-   *  convolution is a method for reducing parameterization by selectively
-   *  connecting input and output channels. The input and output channel dimensions must be divisible
-   *  by the number of groups. For group @f$ \geq 1 @f$, the
-   *  convolutional filters' input and output channels are separated s.t. each
-   *  group takes 1 / group of the input channels and makes 1 / group of the
-   *  output channels. Concretely 4 input channels, 8 output channels, and
-   *  2 groups separate input channels 1-2 and output channels 1-4 into the
-   *  first group and input channels 3-4 and output channels 5-8 into the second
-   *  group.
-   *  - bias_term (\b optional, default true). Whether to have a bias.
-   *  - engine: convolution has CAFFE (matrix multiplication) and CUDNN (library
-   *    kernels + stream parallelism) engines.
-   */
-  explicit ConvolutionLayer(const LayerParameter& param)
-      : BaseConvolutionLayer<Dtype>(param) {}
-    }
+    class EventsConfigParserPluginTests : public testing::Test {};
     
     
     {
-    {  // Recursive copy function: this is similar to crop_copy() but loops over all
-  // but the last two dimensions to allow for ND cropping while still relying on
-  // a CUDA kernel for the innermost two dimensions for performance reasons.  An
-  // alterantive implementation could rely on the kernel more by passing
-  // offsets, but this is problematic because of its variable length.
-  // Since in the standard (N,C,W,H) case N,C are usually not cropped a speedup
-  // could be achieved by not looping the application of the copy_kernel around
-  // these dimensions.
-  void crop_copy_gpu(const vector<Blob<Dtype>*>& bottom,
-                const vector<Blob<Dtype>*>& top,
-                const vector<int>& offsets,
-                vector<int> indices,
-                int cur_dim,
-                const Dtype* src_data,
-                Dtype* dest_data,
-                bool is_forward);
+    {  c.reset();
+}
+}
+
+    
+    #include <gtest/gtest.h>
+    
+    TEST_F(QueryTests, test_add_and_get_current_results) {
+  // Test adding a 'current' set of results to a scheduled query instance.
+  auto query = getOsqueryScheduledQuery();
+  auto cf = Query('foobar', query);
+  uint64_t counter = 128;
+  auto status = cf.addNewResults(getTestDBExpectedResults(), 0, counter);
+  EXPECT_TRUE(status.ok());
+  EXPECT_EQ(status.toString(), 'OK');
+  EXPECT_EQ(counter, 0UL);
+    }
+    
+      /// Enumerate each extension and check sanity.
+  virtual void watchExtensions();
+    
+      std::set<std::string> queries_to_run;
+  // Check for and run discovery queries first
+  if (doc.doc().HasMember('discovery')) {
+    const auto& queries = doc.doc()['discovery'];
+    assert(queries.IsObject());
+    }
+    
+    std::map<int, std::string> kMaskActions = {
+    {IN_ACCESS, 'ACCESSED'},
+    {IN_ATTRIB, 'ATTRIBUTES_MODIFIED'},
+    {IN_CLOSE_WRITE, 'UPDATED'},
+    {IN_CREATE, 'CREATED'},
+    {IN_DELETE, 'DELETED'},
+    {IN_MODIFY, 'UPDATED'},
+    {IN_MOVED_FROM, 'MOVED_FROM'},
+    {IN_MOVED_TO, 'MOVED_TO'},
+    {IN_OPEN, 'OPENED'},
 };
-}  // namespace caffe
     
     
-    {  size_t *workspace_fwd_sizes_;
-  size_t *workspace_bwd_data_sizes_;
-  size_t *workspace_bwd_filter_sizes_;
-  size_t workspaceSizeInBytes;  // size of underlying storage
-  void *workspaceData;  // underlying storage
-  void **workspace;  // aliases into workspaceData
-};
-#endif
+    {  // Make sure only the first event type was recorded.
+  EXPECT_EQ(EventFactory::numEventPublishers(), 1U);
+}
     
-    #include 'caffe/layers/lrn_layer.hpp'
-#include 'caffe/layers/power_layer.hpp'
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
     
-        //
-    // Override
-    //
-    virtual Follow* clone() const override;
-    virtual Follow* reverse() const override;
-    /**
-     * @param dt in seconds.
-     * @js NA
+    protected:
+    Node    *_originalTarget;
+    /** 
+     * The 'target'.
+     * The target will be set with the 'startWithTarget' method.
+     * When the 'stop' method is called, target will be set to nil.
+     * The target is 'assigned', it is not 'retained'.
      */
-    virtual void step(float dt) override;
-    virtual bool isDone() const override;
-    virtual void stop() override;
+    Node    *_target;
+    /** The action tag. An identifier of the action. */
+    int     _tag;
+    /** The action flag field. To categorize action into certain groups.*/
+    unsigned int _flags;
     
-    #include '2d/CCActionEase.h'
-#include '2d/CCTweenFunction.h'
-    
-    /**
-@brief Twirl action.
-@details This action is used for take effect on the target node as twirl.
-        You can control the effect by these parameters:
-        duration, grid size, center position, twirls count, amplitude.
-*/
-class CC_DLL Twirl : public Grid3DAction
-{
-public:
-    /**
-    @brief Create the action with center position, number of twirls, amplitude, a grid size and duration.
-    @param duration Specify the duration of the Twirl action. It's a value in seconds.
-    @param gridSize Specify the size of the grid.
-    @param position Specify the center position of the twirl action.
-    @param twirls Specify the twirls count of the Twirl action.
-    @param amplitude Specify the amplitude of the Twirl action.
-    @return If the creation success, return a pointer of Twirl action; otherwise, return nil.
+        /**
+    @brief Shuffle the array specified.
+    @param array The array will be shuffled.
+    @param len The size of the array.
     */
-    static Twirl* create(float duration, const Size& gridSize, const Vec2& position, unsigned int twirls, float amplitude);
-    }
-    
-    //
-// Hide
-//
-Hide * Hide::create() 
-{
-    Hide *ret = new (std::nothrow) Hide();
-    }
-    
-        //
-    // Overrides
-    //
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    virtual FlipX* reverse() const override;
-    virtual FlipX* clone() const override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    FlipX() :_flipX(false) {}
-    virtual ~FlipX() {}
-    
-    /** @class BezierBy
- * @brief An action that moves the target with a cubic Bezier curve by a certain distance.
- */
-class CC_DLL BezierBy : public ActionInterval
-{
-public:
-    /** Creates the action with a duration and a bezier configuration.
-     * @param t Duration time, in seconds.
-     * @param c Bezier config.
-     * @return An autoreleased BezierBy object.
-     * @code
-     * When this function bound to js or lua,the input params are changed.
-     * in js: var create(var t,var table)
-     * in lua: local create(local t, local table)
-     * @endcode
-     */
-    static BezierBy* create(float t, const ccBezierConfig& c);
-    }
-    
-    #include 'base/CCRef.h'
-#include 'base/CCMap.h'
-#include 'base/CCValue.h'
-#include '2d/CCAnimation.h'
+    void shuffle(unsigned int *array, unsigned int len);
