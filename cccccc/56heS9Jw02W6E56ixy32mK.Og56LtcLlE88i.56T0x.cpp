c5be22712d83bfd0446da5b84d566fbd791a3d02
SUNsung <weatherius@gmail.com>
2018-12-08 18:04:29 +0200

    Rewrite [56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp] File in: 2018-12-08 18:04:29

--- cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
+++ cccccc/56heS9Jw02W6E56ixy32mK.Og56LtcLlE88i.56T0x.cpp
@@ -1,208 +1,112 @@
 
         
-          if (conformanceSig) {
-    for (auto &rawReq : conformanceSig->getRequirements()) {
-      if (auto req = rawReq.subst(conformanceToSyntheticTypeFn,
-                                  conformanceToSyntheticConformanceFn))
-        builder.addRequirement(*req, source, nullptr);
-    }
-  }
+        CallCredentials::CallCredentials() { g_gli_initializer.summon(); }
     
-      DefaultCacheKey CKey(Key, &DCache.CBs);
-  auto Entry = DCache.Entries.find(CKey);
-  if (Entry != DCache.Entries.end()) {
-    if (Entry->second == Value)
-      return;
-    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
-    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
-    DCache.Entries.erase(Entry);
-  }
-    
-    void ClusteredBitVector::reallocate(size_t newCapacityInChunks) {
-  // If we already have out-of-line storage, the padding invariants
-  // will still apply, and we just need to copy the old data into
-  // the new allocation.
-  if (hasOutOfLineData()) {
-    auto oldData = getOutOfLineChunksPtr();
-    allocateAndCopyFrom(oldData, newCapacityInChunks, getLengthInChunks());
-    delete[] (oldData - 1);
-    return;
-  }
+    namespace grpc {
     }
     
-      assert(capacity % 16 == 0 && 'not allocating multiple of alignment');
+    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
+::opencensus::stats::MeasureDouble RpcClientServerLatency();
+::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
     
-      // Set the '_endian' platform condition.
-  switch (Target.getArch()) {
-  case llvm::Triple::ArchType::arm:
-  case llvm::Triple::ArchType::thumb:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::aarch64:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::ppc64:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'big');
-    break;
-  case llvm::Triple::ArchType::ppc64le:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::x86:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::x86_64:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'little');
-    break;
-  case llvm::Triple::ArchType::systemz:
-    addPlatformConditionValue(PlatformConditionKind::Endianness, 'big');
-    break;
-  default:
-    llvm_unreachable('undefined architecture endianness');
-  }
+      void Destroy(grpc_call_element* elem, const grpc_call_final_info* final_info,
+               grpc_closure* then_call_closure) override;
     
-    namespace {
+    namespace grpc {
     }
     
-      // If the word preceding the preposition extends the preposition, it
-  // will never be dropped.
-  if (std::next(nameWordRevIter) != nameWordRevIterEnd &&
-      priorWordExtendsPreposition(*std::next(nameWordRevIter), preposition)) {
-    ++nameWordRevIter;
-    preposition = StringRef((*nameWordRevIter).begin(),
-                            preposition.size() + (*nameWordRevIter).size());
-  }
     
-    int swift::UUID::compare(UUID y) const {
-#if defined(_WIN32)
-  RPC_STATUS s;
-  ::UUID uuid1;
-  memcpy(&uuid1, Value, Size);
-    }
-    
-                // Validate that each of the dynamic axes are unique
-            std::unordered_set<Axis> uniqueDynamicAxis;
-            for (auto& currentDynamicAxis : dynamicAxes)
-            {
-                auto retVal = uniqueDynamicAxis.insert(currentDynamicAxis);
-                if (!retVal.second)
-                    InvalidArgument('Dynamic axis named %S is specified more than once for Variable '%S'', currentDynamicAxis.Name().c_str(), AsString().c_str());
+    {
+    {                // Reset the window.
+                m_loss.first = m_loss.second;
+                m_metric.first = m_metric.second;
+                m_samples.first = m_samples.second;
+                m_updates.first = m_updates.second;
             }
+        }
     
-    //SupportsDistributedMBRead - Tells if the reader supports distributed minibatch reading for parallel training
-bool DataReader::SupportsDistributedMBRead() const
+    namespace CNTK
 {
-    bool supportsDistributedMBRead = true;
-    for (size_t i = 0; i < m_ioNames.size(); i++)
+    class UDFUtils
     {
-        auto currReaderIter = m_dataReaders.find(m_ioNames[i]);
-        assert(currReaderIter != m_dataReaders.end());
+    public:
     }
     }
     
     
-    {    bool IsRandomizationDisabled() const
+    {        return MakeSharedObject<Value>(deviceValueData, deviceValueMask);
+    }
+    
+            bool IsPacked() const { return m_isPacked; }
+    
+        typedef unsigned int INDEXTYPE; // don't use size_t, as this saves HUGE amounts of RAM
+    std::vector<INDEXTYPE> map;     // [t] -> t' indices in randomized order
+    size_t currentseed;             // seed for current sequence
+    size_t randomizationrange;      // t - randomizationrange/2 <= t' < t + randomizationrange/2 (we support this to enable swapping)
+                                    // special values (randomizeDisable)
+    void Invalidate()
     {
-        return randomizationrange == randomizeDisable;
+        currentseed = (size_t) -1;
     }
-};
     
-    // ===================================================================
-// CloneFunctionConfigLambda -- lambda to produce a clone of a network
-//  - creates a BrainScript function that carbon-copies a subsection of an existing network
-//  - the copy can be shallow or deep, where a deep copy gets its own copy of LearnableParameters
-//     - a shallow copy (parameters='shared') is a copy of all nodes that depend on the specified input(s),
-//       while all other nodes are shared from the original network section
-//     - a deep copy (parameters='lernable' or 'constant') also copies all reachable LearnableParameters and their dependents
-//     - Input() nodes not listed as `inputNodes` are always shared
-//  - the source network may be a different network, e.g. loaded with BS.Network.Load()
-//  - a deep copy can be read-only (parameters='constant')
-//     - Note: multiple uses of the lambda will not share read-only parameters. This is trickier to implement that one might expect.
-//  - example use cases:
-//     - adaptation (KL): a frozen read-only copy of the starting model is used as a KL-regularizer
-//     - adaptation (DLR): an injected input transform is trained while the network is fixed
-//     - image: lower layers of ImageNet networks serve as immutable feature extractors for another image task
-//     - DSSM: applying the same network subsection to two inputs
-// Usage:
-//    f = CloneFunction (inputNodes, outputNodes, parameters='lernable' /*|'constant'|'shared'*/)
-// Parameters:
-//  - inputNodes:  single node or array of nodes that will become parameters of the function.
-//                 Commonly, this list will include all Input()s that the outputNode(s) depend on.
-//  - outputNodes: single node or dictionary of nodes that the function will emit
-// Example:
-//    # create a BS function by copying a piece of network
-//    net = CloneFunction (network.features, network.logP)
-//    # apply the copy to a new input
-//    out = net (myFeatures)
-//    # This will create a copy of the subsection from network.features to network.logP
-//    # where all links to network.features get replaced by links to myFeatures.
-// Example with multiple input and output nodes:
-//    # create a BS function by copying a piece of network
-//    # This specific example converts a network back into a BrainScript function.
-//    # It passes two input nodes --> the BS function will have 2 inputs;
-//    # and it passes a record of output nodes --> the BS function will return a record with the same member names
-//    network = BS.Network.Load ('some.dnn')
-//    net = CloneFunction ((network.features:network.labels), [ ce = network.ce ; errs = network.errs ])
-//    # create a network from the BS function
-//    features = Input (13)
-//    labels = Input (42)
-//    out = net (features, labels)
-//    criterionNodes = (out.ce)
-//    evaluationNodes = (out.errs)
-// A specific example: Adapting a network, while using the original network as a regularizer (KLD)
-//    # load network
-//    network = BS.Network.Load ('some.dnn')
-//    # create a trainable clone and a read-only reference clone
-//    adaptNet = CloneFunction (network.features, [ z = network.z ], readOnly=false)
-//    # create a read-only clone
-//    refNet = CloneFunction (network.features, [ z = network.z ], readOnly=true)
-//    # create the main network
-//    features = Input (42)
-//    labels = Input (9000)
-//    z = adaptNet (features).z
-//    zRef = refNet (features).z
-//    # training criterion
-//    refWeight = 0.9
-//    kldLabels = labels * (1-refWeight) + Softmax (zRef) * refWeight  # interpolate with ref output
-//    ce = CrossEntropyWithSoftmax (z, kldLabels)
-//    errs = ClassificationError (z, labels)
-//    criterionNodes = (ce)
-//    evaluationNodes = (errs)
-// ===================================================================
-    
-                // check with expected values
-            Value().TransferFromDeviceToDevice(m_deviceId, CPUDEVICE, true);
-            if (!ISCLOSE(Value()(0, 0), 1.0, EPSILON) ||
-                !ISCLOSE(Value()(0, 1), 2.0, EPSILON) ||
-                !ISCLOSE(Value()(1, 1), 2.0, EPSILON))
-                throw('LSTMNode forward computation error');
-    
-      // Run this benchmark once for a number of values picked from the
-  // ranges [start..limit].  (starts and limits are always picked.)
-  // REQUIRES: The function passed to the constructor must accept arg1, arg2 ...
-  Benchmark* Ranges(const std::vector<std::pair<int, int> >& ranges);
+            FrameRange frameRange(Input(0)->GetMBLayout());
+        Input(0)->ValueFor(frameRange).VectorMax(*m_maxIndexes0, *m_maxValues, true);
+        Input(1)->ValueFor(frameRange).VectorMax(*m_maxIndexes1, *m_maxValues, true);
     
     
-    {  return benchmarks.size();
+    {    return Vec2(x,y);
 }
     
-    #include <cstdarg>
-#include <iostream>
-#include <string>
     
-      // Only add label to mean/stddev if it is same for all runs
-  Run rms;
-  big_o.report_label = reports[0].report_label;
-  rms.benchmark_name = benchmark_name + '_RMS';
-  rms.report_label = big_o.report_label;
-  rms.iterations = 0;
-  rms.real_accumulated_time = result_real.rms / multiplier;
-  rms.cpu_accumulated_time = result_cpu.rms / multiplier;
-  rms.report_rms = true;
-  rms.complexity = result_cpu.complexity;
-  // don't forget to keep the time unit, or we won't be able to
-  // recover the correct value.
-  rms.time_unit = reports[0].time_unit;
+    {    return false;
+}
     
-    #include 'counter.h'
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
     
-    #include <cstdlib>
+    @since v0.99.5
+@js cc.animationCache
+*/
+class CC_DLL AnimationCache : public Ref
+{
+public:
+    /**
+     * @js ctor
+     */
+    AnimationCache();
+    /**
+     * @js NA
+     * @lua NA
+     */
+    ~AnimationCache();
+    /** Returns the shared instance of the Animation cache 
+	 @js NA
+	*/
+    static AnimationCache* getInstance();
+    }
+    
+        /** Width of each char. */
+    int    _itemWidth;
+    /** Height of each char. */
+    int    _itemHeight;
+    
+    Color3B    _colorUnmodified;
+    
+    TextureAtlas* _textureAtlas;
+    /** Protocol variables. */
+    bool _isOpacityModifyRGB;
+    BlendFunc _blendFunc;
+    
+    PolygonInfo AutoPolygon::generatePolygon(const std::string& filename, const Rect& rect, float epsilon, float threshold)
+{
+    AutoPolygon ap(filename);
+    return ap.generateTriangles(rect, epsilon, threshold);
+}
