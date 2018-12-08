534589bbf3fc2f5b191ff113c378c7f8348cfec9
SUNsung <weatherius@gmail.com>
2018-12-08 11:59:00 +0200

    Rewrite [545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py] File in: 2018-12-08 11:58:59

--- pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
+++ pyton/545vVwGOshUEc544WBzNjj79n254yYdcNefuLX254lmb.py
@@ -1,92 +1,178 @@
 
         
         
-class PrivateChat(Chat):
+def test_cifar():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine')
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
+        (x_train, y_train), (x_test, y_test) = cifar100.load_data('coarse')
+        assert len(x_train) == len(y_train) == 50000
+        assert len(x_test) == len(y_test) == 10000
     
-        def override_category_budget(self, category, amount):
-        self.categories_to_budget_map[category] = amount
+        if isinstance(gpus, (list, tuple)):
+        if len(gpus) <= 1:
+            raise ValueError('For multi-gpu usage to be effective, '
+                             'call `multi_gpu_model` with `len(gpus) >= 2`. '
+                             'Received: `gpus=%s`' % gpus)
+        num_gpus = len(gpus)
+        target_gpu_ids = gpus
+    else:
+        if gpus <= 1:
+            raise ValueError('For multi-gpu usage to be effective, '
+                             'call `multi_gpu_model` with `gpus >= 2`. '
+                             'Received: `gpus=%d`' % gpus)
+        num_gpus = gpus
+        target_gpu_ids = range(num_gpus)
+    
+            from tensorflow.contrib.cudnn_rnn.python.ops import cudnn_rnn_ops
+        self._cudnn_lstm = cudnn_rnn_ops.CudnnLSTM(
+            num_layers=1,
+            num_units=self.units,
+            input_size=input_dim,
+            input_mode='linear_input')
+    
+                # Size of the square
+            w = np.random.randint(2, 4)
+    
+    print('Pad sequences (samples x time)')
+x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
+x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
+print('x_train shape:', x_train.shape)
+print('x_test shape:', x_test.shape)
+    
+        def test_decode(self):
+        self.assertEqual('x', self.field.decode('x'))
+    
+        @classmethod
+    # pylint: disable=arguments-differ,too-many-arguments
+    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
+        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
+        # jwk field if kid is not provided.
+        include_jwk = kid is None
+        return super(JWS, cls).sign(payload, key=key, alg=alg,
+                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
+                                    nonce=nonce, url=url, kid=kid,
+                                    include_jwk=include_jwk)
 
     
+        def test_kid_serialize(self):
+        from acme.jws import JWS
+        jws = JWS.sign(payload=b'foo', key=self.privkey,
+                       alg=jose.RS256, nonce=self.nonce,
+                       url=self.url, kid=self.kid)
+        self.assertEqual(jws.signature.combined.nonce, self.nonce)
+        self.assertEqual(jws.signature.combined.url, self.url)
+        self.assertEqual(jws.signature.combined.kid, self.kid)
+        self.assertEqual(jws.signature.combined.jwk, None)
+        # TODO: check that nonce is in protected header
     
-class RemoveDuplicateUrls(MRJob):
     
-        def move_to_front(self, node):
-        ...
+MOD_SSL_CONF_DEST = 'options-ssl-apache.conf'
+'''Name of the mod_ssl config file as saved in `IConfig.config_dir`.'''
     
-    from sklearn.dummy import DummyClassifier
+        def verify_fnmatch(self, arg, hit=True):
+        '''Test if Include was correctly parsed.'''
+        from certbot_apache import parser
+        self.parser.add_dir(parser.get_aug_path(self.parser.loc['default']),
+                            'Include', [arg])
+        if hit:
+            self.assertTrue(self.parser.find_dir('FNMATCH_DIRECTIVE'))
+        else:
+            self.assertFalse(self.parser.find_dir('FNMATCH_DIRECTIVE'))
+    
+            self.assertTrue(self.addr_default.conflicts(self.addr))
+        self.assertTrue(self.addr_default.conflicts(self.addr1))
+        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
+    
+            vhs = []
+        for match in vh_match:
+            # pylint: disable=protected-access
+            vhs.append(self.sni.configurator._create_vhost(match))
+        self.assertEqual(len(vhs), 2)
+        for vhost in vhs:
+            self.assertEqual(vhost.addrs, set([obj.Addr.fromstring('*:443')]))
+            names = vhost.get_names()
+            self.assertTrue(names in z_domains)
     
     
-def bench_isotonic_regression(Y):
+@callback
+def async_setup(hass, flash_briefing_config):
+    '''Activate Alexa component.'''
+    hass.http.register_view(
+        AlexaFlashBriefingView(hass, flash_briefing_config))
+    
+            self._hass.bus.fire(EVENT_NAME, {
+            EVENT_DATA_NAME: self.name,
+            EVENT_DATA_ADDRESS: self.address,
+            EVENT_DATA_QUEUED_TIME: time_diff,
+            EVENT_DATA_TYPE: hass_click_type
+        })
+    
+    For more details about this component, please refer to the documentation at
+https://home-assistant.io/components/browser/
+'''
+import voluptuous as vol
+    
+        def observe(call=None):
+        '''Observe three entities.'''
+        random_see('demo_paulus', 'Paulus')
+        random_see('demo_anne_therese', 'Anne Therese')
+    
+    import requests
+import voluptuous as vol
+    
+        def lost_iphone(call):
+        '''Call the lost iPhone function if the device is found.'''
+        accounts = call.data.get(ATTR_ACCOUNTNAME, ICLOUDTRACKERS)
+        devicename = call.data.get(ATTR_DEVICENAME)
+        for account in accounts:
+            if account in ICLOUDTRACKERS:
+                ICLOUDTRACKERS[account].lost_iphone(devicename)
+    
+        if not os.path.isdir(download_path):
+        _LOGGER.error(
+            'Download path %s does not exist. File Downloader not active',
+            download_path)
+    
+        def __init__(self, name, cfg):
+        '''Initialize the graph.'''
+        self._name = name
+        self._hours = cfg[CONF_HOURS_TO_SHOW]
+        self._refresh = cfg[CONF_REFRESH]
+        self._entities = cfg[CONF_ENTITIES]
+    
+    
+if __name__ == '__main__':
+    import doctest
+    
+        a_transaction = Transaction(True, num_obj)
+    try:
+        for i in range(3):
+            num_obj.increment()
+            print(num_obj)
+        a_transaction.commit()
+        print('-- committed')
+    
     '''
-    Runs a single iteration of isotonic regression on the input data,
-    and reports the total time taken (in seconds).
-    '''
-    gc.collect()
+http://code.activestate.com/recipes/131499-observer-pattern/
     
-                gc.collect()
-            print('benchmarking orthogonal_mp (with Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            orthogonal_mp(X, y, precompute=True,
-                          n_nonzero_coefs=n_informative)
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            omp_gram[i_f, i_s] = delta
+        def is_satisfied_by(self, candidate):
+        return bool(self._one.is_satisfied_by(candidate) or self._other.is_satisfied_by(candidate))
     
-        ###########################################################################
-    # Set transformer input
-    ###########################################################################
-    transformers = {}
+        sample_queue = queue.Queue()
     
-    Typical output
---------------
-    
-        mem_usage = memory_usage(run_vectorizer(Vectorizer, text, **params))
-    
-    from distutils.version import LooseVersion
-    
-        def prepare(self, **kwargs):
-    
-    __all__ = ['cbs_download']
-    
-    import json
-import re
-    
-    headers = {
-    'DNT': '1',
-    'Accept-Encoding': 'gzip, deflate, sdch, br',
-    'Accept-Language': 'en-CA,en;q=0.8,en-US;q=0.6,zh-CN;q=0.4,zh;q=0.2',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Cache-Control': 'max-age=0',
-    'Referer': 'http://www.dilidili.com/',
-    'Connection': 'keep-alive',
-    'Save-Data': 'on',
-}
-    
-    __all__ = ['douban_download']
-    
-        vids = matchall(content, yinyuetai_embed_patterns)
-    for vid in vids:
-        found = True
-        yinyuetai_download_by_id(vid, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
+        def register_object(self, name, obj):
+        '''Register an object'''
+        self._objects[name] = obj
     
     
-def fantasy_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    if 'fantasy.tv' not in url:
-        raise Exception('Wrong place!')
-    
-        def test_path_islink(self):
-        self.assertPathEqual(self.path.islink)
-    
-        The decoding-related arguments have the same semantics as those of
-    bytes.decode().
-    '''
-    resource = _normalize_path(resource)
-    package = _get_package(package)
-    with open_text(package, resource, encoding, errors) as fp:
-        return fp.read()
-    
-    print('Enter your SQL commands to execute in sqlite3.')
-print('Enter a blank line to exit.')
+class TimeDisplay(object):
+    def __init__(self):
+        pass
