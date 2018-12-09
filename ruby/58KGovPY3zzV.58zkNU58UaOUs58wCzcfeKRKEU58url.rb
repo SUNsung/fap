e935d07b9c5b4850e1e19d2ffd0b82303af998c5
SUNsung <weatherius@gmail.com>
2018-12-09 14:12:59 +0200

    Rewrite [58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb] File in: 2018-12-09 14:12:59

--- ruby/58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb
+++ ruby/58KGovPY3zzV.58zkNU58UaOUs58wCzcfeKRKEU58url.rb
@@ -1,197 +1,74 @@
 
         
-              command.args.each do |arg|
-        arg_value = arg.value
-        if arg.value_type.to_s.to_sym == :string_closure
-          closure = proc { |string_value| closure_argument_value = string_value }
-          arg_value = closure
+            def find_file(path, prefixes = [], *args)
+      _find_all(path, prefixes, args, true).first || raise(MissingTemplate.new(self, path, prefixes, *args))
+    end
+    
+        initializer 'action_view.caching' do |app|
+      ActiveSupport.on_load(:action_view) do
+        if app.config.action_view.cache_template_loading.nil?
+          ActionView::Resolver.caching = app.config.cache_classes
         end
-        parameter_map[arg.name.to_sym] = arg_value
-      end
-    
-    module FastlaneCore
-  class AnalyticsSession
-    GA_TRACKING = 'UA-121171860-1'
-    
-        def action_completed(action_name, status: nil, exception: nil)
-      # https://github.com/fastlane/fastlane/issues/11913
-      # if exception.nil? || exception.fastlane_should_report_metrics?
-      #   action_completion_context = FastlaneCore::ActionCompletionContext.context_for_action_name(action_name, args: ARGV, status: status)
-      #   FastlaneCore.session.action_completed(completion_context: action_completion_context)
-      # end
-    end
-    
-            expect(result).to eq('appledoc --project-name \'Project Name\' --project-company \'Company\' --exit-threshold \'2\' #{input_dir.shellescape}')
-      end
-    
-          it 'sets the platform to Mac' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              platform: 'Mac'
-            )
-          end').runner.execute(:test)
-    
-          it 'Handles tag names with characters that need shell escaping' do
-        tag = 'v1.8.0(30)'
-        result = Fastlane::FastFile.new.parse('lane :test do
-          changelog_from_git_commits(between: ['#{tag}', 'HEAD'])
-        end').runner.execute(:test)
-    
-            Fastlane::FastFile.new.parse('lane :test do
-          import_certificate ({
-            keychain_name: '#{keychain}',
-            certificate_path: '#{cert_name}',
-            certificate_password: '#{password}',
-            log_output: true
-          })
-        end').runner.execute(:test)
       end
     end
-  end
-end
-
     
-          it 'works with single quote in rule name' do
-        rule = 'CoveredSwitchStatementsDon'tNeedDefault'
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              enable_rules: [\'#{rule}\'],
-              disable_rules: [\'#{rule}\']
-            )
-          end').runner.execute(:test)
+    module ActionView
+  # This class defines the interface for a renderer. Each class that
+  # subclasses +AbstractRenderer+ is used by the base +Renderer+ class to
+  # render a specific type of object.
+  #
+  # The base +Renderer+ class uses its +render+ method to delegate to the
+  # renderers. These currently consist of
+  #
+  #   PartialRenderer - Used for rendering partials
+  #   TemplateRenderer - Used for rendering other types of templates
+  #   StreamingTemplateRenderer - Used for streaming
+  #
+  # Whenever the +render+ method is called on the base +Renderer+ class, a new
+  # renderer object of the correct type is created, and the +render+ method on
+  # that new object is called in turn. This abstracts the setup and rendering
+  # into a separate classes for partials and templates.
+  class AbstractRenderer #:nodoc:
+    delegate :find_template, :find_file, :template_exists?, :any_templates?, :with_fallbacks, :with_layout_format, :formats, to: :@lookup_context
     
-        os = 'windows'
-    shellescape_testcases.each do |testcase|
-      it testcase['it'] + ': ' + testcase['it_result'][os] do
-        str = testcase['str'].to_s
-        expect_correct_implementation_to_be_called(str, :shellescape, os)
-        escaped = str.shellescape
-        expect(escaped).to eq(testcase['expect'][os])
-      end
-    end
-  end
+          private
     
-          click_on 'Save'
-    
-      it 'requires a URL or file uplaod' do
-    visit new_scenario_imports_path
-    click_on 'Start Import'
-    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
-  end
-    
-      describe '#scenario_label' do
-    it 'creates a scenario label with the scenario name' do
-      expect(scenario_label(scenario)).to eq(
-        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Scene</span>'
-      )
-    end
-    
-              expect(scenario_import.scenario.name).to eq(name)
-          expect(scenario_import.scenario.description).to eq(description)
-          expect(scenario_import.scenario.guid).to eq(guid)
-          expect(scenario_import.scenario.tag_fg_color).to eq(tag_fg_color)
-          expect(scenario_import.scenario.tag_bg_color).to eq(tag_bg_color)
-          expect(scenario_import.scenario.icon).to eq(icon)
-          expect(scenario_import.scenario.source_url).to eq(source_url)
-          expect(scenario_import.scenario.public).to be_falsey
+            # Should be overriden if you have areas of your checkout that don't match
+        # up to a step within checkout_steps, such as a registration step
+        def skip_state_validation?
+          false
         end
     
-      it 'provides hash-style access to its properties with both symbol and string keys' do
-    expect(location[:lat]).to be_a Float
-    expect(location[:lat]).to eq 2.0
-    expect(location['lat']).to be_a Float
-    expect(location['lat']).to eq 2.0
-  end
-    
-        it 'cleans up old logs when there are more than log_length' do
-      stub(AgentLog).log_length { 4 }
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 1')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 2')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 3')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 4')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 4')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 1')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 5')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 5')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 2')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 6')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 6')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 3')
-    end
-    
-    gemspec
-    
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
-    
-      # Set the OmniAuth path prefix so it can be overridden when
-  # Devise is used in a mountable engine
-  mattr_accessor :omniauth_path_prefix
-  @@omniauth_path_prefix = nil
-    
-          # Forgets the given resource by deleting a cookie
-      def forget_me(resource)
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.forget_me!
-        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
-      end
-    
-            if uri 
-          path = remove_domain_from_uri(uri)
-          path = add_fragment_back_to_path(uri, path)
-    
-          selected_modules = modules.map(&:to_sym).uniq.sort_by do |s|
-        Devise::ALL.index(s) || -1  # follow Devise::ALL order
-      end
-    
-    describe 'Kernel.throw' do
-  it 'transfers control to the end of the active catch block waiting for symbol' do
-    catch(:blah) do
-      :value
-      throw :blah
-      fail('throw didn't transfer the control')
-    end.should be_nil
-  end
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
-    
-              # Encodes a Rex::Proto::Kerberos::Model::AuthorizationData into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            seqs = []
-            elements.each do |elem|
-              elems = []
-              type_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_type(elem[:type])], 0, :CONTEXT_SPECIFIC)
-              elems << type_asn1
-              data_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_data(elem[:data])], 1, :CONTEXT_SPECIFIC)
-              elems << data_asn1
-              seqs << OpenSSL::ASN1::Sequence.new(elems)
-            end
-    
-              # Decodes the req_body from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::KdcRequestBody]
-          def decode_asn1_req_body(input)
-            Rex::Proto::Kerberos::Model::KdcRequestBody.decode(input.value[0])
+            def scope
+          if params[:product_id]
+            Spree::Product.friendly.find(params[:product_id])
+          elsif params[:variant_id]
+            Spree::Variant.find(params[:variant_id])
           end
         end
       end
     end
   end
 end
+
+    
+            def update
+          @option_type = Spree::OptionType.accessible_by(current_ability, :update).find(params[:id])
+          if @option_type.update_attributes(option_type_params)
+            render :show
+          else
+            invalid_resource!(@option_type)
+          end
+        end
+    
+            def new
+          authorize! :admin, ReturnAuthorization
+        end
+    
+              count_on_hand = 0
+          if params[:stock_item].key?(:count_on_hand)
+            count_on_hand = params[:stock_item][:count_on_hand].to_i
+            params[:stock_item].delete(:count_on_hand)
+          end
+    
+            private
