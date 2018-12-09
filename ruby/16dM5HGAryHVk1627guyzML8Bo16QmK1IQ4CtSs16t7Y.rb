52e0140423ebfe12e9b76015f7eccda0929aa955
SUNsung <weatherius@gmail.com>
2018-12-09 05:23:44 +0200

    Rewrite [16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb] File in: 2018-12-09 05:23:43

--- ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
+++ ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
@@ -1,17 +1,141 @@
 
         
-          # set version
-  content.sub! /(s\.version.*=\s+).*/, '\\1\'#{Rack::Protection::VERSION}\''
+        UserOption.where(user_id: -1).update_all(
+  email_private_messages: false,
+  email_direct: false
+)
     
-            begin
-          token = decode_token(token)
-        rescue ArgumentError # encoded_masked_token is invalid Base64
-          return false
+            unless post && post.id
+          puts post.errors.full_messages if post
+          puts creator.errors.inspect
+          raise 'Failed to create description for trust level 3 lounge!'
         end
     
-          def has_vector?(request, headers)
-        return false if request.xhr?
-        return false if options[:allow_if] && options[:allow_if].call(request.env)
-        return false unless headers['Content-Type'].to_s.split(';', 2).first =~ /^\s*application\/json\s*$/
-        origin(request.env).nil? and referrer(request.env) != request.host
+        # This will detect the proper guest OS for the machine and set up
+    # the class to actually execute capabilities.
+    def detect!
+      guest_name = @machine.config.vm.guest
+      initialize_capabilities!(guest_name, @guests, @capabilities, @machine)
+    rescue Errors::CapabilityHostExplicitNotDetected => e
+      raise Errors::GuestExplicitNotDetected, value: e.extra_data[:value]
+    rescue Errors::CapabilityHostNotDetected
+      raise Errors::GuestNotDetected
+    end
+    
+              # We split the arguments into two: One set containing any
+          # flags before a word, and then the rest. The rest are what
+          # get actually sent on to the subcommand.
+          argv.each_index do |i|
+            if !argv[i].start_with?('-')
+              # We found the beginning of the sub command. Split the
+              # args up.
+              main_args   = argv[0, i]
+              sub_command = argv[i]
+              sub_args    = argv[i + 1, argv.length - i + 1]
+    
+            # The VM which this system is tied to.
+        attr_reader :vm
+    
+            # This method is expected to return a class that is used for
+        # configuring the provisioner. This return value is expected to be
+        # a subclass of {Config}.
+        #
+        # @return [Config]
+        def self.config_class
+        end
+    
+    require 'vagrant/util/safe_puts'
+    
+            # A default to_s implementation.
+        def to_s
+          self.class.to_s
+        end
+    
+        # Get a value by the given key.
+    #
+    # This will evaluate the block given to `register` and return the
+    # resulting value.
+    def get(key)
+      return nil if !@items.key?(key)
+      return @results_cache[key] if @results_cache.key?(key)
+      @results_cache[key] = @items[key].call
+    end
+    alias :[] :get
+    
+        cmd = HOMEBREW_INTERNAL_COMMAND_ALIASES.fetch(ARGV.first, ARGV.first)
+    
+          new_initial_revision_var = 'HOMEBREW_UPDATE_BEFORE#{repo_var}'
+      new_current_revision_var = 'HOMEBREW_UPDATE_AFTER#{repo_var}'
+    
+      # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both thread web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
+    
+          ext = @options[:from]
+      Sass::Util.glob('#{@options[:input]}/**/*.#{ext}') do |f|
+        output =
+          if @options[:in_place]
+            f
+          elsif @options[:output]
+            output_name = f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
+            output_name[0...@options[:input].size] = @options[:output]
+            output_name
+          else
+            f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
+          end
+    
+        def input_and_output(opts)
+      opts.separator ''
+      opts.separator 'Input and Output:'
+    
+          # Get the cache key pair for the given Sass URI.
+      # The URI need not be checked for validity.
+      #
+      # The only strict requirement is that the returned pair of strings
+      # uniquely identify the file at the given URI.
+      # However, the first component generally corresponds roughly to the directory,
+      # and the second to the basename, of the URI.
+      #
+      # Note that keys must be unique *across importers*.
+      # Thus it's probably a good idea to include the importer name
+      # at the beginning of the first component.
+      #
+      # @param uri [String] A URI known to be valid for this importer.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` currently being checked.
+      # @return [(String, String)] The key pair which uniquely identifies
+      #   the file at the given URI.
+      def key(uri, options)
+        Sass::Util.abstract(self)
       end
+    
+    class ConfigTag < Liquid::Tag
+  def initialize(tag_name, options, tokens)
+    super
+    options = options.split(' ').map {|i| i.strip }
+    @key = options.slice!(0)
+    @tag = nil
+    @classname = nil
+    options.each do |option|
+      @tag = $1 if option =~ /tag:(\S+)/ 
+      @classname = $1 if option =~ /classname:(\S+)/
+    end
+  end
+    
+          cache(gist, file, data.body) unless @cache_disabled
+      data.body
+    end
+    
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+      # Summary is used on the Archive pages to return the first block of content from a post.
+  def summary(input)
+    if input.index(/\n\n/)
+      input.split(/\n\n/)[0]
+    else
+      input
+    end
+  end
