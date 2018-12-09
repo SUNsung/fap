1253c3baf8c61983827c31953ab728aba0bcfd4b
SUNsung <weatherius@gmail.com>
2018-12-09 04:17:55 +0200

    Rewrite [09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp] File in: 2018-12-09 04:17:54

--- cccccc/09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp
+++ cccccc/09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp
@@ -1,97 +1,259 @@
 
         
-        // This template function declaration is used in defining arraysize.
-// Note that the function doesn't need an implementation, as we only
-// use its type.
-template <typename T, size_t N>
-char (&ArraySizeHelper(T (&array)[N]))[N];
+        namespace tensorflow {
+    }
     
-    #endif  // CHECK_H_
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/platform/mutex.h'
+#include 'tensorflow/python/lib/core/bfloat16.h'
+#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
+    
+    #endif  // TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
 
     
-    // Parses 'str' for a double.  If successful, writes the result to *value and
-// returns true; otherwise leaves *value unchanged and returns false.
-bool ParseDouble(const std::string& src_text, const char* str, double* value) {
-  // Parses the environment variable as a decimal integer.
-  char* end = nullptr;
-  const double double_value = strtod(str, &end);  // NOLINT
-    }
+    #include 'tensorflow/python/lib/core/numpy.h'
     
-    namespace benchmark {
-    }
-    
-    namespace benchmark {
-namespace internal {
-    }
-    }
-    
-    void JSONReporter::ReportRuns(std::vector<Run> const& reports) {
-  if (reports.empty()) {
-    return;
-  }
-  std::string indent(4, ' ');
-  std::ostream& out = GetOutputStream();
-  if (!first_report_) {
-    out << ',\n';
-  }
-  first_report_ = false;
-    }
-    
-    inline bool Regex::Match(const std::string& str) {
-  if (!init_) {
-    return false;
-  }
-  return regexec(&re_, str.c_str(), 0, nullptr, 0) == 0;
-}
-#endif
-    
-    #include <cerrno>
-#include <cstdlib>
-#include <ctime>
-    
-      double CompareBlock(const OutputImage& img,
-                      int off_x, int off_y) const override;
-    
-    void IDCT1d(const double* in, int stride, double* out) {
-  for (int x = 0; x < 8; ++x) {
-    out[x * stride] = 0.0;
-    for (int u = 0; u < 8; ++u) {
-      out[x * stride] += kDCTMatrix[8 * u + x] * in[u * stride];
-    }
-  }
+    Safe_TF_StatusPtr make_safe(TF_Status* status) {
+  return Safe_TF_StatusPtr(status);
 }
     
-    namespace guetzli {
+    string TryFindKernelClass(const string& serialized_node_def) {
+  tensorflow::NodeDef node_def;
+  if (!node_def.ParseFromString(serialized_node_def)) {
+    LOG(WARNING) << 'Error parsing node_def';
+    return '';
+  }
+    }
+    
+      // Returns the compute capability for the device; i.e (3, 5).
+  // This is currently done via the deprecated device API.
+  // http://docs.nvidia.com/cuda/cuda-driver-api/group__CUDA__DEVICE__DEPRECATED.html#group__CUDA__DEVICE__DEPRECATED_1ge2091bbac7e1fb18c2821612115607ea
+  static port::Status GetComputeCapability(int *cc_major, int *cc_minor,
+                                           CUdevice device);
+    
+        void operator() (const typename internal::VecTraits<T>::vec128 & v_src0,
+                     const typename internal::VecTraits<T>::vec128 & v_src1,
+                     typename internal::VecTraits<T>::vec128 & v_dst) const
+    {
+        v_dst = internal::vabdq(v_src0, v_src1);
+    }
+    
+    struct BitwiseOr
+{
+    typedef u8 type;
+    }
+    
+    bool isBlur3x3Supported(const Size2D &size, BORDER_MODE border)
+{
+    return isSupportedConfiguration() && size.width >= 8 &&
+        (border == BORDER_MODE_CONSTANT ||
+            border == BORDER_MODE_REPLICATE);
+}
+    
+                uint16x8_t v_mask0 = vorrq_u16(vceqq_u16(v_src0, v_maxval8), vceqq_u16(v_src0, v_minval8));
+            uint16x8_t v_mask1 = vorrq_u16(vceqq_u16(v_src1, v_maxval8), vceqq_u16(v_src1, v_minval8));
+    
+                    uint16x8_t vSum_0_4 = vaddq_u16(vLane0.val[0], vLane4.val[0]);
+                uint16x8_t vSum_1_5 = vaddq_u16(vLane0.val[1], vLane4.val[1]);
+                uint16x8_t vSum_2_6 = vaddq_u16(vLane0.val[2], vLane4.val[2]);
+    
+    void Laplacian5OpenCV(const Size2D &size,
+                      const u8 * srcBase, ptrdiff_t srcStride,
+                      s16 * dstBase, ptrdiff_t dstStride,
+                      BORDER_MODE border, u8 borderValue)
+{
+    internal::assertSupportedConfiguration(isLaplacianOpenCVSupported(size, border));
+#ifdef CAROTENE_NEON
+    ptrdiff_t rows = size.height, cols = size.width;
+    }
+    
+      bool IsPeerAuthenticated() const override;
+    
+      grpc_error* Init(grpc_call_element* elem,
+                   const grpc_call_element_args* args) override;
+    
+    // Serializes the outgoing stats context.  Field IDs are 1 byte followed by
+// field data. A 1 byte version ID is always encoded first. Tags are directly
+// serialized into the given grpc_slice.
+size_t StatsContextSerialize(size_t max_tags_len, grpc_slice* tags);
+    
+        int expected_num_data = param_.num_weight + param_.num_data;
+    CHECK(in_data.size() == expected_num_data && in_grad.size() == expected_num_data);
+    CHECK_EQ(req.size(), expected_num_data);
+    
+    MXNET_DLL int MXCVcopyMakeBorder(NDArrayHandle src,
+                                 const int top,
+                                 const int bot,
+                                 const int left,
+                                 const int right,
+                                 const int type,
+                                 const double value,
+                                 NDArrayHandle *out) {
+  API_BEGIN();
+  NDArray ndsrc = *static_cast<NDArray*>(src);
+  CHECK_EQ(ndsrc.shape().ndim(), 3);
+  CHECK_EQ(ndsrc.ctx(), Context::CPU());
+  CHECK_EQ(ndsrc.dtype(), mshadow::kUint8);
+    }
+    
+     private:
+  /*!
+   * \brief Wait for all started threads to signal that they're ready
+   */
+  void WaitForReady() {
+    for (const std::shared_ptr<dmlc::ManualEvent>& ptr : ready_events_) {
+      ptr->wait();
+    }
+  }
+    
+      for (uint32_t nid = 0; nid < idx.num_nodes(); ++nid) {
+    const auto& inode = idx[nid];
+    if (inode.source->op() != ewise_plus_op) continue;
+    int sid = storage_id[idx.entry_id(inode.inputs[0])];
+    if (sid != storage_id[idx.entry_id(nid, 0)]) continue;
+    if (idx[inode.inputs[0].node_id].source->is_variable()) continue;
+    if (idx[inode.inputs[1].node_id].source->is_variable()) continue;
+    uint32_t eid_rhs  = idx.entry_id(inode.inputs[1]);
+    if (ref_count[eid_rhs] != 1) continue;
+    if (inode.inputs[0].node_id >= inode.inputs[1].node_id) continue;
+    // TODO(haibin) support inplace addto for Dynamic Storage
+    if (storage_id[eid_rhs] == kDynamicStorageID) continue;
+    CHECK_NE(storage_id[eid_rhs], sid);
+    storage_id[eid_rhs] = sid;
+    addto_entry[eid_rhs] = 1;
+    storage_inplace_index[eid_rhs] = -1;
+    skip_plus_node[nid] = 1;
+  }
+    
+    ActionCamera* ActionCamera::clone() const
+{
+    auto action = new (std::nothrow) ActionCamera();
+    if (action)
+    {
+        action->autorelease();
+        return action;
+    }
+    
+    delete action;
+    return nullptr;
+}
+    
+    NS_CC_BEGIN
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    void Twirl::update(float time)
+{
+    int i, j;
+    Vec2    c = _position;
+    
+    for (i = 0; i < (_gridSize.width+1); ++i)
+    {
+        for (j = 0; j < (_gridSize.height+1); ++j)
+        {
+            Vec3 v = getOriginalVertex(Vec2(i ,j));
+            
+            Vec2 avg(i-(_gridSize.width/2.0f), j-(_gridSize.height/2.0f));
+            float r = avg.getLength();
+            
+            float amp = 0.1f * _amplitude * _amplitudeRate;
+            float a = r * cosf( (float)M_PI/2.0f + time * (float)M_PI * _twirls * 2 ) * amp;
+            
+            Vec2 d(
+                sinf(a) * (v.y-c.y) + cosf(a) * (v.x-c.x),
+                cosf(a) * (v.y-c.y) - sinf(a) * (v.x-c.x));
+            
+            v.x = c.x + d.x;
+            v.y = c.y + d.y;
+    }
+    }
     }
     
     
-    {  tmp0 = in[7 * stride];
-  tmp1 = kIDCTMatrix[ 7] * tmp0;
-  tmp2 = kIDCTMatrix[15] * tmp0;
-  tmp3 = kIDCTMatrix[23] * tmp0;
-  tmp4 = kIDCTMatrix[31] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] += tmp3;
-  out[3] += tmp4;
-  out[4] -= tmp4;
-  out[5] -= tmp3;
-  out[6] -= tmp2;
-  out[7] -= tmp1;
+    {private:
+    CC_DISALLOW_COPY_AND_ASSIGN(CallFuncN);
+};
+    
+        /** Removes all actions from a certain target.
+     All the actions that belongs to the target will be removed.
+     *
+     * @param target    A certain target.
+     */
+    virtual void removeAllActionsFromTarget(Node *target);
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+                // Y
+            coords.bl.y += ( rand() % (_randrange*2) ) - _randrange;
+            coords.br.y += ( rand() % (_randrange*2) ) - _randrange;
+            coords.tl.y += ( rand() % (_randrange*2) ) - _randrange;
+            coords.tr.y += ( rand() % (_randrange*2) ) - _randrange;
+    
+        // Overrides
+    virtual void startWithTarget(Node *target) override;
+    virtual void update(float time) override;
+    virtual ShuffleTiles* clone() const override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    ShuffleTiles() {}
+    virtual ~ShuffleTiles();
+    
+    
+    {    CC_SAFE_RELEASE(_spriteFrame);
 }
     
-    #include <assert.h>
-#include <string.h>
+    void PolygonInfo::setTriangles(const TrianglesCommand::Triangles& other)
+{
+    this->releaseVertsAndIndices();
+    _isVertsOwner = false;
     
-    #ifndef GUETZLI_JPEG_DATA_DECODER_H_
-#define GUETZLI_JPEG_DATA_DECODER_H_
+    this->triangles.vertCount = other.vertCount;
+    this->triangles.indexCount = other.indexCount;
+    this->triangles.verts = other.verts;
+    this->triangles.indices = other.indices;
+}
     
-    #define VERIFY_INPUT(var, low, high, code)                              \
-  if (var < low || var > high) {                                        \
-    fprintf(stderr, 'Invalid %s: %d\n', #var, static_cast<int>(var));   \
-    jpg->error = JPEG_INVALID_ ## code;                                 \
-        return false;                                                   \
-  }
+      env.now_micros_ += 100u;  // sleep credit 200
+  // One refill: 10240 fileed, sleep credit generates 2000. 8000 used
+  //             7240 + 10240 + 2000 - 8000 = 11480 left
+  ASSERT_EQ(static_cast<uint64_t>(1024u), controller.GetDelay(&env, 8000u));
+    
+    Status GetMemTableRepFactoryFromString(
+    const std::string& opts_str,
+    std::unique_ptr<MemTableRepFactory>* new_mem_factory);
+    
+    struct DumpOptions {
+  // Database that will be dumped
+  std::string db_path;
+  // File location that will contain dump output
+  std::string dump_location;
+  // Don't include db information header in the dump
+  bool anonymous = false;
+};
     
     
-    {}  // namespace guetzli
+    {}  // namespace rocksdb
