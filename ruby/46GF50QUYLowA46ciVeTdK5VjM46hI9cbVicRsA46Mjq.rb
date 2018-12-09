0157a6682ea51b1f23972adb2cfab6658d9955be
SUNsung <weatherius@gmail.com>
2018-12-09 07:38:52 +0200

    Rewrite [46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb] File in: 2018-12-09 07:38:51

--- ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
+++ ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
@@ -1,125 +1,184 @@
 
         
-            it 'shows the dry run pop up with previous events and allows use previously received event' do
-      emitter.events << Event.new(payload: {url: 'http://xkcd.com/'})
-      agent.sources << emitter
-      agent.options.merge!('url' => '', 'url_from_event' => '{{url}}')
-      agent.save!
-    
-      it 'asks to accept conflicts when the scenario was modified' do
-    DefaultScenarioImporter.seed(user)
-    agent = user.agents.where(name: 'Rain Notifier').first
-    agent.options['expected_receive_period_in_days'] = 9001
-    agent.save!
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This Scenario already exists in your system.')
-    expect(page).to have_text('9001')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
-    
-        it 'respects an environment variable that specifies a path or URL to a different scenario' do
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
-    end
-    
-              expect(scenario_import.import).to be_falsey
-    
-        def to_json
-      JSON.generate(as_json)
-    end
-    
-        def parse_as_document
-      document = Nokogiri::HTML.parse @content, nil, 'UTF-8'
-      @title = document.at_css('title').try(:content)
-      document
-    end
-    
-        def request(urls, options = {}, &block)
-      requests = [urls].flatten.map do |url|
-        build_and_queue_request(url, options, &block)
-      end
-      requests.length == 1 ? requests.first : requests
-    end
-    
-        class << self
-      attr_accessor :dir
-    
-        def log(msg)
-      puts '\r' + justify(msg)
-    end
-    
-          if base == dest
-        ''
-      elsif dest.start_with? File.join(base, '')
-        url.path[(path.length)..-1]
-      end
-    end
-    
-            css('tr[style]').each do |node|
-          node.remove_attribute 'style'
-        end
-    
-        # @param options [{Symbol => Object}] The options hash. See
-    #   {file:SASS_REFERENCE.md#Options the Sass options documentation}.
-    # @param parent [Environment] See \{#parent}
-    def initialize(parent = nil, options = nil)
-      @parent = parent
-      @options = options || (parent && parent.options) || {}
-      @stack = @parent.nil? ? Sass::Stack.new : nil
-      @caller = nil
-      @content = nil
-      @filename = nil
-      @functions = nil
-      @mixins = nil
-      @selector = nil
-      @vars = nil
-    end
-    
-        # Same as `Kernel.puts`, but doesn't print anything if the `--quiet` option is set.
-    #
-    # @param args [Array] Passed on to `Kernel.puts`
-    def puts(*args)
-      return if @options[:for_engine][:quiet]
-      Kernel.puts(*args)
-    end
-    
-          opts.on('--trace', :NONE, 'Show a full Ruby stack trace on error') do
-        @options[:trace] = true
-      end
-    end
-    
-          # Get the cache key pair for the given Sass URI.
-      # The URI need not be checked for validity.
-      #
-      # The only strict requirement is that the returned pair of strings
-      # uniquely identify the file at the given URI.
-      # However, the first component generally corresponds roughly to the directory,
-      # and the second to the basename, of the URI.
-      #
-      # Note that keys must be unique *across importers*.
-      # Thus it's probably a good idea to include the importer name
-      # at the beginning of the first component.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [(String, String)] The key pair which uniquely identifies
-      #   the file at the given URI.
-      def key(uri, options)
-        Sass::Util.abstract(self)
+                [
+          'This will automatically tag your build with the following format: `<grouping>/<lane>/<prefix><build_number>`, where:'.markdown_preserve_newlines,
+          list,
+          'For example, for build 1234 in the 'appstore' lane, it will tag the commit with `builds/appstore/1234`.'
+        ].join('\n')
       end
     
-          # Returns the body of the `for` loop.
-      #
-      # @return [Node, nil] The body of the `for` loop.
-      def body
-        node_parts[2]
+          it 'allows you to force the tag creation' do
+        tag = '2.0.0'
+        message = 'message'
+    
+            expect(result).to eq('appledoc --project-name \'Project Name\' --project-company \'Company\' --verbose \'1\' --exit-threshold \'2\' input/dir')
       end
     end
   end
 end
+
+    
+            inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
+        pseudocommand = 'git log --pretty=\'%B\' #{inner_command.shellescape}...HEAD --no-merges'
+        expect(result).to eq(pseudocommand)
+      end
+    
+          it 'treats a keychain name it cannot find in ~/Library/Keychains as the full keychain path' do
+        tmp_path = Dir.mktmpdir
+        keychain = '#{tmp_path}/my/special.keychain'
+        expected_command = 'security import item.path -k '#{keychain}' -P #{''.shellescape} -T /usr/bin/codesign -T /usr/bin/security &> /dev/null'
+    
+          it 'deactivates an existing user' do
+        visit admin_users_path
+        expect(page).to have_no_text('inactive')
+        find(:css, 'a[href='/admin/users/#{users(:bob).id}/deactivate']').click
+        expect(page).to have_text('inactive')
+        users(:bob).reload
+        expect(users(:bob)).not_to be_active
+      end
+    
+              @bar3 = Agents::DotBar.new(name: 'bar3').tap { |agent|
+            agent.user = users(:bob)
+            agent.sources << @bar2
+            agent.save!
+          },
+        ]
+        @foo.reload
+        @bar2.reload
+    
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
+    end
+    
+        it 'should work with nested hashes' do
+      @agent.options['very'] = {'nested' => '$.value'}
+      LiquidMigrator.convert_all_agent_options(@agent)
+      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'very' => {'nested' => '{{value}}'}, 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
+    end
+    
+      let :new_template do
+    {
+      'description' => '{{ hovertext }}',
+      'comment' => '{{ comment }}'
+    }
+  end
+    
+        def javascripts_path
+      File.join assets_path, 'javascripts'
+    end
+    
+    class Converter
+  extend Forwardable
+  include Network
+  include LessConversion
+  include JsConversion
+  include FontsConversion
+    
+        def initialize(*args)
+      @s = StringScanner.new(*args)
+    end
+    
+        def silence_log
+      @silence = true
+      yield
+    ensure
+      @silence = false
+    end
+  end
+end
+
+    
+    class PolymorphicMentions < ActiveRecord::Migration[4.2]
+  def change
+    remove_index :mentions, column: %i(post_id)
+    remove_index :mentions, column: %i(person_id post_id), unique: true
+    rename_column :mentions, :post_id, :mentions_container_id
+    add_column :mentions, :mentions_container_type, :string
+    add_index :mentions,
+              %i(mentions_container_id mentions_container_type),
+              name:   'index_mentions_on_mc_id_and_mc_type',
+              length: {mentions_container_type: 191}
+    add_index :mentions,
+              %i(person_id mentions_container_id mentions_container_type),
+              name:   'index_mentions_on_person_and_mc_id_and_mc_type',
+              length: {mentions_container_type: 191},
+              unique: true
+    
+    When /^I try to sign in manually$/ do
+  manual_login
+end
+    
+      aspect_id = @me.aspects.length == 1 ? @me.aspects.first.id : @me.aspects.where(:name => 'Besties').first.id
+  contacts.each do |contact|
+    aspect_memberships << AspectMembership.new(:contact_id => contact.id, :aspect_id => aspect_id)
+  end
+  AspectMembership.import(aspect_memberships)
+end
+    
+      def post_path_by_content(text)
+    p = Post.find_by_text(text)
+    post_path(p)
+  end
+    
+      end
+    
+        it 'returns likes for a public post without login' do
+      post = alice.post(:status_message, text: 'hey', public: true)
+      bob.like!(post)
+      sign_out :user
+      get :index, params: {post_id: post.id}, format: :json
+      expect(JSON.parse(response.body).map {|h| h['id'] }).to match_array(post.likes.map(&:id))
+    end
+    
+          it 'should not create the participation' do
+        post :create, params: {post_id: @post.id}
+        expect(alice.participations.where(:target_id => @post.id)).not_to exist
+        expect(response.code).to eq('403')
+      end
+    end
+  end
+    
+      it 'ignores implicit hashes' do
+    expect_no_offenses(<<-RUBY.strip_indent)
+      foo(a: 1,
+      b: 2)
+    RUBY
+  end
+    
+          # The body of the method definition.
+      #
+      # @note this can be either a `begin` node, if the method body contains
+      #       multiple expressions, or any other node, if it contains a single
+      #       expression.
+      #
+      # @return [Node] the body of the method definition
+      def body
+        node_parts[0]
+      end
+    
+          # Returns an array of all the keys in the `hash` literal.
+      #
+      # @return [Array<Node>] an array of keys in the `hash` literal
+      def keys
+        each_key.to_a
+      end
+    
+      def migration_class_name
+    migration_name.camelize
+  end
+    
+        def add_active_record_callbacks
+      name = @name
+      @klass.send(:after_save) { send(name).send(:save) }
+      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
+      if @klass.respond_to?(:after_commit)
+        @klass.send(:after_commit, on: :destroy) do
+          send(name).send(:flush_deletes)
+        end
+      else
+        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
+      end
+    end
