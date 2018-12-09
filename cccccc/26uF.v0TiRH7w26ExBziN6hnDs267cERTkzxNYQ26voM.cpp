a38407bf09ad36f554132486561d907f7fe563f8
SUNsung <weatherius@gmail.com>
2018-12-09 06:44:53 +0200

    Rewrite [26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp] File in: 2018-12-09 06:44:52

--- cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
+++ cccccc/26uF.v0TiRH7w26ExBziN6hnDs267cERTkzxNYQ26voM.cpp
@@ -1,187 +1,270 @@
 
         
-        // Calls the registered C++ shape inference function for <node> (a serialized
-// NodeDef).
-// Should not be called for shape functions that access input tensors; constant
-// input tensor values are not made available, and so the inferred shapes will
-// be less precise than they could be.
-//
-// Returns an error, or OK, in <out_status> according to whether the shape
-// inference was successful.
-//
-// On success, returns a vector populated with the inferred output shapes (as
-// serialized CppShapeInferenceResult protos) followed by a serialized
-// CppShapeInferenceInputsNeeded proto.
-//
-// This is temporary code to be used during the migration
-// from python shape inference functions to C++ shape inference functions.
-std::vector<string> RunCppShapeInference(
-    int graph_def_version, const string& serialized_node_def,
-    const std::vector<string>& input_serialized_shapes,
-    PyObject* input_constant_tensor_values,
-    const std::vector<string>& input_constant_tensor_as_shape_values,
-    TF_Status* out_status);
+        #ifndef ATOM_BROWSER_API_ATOM_API_IN_APP_PURCHASE_H_
+#define ATOM_BROWSER_API_ATOM_API_IN_APP_PURCHASE_H_
     
-    #endif  // TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_INTERNAL_H_
-
+    using atom::api::Net;
+using atom::api::URLRequest;
     
-    // Unlike test_ops.cc, these are built with the 'require_shapes' option in the
-// BUILD file.
-    
-      int width = 35;
-  int width_narrow = 15;
-  int width_wide = 20;
-  os << std::setw(width + 1) << 'Op,';
-  os << std::setw(width_narrow + 1) << 'Count,';
-  os << std::setw(width_wide + 1) << 'Measured time (ns),';
-  os << std::setw(width_narrow + 2) << 'Time percent,';
-  os << std::setw(width_narrow + 2) << 'Acc percent,';
-  os << std::setw(width_wide + 1) << 'Analytical upper,';
-  os << std::setw(width_wide + 1) << 'Analytical lower,';
-  os << std::setw(width_narrow + 2) << 'Overall eff';
-  os << std::setw(width_narrow + 2) << 'Compute eff';
-  os << std::setw(width_narrow + 2) << 'Memory eff' << std::endl;
-  float acc_percent = 0;
-  for (const auto& op : ops_) {
-    double percent = static_cast<double>(op.time) /
-                     static_cast<double>(total_time_measured_serialized_);
-    double eff =
-        static_cast<double>(op.time_upper) / static_cast<double>(op.time);
-    double compute_eff =
-        static_cast<double>(op.compute_time) / static_cast<double>(op.time);
-    double memory_eff =
-        static_cast<double>(op.memory_time) / static_cast<double>(op.time);
-    os << std::setw(width) << op.name << ',';
-    os << std::setw(width_narrow) << op.count << ',';
-    os << std::setw(width_wide) << op.time << ',';
-    os << std::setw(width_narrow) << std::setprecision(2) << percent * 100
-       << '%,';
-    acc_percent += percent;
-    os << std::setw(width_narrow) << std::setprecision(2) << acc_percent * 100
-       << '%,';
-    os << std::setw(width_wide) << op.time_upper << ',';
-    os << std::setw(width_wide) << op.time_lower << ',';
-    os << std::setw(width_narrow) << std::setprecision(2) << eff * 100 << '%,';
-    os << std::setw(width_narrow) << std::setprecision(2) << compute_eff * 100
-       << '%,';
-    os << std::setw(width_narrow) << std::setprecision(2) << memory_eff * 100
-       << '%,';
-    os << std::endl;
-  }
-  os << std::endl;
-    
-    #include 'tensorflow/c/c_api.h'
-#include 'tensorflow/c/tf_status_helper.h'
-#include 'tensorflow/core/framework/tensor.h'
-#include 'tensorflow/python/lib/core/safe_ptr.h'
-    
-    // Must be included first
-#include 'tensorflow/python/lib/core/numpy.h'
-    
-    // This file contains APIs that assume a StreamExecutor is backed by CUDA.
-// It reaches into the CUDA implementation to activate an underlying CUDA
-// context.
-//
-// Having this file separate from cuda_gpu_executor.h means that dependent
-// code does not also have to depend on cuda.h.
-    
-    #include 'intsimdmatrix.h'
-#include 'genericvector.h'      // for GenericVector
-#include 'intsimdmatrixavx2.h'  // for IntSimdMatrixAVX2
-#include 'intsimdmatrixsse.h'   // for IntSimdMatrixSSE
-#include 'matrix.h'             // for GENERIC_2D_ARRAY
-#include 'simddetect.h'         // for SIMDDetect
-    
-    #ifdef __SSE4_1__
-// Computes part of matrix.vector v = Wu. Computes 1 result.
-static void PartialMatrixDotVector1(const int8_t* wi, const double* scales,
-                                    const int8_t* u, int num_in, int num_out,
-                                    double* v) {
-  int total = IntDotProductSSE(u, wi, num_in);
-  // Add in the bias and correct for integer values.
-  *v = (static_cast<double>(total) / INT8_MAX + wi[num_in]) * *scales;
+    void PowerMonitor::QuerySystemIdleTime(const ui::IdleTimeCallback& callback) {
+  ui::CalculateIdleTime(callback);
 }
-#endif  // __SSE4_1__
     
-      /**
-   * Returns an image of the current object at the given level in greyscale
-   * if available in the input. To guarantee a binary image use BinaryImage.
-   * NOTE that in order to give the best possible image, the bounds are
-   * expanded slightly over the binary connected component, by the supplied
-   * padding, so the top-left position of the returned image is returned
-   * in (left,top). These will most likely not match the coordinates
-   * returned by BoundingBox.
-   * If you do not supply an original image, you will get a binary one.
-   * Use pixDestroy to delete the image after use.
-   */
-  Pix* GetImage(PageIteratorLevel level, int padding, Pix* original_img,
-                int* left, int* top) const;
+    namespace atom {
+    }
     
-      // Write all (changed_) parameters to a config file.
-  void WriteParams(char* filename, bool changes_only);
-    
-    #endif // D_DHT_REPLACE_NODE_TASK_H
+    #endif  // ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
 
     
-    const std::string DHTResponseMessage::R('r');
     
-      // header
-  readBytes(fp, buf, buf.size(), 8);
-  if (memcmp(header, buf, 8) == 0) {
-    version = 3;
-  }
-  else if (memcmp(headerCompat, buf, 8) == 0) {
-    version = 2;
-  }
-  else {
-    throw DL_ABORT_EX(fmt('Failed to load DHT routing table from %s. cause:%s',
-                          filename.c_str(), 'bad header'));
-  }
+    bool isGaussianPyramidDownS16Supported(const Size2D &srcSize, const Size2D &dstSize, u8 cn);
+    void gaussianPyramidDown(const Size2D &srcSize,
+                             const s16 *srcBase, ptrdiff_t srcStride,
+                             const Size2D &dstSize,
+                             s16 *dstBase, ptrdiff_t dstStride, u8 cn);
     
-    #include <vector>
-#include <memory>
     
-    class DHTNode;
-class DHTRoutingTable;
-class DHTMessageDispatcher;
-class DHTMessageFactory;
-class DHTTaskQueue;
-class DHTAbstractTask;
+    {} //namespace CAROTENE_NS
+
     
-    void DHTTaskQueueImpl::addPeriodicTask2(const std::shared_ptr<DHTTask>& task)
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
+CVT_FUNC(s8, s16, 16,
+,
 {
-  periodicTaskQueue2_.addTask(task);
+     for (size_t i = 0; i < w; i += 16)
+     {
+         internal::prefetch(_src + i);
+         __asm__ (
+             'vld1.8 {d0-d1}, [%[src]]                              \n\t'
+             'vmovl.s8 q1, d0                                       \n\t'
+             'vmovl.s8 q2, d1                                       \n\t'
+             'vst1.16 {d2-d3}, [%[dst1]]                            \n\t'
+             'vst1.16 {d4-d5}, [%[dst2]]                            \n\t'
+             : /*no output*/
+             : [src] 'r' (_src + i),
+               [dst1] 'r' (_dst + i + 0),
+               [dst2] 'r' (_dst + i + 8)
+             : 'd0','d1','d2','d3','d4','d5'
+         );
+     }
+})
+#else
+CVT_FUNC(s8, s16, 16,
+,
+{
+     for (size_t i = 0; i < w; i += 16)
+     {
+         internal::prefetch(_src + i);
+         int8x16_t vline_s8 = vld1q_s8(_src + i);
+    }
+    }
+    
+    #define DOT_FLOAT_BLOCKSIZE (1 << 13)
+    f64 result = 0.0;
+    for (size_t row = 0; row < size.height; ++row)
+    {
+        const f32 * src0 = internal::getRowPtr(src0Base, src0Stride, row);
+        const f32 * src1 = internal::getRowPtr(src1Base, src1Stride, row);
+    }
+    
+    
+    {            if (mask[0])
+                process(src, j, j + 8, i,
+                        minVal, minLocPtr, minLocCount, minLocCapacity,
+                        maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
+        }
+    
+        for(size_t j = 0; j < size.height; ++j)
+    {
+        const T *  src = internal::getRowPtr( srcBase,  srcStride, j);
+        const T * rng1 = internal::getRowPtr(rng1Base, rng1Stride, j);
+        const T * rng2 = internal::getRowPtr(rng2Base, rng2Stride, j);
+             u8 *  dst = internal::getRowPtr( dstBase,  dstStride, j);
+        size_t i = 0;
+        for( ; i < width; i += 32/sizeof(T) )
+        {
+            internal::prefetch(src + i);
+            internal::prefetch(rng1 + i);
+            internal::prefetch(rng2 + i);
+    }
+    }
+    
+    #include 'constraint_bullet.h'
+    
+    	resource_loader_pkm = memnew(ResourceFormatPKM);
+	ResourceLoader::add_resource_format_loader(resource_loader_pkm);
+    
+    	jclass activityThread = env->FindClass('android/app/ActivityThread');
+	jmethodID currentActivityThread = env->GetStaticMethodID(activityThread, 'currentActivityThread', '()Landroid/app/ActivityThread;');
+	jobject at = env->CallStaticObjectMethod(activityThread, currentActivityThread);
+	jmethodID getApplication = env->GetMethodID(activityThread, 'getApplication', '()Landroid/app/Application;');
+	jobject context = env->CallObjectMethod(at, getApplication);
+    
+    	if (instance == NULL) {
+		instance = memnew(ZipArchive);
+	};
+    
+    
+    {	return false;
 }
     
-    #include <cstring>
+    /*! \brief gradient statistics pair usually needed in gradient boosting */
+using GradientPair = detail::GradientPairInternal<float>;
     
-    DNSCache::CacheEntry::~CacheEntry() = default;
+      /*!
+   * \brief determines whether updater has enough knowledge about a given dataset
+   *        to quickly update prediction cache its training data and performs the
+   *        update if possible.
+   * \param data: data matrix
+   * \param out_preds: prediction cache to be updated
+   * \return boolean indicating whether updater has capability to update
+   *         the prediction cache. If true, the prediction cache will have been
+   *         updated by the time this function returns.
+   */
+  virtual bool UpdatePredictionCache(const DMatrix* data,
+                                     HostDeviceVector<bst_float>* out_preds) {
+    return false;
+  }
     
-    public:
-  DNSCache();
-  DNSCache(const DNSCache& c);
-  ~DNSCache();
+    
+    {template<typename IndexType, typename DType = real_t>
+Parser<IndexType> *
+CreateDenseLibSVMParser(const std::string& path,
+                        const std::map<std::string, std::string>& args,
+                        unsigned part_index,
+                        unsigned num_parts) {
+  CHECK_NE(args.count('num_col'), 0) << 'expect num_col in dense_libsvm';
+  return new DensifyParser<IndexType>(
+            Parser<IndexType>::Create(path.c_str(), part_index, num_parts, 'libsvm'),
+           uint32_t(atoi(args.at('num_col').c_str())));
+}
+}  // namespace data
     
     
     {
-    {            return std::string_view(cache.data() + offset - cacheOffset, chunkSize);
-        } else {
-            /* Cache miss */
-            //std::cout << 'Cache miss!' << std::endl;
-            return std::string_view(nullptr, 0);
-        }
+    {
+    {  inline void PutChar(char ch) {
+    out_buf += ch;
+    if (out_buf.length() >= kBufferSize) Flush();
+  }
+  inline void Flush(void) {
+    if (out_buf.length() != 0) {
+      fp->Write(&out_buf[0], out_buf.length());
+      out_buf.clear();
     }
-    
-        /* For debugging you may want to print this */
-    void printTree() {
-        printNode(&tree, -1);
-    }
-    
-    
-    {    /* Compression data */
-    InflationStream *inflationStream = nullptr;
+  }
 };
+}  // namespace common
+}  // namespace xgboost
+#endif  // XGBOOST_COMMON_BASE64_H_
+
     
-    
-    {        return this;
+    namespace xgboost {
+namespace gbm {
+/*! \brief model parameters */
+struct GBTreeModelParam : public dmlc::Parameter<GBTreeModelParam> {
+  /*! \brief number of trees */
+  int num_trees;
+  /*! \brief number of roots */
+  int num_roots;
+  /*! \brief number of features to be used by trees */
+  int num_feature;
+  /*! \brief pad this space, for backward compatibility reason.*/
+  int pad_32bit;
+  /*! \brief deprecated padding space. */
+  int64_t num_pbuffer_deprecated;
+  /*!
+   * \brief how many output group a single instance can produce
+   *  this affects the behavior of number of output we have:
+   *    suppose we have n instance and k group, output will be k * n
+   */
+  int num_output_group;
+  /*! \brief size of leaf vector needed in tree */
+  int size_leaf_vector;
+  /*! \brief reserved parameters */
+  int reserved[32];
+  /*! \brief constructor */
+  GBTreeModelParam() {
+    std::memset(this, 0, sizeof(GBTreeModelParam));
+    static_assert(sizeof(GBTreeModelParam) == (4 + 2 + 2 + 32) * sizeof(int),
+                  '64/32 bit compatibility issue');
+  }
+  // declare parameters, only declare those that need to be set.
+  DMLC_DECLARE_PARAMETER(GBTreeModelParam) {
+    DMLC_DECLARE_FIELD(num_output_group)
+        .set_lower_bound(1)
+        .set_default(1)
+        .describe(
+            'Number of output groups to be predicted,'
+            ' used for multi-class classification.');
+    DMLC_DECLARE_FIELD(num_roots).set_lower_bound(1).set_default(1).describe(
+        'Tree updater sequence.');
+    DMLC_DECLARE_FIELD(num_feature)
+        .set_lower_bound(0)
+        .describe('Number of features used for training and prediction.');
+    DMLC_DECLARE_FIELD(size_leaf_vector)
+        .set_lower_bound(0)
+        .set_default(0)
+        .describe('Reserved option for vector tree.');
+  }
+};
     }
+    }
+    
+    SEXP XGDMatrixSaveBinary_R(SEXP handle, SEXP fname, SEXP silent) {
+  R_API_BEGIN();
+  CHECK_CALL(XGDMatrixSaveBinary(R_ExternalPtrAddr(handle),
+                                 CHAR(asChar(fname)),
+                                 asInteger(silent)));
+  R_API_END();
+  return R_NilValue;
+}
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    void ImGui_ImplDX10_NewFrame()
+{
+    if (!g_pFontSampler)
+        ImGui_ImplDX10_CreateDeviceObjects();
+}
+
+    
+    namespace {
+const int MAX_RETRY = 2;
+} // namespace
+    
+    #include 'DHTNode.h'
+#include 'DlAbortEx.h'
+#include 'DHTConstants.h'
+#include 'bittorrent_helper.h'
+#include 'Logger.h'
+#include 'a2netcompat.h'
+#include 'util.h'
+#include 'TimeA2.h'
+#include 'fmt.h'
+#include 'File.h'
+#include 'LogFactory.h'
+#include 'BufferedFile.h'
+    
+    
+    {} // namespace aria2
+    
+    class DHTTask {
+public:
+  virtual ~DHTTask() = default;
+    }
+    
+    bool DHTTokenTracker::validateToken(const std::string& token,
+                                    const unsigned char* infoHash,
+                                    const std::string& ipaddr,
+                                    uint16_t port) const
+{
+  for (auto& elem : secret_) {
+    if (generateToken(infoHash, ipaddr, port, elem) == token) {
+      return true;
+    }
+  }
+  return false;
+}
