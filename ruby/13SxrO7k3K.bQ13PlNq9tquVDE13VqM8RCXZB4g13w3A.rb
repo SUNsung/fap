0961b8b6f623dcd970263b100120d643226b4412
SUNsung <weatherius@gmail.com>
2018-12-09 03:12:47 +0200

    Rewrite [13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb] File in: 2018-12-09 03:12:47

--- ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
+++ ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
@@ -1,148 +1,160 @@
 
         
-            #####################################################
-    # @!group Other things
-    #####################################################
+          it 'correctly sets notification level' do
+    moderator = Fabricate(:moderator)
     
-          @launch_event_sent = true
-      builder = AnalyticsEventBuilder.new(
-        p_hash: launch_context.p_hash,
-        session_id: session_id,
-        action_name: nil,
-        fastlane_client_language: launch_context.fastlane_client_language
-      )
+      UserOption.where(user_id: smoke_user.id).update_all(
+    email_direct: false,
+    email_digests: false,
+    email_private_messages: false,
+  )
     
-        def handle_unknown_error!(e)
-      # Some spaceship exception classes implement #preferred_error_info in order to share error info
-      # that we'd rather display instead of crashing with a stack trace. However, fastlane_core and
-      # spaceship can not know about each other's classes! To make this information passing work, we
-      # use a bit of Ruby duck-typing to check whether the unknown exception type implements the right
-      # method. If so, we'll present any returned error info in the manner of a user_error!
-      error_info = e.respond_to?(:preferred_error_info) ? e.preferred_error_info : nil
-      should_show_github_issues = e.respond_to?(:show_github_issues) ? e.show_github_issues : true
+        def initialize(analytics_ingester_client: AnalyticsIngesterClient.new(GA_TRACKING))
+      require 'securerandom'
+      @session_id = SecureRandom.uuid
+      @client = analytics_ingester_client
+      @threads = []
+      @launch_event_sent = false
+    end
     
-          it 'sets the platform to Mac' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              platform: 'Mac'
-            )
-          end').runner.execute(:test)
+    require 'commander'
     
-            expect(result[1]).to start_with('security set-keychain-settings')
-        expect(result[1]).to include('-t 300')
-        expect(result[1]).to_not(include('-l'))
-        expect(result[1]).to_not(include('-u'))
-        expect(result[1]).to include('~/Library/Keychains/test.keychain')
-        expect(result[2]).to start_with('security list-keychains -s')
-        expect(result[2]).to end_with(File.expand_path('~/Library/Keychains/test.keychain').to_s)
+            expect(result[2]).to start_with('security set-keychain-settings')
+        expect(result[2]).to include('-t 300')
+        expect(result[2]).to_not(include('-l'))
+        expect(result[2]).to_not(include('-u'))
+        expect(result[2]).to include('~/Library/Keychains/test.keychain')
       end
     
-          it 'cannot have both path and pathspec parameters' do
-        expect do
-          Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: 'myfile.txt', pathspec: '*.txt')
-          end').runner.execute(:test)
-        end.to raise_error(FastlaneCore::Interface::FastlaneError)
+          it 'generates the correct git command with a shell-escaped message' do
+        message = 'message with 'quotes' (and parens)'
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: './fastlane/README.md', message: \'#{message}\')
+        end').runner.execute(:test)
+        expect(result).to eq('git commit -m #{message.shellescape} ./fastlane/README.md')
       end
     end
   end
 end
 
     
-            # this command is also sent on macOS Sierra and we need to allow it or else the test will fail
-        allowed_command = 'security set-key-partition-list -S apple-tool:,apple: -k #{''.shellescape} #{keychain_path.shellescape} &> /dev/null'
+            keychain_path = File.expand_path(File.join('~', 'Library', 'Keychains', keychain))
+        expected_security_import_command = 'security import #{cert_name.shellescape} -k '#{keychain_path.shellescape}' -P #{password.shellescape} -T /usr/bin/codesign -T /usr/bin/security &> /dev/null'
     
-          it 'passes an environment Hash' do
-        expect_command({ 'PATH' => '/usr/local/bin' }, 'git', 'commit')
-        Fastlane::Actions.sh({ 'PATH' => '/usr/local/bin' }, 'git', 'commit')
-      end
-    
-            MergeRequest
-          .where(id: start_id..stop_id)
-          .where(latest_merge_request_diff_id: nil)
-          .each_batch(of: BATCH_SIZE) do |relation|
-    
-                ##
-            # We assume that unspecified entry is undefined.
-            # See issue #18775.
-            #
-            if @value.nil?
-              Entry::Unspecified.new(
-                fabricate_unspecified
-              )
-            else
-              fabricate(@entry, @value)
-            end
-          end
-    
-              def location
-            name = @key.presence || self.class.name.to_s.demodulize
-                                        .underscore.humanize.downcase
-    
-    desc 'Test all Gemfiles from test/*.gemfile'
-task :test_all_gemfiles do
-  require 'term/ansicolor'
-  require 'pty'
-  require 'shellwords'
-  cmd      = 'bundle install --quiet && bundle exec rake --trace'
-  statuses = Dir.glob('./test/gemfiles/*{[!.lock]}').map do |gemfile|
-    env = {'BUNDLE_GEMFILE' => gemfile}
-    cmd_with_env = '  (#{env.map { |k, v| 'export #{k}=#{Shellwords.escape v}' } * ' '}; #{cmd})'
-    $stderr.puts Term::ANSIColor.cyan('Testing\n#{cmd_with_env}')
-    PTY.spawn(env, cmd) do |r, _w, pid|
-      begin
-        r.each_line { |l| puts l }
-      rescue Errno::EIO
-        # Errno:EIO error means that the process has finished giving output.
-      ensure
-        ::Process.wait pid
+          it 'returns the return value of the block if present' do
+        expect_command('ls', '-la')
+        return_value = Fastlane::Actions.sh('ls', '-la') do |status, result|
+          42
+        end
+        expect(return_value).to eq(42)
       end
     end
-    [$? && $?.exitstatus == 0, cmd_with_env]
   end
-  failed_cmds = statuses.reject(&:first).map { |(_status, cmd_with_env)| cmd_with_env }
-  if failed_cmds.empty?
-    $stderr.puts Term::ANSIColor.green('Tests pass with all gemfiles')
-  else
-    $stderr.puts Term::ANSIColor.red('Failing (#{failed_cmds.size} / #{statuses.size})\n#{failed_cmds * '\n'}')
-    exit 1
+    
+    # test shellescape Windows implementation directly
+describe 'WindowsShellwords#shellescape' do
+  os = 'windows'
+  shellescape_testcases.each do |testcase|
+    it testcase['it'] + ': ' + testcase['it_result'][os] do
+      str = testcase['str']
+      escaped = WindowsShellwords.shellescape(str)
+    
+      caveats <<~EOS
+    Installation or Uninstallation may fail with Exit Code 19 (Conflicting Processes running) if Browsers, Safari Notification Service or SIMBL Services (e.g. Flashlight) are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Logs in /Library/Logs/Adobe/Installers if Installation or Uninstallation fails, to identifify the conflicting processes.
+  EOS
+end
+
+    
+        def pattern_path(path)
+      return ::File.join(LOGSTASH_HOME, 'patterns', path)
+    end
+    
+    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
+Gem.use_paths(LogStash::Environment.logstash_gem_home)
+    
+          validate_plugins!
+    end
+    
+            return nil
+      end
+    end
+  end
+end end end
+
+    
+        private
+    def uncompress(source)
+      temporary_directory = Stud::Temporary.pathname
+      LogStash::Util::Zip.extract(source, temporary_directory, LOGSTASH_PATTERN_RE)
+      temporary_directory
+    rescue Zip::Error => e
+      # OK Zip's handling of file is bit weird, if the file exist but is not a valid zip, it will raise
+      # a `Zip::Error` exception with a file not found message...
+      raise InvalidPackError, 'Cannot uncompress the zip: #{source}'
+    end
+    
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-dns')
+          end
+        end
+    
+        context 'update a specific plugin' do
+      it 'has executed successfully' do
+        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
+        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
+        expect(logstash).not_to have_installed?(plugin_name, previous_version)
+      end
+    end
+    
+          # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
+      #
+      # In this case, the `def` node destructures into:
+      #
+      #   `method_name, arguments, body`
+      #
+      # while the `defs` node destructures into:
+      #
+      #   `receiver, method_name, arguments, body`
+      #
+      # so we reverse the destructured array to get the optional receiver
+      # at the end, where it can be discarded.
+      #
+      # @return [Array] the different parts of the `def` or `defs` node
+      def node_parts
+        to_a.reverse
+      end
+    end
   end
 end
+
     
-    @@ login
-<form action='/'>
-  <label for='user'>User Name:</label>
-  <input name='user' value='' />
-  <input type='submit' value='GO!' />
-</form>
+          # Checks whether this node body is a void context.
+      # Always `true` for `for`.
+      #
+      # @return [true] whether the `for` node body is a void context
+      def void_context?
+        true
+      end
     
-        # we assume that the first file that requires 'sinatra' is the
-    # app_file. all other path related options are calculated based
-    # on this path by default.
-    set :app_file, caller_files.first || $0
+            pairs.map(&:key).each do |key|
+          yield key
+        end
     
-        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      subject.options[:allow_empty_referrer] = false
-      expect(subject.referrer(env)).to be_nil
+            klass.client_push(item)
+      end
+    
+        def halt(res)
+      throw :halt, res
     end
     
-            def address_params
-          params.require(:address).permit(permitted_address_attributes)
-        end
+      context 'called with one style' do
+    it 'applies same style to all sides' do
+      rule = 'border-style: solid'
     
-            def empty
-          authorize! :update, @order, order_token
-          @order.empty!
-          render plain: nil, status: 204
-        end
-    
-            def create
-          @order.validate_payments_attributes([payment_params])
-          @payment = @order.payments.build(payment_params)
-          if @payment.save
-            respond_with(@payment, status: 201, default_template: :show)
-          else
-            invalid_resource!(@payment)
-          end
-        end
+    describe 'position' do
+  before(:all) do
+    ParserSupport.parse_file('library/position')
+  end
