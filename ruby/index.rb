1f579124f01016e9e9b6d98ec90a3c14e8980c7e
SUNsung <weatherius@gmail.com>
2018-12-10 09:41:27 +0200

    Rewrite [index.rb] File in: 2018-12-10 09:41:26

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,113 +1,46 @@
 
         
-              https://pip.readthedocs.org/en/stable/installing/#install-pip
-    EOS
-  when 'pil' then <<-EOS.undent
-    Instead of PIL, consider `pip install pillow` or `brew install Homebrew/python/pillow`.
-    EOS
-  when 'macruby' then <<-EOS.undent
-    MacRuby works better when you install their package:
-      http://www.macruby.org/
-    EOS
-  when /(lib)?lzma/
-    'lzma is now part of the xz formula.'
-  when 'xcode'
-    if MacOS.version >= :lion
-      <<-EOS.undent
-      Xcode can be installed from the App Store.
-      EOS
-    else
-      <<-EOS.undent
-      Xcode can be installed from https://developer.apple.com/xcode/downloads/
-      EOS
+            def str_to_byte_pos(pos)
+      @s.string.slice(0, pos).bytesize
     end
-  when 'gtest', 'googletest', 'google-test' then <<-EOS.undent
-    Installing gtest system-wide is not recommended; it should be vendored
-    in your projects that use it.
-    EOS
-  when 'gmock', 'googlemock', 'google-mock' then <<-EOS.undent
-    Installing gmock system-wide is not recommended; it should be vendored
-    in your projects that use it.
-    EOS
-  when 'sshpass' then <<-EOS.undent
-    We won't add sshpass because it makes it too easy for novice SSH users to
-    ruin SSH's security.
-    EOS
-  when 'gsutil' then <<-EOS.undent
-    Install gsutil with `pip install gsutil`
-    EOS
-  when 'clojure' then <<-EOS.undent
-    Clojure isn't really a program but a library managed as part of a
-    project and Leiningen is the user interface to that library.
-    
-        # The stdlib recorded in the install receipt is used during dependency
-    # compatibility checks, so we only care about the stdlib that libraries
-    # link against.
-    keg.detect_cxx_stdlibs(:skip_executables => true)
-  end
-    
-      def std?
-    @settings.include? :std
-  end
-    
-        if RUBY_PATH.to_s !~ %r{^/System/Library/Frameworks/Ruby.framework/Versions/[12]\.[089]/usr/bin/ruby}
-      s << ' => #{RUBY_PATH}'
-    end
-    s
-  end
-    
-      def snow_leopard_64?
-    MacOS.prefer_64_bit?
   end
 end
     
-      at_exit do
-    if ENV['KEEP_RUNNING']
-      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
-      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
-    else
-      vagrant_cli_command('destroy -f')
+        # @import 'file.less' to '#{target_path}file;'
+    def replace_file_imports(less, target_path = '')
+      less.gsub %r([@\$]import ['|']([\w\-/]+).less['|'];),
+                %Q(@import '#{target_path}\\1';)
+    end
+    
+        def silence_log
+      @silence = true
+      yield
+    ensure
+      @silence = false
     end
   end
+end
+
     
-      namespace :release do
-    GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
-      desc 'Release #{gem} as a package'
-      task gem => 'package:#{gem}' do
-        sh <<-SH
-          gem install #{package(gem, '.gem')} --local &&
-          gem push #{package(gem, '.gem')}
-        SH
+      # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
+    
+          # Returns the else branch of the `case` statement, if any.
+      #
+      # @return [Node] the else branch node of the `case` statement
+      # @return [nil] if the case statement does not have an else branch.
+      def else_branch
+        node_parts[-1]
       end
-    end
     
-            reaction
-      end
-    
-      test 'clean path with leading slash' do
-    assert_equal '/Mordor', clean_path('/Mordor')
+    Given /^I add this snippet to the User model:$/ do |snippet|
+  file_name = 'app/models/user.rb'
+  cd('.') do
+    content = File.read(file_name)
+    File.open(file_name, 'w') { |f| f << content.sub(/end\Z/, '#{snippet}\nend') }
   end
+end
     
-        # Remove all slashes from the start of string.
-    # Remove all double slashes
-    def clean_url url
-      return url if url.nil?
-      url.gsub('%2F', '/').gsub(/^\/+/, '').gsub('//', '/')
-    end
-    
-          # Checks whether this `hash` element is on the same line as `other`.
-      #
-      # @note A multiline element is considered to be on the same line if it
-      #       shares any of its lines with `other`
-      #
-      # @return [Boolean] whether this element is on the same line as `other`
-      def same_line?(other)
-        loc.last_line == other.loc.line || loc.line == other.loc.last_line
-      end
-    
-          # Checks whether the `pair` uses a hash rocket delimiter.
-      #
-      # @return [Boolean] whether this `pair` uses a hash rocket delimiter
-      def hash_rocket?
-        loc.operator.is?(HASH_ROCKET)
-      end
+        alias :empty? :empty_file?
