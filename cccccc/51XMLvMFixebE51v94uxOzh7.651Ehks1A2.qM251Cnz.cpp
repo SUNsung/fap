159bc23f279c9923ddae3945aad85d78f0562e2f
SUNsung <weatherius@gmail.com>
2018-12-11 01:11:52 +0200

    Rewrite [51XMLvMFixebE51v94uxOzh7.651Ehks1A2.qM251Cnz.cpp] File in: 2018-12-11 01:11:52

--- cccccc/51XMLvMFixebE51v94uxOzh7.651Ehks1A2.qM251Cnz.cpp
+++ cccccc/51XMLvMFixebE51v94uxOzh7.651Ehks1A2.qM251Cnz.cpp
@@ -1,108 +1,230 @@
 
         
         
-    {        auto maskMatrix = GetMatrix();
-        size_t rowOffset = offset[0];
-        size_t colOffset = offset[1];
-        size_t sliceRowLength = (shape[0] != NDShape::InferredDimension) ? shape[0] : (maskMatrix->GetNumRows() - rowOffset);
-        size_t sliceColLength = (shape[1] != NDShape::InferredDimension) ? shape[1] : (maskMatrix->GetNumCols() - colOffset);
-        if ((rowOffset == 0) && (sliceRowLength == maskMatrix->GetNumRows()))
-            maskMatrix->ColumnSlice(colOffset, sliceColLength).SetValue((char)maskKind);
-        else
-        {
-            // Since Matrix does not support strides in the row dimension, we will need to create separate slices for each column
-            for (size_t i = colOffset; i < (colOffset + sliceColLength); ++i)
-            {
-                auto column = maskMatrix->ColumnSlice(i, 1);
-                column.Reshape(1, maskMatrix->GetNumRows());
-                column.ColumnSlice(rowOffset, sliceRowLength).SetValue((char)maskKind);
-            }
-        }
+    {        return true;
+      }
+    
+    namespace atom {
     }
     
     
-    {        return dict[versionKey].Value<size_t>();
+    {}  // namespace
+    
+    namespace mate {
+template <>
+struct Converter<ui::IdleState> {
+  static v8::Local<v8::Value> ToV8(v8::Isolate* isolate,
+                                   const ui::IdleState& in) {
+    switch (in) {
+      case ui::IDLE_STATE_ACTIVE:
+        return mate::StringToV8(isolate, 'active');
+      case ui::IDLE_STATE_IDLE:
+        return mate::StringToV8(isolate, 'idle');
+      case ui::IDLE_STATE_LOCKED:
+        return mate::StringToV8(isolate, 'locked');
+      case ui::IDLE_STATE_UNKNOWN:
+      default:
+        return mate::StringToV8(isolate, 'unknown');
+    }
+  }
+};
+}  // namespace mate
+    
+    // static
+v8::Local<v8::Value> Screen::Create(v8::Isolate* isolate) {
+  if (!Browser::Get()->is_ready()) {
+    isolate->ThrowException(v8::Exception::Error(mate::StringToV8(
+        isolate, 'Cannot require \'screen\' module before app is ready')));
+    return v8::Null(isolate);
+  }
     }
     
-                auto sequenceValueVarSubshape = currentSequenceDataShape.SubShape(0, fullyDefinedSampleShape.Rank());
-            if (sequenceValueVarSubshape != fullyDefinedSampleShape)
-            {
-                for (size_t k = 0; k < fullyDefinedSampleShape.Rank(); ++k)
-                {
-                    if (fullyDefinedSampleShape[k] == NDShape::FreeDimension)
-                        fullyDefinedSampleShape[k] = sequenceValueVarSubshape[k];
-                    else if (fullyDefinedSampleShape[k] != sequenceValueVarSubshape[k])
-                        InvalidArgument('Value::Create: The shape '%S' of sequence #%zu is not compatible with the sample shape '%S'.', currentSequenceData->Shape().AsString().c_str(), i, sampleShape.AsString().c_str());
-                }
-            }
+    #include 'atom/common/api/api_messages.h'
+#include 'atom/common/native_mate_converters/string16_converter.h'
+#include 'atom/common/native_mate_converters/value_converter.h'
+#include 'content/public/browser/render_frame_host.h'
+#include 'content/public/browser/web_contents.h'
+#include 'native_mate/object_template_builder.h'
     
-    
-    {        m_dataFields->SetValueInitialization(initializer, device);
-    }
-    
-            if (::WaitForSingleObject(m_handle, wait ? INFINITE : 0) != WAIT_OBJECT_0)
-        {
-            // failed to acquire
-            int rc = ::CloseHandle(m_handle);
-            if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
-            {
-                RuntimeError('Acquire: Handler close failure with error code %d', ::GetLastError());
-            }
-            m_handle = NULL;
-            return false;
-        }
-    
-    // GetHmmData - Get the HMM definition for SE training
-// hmm - HMM definition
-// returns - true if succeed
-bool DataReader::GetHmmData(msra::asr::simplesenonehmm* hmm)
-{
-    bool bRet = true;
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-        bRet &= m_dataReaders[m_ioNames[i]]->GetHmmData(hmm);
-    return bRet;
+    namespace content {
+class WebContents;
 }
     
-    /** Returns the Cardinal Spline position for a given set of control points, tension and time */
-extern CC_DLL Vec2 ccCardinalSplineAt(const Vec2 &p0, const Vec2 &p1, const Vec2 &p2, const Vec2 &p3, float tension, float t);
+    void AtomQuotaPermissionContext::RequestQuotaPermission(
+    const content::StorageQuotaParams& params,
+    int render_process_id,
+    const PermissionCallback& callback) {
+  callback.Run(response::QUOTA_PERMISSION_RESPONSE_ALLOW);
+}
     
-    #include '2d/CCActionInstant.h'
-#include '2d/CCNode.h'
-#include '2d/CCSprite.h'
-    
-        //
-    // Overrides
-    //
-    virtual ResizeTo* clone() const override;
-    void startWithTarget(cocos2d::Node* target) override;
-    void update(float time) override;
-    
-    /**
- * @addtogroup actions
- * @{
- */
+        size_t i = 0;
+    f32* dsta = internal::getRowPtr(dstBase, dstStride, 0);
+    for (; i < size.height-1; i+=2)
+    {
+        //vertical convolution
+        ptrdiff_t idx_rm1 = internal::borderInterpolate(i - 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rp2 = internal::borderInterpolate(i + 2, size.height, borderType, borderMargin.top, borderMargin.bottom);
     }
     
-    protected:
-    int _randrange;
-    bool _once;
-    bool _shatterZ;
+                // calculate values for plain CPU part below if needed
+            if (x + 8 >= bwidth)
+            {
+                ptrdiff_t x3 = x == width ? width - 1 : x;
+                ptrdiff_t x4 = border == BORDER_MODE_CONSTANT ? x3 - 1 : std::max<ptrdiff_t>(x3 - 1, 0);
+    }
     
-    // implementation of Animation
     
-        /** updates the Atlas (indexed vertex array).
-    * Shall be overridden in subclasses.
-    */
-    virtual void updateAtlasValues();
+    {    return 0;
+#endif
+}
     
-    /** Set an buffer manager of the texture vertex. */
-    void setTextureAtlas(TextureAtlas* textureAtlas);
+                if (mask[0])
+                process(src, j, j + 8, i,
+                        minVal, minLocPtr, minLocCount, minLocCapacity,
+                        maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
+            if (mask[1])
+                process(src, j + 8, j + 16, i,
+                        minVal, minLocPtr, minLocCount, minLocCapacity,
+                        maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
+        }
+        for ( ; j < roiw8; j += 8)
+        {
+            uint8x8_t v_src = vld1_u8(src + j);
     
-    /** Return the buffer manager of the texture vertex. 
-     *
-     * @return Return A TextureAtlas.
-     */
-    TextureAtlas* getTextureAtlas() const;
+    #ifndef TESSERACT_ARCH_DOTPRODUCTSSE_H_
+#define TESSERACT_ARCH_DOTPRODUCTSSE_H_
     
-    void setQuadsToDraw(ssize_t quadsToDraw);
-    ssize_t getQuadsToDraw() const;
+    #ifndef DOCQUAL_H
+#define DOCQUAL_H
+    
+    
+    {}  // namespace tesseract.
+
+    
+    
+    {
+}  // namespace tesseract
+
+    
+      auto axis = helper.GetSingleArgument<int32_t>('axis', 1);
+  const auto canonical_axis = canonical_axis_index_(axis, in[0].dims().size());
+  auto axis_w = helper.GetSingleArgument<int32_t>('axis_w', 1);
+  const int canonical_axis_w =
+      canonical_axis_index_(axis_w, in[1].dims().size());
+  const int N = pretransposed_weight
+      ? size_from_dim_(canonical_axis_w, GetDimsVector(in[1]))
+      : size_to_dim_(canonical_axis_w, GetDimsVector(in[1]));
+    
+    <summary> <b>Example</b> </summary>
+    
+    Contrast Example 2 with Example 1. For each data point per feature, the values
+are sorted by the corresponding KEY.
+)DOC')
+    .Input(0, 'DATA', 'Tensor of rank 1.')
+    .Input(
+        1,
+        'RANGES',
+        'Tensor of int32/int64 ranges, of dims (N, M, 2). '
+        'Where N is number of examples and M is a size of each example. '
+        'Last dimention represents a range in the format (start, lengths)')
+    .Input(2, 'KEY', 'Tensor of rank 1 and type int64.')
+    .Output(0, 'OUTPUT', '1-D tensor of size sum of range lengths')
+    .Arg('lengths', 'Expected lengths for ranges')
+    .TensorInferenceFunction([](const OperatorDef& def,
+                                const vector<TensorShape>& in) {
+      ArgumentHelper helper(def);
+      auto lengths = helper.GetRepeatedArgument<int>('lengths');
+      CAFFE_ENFORCE_EQ(in[0].dims_size(), 1, 'DATA should be 1-D tensor.');
+      CAFFE_ENFORCE_EQ(in[1].dims_size(), 3, 'RANGES should be 3-D tensor.');
+      if (in.size() > 2) {
+        CAFFE_ENFORCE_EQ(in[2].dims_size(), 1, 'KEY should be 1-D tensor.');
+      }
+      CAFFE_ENFORCE_GT(lengths.size(), 0, 'lengths should be non-empty.');
+      std::vector<TensorShape> out(lengths.size());
+      for (int i = 0; i < lengths.size(); ++i) {
+        out[i].set_data_type(in[0].data_type());
+        out[i].add_dims(in[1].dims(0));
+        out[i].add_dims(lengths[i]);
+      }
+      return out;
+    });
+    
+    
+    {          return out;
+        });
+OPERATOR_SCHEMA(Float16ConstantFill)
+    .NumInputs(0)
+    .NumOutputs(1)
+    .TensorInferenceFunction(Float16FillerTensorInference)
+    .Arg('value', 'The value for the elements of the output tensor.')
+    .Arg('shape', 'The shape of the output tensor.')
+    .Output(
+        0,
+        'output',
+        'Output tensor of constant values specified by 'value'');
+    
+    	// Check for machine-specific then user config source file.
+	PathCombine(cpuCfgPath, userConfigDirPath, L'ConEmu-%COMPUTERNAME%.xml');
+	ExpandEnvironmentStrings(cpuCfgPath, cpuCfgPath, sizeof(cpuCfgPath) / sizeof(cpuCfgPath[0]));
+    
+    #ifndef COMPLEXITY_H_
+#define COMPLEXITY_H_
+    
+    class HermesCanClient : public CanClient {
+ public:
+  /**
+   * @brief Initialize the BCAN client by specified CAN card parameters.
+   * @param parameter CAN card parameters to initialize the CAN client.
+   */
+  // explicit HermesCanClient(const CANCardParameter &parameter);
+    }
+    
+      std::string can_name('can' + std::to_string(port_));
+  std::strncpy(ifr.ifr_name, can_name.c_str(), IFNAMSIZ);
+  if (ioctl(dev_handler_, SIOCGIFINDEX, &ifr) < 0) {
+    AERROR << 'ioctl error';
+    return ErrorCode::CAN_CLIENT_ERROR_BASE;
+  }
+    
+    #include <string>
+#include <vector>
+    
+    TEST(ByteTest, SetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  value.set_value(0x06, 3, 3);
+  EXPECT_EQ(0x32, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 0, 8);
+  EXPECT_EQ(0x06, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 0, 10);
+  EXPECT_EQ(0x06, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 1, 7);
+  EXPECT_EQ(0x0C, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x07, 1, 1);
+  EXPECT_EQ(0x1A, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x07, -1, 1);
+  EXPECT_EQ(0x1A, value.get_byte());
+}
+    
+    
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+    
+    using apollo::common::adapter::AdapterConfig;
+using apollo::common::adapter::AdapterManager;
+using apollo::common::monitor::MonitorMessageItem;
+using apollo::common::Status;
+using apollo::common::ErrorCode;
+using apollo::common::time::Clock;
+using apollo::drivers::canbus::CanClientFactory;
+using apollo::drivers::canbus::CanClient;
+using apollo::drivers::canbus::CanReceiver;
+using apollo::drivers::canbus::SensorCanbusConf;
