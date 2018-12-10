517cb3055d37ef8aa5f29a948e6f391e1cd8178e
SUNsung <weatherius@gmail.com>
2018-12-10 06:22:02 +0200

    Rewrite [50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp] File in: 2018-12-10 06:22:01

--- cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
+++ cccccc/50P0ZcCidDhPE50XBIzNK6Jj1k507j87Sc5zQU50Mcd.cpp
@@ -1,340 +1,81 @@
 
         
         
-    {  return PlatformKind::none;
-}
-
-    
-      // Next, add each of the requirements (mapped from the requirement's
-  // interface types into the abstract type parameters).
-  for (auto &rawReq : reqSig->getRequirements()) {
-    if (auto req = rawReq.subst(reqToSyntheticEnvMap))
-      builder.addRequirement(*req, source, conformanceDC->getParentModule());
-  }
-    
-    using namespace swift::sys;
-using llvm::StringRef;
-    
-    char *DiverseListBase::addNewStorageSlow(std::size_t needed) {
-  bool wasInline = isAllocatedInline();
+    {
+    {
+    {
+    {                vst1_s16(dst + x, ls);
+            }
+            break;
+        }
+        for (s32 h = 0; h < cn; ++h)
+        {
+            s32* ln = lane + h;
+            s16* dt = dst + h;
+            for (size_t k = x; k < colsn; k += cn)
+            {
+                dt[k] = (s16)((ln[k-2*cn] + ln[k+2*cn] + 4*(ln[k-cn] + ln[k+cn]) + 6*ln[k] + (1<<7))>>8);
+            }
+        }
     }
-    
-      if (*BufferPtr == '\n')
-    return 1;
-    
-    
-    {  char* signedStr = reinterpret_cast<char*>(str);
-  memcpy(out.data(), signedStr, StringBufferSize);
 #else
-  uuid_unparse_upper(Value, out.data());
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)borderValue;
+    (void)borderMargin;
 #endif
-  // Pop off the null terminator.
-  assert(out.back() == '\0' && 'did not null-terminate?!');
-  out.pop_back();
 }
     
-    const char *Demangle::getNodeKindString(swift::Demangle::Node::Kind k) {
-  switch (k) {
-#define NODE(ID)                                                               \
-  case Node::Kind::ID:                                                         \
-    return #ID;
-#include 'swift/Demangling/DemangleNodes.def'
-  }
-  return 'Demangle::Node::Kind::???';
-}
-    
-    
-    {    for (size_t i = 0; i < batch.size; ++i) {
-      offset_[i + 1] = (i + 1) * num_col_;
-      Row<IndexType> row = batch[i];
-      for (uint32_t j = 0; j < num_col_; ++j) {
-        dense_index_[i * num_col_ + j] = j;
-      }
-      for (unsigned k = 0; k < row.length; ++k) {
-        uint32_t index = row.get_index(k);
-        CHECK_LT(index, num_col_)
-            << 'Featuere index larger than num_col';
-        dense_value_[i * num_col_ + index]  = row.get_value(k);
-      }
-    }
-    out_ = batch;
-    out_.index = dmlc::BeginPtr(dense_index_);
-    out_.value = dmlc::BeginPtr(dense_value_);
-    out_.offset = dmlc::BeginPtr(offset_);
-    return true;
-  }
-    
-      void InitTreesToUpdate() {
-    if (trees_to_update.size() == 0u) {
-      for (auto & tree : trees) {
-        trees_to_update.push_back(std::move(tree));
-      }
-      trees.clear();
-      param.num_trees = 0;
-      tree_info.clear();
-    }
-  }
-    
-    // logistic loss for probability regression task
-struct LogisticRegression {
-  // duplication is necessary, as __device__ specifier
-  // cannot be made conditional on template parameter
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return common::Sigmoid(x); }
-  XGBOOST_DEVICE static bool CheckLabel(bst_float x) { return x >= 0.0f && x <= 1.0f; }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    return predt - label;
-  }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    const float eps = 1e-16f;
-    return fmaxf(predt * (1.0f - predt), eps);
-  }
-  template <typename T>
-  static T PredTransform(T x) { return common::Sigmoid(x); }
-  template <typename T>
-  static T FirstOrderGradient(T predt, T label) { return predt - label; }
-  template <typename T>
-  static T SecondOrderGradient(T predt, T label) {
-    const T eps = T(1e-16f);
-    return std::max(predt * (T(1.0f) - predt), eps);
-  }
-  static bst_float ProbToMargin(bst_float base_score) {
-    CHECK(base_score > 0.0f && base_score < 1.0f)
-      << 'base_score must be in (0,1) for logistic loss';
-    return -logf(1.0f / base_score - 1.0f);
-  }
-  static const char* LabelErrorMsg() {
-    return 'label must be in [0,1] for logistic regression';
-  }
-  static const char* DefaultEvalMetric() { return 'rmse'; }
-};
-    
-      // Compute the Score for a node with the given stats
-  virtual bst_float ComputeScore(bst_uint parentid,
-                                const GradStats &stats,
-                                bst_float weight) const = 0;
-    
-      /**
-   * \fn  static size_t CompressedBufferWriter::CalculateBufferSize(int
-   * num_elements, int num_symbols)
-   *
-   * \brief Calculates number of bytes requiredm for a given number of elements
-   * and a symbol range.
-   *
-   * \author  Rory
-   * \date  7/9/2017
-   *
-   * \param num_elements  Number of elements.
-   * \param num_symbols   Max number of symbols (alphabet size)
-   *
-   * \return  The calculated buffer size.
-   */
-    
-      ASSERT_TRUE(data.doc().HasMember('events'));
-  ASSERT_TRUE(data.doc()['events'].HasMember('environment_variables'));
-  ASSERT_TRUE(data.doc()['events']['environment_variables'].IsArray());
-  for (const auto& var :
-       data.doc()['events']['environment_variables'].GetArray()) {
-    std::string value = var.GetString();
-    EXPECT_TRUE(value == 'foo' || value == 'bar');
-  }
-    
-    class OptionsConfigParserPluginTests : public testing::Test {};
-    
-    /// Unlike checkChildProcessStatus, this will block until process exits.
-static bool getProcessExitCode(osquery::PlatformProcess& process,
-                               int& exitCode) {
-  if (!process.isValid()) {
-    return false;
-  }
-    }
-    
-      /// Remove an autoloadable extension path.
-  void removeExtensionPath(const std::string& extension);
-    
-    void Initializer::platformSetup() {
-  // Initialize the COM libraries utilized by Windows WMI calls.
-  auto ret = ::CoInitializeEx(0, COINIT_MULTITHREADED);
-  if (ret != S_OK) {
-    ::CoUninitialize();
-  }
-}
-    
-    
-    {  Status genConfig(std::map<std::string, std::string>& config) {
-    config['data'] = '{\'queries\':{}}';
-    return Status(0, 'OK');
-  }
-};
-    
-    bool DHTRoutingTable::addNode(const std::shared_ptr<DHTNode>& node, bool good)
+    inline float32x2_t vrsqrt_f32(float32x2_t val)
 {
-  A2_LOG_DEBUG(fmt('Trying to add node:%s', node->toString().c_str()));
-  if (*localNode_ == *node) {
-    A2_LOG_DEBUG('Adding node with the same ID with localnode is not allowed.');
-    return false;
-  }
-  auto treeNode = dht::findTreeNodeFor(root_.get(), node->getID());
-  while (1) {
-    const std::shared_ptr<DHTBucket>& bucket = treeNode->getBucket();
-    if (bucket->addNode(node)) {
-      A2_LOG_DEBUG('Added DHTNode.');
-      return true;
-    }
-    else if (bucket->splitAllowed()) {
-      A2_LOG_DEBUG(fmt('Splitting bucket. Range:%s-%s',
-                       util::toHex(bucket->getMinID(), DHT_ID_LENGTH).c_str(),
-                       util::toHex(bucket->getMaxID(), DHT_ID_LENGTH).c_str()));
-      treeNode->split();
-      ++numBucket_;
-      if (treeNode->getLeft()->isInRange(node->getID())) {
-        treeNode = treeNode->getLeft();
-      }
-      else {
-        treeNode = treeNode->getRight();
-      }
-    }
-    else {
-      if (good) {
-        bucket->cacheNode(node);
-        A2_LOG_DEBUG(fmt('Cached node=%s', node->toString().c_str()));
-      }
-      return false;
-    }
-  }
-  return false;
+    float32x2_t e = vrsqrte_f32(val);
+    e = vmul_f32(vrsqrts_f32(vmul_f32(e, e), val), e);
+    e = vmul_f32(vrsqrts_f32(vmul_f32(e, e), val), e);
+    return e;
 }
     
-    #include 'LogFactory.h'
-#include 'Logger.h'
-#include 'util.h'
-#include 'DHTNode.h'
-#include 'DHTConnectionImpl.h'
-#include 'DHTRoutingTable.h'
-#include 'DHTMessageFactoryImpl.h'
-#include 'DHTMessageTracker.h'
-#include 'DHTMessageDispatcherImpl.h'
-#include 'DHTMessageReceiver.h'
-#include 'DHTTaskQueueImpl.h'
-#include 'DHTTaskFactoryImpl.h'
-#include 'DHTPeerAnnounceStorage.h'
-#include 'DHTTokenTracker.h'
-#include 'DHTInteractionCommand.h'
-#include 'DHTTokenUpdateCommand.h'
-#include 'DHTBucketRefreshCommand.h'
-#include 'DHTPeerAnnounceCommand.h'
-#include 'DHTEntryPointNameResolveCommand.h'
-#include 'DHTAutoSaveCommand.h'
-#include 'DHTTask.h'
-#include 'DHTRoutingTableDeserializer.h'
-#include 'DHTRegistry.h'
-#include 'DHTBucketRefreshTask.h'
-#include 'DHTMessageCallback.h'
-#include 'DHTMessageTrackerEntry.h'
-#include 'DHTMessageEntry.h'
-#include 'UDPTrackerClient.h'
-#include 'BtRegistry.h'
-#include 'prefs.h'
-#include 'Option.h'
-#include 'SocketCore.h'
-#include 'DlAbortEx.h'
-#include 'RecoverableException.h'
-#include 'a2functional.h'
-#include 'DownloadEngine.h'
-#include 'fmt.h'
+    namespace HPHP { namespace HHBBC {
+    }
+    }
     
-      int getNumConcurrent() const { return numConcurrent_; }
-    
-    namespace {
-const size_t NUM_CONCURRENT_TASK = 15;
-} // namespace
-    
-    
-    {} // namespace aria2
-    
-    public:
-  // _remoteNode is always null
-  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
-                    const unsigned char* data, size_t length,
-                    const std::string& ipaddr, uint16_t port);
-    
-    const std::string& DNSCache::find(const std::string& hostname,
-                                  uint16_t port) const
-{
-  auto target = std::make_shared<CacheEntry>(hostname, port);
-  auto i = entries_.find(target);
-  if (i == entries_.end()) {
-    return A2STR::NIL;
-  }
-  else {
-    return (*i)->getGoodAddr();
-  }
+    void Config::ParseIniFile(const std::string &filename, IniSettingMap &ini,
+                          const bool constants_only /* = false */,
+                          const bool is_system /* = true */ ) {
+    std::ifstream ifs(filename);
+    std::string str((std::istreambuf_iterator<char>(ifs)),
+                    std::istreambuf_iterator<char>());
+    std::string with_includes;
+    Config::ReplaceIncludesWithIni(filename, str, with_includes);
+    Config::SetParsedIni(ini, with_includes, filename, constants_only,
+                         is_system);
 }
     
-    // kDCTMatrix[8*u+x] = 0.5*alpha(u)*cos((2*x+1)*u*M_PI/16),
-// where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
-static const double kDCTMatrix[64] = {
-  0.3535533906,  0.3535533906,  0.3535533906,  0.3535533906,
-  0.3535533906,  0.3535533906,  0.3535533906,  0.3535533906,
-  0.4903926402,  0.4157348062,  0.2777851165,  0.0975451610,
- -0.0975451610, -0.2777851165, -0.4157348062, -0.4903926402,
-  0.4619397663,  0.1913417162, -0.1913417162, -0.4619397663,
- -0.4619397663, -0.1913417162,  0.1913417162,  0.4619397663,
-  0.4157348062, -0.0975451610, -0.4903926402, -0.2777851165,
-  0.2777851165,  0.4903926402,  0.0975451610, -0.4157348062,
-  0.3535533906, -0.3535533906, -0.3535533906,  0.3535533906,
-  0.3535533906, -0.3535533906, -0.3535533906,  0.3535533906,
-  0.2777851165, -0.4903926402,  0.0975451610,  0.4157348062,
- -0.4157348062, -0.0975451610,  0.4903926402, -0.2777851165,
-  0.1913417162, -0.4619397663,  0.4619397663, -0.1913417162,
- -0.1913417162,  0.4619397663, -0.4619397663,  0.1913417162,
-  0.0975451610, -0.2777851165,  0.4157348062, -0.4903926402,
-  0.4903926402, -0.4157348062,  0.2777851165, -0.0975451610,
-};
+    static int get_tempfile_if_not_exists(int ini_fd, char ini_path[]) {
+  if (ini_fd == -1) {
+#ifdef _MSC_VER
+    // MSVC doesn't require the characters to be the last
+    // 6 in the string.
+    ini_fd = open(mktemp(ini_path), O_RDWR | O_EXCL);
+#else
+    ini_fd = mkstemps(ini_path, 4); // keep the .ini suffix
+#endif
+    if (ini_fd == -1) {
+      fprintf(stderr, 'Error: unable to open temporary file');
+      exit(EXIT_FAILURE);
+    }
+  }
+  return ini_fd;
+}
     
-    #ifndef GUETZLI_DCT_DOUBLE_H_
-#define GUETZLI_DCT_DOUBLE_H_
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+    }
     
-    
-    {}  // namespace guetzli
-    
-    // Integer implementation of the Inverse Discrete Cosine Transform (IDCT).
-    
-    
-    {}  // namespace guetzli
+    #endif // incl_HPHP_OUTPUT_FILE_H_
 
     
-    namespace guetzli {
-    }
     
-    // Creates a JPEG from the rgb pixel data. Returns true on success.
-bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     JPEGData* jpg);
-    
-    // Reads the Start of Frame (SOF) marker segment and fills in *jpg with the
-// parsed data.
-bool ProcessSOF(const uint8_t* data, const size_t len,
-                JpegReadMode mode, size_t* pos, JPEGData* jpg) {
-  if (jpg->width != 0) {
-    fprintf(stderr, 'Duplicate SOF marker.\n');
-    jpg->error = JPEG_DUPLICATE_SOF;
-    return false;
-  }
-  const size_t start_pos = *pos;
-  VERIFY_LEN(8);
-  size_t marker_len = ReadUint16(data, pos);
-  int precision = ReadUint8(data, pos);
-  int height = ReadUint16(data, pos);
-  int width = ReadUint16(data, pos);
-  int num_components = ReadUint8(data, pos);
-  VERIFY_INPUT(precision, 8, 8, PRECISION);
-  VERIFY_INPUT(height, 1, 65535, HEIGHT);
-  VERIFY_INPUT(width, 1, 65535, WIDTH);
-  VERIFY_INPUT(num_components, 1, kMaxComponents, NUMCOMP);
-  VERIFY_LEN(3 * num_components);
-  jpg->height = height;
-  jpg->width = width;
-  jpg->components.resize(num_components);
-    }
-    
-    
-    {}  // namespace guetzli
+    {  logAHMSubMapWarning(mapName);
+}
