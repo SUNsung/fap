485cf534d9d986a582ae848bc57a7ef34b67713b
SUNsung <weatherius@gmail.com>
2018-12-10 16:53:05 +0200

    Rewrite [182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb] File in: 2018-12-10 16:53:04

--- ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
+++ ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
@@ -1,173 +1,55 @@
 
         
-              it 'requires the passwords to match' do
-        visit new_admin_user_path
-        fill_in 'Email', with: 'test@test.com'
-        fill_in 'Username', with: 'usertest'
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: 'no_match'
-        click_on 'Create User'
-        expect(page).to have_text('Password confirmation doesn't match')
-      end
-    end
+          find_files = ->(path) {
+    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
+      path if File.file?(path)
+    end.compact
+  }
     
-      it 'imports a scenario which requires a service' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
-    click_on 'Start Import'
-    check('I confirm that I want to import these Agents.')
-    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
-    expect(page).to have_text('Import successful!')
-  end
-end
-
+        %w[iOS macOS].each do |platform|
+        abstract_target '#{platform} Pods' do
+            project '#{platform} Modules.xcodeproj'
     
-        it 'can be turned off' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
-      DefaultScenarioImporter.import(user)
-    end
-    
-          it 'does not load workers specified in the :except option' do
-        agent_runner = AgentRunner.new(except: HuginnScheduler)
-        workers = agent_runner.send(:load_workers)
-        expect(workers.keys).to eq(['DelayedJobWorker'])
-    
-    
-    it 'work without the FAILED_JOBS_TO_KEEP env variable' do
-      old = ENV['FAILED_JOBS_TO_KEEP']
-      ENV['FAILED_JOBS_TO_KEEP'] = nil
-      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(0)
-      ENV['FAILED_JOBS_TO_KEEP'] = old
-    end
-  end
-    
-      it 'is droppable' do
-    {
-      '{{location.lat}}' => '2.0',
-      '{{location.latitude}}' => '2.0',
-      '{{location.lng}}' => '3.0',
-      '{{location.longitude}}' => '3.0',
-      '{{location.latlng}}' => '2.0,3.0',
-    }.each { |template, result|
-      expect(Liquid::Template.parse(template).render('location' => location.to_liquid)).to eq(result),
-        'expected #{template.inspect} to expand to #{result.inspect}'
-    }
-  end
-end
-
-    
-        it 'should skip disabled Agents' do
-      agents(:bob_weather_agent).update_attribute :disabled, true
-      mock(Agents::WeatherAgent).async_check(anything).times(@weather_agent_count - 1)
-      Agents::WeatherAgent.bulk_check('midnight')
-    end
-    
-    class Devise::OmniauthCallbacksController < DeviseController
-  prepend_before_action { request.env['devise.skip_timeout'] = true }
-    
-            routes.each do |module_name, actions|
-          [:path, :url].each do |path_or_url|
-            actions.each do |action|
-              action = action ? '#{action}_' : ''
-              method = :'#{action}#{module_name}_#{path_or_url}'
-    
-          if failed_attributes.any?
-        fail Devise::Models::MissingAttribute.new(failed_attributes)
-      end
-    end
-    
-      def create_private?
-    ARGV.include?('--private') || ARGV.switch?('p')
-  end
-    
-        def get_mixin_names(file, opts = {})
-      names = get_css_selectors(file).join('\n' * 2).scan(/^\.([\w-]+)\(#{LESS_MIXIN_DEF_ARGS_RE}\)(?: when.*?)?[ ]*\{/).map(&:first).uniq.sort
-      log_file_info 'mixin defs: #{names * ', '}' unless opts[:silent] || names.empty?
-      names
-    end
-    }
-    
-    # This bin wrapper runs the `pod` command in a OS X sandbox. The reason for this
-# is to ensure that people can’t use malicious code from pod specifications.
-#
-# It does this by creating a ‘seatbelt’ profile on the fly and executing the
-# given command through `/usr/bin/sandbox-exec`. This profile format is an
-# undocumented format, which uses TinyScheme to implement its DSL.
-#
-# Even though it uses a undocumented format, it’s actually very self-explanatory.
-# Because we use a whitelist approach, `(deny default)`, any action that is
-# denied is logged to `/var/log/system.log`. So tailing that should provide
-# enough information on steps that need to be take to get something to work.
-#
-# For more information see:
-#
-# * https://github.com/CocoaPods/CocoaPods/issues/939
-# * http://reverse.put.as/wp-content/uploads/2011/08/The-Apple-Sandbox-BHDC2011-Slides.pdf
-# * http://reverse.put.as/wp-content/uploads/2011/08/The-Apple-Sandbox-BHDC2011-Paper.pdf
-# * https://github.com/s7ephen/OSX-Sandbox--Seatbelt--Profiles
-# * `$ man sandbox-exec`
-# * `$ ls /usr/share/sandbox`
-    
-          def requires_authentication?
-        Spree::Api::Config[:requires_authentication]
-      end
-    
-            def find_order
-          @order = Spree::Order.find_by!(number: order_id)
-        end
-    
-              if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
-            if current_api_user.has_spree_role?('admin') && user_id.present?
-              @order.associate_user!(Spree.user_class.find(user_id))
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
             end
-    
-            private
-    
-            def stock_location
-          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:id])
-        end
-    
-            def show
-          authorize! :read, @store
-          respond_with(@store)
-        end
-    
-            def show
-          respond_with(taxonomy)
-        end
-    
-            def create
-          authorize! :create, Spree.user_class
-          @user = Spree.user_class.new(user_params)
-          if @user.save
-            respond_with(@user, status: 201, default_template: :show)
-          else
-            invalid_resource!(@user)
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
           end
         end
     
-    Given 'I remove turbolinks' do
-  cd('.') do
-    transform_file('app/assets/javascripts/application.js') do |content|
-      content.gsub('//= require turbolinks', '')
-    end
-    transform_file('app/views/layouts/application.html.erb') do |content|
-      content.gsub(', 'data-turbolinks-track' => true', '')
-    end
-  end
-end
+          def executable_path
+        <<-EOS
+### Installation Source
     
-      argument :attachment_names, :required => true, :type => :array, :desc => 'The names of the attachment(s) to add.',
-           :banner => 'attachment_one attachment_two attachment_three ...'
-    
-        def cropping dst, ratio, scale
-      if ratio.horizontal? || ratio.square?
-        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
-      else
-        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
+          def create_worker_test
+        template_file = File.join(
+            'test/workers',
+            class_path,
+            '#{file_name}_worker_test.rb'
+        )
+        template 'worker_test.rb.erb', template_file
       end
-    end
     
-        private
+    
+    # By leaving this as a class method, it can be pluggable and used by the Manager actor. Making it
+    # an instance method will make it async to the Fetcher actor
+    def self.bulk_requeue(inprogress, options)
+      return if inprogress.empty?
+    
+        def self.initialize_logger(log_target = STDOUT)
+      oldlogger = defined?(@logger) ? @logger : nil
+      @logger = Logger.new(log_target)
+      @logger.level = Logger::INFO
+      @logger.formatter = ENV['DYNO'] ? WithoutTimestamp.new : Pretty.new
+      oldlogger.close if oldlogger && !$TESTING # don't want to close testing's STDOUT logging
+      @logger
+    end
