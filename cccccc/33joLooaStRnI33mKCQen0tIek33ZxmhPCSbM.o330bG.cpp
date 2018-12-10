20fcb4e7256e0518fbf075e61b9acd14d60b3c41
SUNsung <weatherius@gmail.com>
2018-12-10 03:48:00 +0200

    Rewrite [33joLooaStRnI33mKCQen0tIek33ZxmhPCSbM.o330bG.cpp] File in: 2018-12-10 03:47:59

--- cccccc/33joLooaStRnI33mKCQen0tIek33ZxmhPCSbM.o330bG.cpp
+++ cccccc/33joLooaStRnI33mKCQen0tIek33ZxmhPCSbM.o330bG.cpp
@@ -1,280 +1,272 @@
 
         
-        namespace {
+        #include 'tensorflow/core/framework/node_def.pb.h'
+#include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/shape_inference.h'
+#include 'tensorflow/core/framework/tensor_shape.pb.h'
+#include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/lib/strings/strcat.h'
+#include 'tensorflow/python/framework/cpp_shape_inference.pb.h'
+#include 'tensorflow/python/lib/core/py_func.h'
+    
+    // hidden_ops should be a list of Op names that should get a leading _
+// in the output. Prints the output to stdout.
+// Optional fourth argument is the name of the original C++ source file
+// where the ops' REGISTER_OP() calls reside.
+void PrintPythonOps(const OpList& ops, const ApiDefMap& api_defs,
+                    const std::vector<string>& hidden_ops, bool require_shapes,
+                    const string& source_file_name = '');
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    PyArray_Descr NPyBfloat16_Descr = {
+    PyObject_HEAD_INIT(nullptr) & PyBfloat16_Type,  // typeobj
+    // We must register bfloat16 with a kind other than 'f', because numpy
+    // considers two types with the same kind and size to be equal, but
+    // float16 != bfloat16.
+    'V',  // kind
+    // TODO(phawkins): there doesn't seem to be a way of guaranteeing a type
+    // character is unique.
+    'E',                                                  // type
+    '=',                                                  // byteorder
+    NPY_NEEDS_PYAPI | NPY_USE_GETITEM | NPY_USE_SETITEM,  // hasobject
+    0,                                                    // type_num
+    sizeof(bfloat16),                                     // elsize
+    alignof(bfloat16),                                    // alignment
+    nullptr,                                              // subarray
+    nullptr,                                              // fields
+    nullptr,                                              // names
+    &NPyBfloat16_ArrFuncs,                                // f
+};
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    // Activates a CUDA context within an enclosing scope.
+class ScopedActivateExecutorContext {
+ public:
+  // Form that takes a CUDA executor implementation.
+  explicit ScopedActivateExecutorContext(CUDAExecutor* cuda_exec);
     }
     
-    void AutoUpdater::OnWindowAllClosed() {
-  QuitAndInstall();
+      static port::Status SynchronousMemcpyD2H(CudaContext* context, void* host_dst,
+                                           CUdeviceptr gpu_src, uint64 size);
+  static port::Status SynchronousMemcpyH2D(CudaContext* context,
+                                           CUdeviceptr gpu_dst,
+                                           const void* host_src, uint64 size);
+  static port::Status SynchronousMemcpyD2D(CudaContext* context,
+                                           CUdeviceptr gpu_dst,
+                                           CUdeviceptr gpu_src, uint64 size);
+    
+      OneofDef* FindOneofByName(const char* name) {
+    return FindOneofByName(name, strlen(name));
+  }
+  const OneofDef* FindOneofByName(const char* name) const {
+    return FindOneofByName(name, strlen(name));
+  }
+    
+    bool ParseAnyTypeUrl(const string& type_url, string* url_prefix,
+                     string* full_type_name) {
+  size_t pos = type_url.find_last_of('/');
+  if (pos == string::npos || pos + 1 == type_url.size()) {
+    return false;
+  }
+  if (url_prefix) {
+    *url_prefix = type_url.substr(0, pos + 1);
+  }
+  *full_type_name = type_url.substr(pos + 1);
+  return true;
 }
+    
+    io::ZeroCopyOutputStream*
+GeneratorContext::OpenForAppend(const string& filename) {
+  return NULL;
+}
+    
+    void WriteFieldDocComment(io::Printer* printer, const FieldDescriptor* field) {
+  // In theory we should have slightly different comments for setters, getters,
+  // etc., but in practice everyone already knows the difference between these
+  // so it's redundant information.
+    }
+    
+      for (int i = 0; i < dependencies.size(); i++) {
+    const string& dependency = dependencies[i].second;
+    printer->Print(
+        '      $dependency$.getDescriptor(),\n',
+        'dependency', dependency);
+  }
+    
+    
+    {  string str_with_null_char('ab\0c', 4);
+  EXPECT_EXIT(
+      TextFormatDecodeData::DecodeDataForString(str_with_null_char, 'def'),
+      ::testing::KilledBySignal(SIGABRT),
+      'error: got a null char in a string for making TextFormat data, input:');
+  EXPECT_EXIT(
+      TextFormatDecodeData::DecodeDataForString('def', str_with_null_char),
+      ::testing::KilledBySignal(SIGABRT),
+      'error: got a null char in a string for making TextFormat data, input:');
+}
+#endif  // PROTOBUF_HAS_DEATH_TEST
+    
+    CallCredentials::~CallCredentials() {}
+    
+    #include <grpc/support/port_platform.h>
+    
+    const ViewDescriptor& ServerReceivedMessagesPerRpcMinute() {
+  const static ViewDescriptor descriptor =
+      MinuteDescriptor()
+          .set_name('grpc.io/server/received_messages_per_rpc/minute')
+          .set_measure(kRpcServerReceivedMessagesPerRpcMeasureName)
+          .set_aggregation(CountDistributionAggregation())
+          .add_column(ServerMethodTagKey());
+  return descriptor;
+}
+    
+    namespace grpc {
+    }
+    
+        inline size_t GetVersion(const Dictionary& dict)
+    {
+        if (!dict.Contains(versionKey))
+             LogicError('Required key '%ls' is not found in the dictionary.', versionKey.c_str());
+    }
+    
+                auto sequenceValueVarSubshape = currentSequenceDataShape.SubShape(0, fullyDefinedSampleShape.Rank());
+            if (sequenceValueVarSubshape != fullyDefinedSampleShape)
+            {
+                for (size_t k = 0; k < fullyDefinedSampleShape.Rank(); ++k)
+                {
+                    if (fullyDefinedSampleShape[k] == NDShape::FreeDimension)
+                        fullyDefinedSampleShape[k] = sequenceValueVarSubshape[k];
+                    else if (fullyDefinedSampleShape[k] != sequenceValueVarSubshape[k])
+                        InvalidArgument('Value::Create: The shape '%S' of sequence #%zu is not compatible with the sample shape '%S'.', currentSequenceData->Shape().AsString().c_str(), i, sampleShape.AsString().c_str());
+                }
+            }
+    
+        virtual void /*ComputationNodeBase::*/ Validate(bool isFinalValidationPass) override
+    {
+        Base::Validate(isFinalValidationPass);
+        InferMBLayoutFromInputsForStandardCase(isFinalValidationPass);
+    }
+    
+        virtual void ForwardPropNonLooping() override
+    {
+        auto& dims = GetSampleLayout().GetDims();
+        vector<size_t> shape;
+        shape.assign(dims.begin(), dims.end());
+    }
+    
+    #include '2d/CCActionCamera.h'
+#include '2d/CCNode.h'
+#include 'platform/CCStdC.h'
+    
+    
+    {        return true;
+    }
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+    
+    /**
+@brief Progress from a percentage to another percentage.
+@since v0.99.1
+*/
+class CC_DLL ProgressFromTo : public ActionInterval
+{
+public:
+    /** 
+     * @brief Create and initializes the action with a duration, a 'from' percentage and a 'to' percentage.
+     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
+     * @param fromPercentage Specify the source percentage.
+     * @param toPercentage Specify the destination percentage.
+     * @return If the creation success, return a pointer of ProgressFromTo action; otherwise, return nil.
+     */
+    static ProgressFromTo* create(float duration, float fromPercentage, float toPercentage);
+    }
+    
+    protected:
+    unsigned int _cols;
+    Size _winSize;
+    
+    void Animation::addSpriteFrameWithFile(const std::string& filename)
+{
+    Texture2D *texture = Director::getInstance()->getTextureCache()->addImage(filename);
+    Rect rect = Rect::ZERO;
+    rect.size = texture->getContentSize();
+    SpriteFrame *frame = SpriteFrame::createWithTexture(texture, rect);
+    addSpriteFrame(frame);
+}
+    
+    /** Read an NSDictionary from a plist file and parse it automatically for animations */
+void AnimationCache::addAnimationsWithFile(const std::string& plist)
+{
+    CCASSERT(!plist.empty(), 'Invalid texture file name');
+    if (plist.empty()) {
+        log('%s error:file name is empty!', __FUNCTION__);
+        return;
+    }
+    
+    ValueMap dict = FileUtils::getInstance()->getValueMapFromFile(plist);
+    }
+    
+    // You might have a microbenchmark that depends on two inputs.  For
+// example, the following code defines a family of microbenchmarks for
+// measuring the speed of set insertion.
+static void BM_SetInsert(benchmark::State& state) {
+  set<int> data;
+  for (auto _ : state) {
+    state.PauseTiming();
+    data = ConstructRandomSet(state.range(0));
+    state.ResumeTiming();
+    for (int j = 0; j < state.range(1); ++j)
+      data.insert(RandomNumber());
+  }
+}
+BENCHMARK(BM_SetInsert)
+   ->Args({1<<10, 128})
+   ->Args({2<<10, 128})
+   ->Args({4<<10, 128})
+   ->Args({8<<10, 128})
+   ->Args({1<<10, 512})
+   ->Args({2<<10, 512})
+   ->Args({4<<10, 512})
+   ->Args({8<<10, 512});
     
     
     {
-    {    *auto_resize_flags = static_cast<atom::AutoResizeFlags>(flags);
-    return true;
-  }
-};
-    
-    NODE_BUILTIN_MODULE_CONTEXT_AWARE(atom_browser_download_item, Initialize);
-
-    
-     protected:
-  DownloadItem(v8::Isolate* isolate, download::DownloadItem* download_item);
-  ~DownloadItem() override;
-    
-    #include 'atom/browser/api/atom_api_global_shortcut.h'
-    
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-    
-    {  delegate.Get('isCommandIdChecked', &is_checked_);
-  delegate.Get('isCommandIdEnabled', &is_enabled_);
-  delegate.Get('isCommandIdVisible', &is_visible_);
-  delegate.Get('getAcceleratorForCommandId', &get_accelerator_);
-  delegate.Get('executeCommand', &execute_command_);
-  delegate.Get('menuWillShow', &menu_will_show_);
-}
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Screen);
-};
-    
-    namespace {
-// A suffix that will be appended to the file's outer class name if the name
-// conflicts with some other types defined in the file.
-const char* kOuterClassNameSuffix = 'OuterClass';
-    }
-    
-    static const int kValueSize = 200 * 1024;
-static const int kTotalSize = 100 * 1024 * 1024;
-static const int kCount = kTotalSize / kValueSize;
-    
-      InternalKey begin_storage, end_storage;
-    
-      // Queue of writers.
-  std::deque<Writer*> writers_ GUARDED_BY(mutex_);
-  WriteBatch* tmp_batch_ GUARDED_BY(mutex_);
-    
-    namespace leveldb {
-    }
-    
-    void InternalFilterPolicy::CreateFilter(const Slice* keys, int n,
-                                        std::string* dst) const {
-  // We rely on the fact that the code in table.cc does not mind us
-  // adjusting keys[].
-  Slice* mkey = const_cast<Slice*>(keys);
-  for (int i = 0; i < n; i++) {
-    mkey[i] = ExtractUserKey(keys[i]);
-    // TODO(sanjay): Suppress dups?
-  }
-  user_policy_->CreateFilter(keys, n, dst);
-}
-    
-    // Return the name of the sstable with the specified number
-// in the db named by 'dbname'.  The result will be prefixed with
-// 'dbname'.
-std::string TableFileName(const std::string& dbname, uint64_t number);
-    
-    bool HandleDumpCommand(Env* env, char** files, int num) {
-  StdoutPrinter printer;
-  bool ok = true;
-  for (int i = 0; i < num; i++) {
-    Status s = DumpFile(env, files[i], &printer);
-    if (!s.ok()) {
-      fprintf(stderr, '%s\n', s.ToString().c_str());
-      ok = false;
-    }
-  }
-  return ok;
-}
-    
-    TEST(LogTest, ChecksumMismatch) {
-  Write('foo');
-  IncrementByte(0, 10);
-  ASSERT_EQ('EOF', Read());
-  ASSERT_EQ(10, DroppedBytes());
-  ASSERT_EQ('OK', MatchError('checksum mismatch'));
-}
-    
-    
-    {  // Force creation of multiple memtables by reducing the write buffer size.
-  Options opt;
-  opt.reuse_logs = true;
-  opt.write_buffer_size = (kNum*100) / 2;
-  Open(&opt);
-  ASSERT_LE(2, NumTables());
-  ASSERT_EQ(1, NumLogs());
-  ASSERT_NE(old_log_file, FirstLogFile()) << 'must not reuse log';
-  for (int i = 0; i < kNum; i++) {
-    char buf[100];
-    snprintf(buf, sizeof(buf), '%050d', i);
-    ASSERT_EQ(buf, Get(buf));
-  }
-}
-    
-    template <typename ValueType>
-class ForEach {
- public:
-  template <typename InputIterator>
-  ForEach(InputIterator begin, InputIterator end)
-      : func_([begin, end](FunctionRef<void(ValueType)> f) {
-          for (auto it = begin; it != end; ++it) {
-            f(*it);
-          }
-        }) {}
-    }
-    
-    using namespace std;
-    
-    #ifndef FOLLY_RANDOM_H_
-#error This file may only be included from folly/Random.h
+    {#if defined(NDEBUG)
+  const char build_type[] = 'release';
+#else
+  const char build_type[] = 'debug';
 #endif
-    
-      /**
-   * Returns a random uint32_t given a specific RNG
-   */
-  template <class RNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static uint32_t rand32(RNG&& rng) {
-    return rng();
-  }
-    
-    /*
- * Encode a single unicode code point into a UTF-8 byte sequence.
- *
- * Return value is undefined if `cp' is an invalid code point.
- */
-std::string codePointToUtf8(char32_t cp);
-    
-    inline UriTuple as_tuple(const folly::Uri& k) {
-  return UriTuple(
-      k.scheme(),
-      k.username(),
-      k.password(),
-      k.host(),
-      k.port(),
-      k.path(),
-      k.query(),
-      k.fragment());
+  out << indent << FormatKV('library_build_type', build_type) << '\n';
+  // Close context block and open the list of benchmarks.
+  out << inner_indent << '},\n';
+  out << inner_indent << '\'benchmarks\': [\n';
+  return true;
 }
     
-      explicit VirtualExecutor(Executor* executor)
-      : VirtualExecutor(getKeepAliveToken(executor)) {}
     
-      // Test negative values
-  // When going to coarser grained types these should be rounded up towards 0.
-  ts.tv_sec = -5;
-  ts.tv_nsec = 123456;
-  EXPECT_EQ(-4999876544, to<nanoseconds>(ts).count());
-  EXPECT_EQ(-4999876544, duration_cast<nanoseconds>(-5s + 123456ns).count());
-  EXPECT_EQ(-4999876, to<microseconds>(ts).count());
-  EXPECT_EQ(-4999876, duration_cast<microseconds>(-5s + 123456ns).count());
-  EXPECT_EQ(-4999, to<milliseconds>(ts).count());
-  EXPECT_EQ(-4999, duration_cast<milliseconds>(-5s + 123456ns).count());
-  EXPECT_EQ(-4s, to<seconds>(ts));
-  EXPECT_EQ(-4, duration_cast<seconds>(-5s + 123456ns).count());
-  ts.tv_sec = -7200;
-  ts.tv_nsec = 123456;
-  EXPECT_EQ(-1h, to<hours>(ts));
-  EXPECT_EQ(
-      -1,
-      duration_cast<hours>(seconds{ts.tv_sec} + nanoseconds{ts.tv_nsec})
-          .count());
-  ts.tv_sec = -7000;
-  ts.tv_nsec = 123456;
-  EXPECT_EQ(-1h, to<hours>(ts));
-  EXPECT_EQ(
-      -1,
-      duration_cast<hours>(seconds{ts.tv_sec} + nanoseconds{ts.tv_nsec})
-          .count());
-  ts.tv_sec = -7201;
-  ts.tv_nsec = 123456;
-  EXPECT_EQ(-2h, to<hours>(ts));
-  EXPECT_EQ(
-      -2,
-      duration_cast<hours>(seconds{ts.tv_sec} + nanoseconds{ts.tv_nsec})
-          .count());
-    
-    /// Wrapper around the makeCompressionCounterHandler() extension point.
-class CompressionCounter {
- public:
-  CompressionCounter() {}
-  CompressionCounter(
-      folly::io::CodecType codecType,
-      folly::StringPiece codecName,
-      folly::Optional<int> level,
-      CompressionCounterKey key,
-      CompressionCounterType counterType) {
-    initialize_ = [=]() {
-      return makeCompressionCounterHandler(
-          codecType, codecName, level, key, counterType);
-    };
-    DCHECK(!initialize_.hasAllocatedMemory());
-  }
-    }
-    
-    template <typename T>
-typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type
-prefixToStringLE(T prefix, uint64_t n = sizeof(T)) {
-  DCHECK_GT(n, 0);
-  DCHECK_LE(n, sizeof(T));
-  prefix = Endian::little(prefix);
-  std::string result;
-  result.resize(n);
-  memcpy(&result[0], &prefix, n);
-  return result;
-}
-    
-        oldptr = takeOwnedBase(success);
-    if (!owners_eq(oldptr, CountedDetail::get_counted_base(expected))) {
-      expected = get_shared_ptr(oldptr, false);
-      release_external(newptr);
-      return false;
-    }
-    expectedptr = oldptr; // Need oldptr to release if failed
-    if (ptr_.compare_exchange_weak(expectedptr, newptr, success, failure)) {
-      if (oldptr.get()) {
-        release_external(oldptr, -1);
-      }
-      return true;
-    } else {
-      if (oldptr.get()) {
-        expected = get_shared_ptr(oldptr, false);
-      } else {
-        expected = SharedPtr(nullptr);
-      }
-      release_external(newptr);
-      return false;
-    }
-  }
-  bool compare_exchange_weak(
-      SharedPtr& expected,
-      SharedPtr&& desired,
-      std::memory_order mo = std::memory_order_seq_cst) noexcept {
-    return compare_exchange_weak(
-        expected, desired, mo, detail::default_failure_memory_order(mo));
-  }
-  bool compare_exchange_weak(
-      SharedPtr& expected,
-      SharedPtr&& desired,
-      std::memory_order success,
-      std::memory_order failure) /* noexcept */ {
-    return compare_exchange_weak(expected, desired, success, failure);
-  }
-  bool compare_exchange_strong(
-      SharedPtr& expected,
-      const SharedPtr& n,
-      std::memory_order mo = std::memory_order_seq_cst) noexcept {
-    return compare_exchange_strong(
-        expected, n, mo, detail::default_failure_memory_order(mo));
-  }
-  bool compare_exchange_strong(
-      SharedPtr& expected,
-      const SharedPtr& n,
-      std::memory_order success,
-      std::memory_order failure) /* noexcept */ {
-    auto local_expected = expected;
-    do {
-      if (compare_exchange_weak(expected, n, success, failure)) {
-        return true;
-      }
-    } while (local_expected == expected);
+    { private:
+  std::mutex mut_;
+};
