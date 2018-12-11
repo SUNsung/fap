b1d46bf595a23b862c75944e60dddc23c0ad72aa
SUNsung <weatherius@gmail.com>
2018-12-11 15:54:41 +0200

    Rewrite [02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py] File in: 2018-12-11 15:54:40

--- pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
+++ pyton/02tq1Artf6mzE02AVmXOoWvYME022crIoRD7tZs02UXl.py
@@ -1,207 +1,25 @@
 
         
-        # build the model
-model = Sequential()
-model.add(layers.Dense(256, input_shape=(784,)))
-model.add(Antirectifier())
-model.add(layers.Dropout(0.1))
-model.add(layers.Dense(256))
-model.add(Antirectifier())
-model.add(layers.Dropout(0.1))
-model.add(layers.Dense(num_classes))
-model.add(layers.Activation('softmax'))
-    
-    from ..utils.data_utils import get_file
-import numpy as np
-    
-        def __call__(self, x):
-        regularization = 0.
-        if self.l1:
-            regularization += K.sum(self.l1 * K.abs(x))
-        if self.l2:
-            regularization += K.sum(self.l2 * K.square(x))
-        return regularization
-    
-        # Arguments
-        model: Keras model instance.
-        line_length: Total length of printed lines
-            (e.g. set this to adapt the display to different
-            terminal window sizes).
-        positions: Relative or absolute positions of log elements in each line.
-            If not provided, defaults to `[.33, .55, .67, 1.]`.
-        print_fn: Print function to use.
-            It will be called on each line of the summary.
-            You can set it to a custom function
-            in order to capture the string summary.
-            It defaults to `print` (prints to stdout).
-    '''
-    if print_fn is None:
-        print_fn = print
-    
-    
-def _normalize_device_name(name):
-    name = '/' + ':'.join(name.lower().replace('/', '').split(':')[-2:])
-    return name
-    
-        # When using the delayed-build pattern (no input shape specified), you can
-    # choose to manually build your model by calling
-    # `build(batch_input_shape)`:
-    model = Sequential()
-    model.add(Dense(32))
-    model.add(Dense(32))
-    model.build((None, 500))
-    model.weights  # returns list of length 4
-    ```
-    '''
-    
-                    # Shift the ground truth by 1
-                x_shift = xstart + directionx * (t + 1)
-                y_shift = ystart + directiony * (t + 1)
-                shifted_movies[i, t, x_shift - w: x_shift + w,
-                               y_shift - w: y_shift + w, 0] += 1
-    
-    print('Train...')
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=4,
-          validation_data=[x_test, y_test])
-
-    
-    # we add a Convolution1D, which will learn filters
-# word group filters of size filter_length:
-model.add(Conv1D(filters,
-                 kernel_size,
-                 padding='valid',
-                 activation='relu',
-                 strides=1))
-# we use max pooling:
-model.add(GlobalMaxPooling1D())
-    
-        return out
-    
-                # Consume content and release the original connection
-            # to allow our new request to reuse the same one.
-            r.content
-            r.close()
-            prep = r.request.copy()
-            extract_cookies_to_jar(prep._cookies, r.request, r.raw)
-            prep.prepare_cookies(prep._cookies)
-    
-    # ---------
-# Specifics
-# ---------
-    
-        def initialize_options(self):
-        TestCommand.initialize_options(self)
+            def load(self):
         try:
-            from multiprocessing import cpu_count
-            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ['-n', '1', '--boxed']
+            session_data = self._cache.get(self.cache_key)
+        except Exception:
+            # Some backends (e.g. memcache) raise an exception on invalid
+            # cache keys. If this happens, reset the session. See #17810.
+            session_data = None
+        if session_data is not None:
+            return session_data
+        self._session_key = None
+        return {}
     
-    # The scheme of the identifier. Typical schemes are ISBN or URL.
-#epub_scheme = ''
+        def save(self, must_create=False):
+        '''
+        To save, get the session key as a securely signed string and then set
+        the modified flag so that the cookie is set on the client for the
+        current request.
+        '''
+        self._session_key = self._get_session_key()
+        self.modified = True
     
-        :param url: URL for the new :class:`Request` object.
-    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-        object to send in the body of the :class:`Request`.
-    :param json: (optional) json data to send in the body of the :class:`Request`.
-    :param \*\*kwargs: Optional arguments that ``request`` takes.
-    :return: :class:`Response <Response>` object
-    :rtype: requests.Response
-    '''
-    
-        print('Classifier Training')
-    print('===================')
-    accuracy, train_time, test_time = {}, {}, {}
-    for name in sorted(args['estimators']):
-        clf = ESTIMATORS[name]
-        try:
-            clf.set_params(random_state=0)
-        except (TypeError, ValueError):
-            pass
-    
-    from sklearn.cluster import AgglomerativeClustering
-    
-    if __name__ == '__main__':
-    list_n_samples = np.linspace(100, 10000, 5).astype(np.int)
-    list_n_features = [10, 100, 1000]
-    n_test = 1000
-    max_iter = 1000
-    noise = 0.1
-    alpha = 0.01
-    sgd_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
-    elnet_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
-    ridge_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
-    asgd_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
-    for i, n_train in enumerate(list_n_samples):
-        for j, n_features in enumerate(list_n_features):
-            X, y, coef = make_regression(
-                n_samples=n_train + n_test, n_features=n_features,
-                noise=noise, coef=True)
-    
-    File: sparsity_benchmark.py
-Function: benchmark_dense_predict at line 51
-Total time: 0.532979 s
-    
-    from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import (CountVectorizer, TfidfVectorizer,
-                                             HashingVectorizer)
-    
-    
-class Header(jose.Header):
-    '''ACME-specific JOSE Header. Implements nonce, kid, and url.
-    '''
-    nonce = jose.Field('nonce', omitempty=True, encoder=jose.encode_b64jose)
-    kid = jose.Field('kid', omitempty=True)
-    url = jose.Field('url', omitempty=True)
-    
-    
-class HeaderTest(unittest.TestCase):
-    '''Tests for acme.jws.Header.'''
-    
-            :raises .errors.PluginError: If there has been an error in parsing with
-            the specified lens.
-    
-    
-if __name__ == '__main__':
-    unittest.main()  # pragma: no cover
-
-    
-        def test_include_missing(self):
-        # This should miss
-        self.verify_fnmatch('test_*.onf', False)
-    
-        print(' - Face locations: {:.4f}s ({:.2f} fps)'.format(*run_test(setup_locate_faces.format(image), test_locate_faces)))
-    print(' - Face landmarks: {:.4f}s ({:.2f} fps)'.format(*run_test(setup_face_landmarks.format(image), test_face_landmarks)))
-    print(' - Encode face (inc. landmarks): {:.4f}s ({:.2f} fps)'.format(*run_test(setup_encode_face.format(image), test_encode_face)))
-    print(' - End-to-end: {:.4f}s ({:.2f} fps)'.format(*run_test(setup_end_to_end.format(image), test_end_to_end)))
-    print()
-
-    
-    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-    frames = []
-frame_count = 0
-    
-            result = runner.invoke(face_detection_cli.main, args=[image_file])
-        self.assertEqual(result.exit_code, 0)
-        self.assertTrue('obama_partial_face2.jpg' in result.output)
-        self.assertTrue('obama.jpg' in result.output)
-        self.assertTrue('obama2.jpg' in result.output)
-        self.assertTrue('obama3.jpg' in result.output)
-        self.assertTrue('biden.jpg' in result.output)
-    
-    requirements = [
-    'face_recognition_models>=0.3.0',
-    'Click>=6.0',
-    'dlib>=19.7',
-    'numpy',
-    'Pillow'
-]
-    
-    import face_recognition
-import picamera
-import numpy as np
+        def __str__(self):
+        return self.session_key
