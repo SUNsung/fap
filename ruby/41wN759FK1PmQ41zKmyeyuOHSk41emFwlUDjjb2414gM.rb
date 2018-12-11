7f7b0849e585fba04990d947b0e59f89bad9ae4c
SUNsung <weatherius@gmail.com>
2018-12-12 00:03:36 +0200

    Rewrite [41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb] File in: 2018-12-12 00:03:36

--- ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
+++ ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
@@ -1,319 +1,153 @@
-def local_require
-  require 'json'
-  JSON.pretty_generate(DATA)
-end
-    
-              theme.create!
-          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
-                             ' is ready for you in #{theme.path.to_s.cyan}!'
-          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
-        end
-        # rubocop:enable Metrics/AbcSize
-      end
-    end
-  end
-end
 
-    
-              # WebSockets requests will have a Connection: Upgrade header
-          if parser.http_method != 'GET' || parser.upgrade?
-            super
-          elsif parser.request_url =~ %r!^\/livereload.js!
-            headers = [
-              'HTTP/1.1 200 OK',
-              'Content-Type: application/javascript',
-              'Content-Length: #{reload_size}',
-              '',
-              '',
-            ].join('\r\n')
-            send_data(headers)
-    
-            UI.message('Adding git tag '#{tag}' 🎯.')
-        Actions.sh(cmd.join(' '))
-      end
-    
-            expect(result).to eq('git log --pretty=\'%B\' -n 10')
-      end
-    
-            it 'executes the correct git command' do
-          allow(Fastlane::Actions).to receive(:sh).with('git add #{path[0].shellescape} #{path[1].shellescape}', anything).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: #{path})
-          end').runner.execute(:test)
-        end
-      end
-    
-            it 'escapes spaces in list of files to process' do
-          file = 'path/to/my project/source code/AppDelegate.swift'
-          result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-                files: ['#{file}']
-            )
-          end').runner.execute(:test)
-    
-        # Creates a new option
-    # @param key (Symbol) the key which is used as command parameters or key in the fastlane tools
-    # @param env_name (String) the name of the environment variable, which is only used if no other values were found
-    # @param description (String) A description shown to the user
-    # @param short_option (String) A string of length 1 which is used for the command parameters (e.g. -f)
-    # @param default_value the value which is used if there was no given values and no environment values
-    # @param default_value_dynamic (Boolean) Set if the default value is generated dynamically
-    # @param verify_block an optional block which is called when a new value is set.
-    #   Check value is valid. This could be type checks or if a folder/file exists
-    #   You have to raise a specific exception if something goes wrong. Append .red after the string
-    # @param is_string *DEPRECATED: Use `type` instead* (Boolean) is that parameter a string? Defaults to true. If it's true, the type string will be verified.
-    # @param type (Class) the data type of this config item. Takes precedence over `is_string`. Use `:shell_string` to allow types `String`, `Hash` and `Array` that will be converted to shell-escaped strings
-    # @param skip_type_validation (Boolean) is false by default. If set to true, type of the parameter will not be validated.
-    # @param optional (Boolean) is false by default. If set to true, also string values will not be asked to the user
-    # @param conflicting_options ([]) array of conflicting option keys(@param key). This allows to resolve conflicts intelligently
-    # @param conflict_block an optional block which is called when options conflict happens
-    # @param deprecated (Boolean|String) Set if the option is deprecated. A deprecated option should be optional and is made optional if the parameter isn't set, and fails otherwise
-    # @param sensitive (Boolean) Set if the variable is sensitive, such as a password or API token, to prevent echoing when prompted for the parameter
-    # @param display_in_shell (Boolean) Set if the variable can be used from shell
-    # rubocop:disable Metrics/ParameterLists
-    def initialize(key: nil,
-                   env_name: nil,
-                   description: nil,
-                   short_option: nil,
-                   default_value: nil,
-                   default_value_dynamic: false,
-                   verify_block: nil,
-                   is_string: true,
-                   type: nil,
-                   skip_type_validation: false,
-                   optional: nil,
-                   conflicting_options: nil,
-                   conflict_block: nil,
-                   deprecated: nil,
-                   sensitive: nil,
-                   code_gen_sensitive: false,
-                   code_gen_default_value: nil,
-                   display_in_shell: true)
-      UI.user_error!('key must be a symbol') unless key.kind_of?(Symbol)
-      UI.user_error!('env_name must be a String') unless (env_name || '').kind_of?(String)
-    
-        private
-    
-    module Homebrew
-  module_function
-    
-    RSpec::Matchers.define :have_path do |expected|
-  match do |actual|
-    await_condition { actual.current_path == expected }
-  end
-    
-          sign_in alice, scope: :user
+        
+            def types_as_json
+      @types.values.sort! { |a, b| sort_fn(a.name, b.name) }.map(&:as_json)
     end
     
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
-    end
-  end
-end
-
-    
-          delete :destroy, params: {post_id: @message.id, id: @like.id}, format: :json
-      expect(response.status).to eq(204)
-    end
-    
-          * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-      * Redistributions in binary form must reproduce the above
-        copyright notice, this list of conditions and the following
-        disclaimer in the documentation and/or other materials provided
-        with the distribution.
-      * Neither the name of Google Inc. nor the names of its
-        contributors may be used to endorse or promote products derived
-        from this software without specific prior written permission.
-    
-          def footer
-        if @footer.nil?
-          if page = @page.footer
-            @footer = page.text_data
-          else
-            @footer = false
+            # Returns the instance variables as a hash of key-value pairs.
+        def instance_variables_hash
+          instance_variables.inject({}) do |acc, iv|
+            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
+            acc
           end
         end
-        @footer
-      end
     
-          def remove_page_extentions(page_path)
-        Gollum::Markup.formats.values.each do |format|
-          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
-        end
-        return page_path
-      end
+        case itype
+    when IAX_TYPE_DTMF_BEGIN
+      self.dprint('DTMF BEG: #{pkt[11,1]}')
+      self.dtmf << pkt[11,1]
     
-    # test/spec/mini 3
-# http://gist.github.com/25455
-# chris@ozmm.org
-# file:lib/test/spec/mini.rb
-def context(*args, &block)
-  return super unless (name = args.first) && block
-  require 'test/unit'
-  klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
-    def self.test(name, &block)
-      define_method('test_#{name.gsub(/\W/, '_')}', &block) if block
+    
+end
+end
+end
+
+    
+              private
+    
+                int
+          end
+    
+              # Decodes the key from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [EncryptionKey]
+          def decode_key(input)
+            Rex::Proto::Kerberos::Model::EncryptionKey.decode(input.value[0])
+          end
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of an encrypted message.
+        class EncryptedData < Element
+          # @!attribute name_type
+          #   @return [Integer] The encryption algorithm
+          attr_accessor :etype
+          # @!attribute kvno
+          #   @return [Integer] The version number of the key
+          attr_accessor :kvno
+          # @!attribute cipher
+          #   @return [String] The enciphered text
+          attr_accessor :cipher
+    
+                seq_values.each do |val|
+              case val.tag
+              when 0
+                self.options = decode_options(val)
+              when 1
+                self.cname = decode_cname(val)
+              when 2
+                self.realm = decode_realm(val)
+              when 3
+                self.sname = decode_sname(val)
+              when 4
+                self.from = decode_from(val)
+              when 5
+                self.till = decode_till(val)
+              when 6
+                self.rtime = decode_rtime(val)
+              when 7
+                self.nonce = decode_nonce(val)
+              when 8
+                self.etype = decode_etype(val)
+              when 10
+                self.enc_auth_data = decode_enc_auth_data(val)
+              else
+                raise ::RuntimeError, 'Failed to decode KdcRequestBody SEQUENCE'
+              end
+            end
+          end
+    
+              # Decodes a Rex::Proto::Kerberos::Model::KrbError from an String
+          #
+          # @param input [String] the input to decode from
+          def decode_string(input)
+            asn1 = OpenSSL::ASN1.decode(input)
+    
+        # Create a {Sass::Selector::CommaSequence} containing only a single
+    # {Sass::Selector::Sequence}.
+    #
+    # @param sseqs [Array<Sass::Selector::Sequence, String>]
+    # @return [Sass::Selector::CommaSequence]
+    def make_seq(*sseqs)
+      make_cseq(Sass::Selector::Sequence.new(sseqs))
     end
     
-      test 'frontend links for editing blocked' do
-    Precious::App.set(:wiki_options, { allow_editing: false })
-    get '/A'
+              def set_local_#{name}(name, value)
+            @#{name}s ||= {}
+            @#{name}s[name.tr('_', '-')] = value
+          end
     
-        # TODO: Remove to_url once write_page changes are merged.
-    @wiki.write_page('ééééé'.to_url, :markdown, '한글 text', commit_details)
-    page = @wiki.page('eeeee')
-    assert_equal '한글 text', utf8(page.raw_data)
-  end
-    
-        # make a backup of the option and sanitize it
-    base_path_original = base_path.dup
-    base_path = CGI.escape(base_path)
-    
-    # then let the user know if we changed the URL
-    unless base_path_original == base_path
-      puts <<MSG
-Warning: your base-path has been sanitized:
-  - original: '#{base_path_original}'
-  - sanitized: '#{base_path}'
-MSG
+          opts.on('--stop-on-error', 'If a file fails to compile, exit immediately.',
+                                 'Only meaningful for --watch and --update.') do
+        @options[:stop_on_error] = true
+      end
     end
-      
-    # and finally, let others enjoy our hard work:
-    wiki_options[:base_path] = base_path
-  end
-  opts.on('--page-file-dir [PATH]', 'Specify the subdirectory for all pages. Default: repository root.', 
-    'Example: setting this to 'pages' will make Gollum serve only pages at '<git-repo>/pages/*'.') do |path|
-    wiki_options[:page_file_dir] = path
-  end
-  opts.on('--css', 'Inject custom CSS into each page. The '<git-repo>/custom.css' file is used (must be committed).') do
-    wiki_options[:css] = true
-  end
-  opts.on('--js', 'Inject custom JavaScript into each page. The '<git-repo>/custom.js' file is used (must be committed).') do
-    wiki_options[:js] = true
-  end
-  opts.on('--emoji', 'Parse and interpret emoji tags (e.g. :heart:).') do
-    wiki_options[:emoji] = true
-  end
-  opts.on('--no-edit', 'Disable the feature of editing pages.')  do
-    wiki_options[:allow_editing] = false
-  end
-  opts.on('--live-preview', 'Enable the live preview feature in page editor.') do
-    wiki_options[:live_preview] = true
-  end
-  opts.on('--no-live-preview', 'Disable the live preview feature in page editor.') do
-    wiki_options[:live_preview] = false
-  end
-  opts.on('--allow-uploads [MODE]', [:dir, :page], 'Enable file uploads.',
-    'If set to 'dir', Gollum will store all uploads in the '<git-repo>/uploads/' directory.',
-    'If set to 'page', Gollum will store each upload at the currently edited page.') do |mode|
-    wiki_options[:allow_uploads]    = true
-    wiki_options[:per_page_uploads] = true if mode == :page
-  end
-  opts.on('--mathjax', 'Enable MathJax (renders mathematical equations).',
-    'By default, uses the 'TeX-AMS-MML_HTMLorMML' config with the 'autoload-all' extension.') do
-    wiki_options[:mathjax] = true
-  end
-  opts.on('--irb', 'Launch Gollum in 'console mode', with a predefined API.') do
-    options[:irb] = true
-  end
-  
-  opts.separator ''
-  opts.separator '  Minor:'
-  
-  opts.on('--h1-title', 'Use the first '<h1>' as page title.') do
-    wiki_options[:h1_title] = true
-  end
-  opts.on('--show-all', 'Also show files in the file view. By default, only valid pages are shown.') do
-    wiki_options[:show_all] = true
-  end
-  opts.on('--collapse-tree', 'Collapse the tree, when file view is opened. By default, the tree is expanded.') do
-    wiki_options[:collapse_tree] = true
-  end
-  opts.on('--user-icons [MODE]', [:gravatar, :identicon, :none], 'Use specific user-icons for history view.',
-    'Can be set to 'gravatar', 'identicon' or 'none'. Default: 'none'.') do |mode|
-    wiki_options[:user_icons] = mode
-  end
-  opts.on('--mathjax-config [FILE]', 'Specify path to a custom MathJax configuration.',
-    'If not specified, uses the '<git-repo>/mathjax.config.js' file.') do |file|
-    wiki_options[:mathjax_config] = file || 'mathjax.config.js'
-  end
-  opts.on('--plantuml-url [URL]', 'Sets the PlantUML server endpoint.') do |url|
-    wiki_options[:plantuml_url] = url
-  end
-  opts.on('--template-dir [PATH]', 'Specify custom mustache template directory.') do |path|
-    wiki_options[:template_dir] = path
-  end
-  
-  opts.separator ''
-  opts.separator '  Common:'
-  
-  opts.on('--help', 'Display this message.') do
-    puts opts
-    exit 0
-  end
-  opts.on('--version', 'Display the current version of Gollum.') do
-    puts 'Gollum ' + Gollum::VERSION
-    exit 0
-  end
-  
-  opts.separator ''
+    
+    When /^(?:|I )choose '([^']*)'$/ do |field|
+  choose(field)
 end
     
-    # The project root directory
-$root = ::File.dirname(__FILE__)
+    ENV['RAILS_ENV'] = 'test'
     
-    class ConfigTag < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-    options = options.split(' ').map {|i| i.strip }
-    @key = options.slice!(0)
-    @tag = nil
-    @classname = nil
-    options.each do |option|
-      @tag = $1 if option =~ /tag:(\S+)/ 
-      @classname = $1 if option =~ /classname:(\S+)/
+    require 'mimemagic'
+require 'mimemagic/overlay'
+require 'logger'
+require 'terrapin'
+    
+        # resize to a new geometry
+    # @param geometry [String] the Paperclip geometry definition to resize to
+    # @example
+    #   Paperclip::Geometry.new(150, 150).resize_to('50x50!')
+    #   #=> Paperclip::Geometry(50, 50)
+    def resize_to(geometry)
+      new_geometry = Paperclip::Geometry.parse geometry
+      case new_geometry.modifier
+      when '!', '#'
+        new_geometry
+      when '>'
+        if new_geometry.width >= self.width && new_geometry.height >= self.height
+          self
+        else
+          scale_to new_geometry
+        end
+      when '<'
+        if new_geometry.width <= self.width || new_geometry.height <= self.height
+          self
+        else
+          scale_to new_geometry
+        end
+      else
+        scale_to new_geometry
+      end
+    end
+    
+        module ClassMethods
+      def attachment_definitions
+        Paperclip::AttachmentRegistry.definitions_for(self)
+      end
     end
   end
-    
-    end
-    
-      class PageFilters < Octopress::Hooks::Page
-    def pre_render(page)
-      OctopressFilters::pre_filter(page)
-    end
-    
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
-    end
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
-        end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
-      end
-    end
+end
