0d5c696091f16e67dd372da93f934eb1d71e2f03
SUNsung <weatherius@gmail.com>
2018-12-12 02:58:29 +0200

    Rewrite [11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb] File in: 2018-12-12 02:58:28

--- ruby/11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb
+++ ruby/11qgYwUbyfJ.w11m9XAkgb2e.Q11xDJTtmlAVNw11jfo.rb
@@ -1,184 +1,98 @@
 
         
-          def failed_strategy
-    request.respond_to?(:get_header) ? request.get_header('omniauth.error.strategy') : request.env['omniauth.error.strategy']
-  end
+          context 'with associated agents' do
+    let!(:bob_scheduler_agent) {
+      Agents::SchedulerAgent.create!(
+        user: users(:bob),
+        name: 'Example Scheduler',
+        options: {
+          'action' => 'run',
+          'schedule' => '0 * * * *'
+        },
+      )
+    }
     
-      # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
-  end
+        if self.junk_headers
+      while str.length < 4096
+        if self.fold
+          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}:\r\n\t#{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
+        else
+          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}: #{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
+        end
+      end
+    end
     
-      # Override prefixes to consider the scoped view.
-  # Notice we need to check for the request due to a bug in
-  # Action Controller tests that forces _prefixes to be
-  # loaded before even having a request object.
+      #
+  # The method being used for the request (e.g. GET).
   #
-  # This method should be public as it is in ActionPack
-  # itself. Changing its visibility may break other gems.
-  def _prefixes #:nodoc:
-    @_prefixes ||= if self.class.scoped_views? && request && devise_mapping
-      ['#{devise_mapping.scoped_path}/#{controller_name}'] + super
-    else
-      super
+  attr_accessor :method
+  #
+  # The raw URI being requested, before any mucking gets to it
+  #
+  attr_accessor :raw_uri
+    
+      # Returns meta tags.
+  # You will probably want to use this the web app's version info (or other stuff) can be found
+  # in the metadata.
+  #
+  # @return [Array<Nokogiri::XML::Element>]
+  def get_html_meta_elements
+    n = get_html_document
+    n.search('//meta')
+  end
+    
+                components.each do |c|
+              encoded << [c.length].pack('N')
+              encoded << c
+            end
+    
+              # Encodes the authenticator field
+          #
+          # @return [String]
+          def encode_authenticator
+            authenticator.encode
+          end
+        end
+      end
     end
   end
-    
-    module Devise
-  module Controllers
-    # A module that may be optionally included in a controller in order
-    # to provide remember me behavior. Useful when signing in is done
-    # through a callback, like in OmniAuth.
-    module Rememberable
-      # Return default cookie values retrieved from session options.
-      def self.cookie_values
-        Rails.configuration.session_options.slice(:path, :domain, :secure)
-      end
-    
-        # Try retrieving the URL options from the parent controller (usually
-    # ApplicationController). Instance methods are not supported at the moment,
-    # so only the class-level attribute is used.
-    def self.default_url_options(*args)
-      if defined?(Devise.parent_controller.constantize)
-        Devise.parent_controller.constantize.try(:default_url_options) || {}
-      else
-        {}
-      end
-    end
-    
-            format('%1$*2$e', 109.52, -20).should == '1.095200e+02        '
-        format('%1$*2$E', 109.52, -20).should == '1.095200E+02        '
-        format('%1$*2$f', 10.952, -20).should == '10.952000           '
-        format('%1$*2$g', 12.1234, -20).should == '12.1234             '
-        format('%1$*2$G', 12.1234, -20).should == '12.1234             '
-        format('%1$*2$a', 196, -20).should == '0x1.88p+7           '
-        format('%1$*2$A', 196, -20).should == '0X1.88P+7           '
-    
-      it 'accepts and uses a seed of 0' do
-    srand(0)
-    srand.should == 0
-  end
-    
-      it 'no raises a RuntimeError on symbols' do
-    v = :sym
-    lambda { v.taint }.should_not raise_error(RuntimeError)
-    v.tainted?.should == false
-  end
-    
-      it 'returns true when passed ?f if the argument is a regular file' do
-    Kernel.test(?f, @file).should == true
-  end
-    
-      it 'raises an UncaughtThrowError if there is no catch block for the symbol' do
-    lambda { throw :blah }.should raise_error(UncaughtThrowError)
-  end
-    
-      after :each do
-    Object.send :remove_method, :boom
-  end
-    
-    require 'rake/testtask'
-Rake::TestTask.new do |t|
-  t.libs << 'test'
-  t.test_files = FileList['test/**/*_test.rb']
-  t.verbose = true
 end
     
-      # Update version.rb file with BOOTSTRAP_SHA
-  def store_version
-    path    = 'lib/bootstrap-sass/version.rb'
-    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
-    File.open(path, 'w') { |f| f.write(content) }
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a Kerberos AuthorizationData data
+        # definition.
+        class AuthorizationData < Element
+          # @!attribute elements
+          #   @return [Hash{Symbol => <Integer, String>}] The type of the authorization data
+          #   @option [Integer] :type
+          #   @option [String] :data
+          attr_accessor :elements
+    
+    When /^(?:|I )click on '([^']*)' navbar title$/ do |title|
+  with_scope('.info-bar') do
+    find('h5', text: title).click
   end
 end
 
     
-        def byte_to_str_pos(pos)
-      @s.string.byteslice(0, pos).length
-    end
+      def post_path_by_content(text)
+    p = Post.find_by_text(text)
+    post_path(p)
+  end
     
-        def log_transform(*args, from: caller[1][/`.*'/][1..-2].sub(/^block in /, ''))
-      puts '    #{cyan from}#{cyan ': #{args * ', '}' unless args.empty?}'
+        it 'generates the contacts_json fixture', :fixture => true do
+      json = bob.contacts.map { |c|
+               ContactPresenter.new(c, bob).full_hash_with_person
+             }.to_json
+      save_fixture(json, 'contacts_json')
     end
-    
-      # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-    
-    desc 'Update configurations to support publishing to root or sub directory'
-task :set_root_dir, :dir do |t, args|
-  puts '>>> !! Please provide a directory, eg. rake config_dir[publishing/subdirectory]' unless args.dir
-  if args.dir
-    if args.dir == '/'
-      dir = ''
-    else
-      dir = '/' + args.dir.sub(/(\/*)(.+)/, '\\2').sub(/\/$/, '');
-    end
-    rakefile = IO.read(__FILE__)
-    rakefile.sub!(/public_dir(\s*)=(\s*)([''])[\w\-\/]*['']/, 'public_dir\\1=\\2\\3public#{dir}\\3')
-    File.open(__FILE__, 'w') do |f|
-      f.write rakefile
-    end
-    compass_config = IO.read('config.rb')
-    compass_config.sub!(/http_path(\s*)=(\s*)([''])[\w\-\/]*['']/, 'http_path\\1=\\2\\3#{dir}/\\3')
-    compass_config.sub!(/http_images_path(\s*)=(\s*)([''])[\w\-\/]*['']/, 'http_images_path\\1=\\2\\3#{dir}/images\\3')
-    compass_config.sub!(/http_fonts_path(\s*)=(\s*)([''])[\w\-\/]*['']/, 'http_fonts_path\\1=\\2\\3#{dir}/fonts\\3')
-    compass_config.sub!(/css_dir(\s*)=(\s*)([''])[\w\-\/]*['']/, 'css_dir\\1=\\2\\3public#{dir}/stylesheets\\3')
-    File.open('config.rb', 'w') do |f|
-      f.write compass_config
-    end
-    jekyll_config = IO.read('_config.yml')
-    jekyll_config.sub!(/^destination:.+$/, 'destination: public#{dir}')
-    jekyll_config.sub!(/^subscribe_rss:\s*\/.+$/, 'subscribe_rss: #{dir}/atom.xml')
-    jekyll_config.sub!(/^root:.*$/, 'root: /#{dir.sub(/^\//, '')}')
-    File.open('_config.yml', 'w') do |f|
-      f.write jekyll_config
-    end
-    rm_rf public_dir
-    mkdir_p '#{public_dir}#{dir}'
-    puts '## Site's root directory is now '/#{dir.sub(/^\//, '')}' ##'
   end
 end
+
     
-    class SinatraStaticServer < Sinatra::Base
-    
-    
-  # Adds some extra filters used during the category creation process.
-  module Filters
-    
-        def get_cache_file_for(gist, file)
-      bad_chars = /[^a-zA-Z0-9\-_.]/
-      gist      = gist.gsub bad_chars, ''
-      file      = file.gsub bad_chars, ''
-      md5       = Digest::MD5.hexdigest '#{gist}-#{file}'
-      File.join @cache_folder, '#{gist}-#{file}-#{md5}.cache'
-    end
-    
-      # Summary is used on the Archive pages to return the first block of content from a post.
-  def summary(input)
-    if input.index(/\n\n/)
-      input.split(/\n\n/)[0]
-    else
-      input
-    end
-  end
-    
-            def right_brace_and_space(loc_end, space)
-          brace_and_space =
-            range_with_surrounding_space(
-              range: loc_end,
-              side: :left,
-              newlines: space[:newlines],
-              whitespace: space[:right]
-            )
-          range_with_surrounding_comma(brace_and_space, :left)
-        end
-    
-      let(:cop_config) { { 'EnforcedStyle' => 'symmetrical' } }
-    
-    RSpec.describe RuboCop::Cop::Layout::MultilineHashBraceLayout, :config do
-  subject(:cop) { described_class.new(config) }
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
