977dd5b35b864f0445f8077bce2349adb6991b5c
SUNsung <weatherius@gmail.com>
2018-12-12 18:55:56 +0200

    Rewrite [50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py] File in: 2018-12-12 18:55:55

--- pyton/50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py
+++ pyton/50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py
@@ -1,93 +1,164 @@
 
         
-            return app
+        
+class Operator(Employee):
+    
+        def add_friend_request(self, from_user_id, to_user_id):
+        pass
+    
+        HOUSING = 0
+    FOOD = 1
+    GAS = 2
+    SHOPPING = 3
+    # ...
+    
+            Emit key value pairs of the form:
+    
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
+    
+    
+atom_template = textwrap.dedent('''\
+    <?xml version='1.0' encoding='utf-8'?>
+    <feed xmlns='http://www.w3.org/2005/Atom'>
+        <link rel='self' href='http://rg3.github.io/youtube-dl/update/releases.atom' />
+        <title>youtube-dl releases</title>
+        <id>https://yt-dl.org/feed/youtube-dl-updates-feed</id>
+        <updated>@TIMESTAMP@</updated>
+        @ENTRIES@
+    </feed>''')
+    
+        # Get the version from youtube_dl/version.py without importing the package
+    exec(compile(open('youtube_dl/version.py').read(),
+                 'youtube_dl/version.py', 'exec'))
+    
+    
+if __name__ == '__main__':
+    main()
 
     
+        def __str__(self):
+        '''Identify each test with the `add_ie` attribute, if available.'''
     
-@pytest.mark.parametrize('path', (
-    '/create',
-    '/1/update',
-))
-def test_create_update_validate(client, auth, path):
-    auth.login()
-    response = client.post(path, data={'title': '', 'body': ''})
-    assert b'Title is required.' in response.data
+        def _targets_request(self, targets):
+        '''Formats each target for the request'''
+        targets_request = []
+        for target in targets:
+            target_request = {
+                'Id': target['id'],
+                'Arn': target['arn']
+            }
+            if 'input' in target:
+                target_request['Input'] = target['input']
+            if 'input_path' in target:
+                target_request['InputPath'] = target['input_path']
+            if 'role_arn' in target:
+                target_request['RoleArn'] = target['role_arn']
+            if 'ecs_parameters' in target:
+                target_request['EcsParameters'] = {}
+                ecs_parameters = target['ecs_parameters']
+                if 'task_definition_arn' in target['ecs_parameters']:
+                    target_request['EcsParameters']['TaskDefinitionArn'] = ecs_parameters['task_definition_arn']
+                if 'task_count' in target['ecs_parameters']:
+                    target_request['EcsParameters']['TaskCount'] = ecs_parameters['task_count']
+            targets_request.append(target_request)
+        return targets_request
     
-    import pytest
-from flaskr.db import get_db
-    
-        from inspect import getargspec
-    from cStringIO import StringIO
-    
-    
-def jsonify(*args, **kwargs):
-    '''This function wraps :func:`dumps` to add a few enhancements that make
-    life easier.  It turns the JSON output into a :class:`~flask.Response`
-    object with the :mimetype:`application/json` mimetype.  For convenience, it
-    also converts multiple arguments into an array or multiple keyword arguments
-    into a dict.  This means that both ``jsonify(1,2,3)`` and
-    ``jsonify([1,2,3])`` serialize to ``[1,2,3]``.
-    
-    Originally created for this issue:
-https://github.com/scrapy/scrapy/issues/606
-    
-        cmd = cmds[cmdname]
-    parser.usage = 'scrapy %s %s' % (cmdname, cmd.syntax())
-    parser.description = cmd.long_desc()
-    settings.setdict(cmd.default_settings, priority='command')
-    cmd.settings = settings
-    cmd.add_options(parser)
-    opts, args = parser.parse_args(args=argv[1:])
-    _run_print_help(parser, cmd.process_options, args, opts)
-    
-                tested_methods = conman.tested_methods_from_spidercls(spidercls)
-            if opts.list:
-                for method in tested_methods:
-                    contract_reqs[spidercls.name].append(method)
-            elif tested_methods:
-                self.crawler_process.crawl(spidercls)
-    
-        def gotClient(self, client, request, filepath):
-        self.client = client
-        protocol = ReceivedDataProtocol(request.meta.get('ftp_local_filename'))
-        return client.retrieveFile(filepath, protocol)\
-                .addCallbacks(callback=self._build_response,
-                        callbackArgs=(request, protocol),
-                        errback=self._failed,
-                        errbackArgs=(request,))
+            last_snapshot_min_age = last_snapshot_min_age * 60  # Convert to seconds
+        snapshot = _get_most_recent_snapshot(current_snapshots,
+                                             max_snapshot_age_secs=last_snapshot_min_age)
+    try:
+        # Create a new snapshot if we didn't find an existing one to use
+        if snapshot is None:
+            snapshot = ec2.create_snapshot(volume_id, description=description)
+            changed = True
+        if wait:
+            if not _create_with_wait(snapshot, wait_timeout):
+                module.fail_json(msg='Timed out while creating snapshot.')
+        if snapshot_tags:
+            for k, v in snapshot_tags.items():
+                snapshot.add_tag(k, v)
+    except boto.exception.BotoServerError as e:
+        module.fail_json(msg='%s: %s' % (e.error_code, e.error_message))
     
     
-def generateKey(keySize):
-    print('Generating prime p...')
-    p = rabinMiller.generateLargePrime(keySize)  # select large prime number.
-    e_1 = primitiveRoot(p)  # one primitive root on modulo p.
-    d = random.randrange(3, p)  # private_key -> have to be greater than 2 for safety.
-    e_2 = cryptoMath.findModInverse(pow(e_1, d, p), p)
-    
-    def rabinMiller(num):
-    s = num - 1
-    t = 0
-    
-            next_prime_gt = next_prime(value % self.size_table) \
-            if not check_prime(value % self.size_table) else value % self.size_table  #gt = bigger than
-        return next_prime_gt - (data % next_prime_gt)
-    
-    print('writing anagrams to file...')
-with open('anagrams.txt', 'w') as file:
-    file.write('all_anagrams = ')
-    file.write(pprint.pformat(all_anagrams))
+@AWSRetry.exponential_backoff()
+def describe_cache_clusters_with_backoff(client, cluster_id=None):
+    paginator = client.get_paginator('describe_cache_clusters')
+    params = dict(ShowCacheNodeInfo=True)
+    if cluster_id:
+        params['CacheClusterId'] = cluster_id
+    try:
+        response = paginator.paginate(**params).build_full_result()
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == 'CacheClusterNotFound':
+            return []
+        raise
+    except botocore.exceptions.BotoCoreError:
+        raise
+    return response['CacheClusters']
     
     
-'''
-* Wondering how this method works !
-* It's pretty simple.
-* Let's say you need to calculate a ^ b
-* RULE 1 : a * b = (a+a) * (b/2) ---- example : 4 * 4 = (4+4) * (4/2) = 8 * 2
-* RULE 2 : IF b is ODD, then ---- a * b = a + (a * (b - 1)) :: where (b - 1) is even.
-* Once b is even, repeat the process to get a * b
-* Repeat the process till b = 1 OR b = 0, because a*1 = a AND a*0 = 0
-*
-* As far as the modulo is concerned,
-* the fact : (a+b) % c = ((a%c) + (b%c)) % c
-* Now apply RULE 1 OR 2, whichever is required.
-'''
+@AWSRetry.exponential_backoff()
+def list_iam_roles_with_backoff(client, **kwargs):
+    paginator = client.get_paginator('list_roles')
+    return paginator.paginate(**kwargs).build_full_result()
+    
+    # Configure an s3 bucket to redirect all requests to example.com
+- s3_website:
+    name: mybucket.com
+    redirect_all_requests: example.com
+    state: present
+    
+    
+class AzureRMManagedClusterFacts(AzureRMModuleBase):
+    '''Utility class to get Azure Kubernetes Service facts'''
+    
+    
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+    
+            print('Training %s ... ' % name, end='')
+        t0 = time()
+        clf.fit(X_train, y_train)
+        train_time[name] = time() - t0
+        t0 = time()
+        y_pred = clf.predict(X_test)
+        test_time[name] = time() - t0
+        accuracy[name] = accuracy_score(y_test, y_pred)
+        print('done')
+    
+        i = 0
+    for c, (label, timings) in zip('br',
+                                   sorted(six.iteritems(results_2))):
+        i += 1
+        ax = fig.add_subplot(2, 2, i + 2)
+        y = np.asarray(timings)
+        ax.plot(chunks, y, color=c, alpha=0.8)
+        ax.set_xlabel('Chunks')
+        ax.set_ylabel(label)
+    
+        #------------------------------------------------------------
+    # varying N
+    N_results_build = dict([(alg, np.zeros(len(Nrange)))
+                            for alg in algorithms])
+    N_results_query = dict([(alg, np.zeros(len(Nrange)))
+                            for alg in algorithms])
+    
+        ax.set_xlabel('n_samples')
+    ax.set_ylabel('n_features')
+    ax.set_zlabel('Time (s)')
+    ax.legend()
+    plt.show()
+
+    
+    ROOT_URL = 'https://api.github.com/repos/scikit-learn/scikit-learn.github.io/contents/'  # noqa
+RAW_FMT = 'https://raw.githubusercontent.com/scikit-learn/scikit-learn.github.io/master/%s/documentation.html'  # noqa
+VERSION_RE = re.compile(r'\bVERSION:\s*'([^']+)'')
+NAMED_DIRS = ['dev', 'stable']
