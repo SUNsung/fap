f4321cacb5fa374a25acc2d7db0605ec96731dd1
SUNsung <weatherius@gmail.com>
2018-12-12 03:56:33 +0200

    Rewrite [25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb] File in: 2018-12-12 03:56:32

--- ruby/25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb
+++ ruby/25P653kMU1H4Y25xRF01Cn5I6225R9gYxPtvDr62509u.rb
@@ -1,122 +1,103 @@
 
         
-              if staff.topic_id.nil?
-        creator = PostCreator.new(Discourse.system_user,
-          raw: I18n.t('staff_category_description'),
-          title: I18n.t('category.topic_prefix', category: staff.name),
-          category: staff.name,
-          archetype: Archetype.default
-        )
-        post = creator.create
-    
-    module Vagrant
-  # This class handles guest-OS specific interactions with a machine.
-  # It is primarily responsible for detecting the proper guest OS
-  # implementation and then delegating capabilities.
-  #
-  # Vagrant has many tasks which require specific guest OS knowledge.
-  # These are implemented using a guest/capability system. Various plugins
-  # register as 'guests' which determine the underlying OS of the system.
-  # Then, 'guest capabilities' register themselves for a specific OS (one
-  # or more), and these capabilities are called.
-  #
-  # Example capabilities might be 'mount_virtualbox_shared_folder' or
-  # 'configure_networks'.
-  #
-  # This system allows for maximum flexibility and pluginability for doing
-  # guest OS specific operations.
-  class Guest
-    include CapabilityHost
-    
-            # This is the method called to provision the system. This method
-        # is expected to do whatever necessary to provision the system (create files,
-        # SSH, etc.)
-        def provision!
-        end
-    
-                    @env.machine_names.each do |machine_name|
-                  if machine_name =~ regex
-                    machines << get_machine.call(machine_name)
-                  end
-                end
-    
-      preflight do
-    processes = system_command '/bin/launchctl', args: ['list']
-    
-    require_relative 'converter/fonts_conversion'
-require_relative 'converter/less_conversion'
-require_relative 'converter/js_conversion'
-require_relative 'converter/logger'
-require_relative 'converter/network'
-    
-        def byte_to_str_pos(pos)
-      @s.string.byteslice(0, pos).length
-    end
-    
-        # apply general less to scss conversion
-    def convert_to_scss(file)
-      # get local mixin names before converting the definitions
-      mixins = shared_mixins + read_mixins(file)
-      file   = replace_vars(file)
-      file   = replace_mixin_definitions(file)
-      file   = replace_mixins(file, mixins)
-      file   = extract_mixins_from_selectors(file, CLASSES_TO_MIXINS.inject({}) { |h, cl| h.update('.#{cl}' => cl) })
-      file   = replace_spin(file)
-      file   = replace_fadein(file)
-      file   = replace_image_urls(file)
-      file   = replace_escaping(file)
-      file   = convert_less_ampersand(file)
-      file   = deinterpolate_vararg_mixins(file)
-      file   = replace_calculation_semantics(file)
-      file   = replace_file_imports(file)
-      file   = wrap_at_groups_with_at_root(file)
-      file
-    end
-    
-      def run_vagrant_command(command)
-    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
-    return [stdout, stderr] if status.success?
-    raise VagrantSSHCommandError, status
+          def to_str
+    @paths.join(File::PATH_SEPARATOR)
   end
+  alias to_s to_str
+    
+          old_initial_revision_var = 'HOMEBREW_UPDATE_BEFORE#{repo_var}'
+      old_current_revision_var = 'HOMEBREW_UPDATE_AFTER#{repo_var}'
+    
+      url 'http://swupdl.adobe.com/updates/oobe/aam20/mac/AdobeLightroom-#{version.major}.0/#{version}/setup.dmg'
+  name 'Adobe Photoshop Lightroom'
+  homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
+    
+    Rake::TestTask.new(:'test:core') do |t|
+  core_tests = %w[base delegator encoding extensions filter
+     helpers mapped_error middleware radius rdoc
+     readme request response result route_added_hook
+     routing server settings sinatra static templates]
+  t.test_files = core_tests.map {|n| 'test/#{n}_test.rb'}
+  t.ruby_opts = ['-r rubygems'] if defined? Gem
+  t.ruby_opts << '-I.'
+  t.warning = true
 end
     
-          def assert_value_or_block_not_both(value, block)
-        return if value.nil? || block.nil?
-        raise Capistrano::ValidationError,
-              'Value and block both passed to Configuration#set'
+      # fetch data
+  fields = {
+    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
+    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
+    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
+  }
+    
+          private
+    
+          it 'does not autocorrect the closing brace' do
+        new_source = autocorrect_source(source)
+        expect(new_source).to eq([source].join($RS))
       end
+    end
     
-    Given /^(?:|I )am on (.+)$/ do |page_name|
-  visit path_to(page_name)
-end
-    
-      def framework_major_version
-    framework_version.split('.').first.to_i
+          # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
+      #
+      # In this case, the `def` node destructures into:
+      #
+      #   `method_name, arguments, body`
+      #
+      # while the `defs` node destructures into:
+      #
+      #   `receiver, method_name, arguments, body`
+      #
+      # so we reverse the destructured array to get the optional receiver
+      # at the end, where it can be discarded.
+      #
+      # @return [Array] the different parts of the `def` or `defs` node
+      def node_parts
+        to_a.reverse
+      end
+    end
   end
 end
-World(RailsCommandHelpers)
 
     
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
-    
-        def geometry_string
-      begin
-        orientation = Paperclip.options[:use_exif_orientation] ?
-          '%[exif:orientation]' : '1'
-        Paperclip.run(
-          Paperclip.options[:is_windows] ? 'magick identify' : 'identify',
-          '-format '%wx%h,#{orientation}' :file', {
-            :file => '#{path}[0]'
-          }, {
-            :swallow_stderr => true
-          }
-        )
-      rescue Terrapin::ExitStatusError
-        ''
-      rescue Terrapin::CommandNotFoundError => e
-        raise_because_imagemagick_missing
+          # Returns the operator for the `kwsplat` as a string.
+      #
+      # @return [String] the double splat operator
+      def operator
+        DOUBLE_SPLAT
       end
+    
+          def self.default_generator_root
+        File.dirname(__FILE__)
+      end
+    
+            ActiveSupport.on_load(:active_record) do
+          include Sidekiq::Extensions::ActiveRecord
+        end
+        ActiveSupport.on_load(:action_mailer) do
+          extend Sidekiq::Extensions::ActionMailer
+        end
+      end
+    
+          Sidekiq.logger.debug { 'Re-queueing terminated jobs' }
+      jobs_to_requeue = {}
+      inprogress.each do |unit_of_work|
+        jobs_to_requeue[unit_of_work.queue_name] ||= []
+        jobs_to_requeue[unit_of_work.queue_name] << unit_of_work.job
+      end
+    
+        def self.job_hash_context(job_hash)
+      # If we're using a wrapper class, like ActiveJob, use the 'wrapped'
+      # attribute to expose the underlying thing.
+      klass = job_hash['wrapped'] || job_hash['class']
+      bid = job_hash['bid']
+      '#{klass} JID-#{job_hash['jid']}#{' BID-#{bid}' if bid}'
     end
+    
+          def jobs_by_queue
+        @jobs_by_queue ||= Hash.new { |hash, key| hash[key] = [] }
+      end
+    
+        def render(engine, content, options = {})
+      raise 'Only erb templates are supported' if engine != :erb
