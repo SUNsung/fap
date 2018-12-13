aef8e9c5fc0716a5000196c19304d04fccaba842
SUNsung <weatherius@gmail.com>
2018-12-13 05:50:04 +0200

    Create [03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp] File in: 2018-12-13 05:50:03

---
+++ cccccc/03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp
@@ -0,0 +1,192 @@
+    /** Colorize an image (given filename) with the icon color */
+    QImage SingleColorImage(const QString& filename) const;
+    
+    /* QComboBox that can be used with QDataWidgetMapper to select ordinal values from a model. */
+class QValueComboBox : public QComboBox
+{
+    Q_OBJECT
+    }
+    
+        explicit reverse_lock(Lock& _lock) : lock(_lock) {
+        _lock.unlock();
+        _lock.swap(templock);
+    }
+    
+        // Special handling for null case
+    // (needed because string comparison reads the null as end-of-string)
+    BOOST_TEST_MESSAGE(std::string('CheckParseTorReplyMapping(Null=\'\\0\')'));
+    auto ret = ParseTorReplyMapping('Null=\'\\0\'');
+    BOOST_CHECK_EQUAL(ret.size(), 1U);
+    auto r_it = ret.begin();
+    BOOST_CHECK_EQUAL(r_it->first, 'Null');
+    BOOST_CHECK_EQUAL(r_it->second.size(), 1U);
+    BOOST_CHECK_EQUAL(r_it->second[0], '\0');
+    
+    SparsePageWriter::SparsePageWriter(
+    const std::vector<std::string>& name_shards,
+    const std::vector<std::string>& format_shards,
+    size_t extra_buffer_capacity)
+    : num_free_buffer_(extra_buffer_capacity + name_shards.size()),
+      clock_ptr_(0),
+      workers_(name_shards.size()),
+      qworkers_(name_shards.size()) {
+  CHECK_EQ(name_shards.size(), format_shards.size());
+  // start writer threads
+  for (size_t i = 0; i < name_shards.size(); ++i) {
+    std::string name_shard = name_shards[i];
+    std::string format_shard = format_shards[i];
+    auto* wqueue = &qworkers_[i];
+    workers_[i].reset(new std::thread(
+        [this, name_shard, format_shard, wqueue] () {
+          std::unique_ptr<dmlc::Stream> fo(
+              dmlc::Stream::Create(name_shard.c_str(), 'w'));
+          std::unique_ptr<SparsePageFormat> fmt(
+              SparsePageFormat::Create(format_shard));
+          fo->Write(format_shard);
+          std::shared_ptr<SparsePage> page;
+          while (wqueue->Pop(&page)) {
+            if (page == nullptr) break;
+            fmt->Write(*page, fo.get());
+            qrecycle_.Push(std::move(page));
+          }
+          fo.reset(nullptr);
+          LOG(CONSOLE) << 'SparsePage::Writer Finished writing to ' << name_shard;
+        }));
+  }
+}
+    
+    // logistic loss for probability regression task
+struct LogisticRegression {
+  // duplication is necessary, as __device__ specifier
+  // cannot be made conditional on template parameter
+  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return common::Sigmoid(x); }
+  XGBOOST_DEVICE static bool CheckLabel(bst_float x) { return x >= 0.0f && x <= 1.0f; }
+  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
+    return predt - label;
+  }
+  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
+    const float eps = 1e-16f;
+    return fmaxf(predt * (1.0f - predt), eps);
+  }
+  template <typename T>
+  static T PredTransform(T x) { return common::Sigmoid(x); }
+  template <typename T>
+  static T FirstOrderGradient(T predt, T label) { return predt - label; }
+  template <typename T>
+  static T SecondOrderGradient(T predt, T label) {
+    const T eps = T(1e-16f);
+    return std::max(predt * (T(1.0f) - predt), eps);
+  }
+  static bst_float ProbToMargin(bst_float base_score) {
+    CHECK(base_score > 0.0f && base_score < 1.0f)
+      << 'base_score must be in (0,1) for logistic loss';
+    return -logf(1.0f / base_score - 1.0f);
+  }
+  static const char* LabelErrorMsg() {
+    return 'label must be in [0,1] for logistic regression';
+  }
+  static const char* DefaultEvalMetric() { return 'rmse'; }
+};
+    
+    namespace xgboost {
+namespace common {
+    }
+    }
+    
+    
+    {    // loop over all rows and fill column entries
+    // num_nonzeros[fid] = how many nonzeros have this feature accumulated so far?
+    std::vector<size_t> num_nonzeros;
+    num_nonzeros.resize(nfeature);
+    std::fill(num_nonzeros.begin(), num_nonzeros.end(), 0);
+    for (size_t rid = 0; rid < nrow; ++rid) {
+      const size_t ibegin = gmat.row_ptr[rid];
+      const size_t iend = gmat.row_ptr[rid + 1];
+      size_t fid = 0;
+      for (size_t i = ibegin; i < iend; ++i) {
+        const uint32_t bin_id = gmat.index[i];
+        while (bin_id >= gmat.cut.row_ptr[fid + 1]) {
+          ++fid;
+        }
+        if (type_[fid] == kDenseColumn) {
+          uint32_t* begin = &index_[boundary_[fid].index_begin];
+          begin[rid] = bin_id - index_base_[fid];
+        } else {
+          uint32_t* begin = &index_[boundary_[fid].index_begin];
+          begin[num_nonzeros[fid]] = bin_id - index_base_[fid];
+          row_ind_[boundary_[fid].row_ind_begin + num_nonzeros[fid]] = rid;
+          ++num_nonzeros[fid];
+        }
+      }
+    }
+  }
+    
+    namespace {
+void readBytes(BufferedFile& fp, unsigned char* buf, size_t buflen,
+               size_t readlen)
+{
+  assert(readlen <= buflen);
+  READ_CHECK(fp, buf, readlen);
+}
+} // namespace
+    
+    DHTTaskExecutor::~DHTTaskExecutor() = default;
+    
+      int getNumConcurrent() const { return numConcurrent_; }
+    
+    
+    {} // namespace aria2
+    
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port) const
+{
+  return generateToken(infoHash, ipaddr, port, secret_[0]);
+}
+    
+    const std::string& DNSCache::find(const std::string& hostname,
+                                  uint16_t port) const
+{
+  auto target = std::make_shared<CacheEntry>(hostname, port);
+  auto i = entries_.find(target);
+  if (i == entries_.end()) {
+    return A2STR::NIL;
+  }
+  else {
+    return (*i)->getGoodAddr();
+  }
+}
+    
+        while (state.KeepRunning())
+    {
+        state.PauseTiming();
+        auto* j = new json();
+        state.ResumeTiming();
+    }
+    
+    
+If a benchmark runs a few milliseconds it may be hard to visually compare the
+measured times, since the output data is given in nanoseconds per default. In
+order to manually set the time unit, you can specify it manually:
+    
+    
+    {
+    {}  // end namespace internal
+}  // end namespace benchmark
+    
+    ConsoleReporter::OutputOptions GetOutputOptions(bool force_no_color = false);
+    
+      const CPUInfo &info = context.cpu_info;
+  Out << 'Run on (' << info.num_cpus << ' X '
+      << (info.cycles_per_second / 1000000.0) << ' MHz CPU '
+      << ((info.num_cpus > 1) ? 's' : '') << ')\n';
+  if (info.caches.size() != 0) {
+    Out << 'CPU Caches:\n';
+    for (auto &CInfo : info.caches) {
+      Out << '  L' << CInfo.level << ' ' << CInfo.type << ' '
+          << (CInfo.size / 1000) << 'K';
+      if (CInfo.num_sharing != 0)
+        Out << ' (x' << (info.num_cpus / CInfo.num_sharing) << ')';
+      Out << '\n';
+    }
+  }
