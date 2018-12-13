ee203c0ae59d22c30dc465ad56840b777c97636a
SUNsung <weatherius@gmail.com>
2018-12-13 12:19:36 +0200

    Rewrite [55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp] File in: 2018-12-13 12:19:36

--- cccccc/55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp
+++ cccccc/55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp
@@ -1,150 +1,139 @@
 
         
-        void AutoUpdater::OnWindowAllClosed() {
-  QuitAndInstall();
-}
-    
-    #include 'atom/browser/api/event_emitter.h'
-#include 'atom/browser/mac/in_app_purchase.h'
-#include 'atom/browser/mac/in_app_purchase_observer.h'
-#include 'atom/browser/mac/in_app_purchase_product.h'
-#include 'native_mate/handle.h'
-    
-    namespace {
-    }
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(TrackableObjectBase);
+        
+    {  std::vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'SubGradient',
+        '',
+        std::vector<std::string>{GO(0), I(0), I(1)},
+        std::vector<std::string>{GI(0), GI(1)});
+  }
 };
     
-    #include 'content/public/browser/javascript_dialog_manager.h'
+    #include 'caffe2/core/context.h'
     
-    IPC_MESSAGE_ROUTED3(ShellViewHostMsg_Call_Static_Method,
-                    std::string /* type name */,
-                    std::string /* method name */,
-                    base::ListValue /* arguments */)
-    
-    
-void Base::Call(const std::string& method, const base::ListValue& arguments,
-                content::RenderFrameHost* rvh) {
-  NOTREACHED() << 'Uncatched call in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
-}
-    
-    // Call method of an object in browser.
-// function CallObjectMethod(id, type, method, args);
-v8::Handle<v8::Value> CallObjectMethod(int routing_id,
-                                       int object_id,
-                                       const std::string& type,
-                                       const std::string& method,
-                                       v8::Handle<v8::Value> args);
-    
-    
-    {  // Convert from content coordinates to window coordinates.
-  // This code copied from chrome_web_contents_view_delegate_views.cc
-  aura::Window* target_window = GetActiveNativeView(rfh);
-  aura::Window* root_window = target_window->GetRootWindow();
-  views::Widget* top_level_widget =
-    views::Widget::GetTopLevelWidgetForNativeView(target_window);
-  aura::client::ScreenPositionClient* screen_position_client =
-        aura::client::GetScreenPositionClient(root_window);
-  if (screen_position_client) {
-    screen_position_client->ConvertPointToScreen(target_window,
-             &screen_point);
-  }
-  set_delay_destruction(true);
-  menu_runner_.reset(new views::MenuRunner(menu_model_.get(), views::MenuRunner::CONTEXT_MENU,
-                                           base::Bind(&Menu::OnMenuClosed, base::Unretained(this))));
-  menu_runner_->RunMenuAt(top_level_widget,
-                       nullptr,
-                       gfx::Rect(screen_point, gfx::Size()),
-                       views::MENU_ANCHOR_TOPRIGHT,
-                       ui::MENU_SOURCE_NONE);
-  // It is possible for the same MenuMessageLoopAura to start a nested
-  // message-loop while it is already running a nested loop. So make
-  // sure the quit-closure gets reset to the outer loop's quit-closure
-  // once the innermost loop terminates.
-  {
-    base::AutoReset<base::Closure> reset_quit_closure(&message_loop_quit_,
-                                                      base::Closure());
-  
-    //base::MessageLoop* loop = base::MessageLoop::current();
-    base::MessageLoopCurrent::ScopedNestableTaskAllower allow;
-    base::RunLoop run_loop;
-    message_loop_quit_ = run_loop.QuitClosure();
-  
-    run_loop.Run();
-  }
-  set_delay_destruction(false);
-  if (pending_destruction())
-    object_manager_->OnDeallocateObject(id_);
-}
-    
-    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
+    // FreeOp frees the content of the output blob. We allow it to take in input
+// blobs purely for the reason that it can 'wait' on the input blobs to be
+// produced by some of the earlier operators before a free is called.
+template <class Context>
+class FreeOp : public Operator<Context> {
  public:
-  NwMenuGetNSStringFWithFixupFunction() {}
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    
- protected:
-  ~NwMenuGetNSStringFWithFixupFunction() override {}
-    
-  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
-};
-    
-      uint64_t Size(const Slice& start, const Slice& limit) {
-    Range r(start, limit);
-    uint64_t size;
-    db_->GetApproximateSizes(&r, 1, &size);
-    return size;
-  }
-    
-        const char* benchmarks = FLAGS_benchmarks;
-    while (benchmarks != nullptr) {
-      const char* sep = strchr(benchmarks, ',');
-      Slice name;
-      if (sep == nullptr) {
-        name = benchmarks;
-        benchmarks = nullptr;
-      } else {
-        name = Slice(benchmarks, sep - benchmarks);
-        benchmarks = sep + 1;
-      }
+  FreeOp(const OperatorDef& def, Workspace* ws) : Operator<Context>(def, ws) {}
     }
     
-      MutexLock l(&mutex_);
-  writers_.push_back(&w);
-  while (!w.done && &w != writers_.front()) {
-    w.cv.Wait();
-  }
-  if (w.done) {
-    return w.status;
-  }
+    OPERATOR_SCHEMA(GatherRangesToDense)
+    .NumInputs(2, 3)
+    .NumOutputs(1, INT_MAX)
+    .SetDoc(R'DOC(
+Given DATA tensor of rank 1, and RANGES tensor of rank 3, gather values
+corresponding to each range into a separate output tensor. If the optional input
+KEY tensor is also given, the output will be sorted by KEY for each example.
     
-    TEST(FormatTest, InternalKeyShortestSuccessor) {
-  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
-            ShortSuccessor(IKey('foo', 100, kTypeValue)));
-  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
-            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
-}
+            // Return true if the update should use classic momentum and 
+        // false if the unit-gain momentum should be used instead.
+        bool UseUnitGainMomentum() const
+        {
+            return m_unitGain;
+        }
+    
+    
+    {            sequenceLengths[i] = currentSequenceDataShape.SubShape(fullyDefinedSampleShape.Rank()).TotalSize();
+            maxSequenceLength = std::max(maxSequenceLength, sequenceLengths[i]);
+        }
+    
+            static NDShape GetUnpackedShape(const NDShape& sampleShape, const std::vector<Axis>& sampleDynamicAxes, const std::shared_ptr<Microsoft::MSR::CNTK::MBLayout>& packedDataLayout)
+        {
+            // Determine unpacked shape
+            auto unpackedShape = sampleShape;
+            if (packedDataLayout)
+            {
+                if (sampleDynamicAxes.empty())
+                    LogicError('A PackedValue object that has a layout must have at least one dynamic axis.');
+    }
+    }
     
     
     {
-    {}  // namespace
-}  // namespace leveldb
+    {
+    {}}}
+
     
-    Writer::Writer(WritableFile* dest, uint64_t dest_length)
-    : dest_(dest), block_offset_(dest_length % kBlockSize) {
-  InitTypeCrc(type_crc_);
+    
+    {
+    {
+    {}}}
+
+    
+            // also 'edit' all nodes that have updated *inputs*
+        // All nodes that take inputs that have been edited must have their inputs updated.
+        // Since we do not update the model in-place, we must also create replacements for these.
+        // That is achieved by recursively including all parents of edits into the set of edits.
+        let parents = net.CreateParentsMap();
+        deque<ComputationNodeBasePtr> workList; // work list for recursion
+        for (let& replacement : replacements)
+            workList.push_back(replacement.first);
+        while (!workList.empty())
+        {
+            let node = workList.front();
+            workList.pop_front();
+            // loop over the node's parents
+            for (let& parent : parents.find(node)->second)
+            {
+                // 'edit' (clone) the parent if not yet
+                if (replacements.find(parent) != replacements.end())
+                    continue; // already a known replacement
+                // we must 'edit' the parent since it depends on a replaced input
+                replacements[parent] = parent->Duplicate();
+                // and put this parent into the workList, so that we will gets its parent in turn, etc.
+                workList.push_back(parent);
+#if 0 //def _DEBUG
+                fprintf(stderr, '\t%ls = %ls() --> relink %ls\n', parent->NodeName().c_str(), parent->OperationName().c_str(), replacements[parent]->NodeName().c_str());
+#endif
+            }
+        }
+        if (TraceLevel() > 0)
+            fprintf(stderr, 'Edit: %d out of %d nodes were either edited or need to be relinked.\n', (int)replacements.size(), (int)net.GetTotalNumberOfNodes());
+        // Now the keys of replacements[] define the set of all nodes that must be relinked.
+    
+        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange& fr) override
+    {
+        auto sliceInputValue  = InputRef(0).ValueFor(fr);
+        auto sliceOutputValue =           ValueFor(fr); // row vector
+    }
+    
+        /** Returns a clone of action.
+     *
+     * @return A clone action.
+     */
+    virtual Action* clone() const
+    {
+        CC_ASSERT(0);
+        return nullptr;
+    }
+    
+    
+    {    return ret;
 }
     
-      // Drop reference count.  Delete if no more references exist.
-  void Unref() {
-    --refs_;
-    assert(refs_ >= 0);
-    if (refs_ <= 0) {
-      delete this;
+        // Overrides
+    void startWithTarget(Node *target) override;
+    void update(float dt) override;
+    ActionTween* reverse() const override;
+    ActionTween *clone() const override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    /** 
+     * @brief Initializes the action with the property name (key), and the from and to parameters.
+     * @param duration The duration of the ActionTween. It's a value in seconds.
+     * @param key The key of property which should be updated.
+     * @param from The value of the specified property when the action begin.
+     * @param to The value of the specified property when the action end.
+     * @return If the initialization success, return true; otherwise, return false.
+     */
+    bool initWithDuration(float duration, const std::string& key, float from, float to);
+    
+    /**
+ * @addtogroup _2d
+ * @{
+ */
     }
-  }
