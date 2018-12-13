00d84ebd56623aecbd95d23803871259176fe229
SUNsung <weatherius@gmail.com>
2018-12-13 13:50:04 +0200

    Rewrite [57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py] File in: 2018-12-13 13:50:04

--- pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
+++ pyton/57TvjXvPIjX5657N2FR7xbX41E57GvCnecFZmuw57gc3.py
@@ -1,81 +1,216 @@
 
         
-            For complete documentation on using Sessions in your code, consult
-    the sessions documentation that is shipped with Django (also available
-    on the Django Web site).
-    '''
-    objects = SessionManager()
+            self.path_embeddings = path_embeddings
+    self.relata_embeddings = relata_embeddings
+    self.nc_embeddings = nc_embeddings
     
-        def close_all(self):
-        for alias in self:
-            try:
-                connection = getattr(self._connections, alias)
-            except AttributeError:
-                continue
-            connection.close()
+      input_title = ''
+  if 'controller_outputs' in model_vals.keys():
+    input_title += ' Controller Output'
+    plt.subplot(nrows,2,3+subplot_cidx)
+    u_t = model_vals['controller_outputs'][0:-1]
+    plot_time_series(u_t, bidx, n_to_plot=n_to_plot, color='c', scale=1.0,
+                     title=col_title + input_title)
     
-    # Read lines from the linkcheck output file
-try:
-    with open('build/linkcheck/output.txt') as out:
-        output_lines = out.readlines()
-except IOError:
-    print('linkcheck output not found; please run linkcheck first.')
-    exit(1)
+    flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'thits_data',
+                    'Name of data file for input case.')
+flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 100, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
+flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 40,
+                     'Number of noise replications of the same underlying rates.')
+flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
+flags.DEFINE_float('x0_std', 1.0,
+                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('input_magnitude', 20.0,
+                   'For the input case, what is the value of the input?')
+flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
+FLAGS = flags.FLAGS
     
-    # Check minimum required Python version
-import sys
-if sys.version_info < (2, 7):
-    print('Scrapy %s requires Python 2.7' % __version__)
-    sys.exit(1)
-    
-        def print_items(self, lvl=None, colour=True):
-        if lvl is None:
-            items = [item for lst in self.items.values() for item in lst]
-        else:
-            items = self.items.get(lvl, [])
-    
-    
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'ApiGrant'
-        db.create_table(
-            'sentry_apigrant', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'user', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.User']
-                    )
-                ), (
-                    'application', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.ApiApplication']
-                    )
-                ), (
-                    'code',
-                    self.gf('django.db.models.fields.CharField')(max_length=64, db_index=True)
-                ), ('expires_at', self.gf('django.db.models.fields.DateTimeField')(db_index=True)),
-                ('redirect_uri', self.gf('django.db.models.fields.CharField')(max_length=255)),
-                ('scopes', self.gf('django.db.models.fields.BigIntegerField')(default=None)),
-            )
-        )
-        db.send_create_signal('sentry', ['ApiGrant'])
+    flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'conditioned_rnn_data',
+                    'Name of data file for input case.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 400, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 10,
+                     'Number of spikifications of the same underlying rates.')
+flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
+flags.DEFINE_float('x0_std', 1.0,
+                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
+FLAGS = flags.FLAGS
     
     
-class Migration(DataMigration):
-    def forwards(self, orm):
-        from sentry.utils.query import RangeQuerySetWrapperWithProgressBar
+def nparray_and_transpose(data_a_b_c):
+  '''Convert the list of items in data to a numpy array, and transpose it
+  Args:
+    data: data_asbsc: a nested, nested list of length a, with sublist length
+      b, with sublist length c.
+  Returns:
+    a numpy 3-tensor with dimensions a x c x b
+'''
+  data_axbxc = np.array([datum_b_c for datum_b_c in data_a_b_c])
+  data_axcxb = np.transpose(data_axbxc, axes=[0,2,1])
+  return data_axcxb
     
-        complete_apps = ['sentry']
+      samples = []
+  for sequence_id in xrange(min(len(arr), max_num_to_print)):
+    sample = []
+    for i, index in enumerate(arr[sequence_id, :]):
+      if p[sequence_id, i] == 1:
+        sample.append(str(id_to_word[index]))
+      else:
+        sample.append('*' + str(id_to_word[index]))
+    buffer_str = ' '.join(sample)
+    samples.append(buffer_str)
+  return samples
+    
+    
+def generator_GAN_loss(predictions):
+  '''Generator GAN loss based on Discriminator predictions.'''
+  return -tf.log(tf.reduce_mean(predictions))
+    
+      # The unique ngrams in the training set.
+  unique_ngrams_in_train = 0.
+    
+    import tensorflow as tf
+    
+            # reshape query: [batch_size, 1, num_units]
+        query = tf.reshape(query, [-1, 1, num_units])
+    
+    versions_info['versions'][version] = new_version
+versions_info['latest'] = version
+    
+    with io.open('update/releases.atom', 'w', encoding='utf-8') as atom_file:
+    atom_file.write(atom_template)
 
     
-            # Adding field 'ReleaseFile.dist'
-        db.add_column(
-            'sentry_releasefile',
-            'dist',
-            self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                to=orm['sentry.Distribution'], null=True
-            ),
-            keep_default=False
-        )
+    
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
+    
+        flags = [opt.get_opt_string() for opt in opts]
+    
+    # -- Options for HTML output ----------------------------------------------
+    
+        n_iter = 40
+    
+        if dataset_name == 'SF':
+        lb = LabelBinarizer()
+        x1 = lb.fit_transform(X[:, 1].astype(str))
+        X = np.c_[X[:, :1], x1, X[:, 2:]]
+        y = (y != b'normal.').astype(int)
+    
+        for n_components in [i.astype(int) for i in
+                         np.linspace(data.shape[1] // 10,
+                                     data.shape[1], num=4)]:
+        all_times = defaultdict(list)
+        all_errors = defaultdict(list)
+        pca = PCA(n_components=n_components)
+        rpca = PCA(n_components=n_components, svd_solver='randomized',
+                   random_state=1999)
+        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
+                                                               ('rpca', rpca)]}
+    
+        for i, NN in enumerate(Nrange):
+        print('N = %i (%i out of %i)' % (NN, i + 1, len(Nrange)))
+        X = get_data(NN, D, dataset)
+        for algorithm in algorithms:
+            nbrs = neighbors.NearestNeighbors(n_neighbors=min(NN, k),
+                                              algorithm=algorithm,
+                                              leaf_size=leaf_size)
+            t0 = time()
+            nbrs.fit(X)
+            t1 = time()
+            nbrs.kneighbors(X)
+            t2 = time()
+    
+            start = time.time()
+        func(X, n_jobs=1)
+        one_core.append(time.time() - start)
+    
+    n_samples = np.logspace(.5, 3, 9)
+n_features = np.logspace(1, 3.5, 7)
+N_samples, N_features = np.meshgrid(n_samples,
+                                    n_features)
+scikits_time = np.zeros(N_samples.shape)
+scipy_time = np.zeros(N_samples.shape)
+    
+        ###########################################################################
+    # Set SparseRandomProjection input
+    sparse_matrix_params = {
+        'n_components': opts.n_components,
+        'random_state': opts.random_seed,
+        'density': opts.density,
+        'eps': opts.eps,
+    }
+    
+        selected_algorithm = opts.selected_algorithm.split(',')
+    for key in selected_algorithm:
+        if key not in default_algorithms.split(','):
+            raise ValueError('Unknown sampling algorithm \'%s\' not in (%s).'
+                             % (key, default_algorithms))
+    
+        print('============================================')
+    print('Warning: this is going to take a looong time')
+    print('============================================')
+    
+        if path_details['type'] == 'symlink':
+        symlinks[name] = json_urlread(path_details['_links']['self'])['target']
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+    
+            idx = 0
+        while True:
+            url = urls[idx % len(urls)]
+            yield Request(url, dont_filter=True)
+            idx += 1
+    
+            parser.add_option_group(group)
+    
+        def run(self, args, opts):
+        if len(args) != 1:
+            raise UsageError()
+    
+    from scrapy.commands import ScrapyCommand
+from scrapy.settings import BaseSettings
+    
+    from scrapy.commands import ScrapyCommand
+from scrapy.shell import Shell
+from scrapy.http import Request
+from scrapy.utils.spider import spidercls_for_request, DefaultSpider
+from scrapy.utils.url import guess_scheme
+    
+        def __init__(self, settings):
+        self.default_user = settings['FTP_USER']
+        self.default_password = settings['FTP_PASSWORD']
+        self.passive_mode = settings['FTP_PASSIVE_MODE']
