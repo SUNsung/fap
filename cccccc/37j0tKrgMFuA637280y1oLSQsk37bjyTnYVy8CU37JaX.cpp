bf7a232ff8f1a2dd2b17c339526935611603fe11
SUNsung <weatherius@gmail.com>
2018-12-14 06:25:33 +0200

    Rewrite [37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp] File in: 2018-12-14 06:25:32

--- cccccc/37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp
+++ cccccc/37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp
@@ -1,256 +1,149 @@
 
         
-        #else  // !defined(__AVX__)
-// Implementation for avx capable archs.
-#include <immintrin.h>
-#include <cstdint>
-#include 'dotproductavx.h'
+        #define INSTANTIATE_LAYER_GPU_FUNCS(classname) \
+  INSTANTIATE_LAYER_GPU_FORWARD(classname); \
+  INSTANTIATE_LAYER_GPU_BACKWARD(classname)
+    
+      /**
+   * @brief Applies the transformation defined in the data layer's
+   * transform_param block to the data.
+   *
+   * @param datum
+   *    Datum containing the data to be transformed.
+   * @param transformed_blob
+   *    This is destination blob. It can be part of top blob's data if
+   *    set_cpu_data() is used. See data_layer.cpp for an example.
+   */
+  void Transform(const Datum& datum, Blob<Dtype>* transformed_blob);
     
     
-    {  PARA *para = it_->row()->row->para();
-  *is_list_item = para->is_list_item;
-  *is_crown = para->is_very_first_or_continuation;
-  *first_line_indent = para->model->first_indent() -
-      para->model->body_indent();
-  *just = para->model->justification();
-}
-    
-    
-    {  // Do sanity checks and minor fixes on best_choice.
-  if (word->best_choice->length() > word_length) {
-    word->best_choice->make_bad();  // should never happen
-    tprintf('recog_word: Discarded long string \'%s\''
-            ' (%d characters vs %d blobs)\n',
-            word->best_choice->unichar_string().string(),
-            word->best_choice->length(), word_length);
-    tprintf('Word is at:');
-    word->word->bounding_box().print();
-  }
-  if (word->best_choice->length() < word_length) {
-    UNICHAR_ID space_id = unicharset.unichar_to_id(' ');
-    while (word->best_choice->length() < word_length) {
-      word->best_choice->append_unichar_id(space_id, 1, 0.0,
-                                           word->best_choice->certainty());
+    { private:
+  struct pair_sort_first {
+    bool operator()(const std::pair<int, int> &left,
+                    const std::pair<int, int> &right) {
+      return left.first < right.first;
     }
-  }
-}
-    
-    
-    {  name += UNLV_EXT;              //add extension
-  if ((pdfp = fopen (name.string (), 'rb')) == nullptr) {
-    return false;                //didn't read one
-  } else {
-    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
-                                 //make rect block
-      block = new BLOCK (name.string (), TRUE, 0, 0,
-                         (int16_t) x, (int16_t) (ysize - y - height),
-                         (int16_t) (x + width), (int16_t) (ysize - y));
-                                 //on end of list
-      block_it.add_to_end (block);
-    }
-    fclose(pdfp);
-  }
-  return true;
-}
-    
-      // This and other putatively are the same, so call the (permanent) callback
-  // for each blob index where the bounding boxes match.
-  // The callback is deleted on completion.
-  void ProcessMatchedBlobs(const TWERD& other, TessCallback1<int>* cb) const;
-    
-    
-    {  ASSERT_OK(Put('foo', 'v1'));
-  ASSERT_EQ('v1', Get('foo'));
-  Compact('a', 'z');
-  const int num_files = CountFiles();
-  env_->no_space_.Release_Store(env_);   // Force out-of-space errors
-  for (int i = 0; i < 10; i++) {
-    for (int level = 0; level < config::kNumLevels-1; level++) {
-      dbfull()->TEST_CompactRange(level, nullptr, nullptr);
-    }
-  }
-  env_->no_space_.Release_Store(nullptr);
-  ASSERT_LT(CountFiles(), num_files + 3);
-}
-    
-    #include 'db/log_reader.h'
-    
-    
-    {  // Write the header and the payload
-  Status s = dest_->Append(Slice(buf, kHeaderSize));
-  if (s.ok()) {
-    s = dest_->Append(Slice(ptr, n));
-    if (s.ok()) {
-      s = dest_->Flush();
-    }
-  }
-  block_offset_ += kHeaderSize + n;
-  return s;
-}
-    
-    
-    {
-    {  Status update(const std::string& source, const ParserConfig& config) override;
+  };
+  void check_batch_reindex(int initial_num, int final_num,
+                           const Dtype* ridx_data);
 };
-} // namespace osquery
+    
+      virtual inline const char* type() const { return 'Concat'; }
+  virtual inline int MinBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+    
+    #include <vector>
+    
+    
+    {  size_t *workspace_fwd_sizes_;
+  size_t *workspace_bwd_data_sizes_;
+  size_t *workspace_bwd_filter_sizes_;
+  size_t workspaceSizeInBytes;  // size of underlying storage
+  void *workspaceData;  // underlying storage
+  void **workspace;  // aliases into workspaceData
+};
+#endif
+    
+    #endif  // CAFFE_CUDNN_LCN_LAYER_HPP_
 
     
-    namespace osquery {
+    
+    {    printf('DestroyContext()\n');
+    ImGui::DestroyContext();
+    return 0;
+}
+
+    
+    
+    {    InputTextCallback_UserData cb_user_data;
+    cb_user_data.Str = str;
+    cb_user_data.ChainCallback = callback;
+    cb_user_data.ChainCallbackUserData = user_data;
+    return InputTextMultiline(label, (char*)str->c_str(), str->capacity() + 1, size, flags, InputTextCallback, &cb_user_data);
+}
+
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    
+    {        // Create the input layout
+        static D3D12_INPUT_ELEMENT_DESC local_layout[] = {
+            { 'POSITION', 0, DXGI_FORMAT_R32G32_FLOAT,   0, (size_t)(&((ImDrawVert*)0)->pos), D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+            { 'TEXCOORD', 0, DXGI_FORMAT_R32G32_FLOAT,   0, (size_t)(&((ImDrawVert*)0)->uv),  D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+            { 'COLOR',    0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, (size_t)(&((ImDrawVert*)0)->col), D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
+        };
+        psoDesc.InputLayout = { local_layout, 3 };
     }
     
-      Status update(const std::string& source, const ParserConfig& config) override;
+            if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text('counter = %d', counter);
     
-    TEST_F(OptionsConfigParserPluginTests, test_get_option) {
-  Config c;
-  auto s = c.update(getTestConfigMap());
-  EXPECT_TRUE(s.ok());
-  EXPECT_EQ(s.toString(), 'OK');
+            ImGui_ImplVulkan_CreateFontsTexture(command_buffer);
+    
+        {
+        D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+        desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+        desc.NumDescriptors = 1;
+        desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+        if (g_pd3dDevice->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&g_pd3dSrvDescHeap)) != S_OK)
+            return E_FAIL;
     }
     
+    namespace rocksdb {
+    }
     
-    {  expected = {'select pid from processes where name = 'foobar';'};
-  Pack fpack('discovery_pack', getPackWithDiscovery().doc());
-  EXPECT_EQ(fpack.getDiscoveryQueries(), expected);
+    #include 'db/compaction.h'
+#include 'db/compaction_iteration_stats.h'
+#include 'db/merge_helper.h'
+#include 'db/pinned_iterators_manager.h'
+#include 'db/range_del_aggregator.h'
+#include 'db/snapshot_checker.h'
+#include 'options/cf_options.h'
+#include 'rocksdb/compaction_filter.h'
+    
+    Status WriteBatchBase::DeleteRange(ColumnFamilyHandle* column_family,
+                                   const SliceParts& begin_key,
+                                   const SliceParts& end_key) {
+  std::string begin_key_buf, end_key_buf;
+  Slice begin_key_slice(begin_key, &begin_key_buf);
+  Slice end_key_slice(end_key, &end_key_buf);
+  return DeleteRange(column_family, begin_key_slice, end_key_slice);
 }
     
-    
-    {  val = getEnvVar('GTEST_OSQUERY');
-  EXPECT_FALSE(val);
-  EXPECT_FALSE(val.is_initialized());
-}
-    
-        // Add the 'current' results and output the differentials.
-    DiffResults dr;
-    counter = 128;
-    auto s = cf.addNewResults(result.second, 0, counter, dr, true);
-    EXPECT_TRUE(s.ok());
-    EXPECT_EQ(counter, expected_counter++);
-    
-    /**
- * @brief The watchdog thread responsible for spawning/monitoring children.
- *
- * The WatcherRunner thread will spawn any autoloaded extensions or optional
- * osquery daemon worker processes. It will then poll for their performance
- * state and kill/respawn osquery child processes if they violate limits.
- */
-class WatcherRunner : public InternalRunnable {
- public:
-  /**
-   * @brief Construct a watcher thread.
-   *
-   * @param argc The osquery process argc.
-   * @param argv The osquery process argv.
-   * @param use_worker True if the process should spawn and monitor a worker.
-   */
-  explicit WatcherRunner(int argc, char** argv, bool use_worker)
-      : InternalRunnable('WatcherRunner'),
-        argc_(argc),
-        argv_(argv),
-        use_worker_(use_worker) {
-    (void)argc_;
-  }
+      static void CompactFiles(void* arg) {
+    std::unique_ptr<CompactionTask> task(
+        reinterpret_cast<CompactionTask*>(arg));
+    assert(task);
+    assert(task->db);
+    Status s = task->db->CompactFiles(
+        task->compact_options,
+        task->input_file_names,
+        task->output_level);
+    printf('CompactFiles() finished with status %s\n', s.ToString().c_str());
+    if (!s.ok() && !s.IsIOError() && task->retry_on_fail) {
+      // If a compaction task with its retry_on_fail=true failed,
+      // try to schedule another compaction in case the reason
+      // is not an IO error.
+      CompactionTask* new_task = task->compactor->PickCompaction(
+          task->db, task->column_family_name);
+      task->compactor->ScheduleCompaction(new_task);
     }
-    
-    using StringMap = std::map<std::string, std::string>;
-    
-      QueryData generate(QueryContext& request) {
-    QueryData results;
-    }
-    
-    
-    { protected:
-  /// The CAN client is started.
-  bool is_started_ = false;
-};
-    
-      // 2. set baudrate to 500k
-  ret = canSetBaudrate(dev_handler_, NTCAN_BAUD_500);
-  if (ret != NTCAN_SUCCESS) {
-    AERROR << 'set baudrate error code: ' << ret << ', ' << GetErrorString(ret);
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
   }
     
-      /**
-   * @brief Get the error string.
-   * @param status The status to get the error string.
-   */
-  std::string GetErrorString(const int32_t status) override;
+      // Set a new snapshot in the transaction
+  txn->SetSnapshot();
+  txn->SetSavePoint();
+  read_options.snapshot = txn_db->GetSnapshot();
     
-    #include 'gtest/gtest.h'
-    
-    using apollo::common::ErrorCode;
-    
-    /**
- * @namespace apollo::drivers::canbus::can
- * @brief apollo::drivers::canbus::can
- */
-namespace apollo {
-namespace drivers {
-namespace canbus {
-namespace can {
-    }
-    }
-    }
-    }
-    
-    /**
- * @class HermesCanClient
- * @brief The class which defines a BCAN client which inherits CanClient.
- */
-    
-    #include 'gtest/gtest.h'
-    
-    template <typename SensorType>
-void MessageManager<SensorType>::Parse(const uint32_t message_id,
-                                       const uint8_t *data, int32_t length) {
-  ProtocolData<SensorType> *protocol_data =
-      GetMutableProtocolDataById(message_id);
-  if (protocol_data == nullptr) {
-    return;
-  }
-  {
-    std::lock_guard<std::mutex> lock(sensor_data_mutex_);
-    protocol_data->Parse(data, length, &sensor_data_);
-  }
-  received_ids_.insert(message_id);
-  // check if need to check period
-  const auto it = check_ids_.find(message_id);
-  if (it != check_ids_.end()) {
-    const int64_t time = apollo::common::time::AsInt64<micros>(Clock::Now());
-    it->second.real_period = time - it->second.last_time;
-    // if period 1.5 large than base period, inc error_count
-    const double period_multiplier = 1.5;
-    if (it->second.real_period > (it->second.period * period_multiplier)) {
-      it->second.error_count += 1;
-    } else {
-      it->second.error_count = 0;
-    }
-    it->second.last_time = time;
-  }
-}
-    
-      /**
-   * @brief destruct protocol data.
-   */
-  virtual ~ProtocolData() = default;
-    
-    void Byte::set_value(const uint8_t value, const int32_t start_pos,
-                     const int32_t length) {
-  if (start_pos > BYTE_LENGTH - 1 || start_pos < 0 || length < 1) {
-    return;
-  }
-  int32_t end_pos = std::min(start_pos + length - 1, BYTE_LENGTH - 1);
-  int32_t real_len = end_pos + 1 - start_pos;
-  uint8_t current_value_low = 0x00;
-  if (start_pos > 0) {
-    current_value_low = *value_ & RANG_MASK_1_L[start_pos - 1];
-  }
-  uint8_t current_value_high = *value_ & RANG_MASK_0_L[end_pos];
-  uint8_t middle_value = value & RANG_MASK_1_L[real_len - 1];
-  middle_value = middle_value << start_pos;
-  *value_ = current_value_high + middle_value + current_value_low;
-}
-    
-    // System gflags
-DECLARE_string(node_name);
-DECLARE_string(canbus_driver_name);
+    // Move all L0 files to target_level skipping compaction.
+// This operation succeeds only if the files in L0 have disjoint ranges; this
+// is guaranteed to happen, for instance, if keys are inserted in sorted
+// order. Furthermore, all levels between 1 and target_level must be empty.
+// If any of the above condition is violated, InvalidArgument will be
+// returned.
+Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
+                 int target_level = 1);
