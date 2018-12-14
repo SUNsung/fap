7c4f5fe399ffe86fd1e36f841b3da75a159f946d
SUNsung <weatherius@gmail.com>
2018-12-14 13:52:59 +0200

    Rewrite [10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb] File in: 2018-12-14 13:52:59

--- ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
+++ ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
@@ -1,55 +1,59 @@
 
         
-                css('.status-badge').each do |node|
-          node.name = 'code'
-          node.content = node.content.strip
-          node.remove_attribute('class')
-        end
+              expect(value_for(user.id, dt)).to eq(0)
+      expect(limit_reached_for(user.id, dt)).to eq(false)
     
-      protected
+            return a.casecmp(b) if a_length == 1 && b_length == 1
+        return 1 if a_length == 1
+        return -1 if b_length == 1
     
-      def devise_i18n_options(options)
-    options
+        def initialize(name = nil, path = nil, type = nil)
+      self.name = name
+      self.path = path
+      self.type = type
+    
+        def as_json
+      @pages
+    end
+    
+          @terminal_width = if !tty?
+        nil
+      elsif ENV['COLUMNS']
+        ENV['COLUMNS'].to_i
+      else
+        `stty size`.scan(/\d+/).last.to_i
+      end
+    rescue
+      @terminal_width = nil
+    end
+  end
+end
+
+    
+          if @account_moderation_note.save
+        redirect_to admin_account_path(@account_moderation_note.target_account_id), notice: I18n.t('admin.account_moderation_notes.created_msg')
+      else
+        @account          = @account_moderation_note.target_account
+        @moderation_notes = @account.targeted_moderation_notes.latest
+    
+    module Admin
+  class ChangeEmailsController < BaseController
+    before_action :set_account
+    before_action :require_local_account!
+    
+          @user.resend_confirmation_instructions
+    
+        def new
+      authorize :email_domain_block, :create?
+      @email_domain_block = EmailDomainBlock.new
+    end
+    
+      def payload
+    @_payload ||= request.body.read
   end
     
-    require 'uri'
+      private
     
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
-        end
-      end
-    
-        alias :name :singular
-    
-        def handle_options
-      options.rakelib = ['rakelib']
-      options.trace_output = $stderr
-    
-    module Capistrano
-  class Configuration
-    # Decorates a Variables object to additionally perform an optional set of
-    # user-supplied validation rules. Each rule for a given key is invoked
-    # immediately whenever `set` is called with a value for that key.
-    #
-    # If `set` is called with a callable value or a block, validation is not
-    # performed immediately. Instead, the validation rules are invoked the first
-    # time `fetch` is used to access the value.
-    #
-    # A rule is simply a block that accepts two arguments: key and value. It is
-    # up to the rule to raise an exception when it deems the value is invalid
-    # (or just print a warning).
-    #
-    # Rules can be registered using the DSL like this:
-    #
-    #   validate(:my_key) do |key, value|
-    #     # rule goes here
-    #   end
-    #
-    class ValidatedVariables < SimpleDelegator
-      include Capistrano::ProcHelpers
+            self.arguments = [
+          CLAide::Argument.new('NAME', false),
+        ]
