0540bda6f39396b97f570cbc699ebd20ef058a08
SUNsung <weatherius@gmail.com>
2018-12-14 16:40:12 +0200

    Rewrite [28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp] File in: 2018-12-14 16:40:12

--- cccccc/28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp
+++ cccccc/28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp
@@ -1,104 +1,160 @@
 
         
-        // hidden_ops should be a list of Op names that should get a leading _
-// in the output. Prints the output to stdout.
-// Optional fourth argument is the name of the original C++ source file
-// where the ops' REGISTER_OP() calls reside.
-void PrintPythonOps(const OpList& ops, const ApiDefMap& api_defs,
-                    const std::vector<string>& hidden_ops, bool require_shapes,
-                    const string& source_file_name = '');
-    
-      // Format the Op's descriptions so that it can be a Python docstring.
-  void AddDocStringDescription();
-    
-    void CostAnalyzer::PreprocessCosts() {
-  for (int i = 0; i < op_perf_.op_performance_size(); i++) {
-    OpPerformance* perf = op_perf_.mutable_op_performance(i);
-    const OpPerformance& analytical = op_perf_analytical_.op_performance(i);
-    perf->set_compute_time(analytical.compute_time());
-    perf->set_memory_time(analytical.memory_time());
-    double measured_cost = perf->compute_cost();
-    }
-    }
-    
-    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
-// a copy.
-Status TensorToNdarray(const Tensor& t, PyObject** ret);
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    #include <Python.h>
-    
-    #ifndef TENSORFLOW_PYTHON_LIB_CORE_PY_SEQ_TENSOR_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_PY_SEQ_TENSOR_H_
-    
-    namespace cuda {
-    }
-    
-      // Returns true if all stream tasks have completed at time of the call. Note
-  // the potential for races around this call (if another thread adds work to
-  // the stream immediately after this returns).
-  static bool IsStreamIdle(CudaContext* context, CUstream stream);
-    
-    bool GodotCollisionDispatcher::needsResponse(const btCollisionObject *body0, const btCollisionObject *body1) {
-	if (body0->getUserIndex() == CASTED_TYPE_AREA || body1->getUserIndex() == CASTED_TYPE_AREA) {
-		// Avoide area narrow phase
-		return false;
-	}
-	return btCollisionDispatcher::needsResponse(body0, body1);
+        void AutoUpdater::OnWindowAllClosed() {
+  QuitAndInstall();
 }
-
     
-    	virtual void processCollision(const btCollisionObjectWrapper *body0Wrap, const btCollisionObjectWrapper *body1Wrap, const btDispatcherInfo &dispatchInfo, btManifoldResult *resultOut);
-	virtual btScalar calculateTimeOfImpact(btCollisionObject *body0, btCollisionObject *body1, const btDispatcherInfo &dispatchInfo, btManifoldResult *resultOut);
+    #include 'atom/browser/api/atom_api_browser_window.h'
     
-    class HingeJointBullet : public JointBullet {
-	class btHingeConstraint *hingeConstraint;
+    Net::~Net() {}
+    
+      // The window class of |window_|.
+  ATOM atom_;
+    
+    // A self-destroyed class for handling save page request.
+class SavePageHandler : public content::DownloadManager::Observer,
+                        public download::DownloadItem::Observer {
+ public:
+  using SavePageCallback = base::Callback<void(v8::Local<v8::Value>)>;
     }
     
-    #include 'constraint_bullet.h'
-#include 'servers/physics_server.h'
     
-    #include 'core/rid.h'
+    {            vst1q_s16(dst + j, v_dst0);
+            vst1q_s16(dst + j + 8, v_dst1);
+        }
+        for (; j < roiw8; j += 8)
+        {
+            int16x8_t v_src = vreinterpretq_s16_u16(vmovl_u8(vld1_u8(src + j)));
+            int16x8_t v_dst = vld1q_s16(dst + j);
+            int16x4_t v_srclo = vget_low_s16(v_src), v_srchi = vget_high_s16(v_src);
+            v_dst = vcombine_s16(vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srclo, v_srclo), shift), vget_low_s16(v_dst))),
+                                 vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srchi, v_srchi), shift), vget_high_s16(v_dst))));
+            vst1q_s16(dst + j, v_dst);
+        }
     
-    /**
-	@author AndreaCatania
-*/
+    void bitwiseAnd(const Size2D &size,
+                const u8 *src0Base, ptrdiff_t src0Stride,
+                const u8 *src1Base, ptrdiff_t src1Stride,
+                u8 *dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride, BitwiseAnd());
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+#endif
+}
     
-      if (ret != ErrorCode::OK) {
-    AERROR << 'Open device error code: ' << ret
-           << ', channel id: ' << _card_port;
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
-  }
-  AERROR << 'Open device succ code: ' << ret << ', channel id: ' << _card_port;
+    #define MERGE_ASM2(sgn, bits) __asm__ ( \
+                                          'vld1.' #bits ' {d0-d1}, [%[in0]]             \n\t' \
+                                          'vld1.' #bits ' {d2-d3}, [%[in1]]             \n\t' \
+                                          'vst2.' #bits ' {d0, d2}, [%[out0]]           \n\t' \
+                                          'vst2.' #bits ' {d1, d3}, [%[out1]]           \n\t' \
+                                          : \
+                                          : [in0] 'r' (src0 + sj), [in1] 'r' (src1 + sj), \
+                                            [out0]  'r' (dst + dj), [out1]  'r' (dst + dj + MUL2(8)/sizeof(sgn##bits)) \
+                                          : 'd0','d1','d2','d3' \
+                                      );
+#define MERGE_ASM3(sgn, bits) __asm__ ( \
+                                          'vld1.' #bits ' {d0-d1}, [%[in0]]             \n\t' \
+                                          'vld1.' #bits ' {d2-d3}, [%[in1]]             \n\t' \
+                                          'vld1.' #bits ' {d4-d5}, [%[in2]]             \n\t' \
+                                          'vst3.' #bits ' {d0, d2, d4}, [%[out0]]       \n\t' \
+                                          'vst3.' #bits ' {d1, d3, d5}, [%[out1]]       \n\t' \
+                                          : \
+                                          : [in0] 'r' (src0 + sj), [in1] 'r' (src1 + sj), [in2] 'r' (src2 + sj), \
+                                            [out0]  'r' (dst + dj), [out1]  'r' (dst + dj + MUL3(8)/sizeof(sgn##bits)) \
+                                          : 'd0','d1','d2','d3','d4','d5' \
+                                      );
+#define MERGE_ASM4(sgn, bits) __asm__ ( \
+                                          'vld1.' #bits ' {d0-d1}, [%[in0]]             \n\t' \
+                                          'vld1.' #bits ' {d2-d3}, [%[in1]]             \n\t' \
+                                          'vld1.' #bits ' {d4-d5}, [%[in2]]             \n\t' \
+                                          'vld1.' #bits ' {d6-d7}, [%[in3]]             \n\t' \
+                                          'vst4.' #bits ' {d0, d2, d4, d6}, [%[out0]]   \n\t' \
+                                          'vst4.' #bits ' {d1, d3, d5, d7}, [%[out1]]   \n\t' \
+                                          : \
+                                          : [in0] 'r' (src0 + sj), [in1] 'r' (src1 + sj), [in2] 'r' (src2 + sj), [in3] 'r' (src3 + sj), \
+                                            [out0]  'r' (dst + dj), [out1]  'r' (dst + dj + MUL4(8)/sizeof(sgn##bits)) \
+                                          : 'd0','d1','d2','d3','d4','d5','d6','d7' \
+                                      );
     
-      /**
-   * @brief get mutable protocol data by message id
-   * @param message_id the id of the message
-   * @return a pointer to the protocol data
-   */
-  ProtocolData<SensorType> *GetMutableProtocolDataById(
-      const uint32_t message_id);
+    #endif
     
-    TEST(ByteTest, SetBit) {
-  unsigned char byte_value = 0xFF;
-  Byte value(&byte_value);
-  value.set_bit_0(1);
-  EXPECT_EQ(0xFD, value.get_byte());
-  value.set_bit_0(7);
-  EXPECT_EQ(0x7D, value.get_byte());
-  value.set_bit_1(7);
-  EXPECT_EQ(0xFD, value.get_byte());
-  value.set_value(0x77);
-  value.set_bit_1(0);
-  EXPECT_EQ(0x77, value.get_byte());
+    namespace internal {
     }
+    
+                int32x2_t vnz1 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx1), vget_high_u32(vlx1)));
+            int32x2_t vnz2 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx2), vget_high_u32(vlx2)));
+            int32x2_t vnz3 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx3), vget_high_u32(vlx3)));
+            int32x2_t vnz4 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx4), vget_high_u32(vlx4)));
     
     
     {
     {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
+    {                    for( k = 0; k < N; k++ )
+                    {
+                        s32 x = ptr[pixel[k]];
+                        if(x > vt)
+                        {
+                            if( ++count > K )
+                            {
+                                cornerpos[ncorners++] = j;
+                                if(nonmax_suppression)
+                                    curr[j] = cornerScore(ptr, pixel);
+                                break;
+                            }
+                        }
+                        else
+                            count = 0;
+                    }
+                }
+            }
+        }
+    
+                if (mask[0])
+                process(src, j, j + 8, i,
+                        minVal, minLocPtr, minLocCount, minLocCapacity,
+                        maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
+            if (mask[1])
+                process(src, j + 8, j + 16, i,
+                        minVal, minLocPtr, minLocCount, minLocCapacity,
+                        maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
+        }
+        for ( ; j < roiw8; j += 8)
+        {
+            internal::prefetch(src + j);
+            int16x8_t v_src = vld1q_s16(src + j);
+    
+    void flip(const Size2D &size,
+          const u8 * srcBase, ptrdiff_t srcStride,
+          u8 * dstBase, ptrdiff_t dstStride,
+          FLIP_MODE flipMode, u32 elemSize)
+{
+    internal::assertSupportedConfiguration(isFlipSupported(flipMode, elemSize));
+#ifdef CAROTENE_NEON
+    }
+    
+        if (borderType == BORDER_MODE_CONSTANT)
+        for (s32 k = 0; k < cn; ++k)
+        {
+            lane[-cn+k] = borderValue;
+            lane[-cn-cn+k] = borderValue;
+            lane[colsn+k] = borderValue;
+            lane[colsn+cn+k] = borderValue;
+        }
+    
+    
+    {	RegCloseKey(command);
+	RegCloseKey(cmderKey);
+	RegCloseKey(root);
+}
