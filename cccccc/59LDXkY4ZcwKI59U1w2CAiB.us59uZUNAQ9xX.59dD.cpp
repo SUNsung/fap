2049b611b880e5e005453f83897e210d91096a53
SUNsung <weatherius@gmail.com>
2018-12-15 22:16:35 +0200

    Rewrite [59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp] File in: 2018-12-15 22:16:35

--- cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
+++ cccccc/59LDXkY4ZcwKI59U1w2CAiB.us59uZUNAQ9xX.59dD.cpp
@@ -1,256 +1,192 @@
 
         
-        namespace CNTK
-{
-    class CompositeMinibatchSource final : public MinibatchSource
-    {
-        static const std::wstring PositionAttributeName;
-        static const std::wstring DistributedAfterSampleCountAttributeName;
-    }
-    }
-    
-    
-    {
-    {    private:
-        static bool IsNativeUDF(const Dictionary& dict);
-    };
+        #ifndef NDEBUG
+/// Verify that the types of fields are valid within a given generic signature.
+static void verifyFields(CanGenericSignature Sig, ArrayRef<SILField> Fields) {
+  for (auto &field : Fields) {
+    auto ty = field.getLoweredType();
+    // Layouts should never refer to archetypes, since they represent an
+    // abstract generic type layout.
+    assert(!ty->hasArchetype()
+           && 'SILLayout field cannot have an archetype type');
+    assert(!ty->hasTypeVariable()
+           && 'SILLayout cannot contain constraint system type variables');
+    if (!ty->hasTypeParameter())
+      continue;
+    field.getLoweredType().findIf([Sig](Type t) -> bool {
+      if (auto gpt = t->getAs<GenericTypeParamType>()) {
+        // Check that the generic param exists in the generic signature.
+        assert(Sig && 'generic param in nongeneric layout?');
+        assert(std::find(Sig.getGenericParams().begin(),
+                         Sig.getGenericParams().end(),
+                         gpt->getCanonicalType()) != Sig.getGenericParams().end()
+               && 'generic param not declared in generic signature?!');
+      }
+      return false;
+    });
+  }
 }
-
+#endif
     
-                if (m_varKind == VariableKind::Input)
-            {
-                for (auto dim : m_shape.Dimensions())
-                {
-                    if (dim == 0)
-                        InvalidArgument('Variable '%S' has invalid shape '%S'.', AsString().c_str(), m_shape.AsString().c_str());
-                }
+    
+    {  /// Profile the substitution map storage, for use with LLVM's FoldingSet.
+  static void Profile(llvm::FoldingSetNodeID &id,
+                      GenericSignature *genericSig,
+                      ArrayRef<Type> replacementTypes,
+                      ArrayRef<ProtocolConformanceRef> conformances);
+};
+    
+    bool CacheImpl::remove(const void *Key) {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
+    }
+    
+    DIRECTIONAL_PREPOSITION(above)
+DIRECTIONAL_PREPOSITION(after)
+DIRECTIONAL_PREPOSITION(along)
+DIRECTIONAL_PREPOSITION(alongside)
+DIRECTIONAL_PREPOSITION(as)
+DIRECTIONAL_PREPOSITION(at)
+DIRECTIONAL_PREPOSITION(before)
+DIRECTIONAL_PREPOSITION(below)
+DIRECTIONAL_PREPOSITION(by)
+DIRECTIONAL_PREPOSITION(following)
+DIRECTIONAL_PREPOSITION(for)
+DIRECTIONAL_PREPOSITION(from)
+DIRECTIONAL_PREPOSITION(given)
+DIRECTIONAL_PREPOSITION(in)
+DIRECTIONAL_PREPOSITION(including)
+DIRECTIONAL_PREPOSITION(inside)
+DIRECTIONAL_PREPOSITION(into)
+DIRECTIONAL_PREPOSITION(matching)
+DIRECTIONAL_PREPOSITION(of)
+DIRECTIONAL_PREPOSITION(on)
+DIRECTIONAL_PREPOSITION(passing)
+DIRECTIONAL_PREPOSITION(preceding)
+DIRECTIONAL_PREPOSITION(since)
+DIRECTIONAL_PREPOSITION(to)
+DIRECTIONAL_PREPOSITION(until)
+DIRECTIONAL_PREPOSITION(using)
+DIRECTIONAL_PREPOSITION(via)
+DIRECTIONAL_PREPOSITION(when)
+PREPOSITION(with)
+DIRECTIONAL_PREPOSITION(within)
+    
+      // Scan forward until we find the word as a complete word.
+  size_t startingIndex = 0;
+  while (true) {
+    size_t index = string.find(word, startingIndex);
+    if (index == StringRef::npos)
+      return StringRef::npos;
+    }
+    
+    break_table = GraphemeClusterBreakPropertyTable(unicodeGraphemeBreakPropertyFile)
+    
+      bool isGetter() const {
+    return accessorKind == IAMAccessorKind::Getter;
+  }
+    
+    void add(const Size2D &size,
+         const u8 * src0Base, ptrdiff_t src0Stride,
+         const u8 * src1Base, ptrdiff_t src1Stride,
+         u8 *dstBase, ptrdiff_t dstStride,
+         CONVERT_POLICY policy)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    if (policy == CONVERT_POLICY_SATURATE)
+    {
+        internal::vtransform(size,
+                             src0Base, src0Stride,
+                             src1Base, src1Stride,
+                             dstBase, dstStride,
+                             AddSaturate<u8, u16>());
+    }
+    else
+    {
+        internal::vtransform(size,
+                             src0Base, src0Stride,
+                             src1Base, src1Stride,
+                             dstBase, dstStride,
+                             AddWrap<u8, u16>());
+    }
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)policy;
+#endif
+}
+    
+    
+    {                currx = (srow2 ? srow2[x3] : borderValue) + srow1[x3] + (srow0 ? srow0[x3] : borderValue);
             }
+    
+    #define CVT_FUNC(T1, T2, SIMD_SIZE, CVTINIT, CVTROW)                            \
+    void convert(const Size2D &,                                                \
+                 const T1 *, ptrdiff_t,                                         \
+                 T2 *, ptrdiff_t)                                               \
+    {                                                                           \
+        internal::assertSupportedConfiguration();                               \
+    }
+    
+                for( ; i <= lim; i += 4 )
+            {
+                internal::prefetch(src0 + i);
+                internal::prefetch(src1 + i);
+                v_sum = vmlaq_f32(v_sum, vld1q_f32(src0 + i), vld1q_f32(src1 + i));
+            }
+    
+    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
+inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
+inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
+    
+    TEST(StaticTracepoint, TestArray) {
+  arrayTestFunc();
+    }
+    
+    namespace folly {
+    }
     
     #include <string>
     
-    // Fills in block[kBlockEdgeHalf..(kBlockHalf+kBlockEdgeHalf)], and leaves the
-// rest unmodified.
-void ButteraugliFFTSquared(double block[kBlockSize]) {
-  double global_mul = 0.000064;
-  Complex block_c[kBlockSize];
-  assert(kBlockEdge == 8);
-  for (int y = 0; y < kBlockEdge; ++y) {
-    RealFFT8(block + y * kBlockEdge, block_c + y * kBlockEdge);
-  }
-  TransposeBlock(block_c);
-  double r0[kBlockEdge];
-  double r1[kBlockEdge];
-  for (int x = 0; x < kBlockEdge; ++x) {
-    r0[x] = block_c[x].real;
-    r1[x] = block_c[kBlockHalf + x].real;
-  }
-  RealFFT8(r0, block_c);
-  RealFFT8(r1, block_c + kBlockHalf);
-  for (int y = 1; y < kBlockEdgeHalf; ++y) {
-    FFT8(block_c + y * kBlockEdge);
-  }
-  for (int i = kBlockEdgeHalf; i < kBlockHalf + kBlockEdgeHalf + 1; ++i) {
-    block[i] = abssq(block_c[i]);
-    block[i] *= global_mul;
-  }
-}
-    
-    static const int kCbToBlueTable[256] = {
-  -227, -225, -223, -222, -220, -218, -216, -214, -213, -211, -209, -207,
-  -206, -204, -202, -200, -198, -197, -195, -193, -191, -190, -188, -186,
-  -184, -183, -181, -179, -177, -175, -174, -172, -170, -168, -167, -165,
-  -163, -161, -159, -158, -156, -154, -152, -151, -149, -147, -145, -144,
-  -142, -140, -138, -136, -135, -133, -131, -129, -128, -126, -124, -122,
-  -120, -119, -117, -115, -113, -112, -110, -108, -106, -105, -103, -101,
-   -99,  -97,  -96,  -94,  -92,  -90,  -89,  -87,  -85,  -83,  -82,  -80,
-   -78,  -76,  -74,  -73,  -71,  -69,  -67,  -66,  -64,  -62,  -60,  -58,
-   -57,  -55,  -53,  -51,  -50,  -48,  -46,  -44,  -43,  -41,  -39,  -37,
-   -35,  -34,  -32,  -30,  -28,  -27,  -25,  -23,  -21,  -19,  -18,  -16,
-   -14,  -12,  -11,   -9,   -7,   -5,   -4,   -2,    0,    2,    4,    5,
-     7,    9,   11,   12,   14,   16,   18,   19,   21,   23,   25,   27,
-    28,   30,   32,   34,   35,   37,   39,   41,   43,   44,   46,   48,
-    50,   51,   53,   55,   57,   58,   60,   62,   64,   66,   67,   69,
-    71,   73,   74,   76,   78,   80,   82,   83,   85,   87,   89,   90,
-    92,   94,   96,   97,   99,  101,  103,  105,  106,  108,  110,  112,
-   113,  115,  117,  119,  120,  122,  124,  126,  128,  129,  131,  133,
-   135,  136,  138,  140,  142,  144,  145,  147,  149,  151,  152,  154,
-   156,  158,  159,  161,  163,  165,  167,  168,  170,  172,  174,  175,
-   177,  179,  181,  183,  184,  186,  188,  190,  191,  193,  195,  197,
-   198,  200,  202,  204,  206,  207,  209,  211,  213,  214,  216,  218,
-   220,  222,  223,  225,
-};
-    
-    #endif  // GUETZLI_ENTROPY_ENCODE_H_
-
-    
-    
-    {}  // namespace guetzli
-    
-    // Computes the DCT (Discrete Cosine Transform) of the 8x8 array in 'block',
-// scaled up by a factor of 16. The values in 'block' are laid out row-by-row
-// and the result is written to the same memory area.
-void ComputeBlockDCT(coeff_t* block);
-    
-      std::istringstream memstream(data, std::ios::in | std::ios::binary);
-  png_set_read_fn(png_ptr, static_cast<void*>(&memstream), [](png_structp png_ptr, png_bytep outBytes, png_size_t byteCountToRead) {
-    std::istringstream& memstream = *static_cast<std::istringstream*>(png_get_io_ptr(png_ptr));
-    
-    memstream.read(reinterpret_cast<char*>(outBytes), byteCountToRead);
-    }
-    
-    // kIDCTMatrix[8*x+u] = alpha(u)*cos((2*x+1)*u*M_PI/16)*sqrt(2), with fixed 13
-// bit precision, where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
-// Some coefficients are off by +-1 to mimick libjpeg's behaviour.
-static const int kIDCTMatrix[kDCTBlockSize] = {
-  8192,  11363,  10703,   9633,   8192,   6437,   4433,   2260,
-  8192,   9633,   4433,  -2259,  -8192, -11362, -10704,  -6436,
-  8192,   6437,  -4433, -11362,  -8192,   2261,  10704,   9633,
-  8192,   2260, -10703,  -6436,   8192,   9633,  -4433, -11363,
-  8192,  -2260, -10703,   6436,   8192,  -9633,  -4433,  11363,
-  8192,  -6437,  -4433,  11362,  -8192,  -2261,  10704,  -9633,
-  8192,  -9633,   4433,   2259,  -8192,  11362, -10704,   6436,
-  8192, -11363,  10703,  -9633,   8192,  -6437,   4433,  -2260,
-};
-    
-    const int kJPEGNaturalOrder[80] = {
-  0,   1,  8, 16,  9,  2,  3, 10,
-  17, 24, 32, 25, 18, 11,  4,  5,
-  12, 19, 26, 33, 40, 48, 41, 34,
-  27, 20, 13,  6,  7, 14, 21, 28,
-  35, 42, 49, 56, 57, 50, 43, 36,
-  29, 22, 15, 23, 30, 37, 44, 51,
-  58, 59, 52, 45, 38, 31, 39, 46,
-  53, 60, 61, 54, 47, 55, 62, 63,
-  // extra entries for safety in decoder
-  63, 63, 63, 63, 63, 63, 63, 63,
-  63, 63, 63, 63, 63, 63, 63, 63
-};
-    
-    // Library to decode jpeg coefficients into an RGB image.
-    
-    
-    {}  // namespace guetzli
-    
-    bool RefineDCTBlock(const HuffmanTableEntry* ac_huff,
-                    int Ss, int Se, int Al,
-                    int* eobrun,
-                    BitReaderState* br,
-                    JPEGData* jpg,
-                    coeff_t* coeffs) {
-  bool eobrun_allowed = Ss > 0;
-  if (Ss == 0) {
-    int s = br->ReadBits(1);
-    coeff_t dc_coeff = coeffs[0];
-    dc_coeff |= s << Al;
-    coeffs[0] = dc_coeff;
-    ++Ss;
-  }
-  if (Ss > Se) {
-    return true;
-  }
-  int p1 = 1 << Al;
-  int m1 = -(1 << Al);
-  int k = Ss;
-  int r;
-  int s;
-  bool in_zero_run = false;
-  if (*eobrun <= 0) {
-    for (; k <= Se; k++) {
-      s = ReadSymbol(ac_huff, br);
-      if (s >= kJpegHuffmanAlphabetSize) {
-        fprintf(stderr, 'Invalid Huffman symbol %d for AC coefficient %d\n',
-                s, k);
-        jpg->error = JPEG_INVALID_SYMBOL;
-        return false;
-      }
-      r = s >> 4;
-      s &= 15;
-      if (s) {
-        if (s != 1) {
-          fprintf(stderr, 'Invalid Huffman symbol %d for AC coefficient %d\n',
-                  s, k);
-          jpg->error = JPEG_INVALID_SYMBOL;
-          return false;
-        }
-        s = br->ReadBits(1) ? p1 : m1;
-        in_zero_run = false;
-      } else {
-        if (r != 15) {
-          *eobrun = 1 << r;
-          if (r > 0) {
-            if (!eobrun_allowed) {
-              fprintf(stderr, 'End-of-block run crossing DC coeff.\n');
-              jpg->error = JPEG_EOB_RUN_TOO_LONG;
-              return false;
-            }
-            *eobrun += br->ReadBits(r);
-          }
-          break;
-        }
-        in_zero_run = true;
-      }
-      do {
-        coeff_t thiscoef = coeffs[kJPEGNaturalOrder[k]];
-        if (thiscoef != 0) {
-          if (br->ReadBits(1)) {
-            if ((thiscoef & p1) == 0) {
-              if (thiscoef >= 0) {
-                thiscoef += p1;
-              } else {
-                thiscoef += m1;
-              }
-            }
-          }
-          coeffs[kJPEGNaturalOrder[k]] = thiscoef;
-        } else {
-          if (--r < 0) {
-            break;
-          }
-        }
-        k++;
-      } while (k <= Se);
-      if (s) {
-        if (k > Se) {
-          fprintf(stderr, 'Out-of-band coefficient %d band was %d-%d\n',
-                  k, Ss, Se);
-          jpg->error = JPEG_OUT_OF_BAND_COEFF;
-          return false;
-        }
-        coeffs[kJPEGNaturalOrder[k]] = s;
-      }
-    }
-  }
-  if (in_zero_run) {
-    fprintf(stderr, 'Extra zero run before end-of-block.\n');
-    jpg->error = JPEG_EXTRA_ZERO_RUN;
+    /**
+ * Reads sizeof(T) bytes, and returns false if not enough bytes are available.
+ * Returns true if the first n bytes are equal to prefix when interpreted as
+ * a little endian T.
+ */
+template <typename T>
+typename std::enable_if<std::is_unsigned<T>::value, bool>::type
+dataStartsWithLE(const IOBuf* data, T prefix, uint64_t n = sizeof(T)) {
+  DCHECK_GT(n, 0);
+  DCHECK_LE(n, sizeof(T));
+  T value;
+  Cursor cursor{data};
+  if (!cursor.tryReadLE(value)) {
     return false;
   }
-  if (*eobrun > 0) {
-    for (; k <= Se; k++) {
-      coeff_t thiscoef = coeffs[kJPEGNaturalOrder[k]];
-      if (thiscoef != 0) {
-        if (br->ReadBits(1)) {
-          if ((thiscoef & p1) == 0) {
-            if (thiscoef >= 0) {
-              thiscoef += p1;
-            } else {
-              thiscoef += m1;
-            }
-          }
-        }
-        coeffs[kJPEGNaturalOrder[k]] = thiscoef;
-      }
-    }
-  }
-  --(*eobrun);
-  return true;
+  const T mask = n == sizeof(T) ? T(-1) : (T(1) << (8 * n)) - 1;
+  return prefix == (value & mask);
 }
     
-    void BuildSequentialHuffmanCodes(
-    const JPEGData& jpg,
-    std::vector<HuffmanCodeTable>* dc_huffman_code_tables,
-    std::vector<HuffmanCodeTable>* ac_huffman_code_tables) {
-  JPEGOutput out(NullOut, nullptr);
-  BuildAndEncodeHuffmanCodes(jpg, out, dc_huffman_code_tables,
-                             ac_huffman_code_tables);
-}
+    /**
+ * This class creates core-local caches for a given shared_ptr, to
+ * mitigate contention when acquiring/releasing it.
+ *
+ * It has the same thread-safety guarantees as shared_ptr: it is safe
+ * to concurrently call get(), but reset()s must be synchronized with
+ * reads and other resets().
+ *
+ * @author Giuseppe Ottaviano <ott@fb.com>
+ */
+template <class T, size_t kNumSlots = 64>
+class CoreCachedSharedPtr {
+ public:
+  explicit CoreCachedSharedPtr(const std::shared_ptr<T>& p = nullptr) {
+    reset(p);
+  }
+    }
