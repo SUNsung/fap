963311db64865a8a70d923356540882e4ecec7bc
SUNsung <weatherius@gmail.com>
2018-12-15 11:54:52 +0200

    Rewrite [index.rb] File in: 2018-12-15 11:54:52

--- ruby/index.rb
+++ ruby/index.rb
@@ -1,46 +1,13 @@
 
         
-            def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
-    end
-  end
-end
+        # -------------------------------------------------------------------
+# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
+# -------------------------------------------------------------------
     
-        # @import 'file.less' to '#{target_path}file;'
-    def replace_file_imports(less, target_path = '')
-      less.gsub %r([@\$]import ['|']([\w\-/]+).less['|'];),
-                %Q(@import '#{target_path}\\1';)
-    end
+      # The CategoryIndex class creates a single category page for the specified category.
+  class CategoryIndex < Page
     
-        def silence_log
-      @silence = true
-      yield
-    ensure
-      @silence = false
-    end
-  end
-end
-
+    module Jekyll
     
-      # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-    
-          # Returns the else branch of the `case` statement, if any.
-      #
-      # @return [Node] the else branch node of the `case` statement
-      # @return [nil] if the case statement does not have an else branch.
-      def else_branch
-        node_parts[-1]
-      end
-    
-    Given /^I add this snippet to the User model:$/ do |snippet|
-  file_name = 'app/models/user.rb'
-  cd('.') do
-    content = File.read(file_name)
-    File.open(file_name, 'w') { |f| f << content.sub(/end\Z/, '#{snippet}\nend') }
-  end
-end
-    
-        alias :empty? :empty_file?
+        def render(context)
+      includes_dir = File.join(context.registers[:site].source, '_includes')
