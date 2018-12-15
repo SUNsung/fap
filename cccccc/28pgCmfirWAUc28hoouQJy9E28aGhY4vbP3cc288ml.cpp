e8c0ec2ce0a319c3e90d92d7c51ca44a4a67c204
SUNsung <weatherius@gmail.com>
2018-12-15 16:58:02 +0200

    Rewrite [28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp] File in: 2018-12-15 16:58:02

--- cccccc/28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp
+++ cccccc/28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp
@@ -1,160 +1,88 @@
 
         
-        void AutoUpdater::OnWindowAllClosed() {
-  QuitAndInstall();
+        
+    {}  // namespace leveldb
+    
+      Status status;
+  if (c == nullptr) {
+    // Nothing to do
+  } else if (!is_manual && c->IsTrivialMove()) {
+    // Move file to next level
+    assert(c->num_input_files(0) == 1);
+    FileMetaData* f = c->input(0, 0);
+    c->edit()->DeleteFile(c->level(), f->number);
+    c->edit()->AddFile(c->level() + 1, f->number, f->file_size,
+                       f->smallest, f->largest);
+    status = versions_->LogAndApply(c->edit(), &mutex_);
+    if (!status.ok()) {
+      RecordBackgroundError(status);
+    }
+    VersionSet::LevelSummaryStorage tmp;
+    Log(options_.info_log, 'Moved #%lld to level-%d %lld bytes %s: %s\n',
+        static_cast<unsigned long long>(f->number),
+        c->level() + 1,
+        static_cast<unsigned long long>(f->file_size),
+        status.ToString().c_str(),
+        versions_->LevelSummary(&tmp));
+  } else {
+    CompactionState* compact = new CompactionState(c);
+    status = DoCompactionWork(compact);
+    if (!status.ok()) {
+      RecordBackgroundError(status);
+    }
+    CleanupCompaction(compact);
+    c->ReleaseInputs();
+    DeleteObsoleteFiles();
+  }
+  delete c;
+    
+        ASSERT_EQ('v1', Get('foo'));
+    ASSERT_EQ('v5', Get('baz'));
+    ASSERT_OK(Put('bar', 'v2'));
+    ASSERT_OK(Put('foo', 'v3'));
+    
+    // Print contents of a log file. (*func)() is called on every record.
+Status PrintLogContents(Env* env, const std::string& fname,
+                        void (*func)(uint64_t, Slice, WritableFile*),
+                        WritableFile* dst) {
+  SequentialFile* file;
+  Status s = env->NewSequentialFile(fname, &file);
+  if (!s.ok()) {
+    return s;
+  }
+  CorruptionReporter reporter;
+  reporter.dst_ = dst;
+  log::Reader reader(file, &reporter, true, 0);
+  Slice record;
+  std::string scratch;
+  while (reader.ReadRecord(&record, &scratch)) {
+    (*func)(reader.LastRecordOffset(), record, dst);
+  }
+  delete file;
+  return Status::OK();
 }
     
-    #include 'atom/browser/api/atom_api_browser_window.h'
+    // Return the name of the old info log file for 'dbname'.
+std::string OldInfoLogFileName(const std::string& dbname);
     
-    Net::~Net() {}
-    
-      // The window class of |window_|.
-  ATOM atom_;
-    
-    // A self-destroyed class for handling save page request.
-class SavePageHandler : public content::DownloadManager::Observer,
-                        public download::DownloadItem::Observer {
- public:
-  using SavePageCallback = base::Callback<void(v8::Local<v8::Value>)>;
-    }
-    
-    
-    {            vst1q_s16(dst + j, v_dst0);
-            vst1q_s16(dst + j + 8, v_dst1);
-        }
-        for (; j < roiw8; j += 8)
-        {
-            int16x8_t v_src = vreinterpretq_s16_u16(vmovl_u8(vld1_u8(src + j)));
-            int16x8_t v_dst = vld1q_s16(dst + j);
-            int16x4_t v_srclo = vget_low_s16(v_src), v_srchi = vget_high_s16(v_src);
-            v_dst = vcombine_s16(vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srclo, v_srclo), shift), vget_low_s16(v_dst))),
-                                 vqmovn_s32(vaddw_s16(vshrq_n_s32(vmull_s16(v_srchi, v_srchi), shift), vget_high_s16(v_dst))));
-            vst1q_s16(dst + j, v_dst);
-        }
-    
-    void bitwiseAnd(const Size2D &size,
-                const u8 *src0Base, ptrdiff_t src0Stride,
-                const u8 *src1Base, ptrdiff_t src1Stride,
-                u8 *dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, BitwiseAnd());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
+    static void Usage() {
+  fprintf(
+      stderr,
+      'Usage: leveldbutil command...\n'
+      '   dump files...         -- dump contents of specified files\n'
+      );
 }
-    
-    #define MERGE_ASM2(sgn, bits) __asm__ ( \
-                                          'vld1.' #bits ' {d0-d1}, [%[in0]]             \n\t' \
-                                          'vld1.' #bits ' {d2-d3}, [%[in1]]             \n\t' \
-                                          'vst2.' #bits ' {d0, d2}, [%[out0]]           \n\t' \
-                                          'vst2.' #bits ' {d1, d3}, [%[out1]]           \n\t' \
-                                          : \
-                                          : [in0] 'r' (src0 + sj), [in1] 'r' (src1 + sj), \
-                                            [out0]  'r' (dst + dj), [out1]  'r' (dst + dj + MUL2(8)/sizeof(sgn##bits)) \
-                                          : 'd0','d1','d2','d3' \
-                                      );
-#define MERGE_ASM3(sgn, bits) __asm__ ( \
-                                          'vld1.' #bits ' {d0-d1}, [%[in0]]             \n\t' \
-                                          'vld1.' #bits ' {d2-d3}, [%[in1]]             \n\t' \
-                                          'vld1.' #bits ' {d4-d5}, [%[in2]]             \n\t' \
-                                          'vst3.' #bits ' {d0, d2, d4}, [%[out0]]       \n\t' \
-                                          'vst3.' #bits ' {d1, d3, d5}, [%[out1]]       \n\t' \
-                                          : \
-                                          : [in0] 'r' (src0 + sj), [in1] 'r' (src1 + sj), [in2] 'r' (src2 + sj), \
-                                            [out0]  'r' (dst + dj), [out1]  'r' (dst + dj + MUL3(8)/sizeof(sgn##bits)) \
-                                          : 'd0','d1','d2','d3','d4','d5' \
-                                      );
-#define MERGE_ASM4(sgn, bits) __asm__ ( \
-                                          'vld1.' #bits ' {d0-d1}, [%[in0]]             \n\t' \
-                                          'vld1.' #bits ' {d2-d3}, [%[in1]]             \n\t' \
-                                          'vld1.' #bits ' {d4-d5}, [%[in2]]             \n\t' \
-                                          'vld1.' #bits ' {d6-d7}, [%[in3]]             \n\t' \
-                                          'vst4.' #bits ' {d0, d2, d4, d6}, [%[out0]]   \n\t' \
-                                          'vst4.' #bits ' {d1, d3, d5, d7}, [%[out1]]   \n\t' \
-                                          : \
-                                          : [in0] 'r' (src0 + sj), [in1] 'r' (src1 + sj), [in2] 'r' (src2 + sj), [in3] 'r' (src3 + sj), \
-                                            [out0]  'r' (dst + dj), [out1]  'r' (dst + dj + MUL4(8)/sizeof(sgn##bits)) \
-                                          : 'd0','d1','d2','d3','d4','d5','d6','d7' \
-                                      );
-    
-    #endif
-    
-    namespace internal {
-    }
-    
-                int32x2_t vnz1 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx1), vget_high_u32(vlx1)));
-            int32x2_t vnz2 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx2), vget_high_u32(vlx2)));
-            int32x2_t vnz3 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx3), vget_high_u32(vlx3)));
-            int32x2_t vnz4 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx4), vget_high_u32(vlx4)));
     
     
     {
-    {
-    {                    for( k = 0; k < N; k++ )
-                    {
-                        s32 x = ptr[pixel[k]];
-                        if(x > vt)
-                        {
-                            if( ++count > K )
-                            {
-                                cornerpos[ncorners++] = j;
-                                if(nonmax_suppression)
-                                    curr[j] = cornerScore(ptr, pixel);
-                                break;
-                            }
-                        }
-                        else
-                            count = 0;
-                    }
-                }
-            }
-        }
+    {}  // namespace log
+}  // namespace leveldb
     
-                if (mask[0])
-                process(src, j, j + 8, i,
-                        minVal, minLocPtr, minLocCount, minLocCapacity,
-                        maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
-            if (mask[1])
-                process(src, j + 8, j + 16, i,
-                        minVal, minLocPtr, minLocCount, minLocCapacity,
-                        maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
-        }
-        for ( ; j < roiw8; j += 8)
-        {
-            internal::prefetch(src + j);
-            int16x8_t v_src = vld1q_s16(src + j);
-    
-    void flip(const Size2D &size,
-          const u8 * srcBase, ptrdiff_t srcStride,
-          u8 * dstBase, ptrdiff_t dstStride,
-          FLIP_MODE flipMode, u32 elemSize)
-{
-    internal::assertSupportedConfiguration(isFlipSupported(flipMode, elemSize));
-#ifdef CAROTENE_NEON
+      void CheckInitialOffsetRecord(uint64_t initial_offset,
+                                int expected_record_offset) {
+    WriteInitialOffsetLog();
+    reading_ = true;
+    source_.contents_ = Slice(dest_.contents_);
+    Reader* offset_reader = new Reader(&source_, &report_, true/*checksum*/,
+                                       initial_offset);
     }
-    
-        if (borderType == BORDER_MODE_CONSTANT)
-        for (s32 k = 0; k < cn; ++k)
-        {
-            lane[-cn+k] = borderValue;
-            lane[-cn-cn+k] = borderValue;
-            lane[colsn+k] = borderValue;
-            lane[colsn+cn+k] = borderValue;
-        }
-    
-    
-    {	RegCloseKey(command);
-	RegCloseKey(cmderKey);
-	RegCloseKey(root);
-}
