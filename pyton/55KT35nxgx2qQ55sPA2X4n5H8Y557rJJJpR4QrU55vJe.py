34bd62c3cdcf20fa977dca9ca9c44c67420dd948
SUNsung <weatherius@gmail.com>
2018-12-15 17:46:13 +0200

    Rewrite [55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py] File in: 2018-12-15 17:46:13

--- pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
+++ pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
@@ -1,117 +1,83 @@
 
         
-          # Note that we put these 'truth' rates and input into this
-  # structure, the only data that is used in LFADS are the noisy
-  # data e.g. spike trains.  The rest is either for printing or posterity.
-  data = {'train_truth': rates_train,
-          'valid_truth': rates_valid,
-          'input_train_truth' : input_train,
-          'input_valid_truth' : inputs_valid,
-          'train_data' : noisy_data_train,
-          'valid_data' : noisy_data_valid,
-          'train_percentage' : train_percentage,
-          'nreplications' : nreplications,
-          'dt' : rnn['dt'],
-          'input_magnitude' : input_magnitude,
-          'input_times_train' : input_times_train,
-          'input_times_valid' : input_times_valid,
-          'P_sxn' : P_sxn,
-          'condition_labels_train' : condition_labels_train,
-          'condition_labels_valid' : condition_labels_valid,
-          'conversion_factor': 1.0 / rnn['conversion_factor']}
-  datasets[dataset_name] = data
+        
+def create_app(test_config=None):
+    '''Create and configure an instance of the Flask application.'''
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY='dev',
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
     
-    def get_data_batch(batch_size, T, rng, u_std):
-  u_bxt = rng.randn(batch_size, T) * u_std
-  running_sum_b = np.zeros([batch_size])
-  labels_bxt = np.zeros([batch_size, T])
-  for t in xrange(T):
-    running_sum_b += u_bxt[:, t]
-    labels_bxt[:, t] += running_sum_b
-  labels_bxt = np.clip(labels_bxt, -1, 1)
-  return u_bxt, labels_bxt
-    
-      def evaluate(self):
-    '''Evaluate the current ensemble.'''
-    # Attach word probabilities and correctness label to each substitution
-    ensembled_probs = sum(self.all_probs) / len(self.all_probs)
-    scorings = []
-    for i, sentence in enumerate(self.sentences):
-      correctness = self.labels[i]
-      word_probs = ensembled_probs[i, :len(sentence)]
-      joint_prob = np.prod(word_probs, dtype=np.float64)
-    
-      return word_to_id
+        # close and remove the temporary database
+    os.close(db_fd)
+    os.unlink(db_path)
     
     
-def create_masked_cross_entropy_loss(targets, present, logits):
-  '''Calculate the cross entropy loss matrices for the masked tokens.'''
-  cross_entropy_losses = losses.cross_entropy_loss_matrix(targets, logits)
-    
-      Args:
-    hparams:  MaskGAN hyperparameters.
-    learning_rate:  tf.Variable scalar learning rate.
-    final_gen_objective:  Scalar final REINFORCE objective for the sequence.
-    averages_op:  ExponentialMovingAverage apply average op to
-      maintain the baseline.
-    global_step:  global_step tf.Variable.
-    
-      ## Load weights from language model checkpoint.
-  if FLAGS.language_model_ckpt_dir:
-    if FLAGS.maskgan_ckpt is None:
-      ## Generator Models.
-      if FLAGS.generator_model == 'rnn_nas':
-        load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-        print('Restoring Generator from %s.' % load_ckpt)
-        tf.logging.info('Restoring Generator from %s.' % load_ckpt)
-        gen_init_saver = init_savers['gen_init_saver']
-        gen_init_saver.restore(sess, load_ckpt)
-    
-    atom_template = atom_template.replace('@TIMESTAMP@', now_iso)
-    
-        # Get the version from youtube_dl/version.py without importing the package
-    exec(compile(open('youtube_dl/version.py').read(),
-                 'youtube_dl/version.py', 'exec'))
-    
-    # The suffix of source filenames.
-source_suffix = '.rst'
-    
-            ydl = YDL({'format': 'mp3'})
-        ie = YoutubeIE(ydl)
-        ie._sort_formats(info_dict['formats'])
-        ydl.process_ie_result(copy.deepcopy(info_dict))
-        downloaded = ydl.downloaded_info_dicts[0]
-        self.assertEqual(downloaded['format_id'], 'mp3-64')
-    
-        def test_cbc_decrypt(self):
-        data = bytes_to_intlist(
-            b'\x97\x92+\xe5\x0b\xc3\x18\x91ky9m&\xb3\xb5@\xe6'\xc2\x96.\xc8u\x88\xab9-[\x9e|\xf1\xcd'
-        )
-        decrypted = intlist_to_bytes(aes_cbc_decrypt(data, self.key, self.iv))
-        self.assertEqual(decrypted.rstrip(b'\x08'), self.secret_msg)
+def test_register(client, app):
+    # test that viewing the page renders without template errors
+    assert client.get('/auth/register').status_code == 200
     
     
-if __name__ == '__main__':
-    unittest.main()
-
+_request_ctx_err_msg = '''\
+Working outside of request context.
     
-    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url)
-    title = r1(r'<title>(.*)</title>', html)
-    assert title
-    id = r1(r'flvid\s*=\s*(\d+)', html)
-    assert id
-    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
     
-            for i in range(len(titles)):
-            title = titles[i]
-            real_url = real_urls[i]
+class TagDateTime(JSONTag):
+    __slots__ = ()
+    key = ' d'
     
+        def __getitem__(self, key):
+        self.accessed = True
+        return super(SecureCookieSession, self).__getitem__(key)
+    
+        The decorators stored in the decorators list are applied one after another
+    when the view function is created.  Note that you can *not* use the class
+    based decorators since those would decorate the view class and not the
+    generated view function!
     '''
-refer to http://open.tudou.com/wiki/video/info
-'''
-tudou_api_patterns = [ ]
     
-        print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([video_url], title, ext, size, output_dir, merge=merge, headers = fake_headers)
+    
+class Regularizer(object):
+    '''Regularizer base class.
+    '''
+    
+    
+def test_categorical_hinge():
+    y_pred = K.variable(np.array([[0.3, 0.2, 0.1],
+                                  [0.1, 0.2, 0.7]]))
+    y_true = K.variable(np.array([[0, 1, 0],
+                                  [1, 0, 0]]))
+    expected_loss = ((0.3 - 0.2 + 1) + (0.7 - 0.1 + 1)) / 2.0
+    loss = K.eval(losses.categorical_hinge(y_true, y_pred))
+    assert np.isclose(expected_loss, np.mean(loss))
+    
+        available_devices = _get_available_devices()
+    available_devices = [_normalize_device_name(name)
+                         for name in available_devices]
+    if not gpus:
+        # Using all visible GPUs when not specifying `gpus`
+        # e.g. CUDA_VISIBLE_DEVICES=0,2 python keras_mgpu.py
+        gpus = len([x for x in available_devices if 'gpu' in x])
+    
+            self.kernel_constraint = constraints.get(kernel_constraint)
+        self.recurrent_constraint = constraints.get(recurrent_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+    
+    
+def fit_loop(model, fit_function, fit_inputs,
+             out_labels=None,
+             batch_size=None,
+             epochs=100,
+             verbose=1,
+             callbacks=None,
+             val_function=None,
+             val_inputs=None,
+             shuffle=True,
+             callback_metrics=None,
+             initial_epoch=0,
+             steps_per_epoch=None,
+             validation_steps=None):
+    '''Abstract fit function for `fit_function(fit_inputs)`.
