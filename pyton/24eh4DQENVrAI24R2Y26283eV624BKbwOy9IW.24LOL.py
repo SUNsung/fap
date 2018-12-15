674ef432b96ec3d80fb265d0d9ee0b5b3f8dadea
SUNsung <weatherius@gmail.com>
2018-12-15 10:02:50 +0200

    Rewrite [24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py] File in: 2018-12-15 10:02:50

--- pyton/24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py
+++ pyton/24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py
@@ -1,140 +1,208 @@
 
         
-            def extract_year_month(self, line):
-        '''Return the year and month portions of the timestamp.'''
-        pass
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Isotonic Regression benchmark tool')
+    parser.add_argument('--seed', type=int,
+                        help='RNG seed')
+    parser.add_argument('--iterations', type=int, required=True,
+                        help='Number of iterations to average timings over '
+                        'for each problem size')
+    parser.add_argument('--log_min_problem_size', type=int, required=True,
+                        help='Base 10 logarithm of the minimum problem size')
+    parser.add_argument('--log_max_problem_size', type=int, required=True,
+                        help='Base 10 logarithm of the maximum problem size')
+    parser.add_argument('--show_plot', action='store_true',
+                        help='Plot timing output with matplotlib')
+    parser.add_argument('--dataset', choices=DATASET_GENERATORS.keys(),
+                        required=True)
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
+    
+def compute_bench(alpha, n_samples, n_features, precompute):
+    lasso_results = []
+    lars_lasso_results = []
+    
+        if args.plot is not None:
+        max_val = getattr(args, args.plot)
+        if args.plot in ('classes', 'samples'):
+            min_val = 2
+        else:
+            min_val = 0
+        steps = np.linspace(min_val, max_val, num=args.n_steps + 1)[1:]
+        if args.plot in ('classes', 'samples'):
+            steps = np.unique(np.round(steps).astype(int))
+        setattr(args, args.plot, steps)
+    
+    from sklearn.utils import check_random_state
+from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.metrics.pairwise import pairwise_kernels
+    
+        ###########################################################################
+    # List sampling algorithm
+    ###########################################################################
+    # We assume that sampling algorithm has the following signature:
+    #   sample(n_population, n_sample)
+    #
+    sampling_algorithm = {}
+    
+    try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+    
+    
+    
+            import_command = 'security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ../../../../data/gae_proxy/CA.crt'# % certfile.decode('utf-8')
+        if exist_ca_sha1:
+            delete_ca_command = 'security delete-certificate -Z %s' % exist_ca_sha1
+            exec_command = '%s;%s' % (delete_ca_command, import_command)
+        else:
+            exec_command = import_command
+    
+        def get(self):
+        n = random.randint(2, 3)
+        ws = []
+        for i in range(0, n):
+            w = self.slice.get()
+            ws.append(w)
+    
+    
+    def _getRuleInvocationStack(cls, module):
+        '''
+        A more general version of getRuleInvocationStack where you can
+        pass in, for example, a RecognitionException to get it's rule
+        stack trace.  This routine is shared with all recognizers, hence,
+        static.
+    
+            self.assertEqual(jws, JWS.from_json(jws.to_json()))
+    
+    csr = OpenSSL.crypto.load_certificate_request(
+    OpenSSL.crypto.FILETYPE_ASN1, pkg_resources.resource_string(
+        'acme', os.path.join('testdata', 'csr.der')))
+try:
+    acme.request_issuance(jose.util.ComparableX509(csr), (authzr,))
+except messages.Error as error:
+    print ('This script is doomed to fail as no authorization '
+           'challenges are ever solved. Error from server: {0}'.format(error))
+
+    
+        :returns: filename of vhost
+    :rtype: str
+    
+    from certbot_apache import obj
+    
+            self.assertTrue(self.vhost1.conflicts([self.addr2]))
+        self.assertFalse(self.vhost1.conflicts([self.addr_default]))
+    
+    
+class DBUnpickler(pickle.Unpickler):
+    
+    [1] http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718
+    
+    # Import the email modules we'll need
+from email import policy
+from email.parser import BytesParser
+    
+    #
+# Functions referenced by tasks
+#
+    
+        print('Writing private key to file %s_privkey.txt...' % name)
+    with open('%s_privkey.txt' % name, 'w') as fo:
+        fo.write('%s,%s,%s' % (keySize, privateKey[0], privateKey[1]))
+    
+            return new_key
+
+    
+    
+
+    
+            print()
+        print('The minimum Edit Distance is: %d' % (solver.solve(S1, S2)))
+        print()
+        print('*************** End of Testing Edit Distance DP Algorithm ***************')
+
+    
+    The problem is  :
+Given an array, to find the longest and continuous sub array and get the max sum of the sub array in the given array.
+'''
+from __future__ import print_function
+    
+    
+def b_expo_mod(a, b, c):
+    res = 0
+    while b > 0:
+        if b&1:
+            res = ((res%c) + (a%c)) % c
+    
+        >>> isEnglish('llold HorWd')
+    False
+    '''
+    wordsMatch = getEnglishCount(message) * 100 >= wordPercentage
+    numLetters = len(removeNonLetters(message))
+    messageLettersPercentage = (float(numLetters) / len(message)) * 100
+    lettersMatch = messageLettersPercentage >= letterPercentage
+    return wordsMatch and lettersMatch
+    
+        # Make the eyebrows into a nightmare
+    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
+    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
+    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
+    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
+    
+    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
+# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
+# positive matches at the risk of more false negatives.
+    
+    while True:
+    print('Capturing image.')
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format='rgb')
+    
+            # Now let's list all the faces we found in all 128 frames
+        for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
+            number_of_faces_in_frame = len(face_locations)
+    
+    setup(
+    name='face_recognition',
+    version='1.2.3',
+    description='Recognize faces from Python or from the command line',
+    long_description=readme + '\n\n' + history,
+    author='Adam Geitgey',
+    author_email='ageitgey@gmail.com',
+    url='https://github.com/ageitgey/face_recognition',
+    packages=[
+        'face_recognition',
+    ],
+    package_dir={'face_recognition': 'face_recognition'},
+    package_data={
+        'face_recognition': ['models/*.dat']
+    },
+    entry_points={
+        'console_scripts': [
+            'face_recognition=face_recognition.face_recognition_cli:main',
+            'face_detection=face_recognition.face_detection_cli:main'
         ]
-    
-        def set(self, key, value):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                item.value = value
-                return
-        self.table[hash_index].append(Item(key, value))
-    
-      for group in itervalues( group_name_to_group ):
-    parent_names = GetParentNames( group )
-    
-      post_vim_message.assert_has_exact_calls( [
-    call( 'On the first day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'A test file in a Command-T', warning=False, truncate=True ),
-    call( 'On the second day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'Two popup menus, and a test file in a Command-T',
-          warning=False,
-          truncate=True ),
-  ] )
-
-    
-    
-@YouCompleteMeInstance()
-def SendCompletionRequest_UnicodeWorkingDirectory_test( ycm ):
-  unicode_dir = PathToTestFile( 'uni¢𐍈d€' )
-  current_buffer = VimBuffer( PathToTestFile( 'uni¢𐍈d€', 'current_buffer' ) )
-    
-      def _adjust_thread_count( self ):
-    # When the executor gets lost, the weakref callback will wake up
-    # the worker threads.
-    def weakref_cb( _, q=self._work_queue ):
-      q.put( None )
-    # TODO(bquinlan): Should avoid creating new threads if there are more
-    # idle threads than items in the work queue.
-    if len( self._threads ) < self._max_workers:
-      t = threading.Thread( target=_worker,
-                            args=( weakref.ref( self, weakref_cb ),
-                                   self._work_queue ) )
-      t.daemon = True
-      t.start()
-      self._threads.add( t )
-    
-            result_item = result_queue.get(block=True)
-        if result_item is not None:
-            work_item = pending_work_items[result_item.work_id]
-            del pending_work_items[result_item.work_id]
-    
-    # If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.append(os.path.abspath('.'))
-    
-            if cfg.TEST.MASK_AUG.SCALE_H_FLIP:
-            masks_scl_hf = im_detect_mask_scale(
-                model, im, scale, max_size, boxes, hflip=True
-            )
-            masks_ts.append(masks_scl_hf)
-    
-            return self.net.Conv(
-            blobs_in, blob_out, kernel=kernel, order=self.order, **kwargs
-        )
-    
-    
-def _whctrs(anchor):
-    '''Return width, height, x center, and y center for an anchor (window).'''
-    w = anchor[2] - anchor[0] + 1
-    h = anchor[3] - anchor[1] + 1
-    x_ctr = anchor[0] + 0.5 * (w - 1)
-    y_ctr = anchor[1] + 0.5 * (h - 1)
-    return w, h, x_ctr, y_ctr
-    
-    
-def add_single_scale_rpn_losses(model):
-    '''Add losses for a single scale RPN model (i.e., no FPN).'''
-    # Spatially narrow the full-sized RPN label arrays to match the feature map
-    # shape
-    model.net.SpatialNarrowAs(
-        ['rpn_labels_int32_wide', 'rpn_cls_logits'], 'rpn_labels_int32'
-    )
-    for key in ('targets', 'inside_weights', 'outside_weights'):
-        model.net.SpatialNarrowAs(
-            ['rpn_bbox_' + key + '_wide', 'rpn_bbox_pred'], 'rpn_bbox_' + key
-        )
-    loss_rpn_cls = model.net.SigmoidCrossEntropyLoss(
-        ['rpn_cls_logits', 'rpn_labels_int32'],
-        'loss_rpn_cls',
-        scale=model.GetLossScale()
-    )
-    loss_rpn_bbox = model.net.SmoothL1Loss(
-        [
-            'rpn_bbox_pred', 'rpn_bbox_targets', 'rpn_bbox_inside_weights',
-            'rpn_bbox_outside_weights'
-        ],
-        'loss_rpn_bbox',
-        beta=1. / 9.,
-        scale=model.GetLossScale()
-    )
-    loss_gradients = blob_utils.get_loss_gradients(
-        model, [loss_rpn_cls, loss_rpn_bbox]
-    )
-    model.AddLosses(['loss_rpn_cls', 'loss_rpn_bbox'])
-    return loss_gradients
-
-    
-    logger = logging.getLogger(__name__)
-    
-    
-# octave and aspect fields are only used on RetinaNet. Octave corresponds to the
-# scale of the anchor and aspect denotes which aspect ratio is used in the range
-# of aspect ratios
-FieldOfAnchors = namedtuple(
-    'FieldOfAnchors', [
-        'field_of_anchors', 'num_cell_anchors', 'stride', 'field_size',
-        'octave', 'aspect'
-    ]
+    },
+    install_requires=requirements,
+    license='MIT license',
+    zip_safe=False,
+    keywords='face_recognition',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
-    
-    from detectron.core.config import cfg
-import detectron.modeling.FPN as fpn
-import detectron.roi_data.keypoint_rcnn as keypoint_rcnn_roi_data
-import detectron.roi_data.mask_rcnn as mask_rcnn_roi_data
-import detectron.utils.blob as blob_utils
-import detectron.utils.boxes as box_utils
