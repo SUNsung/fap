292846a2af89d9c6bed07734197ce5fe86f07688
SUNsung <weatherius@gmail.com>
2018-12-16 03:23:23 +0200

    Rewrite [17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py] File in: 2018-12-16 03:23:23

--- pyton/17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py
+++ pyton/17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py
@@ -1,79 +1,220 @@
 
         
-            module = AnsibleModule(argument_spec=argument_spec,
-                           supports_check_mode=True,
-                           required_if=[
-                               ('routing', 'dynamic', ['bgp_asn'])
-                           ]
-                           )
+              # Second, take the sum to get a tensor of shape [batch_size, output_dim].
+      self.pair_path_embeddings = tf.reduce_sum(self.weighted, 1)
     
-    from ansible.module_utils.aws.core import AnsibleAWSModule
-from ansible.module_utils.ec2 import (ansible_dict_to_boto3_filter_list,
-                                      boto3_tag_list_to_ansible_dict,
-                                      camel_dict_to_snake_dict)
-try:
-    from botocore.exceptions import (BotoCoreError, ClientError)
-except ImportError:
-    pass  # caught by imported AnsibleAWSModule
+    # save down the network outputs (may be useful later)
+train_outputs_u, valid_outputs_u = split_list_by_inds(outs_e,
+                                                      train_inds,
+                                                      valid_inds)
+train_outputs_u = np.array(train_outputs_u)
+valid_outputs_u = np.array(valid_outputs_u)
     
-    # Create a snapshot only if the most recent one is older than 1 hour
-- local_action:
-    module: ec2_snapshot
-    volume_id: vol-abcdef12
-    last_snapshot_min_age: 60
-'''
+    # pick which RNN is used on each trial
+rnn_to_use = rng.randint(2, size=E)
+ext_input = np.repeat(np.expand_dims(rnn_to_use, axis=1), ntimesteps, axis=1)
+ext_input = np.expand_dims(ext_input, axis=2)  # these are 'a's' in the paper
+    
+      Returns:
+    The length B list of TxN numpy tensors.
+  '''
+  T = len(values_t_bxn)
+  B, N = values_t_bxn[0].shape
+  values_b_txn = []
+  for b in range(B):
+    values_pb_txn = np.zeros([T,N])
+    for t in range(T):
+      values_pb_txn[t,:] = values_t_bxn[t][b,:]
+    values_b_txn.append(values_pb_txn)
+    
+      @property
+  def word_char_ids(self):
+    return self._word_char_ids
+    
+      decoder_embedding = [
+      v for v in tf.trainable_variables()
+      if v.op.name == 'gen/decoder/rnn/embedding'
+  ][0]
+  decoder_lstm_w_0 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel'
+  ][0]
+  decoder_lstm_b_0 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias'
+  ][0]
+  decoder_lstm_w_1 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel'
+  ][0]
+  decoder_lstm_b_1 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias'
+  ][0]
+  decoder_softmax_b = [
+      v for v in tf.trainable_variables()
+      if v.op.name == 'gen/decoder/rnn/softmax_b'
+  ][0]
     
     
-DOCUMENTATION = '''
----
-module: iam_role_facts
-short_description: Gather information on IAM roles
-description:
-    - Gathers information about IAM roles
-version_added: '2.5'
-requirements: [ boto3 ]
-author:
-    - 'Will Thames (@willthames)'
-options:
-    name:
-        description:
-            - Name of a role to search for
-            - Mutually exclusive with C(prefix)
-        aliases:
-            - role_name
-    path_prefix:
-        description:
-            - Prefix of role I(path) to restrict IAM role search for
-            - Mutually exclusive with C(name)
-extends_documentation_fragment:
-  - aws
-  - ec2
-'''
+def attention_decoder_fn_train(encoder_state,
+                               attention_keys,
+                               attention_values,
+                               attention_score_fn,
+                               attention_construct_fn,
+                               name=None):
+  '''Attentional decoder function for `dynamic_rnn_decoder` during training.
     
-    
-DOCUMENTATION = '''
----
-module: iam_server_certificate_facts
-short_description: Retrieve the facts of a server certificate
-description:
-  - Retrieve the attributes of a server certificate
-version_added: '2.2'
-author: 'Allen Sanabria (@linuxdynasty)'
-requirements: [boto3, botocore]
-options:
-  name:
-    description:
-      - The name of the server certificate you are retrieving attributes for.
-    required: true
-extends_documentation_fragment:
-    - aws
-    - ec2
-'''
-    
-        if region:
-        client_connection = boto3_conn(module, conn_type='client', resource='s3', region=region, endpoint=ec2_url, **aws_connect_params)
-        resource_connection = boto3_conn(module, conn_type='resource', resource='s3', region=region, endpoint=ec2_url, **aws_connect_params)
+        if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
     else:
-        module.fail_json(msg='region must be specified')
+        # load the test config if passed in
+        app.config.update(test_config)
     
-            return self.results
+        def __init__(self, name, import_name, static_folder=None,
+                 static_url_path=None, template_folder=None,
+                 url_prefix=None, subdomain=None, url_defaults=None,
+                 root_path=None):
+        _PackageBoundObject.__init__(self, import_name, template_folder,
+                                     root_path=root_path)
+        self.name = name
+        self.url_prefix = url_prefix
+        self.subdomain = subdomain
+        self.static_folder = static_folder
+        self.static_url_path = static_url_path
+        self.deferred_functions = []
+        if url_defaults is None:
+            url_defaults = {}
+        self.url_values_defaults = url_defaults
+    
+    
+def explain_template_loading_attempts(app, template, attempts):
+    '''This should help developers understand what failed'''
+    info = ['Locating template '%s':' % template]
+    total_found = 0
+    blueprint = None
+    reqctx = _request_ctx_stack.top
+    if reqctx is not None and reqctx.request.blueprint is not None:
+        blueprint = reqctx.request.blueprint
+    
+        if not has_level_handler(logger):
+        logger.addHandler(default_handler)
+    
+    from .globals import _request_ctx_stack, _app_ctx_stack
+from .signals import template_rendered, before_render_template
+    
+            if 'methods' not in d:
+            methods = set()
+    
+    min_primitive_root = 3
+    
+    if __name__ == '__main__':
+    num = generateLargePrime()
+    print(('Prime number:', num))
+    print(('isPrime:', isPrime(num)))
+
+    
+            next_prime_gt = next_prime(value % self.size_table) \
+            if not check_prime(value % self.size_table) else value % self.size_table  #gt = bigger than
+        return next_prime_gt - (data % next_prime_gt)
+    
+    The problem is :
+Given two strings A and B. Find the minimum number of operations to string B such that A = B. The permitted operations are removal,  insertion, and substitution.
+'''
+from __future__ import print_function
+    
+    if __name__ == '__main__':
+	import sys
+    
+    The problem is  :
+Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
+Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
+'''
+from __future__ import print_function
+    
+    def revise_centroids(data, k, cluster_assignment):
+    new_centroids = []
+    for i in range(k):
+        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
+        member_data_points = data[cluster_assignment==i]
+        # Compute the mean of the data points. Fill in the blank (RHS only)
+        centroid = member_data_points.mean(axis=0)
+        new_centroids.append(centroid)
+    new_centroids = np.array(new_centroids)
+    
+    return new_centroids
+    
+    word_bysig = collections.defaultdict(list)
+for word in word_list:
+    word_bysig[signature(word)].append(word)
+    
+        return res
+    
+            a += a
+        b >>= 1
+    
+        def render(self):
+        for graphic in self.graphics:
+            graphic.render()
+    
+    ### OUTPUT ###
+# Counting to two...
+# one two
+# Counting to five...
+# one two three four five
+
+    
+    import random
+import time
+    
+        def rollback(self):
+        for a_state in self.states:
+            a_state()
+    
+    
+class DecimalViewer:
+    def update(self, subject):
+        print(u'DecimalViewer: Subject %s has data %d' % (subject.name, subject.data))
+    
+        def get_current_time_as_html_fragment(self):
+        current_time = self.time_provider.now()
+        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
+        return current_time_as_html_fragment
+'''
+    
+    
+class AbstractExpert(object):
+    
+    # Repeat the entire benchmark this many times (on different ports)
+# This gives JITs time to warm up, etc.  Pypy needs 3-5 runs at
+# --n=15000 for its JIT to reach full effectiveness
+define('num_runs', type=int, default=1)
+    
+        * ``permitted_protocols``: List (or set) of protocols which should be
+      linkified, e.g. ``linkify(text, permitted_protocols=['http', 'ftp',
+      'mailto'])``. It is very unsafe to include protocols such as
+      ``javascript``.
+    '''
+    if extra_params and not callable(extra_params):
+        extra_params = ' ' + extra_params.strip()
+    
+                if __name__ == '__main__':
+                IOLoop.current().run_sync(main)
+    
+        # Supported date/time formats in our options
+    _DATETIME_FORMATS = [
+        '%a %b %d %H:%M:%S %Y',
+        '%Y-%m-%d %H:%M:%S',
+        '%Y-%m-%d %H:%M',
+        '%Y-%m-%dT%H:%M',
+        '%Y%m%d %H:%M:%S',
+        '%Y%m%d %H:%M',
+        '%Y-%m-%d',
+        '%Y%m%d',
+        '%H:%M:%S',
+        '%H:%M',
+    ]
+    
+            def assign_thread_identity() -> None:
+            self._thread_identity = get_ident()
