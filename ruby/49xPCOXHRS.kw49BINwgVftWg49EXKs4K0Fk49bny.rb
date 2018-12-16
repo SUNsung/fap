fdb3dedf42d06a55602a760e8d1962b9ddbac6fa
SUNsung <weatherius@gmail.com>
2018-12-16 08:18:16 +0200

    Rewrite [49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb] File in: 2018-12-16 08:18:16

--- ruby/49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb
+++ ruby/49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb
@@ -1,157 +1,222 @@
 
         
-                  def initialize(template_object, object_name, method_name, object,
-                         sanitized_attribute_name, text, value, input_html_options)
-            @template_object = template_object
-            @object_name = object_name
-            @method_name = method_name
-            @object = object
-            @sanitized_attribute_name = sanitized_attribute_name
-            @text = text
-            @value = value
-            @input_html_options = input_html_options
-          end
+            def run(action_named: nil, action_class_ref: nil, parameter_map: nil)
+      action_return = runner.execute_action(action_named, action_class_ref, [parameter_map], custom_dir: '.')
+      return action_return
+    end
     
-            private
+          it 'adds verbose param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            verbose: '1'
+          )
+        end').runner.execute(:test)
     
-    module ActionView
-  # This is the main entry point for rendering. It basically delegates
-  # to other objects like TemplateRenderer and PartialRenderer which
-  # actually renders the template.
-  #
-  # The Renderer will parse the options from the +render+ or +render_body+
-  # method and render a partial or a template based on the options. The
-  # +TemplateRenderer+ and +PartialRenderer+ objects are wrappers which do all
-  # the setup and logic necessary to render a view and a new object is created
-  # each time +render+ is called.
-  class Renderer
-    attr_accessor :lookup_context
+            inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
+        pseudocommand = 'git log --pretty=\'%B\' #{inner_command.shellescape}...HEAD'
+        expect(result).to eq(pseudocommand)
+      end
     
-    def version
-  Jekyll::VERSION
-end
-    
-            def make_accessible(hash = @config)
-          hash.keys.each do |key|
-            hash[key.to_sym] = hash[key]
-            make_accessible(hash[key]) if hash[key].is_a?(Hash)
-          end
-        end
-    
-      after :each do
-    untrace_var :$Kernel_trace_var_global
-    
-          it 'converts value to Integer' do
-        w = KernelSpecs::WarnInNestedCall.new
-    
-    
-IAX_SUBTYPE_NEW     = 1
-IAX_SUBTYPE_PING    = 2
-IAX_SUBTYPE_PONG    = 3
-IAX_SUBTYPE_ANSWER  = 4
-IAX_SUBTYPE_ACK     = 4
-IAX_SUBTYPE_HANGUP  = 5
-IAX_SUBTYPE_REJECT  = 6
-IAX_SUBTYPE_ACCEPT  = 7
-IAX_SUBTYPE_AUTHREQ = 8
-IAX_SUBTYPE_AUTHREP = 9
-IAX_SUBTYPE_INVAL   = 10
-IAX_SUBTYPE_LAGRQ   = 11
-IAX_SUBTYPE_LAGRP   = 12
-IAX_SUBTYPE_REGREQ  = 13
-IAX_SUBTYPE_REGAUTH = 14
-IAX_SUBTYPE_REGACK  = 15
-IAX_SUBTYPE_REGREJ  = 16
-IAX_SUBTYPE_REGREL  = 17
-IAX_SUBTYPE_VNAK    = 18
-    
-                encoded
-          end
-    
-                res = checksum + encrypted
-            res
-          end
-        end
+          it 'handles the exclude_dirs parameter with multiple  elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: ['.bundle', 'Packages/'])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --exclude-dir .bundle --exclude-dir Packages/')
       end
     end
   end
-end
-    
-              # Rex::Proto::Kerberos::Model::AuthorizationData decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'Authorization Data decoding not supported'
-          end
-    
-              # Decodes the last_req from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Array<Rex::Proto::Kerberos::Model::LastRequest>]
-          def decode_last_req(input)
-            last_requests = []
-            input.value[0].value.each do |last_request|
-              last_requests << Rex::Proto::Kerberos::Model::LastRequest.decode(last_request)
-            end
-    
-        def check_encoding!
-      return if @checked_encoding
-      @checked_encoding = true
-      @template, @original_encoding = Sass::Util.check_sass_encoding(@template)
-    end
-    
-      gem 'rb-fsevent'
-  gem 'kicker'
-  gem 'awesome_print'
-  gem 'ruby-prof', :platforms => [:ruby]
 end
 
     
-    (allow file-write*
-  (literal
-    '/dev/dtracehelper'
-    '/dev/null'
-  )
-  (regex
-    #'^<%= Pod::Config.instance.project_root %>'
-    #'^<%= Pod::Config.instance.repos_dir %>'
-    #'^/Users/[^.]+/Library/Caches/CocoaPods/*'
-    #'^/dev/tty'
-    #'^/private/var'
-  )
-)
+          it 'allows override of argv[0]' do
+        expect_command(['/usr/local/bin/git', 'git'], 'commit', '-m', 'A message')
+        Fastlane::Actions.sh(['/usr/local/bin/git', 'git'], 'commit', '-m', 'A message')
+      end
     
-        def validate_plugins!
-      @plugins_to_package.each do |plugin_name|
-        if INVALID_PLUGINS_TO_EXPLICIT_PACK.any? { |invalid_name| plugin_name =~ invalid_name }
-          raise UnpackablePluginError, 'Cannot explicitly pack `#{plugin_name}` for offline installation'
+          if data_type == Array
+        return value.split(',') if value.kind_of?(String)
+      elsif data_type == Integer
+        return value.to_i if value.to_i.to_s == value.to_s
+      elsif data_type == Float
+        return value.to_f if value.to_f.to_s == value.to_s
+      elsif allow_shell_conversion
+        return value.shelljoin if value.kind_of?(Array)
+        return value.map { |k, v| '#{k.to_s.shellescape}=#{v.shellescape}' }.join(' ') if value.kind_of?(Hash)
+      elsif data_type != String
+        # Special treatment if the user specified true, false or YES, NO
+        # There is no boolean type, so we just do it here
+        if %w(YES yes true TRUE).include?(value)
+          return true
+        elsif %w(NO no false FALSE).include?(value)
+          return false
         end
       end
+    
+        # An empty argument will be skipped, so return empty quotes.
+    # https://github.com/ruby/ruby/blob/a6413848153e6c37f6b0fea64e3e871460732e34/lib/shellwords.rb#L142-L143
+    return ''''.dup if str.empty?
+    
+            it 'auto converts Hash values to Strings if allowed' do
+          config_item = FastlaneCore::ConfigItem.new(key: :xcargs,
+                                                     description: 'xcargs',
+                                                     type: :shell_string)
+    
+        it 'does not send previously configured receivers when the current agent does not support them' do
+      select_agent_type('Website Agent scrapes')
+      sleep 0.5
+      select2('ZKCD', from: 'Receivers')
+      select_agent_type('Email Agent')
+      fill_in(:agent_name, with: 'No receivers')
+      click_on 'Save'
+      expect(page).to have_content('No receivers')
+      agent = Agent.find_by(name: 'No receivers')
+      expect(agent.receivers).to eq([])
+    end
+  end
+end
+
+    
+      describe 'DotHelper::DotDrawer' do
+    describe '#id' do
+      it 'properly escapes double quotaion and backslash' do
+        expect(DotHelper::DotDrawer.draw(foo: '') {
+          id('hello\\'')
+        }).to eq(''hello\\\\\\''')
+      end
+    end
+  end
+end
+
+    
+    describe JobsHelper do
+  let(:job) { Delayed::Job.new }
+    
+      describe '#style_colors' do
+    it 'returns a css style-formated version of the scenario foreground and background colors' do
+      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
     end
     
-    class LogStash::PluginManager::Unpack < LogStash::PluginManager::PackCommand
-  option '--tgz', :flag, 'unpack a packaged tar.gz file', :default => !LogStash::Environment.windows?
-  option '--zip', :flag, 'unpack a packaged  zip file', :default => LogStash::Environment.windows?
+      context '#set_traps' do
+    it 'sets traps for INT TERM and QUIT' do
+      agent_runner = AgentRunner.new
+      mock(Signal).trap('INT')
+      mock(Signal).trap('TERM')
+      mock(Signal).trap('QUIT')
+      agent_runner.set_traps
     
-          options = {:debug => ENV['LS_QA_DEBUG']}
-      puts 'Destroying #{machines}'
-      LogStash::VagrantHelpers.destroy(machines, options)
-      puts 'Bootstrapping #{machines}'
-      LogStash::VagrantHelpers.bootstrap(machines, options)
+      def guid_order(agent_list, agent_name)
+    agent_list.map{|a|a.guid}.sort.find_index(agents(agent_name).guid)
+  end
+end
+
+    
+        Thread.abort_on_exception = @taoe
+    $stdout, $stderr = @oso, @ose
+  end
+    
+      describe 'converting escaped JSONPath strings' do
+    it 'should work' do
+      expect(LiquidMigrator.convert_string('Weather looks like <$.conditions> according to the forecast at <$.pretty_date.time>')).to eq(
+                                    'Weather looks like {{conditions}} according to the forecast at {{pretty_date.time}}'
+      )
     end
     
-    Given 'I allow the attachment to be submitted' do
-  cd('.') do
-    transform_file('app/controllers/users_controller.rb') do |content|
-      content.gsub('params.require(:user).permit(:name)',
-                   'params.require(:user).permit!')
+      def failure
+    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+    redirect_to after_omniauth_failure_path_for(resource_name)
+  end
+    
+        if resource.errors.empty?
+      set_flash_message! :notice, :unlocked
+      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+    
+        def reset_password_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :reset_password_instructions, opts)
+    end
+    
+    module Devise
+  module Controllers
+    # A module that may be optionally included in a controller in order
+    # to provide remember me behavior. Useful when signing in is done
+    # through a callback, like in OmniAuth.
+    module Rememberable
+      # Return default cookie values retrieved from session options.
+      def self.cookie_values
+        Rails.configuration.session_options.slice(:path, :domain, :secure)
+      end
+    
+        def initialize(name, options) #:nodoc:
+      @scoped_path = options[:as] ? '#{options[:as]}/#{name}' : name.to_s
+      @singular = (options[:singular] || @scoped_path.tr('/', '_').singularize).to_sym
+    
+            # Find a user by its unlock token and try to unlock it.
+        # If no user is found, returns a new user with an error.
+        # If the user is not locked, creates an error for the user
+        # Options must have the unlock_token
+        def unlock_access_by_token(unlock_token)
+          original_token = unlock_token
+          unlock_token   = Devise.token_generator.digest(self, :unlock_token, unlock_token)
+    
+        if other.respond_to?(:to_str)
+      return true if to_str == other.to_str
+    end
+    
+    desc 'Initializes your working copy to run the specs'
+task :bootstrap, :use_bundle_dir? do |_, args|
+  title 'Environment bootstrap'
+    
+            self.arguments = [
+          CLAide::Argument.new('NAME', false),
+        ]
+    
+    ```
+#{plugins_string}
+```
+#{markdown_podfile}
+EOS
+      end
+    
+      it 'denies post requests with wrong X-CSRF-Token header' do
+    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => bad_token)
+    expect(last_response).not_to be_ok
+  end
+    
+      describe '#referrer' do
+    it 'Reads referrer from Referer header' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
+      expect(subject.referrer(env)).to eq('bar.com')
+    end
+    
+      it 'should not override the header if already set' do
+    mock_app with_headers('Content-Security-Policy' => 'default-src: none')
+    expect(get('/', {}, 'wants' => 'text/html').headers['Content-Security-Policy']).to eq('default-src: none')
+  end
+end
+
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
     end
   end
 end
     
-    # The base module that gets included in ActiveRecord::Base. See the
-# documentation for Paperclip::ClassMethods for more useful information.
-module Paperclip
-  extend Helpers
-  extend Logger
-  extend ProcessorHelpers
+        def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
+    
+        def poster
+      'poster='#{@poster}'' if @poster
+    end
