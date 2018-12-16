b57c55e11407f8f671b2c2d8ced8eb29a2a506fc
SUNsung <weatherius@gmail.com>
2018-12-16 05:40:17 +0200

    Rewrite [04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb] File in: 2018-12-16 05:40:16

--- ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
+++ ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
@@ -1,97 +1,212 @@
 
         
-            group.add(moderator)
-    group.save
+        # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
     
-      def select(&block)
-    self.class.new(@paths.select(&block))
+          def feature_element_timing_key(feature_element)
+        '\'#{feature_element.name}\' (#{feature_element.location})'
+      end
+    
+      [
+    Regexp.escape(date),
+    '#{time}:\\d{2}',
+    Regexp.escape(zone),
+  ] \
+    .join('\\ ')
+end
+    
+        context '#run' do
+      before do
+        mock(@agent_runner).run_workers
+      end
+    
+      describe '#hour_to_schedule_name' do
+    it 'for 0h' do
+      expect(@scheduler.send(:hour_to_schedule_name, 0)).to eq('midnight')
+    end
+    
+        def destroy
+      authorize @report_note, :destroy?
+      @report_note.destroy!
+      redirect_to admin_report_path(@report_note.report_id), notice: I18n.t('admin.report_notes.destroyed_msg')
+    end
+    
+      def maxwidth_or_default
+    (params[:maxwidth].presence || 400).to_i
   end
     
-        cmd_paths = PATH.new(ENV['PATH']).append(Tap.cmd_directories) unless path
-    path ||= which('brew-#{cmd}', cmd_paths)
-    path ||= which('brew-#{cmd}.rb', cmd_paths)
+      def hub_secret
+    params['hub.secret']
+  end
     
-          return unless old_path.directory?
+      def future_expires
+    Time.now.utc + lease_seconds_or_default
+  end
     
-        desc 'Release all gems as packages'
-    task :all => [:test, :commit_version] + GEMS_AND_ROOT_DIRECTORIES.keys
+          it 'uses the specified argument as the width if * is followed by a number and $' do
+        format('%1$*2$b', 10, 10).should == '      1010'
+        format('%1$*2$B', 10, 10).should == '      1010'
+        format('%1$*2$d', 112, 10).should == '       112'
+        format('%1$*2$i', 112, 10).should == '       112'
+        format('%1$*2$o', 87, 10).should == '       127'
+        format('%1$*2$u', 112, 10).should == '       112'
+        format('%1$*2$x', 196, 10).should == '        c4'
+        format('%1$*2$X', 196, 10).should == '        C4'
+    
+      platform_is :windows do
+    it 'does expand shell variables when given multiples arguments' do
+      # See https://bugs.ruby-lang.org/issues/12231
+      lambda { @object.system('echo', @shell_var) }.should output_to_fd('foo\n')
+    end
+  end
+    
+        it 'returns the last access time for the provided file when passed ?A' do
+      Kernel.test(?A, @tmp_file).should == @tmp_file.atime
+    end
+    
+      it 'raises ArgumentError if no block or proc is provided' do
+    lambda do
+      trace_var :$Kernel_trace_var_global
+    end.should raise_error(ArgumentError)
   end
 end
 
     
-    post '/' do
-  connections.each { |out| out << 'data: #{params[:msg]}\n\n' }
-  204 # response without entity body
+      def up
+    Photo.joins('INNER JOIN posts ON posts.guid = photos.status_message_guid')
+         .where(posts: {type: 'StatusMessage', public: true}).update_all(public: true)
+    
+      class Notification < ApplicationRecord
+  end
+    
+    Then /^I should see an image in the publisher$/ do
+  photo_in_publisher.should be_present
 end
     
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
+        # @param options [{Symbol => Object}] The options hash. See
+    #   {file:SASS_REFERENCE.md#Options the Sass options documentation}.
+    # @param parent [Environment] See \{#parent}
+    def initialize(parent = nil, options = nil)
+      @parent = parent
+      @options = options || (parent && parent.options) || {}
+      @stack = @parent.nil? ? Sass::Stack.new : nil
+      @caller = nil
+      @content = nil
+      @filename = nil
+      @functions = nil
+      @mixins = nil
+      @selector = nil
+      @vars = nil
+    end
+    
+        class << self
+      # Returns an error report for an exception in CSS format.
+      #
+      # @param e [Exception]
+      # @param line_offset [Integer] The number of the first line of the Sass template.
+      # @return [String] The error report
+      # @raise [Exception] `e`, if the
+      #   {file:SASS_REFERENCE.md#full_exception-option `:full_exception`} option
+      #   is set to false.
+      def exception_to_css(e, line_offset = 1)
+        header = header_string(e, line_offset)
+    
+          # Splits a filename into three parts, a directory part, a basename, and an extension
+      # Only the known extensions returned from the extensions method will be recognized as such.
+      def split(name)
+        extension = nil
+        dirname, basename = File.dirname(name), File.basename(name)
+        if basename =~ /^(.*)\.(#{extensions.keys.map {|e| Regexp.escape(e)}.join('|')})$/
+          basename = $1
+          extension = $2
+        end
+        [dirname, basename, extension]
+      end
+    
+      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
+  gem.executables   = %w(cap capify)
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ['lib']
+    
+    Then(/^references in the remote repo are listed$/) do
+  expect(@output).to include('refs/heads/master')
+end
+    
+    Given(/^a stage file named (.+)$/) do |filename|
+  TestApp.write_local_stage_file(filename)
+end
+    
+      at_exit do
+    if ENV['KEEP_RUNNING']
+      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
+      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
+    else
+      vagrant_cli_command('destroy -f')
     end
   end
     
-          TEMPLATE.result(binding)
-    end
+        def any?(key)
+      value = fetch(key)
+      if value && value.respond_to?(:any?)
+        begin
+          return value.any?
+        rescue ArgumentError # rubocop:disable Lint/HandleExceptions
+          # Gracefully ignore values whose `any?` method doesn't accept 0 args
+        end
+      end
     
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   XSS and others
-    # Supported browsers:: Firefox 23+, Safari 7+, Chrome 25+, Opera 15+
-    #
-    # Description:: Content Security Policy, a mechanism web applications
-    #               can use to mitigate a broad class of content injection
-    #               vulnerabilities, such as cross-site scripting (XSS).
-    #               Content Security Policy is a declarative policy that lets
-    #               the authors (or server administrators) of a web application
-    #               inform the client about the sources from which the
-    #               application expects to load resources.
-    #
-    # More info::   W3C CSP Level 1 : https://www.w3.org/TR/CSP1/ (deprecated)
-    #               W3C CSP Level 2 : https://www.w3.org/TR/CSP2/ (current)
-    #               W3C CSP Level 3 : https://www.w3.org/TR/CSP3/ (draft)
-    #               https://developer.mozilla.org/en-US/docs/Web/Security/CSP
-    #               http://caniuse.com/#search=ContentSecurityPolicy
-    #               http://content-security-policy.com/
-    #               https://securityheaders.io
-    #               https://scotthelme.co.uk/csp-cheat-sheet/
-    #               http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-    #
-    # Sets the 'Content-Security-Policy[-Report-Only]' header.
-    #
-    # Options: ContentSecurityPolicy configuration is a complex topic with
-    #          several levels of support that has evolved over time.
-    #          See the W3C documentation and the links in the more info
-    #          section for CSP usage examples and best practices. The
-    #          CSP3 directives in the 'NO_ARG_DIRECTIVES' constant need to be
-    #          presented in the options hash with a boolean 'true' in order
-    #          to be used in a policy.
-    #
-    class ContentSecurityPolicy < Base
-      default_options default_src: :none, script_src: ''self'',
-                      img_src: ''self'', style_src: ''self'',
-                      connect_src: ''self'', report_only: false
+            if echo?
+          $stdin.gets
+        else
+          $stdin.noecho(&:gets).tap { $stdout.print '\n' }
+        end
+      rescue Errno::EIO
+        # when stdio gets closed
+        return
+      end
     
-          def escape_string(str)
-        str = @escaper.escape_url(str)        if @url
-        str = @escaper.escape_html(str)       if @html
-        str = @escaper.escape_javascript(str) if @javascript
-        str
+        context 'opening brace on same line as first element' do
+      it 'allows closing brace on different line from last element' do
+        expect_no_offenses(construct(false, true))
+      end
+    
+            expect(new_source).to eq(['#{prefix}#{open}#{a},',
+                                  '#{b}#{close}',
+                                  suffix].join($RS))
       end
     end
   end
 end
 
     
-      describe '#referrer' do
-    it 'Reads referrer from Referer header' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
-      expect(subject.referrer(env)).to eq('bar.com')
-    end
+            def purchase
+          perform_payment_action(:purchase)
+        end
+    
+            def update
+          @return_authorization = order.return_authorizations.accessible_by(current_ability, :update).find(params[:id])
+          if @return_authorization.update_attributes(return_authorization_params)
+            respond_with(@return_authorization, default_template: :show)
+          else
+            invalid_resource!(@return_authorization)
+          end
+        end
+    
+              unless @quantity > 0
+            unprocessable_entity('#{Spree.t(:shipment_transfer_errors_occured, scope: 'api')} \n #{Spree.t(:negative_quantity, scope: 'api')}')
+            return
+          end
+    
+            def zone_params
+          attrs = params.require(:zone).permit!
+          if attrs[:zone_members]
+            attrs[:zone_members_attributes] = attrs.delete(:zone_members)
+          end
+          attrs
+        end
