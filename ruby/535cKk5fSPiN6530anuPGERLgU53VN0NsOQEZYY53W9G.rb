f960369f1990f0c72d2d79f4b3d68f45cd93f730
SUNsung <weatherius@gmail.com>
2018-12-17 23:08:50 +0200

    Rewrite [535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb] File in: 2018-12-17 23:08:50

--- ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
+++ ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
@@ -1,182 +1,154 @@
 
         
-          #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
+        describe GivenDailyLike do
     
-      def self.create_ipmi_getchannel_probe
-    [   # Get Channel Authentication Capabilities
-      0x06, 0x00, 0xff, 0x07, # RMCP Header
-      0x00, 0x00, 0x00, 0x00,
-      0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x20, 0x18,
-      0xc8, 0x81, 0x00, 0x38, 0x8e, 0x04, 0xb5
-    ].pack('C*')
-  end
+      it { is_expected.to belong_to :user }
+  it { is_expected.to belong_to :topic }
     
-              # Encodes the realm field
-          #
-          # @return [String]
-          def encode_realm
-            encoded = ''
-            encoded << [realm.length].pack('N')
-            encoded << realm
+          if staff.topic_id.nil?
+        creator = PostCreator.new(Discourse.system_user,
+          raw: I18n.t('staff_category_description'),
+          title: I18n.t('category.topic_prefix', category: staff.name),
+          category: staff.name,
+          archetype: Archetype.default
+        )
+        post = creator.create
     
-                k3 = OpenSSL::HMAC.digest('MD5', k1, checksum)
+      it 'shows the dry run pop up without previous events and selects the log tab when no event was created' do
+    stub_request(:get, 'http://xkcd.com/').
+      with(:headers => {'Accept-Encoding'=>'gzip,deflate', 'User-Agent'=>'Huginn - https://github.com/huginn/huginn'}).
+      to_return(:status => 200, :body => '', :headers => {})
     
-              # Encodes the Rex::Proto::Kerberos::Model::ApReq into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            elems = []
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_pvno], 0, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_msg_type], 1, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_options], 2, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_ticket], 3, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_authenticator], 4, :CONTEXT_SPECIFIC)
-            seq = OpenSSL::ASN1::Sequence.new(elems)
-    
-              # Decodes the srealm field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_srealm(input)
-            input.value[0].value
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::KdcResponse from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-          if rails?
-        register_rails_engine
-      elsif lotus?
-        register_lotus
-      elsif sprockets?
-        register_sprockets
-      end
-    
-      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
-    @logger     = Logger.new
-    @repo       = repo
-    @branch     = branch || 'master'
-    @branch_sha = get_branch_sha
-    @cache_path = cache_path
-    @repo_url   = 'https://github.com/#@repo'
-    @save_to    = {
-        js:    'assets/javascripts/bootstrap',
-        scss:  'assets/stylesheets/bootstrap',
-        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
-  end
-    
-          # generate variables template
-      save_file 'templates/project/_bootstrap-variables.sass',
-                '// Override Bootstrap variables here (defaults from bootstrap-sass v#{Bootstrap::VERSION}):\n\n' +
-                    File.read('#{save_to}/_variables.scss').lines[1..-1].join.gsub(/^(?=\$)/, '// ').gsub(/ !default;/, '')
+      describe '#omniauth_provider_icon' do
+    it 'returns a correct icon tag for Twitter' do
+      icon = omniauth_provider_icon(:twitter)
+      expect(icon).to be_html_safe
+      elem = Nokogiri(icon).at('i.fa.fa-twitter')
+      expect(elem).to be_a Nokogiri::XML::Element
     end
     
-    # ECMA-262, section 15.1.3
-    def Encode(uri, unescape)
-      uriLength = uri.length;
-      # We are going to pass result to %StringFromCharCodeArray
-      # which does not expect any getters/setters installed
-      # on the incoming array.
-      result    = Array.new(uriLength);
-      index = 0;
-      k = -1;
-      while ((k+=1) < uriLength) do
-        cc1 = uri.charCodeAt(k);
-        next if cc1.nil?
-        if (self.send(unescape, cc1))
-          result[index] = cc1;
-          index += 1
-        else
-          if (cc1 >= 0xDC00 && cc1 <= 0xDFFF);
-            throw('URI malformed')
-          end
-          if (cc1 < 0xD800 || cc1 > 0xDBFF)
-            index = URIEncodeSingle(cc1, result, index);
-          else
-            k+=1;
-            if (k == uriLength);
-              throw('URI malformed')
-            end
-            cc2 = uri.charCodeAt(k);
-            if (cc2 < 0xDC00 || cc2 > 0xDFFF);
-              throw('URI malformed')
-            end
-            index = URIEncodePair(cc1, cc2, result, index);
-          end
-        end
+      describe 'DotHelper::DotDrawer' do
+    describe '#id' do
+      it 'properly escapes double quotaion and backslash' do
+        expect(DotHelper::DotDrawer.draw(foo: '') {
+          id('hello\\'')
+        }).to eq(''hello\\\\\\''')
       end
-      # use .compact to get rid of nils from charCodeAt
-      # return %StringFromCharCodeArray(result);
-      # 'c' = 8 bit signed char
-      # http://www.ruby-doc.org/core-1.9.3/Array.html#method-i-pack
-      return result.compact.pack 'c*'
     end
-  end # class << self
-end # module
+  end
+end
 
     
-          def string_to_code string
-        # sha bytes
-        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
-        # Thanks donpark's IdenticonUtil.java for this.
-        # Match the following Java code
-        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
-        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
-    
-          def _identicon_code(blob)
-        string_to_code blob + @request.host
-      end
-    
-          def sidebar_format
-        has_sidebar && @sidebar.format.to_s
-      end
-    
-      test 'h1 title can be disabled' do
-    title = 'H1'
-    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
-    page = @wiki.page(title)
-    
-    def date
-  Date.today.to_s
-end
-    
-      s.name              = 'gollum'
-  s.version           = '4.1.4'
-  s.date              = '2018-10-01'
-  s.rubyforge_project = 'gollum'
-  s.license           = 'MIT'
-    
-          # Install the gems to make them available locally when bundler does his local resolution
-      post_install_messages = []
-      pack.gems.each do |packed_gem|
-        PluginManager.ui.debug('Installing, #{packed_gem.name}, version: #{packed_gem.version} file: #{packed_gem.file}')
-        post_install_messages << LogStash::PluginManager::GemInstaller::install(packed_gem.file, packed_gem.plugin?)
-      end
-    
-        FileUtils.rm_rf(LogStash::Environment::CACHE_PATH)
-    validate_cache_location
-    archive_manager.extract(package_file, LogStash::Environment::CACHE_PATH)
-    puts('Unpacked at #{LogStash::Environment::CACHE_PATH}')
-    puts('The unpacked plugins can now be installed in local-only mode using bin/logstash-plugin install --local [plugin name]')
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
   end
     
-          puts user_feedback_string_for('halting', args[:platform], machines, {'experimental' => experimental})
-      options = {:debug => ENV['LS_QA_DEBUG']}
+        context 'when an a scenario already exists with the given guid for the importing user' do
+      let!(:existing_scenario) do
+        _existing_scenerio = users(:bob).scenarios.build(:name => 'an existing scenario', :description => 'something')
+        _existing_scenerio.guid = guid
+        _existing_scenerio.save!
     
-              it 'fails when installing a non logstash plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
-            expect(command).not_to install_successfully
+      describe 'cleanup_failed_jobs!' do
+    before do
+      3.times do |i|
+        Delayed::Job.create(failed_at: Time.now - i.minutes)
+      end
+      @keep = Delayed::Job.order(:failed_at)[1]
+    end
+    
+      it 'provides hash-style access to its properties with both symbol and string keys' do
+    expect(location[:lat]).to be_a Float
+    expect(location[:lat]).to eq 2.0
+    expect(location['lat']).to be_a Float
+    expect(location['lat']).to eq 2.0
+  end
+    
+        # If a procedure was passed, mount the resource with it.
+    if (opts['Proc'])
+      mount(name, Handler::Proc, false, opts['Proc'], opts['VirtualDirectory'])
+    else
+      raise ArgumentError, 'You must specify a procedure.'
+    end
+  end
+    
+    
+end
+end
+end
+
+    
+    
+end
+end
+end
+
+    
+      # open rmcpplus_request
+  def self.create_ipmi_session_open_request(console_session_id)
+    head = [
+      0x06, 0x00, 0xff, 0x07,   # RMCP Header
+      0x06,                     # RMCP+ Authentication Type
+      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
+      0x00, 0x00, 0x00, 0x00,   # Session ID
+      0x00, 0x00, 0x00, 0x00    # Sequence Number
+    ].pack('C*')
+    
+              # Encodes the pvno field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_pvno
+            bn = OpenSSL::BN.new(pvno.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+              # @!attribute type
+          #   @return [Integer] The algorithm used to generate the checksum
+          attr_accessor :type
+          # @!attribute checksum
+          #   @return [String] The checksum itself
+          attr_accessor :checksum
+    
+              include Rex::Proto::Kerberos::Crypto
+          include Rex::Proto::Kerberos::Model
+    
+              # Decodes the key from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [EncryptionKey]
+          def decode_key(input)
+            Rex::Proto::Kerberos::Model::EncryptionKey.decode(input.value[0])
           end
+    
+              # Decodes the msg_type from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_msg_type(input)
+            input.value[0].value.to_i
+          end
+    
+        context 'but no comment after the last element' do
+      it 'autocorrects the closing brace' do
+        new_source = autocorrect_source(source)
+    
+          # Checks whether this node is an `unless` statement. (This is not true
+      # of ternary operators and `if` statements.)
+      #
+      # @return [Boolean] whether the node is an `unless` statement
+      def unless?
+        keyword == 'unless'
+      end
+    
+          # This is used for duck typing with `pair` nodes which also appear as
+      # `hash` elements.
+      #
+      # @return [false]
+      def colon?
+        false
+      end
+    
+              raise ArgumentError unless valid_argument_types?
+        end
