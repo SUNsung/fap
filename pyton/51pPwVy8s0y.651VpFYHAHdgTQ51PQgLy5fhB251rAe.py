780d8b787da36d4a4cead7cdcdb0df6191af7be0
SUNsung <weatherius@gmail.com>
2018-12-17 09:52:33 +0200

    Rewrite [51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py] File in: 2018-12-17 09:52:32

--- pyton/51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py
+++ pyton/51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py
@@ -1,99 +1,60 @@
 
         
-        
-class HasKeys(PostgresSimpleLookup):
-    lookup_name = 'has_keys'
-    operator = '?&'
+        def _iter_command_classes(module_name):
+    # TODO: add `name` attribute to commands and and merge this function with
+    # scrapy.utils.spider.iter_spider_classes
+    for module in walk_modules(module_name):
+        for obj in vars(module).values():
+            if inspect.isclass(obj) and \
+                    issubclass(obj, ScrapyCommand) and \
+                    obj.__module__ == module.__name__ and \
+                    not obj == ScrapyCommand:
+                yield obj
     
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('--get', dest='get', metavar='SETTING',
+            help='print raw setting value')
+        parser.add_option('--getbool', dest='getbool', metavar='SETTING',
+            help='print setting value, interpreted as a boolean')
+        parser.add_option('--getint', dest='getint', metavar='SETTING',
+            help='print setting value, interpreted as an integer')
+        parser.add_option('--getfloat', dest='getfloat', metavar='SETTING',
+            help='print setting value, interpreted as a float')
+        parser.add_option('--getlist', dest='getlist', metavar='SETTING',
+            help='print setting value, interpreted as a list')
     
-class RangeMinValueValidator(MinValueValidator):
-    def compare(self, a, b):
-        return a.lower is None or a.lower < b
-    message = _('Ensure that this range is completely greater than or equal to %(limit_value)s.')
-
+        def long_desc(self):
+        return 'Interactive console for scraping the given url'
     
         @property
-    def cache_key(self):
-        return self.cache_key_prefix + self._get_or_create_session_key()
+    def filename(self):
+        return self.__filename
     
-        def exists(self, session_key=None):
-        '''
-        This method makes sense when you're talking to a shared resource, but
-        it doesn't matter when you're storing the information in the client's
-        cookie.
-        '''
-        return False
+        def get_sni_addr(self, port):
+        '''Returns the least specific address that resolves on the port.
     
-        def __call__(self, func):
-        # Note that we are intentionally not using @wraps here for performance
-        # reasons. Refs #21109.
-        def inner(*args, **kwargs):
-            with self:
-                return func(*args, **kwargs)
-        return inner
+    # Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#html_theme_options = {}
     
+        def test_3rd_am_station_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1510'
+        self.assertEqual(station, expected_station)
     
-def l2(l=0.01):
-    return L1L2(l2=l)
+    '''
+*TL;DR80
+Encapsulates all information needed to perform an action or trigger an event.
+'''
     
-        Also works from TensorFlow to Theano.
+    *TL;DR80
+Separates an algorithm from an object structure on which it operates.
+'''
     
-    
-allobj = [losses.mean_squared_error,
-          losses.mean_absolute_error,
-          losses.mean_absolute_percentage_error,
-          losses.mean_squared_logarithmic_error,
-          losses.squared_hinge,
-          losses.hinge,
-          losses.categorical_crossentropy,
-          losses.binary_crossentropy,
-          losses.kullback_leibler_divergence,
-          losses.poisson,
-          losses.cosine_proximity,
-          losses.logcosh,
-          losses.categorical_hinge]
-    
-            # Arguments
-          inputs: Single array, or list of arrays. The arrays could be placeholders,
-            Numpy arrays, or data tensors.
-            - if placeholders: the model is built on top of these placeholders,
-              and we expect Numpy data to be fed for them when calling `fit`/etc.
-            - if Numpy data: we create placeholders matching the shape of the Numpy
-              arrays. We expect Numpy data to be fed for these placeholders
-              when calling `fit`/etc.
-            - if data tensors: the model is built on top of these tensors.
-              We do not expect any Numpy data to be provided when calling `fit`/etc.
-          outputs: Optional output tensors (if already computed by running
-            the model).
-          training: Boolean or None. Only relevant in symbolic mode. Specifies
-            whether to build the model's graph in inference mode (False), training
-            mode (True), or using the Keras learning phase (None).
-        '''
-        if self.__class__.__name__ == 'Sequential':
-            # Note: we can't test whether the model
-            # is `Sequential` via `isinstance`
-            # since `Sequential` depends on `Model`.
-            if isinstance(inputs, list):
-                assert len(inputs) == 1
-                inputs = inputs[0]
-            self.build(input_shape=(None,) + inputs.shape[1:])
-            return
-    
-    import numpy as np
-from scipy.sparse import issparse
-    
-        def build(self, input_shape=None):
-        if input_shape and not self.inputs:
-            batch_shape = tuple(input_shape)
-            dtype = K.floatx()
-            x = Input(batch_shape=batch_shape,
-                      dtype=dtype,
-                      name=self.name + '_input')
-            self.inputs = [x]
-            for layer in self._layers:
-                x = layer(x)
-            self.outputs = [x]
-            self._build_input_shape = input_shape
-    
-        proxy_server = simple_http_server.HTTPServer(
-        (listen_ip, front.config.listen_port), proxy_handler.GAEProxyHandler, logger=xlog)
+        def __exit__(self, Type, value, traceback):
+        if self.item is not None:
+            self._queue.put(self.item)
+            self.item = None
