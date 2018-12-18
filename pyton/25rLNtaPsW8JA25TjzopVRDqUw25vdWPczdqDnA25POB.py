fb112b7f00de80e226cc25545bfd72b0f43bd1fc
SUNsung <weatherius@gmail.com>
2018-12-18 20:28:54 +0200

    Rewrite [25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py] File in: 2018-12-18 20:28:54

--- pyton/25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py
+++ pyton/25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py
@@ -1,114 +1,198 @@
 
         
-        def get_extension(link):
-    extension = os.path.splitext(link)[1][1:]
-    if extension in ['pdf', 'html']:
-        return extension
-    if 'pdf' in extension:
-        return 'pdf'    
-    return 'pdf'    
+                    k_results_build[algorithm][i] = (t1 - t0)
+            k_results_query[algorithm][i] = (t2 - t1)
     
-        publicKey = (n, e)
-    privateKey = (n, d)
-    return (publicKey, privateKey)
+        plt.subplots_adjust(0.1, 0.08, 0.96, 0.98, 0.4, 0.63)
+    ax = plt.axes([0.1, 0.08, 0.8, 0.06])
+    plt.colorbar(cax=ax, orientation='horizontal')
+    plt.show()
+
     
-    from .hash_table import HashTable
+        ###########################################################################
+    # Set custom reservoir based method
+    sampling_algorithm['custom-pool'] = \
+        lambda n_population, n_samples, random_state=None: \
+            sample_without_replacement(n_population,
+                                       n_samples,
+                                       method='pool',
+                                       random_state=random_state)
     
-        def __init__(self):
-        self.__prepare__()
+        plt.show()
+
     
-        def solve_sub_array(self):
-        rear = [int(self.array[0])]*len(self.array)
-        sum_value = [int(self.array[0])]*len(self.array)
-        for i in range(1, len(self.array)):
-            sum_value[i] = max(int(self.array[i]) + sum_value[i-1], int(self.array[i]))
-            rear[i] = max(sum_value[i], rear[i-1])
-        return rear[len(self.array)-1]
+    input data sparsity: 0.050000
+true coef sparsity: 0.000100
+test data sparsity: 0.027400
+model sparsity: 0.000024
+r^2 on test data (dense model) : 0.233651
+r^2 on test data (sparse model) : 0.233651
+Wrote profile results to sparsity_benchmark.py.lprof
+Timer unit: 1e-06 s
     
-        test_cases = (np.random.rand(10) * 2) - 1
-    predictions = np.array([tree.predict(x) for x in test_cases])
-    avg_error = np.mean((predictions - test_cases) ** 2)
+        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
+                                   'blob/{revision}/{package}/'
+                                   '{path}#L{lineno}')
+    '''
+    revision = _get_git_revision()
+    return partial(_linkcode_resolve, revision=revision, package=package,
+                   url_fmt=url_fmt)
+
     
-        def get_actiontec_data(self):
-        '''Retrieve data from Actiontec MI424WR and return parsed result.'''
+        short_text_lang_folder = os.path.join(short_text_folder, lang)
+    if not os.path.exists(short_text_lang_folder):
+        os.makedirs(short_text_lang_folder)
+    
+        def test_nested_exception(self):
+        def f(p):
+            1/0
+        f_ident = ident(f)
+        self.check_events(f, [(1, 'call', f_ident),
+                              # This isn't what I expected:
+                              # (0, 'exception', protect_ident),
+                              # I expected this again:
+                              (1, 'return', f_ident),
+                              ])
+    
+            # Faulthandler
         try:
-            telnet = telnetlib.Telnet(self.host)
-            telnet.read_until(b'Username: ')
-            telnet.write((self.username + '\n').encode('ascii'))
-            telnet.read_until(b'Password: ')
-            telnet.write((self.password + '\n').encode('ascii'))
-            prompt = telnet.read_until(
-                b'Wireless Broadband Router> ').split(b'\n')[-1]
-            telnet.write('firewall mac_cache_dump\n'.encode('ascii'))
-            telnet.write('\n'.encode('ascii'))
-            telnet.read_until(prompt)
-            leases_result = telnet.read_until(prompt).split(b'\n')[1:-1]
-            telnet.write('exit\n'.encode('ascii'))
-        except EOFError:
-            _LOGGER.exception('Unexpected response from router')
-            return
-        except ConnectionRefusedError:
-            _LOGGER.exception('Connection refused by router. Telnet enabled?')
-            return None
-    
-    For more details about this platform, please refer to the documentation
-https://home-assistant.io/components/demo/
-'''
-import random
-    
-    DEFAULT_TIMEOUT = 10
-    
-    ATTR_COUNTRY = 'country_code'
-ATTR_FIRST_NAME = 'first_name'
-ATTR_LAST_NAME = 'last_name'
-ATTR_EMAIL = 'email'
-ATTR_PHONE = 'phone'
-ATTR_ADDRESS = 'address'
-ATTR_ORDERS = 'orders'
-ATTR_SHOW_MENU = 'show_menu'
-ATTR_ORDER_ENTITY = 'order_entity_id'
-ATTR_ORDER_NAME = 'name'
-ATTR_ORDER_CODES = 'codes'
-    
-    
-def setup(hass, config):
-    '''Set up the folder watcher.'''
-    conf = config[DOMAIN]
-    for watcher in conf:
-        path = watcher[CONF_FOLDER]
-        patterns = watcher[CONF_PATTERNS]
-        if not hass.config.is_allowed_path(path):
-            _LOGGER.error('folder %s is not valid or allowed', path)
-            return False
-        Watcher(path, patterns, hass)
-    
-        # verify that none of the target files exist
-    assert not lexists('foo.txt')
-    assert not lexists('bar.txt')
-    assert not lexists('baz.txt')
-    try:
-        with open('foo.txt', 'w'):  # Creating the file
+            import faulthandler
+        except ImportError:
             pass
+        else:
+            code = 'import faulthandler; print(faulthandler.is_enabled())'
+            out = self.run_xdev('-c', code)
+            self.assertEqual(out, 'True')
     
-    print('Counting to two...')
-for number in count_to_two():
-    print(number, end=' ')
+        print('Pickled records:')
+    pprint.pprint(memos)
     
-        def __init__(self, method):
-        self.method = method
+    # Add the html version.  This converts the message into a multipart/alternative
+# container, with the original text message as the first part and the new html
+# message as the second part.
+asparagus_cid = make_msgid()
+msg.add_alternative('''\
+<html>
+  <head></head>
+  <body>
+    <p>Salut!</p>
+    <p>Cela ressemble à un excellent
+        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
+            recipie
+        </a> déjeuner.
+    </p>
+    <img src='cid:{asparagus_cid}' />
+  </body>
+</html>
+'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
+# note that we needed to peel the <> off the msgid for use in the html.
     
-            if start == end:
-            return path
-        shortest = None
-        for node in self.graph.get(start, []):
-            if node not in path:
-                newpath = self.find_shortest_path(node, end, path[:])
-                if newpath:
-                    if not shortest or len(newpath) < len(shortest):
-                        shortest = newpath
-        return shortest
+    buffer = ''
     
-        def on_diagnostics_passed(self):
-        super(Suspect, self).send_diagnostics_pass_report()
-        super(Suspect, self).clear_alarm()  # loss of redundancy alarm
-        super(Suspect, self).next_state('standby')
+    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
+    host = r1(r'host=([^&]*)', html)
+    assert host
+    type = r1(r'videofiletype=([^&]*)', html)
+    assert type
+    vid = r1(r'&stream_name=([^&]*)', html)
+    assert vid
+    dir_str = r1(r'&dir=([^&]*)', html).strip()
+    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
+    _, ext, size = url_info(url)
+    print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+    
+    def extract(self, **kwargs):
+        for i in self.streams:
+            s = self.streams[i]
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
+        if 'stream_id' in kwargs and kwargs['stream_id']:
+            # Extract the stream
+            stream_id = kwargs['stream_id']
+    
+    __all__ = ['cbs_download']
+    
+    from .bilibili import bilibili_download
+from .dailymotion import dailymotion_download
+from .iqiyi import iqiyi_download_by_vid
+from .le import letvcloud_download_by_vu
+from .netease import netease_download
+from .qq import qq_download_by_vid
+from .sina import sina_download_by_vid
+from .tudou import tudou_download_by_id
+from .vimeo import vimeo_download_by_id
+from .yinyuetai import yinyuetai_download_by_id
+from .youku import youku_download_by_vid
+from . import iqiyi
+from . import bokecc
+    
+        @classmethod
+    def funshion_decrypt(cls, a_bytes, coeff):
+        res_list = []
+        pos = 0
+        while pos < len(a_bytes):
+            a = a_bytes[pos]
+            if pos == len(a_bytes) - 1:
+                res_list.append(a)
+                pos += 1
+            else:
+                b = a_bytes[pos + 1]
+                m = a * coeff[0] + b * coeff[2]
+                n = a * coeff[1] + b * coeff[3]
+                res_list.append(m & 0xff)
+                res_list.append(n & 0xff)
+                pos += 2
+        return bytes(res_list).decode('utf8')
+    
+    __all__ = ['giphy_download']
+    
+                post_url = r1(r''(https://plus.google.com/[^/]+/posts/[^']*)'', html)
+            post_author = r1(r'/\+([^/]+)/posts', post_url)
+            if post_author:
+                post_url = 'https://plus.google.com/+%s/posts/%s' % (parse.quote(post_author), r1(r'posts/(.+)', post_url))
+            post_html = get_html(post_url, faker=True)
+            title = r1(r'<title[^>]*>([^<\n]+)', post_html)
+    
+    *TL;DR80
+Describes a group of objects that is treated as a single instance.
+'''
+    
+        client1.delegate(requests)
+    print('-' * 30)
+    client2.delegate(requests)
+    
+    print()
+    
+    ### OUTPUT ###
+# Setting Data 1 = 10
+# DecimalViewer: Subject Data 1 has data 10
+# HexViewer: Subject Data 1 has data 0xa
+# Setting Data 2 = 15
+# HexViewer: Subject Data 2 has data 0xf
+# DecimalViewer: Subject Data 2 has data 15
+# Setting Data 1 = 3
+# DecimalViewer: Subject Data 1 has data 3
+# HexViewer: Subject Data 1 has data 0x3
+# Setting Data 2 = 5
+# HexViewer: Subject Data 2 has data 0x5
+# DecimalViewer: Subject Data 2 has data 5
+# Detach HexViewer from data1 and data2.
+# Setting Data 1 = 10
+# DecimalViewer: Subject Data 1 has data 10
+# Setting Data 2 = 15
+# DecimalViewer: Subject Data 2 has data 15
+
+    
+    *References:
+http://stackoverflow.com/questions/1514120/python-implementation-of-the-object-pool-design-pattern
+https://sourcemaking.com/design_patterns/object_pool
+    
+    
+class Student(AbstractExpert):
+    @property
+    def is_eager_to_contribute(self):
+        return True
