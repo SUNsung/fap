a9d2460f1333d0d75a4c1bc32b7b5b4eec0f7570
SUNsung <weatherius@gmail.com>
2018-12-18 02:32:30 +0200

    Rewrite [49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb] File in: 2018-12-18 02:32:30

--- ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
+++ ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
@@ -1,67 +1,116 @@
 
         
-        def bottle_file_outdated?(f, file)
-  filename = file.basename.to_s
-  return unless f.bottle && filename.match(Pathname::BOTTLE_EXTNAME_RX)
-    
-      def observe_file_removal(path)
-    path.extend(ObserverPathnameExtension).unlink if path.exist?
-  end
-    
-        def self.cleanup_logs
-      return unless HOMEBREW_LOGS.directory?
-      HOMEBREW_LOGS.subdirs.each do |dir|
-        cleanup_path(dir) { dir.rmtree } if prune?(dir, :days_default => 14)
-      end
-    end
-    
-        # Unbrewed uses the PREFIX, which will exist
-    # Things below use the CELLAR, which doesn't until the first formula is installed.
-    unless HOMEBREW_CELLAR.exist?
-      raise NoSuchKegError.new(ARGV.named.first) if ARGV.named.any?
-      return
-    end
-    
-      def self.canonical_name(name)
-    Formulary.canonical_name(name)
-  end
-    
-          @launch_event_sent = true
-      builder = AnalyticsEventBuilder.new(
-        p_hash: launch_context.p_hash,
-        session_id: session_id,
-        action_name: nil,
-        fastlane_client_language: launch_context.fastlane_client_language
-      )
-    
-          it 'adds docset_bundle_name param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            docset_bundle_name: 'Bundle name'
-          )
-        end').runner.execute(:test)
-    
-            Fastlane::FastFile.new.parse('lane :test do
-          import_certificate ({
-            keychain_name: '#{keychain}',
-            certificate_path: '#{cert_name}',
-            certificate_password: '#{password}',
-            log_output: true
-          })
-        end').runner.execute(:test)
-      end
+            it 'works for queued jobs' do
+      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
     end
   end
-end
-
     
-            expect(result).to include(' -rule #{rule.shellescape} ')
-        expect(result).to include(' -disable-rule #{rule.shellescape} ')
-      end
+              # Already exists
+          expect(weather_agent_diff.agent).to eq(agents(:bob_weather_agent))
+          valid_parsed_weather_agent_data.each do |key, value|
+            next if key == :type
+            expect(weather_agent_diff.send(key).current).to eq(agents(:bob_weather_agent).send(key))
+          end
     
-            FastlaneCore::CertChecker.installed_identies
-      end
+        it 'should work with nested arrays' do
+      @agent.options['array'] = ['one', '$.two']
+      LiquidMigrator.convert_all_agent_options(@agent)
+      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'array' => ['one', '{{two}}'], 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
     end
+    
+      it 'converts values to Float' do
+    expect(location.lat).to be_a Float
+    expect(location.lat).to eq 2.0
+    expect(location.lng).to be_a Float
+    expect(location.lng).to eq 3.0
+    expect(location.radius).to be_a Float
+    expect(location.radius).to eq 300.0
+    expect(location.speed).to be_a Float
+    expect(location.speed).to eq 2.0
+    expect(location.course).to be_a Float
+    expect(location.course).to eq 30.0
+  end
+    
+        it 'requires a valid log level' do
+      @log.level = nil
+      expect(@log).not_to be_valid
+      expect(@log).to have(1).error_on(:level)
+    
+        ActiveSupport.on_load(:action_view) do
+      include scope::UrlHelpers
+    end
+  end
+    
+            if options[:bypass]
+          ActiveSupport::Deprecation.warn(<<-DEPRECATION.strip_heredoc, caller)
+          [Devise] bypass option is deprecated and it will be removed in future version of Devise.
+          Please use bypass_sign_in method instead.
+          Example:
+    
+          protected
+    
+                if class_mod.respond_to?(:available_configs)
+              available_configs = class_mod.available_configs
+              available_configs.each do |config|
+                next unless options.key?(config)
+                send(:'#{config}=', options.delete(config))
+              end
+            end
+          end
+    
+          def run
+        UI.puts report
+      end
+    
+        file package(gem, '.tar.gz') => ['pkg/'] do |f|
+      sh <<-SH
+        git archive \
+          --prefix=#{gem}-#{source_version}/ \
+          --format=tar \
+          HEAD -- #{directory} | gzip > #{f.name}
+      SH
+    end
+  end
+    
+    desc 'generate gemspec'
+task 'rack-protection.gemspec' do
+  require 'rack/protection/version'
+  content = File.binread 'rack-protection.gemspec'
+    
+          def warn(env, message)
+        return unless options[:logging]
+        l = options[:logger] || env['rack.logger'] || ::Logger.new(env['rack.errors'])
+        l.warn(message)
+      end
+    
+          def remove_bad_cookies(request, response)
+        return if bad_cookies.empty?
+        paths = cookie_paths(request.path)
+        bad_cookies.each do |name|
+          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
+        end
+      end
+    
+          def base_url
+        @base_url
+      end
+    
+          def content
+        content_without_page_header(@content)
+      end
+    
+        get page
+    expected = '<h2><a class=\'anchor\' (href|id)=\'(#)?#{text}\' (href|id)=\'(#)?#{text}\'><i class=\'fa fa-link\'></i></a>#{text}</h2>'
+    actual   = nfd(last_response.body)
+    
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
+  end
+    
+      teardown do
+    FileUtils.rm_rf(@path)
+  end
