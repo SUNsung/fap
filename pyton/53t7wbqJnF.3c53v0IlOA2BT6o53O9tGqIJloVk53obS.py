a29f5f6e4ed7aa07e639f4fcfeb9acf57c396d8f
SUNsung <weatherius@gmail.com>
2018-12-19 01:27:47 +0200

    Rewrite [53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py] File in: 2018-12-19 01:27:46

--- pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
+++ pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
@@ -1,147 +1,58 @@
 
         
-            return render_template('auth/register.html')
+            def __init__(self, cards):
+        self.cards = cards
+    
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
     
-def get_db():
-    '''Connect to the application's configured database. The connection
-    is unique for each request and will be reused if this is called
-    again.
-    '''
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+def get_response(args, config_dir):
+    '''Send the request and return a `request.Response`.'''
     
-        def from_envvar(self, variable_name, silent=False):
-        '''Loads a configuration from an environment variable pointing to
-        a configuration file.  This is basically just a shortcut with nicer
-        error messages for this line of code::
-    
-        for idx, (loader, srcobj, triple) in enumerate(attempts):
-        if isinstance(srcobj, Flask):
-            src_info = 'application '%s'' % srcobj.import_name
-        elif isinstance(srcobj, Blueprint):
-            src_info = 'blueprint '%s' (%s)' % (srcobj.name,
-                                                srcobj.import_name)
-        else:
-            src_info = repr(srcobj)
-    
-    This typically means that you attempted to use functionality that needed
-an active HTTP request.  Consult the documentation on testing for
-information about how to avoid this problem.\
-'''
-_app_ctx_err_msg = '''\
-Working outside of application context.
-    
-    signals_available = False
-try:
-    from blinker import Namespace
-    signals_available = True
-except ImportError:
-    class Namespace(object):
-        def signal(self, name, doc=None):
-            return _FakeSignal(name, doc)
-    
-    from jinja2 import BaseLoader, Environment as BaseEnvironment, \
-     TemplateNotFound
-    
-        def __init__(self, keys, strict=False, messages=None):
-        self.keys = set(keys)
-        self.strict = strict
-        if messages is not None:
-            self.messages = {**self.messages, **messages}
-    
-        def cycle_key(self):
-        '''
-        Keep the same data but with a new key. Call save() and it will
-        automatically save a cookie with a new key at the end of the request.
-        '''
-        self.save()
+        def get_auth_plugin(self, auth_type):
+        return self.get_auth_plugin_mapping()[auth_type]
     
     
-@x_robots_tag
-def sitemap(request, sitemaps, section=None,
-            template_name='sitemap.xml', content_type='application/xml'):
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
     
-    IPv6 = CheckNetwork('IPv6')
-IPv6.urls = ['http://[2001:41d0:8:e8ad::1]',
-             'http://[2001:260:401:372::5f]',
-             'http://[2a02:188:3e00::32]',
-             'http://[2804:10:4068::202:82]'
-             ]
-IPv6.triger_check_network()
     
-    from xlog import getLogger
-xlog = getLogger('gae_proxy')
+tests_require = [
+    # Pytest needs to come last.
+    # https://bitbucket.org/pypa/setuptools/issue/196/
+    'pytest-httpbin',
+    'pytest',
+    'mock',
+]
     
-            self.error(nvae)
-        raise nvae
+        def test_download_interrupted(self, httpbin_both):
+        devnull = open(os.devnull, 'w')
+        downloader = Downloader(output_file=devnull, progress_file=devnull)
+        downloader.start(Response(
+            url=httpbin_both.url + '/',
+            headers={'Content-Length': 5}
+        ))
+        downloader.chunk_downloaded(b'1234')
+        downloader.finish()
+        assert downloader.interrupted
+
     
-            Using setter/getter methods is deprecated. Use o.type instead.'''
     
-        # Load image file and find face locations
-    X_img = face_recognition.load_image_file(X_img_path)
-    X_face_locations = face_recognition.face_locations(X_img)
+@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
+def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
+    r = http(follow_flag, httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 1
+    assert HTTP_OK in r
     
-    # Find all the faces in the image using a pre-trained convolutional neural network.
-# This method is more accurate than the default HOG model, but it's slower
-# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
-# this will use GPU acceleration and perform well.
-# See also: find_faces_in_picture.py
-face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
-    
-        # Bail out when the video file ends
-    if not ret:
-        break
-    
-    with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-    
-        # 图片上传失败，输出以下html代码
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
-    
-    _LOGGER = logging.getLogger(__name__)
-    
-        def get_device_name(self, device):
-        '''Return the name of the given device or None if we don't know.'''
-        if self.mac2name is None:
-            result = self._retrieve_list_with_retry()
-            if result:
-                hosts = [x for x in result
-                         if 'mac' in x and 'name' in x]
-                mac2name_list = [
-                    (x['mac'].upper(), x['name']) for x in hosts]
-                self.mac2name = dict(mac2name_list)
-            else:
-                # Error, handled in the _retrieve_list_with_retry
-                return
-        return self.mac2name.get(device.upper(), None)
-    
-                        _LOGGER.debug('Downloading of %s done', url)
-                    hass.bus.fire(
-                        '{}_{}'.format(DOMAIN, DOWNLOAD_COMPLETED_EVENT), {
-                            'url': url,
-                            'filename': filename
-                            })
-    
-    import homeassistant.helpers.config_validation as cv
-from homeassistant.const import (
-    CONF_API_KEY, CONF_WHITELIST, CONF_URL, STATE_UNKNOWN, STATE_UNAVAILABLE,
-    CONF_SCAN_INTERVAL)
-from homeassistant.helpers import state as state_helper
-from homeassistant.helpers.event import track_point_in_time
-from homeassistant.util import dt as dt_util
-    
-    from functools import wraps
-import logging
+        def test_non_existent_file_raises_parse_error(self, httpbin):
+        with pytest.raises(ParseError):
+            http('--form',
+                 'POST', httpbin.url + '/post', 'foo@/__does_not_exist__')
