43db0f4e92f09259bee7bd2cbdc89999d6dc2f7e
SUNsung <weatherius@gmail.com>
2018-12-18 23:55:03 +0200

    Rewrite [25ZVjDwQNCvWc25AMEE8mRYzTU252mvrzMw7eVI250.cpp] File in: 2018-12-18 23:55:02

--- cccccc/25ZVjDwQNCvWc25AMEE8mRYzTU252mvrzMw7eVI250.cpp
+++ cccccc/25ZVjDwQNCvWc25AMEE8mRYzTU252mvrzMw7eVI250.cpp
@@ -1,105 +1,35 @@
 
         
-        template <>
-struct Converter<base::trace_event::TraceConfig> {
-  static bool FromV8(v8::Isolate* isolate,
-                     v8::Local<v8::Value> val,
-                     base::trace_event::TraceConfig* out) {
-    Dictionary options;
-    if (!ConvertFromV8(isolate, val, &options))
-      return false;
-    std::string category_filter, trace_options;
-    if (!options.Get('categoryFilter', &category_filter) ||
-        !options.Get('traceOptions', &trace_options))
-      return false;
-    *out = base::trace_event::TraceConfig(category_filter, trace_options);
-    return true;
-  }
-};
+        /**
+ * \class Predictor
+ *
+ * \brief Performs prediction on individual training instances or batches of
+ * instances for GBTree. The predictor also manages a prediction cache
+ * associated with input matrices. If possible, it will use previously
+ * calculated predictions instead of calculating new predictions.
+ *        Prediction functions all take a GBTreeModel and a DMatrix as input and
+ * output a vector of predictions. The predictor does not modify any state of
+ * the model itself.
+ */
     
-    namespace mate {
-class Arguments;
-class Dictionary;
-}  // namespace mate
     
-    namespace base {
-class FilePath;
+    {template<typename IndexType, typename DType = real_t>
+Parser<IndexType> *
+CreateDenseLibSVMParser(const std::string& path,
+                        const std::map<std::string, std::string>& args,
+                        unsigned part_index,
+                        unsigned num_parts) {
+  CHECK_NE(args.count('num_col'), 0) << 'expect num_col in dense_libsvm';
+  return new DensifyParser<IndexType>(
+            Parser<IndexType>::Create(path.c_str(), part_index, num_parts, 'libsvm'),
+           uint32_t(atoi(args.at('num_col').c_str())));
 }
+}  // namespace data
     
     
-    {  api::WebContents* api_web_contents_;
-  std::map<std::string, int> origin_counts_;
-};
-    
-    #include 'common.hpp'
-#include 'vtransform.hpp'
-    
-                uint8x16x4_t v_src = vld4q_u8(src + sj);
-            vst1q_u8(dst + dj, v_src.val[coi]);
-    
-    void assertSupportedConfiguration(bool parametersSupported)
-{
-    if (!isSupportedConfiguration()) {
-        std::cerr << 'internal error: attempted to use an unavailable function' << std::endl;
-        std::abort();
-    }
-    }
-    
-                uint32x4_t v_mask0 = vorrq_u32(vceqq_u32(v_src0, v_maxval4), vceqq_u32(v_src0, v_minval4));
-            uint32x4_t v_mask1 = vorrq_u32(vceqq_u32(v_src1, v_maxval4), vceqq_u32(v_src1, v_minval4));
-    
-    #endif
-    
-    INRANGEFUNC(u8)
-INRANGEFUNC(s8)
-INRANGEFUNC(u16)
-INRANGEFUNC(s16)
-INRANGEFUNC(s32)
-INRANGEFUNC(f32)
-    
-    #ifndef CAROTENE_INTRINSICS_HPP
-#define CAROTENE_INTRINSICS_HPP
-    
-      // Returns the number of TestPartResult objects in the array.
-  int size() const;
-    
-    #ifndef GTEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
-#define GTEST_INCLUDE_GTEST_INTERNAL_GTEST_DEATH_TEST_INTERNAL_H_
-    
-        void ComputeCurrentValue() {
-      if (!AtEnd())
-        current_value_ = ParamType(*current1_, *current2_, *current3_,
-            *current4_, *current5_);
-    }
-    bool AtEnd() const {
-      // We must report iterator past the end of the range when either of the
-      // component iterators has reached the end of its range.
-      return
-          current1_ == end1_ ||
-          current2_ == end2_ ||
-          current3_ == end3_ ||
-          current4_ == end4_ ||
-          current5_ == end5_;
-    }
-    
-    // Gets the content of the stringstream's buffer as an std::string.  Each '\0'
-// character in the buffer is replaced with '\\0'.
-GTEST_API_ std::string StringStreamToString(::std::stringstream* stream);
-    
-    template <GTEST_2_TYPENAMES_(T)>
-struct tuple_size<GTEST_2_TUPLE_(T) > {
-  static const int value = 2;
-};
-    
-      // Try to divide n by every odd number i, starting from 3
-  for (int i = 3; ; i += 2) {
-    // We only have to try i up to the squre root of n
-    if (i > n/i) break;
-    }
-    
-    // This should fail when the --check_for_leaks command line flag is
-// specified.
-TEST(ListenersTest, LeaksWater) {
-  Water* water = new Water;
-  EXPECT_TRUE(water != NULL);
+    {// The number of bits required to represent a given unsigned range
+static size_t SymbolBits(size_t num_symbols) {
+  auto bits = std::ceil(std::log2(num_symbols));
+  return std::max(static_cast<size_t>(bits), size_t(1));
 }
+}  // namespace detail
