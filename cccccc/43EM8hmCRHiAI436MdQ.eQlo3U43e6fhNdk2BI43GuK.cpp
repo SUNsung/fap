8a7565a16a26613516ce6f90ad71047eb5f7b6ca
SUNsung <weatherius@gmail.com>
2018-12-18 06:48:33 +0200

    Rewrite [43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp] File in: 2018-12-18 06:48:32

--- cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
+++ cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
@@ -1,216 +1,29 @@
 
         
-          uv_loop_t* loop_;
+        DEFINE_string(adapter_config_filename, 'modules/canbus/conf/adapter.conf',
+              'The adapter config file');
     
-    namespace api {
+    double ClusterGeneralInfo701::lateral_dist(const std::uint8_t* bytes,
+                                           int32_t length) const {
+  Byte t0(bytes + 2);
+  uint32_t x = t0.get_byte(0, 2);
     }
     
     
-    {  return mate::CreateHandle(isolate, new Screen(isolate, screen)).ToV8();
+    {  int ret = x;
+  return ret;
 }
     
-    #ifndef ATOM_BROWSER_API_ATOM_API_WEB_REQUEST_H_
-#define ATOM_BROWSER_API_ATOM_API_WEB_REQUEST_H_
+    #include <string>
+#include <vector>
     
-    // static
-void Event::BuildPrototype(v8::Isolate* isolate,
-                           v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, 'Event'));
-  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
-      .SetMethod('preventDefault', &Event::PreventDefault)
-      .SetMethod('sendReply', &Event::SendReply);
-}
-    
-    namespace IPC {
-class Message;
-}
-    
-     private:
-  static int32_t next_id_;
-  static atom::KeyWeakMap<int32_t>* weak_map_;  // leaked on purpose
-    
-    // Common code between SIG{HUP, INT, TERM}Handler.
-void GracefulShutdownHandler(int signal) {
-  // Reinstall the default handler.  We had one shot at graceful shutdown.
-  struct sigaction action;
-  memset(&action, 0, sizeof(action));
-  action.sa_handler = SIG_DFL;
-  RAW_CHECK(sigaction(signal, &action, nullptr) == 0);
-    }
-    
-     private:
-  ~AtomQuotaPermissionContext() override;
-    
-    class TreePrinter {
-  llvm::raw_ostream &Out;
-  void (&PrintNodeData)(llvm::raw_ostream &out, void *node);
-  SmallString<40> Indent;
-public:
-  TreePrinter(llvm::raw_ostream &out,
-              void (&printNodeData)(llvm::raw_ostream &out, void *node))
-    : Out(out), PrintNodeData(printNodeData) {}
-    }
-    
-            for (; j < roiw16; j += 16)
-        {
-            internal::prefetch(src + j);
-            internal::prefetch(dst + j);
-            uint8x16_t v_src = vld1q_u8(src + j);
-            int16x8_t v_dst0 = vld1q_s16(dst + j), v_dst1 = vld1q_s16(dst + j + 8);
-            int16x8_t v_src0 = vreinterpretq_s16_u16(vmovl_u8(vget_low_u8(v_src)));
-            int16x8_t v_src1 = vreinterpretq_s16_u16(vmovl_u8(vget_high_u8(v_src)));
-    }
-    
-    
-    {        if (result < 0)
-        {
-            return 0x7fFFffFF;
-        }
-    }
-    return result;
-#else
-    (void)_size;
-    (void)srcBase;
-    (void)srcStride;
-    
-    #define INRANGEFUNC(T)                                       \
-void inRange(const Size2D &_size,                            \
-             const T * srcBase, ptrdiff_t srcStride,         \
-             const T * rng1Base, ptrdiff_t rng1Stride,       \
-             const T * rng2Base, ptrdiff_t rng2Stride,       \
-             u8 * dstBase, ptrdiff_t dstStride)              \
-{                                                            \
-    internal::assertSupportedConfiguration();                \
-    inRangeCheck(_size, srcBase, srcStride,                  \
-                 rng1Base, rng1Stride, rng2Base, rng2Stride, \
-                 dstBase, dstStride);                        \
-}
-#else
-#define INRANGEFUNC(T)                                       \
-void inRange(const Size2D &,                                 \
-             const T *, ptrdiff_t,                           \
-             const T *, ptrdiff_t,                           \
-             const T *, ptrdiff_t,                           \
-             u8 *, ptrdiff_t)                                \
-{                                                            \
-    internal::assertSupportedConfiguration();                \
-}
-#endif
-    
-            uint16x8_t el8shr12 =  vaddl_u8(el8shr1, el8shr2);
-        uint16x8_t el8shr03 =  vaddl_u8(el8shr0, el8shr3);
-    
-    inline uint8x8_t vqtbl1_u8 (uint8x16_t a, uint8x8_t b)
-{
-#ifdef __aarch64__
-    // AArch64 supports this natively
-    return ::vqtbl1_u8(a, b);
-#else
-    union { uint8x16_t v; uint8x8x2_t w; } u = { a };
-    return vtbl2_u8(u.w, b);
-#endif
-}
-    
-        private:
-        static Microsoft::MSR::CNTK::InputStreamDescription GetInputStreamDescription(const StreamInformation& s, const DeviceDescriptor& device)
-        {
-            assert(s.m_storageFormat == StorageFormat::Dense || s.m_storageFormat == StorageFormat::SparseCSC);
-            auto CNTKdeviceId = AsCNTKImplDeviceId(device);
-            auto CNTKMatrixType = s.m_storageFormat == StorageFormat::Dense ? Microsoft::MSR::CNTK::MatrixType::DENSE : Microsoft::MSR::CNTK::MatrixType::SPARSE;
-            auto CNTKMatrixFormat = AsCNTKImplMatrixFormat(s.m_storageFormat);
-            return Microsoft::MSR::CNTK::InputStreamDescription(s.m_name, CNTKdeviceId, CNTKMatrixType, CNTKMatrixFormat);
-        }
-    
-            static bool IsUDF(const FunctionPtr& f);
-    
-            ValuePtr DeepClone(bool readOnly) const override
-        {
-            if (m_isPacked)
-            {
-                std::shared_ptr<Microsoft::MSR::CNTK::MBLayout> packedLayoutCopy;
-                if (m_packedDataLayout)
-                {
-                    packedLayoutCopy = std::make_shared<Microsoft::MSR::CNTK::MBLayout>();
-                    packedLayoutCopy->CopyFrom(m_packedDataLayout);
-                }
-                return MakeSharedObject<PackedValue>(m_sampleShape, m_sampleDynamicAxes, m_packedData->DeepClone(readOnly), packedLayoutCopy, readOnly);
-            }
-            else
-                return Value::DeepClone(readOnly);
-        }
-    
-        int m_fd;               // file descriptor
-    std::string m_fileName; // lock file name
-    struct flock m_lock;    // fnctl lock structure
-    
-    namespace Microsoft { namespace MSR { namespace CNTK {
-    }
-    }
-    }
-    
-    U_NAMESPACE_BEGIN
-    
-    U_NAMESPACE_BEGIN
-    
-    #include 'unicode/format.h'
-#include 'unicode/locid.h'
-#include 'unicode/parseerr.h'
-#include 'unicode/utypes.h'
-#include 'uvector.h'
-#include 'hash.h'
-    
-    #if !UCONFIG_NO_BREAK_ITERATION
-    
-    class Calendar;
-    
-    // Bitwise comparison for the collation keys.
-Collator::EComparisonResult
-CollationKey::compareTo(const CollationKey& target) const
-{
-    UErrorCode errorCode = U_ZERO_ERROR;
-    return static_cast<Collator::EComparisonResult>(compareTo(target, errorCode));
-}
-    
-    /**
- * Destructor
- */
-StringMatcher::~StringMatcher() {
-}
-    
-    static void ParseString(benchmark::State& state, const char* filename)
-{
-    std::ifstream f(filename);
-    std::string str((std::istreambuf_iterator<char>(f)), std::istreambuf_iterator<char>());
-    }
-    
-    // The base class for all fixture tests.
-class Fixture : public internal::Benchmark {
+    class RouteSegmentsTest : public ::testing::Test {
  public:
-  Fixture() : internal::Benchmark('') {}
-    }
-    
-    ConsoleReporter::OutputOptions GetOutputOptions(bool force_no_color = false);
-    
-      va_end(args_cp);
-    
-      for (auto& c : result.counters) {
-    const std::size_t cNameLen = std::max(std::string::size_type(10),
-                                          c.first.length());
-    auto const& s = HumanReadableNumber(c.second.value, 1000);
-    if (output_options_ & OO_Tabular) {
-      if (c.second.flags & Counter::kIsRate) {
-        printer(Out, COLOR_DEFAULT, ' %*s/s', cNameLen - 2, s.c_str());
-      } else {
-        printer(Out, COLOR_DEFAULT, ' %*s', cNameLen, s.c_str());
-      }
-    } else {
-      const char* unit = (c.second.flags & Counter::kIsRate) ? '/s' : '';
-      printer(Out, COLOR_DEFAULT, ' %s=%s%s', c.first.c_str(), s.c_str(),
-              unit);
+  static void SetUpTestCase() {
+    AINFO << 'map file: ' << FLAGS_test_map_file;
+    if (hdmap_.LoadMapFromFile(FLAGS_test_map_file) != 0) {
+      AERROR << 'Failed to load map: ' << FLAGS_test_map_file;
+      CHECK(false);
     }
   }
-    
-    
-    {}
-    
-    #if defined(HAVE_STD_REGEX)
+    }
