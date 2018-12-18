b5f041e2f55b30e55fee37b0ec02f7fb9a822b3c
SUNsung <weatherius@gmail.com>
2018-12-18 23:22:25 +0200

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2018-12-18 23:22:24

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,191 +1,61 @@
 
         
-            @value.setter
-    def value(self, new_value):
-        if 1 <= new_value <= 13:
-            self._value = new_value
-        else:
-            raise ValueError('Invalid card value: {}'.format(new_value))
+        
+@pytest.fixture
+def app():
+    '''Create and configure a new app instance for each test.'''
+    # create a temporary file to isolate the database for each test
+    db_fd, db_path = tempfile.mkstemp()
+    # create the app with common test config
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path,
+    })
+    
+        auth.login()
+    # current user can't modify other user's post
+    assert client.post('/1/update').status_code == 403
+    assert client.post('/1/delete').status_code == 403
+    # current user doesn't see edit link
+    assert b'href='/1/update'' not in client.get('/').data
+    
+        with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
     
     
-class GroupChat(Chat):
+class PassDict(JSONTag):
+    __slots__ = ()
+    
+        If there is no handler for the logger's effective level, add a
+    :class:`~logging.StreamHandler` for
+    :func:`~flask.logging.wsgi_errors_stream` with a basic format.
+    '''
+    logger = logging.getLogger('flask.app')
+    
+    def handlePoint(point):
+    print('<li>%s</li>' % getText(point.childNodes))
+    
+            print('Ordered results using pool.map() --- will block till complete:')
+        for x in pool.map(calculatestar, TASKS):
+            print('\t', x)
+        print()
+    
+    setup_end_to_end = '''
+import face_recognition
+    
+    # Load some images to compare against
+known_obama_image = face_recognition.load_image_file('obama.jpg')
+known_biden_image = face_recognition.load_image_file('biden.jpg')
     
     
-class DefaultCategories(Enum):
-    
-        def extract_year_month(self, line):
-        '''Return the year and month portions of the timestamp.'''
-        pass
-    
-    
-class HashTable(object):
-    
-    import datetime
-import io
-import json
-import textwrap
-    
-    
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
-    
-    
-def main():
-    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
-    options, args = parser.parse_args()
-    if len(args) != 2:
-        parser.error('Expected an input and an output filename')
-    
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
-    
-        flags = [opt.get_opt_string() for opt in opts]
-    
-        def test_encrypt(self):
-        msg = b'message'
-        key = list(range(16))
-        encrypted = aes_encrypt(bytes_to_intlist(msg), key)
-        decrypted = intlist_to_bytes(aes_decrypt(encrypted, key))
-        self.assertEqual(decrypted, msg)
-    
-    
-class TestAgeRestriction(unittest.TestCase):
-    def _assert_restricted(self, url, filename, age, old_age=None):
-        self.assertTrue(_download_restricted(url, filename, old_age))
-        self.assertFalse(_download_restricted(url, filename, age))
-    
-        def test_yahoo_https(self):
-        # https://github.com/rg3/youtube-dl/issues/2701
-        self.assertMatch(
-            'https://screen.yahoo.com/smartwatches-latest-wearable-gadgets-163745379-cbs.html',
-            ['Yahoo'])
-    
-    
-@pytest.mark.parametrize('script, output', [
-    ('apt', invalid_operation('saerch')),
-    ('apt-get', invalid_operation('isntall')),
-    ('apt-cache', invalid_operation('rumove'))])
-def test_match(script, output):
-    assert match(Command(script, output))
-    
-    
-no_suggestions = '''\
-usage: aws [options] <command> <subcommand> [<subcommand> ...] [parameters]
-To see help text, you can run:
-    
-    
-no_such_subcommand_old = '''No such subcommand
-    
-        def lower_items(self):
-        '''Like iteritems(), but with all lowercase keys.'''
-        return (
-            (lowerkey, keyval[1])
-            for (lowerkey, keyval)
-            in self._store.items()
-        )
-    
-            return r
-    
-    
-def test_system_ssl():
-    '''Verify we're actually setting system_ssl when it should be available.'''
-    assert info()['system_ssl']['version'] != ''
-    
-            assert len(server.handler_results) == 0
-    
-        Usage::
-    
-        By default this will get the strings from the blns.txt file
-    
-            if 'quality' in stream:
-            print('      quality:       %s' % stream['quality'])
-    
-    site_info = 'baomihua.com'
-download = baomihua_download
-download_playlist = playlist_not_supported('baomihua')
-
-    
-            html = get_content(self.url)
-    
-    
-def get_title_and_urls(json_data):
-    title = legitimize(re.sub('[\s*]', '_', json_data['title']))
-    video_info = json_data['file_versions']['html5']['video']
-    if 'high' not in video_info:
-        if 'med' not in video_info:
-            video_url = video_info['low']['url']
-        else:
-            video_url = video_info['med']['url']
+def print_result(filename, name, distance, show_distance=False):
+    if show_distance:
+        print('{},{},{}'.format(filename, name, distance))
     else:
-        video_url = video_info['high']['url']
-    audio_info = json_data['file_versions']['html5']['audio']
-    if 'high' not in audio_info:
-        if 'med' not in audio_info:
-            audio_url = audio_info['low']['url']
-        else:
-            audio_url = audio_info['med']['url']
-    else:
-        audio_url = audio_info['high']['url']
-    return title, video_url, audio_url
+        print('{},{}'.format(filename, name))
     
-        vids = matchall(content, tudou_embed_patterns)
-    for vid in set(vids):
-        found = True
-        tudou_download_by_id(vid, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
+    # Create a PIL imagedraw object so we can draw on the picture
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
     
-    
-def fantasy_download_by_id_channelId(id = 0, channelId = 0, output_dir = '.', merge = True, info_only = False,
-                                     **kwargs):
-    api_url = 'http://www.fantasy.tv/tv/playDetails.action?' \
-              'myChannelId=1&id={id}&channelId={channelId}&t={t}'.format(id = id,
-                                                                         channelId = channelId,
-                                                                         t = str(random.random())
-                                                                         )
-    html = get_content(api_url)
-    html = json.loads(html)
-    
-    Loosely based on https://github.com/astropy/astropy/pull/347
-'''
-    
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.last_results = []
-        host = config[CONF_HOST]
-        self._url = 'http://{}/data/getConnectInfo.asp'.format(host)
-        self._loginurl = 'http://{}/goform/login'.format(host)
-    
-    
-def setup_scanner(hass, config: dict, see, discovery_info=None):
-    '''Set up the iCloud Scanner.'''
-    username = config.get(CONF_USERNAME)
-    password = config.get(CONF_PASSWORD)
-    account = config.get(CONF_ACCOUNTNAME, slugify(username.partition('@')[0]))
-    max_interval = config.get(CONF_MAX_INTERVAL)
-    gps_accuracy_threshold = config.get(CONF_GPS_ACCURACY_THRESHOLD)
-    
-            self.hosts = config[CONF_HOSTS]
-        self.exclude = config[CONF_EXCLUDE]
-        minutes = config[CONF_HOME_INTERVAL]
-        self._options = config[CONF_OPTIONS]
-        self.home_interval = timedelta(minutes=minutes)
-    
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.host = config[CONF_HOST]
-        self.last_results = {}
-    
-            def on_modified(self, event):
-            '''File modified.'''
-            self.process(event)
+    app = Flask(__name__)
