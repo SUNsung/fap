fabe2fdaeb246c6be8e1c218826b8298ca13afc5
SUNsung <weatherius@gmail.com>
2018-12-18 22:08:51 +0200

    Rewrite [41D0lu7fs1BBs41iNFqdVhfkPg41t8D64yj95j6415s4.rb] File in: 2018-12-18 22:08:50

--- ruby/41D0lu7fs1BBs41iNFqdVhfkPg41t8D64yj95j6415s4.rb
+++ ruby/41D0lu7fs1BBs41iNFqdVhfkPg41t8D64yj95j6415s4.rb
@@ -1,52 +1,91 @@
 
         
-                # This contains all the provider plugins by name, and returns
-        # the provider class and options.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :providers
+                  add_default_name_and_id_for_value(tag_value, name_and_id)
+          options.delete('index')
+          options.delete('namespace')
+          options['for'] = name_and_id['id'] unless options.key?('for')
     
-            # This returns all the registered communicators.
-        #
-        # @return [Hash]
-        def communicators
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.communicator)
-            end
+        initializer 'action_view.default_enforce_utf8' do |app|
+      ActiveSupport.on_load(:action_view) do
+        default_enforce_utf8 = app.config.action_view.delete(:default_enforce_utf8)
+        unless default_enforce_utf8.nil?
+          ActionView::Helpers::FormTagHelper.default_enforce_utf8 = default_enforce_utf8
+        end
+      end
+    end
+    
+        brew cask install mactex
+    EOS
+  when 'pip' then <<-EOS.undent
+    Homebrew provides pip via: `brew install python`. However you will then
+    have two Pythons installed on your Mac, so alternatively you can install
+    pip via the instructions at:
+    
+          out = checks.send(method)
+      unless out.nil? || out.empty?
+        if first_warning
+          $stderr.puts <<-EOS.undent
+            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
+            with debugging if you file an issue. If everything you use Homebrew for is
+            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
+          EOS
+        end
+    
+      def cxxstdlib_check(check_type)
+    self.class.cxxstdlib_check check_type
+  end
+    
+          GivenDailyLike.decrement_for(user.id)
+      expect(value_for(user.id, dt)).to eq(0)
+      expect(limit_reached_for(user.id, dt)).to eq(false)
+    end
+    
+        expect(gu.notification_level).to eq(NotificationLevels.all[:tracking])
+    
+        end
+  end
+    
+    class Devise::SessionsController < DeviseController
+  prepend_before_action :require_no_authentication, only: [:new, :create]
+  prepend_before_action :allow_params_authentication!, only: :create
+  prepend_before_action :verify_signed_out_user, only: :destroy
+  prepend_before_action(only: [:create, :destroy]) { request.env['devise.skip_timeout'] = true }
+    
+       @request.env['devise.mapping'] = Devise.mappings[:user]
+    
+          # Forgets the given resource by deleting a cookie
+      def forget_me(resource)
+        scope = Devise::Mapping.find_scope!(resource)
+        resource.forget_me!
+        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
+      end
+    
+          def stored_location_key_for(resource_or_scope)
+        scope = Devise::Mapping.find_scope!(resource_or_scope)
+        '#{scope}_return_to'
+      end
+    
+        def self.check_fields!(klass)
+      failed_attributes = []
+      instance = klass.new
+    
+              it 'cuts fraction part to have only 6 digits at all' do
+            format('%#{f}', 1.1234567).should == '1.12346'
+            format('%#{f}', 12.1234567).should == '12.1235'
+            format('%#{f}', 123.1234567).should == '123.123'
+            format('%#{f}', 1234.1234567).should == '1234.12'
+            format('%#{f}', 12345.1234567).should == '12345.1'
+            format('%#{f}', 123456.1234567).should == '123456'
           end
         end
     
-            # Initialize the provider to represent the given machine.
-        #
-        # @param [Vagrant::Machine] machine The machine that this provider
-        #   is responsible for.
-        def initialize(machine)
-        end
-    
-        def type_from_file_command
-      @type_from_file_command ||=
-        FileCommandContentTypeDetector.new(@filepath).detect
+      platform_is :windows do
+    it 'does expand shell variables when given multiples arguments' do
+      # See https://bugs.ruby-lang.org/issues/12231
+      lambda { @object.system('echo', @shell_var) }.should output_to_fd('foo\n')
     end
   end
-end
-
     
-        def geometry_string
-      begin
-        orientation = Paperclip.options[:use_exif_orientation] ?
-          '%[exif:orientation]' : '1'
-        Paperclip.run(
-          Paperclip.options[:is_windows] ? 'magick identify' : 'identify',
-          '-format '%wx%h,#{orientation}' :file', {
-            :file => '#{path}[0]'
-          }, {
-            :swallow_stderr => true
-          }
-        )
-      rescue Terrapin::ExitStatusError
-        ''
-      rescue Terrapin::CommandNotFoundError => e
-        raise_because_imagemagick_missing
-      end
-    end
+        $Kernel_trace_var_global = 'foo'
+    captured.should == 'foo'
+  end
