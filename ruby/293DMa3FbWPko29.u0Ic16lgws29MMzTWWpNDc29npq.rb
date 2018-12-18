af519b85564d707f9c15285e4fd846e0b715541c
SUNsung <weatherius@gmail.com>
2018-12-18 21:08:25 +0200

    Rewrite [293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb] File in: 2018-12-18 21:08:24

--- ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
+++ ruby/293DMa3FbWPko29.u0Ic16lgws29MMzTWWpNDc29npq.rb
@@ -1,120 +1,64 @@
 
         
-          it 'has no effect on immediate values' do
-    [nil, true, false].each do |v|
-      v.taint
-      v.tainted?.should == false
-    end
-  end
-    
-      it 'calls #to_path on second argument when passed ?d and a directory' do
-    p = mock('path')
-    p.should_receive(:to_path).and_return @dir
-    Kernel.test(?d, p)
+          def run_vagrant_command(command)
+    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
+    return [stdout, stderr] if status.success?
+    raise VagrantSSHCommandError, status
   end
 end
     
-      it 'transfers control to the innermost catch block waiting for the same sympol' do
-    one = two = three = 0
-    catch :duplicate do
-      catch :duplicate do
-        catch :duplicate do
-          one = 1
-          throw :duplicate
-        end
-        two = 2
-        throw :duplicate
-      end
-      three = 3
-      throw :duplicate
-    end
-    [one, two, three].should == [1, 2, 3]
-  end
-    
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
-    end
-  end
-    
-        <div id='rack'>
-      <h3 id='env-info'>Rack ENV</h3>
-      <table class='req'>
-        <tr>
-          <th>Variable</th>
-          <th>Value</th>
-        </tr>
-         <% env.sort_by { |k, v| k.to_s }.each { |key, val| %>
-         <tr>
-           <td><%=h key %></td>
-           <td class='code'><div><%=h val %></div></td>
-         </tr>
-         <% } %>
-      </table>
-      <div class='clear'></div>
-    </div> <!-- /RACK ENV -->
-    
-          # Essentially the inverse of +mask_token+.
-      def unmask_token(masked_token)
-        # Split the token into the one-time pad and the encrypted
-        # value and decrypt it
-        token_length = masked_token.length / 2
-        one_time_pad = masked_token[0...token_length]
-        encrypted_token = masked_token[token_length..-1]
-        xor_byte_strings(one_time_pad, encrypted_token)
+          def properties
+        @properties ||= Properties.new
       end
     
-          def session(env)
-        return env[options[:session_key]] if session? env
-        fail 'you need to set up a session middleware *before* #{self.class}'
-      end
+            value_to_evaluate = block || value
     
-      describe '.random_token' do
-    it 'generates a base64 encoded 32 character string' do
-      expect(Base64.strict_decode64(token).length).to eq(32)
-    end
+          def string_to_code(string)
+        # sha bytes
+        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
+        # Thanks donpark's IdenticonUtil.java for this.
+        # Match the following Java code
+        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
+        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
+    
+        # Test page_header_from_content(@content)
+    actual = @view.title
+    assert_equal '1 & 2', actual
   end
+    
+    desc 'Build and install'
+task :install => :build do
+  sh 'gem install --local --no-ri --no-rdoc pkg/#{name}-#{version}.gem'
+end
+    
+        # Extract the path string that Gollum::Wiki expects
+    def extract_path(file_path)
+      return nil if file_path.nil?
+      last_slash = file_path.rindex('/')
+      if last_slash
+        file_path[0, last_slash]
+      end
+    end
+    
+      s.test_files = s.files.select { |path| path =~ /^test\/test_.*\.rb/ }
 end
 
     
-      SPREE_GEMS.each do |gem_name|
-    rm_f  '#{gem_name}/Gemfile.lock'
-    rm_rf '#{gem_name}/pkg'
-    rm_rf '#{gem_name}/spec/dummy'
-  end
-end
+        def handle_gist_redirecting(data)
+      redirected_url = data.header['Location']
+      if redirected_url.nil? || redirected_url.empty?
+        raise ArgumentError, 'GitHub replied with a 302 but didn't provide a location in the response headers.'
+      end
     
-            def create
-          authorize! :create, StockMovement
-          @stock_movement = scope.new(stock_movement_params)
-          if @stock_movement.save
-            respond_with(@stock_movement, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_movement)
-          end
-        end
+      class IncludeArrayTag < Liquid::Tag
+    Syntax = /(#{Liquid::QuotedFragment}+)/
+    def initialize(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @array_name = $1
+      else
+        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
+      end
     
-            def update
-          authorize! :update, @store
-          if @store.update_attributes(store_params)
-            respond_with(@store, status: 200, default_template: :show)
-          else
-            invalid_resource!(@store)
-          end
-        end
-    
-            def update
-          authorize! :update, user
-          if user.update_attributes(user_params)
-            respond_with(user, status: 200, default_template: :show)
-          else
-            invalid_resource!(user)
-          end
-        end
+        def fire_event(event, options={})
+      reverse = options[:reverse]
+      reraise = options[:reraise]
