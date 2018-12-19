d30da27141c27f9e24d1d1c196e2b7456131cef0
SUNsung <weatherius@gmail.com>
2018-12-19 17:26:04 +0200

    Rewrite [19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb] File in: 2018-12-19 17:26:03

--- ruby/19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb
+++ ruby/19Hfue33ZMVfg19y.94QpzaLo19zRazly0jJGY19hf.rb
@@ -1,155 +1,88 @@
 
         
-        def global_require
-  JSON.pretty_generate(DATA)
-end
+            group.remove(moderator)
+    group.save
     
-    require 'benchmark/ips'
-require 'pathutil'
+    Group.user_trust_level_change!(-1, TrustLevel[4])
     
-            MergeRequest
-          .where(id: start_id..stop_id)
-          .where(latest_merge_request_diff_id: nil)
-          .each_batch(of: BATCH_SIZE) do |relation|
+          if staff.topic_id.nil?
+        creator = PostCreator.new(Discourse.system_user,
+          raw: I18n.t('staff_category_description'),
+          title: I18n.t('category.topic_prefix', category: staff.name),
+          category: staff.name,
+          archetype: Archetype.default
+        )
+        post = creator.create
     
-          def id
-        raw_data['ixProject']
-      end
-    
-                    type = nil
-              elsif line[0] == '\\'
-                type = 'nonewline'
-                lines << {
-                  full_line: full_line,
-                  type: type,
-                  line_obj_index: line_obj_index,
-                  line_old: line_old,
-                  line_new: line_new
-                }
-              else
-                lines << {
-                  full_line: full_line,
-                  type: type,
-                  line_obj_index: line_obj_index,
-                  line_old: line_old,
-                  line_new: line_new
-                }
-    
-    module Vagrant
-  # This class handles guest-OS specific interactions with a machine.
-  # It is primarily responsible for detecting the proper guest OS
-  # implementation and then delegating capabilities.
-  #
-  # Vagrant has many tasks which require specific guest OS knowledge.
-  # These are implemented using a guest/capability system. Various plugins
-  # register as 'guests' which determine the underlying OS of the system.
-  # Then, 'guest capabilities' register themselves for a specific OS (one
-  # or more), and these capabilities are called.
-  #
-  # Example capabilities might be 'mount_virtualbox_shared_folder' or
-  # 'configure_networks'.
-  #
-  # This system allows for maximum flexibility and pluginability for doing
-  # guest OS specific operations.
-  class Guest
-    include CapabilityHost
-    
-            # The configuration for this provisioner. This will be an instance of
-        # the `Config` class which is part of the provisioner.
-        attr_reader :config
-    
-              # Determine if we require a local Vagrant environment. There are
-          # two cases that we require a local environment:
-          #
-          #   * We're asking for ANY/EVERY VM (no names given).
-          #
-          #   * We're asking for specific VMs, at least once of which
-          #     is NOT in the local machine index.
-          #
-          requires_local_env = false
-          requires_local_env = true if names.empty?
-          requires_local_env ||= names.any? { |n|
-            !@env.machine_index.include?(n)
-          }
-          raise Errors::NoEnvironmentError if requires_local_env && !@env.root_path
-    
-                [block.call, parent]
-          end
-          nil
+            if b_length > a_length
+          (b_length - a_length).times { a_split.insert(-2, 0) }
+        elsif a_length > b_length
+          (a_length - b_length).times { b_split.insert(-2, 0) }
         end
     
-            it 'collapse negative number representation if it equals 1' do
-          format('%#{f}', -1).should_not == '..11'
-          format('%#{f}', -1).should == '..1'
+        DATA_URL = 'data:'.freeze
+    
+        def to_a
+      @filters.dup
+    end
+    
+        def url
+      @url ||= URL.parse request.base_url
+    end
+    
+            subclass.base_url = base_url
+        subclass.root_path = root_path
+        subclass.initial_paths = initial_paths.dup
+        subclass.options = options.deep_dup
+        subclass.html_filters = html_filters.inheritable_copy
+        subclass.text_filters = text_filters.inheritable_copy
+        subclass.stubs = stubs.dup
+      end
+    
+        def join(*args)
+      self.class.join self, *args
+    end
+    
+            css('img[style]').each do |node|
+          node['align'] ||= node['style'][/float:\s*(left|right)/, 1]
+          node['style'] = node['style'].split(';').map(&:strip).select { |s| s =~ /\Awidth|height/ }.join(';')
+        end
+    
+      # Causes some terminals to display secure password entry indicators
+  def noecho_gets
+    system 'stty -echo'
+    result = $stdin.gets
+    system 'stty echo'
+    puts
+    result
+  end
+    
+          new_initial_revision_var = 'HOMEBREW_UPDATE_BEFORE#{repo_var}'
+      new_current_revision_var = 'HOMEBREW_UPDATE_AFTER#{repo_var}'
+    
+        # @return [String] the ruby version string bundler uses to craft its gem path
+    def gem_ruby_version
+      RbConfig::CONFIG['ruby_version']
+    end
+    
+            if explicit_plugins.any? { |spec| filename =~ /^#{spec.name}/ }
+          FileUtils.mv(gem_file, ::File.join(explicit_path, filename))
+        else
+          FileUtils.mv(gem_file, ::File.join(dependencies_path, filename))
         end
       end
     end
     
-      it 'accepts a negative seed' do
-    srand(-17)
-    srand.should == -17
-  end
+          def get_installer_for(plugin_name)
+        uri = pack_uri(plugin_name)
     
-            -> { w.f4('', 0) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.warn_call_lineno}: warning: \n$|)
-        -> { w.f4(nil, 0) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.warn_call_lineno}: warning: \n$|)
-      end
+      describe '#system?' do
+    context 'when the pipeline is a system pipeline' do
+      let(:settings) { mock_settings({ 'pipeline.system' => true })}
     
-      it 'creates a public method in script binding' do
-    eval @code, script_binding
-    Object.should have_method :boom
-  end
-    
-          # @see Base#\_retrieve
-      def _retrieve(key, version, sha)
-        return unless File.readable?(path_to(key))
-        begin
-          File.open(path_to(key), 'rb') do |f|
-            if f.readline('\n').strip == version && f.readline('\n').strip == sha
-              return f.read
-            end
-          end
-          File.unlink path_to(key)
-        rescue Errno::ENOENT
-          # Already deleted. Race condition?
-        end
-        nil
-      rescue EOFError, TypeError, ArgumentError => e
-        Sass::Util.sass_warn 'Warning. Error encountered while reading cache #{path_to(key)}: #{e}'
-      end
-    
-          root.options = @options
-      if @options[:cache] && key && sha
-        begin
-          old_options = root.options
-          root.options = {}
-          @options[:cache_store].store(key, sha, root)
-        ensure
-          root.options = old_options
-        end
-      end
-      root
-    rescue SyntaxError => e
-      e.modify_backtrace(:filename => @options[:filename], :line => @line)
-      e.sass_template = @template
-      raise e
-    end
-    
-        def handle_load_error(err)
-      dep = err.message[/^no such file to load -- (.*)/, 1]
-      raise err if @options[:trace] || dep.nil? || dep.empty?
-      $stderr.puts <<MESSAGE
-Required dependency #{dep} not found!
-    Run 'gem install #{dep}' to get it.
-  Use --trace for backtrace.
-MESSAGE
-      exit 1
-    end
-  end
-end
-
-    
-          opts.on('-s', '--stdin', :NONE,
-              'Read input from standard input instead of an input file.',
-              'This is the default if no input file is specified. Requires --from.') do
-        @options[:input] = $stdin
+        context 'with a specific plugin' do
+      let(:plugin_name) { 'logstash-input-stdin' }
+      it 'list the plugin and display the plugin name' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
       end
