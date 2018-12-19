4a02c05f99219cb9f5d3f45ca324af8e300dd7cc
SUNsung <weatherius@gmail.com>
2018-12-19 18:23:09 +0200

    Rewrite [39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb] File in: 2018-12-19 18:23:08

--- ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
+++ ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
@@ -1,78 +1,84 @@
 
         
-                scope :exclude_ignored, -> do
-          where('allow_failure = ? OR status IN (?)',
-            false, %w[created pending running success skipped])
-        end
+        begin
+  error_pipe = UNIXSocket.open(ENV['HOMEBREW_ERROR_PIPE'], &:recv_io)
+  error_pipe.fcntl(Fcntl::F_SETFD, Fcntl::FD_CLOEXEC)
     
-            def sidekiq_worker_class
-          ImportIssueWorker
-        end
-    
-              issue.label_names.each do |label_name|
-            # Although unlikely it's technically possible for an issue to be
-            # given a label that was created and assigned after we imported all
-            # the project's labels.
-            next unless (label_id = label_finder.id_for(label_name))
-    
-    module Gitlab
-  module GithubImport
-    module Importer
-      class NotesImporter
-        include ParallelScheduling
-    
-          # project - An instance of `Project`.
-      # object - The object to look up or set a database ID for.
-      def initialize(project, object)
-        @project = project
-        @object = object
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
       end
-    
-              importer_class.new(repr, project, client).execute
-        end
-      end
-    
-            # attributes - A Hash containing the raw lfs_object details. The keys of this
-        #              Hash must be Symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-      end
-    end
+      cmds
+    end.sort
   end
-end
-
     
-              user = Representation::User.from_api_response(note.user) if note.user
-          hash = {
-            noteable_type: noteable_type,
-            noteable_id: matches[:iid].to_i,
-            author: user,
-            note: note.body,
-            created_at: note.created_at,
-            updated_at: note.updated_at,
-            github_id: note.id
-          }
+      def migrate_formula_rename
+    report[:R].each do |old_full_name, new_full_name|
+      old_name = old_full_name.split('/').last
+      next unless (dir = HOMEBREW_CELLAR/old_name).directory? && !dir.subdirs.empty?
     
-            attr_reader :attributes
+        $Kernel_trace_var_global = nil
+    $Kernel_trace_var_extra  = nil
+  end
     
-    # This stuff needs to be run after Paperclip is defined.
-require 'paperclip/io_adapters/registry'
-require 'paperclip/io_adapters/abstract_adapter'
-require 'paperclip/io_adapters/empty_string_adapter'
-require 'paperclip/io_adapters/identity_adapter'
-require 'paperclip/io_adapters/file_adapter'
-require 'paperclip/io_adapters/stringio_adapter'
-require 'paperclip/io_adapters/data_uri_adapter'
-require 'paperclip/io_adapters/nil_adapter'
-require 'paperclip/io_adapters/attachment_adapter'
-require 'paperclip/io_adapters/uploaded_file_adapter'
-require 'paperclip/io_adapters/uri_adapter'
-require 'paperclip/io_adapters/http_url_proxy_adapter'
-
+      if rss_url.nil?
+    puts '#{name}: GETTING'
+    rss_check_url = 'http://ajax.googleapis.com/ajax/services/feed/lookup?v=1.0&q=#{web_url}'
+    uri = URI.parse(rss_check_url)
+    response = JSON.parse(Net::HTTP.get(uri))
+    rss_url = response['responseData']['url'] if response['responseData'] && response['responseData'].has_key?('url')
     
-        def self.each_definition(&block)
-      instance.each_definition(&block)
+        def URIEncodeOctets(octets, result, index)
+      if (@@hexCharCodeArray == 0)
+        @@hexCharCodeArray = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+                              65, 66, 67, 68, 69, 70];
+      end
+      index = URIAddEncodedOctetToBuffer(octets[0], result, index);
+      if (octets[1]);
+        index = URIAddEncodedOctetToBuffer(octets[1], result, index)
+      end
+      if (octets[2]);
+        index = URIAddEncodedOctetToBuffer(octets[2], result, index)
+      end
+      if (octets[3]);
+        index = URIAddEncodedOctetToBuffer(octets[3], result, index)
+      end
+      return index;
     end
     
-        private
+          def string_to_code string
+        # sha bytes
+        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
+        # Thanks donpark's IdenticonUtil.java for this.
+        # Match the following Java code
+        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
+        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
+    
+          private
+    
+      test 'remove page extentions' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'page', view.remove_page_extentions('page.wiki')
+    assert_equal 'page-wiki', view.remove_page_extentions('page-wiki.md')
+    assert_equal 'file.any_extention', view.remove_page_extentions('file.any_extention')
+  end
+    
+        @view = Precious::Views::Page.new
+    @view.instance_variable_set :@page, page
+    @view.instance_variable_set :@content, page.formatted_data
+    @view.instance_variable_set :@h1_title, true
+    
+        def possible_types
+      MIME::Types.type_for(@filepath).collect(&:content_type)
+    end
+    
+        # Returns the dot+extension of the file. e.g. '.jpg' for 'file.jpg'
+    # If the style has a format defined, it will return the format instead
+    # of the actual extension. If the extension is empty, no dot is added.
+    def dotextension attachment, style_name
+      ext = extension(attachment, style_name)
+      ext.empty? ? ext : '.#{ext}'
+    end
