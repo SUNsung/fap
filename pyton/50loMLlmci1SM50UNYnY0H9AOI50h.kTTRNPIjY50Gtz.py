852ebdaf54a0207a13c379c0c22c14ba50f79c50
SUNsung <weatherius@gmail.com>
2018-12-19 03:42:33 +0200

    Rewrite [50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py] File in: 2018-12-19 03:42:32

--- pyton/50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py
+++ pyton/50loMLlmci1SM50UNYnY0H9AOI50h.kTTRNPIjY50Gtz.py
@@ -1,16 +1,32 @@
 
         
-        
-def naughty_strings(filepath=FILEPATH):
-    '''Get the list of naughty_strings.
+        # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
+# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
+# positive matches at the risk of more false negatives.
     
-    current_path = os.path.dirname(os.path.abspath(__file__))
+        # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
     
-                    mte = MismatchedTokenException(unichr(s), self.input)
-                self.recover(mte) # don't really recover; just consume in lexer
-                raise mte
-        
-            self.input.consume()
-            
+        # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
     
-            return m
+        # Every 128 frames (the default batch size), batch process the list of frames to find faces
+    if len(frames) == 128:
+        batch_of_face_locations = face_recognition.batch_face_locations(frames, number_of_times_to_upsample=0)
+    
+    
+# Remove the drawing library from memory as per the Pillow docs
+del draw
+    
+    test_requirements = [
+    'tox',
+    'flake8==2.6.0'
+]
+    
+        face_found = False
+    is_obama = False
