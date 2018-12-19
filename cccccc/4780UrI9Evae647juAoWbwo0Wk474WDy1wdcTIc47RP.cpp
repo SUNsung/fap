526510d0eb2c95ce17afede471060680637384f9
SUNsung <weatherius@gmail.com>
2018-12-19 09:19:28 +0200

    Rewrite [4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp] File in: 2018-12-19 09:19:28

--- cccccc/4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp
+++ cccccc/4780UrI9Evae647juAoWbwo0Wk474WDy1wdcTIc47RP.cpp
@@ -1,233 +1,126 @@
 
         
-        
-    { private:
-  // destructor
-  ~SimpleOpRegistry();
-  /*! \brief internal registry map */
-  std::map<std::string, SimpleOpRegEntry*> fmap_;
+        #ifndef DOCQUAL_H
+#define DOCQUAL_H
+    
+    class ScriptDetector {
+ public:
+  ScriptDetector(const GenericVector<int>* allowed_scripts,
+                 OSResults* osr, tesseract::Tesseract* tess);
+  void detect_blob(BLOB_CHOICE_LIST* scores);
+  bool must_stop(int orientation);
+ private:
+  OSResults* osr_;
+  static const char* korean_script_;
+  static const char* japanese_script_;
+  static const char* fraktur_script_;
+  int korean_id_;
+  int japanese_id_;
+  int katakana_id_;
+  int hiragana_id_;
+  int han_id_;
+  int hangul_id_;
+  int latin_id_;
+  int fraktur_id_;
+  tesseract::Tesseract* tess_;
+  const GenericVector<int>* allowed_scripts_;
 };
+    
+      /**
+   * Returns the type of the current block. See apitypes.h for
+   * PolyBlockType.
+   */
+  PolyBlockType BlockType() const;
     
     
     {
-    {   private:
-    friend class CudaModule;
-    /*!
-     * \brief constructor
-     * \param mod module of this kernel
-     * \param mangled_name mangled kernel name
-     * \param signature kernel argument signature
-     */
-    Kernel(const std::shared_ptr<Chunk>& mod,
-           const std::string& mangled_name,
-           const std::vector<ArgType>& signature);
-    /*! \brief mangled kernel name */
-    std::string mangled_name_;
-    /*! \brief kernel argument signature */
-    std::vector<ArgType> signature_;
-    /*! \brief module of this kernel */
-    std::shared_ptr<Chunk> mod_;
-    /*! \brief cached kernel function on each device */
-    std::unordered_map<int, CUfunction> func_;
-  };
-  /*!
-   * \brief CudaModule constructor
-   * \param source cuda source code.
-   * \param exports export symbols before mangling.
-   */
-  CudaModule(const char* source,
-             const std::vector<std::string>& options,
-             const std::vector<std::string>& exports)
-      : ptr_(std::make_shared<Chunk>(source, options, exports)) {}
-  /*!
-   * \brief Get cuda kernal from module by name
-   * \param name kernel name
-   * \param signature kernel signature
-   * \return shared pointer to cuda kernel
-   */
-  std::shared_ptr<Kernel> GetKernel(const std::string& name,
-                                    const std::vector<ArgType>& signature);
-};
-    
-      virtual bool Next(void) {
-    // MxNet iterator is expected to return CPU-accessible memory
-    if (::caffe::Caffe::mode() != ::caffe::Caffe::CPU) {
-      ::caffe::Caffe::set_mode(::caffe::Caffe::CPU);
-      CHECK_EQ(::caffe::Caffe::mode(), ::caffe::Caffe::CPU);
-    }
-    caffe_data_layer_->Forward(bottom_, top_);
-    CHECK_GT(batch_size_, 0) << 'batch size must be greater than zero';
-    CHECK_EQ(out_.batch_size, batch_size_) << 'Internal Error: batch size mismatch';
-    }
-    
-      void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) override {
-    param_.Init(kwargs);
-    CHECK_EQ(param_.num_out, 1);
-    CHECK_EQ(param_.num_data, 2);
-    }
-    
-      void Start() override {
-    streams_.reset(new StreamManager<kMaxNumGpus, kNumStreamsPerGpu>());
-    task_queue_.reset(new dmlc::ConcurrentBlockingQueue<OprBlock*>());
-    io_task_queue_.reset(new dmlc::ConcurrentBlockingQueue<OprBlock*>());
-    thread_pool_.reset(new ThreadPool(kNumWorkingThreads,
-                                      [this](std::shared_ptr<dmlc::ManualEvent> ready_event) {
-                                        ThreadWorker(task_queue_, ready_event); },
-                                      true));
-    io_thread_pool_.reset(new ThreadPool(1,
-                                         [this](std::shared_ptr<dmlc::ManualEvent> ready_event) {
-                                           ThreadWorker(io_task_queue_, ready_event); },
-                                         true));
-  }
-    
-    namespace CNTK
-{
-    const std::wstring versionKey = L'version';
-    const std::wstring typeKey = L'type';
-    const std::wstring uidKey = L'uid';
-    const std::wstring kindKey = L'kind';
-    const std::wstring dataTypeKey = L'data_type';
-    const std::wstring dynamicAxisKey = L'dynamic_axis';
-    const std::wstring isSparseKey = L'is_sparse';
-    const std::wstring nameKey = L'name';
-    const std::wstring needsGradientKey = L'needs_gradient';
-    const std::wstring shapeKey = L'shape';
-    const std::wstring valueKey = L'value';
-    const std::wstring opKey = L'op';
-    const std::wstring attributesKey = L'attributes';
-    const std::wstring inputsKey = L'inputs';
-    const std::wstring rootKey = L'root';
-    const std::wstring functionsKey = L'primitive_functions';
-    const std::wstring sampleCountKey = L'sample_count';
-    const std::wstring minibatchCountKey = L'minibatchCount'; // TODO: Python-style spelling
-    const std::wstring sweepCountKey = L'sweepCount';
-    const std::wstring unitKey = L'unit';
-    const std::wstring refMBSizeKey = L'ref_mb_size';
-    const std::wstring epochSizeKey = L'epoch_size';
-    const std::wstring scheduleKey = L'schedule';
-    const std::wstring learningRateScheduleKey = L'learnig_rate_schedule';
-    const std::wstring smoothedGradientsKey = L'smoothed_gradients';
-    const std::wstring noiseInjectionSeedKey = L'noise_injection_seed';
-    const std::wstring masterParameterUpdatedKey = L'master_parameter_updated';
-    const std::wstring smoothedCountKey = L'smoothed_count';
-    const std::wstring stateKey = L'state';
-    const std::wstring rngSeedKey = L'rng_seed';
-    const std::wstring rngOffsetKey = L'rng_offset';
-    const std::wstring blockFunctionCompositeKey = L'block_function_composite';
-    const std::wstring blockFunctionOpNameKey = L'block_function_op_name';
-    const std::wstring blockFunctionCompositeArgumentsMapKeysKey = L'block_function_composite_arguments_map_keys';
-    const std::wstring blockFunctionCompositeArgumentsMapValuesKey = L'block_function_composite_arguments_map_values';
-    const std::wstring internalWorkerStateKey = L'internal_worker_state';
-    const std::wstring externalWorkerStateKey = L'external_worker_state';
-    const std::wstring userDefinedStateKey = L'user_defined_state';
-    const std::wstring udfModuleNameKey = L'module';
-    const std::wstring udfFactoryMethodNameKey = L'deserialize_method';
-    const std::wstring nativeUDFKey = L'native';
-    }
-    
-    
-    {            if ((m_varKind == VariableKind::Parameter) || (m_varKind == VariableKind::Constant))
-            {
-                if (m_shape.HasFreeDimension())
-                    InvalidArgument('Parameter/Constant '%S' has invalid shape '%S'; it is illegal for a Parameter/Constant to have a FreeDimension.', AsString().c_str(), m_shape.AsString().c_str());
-            }
-        }
-    
-    #endif
+}  // namespace tesseract
 
     
-    // OneHotNode will create corresponding one hot tensor based on the input tensor. 
-template <class ElemType>
-class OneHotNode : public ComputationNodeNonLooping<ElemType>, public NumInputs<1>
-{
-    typedef ComputationNodeNonLooping<ElemType> Base;
-    UsingComputationNodeMembersBoilerplate;
-    static const std::wstring TypeName()
-    {
-        return L'OneHot';
+    
+    {  name += UNLV_EXT;              //add extension
+  if ((pdfp = fopen (name.string (), 'rb')) == nullptr) {
+    return false;                //didn't read one
+  } else {
+    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
+                                 //make rect block
+      block = new BLOCK (name.string (), TRUE, 0, 0,
+                         (int16_t) x, (int16_t) (ysize - y - height),
+                         (int16_t) (x + width), (int16_t) (ysize - y));
+                                 //on end of list
+      block_it.add_to_end (block);
     }
-    }
-    
-       const_reference get_last_closed_paren()const
-   {
-      if(m_is_singular)
-         raise_logic_error();
-      return m_last_closed_paren == 0 ? m_null : (*this)[m_last_closed_paren];
-   }
-    
-    #ifndef BOOST_REGEX_V4_REGEX_REPLACE_HPP
-#define BOOST_REGEX_V4_REGEX_REPLACE_HPP
-    
-    //
-// class regex_traits_wrapper.
-// this is what our implementation will actually store;
-// it provides default implementations of the 'optional'
-// interfaces that we support, in addition to the
-// required 'standard' ones:
-//
-namespace BOOST_REGEX_DETAIL_NS{
-#if !BOOST_WORKAROUND(__HP_aCC, < 60000)
-BOOST_MPL_HAS_XXX_TRAIT_DEF(boost_extensions_tag)
-#else
-template<class T>
-struct has_boost_extensions_tag
-{
-   BOOST_STATIC_CONSTANT(bool, value = false);
-};
-#endif
-    }
-    
-    std::vector<std::string> PrometheusMetricsConfigParserPlugin::keys() const {
-  return {kPrometheusParserRootKey};
-}
-    
-    TEST_F(PacksTests, test_check_version) {
-  Pack zpack('fake_version_pack', getPackWithFakeVersion().doc());
-  EXPECT_FALSE(zpack.checkVersion());
-    }
-    
-        // Use the basic 'force' flag to check implicit SQL usage.
-    auto flags =
-        SQL('select default_value from osquery_flags where name = 'force'');
-    if (flags.rows().size() > 0) {
-      r['flag_test'] = flags.rows().back().at('default_value');
-    }
-    
-    #include <algorithm>
-    
-    std::shared_ptr<DHTTask>
-DHTTaskFactoryImpl::createPeerAnnounceTask(const unsigned char* infoHash)
-{
-  // TODO
-  return nullptr;
-}
-    
-      virtual std::shared_ptr<DHTTask> createPeerLookupTask(
-      const std::shared_ptr<DownloadContext>& ctx, uint16_t tcpPort,
-      const std::shared_ptr<PeerStorage>& peerStorage) CXX11_OVERRIDE;
-    
-    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
-                                           const std::string& ipaddr,
-                                           uint16_t port,
-                                           const unsigned char* secret) const
-{
-  unsigned char src[DHT_ID_LENGTH + COMPACT_LEN_IPV6 + SECRET_SIZE];
-  memset(src, 0, sizeof(src));
-  int compactlen = bittorrent::packcompact(src + DHT_ID_LENGTH, ipaddr, port);
-  if (compactlen == 0) {
-    throw DL_ABORT_EX(fmt('Token generation failed: ipaddr=%s, port=%u',
-                          ipaddr.c_str(), port));
+    fclose(pdfp);
   }
-  memcpy(src, infoHash, DHT_ID_LENGTH);
-  memcpy(src + DHT_ID_LENGTH + COMPACT_LEN_IPV6, secret, SECRET_SIZE);
-  unsigned char md[20];
-  message_digest::digest(md, sizeof(md), MessageDigest::sha1().get(), src,
-                         sizeof(src));
-  return std::string(&md[0], &md[sizeof(md)]);
+  return true;
 }
     
-    DHTTokenUpdateCommand::~DHTTokenUpdateCommand() = default;
-    
-    
-    {  static const std::string UNKNOWN;
+    template <typename T>
+struct GetTypeInfo<const T *, typename EnableIf<TypeInherits<Object, T>::value>::type> {
+	static const Variant::Type VARIANT_TYPE = Variant::OBJECT;
+	static inline PropertyInfo get_class_info() {
+		return PropertyInfo(StringName(T::get_class_static()));
+	}
 };
+    
+    void ConstraintBullet::destroy_internal_constraint() {
+	space->remove_constraint(this);
+}
+    
+    public:
+	HingeJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Transform &frameA, const Transform &frameB);
+	HingeJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Vector3 &pivotInA, const Vector3 &pivotInB, const Vector3 &axisInA, const Vector3 &axisInB);
+    
+    class PinJointBullet : public JointBullet {
+	class btPoint2PointConstraint *p2pConstraint;
+    }
+    
+    	ClassDB::bind_method(D_METHOD('poll'), &NetworkedMultiplayerPeer::poll);
+    
+        // Count glyphs/ranges, initialize font
+    int total_glyphs_count = 0;
+    int total_ranges_count = 0;
+    for (int input_i = 0; input_i < atlas->ConfigData.Size; input_i++) 
+    {
+        ImFontConfig& cfg = atlas->ConfigData[input_i];
+        FreeTypeFont& font_face = fonts[input_i];
+        IM_ASSERT(cfg.DstFont && (!cfg.DstFont->IsLoaded() || cfg.DstFont->ContainerAtlas == atlas));
+    }
+    
+    // Render function
+// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
+void ImGui_ImplDX11_RenderDrawData(ImDrawData* draw_data)
+{
+    ID3D11DeviceContext* ctx = g_pd3dDeviceContext;
+    }
+    
+        // Create the root signature
+    {
+        D3D12_DESCRIPTOR_RANGE descRange = {};
+        descRange.RangeType = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;
+        descRange.NumDescriptors = 1;
+        descRange.BaseShaderRegister = 0;
+        descRange.RegisterSpace = 0;
+        descRange.OffsetInDescriptorsFromTableStart = 0;
+    }
+    
+    
+    {        // Rendering
+        ImGui::Render();
+        al_clear_to_color(al_map_rgba_f(clear_color.x, clear_color.y, clear_color.z, clear_color.w));
+        ImGui_ImplAllegro5_RenderDrawData(ImGui::GetDrawData());
+        al_flip_display();
+    }
+    
+            // Start the Dear ImGui frame
+        ImGui_ImplOpenGL2_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
