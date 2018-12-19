46f6e9729f5f702f866eee4a597f7c528bf7f48a
SUNsung <weatherius@gmail.com>
2018-12-19 06:45:38 +0200

    Rewrite [51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp] File in: 2018-12-19 06:45:38

--- cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
+++ cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
@@ -1,195 +1,24 @@
 
         
-        Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+        <details>
     
-    void CostAnalyzer::PreprocessCosts() {
-  for (int i = 0; i < op_perf_.op_performance_size(); i++) {
-    OpPerformance* perf = op_perf_.mutable_op_performance(i);
-    const OpPerformance& analytical = op_perf_analytical_.op_performance(i);
-    perf->set_compute_time(analytical.compute_time());
-    perf->set_memory_time(analytical.memory_time());
-    double measured_cost = perf->compute_cost();
-    }
-    }
-    
-    
-    {}  // namespace tensorflow
+    #endif // CAFFE2_OPERATORS_FREE_OP_H_
 
     
-    // Creates a tensor in 'ret' from the input Ndarray.
-Status NdarrayToTensor(PyObject* obj, Tensor* ret);
-    
-    #include 'tensorflow/c/c_api.h'
-#include 'tensorflow/core/lib/core/errors.h'
-#include 'tensorflow/core/platform/mutex.h'
-#include 'tensorflow/python/lib/core/bfloat16.h'
-#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
-    
-    
-    {  // Maps error codes to the corresponding Python exception type.
-  std::map<TF_Code, PyObject*> exc_types_;
+    class GetFloatToHalfGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'HalfToFloat', '', vector<string>{GO(0)}, vector<string>{GI(0)});
+  }
 };
+REGISTER_GRADIENT(FloatToHalf, GetFloatToHalfGradient);
     
-    #include 'tensorflow/core/framework/node_def.pb.h'
-#include 'tensorflow/core/framework/node_def_util.h'
-#include 'tensorflow/core/framework/op.h'
-#include 'tensorflow/core/framework/op_kernel.h'
-#include 'tensorflow/core/framework/types.h'
-#include 'tensorflow/core/lib/core/status.h'
-#include 'tensorflow/core/util/device_name_utils.h'
-    
-    // This file contains APIs that assume a StreamExecutor is backed by CUDA.
-// It reaches into the CUDA implementation to activate an underlying CUDA
-// context.
-//
-// Having this file separate from cuda_gpu_executor.h means that dependent
-// code does not also have to depend on cuda.h.
-    
-        enum BORDER_MODE
-    {
-        BORDER_MODE_UNDEFINED,
-        BORDER_MODE_CONSTANT,
-        BORDER_MODE_REPLICATE,
-        BORDER_MODE_REFLECT,
-        BORDER_MODE_REFLECT101,
-        BORDER_MODE_WRAP
-    };
-    
-    
-    {    void operator() (const u32 * src0, const u32 * src1, u32 * dst) const
-    {
-        dst[0] = saturate_cast<u32>(alpha*src0[0] + beta*src1[0] + gamma);
-    }
+    class GetCol2ImGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'Im2Col', '', std::vector<string>{GO(0)}, std::vector<string>{GI(0)});
+  }
 };
-    
-            for (; sj < roiw8; sj += 8, syj += 16, dj += 32)
-        {
-            uint8x8x2_t v_y = vld2_u8(srcy + syj);
-            uint8x8x4_t v_dst;
-            v_dst.val[0] = vld1_u8(srcu + sj);
-            v_dst.val[1] = v_y.val[0];
-            v_dst.val[2] = vld1_u8(srcv + sj);
-            v_dst.val[3] = v_y.val[1];
-            vst4_u8(dst + dj, v_dst);
-        }
-    
-    #endif
-    
-            int32x2_t vs1 = vmov_n_s32(0);
-        int32x2_t vs2 = vmov_n_s32(0);
-        int32x2_t vs3 = vmov_n_s32(0);
-        int32x2_t vs4 = vmov_n_s32(0);
-    
-    /*
-The references are:
- * Machine learning for high-speed corner detection,
-   E. Rosten and T. Drummond, ECCV 2006
- * Faster and better: A machine learning approach to corner detection
-   E. Rosten, R. Porter and T. Drummond, PAMI, 2009
-*/
-    
-    template <typename T>
-void process(const T * src, size_t j0, size_t j1, size_t i,
-             T minVal, size_t * minLocPtr, s32 & minLocCount, s32 minLocCapacity,
-             T maxVal, size_t * maxLocPtr, s32 & maxLocCount, s32 maxLocCapacity)
-{
-    for (size_t j = j0; j < j1; ++j)
-    {
-        T val = src[j];
-    }
-    }
-    
-    
-    {        u32 buf[8];
-        vst1_u32(buf, vget_low_u32(el8shr01l));
-        vst1_u32(buf+2, el2l);
-        vst1_u32(buf+4, el2hl);
-        vst1_u32(buf+6, el2hh);
-        for(u32 k=0; k < 8; k++)
-            sqsum[j+k] = prev + buf[k];
-        prev += buf[7];
-    }
-    
-    #pragma once
-    
-    Status WriteBatchBase::Merge(ColumnFamilyHandle* column_family,
-                             const SliceParts& key, const SliceParts& value) {
-  std::string key_buf, value_buf;
-  Slice key_slice(key, &key_buf);
-  Slice value_slice(value, &value_buf);
-    }
-    
-      // open DB
-  Status s = DB::Open(options, kDBPath, &db);
-  assert(s.ok());
-    
-    Status GetStringFromCompressionType(std::string* compression_str,
-                                    CompressionType compression_type);
-    
-    // This template function declaration is used in defining arraysize.
-// Note that the function doesn't need an implementation, as we only
-// use its type.
-template <typename T, size_t N>
-char (&ArraySizeHelper(T (&array)[N]))[N];
-    
-    bool FindBenchmarksInternal(const std::string& re,
-                            std::vector<Benchmark::Instance>* benchmarks,
-                            std::ostream* Err);
-    
-    Benchmark* Benchmark::Apply(void (*custom_arguments)(Benchmark* benchmark)) {
-  custom_arguments(this);
-  return this;
-}
-    
-    #define CHECK_EQ(a, b) CHECK((a) == (b))
-#define CHECK_NE(a, b) CHECK((a) != (b))
-#define CHECK_GE(a, b) CHECK((a) >= (b))
-#define CHECK_LE(a, b) CHECK((a) <= (b))
-#define CHECK_GT(a, b) CHECK((a) > (b))
-#define CHECK_LT(a, b) CHECK((a) < (b))
-    
-    // Returns the name of the environment variable corresponding to the
-// given flag.  For example, FlagToEnvVar('foo') will return
-// 'BENCHMARK_FOO' in the open-source version.
-static std::string FlagToEnvVar(const char* flag) {
-  const std::string flag_str(flag);
-    }
-    
-      if (reports[0].complexity == oLambda) {
-    result_cpu = MinimalLeastSq(n, cpu_time, reports[0].complexity_lambda);
-    result_real = MinimalLeastSq(n, real_time, reports[0].complexity_lambda);
-  } else {
-    result_cpu = MinimalLeastSq(n, cpu_time, reports[0].complexity);
-    result_real = MinimalLeastSq(n, real_time, result_cpu.complexity);
-  }
-  std::string benchmark_name =
-      reports[0].benchmark_name.substr(0, reports[0].benchmark_name.find('/'));
-    
-      if (result.error_occurred) {
-    printer(Out, COLOR_RED, 'ERROR OCCURRED: \'%s\'',
-            result.error_message.c_str());
-    printer(Out, COLOR_DEFAULT, '\n');
-    return;
-  }
-  // Format bytes per second
-  std::string rate;
-  if (result.bytes_per_second > 0) {
-    rate = StrCat(' ', HumanReadableNumber(result.bytes_per_second), 'B/s');
-  }
-    
-    #include <cstdint>
-    
-    double BenchmarkReporter::Run::GetAdjustedRealTime() const {
-  double new_time = real_accumulated_time * GetTimeUnitMultiplier(time_unit);
-  if (iterations != 0) new_time /= static_cast<double>(iterations);
-  return new_time;
-}
-    
-    
-    {void SleepForSeconds(double seconds) {
-  SleepForMicroseconds(static_cast<int>(seconds * kNumMicrosPerSecond));
-}
-#endif  // BENCHMARK_OS_WINDOWS
-}  // end namespace benchmark
+REGISTER_GRADIENT(Col2Im, GetCol2ImGradient);
