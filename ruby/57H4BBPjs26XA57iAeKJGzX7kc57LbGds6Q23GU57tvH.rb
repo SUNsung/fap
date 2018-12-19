391692d17622b5d015308c1c9259b32975e839ef
SUNsung <weatherius@gmail.com>
2018-12-19 07:36:18 +0200

    Rewrite [57H4BBPjs26XA57iAeKJGzX7kc57LbGds6Q23GU57tvH.rb] File in: 2018-12-19 07:36:18

--- ruby/57H4BBPjs26XA57iAeKJGzX7kc57LbGds6Q23GU57tvH.rb
+++ ruby/57H4BBPjs26XA57iAeKJGzX7kc57LbGds6Q23GU57tvH.rb
@@ -1,166 +1,147 @@
 
         
-            it 'works for running jobs' do
-      job.locked_at = Time.now
-      job.locked_by = 'test'
-      expect(status(job)).to eq('<span class='label label-info'>running</span>')
-    end
+              new_path = File.join(FastlaneCore.fastlane_user_dir, file_name)
+      did_show = File.exist?(new_path)
     
-        it 'interpolates jsonpath expressions between matching <>'s' do
-      expect(Utils.interpolate_jsonpaths('hello <$.there.world> this <escape works>', payload)).to eq('hello WORLD this should+work')
-    end
-    
-          it 'should not allow source agents owned by other people' do
-        agent = Agents::SomethingSource.new(:name => 'something')
-        agent.user = users(:bob)
-        agent.source_ids = [agents(:bob_weather_agent).id]
-        expect(agent).to have(0).errors_on(:sources)
-        agent.source_ids = [agents(:jane_weather_agent).id]
-        expect(agent).to have(1).errors_on(:sources)
-        agent.user = users(:jane)
-        expect(agent).to have(0).errors_on(:sources)
+          it 'handles the extensions parameter with no elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', extensions: [])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
       end
     
-    When /^I fill out change password section with my password and '([^']*)' and '([^']*)'$/ do |new_pass, confirm_pass|
-  fill_change_password_section(@me.password, new_pass, confirm_pass)
-end
+          it 'generates the correct git command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: './fastlane/README.md', message: 'message')
+        end').runner.execute(:test)
     
-    Given /^I have been invited by an admin$/ do
-  admin = FactoryGirl.create(:user)
-  admin.invitation_code
-  i = EmailInviter.new('new_invitee@example.com', admin)
-  i.send!
-end
+            context 'with given path to oclint' do
+          let(:result) do
+            Fastlane::FastFile.new.parse('lane :test do
+              oclint(
+                compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+                oclint_path: 'test/bin/oclint'
+              )
+            end').runner.execute(:test)
+          end
+          let(:command) { 'cd #{File.expand_path('.').shellescape} && test/bin/oclint -report-type=html -o=oclint_report.html' }
     
-        it 'generates a jasmine fixture', :fixture => true do
-      get :bookmarklet
-      save_fixture(html_for('body'), 'bookmarklet')
+        context 'with a postfix block' do
+      it 'yields the status, result and command' do
+        expect_command('ls', '-la')
+        Fastlane::Actions.sh('ls', '-la') do |status, result, command|
+          expect(status.exitstatus).to eq(0)
+          expect(result).to be_empty
+          expect(command).to eq('ls -la')
+        end
+      end
+    
+        os = 'windows'
+    shellescape_testcases.each do |testcase|
+      it testcase['it'] + ': ' + testcase['it_result'][os] do
+        str = testcase['str'].to_s
+        expect_correct_implementation_to_be_called(str, :shellescape, os)
+        escaped = str.shellescape
+        expect(escaped).to eq(testcase['expect'][os])
+      end
+    end
+  end
+    
+        def current_url
+      context[:url]
     end
     
-    desc 'list of authors'
-task :authors, [:commit_range, :format, :sep] do |t, a|
-  a.with_defaults :format => '%s (%d)', :sep => ', ', :commit_range => '--all'
-  authors = Hash.new(0)
-  blake   = 'Blake Mizerany'
-  overall = 0
-  mapping = {
-    'blake.mizerany@gmail.com' => blake, 'bmizerany' => blake,
-    'a_user@mac.com' => blake, 'ichverstehe' => 'Harry Vangberg',
-    'Wu Jiang (nouse)' => 'Wu Jiang' }
-  `git shortlog -s #{a.commit_range}`.lines.map do |line|
-    line = line.force_encoding 'binary' if line.respond_to? :force_encoding
-    num, name = line.split('\t', 2).map(&:strip)
-    authors[mapping[name] || name] += num.to_i
-    overall += num.to_i
+        private
+    
+            doc
+      end
+    end
   end
-  puts '#{overall} commits by #{authors.count} authors:'
-  puts authors.sort_by { |n,c| -c }.map { |e| a.format % e }.join(a.sep)
-end
-    
-      // writing
-  $('form').on('submit',function(e) {
-    $.post('/', {msg: '<%= user %>: ' + $('#msg').val()});
-    $('#msg').val(''); $('#msg').focus();
-    e.preventDefault();
-  });
-</script>
-    
-    class Rack::Builder
-  include Sinatra::Delegator
 end
 
     
-        def frame_class(frame)
-      if frame.filename =~ %r{lib/sinatra.*\.rb}
-        'framework'
-      elsif (defined?(Gem) && frame.filename.include?(Gem.dir)) ||
-            frame.filename =~ %r{/bin/(\w+)\z}
-        'system'
-      else
-        'app'
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
+  end
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
+  end
+end
+    
+        context 'on a public post from a stranger' do
+      before do
+        @post = stranger.post :status_message, :text => 'something', :public => true, :to => 'all'
       end
-    end
     
-          private
+              code = InvitationCode.create(user: bob)
     
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
+    Then(/^the invalid (.+) release is ignored$/) do |filename|
+  test = 'ls -g #{TestApp.releases_path} | grep #{filename}'
+  _, _, status = vagrant_cli_command('ssh -c #{test.shellescape}')
+  expect(status).to be_success
+end
+    
+        def backtrace_pattern
+      loc = Rake.application.find_rakefile_location
+      return unless loc
+    
+          def question
+        if default.nil?
+          I18n.t(:question, key: key, scope: :capistrano)
+        else
+          I18n.t(:question_default, key: key, default_value: default, scope: :capistrano)
         end
-        bad_cookies.empty?
       end
+    
+          class Properties
+        def initialize
+          @properties = {}
+        end
     
     module Rack
   module Protection
     ##
-    # Prevented attack::   CSRF
+    # Prevented attack::   Cookie Tossing
     # Supported browsers:: all
-    # More infos::         http://flask.pocoo.org/docs/0.10/security/#json-security
-    #                      http://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx
+    # More infos::         https://github.com/blog/1466-yummy-cookies-across-domains
     #
-    # JSON GET APIs are vulnerable to being embedded as JavaScript when the
-    # Array prototype has been patched to track data. Checks the referrer
-    # even on GET requests if the content type is JSON.
+    # Does not accept HTTP requests if the HTTP_COOKIE header contains more than one
+    # session cookie. This does not protect against a cookie overflow attack.
     #
-    # If request includes Origin HTTP header, defers to HttpOrigin to determine
-    # if the request is safe. Please refer to the documentation for more info.
+    # Options:
     #
-    # The `:allow_if` option can be set to a proc to use custom allow/deny logic.
-    class JsonCsrf < Base
-      default_options :allow_if => nil
+    # session_key:: The name of the session cookie (default: 'rack.session')
+    class CookieTossing < Base
+      default_reaction :deny
     
-        it 'Returns nil when Referer header is invalid' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
-      expect(subject.referrer(env)).to be_nil
-    end
-  end
-end
-
-    
-      def self.source_root
-    @source_root ||= File.expand_path('../templates', __FILE__)
-  end
-    
-        # Returns a String describing the file's content type
-    def detect
-      if blank_name?
-        SENSIBLE_DEFAULT
-      elsif empty_file?
-        EMPTY_TYPE
-      elsif calculated_type_matches.any?
-        calculated_type_matches.first
-      else
-        type_from_file_contents || SENSIBLE_DEFAULT
-      end.to_s
-    end
-    
-        # Perform the actual interpolation. Takes the pattern to interpolate
-    # and the arguments to pass, which are the attachment and style name.
-    # You can pass a method name on your record as a symbol, which should turn
-    # an interpolation pattern for Paperclip to use.
-    def self.interpolate pattern, *args
-      pattern = args.first.instance.send(pattern) if pattern.kind_of? Symbol
-      result = pattern.dup
-      interpolators_cache.each do |method, token|
-        result.gsub!(token) { send(method, *args) } if result.include?(token)
+          explicit_plugins_specs.each do |spec|
+        packet_gem.add(spec.name)
       end
-      result
-    end
     
-      expansion(:attachments) {
-    o.attachments.map do |attachment|
-      {
-        :filename => attachment.filename.to_s,
-        :content_type => attachment.mime_type,
-        :data => Base64.encode64(attachment.body.to_s),
-        :size => attachment.body.to_s.bytesize,
-        :hash => Digest::SHA1.hexdigest(attachment.body.to_s)
-      }
+        puts('Generated at #{target_file}')
+  end
+    
+        def execute
+      raise PluginManager::FileNotFoundError, 'Can't file local file #{local_file}' unless ::File.exist?(local_file)
+      raise PluginManager::InvalidPackError, 'Invalid format, the pack must be in zip format' unless valid_format?(local_file)
+    
+        # remove any version constrain from the Gemfile so the plugin(s) can be updated to latest version
+    # calling update without requirements will remove any previous requirements
+    plugins = plugins_to_update(previous_gem_specs_map)
+    # Skipping the major version validation when using a local cache as we can have situations
+    # without internet connection.
+    filtered_plugins = plugins.map { |plugin| gemfile.find(plugin) }
+      .compact
+      .reject { |plugin| REJECTED_OPTIONS.any? { |key| plugin.options.has_key?(key) } }
+      .each   { |plugin| gemfile.update(plugin.name) }
+    
+          it 'display a list of installed plugins' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
+        expect(result.stdout.split('\n').size).to be > 1
+      end
+    
+    shared_examples 'logstash update' do |logstash|
+  describe 'logstash-plugin update on #{logstash.hostname}' do
+    before :each do
+      logstash.install({:version => LOGSTASH_VERSION})
     end
-  }
