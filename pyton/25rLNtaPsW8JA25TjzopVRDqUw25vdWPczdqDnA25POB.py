b2ef8d7236ad26e0f074a1df12517595a3f91f5c
SUNsung <weatherius@gmail.com>
2018-12-20 07:50:05 +0200

    Rewrite [25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py] File in: 2018-12-20 07:50:03

--- pyton/25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py
+++ pyton/25rLNtaPsW8JA25TjzopVRDqUw25vdWPczdqDnA25POB.py
@@ -1,198 +1,166 @@
 
         
-                    k_results_build[algorithm][i] = (t1 - t0)
-            k_results_query[algorithm][i] = (t2 - t1)
+        from django.db import connections
+from django.db.backends.base.base import NO_DB_ALIAS
     
-        plt.subplots_adjust(0.1, 0.08, 0.96, 0.98, 0.4, 0.63)
-    ax = plt.axes([0.1, 0.08, 0.8, 0.06])
-    plt.colorbar(cax=ax, orientation='horizontal')
-    plt.show()
+        def __init__(self, get_response=None):
+        if not apps.is_installed('django.contrib.sites'):
+            raise ImproperlyConfigured(
+                'You cannot use RedirectFallbackMiddleware when '
+                'django.contrib.sites is not installed.'
+            )
+        super().__init__(get_response)
+    
+    
+def prepare_url(value):
+    # Issue #1483: Make sure the URL always has a trailing slash
+    httpbin_url = value.url.rstrip('/') + '/'
+    
+                if self.wait_to_close_event:
+                self.wait_to_close_event.wait(self.WAIT_EVENT_TIMEOUT)
+        finally:
+            self.ready_event.set() # just in case of exception
+            self._close_server_sock_ignore_errors()
+            self.stop_event.set()
+    
+        # Check chardet for compatibility.
+    major, minor, patch = chardet_version.split('.')[:3]
+    major, minor, patch = int(major), int(minor), int(patch)
+    # chardet >= 3.0.2, < 3.1.0
+    assert major == 3
+    assert minor < 1
+    assert patch >= 2
+    
+            :rtype: requests.Response
+        '''
+    
+    
+def morsel_to_cookie(morsel):
+    '''Convert a Morsel object into a Cookie containing the one k/v pair.'''
+    
+    
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or {}
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
 
     
-        ###########################################################################
-    # Set custom reservoir based method
-    sampling_algorithm['custom-pool'] = \
-        lambda n_population, n_samples, random_state=None: \
-            sample_without_replacement(n_population,
-                                       n_samples,
-                                       method='pool',
-                                       random_state=random_state)
+        @pytest.mark.skip(reason='this fails non-deterministically under pytest-xdist')
+    def test_request_recovery(self):
+        '''can check the requests content'''
+        # TODO: figure out why this sometimes fails when using pytest-xdist.
+        server = Server.basic_response_server(requests_to_handle=2)
+        first_request = b'put your hands up in the air'
+        second_request = b'put your hand down in the floor'
     
-        plt.show()
+        def test_requests_history_is_saved(self, httpbin):
+        r = requests.get(httpbin('redirect/5'))
+        total = r.history[-1].history
+        i = 0
+        for item in r.history:
+            assert item.history == total[0:i]
+            i += 1
+    
+    site_info = 'CBS.com'
+download = cbs_download
+download_playlist = playlist_not_supported('cbs')
 
     
-    input data sparsity: 0.050000
-true coef sparsity: 0.000100
-test data sparsity: 0.027400
-model sparsity: 0.000024
-r^2 on test data (dense model) : 0.233651
-r^2 on test data (sparse model) : 0.233651
-Wrote profile results to sparsity_benchmark.py.lprof
-Timer unit: 1e-06 s
-    
-        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
-                                   'blob/{revision}/{package}/'
-                                   '{path}#L{lineno}')
-    '''
-    revision = _get_git_revision()
-    return partial(_linkcode_resolve, revision=revision, package=package,
-                   url_fmt=url_fmt)
-
-    
-        short_text_lang_folder = os.path.join(short_text_folder, lang)
-    if not os.path.exists(short_text_lang_folder):
-        os.makedirs(short_text_lang_folder)
-    
-        def test_nested_exception(self):
-        def f(p):
-            1/0
-        f_ident = ident(f)
-        self.check_events(f, [(1, 'call', f_ident),
-                              # This isn't what I expected:
-                              # (0, 'exception', protect_ident),
-                              # I expected this again:
-                              (1, 'return', f_ident),
-                              ])
-    
-            # Faulthandler
         try:
-            import faulthandler
-        except ImportError:
-            pass
-        else:
-            code = 'import faulthandler; print(faulthandler.is_enabled())'
-            out = self.run_xdev('-c', code)
-            self.assertEqual(out, 'True')
+        json_data = get_coub_data(html)
+        title, video_url, audio_url = get_title_and_urls(json_data)
+        video_file_name, video_file_path = get_file_path(merge, output_dir, title, video_url)
+        audio_file_name, audio_file_path = get_file_path(merge, output_dir, title, audio_url)
+        download_url(audio_url, merge, output_dir, title, info_only)
+        download_url(video_url, merge, output_dir, title, info_only)
+        if not info_only:
+            try:
+                fix_coub_video_file(video_file_path)
+                audio_duration = float(ffmpeg.ffprobe_get_media_duration(audio_file_path))
+                video_duration = float(ffmpeg.ffprobe_get_media_duration(video_file_path))
+                loop_file_path = get_loop_file_path(title, output_dir)
+                single_file_path = audio_file_path
+                if audio_duration > video_duration:
+                    write_loop_file(int(audio_duration / video_duration), loop_file_path, video_file_name)
+                else:
+                    single_file_path = audio_file_path
+                    write_loop_file(int(video_duration / audio_duration), loop_file_path, audio_file_name)
     
-        print('Pickled records:')
-    pprint.pprint(memos)
+    def rebuilt_url(url):
+    path = urllib.parse.urlparse(url).path
+    aid = path.split('/')[-1].split('_')[0]
+    return 'http://www.dailymotion.com/embed/video/{}?autoplay=1'.format(aid)
     
-    # Add the html version.  This converts the message into a multipart/alternative
-# container, with the original text message as the first part and the new html
-# message as the second part.
-asparagus_cid = make_msgid()
-msg.add_alternative('''\
-<html>
-  <head></head>
-  <body>
-    <p>Salut!</p>
-    <p>Cela ressemble à un excellent
-        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
-            recipie
-        </a> déjeuner.
-    </p>
-    <img src='cid:{asparagus_cid}' />
-  </body>
-</html>
-'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
-# note that we needed to peel the <> off the msgid for use in the html.
+    __all__ = ['ehow_download']
     
-    buffer = ''
+        if int(html['status']) != 100000:
+        raise Exception('API error!')
     
-    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
-    host = r1(r'host=([^&]*)', html)
-    assert host
-    type = r1(r'videofiletype=([^&]*)', html)
-    assert type
-    vid = r1(r'&stream_name=([^&]*)', html)
-    assert vid
-    dir_str = r1(r'&dir=([^&]*)', html).strip()
-    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
-    _, ext, size = url_info(url)
-    print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
+    pattern_url_photoset = r'https?://www\.flickr\.com/photos/.+/(?:(?:sets)|(?:albums))?/([^/]+)'
+pattern_url_photostream = r'https?://www\.flickr\.com/photos/([^/]+)(?:/|(?:/page))?$'
+pattern_url_single_photo = r'https?://www\.flickr\.com/photos/[^/]+/(\d+)'
+pattern_url_gallery = r'https?://www\.flickr\.com/photos/[^/]+/galleries/(\d+)'
+pattern_url_group = r'https?://www\.flickr\.com/groups/([^/]+)'
+pattern_url_favorite = r'https?://www\.flickr\.com/photos/([^/]+)/favorites'
+    
+        _class_method_choices = {'param_value_1': _class_method_1, 'param_value_2': _class_method_2}
     
     
-    def extract(self, **kwargs):
-        for i in self.streams:
-            s = self.streams[i]
-            _, s['container'], s['size'] = url_info(s['url'])
-            s['src'] = [s['url']]
-        if 'stream_id' in kwargs and kwargs['stream_id']:
-            # Extract the stream
-            stream_id = kwargs['stream_id']
+def count_to(count):
+    '''Counts by word numbers, up to a maximum of five'''
+    numbers = ['one', 'two', 'three', 'four', 'five']
+    for number in numbers[:count]:
+        yield number
     
-    __all__ = ['cbs_download']
+        def run(self, msg):
+        print('{} got {}'.format(self.name, msg))
     
-    from .bilibili import bilibili_download
-from .dailymotion import dailymotion_download
-from .iqiyi import iqiyi_download_by_vid
-from .le import letvcloud_download_by_vu
-from .netease import netease_download
-from .qq import qq_download_by_vid
-from .sina import sina_download_by_vid
-from .tudou import tudou_download_by_id
-from .vimeo import vimeo_download_by_id
-from .yinyuetai import yinyuetai_download_by_id
-from .youku import youku_download_by_vid
-from . import iqiyi
-from . import bokecc
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+        '''
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
     
-        @classmethod
-    def funshion_decrypt(cls, a_bytes, coeff):
-        res_list = []
-        pos = 0
-        while pos < len(a_bytes):
-            a = a_bytes[pos]
-            if pos == len(a_bytes) - 1:
-                res_list.append(a)
-                pos += 1
-            else:
-                b = a_bytes[pos + 1]
-                m = a * coeff[0] + b * coeff[2]
-                n = a * coeff[1] + b * coeff[3]
-                res_list.append(m & 0xff)
-                res_list.append(n & 0xff)
-                pos += 2
-        return bytes(res_list).decode('utf8')
-    
-    __all__ = ['giphy_download']
-    
-                post_url = r1(r''(https://plus.google.com/[^/]+/posts/[^']*)'', html)
-            post_author = r1(r'/\+([^/]+)/posts', post_url)
-            if post_author:
-                post_url = 'https://plus.google.com/+%s/posts/%s' % (parse.quote(post_author), r1(r'posts/(.+)', post_url))
-            post_html = get_html(post_url, faker=True)
-            title = r1(r'<title[^>]*>([^<\n]+)', post_html)
-    
-    *TL;DR80
-Describes a group of objects that is treated as a single instance.
-'''
-    
-        client1.delegate(requests)
-    print('-' * 30)
-    client2.delegate(requests)
-    
-    print()
+    '''
+*What is this pattern about?
+This pattern aims to encapsulate each algorithm and allow them to be
+interchangeable. Separating algorithms allows the client to scale
+with larger and more complex algorithms, since the client and the
+strategies are kept independent of each other.
     
     ### OUTPUT ###
-# Setting Data 1 = 10
-# DecimalViewer: Subject Data 1 has data 10
-# HexViewer: Subject Data 1 has data 0xa
-# Setting Data 2 = 15
-# HexViewer: Subject Data 2 has data 0xf
-# DecimalViewer: Subject Data 2 has data 15
-# Setting Data 1 = 3
-# DecimalViewer: Subject Data 1 has data 3
-# HexViewer: Subject Data 1 has data 0x3
-# Setting Data 2 = 5
-# HexViewer: Subject Data 2 has data 0x5
-# DecimalViewer: Subject Data 2 has data 5
-# Detach HexViewer from data1 and data2.
-# Setting Data 1 = 10
-# DecimalViewer: Subject Data 1 has data 10
-# Setting Data 2 = 15
-# DecimalViewer: Subject Data 2 has data 15
+# generic_visit A
+# visit_B B
+# visit_B C
 
     
-    *References:
-http://stackoverflow.com/questions/1514120/python-implementation-of-the-object-pool-design-pattern
-https://sourcemaking.com/design_patterns/object_pool
+    # Create a random animal
+def random_animal():
+    '''Let's be dynamic!'''
+    return random.choice([Dog, Cat])()
+    
+    The first example achieves this by using an abstract base
+class for a building, where the initializer (__init__ method) specifies the
+steps needed, and the concrete subclasses implement these steps.
+    
+        def get_current_time_as_html_fragment(self):
+        current_time = self.time_provider.now()
+        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
+        return current_time_as_html_fragment
+'''
     
     
-class Student(AbstractExpert):
-    @property
-    def is_eager_to_contribute(self):
-        return True
+class Standby(Inservice):
+    def __init__(self, HierachicalStateMachine):
+        self._hsm = HierachicalStateMachine
