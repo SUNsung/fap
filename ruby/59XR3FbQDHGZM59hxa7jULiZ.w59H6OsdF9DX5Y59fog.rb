51e95dc33bd50cdf1bfdde024d080ab4a5b4012f
SUNsung <weatherius@gmail.com>
2018-12-20 23:18:47 +0200

    Rewrite [59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb] File in: 2018-12-20 23:18:46

--- ruby/59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb
+++ ruby/59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb
@@ -1,61 +1,223 @@
 
         
-                expect(result).to eq('carthage bootstrap')
+          Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
+    cmd = g.split('-').last
+    p.command(cmd.to_sym) do |c|
+      c.syntax cmd
+      c.action do
+        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
+          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
       end
-    
-          it 'Does not accept an invalid value for :merge_commit_filtering' do
-        values = Fastlane::Actions::GIT_MERGE_COMMIT_FILTERING_OPTIONS.map { |o| ''#{o}'' }.join(', ')
-        error_msg = 'Valid values for :merge_commit_filtering are #{values}'
-    
-            allow(File).to receive(:file?).and_return(false)
-        allow(File).to receive(:file?).with(keychain_path).and_return(true)
-        allow(File).to receive(:exist?).and_return(false)
-        expect(File).to receive(:exist?).with(cert_name).and_return(true)
-        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_set_key_partition_list_command, print: false)
-        expect(FastlaneCore::Helper).to receive(:backticks).with(expected_security_import_command, print: false)
-    
-    module CrossplatformShellwords
-  # handle switching between implementations of shellescape
-  def shellescape(str)
-    if FastlaneCore::Helper.windows?
-      WindowsShellwords.shellescape(str)
-    else
-      # using `escape` instead of expected `shellescape` here
-      # which corresponds to Shellword's `String.shellescape` implementation
-      # https://github.com/ruby/ruby/blob/1cf2bb4b2085758112503e7da7414d1ef52d4f48/lib/shellwords.rb#L216
-      Shellwords.escape(str)
     end
   end
-  module_function :shellescape
     
-          it 'uses the correct command to import it' do
-        # We have to execute *something* using ` since otherwise we set expectations to `nil`, which is not healthy
-        `ls`
+    #
     
-              config = FastlaneCore::Configuration.create(c, {
-            true_value: 'true',
-            true_value2: 'YES',
-            false_value: 'false',
-            false_value2: 'NO'
-          })
+              # WebSockets requests will have a Connection: Upgrade header
+          if parser.http_method != 'GET' || parser.upgrade?
+            super
+          elsif parser.request_url =~ %r!^\/livereload.js!
+            headers = [
+              'HTTP/1.1 200 OK',
+              'Content-Type: application/javascript',
+              'Content-Length: #{reload_size}',
+              '',
+              '',
+            ].join('\r\n')
+            send_data(headers)
     
-    module Admin
-  class ConfirmationsController < BaseController
-    before_action :set_user
-    before_action :check_confirmation, only: [:resend]
-    
-        def set_custom_emoji
-      @custom_emoji = CustomEmoji.find(params[:id])
+          keg_only_deps.each do |dep|
+        ENV.prepend_path 'PATH', dep.opt_bin.to_s
+        ENV.prepend_path 'PKG_CONFIG_PATH', '#{dep.opt_lib}/pkgconfig'
+        ENV.prepend_path 'PKG_CONFIG_PATH', '#{dep.opt_share}/pkgconfig'
+        ENV.prepend_path 'ACLOCAL_PATH', '#{dep.opt_share}/aclocal'
+        ENV.prepend_path 'CMAKE_PREFIX_PATH', dep.opt_prefix.to_s
+        ENV.prepend 'LDFLAGS', '-L#{dep.opt_lib}' if dep.opt_lib.directory?
+        ENV.prepend 'CPPFLAGS', '-I#{dep.opt_include}' if dep.opt_include.directory?
+      end
     end
     
-          redirect_to admin_report_path(@report)
+    module Homebrew
+  def build_env_keys(env)
+    %w[
+      CC CXX LD OBJC OBJCXX
+      HOMEBREW_CC HOMEBREW_CXX
+      CFLAGS CXXFLAGS CPPFLAGS LDFLAGS SDKROOT MAKEFLAGS
+      CMAKE_PREFIX_PATH CMAKE_INCLUDE_PATH CMAKE_LIBRARY_PATH CMAKE_FRAMEWORK_PATH
+      MACOSX_DEPLOYMENT_TARGET PKG_CONFIG_PATH PKG_CONFIG_LIBDIR
+      HOMEBREW_DEBUG HOMEBREW_MAKE_JOBS HOMEBREW_VERBOSE
+      HOMEBREW_SVN HOMEBREW_GIT
+      HOMEBREW_SDKROOT HOMEBREW_BUILD_FROM_SOURCE
+      MAKE GIT CPP
+      ACLOCAL_PATH PATH CPATH].select { |key| env.key?(key) }
+  end
+    
+      def bash_completion_caveats
+    if keg && keg.completion_installed?(:bash) then <<-EOS.undent
+      Bash completion has been installed to:
+        #{HOMEBREW_PREFIX}/etc/bash_completion.d
+      EOS
+    end
+  end
+    
+        updated_taps = []
+    Tap.each do |tap|
+      next unless tap.git?
+      begin
+        reporter = Reporter.new(tap)
+      rescue Reporter::ReporterRevisionUnsetError => e
+        onoe e if ARGV.homebrew_developer?
+        next
+      end
+      if reporter.updated?
+        updated_taps << tap.name
+        hub.add(reporter)
+      end
     end
     
-      def account_feed_path?
-    hub_topic_params[:controller] == 'accounts' && hub_topic_params[:action] == 'show' && hub_topic_params[:format] == 'atom'
+        path = Commands.path(cmd)
+    
+          if old_path.git?
+        puts 'Changing remote from #{old_remote} to #{new_remote}...' if $stdout.tty?
+        old_path.git_origin = new_remote
+      end
+    
+        def log_transform(*args, from: caller[1][/`.*'/][1..-2].sub(/^block in /, ''))
+      puts '    #{cyan from}#{cyan ': #{args * ', '}' unless args.empty?}'
+    end
+    
+      # Enable Rack::Cache to put a simple HTTP cache in front of your application
+  # Add `rack-cache` to your Gemfile before enabling this.
+  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+  # config.action_dispatch.rack_cache = true
+    
+      gem 'rb-fsevent'
+  gem 'kicker'
+  gem 'awesome_print'
+  gem 'ruby-prof', :platforms => [:ruby]
+end
+
+    
+        pod 'Alamofire', path: '../Alamofire Example/Alamofire'
+    
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
+          end
+        end
+    
+          # private
+    
+          def title
+        '#{@page.title}'
+      end
+    
+      test 'remove page extentions' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'page', view.remove_page_extentions('page.wiki')
+    assert_equal 'page-wiki', view.remove_page_extentions('page-wiki.md')
+    assert_equal 'file.any_extention', view.remove_page_extentions('file.any_extention')
+  end
+    
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
+  end
+    
+      s.rdoc_options = ['--charset=UTF-8']
+  s.extra_rdoc_files = %w[README.md LICENSE]
+    
+    end
+    
+    class ConfigTag < Liquid::Tag
+  def initialize(tag_name, options, tokens)
+    super
+    options = options.split(' ').map {|i| i.strip }
+    @key = options.slice!(0)
+    @tag = nil
+    @classname = nil
+    options.each do |option|
+      @tag = $1 if option =~ /tag:(\S+)/ 
+      @classname = $1 if option =~ /classname:(\S+)/
+    end
+  end
+    
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
+    end
+  end
+end
+    
+        def poster
+      'poster='#{@poster}'' if @poster
+    end
+    
+    When /^(?:|I )press '([^']*)'$/ do |button|
+  click_button(button)
+end
+    
+    ENV['RAILS_ENV'] = 'test'
+    
+    class PaperclipGenerator < ActiveRecord::Generators::Base
+  desc 'Create a migration to add paperclip-specific fields to your model. ' +
+       'The NAME argument is the name of your model, and the following ' +
+       'arguments are the name of the attachments'
+    
+    # This stuff needs to be run after Paperclip is defined.
+require 'paperclip/io_adapters/registry'
+require 'paperclip/io_adapters/abstract_adapter'
+require 'paperclip/io_adapters/empty_string_adapter'
+require 'paperclip/io_adapters/identity_adapter'
+require 'paperclip/io_adapters/file_adapter'
+require 'paperclip/io_adapters/stringio_adapter'
+require 'paperclip/io_adapters/data_uri_adapter'
+require 'paperclip/io_adapters/nil_adapter'
+require 'paperclip/io_adapters/attachment_adapter'
+require 'paperclip/io_adapters/uploaded_file_adapter'
+require 'paperclip/io_adapters/uri_adapter'
+require 'paperclip/io_adapters/http_url_proxy_adapter'
+
+    
+        def definitions_for(klass)
+      parent_classes = klass.ancestors.reverse
+      parent_classes.each_with_object({}) do |ancestor, inherited_definitions|
+        inherited_definitions.deep_merge! @attachments[ancestor]
+      end
+    end
   end
 end
 
     
-          def response
-        return @response if defined? @response
+        def initialize(filepath)
+      @filepath = filepath
+    end
+    
+        def cropping dst, ratio, scale
+      if ratio.horizontal? || ratio.square?
+        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
+      else
+        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
+      end
+    end
+    
+            if attachment.nil?
+          attachment = Attachment.new(name, self, options)
+          instance_variable_set(ivar, attachment)
+        end
