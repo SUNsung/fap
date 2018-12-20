e828740de24052eb34d78581f2bc90496f595bbd
SUNsung <weatherius@gmail.com>
2018-12-20 22:35:19 +0200

    Rewrite [41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb] File in: 2018-12-20 22:35:19

--- ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
+++ ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
@@ -1,153 +1,175 @@
 
         
-            def types_as_json
-      @types.values.sort! { |a, b| sort_fn(a.name, b.name) }.map(&:as_json)
+              File.write(new_path, '1')
+      false
     end
     
-            # Returns the instance variables as a hash of key-value pairs.
-        def instance_variables_hash
-          instance_variables.inject({}) do |acc, iv|
-            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
-            acc
-          end
+          xcode_outdated = false
+      begin
+        unless FastlaneCore::Helper.xcode_at_least?(Fastlane::MINIMUM_XCODE_RELEASE)
+          xcode_outdated = true
         end
+      rescue
+        # We don't care about exceptions here
+        # We'll land here if the user doesn't have Xcode at all for example
+        # which is fine for someone who uses fastlane just for Android project
+        # What we *do* care about is when someone links an old version of Xcode
+      end
     
-        case itype
-    when IAX_TYPE_DTMF_BEGIN
-      self.dprint('DTMF BEG: #{pkt[11,1]}')
-      self.dtmf << pkt[11,1]
+            result = Fastlane::FastFile.new.parse('lane :test do
+          add_git_tag ({
+            tag: '#{tag}',
+          })
+        end').runner.execute(:test)
     
+          it 'does not add a cache_builds flag to command if cache_builds is set to false' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            carthage(
+              cache_builds: false
+            )
+          end').runner.execute(:test)
     
-end
-end
+          it 'generates the correct git command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: './fastlane/README.md', message: 'message')
+        end').runner.execute(:test)
+    
+          it 'works with select regex when regex is string' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+              select_regex: \'\/AppDelegate\'
+            )
+          end').runner.execute(:test)
+    
+          it '#environment_variable_name_profile_name uses the correct env variable' do
+        result = Match::Utils.environment_variable_name_profile_name(app_identifier: 'tools.fastlane.app', type: 'appstore')
+        expect(result).to eq('sigh_tools.fastlane.app_appstore_profile-name')
+      end
+    
+    if File.exist?(file_path)
+  junit.parse(file_path)
+  junit.headers = [:name, :file]
+  junit.report
+else
+  puts('Couldn't find any test artifacts in path #{file_path}')
 end
 
     
-              private
-    
-                int
-          end
-    
-              # Decodes the key from an OpenSSL::ASN1::ASN1Data
+              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
           #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [EncryptionKey]
-          def decode_key(input)
-            Rex::Proto::Kerberos::Model::EncryptionKey.decode(input.value[0])
+          # @return [String] encoded principal
+          def encode
+            encoded = ''
+            encoded << encode_name_type
+            encoded << [components.length].pack('N')
+            encoded << encode_realm
+            encoded << encode_components
+    
+                encoded
           end
     
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of an encrypted message.
-        class EncryptedData < Element
-          # @!attribute name_type
-          #   @return [Integer] The encryption algorithm
-          attr_accessor :etype
-          # @!attribute kvno
-          #   @return [Integer] The version number of the key
-          attr_accessor :kvno
-          # @!attribute cipher
-          #   @return [String] The enciphered text
-          attr_accessor :cipher
-    
-                seq_values.each do |val|
-              case val.tag
-              when 0
-                self.options = decode_options(val)
-              when 1
-                self.cname = decode_cname(val)
-              when 2
-                self.realm = decode_realm(val)
-              when 3
-                self.sname = decode_sname(val)
-              when 4
-                self.from = decode_from(val)
-              when 5
-                self.till = decode_till(val)
-              when 6
-                self.rtime = decode_rtime(val)
-              when 7
-                self.nonce = decode_nonce(val)
-              when 8
-                self.etype = decode_etype(val)
-              when 10
-                self.enc_auth_data = decode_enc_auth_data(val)
-              else
-                raise ::RuntimeError, 'Failed to decode KdcRequestBody SEQUENCE'
-              end
-            end
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::KrbError from an String
+              # Decodes a Rex::Proto::Kerberos::Model::LastReque from an String
           #
           # @param input [String] the input to decode from
           def decode_string(input)
             asn1 = OpenSSL::ASN1.decode(input)
     
-        # Create a {Sass::Selector::CommaSequence} containing only a single
-    # {Sass::Selector::Sequence}.
-    #
-    # @param sseqs [Array<Sass::Selector::Sequence, String>]
-    # @return [Sass::Selector::CommaSequence]
-    def make_seq(*sseqs)
-      make_cseq(Sass::Selector::Sequence.new(sseqs))
-    end
+    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
+Gem.use_paths(LogStash::Environment.logstash_gem_home)
     
-              def set_local_#{name}(name, value)
-            @#{name}s ||= {}
-            @#{name}s[name.tr('_', '-')] = value
+          packet_gem.pack
+    
+          PluginManager.ui.info('Installing file: #{local_file}')
+      uncompressed_path = uncompress(local_file)
+      PluginManager.ui.debug('Pack uncompressed to #{uncompressed_path}')
+      pack = LogStash::PluginManager::PackInstaller::Pack.new(uncompressed_path)
+      raise PluginManager::InvalidPackError, 'The pack must contains at least one plugin' unless pack.valid?
+    
+      # create list of plugins to update
+  def plugins_to_update(previous_gem_specs_map)
+    if update_all?
+      previous_gem_specs_map.values.map{|spec| spec.name}
+    else
+      # If the plugins isn't available in the gemspec or in 
+      # the gemfile defined with a local path, we assume the plugins is not
+      # installed.
+      not_installed = plugins_arg.select{|plugin| !previous_gem_specs_map.has_key?(plugin.downcase) && !gemfile.find(plugin) }
+      signal_error('Plugin #{not_installed.join(', ')} is not installed so it cannot be updated, aborting') unless not_installed.empty?
+      plugins_arg
+    end
+  end
+    
+      it 'returns the pipeline id' do
+    expect(subject.pipeline_id).to eq(pipeline_id)
+  end
+    
+        desc 'Halt all VM's involved in the acceptance test round'
+    task :halt, :platform do |t, args|
+      config   = PlatformConfig.new
+      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
+      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
+    
+              it 'fails when installing a non logstash plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
+            expect(command).not_to install_successfully
           end
     
-          opts.on('--stop-on-error', 'If a file fails to compile, exit immediately.',
-                                 'Only meaningful for --watch and --update.') do
-        @options[:stop_on_error] = true
+          it 'display a list of installed plugins' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
+        expect(result.stdout.split('\n').size).to be > 1
+      end
+    
+        context 'update a specific plugin' do
+      it 'has executed successfully' do
+        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
+        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
+        expect(logstash).not_to have_installed?(plugin_name, previous_version)
       end
     end
     
-    When /^(?:|I )choose '([^']*)'$/ do |field|
-  choose(field)
-end
+    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
+
     
-    ENV['RAILS_ENV'] = 'test'
-    
-    require 'mimemagic'
-require 'mimemagic/overlay'
-require 'logger'
-require 'terrapin'
-    
-        # resize to a new geometry
-    # @param geometry [String] the Paperclip geometry definition to resize to
-    # @example
-    #   Paperclip::Geometry.new(150, 150).resize_to('50x50!')
-    #   #=> Paperclip::Geometry(50, 50)
-    def resize_to(geometry)
-      new_geometry = Paperclip::Geometry.parse geometry
-      case new_geometry.modifier
-      when '!', '#'
-        new_geometry
-      when '>'
-        if new_geometry.width >= self.width && new_geometry.height >= self.height
-          self
-        else
-          scale_to new_geometry
-        end
-      when '<'
-        if new_geometry.width <= self.width || new_geometry.height <= self.height
-          self
-        else
-          scale_to new_geometry
-        end
-      else
-        scale_to new_geometry
-      end
+          cache(gist, file, data.body) unless @cache_disabled
+      data.body
     end
     
-        module ClassMethods
-      def attachment_definitions
-        Paperclip::AttachmentRegistry.definitions_for(self)
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
+
+    
+          Dir.chdir(file_path) do
+        contents = file.read
+        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
+          contents = $1.lstrip
+        end
+        contents = pre_filter(contents)
+        if @raw
+          contents
+        else
+          partial = Liquid::Template.parse(contents)
+          context.stack do
+            partial.render(context)
+          end
+        end
       end
     end
   end
 end
+    
+    Liquid::Template.register_tag('video', Jekyll::VideoTag)
+    
+        def clear
+      @attachments = Hash.new { |h,k| h[k] = {} }
+    end
+    
+        # Swaps the height and width if necessary
+    def auto_orient
+      if EXIF_ROTATED_ORIENTATION_VALUES.include?(@orientation)
+        @height, @width = @width, @height
+        @orientation -= 4
+      end
+    end
