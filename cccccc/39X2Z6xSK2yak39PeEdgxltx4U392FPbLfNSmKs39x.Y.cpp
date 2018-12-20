21fb6c5c96bf189cee00cc79fda9cd992942a261
SUNsung <weatherius@gmail.com>
2018-12-20 17:34:05 +0200

    Rewrite [39X2Z6xSK2yak39PeEdgxltx4U392FPbLfNSmKs39x.Y.cpp] File in: 2018-12-20 17:34:04

--- cccccc/39X2Z6xSK2yak39PeEdgxltx4U392FPbLfNSmKs39x.Y.cpp
+++ cccccc/39X2Z6xSK2yak39PeEdgxltx4U392FPbLfNSmKs39x.Y.cpp
@@ -1,158 +1,27 @@
 
         
-        #if PY_MAJOR_VERSION >= 3
-static struct PyModuleDef _module = {
-  PyModuleDef_HEAD_INIT,
-  kModuleName,
-  kModuleDocstring,
-  -1,
-  NULL,
-  NULL,
-  NULL,
-  NULL,
-  NULL
-};
-#define INITFUNC PyInit__api_implementation
-#define INITFUNC_ERRORVAL NULL
-#else
-#define INITFUNC init_api_implementation
-#define INITFUNC_ERRORVAL
-#endif
+        #include 'collision_object_bullet.h'
+#include 'space_bullet.h'
     
-    /* Prior to C++11, the 'template' disambiguator may only appear inside a
- * template, so the regular macro must not use 'template' */
-    
-    #include <google/protobuf/compiler/csharp/csharp_source_generator_base.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_names.h>
-#include <google/protobuf/compiler/csharp/csharp_options.h>
-    
-    
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace java
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-      ObjectiveCType objc_type = GetObjectiveCType(descriptor_);
-  string singular_type;
-  if (objc_type == OBJECTIVECTYPE_MESSAGE) {
-    vars['type'] = string('GPBStringifySymbol(') +
-                   ClassName(descriptor_->message_type()) + ')';
-  } else {
-    vars['type'] = 'NULL';
-  }
-    
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+    class PinJointBullet : public JointBullet {
+	class btPoint2PointConstraint *p2pConstraint;
     }
     
-    #endif  // CAFFE_BIAS_LAYER_HPP_
-
+    class BulletPhysicsServer;
     
-    #endif  // CAFFE_CONCAT_LAYER_HPP_
-
-    
-    
-    { protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual inline bool reverse_dimensions() { return false; }
-  virtual void compute_output_shape();
-};
-    
-      vector<cudnnTensorDescriptor_t> bottom_descs_, top_descs_;
-  cudnnTensorDescriptor_t    bias_desc_;
-  cudnnFilterDescriptor_t      filter_desc_;
-  vector<cudnnConvolutionDescriptor_t> conv_descs_;
-  int bottom_offset_, top_offset_, bias_offset_;
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-      // Sets the coordinates of the current macro-block for the purpose of
-  // CompareBlock() calls.
-  virtual void SwitchBlock(int block_x, int block_y,
-                           int factor_x, int factor_y) = 0;
-    
-      std::istringstream memstream(data, std::ios::in | std::ios::binary);
-  png_set_read_fn(png_ptr, static_cast<void*>(&memstream), [](png_structp png_ptr, png_bytep outBytes, png_size_t byteCountToRead) {
-    std::istringstream& memstream = *static_cast<std::istringstream*>(png_get_io_ptr(png_ptr));
-    
-    memstream.read(reinterpret_cast<char*>(outBytes), byteCountToRead);
+    bool ImGui::InputText(const char* label, std::string* str, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* user_data)
+{
+    IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
+    flags |= ImGuiInputTextFlags_CallbackResize;
     }
     
-    // kIDCTMatrix[8*x+u] = alpha(u)*cos((2*x+1)*u*M_PI/16)*sqrt(2), with fixed 13
-// bit precision, where alpha(0) = 1/sqrt(2) and alpha(u) = 1 for u > 0.
-// Some coefficients are off by +-1 to mimick libjpeg's behaviour.
-static const int kIDCTMatrix[kDCTBlockSize] = {
-  8192,  11363,  10703,   9633,   8192,   6437,   4433,   2260,
-  8192,   9633,   4433,  -2259,  -8192, -11362, -10704,  -6436,
-  8192,   6437,  -4433, -11362,  -8192,   2261,  10704,   9633,
-  8192,   2260, -10703,  -6436,   8192,   9633,  -4433, -11363,
-  8192,  -2260, -10703,   6436,   8192,  -9633,  -4433,  11363,
-  8192,  -6437,  -4433,  11362,  -8192,  -2261,  10704,  -9633,
-  8192,  -9633,   4433,   2259,  -8192,  11362, -10704,   6436,
-  8192, -11363,  10703,  -9633,   8192,  -6437,   4433,  -2260,
+    struct VERTEX_CONSTANT_BUFFER
+{
+    float   mvp[4][4];
 };
     
-    #include 'guetzli/jpeg_data.h'
     
-      // Compute YUV444 DCT coefficients.
-  int block_ix = 0;
-  for (int block_y = 0; block_y < jpg->MCU_rows; ++block_y) {
-    for (int block_x = 0; block_x < jpg->MCU_cols; ++block_x) {
-      coeff_t block[3 * kDCTBlockSize];
-      // RGB->YUV transform.
-      for (int iy = 0; iy < 8; ++iy) {
-        for (int ix = 0; ix < 8; ++ix) {
-          int y = std::min(h - 1, 8 * block_y + iy);
-          int x = std::min(w - 1, 8 * block_x + ix);
-          int p = y * w + x;
-          RGBToYUV16(&rgb[3 * p], &block[8 * iy + ix]);
-        }
-      }
-      // DCT
-      for (int i = 0; i < 3; ++i) {
-        ComputeBlockDCT(&block[i * kDCTBlockSize]);
-      }
-      // Quantization
-      for (int i = 0; i < 3 * 64; ++i) {
-        Quantize(&block[i], iquant[i]);
-      }
-      // Copy the resulting coefficients to *jpg.
-      for (int i = 0; i < 3; ++i) {
-        memcpy(&jpg->components[i].coeffs[block_ix * kDCTBlockSize],
-               &block[i * kDCTBlockSize], kDCTBlockSize * sizeof(block[0]));
-      }
-      ++block_ix;
-    }
-  }
-    
-    #include 'guetzli/jpeg_data.h'
+    {    // Store our identifier
+    static_assert(sizeof(ImTextureID) >= sizeof(g_hFontSrvGpuDescHandle.ptr), 'Can't pack descriptor handle into TexID, 32-bit not supported yet.');
+    io.Fonts->TexID = (ImTextureID)g_hFontSrvGpuDescHandle.ptr;
+}
