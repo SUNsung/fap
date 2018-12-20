95abdc413982b7880720aacc699217daf059e41b
SUNsung <weatherius@gmail.com>
2018-12-20 20:26:02 +0200

    Rewrite [02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb] File in: 2018-12-20 20:26:01

--- ruby/02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb
+++ ruby/02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb
@@ -1,86 +1,131 @@
 
         
-          path = 'assets/stylesheets'
-  css_path = args.with_defaults(css_path: 'tmp')[:css_path]
-  puts Term::ANSIColor.bold 'Compiling SCSS in #{path}'
-  Dir.mkdir(css_path) unless File.directory?(css_path)
-  %w(_bootstrap bootstrap/_theme).each do |file|
-    save_path = '#{css_path}/#{file.sub(/(^|\/)?_+/, '\1').sub('/', '-')}.css'
-    puts Term::ANSIColor.cyan('  #{save_path}') + '...'
-    engine    = Sass::Engine.for_file('#{path}/#{file}.scss', syntax: :scss, load_paths: [path])
-    css       = engine.render
-    File.open(save_path, 'w') { |f| f.write css }
+            it 'shows the dry run pop up without previous events and selects the events tab when a event was created' do
+      open_dry_run_modal(agent)
+      click_on('Dry Run')
+      expect(page).to have_text('Biologists play reverse')
+      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
+    end
+    
+              @bar2 = Agents::DotBar.new(name: 'bar2').tap { |agent|
+            agent.user = users(:bob)
+            agent.sources << @foo
+            agent.propagate_immediately = true
+            agent.disabled = true
+            agent.save!
+          },
+    
+          it 'runs until stop is called' do
+        mock.instance_of(Rufus::Scheduler).join
+        Thread.new { while @agent_runner.instance_variable_get(:@running) != false do sleep 0.1; @agent_runner.stop end }
+        @agent_runner.run
+      end
+    
+      it 'provides hash-style access to its properties with both symbol and string keys' do
+    expect(location[:lat]).to be_a Float
+    expect(location[:lat]).to eq 2.0
+    expect(location['lat']).to be_a Float
+    expect(location['lat']).to eq 2.0
   end
+    
+        it 'cleans up old logs when there are more than log_length' do
+      stub(AgentLog).log_length { 4 }
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 1')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 2')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 3')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 4')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 4')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 1')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 5')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 5')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 2')
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 6')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 6')
+      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 3')
+    end
+    
+          # Returns the path to a file for the given key.
+      #
+      # @param key [String]
+      # @return [String] The path to the cache file.
+      def path_to(key)
+        key = key.gsub(/[<>:\\|?*%]/) {|c| '%%%03d' % c.ord}
+        File.join(cache_location, key)
+      end
+    end
+  end
+end
+
+    
+          def add_roles(roles)
+        Array(roles).each { |role| add_role(role) }
+        self
+      end
+      alias roles= add_roles
+    
+          def role_properties_for(rolenames)
+        roles = rolenames.to_set
+        rps = Set.new unless block_given?
+        roles_for(rolenames).each do |host|
+          host.roles.intersection(roles).each do |role|
+            [host.properties.fetch(role)].flatten(1).each do |props|
+              if block_given?
+                yield host, role, props
+              else
+                rps << (props || {}).merge(role: role, hostname: host.hostname)
+              end
+            end
+          end
+        end
+        block_given? ? nil : rps
+      end
+    
+        def URIEncodePair(cc1, cc2, result, index)
+      u = ((cc1 >> 6) & 0xF) + 1;
+      w = (cc1 >> 2) & 0xF;
+      x = cc1 & 3;
+      y = (cc2 >> 6) & 0xF;
+      z = cc2 & 63;
+      octets = Array.new(4);
+      octets[0] = (u >> 2) + 240;
+      octets[1] = (((u & 3) << 4) | w) + 128;
+      octets[2] = ((x << 4) | y) + 128;
+      octets[3] = z + 128;
+      return URIEncodeOctets(octets, result, index);
+    end
+    
+          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
+      def left_shift int, shift
+        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
+        # 1>>31, 2**32
+        (r & 2147483648) == 0 ? r : r - 4294967296
+      end
+    
+          def page_header
+        title
+      end
+    
+    def cloned_testpath(path)
+  repo   = File.expand_path(testpath(path))
+  path   = File.dirname(repo)
+  cloned = File.join(path, self.class.name)
+  FileUtils.rm_rf(cloned)
+  Dir.chdir(path) do
+    %x{git clone #{File.basename(repo)} #{self.class.name} 2>/dev/null}
+  end
+  cloned
 end
     
-      def process_bootstrap
-    log_status 'Convert Bootstrap LESS to Sass'
-    puts ' repo   : #@repo_url'
-    puts ' branch : #@branch_sha #@repo_url/tree/#@branch'
-    puts ' save to: #{@save_to.to_json}'
-    puts ' twbs cache: #{@cache_path}'
-    puts '-' * 60
+        assert_no_match /Edit/, last_response.body, ''Edit' link not blocked in history template'
     
-        def pos=(i)
-      @s.pos = str_to_byte_pos i
-      i
-    end
+    desc 'Update version number and gemspec'
+task :bump do
+  puts 'Updated version to #{bump_version}'
+  # Execute does not invoke dependencies.
+  # Manually invoke gemspec then validate.
+  Rake::Task[:gemspec].execute
+  Rake::Task[:validate].execute
+end
     
-      # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-    
-          it 'detects closing brace on separate line from last element' do
-        inspect_source(source)
-    
-          # Checks whether this `hash` element is on the same line as `other`.
-      #
-      # @note A multiline element is considered to be on the same line if it
-      #       shares any of its lines with `other`
-      #
-      # @return [Boolean] whether this element is on the same line as `other`
-      def same_line?(other)
-        loc.last_line == other.loc.line || loc.line == other.loc.last_line
-      end
-    
-            ActiveSupport.on_load(:active_record) do
-          include Sidekiq::Extensions::ActiveRecord
-        end
-        ActiveSupport.on_load(:action_mailer) do
-          extend Sidekiq::Extensions::ActionMailer
-        end
-      end
-    
-        def self.job_hash_context(job_hash)
-      # If we're using a wrapper class, like ActiveJob, use the 'wrapped'
-      # attribute to expose the underlying thing.
-      klass = job_hash['wrapped'] || job_hash['class']
-      bid = job_hash['bid']
-      '#{klass} JID-#{job_hash['jid']}#{' BID-#{bid}' if bid}'
-    end
-    
-      module TestingClient
-    def raw_push(payloads)
-      if Sidekiq::Testing.fake?
-        payloads.each do |job|
-          job = Sidekiq.load_json(Sidekiq.dump_json(job))
-          job.merge!('enqueued_at' => Time.now.to_f) unless job['at']
-          Queues.push(job['queue'], job['class'], job)
-        end
-        true
-      elsif Sidekiq::Testing.inline?
-        payloads.each do |job|
-          klass = Sidekiq::Testing.constantize(job['class'])
-          job['id'] ||= SecureRandom.hex(12)
-          job_hash = Sidekiq.load_json(Sidekiq.dump_json(job))
-          klass.process_job(job_hash)
-        end
-        true
-      else
-        super
-      end
-    end
-  end
-    
-        def use(*middleware_args, &block)
-      middlewares << [middleware_args, block]
-    end
+      s.summary     = 'A simple, Git-powered wiki.'
+  s.description = 'A simple, Git-powered wiki with a sweet API and local frontend.'
