24278560c1cedd831fe1b6b3d74529b968247328
SUNsung <weatherius@gmail.com>
2018-12-21 06:44:28 +0200

    Rewrite [59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py] File in: 2018-12-21 06:44:27

--- pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
+++ pyton/59agnDlRlXKk59vHRM7y.wVow599XI.UfLEqWM59l3C.py
@@ -1,144 +1,136 @@
 
         
-            def get(self, key, default=None):
-        return self.__dict__.get(key, default)
-
+                # Insert the import statement to setup.py if not present
+        with open(setup_path, 'a+') as setup:
+            setup.seek(0)
+            setup_content = setup.read()
+            if not 'import fastentrypoints' in setup_content:
+                setup.seek(0)
+                setup.truncate()
+                setup.write('import fastentrypoints\n' + setup_content)
     
     
-    builtin_str = str
-    bytes = str
-    str = unicode
-    basestring = basestring
-    numeric_types = (int, long, float)
-    integer_types = (int, long)
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
     
+    no_match_output = '''
+Listing... Done
+'''
     
-def test_idna_with_version_attribute(mocker):
-    '''Verify we're actually setting idna version when it should be available.'''
-    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
-    assert info()['idna'] == {'version': '2.6'}
-
+        if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
     
-        @pytest.mark.skip(reason='this fails non-deterministically under pytest-xdist')
-    def test_request_recovery(self):
-        '''can check the requests content'''
-        # TODO: figure out why this sometimes fails when using pytest-xdist.
-        server = Server.basic_response_server(requests_to_handle=2)
-        first_request = b'put your hands up in the air'
-        second_request = b'put your hand down in the floor'
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    from ansible.module_utils.ec2 import get_aws_connection_info, ec2_argument_spec
-from ansible.module_utils.ec2 import boto3_conn
-from ansible.module_utils.aws.cloudfront_facts import CloudFrontFactsServiceManager
-from ansible.module_utils.ec2 import camel_dict_to_snake_dict
-from ansible.module_utils.aws.core import AnsibleAWSModule
-import datetime
-from functools import partial
-import json
-import traceback
-    
-    EXAMPLES = '''
-# Note: These examples do not set authentication details, see the AWS Guide for details.
+        outputs1 = Lambda(lambda x: utils.preprocess_input(x, 'channels_last'),
+                      output_shape=x.shape)(inputs)
+    model1 = Model(inputs, outputs1)
+    out1 = model1.predict(x[np.newaxis])[0]
+    x2 = np.transpose(x, (2, 0, 1))
+    inputs2 = Input(shape=x2.shape)
+    outputs2 = Lambda(lambda x: utils.preprocess_input(x, 'channels_first'),
+                      output_shape=x2.shape)(inputs2)
+    model2 = Model(inputs2, outputs2)
+    out2 = model2.predict(x2[np.newaxis])[0]
+    assert_allclose(out1, out2.transpose(1, 2, 0))
     
     
 if __name__ == '__main__':
-    main()
+    pytest.main([__file__])
 
     
-    # Gather facts about launch configuration with name 'example'
-- ec2_lc_facts:
-    name: example
     
-    - debug: msg='{{ specific_ec2_placement_groups | json_query(\'[?name=='my-cluster']\') }}'
+def create_multi_input_model_from(layer1, layer2):
+    input_1 = Input(shape=(data_dim,))
+    input_2 = Input(shape=(data_dim,))
+    out1 = layer1(input_1)
+    out2 = layer2(input_2)
+    out = Average()([out1, out2])
+    model = Model([input_1, input_2], out)
+    model.add_loss(K.mean(out2))
+    model.add_loss(1)
+    model.add_loss(1)
+    return model
     
-            final_state = camel_dict_to_snake_dict(get_vpc(module, connection, vpc_id))
-        final_state['tags'] = boto3_tag_list_to_ansible_dict(final_state.get('tags', []))
-        final_state['id'] = final_state.pop('vpc_id')
+        E.g. if your `batch_size` is 64 and you use `gpus=2`,
+    then we will divide the input into 2 sub-batches of 32 samples,
+    process each sub-batch on one GPU, then return the full
+    batch of 64 processed samples.
     
-        if state == 'present':
-        for required in ['name', 'description', 'subnets']:
-            if not module.params.get(required):
-                module.fail_json(msg=str('Parameter %s required for state='present'' % required))
-    else:
-        for not_allowed in ['description', 'subnets']:
-            if module.params.get(not_allowed):
-                module.fail_json(msg=str('Parameter %s not allowed for state='absent'' % not_allowed))
+            self.built = True
     
+        # To prevent a slowdown,
+    # we find beforehand the arrays that need conversion.
+    feed = (model._feed_inputs +
+            model._feed_targets +
+            model._feed_sample_weights)
+    indices_for_conversion_to_dense = []
+    for i in range(len(feed)):
+        if issparse(ins[i]) and not K.is_sparse(feed[i]):
+            indices_for_conversion_to_dense.append(i)
     
-try:
-    import boto3
-    import botocore.exceptions
-    HAS_BOTO3 = True
+    from __future__ import print_function
+import numpy as np
+    
+    # We add a vanilla hidden layer:
+model.add(Dense(hidden_dims))
+model.add(Dropout(0.2))
+model.add(Activation('relu'))
+    
+    import pytest
+    
+    try:
+    import simplejson as json
 except ImportError:
-    HAS_BOTO3 = False
+    import json
+    
+        def get_origin_req_host(self):
+        return self.get_host()
+    
+            # Verify Authorization is sent correctly again, and return 200 OK.
+        request_content = consume_socket_content(sock, timeout=0.5)
+        assert expected_digest in request_content
+        sock.send(text_200)
+    
+        def close(self):
+        '''Releases the connection back to the pool. Once this method has been
+        called the underlying ``raw`` object must not be accessed again.
+    
+        # Redirection.
+    300: ('multiple_choices',),
+    301: ('moved_permanently', 'moved', '\\o-'),
+    302: ('found',),
+    303: ('see_other', 'other'),
+    304: ('not_modified',),
+    305: ('use_proxy',),
+    306: ('switch_proxy',),
+    307: ('temporary_redirect', 'temporary_moved', 'temporary'),
+    308: ('permanent_redirect',
+          'resume_incomplete', 'resume',),  # These 2 to be removed in 3.0
     
     
-def main():
-    '''
-    Main entry point.
+def get_cmd_out(cmd):
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out = proc.stdout
+    lines = out.readlines()
+    return lines
     
-        CNTV().download_by_vid(rid, **kwargs)
+            An index from 0..n-1 of the token object in the input stream.
+        This must be valid in order to use the ANTLRWorks debugger.
+        
+        Using setter/getter methods is deprecated. Use o.index instead.'''
     
-        mime, ext, size = url_info(real_url)
+    def get_marker(line):
+    matchlist = TAG_REGEX.findall(line)
+    if matchlist:
+        namematch = NAMED_A_TAG_REGEX.match(line)
+        if namematch:
+            return namematch.group(1) # group 0 is full match
     
-    site_info = 'dilidili'
-download = dilidili_download
-download_playlist = playlist_not_supported('dilidili')
-
+        def __init__(self):
+        self.__prepare__()
     
-    site_info = 'ehow.com'
-download = ehow_download
-download_playlist = playlist_not_supported('ehow')
-
-    
-        sd_urls = list(set([
-        unicodize(str.replace(i, '\\/', '/'))
-        for i in re.findall(r'sd_src_no_ratelimit:'([^']*)'', html)
-    ]))
-    hd_urls = list(set([
-        unicodize(str.replace(i, '\\/', '/'))
-        for i in re.findall(r'hd_src_no_ratelimit:'([^']*)'', html)
-    ]))
-    urls = hd_urls if hd_urls else sd_urls
-    
-    
-setup_locate_faces = '''
-import face_recognition
-    
-        # Apply some eyeliner
-    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    
-    # Find all the faces in the image using a pre-trained convolutional neural network.
-# This method is more accurate than the default HOG model, but it's slower
-# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
-# this will use GPU acceleration and perform well.
-# See also: find_faces_in_picture.py
-face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
-    
-        # For a definition of each point index, see https://cdn-images-1.medium.com/max/1600/1*AbEg31EgkbXSQehuNJBlWg.png
-    if model == 'large':
-        return [{
-            'chin': points[0:17],
-            'left_eyebrow': points[17:22],
-            'right_eyebrow': points[22:27],
-            'nose_bridge': points[27:31],
-            'nose_tip': points[31:36],
-            'left_eye': points[36:42],
-            'right_eye': points[42:48],
-            'top_lip': points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
-            'bottom_lip': points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
-        } for points in landmarks_as_tuples]
-    elif model == 'small':
-        return [{
-            'nose_tip': [points[4]],
-            'left_eye': points[2:4],
-            'right_eye': points[0:2],
-        } for points in landmarks_as_tuples]
-    else:
-        raise ValueError('Invalid landmarks model type. Supported models are ['small', 'large'].')
+    print('writing anagrams to file...')
+with open('anagrams.txt', 'w') as file:
+    file.write('all_anagrams = ')
+    file.write(pprint.pformat(all_anagrams))
