d1034c9447a2187f92b3ff8c09e94009023b38c1
SUNsung <weatherius@gmail.com>
2018-12-22 20:22:57 +0200

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2018-12-22 20:22:56

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,85 +1,65 @@
 
         
-        platforms :ruby do
-  gem 'sqlite3'
-end
+              expect(page).to have_text('Test Scheduler Agent')
     
-      def serialize_options(resource)
-    methods = resource_class.authentication_keys.dup
-    methods = methods.keys if methods.is_a?(Hash)
-    methods << :password if resource.respond_to?(:password)
-    { methods: methods, only: [:password] }
-  end
-    
-        # The path used after unlocking the resource
-    def after_unlock_path_for(resource)
-      new_session_path(resource)  if is_navigational_format?
+        it 'shows the dry run pop up without previous events and selects the events tab when a event was created' do
+      open_dry_run_modal(agent)
+      click_on('Dry Run')
+      expect(page).to have_text('Biologists play reverse')
+      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
     end
     
-    class DeviseCreateUsers < ActiveRecord::Migration
-  def change
-    create_table(:users) do |t|
-      t.string :email,              null: false
-      t.string :encrypted_password, null: true
-      t.timestamps null: false
+      describe '#status' do
+    it 'works for failed jobs' do
+      job.failed_at = Time.now
+      expect(status(job)).to eq('<span class='label label-danger'>failed</span>')
     end
     
-          if options.has_key?(:only)
-        @used_routes = self.routes & Array(options[:only]).map(&singularizer)
-      elsif options[:skip] == :all
-        @used_routes = []
-      else
-        @used_routes = self.routes - Array(options[:skip]).map(&singularizer)
-      end
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
     end
     
-          ident_range = Sass::Source::Range.new(
-        Sass::Source::Position.new(@line, to_parser_offset(line.offset)),
-        Sass::Source::Position.new(@line, parser.offset),
-        @options[:filename], @options[:importer])
-      offset = parser.offset - 1
-      res.unshift(hack_char) if hack_char
-    
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
+        def backend
+      @backend ||= SSHKit
     end
-  end
     
-        <div id='rack'>
-      <h3 id='env-info'>Rack ENV</h3>
-      <table class='req'>
-        <tr>
-          <th>Variable</th>
-          <th>Value</th>
-        </tr>
-         <% env.sort_by { |k, v| k.to_s }.each { |key, val| %>
-         <tr>
-           <td><%=h key %></td>
-           <td class='code'><div><%=h val %></div></td>
-         </tr>
-         <% } %>
-      </table>
-      <div class='clear'></div>
-    </div> <!-- /RACK ENV -->
-    
-            if unmasked_token?(token)
-          compare_with_real_token token, session
-    
-            if @javascript and not @escaper.respond_to? :escape_javascript
-          fail('Use EscapeUtils for JavaScript escaping.')
-        end
+          def allow_uploads
+        @allow_uploads
       end
     
-      describe '#referrer' do
-    it 'Reads referrer from Referer header' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
-      expect(subject.referrer(env)).to eq('bar.com')
+          def use_identicon
+        @wiki.user_icons == 'identicon'
+      end
+    
+        assert_no_match /New/, last_response.body, ''New' link not blocked in pages template'
+    
+    context 'Precious::Helpers' do
+  include Precious::Helpers
+    
+    context 'Precious::Views::LatestChanges' do
+  include Rack::Test::Methods
+  
+  def app
+    Precious::App
+  end
+  
+  setup do
+    @path = cloned_testpath('examples/lotr.git')
+    @wiki = Gollum::Wiki.new(@path)
+    Precious::App.set(:gollum_path, @path)
+    Precious::App.set(:wiki_options, {:latest_changes_count => 10})
+  end
+    
+      test 'h1 title can be disabled' do
+    title = 'H1'
+    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
+    page = @wiki.page(title)
+    
+        def forbid(msg = 'Forbidden. This wiki is set to no-edit mode.')
+      @message = msg
+      status 403
+      halt mustache :error
     end
