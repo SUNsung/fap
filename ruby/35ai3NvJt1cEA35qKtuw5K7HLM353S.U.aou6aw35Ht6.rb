94ffb76c705dd52b2544efa5942aec6aba944abb
SUNsung <weatherius@gmail.com>
2018-12-23 12:55:25 +0200

    Rewrite [35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb] File in: 2018-12-23 12:55:24

--- ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
+++ ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
@@ -1,92 +1,52 @@
 
         
-            context 'string formats' do
-      it 'determines the maximum number of characters to be copied from the string' do
-        format('%.1p', [1]).should == '['
-        format('%.2p', [1]).should == '[1'
-        format('%.10p', [1]).should == '[1]'
-        format('%.0p', [1]).should == ''
+        Backtrace:\n#{e.backtrace.join('\n').gsub('*/', '*\\/')}
+*/
+body:before {
+  white-space: pre;
+  font-family: monospace;
+  content: '#{header.gsub(''', '\'').gsub('\n', '\\A ')}'; }
+END
+      end
     
-      it 'raises a TypeError when passed a String' do
-    lambda { sleep('2')   }.should raise_error(TypeError)
-  end
+        <% unless bad_request?(exception) %>
+      <div id='get'>
+        <h3 id='get-info'>GET</h3>
+        <% if req.GET and not req.GET.empty? %>
+          <table class='req'>
+            <tr>
+              <th>Variable</th>
+              <th>Value</th>
+            </tr>
+             <% req.GET.sort_by { |k, v| k.to_s }.each { |key, val| %>
+            <tr>
+              <td><%=h key %></td>
+              <td class='code'><div><%=h val.inspect %></div></td>
+            </tr>
+            <% } %>
+          </table>
+        <% else %>
+          <p class='no-data'>No GET data.</p>
+        <% end %>
+        <div class='clear'></div>
+      </div> <!-- /GET -->
     
-      it 'returns true when passed ?d if the argument is a directory' do
-    Kernel.test(?d, @dir).should == true
-  end
+          def redirect(env)
+        request = Request.new(env)
+        warn env, 'attack prevented by #{self.class}'
+        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
+      end
     
-    
-  def dprint(msg)
-    self.client.dprint(msg)
-  end
-    
-    =begin
-   +-------------+---------------+-------------------------------------+
-   | VALUE       | Name          | Description                         |
-   +-------------+---------------+-------------------------------------+
-   | 0x01        | Hangup        | The call has been hungup at the     |
-   |             |               | remote end                          |
-   |             |               |                                     |
-   | 0x02        | Reserved      | Reserved for future use             |
-   |             |               |                                     |
-   | 0x03        | Ringing       | Remote end is ringing (ring-back)   |
-   |             |               |                                     |
-   | 0x04        | Answer        | Remote end has answered             |
-   |             |               |                                     |
-   | 0x05        | Busy          | Remote end is busy                  |
-   |             |               |                                     |
-   | 0x06        | Reserved      | Reserved for future use             |
-   |             |               |                                     |
-   | 0x07        | Reserved      | Reserved for future use             |
-   |             |               |                                     |
-   | 0x08        | Congestion    | The call is congested               |
-   |             |               |                                     |
-   | 0x09        | Flash Hook    | Flash hook                          |
-   |             |               |                                     |
-   | 0x0a        | Reserved      | Reserved for future use             |
-   |             |               |                                     |
-   | 0x0b        | Option        | Device-specific options are being   |
-   |             |               | transmitted                         |
-   |             |               |                                     |
-   | 0x0c        | Key Radio     | Key Radio                           |
-   |             |               |                                     |
-   | 0x0d        | Unkey Radio   | Unkey Radio                         |
-   |             |               |                                     |
-   | 0x0e        | Call Progress | Call is in progress                 |
-   |             |               |                                     |
-   | 0x0f        | Call          | Call is proceeding                  |
-   |             | Proceeding    |                                     |
-   |             |               |                                     |
-   | 0x10        | Hold          | Call is placed on hold              |
-   |             |               |                                     |
-   | 0x11        | Unhold        | Call is taken off hold              |
-   +-------------+---------------+-------------------------------------+
-=end
-    
-              res
+          def escape(object)
+        case object
+        when Hash   then escape_hash(object)
+        when Array  then object.map { |o| escape(o) }
+        when String then escape_string(object)
+        when Tempfile then object
+        else nil
         end
+      end
     
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
-    
-                int
-          end
-    
-              # Decodes the realm field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_realm(input)
-            input.value[0].value
-          end
-    
-      # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+        post('/', {'csrf_param' => token}, 'rack.session' => {:csrf => token})
+    expect(last_response).to be_ok
+  end
