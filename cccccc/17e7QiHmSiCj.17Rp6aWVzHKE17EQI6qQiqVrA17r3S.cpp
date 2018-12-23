0268b61bd7ad9fb1ccf817edde1b73591795337f
SUNsung <weatherius@gmail.com>
2018-12-23 08:16:56 +0200

    Rewrite [17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp] File in: 2018-12-23 08:16:56

--- cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
+++ cccccc/17e7QiHmSiCj.17Rp6aWVzHKE17EQI6qQiqVrA17r3S.cpp
@@ -1,79 +1,154 @@
 
         
-        
-    {    elem_of_each_node_[left_node_id] = Elem(begin, split_pt, left_node_id);
-    elem_of_each_node_[right_node_id] = Elem(split_pt, e.end, right_node_id);
-    elem_of_each_node_[node_id] = Elem(nullptr, nullptr, -1);
+        #endif  // ATOM_APP_COMMAND_LINE_ARGS_H_
+
+    
+    // static
+v8::Local<v8::Value> Net::Create(v8::Isolate* isolate) {
+  return mate::CreateHandle(isolate, new Net(isolate)).ToV8();
+}
+    
+    #ifndef ATOM_BROWSER_API_ATOM_API_POWER_MONITOR_H_
+#define ATOM_BROWSER_API_ATOM_API_POWER_MONITOR_H_
+    
+    namespace mate {
+class Arguments;
+class Dictionary;
+}  // namespace mate
+    
+    // static
+void Event::BuildPrototype(v8::Isolate* isolate,
+                           v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'Event'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod('preventDefault', &Event::PreventDefault)
+      .SetMethod('sendReply', &Event::SendReply);
+}
+    
+      // Returns all objects in this class's weak map.
+  static std::vector<v8::Local<v8::Object>> GetAll(v8::Isolate* isolate) {
+    if (weak_map_)
+      return weak_map_->Values(isolate);
+    else
+      return std::vector<v8::Local<v8::Object>>();
   }
     
-    /**
- * @brief Hash is a general utility class for hashing content
- *
- * @code{.cpp}
- *   Hash my_hash(HASH_TYPE_SHA256);
- *   my_hash.update(my_buffer, my_buffer_size);
- *   std::cout << my_hash.digest();
- * @endcode
- *
- */
-class Hash : private boost::noncopyable {
- public:
-  /**
-   * @brief Hash constructor
-   *
-   * The hash class should be initialized with one of osquery::HashType as a
-   * constructor argument.
-   *
-   * @param algorithm The hashing algorithm which will be used to compute the
-   * hash
-   */
-  explicit Hash(HashType algorithm);
+      if (!BrowserThread::PostTask(BrowserThread::UI, FROM_HERE, task)) {
+    // Without a UI thread to post the exit task to, there aren't many
+    // options.  Raise the signal again.  The default handler will pick it up
+    // and cause an ungraceful exit.
+    RAW_LOG(WARNING, 'No UI thread, exiting ungracefully.');
+    kill(getpid(), signal);
     }
     
-    bool PlatformProcess::operator==(const PlatformProcess& process) const {
-  return (nativeHandle() == process.nativeHandle());
-}
+     private:
+  void OnMessageBoxCallback(DialogClosedCallback callback,
+                            const std::string& origin,
+                            int code,
+                            bool checkbox_checked);
     
-    #ifdef WIN32
-  pid = (int)::GetCurrentProcessId();
-#else
-  pid = getpid();
-#endif
+    #include 'base/macros.h'
     
-    #include <gtest/gtest.h>
+    #include <cstdint>      // for int32_t
+    
+    #endif  // TESSERACT_CCMAIN_PAGEITERATOR_H_
+
+    
+    namespace tesseract {
+    }
     
     
-    {  EXPECT_EQ(constraint.op, EQUALS);
-  EXPECT_EQ(constraint.expr, 'none');
-}
-    
-    #include <Objbase.h>
-#include <Windows.h>
-    
-      // exclude paths should be applied at last
-  auto path = ec->path.substr(0, ec->path.rfind('/'));
-  // Need to have two finds,
-  // what if somebody excluded an individual file inside a directory
-  if (!exclude_paths_.empty() &&
-      (exclude_paths_.find(path) || exclude_paths_.find(ec->path))) {
-    return false;
+    {  // Do sanity checks and minor fixes on best_choice.
+  if (word->best_choice->length() > word_length) {
+    word->best_choice->make_bad();  // should never happen
+    tprintf('recog_word: Discarded long string \'%s\''
+            ' (%d characters vs %d blobs)\n',
+            word->best_choice->unichar_string().string(),
+            word->best_choice->length(), word_length);
+    tprintf('Word is at:');
+    word->word->bounding_box().print();
   }
+  if (word->best_choice->length() < word_length) {
+    UNICHAR_ID space_id = unicharset.unichar_to_id(' ');
+    while (word->best_choice->length() < word_length) {
+      word->best_choice->append_unichar_id(space_id, 1, 0.0,
+                                           word->best_choice->certainty());
+    }
+  }
+}
     
-    #include <vector>
+      // Changes the box at the given index to the new box.
+  // Recomputes the bounding box.
+  void ChangeBox(int index, const TBOX& box);
+    
+    #endif /* !UCONFIG_NO_FORMATTING */
+
+    
+    
+    {    BreakIterator *get() const { return ptr; }
+    BreakIterator *operator->() const { return ptr; }
+    BreakIterator &operator*() const { return *ptr; }
+private:
+    BreakIterator *ptr;
+    SharedBreakIterator(const SharedBreakIterator &);
+    SharedBreakIterator &operator=(const SharedBreakIterator &);
+};
+    
+        /**
+     * Get maximum significant digits. INT32_MAX means no maximum.
+     */
+    int32_t getMax() const {
+        return fMax;
+    }
+    
+        if (hasCursor) {
+        // Adjust the cursor for positions outside the key.  These
+        // refer to code points rather than code units.  If cursorPos
+        // is within the output string, then use newStart, which has
+        // already been set above.
+        if (cursorPos < 0) {
+            newStart = start;
+            int32_t n = cursorPos;
+            // Outside the output string, cursorPos counts code points
+            while (n < 0 && newStart > 0) {
+                newStart -= U16_LENGTH(text.char32At(newStart-1));
+                ++n;
+            }
+            newStart += n;
+        } else if (cursorPos > output.length()) {
+            newStart = start + outLen;
+            int32_t n = cursorPos - output.length();
+            // Outside the output string, cursorPos counts code points
+            while (n > 0 && newStart < text.length()) {
+                newStart += U16_LENGTH(text.char32At(newStart));
+                --n;
+            }
+            newStart += n;
+        } else {
+            // Cursor is within output string.  It has been set up above
+            // to be relative to start.
+            newStart += start;
+        }
+    }
+    
+    #include 'benchmark/benchmark.h'
+    
+      // Gets the current text color.
+  CONSOLE_SCREEN_BUFFER_INFO buffer_info;
+  GetConsoleScreenBufferInfo(stdout_handle, &buffer_info);
+  const WORD old_color_attrs = buffer_info.wAttributes;
+    
+    #include <cstdarg>
+#include <iostream>
 #include <string>
-#include <memory>
     
-    #include 'DHTNode.h'
-#include 'DlAbortEx.h'
-#include 'DHTConstants.h'
-#include 'bittorrent_helper.h'
-#include 'Logger.h'
-#include 'a2netcompat.h'
-#include 'util.h'
-#include 'TimeA2.h'
-#include 'fmt.h'
-#include 'File.h'
-#include 'LogFactory.h'
-#include 'BufferedFile.h'
+    std::string FormatKV(std::string const& key, int64_t value) {
+  std::stringstream ss;
+  ss << ''' << key << '\': ' << value;
+  return ss.str();
+}
     
-    DHTTokenUpdateCommand::~DHTTokenUpdateCommand() = default;
+    
+    {  init_ = true;
+  return true;
+}
