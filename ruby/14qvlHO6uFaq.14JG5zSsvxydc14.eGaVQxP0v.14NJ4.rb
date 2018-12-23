0adc2d6873759ff2f06987c27d8cb36a682a8b40
SUNsung <weatherius@gmail.com>
2018-12-23 18:24:24 +0200

    Rewrite [14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb] File in: 2018-12-23 18:24:24

--- ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
+++ ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
@@ -1,90 +1,145 @@
 
         
-            Category.transaction do
-      lounge.group_names = ['trust_level_3']
-      unless lounge.save
-        puts lounge.errors.full_messages
-        raise 'Failed to set permissions on trust level 3 lounge category!'
-      end
-    
-          def self.details
-        list = <<-LIST.markdown_list
-          `grouping` is just to keep your tags organised under one 'folder', defaults to 'builds'
-          `lane` is the name of the current fastlane lane
-          `prefix` is anything you want to stick in front of the version number, e.g. 'v'
-          `postfix` is anything you want to stick at the end of the version number, e.g. '-RC1'
-          `build_number` is the build number, which defaults to the value emitted by the `increment_build_number` action
-        LIST
-    
-          it 'works with select regex' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              select_regex: /AppDelegate/
-            )
-          end').runner.execute(:test)
-    
-              expect(result).to eq('swiftlint lint')
-        end
-      end
-    
-        # wrap in double quotes if contains space
-    if str =~ /\s/
-      # double quotes have to be doubled if will be quoted
-      str.gsub!(''', '''')
-      return ''' + str + '''
-    else
-      return str
-    end
-  end
-  module_function :shellescape
+        # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
 end
 
     
-    # To avoid 'PR & Runs' for which tests don't pass, we want to make spec errors more visible
-# The code below will run on Circle, parses the results in JSON and posts them to the PR as comment
-containing_dir = ENV['CIRCLE_TEST_REPORTS'] || '.' # for local testing
-file_path = File.join(containing_dir, 'rspec', 'fastlane-junit-results.xml')
+    Given(%r!^I have the following documents? under the '(.*)' collection within the '(.*)' directory:$!) do |label, dir, table|
+  table.hashes.each do |input_hash|
+    title = slug(input_hash['title'])
+    path = File.join(dir, '_#{label}', '#{title}.md')
+    File.write(path, file_content_from_hash(input_hash))
+  end
+end
     
-        # This will detect the proper guest OS for the machine and set up
-    # the class to actually execute capabilities.
-    def detect!
-      guest_name = @machine.config.vm.guest
-      initialize_capabilities!(guest_name, @guests, @capabilities, @machine)
-    rescue Errors::CapabilityHostExplicitNotDetected => e
-      raise Errors::GuestExplicitNotDetected, value: e.extra_data[:value]
-    rescue Errors::CapabilityHostNotDetected
-      raise Errors::GuestNotDetected
+            def initialize(_opts)
+          # If EventMachine SSL support on Windows ever gets better, the code below will
+          # set up the reactor to handle SSL
+          #
+          # @ssl_enabled = opts['ssl_cert'] && opts['ssl_key']
+          # if @ssl_enabled
+          #   em_opts[:tls_options] = {
+          #   :private_key_file => Jekyll.sanitized_path(opts['source'], opts['ssl_key']),
+          #   :cert_chain_file  => Jekyll.sanitized_path(opts['source'], opts['ssl_cert'])
+          #   }
+          #   em_opts[:secure] = true
+          # end
+    
+        def arg_is_present?(args, deprecated_argument, message)
+      deprecation_message(message) if args.include?(deprecated_argument)
     end
     
-            # Be careful with the paths
-        @local_data_path = nil  if @local_data_path == ''
-        @vagrantfile_path = nil if @vagrantfile_path == ''
+          it 'works with default_keychain and name and password' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          create_keychain ({
+            name: 'test.keychain',
+            password: 'testpassword',
+            default_keychain: true,
+          })
+        end').runner.execute(:test)
     
-            # Allows setting options from a hash. By default this simply calls
-        # the `#{key}=` method on the config class with the value, which is
-        # the expected behavior most of the time.
-        #
-        # This is expected to mutate itself.
-        #
-        # @param [Hash] options A hash of options to set on this configuration
-        #   key.
-        def set_options(options)
-          options.each do |key, value|
-            send('#{key}=', value)
-          end
-        end
+          it 'handles the exclude_dirs parameter with no elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: [])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
+      end
     
-      def prefixes
-    prefixes = ['/bin', '/usr/bin', '/usr/libexec', xcode_app_path]
-    prefixes << `brew --prefix`.strip unless `which brew`.strip.empty?
+          it 'generates the correct git command with a shell-escaped message' do
+        message = 'message with 'quotes' (and parens)'
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: './fastlane/README.md', message: \'#{message}\')
+        end').runner.execute(:test)
+        expect(result).to eq('git commit -m #{message.shellescape} ./fastlane/README.md')
+      end
+    end
+  end
+end
+
     
-      def test_symlink_exists(path)
-    exists?('L', path)
+        # An empty argument will be skipped, so return empty quotes.
+    # https://github.com/ruby/ruby/blob/a6413848153e6c37f6b0fea64e3e871460732e34/lib/shellwords.rb#L142-L143
+    return ''''.dup if str.empty?
+    
+      def navigate_to(page_name)
+    path = path_to(page_name)
+    if path.is_a?(Hash)
+      visit(path[:path])
+      await_elem = path[:special_elem]
+      find(await_elem.delete(:selector), await_elem)
+    else
+      visit(path)
+    end
   end
     
-            # Note that `scm_plugin_installed?` comes from Capistrano::DSL
-        if scm_plugin_installed?
-          delete(:scm)
-          return
+        it 'generates a jasmine fixture', fixture: true do
+      session[:mobile_view] = true
+      get :new, format: :mobile
+      save_fixture(html_for('body'), 'conversations_new_mobile')
+    end
+  end
+end
+
+    
+        # The name of the file in which the exception was raised.
+    # This could be `nil` if no filename is available.
+    #
+    # @return [String, nil]
+    def sass_filename
+      sass_backtrace.first[:filename]
+    end
+    
+          def is_create_page
+        false
+      end
+    
+          def next_link
+        label = 'Next &raquo;'
+        if @versions.size == Gollum::Page.per_page
+          link = '/history/#{@page.name}?page=#{@page_num+1}'
+          %(<a href='#{link}' hotkey='l'>#{label}</a>)
+        else
+          %(<span class='disabled'>#{label}</span>)
         end
+      end
+    end
+  end
+end
+
+    
+    module Precious
+  module Views
+    class Layout < Mustache
+      include Rack::Utils
+      alias_method :h, :escape_html
+    
+        # Extract the path string that Gollum::Wiki expects
+    def extract_path(file_path)
+      return nil if file_path.nil?
+      last_slash = file_path.rindex('/')
+      if last_slash
+        file_path[0, last_slash]
+      end
+    end
+    
+      context 'when arguments to a method' do
+    let(:prefix) { 'bar(' }
+    let(:suffix) { ')' }
+    let(:source) { construct(false, true) }
+    
+          # Checks whether the `hash` node contains any `pair`- or `kwsplat` nodes.
+      #
+      # @return[Boolean] whether the `hash` is empty
+      def empty?
+        children.empty?
+      end
+    
+        def register(klass, attachment_name, attachment_options)
+      @attachments ||= {}
+      @attachments[klass] ||= {}
+      @attachments[klass][attachment_name] = attachment_options
+    end
