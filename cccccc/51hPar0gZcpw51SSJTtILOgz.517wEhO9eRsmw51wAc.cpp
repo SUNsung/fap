eb80aee2eaad179c96ca992e3056c025456dfa02
SUNsung <weatherius@gmail.com>
2018-12-24 23:40:29 +0200

    Rewrite [51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp] File in: 2018-12-24 23:40:28

--- cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
+++ cccccc/51hPar0gZcpw51SSJTtILOgz.517wEhO9eRsmw51wAc.cpp
@@ -1,24 +1,215 @@
 
         
-        <details>
+                virtual const std::unordered_set<StreamInformation>& StreamInfos() override { return m_streamInfos; }
     
-    #endif // CAFFE2_OPERATORS_FREE_OP_H_
-
+            if (!m_lossFunction->Output().DynamicAxes().empty())
+        {
+            m_aggregatedLossFunction = ReduceSum(lossFunction, Axis::AllAxes(), L'aggregateLoss');
+            combinedFunctionArgs.push_back(m_aggregatedLossFunction);
+            m_trainingSampleCountVar = m_lossFunction;
+        }
+        else
+        {
+            m_aggregatedLossFunction = m_lossFunction;
+    }
     
-    class GetFloatToHalfGradient : public GradientMakerBase {
-  using GradientMakerBase::GradientMakerBase;
-  vector<OperatorDef> GetGradientDefs() override {
-    return SingleGradientDef(
-        'HalfToFloat', '', vector<string>{GO(0)}, vector<string>{GI(0)});
-  }
+    
+    {
+    {            if (m_unpackedShape != m_data->Shape())
+                LogicError('The computed unpacked shape '%S' of the PackedValue object does not match the actual Data NDArrayView's shape '%S' after unpacking.',
+                           m_unpackedShape.AsString().c_str(), m_data->Shape().AsString().c_str());
+        }
+    }
+    
+            VariableFields(const NDShape& shape, VariableKind varType, ::CNTK::DataType type, const std::weak_ptr<Function>& ownerFunction, const NDArrayViewPtr& value, bool needsGradient, const std::vector<Axis>& dynamicAxes, bool isSparse, const std::wstring& name, const std::wstring& uid)
+            : m_shape(shape), m_varKind(varType), m_dataType(type), m_ownerFunction(ownerFunction), m_value(value), m_needsGradient(needsGradient), m_dynamicAxes(dynamicAxes), m_isSparse(isSparse), m_name(name), m_uid(uid), m_valueTimeStamp(0)
+        {
+            if (value && (type != value->GetDataType()))
+                InvalidArgument('The DataType of the Parameter/Constant Variable '%S' does not match the DataType of the associated Value', AsString().c_str());
+    }
+    
+    // implementations differ greatly between Windows and Linux
+    
+    
+    {private:
+    long long m_start;
+    long long m_end;
 };
-REGISTER_GRADIENT(FloatToHalf, GetFloatToHalfGradient);
     
-    class GetCol2ImGradient : public GradientMakerBase {
-  using GradientMakerBase::GradientMakerBase;
-  vector<OperatorDef> GetGradientDefs() override {
-    return SingleGradientDef(
-        'Im2Col', '', std::vector<string>{GO(0)}, std::vector<string>{GI(0)});
-  }
+            // If node derives from ILateAttachingNode() then it has unresolved inputs. Resolve them now.
+        // This may generate a whole new load of nodes, including nodes which in turn have late init.
+        // Note: In case of editing, we may be adding a new node that references nodes from the old
+        // network that must be mapped because their inputs have changed. Hence, it is important to
+        // to the mapping *after* late attaching.
+        if (node->GetNumInputs() == 0) // (if this function is called during model editing, we may already have our inputs)
+        {
+            let lateAttachingNode = dynamic_pointer_cast<ILateAttachingNode>(node);
+            if (lateAttachingNode)
+                lateAttachingNode->LateAttachInputs();
+        }
+    
+        // Overrides
+    virtual void startWithTarget(Node *target) override;
+    virtual void updatePosition(const Vec2 &newPos) override;
+    virtual CardinalSplineBy *clone() const override;
+    virtual CardinalSplineBy* reverse() const override;
+    
+        /**
+    @brief Get the amplitude rate of the effect.
+    @return Return the amplitude rate of the effect.
+    */
+    float getAmplitudeRate() const { return _amplitudeRate; }
+    /**
+    @brief Set the amplitude rate of the effect.
+    @param amplitudeRate The value of amplitude rate will be set.
+    */
+    void setAmplitudeRate(float amplitudeRate) { _amplitudeRate = amplitudeRate; }
+    
+    //
+// Show
+//
+    
+        //
+    // Overrides
+    //
+    virtual void startWithTarget(Node *target) override;
+    virtual SkewBy* clone() const  override;
+    virtual SkewBy* reverse() const override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    SkewBy() {}
+    virtual ~SkewBy() {}
+    /**
+     * @param t In seconds.
+     */
+    bool initWithDuration(float t, float sx, float sy);
+    
+    
+    {
+    {
+    {            if (action->getTag() == (int)tag && action->getOriginalTarget() == target)
+            {
+                removeActionAtIndex(i, element);
+                --limit;
+            }
+            else
+            {
+                ++i;
+            }
+        }
+    }
+}
+    
+    /**
+@brief Progress from a percentage to another percentage.
+@since v0.99.1
+*/
+class CC_DLL ProgressFromTo : public ActionInterval
+{
+public:
+    /** 
+     * @brief Create and initializes the action with a duration, a 'from' percentage and a 'to' percentage.
+     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
+     * @param fromPercentage Specify the source percentage.
+     * @param toPercentage Specify the destination percentage.
+     * @return If the creation success, return a pointer of ProgressFromTo action; otherwise, return nil.
+     */
+    static ProgressFromTo* create(float duration, float fromPercentage, float toPercentage);
+    }
+    
+    /**
+@brief SplitRows action.
+@details Split the target node in many rows.
+        Then move out some rows from left, move out the other rows from right.
+*/
+class CC_DLL SplitRows : public TiledGrid3DAction
+{
+public :
+    /** 
+     * @brief Create the action with the number of rows and the duration.
+     * @param duration Specify the duration of the SplitRows action. It's a value in seconds.
+     * @param rows Specify the rows count should be split.
+     * @return If the creation success, return a pointer of SplitRows action; otherwise, return nil.
+     */
+    static SplitRows* create(float duration, unsigned int rows);
+    }
+    
+    void ActionTween::startWithTarget(Node *target)
+{
+    CCASSERT(dynamic_cast<ActionTweenDelegate*>(target), 'target must implement ActionTweenDelegate');
+    ActionInterval::startWithTarget(target);
+    _delta = _to - _from;
+}
+    
+    
+    {protected:
+    std::string       _key;
+    float            _from, _to;
+    float            _delta;
 };
-REGISTER_GRADIENT(Col2Im, GetCol2ImGradient);
+    
+    AutoPolygon::AutoPolygon(const std::string &filename)
+:_image(nullptr)
+,_data(nullptr)
+,_filename('')
+,_width(0)
+,_height(0)
+,_scaleFactor(0)
+{
+    _filename = filename;
+    _image = new (std::nothrow) Image();
+    _image->initWithImageFile(filename);
+    CCASSERT(_image->getRenderFormat()==Texture2D::PixelFormat::RGBA8888, 'unsupported format, currently only supports rgba8888');
+    _data = _image->getData();
+    _width = _image->getWidth();
+    _height = _image->getHeight();
+    _scaleFactor = Director::getInstance()->getContentScaleFactor();
+}
+    
+    void ClusterListStatus600::Parse(const std::uint8_t* bytes, int32_t length,
+                                 ContiRadar* conti_radar) const {
+  auto status = conti_radar->mutable_cluster_list_status();
+  status->set_near(near(bytes, length));
+  status->set_far(far(bytes, length));
+  status->set_meas_counter(meas_counter(bytes, length));
+  status->set_interface_version(interface_version(bytes, length));
+  auto counter = status->near() + status->far();
+  conti_radar->mutable_contiobs()->Reserve(counter);
+}
+    
+      Byte t1(bytes + 6);
+  int32_t t = t1.get_byte(5, 3);
+    
+    using apollo::drivers::canbus::Byte;
+using apollo::drivers::ContiRadarObs;
+    
+    int ObjectQualityInfo60C::longitude_accel_rms(const std::uint8_t* bytes,
+                                              int32_t length) const {
+  Byte t0(bytes + 3);
+  int32_t x = t0.get_byte(0, 4);
+    }
+    
+    #include <string>
+    
+    
+    {  MatrixXd bd_golden(20, 1);
+  bd_golden << -0.03, -0.03, -0.02, -0.04, -0.02, -0.04, -0.02, -0.04, -0.02,
+      -0.04, -0.02, -0.04, -0.02, -0.04, -0.02, -0.04, -0.02, -0.04, -0.02,
+      -0.04;
+  EXPECT_EQ(bd.rows(), 20);
+  EXPECT_EQ(bd.cols(), 1);
+  for (uint32_t i = 0; i < bd.rows(); ++i) {
+    EXPECT_DOUBLE_EQ(bd(i, 0), bd_golden(i, 0));
+  }
+}
+    
+    
+    {  for (int i = 0; i < 10; ++i) {
+    EXPECT_DOUBLE_EQ(offset(i, 0), offset_golden(i, 0));
+  }
+}
+    
+    
+    {
+    {}  // namespace planning
+}  // namespace apollo
