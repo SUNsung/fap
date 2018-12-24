1b01cd0d21d8914da0cb1393ba2d4610743b21e7
SUNsung <weatherius@gmail.com>
2018-12-24 06:41:04 +0200

    Rewrite [52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp] File in: 2018-12-24 06:41:03

--- cccccc/52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp
+++ cccccc/52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp
@@ -1,406 +1,104 @@
 
         
-        public:
-    explicit TransactionDescDialog(const QModelIndex &idx, QWidget *parent = 0);
-    ~TransactionDescDialog();
-    
-    #ifndef BITCOIN_REVERSELOCK_H
-#define BITCOIN_REVERSELOCK_H
-    
-    
-    {        secp256k1_sha256_initialize(&sha);
-        secp256k1_sha256_write(&sha, y, sizeof(y));
-        secp256k1_sha256_write(&sha, x, sizeof(x));
-        secp256k1_sha256_finalize(&sha, result);
-        ret = 1;
-    }
-    
-        /* Check bad contexts and NULLs for recovery */
-    ecount = 0;
-    CHECK(secp256k1_ecdsa_recover(none, &recpubkey, &recsig, message) == 0);
-    CHECK(ecount == 1);
-    CHECK(secp256k1_ecdsa_recover(sign, &recpubkey, &recsig, message) == 0);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(vrfy, &recpubkey, &recsig, message) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, message) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(both, NULL, &recsig, message) == 0);
-    CHECK(ecount == 3);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, NULL, message) == 0);
-    CHECK(ecount == 4);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, NULL) == 0);
-    CHECK(ecount == 5);
-    
-    
-    {    void operator() (const T * src0, const T * src1, T * dst) const
-    {
-        dst[0] = saturate_cast<T>(alpha*src0[0] + beta*src1[0] + gamma);
-    }
-};
-    
-    #define CVTS_FUNC(T1, T2, SIMD_SIZE, CVTINIT, CVTROW)                            \
-    void convertScale(const Size2D &_size,                                       \
-                      const T1 * srcBase, ptrdiff_t srcStride,                   \
-                      T2 * dstBase, ptrdiff_t dstStride,                         \
-                      f64 alpha, f64 beta)                                       \
-    {                                                                            \
-        internal::assertSupportedConfiguration();                                \
-        Size2D size(_size);                                                      \
-        if (srcStride == dstStride &&                                            \
-            srcStride == (ptrdiff_t)(size.width))                                \
-        {                                                                        \
-            size.width *= size.height;                                           \
-            size.height = 1;                                                     \
-        }                                                                        \
-        const ptrdiff_t sstep = srcStride / sizeof(T1);                          \
-        const ptrdiff_t dstep = dstStride / sizeof(T2);                          \
-        const size_t w = size.width & ~(SIMD_SIZE-1);                            \
-        if (size.width >= SIMD_SIZE)                                             \
-        {                                                                        \
-            const T1* _src = srcBase;                                            \
-            T2* _dst = dstBase;                                                  \
-            CVTINIT                                                              \
-            for (ptrdiff_t h = size.height; h--; _src += sstep, _dst += dstep )  \
-                CVTROW                                                           \
-        }                                                                        \
-        if(w < size.width)                                                       \
-        {                                                                        \
-            const T1* _src = srcBase;                                            \
-            T2* _dst = dstBase;                                                  \
-            for (ptrdiff_t h = size.height; h--; _src += sstep, _dst += dstep )  \
-                for(size_t i = w; i < size.width; i++ )                          \
-                    _dst[i] = internal::saturate_cast<T2>(_src[i]*alpha + beta); \
-        }                                                                        \
-    }
-    
-    
-    {
-    {            if (s[0] < 0 || s[1] < 0)//saturate in case of overflow ~ 4GB of non-zeros...
-            {
-                return 0x7fFFffFF;
-            }
-            result += (s[0] += s[1]);
-            if (s[0] < 0 || result < 0)
-            {
-                return 0x7fFFffFF;
-            }
-        }
-        for (; i < size.width; i++)
-            result += (src[i] != 0)?1:0;
-        if (result < 0)//saturate in case of overflow ~ 4GB of non-zeros...
-        {
-            return 0x7fFFffFF;
-        }
-    }
-    return result;
-#else
-    (void)_size;
-    (void)srcBase;
-    (void)srcStride;
-    
-        u64 mask[2] = { 0ul };
-    
-    #ifndef __ANDROID__
-    u32 step_base = 16 / sizeof(T), step_base3 = step_base * 3;
-    size_t roiw_base = size.width >= (step_base - 1) ? size.width - step_base + 1 : 0;
-#endif
-    u32 step_tail = 8 / sizeof(T), step_tail3 = step_tail * 3;
-    size_t roiw_tail = size.width >= (step_tail - 1) ? size.width - step_tail + 1 : 0;
-    
-    inline float32x2_t vrsqrt_f32(float32x2_t val)
-{
-    float32x2_t e = vrsqrte_f32(val);
-    e = vmul_f32(vrsqrts_f32(vmul_f32(e, e), val), e);
-    e = vmul_f32(vrsqrts_f32(vmul_f32(e, e), val), e);
-    return e;
-}
-    
-    int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPTSTR    lpCmdLine,
-	_In_ int       nCmdShow)
-{
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
-	UNREFERENCED_PARAMETER(nCmdShow);
-    }
-    
-    //---------------------------------------------------------------------------------
-// The following part are API Registration of Operators
-// See also MXNET_REGISTER_SIMPLE_OP in operator_util.h for registering simple ops.
-//---------------------------------------------------------------------------------
-/*!
- * \brief Macro to register OperatorProperty
- *
- * \code
- * // example of registering a fully connected operator
- * REGISTER_OP_PROPERTY(FullyConnected, FullyConnectedOpProp)
- * .describe('Fully connected layer');
- *
- * \endcode
+        /**
+ * Class to iterate over tesseract page structure, providing access to all
+ * levels of the page hierarchy, without including any tesseract headers or
+ * having to handle any tesseract structures.
+ * WARNING! This class points to data held within the TessBaseAPI class, and
+ * therefore can only be used while the TessBaseAPI class still exists and
+ * has not been subjected to a call of Init, SetImage, Recognize, Clear, End
+ * DetectOS, or anything else that changes the internal PAGE_RES.
+ * See apitypes.h for the definition of PageIteratorLevel.
+ * See also ResultIterator, derived from PageIterator, which adds in the
+ * ability to access OCR output with text-specific methods.
  */
-#define MXNET_REGISTER_OP_PROPERTY(name, OperatorPropertyType)          \
-  DMLC_REGISTRY_REGISTER(::mxnet::OperatorPropertyReg, OperatorPropertyReg, name) \
-  .set_body([]() { return new OperatorPropertyType(); })                \
-  .set_return_type('NDArray-or-Symbol') \
-  .check_name()
     
-    
-    {  DMLC_DECLARE_PARAMETER(CaffeLossParam) {
-    DMLC_DECLARE_FIELD(prototxt).set_default('layer{}')
-    .describe('Caffe's layer parameter');
-    DMLC_DECLARE_FIELD(num_data).set_range(0, 100).set_default(2)
-    .describe('Operator input number');
-    DMLC_DECLARE_FIELD(num_out).set_range(0, 100).set_default(1)
-    .describe('Operator output number');
-    DMLC_DECLARE_FIELD(grad_scale)
-    .set_default(1.0f)
-    .describe('Scale the gradient by a float factor (a.k.a weight of this loss).');
-  }
-};
-    
-    
-    {
-    {}  // namespace op
-}  // namespace mxnet
+    #endif  // TESSERACT_CCMAIN_PARAGRAPHS_H_
 
     
-    /*!
- * \brief tblob batch
- *
- * data are stored in tblob before going into NDArray
- */
-struct TBlobBatch {
+    
+    {  for (std::map<int, ParamContent*>::iterator iter = vcMap.begin();
+                                          iter != vcMap.end();
+                                          ++iter) {
+    ParamContent* cur = iter->second;
+    if (!changes_only || cur->HasChanged()) {
+      fprintf(fp, '%-25s   %-12s   # %s\n',
+              cur->GetName(), cur->GetValue().string(), cur->GetDescription());
+    }
+  }
+  fclose(fp);
+}
+#endif // GRAPHICS_DISABLED
+
+    
+      // Construct the cartesian product of the best_choices of word(1) and word2.
+  WERD_CHOICE_LIST joined_choices;
+  WERD_CHOICE_IT jc_it(&joined_choices);
+  WERD_CHOICE_IT bc1_it(&word->best_choices);
+  WERD_CHOICE_IT bc2_it(&word2->best_choices);
+  int num_word1_choices = word->best_choices.length();
+  int total_joined_choices = num_word1_choices;
+  // Nota Bene: For the main loop here, we operate only on the 2nd and greater
+  // word2 choices, and put them in the joined_choices list. The 1st word2
+  // choice gets added to the original word1 choices in-place after we have
+  // finished with them.
+  int bc2_index = 1;
+  for (bc2_it.forward(); !bc2_it.at_first(); bc2_it.forward(), ++bc2_index) {
+    if (total_joined_choices >= kTooManyAltChoices &&
+        bc2_index > kAltsPerPiece)
+      break;
+    int bc1_index = 0;
+    for (bc1_it.move_to_first(); bc1_index < num_word1_choices;
+        ++bc1_index, bc1_it.forward()) {
+      if (total_joined_choices >= kTooManyAltChoices &&
+          bc1_index > kAltsPerPiece)
+        break;
+      WERD_CHOICE *wc = new WERD_CHOICE(*bc1_it.data());
+      *wc += *bc2_it.data();
+      jc_it.add_after_then_move(wc);
+      ++total_joined_choices;
+    }
+  }
+  // Now that we've filled in as many alternates as we want, paste the best
+  // choice for word2 onto the original word alt_choices.
+  bc1_it.move_to_first();
+  bc2_it.move_to_first();
+  for (bc1_it.mark_cycle_pt(); !bc1_it.cycled_list(); bc1_it.forward()) {
+    *bc1_it.data() += *bc2_it.data();
+  }
+  bc1_it.move_to_last();
+  bc1_it.add_list_after(&joined_choices);
+    
+    // Class to hold an array of bounding boxes for an output word and
+// the bounding box of the whole word.
+class BoxWord {
  public:
-  /*! \brief unique id for instance, can be NULL, sometimes is useful */
-  unsigned *inst_index;
-  /*! \brief number of instance */
-  mshadow::index_t batch_size;
-  /*! \brief number of padding elements in this batch,
-       this is used to indicate the last elements in the batch are only padded up to match the batch, and should be discarded */
-  mshadow::index_t num_batch_padd;
-  /*! \brief content of dense data */
-  std::vector<TBlob> data;
-  /*! \brief extra data to be fed to the network */
-  std::string extra_data;
-  /*! \brief constructor */
-  TBlobBatch(void) {
-    inst_index = NULL;
-    batch_size = 0; num_batch_padd = 0;
-  }
-  /*! \brief destructor */
-  ~TBlobBatch() {
-    delete[] inst_index;
-  }
-};  // struct TBlobBatch
-    
-    The `LibSVMIter` only support `round_batch` parameter set to ``True``. Therefore, if `batch_size`
-is 3 and there are 4 total rows in libsvm file, 2 more examples are consumed at the first round.
-    
-    //---- Don't implement demo windows functionality (ShowDemoWindow()/ShowStyleEditor()/ShowUserGuide() methods will be empty)
-//---- It is very strongly recommended to NOT disable the demo windows during development. Please read the comments in imgui_demo.cpp.
-//#define IMGUI_DISABLE_DEMO_WINDOWS
-    
-    // About GLSL version:
-// The 'glsl_version' initialization parameter should be NULL (default) or a '#version XXX' string.
-// On computer platform the GLSL version default to '#version 130'. On OpenGL ES 3 platform it defaults to '#version 300 es'
-// Only override if your GL version doesn't handle this GLSL version. See GLSL version table at the top of imgui_impl_opengl3.cpp.
-    
-    
-    {    if (g_pBlendState) { g_pBlendState->Release(); g_pBlendState = NULL; }
-    if (g_pDepthStencilState) { g_pDepthStencilState->Release(); g_pDepthStencilState = NULL; }
-    if (g_pRasterizerState) { g_pRasterizerState->Release(); g_pRasterizerState = NULL; }
-    if (g_pPixelShader) { g_pPixelShader->Release(); g_pPixelShader = NULL; }
-    if (g_pPixelShaderBlob) { g_pPixelShaderBlob->Release(); g_pPixelShaderBlob = NULL; }
-    if (g_pVertexConstantBuffer) { g_pVertexConstantBuffer->Release(); g_pVertexConstantBuffer = NULL; }
-    if (g_pInputLayout) { g_pInputLayout->Release(); g_pInputLayout = NULL; }
-    if (g_pVertexShader) { g_pVertexShader->Release(); g_pVertexShader = NULL; }
-    if (g_pVertexShaderBlob) { g_pVertexShaderBlob->Release(); g_pVertexShaderBlob = NULL; }
-}
-    
-                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox('Another Window', &show_another_window);
-    
-            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-    
-        // Initialize OpenGL loader
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-    bool err = gl3wInit() != 0;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-    bool err = glewInit() != GLEW_OK;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-    bool err = gladLoadGL() == 0;
-#else
-    bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
-#endif
-    if (err)
-    {
-        fprintf(stderr, 'Failed to initialize OpenGL loader!\n');
-        return 1;
+  BoxWord();
+  explicit BoxWord(const BoxWord& src);
+  ~BoxWord() = default;
     }
     
-    HRESULT CreateDeviceD3D(HWND hWnd)
-{
-    // Setup swap chain
-    DXGI_SWAP_CHAIN_DESC sd;
-    ZeroMemory(&sd, sizeof(sd));
-    sd.BufferCount = 2;
-    sd.BufferDesc.Width = 0;
-    sd.BufferDesc.Height = 0;
-    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    sd.BufferDesc.RefreshRate.Numerator = 60;
-    sd.BufferDesc.RefreshRate.Denominator = 1;
-    sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
-    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    sd.OutputWindow = hWnd;
-    sd.SampleDesc.Count = 1;
-    sd.SampleDesc.Quality = 0;
-    sd.Windowed = TRUE;
-    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-    }
+          case kLastType:
+        if (!in_fragmented_record) {
+          ReportCorruption(fragment.size(),
+                           'missing start of fragmented record(2)');
+        } else {
+          scratch->append(fragment.data(), fragment.size());
+          *record = Slice(*scratch);
+          last_record_offset_ = prospective_record_offset;
+          return true;
+        }
+        break;
     
-    /** @class FiniteTimeAction
- * @brief
- * Base class actions that do have a finite time duration.
- * Possible actions:
- * - An action with a duration of 0 seconds.
- * - An action with a duration of 35.5 seconds.
- * Infinite time actions are valid.
- */
-class CC_DLL FiniteTimeAction : public Action
-{
-public:
-    /** Get duration in seconds of the action. 
-     *
-     * @return The duration in seconds of the action.
-     */
-    float getDuration() const { return _duration; }
-    /** Set duration in seconds of the action. 
-     *
-     * @param duration In seconds of the action.
-     */
-    void setDuration(float duration) { _duration = duration; }
-    }
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+      Status AddRecord(const Slice& slice);
     
     
-    {        return true;
-    }
+    {}  // namespace leveldb
     
-    bool Ripple3D::initWithDuration(float duration, const Size& gridSize, const Vec2& position, float radius, unsigned int waves, float amplitude)
-{
-    if (Grid3DAction::initWithDuration(duration, gridSize))
-    {
-        setPosition(position);
-        _radius = radius;
-        _waves = waves;
-        _amplitude = amplitude;
-        _amplitudeRate = 1.0f;
-    }
-    }
-    
-        /** 
-    @brief Initializes an action with duration.
-    @param duration Specify the duration of the FlipX3D action. It's a value in seconds.
-    @return If the initialization success, return true; otherwise, return false.
-    */
-    bool initWithDuration(float duration);
-    
-    /** @class SkewBy
-* @brief Skews a Node object by skewX and skewY degrees.
-@since v1.0
-*/
-class CC_DLL SkewBy : public SkewTo
-{
-public:
-    /** 
-     * Creates the action.
-     * @param t Duration time, in seconds.
-     * @param deltaSkewX Skew x delta angle.
-     * @param deltaSkewY Skew y delta angle.
-     * @return An autoreleased SkewBy object.
-     */
-    static SkewBy* create(float t, float deltaSkewX, float deltaSkewY);
-    }
-    
-    /** @class ActionManager
- @brief ActionManager is a singleton that manages all the actions.
- Normally you won't need to use this singleton directly. 99% of the cases you will use the Node interface,
- which uses this singleton.
- But there are some cases where you might need to use this singleton.
- Examples:
-    - When you want to run an action where the target is different from a Node. 
-    - When you want to pause / resume the actions.
- 
- @since v0.8
- */
-class CC_DLL ActionManager : public Ref
-{
-public:
-    /**
-     * @js ctor
-     */
-    ActionManager();
-    }
-    
-    #include '2d/CCActionGrid3D.h'
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-    bool Animation::init()
-{
-    _loops = 1;
-    _delayPerUnit = 0.0f;
-    
-    return true;
-}
-    
-    AtlasNode * AtlasNode::create(const std::string& tile, int tileWidth, int tileHeight, int itemsToRender)
-{
-    AtlasNode * ret = new (std::nothrow) AtlasNode();
-    if (ret->initWithTileFile(tile, tileWidth, tileHeight, itemsToRender))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    CC_SAFE_DELETE(ret);
-    return nullptr;
-}
-    
-      void set_initialized() { initialized_.store(true); }
-    
-    #include 'rocksdb/slice.h'
-#include 'rocksdb/status.h'
-    
-    namespace {
-// A dummy compaction filter
-class DummyCompactionFilter : public CompactionFilter {
+    class SnapshotList {
  public:
-  virtual ~DummyCompactionFilter() {}
-  virtual bool Filter(int level, const Slice& key, const Slice& existing_value,
-                      std::string* new_value, bool* value_changed) const {
-    return false;
+  SnapshotList() : head_(0) {
+    head_.prev_ = &head_;
+    head_.next_ = &head_;
   }
-  virtual const char* Name() const { return 'DummyCompactionFilter'; }
-};
-    }
-    
-    namespace rocksdb {
     }
