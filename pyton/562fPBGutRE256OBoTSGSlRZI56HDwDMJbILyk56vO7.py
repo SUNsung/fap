e570ba795244d26f6725b98d070a027b2fe695f5
SUNsung <weatherius@gmail.com>
2018-12-24 05:34:58 +0200

    Rewrite [562fPBGutRE256OBoTSGSlRZI56HDwDMJbILyk56vO7.py] File in: 2018-12-24 05:34:58

--- pyton/562fPBGutRE256OBoTSGSlRZI56HDwDMJbILyk56vO7.py
+++ pyton/562fPBGutRE256OBoTSGSlRZI56HDwDMJbILyk56vO7.py
@@ -1,175 +1,183 @@
-    # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
-    app.add_url_rule('/', endpoint='index')
+
+        
+        
+class PyTest(TestCommand):
+    # `$ python setup.py test' simply installs minimal requirements
+    # and runs the tests with no fancy stuff like parallel execution.
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '--doctest-modules', '--verbose',
+            './httpie', './tests'
+        ]
+        self.test_suite = True
     
     
-def get_db():
-    '''Connect to the application's configured database. The connection
-    is unique for each request and will be reused if this is called
-    again.
-    '''
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+def test_missing_auth(httpbin):
+    r = http(
+        '--auth-type=basic',
+        'GET',
+        httpbin + '/basic-auth/user/password',
+        error_exit_ok=True
+    )
+    assert HTTP_OK not in r
+    assert '--auth required' in r.stderr
+
     
-        def app_url_defaults(self, f):
-        '''Same as :meth:`url_defaults` but application wide.
-        '''
-        self.record_once(lambda s: s.app.url_default_functions
-            .setdefault(None, []).append(f))
-        return f
+        def test_download_interrupted(self, httpbin_both):
+        devnull = open(os.devnull, 'w')
+        downloader = Downloader(output_file=devnull, progress_file=devnull)
+        downloader.start(Response(
+            url=httpbin_both.url + '/',
+            headers={'Content-Length': 5}
+        ))
+        downloader.chunk_downloaded(b'1234')
+        downloader.finish()
+        assert downloader.interrupted
+
     
-    
-class JSONEncoder(_json.JSONEncoder):
-    '''The default Flask JSON encoder.  This one extends the default simplejson
-    encoder by also supporting ``datetime`` objects, ``UUID`` as well as
-    ``Markup`` objects which are serialized as RFC 822 datetime strings (same
-    as the HTTP date format).  In order to support more data types override the
-    :meth:`default` method.
-    '''
-    
-    from __future__ import absolute_import
-    
-        class _FakeSignal(object):
-        '''If blinker is unavailable, create a fake class with the same
-        interface that allows sending of signals but will fail with an
-        error on anything else.  Instead of doing anything on send, it
-        will just ignore the arguments and do nothing instead.
-        '''
-    
-            # on exit we want to clean up earlier.  Normally the request context
-        # stays preserved until the next request in the same thread comes
-        # in.  See RequestGlobals.push() for the general behavior.
-        top = _request_ctx_stack.top
-        if top is not None and top.preserved:
-            top.pop()
-    
-    from httpie import sessions
-from httpie import __version__
-from httpie.compat import str
-from httpie.input import SSL_VERSION_ARG_MAPPING
-from httpie.plugins import plugin_manager
-from httpie.utils import repr_dict_nice
-    
-            # Keyword arguments > stream.encoding > default utf8
-        if self.stdin_encoding is None:
-            self.stdin_encoding = getattr(
-                self.stdin, 'encoding', None) or 'utf8'
-        if self.stdout_encoding is None:
-            actual_stdout = self.stdout
-            if is_windows:
-                # noinspection PyUnresolvedReferences
-                from colorama import AnsiToWin32
-                if isinstance(self.stdout, AnsiToWin32):
-                    actual_stdout = self.stdout.wrapped
-            self.stdout_encoding = getattr(
-                actual_stdout, 'encoding', None) or 'utf8'
-    
-        if resp:
-        output.append(Stream(
-            msg=HTTPResponse(response),
-            with_headers=resp_h,
-            with_body=resp_b))
-    
-            '''
-        r.headers['Authorization'] = type(self).make_header(
-            self.username, self.password).encode('latin1')
-        return r
-    
-        '''
-    abbrevs = [
-        (1 << 50, 'PB'),
-        (1 << 40, 'TB'),
-        (1 << 30, 'GB'),
-        (1 << 20, 'MB'),
-        (1 << 10, 'kB'),
-        (1, 'B')
-    ]
-    
-    
-def test_basic_auth(httpbin_both):
-    r = http('--auth=user:password',
-             'GET', httpbin_both + '/basic-auth/user/password')
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-        plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            BASIC_AUTH_HEADER_VALUE,
-        )
+        def test_request_body_from_file_by_path(self, httpbin):
+        r = http('--verbose',
+                 'POST', httpbin.url + '/post', '@' + FILE_PATH_ARG)
         assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
+        assert FILE_CONTENT in r, r
+        assert ''Content-Type': 'text/plain'' in r
     
-            def attempts(unique_on_attempt=0):
-            # noinspection PyUnresolvedReferences,PyUnusedLocal
-            def exists(filename):
-                if exists.attempt == unique_on_attempt:
-                    return False
-                exists.attempt += 1
-                return True
+                gc.collect()
+            print('- benchmarking Lasso')
+            clf = Lasso(alpha=alpha, fit_intercept=False,
+                        precompute=precompute)
+            tstart = time()
+            clf.fit(X, Y)
+            lasso_results.append(time() - tstart)
     
-        chmod = os.chmod
+                print('K-Means')
+            tstart = time()
+            kmeans = KMeans(init='k-means++', n_clusters=10).fit(data)
     
-    def normcase(s):
-    '''Normalize case of pathname.  Has no effect under Posix'''
-    s = os.fspath(s)
-    if not isinstance(s, (bytes, str)):
-        raise TypeError('normcase() argument must be str or bytes, '
-                        'not '{}''.format(s.__class__.__name__))
-    return s
+    import gc
+import sys
+from time import time
     
-    # Create the base text message.
-msg = EmailMessage()
-msg['Subject'] = 'Ayons asperges pour le déjeuner'
-msg['From'] = Address('Pepé Le Pew', 'pepe', 'example.com')
-msg['To'] = (Address('Penelope Pussycat', 'penelope', 'example.com'),
-             Address('Fabrette Pussycat', 'fabrette', 'example.com'))
-msg.set_content('''\
-Salut!
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
+
     
-    import sqlite3
+    import numpy as np
+import scipy.sparse as sp
     
-    from .const import (
-    ATTR_MAIN_TEXT, ATTR_REDIRECTION_URL, ATTR_STREAM_URL, ATTR_TITLE_TEXT,
-    ATTR_UID, ATTR_UPDATE_DATE, CONF_AUDIO, CONF_DISPLAY_URL, CONF_TEXT,
-    CONF_TITLE, CONF_UID, DATE_FORMAT)
+        fig = plt.figure('scikit-learn sample w/o replacement benchmark results')
+    plt.title('n_population = %s, n_times = %s' %
+              (opts.n_population, opts.n_times))
+    ax = fig.add_subplot(111)
+    for name in sampling_algorithm:
+        ax.plot(ratio, time[name], label=name)
     
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/binary_sensor.flic/
-'''
-import logging
-import threading
+        xx = np.arange(start_dim, start_dim + n * step, step)
+    plt.subplot(212)
+    plt.title('Learning in high dimensional spaces')
+    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
+    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
+    plt.legend(loc='upper left')
+    plt.xlabel('number of dimensions')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
     
-            active_clients = [client for client in data.values() if
-                          client['status']]
-        self.last_results = active_clients
-        return True
     
-            # Flag C stands for CONNECTED
-        active_clients = [client for client in data.values() if
-                          client['status'].find('C') != -1]
-        self.last_results = active_clients
-        return True
+def json_urlread(url):
+    try:
+        return json.loads(urlopen(url).read().decode('utf8'))
+    except Exception:
+        print('Error reading', url, file=sys.stderr)
+        raise
     
-        @Throttle(MIN_TIME_BETWEEN_STORE_UPDATES)
-    def update_closest_store(self):
-        '''Update the shared closest store (if open).'''
-        from pizzapi.address import StoreException
-        try:
-            self.closest_store = self.address.closest_store()
-            return True
-        except StoreException:
-            self.closest_store = None
-            return False
+    
+def user_role(name, rawtext, text, lineno,
+              inliner, options=None, content=None):
+    '''Sphinx role for linking to a user profile. Defaults to linking to
+    GitHub profiles, but the profile URIS can be configured via the
+    ``issues_user_uri`` config value.
+    
+    
+if not os.path.exists(DATA_FOLDER):
+    
+        if not app.config.edit_on_github_project:
+        warnings.warn('edit_on_github_project not specified')
+        return
+    if not doctree:
+        warnings.warn('doctree is None')
+        return
+    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
+    show_url = get_github_url(app, 'blob', path)
+    edit_url = get_github_url(app, 'edit', path)
+    
+            self._hass.bus.fire(EVENT_NAME, {
+            EVENT_DATA_NAME: self.name,
+            EVENT_DATA_ADDRESS: self.address,
+            EVENT_DATA_QUEUED_TIME: time_diff,
+            EVENT_DATA_TYPE: hass_click_type
+        })
+    
+        hass.services.register(DOMAIN, SERVICE_BROWSE_URL,
+                           lambda service:
+                           webbrowser.open(service.data[ATTR_URL]),
+                           schema=SERVICE_BROWSE_URL_SCHEMA)
+    
+        return scanner if scanner.success_init else None
+    
+                    response = await self.websession.get(url)
+    
+            self.last_results = {}
+        self.token = _get_token(self.host, self.username, self.password)
+    
+    
+if __name__ == '__main__':
+    
+    
+class Specification(object):
+    def and_specification(self, candidate):
+        raise NotImplementedError()
+    
+    
+class Radio(object):
+    
+        def template():
+        skeleton(getter, action)
+    
+    
+def construct_building(cls):
+    building = cls()
+    building.build_floor()
+    building.build_size()
+    return building
+    
+        @lazy_property
+    def relatives(self):
+        # Get all relatives, let's assume that it costs much time.
+        relatives = 'Many relatives.'
+        return relatives
+    
+            if start == end:
+            return path
+        shortest = None
+        for node in self.graph.get(start, []):
+            if node not in path:
+                newpath = self.find_shortest_path(node, end, path[:])
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
+    
+        def on_switchover(self):
+        super(Standby, self).on_switchover()  # message ignored
+        self._hsm._next_state('active')
