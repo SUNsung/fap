bf332e4be533debffb238ce90a5023d2e18a78f9
SUNsung <weatherius@gmail.com>
2018-12-24 02:10:59 +0200

    Rewrite [17pj.gFUvTDOA17tUOAPFBIaUU17XmdtBq.ulLw17gq3.cpp] File in: 2018-12-24 02:10:59

--- cccccc/17pj.gFUvTDOA17tUOAPFBIaUU17XmdtBq.ulLw17gq3.cpp
+++ cccccc/17pj.gFUvTDOA17tUOAPFBIaUU17XmdtBq.ulLw17gq3.cpp
@@ -1,255 +1,212 @@
 
         
-        #include <utility>
+        #include 'content/nw/src/api/clipboard/clipboard.h'
     
-    
-    {  DISALLOW_COPY_AND_ASSIGN(UvTaskRunner);
-};
-    
-    AutoUpdater::~AutoUpdater() {
-  auto_updater::AutoUpdater::SetDelegate(nullptr);
-}
-    
-    // static
-void GlobalShortcut::BuildPrototype(v8::Isolate* isolate,
-                                    v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, 'GlobalShortcut'));
-  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
-      .SetMethod('register', &GlobalShortcut::Register)
-      .SetMethod('isRegistered', &GlobalShortcut::IsRegistered)
-      .SetMethod('unregister', &GlobalShortcut::Unregister)
-      .SetMethod('unregisterAll', &GlobalShortcut::UnregisterAll);
-}
-    
-    
-    {}  // namespace
-    
-    #include 'atom/browser/api/event_emitter.h'
-#include 'atom/browser/mac/in_app_purchase.h'
-#include 'atom/browser/mac/in_app_purchase_observer.h'
-#include 'atom/browser/mac/in_app_purchase_product.h'
-#include 'native_mate/handle.h'
-    
-    
-    {}  // namespace atom
-    
-    #ifndef ATOM_BROWSER_API_ATOM_API_NOTIFICATION_H_
-#define ATOM_BROWSER_API_ATOM_API_NOTIFICATION_H_
-    
-      // Calculate the depth at which the requirement's generic parameters
-  // appear in the synthetic signature.
-  unsigned depth = 0;
-  if (covariantSelf) {
-    depth++;
-  }
-  if (conformanceSig) {
-    depth += conformanceSig->getGenericParams().back()->getDepth() + 1;
+      template<typename T> T* AddListener() {
+    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
+    if (i==listerners_.end()) {
+      T* listener_object = new T(this);
+      listerners_[T::id] = listener_object;
+      return listener_object;
+    }
+    return NULL;
   }
     
-    /// When splitting based on a preposition, whether we should place the
-/// preposition on the argument label (vs. on the base name).
-static bool shouldPlacePrepositionOnArgLabel(StringRef beforePreposition,
-                                             StringRef preposition,
-                                             StringRef afterPreposition) {
-  // X/Y/Z often used as coordinates and should be the labels.
-  if (afterPreposition == 'X' ||
-      afterPreposition == 'Y' ||
-      afterPreposition == 'Z')
-    return false;
+    void Menu::Call(const std::string& method,
+                const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  if (method == 'Append') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    Append(object_manager()->GetApiObject<MenuItem>(object_id));
+  } else if (method == 'Insert') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Remove') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Popup') {
+    int x = 0;
+    arguments.GetInteger(0, &x);
+    int y = 0;
+    arguments.GetInteger(1, &y);
+    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
+    DCHECK(web_contents);
+    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
+    }
     }
     
-      static CFPointeeInfo forRecord(bool isConst, const clang::RecordDecl *decl) {
-    assert(decl);
-    CFPointeeInfo info;
-    info.IsValid = true;
-    info.IsConst = isConst;
-    info.Decl = decl;
-    return info;
-  }
+    void MenuItem::SetChecked(bool checked) {
+  // Set active will cause 'activate' to be emitted, so block here
+  block_active_ = true;
+  gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_item_), checked);
+  block_active_ = false;
+}
     
-      const ASTContext &ctx = ImporterImpl.SwiftContext;
     
-    /* Coin network-specific GUI style information */
-class NetworkStyle
-{
-public:
-    /** Get style associated with provided BIP70 network id, or 0 if not known */
-    static const NetworkStyle *instantiate(const QString &networkId);
+    {  int menu_id;
+  if (option.GetInteger('submenu', &menu_id))
+    SetSubmenu(object_manager()->GetApiObject<Menu>(menu_id));
+}
+    
+      static void DoJob(AppWindowRegistry* registry, std::string id);
+ protected:
+  ~NwAppCloseAllWindowsFunction() override {}
+    
+    bool NwObjAllocateIdFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  response->AppendInteger(nw::ObjectManager::AllocateId());
+  return true;
+}
+    
+    namespace extensions {
     }
     
-    #ifndef BITCOIN_QT_QVALUECOMBOBOX_H
-#define BITCOIN_QT_QVALUECOMBOBOX_H
-    
+      // implement nw.Screen.getScreens()
+  class NwScreenGetScreensFunction: public NWSyncExtensionFunction {
     public:
-    explicit TrafficGraphWidget(QWidget *parent = 0);
-    void setClientModel(ClientModel *model);
-    int getGraphRangeMins() const;
-    
-    /**
- * Changelog:
- * - March 2013, Diederik Huys:    original version
- * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
- * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
- */
-    
-        secp256k1_context_set_error_callback(tctx, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_illegal_callback(tctx, counting_illegal_callback_fn, &ecount);
-    CHECK(secp256k1_ec_pubkey_create(tctx, &point, s_one) == 1);
-    
-    static int recovery_test_nonce_function(unsigned char *nonce32, const unsigned char *msg32, const unsigned char *key32, const unsigned char *algo16, void *data, unsigned int counter) {
-    (void) msg32;
-    (void) key32;
-    (void) algo16;
-    (void) data;
+      NwScreenGetScreensFunction();
+      bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-    
-    {} // namespace caffe2
-    
-    namespace caffe2 {
-    }
-    
-    OPERATOR_SCHEMA(Glu)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-Applies gated linear unit to the input Tensor X. The output Y is half the size
-of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
-[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
-X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
-)DOC')
-    .Input(0, 'X', '1D input tensor')
-    .Output(0, 'Y', '1D output tensor');
-    
-    std::vector<grpc::string_ref> SecureAuthContext::GetPeerIdentity() const {
-  if (!ctx_) {
-    return std::vector<grpc::string_ref>();
-  }
-  grpc_auth_property_iterator iter = grpc_auth_context_peer_identity(ctx_);
-  std::vector<grpc::string_ref> identity;
-  const grpc_auth_property* property = nullptr;
-  while ((property = grpc_auth_property_iterator_next(&iter))) {
-    identity.push_back(
-        grpc::string_ref(property->value, property->value_length));
-  }
-  return identity;
+    // Inserts a new box before the given index.
+// Recomputes the bounding box.
+void BoxWord::InsertBox(int index, const TBOX& box) {
+  if (index < length_)
+    boxes_.insert(box, index);
+  else
+    boxes_.push_back(box);
+  length_ = boxes_.size();
+  ComputeBoundingBox();
 }
     
-    #include <grpcpp/security/auth_context.h>
+    #pragma once
     
-    // server cumulative
-const ViewDescriptor& ServerSentBytesPerRpcCumulative() {
-  const static ViewDescriptor descriptor =
-      ViewDescriptor()
-          .set_name('grpc.io/server/received_bytes_per_rpc/cumulative')
-          .set_measure(kRpcServerSentBytesPerRpcMeasureName)
-          .set_aggregation(BytesDistributionAggregation())
-          .add_column(ServerMethodTagKey());
-  return descriptor;
-}
-    
-                if (smoothedGradientValue->GetDataType() != checkpointedValue.GetDataType())
-                LogicError('DataType of the smoothed gradient value restored from checkpoint for the parameter '%S' (uid = %ls) does not match the expected value.',
-                            parameter.AsString().c_str(), uid.c_str());
-    
-    namespace CNTK
+    // Render function
+// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
+void ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data)
 {
-    class UDFUtils
+    ID3D10Device* ctx = g_pd3dDevice;
+    }
+    
+        // Setup orthographic projection matrix
+    // Our visible imgui space lies from draw_data->DisplayPos (top left) to draw_data->DisplayPos+data_data->DisplaySize (bottom right).
     {
-    public:
-    }
-    }
-    
-            void Erase() override
-        {
-            if (IsPacked())
-            {
-                m_packedData = nullptr;
-                m_packedDataLayout = nullptr;
-                m_isPacked = false;
-            }
-            else
-                Value::Erase();
-        }
-    
-    
-    {        auto originalConstantValue = Value();
-        auto constantValueCPU = originalConstantValue->DeepClone(DeviceDescriptor::CPUDevice(), true);
-        NDArrayViewPtr newConstantValue = CloneAsDataType(constantValueCPU, dataType, true);
-        return Constant(newConstantValue->DeepClone(originalConstantValue->Device(), originalConstantValue->IsReadOnly()), Name());
+        float L = draw_data->DisplayPos.x;
+        float R = draw_data->DisplayPos.x + draw_data->DisplaySize.x;
+        float T = draw_data->DisplayPos.y;
+        float B = draw_data->DisplayPos.y + draw_data->DisplaySize.y;
+        ALLEGRO_TRANSFORM transform;
+        al_identity_transform(&transform);
+        al_use_transform(&transform);
+        al_orthographic_transform(&transform, L, T, 1.0f, R, B, -1.0f);
+        al_use_projection_transform(&transform);
     }
     
-    // some older code uses this namespace
-namespace DebugUtil
-{
-    void PrintCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
-    }
-    
-    class ScopeTimer
-{
-    Timer m_aggregateTimer;
-    size_t m_verbosity;
-    std::string m_message;
-    }
-    
-        //request matrices needed to do node function value evaluation
-    virtual void RequestMatricesBeforeForwardProp(MatrixPool& matrixPool)
+        if (int size = s3eClipboardGetText(NULL, 0))
     {
-        Base::RequestMatricesBeforeForwardProp(matrixPool);
-        RequestMatrixFromPool(m_maxIndexes0, matrixPool);
-        RequestMatrixFromPool(m_maxIndexes1, matrixPool);
-        RequestMatrixFromPool(m_maxValues, matrixPool);
+        if (g_ClipboardText)
+            delete[] g_ClipboardText;
+        g_ClipboardText = new char[size];
+        g_ClipboardText[0] = '\0';
+        s3eClipboardGetText(g_ClipboardText, size);
     }
     
-    inline void ColorTransformYCbCrToRGB(uint8_t* pixel) {
-  int y  = pixel[0];
-  int cb = pixel[1];
-  int cr = pixel[2];
-  pixel[0] = kRangeLimit[y + kCrToRedTable[cr]];
-  pixel[1] = kRangeLimit[y +
-                         ((kCrToGreenTable[cr] + kCbToGreenTable[cb]) >> 16)];
-  pixel[2] = kRangeLimit[y + kCbToBlueTable[cb]];
+        // We are using the OpenGL fixed pipeline to make the example code simpler to read!
+    // Setup render state: alpha-blending enabled, no face culling, no depth testing, scissor enabled, vertex/texcoord/color pointers, polygon fill.
+    GLint last_texture; glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
+    GLint last_polygon_mode[2]; glGetIntegerv(GL_POLYGON_MODE, last_polygon_mode);
+    GLint last_viewport[4]; glGetIntegerv(GL_VIEWPORT, last_viewport);
+    GLint last_scissor_box[4]; glGetIntegerv(GL_SCISSOR_BOX, last_scissor_box); 
+    glPushAttrib(GL_ENABLE_BIT | GL_COLOR_BUFFER_BIT | GL_TRANSFORM_BIT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_LIGHTING);
+    glDisable(GL_COLOR_MATERIAL);
+    glEnable(GL_SCISSOR_TEST);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+    glEnable(GL_TEXTURE_2D);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
+    
+        const GLchar* vertex_shader_glsl_300_es =
+        'precision mediump float;\n'
+        'layout (location = 0) in vec2 Position;\n'
+        'layout (location = 1) in vec2 UV;\n'
+        'layout (location = 2) in vec4 Color;\n'
+        'uniform mat4 ProjMtx;\n'
+        'out vec2 Frag_UV;\n'
+        'out vec4 Frag_Color;\n'
+        'void main()\n'
+        '{\n'
+        '    Frag_UV = UV;\n'
+        '    Frag_Color = Color;\n'
+        '    gl_Position = ProjMtx * vec4(Position.xy,0,1);\n'
+        '}\n';
+    
+        // Render text label (with clipping + alpha gradient) + unsaved marker
+    const char* TAB_UNSAVED_MARKER = '*';
+    ImRect text_pixel_clip_bb(bb.Min.x + style.FramePadding.x, bb.Min.y + style.FramePadding.y, bb.Max.x - style.FramePadding.x, bb.Max.y);
+    if (flags & ImGuiTabItemFlags_UnsavedDocument)
+    {
+        text_pixel_clip_bb.Max.x -= CalcTextSize(TAB_UNSAVED_MARKER, NULL, false).x;
+        ImVec2 unsaved_marker_pos(ImMin(bb.Min.x + style.FramePadding.x + label_size.x + 2, text_pixel_clip_bb.Max.x), bb.Min.y + style.FramePadding.y + (float)(int)(-g.FontSize * 0.25f));
+        RenderTextClippedEx(draw_list, unsaved_marker_pos, bb.Max - style.FramePadding, TAB_UNSAVED_MARKER, NULL, NULL);
+    }
+    ImRect text_ellipsis_clip_bb = text_pixel_clip_bb;
+    
+    
+    {    state.SetBytesProcessed(state.iterations() * j.dump(indent).size());
+}
+BENCHMARK_CAPTURE(Dump, jeopardy / -,      'data/jeopardy/jeopardy.json',                 -1);
+BENCHMARK_CAPTURE(Dump, jeopardy / 4,      'data/jeopardy/jeopardy.json',                 4);
+BENCHMARK_CAPTURE(Dump, canada / -,        'data/nativejson-benchmark/canada.json',       -1);
+BENCHMARK_CAPTURE(Dump, canada / 4,        'data/nativejson-benchmark/canada.json',       4);
+BENCHMARK_CAPTURE(Dump, citm_catalog / -,  'data/nativejson-benchmark/citm_catalog.json', -1);
+BENCHMARK_CAPTURE(Dump, citm_catalog / 4,  'data/nativejson-benchmark/citm_catalog.json', 4);
+BENCHMARK_CAPTURE(Dump, twitter / -,       'data/nativejson-benchmark/twitter.json',      -1);
+BENCHMARK_CAPTURE(Dump, twitter / 4,       'data/nativejson-benchmark/twitter.json',      4);
+BENCHMARK_CAPTURE(Dump, floats / -,        'data/numbers/floats.json',                    -1);
+BENCHMARK_CAPTURE(Dump, floats / 4,        'data/numbers/floats.json',                    4);
+BENCHMARK_CAPTURE(Dump, signed_ints / -,   'data/numbers/signed_ints.json',               -1);
+BENCHMARK_CAPTURE(Dump, signed_ints / 4,   'data/numbers/signed_ints.json',               4);
+BENCHMARK_CAPTURE(Dump, unsigned_ints / -, 'data/numbers/unsigned_ints.json',             -1);
+BENCHMARK_CAPTURE(Dump, unsigned_ints / 4, 'data/numbers/unsigned_ints.json',             4);
+    
+    void State::SetIterationTime(double seconds) {
+  timer_->SetIterationTime(seconds);
 }
     
-      // Returns the combined score of the output image in the last Compare() call
-  // (or the baseline image, if Compare() was not called yet), based on output
-  // size and the similarity metric.
-  virtual double ScoreOutputSize(int size) const = 0;
     
-    void PrintDebug(ProcessStats* stats, std::string s);
-    
-    
-    {}  // namespace guetzli
-
-    
-      tmp0 = in[3 * stride];
-  tmp1 = kIDCTMatrix[ 3] * tmp0;
-  tmp2 = kIDCTMatrix[11] * tmp0;
-  tmp3 = kIDCTMatrix[19] * tmp0;
-  tmp4 = kIDCTMatrix[27] * tmp0;
-  out[0] += tmp1;
-  out[1] += tmp2;
-  out[2] += tmp3;
-  out[3] += tmp4;
-  out[4] -= tmp4;
-  out[5] -= tmp3;
-  out[6] -= tmp2;
-  out[7] -= tmp1;
-    
-    // Decodes the parsed jpeg coefficients into an RGB image.
-// There can be only either 1 or 3 image components, in either case, an RGB
-// output image will be generated.
-// Only YUV420 and YUV444 sampling factors are supported.
-// Vector will be empty if a decoding error occurred.
-std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg);
-    
-    // Output callback function with associated data.
-struct JPEGOutput {
-  JPEGOutput(JPEGOutputHook cb, void* data) : cb(cb), data(data) {}
-  bool Write(const uint8_t* buf, size_t len) const {
-    return (len == 0) || (cb(data, buf, len) == len);
+    {    // Compute all possible fitting curves and stick to the best one
+    for (const auto& fit : fit_curves) {
+      LeastSq current_fit = MinimalLeastSq(n, time, FittingCurve(fit));
+      if (current_fit.rms < best_fit.rms) {
+        best_fit = current_fit;
+        best_fit.complexity = fit;
+      }
+    }
+  } else {
+    best_fit = MinimalLeastSq(n, time, FittingCurve(complexity));
+    best_fit.complexity = complexity;
   }
- private:
-  JPEGOutputHook cb;
-  void* data;
-};
+    
+    double Finish(Counter const& c, double cpu_time, double num_threads) {
+  double v = c.value;
+  if (c.flags & Counter::kIsRate) {
+    v /= cpu_time;
+  }
+  if (c.flags & Counter::kAvgThreads) {
+    v /= num_threads;
+  }
+  return v;
+}
