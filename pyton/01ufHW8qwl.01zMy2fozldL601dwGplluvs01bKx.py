75c8d3bb281fa803f1a2e9ada5a3aeba59c9e1be
SUNsung <weatherius@gmail.com>
2018-12-24 03:10:28 +0200

    Rewrite [01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py] File in: 2018-12-24 03:10:27

--- pyton/01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py
+++ pyton/01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py
@@ -1,243 +1,120 @@
 
         
-        import pytest
+        
+@pytest.fixture(autouse=True)
+def shell_config(mocker):
+    path_mock = mocker.patch('thefuck.entrypoints.not_configured.Path',
+                             new_callable=MagicMock)
+    return path_mock.return_value \
+        .expanduser.return_value \
+        .open.return_value \
+        .__enter__.return_value
     
-        def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type is None:
-            self.stop_event.wait(self.WAIT_EVENT_TIMEOUT)
-        else:
-            if self.wait_to_close_event:
-                # avoid server from waiting for event timeouts
-                # if an exception is found in the main thread
-                self.wait_to_close_event.set()
+        print()
+    print('Classification performance:')
+    print('===========================')
+    print()
+    print('%s %s %s %s' % ('Classifier  ', 'train-time', 'test-time',
+                           'Accuracy'))
+    print('-' * 44)
+    for name in sorted(accuracy, key=accuracy.get):
+        print('%s %s %s %s' % (name.ljust(16),
+                               ('%.4fs' % train_time[name]).center(10),
+                               ('%.4fs' % test_time[name]).center(10),
+                               ('%.4f' % accuracy[name]).center(10)))
     
-            :param connections: The number of urllib3 connection pools to cache.
-        :param maxsize: The maximum number of connections to save in the pool.
-        :param block: Block when no free connections are available.
-        :param pool_kwargs: Extra keyword arguments used to initialize the Pool Manager.
+    
+DATASET_GENERATORS = {
+    'perturbed_logarithm': generate_perturbed_logarithm_dataset,
+    'logistic': generate_logistic_dataset,
+    'pathological': generate_pathological_dataset,
+}
+    
+                tick_vals += list(xvals + 0.5 * width)
+            tick_labels += ['%i' % val for val in vals]
+    
+    n_samples = np.logspace(.5, 3, 9)
+n_features = np.logspace(1, 3.5, 7)
+N_samples, N_features = np.meshgrid(n_samples,
+                                    n_features)
+scikits_time = np.zeros(N_samples.shape)
+scipy_time = np.zeros(N_samples.shape)
+    
+        print('Dataset statics')
+    print('===========================')
+    print('n_samples \t= %s' % opts.n_samples)
+    print('n_features \t= %s' % opts.n_features)
+    if opts.n_components == 'auto':
+        print('n_components \t= %s (auto)' %
+              johnson_lindenstrauss_min_dim(n_samples=opts.n_samples,
+                                            eps=opts.eps))
+    else:
+        print('n_components \t= %s' % opts.n_components)
+    print('n_elements \t= %s' % (opts.n_features * opts.n_samples))
+    print('n_nonzeros \t= %s per feature' % n_nonzeros)
+    print('ratio_nonzeros \t= %s' % opts.ratio_nonzeros)
+    print('')
+    
+        An example with a long-untouched module that everyone has
+    >>> _linkcode_resolve('py', {'module': 'tty',
+    ...                          'fullname': 'setraw'},
+    ...                   package='tty',
+    ...                   url_fmt='http://hg.python.org/cpython/file/'
+    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
+    ...                   revision='xxxx')
+    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
+    '''
+    
+        print('Decompressing %s' % ARCHIVE_NAME)
+    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
+        archive.extractall(path='.')
+    os.remove(ARCHIVE_NAME)
+
+    
+    
+if __name__ == '__main__':
+    # Create our localizers
+    e, g = get_localizer(language='English'), get_localizer(language='Greek')
+    # Localize some text
+    for msgid in 'dog parrot cat bear'.split():
+        print(e.get(msgid), g.get(msgid))
+    
+    ### OUTPUT ###
+# Jack move 5m then stop
+
+    
+    
+def count_to(count):
+    '''Counts by word numbers, up to a maximum of five'''
+    numbers = ['one', 'two', 'three', 'four', 'five']
+    for number in numbers[:count]:
+        yield number
+    
+        def commit(self):
+        self.states = [memento(target, self.deep) for target in self.targets]
+    
+        def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
         '''
-        # save these values for pickling
-        self._pool_connections = connections
-        self._pool_maxsize = maxsize
-        self._pool_block = block
+            Here the name of the class is used as key but it could be any class
+            parameter.
+        '''
+        cls.REGISTRY[new_cls.__name__] = new_cls
+        return new_cls
     
-    This module handles import compatibility issues between Python 2 and
-Python 3.
+    ### OUTPUT ###
+# Scanning... Station is 1380 AM
+# Scanning... Station is 1510 AM
+# Switching to FM
+# Scanning... Station is 89.1 FM
+# Scanning... Station is 103.9 FM
+# Scanning... Station is 81.3 FM
+# Scanning... Station is 89.1 FM
+# Switching to AM
+# Scanning... Station is 1250 AM
+# Scanning... Station is 1380 AM
+
+    
+    *TL;DR80
+Creates new object instances by cloning prototype.
 '''
-    
-    try:
-    from urllib3.contrib import pyopenssl
-except ImportError:
-    pyopenssl = None
-    OpenSSL = None
-    cryptography = None
-else:
-    import OpenSSL
-    import cryptography
-    
-        def test_basic_response(self):
-        '''the basic response server returns an empty http response'''
-        with Server.basic_response_server() as (host, port):
-            r = requests.get('http://{}:{}'.format(host, port))
-            assert r.status_code == 200
-            assert r.text == u''
-            assert r.headers['Content-Length'] == '0'
-    
-    
-@pytest.mark.parametrize(
-    'url, auth', (
-        (
-            'http://' + ENCODED_USER + ':' + ENCODED_PASSWORD + '@' +
-            'request.com/url.html#test',
-            (USER, PASSWORD)
-        ),
-        (
-            'http://user:pass@complex.url.com/path?query=yes',
-            ('user', 'pass')
-        ),
-        (
-            'http://user:pass%20pass@complex.url.com/path?query=yes',
-            ('user', 'pass pass')
-        ),
-        (
-            'http://user:pass pass@complex.url.com/path?query=yes',
-            ('user', 'pass pass')
-        ),
-        (
-            'http://user%25user:pass@complex.url.com/path?query=yes',
-            ('user%user', 'pass')
-        ),
-        (
-            'http://user:pass%23pass@complex.url.com/path?query=yes',
-            ('user', 'pass#pass')
-        ),
-        (
-            'http://complex.url.com/path?query=yes',
-            ('', '')
-        ),
-    ))
-def test_get_auth_from_url(url, auth):
-    assert get_auth_from_url(url) == auth
-    
-        def test_unicode_method_name_with_request_object(self, httpbin):
-        files = {'file': open(__file__, 'rb')}
-        s = requests.Session()
-        req = requests.Request(u('POST'), httpbin('post'), files=files)
-        prep = s.prepare_request(req)
-        assert isinstance(prep.method, builtin_str)
-        assert prep.method == 'POST'
-    
-    
-def bench_isotonic_regression(Y):
-    '''
-    Runs a single iteration of isotonic regression on the input data,
-    and reports the total time taken (in seconds).
-    '''
-    gc.collect()
-    
-        samples : array-like of ints (1d or 0d)
-        The number of samples to generate as input.
-    
-        ###########################################################################
-    # Numpy permutation based
-    sampling_algorithm['numpy-permutation'] = \
-        lambda n_population, n_sample: \
-            np.random.permutation(n_population)[:n_sample]
-    
-    input data sparsity: 0.050000
-true coef sparsity: 0.000100
-test data sparsity: 0.027400
-model sparsity: 0.000024
-r^2 on test data (dense model) : 0.233651
-r^2 on test data (sparse model) : 0.233651
-Wrote profile results to sparsity_benchmark.py.lprof
-Timer unit: 1e-06 s
-    
-    
-print(':orphan:')
-print()
-heading = 'Available documentation for Scikit-learn'
-print(heading)
-print('=' * len(heading))
-print()
-print('Web-based documentation is available for versions listed below:')
-print()
-    
-    ARCHIVE_NAME = URL.rsplit('/', 1)[1]
-DATA_FOLDER = 'txt_sentoken'
-    
-    The goal of this exercise is to train a linear classifier on text features
-that represent sequences of up to 3 consecutive characters so as to be
-recognize natural languages by using the frequencies of short character
-sequences as 'fingerprints'.
-    
-        # TODO: decoder/encoder should accept cls? Otherwise, subclassing
-    # JSONObjectWithFields is tricky...
-    header_cls = Header
-    header = jose.Field(
-        'header', omitempty=True, default=header_cls(),
-        decoder=header_cls.from_json)
-    
-    REWRITE_HTTPS_ARGS_WITH_END = [
-    '^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[END,NE,R=permanent]']
-'''Apache version >= 2.3.9 rewrite rule arguments used for redirections to
-    https vhost'''
-    
-    from acme.magic_typing import Set # pylint: disable=unused-import, no-name-in-module
-from certbot.plugins import common
-    
-        def test_eq(self):
-        self.assertTrue(self.vhost1b == self.vhost1)
-        self.assertFalse(self.vhost1 == self.vhost2)
-        self.assertEqual(str(self.vhost1b), str(self.vhost1))
-        self.assertFalse(self.vhost1b == 1234)
-    
-    
-def open_text(package: Package,
-              resource: Resource,
-              encoding: str = 'utf-8',
-              errors: str = 'strict') -> TextIO:
-    '''Return a file-like object opened for text reading of the resource.'''
-    resource = _normalize_path(resource)
-    package = _get_package(package)
-    reader = _get_resource_reader(package)
-    if reader is not None:
-        return TextIOWrapper(reader.open_resource(resource), encoding, errors)
-    _check_location(package)
-    absolute_package_path = os.path.abspath(package.__spec__.origin)
-    package_path = os.path.dirname(absolute_package_path)
-    full_path = os.path.join(package_path, resource)
-    try:
-        return open(full_path, mode='r', encoding=encoding, errors=errors)
-    except OSError:
-        # Just assume the loader is a resource loader; all the relevant
-        # importlib.machinery loaders are and an AttributeError for
-        # get_data() will make it clear what is needed from the loader.
-        loader = cast(ResourceLoader, package.__spec__.loader)
-        data = None
-        if hasattr(package.__spec__.loader, 'get_data'):
-            with suppress(OSError):
-                data = loader.get_data(full_path)
-        if data is None:
-            package_name = package.__spec__.name
-            message = '{!r} resource not found in {!r}'.format(
-                resource, package_name)
-            raise FileNotFoundError(message)
-        else:
-            return TextIOWrapper(BytesIO(data), encoding, errors)
-    
-    
-def main():
-    import io
-    import pprint
-    
-    --Pepé
-''')
-    
-    
-def main():
-    parser = ArgumentParser(description='''\
-Send the contents of a directory as a MIME message.
-Unless the -o option is given, the email is sent by forwarding to your local
-SMTP server, which then does the normal delivery process.  Your local machine
-must be running an SMTP server.
-''')
-    parser.add_argument('-d', '--directory',
-                        help='''Mail the contents of the specified directory,
-                        otherwise use the current directory.  Only the regular
-                        files in the directory are sent, and we don't recurse to
-                        subdirectories.''')
-    parser.add_argument('-o', '--output',
-                        metavar='FILE',
-                        help='''Print the composed message to FILE instead of
-                        sending the message to the SMTP server.''')
-    parser.add_argument('-s', '--sender', required=True,
-                        help='The value of the From: header (required)')
-    parser.add_argument('-r', '--recipient', required=True,
-                        action='append', metavar='RECIPIENT',
-                        default=[], dest='recipients',
-                        help='A To: header value (at least one required)')
-    args = parser.parse_args()
-    directory = args.directory
-    if not directory:
-        directory = '.'
-    # Create the message
-    msg = EmailMessage()
-    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
-    msg['To'] = ', '.join(args.recipients)
-    msg['From'] = args.sender
-    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
-    
-        # Tell child processes to stop
-    for i in range(NUMBER_OF_PROCESSES):
-        task_queue.put('STOP')
-    
-    while True:
-    line = input()
-    if line == '':
-        break
-    buffer += line
-    if sqlite3.complete_statement(buffer):
-        try:
-            buffer = buffer.strip()
-            cur.execute(buffer)
