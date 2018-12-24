42b9fb9c8e709a4c56ea10382c34bdcde48bb624
SUNsung <weatherius@gmail.com>
2018-12-24 19:47:32 +0200

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2018-12-24 19:47:32

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,77 +1,97 @@
 
         
-        require 'action_view/helpers/tags/collection_helpers'
+              it 'can be set to multiple the platforms' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            carthage(
+              platform: 'iOS,Mac'
+            )
+          end').runner.execute(:test)
     
-        # Controls whether an action should be rendered using a layout.
-    # If you want to disable any <tt>layout</tt> settings for the
-    # current action so that it is rendered without a layout then
-    # either override this method in your controller to return false
-    # for that action or set the <tt>action_has_layout</tt> attribute
-    # to false before rendering.
-    def action_has_layout?
-      @_action_has_layout
+          it 'works with select regex' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+              select_regex: /AppDelegate/
+            )
+          end').runner.execute(:test)
+    
+          if @custom_emoji.save
+        log_action :create, @custom_emoji
+        redirect_to admin_custom_emojis_path, notice: I18n.t('admin.custom_emojis.created_msg')
+      else
+        render :new
+      end
     end
     
-    module ActionView #:nodoc:
-  # = Action View PathSet
-  #
-  # This class is used to store and access paths in Action View. A number of
-  # operations are defined so that you can search among the paths in this
-  # set and also perform operations on other +PathSet+ objects.
-  #
-  # A +LookupContext+ will use a +PathSet+ to store the paths in its context.
-  class PathSet #:nodoc:
-    include Enumerable
+    class Api::OEmbedController < Api::BaseController
+  respond_to :json
     
-          def extract_details(options) # :doc:
-        @lookup_context.registered_details.each_with_object({}) do |key, details|
-          value = options[key]
+      def create
+    @web_subscription&.destroy!
     
-    # User for the smoke tests
-if ENV['SMOKE'] == '1'
-  UserEmail.seed do |ue|
-    ue.id = 0
-    ue.email = 'smoke_user@discourse.org'
-    ue.primary = true
-    ue.user_id = 0
+        render json: web_subscription, serializer: REST::WebPushSubscriptionSerializer
   end
     
-    module Sidekiq
-  module Extensions
-    ##
-    # Adds 'delay', 'delay_for' and `delay_until` methods to ActionMailer to offload arbitrary email
-    # delivery to Sidekiq.  Example:
+        # use Feedbag as a backup to Google Feeds Api
+    if rss_url.nil?
+      rss_url = Feedbag.find(web_url).first
+      if rss_url.nil?
+        suggested_paths = ['/rss', '/feed', '/feeds', '/atom.xml', '/feed.xml', '/rss.xml', '.atom']
+        suggested_paths.each do |suggested_path|
+          rss_url = Feedbag.find('#{web_url.chomp('/')}#{suggested_path}').first
+          break if rss_url
+        end
+      end
+    end
+  end
+    
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+        # The name of the file in which the exception was raised.
+    # This could be `nil` if no filename is available.
     #
-    #    UserMailer.delay.send_welcome_email(new_user)
-    #    UserMailer.delay_for(5.days).send_welcome_email(new_user)
-    #    UserMailer.delay_until(5.days.from_now).send_welcome_email(new_user)
-    class DelayedMailer
-      include Sidekiq::Worker
-    
-          def invoke(*args)
-        chain = retrieve.dup
-        traverse_chain = lambda do
-          if chain.empty?
-            yield
-          else
-            chain.shift.call(*args, &traverse_chain)
-          end
-        end
-        traverse_chain.call
-      end
+    # @return [String, nil]
+    def sass_filename
+      sass_backtrace.first[:filename]
     end
     
-          arr = Sidekiq.options[:lifecycle_events][event]
-      arr.reverse! if reverse
-      arr.each do |block|
-        begin
-          block.call
-        rescue => ex
-          handle_exception(ex, { context: 'Exception during Sidekiq lifecycle event.', event: event })
-          raise ex if reraise
+          def lines
+        lines = []
+        @diff.diff.split('\n')[2..-1].each_with_index do |line, line_index|
+          lines << { :line  => line,
+                     :class => line_class(line),
+                     :ldln  => left_diff_line_number(0, line),
+                     :rdln  => right_diff_line_number(0, line) }
+        end if @diff
+        lines
+      end
+    
+          def versions
+        i = @versions.size + 1
+        @versions.map do |v|
+          i -= 1
+          { :id        => v.id,
+            :id7       => v.id[0..6],
+            :num       => i,
+            :selected  => @page.version.id == v.id,
+            :author    => v.author.name.respond_to?(:force_encoding) ? v.author.name.force_encoding('UTF-8') : v.author.name,
+            :message   => v.message.respond_to?(:force_encoding) ? v.message.force_encoding('UTF-8') : v.message,
+            :date      => v.authored_date.strftime('%B %d, %Y'),
+            :gravatar  => Digest::MD5.hexdigest(v.author.email.strip.downcase),
+            :identicon => self._identicon_code(v.author.email),
+            :date_full => v.authored_date,
+          }
         end
       end
-      arr.clear
-    end
-  end
-end
+    
+          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
+      def left_shift(int, shift)
+        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
+        # 1>>31, 2**32
+        (r & 2147483648) == 0 ? r : r - 4294967296
+      end
+    
+    module Gollum
+  VERSION = '4.1.4'
