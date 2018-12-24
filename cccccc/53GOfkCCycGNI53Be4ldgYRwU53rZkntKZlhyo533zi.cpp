8caace8df43868a18aa582ce40e960944dbd96e4
SUNsung <weatherius@gmail.com>
2018-12-25 01:11:49 +0200

    Rewrite [53GOfkCCycGNI53Be4ldgYRwU53rZkntKZlhyo533zi.cpp] File in: 2018-12-25 01:11:48

--- cccccc/53GOfkCCycGNI53Be4ldgYRwU53rZkntKZlhyo533zi.cpp
+++ cccccc/53GOfkCCycGNI53Be4ldgYRwU53rZkntKZlhyo533zi.cpp
@@ -1,122 +1,264 @@
-bool ParseAnyTypeUrl(const string& type_url, string* url_prefix,
-                     string* full_type_name) {
-  size_t pos = type_url.find_last_of('/');
-  if (pos == string::npos || pos + 1 == type_url.size()) {
-    return false;
+
+        
+          // Returns true iff the test part failed.
+  bool failed() const { return type_ != kSuccess; }
+    
+    // Helper function for implementing {EXPECT|ASSERT}_PRED4.  Don't use
+// this in your code.
+template <typename Pred,
+          typename T1,
+          typename T2,
+          typename T3,
+          typename T4>
+AssertionResult AssertPred4Helper(const char* pred_text,
+                                  const char* e1,
+                                  const char* e2,
+                                  const char* e3,
+                                  const char* e4,
+                                  Pred pred,
+                                  const T1& v1,
+                                  const T2& v2,
+                                  const T3& v3,
+                                  const T4& v4) {
+  if (pred(v1, v2, v3, v4)) return AssertionSuccess();
+    }
+    
+    
+    {
+    {    // Next, recurses (at compile time) with the tail of the test list.
+    return TypeParameterizedTestCase<Fixture, typename Tests::Tail, Types>
+        ::Register(prefix, case_name, SkipComma(test_names));
   }
-  if (url_prefix) {
-    *url_prefix = type_url.substr(0, pos + 1);
-  }
-  *full_type_name = type_url.substr(pos + 1);
-  return true;
-}
-    
-    #include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/io/printer.h>
-    
-    const Options* SourceGeneratorBase::options() {
-  return this->options_;
-}
-    
-        if (options_.annotate_code) {
-      std::unique_ptr<io::ZeroCopyOutputStream> info_output(
-          context->Open(info_full_path));
-      annotations.SerializeToZeroCopyStream(info_output.get());
-      annotation_file_list->push_back(info_full_path);
-    }
-    
-    TEST(ObjCHelper, TextFormatDecodeData_RawStrings) {
-  TextFormatDecodeData decode_data;
-    }
-    
-    void OneofGenerator::GenerateClearFunctionDeclaration(io::Printer* printer) {
-  printer->Print(
-      variables_,
-      '/**\n'
-      ' * Clears whatever value was set for the oneof '$name$'.\n'
-      ' **/\n'
-      'void $owning_message_class$_Clear$capitalized_name$OneOfCase($owning_message_class$ *message);\n');
-}
-    
-    #include <cstdint>      // for int32_t
-    
-      // Add all parameters to a list.
-  int v, i;
-  int num_iterations = (tess->params() == nullptr) ? 1 : 2;
-  for (v = 0; v < num_iterations; ++v) {
-    tesseract::ParamsVectors *vec = (v == 0) ? GlobalParams() : tess->params();
-    for (i = 0; i < vec->int_params.size(); ++i) {
-      vc_it.add_after_then_move(new ParamContent(vec->int_params[i]));
-    }
-    for (i = 0; i < vec->bool_params.size(); ++i) {
-      vc_it.add_after_then_move(new ParamContent(vec->bool_params[i]));
-    }
-    for (i = 0; i < vec->string_params.size(); ++i) {
-      vc_it.add_after_then_move(new ParamContent(vec->string_params[i]));
-    }
-    for (i = 0; i < vec->double_params.size(); ++i) {
-      vc_it.add_after_then_move(new ParamContent(vec->double_params[i]));
-    }
-  }
-    
-    // A list of all possible parameter types used.
-enum ParamType {
-  VT_INTEGER,
-  VT_BOOLEAN,
-  VT_STRING,
-  VT_DOUBLE
 };
     
-      /// Return true if we are processing the full image.
-  bool IsFullImage() const {
-    return rect_left_ == 0 && rect_top_ == 0 &&
-           rect_width_ == image_width_ && rect_height_ == image_height_;
+      T* value_;
+  linked_ptr_internal link_;
+    
+    
+// Tests IsPrime()
+    
+    // Clones a 0-terminated C string, allocating memory using new.
+const char* MyString::CloneCString(const char* a_c_string) {
+  if (a_c_string == NULL) return NULL;
+    }
+    
+      // Asserts that s.c_string() returns NULL.
+  //
+  // <TechnicalDetails>
+  //
+  // If we write NULL instead of
+  //
+  //   static_cast<const char *>(NULL)
+  //
+  // in this assertion, it will generate a warning on gcc 3.4.  The
+  // reason is that EXPECT_EQ needs to know the types of its
+  // arguments in order to print them when it fails.  Since NULL is
+  // #defined as 0, the compiler will use the formatter function for
+  // int to print it.  However, gcc thinks that NULL should be used as
+  // a pointer, not an int, and therefore complains.
+  //
+  // The root of the problem is C++'s lack of distinction between the
+  // integer number 0 and the null pointer constant.  Unfortunately,
+  // we have to live with this fact.
+  //
+  // </TechnicalDetails>
+  EXPECT_STREQ(NULL, s.c_string());
+    
+    // specialize define for Layer Parameter
+template<>
+class FieldEntry<caffe::LayerParameter>
+    : public FieldEntryBase<FieldEntry<caffe::LayerParameter>, caffe::LayerParameter> {
+ public:
+  // parent class
+  typedef FieldEntryBase<FieldEntry<caffe::LayerParameter>, caffe::LayerParameter> Parent;
+    }
+    
+    namespace mxnet {
+namespace op {
+template<>
+Operator *CreateOp<cpu>(CaffeLossParam param, int dtype) {
+  Operator *op = NULL;
+  switch (dtype) {
+  case mshadow::kFloat32:
+    op = new CaffeLoss<cpu, float>(param);
+    break;
+  case mshadow::kFloat64:
+    op = new CaffeLoss<cpu, double>(param);
+    break;
+  case mshadow::kFloat16:
+    LOG(FATAL) << 'float16 layer is not supported by caffe';
+    break;
+  default:
+    LOG(FATAL) << 'Unsupported type ' << dtype;
+  }
+  return op;
+}
+    }
+    }
+    
+      std::map<std::string, std::string> GetParams() const override {
+    return param_.__DICT__();
   }
     
-    /**********************************************************************
- * read_unlv_file
- *
- * Read a whole unlv zone file to make a list of blocks.
- **********************************************************************/
+      virtual void BeforeFirst(void) {
+    if (param_.round_batch == 0 || num_overflow_ == 0) {
+      // otherise, we already called before first
+      base_->BeforeFirst();
+    } else {
+      num_overflow_ = 0;
+    }
+    head_ = 1;
+  }
     
-      // Merges the boxes from start to end, not including end, and deletes
-  // the boxes between start and end.
-  void MergeBoxes(int start, int end);
+      // Get initial measurement of the space we will be reading.
+  const int64_t initial_size = Size(Key(0), Key(n));
+  const int64_t initial_other_size = Size(Key(n), Key(kCount));
     
-    	const btDiscreteDynamicsWorld *m_world;
-	btPersistentManifold *m_manifoldPtr;
-	bool m_ownManifold;
-	bool m_isSwapped;
+    TEST(CorruptionTest, CorruptedDescriptor) {
+  ASSERT_OK(db_->Put(WriteOptions(), 'foo', 'hello'));
+  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
+  dbi->TEST_CompactMemTable();
+  dbi->TEST_CompactRange(0, nullptr, nullptr);
+    }
     
-    size_t MemoryPool::total_memory = 0;
-size_t MemoryPool::max_memory = 0;
+      // Compact the in-memory write buffer to disk.  Switches to a new
+  // log-file/memtable and writes a new descriptor iff successful.
+  // Errors are recorded in bg_error_.
+  void CompactMemTable() EXCLUSIVE_LOCKS_REQUIRED(mutex_);
     
     
-    {	return singleton;
+    {}  // namespace leveldb
+    
+    // Print contents of a log file. (*func)() is called on every record.
+Status PrintLogContents(Env* env, const std::string& fname,
+                        void (*func)(uint64_t, Slice, WritableFile*),
+                        WritableFile* dst) {
+  SequentialFile* file;
+  Status s = env->NewSequentialFile(fname, &file);
+  if (!s.ok()) {
+    return s;
+  }
+  CorruptionReporter reporter;
+  reporter.dst_ = dst;
+  log::Reader reader(file, &reporter, true, 0);
+  Slice record;
+  std::string scratch;
+  while (reader.ReadRecord(&record, &scratch)) {
+    (*func)(reader.LastRecordOffset(), record, dst);
+  }
+  delete file;
+  return Status::OK();
 }
     
-    Thread::ID Thread::get_caller_id() {
+    bool HandleDumpCommand(Env* env, char** files, int num) {
+  StdoutPrinter printer;
+  bool ok = true;
+  for (int i = 0; i < num; i++) {
+    Status s = DumpFile(env, files[i], &printer);
+    if (!s.ok()) {
+      fprintf(stderr, '%s\n', s.ToString().c_str());
+      ok = false;
+    }
+  }
+  return ok;
+}
+    
+      // Pad with zeroes to make manifest file very big.
+  {
+    uint64_t len = FileSize(old_manifest);
+    WritableFile* file;
+    ASSERT_OK(env()->NewAppendableFile(old_manifest, &file));
+    std::string zeroes(3*1048576 - static_cast<size_t>(len), 0);
+    ASSERT_OK(file->Append(zeroes));
+    ASSERT_OK(file->Flush());
+    delete file;
+  }
+    
+      Iterator* NewTableIterator(const FileMetaData& meta) {
+    // Same as compaction iterators: if paranoid_checks are on, turn
+    // on checksum verification.
+    ReadOptions r;
+    r.verify_checksums = options_.paranoid_checks;
+    return table_cache_->NewIterator(r, meta.number, meta.file_size);
+  }
+    
+    struct ID3D10Device;
+    
+    // Implemented features:
+//  [X] Renderer: User texture binding. Use 'LPDIRECT3DTEXTURE9' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
+    
+        g_AttribLocationTex = glGetUniformLocation(g_ShaderHandle, 'Texture');
+    g_AttribLocationProjMtx = glGetUniformLocation(g_ShaderHandle, 'ProjMtx');
+    g_AttribLocationPosition = glGetAttribLocation(g_ShaderHandle, 'Position');
+    g_AttribLocationUV = glGetAttribLocation(g_ShaderHandle, 'UV');
+    g_AttribLocationColor = glGetAttribLocation(g_ShaderHandle, 'Color');
+    
+    DHTResponseMessage::DHTResponseMessage(
+    const std::shared_ptr<DHTNode>& localNode,
+    const std::shared_ptr<DHTNode>& remoteNode,
+    const std::string& transactionID)
+    : DHTAbstractMessage(localNode, remoteNode, transactionID)
+{
+}
+    
+      virtual void accept(DHTMessageCallback* callback) = 0;
+    
+      bool addNode(const std::shared_ptr<DHTNode>& node);
+    
+    #include <vector>
+#include <string>
+#include <memory>
+    
+    #endif // D_DHT_SETUP_H
+
+    
+      void setMessageDispatcher(DHTMessageDispatcher* dispatcher);
+    
+    void DHTTaskQueueImpl::executeTask()
+{
+  A2_LOG_DEBUG('Updating periodicTaskQueue1');
+  periodicTaskQueue1_.update();
+  A2_LOG_DEBUG('Updating periodicTaskQueue2');
+  periodicTaskQueue2_.update();
+  A2_LOG_DEBUG('Updating immediateTaskQueue');
+  immediateTaskQueue_.update();
+}
+    
+    int ClusterListStatus600::near(const std::uint8_t* bytes,
+                               int32_t length) const {
+  Byte t0(bytes);
+  int32_t x = t0.get_byte(0, 8);
     }
     
     
-    {			return OK;
-		}
+    {  double ret = x * OBJECT_VREL_RES + OBJECT_VREL_LAT_MIN;
+  return ret;
+}
     
-    
-    {}
-    
-    
-struct Config {
-  /*
-   * Normalizes hdf string names to their ini counterparts
-   *
-   * We have special handling for a few hdf strings such as those containing
-   * MySQL, Eval, IPv[4|6] and EnableHipHopSyntax
-   */
-  static std::string IniName(const Hdf& config,
-                             const bool prepend_hhvm = true);
-  static std::string IniName(const std::string& config,
-                             const bool prepend_hhvm = true);
+    int ObjectListStatus60A::num_of_objects(const std::uint8_t* bytes,
+                                        int32_t length) const {
+  Byte t0(bytes);
+  int32_t x = t0.get_byte(0, 8);
     }
+    
+    void RadarState201::Parse(const std::uint8_t* bytes, int32_t length,
+                          ContiRadar* conti_radar) const {
+  auto state = conti_radar->mutable_radar_state();
+  state->set_max_distance(max_dist(bytes, length));
+  state->set_output_type(output_type(bytes, length));
+  state->set_rcs_threshold(rcs_threshold(bytes, length));
+  state->set_radar_power(radar_power(bytes, length));
+  state->set_send_quality(send_quality(bytes, length));
+  state->set_send_ext_info(send_ext_info(bytes, length));
+}
+    
+    namespace apollo {
+namespace localization {
+namespace msf {
+    }
+    }
+    }
+    
+    double Spline1dSeg::ThirdOrderDerivative(const double x) const {
+  return third_order_derivative_(x);
+}
