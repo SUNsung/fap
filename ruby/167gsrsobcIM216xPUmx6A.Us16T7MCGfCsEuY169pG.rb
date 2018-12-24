b7f463d57ebac199a010f8f7683710264d31ed91
SUNsung <weatherius@gmail.com>
2018-12-24 13:22:49 +0200

    Rewrite [167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb] File in: 2018-12-24 13:22:48

--- ruby/167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb
+++ ruby/167gsrsobcIM216xPUmx6A.Us16T7MCGfCsEuY169pG.rb
@@ -1,172 +1,47 @@
 
         
-        #############################################################################
-#
-# Standard tasks
-#
-#############################################################################
-    
-    def native_relative
-  DOC_PATH.sub('#{COL_PATH}/', '')
-end
-    
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
-    
-          def background_name(keyword, name, source_line, indent)
-        print_feature_element_name(
-          keyword, name, source_line, indent
-        )
-      end
-    
-              theme.create!
-          Jekyll.logger.info 'Your new Jekyll theme, #{theme.name.cyan},' \
-                             ' is ready for you in #{theme.path.to_s.cyan}!'
-          Jekyll.logger.info 'For help getting started, read #{theme.path}/README.md.'
-        end
-        # rubocop:enable Metrics/AbcSize
-      end
-    end
-  end
-end
-
-    
-            def connect(websocket, handshake)
-          @connections_count += 1
-          if @connections_count == 1
-            message = 'Browser connected'
-            message += ' over SSL/TLS' if handshake.secure?
-            Jekyll.logger.info 'LiveReload:', message
+                  def placeholder_required?(html_options)
+            # See https://html.spec.whatwg.org/multipage/forms.html#attr-select-required
+            html_options['required'] && !html_options['multiple'] && html_options.fetch('size', 1).to_i == 1
           end
-          websocket.send(
-            JSON.dump(
-              :command    => 'hello',
-              :protocols  => ['http://livereload.com/protocols/official-7'],
-              :serverName => 'jekyll'
-            )
-          )
     
-            # config[kramdown][syntax_higlighter] >
-        #   config[kramdown][enable_coderay] >
-        #   config[highlighter]
-        # Where `enable_coderay` is now deprecated because Kramdown
-        # supports Rouge now too.
-        def highlighter
-          return @highlighter if @highlighter
+                  time = Time.current
     
-      def std?
-    @settings.include? :std
-  end
-    
-      private
-    
-            next unless path.file?
-        file = path
-    
-        checks.inject_dump_stats! if ARGV.switch? 'D'
-    
-      def select_formula(key)
-    @hash.fetch(key, [])
-  end
-    
-      def self.path(name)
-    Formulary.core_path(name)
-  end
-    
-        def set_filter_params
-      @filter_params = filter_params.to_hash.symbolize_keys
+        initializer 'action_view.embed_authenticity_token_in_remote_forms' do |app|
+      ActiveSupport.on_load(:action_view) do
+        ActionView::Helpers::FormTagHelper.embed_authenticity_token_in_remote_forms =
+          app.config.action_view.delete(:embed_authenticity_token_in_remote_forms)
+      end
     end
     
-        def destroy
-      authorize @report_note, :destroy?
-      @report_note.destroy!
-      redirect_to admin_report_path(@report_note.report_id), notice: I18n.t('admin.report_notes.destroyed_msg')
+          def instrument(name, **options) # :doc:
+        options[:identifier] ||= (@template && @template.identifier) || @path
+    
+        # Main render entry point shared by Action View and Action Controller.
+    def render(context, options)
+      if options.key?(:partial)
+        render_partial(context, options)
+      else
+        render_template(context, options)
+      end
     end
     
-    class Api::SalmonController < Api::BaseController
-  include SignatureVerification
+    class SinatraStaticServer < Sinatra::Base
     
-        $Kernel_trace_var_global = nil
-    $Kernel_trace_var_extra  = nil
+        def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
+    
+      # Extracts raw content DIV from template, used for page description as {{ content }}
+  # contains complete sub-template code on main page level
+  def raw_content(input)
+    /<div class='entry-content'>(?<content>[\s\S]*?)<\/div>\s*<(footer|\/article)>/ =~ input
+    return (content.nil?) ? input : content
   end
     
-      # The global load paths for Sass files. This is meant for plugins and
-  # libraries to register the paths to their Sass stylesheets to that they may
-  # be `@imported`. This load path is used by every instance of {Sass::Engine}.
-  # They are lower-precedence than any load paths passed in via the
-  # {file:SASS_REFERENCE.md#load_paths-option `:load_paths` option}.
-  #
-  # If the `SASS_PATH` environment variable is set,
-  # the initial value of `load_paths` will be initialized based on that.
-  # The variable should be a colon-separated list of path names
-  # (semicolon-separated on Windows).
-  #
-  # Note that files on the global load path are never compiled to CSS
-  # themselves, even if they aren't partials. They exist only to be imported.
-  #
-  # @example
-  #   Sass.load_paths << File.dirname(__FILE__ + '/sass')
-  # @return [Array<String, Pathname, Sass::Importers::Base>]
-  def self.load_paths
-    @load_paths ||= if ENV['SASS_PATH']
-                      ENV['SASS_PATH'].split(Sass::Util.windows? ? ';' : ':')
-                    else
-                      []
-                    end
-  end
-    
-        # Return the first {Sass::Selector::Simple} in a {Sass::Tree::RuleNode},
-    # unless the rule begins with a combinator.
-    #
-    # @param rule [Sass::Tree::RuleNode]
-    # @return [Sass::Selector::Simple?]
-    def first_simple_sel(rule)
-      sseq = first_sseq(rule)
-      return unless sseq.is_a?(Sass::Selector::SimpleSequence)
-      sseq.members.first
-    end
-  end
-end
-
-    
-          Sass::Plugin.on_template_modified do |template|
-        puts '>>> Change detected to: #{template}'
-        STDOUT.flush
-      end
-      Sass::Plugin.on_template_created do |template|
-        puts '>>> New template detected: #{template}'
-        STDOUT.flush
-      end
-      Sass::Plugin.on_template_deleted do |template|
-        puts '>>> Deleted template detected: #{template}'
-        STDOUT.flush
-      end
-    
-      class VagrantSSHCommandError < RuntimeError; end
-    
-          class Properties
-        def initialize
-          @properties = {}
-        end
-    
-          # Returns the iteration variable of the `for` loop.
-      #
-      # @return [Node] The iteration variable of the `for` loop
-      def variable
-        node_parts[0]
-      end
-    
-          # Returns the keyword of the `if` statement as a string. Returns an empty
-      # string for ternary operators.
-      #
-      # @return [String] the keyword of the `if` statement
-      def keyword
-        ternary? ? '' : loc.keyword.source
-      end
+      class VideoTag < Liquid::Tag
+    @video = nil
+    @poster = ''
+    @height = ''
+    @width = ''
