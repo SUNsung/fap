6a2bb12cf4a5482e353b68f8e99a8bd13d7cd6d8
SUNsung <weatherius@gmail.com>
2018-12-26 01:19:27 +0200

    Rewrite [24FByMm0NbFuk24Mz..736SUU24TnYMlEDMgnQ24UTQ.cpp] File in: 2018-12-26 01:19:26

--- cccccc/24FByMm0NbFuk24Mz..736SUU24TnYMlEDMgnQ24UTQ.cpp
+++ cccccc/24FByMm0NbFuk24Mz..736SUU24TnYMlEDMgnQ24UTQ.cpp
@@ -1,304 +1,119 @@
 
-    {}  // namespace
+        
+        grpc_error* CensusClientCallData::Init(grpc_call_element* elem,
+                                       const grpc_call_element_args* args) {
+  path_ = grpc_slice_ref_internal(args->path);
+  start_time_ = absl::Now();
+  method_ = GetMethod(&path_);
+  qualified_method_ = absl::StrCat('Sent.', method_);
+  GRPC_CLOSURE_INIT(&on_done_recv_message_, OnDoneRecvMessageCb, elem,
+                    grpc_schedule_on_exec_ctx);
+  GRPC_CLOSURE_INIT(&on_done_recv_trailing_metadata_,
+                    OnDoneRecvTrailingMetadataCb, elem,
+                    grpc_schedule_on_exec_ctx);
+  return GRPC_ERROR_NONE;
+}
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    #include <grpc/support/port_platform.h>
     
-    // Register the bfloat16 numpy type.
-void RegisterNumpyBfloat16();
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    namespace tensorflow {
+    namespace grpc {
     }
     
-    #include <map>
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    {}  // namespace grpc
     
+    void ProtoServerReflection::FillErrorResponse(const Status& status,
+                                              ErrorResponse* error_response) {
+  error_response->set_error_code(status.error_code());
+  error_response->set_error_message(status.error_message());
+}
     
-    {  tensorflow::DeviceNameUtils::ParsedName parsed_name;
-  if (!tensorflow::DeviceNameUtils::ParseFullName(node_def.device(),
-                                                  &parsed_name)) {
-    LOG(WARNING) << 'Failed to parse device from node_def: '
-                 << node_def.ShortDebugString();
-    return '';
+    namespace grpc {
+namespace reflection {
+    }
+    }
+    
+    #ifndef GRPC_CUSTOM_DEFAULT_THREAD_POOL
+    
+    namespace grpc {
+    }
+    
+    #ifndef GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+#define GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+    
+    DHTResponseMessage::DHTResponseMessage(
+    const std::shared_ptr<DHTNode>& localNode,
+    const std::shared_ptr<DHTNode>& remoteNode,
+    const std::string& transactionID)
+    : DHTAbstractMessage(localNode, remoteNode, transactionID)
+{
+}
+    
+    void DHTRoutingTable::getBuckets(
+    std::vector<std::shared_ptr<DHTBucket>>& buckets) const
+{
+  dht::enumerateBucket(buckets, root_.get());
+}
+    
+    DHTTaskQueueImpl::~DHTTaskQueueImpl() = default;
+    
+    public:
+  DHTTaskQueueImpl();
+    
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port,
+                                           const unsigned char* secret) const
+{
+  unsigned char src[DHT_ID_LENGTH + COMPACT_LEN_IPV6 + SECRET_SIZE];
+  memset(src, 0, sizeof(src));
+  int compactlen = bittorrent::packcompact(src + DHT_ID_LENGTH, ipaddr, port);
+  if (compactlen == 0) {
+    throw DL_ABORT_EX(fmt('Token generation failed: ipaddr=%s, port=%u',
+                          ipaddr.c_str(), port));
   }
-  string class_name = '';
-  tensorflow::FindKernelDef(tensorflow::DeviceType(parsed_name.type.c_str()),
-                            node_def, nullptr /* kernel_def */, &class_name)
-      .IgnoreError();
-  return class_name;
-}
-    
-      // Polls the CUDA platform for the event's current status.
-  Event::Status PollForStatus();
-    
-    // static
-void AutoUpdater::BuildPrototype(v8::Isolate* isolate,
-                                 v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, 'AutoUpdater'));
-  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
-      .SetMethod('checkForUpdates', &auto_updater::AutoUpdater::CheckForUpdates)
-      .SetMethod('getFeedURL', &auto_updater::AutoUpdater::GetFeedURL)
-      .SetMethod('setFeedURL', &AutoUpdater::SetFeedURL)
-      .SetMethod('quitAndInstall', &AutoUpdater::QuitAndInstall);
-}
-    
-      // Delegate implementations.
-  void OnError(const std::string& error) override;
-  void OnError(const std::string& message,
-               const int code,
-               const std::string& domain) override;
-  void OnCheckingForUpdate() override;
-  void OnUpdateAvailable() override;
-  void OnUpdateNotAvailable() override;
-  void OnUpdateDownloaded(const std::string& release_notes,
-                          const std::string& release_name,
-                          const base::Time& release_date,
-                          const std::string& update_url) override;
-    
-    void BrowserWindow::UpdateDraggableRegions(
-    content::RenderFrameHost* rfh,
-    const std::vector<DraggableRegion>& regions) {
-  if (window_->has_frame())
-    return;
-  static_cast<NativeWindowViews*>(window_.get())
-      ->UpdateDraggableRegions(DraggableRegionsToSkRegion(regions));
+  memcpy(src, infoHash, DHT_ID_LENGTH);
+  memcpy(src + DHT_ID_LENGTH + COMPACT_LEN_IPV6, secret, SECRET_SIZE);
+  unsigned char md[20];
+  message_digest::digest(md, sizeof(md), MessageDigest::sha1().get(), src,
+                         sizeof(src));
+  return std::string(&md[0], &md[sizeof(md)]);
 }
     
     
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(InAppPurchase);
-};
+    {} // namespace aria2
     
-      // base::PowerObserver implementations:
-  void OnPowerStateChange(bool on_battery_power) override;
-  void OnSuspend() override;
-  void OnResume() override;
-    
-    void Screen::OnDisplayMetricsChanged(const display::Display& display,
-                                     uint32_t changed_metrics) {
-  Emit('display-metrics-changed', display, MetricsToArray(changed_metrics));
-}
-    
-    
-    {  content::WebContents* web_contents_;  // weak
-  SavePageCallback callback_;
-};
-    
-    
-    {}  // namespace atom
-    
-    	if ( PathFileExists(cpuCfgPath) || use_user_cfg == false ) // config/ConEmu-%COMPUTERNAME%.xml file exists or /m was specified on command line, use machine specific config.
-	{
-		if (cfgRoot.length() == 0) // '/c [path]' was NOT specified
-		{
-			if (PathFileExists(cfgPath)) // vendor/conemu-maximus5/ConEmu.xml file exists, copy vendor/conemu-maximus5/ConEmu.xml to config/ConEmu-%COMPUTERNAME%.xml.
-			{
-				if (!CopyFile(cfgPath, cpuCfgPath, FALSE))
-				{
-					MessageBox(NULL,
-						(GetLastError() == ERROR_ACCESS_DENIED)
-						? L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/ConEmu-%COMPUTERNAME%.xml! Access Denied.'
-						: L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/ConEmu-%COMPUTERNAME%.xml!', MB_TITLE, MB_ICONSTOP);
-					exit(1);
-				}
-			}
-			else // vendor/conemu-maximus5/ConEmu.xml config file does not exist, copy config/ConEmu-%COMPUTERNAME%.xml to vendor/conemu-maximus5/ConEmu.xml file
-			{
-				if (!CopyFile(cpuCfgPath, cfgPath, FALSE))
-				{
-					MessageBox(NULL,
-						(GetLastError() == ERROR_ACCESS_DENIED)
-						? L'Failed to copy conig/ConEmu-%COMPUTERNAME%.xml file to vendor/conemu-maximus5/ConEmu.xml! Access Denied.'
-						: L'Failed to copy config/ConEmu-%COMPUTERNAME%.xml file to vendor/conemu-maximus5/ConEmu.xml!', MB_TITLE, MB_ICONSTOP);
-					exit(1);
-				}
-			}
-		}
-		else // '/c [path]' was specified, don't copy anything and use existing conemu-%COMPUTERNAME%.xml to start comemu.
-		{
-			if (use_user_cfg == false && PathFileExists(cfgPath) && !PathFileExists(cpuCfgPath)) // vendor/conemu-maximus5/ConEmu.xml file exists, copy vendor/conemu-maximus5/ConEmu.xml to config/ConEmu-%COMPUTERNAME%.xml.
-			{
-				if (!CopyFile(cfgPath, cpuCfgPath, FALSE))
-				{
-					MessageBox(NULL,
-						(GetLastError() == ERROR_ACCESS_DENIED)
-						? L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/ConEmu-%COMPUTERNAME%.xml! Access Denied.'
-						: L'Failed to copy vendor/conemu-maximus5/ConEmu.xml file to config/ConEmu-%COMPUTERNAME%.xml!', MB_TITLE, MB_ICONSTOP);
-					exit(1);
-				}
-			}
-    }
+      static void ThreadBody(void* v) {
+    ThreadState* thread = reinterpret_cast<ThreadState*>(v);
+    SharedState* shared = thread->shared;
     }
     
-    /*! \brief Cuda runtime compile module. */
-class CudaModule {
- private:
-  /*! \brief Structure for holding internal info. */
-  struct Chunk {
-    /*!
-     * \brief Constructs cuda module.
-     * \param source cuda source code.
-     * \param exports export symbols before mangling.
-     */
-    Chunk(const char* source,
-          const std::vector<std::string>& options,
-          const std::vector<std::string>& exports);
-    /*! \brief deconstrutor */
-    ~Chunk();
-    /*!
-     * \brief Get handle to cuda kernel from loaded module
-     * \param mangled_name mangled kernel name
-     * \param ctx context to run kernel on
-     * \return loaded function handle
-     */
-    CUfunction GetFunction(const std::string& mangled_name, const Context& ctx);
-    /*! \brief nvrtc program handle. */
-    nvrtcProgram prog_;
-    /*! \brief compiled cuda PTX */
-    char* ptx_;
-    /*! \brief lazily loaded cuda module */
-    std::unordered_map<int, CUmodule> mod_;
-    /*! \brief exported names */
-    std::unordered_set<std::string> exports_;
+      // Doesn't include records skipped because of
+  // CompactionFilter::Decision::kRemoveAndSkipUntil.
+  int64_t num_record_drop_user = 0;
+    
+      // Will be called while on the write thread before the write executes.  If
+  // this function returns a non-OK status, the write will be aborted and this
+  // status will be returned to the caller of DB::Write().
+  virtual Status Callback(DB* db) = 0;
+    
+      delay_token_2.reset();
+  // 1000 used, 8240 left
+  ASSERT_EQ(static_cast<uint64_t>(0), controller.GetDelay(&env, 1000u));
+    
+      // number of records being replaced by newer record associated with same key.
+  // this could be a new value or a deletion entry for that key so this field
+  // sums up all updated and deleted keys
+  uint64_t num_records_replaced;
+    
+      // the thread pool id
+  uint64_t thread_pool_id;
+    
+      // The type used to refer to a thread state.
+  // A state describes lower-level action of a thread
+  // such as reading / writing a file or waiting for a mutex.
+  enum StateType : int {
+    STATE_UNKNOWN = 0,
+    STATE_MUTEX_WAIT = 1,
+    NUM_STATE_TYPES
   };
-  /*! \brief pointer to Chunk */
-  std::shared_ptr<Chunk> ptr_;
-    }
-    
-    
-    {  DMLC_DECLARE_PARAMETER(CaffeLossParam) {
-    DMLC_DECLARE_FIELD(prototxt).set_default('layer{}')
-    .describe('Caffe's layer parameter');
-    DMLC_DECLARE_FIELD(num_data).set_range(0, 100).set_default(2)
-    .describe('Operator input number');
-    DMLC_DECLARE_FIELD(num_out).set_range(0, 100).set_default(1)
-    .describe('Operator output number');
-    DMLC_DECLARE_FIELD(grad_scale)
-    .set_default(1.0f)
-    .describe('Scale the gradient by a float factor (a.k.a weight of this loss).');
-  }
-};
-    
-    namespace leveldb {
-    }
-    
-    namespace leveldb {
-    }
-    
-    std::string InfoLogFileName(const std::string& dbname) {
-  return dbname + '/LOG';
-}
-    
-    class StdoutPrinter : public WritableFile {
- public:
-  virtual Status Append(const Slice& data) {
-    fwrite(data.data(), 1, data.size(), stdout);
-    return Status::OK();
-  }
-  virtual Status Close() { return Status::OK(); }
-  virtual Status Flush() { return Status::OK(); }
-  virtual Status Sync() { return Status::OK(); }
-};
-    
-    enum RecordType {
-  // Zero is reserved for preallocated files
-  kZeroType = 0,
-    }
-    
-          case kEof:
-        if (in_fragmented_record) {
-          // This can be caused by the writer dying immediately after
-          // writing a physical record but before completing the next; don't
-          // treat it as a corruption, just ignore the entire logical record.
-          scratch->clear();
-        }
-        return false;
-    
-    size_t LogTest::initial_offset_record_sizes_[] =
-    {10000,  // Two sizable records in first block
-     10000,
-     2 * log::kBlockSize - 1000,  // Span three blocks
-     1,
-     13716,  // Consume all but two bytes of block 3.
-     log::kBlockSize - kHeaderSize, // Consume the entirety of block 4.
-    };
-    
-    template<typename Key, class Comparator>
-inline void SkipList<Key,Comparator>::Iterator::Next() {
-  assert(Valid());
-  node_ = node_->Next(0);
-}
-    
-      // Removes a SnapshotImpl from this list.
-  //
-  // The snapshot must have been created by calling New() on this list.
-  //
-  // The snapshot pointer should not be const, because its memory is
-  // deallocated. However, that would force us to change DB::ReleaseSnapshot(),
-  // which is in the API, and currently takes a const Snapshot.
-  void Delete(const SnapshotImpl* snapshot) {
-#if !defined(NDEBUG)
-    assert(snapshot->list_ == this);
-#endif  // !defined(NDEBUG)
-    snapshot->prev_->next_ = snapshot->next_;
-    snapshot->next_->prev_ = snapshot->prev_;
-    delete snapshot;
-  }
-    
-    #include 'gtest/gtest.h'
-    
-    TEST(ByteTest, ByteToString) {
-  unsigned char value = 0x34;
-  EXPECT_EQ('34', Byte::byte_to_hex(value));
-  EXPECT_EQ('00110100', Byte::byte_to_binary(value));
-  uint32_t int_value = 0xE13A;
-  EXPECT_EQ('E13A', Byte::byte_to_hex(int_value));
-}
-    
-    // System gflags
-DEFINE_string(sensor_node_name, '', 'Sensor node name.');
-
-    
-    int ClusterListStatus600::far(const std::uint8_t* bytes, int32_t length) const {
-  Byte t0(bytes + 1);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-    
-    {  double ret = x * OBJECT_DIST_RES + OBJECT_DIST_LONG_MIN;
-  return ret;
-}
-    
-    #include 'gtest/gtest.h'
-    
-    double STPoint::t() const { return x_; }
-    
-    BackupTrajectoryGenerator::BackupTrajectoryGenerator(
-    const State& init_s, const State& init_d,
-    const double init_relative_time,
-    const std::shared_ptr<CollisionChecker>& ptr_collision_checker,
-    const Trajectory1dGenerator* trajectory1d_generator)
-    : init_relative_time_(init_relative_time),
-      ptr_collision_checker_(ptr_collision_checker),
-      ptr_trajectory1d_generator_(trajectory1d_generator) {
-  GenerateTrajectory1dPairs(init_s, init_d);
-}
-    
-    void Spline1dSeg::SetSplineFunc(const PolynomialXd& spline_func) {
-  spline_func_ = spline_func;
-  derivative_ = PolynomialXd::DerivedFrom(spline_func_);
-  second_order_derivative_ = PolynomialXd::DerivedFrom(derivative_);
-  third_order_derivative_ = PolynomialXd::DerivedFrom(second_order_derivative_);
-}
