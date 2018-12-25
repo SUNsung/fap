bffe536de5819692c451b6f566d445e5effd23a7
SUNsung <weatherius@gmail.com>
2018-12-25 07:03:46 +0200

    Rewrite [54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb] File in: 2018-12-25 07:03:45

--- ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
+++ ruby/54m9P9woVKAxQ54ZipMQ4u6P5k54QIlBuGgIVkA540oC.rb
@@ -1,223 +1,87 @@
 
         
-        module Gitlab
-  module Ci
-    module Pipeline
-      # Class for preloading data associated with pipelines such as commit
-      # authors.
-      class Preloader
-        def self.preload!(pipelines)
-          ##
-          # This preloads all commits at once, because `Ci::Pipeline#commit` is
-          # using a lazy batch loading, what results in only one batched Gitaly
-          # call.
-          #
-          pipelines.each(&:commit)
+            odie 'Unknown command: #{cmd}' unless path
+    puts path
+  end
+end
+
     
-              lfs_objects.each do |object|
-            yield object
-          end
-        rescue StandardError => e
-          Rails.logger.error('The Lfs import process failed. #{e.message}')
-        end
+      it 'returns the previous seed value' do
+    srand(10)
+    srand(20).should == 10
+  end
+    
+          File.open('bower.json', 'w') do |f|
+        f.puts JSON.pretty_generate(spec)
       end
     end
   end
 end
 
     
-          # Returns the identifier to use for cache keys.
-      #
-      # For issues and pull requests this will be 'Issue' or 'MergeRequest'
-      # respectively. For diff notes this will return 'MergeRequest', for
-      # regular notes it will either return 'Issue' or 'MergeRequest' depending
-      # on what type of object the note belongs to.
-      def cache_key_type
-        if object.respond_to?(:issuable_type)
-          object.issuable_type
-        elsif object.respond_to?(:noteable_type)
-          object.noteable_type
+          def bad_cookies
+        @bad_cookies ||= []
+      end
+    
+          def call(env)
+        request               = Request.new(env)
+        status, headers, body = app.call(env)
+    
+        headers = get('/', {}, 'wants' => 'text/html').headers
+    expect(headers['Content-Security-Policy']).to be_nil
+    expect(headers['Content-Security-Policy-Report-Only']).to eq('connect-src 'self'; default-src none; img-src 'self'; report-uri /my_amazing_csp_report_parser; script-src 'self'; style-src 'self'')
+  end
+    
+    require_relative '../lib/bootstrap/environment'
+    
+              it 'successfully install the plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install #{gem_path_on_vagrant}')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-dns')
+          end
+        end
+    
+          def next_link
+        label = 'Next &raquo;'
+        if @versions.size == Gollum::Page.per_page
+          link = '/history/#{@page.name}?page=#{@page_num+1}'
+          %(<a href='#{link}' hotkey='l'>#{label}</a>)
         else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
+          %(<span class='disabled'>#{label}</span>)
         end
-      end
-    
-            # Executes a command on the remote machine with administrative
-        # privileges. See {#execute} for documentation, as the API is the
-        # same.
-        #
-        # @see #execute
-        def sudo(command, opts=nil)
-        end
-    
-            # Helper method that will set a value if a value is given, or otherwise
-        # return the already set value.
-        #
-        # @param [Symbol] key Key for the data
-        # @param [Object] value Value to store.
-        # @return [Object] Stored value.
-        def self.get_or_set(key, value=UNSET_VALUE)
-          # If no value is to be set, then return the value we have already set
-          return data[key] if value.eql?(UNSET_VALUE)
-    
-            # This contains all the guests and their parents.
-        #
-        # @return [Registry<Symbol, Array<Class, Symbol>>]
-        attr_reader :guests
-    
-              @registered.each do |plugin|
-            plugin.components.host_capabilities.each do |host, caps|
-              results[host].merge!(caps)
-            end
-          end
-    
-      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
-    @logger     = Logger.new
-    @repo       = repo
-    @branch     = branch || 'master'
-    @branch_sha = get_branch_sha
-    @cache_path = cache_path
-    @repo_url   = 'https://github.com/#@repo'
-    @save_to    = {
-        js:    'assets/javascripts/bootstrap',
-        scss:  'assets/stylesheets/bootstrap',
-        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
-  end
-    
-          * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-      * Redistributions in binary form must reproduce the above
-        copyright notice, this list of conditions and the following
-        disclaimer in the documentation and/or other materials provided
-        with the distribution.
-      * Neither the name of Google Inc. nor the names of its
-        contributors may be used to endorse or promote products derived
-        from this software without specific prior written permission.
-    
-          def default_markup
-        Precious::App.settings.default_markup
       end
     end
   end
 end
 
     
-        get page
-    assert_no_match /custom.js/, last_response.body
-  end
-    
-      teardown do
-    FileUtils.rm_r(File.join(File.dirname(__FILE__), *%w[examples test.git]))
-  end
-    
-        # Extract the path string that Gollum::Wiki expects
-    def extract_path(file_path)
-      return nil if file_path.nil?
-      last_slash = file_path.rindex('/')
-      if last_slash
-        file_path[0, last_slash]
-      end
-    end
-    
-      s.add_dependency 'gollum-lib', '~> 4.2', '>= 4.2.10'
-  s.add_dependency 'kramdown', '~> 1.9.0'
-  s.add_dependency 'sinatra', '~> 1.4', '>= 1.4.4'
-  s.add_dependency 'mustache', ['>= 0.99.5', '< 1.0.0']
-  s.add_dependency 'useragent', '~> 0.16.2'
-  s.add_dependency 'gemojione', '~> 3.2'
-    
-            expect(new_source)
-          .to eq('#{prefix}#{open}#{a}, # a\n#{b}#{close} # b\n#{suffix}')
+          def footer_content
+        has_footer && @footer.formatted_data
       end
     
-          DOUBLE_SPLAT = '**'.freeze
-    
-            def image_params
-          params.require(:image).permit(permitted_image_attributes)
-        end
-    
-            def destroy
-          @option_type = Spree::OptionType.accessible_by(current_ability, :destroy).find(params[:id])
-          @option_type.destroy
-          render plain: nil, status: 204
-        end
-    
-            def create
-          @order.validate_payments_attributes([payment_params])
-          @payment = @order.payments.build(payment_params)
-          if @payment.save
-            respond_with(@payment, status: 201, default_template: :show)
-          else
-            invalid_resource!(@payment)
-          end
-        end
-    
-            private
-    
-            def index
-          authorize! :read, StockMovement
-          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@stock_movements)
-        end
-    
-            def zone_params
-          attrs = params.require(:zone).permit!
-          if attrs[:zone_members]
-            attrs[:zone_members_attributes] = attrs.delete(:zone_members)
-          end
-          attrs
-        end
-    
-    desc 'Test the paperclip plugin under all supported Rails versions.'
-task :all do |t|
-  if ENV['BUNDLE_GEMFILE']
-    exec('rake spec && cucumber')
-  else
-    exec('rm -f gemfiles/*.lock')
-    Rake::Task['appraisal:gemfiles'].execute
-    Rake::Task['appraisal:install'].execute
-    exec('rake appraisal')
+        # Title is based on file name when h1_title is false.
+    actual = @view.title
+    assert_equal 'H1', title
   end
 end
+
     
-    ENV['RAILS_ENV'] = 'test'
+        # TODO: Remove to_url once write_page changes are merged.
+    @wiki.write_page('ééééé'.to_url, :markdown, '한글 text', commit_details)
+    page = @wiki.page('eeeee')
+    assert_equal '한글 text', utf8(page.raw_data)
+  end
     
-    require 'erb'
-require 'digest'
-require 'tempfile'
-require 'paperclip/version'
-require 'paperclip/geometry_parser_factory'
-require 'paperclip/geometry_detector_factory'
-require 'paperclip/geometry'
-require 'paperclip/processor'
-require 'paperclip/processor_helpers'
-require 'paperclip/tempfile'
-require 'paperclip/thumbnail'
-require 'paperclip/interpolations/plural_cache'
-require 'paperclip/interpolations'
-require 'paperclip/tempfile_factory'
-require 'paperclip/style'
-require 'paperclip/attachment'
-require 'paperclip/storage'
-require 'paperclip/callbacks'
-require 'paperclip/file_command_content_type_detector'
-require 'paperclip/media_type_spoof_detector'
-require 'paperclip/content_type_detector'
-require 'paperclip/glue'
-require 'paperclip/errors'
-require 'paperclip/missing_attachment_styles'
-require 'paperclip/validators'
-require 'paperclip/logger'
-require 'paperclip/helpers'
-require 'paperclip/has_attached_file'
-require 'paperclip/attachment_registry'
-require 'paperclip/filename_cleaner'
-require 'paperclip/rails_environment'
+      # replace name version and date
+  replace_header(head, :name)
+  replace_header(head, :version)
+  replace_header(head, :date)
+  #comment this out if your rubyforge_project has a different name
+  replace_header(head, :rubyforge_project)
     
-        # Returns a sorted list of all interpolations.
-    def self.all
-      self.instance_methods(false).sort!
+        def safe_thread(name, &block)
+      Thread.new do
+        Thread.current['sidekiq_label'] = name
+        watchdog(name, &block)
+      end
     end
