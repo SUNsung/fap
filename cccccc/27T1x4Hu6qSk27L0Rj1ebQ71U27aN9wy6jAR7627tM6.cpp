12ba782eb944a57b2f78dcd0e50a95edab0f3cbe
SUNsung <weatherius@gmail.com>
2018-12-27 01:34:01 +0200

    Rewrite [27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp] File in: 2018-12-27 01:34:01

--- cccccc/27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp
+++ cccccc/27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp
@@ -1,194 +1,85 @@
 
         
-        void Base::CallSync(const std::string& method,
-                    const base::ListValue& arguments,
-                    base::ListValue* result) {
-  NOTREACHED() << 'Uncatched callAsync in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
-}
-    
-    void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
-}
-    
-    
-    {} // namespace nwapi
-
-    
-    #include 'base/values.h'
-#include 'components/zoom/zoom_controller.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menuitem/menuitem.h'
-#include 'content/public/browser/web_contents.h'
-#include 'content/public/common/page_zoom.h'
-#include 'ui/views/controls/menu/menu_runner.h'
-    
-          // strip off data uri header if raw is set
-      if (!(data.raw.get() && *(data.raw))) {
-        if (data.type == TYPE_PNG && base::StartsWith(content, kPNGDataUriPrefix, base::CompareCase::INSENSITIVE_ASCII)) {
-          content = content.substr(strlen(kPNGDataUriPrefix));
-        } else if (data.type == TYPE_JPEG && base::StartsWith(content, kJPEGDataUriPrefix, base::CompareCase::INSENSITIVE_ASCII)) {
-          content = content.substr(strlen(kJPEGDataUriPrefix));
-        } else {
-          error_ = base::StringPrintf('Invalid data URI. Only \'%s\' or \'%s\' is accepted.', kPNGDataUriPrefix, kJPEGDataUriPrefix);
-          return false;
-        }
-      }
-    
-        void DispatchEvent(
-        events::HistogramValue histogram_value,
-        const std::string& event_name,
-        std::unique_ptr<base::ListValue> args) {
-      DCHECK_CURRENTLY_ON(BrowserThread::UI);
-      ExtensionsBrowserClient::Get()->BroadcastEventToRenderers(
-                                                                histogram_value, event_name, std::move(args));
+        namespace tensorflow {
     }
     
-    namespace testing {
+    #include <unordered_map>
+    
+    #include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    namespace tensorflow {
     }
     
-      // Allows streaming basic output manipulators such as endl or flush into
-  // this object.
-  AssertionResult& operator<<(
-      ::std::ostream& (*basic_manipulator)(::std::ostream& stream)) {
-    AppendMessage(Message() << basic_manipulator);
-    return *this;
-  }
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    // DeathTest is a class that hides much of the complexity of the
-// GTEST_DEATH_TEST_ macro.  It is abstract; its static Create method
-// returns a concrete class that depends on the prevailing death test
-// style, as defined by the --gtest_death_test_style and/or
-// --gtest_internal_run_death_test flags.
-    
-    // This helper reduces code bloat.  If we instead put its logic inside
-// the previous CopyArray() function, arrays with different sizes
-// would lead to different copies of the template code.
-template <typename T, typename U>
-void CopyArray(const T* from, size_t size, U* to) {
-  for (size_t i = 0; i != size; i++) {
-    internal::CopyArray(from[i], to + i);
-  }
-}
-    
-      // Formats an int value as '%02d'.
-  static std::string FormatIntWidth2(int value);  // '%02d' for width == 2
-    
-    template <typename E>  // E is the element type.
-class Queue {
- public:
-  // Creates an empty queue.
-  Queue() : head_(NULL), last_(NULL), size_(0) {}
-    }
-    
-    bool DHTRoutingTable::addGoodNode(const std::shared_ptr<DHTNode>& node)
-{
-  return addNode(node, true);
-}
-    
-    #include <memory>
-    
-      virtual std::shared_ptr<DHTTask>
-  createReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
-                        const std::shared_ptr<DHTNode>& newNode) CXX11_OVERRIDE;
-    
-    namespace aria2 {
-    }
-    
-    namespace aria2 {
-    }
-    
-    
-    {  virtual void
-  addImmediateTask(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+    struct TFTensorDeleter {
+  void operator()(TF_Tensor* p) const { TF_DeleteTensor(p); }
 };
     
-    DEFINE_int32(v, 0, 'The level of verbose logging to output');
+    
+    {  uint64 offset_;
+  RandomAccessFile* file_;    // Owned
+  io::RecordReader* reader_;  // Owned
+  string record_;
+  TF_DISALLOW_COPY_AND_ASSIGN(PyRecordReader);
+};
     
     
-    {
-    {
-    {
-    {        if (re.Match(instance.name)) {
-          instance.last_benchmark_instance = (&args == &family->args_.back());
-          benchmarks->push_back(std::move(instance));
-        }
-      }
-    }
+    {  tensorflow::DeviceNameUtils::ParsedName parsed_name;
+  if (!tensorflow::DeviceNameUtils::ParseFullName(node_def.device(),
+                                                  &parsed_name)) {
+    LOG(WARNING) << 'Failed to parse device from node_def: '
+                 << node_def.ShortDebugString();
+    return '';
   }
-  return true;
+  string class_name = '';
+  tensorflow::FindKernelDef(tensorflow::DeviceType(parsed_name.type.c_str()),
+                            node_def, nullptr /* kernel_def */, &class_name)
+      .IgnoreError();
+  return class_name;
 }
     
-      // We need to flush the stream buffers into the console before each
-  // SetConsoleTextAttribute call lest it affect the text that is already
-  // printed but has not yet reached the console.
-  fflush(stdout);
-  SetConsoleTextAttribute(stdout_handle,
-                          GetPlatformColorCode(color) | FOREGROUND_INTENSITY);
-  vprintf(fmt, args);
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    // Returns true if the string matches the flag.
-bool IsFlag(const char* str, const char* flag);
     
-      // Do not print iteration on bigO and RMS report
-  if (!run.report_big_o && !run.report_rms) {
-    Out << run.iterations;
-  }
-  Out << ',';
+    {  GTEST_DISALLOW_COPY_AND_ASSIGN_(ScopedFakeTestPartResultReporter);
+};
     
-    #include 'benchmark/benchmark.h'
-#include 'internal_macros.h'
+      // Stores result of the assertion predicate.
+  bool success_;
+  // Stores the message describing the condition in case the expectation
+  // construct is not satisfied with the predicate's outcome.
+  // Referenced via a pointer to avoid taking too much stack frame space
+  // with test assertions.
+  internal::scoped_ptr< ::std::string> message_;
     
-    #include 'internal_macros.h'
+      // Returns true iff the given string ends with the given suffix, ignoring
+  // case. Any string is considered to end with an empty suffix.
+  static bool EndsWithCaseInsensitive(
+      const std::string& str, const std::string& suffix);
     
-    bool RadarState201::send_quality(const std::uint8_t* bytes,
-                                 int32_t length) const {
-  Byte t0(bytes + 5);
-  uint32_t x = t0.get_byte(4, 1);
-    }
-    
-      auto geo_reference_node = header_node->FirstChildElement('geoReference');
-  if (!geo_reference_node) {
-    std::string err_msg = 'Error parsing header geoReoference attributes';
-    return Status(apollo::common::ErrorCode::HDMAP_DATA_ERROR, err_msg);
-  }
-  auto geo_text = geo_reference_node->FirstChild()->ToText();
-  if (!geo_text) {
-    std::string err_msg = 'Error parsing header geoReoference text';
-    return Status(apollo::common::ErrorCode::HDMAP_DATA_ERROR, err_msg);
-  }
-    
-    Status JunctionsXmlParser::Parse(const tinyxml2::XMLElement& xml_node,
-                                 std::vector<JunctionInternal>* junctions) {
-  const tinyxml2::XMLElement* junction_node =
-      xml_node.FirstChildElement('junction');
-  while (junction_node) {
-    // id
-    std::string junction_id;
-    int checker =
-        UtilXmlParser::QueryStringAttribute(*junction_node, 'id', &junction_id);
-    if (checker != tinyxml2::XML_SUCCESS) {
-      std::string err_msg = 'Error parse junction id';
-      return Status(apollo::common::ErrorCode::HDMAP_DATA_ERROR, err_msg);
-    }
-    }
-    }
-    
-    #include <vector>
-    
-        static BOOST_FORCEINLINE storage_type fetch_sub(storage_type volatile& storage, storage_type v, memory_order order) BOOST_NOEXCEPT
-    {
-        typedef typename make_signed< storage_type >::type signed_storage_type;
-        return Derived::fetch_add(storage, static_cast< storage_type >(-static_cast< signed_storage_type >(v)), order);
-    }
-    
-    template< std::size_t Size, bool Signed >
-struct make_storage_type
-{
-    typedef buffer_storage< Size > type;
-    }
+    // Tests the c'tor that accepts a C string.
+TEST(MyString, ConstructorFromCString) {
+  const MyString s(kHelloString);
+  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
+  EXPECT_EQ(sizeof(kHelloString)/sizeof(kHelloString[0]) - 1,
+            s.Length());
+}
