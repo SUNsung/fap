f96164029dc5656bde1ddcb7f15cdc3d97a9da03
SUNsung <weatherius@gmail.com>
2018-12-26 21:52:12 +0200

    Rewrite [162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py] File in: 2018-12-26 21:52:12

--- pyton/162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py
+++ pyton/162DdPZugselA16dYVt6buLtiA16hf7orJSY8Ak16AQ5.py
@@ -1,160 +1,99 @@
 
         
-            def __init__(self, cards):
-        self.cards = cards
-        self.deal_index = 0
-    
-        def message_group(self, group_id, message):
-        pass
-    
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-        Args:
-      batch_size: The size of the batch, i.e. 0th dim in 2D tensor of samples.
-      z_size: The dimension of the distribution, i.e. 1st dim in 2D tensor.
-      mean: The N-D mean of the distribution.
-      logvar: The N-D log variance of the diagonal distribution.
-    '''
-    size__xz = [None, z_size]
-    self.mean = mean            # bxn already
-    self.logvar = logvar        # bxn already
-    self.noise = noise = tf.random_normal(tf.shape(logvar))
-    self.sample = mean + tf.exp(0.5 * logvar) * noise
-    mean.set_shape(size__xz)
-    logvar.set_shape(size__xz)
-    self.sample.set_shape(size__xz)
-    
-      Args:
-    hps: The hyper parameters for the model.
-    kind: (optional) The kind of model to build.  Training vs inference require
-      different graphs.
-    datasets: The datasets structure (see top of lfads.py).
-    
-    rates_train, rates_valid = split_list_by_inds(rates, train_inds, valid_inds)
-spikes_train, spikes_valid = split_list_by_inds(spikes, train_inds, valid_inds)
-condition_labels_train, condition_labels_valid = split_list_by_inds(
-    condition_labels, train_inds, valid_inds)
-ext_input_train, ext_input_valid = split_list_by_inds(
-    ext_input, train_inds, valid_inds)
+            >>> humanize_bytes(1)
+    '1 B'
+    >>> humanize_bytes(1024, precision=1)
+    '1.0 kB'
+    >>> humanize_bytes(1024 * 123, precision=1)
+    '123.0 kB'
+    >>> humanize_bytes(1024 * 12342, precision=1)
+    '12.1 MB'
+    >>> humanize_bytes(1024 * 12342, precision=2)
+    '12.05 MB'
+    >>> humanize_bytes(1024 * 1234, precision=2)
+    '1.21 MB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
+    '1.31 GB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
+    '1.3 GB'
     
     
-def linear(x, out_size, do_bias=True, alpha=1.0, identity_if_possible=False,
-           normalized=False, name=None, collections=None):
-  '''Linear (affine) transformation, y = x W + b, for a variety of
-  configurations.
-    
-    tf.app.flags.DEFINE_string(
-    'data_dir', 'reproduce', 
-    'Path to directory containing data and model checkpoints.')
-    
-      # If there are no real or fake entries in the batch, we assign an average
-  # value of zero.
-  real_avg = tf.where(tf.equal(real_count, 0), zero_tensor, real_avg)
-  fake_avg = tf.where(tf.equal(fake_count, 0), zero_tensor, fake_avg)
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-      Args:
-    hparams:  Hyperparameters for the MaskGAN.
-    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
-    is_training:  Whether the model is training.
-    reuse (Optional):  Whether to reuse the model.
-    
-    '''Model loss construction.'''
-    
-      Args:
-    hparams:  MaskGAN hyperparameters.
-    learning_rate:  tf.Variable scalar learning rate.
-    final_gen_objective:  Scalar final REINFORCE objective for the sequence.
-    averages_op:  ExponentialMovingAverage apply average op to
-      maintain the baseline.
-    global_step:  global_step tf.Variable.
-    
-      ## Load Generator weights from MaskGAN checkpoint.
-  if FLAGS.maskgan_ckpt:
-    gen_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
-    ]
-    init_saver = tf.train.Saver(var_list=gen_vars)
-    init_savers['init_saver'] = init_saver
-    
-        x = inputs
-    if conv_first:
-        x = conv(x)
-        if batch_normalization:
-            x = BatchNormalization()(x)
-        if activation is not None:
-            x = Activation(activation)(x)
-    else:
-        if batch_normalization:
-            x = BatchNormalization()(x)
-        if activation is not None:
-            x = Activation(activation)(x)
-        x = conv(x)
-    return x
-    
-    batch_size = 128
-num_classes = 10
-epochs = 12
-log_dir = './logs'
-    
-        with gzip.open(paths[0], 'rb') as lbpath:
-        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+# bdist_wheel
+extras_require = {
+    # http://wheel.readthedocs.io/en/latest/#defining-conditional-dependencies
+    'python_version == '3.0' or python_version == '3.1'': ['argparse>=1.2.1'],
+    ':sys_platform == 'win32'': ['colorama>=0.2.4'],
+}
     
     
-def l1(l=0.01):
-    return L1L2(l1=l)
+with codecs.open(FILE_PATH, encoding='utf8') as f:
+    # Strip because we don't want new lines in the data so that we can
+    # easily count occurrences also when embedded in JSON (where the new
+    # line would be escaped).
+    FILE_CONTENT = f.read().strip()
     
     
-def get_source_inputs(tensor, layer=None, node_index=None):
-    '''Returns the list of input tensors necessary to compute `tensor`.
+@mock.patch('httpie.core.get_response')
+def test_timeout(get_response):
+    def error(msg, *args, **kwargs):
+        global error_msg
+        error_msg = msg % args
     
     
-def test_preprocess_input():
-    # Test image batch with float and int image input
-    x = np.random.uniform(0, 255, (2, 10, 10, 3))
-    xint = x.astype('int32')
-    assert utils.preprocess_input(x).shape == x.shape
-    assert utils.preprocess_input(xint).shape == xint.shape
-    
-        # Returns
-        A binary matrix representation of the input. The classes axis
-        is placed last.
-    
-        for epoch in range(initial_epoch, epochs):
-        # Reset stateful metrics
-        for m in model.stateful_metric_functions:
-            m.reset_states()
-        callbacks.on_epoch_begin(epoch)
-        epoch_logs = {}
-        if steps_per_epoch is not None:
-            for step_index in range(steps_per_epoch):
-                batch_logs = {}
-                batch_logs['batch'] = step_index
-                batch_logs['size'] = 1
-                callbacks.on_batch_begin(step_index, batch_logs)
-                outs = fit_function(fit_inputs)
+@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
+def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
+    r = http(follow_flag, httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 1
+    assert HTTP_OK in r
     
     
-class TestCaseBase(unittest.TestCase):
-    def check_events(self, callable, expected):
-        events = capture_events(callable, self.new_watcher())
-        if events != expected:
-            self.fail('Expected events:\n%s\nReceived events:\n%s'
-                      % (pprint.pformat(expected), pprint.pformat(events)))
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
+
     
-    # Now add the related image to the html part.
-with open('roasted-asparagus.jpg', 'rb') as img:
-    msg.get_payload()[1].add_related(img.read(), 'image', 'jpeg',
-                                     cid=asparagus_cid)
+    from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+import josepy as jose
+import OpenSSL
+    
+    
+    def init_augeas(self):
+        ''' Initialize the actual Augeas instance '''
+        import augeas
+        self.aug = augeas.Augeas(
+            # specify a directory to load our preferred lens from
+            loadpath=constants.AUGEAS_LENS_DIR,
+            # Do not save backup (we do it ourselves), do not load
+            # anything by default
+            flags=(augeas.Augeas.NONE |
+                   augeas.Augeas.NO_MODL_AUTOLOAD |
+                   augeas.Augeas.ENABLE_SPAN))
+        # See if any temporary changes need to be recovered
+        # This needs to occur before VirtualHost objects are setup...
+        # because this will change the underlying configuration and potential
+        # vhosts
+        self.recovery_routine()
+    
+    HEADER_ARGS = {'Strict-Transport-Security': HSTS_ARGS,
+               'Upgrade-Insecure-Requests': UIR_ARGS}
+    
+            for addr in vhost.addrs:
+            if '_default_' == addr.get_addr():
+                addrs.add(default_addr)
+            else:
+                addrs.add(
+                    addr.get_sni_addr(
+                        self.configurator.config.tls_sni_01_port))
+    
+    # If false, no index is generated.
+#html_use_index = True
+    
+      # Didn't find the corresponding '//' comment.  If this file does not
+  # contain any '//' comments at all, it could be that the compiler
+  # only wants '/**/' comments, look for those instead.
+  no_single_line_comments = True
+  for i in xrange(1, len(raw_lines) - 1):
+    line = raw_lines[i]
+    if Match(r'^(?:(?:\'(?:\.|[^\'])*\')|(?:'(?:\.|[^'])*')|[^\''])*//', line):
+      no_single_line_comments = False
+      break
