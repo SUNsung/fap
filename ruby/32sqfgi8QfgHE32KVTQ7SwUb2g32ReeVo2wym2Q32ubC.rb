da6c537f6cf1b61a3b7aa8c180eecbe337f9a600
SUNsung <weatherius@gmail.com>
2018-12-26 14:46:56 +0200

    Rewrite [32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb] File in: 2018-12-26 14:46:55

--- ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
+++ ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
@@ -1,95 +1,63 @@
 
         
-              # Converts the array to a comma-separated sentence where the last element is
-      # joined by the connector word. This is the html_safe-aware version of
-      # ActiveSupport's {Array#to_sentence}[http://api.rubyonrails.org/classes/Array.html#method-i-to_sentence].
-      #
-      def to_sentence(array, options = {})
-        options.assert_valid_keys(:words_connector, :two_words_connector, :last_word_connector, :locale)
+                next unless path.file?
+        file = path
     
-          def perform(start_id, stop_id)
-        update = '
-          latest_merge_request_diff_id = (
-            SELECT MAX(id)
-            FROM merge_request_diffs
-            WHERE merge_requests.id = merge_request_diffs.merge_request_id
-          )'.squish
+          # Find commands in Homebrew/dev-cmd
+      if ARGV.homebrew_developer?
+        puts
+        puts 'Built-in development commands'
+        puts_columns internal_development_commands
+      end
     
-          # token - The GitHub API token to use.
-      #
-      # per_page - The number of objects that should be displayed per page.
-      #
-      # parallel - When set to true hitting the rate limit will result in a
-      #            dedicated error being raised. When set to `false` we will
-      #            instead just `sleep()` until the rate limit is reset. Setting
-      #            this value to `true` for parallel importing is crucial as
-      #            otherwise hitting the rate limit will result in a thread
-      #            being blocked in a `sleep()` call for up to an hour.
-      def initialize(token, per_page: 100, parallel: true)
-        @octokit = Octokit::Client.new(
-          access_token: token,
-          per_page: per_page,
-          api_endpoint: api_endpoint
-        )
+      def self.canonical_name(name)
+    Formulary.canonical_name(name)
+  end
     
-            def representation_class
-          Representation::DiffNote
-        end
+      it 'raises a TypeError when passed a String' do
+    lambda { srand('7') }.should raise_error(TypeError)
+  end
+end
     
-              lfs_objects.each do |object|
-            yield object
-          end
-        rescue StandardError => e
-          Rails.logger.error('The Lfs import process failed. #{e.message}')
-        end
+        context 'after deleting a person' do
+      before do
+        user = FactoryGirl.create(:user_with_aspect)
+        user.share_with(alice.person, user.aspects.first)
+        user.person.delete
+      end
+    
+          it 'keeps invalid params in form' do
+        get :create, params: invalid_params
+        expect(response.body).to match /jdoe@example.com/m
       end
     end
   end
 end
 
     
-            # Builds a diff note from a GitHub API response.
-        #
-        # note - An instance of `Sawyer::Resource` containing the note details.
-        def self.from_api_response(note)
-          matches = note.html_url.match(NOTEABLE_ID_REGEX)
+    Given(/^I add a '(.*?)' processor in '(.*?)'$/) do |processor, directory|
+  filename = '#{directory}/#{processor}.rb'
+  cd('.') do
+    FileUtils.mkdir_p directory
+    File.open(filename, 'w') do |f|
+      f.write(<<-CLASS)
+        module Paperclip
+          class #{processor.capitalize} < Processor
+            def make
+              basename = File.basename(file.path, File.extname(file.path))
+              dst_format = options[:format] ? '.\#{options[:format]}' : ''
     
-              user = Representation::User.from_api_response(note.user) if note.user
-          hash = {
-            noteable_type: noteable_type,
-            noteable_id: matches[:iid].to_i,
-            author: user,
-            note: note.body,
-            created_at: note.created_at,
-            updated_at: note.updated_at,
-            github_id: note.id
-          }
-    
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
-          end
-        end
-      end
-      rtn
-    end
+      def generate_migration
+    migration_template('paperclip_migration.rb.erb',
+                       'db/migrate/#{migration_file_name}',
+                       migration_version: migration_version)
   end
     
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
-      end
+    require 'mimemagic'
+require 'mimemagic/overlay'
+require 'logger'
+require 'terrapin'
+    
+        def initialize
+      clear
     end
-  end
