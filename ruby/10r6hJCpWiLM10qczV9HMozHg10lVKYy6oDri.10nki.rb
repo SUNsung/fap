df948164ad478f0e7fbf3f16dd536e40597e1478
SUNsung <weatherius@gmail.com>
2018-12-27 16:27:19 +0200

    Rewrite [10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb] File in: 2018-12-27 16:27:18

--- ruby/10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb
+++ ruby/10r6hJCpWiLM10qczV9HMozHg10lVKYy6oDri.10nki.rb
@@ -1,218 +1,132 @@
 
         
-          # Removes any empty directories in the formula's prefix subtree
-  # Keeps any empty directions projected by skip_clean
-  # Removes any unresolved symlinks
-  def prune
-    dirs = []
-    symlinks = []
-    @f.prefix.find do |path|
-      if path == @f.libexec || @f.skip_clean?(path)
-        Find.prune
-      elsif path.symlink?
-        symlinks << path
-      elsif path.directory?
-        dirs << path
+            private
+    
+            css('#angular-2-glossary ~ .l-sub-section').each do |node|
+          node.before(node.children).remove
+        end
+    
+          def mod
+        return @mod if defined?(@mod)
+        @mod = slug[/api\/([\w\-\.]+)\//, 1]
+        @mod.remove! 'angular2.' if @mod
+        @mod
       end
-    end
-    
-          puts_columns Array(result)
-    else
-      query = ARGV.first
-      rx = query_regexp(query)
-      local_results = search_formulae(rx)
-      puts_columns(local_results)
-      tap_results = search_taps(rx)
-      puts_columns(tap_results)
-    
-      def self.bottle_sha1(*)
-  end
-    
-            expose_attribute :iid, :title, :description, :milestone_number,
-                         :created_at, :updated_at, :state, :assignees,
-                         :label_names, :author
-    
-            # attributes - A Hash containing the raw note details. The keys of this
-        #              Hash must be Symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-    
-              source_repository_id != target_repository_id
-        end
-    
-            retval
-      end
-    
-          def initial_urls
-        return super unless self.class.internal_urls
-        @initial_urls ||= self.class.internal_urls.map(&method(:url_for)).freeze
-      end
-    
-        class RateLimiter
-      attr_accessor :limit
-    
-                # If we still don't have an ID, generate a random one
-            id = SecureRandom.uuid.gsub('-', '') if !id
-    
-              # Handle the case that argv was empty or didn't contain any subcommand
-          main_args = argv.dup if main_args.nil?
-    
-            # This registers a plugin. This should _NEVER_ be called by the public
-        # and should only be called from within Vagrant. Vagrant will
-        # automatically register V1 plugins when a name is set on the
-        # plugin.
-        def register(plugin)
-          if !@registered.include?(plugin)
-            @logger.info('Registered plugin: #{plugin.name}')
-            @registered << plugin
-          end
-        end
-    
-            # Defines additional configuration keys to be available in the
-        # Vagrantfile. The configuration class should be returned by a
-        # block passed to this method. This is done to ensure that the class
-        # is lazy loaded, so if your class inherits from any classes that
-        # are specific to Vagrant 1.0, then the plugin can still be defined
-        # without breaking anything in future versions of Vagrant.
-        #
-        # @param [String] name Configuration key.
-        # @param [Boolean] upgrade_safe If this is true, then this configuration
-        #   key is safe to load during an upgrade, meaning that it depends
-        #   on NO Vagrant internal classes. Do _not_ set this to true unless
-        #   you really know what you're doing, since you can cause Vagrant
-        #   to crash (although Vagrant will output a user-friendly error
-        #   message if this were to happen).
-        def self.config(name=UNSET_VALUE, upgrade_safe=false, &block)
-          data[:config] ||= Registry.new
-    
-              # Make sure we're only working with one VM if single target
-          if options[:single_target] && machines.length != 1
-            @logger.debug('Using primary machine since single target')
-            primary_name = @env.primary_machine_name
-            raise Errors::MultiVMTargetRequired if !primary_name
-            machines = [get_machine.call(primary_name)]
-          end
-    
-    module Vagrant
-  module Plugin
-    module V2
-      # This is the superclass for all V2 plugins.
-      class Plugin
-        # Special marker that can be used for action hooks that matches
-        # all action sequences.
-        ALL_ACTIONS = :__all_actions__
-    
-            # This is called early, before a machine is instantiated, to check
-        # if this provider is usable. This should return true or false.
-        #
-        # If raise_error is true, then instead of returning false, this
-        # should raise an error with a helpful message about why this
-        # provider cannot be used.
-        #
-        # @param [Boolean] raise_error If true, raise exception if not usable.
-        # @return [Boolean]
-        def self.usable?(raise_error=false)
-          # Return true by default for backwards compat since this was
-          # introduced long after providers were being written.
-          true
-        end
-    
-      describe 'width' do
-    it 'specifies the minimum number of characters that will be written to the result' do
-      format('%10b', 10).should == '      1010'
-      format('%10B', 10).should == '      1010'
-      format('%10d', 112).should == '       112'
-      format('%10i', 112).should == '       112'
-      format('%10o', 87).should == '       127'
-      format('%10u', 112).should == '       112'
-      format('%10x', 196).should == '        c4'
-      format('%10X', 196).should == '        C4'
-    
-      platform_is_not :windows do
-    it 'does not expand shell variables when given multiples arguments' do
-      lambda { @object.system('echo', @shell_var) }.should output_to_fd('#{@shell_var}\n')
-    end
-  end
-    
-      # Compile a file on disk to CSS.
-  #
-  # @raise [Sass::SyntaxError] if there's an error in the document
-  # @raise [Encoding::UndefinedConversionError] if the source encoding
-  #   cannot be converted to UTF-8
-  # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
-  #
-  # @overload compile_file(filename, options = {})
-  #   Return the compiled CSS rather than writing it to a file.
-  #
-  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
-  #   @param options [{Symbol => Object}] An options hash;
-  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
-  #   @return [String] The compiled CSS.
-  #
-  # @overload compile_file(filename, css_filename, options = {})
-  #   Write the compiled CSS to a file.
-  #
-  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
-  #   @param options [{Symbol => Object}] An options hash;
-  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
-  #   @param css_filename [String] The location to which to write the compiled CSS.
-  def self.compile_file(filename, *args)
-    options = args.last.is_a?(Hash) ? args.pop : {}
-    css_filename = args.shift
-    result = Sass::Engine.for_file(filename, options).render
-    if css_filename
-      options[:css_filename] ||= css_filename
-      open(css_filename, 'w') {|css_file| css_file.write(result)}
-      nil
-    else
-      result
     end
   end
 end
+
     
-        # Transform
-    #
-    #     foo
-    #       bar
-    #         color: blue
-    #       baz
-    #         color: blue
-    #
-    # into
-    #
-    #     foo
-    #       bar, baz
-    #         color: blue
-    #
-    # @param root [Tree::Node] The parent node
-    def fold_commas(root)
-      prev_rule = nil
-      root.children.map! do |child|
-        unless child.is_a?(Tree::RuleNode)
-          fold_commas(child) if child.is_a?(Tree::DirectiveNode)
-          next child
+          def other
+        css('#example', '.example', '#description_source', '#description_demo', '[id$='example']', 'hr').remove
+    
+        def resource_params
+      params.require(:email_domain_block).permit(:domain)
+    end
+  end
+end
+
+    
+      def status_finder
+    StatusFinder.new(params[:url])
+  end
+    
+      def local_domain?
+    TagManager.instance.web_domain?(hub_topic_domain)
+  end
+    
+      private
+    
+        $Kernel_trace_var_global = 'foo'
+    captured.should == 'foo'
+  end
+    
+    Then(/^the deploy\.rb file is created$/) do
+  file = TestApp.test_app_path.join('config/deploy.rb')
+  expect(File.exist?(file)).to be true
+end
+    
+    module VagrantHelpers
+  extend self
+    
+          # rubocop:disable Security/MarshalLoad
+      def add_role(role, hosts, options={})
+        options_deepcopy = Marshal.dump(options.merge(roles: role))
+        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
+      end
+      # rubocop:enable Security/MarshalLoad
+    
+    Rake::TestTask.new(:'test:core') do |t|
+  core_tests = %w[base delegator encoding extensions filter
+     helpers mapped_error middleware radius rdoc
+     readme request response result route_added_hook
+     routing server settings sinatra static templates]
+  t.test_files = core_tests.map {|n| 'test/#{n}_test.rb'}
+  t.ruby_opts = ['-r rubygems'] if defined? Gem
+  t.ruby_opts << '-I.'
+  t.warning = true
+end
+    
+      # fetch data
+  fields = {
+    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
+    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
+    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
+  }
+    
+            if @javascript and not @escaper.respond_to? :escape_javascript
+          fail('Use EscapeUtils for JavaScript escaping.')
         end
+      end
     
-        def check_for_no_children(node)
-      return unless node.is_a?(Tree::RuleNode) && node.children.empty?
-      Sass::Util.sass_warn(<<WARNING.strip)
-WARNING on line #{node.line}#{' of #{node.filename}' if node.filename}:
-This selector doesn't have any properties and will not be rendered.
-WARNING
+          def sidebar
+        if @sidebar.nil?
+          if page = @page.sidebar
+            @sidebar = page.text_data
+          else
+            @sidebar = false
+          end
+        end
+        @sidebar
+      end
+    
+          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
+      def left_shift int, shift
+        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
+        # 1>>31, 2**32
+        (r & 2147483648) == 0 ? r : r - 4294967296
+      end
+    
+          def editable
+        @editable
+      end
+    
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
+  end
+    
+      s.name              = 'gollum'
+  s.version           = '4.1.4'
+  s.date              = '2018-10-01'
+  s.rubyforge_project = 'gollum'
+  s.license           = 'MIT'
+    
+          it 'does not autocorrect the closing brace' do
+        new_source = autocorrect_source(source)
+        expect(new_source).to eq([source].join($RS))
+      end
     end
     
-        # mixin
-    # Sass::Callable
-    inherited_hash_reader :mixin
+            self
+      end
     
-        # @param msg [String] The error message
-    # @param attrs [{Symbol => Object}] The information in the backtrace entry.
-    #   See \{#sass\_backtrace}
-    def initialize(msg, attrs = {})
-      @message = msg
-      @sass_backtrace = []
-      add_backtrace(attrs)
-    end
-    
-          protected
+          # Returns the collection the `for` loop is iterating over.
+      #
+      # @return [Node] The collection the `for` loop is iterating over
+      def collection
+        node_parts[1]
+      end
