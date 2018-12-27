58222fc65e36b9324bf2762c4d53de4b2a28ac29
SUNsung <weatherius@gmail.com>
2018-12-27 08:01:04 +0200

    Rewrite [30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb] File in: 2018-12-27 08:01:04

--- ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
+++ ruby/30LN4p5pTglE.306iuSjdGGggE30CSvC.4rcWc6305mJ.rb
@@ -1,114 +1,49 @@
 
         
-                if scanner.eos?
-          end_pos = str.source_range.end_pos
-          node = Tree::CssImportNode.new(str)
-        else
-          supports_parser = Sass::SCSS::Parser.new(scanner,
-            @options[:filename], @options[:importer],
-            @line, str.source_range.end_pos.offset)
-          supports_condition = supports_parser.parse_supports_clause
+                  # Make sure we're only working with one VM if single target
+          if options[:single_target] && vms.length != 1
+            vm = @env.primary_vm
+            raise Errors::MultiVMTargetRequired if !vm
+            vms = [vm]
+          end
     
-        # The content passed to this environment. This is naturally only set
-    # for mixin body environments with content passed in.
+            def initialize
+          @logger = Log4r::Logger.new('vagrant::plugin::v2::manager')
+          @registered = []
+        end
+    
+        # Checks if this registry has any items.
     #
-    # @return {[Array<Sass::Tree::Node>, Environment]?} The content nodes and
-    #   the lexical environment of the content block.
-    def content
-      @content || (@parent && @parent.content)
+    # @return [Boolean]
+    def empty?
+      @items.keys.empty?
     end
     
-          super
-      input = @options[:input]
-      if File.directory?(input)
-        raise 'Error: '#{input.path}' is a directory (did you mean to use --recursive?)'
-      end
-      output = @options[:output]
-      output = input if @options[:in_place]
-      process_file(input, output)
-    end
-    
-    get '/' do
-  halt erb(:login) unless params[:user]
-  erb :chat, :locals => { :user => params[:user].gsub(/\W/, '') }
+    desc 'Start an IRB session with all necessary files required.'
+task :shell do |t|
+  chdir File.dirname(__FILE__)
+  exec 'irb -I lib/ -I lib/paperclip -r rubygems -r active_record -r tempfile -r init'
 end
     
-          def referrer(env)
-        ref = env['HTTP_REFERER'].to_s
-        return if !options[:allow_empty_referrer] and ref.empty?
-        URI.parse(ref).host || Request.new(env).host
-      rescue URI::InvalidURIError
-      end
+    ENV['RAILS_ENV'] = 'test'
     
-          DIRECTIVES = %i(base_uri child_src connect_src default_src
-                      font_src form_action frame_ancestors frame_src
-                      img_src manifest_src media_src object_src
-                      plugin_types referrer reflected_xss report_to
-                      report_uri require_sri_for sandbox script_src
-                      style_src worker_src).freeze
-    
-          def page_dir
-        @page_dir
-      end
-    
-        def self.xtest(*args)
-    end
-    
-        get '/pages'
-    
-      test 'clean path without leading slash' do
-    assert_equal '/Mordor', clean_path('Mordor')
-  end
-    
-      test 'h1 title can be disabled' do
-    title = 'H1'
-    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
-    page = @wiki.page(title)
-    
-        # Ensure path begins with a single leading slash
-    def clean_path(path)
-      if path
-        (path[0] != '/' ? path.insert(0, '/') : path).gsub(/\/{2,}/, '/')
-      end
-    end
-    
-    run SinatraStaticServer
+    # This stuff needs to be run after Paperclip is defined.
+require 'paperclip/io_adapters/registry'
+require 'paperclip/io_adapters/abstract_adapter'
+require 'paperclip/io_adapters/empty_string_adapter'
+require 'paperclip/io_adapters/identity_adapter'
+require 'paperclip/io_adapters/file_adapter'
+require 'paperclip/io_adapters/stringio_adapter'
+require 'paperclip/io_adapters/data_uri_adapter'
+require 'paperclip/io_adapters/nil_adapter'
+require 'paperclip/io_adapters/attachment_adapter'
+require 'paperclip/io_adapters/uploaded_file_adapter'
+require 'paperclip/io_adapters/uri_adapter'
+require 'paperclip/io_adapters/http_url_proxy_adapter'
 
-    
-    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
-
-    
-    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
-
-    
-    When /^(?:|I )press '([^']*)'$/ do |button|
-  click_button(button)
-end
     
         alias :empty? :empty_file?
     
-        # True if the dimensions represent a vertical rectangle
-    def vertical?
-      height > width
+        def self.plural_cache
+      @plural_cache ||= PluralCache.new
     end
-    
-        def make
-      geometry = GeometryParser.new(geometry_string.strip).make
-      geometry || raise(Errors::NotIdentifiedByImageMagickError.new)
-    end
-    
-        module ClassMethods
-      def attachment_definitions
-        Paperclip::AttachmentRegistry.definitions_for(self)
-      end
-    end
-  end
-end
-
-    
-        # Returns the style, or the default style if nil is supplied.
-    def style attachment, style_name
-      style_name || attachment.default_style
-    end
-  end
-end
