b9d577defd69266377297c3ecc48456a35a1e300
SUNsung <weatherius@gmail.com>
2018-12-28 05:15:16 +0200

    Rewrite [04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb] File in: 2018-12-28 05:15:15

--- ruby/04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb
+++ ruby/04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb
@@ -1,57 +1,93 @@
 
         
-            def style(opts)
-      opts.separator ''
-      opts.separator 'Style:'
+        def run_rubygem(args)
+  run_in_shell('gem', *args.strip.split(' '))
+end
     
-            private
+            log_action :change_email, @user
     
-            def store_params
-          params.require(:store).permit(permitted_store_attributes)
+    module Admin
+  class EmailDomainBlocksController < BaseController
+    before_action :set_email_domain_block, only: [:show, :destroy]
+    
+            if params[:create_and_unresolve]
+          @report.unresolve!
+          log_action :reopen, @report
         end
+    
+          @form         = Form::StatusBatch.new(form_status_batch_params.merge(current_account: current_account, action: action_from_button))
+      flash[:alert] = I18n.t('admin.statuses.failed_to_execute') unless @form.save
+    
+      private
+    
+      def payload
+    @_payload ||= request.body.read
+  end
+    
+      respond_to :json
+    
+            it 'does not use two's complement form for negative numbers for formats bBoxX' do
+          format('%+b', -10).should == '-1010'
+          format('%+B', -10).should == '-1010'
+          format('%+o', -87).should == '-127'
+          format('%+x', -196).should == '-c4'
+          format('%+X', -196).should == '-C4'
+        end
+      end
+    end
+    
+      it 'transfers control to the innermost catch block waiting for the same sympol' do
+    one = two = three = 0
+    catch :duplicate do
+      catch :duplicate do
+        catch :duplicate do
+          one = 1
+          throw :duplicate
+        end
+        two = 2
+        throw :duplicate
+      end
+      three = 3
+      throw :duplicate
+    end
+    [one, two, three].should == [1, 2, 3]
+  end
+    
+      it 'accepts a proc argument instead of a block' do
+    captured = nil
+    
+        # The name of the mixin in which the error occurred.
+    # This could be `nil` if the error occurred outside a mixin.
+    #
+    # @return [String]
+    def sass_mixin
+      sass_backtrace.first[:mixin]
+    end
+    
+    require 'bundler/cli'
+require 'bundler/friendly_errors'
+    
+            return nil
       end
     end
   end
-end
+end end end
 
     
-            def products
-          # Returns the products sorted by their position with the classification
-          # Products#index does not do the sorting.
-          taxon = Spree::Taxon.find(params[:id])
-          @products = taxon.products.ransack(params[:q]).result
-          @products = @products.page(params[:page]).per(params[:per_page] || 500)
-          render 'spree/api/v1/products/index'
+      def execute
+    signal_deprecation_warning_for_pack
+    
+        not_matching_pipeline = described_class.new(source, pipeline_id, [], settings)
+    expect(subject).not_to eq(not_matching_pipeline)
+    
+              it 'allow to install a specific version' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version 0.1.0 logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest', '0.1.0')
+          end
         end
-    
-      FileUtils.chdir('../../..')
-end
-    
-    When /^(?:|I )fill in '([^']*)' for '([^']*)'$/ do |value, field|
-  fill_in(field, :with => value)
-end
-    
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
-    
-        def initialize
-      clear
-    end
-    
-        # Returns the scaling and cropping geometries (in string-based ImageMagick format)
-    # neccessary to transform this Geometry into the Geometry given. If crop is true,
-    # then it is assumed the destination Geometry will be the exact final resolution.
-    # In this case, the source Geometry is scaled so that an image containing the
-    # destination Geometry would be completely filled by the source image, and any
-    # overhanging image would be cropped. Useful for square thumbnail images. The cropping
-    # is weighted at the center of the Geometry.
-    def transformation_to dst, crop = false
-      if crop
-        ratio = Geometry.new( dst.width / self.width, dst.height / self.height )
-        scale_geometry, scale = scaling(dst, ratio)
-        crop_geometry         = cropping(dst, ratio, scale)
-      else
-        scale_geometry        = dst.to_s
       end
+    
+        def self.clear
+      instance.clear
+    end
