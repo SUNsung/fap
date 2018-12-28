ccd3d7e5aa784ec801504fa83030857269f22175
SUNsung <weatherius@gmail.com>
2018-12-29 00:29:25 +0200

    Rewrite [12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb] File in: 2018-12-29 00:29:25

--- ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
+++ ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
@@ -1,140 +1,70 @@
 
         
-            $?.should be_an_instance_of Process::Status
-    $?.success?.should == true
-  end
-    
-      it 'creates a public method in script binding' do
-    eval @code, script_binding
-    Object.should have_method :boom
-  end
-    
-      def parse(*paths)
-    paths.flatten
-         .compact
-         .flat_map { |p| Pathname.new(p).to_path.split(File::PATH_SEPARATOR) }
-         .uniq
-  end
-end
-
-    
-          old_initial_revision_var = 'HOMEBREW_UPDATE_BEFORE#{repo_var}'
-      old_current_revision_var = 'HOMEBREW_UPDATE_AFTER#{repo_var}'
-    
-        self.store(key, value) if (stored == false)
-    self.dcase_hash[key.downcase] = value
-  end
-    
-              # Encodes the msg_type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_msg_type
-            bn = OpenSSL::BN.new(msg_type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-                seq = OpenSSL::ASN1::Sequence.new(elems)
-    
-                int
-          end
-    
-              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_asn1_pvno(input)
-            input.value[0].value.to_i
-          end
-    
-                OpenSSL::ASN1::Sequence.new(encoded_types)
-          end
-    
-              private
-    
-    namespace :doc do
-  task :readmes do
-    Dir.glob 'lib/rack/protection/*.rb' do |file|
-      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
-      next if excluded_files.include?(file)
-      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
-      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
-      Dir.mkdir 'doc' unless File.directory? 'doc'
-      puts 'writing #{file}'
-      File.open(file, 'w') { |f| f << doc }
+            def slug
+      @slug ||= subpath.sub(/\A\//, '').remove(/\.html\z/)
     end
+    
+            css('.filetree .children').each do |node|
+          node.css('.file').each do |n|
+            n.content = '  #{n.content}'
+          end
+        end
+    
+            # Remove examples
+        css('.runnable-example').each do |node|
+          node.parent.remove
+        end
+    
+        def confirmation_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :confirmation_instructions, opts)
+    end
+    
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
+    
+        def default_controllers(options)
+      mod = options[:module] || 'devise'
+      @controllers = Hash.new { |h,k| h[k] = '#{mod}/#{k}' }
+      @controllers.merge!(options[:controllers]) if options[:controllers]
+      @controllers.each { |k,v| @controllers[k] = v.to_s }
+    end
+    
+        def form_status_batch_params
+      params.require(:form_status_batch).permit(status_ids: [])
+    end
+    
+      def show
+    @status = status_finder.status
+    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
   end
     
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   XSS and others
-    # Supported browsers:: Firefox 23+, Safari 7+, Chrome 25+, Opera 15+
-    #
-    # Description:: Content Security Policy, a mechanism web applications
-    #               can use to mitigate a broad class of content injection
-    #               vulnerabilities, such as cross-site scripting (XSS).
-    #               Content Security Policy is a declarative policy that lets
-    #               the authors (or server administrators) of a web application
-    #               inform the client about the sources from which the
-    #               application expects to load resources.
-    #
-    # More info::   W3C CSP Level 1 : https://www.w3.org/TR/CSP1/ (deprecated)
-    #               W3C CSP Level 2 : https://www.w3.org/TR/CSP2/ (current)
-    #               W3C CSP Level 3 : https://www.w3.org/TR/CSP3/ (draft)
-    #               https://developer.mozilla.org/en-US/docs/Web/Security/CSP
-    #               http://caniuse.com/#search=ContentSecurityPolicy
-    #               http://content-security-policy.com/
-    #               https://securityheaders.io
-    #               https://scotthelme.co.uk/csp-cheat-sheet/
-    #               http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-    #
-    # Sets the 'Content-Security-Policy[-Report-Only]' header.
-    #
-    # Options: ContentSecurityPolicy configuration is a complex topic with
-    #          several levels of support that has evolved over time.
-    #          See the W3C documentation and the links in the more info
-    #          section for CSP usage examples and best practices. The
-    #          CSP3 directives in the 'NO_ARG_DIRECTIVES' constant need to be
-    #          presented in the options hash with a boolean 'true' in order
-    #          to be used in a policy.
-    #
-    class ContentSecurityPolicy < Base
-      default_options default_src: :none, script_src: ''self'',
-                      img_src: ''self'', style_src: ''self'',
-                      connect_src: ''self'', report_only: false
+          weeks << {
+        week: week.to_time.to_i.to_s,
+        statuses: Redis.current.get('activity:statuses:local:#{week_id}') || '0',
+        logins: Redis.current.pfcount('activity:logins:#{week_id}').to_s,
+        registrations: Redis.current.get('activity:accounts:local:#{week_id}') || '0',
+      }
+    end
     
-            def update
-          authorize! :update, @order, order_token
-          @address = find_address
+      def show
+    raise ActiveRecord::RecordNotFound if @web_subscription.nil?
     
-              if error
-            unprocessable_entity('#{Spree.t(:shipment_transfer_errors_occured, scope: 'api')} \n#{error}')
-          else
-            @original_shipment.transfer_to_shipment(@variant, @quantity, @target_shipment)
-            render json: { success: true, message: Spree.t(:shipment_transfer_success) }, status: 201
-          end
-        end
+      gem.required_ruby_version = '>= 2.0'
+  gem.add_dependency 'airbrussh', '>= 1.0.0'
+  gem.add_dependency 'i18n'
+  gem.add_dependency 'rake', '>= 10.0.0'
+  gem.add_dependency 'sshkit', '>= 1.9.0'
     
-            private
+    Then(/^references in the remote repo are listed$/) do
+  expect(@output).to include('refs/heads/master')
+end
     
-            def update
-          authorize! :update, @store
-          if @store.update_attributes(store_params)
-            respond_with(@store, status: 200, default_template: :show)
-          else
-            invalid_resource!(@store)
-          end
-        end
-    
-            def show
-          respond_with(taxonomy)
-        end
-    
-            def create
-          authorize! :create, Spree.user_class
-          @user = Spree.user_class.new(user_params)
-          if @user.save
-            respond_with(@user, status: 201, default_template: :show)
-          else
-            invalid_resource!(@user)
-          end
-        end
+            @response = (gets || '').chomp
+      end
