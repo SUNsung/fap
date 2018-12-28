25cf7245dda7a937ead0c4484719a03c442d6889
SUNsung <weatherius@gmail.com>
2018-12-28 21:37:57 +0200

    Rewrite [15vm20b.5But.15GtpqTjbGtY15HUS8sbPZYSg15QH7.py] File in: 2018-12-28 21:37:56

--- pyton/15vm20b.5But.15GtpqTjbGtY15HUS8sbPZYSg15QH7.py
+++ pyton/15vm20b.5But.15GtpqTjbGtY15HUS8sbPZYSg15QH7.py
@@ -1,410 +1,148 @@
 
         
-            # Slice the last *relevant* output for each instance ->
-    # [num_batch_paths, output_dim]
-    self.path_embeddings = _extract_last_relevant(self.lstm_outputs,
-                                                  self.sequence_lengths)
+        
+def test_preprocess_input_symbolic():
+    # Test image batch
+    x = np.random.uniform(0, 255, (2, 10, 10, 3))
+    inputs = Input(shape=x.shape[1:])
+    outputs = Lambda(utils.preprocess_input, output_shape=x.shape[1:])(inputs)
+    model = Model(inputs, outputs)
+    assert model.predict(x).shape == x.shape
     
-    def _plot_item(W, name, full_name, nspaces):
-  plt.figure()
-  if W.shape == ():
-    print(name, ': ', W)
-  elif W.shape[0] == 1:
-    plt.stem(W.T)
-    plt.title(full_name)
-  elif W.shape[1] == 1:
-    plt.stem(W)
-    plt.title(full_name)
-  else:
-    plt.imshow(np.abs(W), interpolation='nearest', cmap='jet');
-    plt.colorbar()
-    plt.title(full_name)
+        available_devices = _get_available_devices()
+    available_devices = [_normalize_device_name(name)
+                         for name in available_devices]
+    if not gpus:
+        # Using all visible GPUs when not specifying `gpus`
+        # e.g. CUDA_VISIBLE_DEVICES=0,2 python keras_mgpu.py
+        gpus = len([x for x in available_devices if 'gpu' in x])
     
-    # not the best way to do this but E is small enough
-rates = []
-spikes = []
-for trial in xrange(E):
-  if rnn_to_use[trial] == 0:
-    rates.append(rates_a[trial])
-    spikes.append(spikes_a[trial])
-  else:
-    rates.append(rates_b[trial])
-    spikes.append(spikes_b[trial])
+        if hasattr(model, 'metrics'):
+        for m in model.stateful_metric_functions:
+            m.reset_states()
+        stateful_metric_indices = [
+            i for i, name in enumerate(model.metrics_names)
+            if str(name) in model.stateful_metric_names]
+    else:
+        stateful_metric_indices = []
     
-      def evaluate(self):
-    '''Evaluate the current ensemble.'''
-    # Attach word probabilities and correctness label to each substitution
-    ensembled_probs = sum(self.all_probs) / len(self.all_probs)
-    scorings = []
-    for i, sentence in enumerate(self.sentences):
-      correctness = self.labels[i]
-      word_probs = ensembled_probs[i, :len(sentence)]
-      joint_prob = np.prod(word_probs, dtype=np.float64)
+            # Raises
+            TypeError: if there are no layers in the model.
+        '''
+        if not self.layers:
+            raise TypeError('There are no layers in the model.')
     
-      if epoch_size_override:
-    epoch_size = epoch_size_override
-  else:
-    epoch_size = (batch_len - 1) // num_steps
+    # We create a layer which take as input movies of shape
+# (n_frames, width, height, channels) and returns a movie
+# of identical shape.
     
-      present = tf.cast(is_real_input, tf.float32)
-  missing = tf.cast(1 - present, tf.float32)
+    # set parameters:
+max_features = 5000
+maxlen = 400
+batch_size = 32
+embedding_dims = 50
+filters = 250
+kernel_size = 3
+hidden_dims = 250
+epochs = 2
     
-    import numpy as np
-from six.moves import xrange
-import tensorflow as tf
+        @possible_keys
+    def test_getitem(self, key):
+        assert self.case_insensitive_dict[key] == 'application/json'
     
-    '''Model construction.'''
+        def __eq__(self, other):
+        return all([
+            self.username == getattr(other, 'username', None),
+            self.password == getattr(other, 'password', None)
+        ])
     
-        elif FLAGS.discriminator_model == 'seq2seq_vd':
-      # Encoder.
-      dis_encoder_variable_maps = variable_mapping.dis_encoder_seq2seq(hparams)
-      dis_encoder_init_saver = tf.train.Saver(
-          var_list=dis_encoder_variable_maps)
-      # Decoder.
-      dis_decoder_variable_maps = variable_mapping.dis_decoder_seq2seq(hparams)
-      dis_decoder_init_saver = tf.train.Saver(
-          var_list=dis_decoder_variable_maps)
-      init_savers['dis_encoder_init_saver'] = dis_encoder_init_saver
-      init_savers['dis_decoder_init_saver'] = dis_decoder_init_saver
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    from collections import OrderedDict
+    from collections.abc import Callable, Mapping, MutableMapping
     
-        if n == 1:
-        return '1 B'
+        if cookie_dict is not None:
+        names_from_jar = [cookie.name for cookie in cookiejar]
+        for name in cookie_dict:
+            if overwrite or (name not in names_from_jar):
+                cookiejar.set_cookie(create_cookie(name, cookie_dict[name]))
     
+    
+def test_idna_without_version_attribute(mocker):
+    '''Older versions of IDNA don't provide a __version__ attribute, verify
+    that if we have such a package, we don't blow up.
     '''
-from httpie.client import JSON_ACCEPT
-from utils import MockEnvironment, http, HTTP_OK
-from fixtures import FILE_PATH
+    mocker.patch('requests.help.idna', new=None)
+    assert info()['idna'] == {'version': ''}
     
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
+    # The name for this set of Sphinx documents.  If None, it defaults to
+# '<project> v<release> documentation'.
+#html_title = None
     
+        if session_setting is None:
+        return request_setting
     
-def test_follow_all_output_options_used_for_redirects(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 3
-    assert HTTP_OK not in r
+        def test_params_original_order_is_preserved_by_default(self):
+        param_ordered_dict = OrderedDict((('z', 1), ('a', 1), ('k', 1), ('d', 1)))
+        session = requests.Session()
+        request = requests.Request('GET', 'http://example.com/', params=param_ordered_dict)
+        prep = session.prepare_request(request)
+        assert prep.url == 'http://example.com/?z=1&a=1&k=1&d=1'
     
-        def validate_invalidation_batch(self, invalidation_batch, caller_reference):
-        try:
-            if caller_reference is not None:
-                valid_caller_reference = caller_reference
-            else:
-                valid_caller_reference = datetime.datetime.now().isoformat()
-            valid_invalidation_batch = {
-                'paths': self.create_aws_list(invalidation_batch),
-                'caller_reference': valid_caller_reference
-            }
-            return valid_invalidation_batch
-        except (ClientError, BotoCoreError) as e:
-            self.module.fail_json_aws(e, msg='Error validating invalidation batch.')
+    # The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
     
-    RETURN = '''
-cloud_front_origin_access_identity:
-  description: The origin access identity's information.
-  returned: always
-  type: complex
-  contains:
-    cloud_front_origin_access_identity_config:
-      description: describes a url specifying the origin access identity.
-      returned: always
-      type: complex
-      contains:
-        caller_reference:
-          description: a caller reference for the oai
-          returned: always
-          type: string
-        comment:
-          description: a comment describing the oai
-          returned: always
-          type: string
-    id:
-      description: a unique identifier of the oai
-      returned: always
-      type: string
-    s3_canonical_user_id:
-      description: the cannonical user id of the user who created the oai
-      returned: always
-      type: string
-e_tag:
-  description: The current version of the origin access identity created.
-  returned: always
-  type: string
-location:
-  description: The fully qualified URI of the new origin access identity just created.
-  returned: when initially created
-  type: string
-    
-        def remove_targets(self, target_ids):
-        '''Removes the provided targets from the rule in AWS'''
-        if not target_ids:
-            return
-        request = {
-            'Rule': self.name,
-            'Ids': target_ids
-        }
-        try:
-            response = self.client.remove_targets(**request)
-        except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
-            self.module.fail_json_aws(e, msg='Could not remove rule targets from rule %s' % self.name)
-        self.changed = True
-        return response
-    
-    RETURN = '''
-elasticache_clusters:
-  description: List of elasticache clusters
-  returned: always
-  type: complex
-  contains:
-    auto_minor_version_upgrade:
-      description: Whether to automatically upgrade to minor versions
-      returned: always
-      type: bool
-      sample: true
-    cache_cluster_create_time:
-      description: Date and time cluster was created
-      returned: always
-      type: string
-      sample: '2017-09-15T05:43:46.038000+00:00'
-    cache_cluster_id:
-      description: ID of the cache cluster
-      returned: always
-      type: string
-      sample: abcd-1234-001
-    cache_cluster_status:
-      description: Status of Elasticache cluster
-      returned: always
-      type: string
-      sample: available
-    cache_node_type:
-      description: Instance type of Elasticache nodes
-      returned: always
-      type: string
-      sample: cache.t2.micro
-    cache_nodes:
-      description: List of Elasticache nodes in the cluster
-      returned: always
-      type: complex
-      contains:
-        cache_node_create_time:
-          description: Date and time node was created
-          returned: always
-          type: string
-          sample: '2017-09-15T05:43:46.038000+00:00'
-        cache_node_id:
-          description: ID of the cache node
-          returned: always
-          type: string
-          sample: '0001'
-        cache_node_status:
-          description: Status of the cache node
-          returned: always
-          type: string
-          sample: available
-        customer_availability_zone:
-          description: Availability Zone in which the cache node was created
-          returned: always
-          type: string
-          sample: ap-southeast-2b
-        endpoint:
-          description: Connection details for the cache node
-          returned: always
-          type: complex
-          contains:
-            address:
-              description: URL of the cache node endpoint
-              returned: always
-              type: string
-              sample: abcd-1234-001.bgiz2p.0001.apse2.cache.amazonaws.com
-            port:
-              description: Port of the cache node endpoint
-              returned: always
-              type: int
-              sample: 6379
-        parameter_grou_status:
-          description: Status of the Cache Parameter Group
-          returned: always
-          type: string
-          sample: in-sync
-    cache_parameter_group:
-      description: Contents of the Cache Parameter GGroup
-      returned: always
-      type: complex
-      contains:
-        cache_node_ids_to_reboot:
-          description: Cache nodes which need to be rebooted for parameter changes to be applied
-          returned: always
-          type: list
-          sample: []
-        cache_parameter_group_name:
-          description: Name of the cache parameter group
-          returned: always
-          type: string
-          sample: default.redis3.2
-        parameter_apply_status:
-          description: Status of parameter updates
-          returned: always
-          type: string
-          sample: in-sync
-    cache_security_groups:
-      description: Security Groups used by the cache
-      returned: always
-      type: list
-      sample:
-        - 'sg-abcd1234'
-    cache_subnet_group_name:
-      description: Elasticache Subnet Group used by the cache
-      returned: always
-      type: string
-      sample: abcd-subnet-group
-    client_download_landing_page:
-      description: URL of client download web page
-      returned: always
-      type: string
-      sample: 'https://console.aws.amazon.com/elasticache/home#client-download:'
-    engine:
-      description: Engine used by elasticache
-      returned: always
-      type: string
-      sample: redis
-    engine_version:
-      description: Version of elasticache engine
-      returned: always
-      type: string
-      sample: 3.2.4
-    notification_configuration:
-      description: Configuration of notifications
-      returned: if notifications are enabled
-      type: complex
-      contains:
-        topic_arn:
-          description: ARN of notification destination topic
-          returned: if notifications are enabled
-          type: string
-          sample: arn:aws:sns:*:123456789012:my_topic
-        topic_name:
-          description: Name of notification destination topic
-          returned: if notifications are enabled
-          type: string
-          sample: MyTopic
-    num_cache_nodes:
-      description: Number of Cache Nodes
-      returned: always
-      type: int
-      sample: 1
-    pending_modified_values:
-      description: Values that are pending modification
-      returned: always
-      type: complex
-      contains: {}
-    preferred_availability_zone:
-      description: Preferred Availability Zone
-      returned: always
-      type: string
-      sample: ap-southeast-2b
-    preferred_maintenance_window:
-      description: Time slot for preferred maintenance window
-      returned: always
-      type: string
-      sample: sat:12:00-sat:13:00
-    replication_group_id:
-      description: Replication Group Id
-      returned: always
-      type: string
-      sample: replication-001
-    security_groups:
-      description: List of Security Groups associated with Elasticache
-      returned: always
-      type: complex
-      contains:
-        security_group_id:
-          description: Security Group ID
-          returned: always
-          type: string
-          sample: sg-abcd1234
-        status:
-          description: Status of Security Group
-          returned: always
-          type: string
-          sample: active
-    tags:
-      description: Tags applied to the elasticache cluster
-      returned: always
-      type: complex
-      sample:
-        Application: web
-        Environment: test
-'''
-    
-    
-def main():
-    argument_spec = ec2_argument_spec()
-    argument_spec.update(dict(
-        state=dict(required=True, choices=['present', 'absent']),
-        name=dict(required=True),
-        description=dict(required=False),
-        subnets=dict(required=False, type='list'),
-    )
-    )
-    module = AnsibleModule(argument_spec=argument_spec)
-    
-    
-def main():
+        :param known_face_encodings: A list of known face encodings
+    :param face_encoding_to_check: A single face encoding to compare against the list
+    :param tolerance: How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.
+    :return: A list of True/False values indicating which known_face_encodings match the face encoding to check
     '''
-     Module action handler
-    '''
-    argument_spec = ec2_argument_spec()
-    argument_spec.update(dict(
-        name=dict(aliases=['role_name']),
-        path_prefix=dict(),
-    ))
+    return list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
+
     
-        try:
-        resource = redshift.describe_clusters(identifier)['DescribeClustersResponse']['DescribeClustersResult']['Clusters'][0]
-    except boto.exception.JSONResponseError as e:
-        module.fail_json(msg=str(e))
+        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
+    context = multiprocessing
+    if 'forkserver' in multiprocessing.get_all_start_methods():
+        context = multiprocessing.get_context('forkserver')
     
-    EXAMPLES = '''
-- name: Create Minimum GLOBAL Forwarding_Rule
-  gcp_forwarding_rule:
-    service_account_email: '{{ service_account_email }}'
-    credentials_file: '{{ credentials_file }}'
-    project_id: '{{ project_id }}'
-    forwarding_rule_name: my-forwarding_rule
-    protocol: TCP
-    port_range: 80
-    region: global
-    target: my-target-proxy
-    state: present
+    with open('HISTORY.rst') as history_file:
+    history = history_file.read()
     
-        def test_default_encoder(self):
-        from acme.fields import RFC3339Field
-        self.assertEqual(
-            self.encoded, RFC3339Field.default_encoder(self.decoded))
+        def test_3rd_am_station_after_scan(self):
+        self.radio.scan()
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1510'
+        self.assertEqual(station, expected_station)
     
-    _LOGGER = logging.getLogger(__name__)
+        def update(self):
+        print('Updating the test results in Database')
+        time.sleep(0.1)
+    
+    ### OUTPUT ###
+# Scanning... Station is 1380 AM
+# Scanning... Station is 1510 AM
+# Switching to FM
+# Scanning... Station is 89.1 FM
+# Scanning... Station is 103.9 FM
+# Scanning... Station is 81.3 FM
+# Scanning... Station is 89.1 FM
+# Switching to AM
+# Scanning... Station is 1250 AM
+# Scanning... Station is 1380 AM
+
+    
+        def show_pet(self):
+        '''Creates and shows a pet using the abstract factory'''
     
     
-class DescriptionXmlView(HomeAssistantView):
-    '''Handles requests for the description.xml file.'''
+class ObjectPool(object):
+    def __init__(self, queue, auto_get=False):
+        self._queue = queue
+        self.item = self._queue.get() if auto_get else None
     
-    
-class Watcher():
-    '''Class for starting Watchdog.'''
-    
-        def __init__(self, name, cfg):
-        '''Initialize the graph.'''
-        self._name = name
-        self._hours = cfg[CONF_HOURS_TO_SHOW]
-        self._refresh = cfg[CONF_REFRESH]
-        self._entities = cfg[CONF_ENTITIES]
+    ### OUTPUT ###
+# [{'objectb': 'b-value'}, {'default': 'default'}, {'objecta': 'a-value'}]
