362b0f337a05e258dcc0d3db6e37aa559f91d717
SUNsung <weatherius@gmail.com>
2018-12-29 08:43:28 +0200

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2018-12-29 08:43:28

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,98 +1,77 @@
 
         
-          p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
-    
-          # Fetches data from the GitHub API and yields a Page object for every page
-      # of data, without loading all of them into memory.
-      #
-      # method - The Octokit method to use for getting the data.
-      # args - Arguments to pass to the Octokit method.
-      #
-      # rubocop: disable GitlabSecurity/PublicSend
-      def each_page(method, *args, &block)
-        return to_enum(__method__, method, *args) unless block_given?
-    
-            def importer_class
-          IssueAndLabelLinksImporter
+                    # Call the state method so that we update our index state. Don't
+            # worry about exceptions here, since we just care about updating
+            # the cache.
+            begin
+              # Called for side effects
+              machine.state
+            rescue Errors::VagrantError
+            end
+          end
         end
     
-              Gitlab::Database.bulk_insert(LabelLink.table_name, rows)
+            # This contains all the configuration plugins by scope.
+        #
+        # @return [Hash<Symbol, Registry>]
+        attr_reader :configs
+    
+              nil
         end
     
-          # Returns the identifier to use for cache keys.
-      #
-      # For issues and pull requests this will be 'Issue' or 'MergeRequest'
-      # respectively. For diff notes this will return 'MergeRequest', for
-      # regular notes it will either return 'Issue' or 'MergeRequest' depending
-      # on what type of object the note belongs to.
-      def cache_key_type
-        if object.respond_to?(:issuable_type)
-          object.issuable_type
-        elsif object.respond_to?(:noteable_type)
-          object.noteable_type
-        else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
-        end
-      end
+        # Register a key with a lazy-loaded value.
+    #
+    # If a key with the given name already exists, it is overwritten.
+    def register(key, &block)
+      raise ArgumentError, 'block required' if !block_given?
+      @items[key] = block
+    end
     
-    platforms :ruby do
-  gem 'sqlite3'
-end
-    
-        if successfully_sent?(resource)
-      respond_with({}, location: after_sending_unlock_instructions_path_for(resource))
-    else
-      respond_with(resource)
+        it 'ignores SHELL env var and always uses `sh`' do
+      ENV['SHELL'] = '/bin/fakeshell'
+      lambda { @object.system('echo $0') }.should output_to_fd('sh\n')
     end
   end
     
-      config.middleware.use Warden::Manager do |config|
-    Devise.warden_config = config
+      it 'sets the tainted bit' do
+    o = Object.new
+    o.taint
+    o.tainted?.should == true
   end
     
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
+        lambda {
+      catch :blah do
+        throw :blah, :return_value, 2, 3, 4, 5
       end
+    }.should raise_error(ArgumentError)
+  end
     
-                bypass_sign_in(user)
-          DEPRECATION
-          warden.session_serializer.store(resource, scope)
-        elsif warden.user(scope) == resource && !options.delete(:force)
-          # Do nothing. User already signed in and we are not forcing it.
-          true
+        $Kernel_trace_var_global = 'foo'
+    captured.should == 'foo'
+  end
+    
+        def top_level_tasks
+      if tasks_without_stage_dependency.include?(@top_level_tasks.first)
+        @top_level_tasks
+      else
+        @top_level_tasks.unshift(ensure_stage.to_s)
+      end
+    end
+    
+        def scm_plugin_installed?
+      installer.scm_installed?
+    end
+    
+          def add_host(host, properties={})
+        new_host = Server[host]
+        new_host.port = properties[:port] if properties.key?(:port)
+        # This matching logic must stay in sync with `Server#matches?`.
+        key = ServerKey.new(new_host.hostname, new_host.port)
+        existing = servers_by_key[key]
+        if existing
+          existing.user = new_host.user if new_host.user
+          existing.with(properties)
         else
-          warden.set_user(resource, options.merge!(scope: scope))
+          servers_by_key[key] = new_host.with(properties)
         end
       end
-    
-          def stored_location_key_for(resource_or_scope)
-        scope = Devise::Mapping.find_scope!(resource_or_scope)
-        '#{scope}_return_to'
-      end
-    
-      it 'accepts a negative seed' do
-    srand(-17)
-    srand.should == -17
-  end
-    
-      it 'has no effect on immediate values' do
-    [nil, true, false].each do |v|
-      v.taint
-      v.tainted?.should == false
-    end
-  end
