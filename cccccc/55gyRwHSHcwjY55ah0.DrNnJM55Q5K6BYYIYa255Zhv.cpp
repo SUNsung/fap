3ad3bf83f9d0a5c5075c247107ddd67aa91a37ca
SUNsung <weatherius@gmail.com>
2018-12-29 03:43:04 +0200

    Rewrite [55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp] File in: 2018-12-29 03:43:03

--- cccccc/55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp
+++ cccccc/55gyRwHSHcwjY55ah0.DrNnJM55Q5K6BYYIYa255Zhv.cpp
@@ -1,139 +1,44 @@
 
         
-        
-    {  std::vector<OperatorDef> GetGradientDefs() override {
-    return SingleGradientDef(
-        'SubGradient',
-        '',
-        std::vector<std::string>{GO(0), I(0), I(1)},
-        std::vector<std::string>{GI(0), GI(1)});
-  }
-};
+        OpSchema::Cost CostInferenceForFC(
+    const OperatorDef& def,
+    const vector<TensorShape>& in) {
+  CAFFE_ENFORCE_EQ(in.size(), 3, 'FC requires three inputs');
+  struct OpSchema::Cost c;
+  ArgumentHelper helper(def);
+    }
     
-    #include 'caffe2/core/context.h'
-    
-    // FreeOp frees the content of the output blob. We allow it to take in input
-// blobs purely for the reason that it can 'wait' on the input blobs to be
-// produced by some of the earlier operators before a free is called.
-template <class Context>
-class FreeOp : public Operator<Context> {
+    template <typename T, class Context>
+class FlexibleTopKGradientOp : public Operator<Context> {
  public:
-  FreeOp(const OperatorDef& def, Workspace* ws) : Operator<Context>(def, ws) {}
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
     }
     
-    OPERATOR_SCHEMA(GatherRangesToDense)
-    .NumInputs(2, 3)
-    .NumOutputs(1, INT_MAX)
-    .SetDoc(R'DOC(
-Given DATA tensor of rank 1, and RANGES tensor of rank 3, gather values
-corresponding to each range into a separate output tensor. If the optional input
-KEY tensor is also given, the output will be sorted by KEY for each example.
-    
-            // Return true if the update should use classic momentum and 
-        // false if the unit-gain momentum should be used instead.
-        bool UseUnitGainMomentum() const
-        {
-            return m_unitGain;
-        }
-    
-    
-    {            sequenceLengths[i] = currentSequenceDataShape.SubShape(fullyDefinedSampleShape.Rank()).TotalSize();
-            maxSequenceLength = std::max(maxSequenceLength, sequenceLengths[i]);
-        }
-    
-            static NDShape GetUnpackedShape(const NDShape& sampleShape, const std::vector<Axis>& sampleDynamicAxes, const std::shared_ptr<Microsoft::MSR::CNTK::MBLayout>& packedDataLayout)
-        {
-            // Determine unpacked shape
-            auto unpackedShape = sampleShape;
-            if (packedDataLayout)
-            {
-                if (sampleDynamicAxes.empty())
-                    LogicError('A PackedValue object that has a layout must have at least one dynamic axis.');
-    }
-    }
-    
-    
-    {
-    {
-    {}}}
+    #endif // CAFFE2_OPERATORS_FREE_OP_H_
 
     
-    
-    {
-    {
-    {}}}
-
-    
-            // also 'edit' all nodes that have updated *inputs*
-        // All nodes that take inputs that have been edited must have their inputs updated.
-        // Since we do not update the model in-place, we must also create replacements for these.
-        // That is achieved by recursively including all parents of edits into the set of edits.
-        let parents = net.CreateParentsMap();
-        deque<ComputationNodeBasePtr> workList; // work list for recursion
-        for (let& replacement : replacements)
-            workList.push_back(replacement.first);
-        while (!workList.empty())
-        {
-            let node = workList.front();
-            workList.pop_front();
-            // loop over the node's parents
-            for (let& parent : parents.find(node)->second)
-            {
-                // 'edit' (clone) the parent if not yet
-                if (replacements.find(parent) != replacements.end())
-                    continue; // already a known replacement
-                // we must 'edit' the parent since it depends on a replaced input
-                replacements[parent] = parent->Duplicate();
-                // and put this parent into the workList, so that we will gets its parent in turn, etc.
-                workList.push_back(parent);
-#if 0 //def _DEBUG
-                fprintf(stderr, '\t%ls = %ls() --> relink %ls\n', parent->NodeName().c_str(), parent->OperationName().c_str(), replacements[parent]->NodeName().c_str());
-#endif
-            }
-        }
-        if (TraceLevel() > 0)
-            fprintf(stderr, 'Edit: %d out of %d nodes were either edited or need to be relinked.\n', (int)replacements.size(), (int)net.GetTotalNumberOfNodes());
-        // Now the keys of replacements[] define the set of all nodes that must be relinked.
-    
-        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange& fr) override
-    {
-        auto sliceInputValue  = InputRef(0).ValueFor(fr);
-        auto sliceOutputValue =           ValueFor(fr); // row vector
+    template <>
+void GluOp<float, CPUContext>::ComputeGlu(
+    const int M,
+    const int split_dim,
+    const int N,
+    const float* Xdata,
+    float* Ydata) {
+  const int xStride = 2 * split_dim * N;
+  const int yStride = split_dim * N;
+  for (int i = 0; i < M; ++i) {
+    const int idx = i * xStride;
+    const int idy = i * yStride;
+    for (int j = 0; j < split_dim; ++j) {
+      const int jN = j * N;
+      const int jdx1 = idx + jN;
+      const int jdx2 = idx + (j + split_dim) * N;
+      const int jdy = idy + jN;
+      for (int k = 0; k < N; ++k) {
+        const float x1 = Xdata[jdx1 + k];
+        const float x2 = Xdata[jdx2 + k];
+        Ydata[jdy + k] = x1 * sigmoid(x2);
+      }
     }
-    
-        /** Returns a clone of action.
-     *
-     * @return A clone action.
-     */
-    virtual Action* clone() const
-    {
-        CC_ASSERT(0);
-        return nullptr;
-    }
-    
-    
-    {    return ret;
+  }
 }
-    
-        // Overrides
-    void startWithTarget(Node *target) override;
-    void update(float dt) override;
-    ActionTween* reverse() const override;
-    ActionTween *clone() const override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    /** 
-     * @brief Initializes the action with the property name (key), and the from and to parameters.
-     * @param duration The duration of the ActionTween. It's a value in seconds.
-     * @param key The key of property which should be updated.
-     * @param from The value of the specified property when the action begin.
-     * @param to The value of the specified property when the action end.
-     * @return If the initialization success, return true; otherwise, return false.
-     */
-    bool initWithDuration(float duration, const std::string& key, float from, float to);
-    
-    /**
- * @addtogroup _2d
- * @{
- */
-    }
