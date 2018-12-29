45b7e08fd28917b934321a799d90d619ac16c15d
SUNsung <weatherius@gmail.com>
2018-12-29 18:04:29 +0200

    Rewrite [00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp] File in: 2018-12-29 18:04:28

--- cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
+++ cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
@@ -1,69 +1,100 @@
 
         
-        const Vec2& PointArray::getControlPointAtIndex(ssize_t index) const
-{
-    index = MIN(static_cast<ssize_t>(_controlPoints.size())-1, MAX(index, 0));
-    return _controlPoints.at(index);
-}
+        #endif // BITCOIN_QT_NETWORKSTYLE_H
+
     
-    /** @class Show
-* @brief Show the node.
-**/
-class CC_DLL Show : public ActionInstant
-{
-public:
-    /** Allocates and initializes the action.
-     *
-     * @return  An autoreleased Show object.
-     */
-    static Show * create();
+    #ifndef BITCOIN_QT_QVALUECOMBOBOX_H
+#define BITCOIN_QT_QVALUECOMBOBOX_H
+    
+    #ifndef BITCOIN_REVERSELOCK_H
+#define BITCOIN_REVERSELOCK_H
+    
+    v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
+                                           int object_id,
+                                           const std::string& type,
+                                           const std::string& method,
+                                           v8::Handle<v8::Value> args) {
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  scoped_ptr<V8ValueConverter> converter(V8ValueConverter::create());
     }
     
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+    // Call method of an object in browser.
+// function CallObjectMethod(id, type, method, args);
+v8::Handle<v8::Value> CallObjectMethod(int routing_id,
+                                       int object_id,
+                                       const std::string& type,
+                                       const std::string& method,
+                                       v8::Handle<v8::Value> args);
     
-    #include '2d/CCActionGrid3D.h'
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    }
     
-    bool Animation::initWithSpriteFrames(const Vector<SpriteFrame*>& frames, float delay/* = 0.0f*/, unsigned int loops/* = 1*/)
-{
-    _delayPerUnit = delay;
-    _loops = loops;
+    
+bool MenuDelegate::GetAcceleratorForCommandId(
+      int command_id,
+      ui::Accelerator* accelerator) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+    }
+    
+      refresh_runner_ = std::make_shared<ConfigRefreshRunner>();
+  started_thread_ = false;
+    
+    /**
+ * @brief A pluggable configuration parser.
+ *
+ * An osquery config instance is populated from JSON using a ConfigPlugin.
+ * That plugin may update the config data asynchronously and read from
+ * several sources, as is the case with 'filesystem' and reading multiple files.
+ *
+ * A ConfigParserPlugin will receive the merged configuration at osquery start
+ * and the updated (still merged) config if any ConfigPlugin updates the
+ * instance asynchronously. Each parser specifies a set of top-level JSON
+ * keys to receive. The config instance will auto-merge the key values
+ * from multiple sources.
+ *
+ * The keys must contain either dictionaries or lists.
+ *
+ * If a top-level key is a dictionary, each source with the top-level key
+ * will have its own dictionary keys merged and replaced based on the lexical
+ * order of sources. For the 'filesystem' config plugin this is the lexical
+ * sorting of filenames. If the top-level key is a list, each source with the
+ * top-level key will have its contents appended.
+ *
+ * Each config parser plugin will live alongside the config instance for the
+ * life of the osquery process. The parser may perform actions at config load
+ * and config update 'time' as well as keep its own data members and be
+ * accessible through the Config class API.
+ */
+class ConfigParserPlugin : public Plugin {
+ public:
+  using ParserConfig = std::map<std::string, JSON>;
+    }
+    
+    #include <osquery/config/config.h>
+    
+      Status update(const std::string& source, const ParserConfig& config) override;
+    
+    namespace osquery {
     }
     
     /**
- * @addtogroup _2d
- * @{
+ * @brief Parser plugin for logger configurations.
  */
+class LoggerConfigParserPlugin : public ConfigParserPlugin {
+ public:
+  std::vector<std::string> keys() const override {
+    return {kLoggerKey};
+  }
     }
     
-    
-    {    switch (version) {
-        case 1:
-            parseVersion1(animations.asValueMap());
-            break;
-        case 2:
-            parseVersion2(animations.asValueMap());
-            break;
-        default:
-            CCASSERT(false, 'Invalid animation format');
+    TEST_F(FilePathsConfigParserPluginTests, test_remove_source) {
+  Config::get().update(config_data_);
+  Config::get().removeFiles('awesome');
+  // Expect the pack's set to persist.
+  // Do not call removeFiles, instead only update the pack/config content.
+  EXPECT_EQ(numFiles(), 1U);
     }
-}
-    
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-    
-                    
-            case 8:
-            case 10:
-            case 11:
-                /* going DOWN with these cases:
-                 8          10          11
-                 +---+---+  +---+---+   +---+---+
-                 |   |   |  |   | 2 |   | 1 | 2 |
-                 +---+---+  +---+---+   +---+---+
-                 |   | 8 |  |   | 8 |   |   | 8 |
-                 +---+---+  +---+---+  	+---+---+
-                 */
-                stepx = 0;
-                stepy = 1;
-                break;
