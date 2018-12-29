630cebd9f9158303d039cca27e6b1b47189d5aa3
SUNsung <weatherius@gmail.com>
2018-12-29 23:12:21 +0200

    Rewrite [56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py] File in: 2018-12-29 23:12:20

--- pyton/56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py
+++ pyton/56Ts240spxFew56k8Ehy6.KXc.56Bya8hFm8l7.56Wgv.py
@@ -1,47 +1,294 @@
 
         
-            def __init__(self):
-        self.name = 'unaccent'
+          Args:
+    prefix_words: Prefix words.
+    vocab: Vocabulary. Contains max word chard id length and converts between
+        words and ids.
+  '''
+  targets = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
+  weights = np.ones([BATCH_SIZE, NUM_TIMESTEPS], np.float32)
+    
+      Returns:
+    values:  tf.float32 Tensor of predictions of shape [batch_size,
+      sequence_length]
+  '''
+  if FLAGS.baseline_method == 'critic':
+    if FLAGS.discriminator_model == 'seq2seq_vd':
+      values = critic_vd.critic_seq2seq_vd_derivative(
+          hparams, sequence, is_training, reuse=reuse)
+    else:
+      raise NotImplementedError
+  else:
+    raise NotImplementedError
+  return values
 
     
-    KEY_PREFIX = 'django.contrib.sessions.cache'
+      # Generator rewards are log-probabilities.
+  eps = tf.constant(1e-7, tf.float32)
+  dis_predictions = tf.nn.sigmoid(dis_predictions)
+  rewards = tf.log(dis_predictions + eps)
+    
+      if FLAGS.data_set == 'ptb':
+    model_str = 'Model'
+  else:
+    model_str = 'model'
     
     
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
+def main():
+    import os
+    import re
+    import shutil
+    import sys
+    dests = sys.argv[1:] or ['.']
+    filename = re.sub('\.pyc$', '.py', __file__)
+    
+    
+def test_when_cant_configure_automatically(shell_pid, shell, logs):
+    shell_pid.return_value = 12
+    shell.how_to_configure.return_value = ShellConfiguration(
+        content='eval $(thefuck --alias)',
+        path='/tmp/.bashrc',
+        reload='bash',
+        can_configure_automatically=False)
+    main()
+    logs.how_to_configure_alias.assert_called_once()
+    
+    
+def how_to_configure(proc, TIMEOUT):
+    proc.sendline(u'fuck')
+    assert proc.expect([TIMEOUT, u'alias isn't configured'])
+
+    
+    
+@pytest.mark.functional
+def test_without_confirmation(proc, TIMEOUT):
+    without_confirmation(proc, TIMEOUT)
+    
+    
+@pytest.mark.functional
+def test_how_to_configure_alias(proc, TIMEOUT):
+    proc.sendline(u'unfunction fuck')
+    how_to_configure(proc, TIMEOUT)
+
+    
+    \tDid you mean `build`?
+'''
+    
+        @property
+    def body(self):
+        '''Return a `bytes` with the message's body.'''
+        raise NotImplementedError()
+    
+        def get_formatters_grouped(self):
+        groups = {}
+        for group_name, group in groupby(
+                self.get_formatters(),
+                key=lambda p: getattr(p, 'group_name', 'format')):
+            groups[group_name] = list(group)
+        return groups
+    
+    
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
+    
+        plugin_manager.register(Plugin)
+    try:
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            USERNAME + SEP_CREDENTIALS + PASSWORD,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
+    
+    
+def test_default_options_overwrite(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['json'] == {'foo': 'bar'}
+    
+    error_msg = None
+    
+    
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
+    
+    
+def test_unicode_headers(httpbin):
+    # httpbin doesn't interpret utf8 headers
+    r = http(httpbin.url + '/headers', u'Test:%s' % UNICODE)
+    assert HTTP_OK in r
+    
+    
+class SettingsListDirective(Directive):
+    def run(self):
+        return [settingslist_node('')]
+    
+    def _get_commands_from_entry_points(inproject, group='scrapy.commands'):
+    cmds = {}
+    for entry_point in pkg_resources.iter_entry_points(group):
+        obj = entry_point.load()
+        if inspect.isclass(obj):
+            cmds[entry_point.name] = obj()
+        else:
+            raise Exception('Invalid entry point %s' % entry_point.name)
+    return cmds
+    
+            sfile = sys.modules[spidercls.__module__].__file__
+        sfile = sfile.replace('.pyc', '.py')
+        self.exitcode = os.system('%s '%s'' % (editor, sfile))
+
+    
+            spidercls = DefaultSpider
+        spider_loader = self.crawler_process.spider_loader
+        if opts.spider:
+            spidercls = spider_loader.load(opts.spider)
+        else:
+            spidercls = spidercls_for_request(spider_loader, request, spidercls)
+        self.crawler_process.crawl(spidercls, start_requests=lambda: [request])
+        self.crawler_process.start()
+
+    
+                # trustRoot set to platformTrust() will use the platform's root CAs.
+            #
+            # This means that a website like https://www.cacert.org will be rejected
+            # by default, since CAcert.org CA certificate is seldom shipped.
+            return optionsForClientTLS(hostname.decode('ascii'),
+                                       trustRoot=platformTrust(),
+                                       extraCertificateOptions={
+                                            'method': self._ssl_method,
+                                       })
+    
+            print('step {0}'.format(step_ord))
+        print([i for i in range(len(self.values))])
+        print(self.values)
+    
+    
+class SubArray:
+    
+    	for m in getBlock(bs):
+		A = a0 
+		B = b0
+		C = c0
+		D = d0
+		for i in range(64):
+			if i <= 15:
+				#f = (B & C) | (not32(B) & D)
+				f = D ^ (B & (C ^ D))
+				g = i
+			elif i<= 31:
+				#f = (D & B) | (not32(D) & C)
+				f = C ^ (D & (B ^ C))
+				g = (5*i+1) % 16
+			elif i <= 47:
+				f = B ^ C ^ D
+				g = (3*i+5) % 16
+			else:
+				f = C ^ (B | not32(D))
+				g = (7*i) % 16
+			dtemp = D
+			D = C
+			C = B
+			B = sum32(B,leftrot32((A + f + tvals[i] + m[g]) % 2**32, s[i]))
+			A = dtemp
+		a0 = sum32(a0, A)
+		b0 = sum32(b0, B)
+		c0 = sum32(c0, C)
+		d0 = sum32(d0, D)
+    
+        return res
+    
+        complete_apps = ['sentry']
+    symmetrical = True
+
+    
+            # Adding field 'ApiKey.scope_list'
+        db.add_column(
+            'sentry_apikey',
+            'scope_list',
+            self.gf('sentry.db.models.fields.array.ArrayField')(
+                of=('django.db.models.fields.TextField', [], {})
+            ),
+            keep_default=False
+        )
+    
+        def _forwards(self, orm):
+        'Write your forwards methods here.'
+    
     '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
-        else:
-            out = string.decode(encoding)
+Reference: https://en.wikipedia.org/wiki/Delegation_pattern
+Author: https://github.com/IuryAlves
     
-            cid = CaseInsensitiveDict()
-        cid['Accept'] = 'application/json'
-        cid['aCCEPT'] == 'application/json'  # True
-        list(cid) == ['Accept']  # True
     
-            with open(filename, 'rb', 0) as fp1:
-            fd1 = fp1.fileno()
-            with open(filename, 'rb', 0) as fp2:
-                fd2 = fp2.fileno()
-                self.assertTrue(self.pathmodule.sameopenfile(fd1, fd2))
+if __name__ == '__main__':
+    reporter = Reporter()
+    db = DB()
+    tm = TestManager()
+    tm.setReporter(reporter)
+    tm.setDB(db)
+    reporter.setTM(tm)
+    db.setTM(tm)
+    # For simplification we are looping on the same test.
+    # Practically, it could be about various unique test classes and their
+    # objects
+    for i in range(3):
+        tc = TC()
+        tc.setTM(tm)
+        tm.setTC(tc)
+        tc.setup()
+        tc.execute()
+        tc.tearDown()
     
-    document = '''\
-<slideshow>
-<title>Demo slideshow</title>
-<slide><title>Slide title</title>
-<point>This is a demo</point>
-<point>Of a program for processing slides</point>
-</slide>
     
-            try:
-            print(pool.map(f, list(range(10))))
-        except ZeroDivisionError:
-            print('\tGot ZeroDivisionError as expected from pool.map()')
-        else:
-            raise AssertionError('expected ZeroDivisionError')
+class Transaction(object):
+    '''A transaction guard.
+    
+    
+class Visitor(object):
+    def visit(self, node, *args, **kwargs):
+        meth = None
+        for cls in node.__class__.__mro__:
+            meth_name = 'visit_' + cls.__name__
+            meth = getattr(self, meth_name, None)
+            if meth:
+                break
+    
+    ### OUTPUT ###
+# Name: Jhon    Occupation: Coder
+# Before we access `relatives`:
+# {'call_count2': 0, 'name': 'Jhon', 'occupation': 'Coder'}
+# Jhon's relatives: Many relatives.
+# After we've accessed `relatives`:
+# {'relatives': 'Many relatives.', 'call_count2': 0, 'name': 'Jhon', 'occupation': 'Coder'}
+# Father and mother
+# {'_lazy__parents': 'Father and mother', 'relatives': 'Many relatives.', 'call_count2': 1, 'name': 'Jhon', 'occupation': 'Coder'}  # noqa flake8
+# Father and mother
+# 1
+
+    
+    
+class Scientist(AbstractExpert):
+    @property
+    def is_eager_to_contribute(self):
+        return random.randint(0, 1)
+    
+        def get_product_list(self):
+        print('PRODUCT LIST:')
+        for product in self.business_logic.product_list():
+            print(product)
+        print('')
