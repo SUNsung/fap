dee86f00ead23a3e104fabdbf9374193b44451e2
SUNsung <weatherius@gmail.com>
2018-12-31 01:35:34 +0200

    Rewrite [12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp] File in: 2018-12-31 01:35:33

--- cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
+++ cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
@@ -1,206 +1,196 @@
 
         
-          static void Call(content::Shell* shell,
-                   const std::string& method,
-                   const base::ListValue& arguments,
-                   base::ListValue* result,
-                   DispatcherHost* dispatcher_host);
-    
-    Base::Base(int id,
-           const base::WeakPtr<ObjectManager>& object_manager,
-           const base::DictionaryValue& option,
-	   const std::string& extension_id)
-    : extension_id_(extension_id),
-      id_(id),
-      delay_destruction_(false),
-      pending_destruction_(false),
-      object_manager_(object_manager) {
-}
+         private:
+  void SetText(std::string& text);
+  std::string GetText();
+  void Clear();
     
     
-    {  RenderThread::Get()->Send(new ShellViewHostMsg_Call_Object_Method(
-      routing_id,
-      object_id,
-      type,
-      method,
-      *static_cast<base::ListValue*>(value_args.get())));
-  return v8::Undefined(isolate);
-}
+#include 'content/nw/src/api/event/event.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'ui/gfx/screen.h'
     
-    void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
-}
-    
-    
-bool MenuDelegate::GetAcceleratorForCommandId(
-      int command_id,
-      ui::Accelerator* accelerator) const {
-  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
+    bool MenuDelegate::IsItemForCommandIdDynamic(int command_id) const {
+  if (command_id < 0)
     return false;
     }
     
-    void MenuItem::Destroy() {
-  gtk_widget_destroy(menu_item_);
-  g_object_unref(G_OBJECT(menu_item_));
-}
-    
-      // implement nw.Screen.startMonitor()
-  class NwScreenStartMonitorFunction : public NWSyncExtensionFunction {
-  public:
-    NwScreenStartMonitorFunction();
-    bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    bool GodotCollisionDispatcher::needsCollision(const btCollisionObject *body0, const btCollisionObject *body1) {
-	if (body0->getUserIndex() == CASTED_TYPE_AREA || body1->getUserIndex() == CASTED_TYPE_AREA) {
-		// Avoide area narrow phase
-		return false;
-	}
-	return btCollisionDispatcher::needsCollision(body0, body1);
-}
-    
-    	ClassDB::register_virtual_class<CSGShape>();
-	ClassDB::register_virtual_class<CSGPrimitive>();
-	ClassDB::register_class<CSGMesh>();
-	ClassDB::register_class<CSGSphere>();
-	ClassDB::register_class<CSGBox>();
-	ClassDB::register_class<CSGCylinder>();
-	ClassDB::register_class<CSGTorus>();
-	ClassDB::register_class<CSGPolygon>();
-	ClassDB::register_class<CSGCombiner>();
-    
-    #include 'modules/gdnative/gdnative.h'
+      base::ThreadRestrictions::ScopedAllowWait allow_wait;
     
     
-    {	ERR_FAIL();
+    {  DECLARE_EXTENSION_FUNCTION('nw.App.clearCache', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwAppClearCacheFunction);
 };
     
-    	} else if (p_string.get_slice_count('.') == 4) {
-		// IPv4 (mapped to IPv6 internally)
-		field16[5] = 0xffff;
-		_parse_ipv4(p_string, 0, &field8[12]);
-		valid = true;
+    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringFWithFixupFunction() {}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     
-    class PCKPacker : public Reference {
-    }
+ protected:
+  ~NwMenuGetNSStringFWithFixupFunction() override {}
     
-    #ifdef _MSC_VER
-#pragma warning(disable:4503)  // disable warning: decorated name length exceeded.
-#endif
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
+};
     
-    DMLC_REGISTER_PARAMETER(CaffeDataParam);
+    	void setPivotInA(const Vector3 &p_pos);
+	void setPivotInB(const Vector3 &p_pos);
     
-    // DO_BIND_DISPATCH comes from static_operator_common.h
-Operator *CaffeLossProp::CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
-                                     std::vector<int> *in_type) const {
-  std::vector<int> out_type, aux_type;
-  std::vector<TShape> out_shape, aux_shape;
-  out_type.resize(this->ListOutputs().size());
-  out_shape.resize(this->ListOutputs().size());
-  aux_type.resize(this->ListAuxiliaryStates().size());
-  aux_shape.resize(this->ListAuxiliaryStates().size());
-  CHECK(InferType(in_type, &out_type, &aux_type));
-  CHECK(InferShape(in_shape, &out_shape, &aux_shape));
-  DO_BIND_DISPATCH(CreateOp, param_, (*in_type)[0]);
+    #include 'networked_multiplayer_peer.h'
+    
+    	static void _bind_methods();
+    
+    Shell::~Shell() {
 }
     
-      # Contents of libsvm file ``label.t``
-  1.0
-  -2.0 0:0.125
-  -3.0 2:1.2
-  4 1:1.0 2:-1.2
+    		String id = token.value;
+		if (id == 'true')
+			value = true;
+		else if (id == 'false')
+			value = false;
+		else if (id == 'null')
+			value = Variant();
+		else {
+			r_err_str = 'Expected 'true','false' or 'null', got '' + id + ''.';
+			return ERR_PARSE_ERROR;
+		}
+		return OK;
     
-            ValidateType<T>(dict, typeValue, currentVersion);
-    
-        Constant Constant::CloneAs(DataType dataType) const
-    {
-        if (dataType != DataType::Double)
-            InvalidArgument('Constant::Clone: Cannot clone Constant '%S' with DataType '%s' to DataType '%s'.', AsString().c_str(), DataTypeName(GetDataType()), DataTypeName(dataType));
+    Status BuildTable(const std::string& dbname,
+                  Env* env,
+                  const Options& options,
+                  TableCache* table_cache,
+                  Iterator* iter,
+                  FileMetaData* meta) {
+  Status s;
+  meta->file_size = 0;
+  iter->SeekToFirst();
     }
     
-        std::string m_name; // lock name
-    HANDLE m_handle;
-    
-        // return the randomized feature bounds for a time range
-    std::pair<size_t, size_t> Bounds(size_t ts, size_t te) const
-    {
-        size_t tbegin = max(ts, randomizationrange / 2) - randomizationrange / 2;
-        size_t tend = min(te + randomizationrange / 2, map.size());
-        return std::make_pair<size_t, size_t>(std::move(tbegin), move(tend));
+        if (mem == nullptr) {
+      mem = new MemTable(internal_comparator_);
+      mem->Ref();
+    }
+    status = WriteBatchInternal::InsertInto(&batch, mem);
+    MaybeIgnoreError(&status);
+    if (!status.ok()) {
+      break;
+    }
+    const SequenceNumber last_seq =
+        WriteBatchInternal::Sequence(&batch) +
+        WriteBatchInternal::Count(&batch) - 1;
+    if (last_seq > *max_sequence) {
+      *max_sequence = last_seq;
     }
     
-        void Start();
-    void Stop();
-    void Restart();
-    
-        virtual void /*ComputationNodeBase::*/ Validate(bool isFinalValidationPass) override
-    {
-        Base::Validate(isFinalValidationPass);
-        InferMBLayoutFromInputsForStandardCase(isFinalValidationPass);
+    namespace leveldb {
     }
     
-    Speed* Speed::create(ActionInterval* action, float speed)
-{
-    Speed *ret = new (std::nothrow) Speed();
-    if (ret && ret->initWithAction(action, speed))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    CC_SAFE_DELETE(ret);
-    return nullptr;
+    std::string ParsedInternalKey::DebugString() const {
+  char buf[50];
+  snprintf(buf, sizeof(buf), '' @ %llu : %d',
+           (unsigned long long) sequence,
+           int(type));
+  std::string result = ''';
+  result += EscapeString(user_key.ToString());
+  result += buf;
+  return result;
 }
     
-        //
-    // convert 'absolutes' to 'diffs'
-    //
-    Vec2 p = copyConfig->getControlPointAtIndex(0);
-    for (ssize_t i = 1; i < copyConfig->count(); ++i)
-    {
-        Vec2 current = copyConfig->getControlPointAtIndex(i);
-        Vec2 diff = current - p;
-        copyConfig->replaceControlPoint(diff, i);
+    namespace leveldb {
     }
     
-        /** Creates an action with a Cardinal Spline array of points and tension.
-     * @param dt In seconds.
-     * @param points An PointArray.
-     * @code
-     * When this function bound to js or lua,the input params are changed.
-     * In js: var create(var dt,var table).
-     * In lua: local create(local dt, local table).
-     * @endcode
-     */
-    static CatmullRomTo* create(float dt, PointArray* points);
+      fname = InfoLogFileName('foo');
+  ASSERT_EQ('foo/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(0, number);
+  ASSERT_EQ(kInfoLogFile, type);
     
-    // implementation of FlipX3D
+    static const int kBlockSize = 32768;
     
-    For example, you can simulate a Ping Pong effect running the action normally and
-then running it again in Reverse mode.
-    
-        if (element)
-    {
-        auto limit = element->actions->num;
-        for (int i = 0; i < limit;)
-        {
-            Action *action = static_cast<Action*>(element->actions->arr[i]);
+    namespace leveldb {
+namespace log {
     }
     }
     
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+      // Drop reference count.  Delete if no more references exist.
+  void Unref() {
+    --refs_;
+    assert(refs_ >= 0);
+    if (refs_ <= 0) {
+      delete this;
+    }
+  }
     
     
-    {    return false;
+    {    // Compare against model iterator
+    std::set<Key>::iterator model_iter = keys.lower_bound(i);
+    for (int j = 0; j < 3; j++) {
+      if (model_iter == keys.end()) {
+        ASSERT_TRUE(!iter.Valid());
+        break;
+      } else {
+        ASSERT_TRUE(iter.Valid());
+        ASSERT_EQ(*model_iter, iter.key());
+        ++model_iter;
+        iter.Next();
+      }
+    }
+  }
+    
+    namespace HPHP { namespace HHBBC {
+    }
+    }
+    
+    struct DataStreamWrapper final : Stream::Wrapper {
+  DataStreamWrapper() {
+    m_isLocal = true;
+  }
+    }
+    
+      // implementing File
+  bool open(const String& filename, const String& mode) override;
+  bool close() override;
+  int64_t readImpl(char *buffer, int64_t length) override;
+  int getc() override;
+  int64_t writeImpl(const char *buffer, int64_t length) override;
+  bool seek(int64_t offset, int whence = SEEK_SET) override;
+  int64_t tell() override;
+  bool eof() override;
+  bool rewind() override;
+  bool flush() override;
+  bool truncate(int64_t size) override;
+    
+    #include <memory>
+#include <set>
+    
+    
+    {  int ret = x;
+  return ret;
 }
     
-    // implementation AtlasNode
+      x <<= 4;
+  x |= t;
+    
+    int ObjectListStatus60A::interface_version(const std::uint8_t* bytes,
+                                           int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(4, 4);
+    }
+    
+      pnc_map_->adc_waypoint_.s = 100;
+  pnc_map_->UpdateNextRoutingWaypointIndex(14);
+  EXPECT_EQ(3, pnc_map_->next_routing_waypoint_index_);
+    
+    
+    {  static hdmap::HDMap hdmap_;
+};
+    
+    
+    {
+    {}  // namespace planning
+}  // namespace apollo
