53e0f1cb1e20f01d728e683e41797a7fe374f804
SUNsung <weatherius@gmail.com>
2018-12-31 00:01:57 +0200

    Rewrite [2354SS6V0F8E23BCvp.ewgeVc23V8aXg5iXePM23C6y.cpp] File in: 2018-12-31 00:01:56

--- cccccc/2354SS6V0F8E23BCvp.ewgeVc23V8aXg5iXePM23C6y.cpp
+++ cccccc/2354SS6V0F8E23BCvp.ewgeVc23V8aXg5iXePM23C6y.cpp
@@ -1,273 +1,204 @@
 
         
-          auto axis = helper.GetSingleArgument<int32_t>('axis', 1);
-  const auto canonical_axis = canonical_axis_index_(axis, in[0].dims().size());
-  auto axis_w = helper.GetSingleArgument<int32_t>('axis_w', 1);
-  const int canonical_axis_w =
-      canonical_axis_index_(axis_w, in[1].dims().size());
-  const int N = pretransposed_weight
-      ? size_from_dim_(canonical_axis_w, GetDimsVector(in[1]))
-      : size_to_dim_(canonical_axis_w, GetDimsVector(in[1]));
-    
-    #endif // CAFFE2_OPERATORS_FREE_OP_H_
+        #endif  // CAFFE_DATA_TRANSFORMER_HPP_
 
     
-    OPERATOR_SCHEMA(HalfToFloat)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .TensorInferenceFunction(
-        [](const OperatorDef& def, const vector<TensorShape>& in) {
-          vector<TensorShape> out;
-          const TensorShape& X = in[0];
-          out.push_back(X);
-          out[0].set_data_type(TensorProto_DataType_FLOAT);
+    
+#define REGISTER_LAYER_CREATOR(type, creator)                                  \
+  static LayerRegisterer<float> g_creator_f_##type(#type, creator<float>);     \
+  static LayerRegisterer<double> g_creator_d_##type(#type, creator<double>)    \
+    
+    
+    {}  // namespace caffe
+    
+    /**
+ * @brief Normalizes the input to have 0-mean and/or unit (1) variance across
+ *        the batch.
+ *
+ * This layer computes Batch Normalization as described in [1]. For each channel
+ * in the data (i.e. axis 1), it subtracts the mean and divides by the variance,
+ * where both statistics are computed across both spatial dimensions and across
+ * the different examples in the batch.
+ *
+ * By default, during training time, the network is computing global
+ * mean/variance statistics via a running average, which is then used at test
+ * time to allow deterministic outputs for each input. You can manually toggle
+ * whether the network is accumulating or using the statistics via the
+ * use_global_stats option. For reference, these statistics are kept in the
+ * layer's three blobs: (0) mean, (1) variance, and (2) moving average factor.
+ *
+ * Note that the original paper also included a per-channel learned bias and
+ * scaling factor. To implement this in Caffe, define a `ScaleLayer` configured
+ * with `bias_term: true` after each `BatchNormLayer` to handle both the bias
+ * and scaling factor.
+ *
+ * [1] S. Ioffe and C. Szegedy, 'Batch Normalization: Accelerating Deep Network
+ *     Training by Reducing Internal Covariate Shift.' arXiv preprint
+ *     arXiv:1502.03167 (2015).
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class BatchNormLayer : public Layer<Dtype> {
+ public:
+  explicit BatchNormLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    
+    {}  // namespace caffe
+    
+    #endif  // CAFFE_CONTRASTIVE_LOSS_LAYER_HPP_
+
+    
+    namespace caffe {
+    }
+    
+    #ifdef USE_CUDNN
+template <typename Dtype>
+class CuDNNLRNLayer : public LRNLayer<Dtype> {
+ public:
+  explicit CuDNNLRNLayer(const LayerParameter& param)
+      : LRNLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNLRNLayer();
+    }
+    
+    
+    {}  // namespace caffe
+    
+    namespace mxnet {
+    }
+    
+    /*! \brief Cuda runtime compile module. */
+class CudaModule {
+ private:
+  /*! \brief Structure for holding internal info. */
+  struct Chunk {
+    /*!
+     * \brief Constructs cuda module.
+     * \param source cuda source code.
+     * \param exports export symbols before mangling.
+     */
+    Chunk(const char* source,
+          const std::vector<std::string>& options,
+          const std::vector<std::string>& exports);
+    /*! \brief deconstrutor */
+    ~Chunk();
+    /*!
+     * \brief Get handle to cuda kernel from loaded module
+     * \param mangled_name mangled kernel name
+     * \param ctx context to run kernel on
+     * \return loaded function handle
+     */
+    CUfunction GetFunction(const std::string& mangled_name, const Context& ctx);
+    /*! \brief nvrtc program handle. */
+    nvrtcProgram prog_;
+    /*! \brief compiled cuda PTX */
+    char* ptx_;
+    /*! \brief lazily loaded cuda module */
+    std::unordered_map<int, CUmodule> mod_;
+    /*! \brief exported names */
+    std::unordered_set<std::string> exports_;
+  };
+  /*! \brief pointer to Chunk */
+  std::shared_ptr<Chunk> ptr_;
     }
     
     /*!
- * \brief source function that generate output based on env
- *  The result container is pre-allocated with the correct shape.
- * \param env The Environment arguments.
- * \param ret The containter to store return value.
- * \param req The requirement to stroe the ret.
- * \param ctx Runtime context to execute the function.
- */
-typedef void (*SourceFunction)(const EnvArguments& env,
-                               TBlob* ret,
-                               OpReqType req,
-                               RunContext ctx);
-    
-    /**
- * \brief The interface to convert mxnet's tensor to caffe's blob
- * \brief called in caffe_operator_inl.h
- */
-template<typename Device, typename Dtype>
-void TBlob2CaffeBlob(caffeMemoryTypes memType,
-                     typename std::vector< ::caffe::Blob<Dtype>*>::iterator blob,
-                     typename std::vector<TBlob>::const_iterator tblob,
-                     int n = 1) {
-  for (int i = 0; i < n; ++i, ++blob, ++tblob) {
-    (*blob)->Reshape(TShape2Vector((*tblob).shape_));
-    SetDataGradToBlob<Device, Dtype>(memType, blob, tblob);
-  }
-}
-    
-    DMLC_REGISTER_PARAMETER(CaffeOpParam);
-    
-    
-    { private:
-  /*! \brief Concurrency for thread pool */
-  static constexpr std::size_t kNumWorkingThreads = 16;
-  /*! \brief Maximum number of GPUs */
-  static constexpr std::size_t kMaxNumGpus = 16;
-  /*!\brief number of streams allocated for each GPU */
-  static constexpr std::size_t kNumStreamsPerGpu = 16;
-  /*!
-   * \brief Streams.
-   */
-  std::unique_ptr<StreamManager<kMaxNumGpus, kNumStreamsPerGpu>> streams_;
-  /*!
-   * \brief Task queues.
-   */
-  std::shared_ptr<dmlc::ConcurrentBlockingQueue<OprBlock*>> task_queue_;
-  std::shared_ptr<dmlc::ConcurrentBlockingQueue<OprBlock*>> io_task_queue_;
-  /*!
-   * \brief Thread pools.
-   */
-  std::unique_ptr<ThreadPool> thread_pool_;
-  std::unique_ptr<ThreadPool> io_thread_pool_;
-  /*!
-   * \brief Worker.
-   * \param task_queue Queue to work on.
-   *
-   * The method to pass to thread pool to parallelize.
-   */
-  void ThreadWorker(std::shared_ptr<dmlc::ConcurrentBlockingQueue<OprBlock*>> task_queue,
-                    const std::shared_ptr<dmlc::ManualEvent>& ready_event) {
-    OprBlock* opr_block;
-    ready_event->signal();
-    while (task_queue->Pop(&opr_block)) {
-      DoExecute(opr_block);
-    }
-  }
-  /*!
-   * \brief Execute an operation.
-   * \param opr_block The operator block.
-   */
-  void DoExecute(OprBlock* opr_block) {
-    assert(opr_block->wait.load() == 0);
-    if (opr_block->ctx.dev_mask() == gpu::kDevMask) {
-      #if MXNET_USE_CUDA
-      CUDA_CALL(cudaSetDevice(opr_block->ctx.dev_id));
-      #else   // MXNET_USE_CUDA
-      LOG(FATAL) << 'Please compile with CUDA enabled';
-      #endif  // MXNET_USE_CUDA
-    }
-    bool is_copy = (opr_block->opr->prop == FnProperty::kCopyFromGPU ||
-                    opr_block->opr->prop == FnProperty::kCopyToGPU);
-    auto&& rctx = is_copy
-        ? streams_->GetIORunContext(opr_block->ctx)
-        : streams_->GetRunContext(opr_block->ctx);
-    this->ExecuteOprBlock(rctx, opr_block);
-  }
-  /*!
-   * \brief Push the operation to the queue.
-   * \param opr_block The operator block.
-   */
-  void DoPushToQueue(OprBlock* opr_block) {
-    switch (opr_block->opr->prop) {
-      case FnProperty::kCopyFromGPU:
-      case FnProperty::kCopyToGPU: {
-        io_task_queue_->Push(opr_block);
-        break;
-      }
-      default: {
-        task_queue_->Push(opr_block);
-        break;
-      }
-    }
-  }
-};
-    
-      inline TBlob AsIdxBlob(const dmlc::Row<uint64_t>& row) {
-    const uint64_t* ptr = row.index;
-    TShape shape(mshadow::Shape1(row.length));
-    return TBlob((int64_t*) ptr, shape, cpu::kDevMask, mshadow::kInt64);  // NOLINT(*)
-  }
-    
-    namespace HPHP { namespace HHBBC {
+ * Copyright (c) 2016 by Contributors
+ * \file caffe_op.cc
+ * \brief caffe operator
+ * \author Haoran Wang
+*/
+#include './caffe_op-inl.h'
+namespace mxnet {
+namespace op {
     }
     }
     
-    void Assembler::limmediate(const Reg64& rt, int64_t imm64,
-                           ImmType immt, bool immMayChange) {
-  static_assert(
-      std::is_unsigned<
-        decltype(HPHP::RuntimeOption::EvalPPC64MinTOCImmSize)>::value,
-      'RuntimeOption::EvalPPC64MinTOCImmSize is expected to be unsigned.');
-  always_assert(HPHP::RuntimeOption::EvalPPC64MinTOCImmSize <= 64);
-    }
-    
-      // Label variants
-    
-    inline APCLocalArray::APCLocalArray(const APCArray* source)
-  : ArrayData(kApcKind)
-  , m_arr(source)
-{
-  m_size = m_arr->size();
-  source->reference();
-  tl_heap->addApcArray(this);
-  memset(localCache(), static_cast<data_type_t>(KindOfUninit),
-         m_size * sizeof(TypedValue));
-  assertx(hasExactlyOneRef());
-}
-    
-    bool PlainDirectory::isValid() const {
-  return m_dir;
-}
-    
-    inline void ExecutionContext::write(const char* s) {
-  write(s, strlen(s));
-}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-      // implementing File
-  bool open(const String& filename, const String& mode) override;
-  bool close() override;
-  int64_t readImpl(char *buffer, int64_t length) override;
-  int getc() override;
-  int64_t writeImpl(const char *buffer, int64_t length) override;
-  bool seek(int64_t offset, int whence = SEEK_SET) override;
-  int64_t tell() override;
-  bool eof() override;
-  bool rewind() override;
-  bool flush() override;
-  bool truncate(int64_t size) override;
-    
-    /**
- * @brief A simple ConfigParserPlugin for feature vector dictionary keys.
- */
-class FeatureVectorsConfigParserPlugin : public ConfigParserPlugin {
- public:
-  std::vector<std::string> keys() const override;
-    }
-    
-    #include <osquery/config.h>
-#include <osquery/registry_factory.h>
-    
-    #include 'osquery/config/parsers/prometheus_targets.h'
-    
-      const auto& doc = c.getParser('options')->getData().doc()['options'];
-    
-          std::string old_query = '';
-      getDatabaseValue(kQueries, kConfigViews + name, old_query);
-      erase_views.erase(name);
-      if (old_query == query) {
-        continue;
-      }
+      // should be called outside the mutex
+  SuperVersion() = default;
+  ~SuperVersion();
+  SuperVersion* Ref();
+  // If Unref() returns true, Cleanup() should be called with mutex held
+  // before deleting this SuperVersion.
+  bool Unref();
     
     
-    {  // And of the column has constraints:
-  EXPECT_TRUE(cm['path'].notExistsOrMatches('some'));
-  EXPECT_FALSE(cm['path'].notExistsOrMatches('not_some'));
-  EXPECT_TRUE(cm['path'].exists());
-  EXPECT_TRUE(cm['path'].existsAndMatches('some'));
+    {  return Merge(key_slice, value_slice);
 }
     
     
-    {  // Similarly deregistering closes the handle.
-  EventFactory::deregisterEventPublisher('inotify');
-  EXPECT_FALSE(event_pub->isHandleOpen());
+    {  // Sleep just until `num_bytes` is allowed.
+  uint64_t sleep_amount =
+      static_cast<uint64_t>(num_bytes /
+                            static_cast<long double>(delayed_write_rate_) *
+                            kMicrosPerSecond) +
+      sleep_debt;
+  last_refill_time_ = time_now + sleep_amount;
+  return sleep_amount;
 }
     
-    struct Statistics {
-  std::string name_;
-  StatisticsFunc* compute_;
+    int main() {
+  Options options;
+  options.create_if_missing = true;
+  // Disable RocksDB background compaction.
+  options.compaction_style = kCompactionStyleNone;
+  // Small slowdown and stop trigger for experimental purpose.
+  options.level0_slowdown_writes_trigger = 3;
+  options.level0_stop_writes_trigger = 5;
+  options.IncreaseParallelism(5);
+  options.listeners.emplace_back(new FullCompactor(options));
     }
     
-    #include <cstdlib>
-#include <ostream>
-#include <cmath>
+      MyFilter filter;
     
+      // Load the options file.
+  DBOptions loaded_db_opt;
+  std::vector<ColumnFamilyDescriptor> loaded_cf_descs;
+  s = LoadLatestOptions(kDBPath, Env::Default(), &loaded_db_opt,
+                        &loaded_cf_descs);
+  assert(s.ok());
+  assert(loaded_db_opt.create_if_missing == db_opt.create_if_missing);
     
-    {}  // end namespace benchmark
+      // Put key-value
+  s = db->Put(WriteOptions(), 'key1', 'value');
+  assert(s.ok());
+  std::string value;
+  // get value
+  s = db->Get(ReadOptions(), 'key1', &value);
+  assert(s.ok());
+  assert(value == 'value');
     
-      if (!printed_header_) {
-    // save the names of all the user counters
-    for (const auto& run : reports) {
-      for (const auto& cnt : run.counters) {
-        user_counter_names_.insert(cnt.first);
-      }
+      // Following counters are only populated if
+  // options.report_bg_io_stats = true;
+    
+    // Supported only for Leveled compaction
+Status SuggestCompactRange(DB* db, ColumnFamilyHandle* column_family,
+                           const Slice* begin, const Slice* end);
+Status SuggestCompactRange(DB* db, const Slice* begin, const Slice* end);
+    
+    // A structure that describes the current status of a thread.
+// The status of active threads can be fetched using
+// rocksdb::GetThreadList().
+struct ThreadStatus {
+  // The type of a thread.
+  enum ThreadType : int {
+    HIGH_PRIORITY = 0,  // RocksDB BG thread in high-pri thread pool
+    LOW_PRIORITY,  // RocksDB BG thread in low-pri thread pool
+    USER,  // User thread (Non-RocksDB BG thread)
+    BOTTOM_PRIORITY,  // RocksDB BG thread in bottom-pri thread pool
+    NUM_THREAD_TYPES
+  };
     }
+    
+    namespace rocksdb {
     }
-    
-    inline bool Regex::Init(const std::string& spec, std::string* error) {
-#ifdef BENCHMARK_HAS_NO_EXCEPTIONS
-  ((void)error); // suppress unused warning
-#else
-  try {
-#endif
-    re_ = std::regex(spec, std::regex_constants::extended);
-    init_ = true;
-#ifndef BENCHMARK_HAS_NO_EXCEPTIONS
-  } catch (const std::regex_error& e) {
-    if (error) {
-      *error = e.what();
-    }
-  }
-#endif
-  return init_;
-}
-    
-    double BenchmarkReporter::Run::GetAdjustedCPUTime() const {
-  double new_time = cpu_accumulated_time * GetTimeUnitMultiplier(time_unit);
-  if (iterations != 0) new_time /= static_cast<double>(iterations);
-  return new_time;
-}
-    
-    
-    {void SleepForSeconds(double seconds) {
-  SleepForMicroseconds(static_cast<int>(seconds * kNumMicrosPerSecond));
-}
-#endif  // BENCHMARK_OS_WINDOWS
-}  // end namespace benchmark
