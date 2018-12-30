2d8066b315a9f7d065c188722e3932d7d79047dc
SUNsung <weatherius@gmail.com>
2018-12-30 06:56:18 +0200

    Rewrite [21fBybYV5pmhg21wtreWqRzb56214ukHM6ovc6w21IcP.rb] File in: 2018-12-30 06:56:17

--- ruby/21fBybYV5pmhg21wtreWqRzb56214ukHM6ovc6w21IcP.rb
+++ ruby/21fBybYV5pmhg21wtreWqRzb56214ukHM6ovc6w21IcP.rb
@@ -1,174 +1,190 @@
 
         
-        FORWARD_SLASH = '/'.freeze
+                default_connectors = {
+          words_connector: ', ',
+          two_words_connector: ' and ',
+          last_word_connector: ', and '
+        }
+        if defined?(I18n)
+          i18n_connectors = I18n.translate(:'support.array', locale: options[:locale], default: {})
+          default_connectors.merge!(i18n_connectors)
+        end
+        options = default_connectors.merge!(options)
     
-    STDOUT.sync = true
-    
-                  Jekyll.logger.info 'LiveReload address:',
-                                 'http://#{opts['host']}:#{opts['livereload_port']}'
-            end
+              def hidden_field_for_checkbox(options)
+            @unchecked_value ? tag('input', options.slice('name', 'disabled', 'form').merge!('type' => 'hidden', 'value' => @unchecked_value)) : ''.html_safe
           end
-          @thread.abort_on_exception = true
-        end
-    
-            # Mounts a shared folder.
-        #
-        # This method should create, mount, and properly set permissions
-        # on the shared folder. This method should also properly
-        # adhere to any configuration values such as `shared_folder_uid`
-        # on `config.vm`.
-        #
-        # @param [String] name The name of the shared folder.
-        # @param [String] guestpath The path on the machine which the user
-        #   wants the folder mounted.
-        # @param [Hash] options Additional options for the shared folder
-        #   which can be honored.
-        def mount_shared_folder(name, guestpath, options)
-          raise BaseError, _key: :unsupported_shared_folder
-        end
-    
-            # This is the method called to provision the system. This method
-        # is expected to do whatever necessary to provision the system (create files,
-        # SSH, etc.)
-        def provision!
-        end
-    
-            # This contains all the guests and their parents.
-        #
-        # @return [Registry<Symbol, Array<Class, Symbol>>]
-        attr_reader :guests
-    
-            # This returns any automatically detected errors.
-        #
-        # @return [Array<String>]
-        def _detected_errors
-          return [] if !@__invalid_methods || @__invalid_methods.empty?
-          return [I18n.t('vagrant.config.common.bad_field',
-                         fields: @__invalid_methods.to_a.sort.join(', '))]
-        end
-    
-        if self.junk_headers
-      while str.length < 4096
-        if self.fold
-          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}:\r\n\t#{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
-        else
-          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}: #{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
-        end
-      end
-    end
-    
-              # Encodes the realm field
-          #
-          # @return [String]
-          def encode_realm
-            encoded = ''
-            encoded << [realm.length].pack('N')
-            encoded << realm
-    
-              # Rex::Proto::Kerberos::Model::ApReq decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'AP-REQ decoding not supported'
-          end
-    
-              # @!attribute type
-          #   @return [Integer] The algorithm used to generate the checksum
-          attr_accessor :type
-          # @!attribute checksum
-          #   @return [String] The checksum itself
-          attr_accessor :checksum
-    
-              # Decodes the req_body from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::KdcRequestBody]
-          def decode_asn1_req_body(input)
-            Rex::Proto::Kerberos::Model::KdcRequestBody.decode(input.value[0])
-          end
-        end
       end
     end
   end
 end
 
     
-              # Decodes the Rex::Proto::Kerberos::Model::KrbError from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KrbError, invalid input'
-            end
-    
-            def initialize(argv)
-          @pod_name = argv.shift_argument
-          @short_output = argv.flag?('short')
-          super
-        end
-    
-    Given(/^I make (\d+) deployments$/) do |count|
-  step 'all linked files exists in shared path'
-    
-      def run_vagrant_command(command)
-    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
-    return [stdout, stderr] if status.success?
-    raise VagrantSSHCommandError, status
-  end
-end
-    
-        def dry_run
-      ['--dry-run', '-n',
-       'Do a dry run without executing actions',
-       lambda do |_value|
-         Configuration.env.set(:sshkit_backend, SSHKit::Backend::Printer)
-       end]
-    end
-    
-            def set(key, value)
-          pval = @properties[key]
-          if pval.is_a?(Hash) && value.is_a?(Hash)
-            pval.merge!(value)
-          elsif pval.is_a?(Set) && value.is_a?(Set)
-            pval.merge(value)
-          elsif pval.is_a?(Array) && value.is_a?(Array)
-            pval.concat value
-          else
-            @properties[key] = value
+            class RadioButtonBuilder < Builder # :nodoc:
+          def radio_button(extra_html_options = {})
+            html_options = extra_html_options.merge(@input_html_options)
+            html_options[:skip_default_ids] = false
+            @template_object.radio_button(@object_name, @method_name, @value, html_options)
           end
         end
     
-          def remember_location(key)
-        location = caller.find do |line|
-          IGNORED_LOCATIONS.none? { |i| line.include?(i) }
+        private
+    
+        # Direct access to template rendering.
+    def render_template(context, options) #:nodoc:
+      TemplateRenderer.new(@lookup_context).render(context, options)
+    end
+    
+    def custom_release_header_anchors(markdown)
+  header_regexp = %r!^(\d{1,2})\.(\d{1,2})\.(\d{1,2}) \/ \d{4}-\d{2}-\d{2}!
+  section_regexp = %r!^### \w+ \w+$!
+  markdown.split(%r!^##\s!).map do |release_notes|
+    _, major, minor, patch = *release_notes.match(header_regexp)
+    release_notes
+      .gsub(header_regexp, '\\0\n{: #v\\1-\\2-\\3}')
+      .gsub(section_regexp) { |section| '#{section}\n{: ##{slugify(section)}-v#{major}-#{minor}-#{patch}}' }
+  end.join('\n## ')
+end
+    
+    # No trailing slash
+Benchmark.ips do |x|
+  path = '/some/very/very/long/path/to/a/file/i/like'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+    
+    DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
+    
+    require 'jekyll'
+require 'mercenary'
+    
+          #
+    
+                  # Notify blocked threads that EventMachine has started or shutdown
+              EM.schedule { @started_event.set }
+              EM.add_shutdown_hook { @stopped_event.set }
+    
+                # stream_file_data would free us from keeping livereload.js in memory
+            # but JRuby blocks on that call and never returns
+            send_data(reload_body)
+            close_connection_after_writing
+          else
+            body = 'This port only serves livereload.js over HTTP.\n'
+            headers = [
+              'HTTP/1.1 400 Bad Request',
+              'Content-Type: text/plain',
+              'Content-Length: #{body.bytesize}',
+              '',
+              '',
+            ].join('\r\n')
+            send_data(headers)
+            send_data(body)
+            close_connection_after_writing
+          end
         end
-        (locations[key] ||= []) << location
+        # rubocop:enable Metrics/MethodLength
       end
+    end
+  end
+end
+
     
-            def show
-          authorize! :read, @order, order_token
-          @address = find_address
-          respond_with(@address)
+        self.store(key, value) if (stored == false)
+    self.dcase_hash[key.downcase] = value
+  end
+    
+    IAX_IE_CALLED_NUMBER  = 1
+IAX_IE_CALLING_NUMBER = 2
+IAX_IE_AUTH_METHODS   = 3
+IAX_IE_CALLING_NAME   = 4
+IAX_IE_USERNAME       = 6
+IAX_IE_DESIRED_CODEC  = 9
+IAX_IE_ORIGINAL_DID   = 10
+IAX_IE_ACTUAL_CODECS  = 8
+IAX_IE_PROTO_VERSION  = 11
+IAX_IE_REG_REFRESH    = 19
+IAX_IE_CHALLENGE_DATA = 15
+IAX_IE_CHALLENGE_RESP = 16
+IAX_IE_APPARENT_ADDR  = 18
+IAX_IE_REGREJ_CAUSE   = 22
+IAX_IE_HANGUP_CAUSE   = 42
+    
+      #
+  # Payload types were identified from xCAT-server source code (IPMI.pm)
+  #
+  PAYLOAD_IPMI = 0
+  PAYLOAD_SOL  = 1
+  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
+  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
+  PAYLOAD_RAKP1 = 0x12
+  PAYLOAD_RAKP2 = 0x13
+  PAYLOAD_RAKP3 = 0x14
+  PAYLOAD_RAKP4 = 0x15
+    
+              private
+    
+              # Encodes the Rex::Proto::Kerberos::Model::Element into an ASN.1 String. This
+          # method has been designed to be overridden by subclasses.
+          #
+          # @raise [NoMethodError]
+          def encode
+            raise ::NoMethodError, 'Method designed to be overridden'
+          end
         end
+      end
+    end
+  end
+end
     
-            def new; end
+              # Decodes the msg_type from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_asn1_msg_type(input)
+            input.value[0].value.to_i
+          end
     
-            def find_property
-          @property = Spree::Property.accessible_by(current_ability, :read).find(params[:id])
-        rescue ActiveRecord::RecordNotFound
-          @property = Spree::Property.accessible_by(current_ability, :read).find_by!(name: params[:id])
+              # Decodes a Rex::Proto::Kerberos::Model::KdcRequestBody from an
+          # OpenSSL::ASN1::Sequence
+          #
+          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode_asn1(input)
+            seq_values = input.value
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
         end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
     
-            def create
-          @order = Spree::Order.find_by!(number: params.fetch(:shipment).fetch(:order_id))
-          authorize! :read, @order
-          authorize! :create, Shipment
-          quantity = params[:quantity].to_i
-          @shipment = @order.shipments.create(stock_location_id: params.fetch(:stock_location_id))
+    ERR
+      end
+    end
+    
+    end
