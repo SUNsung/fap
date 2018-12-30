0c5e4a8d8b26b0d532d8742df42effd44a7f8f63
SUNsung <weatherius@gmail.com>
2018-12-30 12:10:59 +0200

    Rewrite [564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb] File in: 2018-12-30 12:10:58

--- ruby/564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb
+++ ruby/564AfeZTWtolg56HMofe.vU0cs563DMHZ8f7Xe656GZ1.rb
@@ -1,100 +1,100 @@
 
         
-              def initialize(options = {})
-        if options[:uri] && options[:token]
-          @api = ::Fogbugz::Interface.new(options)
-        elsif options[:uri] && options[:email] && options[:password]
-          @api = ::Fogbugz::Interface.new(options)
-          @api.authenticate
-          @api
-        end
-      end
-    
-          def id
-        raw_data['ixProject']
-      end
-    
-          def initialize(raw_change)
-        if raw_change.is_a?(Gitaly::GetRawChangesResponse::RawChange)
-          @blob_id = raw_change.blob_id
-          @blob_size = raw_change.size
-          @old_path = raw_change.old_path.presence
-          @new_path = raw_change.new_path.presence
-          @operation = raw_change.operation&.downcase || :unknown
-        else
-          parse(raw_change)
-        end
-      end
-    
-    class PolymorphicMentions < ActiveRecord::Migration[4.2]
-  def change
-    remove_index :mentions, column: %i(post_id)
-    remove_index :mentions, column: %i(person_id post_id), unique: true
-    rename_column :mentions, :post_id, :mentions_container_id
-    add_column :mentions, :mentions_container_type, :string
-    add_index :mentions,
-              %i(mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_mc_id_and_mc_type',
-              length: {mentions_container_type: 191}
-    add_index :mentions,
-              %i(person_id mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_person_and_mc_id_and_mc_type',
-              length: {mentions_container_type: 191},
-              unique: true
-    
-      class PostToService < Base
-    def perform(*_args)
-      # don't post to services in cucumber
-    end
-  end
-    
-      # fill change password section on the user edit page
-  def fill_change_password_section(cur_pass, new_pass, confirm_pass)
-    fill_in 'user_current_password', :with => cur_pass
-    fill_in 'user_password', :with => new_pass
-    fill_in 'user_password_confirmation', :with => confirm_pass
-  end
-    
-            def address_params
-          params.require(:address).permit(permitted_address_attributes)
-        end
-    
-            def create
-          authorize! :create, Image
-          @image = scope.images.new(image_params)
-          if @image.save
-            respond_with(@image, status: 201, default_template: :show)
-          else
-            invalid_resource!(@image)
+                  def hidden_field_for_checkbox(options)
+            @unchecked_value ? tag('input', options.slice('name', 'disabled', 'form').merge!('type' => 'hidden', 'value' => @unchecked_value)) : ''.html_safe
           end
-        end
-    
-            def find_property
-          @property = Spree::Property.accessible_by(current_ability, :read).find(params[:id])
-        rescue ActiveRecord::RecordNotFound
-          @property = Spree::Property.accessible_by(current_ability, :read).find_by!(name: params[:id])
-        end
-    
-            def scope
-          if params[:country_id]
-            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
-            @country.states.accessible_by(current_ability, :read).order('name ASC')
-          else
-            State.accessible_by(current_ability, :read).order('name ASC')
-          end
-        end
       end
     end
   end
 end
 
     
-            def create
-          authorize! :create, StockMovement
-          @stock_movement = scope.new(stock_movement_params)
-          if @stock_movement.save
-            respond_with(@stock_movement, status: 201, default_template: :show)
-          else
-            invalid_resource!(@stock_movement)
-          end
+        initializer 'action_view.collection_caching', after: 'action_controller.set_configs' do |app|
+      PartialRenderer.collection_cache = app.config.action_controller.cache_store
+    end
+    
+          def self.cache_key_for(raw_key)
+        '#{Redis::Cache::CACHE_NAMESPACE}:#{raw_key}'
+      end
+    end
+  end
+end
+
+    
+          # Returns the details of a GitHub user.
+      #
+      # username - The username of the user.
+      def user(username)
+        with_rate_limit { octokit.user(username) }
+      end
+    
+            def representation_class
+          Representation::DiffNote
         end
+    
+    module Gitlab
+  module GithubImport
+    module Importer
+      class IssuesImporter
+        include ParallelScheduling
+    
+            def sidekiq_worker_class
+          ImportLfsObjectWorker
+        end
+    
+    module Gitlab
+  module GithubImport
+    # IssuableFinder can be used for caching and retrieving database IDs for
+    # issuable objects such as issues and pull requests. By caching these IDs we
+    # remove the need for running a lot of database queries when importing
+    # GitHub projects.
+    class IssuableFinder
+      attr_reader :project, :object
+    
+        context 'running workers' do
+      before do
+        AgentRunner.class_variable_set(:@@agents, [HuginnScheduler, DelayedJobWorker])
+        stub.instance_of(HuginnScheduler).setup
+        stub.instance_of(DelayedJobWorker).setup
+      end
+    
+        it 'has a default when the result is empty' do
+      expect(AgentsExporter.new(:name => '').filename).to eq('exported-agents.json')
+      expect(AgentsExporter.new(:name => 'Ə').filename).to eq('exported-agents.json')
+      expect(AgentsExporter.new(:name => '-').filename).to eq('exported-agents.json')
+      expect(AgentsExporter.new(:name => ',,').filename).to eq('exported-agents.json')
+    end
+  end
+    
+      describe 'down' do
+    let :valid_options do
+      super().merge('extract' => new_extract,
+                    'template' => new_template)
+    end
+    
+      it 'truncates message to a reasonable length' do
+    log = AgentLog.new(:agent => agents(:jane_website_agent), :level => 3)
+    log.message = 'a' * 11_000
+    log.save!
+    expect(log.message.length).to eq(10_000)
+  end
+    
+      before_action :set_account
+  before_action :set_statuses
+    
+          redirect_to admin_account_path(@account.id), notice: I18n.t('admin.accounts.change_email.changed_msg')
+    end
+    
+    module Admin
+  class CustomEmojisController < BaseController
+    before_action :set_custom_emoji, except: [:index, :new, :create]
+    before_action :set_filter_params
+    
+      def show
+    if subscription.valid?(params['hub.topic'])
+      @account.update(subscription_expires_at: future_expires)
+      render plain: encoded_challenge, status: 200
+    else
+      head 404
+    end
+  end
