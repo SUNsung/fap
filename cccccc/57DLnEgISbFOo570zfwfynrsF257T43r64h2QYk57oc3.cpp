2d1a00706011673e08cc13570bc53060d0a498d8
SUNsung <weatherius@gmail.com>
2018-12-30 20:07:56 +0200

    Rewrite [57DLnEgISbFOo570zfwfynrsF257T43r64h2QYk57oc3.cpp] File in: 2018-12-30 20:07:56

--- cccccc/57DLnEgISbFOo570zfwfynrsF257T43r64h2QYk57oc3.cpp
+++ cccccc/57DLnEgISbFOo570zfwfynrsF257T43r64h2QYk57oc3.cpp
@@ -1,287 +1,107 @@
-static const char* kModuleName = '_api_implementation';
-static const char kModuleDocstring[] =
-'_api_implementation is a module that exposes compile-time constants that\n'
-'determine the default API implementation to use for Python proto2.\n'
-'\n'
-'It complements api_implementation.py by setting defaults using compile-time\n'
-'constants defined in C, such that one can set defaults at compilation\n'
-'(e.g. with blaze flag --copt=-DPYTHON_PROTO2_CPP_IMPL_V2).';
-    
-    // Author: kenton@google.com (Kenton Varda)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-    
-    string EscapeJavadoc(const string& input) {
-  string result;
-  result.reserve(input.size() * 2);
-    }
-    
-    #include <google/protobuf/compiler/java/java_doc_comment.h>
-    
-    int ImmutableExtensionLiteGenerator::GenerateNonNestedInitializationCode(
-    io::Printer* printer) {
-  return 0;
-}
-    
-    // Check whether there is any type defined in the proto file that has
-// the given class name.
-bool ClassNameResolver::HasConflictingClassName(
-    const FileDescriptor* file, const string& classname) {
-  for (int i = 0; i < file->enum_type_count(); i++) {
-    if (file->enum_type(i)->name() == classname) {
-      return true;
-    }
-  }
-  for (int i = 0; i < file->service_count(); i++) {
-    if (file->service(i)->name() == classname) {
-      return true;
-    }
-  }
-  for (int i = 0; i < file->message_type_count(); i++) {
-    if (MessageHasConflictingClassName(file->message_type(i), classname)) {
-      return true;
-    }
-  }
-  return false;
-}
-    
-    // Checks if a message contains any enums definitions (on the message or
-// a nested message under it).
-bool MessageContainsEnums(const Descriptor* message) {
-  if (message->enum_type_count() > 0) {
-    return true;
-  }
-  for (int i = 0; i < message->nested_type_count(); i++) {
-    if (MessageContainsEnums(message->nested_type(i))) {
-      return true;
-    }
-  }
-  return false;
-}
-    
-    #include <string>
-#include <utility>
-#include <vector>
-    
-    #define ERROR_RAISE_WARNING(exp)        \
-  int ret = (exp);                      \
-  if (ret != 0) {                       \
-    raise_warning(                      \
-      '%s(): %s',                       \
-      __FUNCTION__,                     \
-      folly::errnoStr(errno).c_str()    \
-    );                                  \
-  }                                     \
-    
-    
-    {  StructuredLogEntry cols;
-  fillCols(cols);
-  cols.setStr('event_name', event);
-  cols.setInt('priority', priority);
-  StructuredLog::log('hhvm_perf_warning', cols);
-}
-    
-        void LearnerNesterov::UpdateHalf(const Parameter& parameter, const NDArrayViewPtr& gradientValue,
-        const NDArrayViewPtr& smoothedGradientValue, size_t trainingSampleCount) const
-    {
-        const auto& compoundMatrix = GetWritableMatrix<float>(smoothedGradientValue);
-        const auto& gradientMatrix = GetWritableMatrix<half>(gradientValue);
-        auto smoothedGradientMatrix = compoundMatrix->ColumnSlice(0, gradientMatrix->GetNumCols());
-        auto tempGradientMatrix = compoundMatrix->ColumnSlice(gradientMatrix->GetNumCols(), gradientMatrix->GetNumCols());
-        auto parameterMatrix = compoundMatrix->ColumnSlice(2 * gradientMatrix->GetNumCols(), gradientMatrix->GetNumCols());
-    }
-    
-            static bool IsUDF(const Dictionary& dict);
-    
-    #ifdef WIN32 // --- Windows version
-    
-        virtual void /*ComputationNodeNonLooping::*/ ForwardPropNonLooping() override
-    {
-        // Inputs:
-        //      0. gain
-        //      1. pred
-        //      2. query id
-        FrameRange fr(Input(0)->GetMBLayout());
-    }
-    
-      Status s;
-  for (const auto& p : paths_) {
-    s = carve.carve(fs::path(p));
-    EXPECT_TRUE(s.ok());
-  }
-    
-      /**
-   * @brief Virtual method which could implement custom query pack retrieval
-   *
-   * The default config syntax for query packs is like the following:
-   *
-   * @code
-   *   {
-   *     'packs': {
-   *       'foo': {
-   *         'version': '1.5.0',
-   *         'platform:' 'any',
-   *         'queries': {
-   *           // ...
-   *         }
-   *       }
-   *     }
-   *   }
-   * @endcode
-   *
-   * Alternatively, you can define packs like the following as well:
-   *
-   * @code
-   *   {
-   *     'packs': {
-   *       'foo': '/var/osquery/packs/foo.json',
-   *       'bar': '/var/osquery/packs/bar.json'
-   *     }
-   *   }
-   * @endcode
-   *
-   * If you defined the 'value' of your pack as a string instead of an inline
-   * data structure, then osquery will pass the responsibility of retrieving
-   * the pack to the active config plugin. In the above example, it seems
-   * obvious that the value is a local file path. Alternatively, if the
-   * filesystem config plugin wasn't being used, the string could be a remote
-   * URL, etc.
-   *
-   * genPack is not a pure virtual, so you don't have to define it if you don't
-   * want to use the shortened query pack syntax. The default implementation
-   * returns a failed status.
-   *
-   * @param name is the name of the query pack
-   * @param value is the string based value that was provided with the pack
-   * @param pack should be populated with the string JSON pack content
-   *
-   * @return a Status instance indicating the success or failure of the call
-   */
-  virtual Status genPack(const std::string& name,
-                         const std::string& value,
-                         std::string& pack);
-    
-    namespace osquery {
-    }
-    
-    REGISTER_INTERNAL(LoggerConfigParserPlugin, 'config_parser', 'logger');
-    
-    #include <iostream>
-    
-      ASSERT_TRUE(data.doc().HasMember('events'));
-  ASSERT_TRUE(data.doc()['events'].HasMember('environment_variables'));
-  ASSERT_TRUE(data.doc()['events']['environment_variables'].IsArray());
-  for (const auto& var :
-       data.doc()['events']['environment_variables'].GetArray()) {
-    std::string value = var.GetString();
-    EXPECT_TRUE(value == 'foo' || value == 'bar');
-  }
-    
-    #include <osquery/config/config.h>
-#include <osquery/registry.h>
-#include <osquery/system.h>
-    
-    
-    {
-    {
-    {
-    {
-    {  std::vector<CanFrame> frames;
-  int32_t num = 0;
-  CanFrame frame;
-  // frame.id = 0x60;
-  // frame.len = 8;
-  // frame.data[0] = 0;
-  // frames.push_back(frame);
-  // num = 1;
-  EXPECT_EQ(hermes_can.Start(), ErrorCode::OK);
-  EXPECT_EQ(hermes_can.Receive(&frames, &num), ErrorCode::OK);
-}
-*/
-}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-    
-      ProtocolData<::apollo::canbus::ChassisDetail> mpd;
-  SenderMessage<::apollo::canbus::ChassisDetail> msg(1, &mpd);
-  EXPECT_FALSE(sender.NeedSend(msg, 1));
-  EXPECT_EQ(msg.message_id(), 1);
-  int32_t period = msg.curr_period();
-  msg.UpdateCurrPeriod(-50);
-  EXPECT_EQ(msg.curr_period(), period + 50);
-  EXPECT_EQ(msg.CanFrame().id, 1);
-    
-    #include 'modules/common/time/time.h'
-#include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-#include 'modules/drivers/radar/conti_radar/protocol/const_vars.h'
-    
-      x <<= 3;
-  x |= t;
-    
-    
-    {
-    {
-    {}  // namespace msf
-}  // namespace localization
-}  // namespace apollo
 
+        
+        class PlatformStyle;
+class WalletModel;
     
-    static int get4BytesValue(const std::vector<uint8_t>& v, size_t& pos) {
-  pos += 4;
-  return folly::Endian::little(folly::loadUnaligned<int>(v.data() + pos - 4));
+    
+    {    /* We rely on the fact that the Jacobi symbol of 1 / a->z^3 is the same as
+     * that of a->z. Thus a->y / a->z^3 is a quadratic residue iff a->y * a->z
+       is */
+    secp256k1_fe_mul(&yz, &a->y, &a->z);
+    return secp256k1_fe_is_quad_var(&yz);
 }
     
-    exception_wrapper exception_wrapper::from_exception_ptr(
-    std::exception_ptr const& ptr) noexcept {
-  if (!ptr) {
-    return exception_wrapper();
-  }
-  try {
-    std::rethrow_exception(ptr);
-  } catch (std::exception& e) {
-    return exception_wrapper(std::current_exception(), e);
-  } catch (...) {
-    return exception_wrapper(std::current_exception());
-  }
+    static void secp256k1_sha256_finalize(secp256k1_sha256_t *hash, unsigned char *out32) {
+    static const unsigned char pad[64] = {0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    uint32_t sizedesc[2];
+    uint32_t out[8];
+    int i = 0;
+    sizedesc[0] = BE32(hash->bytes >> 29);
+    sizedesc[1] = BE32(hash->bytes << 3);
+    secp256k1_sha256_write(hash, pad, 1 + ((119 - (hash->bytes % 64)) % 64));
+    secp256k1_sha256_write(hash, (const unsigned char*)sizedesc, 8);
+    for (i = 0; i < 8; i++) {
+        out[i] = BE32(hash->s[i]);
+        hash->s[i] = 0;
+    }
+    memcpy(out32, (const unsigned char*)out, 32);
 }
     
-    inline UriTuple as_tuple(const folly::Uri& k) {
-  return UriTuple(
-      k.scheme(),
-      k.username(),
-      k.password(),
-      k.host(),
-      k.port(),
-      k.path(),
-      k.query(),
-      k.fragment());
-}
-    
-      VirtualExecutor(const VirtualExecutor&) = delete;
-  VirtualExecutor& operator=(const VirtualExecutor&) = delete;
-    
-      // All of our boundary conditions below assume time_t is int64_t.
-  // This is true on most modern platforms.
-  if (!std::is_same<decltype(ts.tv_sec), int64_t>::value) {
-    LOG(INFO) << 'skipping most overflow tests: time_t is not int64_t';
-  } else {
-    // Test the upper boundary of conversion to uint64_t nanoseconds
-    using nsec_u64 = std::chrono::duration<uint64_t, std::nano>;
-    ts.tv_sec = 18446744073;
-    ts.tv_nsec = 709551615;
-    EXPECT_EQ(std::numeric_limits<uint64_t>::max(), to<nsec_u64>(ts).count());
+    int secp256k1_ecdh(const secp256k1_context* ctx, unsigned char *result, const secp256k1_pubkey *point, const unsigned char *scalar) {
+    int ret = 0;
+    int overflow = 0;
+    secp256k1_gej res;
+    secp256k1_ge pt;
+    secp256k1_scalar s;
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(result != NULL);
+    ARG_CHECK(point != NULL);
+    ARG_CHECK(scalar != NULL);
     }
     
-      void reset(const std::shared_ptr<T>& p = nullptr) {
-    auto newslots = folly::make_unique<Slots>();
-    // Allocate each Holder in a different CoreRawAllocator stripe to
-    // prevent false sharing. Their control blocks will be adjacent
-    // thanks to allocate_shared().
-    for (auto slot : folly::enumerate(newslots->slots_)) {
-      auto alloc = getCoreAllocator<Holder, kNumSlots>(slot.index);
-      auto holder = std::allocate_shared<Holder>(alloc, p);
-      *slot = std::shared_ptr<T>(holder, p.get());
+    
+    {    /* On the first run, return 0 to force a second run */
+    if (counter == 0) {
+        memset(nonce32, 0, 32);
+        return 1;
     }
+    /* On the second run, return an overflow to force a third run */
+    if (counter == 1) {
+        memset(nonce32, 0xff, 32);
+        return 1;
+    }
+    /* On the next run, return a valid nonce, but flip a coin as to whether or not to fail signing. */
+    memset(nonce32, 1, 32);
+    return secp256k1_rand_bits(1);
+}
+    
+      // Computes matrix.vector v = Wu.
+  // u is of size W.dim2() - 1 and the output v is of size W.dim1().
+  // u is imagined to have an extra element at the end with value 1, to
+  // implement the bias, but it doesn't actually have it.
+  // Computes the base C++ implementation, if there are no partial_funcs_.
+  // NOTE: The size of the input vector (u) must be padded using
+  // RoundInputs above.
+  // The input will be over-read to the extent of the padding. There are no
+  // alignment requirements.
+  void MatrixDotVector(const GENERIC_2D_ARRAY<int8_t>& w,
+                       const GenericVector<double>& scales, const int8_t* u,
+                       double* v) const;
+    
+    // DeathTest is a class that hides much of the complexity of the
+// GTEST_DEATH_TEST_ macro.  It is abstract; its static Create method
+// returns a concrete class that depends on the prevailing death test
+// style, as defined by the --gtest_death_test_style and/or
+// --gtest_internal_run_death_test flags.
+    
+    
+    {  const ParamGenerator<T1> g1_;
+  const ParamGenerator<T2> g2_;
+  const ParamGenerator<T3> g3_;
+  const ParamGenerator<T4> g4_;
+  const ParamGenerator<T5> g5_;
+  const ParamGenerator<T6> g6_;
+};  // class CartesianProductGenerator6
+    
+      virtual int GetNextPrime(int p) const {
+    for (int n = p + 1; n > 0; n++) {
+      if (IsPrime(n)) return n;
+    }
+    }
+    
+    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+int Factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
+    }
+    
+    // Tests factorial of negative numbers.
+TEST(FactorialTest, Negative) {
+  // This test is named 'Negative', and belongs to the 'FactorialTest'
+  // test case.
+  EXPECT_EQ(1, Factorial(-5));
+  EXPECT_EQ(1, Factorial(-1));
+  EXPECT_GT(Factorial(-10), 0);
     }
