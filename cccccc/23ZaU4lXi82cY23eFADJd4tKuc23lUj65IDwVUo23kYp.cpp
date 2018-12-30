0a01b253351fa857156dc281d12c007f3f23b315
SUNsung <weatherius@gmail.com>
2018-12-30 06:34:04 +0200

    Rewrite [23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp] File in: 2018-12-30 06:34:03

--- cccccc/23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp
+++ cccccc/23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp
@@ -1,365 +1,150 @@
 
         
-        #include 'tesseractclass.h'
-#ifdef _OPENMP
-#include <omp.h>
-#endif  // _OPENMP
-    
-    
-    {  for (std::map<int, ParamContent*>::iterator iter = vcMap.begin();
-                                          iter != vcMap.end();
-                                          ++iter) {
-    ParamContent* cur = iter->second;
-    if (!changes_only || cur->HasChanged()) {
-      fprintf(fp, '%-25s   %-12s   # %s\n',
-              cur->GetName(), cur->GetValue().string(), cur->GetDescription());
-    }
+        bool swift::isPlatformActive(PlatformKind Platform, LangOptions &LangOpts) {
+  if (Platform == PlatformKind::none)
+    return true;
+  
+  if (Platform == PlatformKind::OSXApplicationExtension ||
+      Platform == PlatformKind::iOSApplicationExtension)
+    if (!LangOpts.EnableAppExtensionRestrictions)
+      return false;
+  
+  // FIXME: This is an awful way to get the current OS.
+  switch (Platform) {
+    case PlatformKind::OSX:
+    case PlatformKind::OSXApplicationExtension:
+      return LangOpts.Target.isMacOSX();
+    case PlatformKind::iOS:
+    case PlatformKind::iOSApplicationExtension:
+      return LangOpts.Target.isiOS() && !LangOpts.Target.isTvOS();
+    case PlatformKind::tvOS:
+    case PlatformKind::tvOSApplicationExtension:
+      return LangOpts.Target.isTvOS();
+    case PlatformKind::watchOS:
+    case PlatformKind::watchOSApplicationExtension:
+      return LangOpts.Target.isWatchOS();
+    case PlatformKind::none:
+      llvm_unreachable('handled above');
   }
-  fclose(fp);
+  llvm_unreachable('bad PlatformKind');
 }
-#endif // GRAPHICS_DISABLED
+    
+    bool swift::canBeArgumentLabel(StringRef identifier) {
+  return llvm::StringSwitch<bool>(identifier)
+    .Case('var', false)
+    .Case('let', false)
+    .Case('inout', false)
+    .Case('$', false)
+    .Default(true);
+}
+    
+    #endif // SWIFT_IMPORTER_CFTYPEINFO_H
 
     
-    struct Pix;
-    
-    // Changes the box at the given index to the new box.
-// Recomputes the bounding box.
-void BoxWord::ChangeBox(int index, const TBOX& box) {
-  boxes_[index] = box;
-  ComputeBoundingBox();
+    static SourceLoc findEndOfLine(SourceManager &SM, SourceLoc loc,
+                               unsigned bufferID) {
+  CharSourceRange entireBuffer = SM.getRangeForBuffer(bufferID);
+  CharSourceRange rangeFromLoc{SM, loc, entireBuffer.getEnd()};
+  StringRef textFromLoc = SM.extractText(rangeFromLoc);
+  size_t newlineOffset = textFromLoc.find_first_of({'\r\n\0', 3});
+  if (newlineOffset == StringRef::npos)
+    return entireBuffer.getEnd();
+  return loc.getAdvancedLoc(newlineOffset);
 }
     
-    #if !UCONFIG_NO_TRANSLITERATION
-    
-    int32_t ScriptSet::countMembers() const {
-    // This bit counter is good for sparse numbers of '1's, which is
-    //  very much the case that we will usually have.
-    int32_t count = 0;
-    for (uint32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
-        uint32_t x = bits[i];
-        while (x > 0) {
-            count++;
-            x &= (x - 1);    // and off the least significant one bit.
-        }
-    }
-    return count;
-}
-    
-    #define LOW_A             ((UChar)0x0061)
-#define LOW_B             ((UChar)0x0062)
-#define LOW_C             ((UChar)0x0063)
-#define LOW_D             ((UChar)0x0064)
-#define LOW_E             ((UChar)0x0065)
-#define LOW_F             ((UChar)0x0066)
-#define LOW_G             ((UChar)0x0067)
-#define LOW_H             ((UChar)0x0068)
-#define LOW_I             ((UChar)0x0069)
-#define LOW_J             ((UChar)0x006a)
-#define LOW_K             ((UChar)0x006B)
-#define LOW_L             ((UChar)0x006C)
-#define LOW_M             ((UChar)0x006D)
-#define LOW_N             ((UChar)0x006E)
-#define LOW_O             ((UChar)0x006F)
-#define LOW_P             ((UChar)0x0070)
-#define LOW_Q             ((UChar)0x0071)
-#define LOW_R             ((UChar)0x0072)
-#define LOW_S             ((UChar)0x0073)
-#define LOW_T             ((UChar)0x0074)
-#define LOW_U             ((UChar)0x0075)
-#define LOW_V             ((UChar)0x0076)
-#define LOW_W             ((UChar)0x0077)
-#define LOW_X             ((UChar)0x0078)
-#define LOW_Y             ((UChar)0x0079)
-#define LOW_Z             ((UChar)0x007A)
-    
-    
-    {    BreakIterator *get() const { return ptr; }
-    BreakIterator *operator->() const { return ptr; }
-    BreakIterator &operator*() const { return *ptr; }
-private:
-    BreakIterator *ptr;
-    SharedBreakIterator(const SharedBreakIterator &);
-    SharedBreakIterator &operator=(const SharedBreakIterator &);
-};
-    
-    #ifndef __SHARED_CALENDAR_H__
-#define __SHARED_CALENDAR_H__
-    
-    // -------------------------------------
-    
-    CollationKey::CollationKey(const CollationKey& other)
-    : UObject(other), fFlagAndLength(other.getLength()),
-      fHashCode(other.fHashCode)
-{
-    if (other.isBogus())
-    {
-        setToBogus();
-        return;
-    }
-    }
-    
-        /**
-     * @return the lowercase CLDR keyword string for the plural form
-     */
-    static const char *getKeyword(Form p);
-    
-        /**
-     * Implement UnicodeMatcher
-     * @param text the text to be matched
-     * @param offset on input, the index into text at which to begin
-     * matching.  On output, the limit of the matched text.  The
-     * number of matched characters is the output value of offset
-     * minus the input value.  Offset should always point to the
-     * HIGH SURROGATE (leading code unit) of a pair of surrogates,
-     * both on entry and upon return.
-     * @param limit the limit index of text to be matched.  Greater
-     * than offset for a forward direction match, less than offset for
-     * a backward direction match.  The last character to be
-     * considered for matching will be text.charAt(limit-1) in the
-     * forward direction or text.charAt(limit+1) in the backward
-     * direction.
-     * @param incremental  if TRUE, then assume further characters may
-     * be inserted at limit and check for partial matching.  Otherwise
-     * assume the text as given is complete.
-     * @return a match degree value indicating a full match, a partial
-     * match, or a mismatch.  If incremental is FALSE then
-     * U_PARTIAL_MATCH should never be returned.
-     */
-    virtual UMatchDegree matches(const Replaceable& text,
-                                 int32_t& offset,
-                                 int32_t limit,
-                                 UBool incremental);
-    
-      /*!
-   * \brief determines whether updater has enough knowledge about a given dataset
-   *        to quickly update prediction cache its training data and performs the
-   *        update if possible.
-   * \param data: data matrix
-   * \param out_preds: prediction cache to be updated
-   * \return boolean indicating whether updater has capability to update
-   *         the prediction cache. If true, the prediction cache will have been
-   *         updated by the time this function returns.
-   */
-  virtual bool UpdatePredictionCache(const DMatrix* data,
-                                     HostDeviceVector<bst_float>* out_preds) {
-    return false;
-  }
-    
-    template<typename IndexType>
-class DensifyParser : public dmlc::Parser<IndexType> {
- public:
-  DensifyParser(dmlc::Parser<IndexType>* parser, uint32_t num_col)
-      : parser_(parser), num_col_(num_col) {
-  }
-    }
-    
-    #if DMLC_ENABLE_STD_THREAD
-/*!
- * \brief A threaded writer to write sparse batch page to sharded files.
- */
-class SparsePageWriter {
- public:
-  /*!
-   * \brief constructor
-   * \param name_shards name of shard files.
-   * \param format_shards format of each shard.
-   * \param extra_buffer_capacity Extra buffer capacity before block.
-   */
-  explicit SparsePageWriter(
-      const std::vector<std::string>& name_shards,
-      const std::vector<std::string>& format_shards,
-      size_t extra_buffer_capacity);
-  /*! \brief destructor, will close the files automatically */
-  ~SparsePageWriter();
-  /*!
-   * \brief Push a write job to the writer.
-   * This function won't block,
-   * writing is done by another thread inside writer.
-   * \param page The page to be written
-   */
-  void PushWrite(std::shared_ptr<SparsePage>&& page);
-  /*!
-   * \brief Allocate a page to store results.
-   *  This function can block when the writer is too slow and buffer pages
-   *  have not yet been recycled.
-   * \param out_page Used to store the allocated pages.
-   */
-  void Alloc(std::shared_ptr<SparsePage>* out_page);
-    }
-    
-    SEXP XGDMatrixGetInfo_R(SEXP handle, SEXP field) {
-  SEXP ret;
-  R_API_BEGIN();
-  bst_ulong olen;
-  const float *res;
-  CHECK_CALL(XGDMatrixGetFloatInfo(R_ExternalPtrAddr(handle),
-                                   CHAR(asChar(field)),
-                                 &olen,
-                                 &res));
-  ret = PROTECT(allocVector(REALSXP, olen));
-  for (size_t i = 0; i < olen; ++i) {
-    REAL(ret)[i] = res[i];
-  }
-  R_API_END();
-  UNPROTECT(1);
+    vector<detail::BenchmarkResult> resultsFromFile(const std::string& filename) {
+  string content;
+  readFile(filename.c_str(), content);
+  vector<detail::BenchmarkResult> ret;
+  benchmarkResultsFromDynamic(parseJson(content), ret);
   return ret;
 }
     
-    // Should GradStats be in this header, rather than param.h?
-struct GradStats;
+    exception_wrapper exception_wrapper::from_exception_ptr(
+    std::exception_ptr const& ptr) noexcept {
+  if (!ptr) {
+    return exception_wrapper();
+  }
+  try {
+    std::rethrow_exception(ptr);
+  } catch (std::exception& e) {
+    return exception_wrapper(std::current_exception(), e);
+  } catch (...) {
+    return exception_wrapper(std::current_exception());
+  }
+}
     
+    #include <stdexcept>
     
-    {  inline void SetPrune(const WQSummary &src, size_t maxsize) {
-    if (src.size <= maxsize) {
-      this->CopyFrom(src); return;
+        ExecutorT& operator*() const {
+      return *get();
     }
-    const RType begin = src.data[0].rmax;
-    const RType range = src.data[src.size - 1].rmin - src.data[0].rmax;
-    const size_t n = maxsize - 1;
-    data[0] = src.data[0];
-    this->size = 1;
-    // lastidx is used to avoid duplicated records
-    size_t i = 1, lastidx = 0;
-    for (size_t k = 1; k < n; ++k) {
-      RType dx2 =  2 * ((k * range) / n + begin);
-      // find first i such that  d < (rmax[i+1] + rmin[i+1]) / 2
-      while (i < src.size - 1
-             && dx2 >= src.data[i + 1].rmax + src.data[i + 1].rmin) ++i;
-      CHECK(i != src.size - 1);
-      if (dx2 < src.data[i].RMinNext() + src.data[i + 1].RMaxPrev()) {
-        if (i != lastidx) {
-          data[size++] = src.data[i]; lastidx = i;
-        }
-      } else {
-        if (i + 1 != lastidx) {
-          data[size++] = src.data[i + 1]; lastidx = i + 1;
-        }
-      }
-    }
-    if (lastidx != src.size - 1) {
-      data[size++] = src.data[src.size - 1];
-    }
-  }
-  /*!
-   * \brief set current summary to be merged summary of sa and sb
-   * \param sa first input summary to be merged
-   * \param sb second input summary to be merged
-   */
-  inline void SetCombine(const WQSummary &sa,
-                         const WQSummary &sb) {
-    if (sa.size == 0) {
-      this->CopyFrom(sb); return;
-    }
-    if (sb.size == 0) {
-      this->CopyFrom(sa); return;
-    }
-    CHECK(sa.size > 0 && sb.size > 0);
-    const Entry *a = sa.data, *a_end = sa.data + sa.size;
-    const Entry *b = sb.data, *b_end = sb.data + sb.size;
-    // extended rmin value
-    RType aprev_rmin = 0, bprev_rmin = 0;
-    Entry *dst = this->data;
-    while (a != a_end && b != b_end) {
-      // duplicated value entry
-      if (a->value == b->value) {
-        *dst = Entry(a->rmin + b->rmin,
-                     a->rmax + b->rmax,
-                     a->wmin + b->wmin, a->value);
-        aprev_rmin = a->RMinNext();
-        bprev_rmin = b->RMinNext();
-        ++dst; ++a; ++b;
-      } else if (a->value < b->value) {
-        *dst = Entry(a->rmin + bprev_rmin,
-                     a->rmax + b->RMaxPrev(),
-                     a->wmin, a->value);
-        aprev_rmin = a->RMinNext();
-        ++dst; ++a;
-      } else {
-        *dst = Entry(b->rmin + aprev_rmin,
-                     b->rmax + a->RMaxPrev(),
-                     b->wmin, b->value);
-        bprev_rmin = b->RMinNext();
-        ++dst; ++b;
-      }
-    }
-    if (a != a_end) {
-      RType brmax = (b_end - 1)->rmax;
-      do {
-        *dst = Entry(a->rmin + bprev_rmin, a->rmax + brmax, a->wmin, a->value);
-        ++dst; ++a;
-      } while (a != a_end);
-    }
-    if (b != b_end) {
-      RType armax = (a_end - 1)->rmax;
-      do {
-        *dst = Entry(b->rmin + aprev_rmin, b->rmax + armax, b->wmin, b->value);
-        ++dst; ++b;
-      } while (b != b_end);
-    }
-    this->size = dst - data;
-    const RType tol = 10;
-    RType err_mingap, err_maxgap, err_wgap;
-    this->FixError(&err_mingap, &err_maxgap, &err_wgap);
-    if (err_mingap > tol || err_maxgap > tol || err_wgap > tol) {
-      LOG(INFO) << 'mingap=' << err_mingap
-                << ', maxgap=' << err_maxgap
-                << ', wgap=' << err_wgap;
-    }
-    CHECK(size <= sa.size + sb.size) << 'bug in combine';
-  }
-  // helper function to print the current content of sketch
-  inline void Print() const {
-    for (size_t i = 0; i < this->size; ++i) {
-      LOG(CONSOLE) << '[' << i << '] rmin=' << data[i].rmin
-                   << ', rmax=' << data[i].rmax
-                   << ', wmin=' << data[i].wmin
-                   << ', v=' << data[i].value;
-    }
-  }
-  // try to fix rounding error
-  // and re-establish invariance
-  inline void FixError(RType *err_mingap,
-                       RType *err_maxgap,
-                       RType *err_wgap) const {
-    *err_mingap = 0;
-    *err_maxgap = 0;
-    *err_wgap = 0;
-    RType prev_rmin = 0, prev_rmax = 0;
-    for (size_t i = 0; i < this->size; ++i) {
-      if (data[i].rmin < prev_rmin) {
-        data[i].rmin = prev_rmin;
-        *err_mingap = std::max(*err_mingap, prev_rmin - data[i].rmin);
-      } else {
-        prev_rmin = data[i].rmin;
-      }
-      if (data[i].rmax < prev_rmax) {
-        data[i].rmax = prev_rmax;
-        *err_maxgap = std::max(*err_maxgap, prev_rmax - data[i].rmax);
-      }
-      RType rmin_next = data[i].RMinNext();
-      if (data[i].rmax < rmin_next) {
-        data[i].rmax = rmin_next;
-        *err_wgap = std::max(*err_wgap, data[i].rmax - rmin_next);
-      }
-      prev_rmax = data[i].rmax;
-    }
-  }
-  // check consistency of the summary
-  inline bool Check(const char *msg) const {
-    const float tol = 10.0f;
-    for (size_t i = 0; i < this->size; ++i) {
-      if (data[i].rmin + data[i].wmin > data[i].rmax + tol ||
-          data[i].rmin < -1e-6f || data[i].rmax < -1e-6f) {
-        LOG(INFO) << '---------- WQSummary::Check did not pass ----------';
-        this->Print();
-        return false;
-      }
-    }
-    return true;
-  }
+    
+    #ifndef FOLLY_RANDOM_H_
+#error This file may only be included from folly/Random.h
+#endif
+    
+    /*
+ * Decode a single unicode code point from UTF-8 byte sequence.
+ */
+char32_t utf8ToCodePoint(
+    const unsigned char*& p,
+    const unsigned char* const e,
+    bool skipOnError);
+    
+    #include <folly/DefaultKeepAliveExecutor.h>
+    
+        // Test the boundaries of conversion to int32_t seconds
+    using sec_i32 = std::chrono::duration<int32_t>;
+    ts.tv_sec = 2147483647;
+    ts.tv_nsec = 0;
+    EXPECT_EQ(std::numeric_limits<int32_t>::max(), to<sec_i32>(ts).count());
+    ts.tv_nsec = 1000000000;
+    EXPECT_THROW(to<sec_i32>(ts), std::range_error);
+    ts.tv_sec = -2147483648;
+    ts.tv_nsec = 0;
+    EXPECT_EQ(std::numeric_limits<int32_t>::min(), to<sec_i32>(ts).count());
+    ts.tv_sec = -2147483649;
+    ts.tv_nsec = 999999999;
+    EXPECT_THROW(to<sec_i32>(ts), std::range_error);
+    ts.tv_sec = -2147483649;
+    ts.tv_nsec = 0;
+    EXPECT_THROW(to<sec_i32>(ts), std::range_error);
+    ts.tv_sec = -2147483650;
+    ts.tv_nsec = 0;
+    EXPECT_THROW(to<sec_i32>(ts), std::range_error);
+    
+    enum class CompressionCounterKey {
+  BYTES_BEFORE_COMPRESSION = 0,
+  BYTES_AFTER_COMPRESSION = 1,
+  BYTES_BEFORE_DECOMPRESSION = 2,
+  BYTES_AFTER_DECOMPRESSION = 3,
+  COMPRESSIONS = 4,
+  DECOMPRESSIONS = 5,
+  COMPRESSION_MILLISECONDS = 6,
+  DECOMPRESSION_MILLISECONDS = 7,
 };
     
+    #pragma once
     
-    {  // index_base_[fid]: least bin id for feature fid
-  std::vector<uint32_t> index_base_;
-};
+      // Check pointer equality considering wrapped aliased pointers.
+  bool owners_eq(PackedPtr& p1, BasePtr* p2) {
+    bool aliased1 = p1.extra() & ALIASED_PTR;
+    if (aliased1) {
+      auto p1a = CountedDetail::template get_shared_ptr_from_counted_base<T>(
+          p1.get(), false);
+      return CountedDetail::get_counted_base(p1a) == p2;
+    }
+    return p1.get() == p2;
+  }
+    
+      void reset(const std::shared_ptr<T>& p = nullptr) {
+    // Allocate each Holder in a different CoreRawAllocator stripe to
+    // prevent false sharing. Their control blocks will be adjacent
+    // thanks to allocate_shared().
+    for (auto slot : folly::enumerate(slots_)) {
+      auto alloc = getCoreAllocator<Holder, kNumSlots>(slot.index);
+      auto holder = std::allocate_shared<Holder>(alloc, p);
+      *slot = std::shared_ptr<T>(holder, p.get());
+    }
+  }
