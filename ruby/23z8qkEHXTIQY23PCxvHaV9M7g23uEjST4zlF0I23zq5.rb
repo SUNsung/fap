df207de1840559e5c87a64bf7ed6817355698997
SUNsung <weatherius@gmail.com>
2018-12-30 19:19:14 +0200

    Rewrite [23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb] File in: 2018-12-30 19:19:13

--- ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
+++ ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
@@ -1,32 +1,35 @@
 
         
-            def register_lotus
-      Lotus::Assets.sources << assets_path
-    end
-    
-        @save_to.each { |_, v| FileUtils.mkdir_p(v) }
-    
-        private
-    
-      # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = false
-    
-    desc 'generate gemspec'
-task 'rack-protection.gemspec' do
-  require 'rack/protection/version'
-  content = File.binread 'rack-protection.gemspec'
-    
-          def unmasked_token?(token)
-        token.length == TOKEN_LENGTH
+        ```
+Executable Path: #{actual_path}
+```
+EOS
       end
     
-      test 'extract destination file name in case of path renaming' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
-    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
-  end
+    # Website =============================================================
     
-      test 'h1 title sanitizes correctly' do
-    title = 'H1'
-    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
-    page = @wiki.page(title)
+          <div id='post'>
+        <h3 id='post-info'>POST</h3>
+        <% if req.POST and not req.POST.empty? %>
+          <table class='req'>
+            <tr>
+              <th>Variable</th>
+              <th>Value</th>
+            </tr>
+            <% req.POST.sort_by { |k, v| k.to_s }.each { |key, val| %>
+            <tr>
+              <td><%=h key %></td>
+              <td class='code'><div><%=h val.inspect %></div></td>
+            </tr>
+            <% } %>
+          </table>
+        <% else %>
+          <p class='no-data'>No POST data.</p>
+        <% end %>
+        <div class='clear'></div>
+      </div> <!-- /POST -->
+    <% end %>
+    
+          def set_token(session)
+        session[:csrf] ||= self.class.random_token
+      end
