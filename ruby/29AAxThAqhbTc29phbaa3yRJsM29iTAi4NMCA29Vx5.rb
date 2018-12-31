71f0e69f406ad3cd105a80c1ee5774396e3aeb8b
SUNsung <weatherius@gmail.com>
2018-12-31 04:49:11 +0200

    Rewrite [29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb] File in: 2018-12-31 04:49:11

--- ruby/29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb
+++ ruby/29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb
@@ -1,112 +1,77 @@
 
         
-        FORWARD_SLASH = '/'.freeze
+                unless post && post.id
+          puts post.errors.full_messages if post
+          puts creator.errors.inspect
+          raise 'Failed to create description for trust level 3 lounge!'
+        end
     
-              new_theme_name = args.join('_')
-          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
-    
-            # If our highlighter is CodeRay we go in to merge the CodeRay defaults
-        # with your 'coderay' key if it's there, deprecating it in the
-        # process of you using it.
-        def modernize_coderay_config
-          unless @config['coderay'].empty?
-            Jekyll::Deprecator.deprecation_message(
-              'You are using 'kramdown.coderay' in your configuration, ' \
-              'please use 'syntax_highlighter_opts' instead.'
-            )
-    
-        brew cask install mactex
-    EOS
-  when 'pip' then <<-EOS.undent
-    Homebrew provides pip via: `brew install python`. However you will then
-    have two Pythons installed on your Mac, so alternatively you can install
-    pip via the instructions at:
-    
-    module BuildEnvironmentDSL
-  def env(*settings)
-    @env ||= BuildEnvironment.new
-    @env.merge(settings)
-  end
-end
-    
-        def self.prune?(path, options = {})
-      @time ||= Time.now
-    
-      def describe_python
-    python = which 'python'
-    return 'N/A' if python.nil?
-    python_binary = Utils.popen_read python, '-c', 'import sys; sys.stdout.write(sys.executable)'
-    python_binary = Pathname.new(python_binary).realpath
-    if python == python_binary
-      python
+      # Override prefixes to consider the scoped view.
+  # Notice we need to check for the request due to a bug in
+  # Action Controller tests that forces _prefixes to be
+  # loaded before even having a request object.
+  #
+  # This method should be public as it is in ActionPack
+  # itself. Changing its visibility may break other gems.
+  def _prefixes #:nodoc:
+    @_prefixes ||= if self.class.scoped_views? && request && devise_mapping
+      ['#{devise_mapping.scoped_path}/#{controller_name}'] + super
     else
-      '#{python} => #{python_binary}'
+      super
     end
   end
     
-    class PrettyListing
-  def initialize(path)
-    Pathname.new(path).children.sort_by { |p| p.to_s.downcase }.each do |pn|
-      case pn.basename.to_s
-      when 'bin', 'sbin'
-        pn.find { |pnn| puts pnn unless pnn.directory? }
-      when 'lib'
-        print_dir pn do |pnn|
-          # dylibs have multiple symlinks and we don't care about them
-          (pnn.extname == '.dylib' || pnn.extname == '.pc') && !pnn.symlink?
+        def password_change(record, opts={})
+      devise_mail(record, :password_change, opts)
+    end
+  end
+end
+
+    
+      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
+     options[:store] != false && !env['devise.skip_timeoutable']
+    last_request_at = warden.session(scope)['last_request_at']
+    
+          # If the record is persisted, remove the remember token (but only if
+      # it exists), and save the record without validations.
+      def forget_me!
+        return unless persisted?
+        self.remember_token = nil if respond_to?(:remember_token)
+        self.remember_created_at = nil if self.class.expire_all_remember_me_on_sign_out
+        save(validate: false)
+      end
+    
+            # This is what is called on the class to actually execute it. Any
+        # subclasses should implement this method and do any option parsing
+        # and validation here.
+        def execute
         end
-      else
-        if pn.directory?
-          if pn.symlink?
-            puts '#{pn} -> #{pn.readlink}'
-          else
-            print_dir pn
-          end
-        elsif Metafiles.list?(pn.basename.to_s)
-          puts pn
+    
+            # Executes a command on the remote machine with administrative
+        # privileges. See {#execute} for documentation, as the API is the
+        # same.
+        #
+        # @see #execute
+        def sudo(command, opts=nil)
+        end
+    
+              return [main_args, sub_command, sub_args]
         end
       end
     end
   end
-    
-    post '/' do
-  connections.each { |out| out << 'data: #{params[:msg]}\n\n' }
-  204 # response without entity body
 end
-    
-    task :gemspec => 'rack-protection.gemspec'
-task :default => :spec
-task :test    => :spec
 
     
-    desc 'Run integration test'
-Cucumber::Rake::Task.new do |t|
-  t.cucumber_opts = %w{--format progress}
+            # This unregisters a plugin so that its components will no longer
+        # be used. Note that this should only be used for testing purposes.
+        def unregister(plugin)
+          if @registered.include?(plugin)
+            @logger.info('Unregistered: #{plugin.name}')
+            @registered.delete(plugin)
+          end
+        end
+      end
+    end
+  end
 end
-    
-        def self.definitions_for(klass)
-      instance.definitions_for(klass)
-    end
-    
-        def initialize(filepath)
-      @filepath = filepath
-    end
-    
-        # True if the dimensions represent a square
-    def square?
-      height == width
-    end
-    
-        def define_getters
-      define_instance_getter
-      define_class_getter
-    end
-    
-        # Returns the extension of the file. e.g. 'jpg' for 'file.jpg'
-    # If the style has a format defined, it will return the format instead
-    # of the actual extension.
-    def extension attachment, style_name
-      ((style = attachment.styles[style_name.to_s.to_sym]) && style[:format]) ||
-        File.extname(attachment.original_filename).sub(/\A\.+/, ''.freeze)
-    end
