c0afa54d97ad4a002055611b99c0f239e61ae76e
SUNsung <weatherius@gmail.com>
2018-12-31 14:46:05 +0200

    Rewrite [30pACRue0tDM630Tl7luIHNUCk30TSfHzV5Xfc30yg5.py] File in: 2018-12-31 14:46:04

--- pyton/30pACRue0tDM630Tl7luIHNUCk30TSfHzV5Xfc30yg5.py
+++ pyton/30pACRue0tDM630Tl7luIHNUCk30TSfHzV5Xfc30yg5.py
@@ -1,3 +1,67 @@
 
         
-        
+            return (x_train, y_train), (x_test, y_test)
+
+    
+        To save the multi-gpu model, use `.save(fname)` or `.save_weights(fname)`
+    with the template model (the argument you passed to `multi_gpu_model`),
+    rather than the model returned by `multi_gpu_model`.
+    '''
+    if K.backend() != 'tensorflow':
+        raise ValueError('`multi_gpu_model` is only available '
+                         'with the TensorFlow backend.')
+    
+                # Size of the square
+            w = np.random.randint(2, 4)
+    
+    model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(LSTM(lstm_output_size))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+    
+    print('I found {} face(s) in this photograph.'.format(len(face_locations)))
+    
+        # If no valid image file was uploaded, show the file upload form:
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method='POST' enctype='multipart/form-data'>
+      <input type='file' name='file'>
+      <input type='submit' value='Upload'>
+    </form>
+    '''
+    
+                frame_number = frame_count - 128 + frame_number_in_batch
+            print('I found {} face(s) in frame #{}.'.format(number_of_faces_in_frame, frame_number))
+    
+            self.assertEqual(len(detected_faces), 1)
+        self.assertEqual(detected_faces[0], (142, 617, 409, 349))
+    
+    # Show the picture
+pil_image.show()
+
+    
+        # 获得所有人脸的位置以及它们的编码
+    face_locations = face_recognition.face_locations(output)
+    print('Found {} faces in image.'.format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
+    
+    try:
+    # compatible for python2
+    from urllib2 import urlopen
+    from urllib2 import HTTPError
+    from urllib2 import URLError
+except ImportError:
+    # compatible for python3
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+    from urllib.error import URLError
