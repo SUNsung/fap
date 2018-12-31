596245cf6848328340ea173342d3b5700f6de477
SUNsung <weatherius@gmail.com>
2018-12-31 19:12:53 +0200

    Rewrite [211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb] File in: 2018-12-31 19:12:52

--- ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
+++ ruby/211HdXYK4q4Wg21yRNP3Xw2DQ621PlunLivsVc216zN.rb
@@ -1,115 +1,163 @@
 
         
-                private
-    
-    module ActionView #:nodoc:
-  # = Action View PathSet
-  #
-  # This class is used to store and access paths in Action View. A number of
-  # operations are defined so that you can search among the paths in this
-  # set and also perform operations on other +PathSet+ objects.
-  #
-  # A +LookupContext+ will use a +PathSet+ to store the paths in its context.
-  class PathSet #:nodoc:
-    include Enumerable
-    
-        rake_tasks do |app|
-      unless app.config.api_only
-        load 'action_view/tasks/cache_digests.rake'
+            keys.each do |key|
+      value = env[key]
+      s = '#{key}: #{value}'
+      case key
+      when 'CC', 'CXX', 'LD'
+        s << ' => #{Pathname.new(value).realpath}' if File.symlink?(value)
       end
+      f.puts s
     end
   end
 end
 
     
-        def cache_hits # :nodoc:
-      @cache_hits ||= {}
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
+      end
+      cmds
+    end.sort
+  end
+    
+    module Homebrew
+  def config
+    dump_verbose_config
+  end
+    
+        if ARGV.named.empty?
+      slow_checks = %w[
+        check_for_broken_symlinks
+        check_missing_deps
+        check_for_outdated_homebrew
+        check_for_linked_keg_only_brews
+      ]
+      methods = (checks.all.sort - slow_checks) + slow_checks
+    else
+      methods = ARGV.named
+    end
+    
+          begin
+        result = Formulary.factory(query).name
+      rescue FormulaUnavailableError
+        result = search_tap(user, repo, name)
+      end
+    
+            expect(result).to match(%r{cd .* && oclint -report-type=html -o=oclint_report.html -p ./fastlane/spec/fixtures/oclint \'.*})
+      end
+    
+            def execute
+          create_labels
+        end
+    
+          # Returns true if the given object has already been imported, false
+      # otherwise.
+      #
+      # object - The object to check.
+      def already_imported?(object)
+        id = id_for_already_imported_cache(object)
+    
+            # Returns a Hash that can be used to populate `notes.st_diff`, removing
+        # the need for requesting Git data for every diff note.
+        def diff_hash
+          {
+            diff: diff_hunk,
+            new_path: file_path,
+            old_path: file_path,
+    }
+    
+              hash[:author] &&= Representation::User.from_json_hash(hash[:author])
+    
+            a_split.each_with_index { |s, i| a_split[i] = s.to_i unless i == a_length - 1 }
+        b_split.each_with_index { |s, i| b_split[i] = s.to_i unless i == b_length - 1 }
+    
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
+    end
+    
+        def effective_path
+      @effective_path ||= effective_url.path
     end
   end
 end
 
     
-    UserOption.where(user_id: -1).update_all(
-  email_private_messages: false,
-  email_direct: false
-)
+        self.params = {}
+    self.headers = { 'User-Agent' => 'DevDocs' }
+    self.force_gzip = false
     
-            lounge.topic_id = post.topic.id
-        unless lounge.save
-          puts lounge.errors.full_messages
-          puts 'Failed to set the lounge description topic!'
-        end
+        @statuses = @account.statuses.permitted_for(@account, signed_request_account)
+    @statuses = params[:min_id].present? ? @statuses.paginate_by_min_id(LIMIT, params[:min_id]).reverse : @statuses.paginate_by_max_id(LIMIT, params[:max_id])
+    @statuses = cache_collection(@statuses, Status)
+  end
     
-        Category.transaction do
-      staff.group_names = ['staff']
-      unless staff.save
-        puts staff.errors.full_messages
-        raise 'Failed to set permissions on the Staff category!'
-      end
+        def show
+      authorize @user, :change_email?
+    end
     
-    Capybara.register_driver :javascript_test do |app|
-  Capybara::RackTest::Driver.new(app)
+      def hub_callback
+    params['hub.callback']
+  end
+    
+      caveats <<~EOS
+    Installation or Uninstallation may fail with Exit Code 19 (Conflicting Processes running) if Browsers, Safari Notification Service or SIMBL Services (e.g. Flashlight) are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Logs in /Library/Logs/Adobe/Installers if Installation or Uninstallation fails, to identifify the conflicting processes.
+  EOS
 end
+
     
-          def send_keys(*args)
-        raise NotImplementedError
+    # The module that contains everything Sass-related:
+#
+# * {Sass::Engine} is the class used to render Sass/SCSS within Ruby code.
+# * {Sass::Plugin} is interfaces with web frameworks (Rails and Merb in particular).
+# * {Sass::SyntaxError} is raised when Sass encounters an error.
+# * {Sass::CSS} handles conversion of CSS to Sass.
+#
+# Also see the {file:SASS_REFERENCE.md full Sass reference}.
+module Sass
+  class << self
+    # @private
+    attr_accessor :tests_running
+  end
+    
+          root.children.each {|v| nest_seqs(v)}
+    end
+    
+          opts.on('--unix-newlines', 'Use Unix-style newlines in written files.',
+                                 ('Always true on Unix.' unless Sass::Util.windows?)) do
+        @options[:unix_newlines] = true if Sass::Util.windows?
+      end
+    end
+    
+          # If the importer is based on files on the local filesystem
+      # this method should return folders which should be watched
+      # for changes.
+      #
+      # @return [Array<String>] List of absolute paths of directories to watch
+      def directories_to_watch
+        []
       end
     
-      it 'should raise an error if there are multiple matches' do
-    el = @session.find(:css, '#child')
-    expect { el.ancestor('//div') }.to raise_error(Capybara::Ambiguous)
-    expect { el.ancestor('//div', text: 'Ancestor') }.to raise_error(Capybara::Ambiguous)
-  end
-    
-      action :raw do
-    title 'Send a raw RFC2882 message'
-    description 'This action allows you to send us a raw RFC2822 formatted message along with the recipients that it should be sent to. This is similar to sending a message through our SMTP service.'
-    param :mail_from, 'The address that should be logged as sending the message', :type => String, :required => true
-    param :rcpt_to, 'The addresses this message should be sent to', :type => Array, :required => true
-    param :data, 'A base64 encoded RFC2822 message to send', :type => String, :required => true
-    param :bounce, 'Is this message a bounce?', :type => :boolean
-    returns Hash
-    error 'UnauthenticatedFromAddress', 'The From address is not authorised to send mail from this server'
-    action do
-      # Decode the raw message
-      raw_message = Base64.decode64(params.data)
-    
-      private
-    
-      def create
-    @http_endpoint = @server.http_endpoints.build(safe_params)
-    if @http_endpoint.save
-      flash[:notice] = params[:return_notice] if params[:return_notice].present?
-      redirect_to_with_json [:return_to, [organization, @server, :http_endpoints]]
-    else
-      render_form_errors 'new', @http_endpoint
-    end
-  end
-    
-      def create
-    scope = @server ? @server.ip_pool_rules : organization.ip_pool_rules
-    @ip_pool_rule = scope.build(safe_params)
-    if @ip_pool_rule.save
-      redirect_to_with_json [organization, @server, :ip_pool_rules]
-    else
-      render_form_errors 'new', @ip_pool_rule
-    end
-  end
-    
-      def destroy
-    @ip_pool.destroy
-    redirect_to_with_json :ip_pools, :notice => 'IP pool has been removed successfully.'
-  rescue ActiveRecord::DeleteRestrictionError => e
-    redirect_to_with_json [:edit, @ip_pool], :alert => 'IP pool cannot be removed because it is still assigned to servers/rules.'
-  end
-    
-          address_endpoints = server.address_endpoints.order(:address).to_a
-      if address_endpoints.present?
-        s << '<optgroup label='Address Endpoints'>'
-        for endpoint in address_endpoints
-          value = '#{endpoint.class}##{endpoint.uuid}'
-          selected = value == selected_value ? 'selected='selected'' : ''
-          s << '<option value='#{value}' #{selected}>#{endpoint.address}</option>'
+            def destroy
+          if @property
+            authorize! :destroy, @property
+            @property.destroy
+            respond_with(@property, status: 204)
+          else
+            invalid_resource!(@property)
+          end
         end
-        s << '</optgroup>'
-      end
+    
+            def show
+          @state = scope.find(params[:id])
+          respond_with(@state)
+        end
+    
+            def update
+          @stock_item = StockItem.accessible_by(current_ability, :update).find(params[:id])
