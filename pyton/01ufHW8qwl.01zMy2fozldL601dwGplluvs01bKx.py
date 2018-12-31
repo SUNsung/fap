5cdf253ae55a6f5a1172b89d41d7107f5cbe631a
SUNsung <weatherius@gmail.com>
2018-12-31 18:00:38 +0200

    Rewrite [01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py] File in: 2018-12-31 18:00:38

--- pyton/01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py
+++ pyton/01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py
@@ -1,120 +1,25 @@
 
         
-        
-@pytest.fixture(autouse=True)
-def shell_config(mocker):
-    path_mock = mocker.patch('thefuck.entrypoints.not_configured.Path',
-                             new_callable=MagicMock)
-    return path_mock.return_value \
-        .expanduser.return_value \
-        .open.return_value \
-        .__enter__.return_value
-    
-        print()
-    print('Classification performance:')
-    print('===========================')
-    print()
-    print('%s %s %s %s' % ('Classifier  ', 'train-time', 'test-time',
-                           'Accuracy'))
-    print('-' * 44)
-    for name in sorted(accuracy, key=accuracy.get):
-        print('%s %s %s %s' % (name.ljust(16),
-                               ('%.4fs' % train_time[name]).center(10),
-                               ('%.4fs' % test_time[name]).center(10),
-                               ('%.4f' % accuracy[name]).center(10)))
-    
-    
-DATASET_GENERATORS = {
-    'perturbed_logarithm': generate_perturbed_logarithm_dataset,
-    'logistic': generate_logistic_dataset,
-    'pathological': generate_pathological_dataset,
-}
-    
-                tick_vals += list(xvals + 0.5 * width)
-            tick_labels += ['%i' % val for val in vals]
-    
-    n_samples = np.logspace(.5, 3, 9)
-n_features = np.logspace(1, 3.5, 7)
-N_samples, N_features = np.meshgrid(n_samples,
-                                    n_features)
-scikits_time = np.zeros(N_samples.shape)
-scipy_time = np.zeros(N_samples.shape)
-    
-        print('Dataset statics')
-    print('===========================')
-    print('n_samples \t= %s' % opts.n_samples)
-    print('n_features \t= %s' % opts.n_features)
-    if opts.n_components == 'auto':
-        print('n_components \t= %s (auto)' %
-              johnson_lindenstrauss_min_dim(n_samples=opts.n_samples,
-                                            eps=opts.eps))
-    else:
-        print('n_components \t= %s' % opts.n_components)
-    print('n_elements \t= %s' % (opts.n_features * opts.n_samples))
-    print('n_nonzeros \t= %s per feature' % n_nonzeros)
-    print('ratio_nonzeros \t= %s' % opts.ratio_nonzeros)
-    print('')
-    
-        An example with a long-untouched module that everyone has
-    >>> _linkcode_resolve('py', {'module': 'tty',
-    ...                          'fullname': 'setraw'},
-    ...                   package='tty',
-    ...                   url_fmt='http://hg.python.org/cpython/file/'
-    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
-    ...                   revision='xxxx')
-    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
-    '''
-    
-        print('Decompressing %s' % ARCHIVE_NAME)
-    with closing(tarfile.open(ARCHIVE_NAME, 'r:gz')) as archive:
-        archive.extractall(path='.')
-    os.remove(ARCHIVE_NAME)
-
-    
-    
-if __name__ == '__main__':
-    # Create our localizers
-    e, g = get_localizer(language='English'), get_localizer(language='Greek')
-    # Localize some text
-    for msgid in 'dog parrot cat bear'.split():
-        print(e.get(msgid), g.get(msgid))
-    
-    ### OUTPUT ###
-# Jack move 5m then stop
-
-    
-    
-def count_to(count):
-    '''Counts by word numbers, up to a maximum of five'''
-    numbers = ['one', 'two', 'three', 'four', 'five']
-    for number in numbers[:count]:
-        yield number
-    
-        def commit(self):
-        self.states = [memento(target, self.deep) for target in self.targets]
-    
-        def __new__(cls, name, bases, attrs):
-        new_cls = type.__new__(cls, name, bases, attrs)
-        '''
-            Here the name of the class is used as key but it could be any class
-            parameter.
-        '''
-        cls.REGISTRY[new_cls.__name__] = new_cls
-        return new_cls
-    
-    ### OUTPUT ###
-# Scanning... Station is 1380 AM
-# Scanning... Station is 1510 AM
-# Switching to FM
-# Scanning... Station is 89.1 FM
-# Scanning... Station is 103.9 FM
-# Scanning... Station is 81.3 FM
-# Scanning... Station is 89.1 FM
-# Switching to AM
-# Scanning... Station is 1250 AM
-# Scanning... Station is 1380 AM
-
-    
-    *TL;DR80
-Creates new object instances by cloning prototype.
+            This could be extended by having nested blocks, sorting them recursively
+    and flattening the end structure into a list of lines. Revision 2 maybe ^.^.
 '''
+    
+    
+def load_data():
+    '''Loads the Fashion-MNIST dataset.
+    
+    
+def test_reuters():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = reuters.load_data()
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        assert len(x_train) + len(x_test) == 11228
+        (x_train, y_train), (x_test, y_test) = reuters.load_data(maxlen=10)
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        word_index = reuters.get_word_index()
+        assert isinstance(word_index, dict)
