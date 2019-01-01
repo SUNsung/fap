3bb402de7a1930762e217451b197e23feb2b129a
SUNsung <weatherius@gmail.com>
2019-01-01 10:32:56 +0200

    Rewrite [46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb] File in: 2019-01-01 10:32:56

--- ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
+++ ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
@@ -1,126 +1,215 @@
 
         
-                display_user_error!(e, error_info)
-      else
-        # Pass the error instead of a message so that the inspector can do extra work to simplify the query
-        show_github_issues(e) if should_show_github_issues
+        def converted_history(markdown)
+  remove_head_from_history(
+    custom_release_header_anchors(
+      liquid_escape(
+        linkify(
+          normalize_bullets(markdown)
+        )
+      )
+    )
+  )
+end
     
-          it 'sets the project directory to other and the toolchain to Swift_2_3' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          carthage(toolchain: 'com.apple.dt.toolchain.Swift_2_3', project_directory: 'other')
-        end').runner.execute(:test)
+    require 'benchmark/ips'
+require 'jekyll'
+require 'json'
     
-            def importer_class
-          DiffNoteImporter
-        end
+    # -------------------------------------------------------------------
+# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
+# -------------------------------------------------------------------
     
-                rows << {
-              label_id: label_id,
-              target_id: target_id,
-              target_type: issue.issuable_type,
-              created_at: time,
-              updated_at: time
-            }
-          end
+    #
     
-          # Returns true if the given object has already been imported, false
-      # otherwise.
-      #
-      # object - The object to check.
-      def already_imported?(object)
-        id = id_for_already_imported_cache(object)
+        group.remove(moderator)
+    group.save
     
-            # Builds an issue from a GitHub API response.
-        #
-        # issue - An instance of `Sawyer::Resource` containing the issue
-        #         details.
-        def self.from_api_response(issue)
-          user =
-            if issue.user
-              Representation::User.from_api_response(issue.user)
-            end
-    
-            attr_reader :attributes
-    
-    describe 'Kernel#test' do
-  before :all do
-    @file = File.dirname(__FILE__) + '/fixtures/classes.rb'
-    @dir = File.dirname(__FILE__) + '/fixtures'
-  end
-    
-    describe 'Kernel.throw' do
-  it 'transfers control to the end of the active catch block waiting for symbol' do
-    catch(:blah) do
-      :value
-      throw :blah
-      fail('throw didn't transfer the control')
-    end.should be_nil
-  end
-    
-      it 'hooks assignments to a global variable' do
-    captured = nil
-    
-      desc 'Run sass-spec tests against the local code.'
-  task :spec do
-    require 'yaml'
-    sass_spec_options = YAML.load_file(scope('test/sass-spec.yml'))
-    enabled = sass_spec_options.delete(:enabled)
-    unless enabled
-      puts 'SassSpec tests are disabled.'
-      next
+        it 'defauls foreground and background colors' do
+      scenario.tag_fg_color = nil
+      scenario.tag_bg_color = nil
+      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
     end
-    if ruby_version_at_least?('1.9.2')
-      old_load_path = $:.dup
-      begin
-        $:.unshift(File.join(File.dirname(__FILE__), 'lib'))
-        begin
-          require 'sass_spec'
-        rescue LoadError
-          puts 'You probably forgot to run: bundle exec rake'
-          raise
-        end
-        default_options = {
-          :spec_directory => SassSpec::SPEC_DIR,
-          :engine_adapter => SassEngineAdapter.new,
-          :generate => false,
-          :tap => false,
-          :skip => false,
-          :verbose => false,
-          :filter => '',
-          :limit => -1,
-          :unexpected_pass => false,
-          :nuke => false,
-        }
-        SassSpec::Runner.new(default_options.merge(sass_spec_options)).run || exit(1)
-      ensure
-        $:.replace(old_load_path)
-      end
+  end
+    
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
+    end
+    
+          context '#restart_dead_workers' do
+        before do
+          mock.instance_of(HuginnScheduler).run!
+          mock.instance_of(DelayedJobWorker).run!
+          @agent_runner.send(:run_workers)
+    
+        it 'should convert the 'escape' method correctly' do
+      expect(LiquidMigrator.convert_string('Escaped: <escape $.content.name>\nNot escaped: <$.content.name>')).to eq(
+                                    'Escaped: {{content.name | uri_escape}}\nNot escaped: {{content.name}}'
+      )
+    end
+    
+      it 'converts values to Float' do
+    expect(location.lat).to be_a Float
+    expect(location.lat).to eq 2.0
+    expect(location.lng).to be_a Float
+    expect(location.lng).to eq 3.0
+    expect(location.radius).to be_a Float
+    expect(location.radius).to eq 300.0
+    expect(location.speed).to be_a Float
+    expect(location.speed).to eq 2.0
+    expect(location.course).to be_a Float
+    expect(location.course).to eq 30.0
+  end
+    
+          it 'sets the default on new instances, allows setting new schedules, and prevents invalid schedules' do
+        @checker = Agents::SomethingSource.new(:name => 'something')
+        @checker.user = users(:bob)
+        expect(@checker.schedule).to eq('2pm')
+        @checker.save!
+        expect(@checker.reload.schedule).to eq('2pm')
+        @checker.update_attribute :schedule, '5pm'
+        expect(@checker.reload.schedule).to eq('5pm')
+    
+            format('%1$*2$e', 109.52, -20).should == '1.095200e+02        '
+        format('%1$*2$E', 109.52, -20).should == '1.095200E+02        '
+        format('%1$*2$f', 10.952, -20).should == '10.952000           '
+        format('%1$*2$g', 12.1234, -20).should == '12.1234             '
+        format('%1$*2$G', 12.1234, -20).should == '12.1234             '
+        format('%1$*2$a', 196, -20).should == '0x1.88p+7           '
+        format('%1$*2$A', 196, -20).should == '0X1.88P+7           '
+    
+    describe 'Kernel#taint' do
+  it 'returns self' do
+    o = Object.new
+    o.taint.should equal(o)
+  end
+    
+      it 'creates a public method in TOPLEVEL_BINDING' do
+    eval @code, TOPLEVEL_BINDING
+    Object.should have_method :boom
+  end
+    
+      # Compile a file on disk to CSS.
+  #
+  # @raise [Sass::SyntaxError] if there's an error in the document
+  # @raise [Encoding::UndefinedConversionError] if the source encoding
+  #   cannot be converted to UTF-8
+  # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
+  #
+  # @overload compile_file(filename, options = {})
+  #   Return the compiled CSS rather than writing it to a file.
+  #
+  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
+  #   @param options [{Symbol => Object}] An options hash;
+  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
+  #   @return [String] The compiled CSS.
+  #
+  # @overload compile_file(filename, css_filename, options = {})
+  #   Write the compiled CSS to a file.
+  #
+  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
+  #   @param options [{Symbol => Object}] An options hash;
+  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
+  #   @param css_filename [String] The location to which to write the compiled CSS.
+  def self.compile_file(filename, *args)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    css_filename = args.shift
+    result = Sass::Engine.for_file(filename, options).render
+    if css_filename
+      options[:css_filename] ||= css_filename
+      open(css_filename, 'w') {|css_file| css_file.write(result)}
+      nil
     else
-      'Skipping sass-spec on ruby versions less than 1.9.2'
+      result
     end
   end
 end
     
-          # Returns the path to a file for the given key.
+        # Same as `Kernel.puts`, but doesn't print anything if the `--quiet` option is set.
+    #
+    # @param args [Array] Passed on to `Kernel.puts`
+    def puts(*args)
+      return if @options[:for_engine][:quiet]
+      Kernel.puts(*args)
+    end
+    
+          opts.on('-v', '--version', 'Print the Sass version.') do
+        puts('Sass #{Sass.version[:string]}')
+        exit
+      end
+    end
+    
+          # If the importer is based on files on the local filesystem
+      # this method should return folders which should be watched
+      # for changes.
       #
-      # @param key [String]
-      # @return [String] The path to the cache file.
-      def path_to(key)
-        key = key.gsub(/[<>:\\|?*%]/) {|c| '%%%03d' % c.ord}
-        File.join(cache_location, key)
+      # @return [Array<String>] List of absolute paths of directories to watch
+      def directories_to_watch
+        []
+      end
+    
+          # @see Base#key
+      def key(name, options)
+        [self.class.name + ':' + File.dirname(File.expand_path(name)),
+         File.basename(name)]
+      end
+    
+          before_action :set_content_type
+      before_action :load_user
+      before_action :authorize_for_order, if: proc { order_token.present? }
+      before_action :authenticate_user
+      before_action :load_user_roles
+    
+            def update
+          @option_type = Spree::OptionType.accessible_by(current_ability, :update).find(params[:id])
+          if @option_type.update_attributes(option_type_params)
+            render :show
+          else
+            invalid_resource!(@option_type)
+          end
+        end
+    
+            def return_authorization_params
+          params.require(:return_authorization).permit(permitted_return_authorization_attributes)
+        end
       end
     end
   end
 end
 
     
-          # A string representation of the importer.
-      # Should be overridden by subclasses.
-      #
-      # This is used to help debugging,
-      # and should usually just show the load path encapsulated by this importer.
-      #
-      # @return [String]
-      def to_s
-        Sass::Util.abstract(self)
-      end
+            def create
+          @order = Spree::Order.find_by!(number: params.fetch(:shipment).fetch(:order_id))
+          authorize! :read, @order
+          authorize! :create, Shipment
+          quantity = params[:quantity].to_i
+          @shipment = @order.shipments.create(stock_location_id: params.fetch(:stock_location_id))
+    
+            def scope
+          includes = { variant: [{ option_values: :option_type }, :product] }
+          @stock_location.stock_items.accessible_by(current_ability, :read).includes(includes)
+        end
+    
+            def create
+          authorize! :create, StockLocation
+          @stock_location = StockLocation.new(stock_location_params)
+          if @stock_location.save
+            respond_with(@stock_location, status: 201, default_template: :show)
+          else
+            invalid_resource!(@stock_location)
+          end
+        end
+    
+            def create
+          authorize! :create, Store
+          @store = Store.new(store_params)
+          @store.code = params[:store][:code]
+          if @store.save
+            respond_with(@store, status: 201, default_template: :show)
+          else
+            invalid_resource!(@store)
+          end
+        end
