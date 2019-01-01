72c4c2003bf2efe5e06cb38ab660d223418cbbaa
SUNsung <weatherius@gmail.com>
2019-01-01 16:25:29 +0200

    Rewrite [11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb] File in: 2019-01-01 16:25:29

--- ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
+++ ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
@@ -1,172 +1,29 @@
 
         
-        # Test https://github.com/jekyll/jekyll/pull/6735#discussion_r165499868
-# ------------------------------------------------------------------------
-def check_with_regex(content)
-  !content.to_s.match?(%r!{[{%]!)
-end
-    }
-    }
+                unless post && post.id
+          puts post.errors.full_messages if post
+          puts creator.errors.inspect
+          raise 'Failed to create description for trust level 3 lounge!'
+        end
     
-    # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
+      def open_dry_run_modal(agent)
+    visit edit_agent_path(agent)
+    click_on('Dry Run')
+    expect(page).to have_text('Event to send')
+  end
     
-      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
-    cmd = g.split('-').last
-    p.command(cmd.to_sym) do |c|
-      c.syntax cmd
-      c.action do
-        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
-          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
-      end
+            it 'updates any existing agents in the scenario, and makes new ones as needed' do
+          expect(scenario_import).to be_valid
+    
+      describe 'migrating the 'make_message' format' do
+    it 'should work' do
+      expect(LiquidMigrator.convert_make_message('<message>')).to eq('{{message}}')
+      expect(LiquidMigrator.convert_make_message('<new.message>')).to eq('{{new.message}}')
+      expect(LiquidMigrator.convert_make_message('Hello <world>. How is <nested.life>')).to eq('Hello {{world}}. How is {{nested.life}}')
     end
   end
     
-    Then(%r!^I should get an updated git index$!) do
-  index = %w(
-    .gitignore
-    Gemfile
-    LICENSE.txt
-    README.md
-    _includes/blank.html
-    _layouts/default.html
-    _layouts/page.html
-    _layouts/post.html
-    _sass/blank.scss
-    assets/blank.scss
-    my-cool-theme.gemspec
-  )
-  index.each do |file|
-    expect(jekyll_run_output).to match file
-  end
-end
-    
-        private
-    
-      def load_logs(dir)
-    logs = {}
-    if dir.exist?
-      dir.children.sort.each do |file|
-        contents = file.size? ? file.read : 'empty log'
-        # small enough to avoid GitHub 'unicorn' page-load-timeout errors
-        max_file_size = 1_000_000
-        contents = truncate_text_to_approximate_size(contents, max_file_size, front_weight: 0.2)
-        logs[file.basename.to_s] = { content: contents }
-      end
-    end
-    raise 'No logs.' if logs.empty?
-    
-        false
-  end
-    
-          old_name = name
-      old_path = path
-      old_remote = path.git_origin
-    
-    When /^I try to sign in manually$/ do
-  manual_login
-end
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
-    end
-  end
-end
-
-    
-          code = InvitationCode.create(user: bob)
-      code.update_attributes(count: 0)
-    
-          @options = options.merge(:_convert => true)
-      # Backwards compatibility
-      @options[:old] = true if @options[:alternate] == false
-      @template = template
-      @checked_encoding = false
-    end
-    
-            if child.is_a?(Tree::CommentNode) && child.type == :silent
-          if continued_comment &&
-              child.line == continued_comment.line +
-              continued_comment.lines + 1
-            continued_comment.value.last.sub!(%r{ \*/\Z}, '')
-            child.value.first.gsub!(%r{\A/\*}, ' *')
-            continued_comment.value += ['\n'] + child.value
-            next
-          end
-    
-        def default_sass_path
-      return unless ENV['SASS_PATH']
-      # The select here prevents errors when the environment's
-      # load paths specified do not exist.
-      ENV['SASS_PATH'].split(File::PATH_SEPARATOR).select {|d| File.directory?(d)}
-    end
-  end
-end
-
-    
-          # Get the cache key pair for the given Sass URI.
-      # The URI need not be checked for validity.
-      #
-      # The only strict requirement is that the returned pair of strings
-      # uniquely identify the file at the given URI.
-      # However, the first component generally corresponds roughly to the directory,
-      # and the second to the basename, of the URI.
-      #
-      # Note that keys must be unique *across importers*.
-      # Thus it's probably a good idea to include the importer name
-      # at the beginning of the first component.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [(String, String)] The key pair which uniquely identifies
-      #   the file at the given URI.
-      def key(uri, options)
-        Sass::Util.abstract(self)
-      end
-    
-    group :development do
-  cp_gem 'claide',                'CLAide'
-  cp_gem 'cocoapods-core',        'Core'
-  cp_gem 'cocoapods-deintegrate', 'cocoapods-deintegrate'
-  cp_gem 'cocoapods-downloader',  'cocoapods-downloader'
-  cp_gem 'cocoapods-plugins',     'cocoapods-plugins'
-  cp_gem 'cocoapods-search',      'cocoapods-search'
-  cp_gem 'cocoapods-stats',       'cocoapods-stats'
-  cp_gem 'cocoapods-trunk',       'cocoapods-trunk'
-  cp_gem 'cocoapods-try',         'cocoapods-try'
-  cp_gem 'molinillo',             'Molinillo'
-  cp_gem 'nanaimo',               'Nanaimo'
-  cp_gem 'xcodeproj',             'Xcodeproj'
-    
-                case platform
-            when 'iOS' then self.platform :ios, '10.0'
-            when 'macOS' then self.platform :macos, '10.10'
-            end
-    
-        execute 'ensure-sidekiq-is-setup-with-monit' do 
-      command %Q{ 
-        monit reload 
-      } 
-    end
-    
-    @@ index
-  <h1>Sinatra + Sidekiq Example</h1>
-  <h2>Failed: <%= @failed %></h2>
-  <h2>Processed: <%= @processed %></h2>
-    
-          # Provide a call() method that returns the formatted message.
-      def call(severity, time, program_name, message)
-        '#{time.utc.iso8601(3)} #{::Process.pid} TID-#{Sidekiq::Logging.tid}#{context} #{severity}: #{message}\n'
-      end
-    
-          def prepend(klass, *args)
-        remove(klass) if exists?(klass)
-        entries.insert(0, Entry.new(klass, *args))
+            expect(cop.highlights).to eq([close])
+        expect(cop.messages)
+          .to eq([described_class::SAME_LINE_MESSAGE])
       end
