3adab717349da14c9fa699f195548a49c77c21c8
SUNsung <weatherius@gmail.com>
2019-01-02 01:39:56 +0200

    Rewrite [00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb] File in: 2019-01-02 01:39:55

--- ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
+++ ruby/00xxlCQA4UEPE00Iq8GhwlWf4k00l6jaWEs.Uw00qkV.rb
@@ -1,58 +1,87 @@
 
         
-            def self.cleanup
-      cleanup_cellar
-      cleanup_cache
-      cleanup_logs
-      unless ARGV.dry_run?
-        cleanup_lockfiles
-        rm_DS_Store
+              it 'adds docset_min_xcode_version param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            docset_min_xcode_version: '6.4'
+          )
+        end').runner.execute(:test)
+    
+          it 'generates the correct git command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          git_commit(path: './fastlane/README.md', message: 'message')
+        end').runner.execute(:test)
+    
+            expect(result).to include(' oclint -report-type=pmd -o=report_path.xml ')
+        expect(result).to include(' -max-priority-1=10 ')
+        expect(result).to include(' -max-priority-2=20 ')
+        expect(result).to include(' -max-priority-3=30 ')
+        expect(result).to include(' -rc=LONG_LINE=200 -rc=LONG_METHOD=200 ')
+        expect(result).to include(' -rule DoubleNegative -rule DeadCode ')
+        expect(result).to include(' -disable-rule GotoStatement -disable-rule ShortVariableName ')
+        expect(result).to include(' -list-enabled-rules ')
+        expect(result).to include(' -enable-clang-static-analyzer ')
+        expect(result).to include(' -enable-global-analysis ')
+        expect(result).to include(' -allow-duplicated-violations ')
       end
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
     end
     
-          # Find commands in Homebrew/dev-cmd
-      if ARGV.homebrew_developer?
-        puts
-        puts 'Built-in development commands'
-        puts_columns internal_development_commands
-      end
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
     
-      def describe_python
-    python = which 'python'
-    return 'N/A' if python.nil?
-    python_binary = Utils.popen_read python, '-c', 'import sys; sys.stdout.write(sys.executable)'
-    python_binary = Pathname.new(python_binary).realpath
-    if python == python_binary
-      python
-    else
-      '#{python} => #{python_binary}'
-    end
+      # Extracts raw content DIV from template, used for page description as {{ content }}
+  # contains complete sub-template code on main page level
+  def raw_content(input)
+    /<div class='entry-content'>(?<content>[\s\S]*?)<\/div>\s*<(footer|\/article)>/ =~ input
+    return (content.nil?) ? input : content
   end
     
-        # Exclude cache, logs, and repository, if they are located under the prefix.
-    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
-      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
-    end
-    dirs.delete 'etc'
-    dirs.delete 'var'
-    
-      def install_core_tap_if_necessary
-    core_tap = CoreTap.instance
-    return if core_tap.installed?
-    CoreTap.ensure_installed! :quiet => false
-    revision = core_tap.git_head
-    ENV['HOMEBREW_UPDATE_BEFORE_HOMEBREW_HOMEBREW_CORE'] = revision
-    ENV['HOMEBREW_UPDATE_AFTER_HOMEBREW_HOMEBREW_CORE'] = revision
-  end
-end
-    
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
-    
-        def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
+          Dir.chdir(file_path) do
+        contents = file.read
+        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
+          contents = $1.lstrip
+        end
+        contents = pre_filter(contents)
+        if @raw
+          contents
+        else
+          partial = Liquid::Template.parse(contents)
+          context.stack do
+            partial.render(context)
+          end
+        end
+      end
     end
   end
 end
