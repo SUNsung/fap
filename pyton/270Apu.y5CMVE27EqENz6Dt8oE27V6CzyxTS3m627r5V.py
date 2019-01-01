22763fc644e65dfa469a3247298d0cb57f1b057b
SUNsung <weatherius@gmail.com>
2019-01-02 00:29:47 +0200

    Rewrite [270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py] File in: 2019-01-02 00:29:47

--- pyton/270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py
+++ pyton/270Apu.y5CMVE27EqENz6Dt8oE27V6CzyxTS3m627r5V.py
@@ -1,86 +1,223 @@
 
         
-        Provides utility functions that are consumed internally by Requests
-which depend on extremely few external helpers (such as compat)
-'''
-    
-    try:
-    import simplejson as json
-except ImportError:
-    import json
-    
-            :rtype: bool
-        '''
-        domains = []
-        for cookie in iter(self):
-            if cookie.domain is not None and cookie.domain in domains:
-                return True
-            domains.append(cookie.domain)
-        return False  # there is only one domain in jar
-    
-    This module provides the capabilities for the Requests hooks system.
-    
-    import os
-import copy
-import filecmp
-from io import BytesIO
-import zipfile
-from collections import deque
+            def mapper(self, _, line):
+        yield line, 1
     
     
-def post(url, data=None, json=None, **kwargs):
-    r'''Sends a POST request.
+class State(Enum):
+    unvisited = 0
+    visited = 1
     
-        '''
-    deps = {
-        'ssl': ['setenvif', 'mime']
-    }
-    return deps.get(mod_name, [])
+        def get(self, key):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                return item.value
+        raise KeyError('Key not found')
     
+        def reduce_priority_link_to_crawl(self, url):
+        '''Reduce the priority of a link in `links_to_crawl` to avoid cycles.'''
+        pass
     
-MOD_SSL_CONF_DEST = 'options-ssl-apache.conf'
-'''Name of the mod_ssl config file as saved in `IConfig.config_dir`.'''
+        # general training summaries
+    self.lr_summ = tf.summary.scalar('Learning rate', self.learning_rate)
+    self.kl_weight_summ = tf.summary.scalar('KL weight', self.kl_weight)
+    self.l2_weight_summ = tf.summary.scalar('L2 weight', self.l2_weight)
+    self.corr_cost_summ = tf.summary.scalar('Corr cost', self.weight_corr_cost)
+    self.grad_global_norm_summ = tf.summary.scalar('Gradient global norm',
+                                                   self.grad_global_norm)
+    if hps.co_dim > 0:
+      self.atau_summ = [None] * hps.co_dim
+      self.pvar_summ = [None] * hps.co_dim
+      for c in range(hps.co_dim):
+        self.atau_summ[c] = \
+            tf.summary.scalar('AR Autocorrelation taus ' + str(c),
+                              tf.exp(self.prior_zs_ar_con.logataus_1xu[0,c]))
+        self.pvar_summ[c] = \
+            tf.summary.scalar('AR Variances ' + str(c),
+                              tf.exp(self.prior_zs_ar_con.logpvars_1xu[0,c]))
     
-    from certbot import errors
+      wasserstein_loss = real_avg - fake_avg
+  return wasserstein_loss
+
     
-    
-def get_github_url(app, view, path):
-    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
-    return (
-        github_fmt.format(app.config.edit_on_github_project, view,
-                          app.config.edit_on_github_branch,
-                          app.config.edit_on_github_src_path, path))
-    
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
-    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
-    vol.Optional(CONF_DISCOVERY, default=True): cv.boolean,
-    vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
-    vol.Optional(CONF_IGNORED_CLICK_TYPES):
-        vol.All(cv.ensure_list, [vol.In(CLICK_TYPES)])
-})
-    
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.host = config[CONF_HOST]
-        self.username = config[CONF_USERNAME]
-        self.password = config[CONF_PASSWORD]
-        self.last_results = []
-        data = self.get_actiontec_data()
-        self.success_init = data is not None
-        _LOGGER.info('canner initialized')
-    
-        def get_menu(self):
-        '''Return the products from the closest stores menu.'''
-        self.update_closest_store()
-        if self.closest_store is None:
-            _LOGGER.warning('Cannot get menu. Store may be closed')
-            return []
-        menu = self.closest_store.get_menu()
-        product_entries = []
+      batch_of_indices_predictions = []
     
     
-def setup(hass, config):
-    '''Set up the Emoncms history component.'''
-    conf = config[DOMAIN]
-    whitelist = conf.get(CONF_WHITELIST)
+def _create_attention_score_fn(name,
+                               num_units,
+                               attention_option,
+                               reuse,
+                               dtype=tf.float32):
+  '''Different ways to compute attention scores.
+    
+        @property
+    def config(self):
+        if not hasattr(self, '_config'):
+            self._config = Config(directory=self.config_dir)
+            if self._config.is_new():
+                self._config.save()
+            else:
+                self._config.load()
+        return self._config
+    
+        if req_b and resp:
+        # Request/Response separator.
+        output.append([b'\n\n'])
+    
+        @staticmethod
+    def make_header(username, password):
+        credentials = u'%s:%s' % (username, password)
+        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
+        return 'Basic %s' % token
+    
+    
+ENTRY_POINT_NAMES = [
+    'httpie.plugins.auth.v1',
+    'httpie.plugins.formatter.v1',
+    'httpie.plugins.converter.v1',
+    'httpie.plugins.transport.v1',
+]
+    
+    
+setup(
+    name='httpie',
+    version=httpie.__version__,
+    description=httpie.__doc__.strip(),
+    long_description=long_description(),
+    url='http://httpie.org/',
+    download_url='https://github.com/jakubroztocil/httpie',
+    author=httpie.__author__,
+    author_email='jakub@roztocil.co',
+    license=httpie.__licence__,
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'http = httpie.__main__:main',
+        ],
+    },
+    extras_require=extras_require,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development',
+        'Topic :: System :: Networking',
+        'Topic :: Terminals',
+        'Topic :: Text Processing',
+        'Topic :: Utilities'
+    ],
+)
+
+    
+    from httpie import ExitStatus
+from httpie.core import main
+    
+            if data is None:
+            s = self._get_session_from_db()
+            if s:
+                data = self.decode(s.session_data)
+                self._cache.set(self.cache_key, data, self.get_expiry_age(expiry=s.expire_date))
+            else:
+                data = {}
+        return data
+    
+        @cached_property
+    def model(self):
+        return self.get_model_class()
+    
+        class Meta:
+        abstract = True
+        verbose_name = _('session')
+        verbose_name_plural = _('sessions')
+    
+    from django.contrib.sites.shortcuts import get_current_site
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.utils.http import http_date
+    
+        lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+                 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
+                 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
+                 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
+                 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
+                 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
+                 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,
+                 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
+                 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631,
+                 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709,
+                 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
+                 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877,
+                 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967,
+                 971, 977, 983, 991, 997]
+    
+    This is a pure Python implementation of Dynamic Programming solution to the longest_sub_array problem.
+    
+    def reformatHex(i):
+	'''[summary]
+	Converts the given integer into 8-digit hex number.
+    
+        difference = abs(predict - actual)
+    score = difference.mean()
+    
+    
+def b_expo_mod(a, b, c):
+    res = 1
+    while b > 0:
+        if b&1:
+            res = ((res%c) * (a%c)) % c
+    
+    def getEnglishCount(message):
+    message = message.upper()
+    message = removeNonLetters(message)
+    possibleWords = message.split()
+    
+    import PIL.Image
+import dlib
+import numpy as np
+    
+        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
+    context = multiprocessing
+    if 'forkserver' in multiprocessing.get_all_start_methods():
+        context = multiprocessing.get_context('forkserver')
+    
+            # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 25), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+    
+            # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    
+        # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(output)
+    print('Found {} faces in image.'.format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
+    
+        def test_command_line_interface(self):
+        target_string = 'obama.jpg,obama'
+        runner = CliRunner()
+        image_folder = os.path.join(os.path.dirname(__file__), 'test_images')
+        image_file = os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg')
+    
+    import face_recognition
+from flask import Flask, jsonify, request, redirect
