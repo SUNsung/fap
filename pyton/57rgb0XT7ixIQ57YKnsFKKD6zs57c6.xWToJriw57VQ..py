6b2bf8a1d798e103f7b6edf2e5c6236068a5b97c
SUNsung <weatherius@gmail.com>
2019-01-02 14:00:34 +0200

    Rewrite [57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py] File in: 2019-01-02 14:00:34

--- pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
+++ pyton/57rgb0XT7ixIQ57YKnsFKKD6zs57c6.xWToJriw57VQ..py
@@ -1,115 +1,240 @@
 
         
-          # Split the data, inputs, labels and times into train vs. validation.
-  rates_train, rates_valid = \
-      split_list_by_inds(rates, train_inds, valid_inds)
-  noisy_data_train, noisy_data_valid = \
-      split_list_by_inds(noisy_data, train_inds, valid_inds)
-  input_train, inputs_valid = \
-      split_list_by_inds(inputs, train_inds, valid_inds)
-  condition_labels_train, condition_labels_valid = \
-      split_list_by_inds(condition_labels, train_inds, valid_inds)
-  input_times_train, input_times_valid = \
-      split_list_by_inds(input_times, train_inds, valid_inds)
+        # TODO: ensure that history changes.
+
     
-    with tf.Session() as sess:
-  # restore the latest model ckpt
-  if FLAGS.checkpoint_path == 'SAMPLE_CHECKPOINT':
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    model_checkpoint_path = os.path.join(dir_path, 'trained_itb/model-65000')
-  else:
-    model_checkpoint_path = FLAGS.checkpoint_path
-  try:
-    saver.restore(sess, model_checkpoint_path)
-    print ('Model restored from', model_checkpoint_path)
-  except:
-    assert False, ('No checkpoints to restore from, is the path %s correct?'
-                   %model_checkpoint_path)
+    containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
     
-      data_len = len(raw_data)
-  batch_len = data_len // batch_size
-  data = np.full([batch_size, batch_len], EOS_INDEX, dtype=np.int32)
-  for i in range(batch_size):
-    data[i] = raw_data[batch_len * i:batch_len * (i + 1)]
+    Commands:
+   update - Retrieve new lists of packages
+   upgrade - Perform an upgrade
+   install - Install new packages (pkg is libc6 not libc6.deb)
+   remove - Remove packages
+   autoremove - Remove automatically all unused packages
+   purge - Remove packages and config files
+   source - Download source archives
+   build-dep - Configure build-dependencies for source packages
+   dist-upgrade - Distribution upgrade, see apt-get(8)
+   dselect-upgrade - Follow dselect selections
+   clean - Erase downloaded archive files
+   autoclean - Erase old downloaded archive files
+   check - Verify that there are no broken dependencies
+   changelog - Download and display the changelog for the given package
+   download - Download the binary package into the current directory
     
-          avg_baseline = tf.reduce_sum(
-          baseline_missing_list[t] * baseline_list[t], keep_dims=True) / (
-              num_missing + eps)
-      baseline = tf.tile(avg_baseline, multiples=[FLAGS.batch_size / 2])
-      baselines.append(baseline)
+    match_output = '''
+Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
+Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
+Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
+Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
+Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
+Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
+Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
+Fetched 89.2 kB in 0s (122 kB/s)
+Reading package lists... Done
+Building dependency tree
+Reading state information... Done
+8 packages can be upgraded. Run 'apt list --upgradable' to see them.
+'''
     
-      Returns:
-    attn: initial zero attention vector.
-  '''
+      aws help
+  aws <command> help
+  aws <command> <subcommand> help
+aws: error: argument operation: Invalid choice, valid choices are:
     
-    anchor = '###'
-min_entries_per_section = 3
-auth_keys = ['apiKey', 'OAuth', 'X-Mashape-Key', 'No']
-punctuation = ['.', '?', '!']
-https_keys = ['Yes', 'No']
-cors_keys = ['Yes', 'No', 'Unknown']
+        def __init__(self):
+        self.name = 'pg_trgm'
     
-        Useful for long-lived HTTP responses that stream by lines
-    such as the Twitter streaming API.
+        def __init__(self, session_key=None):
+        self._cache = caches[settings.SESSION_CACHE_ALIAS]
+        super().__init__(session_key)
     
-        def __call__(self, r):
-        '''
-        Override username/password serialization to allow unicode.
+        data_train = fetch_20newsgroups_vectorized(subset='train')
+    data_test = fetch_20newsgroups_vectorized(subset='test')
+    X_train = check_array(data_train.data, dtype=np.float32,
+                          accept_sparse='csc')
+    X_test = check_array(data_test.data, dtype=np.float32, accept_sparse='csr')
+    y_train = data_train.target
+    y_test = data_test.target
     
-        plugin_manager.register(Plugin)
+                gc.collect()
+            print('benchmarking lars_path (with Gram):', end='')
+            sys.stdout.flush()
+            tstart = time()
+            G = np.dot(X.T, X)  # precomputed Gram matrix
+            Xy = np.dot(X.T, y)
+            lars_path(X, y, Xy=Xy, Gram=G, max_iter=n_informative)
+            delta = time() - tstart
+            print('%0.3fs' % delta)
+            lars_gram[i_f, i_s] = delta
+    
+        label = 'scikit-learn singular value decomposition benchmark results'
+    fig = plt.figure(label)
+    ax = fig.gca(projection='3d')
+    for c, (label, timings) in zip('rbg', sorted(six.iteritems(results))):
+        X, Y = np.meshgrid(samples_range, features_range)
+        Z = np.asarray(timings).reshape(samples_range.shape[0],
+                                        features_range.shape[0])
+        # plot the actual surface
+        ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3,
+                        color=c)
+        # dummy point plot to stick the legend to since surface plot do not
+        # support legends (yet?)
+        ax.plot([1], [1], [1], color=c, label=label)
+    
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
+    
+                gc.collect()
+            print('- benchmarking RidgeRegression')
+            clf = Ridge(alpha=alpha, fit_intercept=False)
+            tstart = time()
+            clf.fit(X_train, y_train)
+            ridge_results[i, j, 0] = mean_squared_error(clf.predict(X_test),
+                                                        y_test)
+            ridge_results[i, j, 1] = time() - tstart
+    
+        opener = build_opener()
+    html_filename = os.path.join(html_folder, lang + '.html')
+    if not os.path.exists(html_filename):
+        print('Downloading %s' % page)
+        request = Request(page)
+        # change the User Agent to avoid being blocked by Wikipedia
+        # downloading a couple of articles should not be considered abusive
+        request.add_header('User-Agent', 'OpenAnything/1.0')
+        html_content = opener.open(request).read()
+        open(html_filename, 'wb').write(html_content)
+    
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (3 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
+    
+        if not os.path.exists(ARCHIVE_NAME):
+        print('Downloading dataset from %s (14 MB)' % URL)
+        opener = urlopen(URL)
+        with open(ARCHIVE_NAME, 'wb') as archive:
+            archive.write(opener.read())
+    
+    
+# Is a path a directory?
+# This follows symbolic links, so both islink() and isdir()
+# can be true for the same path on systems that support symlinks
+def isdir(s):
+    '''Return true if the pathname refers to an existing directory.'''
     try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            USERNAME + SEP_CREDENTIALS + PASSWORD,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
+        st = os.stat(s)
+    except (OSError, ValueError):
+        return False
+    return stat.S_ISDIR(st.st_mode)
     
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
+    from email.message import EmailMessage
+from email.policy import SMTP
     
+    # If we want to print a preview of the message content, we can extract whatever
+# the least formatted payload is and print the first three lines.  Of course,
+# if the message has no plain text part printing the first three lines of html
+# is probably useless, but this is just a conceptual example.
+simplest = msg.get_body(preferencelist=('plain', 'html'))
+print()
+print(''.join(simplest.get_content().splitlines(keepends=True)[:3]))
     
-def test_unicode_url(httpbin):
-    r = http(httpbin.url + u'/get?test=' + UNICODE)
-    assert HTTP_OK in r
-    assert r.json['args'] == {'test': UNICODE}
+    ##
     
-        server = ''
-    if probe_server:
-        server = ' and the best server is %s.' % best_server(probe_nat=probe_nat)
-    else:
-        new_pteredor()
+    def convert_point(s):
+    x, y = list(map(float, s.split(b';')))
+    return Point(x, y)
     
-            
-    def reset(self):
-        '''
-        reset the parser's state; subclasses must rewinds the input stream
-        '''
-        
-        # wack everything related to error recovery
-        if self._state is None:
-            # no shared state work to do
-            return
-        
-        self._state.following = []
-        self._state.errorRecovery = False
-        self._state.lastErrorIndex = -1
-        self._state.syntaxErrors = 0
-        # wack everything related to backtracking and memoization
-        self._state.backtracking = 0
-        if self._state.ruleMemo is not None:
-            self._state.ruleMemo = {}
+    DB_FILE = 'mydb'
     
-    import markdown
+    # If false, no module index is generated.
+#
+# texinfo_domain_indices = True
+    
+        def observe(call=None):
+        '''Observe three entities.'''
+        random_see('demo_paulus', 'Paulus')
+        random_see('demo_anne_therese', 'Anne Therese')
+    
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/device_tracker.swisscom/
+'''
+import logging
+    
+            self.username = config[CONF_USERNAME]
+        self.password = config[CONF_PASSWORD]
+    
+                    overwrite = service.data.get(ATTR_OVERWRITE)
+    
+        # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
+    
+        # If no valid image file was uploaded, show the file upload form:
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method='POST' enctype='multipart/form-data'>
+      <input type='file' name='file'>
+      <input type='submit' value='Upload'>
+    </form>
+    '''
+    
+            # Draw a label with a name below the face
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    
+        # Scale down image if it's giant so things run a little faster
+    if max(unknown_image.shape) > 1600:
+        pil_img = PIL.Image.fromarray(unknown_image)
+        pil_img.thumbnail((1600, 1600), PIL.Image.LANCZOS)
+        unknown_image = np.array(pil_img)
+    
+        # Print the location of each facial feature in this image
+    for facial_feature in face_landmarks.keys():
+        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
+    
+        # 将每一个人脸与已知样本图片比对
+    for face_encoding in face_encodings:
+        # 看是否属于奥巴马或者拜登
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = '<Unknown Person>'
+    
+        if len(unknown_face_encodings) > 0:
+        face_found = True
+        # 看看图片中的第一张脸是不是奥巴马
+        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
+        if match_results[0]:
+            is_obama = True
+    
+            release.delete()
+    
+            for environment in RangeQuerySetWrapperWithProgressBar(orm.Environment.objects.all()):
+            try:
+                with transaction.atomic():
+                    orm.EnvironmentProject.objects.create(
+                        environment=environment, project_id=environment.project_id
+                    )
+            except IntegrityError:
+                pass
