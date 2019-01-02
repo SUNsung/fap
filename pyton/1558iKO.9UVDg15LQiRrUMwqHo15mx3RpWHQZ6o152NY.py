5bc925029c878cf1f63b6e2c2f391f01c7c082e1
SUNsung <weatherius@gmail.com>
2019-01-02 11:28:04 +0200

    Rewrite [1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py] File in: 2019-01-02 11:28:04

--- pyton/1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py
+++ pyton/1558iKO.9UVDg15LQiRrUMwqHo15mx3RpWHQZ6o152NY.py
@@ -1,143 +1,119 @@
 
         
-        
-class Conversion(object):
-    
-    
-def generate_pathological_dataset(size):
-    # Triggers O(n^2) complexity on the original implementation.
-    return np.r_[np.arange(size),
-                 np.arange(-(size - 1), size),
-                 np.arange(-(size - 1), 1)]
-    
-        #------------------------------------------------------------
-    # varying k
-    k_results_build = dict([(alg, np.zeros(len(krange)))
-                            for alg in algorithms])
-    k_results_query = dict([(alg, np.zeros(len(krange)))
-                            for alg in algorithms])
-    
-    Compares SGD regression against coordinate descent and Ridge
-on synthetic data.
-'''
-    
-        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
-                                   'blob/{revision}/{package}/'
-                                   '{path}#L{lineno}')
-    '''
-    revision = _get_git_revision()
-    return partial(_linkcode_resolve, revision=revision, package=package,
-                   url_fmt=url_fmt)
-
-    
-    import os
-import tarfile
-from contextlib import closing
-    
-            idx = 0
-        while True:
-            url = urls[idx % len(urls)]
-            yield Request(url, dont_filter=True)
-            idx += 1
-    
-        def _genspider(self, module, name, domain, template_name, template_file):
-        '''Generate the spider module, based on the given template'''
-        tvars = {
-            'project_name': self.settings.get('BOT_NAME'),
-            'ProjectName': string_camelcase(self.settings.get('BOT_NAME')),
-            'module': module,
-            'name': name,
-            'domain': domain,
-            'classname': '%sSpider' % ''.join(s.capitalize() \
-                for s in module.split('_'))
-        }
-        if self.settings.get('NEWSPIDER_MODULE'):
-            spiders_module = import_module(self.settings['NEWSPIDER_MODULE'])
-            spiders_dir = abspath(dirname(spiders_module.__file__))
-        else:
-            spiders_module = None
-            spiders_dir = '.'
-        spider_file = '%s.py' % join(spiders_dir, module)
-        shutil.copyfile(template_file, spider_file)
-        render_templatefile(spider_file, **tvars)
-        print('Created spider %r using template %r ' % (name, \
-            template_name), end=('' if spiders_module else '\n'))
-        if spiders_module:
-            print('in module:\n  %s.%s' % (spiders_module.__name__, module))
+        '''
     
         def add_options(self, parser):
         ScrapyCommand.add_options(self, parser)
-        parser.add_option('-c', dest='code',
-            help='evaluate the code in the shell, print the result and exit')
-        parser.add_option('--spider', dest='spider',
-            help='use this spider')
-        parser.add_option('--no-redirect', dest='no_redirect', action='store_true', \
-            default=False, help='do not handle HTTP 3xx status codes and print response as-is')
+        parser.add_option('-l', '--list', dest='list', action='store_true',
+                          help='only list contracts, without checking them')
+        parser.add_option('-v', '--verbose', dest='verbose', default=False, action='store_true',
+                          help='print contract tests for all spiders')
     
-      def Extension(self):
-    '''File extension - text following the final period, includes that period.'''
-    return self.Split()[2]
+        def long_desc(self):
+        return ('Edit a spider using the editor defined in the EDITOR environment'
+                ' variable or else the EDITOR setting')
     
-        lowPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
-                 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
-                 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191,
-                 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257,
-                 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331,
-                 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401,
-                 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467,
-                 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
-                 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631,
-                 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709,
-                 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797,
-                 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877,
-                 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967,
-                 971, 977, 983, 991, 997]
+                # backward-compatible SSL/TLS method:
+            #
+            # * this will respect `method` attribute in often recommended
+            #   `ScrapyClientContextFactory` subclass
+            #   (https://github.com/scrapy/scrapy/issues/1429#issuecomment-131782133)
+            #
+            # * getattr() for `_ssl_method` attribute for context factories
+            #   not calling super(..., self).__init__
+            return CertificateOptions(verify=False,
+                        method=getattr(self, 'method',
+                                       getattr(self, '_ssl_method', None)),
+                        fixBrokenPeers=True,
+                        acceptableCiphers=DEFAULT_CIPHERS)
     
-    Usage:
-  1. define 'k' value, 'X' features array and 'hetrogeneity' empty list
-  
-  2. create initial_centroids,
-        initial_centroids = get_initial_centroids(
-            X, 
-            k, 
-            seed=0 # seed value for initial centroid generation, None for randomness(default=None)
-            )
+                    except ValueError as e:
+                    logger.warning(
+                        'Ignoring error while verifying certificate '
+                        'from host '{}' (exception: {})'.format(
+                            self._hostnameASCII, repr(e)))
     
-    
-import doctest
-doctest.testmod()
+    ### OUTPUT ###
+# dog σκύλος
+# parrot parrot
+# cat γάτα
+# bear bear
 
     
-    *Where can the pattern be used practically?
-The Factory Method can be seen in the popular web framework Django:
-http://django.wikispaces.asu.edu/*NEW*+Django+Design+Patterns For
-example, in a contact form of a web page, the subject and the message
-fields are created using the same form factory (CharField()), even
-though they have different implementations according to their
-purposes.
-    
-    
-if __name__ == '__main__':
-    import doctest
-    
-        def __init__(self, param):
-        # simple test to validate param value
-        if param in self._class_method_choices:
-            self.param = param
-        else:
-            raise ValueError('Invalid Value for Param: {0}'.format(param))
+    '''
+Reference: https://en.wikipedia.org/wiki/Delegation_pattern
+Author: https://github.com/IuryAlves
     
     from __future__ import print_function
+import os
+from os.path import lexists
+    
+    print()
     
     
-def reverse_item(item):
-    print(item[::-1])
+### OUTPUT ###
+# <NumObj: -1>
+# <NumObj: 0>
+# <NumObj: 1>
+# <NumObj: 2>
+# -- committed
+# <NumObj: 3>
+# <NumObj: 4>
+# <NumObj: 5>
+# -- rolled back
+# <NumObj: 2>
+# -- now doing stuff ...
+# -> doing stuff failed!
+# Traceback (most recent call last):
+# File 'memento.py', line 97, in <module>
+#     num_obj.do_stuff()
+#   File 'memento.py', line 52, in transaction
+#     raise e
+#   File 'memento.py', line 49, in transaction
+#     return self.method(obj, *args, **kwargs)
+#   File 'memento.py', line 70, in do_stuff
+#     self.increment()     # <- will fail and rollback
+#   File 'memento.py', line 65, in increment
+#     self.value += 1
+# TypeError: Can't convert 'int' object to str implicitly
+# <NumObj: 2>
+
     
-        def visit_B(self, node, *args, **kwargs):
-        print('visit_B ' + node.__class__.__name__)
     
-    *What does this example do?
-When the number of prototypes in an application can vary, it can be
-useful to keep a Dispatcher (aka, Registry or Manager). This allows
-clients to query the Dispatcher for a prototype before cloning a new
-instance.
+class BaseRegisteredClass(object):
+    __metaclass__ = RegistryHolder
+    '''
+        Any class that will inherits from BaseRegisteredClass will be included
+        inside the dict RegistryHolder.REGISTRY, the key being the name of the
+        class and the associated value, the class itself.
+    '''
+    pass
+    
+    '''
+@author: Gordeev Andrey <gordeev.and.and@gmail.com>
+    
+    
+def execute_replacement2(self):
+    print(self.name + ' from execute 2')
+    
+        def build_size(self):
+        self.size = 'Small'
+    
+    '''
+Port of the Java example of 'Parameter Injection' in
+'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
+(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
+http://xunitpatterns.com/Dependency%20Injection.html.
+    
+        def now(self):
+        current_time_is_always_midnight = '24:01'
+        return current_time_is_always_midnight
+
+    
+    ### OUTPUT ###
+# ['A', 'B', 'C', 'D']
+# [['A', 'B', 'C', 'D'], ['A', 'B', 'D'], ['A', 'C', 'D']]
+# ['A', 'B', 'D']
+
+    
+        data = Data()
