b6387fff67769ab75765c5c79e6f5f4e86ff9f89
SUNsung <weatherius@gmail.com>
2019-01-02 18:47:42 +0200

    Rewrite [00zBLWtqMM9qY00QkuzCJdXjm60028UszKeGc200MSP.py] File in: 2019-01-02 18:47:41

--- pyton/00zBLWtqMM9qY00QkuzCJdXjm60028UszKeGc200MSP.py
+++ pyton/00zBLWtqMM9qY00QkuzCJdXjm60028UszKeGc200MSP.py
@@ -1,111 +1,143 @@
 
         
-            self.seso_saver = tf.train.Saver(tf.global_variables(),
-                                     max_to_keep=hps.max_ckpt_to_keep)
-    
-    rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
-rnn_rngs = [np.random.RandomState(seed=FLAGS.synth_data_seed+1),
-            np.random.RandomState(seed=FLAGS.synth_data_seed+2)]
-T = FLAGS.T
-C = FLAGS.C
-N = FLAGS.N
-nreplications = FLAGS.nreplications
-E = nreplications * C
-train_percentage = FLAGS.train_percentage
-ntimesteps = int(T / FLAGS.dt)
+        
+def _DumpSentenceEmbedding(sentence, vocab):
+  '''Predict next words using the given prefix words.
     
     
-def _DumpEmb(vocab):
-  '''Dump the softmax weights and word embeddings to files.
+def generator_blended_forward_loss(gen_logits, gen_labels, dis_predictions,
+                                   is_real_input):
+  '''Computes the masked-loss for G.  This will be a blend of cross-entropy
+  loss where the true label is known and GAN loss where the true label has been
+  masked.
     
-        max_it = len(samples_range) * len(features_range)
-    for i_s, n_samples in enumerate(samples_range):
-        for i_f, n_features in enumerate(features_range):
-            it += 1
-            n_informative = n_features / 10
-            print('====================')
-            print('Iteration %03d of %03d' % (it, max_it))
-            print('====================')
-            # dataset_kwargs = {
-            #     'n_train_samples': n_samples,
-            #     'n_test_samples': 2,
-            #     'n_features': n_features,
-            #     'n_informative': n_informative,
-            #     'effective_rank': min(n_samples, n_features) / 10,
-            #     #'effective_rank': None,
-            #     'bias': 0.0,
-            # }
-            dataset_kwargs = {
-                'n_samples': 1,
-                'n_components': n_features,
-                'n_features': n_samples,
-                'n_nonzero_coefs': n_informative,
-                'random_state': 0
-            }
-            print('n_samples: %d' % n_samples)
-            print('n_features: %d' % n_features)
-            y, X, _ = make_sparse_coded_signal(**dataset_kwargs)
-            X = np.asfortranarray(X)
+        # Unstack to lists.
+    baseline_list = tf.unstack(baseline_half, axis=1)
+    baseline_missing = 1. - tf.cast(baseline_present_half, tf.float32)
+    baseline_missing_list = tf.unstack(baseline_missing, axis=1)
     
-        ###########################################################################
-    # Set SparseRandomProjection input
-    sparse_matrix_params = {
-        'n_components': opts.n_components,
-        'random_state': opts.random_seed,
-        'density': opts.density,
-        'eps': opts.eps,
-    }
+        auth.login()
+    response = client.get('/')
+    assert b'test title' in response.data
+    assert b'by test on 2018-01-01' in response.data
+    assert b'test\nbody' in response.data
+    assert b'href='/1/update'' in response.data
     
-                gc.collect()
-            print('- benchmarking SGD')
-            clf = SGDRegressor(alpha=alpha / n_train, fit_intercept=False,
-                               max_iter=max_iter, learning_rate='invscaling',
-                               eta0=.01, power_t=0.25, tol=1e-3)
+            url_prefix = self.options.get('url_prefix')
+        if url_prefix is None:
+            url_prefix = self.blueprint.url_prefix
+        #: The prefix that should be used for all URLs defined on the
+        #: blueprint.
+        self.url_prefix = url_prefix
     
-    from scipy.sparse.csr import csr_matrix
-import numpy as np
-from sklearn.linear_model.stochastic_gradient import SGDRegressor
-from sklearn.metrics import r2_score
     
-    # Scrapy version
-import pkgutil
-__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
-version_info = tuple(int(v) if v.isdigit() else v
-                     for v in __version__.split('.'))
-del pkgutil
+def dumps(obj, **kwargs):
+    '''Serialize ``obj`` to a JSON formatted ``str`` by using the application's
+    configured encoder (:attr:`~flask.Flask.json_encoder`) if there is an
+    application on the stack.
     
-    def _print_header(settings, inproject):
-    if inproject:
-        print('Scrapy %s - project: %s\n' % (scrapy.__version__, \
-            settings['BOT_NAME']))
+        def to_json(self, value):
+        return [self.serializer.tag(item) for item in value]
+    
+            # We attach the view class to the view function for two reasons:
+        # first of all it allows us to easily figure out what class-based
+        # view this thing came from, secondly it's also used for instantiating
+        # the view class so you can actually replace it with something else
+        # for testing purposes and debugging.
+        view.view_class = cls
+        view.__name__ = name
+        view.__doc__ = cls.__doc__
+        view.__module__ = cls.__module__
+        view.methods = cls.methods
+        view.provide_automatic_options = cls.provide_automatic_options
+        return view
+    
+    
+def to_native_string(string, encoding='ascii'):
+    '''Given a string object, regardless of type, returns a representation of
+    that string in the native string type, encoding and decoding where
+    necessary. This assumes ASCII unless told otherwise.
+    '''
+    if isinstance(string, builtin_str):
+        out = string
     else:
-        print('Scrapy %s - no active project\n' % scrapy.__version__)
+        if is_py2:
+            out = string.encode(encoding)
+        else:
+            out = string.decode(encoding)
     
-        def long_desc(self):
-        '''A long description of the command. Return short description when not
-        available. It cannot contain newlines, since contents will be formatted
-        by optparser which removes newlines and wraps text.
-        '''
-        return self.short_desc()
+        def inner(*suffix):
+        return urljoin(httpbin_url, '/'.join(suffix))
     
-            editor = self.settings['EDITOR']
+        def __setstate__(self, state):
+        # Can't handle by adding 'proxy_manager' to self.__attrs__ because
+        # self.poolmanager uses a lambda function, which isn't pickleable.
+        self.proxy_manager = {}
+        self.config = {}
+    
+    #: Python 2.x?
+is_py2 = (_ver[0] == 2)
+    
+    codes = LookupDict(name='status_codes')
+    
+    # Insert Requests' path into the system.
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('_themes'))
+    
+            if 'Authorization' in headers and self.should_strip_auth(response.request.url, url):
+            # If we get redirected to a new host, we should strip out any
+            # authentication headers.
+            del headers['Authorization']
+    
+    
+ALL_SSL_OPTIONS_HASHES = [
+    '0f81093a1465e3d4eaa8b0c14e77b2a2e93568b0fc1351c2b87893a95f0de87c',
+    '9a7b32c49001fed4cff8ad24353329472a50e86ade1ef9b2b9e43566a619612e',
+    'a6d9f1c7d6b36749b52ba061fff1421f9a0a3d2cfdafbd63c05d06f65b990937',
+    '7f95624dd95cf5afc708b9f967ee83a24b8025dc7c8d9df2b556bbc64256b3ff',
+    '394732f2bbe3e5e637c3fb5c6e980a1f1b90b01e2e8d6b7cff41dde16e2a756d',
+    '4b16fec2bcbcd8a2f3296d886f17f9953ffdcc0af54582452ca1e52f5f776f16',
+]
+'''SHA256 hashes of the contents of all versions of MOD_SSL_CONF_SRC'''
+    
+        @nonce.decoder
+    def nonce(value):  # pylint: disable=missing-docstring,no-self-argument
         try:
-            spidercls = self.crawler_process.spider_loader.load(args[0])
-        except KeyError:
-            return self._err('Spider not found: %s' % args[0])
+            return jose.decode_b64jose(value)
+        except jose.DeserializationError as error:
+            # TODO: custom error
+            raise jose.DeserializationError('Invalid nonce: {0}'.format(error))
     
-    from scrapy.commands import ScrapyCommand
-from scrapy.http import Request
-from scrapy.exceptions import UsageError
-from scrapy.utils.datatypes import SequenceExclude
-from scrapy.utils.spider import spidercls_for_request, DefaultSpider
+        :returns: Augeas path to vhost relative to the containing file
+    :rtype: str
     
-                if depth < opts.depth:
-                for req in requests:
-                    req.meta['_depth'] = depth + 1
-                    req.meta['_callback'] = req.callback
-                    req.callback = callback
-                return requests
+    REWRITE_HTTPS_ARGS = [
+    '^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[L,NE,R=permanent]']
+'''Apache version<2.3.9 rewrite rule arguments used for redirections to
+https vhost'''
     
-            general form:
-        @returns request(s)/item(s) [min=1 [max]]
+    # If true, show URL addresses after external links.
+#man_show_urls = False
+    
+        print('Writing private key to file %s_privkey.txt...' % name)
+    with open('%s_privkey.txt' % name, 'w') as fo:
+        fo.write('%s,%s,%s' % (keySize, privateKey[0], privateKey[1]))
+    
+    print(longestSub([4,8,7,5,1,12,2,3,9]))
+print(longestSub([9,8,7,6,5,7]))
+    
+                if (arr[i-1] <= j):
+                dp[i][j] = dp[i][j] or dp[i-1][j-arr[i-1]]
+    
+        def mean_squared_error(self, labels, prediction):
+        '''
+        mean_squared_error:
+        @param labels: a one dimensional numpy array 
+        @param prediction: a floating point value
+        return value: mean_squared_error calculates the error if prediction is used to estimate the labels
+        '''
+        if labels.ndim != 1:
+            print('Error: Input labels must be one dimensional')
+    
+        difference = abs(predict - actual)
+    score = difference.mean()
