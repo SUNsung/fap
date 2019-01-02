f5b35b1fb8259cc9e12c8944d163a26b4d3d6b33
SUNsung <weatherius@gmail.com>
2019-01-02 21:13:37 +0200

    Rewrite [52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp] File in: 2019-01-02 21:13:37

--- cccccc/52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp
+++ cccccc/52Ib8VUs.nJIM52PhZ1nEz32Mk52jfuzLA..aY65239b.cpp
@@ -1,104 +1,98 @@
 
         
-        /**
- * Class to iterate over tesseract page structure, providing access to all
- * levels of the page hierarchy, without including any tesseract headers or
- * having to handle any tesseract structures.
- * WARNING! This class points to data held within the TessBaseAPI class, and
- * therefore can only be used while the TessBaseAPI class still exists and
- * has not been subjected to a call of Init, SetImage, Recognize, Clear, End
- * DetectOS, or anything else that changes the internal PAGE_RES.
- * See apitypes.h for the definition of PageIteratorLevel.
- * See also ResultIterator, derived from PageIterator, which adds in the
- * ability to access OCR output with text-specific methods.
- */
-    
-    #endif  // TESSERACT_CCMAIN_PARAGRAPHS_H_
-
-    
-    
-    {  for (std::map<int, ParamContent*>::iterator iter = vcMap.begin();
-                                          iter != vcMap.end();
-                                          ++iter) {
-    ParamContent* cur = iter->second;
-    if (!changes_only || cur->HasChanged()) {
-      fprintf(fp, '%-25s   %-12s   # %s\n',
-              cur->GetName(), cur->GetValue().string(), cur->GetDescription());
+          // Do n memtable compactions, each of which produces an sstable
+  // covering the range [small,large].
+  void MakeTables(int n, const std::string& small, const std::string& large) {
+    for (int i = 0; i < n; i++) {
+      Put(small, 'begin');
+      Put(large, 'end');
+      dbfull()->TEST_CompactMemTable();
     }
   }
-  fclose(fp);
+    
+    
+    {  ParsedInternalKey() { }  // Intentionally left uninitialized (for speed)
+  ParsedInternalKey(const Slice& u, const SequenceNumber& seq, ValueType t)
+      : user_key(u), sequence(seq), type(t) { }
+  std::string DebugString() const;
+};
+    
+    std::string SSTTableFileName(const std::string& dbname, uint64_t number) {
+  assert(number > 0);
+  return MakeFileName(dbname, number, 'sst');
 }
-#endif // GRAPHICS_DISABLED
+    
+    #ifndef STORAGE_LEVELDB_DB_FILENAME_H_
+#define STORAGE_LEVELDB_DB_FILENAME_H_
+    
+    #endif  // STORAGE_LEVELDB_DB_LOG_FORMAT_H_
 
     
-      // Construct the cartesian product of the best_choices of word(1) and word2.
-  WERD_CHOICE_LIST joined_choices;
-  WERD_CHOICE_IT jc_it(&joined_choices);
-  WERD_CHOICE_IT bc1_it(&word->best_choices);
-  WERD_CHOICE_IT bc2_it(&word2->best_choices);
-  int num_word1_choices = word->best_choices.length();
-  int total_joined_choices = num_word1_choices;
-  // Nota Bene: For the main loop here, we operate only on the 2nd and greater
-  // word2 choices, and put them in the joined_choices list. The 1st word2
-  // choice gets added to the original word1 choices in-place after we have
-  // finished with them.
-  int bc2_index = 1;
-  for (bc2_it.forward(); !bc2_it.at_first(); bc2_it.forward(), ++bc2_index) {
-    if (total_joined_choices >= kTooManyAltChoices &&
-        bc2_index > kAltsPerPiece)
-      break;
-    int bc1_index = 0;
-    for (bc1_it.move_to_first(); bc1_index < num_word1_choices;
-        ++bc1_index, bc1_it.forward()) {
-      if (total_joined_choices >= kTooManyAltChoices &&
-          bc1_index > kAltsPerPiece)
-        break;
-      WERD_CHOICE *wc = new WERD_CHOICE(*bc1_it.data());
-      *wc += *bc2_it.data();
-      jc_it.add_after_then_move(wc);
-      ++total_joined_choices;
-    }
-  }
-  // Now that we've filled in as many alternates as we want, paste the best
-  // choice for word2 onto the original word alt_choices.
-  bc1_it.move_to_first();
-  bc2_it.move_to_first();
-  for (bc1_it.mark_cycle_pt(); !bc1_it.cycled_list(); bc1_it.forward()) {
-    *bc1_it.data() += *bc2_it.data();
-  }
-  bc1_it.move_to_last();
-  bc1_it.add_list_after(&joined_choices);
+      // Make a bunch of uncompacted log files.
+  uint64_t old_log = FirstLogFile();
+  MakeLogFile(old_log+1, 1000, 'hello', 'world');
+  MakeLogFile(old_log+2, 1001, 'hi', 'there');
+  MakeLogFile(old_log+3, 1002, 'foo', 'bar2');
     
-    // Class to hold an array of bounding boxes for an output word and
-// the bounding box of the whole word.
-class BoxWord {
- public:
-  BoxWord();
-  explicit BoxWord(const BoxWord& src);
-  ~BoxWord() = default;
+      Status ConvertLogToTable(uint64_t log) {
+    struct LogReporter : public log::Reader::Reporter {
+      Env* env;
+      Logger* info_log;
+      uint64_t lognum;
+      virtual void Corruption(size_t bytes, const Status& s) {
+        // We print error messages for corruption, but continue repairing.
+        Log(info_log, 'Log #%llu: dropping %d bytes; %s',
+            (unsigned long long) lognum,
+            static_cast<int>(bytes),
+            s.ToString().c_str());
+      }
+    };
     }
     
-          case kLastType:
-        if (!in_fragmented_record) {
-          ReportCorruption(fragment.size(),
-                           'missing start of fragmented record(2)');
-        } else {
-          scratch->append(fragment.data(), fragment.size());
-          *record = Slice(*scratch);
-          last_record_offset_ = prospective_record_offset;
-          return true;
-        }
-        break;
-    
-      Status AddRecord(const Slice& slice);
-    
-    
-    {}  // namespace leveldb
-    
-    class SnapshotList {
- public:
-  SnapshotList() : head_(0) {
-    head_.prev_ = &head_;
-    head_.next_ = &head_;
-  }
+    namespace leveldb {
     }
+    
+    class DHTResponseMessage : public DHTAbstractMessage {
+protected:
+  virtual std::string toStringOptional() const { return A2STR::NIL; }
+    }
+    
+    DHTRoutingTable::DHTRoutingTable(const std::shared_ptr<DHTNode>& localNode)
+    : localNode_(localNode),
+      root_(make_unique<DHTBucketTreeNode>(
+          std::make_shared<DHTBucket>(localNode_))),
+      numBucket_(1),
+      taskQueue_{nullptr},
+      taskFactory_{nullptr}
+{
+}
+    
+    
+    {
+    {    nodes.push_back(node);
+  }
+  localNode_ = localNode;
+  nodes_ = nodes;
+  A2_LOG_INFO('DHT routing table was loaded successfully');
+}
+    
+    #include <vector>
+#include <string>
+#include <memory>
+    
+    namespace aria2 {
+    }
+    
+      virtual void executeTask() = 0;
+    
+      DHTTaskExecutor immediateTaskQueue_;
+    
+    public:
+  DHTTokenTracker();
+    
+    bool DHTUnknownMessage::isReply() const { return false; }
+    
+      virtual ~DHTUnknownMessage();
+    
+      void put(const std::string& hostname, const std::string& ipaddr,
+           uint16_t port);
