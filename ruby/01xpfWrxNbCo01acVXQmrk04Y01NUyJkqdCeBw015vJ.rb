cf85e3248f64879323abe5b265e436032e875296
SUNsung <weatherius@gmail.com>
2019-01-02 02:58:31 +0200

    Rewrite [01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb] File in: 2019-01-02 02:58:30

--- ruby/01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb
+++ ruby/01xpfWrxNbCo01acVXQmrk04Y01NUyJkqdCeBw015vJ.rb
@@ -1,130 +1,63 @@
 
         
-            def subpath_to(url)
-      base_url.subpath_to url, ignore_case: true
-    end
+                  def sanitized_method_name
+            @sanitized_method_name ||= @method_name.sub(/\?$/, '')
+          end
     
-        def initialize(name = nil, path = nil, type = nil)
-      self.name = name
-      self.path = path
-      self.type = type
+    module ActionView
+  module Helpers
+    module Tags # :nodoc:
+      class Label < Base # :nodoc:
+        class LabelBuilder # :nodoc:
+          attr_reader :object
     
-        DOCUMENT_RGX = /\A(?:\s|(?:<!--.*?-->))*<(?:\!doctype|html)/i
+    module ActionView #:nodoc:
+  # = Action View PathSet
+  #
+  # This class is used to store and access paths in Action View. A number of
+  # operations are defined so that you can search among the paths in this
+  # set and also perform operations on other +PathSet+ objects.
+  #
+  # A +LookupContext+ will use a +PathSet+ to store the paths in its context.
+  class PathSet #:nodoc:
+    include Enumerable
     
-        def read_file(path)
-      File.read(path)
-    rescue
-      instrument 'warn.doc', msg: 'Failed to open file: #{path}'
-      nil
-    end
-  end
+          it 'adds docset_bundle_id param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            docset_bundle_id: 'com.bundle.id'
+          )
+        end').runner.execute(:test)
+    
+            expect do
+          Fastlane::FastFile.new.parse('lane :test do
+            changelog_from_git_commits(merge_commit_filtering: 'invalid')
+          end').runner.execute(:test)
+        end.to raise_error(error_msg)
+      end
+    
+            expect(result[1]).to eq('security default-keychain -s ~/Library/Keychains/test.keychain')
+        expect(result[2]).to eq('security unlock-keychain -p testpassword ~/Library/Keychains/test.keychain')
+    
+          it 'handles the exclude_dirs parameter with no elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: [])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
+      end
+    
+    # Contributors should always provide a changelog when submitting a PR
+if github.pr_body.length < 5
+  warn('Please provide a changelog summary in the Pull Request description @#{github.pr_author}')
 end
-
     
-            css('label', 'h2 > em', 'h3 > em').each do |node|
-          node.name = 'code'
-        end
-    
-            css('pre.prettyprint').each do |node|
-          node.content = node.content.strip
-          node['data-language'] = 'dart' if node['class'].include?('dart')
-          node['data-language'] = 'html' if node.content.start_with?('<')
-          node.remove_attribute('class')
-        end
-    
-            # Remove root-level <div>
-        while div = at_css('h1 + div')
-          div.before(div.children)
-          div.remove
-        end
-    
-          def right_diff_line_number(id, line)
-        if line =~ /^@@/
-          m, ri                   = *line.match(/\+(\d+)/)
-          @right_diff_line_number = ri.to_i
-          @current_line_number    = @right_diff_line_number
-          ret                     = '...'
-        elsif line[0] == ?-
-          ret = ' '
-        elsif line[0] == ?+
-          ret                     = @right_diff_line_number.to_s
-          @right_diff_line_number += 1
-          @current_line_number    = @right_diff_line_number - 1
-        else
-          ret                     = @right_diff_line_number.to_s
-          @right_diff_line_number += 1
-          @current_line_number    = @right_diff_line_number - 1
-        end
-        ret
-      end
+        def puts(*args)
+      STDERR.puts *args unless @silence
     end
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
   end
-end
-
-    
-          def page_header
-        title
-      end
-    
-    context 'Precious::Views::Page' do
-  setup do
-    examples = testpath 'examples'
-    @path    = File.join(examples, 'test.git')
-    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
-    @wiki = Gollum::Wiki.new(@path)
-  end
-    
-      begin
-    require 'gollum-lib'
-    wiki = Gollum::Wiki.new(gollum_path, wiki_options)
-    if !wiki.exist? then
-      raise Gollum::InvalidGitRepositoryError
-    end
-    if wiki_options[:plantuml_url]
-      Gollum::Filter::PlantUML.configure do |config|
-        puts 'Using #{wiki_options[:plantuml_url]} as PlantUML endpoint'
-        config.url = wiki_options[:plantuml_url]
-      end
-    end
-    puts
-    puts 'Loaded Gollum wiki at:'
-    puts '#{File.expand_path(gollum_path).inspect}'
-    puts
-    puts 'Example API calls:'
-    puts %(    page = wiki.page('page-name'))
-    puts %(    # => <Gollum::Page>)
-    puts
-    puts %(    page.raw_data)
-    puts %(    # => '# My wiki page')
-    puts
-    puts %(    page.formatted_data)
-    puts %(    # => '<h1>My wiki page</h1>')
-    puts
-    puts 'Full API documentation at:'
-    puts 'https://github.com/gollum/gollum-lib'
-    puts
-    IRB.start_session(binding)
-  rescue Gollum::InvalidGitRepositoryError, Gollum::NoSuchPathError
-    puts 'Invalid Gollum wiki at #{File.expand_path(gollum_path).inspect}'
-    exit 0
-  end
-else
-  require 'gollum/app'
-  Precious::App.set(:gollum_path, gollum_path)
-  Precious::App.set(:wiki_options, wiki_options)
-  Precious::App.settings.mustache[:templates] = wiki_options[:template_dir] if wiki_options[:template_dir]
-    
-      context 'same_line style' do
-    let(:cop_config) { { 'EnforcedStyle' => 'same_line' } }
-    
-            expect(cop.highlights).to eq([close])
-        expect(cop.messages)
-          .to eq([described_class::SAME_LINE_MESSAGE])
-      end
-    
-          # A helper class for comparing the positions of different parts of a
-      # `pair` node.
-      class HashElementDelta
-        def initialize(first, second)
-          @first = first
-          @second = second
