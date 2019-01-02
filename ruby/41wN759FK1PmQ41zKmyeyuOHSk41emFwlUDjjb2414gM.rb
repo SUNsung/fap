6e27949ab5c502fe4958cabc6a665bb62824a430
SUNsung <weatherius@gmail.com>
2019-01-02 03:34:54 +0200

    Rewrite [41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb] File in: 2019-01-02 03:34:53

--- ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
+++ ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
@@ -1,175 +1,105 @@
 
         
-              File.write(new_path, '1')
-      false
+            env.each do |key, value|
+      ENV[key] = value
     end
-    
-          xcode_outdated = false
-      begin
-        unless FastlaneCore::Helper.xcode_at_least?(Fastlane::MINIMUM_XCODE_RELEASE)
-          xcode_outdated = true
-        end
-      rescue
-        # We don't care about exceptions here
-        # We'll land here if the user doesn't have Xcode at all for example
-        # which is fine for someone who uses fastlane just for Android project
-        # What we *do* care about is when someone links an old version of Xcode
-      end
-    
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            tag: '#{tag}',
-          })
-        end').runner.execute(:test)
-    
-          it 'does not add a cache_builds flag to command if cache_builds is set to false' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              cache_builds: false
-            )
-          end').runner.execute(:test)
-    
-          it 'generates the correct git command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          git_commit(path: './fastlane/README.md', message: 'message')
-        end').runner.execute(:test)
-    
-          it 'works with select regex when regex is string' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              select_regex: \'\/AppDelegate\'
-            )
-          end').runner.execute(:test)
-    
-          it '#environment_variable_name_profile_name uses the correct env variable' do
-        result = Match::Utils.environment_variable_name_profile_name(app_identifier: 'tools.fastlane.app', type: 'appstore')
-        expect(result).to eq('sigh_tools.fastlane.app_appstore_profile-name')
-      end
-    
-    if File.exist?(file_path)
-  junit.parse(file_path)
-  junit.headers = [:name, :file]
-  junit.report
-else
-  puts('Couldn't find any test artifacts in path #{file_path}')
+    tasks.each do |task|
+      print_header('Running task: #{task}, env: #{env}')
+      Rake::Task[task].execute
+    end
+  end
 end
-
     
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
-          #
-          # @return [String] encoded principal
-          def encode
-            encoded = ''
-            encoded << encode_name_type
-            encoded << [components.length].pack('N')
-            encoded << encode_realm
-            encoded << encode_components
-    
-                encoded
+              if scanner.eos?
+            node = Tree::CssImportNode.new(str, [], supports_condition)
+          else
+            media_parser = Sass::SCSS::Parser.new(scanner,
+              @options[:filename], @options[:importer],
+              @line, str.source_range.end_pos.offset)
+            media = media_parser.parse_media_query_list
+            end_pos = Sass::Source::Position.new(@line, media_parser.offset + 1)
+            node = Tree::CssImportNode.new(str, media.to_a, supports_condition)
           end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::LastReque from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
-Gem.use_paths(LogStash::Environment.logstash_gem_home)
-    
-          packet_gem.pack
-    
-          PluginManager.ui.info('Installing file: #{local_file}')
-      uncompressed_path = uncompress(local_file)
-      PluginManager.ui.debug('Pack uncompressed to #{uncompressed_path}')
-      pack = LogStash::PluginManager::PackInstaller::Pack.new(uncompressed_path)
-      raise PluginManager::InvalidPackError, 'The pack must contains at least one plugin' unless pack.valid?
-    
-      # create list of plugins to update
-  def plugins_to_update(previous_gem_specs_map)
-    if update_all?
-      previous_gem_specs_map.values.map{|spec| spec.name}
-    else
-      # If the plugins isn't available in the gemspec or in 
-      # the gemfile defined with a local path, we assume the plugins is not
-      # installed.
-      not_installed = plugins_arg.select{|plugin| !previous_gem_specs_map.has_key?(plugin.downcase) && !gemfile.find(plugin) }
-      signal_error('Plugin #{not_installed.join(', ')} is not installed so it cannot be updated, aborting') unless not_installed.empty?
-      plugins_arg
-    end
-  end
-    
-      it 'returns the pipeline id' do
-    expect(subject.pipeline_id).to eq(pipeline_id)
-  end
-    
-        desc 'Halt all VM's involved in the acceptance test round'
-    task :halt, :platform do |t, args|
-      config   = PlatformConfig.new
-      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
-      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
-    
-              it 'fails when installing a non logstash plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
-            expect(command).not_to install_successfully
-          end
-    
-          it 'display a list of installed plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
-    
-        context 'update a specific plugin' do
-      it 'has executed successfully' do
-        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
-        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
-        expect(logstash).not_to have_installed?(plugin_name, previous_version)
-      end
-    end
-    
-    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
-
-    
-          cache(gist, file, data.body) unless @cache_disabled
-      data.body
-    end
-    
-    Liquid::Template.register_tag('img', Jekyll::ImageTag)
-
-    
-    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
-
-    
-          Dir.chdir(file_path) do
-        contents = file.read
-        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
-          contents = $1.lstrip
         end
-        contents = pre_filter(contents)
-        if @raw
-          contents
+    
+        # @return [String] A description of the executable
+    def to_s
+      @opts.to_s
+    end
+    
+      gem.required_ruby_version = '>= 2.0'
+  gem.add_dependency 'airbrussh', '>= 1.0.0'
+  gem.add_dependency 'i18n'
+  gem.add_dependency 'rake', '>= 10.0.0'
+  gem.add_dependency 'sshkit', '>= 1.9.0'
+    
+      def exists?(type, path)
+    %Q{[ -#{type} '#{path}' ]}
+  end
+    
+          def add_property(key, value)
+        if respond_to?('#{key}=')
+          send('#{key}=', value)
         else
-          partial = Liquid::Template.parse(contents)
-          context.stack do
-            partial.render(context)
+          set(key, value)
+        end
+      end
+    
+          def add_host(host, properties={})
+        new_host = Server[host]
+        new_host.port = properties[:port] if properties.key?(:port)
+        # This matching logic must stay in sync with `Server#matches?`.
+        key = ServerKey.new(new_host.hostname, new_host.port)
+        existing = servers_by_key[key]
+        if existing
+          existing.user = new_host.user if new_host.user
+          existing.with(properties)
+        else
+          servers_by_key[key] = new_host.with(properties)
+        end
+      end
+    
+          def remember_location(key)
+        location = caller.find do |line|
+          IGNORED_LOCATIONS.none? { |i| line.include?(i) }
+        end
+        (locations[key] ||= []) << location
+      end
+    
+      not_found do
+    send_file(File.join(File.dirname(__FILE__), 'public', '404.html'), {:status => 404})
+  end
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
           end
         end
       end
+      rtn
     end
   end
-end
     
-    Liquid::Template.register_tag('video', Jekyll::VideoTag)
-    
-        def clear
-      @attachments = Hash.new { |h,k| h[k] = {} }
-    end
-    
-        # Swaps the height and width if necessary
-    def auto_orient
-      if EXIF_ROTATED_ORIENTATION_VALUES.include?(@orientation)
-        @height, @width = @width, @height
-        @orientation -= 4
+          unless file.file?
+        return 'File #{file} could not be found'
       end
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
     end
