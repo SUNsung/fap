25ca2fb61beb5c2c63ac93df65b166decbf5b785
SUNsung <weatherius@gmail.com>
2019-01-02 20:17:28 +0200

    Rewrite [27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp] File in: 2019-01-02 20:17:27

--- cccccc/27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp
+++ cccccc/27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp
@@ -1,85 +1,268 @@
 
         
-        namespace tensorflow {
-    }
+        
+    {}  // namespace atom
+
     
-    #include <unordered_map>
     
-    #include 'tensorflow/core/framework/op.h'
-#include 'tensorflow/core/framework/op_kernel.h'
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    namespace tensorflow {
-    }
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    struct TFTensorDeleter {
-  void operator()(TF_Tensor* p) const { TF_DeleteTensor(p); }
+    {  DISALLOW_COPY_AND_ASSIGN(UvTaskRunner);
 };
     
+    namespace atom {
+    }
     
-    {  uint64 offset_;
-  RandomAccessFile* file_;    // Owned
-  io::RecordReader* reader_;  // Owned
-  string record_;
-  TF_DISALLOW_COPY_AND_ASSIGN(PyRecordReader);
-};
+    class Net : public mate::EventEmitter<Net> {
+ public:
+  static v8::Local<v8::Value> Create(v8::Isolate* isolate);
+    }
     
-    
-    {  tensorflow::DeviceNameUtils::ParsedName parsed_name;
-  if (!tensorflow::DeviceNameUtils::ParseFullName(node_def.device(),
-                                                  &parsed_name)) {
-    LOG(WARNING) << 'Failed to parse device from node_def: '
-                 << node_def.ShortDebugString();
-    return '';
-  }
-  string class_name = '';
-  tensorflow::FindKernelDef(tensorflow::DeviceType(parsed_name.type.c_str()),
-                            node_def, nullptr /* kernel_def */, &class_name)
-      .IgnoreError();
-  return class_name;
+    void AtomQuotaPermissionContext::RequestQuotaPermission(
+    const content::StorageQuotaParams& params,
+    int render_process_id,
+    const PermissionCallback& callback) {
+  callback.Run(response::QUOTA_PERMISSION_RESPONSE_ALLOW);
 }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    
+    {}  // namespace atom
+    
+    namespace atom {
+    }
+    
+    public:
+  /// Form storage for the given generic signature and its replacement
+  /// types and conformances.
+  static Storage *get(GenericSignature *genericSig,
+                      ArrayRef<Type> replacementTypes,
+                      ArrayRef<ProtocolConformanceRef> conformances);
+    
+    CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
+  llvm::SmallString<32> NameBuf(Name);
+  cache_attributes_t Attrs = {
+    CACHE_ATTRIBUTES_VERSION_2,
+    CBs.keyHashCB,
+    CBs.keyIsEqualCB,
+    nullptr,
+    CBs.keyDestroyCB,
+    CBs.valueReleaseCB,
+    nullptr,
+    nullptr,
+    CBs.UserData,
+    CBs.valueRetainCB,
+  };
+    }
+    
+    SourceLoc ClangDiagnosticConsumer::resolveSourceLocation(
+    const clang::SourceManager &clangSrcMgr,
+    clang::SourceLocation clangLoc) {
+  SourceManager &swiftSrcMgr = ImporterImpl.SwiftContext.SourceMgr;
+  SourceLoc loc;
+    }
+    
+    /// The result of out inference system
+struct IAMResult {
+  // The name to import as
+  DeclName name = {};
+    }
+    
+      // Set a new snapshot in the transaction
+  txn->SetSnapshot();
+  read_options.snapshot = db->GetSnapshot();
+    
+    #include 'rocksdb/cache.h'
+#include 'rocksdb/compaction_filter.h'
+#include 'rocksdb/db.h'
+#include 'rocksdb/options.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/table.h'
+#include 'rocksdb/utilities/options_util.h'
+    
+      // the number of compaction input records.
+  uint64_t num_input_records;
+  // the number of compaction input files.
+  size_t num_input_files;
+  // the number of compaction input files at the output level.
+  size_t num_input_files_at_output_level;
+    
+    // Take a default ColumnFamilyOptions 'base_options' in addition to a
+// map 'opts_map' of option name to option value to construct the new
+// ColumnFamilyOptions 'new_options'.
+//
+// Below are the instructions of how to config some non-primitive-typed
+// options in ColumnFOptions:
+//
+// * table_factory:
+//   table_factory can be configured using our custom nested-option syntax.
+//
+//   {option_a=value_a; option_b=value_b; option_c=value_c; ... }
+//
+//   A nested option is enclosed by two curly braces, within which there are
+//   multiple option assignments.  Each assignment is of the form
+//   'variable_name=value;'.
+//
+//   Currently we support the following types of TableFactory:
+//   - BlockBasedTableFactory:
+//     Use name 'block_based_table_factory' to initialize table_factory with
+//     BlockBasedTableFactory.  Its BlockBasedTableFactoryOptions can be
+//     configured using the nested-option syntax.
+//     [Example]:
+//     * {'block_based_table_factory', '{block_cache=1M;block_size=4k;}'}
+//       is equivalent to assigning table_factory with a BlockBasedTableFactory
+//       that has 1M LRU block-cache with block size equals to 4k:
+//         ColumnFamilyOptions cf_opt;
+//         BlockBasedTableOptions blk_opt;
+//         blk_opt.block_cache = NewLRUCache(1 * 1024 * 1024);
+//         blk_opt.block_size = 4 * 1024;
+//         cf_opt.table_factory.reset(NewBlockBasedTableFactory(blk_opt));
+//   - PlainTableFactory:
+//     Use name 'plain_table_factory' to initialize table_factory with
+//     PlainTableFactory.  Its PlainTableFactoryOptions can be configured using
+//     the nested-option syntax.
+//     [Example]:
+//     * {'plain_table_factory', '{user_key_len=66;bloom_bits_per_key=20;}'}
+//
+// * memtable_factory:
+//   Use 'memtable' to config memtable_factory.  Here are the supported
+//   memtable factories:
+//   - SkipList:
+//     Pass 'skip_list:<lookahead>' to config memtable to use SkipList,
+//     or simply 'skip_list' to use the default SkipList.
+//     [Example]:
+//     * {'memtable', 'skip_list:5'} is equivalent to setting
+//       memtable to SkipListFactory(5).
+//   - PrefixHash:
+//     Pass 'prfix_hash:<hash_bucket_count>' to config memtable
+//     to use PrefixHash, or simply 'prefix_hash' to use the default
+//     PrefixHash.
+//     [Example]:
+//     * {'memtable', 'prefix_hash:1000'} is equivalent to setting
+//       memtable to NewHashSkipListRepFactory(hash_bucket_count).
+//   - HashLinkedList:
+//     Pass 'hash_linkedlist:<hash_bucket_count>' to config memtable
+//     to use HashLinkedList, or simply 'hash_linkedlist' to use the default
+//     HashLinkedList.
+//     [Example]:
+//     * {'memtable', 'hash_linkedlist:1000'} is equivalent to
+//       setting memtable to NewHashLinkListRepFactory(1000).
+//   - VectorRepFactory:
+//     Pass 'vector:<count>' to config memtable to use VectorRepFactory,
+//     or simply 'vector' to use the default Vector memtable.
+//     [Example]:
+//     * {'memtable', 'vector:1024'} is equivalent to setting memtable
+//       to VectorRepFactory(1024).
+//   - HashCuckooRepFactory:
+//     Pass 'cuckoo:<write_buffer_size>' to use HashCuckooRepFactory with the
+//     specified write buffer size, or simply 'cuckoo' to use the default
+//     HashCuckooRepFactory.
+//     [Example]:
+//     * {'memtable', 'cuckoo:1024'} is equivalent to setting memtable
+//       to NewHashCuckooRepFactory(1024).
+//
+//  * compression_opts:
+//    Use 'compression_opts' to config compression_opts.  The value format
+//    is of the form '<window_bits>:<level>:<strategy>:<max_dict_bytes>'.
+//    [Example]:
+//    * {'compression_opts', '4:5:6:7'} is equivalent to setting:
+//        ColumnFamilyOptions cf_opt;
+//        cf_opt.compression_opts.window_bits = 4;
+//        cf_opt.compression_opts.level = 5;
+//        cf_opt.compression_opts.strategy = 6;
+//        cf_opt.compression_opts.max_dict_bytes = 7;
+//
+// @param base_options the default options of the output 'new_options'.
+// @param opts_map an option name to value map for specifying how 'new_options'
+//     should be set.
+// @param new_options the resulting options based on 'base_options' with the
+//     change specified in 'opts_map'.
+// @param input_strings_escaped when set to true, each escaped characters
+//     prefixed by '\' in the values of the opts_map will be further converted
+//     back to the raw string before assigning to the associated options.
+// @param ignore_unknown_options when set to true, unknown options are ignored
+//     instead of resulting in an unknown-option error.
+// @return Status::OK() on success.  Otherwise, a non-ok status indicating
+//     error will be returned, and 'new_options' will be set to 'base_options'.
+Status GetColumnFamilyOptionsFromMap(
+    const ColumnFamilyOptions& base_options,
+    const std::unordered_map<std::string, std::string>& opts_map,
+    ColumnFamilyOptions* new_options, bool input_strings_escaped = false,
+    bool ignore_unknown_options = false);
+    
+    #include 'rocksdb/db.h'
+#include 'rocksdb/status.h'
     
     
-    {  GTEST_DISALLOW_COPY_AND_ASSIGN_(ScopedFakeTestPartResultReporter);
+    {// Factor method to create a new persistent cache
+Status NewPersistentCache(Env* const env, const std::string& path,
+                          const uint64_t size,
+                          const std::shared_ptr<Logger>& log,
+                          const bool optimized_for_nvm,
+                          std::shared_ptr<PersistentCache>* cache);
+}  // namespace rocksdb
+
+    
+    
+    { private:
+  DB* db_;
+  const Snapshot* snapshot_;
 };
     
-      // Stores result of the assertion predicate.
-  bool success_;
-  // Stores the message describing the condition in case the expectation
-  // construct is not satisfied with the predicate's outcome.
-  // Referenced via a pointer to avoid taking too much stack frame space
-  // with test assertions.
-  internal::scoped_ptr< ::std::string> message_;
+    // Sometimes a family of microbenchmarks can be implemented with
+// just one routine that takes an extra argument to specify which
+// one of the family of benchmarks to run.  For example, the following
+// code defines a family of microbenchmarks for measuring the speed
+// of memcpy() calls of different lengths:
     
-      // Returns true iff the given string ends with the given suffix, ignoring
-  // case. Any string is considered to end with an empty suffix.
-  static bool EndsWithCaseInsensitive(
-      const std::string& str, const std::string& suffix);
+    namespace benchmark {
+enum LogColor {
+  COLOR_DEFAULT,
+  COLOR_RED,
+  COLOR_GREEN,
+  COLOR_YELLOW,
+  COLOR_BLUE,
+  COLOR_MAGENTA,
+  COLOR_CYAN,
+  COLOR_WHITE
+};
+    }
     
-    // Tests the c'tor that accepts a C string.
-TEST(MyString, ConstructorFromCString) {
-  const MyString s(kHelloString);
-  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
-  EXPECT_EQ(sizeof(kHelloString)/sizeof(kHelloString[0]) - 1,
-            s.Length());
+      // Do not print timeLabel on bigO and RMS report
+  if (run.report_big_o) {
+    Out << GetBigOString(run.complexity);
+  } else if (!run.report_rms) {
+    Out << GetTimeUnitString(run.time_unit);
+  }
+  Out << ',';
+    
+    void JSONReporter::ReportRuns(std::vector<Run> const& reports) {
+  if (reports.empty()) {
+    return;
+  }
+  std::string indent(4, ' ');
+  std::ostream& out = GetOutputStream();
+  if (!first_report_) {
+    out << ',\n';
+  }
+  first_report_ = false;
+    }
+    
+    inline LogType& operator<<(LogType& log, EndLType* m) {
+  if (log.out_) {
+    *log.out_ << m;
+  }
+  return log;
+}
+    
+    
+    {}  // end namespace benchmark
+
+    
+    #include 'internal_macros.h'
+    
+    BOOST_FORCEINLINE bool operator== (storage128_t const& left, storage128_t const& right) BOOST_NOEXCEPT
+{
+    return left.data[0] == right.data[0] && left.data[1] == right.data[1];
+}
+BOOST_FORCEINLINE bool operator!= (storage128_t const& left, storage128_t const& right) BOOST_NOEXCEPT
+{
+    return !(left == right);
 }
