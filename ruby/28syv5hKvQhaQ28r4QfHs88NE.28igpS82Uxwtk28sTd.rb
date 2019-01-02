81a75e5ab1a3c869d180a4adcd1ad12a4450af82
SUNsung <weatherius@gmail.com>
2019-01-02 14:46:38 +0200

    Rewrite [28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb] File in: 2019-01-02 14:46:37

--- ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
+++ ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
@@ -1,143 +1,48 @@
 
         
-                # Using the LDAP attributes configuration, find and return the first
-        # attribute with a value. For example, by default, when given 'email',
-        # this method looks for 'mail', 'email' and 'userPrincipalName' and
-        # returns the first with a value.
-        def attribute_value(attribute)
-          attributes = Array(config.attributes[attribute.to_s])
-          selected_attr = attributes.find { |attr| entry.respond_to?(attr) }
-    
-          def provider
-        oauth['provider']
+            Category.transaction do
+      staff.group_names = ['staff']
+      unless staff.save
+        puts staff.errors.full_messages
+        raise 'Failed to set permissions on the Staff category!'
       end
     
-          def perform(start_id, stop_id)
-        update = '
-          latest_merge_request_diff_id = (
-            SELECT MAX(id)
-            FROM merge_request_diffs
-            WHERE merge_requests.id = merge_request_diffs.merge_request_id
-          )'.squish
+            cmd = ['git tag']
     
-            def entity
-          'pipeline'
-        end
+            it 'supports lint mode option' do
+          result = Fastlane::FastFile.new.parse('lane :test do
+            swiftlint(
+              mode: :lint
+            )
+          end').runner.execute(:test)
     
-          it 'requires the passwords to match' do
-        visit new_admin_user_path
-        fill_in 'Email', with: 'test@test.com'
-        fill_in 'Username', with: 'usertest'
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: 'no_match'
-        click_on 'Create User'
-        expect(page).to have_text('Password confirmation doesn't match')
-      end
-    end
-    
-      describe '#recursively_interpolate_jsonpaths' do
-    it 'interpolates all string values in a structure' do
-      struct = {
-        :int => 5,
-        :string => 'this <escape $.works>',
-        :array => ['<works>', 'now', '<$.there.world>'],
-        :deep => {
-          :string => 'hello <there.world>',
-          :hello => :world
-        }
-      }
-      data = { :there => { :world => 'WORLD' }, :works => 'should work' }
-      expect(Utils.recursively_interpolate_jsonpaths(struct, data)).to eq({
-        :int => 5,
-        :string => 'this should+work',
-        :array => ['should work', 'now', 'WORLD'],
-        :deep => {
-          :string => 'hello WORLD',
-          :hello => :world
-        }
-      })
-    end
-  end
-    
-            @receiver = Agents::CannotBeScheduled.new(
-          name: 'Agent',
-          options: { foo: 'bar3' },
-          keep_events_for: 3.days,
-          propagate_immediately: true)
-        @receiver.user = users(:bob)
-        @receiver.sources << @sender
-        @receiver.memory[:test] = 1
-        @receiver.save!
-      end
-    
-          it 'raises ArgumentError if passed negative value' do
-        -> { warn '', uplevel: -2 }.should raise_error(ArgumentError)
-        -> { warn '', uplevel: -100 }.should raise_error(ArgumentError)
-      end
-    
-              # Encodes the checksum field
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_checksum
-            OpenSSL::ASN1::OctetString.new(checksum)
-          end
-        end
-      end
+        it 'recognizes an array as the first element of a command' do
+      message = 'A message'
+      command = command_from_args(['/usr/local/bin/git', 'git'], 'commit', '-m', message)
+      expect(command).to eq('/usr/local/bin/git commit -m #{message.shellescape}')
     end
   end
 end
     
-              # Decodes the nonce field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_nonce(input)
-            input.value[0].value.to_i
-          end
+            it 'auto converts '0' to the integer 0' do
+          c = [
+            FastlaneCore::ConfigItem.new(key: :int_value,
+                                         type: Integer)
+          ]
+          config = FastlaneCore::Configuration.create(c, {
+            int_value: '0'
+          })
     
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-              # Decodes the sname field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::PrincipalName]
-          def decode_sname(input)
-            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::LastRequest
-          #
-          # @param input [String, OpenSSL::ASN1::Sequence] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::Sequence
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode LastRequest, invalid input'
-            end
-    
-        # Converts the CSS template into Sass or SCSS code.
-    #
-    # @param fmt [Symbol] `:sass` or `:scss`, designating the format to return.
-    # @return [String] The resulting Sass or SCSS code
-    # @raise [Sass::SyntaxError] if there's an error parsing the CSS template
-    def render(fmt = :sass)
-      check_encoding!
-      build_tree.send('to_#{fmt}', @options).strip + '\n'
-    rescue Sass::SyntaxError => err
-      err.modify_backtrace(:filename => @options[:filename] || '(css)')
-      raise err
-    end
-    
-          if @options[:to] == @options[:from] && !@options[:in_place]
-        fmt = @options[:from]
-        raise 'Error: converting from #{fmt} to #{fmt} without --in-place'
+          # Returns the time the given Sass file was last modified.
+      #
+      # If the given file has been deleted or the time can't be accessed
+      # for some other reason, this should return nil.
+      #
+      # @param uri [String] The URI of the file to check.
+      #   Comes from a `:filename` option set on an engine returned by this importer.
+      # @param options [{Symbol => Object}] Options for the Sass file
+      #   containing the `@import` currently being checked.
+      # @return [Time, nil]
+      def mtime(uri, options)
+        Sass::Util.abstract(self)
       end
