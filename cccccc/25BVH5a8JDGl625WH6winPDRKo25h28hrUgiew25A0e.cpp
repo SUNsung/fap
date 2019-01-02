787a78683aa8941957a676b8625866ba1505b3e5
SUNsung <weatherius@gmail.com>
2019-01-02 22:01:39 +0200

    Rewrite [25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp] File in: 2019-01-02 22:01:38

--- cccccc/25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp
+++ cccccc/25BVH5a8JDGl625WH6winPDRKo25h28hrUgiew25A0e.cpp
@@ -1,254 +1,190 @@
 
         
-        #include <string>
-#include <vector>
+        protected Q_SLOTS:
+    void accept();
     
-    template <typename T, class Context>
-class FlexibleTopKOp : public Operator<Context> {
- public:
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-    }
+        QVariant value() const;
+    void setValue(const QVariant &value);
     
-    template <>
-void GluOp<float, CPUContext>::ComputeGlu(
-    const int M,
-    const int split_dim,
-    const int N,
-    const float* Xdata,
-    float* Ydata) {
-  const int xStride = 2 * split_dim * N;
-  const int yStride = split_dim * N;
-  for (int i = 0; i < M; ++i) {
-    const int idx = i * xStride;
-    const int idy = i * yStride;
-    for (int j = 0; j < split_dim; ++j) {
-      const int jN = j * N;
-      const int jdx1 = idx + jN;
-      const int jdx2 = idx + (j + split_dim) * N;
-      const int jdy = idy + jN;
-      for (int k = 0; k < N; ++k) {
-        const float x1 = Xdata[jdx1 + k];
-        const float x2 = Xdata[jdx2 + k];
-        Ydata[jdy + k] = x1 * sigmoid(x2);
-      }
-    }
-  }
-}
+        /* Check NULLs for conversion */
+    CHECK(secp256k1_ecdsa_sign(both, &normal_sig, message, privkey, NULL, NULL) == 1);
+    ecount = 0;
+    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, NULL, &recsig) == 0);
+    CHECK(ecount == 1);
+    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, &normal_sig, NULL) == 0);
+    CHECK(ecount == 2);
+    CHECK(secp256k1_ecdsa_recoverable_signature_convert(both, &normal_sig, &recsig) == 1);
     
-              const int dkernel_h = dilation_h * (kernel_h - 1) + 1;
-          const int dkernel_w = dilation_w * (kernel_w - 1) + 1;
-          CAFFE_ENFORCE(H >= dkernel_h);
-          CAFFE_ENFORCE(W >= dkernel_w);
-          const int out_h = (H + 2 * pad - dkernel_h) / stride_h + 1;
-          const int out_w = (W + 2 * pad - dkernel_w) / stride_w + 1;
-    
-    #endif /* #if !UCONFIG_NO_TRANSLITERATION */
-
-    
-    UnicodeString &ScientificNumberFormatter::SuperscriptStyle::format(
-        const UnicodeString &original,
-        FieldPositionIterator &fpi,
-        const UnicodeString &preExponent,
-        const DecimalFormatStaticSets &staticSets,
-        UnicodeString &appendTo,
-        UErrorCode &status) const {
-    if (U_FAILURE(status)) {
-        return appendTo;
-    }
-    FieldPosition fp;
-    int32_t copyFromOffset = 0;
-    while (fpi.next(fp)) {
-        switch (fp.getField()) {
-        case UNUM_EXPONENT_SYMBOL_FIELD:
-            appendTo.append(
-                    original,
-                    copyFromOffset,
-                    fp.getBeginIndex() - copyFromOffset);
-            copyFromOffset = fp.getEndIndex();
-            appendTo.append(preExponent);
-            break;
-        case UNUM_EXPONENT_SIGN_FIELD:
-            {
-                int32_t beginIndex = fp.getBeginIndex();
-                int32_t endIndex = fp.getEndIndex();
-                UChar32 aChar = original.char32At(beginIndex);
-                if (staticSets.fMinusSigns->contains(aChar)) {
-                    appendTo.append(
-                            original,
-                            copyFromOffset,
-                            beginIndex - copyFromOffset);
-                    appendTo.append(kSuperscriptMinusSign);
-                } else if (staticSets.fPlusSigns->contains(aChar)) {
-                    appendTo.append(
-                           original,
-                           copyFromOffset,
-                           beginIndex - copyFromOffset);
-                    appendTo.append(kSuperscriptPlusSign);
-                } else {
-                    status = U_INVALID_CHAR_FOUND;
-                    return appendTo;
-                }
-                copyFromOffset = endIndex;
-            }
-            break;
-        case UNUM_EXPONENT_FIELD:
-            appendTo.append(
-                    original,
-                    copyFromOffset,
-                    fp.getBeginIndex() - copyFromOffset);
-            if (!copyAsSuperscript(
-                    original,
-                    fp.getBeginIndex(),
-                    fp.getEndIndex(),
-                    appendTo,
-                    status)) {
-              return appendTo;
-            }
-            copyFromOffset = fp.getEndIndex();
-            break;
-        default:
-            break;
-        }
-    }
-    appendTo.append(
-            original, copyFromOffset, original.length() - copyFromOffset);
-    return appendTo;
-}
-    
-    UBool ScriptSet::test(UScriptCode script, UErrorCode &status) const {
-    if (U_FAILURE(status)) {
-        return FALSE;
-    }
-    if (script < 0 || script >= (int32_t)sizeof(bits) * 8) {
-        status = U_ILLEGAL_ARGUMENT_ERROR;
-        return FALSE;
-    }
-    uint32_t index = script / 32;
-    uint32_t bit   = 1 << (script & 31);
-    return ((bits[index] & bit) != 0);
-}
-    
-    void
-SelectFormat::parseObject(const UnicodeString& /*source*/,
-                        Formattable& /*result*/,
-                        ParsePosition& pos) const
+    static void CheckParseTorReplyMapping(std::string input, std::map<std::string,std::string> expected)
 {
-    // Parsing not supported.
-    pos.setErrorIndex(pos.getIndex());
+    BOOST_TEST_MESSAGE(std::string('CheckParseTorReplyMapping(') + input + ')');
+    auto ret = ParseTorReplyMapping(input);
+    BOOST_CHECK_EQUAL(ret.size(), expected.size());
+    auto r_it = ret.begin();
+    auto e_it = expected.begin();
+    while (r_it != ret.end() && e_it != expected.end()) {
+        BOOST_CHECK_EQUAL(r_it->first, e_it->first);
+        BOOST_CHECK_EQUAL(r_it->second, e_it->second);
+        r_it++;
+        e_it++;
+    }
 }
     
-        /**
-     * No limits on significant digits.
-     */
-    SignificantDigitInterval()
-            : fMax(INT32_MAX), fMin(0) { }
+    // To keep it as transparent as possible, the macros below have been generated
+// using python via the excellent cog.py code generation script.  This avoids
+// the need for a bunch of complex (but more general) preprocessor tricks as
+// used in boost.preprocessor.
+//
+// To rerun the code generation in place, use `cog.py -r tinyformat.h`
+// (see http://nedbatchelder.com/code/cog).  Alternatively you can just create
+// extra versions by hand.
     
-    UnicodeString &
-SmallIntFormatter::format(
-        int32_t smallPositiveValue,
-        const IntDigitCountRange &range,
-        UnicodeString &appendTo) {
-    int32_t digits = range.pin(gDigitCount[smallPositiveValue]);
-    }
+    // Bech32 is a string encoding format used in newer address types.
+// The output consists of a human-readable part (alphanumeric), a
+// separator character (1), and a base32 data section, the last
+// 6 characters of which are a checksum.
+//
+// For more information, see BIP 173.
     
-        if (matchLenData == data.length() /* normal match */
-        || (data.charAt(data.length() - 1) == 0x2e
-            && matchLenData == data.length() - 1 /* match without trailing dot */)) {
-        return matchLenText;
-    }
-    
-    UnicodeSet *SimpleDateFormatStaticSets::getIgnorables(UDateFormatField fieldIndex)
+    inline b3Quat b3QuatInvert(b3QuatConstArg q)
 {
-    UErrorCode status = U_ZERO_ERROR;
-    umtx_initOnce(gSimpleDateFormatStaticSetsInitOnce, &smpdtfmt_initSets, status);
-    if (U_FAILURE(status)) {
-        return NULL;
-    }
-    
-    switch (fieldIndex) {
-        case UDAT_YEAR_FIELD:
-        case UDAT_MONTH_FIELD:
-        case UDAT_DATE_FIELD:
-        case UDAT_STANDALONE_DAY_FIELD:
-        case UDAT_STANDALONE_MONTH_FIELD:
-            return gStaticSets->fDateIgnorables;
-            
-        case UDAT_HOUR_OF_DAY1_FIELD:
-        case UDAT_HOUR_OF_DAY0_FIELD:
-        case UDAT_MINUTE_FIELD:
-        case UDAT_SECOND_FIELD:
-        case UDAT_HOUR1_FIELD:
-        case UDAT_HOUR0_FIELD:
-            return gStaticSets->fTimeIgnorables;
-            
-        default:
-            return gStaticSets->fOtherIgnorables;
-    }
+	return (b3Quat)(-q.xyz, q.w);
 }
     
-    /**
- * Implement UnicodeFunctor
- */
-UnicodeFunctor* StringMatcher::clone() const {
-    return new StringMatcher(*this);
-}
+    This software is provided 'as-is', without any express or implied warranty.
+In no event will the authors be held liable for any damages arising from the use of this software.
+Permission is granted to anyone to use this software for any purpose, 
+including commercial applications, and to alter it and redistribute it freely, 
+subject to the following restrictions:
     
-        /**
-     * UnicodeFunctor API.  Cast 'this' to a UnicodeMatcher* pointer
-     * and return the pointer.
-     * @return the UnicodeMatcher point.
-     */
-    virtual UnicodeMatcher* toMatcher() const;
+    	void getInfo1NonVirtual (b3ConstraintInfo1* info,const b3RigidBodyData* bodies);
     
+    1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
     
-    {  int ret = x;
-  return ret;
-}
-    
-    #include 'modules/drivers/radar/conti_radar/protocol/object_quality_info_60c.h'
-#include 'modules/drivers/radar/conti_radar/protocol/const_vars.h'
+    	int		registerConvexPolyhedron(class b3ConvexUtility* convex);
     
     
     {
-    {}  // namespace hdmap
-}  // namespace apollo
-
-    
-    namespace apollo {
-namespace planning {
-    }
-    }
-    
-    STPoint::STPoint(const double s, const double t) : Vec2d(t, s) {}
-    
-    namespace apollo {
-namespace planning {
-    }
-    }
-    
-      MatrixXd mat_golden(10, 10);
-  // clang-format off
-  mat_golden <<
-   1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-  -1,  1,  0,  0,  0,  0,  0,  0,  0,  0,
-   0, -1,  1,  0,  0,  0,  0,  0,  0,  0,
-   0,  0, -1,  1,  0,  0,  0,  0,  0,  0,
-   0,  0,  0, -1,  1,  0,  0,  0,  0,  0,
-   0,  0,  0,  0, -1,  1,  0,  0,  0,  0,
-   0,  0,  0,  0,  0, -1,  1,  0,  0,  0,
-   0,  0,  0,  0,  0,  0, -1,  1,  0,  0,
-   0,  0,  0,  0,  0,  0,  0, -1,  1,  0,
-   0,  0,  0,  0,  0,  0,  0,  0, -1,  1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-      kernel.AddSecondOrderDerivativeMatrix(init_derivative, 1.0);
-    
-    Spline2d::Spline2d(const std::vector<double>& t_knots, const uint32_t order)
-    : t_knots_(t_knots), spline_order_(order) {
-  if (t_knots.size() > 1) {
-    for (uint32_t i = 1; i < t_knots_.size(); ++i) {
-      splines_.emplace_back(spline_order_);
-    }
-  }
+    {
+    {			float fAngle = b3Sqrt(b3Dot3F4(angvel, angvel));
+			
+			//limit the angular motion
+			if(fAngle*timeStep > BT_GPU_ANGULAR_MOTION_THRESHOLD)
+			{
+				fAngle = BT_GPU_ANGULAR_MOTION_THRESHOLD / timeStep;
+			}
+			if(fAngle < 0.001f)
+			{
+				// use Taylor's expansions of sync function
+				axis = angvel * (0.5f*timeStep-(timeStep*timeStep*timeStep)*0.020833333333f * fAngle * fAngle);
+			}
+			else
+			{
+				// sync(fAngle) = sin(c*fAngle)/t
+				axis = angvel * ( b3Sin(0.5f * fAngle * timeStep) / fAngle);
+			}
+			
+			b3Quat dorn;
+			dorn.x = axis.x;
+			dorn.y = axis.y;
+			dorn.z = axis.z;
+			dorn.w = b3Cos(fAngle * timeStep * 0.5f);
+			b3Quat orn0 = bodies[nodeID].m_quat;
+			b3Quat predictedOrn = b3QuatMul(dorn, orn0);
+			predictedOrn = b3QuatNormalized(predictedOrn);
+			bodies[nodeID].m_quat=predictedOrn;
+		}
+		//linear velocity		
+		bodies[nodeID].m_pos +=  bodies[nodeID].m_linVel * timeStep;
+		
+		//apply gravity
+		bodies[nodeID].m_linVel += gravityAcceleration * timeStep;
+		
+	}
+	
 }
+    
+    ///The b3GeometryUtil helper class provides a few methods to convert between plane equations and vertices.
+class b3GeometryUtil
+{
+	public:
+	
+	
+		static void	getPlaneEquationsFromVertices(b3AlignedObjectArray<b3Vector3>& vertices, b3AlignedObjectArray<b3Vector3>& planeEquationsOut );
+    }
+    
+    /**
+ * @brief Return true if the failed query is no longer blacklisted.
+ *
+ * There are two scenarios where a blacklisted query becomes 'unblacklisted'.
+ * The first is simple, the amount of time it was blacklisted for has expired.
+ * The second is more complex, the query failed but the schedule has requested
+ * that the query should not be blacklisted.
+ *
+ * @param blt The time the query was originally blacklisted.
+ * @param query The scheduled query and its options.
+ */
+static inline bool blacklistExpired(size_t blt, const ScheduledQuery& query) {
+  if (getUnixTime() > blt) {
+    return true;
+  }
+    }
+    
+    Status EventsConfigParserPlugin::update(const std::string& source,
+                                        const ParserConfig& config) {
+  auto events = config.find('events');
+  if (events != config.end()) {
+    auto obj = data_.getObject();
+    data_.copyFrom(events->second.doc(), obj);
+    data_.add('events', obj, data_.doc());
+  }
+  return Status();
+}
+    
+    Status KafkaTopicsConfigParserPlugin::update(const std::string& source,
+                                             const ParserConfig& config) {
+  auto topics = config.find(kKafkaTopicParserRootKey);
+  if (topics != config.end()) {
+    auto obj = data_.getObject();
+    data_.copyFrom(topics->second.doc(), obj);
+    data_.add(kKafkaTopicParserRootKey, obj);
+  }
+  return Status();
+}
+    
+    Status OptionsConfigParserPlugin::update(const std::string& source,
+                                         const ParserConfig& config) {
+  auto co = config.find('options');
+  if (co == config.end()) {
+    return Status();
+  }
+    }
+    
+    #include <osquery/config/config.h>
+#include <osquery/config/tests/test_utils.h>
+#include <osquery/database.h>
+#include <osquery/filesystem/filesystem.h>
+#include <osquery/registry.h>
+#include <osquery/registry_interface.h>
+#include <osquery/system.h>
+    
+    
+    {  size_t accesses = 0_sz;
+  if (doc.doc().HasMember('file_accesses') &&
+      doc.doc()['file_accesses'].IsArray()) {
+    accesses = doc.doc()['file_accesses'].Size();
+  }
+  EXPECT_EQ(accesses, 2_sz);
+}
+    
+    namespace osquery {
+    }
+    
+          JSON tree;
+      Status parse_status = tree.fromString(json);
+      if (!parse_status.ok()) {
+        VLOG(1) << 'Could not parse JSON from TLS config node API';
+      }
