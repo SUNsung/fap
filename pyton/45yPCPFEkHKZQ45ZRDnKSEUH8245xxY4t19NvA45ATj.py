873dff8639a9f843414f3443d3e9da899b969784
SUNsung <weatherius@gmail.com>
2019-01-02 07:50:49 +0200

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2019-01-02 07:50:48

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,68 +1,212 @@
 
         
-            # TODO: decoder/encoder should accept cls? Otherwise, subclassing
-    # JSONObjectWithFields is tricky...
-    header_cls = Header
-    header = jose.Field(
-        'header', omitempty=True, default=header_cls(),
-        decoder=header_cls.from_json)
+        from mrjob.job import MRJob
     
-    # generate_private_key requires cryptography>=0.5
-key = jose.JWKRSA(key=rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=BITS,
-    backend=default_backend()))
-acme = client.Client(DIRECTORY_URL, key)
+        def remove_from_tail(self):
+        ...
     
+        def __init__(self, debuglevel=0):
+        self._init_github_account()
+        https_handler = make_HTTPS_handler({}, debuglevel=debuglevel)
+        self._opener = compat_urllib_request.build_opener(https_handler)
     
-def unique_id():
-    ''' Returns an unique id to be used as a VirtualHost identifier'''
-    return binascii.hexlify(os.urandom(16)).decode('utf-8')
-
-    
-            # Force reload if files were modified
-        # This is needed to recalculate augeas directive span
-        if save_files:
-            for sf in save_files:
-                self.aug.remove('/files/'+sf)
-            self.aug.load()
-        if title and not temporary:
-            self.finalize_checkpoint(title)
-    
-            for achall in self.achalls:
-            achall_addrs = self._get_addrs(achall)
-            addrs.update(achall_addrs)
-    
-      return filters
-
+    import json
+import sys
+import hashlib
+import os.path
     
     
-@contextlib.contextmanager
-def UserOptions( options ):
-  old_vim_options = test_utils.VIM_OPTIONS.copy()
-  test_utils.VIM_OPTIONS.update( DEFAULT_CLIENT_OPTIONS )
-  test_utils.VIM_OPTIONS.update( options )
-  try:
-    yield
-  finally:
-    test_utils.VIM_OPTIONS = old_vim_options
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
+    
+    options = helptext[helptext.index('  General Options:') + 19:]
+options = re.sub(r'(?m)^  (\w.+)$', r'## \1', options)
+options = '# OPTIONS\n' + options + '\n'
+    
+        if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
+    
+        available_devices = _get_available_devices()
+    available_devices = [_normalize_device_name(name)
+                         for name in available_devices]
+    if not gpus:
+        # Using all visible GPUs when not specifying `gpus`
+        # e.g. CUDA_VISIBLE_DEVICES=0,2 python keras_mgpu.py
+        gpus = len([x for x in available_devices if 'gpu' in x])
+    
+        if transforms:
+        f = tf.contrib.image.compose_transforms(*transforms)
+        inputs = tf.contrib.image.transform(inputs, f, interpolation='BILINEAR')
+    return inputs
     
     
-@YouCompleteMeInstance( { 'g:ycm_open_loclist_on_ycm_diags': 1 } )
-@patch( 'ycm.youcompleteme.YouCompleteMe.FiletypeCompleterExistsForFiletype',
-        return_value = True )
-@patch( 'ycm.vimsupport.PostVimMessage', new_callable = ExtendedMock )
-@patch( 'ycm.vimsupport.SetLocationListForWindow', new_callable = ExtendedMock )
-@patch( 'ycm.vimsupport.OpenLocationList', new_callable = ExtendedMock )
-def YouCompleteMe_ShowDiagnostics_DiagnosticsFound_OpenLocationList_test(
-  ycm,
-  open_location_list,
-  set_location_list_for_window,
-  post_vim_message,
-  *args ):
+# Artificial data generation:
+# Generate movies with 3 to 7 moving squares inside.
+# The squares are of shape 1x1 or 2x2 pixels,
+# which move linearly over time.
+# For convenience we first create movies with bigger width and height (80x80)
+# and at the end we select a 40x40 window.
     
-      if parsed_args.coverage:
-    nosetests_args += [ '--with-coverage',
-                        '--cover-erase',
-                        '--cover-package=ycm',
-                        '--cover-html' ]
+    Gets to 0.8498 test accuracy after 2 epochs. 41s/epoch on K520 GPU.
+'''
+from __future__ import print_function
+    
+    try:
+    import botocore
+except ImportError:
+    pass  # handled by AnsibleAWSModule
+    
+    
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+    
+            firewall_policy = oneandone_conn.remove_firewall_server(
+            firewall_id=firewall_id,
+            server_ip_id=server_ip_id)
+        return firewall_policy
+    except Exception as e:
+        module.fail_json(msg=str(e))
+    
+    DOCUMENTATION = '''
+---
+module: vca_fw
+short_description: add remove firewall rules in a gateway  in a vca
+description:
+  - Adds or removes firewall rules from a gateway in a vca environment
+version_added: '2.0'
+author:
+- Peter Sprygada (@privateip)
+options:
+    fw_rules:
+      description:
+        - A list of firewall rules to be added to the gateway, Please see examples on valid entries
+      required: True
+      default: false
+extends_documentation_fragment: vca.documentation
+'''
+    
+    # module logic
+    
+    
+DOCUMENTATION = '''
+---
+module: group_by
+short_description: Create Ansible groups based on facts
+description:
+  - Use facts to create ad-hoc groups that can be used later in a playbook.
+  - This module is also supported for Windows targets.
+version_added: '0.9'
+options:
+  key:
+    description:
+    - The variables whose values will be used as groups
+    required: true
+  parents:
+    description:
+    - The list of the parent groups
+    required: false
+    default: 'all'
+    version_added: '2.4'
+author: 'Jeroen Hoekx (@jhoekx)'
+notes:
+  - Spaces in group names are converted to dashes '-'.
+  - This module is also supported for Windows targets.
+'''
+    
+    
+def bench_isotonic_regression(Y):
+    '''
+    Runs a single iteration of isotonic regression on the input data,
+    and reports the total time taken (in seconds).
+    '''
+    gc.collect()
+    
+    
+@ignore_warnings
+def benchmark(metrics=tuple(v for k, v in sorted(METRICS.items())),
+              formats=tuple(v for k, v in sorted(FORMATS.items())),
+              samples=1000, classes=4, density=.2,
+              n_times=5):
+    '''Times metric calculations for a number of inputs
+    
+    
+def barplot_neighbors(Nrange=2 ** np.arange(1, 11),
+                      Drange=2 ** np.arange(7),
+                      krange=2 ** np.arange(10),
+                      N=1000,
+                      D=64,
+                      k=5,
+                      leaf_size=30,
+                      dataset='digits'):
+    algorithms = ('kd_tree', 'brute', 'ball_tree')
+    fiducial_values = {'N': N,
+                       'D': D,
+                       'k': k}
+    
+        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
+    plt.plot(sample_sizes, one_core, label='one core')
+    plt.plot(sample_sizes, multi_core, label='multi core')
+    plt.xlabel('n_samples')
+    plt.ylabel('Time (s)')
+    plt.title('Parallel %s' % func.__name__)
+    plt.legend()
+    
+        ###########################################################################
+    # Numpy permutation based
+    sampling_algorithm['numpy-permutation'] = \
+        lambda n_population, n_sample: \
+            np.random.permutation(n_population)[:n_sample]
+    
+                gc.collect()
+            print('max_iter', max_iter)
+            print('- benchmarking A-SGD')
+            clf = SGDRegressor(alpha=alpha / n_train, fit_intercept=False,
+                               max_iter=max_iter, learning_rate='invscaling',
+                               eta0=.002, power_t=0.05, tol=1e-3,
+                               average=(max_iter * n_train // 2))
+    
+    n_samples, n_features = 5000, 300
+X = np.random.randn(n_samples, n_features)
+inds = np.arange(n_samples)
+np.random.shuffle(inds)
+X[inds[int(n_features / 1.2):]] = 0  # sparsify input
+print('input data sparsity: %f' % sparsity_ratio(X))
+coef = 3 * np.random.randn(n_features)
+inds = np.arange(n_features)
+np.random.shuffle(inds)
+coef[inds[n_features // 2:]] = 0  # sparsify coef
+print('true coef sparsity: %f' % sparsity_ratio(coef))
+y = np.dot(X, coef)
+    
+    
+def human_readable_data_quantity(quantity, multiple=1024):
+    # https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
+    if quantity == 0:
+        quantity = +0
+    SUFFIXES = ['B'] + [i + {1000: 'B', 1024: 'iB'}[multiple]
+                        for i in 'KMGTPEZY']
+    for suffix in SUFFIXES:
+        if quantity < multiple or suffix == SUFFIXES[-1]:
+            if suffix == SUFFIXES[0]:
+                return '%d %s' % (quantity, suffix)
+            else:
+                return '%.1f %s' % (quantity, suffix)
+        else:
+            quantity /= multiple
+    
+    from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+    
+    
+    
+            a += a
+        b >>= 1
