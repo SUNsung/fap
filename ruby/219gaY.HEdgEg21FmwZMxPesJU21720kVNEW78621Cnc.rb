9075b8eaa75303e9e7d17a93e567085999a4a033
SUNsung <weatherius@gmail.com>
2019-01-03 10:04:55 +0200

    Rewrite [219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb] File in: 2019-01-03 10:04:55

--- ruby/219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb
+++ ruby/219gaY.HEdgEg21FmwZMxPesJU21720kVNEW78621Cnc.rb
@@ -1,25 +1,26 @@
 
         
-          preflight do
-    processes = system_command '/bin/launchctl', args: ['list']
+            s = StringIO.new
+    SystemConfig.dump_verbose_config s
+    # Dummy summary file, asciibetically first, to control display title of gist
+    files['# #{f.name} - #{timestamp}.txt'] = { content: brief_build_info(f) }
+    files['00.config.out'] = { content: s.string }
+    files['00.doctor.out'] = { content: Utils.popen_read('#{HOMEBREW_PREFIX}/bin/brew', 'doctor', err: :out) }
+    unless f.core_formula?
+      tap = <<~EOS
+        Formula: #{f.name}
+        Tap: #{f.tap}
+        Path: #{f.path}
+      EOS
+      files['00.tap.out'] = { content: tap }
+    end
     
-          def html?(headers)
-        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
-        options[:html_types].include? header.last[/^\w+\/\w+/]
+          if old_path.git?
+        puts 'Changing remote from #{old_remote} to #{new_remote}...' if $stdout.tty?
+        old_path.git_origin = new_remote
       end
-    end
-  end
-end
-
     
-        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      subject.options[:allow_empty_referrer] = false
-      expect(subject.referrer(env)).to be_nil
-    end
-    
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
-  end
+            def ship
+          @shipment.ship! unless @shipment.shipped?
+          respond_with(@shipment, default_template: :show)
+        end
