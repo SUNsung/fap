ee2d5244bc325218f896cb5182216768d35d10df
SUNsung <weatherius@gmail.com>
2019-01-03 17:16:35 +0200

    Rewrite [09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp] File in: 2019-01-03 17:16:34

--- cccccc/09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp
+++ cccccc/09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp
@@ -1,272 +1,70 @@
 
         
-        template <>
-struct GetTypeInfo<const Variant &> {
-	static const Variant::Type VARIANT_TYPE = Variant::NIL;
-	static inline PropertyInfo get_class_info() {
-		return PropertyInfo(Variant::NIL, String(), PROPERTY_HINT_NONE, String(), PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_NIL_IS_VARIANT);
-	}
-};
+         public:
+  PosixSequentialFile(const std::string& fname, FILE* file, int fd,
+                      const EnvOptions& options);
+  virtual ~PosixSequentialFile();
     
-    	ClassDB::register_virtual_class<CSGShape>();
-	ClassDB::register_virtual_class<CSGPrimitive>();
-	ClassDB::register_class<CSGMesh>();
-	ClassDB::register_class<CSGSphere>();
-	ClassDB::register_class<CSGBox>();
-	ClassDB::register_class<CSGCylinder>();
-	ClassDB::register_class<CSGTorus>();
-	ClassDB::register_class<CSGPolygon>();
-	ClassDB::register_class<CSGCombiner>();
+      // close db
+  for (auto handle : handles) {
+    delete handle;
+  }
+  delete db;
+    
+      ////////////////////////////////////////////////////////
+  //
+  // 'Read Committed' (Monotonic Atomic Views) Example
+  //   --Using multiple Snapshots
+  //
+  ////////////////////////////////////////////////////////
     
     
-    {	ERR_EXPLAINC('There are still MemoryPool allocs in use at exit!');
-	ERR_FAIL_COND(allocs_used > 0);
-}
+    {// Factor method to create a new persistent cache
+Status NewPersistentCache(Env* const env, const std::string& path,
+                          const uint64_t size,
+                          const std::shared_ptr<Logger>& log,
+                          const bool optimized_for_nvm,
+                          std::shared_ptr<PersistentCache>* cache);
+}  // namespace rocksdb
 
     
-    /*!
- * \brief The result holder of storage type of each NodeEntry in the graph.
- * \note Stored under graph.attrs['storage_type'], provided by Pass 'InferStorageType'
- *
- * \code
- *  Graph g = ApplyPass(src_graph, 'InferStorageType');
- *  const StorageVector& stypes = g.GetAttr<StorageTypeVector>('storage_type');
- *  // get storage type by entry id
- *  int entry_type = stypes[g.indexed_graph().entry_id(my_entry)];
- * \endcode
- *
- * \sa FInferStorageType
- */
-using StorageTypeVector = std::vector<int>;
-    
-    #include <mxnet/op_attr_types.h>
-#include <mxnet/graph_attr_types.h>
-#include <mxnet/c_api.h>
-#include <nnvm/symbolic.h>
-#include <nnvm/op.h>
-#include <nnvm/graph.h>
-#include <vector>
-#include <atomic>
-#include <utility>
-#include <string>
-#include <unordered_map>
-    
-    /*!
- * \brief Gradient function that takes output value of function and computes gradient wrt to input.
- * \param out_grad the gradient wrt to output of the function.
- * \param env The Environment arguments.
- * \param in_grad The container to store result input gradient.
- * \param req The requirement to store the ret value.
- * \param ctx Runtime context to execute the function.
- */
-typedef void (*UnaryGradFunctionT0)(const OutputGrad& out_grad,
-                                    const EnvArguments& env,
-                                    TBlob* in_grad,
-                                    OpReqType req,
-                                    RunContext ctx);
-/*!
- * \brief Gradient function that takes output value of function and computes gradient wrt to input.
- * \param out_grad the gradient wrt to output of the function.
- * \param out_value the value of the function.
- * \param env The Environment arguments.
- * \param in_grad The container to store result input gradient.
- * \param req The requirement to store the ret value.
- * \param ctx Runtime context to execute the function.
- */
-typedef void (*UnaryGradFunctionT1)(const OutputGrad& out_grad,
-                                    const OutputValue& out_value,
-                                    const EnvArguments& env,
-                                    TBlob* in_grad,
-                                    OpReqType req,
-                                    RunContext ctx);
-/*!
- * \brief Gradient function that takes input value of function and computes gradient wrt to input.
- * \param out_grad the gradient wrt to output of the function.
- * \param in_data0 the input value of the function.
- * \param env The Environment arguments.
- * \param in_grad The container to store result input gradient.
- * \param req The requirement to store the ret value.
- * \param ctx Runtime context to execute the function.
- */
-typedef void (*UnaryGradFunctionT2)(const OutputGrad& out_grad,
-                                    const Input0& in_data0,
-                                    const EnvArguments& env,
-                                    TBlob* in_grad,
-                                    OpReqType req,
-                                    RunContext ctx);
-/*!
- * \brief Binary function that takes lhs, rhs and save result to ret.
- *  The result container is pre-allocated with the correct shape.
- * \param lhs The left operand
- * \param rhs The right operand
- * \param env The Environment arguments.
- * \param ret The containter to store return value.
- * \param req The requirement to stroe the ret.
- * \param ctx Runtime context to execute the function.
- */
-typedef void (*BinaryFunction)(const TBlob& lhs,
-                               const TBlob& rhs,
-                               const EnvArguments& env,
-                               TBlob* ret,
-                               OpReqType req,
-                               RunContext ctx);
-    
-        caffe::TBlob2CaffeBlob<xpu, Dtype>(caffe::Grad,
-                                       bot_.begin(),
-                                       in_grad.begin(),
-                                       param_.num_data);
-    caffe::TBlob2CaffeBlob<xpu, Dtype>(caffe::Grad,
-                                       top_.begin(),
-                                       out_grad.begin(),
-                                       param_.num_out);
-    
-    /*!
- * \brief Thread pool.
- */
-class ThreadPool {
- public:
-  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
-  struct SetReadyOnDestroy {
-    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
-      : event_(event) {
-    }
-    inline ~SetReadyOnDestroy() {
-      if (event_) {
-        event_->signal();
-      }
-    }
-    std::shared_ptr<dmlc::ManualEvent>  event_;
-  };
-    }
-    
-     protected:
-  void PushToExecute(OprBlock *opr_block, bool pusher_thread) override {
-    if (opr_block->opr->prop == FnProperty::kAsync && pusher_thread) {
-      DoExecute(opr_block);
-    } else {
-      DoPushToQueue(opr_block);
-    }
-  }
+    #pragma once
     
     
-    {
-    {        s1 %= ADLER_MOD, s2 %= ADLER_MOD;
-        buflen -= blocklen;
-        blocklen = 5552;
-    }
-    return (s2 << 16) + s1;
+    {  EsdCanClient esd_can_client;
+  EXPECT_TRUE(esd_can_client.Init(param));
+  EXPECT_EQ(esd_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
+  std::vector<CanFrame> frames;
+  int32_t num = 0;
+  EXPECT_EQ(esd_can_client.Send(frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  EXPECT_EQ(esd_can_client.Receive(&frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
+  CanFrame can_frame;
+  frames.push_back(can_frame);
+  EXPECT_EQ(esd_can_client.SendSingleFrame(frames),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  esd_can_client.Stop();
 }
     
-    IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_Init();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data);
-    
-        ALLEGRO_LOCKED_REGION *locked_img = al_lock_bitmap(img, al_get_bitmap_format(img), ALLEGRO_LOCK_WRITEONLY);
-    if (!locked_img)
-    {
-        al_destroy_bitmap(img);
-        return false;
-    }
-    memcpy(locked_img->data, pixels, sizeof(int)*width*height);
-    al_unlock_bitmap(img);
-    
-    // CHANGELOG 
-// (minor and older changes stripped away, please see git history for details)
-//  2018-11-30: Misc: Setting up io.BackendRendererName so it can be displayed in the About Window.
-//  2018-06-08: Misc: Extracted imgui_impl_dx9.cpp/.h away from the old combined DX9+Win32 example.
-//  2018-06-08: DirectX9: Use draw_data->DisplayPos and draw_data->DisplaySize to setup projection matrix and clipping rectangle.
-//  2018-05-07: Render: Saving/restoring Transform because they don't seem to be included in the StateBlock. Setting shading mode to Gouraud.
-//  2018-02-16: Misc: Obsoleted the io.RenderDrawListsFn callback and exposed ImGui_ImplDX9_RenderDrawData() in the .h file so you can call it yourself.
-//  2018-02-06: Misc: Removed call to ImGui::Shutdown() which is not available from 1.60 WIP, user needs to call CreateContext/DestroyContext themselves.
-    
-        // Restore state
-    glBindTexture(GL_TEXTURE_2D, last_texture);
-    
-    void ImGui_ImplVulkanH_CreateWindowDataCommandBuffers(VkPhysicalDevice physical_device, VkDevice device, uint32_t queue_family, ImGui_ImplVulkanH_WindowData* wd, const VkAllocationCallbacks* allocator)
-{
-    IM_ASSERT(physical_device != VK_NULL_HANDLE && device != VK_NULL_HANDLE);
-    (void)physical_device;
-    (void)allocator;
+    TEST(CanSenderTest, OneRunCase) {
+  CanSender<::apollo::canbus::ChassisDetail> sender;
+  can::FakeCanClient can_client;
+  sender.Init(&can_client, true);
     }
     
-    // Use your programming IDE 'Go to definition' facility on the names of the center columns to find the actual flags/enum lists.
-typedef int ImGuiLayoutType;        // -> enum ImGuiLayoutType_        // Enum: Horizontal or vertical
-typedef int ImGuiButtonFlags;       // -> enum ImGuiButtonFlags_       // Flags: for ButtonEx(), ButtonBehavior()
-typedef int ImGuiItemFlags;         // -> enum ImGuiItemFlags_         // Flags: for PushItemFlag()
-typedef int ImGuiItemStatusFlags;   // -> enum ImGuiItemStatusFlags_   // Flags: for DC.LastItemStatusFlags
-typedef int ImGuiNavHighlightFlags; // -> enum ImGuiNavHighlightFlags_ // Flags: for RenderNavHighlight()
-typedef int ImGuiNavDirSourceFlags; // -> enum ImGuiNavDirSourceFlags_ // Flags: for GetNavInputAmount2d()
-typedef int ImGuiNavMoveFlags;      // -> enum ImGuiNavMoveFlags_      // Flags: for navigation requests
-typedef int ImGuiSeparatorFlags;    // -> enum ImGuiSeparatorFlags_    // Flags: for Separator() - internal
-typedef int ImGuiSliderFlags;       // -> enum ImGuiSliderFlags_       // Flags: for SliderBehavior()
-typedef int ImGuiDragFlags;         // -> enum ImGuiDragFlags_         // Flags: for DragBehavior()
-    
-        // Set OS mouse position if requested (rarely used, only when ImGuiConfigFlags_NavEnableSetMousePos is enabled by user)
-    if (io.WantSetMousePos)
-        SDL_WarpMouseInWindow(g_Window, (int)io.MousePos.x, (int)io.MousePos.y);
-    else
-        io.MousePos = ImVec2(-FLT_MAX, -FLT_MAX);
-    
-    // Functions
-bool    ImGui_ImplWin32_Init(void* hwnd)
-{
-    if (!::QueryPerformanceFrequency((LARGE_INTEGER *)&g_TicksPerSecond))
-        return false;
-    if (!::QueryPerformanceCounter((LARGE_INTEGER *)&g_Time))
-        return false;
-    }
-    
-    using apollo::common::ErrorCode;
-    
-    TEST(ProtocolDataTest, CheckSum) {
-  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
-  const uint8_t result =
-      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
-  EXPECT_EQ(0xE7, result);
-}
-    
-    TEST(ByteTest, CopyConstructor) {
-  unsigned char byte_value = 0xFF;
-  Byte value(&byte_value);
-  Byte another_value(value);
-  EXPECT_EQ(another_value.to_hex_string(), value.to_hex_string());
-  EXPECT_EQ(another_value.to_binary_string(), value.to_binary_string());
-}
-    
-      x <<= 2;
-  x |= t;
-    
-    unsigned int BaseMapMatrix::GetBinarySize() const { return 0; }
-    
-    std::unique_ptr<PncMap> PncMapTest::pnc_map_;
-hdmap::HDMap PncMapTest::hdmap_;
-routing::RoutingResponse PncMapTest::routing_;
-    
-    TEST_F(SpeedLimitTest, SimpleSpeedLimitCreation) {
-  SpeedLimit simple_speed_limit;
-  EXPECT_TRUE(simple_speed_limit.speed_limit_points().empty());
+    TEST_F(SpeedLimitTest, GetSpeedLimitByS) {
   EXPECT_EQ(speed_limit_.speed_limit_points().size(), 100);
-}
-    
-    double Spline1dSeg::operator()(const double x) const { return spline_func_(x); }
-    
-    
-    {
-    {  } else {
-    for (uint32_t r = 3; r < num_params; ++r) {
-      for (uint32_t c = 3; c < num_params; ++c) {
-        (*term_matrix)(r, c) = x_pow[r + c - 5];
-      }
+  double s = 0.0;
+  const double ds = 0.01;
+  while (s < 99.0) {
+    double v_limit = speed_limit_.GetSpeedLimitByS(s);
     }
-    (*term_matrix).block(0, 0, num_params, 3) =
-        Eigen::MatrixXd::Zero(num_params, 3);
-    (*term_matrix).block(0, 0, 3, num_params) =
-        Eigen::MatrixXd::Zero(3, num_params);
-  }
-}
-    
-        BOOST_FORCEINLINE bool operator! () const BOOST_NOEXCEPT
-    {
-        return (data[0] == 0u && BOOST_ATOMIC_DETAIL_MEMCMP(data, data + 1, Size - 1) == 0);
     }
+    
+    double NodeWithRange::FullLength() const { return topo_node_->Length(); }
+    
+    
+    { protected:
+  GemMessageManager manager_;
+};
