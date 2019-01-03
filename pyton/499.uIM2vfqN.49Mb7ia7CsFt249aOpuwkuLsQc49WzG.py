eb432544d909c6b1802fca14f5e7d6abcccc8c46
SUNsung <weatherius@gmail.com>
2019-01-03 13:10:32 +0200

    Rewrite [499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py] File in: 2019-01-03 13:10:32

--- pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
+++ pyton/499.uIM2vfqN.49Mb7ia7CsFt249aOpuwkuLsQc49WzG.py
@@ -1,3 +1,89 @@
 
         
-        
+        print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
+    
+        infile, outfile = args
+    
+    
+def _is_empty(d):
+    return not bool(os.listdir(d))
+    
+    CLI_DEFAULTS = dict(
+    server_root=server_root_tmp,
+    ctl='nginx',
+)
+'''CLI defaults.'''
+    
+        def test_jwk_serialize(self):
+        from acme.jws import JWS
+        jws = JWS.sign(payload=b'foo', key=self.privkey,
+                       alg=jose.RS256, nonce=self.nonce,
+                       url=self.url)
+        self.assertEqual(jws.signature.combined.kid, None)
+        self.assertEqual(jws.signature.combined.jwk, self.pubkey)
+    
+        .. note:: This does not need to be accurate in order for the client to
+        run.  This simply keeps things clean if the user decides to revert
+        changes.
+    .. warning:: If all deps are not included, it may cause incorrect parsing
+        behavior, due to enable_mod's shortcut for updating the parser's
+        currently defined modules (`.ApacheParser.add_mod`)
+        This would only present a major problem in extremely atypical
+        configs that use ifmod for the missing deps.
+    
+    
+    def init_augeas(self):
+        ''' Initialize the actual Augeas instance '''
+        import augeas
+        self.aug = augeas.Augeas(
+            # specify a directory to load our preferred lens from
+            loadpath=constants.AUGEAS_LENS_DIR,
+            # Do not save backup (we do it ourselves), do not load
+            # anything by default
+            flags=(augeas.Augeas.NONE |
+                   augeas.Augeas.NO_MODL_AUTOLOAD |
+                   augeas.Augeas.ENABLE_SPAN))
+        # See if any temporary changes need to be recovered
+        # This needs to occur before VirtualHost objects are setup...
+        # because this will change the underlying configuration and potential
+        # vhosts
+        self.recovery_routine()
+    
+            for addr in vhost.addrs:
+            for local_addr in self.addrs:
+                if (local_addr.get_addr() == addr.get_addr() and
+                        local_addr != addr and
+                        local_addr.get_addr() not in already_found):
+    
+            with mock.patch(
+            'certbot_apache.override_debian.DebianConfigurator.enable_mod'):
+            sni_responses = self.sni.perform()
+    
+        main()
+    
+        # verify that none of the target files exist
+    assert not lexists('foo.txt')
+    assert not lexists('bar.txt')
+    assert not lexists('baz.txt')
+    try:
+        with open('foo.txt', 'w'):  # Creating the file
+            pass
+    
+    print()
+    
+        @Transactional will rollback to entry-state upon exceptions.
+    '''
+    
+        print('After subclassing: ')
+    for k in RegistryHolder.REGISTRY:
+        print(k)
