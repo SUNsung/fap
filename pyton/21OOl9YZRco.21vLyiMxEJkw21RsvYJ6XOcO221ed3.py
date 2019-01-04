72c82e03942ab63ae4e9ed2f31085a9ecfa8d9b8
SUNsung <weatherius@gmail.com>
2019-01-04 15:05:01 +0200

    Rewrite [21OOl9YZRco.21vLyiMxEJkw21RsvYJ6XOcO221ed3.py] File in: 2019-01-04 15:05:01

--- pyton/21OOl9YZRco.21vLyiMxEJkw21RsvYJ6XOcO221ed3.py
+++ pyton/21OOl9YZRco.21vLyiMxEJkw21RsvYJ6XOcO221ed3.py
@@ -1,71 +1,189 @@
 
         
-            @get_item_parameters
-    def test_get(self, key, value):
-        assert self.lookup_dict.get(key) == value
-
+        shells.shell = shells.Generic()
     
-        def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type is None:
-            self.stop_event.wait(self.WAIT_EVENT_TIMEOUT)
-        else:
-            if self.wait_to_close_event:
-                # avoid server from waiting for event timeouts
-                # if an exception is found in the main thread
-                self.wait_to_close_event.set()
-    
-            with server as (host, port):
-            server_url = 'http://{}:{}'.format(host, port)
-            for _ in range(requests_to_handle):
-                r = requests.get(server_url)
-                assert r.status_code == 200
-    
-        global __doc__
-    __doc__ = (__doc__ + '\n' +
-               '\n'.join(doc(code) for code in sorted(_codes))
-               if __doc__ is not None else None)
-    
-    # If false, no module index is generated.
-#latex_domain_indices = True
-    
-        plt.figure('scikit-learn GLM benchmark results')
-    plt.xlabel('Dimensions')
-    plt.ylabel('Time (s)')
-    plt.plot(dimensions, time_ridge, color='r')
-    plt.plot(dimensions, time_ols, color='g')
-    plt.plot(dimensions, time_lasso, color='b')
-    
-    print(__doc__)
+    containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
     
     
-@ignore_warnings
-def benchmark(metrics=tuple(v for k, v in sorted(METRICS.items())),
-              formats=tuple(v for k, v in sorted(FORMATS.items())),
-              samples=1000, classes=4, density=.2,
-              n_times=5):
-    '''Times metric calculations for a number of inputs
+@pytest.mark.parametrize('script, output', [
+    ('brew link sshfs', output),
+    ('cat output', output),
+    ('brew install sshfs', '')])
+def test_not_match(script, output):
+    command = Command(script, output)
+    assert not match(command)
     
-        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
-    plt.plot(sample_sizes, one_core, label='one core')
-    plt.plot(sample_sizes, multi_core, label='multi core')
-    plt.xlabel('n_samples')
-    plt.ylabel('Time (s)')
-    plt.title('Parallel %s' % func.__name__)
-    plt.legend()
+        return out
     
-        label = 'scikit-learn singular value decomposition benchmark results'
-    fig = plt.figure(label)
-    ax = fig.gca(projection='3d')
-    for c, (label, timings) in zip('rbg', sorted(six.iteritems(results))):
-        X, Y = np.meshgrid(samples_range, features_range)
-        Z = np.asarray(timings).reshape(samples_range.shape[0],
-                                        features_range.shape[0])
-        # plot the actual surface
-        ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3,
-                        color=c)
-        # dummy point plot to stick the legend to since surface plot do not
-        # support legends (yet?)
-        ax.plot([1], [1], [1], color=c, label=label)
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    from collections import OrderedDict
+    from collections.abc import Callable, Mapping, MutableMapping
+    
+    
+@pytest.mark.parametrize('var,scheme', _proxy_combos)
+def test_use_proxy_from_environment(httpbin, var, scheme):
+    url = '{}://httpbin.org'.format(scheme)
+    fake_proxy = Server()  # do nothing with the requests; just close the socket
+    with fake_proxy as (host, port):
+        proxy_url = 'socks5://{}:{}'.format(host, port)
+        kwargs = {var: proxy_url}
+        with override_environ(**kwargs):
+            # fake proxy's lack of response will cause a ConnectionError
+            with pytest.raises(requests.exceptions.ConnectionError):
+                requests.get(url)
+    
+            # if the server thread fails to finish, the test suite will hang
+        # and get killed by the jenkins timeout.
+    
+        @pytest.mark.parametrize(
+        'encoding', (
+            'utf-32', 'utf-8-sig', 'utf-16', 'utf-8', 'utf-16-be', 'utf-16-le',
+            'utf-32-be', 'utf-32-le'
+        ))
+    def test_encoded(self, encoding):
+        data = '{}'.encode(encoding)
+        assert guess_json_utf(data) == encoding
+    
+        def test_session_hooks_are_used_with_no_request_hooks(self, httpbin):
+        hook = lambda x, *args, **kwargs: x
+        s = requests.Session()
+        s.hooks['response'].append(hook)
+        r = requests.Request('GET', httpbin())
+        prep = s.prepare_request(r)
+        assert prep.hooks['response'] != []
+        assert prep.hooks['response'] == [hook]
+    
+        return proxy
+    
+        def remove_targets(self, target_ids):
+        '''Removes the provided targets from the rule in AWS'''
+        if not target_ids:
+            return
+        request = {
+            'Rule': self.name,
+            'Ids': target_ids
+        }
+        try:
+            response = self.client.remove_targets(**request)
+        except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
+            self.module.fail_json_aws(e, msg='Could not remove rule targets from rule %s' % self.name)
+        self.changed = True
+        return response
+    
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+    
+    # Copyright: (c) 2018, Ansible Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+    
+    
+def get_configuration_facts(cursor, parameter_name=''):
+    facts = {}
+    cursor.execute('''
+        select c.parameter_name, c.current_value, c.default_value
+        from configuration_parameters c
+        where c.node_name = 'ALL'
+        and (? = '' or c.parameter_name ilike ?)
+    ''', parameter_name, parameter_name)
+    while True:
+        rows = cursor.fetchmany(100)
+        if not rows:
+            break
+        for row in rows:
+            facts[row.parameter_name.lower()] = {
+                'parameter_name': row.parameter_name,
+                'current_value': row.current_value,
+                'default_value': row.default_value}
+    return facts
+    
+    
+DOCUMENTATION = '''
+---
+module: vertica_schema
+version_added: '2.0'
+short_description: Adds or removes Vertica database schema and roles.
+description:
+  - Adds or removes Vertica database schema and, optionally, roles
+    with schema access privileges.
+  - A schema will not be removed until all the objects have been dropped.
+  - In such a situation, if the module tries to remove the schema it
+    will fail and only remove roles created for the schema if they have
+    no dependencies.
+options:
+  name:
+    description:
+      - Name of the schema to add or remove.
+    required: true
+  usage_roles:
+    description:
+      - Comma separated list of roles to create and grant usage access to the schema.
+    aliases: ['usage_role']
+  create_roles:
+    description:
+      - Comma separated list of roles to create and grant usage and create access to the schema.
+    aliases: ['create_role']
+  owner:
+    description:
+      - Name of the user to set as owner of the schema.
+  state:
+    description:
+      - Whether to create C(present), or drop C(absent) a schema.
+    default: present
+    choices: ['present', 'absent']
+  db:
+    description:
+      - Name of the Vertica database.
+  cluster:
+    description:
+      - Name of the Vertica cluster.
+    default: localhost
+  port:
+    description:
+      - Vertica cluster port to connect to.
+    default: 5433
+  login_user:
+    description:
+      - The username used to authenticate with.
+    default: dbadmin
+  login_password:
+    description:
+      - The password used to authenticate with.
+notes:
+  - The default authentication assumes that you are either logging in as or sudo'ing
+    to the C(dbadmin) account on the host.
+  - This module uses C(pyodbc), a Python ODBC database adapter. You must ensure
+    that C(unixODBC) and C(pyodbc) is installed on the host and properly configured.
+  - Configuring C(unixODBC) for Vertica requires C(Driver = /opt/vertica/lib64/libverticaodbc.so)
+    to be added to the C(Vertica) section of either C(/etc/odbcinst.ini) or C($HOME/.odbcinst.ini)
+    and both C(ErrorMessagesPath = /opt/vertica/lib64) and C(DriverManagerEncoding = UTF-16)
+    to be added to the C(Driver) section of either C(/etc/vertica.ini) or C($HOME/.vertica.ini).
+requirements: [ 'unixODBC', 'pyodbc' ]
+author: 'Dariusz Owczarek (@dareko)'
+'''
+    
+    EXAMPLES = '''
+# Create groups based on the machine architecture
+- group_by:
+    key: machine_{{ ansible_machine }}
+    
+            # If feature is already in good state, just exit
+        if (re.search('Disabled features:.* %s[ \n]' % self.feature_name, out) and self.state == 'absent') or \
+                (re.search('Enabled features:.* %s[ \n]' % self.feature_name, out) and self.state == 'present'):
+            self.module.exit_json(changed=False)
     
     n_samples = np.logspace(.5, 3, 9)
 n_features = np.logspace(1, 3.5, 7)
@@ -74,35 +192,14 @@ N_samples, N_features = np.meshgrid(n_samples,
 scikits_time = np.zeros(N_samples.shape)
 scipy_time = np.zeros(N_samples.shape)
     
-        An example with a long-untouched module that everyone has
-    >>> _linkcode_resolve('py', {'module': 'tty',
-    ...                          'fullname': 'setraw'},
-    ...                   package='tty',
-    ...                   url_fmt='http://hg.python.org/cpython/file/'
-    ...                           '{revision}/Lib/{package}/{path}#L{lineno}',
-    ...                   revision='xxxx')
-    'http://hg.python.org/cpython/file/xxxx/Lib/tty/tty.py#L18'
-    '''
+    URL = ('http://people.csail.mit.edu/jrennie/'
+       '20Newsgroups/20news-bydate.tar.gz')
     
+        # Print the classification report
+    print(metrics.classification_report(y_test, y_predicted,
+                                        target_names=dataset.target_names))
     
-if not os.path.exists(TRAIN_FOLDER) or not os.path.exists(TEST_FOLDER):
-    
-                if self.values.count(None) > 0:
-                new_key = self.hash_function(new_key + 1)
-            else:
-                new_key = None
-                break
-    
-    	for n in xrange(m+1):
-		for k in xrange(1, m):
-			memo[n][k] += memo[n][k-1]
-			if n-k > 0:
-				memo[n][k] += memo[n-k-1][k]
-    
-    This is a pure Python implementation of Dynamic Programming solution to the longest_sub_array problem.
-    
-    	Arguments:
-		i {[int]} -- [integer]
-	'''
-    
-        return res
+        # the training data folder must be passed as first argument
+    movie_reviews_data_folder = sys.argv[1]
+    dataset = load_files(movie_reviews_data_folder, shuffle=False)
+    print('n_samples: %d' % len(dataset.data))
