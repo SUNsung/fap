6797c55a4f2c247f607fdb325c98e19f352a6e89
SUNsung <weatherius@gmail.com>
2019-01-04 13:55:39 +0200

    Rewrite [54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb] File in: 2019-01-04 13:55:39

--- ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
+++ ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
@@ -1,100 +1,46 @@
 
         
-                    html_options[:object] = @object
-            html_options
-          end
+          UserOption.where(user_id: smoke_user.id).update_all(
+    email_direct: false,
+    email_digests: false,
+    email_private_messages: false,
+  )
     
-    module ActionView
-  # This is the main entry point for rendering. It basically delegates
-  # to other objects like TemplateRenderer and PartialRenderer which
-  # actually renders the template.
-  #
-  # The Renderer will parse the options from the +render+ or +render_body+
-  # method and render a partial or a template based on the options. The
-  # +TemplateRenderer+ and +PartialRenderer+ objects are wrappers which do all
-  # the setup and logic necessary to render a view and a new object is created
-  # each time +render+ is called.
-  class Renderer
-    attr_accessor :lookup_context
+          class MergeRequest < ActiveRecord::Base
+        self.table_name = 'merge_requests'
     
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
+            # issue - An instance of `Gitlab::GithubImport::Representation::Issue`
+        # project - An instance of `Project`
+        # client - An instance of `Gitlab::GithubImport::Client`
+        def initialize(issue, project, client)
+          @issue = issue
+          @project = project
+          @client = client
+          @label_finder = LabelFinder.new(project)
+        end
     
-        it 'auto-corrects safe heredoc offenses' do
-      new_source = autocorrect_source(
-        construct(false, a, make_multi(safe_heredoc), true)
-      )
-    
-    module RuboCop
-  module AST
-    # A node extension for `case` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `case` nodes within RuboCop.
-    class CaseNode < Node
-      include ConditionalNode
-    
-    module RuboCop
-  module AST
-    # A node extension for `def` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `def` nodes within RuboCop.
-    class DefNode < Node
-      include ParameterizedNode
-      include MethodIdentifierPredicates
-    
-          # Checks whether any argument of the node is a splat
-      # argument, i.e. `*splat`.
+          # Returns the identifier to use for cache keys.
       #
-      # @return [Boolean] whether the node is a splat argument
-      def splat_argument?
-        arguments? &&
-          (arguments.any?(&:splat_type?) || arguments.any?(&:restarg_type?))
-      end
-      alias rest_argument? splat_argument?
-    
-      class IncludeArrayTag < Liquid::Tag
-    Syntax = /(#{Liquid::QuotedFragment}+)/
-    def initialize(tag_name, markup, tokens)
-      if markup =~ Syntax
-        @array_name = $1
-      else
-        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
-      end
-    
-    # brew tap shopify/shopify
-# brew install toxiproxy
-# gem install toxiproxy
-#require 'toxiproxy'
-# simulate a non-localhost network for realer-world conditions.
-# adding 1ms of network latency has an ENORMOUS impact on benchmarks
-#Toxiproxy.populate([{
-    #'name': 'redis',
-    #'listen': '127.0.0.1:6380',
-    #'upstream': '127.0.0.1:6379'
-#}])
-    
-          def context
-        c = Thread.current[:sidekiq_context]
-        ' #{c.join(SPACE)}' if c && c.any?
-      end
-    end
-    
-        def safe_thread(name, &block)
-      Thread.new do
-        Thread.current['sidekiq_label'] = name
-        watchdog(name, &block)
-      end
-    end
-    
-      Sidekiq::WebAction.class_eval 'def _render\n#{ERB.new(File.read(Web::LAYOUT)).src}\nend'
-end
-    
-        def erb(content, options = {})
-      if content.kind_of? Symbol
-        unless respond_to?(:'_erb_#{content}')
-          src = ERB.new(File.read('#{Web.settings.views}/#{content}.erb')).src
-          WebAction.class_eval('def _erb_#{content}\n#{src}\n end')
+      # For issues and pull requests this will be 'Issue' or 'MergeRequest'
+      # respectively. For diff notes this will return 'MergeRequest', for
+      # regular notes it will either return 'Issue' or 'MergeRequest' depending
+      # on what type of object the note belongs to.
+      def cache_key_type
+        if object.respond_to?(:issuable_type)
+          object.issuable_type
+        elsif object.respond_to?(:noteable_type)
+          object.noteable_type
+        else
+          raise(
+            TypeError,
+            'Instances of #{object.class} are not supported'
+          )
         end
       end
+    
+            expose_attribute :oid, :download_link
+    
+              new(hash)
+        end
+    
+          private
