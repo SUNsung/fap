f1c021ed6494b1af857d7bf93c571179d4f1092d
SUNsung <weatherius@gmail.com>
2019-01-04 08:35:19 +0200

    Rewrite [58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb] File in: 2019-01-04 08:35:18

--- ruby/58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb
+++ ruby/58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb
@@ -1,252 +1,106 @@
 
         
-                # Fix the order of receivers
-        @agents.each do |agent|
-          stub.proxy(agent).receivers { |orig| orig.order(:id) }
-        end
+          it 'can throw an object' do
+    lambda {
+      obj = Object.new
+      catch obj do
+        throw obj
       end
-    
-          agent_runner.stop
-    end
+    }.should_not raise_error(NameError)
   end
 end
     
-      it 'does not allow hash-style assignment' do
-    expect {
-      location[:lat] = 2.0
-    }.to raise_error(NoMethodError)
+      it 'does not append line-end if last character is line-end' do
+    lambda {
+      $VERBOSE = true
+      warn('this is some simple text with line-end\n')
+    }.should output(nil, 'this is some simple text with line-end\n')
   end
     
-      describe 'up' do
-    it 'should update extract and template options for an existing WebsiteAgent' do
-      expect(agent.options).to include('extract' => old_extract,
-                                       'template' => old_template)
-      ConvertWebsiteAgentTemplateForMerge.new.up
-      agent.reload
-      expect(agent.options).to include('extract' => new_extract,
-                                       'template' => new_template)
-    end
-  end
+        BUNDLE_DIR = ::File.join(LOGSTASH_HOME, 'vendor', 'bundle')
+    GEMFILE_PATH = ::File.join(LOGSTASH_HOME, 'Gemfile')
+    LOCAL_GEM_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'local_gems')
+    CACHE_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'cache')
+    LOCKFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile.lock'))
+    GEMFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile'))
     
-            im_receiver = Agents::CannotBeScheduled.new(
-           :name => 'Immediate Receiving Agent',
-        )
-        im_receiver.propagate_immediately = true
-        im_receiver.user = users(:bob)
-        im_receiver.sources << sender
-    
-        def to_json
-      JSON.generate(as_json)
-    end
-    
-        def document?
-      @content =~ DOCUMENT_RGX
-    end
-    
-          def process_response(response)
-        super.merge! response_url: response.url
-      end
-    end
-  end
-end
-
-    
-          @terminal_width = if !tty?
-        nil
-      elsif ENV['COLUMNS']
-        ENV['COLUMNS'].to_i
-      else
-        `stty size`.scan(/\d+/).last.to_i
-      end
-    rescue
-      @terminal_width = nil
-    end
-  end
-end
-
-    
-            css('.c10', '.showcase', '.showcase-content', '.l-main-section', 'div.div', 'div[flex]', 'code-tabs', 'md-card', 'md-card-content', 'div:not([class])', 'footer', '.card-row', '.card-row-container', 'figure', 'blockquote', 'exported', 'defined', 'div.ng-scope', '.code-example header', 'section.desc', '.row', '.dart-api-entry-main', '.main-content', 'section.summary', 'span.signature').each do |node|
-          node.before(node.children).remove
-        end
-    
-    protected
-    
-      #
-  # Gets cookies from the Set-Cookie header in a format to be used
-  # in the 'cookie' send_request field
-  #
-  def get_cookies
-    cookies = ''
-    if (self.headers.include?('Set-Cookie'))
-      set_cookies = self.headers['Set-Cookie']
-      key_vals = set_cookies.scan(/\s?([^, ;]+?)=([^, ;]*?)[;,]/)
-      key_vals.each do |k, v|
-        # Dont downcase actual cookie name as may be case sensitive
-        name = k.downcase
-        next if name == 'path'
-        next if name == 'expires'
-        next if name == 'domain'
-        next if name == 'max-age'
-        cookies << '#{k}=#{v}; '
-      end
-    end
-    
-      #
-  # Move these into an IPMI stack or mixin at some point
-  #
-    
-        data =
-      [0x00, 0x00, 0x00, 0x00].pack('C*') +
-      bmc_session_id +
-      console_random_id +
-      [
-        0x14, 0x00, 0x00,
-        username.length
-      ].pack('C*') +
-      username
-    
-              # Encodes a Rex::Proto::Kerberos::Model::Checksum into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            elems = []
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_type], 0, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_checksum], 1, :CONTEXT_SPECIFIC)
-    
-                if kvno
-              kvno_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_kvno], 1, :CONTEXT_SPECIFIC)
-              elems << kvno_asn1
-            end
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
-    end
-  end
-end
-
-    
-    When /^I (?:sign|log) in manually as '([^']*)' with password '([^']*)'( on the mobile website)?$/ \
-do |username, password, mobile|
-  @me = User.find_by_username(username)
-  @me.password ||= password
-  manual_login
-  confirm_login mobile
-end
-    
-    Then /^I should have (\d+) nsfw posts$/ do |num_posts|
-  page.should have_css('.nsfw-shield', count: num_posts.to_i)
-end
-    
-          @conv2 = Conversation.create(hash)
-      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
-             .increase_unread(alice)
-    
-    Given(/^file '(.*?)' exists in shared path$/) do |file|
-  file_shared_path = TestApp.shared_path.join(file)
-  run_vagrant_command('mkdir -p #{file_shared_path.dirname}')
-  run_vagrant_command('touch #{file_shared_path}')
-end
-    
-          def question
-        if default.nil?
-          I18n.t(:question, key: key, scope: :capistrano)
+            if explicit_plugins.any? { |spec| filename =~ /^#{spec.name}/ }
+          FileUtils.mv(gem_file, ::File.join(explicit_path, filename))
         else
-          I18n.t(:question_default, key: key, default_value: default, scope: :capistrano)
-        end
-      end
-    
-            def lvalue(key)
-          key.to_s.chomp('=').to_sym
+          FileUtils.mv(gem_file, ::File.join(dependencies_path, filename))
         end
       end
     end
-  end
-end
-
     
-    # when launched as a script, not require'd, (currently from bin/logstash and bin/logstash-plugin) the first
-# argument is the path of a Ruby file to require and a LogStash::Runner class is expected to be
-# defined and exposing the LogStash::Runner#main instance method which will be called with the current ARGV
-# currently lib/logstash/runner.rb and lib/pluginmanager/main.rb are called using this.
-if $0 == __FILE__
-  LogStash::Bundler.setup!({:without => [:build, :development]})
-  require_relative 'patches/jar_dependencies'
+        puts('Unpacking #{package_file}')
     
-    require 'bootstrap/environment'
+        desc 'Halt all VM's involved in the acceptance test round'
+    task :halt, :platform do |t, args|
+      config   = PlatformConfig.new
+      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
+      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
     
-          prepare_package(explicit_plugins_specs, temp_path)
-      LogStash::Util::Zip.compress(temp_path, @target)
-    ensure
-      FileUtils.rm_rf(temp_path)
-    end
+            def after_update_attributes
+          if params[:order] && params[:order][:coupon_code].present?
+            handler = PromotionHandler::Coupon.new(@order)
+            handler.apply
     
-        # any errors will be logged to $stderr by invoke!
-    # Bundler cannot update and clean gems in one operation so we have to call the CLI twice.
-    options = {:update => plugins, :rubygems_source => gemfile.gemset.sources}
-    options[:local] = true if local?
-    output = LogStash::Bundler.invoke!(options)
-    # We currently dont removed unused gems from the logstash installation
-    # see: https://github.com/elastic/logstash/issues/6339
-    # output = LogStash::Bundler.invoke!(:clean => true)
-    display_updated_plugins(previous_gem_specs_map)
-  rescue => exception
-    gemfile.restore!
-    report_exception('Updated Aborted', exception)
-  ensure
-    display_bundler_output(output)
-  end
-    
-              it 'fails when installing a non logstash plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
-            expect(command).not_to install_successfully
-          end
-    
-          it 'display a list of installed plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
-    
-              scope = scope.not_deleted unless params[:show_deleted]
-          scope = scope.not_discontinued unless params[:show_discontinued]
-        else
-          scope = Product.accessible_by(current_ability, :read).active.includes(*product_includes)
+            def taxonomies
+          @taxonomies = Taxonomy.accessible_by(current_ability, :read).order('name').includes(root: :children).
+                        ransack(params[:q]).result.
+                        page(params[:page]).per(params[:per_page])
         end
-    
-            def advance
-          authorize! :update, @order, order_token
-          while @order.next; end
-          respond_with(@order, default_template: 'spree/api/v1/orders/show', status: 200)
-        end
-    
-            def new; end
-    
-            def update
-          @return_authorization = order.return_authorizations.accessible_by(current_ability, :update).find(params[:id])
-          if @return_authorization.update_attributes(return_authorization_params)
-            respond_with(@return_authorization, default_template: :show)
-          else
-            invalid_resource!(@return_authorization)
-          end
-        end
-    
-              @line_item = Spree::Cart::AddItem.call(order: @order,
-                                                 variant: variant,
-                                                 quantity: quantity,
-                                                 options: { shipment: @shipment }).value
-    
-              if params[:page] || params[:per_page]
-            @states = @states.page(params[:page]).per(params[:per_page])
-          end
     
             def show
-          authorize! :read, @store
-          respond_with(@store)
+          respond_with(user)
         end
     
-            def jstree
-          show
+            def show
+          respond_with(zone)
         end
     
-            private
+        def render(context)
+      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
+        gist, file = parts[1].strip, parts[2].strip
+      else
+        gist, file = @text.strip, ''
+      end
+      if gist.empty?
+        ''
+      else
+        script_url = script_url_for gist, file
+        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        html_output_for script_url, code
+      end
+    end
+    
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+      expansion(:inspection) {
+    {
+      :inspected => o.inspected == 1 ? true : false,
+      :spam => o.spam == 1 ? true : false,
+      :spam_score => o.spam_score.to_f,
+      :threat => o.threat == 1 ? true : false,
+      :threat_details => o.threat_details
+    }
+  }
+    
+      def destroy
+    @http_endpoint.destroy
+    redirect_to_with_json [organization, @server, :http_endpoints]
+  end
+    
+      include WithinOrganization
+    
+        if @user.save
+      if email_changed
+        redirect_to_with_json verify_path(:return_to => settings_path), :notice => 'Your settings have been updated successfully. As you've changed, your e-mail address you'll need to verify it before you can continue.'
+      else
+        redirect_to_with_json settings_path, :notice => 'Your settings have been updated successfully.'
+      end
+    else
+      render_form_errors 'edit', @user
+    end
+  end
