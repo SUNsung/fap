89d23bd897155a171044b638a86243426d6ab67b
SUNsung <weatherius@gmail.com>
2019-01-04 20:57:37 +0200

    Rewrite [24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb] File in: 2019-01-04 20:57:37

--- ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
+++ ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
@@ -1,37 +1,24 @@
 
         
-                  pipelines.each do |pipeline|
-            self.new(pipeline).tap do |preloader|
-              preloader.preload_commit_authors
-              preloader.preload_pipeline_warnings
-              preloader.preload_stages_warnings
-            end
-          end
-        end
+          # Controllers inheriting DeviseController are advised to override this
+  # method so that other controllers inheriting from them would use
+  # existing translations.
+  def translation_scope
+    'devise.#{controller_name}'
+  end
     
-          def rate_limit_counter
-        @rate_limit_counter ||= Gitlab::Metrics.counter(
-          :github_importer_rate_limit_hits,
-          'The number of times we hit the GitHub rate limit when importing projects'
-        )
+    class BugTest < ActionDispatch::IntegrationTest
+  include Rack::Test::Methods
+  include Warden::Test::Helpers
+    
+          def _devise_route_context
+        @_devise_route_context ||= send(Devise.available_router_name)
       end
-    
-      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
-  gem.executables   = %w(cap capify)
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ['lib']
-    
-    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
-  file_shared_path = TestApp.shared_path.join(file)
-  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
-  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
+    end
+  end
 end
+
     
-          # Internal use only.
-      def peek(key, default=nil, &block)
-        value = fetch_for(key, default, &block)
-        while callable_without_parameters?(value)
-          value = (values[key] = value.call)
-        end
-        value
+          selected_modules = modules.map(&:to_sym).uniq.sort_by do |s|
+        Devise::ALL.index(s) || -1  # follow Devise::ALL order
       end
