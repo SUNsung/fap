e51303f755aafe2e00bca36bea59e326d40b52fd
SUNsung <weatherius@gmail.com>
2019-01-04 04:52:29 +0200

    Rewrite [226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp] File in: 2019-01-04 04:52:28

--- cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
+++ cccccc/226VqKJ1U1MY224ENy6hm3N6Y22Lz9EdFJ66eI22aNH.cpp
@@ -1,129 +1,79 @@
 
         
-        # if !GTEST_OS_WINDOWS
-// Tests that an exit code describes an exit due to termination by a
-// given signal.
-class GTEST_API_ KilledBySignal {
+        
+    {}  // namespace grpc
+
+    
+    class CensusChannelData : public ChannelData {
  public:
-  explicit KilledBySignal(int signum);
-  bool operator()(int exit_status) const;
- private:
-  const int signum_;
+  grpc_error* Init(grpc_channel_element* elem,
+                   grpc_channel_element_args* args) override;
 };
-# endif  // !GTEST_OS_WINDOWS
     
-      // Since the basic IO manipulators are overloaded for both narrow
-  // and wide streams, we have to provide this specialized definition
-  // of operator <<, even though its body is the same as the
-  // templatized version above.  Without this definition, streaming
-  // endl or other basic IO manipulators to Message will confuse the
-  // compiler.
-  Message& operator <<(BasicNarrowIoManip val) {
-    *ss_ << val;
-    return *this;
-  }
+    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CLIENT_FILTER_H */
+
     
-    ]]
-    
-    // Type-parameterized tests are abstract test patterns parameterized
-// by a type.  Compared with typed tests, type-parameterized tests
-// allow you to define the test pattern without knowing what the type
-// parameters are.  The defined pattern can be instantiated with
-// different types any number of times, in any number of translation
-// units.
-//
-// If you are designing an interface or concept, you can define a
-// suite of type-parameterized tests to verify properties that any
-// valid implementation of the interface/concept should have.  Then,
-// each implementation can easily instantiate the test suite to verify
-// that it conforms to the requirements, without having to write
-// similar tests repeatedly.  Here's an example:
-    
-      // Returns a pathname for a file that does not currently exist. The pathname
-  // will be directory/base_name.extension or
-  // directory/base_name_<number>.extension if directory/base_name.extension
-  // already exists. The number will be incremented until a pathname is found
-  // that does not already exist.
-  // Examples: 'dir/foo_test.xml' or 'dir/foo_test_1.xml'.
-  // There could be a race condition if two or more processes are calling this
-  // function at the same time -- they could both pick the same filename.
-  static FilePath GenerateUniqueFileName(const FilePath& directory,
-                                         const FilePath& base_name,
-                                         const char* extension);
-    
-      // # of bits in a number.
-  static const size_t kBitCount = 8*sizeof(RawType);
-    
-    #ifdef __BORLANDC__
-// string.h is not guaranteed to provide strcpy on C++ Builder.
-# include <mem.h>
-#endif
-    
-    ]]
+    // client hour
+const ViewDescriptor& ClientSentBytesPerRpcHour() {
+  const static ViewDescriptor descriptor =
+      HourDescriptor()
+          .set_name('grpc.io/client/sent_bytes_per_rpc/hour')
+          .set_measure(kRpcClientSentBytesPerRpcMeasureName)
+          .set_aggregation(BytesDistributionAggregation())
+          .add_column(ClientMethodTagKey());
+  return descriptor;
+}
     
     
-    {
-    {    // Adds the leak checker to the end of the test event listener list,
-    // after the default text output printer and the default XML report
-    // generator.
-    //
-    // The order is important - it ensures that failures generated in the
-    // leak checker's OnTestEnd() method are processed by the text and XML
-    // printers *before* their OnTestEnd() methods are called, such that
-    // they are attributed to the right test. Remember that a listener
-    // receives an OnXyzStart event *after* listeners preceding it in the
-    // list received that event, and receives an OnXyzEnd event *before*
-    // listeners preceding it.
-    //
-    // We don't need to worry about deleting the new listener later, as
-    // Google Test will do it.
-    listeners.Append(new LeakChecker);
-  }
-  return RUN_ALL_TESTS();
+    {}  // namespace
+    
+    namespace grpc {
+    }
+    
+    #include <grpc/impl/codegen/port_platform.h>
+    
+      // 4170 left
+  ASSERT_EQ(static_cast<uint64_t>(0u), controller.GetDelay(&env, 8000u));
+    
+    
+    {  rocksdb::WriteOptions wopts;
+  db->Merge(wopts, '0', 'bad');  // This is filtered out
+  db->Merge(wopts, '1', 'data1');
+  db->Merge(wopts, '1', 'bad');
+  db->Merge(wopts, '1', 'data2');
+  db->Merge(wopts, '1', 'bad');
+  db->Merge(wopts, '3', 'data3');
+  db->CompactRange(rocksdb::CompactRangeOptions(), nullptr, nullptr);
+  fprintf(stderr, 'filter.count_ = %d\n', filter.count_);
+  assert(filter.count_ == 0);
+  fprintf(stderr, 'filter.merge_count_ = %d\n', filter.merge_count_);
+  assert(filter.merge_count_ == 6);
 }
 
     
-    // Sets the 0-terminated C string this MyString object
-// represents.
-void MyString::Set(const char* a_c_string) {
-  // Makes sure this works when c_string == c_string_
-  const char* const temp = MyString::CloneCString(a_c_string);
-  delete[] c_string_;
-  c_string_ = temp;
-}
-
+    #pragma once
     
-      void SwitchBlock(int block_x, int block_y,
-                   int factor_x, int factor_y) override;
+    // FlushBlockPolicy provides a configurable way to determine when to flush a
+// block in the block based tables,
+class FlushBlockPolicy {
+ public:
+  // Keep track of the key/value sequences and return the boolean value to
+  // determine if table builder should flush current data block.
+  virtual bool Update(const Slice& key,
+                      const Slice& value) = 0;
+    }
     
-      // Returns true if the argument of the last Compare() call (or the baseline
-  // image, if Compare() was not called yet) meets the image acceptance
-  // criteria. The target_mul modifies the acceptance criteria used in this call
-  // the following way:
-  //    = 1.0 : the original acceptance criteria is used,
-  //    < 1.0 : a more strict acceptance criteria is used,
-  //    > 1.0 : a less strict acceptance criteria is used.
-  virtual bool DistanceOK(double target_mul) const = 0;
+    #pragma once
     
-    // Performs in-place floating point 8x8 DCT on block[0..63].
-// Note that the DCT used here is the DCT-2 with the first term multiplied by
-// 1/sqrt(2) and the result scaled by 1/2.
-void ComputeBlockDCTDouble(double block[64]);
+      // When Open() is called, it will open at most this many of the latest
+  // non-corrupted backups.
+  //
+  // Note setting this to a non-default value prevents old files from being
+  // deleted in the shared directory, as we can't do proper ref-counting. If
+  // using this option, make sure to occasionally disable it (by resetting to
+  // INT_MAX) and run GarbageCollect to clean accumulated stale files.
+  //
+  // Default: INT_MAX
+  int max_valid_backups_to_open;
     
-    #ifndef GUETZLI_FDCT_H_
-#define GUETZLI_FDCT_H_
-    
-      png_read_png(png_ptr, info_ptr, png_transforms, nullptr);
-    
-      tmp1 = kIDCTMatrix[0] * in[0];
-  out[0] = out[1] = out[2] = out[3] = out[4] = out[5] = out[6] = out[7] = tmp1;
-    
-    #define EXPECT_MARKER() \
-  if (pos + 2 > len || data[pos] != 0xff) {                             \
-    fprintf(stderr, 'Marker byte (0xff) expected, found: %d '           \
-            'pos=%d len=%d\n',                                          \
-            (pos < len ? data[pos] : 0), static_cast<int>(pos),         \
-            static_cast<int>(len));                                     \
-    jpg->error = JPEG_MARKER_BYTE_NOT_FOUND;                            \
-    return false;                                                       \
-  }
+    class DB;
