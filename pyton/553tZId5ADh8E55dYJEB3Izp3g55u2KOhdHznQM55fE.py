574a40f5ccbda22f9fcd15b268cbb265995c136e
SUNsung <weatherius@gmail.com>
2019-01-05 01:31:05 +0200

    Rewrite [553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py] File in: 2019-01-05 01:31:04

--- pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
+++ pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
@@ -1,173 +1,60 @@
 
         
-            assert proc.expect([TIMEOUT, u'usage'])
+            def test_encrypt(self):
+        msg = b'message'
+        key = list(range(16))
+        encrypted = aes_encrypt(bytes_to_intlist(msg), key)
+        decrypted = intlist_to_bytes(aes_decrypt(encrypted, key))
+        self.assertEqual(decrypted, msg)
     
+                for tc_num, tc in enumerate(test_cases):
+                tc_res_dict = res_dict['entries'][tc_num]
+                # First, check test cases' data against extracted data alone
+                expect_info_dict(self, tc_res_dict, tc.get('info_dict', {}))
+                # Now, check downloaded file consistency
+                tc_filename = get_tc_filename(tc)
+                if not test_case.get('params', {}).get('skip_download', False):
+                    self.assertTrue(os.path.exists(tc_filename), msg='Missing file ' + tc_filename)
+                    self.assertTrue(tc_filename in finished_hook_called)
+                    expected_minsize = tc.get('file_minsize', 10000)
+                    if expected_minsize is not None:
+                        if params.get('test'):
+                            expected_minsize = max(expected_minsize, 10000)
+                        got_fsize = os.path.getsize(tc_filename)
+                        assertGreaterEqual(
+                            self, got_fsize, expected_minsize,
+                            'Expected %s to be at least %s, but it\'s only %s ' %
+                            (tc_filename, format_bytes(expected_minsize),
+                                format_bytes(got_fsize)))
+                    if 'md5' in tc:
+                        md5_for_file = _file_md5(tc_filename)
+                        self.assertEqual(tc['md5'], md5_for_file)
+                # Finally, check test cases' data again but this time against
+                # extracted data from info JSON file written during processing
+                info_json_fn = os.path.splitext(tc_filename)[0] + '.info.json'
+                self.assertTrue(
+                    os.path.exists(info_json_fn),
+                    'Missing info file %s' % info_json_fn)
+                with io.open(info_json_fn, encoding='utf-8') as infof:
+                    info_dict = json.load(infof)
+                expect_info_dict(self, info_dict, tc.get('info_dict', {}))
+        finally:
+            try_rm_tcs_files()
+            if is_playlist and res_dict is not None and res_dict.get('entries'):
+                # Remove all other files that may have been extracted if the
+                # extractor returns full results even with extract_flat
+                res_tcs = [{'info_dict': e} for e in res_dict['entries']]
+                try_rm_tcs_files(res_tcs)
     
-output = '''sshfs: OsxfuseRequirement unsatisfied!
-    
-    no_such_subcommand = '''error: no such subcommand
-    
+    def cbs_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    '''Downloads CBS videos by URL.
     '''
     
     
-def benchmark(estimator, data):
-    gc.collect()
-    print('Benching %s' % estimator)
-    t0 = time()
-    estimator.fit(data)
-    training_time = time() - t0
-    data_t = estimator.transform(data)
-    data_r = estimator.inverse_transform(data_t)
-    reconstruction_error = np.mean(np.abs(data - data_r))
-    return {'time': training_time, 'error': reconstruction_error}
+def get_file_path(merge, output_dir, title, url):
+    mime, ext, size = url_info(url)
+    file_name = get_output_filename([], title, ext, output_dir, merge)
+    file_path = os.path.join(output_dir, file_name)
+    return file_name, file_path
     
-        for i, DD in enumerate(Drange):
-        print('D = %i (%i out of %i)' % (DD, i + 1, len(Drange)))
-        X = get_data(N, DD, dataset)
-        for algorithm in algorithms:
-            nbrs = neighbors.NearestNeighbors(n_neighbors=k,
-                                              algorithm=algorithm,
-                                              leaf_size=leaf_size)
-            t0 = time()
-            nbrs.fit(X)
-            t1 = time()
-            nbrs.kneighbors(X)
-            t2 = time()
-    
-    
-def compute_bench(samples_range, features_range):
-    
-    def plot(func):
-    random_state = check_random_state(0)
-    one_core = []
-    multi_core = []
-    sample_sizes = range(1000, 6000, 1000)
-    
-    
-def compute_bench(samples_range, features_range, n_iter=3, rank=50):
-    
-    ratio = scikits_time / scipy_time
-    
-    from time import time
-    
-    from scipy.sparse.csr import csr_matrix
-import numpy as np
-from sklearn.linear_model.stochastic_gradient import SGDRegressor
-from sklearn.metrics import r2_score
-    
-    
-df = pd.DataFrame(res).set_index(['analyzer', 'ngram_range', 'vectorizer'])
-    
-        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
-                                   'blob/{revision}/{package}/'
-                                   '{path}#L{lineno}')
-    '''
-    revision = _get_git_revision()
-    return partial(_linkcode_resolve, revision=revision, package=package,
-                   url_fmt=url_fmt)
-
-    
-    
-class Resource(jose.Field):
-    '''Resource MITM field.'''
-    
-    
-class Signature(jose.Signature):
-    '''ACME-specific Signature. Uses ACME-specific Header for customer fields.'''
-    __slots__ = jose.Signature._orig_slots  # pylint: disable=no-member
-    
-            self.assertRaises(
-            jose.DeserializationError, nonce_field.decode, self.wrong_nonce)
-        self.assertEqual(b'foo', nonce_field.decode(self.good_nonce))
-    
-    # One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'acme-python', u'acme-python Documentation',
-     [author], 1),
-    ('man/jws', 'jws', u'jws script documentation', [project], 1),
-]
-    
-    
-DIRECTORY_URL = 'https://acme-staging.api.letsencrypt.org/directory'
-BITS = 2048  # minimum for Boulder
-DOMAIN = 'example1.com'  # example.com is ignored by Boulder
-    
-            self.assertRaises(
-            errors.PluginError, self.config.recovery_routine)
-    
-        def test_perform2(self):
-        # Avoid load module
-        self.sni.configurator.parser.modules.add('ssl_module')
-        self.sni.configurator.parser.modules.add('socache_shmcb_module')
-        acme_responses = []
-        for achall in self.achalls:
-            self.sni.add_chall(achall)
-            acme_responses.append(achall.response(self.auth_key))
-    
-    # Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'certbot-apache.tex', u'certbot-apache Documentation',
-     u'Certbot Project', 'manual'),
-]
-    
-            return link_list
-    
-    from ..common import *
-    
-    #----------------------------------------------------------------------
-def dilidili_parser_data_to_stream_types(typ ,vid ,hd2 ,sign, tmsign, ulk):
-    '''->list'''
-    another_url = 'https://newplayer.jfrft.com/parse.php?xmlurl=null&type={typ}&vid={vid}&hd={hd2}&sign={sign}&tmsign={tmsign}&userlink={ulk}'.format(typ = typ, vid = vid, hd2 = hd2, sign = sign, tmsign = tmsign, ulk = ulk)
-    parse_url = 'http://player.005.tv/parse.php?xmlurl=null&type={typ}&vid={vid}&hd={hd2}&sign={sign}&tmsign={tmsign}&userlink={ulk}'.format(typ = typ, vid = vid, hd2 = hd2, sign = sign, tmsign = tmsign, ulk = ulk)
-    html = get_content(another_url, headers=headers)
-    
-    info = re.search(r'(\{[^{]+\})(\{[^{]+\})(\{[^{]+\})(\{[^{]+\})(\{[^{]+\})', html).groups()
-    info = [i.strip('{}').split('->') for i in info]
-    info = {i[0]: i [1] for i in info}
-    
-    stream_types = []
-    for i in zip(info['deft'].split('|'), info['defa'].split('|')):
-        stream_types.append({'id': str(i[1][-1]), 'container': 'mp4', 'video_profile': i[0]})
-    return stream_types
-    }
-    }
-    }
-    }
-    }
-    
-    def facebook_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url)
-    
-        fc2video_download_by_upid(upid, output_dir, merge, info_only)
-    
-        for i in range(1, num_tests + 1):
-        logging.info('running test case %d', i)
-        url = options.url + '/runCase?case=%d&agent=%s' % (i, options.name)
-        test_ws = yield websocket_connect(url, None, compression_options={})
-        while True:
-            message = yield test_ws.read_message()
-            if message is None:
-                break
-            test_ws.write_message(message, binary=isinstance(message, bytes))
-    
-    # The following code is copied from
-# https://github.com/mongodb/mongo-python-driver/blob/master/setup.py
-# to support installing without the extension on platforms where
-# no compiler is available.
-from distutils.command.build_ext import build_ext
-    
-            if threading.active_count() > 1:
-            # libcurl/pycurl is not thread-safe by default.  When multiple threads
-            # are used, signals should be disabled.  This has the side effect
-            # of disabling DNS timeouts in some environments (when libcurl is
-            # not linked against ares), so we don't do it when there is only one
-            # thread.  Applications that use many short-lived threads may need
-            # to set NOSIGNAL manually in a prepare_curl_callback since
-            # there may not be any other threads running at the time we call
-            # threading.activeCount.
-            curl.setopt(pycurl.NOSIGNAL, 1)
-        if request.prepare_curl_callback is not None:
-            request.prepare_curl_callback(curl)
+        service = url.split('/')[2].split('.')[0]
