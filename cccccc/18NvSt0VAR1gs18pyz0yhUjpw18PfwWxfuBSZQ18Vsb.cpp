d07599a8c2e47466336ed409d79ee94da5ceda71
SUNsung <weatherius@gmail.com>
2019-01-04 02:53:29 +0200

    Rewrite [18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp] File in: 2019-01-04 02:53:29

--- cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
+++ cccccc/18NvSt0VAR1gs18pyz0yhUjpw18PfwWxfuBSZQ18Vsb.cpp
@@ -1,213 +1,147 @@
 
         
         
-    {private:
-    virtual void AppendBeyondCapacity(const char *bytes, int32_t n, int32_t length);
-    virtual UBool Resize(int32_t appendCapacity, int32_t length);
-};
-    
-    
-UBool ScriptSet::operator == (const ScriptSet &other) const {
-    for (uint32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
-        if (bits[i] != other.bits[i]) {
-            return FALSE;
-        }
+    {
+    {      // white-space before a colon means it's not a URL
+      if (c == ' ' || (0x9 <= c && c <= 0xD))
+        break;
     }
-    return TRUE;
+  }
+    
+    #include 'atom/browser/native_window_views.h'
+    
+    namespace mate {
+    }
+    
+    void Screen::OnDisplayMetricsChanged(const display::Display& display,
+                                     uint32_t changed_metrics) {
+  Emit('display-metrics-changed', display, MetricsToArray(changed_metrics));
 }
     
-    #define DOT               ((UChar)0x002E)
-#define SINGLE_QUOTE      ((UChar)0x0027)
-#define SLASH             ((UChar)0x002F)
-#define BACKSLASH         ((UChar)0x005C)
-#define SPACE             ((UChar)0x0020)
-#define TAB               ((UChar)0x0009)
-#define QUOTATION_MARK    ((UChar)0x0022)
-#define ASTERISK          ((UChar)0x002A)
-#define COMMA             ((UChar)0x002C)
-#define HYPHEN            ((UChar)0x002D)
-#define U_ZERO            ((UChar)0x0030)
-#define U_ONE             ((UChar)0x0031)
-#define U_TWO             ((UChar)0x0032)
-#define U_THREE           ((UChar)0x0033)
-#define U_FOUR            ((UChar)0x0034)
-#define U_FIVE            ((UChar)0x0035)
-#define U_SIX             ((UChar)0x0036)
-#define U_SEVEN           ((UChar)0x0037)
-#define U_EIGHT           ((UChar)0x0038)
-#define U_NINE            ((UChar)0x0039)
-#define COLON             ((UChar)0x003A)
-#define SEMI_COLON        ((UChar)0x003B)
-#define CAP_A             ((UChar)0x0041)
-#define CAP_B             ((UChar)0x0042)
-#define CAP_R             ((UChar)0x0052)
-#define CAP_Z             ((UChar)0x005A)
-#define LOWLINE           ((UChar)0x005F)
-#define LEFTBRACE         ((UChar)0x007B)
-#define RIGHTBRACE        ((UChar)0x007D)
+    void Event::RenderFrameHostChanged(content::RenderFrameHost* old_rfh,
+                                   content::RenderFrameHost* new_rfh) {
+  if (sender_ && sender_ == old_rfh)
+    sender_ = new_rfh;
+}
     
-    #endif
+    
+    {}  // namespace atom
+    
+      // content::JavaScriptDialogManager implementations.
+  void RunJavaScriptDialog(content::WebContents* web_contents,
+                           content::RenderFrameHost* rfh,
+                           content::JavaScriptDialogType dialog_type,
+                           const base::string16& message_text,
+                           const base::string16& default_prompt_text,
+                           DialogClosedCallback callback,
+                           bool* did_suppress_message) override;
+  void RunBeforeUnloadDialog(content::WebContents* web_contents,
+                             content::RenderFrameHost* rfh,
+                             bool is_reload,
+                             DialogClosedCallback callback) override;
+  void CancelDialogs(content::WebContents* web_contents,
+                     bool reset_state) override;
+    
+    #if defined(OS_LINUX)
+typedef PowerObserverLinux PowerObserver;
+#else
+typedef base::PowerObserver PowerObserver;
+#endif  // defined(OS_LINUX)
+    
+    #ifndef PREPOSITION
+#  define PREPOSITION(Word)
+#endif
+    
+    
+    {} // end anonymous namespace
+    
+      bool isValid() const { return IsValid; }
+  explicit operator bool() const { return isValid(); }
+    
+    #include 'ClangDiagnosticConsumer.h'
+#include 'ImporterImpl.h'
+#include 'swift/AST/ASTContext.h'
+#include 'swift/AST/DiagnosticEngine.h'
+#include 'swift/AST/DiagnosticsClangImporter.h'
+#include 'clang/AST/ASTContext.h'
+#include 'clang/Frontend/DiagnosticRenderer.h'
+#include 'clang/Frontend/FrontendDiagnostic.h'
+#include 'clang/Lex/LexDiagnostic.h'
+#include 'llvm/ADT/STLExtras.h'
+    
+    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
+::opencensus::stats::MeasureDouble RpcClientServerLatency();
+::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
+    
+    void ProtoServerReflection::FillFileDescriptorResponse(
+    const protobuf::FileDescriptor* file_desc,
+    ServerReflectionResponse* response,
+    std::unordered_set<grpc::string>* seen_files) {
+  if (seen_files->find(file_desc->name()) != seen_files->end()) {
+    return;
+  }
+  seen_files->insert(file_desc->name());
+    }
+    
+      // implementation of ServerReflectionInfo(stream ServerReflectionRequest) rpc
+  // in ServerReflection service
+  Status ServerReflectionInfo(
+      ServerContext* context,
+      ServerReaderWriter<reflection::v1alpha::ServerReflectionResponse,
+                         reflection::v1alpha::ServerReflectionRequest>* stream)
+      override;
+    
+    // Force InitProtoReflectionServerBuilderPlugin() to be called at static
+// initialization time.
+struct StaticProtoReflectionPluginInitializer {
+  StaticProtoReflectionPluginInitializer() {
+    InitProtoReflectionServerBuilderPlugin();
+  }
+} static_proto_reflection_plugin_initializer;
+    
+    //
+// Api
+//
+    
+    	int			m_numIterations;
+	int			m_solverMode;
+	int			m_restingContactRestitutionThreshold;
+	int			m_minimumSolverBatchSize;
+    
+    
+    {	b3Scalar impulseClamp = m_setting.m_impulseClamp;//
+	for (j=0; j<3; j++)
+    {
+		if (m_setting.m_impulseClamp > 0)
+		{
+			info->m_lowerLimit[j*info->rowskip] = -impulseClamp;
+			info->m_upperLimit[j*info->rowskip] = impulseClamp;
+		}
+	}
+	info->m_damping = m_setting.m_damping;
+	
+}
+    
+    				const b3Vector3& N3 = planeEquations[k];
+    
+    // Called by Init/NewFrame/Shutdown
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateFontsTexture();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyFontsTexture();
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL3_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL3_DestroyDeviceObjects();
 
     
-    U_NAMESPACE_END
+        // Setup back-end capabilities flags
+    ImGuiIO& io = ImGui::GetIO();
+    io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;       // We can honor GetMouseCursor() values (optional)
+    io.BackendPlatformName = io.BackendRendererName = 'imgui_impl_allegro5';
     
-    #include 'unicode/utypes.h'
-    
-    StringMatcher::StringMatcher(const StringMatcher& o) :
-    UnicodeFunctor(o),
-    UnicodeMatcher(o),
-    UnicodeReplacer(o),
-    pattern(o.pattern),
-    data(o.data),
-    segmentNumber(o.segmentNumber),
-    matchStart(o.matchStart),
-    matchLimit(o.matchLimit)
-{
-}
-    
-    
-    {        // Setup default cursor position (for cursorPos within output)
-        newStart = cursorPos;
-    }
-    
-    bool Pack::checkVersion(const std::string& version) const {
-  if (version.empty() || version == 'null') {
-    return true;
-  }
-    }
-    
-        registered.erase(table_name);
-    std::string table_settings{table_name + query + columns_value + path};
-    std::string old_setting;
-    auto s = getDatabaseValue(
-        kPersistentSettings, kDatabaseKeyPrefix + table_name, old_setting);
-    
-    /// Define a map of decoration points to their expected configuration key.
-extern const std::map<DecorationPoint, std::string> kDecorationPointKeys;
-    
-    
-    {REGISTER_INTERNAL(KafkaTopicsConfigParserPlugin,
-                  'config_parser',
-                  'kafka_topics');
-} // namespace osquery
-
-    
-    std::vector<std::string> PrometheusMetricsConfigParserPlugin::keys() const {
-  return {kPrometheusParserRootKey};
-}
-    
-        /** Replace the interior action.
-     *
-     * @param action The new action, it will replace the running action.
-     */
-    void setInnerAction(ActionInterval *action);
-    /** Return the interior action.
-     *
-     * @return The interior action.
-     */
-    ActionInterval* getInnerAction() const { return _innerAction; }
-    
-    
-    {    // FIXME: Using the AdditionalTransform is a complete hack.
-    // This should be done by multiplying the lookup-Matrix with the Node's MV matrix
-    // And then setting the result as the new MV matrix
-    // But that operation needs to be done after all the 'updates'.
-    // So the Director should emit an 'director_after_update' event.
-    // And this object should listen to it
-    _target->setAdditionalTransform(&mv);
-}
-    
-    
-    {    float _radZ;
-    float _radDeltaZ;
-    float _radX;
-    float _radDeltaX;
-};
-    
-        /** Replaces an existing controlPoint at index.
-     *
-     * @js NA
-     * @param controlPoint A control point.
-     * @param index Replace the point to array in index.
-     */
-    void replaceControlPoint(const Vec2& controlPoint, ssize_t index);
-    
-    /** @class ActionManager
- @brief ActionManager is a singleton that manages all the actions.
- Normally you won't need to use this singleton directly. 99% of the cases you will use the Node interface,
- which uses this singleton.
- But there are some cases where you might need to use this singleton.
- Examples:
-    - When you want to run an action where the target is different from a Node. 
-    - When you want to pause / resume the actions.
- 
- @since v0.8
- */
-class CC_DLL ActionManager : public Ref
-{
-public:
-    /**
-     * @js ctor
-     */
-    ActionManager();
-    }
-    
-    /**
- * @addtogroup actions
- * @{
- */
-    }
-    
-     @code
-     auto modifyWidth = ActionTween::create(2, 'width', 200, 300);
-     target->runAction(modifyWidth);
- @endcode
-    
-    
-    {    frame->autorelease();
-    return frame;
-}
-    
-    
-    {    //real rect is the size that is in scale with the texture file
-    Rect getRealRect(const Rect& rect);
-    
-    Image* _image;
-    unsigned char * _data;
-    std::string _filename;
-    unsigned int _width;
-    unsigned int _height;
-    float _scaleFactor;
-    unsigned int _threshold;
-};
-    
-    namespace benchmark {
-    }
-    
-    void Finish(UserCounters *l, double cpu_time, double num_threads) {
-  for (auto &c : *l) {
-    c.second.value = Finish(c.second, cpu_time, num_threads);
-  }
-}
-    
-    #include 'benchmark/benchmark.h'
-#include 'internal_macros.h'
-    
-    
-    {  for (auto it = reports.begin(); it != reports.end(); ++it) {
-    out << indent << '{\n';
-    PrintRunData(*it);
-    out << indent << '}';
-    auto it_cp = it;
-    if (++it_cp != reports.end()) {
-      out << ',\n';
-    }
-  }
-}
-    
-    
-    {      delete[] errbuf;
-    }
-    
-    #include 'benchmark/benchmark.h'
-#include 'timers.h'
+        // Setup time step
+    INT64 current_time;
+    ::QueryPerformanceCounter((LARGE_INTEGER *)&current_time);
+    io.DeltaTime = (float)(current_time - g_Time) / g_TicksPerSecond;
+    g_Time = current_time;
