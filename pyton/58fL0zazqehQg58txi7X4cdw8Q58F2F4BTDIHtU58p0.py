505b10644081ebfbf6801370c419d078a3f654bc
SUNsung <weatherius@gmail.com>
2019-01-06 01:03:36 +0200

    Rewrite [58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py] File in: 2019-01-06 01:03:36

--- pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
+++ pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
@@ -1,76 +1,24 @@
 
         
-            def __init__(self, license_plate):
-        super(Motorcycle, self).__init__(VehicleSize.MOTORCYCLE, license_plate, spot_size=1)
+        
+if __name__ == '__main__':
+    ellipse1 = Ellipse('1')
+    ellipse2 = Ellipse('2')
+    ellipse3 = Ellipse('3')
+    ellipse4 = Ellipse('4')
     
-        # 128+2 SIGINT <http://www.tldp.org/LDP/abs/html/exitcodes.html>
-    ERROR_CTRL_C = 130
+        def increment(self):
+        self.value += 1
     
-        cert = None
-    if args.cert:
-        cert = args.cert
-        if args.cert_key:
-            cert = cert, args.cert_key
+            pet = self.pet_factory()
+        print('We have a lovely {}'.format(pet))
+        print('It says {}'.format(pet.speak()))
     
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
+        def test_object(queue):
+        pool = ObjectPool(queue, True)
+        print('Inside func: {}'.format(pool.item))
     
     
-def load_json_preserve_order(s):
-    return json.loads(s, object_pairs_hook=OrderedDict)
-    
-    
-tests_require = [
-    # Pytest needs to come last.
-    # https://bitbucket.org/pypa/setuptools/issue/196/
-    'pytest-httpbin',
-    'pytest',
-    'mock',
-]
-    
-        def test_binary_file_form(self, httpbin):
-        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
-        r = http('--print=B', '--form', 'POST', httpbin.url + '/post',
-                 'test@' + BIN_FILE_PATH_ARG, env=env)
-        assert bytes(BIN_FILE_CONTENT) in bytes(r)
-    
-        def test_POST_with_data_auto_JSON_headers(self, httpbin):
-        r = http('POST', httpbin.url + '/post', 'a=b')
-        assert HTTP_OK in r
-        assert r.json['headers']['Accept'] == JSON_ACCEPT
-        assert r.json['headers']['Content-Type'] == 'application/json'
-    
-    
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
-    
-    
-class Command(ScrapyCommand):
-    
-            shell = Shell(crawler, update_vars=self.update_vars, code=opts.code)
-        shell.start(url=url, redirect=not opts.no_redirect)
-    
-                # trustRoot set to platformTrust() will use the platform's root CAs.
-            #
-            # This means that a website like https://www.cacert.org will be rejected
-            # by default, since CAcert.org CA certificate is seldom shipped.
-            return optionsForClientTLS(hostname.decode('ascii'),
-                                       trustRoot=platformTrust(),
-                                       extraCertificateOptions={
-                                            'method': self._ssl_method,
-                                       })
+class OutOfService(Unit):
+    def __init__(self, HierachicalStateMachine):
+        self._hsm = HierachicalStateMachine
