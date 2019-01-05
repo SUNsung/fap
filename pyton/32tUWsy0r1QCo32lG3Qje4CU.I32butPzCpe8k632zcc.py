6f6dd82e0bae59727dd5b7a5aa9a71d95adf68c8
SUNsung <weatherius@gmail.com>
2019-01-05 04:03:19 +0200

    Rewrite [32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py] File in: 2019-01-05 04:03:19

--- pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
+++ pyton/32tUWsy0r1QCo32lG3Qje4CU.I32butPzCpe8k632zcc.py
@@ -1,43 +1,335 @@
 
         
-        new_version = {}
+        try:
+    # https://urllib3.readthedocs.io/en/latest/security.html
+    # noinspection PyPackageRequirements
+    import urllib3
+    urllib3.disable_warnings()
+except (ImportError, AttributeError):
+    # In some rare cases, the user may have an old version of the requests
+    # or urllib3, and there is no method called 'disable_warnings.' In these
+    # cases, we don't need to call the method.
+    # They may get some noisy output but execution shouldn't die. Move on.
+    pass
     
-    compat_print('total downloads traffic: %s' % format_size(total_bytes))
+            return '\r\n'.join(headers)
+    
+    
+def test_current_version():
+    version = Environment().config['__meta__']['httpie']
+    assert version == __version__
 
     
-        flags = [opt.get_opt_string() for opt in opts]
     
-            info_dict = _make_result(list(formats_order), extractor='youtube')
-        ydl = YDL({'format': '(bestvideo[ext=mp4],bestvideo[ext=webm])+bestaudio'})
-        yie = YoutubeIE(ydl)
-        yie._sort_formats(info_dict['formats'])
-        ydl.process_ie_result(info_dict)
-        downloaded_ids = [info['format_id'] for info in ydl.downloaded_info_dicts]
-        self.assertEqual(downloaded_ids, ['137+141', '248+141'])
+@mock.patch('httpie.core.get_response')
+def test_timeout(get_response):
+    def error(msg, *args, **kwargs):
+        global error_msg
+        error_msg = msg % args
     
-        def test_youtube_playlist_matching(self):
-        assertPlaylist = lambda url: self.assertMatch(url, ['youtube:playlist'])
-        assertPlaylist('ECUl4u3cNGP61MdtwGTqZA0MreSaDybji8')
-        assertPlaylist('UUBABnxM4Ar9ten8Mdjj1j0Q')  # 585
-        assertPlaylist('PL63F0C78739B09958')
-        assertPlaylist('https://www.youtube.com/playlist?list=UUBABnxM4Ar9ten8Mdjj1j0Q')
-        assertPlaylist('https://www.youtube.com/course?list=ECUl4u3cNGP61MdtwGTqZA0MreSaDybji8')
-        assertPlaylist('https://www.youtube.com/playlist?list=PLwP_SiAcdui0KVebT0mU9Apz359a4ubsC')
-        assertPlaylist('https://www.youtube.com/watch?v=AV6J6_AeFEQ&playnext=1&list=PL4023E734DA416012')  # 668
-        self.assertFalse('youtube:playlist' in self.matching_ies('PLtS2H6bU1M'))
-        # Top tracks
-        assertPlaylist('https://www.youtube.com/playlist?list=MCUS.20142101')
     
+class TestRequestBodyFromFilePath:
     '''
-from datetime import datetime
-import numpy as np
-from sklearn import linear_model
-from sklearn.utils.bench import total_seconds
+    `http URL @file'
     
-        formats : array-like of callables (1d or 0d)
-        These may transform a dense indicator matrix into multilabel
-        representation.
+        def __init__(self, get_response=None):
+        if not apps.is_installed('django.contrib.sites'):
+            raise ImproperlyConfigured(
+                'You cannot use RedirectFallbackMiddleware when '
+                'django.contrib.sites is not installed.'
+            )
+        super().__init__(get_response)
+    
+        For complete documentation on using Sessions in your code, consult
+    the sessions documentation that is shipped with Django (also available
+    on the Django Web site).
+    '''
+    objects = SessionManager()
+    
+    import pytest
+from requests.compat import urljoin
     
     
-def sparsity_ratio(X):
-    return np.count_nonzero(X) / float(n_samples * n_features)
+def _check_cryptography(cryptography_version):
+    # cryptography < 1.3.4
+    try:
+        cryptography_version = list(map(int, cryptography_version.split('.')))
+    except ValueError:
+        return
+    
+    
+if __name__ == '__main__':
+    main()
+
+    
+        def test_equality(self):
+        cid = CaseInsensitiveDict({'SPAM': 'blueval', 'Eggs': 'redval'})
+        othercid = CaseInsensitiveDict({'spam': 'blueval', 'eggs': 'redval'})
+        assert cid == othercid
+        del othercid['spam']
+        assert cid != othercid
+        assert cid == {'spam': 'blueval', 'eggs': 'redval'}
+        assert cid != object()
+    
+        cwe_rule = CloudWatchEventRule(module,
+                                   client=get_cloudwatchevents_client(module),
+                                   **rule_data)
+    cwe_rule_manager = CloudWatchEventRuleManager(cwe_rule, targets)
+    
+        module.exit_json(changed=changed,
+                     snapshot_id=snapshot.id,
+                     volume_id=snapshot.volume_id,
+                     volume_size=snapshot.volume_size,
+                     tags=snapshot.tags.copy())
+    
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+    
+        creds, params = get_google_cloud_credentials(module)
+    spanner_client = spanner.Client(project=params['project_id'],
+                                    credentials=creds,
+                                    user_agent=CLOUD_CLIENT_USER_AGENT)
+    changed = False
+    json_output = {}
+    
+            if state == 'absent' and user in heroku_collaborator_list:
+            if not module.check_mode:
+                heroku_app.remove_collaborator(user)
+            affected_apps += [app]
+            result_state = True
+        elif state == 'present' and user not in heroku_collaborator_list:
+            if not module.check_mode:
+                heroku_app.add_collaborator(user_id_or_email=user, silent=module.params['suppress_invitation'])
+            affected_apps += [app]
+            result_state = True
+    
+    HOST_ABSENT = -99  # the host is absent (special case defined by this module)
+    
+    
+def get_role_facts(cursor, role=''):
+    facts = {}
+    cursor.execute('''
+        select r.name, r.assigned_roles
+        from roles r
+        where (? = '' or r.name ilike ?)
+    ''', role, role)
+    while True:
+        rows = cursor.fetchmany(100)
+        if not rows:
+            break
+        for row in rows:
+            role_key = row.name.lower()
+            facts[role_key] = {
+                'name': row.name,
+                'assigned_roles': []}
+            if row.assigned_roles:
+                facts[role_key]['assigned_roles'] = row.assigned_roles.replace(' ', '').split(',')
+    return facts
+    
+    
+DOCUMENTATION = '''
+---
+module: vertica_schema
+version_added: '2.0'
+short_description: Adds or removes Vertica database schema and roles.
+description:
+  - Adds or removes Vertica database schema and, optionally, roles
+    with schema access privileges.
+  - A schema will not be removed until all the objects have been dropped.
+  - In such a situation, if the module tries to remove the schema it
+    will fail and only remove roles created for the schema if they have
+    no dependencies.
+options:
+  name:
+    description:
+      - Name of the schema to add or remove.
+    required: true
+  usage_roles:
+    description:
+      - Comma separated list of roles to create and grant usage access to the schema.
+    aliases: ['usage_role']
+  create_roles:
+    description:
+      - Comma separated list of roles to create and grant usage and create access to the schema.
+    aliases: ['create_role']
+  owner:
+    description:
+      - Name of the user to set as owner of the schema.
+  state:
+    description:
+      - Whether to create C(present), or drop C(absent) a schema.
+    default: present
+    choices: ['present', 'absent']
+  db:
+    description:
+      - Name of the Vertica database.
+  cluster:
+    description:
+      - Name of the Vertica cluster.
+    default: localhost
+  port:
+    description:
+      - Vertica cluster port to connect to.
+    default: 5433
+  login_user:
+    description:
+      - The username used to authenticate with.
+    default: dbadmin
+  login_password:
+    description:
+      - The password used to authenticate with.
+notes:
+  - The default authentication assumes that you are either logging in as or sudo'ing
+    to the C(dbadmin) account on the host.
+  - This module uses C(pyodbc), a Python ODBC database adapter. You must ensure
+    that C(unixODBC) and C(pyodbc) is installed on the host and properly configured.
+  - Configuring C(unixODBC) for Vertica requires C(Driver = /opt/vertica/lib64/libverticaodbc.so)
+    to be added to the C(Vertica) section of either C(/etc/odbcinst.ini) or C($HOME/.odbcinst.ini)
+    and both C(ErrorMessagesPath = /opt/vertica/lib64) and C(DriverManagerEncoding = UTF-16)
+    to be added to the C(Driver) section of either C(/etc/vertica.ini) or C($HOME/.vertica.ini).
+requirements: [ 'unixODBC', 'pyodbc' ]
+author: 'Dariusz Owczarek (@dareko)'
+'''
+    
+        # Send the data to airbrake
+    data = urlencode(params)
+    response, info = fetch_url(module, url, data=data)
+    if info['status'] == 200:
+        module.exit_json(changed=True)
+    else:
+        module.fail_json(msg='HTTP result code: %d connecting to %s' % (info['status'], url))
+    
+    from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+    
+    
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('metrics', nargs='*', default=sorted(METRICS),
+                    help='Specifies metrics to benchmark, defaults to all. '
+                         'Choices are: {}'.format(sorted(METRICS)))
+    ap.add_argument('--formats', nargs='+', choices=sorted(FORMATS),
+                    help='Specifies multilabel formats to benchmark '
+                         '(defaults to all).')
+    ap.add_argument('--samples', type=int, default=1000,
+                    help='The number of samples to generate')
+    ap.add_argument('--classes', type=int, default=10,
+                    help='The number of classes')
+    ap.add_argument('--density', type=float, default=.2,
+                    help='The average density of labels per sample')
+    ap.add_argument('--plot', choices=['classes', 'density', 'samples'],
+                    default=None,
+                    help='Plot time with respect to this parameter varying '
+                         'up to the specified value')
+    ap.add_argument('--n-steps', default=10, type=int,
+                    help='Plot this many points for each metric')
+    ap.add_argument('--n-times',
+                    default=5, type=int,
+                    help='Time performance over n_times trials')
+    args = ap.parse_args()
+    
+    
+def benchmark(estimator, data):
+    gc.collect()
+    print('Benching %s' % estimator)
+    t0 = time()
+    estimator.fit(data)
+    training_time = time() - t0
+    data_t = estimator.transform(data)
+    data_r = estimator.inverse_transform(data_t)
+    reconstruction_error = np.mean(np.abs(data - data_r))
+    return {'time': training_time, 'error': reconstruction_error}
+    
+        for (sbplt, vals, quantity,
+         build_time, query_time) in [(311, Nrange, 'N',
+                                      N_results_build,
+                                      N_results_query),
+                                     (312, Drange, 'D',
+                                      D_results_build,
+                                      D_results_query),
+                                     (313, krange, 'k',
+                                      k_results_build,
+                                      k_results_query)]:
+        ax = plt.subplot(sbplt, yscale='log')
+        plt.grid(True)
+    
+        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
+    plt.plot(sample_sizes, one_core, label='one core')
+    plt.plot(sample_sizes, multi_core, label='multi core')
+    plt.xlabel('n_samples')
+    plt.ylabel('Time (s)')
+    plt.title('Parallel %s' % func.__name__)
+    plt.legend()
+    
+    First, we fix a training set, increase the number of
+samples to classify and plot number of classified samples as a
+function of time.
+    
+    # Plot the confusion matrix
+cm = metrics.confusion_matrix(y_test, y_predicted)
+print(cm)
+    
+    import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+    
+        # import matplotlib.pyplot as plt
+    # plt.matshow(cm)
+    # plt.show()
+
+    
+        def test_decode_good(self):
+        self.assertEqual('x', self.field.decode('x'))
+    
+    latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
+    }
+    
+        '''
+    deps = {
+        'ssl': ['setenvif', 'mime']
+    }
+    return deps.get(mod_name, [])
+    
+            :param bool temporary: Indicates whether the changes made will
+            be quickly reversed in the future (ie. challenges)
+    
+    AUTOHSTS_FREQ = 172800
+'''Minimum time since last increase to perform a new one: 48h'''
+    
+            from certbot_apache import tls_sni_01
+        self.sni = tls_sni_01.ApacheTlsSni01(config)
+    
+    # Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+]
+    
+        # Make the eyebrows into a nightmare
+    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
+    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
+    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
+    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
+    
+    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
+# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
+# positive matches at the risk of more false negatives.
+    
+    # Initialize some variables
+face_locations = []
+face_encodings = []
