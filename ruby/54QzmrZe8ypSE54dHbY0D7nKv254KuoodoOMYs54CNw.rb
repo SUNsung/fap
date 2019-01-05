9b222f68d0f272d5ddc00be42f67b93380bd4d91
SUNsung <weatherius@gmail.com>
2019-01-05 22:58:26 +0200

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2019-01-05 22:58:25

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,65 +1,50 @@
 
         
-              expect(page).to have_text('Test Scheduler Agent')
-    
-        it 'shows the dry run pop up without previous events and selects the events tab when a event was created' do
-      open_dry_run_modal(agent)
-      click_on('Dry Run')
-      expect(page).to have_text('Biologists play reverse')
-      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
+            def resource_params
+      params.require(:user).permit(
+        :unconfirmed_email
+      )
     end
-    
-      describe '#status' do
-    it 'works for failed jobs' do
-      job.failed_at = Time.now
-      expect(status(job)).to eq('<span class='label label-danger'>failed</span>')
-    end
-    
-        it 'is turned off for existing instances of Huginn' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
-      DefaultScenarioImporter.import(user)
-    end
-    
-        def backend
-      @backend ||= SSHKit
-    end
-    
-          def allow_uploads
-        @allow_uploads
-      end
-    
-          def use_identicon
-        @wiki.user_icons == 'identicon'
-      end
-    
-        assert_no_match /New/, last_response.body, ''New' link not blocked in pages template'
-    
-    context 'Precious::Helpers' do
-  include Precious::Helpers
-    
-    context 'Precious::Views::LatestChanges' do
-  include Rack::Test::Methods
-  
-  def app
-    Precious::App
   end
-  
-  setup do
-    @path = cloned_testpath('examples/lotr.git')
-    @wiki = Gollum::Wiki.new(@path)
-    Precious::App.set(:gollum_path, @path)
-    Precious::App.set(:wiki_options, {:latest_changes_count => 10})
+end
+
+    
+      def hub_topic_uri
+    @_hub_topic_uri ||= Addressable::URI.parse(hub_topic).normalize
   end
     
-      test 'h1 title can be disabled' do
-    title = 'H1'
-    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
-    page = @wiki.page(title)
+      def verify_payload?
+    payload.present? && VerifySalmonService.new.call(payload)
+  end
     
-        def forbid(msg = 'Forbidden. This wiki is set to no-edit mode.')
-      @message = msg
-      status 403
-      halt mustache :error
+        head 200
+  end
+    
+          expect(cop.offenses.size).to eq(1)
     end
+    
+          it 'does not autocorrect the closing brace' do
+        new_source = autocorrect_source(source)
+        expect(new_source).to eq([source].join($RS))
+      end
+    end
+    
+            def delta(first, second, alignment = :left)
+          case alignment
+          when :left
+            first.column - second.column
+          when :right
+            first.last_column - second.last_column
+          else
+            0
+          end
+        end
+    
+          # A shorthand for getting the last argument of the node.
+      # Equivalent to `arguments.last`.
+      #
+      # @return [Node, nil] the last argument of the node,
+      #                     or `nil` if there are no arguments
+      def last_argument
+        arguments[-1]
+      end
