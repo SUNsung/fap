adab0977e93098ac0f39adaeb004ff6d7048eaff
SUNsung <weatherius@gmail.com>
2019-01-05 03:31:36 +0200

    Rewrite [53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py] File in: 2019-01-05 03:31:36

--- pyton/53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py
+++ pyton/53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py
@@ -1,205 +1,45 @@
 
         
-            if RESULT and ('info_dict' not in test or 'age_limit' not in test['info_dict'] or
-                   test['info_dict']['age_limit'] != 18):
-        print('\nPotential missing age_limit check: {0}'.format(test['name']))
+            event_log = advapi32.RegisterEventSourceW(None, service_name)
+    if not event_log:
+        raise OSError('Could not report event: %s' % ctypes.FormatError())
     
+        elif not RESULT and ('info_dict' in test and 'age_limit' in test['info_dict'] and
+                         test['info_dict']['age_limit'] == 18):
+        print('\nPotential false negative: {0}'.format(test['name']))
     
-def main():
-    parser = optparse.OptionParser(usage='%prog CHANGELOG VERSION BUILDPATH')
-    options, args = parser.parse_args()
-    if len(args) != 3:
-        parser.error('Expected a version and a build directory')
+    versions_info = json.load(open('update/versions.json'))
+if 'signature' in versions_info:
+    del versions_info['signature']
     
-    
-    {    # Options that need a file parameter
-    'download-archive': ['--require-parameter'],
-    'cookies': ['--require-parameter'],
-    'load-info': ['--require-parameter'],
-    'batch-file': ['--require-parameter'],
-}
-    
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
+        with open('supportedsites.html', 'w', encoding='utf-8') as sitesf:
+        sitesf.write(template)
     
         def test_module_exec(self):
         if sys.version_info >= (2, 7):  # Python 2.6 doesn't support package execution
             subprocess.check_call([sys.executable, '-m', 'youtube_dl', '--version'], cwd=rootDir, stdout=_DEV_NULL)
     
+        def get_converter(self, mime):
+        if is_valid_mime(mime):
+            for converter_class in plugin_manager.get_converters():
+                if converter_class.supports(mime):
+                    return converter_class(mime)
     
-@pytest.mark.parametrize('script, output', [
-    ('brew link sshfs', output),
-    ('cat output', output),
-    ('brew install sshfs', '')])
-def test_not_match(script, output):
-    command = Command(script, output)
-    assert not match(command)
+        See also `test_auth_plugins.py`
     
-    x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-    
-        def __init__(self, l1=0., l2=0.):
-        self.l1 = K.cast_to_floatx(l1)
-        self.l2 = K.cast_to_floatx(l2)
+        def test_POST_form_auto_Content_Type(self, httpbin):
+        r = http('--form', 'POST', httpbin.url + '/post')
+        assert HTTP_OK in r
+        assert ''Content-Type': 'application/x-www-form-urlencoded' in r
     
     
-def test_boston_housing():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
+def unicode_is_ascii(u_string):
+    '''Determine if unicode string only contains ASCII characters.
     
-    from keras.models import Sequential, Model
-from keras.layers import Dense, Input, Average
-from keras.utils import np_utils
-from keras.utils import test_utils
-from keras import regularizers
-from keras import backend as K
-    
-        if isinstance(gpus, (list, tuple)):
-        if len(gpus) <= 1:
-            raise ValueError('For multi-gpu usage to be effective, '
-                             'call `multi_gpu_model` with `len(gpus) >= 2`. '
-                             'Received: `gpus=%s`' % gpus)
-        num_gpus = len(gpus)
-        target_gpu_ids = gpus
-    else:
-        if gpus <= 1:
-            raise ValueError('For multi-gpu usage to be effective, '
-                             'call `multi_gpu_model` with `gpus >= 2`. '
-                             'Received: `gpus=%d`' % gpus)
-        num_gpus = gpus
-        target_gpu_ids = range(num_gpus)
-    
-        def reset_states(self, states=None):
-        if not self.stateful:
-            raise AttributeError('Layer must be stateful.')
-        batch_size = self.input_spec[0].shape[0]
-        if not batch_size:
-            raise ValueError('If a RNN is stateful, it needs to know '
-                             'its batch size. Specify the batch size '
-                             'of your input tensors: \n'
-                             '- If using a Sequential model, '
-                             'specify the batch size by passing '
-                             'a `batch_input_shape` '
-                             'argument to your first layer.\n'
-                             '- If using the functional API, specify '
-                             'the batch size by passing a '
-                             '`batch_shape` argument to your Input layer.')
-        # initialize state if None
-        if self.states[0] is None:
-            if hasattr(self.cell.state_size, '__len__'):
-                self.states = [K.zeros((batch_size, dim))
-                               for dim in self.cell.state_size]
-            else:
-                self.states = [K.zeros((batch_size, self.cell.state_size))]
-        elif states is None:
-            if hasattr(self.cell.state_size, '__len__'):
-                for state, dim in zip(self.states, self.cell.state_size):
-                    K.set_value(state, np.zeros((batch_size, dim)))
-            else:
-                K.set_value(self.states[0],
-                            np.zeros((batch_size, self.cell.state_size)))
-        else:
-            states = to_list(states, allow_tuple=True)
-            if len(states) != len(self.states):
-                raise ValueError('Layer ' + self.name + ' expects ' +
-                                 str(len(self.states)) + ' states, '
-                                 'but it received ' + str(len(states)) +
-                                 ' state values. Input received: ' +
-                                 str(states))
-            for index, (value, state) in enumerate(zip(states, self.states)):
-                if hasattr(self.cell.state_size, '__len__'):
-                    dim = self.cell.state_size[index]
-                else:
-                    dim = self.cell.state_size
-                if value.shape != (batch_size, dim):
-                    raise ValueError('State ' + str(index) +
-                                     ' is incompatible with layer ' +
-                                     self.name + ': expected shape=' +
-                                     str((batch_size, dim)) +
-                                     ', found shape=' + str(value.shape))
-                # TODO: consider batch calls to `set_value`.
-                K.set_value(state, value)
-    
-        asg_name = module.params.get('name')
-    asg_tags = module.params.get('tags')
-    
-    
-if __name__ == '__main__':
-    main()
+        @get_item_parameters
+    def test_get(self, key, value):
+        assert self.lookup_dict.get(key) == value
 
     
-    
-def get_eips_details(module):
-    connection = module.client('ec2')
-    filters = module.params.get('filters')
-    try:
-        response = connection.describe_addresses(
-            Filters=ansible_dict_to_boto3_filter_list(filters)
-        )
-    except (BotoCoreError, ClientError) as e:
-        module.fail_json_aws(
-            e,
-            msg='Error retrieving EIPs')
-    
-        params = dict()
-    if snapshot_name:
-        params['DBSnapshotIdentifier'] = snapshot_name
-    if instance_name:
-        params['DBInstanceIdentifier'] = instance_name
-    if snapshot_type:
-        params['SnapshotType'] = snapshot_type
-        if snapshot_type == 'public':
-            params['IsPublic'] = True
-        elif snapshot_type == 'shared':
-            params['IsShared'] = True
-    
-    
-def _collect_facts(resource):
-    '''Transfrom cluster information to dict.'''
-    facts = {
-        'identifier': resource['ClusterIdentifier'],
-        'create_time': resource['ClusterCreateTime'],
-        'status': resource['ClusterStatus'],
-        'username': resource['MasterUsername'],
-        'db_name': resource['DBName'],
-        'availability_zone': resource['AvailabilityZone'],
-        'maintenance_window': resource['PreferredMaintenanceWindow'],
-        'url': resource['Endpoint']['Address'],
-        'port': resource['Endpoint']['Port']
-    }
-    
-    EXAMPLES = '''
-- name: Create Minimum GLOBAL Forwarding_Rule
-  gcp_forwarding_rule:
-    service_account_email: '{{ service_account_email }}'
-    credentials_file: '{{ credentials_file }}'
-    project_id: '{{ project_id }}'
-    forwarding_rule_name: my-forwarding_rule
-    protocol: TCP
-    port_range: 80
-    region: global
-    target: my-target-proxy
-    state: present
-    
-    from __future__ import print_function
-from socket import timeout
+    # If true, 'Created using Sphinx' is shown in the HTML footer. Default is True.
+html_show_sphinx = False
