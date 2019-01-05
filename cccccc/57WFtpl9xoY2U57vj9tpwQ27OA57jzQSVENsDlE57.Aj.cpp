873077fe6c6176fc4e0b44e21cd3aae6cde09f71
SUNsung <weatherius@gmail.com>
2019-01-05 05:20:09 +0200

    Rewrite [57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp] File in: 2019-01-05 05:20:09

--- cccccc/57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp
+++ cccccc/57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp
@@ -1,106 +1,231 @@
 
         
-        
-    {  delete db;
-  delete [] pixels;
+        void WriteDocCommentBodyImpl(io::Printer* printer, SourceLocation location) {
+    string comments = location.leading_comments.empty() ?
+        location.trailing_comments : location.leading_comments;
+    if (comments.empty()) {
+        return;
+    }
+    // XML escaping... no need for apostrophes etc as the whole text is going to be a child
+    // node of a summary element, not part of an attribute.
+    comments = StringReplace(comments, '&', '&amp;', true);
+    comments = StringReplace(comments, '<', '&lt;', true);
+    std::vector<string> lines = Split(comments, '\n', false /* skip_empty */);
+    // TODO: We really should work out which part to put in the summary and which to put in the remarks...
+    // but that needs to be part of a bigger effort to understand the markdown better anyway.
+    printer->Print('/// <summary>\n');
+    bool last_was_empty = false;
+    // We squash multiple blank lines down to one, and remove any trailing blank lines. We need
+    // to preserve the blank lines themselves, as this is relevant in the markdown.
+    // Note that we can't remove leading or trailing whitespace as *that's* relevant in markdown too.
+    // (We don't skip 'just whitespace' lines, either.)
+    for (std::vector<string>::iterator it = lines.begin(); it != lines.end(); ++it) {
+        string line = *it;
+        if (line.empty()) {
+            last_was_empty = true;
+        } else {
+            if (last_was_empty) {
+                printer->Print('///\n');
+            }
+            last_was_empty = false;
+            printer->Print('///$line$\n', 'line', *it);
+        }
+    }
+    printer->Print('/// </summary>\n');
 }
     
-    /**
- * @brief Abstract base class that factors out the BLAS code common to
- *        ConvolutionLayer and DeconvolutionLayer.
- */
-template <typename Dtype>
-class BaseConvolutionLayer : public Layer<Dtype> {
+    
+    {  WriteFieldDocComment(printer, descriptor_);
+  if (descriptor_->is_repeated()) {
+    printer->Print(
+        vars,
+        'public static final\n'
+        '  com.google.protobuf.GeneratedMessageLite.GeneratedExtension<\n'
+        '    $containing_type$,\n'
+        '    $type$> $name$ = com.google.protobuf.GeneratedMessageLite\n'
+        '        .newRepeatedGeneratedExtension(\n'
+        '      $containing_type$.getDefaultInstance(),\n'
+        '      $prototype$,\n'
+        '      $enum_map$,\n'
+        '      $number$,\n'
+        '      com.google.protobuf.WireFormat.FieldType.$type_constant$,\n'
+        '      $packed$,\n'
+        '      $singular_type$.class);\n');
+  } else {
+    printer->Print(
+        vars,
+        'public static final\n'
+        '  com.google.protobuf.GeneratedMessageLite.GeneratedExtension<\n'
+        '    $containing_type$,\n'
+        '    $type$> $name$ = com.google.protobuf.GeneratedMessageLite\n'
+        '        .newSingularGeneratedExtension(\n'
+        '      $containing_type$.getDefaultInstance(),\n'
+        '      $default$,\n'
+        '      $prototype$,\n'
+        '      $enum_map$,\n'
+        '      $number$,\n'
+        '      com.google.protobuf.WireFormat.FieldType.$type_constant$,\n'
+        '      $singular_type$.class);\n');
+  }
+  printer->Annotate('name', descriptor_);
+}
+    
+    const FieldDescriptor* ValueField(const FieldDescriptor* descriptor) {
+  GOOGLE_CHECK_EQ(FieldDescriptor::TYPE_MESSAGE, descriptor->type());
+  const Descriptor* message = descriptor->message_type();
+  GOOGLE_CHECK(message->options().map_entry());
+  return message->FindFieldByName('value');
+}
+    
+      // Note:
+  //  deprecated-declarations suppression is only needed if some place in this
+  //    proto file is something deprecated or if it references something from
+  //    another file that is deprecated.
+  printer->Print(
+      '// @@protoc_insertion_point(imports)\n'
+      '\n'
+      '#pragma clang diagnostic push\n'
+      '#pragma clang diagnostic ignored \'-Wdeprecated-declarations\'\n'
+      '\n'
+      'CF_EXTERN_C_BEGIN\n'
+      '\n');
+    
+      EXPECT_EQ(4, decode_data.num_entries());
+    
+      // write central directory
+  io::CodedOutputStream output(raw_output_);
+  for (int i = 0; i < num_entries; ++i) {
+    const string &filename = files_[i].name;
+    uint16 filename_size = filename.size();
+    uint32 crc32 = files_[i].crc32;
+    uint32 size = files_[i].size;
+    uint32 offset = files_[i].offset;
+    }
+    
+      // Also ensure that the default instance handles field presence properly.
+  EXPECT_EQ(false, refl->HasField(*proto3_prototype_, optional_msg));
+    
+      /// Get enough parameters to be able to rebuild bounding boxes in the
+  /// original image (not just within the rectangle).
+  /// Left and top are enough with top-down coordinates, but
+  /// the height of the rectangle and the image are needed for bottom-up.
+  virtual void GetImageSizes(int* left, int* top, int* width, int* height,
+                             int* imagewidth, int* imageheight);
+    
+      // Adds a new point. Takes a copy - the pt doesn't need to stay in scope.
+  // Add must be called on points in sequence along the line.
+  void Add(const ICOORD& pt);
+  // Associates a half-width with the given point if a point overlaps the
+  // previous point by more than half the width, and its distance is further
+  // than the previous point, then the more distant point is ignored in the
+  // distance calculation. Useful for ignoring i dots and other diacritics.
+  void Add(const ICOORD& pt, int halfwidth);
+    
+    class CensusChannelData : public ChannelData {
  public:
-  explicit BaseConvolutionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-     private:
-  // Recursive copy function.
-  void crop_copy(const vector<Blob<Dtype>*>& bottom,
-               const vector<Blob<Dtype>*>& top,
-               const int* offsets,
-               vector<int> indices,
-               int cur_dim,
-               const Dtype* src_data,
-               Dtype* dest_data,
-               bool is_forward);
-    
-    namespace caffe {
-    }
-    
-    
-    {  size_t *workspace_fwd_sizes_;
-  size_t *workspace_bwd_data_sizes_;
-  size_t *workspace_bwd_filter_sizes_;
-  size_t workspaceSizeInBytes;  // size of underlying storage
-  void *workspaceData;  // underlying storage
-  void **workspace;  // aliases into workspaceData
+  grpc_error* Init(grpc_channel_element* elem,
+                   grpc_channel_element_args* args) override;
 };
-#endif
     
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of ReLULayer.
- */
-template <typename Dtype>
-class CuDNNReLULayer : public ReLULayer<Dtype> {
- public:
-  explicit CuDNNReLULayer(const LayerParameter& param)
-      : ReLULayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNReLULayer();
+      // Serializes a GrpcTraceContext into the provided buffer. Returns the number
+  // of bytes serialized into the buffer. If the buffer is not of sufficient
+  // size (it must be at least kGrpcTraceContextSize bytes) it will drop
+  // everything and return 0 bytes serialized. Inlined for performance reasons.
+  static size_t Encode(const GrpcTraceContext& tc, char* buf, size_t buf_size) {
+    if (buf_size < kGrpcTraceContextSize) {
+      return kEncodeDecodeFailure;
     }
-    
-    #include <grpcpp/security/auth_context.h>
-    
-    #endif  // GRPC_INTERNAL_CPP_DYNAMIC_THREAD_POOL_H
-
-    
-    #include <dmlc/registry.h>
-#include <functional>
-#include <vector>
-#include <utility>
-#include <string>
-#include './base.h'
-#include './data.h'
-#include './tree_model.h'
-#include '../../src/common/host_device_vector.h'
-    
-    
-    {    for (size_t i = 0; i < batch.size; ++i) {
-      offset_[i + 1] = (i + 1) * num_col_;
-      Row<IndexType> row = batch[i];
-      for (uint32_t j = 0; j < num_col_; ++j) {
-        dense_index_[i * num_col_ + j] = j;
-      }
-      for (unsigned k = 0; k < row.length; ++k) {
-        uint32_t index = row.get_index(k);
-        CHECK_LT(index, num_col_)
-            << 'Featuere index larger than num_col';
-        dense_value_[i * num_col_ + index]  = row.get_value(k);
-      }
-    }
-    out_ = batch;
-    out_.index = dmlc::BeginPtr(dense_index_);
-    out_.value = dmlc::BeginPtr(dense_value_);
-    out_.offset = dmlc::BeginPtr(offset_);
-    return true;
+    buf[kVersionIdOffset] = kVersionId;
+    buf[kTraceIdOffset] = kTraceIdField;
+    memcpy(&buf[kTraceIdOffset + 1], tc.trace_id,
+           opencensus::trace::TraceId::kSize);
+    buf[kSpanIdOffset] = kSpanIdField;
+    memcpy(&buf[kSpanIdOffset + 1], tc.span_id,
+           opencensus::trace::SpanId::kSize);
+    buf[kTraceOptionsOffset] = kTraceOptionsField;
+    memcpy(&buf[kTraceOptionsOffset + 1], tc.trace_options,
+           opencensus::trace::TraceOptions::kSize);
+    return kGrpcTraceContextSize;
   }
     
+    void ProtoServerReflectionPlugin::Finish(grpc::ServerInitializer* si) {
+  reflection_service_->SetServiceList(si->GetServiceList());
+}
     
-    {
-    {}  // namespace data
-}  // namespace xgboost
-#endif  // XGBOOST_DATA_SPARSE_PAGE_WRITER_H_
-
+    void SetCreateThreadPool(CreateThreadPoolFunc func) { g_ctp_impl = func; }
     
-          cbw.Write(buffer.data(), input.begin(), input.end());
+    namespace leveldb {
+    }
+    
+    
+    {    db_ = nullptr;
+    options_.create_if_missing = true;
+    Reopen();
+    options_.create_if_missing = false;
+  }
+    
+    // Return a new iterator that converts internal keys (yielded by
+// '*internal_iter') that were live at the specified 'sequence' number
+// into appropriate user keys.
+Iterator* NewDBIterator(DBImpl* db,
+                        const Comparator* user_key_comparator,
+                        Iterator* internal_iter,
+                        SequenceNumber sequence,
+                        uint32_t seed);
+    
+    TEST(DBTest, BloomFilter) {
+  env_->count_random_reads_ = true;
+  Options options = CurrentOptions();
+  options.env = env_;
+  options.block_cache = NewLRUCache(0);  // Prevent cache hits
+  options.filter_policy = NewBloomFilterPolicy(10);
+  Reopen(&options);
+    }
+    
+    
+// Called on every log record (each one of which is a WriteBatch)
+// found in a kLogFile.
+static void WriteBatchPrinter(uint64_t pos, Slice record, WritableFile* dst) {
+  std::string r = '--- offset ';
+  AppendNumberTo(&r, pos);
+  r += '; ';
+  if (record.size() < 12) {
+    r += 'log record length ';
+    AppendNumberTo(&r, record.size());
+    r += ' is too small\n';
+    dst->Append(r);
+    return;
+  }
+  WriteBatch batch;
+  WriteBatchInternal::SetContents(&batch, record);
+  r += 'sequence ';
+  AppendNumberTo(&r, WriteBatchInternal::Sequence(&batch));
+  r.push_back('\n');
+  dst->Append(r);
+  WriteBatchItemPrinter batch_item_printer;
+  batch_item_printer.dst_ = dst;
+  Status s = batch.Iterate(&batch_item_printer);
+  if (!s.ok()) {
+    dst->Append('  error: ' + s.ToString() + '\n');
+  }
+}
+    
+    // Return the name of the descriptor file for the db named by
+// 'dbname' and the specified incarnation number.  The result will be
+// prefixed with 'dbname'.
+std::string DescriptorFileName(const std::string& dbname, uint64_t number);
+    
+    namespace log {
+    }
+    
+      virtual bool Valid() const { return iter_.Valid(); }
+  virtual void Seek(const Slice& k) { iter_.Seek(EncodeKey(&tmp_, k)); }
+  virtual void SeekToFirst() { iter_.SeekToFirst(); }
+  virtual void SeekToLast() { iter_.SeekToLast(); }
+  virtual void Next() { iter_.Next(); }
+  virtual void Prev() { iter_.Prev(); }
+  virtual Slice key() const { return GetLengthPrefixedSlice(iter_.key()); }
+  virtual Slice value() const {
+    Slice key_slice = GetLengthPrefixedSlice(iter_.key());
+    return GetLengthPrefixedSlice(key_slice.data() + key_slice.size());
+  }
