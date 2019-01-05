d941f065e407481b9776f1f1707f5beaee860e7e
SUNsung <weatherius@gmail.com>
2019-01-05 23:19:29 +0200

    Rewrite [22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb] File in: 2019-01-05 23:19:29

--- ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
+++ ruby/22q3ia4dckJ5M22Ald5.FQNAfk22VYx.b0drFM22HIl.rb
@@ -1,77 +1,65 @@
 
         
-          end
-    
-        it 'supports a limit per_page parameter' do
-      2.times { FactoryGirl.create(:notification, :recipient => alice, :target => @post) }
-      get :index, params: {per_page: 2}
-      expect(assigns[:notifications].count).to eq(2)
+            def as_json
+      @pages
     end
     
-      Rcov::RcovTask.new do |t|
-    t.test_files = FileList[scope('test/**/*_test.rb')]
-    t.rcov_opts << '-x' << ''^\/''
-    if ENV['NON_NATIVE']
-      t.rcov_opts << '--no-rcovrt'
+            # Remove examples
+        css('.runnable-example').each do |node|
+          node.parent.remove
+        end
+    
+            css('.toplang', '#quickview', '.top').remove
+    
+            if is_navigational_format?
+          session.delete(session_key)
+        else
+          session[session_key]
+        end
+      end
+    
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
     end
-    t.verbose = true
+    
+          # Update password saving the record and clearing token. Returns true if
+      # the passwords are valid and the record was saved, false otherwise.
+      def reset_password(new_password, new_password_confirmation)
+        if new_password.present?
+          self.password = new_password
+          self.password_confirmation = new_password_confirmation
+          save
+        else
+          errors.add(:password, :blank)
+          false
+        end
+      end
+    
+          def remember_me!
+        self.remember_token ||= self.class.remember_token if respond_to?(:remember_token)
+        self.remember_created_at ||= Time.now.utc
+        save(validate: false) if self.changed?
+      end
+    
+    if $0 == __FILE__
+  begin
+    LogStash::PluginManager::Main.run('bin/logstash-plugin', ARGV)
+  rescue LogStash::PluginManager::Error => e
+    $stderr.puts(e.message)
+    exit(1)
   end
-rescue LoadError; end
+end
+
     
-        # variable
-    # Script::Value
-    inherited_hash_reader :var
-    
-          if @options[:update]
-        Sass::Plugin.update_stylesheets(files)
-        exit 1 if had_error
-        return
+        def validate_plugins!
+      @plugins_to_package.each do |plugin_name|
+        if INVALID_PLUGINS_TO_EXPLICIT_PACK.any? { |invalid_name| plugin_name =~ invalid_name }
+          raise UnpackablePluginError, 'Cannot explicitly pack `#{plugin_name}` for offline installation'
+        end
       end
+    end
     
-          # Find a Sass file, if it exists.
-      #
-      # This is the primary entry point of the Importer.
-      # It corresponds directly to an `@import` statement in Sass.
-      # It should do three basic things:
-      #
-      # * Determine if the URI is in this importer's format.
-      #   If not, return nil.
-      # * Determine if the file indicated by the URI actually exists and is readable.
-      #   If not, return nil.
-      # * Read the file and place the contents in a {Sass::Engine}.
-      #   Return that engine.
-      #
-      # If this importer's format allows for file extensions,
-      # it should treat them the same way as the default {Filesystem} importer.
-      # If the URI explicitly has a `.sass` or `.scss` filename,
-      # the importer should look for that exact file
-      # and import it as the syntax indicated.
-      # If it doesn't exist, the importer should return nil.
-      #
-      # If the URI doesn't have either of these extensions,
-      # the importer should look for files with the extensions.
-      # If no such files exist, it should return nil.
-      #
-      # The {Sass::Engine} to be returned should be passed `options`,
-      # with a few modifications. `:syntax` should be set appropriately,
-      # `:filename` should be set to `uri`,
-      # and `:importer` should be set to this importer.
-      #
-      # @param uri [String] The URI to import.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` that's currently being resolved.
-      #   This is safe for subclasses to modify destructively.
-      #   Callers should only pass in a value they don't mind being destructively modified.
-      # @return [Sass::Engine, nil] An Engine containing the imported file,
-      #   or nil if it couldn't be found or was in the wrong format.
-      def find(uri, options)
-        Sass::Util.abstract(self)
-      end
-    
-          # @see Base#directories_to_watch
-      def directories_to_watch
-        [root]
-      end
-    
-    module Sidekiq
-  module Logging
+            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
