7bf8db7994c85cbde61bed5d75451ca1b0eefad3
SUNsung <weatherius@gmail.com>
2019-01-05 14:47:51 +0200

    Rewrite [46N1k270saVCE463gRr8GwxSOk467PfB4MHlBEg46wlE.cpp] File in: 2019-01-05 14:47:50

--- cccccc/46N1k270saVCE463gRr8GwxSOk467PfB4MHlBEg46wlE.cpp
+++ cccccc/46N1k270saVCE463gRr8GwxSOk467PfB4MHlBEg46wlE.cpp
@@ -1,223 +1,219 @@
 
         
-        struct cvhalFilter2D;
+        #ifndef ATOM_APP_COMMAND_LINE_ARGS_H_
+#define ATOM_APP_COMMAND_LINE_ARGS_H_
     
-    #include 'common.hpp'
-#include 'vtransform.hpp'
+    #include <map>
     
-        //last line
-    if(i < size.height)
-    {
-        //vertical convolution
-        ptrdiff_t idx_rm1 = internal::borderInterpolate(i - 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
-        ptrdiff_t idx_rp1 = internal::borderInterpolate(i + 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+    #endif  // ATOM_BROWSER_API_ATOM_API_AUTO_UPDATER_H_
+
+    
+    namespace atom {
     }
     
-            map = (u8*)(mag_buf[2] + mapstep*cn);
-        memset(map, 1, mapstep);
-        memset(map + mapstep*(size.height + 1), 1, mapstep);
-    }
-    inline void firstRow(const Size2D &size, s32 cn,
-                         const u8 *, ptrdiff_t,
-                         s16* dxBase, ptrdiff_t dxStride,
-                         s16* dyBase, ptrdiff_t dyStride,
-                         s32** mag_buf)
-    {
-        s32* _norm = mag_buf[1] + 1;
+    #endif  // ATOM_BROWSER_API_ATOM_API_NET_H_
+
     
-    ptrdiff_t borderInterpolate(ptrdiff_t _p, size_t _len, BORDER_MODE borderType, size_t startMargin, size_t endMargin)
+    // static
+v8::Local<v8::Value> Screen::Create(v8::Isolate* isolate) {
+  if (!Browser::Get()->is_ready()) {
+    isolate->ThrowException(v8::Exception::Error(mate::StringToV8(
+        isolate, 'Cannot require \'screen\' module before app is ready')));
+    return v8::Null(isolate);
+  }
+    }
+    
+    #include 'content/public/browser/web_contents_observer.h'
+#include 'native_mate/handle.h'
+#include 'native_mate/wrappable.h'
+    
+    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
+ public:
+  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
+  ~AtomJavaScriptDialogManager() override;
+    }
+    
+    #include 'content/public/browser/quota_permission_context.h'
+#include 'content/public/common/storage_quota_params.h'
+    
+    
+void b3PgsJacobiSolver::setupContactConstraint(b3RigidBodyData* bodies, b3InertiaData* inertias,b3SolverConstraint& solverConstraint, 
+																 int solverBodyIdA, int solverBodyIdB,
+																 b3ContactPoint& cp, const b3ContactSolverInfo& infoGlobal,
+																 b3Vector3& vel, b3Scalar& rel_vel, b3Scalar& relaxation,
+																 b3Vector3& rel_pos1, b3Vector3& rel_pos2)
 {
-    ptrdiff_t p = _p + (ptrdiff_t)startMargin;
-    size_t len = _len + startMargin + endMargin;
-    if( (size_t)p < len )
-        return _p;
-    else if( borderType == BORDER_MODE_REPLICATE )
-        p = p < 0 ? 0 : (ptrdiff_t)len - 1;
-    else if( borderType == BORDER_MODE_REFLECT || borderType == BORDER_MODE_REFLECT101 )
-    {
-        s32 delta = borderType == BORDER_MODE_REFLECT101;
-        if( len == 1 )
-            return 0;
-        do
-        {
-            if( p < 0 )
-                p = -p - 1 + delta;
-            else
-                p = (ptrdiff_t)len - 1 - (p - (ptrdiff_t)len) - delta;
-        }
-        while( (size_t)p >= len );
+			
+			const b3Vector3& pos1 = cp.getPositionWorldOnA();
+			const b3Vector3& pos2 = cp.getPositionWorldOnB();
     }
-    else if( borderType == BORDER_MODE_WRAP )
+    
+    
+    // set right hand side
+	b3Scalar currERP = (m_flags & B3_P2P_FLAGS_ERP) ? m_erp : info->erp;
+    b3Scalar k = info->fps * currERP;
+    int j;
+	for (j=0; j<3; j++)
     {
-        if( p < 0 )
-            p -= ((p-(ptrdiff_t)len+1)/(ptrdiff_t)len)*(ptrdiff_t)len;
-        if( p >= (ptrdiff_t)len )
-            p %= (ptrdiff_t)len;
+        info->m_constraintError[j*info->rowskip] = k * (a2[j] + body1_trans.getOrigin()[j] - a1[j] - body0_trans.getOrigin()[j]);
+		//printf('info->m_constraintError[%d]=%f\n',j,info->m_constraintError[j]);
     }
-    else if( borderType == BORDER_MODE_CONSTANT )
-        p = -1;
-    else
-        internal::assertSupportedConfiguration(false);
-    return p - (ptrdiff_t)startMargin;
+	if(m_flags & B3_P2P_FLAGS_CFM)
+	{
+		for (j=0; j<3; j++)
+		{
+			info->cfm[j*info->rowskip] = m_cfm;
+		}
+	}
+    
+    bool Pack::checkPlatform() const {
+  return checkPlatform(platform_);
 }
     
-    
-    {    minLocCount >>= 1;
-    maxLocCount >>= 1;
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)minVal;
-    (void)minLocPtr;
-    (void)minLocCount;
-    (void)minLocCapacity;
-    (void)maxVal;
-    (void)maxLocPtr;
-    (void)maxLocCount;
-    (void)maxLocCapacity;
-#endif
-}
-    
-    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
-inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
-inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
-    
-    /*
- * If Trace::hhbbc_time >= 1, print some stats about the program to a
- * temporary file.  If it's greater than or equal to 2, also dump it
- * to stdout.
- */
-void print_stats(const Index&, const php::Program&);
-    
-    void Assembler::li64 (const Reg64& rt, int64_t imm64, bool fixedSize) {
-  // li64 always emits 5 instructions i.e. 20 bytes of instructions.
-  // Assumes that 0 bytes will be missing in the end.
-  uint8_t missing = 0;
+        if (!s.ok()) {
+      LOG(WARNING) << 'Could not write to database';
+      continue;
     }
     
-    namespace HPHP {
-    }
     
-    std::string Config::IniName(const std::string& config,
-                            bool prepend_hhvm /* = true */) {
-  std::string out = '';
-  if (prepend_hhvm) {
-    out += 'hhvm.';
+    {} // namespace osquery
+
+    
+    
+    {
+    {
+    {      // Force registry to use ephemeral database plugin
+      FLAGS_disable_database = true;
+      DatabasePlugin::setAllowOpen(true);
+      DatabasePlugin::initPlugin();
+    }
   }
-  size_t idx = 0;
-  for (auto& c : config) {
-    // This is the first or last character
-    if (idx == 0 || idx == config.length() - 1) {
-      out += tolower(c);
-    } else if (!isalpha(c)) {
-      // Any . or _ or numeral is just output with no special behavior
-      out += c;
-    } else {
-      if (isupper(c) && isupper(config[idx - 1 ]) && islower(config[idx + 1])) {
-      // Handle something like 'SSLPort', and c = 'P', which will then put
-      // the underscore between the 'L' and 'P'
-        out += '_';
-        out += tolower(c);
-      } else if (islower(c) && isupper(config[idx + 1])) {
-      // Handle something like 'PathDebug', and c = 'h', which will then put
-      // the underscore between the 'h' and 'D'
-        out += tolower(c);
-        out += '_';
-      } else {
-      // Otherwise we just output as lower
-        out += tolower(c);
-      }
-    }
-    idx++;
-  }
-    }
-    
-        if (!semi) {
-      // only media type (type/subtype,data)
-      ssize_t media_len = comma - data;
-      meta_len -= media_len;
-      data += media_len;
-    } else if (slash && slash < semi) {
-      // media type + param (type/subtype;param,data)
-      ssize_t media_len = semi - data;
-      meta_len -= media_len;
-      data += media_len;
-    } else {
-      // no media type (;base64,data)
-      if (semi != data // ex. foo;base64,data
-          || meta_len != sizeof(';base64') - 1 // ex. ;something,data
-          || memcmp(data, ';base64',
-                    sizeof(';base64') - 1)) { // ex. ;base65,data
-          raise_warning('rfc2397: invalid meta data');
-          return nullptr;
-        }
-    }
-    
-        UCollationResult result;
-    if(settings->dontCheckFCD()) {
-        UIterCollationIterator leftIter(data, numeric, left);
-        UIterCollationIterator rightIter(data, numeric, right);
-        result = CollationCompare::compareUpToQuaternary(leftIter, rightIter, *settings, errorCode);
-    } else {
-        FCDUIterCollationIterator leftIter(data, numeric, left, equalPrefixLength);
-        FCDUIterCollationIterator rightIter(data, numeric, right, equalPrefixLength);
-        result = CollationCompare::compareUpToQuaternary(leftIter, rightIter, *settings, errorCode);
-    }
-    if(result != UCOL_EQUAL || settings->getStrength() < UCOL_IDENTICAL || U_FAILURE(errorCode)) {
-        return result;
-    }
-    
-    #include 'unicode/utypes.h'
-#include 'sharedobject.h'
-    
-    U_NAMESPACE_BEGIN
-    
-    #include 'gregoimp.h'
-#include 'umutex.h'
-    
-    /**
- * UnicodeFunctor API.  Cast 'this' to a UnicodeMatcher* pointer
- * and return the pointer.
- */
-UnicodeMatcher* StringMatcher::toMatcher() const {
-  StringMatcher  *nonconst_this = const_cast<StringMatcher *>(this);
-  UnicodeMatcher *nonconst_base = static_cast<UnicodeMatcher *>(nonconst_this);
-  
-  return nonconst_base;
-}
-    
-    
-    {        // Delete the old text (the key)
-        text.handleReplaceBetween(start + outLen, limit + outLen, UnicodeString());
-    }        
-    
-    
-    {    static BOOST_FORCEINLINE storage_type fetch_xor(storage_type volatile& storage, storage_type v, memory_order order) BOOST_NOEXCEPT
-    {
-        base_type::fence_before(order);
-        int backup;
-        __asm
-        {
-            mov backup, ebx
-            xor edx, edx
-            mov edi, storage
-            movzx ebx, v
-            movzx eax, word ptr [edi]
-            align 16
-        again:
-            mov dx, ax
-            xor dx, bx
-            lock cmpxchg word ptr [edi], dx
-            jne again
-            mov v, ax
-            mov ebx, backup
-        };
-        base_type::fence_after(order);
-        return v;
-    }
 };
     
-    #include <boost/atomic/detail/config.hpp>
+        // We should have a property tree of pack content mimicking embedded
+    // configuration packs, ready to parse as a string.
+    std::ostringstream output;
+    pt::write_json(output, multi_pack, false);
+    pack = output.str();
+    if (pack.empty()) {
+      return Status(1, 'Multi-pack content empty');
+    }
     
-    #elif !defined(BOOST_NO_ALIGNMENT)
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    // iOS, Android and Emscripten can use GL ES 3
+// Call ImGui_ImplOpenGL3_Init() with '#version 300 es'
+#if (defined(__APPLE__) && TARGET_OS_IOS) || (defined(__ANDROID__)) || (defined(__EMSCRIPTEN__))
+#define USE_GL_ES3
+#endif
+    
+    // **DO NOT USE THIS CODE IF YOUR CODE/ENGINE IS USING MODERN OPENGL (SHADERS, VBO, VAO, etc.)**
+// **Prefer using the code in the example_glfw_opengl2/ folder**
+// See imgui_impl_glfw.cpp for details.
+    
+        // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them. 
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple. 
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
+    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+    
+    TEST(ByteTest, SetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  value.set_value(0x06, 3, 3);
+  EXPECT_EQ(0x32, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 0, 8);
+  EXPECT_EQ(0x06, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 0, 10);
+  EXPECT_EQ(0x06, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x06, 1, 7);
+  EXPECT_EQ(0x0C, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x07, 1, 1);
+  EXPECT_EQ(0x1A, value.get_byte());
+  value.set_value(0x1A);
+  value.set_value(0x07, -1, 1);
+  EXPECT_EQ(0x1A, value.get_byte());
+}
+    
+      Byte t1(bytes + 3);
+  uint32_t t = t0.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
+    
+    using apollo::drivers::canbus::Byte;
+    
+    
+    {  int ret = x;
+  return ret;
+}
+    
+    
+    {  int ret = x;
+  return ret;
+}
+    
+    namespace apollo {
+namespace hdmap {
+DEFINE_string(test_map_file,
+              'modules/map/data/sunnyvale_loop/base_map_test.bin',
+              'The test map file');
+DEFINE_string(
+    test_routing_file,
+    'modules/map/pnc_map/testdata/sample_sunnyvale_loop_routing.pb.txt',
+    'The test map file');
+    }
+    }
+    
+        auto it_lower = std::lower_bound(
+        speed_limit_.speed_limit_points().begin(),
+        speed_limit_.speed_limit_points().end(), s,
+        [](const std::pair<double, double>& point, const double curr_s) {
+          return point.first < curr_s;
+        });
+    
+    
+    {
+    {  } else {
+    for (uint32_t r = 3; r < num_params; ++r) {
+      for (uint32_t c = 3; c < num_params; ++c) {
+        (*term_matrix)(r, c) = x_pow[r + c - 5];
+      }
+    }
+    (*term_matrix).block(0, 0, num_params, 3) =
+        Eigen::MatrixXd::Zero(num_params, 3);
+    (*term_matrix).block(0, 0, 3, num_params) =
+        Eigen::MatrixXd::Zero(3, num_params);
+  }
+}
+    
+    const std::string& NodeWithRange::LaneId() const {
+  return topo_node_->LaneId();
+}
+    
+    GemMessageManager::GemMessageManager() {
+  // Control Messages
+  AddSendProtocolData<Accelcmd67, true>();
+  AddSendProtocolData<Brakecmd6b, true>();
+  AddSendProtocolData<Globalcmd69, true>();
+  AddSendProtocolData<Headlightcmd76, true>();
+  AddSendProtocolData<Horncmd78, true>();
+  AddSendProtocolData<Shiftcmd65, true>();
+  AddSendProtocolData<Steeringcmd6d, true>();
+  AddSendProtocolData<Turncmd63, true>();
+  AddSendProtocolData<Wipercmd90, true>();
+    }
