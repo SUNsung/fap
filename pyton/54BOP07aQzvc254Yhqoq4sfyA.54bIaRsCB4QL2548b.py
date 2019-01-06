7e624e984ef64221069acf810eed6339bc4ad3ca
SUNsung <weatherius@gmail.com>
2019-01-06 17:01:07 +0200

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2019-01-06 17:01:06

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,70 +1,91 @@
 
         
-        Data comes from a random square matrix.
+                # If a match was found in known_face_encodings, just use the first one.
+        if True in matches:
+            first_match_index = matches.index(True)
+            name = known_face_names[first_match_index]
     
-        if dataset_name == 'SA':
-        lb = LabelBinarizer()
-        x1 = lb.fit_transform(X[:, 1].astype(str))
-        x2 = lb.fit_transform(X[:, 2].astype(str))
-        x3 = lb.fit_transform(X[:, 3].astype(str))
-        X = np.c_[X[:, :1], x1, x2, x3, X[:, 4:]]
-        y = (y != b'normal.').astype(int)
+    # Get a reference to webcam #0 (the default one)
+video_capture = cv2.VideoCapture(0)
     
-        max_it = len(samples_range) * len(features_range)
-    for i_s, n_samples in enumerate(samples_range):
-        for i_f, n_features in enumerate(features_range):
-            it += 1
-            n_informative = n_features / 10
-            print('====================')
-            print('Iteration %03d of %03d' % (it, max_it))
-            print('====================')
-            # dataset_kwargs = {
-            #     'n_train_samples': n_samples,
-            #     'n_test_samples': 2,
-            #     'n_features': n_features,
-            #     'n_informative': n_informative,
-            #     'effective_rank': min(n_samples, n_features) / 10,
-            #     #'effective_rank': None,
-            #     'bias': 0.0,
-            # }
-            dataset_kwargs = {
-                'n_samples': 1,
-                'n_components': n_features,
-                'n_features': n_samples,
-                'n_nonzero_coefs': n_informative,
-                'random_state': 0
-            }
-            print('n_samples: %d' % n_samples)
-            print('n_features: %d' % n_features)
-            y, X, _ = make_sparse_coded_signal(**dataset_kwargs)
-            X = np.asfortranarray(X)
-    
-    # add noise
-y += 0.01 * np.random.normal((n_samples,))
-    
-        if not os.path.exists(ARCHIVE_NAME):
-        print('Downloading dataset from %s (3 MB)' % URL)
-        opener = urlopen(URL)
-        with open(ARCHIVE_NAME, 'wb') as archive:
-            archive.write(opener.read())
-    
-    current_path = os.path.dirname(os.path.abspath(__file__))
-root_path = os.path.abspath( os.path.join(current_path, os.pardir, os.pardir))
-data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data'))
-module_data_path = os.path.join(data_path, 'gae_proxy')
-python_path = os.path.abspath( os.path.join(root_path, 'python27', '1.0'))
-    
-            elif isinstance(e, MismatchedNotSetException):
-            msg = 'mismatched input ' \
-                  + self.getTokenErrorDisplay(e.token) \
-                  + ' expecting set ' \
-                  + repr(e.expecting)
+            self.assertEqual(
+            set(face_landmarks[0].keys()),
+            set(['chin', 'left_eyebrow', 'right_eyebrow', 'nose_bridge',
+                 'nose_tip', 'left_eye', 'right_eye', 'top_lip',
+                 'bottom_lip']))
+        self.assertEqual(
+            face_landmarks[0]['chin'],
+            [(369, 220), (372, 254), (378, 289), (384, 322), (395, 353),
+             (414, 382), (437, 407), (464, 424), (495, 428), (527, 420),
+             (552, 399), (576, 372), (594, 344), (604, 314), (610, 282),
+             (613, 250), (615, 219)])
     
     
-    def getText(self):
-        return self.text
+if __name__ == '__main__':
     
-        if (has_actual_code and not has_question_marks):
-        linebuffer = clean_trailing_newlines(linebuffer)
-        write_with_harness(codefile, sourcefile, start_linenum, linebuffer)
-    return (line, linenum)
+    *References:
+http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+https://fkromer.github.io/python-pattern-references/design/#factory-method
+https://sourcemaking.com/design_patterns/factory_method
+    
+    
+if __name__ == '__main__':
+    template_function(get_text, to_save=True)
+    print('-' * 30)
+    template_function(get_pdf, converter=convert_to_text)
+    print('-' * 30)
+    template_function(get_csv, to_save=True)
+    
+    from __future__ import print_function
+    
+    *TL;DR80
+Traverses a container and accesses the container's elements.
+'''
+    
+        def update(self):
+        print('Updating the test results in Database')
+        time.sleep(0.1)
+    
+        print(u'Setting Data 1 = 10')
+    data1.data = 10
+    print(u'Setting Data 2 = 15')
+    data2.data = 15
+    print(u'Setting Data 1 = 3')
+    data1.data = 3
+    print(u'Setting Data 2 = 5')
+    data2.data = 5
+    print(u'Detach HexViewer from data1 and data2.')
+    data1.detach(view2)
+    data2.detach(view2)
+    print(u'Setting Data 1 = 10')
+    data1.data = 10
+    print(u'Setting Data 2 = 15')
+    data2.data = 15
+    
+        for action in actions:
+        action()
+    
+    '''
+*What is this pattern about?
+This pattern aims to encapsulate each algorithm and allow them to be
+interchangeable. Separating algorithms allows the client to scale
+with larger and more complex algorithms, since the client and the
+strategies are kept independent of each other.
+    
+    
+class Visitor(object):
+    def visit(self, node, *args, **kwargs):
+        meth = None
+        for cls in node.__class__.__mro__:
+            meth_name = 'visit_' + cls.__name__
+            meth = getattr(self, meth_name, None)
+            if meth:
+                break
+    
+        sample_queue.put('yam')
+    with ObjectPool(sample_queue) as obj:
+        print('Inside with: {}'.format(obj))
+    print('Outside with: {}'.format(sample_queue.get()))
+    
+        def __init__(self):
+        self.time_provider = datetime.datetime
