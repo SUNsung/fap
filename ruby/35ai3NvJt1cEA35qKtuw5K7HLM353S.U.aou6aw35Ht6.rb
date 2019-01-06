4fb1502274e94f2c654da7163947bed0b7abad66
SUNsung <weatherius@gmail.com>
2019-01-06 23:29:20 +0200

    Rewrite [35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb] File in: 2019-01-06 23:29:20

--- ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
+++ ruby/35ai3NvJt1cEA35qKtuw5K7HLM353S.U.aou6aw35Ht6.rb
@@ -1,190 +1,88 @@
 
         
-                sql = <<-SQL
-          UPDATE ci_stages SET status = (#{status_sql})
-            WHERE ci_stages.status IS NULL
-            AND ci_stages.id BETWEEN #{start_id.to_i} AND #{stop_id.to_i}
-        SQL
+        begin
+  require 'cucumber/rake/task'
+  Cucumber::Rake::Task.new(:features) do |t|
+    t.profile = 'travis'
+  end
+  Cucumber::Rake::Task.new(:'features:html', 'Run Cucumber features and produce HTML output') do |t|
+    t.profile = 'html_report'
+  end
+rescue LoadError
+  desc 'Cucumber rake task not available'
+  task :features do
+    abort 'Cucumber rake task is not available. Be sure to install cucumber as a gem or plugin'
+  end
+end
     
-            def preload_stages_warnings
-          # This preloads the number of warnings for every stage, ensuring
-          # that Ci::Stage#has_warnings? doesn't execute any additional
-          # queries.
-          @pipeline.stages.each { |stage| stage.number_of_warnings }
-        end
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+        def no_subcommand(args)
+      unless args.empty? ||
+          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
+        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
+                          Run `jekyll help` to find out more.'
+        abort
       end
     end
+    
+        puts 'Your system is ready to brew.' unless Homebrew.failed?
   end
 end
 
     
-    module Gitlab
-  module GithubImport
-    module Importer
-      class IssuesImporter
-        include ParallelScheduling
+            def collection_method
+          :pull_requests_comments
+        end
     
             def importer_class
           LfsObjectImporter
         end
     
-            def id_for_already_imported_cache(note)
-          note.id
-        end
-      end
-    end
-  end
-end
-
+              unless matches
+            raise(
+              ArgumentError,
+              'The note URL #{note.html_url.inspect} is not supported'
+            )
+          end
     
     module Gitlab
   module GithubImport
-    # IssuableFinder can be used for caching and retrieving database IDs for
-    # issuable objects such as issues and pull requests. By caching these IDs we
-    # remove the need for running a lot of database queries when importing
-    # GitHub projects.
-    class IssuableFinder
-      attr_reader :project, :object
+    module Representation
+      class Issue
+        include ToHash
+        include ExposeAttribute
     
-            def labels?
-          label_names && label_names.any?
-        end
+            attr_reader :attributes
     
-            # attributes - A Hash containing the raw lfs_object details. The keys of this
-        #              Hash must be Symbols.
+            # attributes - A Hash containing the raw PR details. The keys of this
+        #              Hash (and any nested hashes) must be symbols.
         def initialize(attributes)
           @attributes = attributes
         end
+    
+            '#{route.request_method} #{route.path}' if route
       end
     end
   end
 end
 
     
-    module Gitlab
-  module QueryLimiting
-    # Middleware for reporting (or raising) when a request performs more than a
-    # certain amount of database queries.
-    class Middleware
-      CONTROLLER_KEY = 'action_controller.instance'.freeze
-      ENDPOINT_KEY = 'api.endpoint'.freeze
-    
-          @form         = Form::StatusBatch.new(form_status_batch_params.merge(current_account: current_account, action: action_from_button))
-      flash[:alert] = I18n.t('admin.statuses.failed_to_execute') unless @form.save
-    
-      def update
-    if verify_payload?
-      process_salmon
-      head 202
-    elsif payload.present?
-      render plain: signature_verification_failure_reason, status: 401
-    else
-      head 400
-    end
+      it 'creates a public method in script binding' do
+    eval @code, script_binding
+    Object.should have_method :boom
   end
     
-      def show
-    if subscription.valid?(params['hub.topic'])
-      @account.update(subscription_expires_at: future_expires)
-      render plain: encoded_challenge, status: 200
-    else
-      head 404
-    end
-  end
-    
-        data.deep_merge!(data_params) if params[:data]
-    
-      # log-levels from the diaspora.yml for SQL and federation debug-logging
-  Logging.logger[ActionView::Base].level = Rails.env.development? ? :debug : :warn
-  Logging.logger[ActiveRecord::Base].level = AppConfig.environment.logging.debug.sql? ? :debug : :info
-  Logging.logger[DiasporaFederation::Salmon::MagicEnvelope].level =
-    AppConfig.environment.logging.debug.federation? ? :debug : :info
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
-    end
-  end
-end
-
-    
-    When /^I (?:like|unlike) the post '([^']*)' in the stream$/ do |post_text|
-  like_stream_post(post_text)
-end
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
-  end
-end
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
-    end
-  end
-end
-
-    
-        it 'returns a 401 for a private post when logged out' do
-      bob.like!(@message)
-      sign_out :user
-      get :index, params: {post_id: @message.id}, format: :json
-      expect(response.status).to eq(401)
-    end
-  end
-    
-      describe '#update' do
-    it 'marks a notification as read if it gets no other information' do
-      note = FactoryGirl.create(:notification)
-      expect(Notification).to receive(:where).and_return([note])
-      expect(note).to receive(:set_read_state).with(true)
-      get :update, params: {id: note.id}, format: :json
-    end
-    
-    def get_stdin(message)
-  print message
-  STDIN.gets.chomp
-end
-    
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
-      end
-      super
-    end
-    
-        def render(context)
-      output = super
-      types = {
-        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
-        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
-        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
-      }
-      if @videos.size > 0
-        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
-        @videos.each do |v|
-          video << '<source src='#{v}' #{types[File.extname(v)]}>'
-        end
-        video += '</video>'
-      else
-        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
-      end
-    end
-    
-    class PaperclipGenerator < ActiveRecord::Generators::Base
-  desc 'Create a migration to add paperclip-specific fields to your model. ' +
-       'The NAME argument is the name of your model, and the following ' +
-       'arguments are the name of the attachments'
+    # The project root directory
+$root = ::File.dirname(__FILE__)
