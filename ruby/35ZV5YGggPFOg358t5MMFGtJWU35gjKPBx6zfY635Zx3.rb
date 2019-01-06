b9f782f04248a0c0035aee52fc2c87c20d4f62f7
SUNsung <weatherius@gmail.com>
2019-01-06 21:45:00 +0200

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2019-01-06 21:44:59

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,90 +1,121 @@
 
         
-        __END__
+        FORWARD_SLASH = '/'.freeze
     
-    module Sidekiq
-  module Logging
+    Benchmark.ips do |x|
+  x.report('no body include?') { CONTENT_NOT_CONTAINING.include?('<body') }
+  x.report('no body regexp')   { CONTENT_NOT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
     
-      module Queues
-    ##
-    # The Queues class is only for testing the fake queue implementation.
-    # There are 2 data structures involved in tandem. This is due to the
-    # Rspec syntax of change(QueueWorker.jobs, :size). It keeps a reference
-    # to the array. Because the array was dervied from a filter of the total
-    # jobs enqueued, it appeared as though the array didn't change.
-    #
-    # To solve this, we'll keep 2 hashes containing the jobs. One with keys based
-    # on the queue, and another with keys of the worker names, so the array for
-    # QueueWorker.jobs is a straight reference to a real array.
-    #
-    # Queue-based hash:
-    #
-    # {
-    #   'default'=>[
-    #     {
-    #       'class'=>'TestTesting::QueueWorker',
-    #       'args'=>[1, 2],
-    #       'retry'=>true,
-    #       'queue'=>'default',
-    #       'jid'=>'abc5b065c5c4b27fc1102833',
-    #       'created_at'=>1447445554.419934
-    #     }
-    #   ]
-    # }
-    #
-    # Worker-based hash:
-    #
-    # {
-    #   'TestTesting::QueueWorker'=>[
-    #     {
-    #       'class'=>'TestTesting::QueueWorker',
-    #       'args'=>[1, 2],
-    #       'retry'=>true,
-    #       'queue'=>'default',
-    #       'jid'=>'abc5b065c5c4b27fc1102833',
-    #       'created_at'=>1447445554.419934
-    #     }
-    #   ]
-    # }
-    #
-    # Example:
-    #
-    #   require 'sidekiq/testing'
-    #
-    #   assert_equal 0, Sidekiq::Queues['default'].size
-    #   HardWorker.perform_async(:something)
-    #   assert_equal 1, Sidekiq::Queues['default'].size
-    #   assert_equal :something, Sidekiq::Queues['default'].first['args'][0]
-    #
-    # You can also clear all workers' jobs:
-    #
-    #   assert_equal 0, Sidekiq::Queues['default'].size
-    #   HardWorker.perform_async(:something)
-    #   Sidekiq::Queues.clear_all
-    #   assert_equal 0, Sidekiq::Queues['default'].size
-    #
-    # This can be useful to make sure jobs don't linger between tests:
-    #
-    #   RSpec.configure do |config|
-    #     config.before(:each) do
-    #       Sidekiq::Queues.clear_all
-    #     end
-    #   end
-    #
-    class << self
-      def [](queue)
-        jobs_by_queue[queue]
-      end
-    
-        def match(request_method, path)
-      case matcher
-      when String
-        {} if path == matcher
-      else
-        if path_match = path.match(matcher)
-          Hash[path_match.names.map(&:to_sym).zip(path_match.captures)]
-        end
+          def print_summary(features)
+        @io.puts
+        print_stats(features, @options)
+        print_snippets(@options)
+        print_passing_wip(@options)
       end
     end
   end
 end
+    
+    def file_contents(path)
+  return Pathname.new(path).read
+end
+    
+              # WebSockets requests will have a Connection: Upgrade header
+          if parser.http_method != 'GET' || parser.upgrade?
+            super
+          elsif parser.request_url =~ %r!^\/livereload.js!
+            headers = [
+              'HTTP/1.1 200 OK',
+              'Content-Type: application/javascript',
+              'Content-Length: #{reload_size}',
+              '',
+              '',
+            ].join('\r\n')
+            send_data(headers)
+    
+    Group.user_trust_level_change!(-1, TrustLevel[4])
+    
+            if b_length > a_length
+          (b_length - a_length).times { a_split.insert(-2, 0) }
+        elsif a_length > b_length
+          (a_length - b_length).times { b_split.insert(-2, 0) }
+        end
+    
+        def ==(other)
+      other.name == name && other.path == path && other.type == type
+    end
+    
+            def store_pages(store)
+          instrument 'info.doc', msg: 'Building internal urls...'
+          with_internal_urls do
+            instrument 'info.doc', msg: 'Continuing...'
+            super
+          end
+        end
+    
+        def load_capybara_selenium
+      require 'capybara/dsl'
+      require 'selenium/webdriver'
+      Capybara.register_driver :chrome do |app|
+        options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+        Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+      end
+      Capybara.javascript_driver = :chrome
+      Capybara.current_driver = :chrome
+      Capybara.run_server = false
+      Capybara
+    end
+    
+        def initialize(*args)
+      if args.empty?
+        super(*Array.new(9))
+      elsif args.length == 1 && args.first.is_a?(Hash)
+        args.first.assert_valid_keys URI::Generic::COMPONENT
+        super(*args.first.values_at(*URI::Generic::COMPONENT))
+      else
+        super
+      end
+    end
+    
+            name = name.split(':').first
+    
+            css('.nav-index-section').each do |node|
+          node.content = node.content
+        end
+    
+      it 'accepts and uses a seed of 0' do
+    srand(0)
+    srand.should == 0
+  end
+    
+      gem.add_development_dependency 'danger'
+  gem.add_development_dependency 'mocha'
+  gem.add_development_dependency 'rspec'
+  gem.add_development_dependency 'rubocop', '0.48.1'
+end
+
+    
+      at_exit do
+    if ENV['KEEP_RUNNING']
+      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
+      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
+    else
+      vagrant_cli_command('destroy -f')
+    end
+  end
+    
+          attr_reader :key, :default, :options
+    
+    MESSAGE
+      end
+    
+          def matches?(other)
+        # This matching logic must stay in sync with `Servers#add_host`.
+        hostname == other.hostname && port == other.port
+      end
+    
+          def servers_by_key
+        @servers_by_key ||= {}
+      end
