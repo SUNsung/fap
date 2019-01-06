19d8ec8b4e709d5709767bd09355b8a72cd85cfc
SUNsung <weatherius@gmail.com>
2019-01-06 22:35:10 +0200

    Rewrite [40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp] File in: 2019-01-06 22:35:09

--- cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
+++ cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
@@ -1,78 +1,170 @@
 
         
-        
-    {  static void setInitialized6(bool f) { data6_.initialized = f; }
+        // Generate destructors.
+#include 'ipc/struct_destructor_macros.h'
+#include 'content/nw/src/common/common_message_generator.h'
+    
+    #endif  // CONTENT_NW_SRC_API_APP_APP_H_
+    
+    class Clipboard : public Base {
+ public:
+  Clipboard(int id,
+            const base::WeakPtr<DispatcherHost>& dispatcher_host,
+            const base::DictionaryValue& option);
+  ~Clipboard() override;
+    }
+    
+    
+    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  return !item->icon_.IsEmpty();
+}
+    
+    
+    {#endif
+}  // namespace nwapi
+    
+    NwObjCallObjectMethodSyncFunction::NwObjCallObjectMethodSyncFunction() {
+}
+    
+        base::LazyInstance<NwDesktopCaptureMonitor>::Leaky
+      g_desktop_capture_monitor = LAZY_INSTANCE_INITIALIZER;
+    
+    template <$for j, [[typename Generator$j]]>
+internal::CartesianProductHolder$i<$for j, [[Generator$j]]> Combine(
+    $for j, [[const Generator$j& g$j]]) {
+  return internal::CartesianProductHolder$i<$for j, [[Generator$j]]>(
+      $for j, [[g$j]]);
+}
+    
+    
+    {  // The name of the source file where the test part took place, or
+  // '' if the source file is unknown.
+  std::string file_name_;
+  // The line in the source file where the test part took place, or -1
+  // if the line number is unknown.
+  int line_number_;
+  std::string summary_;  // The test failure summary.
+  std::string message_;  // The test failure message.
 };
     
-    void DHTReplaceNodeTask::sendMessage()
-{
-  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
-  if (!questionableNode) {
-    setFinished(true);
-  }
-  else {
-    getMessageDispatcher()->addMessageToQueue(
-        getMessageFactory()->createPingMessage(questionableNode), timeout_,
-        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
-  }
+    
+    {  return AssertionFailure() << pred_text << '('
+                            << e1 << ', '
+                            << e2 << ', '
+                            << e3 << ', '
+                            << e4 << ') evaluates to false, where'
+                            << '\n' << e1 << ' evaluates to ' << v1
+                            << '\n' << e2 << ' evaluates to ' << v2
+                            << '\n' << e3 << ' evaluates to ' << v3
+                            << '\n' << e4 << ' evaluates to ' << v4;
 }
     
-      virtual std::unique_ptr<Dict> getResponse() = 0;
+      // Returns a copy of the FilePath with the case-insensitive extension removed.
+  // Example: FilePath('dir/file.exe').RemoveExtension('EXE') returns
+  // FilePath('dir/file'). If a case-insensitive extension is not
+  // found, returns a copy of the original FilePath.
+  FilePath RemoveExtension(const char* extension) const;
     
-    public:
-  DHTRoutingTableSerializer(int family);
-    
-    // Timer management class
-class ThreadTimer {
+    template <class Generator1, class Generator2, class Generator3,
+    class Generator4, class Generator5, class Generator6, class Generator7>
+class CartesianProductHolder7 {
  public:
-  ThreadTimer() = default;
-    }
-    
-    void Benchmark::AddRange(std::vector<int>* dst, int lo, int hi, int mult) {
-  CHECK_GE(lo, 0);
-  CHECK_GE(hi, lo);
-  CHECK_GE(mult, 2);
-    }
-    
-    
-    {// Returns true unless value starts with one of: '0', 'f', 'F', 'n' or 'N', or
-// some non-alphanumeric character. As a special case, also returns true if
-// value is the empty string.
-bool IsTruthyFlagValue(const std::string& value);
-}  // end namespace benchmark
-    
-    inline LogType& operator<<(LogType& log, EndLType* m) {
-  if (log.out_) {
-    *log.out_ << m;
+CartesianProductHolder7(const Generator1& g1, const Generator2& g2,
+    const Generator3& g3, const Generator4& g4, const Generator5& g5,
+    const Generator6& g6, const Generator7& g7)
+      : g1_(g1), g2_(g2), g3_(g3), g4_(g4), g5_(g5), g6_(g6), g7_(g7) {}
+  template <typename T1, typename T2, typename T3, typename T4, typename T5,
+      typename T6, typename T7>
+  operator ParamGenerator< ::std::tr1::tuple<T1, T2, T3, T4, T5, T6,
+      T7> >() const {
+    return ParamGenerator< ::std::tr1::tuple<T1, T2, T3, T4, T5, T6, T7> >(
+        new CartesianProductGenerator7<T1, T2, T3, T4, T5, T6, T7>(
+        static_cast<ParamGenerator<T1> >(g1_),
+        static_cast<ParamGenerator<T2> >(g2_),
+        static_cast<ParamGenerator<T3> >(g3_),
+        static_cast<ParamGenerator<T4> >(g4_),
+        static_cast<ParamGenerator<T5> >(g5_),
+        static_cast<ParamGenerator<T6> >(g6_),
+        static_cast<ParamGenerator<T7> >(g7_)));
   }
-  return log;
+    }
+    
+    
+// This sample shows how to write a simple unit test for a function,
+// using Google C++ testing framework.
+//
+// Writing a unit test using Google C++ testing framework is easy as 1-2-3:
+    
+    #include 'sample2.h'
+    
+    // A sample program demonstrating using Google C++ testing framework.
+//
+// Author: wan@google.com (Zhanyong Wan)
+    
+    TEST(CorruptionTest, RecoverWriteError) {
+  env_.writable_file_error_ = true;
+  Status s = TryReopen();
+  ASSERT_TRUE(!s.ok());
 }
     
-      const CPUInfo &info = context.cpu_info;
-  Out << 'Run on (' << info.num_cpus << ' X '
-      << (info.cycles_per_second / 1000000.0) << ' MHz CPU '
-      << ((info.num_cpus > 1) ? 's' : '') << ')\n';
-  if (info.caches.size() != 0) {
-    Out << 'CPU Caches:\n';
-    for (auto &CInfo : info.caches) {
-      Out << '  L' << CInfo.level << ' ' << CInfo.type << ' '
-          << (CInfo.size / 1000) << 'K';
-      if (CInfo.num_sharing != 0)
-        Out << ' (x' << (info.num_cpus / CInfo.num_sharing) << ')';
-      Out << '\n';
-    }
+    TEST(LogTest, ReadInitialOffsetIntoBlockPadding) {
+  CheckInitialOffsetRecord(3 * log::kBlockSize - 3, 5);
+}
+    
+    
+    {#if !defined(NDEBUG)
+    snapshot->list_ = this;
+#endif  // !defined(NDEBUG)
+    snapshot->next_ = &head_;
+    snapshot->prev_ = head_.prev_;
+    snapshot->prev_->next_ = snapshot;
+    snapshot->next_->prev_ = snapshot;
+    return snapshot;
   }
     
-    #include 'sleep.h'
+        void Trainer::Save(const std::wstring& modelFilePath, const std::vector<DictionaryValue>& learnerState, const Dictionary& externalState, const Dictionary& distributedState)
+    {
+        std::wstring tempModelFile = modelFilePath + L'.tmp';
+        Dictionary state;
+        state[versionPropertyName] = trainerCheckpointVersion;
+        state[learnersPropertyName] = learnerState;
+        state[externalStatePropertyName] = externalState;
+        state[distributedStatePropertyName] = distributedState;
+    }
     
-    #if BOOST_ATOMIC_SIGNAL_FENCE > 0
-BOOST_FORCEINLINE void atomic_signal_fence(memory_order order) BOOST_NOEXCEPT
+    // Rand based on Mersenne Twister.
+// We use our own distribution in order to match baselines between different operating systems,
+// instead of using std::shuffle which uses unitform_distribution internally.
+// TODO: Switching to Boost would eliminate this problem.
+template <typename TVector>
+inline void RandomShuffleMT(TVector& v, size_t begin, size_t end, std::mt19937_64& rng)
 {
-    detail::signal_fence(order);
+    for(size_t i = begin; i < end; ++i)
+    {
+        const size_t randomLocation = RandMT(i, end, rng);
+        std::swap(v[i], v[randomLocation]);
+    }
 }
-#else
-BOOST_FORCEINLINE void atomic_signal_fence(memory_order) BOOST_NOEXCEPT
-{
-    detail::lockpool::signal_fence();
-}
+    
+            // add to set
+        let wasAdded = AddNodeToNetIfNotYet(node, /*makeUniqueName=*/ true);
+        if (!wasAdded) // node already there (above will fail if there is a different node with the same name)
+            continue;
+    
+        virtual void /*ComputationNodeNonLooping::*/ ForwardPropNonLooping() override
+    {
+        FrameRange fr(InputRef(0).GetMBLayout());
+        InputRef(0).ValueFor(fr).VectorMax(*m_maxIndexes0, *m_maxValues, true);
+        InputRef(1).ValueFor(fr).VectorMax(*m_maxIndexes1, *m_maxValues, true, m_topK);
+        MaskMissingColumnsToZero(*m_maxIndexes0, InputRef(0).GetMBLayout(), fr);
+        MaskMissingColumnsToZero(*m_maxIndexes1, InputRef(1).GetMBLayout(), fr);
+        Value().AssignNumOfDiff(*m_maxIndexes0, *m_maxIndexes1, m_topK > 1);
+#if NANCHECK
+        Value().HasNan('ClassificationError');
 #endif
+#if DUMPOUTPUT
+        Value().Print('ClassificationErrorNode');
+#endif
+    }
