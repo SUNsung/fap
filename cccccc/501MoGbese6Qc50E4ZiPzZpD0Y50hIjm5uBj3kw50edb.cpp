442505d34a523761f4483a252796acd5f28c837f
SUNsung <weatherius@gmail.com>
2019-01-06 08:30:22 +0200

    Rewrite [501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp] File in: 2019-01-06 08:30:21

--- cccccc/501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp
+++ cccccc/501MoGbese6Qc50E4ZiPzZpD0Y50hIjm5uBj3kw50edb.cpp
@@ -1,100 +1,287 @@
 
         
-        
-    {  std::vector<OperatorDef> GetGradientDefs() override {
-    return SingleGradientDef(
-        'SubGradient',
-        '',
-        std::vector<std::string>{GO(0), I(0), I(1)},
-        std::vector<std::string>{GI(0), GI(1)});
+        namespace swig {
+namespace {
+    }
+    }
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+      CostGraphDef cost_graph_analytical;
+  PredictCosts(&analytical_estimator_, &cost_graph_analytical,
+               &total_time_analytical_);
+  VLOG(1) << 'cost_graph_analytical size: '
+          << cost_graph_analytical.node_size();
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
+                       std::function<void()> destructor, PyObject** result) {
+  int size = 1;
+  for (int i = 0; i < dim_size; ++i) {
+    size *= dims[i];
   }
+  if (dtype == DT_STRING || dtype == DT_RESOURCE || size == 0) {
+    return errors::FailedPrecondition(
+        'Cannot convert strings, resources, or empty Tensors.');
+  }
+    }
+    
+    // Destructor passed to TF_NewTensor when it reuses a numpy buffer. Stores a
+// pointer to the pyobj in a buffer to be dereferenced later when we're actually
+// holding the GIL. Data and len are ignored.
+void DelayedNumpyDecref(void* data, size_t len, void* obj);
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Event::Status CUDAEvent::PollForStatus() {
+  port::StatusOr<CUresult> status =
+      CUDADriver::QueryEvent(parent_->cuda_context(), cuda_event_);
+  if (!status.ok()) {
+    LOG(ERROR) << 'Error polling for event status: '
+               << status.status().error_message();
+    return Event::Status::kError;
+  }
+    }
+    
+    void AutoUpdater::OnUpdateNotAvailable() {
+  Emit('update-not-available');
+}
+    
+      // The window class of |window_|.
+  ATOM atom_;
+    
+    #ifndef ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
+#define ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
+    
+    ShutdownDetector::ShutdownDetector(int shutdown_fd)
+    : shutdown_fd_(shutdown_fd) {
+  CHECK_NE(shutdown_fd_, -1);
+}
+    
+    AtomQuotaPermissionContext::AtomQuotaPermissionContext() {}
+    
+    #include 'atom/browser/auto_updater.h'
+    
+      TemporaryFile() {
+    path = tmppath();
+  }
+    
+    OPERATOR_SCHEMA(EnforceFinite)
+    .NumInputs(1)
+    .NumOutputs(0)
+    .SetDoc(R'DOC(
+Raise if there is NaN or Inf values in the input tensor.
+)DOC')
+    .Input(0, 'input', 'Input tensor');
+    
+    // FreeOp frees the content of the output blob. We allow it to take in input
+// blobs purely for the reason that it can 'wait' on the input blobs to be
+// produced by some of the earlier operators before a free is called.
+template <class Context>
+class FreeOp : public Operator<Context> {
+ public:
+  FreeOp(const OperatorDef& def, Workspace* ws) : Operator<Context>(def, ws) {}
+    }
+    
+    RANGES dimensions description:
+1: represents list of examples within a batch
+2: represents list features
+3: two values which are start and length or a range (to be applied on DATA)
+    
+    ```
+    
+    OPERATOR_SCHEMA(Glu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc(R'DOC(
+Applies gated linear unit to the input Tensor X. The output Y is half the size
+of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
+[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
+X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
+)DOC')
+    .Input(0, 'X', '1D input tensor')
+    .Output(0, 'Y', '1D output tensor');
+    
+    
+    { private:
+  friend class Config;
 };
     
-    REGISTER_CPU_OPERATOR(
-    MergeSingleMapFeatureTensors,
-    MergeSingleMapFeatureTensorsOp<CPUContext>);
-OPERATOR_SCHEMA(MergeSingleMapFeatureTensors)
-    .SetDoc(
-        'Merge given single-feature tensors with map features into one '
-        'multi-feature tensor.' +
-        doc)
-    .NumInputs([](int n) { return n >= 4 && n % 4 == 0; })
-    .NumOutputs(5)
-    .Input(0, 'in1_lengths', '.lengths')
-    .Input(1, 'in1_keys', '.keys')
-    .Input(2, 'in1_values', '.values')
-    .Input(3, 'in1_presence', '.presence')
-    .Output(0, 'out_lengths', '.lengths')
-    .Output(1, 'out_keys', '.keys')
-    .Output(2, 'out_values_lengths', '.values.lengths')
-    .Output(3, 'out_values_keys', '.values.keys')
-    .Output(4, 'out_values_values', '.values.values')
-    .Arg('feature_ids', 'feature ids');
-    
-      FlexibleTopKOp(const OperatorDef& operator_def, Workspace* ws)
-      : Operator<Context>(operator_def, ws) {}
-    
-    /// Root key to retrieve Kafka topic configurations.
-const std::string kKafkaTopicParserRootKey('kafka_topics');
-    
-    REGISTER_INTERNAL(LoggerConfigParserPlugin, 'config_parser', 'logger');
-    
-    #include <osquery/core.h>
+    Status FeatureVectorsConfigParserPlugin::update(const std::string& source,
+                                                const ParserConfig& config) {
+  auto fv = config.find(kFeatureVectorsRootKey);
+  if (fv == config.end()) {
+    return Status();
+  }
+    }
     
     /**
- * @brief Thread-safe watched child process state manager.
- *
- * The Watcher instance is separated from the WatcherRunner thread to allow
- * signals and osquery-introspection to monitor the autoloaded extensions
- * and optional worker stats. A child-process change signal may indicate an
- * autoloaded extension ended. Tables may also report on the historic worker
- * or extension utilizations.
- *
- * Though not critical, it is preferred to remove the extension's broadcasted
- * routes quickly. Locking access to the extensions list between signals and
- * the WatcherRunner thread allows osquery to tearDown registry changes before
- * attempting to respawn an extension process.
+ * @brief A simple ConfigParserPlugin for feature vector dictionary keys.
  */
-class Watcher : private boost::noncopyable {
+class FeatureVectorsConfigParserPlugin : public ConfigParserPlugin {
  public:
-  /// Instance accessor
-  static Watcher& get() {
-    static Watcher instance;
-    return instance;
+  std::vector<std::string> keys() const override;
+    }
+    
+    std::vector<std::string> KafkaTopicsConfigParserPlugin::keys() const {
+  return {kKafkaTopicParserRootKey};
+}
+    
+    
+    {/// KafkaTopicsConfigParserPlugin extracts, updates, and parses Kafka topic
+/// configurations from Osquery's configurations.
+class KafkaTopicsConfigParserPlugin : public ConfigParserPlugin {
+ public:
+  std::vector<std::string> keys() const override;
+  Status update(const std::string& source, const ParserConfig& config) override;
+};
+} // namespace osquery
+
+    
+    #include <osquery/config/config.h>
+    
+    namespace osquery {
+    }
+    
+      // serialize the QueryLogItem and make sure decorations go top level
+  auto doc = JSON::newObject();
+  auto status = serializeQueryLogItem(item, doc);
+  std::string expected = 'test';
+  std::string result = doc.doc()['load_test'].GetString();
+  EXPECT_EQ(result, expected);
+    
+    
+    { private:
+  const std::string kConfigViews = 'config_views.';
+};
+    
+        // We should have a property tree of pack content mimicking embedded
+    // configuration packs, ready to parse as a string.
+    std::ostringstream output;
+    pt::write_json(output, multi_pack, false);
+    pack = output.str();
+    if (pack.empty()) {
+      return Status(1, 'Multi-pack content empty');
+    }
+    
+    Status TLSConfigPlugin::setUp() {
+  if (FLAGS_enroll_always && !FLAGS_disable_enrollment) {
+    // clear any cached node key
+    clearNodeKey();
+    auto node_key = getNodeKey('tls');
+    if (node_key.size() == 0) {
+      // Could not generate a node key, continue logging to stderr.
+      return Status(1, 'No node key, TLS config failed.');
+    }
   }
     }
     
+        /** initializes the action with the std::function<void(Node*)> */
+    bool initWithFunction(const std::function<void(Node*)>& func);
     
-    {  if (console_reporter->ReportContext(context) &&
-      (!file_reporter || file_reporter->ReportContext(context))) {
-    flushStreams(console_reporter);
-    flushStreams(file_reporter);
-    for (const auto& benchmark : benchmarks) {
-      std::vector<BenchmarkReporter::Run> reports =
-          RunBenchmark(benchmark, &complexity_reports);
-      console_reporter->ReportRuns(reports);
-      if (file_reporter) file_reporter->ReportRuns(reports);
-      flushStreams(console_reporter);
-      flushStreams(file_reporter);
+    /** initializes the action with the callback
+     
+     typedef void (Ref::*SEL_CallFuncN)(Node*);
+     @deprecated Use the std::function API instead.
+     */
+    CC_DEPRECATED_ATTRIBUTE bool initWithTarget(Ref* target, SEL_CallFuncN selector);
+    
+    http://www.cocos2d-x.org
+    
+            if ( frameNames.empty() )
+        {
+            CCLOG('cocos2d: AnimationCache: Animation '%s' found in dictionary without any frames - cannot add to animation cache.', anim.first.c_str());
+            continue;
+        }
+    
+        /** Adds a Animation with a name.
+     *
+     * @param animation An animation.
+     * @param name The name of animation.
+     */
+    void addAnimation(Animation *animation, const std::string& name);
+    
+    namespace rocksdb {
     }
+    
+      // drop column family
+  s = db->DropColumnFamily(handles[1]);
+  assert(s.ok());
+    
+    int main() {
+  DBOptions db_opt;
+  db_opt.create_if_missing = true;
+    }
+    
+      std::string ToString(bool exclude_zero_counters = false) const;
+    
+      // Wait for all threads to finish.
+  // Discard those threads that did not start
+  // executing
+  virtual void JoinAllThreads() = 0;
+    
+    
+    {  virtual ~Checkpoint() {}
+};
+    
+    template <
+    class Iterator = const char*,
+    class Base = folly::Range<boost::u8_to_u32_iterator<Iterator>>>
+class UTF8Range : public Base {
+ public:
+  /* implicit */ UTF8Range(const folly::Range<Iterator> baseRange)
+      : Base(
+            boost::u8_to_u32_iterator<Iterator>(
+                baseRange.begin(),
+                baseRange.begin(),
+                baseRange.end()),
+            boost::u8_to_u32_iterator<Iterator>(
+                baseRange.end(),
+                baseRange.begin(),
+                baseRange.end())) {}
+  /* implicit */ UTF8Range(const std::string& baseString)
+      : Base(folly::Range<Iterator>(baseString)) {}
+};
+    
+    
+    {    return newptr;
   }
-  console_reporter->Finalize();
-  if (file_reporter) file_reporter->Finalize();
-  flushStreams(console_reporter);
-  flushStreams(file_reporter);
+  void init() {
+    PackedPtr data;
+    data.init();
+    ptr_.store(data);
+  }
+    
+    template <
+    template <typename> class Atom = std::atomic,
+    typename T,
+    typename... Args>
+counted_ptr<T, Atom> make_counted(Args&&... args) {
+  char* mem = (char*)malloc(sizeof(T) + sizeof(intrusive_shared_count<Atom>));
+  if (!mem) {
+    throw std::bad_alloc();
+  }
+  new (mem) intrusive_shared_count<Atom>();
+  T* ptr = (T*)(mem + sizeof(intrusive_shared_count<Atom>));
+  new (ptr) T(std::forward<Args>(args)...);
+  return counted_ptr<T, Atom>(counted_shared_tag(), ptr);
 }
     
-    namespace benchmark {
-namespace internal {
-    }
-    }
-    
-    typedef void(AbortHandlerT)();
-    
-    std::string FormatKV(std::string const& key, std::string const& value) {
-  return StringPrintF('\'%s\': \'%s\'', key.c_str(), value.c_str());
-}
-    
-    class SCOPED_CAPABILITY MutexLock {
-  typedef std::unique_lock<std::mutex> MutexLockImp;
-    }
+    #include <folly/container/SparseByteSet.h>
