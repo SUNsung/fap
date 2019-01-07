1da6c3e58bcd372f6a09958d4c440bc5717152f5
SUNsung <weatherius@gmail.com>
2019-01-07 20:30:29 +0200

    Rewrite [49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb] File in: 2019-01-07 20:30:29

--- ruby/49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb
+++ ruby/49xPCOXHRS.kw49BINwgVftWg49EXKs4K0Fk49bny.rb
@@ -1,250 +1,64 @@
 
         
-        def global_require
-  JSON.pretty_generate(DATA)
-end
-    
-    def location(folder, direction)
-  if folder
-    before = folder if direction == 'in'
-    after  = folder if direction == 'under'
-  end
-    
-                  Jekyll.logger.info 'LiveReload address:',
-                                 'http://#{opts['host']}:#{opts['livereload_port']}'
-            end
-          end
-          @thread.abort_on_exception = true
-        end
-    
-        brew cask install mactex
-    EOS
-  when 'pip' then <<-EOS.undent
-    Homebrew provides pip via: `brew install python`. However you will then
-    have two Pythons installed on your Mac, so alternatively you can install
-    pip via the instructions at:
-    
-          # Find commands in the path
-      unless (exts = external_commands).empty?
-        puts
-        puts 'External commands'
-        puts_columns exts
-      end
-    end
-  end
-    
-      def gcc_42
-    @gcc_42 ||= MacOS.gcc_42_build_version if MacOS.has_apple_developer_tools?
-  end
-    
-    class PrettyListing
-  def initialize(path)
-    Pathname.new(path).children.sort_by { |p| p.to_s.downcase }.each do |pn|
-      case pn.basename.to_s
-      when 'bin', 'sbin'
-        pn.find { |pnn| puts pnn unless pnn.directory? }
-      when 'lib'
-        print_dir pn do |pnn|
-          # dylibs have multiple symlinks and we don't care about them
-          (pnn.extname == '.dylib' || pnn.extname == '.pc') && !pnn.symlink?
-        end
-      else
-        if pn.directory?
-          if pn.symlink?
-            puts '#{pn} -> #{pn.readlink}'
-          else
-            print_dir pn
-          end
-        elsif Metafiles.list?(pn.basename.to_s)
-          puts pn
-        end
-      end
-    end
-  end
-    
-              pipelines.each do |pipeline|
-            self.new(pipeline).tap do |preloader|
-              preloader.preload_commit_authors
-              preloader.preload_pipeline_warnings
-              preloader.preload_stages_warnings
-            end
-          end
-        end
-    
-          # Sets a cache key to the given value.
-      #
-      # key - The cache key to write.
-      # value - The value to set.
-      # timeout - The time after which the cache key should expire.
-      def self.write(raw_key, value, timeout: TIMEOUT)
-        key = cache_key_for(raw_key)
-    
-          def request_count_counter
-        @request_counter ||= Gitlab::Metrics.counter(
-          :github_importer_request_count,
-          'The number of GitHub API calls performed when importing projects'
-        )
-      end
+            def assert_index(index)
+      i = index.is_a?(Integer) ? index : @filters.index(filter_const(index))
+      raise 'No such filter to insert: #{index}' unless i
+      i
     end
   end
 end
 
     
-            def representation_class
-          Representation::Note
-        end
-    
-            # attributes - A Hash containing the raw lfs_object details. The keys of this
-        #              Hash must be Symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-      end
-    end
-  end
-end
-
-    
-            # attributes - A Hash containing the user details. The keys of this
-        #              Hash (and any nested hashes) must be symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-      end
-    end
-  end
-end
-
-    
-          def call(env)
-        transaction, retval = Transaction.run do
-          @app.call(env)
-        end
-    
-        def action_from_button
-      if params[:nsfw_on]
-        'nsfw_on'
-      elsif params[:nsfw_off]
-        'nsfw_off'
-      elsif params[:delete]
-        'delete'
-      end
+        def effective_url
+      @effective_url ||= URL.parse super
     end
     
-          weeks << {
-        week: week.to_time.to_i.to_s,
-        statuses: Redis.current.get('activity:statuses:local:#{week_id}') || '0',
-        logins: Redis.current.pfcount('activity:logins:#{week_id}').to_s,
-        registrations: Redis.current.get('activity:accounts:local:#{week_id}') || '0',
-      }
+          str.truncate(max_length).ljust(max_length) << tag.to_s
     end
     
-        define_method provider do
-      @user = User.find_for_oauth(request.env['omniauth.auth'], current_user)
-    
-      directory 'pkg/'
-  CLOBBER.include('pkg')
-    
-    get '/stream', :provides => 'text/event-stream' do
-  stream :keep_open do |out|
-    connections << out
-    out.callback { connections.delete(out) }
-  end
-end
-    
-        <% unless bad_request?(exception) %>
-      <div id='get'>
-        <h3 id='get-info'>GET</h3>
-        <% if req.GET and not req.GET.empty? %>
-          <table class='req'>
-            <tr>
-              <th>Variable</th>
-              <th>Value</th>
-            </tr>
-             <% req.GET.sort_by { |k, v| k.to_s }.each { |key, val| %>
-            <tr>
-              <td><%=h key %></td>
-              <td class='code'><div><%=h val.inspect %></div></td>
-            </tr>
-            <% } %>
-          </table>
-        <% else %>
-          <p class='no-data'>No GET data.</p>
-        <% end %>
-        <div class='clear'></div>
-      </div> <!-- /GET -->
-    
-            # Set these key values to boolean 'true' to include in policy
-        NO_ARG_DIRECTIVES.each do |d|
-          if options.key?(d) && options[d].is_a?(TrueClass)
-            directives << d.to_s.sub(/_/, '-')
+            css('.filetree .children').each do |node|
+          node.css('.file').each do |n|
+            n.content = '  #{n.content}'
           end
         end
     
-      it 'returns the sorted config parts' do
-    expect(subject.config_parts).to eq(ordered_config_parts)
+      def command
+    abort 'This command requires a command argument' if ARGV.empty?
+    
+        def log_transform(*args, from: caller[1][/`.*'/][1..-2].sub(/^block in /, ''))
+      puts '    #{cyan from}#{cyan ': #{args * ', '}' unless args.empty?}'
+    end
+    
+      # Configure static asset server for tests with Cache-Control for performance.
+  if config.respond_to?(:serve_static_files)
+    # rails >= 4.2
+    config.serve_static_files = true
+  elsif config.respond_to?(:serve_static_assets)
+    # rails < 4.2
+    config.serve_static_assets = true
+  end
+  config.static_cache_control = 'public, max-age=3600'
+    
+          sh 'git commit #{yaml_file} -m 'CocoaPods release #{gem_version}''
+      sh 'git push'
+    end
   end
     
-            def new; end
+    ; TODO make this stricter if possible
+(allow network-outbound)
     
-            def mine_includes
-          {
-            order: {
-              bill_address: {
-                state: {},
-                country: {}
-              },
-              ship_address: {
-                state: {},
-                country: {}
-              },
-              adjustments: {},
-              payments: {
-                order: {},
-                payment_method: {}
-              }
-            },
-            inventory_units: {
-              line_item: {
-                product: {},
-                variant: {}
-              },
-              variant: {
-                product: {},
-                default_price: {},
-                option_values: {
-                  option_type: {}
-                }
-              }
-            }
-          }
-        end
-      end
+    abstract_target 'Abstract Target' do
+    use_modular_headers!
+    
+    When /^(?:|I )fill in '([^']*)' with '([^']*)'$/ do |field, value|
+  fill_in(field, :with => value)
+end
+    
+        def raise_because_imagemagick_missing
+      raise Errors::CommandNotFoundError.new('Could not run the `identify` command. Please install ImageMagick.')
     end
   end
 end
 
     
-            def scope
-          if params[:country_id]
-            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
-            @country.states.accessible_by(current_ability, :read).order('name ASC')
-          else
-            State.accessible_by(current_ability, :read).order('name ASC')
-          end
-        end
-      end
-    end
-  end
-end
-
-    
-            def create
-          authorize! :create, Taxonomy
-          @taxonomy = Taxonomy.new(taxonomy_params)
-          if @taxonomy.save
-            respond_with(@taxonomy, status: 201, default_template: :show)
-          else
-            invalid_resource!(@taxonomy)
-          end
-        end
+        private
