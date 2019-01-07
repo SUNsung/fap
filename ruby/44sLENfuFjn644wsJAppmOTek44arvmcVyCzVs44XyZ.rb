ae9cdee2bc17ef77238b35331dabf04623f87b16
SUNsung <weatherius@gmail.com>
2019-01-07 17:40:38 +0200

    Rewrite [44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb] File in: 2019-01-07 17:40:38

--- ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
+++ ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
@@ -1,131 +1,99 @@
 
         
-        # Just a slash
-Benchmark.ips do |x|
-  path = '/'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
-end
+            def handle_unknown_error!(e)
+      # Some spaceship exception classes implement #preferred_error_info in order to share error info
+      # that we'd rather display instead of crashing with a stack trace. However, fastlane_core and
+      # spaceship can not know about each other's classes! To make this information passing work, we
+      # use a bit of Ruby duck-typing to check whether the unknown exception type implements the right
+      # method. If so, we'll present any returned error info in the manner of a user_error!
+      error_info = e.respond_to?(:preferred_error_info) ? e.preferred_error_info : nil
+      should_show_github_issues = e.respond_to?(:show_github_issues) ? e.show_github_issues : true
     
-    def global_require
-  JSON.pretty_generate(DATA)
-end
-    
-          def step_name(_keyword, _step_match, status, _source_indent, _background, _file_colon_line)
-        @io.print CHARS[status]
-        @io.print ' '
+          it 'handles the extension parameter correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', extension: 'rb')
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --include=\\*.rb')
       end
-      # rubocop:enable Metrics/ParameterLists
     
-            # For a description of the protocol see
-        # http://feedback.livereload.com/knowledgebase/articles/86174-livereload-protocol
-        def reload(pages)
-          pages.each do |p|
-            json_message = JSON.dump(
-              :command => 'reload',
-              :path    => p.url,
-              :liveCSS => true
-            )
-    
-            def initialize(_opts)
-          # If EventMachine SSL support on Windows ever gets better, the code below will
-          # set up the reactor to handle SSL
-          #
-          # @ssl_enabled = opts['ssl_cert'] && opts['ssl_key']
-          # if @ssl_enabled
-          #   em_opts[:tls_options] = {
-          #   :private_key_file => Jekyll.sanitized_path(opts['source'], opts['ssl_key']),
-          #   :cert_chain_file  => Jekyll.sanitized_path(opts['source'], opts['ssl_cert'])
-          #   }
-          #   em_opts[:secure] = true
-          # end
-    
-    module Admin
-  class EmailDomainBlocksController < BaseController
-    before_action :set_email_domain_block, only: [:show, :destroy]
-    
-              redirect_to admin_reports_path, notice: I18n.t('admin.reports.resolved_msg')
-          return
+            it 'adds invalid path option' do
+          path = './non/existent/path'
+          expect do
+            Fastlane::FastFile.new.parse('lane :test do
+              swiftlint(
+                path: '#{path}'
+              )
+            end').runner.execute(:test)
+          end.to raise_error(/Couldn't find path '.*#{path}'/)
         end
+      end
     
-        def create
-      authorize :status, :update?
-    
-      def setting
-    @_setting ||= ::Web::Setting.where(user: current_user).first_or_initialize(user: current_user)
-  end
+    if git.modified_files.include?('snapshot/lib/assets/SnapshotHelperXcode8.swift')
+  warn('You modified `SnapshotHelperXcode8.swift`, make sure to update the version number at the bottom of the file to notify users about the new helper file.')
 end
-
     
-        alias log puts
-    
-      # Compile a Sass or SCSS string to CSS.
-  # Defaults to SCSS.
-  #
-  # @param contents [String] The contents of the Sass file.
-  # @param options [{Symbol => Object}] An options hash;
-  #   see {file:SASS_REFERENCE.md#Options the Sass options documentation}
-  # @raise [Sass::SyntaxError] if there's an error in the document
-  # @raise [Encoding::UndefinedConversionError] if the source encoding
-  #   cannot be converted to UTF-8
-  # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
-  def self.compile(contents, options = {})
-    options[:syntax] ||= :scss
-    Engine.new(contents, options).to_css
-  end
-    
-          files.map! do |from, to|
-        to ||= from.gsub(/\.[^.]*?$/, '.css')
-        sourcemap = Sass::Util.sourcemap_name(to) if @options[:sourcemap]
-        [from, to, sourcemap]
-      end
-      dirs.map! {|from, to| [from, to || from]}
-      Sass::Plugin.options[:template_location] = dirs
-    
-          # Returns the time the given Sass file was last modified.
-      #
-      # If the given file has been deleted or the time can't be accessed
-      # for some other reason, this should return nil.
-      #
-      # @param uri [String] The URI of the file to check.
-      #   Comes from a `:filename` option set on an engine returned by this importer.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` currently being checked.
-      # @return [Time, nil]
-      def mtime(uri, options)
-        Sass::Util.abstract(self)
+            @user.send_confirmation_instructions
       end
     
-      namespace :release do
-    GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
-      desc 'Release #{gem} as a package'
-      task gem => 'package:#{gem}' do
-        sh <<-SH
-          gem install #{package(gem, '.gem')} --local &&
-          gem push #{package(gem, '.gem')}
-        SH
-      end
+        def update
+      authorize @custom_emoji, :update?
+    
+        private
+    
+        def status_params
+      params.require(:status).permit(:sensitive)
     end
     
-          def set_token(session)
-        session[:csrf] ||= self.class.random_token
+      before_action :require_user!
+    
+    desc 'Clean out caches: .pygments-cache, .gist-cache, .sass-cache'
+task :clean do
+  rm_rf [Dir.glob('.pygments-cache/**'), Dir.glob('.gist-cache/**'), Dir.glob('.sass-cache/**'), 'source/stylesheets/screen.css']
+end
+    
+          # Create an Atom-feed for each index.
+      feed = CategoryFeed.new(self, self.source, category_dir, category)
+      feed.render(self.layouts, site_payload)
+      feed.write(self.dest)
+      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
+      self.pages << feed
+    end
+    
+      class GistTagNoCache < GistTag
+    def initialize(tag_name, text, token)
+      super
+      @cache_disabled = true
+    end
+  end
+end
+    
+        def initialize(tag_name, markup, tokens)
+      attributes = ['class', 'src', 'width', 'height', 'title']
+    
+      # Summary is used on the Archive pages to return the first block of content from a post.
+  def summary(input)
+    if input.index(/\n\n/)
+      input.split(/\n\n/)[0]
+    else
+      input
+    end
+  end
+    
+          Dir.chdir(file_path) do
+        contents = file.read
+        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
+          contents = $1.lstrip
+        end
+        contents = pre_filter(contents)
+        if @raw
+          contents
+        else
+          partial = Liquid::Template.parse(contents)
+          context.stack do
+            partial.render(context)
+          end
+        end
       end
-    
-    # when launched as a script, not require'd, (currently from bin/logstash and bin/logstash-plugin) the first
-# argument is the path of a Ruby file to require and a LogStash::Runner class is expected to be
-# defined and exposing the LogStash::Runner#main instance method which will be called with the current ARGV
-# currently lib/logstash/runner.rb and lib/pluginmanager/main.rb are called using this.
-if $0 == __FILE__
-  LogStash::Bundler.setup!({:without => [:build, :development]})
-  require_relative 'patches/jar_dependencies'
-    
-          # Try to add the gems to the current gemfile and lock file, if successful
-      # both of them will be updated. This injector is similar to Bundler's own injector class
-      # minus the support for additionals source and doing local resolution only.
-      ::Bundler::LogstashInjector.inject!(pack)
-    
-      it 'does object equality on config_hash and pipeline_id' do
-    another_exact_pipeline = described_class.new(source, pipeline_id, ordered_config_parts, settings)
-    expect(subject).to eq(another_exact_pipeline)
+    end
+  end
+end
