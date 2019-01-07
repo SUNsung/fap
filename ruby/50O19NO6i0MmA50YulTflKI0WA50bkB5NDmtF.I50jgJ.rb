eedd1deb9f83952b8e017433fb0c30cd1ea93c8a
SUNsung <weatherius@gmail.com>
2019-01-07 07:28:55 +0200

    Rewrite [50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb] File in: 2019-01-07 07:28:55

--- ruby/50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb
+++ ruby/50O19NO6i0MmA50YulTflKI0WA50bkB5NDmtF.I50jgJ.rb
@@ -1,46 +1,120 @@
 
         
-                # Executes a command and returns true if the command succeeded,
-        # and false otherwise. By default, this executes as a normal user,
-        # and it is up to the communicator implementation if they expose an
-        # option for running tests as an administrator.
-        #
-        # @see #execute
-        def test(command, opts=nil)
-        end
+              it 'requires the passwords to match' do
+        visit new_admin_user_path
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Username', with: 'usertest'
+        fill_in 'Password', with: '12345678'
+        fill_in 'Password confirmation', with: 'no_match'
+        click_on 'Create User'
+        expect(page).to have_text('Password confirmation doesn't match')
       end
     end
+    
+          it 'generates a DOT script' do
+        expect(agents_dot(@agents)).to match(%r{
+          \A
+          digraph \x20 'Agent \x20 Event \x20 Flow' \{
+            node \[ [^\]]+ \];
+            edge \[ [^\]]+ \];
+            (?<foo>\w+) \[label=foo\];
+            \k<foo> -> (?<bar1>\w+) \[style=dashed\];
+            \k<foo> -> (?<bar2>\w+) \[color='\#999999'\];
+            \k<bar1> \[label=bar1\];
+            \k<bar2> \[label=bar2,style='rounded,dashed',color='\#999999',fontcolor='\#999999'\];
+            \k<bar2> -> (?<bar3>\w+) \[style=dashed,color='\#999999'\];
+            \k<bar3> \[label=bar3\];
+          \}
+          \z
+        }x)
+      end
+    
+        it 'works for queued jobs' do
+      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
+    end
+  end
+    
+        it 'has a default when the result is empty' do
+      expect(AgentsExporter.new(:name => '').filename).to eq('exported-agents.json')
+      expect(AgentsExporter.new(:name => 'Ə').filename).to eq('exported-agents.json')
+      expect(AgentsExporter.new(:name => '-').filename).to eq('exported-agents.json')
+      expect(AgentsExporter.new(:name => ',,').filename).to eq('exported-agents.json')
+    end
+  end
+    
+      it 'schould register the schedules with the rufus scheduler and run' do
+    mock(@rufus_scheduler).join
+    scheduler = HuginnScheduler.new
+    scheduler.setup!(@rufus_scheduler, Mutex.new)
+    scheduler.run
+  end
+    
+      describe '#sort_tuples!' do
+    let(:tuples) {
+      time = Time.now
+      [
+        [2, 'a', time - 1],  # 0
+        [2, 'b', time - 1],  # 1
+        [1, 'b', time - 1],  # 2
+        [1, 'b', time],      # 3
+        [1, 'a', time],      # 4
+        [2, 'a', time + 1],  # 5
+        [2, 'a', time],      # 6
+      ]
+    }
+    
+            # Yields a VM for each target VM for the command.
+        #
+        # This is a convenience method for easily implementing methods that
+        # take a target VM (in the case of multi-VM) or every VM if no
+        # specific VM name is specified.
+        #
+        # @param [String] name The name of the VM. Nil if every VM.
+        # @param [Boolean] single_target If true, then an exception will be
+        #   raised if more than one target is found.
+        def with_target_vms(names=nil, options=nil)
+          # Using VMs requires a Vagrant environment to be properly setup
+          raise Errors::NoEnvironmentError if !@env.root_path
+    
+              # First determine the proper array of VMs.
+          machines = []
+          if names.length > 0
+            names.each do |name|
+              if pattern = name[/^\/(.+?)\/$/, 1]
+                @logger.debug('Finding machines that match regex: #{pattern}')
+    
+    module Vagrant
+  module Plugin
+    module V2
+      # This is the superclass for all V2 plugins.
+      class Plugin
+        # Special marker that can be used for action hooks that matches
+        # all action sequences.
+        ALL_ACTIONS = :__all_actions__
+    
+        def set_account
+      @account = Account.find(params[:account_id])
+      @user = @account.user
+    end
+    
+    module Admin
+  class CustomEmojisController < BaseController
+    before_action :set_custom_emoji, except: [:index, :new, :create]
+    before_action :set_filter_params
+    
+          @form         = Form::StatusBatch.new(form_status_batch_params.merge(current_account: current_account, action: action_from_button))
+      flash[:alert] = I18n.t('admin.statuses.failed_to_execute') unless @form.save
+    
+      def set_account
+    @account = Account.find(params[:id])
   end
 end
 
     
-            # Helper method that will set a value if a value is given, or otherwise
-        # return the already set value.
-        #
-        # @param [Symbol] key Key for the data
-        # @param [Object] value Value to store.
-        # @return [Object] Stored value.
-        def self.get_or_set(key, value=UNSET_VALUE)
-          # If no value is to be set, then return the value we have already set
-          return data[key] if value.eql?(UNSET_VALUE)
+      respond_to :json
     
-        # @import 'file.less' to '#{target_path}file;'
-    def replace_file_imports(less, target_path = '')
-      less.gsub %r([@\$]import ['|']([\w\-/]+).less['|'];),
-                %Q(@import '#{target_path}\\1';)
-    end
+        render json: web_subscription, serializer: REST::WebPushSubscriptionSerializer
+  end
     
-        def log_http_get_files(files, from, cached = false)
-      return if files.empty?
-      s = '  #{'CACHED ' if cached}GET #{files.length} files from #{from} #{files * ' '}...'
-      if cached
-        puts dark green s
-      else
-        puts dark cyan s
-      end
-    end
-    
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+    class Api::Web::SettingsController < Api::Web::BaseController
+  respond_to :json
