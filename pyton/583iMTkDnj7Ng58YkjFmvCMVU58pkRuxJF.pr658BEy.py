4e6c8ae4dfd63749e0cd5aca66bdba7fd74dcd17
SUNsung <weatherius@gmail.com>
2019-01-07 06:17:15 +0200

    Rewrite [583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py] File in: 2019-01-07 06:17:14

--- pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
+++ pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
@@ -1,140 +1,127 @@
 
         
         
-class HasKey(PostgresSimpleLookup):
-    lookup_name = 'has_key'
-    operator = '?'
-    prepare_rhs = False
+class BlackJackCard(Card):
     
-        def __eq__(self, other):
-        return (
-            isinstance(other, self.__class__) and
-            self.keys == other.keys and
-            self.messages == other.messages and
-            self.strict == other.strict
-        )
+            Accessing a node updates its position to the front of the LRU list.
+        '''
+        node = self.lookup.get(query)
+        if node is None:
+            return None
+        self.linked_list.move_to_front(node)
+        return node.results
     
-        For complete documentation on using Sessions in your code, consult
-    the sessions documentation that is shipped with Django (also available
-    on the Django Web site).
-    '''
-    objects = SessionManager()
+        def crawl_page(self, page):
+        for url in page.child_urls:
+            self.data_store.add_link_to_crawl(url)
+        self.reverse_index_queue.generate(page)
+        self.doc_index_queue.generate(page)
+        self.data_store.remove_link_to_crawl(page.url)
+        self.data_store.insert_crawled_link(page.url, page.signature)
     
-        @cached_property
-    def routers(self):
-        if self._routers is None:
-            self._routers = settings.DATABASE_ROUTERS
-        routers = []
-        for r in self._routers:
-            if isinstance(r, str):
-                router = import_string(r)()
-            else:
-                router = r
-            routers.append(router)
-        return routers
+        for asset in os.listdir(build_path):
+        compat_print('Uploading %s...' % asset)
+        releaser.create_asset(release_id, os.path.join(build_path, asset))
     
-    from .compat import OrderedDict, Mapping, MutableMapping
     
-    from .__version__ import __title__, __description__, __url__, __version__
-from .__version__ import __build__, __author__, __author_email__, __license__
-from .__version__ import __copyright__, __cake__
+def openssl_encode(algo, key, iv):
+    cmd = ['openssl', 'enc', '-e', '-' + algo, '-K', hex_str(key), '-iv', hex_str(iv)]
+    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    out, _ = prog.communicate(secret_msg)
+    return out
     
-            assert r.status_code == 200
-        assert len(r.history) == 2
-        assert r.history[0].request.url == url
     
-        By default this will get the strings from the blns.txt file
+if len(sys.argv) <= 1:
+    print('Specify the version number as parameter')
+    sys.exit()
+version = sys.argv[1]
     
-        # Fetch the records to be pickled.
-    cursor.execute('SELECT * FROM memos')
-    memos = [MemoRecord(key, task) for key, task in cursor]
-    # Save the records using our custom DBPickler.
-    file = io.BytesIO()
-    DBPickler(file).dump(memos)
+        with io.open(infile, encoding='utf-8') as inf:
+        issue_template_tmpl = inf.read()
     
-    def handleSlideshowTitle(title):
-    print('<title>%s</title>' % getText(title.childNodes))
     
-    con = sqlite3.connect(':memory:')
-con.isolation_level = None
-cur = con.cursor()
+def main():
+    parser = optparse.OptionParser(usage='%prog OUTFILE.md')
+    options, args = parser.parse_args()
+    if len(args) != 1:
+        parser.error('Expected an output filename')
     
-            devices = {}
-        for device in request.json()['status']:
-            try:
-                devices[device['Key']] = {
-                    'ip': device['IPAddress'],
-                    'mac': device['PhysAddress'],
-                    'host': device['Name'],
-                    'status': device['Active']
-                    }
-            except (KeyError, requests.exceptions.RequestException):
-                pass
-        return devices
+    
+def pytest_addoption(parser):
+    '''Adds `--enable-functional` argument.'''
+    group = parser.getgroup('thefuck')
+    group.addoption('--enable-functional', action='store_true', default=False,
+                    help='Enable functional tests')
+    
+    
+python_3 = (u'thefuck/python3-bash',
+            u'FROM python:3',
+            u'sh')
+    
+    # TODO: ensure that history changes.
 
     
-        def _forwards(self, orm):
-        'Write your forwards methods here.'
-        for release in RangeQuerySetWrapperWithProgressBar(
-            orm.Release.objects.exclude(new_groups=0)
-        ):
-            projects = list(release.projects.values_list('id', flat=True))
-            if len(projects) > 1:
-                # do something fancy where we look at Group.first_release
-                # to calculate ReleaseProject.new_group
-                for p_id in projects:
-                    new_groups = orm.Group.objects.filter(
-                        first_release=release, project_id=p_id
-                    ).count()
-                    if not new_groups:
-                        continue
-                    orm.ReleaseProject.objects.filter(
-                        release_id=release.id, project_id=p_id
-                    ).update(new_groups=new_groups)
-            elif len(projects) == 1:
-                # copy Release.new_groups to ReleaseProject.new_group
-                orm.ReleaseProject.objects.filter(
-                    release_id=release.id, project_id=projects[0]
-                ).update(new_groups=release.new_groups)
     
-            # Adding unique constraint on 'EventProcessingIssue', fields ['raw_event',
-        # 'processing_issue']
-        db.create_unique('sentry_eventprocessingissue', ['raw_event_id', 'processing_issue_id'])
+@pytest.mark.parametrize('command, packages', [
+    (Command('vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo vim', 'vim: command not found'),
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
+     [('vim', 'main'), ('vim-tiny', 'main')])])
+def test_match(mocker, command, packages):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
     
-        def backwards(self, orm):
-        'Write your backwards methods here.'
+    match_output = '''
+Listing... Done
+heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
+resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
+squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
+unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
+'''
     
-            # Deleting field 'ApiToken.application'
-        db.delete_column('sentry_apitoken', 'application_id')
     
-            # Adding model 'DSymApp'
-        db.create_table(
-            'sentry_dsymapp', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.Project']
-                    )
-                ), ('app_id', self.gf('django.db.models.fields.CharField')(max_length=64)),
-                ('sync_id', self.gf('django.db.models.fields.CharField')(max_length=64, null=True)),
-                ('data', self.gf('jsonfield.fields.JSONField')(default={})), (
-                    'platform',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(
-                        default=0
-                    )
-                ), (
-                    'last_synced',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['DSymApp'])
+@pytest.mark.parametrize('command', [
+    Command('cargo buid', no_such_subcommand_old),
+    Command('cargo buils', no_such_subcommand)])
+def test_match(command):
+    assert match(command)
     
-            # Deleting field 'CommitAuthor.external_id'
-        db.delete_column('sentry_commitauthor', 'external_id')
+            return False
+    
+    Tree objects are wrapper for Token objects.
+    
+                    # EOF Transition to accept state?
+                if c == EOF and self.eof[s] >= 0:
+                    #print 'EOF Transition to accept state %d' \
+                    #  % self.accept[self.eof[s]]
+                    return self.accept[self.eof[s]]
+    
+        def release(self, marker=None):
+        # no resources to release
+        pass
+    
+    
+    # This is the file name suffix for HTML files (e.g. '.xhtml').
+#html_file_suffix = None
+    
+    # Find all the faces in the image using a pre-trained convolutional neural network.
+# This method is more accurate than the default HOG model, but it's slower
+# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
+# this will use GPU acceleration and perform well.
+# See also: find_faces_in_picture.py
+face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
+    
+            face_names.append(name)
+    
+        # Loop over each face found in the frame to see if it's someone we know.
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = '<Unknown Person>'
+    
+    # Create a PIL imagedraw object so we can draw on the picture
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
