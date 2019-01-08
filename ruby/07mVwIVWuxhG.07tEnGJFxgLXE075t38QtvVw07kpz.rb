d5ff33d0720ae0ae85e83de9a7475efa6a14edd6
SUNsung <weatherius@gmail.com>
2019-01-09 00:21:27 +0200

    Rewrite [07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb] File in: 2019-01-09 00:21:27

--- ruby/07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb
+++ ruby/07mVwIVWuxhG.07tEnGJFxgLXE075t38QtvVw07kpz.rb
@@ -1,168 +1,205 @@
 
         
-        module Homebrew
-  module_function
+                next unless HOMEBREW_CELLAR.directory?
     
-    module Homebrew
-  module_function
+      def external_commands
+    paths.reduce([]) do |cmds, path|
+      Dir['#{path}/brew-*'].each do |file|
+        next unless File.executable?(file)
+        cmd = File.basename(file, '.rb')[5..-1]
+        cmds << cmd unless cmd.include?('.')
+      end
+      cmds
+    end.sort
+  end
     
-        begin
-      json = JSON.parse(self.body)
-    rescue JSON::ParserError => e
-      elog('#{e.class} #{e.message}\n#{e.backtrace * '\n'}')
-    end
+        initial_revision_var = 'HOMEBREW_UPDATE_BEFORE#{repo_var}'
+    @initial_revision = ENV[initial_revision_var].to_s
+    raise ReporterRevisionUnsetError, initial_revision_var if @initial_revision.empty?
     
-    =begin
-      +------+-----------+-----------------------------------------+
-      | Hex  | Name      | Description                             |
-      +------+-----------+-----------------------------------------+
-      | 0x01 | NEW       | Initiate a new call                     |
-      |      |           |                                         |
-      | 0x02 | PING      | Ping request                            |
-      |      |           |                                         |
-      | 0x03 | PONG      | Ping or poke reply                      |
-      |      |           |                                         |
-      | 0x04 | ACK       | Explicit acknowledgment                 |
-      |      |           |                                         |
-      | 0x05 | HANGUP    | Initiate call tear-down                 |
-      |      |           |                                         |
-      | 0x06 | REJECT    | Reject a call                           |
-      |      |           |                                         |
-      | 0x07 | ACCEPT    | Accept a call                           |
-      |      |           |                                         |
-      | 0x08 | AUTHREQ   | Authentication request                  |
-      |      |           |                                         |
-      | 0x09 | AUTHREP   | Authentication reply                    |
-      |      |           |                                         |
-      | 0x0a | INVAL     | Invalid message                         |
-      |      |           |                                         |
-      | 0x0b | LAGRQ     | Lag request                             |
-      |      |           |                                         |
-      | 0x0c | LAGRP     | Lag reply                               |
-      |      |           |                                         |
-      | 0x0d | REGREQ    | Registration request                    |
-      |      |           |                                         |
-      | 0x0e | REGAUTH   | Registration authentication             |
-      |      |           |                                         |
-      | 0x0f | REGACK    | Registration acknowledgement            |
-      |      |           |                                         |
-      | 0x10 | REGREJ    | Registration reject                     |
-      |      |           |                                         |
-      | 0x11 | REGREL    | Registration release                    |
-      |      |           |                                         |
-      | 0x12 | VNAK      | Video/Voice retransmit request          |
-      |      |           |                                         |
-      | 0x13 | DPREQ     | Dialplan request                        |
-      |      |           |                                         |
-      | 0x14 | DPREP     | Dialplan reply                          |
-      |      |           |                                         |
-      | 0x15 | DIAL      | Dial                                    |
-      |      |           |                                         |
-      | 0x16 | TXREQ     | Transfer request                        |
-      |      |           |                                         |
-      | 0x17 | TXCNT     | Transfer connect                        |
-      |      |           |                                         |
-      | 0x18 | TXACC     | Transfer accept                         |
-      |      |           |                                         |
-      | 0x19 | TXREADY   | Transfer ready                          |
-      |      |           |                                         |
-      | 0x1a | TXREL     | Transfer release                        |
-      |      |           |                                         |
-      | 0x1b | TXREJ     | Transfer reject                         |
-      |      |           |                                         |
-      | 0x1c | QUELCH    | Halt audio/video [media] transmission   |
-      |      |           |                                         |
-      | 0x1d | UNQUELCH  | Resume audio/video [media] transmission |
-      |      |           |                                         |
-      | 0x1e | POKE      | Poke request                            |
-      |      |           |                                         |
-      | 0x1f | Reserved  | Reserved for future use                 |
-      |      |           |                                         |
-      | 0x20 | MWI       | Message waiting indication              |
-      |      |           |                                         |
-      | 0x21 | UNSUPPORT | Unsupported message                     |
-      |      |           |                                         |
-      | 0x22 | TRANSFER  | Remote transfer request                 |
-      |      |           |                                         |
-      | 0x23 | Reserved  | Reserved for future use                 |
-      |      |           |                                         |
-      | 0x24 | Reserved  | Reserved for future use                 |
-      |      |           |                                         |
-      | 0x25 | Reserved  | Reserved for future use                 |
-      +------+-----------+-----------------------------------------+
-=end
+    User.seed do |u|
+  u.id = -1
+  u.name = 'system'
+  u.username = 'system'
+  u.username_lower = 'system'
+  u.password = SecureRandom.hex
+  u.active = true
+  u.admin = true
+  u.moderator = true
+  u.approved = true
+  u.trust_level = TrustLevel[4]
+end
     
-              res
-        end
-    
-                if OpenSSL::HMAC.digest('MD5', k1, decrypted) != checksum
-              raise ::RuntimeError, 'RC4-HMAC decryption failed, incorrect checksum verification'
-            end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::KdcResponse
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode_asn1(input)
-            input.value[0].value.each do |val|
-              case val.tag
-              when 0
-                self.pvno = decode_pvno(val)
-              when 1
-                self.msg_type = decode_msg_type(val)
-              when 3
-                self.crealm = decode_crealm(val)
-              when 4
-                self.cname = decode_cname(val)
-              when 5
-                self.ticket = decode_ticket(val)
-              when 6
-                self.enc_part = decode_enc_part(val)
-              else
-                raise ::RuntimeError, 'Failed to decode KDC-RESPONSE SEQUENCE'
-              end
-            end
-          end
-    
-            context 'when fetching a gem from rubygems' do
-    
-              unless inventory_unit.respond_to?(can_event) &&
-              inventory_unit.send(can_event)
-            render plain: { exception: 'cannot transition to #{@event}' }.to_json,
-                   status: 200
-            false
-          end
-        end
-    
-            def perform_payment_action(action, *args)
-          authorize! action, Payment
-          @payment.send('#{action}!', *args)
-          respond_with(@payment, default_template: :show)
-        end
-    
-            def show
-          @stock_movement = scope.find(params[:id])
-          respond_with(@stock_movement)
-        end
-    
-            def taxon_params
-          if params[:taxon] && !params[:taxon].empty?
-            params.require(:taxon).permit(permitted_taxon_attributes)
-          else
-            {}
-          end
-        end
+            # Reset topic count because we don't count the description topic
+        DB.exec 'UPDATE categories SET topic_count = 0 WHERE id = #{lounge.id}'
       end
     end
   end
 end
 
     
-            def update
-          authorize! :update, user
-          if user.update_attributes(user_params)
-            respond_with(user, status: 200, default_template: :show)
-          else
-            invalid_resource!(user)
-          end
-        end
+        if successfully_sent?(resource)
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
+  end
+    
+          respond_to_on_destroy
+    end
+  end
+    
+          def remember_me_is_active?(resource)
+        return false unless resource.respond_to?(:remember_me)
+        scope = Devise::Mapping.find_scope!(resource)
+        _, token, generated_at = cookies.signed[remember_key(resource, scope)]
+        resource.remember_me?(token, generated_at)
+      end
+    
+          def expire_data_after_sign_in!
+        # session.keys will return an empty array if the session is not yet loaded.
+        # This is a bug in both Rack and Rails.
+        # A call to #empty? forces the session to be loaded.
+        session.empty?
+        session.keys.grep(/^devise\./).each { |k| session.delete(k) }
+      end
+    
+    module Devise
+  module Controllers
+    # Provide the ability to store a location.
+    # Used to redirect back to a desired path after sign in.
+    # Included by default in all controllers.
+    module StoreLocation
+      # Returns and delete (if it's navigational format) the url stored in the session for
+      # the given scope. Useful for giving redirect backs after sign up:
+      #
+      # Example:
+      #
+      #   redirect_to stored_location_for(:user) || root_path
+      #
+      def stored_location_for(resource_or_scope)
+        session_key = stored_location_key_for(resource_or_scope)
+    
+        def default_failure_app(options)
+      @failure_app = options[:failure_app] || Devise::FailureApp
+      if @failure_app.is_a?(String)
+        ref = Devise.ref(@failure_app)
+        @failure_app = lambda { |env| ref.get.call(env) }
+      end
+    end
+    
+        # Recoverable takes care of resetting the user password and send reset instructions.
+    #
+    # ==Options
+    #
+    # Recoverable adds the following options to devise_for:
+    #
+    #   * +reset_password_keys+: the keys you want to use when recovering the password for an account
+    #   * +reset_password_within+: the time period within which the password must be reset or the token expires.
+    #   * +sign_in_after_reset_password+: whether or not to sign in the user automatically after a password reset.
+    #
+    # == Examples
+    #
+    #   # resets the user password and save the record, true if valid passwords are given, otherwise false
+    #   User.find(1).reset_password('password123', 'password123')
+    #
+    #   # creates a new token and send it with instructions about how to reset the password
+    #   User.find(1).send_reset_password_instructions
+    #
+    module Recoverable
+      extend ActiveSupport::Concern
+    
+    module Vagrant
+  # This class handles guest-OS specific interactions with a machine.
+  # It is primarily responsible for detecting the proper guest OS
+  # implementation and then delegating capabilities.
+  #
+  # Vagrant has many tasks which require specific guest OS knowledge.
+  # These are implemented using a guest/capability system. Various plugins
+  # register as 'guests' which determine the underlying OS of the system.
+  # Then, 'guest capabilities' register themselves for a specific OS (one
+  # or more), and these capabilities are called.
+  #
+  # Example capabilities might be 'mount_virtualbox_shared_folder' or
+  # 'configure_networks'.
+  #
+  # This system allows for maximum flexibility and pluginability for doing
+  # guest OS specific operations.
+  class Guest
+    include CapabilityHost
+    
+      it 'raises a TypeError when passed a String' do
+    lambda { sleep('2')   }.should raise_error(TypeError)
+  end
+    
+      it 'raises a TypeError when passed a String' do
+    lambda { srand('7') }.should raise_error(TypeError)
+  end
+end
+    
+      it 'returns true when passed ?R if the argument is readable by the real uid' do
+    Kernel.test(?R, @file).should be_true
+  end
+    
+        while readable_io = IO.select([self_read])
+      signal = readable_io.first[0].gets.strip
+      handle_signal(launcher, signal)
+    end
+  #end
+rescue SystemExit => e
+  #Sidekiq.logger.error('Profiling...')
+  #result = RubyProf.stop
+  #printer = RubyProf::GraphHtmlPrinter.new(result)
+  #printer.print(File.new('output.html', 'w'), :min_percent => 1)
+  # normal
+rescue => e
+  raise e if $DEBUG
+  STDERR.puts e.message
+  STDERR.puts e.backtrace.join('\n')
+  exit 1
+end
+
+    
+          # Example usage:
+      #   Sidekiq::Client.enqueue_in(3.minutes, MyWorker, 'foo', 1, :bat => 'bar')
+      #
+      def enqueue_in(interval, klass, *args)
+        klass.perform_in(interval, *args)
+      end
+    end
+    
+        # Creating the Redis#brpop command takes into account any
+    # configured queue weights. By default Redis#brpop returns
+    # data from the first queue that has pending elements. We
+    # recreate the queue command each time we invoke Redis#brpop
+    # to honor weights and avoid queue starvation.
+    def queues_cmd
+      if @strictly_ordered_queues
+        @queues
+      else
+        queues = @queues.shuffle.uniq
+        queues << TIMEOUT
+        queues
+      end
+    end
+    
+        def logger
+      Sidekiq::Logging.logger
+    end
+  end
+end
+
+    
+      Sidekiq::Client.prepend TestingClient
+    
+        EXPIRY = 60 * 60 * 24
+    
+      context 'called with one style' do
+    it 'applies same style to all sides' do
+      rule = 'border-style: solid'
+    
+      context 'called with arguments (1, $ratio: $golden-ratio)' do
+    it 'output the first value from the golden ratio scale' do
+      expect('.one-golden-ratio').to have_rule('font-size: 1.618em')
+    end
+  end
