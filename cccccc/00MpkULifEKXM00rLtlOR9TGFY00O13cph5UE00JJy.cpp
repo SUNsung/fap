bb92a7b155d9acfee39fd81082a1d0388a58796c
SUNsung <weatherius@gmail.com>
2019-01-08 15:58:39 +0200

    Rewrite [00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp] File in: 2019-01-08 15:58:38

--- cccccc/00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp
+++ cccccc/00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp
@@ -1,185 +1,97 @@
 
         
-        namespace {
-string GetTypeUrl(const Descriptor* message,
-                  const string& type_url_prefix) {
-  if (!type_url_prefix.empty() &&
-      type_url_prefix[type_url_prefix.size() - 1] == '/') {
-    return type_url_prefix + message->full_name();
-  } else {
-    return type_url_prefix + '/' + message->full_name();
-  }
-}
-}  // namespace
-    
-    #include <memory>
-#include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/command_line_interface.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
-#include <google/protobuf/descriptor.pb.h>
-    
-    void SourceGeneratorBase::WriteGeneratedCodeAttributes(io::Printer* printer) {
-  printer->Print('[global::System.Diagnostics.DebuggerNonUserCodeAttribute]\n');
-}
-    
-    void WriteMessageDocComment(io::Printer* printer, const Descriptor* message) {
-  printer->Print('/**\n');
-  WriteDocCommentBody(printer, message);
-  printer->Print(
-    ' * Protobuf type {@code $fullname$}\n'
-    ' */\n',
-    'fullname', EscapeJavadoc(message->full_name()));
-}
-    
-    #include <google/protobuf/compiler/java/java_doc_comment.h>
-    
-    // Get the name of a service's Java class without package name prefix.
-string ClassNameWithoutPackage(const ServiceDescriptor* descriptor,
-                               bool immutable) {
-  string full_name = StripPackageName(descriptor->full_name(),
-                                      descriptor->file());
-  // We don't allow nested service definitions.
-  GOOGLE_CHECK(full_name.find('.') == string::npos);
-  return full_name;
-}
-    
-    void SharedCodeGenerator::Generate(GeneratorContext* context,
-                                   std::vector<string>* file_list,
-                                   std::vector<string>* annotation_file_list) {
-  string java_package = FileJavaPackage(file_);
-  string package_dir = JavaPackageToDir(java_package);
-    }
-    
-      virtual io::ZeroCopyOutputStream* OpenForInsert(
-      const string& filename, const string& insertion_point) {
-    CodeGeneratorResponse::File* file = response_->add_file();
-    file->set_name(filename);
-    file->set_insertion_point(insertion_point);
-    return new io::StringOutputStream(file->mutable_content());
-  }
-    
-    
-    {} // namespace CAROTENE_NS
-
-    
-    #include 'common.hpp'
-#include 'vtransform.hpp'
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && defined(__arm__)
-CVTS_FUNC(s8, s16, 16,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 16)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.8 {d4-d5}, [%[src]]                              \n\t'
-            'vmovl.s8 q3, d4                                       \n\t'
-            'vmovl.s8 q4, d5                                       \n\t'
-            'vmovl.s16 q5, d6                                      \n\t'
-            'vmovl.s16 q6, d7                                      \n\t'
-            'vmovl.s16 q7, d8                                      \n\t'
-            'vmovl.s16 q8, d9                                      \n\t'
-            'vcvt.f32.s32 q9, q5                                   \n\t'
-            'vcvt.f32.s32 q10, q6                                  \n\t'
-            'vcvt.f32.s32 q11, q7                                  \n\t'
-            'vcvt.f32.s32 q12, q8                                  \n\t'
-            'vmul.f32 q13, q9, q0                                  \n\t'
-            'vmul.f32 q14, q10, q0                                 \n\t'
-            'vmul.f32 q15, q11, q0                                 \n\t'
-            'vmul.f32 q2, q12, q0                                  \n\t'
-            'vadd.f32 q3, q13, q1                                  \n\t'
-            'vadd.f32 q4, q14, q1                                  \n\t'
-            'vadd.f32 q5, q15, q1                                  \n\t'
-            'vadd.f32 q6, q2, q1                                   \n\t'
-            'vcvt.s32.f32 q7, q3                                   \n\t'
-            'vcvt.s32.f32 q8, q4                                   \n\t'
-            'vcvt.s32.f32 q9, q5                                   \n\t'
-            'vcvt.s32.f32 q10, q6                                  \n\t'
-            'vqmovn.s32 d22, q7                                    \n\t'
-            'vqmovn.s32 d23, q8                                    \n\t'
-            'vqmovn.s32 d24, q9                                    \n\t'
-            'vqmovn.s32 d25, q10                                   \n\t'
-            'vst1.16 {d22-d23}, [%[dst1]]                          \n\t'
-            'vst1.16 {d24-d25}, [%[dst2]]                          \n\t'
-            : /*no output*/
-            : [src] 'r' (_src + i),
-              [dst1] 'r' (_dst + i + 0),
-              [dst2] 'r' (_dst + i + 8),
-              'w'  (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27','d28','d29','d30','d31'
-        );
-    }
-})
+        #ifndef BENCHMARK_HAS_CXX11
+#define BENCHMARK_DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&);                         \
+  TypeName& operator=(const TypeName&)
 #else
-CVTS_FUNC(s8, s16, 16,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 16)
-    {
-        internal::prefetch(_src + i);
-        int8x16_t vline = vld1q_s8(_src + i);
-        int16x8_t vline1_s16 = vmovl_s8(vget_low_s8 (vline));
-        int16x8_t vline2_s16 = vmovl_s8(vget_high_s8(vline));
-        int32x4_t vline1_s32 = vmovl_s16(vget_low_s16 (vline1_s16));
-        int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline1_s16));
-        int32x4_t vline3_s32 = vmovl_s16(vget_low_s16 (vline2_s16));
-        int32x4_t vline4_s32 = vmovl_s16(vget_high_s16(vline2_s16));
-        float32x4_t vline1_f32 = vcvtq_f32_s32(vline1_s32);
-        float32x4_t vline2_f32 = vcvtq_f32_s32(vline2_s32);
-        float32x4_t vline3_f32 = vcvtq_f32_s32(vline3_s32);
-        float32x4_t vline4_f32 = vcvtq_f32_s32(vline4_s32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline3_f32 = vmulq_f32(vline3_f32, vscale);
-        vline4_f32 = vmulq_f32(vline4_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        vline3_f32 = vaddq_f32(vline3_f32, vshift);
-        vline4_f32 = vaddq_f32(vline4_f32, vshift);
-        vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        vline3_s32 = vcvtq_s32_f32(vline3_f32);
-        vline4_s32 = vcvtq_s32_f32(vline4_f32);
-        int16x8_t vRes1_s16 = vcombine_s16(vqmovn_s32(vline1_s32), vqmovn_s32(vline2_s32));
-        int16x8_t vRes2_s16 = vcombine_s16(vqmovn_s32(vline3_s32), vqmovn_s32(vline4_s32));
-        vst1q_s16(_dst + i + 0, vRes1_s16);
-        vst1q_s16(_dst + i + 8, vRes2_s16);
-    }
-})
+#define BENCHMARK_DISALLOW_COPY_AND_ASSIGN(TypeName) \
+  TypeName(const TypeName&) = delete;                \
+  TypeName& operator=(const TypeName&) = delete
 #endif
     
-            // perform vertical convolution
-        for ( ; x <= bwidth; x += 8)
-        {
-            internal::prefetch(srow0 + x);
-            internal::prefetch(srow1 + x);
-            internal::prefetch(srow2 + x);
+      // We need to flush the stream buffers into the console before each
+  // SetConsoleTextAttribute call lest it affect the text that is already
+  // printed but has not yet reached the console.
+  fflush(stdout);
+  SetConsoleTextAttribute(stdout_handle,
+                          GetPlatformColorCode(color) | FOREGROUND_INTENSITY);
+  vprintf(fmt, args);
+    
+    static void IgnoreColorPrint(std::ostream& out, LogColor, const char* fmt,
+                             ...) {
+  va_list args;
+  va_start(args, fmt);
+  out << FormatString(fmt, args);
+  va_end(args);
+}
+    
+     private:
+  Mutex lock_;
+  Condition phase_condition_;
+  int running_threads_;
+    
+    #if defined(HAVE_STD_REGEX)
+    
+      if (info.scaling_enabled) {
+    Out << '***WARNING*** CPU scaling is enabled, the benchmark '
+           'real time measurements may be noisy and will incur extra '
+           'overhead.\n';
+  }
+    
+    exception_wrapper::VTable const exception_wrapper::SharedPtr::ops_{
+    copy_,
+    move_,
+    delete_,
+    throw_,
+    type_,
+    get_exception_,
+    get_exception_ptr_};
+    
+    namespace uri_detail {
     }
     
-        std::vector<u8> _buf((size.width+16)*3*(sizeof(ptrdiff_t) + sizeof(u8)) + 128);
-    u8* buf[3];
-    buf[0] = &_buf[0]; buf[1] = buf[0] + size.width; buf[2] = buf[1] + size.width;
-    ptrdiff_t* cpbuf[3];
-    cpbuf[0] = (ptrdiff_t*)internal::alignPtr(buf[2] + size.width, sizeof(ptrdiff_t)) + 1;
-    cpbuf[1] = cpbuf[0] + size.width + 1;
-    cpbuf[2] = cpbuf[1] + size.width + 1;
-    memset(buf[0], 0, size.width*3);
+    /**
+ * Reads sizeof(T) bytes, and returns false if not enough bytes are available.
+ * Returns true if the first n bytes are equal to prefix when interpreted as
+ * a little endian T.
+ */
+template <typename T>
+typename std::enable_if<std::is_unsigned<T>::value, bool>::type
+dataStartsWithLE(const IOBuf* data, T prefix, uint64_t n = sizeof(T)) {
+  DCHECK_GT(n, 0);
+  DCHECK_LE(n, sizeof(T));
+  T value;
+  Cursor cursor{data};
+  if (!cursor.tryReadLE(value)) {
+    return false;
+  }
+  const T mask = n == sizeof(T) ? T(-1) : (T(1) << (8 * n)) - 1;
+  return prefix == (value & mask);
+}
     
-            // perform vertical convolution
-        for ( ; x <= bwidth; x += 8)
-        {
-            internal::prefetch(srow0 + x);
-            internal::prefetch(srow1 + x);
-            internal::prefetch(srow2 + x);
+    template <template <typename> class Atom = std::atomic>
+class counted_ptr_internals : public counted_ptr_base<Atom> {
+ public:
+  template <typename T, typename... Args>
+  static counted_ptr<T, Atom> make_ptr(Args&&... args) {
+    return make_counted<Atom, T>(std::forward<Args...>(args...));
+  }
+  template <typename T>
+  using CountedPtr = counted_ptr<T, Atom>;
+  typedef void counted_base;
     }
     
-    namespace caffe2 {
-    }
-    
-    
-    {} // namespace caffe2
+    TEST_F(SparseByteSetTest, each_random) {
+  mt19937 rng;
+  uniform_int_distribution<uint16_t> dist{lims::min(), lims::max()};
+  set<uint8_t> added;
+  while (added.size() <= lims::max()) {
+    auto c = uint8_t(dist(rng));
+    EXPECT_EQ(added.count(c), s.contains(c));
+    EXPECT_EQ(!added.count(c), s.add(c));
+    added.insert(c);
+    EXPECT_TRUE(added.count(c)); // sanity
+    EXPECT_TRUE(s.contains(c));
+  }
+}
