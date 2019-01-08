5279e299dd9a9c51d46da434f991557556007076
SUNsung <weatherius@gmail.com>
2019-01-08 20:44:49 +0200

    Rewrite [08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb] File in: 2019-01-08 20:44:49

--- ruby/08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb
+++ ruby/08mw8ximLeuY08.Gzw4hegNfg08B6EqKdkfdU08iny.rb
@@ -1,102 +1,122 @@
 
         
-        # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
+            def update
+      authorize @user, :change_email?
     
-          #
-    
-      File.write(Paths.status_file, p.exitstatus)
-  File.open(Paths.output_file, 'wb') do |f|
-    f.print '$ '
-    f.puts args.join(' ')
-    f.puts output
-    f.puts 'EXIT STATUS: #{p.exitstatus}'
-  end
-    
-    module Jekyll
-  module Converters
-    class Markdown
-      class KramdownParser
-        CODERAY_DEFAULTS = {
-          'css'               => 'style',
-          'bold_every'        => 10,
-          'line_numbers'      => 'inline',
-          'line_number_start' => 1,
-          'tab_width'         => 4,
-          'wrap'              => 'div',
-        }.freeze
-    
-          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent)).to eq([@agent2])
-    end
-    
-      let :reverted_template do
-    old_template.merge('url' => '{{ url }}')
-  end
+          if emoji.save
+        log_action :create, emoji
+        flash[:notice] = I18n.t('admin.custom_emojis.copied_msg')
+      else
+        flash[:alert] = I18n.t('admin.custom_emojis.copy_failed_msg')
+      end
     
         private
     
-            # Remove examples
-        css('.runnable-example').each do |node|
-          node.parent.remove
-        end
+          @report_note = current_account.report_notes.new(resource_params)
+      @report = @report_note.report
     
-      def remove_linked_keg_record
-    linked_keg_record.unlink
-    linked_keg_record.parent.rmdir_if_possible
+        web_subscription = ::Web::PushSubscription.create!(
+      endpoint: subscription_params[:endpoint],
+      key_p256dh: subscription_params[:keys][:p256dh],
+      key_auth: subscription_params[:keys][:auth],
+      data: data,
+      user_id: active_session.user_id,
+      access_token_id: active_session.access_token_id
+    )
+    
+          EOS
+      login!
+    end
+    
+        odie 'Unknown command: #{cmd}' unless path
+    puts path
   end
+end
+
     
-        if GitHub.api_credentials_type == :none
-      puts <<~EOS
-        You can create a new personal access token:
-          #{GitHub::ALL_SCOPES_URL}
-        #{Utils::Shell.set_variable_in_profile('HOMEBREW_GITHUB_API_TOKEN', 'your_token_here')}
+          ohai 'Migrating tap #{old_name} to #{new_name}...' if $stdout.tty?
     
-        ff = if ARGV.named.empty?
-      Formula.installed.sort
+    When /^I (?:sign|log) in manually as '([^']*)' with password '([^']*)'( on the mobile website)?$/ \
+do |username, password, mobile|
+  @me = User.find_by_username(username)
+  @me.password ||= password
+  manual_login
+  confirm_login mobile
+end
+    
+      def navigate_to(page_name)
+    path = path_to(page_name)
+    if path.is_a?(Hash)
+      visit(path[:path])
+      await_elem = path[:special_elem]
+      find(await_elem.delete(:selector), await_elem)
     else
-      ARGV.resolved_formulae.sort
-    end
-    
-    # Get the version string. If this is being installed from Git,
-# this includes the proper prerelease version.
-def get_version
-  File.read(scope('VERSION').strip)
-end
-    
-        # Converts the CSS template into Sass or SCSS code.
-    #
-    # @param fmt [Symbol] `:sass` or `:scss`, designating the format to return.
-    # @return [String] The resulting Sass or SCSS code
-    # @raise [Sass::SyntaxError] if there's an error parsing the CSS template
-    def render(fmt = :sass)
-      check_encoding!
-      build_tree.send('to_#{fmt}', @options).strip + '\n'
-    rescue Sass::SyntaxError => err
-      err.modify_backtrace(:filename => @options[:filename] || '(css)')
-      raise err
-    end
-    
-    module RuboCop
-  module AST
-    # A node extension for `hash` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `hash` nodes within RuboCop.
-    class HashNode < Node
-      # Returns an array of all the key value pairs in the `hash` literal.
-      #
-      # @return [Array<PairNode>] an array of `pair` nodes
-      def pairs
-        each_pair.to_a
-      end
-    
-          # Custom destructuring method. This is used to normalize the branches
-      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
-      #
-      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
-      def node_parts
-        [self, self]
-      end
+      visit(path)
     end
   end
+    
+          get :index, params: {a_id: @aspect.id, page: '1'}, format: :json
+      save_fixture(response.body, 'aspects_manage_contacts_json')
+    end
+    
+        context 'with invalid parameters' do
+      let(:invalid_params) { valid_params.deep_merge(user: {password_confirmation: 'baddword'}) }
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
+        end
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
+      end
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
+      else
+        '#{quote}<footer>#{author}</footer>'
+      end
+      '<blockquote>#{blockquote}</blockquote>'
+    end
+    
+          # Create an Atom-feed for each index.
+      feed = CategoryFeed.new(self, self.source, category_dir, category)
+      feed.render(self.layouts, site_payload)
+      feed.write(self.dest)
+      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
+      self.pages << feed
+    end
+    
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
+    
+      def framework_major_version
+    framework_version.split('.').first.to_i
+  end
 end
+World(RailsCommandHelpers)
+
+    
+    class PaperclipGenerator < ActiveRecord::Generators::Base
+  desc 'Create a migration to add paperclip-specific fields to your model. ' +
+       'The NAME argument is the name of your model, and the following ' +
+       'arguments are the name of the attachments'
+    
+        def path
+      @file.respond_to?(:path) ? @file.path : @file
+    end
