dadc114e9773a3912bc269950e04cd5048e2bd93
SUNsung <weatherius@gmail.com>
2019-01-09 22:24:45 +0200

    Rewrite [45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb] File in: 2019-01-09 22:24:45

--- ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
+++ ruby/45E3wcGN.4aGg45tGdgqcb2.M645SVUEfB1n9M45Hx6.rb
@@ -1,236 +1,173 @@
 
         
-          p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
-    
-                c.action do |args, opts|
-              Jekyll::Commands::NewTheme.process(args, opts)
-            end
-          end
-        end
-    
-          # It's important to *not* have this code inside the rescue block
-      # otherwise all NameErrors will be caught and the error message is
-      # confusing
-      begin
-        return self.try_switch_to_lane(method_sym, arguments)
-      rescue LaneNotAvailableError
-        # We don't actually handle this here yet
-        # We just try to use a user configured lane first
-        # and only if there is none, we're gonna check for the
-        # built-in actions
-      end
-    
-            inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
-        pseudocommand = 'git log --pretty=\'%s%n%b\' --date=\'short\' #{inner_command.shellescape}...HEAD'
-        expect(result).to eq(pseudocommand)
-      end
-    
-            expect(result.size).to eq(4)
-        expect(result[0]).to eq('security create-keychain -p testpassword ~/Library/Keychains/test.keychain')
-    
-        context 'external commands are failed' do
-      context 'with error_callback' do
-        it 'doesn't raise shell_error' do
-          allow(FastlaneCore::UI).to receive(:error)
-          called = false
-          expect_command('exit 1', exitstatus: 1)
-          Fastlane::Actions.sh('exit 1', error_callback: ->(_) { called = true })
-    
-        # Determines the defined data type of this ConfigItem
-    def data_type
-      if @data_type.kind_of?(Symbol)
-        nil
-      elsif @data_type
-        @data_type
-      else
-        (@is_string ? String : nil)
+              it 'requires the passwords to match' do
+        visit new_admin_user_path
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Username', with: 'usertest'
+        fill_in 'Password', with: '12345678'
+        fill_in 'Password confirmation', with: 'no_match'
+        click_on 'Create User'
+        expect(page).to have_text('Password confirmation doesn't match')
       end
     end
     
-    module CrossplatformShellwords
-  # handle switching between implementations of shellescape
-  def shellescape(str)
-    if FastlaneCore::Helper.windows?
-      WindowsShellwords.shellescape(str)
+      before do
+    login_as(user)
+  end
+    
+          it 'generates a richer DOT script' do
+        expect(agents_dot(@agents, rich: true)).to match(%r{
+          \A
+          digraph \x20 'Agent \x20 Event \x20 Flow' \{
+            (graph \[ [^\]]+ \];)?
+            node \[ [^\]]+ \];
+            edge \[ [^\]]+ \];
+            (?<foo>\w+) \[label=foo,tooltip='Dot \x20 Foo',URL='#{Regexp.quote(agent_path(@foo))}'\];
+            \k<foo> -> (?<bar1>\w+) \[style=dashed\];
+            \k<foo> -> (?<bar2>\w+) \[color='\#999999'\];
+            \k<bar1> \[label=bar1,tooltip='Dot \x20 Bar',URL='#{Regexp.quote(agent_path(@bar1))}'\];
+            \k<bar2> \[label=bar2,tooltip='Dot \x20 Bar',URL='#{Regexp.quote(agent_path(@bar2))}',style='rounded,dashed',color='\#999999',fontcolor='\#999999'\];
+            \k<bar2> -> (?<bar3>\w+) \[style=dashed,color='\#999999'\];
+            \k<bar3> \[label=bar3,tooltip='Dot \x20 Bar',URL='#{Regexp.quote(agent_path(@bar3))}'\];
+          \}
+          \z
+        }x)
+      end
+    end
+  end
+    
+          a = '        Events will have the fields you specified.  Your options look like:\n\n            {\n      \'url\': {\n        \'css\': \'#comic img\',\n        \'value\': \'@src\'\n      },\n      \'title\': {\n        \'css\': \'#comic img\',\n        \'value\': \'@title\'\n      }\n    }\'\n'
+      expect(Utils.unindent(a)).to eq('Events will have the fields you specified.  Your options look like:\n\n    {\n      \'url\': {\n\'css\': \'#comic img\',\n\'value\': \'@src\'\n      },\n      \'title\': {\n\'css\': \'#comic img\',\n\'value\': \'@title\'\n      }\n    }\'')
+    end
+  end
+    
+      let :new_template do
+    {
+      'description' => '{{ hovertext }}',
+      'comment' => '{{ comment }}'
+    }
+  end
+    
+      def translation_scope
+    'devise.sessions'
+  end
+    
+        if resource.errors.empty?
+      set_flash_message! :notice, :unlocked
+      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
     else
-      # using `escape` instead of expected `shellescape` here
-      # which corresponds to Shellword's `String.shellescape` implementation
-      # https://github.com/ruby/ruby/blob/1cf2bb4b2085758112503e7da7414d1ef52d4f48/lib/shellwords.rb#L216
-      Shellwords.escape(str)
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
     end
   end
-  module_function :shellescape
     
-          # Forgets the given resource by deleting a cookie
-      def forget_me(resource)
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.forget_me!
-        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
+      end
+end
+    
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
+    
+          def template_paths
+        template_path = _prefixes.dup
+        template_path.unshift '#{@devise_mapping.scoped_path}/mailer' if self.class.scoped_views?
+        template_path
       end
     
-          def extract_path_from_location(location)
-        uri = parse_uri(location)
-    
-        if record.timedout?(last_request_at) &&
-        !env['devise.skip_timeout'] &&
-        !proxy.remember_me_is_active?(record)
-      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
-      throw :warden, scope: scope, message: :timeout
+        def default_path_names(options)
+      @path_names = Hash.new { |h,k| h[k] = k.to_s }
+      @path_names[:registration] = ''
+      @path_names.merge!(options[:path_names]) if options[:path_names]
     end
     
-              if recoverable.persisted?
-            if recoverable.reset_password_period_valid?
-              recoverable.reset_password(attributes[:password], attributes[:password_confirmation])
-            else
-              recoverable.errors.add(:reset_password_token, :expired)
+              record = to_adapter.get(id)
+          record if record && record.remember_me?(token, generated_at)
+        end
+    
+      # if rss_url already in existing opml file, use that; otherwise, do a lookup
+  rss_url = nil
+  if File.exist?(OUTPUT_FILENAME)
+    xml = Nokogiri::XML(File.open(OUTPUT_FILENAME))
+    existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first
+    if existing_blog
+      rss_url = existing_blog.attr('xmlUrl')
+      puts '#{name}: ALREADY HAVE'
+    end
+  end
+    
+          yaml_file = 'CocoaPods-version.yml'
+      unless File.exist?(yaml_file)
+        $stderr.puts red('[!] Unable to find #{yaml_file}!')
+        exit 1
+      end
+      require 'yaml'
+      cocoapods_version = YAML.load_file(yaml_file)
+      cocoapods_version['last'] = gem_version
+      File.open(yaml_file, 'w') do |f|
+        f.write(cocoapods_version.to_yaml)
+      end
+    
+    #{stack}
+#{executable_path}
+### Plugins
+    
+                  expect(new_source).to eq(['#{prefix}#{open}#{a},',
+                                        '#{b}#{close}',
+                                        suffix].join($RS))
             end
           end
-    
-        def resource_params
-      params.require(:report_note).permit(
-        :content,
-        :report_id
-      )
-    end
-    
-      def maxheight_or_default
-    params[:maxheight].present? ? params[:maxheight].to_i : nil
-  end
-end
-
-    
-      def set_account
-    @account = Account.find(params[:id])
-  end
-end
-
-    
-      def require_enabled_api!
-    head 404 unless Setting.activity_api_enabled
-  end
-end
-
-    
-      def subscription_params
-    params.require(:subscription).permit(:endpoint, keys: [:auth, :p256dh])
-  end
-    
-      it 'calls #to_path on second argument when passed ?e and a filename' do
-    p = mock('path')
-    p.should_receive(:to_path).and_return @file
-    Kernel.test(?e, p)
-  end
-    
-      it 'sets the return value of the catch block to nil by default' do
-    res = catch :blah do
-      throw :blah
-    end
-    res.should == nil
-  end
-    
-      it 'creates a public method in script binding' do
-    eval @code, script_binding
-    Object.should have_method :boom
-  end
-    
-      # normalize out multiple slashes, directory traversal, and self referrential directories
-  def normalize!(str)
-    i = 0
-    while (str.gsub!(/(\/\.\/|\/\w+\/\.\.\/|\/\/)/,'/')); i += 1; end
-    i
-  end
-    
-        head + [data.length].pack('v') + data
-  end
-    
-              # Encodes the auth_time field
-          #
-          # @return [String]
-          def encode_auth_time
-            [auth_time].pack('N')
-          end
-    
-                decode_asn1(asn1)
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::KdcRequestBody from an String
-          #
-          # @param input [String] the input to decode from
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-              # Decodes a Rex::Proto::Kerberos::Model::LastRequest
-          #
-          # @param input [String, OpenSSL::ASN1::Sequence] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::Sequence
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode LastRequest, invalid input'
-            end
-    
-    @@ login
-<form action='/'>
-  <label for='user'>User Name:</label>
-  <input name='user' value='' />
-  <input type='submit' value='GO!' />
-</form>
-    
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   Cookie Tossing
-    # Supported browsers:: all
-    # More infos::         https://github.com/blog/1466-yummy-cookies-across-domains
-    #
-    # Does not accept HTTP requests if the HTTP_COOKIE header contains more than one
-    # session cookie. This does not protect against a cookie overflow attack.
-    #
-    # Options:
-    #
-    # session_key:: The name of the session cookie (default: 'rack.session')
-    class CookieTossing < Base
-      default_reaction :deny
-    
-          def escape(object)
-        case object
-        when Hash   then escape_hash(object)
-        when Array  then object.map { |o| escape(o) }
-        when String then escape_string(object)
-        when Tempfile then object
-        else nil
         end
       end
-    
-      it 'accepts post form requests with masked authenticity_token field' do
-    post('/', {'authenticity_token' => masked_token}, 'rack.session' => session)
-    expect(last_response).to be_ok
-  end
-    
-      it 'should allow changing report only' do
-    # I have no clue what other modes are available
-    mock_app do
-      use Rack::Protection::ContentSecurityPolicy, :report_uri => '/my_amazing_csp_report_parser', :report_only => true
-      run DummyApp
     end
     
-      def render(context)
-    config_tag(context.registers[:site].config, @key, @tag, @classname)
+            expect(cop.highlights).to eq([close])
+        expect(cop.messages)
+          .to eq([described_class::SAME_LINE_MESSAGE])
+      end
+    
+            when_branches.each do |condition|
+          yield condition
+        end
+    
+          # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
+      #
+      # In this case, the `def` node destructures into:
+      #
+      #   `method_name, arguments, body`
+      #
+      # while the `defs` node destructures into:
+      #
+      #   `receiver, method_name, arguments, body`
+      #
+      # so we reverse the destructured array to get the optional receiver
+      # at the end, where it can be discarded.
+      #
+      # @return [Array] the different parts of the `def` or `defs` node
+      def node_parts
+        to_a.reverse
+      end
+    end
   end
 end
+
     
-    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
+          # Checks whether this node body is a void context.
+      # Always `true` for `for`.
+      #
+      # @return [true] whether the `for` node body is a void context
+      def void_context?
+        true
+      end
+    
+          # Calls the given block for each condition node in the `when` branch.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_condition
+        return conditions.to_enum(__method__) unless block_given?
