34e6d5c9f6e5d668bf4a46429647f9977c311782
SUNsung <weatherius@gmail.com>
2019-01-09 10:29:01 +0200

    Rewrite [40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp] File in: 2019-01-09 10:29:01

--- cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
+++ cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
@@ -1,170 +1,167 @@
 
         
-        // Generate destructors.
-#include 'ipc/struct_destructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
-    
-    #endif  // CONTENT_NW_SRC_API_APP_APP_H_
-    
-    class Clipboard : public Base {
- public:
-  Clipboard(int id,
-            const base::WeakPtr<DispatcherHost>& dispatcher_host,
-            const base::DictionaryValue& option);
-  ~Clipboard() override;
+          // generic template, for CodeAddress and Label
+  template <typename T>
+  void call(T& target, CallArg ca = CallArg::Internal) {
+    if (CallArg::Internal == ca) {
+      // tries best performance possible
+      branchAuto(target, BranchConditions::Always, LinkReg::Save, true);
+    } else {
+      // branchFar is not only smashable but also it will use r12 so an
+      // external branch can correctly set its TOC address appropriately.
+      branchFar(target, BranchConditions::Always, LinkReg::Save,
+                ImmType::TocOnly, true);
     }
-    
-    
-    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
-    return false;
-  return !item->icon_.IsEmpty();
-}
-    
-    
-    {#endif
-}  // namespace nwapi
-    
-    NwObjCallObjectMethodSyncFunction::NwObjCallObjectMethodSyncFunction() {
-}
-    
-        base::LazyInstance<NwDesktopCaptureMonitor>::Leaky
-      g_desktop_capture_monitor = LAZY_INSTANCE_INITIALIZER;
-    
-    template <$for j, [[typename Generator$j]]>
-internal::CartesianProductHolder$i<$for j, [[Generator$j]]> Combine(
-    $for j, [[const Generator$j& g$j]]) {
-  return internal::CartesianProductHolder$i<$for j, [[Generator$j]]>(
-      $for j, [[g$j]]);
-}
-    
-    
-    {  // The name of the source file where the test part took place, or
-  // '' if the source file is unknown.
-  std::string file_name_;
-  // The line in the source file where the test part took place, or -1
-  // if the line number is unknown.
-  int line_number_;
-  std::string summary_;  // The test failure summary.
-  std::string message_;  // The test failure message.
-};
-    
-    
-    {  return AssertionFailure() << pred_text << '('
-                            << e1 << ', '
-                            << e2 << ', '
-                            << e3 << ', '
-                            << e4 << ') evaluates to false, where'
-                            << '\n' << e1 << ' evaluates to ' << v1
-                            << '\n' << e2 << ' evaluates to ' << v2
-                            << '\n' << e3 << ' evaluates to ' << v3
-                            << '\n' << e4 << ' evaluates to ' << v4;
-}
-    
-      // Returns a copy of the FilePath with the case-insensitive extension removed.
-  // Example: FilePath('dir/file.exe').RemoveExtension('EXE') returns
-  // FilePath('dir/file'). If a case-insensitive extension is not
-  // found, returns a copy of the original FilePath.
-  FilePath RemoveExtension(const char* extension) const;
-    
-    template <class Generator1, class Generator2, class Generator3,
-    class Generator4, class Generator5, class Generator6, class Generator7>
-class CartesianProductHolder7 {
- public:
-CartesianProductHolder7(const Generator1& g1, const Generator2& g2,
-    const Generator3& g3, const Generator4& g4, const Generator5& g5,
-    const Generator6& g6, const Generator7& g7)
-      : g1_(g1), g2_(g2), g3_(g3), g4_(g4), g5_(g5), g6_(g6), g7_(g7) {}
-  template <typename T1, typename T2, typename T3, typename T4, typename T5,
-      typename T6, typename T7>
-  operator ParamGenerator< ::std::tr1::tuple<T1, T2, T3, T4, T5, T6,
-      T7> >() const {
-    return ParamGenerator< ::std::tr1::tuple<T1, T2, T3, T4, T5, T6, T7> >(
-        new CartesianProductGenerator7<T1, T2, T3, T4, T5, T6, T7>(
-        static_cast<ParamGenerator<T1> >(g1_),
-        static_cast<ParamGenerator<T2> >(g2_),
-        static_cast<ParamGenerator<T3> >(g3_),
-        static_cast<ParamGenerator<T4> >(g4_),
-        static_cast<ParamGenerator<T5> >(g5_),
-        static_cast<ParamGenerator<T6> >(g6_),
-        static_cast<ParamGenerator<T7> >(g7_)));
-  }
-    }
-    
-    
-// This sample shows how to write a simple unit test for a function,
-// using Google C++ testing framework.
-//
-// Writing a unit test using Google C++ testing framework is easy as 1-2-3:
-    
-    #include 'sample2.h'
-    
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-    
-    TEST(CorruptionTest, RecoverWriteError) {
-  env_.writable_file_error_ = true;
-  Status s = TryReopen();
-  ASSERT_TRUE(!s.ok());
-}
-    
-    TEST(LogTest, ReadInitialOffsetIntoBlockPadding) {
-  CheckInitialOffsetRecord(3 * log::kBlockSize - 3, 5);
-}
-    
-    
-    {#if !defined(NDEBUG)
-    snapshot->list_ = this;
-#endif  // !defined(NDEBUG)
-    snapshot->next_ = &head_;
-    snapshot->prev_ = head_.prev_;
-    snapshot->prev_->next_ = snapshot;
-    snapshot->next_->prev_ = snapshot;
-    return snapshot;
+    callEpilogue(ca);
   }
     
-        void Trainer::Save(const std::wstring& modelFilePath, const std::vector<DictionaryValue>& learnerState, const Dictionary& externalState, const Dictionary& distributedState)
-    {
-        std::wstring tempModelFile = modelFilePath + L'.tmp';
-        Dictionary state;
-        state[versionPropertyName] = trainerCheckpointVersion;
-        state[learnersPropertyName] = learnerState;
-        state[externalStatePropertyName] = externalState;
-        state[distributedStatePropertyName] = distributedState;
+    //////////////////////////////////////////////////////////////////////
+    
+      static const APCCollection* fromHandle(const APCHandle* handle) {
+    assertx(handle->checkInvariants());
+    assertx(handle->kind() == APCKind::SharedCollection);
+    static_assert(offsetof(APCCollection, m_handle) == 0, '');
+    return reinterpret_cast<const APCCollection*>(handle);
+  }
+    
+      if (comma != data) {
+    // we have meta
+    ssize_t meta_len = comma - data;
+    data_len -= meta_len;
+    char* semi = (char*)memchr(data, ';', meta_len);
+    char* slash = (char*)memchr(data, '/', meta_len);
     }
     
-    // Rand based on Mersenne Twister.
-// We use our own distribution in order to match baselines between different operating systems,
-// instead of using std::shuffle which uses unitform_distribution internally.
-// TODO: Switching to Boost would eliminate this problem.
-template <typename TVector>
-inline void RandomShuffleMT(TVector& v, size_t begin, size_t end, std::mt19937_64& rng)
+    #include <sys/types.h>
+    
+      // implementing File
+  bool open(const String& filename, const String& mode) override;
+  bool close() override;
+  int64_t readImpl(char *buffer, int64_t length) override;
+  int getc() override;
+  int64_t writeImpl(const char *buffer, int64_t length) override;
+  bool seek(int64_t offset, int whence = SEEK_SET) override;
+  int64_t tell() override;
+  bool eof() override;
+  bool rewind() override;
+  bool flush() override;
+  bool truncate(int64_t size) override;
+    
+    void record_perf_mem_event(PerfEvent kind, const perf_event_sample* sample);
+    
+    #include 'hphp/runtime/base/perf-warning-inl.h'
+    
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+        /* Sets the Eye value of the Camera. 
+     * 
+     * @param eye The Eye value of the Camera.
+     * @js NA
+     */
+    void setEye(const Vec3 &eye);
+    void setEye(float x, float y, float z);
+    /* Returns the Eye value of the Camera. 
+     *
+     * @return The Eye value of the Camera.
+     * @js NA
+     */
+    const Vec3& getEye() const { return _eye; }
+    /* Sets the Center value of the Camera. 
+     *
+     * @param center The Center value of the Camera.
+     * @js NA
+     */
+    void setCenter(const Vec3 &center);
+    /* Returns the Center value of the Camera. 
+     *
+     * @return The Center value of the Camera.
+     * @js NA
+     */
+    const Vec3& getCenter() const { return _center; }
+    /* Sets the Up value of the Camera. 
+     *
+     * @param up The Up value of the Camera.
+     * @js NA
+     */
+    void setUp(const Vec3 &up);
+    /* Returns the Up value of the Camera. 
+     *
+     * @return The Up value of the Camera.
+     * @js NA
+     */
+    const Vec3& getUp() const { return _up; }
+    
+    
+/** @class CallFunc
+* @brief Calls a 'callback'.
+*/
+class CC_DLL CallFunc : public ActionInstant
 {
-    for(size_t i = begin; i < end; ++i)
-    {
-        const size_t randomLocation = RandMT(i, end, rng);
-        std::swap(v[i], v[randomLocation]);
+public:
+    /** Creates the action with the callback of type std::function<void()>.
+     This is the preferred way to create the callback.
+     * When this function bound in js or lua ,the input param will be changed.
+     * In js: var create(var func, var this, var [data]) or var create(var func).
+     * In lua:local create(local funcID).
+     *
+     * @param func  A callback function need to be executed.
+     * @return  An autoreleased CallFunc object.
+     */
+    static CallFunc * create(const std::function<void()>& func);
     }
+    
+    
+    /** Returns the numbers of actions that are running in a
+     *  certain target with a specific tag.
+     * Like getNumberOfRunningActionsInTarget Composable actions
+     * are counted as 1 action. Example:
+     * - If you are running 1 Sequence of 7 actions, it will return 1.
+     * - If you are running 7 Sequences of 2 actions, it will return 7.
+     *
+     * @param target    A certain target.
+     * @param tag       Tag that will be searched.
+     * @return  The numbers of actions that are running in a certain target
+     *          with a specific tag.
+     * @see getNumberOfRunningActionsInTarget
+     * @js NA
+     */
+    virtual size_t getNumberOfRunningActionsInTargetByTag(const Node *target, int tag);
+    
+     Another example: ScaleTo action could be rewritten using PropertyAction:
+    
+    #endif // __CC_ANIMATION_H__
+
+    
+    /**
+ * @addtogroup _2d
+ * @{
+ */
+    }
+    
+    TEST(ByteTest, GetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  EXPECT_EQ(0x05, value.get_byte(1, 3));
+  EXPECT_EQ(0x01, value.get_byte(1, 1));
+  EXPECT_EQ(0x00, value.get_byte(8, 1));
+  EXPECT_EQ(0x00, value.get_byte(-1, 1));
+  EXPECT_EQ(0x1A, value.get_byte(0, 10));
 }
     
-            // add to set
-        let wasAdded = AddNodeToNetIfNotYet(node, /*makeUniqueName=*/ true);
-        if (!wasAdded) // node already there (above will fail if there is a different node with the same name)
-            continue;
+    BaseMapMatrix::BaseMapMatrix(const BaseMapMatrix& cells) {}
     
-        virtual void /*ComputationNodeNonLooping::*/ ForwardPropNonLooping() override
-    {
-        FrameRange fr(InputRef(0).GetMBLayout());
-        InputRef(0).ValueFor(fr).VectorMax(*m_maxIndexes0, *m_maxValues, true);
-        InputRef(1).ValueFor(fr).VectorMax(*m_maxIndexes1, *m_maxValues, true, m_topK);
-        MaskMissingColumnsToZero(*m_maxIndexes0, InputRef(0).GetMBLayout(), fr);
-        MaskMissingColumnsToZero(*m_maxIndexes1, InputRef(1).GetMBLayout(), fr);
-        Value().AssignNumOfDiff(*m_maxIndexes0, *m_maxIndexes1, m_topK > 1);
-#if NANCHECK
-        Value().HasNan('ClassificationError');
-#endif
-#if DUMPOUTPUT
-        Value().Print('ClassificationErrorNode');
-#endif
+    class RouteSegmentsTest : public ::testing::Test {
+ public:
+  static void SetUpTestCase() {
+    AINFO << 'map file: ' << FLAGS_test_map_file;
+    if (hdmap_.LoadMapFromFile(FLAGS_test_map_file) != 0) {
+      AERROR << 'Failed to load map: ' << FLAGS_test_map_file;
+      CHECK(false);
     }
+  }
+    }
+    
+    double Spline1dSeg::ThirdOrderDerivative(const double x) const {
+  return third_order_derivative_(x);
+}
