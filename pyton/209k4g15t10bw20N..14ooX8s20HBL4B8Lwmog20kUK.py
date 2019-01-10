82e21a1787c5f551da3213b896e2dcfe240a24de
SUNsung <weatherius@gmail.com>
2019-01-10 09:47:59 +0200

    Rewrite [209k4g15t10bw20N..14ooX8s20HBL4B8Lwmog20kUK.py] File in: 2019-01-10 09:47:58

--- pyton/209k4g15t10bw20N..14ooX8s20HBL4B8Lwmog20kUK.py
+++ pyton/209k4g15t10bw20N..14ooX8s20HBL4B8Lwmog20kUK.py
@@ -1,67 +1,211 @@
 
         
-            def set(self, results, query):
-        '''Set the result for the given query key in the cache.
+        
+def diag_gaussian_log_likelihood(z, mu=0.0, logvar=0.0):
+  '''Log-likelihood under a Gaussian distribution with diagonal covariance.
+    Returns the log-likelihood for each dimension.  One should sum the
+    results for the log-likelihood under the full multidimensional model.
     
-    from mrjob.job import MRJob
+        return S_bxtxd
     
-        def __init__(self):
-        self.people = {}  # key: person_id, value: person
+    P_nxn = rng.randn(N,N) / np.sqrt(N)  # random projections
     
-        def __init__(self, size):
-        self.size = size
-        self.table = [[] for _ in range(self.size)]
+    rates_b, x0s_b, _ = generate_data(rnn_b, T=T, E=E, x0s=x0s, P_sxn=P_nxn,
+                                  input_magnitude=0.0, input_times=None)
+spikes_b = spikify_data(rates_b, rng, rnn_b['dt'], rnn_b['max_firing_rate'])
     
-            Accessing a node updates its position to the front of the LRU list.
-        '''
-        node = self.lookup[query]
-        if node is None:
-            return None
-        self.linked_list.move_to_front(node)
-        return node.results
+      This function assumes the dataset_dict is a mapping ( string ->
+  to data_dict ).  It calls write_data for each data dictionary,
+  post-fixing the data filename with the key of the dataset.
     
-        def __init__(self, db):
-        self.db = db
-        pass
-    
-      if input_magnitude > 0.0:
-    # time of 'hits' randomly chosen between [1/4 and 3/4] of total time
-    input_times = rng.choice(int(ntime_steps/2), size=[E]) + int(ntime_steps/4)
-  else:
-    input_times = None
+        # Add 'lstm/lstm_0/control_dependency' if you want to dump previous layer
+    # LSTM.
+    lstm_emb = sess.run(t['lstm/lstm_1/control_dependency'],
+                        feed_dict={t['char_inputs_in']: char_ids_inputs,
+                                   t['inputs_in']: inputs,
+                                   t['targets_in']: targets,
+                                   t['target_weights_in']: weights})
     
     
-class IntegrationToBoundModel:
-  def __init__(self, N):
-    scale = 0.8 / float(N**0.5)
-    self.N = N
-    self.Wh_nxn = tf.Variable(tf.random_normal([N, N], stddev=scale))
-    self.b_1xn = tf.Variable(tf.zeros([1, N]))
-    self.Bu_1xn = tf.Variable(tf.zeros([1, N]))
-    self.Wro_nxo = tf.Variable(tf.random_normal([N, 1], stddev=scale))
-    self.bro_o = tf.Variable(tf.zeros([1]))
+_SPECIAL_CHAR_MAP = {
+    '\xe2\x80\x98': '\'',
+    '\xe2\x80\x99': '\'',
+    '\xe2\x80\x9c': ''',
+    '\xe2\x80\x9d': ''',
+    '\xe2\x80\x93': '-',
+    '\xe2\x80\x94': '-',
+    '\xe2\x88\x92': '-',
+    '\xce\x84': '\'',
+    '\xc2\xb4': '\'',
+    '`': '\''
+}
     
-        if np.isnan(log_perp):
-      sys.stderr.error('log_perplexity is Nan.\n')
-    else:
-      sum_num += log_perp * weights.mean()
-      sum_den += weights.mean()
-    if sum_den > 0:
-      perplexity = np.exp(sum_num / sum_den)
     
-      # Load data set.
-  if FLAGS.data_set == 'ptb':
-    raw_data = ptb_loader.ptb_raw_data(FLAGS.data_dir)
-    train_data, valid_data, _, _ = raw_data
-  elif FLAGS.data_set == 'imdb':
-    raw_data = imdb_loader.imdb_raw_data(FLAGS.data_dir)
-    train_data, valid_data = raw_data
-  else:
-    raise NotImplementedError
+def imdb_raw_data(data_path=None):
+  '''Load IMDB raw data from data directory 'data_path'.
+  Reads IMDB tf record files containing integer ids,
+  and performs mini-batching of the inputs.
+  Args:
+    data_path: string path to the directory where simple-examples.tgz has
+      been extracted.
+  Returns:
+    tuple (train_data, valid_data)
+    where each of the data objects can be passed to IMDBIterator.
+  '''
     
-        # rnn_vd derived from the same code base as rnn_zaremba.
-    elif (FLAGS.discriminator_model == 'rnn_zaremba' or
-          FLAGS.discriminator_model == 'rnn_vd'):
-      dis_variable_maps = variable_mapping.rnn_zaremba(hparams, model='dis')
-      dis_init_saver = tf.train.Saver(var_list=dis_variable_maps)
-      init_savers['dis_init_saver'] = dis_init_saver
+    
+def create_dis_pretrain_op(hparams, dis_loss, global_step):
+  '''Create a train op for pretraining.'''
+  with tf.name_scope('pretrain_generator'):
+    optimizer = tf.train.AdamOptimizer(hparams.dis_pretrain_learning_rate)
+    dis_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('dis')
+    ]
+    if FLAGS.dis_update_share_embedding and FLAGS.dis_share_embedding:
+      shared_embedding = [
+          v for v in tf.trainable_variables()
+          if v.op.name == 'gen/decoder/rnn/embedding'
+      ][0]
+      dis_vars.append(shared_embedding)
+    dis_grads = tf.gradients(dis_loss, dis_vars)
+    dis_grads_clipped, _ = tf.clip_by_global_norm(dis_grads,
+                                                  FLAGS.grad_clipping)
+    dis_pretrain_op = optimizer.apply_gradients(
+        zip(dis_grads_clipped, dis_vars), global_step=global_step)
+    return dis_pretrain_op
+    
+      ## Attention.
+  if FLAGS.attention_option is not None:
+    decoder_attention_keys = [
+        v for v in tf.trainable_variables()
+        if v.op.name == 'dis/decoder/attention_keys/weights'
+    ][0]
+    decoder_attention_construct_weights = [
+        v for v in tf.trainable_variables()
+        if v.op.name == 'dis/decoder/rnn/attention_construct/weights'
+    ][0]
+    
+        If `None` is supplied it will act as an identity function, which
+    might be wanted when using the RNNCell `OutputProjectionWrapper`.
+    
+                elif opt in ('-V', '--version'):
+                # Display version.
+                log.println('you-get:', log.BOLD)
+                log.println('    version:  {}'.format(__version__))
+                if head is not None:
+                    log.println('    branch:   {}\n    commit:   {}'.format(*head))
+                else:
+                    log.println('    branch:   {}\n    commit:   {}'.format('(stable)', '(tag v{})'.format(__version__)))
+    
+            link_list = self.get_streams_by_id(account_number, video_id)
+    
+        ep = 'http://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={}'
+    
+    __all__ = ['coub_download']
+    
+    __all__ = ['facebook_download']
+    
+    __all__ = ['funshion_download']
+    
+    site_info = 'Google.com'
+download = google_download
+download_playlist = playlist_not_supported('google')
+
+    
+        def _colision_resolution(self, key, data=None):
+        new_key = self.hash_function(key + 1)
+    
+    	return memo[m][m-1]
+    
+    def longestSub(ARRAY): 			#This function is recursive
+	
+	ARRAY_LENGTH = len(ARRAY)
+	if(ARRAY_LENGTH <= 1):  	#If the array contains only one element, we return it (it's the stop condition of recursion)
+		return ARRAY
+								#Else
+	PIVOT=ARRAY[0]
+	isFound=False
+	i=1
+	LONGEST_SUB=[]
+	while(not isFound and i<ARRAY_LENGTH):
+		if (ARRAY[i] < PIVOT):
+			isFound=True
+			TEMPORARY_ARRAY = [ element for element in ARRAY[i:] if element >= ARRAY[i] ]
+			TEMPORARY_ARRAY = longestSub(TEMPORARY_ARRAY)
+			if ( len(TEMPORARY_ARRAY) > len(LONGEST_SUB) ):
+				LONGEST_SUB = TEMPORARY_ARRAY
+		else:
+			i+=1
+    
+    
+class CompositeGraphic(Graphic):
+    def __init__(self):
+        self.graphics = []
+    
+    
+class NotSpecification(CompositeSpecification):
+    _wrapped = Specification()
+    
+        for action in actions:
+        action()
+    
+    *What does this example do?
+In this example queue.Queue is used to create the pool (wrapped in a
+custom ObjectPool object to use with the with statement), and it is
+populated with strings.
+As we can see, the first string object put in 'yam' is USED by the
+with statement. But because it is released back into the pool
+afterwards it is reused by the explicit call to sample_queue.get().
+Same thing happens with 'sam', when the ObjectPool created insided the
+function is deleted (by the GC) and the object is returned.
+    
+    parser = ArgumentParser(usage='%s -m jieba [options] filename' % sys.executable, description='Jieba command line interface.', epilog='If no filename specified, use STDIN instead.')
+parser.add_argument('-d', '--delimiter', metavar='DELIM', default=' / ',
+                    nargs='?', const=' ',
+                    help='use DELIM instead of ' / ' for word delimiter; or a space if it is used without DELIM')
+parser.add_argument('-p', '--pos', metavar='DELIM', nargs='?', const='_',
+                    help='enable POS tagging; if DELIM is specified, use DELIM instead of '_' for POS delimiter')
+parser.add_argument('-D', '--dict', help='use DICT as dictionary')
+parser.add_argument('-u', '--user-dict',
+                    help='use USER_DICT together with the default dictionary or DICT (if specified)')
+parser.add_argument('-a', '--cut-all',
+                    action='store_true', dest='cutall', default=False,
+                    help='full pattern cutting (ignored with POS tagging)')
+parser.add_argument('-n', '--no-hmm', dest='hmm', action='store_false',
+                    default=True, help='don't use the Hidden Markov Model')
+parser.add_argument('-q', '--quiet', action='store_true', default=False,
+                    help='don't print loading messages to stderr')
+parser.add_argument('-V', '--version', action='version',
+                    version='Jieba ' + jieba.__version__)
+parser.add_argument('filename', nargs='?', help='input file')
+    
+        def __call__(self, text, **kargs):
+        words = jieba.tokenize(text, mode='search')
+        token = Token()
+        for (w, start_pos, stop_pos) in words:
+            if not accepted_chars.match(w) and len(w) <= 1:
+                continue
+            token.original = token.text = w
+            token.pos = start_pos
+            token.startchar = start_pos
+            token.endchar = stop_pos
+            yield token
+    
+    
+if len(args) < 1:
+    print(USAGE)
+    sys.exit(1)
+    
+    file_name = args[0]
+    
+    UDF = 0  # undefined
+OTH = 1  # other
+ASC = 2  # ascii capital letter
+ASS = 3  # ascii small letter
+ACV = 4  # accent capital vowel
+ACO = 5  # accent capital other
+ASV = 6  # accent small vowel
+ASO = 7  # accent small other
+CLASS_NUM = 8  # total classes
+    
+            return self.state
