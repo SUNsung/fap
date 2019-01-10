cf4dbfa423aea9c9d7c66dd93b319836a95b4054
SUNsung <weatherius@gmail.com>
2019-01-10 19:27:40 +0200

    Rewrite [241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb] File in: 2019-01-10 19:27:39

--- ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
+++ ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
@@ -1,106 +1,69 @@
 
         
-          private
+        
+end
+end
+end
+
     
-      def search_tap(user, repo, rx)
-    if (HOMEBREW_LIBRARY/'Taps/#{user.downcase}/homebrew-#{repo.downcase}').directory? && \
-       user != 'Caskroom'
-      return []
-    end
+              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
+          #
+          # @return [String] encoded principal
+          def encode
+            encoded = ''
+            encoded << encode_name_type
+            encoded << [components.length].pack('N')
+            encoded << encode_realm
+            encoded << encode_components
     
-      def std_cmake_parameters
-    '-DCMAKE_INSTALL_PREFIX='#{prefix}' -DCMAKE_BUILD_TYPE=None -DCMAKE_FIND_FRAMEWORK=LAST -Wno-dev'
-  end
+              # Encodes the pvno field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_pvno
+            bn = OpenSSL::BN.new(pvno.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
     
-        Thread.pass until running
-    Thread.pass while t.status and t.status != 'sleep'
-    
-      it 'defaults number to a random value' do
-    lambda { srand }.should_not raise_error
-    srand.should_not == 0
-  end
-    
-    describe :kernel_system, shared: true do
-  it 'executes the specified command in a subprocess' do
-    lambda { @object.system('echo a') }.should output_to_fd('a\n')
-    
-      it 'has no effect on immediate values' do
-    [nil, true, false].each do |v|
-      v.taint
-      v.tainted?.should == false
-    end
-  end
-    
-      it 'is a private method' do
-    Kernel.should have_private_instance_method(:warn)
-  end
-    
-          it 'detects closing brace on different line from last element' do
-        src = construct(true, true)
-        inspect_source(src)
-        expect(cop.offenses.size).to eq(1)
-        expect(cop.highlights).to eq([close])
-        expect(cop.messages).to eq([described_class::ALWAYS_SAME_LINE_MESSAGE])
-      end
-    
-        context 'and a comment after the last element' do
-      let(:b_comment) { ' # comment b' }
-    
-    public_dir      = 'public'    # compiled site directory
-source_dir      = 'source'    # source file directory
-blog_index_dir  = 'source'    # directory for your blog's index page (if you put your index in source/blog/index.html, set this to 'source/blog')
-deploy_dir      = '_deploy'   # deploy directory (for Github pages deployment)
-stash_dir       = '_stash'    # directory to stash posts for speedy generation
-posts_dir       = '_posts'    # directory for blog files
-themes_dir      = '.themes'   # directory for blog files
-new_post_ext    = 'markdown'  # default new post file extension when using the new_post task
-new_page_ext    = 'markdown'  # default new page file extension when using the new_page task
-server_port     = '4000'      # port for preview server eg. localhost:4000
-    
-        # Outputs a list of categories as comma-separated <a> links. This is used
-    # to output the category list for each post on a category page.
-    #
-    #  +categories+ is the list of categories to format.
-    #
-    # Returns string
-    #
-    def category_links(categories)
-      categories.sort.map { |c| category_link c }.join(', ')
-    end
-    
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
-          end
-        end
-      end
-      rtn
-    end
-  end
-    
-        module PsychAutoload
-      def resolve_class(klass_name)
-        return nil if !klass_name || klass_name.empty?
-        # constantize
-        names = klass_name.split('::')
-        names.shift if names.empty? || names.first.empty?
-    
-    
-    # By leaving this as a class method, it can be pluggable and used by the Manager actor. Making it
-    # an instance method will make it async to the Fetcher actor
-    def self.bulk_requeue(inprogress, options)
-      return if inprogress.empty?
-    
-      # Default to fake testing to keep old behavior
-  Sidekiq::Testing.fake!
-    
-        def hostname
-      ENV['DYNO'] || Socket.gethostname
-    end
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        class EncKdcResponse < Element
+          # @!attribute key
+          #   @return [Rex::Proto::Kerberos::Model::EncryptionKey] The session key
+          attr_accessor :key
+          # @!attribute last_req
+          #   @return [Array<Rex::Proto::Kerberos::Model::LastRequest>] This field is returned by the KDC and specifies the time(s)
+          #   of the last request by a principal
+          attr_accessor :last_req
+          # @!attribute nonce
+          #   @return [Integer] random number
+          attr_accessor :nonce
+          # @!attribute key_expiration
+          #   @return [Time] The key-expiration field is part of the response from the
+          #   KDC and specifies the time that the client's secret key is due to expire
+          attr_accessor :key_expiration
+          # @!attribute flags
+          #   @return [Integer] This field indicates which of various options were used or
+          #   requested when the ticket was issued
+          attr_accessor :flags
+          # @!attribute auth_time
+          #   @return [Time] the time of initial authentication for the named principal
+          attr_accessor :auth_time
+          # @!attribute start_time
+          #   @return [Time] Specifies the time after which the ticket is valid
+          attr_accessor :start_time
+          # @!attribute end_time
+          #   @return [Time] This field contains the time after which the ticket will
+          #   not be honored (its expiration time)
+          attr_accessor :end_time
+          # @!attribute renew_till
+          #   @return [Time] This field is only present in tickets that have the
+          #   RENEWABLE flag set in the flags field.  It indicates the maximum
+          #   endtime that may be included in a renewal
+          attr_accessor :renew_till
+          # @!attribute srealm
+          #   @return [String] The realm part of the server's principal identifier
+          attr_accessor :srealm
+          # @!attribute sname
+          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
+          attr_accessor :sname
