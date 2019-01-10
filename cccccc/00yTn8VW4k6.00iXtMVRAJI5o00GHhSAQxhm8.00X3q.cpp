dcbbd78cfa1646c130720767ecad5bf4e0eaecd6
SUNsung <weatherius@gmail.com>
2019-01-10 13:26:02 +0200

    Rewrite [00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp] File in: 2019-01-10 13:26:01

--- cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
+++ cccccc/00yTn8VW4k6.00iXtMVRAJI5o00GHhSAQxhm8.00X3q.cpp
@@ -1,159 +1,155 @@
 
         
-        namespace b2ConvexDecomp {
-    }
-    
-    #ifndef B2_TRIANGLE_H
-#define B2_TRIANGLE_H
-    
-    1. The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation would be appreciated but is not required.
-2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
-3. This notice may not be removed or altered from any source distribution.
-*/
-    
-    inline b3Quat b3QuatInvert(b3QuatConstArg q)
-{
-	return (b3Quat)(-q.xyz, q.w);
-}
-    
-    public:
-	b3FixedConstraint(int  rbA,int rbB, const b3Transform& frameInA,const b3Transform& frameInB);
-	
-	virtual ~b3FixedConstraint();
+        // ##########################################################
     
     
-    
-    
-    
-    static inline void b3SolveContact(b3ContactConstraint4& cs, 
-	const b3Vector3& posA, b3Vector3& linVelA, b3Vector3& angVelA, float invMassA, const b3Matrix3x3& invInertiaA,
-	const b3Vector3& posB, b3Vector3& linVelB, b3Vector3& angVelB, float invMassB, const b3Matrix3x3& invInertiaB, 
-	float maxRambdaDt[4], float minRambdaDt[4])
-{
-    }
-    
-      // Get initial measurement of the space we will be reading.
-  const int64_t initial_size = Size(Key(0), Key(n));
-  const int64_t initial_other_size = Size(Key(n), Key(kCount));
-    
-      void Open() {
-    assert(db_ == nullptr);
-    Options options;
-    options.env = g_env;
-    options.create_if_missing = !FLAGS_use_existing_db;
-    options.block_cache = cache_;
-    options.write_buffer_size = FLAGS_write_buffer_size;
-    options.max_file_size = FLAGS_max_file_size;
-    options.block_size = FLAGS_block_size;
-    options.max_open_files = FLAGS_open_files;
-    options.filter_policy = filter_policy_;
-    options.reuse_logs = FLAGS_reuse_logs;
-    Status s = DB::Open(options, FLAGS_db, &db_);
-    if (!s.ok()) {
-      fprintf(stderr, 'open error: %s\n', s.ToString().c_str());
-      exit(1);
-    }
-  }
-    
-    
-    {  if (s.ok()) {
-    // Commit to the new state
-    imm_->Unref();
-    imm_ = nullptr;
-    has_imm_.Release_Store(nullptr);
-    DeleteObsoleteFiles();
-  } else {
-    RecordBackgroundError(s);
-  }
-}
-    
-    #include <deque>
-#include <set>
-#include 'db/dbformat.h'
-#include 'db/log_writer.h'
-#include 'db/snapshot.h'
-#include 'leveldb/db.h'
-#include 'leveldb/env.h'
-#include 'port/port.h'
-#include 'port/thread_annotations.h'
-    
-    struct ParsedInternalKey {
-  Slice user_key;
-  SequenceNumber sequence;
-  ValueType type;
-    }
-    
-    // Return the name of the descriptor file for the db named by
-// 'dbname' and the specified incarnation number.  The result will be
-// prefixed with 'dbname'.
-std::string DescriptorFileName(const std::string& dbname, uint64_t number);
-    
-    static const int kBlockSize = 32768;
-    
-    namespace log {
-    }
-    
-      // If memtable contains a value for key, store it in *value and return true.
-  // If memtable contains a deletion for key, store a NotFound() error
-  // in *status and return true.
-  // Else, return false.
-  bool Get(const LookupKey& key, std::string* value, Status* s);
-    
-      virtual bool isReply() const CXX11_OVERRIDE;
-    
-    bool DHTRoutingTable::addGoodNode(const std::shared_ptr<DHTNode>& node)
-{
-  return addNode(node, true);
-}
-    
-    #include <string>
-#include <vector>
-#include <memory>
-    
-        taskFactory->setLocalNode(localNode);
-    taskFactory->setRoutingTable(routingTable.get());
-    taskFactory->setMessageDispatcher(dispatcher.get());
-    taskFactory->setMessageFactory(factory.get());
-    taskFactory->setTaskQueue(taskQueue.get());
-    taskFactory->setTimeout(std::chrono::seconds(messageTimeout));
-    
-      virtual void preProcess() CXX11_OVERRIDE;
-    
-      // these three metods are querying the state of the WriteController
-  bool IsStopped() const;
-  bool NeedsDelay() const { return total_delayed_.load() > 0; }
-  bool NeedSpeedupCompaction() const {
-    return IsStopped() || NeedsDelay() || total_compaction_pressure_ > 0;
-  }
-  // return how many microseconds the caller needs to sleep after the call
-  // num_bytes: how many number of bytes to put into the DB.
-  // Prerequisite: DB mutex held.
-  uint64_t GetDelay(Env* env, uint64_t num_bytes);
-  void set_delayed_write_rate(uint64_t write_rate) {
-    // avoid divide 0
-    if (write_rate == 0) {
-      write_rate = 1u;
-    } else if (write_rate > max_delayed_write_rate()) {
-      write_rate = max_delayed_write_rate();
-    }
-    delayed_write_rate_ = write_rate;
-  }
-    
-    
-    {  return 0;
-}
+    {
+    {SHOULD_NOT_DO_GRADIENT(FindDuplicateElements);
+} // namespace
+} // namespace caffe2
 
     
-      // Create column family, and rocksdb will persist the options.
-  ColumnFamilyHandle* cf;
-  s = db->CreateColumnFamily(ColumnFamilyOptions(), 'new_cf', &cf);
-  assert(s.ok());
     
-      // Time spent on file fsync.
-  uint64_t file_fsync_nanos;
+    {  bool RunOnDevice() override {
+    for (Blob* output : OperatorBase::Outputs()) {
+      output->Reset();
+    }
+    return true;
+  }
+};
     
-    class FlushBlockPolicyFactory {
- public:
-  // Return the name of the flush block policy.
-  virtual const char* Name() const = 0;
+    OPERATOR_SCHEMA(GivenTensorDoubleFill)
+    .NumInputs(0, 1)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .Arg(
+        'values',
+        'The value for the elements of the output tensor.',
+        true /* required */)
+    .Arg(
+        'shape',
+        'The shape of the output tensor.'
+        'Cannot set the shape argument and pass in an input at the same time.')
+    .Arg(
+        'extra_shape',
+        'The additional dimensions appended at the end of the shape indicated'
+        'by the input blob.'
+        'Cannot set the extra_shape argument when there is no input blob.')
+    .Arg(
+        'input_as_shape',
+        '1D tensor containing the desired output shape. First input must be in CPU context.')
+    .TensorInferenceFunction(
+        FillerTensorInference<TensorProto_DataType_DOUBLE>);
+    
+    // Then, use the TEST_P macro to define as many parameterized tests
+// for this fixture as you want. The _P suffix is for 'parameterized'
+// or 'pattern', whichever you prefer to think.
+    
+    
+    {  GTEST_DISALLOW_COPY_AND_ASSIGN_(SingleFailureChecker);
+};
+    
+    // Internal macro for implementing {EXPECT|ASSERT}_PRED4.  Don't use
+// this in your code.
+#define GTEST_PRED4_(pred, v1, v2, v3, v4, on_failure)\
+  GTEST_ASSERT_(::testing::AssertPred4Helper(#pred, \
+                                             #v1, \
+                                             #v2, \
+                                             #v3, \
+                                             #v4, \
+                                             pred, \
+                                             v1, \
+                                             v2, \
+                                             v3, \
+                                             v4), on_failure)
+    
+    
+    {    return -1;
+  }
+    
+    #include <string.h>
+    
+      // Applies a function/functor on each element of the queue, and
+  // returns the result in a new queue.  The original queue is not
+  // affected.
+  template <typename F>
+  Queue* Map(F function) const {
+    Queue* new_queue = new Queue();
+    for (const QueueNode<E>* node = head_; node != NULL; node = node->next_) {
+      new_queue->Enqueue(function(node->element()));
+    }
+    }
+    
+    bool b2Triangle::IsInside(float32 _x, float32 _y){
+	if (_x < x[0] && _x < x[1] && _x < x[2]) return false;
+	if (_x > x[0] && _x > x[1] && _x > x[2]) return false;
+	if (_y < y[0] && _y < y[1] && _y < y[2]) return false;
+	if (_y > y[0] && _y > y[1] && _y > y[2]) return false;
+		
+		float32 vx2 = _x-x[0]; float32 vy2 = _y-y[0];
+		float32 vx1 = x[1]-x[0]; float32 vy1 = y[1]-y[0];
+		float32 vx0 = x[2]-x[0]; float32 vy0 = y[2]-y[0];
+		
+		float32 dot00 = vx0*vx0+vy0*vy0;
+		float32 dot01 = vx0*vx1+vy0*vy1;
+		float32 dot02 = vx0*vx2+vy0*vy2;
+		float32 dot11 = vx1*vx1+vy1*vy1;
+		float32 dot12 = vx1*vx2+vy1*vy2;
+		float32 invDenom = 1.0f / (dot00*dot11 - dot01*dot01);
+		float32 u = (dot11*dot02 - dot01*dot12)*invDenom;
+		float32 v = (dot00*dot12 - dot01*dot02)*invDenom;
+		
+		return ((u>=0)&&(v>=0)&&(u+v<=1));    
+}
+    
+    			float fDRed = a_frgbaDecodedColor.fR - a_frgbaSourcePixel.fR;
+			float fDGreen = a_frgbaDecodedColor.fG - a_frgbaSourcePixel.fG;
+			float fDBlue = a_frgbaDecodedColor.fB - a_frgbaSourcePixel.fB;
+			float fDAlpha = a_fDecodedAlpha - a_frgbaSourcePixel.fA;
+    
+    					ErrorMetric a_errormetric);
+    
+    			ColorFloatRGBA frgbaSumLR = afrgbaAlphaWeightedSource[10] +
+										afrgbaAlphaWeightedSource[11] +
+										afrgbaAlphaWeightedSource[14] +
+										afrgbaAlphaWeightedSource[15];
+    
+    #ifndef C_ADD
+#define  C_ADD( res, a,b)\
+    do { \
+            CHECK_OVERFLOW_OP((a).r,+,(b).r)\
+            CHECK_OVERFLOW_OP((a).i,+,(b).i)\
+            (res).r=(a).r+(b).r;  (res).i=(a).i+(b).i; \
+    }while(0)
+#define  C_SUB( res, a,b)\
+    do { \
+            CHECK_OVERFLOW_OP((a).r,-,(b).r)\
+            CHECK_OVERFLOW_OP((a).i,-,(b).i)\
+            (res).r=(a).r-(b).r;  (res).i=(a).i-(b).i; \
+    }while(0)
+#define C_ADDTO( res , a)\
+    do { \
+            CHECK_OVERFLOW_OP((res).r,+,(a).r)\
+            CHECK_OVERFLOW_OP((res).i,+,(a).i)\
+            (res).r += (a).r;  (res).i += (a).i;\
+    }while(0)
+    
+    /** 16x16 multiplication where the result fits in 32 bits */
+#undef MULT16_16
+static OPUS_INLINE opus_val32 MULT16_16_armv5e(opus_val16 a, opus_val16 b)
+{
+  int res;
+  __asm__(
+      '#MULT16_16\n\t'
+      'smulbb %0, %1, %2;\n'
+      : '=r'(res)
+      : 'r'(a), 'r'(b)
+  );
+  return res;
+}
+#define MULT16_16(a, b) (MULT16_16_armv5e(a, b))
+    
+        struct aligned
+    {
+        BOOST_ALIGNMENT(4) type value;
     }
