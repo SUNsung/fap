db387677718c33834770ac8c99dd18430dfec183
SUNsung <weatherius@gmail.com>
2019-01-11 07:08:54 +0200

    Rewrite [405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb] File in: 2019-01-11 07:08:54

--- ruby/405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb
+++ ruby/405GFikTwSQ9U40Wl3LDo23D6o40sg.V4xx3edo40Gxd.rb
@@ -1,80 +1,136 @@
 
         
-                  # Default opts to a blank optionparser if none is given
-          opts ||= OptionParser.new
+              # This method returns an HTML safe string similar to what <tt>Array#join</tt>
+      # would return. The array is flattened, and all items, including
+      # the supplied separator, are HTML escaped unless they are HTML
+      # safe, and the returned string is marked as HTML safe.
+      #
+      #   safe_join([raw('<p>foo</p>'), '<p>bar</p>'], '<br />')
+      #   # => '<p>foo</p>&lt;br /&gt;&lt;p&gt;bar&lt;/p&gt;'
+      #
+      #   safe_join([raw('<p>foo</p>'), raw('<p>bar</p>')], raw('<br />'))
+      #   # => '<p>foo</p><br /><p>bar</p>'
+      #
+      def safe_join(array, sep = $,)
+        sep = ERB::Util.unwrapped_html_escape(sep)
     
-              # Register a new provisioner class only if a name was given
-          data[:provisioners].register(name.to_sym, &block) if name != UNSET_VALUE
-    
-            # This method is expected to return a class that is used for
-        # configuring the provisioner. This return value is expected to be
-        # a subclass of {Config}.
-        #
-        # @return [Config]
-        def self.config_class
-        end
-    
-            # Called after the configuration is finalized and loaded to validate
-        # this object.
-        #
-        # @param [Machine] machine Access to the machine that is being
-        #   validated.
-        # @return [Hash]
-        def validate(machine)
-          return { self.to_s => _detected_errors }
-        end
-    
-                [block.call, parent]
+              def render_component(builder)
+            builder.radio_button + builder.label
           end
-          nil
-        end
-    
-            # This should return the state of the machine within this provider.
-        # The state must be an instance of {MachineState}. Please read the
-        # documentation of that class for more information.
-        #
-        # @return [MachineState]
-        def state
-          nil
-        end
-    
-        # Get a value by the given key.
-    #
-    # This will evaluate the block given to `register` and return the
-    # resulting value.
-    def get(key)
-      return nil if !@items.key?(key)
-      return @results_cache[key] if @results_cache.key?(key)
-      @results_cache[key] = @items[key].call
+      end
     end
-    alias :[] :get
-    
-        odie 'Unknown command: #{cmd}' unless path
-    puts path
   end
 end
 
     
-        # Returns the standard exception backtrace,
-    # including the Sass backtrace.
-    #
-    # @return [Array<String>]
-    def backtrace
-      return nil if super.nil?
-      return super if sass_backtrace.all? {|h| h.empty?}
-      sass_backtrace.map do |h|
-        '#{h[:filename] || '(sass)'}:#{h[:line]}' +
-          (h[:mixin] ? ':in `#{h[:mixin]}'' : '')
-      end + super
+              content_is_options = content_or_options.is_a?(Hash)
+          if content_is_options
+            options.merge! content_or_options
+            @content = nil
+          else
+            @content = content_or_options
+          end
+    
+    require 'action_view/helpers/tags/placeholderable'
+    
+        %w(<< concat push insert unshift).each do |method|
+      class_eval <<-METHOD, __FILE__, __LINE__ + 1
+        def #{method}(*args)
+          paths.#{method}(*typecast(args))
+        end
+      METHOD
     end
     
-            unless File.directory?(File.dirname(output))
-          puts_action :directory, :green, File.dirname(output)
-          FileUtils.mkdir_p(File.dirname(output))
+    module ActionView
+  # This is the main entry point for rendering. It basically delegates
+  # to other objects like TemplateRenderer and PartialRenderer which
+  # actually renders the template.
+  #
+  # The Renderer will parse the options from the +render+ or +render_body+
+  # method and render a partial or a template based on the options. The
+  # +TemplateRenderer+ and +PartialRenderer+ objects are wrappers which do all
+  # the setup and logic necessary to render a view and a new object is created
+  # each time +render+ is called.
+  class Renderer
+    attr_accessor :lookup_context
+    
+    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
+COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
+    
+          def after_test_step(test_step, result)
+        collect_snippet_data(
+          test_step, result
+        )
+      end
+    
+        def root_path
+      context[:root_path]
+    end
+    
+        def initialize(name = nil, path = nil, type = nil)
+      self.name = name
+      self.path = path
+      self.type = type
+    
+            css('img[style]').each do |node|
+          node['align'] ||= node['style'][/float:\s*(left|right)/, 1]
+          node['style'] = node['style'].split(';').map(&:strip).select { |s| s =~ /\Awidth|height/ }.join(';')
         end
-        puts_action :convert, :green, f
-        if File.exist?(output)
-          puts_action :overwrite, :yellow, output
-        else
-          puts_action :create, :green, output
+    
+      get(/.+/) do
+    send_sinatra_file(request.path) {404}
+  end
+    
+      def perform(msg='lulz you forgot a msg!')
+    $redis.lpush('sinkiq-example-messages', msg)
+  end
+end
+    
+        def atomic_push(conn, payloads)
+      if payloads.first['at']
+        conn.zadd('schedule', payloads.map do |hash|
+          at = hash.delete('at').to_s
+          [at, Sidekiq.dump_json(hash)]
+        end)
+      else
+        q = payloads.first['queue']
+        now = Time.now.to_f
+        to_push = payloads.map do |entry|
+          entry['enqueued_at'] = now
+          Sidekiq.dump_json(entry)
         end
+        conn.sadd('queues', q)
+        conn.lpush('queue:#{q}', to_push)
+      end
+    end
+    
+          Sidekiq.logger.debug { 'Re-queueing terminated jobs' }
+      jobs_to_requeue = {}
+      inprogress.each do |unit_of_work|
+        jobs_to_requeue[unit_of_work.queue_name] ||= []
+        jobs_to_requeue[unit_of_work.queue_name] << unit_of_work.job
+      end
+    
+    module Sidekiq
+  class Web
+    ROOT = File.expand_path('#{File.dirname(__FILE__)}/../../web')
+    VIEWS = '#{ROOT}/views'
+    LOCALES = ['#{ROOT}/locales']
+    LAYOUT = '#{VIEWS}/layout.erb'
+    ASSETS = '#{ROOT}/assets'
+    
+          erb(content, options)
+    end
+    
+    Then /^the file at '([^']*)' should be the same as '([^']*)'$/ do |web_file, path|
+  expected = IO.read(path)
+  actual = read_from_web(web_file)
+  expect(actual).to eq(expected)
+end
+    
+    begin
+  # Use mime/types/columnar if available, for reduced memory usage
+  require 'mime/types/columnar'
+rescue LoadError
+  require 'mime/types'
+end
