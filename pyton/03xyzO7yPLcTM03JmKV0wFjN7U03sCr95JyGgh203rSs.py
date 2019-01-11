d850f16f3566db05a0cc9623f52dd632203b375e
SUNsung <weatherius@gmail.com>
2019-01-11 07:45:35 +0200

    Rewrite [03xyzO7yPLcTM03JmKV0wFjN7U03sCr95JyGgh203rSs.py] File in: 2019-01-11 07:45:34

--- pyton/03xyzO7yPLcTM03JmKV0wFjN7U03sCr95JyGgh203rSs.py
+++ pyton/03xyzO7yPLcTM03JmKV0wFjN7U03sCr95JyGgh203rSs.py
@@ -1,183 +1,41 @@
 
         
-        
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
+        # If false, no index is generated.
+#html_use_index = True
+    
+    # Note: This example is only tested with Python 3 (not Python 2)
+    
+    
+def _raw_face_locations_batched(images, number_of_times_to_upsample=1, batch_size=128):
     '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
-        else:
-            out = string.decode(encoding)
+    Returns an 2d array of dlib rects of human faces in a image using the cnn face detector
     
+        function_parameters = zip(
+        images_to_check,
+        itertools.repeat(model),
+    )
     
-@pytest.fixture
-def httpbin(httpbin):
-    return prepare_url(httpbin)
+    # This is a demo of running face recognition on a video file and saving the results to a new video file.
+#
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
     
+    # This code finds all faces in a list of images using the CNN model.
+#
+# This demo is for the _special case_ when you need to find faces in LOTS of images very quickly and all the images
+# are the exact same size. This is common in video processing applications where you have lots of video frames
+# to process.
+#
+# If you are processing a lot of images and using a GPU with CUDA, batch processing can be ~3x faster then processing
+# single images at a time. But if you aren't using a GPU, then batch processing isn't going to be very helpful.
+#
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read the video file.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
     
-def test_system_ssl():
-    '''Verify we're actually setting system_ssl when it should be available.'''
-    assert info()['system_ssl']['version'] != ''
+            img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama_partial_face2.jpg'))
+        detected_faces = api.face_locations(img)
     
-        # Client Error.
-    400: ('bad_request', 'bad'),
-    401: ('unauthorized',),
-    402: ('payment_required', 'payment'),
-    403: ('forbidden',),
-    404: ('not_found', '-o-'),
-    405: ('method_not_allowed', 'not_allowed'),
-    406: ('not_acceptable',),
-    407: ('proxy_authentication_required', 'proxy_auth', 'proxy_authentication'),
-    408: ('request_timeout', 'timeout'),
-    409: ('conflict',),
-    410: ('gone',),
-    411: ('length_required',),
-    412: ('precondition_failed', 'precondition'),
-    413: ('request_entity_too_large',),
-    414: ('request_uri_too_large',),
-    415: ('unsupported_media_type', 'unsupported_media', 'media_type'),
-    416: ('requested_range_not_satisfiable', 'requested_range', 'range_not_satisfiable'),
-    417: ('expectation_failed',),
-    418: ('im_a_teapot', 'teapot', 'i_am_a_teapot'),
-    421: ('misdirected_request',),
-    422: ('unprocessable_entity', 'unprocessable'),
-    423: ('locked',),
-    424: ('failed_dependency', 'dependency'),
-    425: ('unordered_collection', 'unordered'),
-    426: ('upgrade_required', 'upgrade'),
-    428: ('precondition_required', 'precondition'),
-    429: ('too_many_requests', 'too_many'),
-    431: ('header_fields_too_large', 'fields_too_large'),
-    444: ('no_response', 'none'),
-    449: ('retry_with', 'retry'),
-    450: ('blocked_by_windows_parental_controls', 'parental_controls'),
-    451: ('unavailable_for_legal_reasons', 'legal_reasons'),
-    499: ('client_closed_request',),
-    
-        if not HAS_PYTHON26:
-        module.fail_json(
-            msg='GCE module requires python's 'ast' module, python v2.6+')
-    
-    
-def _add_firewall_rules(module, oneandone_conn, firewall_id, rules):
-    '''
-    Adds new rules to a firewall policy.
-    '''
-    try:
-        firewall_rules = []
-    
-    # module specific functions
-    
-        def _exec(self, args):
-        cmd = [self._icinga2, 'feature']
-        rc, out, err = self.module.run_command(cmd + args, check_rc=True)
-        return rc, out
-    
-    
-DOCUMENTATION = '''
----
-module: monit
-short_description: Manage the state of a program monitored via Monit
-description:
-     - Manage the state of a program monitored via I(Monit)
-version_added: '1.2'
-options:
-  name:
-    description:
-      - The name of the I(monit) program/process to manage
-    required: true
-  state:
-    description:
-      - The state of service
-    required: true
-    choices: [ 'present', 'started', 'stopped', 'restarted', 'monitored', 'unmonitored', 'reloaded' ]
-  timeout:
-    description:
-      - If there are pending actions for the service monitored by monit, then Ansible will check
-        for up to this many seconds to verify the requested action has been performed.
-        Ansible will sleep for five seconds between each check.
-    default: 300
-    version_added: '2.1'
-author: 'Darryl Stoflet (@dstoflet)'
-'''
-    
-            if connection_status == pyflic.ConnectionStatus.Disconnected:
-            _LOGGER.warning('Button (%s) disconnected. Reason: %s',
-                            self.address, disconnect_reason)
-
-    
-                    name = None
-                for prop in device['properties']:
-                    if prop['name'] == 'userDeviceName':
-                        name = prop['value']
-                if not name:
-                    name = device.get('friendlyName', device['deviceID'])
-    
-        def get_device_name(self, device):
-        '''Return the name of the given device or None if we don't know.'''
-        filter_named = [result.name for result in self.last_results
-                        if result.mac == device]
-    
-        def get_swisscom_data(self):
-        '''Retrieve data from Swisscom and return parsed result.'''
-        url = 'http://{}/ws'.format(self.host)
-        headers = {CONTENT_TYPE: 'application/x-sah-ws-4-call+json'}
-        data = '''
-        {'service':'Devices', 'method':'get',
-        'parameters':{'expression':'lan and not self'}}'''
-    
-        for idx, module_conf in enumerate(sensors):
-        load_module(CONF_SENSOR, idx, module_conf)
-    
-    import random
-    
-        publicKey, privateKey = generateKey(keySize)
-    print('\nWriting public key to file %s_pubkey.txt...' % name)
-    with open('%s_pubkey.txt' % name, 'w') as fo:
-        fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
-    
-        def _step_by_step(self, step_ord):
-    
-    	s = [7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22, \
-		5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20, \
-		4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23, \
-		6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 ]
-    
-    #Root Mean Squared Error
-def rmse(predict, actual):
-    predict = np.array(predict)
-    actual = np.array(actual)
-    
-    
-def main(path):
-    
-    ### OUTPUT ###
-# renaming foo.txt to bar.txt
-# renaming bar.txt to baz.txt
-# renaming baz.txt to bar.txt
-# renaming bar.txt to foo.txt
-
-    
-    '''
-http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
-Implementation of the iterator pattern with a generator
-    
-        def detach(self, observer):
-        try:
-            self._observers.remove(observer)
-        except ValueError:
-            pass
-    
-        def get_current_time_as_html_fragment(self, time_provider):
-        current_time = time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-    
-    
-if __name__ == '__main__':
-    main()
+    # Find all facial features in all the faces in the image
+face_landmarks_list = face_recognition.face_landmarks(image)
