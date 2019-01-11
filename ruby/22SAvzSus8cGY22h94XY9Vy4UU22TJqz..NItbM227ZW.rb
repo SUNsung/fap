c4c2d917f6b5d025e83727e4f219be18bcda1b2b
SUNsung <weatherius@gmail.com>
2019-01-11 08:07:29 +0200

    Rewrite [22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb] File in: 2019-01-11 08:07:29

--- ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
+++ ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
@@ -1,11 +1,52 @@
 
         
-                    target 'Static' do
-                use_frameworks!(false)
-            end
-            target 'Dynamic' do
-                use_frameworks!(true)
-            end
-        end
+        (allow file-read-metadata)
+(allow file-read*
+  ; This is currenly only added because using `xcodebuild` to build a resource
+  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
+  ; needed, but for now it doesn’t seem like a real problem.
+  (literal '/')
+  (regex
+    ; TODO see if we can restrict this more, but it's going to be hard
+    #'^/Users/[^.]+/*'
+    ;#'^/Users/[^.]+/.netrc'
+    ;#'^/Users/[^.]+/.gemrc'
+    ;#'^/Users/[^.]+/.gem/*'
+    ;#'^/Users/[^.]+/Library/.*'
+    #'^/Library/*'
+    #'^/System/Library/*'
+    #'^/usr/lib/*'
+    #'^/usr/share/*'
+    #'^/private/*'
+    #'^/dev/*'
+    #'^<%= ruby_prefix %>'
+    #'^<%= pod_prefix %>'
+    #'^<%= xcode_app_path %>'
+    #'^<%= Pod::Config.instance.repos_dir %>'
+<% prefixes.each do |prefix| %>
+    #'^<%= prefix %>/*'
+<% end %>
+  )
+)
+    
+          def html?(headers)
+        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
+        options[:html_types].include? header.last[/^\w+\/\w+/]
+      end
     end
+  end
 end
+
+    
+          def remove_bad_cookies(request, response)
+        return if bad_cookies.empty?
+        paths = cookie_paths(request.path)
+        bad_cookies.each do |name|
+          paths.each { |path| response.set_cookie name, empty_cookie(request.host, path) }
+        end
+      end
+    
+      it 'denies post requests with wrong X-CSRF-Token header' do
+    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => bad_token)
+    expect(last_response).not_to be_ok
+  end
