d797116f556339f51e8eecdd285ef6367d252f9c
SUNsung <weatherius@gmail.com>
2019-01-11 22:19:07 +0200

    Rewrite [29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py] File in: 2019-01-11 22:19:06

--- pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
+++ pyton/29I7bVbHTT2229qr4nqFZxM29CN1ZbW.MZI29Vee.py
@@ -1,127 +1,59 @@
 
         
-            # Separating the 'table of contents' from the contents (blocks)
-    table_of_contents = ''.join(read_me.split('- - -')[0])
-    blocks = ''.join(read_me.split('- - -')[1]).split('\n# ')
-    for i in range(len(blocks)):
-        if i == 0:
-            blocks[i] = blocks[i] + '\n'
-        else:
-            blocks[i] = '# ' + blocks[i] + '\n'
+            return response
     
-        def delete(self, session_key=None):
-        '''
-        To delete, clear the session key and the underlying data structure
-        and set the modified flag so that the cookie is set on the client for
-        the current request.
-        '''
-        self._session_key = ''
-        self._session_cache = {}
-        self.modified = True
+        def __iter__(self):
+        return iter(self._plugins)
     
-            pemfile = '/etc/ssl/certs/CA.pem'
-        new_certfile = '/usr/local/share/ca-certificates/CA.crt'
-        if not os.path.exists(pemfile) or not CertUtil.file_is_same(certfile, new_certfile):
-            if os.system('cp '%s' '%s' && update-ca-certificates' % (certfile, new_certfile)) != 0:
-                xlog.warning('install root certificate failed, Please run as administrator/root/sudo')
-    
-            if self.continue_fail_count > 10:
-            # don't set network_stat to 'unknown', wait for check
-            # network_stat = 'unknown'
-            xlog.debug('report_connect_fail %s continue_fail_count:%d',
-                       self.type, self.continue_fail_count)
-            self.triger_check_network(True)
-    
-            name = p.join(ws)
-        name += '.' + random.choice(self.end)
-    
-    __version__ = '3.0.7'
-__password__ = ''
-__hostsdeny__ = ()
-#__hostsdeny__ = ('.youtube.com', '.youku.com', '.googlevideo.com')
-__content_type__ = 'image/gif'
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
-    
-    
-    def getType(self):
-        return self.type 
-    
-    site_info = 'Dailymotion.com'
-download = dailymotion_download
-download_playlist = playlist_not_supported('dailymotion')
+    UNICODE = FILE_CONTENT
 
     
-    headers = {
-    'DNT': '1',
-    'Accept-Encoding': 'gzip, deflate, sdch, br',
-    'Accept-Language': 'en-CA,en;q=0.8,en-US;q=0.6,zh-CN;q=0.4,zh;q=0.2',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.75 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Cache-Control': 'max-age=0',
-    'Referer': 'http://www.dilidili.com/',
-    'Connection': 'keep-alive',
-    'Save-Data': 'on',
-}
     
-    __all__ = ['ehow_download']
-    
-        title = r1(r'<title id='pageTitle'>(.+)</title>', html)
-    
-        def render(self):
-        for graphic in self.graphics:
-            graphic.render()
-    
-        def notify(self, msg):
-        self.msg_queue.append(msg)
-    
-    # Execute them
-for template in templates:
-    template()
+def rst_filenames():
+    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
+        if '.tox' not in root:
+            for filename in fnmatch.filter(filenames, '*.rst'):
+                yield os.path.join(root, filename)
     
     
-class Node(object):
-    pass
+def test_follow_all_redirects_shown(httpbin):
+    r = http('--follow', '--all', httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 3
+    assert r.count('HTTP/1.1 302 FOUND', 2)
+    assert HTTP_OK in r
     
-        def clone(self, **attrs):
-        '''Clone a prototype and update inner attributes dictionary'''
-        # Python in Practice, Mark Summerfield
-        obj = self.__class__()
-        obj.__dict__.update(attrs)
-        return obj
+    # def test_unicode_url_verbose(self):
+#     r = http(httpbin.url + '--verbose', u'/get?test=' + UNICODE)
+#     assert HTTP_OK in r
     
-        def run_loop(self):
-        while self.blackboard.common_state['progress'] < 100:
-            for expert in self.blackboard.experts:
-                if expert.is_eager_to_contribute:
-                    expert.contribute()
-        return self.blackboard.common_state['contributions']
+        while s % 2 == 0:
+        s = s // 2
+        t += 1
+    
+    def main():
+    print('Making key files...')
+    makeKeyFiles('rsa', 1024)
+    print('Key files generation successful.')
+    
+    def longestSub(ARRAY): 			#This function is recursive
+	
+	ARRAY_LENGTH = len(ARRAY)
+	if(ARRAY_LENGTH <= 1):  	#If the array contains only one element, we return it (it's the stop condition of recursion)
+		return ARRAY
+								#Else
+	PIVOT=ARRAY[0]
+	isFound=False
+	i=1
+	LONGEST_SUB=[]
+	while(not isFound and i<ARRAY_LENGTH):
+		if (ARRAY[i] < PIVOT):
+			isFound=True
+			TEMPORARY_ARRAY = [ element for element in ARRAY[i:] if element >= ARRAY[i] ]
+			TEMPORARY_ARRAY = longestSub(TEMPORARY_ARRAY)
+			if ( len(TEMPORARY_ARRAY) > len(LONGEST_SUB) ):
+				LONGEST_SUB = TEMPORARY_ARRAY
+		else:
+			i+=1
+    
+    
+if __name__ == '__main__':
