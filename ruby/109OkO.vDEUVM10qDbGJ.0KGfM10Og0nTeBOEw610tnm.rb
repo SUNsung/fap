647b55574ea66315ab8c67db4a0fd0ab9d1aea1e
SUNsung <weatherius@gmail.com>
2019-01-11 18:20:31 +0200

    Rewrite [109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb] File in: 2019-01-11 18:20:31

--- ruby/109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb
+++ ruby/109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb
@@ -1,359 +1,173 @@
 
         
-              def print_worst_offenders
-        @io.puts
-        @io.puts 'Worst offenders:'
-        @timings.sort_by { |_f, t| -t }.take(10).each do |(f, t)|
-          @io.puts '  #{t}s for #{f}'
-        end
-        @io.puts
-      end
+        #############################################################################
+#
+# Helper functions
+#
+#############################################################################
     
-    def slug(title = nil)
-  if !title
-    then Time.now.strftime('%s%9N') # nanoseconds since the Epoch
-  else title.downcase.gsub(%r![^\w]!, ' ').strip.gsub(%r!\s+!, '-')
-  end
-end
+          #
     
-            # rubocop:disable Metrics/AbcSize
-        def process(args, opts)
-          if !args || args.empty?
-            raise Jekyll::Errors::InvalidThemeName, 'You must specify a theme name.'
+              new_theme_name = args.join('_')
+          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
+    
+                    raise Errors::VMNoMatchError if vms.empty?
+              else
+                # String name, just look for a specific VM
+                vms << @env.vms[name.to_sym]
+                raise Errors::VMNotFoundError, name: name if !vms[0]
+              end
+            end
+          else
+            vms = @env.vms_ordered
           end
     
-              # WebSockets requests will have a Connection: Upgrade header
-          if parser.http_method != 'GET' || parser.upgrade?
-            super
-          elsif parser.request_url =~ %r!^\/livereload.js!
-            headers = [
-              'HTTP/1.1 200 OK',
-              'Content-Type: application/javascript',
-              'Content-Length: #{reload_size}',
-              '',
-              '',
-            ].join('\r\n')
-            send_data(headers)
-    
-            def setup
-          @config['syntax_highlighter'] ||= highlighter
-          @config['syntax_highlighter_opts'] ||= {}
-          @config['coderay'] ||= {} # XXX: Legacy.
-          modernize_coderay_config
-          make_accessible
-        end
-    
-            expect(result).to include(' oclint -report-type=pmd -o=report_path.xml ')
-        expect(result).to include(' -max-priority-1=10 ')
-        expect(result).to include(' -max-priority-2=20 ')
-        expect(result).to include(' -max-priority-3=30 ')
-        expect(result).to include(' -rc=LONG_LINE=200 -rc=LONG_METHOD=200 ')
-        expect(result).to include(' -rule DoubleNegative -rule DeadCode ')
-        expect(result).to include(' -disable-rule GotoStatement -disable-rule ShortVariableName ')
-        expect(result).to include(' -list-enabled-rules ')
-        expect(result).to include(' -enable-clang-static-analyzer ')
-        expect(result).to include(' -enable-global-analysis ')
-        expect(result).to include(' -allow-duplicated-violations ')
-      end
-    
-          context 'the `ignore_exit_status` option' do
-        context 'by default' do
-          it 'should raise if swiftlint completes with a non-zero exit status' do
-            allow(FastlaneCore::UI).to receive(:important)
-            expect(FastlaneCore::UI).to receive(:important).with(/If you want fastlane to continue anyway/)
-            # This is simulating the exception raised if the return code is non-zero
-            expect(Fastlane::Actions).to receive(:sh).and_raise('fake error')
-            expect(FastlaneCore::UI).to receive(:user_error!).with(/SwiftLint finished with errors/).and_call_original
-    
-    module Gitlab
-  module GithubImport
-    module Importer
-      class LabelLinksImporter
-        attr_reader :issue, :project, :client, :label_finder
-    
-            def truncated_title
-          title.truncate(255)
-        end
-    
-    require 'vagrant/capability_host'
-    
-            # Called after the configuration is finalized and loaded to validate
-        # this object.
+            # Merge another configuration object into this one. This assumes that
+        # the other object is the same class as this one. This should not
+        # mutate this object, but instead should return a new, merged object.
         #
-        # @param [Environment] env Vagrant::Environment object of the
-        #   environment that this configuration has been loaded into. This
-        #   gives you convenient access to things like the the root path
-        #   and so on.
-        # @param [ErrorRecorder] errors
-        def validate(env, errors)
-        end
-      end
-    end
-  end
-end
-
-    
-            # Configures the given list of networks on the virtual machine.
+        # The default implementation will simply iterate over the instance
+        # variables and merge them together, with this object overriding
+        # any conflicting instance variables of the older object. Instance
+        # variables starting with '__' (double underscores) will be ignored.
+        # This lets you set some sort of instance-specific state on your
+        # configuration keys without them being merged together later.
         #
-        # The networks parameter will be an array of hashes where the hashes
-        # represent the configuration of a network interface. The structure
-        # of the hash will be roughly the following:
-        #
-        # {
-        #   type:      :static,
-        #   ip:        '192.168.33.10',
-        #   netmask:   '255.255.255.0',
-        #   interface: 1
-        # }
-        #
-        def configure_networks(networks)
-          raise BaseError, _key: :unsupported_configure_networks
-        end
+        # @param [Object] other The other configuration object to merge from,
+        #   this must be the same type of object as this one.
+        # @return [Object] The merged object.
+        def merge(other)
+          result = self.class.new
     
-            # Defines additional command line commands available by key. The key
-        # becomes the subcommand, so if you register a command 'foo' then
-        # 'vagrant foo' becomes available.
-        #
-        # @param [String] name Subcommand key.
-        def self.command(name=UNSET_VALUE, &block)
-          data[:command] ||= Registry.new
-    
-              @commands = Registry.new
-          @configs = Hash.new { |h, k| h[k] = Registry.new }
-          @guests  = Registry.new
-          @guest_capabilities = Hash.new { |h, k| h[k] = Registry.new }
-          @hosts   = Registry.new
-          @host_capabilities = Hash.new { |h, k| h[k] = Registry.new }
-          @providers = Registry.new
-          @provider_capabilities = Hash.new { |h, k| h[k] = Registry.new }
-          @pushes = Registry.new
-          @synced_folders = Registry.new
-        end
-      end
-    end
-  end
-end
-
-    
-            # This returns all the registered communicators.
+            # This returns all registered provisioners.
         #
         # @return [Hash]
-        def communicators
+        def provisioners
           Registry.new.tap do |result|
             @registered.each do |plugin|
-              result.merge!(plugin.communicator)
+              result.merge!(plugin.provisioner)
             end
           end
         end
     
-        def destroy
-      authorize @email_domain_block, :destroy?
-      @email_domain_block.destroy!
-      log_action :destroy, @email_domain_block
-      redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.destroyed_msg')
-    end
+            # Helper method that will set a value if a value is given, or otherwise
+        # return the already set value.
+        #
+        # @param [Symbol] key Key for the data
+        # @param [Object] value Value to store.
+        # @return [Object] Stored value.
+        def self.get_or_set(key, value=UNSET_VALUE)
+          # If no value is to be set, then return the value we have already set
+          return data[key] if value.eql?(UNSET_VALUE)
     
-        def status_params
-      params.require(:status).permit(:sensitive)
-    end
-    
-      def body
-    @_body ||= request.body.read
-  end
-    
-        @web_subscription = ::Web::PushSubscription.create!(
-      endpoint: subscription_params[:endpoint],
-      key_p256dh: subscription_params[:keys][:p256dh],
-      key_auth: subscription_params[:keys][:auth],
-      data: data_params,
-      user_id: current_user.id,
-      access_token_id: doorkeeper_token.id
-    )
-    
-      def self.provides_callback_for(provider)
-    provider_id = provider.to_s.chomp '_oauth2'
-    
-    class PolymorphicMentions < ActiveRecord::Migration[4.2]
-  def change
-    remove_index :mentions, column: %i(post_id)
-    remove_index :mentions, column: %i(person_id post_id), unique: true
-    rename_column :mentions, :post_id, :mentions_container_id
-    add_column :mentions, :mentions_container_type, :string
-    add_index :mentions,
-              %i(mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_mc_id_and_mc_type',
-              length: {mentions_container_type: 191}
-    add_index :mentions,
-              %i(person_id mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_person_and_mc_id_and_mc_type',
-              length: {mentions_container_type: 191},
-              unique: true
-    
-    When /^I toggle nsfw posts$/ do
-  find('.toggle_nsfw_state', match: :first).click
-end
-    
-      class FetchWebfinger < Base
-    def perform(*_args)
-      # don't do real discovery in cucumber
-    end
-  end
-end
-
-    
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
-    end
-  end
-end
-
-    
-    # ECMA-262, section 15.1.3
-    def Encode(uri, unescape)
-      uriLength = uri.length;
-      # We are going to pass result to %StringFromCharCodeArray
-      # which does not expect any getters/setters installed
-      # on the incoming array.
-      result    = Array.new(uriLength);
-      index = 0;
-      k = -1;
-      while ((k+=1) < uriLength) do
-        cc1 = uri.charCodeAt(k);
-        next if cc1.nil?
-        if (self.send(unescape, cc1))
-          result[index] = cc1;
-          index += 1
-        else
-          if (cc1 >= 0xDC00 && cc1 <= 0xDFFF);
-            throw('URI malformed')
-          end
-          if (cc1 < 0xD800 || cc1 > 0xDBFF)
-            index = URIEncodeSingle(cc1, result, index);
-          else
-            k+=1;
-            if (k == uriLength);
-              throw('URI malformed')
-            end
-            cc2 = uri.charCodeAt(k);
-            if (cc2 < 0xDC00 || cc2 > 0xDFFF);
-              throw('URI malformed')
-            end
-            index = URIEncodePair(cc1, cc2, result, index);
-          end
+            # This is an internal initialize function that should never be
+        # overridden. It is used to initialize some common internal state
+        # that is used in a provider.
+        def _initialize(name, machine)
+          initialize_capabilities!(
+            name.to_sym,
+            { name.to_sym => [Class.new, nil] },
+            Vagrant.plugin('2').manager.provider_capabilities,
+            machine,
+          )
         end
       end
-      # use .compact to get rid of nils from charCodeAt
-      # return %StringFromCharCodeArray(result);
-      # 'c' = 8 bit signed char
-      # http://www.ruby-doc.org/core-1.9.3/Array.html#method-i-pack
-      return result.compact.pack 'c*'
     end
-  end # class << self
-end # module
+  end
+end
 
     
-          attr_reader :page, :diff, :versions, :message, :allow_editing
+        # Get a value by the given key.
+    #
+    # This will evaluate the block given to `register` and return the
+    # resulting value.
+    def get(key)
+      return nil if !@items.key?(key)
+      return @results_cache[key] if @results_cache.key?(key)
+      @results_cache[key] = @items[key].call
+    end
+    alias :[] :get
     
-          def upload_dest
-        @upload_dest
+        remove_duplicates
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
+    
+        change.down do
+      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
+      Mention.where(mentions_container_type: 'Comment').destroy_all
+      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
+    end
+  end
+end
+
+    
+    describe ConversationsController, :type => :controller do
+  describe '#index' do
+    before do
+      @person = alice.contacts.first.person
+      hash = {
+        :author => @person,
+        :participant_ids => [alice.person.id, @person.id],
+        :subject => 'not spam',
+        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
+      }
+      @conv1 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+    
+          delete :destroy, params: {post_id: @message.id, id: like2.id}, format: :json
+      expect(response.status).to eq(404)
+      expect(response.body).to eq(I18n.t('likes.destroy.error'))
+      expect(Like.count).to eq(like_count)
+    end
+  end
+end
+
+    
+            if (trailing = (scanner.scan(/\s*#{Sass::SCSS::RX::COMMENT}/) ||
+                        scanner.scan(/\s*#{Sass::SCSS::RX::SINGLE_LINE_COMMENT}/)))
+          trailing.strip!
+        end
+        interp_parsed = parse_interp(scanner.rest)
+        selector_range = Sass::Source::Range.new(
+          ident_range.start_pos,
+          Sass::Source::Position.new(@line, to_parser_offset(line.offset) + line.text.length),
+          @options[:filename], @options[:importer])
+        rule = Tree::RuleNode.new(res + interp_parsed, selector_range)
+        rule << Tree::CommentNode.new([trailing], :silent) if trailing
+        rule
       end
+    end
     
-          def has_path
-        !@path.nil?
-      end
-    
-          # Finds header node inside Nokogiri::HTML document.
+          # A string representation of the importer.
+      # Should be overridden by subclasses.
       #
-      def find_header_node(doc)
-        case @page.format
-          when :asciidoc
-            doc.css('div#gollum-root > h1:first-child')
-          when :org
-            doc.css('div#gollum-root > p.title:first-child')
-          when :pod
-            doc.css('div#gollum-root > a.dummyTopAnchor:first-child + h1')
-          when :rest
-            doc.css('div#gollum-root > div > div > h1:first-child')
-          else
-            doc.css('div#gollum-root > h1:first-child')
-        end
+      # This is used to help debugging,
+      # and should usually just show the load path encapsulated by this importer.
+      #
+      # @return [String]
+      def to_s
+        Sass::Util.abstract(self)
       end
     
-        page = @wiki.page('D')
-    assert page.nil?
+    def ask(message, valid_options)
+  if valid_options
+    answer = get_stdin('#{message} #{valid_options.to_s.gsub(/'/, '').gsub(/, /,'/')} ') while !valid_options.include?(answer)
+  else
+    answer = get_stdin(message)
   end
+  answer
+end
     
-      test 'clean path without leading slash' do
-    assert_equal '/Mordor', clean_path('Mordor')
-  end
+    module Jekyll
     
-      test 'remove page extentions' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'page', view.remove_page_extentions('page.wiki')
-    assert_equal 'page-wiki', view.remove_page_extentions('page-wiki.md')
-    assert_equal 'file.any_extention', view.remove_page_extentions('file.any_extention')
-  end
+    def config_tag(config, key, tag=nil, classname=nil)
+  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
+  tag       ||= 'div'
+  classname ||= key.sub(/_/, '-').sub(/\./, '-')
+  output      = '<#{tag} class='#{classname}''
     
-    exec         = {}
-options      = {
-  :port => 4567,
-  :bind => '0.0.0.0',
-}
-wiki_options = {
-  :live_preview  => false,
-  :allow_uploads => false,
-  :allow_editing => true,
-}
-    
-        def forbid(msg = 'Forbidden. This wiki is set to no-edit mode.')
-      @message = msg
-      status 403
-      halt mustache :error
-    end
-    
-    begin
-  #RubyProf::exclude_threads = [ Monitoring ]
-  #RubyProf.start
-  fire_event(:startup)
-  #Sidekiq.logger.error 'Simulating 1ms of latency between Sidekiq and redis'
-  #Toxiproxy[:redis].downstream(:latency, latency: 1).apply do
-    launcher = Sidekiq::Launcher.new(Sidekiq.options)
-    launcher.run
-    
-          def create_worker_spec
-        template_file = File.join(
-            'spec/workers',
-            class_path,
-            '#{file_name}_worker_spec.rb'
-        )
-        template 'worker_spec.rb.erb', template_file
-      end
-    
-        module PsychAutoload
-      def resolve_class(klass_name)
-        return nil if !klass_name || klass_name.empty?
-        # constantize
-        names = klass_name.split('::')
-        names.shift if names.empty? || names.first.empty?
-    
-            begin
-          File.open(fp.path, 'a') { |tmpfp| fp.reopen(tmpfp) }
-          fp.sync = true
-          nr += 1
-        rescue IOError, Errno::EBADF
-          # not much we can do...
-        end
-      end
-      nr
-    rescue RuntimeError => ex
-      # RuntimeError: ObjectSpace is disabled; each_object will only work with Class, pass -X+O to enable
-      puts 'Unable to reopen logs: #{ex.message}'
-    end
-    
-          def initialize
-        @entries = []
-        yield self if block_given?
-      end
+    Liquid::Template.register_tag('include_array', Jekyll::IncludeArrayTag)
