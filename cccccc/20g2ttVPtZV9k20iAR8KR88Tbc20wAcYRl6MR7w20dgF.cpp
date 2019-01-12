65d4f7da04f8e4e574b1d58b73a296ddcf452c50
SUNsung <weatherius@gmail.com>
2019-01-12 16:24:34 +0200

    Rewrite [20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp] File in: 2019-01-12 16:24:34

--- cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
+++ cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
@@ -1,58 +1,232 @@
 
         
-        
-    {
-    {
-    {        for (;;) {
-          auto start = index.fetch_add(work_chunk);
-          auto const stop = std::min(start + work_chunk, inputs.size());
-          if (start >= stop) break;
-          for (auto i = start; i != stop; ++i) func(inputs[i]);
-        }
-      } catch (const std::exception& e) {
-        std::fprintf(stderr,
-          'worker thread exited with exception: %s\n', e.what());
-        failed = true;
-      }
-    }));
+        // Calls the registered C++ shape inference function for <node> (a serialized
+// NodeDef).
+// Should not be called for shape functions that access input tensors; constant
+// input tensor values are not made available, and so the inferred shapes will
+// be less precise than they could be.
+//
+// Returns an error, or OK, in <out_status> according to whether the shape
+// inference was successful.
+//
+// On success, returns a vector populated with the inferred output shapes (as
+// serialized CppShapeInferenceResult protos) followed by a serialized
+// CppShapeInferenceInputsNeeded proto.
+//
+// This is temporary code to be used during the migration
+// from python shape inference functions to C++ shape inference functions.
+std::vector<string> RunCppShapeInference(
+    int graph_def_version, const string& serialized_node_def,
+    const std::vector<string>& input_serialized_shapes,
+    PyObject* input_constant_tensor_values,
+    const std::vector<string>& input_constant_tensor_as_shape_values,
+    TF_Status* out_status);
+    
+    #ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_H_
+    
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/platform/mutex.h'
+#include 'tensorflow/python/lib/core/bfloat16.h'
+#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
+    
+    // Safe containers for an owned TFE_TensorHandle. On destruction, the handle
+// will be deleted by TFE_DeleteTensorHandle.
+using Safe_TFE_TensorHandlePtr =
+    std::unique_ptr<TFE_TensorHandle, detail::TFETensorHandleDeleter>;
+Safe_TFE_TensorHandlePtr make_safe(TFE_TensorHandle* handle);
+    
+      // Calculate the depth at which the requirement's generic parameters
+  // appear in the synthetic signature.
+  unsigned depth = 0;
+  if (covariantSelf) {
+    depth++;
+  }
+  if (conformanceSig) {
+    depth += conformanceSig->getGenericParams().back()->getDepth() + 1;
   }
     
-    enum class RegNumber : uint32_t {};
+    DIRECTIONAL_PREPOSITION(above)
+DIRECTIONAL_PREPOSITION(after)
+DIRECTIONAL_PREPOSITION(along)
+DIRECTIONAL_PREPOSITION(alongside)
+DIRECTIONAL_PREPOSITION(as)
+DIRECTIONAL_PREPOSITION(at)
+DIRECTIONAL_PREPOSITION(before)
+DIRECTIONAL_PREPOSITION(below)
+DIRECTIONAL_PREPOSITION(by)
+DIRECTIONAL_PREPOSITION(following)
+DIRECTIONAL_PREPOSITION(for)
+DIRECTIONAL_PREPOSITION(from)
+DIRECTIONAL_PREPOSITION(given)
+DIRECTIONAL_PREPOSITION(in)
+DIRECTIONAL_PREPOSITION(including)
+DIRECTIONAL_PREPOSITION(inside)
+DIRECTIONAL_PREPOSITION(into)
+DIRECTIONAL_PREPOSITION(matching)
+DIRECTIONAL_PREPOSITION(of)
+DIRECTIONAL_PREPOSITION(on)
+DIRECTIONAL_PREPOSITION(passing)
+DIRECTIONAL_PREPOSITION(preceding)
+DIRECTIONAL_PREPOSITION(since)
+DIRECTIONAL_PREPOSITION(to)
+DIRECTIONAL_PREPOSITION(until)
+DIRECTIONAL_PREPOSITION(using)
+DIRECTIONAL_PREPOSITION(via)
+DIRECTIONAL_PREPOSITION(when)
+PREPOSITION(with)
+DIRECTIONAL_PREPOSITION(within)
     
-    // No `ini` binding yet. Hdf still takes precedence but will be removed
-// once we have made all options ini-aware. All new settings should
-// use the ini path of this method (i.e., pass a bogus Hdf or keep it null)
-void Config::Iterate(std::function<void (const IniSettingMap&,
-                                         const Hdf&,
-                                         const std::string&)> cb,
-                     const IniSettingMap &ini, const Hdf& config,
-                     const std::string &name,
-                     const bool prepend_hhvm /* = true */) {
-  Hdf hdf = name.empty() ? config : config[name];
-  if (hdf.exists() && !hdf.isEmpty()) {
-    for (Hdf c = hdf.firstChild(); c.exists(); c = c.next()) {
-      cb(IniSetting::Map::object, c, '');
-    }
-  } else {
-    Hdf empty;
-    auto ini_value = name.empty() ? ini :
-      ini_iterate(ini, IniName(name, prepend_hhvm));
-    if (ini_value.isArray()) {
-      for (ArrayIter iter(ini_value.toArray()); iter; ++iter) {
-        cb(iter.second(), empty, iter.first().toString().toCppString());
+        {
+      Out << Indent;
+      if (childKind == ChildKind::Root) {
+        Out << '+- ';
+      } else if (childKind == ChildKind::Left) {
+        Out << '/- ';
+      } else if (childKind == ChildKind::Right) {
+        Out << '\\- ';
+      } else if (childKind == ChildKind::Further) {
+        Out << '\\-> ';
       }
+      PrintNodeData(Out, node);
+      Out << '\n';
     }
+    
+    
+    {    RawText = RawText.drop_front(Pos);
+    unsigned NewlineBytes = measureNewline(RawText);
+    RawText = RawText.drop_front(NewlineBytes);
+  }
+    
+    bool importer::isCFTypeDecl(
+       const clang::TypedefNameDecl *Decl) {
+  if (CFPointeeInfo::classifyTypedef(Decl))
+    return true;
+  return false;
+}
+    
+      bool isStaticMember() const {
+    return isImportAsMember() && !selfIndex.hasValue();
+  }
+    
+    // Generate param traits read methods.
+#include 'ipc/param_traits_read_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+    
+    #endif  // CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+
+    
+    
+namespace nwapi {
+    }
+    
+    #include 'base/run_loop.h'
+#include 'base/values.h'
+#include 'base/strings/utf_string_conversions.h'
+#include 'base/message_loop/message_loop_current.h'
+#include 'content/nw/src/api/object_manager.h'
+#include 'content/nw/src/api/menuitem/menuitem.h'
+#include 'content/public/browser/render_frame_host.h'
+#include 'content/public/browser/render_view_host.h'
+#include 'content/public/browser/render_widget_host_view.h'
+#include 'content/public/browser/web_contents.h'
+#include 'extensions/browser/app_window/app_window.h'
+#include 'skia/ext/image_operations.h'
+#include 'ui/aura/client/screen_position_client.h'
+#include 'ui/aura/window.h'
+#include 'ui/aura/window_tree_host.h'
+#include 'ui/events/platform/platform_event_source.h'
+#include 'ui/views/controls/menu/menu_runner.h'
+#include 'ui/views/widget/widget.h'
+#include 'ui/views/focus/focus_manager.h'
+#include 'vector'
+    
+    void MenuItem::CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) {
+  if (method == 'GetChecked') {
+    result->AppendBoolean(GetChecked());
+  } else {
+    NOTREACHED() << 'Invalid call to MenuItem method:' << method
+                 << ' arguments:' << arguments;
   }
 }
     
-      static std::string FormatNumber(const char* fmt, int64_t n);
-  static std::string FormatSize(int64_t size);
-  static std::string FormatTime(int64_t milliSeconds);
+    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringWithFixupFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     
-    #include 'hphp/runtime/base/file-util.h'
-#include 'hphp/runtime/base/stream-wrapper-registry.h'
-#include 'hphp/runtime/base/unit-cache.h'
-#include 'hphp/runtime/ext/std/ext_std_file.h'
-#include 'hphp/util/logger.h'
+ protected:
+  ~NwMenuGetNSStringWithFixupFunction() override {}
     
-    #include 'hphp/runtime/base/glob-stream-wrapper.h'
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
+};
+    
+        base::LazyInstance<NwDesktopCaptureMonitor>::Leaky
+      g_desktop_capture_monitor = LAZY_INSTANCE_INITIALIZER;
+    
+    #include 'extensions/browser/extension_function.h'
+    
+    ```
+    
+    namespace caffe2 {
+    }
+    
+    OPERATOR_SCHEMA(GatherRangesToDense)
+    .NumInputs(2, 3)
+    .NumOutputs(1, INT_MAX)
+    .SetDoc(R'DOC(
+Given DATA tensor of rank 1, and RANGES tensor of rank 3, gather values
+corresponding to each range into a separate output tensor. If the optional input
+KEY tensor is also given, the output will be sorted by KEY for each example.
+    
+      // Compute the foreground pixel density for a tbox area.
+  float ComputeForegroundDensity(const TBOX& tbox);
+    
+      /**
+   * Page/ResultIterators may be copied! This makes it possible to iterate over
+   * all the objects at a lower level, while maintaining an iterator to
+   * objects at a higher level. These constructors DO NOT CALL Begin, so
+   * iterations will continue from the location of src.
+   */
+  PageIterator(const PageIterator& src);
+  const PageIterator& operator=(const PageIterator& src);
+    
+      STRING lword_text;   // the UTF-8 text of the leftmost werd
+  STRING rword_text;   // the UTF-8 text of the rightmost werd
+    
+    #define UNLV_EXT  '.uzn'  // unlv zone file
+    
+     private:
+  std::shared_ptr<Cache> cache_;
+  uint32_t num_threads_;
+    
+      int64_t num_record_drop_hidden = 0;
+  int64_t num_record_drop_obsolete = 0;
+  int64_t num_record_drop_range_del = 0;
+  int64_t num_range_del_drop_obsolete = 0;
+  // Deletions obsoleted before bottom level due to file gap optimization.
+  int64_t num_optimized_del_drop_obsolete = 0;
+  uint64_t total_filter_time = 0;
+    
+      virtual Status Flush() override;
+  virtual Status Sync() override;
+  virtual Status Fsync() override;
+  virtual Status Close() override;
+    
+      // Do a write outside of the transaction to key 'y'
+  s = db->Put(write_options, 'y', 'y');
+    
+    #include 'rocksdb/env.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/statistics.h'
+#include 'rocksdb/status.h'
+    
+    namespace rocksdb {
+    }
