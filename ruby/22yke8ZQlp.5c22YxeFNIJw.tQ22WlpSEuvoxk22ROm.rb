eca9d8256c0dbc3c5c1d49aba5c2d5627d7d5b44
SUNsung <weatherius@gmail.com>
2019-01-12 12:34:28 +0200

    Rewrite [22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb] File in: 2019-01-12 12:34:27

--- ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
+++ ruby/22yke8ZQlp.5c22YxeFNIJw.tQ22WlpSEuvoxk22ROm.rb
@@ -1,127 +1,49 @@
 
         
-            checks.inject_dump_stats! if ARGV.switch? 'D'
-    
-      def search
-    if ARGV.empty?
-      puts_columns Formula.full_names
-    elsif ARGV.include? '--macports'
-      exec_browser 'https://www.macports.org/ports.php?by=name&substr=#{ARGV.next}'
-    elsif ARGV.include? '--fink'
-      exec_browser 'http://pdb.finkproject.org/pdb/browse.php?summary=#{ARGV.next}'
-    elsif ARGV.include? '--debian'
-      exec_browser 'https://packages.debian.org/search?keywords=#{ARGV.next}&searchon=names&suite=all&section=all'
-    elsif ARGV.include? '--opensuse'
-      exec_browser 'https://software.opensuse.org/search?q=#{ARGV.next}'
-    elsif ARGV.include? '--fedora'
-      exec_browser 'https://admin.fedoraproject.org/pkgdb/packages/%2A#{ARGV.next}%2A/'
-    elsif ARGV.include? '--ubuntu'
-      exec_browser 'http://packages.ubuntu.com/search?keywords=#{ARGV.next}&searchon=names&suite=all&section=all'
-    elsif ARGV.include? '--desc'
-      query = ARGV.next
-      rx = query_regexp(query)
-      Descriptions.search(rx, :desc).print
-    elsif ARGV.first =~ HOMEBREW_TAP_FORMULA_REGEX
-      query = ARGV.first
-      user, repo, name = query.split('/', 3)
-    
-      def self.path(name)
-    Formulary.core_path(name)
+          def parse(*paths)
+    paths.flatten
+         .compact
+         .flat_map { |p| Pathname.new(p).to_path.split(File::PATH_SEPARATOR) }
+         .uniq
   end
-    
-    end
+end
 
     
-        it 'does not output links to other agents outside of the incoming set' do
-      Link.create!(:source_id => agents(:jane_weather_agent).id, :receiver_id => agents(:jane_website_agent).id)
-      Link.create!(:source_id => agents(:jane_website_agent).id, :receiver_id => agents(:jane_rain_notifier_agent).id)
+          return unless user == 'caskroom'
     
-      describe 'migrating an actual agent' do
-    before do
-      valid_params = {
-                        'auth_token' => 'token',
-                        'room_name' => 'test',
-                        'room_name_path' => '',
-                        'username' => 'Huginn',
-                        'username_path' => '$.username',
-                        'message' => 'Hello from Huginn!',
-                        'message_path' => '$.message',
-                        'notify' => false,
-                        'notify_path' => '',
-                        'color' => 'yellow',
-                        'color_path' => '',
-                      }
+      # Configure an appender that will write log events to STDOUT. A colorized
+  # pattern layout is used to format the log events into strings before
+  # writing.
+  Logging.appenders.stdout('stdout',
+                           auto_flushing: true,
+                           layout:        Logging.layouts.pattern(
+                             pattern:      pattern,
+                             color_scheme: 'bright'
+                           )
+                          ) if config.log_to.include? 'stdout'
     
-          a = '        Events will have the fields you specified.  Your options look like:\n\n            {\n      \'url\': {\n        \'css\': \'#comic img\',\n        \'value\': \'@src\'\n      },\n      \'title\': {\n        \'css\': \'#comic img\',\n        \'value\': \'@title\'\n      }\n    }\'\n'
-      expect(Utils.unindent(a)).to eq('Events will have the fields you specified.  Your options look like:\n\n    {\n      \'url\': {\n\'css\': \'#comic img\',\n\'value\': \'@src\'\n      },\n      \'title\': {\n\'css\': \'#comic img\',\n\'value\': \'@title\'\n      }\n    }\'')
-    end
-  end
+    When /^I (?:sign|log) in manually as '([^']*)' with password '([^']*)'( on the mobile website)?$/ \
+do |username, password, mobile|
+  @me = User.find_by_username(username)
+  @me.password ||= password
+  manual_login
+  confirm_login mobile
+end
     
-      describe '#log_length' do
-    it 'defaults to 200' do
-      expect(AgentLog.log_length).to eq(200)
+        it 'generates the contacts_json fixture', :fixture => true do
+      json = bob.contacts.map { |c|
+               ContactPresenter.new(c, bob).full_hash_with_person
+             }.to_json
+      save_fixture(json, 'contacts_json')
     end
   end
 end
 
     
-          # @see Base#\_store
-      def _store(key, version, sha, contents)
-        compiled_filename = path_to(key)
-        FileUtils.mkdir_p(File.dirname(compiled_filename))
-        Sass::Util.atomic_create_and_write_file(compiled_filename) do |f|
-          f.puts(version)
-          f.puts(sha)
-          f.write(contents)
-        end
-      rescue Errno::EACCES
-        # pass
-      end
-    
-            sseq = first_sseq(child)
-        next child unless sseq.is_a?(Sass::Selector::SimpleSequence)
-    
-            if e.is_a?(Sass::SyntaxError)
-          $stderr.puts e.sass_backtrace_str('standard input')
-        else
-          $stderr.print '#{e.class}: ' unless e.class == RuntimeError
-          $stderr.puts e.message.to_s
-        end
-        $stderr.puts '  Use --trace for backtrace.'
-    
-          out =
-        Sass::Util.silence_sass_warnings do
-          if @options[:from] == :css
-            require 'sass/css'
-            Sass::CSS.new(read(input), @options[:for_tree]).render(@options[:to])
-          else
-            if input_path
-              Sass::Engine.for_file(input_path, @options[:for_engine])
-            else
-              Sass::Engine.new(read(input), @options[:for_engine])
-            end.to_tree.send('to_#{@options[:to]}', @options[:for_tree])
-          end
-        end
-    
-          # Checks whether any of the key value pairs in the `hash` literal are on
-      # the same line.
-      #
-      # @note A multiline `pair` is considered to be on the same line if it
-      #       shares any of its lines with another `pair`
-      #
-      # @return [Boolean] whether any `pair` nodes are on the same line
-      def pairs_on_same_line?
-        pairs.each_cons(2).any? { |first, second| first.same_line?(second) }
-      end
-    
-          DOUBLE_SPLAT = '**'.freeze
-    
-          # Checks whether any argument of the node is a splat
-      # argument, i.e. `*splat`.
-      #
-      # @return [Boolean] whether the node is a splat argument
-      def splat_argument?
-        arguments? &&
-          (arguments.any?(&:splat_type?) || arguments.any?(&:restarg_type?))
-      end
-      alias rest_argument? splat_argument?
+          delete :destroy, params: {post_id: @message.id, id: like2.id}, format: :json
+      expect(response.status).to eq(404)
+      expect(response.body).to eq(I18n.t('likes.destroy.error'))
+      expect(Like.count).to eq(like_count)
+    end
+  end
+end
