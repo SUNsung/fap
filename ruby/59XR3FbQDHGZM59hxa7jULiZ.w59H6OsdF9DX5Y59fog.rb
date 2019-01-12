6990471a4b01eef9e36f24e2b817186801b75a49
SUNsung <weatherius@gmail.com>
2019-01-12 02:50:47 +0200

    Rewrite [59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb] File in: 2019-01-12 02:50:46

--- ruby/59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb
+++ ruby/59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb
@@ -1,66 +1,161 @@
 
         
-                  builder = LabelBuilder.new(@template_object, @object_name, @method_name, @object, tag_value)
+                  def select_type
+            self.class.select_type
+          end
     
-            def render
-          options = @options.stringify_keys
-          options['size'] = options['maxlength'] unless options.key?('size')
-          options['type'] ||= field_type
-          options['value'] = options.fetch('value') { value_before_type_cast } unless field_type == 'file'
-          add_default_name_and_id(options)
-          tag('input', options)
+              def field_type
+            self.class.field_type
+          end
+      end
+    end
+  end
+end
+
+    
+        initializer 'action_view.per_request_digest_cache' do |app|
+      ActiveSupport.on_load(:action_view) do
+        unless ActionView::Resolver.caching?
+          app.executor.to_run ActionView::Digestor::PerExecutionDigestCacheExpiry
         end
+      end
+    end
     
-        attr_reader :paths
+        # Render but returns a valid Rack body. If fibers are defined, we return
+    # a streaming body that renders the template piece by piece.
+    #
+    # Note that partials are not supported to be rendered with streaming,
+    # so in such cases, we just wrap them in an array.
+    def render_body(context, options)
+      if options.key?(:partial)
+        [render_partial(context, options)]
+      else
+        StreamingTemplateRenderer.new(@lookup_context).render(context, options)
+      end
+    end
     
-            # Allows setting options from a hash. By default this simply calls
-        # the `#{key}=` method on the config class with the value, which is
-        # the expected behavior most of the time.
-        #
-        # This is expected to mutate itself.
-        #
-        # @param [Hash] options A hash of options to set on this configuration
-        #   key.
-        def set_options(options)
-          options.each do |key, value|
-            send('#{key}=', value)
+              # Go through each VM and yield it!
+          vms.each do |old_vm|
+            # We get a new VM from the environment here to avoid potentially
+            # stale VMs (if there was a config reload on the environment
+            # or something).
+            vm = @env.vms[old_vm.name]
+            yield vm
           end
         end
     
-                    raise Errors::VMNoMatchError if machines.empty?
-              else
-                # String name, just look for a specific VM
-                @logger.debug('Finding machine that match name: #{name}')
-                machines << get_machine.call(name.to_sym)
-                raise Errors::VMNotFoundError, name: name if !machines[0]
-              end
-            end
+              result
+        end
+    
+            # Registers additional providers to be available.
+        #
+        # @param [Symbol] name Name of the provider.
+        def self.provider(name=UNSET_VALUE, &block)
+          data[:providers] ||= Registry.new
+    
+      # log-levels from the diaspora.yml for SQL and federation debug-logging
+  Logging.logger[ActionView::Base].level = Rails.env.development? ? :debug : :warn
+  Logging.logger[ActiveRecord::Base].level = AppConfig.environment.logging.debug.sql? ? :debug : :info
+  Logging.logger[DiasporaFederation::Salmon::MagicEnvelope].level =
+    AppConfig.environment.logging.debug.federation? ? :debug : :info
+    
+        remove_duplicates
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
+    
+      def navigate_to(page_name)
+    path = path_to(page_name)
+    if path.is_a?(Hash)
+      visit(path[:path])
+      await_elem = path[:special_elem]
+      find(await_elem.delete(:selector), await_elem)
+    else
+      visit(path)
+    end
+  end
+    
+        it 'generates a jasmine fixture', fixture: true do
+      session[:mobile_view] = true
+      get :new, format: :mobile
+      save_fixture(html_for('body'), 'conversations_new_mobile')
+    end
+  end
+end
+
+    
+      end
+    
+        context 'on a post you partecipate to' do
+      before { alice.participate! post }
+    
+          it 'returns an empty array for a post visible to the user' do
+        sign_in(bob, scope: :user)
+        get :index, params: {post_id: @post.id}, format: :json
+        expect(JSON.parse(response.body)).to eq([])
+      end
+    end
+    
+    desc 'Creates a sandbox application for simulating the Spree code in a deployed Rails app'
+task :sandbox do
+  Bundler.with_clean_env do
+    exec('lib/sandbox.sh')
+  end
+end
+
+    
+            def find_property
+          @property = Spree::Property.accessible_by(current_ability, :read).find(params[:id])
+        rescue ActiveRecord::RecordNotFound
+          @property = Spree::Property.accessible_by(current_ability, :read).find_by!(name: params[:id])
+        end
+    
+            def scope
+          if params[:country_id]
+            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
+            @country.states.accessible_by(current_ability, :read).order('name ASC')
           else
-            # No name was given, so we return every VM in the order
-            # configured.
-            @logger.debug('Loading all machines...')
-            machines = @env.machine_names.map do |machine_name|
-              get_machine.call(machine_name)
-            end
+            State.accessible_by(current_ability, :read).order('name ASC')
           end
+        end
+      end
+    end
+  end
+end
+
     
-            # This contains all the command plugins by name, and returns
-        # the command class and options. The command class is wrapped
-        # in a Proc so that it can be lazy loaded.
-        #
-        # @return [Registry<Symbol, Array<Proc, Hash>>]
-        attr_reader :commands
+            def stock_item_params
+          params.require(:stock_item).permit(permitted_stock_item_attributes)
+        end
+      end
+    end
+  end
+end
+
     
-            # This should return the state of the machine within this provider.
-        # The state must be an instance of {MachineState}. Please read the
-        # documentation of that class for more information.
-        #
-        # @return [MachineState]
-        def state
-          nil
+            def stock_location
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:id])
         end
     
-    # for each blog URL, check if rss URL exists
-matches.each do |match|
-  name = match[0]
-  web_url = match[1]
+            def taxon_params
+          if params[:taxon] && !params[:taxon].empty?
+            params.require(:taxon).permit(permitted_taxon_attributes)
+          else
+            {}
+          end
+        end
+      end
+    end
+  end
+end
+
+    
+            def update
+          authorize! :update, user
+          if user.update_attributes(user_params)
+            respond_with(user, status: 200, default_template: :show)
+          else
+            invalid_resource!(user)
+          end
+        end
+    
+    end
