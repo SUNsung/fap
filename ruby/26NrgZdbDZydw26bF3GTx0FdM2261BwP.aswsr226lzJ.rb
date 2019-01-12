8d690498635dada035f6159bb8a1f800e30f54b9
SUNsung <weatherius@gmail.com>
2019-01-12 03:52:52 +0200

    Rewrite [26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb] File in: 2019-01-12 03:52:51

--- ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
+++ ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
@@ -1,177 +1,69 @@
 
         
-                default_connectors = {
-          words_connector: ', ',
-          two_words_connector: ' and ',
-          last_word_connector: ', and '
-        }
-        if defined?(I18n)
-          i18n_connectors = I18n.translate(:'support.array', locale: options[:locale], default: {})
-          default_connectors.merge!(i18n_connectors)
-        end
-        options = default_connectors.merge!(options)
+            it 'orders stuff correctly' do
+      a = unread_pm
+          regular
+      c = pm
+      d = regular
     
-                if html_options['multiple'] && options.fetch(:include_hidden, true)
-              tag('input', disabled: html_options['disabled'], name: html_options['name'], type: 'hidden', value: '') + select
-            else
-              select
+          if staff.topic_id.nil?
+        creator = PostCreator.new(Discourse.system_user,
+          raw: I18n.t('staff_category_description'),
+          title: I18n.t('category.topic_prefix', category: staff.name),
+          category: staff.name,
+          archetype: Archetype.default
+        )
+        post = creator.create
+    
+    require 'vagrant/capability_host'
+    
+                  # Break so we don't find the next non flag and shift our
+              # main args.
+              break
             end
           end
     
-                  [:year, :month, :day, :hour, :min, :sec].each do |key|
-                default[key] ||= time.send(key)
-              end
+              # Register a new guest class only if a name was given
+          data[:guests].register(name.to_sym, &block) if name != UNSET_VALUE
     
-        def compact
-      PathSet.new paths.compact
-    end
-    
-        # Main render entry point shared by Action View and Action Controller.
-    def render(context, options)
-      if options.key?(:partial)
-        render_partial(context, options)
-      else
-        render_template(context, options)
+        # Merge one registry with another and return a completely new
+    # registry. Note that the result cache is completely busted, so
+    # any gets on the new registry will result in a cache miss.
+    def merge(other)
+      self.class.new.tap do |result|
+        result.merge!(self)
+        result.merge!(other)
       end
     end
     
-        # This returns whether the guest is ready to work. If this returns
-    # `false`, then {#detect!} should be called in order to detect the
-    # guest OS.
-    #
-    # @return [Boolean]
-    def ready?
-      !!capability_host_chain
-    end
-  end
+    Then(/^the failure task will not run$/) do
+  failed = TestApp.shared_path.join('failed')
+  expect { run_vagrant_command(test_file_exists(failed)) }
+    .to raise_error(VagrantHelpers::VagrantSSHCommandError)
 end
-
     
-    module Vagrant
-  # MachineIndex is able to manage the index of created Vagrant environments
-  # in a central location.
-  #
-  # The MachineIndex stores a mapping of UUIDs to basic information about
-  # a machine. The UUIDs are stored with the Vagrant environment and are
-  # looked up in the machine index.
-  #
-  # The MachineIndex stores information such as the name of a machine,
-  # the directory it was last seen at, its last known state, etc. Using
-  # this information, we can load the entire {Machine} object for a machine,
-  # or we can just display metadata if needed.
-  #
-  # The internal format of the data file is currently JSON in the following
-  # structure:
-  #
-  #   {
-  #     'version': 1,
-  #     'machines': {
-  #       'uuid': {
-  #         'name': 'foo',
-  #         'provider': 'vmware_fusion',
-  #         'data_path': '/path/to/data/dir',
-  #         'vagrantfile_path': '/path/to/Vagrantfile',
-  #         'state': 'running',
-  #         'updated_at': '2014-03-02 11:11:44 +0100'
-  #       }
-  #     }
-  #   }
-  #
-  class MachineIndex
-    include Enumerable
+    http://capistranorb.com/documentation/advanced-features/custom-scm
     
-            # This is called as a last-minute hook that allows the configuration
-        # object to finalize itself before it will be put into use. This is
-        # a useful place to do some defaults in the case the user didn't
-        # configure something or so on.
-        #
-        # An example of where this sort of thing is used or has been used:
-        # the 'vm' configuration key uses this to make sure that at least
-        # one sub-VM has been defined: the default VM.
-        #
-        # The configuration object is expected to mutate itself.
-        def finalize!
-          # Default implementation is to do nothing.
+          # Runs all validation rules registered for the given key against the
+      # user-supplied value for that variable. If no validator raises an
+      # exception, the value is assumed to be valid.
+      def assert_valid_now(key, value)
+        validators[key].each do |validator|
+          validator.call(key, value)
         end
-    
-              result
-        end
-    
-              # Return the dummy object so that anything else works
-          ::Vagrant::Config::V2::DummyConfig.new
-        end
-    
-            # This should return the state of the machine within this provider.
-        # The state must be an instance of {MachineState}. Please read the
-        # documentation of that class for more information.
-        #
-        # @return [MachineState]
-        def state
-          nil
-        end
-    
-            directives.compact.sort.join('; ')
       end
     
-          def handle(hash)
-        was = hash.dup
-        hash.replace escape(hash)
-        was
-      end
+        BUNDLE_DIR = ::File.join(LOGSTASH_HOME, 'vendor', 'bundle')
+    GEMFILE_PATH = ::File.join(LOGSTASH_HOME, 'Gemfile')
+    LOCAL_GEM_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'local_gems')
+    CACHE_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'cache')
+    LOCKFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile.lock'))
+    GEMFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile'))
     
-      teardown do
-    FileUtils.rm_r(File.join(File.dirname(__FILE__), *%w[examples test.git]))
-  end
+    require 'bootstrap/environment'
     
-        post '/edit/' + CGI.escape('한글'), :page => 'k', :content => '바뀐 text',
-         :format                            => 'markdown', :message => 'ghi'
-    follow_redirect!
-    assert last_response.ok?
-    
-      # replace name version and date
-  replace_header(head, :name)
-  replace_header(head, :version)
-  replace_header(head, :date)
-  #comment this out if your rubyforge_project has a different name
-  replace_header(head, :rubyforge_project)
-    
-      if cfg = options[:config]
-    # If the path begins with a '/' it will be considered an absolute path,
-    # otherwise it will be relative to the CWD
-    cfg = File.join(Dir.getwd, cfg) unless cfg.slice(0) == File::SEPARATOR
-    require cfg
-  end
-    
-        EMOJI_PATHNAME = Pathname.new(Gemojione.images_path).freeze
-    
-          def requires_authentication?
-        Spree::Api::Config[:requires_authentication]
-      end
-    
-            def option_type_params
-          params.require(:option_type).permit(permitted_option_type_attributes)
-        end
+            return nil
       end
     end
   end
-end
-
-    
-            def index
-          @payments = @order.payments.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
-          respond_with(@payments)
-        end
-    
-            def stock_location
-          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
-          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
-        end
-    
-            def update
-          authorize! :update, stock_location
-          if stock_location.update_attributes(stock_location_params)
-            respond_with(stock_location, status: 200, default_template: :show)
-          else
-            invalid_resource!(stock_location)
-          end
-        end
+end end end
