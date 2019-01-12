df9e5d473b3c955e4867ffcf23c5bbd5b84a37e2
SUNsung <weatherius@gmail.com>
2019-01-13 01:37:02 +0200

    Rewrite [32M4oa56aTQeU32wtwDr8pzzpA32Uc5oKrftvqs32NyZ.py] File in: 2019-01-13 01:37:02

--- pyton/32M4oa56aTQeU32wtwDr8pzzpA32Uc5oKrftvqs32NyZ.py
+++ pyton/32M4oa56aTQeU32wtwDr8pzzpA32Uc5oKrftvqs32NyZ.py
@@ -1,219 +1,93 @@
 
         
-        
-rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
-u_rng = np.random.RandomState(seed=FLAGS.synth_data_seed+1)
-T = FLAGS.T
-C = FLAGS.C
-N = FLAGS.N  # must be same N as in trained model (provided example is N = 50)
-nreplications = FLAGS.nreplications
-E = nreplications * C  # total number of trials
-train_percentage = FLAGS.train_percentage
-ntimesteps = int(T / FLAGS.dt)
-batch_size = 1  # gives one example per ntrial
+            return content
     
-    flags = tf.app.flags
-flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
-                    'Directory for saving data.')
-flags.DEFINE_string('datafile_name', 'conditioned_rnn_data',
-                    'Name of data file for input case.')
-flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
-flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
-flags.DEFINE_integer('C', 400, 'Number of conditions')
-flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
-flags.DEFINE_float('train_percentage', 4.0/5.0,
-                   'Percentage of train vs validation trials')
-flags.DEFINE_integer('nreplications', 10,
-                     'Number of spikifications of the same underlying rates.')
-flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
-flags.DEFINE_float('x0_std', 1.0,
-                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
-flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
-flags.DEFINE_float('dt', 0.010, 'Time bin')
-flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
-FLAGS = flags.FLAGS
+            :param request: The :class:`PreparedRequest <PreparedRequest>` being sent.
+        :param stream: (optional) Whether to stream the request content.
+        :param timeout: (optional) How long to wait for the server to send
+            data before giving up, as a float, or a :ref:`(connect timeout,
+            read timeout) <timeouts>` tuple.
+        :type timeout: float or tuple
+        :param verify: (optional) Either a boolean, in which case it controls whether we verify
+            the server's TLS certificate, or a string, in which case it must be a path
+            to a CA bundle to use
+        :param cert: (optional) Any user-provided SSL certificate to be trusted.
+        :param proxies: (optional) The proxies dictionary to apply to the request.
+        '''
+        raise NotImplementedError
     
-        if FLAGS.prefix_label and use_prefix:
-      label = sequence_example.context.feature['class'].int64_list.value[0]
-      review_words = [EOS_INDEX + 1 + label]
-    else:
-      review_words = []
-    review_words.extend([
-        f.int64_list.value[0]
-        for f in sequence_example.feature_lists.feature_list['token_id'].feature
-    ])
-    all_words.append(review_words)
-  return all_words
-    
-    FLAGS = tf.app.flags.FLAGS
-    
-          for s in xrange(t, FLAGS.sequence_length):
-        cum_advantage += missing_list[s] * np.power(gamma,
-                                                    (s - t)) * rewards_list[s]
-      cum_advantage -= baselines[t]
-      # Clip advantages.
-      cum_advantage = tf.clip_by_value(cum_advantage, -FLAGS.advantage_clipping,
-                                       FLAGS.advantage_clipping)
-      advantages.append(missing_list[t] * cum_advantage)
-      final_gen_objective += tf.multiply(
-          log_probability, missing_list[t] * tf.stop_gradient(cum_advantage))
-    
-    
-def attention_decoder_fn_train(encoder_state,
-                               attention_keys,
-                               attention_values,
-                               attention_score_fn,
-                               attention_construct_fn,
-                               name=None):
-  '''Attentional decoder function for `dynamic_rnn_decoder` during training.
-    
-    
-@pytest.mark.functional
-def test_with_confirmation(proc, TIMEOUT):
-    with_confirmation(proc, TIMEOUT)
-    
-    
-@pytest.mark.parametrize('command', [
-    Command('aws dynamdb scan', misspelled_command),
-    Command('aws dynamodb scn', misspelled_subcommand),
-    Command('aws dynamodb t-item',
-            misspelled_subcommand_with_multiple_options)])
-def test_match(command):
-    assert match(command)
-    
-    
-output = '''sshfs: OsxfuseRequirement unsatisfied!
-    
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_get_new_command(brew_no_available_formula):
-    assert get_new_command(Command('brew install elsticsearch',
-                                   brew_no_available_formula))\
-        == 'brew install elasticsearch'
-    
-    from setuptools import setup, find_packages
-setup(
-    name = proj_info['name'],
-    version = VERSION,
-    
-        if not opts and not args:
-        # Display help.
-        print(_help)
-        # Enter GUI mode.
-        #from .gui import gui_main
-        #gui_main()
-    else:
-        conf = {}
-        for opt, arg in opts:
-            if opt in ('-h', '--help'):
-                # Display help.
-                print(_help)
-    
-    
-def cntv_download_by_id(rid, **kwargs):
-    CNTV().download_by_vid(rid, **kwargs)
-    
-    __all__ = ['dilidili_download']
-    
-        type, ext, size = url_info(urls[0], True)
-    size = urls_size(urls)
-    
-            for p in js_path:
-            if 'mtool' in p or 'mcore' in p:
-                js_text = get_content(p)
-                hit = re.search(r'\(\'(.+?)\',(\d+),(\d+),\'(.+?)\'\.split\(\'\|\'\),\d+,\{\}\)', js_text)
-    
-        type, ext, size = url_info(url[0], True)
-    size = urls_size(url)
-    
-            real_url = 'https://docs.google.com/uc?export=download&confirm=no_antivirus&id=%s' % docid
-        redirected_url = get_location(real_url)
-        if real_url != redirected_url:
-# tiny file - get real url here
-            type, ext, size = url_info(redirected_url)
-            real_url = redirected_url
+            if nonce == self._thread_local.last_nonce:
+            self._thread_local.nonce_count += 1
         else:
-# huge file - the real_url is a confirm page and real url is in it
-            confirm_page = get_content(real_url)
-            hrefs = re.findall(r'href='(.+?)'', confirm_page)
-            for u in hrefs:
-                if u.startswith('/uc?export=download'):
-                    rel = unescape_html(u)
-            confirm_url = 'https://docs.google.com' + rel
-            real_url = get_location(confirm_url)
-            _, ext, size = url_info(real_url, headers=fake_headers)
-            if size is None:
-                size = 0
+            self._thread_local.nonce_count = 1
+        ncvalue = '%08x' % self._thread_local.nonce_count
+        s = str(self._thread_local.nonce_count).encode('utf-8')
+        s += nonce.encode('utf-8')
+        s += time.ctime().encode('utf-8')
+        s += os.urandom(8)
+    
+    import sys
+    
+        # Informational.
+    100: ('continue',),
+    101: ('switching_protocols',),
+    102: ('processing',),
+    103: ('checkpoint',),
+    122: ('uri_too_long', 'request_uri_too_long'),
+    200: ('ok', 'okay', 'all_ok', 'all_okay', 'all_good', '\\o/', '✓'),
+    201: ('created',),
+    202: ('accepted',),
+    203: ('non_authoritative_info', 'non_authoritative_information'),
+    204: ('no_content',),
+    205: ('reset_content', 'reset'),
+    206: ('partial_content', 'partial'),
+    207: ('multi_status', 'multiple_status', 'multi_stati', 'multiple_stati'),
+    208: ('already_reported',),
+    226: ('im_used',),
     
     
-# -- Options for HTML output ----------------------------------------------
+def bench_scikit_tree_classifier(X, Y):
+    '''Benchmark with scikit-learn decision tree classifier'''
     
-    For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/browser/
+            :issue:`123`
+        :issue:`42,45`
+    '''
+    options = options or {}
+    content = content or []
+    issue_nos = [each.strip() for each in utils.unescape(text).split(',')]
+    config = inliner.document.settings.env.app.config
+    ret = []
+    for i, issue_no in enumerate(issue_nos):
+        node = _make_issue_node(issue_no, config, options=options)
+        ret.append(node)
+        if i != len(issue_nos) - 1:
+            sep = nodes.raw(text=', ', format='html')
+            ret.append(sep)
+    return ret, []
+    
+        text_lang_folder = os.path.join(text_folder, lang)
+    if not os.path.exists(text_lang_folder):
+        os.makedirs(text_lang_folder)
+    
+    
+URL = ('http://www.cs.cornell.edu/people/pabo/'
+       'movie-review-data/review_polarity.tar.gz')
+    
+    from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+    
+    from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
+from sklearn.pipeline import Pipeline
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+    
+    For each class of models we make the model complexity vary through the choice
+of relevant model parameters and measure the influence on both computational
+performance (latency) and predictive power (MSE or Hamming Loss).
 '''
-import voluptuous as vol
-    
-            devices = {}
-        for lease in leases_result:
-            match = _LEASES_REGEX.search(lease.decode('utf-8'))
-            if match is not None:
-                devices[match.group('ip')] = {
-                    'ip': match.group('ip'),
-                    'mac': match.group('mac').upper(),
-                    'timevalid': int(match.group('timevalid'))
-                    }
-        return devices
-
-    
-    
-def setup_scanner(hass, config, see, discovery_info=None):
-    '''Set up the demo tracker.'''
-    def offset():
-        '''Return random offset.'''
-        return (random.randrange(500, 2000)) / 2e5 * random.choice((-1, 1))
-    
-            _LOGGER.info('Request successful')
-        return True
-
-    
-        def get_extra_attributes(self, device):
-        '''Return the IP of the given device.'''
-        filter_ip = next((
-            result.ip for result in self.last_results
-            if result.mac == device), None)
-        return {'ip': filter_ip}
-    
-        async def async_scan_devices(self):
-        '''Scan for devices and return a list containing found device ids.'''
-        await self._async_update_info()
-        return [device.mac for device in self.last_results]
-    
-                    subdir = service.data.get(ATTR_SUBDIR)
-    
-    
-def setup(hass, config):
-    '''Set up the Hive Component.'''
-    from pyhiveapi import Pyhiveapi
-    
-    '''
-A class that uses different static function depending of a parameter passed in
-init. Note the use of a single dictionary instead of multiple conditions
-'''
-    
-        def prepare(self):
-        print('Reporter Class is preparing to report the results')
-        time.sleep(0.1)
-    
-    from __future__ import print_function
-    
-        '''A radio.     It has a scan button, and an AM/FM toggle switch.'''
-    
-    '''
-http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
-    
-    '''
-Port of the Java example of 'Parameter Injection' in
-'xUnit Test Patterns - Refactoring Test Code' by Gerard Meszaros
-(ISBN-10: 0131495054, ISBN-13: 978-0131495050) accessible in outdated version on
-http://xunitpatterns.com/Dependency%20Injection.html.
