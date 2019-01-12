11aa7b1f6e0cf24965aba06dc58fcbe1ba4e4dcd
SUNsung <weatherius@gmail.com>
2019-01-12 06:38:48 +0200

    Rewrite [04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb] File in: 2019-01-12 06:38:47

--- ruby/04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb
+++ ruby/04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb
@@ -1,65 +1,78 @@
 
         
-              it 'requires the passwords to match' do
-        visit new_admin_user_path
-        fill_in 'Email', with: 'test@test.com'
-        fill_in 'Username', with: 'usertest'
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: 'no_match'
-        click_on 'Create User'
-        expect(page).to have_text('Password confirmation doesn't match')
-      end
-    end
+                def importer_class
+          DiffNoteImporter
+        end
     
-        it 'shows the dry run pop up with previous events and allows use previously received event' do
-      emitter.events << Event.new(payload: {url: 'http://xkcd.com/'})
-      agent.sources << emitter
-      agent.options.merge!('url' => '', 'url_from_event' => '{{url}}')
-      agent.save!
+            # attributes - A Hash containing the raw note details. The keys of this
+        #              Hash must be Symbols.
+        def initialize(attributes)
+          @attributes = attributes
+        end
     
-      describe '#working' do
-    before do
-      @agent = agents(:jane_website_agent)
-    end
+            # Builds a new PR using a Hash that was built from a JSON payload.
+        def self.from_json_hash(raw_hash)
+          hash = Representation.symbolize_hash(raw_hash)
     
-              @bar2 = Agents::DotBar.new(name: 'bar2').tap { |agent|
-            agent.user = users(:bob)
-            agent.sources << @foo
-            agent.propagate_immediately = true
-            agent.disabled = true
-            agent.save!
-          },
+      # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both thread web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
+  config.eager_load = true
     
-        it 'should work with nested arrays' do
-      @agent.options['array'] = ['one', '$.two']
-      LiquidMigrator.convert_all_agent_options(@agent)
-      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'array' => ['one', '{{two}}'], 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
-    end
-    
-      let :valid_options do
-    {
-      'name' => 'XKCD',
-      'expected_update_period_in_days' => '2',
-      'type' => 'html',
-      'url' => '{{ url | default: 'http://xkcd.com/' }}',
-      'mode' => 'on_change',
-      'extract' => old_extract,
-      'template' => old_template
-    }
+      def test_image_helper
+    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
   end
     
-        set :run, Proc.new { File.expand_path($0) == File.expand_path(app_file) }
-    
-          DIRECTIVES = %i(base_uri child_src connect_src default_src
-                      font_src form_action frame_ancestors frame_src
-                      img_src manifest_src media_src object_src
-                      plugin_types referrer reflected_xss report_to
-                      report_uri require_sri_for sandbox script_src
-                      style_src worker_src).freeze
-    
-          def cookie_paths(path)
-        path = '/' if path.to_s.empty?
-        paths = []
-        Pathname.new(path).descend { |p| paths << p.to_s }
-        paths
+          def page_dir
+        @page_dir
       end
+    
+      # base path requires 'map' in a config.ru to work correctly.
+  test 'create pages within sub-directories using base path' do
+    Precious::App.set(:wiki_options, { :base_path => 'wiki' })
+    page = 'path'
+    post '/create', :content => '123', :page => page,
+         :path               => 'Mordor', :format => 'markdown', :message => 'oooh, scary'
+    # should be wiki/Mordor/path
+    assert_equal 'http://example.org/Mordor/' + page, last_response.headers['Location']
+    get '/Mordor/' + page
+    assert_match /123/, last_response.body
+    
+      test 'h1 title sanitizes correctly' do
+    title = 'H1'
+    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
+    page = @wiki.page(title)
+    
+        EMOJI_PATHNAME = Pathname.new(Gemojione.images_path).freeze
+    
+            private
+    
+            def scope
+          @scope ||= if params[:option_type_id]
+                       Spree::OptionType.find(params[:option_type_id]).option_values.accessible_by(current_ability, :read)
+                     else
+                       Spree::OptionValue.accessible_by(current_ability, :read).load
+                     end
+        end
+    
+            def perform_payment_action(action, *args)
+          authorize! action, Payment
+          @payment.send('#{action}!', *args)
+          respond_with(@payment, default_template: :show)
+        end
+    
+            def create
+          authorize! :create, Taxon
+          @taxon = Spree::Taxon.new(taxon_params)
+          @taxon.taxonomy_id = params[:taxonomy_id]
+          taxonomy = Spree::Taxonomy.find_by(id: params[:taxonomy_id])
+    
+      def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+  end
+    
+    require 'pathname'
+require './plugins/octopress_filters'
