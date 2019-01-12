9da61402ae428755bb644ac438dade553f5fb923
SUNsung <weatherius@gmail.com>
2019-01-12 02:16:57 +0200

    Rewrite [21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb] File in: 2019-01-12 02:16:56

--- ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
+++ ruby/21mwvZV80BpnQ2166uW53VX0Ac21DeQj8mSfRWU21MP2.rb
@@ -1,57 +1,58 @@
 
         
-                class RadioButtonBuilder < Builder # :nodoc:
-          def radio_button(extra_html_options = {})
-            html_options = extra_html_options.merge(@input_html_options)
-            html_options[:skip_default_ids] = false
-            @template_object.radio_button(@object_name, @method_name, @value, html_options)
-          end
+              lock_file
+    end
+    
+              # Return the registry
+          data[:command]
         end
     
-              content_is_options = content_or_options.is_a?(Hash)
-          if content_is_options
-            options.merge! content_or_options
-            @content = nil
-          else
-            @content = content_or_options
-          end
+              return [main_args, sub_command, sub_args]
+        end
+      end
+    end
+  end
+end
+
     
-    require 'action_view/helpers/tags/placeholderable'
+      class SendPublic < Base
+    def perform(*_args)
+      # don't federate in cucumber
+    end
+  end
     
-        # Render but returns a valid Rack body. If fibers are defined, we return
-    # a streaming body that renders the template piece by piece.
-    #
-    # Note that partials are not supported to be rendered with streaming,
-    # so in such cases, we just wrap them in an array.
-    def render_body(context, options)
-      if options.key?(:partial)
-        [render_partial(context, options)]
-      else
-        StreamingTemplateRenderer.new(@lookup_context).render(context, options)
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+        context 'on my own post' do
+      it 'succeeds' do
+        @target = alice.post :status_message, text: 'AWESOME', to: @alices_aspect.id
+        post :create, params: like_hash, format: :json
+        expect(response.code).to eq('201')
       end
     end
     
-        private
+          it 'returns a 404 for a post not visible to the user' do
+        sign_in(eve, scope: :user)
+        expect {
+          get :index, params: {post_id: @post.id}, format: :json
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
     
-            css('.code-example > h4').each do |node|
-          node['class'] = 'pre-title'
-        end
+            expect(new_source).to eq(['#{prefix}#{open}#{a},',
+                                  '#{b}#{close}',
+                                  suffix].join($RS))
+      end
+    end
+  end
+end
+
     
-            if mod
-          if name == 'Index'
-            return slug.split('/')[1..-2].join('/')
-          elsif name == 'Angular'
-            return slug.split('/').last.split('-').first
-          end
-        end
-    
-            css('pre > code').each do |node|
-          node['class'] ||= ''
-          lang = if node['class'].include?('lang-html') || node.content =~ /\A</
-            'html'
-          elsif node['class'].include?('lang-css')
-            'css'
-          elsif node['class'].include?('lang-js') || node['class'].include?('lang-javascript')
-            'javascript'
-          end
-          node.parent['data-language'] = lang if lang
+          # This is used for duck typing with `pair` nodes which also appear as
+      # `hash` elements.
+      #
+      # @return [false]
+      def hash_rocket?
+        false
+      end
