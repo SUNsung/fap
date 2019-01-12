f05af8b73f890fa4802905ad1f5581f7bc0213c5
SUNsung <weatherius@gmail.com>
2019-01-12 22:46:59 +0200

    Rewrite [20eovpQWXi4AQ20vVYaoBkjxK220d4UpVww20g20cQ7.cpp] File in: 2019-01-12 22:46:58

--- cccccc/20eovpQWXi4AQ20vVYaoBkjxK220d4UpVww20g20cQ7.cpp
+++ cccccc/20eovpQWXi4AQ20vVYaoBkjxK220d4UpVww20g20cQ7.cpp
@@ -1,214 +1,90 @@
 
         
-        template<>
-inline XGBOOST_DEVICE float GradientPairInternal<int64_t>::GetGrad() const {
-  return grad_ * 1e-4f;
-}
-template<>
-inline XGBOOST_DEVICE float GradientPairInternal<int64_t>::GetHess() const {
-  return hess_ * 1e-4f;
-}
-template<>
-inline XGBOOST_DEVICE void GradientPairInternal<int64_t>::SetGrad(float g) {
-  grad_ = static_cast<int64_t>(std::round(g * 1e4));
-}
-template<>
-inline XGBOOST_DEVICE void GradientPairInternal<int64_t>::SetHess(float h) {
-  hess_ = static_cast<int64_t>(std::round(h * 1e4));
-}
-    
-          ASSERT_TRUE(input == output);
-    
-    /*!
- * \brief Macro to register tree split evaluator.
- *
- * \code
- * // example of registering a split evaluator
- * XGBOOST_REGISTER_SPLIT_EVALUATOR(SplitEval, 'splitEval')
- * .describe('Some split evaluator')
- * .set_body([]() {
- *     return new SplitEval();
- *   });
- * \endcode
- */
-#define XGBOOST_REGISTER_SPLIT_EVALUATOR(UniqueID, Name) \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::tree::SplitEvaluatorReg& \
-  __make_ ## SplitEvaluatorReg ## _ ## UniqueID ## __ = \
-      ::dmlc::Registry< ::xgboost::tree::SplitEvaluatorReg>::Get()->__REGISTER__(Name)  //NOLINT
-    
-      inline static void LimitSizeLevel
-    (size_t maxn, double eps, size_t* out_nlevel, size_t* out_limit_size) {
-    size_t& nlevel = *out_nlevel;
-    size_t& limit_size = *out_limit_size;
-    nlevel = 1;
-    while (true) {
-      limit_size = static_cast<size_t>(ceil(nlevel / eps)) + 1;
-      size_t n = (1ULL << nlevel);
-      if (n * limit_size >= maxn) break;
-      ++nlevel;
-    }
-    // check invariant
-    size_t n = (1ULL << nlevel);
-    CHECK(n * limit_size >= maxn) << 'invalid init parameter';
-    CHECK(nlevel <= limit_size * eps) << 'invalid init parameter';
-  }
-    
-        // want to compute storage boundary for each feature
-    // using variants of prefix sum scan
-    boundary_.resize(nfeature);
-    size_t accum_index_ = 0;
-    size_t accum_row_ind_ = 0;
-    for (bst_uint fid = 0; fid < nfeature; ++fid) {
-      boundary_[fid].index_begin = accum_index_;
-      boundary_[fid].row_ind_begin = accum_row_ind_;
-      if (type_[fid] == kDenseColumn) {
-        accum_index_ += static_cast<size_t>(nrow);
-        accum_row_ind_ += static_cast<size_t>(nrow);
-      } else {
-        accum_index_ += feature_counts_[fid];
-        accum_row_ind_ += feature_counts_[fid];
-      }
-      boundary_[fid].index_end = accum_index_;
-      boundary_[fid].row_ind_end = accum_row_ind_;
-    }
-    
-      virtual void PredictInteractionContributions(DMatrix* dmat,
-                           std::vector<bst_float>* out_contribs,
-                           unsigned ntree_limit, bool approximate) = 0;
-    
-    IMGUI_IMPL_API bool     ImGui_ImplDX10_Init(ID3D10Device* device);
-IMGUI_IMPL_API void     ImGui_ImplDX10_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplDX10_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplDX10_RenderDrawData(ImDrawData* draw_data);
-    
-        // Bind shader and vertex buffers
-    unsigned int stride = sizeof(ImDrawVert);
-    unsigned int offset = 0;
-    ctx->IASetInputLayout(g_pInputLayout);
-    ctx->IASetVertexBuffers(0, 1, &g_pVB, &stride, &offset);
-    ctx->IASetIndexBuffer(g_pIB, sizeof(ImDrawIdx) == 2 ? DXGI_FORMAT_R16_UINT : DXGI_FORMAT_R32_UINT, 0);
-    ctx->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-    ctx->VSSetShader(g_pVertexShader);
-    ctx->VSSetConstantBuffers(0, 1, &g_pVertexConstantBuffer);
-    ctx->PSSetShader(g_pPixelShader);
-    ctx->PSSetSamplers(0, 1, &g_pFontSampler);
-    
-        const GLchar* fragment_shader_glsl_130 =
-        'uniform sampler2D Texture;\n'
-        'in vec2 Frag_UV;\n'
-        'in vec4 Frag_Color;\n'
-        'out vec4 Out_Color;\n'
-        'void main()\n'
-        '{\n'
-        '    Out_Color = Frag_Color * texture(Texture, Frag_UV.st);\n'
-        '}\n';
-    
-        // Slider behavior
-    ImRect grab_bb;
-    const bool value_changed = SliderBehavior(frame_bb, id, data_type, v, v_min, v_max, format, power, ImGuiSliderFlags_None, &grab_bb);
-    if (value_changed)
-        MarkItemEdited(id);
-    
-    
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    void RadarState201::Parse(const std::uint8_t* bytes, int32_t length,
-                          ContiRadar* conti_radar) const {
-  auto state = conti_radar->mutable_radar_state();
-  state->set_max_distance(max_dist(bytes, length));
-  state->set_output_type(output_type(bytes, length));
-  state->set_rcs_threshold(rcs_threshold(bytes, length));
-  state->set_radar_power(radar_power(bytes, length));
-  state->set_send_quality(send_quality(bytes, length));
-  state->set_send_ext_info(send_ext_info(bytes, length));
-}
-    
-    unsigned int BaseMapMatrix::CreateBinary(unsigned char* buf,
-                                         unsigned int buf_size) const {
-  return 0;
-}
-    
-    #include 'modules/map/pnc_map/route_segments.h'
-    
-    std::array<char, 300> bigBuf;
-    
-    int main(int argc, char** argv) {
-  folly::init(&argc, &argv);
-  CHECK_GT(argc, 2);
-  folly::compareBenchmarkResults(argv[1], argv[2]);
-  return 0;
+        
+    {  return PlatformKind::none;
 }
 
     
-        intptr_t baseAddr = getAddr(note, pos);
-    CHECK_EQ(0, baseAddr);
-    remaining -= kAddrWidth;
-    
-    namespace folly {
-    }
-    
-        ExecutorT* get() const {
-      return reinterpret_cast<ExecutorT*>(
-          executorAndDummyFlag_ & kExecutorMask);
-    }
-    
-      /**
-   * Returns true 1/n of the time. If n == 0, always returns false
-   */
-  template <class RNG = ThreadLocalPRNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static bool oneIn(uint32_t n, RNG&& rng) {
-    if (n == 0) {
-      return false;
-    }
-    return rand32(0, n, rng) == 0;
+      /// Retrieve the array of replacement types, which line up with the
+  /// generic parameters.
+  ///
+  /// Note that the types may be null, for cases where the generic parameter
+  /// is concrete but hasn't been queried yet.
+  ArrayRef<Type> getReplacementTypes() const {
+    return llvm::makeArrayRef(getTrailingObjects<Type>(),
+                              getNumReplacementTypes());
   }
     
-    enum class CompressionCounterKey {
-  BYTES_BEFORE_COMPRESSION = 0,
-  BYTES_AFTER_COMPRESSION = 1,
-  BYTES_BEFORE_DECOMPRESSION = 2,
-  BYTES_AFTER_DECOMPRESSION = 3,
-  COMPRESSIONS = 4,
-  DECOMPRESSIONS = 5,
-  COMPRESSION_MILLISECONDS = 6,
-  DECOMPRESSION_MILLISECONDS = 7,
+    #ifndef PREPOSITION
+#  define PREPOSITION(Word)
+#endif
+    
+    
+void ClangDiagnosticConsumer::HandleDiagnostic(
+    clang::DiagnosticsEngine::Level clangDiagLevel,
+    const clang::Diagnostic &clangDiag) {
+  // Handle the module-not-found diagnostic specially if it's a top-level module
+  // we're looking for.
+  if (clangDiag.getID() == clang::diag::err_module_not_found &&
+      CurrentImport && clangDiag.getArgStdStr(0) == CurrentImport->getName()) {
+    return;
+  }
+    }
+    
+      // Static members
+  IAMResult(DeclName declName, EffectiveClangContext dc)
+      : name(declName), effectiveDC(dc) {}
+    
+      case Demangle::Node::Kind::Module:
+    // Modules terminate the recursion.
+    return node;
+    
+    
+    {private:
+    Ui::OpenURIDialog *ui;
 };
     
     
-    {    return false;
-  }
-  bool compare_exchange_strong(
-      SharedPtr& expected,
-      SharedPtr&& desired,
-      std::memory_order mo = std::memory_order_seq_cst) noexcept {
-    return compare_exchange_strong(
-        expected, desired, mo, detail::default_failure_memory_order(mo));
-  }
-  bool compare_exchange_strong(
-      SharedPtr& expected,
-      SharedPtr&& desired,
-      std::memory_order success,
-      std::memory_order failure) /* noexcept */ {
-    return compare_exchange_strong(expected, desired, success, failure);
-  }
+    {    Lock& lock;
+    Lock templock;
+};
     
-    // Each level of cache has sharing sets, which are the set of cpus
-// that share a common cache at that level.  These are available in a
-// hex bitset form (/sys/devices/system/cpu/cpu0/index0/shared_cpu_map,
-// for example).  They are also available in a human-readable list form,
-// as in /sys/devices/system/cpu/cpu0/index0/shared_cpu_list.  The list
-// is a comma-separated list of numbers and ranges, where the ranges are
-// a pair of decimal numbers separated by a '-'.
-//
-// To sort the cpus for optimum locality we don't really need to parse
-// the sharing sets, we just need a unique representative from the
-// equivalence class.  The smallest value works fine, and happens to be
-// the first decimal number in the file.  We load all of the equivalence
-// class information from all of the cpu*/index* directories, order the
-// cpus first by increasing last-level cache equivalence class, then by
-// the smaller caches.  Finally, we break ties with the cpu number itself.
+        std::string vs('yawn');
+    UniValue v8(vs);
+    BOOST_CHECK(v8.isStr());
+    BOOST_CHECK_EQUAL(v8.getValStr(), 'yawn');
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    #undef ADDS
+    
+    #include <memory>
+    
+    struct GlobStreamWrapper final : Stream::Wrapper {
+  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+  req::ptr<Directory> opendir(const String& path) override;
+};
+    
+    
+    {  logAHMSubMapWarning(mapName);
+}
+    
+    Animation* Animation::createWithSpriteFrames(const Vector<SpriteFrame*>& frames, float delay/* = 0.0f*/, unsigned int loops/* = 1*/)
+{
+    Animation *animation = new (std::nothrow) Animation();
+    animation->initWithSpriteFrames(frames, delay, loops);
+    animation->autorelease();
+    }
+    
+    
+    {}
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
