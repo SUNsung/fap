039f7f12775377f25ca206de4ec559c2b053f27f
SUNsung <weatherius@gmail.com>
2019-01-12 08:31:13 +0200

    Create [12E2l4KyiCh6k125VvhPg23fc12HNGr1sNKLl612izw.py] File in: 2019-01-12 08:31:12

---
+++ pyton/12E2l4KyiCh6k125VvhPg23fc12HNGr1sNKLl612izw.py
@@ -0,0 +1,127 @@
+    # Then we cluster the lines together as blocks
+    # Each block represents a collection of lines that should be sorted
+    # This was done by assuming only links ([...](...)) are meant to be sorted
+    # Clustering is done by indentation
+    blocks = []
+    last_indent = None
+    for line in read_me:
+        s_line = line.lstrip()
+        indent = len(line) - len(s_line)
+    
+      plt.subplot(nrows,2,5+subplot_cidx)
+  plot_time_series(means, bidx,
+                   n_to_plot=n_to_plot, scale=1.0,
+                   title=col_title + ' Spikes (LFADS - red, Spikes - black)')
+  plot_time_series(data_bxtxn, bidx, n_to_plot=n_to_plot, color='k', scale=1.0)
+    
+      if input_magnitude > 0.0:
+    # time of 'hits' randomly chosen between [1/4 and 3/4] of total time
+    input_times = rng.choice(int(ntime_steps/2), size=[E]) + int(ntime_steps/4)
+  else:
+    input_times = None
+    
+    import random
+    
+      def encode_chars(self, sentence):
+    chars_ids = [self.word_to_char_ids(cur_word)
+                 for cur_word in sentence.split()]
+    return np.vstack([self.bos_chars] + chars_ids + [self.eos_chars])
+    
+      Args:
+    raw_data: one of the raw data outputs from ptb_raw_data.
+    batch_size: int, the batch size.
+    num_steps: int, the number of unrolls.
+    
+      decoder_embedding = [
+      v for v in tf.trainable_variables()
+      if v.op.name == 'gen/decoder/rnn/embedding'
+  ][0]
+  decoder_lstm_w_0 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel'
+  ][0]
+  decoder_lstm_b_0 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias'
+  ][0]
+  decoder_lstm_w_1 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel'
+  ][0]
+  decoder_lstm_b_1 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias'
+  ][0]
+  decoder_softmax_b = [
+      v for v in tf.trainable_variables()
+      if v.op.name == 'gen/decoder/rnn/softmax_b'
+  ][0]
+    
+        def __init__(self, hass, flash_briefings):
+        '''Initialize Alexa view.'''
+        super().__init__()
+        self.flash_briefings = copy.deepcopy(flash_briefings)
+        template.attach(hass, self.flash_briefings)
+    
+    
+def setup_button(hass, config, add_entities, client, address):
+    '''Set up a single button device.'''
+    timeout = config.get(CONF_TIMEOUT)
+    ignored_click_types = config.get(CONF_IGNORED_CLICK_TYPES)
+    button = FlicButton(hass, client, address, timeout, ignored_click_types)
+    _LOGGER.info('Connected to button %s', address)
+    
+            # doing a request
+        try:
+            res = requests.get(self._url, timeout=10, cookies={
+                'userid': self._userid
+            })
+        except requests.exceptions.Timeout:
+            _LOGGER.error(
+                'Connection to the router timed out at URL %s', self._url)
+            return False
+        if res.status_code != 200:
+            _LOGGER.error(
+                'Connection failed with http code %s', res.status_code)
+            return False
+        try:
+            result = res.json()
+        except ValueError:
+            # If json decoder could not parse the response
+            _LOGGER.error('Failed to parse response from router')
+            return False
+    
+        def _make_request(self):
+        # Weirdly enough, this doesn't seem to require authentication
+        data = [{
+            'request': {
+                'sinceRevision': 0
+            },
+            'action': 'http://linksys.com/jnap/devicelist/GetDevices'
+        }]
+        headers = {'X-JNAP-Action': 'http://linksys.com/jnap/core/Transaction'}
+        return requests.post('http://{}/JNAP/'.format(self.host),
+                             timeout=DEFAULT_TIMEOUT,
+                             headers=headers,
+                             json=data)
+
+    
+        def __init__(self, config):
+        '''Initialize the scanner.'''
+        self.last_results = []
+    
+    import homeassistant.helpers.config_validation as cv
+from homeassistant.components.device_tracker import (
+    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+    
+        def scan_devices(self):
+        '''Scan for new devices and return a list with found device IDs.'''
+        self._update_info()
+        return self.last_results
+    
+            for order in target_orders:
+            order.place()
+    
+        if os.path.exists('tldr.pdf'):
+        print('\nCreated tldr.pdf in the current directory!\n')
