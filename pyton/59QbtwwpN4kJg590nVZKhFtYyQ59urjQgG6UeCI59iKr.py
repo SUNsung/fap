33f78fa7a051d2ba207816ed356a60ce57a8c5f9
SUNsung <weatherius@gmail.com>
2019-01-13 16:37:37 +0200

    Rewrite [59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py] File in: 2019-01-13 16:37:36

--- pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
+++ pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
@@ -1,61 +1,109 @@
 
         
-        
-if len(sys.argv) <= 1:
-    print('Specify the version number as parameter')
-    sys.exit()
-version = sys.argv[1]
+            x = inputs
+    if conv_first:
+        x = conv(x)
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+    else:
+        if batch_normalization:
+            x = BatchNormalization()(x)
+        if activation is not None:
+            x = Activation(activation)(x)
+        x = conv(x)
+    return x
     
-    entries = []
-for v in versions:
-    fields = v.split('.')
-    year, month, day = map(int, fields[:3])
-    faked = 0
-    patchlevel = 0
-    while True:
-        try:
-            datetime.date(year, month, day)
-        except ValueError:
-            day -= 1
-            faked += 1
-            assert day > 0
-            continue
-        break
-    if len(fields) >= 4:
-        try:
-            patchlevel = int(fields[3])
-        except ValueError:
-            patchlevel = 1
-    timestamp = '%04d-%02d-%02dT00:%02d:%02dZ' % (year, month, day, faked, patchlevel)
+    from __future__ import print_function
     
+        # Arguments
+        dense: The target `Dense` layer.
+        previous_feature_map_shape: A shape tuple of 3 integers,
+            e.g. `(512, 7, 7)`. The shape of the convolutional
+            feature map right before the `Flatten` layer that
+            came before the target `Dense` layer.
+        target_data_format: One of 'channels_last', 'channels_first'.
+            Set it 'channels_last'
+            if converting a 'channels_first' model to 'channels_last',
+            or reciprocally.
+    '''
+    assert target_data_format in {'channels_last', 'channels_first'}
+    kernel, bias = dense.get_weights()
+    for i in range(kernel.shape[1]):
+        if target_data_format == 'channels_first':
+            c, h, w = previous_feature_map_shape
+            original_fm_shape = (h, w, c)
+            ki = kernel[:, i].reshape(original_fm_shape)
+            ki = np.transpose(ki, (2, 0, 1))  # last -> first
+        else:
+            h, w, c = previous_feature_map_shape
+            original_fm_shape = (c, h, w)
+            ki = kernel[:, i].reshape(original_fm_shape)
+            ki = np.transpose(ki, (1, 2, 0))  # first -> last
+        kernel[:, i] = np.reshape(ki, (np.prod(previous_feature_map_shape),))
+    dense.set_weights([kernel, bias])
     
-def build_lazy_ie(ie, name):
-    valid_url = getattr(ie, '_VALID_URL', None)
-    s = ie_template.format(
-        name=name,
-        bases=', '.join(map(get_base_name, ie.__bases__)),
-        valid_url=valid_url,
-        module=ie.__module__)
-    if ie.suitable.__func__ is not InfoExtractor.suitable.__func__:
-        s += '\n' + getsource(ie.suitable)
-    if hasattr(ie, '_make_valid_url'):
-        # search extractors
-        s += make_valid_template.format(valid_url=ie._make_valid_url())
-    return s
+        # Test that writing over the input data works predictably
+    for mode in ['torch', 'tf']:
+        x = np.random.uniform(0, 255, (2, 10, 10, 3))
+        xint = x.astype('int')
+        x2 = utils.preprocess_input(x, mode=mode)
+        xint2 = utils.preprocess_input(xint)
+        assert_allclose(x, x2)
+        assert xint.astype('float').max() != xint2.max()
+    # Caffe mode works differently from the others
+    x = np.random.uniform(0, 255, (2, 10, 10, 3))
+    xint = x.astype('int')
+    x2 = utils.preprocess_input(x, data_format='channels_last', mode='caffe')
+    xint2 = utils.preprocess_input(xint)
+    assert_allclose(x, x2[..., ::-1])
+    assert xint.astype('float').max() != xint2.max()
     
-    with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
+        def _process_batch(self, inputs, initial_state):
+        import tensorflow as tf
+        inputs = tf.transpose(inputs, (1, 0, 2))
+        input_h = initial_state[0]
+        input_c = initial_state[1]
+        input_h = tf.expand_dims(input_h, axis=0)
+        input_c = tf.expand_dims(input_c, axis=0)
     
-    # NAME
+    model = Sequential()
+model.add(Embedding(max_features, embedding_size, input_length=maxlen))
+model.add(Dropout(0.25))
+model.add(Conv1D(filters,
+                 kernel_size,
+                 padding='valid',
+                 activation='relu',
+                 strides=1))
+model.add(MaxPooling1D(pool_size=pool_size))
+model.add(LSTM(lstm_output_size))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
     
-    # -- Options for HTML output ----------------------------------------------
+    import mock
     
+    The JWS implementation in josepy only implements the base JOSE standard. In
+order to support the new header fields defined in ACME, this module defines some
+ACME-specific classes that layer on top of josepy.
+'''
+import josepy as jose
     
-def _is_empty(d):
-    return not bool(os.listdir(d))
+        :ivar str filep: file path of VH
+    :ivar str path: Augeas path to virtual host
+    :ivar set addrs: Virtual Host addresses (:class:`set` of
+        :class:`common.Addr`)
+    :ivar str name: ServerName of VHost
+    :ivar list aliases: Server aliases of vhost
+        (:class:`list` of :class:`str`)
     
-            def _hook(status):
-            if status['status'] == 'finished':
-                finished_hook_called.add(status['filename'])
-        ydl.add_progress_hook(_hook)
-        expect_warnings(ydl, test_case.get('expected_warnings', []))
+            self.assertEqual(len(self.parser.filter_args_num(matches, 1)), 3)
+        self.assertEqual(len(self.parser.filter_args_num(matches, 2)), 2)
+        self.assertEqual(len(self.parser.filter_args_num(matches, 3)), 1)
+    
+        def test_conflicts(self):
+        # Note: Defined IP is more important than defined port in match
+        self.assertTrue(self.addr.conflicts(self.addr1))
+        self.assertTrue(self.addr.conflicts(self.addr2))
+        self.assertTrue(self.addr.conflicts(self.addr_defined))
+        self.assertFalse(self.addr.conflicts(self.addr_default))
