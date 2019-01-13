1f8923d97e86e91f00be1809a50e7d6b0b416e6f
SUNsung <weatherius@gmail.com>
2019-01-13 21:31:48 +0200

    Rewrite [53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py] File in: 2019-01-13 21:31:47

--- pyton/53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py
+++ pyton/53fiaEmA0xsjo53Ym1drJSxONs53oju0hkWWnkM53iY.py
@@ -1,45 +1,48 @@
 
         
-            event_log = advapi32.RegisterEventSourceW(None, service_name)
-    if not event_log:
-        raise OSError('Could not report event: %s' % ctypes.FormatError())
+        '''
+requests._internal_utils
+~~~~~~~~~~~~~~
     
-        elif not RESULT and ('info_dict' in test and 'age_limit' in test['info_dict'] and
-                         test['info_dict']['age_limit'] == 18):
-        print('\nPotential false negative: {0}'.format(test['name']))
+        # Copy is required
+    def copy(self):
+        return CaseInsensitiveDict(self._store.values())
     
-    versions_info = json.load(open('update/versions.json'))
-if 'signature' in versions_info:
-    del versions_info['signature']
+            new_content = sock.recv(chunks)
+        if not new_content:
+            break
     
-        with open('supportedsites.html', 'w', encoding='utf-8') as sitesf:
-        sitesf.write(template)
+            Punctuation:               'bold #000000',   # class: 'p'
     
-        def test_module_exec(self):
-        if sys.version_info >= (2, 7):  # Python 2.6 doesn't support package execution
-            subprocess.check_call([sys.executable, '-m', 'youtube_dl', '--version'], cwd=rootDir, stdout=_DEV_NULL)
+    This module provides the capabilities for the Requests hooks system.
     
-        def get_converter(self, mime):
-        if is_valid_mime(mime):
-            for converter_class in plugin_manager.get_converters():
-                if converter_class.supports(mime):
-                    return converter_class(mime)
+            # Verify Authorization is sent correctly again, and return 200 OK.
+        request_content = consume_socket_content(sock, timeout=0.5)
+        assert expected_digest in request_content
+        sock.send(text_200)
     
-        See also `test_auth_plugins.py`
+        def test_cookie_parameters(self):
+        key = 'some_cookie'
+        value = 'some_value'
+        secure = True
+        domain = 'test.com'
+        rest = {'HttpOnly': True}
     
-        def test_POST_form_auto_Content_Type(self, httpbin):
-        r = http('--form', 'POST', httpbin.url + '/post')
-        assert HTTP_OK in r
-        assert ''Content-Type': 'application/x-www-form-urlencoded' in r
+        global pteredor_is_running, usable
+    pteredor_is_running = probe_nat
+    prober = teredo_prober(probe_nat=probe_nat)
     
-    
-def unicode_is_ascii(u_string):
-    '''Determine if unicode string only contains ASCII characters.
-    
-        @get_item_parameters
-    def test_get(self, key, value):
-        assert self.lookup_dict.get(key) == value
+        @classmethod
+    # pylint: disable=arguments-differ,too-many-arguments
+    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
+        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
+        # jwk field if kid is not provided.
+        include_jwk = kid is None
+        return super(JWS, cls).sign(payload, key=key, alg=alg,
+                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
+                                    nonce=nonce, url=url, kid=kid,
+                                    include_jwk=include_jwk)
 
     
-    # If true, 'Created using Sphinx' is shown in the HTML footer. Default is True.
-html_show_sphinx = False
+        '''
+    return _split_aug_path(vhost_path)[1]
