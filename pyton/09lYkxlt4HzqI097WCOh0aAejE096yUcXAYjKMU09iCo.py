c14336de1d2d36c0175ba0d377002bb1716a0437
SUNsung <weatherius@gmail.com>
2019-01-13 09:10:10 +0200

    Rewrite [09lYkxlt4HzqI097WCOh0aAejE096yUcXAYjKMU09iCo.py] File in: 2019-01-13 09:10:09

--- pyton/09lYkxlt4HzqI097WCOh0aAejE096yUcXAYjKMU09iCo.py
+++ pyton/09lYkxlt4HzqI097WCOh0aAejE096yUcXAYjKMU09iCo.py
@@ -1,199 +1,83 @@
-    self.kl_cost_b = kl_b
-    self.kl_cost = tf.reduce_mean(kl_b)
-    
-        if not use_nested:
-      return vars_dict
-    
-    from synthetic_data_utils import generate_data, generate_rnn
-from synthetic_data_utils import get_train_n_valid_inds
-from synthetic_data_utils import nparray_and_transpose
-from synthetic_data_utils import spikify_data, split_list_by_inds
-import tensorflow as tf
-from utils import write_datasets
-    
-    import json
-import os
-import numpy as np
-import tensorflow as tf
-    
-    import collections
-import os
-# Dependency imports
-import numpy as np
-    
-    ## Sample Generation.
-## Binary and setup FLAGS.
-tf.app.flags.DEFINE_enum('sample_mode', 'TRAIN',
-                         [SAMPLE_TRAIN, SAMPLE_VALIDATION],
-                         'Dataset to sample from.')
-tf.app.flags.DEFINE_string('output_path', '/tmp', 'Model output directory.')
-tf.app.flags.DEFINE_boolean(
-    'output_masked_logs', False,
-    'Whether to display for human evaluation (show masking).')
-tf.app.flags.DEFINE_integer('number_epochs', 1,
-                            'The number of epochs to produce.')
+
+        
+            y_train = np.reshape(y_train, (len(y_train), 1))
+    y_test = np.reshape(y_test, (len(y_test), 1))
     
     
-def recursive_length(item):
-  '''Recursively determine the total number of elements in nested list.'''
-  if type(item) == list:
-    return sum(recursive_length(subitem) for subitem in item)
-  else:
-    return 1.
+def deserialize(config, custom_objects=None):
+    return deserialize_keras_object(config,
+                                    module_objects=globals(),
+                                    custom_objects=custom_objects,
+                                    printable_module_name='regularizer')
     
-          elif FLAGS.generator_model == 'seq2seq_nas':
-        # Encoder.
-        gen_encoder_variable_maps = variable_mapping.gen_encoder_seq2seq_nas(
-            hparams)
-        gen_encoder_init_saver = tf.train.Saver(
-            var_list=gen_encoder_variable_maps)
-        # Decoder.
-        gen_decoder_variable_maps = variable_mapping.gen_decoder_seq2seq_nas(
-            hparams)
-        gen_decoder_init_saver = tf.train.Saver(
-            var_list=gen_decoder_variable_maps)
-        init_savers['gen_encoder_init_saver'] = gen_encoder_init_saver
-        init_savers['gen_decoder_init_saver'] = gen_decoder_init_saver
+        non_trainable_count = count_params(model.non_trainable_weights)
     
-        # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
+    
+def test_preprocess_input():
+    # Test image batch with float and int image input
+    x = np.random.uniform(0, 255, (2, 10, 10, 3))
+    xint = x.astype('int32')
+    assert utils.preprocess_input(x).shape == x.shape
+    assert utils.preprocess_input(xint).shape == xint.shape
+    
+        E.g. for use with categorical_crossentropy.
+    
+            if vertical_flip:
+            coin = tf.less(tf.random_uniform([batch_size], 0, 1.0), 0.5)
+            shape = [1., 0., 0., 0., -1., height, 0., 0.]
+            flip_transform = tf.convert_to_tensor(shape, dtype=tf.float32)
+            flip = tf.tile(tf.expand_dims(flip_transform, 0), [batch_size, 1])
+            noflip = tf.tile(tf.expand_dims(identity, 0), [batch_size, 1])
+            transforms.append(tf.where(coin, flip, noflip))
+    
+    encoder_inputs = model.input[0]   # input_1
+encoder_outputs, state_h_enc, state_c_enc = model.layers[2].output   # lstm_1
+encoder_states = [state_h_enc, state_c_enc]
+encoder_model = Model(encoder_inputs, encoder_states)
+    
+    print('Training')
+model_stateless.fit(x_train,
+                    y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test),
+                    shuffle=False)
+    
+        def _init(self):
+        # Overridden in concrete Path
         pass
     
-        return render_template('auth/register.html')
     
+def read_text(package: Package,
+              resource: Resource,
+              encoding: str = 'utf-8',
+              errors: str = 'strict') -> str:
+    '''Return the decoded string of the resource.
     
-def test_get_close_db(app):
-    with app.app_context():
-        db = get_db()
-        assert db is get_db()
+    def handleSlides(slides):
+    for slide in slides:
+        handleSlide(slide)
     
-    
-if not PY2:
-    text_type = str
-    string_types = (str,)
-    integer_types = (int,)
-    
-        def make_setup_state(self, app, options, first_registration=False):
-        '''Creates an instance of :meth:`~flask.blueprints.BlueprintSetupState`
-        object that is later passed to the register callback functions.
-        Subclasses can override this to return a subclass of the setup state.
-        '''
-        return BlueprintSetupState(self, app, options, first_registration)
-    
-            :param filename: the filename of the JSON file.  This can either be an
-                         absolute filename or a filename relative to the
-                         root path.
-        :param silent: set to ``True`` if you want silent failure for missing
-                       files.
-    
-        def __init__(self, request, key):
-        form_matches = request.form.getlist(key)
-        buf = ['You tried to access the file '%s' in the request.files '
-               'dictionary but it does not exist.  The mimetype for the request '
-               'is '%s' instead of 'multipart/form-data' which means that no '
-               'file contents were transmitted.  To fix this error you should '
-               'provide enctype='multipart/form-data' in your form.' %
-               (key, request.mimetype)]
-        if form_matches:
-            buf.append('\n\nThe browser instead transmitted some file names. '
-                       'This was submitted: %s' % ', '.join(''%s'' % x
-                            for x in form_matches))
-        self.msg = ''.join(buf)
-    
-        When :attr:`~flask.Flask.debug` is enabled, set the logger level to
-    :data:`logging.DEBUG` if it is not set.
-    
-    signals_available = False
-try:
-    from blinker import Namespace
-    signals_available = True
-except ImportError:
-    class Namespace(object):
-        def signal(self, name, doc=None):
-            return _FakeSignal(name, doc)
-    
-        def state_changed_listener(event):
-        '''Listen for new messages on the bus and sends them to Datadog.'''
-        state = event.data.get('new_state')
-    
-        return scanner if scanner.success_init else None
-    
-            if self.home_interval:
-            boundary = dt_util.now() - self.home_interval
-            last_results = [device for device in self.last_results
-                            if device.last_update > boundary]
-            if last_results:
-                exclude_hosts = self.exclude + [device.ip for device
-                                                in last_results]
-            else:
-                exclude_hosts = self.exclude
-        else:
-            last_results = []
-            exclude_hosts = self.exclude
-        if exclude_hosts:
-            options += ' --exclude {}'.format(','.join(exclude_hosts))
-    
-    
-def get_scanner(hass, config):
-    '''Validate the configuration and return a THOMSON scanner.'''
-    scanner = ThomsonDeviceScanner(config[DOMAIN])
-    
-        if conf.get(ATTR_SHOW_MENU):
-        hass.http.register_view(DominosProductListView(dominos))
-    
-            if self.upnp_bind_multicast:
-            ssdp_socket.bind(('', 1900))
-        else:
-            ssdp_socket.bind((self.host_ip_addr, 1900))
-    
-    
-class GreekGetter(object):
-    
-    
-class Delegate(object):
-    def __init__(self):
-        self.p1 = 123
-    
-            traceback.print_exc(file=sys.stdout)
-    print(num_obj)
-    
-        @property
-    def data(self):
-        return self._data
-    
-    
-class Subscriber:
-    def __init__(self, name, msg_center):
-        self.name = name
-        self.provider = msg_center
-    
-        REGISTRY = {}
-    
-    
-# Actions
-def print_item(item):
-    print(item)
-    
-        def __init__(self, animal_factory=None):
-        '''pet_factory is our abstract factory.  We can set it at will.'''
-    
-        d = prototype.clone()
-    a = prototype.clone(value='a-value', category='a')
-    b = prototype.clone(value='b-value', is_checked=True)
-    dispatcher.register_object('objecta', a)
-    dispatcher.register_object('objectb', b)
-    dispatcher.register_object('default', d)
-    print([{n: p.value} for n, p in dispatcher.get_objects().items()])
-    
-        def run_loop(self):
-        while self.blackboard.common_state['progress'] < 100:
-            for expert in self.blackboard.experts:
-                if expert.is_eager_to_contribute:
-                    expert.contribute()
-        return self.blackboard.common_state['contributions']
-    
-    
-class UnsupportedTransition(BaseException):
+    def noop(x):
     pass
+    
+    # Register the adapter
+sqlite3.register_adapter(Point, adapt_point)
+    
+    cur.close()
+con.close()
+
+    
+    def download_pdf(link, location, name):
+    try:
+        response = requests.get(link)
+        with open(os.path.join(location, name), 'wb') as f:
+        	f.write(response.content)
+        	f.close()
+    except HTTPError:
+        print('>>> Error 404: cannot be downloaded!\n') 
+        raise   
+    except socket.timeout:
+        print(' '.join(('can't download', link, 'due to connection timeout!')) )
+        raise
