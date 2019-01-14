d156a793062af402126457a8ea39207d4f9feb12
SUNsung <weatherius@gmail.com>
2019-01-15 00:32:02 +0200

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2019-01-15 00:32:01

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,266 +1,127 @@
 
         
-        // Generate param traits read methods.
-#include 'ipc/param_traits_read_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
+        #include <dmlc/registry.h>
+#include <dmlc/parameter.h>
+#include <map>
+#include <vector>
+#include <string>
+#include <utility>
+#include './base.h'
+#include './operator.h'
     
-    #ifndef CONTENT_NW_SRC_API_BASE_BASE_H_
-#define CONTENT_NW_SRC_API_BASE_BASE_H_
-    
-    void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
-}
-    
-    EventListener::EventListener(int id,
-  const base::WeakPtr<DispatcherHost>& dispatcher_host,
-  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    namespace mxnet {
+namespace op {
+    }
     }
     
-      option.GetString('type', &type_);
-  option.GetString('label', &label_);
-  option.GetString('tooltip', &tooltip_);
-  option.GetBoolean('checked', &is_checked_);
-  option.GetBoolean('enabled', &is_enabled_);
-    
-      nw::Package* package = nw::package();
-  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  base::CommandLine::StringVector args = command_line->GetArgs();
-  base::CommandLine::StringVector argv = command_line->original_argv();
-    
-      NwScreenIsMonitorStartedFunction::NwScreenIsMonitorStartedFunction() {}
-    
-    #undef cv_hal_merge8u
-#define cv_hal_merge8u TEGRA_MERGE
-#undef cv_hal_merge16u
-#define cv_hal_merge16u TEGRA_MERGE
-#undef cv_hal_merge32s
-#define cv_hal_merge32s TEGRA_MERGE
-#undef cv_hal_merge64s
-#define cv_hal_merge64s(src, dst, len, cn) TEGRA_MERGE64S(CAROTENE_NS::s64, src, dst, len, cn)
-    
-                for( ; i <= lim; i += 4 )
-            {
-                internal::prefetch(src0 + i);
-                internal::prefetch(src1 + i);
-                v_sum = vmlaq_f32(v_sum, vld1q_f32(src0 + i), vld1q_f32(src1 + i));
-            }
-    
-    typedef void (* flipFunc)(const Size2D &size,
-                  const void * srcBase, ptrdiff_t srcStride,
-                  void * dstBase, ptrdiff_t dstStride,
-                  FLIP_MODE flipMode);
-    
-                int32x4_t ln04 = vaddq_s32(lane0, lane4);
-            int32x4_t ln13 = vaddq_s32(lane1, lane3);
-    
-    #include 'common.hpp'
-    
-    template <class charT, class traits>
-bool basic_regex_parser<charT, traits>::parse_extended()
-{
-   bool result = true;
-   switch(this->m_traits.syntax_type(*m_position))
-   {
-   case regex_constants::syntax_open_mark:
-      return parse_open_paren();
-   case regex_constants::syntax_close_mark:
-      return false;
-   case regex_constants::syntax_escape:
-      return parse_extended_escape();
-   case regex_constants::syntax_dot:
-      return parse_match_any();
-   case regex_constants::syntax_caret:
-      ++m_position;
-      this->append_state(
-         (this->flags() & regex_constants::no_mod_m ? syntax_element_buffer_start : syntax_element_start_line));
-      break;
-   case regex_constants::syntax_dollar:
-      ++m_position;
-      this->append_state(
-         (this->flags() & regex_constants::no_mod_m ? syntax_element_buffer_end : syntax_element_end_line));
-      break;
-   case regex_constants::syntax_star:
-      if(m_position == this->m_base)
-      {
-         fail(regex_constants::error_badrepeat, 0, 'The repeat operator \'*\' cannot start a regular expression.');
-         return false;
-      }
-      ++m_position;
-      return parse_repeat();
-   case regex_constants::syntax_question:
-      if(m_position == this->m_base)
-      {
-         fail(regex_constants::error_badrepeat, 0, 'The repeat operator \'?\' cannot start a regular expression.');
-         return false;
-      }
-      ++m_position;
-      return parse_repeat(0,1);
-   case regex_constants::syntax_plus:
-      if(m_position == this->m_base)
-      {
-         fail(regex_constants::error_badrepeat, 0, 'The repeat operator \'+\' cannot start a regular expression.');
-         return false;
-      }
-      ++m_position;
-      return parse_repeat(1);
-   case regex_constants::syntax_open_brace:
-      ++m_position;
-      return parse_repeat_range(false);
-   case regex_constants::syntax_close_brace:
-      if((this->flags() & regbase::no_perl_ex) == regbase::no_perl_ex)
-      {
-         fail(regex_constants::error_brace, this->m_position - this->m_base, 'Found a closing repetition operator } with no corresponding {.');
-         return false;
-      }
-      result = parse_literal();
-      break;
-   case regex_constants::syntax_or:
-      return parse_alt();
-   case regex_constants::syntax_open_set:
-      return parse_set();
-   case regex_constants::syntax_newline:
-      if(this->flags() & regbase::newline_alt)
-         return parse_alt();
-      else
-         return parse_literal();
-   case regex_constants::syntax_hash:
-      //
-      // If we have a mod_x flag set, then skip until
-      // we get to a newline character:
-      //
-      if((this->flags() 
-         & (regbase::no_perl_ex|regbase::mod_x))
-         == regbase::mod_x)
-      {
-         while((m_position != m_end) && !is_separator(*m_position++)){}
-         return true;
-      }
-      BOOST_FALLTHROUGH;
-   default:
-      result = parse_literal();
-      break;
-   }
-   return result;
-}
-#ifdef BOOST_MSVC
-#pragma warning(pop)
-#endif
-    
-       mapfile_iterator& operator += (long off)
-   {
-      position(position() + off);
-      return *this;
-   }
-   mapfile_iterator& operator -= (long off)
-   {
-      position(position() - off);
-      return *this;
-   }
-    
-    #if (defined(__GLIBCPP__) || defined(__GLIBCXX__)) \
-   && !defined(BOOST_REGEX_ICU_INSTANCES)\
-   && !defined(__SGI_STL_PORT)\
-   && !defined(_STLPORT_VERSION)
-// std:basic_string<>::const_iterator instances as well:
-template BOOST_REGEX_DECL void BOOST_REGEX_CALL 
-   match_results<std::basic_string<BOOST_REGEX_CHAR_T>::const_iterator>::maybe_assign(
-      const match_results<std::basic_string<BOOST_REGEX_CHAR_T>::const_iterator>& m);
-    
-     /*
-  *   LOCATION:    see http://www.boost.org for most recent version.
-  *   FILE         regex_match.hpp
-  *   VERSION      see <boost/version.hpp>
-  *   DESCRIPTION: Iterator traits for selecting an iterator type as
-  *                an integral constant expression.
-  */
-    
-    template <class BidiIterator, class Allocator, class traits>
-bool perl_matcher<BidiIterator, Allocator, traits>::match_soft_buffer_end()
-{
-   if(m_match_flags & match_not_eob)
-      return false;
-   BidiIterator p(position);
-   while((p != last) && is_separator(traits_inst.translate(*p, icase)))++p;
-   if(p != last)
-      return false;
-   pstate = pstate->next.p;
-   return true;
+    template<>
+Operator* CreateOp<cpu>(CaffeOpParam param, int dtype) {
+  Operator *op = NULL;
+  switch (dtype) {
+  case mshadow::kFloat32:
+    op = new CaffeOp<cpu, float>(param);
+    break;
+  case mshadow::kFloat64:
+    op = new CaffeOp<cpu, double>(param);
+    break;
+  case mshadow::kFloat16:
+    LOG(FATAL) << 'float16 layer is not supported by caffe';
+    break;
+  default:
+    LOG(FATAL) << 'Unsupported type ' << dtype;
+  }
+  return op;
 }
     
-       BOOST_ASSERT(rep->type == syntax_element_long_set_rep);
-   BOOST_ASSERT(rep->next.p != 0);
-   BOOST_ASSERT(rep->alt.p != 0);
-   BOOST_ASSERT(rep->next.p->type == syntax_element_long_set);
-   BOOST_ASSERT(count < rep->max);
+    #include '../common/utils.h'
     
-    #ifndef BOOST_REGEX_V4_PERL_MATCHER_RECURSIVE_HPP
-#define BOOST_REGEX_V4_PERL_MATCHER_RECURSIVE_HPP
     
-    class BOOST_REGEX_DECL abstract_protected_call
-{
-public:
-   bool BOOST_REGEX_CALL execute()const;
-   // this stops gcc-4 from complaining:
-   virtual ~abstract_protected_call(){}
-private:
-   virtual bool call()const = 0;
-};
+    {
+    {}}
     
-     /*
-  *   LOCATION:    see http://www.boost.org for most recent version.
-  *   FILE         regex_fwd.cpp
-  *   VERSION      see <boost/version.hpp>
-  *   DESCRIPTION: Forward declares boost::basic_regex<> and
-  *                associated typedefs.
-  */
+    struct ObjectData;
+struct Object;
     
-    #ifdef BOOST_MSVC
-#pragma warning(push)
-#pragma warning(disable: 4103)
-#endif
-#ifdef BOOST_HAS_ABI_HEADERS
-#  include BOOST_ABI_SUFFIX
-#endif
-#ifdef BOOST_MSVC
-#pragma warning(pop)
-#endif
-    
-    template <class OutputIterator, class BidirectionalIterator, class traits, class charT, class Formatter>
-OutputIterator regex_replace(OutputIterator out,
-                         BidirectionalIterator first,
-                         BidirectionalIterator last,
-                         const basic_regex<charT, traits>& e, 
-                         Formatter fmt, 
-                         match_flag_type flags = match_default)
-{
-   regex_iterator<BidirectionalIterator, charT, traits> i(first, last, e, flags);
-   regex_iterator<BidirectionalIterator, charT, traits> j;
-   if(i == j)
-   {
-      if(!(flags & regex_constants::format_no_copy))
-         out = BOOST_REGEX_DETAIL_NS::copy(first, last, out);
-   }
-   else
-   {
-      BidirectionalIterator last_m(first);
-      while(i != j)
-      {
-         if(!(flags & regex_constants::format_no_copy))
-            out = BOOST_REGEX_DETAIL_NS::copy(i->prefix().first, i->prefix().second, out); 
-         out = i->format(out, fmt, flags, e);
-         last_m = (*i)[0].second;
-         if(flags & regex_constants::format_first_only)
-            break;
-         ++i;
-      }
-      if(!(flags & regex_constants::format_no_copy))
-         out = BOOST_REGEX_DETAIL_NS::copy(last_m, last, out);
-   }
-   return out;
+    void Config::ParseConfigFile(const std::string &filename, IniSettingMap &ini,
+                             Hdf &hdf, const bool is_system /* = true */) {
+  // We don't allow a filename of just '.ini'
+  if (boost::ends_with(filename, '.ini') && filename.length() > 4) {
+    Config::ParseIniFile(filename, ini, false, is_system);
+  } else {
+    // For now, assume anything else is an hdf file
+    // TODO(#5151773): Have a non-invasive warning if HDF file does not end
+    // .hdf
+    Config::ParseHdfFile(filename, hdf);
+  }
 }
+    
+      // Parse and process a .hdf string (e.g., -v)
+  static void ParseHdfString(const std::string &hdfStr, Hdf &hdf);
+    
+      static std::string FormatNumber(const char* fmt, int64_t n);
+  static std::string FormatSize(int64_t size);
+  static std::string FormatTime(int64_t milliSeconds);
+    
+    String ArrayDirectory::path() {
+  if (!m_it) {
+    return empty_string();
+  }
+    }
+    
+    #ifndef HPHP_FILE_STREAM_WRAPPER_H
+#define HPHP_FILE_STREAM_WRAPPER_H
+    
+    void record_perf_mem_event(PerfEvent kind, const perf_event_sample* sample);
+    
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+    
+    using apollo::drivers::canbus::Byte;
+    
+    namespace apollo {
+namespace localization {
+namespace msf {
+    }
+    }
+    }
+    
+      auto geo_reference_node = header_node->FirstChildElement('geoReference');
+  if (!geo_reference_node) {
+    std::string err_msg = 'Error parsing header geoReoference attributes';
+    return Status(apollo::common::ErrorCode::HDMAP_DATA_ERROR, err_msg);
+  }
+  auto geo_text = geo_reference_node->FirstChild()->ToText();
+  if (!geo_text) {
+    std::string err_msg = 'Error parsing header geoReoference text';
+    return Status(apollo::common::ErrorCode::HDMAP_DATA_ERROR, err_msg);
+  }
+    
+    #include 'modules/canbus/vehicle/gem/protocol/accel_cmd_67.h'
+#include 'modules/canbus/vehicle/gem/protocol/accel_rpt_68.h'
+#include 'modules/canbus/vehicle/gem/protocol/brake_cmd_6b.h'
+#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_1_70.h'
+#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_2_71.h'
+#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_3_72.h'
+#include 'modules/canbus/vehicle/gem/protocol/brake_rpt_6c.h'
+#include 'modules/canbus/vehicle/gem/protocol/date_time_rpt_83.h'
+#include 'modules/canbus/vehicle/gem/protocol/global_cmd_69.h'
+#include 'modules/canbus/vehicle/gem/protocol/global_rpt_6a.h'
+#include 'modules/canbus/vehicle/gem/protocol/headlight_cmd_76.h'
+#include 'modules/canbus/vehicle/gem/protocol/headlight_rpt_77.h'
+#include 'modules/canbus/vehicle/gem/protocol/horn_cmd_78.h'
+#include 'modules/canbus/vehicle/gem/protocol/horn_rpt_79.h'
+#include 'modules/canbus/vehicle/gem/protocol/lat_lon_heading_rpt_82.h'
+#include 'modules/canbus/vehicle/gem/protocol/parking_brake_status_rpt_80.h'
+#include 'modules/canbus/vehicle/gem/protocol/shift_cmd_65.h'
+#include 'modules/canbus/vehicle/gem/protocol/shift_rpt_66.h'
+#include 'modules/canbus/vehicle/gem/protocol/steering_cmd_6d.h'
+#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_1_73.h'
+#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_2_74.h'
+#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_3_75.h'
+#include 'modules/canbus/vehicle/gem/protocol/steering_rpt_1_6e.h'
+#include 'modules/canbus/vehicle/gem/protocol/turn_cmd_63.h'
+#include 'modules/canbus/vehicle/gem/protocol/turn_rpt_64.h'
+#include 'modules/canbus/vehicle/gem/protocol/vehicle_speed_rpt_6f.h'
+#include 'modules/canbus/vehicle/gem/protocol/wheel_speed_rpt_7a.h'
+#include 'modules/canbus/vehicle/gem/protocol/wiper_cmd_90.h'
+#include 'modules/canbus/vehicle/gem/protocol/wiper_rpt_91.h'
+#include 'modules/canbus/vehicle/gem/protocol/yaw_rate_rpt_81.h'
