ba754719e391ee1fbe72926a3ba6b5dbb0a68be9
SUNsung <weatherius@gmail.com>
2019-01-14 08:53:27 +0200

    Rewrite [22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py] File in: 2019-01-14 08:53:26

--- pyton/22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py
+++ pyton/22.V44YRlrDRo22FeyDZUWjuDk22De9zSbvqdwc22l9a.py
@@ -1,73 +1,124 @@
 
         
-        def prepare_input(dirty):
-    '''
-    Prepare the plaintext by up-casing it
-    and separating repeated letters with X's
-    '''
-    
-    dirty = ''.join([c.upper() for c in dirty if c in string.ascii_letters])
-    clean = ''
-    
-    if len(dirty) < 2:
-        return dirty
-    
-    def main():
-    '''
-    In this demonstration we're generating a sample data set from the sin function in numpy.
-    We then train a decision tree on the data set and use the decision tree to predict the
-    label of 10 different test values. Then the mean squared error over this test is displayed.
-    '''
-    X = np.arange(-1., 1., 0.005)
-    y = np.sin(X)
-    
-    def compute_heterogeneity(data, k, centroids, cluster_assignment):
-    
-    heterogeneity = 0.0
-    for i in range(k):
-        
-        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
-        member_data_points = data[cluster_assignment==i, :]
-        
-        if member_data_points.shape[0] > 0: # check if i-th cluster is non-empty
-            # Compute distances from centroid to data points (RHS only)
-            distances = pairwise_distances(member_data_points, [centroids[i]], metric='euclidean')
-            squared_distances = distances**2
-            heterogeneity += np.sum(squared_distances)
-        
-    return heterogeneity
-    
-    word_bysig = collections.defaultdict(list)
-for word in word_list:
-    word_bysig[signature(word)].append(word)
-    
-    SERVICE_BROWSE_URL_SCHEMA = vol.Schema({
-    # pylint: disable=no-value-for-parameter
-    vol.Required(ATTR_URL, default=ATTR_URL_DEFAULT): vol.Url(),
-})
-    
-        def random_see(dev_id, name):
-        '''Randomize a sighting.'''
-        see(
-            dev_id=dev_id,
-            host_name=name,
-            gps=(hass.config.latitude + offset(),
-                 hass.config.longitude + offset()),
-            gps_accuracy=random.randrange(50, 150),
-            battery=random.randrange(10, 90)
-        )
+          # Compute the PCs.
+  all_data_mean_nx1 = np.mean(all_data_nxtc, axis=1, keepdims=True)
+  all_data_zm_nxtc = all_data_nxtc - all_data_mean_nx1
+  corr_mat_nxn = np.dot(all_data_zm_nxtc, all_data_zm_nxtc.T)
+  evals_n, evecs_nxn = np.linalg.eigh(corr_mat_nxn)
+  sidxs = np.flipud(np.argsort(evals_n)) # sort such that 0th is highest
+  evals_n = evals_n[sidxs]
+  evecs_nxn = evecs_nxn[:,sidxs]
     
     
-def get_scanner(hass, config):
-    '''Validate the configuration and return a Linksys AP scanner.'''
-    try:
-        return LinksysSmartWifiDeviceScanner(config[DOMAIN])
-    except ConnectionError:
-        return None
+def test_on_first_run(usage_tracker_io, usage_tracker_exists, shell_pid, logs):
+    shell_pid.return_value = 12
+    main()
+    usage_tracker_exists.return_value = False
+    _assert_tracker_updated(usage_tracker_io, 12)
+    logs.how_to_configure_alias.assert_called_once()
     
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/device_tracker.swisscom/
-'''
-import logging
     
-    _LOGGER = logging.getLogger(__name__)
+@pytest.mark.parametrize('command', [
+    Command('apt list --upgradable', no_match_output),
+    Command('sudo apt list --upgradable', no_match_output)
+])
+def test_not_match(command):
+    assert not match(command)
+    
+    
+@pytest.mark.skipif(_is_not_okay_to_test(),
+                    reason='No need to run if there\'s no formula')
+def test_match(brew_no_available_formula, brew_already_installed,
+               brew_install_no_argument):
+    assert match(Command('brew install elsticsearch',
+                         brew_no_available_formula))
+    assert not match(Command('brew install git',
+                             brew_already_installed))
+    assert not match(Command('brew install', brew_install_no_argument))
+    
+        def __call__(self, value):
+        keys = set(value)
+        missing_keys = self.keys - keys
+        if missing_keys:
+            raise ValidationError(
+                self.messages['missing_keys'],
+                code='missing_keys',
+                params={'keys': ', '.join(missing_keys)},
+            )
+        if self.strict:
+            extra_keys = keys - self.keys
+            if extra_keys:
+                raise ValidationError(
+                    self.messages['extra_keys'],
+                    code='extra_keys',
+                    params={'keys': ', '.join(extra_keys)},
+                )
+    
+        def create(self):
+        # Because a cache can fail silently (e.g. memcache), we don't know if
+        # we are failing to create a new session because of a key collision or
+        # because the cache is missing. So we try for a (large) number of times
+        # and then raise an exception. That's the risk you shoulder if using
+        # cache backing.
+        for i in range(10000):
+            self._session_key = self._get_new_session_key()
+            try:
+                self.save(must_create=True)
+            except CreateError:
+                continue
+            self.modified = True
+            return
+        raise RuntimeError(
+            'Unable to create a new session key. '
+            'It is likely that the cache is unavailable.')
+    
+    import mock
+    
+    from certbot import util
+    
+            with mock.patch('certbot.util.get_os_info') as mock_info:
+            for distro in entrypoint.OVERRIDE_CLASSES.keys():
+                mock_info.return_value = (distro, 'whatever')
+                self.assertEqual(entrypoint.get_configurator(),
+                                 entrypoint.OVERRIDE_CLASSES[distro])
+    
+            achall = self.achalls[0]
+        self.sni.add_chall(achall)
+        response = self.achalls[0].response(self.auth_key)
+        mock_setup_cert = mock.MagicMock(return_value=response)
+        # pylint: disable=protected-access
+        self.sni._setup_challenge_cert = mock_setup_cert
+    
+        print_info(site_info, title, mime, size)
+    if not info_only:
+        download_urls([real_url], title, ext, size, output_dir=output_dir, merge=merge)
+    
+    #----------------------------------------------------------------------
+def dilidili_download(url, output_dir = '.', merge = False, info_only = False, **kwargs):
+    global headers
+    re_str = r'http://www.dilidili.com/watch\S+'
+    if re.match(r'http://www.dilidili.wang', url):
+        re_str = r'http://www.dilidili.wang/watch\S+'
+        headers['Referer'] = 'http://www.dilidili.wang/'
+    elif re.match(r'http://www.dilidili.mobi', url):
+        re_str = r'http://www.dilidili.mobi/watch\S+'
+        headers['Referer'] = 'http://www.dilidili.mobi/'
+    
+    	type, ext, size = url_info(url)
+	print_info(site_info, title, type, size)
+	
+	if not info_only:
+		download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+        print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls(url, title, ext, size, output_dir, merge=False)
+    
+    def shorten_title(title):
+    m1 = re.search('[[0-9]*]', title)
+    m2 = re.search(''.*'', title)
+    if m1:
+        title = m1.group(0)
+    if m2:
+        title = ' '.join((title, m2.group(0)))   
+    return title[:50] + ' [...]'    
