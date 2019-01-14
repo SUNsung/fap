7c7edc625588c4264de7506975b4acf01e665c3b
SUNsung <weatherius@gmail.com>
2019-01-14 13:20:42 +0200

    Rewrite [42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb] File in: 2019-01-14 13:20:41

--- ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
+++ ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
@@ -1,64 +1,149 @@
 
         
-                def preload_commit_authors
-          # This also preloads the author of every commit. We're using 'lazy_author'
-          # here since 'author' immediately loads the data on the first call.
-          @pipeline.commit.try(:lazy_author)
-        end
+              # This method returns an HTML safe string similar to what <tt>Array#join</tt>
+      # would return. The array is flattened, and all items, including
+      # the supplied separator, are HTML escaped unless they are HTML
+      # safe, and the returned string is marked as HTML safe.
+      #
+      #   safe_join([raw('<p>foo</p>'), '<p>bar</p>'], '<br />')
+      #   # => '<p>foo</p>&lt;br /&gt;&lt;p&gt;bar&lt;/p&gt;'
+      #
+      #   safe_join([raw('<p>foo</p>'), raw('<p>bar</p>')], raw('<br />'))
+      #   # => '<p>foo</p><br /><p>bar</p>'
+      #
+      def safe_join(array, sep = $,)
+        sep = ERB::Util.unwrapped_html_escape(sep)
     
-                rows << {
-              label_id: label_id,
-              target_id: target_id,
-              target_type: issue.issuable_type,
-              created_at: time,
-              updated_at: time
-            }
+                # Prepend a hidden field to make sure something will be sent back to the
+            # server if all radio buttons are unchecked.
+            if options.fetch('include_hidden', true)
+              hidden_field + rendered_collection
+            else
+              rendered_collection
+            end
           end
     
-            def importer_class
-          LfsObjectImporter
+            class RadioButtonBuilder < Builder # :nodoc:
+          def radio_button(extra_html_options = {})
+            html_options = extra_html_options.merge(@input_html_options)
+            html_options[:skip_default_ids] = false
+            @template_object.radio_button(@object_name, @method_name, @value, html_options)
+          end
         end
     
-            def importer_class
-          NoteImporter
+          def instrument(name, **options) # :doc:
+        options[:identifier] ||= (@template && @template.identifier) || @path
+    
+          def perform(start_id, stop_id)
+        status_sql = Build
+          .where('ci_builds.commit_id = ci_stages.pipeline_id')
+          .where('ci_builds.stage = ci_stages.name')
+          .status_sql
+    
+            def representation_class
+          Representation::Issue
         end
     
-            client.each_page(collection_method, repo, options) do |page|
-          # Technically it's possible that the same work is performed multiple
-          # times, as Sidekiq doesn't guarantee there will ever only be one
-          # instance of a job. In such a scenario it's possible for one job to
-          # have a lower page number (e.g. 5) compared to another (e.g. 10). In
-          # this case we skip over all the objects until we have caught up,
-          # reducing the number of duplicate jobs scheduled by the provided
-          # block.
-          next unless page_counter.set(page.number)
-    
-            # attributes - A Hash containing the raw PR details. The keys of this
-        #              Hash (and any nested hashes) must be symbols.
-        def initialize(attributes)
-          @attributes = attributes
+            def id_for_already_imported_cache(note)
+          note.id
         end
-    
-      let(:cop_config) { { 'EnforcedStyle' => 'symmetrical' } }
-    
-      include_examples 'multiline literal brace layout' do
-    let(:open) { '{' }
-    let(:close) { '}' }
-    let(:a) { 'a: 1' }
-    let(:b) { 'b: 2' }
-    let(:multi_prefix) { 'b: ' }
-    let(:multi) do
-      <<-RUBY.strip_indent.chomp
-        [
-        1
-        ]
-      RUBY
-    end
-  end
-    
-    shared_examples_for 'multiline literal brace layout method argument' do
-  include MultilineLiteralBraceHelper
-    
-    ERR
       end
     end
+  end
+end
+
+    
+    module Gitlab
+  module GithubImport
+    # IssuableFinder can be used for caching and retrieving database IDs for
+    # issuable objects such as issues and pull requests. By caching these IDs we
+    # remove the need for running a lot of database queries when importing
+    # GitHub projects.
+    class IssuableFinder
+      attr_reader :project, :object
+    
+              new(hash)
+        end
+    
+      it 'renders the import form' do
+    visit new_scenario_imports_path
+    expect(page).to have_text('Import a Public Scenario')
+  end
+    
+      describe '.seed' do
+    it 'imports a set of agents to get the user going when they are first created' do
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
+    
+          it 'handles signals' do
+        @agent_runner.instance_variable_set(:@signal_queue, ['TERM'])
+        @agent_runner.run
+      end
+    end
+    
+      describe 'converting escaped JSONPath strings' do
+    it 'should work' do
+      expect(LiquidMigrator.convert_string('Weather looks like <$.conditions> according to the forecast at <$.pretty_date.time>')).to eq(
+                                    'Weather looks like {{conditions}} according to the forecast at {{pretty_date.time}}'
+      )
+    end
+    
+        @checker = Agents::BasecampAgent.new(:name => 'somename', :options => @valid_params)
+    @checker.service = services(:generic)
+    @checker.user = users(:jane)
+    @checker.save!
+    
+        change.down do
+      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
+      Mention.where(mentions_container_type: 'Comment').destroy_all
+      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
+    end
+  end
+end
+
+    
+    When /^I toggle nsfw posts$/ do
+  find('.toggle_nsfw_state', match: :first).click
+end
+    
+      def confirm_on_page(page_name)
+    if page_name == 'my profile page'
+      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
+    else
+      expect(page).to have_path(path_to(page_name))
+    end
+  end
+end
+    
+          it 'supports filtering by read/unread' do
+        FactoryGirl.create(:notification, :recipient => alice, :target => @post)
+        get :read_all
+        FactoryGirl.create(:notification, :recipient => alice, :target => @post)
+        get :index, params: {show: 'unread'}
+        expect(assigns[:notifications].count).to eq(1)
+      end
+    end
+    
+          # Returns the else branch of the `case` statement, if any.
+      #
+      # @return [Node] the else branch node of the `case` statement
+      # @return [nil] if the case statement does not have an else branch.
+      def else_branch
+        node_parts[-1]
+      end
+    
+          # Calls the given block for each `value` node in the `hash` literal.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_value
+        return pairs.map(&:value).to_enum unless block_given?
+    
+          # Calls the given block for each condition node in the `when` branch.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_condition
+        return conditions.to_enum(__method__) unless block_given?
