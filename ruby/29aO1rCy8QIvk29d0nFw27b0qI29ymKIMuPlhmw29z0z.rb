3aec3434d0e4300ed194a0de21cb253e81ab2295
SUNsung <weatherius@gmail.com>
2019-01-15 12:29:05 +0200

    Rewrite [29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb] File in: 2019-01-15 12:29:04

--- ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
+++ ruby/29aO1rCy8QIvk29d0nFw27b0qI29ymKIMuPlhmw29z0z.rb
@@ -1,117 +1,76 @@
 
         
-                $stderr.puts
-        opoo out
-        Homebrew.failed = true
-        first_warning = false
+                    # Prepend a hidden field to make sure something will be sent back to the
+            # server if all radio buttons are unchecked.
+            if options.fetch('include_hidden', true)
+              hidden_field + rendered_collection
+            else
+              rendered_collection
+            end
+          end
+    
+        def compact
+      PathSet.new paths.compact
+    end
+    
+        initializer 'action_view.per_request_digest_cache' do |app|
+      ActiveSupport.on_load(:action_view) do
+        unless ActionView::Resolver.caching?
+          app.executor.to_run ActionView::Digestor::PerExecutionDigestCacheExpiry
+        end
       end
     end
     
-        root.children.sort.each do |pn|
-      if pn.directory?
-        dirs << pn
-      elsif block_given? && yield(pn)
-        puts pn
-        other = 'other '
-      else
-        remaining_root_files << pn unless pn.basename.to_s == '.DS_Store'
-      end
-    end
+      find_files = ->(path) {
+    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
+      path if File.file?(path)
+    end.compact
+  }
     
-          begin
-        f = Formulary.factory(new_full_name)
-      rescue Exception => e
-        onoe e if ARGV.homebrew_developer?
-        next
-      end
-    
-        def destroy
-      authorize @report_note, :destroy?
-      @report_note.destroy!
-      redirect_to admin_report_path(@report_note.report_id), notice: I18n.t('admin.report_notes.destroyed_msg')
-    end
-    
-      def body
-    @_body ||= request.body.read
-  end
-    
-            # Sends a Kerberos Request over a tcp connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [Integer] the number of bytes sent
-        # @raise [RuntimeError] if the request can't be encoded
-        def send_request_tcp(req)
-          data = req.encode
-          length = [data.length].pack('N')
-          connection.put(length + data)
+            def find_order
+          @order = Spree::Order.find_by!(number: order_id)
         end
     
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
+            def update
+          if @property
+            authorize! :update, @property
+            @property.update_attributes(property_params)
+            respond_with(@property, status: 200, default_template: :show)
+          else
+            invalid_resource!(@property)
+          end
+        end
     
-                res = ''
-            case etype
-            when RC4_HMAC
-              res = encrypt_rc4_hmac(data, key, 5)
-            else
-              raise ::NotImplementedError, 'EncryptedData schema is not supported'
-            end
-    
-      # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-    
-      # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
-  end
-  config.static_cache_control = 'public, max-age=3600'
-    
-        def URIEncodePair(cc1, cc2, result, index)
-      u = ((cc1 >> 6) & 0xF) + 1;
-      w = (cc1 >> 2) & 0xF;
-      x = cc1 & 3;
-      y = (cc2 >> 6) & 0xF;
-      z = cc2 & 63;
-      octets = Array.new(4);
-      octets[0] = (u >> 2) + 240;
-      octets[1] = (((u & 3) << 4) | w) + 128;
-      octets[2] = ((x << 4) | y) + 128;
-      octets[3] = z + 128;
-      return URIEncodeOctets(octets, result, index);
-    end
-    
-          def string_to_code(string)
-        # sha bytes
-        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
-        # Thanks donpark's IdenticonUtil.java for this.
-        # Match the following Java code
-        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
-        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
-    
-          # Wraps page formatted data to Nokogiri::HTML document.
-      #
-      def build_document(content)
-        Nokogiri::HTML::fragment(%{<div id='gollum-root'>} + content.to_s + %{</div>}, 'UTF-8')
+            def return_authorization_params
+          params.require(:return_authorization).permit(permitted_return_authorization_attributes)
+        end
       end
-    
-        post '/edit/Multibyte',
-         :content => 'りんご', :header => 'みかん', :footer => 'バナナ', :sidebar => 'スイカ',
-         :page    => 'Multibyte', :format => :markdown, :message => 'mesg'
-    
-        @wiki = Gollum::Wiki.new(@path)
-    page  = @wiki.page('k')
-    assert_equal '바뀐 text', utf8(page.raw_data)
-    assert_equal 'ghi', page.version.message
+    end
   end
+end
+
+    
+              if params[:page] || params[:per_page]
+            @states = @states.page(params[:page]).per(params[:per_page])
+          end
+    
+            def update
+          authorize! :update, @store
+          if @store.update_attributes(store_params)
+            respond_with(@store, status: 200, default_template: :show)
+          else
+            invalid_resource!(@store)
+          end
+        end
+    
+            def new; end
+    
+            def create
+          authorize! :create, Spree.user_class
+          @user = Spree.user_class.new(user_params)
+          if @user.save
+            respond_with(@user, status: 201, default_template: :show)
+          else
+            invalid_resource!(@user)
+          end
+        end
