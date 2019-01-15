3aee4c7c134fac8468b8ab6dbcd151bb3bcc05d2
SUNsung <weatherius@gmail.com>
2019-01-15 22:37:49 +0200

    Rewrite [25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py] File in: 2019-01-15 22:37:48

--- pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
+++ pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
@@ -1,31 +1,77 @@
 
         
-        
-class ConnectTimeout(ConnectionError, Timeout):
-    '''The request timed out while trying to connect to the remote server.
+        from mrjob.job import MRJob
     
-        def __enter__(self):
-        self.start()
-        self.ready_event.wait(self.WAIT_EVENT_TIMEOUT)
-        return self.host, self.port
+        def extract_year_month(self, line):
+        '''Return the year and month portions of the timestamp.'''
+        pass
     
-    
-def dispatch_hook(key, hooks, hook_data, **kwargs):
-    '''Dispatches a hook dictionary on a given piece of data.'''
-    hooks = hooks or {}
-    hooks = hooks.get(key)
-    if hooks:
-        if hasattr(hooks, '__call__'):
-            hooks = [hooks]
-        for hook in hooks:
-            _hook_data = hook(hook_data, **kwargs)
-            if _hook_data is not None:
-                hook_data = _hook_data
-    return hook_data
-
+        The message bytes are converted to an encoding suitable for
+    `self.env.stdout`. Unicode errors are replaced and binary data
+    is suppressed. The body is always streamed by line.
     
     
-def test_idna_with_version_attribute(mocker):
-    '''Verify we're actually setting idna version when it should be available.'''
-    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
-    assert info()['idna'] == {'version': '2.6'}
+def test_unicode_basic_auth(httpbin):
+    # it doesn't really authenticate us because httpbin
+    # doesn't interpret the utf8-encoded auth
+    http('--verbose', '--auth', u'test:%s' % UNICODE,
+         httpbin.url + u'/basic-auth/test/' + UNICODE)
+    
+        def __init__(self, directory=DEFAULT_CONFIG_DIR):
+        super(Config, self).__init__()
+        self.update(self.DEFAULTS)
+        self.directory = directory
+    
+        @pytest.mark.parametrize(
+        'other, result', (
+            ({'AccePT': 'application/json'}, True),
+            ({}, False),
+            (None, False)
+        )
+    )
+    def test_instance_equality(self, other, result):
+        assert (self.case_insensitive_dict == other) is result
+    
+        @pytest.mark.parametrize(
+        'value, expected', (
+            ([('key', 'val')], [('key', 'val')]),
+            ((('key', 'val'), ), [('key', 'val')]),
+            ({'key': 'val'}, [('key', 'val')]),
+            (None, None)
+        ))
+    def test_valid(self, value, expected):
+        assert to_key_val_list(value) == expected
+    
+        requests = [2, 5, 14, 22, 18, 3, 35, 27, 20]
+    for request in requests:
+        h0.handle(request)
+    
+        def __init__(self, one, other):
+        self._one = one
+        self._other = other
+    
+    
+class Visitor(object):
+    def visit(self, node, *args, **kwargs):
+        meth = None
+        for cls in node.__class__.__mro__:
+            meth_name = 'visit_' + cls.__name__
+            meth = getattr(self, meth_name, None)
+            if meth:
+                break
+    
+    *References:
+bottle
+https://github.com/bottlepy/bottle/blob/cafc15419cbb4a6cb748e6ecdccf92893bb25ce5/bottle.py#L270
+django
+https://github.com/django/django/blob/ffd18732f3ee9e6f0374aff9ccf350d85187fac2/django/utils/functional.py#L19
+pip
+https://github.com/pypa/pip/blob/cb75cca785629e15efb46c35903827b3eae13481/pip/utils/__init__.py#L821
+pyramimd
+https://github.com/Pylons/pyramid/blob/7909e9503cdfc6f6e84d2c7ace1d3c03ca1d8b73/pyramid/decorator.py#L4
+werkzeug
+https://github.com/pallets/werkzeug/blob/5a2bf35441006d832ab1ed5a31963cbc366c99ac/werkzeug/utils.py#L35
+    
+    production code which is untestable:
+    
+        data = Data()
