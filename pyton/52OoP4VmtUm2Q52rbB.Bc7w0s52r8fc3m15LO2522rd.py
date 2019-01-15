3783018ba20e31c7ee3ab9039fa668cbca3db094
SUNsung <weatherius@gmail.com>
2019-01-15 14:04:06 +0200

    Rewrite [52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py] File in: 2019-01-15 14:04:05

--- pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
+++ pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
@@ -1,143 +1,81 @@
 
         
-            n_samples = 2000
-    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
-                                           list_n_features, precompute=False)
-    plt.subplot(212)
-    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
-    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
-    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
-    plt.legend(loc='upper left')
-    plt.xlabel('number of features')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
-
+            Returns:
+      The mean loss for the epoch.
     
-        Returns
-    -------
-    array of floats shaped like (metrics, formats, samples, classes, density)
-        Time in seconds.
+        Returns:
+      a 3 tuple of costs:
+        (epoch total cost, epoch reconstruction cost, epoch KL cost)
     '''
-    metrics = np.atleast_1d(metrics)
-    samples = np.atleast_1d(samples)
-    classes = np.atleast_1d(classes)
-    density = np.atleast_1d(density)
-    formats = np.atleast_1d(formats)
-    out = np.zeros((len(metrics), len(formats), len(samples), len(classes),
-                    len(density)), dtype=float)
-    it = itertools.product(samples, classes, density)
-    for i, (s, c, d) in enumerate(it):
-        _, y_true = make_multilabel_classification(n_samples=s, n_features=1,
-                                                   n_classes=c, n_labels=d * c,
-                                                   random_state=42)
-        _, y_pred = make_multilabel_classification(n_samples=s, n_features=1,
-                                                   n_classes=c, n_labels=d * c,
-                                                   random_state=84)
-        for j, f in enumerate(formats):
-            f_true = f(y_true)
-            f_pred = f(y_pred)
-            for k, metric in enumerate(metrics):
-                t = timeit(partial(metric, f_true, f_pred), number=n_times)
+    ops_to_eval = [self.cost, self.recon_cost, self.kl_cost]
+    collected_op_values = self.run_epoch(datasets, ops_to_eval, kind=kind,
+                                         keep_prob=1.0)
+    
+    flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'conditioned_rnn_data',
+                    'Name of data file for input case.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 400, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 10,
+                     'Number of spikifications of the same underlying rates.')
+flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
+flags.DEFINE_float('x0_std', 1.0,
+                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
+FLAGS = flags.FLAGS
+    
+      full_name_stem = os.path.join(data_path, data_fname_stem)
+  for s, data_dict in dataset_dict.items():
+    write_data(full_name_stem + '_' + s, data_dict, compression=compression)
+    
+        Args:
+      filename: Vocabulary file name.
+    '''
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+    
+      Returns:
+    A decoder function with the required interface of `dynamic_rnn_decoder`
+    intended for training.
+  '''
+  with tf.name_scope(name, 'attention_decoder_fn_train', [
+      encoder_state, attention_keys, attention_values, attention_score_fn,
+      attention_construct_fn
+  ]):
+    pass
     
     
-def get_data(N, D, dataset='dense'):
-    if dataset == 'dense':
-        np.random.seed(0)
-        return np.random.random((N, D))
-    elif dataset == 'digits':
-        X = datasets.load_digits().data
-        i = np.argsort(X[0])[::-1]
-        X = X[:, i]
-        return X[:N, :D]
-    else:
-        raise ValueError('invalid dataset: %s' % dataset)
+class RedirectFallbackMiddleware(MiddlewareMixin):
+    # Defined as class-level attributes to be subclassing-friendly.
+    response_gone_class = HttpResponseGone
+    response_redirect_class = HttpResponsePermanentRedirect
     
-                gc.collect()
-            print('benchmarking scikit-learn randomized_svd: n_iter=0')
-            tstart = time()
-            randomized_svd(X, rank, n_iter=0)
-            results['scikit-learn randomized_svd (n_iter=0)'].append(
-                time() - tstart)
+    KEY_PREFIX = 'django.contrib.sessions.cache'
     
-    
-def get_pdf_size(version):
-    api_url = ROOT_URL + '%s/_downloads' % version
-    for path_details in json_urlread(api_url):
-        if path_details['name'] == 'scikit-learn-docs.pdf':
-            return human_readable_data_quantity(path_details['size'], 1000)
-    
-    
-def setup(app):
-    # Format template for issues URI
-    # e.g. 'https://github.com/sloria/marshmallow/issues/{issue}
-    app.add_config_value('issues_uri', default=None, rebuild='html')
-    # Shortcut for GitHub, e.g. 'sloria/marshmallow'
-    app.add_config_value('issues_github_path', default=None, rebuild='html')
-    # Format template for user profile URI
-    # e.g. 'https://github.com/{user}'
-    app.add_config_value('issues_user_uri', default=None, rebuild='html')
-    app.add_role('issue', issue_role)
-    app.add_role('user', user_role)
+        def flush(self):
+        '''
+        Remove the current session data from the database and regenerate the
+        key.
+        '''
+        self.clear()
+        self.delete(self.session_key)
+        self._session_key = None
 
     
-        @classmethod
-    def _parse_args(cls, args):
-        # This is useful when you don't want to create an instance, just
-        # canonicalize some constructor arguments.
-        parts = []
-        for a in args:
-            if isinstance(a, PurePath):
-                parts += a._parts
-            else:
-                a = os.fspath(a)
-                if isinstance(a, str):
-                    # Force-cast str subclasses to str (issue #21127)
-                    parts.append(str(a))
-                else:
-                    raise TypeError(
-                        'argument should be a str object or an os.PathLike '
-                        'object returning str, not %r'
-                        % type(a))
-        return cls._flavour.parse_parts(parts)
     
-    
-# Expand paths beginning with '~' or '~user'.
-# '~' means $HOME; '~user' means that user's home directory.
-# If the path doesn't begin with '~', or if the user or $HOME is unknown,
-# the path is returned unchanged (leaving error reporting to whatever
-# function is called with the expanded path as argument).
-# See also module 'glob' for expansion of *, ? and [...] in pathnames.
-# (A function should also be defined to do full *sh-style environment
-# variable expansion.)
-    
-        def test_parts_interning(self):
-        P = self.cls
-        p = P('/usr/bin/foo')
-        q = P('/usr/local/bin')
-        # 'usr'
-        self.assertIs(p.parts[1], q.parts[1])
-        # 'bin'
-        self.assertIs(p.parts[2], q.parts[3])
-    
-        def test_xdev(self):
-        # sys.flags.dev_mode
-        code = 'import sys; print(sys.flags.dev_mode)'
-        out = self.run_xdev('-c', code, xdev=False)
-        self.assertEqual(out, 'False')
-        out = self.run_xdev('-c', code)
-        self.assertEqual(out, 'True')
-    
-    # Create the base text message.
-msg = EmailMessage()
-msg['Subject'] = 'Ayons asperges pour le déjeuner'
-msg['From'] = Address('Pepé Le Pew', 'pepe', 'example.com')
-msg['To'] = (Address('Penelope Pussycat', 'penelope', 'example.com'),
-             Address('Fabrette Pussycat', 'fabrette', 'example.com'))
-msg.set_content('''\
-Salut!
-    
-    def handleSlide(slide):
-    handleSlideTitle(slide.getElementsByTagName('title')[0])
-    handlePoints(slide.getElementsByTagName('point'))
+class SessionStore(SessionBase):
+    '''
+    Implement database session store.
+    '''
+    def __init__(self, session_key=None):
+        super().__init__(session_key)
