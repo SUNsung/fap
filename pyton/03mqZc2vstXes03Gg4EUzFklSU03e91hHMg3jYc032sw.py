91a2305bed68ece3278c0c0683a1b5e02c5a5065
SUNsung <weatherius@gmail.com>
2019-01-16 08:04:04 +0200

    Rewrite [03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py] File in: 2019-01-16 08:04:03

--- pyton/03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py
+++ pyton/03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py
@@ -1,71 +1,91 @@
 
         
-            def _get_session_from_db(self):
+        # Read lines from the linkcheck output file
+try:
+    with open('build/linkcheck/output.txt') as out:
+        output_lines = out.readlines()
+except IOError:
+    print('linkcheck output not found; please run linkcheck first.')
+    exit(1)
+    
+        def start_requests(self):
+        qargs = {'total': self.total, 'show': self.show}
+        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
+        return [scrapy.Request(url, dont_filter=True)]
+    
+            editor = self.settings['EDITOR']
         try:
-            return self.model.objects.get(
-                session_key=self.session_key,
-                expire_date__gt=timezone.now()
-            )
-        except (self.model.DoesNotExist, SuspiciousOperation) as e:
-            if isinstance(e, SuspiciousOperation):
-                logger = logging.getLogger('django.security.%s' % e.__class__.__name__)
-                logger.warning(str(e))
-            self._session_key = None
+            spidercls = self.crawler_process.spider_loader.load(args[0])
+        except KeyError:
+            return self._err('Spider not found: %s' % args[0])
     
+        def process_spider_arguments(self, opts):
     
-class AbstractBaseSession(models.Model):
-    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
-    session_data = models.TextField(_('session data'))
-    expire_date = models.DateTimeField(_('expire date'), db_index=True)
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+                          help='set spider argument (may be repeated)')
+        parser.add_option('-o', '--output', metavar='FILE',
+                          help='dump scraped items into FILE (use - for stdout)')
+        parser.add_option('-t', '--output-format', metavar='FORMAT',
+                          help='format to use for dumping items with -o')
     
-        def __exit__(self, exc_type, exc_value, traceback):
-        if exc_type is None:
-            return
-        for dj_exc_type in (
-                DataError,
-                OperationalError,
-                IntegrityError,
-                InternalError,
-                ProgrammingError,
-                NotSupportedError,
-                DatabaseError,
-                InterfaceError,
-                Error,
-        ):
-            db_exc_type = getattr(self.wrapper.Database, dj_exc_type.__name__)
-            if issubclass(exc_type, db_exc_type):
-                dj_exc_value = dj_exc_type(*exc_value.args)
-                # Only set the 'errors_occurred' flag for errors that may make
-                # the connection unusable.
-                if dj_exc_type not in (DataError, IntegrityError):
-                    self.wrapper.errors_occurred = True
-                raise dj_exc_value.with_traceback(traceback) from exc_value
+        # Gloss the lips
+    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
+    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
     
-    from sklearn.cluster import AgglomerativeClustering
+        # For a definition of each point index, see https://cdn-images-1.medium.com/max/1600/1*AbEg31EgkbXSQehuNJBlWg.png
+    if model == 'large':
+        return [{
+            'chin': points[0:17],
+            'left_eyebrow': points[17:22],
+            'right_eyebrow': points[22:27],
+            'nose_bridge': points[27:31],
+            'nose_tip': points[31:36],
+            'left_eye': points[36:42],
+            'right_eye': points[42:48],
+            'top_lip': points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
+            'bottom_lip': points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
+        } for points in landmarks_as_tuples]
+    elif model == 'small':
+        return [{
+            'nose_tip': [points[4]],
+            'left_eye': points[2:4],
+            'right_eye': points[0:2],
+        } for points in landmarks_as_tuples]
+    else:
+        raise ValueError('Invalid landmarks model type. Supported models are ['small', 'large'].')
     
-    np.random.seed(42)
+        if os.path.isdir(image_to_check):
+        if cpus == 1:
+            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
+        else:
+            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
+    else:
+        test_image(image_to_check, model)
     
-        opener = build_opener()
-    html_filename = os.path.join(html_folder, lang + '.html')
-    if not os.path.exists(html_filename):
-        print('Downloading %s' % page)
-        request = Request(page)
-        # change the User Agent to avoid being blocked by Wikipedia
-        # downloading a couple of articles should not be considered abusive
-        request.add_header('User-Agent', 'OpenAnything/1.0')
-        html_content = opener.open(request).read()
-        open(html_filename, 'wb').write(html_content)
+                face_names.append(name)
     
-    from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import Perceptron
-from sklearn.pipeline import Pipeline
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
+    # Get a reference to the Raspberry Pi camera.
+# If this fails, make sure you have a camera connected to the RPi and that you
+# enabled your camera in raspi-config and rebooted first.
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
     
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
+    # Loop through each face found in the unknown image
+for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    # See if the face is a match for the known face(s)
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
     
-    solutions = os.listdir(exercise_dir)
+            if True in result:
+            [print_result(image_to_check, name, distance, show_distance) for is_match, name, distance in zip(result, known_names, distances) if is_match]
+        else:
+            print_result(image_to_check, 'unknown_person', None, show_distance)
+    
+    # 你需要一个2代以上的树莓派，并在树莓派上安装face_recognition，并连接上picamera摄像头
+# 并确保picamera这个模块已经安装（树莓派一般会内置安装）
+# 你可以参考这个教程配制你的树莓派：
+# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
