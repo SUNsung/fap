f1f470a22dd4e104350dccc744f1f943617fbe2e
SUNsung <weatherius@gmail.com>
2019-01-17 00:28:08 +0200

    Rewrite [09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp] File in: 2019-01-17 00:28:07

--- cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
+++ cccccc/09sGCAjvzewk09o9tdVUfv6ZY09QLQG8j54lXs09k8I.cpp
@@ -1,247 +1,134 @@
 
         
-        #include 'base/command_line.h'
-    
-    #include 'atom/browser/native_window_views.h'
-    
-    // static
-void InAppPurchase::BuildPrototype(v8::Isolate* isolate,
-                                   v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, 'InAppPurchase'));
-  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
-      .SetMethod('canMakePayments', &in_app_purchase::CanMakePayments)
-      .SetMethod('getReceiptURL', &in_app_purchase::GetReceiptURL)
-      .SetMethod('purchaseProduct', &InAppPurchase::PurchaseProduct)
-      .SetMethod('finishAllTransactions',
-                 &in_app_purchase::FinishAllTransactions)
-      .SetMethod('finishTransactionByDate',
-                 &in_app_purchase::FinishTransactionByDate)
-      .SetMethod('getProducts', &in_app_purchase::GetProducts);
-}
-    
-    #ifndef ATOM_BROWSER_API_ATOM_API_IN_APP_PURCHASE_H_
-#define ATOM_BROWSER_API_ATOM_API_IN_APP_PURCHASE_H_
-    
-    // static
-v8::Local<v8::Value> PowerMonitor::Create(v8::Isolate* isolate) {
-  if (!Browser::Get()->is_ready()) {
-    isolate->ThrowException(v8::Exception::Error(mate::StringToV8(
-        isolate,
-        'Cannot require \'powerMonitor\' module before app is ready')));
-    return v8::Null(isolate);
-  }
-    }
-    
-    #ifndef ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
-#define ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
-    
-    SavePageHandler::SavePageHandler(content::WebContents* web_contents,
-                                 const SavePageCallback& callback)
-    : web_contents_(web_contents), callback_(callback) {}
-    
-      // Finally, add the generic parameters from the requirement.
-  for (auto genericParam : reqSig->getGenericParams().slice(1)) {
-    // The only depth that makes sense is depth == 1, the generic parameters
-    // of the requirement itself. Anything else is from invalid code.
-    if (genericParam->getDepth() != 1) {
-      return;
-    }
-    }
-    
-    using namespace swift::sys;
-using llvm::StringRef;
-    
-      // Set the 'os' platform condition.
-  if (Target.isMacOSX())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'OSX');
-  else if (Target.isTvOS())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'tvOS');
-  else if (Target.isWatchOS())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'watchOS');
-  else if (Target.isiOS())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'iOS');
-  else if (Target.isAndroid())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'Android');
-  else if (Target.isOSLinux())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'Linux');
-  else if (Target.isOSFreeBSD())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'FreeBSD');
-  else if (Target.isOSWindows())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'Windows');
-  else if (Target.isWindowsCygwinEnvironment())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'Cygwin');
-  else if (Target.isPS4())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'PS4');
-  else if (Target.isOSHaiku())
-    addPlatformConditionValue(PlatformConditionKind::OS, 'Haiku');
-  else
-    UnsupportedOS = true;
-    
-    Optional<swift::UUID> swift::UUID::fromString(const char *s) {
-#if defined(_WIN32)
-  RPC_CSTR t = const_cast<RPC_CSTR>(reinterpret_cast<const unsigned char*>(s));
-    }
-    
-    
-    {  return GraphemeClusterBreakProperty::Other;
-}
-    
-            if(cn > 1)
-        {
-            for(size_t j = 0, jn = 0; j < size.width; ++j, jn += cn)
-            {
-                size_t maxIdx = jn;
-                for(s32 k = 1; k < cn; ++k)
-                    if(_norm[jn + k] > _norm[maxIdx]) maxIdx = jn + k;
-                _norm[j] = _norm[maxIdx];
-                _dx[j] = _dx[maxIdx];
-                _dy[j] = _dy[maxIdx];
-            }
-        }
-    
-    #endif //CAROTENE_NEON
-    
-    
-    {            vst1q_s16(dst + j, v_dst0);
-            vst1q_s16(dst + j + 8, v_dst1);
-        }
-        for (; j < roiw8; j += 8)
-        {
-            int16x8_t v_dst = vreinterpretq_s16_u16(vmovl_u8(vld1_u8(src + j)));
-            vst1q_s16(dst + j, v_dst);
-        }
-    
-    
-    {} // namespace CAROTENE_NS
+        
+    {REGISTER_INTERNAL(FeatureVectorsConfigParserPlugin,
+                  'config_parser',
+                  'feature_vectors');
+} // namespace osquery
 
     
     
-    {        for (; i < size.width; ++i)
-            result += s32(src0[i]) * s32(src1[i]);
-    }
-    return result;
-#else
-    (void)_size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    
-            for (; j < roiw_tail; j += step_tail, js += step_tail3, jd -= step_tail3)
-        {
-            vec64 v_src = vld3(src + js), v_dst;
-            v_dst.val[0] = vrev64(v_src.val[0]);
-            v_dst.val[1] = vrev64(v_src.val[1]);
-            v_dst.val[2] = vrev64(v_src.val[2]);
-    }
-    
-    #include 'common.hpp'
-    
-      rocksdb::SyncPoint::GetInstance()->LoadDependency({
-      {'CompactionJob::Run():Start', 'DynamicLevelMaxBytesBase2:0'},
-      {'DynamicLevelMaxBytesBase2:1', 'CompactionJob::Run():End'},
-      {'DynamicLevelMaxBytesBase2:compact_range_finish',
-       'FlushJob::WriteLevel0Table'},
-  });
-  rocksdb::SyncPoint::GetInstance()->EnableProcessing();
-    
-    class DBEncryptionTest : public DBTestBase {
- public:
-  DBEncryptionTest() : DBTestBase('/db_encryption_test') {}
+    { private:
+  static const std::string kLoggerKey;
 };
     
-        // We try twice: the first time with a fixed-size stack allocated buffer,
-    // and the second time with a much larger dynamically allocated buffer.
-    char buffer[500];
-    for (int iter = 0; iter < 2; iter++) {
-      char* base;
-      int bufsize;
-      if (iter == 0) {
-        bufsize = sizeof(buffer);
-        base = buffer;
-      } else {
-        bufsize = 30000;
-        base = new char[bufsize];
-      }
-      char* p = base;
-      char* limit = base + bufsize;
+    TEST_F(DecoratorsConfigParserPluginTests, test_decorators_list) {
+  // Assume the decorators are disabled.
+  Config::get().update(config_data_);
+  auto parser = Config::getParser('decorators');
+  EXPECT_NE(parser, nullptr);
     }
     
-      // Results of these can be affected by FakeSleepForMicroseconds()
-  virtual Status GetCurrentTime(int64_t* unix_time) override;
-  virtual uint64_t NowMicros() override;
-  virtual uint64_t NowNanos() override;
     
-    std::string kDBPath = '/tmp/rocksdb_column_families_example';
-    
-    
-    {}  // namespace
-    
-    // move-only type
-Expected<std::unique_ptr<int>, Err> f3(int x, double y) {
-  return std::make_unique<int>(int(x + y));
-}
-    
-    void BENCHFUN(fillCtor)(int iters, int size) {
-  FOR_EACH_RANGE (i, 0, iters) {
-    VECTOR v(size_t(size), randomObject<VECTOR::value_type>());
-    doNotOptimizeAway(&v);
+    {    // Construct a config map, the typical output from `Config::genConfig`.
+    config_data_['awesome'] = content_;
+    Config::get().reset();
   }
-}
-BENCHMARK_PARAM(BENCHFUN(fillCtor), 16)
-BENCHMARK_PARAM(BENCHFUN(fillCtor), 128)
-BENCHMARK_PARAM(BENCHFUN(fillCtor), 1024)
     
-    template <size_t... Indexes>
-int snprintf20Numbers(int i, index_sequence<Indexes...>) {
-  static_assert(20 == sizeof...(Indexes), 'Must have exactly 20 indexes');
-  return snprintf(
-      bigBuf.data(),
-      bigBuf.size(),
-      '%d %d %d %d %d'
-      '%d %d %d %d %d'
-      '%d %d %d %d %d'
-      '%d %d %d %d %d',
-      (i + static_cast<int>(Indexes))...);
-}
     
-    int main(int argc, char** argv) {
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
-  folly::runBenchmarks();
-  return 0;
+    {
+    {  c.reset();
+}
 }
 
     
-    static uint32_t pointerTestFunc() {
-  uint32_t v1 = folly::Random::rand32();
-  uint32_t v2 = folly::Random::rand32();
-  std::string str = 'test string';
-  const char* a = str.c_str();
-  FOLLY_SDT(folly, test_static_tracepoint_pointer, a, v2, &v1);
-  return v1 + v2;
+    class TLSConfigPlugin;
+    
+    void InMemoryDatabase::close() {
+  VLOG(1) << 'Closing db... ';
+  debug_only::verifyTrue(is_open_, 'database is not open');
+  is_open_ = false;
+  auto status = destroyDB();
+  debug_only::verifyTrue(status.isValue(),
+                         'InMemoryDatabase::destroyDB couldn't fail');
 }
     
-    // Empirically, this indicates if the runtime supports
-// std::exception_ptr, as not all (arm, for instance) do.
-#if defined(__GNUC__) && defined(__GCC_ATOMIC_INT_LOCK_FREE) && \
-    __GCC_ATOMIC_INT_LOCK_FREE > 1
-inline fbstring exceptionStr(std::exception_ptr ep) {
-  try {
-    std::rethrow_exception(ep);
-  } catch (const std::exception& e) {
-    return exceptionStr(e);
-  } catch (...) {
-    return '<unknown exception>';
+    
+    {  EXPECT_TRUE(db->putString(kPersistentSettings, 'test_key_string', 'string'));
+  auto string_value = db->getString(kPersistentSettings, 'test_key_string');
+  EXPECT_TRUE(string_value);
+  EXPECT_EQ(string_value.take(), 'string');
+}
+    
+    TEST_F(RocksdbDatabaseTest, test_unknown_key) {
+  auto db = std::make_unique<RocksdbDatabase>('test', path_);
+  ASSERT_TRUE(db->open());
+  EXPECT_TRUE(db->putInt32(kPersistentSettings, 'key', 12));
+  auto result = db->getInt32(kPersistentSettings, 'key_');
+  EXPECT_FALSE(result);
+  EXPECT_EQ(result.takeError(), DatabaseError::KeyNotFound);
+}
+    
+    BENCHMARK_PARAM(BENCHFUN(pushBack), 16)
+BENCHMARK_PARAM(BENCHFUN(pushBack), 128)
+BENCHMARK_PARAM(BENCHFUN(pushBack), 1024)
+BENCHMARK_PARAM(BENCHFUN(pushBack), 10240)
+BENCHMARK_PARAM(BENCHFUN(pushBack), 102400)
+BENCHMARK_PARAM(BENCHFUN(pushBack), 1024000)
+
+    
+    BENCHMARK_RELATIVE(sformat_long_string_unsafe, iters) {
+  BenchmarkSuspender suspender;
+  auto const& longString = getLongString();
+  while (iters--) {
+    std::string out;
+    suspender.dismissing([&] { out = sformat(longString); });
   }
 }
-#endif
     
-    template <
-    typename T,
-    template <typename> class Atom = std::atomic,
-    typename CountedDetail = detail::shared_ptr_internals>
-class atomic_shared_ptr {
-  using SharedPtr = typename CountedDetail::template CountedPtr<T>;
-  using BasePtr = typename CountedDetail::counted_base;
-  using PackedPtr = folly::PackedSyncPtr<BasePtr>;
+      for (unsigned i = 0; i < iter; ++i) {
+    FB_LOG_EVERY_MS(INFO, -1) << 'every -1ms';
+  }
+    
+    template <class UIntType, UIntType a, UIntType c, UIntType m>
+struct StateSize<std::linear_congruential_engine<UIntType, a, c, m>> {
+  // From the standard [rand.eng.lcong], this is ceil(log2(m) / 32) + 3,
+  // which is the same as ceil(ceil(log2(m) / 32) + 3, and
+  // ceil(log2(m)) <= std::numeric_limits<UIntType>::digits
+  using type = std::integral_constant<
+      size_t,
+      (std::numeric_limits<UIntType>::digits + 31) / 32 + 3>;
+};
+    
+    /*
+ * Decode a single unicode code point from UTF-8 byte sequence.
+ */
+char32_t utf8ToCodePoint(
+    const unsigned char*& p,
+    const unsigned char* const e,
+    bool skipOnError);
+    
+      // Try converting fractional hours
+  tv = to<struct timeval>(duration<double, std::ratio<3600>>{3.456789});
+  EXPECT_EQ(12444, tv.tv_sec);
+  // The usec field is generally off-by-one due to
+  // floating point rounding error
+  EXPECT_NEAR(440400, tv.tv_usec, 1);
+  tv = to<struct timeval>(duration<double, std::ratio<3600>>{-3.456789});
+  EXPECT_EQ(-12445, tv.tv_sec);
+  EXPECT_NEAR(559600, tv.tv_usec, 1);
+    
+    
+    {
+    {
+    {
+    {} // namespace detail
+} // namespace compression
+} // namespace io
+} // namespace folly
+
+    
+    template <class T, size_t kNumSlots = 64>
+class CoreCachedWeakPtr {
+ public:
+  explicit CoreCachedWeakPtr(const CoreCachedSharedPtr<T, kNumSlots>& p) {
+    for (auto slot : folly::enumerate(slots_)) {
+      *slot = p.slots_[slot.index];
     }
+  }
+    }
+    
+    #include <cstdint>
+#include <limits>
+#include <random>
+#include <set>
