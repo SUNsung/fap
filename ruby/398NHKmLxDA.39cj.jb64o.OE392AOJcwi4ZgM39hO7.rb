ca12348e16ab00a46efb1befc5de725b4ae48310
SUNsung <weatherius@gmail.com>
2019-01-17 09:40:01 +0200

    Rewrite [398NHKmLxDA.39cj.jb64o.OE392AOJcwi4ZgM39hO7.rb] File in: 2019-01-17 09:40:00

--- ruby/398NHKmLxDA.39cj.jb64o.OE392AOJcwi4ZgM39hO7.rb
+++ ruby/398NHKmLxDA.39cj.jb64o.OE392AOJcwi4ZgM39hO7.rb
@@ -1,43 +1,56 @@
 
         
-                it 'prevents converting negative argument to two's complement form' do
-          format('% b', -10).should == '-1010'
-          format('% B', -10).should == '-1010'
-          format('% o', -87).should == '-127'
-          format('% x', -196).should == '-c4'
-          format('% X', -196).should == '-C4'
-        end
+        Then /^I should see an image in the publisher$/ do
+  photo_in_publisher.should be_present
+end
     
-      it 'raises an ArgumentError when passed a negative duration' do
-    lambda { sleep(-0.1) }.should raise_error(ArgumentError)
-    lambda { sleep(-1) }.should raise_error(ArgumentError)
-  end
-    
-      it 'raises a TypeError when passed nil' do
-    lambda { srand(nil) }.should raise_error(TypeError)
-  end
-    
-    if defined?(Gem)
-  GEMS_AND_ROOT_DIRECTORIES = {
-    'sinatra' => '.',
-    'sinatra-contrib' => './sinatra-contrib',
-    'rack-protection' => './rack-protection'
-  }
-    
-          # This double assignment is to prevent an 'unused variable' warning on
-      # Ruby 1.9.3.  Yes, it is dumb, but I don't like Ruby yelling at me.
-      path = path = (req.script_name + req.path_info).squeeze('/')
-    
-          default_options :authenticity_param => 'authenticity_token',
-                      :allow_if => nil
-    
-          def call(env)
-        request               = Request.new(env)
-        status, headers, body = app.call(env)
-    
-        it 'Returns nil when Referer header is invalid' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
-      expect(subject.referrer(env)).to be_nil
+      def confirm_on_page(page_name)
+    if page_name == 'my profile page'
+      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
+    else
+      expect(page).to have_path(path_to(page_name))
     end
   end
 end
+    
+    describe ConversationsController, :type => :controller do
+  describe '#index' do
+    before do
+      @person = alice.contacts.first.person
+      hash = {
+        :author => @person,
+        :participant_ids => [alice.person.id, @person.id],
+        :subject => 'not spam',
+        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
+      }
+      @conv1 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+    
+          # Checks whether this node body is a void context.
+      #
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
+      end
+    
+          # Checks whether any of the key value pairs in the `hash` literal are on
+      # the same line.
+      #
+      # @note A multiline `pair` is considered to be on the same line if it
+      #       shares any of its lines with another `pair`
+      #
+      # @return [Boolean] whether any `pair` nodes are on the same line
+      def pairs_on_same_line?
+        pairs.each_cons(2).any? { |first, second| first.same_line?(second) }
+      end
+    
+          # This is used for duck typing with `pair` nodes which also appear as
+      # `hash` elements.
+      #
+      # @return [false]
+      def hash_rocket?
+        false
+      end
