008f8c181cde32e503ae4efeca0667d27dd8f28a
SUNsung <weatherius@gmail.com>
2019-01-18 17:41:05 +0200

    Rewrite [28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb] File in: 2019-01-18 17:41:05

--- ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
+++ ruby/28syv5hKvQhaQ28r4QfHs88NE.28igpS82Uxwtk28sTd.rb
@@ -1,145 +1,71 @@
 
         
-                  def hidden_field_for_checkbox(options)
-            @unchecked_value ? tag('input', options.slice('name', 'disabled', 'form').merge!('type' => 'hidden', 'value' => @unchecked_value)) : ''.html_safe
-          end
+            brew cask install mactex
+    EOS
+  when 'pip' then <<-EOS.undent
+    Homebrew provides pip via: `brew install python`. However you will then
+    have two Pythons installed on your Mac, so alternatively you can install
+    pip via the instructions at:
+    
+      # @private
+  def used_options
+    @options & @args
+  end
+    
+        dump_formula_report :A, 'New Formulae'
+    dump_formula_report :M, 'Updated Formulae'
+    dump_formula_report :R, 'Renamed Formulae'
+    dump_formula_report :D, 'Deleted Formulae'
+  end
+    
+              builds = scope_relevant.select('count(*)').to_sql
+          created = scope_relevant.created.select('count(*)').to_sql
+          success = scope_relevant.success.select('count(*)').to_sql
+          manual = scope_relevant.manual.select('count(*)').to_sql
+          pending = scope_relevant.pending.select('count(*)').to_sql
+          running = scope_relevant.running.select('count(*)').to_sql
+          skipped = scope_relevant.skipped.select('count(*)').to_sql
+          canceled = scope_relevant.canceled.select('count(*)').to_sql
+          warnings = scope_warnings.select('count(*) > 0').to_sql
+    
+          # Iterates over all of the objects for the given method (e.g. `:labels`).
+      #
+      # method - The method to send to Octokit for querying data.
+      # args - Any arguments to pass to the Octokit method.
+      def each_object(method, *args, &block)
+        return to_enum(__method__, method, *args) unless block_given?
+    
+            def id_for_already_imported_cache(note)
+          note.id
+        end
       end
     end
   end
 end
 
     
-                content ||= Translator
-              .new(object, @object_name, method_and_value, scope: 'helpers.label')
-              .translate
-            content ||= @method_name.humanize
-    
-        def find_all_with_query(query) # :nodoc:
-      paths.each do |resolver|
-        templates = resolver.find_all_with_query(query)
-        return templates unless templates.empty?
-      end
-    
-    describe Admin::UsersController do
-  it 'requires to be signed in as an admin' do
-    login_as(users(:bob))
-    visit admin_users_path
-    expect(page).to have_text('Admin access required to view that page.')
-  end
-    
-      describe '#status' do
-    it 'works for failed jobs' do
-      job.failed_at = Time.now
-      expect(status(job)).to eq('<span class='label label-danger'>failed</span>')
-    end
-    
-        it 'respects an environment variable that specifies a path or URL to a different scenario' do
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
-    end
-    
-            agents(:bob_weather_agent).update_attribute :guid, 'a-weather-agent'
-        agents(:bob_weather_agent).scenarios << _existing_scenerio
-    
-      it 'provides hash-style access to its properties with both symbol and string keys' do
-    expect(location[:lat]).to be_a Float
-    expect(location[:lat]).to eq 2.0
-    expect(location['lat']).to be_a Float
-    expect(location['lat']).to eq 2.0
-  end
-    
-        def log(msg)
-      puts '\r' + justify(msg)
-    end
-    
-        def subpath_from(url, options = nil)
-      self.class.parse(url).subpath_to(self, options)
-    end
-    
-              node.before(%(<div class='pre-title'>#{node['title']}</div>)) if node['title']
-    
-      def command
-    abort 'This command requires a command argument' if ARGV.empty?
-    
-    begin
-    
-      def prefixes
-    prefixes = ['/bin', '/usr/bin', '/usr/libexec', xcode_app_path]
-    prefixes << `brew --prefix`.strip unless `which brew`.strip.empty?
-    
-    namespace :doc do
-  task :readmes do
-    Dir.glob 'lib/rack/protection/*.rb' do |file|
-      excluded_files = %w[lib/rack/protection/base.rb lib/rack/protection/version.rb]
-      next if excluded_files.include?(file)
-      doc  = File.read(file)[/^  module Protection(\n)+(    #[^\n]*\n)*/m].scan(/^ *#(?!#) ?(.*)\n/).join('\n')
-      file = 'doc/#{file[4..-4].tr('/_', '-')}.rdoc'
-      Dir.mkdir 'doc' unless File.directory? 'doc'
-      puts 'writing #{file}'
-      File.open(file, 'w') { |f| f << doc }
-    end
-  end
-    
-          def accepts?(env)
-        session = session env
-        set_token(session)
-    
-          def origin(env)
-        env['HTTP_ORIGIN'] || env['HTTP_X_ORIGIN']
-      end
-    
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
+              unless matches
+            raise(
+              ArgumentError,
+              'The note URL #{note.html_url.inspect} is not supported'
+            )
           end
-        end
-        bad_cookies.empty?
+    
+            attr_reader :attributes
+    
+          def gateway_error(exception)
+        @order.errors.add(:base, exception.message)
+        invalid_resource!(@order)
       end
     
-        it 'Reads referrer from Host header when Referer header is missing' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      expect(subject.referrer(env)).to eq('foo.com')
-    end
+            def new; end
     
-        headers = get('/', {}, 'wants' => 'text/html').headers
-    expect(headers['Content-Security-Policy']).to eq('block-all_mixed_content; connect-src 'self'; default-src none; disown-opener; img-src 'self'; script-src 'self'; style-src 'self'; upgrade-insecure_requests')
-  end
+            def show
+          @state = scope.find(params[:id])
+          respond_with(@state)
+        end
     
-        # Initializes a new CategoryIndex.
-    #
-    #  +base+         is the String path to the <source>.
-    #  +category_dir+ is the String path between <source> and the category folder.
-    #  +category+     is the category currently being processed.
-    def initialize(site, base, category_dir, category)
-      @site = site
-      @base = base
-      @dir  = category_dir
-      @name = 'index.html'
-      self.process(@name)
-      # Read the YAML data from the layout page.
-      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
-      self.data['category']    = category
-      # Set the title for this page.
-      title_prefix             = site.config['category_title_prefix'] || 'Category: '
-      self.data['title']       = '#{title_prefix}#{category}'
-      # Set the meta-description for this page.
-      meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
-      self.data['description'] = '#{meta_description_prefix}#{category}'
-    end
-    
-    module Jekyll
-    
-      # Summary is used on the Archive pages to return the first block of content from a post.
-  def summary(input)
-    if input.index(/\n\n/)
-      input.split(/\n\n/)[0]
-    else
-      input
-    end
-  end
+            def index
+          @zones = Zone.accessible_by(current_ability, :read).order('name ASC').ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@zones)
+        end
