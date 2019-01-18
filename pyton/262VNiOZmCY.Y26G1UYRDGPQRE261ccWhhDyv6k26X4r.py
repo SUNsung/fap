d0c6ca6dc3494ea5bf1a049af09bebe458f6f22b
SUNsung <weatherius@gmail.com>
2019-01-19 00:58:07 +0200

    Rewrite [262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py] File in: 2019-01-19 00:58:07

--- pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
+++ pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
@@ -1,190 +1,30 @@
 
         
-            def approve_friend_request(self, from_user_id, to_user_id):
-        pass
+            def __init__(self, request):
+        exc = request.routing_exception
+        buf = ['A request was sent to this URL (%s) but a redirect was '
+               'issued automatically by the routing system to '%s'.'
+               % (request.url, exc.new_url)]
     
-        def __init__(self, seller_category_map, seller_category_overrides_map):
-        self.seller_category_map = seller_category_map
-        self.seller_category_overrides_map = seller_category_overrides_map
+            current = current.parent
     
-        def __init__(self):
-        self.head = None
-        self.tail = None
-    
-      # generate data for trials
-  data_e = []
-  u_e = []
-  outs_e = []
-  for c in range(C):
-    u_1xt, outs_1xt = get_data_batch(batch_size, ntimesteps, u_rng, FLAGS.u_std)
-    
-      sess, t = _LoadModel(FLAGS.pbtxt, FLAGS.ckpt)
-    
-              [gen_initial_state_eval, fake_gen_initial_state_eval, _] = sess.run(
-              [
-                  model.eval_final_state, model.fake_gen_final_state,
-                  model.global_step
-              ],
-              feed_dict=eval_feed)
-    
-      present = tf.cast(is_real_input, tf.float32)
-  missing = tf.cast(1 - present, tf.float32)
+        Implements the WSGI wrappers (request and response).
     
     
-def percent_correct(real_sequence, fake_sequences):
-  '''Determine the percent of tokens correctly generated within a batch.'''
-  identical = 0.
-  for fake_sequence in fake_sequences:
-    for real, fake in zip(real_sequence, fake_sequence):
-      if real == fake:
-        identical += 1.
-  return identical / recursive_length(fake_sequences)
-
+def test_app_tearing_down_with_handled_exception_by_app_handler(app, client):
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+    cleanup_stuff = []
     
-    '''Model optimization.'''
-    
-        dirname = 'cifar-100-python'
-    origin = 'https://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz'
-    path = get_file(dirname, origin=origin, untar=True)
+        module = AnsibleModule(argument_spec, supports_check_mode=True)
     
     
-def print_summary(model, line_length=None, positions=None, print_fn=None):
-    '''Prints a summary of a model.
-    
-        y = np.array(y, dtype='int')
-    input_shape = y.shape
-    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
-        input_shape = tuple(input_shape[:-1])
-    y = y.ravel()
-    if not num_classes:
-        num_classes = np.max(y) + 1
-    n = y.shape[0]
-    categorical = np.zeros((n, num_classes), dtype=dtype)
-    categorical[np.arange(n), y] = 1
-    output_shape = input_shape + (num_classes,)
-    categorical = np.reshape(categorical, output_shape)
-    return categorical
-    
-        def get_config(self):
-        config = {'filters': self.filters,
-                  'kernel_size': self.kernel_size,
-                  'strides': self.strides,
-                  'padding': self.padding,
-                  'data_format': self.data_format,
-                  'dilation_rate': self.dilation_rate,
-                  'activation': activations.serialize(self.activation),
-                  'recurrent_activation':
-                      activations.serialize(self.recurrent_activation),
-                  'use_bias': self.use_bias,
-                  'kernel_initializer':
-                      initializers.serialize(self.kernel_initializer),
-                  'recurrent_initializer':
-                      initializers.serialize(self.recurrent_initializer),
-                  'bias_initializer': initializers.serialize(self.bias_initializer),
-                  'unit_forget_bias': self.unit_forget_bias,
-                  'kernel_regularizer':
-                      regularizers.serialize(self.kernel_regularizer),
-                  'recurrent_regularizer':
-                      regularizers.serialize(self.recurrent_regularizer),
-                  'bias_regularizer': regularizers.serialize(self.bias_regularizer),
-                  'kernel_constraint':
-                      constraints.serialize(self.kernel_constraint),
-                  'recurrent_constraint':
-                      constraints.serialize(self.recurrent_constraint),
-                  'bias_constraint': constraints.serialize(self.bias_constraint),
-                  'dropout': self.dropout,
-                  'recurrent_dropout': self.recurrent_dropout}
-        base_config = super(ConvLSTM2DCell, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-    
-        def get_config(self):
-        config = {
-            'units': self.units,
-            'kernel_initializer': initializers.serialize(self.kernel_initializer),
-            'recurrent_initializer':
-                initializers.serialize(self.recurrent_initializer),
-            'bias_initializer': initializers.serialize(self.bias_initializer),
-            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
-            'recurrent_regularizer':
-                regularizers.serialize(self.recurrent_regularizer),
-            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
-            'activity_regularizer':
-                regularizers.serialize(self.activity_regularizer),
-            'kernel_constraint': constraints.serialize(self.kernel_constraint),
-            'recurrent_constraint':
-                constraints.serialize(self.recurrent_constraint),
-            'bias_constraint': constraints.serialize(self.bias_constraint)}
-        base_config = super(CuDNNGRU, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-    
-        if i >= 7:
-        ax.text(1, 3, 'Predictions !', fontsize=20, color='w')
-    else:
-        ax.text(1, 3, 'Initial trajectory', fontsize=20)
-    
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
-    
-                if not res or not res.ok:
-                xlog.debug('ip:%s fail', ip)
-                continue
-    
-    ## Anything on different channel than DEFAULT_CHANNEL is not parsed
-# by parser.
-HIDDEN_CHANNEL = 99
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
-    
-        # Make the eyebrows into a nightmare
-    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
-    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
-    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    
-        pool = context.Pool(processes=processes)
-    
-    while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-    
-    # Load a sample picture and learn how to recognize it.
-print('Loading known face image(s)')
-obama_image = face_recognition.load_image_file('obama_small.jpg')
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-    
-        def test_cnn_raw_face_locations(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
-        detected_faces = api._raw_face_locations(img, model='cnn')
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            path=dict(required=True),
+            state=dict(default='present', choices=['present', 'followed', 'absent', 'unfollowed']),
+            name=dict(required=False, default=None, type='str'),
+            logtype=dict(required=False, default=None, type='str', aliases=['type'])
+        ),
+        supports_check_mode=True
+    )
