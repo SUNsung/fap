20623d2702abfaa562cc9057bb39d7f7a26adae0
SUNsung <weatherius@gmail.com>
2019-01-19 21:11:56 +0200

    Rewrite [22E90vew5WJI.22gbEtlQ23Pm.22leOlSzLXpIg225Um.cpp] File in: 2019-01-19 21:11:55

--- cccccc/22E90vew5WJI.22gbEtlQ23Pm.22leOlSzLXpIg225Um.cpp
+++ cccccc/22E90vew5WJI.22gbEtlQ23Pm.22leOlSzLXpIg225Um.cpp
@@ -1,110 +1,335 @@
 
         
-        REGISTER_CPU_OPERATOR(GivenTensorFill, GivenTensorFillOp<float, CPUContext>);
-REGISTER_CPU_OPERATOR(
-    GivenTensorDoubleFill,
-    GivenTensorFillOp<double, CPUContext>);
-REGISTER_CPU_OPERATOR(GivenTensorBoolFill, GivenTensorFillOp<bool, CPUContext>);
-REGISTER_CPU_OPERATOR(GivenTensorIntFill, GivenTensorFillOp<int, CPUContext>);
-REGISTER_CPU_OPERATOR(
-    GivenTensorInt64Fill,
-    GivenTensorFillOp<int64_t, CPUContext>);
-REGISTER_CPU_OPERATOR(
-    GivenTensorStringFill,
-    GivenTensorFillOp<std::string, CPUContext>);
+        StringRef swift::platformString(PlatformKind platform) {
+  switch (platform) {
+  case PlatformKind::none:
+    return '*';
+#define AVAILABILITY_PLATFORM(X, PrettyName)                                   \
+  case PlatformKind::X:                                                        \
+    return #X;
+#include 'swift/AST/PlatformKinds.def'
+  }
+  llvm_unreachable('bad PlatformKind');
+}
     
-    #include <grpc/support/port_platform.h>
+    void SILLayout::Profile(llvm::FoldingSetNodeID &id,
+                        CanGenericSignature Generics,
+                        ArrayRef<SILField> Fields) {
+  id.AddPointer(Generics.getPointer());
+  for (auto &field : Fields) {
+    id.AddPointer(field.getLoweredType().getPointer());
+    id.AddBoolean(field.isMutable());
+  }
+}
+
     
-    #include 'src/cpp/ext/filters/census/rpc_encoding.h'
+    
+    {  return true;
+}
+
+    
+    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
+  DefaultCache &DCache = *static_cast<DefaultCache*>(Impl);
+  llvm::sys::ScopedLock L(DCache.Mux);
+    }
+    
+        StringRef Line = RawText.substr(0, Pos);
+    Lines.push_back(Line);
+    if (!IsFirstLine) {
+      size_t NonWhitespacePos = RawText.find_first_not_of(' ');
+      if (NonWhitespacePos != StringRef::npos)
+        WhitespaceToTrim =
+            std::min(WhitespaceToTrim,
+                     static_cast<unsigned>(NonWhitespacePos));
+    }
+    IsFirstLine = false;
+    
+    /// Maintain a set of known CF types.
+static bool isKnownCFTypeName(StringRef name) {
+  return std::binary_search(KnownCFTypes, KnownCFTypes + NumKnownCFTypes,
+                            name, SortByLengthComparator());
+}
+    
+        void emitCodeContext(clang::FullSourceLoc Loc,
+                         clang::DiagnosticsEngine::Level Level,
+                         SmallVectorImpl<clang::CharSourceRange>& Ranges,
+                         ArrayRef<clang::FixItHint> Hints) override {}
+    
+    )DOC')
+    .Input(0, 'X', '*(type: Tensor`<float>`)* Input tensor.')
+    .Output(0, 'Y', '*(type: Tensor`<float>`)* Output tensor.');
+    
+    workspace.ResetWorkspace()
+    
+    template <>
+void GluOp<float, CPUContext>::ComputeGlu(
+    const int M,
+    const int split_dim,
+    const int N,
+    const float* Xdata,
+    float* Ydata) {
+  const int xStride = 2 * split_dim * N;
+  const int yStride = split_dim * N;
+  for (int i = 0; i < M; ++i) {
+    const int idx = i * xStride;
+    const int idy = i * yStride;
+    for (int j = 0; j < split_dim; ++j) {
+      const int jN = j * N;
+      const int jdx1 = idx + jN;
+      const int jdx2 = idx + (j + split_dim) * N;
+      const int jdy = idy + jN;
+      for (int k = 0; k < N; ++k) {
+        const float x1 = Xdata[jdx1 + k];
+        const float x2 = Xdata[jdx2 + k];
+        Ydata[jdy + k] = x1 * sigmoid(x2);
+      }
+    }
+  }
+}
     
     
-    {  // TODO: For performance:
-  // uint8_t version;
-  // uint8_t trace_id_field_id;
-  uint8_t trace_id[::opencensus::trace::TraceId::kSize];
-  // uint8_t span_id_field_id;
-  uint8_t span_id[::opencensus::trace::SpanId::kSize];
-  // uint8_t trace_options_field_id;
-  uint8_t trace_options[::opencensus::trace::TraceOptions::kSize];
+    {          return out;
+        });
+OPERATOR_SCHEMA(Float16ConstantFill)
+    .NumInputs(0)
+    .NumOutputs(1)
+    .TensorInferenceFunction(Float16FillerTensorInference)
+    .Arg('value', 'The value for the elements of the output tensor.')
+    .Arg('shape', 'The shape of the output tensor.')
+    .Output(
+        0,
+        'output',
+        'Output tensor of constant values specified by 'value'');
+    
+    
+    {          return out;
+        })
+    .Input(0, 'X', '4-tensor in NCHW or NHWC.')
+    .Output(
+        0,
+        'Y',
+        '4-tensor. For NCHW: N x (C x kH x kW) x outH x outW.'
+        'For NHWC: N x outH x outW x (kH x kW x C');
+    
+    #endif  // DMLC_ENABLE_STD_THREAD
+
+    
+    namespace xgboost {
+namespace data {
+/*!
+ * \brief Format specification of SparsePage.
+ */
+class SparsePageFormat {
+ public:
+  /*! \brief virtual destructor */
+  virtual ~SparsePageFormat() = default;
+  /*!
+   * \brief Load all the segments into page, advance fi to end of the block.
+   * \param page The data to read page into.
+   * \param fi the input stream of the file
+   * \return true of the loading as successful, false if end of file was reached
+   */
+  virtual bool Read(SparsePage* page, dmlc::SeekStream* fi) = 0;
+  /*!
+   * \brief read only the segments we are interested in, advance fi to end of the block.
+   * \param page The page to load the data into.
+   * \param fi the input stream of the file
+   * \param sorted_index_set sorted index of segments we are interested in
+   * \return true of the loading as successful, false if end of file was reached
+   */
+  virtual bool Read(SparsePage* page,
+                    dmlc::SeekStream* fi,
+                    const std::vector<bst_uint>& sorted_index_set) = 0;
+  /*!
+   * \brief save the data to fo, when a page was written.
+   * \param fo output stream
+   */
+  virtual void Write(const SparsePage& page, dmlc::Stream* fo) = 0;
+  /*!
+   * \brief Create sparse page of format.
+   * \return The created format functors.
+   */
+  static SparsePageFormat* Create(const std::string& name);
+  /*!
+   * \brief decide the format from cache prefix.
+   * \return pair of row format, column format type of the cache prefix.
+   */
+  static std::pair<std::string, std::string> DecideFormat(const std::string& cache_prefix);
+};
+    }
+    }
+    
+      void InitTreesToUpdate() {
+    if (trees_to_update.size() == 0u) {
+      for (auto & tree : trees) {
+        trees_to_update.push_back(std::move(tree));
+      }
+      trees.clear();
+      param.num_trees = 0;
+      tree_info.clear();
+    }
+  }
+    
+    // logistic loss for binary classification task
+struct LogisticClassification : public LogisticRegression {
+  static const char* DefaultEvalMetric() { return 'error'; }
 };
     
-    #ifndef GRPC_CUSTOM_DEFAULT_THREAD_POOL
+    namespace xgboost {
+namespace common {
+TEST(CompressedIterator, Test) {
+  ASSERT_TRUE(detail::SymbolBits(256) == 8);
+  ASSERT_TRUE(detail::SymbolBits(150) == 8);
+  std::vector<int> test_cases = {1, 3, 426, 21, 64, 256, 100000, INT32_MAX};
+  int num_elements = 1000;
+  int repetitions = 1000;
+  srand(9);
+    }
+    }
+    }
     
-    #include 'src/core/lib/gprpp/thd.h'
-#include 'src/cpp/server/thread_pool_interface.h'
     
-    #include <utility>
-    
-    class MockMessageManager
-    : public MessageManager<::apollo::canbus::ChassisDetail> {
- public:
-  MockMessageManager() {
-    AddRecvProtocolData<MockProtocolData, true>();
-    AddSendProtocolData<MockProtocolData, true>();
+    {
+    {    // Eject all bytes
+    int tmp_bytes =
+        static_cast<int>(std::ceil(static_cast<float>(stored_bits) / 8));
+    for (int j = 0; j < tmp_bytes; j++) {
+      int shift_bits = static_cast<int>(stored_bits) - (j + 1) * 8;
+      if (shift_bits >= 0) {
+        buffer[buffer_position] =
+            static_cast<CompressedByteT>(tmp >> shift_bits);
+      } else {
+        buffer[buffer_position] =
+            static_cast<CompressedByteT>(tmp << std::abs(shift_bits));
+      }
+      buffer_position++;
+    }
   }
 };
     
-    #include 'modules/common/time/time.h'
-#include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-#include 'modules/drivers/radar/conti_radar/protocol/const_vars.h'
+        gmat.GetFeatureCounts(&feature_counts_[0]);
+    // classify features
+    for (bst_uint fid = 0; fid < nfeature; ++fid) {
+      if (static_cast<double>(feature_counts_[fid])
+                 < sparse_threshold * nrow) {
+        type_[fid] = kSparseColumn;
+      } else {
+        type_[fid] = kDenseColumn;
+      }
+    }
+    
+      /*!
+   * \brief generate predictions for given feature matrix
+   * \param dmat feature matrix
+   * \param out_preds output vector to hold the predictions
+   * \param ntree_limit limit the number of trees used in prediction, when it equals 0, this means
+   *    we do not limit number of trees, this parameter is only valid for gbtree, but not for gblinear
+   */
+  virtual void PredictBatch(DMatrix* dmat,
+                            HostDeviceVector<bst_float>* out_preds,
+                            unsigned ntree_limit = 0) = 0;
+  /*!
+   * \brief online prediction function, predict score for one instance at a time
+   *  NOTE: use the batch prediction interface if possible, batch prediction is usually
+   *        more efficient than online prediction
+   *        This function is NOT threadsafe, make sure you only call from one thread
+   *
+   * \param inst the instance you want to predict
+   * \param out_preds output vector to hold the predictions
+   * \param ntree_limit limit the number of trees used in prediction
+   * \param root_index the root index
+   * \sa Predict
+   */
+  virtual void PredictInstance(const SparsePage::Inst& inst,
+                       std::vector<bst_float>* out_preds,
+                       unsigned ntree_limit = 0,
+                       unsigned root_index = 0) = 0;
+  /*!
+   * \brief predict the leaf index of each tree, the output will be nsample * ntree vector
+   *        this is only valid in gbtree predictor
+   * \param dmat feature matrix
+   * \param out_preds output vector to hold the predictions
+   * \param ntree_limit limit the number of trees used in prediction, when it equals 0, this means
+   *    we do not limit number of trees, this parameter is only valid for gbtree, but not for gblinear
+   */
+  virtual void PredictLeaf(DMatrix* dmat,
+                           std::vector<bst_float>* out_preds,
+                           unsigned ntree_limit = 0) = 0;
+    
+      void setNodes(const std::vector<std::shared_ptr<DHTNode>>& nodes);
+    
+        receiver->setMessageFactory(factory.get());
+    receiver->setRoutingTable(routingTable.get());
+    
+    
+    {  virtual void addImmediateTask(const std::shared_ptr<DHTTask>& task) = 0;
+};
+    
+      DHTTaskExecutor periodicTaskQueue2_;
+    
+    #endif // D_DHT_UNKNOWN_MESSAGE_H
+
+    
+    TEST(MessageManagerTest, GetMutableProtocolDataById) {
+  uint8_t mock_data = 1;
+  MockMessageManager manager;
+  manager.Parse(MockProtocolData::ID, &mock_data, 8);
+  manager.ResetSendMessages();
+  EXPECT_TRUE(manager.GetMutableProtocolDataById(MockProtocolData::ID) !=
+              nullptr);
+    }
+    
+    
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+      Byte t1(bytes + 2);
+  int32_t t = t1.get_byte(6, 2);
+    
+    
+    {  double ret = x * OBJECT_LENGTH_RES;
+  return ret;
+}
     
     
     {  int ret = x;
   return ret;
 }
     
-    /**
- * @file st_point.cpp
- **/
     
-    class SpeedLimitTest : public ::testing::Test {
- public:
-  virtual void SetUp() {
-    speed_limit_.Clear();
-    for (int i = 0; i < 100; ++i) {
-      std::pair<double, double> sp;
-      sp.first = i * 1.0;
-      sp.second = (i % 2 == 0) ? 5.0 : 10.0;
-      speed_limit_.AppendSpeedLimit(sp.first, sp.second);
-    }
-  }
+    {  int ret = x;
+  return ret;
+}
+    
+    RcsThreshold RadarState201::rcs_threshold(const std::uint8_t* bytes,
+                                          int32_t length) const {
+  Byte t0(bytes + 7);
+  uint32_t x = t0.get_byte(2, 3);
     }
     
-    void BackupTrajectoryGenerator::GenerateTrajectory1dPairs(
-    const State& init_s, const State& init_d) {
-  std::vector<std::shared_ptr<Curve1d>> lon_trajectories;
-  std::array<double, 5> dds_condidates = {-0.1, -1.0, -2.0, -3.0, -4.0};
-  for (const auto dds : dds_condidates) {
-    lon_trajectories.emplace_back(
-        new ConstantDecelerationTrajectory1d(init_s[0], init_s[1], dds));
-  }
+    BaseMapMatrix::~BaseMapMatrix() {}
+    
+      const auto mat = kernel.kernel_matrix() / (2.0 * 1.0 / std::pow(0.1, 6));
+  const auto offset = kernel.offset_matrix();
+    
+        static BOOST_FORCEINLINE bool compare_exchange_strong(
+        storage_type volatile& storage, storage_type& expected, storage_type desired, memory_order, memory_order) BOOST_NOEXCEPT
+    {
+        storage_type previous = expected;
+        storage_type old_val = static_cast< storage_type >(BOOST_ATOMIC_INTERLOCKED_COMPARE_EXCHANGE(&storage, desired, previous));
+        expected = old_val;
+        return (previous == old_val);
     }
     
-      MatrixXd mat_golden(20, 10);
-  // clang-format off
-  mat_golden <<
-   -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0, -1,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  1,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0, -1,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  1,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0, -1,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  1,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0, -1,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  1,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0, -1,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  1,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0, -1,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  1,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0, -1,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  1,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0, -1,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  1,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0, -1,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-      kernel.AddReferenceLineKernelMatrix(index_list, pos_list, 10.0);
+    BOOST_FORCEINLINE void pause() BOOST_NOEXCEPT
+{
+#if defined(_MSC_VER) && (defined(_M_AMD64) || defined(_M_IX86))
+    _mm_pause();
+#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
+    __asm__ __volatile__('pause;');
+#endif
+}
