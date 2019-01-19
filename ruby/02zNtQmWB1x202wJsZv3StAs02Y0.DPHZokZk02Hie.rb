55ab926bbbe4c6861b1e338756e949526ee0ed3c
SUNsung <weatherius@gmail.com>
2019-01-19 22:06:51 +0200

    Rewrite [02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb] File in: 2019-01-19 22:06:51

--- ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
+++ ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
@@ -1,10 +1,38 @@
 
         
-            expect(gu.notification_level).to eq(NotificationLevels.all[:tracking])
+            def name=(value)
+      @name = value.try :strip
+    end
     
-          def actual_path
-        $PROGRAM_NAME
+        module MultipleBaseUrls
+      def self.included(base)
+        base.extend ClassMethods
+      end
+    
+            css('.file').each do |node|
+          node.content = node.content.strip
+        end
+    
+      def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+  end
+    
+    module Jekyll
+    
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
       end
     end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
   end
+  output += '></#{tag}>'
 end
