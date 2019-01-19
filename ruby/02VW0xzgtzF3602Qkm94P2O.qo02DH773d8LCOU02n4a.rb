3945bd102897682875d70e5e86730d47948125f3
SUNsung <weatherius@gmail.com>
2019-01-19 05:27:36 +0200

    Rewrite [02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb] File in: 2019-01-19 05:27:35

--- ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
+++ ruby/02VW0xzgtzF3602Qkm94P2O.qo02DH773d8LCOU02n4a.rb
@@ -1,108 +1,200 @@
 
         
-            You can install it using Cask:
+                stdlibs = detect_stdlibs(ENV.compiler)
+        Tab.create(formula, ENV.compiler, stdlibs.first, formula.build, formula.source_modified_time).write
     
-      def effective_build_options_for(dependent)
-    args  = dependent.build.used_options
-    args |= Tab.for_formula(dependent).used_options
-    BuildOptions.new(args, dependent.options)
-  end
-    
-      private
-    
-      def query_regexp(query)
-    case query
-    when %r{^/(.*)/$} then Regexp.new($1)
-    else /.*#{Regexp.escape(query)}.*/i
-    end
-  rescue RegexpError
-    odie '#{query} is not a valid regex'
-  end
-    
-        def add(entry)
-      if entry.is_a? Array
-        entry.each(&method(:add))
+        root.children.sort.each do |pn|
+      if pn.directory?
+        dirs << pn
+      elsif block_given? && yield(pn)
+        puts pn
+        other = 'other '
       else
-        add_entry(entry) unless entry.root?
+        remaining_root_files << pn unless pn.basename.to_s == '.DS_Store'
       end
     end
     
-          str.truncate(max_length).ljust(max_length) << tag.to_s
-    end
-    
-        def relative_path_from(url)
-      self.class.parse(url).relative_path_to(self)
-    end
-  end
-end
-
-    
-            css('img[style]').each do |node|
-          node['align'] ||= node['style'][/float:\s*(left|right)/, 1]
-          node['style'] = node['style'].split(';').map(&:strip).select { |s| s =~ /\Awidth|height/ }.join(';')
-        end
-    
-            if h1 = at_css('h1')
-          h1.prepend_child(css('.view-source', '.improve-docs'))
-        end
-    
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
-    
       before_action :set_account
-  before_action :set_size
   before_action :set_statuses
     
-        def set_email_domain_block
-      @email_domain_block = EmailDomainBlock.find(params[:id])
-    end
+        def create
+      authorize :custom_emoji, :create?
     
-        weeks
-  end
+          @form         = Form::StatusBatch.new(form_status_batch_params.merge(current_account: current_account, action: action_from_button))
+      flash[:alert] = I18n.t('admin.statuses.failed_to_execute') unless @form.save
     
-      it 'raises a TypeError when passed a String' do
-    lambda { sleep('2')   }.should raise_error(TypeError)
-  end
+    class Api::PushController < Api::BaseController
+  include SignatureVerification
     
-      ruby_version_is '2.6' do
-    it 'raises RuntimeError when `exception: true` is given and the command exits with a non-zero exit status' do
-      lambda { @object.system(ruby_cmd('exit 1'), exception: true) }.should raise_error(RuntimeError)
-    end
+    class Api::SubscriptionsController < Api::BaseController
+  before_action :set_account
+  respond_to :txt
     
-    describe 'Kernel#trace_var' do
-  before :each do
-    $Kernel_trace_var_global = nil
-  end
+        12.times do |i|
+      day     = i.weeks.ago.to_date
+      week_id = day.cweek
+      week    = Date.commercial(day.cwyear, week_id)
     
-      it 'returns the method name as symbol' do
-    eval(@code, TOPLEVEL_BINDING).should equal :boom
-  end
+    describe 'Kernel.test' do
+  it 'needs to be reviewed for spec completeness'
 end
 
     
-        def lotus?
-      defined?(::Lotus)
+        lambda {
+      catch :blah do
+        throw :blah, :return_value, 2, 3, 4, 5
+      end
+    }.should raise_error(ArgumentError)
+  end
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
+  end
+    
+      if system('which bundle')
+    puts 'Installing gems'
+    if args[:use_bundle_dir?]
+      execute_command 'env bundle install --path ./travis_bundle_dir'
+    else
+      execute_command 'env bundle install'
+    end
+  else
+    $stderr.puts '\033[0;31m' \
+      '[!] Please install the bundler gem manually:\n' \
+      '    $ [sudo] gem install bundler' \
+      '\e[0m'
+    exit 1
+  end
+end
+    
+    module Pod
+  class Command
+    class Env < Command
+      self.summary = 'Display pod environment'
+      self.description = 'Display pod environment.'
+    
+    Then(/^the current directory will be a symlink to the release$/) do
+  run_vagrant_command(exists?('e', TestApp.current_path))
+end
+    
+    Given(/^(\d+) valid existing releases$/) do |num|
+  a_day = 86_400 # in seconds
+  offset = -(a_day * num.to_i)
+  num.to_i.times do
+    run_vagrant_command('mkdir -p #{TestApp.release_path(TestApp.timestamp(offset))}')
+    offset += a_day
+  end
+end
+    
+        (stdout + stderr).each_line { |line| puts '[vagrant] #{line}' }
+    
+        def exit_because_of_exception(ex)
+      if respond_to?(:deploying?) && deploying?
+        exit_deploy_because_of_exception(ex)
+      else
+        super
+      end
     end
     
-    require_relative 'converter/fonts_conversion'
-require_relative 'converter/less_conversion'
-require_relative 'converter/js_conversion'
-require_relative 'converter/logger'
-require_relative 'converter/network'
+          def using_default_scm?
+        return @using_default_scm if defined? @using_default_scm
+        @using_default_scm = (fetch(:scm) == DEFAULT_GIT)
+      end
     
-        private
+          def role_properties_for(rolenames)
+        roles = rolenames.to_set
+        rps = Set.new unless block_given?
+        roles_for(rolenames).each do |host|
+          host.roles.intersection(roles).each do |role|
+            [host.properties.fetch(role)].flatten(1).each do |props|
+              if block_given?
+                yield host, role, props
+              else
+                rps << (props || {}).merge(role: role, hostname: host.hostname)
+              end
+            end
+          end
+        end
+        block_given? ? nil : rps
+      end
     
-      def framework_version
-    @framework_version ||= `rails -v`[/^Rails (.+)$/, 1]
+          def trusted?
+        @trusted
+      end
+    
+        desc 'Build all packages'
+    task :all => GEMS_AND_ROOT_DIRECTORIES.keys
   end
     
-      def generate_migration
-    migration_template('paperclip_migration.rb.erb',
-                       'db/migrate/#{migration_file_name}',
-                       migration_version: migration_version)
-  end
+      # escape unicode
+  content.gsub!(/./) { |c| c.bytesize > 1 ? '\\u{#{c.codepoints.first.to_s(16)}}' : c }
+    
+      # for now
+  worker_count = 1
+    
+          def create_worker_spec
+        template_file = File.join(
+            'spec/workers',
+            class_path,
+            '#{file_name}_worker_spec.rb'
+        )
+        template 'worker_spec.rb.erb', template_file
+      end
+    
+        ##
+    # Define client-side middleware:
+    #
+    #   client = Sidekiq::Client.new
+    #   client.middleware do |chain|
+    #     chain.use MyClientMiddleware
+    #   end
+    #   client.push('class' => 'SomeWorker', 'args' => [1,2,3])
+    #
+    # All client instances default to the globally-defined
+    # Sidekiq.client_middleware but you can change as necessary.
+    #
+    def middleware(&block)
+      @chain ||= Sidekiq.client_middleware
+      if block_given?
+        @chain = @chain.dup
+        yield @chain
+      end
+      @chain
+    end
+    
+        module PsychAutoload
+      def resolve_class(klass_name)
+        return nil if !klass_name || klass_name.empty?
+        # constantize
+        names = klass_name.split('::')
+        names.shift if names.empty? || names.first.empty?
+    
+        module ActionMailer
+      def sidekiq_delay(options={})
+        Proxy.new(DelayedMailer, self, options)
+      end
+      def sidekiq_delay_for(interval, options={})
+        Proxy.new(DelayedMailer, self, options.merge('at' => Time.now.to_f + interval.to_f))
+      end
+      def sidekiq_delay_until(timestamp, options={})
+        Proxy.new(DelayedMailer, self, options.merge('at' => timestamp.to_f))
+      end
+      alias_method :delay, :sidekiq_delay
+      alias_method :delay_for, :sidekiq_delay_for
+      alias_method :delay_until, :sidekiq_delay_until
+    end
+    
+    
+    # By leaving this as a class method, it can be pluggable and used by the Manager actor. Making it
+    # an instance method will make it async to the Fetcher actor
+    def self.bulk_requeue(inprogress, options)
+      return if inprogress.empty?
+    
+          ObjectSpace.each_object(File) do |fp|
+        begin
+          if !fp.closed? && fp.stat.file? && fp.sync && (fp.fcntl(Fcntl::F_GETFL) & append_flags) == append_flags
+            to_reopen << fp
+          end
+        rescue IOError, Errno::EBADF
+        end
+      end
