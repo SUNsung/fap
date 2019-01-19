b3d752a9411cd11a5ed203ece28f0a58b2f8f121
SUNsung <weatherius@gmail.com>
2019-01-19 06:40:32 +0200

    Rewrite [17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py] File in: 2019-01-19 06:40:32

--- pyton/17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py
+++ pyton/17b18mdUNOHDM17M3uRaloo0s17XJocTHCqhO217pIx.py
@@ -1,220 +1,119 @@
 
         
-              # Second, take the sum to get a tensor of shape [batch_size, output_dim].
-      self.pair_path_embeddings = tf.reduce_sum(self.weighted, 1)
+        
+# Import youtube_dl
+ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, ROOT_DIR)
+import youtube_dl
     
-    # save down the network outputs (may be useful later)
-train_outputs_u, valid_outputs_u = split_list_by_inds(outs_e,
-                                                      train_inds,
-                                                      valid_inds)
-train_outputs_u = np.array(train_outputs_u)
-valid_outputs_u = np.array(valid_outputs_u)
+        return ret
     
-    # pick which RNN is used on each trial
-rnn_to_use = rng.randint(2, size=E)
-ext_input = np.repeat(np.expand_dims(rnn_to_use, axis=1), ntimesteps, axis=1)
-ext_input = np.expand_dims(ext_input, axis=2)  # these are 'a's' in the paper
-    
-      Returns:
-    The length B list of TxN numpy tensors.
-  '''
-  T = len(values_t_bxn)
-  B, N = values_t_bxn[0].shape
-  values_b_txn = []
-  for b in range(B):
-    values_pb_txn = np.zeros([T,N])
-    for t in range(T):
-      values_pb_txn[t,:] = values_t_bxn[t][b,:]
-    values_b_txn.append(values_pb_txn)
-    
-      @property
-  def word_char_ids(self):
-    return self._word_char_ids
-    
-      decoder_embedding = [
-      v for v in tf.trainable_variables()
-      if v.op.name == 'gen/decoder/rnn/embedding'
-  ][0]
-  decoder_lstm_w_0 = [
-      v for v in tf.trainable_variables() if v.op.name ==
-      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel'
-  ][0]
-  decoder_lstm_b_0 = [
-      v for v in tf.trainable_variables() if v.op.name ==
-      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias'
-  ][0]
-  decoder_lstm_w_1 = [
-      v for v in tf.trainable_variables() if v.op.name ==
-      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel'
-  ][0]
-  decoder_lstm_b_1 = [
-      v for v in tf.trainable_variables() if v.op.name ==
-      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias'
-  ][0]
-  decoder_softmax_b = [
-      v for v in tf.trainable_variables()
-      if v.op.name == 'gen/decoder/rnn/softmax_b'
-  ][0]
+    from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import boto3_conn, camel_dict_to_snake_dict
+from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info
     
     
-def attention_decoder_fn_train(encoder_state,
-                               attention_keys,
-                               attention_values,
-                               attention_score_fn,
-                               attention_construct_fn,
-                               name=None):
-  '''Attentional decoder function for `dynamic_rnn_decoder` during training.
-    
-        if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.update(test_config)
-    
-        def __init__(self, name, import_name, static_folder=None,
-                 static_url_path=None, template_folder=None,
-                 url_prefix=None, subdomain=None, url_defaults=None,
-                 root_path=None):
-        _PackageBoundObject.__init__(self, import_name, template_folder,
-                                     root_path=root_path)
-        self.name = name
-        self.url_prefix = url_prefix
-        self.subdomain = subdomain
-        self.static_folder = static_folder
-        self.static_url_path = static_url_path
-        self.deferred_functions = []
-        if url_defaults is None:
-            url_defaults = {}
-        self.url_values_defaults = url_defaults
+def get_spanner_configuration_name(config_name, project_name):
+    config_name = 'projects/%s/instanceConfigs/regional-%s' % (project_name,
+                                                               config_name)
+    return config_name
     
     
-def explain_template_loading_attempts(app, template, attempts):
-    '''This should help developers understand what failed'''
-    info = ['Locating template '%s':' % template]
-    total_found = 0
-    blueprint = None
-    reqctx = _request_ctx_stack.top
-    if reqctx is not None and reqctx.request.blueprint is not None:
-        blueprint = reqctx.request.blueprint
-    
-        if not has_level_handler(logger):
-        logger.addHandler(default_handler)
-    
-    from .globals import _request_ctx_stack, _app_ctx_stack
-from .signals import template_rendered, before_render_template
-    
-            if 'methods' not in d:
-            methods = set()
-    
-    min_primitive_root = 3
-    
-    if __name__ == '__main__':
-    num = generateLargePrime()
-    print(('Prime number:', num))
-    print(('isPrime:', isPrime(num)))
-
-    
-            next_prime_gt = next_prime(value % self.size_table) \
-            if not check_prime(value % self.size_table) else value % self.size_table  #gt = bigger than
-        return next_prime_gt - (data % next_prime_gt)
-    
-    The problem is :
-Given two strings A and B. Find the minimum number of operations to string B such that A = B. The permitted operations are removal,  insertion, and substitution.
-'''
-from __future__ import print_function
-    
-    if __name__ == '__main__':
-	import sys
-    
-    The problem is  :
-Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
-Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
-'''
-from __future__ import print_function
-    
-    def revise_centroids(data, k, cluster_assignment):
-    new_centroids = []
-    for i in range(k):
-        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
-        member_data_points = data[cluster_assignment==i]
-        # Compute the mean of the data points. Fill in the blank (RHS only)
-        centroid = member_data_points.mean(axis=0)
-        new_centroids.append(centroid)
-    new_centroids = np.array(new_centroids)
-    
-    return new_centroids
-    
-    word_bysig = collections.defaultdict(list)
-for word in word_list:
-    word_bysig[signature(word)].append(word)
-    
-        return res
-    
-            a += a
-        b >>= 1
-    
-        def render(self):
-        for graphic in self.graphics:
-            graphic.render()
-    
-    ### OUTPUT ###
-# Counting to two...
-# one two
-# Counting to five...
-# one two three four five
-
-    
-    import random
-import time
-    
-        def rollback(self):
-        for a_state in self.states:
-            a_state()
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            auth_token=dict(
+                type='str',
+                default=os.environ.get('ONEANDONE_AUTH_TOKEN')),
+            api_url=dict(
+                type='str',
+                default=os.environ.get('ONEANDONE_API_URL')),
+            name=dict(type='str'),
+            firewall_policy=dict(type='str'),
+            description=dict(type='str'),
+            rules=dict(type='list', default=[]),
+            add_server_ips=dict(type='list', default=[]),
+            remove_server_ips=dict(type='list', default=[]),
+            add_rules=dict(type='list', default=[]),
+            remove_rules=dict(type='list', default=[]),
+            wait=dict(type='bool', default=True),
+            wait_timeout=dict(type='int', default=600),
+            wait_interval=dict(type='int', default=5),
+            state=dict(type='str', default='present', choices=['present', 'absent', 'update']),
+        ),
+        supports_check_mode=True
+    )
     
     
-class DecimalViewer:
-    def update(self, subject):
-        print(u'DecimalViewer: Subject %s has data %d' % (subject.name, subject.data))
+def get_configuration_facts(cursor, parameter_name=''):
+    facts = {}
+    cursor.execute('''
+        select c.parameter_name, c.current_value, c.default_value
+        from configuration_parameters c
+        where c.node_name = 'ALL'
+        and (? = '' or c.parameter_name ilike ?)
+    ''', parameter_name, parameter_name)
+    while True:
+        rows = cursor.fetchmany(100)
+        if not rows:
+            break
+        for row in rows:
+            facts[row.parameter_name.lower()] = {
+                'parameter_name': row.parameter_name,
+                'current_value': row.current_value,
+                'default_value': row.default_value}
+    return facts
     
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-'''
+    EXAMPLES = '''
+- name: creating a new vertica role
+  vertica_role: name=role_name db=db_name state=present
+    
+    # Ensure role with certain details
+- ipa_role:
+    name: another-role
+    description: Just another role
+    group:
+    - editors
+    host:
+    - host01.example.com
+    hostgroup:
+    - hostgroup01
+    privilege:
+    - Group Administrators
+    - User Administrators
+    service:
+    - service01
+    
+    EXAMPLES = '''
+# Create groups based on the machine architecture
+- group_by:
+    key: machine_{{ ansible_machine }}
+    
+        module = AnsibleModule(
+        argument_spec=dict(
+            token=dict(required=True, no_log=True),
+            environment=dict(required=True),
+            user=dict(required=False),
+            repo=dict(required=False),
+            revision=dict(required=False),
+            url=dict(required=False, default='https://api.honeybadger.io/v1/deploys'),
+            validate_certs=dict(default='yes', type='bool'),
+        ),
+        supports_check_mode=True
+    )
+    
+        def parse_current(parts):
+        if len(parts) > 2 and parts[2].lower() == 'process' and parts[0] == name:
+            return ''.join(parts[1]).lower()
+        else:
+            return ''
     
     
-class AbstractExpert(object):
+def cleanup_files(files):
+    for file in files:
+        os.remove(file)
     
-    # Repeat the entire benchmark this many times (on different ports)
-# This gives JITs time to warm up, etc.  Pypy needs 3-5 runs at
-# --n=15000 for its JIT to reach full effectiveness
-define('num_runs', type=int, default=1)
-    
-        * ``permitted_protocols``: List (or set) of protocols which should be
-      linkified, e.g. ``linkify(text, permitted_protocols=['http', 'ftp',
-      'mailto'])``. It is very unsafe to include protocols such as
-      ``javascript``.
-    '''
-    if extra_params and not callable(extra_params):
-        extra_params = ' ' + extra_params.strip()
-    
-                if __name__ == '__main__':
-                IOLoop.current().run_sync(main)
-    
-        # Supported date/time formats in our options
-    _DATETIME_FORMATS = [
-        '%a %b %d %H:%M:%S %Y',
-        '%Y-%m-%d %H:%M:%S',
-        '%Y-%m-%d %H:%M',
-        '%Y-%m-%dT%H:%M',
-        '%Y%m%d %H:%M:%S',
-        '%Y%m%d %H:%M',
-        '%Y-%m-%d',
-        '%Y%m%d',
-        '%H:%M:%S',
-        '%H:%M',
-    ]
-    
-            def assign_thread_identity() -> None:
-            self._thread_identity = get_ident()
+        url = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'<meta property='og:video:secure_url' content='(.*?)'>', html)
+    ]))
