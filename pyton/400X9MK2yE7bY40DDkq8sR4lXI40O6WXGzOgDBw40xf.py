1b8f0eb7747ec0f9388504c9cdd1717c006f9015
SUNsung <weatherius@gmail.com>
2019-01-19 10:59:28 +0200

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2019-01-19 10:59:27

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,137 +1,206 @@
 
         
-            def _init_github_account(self):
-        try:
-            info = netrc.netrc().authenticators(self._NETRC_MACHINE)
-            if info is not None:
-                self._username = info[0]
-                self._password = info[2]
-                compat_print('Using GitHub credentials found in .netrc...')
-                return
-            else:
-                compat_print('No GitHub credentials found in .netrc')
-        except (IOError, netrc.NetrcParseError):
-            compat_print('Unable to parse .netrc')
-        self._username = compat_input(
-            'Type your GitHub username or email address and press [Return]: ')
-        self._password = compat_getpass(
-            'Type your GitHub password and press [Return]: ')
+          def __init__(self, zs, prior_zs):
+    '''Create a lower bound in three parts, normalized reconstruction
+    cost, normalized KL divergence cost, and their sum.
+    
+          if self.hps.co_dim > 0:
+        controller_outputs[es_idx,:,:] = model_values['controller_outputs']
+      gen_states[es_idx,:,:] = model_values['gen_states']
+      factors[es_idx,:,:] = model_values['factors']
+      out_dist_params[es_idx,:,:] = model_values['output_dist_params']
+      costs[es_idx] = model_values['costs']
+      nll_bound_vaes[es_idx] = model_values['nll_bound_vaes']
+      nll_bound_iwaes[es_idx] = model_values['nll_bound_iwaes']
+      train_steps[es_idx] = model_values['train_steps']
+      print('bound nll(vae): %.3f, bound nll(iwae): %.3f' \
+            % (nll_bound_vaes[es_idx], nll_bound_iwaes[es_idx]))
+    
+    import numpy as np
+import tensorflow as tf
+    
+      def encode(self, sentence):
+    word_ids = [self.word_to_id(cur_word) for cur_word in sentence.split()]
+    return np.array([self.bos] + word_ids + [self.eos], dtype=np.int32)
+    
+    ## Sample Generation.
+## Binary and setup FLAGS.
+tf.app.flags.DEFINE_enum('sample_mode', 'TRAIN',
+                         [SAMPLE_TRAIN, SAMPLE_VALIDATION],
+                         'Dataset to sample from.')
+tf.app.flags.DEFINE_string('output_path', '/tmp', 'Model output directory.')
+tf.app.flags.DEFINE_boolean(
+    'output_masked_logs', False,
+    'Whether to display for human evaluation (show masking).')
+tf.app.flags.DEFINE_integer('number_epochs', 1,
+                            'The number of epochs to produce.')
+    
+      wasserstein_loss = real_avg - fake_avg
+  return wasserstein_loss
+    
+      for n in xrange(num_batches):
+    indices_batch = indices[n]
+    new_arr_batch = new_arr[n]
+    
+      return missing_ce_loss
+    
+    from six.moves import xrange
+    
+      Further usage can be found in the `kernel_tests/seq2seq_test.py`.
+    
+        def iter_body(self):
     
     
-iv = key = [0x20, 0x15] + 14 * [0]
-    
-    import io
-import sys
-import re
-    
-    import itertools
-import json
-import os
-import re
-import sys
-    
-    
-# And add them to TestDownload
-for n, test_case in enumerate(defs):
-    tname = 'test_' + str(test_case['name'])
-    i = 1
-    while hasattr(TestDownload, tname):
-        tname = 'test_%s_%d' % (test_case['name'], i)
-        i += 1
-    test_method = generator(test_case, tname)
-    test_method.__name__ = str(tname)
-    ie_list = test_case.get('add_ie')
-    test_method.add_ie = ie_list and ','.join(ie_list)
-    setattr(TestDownload, test_method.__name__, test_method)
-    del test_method
-    
-        def test_module_exec(self):
-        if sys.version_info >= (2, 7):  # Python 2.6 doesn't support package execution
-            subprocess.check_call([sys.executable, '-m', 'youtube_dl', '--version'], cwd=rootDir, stdout=_DEV_NULL)
-    
-                result.printErrors()
-            result.printSummary(start, stop)
-            self.exitcode = int(not result.wasSuccessful())
-    
-        def run(self, args, opts):
-        if len(args) < 1:
-            raise UsageError()
-        elif len(args) > 1:
-            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
-        spname = args[0]
-    
-        def long_desc(self):
-        return ('Edit a spider using the editor defined in the EDITOR environment'
-                ' variable or else the EDITOR setting')
-    
-        def report_out_of_quota(self, appid):
-        self.logger.warn('report_out_of_quota:%s', appid)
-        with self.lock:
-            if appid not in self.out_of_quota_appids:
-                self.out_of_quota_appids.append(appid)
-            try:
-                self.working_appid_list.remove(appid)
-            except:
-                pass
-    
-            exist_ca_sha1 = get_exist_ca_sha1()
-        if exist_ca_sha1 == ca_hash:
-            xlog.info('GoAgent CA exist')
-            return
-    
-    current_path = os.path.dirname(os.path.abspath(__file__))
-root_path = os.path.abspath(os.path.join(current_path, os.pardir, os.pardir, os.pardir))
-data_path = os.path.abspath(os.path.join(root_path, os.pardir, os.pardir, 'data', 'gae_proxy'))
-if not os.path.isdir(data_path):
-    data_path = current_path
-    
-    MIN_TOKEN_TYPE = UP+1
-	
-INVALID_TOKEN_TYPE = 0
+class PyTest(TestCommand):
+    # `$ python setup.py test' simply installs minimal requirements
+    # and runs the tests with no fancy stuff like parallel execution.
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            '--doctest-modules', '--verbose',
+            './httpie', './tests'
+        ]
+        self.test_suite = True
     
     
-    def combineFollows(self, exact):
-        followSet = set()
-        for idx, localFollowSet in reversed(list(enumerate(self._state.following))):
-            followSet |= localFollowSet
-            if exact:
-                # can we see end of rule?
-                if EOR_TOKEN_TYPE in localFollowSet:
-                    # Only leave EOR in set if at top (start rule); this lets
-                    # us know if have to include follow(start rule); i.e., EOF
-                    if idx > 0:
-                        followSet.remove(EOR_TOKEN_TYPE)
-                        
-                else:
-                    # can't see end of rule, quit
-                    break
+def test_credentials_in_url(httpbin_both):
+    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
+                   auth='user:password')
+    r = http('GET', url)
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+        exc = ConnectionError('Connection aborted')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR
+    assert error_msg == (
+        'ConnectionError: '
+        'Connection aborted while doing GET request to URL: '
+        'http://www.google.com')
     
     
-def get_github_url(app, view, path):
-    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
-    return (
-        github_fmt.format(app.config.edit_on_github_project, view,
-                          app.config.edit_on_github_branch,
-                          app.config.edit_on_github_src_path, path))
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
+    assert HTTP_OK in r
     
-            if state is None or state.state == STATE_UNKNOWN:
-            return
+        if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
     
-            if self.accountname in _CONFIGURING:
-            request_id = _CONFIGURING.pop(self.accountname)
-            configurator = self.hass.components.configurator
-            configurator.request_done(request_id)
+        # Arguments
+        model: Keras model instance.
+        line_length: Total length of printed lines
+            (e.g. set this to adapt the display to different
+            terminal window sizes).
+        positions: Relative or absolute positions of log elements in each line.
+            If not provided, defaults to `[.33, .55, .67, 1.]`.
+        print_fn: Print function to use.
+            It will be called on each line of the summary.
+            You can set it to a custom function
+            in order to capture the string summary.
+            It defaults to `print` (prints to stdout).
+    '''
+    if print_fn is None:
+        print_fn = print
     
-            _LOGGER.info('Scanner initialized')
     
-        @property
-    def physical_address(self):
-        '''Return the physical address of device in HDMI network.'''
-        return str(self._device.physical_address)
+def test_decode_predictions():
+    x = np.zeros((2, 1000))
+    x[0, 372] = 1.0
+    x[1, 549] = 1.0
+    outs = utils.decode_predictions(x, top=1)
+    scores = [out[0][2] for out in outs]
+    assert scores[0] == scores[1]
     
-                try:
-                kwargs['data'] = self._schema(data)
-            except vol.Invalid as err:
-                _LOGGER.error('Data does not match schema: %s', err)
-                return view.json_message(
-                    'Message format incorrect: {}'.format(err), 400)
+    Settings: rotation = 30.0
+----------------------------------------------------------------------------
+Epoch     | ImageGenerator | ImageGenerator | AugmentLayer  | Augment Layer
+Number    | %Accuracy      | Performance    | %Accuracy     | Performance
+----------------------------------------------------------------------------
+1         | 43.46          | 15ms/step      | 42.21         | 334us/step
+2         | 48.95          | 11ms/step      | 48.06         | 282us/step
+8         | 63.59          | 11ms/step      | 61.35         | 290us/step
+25        | 72.25          | 12ms/step      | 71.08         | 287us/step
+100       | 76.35          | 11ms/step      | 74.62         | 286us/step
+---------------------------------------------------------------------------
+(Corner process and rotation precision by `ImageGenerator` and `AugmentLayer`
+are slightly different.)
+'''
+    
+        def rollback_checkpoints(self, rollback=1):
+        '''Rollback saved checkpoints.
+    
+        def get_sni_addr(self, port):
+        '''Returns the least specific address that resolves on the port.
+    
+        def test_recovery_routine_error(self):
+        self.config.reverter.recovery_routine = mock.Mock(
+            side_effect=errors.ReverterError)
+    
+    from certbot_apache import configurator
+from certbot_apache import entrypoint
+    
+            # Make sure calls made to mocked function were correct
+        self.assertEqual(
+            mock_setup_cert.call_args_list[0], mock.call(self.achalls[0]))
+        self.assertEqual(
+            mock_setup_cert.call_args_list[1], mock.call(self.achalls[1]))
+    
+    # If true, show URL addresses after external links.
+#latex_show_urls = False
+    
+    
+def read_text(package: Package,
+              resource: Resource,
+              encoding: str = 'utf-8',
+              errors: str = 'strict') -> str:
+    '''Return the decoded string of the resource.
+    
+    import smtplib
+    
+    
+def main():
+    parser = ArgumentParser(description='''\
+Send the contents of a directory as a MIME message.
+Unless the -o option is given, the email is sent by forwarding to your local
+SMTP server, which then does the normal delivery process.  Your local machine
+must be running an SMTP server.
+''')
+    parser.add_argument('-d', '--directory',
+                        help='''Mail the contents of the specified directory,
+                        otherwise use the current directory.  Only the regular
+                        files in the directory are sent, and we don't recurse to
+                        subdirectories.''')
+    parser.add_argument('-o', '--output',
+                        metavar='FILE',
+                        help='''Print the composed message to FILE instead of
+                        sending the message to the SMTP server.''')
+    parser.add_argument('-s', '--sender', required=True,
+                        help='The value of the From: header (required)')
+    parser.add_argument('-r', '--recipient', required=True,
+                        action='append', metavar='RECIPIENT',
+                        default=[], dest='recipients',
+                        help='A To: header value (at least one required)')
+    args = parser.parse_args()
+    directory = args.directory
+    if not directory:
+        directory = '.'
+    # Create the message
+    msg = EmailMessage()
+    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
+    msg['To'] = ', '.join(args.recipients)
+    msg['From'] = args.sender
+    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+    
+    #
+# Function used to calculate result
+#
