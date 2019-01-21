244214dd108b9cd2b9d6c02159f7da236f97af8c
SUNsung <weatherius@gmail.com>
2019-01-21 07:37:27 +0200

    Rewrite [11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb] File in: 2019-01-21 07:37:26

--- ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
+++ ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
@@ -1,159 +1,63 @@
 
         
-                def render(&block)
-          render_collection_for(RadioButtonBuilder, &block)
-        end
-    
-            private
-    
-          # Creates a _layout method to be called by _default_layout .
-      #
-      # If a layout is not explicitly mentioned then look for a layout with the controller's name.
-      # if nothing is found then try same procedure to find super class's layout.
-      def _write_layout_method # :nodoc:
-        silence_redefinition_of_method(:_layout)
-    
-        private
-    
-        # Pass a action alias symbol (e.g. :enable_automatic_code_signing)
-    # and this method will return a reference to the action class
-    # if it exists. In case the action with this alias can't be found
-    # this method will return nil.
-    def class_reference_from_action_alias(method_sym)
-      alias_found = find_alias(method_sym.to_s)
-      return nil unless alias_found
-    
-          def self.authors
-        ['lmirosevic', 'maschall']
+                array.flatten.map! { |i| ERB::Util.unwrapped_html_escape(i) }.join(sep).html_safe
       end
     
-          it 'Collects logs in the specified revision range if specified' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          changelog_from_git_commits(between: ['abcd', '1234'])
-        end').runner.execute(:test)
-    
-          it 'handles no extension or extensions parameters' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.')
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
-      end
-    
-          context 'when specify false for strict option' do
-        it 'doesn't add strict option' do
-          result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-              strict: false
-            )
-          end').runner.execute(:test)
-    
-        it 'raises when no argument passed' do
-      expect do
-        command_from_args
-      end.to raise_error(ArgumentError)
+        initializer 'action_view.logger' do
+      ActiveSupport.on_load(:action_view) { self.logger ||= Rails.logger }
     end
     
-        def help_default_value
-      return '#{self.default_value} *'.strip if self.default_value_dynamic
-      return '' if self.default_value.nil?
-      return '''' if self.default_value.instance_of?(String) && self.default_value.empty?
-      return ':#{self.default_value}' if self.default_value.instance_of?(Symbol)
+    module ActionView
+  # This is the main entry point for rendering. It basically delegates
+  # to other objects like TemplateRenderer and PartialRenderer which
+  # actually renders the template.
+  #
+  # The Renderer will parse the options from the +render+ or +render_body+
+  # method and render a partial or a template based on the options. The
+  # +TemplateRenderer+ and +PartialRenderer+ objects are wrappers which do all
+  # the setup and logic necessary to render a view and a new object is created
+  # each time +render+ is called.
+  class Renderer
+    attr_accessor :lookup_context
     
-    # Contributors should always provide a changelog when submitting a PR
-if github.pr_body.length < 5
-  warn('Please provide a changelog summary in the Pull Request description @#{github.pr_author}')
-end
+            log_action :change_email, @user
     
-        def subpath_to(url)
-      base_url.subpath_to url, ignore_case: true
+          if @report_note.save
+        if params[:create_and_resolve]
+          @report.resolve!(current_account)
+          log_action :resolve, @report
+    
+      def status_finder
+    StatusFinder.new(params[:url])
+  end
+    
+        @web_subscription = ::Web::PushSubscription.create!(
+      endpoint: subscription_params[:endpoint],
+      key_p256dh: subscription_params[:keys][:p256dh],
+      key_auth: subscription_params[:keys][:auth],
+      data: data_params,
+      user_id: current_user.id,
+      access_token_id: doorkeeper_token.id
+    )
+    
+      def update
+    params.require([:id])
+    
+      include_examples 'multiline literal brace layout' do
+    let(:open) { '{' }
+    let(:close) { '}' }
+    let(:a) { 'a: 1' }
+    let(:b) { 'b: 2' }
+    let(:multi_prefix) { 'b: ' }
+    let(:multi) do
+      <<-RUBY.strip_indent.chomp
+        [
+        1
+        ]
+      RUBY
     end
+  end
     
-        def self.subscribe_to(notifier)
-      attach_to(namespace, new, notifier)
-    end
-    
-            title = at_css('h1').content.strip
-        if root_page?
-          at_css('h1').content = 'Angular 2 Documentation'
-        elsif title == 'Index'
-          at_css('h1').content = result[:entries].first.name
-        elsif title == 'Angular'
-          at_css('h1').content = slug.split('/').last.gsub('-', ' ')
-        elsif at_css('.breadcrumbs') && title != result[:entries].first.name
-          at_css('h1').content = result[:entries].first.name
+            when_branches.each do |condition|
+          yield condition
         end
-    
-            css('ul.methods', 'ul.properties', 'ul.events').add_class('defs').each do |node|
-          node.css('> li > h3').each do |h3|
-            next if h3.content.present?
-            h3.content = h3.next_element.content
-            h3.next_element.remove
-          end
-        end
-      end
-    end
-  end
-end
-
-    
-            render template: 'admin/reports/show'
-      end
-    end
-    
-      def maxwidth_or_default
-    (params[:maxwidth].presence || 400).to_i
-  end
-    
-      def process_salmon
-    SalmonWorker.perform_async(@account.id, payload.force_encoding('UTF-8'))
-  end
-end
-
-    
-      def data_params
-    @data_params ||= params.require(:data).permit(alerts: [:follow, :favourite, :reblog, :mention])
-  end
-end
-
-    
-      Devise.omniauth_configs.each_key do |provider|
-    provides_callback_for provider
-  end
-    
-    end
-
-    
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
-        end
-    
-          def deliver(msg)
-        if msg.respond_to?(:deliver_now)
-          # Rails 4.2/5.0
-          msg.deliver_now
-        else
-          # Rails 3.2/4.0/4.1
-          msg.deliver
-        end
-      end
-    end
-    
-    module Sidekiq
-  class BasicFetch
-    # We want the fetch operation to timeout every few seconds so the thread
-    # can check if the process is shutting down.
-    TIMEOUT = 2
-    
-              worker_classes.each do |worker_class|
-            Sidekiq::Testing.constantize(worker_class).drain
-          end
-        end
-      end
-    end
-  end
-end
-    
-      class WebRoute
-    attr_accessor :request_method, :pattern, :block, :name
