162aa7a22809a04873c8c15d667fd63164ffaa65
SUNsung <weatherius@gmail.com>
2019-01-21 13:13:08 +0200

    Rewrite [252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py] File in: 2019-01-21 13:13:07

--- pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
+++ pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
@@ -1,85 +1,46 @@
 
         
-        A second approach implemented relies on a list of porn domains, to activate it
-pass the list filename as the only argument
-'''
-    
-    password = key
-new_key = aes_encrypt(password, key_expansion(password))
-r = openssl_encode('aes-128-ctr', new_key, iv)
-print('aes_decrypt_text 16')
-print(repr(r))
-    
-    versions_info = json.load(open('update/versions.json'))
-if 'signature' in versions_info:
-    del versions_info['signature']
+            def test_encrypt(self):
+        msg = b'message'
+        key = list(range(16))
+        encrypted = aes_encrypt(bytes_to_intlist(msg), key)
+        decrypted = intlist_to_bytes(aes_decrypt(encrypted, key))
+        self.assertEqual(decrypted, msg)
     
     
-def main():
-    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
-        template = tmplf.read()
+from test.helper import FakeYDL
+from youtube_dl.cache import Cache
     
-    with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
+        def warning(self, msg):
+        self.messages.append(msg)
     
-        outfile, = args
+            status_line = 'HTTP/{version} {status} {reason}'.format(
+            version=version,
+            status=original.status,
+            reason=original.reason
+        )
+        headers = [status_line]
+        try:
+            # `original.msg` is a `http.client.HTTPMessage` on Python 3
+            # `_headers` is a 2-tuple
+            headers.extend(
+                '%s: %s' % header for header in original.msg._headers)
+        except AttributeError:
+            # and a `httplib.HTTPMessage` on Python 2.x
+            # `headers` is a list of `name: val<CRLF>`.
+            headers.extend(h.strip() for h in original.msg.headers)
     
-        readme = filter_options(readme)
+        @staticmethod
+    def make_header(username, password):
+        credentials = u'%s:%s' % (username, password)
+        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
+        return 'Basic %s' % token
     
-            info_dict = _make_result(list(formats_order), extractor='youtube')
-        ydl = YDL({'format': 'bestvideo/best,bestaudio'})
-        yie = YoutubeIE(ydl)
-        yie._sort_formats(info_dict['formats'])
-        ydl.process_ie_result(info_dict)
-        downloaded_ids = [info['format_id'] for info in ydl.downloaded_info_dicts]
-        self.assertEqual(downloaded_ids, ['137', '141'])
+    from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
     
-    # Allow direct execution
-import os
-import sys
-import unittest
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    
-    
-class AlexaFlashBriefingView(http.HomeAssistantView):
-    '''Handle Alexa Flash Briefing skill requests.'''
-    
-        def observe(call=None):
-        '''Observe three entities.'''
-        random_see('demo_paulus', 'Paulus')
-        random_see('demo_anne_therese', 'Anne Therese')
-    
-            _LOGGER.info('Scanner initialized')
-    
-    
-def get_scanner(hass, config):
-    '''Validate the configuration and return a THOMSON scanner.'''
-    scanner = ThomsonDeviceScanner(config[DOMAIN])
-    
-        def download_file(service):
-        '''Start thread to download file specified in the URL.'''
-        def do_download():
-            '''Download the file.'''
-            try:
-                url = service.data[ATTR_URL]
-    
-    image = face_recognition.load_image_file('{}')
-'''
-    
-    # Note: This isn't exactly the same as a 'percent match'. The scale isn't linear. But you can assume that images with a
-# smaller distance are more similar to each other than ones with a larger distance.
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-    # Open the input movie file
-input_movie = cv2.VideoCapture('hamilton_clip.mp4')
-length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
-    
-        if len(unknown_face_encodings) > 0:
-        face_found = True
-        # 看看图片中的第一张脸是不是奥巴马
-        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
-        if match_results[0]:
-            is_obama = True
+        def test_implicit_POST_stdin(self, httpbin):
+        with open(FILE_PATH) as f:
+            env = MockEnvironment(stdin_isatty=False, stdin=f)
+            r = http('--form', httpbin.url + '/post', env=env)
+        assert HTTP_OK in r
