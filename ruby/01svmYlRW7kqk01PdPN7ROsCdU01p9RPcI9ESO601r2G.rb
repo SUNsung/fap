770b9fa11be4555400711bc3c1c8a38e27e2f723
SUNsung <weatherius@gmail.com>
2019-01-21 20:11:05 +0200

    Rewrite [01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb] File in: 2019-01-21 20:11:04

--- ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
+++ ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
@@ -1,224 +1,125 @@
 
         
-            To install Clojure you should install Leiningen:
-      brew install leiningen
-    and then follow the tutorial:
-      https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md
-    EOS
-  when 'osmium' then <<-EOS.undent
-    The creator of Osmium requests that it not be packaged and that people
-    use the GitHub master branch instead.
-    EOS
-  when 'gfortran' then <<-EOS.undent
-    GNU Fortran is now provided as part of GCC, and can be installed with:
-      brew install gcc
-    EOS
-  when 'play' then <<-EOS.undent
-    Play 2.3 replaces the play command with activator:
-      brew install typesafe-activator
+          # True if a {Formula} is being built with a specific option
+  # (which isn't named `with-*` or `without-*`).
+  # @deprecated
+  def include?(name)
+    @args.include?('--#{name}')
+  end
     
-          if path.symlink? || path.directory?
-        next
-      elsif path.extname == '.la'
-        path.unlink
-      else
-        # Set permissions for executables and non-executables
-        perms = if path.mach_o_executable? || path.text_executable?
-          0555
-        else
-          0444
-        end
-        if ARGV.debug?
-          old_perms = path.stat.mode & 0777
-          if perms != old_perms
-            puts 'Fixing #{path} permissions from #{old_perms.to_s(8)} to #{perms.to_s(8)}'
+        def self.cleanup_logs
+      return unless HOMEBREW_LOGS.directory?
+      HOMEBREW_LOGS.subdirs.each do |dir|
+        cleanup_path(dir) { dir.rmtree } if prune?(dir, :days_default => 14)
+      end
+    end
+    
+        if ARGV.include?('--pinned') || ARGV.include?('--versions')
+      filtered_list
+    elsif ARGV.named.empty?
+      if ARGV.include? '--full-name'
+        full_names = Formula.installed.map(&:full_name).sort do |a, b|
+          if a.include?('/') && !b.include?('/')
+            1
+          elsif !a.include?('/') && b.include?('/')
+            -1
+          else
+            a <=> b
           end
         end
-        path.chmod perms
+        puts_columns full_names
+      else
+        ENV['CLICOLOR'] = nil
+        exec 'ls', *ARGV.options_only << HOMEBREW_CELLAR
       end
-    end
-  end
-end
-
-    
-            next unless HOMEBREW_CELLAR.directory?
-    
-      def filtered_list
-    names = if ARGV.named.empty?
-      Formula.racks
+    elsif ARGV.verbose? || !$stdout.tty?
+      exec 'find', *ARGV.kegs.map(&:to_s) + %w[-not -type d -print]
     else
-      ARGV.named.map { |n| HOMEBREW_CELLAR+n }.select(&:exist?)
-    end
-    if ARGV.include? '--pinned'
-      pinned_versions = {}
-      names.each do |d|
-        keg_pin = (HOMEBREW_LIBRARY/'PinnedKegs'/d.basename.to_s)
-        if keg_pin.exist? || keg_pin.symlink?
-          pinned_versions[d] = keg_pin.readlink.basename.to_s
-        end
-      end
-      pinned_versions.each do |d, version|
-        puts '#{d.basename}'.concat(ARGV.include?('--versions') ? ' #{version}' : '')
-      end
-    else # --versions without --pinned
-      names.each do |d|
-        versions = d.subdirs.map { |pn| pn.basename.to_s }
-        next if ARGV.include?('--multiple') && versions.length < 2
-        puts '#{d.basename} #{versions*' '}'
-      end
+      ARGV.kegs.each { |keg| PrettyListing.new keg }
     end
   end
-end
     
         current_revision_var = 'HOMEBREW_UPDATE_AFTER#{repo_var}'
     @current_revision = ENV[current_revision_var].to_s
     raise ReporterRevisionUnsetError, current_revision_var if @current_revision.empty?
   end
     
-          export JAVA_HOME='$(/usr/libexec/java_home)'
-      export AWS_ACCESS_KEY='<Your AWS Access ID>'
-      export AWS_SECRET_KEY='<Your AWS Secret Key>'
-      export #{home_name}='#{home_value}'
-    EOS
-  end
-end
-
-    
-    describe GroupUser do
-    
-      UserOption.where(user_id: smoke_user.id).update_all(
-    email_direct: false,
-    email_digests: false,
-    email_private_messages: false,
-  )
-    
-        Category.transaction do
-      staff.group_names = ['staff']
-      unless staff.save
-        puts staff.errors.full_messages
-        raise 'Failed to set permissions on the Staff category!'
-      end
-    
-            # This contains all the registered host capabilities.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :host_capabilities
-    
-            # Called after the configuration is finalized and loaded to validate
-        # this object.
-        #
-        # @param [Machine] machine Access to the machine that is being
-        #   validated.
-        # @return [Hash]
-        def validate(machine)
-          return { self.to_s => _detected_errors }
-        end
-    
-            # Sets a human-friendly description of the plugin.
-        #
-        # @param [String] value Description of the plugin.
-        # @return [String] Description of the plugin.
-        def self.description(value=UNSET_VALUE)
-          get_or_set(:description, value)
-        end
-    
-            # This method is called if the underlying machine ID changes. Providers
-        # can use this method to load in new data for the actual backing
-        # machine or to realize that the machine is now gone (the ID can
-        # become `nil`). No parameters are given, since the underlying machine
-        # is simply the machine instance given to this object. And no
-        # return value is necessary.
-        def machine_id_changed
-        end
-    
-      def load_logs(dir)
-    logs = {}
-    if dir.exist?
-      dir.children.sort.each do |file|
-        contents = file.size? ? file.read : 'empty log'
-        # small enough to avoid GitHub 'unicorn' page-load-timeout errors
-        max_file_size = 1_000_000
-        contents = truncate_text_to_approximate_size(contents, max_file_size, front_weight: 0.2)
-        logs[file.basename.to_s] = { content: contents }
-      end
-    end
-    raise 'No logs.' if logs.empty?
-    
-      def to_ary
-    @paths.dup.to_ary
-  end
-  alias to_a to_ary
-    
-      def command
-    abort 'This command requires a command argument' if ARGV.empty?
-    
-      def up
-    Photo.joins('INNER JOIN posts ON posts.guid = photos.status_message_guid')
-         .where(posts: {type: 'StatusMessage', public: true}).update_all(public: true)
-    
-      class SendPrivate < Base
-    def perform(*_args)
-      # don't federate in cucumber
-    end
-  end
-    
-        it 'generates a jasmine fixture', :fixture => true do
-      contact = alice.contact_for(bob.person)
-      aspect = alice.aspects.create(:name => 'people')
-      contact.aspects << aspect
-      contact.save
-      get :new, params: {person_id: bob.person.id}
-      save_fixture(html_for('body'), 'status_message_new')
-    end
-  end
-end
-
-    
-          it 'doesn't post' do
-        expect(alice).not_to receive(:like!)
-        post :create, params: like_hash
-        expect(response.code).to eq('422')
-      end
-    end
-    
-          def header_content
-        has_header && @header.formatted_data
-      end
-    
-    def bump_version
-  old_file = File.read('lib/#{name}.rb')
-  old_version_line = old_file[/^\s*VERSION\s*=\s*.*/]
-  new_version = next_version
-  # replace first match of old vesion with new version
-  old_file.sub!(old_version_line, '  VERSION = '#{new_version}'')
-    
-      class DuplicatePageError < Error
-    attr_accessor :dir
-    attr_accessor :existing_path
-    attr_accessor :attempted_path
-    
-          def queue_name
-        queue.sub(/.*queue:/, '')
-      end
-    
-          def invoke(*args)
-        chain = retrieve.dup
-        traverse_chain = lambda do
-          if chain.empty?
-            yield
-          else
-            chain.shift.call(*args, &traverse_chain)
+        def verify_supported_os(name, class_ref)
+      if class_ref.respond_to?(:is_supported?)
+        # This value is filled in based on the executed platform block. Might be nil when lane is in root of Fastfile
+        platform = Actions.lane_context[Actions::SharedValues::PLATFORM_NAME]
+        if platform
+          unless class_ref.is_supported?(platform)
+            UI.important('Action '#{name}' isn't known to support operating system '#{platform}'.')
           end
         end
-        traverse_chain.call
       end
     end
     
-          # Clear all jobs for this worker
-      def clear
-        Queues.clear_for(queue, self.to_s)
+          if @launch_event_sent || launch_context.p_hash.nil?
+        return
       end
     
-        def put(path, &block)
-      route(PUT, path, &block)
+          it 'Collects logs in the specified revision range if specified' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          changelog_from_git_commits(between: ['abcd', '1234'])
+        end').runner.execute(:test)
+    
+          it 'handles the extension parameter correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', extension: 'rb')
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --include=\\*.rb')
+      end
+    
+          context 'without parameters' do
+        it 'executes the correct git command' do
+          allow(Fastlane::Actions).to receive(:sh).with('git add .', anything).and_return('')
+          result = Fastlane::FastFile.new.parse('lane :test do
+            git_add
+          end').runner.execute(:test)
+        end
+      end
+    
+            it 'escapes spaces in list of files to process' do
+          file = 'path/to/my project/source code/AppDelegate.swift'
+          result = Fastlane::FastFile.new.parse('lane :test do
+            swiftlint(
+                files: ['#{file}']
+            )
+          end').runner.execute(:test)
+    
+          it 'returns the return value of the block if present' do
+        expect_command('ls', '-la')
+        return_value = Fastlane::Actions.sh('ls', '-la') do |status, result|
+          42
+        end
+        expect(return_value).to eq(42)
+      end
     end
+  end
+    
+          global_option('--verbose') { FastlaneCore::Globals.verbose = true }
+    
+        def resource_params
+      params.require(:report_note).permit(
+        :content,
+        :report_id
+      )
+    end
+    
+        private
+    
+      def maxwidth_or_default
+    (params[:maxwidth].presence || 400).to_i
+  end
+    
+        weeks
+  end
+    
+      if rss_url.nil?
+    puts '#{name}: GETTING'
+    rss_check_url = 'http://ajax.googleapis.com/ajax/services/feed/lookup?v=1.0&q=#{web_url}'
+    uri = URI.parse(rss_check_url)
+    response = JSON.parse(Net::HTTP.get(uri))
+    rss_url = response['responseData']['url'] if response['responseData'] && response['responseData'].has_key?('url')
