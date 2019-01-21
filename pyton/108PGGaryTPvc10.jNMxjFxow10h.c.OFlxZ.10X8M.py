bdaf185be0175f3244d48cefa33b7e2b9fbb00fe
SUNsung <weatherius@gmail.com>
2019-01-21 04:40:08 +0200

    Rewrite [108PGGaryTPvc10.jNMxjFxow10h.c.OFlxZ.10X8M.py] File in: 2019-01-21 04:40:08

--- pyton/108PGGaryTPvc10.jNMxjFxow10h.c.OFlxZ.10X8M.py
+++ pyton/108PGGaryTPvc10.jNMxjFxow10h.c.OFlxZ.10X8M.py
@@ -1,100 +1,93 @@
 
         
-            def remove_user(self, user_id):
-        pass
+              # Reflect on the boundaries to not lose mass.
+      shuffle_tidxs[shuffle_tidxs < 0] = -shuffle_tidxs[shuffle_tidxs < 0]
+      shuffle_tidxs[shuffle_tidxs > T-1] = \
+          (T-1)-(shuffle_tidxs[shuffle_tidxs > T-1] -(T-1))
     
     
-if __name__ == '__main__':
-    SpendingByCategory.run()
-
-    
-    from mrjob.job import MRJob
-    
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
-    
-        def extract_max_priority_page(self):
-        '''Return the highest priority link in `links_to_crawl`.'''
-        pass
-    
-    This module contains the set of Requests' exceptions.
-'''
-from urllib3.exceptions import HTTPError as BaseHTTPError
-    
-    
-@pytest.fixture
-def httpbin_secure(httpbin_secure):
-    return prepare_url(httpbin_secure)
-
-    
-        def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
-    
-    # Attempt to enable urllib3's SNI support, if possible
-try:
-    from urllib3.contrib import pyopenssl
-    pyopenssl.inject_into_urllib3()
-    
-            # Fallback to None if there's no status_code, for whatever reason.
-        response.status_code = getattr(resp, 'status', None)
+def generate_data(rnn, T, E, x0s=None, P_sxn=None, input_magnitude=0.0,
+                  input_times=None):
+  ''' Generates data from an randomly initialized RNN.
+  Args:
+    rnn: the rnn
+    T: Time in seconds to run (divided by rnn['dt'] to get steps, rounded down.
+    E: total number of examples
+    S: number of samples (subsampling N)
+  Returns:
+    A list of length E of NxT tensors of the network being run.
+  '''
+  N = rnn['N']
+  def run_rnn(rnn, x0, ntime_steps, input_time=None):
+    rs = np.zeros([N,ntime_steps])
+    x_tm1 = x0
+    r_tm1 = np.tanh(x0)
+    tau = rnn['tau']
+    dt = rnn['dt']
+    alpha = (1.0-dt/tau)
+    W = dt/tau*rnn['W']*rnn['g']
+    Bin = dt/tau*rnn['Bin']
+    Bin2 = dt/tau*rnn['Bin2']
+    b = dt/tau*rnn['b']
     
     
-class TestGuessJSONUTF:
+def flatten(list_of_lists):
+  '''Takes a list of lists and returns a list of the elements.
     
-        ca_certs = os.path.join(current_path, 'cacert.pem')
-    openssl_context = SSLContext(
-        logger, ca_certs=ca_certs,
-        cipher_suites=['ALL', '!RC4-SHA', '!ECDHE-RSA-RC4-SHA', '!ECDHE-RSA-AES128-GCM-SHA256',
-                       '!AES128-GCM-SHA256', '!ECDHE-RSA-AES128-SHA', '!AES128-SHA']
-    )
-    host_manager = HostManagerBase()
-    connect_creator = ConnectCreator(logger, config, openssl_context, host_manager,
-                                     debug=True)
-    check_ip = CheckIp(logger, config, connect_creator)
+      def _score_patches(self, word_patches):
+    '''Score a 2D matrix of word_patches and stitch results together.'''
+    batch_size, num_timesteps = self.shape
+    nrow, ncol = len(word_patches), len(word_patches[0])
+    max_len = num_timesteps * ncol
+    probs = np.zeros([0, max_len])  # accumulate results into this.
     
-        def getChannel(self):
-        '''@brief Get the channel of the token
+      @property
+  def eos(self):
+    return self._eos
     
+    '''Model construction.'''
     
-class CompositeGraphic(Graphic):
-    def __init__(self):
-        self.graphics = []
+      Returns:
+    avg_log_perplexity:  Scalar indicating the average log perplexity per
+      missing token in the batch.
+  '''
+  # logits = tf.Print(logits, [logits], message='logits:', summarize=50)
+  # targets = tf.Print(targets, [targets], message='targets:', summarize=50)
+  eps = 1e-12
+  logits = tf.reshape(logits, [-1, FLAGS.vocab_size])
     
+      for _, value in gen_ngrams_dict.iteritems():
+    total_ngrams_produced += value
     
-if __name__ == '__main__':
-    
-        def setReporter(self, reporter):
-        self._reporter = reporter
-    
-        def toggle_amfm(self):
-        print(u'Switching to AM')
-        self.radio.state = self.radio.amstate
-    
-    
-class C(A, B):
-    pass
+          next input: `cell_input`, this decoder function does not modify the
+      given input. The input could be modified when applying e.g. attention.
     
     
-class Person(object):
-    def __init__(self, name, occupation):
-        self.name = name
-        self.occupation = occupation
-        self.call_count2 = 0
+class CITextExtension(CreateExtension):
     
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-'''
+        def delete(self, session_key=None):
+        '''
+        To delete, clear the session key and the underlying data structure
+        and set the modified flag so that the cookie is set on the client for
+        the current request.
+        '''
+        self._session_key = ''
+        self._session_cache = {}
+        self.modified = True
     
-        '''Graph search emulation in python, from source
-    http://www.python.org/doc/essays/graphs/'''
+        class Meta:
+        abstract = True
+        verbose_name = _('session')
+        verbose_name_plural = _('sessions')
+    
+    
+class Session(AbstractBaseSession):
+    '''
+    Django provides full support for anonymous sessions. The session
+    framework lets you store and retrieve arbitrary data on a
+    per-site-visitor basis. It stores data on the server side and
+    abstracts the sending and receiving of cookies. Cookies contain a
+    session ID -- not the data itself.
+    
+        req_protocol = request.scheme
+    req_site = get_current_site(request)
