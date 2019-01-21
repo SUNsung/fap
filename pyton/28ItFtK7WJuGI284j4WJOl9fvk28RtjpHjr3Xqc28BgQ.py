159b9752a5bdff7e998316d200a5c0cb8d4854b6
SUNsung <weatherius@gmail.com>
2019-01-21 11:22:46 +0200

    Rewrite [28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py] File in: 2019-01-21 11:22:46

--- pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
+++ pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
@@ -1,116 +1,181 @@
 
         
-          The log p(x|z) term is the reconstruction error under the model.
-  The KL term represents the penalty for passing information from the encoder
-  to the decoder.
-  To sample KL(q||p), we simply sample
-        ln q - ln p
-  by drawing samples from q and averaging.
-  '''
+            def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
-      if identity_if_possible and in_size == out_size:
-    return (tf.constant(np.eye(in_size).astype(np.float32)),
-            tf.zeros(in_size))
+    from mrjob.job import MRJob
     
-      Yields:
-    Pairs of the batched data, each a matrix of shape [batch_size, num_steps].
-    The second element of the tuple is the same data time-shifted to the
-    right by one. The third is a set of weights with 1 indicating a word was
-    present and 0 not.
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-      # Averages for real and fake token values.
-  real = tf.mul(values, present)
-  fake = tf.mul(values, missing)
-  real_avg = tf.reduce_sum(real) / real_count
-  fake_avg = tf.reduce_sum(fake) / fake_count
-    
-      ## REINFORCE with different baselines.
-  # We create a separate critic functionality for the Discriminator.  This
-  # will need to operate unidirectionally and it may take in the past context.
-  if FLAGS.baseline_method == 'critic':
+        def bfs(self, source, dest):
+        if source is None:
+            return False
+        queue = deque()
+        queue.append(source)
+        source.visit_state = State.visited
+        while queue:
+            node = queue.popleft()
+            print(node)
+            if dest is node:
+                return True
+            for adjacent_node in node.adj_nodes.values():
+                if adjacent_node.visit_state == State.unvisited:
+                    queue.append(adjacent_node)
+                    adjacent_node.visit_state = State.visited
+        return False
     
     
-def check_alphabetical(lines):
-    '''
-    checks if all entries per section are in alphabetical order based in entry title
-    '''
-    sections = {}
-    section_line_num = {}
-    for line_num, line in enumerate(lines):
-        if line.startswith(anchor):
-            category = line.split(anchor)[1].strip()
-            sections[category] = []
-            section_line_num[category] = line_num
-            continue
-        if not line.startswith('|') or line.startswith('|---'):
-            continue
-        raw_title = [x.strip() for x in line.split('|')[1:-1]][0]
-        title_re_match = link_re.match(raw_title)
-        if title_re_match:
-            sections[category].append(title_re_match.group(1).upper())
+class Node(object):
     
-        if not hasattr(env, 'scrapy_all_settings'):
-        env.scrapy_all_settings = []
+    x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
     
-            slots = int(self.slots)
-        if slots > 1:
-            urls = [url.replace('localhost', '127.0.0.%d' % (x + 1)) for x in range(slots)]
+        paths = []
+    for fname in files:
+        paths.append(get_file(fname,
+                              origin=base + fname,
+                              cache_subdir=dirname))
+    
+        model._check_trainable_weights_consistency()
+    if hasattr(model, '_collected_trainable_weights'):
+        trainable_count = count_params(model._collected_trainable_weights)
+    else:
+        trainable_count = count_params(model.trainable_weights)
+    
+        outputs1 = Lambda(lambda x: utils.preprocess_input(x, 'channels_last'),
+                      output_shape=x.shape[1:])(inputs)
+    model1 = Model(inputs, outputs1)
+    out1 = model1.predict(x)
+    x2 = np.transpose(x, (0, 3, 1, 2))
+    inputs2 = Input(shape=x2.shape[1:])
+    outputs2 = Lambda(lambda x: utils.preprocess_input(x, 'channels_first'),
+                      output_shape=x2.shape[1:])(inputs2)
+    model2 = Model(inputs2, outputs2)
+    out2 = model2.predict(x2)
+    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
+    
+    
+def test_reuters():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = reuters.load_data()
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        assert len(x_train) + len(x_test) == 11228
+        (x_train, y_train), (x_test, y_test) = reuters.load_data(maxlen=10)
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        word_index = reuters.get_word_index()
+        assert isinstance(word_index, dict)
+    
+        model = create_multi_input_model_from(*models)
+    model.compile(loss='categorical_crossentropy', optimizer='sgd')
+    assert len(model.losses) == 8
+    
+        # Arguments
+        y: class vector to be converted into a matrix
+            (integers from 0 to num_classes).
+        num_classes: total number of classes.
+        dtype: The data type expected by the input, as a string
+            (`float32`, `float64`, `int32`...)
+    
+            if self.use_bias:
+            self.bias_i = self.bias[:self.filters]
+            self.bias_f = self.bias[self.filters: self.filters * 2]
+            self.bias_c = self.bias[self.filters * 2: self.filters * 3]
+            self.bias_o = self.bias[self.filters * 3:]
         else:
-            urls = [url]
+            self.bias_i = None
+            self.bias_f = None
+            self.bias_c = None
+            self.bias_o = None
+        self.built = True
+    
+    Settings: horizontal_flip = True
+----------------------------------------------------------------------------
+Epoch     | ImageGenerator | ImageGenerator | AugmentLayer  | Augment Layer
+Number    | %Accuracy      | Performance    | %Accuracy     | Performance
+----------------------------------------------------------------------------
+1         | 44.84          | 15ms/step      | 45.54         | 358us/step
+2         | 52.34          |  8ms/step      | 50.55         | 285us/step
+8         | 65.45          |  8ms/step      | 65.59         | 281us/step
+25        | 76.74          |  8ms/step      | 76.17         | 280us/step
+100       | 78.81          |  8ms/step      | 78.70         | 285us/step
+---------------------------------------------------------------------------
     
     
-class _BenchServer(object):
+def create_model(stateful):
+    model = Sequential()
+    model.add(LSTM(20,
+              input_shape=(lahead, 1),
+              batch_size=batch_size,
+              stateful=stateful))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+    return model
     
-        def _next_request_from_scheduler(self, spider):
-        slot = self.slot
-        request = slot.scheduler.next_request()
-        if not request:
-            return
-        d = self._download(request, spider)
-        d.addBoth(self._handle_downloader_output, request, spider)
-        d.addErrback(lambda f: logger.info('Error while handling downloader output',
-                                           exc_info=failure_to_exc_info(f),
-                                           extra={'spider': spider}))
-        d.addBoth(lambda _: slot.remove_request(request))
-        d.addErrback(lambda f: logger.info('Error while removing request from slot',
-                                           exc_info=failure_to_exc_info(f),
-                                           extra={'spider': spider}))
-        d.addBoth(lambda _: slot.nextcall.schedule())
-        d.addErrback(lambda f: logger.info('Error while scheduling new request',
-                                           exc_info=failure_to_exc_info(f),
-                                           extra={'spider': spider}))
-        return d
-    
-        result = False
-    try:
-        ret = urlopen(url, timeout=2)
-        result = (ret.code == 200)
-    except HTTPError as e:
-        print(e, file=sys.stderr)
-    except URLError as e:
-        print(e, file=sys.stderr)
-    except timeout as e:
-        print(e, file=sys.stderr)
-    except Exception as e:
-        print(e, file=sys.stderr)
-    
-        def __init__(self, deep, *targets):
-        self.deep = deep
-        self.targets = targets
-        self.commit()
+        if '_text' in dictified['video'][0]['size'][0]:  #size exists for 1 piece
+        video_dict['size'] = sum([int(i['size'][0]['_text']) for i in dictified['video']])
     
     
-if __name__ == '__main__':
-    main()
+def coub_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_content(url)
     
-    *TL;DR80
-Implements state as a derived class of the state pattern interface.
-Implements state transitions by invoking methods from the pattern's superclass.
-'''
+        for quality in ['1080','720','480','380','240','144','auto']:
+        try:
+            real_url = info[quality][1]['url']
+            if real_url:
+                break
+        except KeyError:
+            pass
     
-        def __init__(self):
-        self.time_provider = datetime.datetime
+        if int(html['status']) != 100000:
+        raise Exception('API error!')
+    
+    def get_single_photo_url(url):
+    page = get_html(url)
+    pid = get_photo_id(url, page)
+    title = match1(page, pattern_inline_title)
+    if match1(page, pattern_inline_video_mark):
+        api_key = get_api_key(page)
+        reply = get_content(tmpl_api_call_photo_info % (api_key, get_photo_id(url, page)))
+        secret = json.loads(reply)['photo']['secret']
+        return get_orig_video_source(api_key, pid, secret), title
+    #last match always has the best resolution
+    match = match1(page, pattern_inline_img_url)
+    return 'https:' + match.replace('\\', ''), title
+    
+        output_directory = 'pdfs' if results.directory is None else results.directory
+    
+    
+def html_page_context(app, pagename, templatename, context, doctree):
+    if templatename != 'page.html':
+        return
+    
+    
+Device = namedtuple('Device', ['mac', 'ip', 'last_update'])
+    
+        def random_see(dev_id, name):
+        '''Randomize a sighting.'''
+        see(
+            dev_id=dev_id,
+            host_name=name,
+            gps=(hass.config.latitude + offset(),
+                 hass.config.longitude + offset()),
+            gps_accuracy=random.randrange(50, 150),
+            battery=random.randrange(10, 90)
+        )
+    
+    import voluptuous as vol
+    
+    MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=1)
