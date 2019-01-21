b27c18812fe74c4392e4d018be356938f9198407
SUNsung <weatherius@gmail.com>
2019-01-21 02:02:10 +0200

    Rewrite [50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp] File in: 2019-01-21 02:02:10

--- cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
+++ cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
@@ -1,350 +1,189 @@
 
         
-        // Converts a streamable value to an std::string.  A NULL pointer is
-// converted to '(null)'.  When the input value is a ::string,
-// ::std::string, ::wstring, or ::std::wstring object, each NUL
-// character in it is replaced with '\\0'.
-template <typename T>
-std::string StreamableToString(const T& streamable) {
-  return (Message() << streamable).GetString();
-}
-    
-    // Implements printing an array type T[N].
-template <typename T, size_t N>
-class UniversalPrinter<T[N]> {
- public:
-  // Prints the given array, omitting some elements when there are too
-  // many.
-  static void Print(const T (&a)[N], ::std::ostream* os) {
-    UniversalPrintArray(a, N, os);
-  }
-};
-    
-    #define GTEST_IMPL_FORMAT_C_STRING_AS_STRING_(CharType, OtherStringType) \
-  template <>                                                           \
-  class FormatForComparison<CharType*, OtherStringType> {               \
-   public:                                                              \
-    static ::std::string Format(CharType* value) {                      \
-      return ::testing::PrintToString(value);                           \
-    }                                                                   \
-  }
-    
-        virtual const ParamGeneratorInterface<ParamType>* BaseGenerator() const {
-      return base_;
-    }
-    // Advance should not be called on beyond-of-range iterators
-    // so no component iterators must be beyond end of range, either.
-    virtual void Advance() {
-      assert(!AtEnd());
-      ++current6_;
-      if (current6_ == end6_) {
-        current6_ = begin6_;
-        ++current5_;
-      }
-      if (current5_ == end5_) {
-        current5_ = begin5_;
-        ++current4_;
-      }
-      if (current4_ == end4_) {
-        current4_ = begin4_;
-        ++current3_;
-      }
-      if (current3_ == end3_) {
-        current3_ = begin3_;
-        ++current2_;
-      }
-      if (current2_ == end2_) {
-        current2_ = begin2_;
-        ++current1_;
-      }
-      ComputeCurrentValue();
-    }
-    virtual ParamIteratorInterface<ParamType>* Clone() const {
-      return new Iterator(*this);
-    }
-    virtual const ParamType* Current() const { return &current_value_; }
-    virtual bool Equals(const ParamIteratorInterface<ParamType>& other) const {
-      // Having the same base generator guarantees that the other
-      // iterator is of the same type and we can downcast.
-      GTEST_CHECK_(BaseGenerator() == other.BaseGenerator())
-          << 'The program attempted to compare iterators '
-          << 'from different generators.' << std::endl;
-      const Iterator* typed_other =
-          CheckedDowncastToActualType<const Iterator>(&other);
-      // We must report iterators equal if they both point beyond their
-      // respective ranges. That can happen in a variety of fashions,
-      // so we have to consult AtEnd().
-      return (AtEnd() && typed_other->AtEnd()) ||
-         (
-          current1_ == typed_other->current1_ &&
-          current2_ == typed_other->current2_ &&
-          current3_ == typed_other->current3_ &&
-          current4_ == typed_other->current4_ &&
-          current5_ == typed_other->current5_ &&
-          current6_ == typed_other->current6_);
-    }
-    
-      // Try to divide n by every odd number i, starting from 3
-  for (int i = 3; ; i += 2) {
-    // We only have to try i up to the squre root of n
-    if (i > n/i) break;
-    }
-    
-    #include <limits.h>
-#include 'sample1.h'
-#include 'gtest/gtest.h'
-    
-    // Clones a 0-terminated C string, allocating memory using new.
-const char* MyString::CloneCString(const char* a_c_string) {
-  if (a_c_string == NULL) return NULL;
-    }
-    
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-    
-      // D'tor.  Clears the queue.
-  ~Queue() { Clear(); }
-    
-    #ifndef BULLET_TYPES_CONVERTER_H
-#define BULLET_TYPES_CONVERTER_H
-    
-    #include 'collision_object_bullet.h'
-#include 'space_bullet.h'
-    
-    
-    {
-    {		virtual btCollisionAlgorithm *CreateCollisionAlgorithm(btCollisionAlgorithmConstructionInfo &ci, const btCollisionObjectWrapper *body0Wrap, const btCollisionObjectWrapper *body1Wrap) {
-			void *mem = ci.m_dispatcher1->allocateCollisionAlgorithm(sizeof(GodotRayWorldAlgorithm));
-			return new (mem) GodotRayWorldAlgorithm(m_world, ci.m_manifold, ci, body0Wrap, body1Wrap, true);
-		}
-	};
-};
-    
-    	const RigidBodyBullet *getRigidBodyA() const;
-	const RigidBodyBullet *getRigidBodyB() const;
-	const Transform getCalculatedTransformA() const;
-	const Transform getCalculatedTransformB() const;
-	const Transform getFrameOffsetA() const;
-	const Transform getFrameOffsetB() const;
-	Transform getFrameOffsetA();
-	Transform getFrameOffsetB();
-	real_t getLowerLinLimit() const;
-	void setLowerLinLimit(real_t lowerLimit);
-	real_t getUpperLinLimit() const;
-	void setUpperLinLimit(real_t upperLimit);
-	real_t getLowerAngLimit() const;
-	void setLowerAngLimit(real_t lowerLimit);
-	real_t getUpperAngLimit() const;
-	void setUpperAngLimit(real_t upperLimit);
-    
-    	jclass activityThread = env->FindClass('android/app/ActivityThread');
-	jmethodID currentActivityThread = env->GetStaticMethodID(activityThread, 'currentActivityThread', '()Landroid/app/ActivityThread;');
-	jobject at = env->CallStaticObjectMethod(activityThread, currentActivityThread);
-	jmethodID getApplication = env->GetMethodID(activityThread, 'getApplication', '()Landroid/app/Application;');
-	jobject context = env->CallObjectMethod(at, getApplication);
-    
-    #include 'shell.h'
-    
-    
-    {	AudioDriverDummy(){};
-	~AudioDriverDummy(){};
-};
-    
-      std::string Key(int i) {
-    char buf[100];
-    snprintf(buf, sizeof(buf), 'key%06d', i);
-    return std::string(buf);
-  }
-    
-      void MaybeScheduleCompaction() EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-  static void BGWork(void* db);
-  void BackgroundCall();
-  void BackgroundCompaction() EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-  void CleanupCompaction(CompactionState* compact)
-      EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-  Status DoCompactionWork(CompactionState* compact)
-      EXCLUSIVE_LOCKS_REQUIRED(mutex_);
-    
-    namespace leveldb {
-namespace log {
-    }
-    }
-    
-    // Construct a string of the specified length made out of the supplied
-// partial string.
-static std::string BigString(const std::string& partial_string, size_t n) {
-  std::string result;
-  while (result.size() < n) {
-    result.append(partial_string);
-  }
-  result.resize(n);
-  return result;
-}
-    
-    #include <stdint.h>
-#include 'leveldb/env.h'
-#include 'util/coding.h'
-#include 'util/crc32c.h'
-    
-      ~Writer();
-    
-      std::string Get(const std::string& k, const Snapshot* snapshot = nullptr) {
-    std::string result;
-    Status s = db_->Get(ReadOptions(), k, &result);
-    if (s.IsNotFound()) {
-      result = 'NOT_FOUND';
-    } else if (!s.ok()) {
-      result = s.ToString();
-    }
-    return result;
-  }
-    
-    
-    {    uint64_t number;
-    FileType type;
-    for (size_t i = 0; i < filenames.size(); i++) {
-      if (ParseFileName(filenames[i], &number, &type)) {
-        if (type == kDescriptorFile) {
-          manifests_.push_back(filenames[i]);
-        } else {
-          if (number + 1 > next_file_number_) {
-            next_file_number_ = number + 1;
-          }
-          if (type == kLogFile) {
-            logs_.push_back(number);
-          } else if (type == kTableFile) {
-            table_numbers_.push_back(number);
-          } else {
-            // Ignore other files
-          }
-        }
-      }
-    }
-    return status;
-  }
-    
-        Key pos = RandomTarget(rnd);
-    SkipList<Key, Comparator>::Iterator iter(&list_);
-    iter.Seek(pos);
-    while (true) {
-      Key current;
-      if (!iter.Valid()) {
-        current = MakeKey(K, 0);
-      } else {
-        current = iter.key();
-        ASSERT_TRUE(IsValidKey(current)) << current;
-      }
-      ASSERT_LE(pos, current) << 'should not go backwards';
-    }
-    
-      // Removes a SnapshotImpl from this list.
-  //
-  // The snapshot must have been created by calling New() on this list.
-  //
-  // The snapshot pointer should not be const, because its memory is
-  // deallocated. However, that would force us to change DB::ReleaseSnapshot(),
-  // which is in the API, and currently takes a const Snapshot.
-  void Delete(const SnapshotImpl* snapshot) {
-#if !defined(NDEBUG)
-    assert(snapshot->list_ == this);
-#endif  // !defined(NDEBUG)
-    snapshot->prev_->next_ = snapshot->next_;
-    snapshot->next_->prev_ = snapshot->prev_;
-    delete snapshot;
-  }
-    
-    /*
- * Call a function on each element of `inputs', in parallel.
+        /*!
+ * \brief The result holder of storage type of each NodeEntry in the graph.
+ * \note Stored under graph.attrs['storage_type'], provided by Pass 'InferStorageType'
  *
- * If `func' throws an exception, some of the work will not be
- * attempted.
+ * \code
+ *  Graph g = ApplyPass(src_graph, 'InferStorageType');
+ *  const StorageVector& stypes = g.GetAttr<StorageTypeVector>('storage_type');
+ *  // get storage type by entry id
+ *  int entry_type = stypes[g.indexed_graph().entry_id(my_entry)];
+ * \endcode
+ *
+ * \sa FInferStorageType
  */
-template<class Func, class Item>
-void for_each(const std::vector<Item>& inputs, Func func) {
-  std::atomic<bool> failed{false};
-  std::atomic<size_t> index{0};
+using StorageTypeVector = std::vector<int>;
+    
+    #include <mxnet/op_attr_types.h>
+#include <mxnet/graph_attr_types.h>
+#include <mxnet/c_api.h>
+#include <nnvm/symbolic.h>
+#include <nnvm/op.h>
+#include <nnvm/graph.h>
+#include <vector>
+#include <atomic>
+#include <utility>
+#include <string>
+#include <unordered_map>
+    
+     public:
+  /*! \brief cuda kernel argument descriptor */
+  struct ArgType {
+    /*! \brief whether argument is NDArray */
+    bool is_ndarray;
+    /*! \brief whether argument is constant (input) */
+    bool is_const;
+    /*! \brief data type of argument */
+    mshadow::TypeFlag dtype;
+  };
+  /*! \brief Cuda kernel */
+  class Kernel {
+   public:
+    /*! \brief Launch the kernel */
+    void Launch(const Context& ctx, const std::vector<dmlc::any>& args,
+                uint32_t grid_dim_x, uint32_t grid_dim_y, uint32_t grid_dim_z,
+                uint32_t block_dim_x, uint32_t block_dim_y, uint32_t block_dim_z,
+                uint32_t shared_mem);
+    /*! \brief kernel interface signature */
+    const std::vector<ArgType>& signature() { return signature_; }
     }
     
+      virtual void Backward(const OpContext &ctx,
+                        const std::vector<TBlob> &out_grad,
+                        const std::vector<TBlob> &in_data,
+                        const std::vector<TBlob> &out_data,
+                        const std::vector<OpReqType> &req,
+                        const std::vector<TBlob> &in_grad,
+                        const std::vector<TBlob> &aux_args) {
+    // Set mode before backward
+    caffe::CaffeMode::SetMode<xpu>();
+    using namespace mshadow;
+    using namespace mshadow::expr;
+    CHECK_EQ(out_grad.size(), param_.num_out);
+    for (int i = 0; i < param_.num_data; ++i)
+      CHECK(req[i] != kAddTo) << 'caffe doesn't accm diff on bottom data';
+    CHECK(in_data.size() == param_.num_data);
+    }
     
-    {  template <typename T>
-  RegNumber rn(T r) {
-    return RegNumber(int(r));
+      /*! \brief internal next function, inlined for fater processing. */
+  inline bool Next_(void) {
+    if (!base_->Next()) return false;
+    const DataInst &src = base_->Value();
+    this->SetOutImg(src);
+    out_.data.resize(2);
+    out_.data[0] = outimg_;
+    out_.data[1] = src.data[1];
+    out_.index = src.index;
+    out_.extra_data = src.extra_data;
+    return true;
   }
+  /*!
+   * \brief Set the output image, after augmentation and normalization.
+   * \param src The source image.
+   */
+  inline void SetOutImg(const DataInst &src) {
+    using namespace mshadow::expr;  // NOLINT(*)
+    }
+    
+        template <>
+    /*static*/ NDArrayViewPtr NDArrayView::RandomNormal<float16>(const NDShape& shape, double mean, double stdDev, unsigned long seed, const DeviceDescriptor& device)
+    {
+        return NDArrayView::_RandomNormal<float16, half>(shape, mean, stdDev, seed, device);
+    }
+    
+        template <typename T>
+    inline void ValidateType(const Dictionary& dict, const std::wstring& typeValue, size_t currentVersion)
+    {
+        if (!dict.Contains(typeKey))
+        {
+            const auto& version = GetVersion(dict);
+            LogicError('Required key '%ls' is not found in the dictionary (%s).',
+                       typeKey.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
+        } 
+    }
+    
+            // this ensures that nobody will start writing to the model/checkpoint files, until
+        // everybody is done reading them.
+        DistributedCommunicatorPtr communicator = MPICommunicator();
+        communicator->Barrier();
+    
+        //
+    // Create NDMask for the 'sequences' if the 'sequences' do not have the same length.
+    // It returns null if all the 'sequences' have the same length.
+    //
+    template <typename T>
+    static NDMaskPtr CreateMask(size_t numElementsPerSample, const std::vector<std::vector<T>>& sequences, const std::vector<bool>& sequenceStartFlags, const DeviceDescriptor& device)
+    {
+        size_t numSequences = sequences.size();
+        std::vector<size_t> sequenceLengths(numSequences);
+        for (size_t i = 0; i < numSequences; ++i)
+            sequenceLengths[i] = sequences[i].size() / numElementsPerSample;
+    }
+    
+        private:
+        bool m_isReadOnly;
+        NDShape m_sampleShape;
+        std::vector<Axis> m_sampleDynamicAxes;
+        NDShape m_unpackedShape;
+    
+    #define NOMINMAX
+#include 'Windows.h' // for HANDLE
+    
+    size_t DataReader::GetNumParallelSequencesForFixingBPTTMode()
+{
+    size_t nNbr = 0;
+    for (size_t i = 0; i < m_ioNames.size(); i++)
+    {
+        IDataReader* ptr = m_dataReaders[m_ioNames[i]];
+        if (nNbr == 0)
+            nNbr = ptr->GetNumParallelSequencesForFixingBPTTMode();
+        else if (nNbr != ptr->GetNumParallelSequencesForFixingBPTTMode())
+            LogicError('GetNumParallelSequences: number of slices in each minibatch not consistent for these streams');
+    }
+    return nNbr;
+}
+    
+    class Clock
+{
+public:
+    static long long GetTimeStamp();
+    static long long GetTicksPerSecond();
 };
     
-    #ifndef incl_HPHP_CONFIG_H_
-#define incl_HPHP_CONFIG_H_
-    
-    #endif // HPHP_DATA_STREAM_WRAPPER_H
-
-    
-    
+        // out_of_range.401
+    try
     {
-    {      write(ini_fd, line.c_str(), line.length());
-      write(ini_fd, '\n', 1);
-      cnt += 2;
-      continue;
+        // try to use a an invalid array index
+        json::const_reference ref = j.at('/array/4'_json_pointer);
     }
-    if (argv[cnt][0] != '-') {
-      if (show) {
-        newargv.push_back('-w');
-      } else {
-        newargv.push_back(lint ? '-l' : '-f');
-      }
-      newargv.push_back(argv[cnt++]);
-      need_file = false;
-      break;
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
     }
-    if (strcmp(argv[cnt], '--') == 0) {
-      break;
+    
+    #include <folly/DefaultKeepAliveExecutor.h>
+    
+      std::shared_ptr<T> get() const {
+    folly::hazptr_local<1> hazptr;
+    auto slots = hazptr[0].get_protected(slots_);
+    if (!slots) {
+      return nullptr;
     }
-    cnt++; // skip unknown options
+    return (slots->slots_)[AccessSpreader<>::current(kNumSlots)];
   }
     
-    constexpr int64_t kDefaultPerfWarningRate = 100;
     
-    // note that you can play with the hashing functions all you
-// want without needing to change the decompressor
-#define stb__hc(q,h,c)      (((h) << 7) + ((h) >> 25) + q[c])
-#define stb__hc2(q,h,c,d)   (((h) << 14) + ((h) >> 18) + (q[c] << 7) + q[d])
-#define stb__hc3(q,c,d,e)   ((q[c] << 14) + (q[d] << 7) + q[e])
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplGlfw_NewFrame();
-    
-    // Implemented features:
-//  [X] Renderer: User texture binding. Use 'LPDIRECT3DTEXTURE9' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
-    
-        // Create Descriptor Set:
-    {
-        VkDescriptorSetAllocateInfo alloc_info = {};
-        alloc_info.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
-        alloc_info.descriptorPool = g_DescriptorPool;
-        alloc_info.descriptorSetCount = 1;
-        alloc_info.pSetLayouts = &g_DescriptorSetLayout;
-        err = vkAllocateDescriptorSets(g_Device, &alloc_info, &g_DescriptorSet);
-        check_vk_result(err);
-    }
-    
-        bool hovered, held, pressed = ButtonBehavior(interact_bb, id, &hovered, &held, button_flags);
-    if (!is_leaf)
-    {
-        bool toggled = false;
-        if (pressed)
-        {
-            toggled = !(flags & (ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick)) || (g.NavActivateId == id);
-            if (flags & ImGuiTreeNodeFlags_OpenOnArrow)
-                toggled |= IsMouseHoveringRect(interact_bb.Min, ImVec2(interact_bb.Min.x + text_offset_x, interact_bb.Max.y)) && (!g.NavDisableMouseHover);
-            if (flags & ImGuiTreeNodeFlags_OpenOnDoubleClick)
-                toggled |= g.IO.MouseDoubleClicked[0];
-            if (g.DragDropActive && is_open) // When using Drag and Drop 'hold to open' we keep the node highlighted after opening, but never close it again.
-                toggled = false;
-        }
-    }
+    {  T* get() const {
+    return p_;
+  }
+  T* operator->() const {
+    return p_;
+  }
+  explicit operator bool() const {
+    return p_ == nullptr ? false : true;
+  }
+  bool operator==(const counted_ptr<T, Atom>& p) const {
+    return get() == p.get();
+  }
+};
