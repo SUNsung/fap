f54aa96f89c141c16aab4aebec0b390142d9f1ab
SUNsung <weatherius@gmail.com>
2019-01-21 05:21:23 +0200

    Rewrite [56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp] File in: 2019-01-21 05:21:22

--- cccccc/56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp
+++ cccccc/56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp
@@ -1,125 +1,357 @@
 
         
-        
-    {    if (tmp.handle_data().is_set()) {
-      input_handle_shapes_and_types[i].reset(
-          new std::vector<std::pair<TensorShapeProto, DataType>>);
-      auto& v = *input_handle_shapes_and_types[i];
-      for (const auto& x : tmp.handle_data().shape_and_type()) {
-        v.emplace_back(x.shape(), x.dtype());
+        #include 'atom/app/command_line_args.h'
+    
+    // static
+void AutoUpdater::BuildPrototype(v8::Isolate* isolate,
+                                 v8::Local<v8::FunctionTemplate> prototype) {
+  prototype->SetClassName(mate::StringToV8(isolate, 'AutoUpdater'));
+  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
+      .SetMethod('checkForUpdates', &auto_updater::AutoUpdater::CheckForUpdates)
+      .SetMethod('getFeedURL', &auto_updater::AutoUpdater::GetFeedURL)
+      .SetMethod('setFeedURL', &AutoUpdater::SetFeedURL)
+      .SetMethod('quitAndInstall', &AutoUpdater::QuitAndInstall);
+}
+    
+    
+    {}  // namespace api
+    
+    class Net : public mate::EventEmitter<Net> {
+ public:
+  static v8::Local<v8::Value> Create(v8::Isolate* isolate);
+    }
+    
+     protected:
+  Tray(v8::Isolate* isolate,
+       v8::Local<v8::Object> wrapper,
+       mate::Handle<NativeImage> image);
+  ~Tray() override;
+    
+    // A self-destroyed class for handling save page request.
+class SavePageHandler : public content::DownloadManager::Observer,
+                        public download::DownloadItem::Observer {
+ public:
+  using SavePageCallback = base::Callback<void(v8::Local<v8::Value>)>;
+    }
+    
+    class OpenURIDialog : public QDialog
+{
+    Q_OBJECT
+    }
+    
+    
+    {private Q_SLOTS:
+    /* sign message */
+    void on_addressBookButton_SM_clicked();
+    void on_pasteButton_SM_clicked();
+    void on_signMessageButton_SM_clicked();
+    void on_copySignatureButton_SM_clicked();
+    void on_clearButton_SM_clicked();
+    /* verify message */
+    void on_addressBookButton_VM_clicked();
+    void on_verifyMessageButton_VM_clicked();
+    void on_clearButton_VM_clicked();
+};
+    
+    #include <script/script.h>
+    
+    #include <boost/test/unit_test.hpp>
+    
+    const std::vector<std::string>& UniValue::getKeys() const
+{
+    if (typ != VOBJ)
+        throw std::runtime_error('JSON value is not an object as expected');
+    return keys;
+}
+    
+    #define IPC_MESSAGE_START ShellMsgStart
+    
+      static void Call(content::Shell* shell,
+                   const std::string& method,
+                   const base::ListValue& arguments,
+                   base::ListValue* result,
+                   DispatcherHost* dispatcher_host);
+    
+    namespace {
+RenderView* GetRenderView(v8::Handle<v8::Context> ctx) {
+  WebLocalFrame* frame = WebLocalFrame::frameForContext(ctx);
+  if (!frame || !frame->isNodeJS())
+    return NULL;
+    }
+    }
+    
+      template<typename T> T* AddListener() {
+    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
+    if (i==listerners_.end()) {
+      T* listener_object = new T(this);
+      listerners_[T::id] = listener_object;
+      return listener_object;
+    }
+    return NULL;
+  }
+    
+    void Menu::Call(const std::string& method,
+                const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  if (method == 'Append') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    Append(object_manager()->GetApiObject<MenuItem>(object_id));
+  } else if (method == 'Insert') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Insert(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Remove') {
+    int object_id = 0;
+    arguments.GetInteger(0, &object_id);
+    int pos = 0;
+    arguments.GetInteger(1, &pos);
+    Remove(object_manager()->GetApiObject<MenuItem>(object_id), pos);
+  } else if (method == 'Popup') {
+    int x = 0;
+    arguments.GetInteger(0, &x);
+    int y = 0;
+    arguments.GetInteger(1, &y);
+    content::WebContents* web_contents = content::WebContents::FromRenderFrameHost(rvh);
+    DCHECK(web_contents);
+    zoom::ZoomController* zoom_controller = zoom::ZoomController::FromWebContents(web_contents);
+    }
+    }
+    
+    bool MenuDelegate::HasIcon(int command_id) {
+  if (command_id < 0)
+    return false;
+    }
+    
+    
+    {}  // namespace nw
+    
+    
+    {  menu_model_->RemoveItemAt(pos);
+  menu_items_.erase(menu_items_.begin() + pos);
+  is_menu_modified_ = true;
+  menu_item->menu_ = NULL;
+  menu_item->RemoveKeys();
+}
+    
+    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
+ public:
+  NwClipboardReadAvailableTypesFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+     protected:
+  ~NwObjCallObjectMethodFunction() override;
+    
+    class PAGE_RES_IT;
+class ROW;
+class WERD_RES;
+    
+    /**
+ * Sets up the internal data for iterating the blobs of a new word, then
+ * moves the iterator to the given offset.
+ */
+void PageIterator::BeginWord(int offset) {
+  WERD_RES* word_res = it_->word();
+  if (word_res == nullptr) {
+    // This is a non-text block, so there is no word.
+    word_length_ = 0;
+    blob_index_ = 0;
+    word_ = nullptr;
+    return;
+  }
+  if (word_res->best_choice != nullptr) {
+    // Recognition has been done, so we are using the box_word, which
+    // is already baseline denormalized.
+    word_length_ = word_res->best_choice->length();
+    if (word_res->box_word != nullptr) {
+      if (word_res->box_word->length() != word_length_) {
+        tprintf('Corrupted word! best_choice[len=%d] = %s, box_word[len=%d]: ',
+                word_length_, word_res->best_choice->unichar_string().string(),
+                word_res->box_word->length());
+        word_res->box_word->bounding_box().print();
       }
+      ASSERT_HOST(word_res->box_word->length() == word_length_);
     }
-  }
-    
-    #include <vector>
-#include 'tensorflow/c/tf_status_helper.h'
-#include 'tensorflow/core/platform/types.h'
-    
-    // An example Op.
-    
-    struct Bfloat16EqFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a == b; }
-};
-struct Bfloat16NeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a != b; }
-};
-struct Bfloat16LtFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a < b; }
-};
-struct Bfloat16GtFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a > b; }
-};
-struct Bfloat16LeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a <= b; }
-};
-struct Bfloat16GeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a >= b; }
-};
-    
-    
-    {}  // namespace tensorflow
-    
-    
-    {      // Destructor responsible for releasing the memory.
-      std::function<void()>* destructor;
-};
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    namespace tensorflow {
-    }
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-        // Adds the module variable 'api_version'.
-    if (PyModule_AddIntConstant(
-        module,
-        const_cast<char*>(kImplVersionName),
-        kImplVersion))
-#if PY_MAJOR_VERSION < 3
-      return;
-#else
-      { Py_DECREF(module); return NULL; }
-    
-      // Deletes the current owned object, if any.
-  // Then takes ownership of a new object without incrementing the reference
-  // count.
-  // This function must be called with a reference that you own.
-  //   this->reset(this->get()) is wrong!
-  //   this->reset(this->release()) is OK.
-  PyObjectStruct* reset(PyObjectStruct* p = NULL) {
-    Py_XDECREF(ptr_);
-    ptr_ = p;
-    return ptr_;
-  }
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-    }
-    }
-    }
-    }
-    
-      if (GenerateCode(request, *generator, &response, &error_msg)) {
-    if (!response.SerializeToFileDescriptor(STDOUT_FILENO)) {
-      std::cerr << argv[0] << ': Error writing to stdout.' << std::endl;
-      return 1;
-    }
+    word_ = nullptr;
+    // We will be iterating the box_word.
+    delete cblob_it_;
+    cblob_it_ = nullptr;
   } else {
-    if (!error_msg.empty()) {
-      std::cerr << argv[0] << ': ' << error_msg << std::endl;
-    }
-    return 1;
+    // No recognition yet, so a 'symbol' is a cblob.
+    word_ = word_res->word;
+    ASSERT_HOST(word_->cblob_list() != nullptr);
+    word_length_ = word_->cblob_list()->length();
+    if (cblob_it_ == nullptr) cblob_it_ = new C_BLOB_IT;
+    cblob_it_->set_to_list(word_->cblob_list());
   }
+  for (blob_index_ = 0; blob_index_ < offset; ++blob_index_) {
+    if (cblob_it_ != nullptr)
+      cblob_it_->forward();
+  }
+}
     
-      reflection_tester.SetPackedFieldsViaReflection(message);
-  reflection_tester.ExpectPackedFieldsSetViaReflection(*message);
-    
-    OPERATOR_SCHEMA(FindDuplicateElements)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-The *FindDuplicateElements* op takes a single 1-D tensor *data* as input and returns a single 1-D output tensor *indices*. The output tensor contains the indices of the duplicate elements of the input, excluding the first occurrences. If all elements of *data* are unique, *indices* will be empty.
-    
-    namespace caffe2 {
+    struct BlobData {
+  BlobData() : blob(nullptr), choices(nullptr) {}
+  BlobData(int index, Tesseract* tess, const WERD_RES& word)
+    : blob(word.chopped_word->blobs[index]),
+      tesseract(tess),
+      choices(&(*word.ratings)(index, index)) {}
     }
     
-    class GetFloatToHalfGradient : public GradientMakerBase {
-  using GradientMakerBase::GradientMakerBase;
-  vector<OperatorDef> GetGradientDefs() override {
-    return SingleGradientDef(
-        'HalfToFloat', '', vector<string>{GO(0)}, vector<string>{GI(0)});
-  }
-};
-REGISTER_GRADIENT(FloatToHalf, GetFloatToHalfGradient);
     
-    	SetEnvironmentVariable(L'CMDER_ROOT', exeDir);
-	if (wcscmp(userConfigDirPath, configDirPath) != 0)
-	{
-		SetEnvironmentVariable(L'CMDER_USER_CONFIG', userConfigDirPath);
-		SetEnvironmentVariable(L'CMDER_USER_BIN', userBinDirPath);
-	}
+    {  // Do sanity checks and minor fixes on best_choice.
+  if (word->best_choice->length() > word_length) {
+    word->best_choice->make_bad();  // should never happen
+    tprintf('recog_word: Discarded long string \'%s\''
+            ' (%d characters vs %d blobs)\n',
+            word->best_choice->unichar_string().string(),
+            word->best_choice->length(), word_length);
+    tprintf('Word is at:');
+    word->word->bounding_box().print();
+  }
+  if (word->best_choice->length() < word_length) {
+    UNICHAR_ID space_id = unicharset.unichar_to_id(' ');
+    while (word->best_choice->length() < word_length) {
+      word->best_choice->append_unichar_id(space_id, 1, 0.0,
+                                           word->best_choice->certainty());
+    }
+  }
+}
+    
+    #include 'blread.h'
+#include <cstdio>       // for fclose, fopen, FILE
+#include 'host.h'       // for TRUE
+#include 'ocrblock.h'   // for BLOCK_IT, BLOCK, BLOCK_LIST (ptr only)
+#include 'scanutils.h'  // for tfscanf
+    
+      // Other accessors.
+  const STRING& imagefilename() const {
+    return imagefilename_;
+  }
+  void set_imagefilename(const STRING& name) {
+    imagefilename_ = name;
+  }
+  int page_number() const {
+    return page_number_;
+  }
+  void set_page_number(int num) {
+    page_number_ = num;
+  }
+  const GenericVector<char>& image_data() const {
+    return image_data_;
+  }
+  const STRING& language() const {
+    return language_;
+  }
+  void set_language(const STRING& lang) {
+    language_ = lang;
+  }
+  const STRING& transcription() const {
+    return transcription_;
+  }
+  const GenericVector<TBOX>& boxes() const {
+    return boxes_;
+  }
+  const GenericVector<STRING>& box_texts() const {
+    return box_texts_;
+  }
+  const STRING& box_text(int index) const {
+    return box_texts_[index];
+  }
+  // Saves the given Pix as a PNG-encoded string and destroys it.
+  void SetPix(Pix* pix);
+  // Returns the Pix image for *this. Must be pixDestroyed after use.
+  Pix* GetPix() const;
+  // Gets anything and everything with a non-nullptr pointer, prescaled to a
+  // given target_height (if 0, then the original image height), and aligned.
+  // Also returns (if not nullptr) the width and height of the scaled image.
+  // The return value is the scaled Pix, which must be pixDestroyed after use,
+  // and scale_factor (if not nullptr) is set to the scale factor that was applied
+  // to the image to achieve the target_height.
+  Pix* PreScale(int target_height, int max_height, float* scale_factor,
+                int* scaled_width, int* scaled_height,
+                GenericVector<TBOX>* boxes) const;
+    
+            LearnerRMSProp(const std::vector<Parameter>& parameters,
+                       const LearningRateSchedule& learningRateSchedule,
+                       double gamma, double inc, double dec, double max, double min,
+                       bool needAveMultiplier,
+                       AdditionalLearningOptions additionalOptions);
+    
+        bool Trainer::TrainMinibatch(const std::unordered_map<Variable, ValuePtr>& arguments, bool isSweepEndInArguments, std::unordered_map<Variable, ValuePtr>& outputsToFetch, const DeviceDescriptor& computeDevice /*= DeviceDescriptor::UseDefaultDevice()*/)
+    {
+#ifndef  CNTK_UWP
+        auto profMinibatch = Microsoft::MSR::CNTK::ScopeProfile(Microsoft::MSR::CNTK::profilerEvtMainMinibatch);
+#endif
+    }
+    
+    #pragma once
+    
+    static inline size_t rand(const size_t begin, const size_t end)
+{
+    const size_t randno = ::rand() * RAND_MAX + ::rand(); // BUGBUG: still only covers 32-bit range
+    return begin + randno % (end - begin);
+}
+    
+        // firstSeq - first sequence of samples
+    // secondSeq - second sequence of samples
+    // numParallelSequences - number of parallel sequences in the minibatch
+    // subPen - substitution penalty
+    // delPen - deletion penalty
+    // insPen - insertion penalty
+    // squashInputs - whether to merge sequences of identical samples.
+    // tokensToIgnore - list of samples to ignore during edit distance evaluation
+    ElemType ComputeEditDistanceError(Matrix<ElemType>& firstSeq, const Matrix<ElemType> & secondSeq, MBLayoutPtr pMBLayout, 
+        float subPen, float delPen, float insPen, bool squashInputs, const vector<size_t>& tokensToIgnore)
+    {
+        std::vector<int> firstSeqVec, secondSeqVec;
+    }
+    
+        virtual void /*ComputationNode::*/ ForwardProp(const FrameRange&) override
+    {
+        RuntimeError('%ls is a special node only to be used as input to the Input() node.', NodeDescription().c_str());
+    }
+    
+    #pragma once
+    
+    #include <osquery/config/tests/test_utils.h>
+#include <osquery/database.h>
+#include <osquery/filesystem/filesystem.h>
+#include <osquery/registry_interface.h>
+#include <osquery/utils/conversions/tryto.h>
+    
+     protected:
+  /// Calculate the URL once and cache the result.
+  std::string uri_;
+    
+    
+    // exception type_error.304
+    try
+    {
+        // use at() on a non-array type
+        json str = 'I am a string';
+        str.at(0) = 'Another string';
+    }
+    catch (json::type_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+        // serialize the JSON numbers
+    std::cout << j_integer_t << '\n';
+    std::cout << j_unsigned_t << '\n';
+    std::cout << j_enum << '\n';
+    std::cout << j_short << '\n';
+    std::cout << j_int << '\n';
+    std::cout << j_long << '\n';
+    std::cout << j_int_least32_t << '\n';
+    std::cout << j_uint8_t << '\n';
+    std::cout << j_ok << '\n';
+    std::cout << j_nan << '\n';
+    std::cout << j_infinity << '\n';
+    std::cout << j_float << '\n';
+    std::cout << j_float_nan << '\n';
+    std::cout << j_double << '\n\n';
