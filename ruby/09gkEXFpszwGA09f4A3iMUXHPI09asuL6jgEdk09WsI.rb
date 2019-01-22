4b11e3f3b6e56b152456a2d3c057bd08c3d08de8
SUNsung <weatherius@gmail.com>
2019-01-22 10:38:02 +0200

    Rewrite [09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb] File in: 2019-01-22 10:38:02

--- ruby/09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb
+++ ruby/09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb
@@ -1,225 +1,58 @@
 
         
-              # The minimum number of requests we want to keep available.
-      #
-      # We don't use a value of 0 as multiple threads may be using the same
-      # token in parallel. This could result in all of them hitting the GitHub
-      # rate limit at once. The threshold is put in place to not hit the limit
-      # in most cases.
-      RATE_LIMIT_THRESHOLD = 50
+              Find.prune if @f.skip_clean? path
     
-          # project - An instance of `Project`.
-      # object - The object to look up or set a database ID for.
-      def initialize(project, object)
-        @project = project
-        @object = object
-      end
-    
-            Caching.set_includes?(already_imported_cache_key, id)
-      end
-    
-              hash = {
-            iid: issue.number,
-            title: issue.title,
-            description: issue.body,
-            milestone_number: issue.milestone&.number,
-            state: issue.state == 'open' ? :opened : :closed,
-            assignees: issue.assignees.map do |u|
-              Representation::User.from_api_response(u)
-            end,
-            label_names: issue.labels.map(&:name),
-            author: user,
-            created_at: issue.created_at,
-            updated_at: issue.updated_at,
-            pull_request: issue.pull_request ? true : false
-          }
-    
-      # Provides the uri of the redirection location.
-  #
-  # @return [URI] the uri of the redirection location.
-  # @return [nil] if the response hasn't a Location header or it isn't a valid uri.
-  def redirection
-    begin
-      URI(headers['Location'])
-    rescue ::URI::InvalidURIError
-      nil
-    end
+      def self.class_s(name)
+    Formulary.class_s(name)
   end
     
-        when IAX_TYPE_CONTROL
-      case stype
-      when IAX_CTRL_HANGUP
-        dprint('HANGUP')
-        self.client.send_ack(self)
-        self.state = :hangup
-    
-      #
-  # Move these into an IPMI stack or mixin at some point
-  #
-    
-              # Encodes the options field
-          #
-          # @return [OpenSSL::ASN1::BitString]
-          def encode_options
-            OpenSSL::ASN1::BitString.new([options].pack('N'))
-          end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos AuthorizationData data
-        # definition.
-        class AuthorizationData < Element
-          # @!attribute elements
-          #   @return [Hash{Symbol => <Integer, String>}] The type of the authorization data
-          #   @option [Integer] :type
-          #   @option [String] :data
-          attr_accessor :elements
-    
-              def self.decode(input)
-            elem = self.new
-            elem.decode(input)
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
-    
-              # Encodes the req_body field
-          #
-          # @return [String]
-          def encode_req_body
-            req_body.encode
-          end
-    
-              # Decodes the nonce field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_nonce(input)
-            input.value[0].value.to_i
-          end
-    
-              # Rex::Proto::Kerberos::Model::KdcResponse encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'KdcResponse encoding not supported'
-          end
-    
-              # Decodes the stime field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_stime(input)
-            input.value[0].value
-          end
-    
-              # Decodes the key_type from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_type(input)
-            input.value[0].value.to_i
-          end
-    
-        def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
-    end
+      # Disable Rails's static asset server (Apache or nginx will already do this).
+  if config.respond_to?(:serve_static_files)
+    # rails >= 4.2
+    config.serve_static_files = true
+  elsif config.respond_to?(:serve_static_assets)
+    # rails < 4.2
+    config.serve_static_assets = true
   end
-end
-    
-      # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
     
       # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
     
-    module NavigationHelpers
-  def path_to(page_name)
-    case page_name
-    when /^person_photos page$/
-      person_photos_path(@me.person)
-    when /^the home(?: )?page$/
-      stream_path
-    when /^the mobile path$/
-      force_mobile_path
-    when /^the user applications page$/
-      api_openid_connect_user_applications_path
-    when /^the tag page for '([^\']*)'$/
-      tag_path(Regexp.last_match(1))
-    when /^its ([\w ]+) page$/
-      send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path', @it)
-    when /^the mobile ([\w ]+) page$/
-      public_send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path', format: 'mobile')
-    when /^the ([\w ]+) page$/
-      public_send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path')
-    when /^my edit profile page$/
-      edit_profile_path
-    when /^my profile page$/
-      person_path(@me.person)
-    when /^my acceptance form page$/
-      invite_code_path(InvitationCode.first)
-    when /^the requestors profile$/
-      person_path(Request.where(recipient_id: @me.person.id).first.sender)
-    when /^'([^\']*)''s page$/
-      p = User.find_by_email(Regexp.last_match(1)).person
-      {path:         person_path(p),
-       # '#diaspora_handle' on desktop, '.description' on mobile
-       special_elem: {selector: '#diaspora_handle, .description', text: p.diaspora_handle}
-      }
-    when /^'([^\']*)''s photos page$/
-      p = User.find_by_email(Regexp.last_match(1)).person
-      person_photos_path p
-    when /^my account settings page$/
-      edit_user_path
-    when /^forgot password page$/
-      new_user_password_path
-    when %r{^'(/.*)'}
-      Regexp.last_match(1)
-    else
-      raise 'Can't find mapping from \'#{page_name}\' to a path.'
-    end
-  end
+      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
+  gem.executables   = %w(cap capify)
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ['lib']
     
-          it_behaves_like 'on a visible post'
-    end
+    Then(/^the task is successful$/) do
+  expect(@success).to be true
+end
     
-        # The environment of the caller of this environment's mixin or function.
-    # @return {Environment?}
-    def caller
-      @caller || (@parent && @parent.caller)
-    end
-    
-        # Set an option for specifying `Encoding.default_external`.
-    #
-    # @param opts [OptionParser]
-    def encoding_option(opts)
-      encoding_desc = 'Specify the default encoding for input files.'
-      opts.on('-E', '--default-encoding ENCODING', encoding_desc) do |encoding|
-        Encoding.default_external = encoding
+        def any?(key)
+      value = fetch(key)
+      if value && value.respond_to?(:any?)
+        begin
+          return value.any?
+        rescue ArgumentError # rubocop:disable Lint/HandleExceptions
+          # Gracefully ignore values whose `any?` method doesn't accept 0 args
+        end
       end
-    end
     
-        def initialize(options)
-      @strictly_ordered_queues = !!options[:strict]
-      @queues = options[:queues].map { |q| 'queue:#{q}' }
-      if @strictly_ordered_queues
-        @queues = @queues.uniq
-        @queues << TIMEOUT
+          def call
+        ask_question
+        value_or_default
       end
-    end
     
-        def self.job_hash_context(job_hash)
-      # If we're using a wrapper class, like ActiveJob, use the 'wrapped'
-      # attribute to expose the underlying thing.
-      klass = job_hash['wrapped'] || job_hash['class']
-      bid = job_hash['bid']
-      '#{klass} JID-#{job_hash['jid']}#{' BID-#{bid}' if bid}'
-    end
-    
-        NAMED_SEGMENTS_PATTERN = /\/([^\/]*):([^\.:$\/]+)/
+          def add_host(host, properties={})
+        new_host = Server[host]
+        new_host.port = properties[:port] if properties.key?(:port)
+        # This matching logic must stay in sync with `Server#matches?`.
+        key = ServerKey.new(new_host.hostname, new_host.port)
+        existing = servers_by_key[key]
+        if existing
+          existing.user = new_host.user if new_host.user
+          existing.with(properties)
+        else
+          servers_by_key[key] = new_host.with(properties)
+        end
+      end
