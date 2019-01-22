05466faee58f6ad705a32cc97f535accf6cf9d29
SUNsung <weatherius@gmail.com>
2019-01-22 13:01:36 +0200

    Rewrite [42su1rsFNnm7s42rtXhZ4Rge.426lHZdxIfJvU42ex6.rb] File in: 2019-01-22 13:01:35

--- ruby/42su1rsFNnm7s42rtXhZ4Rge.426lHZdxIfJvU42ex6.rb
+++ ruby/42su1rsFNnm7s42rtXhZ4Rge.426lHZdxIfJvU42ex6.rb
@@ -1,157 +1,103 @@
 
         
-              it 'deactivates an existing user' do
-        visit admin_users_path
-        expect(page).to have_no_text('inactive')
-        find(:css, 'a[href='/admin/users/#{users(:bob).id}/deactivate']').click
-        expect(page).to have_text('inactive')
-        users(:bob).reload
-        expect(users(:bob)).not_to be_active
+          def std?
+    @settings.include? :std
+  end
+    
+      def caveats
+    caveats = []
+    begin
+      build, f.build = f.build, Tab.for_formula(f)
+      s = f.caveats.to_s
+      caveats << s.chomp + '\n' if s.length > 0
+    ensure
+      f.build = build
+    end
+    caveats << keg_only_text
+    caveats << bash_completion_caveats
+    caveats << zsh_completion_caveats
+    caveats << fish_completion_caveats
+    caveats << plist_caveats
+    caveats << python_caveats
+    caveats << app_caveats
+    caveats << elisp_caveats
+    caveats.compact.join('\n')
+  end
+    
+            $stderr.puts
+        opoo out
+        Homebrew.failed = true
+        first_warning = false
       end
+    end
     
-        it 'returns a label 'No' if a given agent is not working' do
-      stub(@agent).working? { false }
-      label = working(@agent)
-      expect(label).to be_html_safe
-      expect(Nokogiri(label).text).to eq 'No'
+      def filtered_list
+    names = if ARGV.named.empty?
+      Formula.racks
+    else
+      ARGV.named.map { |n| HOMEBREW_CELLAR+n }.select(&:exist?)
+    end
+    if ARGV.include? '--pinned'
+      pinned_versions = {}
+      names.each do |d|
+        keg_pin = (HOMEBREW_LIBRARY/'PinnedKegs'/d.basename.to_s)
+        if keg_pin.exist? || keg_pin.symlink?
+          pinned_versions[d] = keg_pin.readlink.basename.to_s
+        end
+      end
+      pinned_versions.each do |d, version|
+        puts '#{d.basename}'.concat(ARGV.include?('--versions') ? ' #{version}' : '')
+      end
+    else # --versions without --pinned
+      names.each do |d|
+        versions = d.subdirs.map { |pn| pn.basename.to_s }
+        next if ARGV.include?('--multiple') && versions.length < 2
+        puts '#{d.basename} #{versions*' '}'
+      end
     end
   end
+end
     
-        it 'should work with nested arrays' do
-      @agent.options['array'] = ['one', '$.two']
-      LiquidMigrator.convert_all_agent_options(@agent)
-      expect(@agent.reload.options).to eq({'auth_token' => 'token', 'color' => 'yellow', 'array' => ['one', '{{two}}'], 'notify' => false, 'room_name' => 'test', 'username' => '{{username}}', 'message' => '{{message}}'})
-    end
+    class Formula
+  include FormulaCompat
+  extend FormulaCompat
     
-        it 'should require credentials' do
-      @checker.options['api_key'] = nil
-      expect(@checker).not_to be_valid
-    end
-  end
-    
-          return unless user == 'caskroom'
-    
-    =begin
- +------+----------------+-------------------------------------------+
-   | HEX  | NAME           | DESCRIPTION                               |
-   +------+----------------+-------------------------------------------+
-   | HEX  | NAME           | DESCRIPTION                               |
-   | 0x01 | CALLED NUMBER  | Number/extension being called             |
-   | 0x02 | CALLING NUMBER | Calling number                            |
-   | 0x03 | CALLING ANI    | Calling number ANI for billing            |
-   | 0x04 | CALLING NAME   | Name of caller                            |
-   | 0x05 | CALLED CONTEXT | Context for number                        |
-   | 0x06 | USERNAME       | Username (peer or user) for               |
-   |      |                | authentication                            |
-   | 0x07 | PASSWORD       | Password for authentication               |
-   | 0x08 | CAPABILITY     | Actual CODEC capability                   |
-   | 0x09 | FORMAT         | Desired CODEC format                      |
-   | 0x0a | LANGUAGE       | Desired language                          |
-   | 0x0b | VERSION        | Protocol version                          |
-   | 0x0c | ADSICPE        | CPE ADSI capability                       |
-   | 0x0d | DNID           | Originally dialed DNID                    |
-   | 0x0e | AUTHMETHODS    | Authentication method(s)                  |
-   | 0x0f | CHALLENGE      | Challenge data for MD5/RSA                |
-   | 0x10 | MD5 RESULT     | MD5 challenge result                      |
-   | 0x11 | RSA RESULT     | RSA challenge result                      |
-   | 0x12 | APPARENT ADDR  | Apparent address of peer                  |
-   | 0x13 | REFRESH        | When to refresh registration              |
-   | 0x14 | DPSTATUS       | Dialplan status                           |
-   | 0x15 | CALLNO         | Call number of peer                       |
-   | 0x16 | CAUSE          | Cause                                     |
-   | 0x17 | IAX UNKNOWN    | Unknown IAX command                       |
-   | 0x18 | MSGCOUNT       | How many messages waiting                 |
-   | 0x19 | AUTOANSWER     | Request auto-answering                    |
-   | 0x1a | MUSICONHOLD    | Request musiconhold with QUELCH           |
-   | 0x1b | TRANSFERID     | Transfer Request Identifier               |
-   | 0x1c | RDNIS          | Referring DNIS                            |
-   | 0x1d | Reserved       | Reserved for future use                   |
-   | 0x1e | Reserved       | Reserved for future use                   |
-   | 0x1f | DATETIME       | Date/Time                                 |
-   | 0x20 | Reserved       | Reserved for future use                   |
-   | 0x21 | Reserved       | Reserved for future use                   |
-   | 0x22 | Reserved       | Reserved for future use                   |
-   | 0x23 | Reserved       | Reserved for future use                   |
-   | 0x24 | Reserved       | Reserved for future use                   |
-   | 0x25 | Reserved       | Reserved for future use                   |
-   | 0x26 | CALLINGPRES    | Calling presentation                      |
-   | 0x27 | CALLINGTON     | Calling type of number                    |
-   | 0x28 | CALLINGTNS     | Calling transit network select            |
-   | 0x29 | SAMPLINGRATE   | Supported sampling rates                  |
-   | 0x2a | CAUSECODE      | Hangup cause                              |
-   | 0x2b | ENCRYPTION     | Encryption format                         |
-   | 0x2c | ENCKEY         | Reserved for future Use                   |
-   | 0x2d | CODEC PREFS    | CODEC Negotiation                         |
-   | 0x2e | RR JITTER      | Received jitter, as in RFC 3550           |
-   | 0x2f | RR LOSS        | Received loss, as in RFC 3550             |
-   | 0x30 | RR PKTS        | Received frames                           |
-   | 0x31 | RR DELAY       | Max playout delay for received frames in  |
-   |      |                | ms                                        |
-   | 0x32 | RR DROPPED     | Dropped frames (presumably by jitter      |
-   |      |                | buffer)                                   |
-   | 0x33 | RR OOO         | Frames received Out of Order              |
-   | 0x34 | OSPTOKEN       | OSP Token Block                           |
-   +------+----------------+-------------------------------------------+
-=end
-    
-            # Sends a Kerberos Request over a tcp connection
+            # This contains all the command plugins by name, and returns
+        # the command class and options. The command class is wrapped
+        # in a Proc so that it can be lazy loaded.
         #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [Integer] the number of bytes sent
-        # @raise [RuntimeError] if the request can't be encoded
-        def send_request_tcp(req)
-          data = req.encode
-          length = [data.length].pack('N')
-          connection.put(length + data)
-        end
+        # @return [Registry<Symbol, Array<Proc, Hash>>]
+        attr_reader :commands
     
-              # Encodes the value field
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_value
-            OpenSSL::ASN1::OctetString.new(value)
-          end
-        end
+    class SinatraStaticServer < Sinatra::Base
+    
+          # Create an Atom-feed for each index.
+      feed = CategoryFeed.new(self, self.source, category_dir, category)
+      feed.render(self.layouts, site_payload)
+      feed.write(self.dest)
+      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
+      self.pages << feed
+    end
+    
+        def get_web_content(url)
+      raw_uri           = URI.parse url
+      proxy             = ENV['http_proxy']
+      if proxy
+        proxy_uri       = URI.parse(proxy)
+        https           = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new raw_uri.host, raw_uri.port
+      else
+        https           = Net::HTTP.new raw_uri.host, raw_uri.port
       end
+      https.use_ssl     = true
+      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request           = Net::HTTP::Get.new raw_uri.request_uri
+      data              = https.request request
     end
   end
-end
     
-              # Decodes the ticket field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Type::Ticket]
-          def decode_ticket(input)
-            Rex::Proto::Kerberos::Model::Ticket.decode(input.value[0])
-          end
-    
-        class Main < Clamp::Command
-      subcommand 'list', 'List all installed Logstash plugins', LogStash::PluginManager::List
-      subcommand 'install', 'Install a Logstash plugin', LogStash::PluginManager::Install
-      subcommand 'remove', 'Remove a Logstash plugin', LogStash::PluginManager::Remove
-      subcommand 'update', 'Update a plugin', LogStash::PluginManager::Update
-      subcommand 'pack', 'Package currently installed plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Pack
-      subcommand 'unpack', 'Unpack packaged plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Unpack
-      subcommand 'generate', 'Create the foundation for a new plugin', LogStash::PluginManager::Generate
-      subcommand 'uninstall', 'Uninstall a plugin. Deprecated: Please use remove instead', LogStash::PluginManager::Remove
-      subcommand 'prepare-offline-pack', 'Create an archive of specified plugins to use for offline installation', LogStash::PluginManager::PrepareOfflinePack
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
     end
-  end
-end
-    
-            if specs.size > 0
-          specs
-        else
-          raise LogStash::PluginManager::PluginNotFoundError, 'Cannot find plugins matching: `#{plugin_pattern}`'
-        end
-      end.flatten
-    end
-    
-        context 'update all the plugins' do
-      it 'has executed successfully' do
-        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
-        expect(logstash).to have_installed?(plugin_name, '0.1.1')
-      end
-    end
-  end
-end
