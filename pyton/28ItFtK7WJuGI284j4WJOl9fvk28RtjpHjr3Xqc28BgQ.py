66d9eeab03208a0f6b05dd02e51bca74e052dd70
SUNsung <weatherius@gmail.com>
2019-01-22 12:37:06 +0200

    Rewrite [28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py] File in: 2019-01-22 12:37:05

--- pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
+++ pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
@@ -1,181 +1,102 @@
 
         
-            def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
+        from utils import write_datasets
+from synthetic_data_utils import add_alignment_projections, generate_data
+from synthetic_data_utils import generate_rnn, get_train_n_valid_inds
+from synthetic_data_utils import nparray_and_transpose
+from synthetic_data_utils import spikify_data, gaussify_data, split_list_by_inds
+import matplotlib
+import matplotlib.pyplot as plt
+import scipy.signal
     
-    from mrjob.job import MRJob
+    rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
+rnn_rngs = [np.random.RandomState(seed=FLAGS.synth_data_seed+1),
+            np.random.RandomState(seed=FLAGS.synth_data_seed+2)]
+T = FLAGS.T
+C = FLAGS.C
+N = FLAGS.N
+nreplications = FLAGS.nreplications
+E = nreplications * C
+train_percentage = FLAGS.train_percentage
+ntimesteps = int(T / FLAGS.dt)
     
-        def bfs(self, source, dest):
-        if source is None:
-            return False
-        queue = deque()
-        queue.append(source)
-        source.visit_state = State.visited
-        while queue:
-            node = queue.popleft()
-            print(node)
-            if dest is node:
-                return True
-            for adjacent_node in node.adj_nodes.values():
-                if adjacent_node.visit_state == State.unvisited:
-                    queue.append(adjacent_node)
-                    adjacent_node.visit_state = State.visited
-        return False
+      # In the data workup in the paper, Chethan did intra condition
+  # averaging, so let's do that here.
+  avg_data_all = {}
+  for name, conditions in conditions_all.items():
+    dataset = datasets[name]
+    avg_data_all[name] = {}
+    for cname in conditions:
+      td_idxs = np.argwhere(np.array(dataset['condition_labels_train'])==cname)
+      data = np.squeeze(dataset['train_data'][td_idxs,:,:], axis=1)
+      avg_data = np.mean(data, axis=0)
+      avg_data_all[name][cname] = avg_data
+    
+          elif FLAGS.generator_model.startswith('seq2seq'):
+        load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
+        print('Restoring Generator from %s.' % load_ckpt)
+        tf.logging.info('Restoring Generator from %s.' % load_ckpt)
+        gen_encoder_init_saver = init_savers['gen_encoder_init_saver']
+        gen_decoder_init_saver = init_savers['gen_decoder_init_saver']
+        gen_encoder_init_saver.restore(sess, load_ckpt)
+        gen_decoder_init_saver.restore(sess, load_ckpt)
+    
+      if not FLAGS.dis_share_embedding:
+    variable_mapping = {
+        str(model_str) + '/embedding':
+            decoder_embedding,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+            decoder_lstm_w_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+            decoder_lstm_b_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+            decoder_lstm_w_1,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+            decoder_lstm_b_1
+    }
+  else:
+    variable_mapping = {
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+            decoder_lstm_w_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+            decoder_lstm_b_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+            decoder_lstm_w_1,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+            decoder_lstm_b_1,
+    }
+  return variable_mapping
     
     
-class Node(object):
-    
-    x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-    
-        paths = []
-    for fname in files:
-        paths.append(get_file(fname,
-                              origin=base + fname,
-                              cache_subdir=dirname))
-    
-        model._check_trainable_weights_consistency()
-    if hasattr(model, '_collected_trainable_weights'):
-        trainable_count = count_params(model._collected_trainable_weights)
-    else:
-        trainable_count = count_params(model.trainable_weights)
-    
-        outputs1 = Lambda(lambda x: utils.preprocess_input(x, 'channels_last'),
-                      output_shape=x.shape[1:])(inputs)
-    model1 = Model(inputs, outputs1)
-    out1 = model1.predict(x)
-    x2 = np.transpose(x, (0, 3, 1, 2))
-    inputs2 = Input(shape=x2.shape[1:])
-    outputs2 = Lambda(lambda x: utils.preprocess_input(x, 'channels_first'),
-                      output_shape=x2.shape[1:])(inputs2)
-    model2 = Model(inputs2, outputs2)
-    out2 = model2.predict(x2)
-    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
-    
-    
-def test_reuters():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = reuters.load_data()
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        assert len(x_train) + len(x_test) == 11228
-        (x_train, y_train), (x_test, y_test) = reuters.load_data(maxlen=10)
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        word_index = reuters.get_word_index()
-        assert isinstance(word_index, dict)
-    
-        model = create_multi_input_model_from(*models)
-    model.compile(loss='categorical_crossentropy', optimizer='sgd')
-    assert len(model.losses) == 8
-    
-        # Arguments
-        y: class vector to be converted into a matrix
-            (integers from 0 to num_classes).
-        num_classes: total number of classes.
-        dtype: The data type expected by the input, as a string
-            (`float32`, `float64`, `int32`...)
-    
-            if self.use_bias:
-            self.bias_i = self.bias[:self.filters]
-            self.bias_f = self.bias[self.filters: self.filters * 2]
-            self.bias_c = self.bias[self.filters * 2: self.filters * 3]
-            self.bias_o = self.bias[self.filters * 3:]
+def rmtree(path):
+    for name in os.listdir(path):
+        fname = os.path.join(path, name)
+        if os.path.isdir(fname):
+            rmtree(fname)
         else:
-            self.bias_i = None
-            self.bias_f = None
-            self.bias_c = None
-            self.bias_o = None
-        self.built = True
-    
-    Settings: horizontal_flip = True
-----------------------------------------------------------------------------
-Epoch     | ImageGenerator | ImageGenerator | AugmentLayer  | Augment Layer
-Number    | %Accuracy      | Performance    | %Accuracy     | Performance
-----------------------------------------------------------------------------
-1         | 44.84          | 15ms/step      | 45.54         | 358us/step
-2         | 52.34          |  8ms/step      | 50.55         | 285us/step
-8         | 65.45          |  8ms/step      | 65.59         | 281us/step
-25        | 76.74          |  8ms/step      | 76.17         | 280us/step
-100       | 78.81          |  8ms/step      | 78.70         | 285us/step
----------------------------------------------------------------------------
+            os.chmod(fname, 0o666)
+            os.remove(fname)
+    os.rmdir(path)
     
     
-def create_model(stateful):
-    model = Sequential()
-    model.add(LSTM(20,
-              input_shape=(lahead, 1),
-              batch_size=batch_size,
-              stateful=stateful))
-    model.add(Dense(1))
-    model.compile(loss='mse', optimizer='adam')
-    return model
+parser = youtube_dl.parseOpts()[0]
+build_completion(parser)
+
     
-        if '_text' in dictified['video'][0]['size'][0]:  #size exists for 1 piece
-        video_dict['size'] = sum([int(i['size'][0]['_text']) for i in dictified['video']])
+    signature = hexlify(rsa.pkcs1.sign(json.dumps(versions_info, sort_keys=True).encode('utf-8'), privkey, 'SHA-256')).decode()
+print('signature: ' + signature)
     
-    
-def coub_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_content(url)
-    
-        for quality in ['1080','720','480','380','240','144','auto']:
-        try:
-            real_url = info[quality][1]['url']
-            if real_url:
-                break
-        except KeyError:
-            pass
-    
-        if int(html['status']) != 100000:
-        raise Exception('API error!')
-    
-    def get_single_photo_url(url):
-    page = get_html(url)
-    pid = get_photo_id(url, page)
-    title = match1(page, pattern_inline_title)
-    if match1(page, pattern_inline_video_mark):
-        api_key = get_api_key(page)
-        reply = get_content(tmpl_api_call_photo_info % (api_key, get_photo_id(url, page)))
-        secret = json.loads(reply)['photo']['secret']
-        return get_orig_video_source(api_key, pid, secret), title
-    #last match always has the best resolution
-    match = match1(page, pattern_inline_img_url)
-    return 'https:' + match.replace('\\', ''), title
-    
-        output_directory = 'pdfs' if results.directory is None else results.directory
+        ies = sorted(youtube_dl.gen_extractors(), key=lambda i: i.IE_NAME.lower())
+    out = '# Supported sites\n' + ''.join(
+        ' - ' + md + '\n'
+        for md in gen_ies_md(ies))
     
     
-def html_page_context(app, pagename, templatename, context, doctree):
-    if templatename != 'page.html':
-        return
+def build_completion(opt_parser):
+    opts = [opt for group in opt_parser.option_groups
+            for opt in group.option_list]
+    opts_file = [opt for opt in opts if opt.metavar == 'FILE']
+    opts_dir = [opt for opt in opts if opt.metavar == 'DIR']
     
-    
-Device = namedtuple('Device', ['mac', 'ip', 'last_update'])
-    
-        def random_see(dev_id, name):
-        '''Randomize a sighting.'''
-        see(
-            dev_id=dev_id,
-            host_name=name,
-            gps=(hass.config.latitude + offset(),
-                 hass.config.longitude + offset()),
-            gps_accuracy=random.randrange(50, 150),
-            battery=random.randrange(10, 90)
-        )
-    
-    import voluptuous as vol
-    
-    MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=1)
+        def debug(self, msg):
+        pass
