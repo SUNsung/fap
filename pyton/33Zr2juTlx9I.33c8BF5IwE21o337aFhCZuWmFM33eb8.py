e17049159eb977b946d406c8b3e120aaae791f7c
SUNsung <weatherius@gmail.com>
2019-01-22 23:07:40 +0200

    Rewrite [33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py] File in: 2019-01-22 23:07:39

--- pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
+++ pyton/33Zr2juTlx9I.33c8BF5IwE21o337aFhCZuWmFM33eb8.py
@@ -1,122 +1,39 @@
 
         
-        from sklearn.datasets import fetch_20newsgroups_vectorized
-from sklearn.metrics import accuracy_score
-from sklearn.utils.validation import check_array
+                # put all lines in the file into a Python list
+        strings = f.readlines()
+        
+        # above line leaves trailing newline characters; strip them out
+        strings = [x.strip(u'\n') for x in strings]
+        
+        # remove empty-lines and comments
+        strings = [x for x in strings if x and not x.startswith(u'#')]
+        
+        # insert empty string since all are being removed
+        strings.insert(0, u'')
     
-    The timings are then output to stdout, or visualized on a log-log scale
-with matplotlib.
+        failures = []
+    while point is not None:
+        if point.name:
+            if re.search('h[1-2]', point.name):
+                if point.name == 'h1':
+                    h1_directory = os.path.join(output_directory, clean_text(point.text))
+                    current_directory = h1_directory
+                elif point.name == 'h2':
+                    current_directory = os.path.join(h1_directory, clean_text(point.text))  
+                if not os.path.exists(current_directory):
+                    os.makedirs(current_directory)
+                print_title(point.text)
     
-    A test of LocalOutlierFactor on classical anomaly detection datasets.
+    def generateLargePrime(keysize = 1024):
+    while True:
+        num = random.randrange(2 ** (keysize - 1), 2 ** (keysize))
+        if isPrime(num):
+            return num
     
-    import numpy as np
-import gc
-from time import time
-from collections import defaultdict
-import matplotlib.pyplot as plt
-from sklearn.datasets import fetch_lfw_people
-from sklearn.decomposition import IncrementalPCA, PCA
-    
-    from sklearn.linear_model import Ridge, SGDRegressor, ElasticNet
-from sklearn.metrics import mean_squared_error
-from sklearn.datasets.samples_generator import make_regression
-    
-    
-# TASK: Build a vectorizer that splits strings into sequence of 1 to 3
-# characters instead of word tokens
-    
-            # reset stats on high iter-request times caused by client restarts
-        if delta > 3: # seconds
-            self._reset_stats()
-            return ''
-    
-        if settings is None:
-        settings = get_project_settings()
-        # set EDITOR from environment if available
-        try:
-            editor = os.environ['EDITOR']
-        except KeyError: pass
-        else:
-            settings['EDITOR'] = editor
-    check_deprecated_settings(settings)
-    
-                items, requests = self.run_callback(response, cb)
-            if opts.pipelines:
-                itemproc = self.pcrawler.engine.scraper.itemproc
-                for item in items:
-                    itemproc.process_item(item, spider)
-            self.add_items(depth, items)
-            self.add_requests(depth, requests)
-    
-        def run(self, args, opts):
-        if len(args) != 1:
-            raise UsageError()
-        filename = args[0]
-        if not os.path.exists(filename):
-            raise UsageError('File not found: %s\n' % filename)
-        try:
-            module = _import_file(filename)
-        except (ImportError, ValueError) as e:
-            raise UsageError('Unable to load %r: %s\n' % (filename, e))
-        spclasses = list(iter_spider_classes(module))
-        if not spclasses:
-            raise UsageError('No spider found in file: %s\n' % filename)
-        spidercls = spclasses.pop()
-    
-        name = 'returns'
-    objects = {
-        'request': Request,
-        'requests': Request,
-        'item': (BaseItem, dict),
-        'items': (BaseItem, dict),
-    }
-    
-            # If no credentials could be found anywhere,
-        # consider this an anonymous connection request by default;
-        # unless 'anon' was set explicitly (True/False).
-        anon = kw.get('anon')
-        if anon is None and not aws_access_key_id and not aws_secret_access_key:
-            kw['anon'] = True
-        self.anon = kw.get('anon')
-    
-            def log_failure(msg):
-            def errback(failure):
-                logger.error(
-                    msg,
-                    exc_info=failure_to_exc_info(failure),
-                    extra={'spider': spider}
-                )
-            return errback
-    
-    
-    
-        res = check_ip.check_ip(ip, host=top_domain, wait_time=wait_time)
-    if not res:
-        print('connect fail')
-    elif res.ok:
-        print('success, domain:%s handshake:%d' % (res.host, res.handshake_time))
-    else:
-        print('not support')
-    
-            Using setter/getter methods is deprecated. Use o.line instead.'''
-    
-        @classmethod
-    def dec_playinfo(cls, info, coeff):
-        res = None
-        clear = cls.funshion_decrypt_str(info['infohash'], coeff)
-        if cls.checksum(clear):
-            res = dict(hashid=clear[:40], token=cls.funshion_decrypt_str(info['token'], coeff))
-        else:
-            clear = cls.funshion_decrypt_str(info['infohash_prev'], coeff)
-            if cls.checksum(clear):
-                res = dict(hashid=clear[:40], token=cls.funshion_decrypt_str(info['token_prev'], coeff))
-        return res
-    
-    __all__ = ['giphy_download']
-    
-                if title is None:
-                response = request.urlopen(request.Request(real_url))
-                if response.headers['content-disposition']:
-                    filename = parse.unquote(r1(r'filename='?(.+)'?', response.headers['content-disposition'])).split('.')
-                    title = ''.join(filename[:-1])
-        except: pass
+    Inputs:
+  - X , a 2D numpy array of features.
+  - k , number of clusters to create.
+  - initial_centroids , initial centroid values generated by utility function(mentioned in usage).
+  - maxiter , maximum number of iterations to process.
+  - heterogeneity , empty list that will be filled with hetrogeneity values if passed to kmeans func.
