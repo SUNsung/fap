841d6f44a6babb1a9c67136c7831b38e874d4c3b
SUNsung <weatherius@gmail.com>
2019-01-22 23:48:10 +0200

    Rewrite [56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb] File in: 2019-01-22 23:48:10

--- ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
+++ ruby/56i9GW1Ho5kvU561l2AVYjN4eg56p2q9bbBQcQ.566Vf.rb
@@ -1,81 +1,137 @@
 
         
-            def self.rm_DS_Store
-      paths = Queue.new
-      %w[Cellar Frameworks Library bin etc include lib opt sbin share var].
-        map { |p| HOMEBREW_PREFIX/p }.each { |p| paths << p if p.exist? }
-      workers = (0...Hardware::CPU.cores).map do
-        Thread.new do
-          begin
-            while p = paths.pop(true)
-              quiet_system 'find', p, '-name', '.DS_Store', '-delete'
-            end
-          rescue ThreadError # ignore empty queue error
-          end
-        end
-      end
-      workers.map(&:join)
+        # -------------------------------------------------------------------
+# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
+# -------------------------------------------------------------------
+    
+        path = File.join(folder, '_#{type}s', filename)
+    File.write(path, file_content_from_hash(input_hash))
+  end
+end
+    
+      def self.theme_gem_dir; source_dir.join('tmp', 'jekyll', 'my-cool-theme'); end
+    
+        # This returns whether the guest is ready to work. If this returns
+    # `false`, then {#detect!} should be called in order to detect the
+    # guest OS.
+    #
+    # @return [Boolean]
+    def ready?
+      !!capability_host_chain
     end
+  end
+end
+
     
-        first_warning = true
-    methods.each do |method|
-      unless checks.respond_to?(method)
-        Homebrew.failed = true
-        puts 'No check available by the name: #{method}'
-        next
-      end
+    module Vagrant
+  module Plugin
+    module V1
+      # This class maintains a list of all the registered plugins as well
+      # as provides methods that allow querying all registered components of
+      # those plugins as a single unit.
+      class Manager
+        attr_reader :registered
     
-              # Encodes a Rex::Proto::Kerberos::Model::AuthorizationData into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            seqs = []
-            elements.each do |elem|
-              elems = []
-              type_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_type(elem[:type])], 0, :CONTEXT_SPECIFIC)
-              elems << type_asn1
-              data_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_data(elem[:data])], 1, :CONTEXT_SPECIFIC)
-              elems << data_asn1
-              seqs << OpenSSL::ASN1::Sequence.new(elems)
-            end
+                @logger.info('With machine: #{machine.name} (#{machine.provider.inspect})')
+            yield machine
     
-              # @!attribute key
-          #   @return [Integer] The type of encryption key
-          attr_accessor :type
-          # @!attribute value
-          #   @return [String] the key itself
-          attr_accessor :value
-    
-              # Decodes a Rex::Proto::Kerberos::Model::KdcRequest
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode_asn1(input)
-            input.value[0].value.each do |val|
-              case val.tag
-              when 1
-                self.pvno = decode_asn1_pvno(val)
-              when 2
-                self.msg_type = decode_asn1_msg_type(val)
-              when 3
-                self.pa_data  = decode_asn1_pa_data(val)
-              when 4
-                self.req_body = decode_asn1_req_body(val)
-              else
-                raise ::RuntimeError, 'Failed to decode KdcRequest SEQUENCE'
-              end
-            end
-          end
-    
-              # Decodes the e_data from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_e_data(input)
-            input.value[0].value
-          end
+            # Initialize the provider to represent the given machine.
+        #
+        # @param [Vagrant::Machine] machine The machine that this provider
+        #   is responsible for.
+        def initialize(machine)
         end
+    
+      it 'returns false when the command exits with a non-zero exit status' do
+    @object.system(ruby_cmd('exit 1')).should == false
+    
+        lambda {
+      catch :blah do
+        throw :blah, :return_value, 2, 3, 4, 5
+      end
+    }.should raise_error(ArgumentError)
+  end
+    
+      it 'writes the default record separator and NOT $/ to $stderr after the warning message' do
+    lambda {
+      $VERBOSE = true
+      $/ = 'rs'
+      warn('')
+    }.should output(nil, /\n/)
+  end
+    
+      opts.separator ''
+  opts.separator 'Commands:'
+  opts.separator '  remove WORKER   Removes a worker'
+  opts.separator '  kill WORKER     Kills a worker'
+  opts.separator '  list            Lists known workers'
+end
+    
+        w.uid = 'git'
+    w.gid = 'git'
+    
+    # 1. update ChangeLog and lib/fluent/version.rb
+# 2. bundle && bundle exec rake build:all
+# 3. release 3 packages built on pkg/ directory
+namespace :build do
+  desc 'Build gems for all platforms'
+  task :all do
+    Bundler.with_clean_env do
+      %w[ruby x86-mingw32 x64-mingw32].each do |name|
+        ENV['GEM_BUILD_FAKE_PLATFORM'] = name
+        Rake::Task['build'].execute
       end
     end
   end
 end
+    
+        @@block_level = 0
+    @@frozen_clock = nil
+    
+          @opt_parser.on('-e KEY=VALUE', 'configure formatter config params') do |v|
+        key, value = v.split('=')
+        usage '#{v} is invalid. valid format is like `key=value`' unless value
+        @options[:config_params].merge!(key => value)
+      end
+    end
+    
+    Options:
+BANNER
+    @parser.on('--verbose', 'Be verbose') do
+      @verbose = true
+    end
+    @parser.on('-c', '--compact', 'Compact output') do
+      @compact = true
+    end
+    @parser.on('-f', '--format=FORMAT', 'Specify format. (#{AVAILABLE_FORMATS.join(',')})') do |s|
+      format = s.to_sym
+      usage('Unsupported format: #{s}') unless AVAILABLE_FORMATS.include?(format)
+      @format = format
+    end
+    @parser.on('-I PATH', 'Add PATH to $LOAD_PATH') do |s|
+      $LOAD_PATH.unshift(s)
+    end
+    @parser.on('-r NAME', 'Load library') do |s|
+      @libs << s
+    end
+    @parser.on('-p', '--plugin=DIR', 'Add plugin directory') do |s|
+      @plugin_dirs << s
+    end
+  end
+    
+          class JSONFormatter < Formatter
+        def call(record, out)
+          out << Yajl.dump(record) << '\n'
+        end
+      end
+    
+              @_timekey = case conf['time_slice_format']
+                      when /\%S/ then 1
+                      when /\%M/ then 60
+                      when /\%H/ then 3600
+                      when /\%d/ then 86400
+                      when nil   then 86400 # default value of TimeSlicedOutput.time_slice_format is '%Y%m%d'
+                      else
+                        raise Fluent::ConfigError, 'time_slice_format only with %Y or %m is too long'
+                      end
+          buf_params['timekey'] = @_timekey
