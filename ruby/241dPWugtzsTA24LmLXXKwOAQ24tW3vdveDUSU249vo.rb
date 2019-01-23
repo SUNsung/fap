257a5510a86adc863291955456ae8e470967ff82
SUNsung <weatherius@gmail.com>
2019-01-23 09:13:55 +0200

    Rewrite [241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb] File in: 2019-01-23 09:13:54

--- ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
+++ ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
@@ -1,69 +1,67 @@
 
         
-        
+        # Mimic how Jekyll's LiquidRenderer would process a non-static file, with
+# some dummy payload
+def always_liquid(content)
+  Liquid::Template.error_mode = :warn
+  Liquid::Template.parse(content, :line_numbers => true).render(
+    'author' => 'John Doe',
+    'title'  => 'FooBar'
+  )
 end
+    
+    def local_require
+  require 'json'
+  JSON.pretty_generate(DATA)
 end
+    
+    if pathutil_relative == native_relative
+  Benchmark.ips do |x|
+    x.report('pathutil') { pathutil_relative }
+    x.report('native')   { native_relative }
+    x.compare!
+  end
+else
+  print 'PATHUTIL: '
+  puts pathutil_relative
+  print 'NATIVE:   '
+  puts native_relative
 end
 
     
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
           #
-          # @return [String] encoded principal
-          def encode
-            encoded = ''
-            encoded << encode_name_type
-            encoded << [components.length].pack('N')
-            encoded << encode_realm
-            encoded << encode_components
     
-              # Encodes the pvno field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_pvno
-            bn = OpenSSL::BN.new(pvno.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
+            def preload_stages_warnings
+          # This preloads the number of warnings for every stage, ensuring
+          # that Ci::Stage#has_warnings? doesn't execute any additional
+          # queries.
+          @pipeline.stages.each { |stage| stage.number_of_warnings }
+        end
+      end
+    end
+  end
+end
+
     
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        class EncKdcResponse < Element
-          # @!attribute key
-          #   @return [Rex::Proto::Kerberos::Model::EncryptionKey] The session key
-          attr_accessor :key
-          # @!attribute last_req
-          #   @return [Array<Rex::Proto::Kerberos::Model::LastRequest>] This field is returned by the KDC and specifies the time(s)
-          #   of the last request by a principal
-          attr_accessor :last_req
-          # @!attribute nonce
-          #   @return [Integer] random number
-          attr_accessor :nonce
-          # @!attribute key_expiration
-          #   @return [Time] The key-expiration field is part of the response from the
-          #   KDC and specifies the time that the client's secret key is due to expire
-          attr_accessor :key_expiration
-          # @!attribute flags
-          #   @return [Integer] This field indicates which of various options were used or
-          #   requested when the ticket was issued
-          attr_accessor :flags
-          # @!attribute auth_time
-          #   @return [Time] the time of initial authentication for the named principal
-          attr_accessor :auth_time
-          # @!attribute start_time
-          #   @return [Time] Specifies the time after which the ticket is valid
-          attr_accessor :start_time
-          # @!attribute end_time
-          #   @return [Time] This field contains the time after which the ticket will
-          #   not be honored (its expiration time)
-          attr_accessor :end_time
-          # @!attribute renew_till
-          #   @return [Time] This field is only present in tickets that have the
-          #   RENEWABLE flag set in the flags field.  It indicates the maximum
-          #   endtime that may be included in a renewal
-          attr_accessor :renew_till
-          # @!attribute srealm
-          #   @return [String] The realm part of the server's principal identifier
-          attr_accessor :srealm
-          # @!attribute sname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
-          attr_accessor :sname
+            # attributes - A hash containing the raw issue details. The keys of this
+        #              Hash (and any nested hashes) must be symbols.
+        def initialize(attributes)
+          @attributes = attributes
+        end
+    
+            # Builds a new lfs_object using a Hash that was built from a JSON payload.
+        def self.from_json_hash(raw_hash)
+          new(Representation.symbolize_hash(raw_hash))
+        end
+    
+    ENV['RAILS_ENV'] = 'test'
+    
+        def self.definitions_for(klass)
+      instance.definitions_for(klass)
+    end
+    
+        def raise_because_imagemagick_missing
+      raise Errors::CommandNotFoundError.new('Could not run the `identify` command. Please install ImageMagick.')
+    end
+  end
+end
