31f2f87256b0f2a2a0eebc46ecac3d5f9461a00a
SUNsung <weatherius@gmail.com>
2019-01-23 12:40:13 +0200

    Rewrite [50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py] File in: 2019-01-23 12:40:12

--- pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
+++ pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
@@ -1,246 +1,57 @@
 
         
-            def get_prep_lookup(self):
-        return [str(item) for item in self.rhs]
-    
-        def __init__(self):
-        self.name = 'pgcrypto'
-    
-        try:
-        oids, array_oids = get_hstore_oids(connection.alias)
-        register_hstore(connection.connection, globally=True, oid=oids, array_oid=array_oids)
-    except ProgrammingError:
-        # Hstore is not available on the database.
-        #
-        # If someone tries to create an hstore field it will error there.
-        # This is necessary as someone may be using PSQL without extensions
-        # installed but be using other features of contrib.postgres.
-        #
-        # This is also needed in order to create the connection in order to
-        # install the hstore extension.
-        pass
-    
-        def __call__(self, value):
-        keys = set(value)
-        missing_keys = self.keys - keys
-        if missing_keys:
-            raise ValidationError(
-                self.messages['missing_keys'],
-                code='missing_keys',
-                params={'keys': ', '.join(missing_keys)},
-            )
-        if self.strict:
-            extra_keys = keys - self.keys
-            if extra_keys:
-                raise ValidationError(
-                    self.messages['extra_keys'],
-                    code='extra_keys',
-                    params={'keys': ', '.join(extra_keys)},
-                )
-    
-        def test_lower_items(self):
-        assert list(self.case_insensitive_dict.lower_items()) == [('accept', 'application/json')]
-    
-            String:                    '#4e9a06',        # class: 's'
-        String.Backtick:           '#4e9a06',        # class: 'sb'
-        String.Char:               '#4e9a06',        # class: 'sc'
-        String.Doc:                'italic #8f5902', # class: 'sd' - like a comment
-        String.Double:             '#4e9a06',        # class: 's2'
-        String.Escape:             '#4e9a06',        # class: 'se'
-        String.Heredoc:            '#4e9a06',        # class: 'sh'
-        String.Interpol:           '#4e9a06',        # class: 'si'
-        String.Other:              '#4e9a06',        # class: 'sx'
-        String.Regex:              '#4e9a06',        # class: 'sr'
-        String.Single:             '#4e9a06',        # class: 's1'
-        String.Symbol:             '#4e9a06',        # class: 'ss'
-    
-       >>> import requests
-   >>> r = requests.get('https://www.python.org')
-   >>> r.status_code
-   200
-   >>> 'Python is a programming language' in r.content
-   True
-    
-    from requests.help import info
+                if k < 0:
+            return self.LB(-k)
+                
+        i = self.p
+        n = 1
+        # find k good tokens
+        while n < k:
+            # skip off-channel tokens
+            i = self.skipOffTokenChannels(i+1) # leave p on valid token
+            n += 1
     
     
-class CheckIp(front_base.check_ip.CheckIp):
-    def check_response(self, response):
-        server_type = response.headers.get('Server', '')
-        self.logger.debug('status:%d', response.status)
-        self.logger.debug('Server type:%s', server_type)
-    
-    from cert_util import CertUtil
-import simple_http_server
-import proxy_handler
-import env_info
-from front import front, direct_front
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licensc]
-    
-    class MismatchedTokenException(RecognitionException):
-    '''@brief A mismatched char or Token or tree node.'''
-    
-    def __init__(self, expecting, input):
-        RecognitionException.__init__(self, input)
-        self.expecting = expecting
-        
-    
-        def getTokenIndex(self):
-        return self.index
-    
-    def setTokenIndex(self, index):
-        self.index = index
-    
-            video_id = match1(html, r'data-brightcove-id='(\d+)'')
-        
-        assert account_number, video_id
-    
-                if stream_id not in self.streams:
-                log.e('[Error] Invalid video format.')
-                log.e('Run \'-i\' command with no specific video format to view all available formats.')
-                exit(2)
+def open_binary(package: Package, resource: Resource) -> BinaryIO:
+    '''Return a file-like object opened for binary reading of the resource.'''
+    resource = _normalize_path(resource)
+    package = _get_package(package)
+    reader = _get_resource_reader(package)
+    if reader is not None:
+        return reader.open_resource(resource)
+    _check_location(package)
+    absolute_package_path = os.path.abspath(package.__spec__.origin)
+    package_path = os.path.dirname(absolute_package_path)
+    full_path = os.path.join(package_path, resource)
+    try:
+        return open(full_path, mode='rb')
+    except OSError:
+        # Just assume the loader is a resource loader; all the relevant
+        # importlib.machinery loaders are and an AttributeError for
+        # get_data() will make it clear what is needed from the loader.
+        loader = cast(ResourceLoader, package.__spec__.loader)
+        data = None
+        if hasattr(package.__spec__.loader, 'get_data'):
+            with suppress(OSError):
+                data = loader.get_data(full_path)
+        if data is None:
+            package_name = package.__spec__.name
+            message = '{!r} resource not found in {!r}'.format(
+                resource, package_name)
+            raise FileNotFoundError(message)
         else:
-            # Extract stream with the best quality
-            stream_id = self.streams_sorted[0]['id']
-            _, s['container'], s['size'] = url_info(s['url'])
-            s['src'] = [s['url']]
+            return BytesIO(data)
     
+        print('Pickled records:')
+    pprint.pprint(memos)
     
-def fix_coub_video_file(file_path):
-    with open(file_path, 'r+b') as file:
-        file.seek(0)
-        file.write(bytes(2))
+    from email.message import EmailMessage
+from email.headerregistry import Address
+from email.utils import make_msgid
     
-        if re.match(r'https?://movie', url):
-        title = match1(html, 'name='description' content='([^']+)')
-        tid = match1(url, 'trailer/(\d+)')
-        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
-        type, ext, size = url_info(real_url)
+    from email.message import EmailMessage
+from email.policy import SMTP
     
-        title = r1(r'<title id='pageTitle'>(.+)</title>', html)
-    
-    tmpl_api_call_photo_info = (
-    'https://api.flickr.com/services/rest?'
-    '&format=json&nojsoncallback=1'
-    '&method=flickr.photos.getInfo'
-    '&api_key=%s'
-    '&photo_id=%s'
-)
-    
-                if item.get(CONF_TEXT) is not None:
-                if isinstance(item.get(CONF_TEXT), template.Template):
-                    output[ATTR_MAIN_TEXT] = item[CONF_TEXT].async_render()
-                else:
-                    output[ATTR_MAIN_TEXT] = item.get(CONF_TEXT)
-    
-            _LOGGER.debug('Sent event %s', event.data.get('entity_id'))
-    
-    
-def setup_scanner(hass, config, see, discovery_info=None):
-    '''Set up the demo tracker.'''
-    def offset():
-        '''Return random offset.'''
-        return (random.randrange(500, 2000)) / 2e5 * random.choice((-1, 1))
-    
-        return scanner if scanner.success_init else None
-    
-    # entity attributes
-ATTR_ACCOUNTNAME = 'account_name'
-ATTR_INTERVAL = 'interval'
-ATTR_DEVICENAME = 'device_name'
-ATTR_BATTERY = 'battery'
-ATTR_DISTANCE = 'distance'
-ATTR_DEVICESTATUS = 'device_status'
-ATTR_LOWPOWERMODE = 'low_power_mode'
-ATTR_BATTERYSTATUS = 'battery_status'
-    
-    
-def _arp(ip_address):
-    '''Get the MAC address for a given IP.'''
-    cmd = ['arp', '-n', ip_address]
-    arp = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    out, _ = arp.communicate()
-    match = re.search(r'(([0-9A-Fa-f]{1,2}\:){5}[0-9A-Fa-f]{1,2})', str(out))
-    if match:
-        return match.group(0)
-    _LOGGER.info('No MAC address found for %s', ip_address)
-    return None
-    
-                    overwrite = service.data.get(ATTR_OVERWRITE)
-    
-        hass.bus.listen(EVENT_STATE_CHANGED, dweet_event_listener)
-    
-            if self.upnp_bind_multicast:
-            ssdp_socket.bind(('', 1900))
-        else:
-            ssdp_socket.bind((self.host_ip_addr, 1900))
-    
-        @classmethod
-    def _class_method_2(cls):
-        print('Value {}'.format(cls.x2))
-    
-    *TL;DR80
-Traverses a container and accesses the container's elements.
-'''
-    
-        def insert(self):
-        print('Inserting the execution begin status in the Database')
-        time.sleep(0.1)
-        # Following code is to simulate a communication from DB to TC
-        if random.randrange(1, 4) == 3:
-            return -1
-    
-        This is, in fact, just syntactic sugar around a memento closure.
-    '''
-    
-        def __new__(cls, name, bases, attrs):
-        new_cls = type.__new__(cls, name, bases, attrs)
-        '''
-            Here the name of the class is used as key but it could be any class
-            parameter.
-        '''
-        cls.REGISTRY[new_cls.__name__] = new_cls
-        return new_cls
-    
-        sample_queue = queue.Queue()
-    
-    
-class ProductionCodeTimeProvider(object):
-    '''
-    Production code version of the time provider (just a wrapper for formatting
-    datetime for this example).
-    '''
-    
-    
-print(graph1.find_path('A', 'D'))
-print(graph1.find_all_path('A', 'D'))
-print(graph1.find_shortest_path('A', 'D'))
+    def test():
+    PROCESSES = 4
+    print('Creating pool with %d processes\n' % PROCESSES)
