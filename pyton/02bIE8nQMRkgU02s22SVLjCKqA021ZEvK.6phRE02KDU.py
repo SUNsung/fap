676b42a140b76436066431638171f04f1fb99fea
SUNsung <weatherius@gmail.com>
2019-01-23 03:03:38 +0200

    Rewrite [02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py] File in: 2019-01-23 03:03:37

--- pyton/02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py
+++ pyton/02bIE8nQMRkgU02s22SVLjCKqA021ZEvK.6phRE02KDU.py
@@ -1,138 +1,33 @@
 
         
-            def can_fit_in_spot(self, spot):
-        return True
-    
-    
-@pytest.fixture(autouse=True)
-def os_environ(monkeypatch):
-    env = {'PATH': os.environ['PATH']}
-    monkeypatch.setattr('os.environ', env)
-    return env
-
-    
-    
-@pytest.mark.functional
-def test_how_to_configure_alias(proc, TIMEOUT):
-    proc.sendline(u'unfunction fuck')
-    how_to_configure(proc, TIMEOUT)
-
-    
-    FORMATS = {
-    'sequences': lambda y: [list(np.flatnonzero(s)) for s in y],
-    'dense': lambda y: y,
-    'csr': lambda y: sp.csr_matrix(y),
-    'csc': lambda y: sp.csc_matrix(y),
-}
-    
-                plt.text((i + 0.02) / len(algorithms), 0.98, alg,
-                     transform=ax.transAxes,
-                     ha='left',
-                     va='top',
-                     bbox=dict(facecolor='w', edgecolor='w', alpha=0.5))
-    
-    # Split data in train set and test set
-n_samples = X.shape[0]
-X_train, y_train = X[:n_samples // 2], y[:n_samples // 2]
-X_test, y_test = X[n_samples // 2:], y[n_samples // 2:]
-print('test data sparsity: %f' % sparsity_ratio(X_test))
-    
-    
-def human_readable_data_quantity(quantity, multiple=1024):
-    # https://stackoverflow.com/questions/1094841/reusable-library-to-get-human-readable-version-of-file-size
-    if quantity == 0:
-        quantity = +0
-    SUFFIXES = ['B'] + [i + {1000: 'B', 1024: 'iB'}[multiple]
-                        for i in 'KMGTPEZY']
-    for suffix in SUFFIXES:
-        if quantity < multiple or suffix == SUFFIXES[-1]:
-            if suffix == SUFFIXES[0]:
-                return '%d %s' % (quantity, suffix)
-            else:
-                return '%.1f %s' % (quantity, suffix)
+                if triple is None:
+            detail = 'no match'
         else:
-            quantity /= multiple
-    
-        target = utils.unescape(target).strip()
-    title = utils.unescape(title).strip()
-    config = inliner.document.settings.env.app.config
-    if config.issues_user_uri:
-        ref = config.issues_user_uri.format(user=target)
-    else:
-        ref = 'https://github.com/{0}'.format(target)
-    if has_explicit_title:
-        text = title
-    else:
-        text = '@{0}'.format(target)
-    
-            # split the paragraph into fake smaller paragraphs to make the
-        # problem harder e.g. more similar to tweets
-        if lang in ('zh', 'ja'):
-        # FIXME: whitespace tokenizing does not work on chinese and japanese
-            continue
-        words = content.split()
-        n_groups = len(words) / n_words_per_short_text
-        if n_groups < 1:
-            continue
-        groups = np.array_split(words, n_groups)
-    
-    import sys
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-    
-    # Define 'classifiers' to be used
-classifiers = {
-    'Empirical Covariance': EllipticEnvelope(support_fraction=1.,
-                                             contamination=0.261),
-    'Robust Covariance (Minimum Covariance Determinant)':
-    EllipticEnvelope(contamination=0.261),
-    'OCSVM': OneClassSVM(nu=0.261, gamma=0.05)}
-colors = ['m', 'g', 'b']
-legend1 = {}
-legend2 = {}
-    
-    _help = '''Usage: {} [OPTION]... [URL]...
-TODO
-'''.format(script_name)
+            detail = 'found (%r)' % (triple[1] or '<string>')
+            total_found += 1
+        info.append('       -> %s' % detail)
     
     
-def download_url(url, merge, output_dir, title, info_only):
-    mime, ext, size = url_info(url)
-    print_info(site_info, title, mime, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge)
+# context locals
+_request_ctx_stack = LocalStack()
+_app_ctx_stack = LocalStack()
+current_app = LocalProxy(_find_app)
+request = LocalProxy(partial(_lookup_req_object, 'request'))
+session = LocalProxy(partial(_lookup_req_object, 'session'))
+g = LocalProxy(partial(_lookup_app_object, 'g'))
+
     
-    __all__ = ['douban_download']
+            if not current.propagate:
+            break
     
-    def get_photoset_id(url, page):
-    return match1(url, pattern_url_photoset)
+        def __init__(self, app, **options):
+        if 'loader' not in options:
+            options['loader'] = app.create_global_jinja_loader()
+        BaseEnvironment.__init__(self, **options)
+        self.app = app
     
-            # attempt to extract images first
-        # TBD: posts with > 4 images
-        # TBD: album links
-        html = get_html(parse.unquote(url), faker=True)
-        real_urls = []
-        for src in re.findall(r'src='([^']+)'[^>]*itemprop='image'', html):
-            t = src.split('/')
-            t[0], t[-2] = t[0] or 'https:', 's0-d'
-            u = '/'.join(t)
-            real_urls.append(u)
-        if not real_urls:
-            real_urls = [r1(r'<meta property='og:image' content='([^']+)', html)]
-            real_urls = [re.sub(r'w\d+-h\d+-p', 's0', u) for u in real_urls]
-        post_date = r1(r''?(20\d\d[-/]?[01]\d[-/]?[0123]\d)'?', html)
-        post_id = r1(r'/posts/([^']+)', html)
-        title = post_date + '_' + post_id
-    
-        for filename in filenames:
-        fd = codecs.open(filename, mode='r', encoding='utf-8')
-        for line in fd.readlines():
-            refs = re.findall(r'(?<=<a href=')[^']*', markdown.markdown(line))
-            for ref in refs:
-                if ref not in urls:
-                    urls.append(ref)
+                resp = app.response_class()
+            if not session_interface.is_null_session(sess):
+                session_interface.save_session(app, sess, resp)
+            headers = resp.get_wsgi_headers(c.request.environ)
+            self.cookie_jar.extract_wsgi(c.request.environ, headers)
