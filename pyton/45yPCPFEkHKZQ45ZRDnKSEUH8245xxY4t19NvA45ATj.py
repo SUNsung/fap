6e22a39f45693b40ca269264bf69054d620fe922
SUNsung <weatherius@gmail.com>
2019-01-24 06:21:35 +0200

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2019-01-24 06:21:34

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,110 +1,15 @@
 
         
-            parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
-    parser.add_argument('-d', action='store', dest='directory')
-    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
-    parser.add_argument('--overwrite', action='store_true', default = False)    
-    results = parser.parse_args()
+        IPv6 = CheckNetwork('IPv6')
+IPv6.urls = ['http://[2001:41d0:8:e8ad::1]',
+             'http://[2001:260:401:372::5f]',
+             'http://[2a02:188:3e00::32]',
+             'http://[2804:10:4068::202:82]'
+             ]
+IPv6.triger_check_network()
     
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.host = config[CONF_HOST]
-        self.username = config[CONF_USERNAME]
-        self.password = config[CONF_PASSWORD]
-        self.last_results = []
-        data = self.get_actiontec_data()
-        self.success_init = data is not None
-        _LOGGER.info('canner initialized')
+    class MismatchedSetException(RecognitionException):
+    '''@brief The next token does not match a set of expected types.'''
     
-    
-def setup(hass, config):
-    '''Set up the folder watcher.'''
-    conf = config[DOMAIN]
-    for watcher in conf:
-        path = watcher[CONF_FOLDER]
-        patterns = watcher[CONF_PATTERNS]
-        if not hass.config.is_allowed_path(path):
-            _LOGGER.error('folder %s is not valid or allowed', path)
-            return False
-        Watcher(path, patterns, hass)
-    
-        @property
-    def should_poll(self):
-        '''No polling needed.'''
-        return False
-    
-    while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-    
-    # This is a demo of running face recognition on a video file and saving the results to a new video file.
-#
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-        # Loop through each face in this frame of video
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    
-    import face_recognition
-import picamera
-import numpy as np
-    
-        # If a match was found in known_face_encodings, just use the first one.
-    if True in matches:
-        first_match_index = matches.index(True)
-        name = known_face_names[first_match_index]
-    
-            self.assertEqual(len(detected_faces), 1)
-        self.assertAlmostEqual(detected_faces[0].rect.top(), 259, delta=25)
-        self.assertAlmostEqual(detected_faces[0].rect.bottom(), 552, delta=25)
-    
-    setup(
-    name='face_recognition',
-    version='1.2.3',
-    description='Recognize faces from Python or from the command line',
-    long_description=readme + '\n\n' + history,
-    author='Adam Geitgey',
-    author_email='ageitgey@gmail.com',
-    url='https://github.com/ageitgey/face_recognition',
-    packages=[
-        'face_recognition',
-    ],
-    package_dir={'face_recognition': 'face_recognition'},
-    package_data={
-        'face_recognition': ['models/*.dat']
-    },
-    entry_points={
-        'console_scripts': [
-            'face_recognition=face_recognition.face_recognition_cli:main',
-            'face_detection=face_recognition.face_detection_cli:main'
-        ]
-    },
-    install_requires=requirements,
-    license='MIT license',
-    zip_safe=False,
-    keywords='face_recognition',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
-
-    
-    import face_recognition
-import picamera
-import numpy as np
+            This is very useful for error messages and for context-sensitive
+        error recovery.
