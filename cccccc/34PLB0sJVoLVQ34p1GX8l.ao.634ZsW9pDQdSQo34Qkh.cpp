81b2870b55db06749f03e7d6ba5006fcc48065b7
SUNsung <weatherius@gmail.com>
2019-01-24 16:29:36 +0200

    Rewrite [34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp] File in: 2019-01-24 16:29:36

--- cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
+++ cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
@@ -1,126 +1,202 @@
 
         
         
-    {
-    {        /* Work our way backwards, using the z-ratios to scale the x/y values. */
-        while (i > 0) {
-            if (i != len - 1) {
-                secp256k1_fe_mul(&zs, &zs, &zr[i]);
-            }
-            i--;
-            secp256k1_ge_set_gej_zinv(&r[i], &a[i], &zs);
-        }
-    }
-}
+    {}  // namespace tensorflow
     
-    /** Perform one SHA-256 transformation, processing 16 big endian 32-bit words. */
-static void secp256k1_sha256_transform(uint32_t* s, const uint32_t* chunk) {
-    uint32_t a = s[0], b = s[1], c = s[2], d = s[3], e = s[4], f = s[5], g = s[6], h = s[7];
-    uint32_t w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15;
-    }
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    #include 'include/secp256k1_ecdh.h'
-#include 'ecmult_const_impl.h'
+    #ifndef TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
     
-    /// Format list of arguments to the stream according to given format string.
-template<typename... Args>
-void format(std::ostream& out, const char* fmt, const Args&... args)
-{
-    vformat(out, fmt, makeFormatList(args...));
-}
+    // Must be included first.
+#include 'tensorflow/python/lib/core/numpy.h'
     
-    const std::vector<UniValue>& UniValue::getValues() const
-{
-    if (typ != VOBJ && typ != VARR)
-        throw std::runtime_error('JSON value is not an object or array as expected');
-    return values;
-}
-    
-        std::string strJson1(json1);
-    BOOST_CHECK(v.read(strJson1));
-    
-    /** Encode a Bech32 string. Returns the empty string in case of failure. */
-std::string Encode(const std::string& hrp, const std::vector<uint8_t>& values);
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
     
     {
-    {        if (bytes <= 64) {
-            if (bytes < 64) {
-                for (i = 0;i < bytes;++i) ctarget[i] = c[i];
-            }
-            input[12] = j12;
-            input[13] = j13;
-            return;
-        }
-        bytes -= 64;
-        c += 64;
-    }
-}
+    {}  // namespace cuda
+}  // namespace stream_executor
 
     
-    #include <string>
     
-    #include 'content/nw/src/api/clipboard/clipboard.h'
+    {
+    {}  // namespace cuda
+}  // namespace stream_executor
     
-    #include 'base/values.h'
-#include 'components/zoom/zoom_controller.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menuitem/menuitem.h'
-#include 'content/public/browser/web_contents.h'
-#include 'content/public/common/page_zoom.h'
-#include 'ui/views/controls/menu/menu_runner.h'
+      // Iterates through loaded DSOs with DlIteratePhdrCallback to find the
+  // driver-interfacing DSO version number. Returns it as a string.
+  static port::StatusOr<DriverVersion> FindDsoVersion();
     
-    void MenuItem::RemoveKeys() {
-  if (!focus_manager_) return;
+    // CUDAEvent wraps a CUevent in the platform-independent EventInterface
+// interface.
+class CUDAEvent : public internal::EventInterface {
+ public:
+  explicit CUDAEvent(CUDAExecutor* parent);
     }
+    
+    #ifndef CONTENT_NW_SRC_API_APP_APP_H_
+#define CONTENT_NW_SRC_API_APP_APP_H_
+    
+    
+    {}  // namespace nw
+
+    
+    #include 'base/strings/string_piece.h'
+#include 'v8/include/v8.h'
+    
+    void Clipboard::CallSync(const std::string& method,
+                         const base::ListValue& arguments,
+                         base::ListValue* result) {
+  if (method == 'Get') {
+    result->AppendString(GetText());
+  } else {
+    NOTREACHED() << 'Invalid call to Clipboard method:' << method
+                 << ' arguments:' << arguments;
+  }
+}
+    
+    class ObjectManager;
+    
+    namespace ui {
+    }
+    
+     protected:
+  ~NwAppClearAppCacheFunction() override;
     
     #include 'extensions/browser/extension_function.h'
     
-    //////////////////////////////////////////////////////////////////////
-    
-    APCHandle::Pair APCCollection::WrapArray(APCHandle::Pair inner,
-                                         CollectionType colType) {
-  auto const col = new APCCollection;
-  col->m_arrayHandle = inner.handle;
-  col->m_colType = colType;
-  return { &col->m_handle, inner.size + sizeof(APCCollection) };
-}
-    
-    
-    {}
-
-    
-    /**
- * Parts of the language can individually be made stricter, warning or
- * erroring when there's dangerous/unintuive usage; for example,
- * array_fill_keys() with non-int/string keys: Hack.Lang.StrictArrayFillKeys
- */
-enum class HackStrictOption {
-  OFF, // PHP5 behavior
-  WARN,
-  ON
+    template <typename Dtype>
+class LayerRegistry {
+ public:
+  static ::caffe::Layer<Dtype> * CreateLayer(const ::caffe::LayerParameter& param) {
+    ::caffe::shared_ptr< ::caffe::Layer<Dtype> > ptr =
+      ::caffe::LayerRegistry<Dtype>::CreateLayer(param);
+    // avoid caffe::layer destructor, which deletes the weights layer owns
+    new ::caffe::shared_ptr< ::caffe::Layer<Dtype> >(ptr);
+    return ptr.get();
+  }
 };
     
     
-    {  auto glob = HHVM_FN(glob)(String(path_str, path_len, CopyString));
-  if (!glob.isArray()) {
-    return nullptr;
+  bool ReadProtoFromTextContent(const std::string& text,
+                                ::google::protobuf::Message* proto) const {
+    bool success = google::protobuf::TextFormat::ParseFromString(text, proto);
+    return success;
   }
-  return req::make<ArrayDirectory>(glob.toArray());
-}
     
-    /**
- * For php://output, a simple wrapper of g_context->out().
- */
-struct OutputFile : File {
-  DECLARE_RESOURCE_ALLOCATION(OutputFile);
+    
+MXNET_DLL int MXCVResize(NDArrayHandle src, const mx_uint w, const mx_uint h,
+                         const int interpolation, NDArrayHandle *out) {
+  API_BEGIN();
+  NDArray ndsrc = *static_cast<NDArray*>(src);
+  CHECK_EQ(ndsrc.shape().ndim(), 3);
+  CHECK_EQ(ndsrc.ctx(), Context::CPU());
+  CHECK_EQ(ndsrc.dtype(), mshadow::kUint8);
     }
     
-    template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, F fillCols) {
-  logPerfWarningImpl(event, 0, kDefaultPerfWarningRate, fillCols);
+    namespace mxnet {
+namespace io {
+    }
+    }
+    
+     private:
+  std::shared_ptr<Cache> cache_;
+  uint32_t num_threads_;
+    
+    Status WriteBatchBase::Delete(const SliceParts& key) {
+  std::string key_buf;
+  Slice key_slice(key, &key_buf);
+  return Delete(key_slice);
 }
-template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, int64_t rate, F fillCols) {
-  logPerfWarningImpl(event, 0, rate, fillCols);
+    
+    std::unique_ptr<WriteControllerToken> WriteController::GetDelayToken(
+    uint64_t write_rate) {
+  total_delayed_++;
+  // Reset counters.
+  last_refill_time_ = 0;
+  bytes_left_ = 0;
+  set_delayed_write_rate(write_rate);
+  return std::unique_ptr<WriteControllerToken>(new DelayWriteToken(this));
 }
+    
+      virtual Status Write(uint64_t offset, const Slice& data) override;
+    
+      // close DB
+  delete cf;
+  delete db;
+    
+    // Take a default DBOptions 'base_options' in addition to a
+// map 'opts_map' of option name to option value to construct the new
+// DBOptions 'new_options'.
+//
+// Below are the instructions of how to config some non-primitive-typed
+// options in DBOptions:
+//
+// * rate_limiter_bytes_per_sec:
+//   RateLimiter can be configured directly by specifying its bytes_per_sec.
+//   [Example]:
+//   - Passing {'rate_limiter_bytes_per_sec', '1024'} is equivalent to
+//     passing NewGenericRateLimiter(1024) to rate_limiter_bytes_per_sec.
+//
+// @param base_options the default options of the output 'new_options'.
+// @param opts_map an option name to value map for specifying how 'new_options'
+//     should be set.
+// @param new_options the resulting options based on 'base_options' with the
+//     change specified in 'opts_map'.
+// @param input_strings_escaped when set to true, each escaped characters
+//     prefixed by '\' in the values of the opts_map will be further converted
+//     back to the raw string before assigning to the associated options.
+// @param ignore_unknown_options when set to true, unknown options are ignored
+//     instead of resulting in an unknown-option error.
+// @return Status::OK() on success.  Otherwise, a non-ok status indicating
+//     error will be returned, and 'new_options' will be set to 'base_options'.
+Status GetDBOptionsFromMap(
+    const DBOptions& base_options,
+    const std::unordered_map<std::string, std::string>& opts_map,
+    DBOptions* new_options, bool input_strings_escaped = false,
+    bool ignore_unknown_options = false);
+    
+      // Submit a fire and forget jobs
+  // This allows to submit the same job multiple times
+  virtual void SubmitJob(const std::function<void()>&) = 0;
+  // This moves the function in for efficiency
+  virtual void SubmitJob(std::function<void()>&&) = 0;
+    
+    
+    {    // exception out_of_range.401
+    try
+    {
+        // try to write at a nonexisting key
+        object.at('the fast') = 'il rapido';
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+}
+
+    
+    
+    // exception type_error.304
+    try
+    {
+        // use at() on a non-array type
+        json str = 'I am a string';
+        str.at(0) = 'Another string';
+    }
+    catch (json::type_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+        // print values
+    std::cout << object << '\n';
+    std::cout << null << '\n';
