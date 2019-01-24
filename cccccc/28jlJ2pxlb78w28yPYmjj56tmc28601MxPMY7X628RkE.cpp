1083f9dbbadf771772cf45927d90715ef547adf2
SUNsung <weatherius@gmail.com>
2019-01-24 02:10:36 +0200

    Rewrite [28jlJ2pxlb78w28yPYmjj56tmc28601MxPMY7X628RkE.cpp] File in: 2019-01-24 02:10:35

--- cccccc/28jlJ2pxlb78w28yPYmjj56tmc28601MxPMY7X628RkE.cpp
+++ cccccc/28jlJ2pxlb78w28yPYmjj56tmc28601MxPMY7X628RkE.cpp
@@ -1,292 +1,114 @@
 
         
-          std::vector<string> output;
-  string input_tensors_needed_out;
-  tensorflow::Status status = RunCppShapeInferenceImpl(
-      graph_def_version, serialized_node_def, input_serialized_shapes,
-      input_constant_tensor_values_v, input_constant_tensor_as_shape_values,
-      &output, &input_tensors_needed_out);
-    
-      virtual string Code();
-    
-    namespace tensorflow {
-    }
-    
-    
-    {}  // namespace tensorflow
-    
-    Status TF_TensorToPyArray(Safe_TF_TensorPtr tensor, PyObject** out_ndarray);
-    
-    // Destructor passed to TF_NewTensor when it reuses a numpy buffer. Stores a
-// pointer to the pyobj in a buffer to be dereferenced later when we're actually
-// holding the GIL.
-void DelayedNumpyDecref(void* data, size_t len, void* obj) {
-  mutex_lock ml(*DelayedDecrefLock());
-  DecrefCache()->push_back(obj);
+        PosixMmapReadableFile::~PosixMmapReadableFile() {
+  int ret = munmap(mmapped_region_, length_);
+  if (ret != 0) {
+    fprintf(stdout, 'failed to munmap %p length %' ROCKSDB_PRIszt ' \n',
+            mmapped_region_, length_);
+  }
+  close(fd_);
 }
     
-    #include 'tensorflow/python/lib/core/numpy.h'
     
-     private:
-  static PyExceptionRegistry* singleton_;
-  PyExceptionRegistry() = default;
+    {#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_AIX)
+  virtual size_t GetUniqueId(char* id, size_t max_size) const override;
+#endif
+  virtual void Hint(AccessPattern pattern) override;
+  virtual Status InvalidateCache(size_t offset, size_t length) override;
+  virtual bool use_direct_io() const override { return use_direct_io_; }
+  virtual size_t GetRequiredBufferAlignment() const override {
+    return logical_sector_size_;
+  }
+};
     
-    
-    {
-    {} // namespace test
-} // namespace c10d
-
-    
-    SHOULD_NOT_DO_GRADIENT(EnforceFinite);
-    
-    #endif // CAFFE2_OPERATORS_FREE_OP_H_
-
-    
-    OPERATOR_SCHEMA(GivenTensorDoubleFill)
-    .NumInputs(0, 1)
-    .NumOutputs(1)
-    .AllowInplace({{0, 0}})
-    .Arg(
-        'values',
-        'The value for the elements of the output tensor.',
-        true /* required */)
-    .Arg(
-        'shape',
-        'The shape of the output tensor.'
-        'Cannot set the shape argument and pass in an input at the same time.')
-    .Arg(
-        'extra_shape',
-        'The additional dimensions appended at the end of the shape indicated'
-        'by the input blob.'
-        'Cannot set the extra_shape argument when there is no input blob.')
-    .Arg(
-        'input_as_shape',
-        '1D tensor containing the desired output shape. First input must be in CPU context.')
-    .TensorInferenceFunction(
-        FillerTensorInference<TensorProto_DataType_DOUBLE>);
-    
-    template <>
-void GluOp<float, CPUContext>::ComputeGlu(
-    const int M,
-    const int split_dim,
-    const int N,
-    const float* Xdata,
-    float* Ydata) {
-  const int xStride = 2 * split_dim * N;
-  const int yStride = split_dim * N;
-  for (int i = 0; i < M; ++i) {
-    const int idx = i * xStride;
-    const int idy = i * yStride;
-    for (int j = 0; j < split_dim; ++j) {
-      const int jN = j * N;
-      const int jdx1 = idx + jN;
-      const int jdx2 = idx + (j + split_dim) * N;
-      const int jdy = idy + jN;
-      for (int k = 0; k < N; ++k) {
-        const float x1 = Xdata[jdx1 + k];
-        const float x2 = Xdata[jdx2 + k];
-        Ydata[jdy + k] = x1 * sigmoid(x2);
+      // When flush happens, it determines whether to trigger compaction. If
+  // triggered_writes_stop is true, it will also set the retry flag of
+  // compaction-task to true.
+  void OnFlushCompleted(
+      DB* db, const FlushJobInfo& info) override {
+    CompactionTask* task = PickCompaction(db, info.cf_name);
+    if (task != nullptr) {
+      if (info.triggered_writes_stop) {
+        task->retry_on_fail = true;
       }
+      // Schedule compaction in a different thread.
+      ScheduleCompaction(task);
     }
   }
-}
+    
+      ////////////////////////////////////////////////////////
+  //
+  // Simple OptimisticTransaction Example ('Read Committed')
+  //
+  ////////////////////////////////////////////////////////
+    
+    // Move all L0 files to target_level skipping compaction.
+// This operation succeeds only if the files in L0 have disjoint ranges; this
+// is guaranteed to happen, for instance, if keys are inserted in sorted
+// order. Furthermore, all levels between 1 and target_level must be empty.
+// If any of the above condition is violated, InvalidArgument will be
+// returned.
+Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
+                 int target_level = 1);
+    
+      // the thread pool id
+  uint64_t thread_pool_id;
     
     
-    {	return userOnly;
-}
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
+    {}  // namespace rocksdb
 
     
-    // State shared by all concurrent executions of the same benchmark.
-struct SharedState {
-  port::Mutex mu;
-  port::CondVar cv GUARDED_BY(mu);
-  int total GUARDED_BY(mu);
-    }
-    
-    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
-#define STORAGE_LEVELDB_DB_DB_ITER_H_
-    
-    #ifndef STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-#define STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-    
-      // crc32c values for all supported record types.  These are
-  // pre-computed to reduce the overhead of computing the crc of the
-  // record type stored in the header.
-  uint32_t type_crc_[kMaxRecordType + 1];
-    
-      void ScanTable(uint64_t number) {
-    TableInfo t;
-    t.meta.number = number;
-    std::string fname = TableFileName(dbname_, number);
-    Status status = env_->GetFileSize(fname, &t.meta.file_size);
-    if (!status.ok()) {
-      // Try alternate file name.
-      fname = SSTTableFileName(dbname_, number);
-      Status s2 = env_->GetFileSize(fname, &t.meta.file_size);
-      if (s2.ok()) {
-        status = Status::OK();
-      }
-    }
-    if (!status.ok()) {
-      ArchiveFile(TableFileName(dbname_, number));
-      ArchiveFile(SSTTableFileName(dbname_, number));
-      Log(options_.info_log, 'Table #%llu: dropped: %s',
-          (unsigned long long) t.meta.number,
-          status.ToString().c_str());
-      return;
-    }
-    }
-    
-      // Modified only by Insert().  Read racily by readers, but stale
-  // values are ok.
-  port::AtomicPointer max_height_;   // Height of the entire list
+      enum CompactionPropertyType : int {
+    COMPACTION_JOB_ID = 0,
+    COMPACTION_INPUT_OUTPUT_LEVEL,
+    COMPACTION_PROP_FLAGS,
+    COMPACTION_TOTAL_INPUT_BYTES,
+    COMPACTION_BYTES_READ,
+    COMPACTION_BYTES_WRITTEN,
+    NUM_COMPACTION_PROPERTIES
+  };
     
     
-    
-    /*
- * If Trace::hhbbc_time >= 1, print some stats about the program to a
- * temporary file.  If it's greater than or equal to 2, also dump it
- * to stdout.
- */
-void print_stats(const Index&, const php::Program&);
-    
-    
-    {  // We had a counted inner array---we need to do an O(N) copy to get the
-  // collection into the request local heap.
-  auto const apcArr = APCArray::fromHandle(m_arrayHandle);
-  auto const col = Object::attach(collections::alloc(m_colType));
-  switch (m_colType) {
-  case CollectionType::ImmVector:
-  case CollectionType::Vector:
-    fillCollection(static_cast<BaseVector*>(col.get()), apcArr);
-    break;
-  case CollectionType::ImmSet:
-  case CollectionType::Set:
-    fillCollection(static_cast<BaseSet*>(col.get()), apcArr);
-    break;
-  case CollectionType::ImmMap:
-  case CollectionType::Map:
-    fillMap(static_cast<BaseMap*>(col.get()), apcArr);
-    break;
-  case CollectionType::Pair:
-    always_assert(0);
-    break;
+    {  explicit BackupableDBOptions(
+      const std::string& _backup_dir, Env* _backup_env = nullptr,
+      bool _share_table_files = true, Logger* _info_log = nullptr,
+      bool _sync = true, bool _destroy_old_data = false,
+      bool _backup_log_files = true, uint64_t _backup_rate_limit = 0,
+      uint64_t _restore_rate_limit = 0, int _max_background_operations = 1,
+      uint64_t _callback_trigger_interval_size = 4 * 1024 * 1024,
+      int _max_valid_backups_to_open = INT_MAX)
+      : backup_dir(_backup_dir),
+        backup_env(_backup_env),
+        share_table_files(_share_table_files),
+        info_log(_info_log),
+        sync(_sync),
+        destroy_old_data(_destroy_old_data),
+        backup_log_files(_backup_log_files),
+        backup_rate_limit(_backup_rate_limit),
+        restore_rate_limit(_restore_rate_limit),
+        share_files_with_checksum(false),
+        max_background_operations(_max_background_operations),
+        callback_trigger_interval_size(_callback_trigger_interval_size),
+        max_valid_backups_to_open(_max_valid_backups_to_open) {
+    assert(share_table_files || !share_files_with_checksum);
   }
-  return col;
-}
-    
-    #endif
-
-    
-    // No `ini` binding yet. Hdf still takes precedence but will be removed
-// once we have made all options ini-aware. All new settings should
-// use the ini path of this method (i.e., pass a bogus Hdf or keep it null)
-void Config::Iterate(std::function<void (const IniSettingMap&,
-                                         const Hdf&,
-                                         const std::string&)> cb,
-                     const IniSettingMap &ini, const Hdf& config,
-                     const std::string &name,
-                     const bool prepend_hhvm /* = true */) {
-  Hdf hdf = name.empty() ? config : config[name];
-  if (hdf.exists() && !hdf.isEmpty()) {
-    for (Hdf c = hdf.firstChild(); c.exists(); c = c.next()) {
-      cb(IniSetting::Map::object, c, '');
-    }
-  } else {
-    Hdf empty;
-    auto ini_value = name.empty() ? ini :
-      ini_iterate(ini, IniName(name, prepend_hhvm));
-    if (ini_value.isArray()) {
-      for (ArrayIter iter(ini_value.toArray()); iter; ++iter) {
-        cb(iter.second(), empty, iter.first().toString().toCppString());
-      }
-    }
-  }
-}
-    
-    String ArrayDirectory::path() {
-  if (!m_it) {
-    return empty_string();
-  }
-    }
-    
-        // skipping .  .. hidden files
-    if (ename[0] == '.' || !*ename) {
-      continue;
-    }
-    auto fe = fullPath + ename;
-    struct stat se;
-    if (stat(fe.c_str(), &se)) {
-      Logger::Error('FileUtil::find(): unable to stat %s', fe.c_str());
-      continue;
-    }
-    
-    namespace HPHP {
-    }
-    
-    
-    {  rocksdb::CacheBench bench;
-  if (FLAGS_populate_cache) {
-    bench.PopulateCache();
-  }
-  if (bench.Run()) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
-    
-      // Will be called while on the write thread before the write executes.  If
-  // this function returns a non-OK status, the write will be aborted and this
-  // status will be returned to the caller of DB::Write().
-  virtual Status Callback(DB* db) = 0;
-    
-    class PosixDirectory : public Directory {
- public:
-  explicit PosixDirectory(int fd) : fd_(fd) {}
-  ~PosixDirectory();
-  virtual Status Fsync() override;
-    }
-    
-    class MyMerge : public rocksdb::MergeOperator {
- public:
-  virtual bool FullMergeV2(const MergeOperationInput& merge_in,
-                           MergeOperationOutput* merge_out) const override {
-    merge_out->new_value.clear();
-    if (merge_in.existing_value != nullptr) {
-      merge_out->new_value.assign(merge_in.existing_value->data(),
-                                  merge_in.existing_value->size());
-    }
-    for (const rocksdb::Slice& m : merge_in.operand_list) {
-      fprintf(stderr, 'Merge(%s)\n', m.ToString().c_str());
-      // the compaction filter filters out bad values
-      assert(m.ToString() != 'bad');
-      merge_out->new_value.assign(m.data(), m.size());
-    }
-    return true;
-  }
-    }
-    
-      // initialize BlockBasedTableOptions
-  auto cache = NewLRUCache(1 * 1024 * 1024 * 1024);
-  BlockBasedTableOptions bbt_opts;
-  bbt_opts.block_size = 32 * 1024;
-  bbt_opts.block_cache = cache;
-    
-      // the number of compaction input records.
-  uint64_t num_input_records;
-  // the number of compaction input files.
-  size_t num_input_files;
-  // the number of compaction input files at the output level.
-  size_t num_input_files_at_output_level;
-    
-    // NewThreadPool() is a function that could be used to create a ThreadPool
-// with `num_threads` background threads.
-extern ThreadPool* NewThreadPool(int num_threads);
+};
     
     namespace rocksdb {
     }
+    
+        // serialize the JSON objects
+    std::cout << j_object_t << '\n';
+    std::cout << j_map << '\n';
+    std::cout << j_umap << '\n';
+    std::cout << j_mmap << '\n';
+    std::cout << j_ummap << '\n\n';
+    
+    BOOST_FORCEINLINE void pause() BOOST_NOEXCEPT
+{
+#if defined(_MSC_VER) && (defined(_M_AMD64) || defined(_M_IX86))
+    _mm_pause();
+#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
+    __asm__ __volatile__('pause;');
+#endif
+}
