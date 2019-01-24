2bbca74f9789deb66e0d49c1e7c3420460ceff2d
SUNsung <weatherius@gmail.com>
2019-01-24 14:07:10 +0200

    Rewrite [28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp] File in: 2019-01-24 14:07:09

--- cccccc/28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp
+++ cccccc/28pgCmfirWAUc28hoouQJy9E28aGhY4vbP3cc288ml.cpp
@@ -1,308 +1,90 @@
 
         
-        #endif  // CONTENT_NW_SRC_API_APP_APP_H_
-    
-    namespace nwapi {
+        struct CaffeDataParam : public dmlc::Parameter<CaffeDataParam> {
+  /*! \brief protobuf text */
+  ::caffe::LayerParameter prototxt;
+  /*! \brief number of iterations per epoch */
+  int num_examples;
+  /*! \brief data mode */
+  bool flat;
     }
     
-    void MenuItem::CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result) {
-  if (method == 'GetChecked') {
-    result->AppendBoolean(GetChecked());
-  } else {
-    NOTREACHED() << 'Invalid call to MenuItem method:' << method
-                 << ' arguments:' << arguments;
-  }
-}
-    
-    NwClipboardSetListSyncFunction::NwClipboardSetListSyncFunction() {
-}
-    
-    namespace extensions {
+        caffeOp_->SetUp(bot_blobs, top_blobs);
+    CHECK_EQ(in_shape->size(), caffeOp_->blobs().size() + param_.num_data);
+    // Initialize out shapes
+    out_shape->clear();
+    for (auto blob : top_blobs) {
+      TShape tshape = caffe::Vector2TShape(blob->shape());
+      out_shape->push_back(tshape);
     }
     
     
-    {    private:
-      DISALLOW_COPY_AND_ASSIGN(NwScreenGetScreensFunction);      
-  };
-    
-    namespace google {
-namespace protobuf {
-namespace python {
-    }
-    }
-    }
-    
-    #include <google/protobuf/compiler/csharp/csharp_doc_comment.h>
-#include <google/protobuf/compiler/csharp/csharp_enum.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_options.h>
-    
-    TEST(CSharpEnumValue, PascalCasedPrefixStripping) {
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO__BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'FOO_BAR_BAZ'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'Foo_BarBaz'));
-  EXPECT_EQ('Bar', GetEnumValueName('FO_O', 'FOO_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('FOO', 'F_O_O_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
-  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO'));
-  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO___'));
-  // Identifiers can't start with digits
-  EXPECT_EQ('_2Bar', GetEnumValueName('Foo', 'FOO_2_BAR'));
-  EXPECT_EQ('_2', GetEnumValueName('Foo', 'FOO___2'));
-}
-    
-    #include <google/protobuf/compiler/java/java_context.h>
-#include <google/protobuf/compiler/java/java_doc_comment.h>
-#include <google/protobuf/compiler/java/java_helpers.h>
-#include <google/protobuf/compiler/java/java_name_resolver.h>
-#include <google/protobuf/io/printer.h>
-    
-      // Limit the number of bytes per line.
-  static const int kBytesPerLine = 40;
-  // Limit the number of lines per string part.
-  static const int kLinesPerPart = 400;
-  // Every block of bytes, start a new string literal, in order to avoid the
-  // 64k length limit. Note that this value needs to be <64k.
-  static const int kBytesPerPart = kBytesPerLine * kLinesPerPart;
-  for (int i = 0; i < file_data.size(); i += kBytesPerLine) {
-    if (i > 0) {
-      if (i % kBytesPerPart == 0) {
-        printer->Print(',\n');
-      } else {
-        printer->Print(' +\n');
+using namespace mxnet;
+// http://www.64lines.com/jpeg-width-height
+// Gets the JPEG size from the array of data passed to the function, file reference: http://www.obrador.com/essentialjpeg/headerinfo.htm
+bool get_jpeg_size(const unsigned char* data, mx_uint data_size, mx_uint *width, mx_uint *height) {
+  // Check for valid JPEG image
+  mx_uint i = 0;  // Keeps track of the position within the file
+  if (data[i] == 0xFF && data[i+1] == 0xD8 && data[i+2] == 0xFF && data[i+3] == 0xE0) {
+    i += 4;
+    // Check for valid JPEG header (null terminated JFIF)
+    if (data[i+2] == 'J' && data[i+3] == 'F' && data[i+4] == 'I'
+        && data[i+5] == 'F' && data[i+6] == 0x00) {
+      // Retrieve the block length of the first block since
+      // the first block will not contain the size of file
+      uint16_t block_length = data[i] * 256 + data[i+1];
+      while (i < data_size) {
+        i+=block_length;  // Increase the file index to get to the next block
+        if (i >= data_size) return false;  // Check to protect against segmentation faults
+        if (data[i] != 0xFF) return false;  // Check that we are truly at the start of another block
+        if (data[i+1] == 0xC0) {
+          // 0xFFC0 is the 'Start of frame' marker which contains the file size
+          // The structure of the 0xFFC0 block is quite simple
+          // [0xFFC0][ushort length][uchar precision][ushort x][ushort y]
+          *height = data[i+5]*256 + data[i+6];
+          *width = data[i+7]*256 + data[i+8];
+          return true;
+        } else {
+          i+=2;  // Skip the block marker
+          block_length = data[i] * 256 + data[i+1];  // Go to the next block
+        }
       }
+      return false;  // If this point is reached then no size was found
+    } else {
+      return false;  // Not a valid JFIF string
     }
-    printer->Print('\'$data$\'',
-      'data', CEscape(file_data.substr(i, kBytesPerLine)));
+  } else {
+    return false;  // Not a valid SOI header
   }
-    
-    void OneofGenerator::GeneratePropertyImplementation(io::Printer* printer) {
-  printer->Print(
-      variables_,
-      '@dynamic $name$OneOfCase;\n');
 }
     
     
     {
-    {
-    {}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
+    {}  // namespace io
+}  // namespace mxnet
 
     
-      // Since the basic IO manipulators are overloaded for both narrow
-  // and wide streams, we have to provide this specialized definition
-  // of operator <<, even though its body is the same as the
-  // templatized version above.  Without this definition, streaming
-  // endl or other basic IO manipulators to Message will confuse the
-  // compiler.
-  Message& operator <<(BasicNarrowIoManip val) {
-    *ss_ << val;
-    return *this;
+      void sendMessage();
+    
+      virtual bool isReply() const CXX11_OVERRIDE;
+    
+      const std::vector<std::shared_ptr<DHTNode>>& getNodes() const
+  {
+    return nodes_;
   }
     
-    template <typename Generator1, typename Generator2, typename Generator3,
-    typename Generator4, typename Generator5, typename Generator6,
-    typename Generator7, typename Generator8>
-internal::CartesianProductHolder8<Generator1, Generator2, Generator3,
-    Generator4, Generator5, Generator6, Generator7, Generator8> Combine(
-    const Generator1& g1, const Generator2& g2, const Generator3& g3,
-        const Generator4& g4, const Generator5& g5, const Generator6& g6,
-        const Generator7& g7, const Generator8& g8) {
-  return internal::CartesianProductHolder8<Generator1, Generator2, Generator3,
-      Generator4, Generator5, Generator6, Generator7, Generator8>(
-      g1, g2, g3, g4, g5, g6, g7, g8);
-}
+    DHTSetup::DHTSetup() = default;
     
-      // Gets the line in the source file where the test part took place,
-  // or -1 if it's unknown.
-  int line_number() const { return line_number_; }
+    class DownloadEngine;
+class Command;
     
-    // This specialization is used when the first argument to ASSERT_EQ()
-// is a null pointer literal, like NULL, false, or 0.
-template <>
-class EqHelper<true> {
- public:
-  // We define two overloaded versions of Compare().  The first
-  // version will be picked when the second argument to ASSERT_EQ() is
-  // NOT a pointer, e.g. ASSERT_EQ(0, AnIntFunction()) or
-  // EXPECT_EQ(false, a_bool).
-  template <typename T1, typename T2>
-  static AssertionResult Compare(
-      const char* expected_expression,
-      const char* actual_expression,
-      const T1& expected,
-      const T2& actual,
-      // The following line prevents this overload from being considered if T2
-      // is not a pointer type.  We need this because ASSERT_EQ(NULL, my_ptr)
-      // expands to Compare('', '', NULL, my_ptr), which requires a conversion
-      // to match the Secret* in the other overload, which would otherwise make
-      // this template match better.
-      typename EnableIf<!is_pointer<T2>::value>::type* = 0) {
-    return CmpHelperEQ(expected_expression, actual_expression, expected,
-                       actual);
-  }
-    }
     
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38, typename T39, typename T40,
-    typename T41, typename T42, typename T43, typename T44>
-struct Types44 {
-  typedef T1 Head;
-  typedef Types43<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
-      T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29,
-      T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42, T43,
-      T44> Tail;
+    {  virtual bool finished() = 0;
 };
     
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
+    const std::string DHTUnknownMessage::UNKNOWN('unknown');
     
-    #include 'sample2.h'
-#include 'gtest/gtest.h'
-    
-    int	b3g_overlappingPairs = 0;
-int b3g_removePairs =0;
-int b3g_addedPairs =0;
-int b3g_findPairs =0;
-    
-    	float		m_erp2;//used in Split Impulse
-	float		m_globalCfm;//constraint force mixing
-	float		m_splitImpulsePenetrationThreshold;
-	float		m_splitImpulseTurnErp;
-    
-    
-b3Scalar b3TypedConstraint::getMotorFactor(b3Scalar pos, b3Scalar lowLim, b3Scalar uppLim, b3Scalar vel, b3Scalar timeFact)
+    DNSCache::CacheEntry::CacheEntry(const std::string& hostname, uint16_t port)
+    : hostname_(hostname), port_(port)
 {
-	if(lowLim > uppLim)
-	{
-		return b3Scalar(1.0f);
-	}
-	else if(lowLim == uppLim)
-	{
-		return b3Scalar(0.0f);
-	}
-	b3Scalar lim_fact = b3Scalar(1.0f);
-	b3Scalar delta_max = vel / timeFact;
-	if(delta_max < b3Scalar(0.0f))
-	{
-		if((pos >= lowLim) && (pos < (lowLim - delta_max)))
-		{
-			lim_fact = (lowLim - pos) / delta_max;
-		}
-		else if(pos  < lowLim)
-		{
-			lim_fact = b3Scalar(0.0f);
-		}
-		else
-		{
-			lim_fact = b3Scalar(1.0f);
-		}
-	}
-	else if(delta_max > b3Scalar(0.0f))
-	{
-		if((pos <= uppLim) && (pos > (uppLim - delta_max)))
-		{
-			lim_fact = (uppLim - pos) / delta_max;
-		}
-		else if(pos  > uppLim)
-		{
-			lim_fact = b3Scalar(0.0f);
-		}
-		else
-		{
-			lim_fact = b3Scalar(1.0f);
-		}
-	}
-	else
-	{
-			lim_fact = b3Scalar(0.0f);
-	}
-	return lim_fact;
 }
-    
-    
-	///internal method used by the constraint solver, don't use them directly
-	virtual	void	setupSolverConstraint(b3ConstraintArray& ca, int solverBodyA,int solverBodyB, b3Scalar timeStep)
-	{
-        (void)ca;
-        (void)solverBodyA;
-        (void)solverBodyB;
-        (void)timeStep;
-	}
-	
-	///internal method used by the constraint solver, don't use them directly
-	virtual void getInfo1 (b3ConstraintInfo1* info,const b3RigidBodyData* bodies)=0;
-    
-    inline void b3IntegrateTransform( __global b3RigidBodyData_t* body, float timeStep, float angularDamping, b3Float4ConstArg gravityAcceleration)
-{
-	float BT_GPU_ANGULAR_MOTION_THRESHOLD = (0.25f * 3.14159254f);
-	
-	if( (body->m_invMass != 0.f))
-	{
-		//angular velocity
-		{
-			b3Float4 axis;
-			//add some hardcoded angular damping
-			body->m_angVel.x *= angularDamping;
-			body->m_angVel.y *= angularDamping;
-			body->m_angVel.z *= angularDamping;
-			
-			b3Float4 angvel = body->m_angVel;
-			float fAngle = b3Sqrt(b3Dot3F4(angvel, angvel));
-			//limit the angular motion
-			if(fAngle*timeStep > BT_GPU_ANGULAR_MOTION_THRESHOLD)
-			{
-				fAngle = BT_GPU_ANGULAR_MOTION_THRESHOLD / timeStep;
-			}
-			if(fAngle < 0.001f)
-			{
-				// use Taylor's expansions of sync function
-				axis = angvel * (0.5f*timeStep-(timeStep*timeStep*timeStep)*0.020833333333f * fAngle * fAngle);
-			}
-			else
-			{
-				// sync(fAngle) = sin(c*fAngle)/t
-				axis = angvel * ( b3Sin(0.5f * fAngle * timeStep) / fAngle);
-			}
-			b3Quat dorn;
-			dorn.x = axis.x;
-			dorn.y = axis.y;
-			dorn.z = axis.z;
-			dorn.w = b3Cos(fAngle * timeStep * 0.5f);
-			b3Quat orn0 = body->m_quat;
-    }
-    }
-    }
-    
-    void	b3GeometryUtil::getVerticesFromPlaneEquations(const b3AlignedObjectArray<b3Vector3>& planeEquations , b3AlignedObjectArray<b3Vector3>& verticesOut )
-{
-	const int numbrushes = planeEquations.size();
-	// brute force:
-	for (int i=0;i<numbrushes;i++)
-	{
-		const b3Vector3& N1 = planeEquations[i];
-		
-    }
-    }
-    
-            // Add arguments to instance name
-        size_t arg_i = 0;
-        for (auto const& arg : args) {
-          instance.name += '/';
-    }
-    
-    #endif  // COMPLEXITY_H_
