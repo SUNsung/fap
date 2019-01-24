cdb8c3e2094854564170dfc0141709214a7a590d
SUNsung <weatherius@gmail.com>
2019-01-24 12:17:09 +0200

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2019-01-24 12:17:08

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,137 +1,113 @@
 
         
-          # Clean a top-level (bin, sbin, lib) directory, recursively, by fixing file
-  # permissions and removing .la files, unless the files (or parent
-  # directories) are protected by skip_clean.
-  #
-  # bin and sbin should not have any subdirectories; if either do that is
-  # caught as an audit warning
-  #
-  # lib may have a large directory tree (see Erlang for instance), and
-  # clean_dir applies cleaning rules to the entire tree
-  def clean_dir(d)
-    d.find do |path|
-      path.extend(ObserverPathnameExtension)
+                default_connectors = {
+          words_connector: ', ',
+          two_words_connector: ' and ',
+          last_word_connector: ', and '
+        }
+        if defined?(I18n)
+          i18n_connectors = I18n.translate(:'support.array', locale: options[:locale], default: {})
+          default_connectors.merge!(i18n_connectors)
+        end
+        options = default_connectors.merge!(options)
     
-      def origin
-    Homebrew.git_origin || '(none)'
-  end
-    
-        # Unbrewed uses the PREFIX, which will exist
-    # Things below use the CELLAR, which doesn't until the first formula is installed.
-    unless HOMEBREW_CELLAR.exist?
-      raise NoSuchKegError.new(ARGV.named.first) if ARGV.named.any?
-      return
-    end
-    
-      def self.bottle_sha1(*)
-  end
-    
-          it 'Does not use pattern matching for tag name if so requested' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          changelog_from_git_commits()
-        end').runner.execute(:test)
-    
-            expect(result[3]).to start_with('security set-keychain-settings')
-        expect(result[3]).to include('-t 600')
-        expect(result[3]).to include('-l')
-        expect(result[3]).to include('-u')
-        expect(result[3]).to include('/tmp/test.keychain')
-      end
-    
-          it 'generates the correct git command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          git_commit(path: './fastlane/README.md', message: 'message')
-        end').runner.execute(:test)
-    
-          describe 'misc features' do
-        it 'makes it non optional by default' do
-          c = FastlaneCore::ConfigItem.new(key: :test,
-                                 default_value: '123')
-          expect(c.optional).to eq(false)
+            def render(&block)
+          render_collection_for(RadioButtonBuilder, &block)
         end
     
-    # The * turns the array into a parameter list
-# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
-# We need to use that, because otherwise invocations like
-# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
-exec(*exec_arr)
-
+              super(object_name, method_name, template_object, options)
+        end
     
-        def create
-      authorize :custom_emoji, :create?
+      def pin
+    pin_at(@f.installed_kegs.map(&:version).max)
+  end
     
-    class Api::SubscriptionsController < Api::BaseController
-  before_action :set_account
-  respond_to :txt
+    module Sinatra
+  class Application < Base
     
-      before_action :require_user!
+    desc 'run specs'
+task(:spec) { ruby '-S rspec spec' }
     
-      namespace :doc do
-    task :sass do
-      require scope('lib/sass')
-      Dir[scope('yard/default/**/*.sass')].each do |sass|
-        File.open(sass.gsub(/sass$/, 'css'), 'w') do |f|
-          f.write(Sass::Engine.new(File.read(sass)).render)
+          def self.random_token
+        SecureRandom.base64(TOKEN_LENGTH)
+      end
+    
+          def escape(object)
+        case object
+        when Hash   then escape_hash(object)
+        when Array  then object.map { |o| escape(o) }
+        when String then escape_string(object)
+        when Tempfile then object
+        else nil
         end
       end
-    end
     
-        # Return the first {Sass::Selector::SimpleSequence} in a
-    # {Sass::Tree::RuleNode}.
-    #
-    # @param rule [Sass::Tree::RuleNode]
-    # @return [Sass::Selector::SimpleSequence, String]
-    def first_sseq(rule)
-      first_seq(rule).members.first
-    end
+            header = file.read(TAR_CHUNK_SIZE)
+        typeflag = header[TAR_TYPEFLAG_OFFSET]
+        record_length = header[TAR_LENGTH_OFFSET_START..TAR_LENGTH_OFFSET_END].to_i(8)
     
-        COLORS = {:red => 31, :green => 32, :yellow => 33}
-    
-        # Tells optparse how to parse the arguments.
-    #
-    # @param opts [OptionParser]
-    def set_opts(opts)
-      opts.banner = <<END
-Usage: sass-convert [options] [INPUT] [OUTPUT]
-    
-          # Get the publicly-visible URL for an imported file. This URL is used by
-      # source maps to link to the source stylesheet. This may return `nil` to
-      # indicate that no public URL is available; however, this will cause
-      # sourcemap generation to fail if any CSS is generated from files imported
-      # from this importer.
-      #
-      # If an absolute 'file:' URI can be produced for an imported file, that
-      # should be preferred to returning `nil`. However, a URL relative to
-      # `sourcemap_directory` should be preferred over an absolute 'file:' URI.
-      #
-      # @param uri [String] A URI known to be valid for this importer.
-      # @param sourcemap_directory [String, NilClass] The absolute path to a
-      #   directory on disk where the sourcemap will be saved. If uri refers to
-      #   a file on disk that's accessible relative to sourcemap_directory, this
-      #   may return a relative URL. This may be `nil` if the sourcemap's
-      #   eventual location is unknown.
-      # @return [String?] The publicly-visible URL for this file, or `nil`
-      #   indicating that no publicly-visible URL exists. This should be
-      #   appropriately URL-escaped.
-      def public_url(uri, sourcemap_directory)
-        return if @public_url_warning_issued
-        @public_url_warning_issued = true
-        Sass::Util.sass_warn <<WARNING
-WARNING: #{self.class.name} should define the #public_url method.
-WARNING
-        nil
+        if File.directory?(source)
+      if !File.symlink?(source)
+        # Create a directory if this path is a directory
+        logger.debug('Creating', :directory => destination)
+        if !File.directory?(destination)
+          FileUtils.mkdir(destination)
+        end
+      else
+        # Linking symlinked directories causes a hardlink to be created, which
+        # results in the source directory being wiped out during cleanup,
+        # so copy the symlink.
+        logger.debug('Copying symlinked directory', :source => source,
+                      :destination => destination)
+        FileUtils.copy_entry(source, destination)
       end
+    else
+      # Otherwise try copying the file.
+      begin
+        logger.debug('Linking', :source => source, :destination => destination)
+        File.link(source, destination)
+      rescue Errno::ENOENT, Errno::EXDEV, Errno::EPERM
+        # Hardlink attempt failed, copy it instead
+        logger.debug('Copying', :source => source, :destination => destination)
+        copy_entry(source, destination)
+      rescue Errno::EEXIST
+        sane_path = destination.gsub(staging_path, '')
+        logger.error('Cannot copy file, the destination path is probably a directory and I attempted to write a file.', :path => sane_path, :staging => staging_path)
+      end
+    end
     
-          # Given an `@import`ed path, returns an array of possible
-      # on-disk filenames and their corresponding syntaxes for that path.
-      #
-      # @param name [String] The filename.
-      # @return [Array(String, Symbol)] An array of pairs.
-      #   The first element of each pair is a filename to look for;
-      #   the second element is the syntax that file would be in (`:sass` or `:scss`).
-      def possible_files(name)
-        name = escape_glob_characters(name)
-        dirname, basename, extname = split(name)
-        sorted_exts = extensions.sort
-        syntax = extensions[extname]
+      option '--postinstall-action', 'POSTINSTALL_ACTION',
+    'Post-install action provided in package metadata. ' \
+    'Optionally one of '#{POSTINSTALL_ACTIONS.join('', '')}'.' do |value|
+    if !POSTINSTALL_ACTIONS.include?(value)
+      raise ArgumentError, 'osxpkg-postinstall-action value of '#{value}' is invalid. ' \
+        'Must be one of #{POSTINSTALL_ACTIONS.join(', ')}'
+    end
+    value
+  end
+    
+        # do channel-update if requested
+    if attributes[:pear_channel_update?]
+      channel = attributes[:pear_channel] || 'pear'
+      logger.info('Updating the channel', :channel => channel)
+      safesystem('pear', '-c', config, 'channel-update', channel)
+    end
+    
+      def iteration
+    return @iteration ? @iteration : 1
+  end
+    
+      require 'pleaserun/platform/systemd'
+  require 'pleaserun/platform/upstart'
+  require 'pleaserun/platform/launchd'
+  require 'pleaserun/platform/sysv'
+    
+      # Where we keep metadata and post install scripts and such
+  def fpm_meta_path
+    @fpm_meta_path ||= begin
+                         path = File.join(staging_path, '.fpm')
+                         FileUtils.mkdir_p(path)
+                         path
+                       end
+  end
+end
