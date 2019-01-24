abecb5b0eaefe85be74493afeca397f4080caa08
SUNsung <weatherius@gmail.com>
2019-01-24 11:11:50 +0200

    Rewrite [55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb] File in: 2019-01-24 11:11:49

--- ruby/55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb
+++ ruby/55uXiG.Zi9qS255Ypq2xUoiEE554wF49PUDjdk55ca8.rb
@@ -1,47 +1,158 @@
 
         
-              # @see Base#\_store
-      def _store(key, version, sha, contents)
-        compiled_filename = path_to(key)
-        FileUtils.mkdir_p(File.dirname(compiled_filename))
-        Sass::Util.atomic_create_and_write_file(compiled_filename) do |f|
-          f.puts(version)
-          f.puts(sha)
-          f.write(contents)
+          EmailToken.where(user_id: smoke_user.id).update_all(confirmed: true)
+end
+
+    
+        context '(de)activating users' do
+      it 'does not show deactivation buttons for the current user' do
+        visit admin_users_path
+        expect(page).to have_no_css('a[href='/admin/users/#{users(:jane).id}/deactivate']')
+      end
+    
+        it 'does not show the target select2 field when the agent can not create events' do
+      select_agent_type('Email Agent')
+      expect(page).to have_content('This type of Agent cannot create events.')
+    end
+  end
+    
+      it 'requires a URL or file uplaod' do
+    visit new_scenario_imports_path
+    click_on 'Start Import'
+    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
+  end
+    
+      let :agent do
+    Agents::WebsiteAgent.create!(
+      user: users(:bob),
+      name: 'xkcd',
+      options: valid_options,
+      keep_events_for: 2.days
+    )
+  end
+    
+        it 'requires an agent' do
+      @log.agent = nil
+      expect(@log).not_to be_valid
+      expect(@log).to have(1).error_on(:agent)
+    end
+    
+      describe 'path request must exist' do
+    it 'should check that validation added if path does not exist' do
+      opts = @opts.tap { |o| o.delete('path') }
+      @checker = Agents::AftershipAgent.new(:name => 'tectonic', :options => opts)
+      @checker.user = users(:bob)
+      expect(@checker.save).to eq false
+      expect(@checker.errors.full_messages.first).to eq('You need to specify a path request')
+    end
+  end
+    
+        it 'should raise error when invalid response arrives' do
+      stub(HTTParty).post { {'blah' => 'blah'} }
+      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Invalid response from Boxcar:/)
+    end
+    
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
+      end
+    end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
+  end
+end
+
+    
+        def validate_plugins!
+      @plugins_to_package.each do |plugin_name|
+        if INVALID_PLUGINS_TO_EXPLICIT_PACK.any? { |invalid_name| plugin_name =~ invalid_name }
+          raise UnpackablePluginError, 'Cannot explicitly pack `#{plugin_name}` for offline installation'
         end
-      rescue Errno::EACCES
-        # pass
       end
+    end
     
-        COLORS = {:red => 31, :green => 32, :yellow => 33}
+      it 'returns the source' do
+    expect(subject.source).to eq(source)
+  end
     
-          out =
-        Sass::Util.silence_sass_warnings do
-          if @options[:from] == :css
-            require 'sass/css'
-            Sass::CSS.new(read(input), @options[:for_tree]).render(@options[:to])
-          else
-            if input_path
-              Sass::Engine.for_file(input_path, @options[:for_engine])
-            else
-              Sass::Engine.new(read(input), @options[:for_engine])
-            end.to_tree.send('to_#{@options[:to]}', @options[:for_tree])
-          end
-        end
-    
-          opts.on('-s', '--stdin', :NONE,
-              'Read input from standard input instead of an input file.',
-              'This is the default if no input file is specified.') do
-        @options[:input] = $stdin
+        context 'update a specific plugin' do
+      it 'has executed successfully' do
+        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
+        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
+        expect(logstash).not_to have_installed?(plugin_name, previous_version)
       end
+    end
     
-          # A string representation of the importer.
-      # Should be overridden by subclasses.
+      let(:cop_config) { { 'EnforcedStyle' => 'symmetrical' } }
+    
+          # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
       #
-      # This is used to help debugging,
-      # and should usually just show the load path encapsulated by this importer.
+      # In this case, the `def` node destructures into:
       #
-      # @return [String]
-      def to_s
-        Sass::Util.abstract(self)
+      #   `method_name, arguments, body`
+      #
+      # while the `defs` node destructures into:
+      #
+      #   `receiver, method_name, arguments, body`
+      #
+      # so we reverse the destructured array to get the optional receiver
+      # at the end, where it can be discarded.
+      #
+      # @return [Array] the different parts of the `def` or `defs` node
+      def node_parts
+        to_a.reverse
       end
+    end
+  end
+end
+
+    
+          # Checks whether any argument of the node is a splat
+      # argument, i.e. `*splat`.
+      #
+      # @return [Boolean] whether the node is a splat argument
+      def splat_argument?
+        arguments? &&
+          (arguments.any?(&:splat_type?) || arguments.any?(&:restarg_type?))
+      end
+      alias rest_argument? splat_argument?
+    
+    ## -- Rsync Deploy config -- ##
+# Be sure your public key is listed in your server's ~/.ssh/authorized_keys file
+ssh_user       = 'user@domain.com'
+ssh_port       = '22'
+document_root  = '~/website.com/'
+rsync_delete   = false
+rsync_args     = ''  # Any extra arguments to pass to rsync
+deploy_default = 'rsync'
+    
+        def initialize(tag_name, markup, tokens)
+      @by = nil
+      @source = nil
+      @title = nil
+      if markup =~ FullCiteWithTitle
+        @by = $1
+        @source = $2 + $3
+        @title = $4.titlecase.strip
+      elsif markup =~ FullCite
+        @by = $1
+        @source = $2 + $3
+      elsif markup =~ AuthorTitle
+        @by = $1
+        @title = $2.titlecase.strip
+      elsif markup =~ Author
+        @by = $1
+      end
+      super
+    end
