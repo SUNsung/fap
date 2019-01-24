295cdea2611471b73da06c8ad951e65614740ceb
SUNsung <weatherius@gmail.com>
2019-01-24 04:26:53 +0200

    Rewrite [15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb] File in: 2019-01-24 04:26:53

--- ruby/15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb
+++ ruby/15xCQczuw6SdA1538Qg7xusA0E159WStPGajsdg15VnJ.rb
@@ -1,105 +1,130 @@
 
         
-                  if options['multiple']
-            add_default_name_and_id_for_value(@checked_value, options)
-            options.delete('multiple')
-          else
-            add_default_name_and_id(options)
+            it 'does not send previously configured control targets when the current agent does not support them' do
+      select_agent_type('Commander Agent')
+      select2('SF Weather', from: 'Control targets')
+      select_agent_type('Webhook Agent')
+      fill_in(:agent_name, with: 'No control targets')
+      click_on 'Save'
+      expect(page).to have_content('No control targets')
+      agent = Agent.find_by(name: 'No control targets')
+      expect(agent.control_targets).to eq([])
+    end
+    
+              expect(scenario_import.scenario.name).to eq(name)
+          expect(scenario_import.scenario.description).to eq(description)
+          expect(scenario_import.scenario.guid).to eq(guid)
+          expect(scenario_import.scenario.tag_fg_color).to eq(tag_fg_color)
+          expect(scenario_import.scenario.tag_bg_color).to eq(tag_bg_color)
+          expect(scenario_import.scenario.icon).to eq(icon)
+          expect(scenario_import.scenario.source_url).to eq(source_url)
+          expect(scenario_import.scenario.public).to be_falsey
+        end
+    
+        it 'outputs a structure containing name, description, the date, all agents & their links' do
+      data = exporter.as_json
+      expect(data[:name]).to eq(name)
+      expect(data[:description]).to eq(description)
+      expect(data[:source_url]).to eq(source_url)
+      expect(data[:guid]).to eq(guid)
+      expect(data[:schema_version]).to eq(1)
+      expect(data[:tag_fg_color]).to eq(tag_fg_color)
+      expect(data[:tag_bg_color]).to eq(tag_bg_color)
+      expect(data[:icon]).to eq(icon)
+      expect(Time.parse(data[:exported_at])).to be_within(2).of(Time.now.utc)
+      expect(data[:links]).to eq([{ :source => guid_order(agent_list, :jane_weather_agent), :receiver => guid_order(agent_list, :jane_rain_notifier_agent)}])
+      expect(data[:control_links]).to eq([])
+      expect(data[:agents]).to eq(agent_list.sort_by{|a| a.guid}.map { |agent| exporter.agent_as_json(agent) })
+      expect(data[:agents].all? { |agent_json| agent_json[:guid].present? && agent_json[:type].present? && agent_json[:name].present? }).to be_truthy
+    
+      describe '#helpers' do
+    it 'should return the correct request header' do
+      expect(@checker.send(:request_options)).to eq({:headers => {'aftership-api-key' => '800deeaf-e285-9d62-bc90-j999c1973cc9', 'Content-Type'=>'application/json'}})
+    end
+    
+        def html?
+      mime_type.include? 'html'
+    end
+    
+        def options
+      @options ||= self.class.options.deep_dup.tap do |options|
+        options.merge! base_url: base_url, root_url: root_url,
+                       root_path: root_path, initial_paths: initial_paths,
+                       version: self.class.version, release: self.class.release
+    
+            css('h1 + code').each do |node|
+          node.before('<p></p>')
+          while node.next_element.name == 'code'
+            node.previous_element << ' '
+            node.previous_element << node.next_element
           end
+          node.previous_element.prepend_child(node)
+        end
     
-      private
-    
-        def limit_reached_for(user_id, date)
-      GivenDailyLike.find_for(user_id, date).pluck(:limit_reached)[0] || false
+            doc
+      end
     end
-    
-          command_return = ActionCommandReturn.new(
-        return_value: action_return,
-        return_value_type: action_class_ref.return_type,
-        closure_argument_value: closure_argument_value
-      )
-      return command_return
-    end
-    
-            inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
-        pseudocommand = 'git log --pretty=\'%s%n%b\' --date=\'short\' #{inner_command.shellescape}...HEAD'
-        expect(result).to eq(pseudocommand)
-      end
-    
-            expect(result[3]).to start_with('security set-keychain-settings')
-        expect(result[3]).to include('-t 600')
-        expect(result[3]).to include('-l')
-        expect(result[3]).to include('-u')
-        expect(result[3]).to include('/tmp/test.keychain')
-      end
-    
-          it 'handles extension and extensions parameters correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', extension: 'rb', extensions: ['m', 'h'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --include=\\*.{rb,m,h}')
-      end
-    
-              result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-              strict: true,
-              executable: '#{CUSTOM_EXECUTABLE_NAME}'
-            )
-          end').runner.execute(:test)
-    
-    # remove all double quotes completely
-def simulate_normal_shell_unwrapping(string)
-  string.gsub!(''', '')
-  regex = /^(')(\S*)(')$/
-  unless string.to_s.match(regex).nil?
-    string = string.to_s.match(regex)[2] # get only part in quotes
   end
-  return string
+end
+
+    
+          it 'raises ArgumentError if passed negative value' do
+        -> { warn '', uplevel: -2 }.should raise_error(ArgumentError)
+        -> { warn '', uplevel: -100 }.should raise_error(ArgumentError)
+      end
+    
+    desc 'generate gemspec'
+task 'rack-protection.gemspec' do
+  require 'rack/protection/version'
+  content = File.binread 'rack-protection.gemspec'
+    
+          def redirect(env)
+        request = Request.new(env)
+        warn env, 'attack prevented by #{self.class}'
+        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
+      end
+    
+              react_and_close(env, body) or [status, headers, body]
+        else
+          [status, headers, body]
+        end
+      end
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+        puts('Generated at #{target_file}')
+  end
+    
+    module LogStash module PluginManager module PackFetchStrategy
+  class Repository
+    DEFAULT_PACK_URL = 'https://artifacts.elastic.co/downloads/logstash-plugins'
+    PACK_EXTENSION = 'zip'
+    
+          def page_name
+        @name.gsub('-', ' ')
+      end
+    
+          def use_identicon
+        @page.wiki.user_icons == 'identicon'
+      end
+    
+          def base_url
+        @base_url
+      end
+    
+    def cloned_testpath(path)
+  repo   = File.expand_path(testpath(path))
+  path   = File.dirname(repo)
+  cloned = File.join(path, self.class.name)
+  FileUtils.rm_rf(cloned)
+  Dir.chdir(path) do
+    %x{git clone #{File.basename(repo)} #{self.class.name} 2>/dev/null}
+  end
+  cloned
 end
     
-        def name=(value)
-      @name = value.try :strip
-    end
-    
-        def load_capybara_selenium
-      require 'capybara/dsl'
-      require 'selenium/webdriver'
-      Capybara.register_driver :chrome do |app|
-        options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
-        Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-      end
-      Capybara.javascript_driver = :chrome
-      Capybara.current_driver = :chrome
-      Capybara.run_server = false
-      Capybara
-    end
-    
-            css('tr[style]').each do |node|
-          node.remove_attribute 'style'
-        end
-    
-          private
-    
-            css('h2:not([id]) a[id]:not([href])').each do |node|
-          node.parent['id'] = node['id']
-          node.before(node.children).remove
-        end
-    
-          def executable_path
-        <<-EOS
-### Installation Source
-    
-              @original_shipment.transfer_to_location(@variant, @quantity, @stock_location)
-          render json: { success: true, message: Spree.t(:shipment_transfer_success) }, status: 201
-        end
-    
-            def scope
-          includes = { variant: [{ option_values: :option_type }, :product] }
-          @stock_location.stock_items.accessible_by(current_ability, :read).includes(includes)
-        end
-    
-            def get_store
-          @store = Store.find(params[:id])
-        end
-    
-            private
+      s.summary     = 'A simple, Git-powered wiki.'
+  s.description = 'A simple, Git-powered wiki with a sweet API and local frontend.'
