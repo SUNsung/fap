408da0cb0531ac50e1b1ef680be85a145b45ab9a
SUNsung <weatherius@gmail.com>
2019-01-24 17:57:20 +0200

    Rewrite [58m6yARuT0tSY58MvmkPbNSHMA58OutFQ9FDyOk581vo.cpp] File in: 2019-01-24 17:57:19

--- cccccc/58m6yARuT0tSY58MvmkPbNSHMA58OutFQ9FDyOk581vo.cpp
+++ cccccc/58m6yARuT0tSY58MvmkPbNSHMA58OutFQ9FDyOk581vo.cpp
@@ -1,204 +1,223 @@
 
         
-        // Calls the registered C++ shape inference function for <node> (a serialized
-// NodeDef).
-// Should not be called for shape functions that access input tensors; constant
-// input tensor values are not made available, and so the inferred shapes will
-// be less precise than they could be.
-//
-// Returns an error, or OK, in <out_status> according to whether the shape
-// inference was successful.
-//
-// On success, returns a vector populated with the inferred output shapes (as
-// serialized CppShapeInferenceResult protos) followed by a serialized
-// CppShapeInferenceInputsNeeded proto.
-//
-// This is temporary code to be used during the migration
-// from python shape inference functions to C++ shape inference functions.
-std::vector<string> RunCppShapeInference(
-    int graph_def_version, const string& serialized_node_def,
-    const std::vector<string>& input_serialized_shapes,
-    PyObject* input_constant_tensor_values,
-    const std::vector<string>& input_constant_tensor_as_shape_values,
-    TF_Status* out_status);
+          static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
     
-    REGISTER_OP('ShapelessOp');
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    {  DISALLOW_COPY_AND_ASSIGN(Screen);
+};
     
-    #include 'tensorflow/core/framework/op.h'
-#include 'tensorflow/core/framework/op_kernel.h'
+    namespace mate {
+class Arguments;
+class Dictionary;
+}  // namespace mate
     
-    PyObject* PyExceptionRegistry::Lookup(TF_Code code) {
-  DCHECK(singleton_ != nullptr) << 'Must call PyExceptionRegistry::Init() '
-                                   'before PyExceptionRegistry::Lookup()';
-  DCHECK_NE(code, TF_OK);
-  DCHECK(singleton_->exc_types_.find(code) != singleton_->exc_types_.end())
-      << 'Unknown error code passed to PyExceptionRegistry::Lookup: ' << code;
-  return singleton_->exc_types_[code];
+    
+    {  DISALLOW_COPY_AND_ASSIGN(WebRequest);
+};
+    
+    Event::~Event() {}
+    
+    void SavePageHandler::Destroy(download::DownloadItem* item) {
+  item->RemoveObserver(this);
+  delete this;
 }
     
-    #ifndef TENSORFLOW_PYTHON_LIB_IO_PY_RECORD_READER_H_
-#define TENSORFLOW_PYTHON_LIB_IO_PY_RECORD_READER_H_
+      // content::QuotaPermissionContext:
+  void RequestQuotaPermission(const content::StorageQuotaParams& params,
+                              int render_process_id,
+                              const PermissionCallback& callback) override;
     
-    
-    {
-    {}  // namespace swig
-}  // namespace tensorflow
-    
-    #include 'tensorflow/stream_executor/cuda/cuda_driver.h'
-#include 'tensorflow/stream_executor/cuda/cuda_stream.h'
-#include 'tensorflow/stream_executor/event.h'
-#include 'tensorflow/stream_executor/lib/status.h'
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    	if (cmderOptions.registerApp == true)
-	{
-		RegisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_BACKGROUND, cmderOptions.cmderCfgRoot, cmderOptions.cmderSingle);
-		RegisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_LISTITEM, cmderOptions.cmderCfgRoot, cmderOptions.cmderSingle);
-	}
-	else if (cmderOptions.unRegisterApp == true)
-	{
-		UnregisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_BACKGROUND);
-		UnregisterShellMenu(cmderOptions.cmderRegScope, SHELL_MENU_REGISTRY_PATH_LISTITEM);
-	}
-	else if (cmderOptions.error == true)
-	{
-		return 1;
-	}
-	else
-	{
-		StartCmder(cmderOptions.cmderStart, cmderOptions.cmderSingle, cmderOptions.cmderTask, cmderOptions.cmderCfgRoot, cmderOptions.cmderUserCfg);
-	}
-    
-      // Initialize a discovery cache at time 0.
-  discovery_cache_ = std::make_pair<size_t, bool>(0, false);
-  valid_ = true;
-    
-    namespace osquery {
-    }
-    
-      if (config.count('file_paths') > 0) {
-    // We know this top-level is an Object.
-    const auto& file_paths = config.at('file_paths').doc();
-    if (file_paths.IsObject()) {
-      for (const auto& category : file_paths.GetObject()) {
-        if (category.value.IsArray()) {
-          for (const auto& path : category.value.GetArray()) {
-            std::string pattern = path.GetString();
-            if (pattern.empty()) {
-              continue;
-            }
-    }
-    }
-    }
-    }
-    }
-    
-    Status ViewsConfigParserPlugin::update(const std::string& source,
-                                       const ParserConfig& config) {
-  auto cv = config.find('views');
-  if (cv == config.end()) {
-    return Status(1);
-  }
-    }
-    
-    #include <osquery/config/config.h>
-#include <osquery/filesystem/filesystem.h>
-#include <osquery/flags.h>
-#include <osquery/logger.h>
-#include <osquery/registry_factory.h>
-#include <osquery/utils/config/default_paths.h>
-    
-    
-    {  uri_ = TLSRequestHelper::makeURI(FLAGS_config_tls_endpoint);
-  return Status(0, 'OK');
+    Delegate* AutoUpdater::GetDelegate() {
+  return delegate_;
 }
     
-    fs::path getConfDirPathImpl() {
-  char const* kEnvVarName = 'TEST_CONF_FILES_DIR';
-  auto const value = std::getenv(kEnvVarName);
-  EXPECT_NE(value, nullptr)
-      << 'Env var ' << boost::io::quoted(kEnvVarName) << ' was not found, '
-      << ' looks like cxx_test argument 'env' is not set up.';
-  return fs::path(value);
-}
-    
-    
-    {
-    {
-    {                // Bind texture, Draw
-                ID3D11ShaderResourceView* texture_srv = (ID3D11ShaderResourceView*)pcmd->TextureId;
-                ctx->PSSetShaderResources(0, 1, &texture_srv);
-                ctx->DrawIndexed(pcmd->ElemCount, idx_offset, vtx_offset);
-            }
-            idx_offset += pcmd->ElemCount;
-        }
-        vtx_offset += cmd_list->VtxBuffer.Size;
-    }
-    
-            for (int cmd_i = 0; cmd_i < cmd_list->CmdBuffer.Size; cmd_i++)
-        {
-            const ImDrawCmd* pcmd = &cmd_list->CmdBuffer[cmd_i];
-            if (pcmd->UserCallback)
-            {
-                // User callback (registered via ImDrawList::AddCallback)
-                pcmd->UserCallback(cmd_list, pcmd);
-            }
-            else
-            {
-                ImVec4 clip_rect = ImVec4(pcmd->ClipRect.x - pos.x, pcmd->ClipRect.y - pos.y, pcmd->ClipRect.z - pos.x, pcmd->ClipRect.w - pos.y);
-                if (clip_rect.x < fb_width && clip_rect.y < fb_height && clip_rect.z >= 0.0f && clip_rect.w >= 0.0f)
-                {
-                    // Apply scissor/clipping rectangle
-                    glScissor((int)clip_rect.x, (int)(fb_height - clip_rect.w), (int)(clip_rect.z - clip_rect.x), (int)(clip_rect.w - clip_rect.y));
-    }
-    }
-    }
-    
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-      void sendMessage();
-    
-      // localnode
-  // 8bytes reserved
-  readBytes(fp, buf, buf.size(), 8);
-  // localnode ID
-  readBytes(fp, buf, buf.size(), DHT_ID_LENGTH);
-  auto localNode = std::make_shared<DHTNode>(buf);
-  // 4bytes reserved
-  readBytes(fp, buf, buf.size(), 4);
-    
-    #endif // D_DHT_TASK_H
+    #endif  // ATOM_BROWSER_NET_ABOUT_PROTOCOL_HANDLER_H_
 
     
-      void update();
+    #include <QDialog>
     
-    #endif // D_DHT_TASK_QUEUE_H
-
-    
-    DHTUnknownMessage::DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
-                                     const unsigned char* data, size_t length,
-                                     const std::string& ipaddr, uint16_t port)
-    : DHTMessage(localNode, std::shared_ptr<DHTNode>()),
-      length_(length),
-      ipaddr_(ipaddr),
-      port_(port)
+    /**
+ * An RAII-style reverse lock. Unlocks on construction and locks on destruction.
+ */
+template<typename Lock>
+class reverse_lock
 {
-  if (length_ == 0) {
-    data_ = nullptr;
-  }
-  else {
-    data_ = new unsigned char[length];
-    memcpy(data_, data, length);
-  }
+public:
+    }
+    
+    
+    {    /* Cleanup */
+    secp256k1_context_destroy(tctx);
 }
+    
+    static void CheckSplitTorReplyLine(std::string input, std::string command, std::string args)
+{
+    BOOST_TEST_MESSAGE(std::string('CheckSplitTorReplyLine(') + input + ')');
+    auto ret = SplitTorReplyLine(input);
+    BOOST_CHECK_EQUAL(ret.first, command);
+    BOOST_CHECK_EQUAL(ret.second, args);
+}
+    
+    #define strprintf tfm::format
+    
+    bool ParseInt64(const std::string& str, int64_t *out)
+{
+    if (!ParsePrechecks(str))
+        return false;
+    char *endp = NULL;
+    errno = 0; // strtoll will not set errno if valid
+    long long int n = strtoll(str.c_str(), &endp, 10);
+    if(out) *out = (int64_t)n;
+    // Note that strtoll returns a *long long int*, so even if strtol doesn't report a over/underflow
+    // we still have to check that the returned value is within the range of an *int64_t*.
+    return endp && *endp == 0 && !errno &&
+        n >= std::numeric_limits<int64_t>::min() &&
+        n <= std::numeric_limits<int64_t>::max();
+}
+    
+    namespace bech32
+{
+    }
+    
+    /** A hasher class for RIPEMD-160. */
+class CRIPEMD160
+{
+private:
+    uint32_t s[5];
+    unsigned char buf[64];
+    uint64_t bytes;
+    }
+    
+      ::opencensus::trace::SpanContext Context() const { return span_.context(); }
+  ::opencensus::trace::Span Span() const { return span_; }
+  void EndSpan() { span_.End(); }
+    
+    namespace grpc {
+namespace reflection {
+    }
+    }
+    
+    #ifdef GPR_LINUX
+    
+    /*!
+ * \file graph_attr_types.h
+ * \brief Data structures that can appear in graph attributes.
+ */
+#ifndef MXNET_GRAPH_ATTR_TYPES_H_
+#define MXNET_GRAPH_ATTR_TYPES_H_
+    
+    /*!
+ * Copyright (c) 2016 by Contributors
+ * \file caffe_loss.cc
+ * \brief caffe loss
+ * \author Haoran Wang
+*/
+#include './caffe_loss-inl.h'
+    
+    namespace mxnet {
+namespace op {
+    }
+    }
+    
+    /*!
+ * Copyright (c) 2016 by Contributors
+ * \file caffe_op.cc
+ * \brief caffe operator
+ * \author Haoran Wang
+*/
+#include './caffe_op-inl.h'
+namespace mxnet {
+namespace op {
+    }
+    }
+    
+    
+MXNET_DLL int MXCVResize(NDArrayHandle src, const mx_uint w, const mx_uint h,
+                         const int interpolation, NDArrayHandle *out) {
+  API_BEGIN();
+  NDArray ndsrc = *static_cast<NDArray*>(src);
+  CHECK_EQ(ndsrc.shape().ndim(), 3);
+  CHECK_EQ(ndsrc.ctx(), Context::CPU());
+  CHECK_EQ(ndsrc.dtype(), mshadow::kUint8);
+    }
+    
+    /*!
+ * \brief Thread pool.
+ */
+class ThreadPool {
+ public:
+  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
+  struct SetReadyOnDestroy {
+    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
+      : event_(event) {
+    }
+    inline ~SetReadyOnDestroy() {
+      if (event_) {
+        event_->signal();
+      }
+    }
+    std::shared_ptr<dmlc::ManualEvent>  event_;
+  };
+    }
+    
+    DHTResponseMessage::DHTResponseMessage(
+    const std::shared_ptr<DHTNode>& localNode,
+    const std::shared_ptr<DHTNode>& remoteNode,
+    const std::string& transactionID)
+    : DHTAbstractMessage(localNode, remoteNode, transactionID)
+{
+}
+    
+      std::shared_ptr<DHTNode> getNode(const unsigned char* id,
+                                   const std::string& ipaddr,
+                                   uint16_t port) const;
+    
+    #include 'DHTNode.h'
+#include 'DlAbortEx.h'
+#include 'DHTConstants.h'
+#include 'bittorrent_helper.h'
+#include 'Logger.h'
+#include 'a2netcompat.h'
+#include 'util.h'
+#include 'TimeA2.h'
+#include 'fmt.h'
+#include 'File.h'
+#include 'LogFactory.h'
+#include 'BufferedFile.h'
+    
+    #endif // D_DHT_ROUTING_TABLE_SERIALIZER_H
+
+    
+      virtual std::shared_ptr<DHTTask>
+  createPingTask(const std::shared_ptr<DHTNode>& remoteNode,
+                 int numRetry = 0) = 0;
+    
+    void DHTTaskQueueImpl::executeTask()
+{
+  A2_LOG_DEBUG('Updating periodicTaskQueue1');
+  periodicTaskQueue1_.update();
+  A2_LOG_DEBUG('Updating periodicTaskQueue2');
+  periodicTaskQueue2_.update();
+  A2_LOG_DEBUG('Updating immediateTaskQueue');
+  immediateTaskQueue_.update();
+}
+    
+    public:
+  // _remoteNode is always null
+  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                    const unsigned char* data, size_t length,
+                    const std::string& ipaddr, uint16_t port);
+    
+    DNSCache::CacheEntry::CacheEntry(const CacheEntry& c) = default;
+    
+        // out_of_range.109
+    try
+    {
+        // try to use an array index that is not a number
+        json::const_reference ref = j.at('/array/one'_json_pointer);
+    }
+    catch (json::parse_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
