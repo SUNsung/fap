4e4f372cdf2f4454a856f477be90a189110a6d33
SUNsung <weatherius@gmail.com>
2019-01-26 01:44:56 +0200

    Rewrite [12yrVrqyqZUo12dST15xW01GE12UeHkej5Naj212tA2.rb] File in: 2019-01-26 01:44:55

--- ruby/12yrVrqyqZUo12dST15xW01GE12UeHkej5Naj212tA2.rb
+++ ruby/12yrVrqyqZUo12dST15xW01GE12UeHkej5Naj212tA2.rb
@@ -1,203 +1,334 @@
 
         
-          p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
-    
-          def feature_element_timing_key(feature_element)
-        '\'#{feature_element.name}\' (#{feature_element.location})'
+        module ActionView #:nodoc:
+  # = Action View Raw Output Helper
+  module Helpers #:nodoc:
+    module OutputSafetyHelper
+      # This method outputs without escaping a string. Since escaping tags is
+      # now default, this can be used when you don't want Rails to automatically
+      # escape tags. This is not recommended if the data is coming from the user's
+      # input.
+      #
+      # For example:
+      #
+      #  raw @user.name
+      #  # => 'Jimmy <alert>Tables</alert>'
+      def raw(stringish)
+        stringish.to_s.html_safe
       end
     
-            # Execute a command on the remote machine. The exact semantics
-        # of this method are up to the implementor, but in general the
-        # users of this class will expect this to be a shell.
-        #
-        # This method gives you no way to write data back to the remote
-        # machine, so only execute commands that don't expect input.
-        #
-        # @param [String] command Command to execute.
-        # @yield [type, data] Realtime output of the command being executed.
-        # @yieldparam [String] type Type of the output. This can be
-        #   `:stdout`, `:stderr`, etc. The exact types are up to the
-        #   implementor.
-        # @yieldparam [String] data Data for the given output.
-        # @return [Integer] Exit code of the command.
-        def execute(command, opts=nil)
-        end
-    
-            # Allows setting options from a hash. By default this simply calls
-        # the `#{key}=` method on the config class with the value, which is
-        # the expected behavior most of the time.
-        #
-        # This is expected to mutate itself.
-        #
-        # @param [Hash] options A hash of options to set on this configuration
-        #   key.
-        def set_options(options)
-          options.each do |key, value|
-            send('#{key}=', value)
-          end
-        end
-    
-              @registered.each do |plugin|
-            plugin.config.each do |key, klass|
-              result[key] = klass
-            end
+              def sanitize_attribute_name(value)
+            '#{sanitized_method_name}_#{sanitized_value(value)}'
           end
     
-            # An internal finalize call that no subclass should override.
-        def _finalize!
-          @__finalized = true
+              add_default_name_and_id_for_value(tag_value, name_and_id)
+          options.delete('index')
+          options.delete('namespace')
+          options['for'] = name_and_id['id'] unless options.key?('for')
+    
+            def render
+          options = @options.stringify_keys
+          options['size'] = options['maxlength'] unless options.key?('size')
+          options['type'] ||= field_type
+          options['value'] = options.fetch('value') { value_before_type_cast } unless field_type == 'file'
+          add_default_name_and_id(options)
+          tag('input', options)
         end
+    
+    module ActionView #:nodoc:
+  # = Action View PathSet
+  #
+  # This class is used to store and access paths in Action View. A number of
+  # operations are defined so that you can search among the paths in this
+  # set and also perform operations on other +PathSet+ objects.
+  #
+  # A +LookupContext+ will use a +PathSet+ to store the paths in its context.
+  class PathSet #:nodoc:
+    include Enumerable
+    
+    def gemspec_file
+  '#{name}.gemspec'
+end
+    
+      p.action do |args, _|
+    if args.empty?
+      Jekyll.logger.error 'A subcommand is required.'
+      puts p
+      abort
+    else
+      subcommand = args.first
+      unless p.has_command? subcommand
+        Jekyll.logger.abort_with 'fatal: 'jekyll #{args.first}' could not' \
+          ' be found. You may need to install the jekyll-#{args.first} gem' \
+          ' or a related gem to be able to use this subcommand.'
       end
     end
   end
 end
 
     
-            # This returns all synced folder implementations.
-        #
-        # @return [Registry]
-        def synced_folders
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.synced_folders)
-            end
-          end
-        end
+    #
     
-            # This should return a hash of information that explains how to
-        # SSH into the machine. If the machine is not at a point where
-        # SSH is even possible, then `nil` should be returned.
-        #
-        # The general structure of this returned hash should be the
-        # following:
-        #
-        #     {
-        #       host: '1.2.3.4',
-        #       port: '22',
-        #       username: 'mitchellh',
-        #       private_key_path: '/path/to/my/key'
-        #     }
-        #
-        # **Note:** Vagrant only supports private key based authentication,
-        # mainly for the reason that there is no easy way to exec into an
-        # `ssh` prompt with a password, whereas we can pass a private key
-        # via commandline.
-        #
-        # @return [Hash] SSH information. For the structure of this hash
-        #   read the accompanying documentation for this method.
-        def ssh_info
-          nil
-        end
-    
-        # ghettoooooo!
-    # If we don't have any newlines..., put one there.
-    if (header.size > 0 && header !~ /\r\n/)
-      header << '\r\n'
+        def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
     end
-    
-        self.method    = method
-    self.raw_uri   = uri
-    self.uri_parts = {}
-    self.proto     = proto || DefaultProtocol
-    self.chunk_min_size = 1
-    self.chunk_max_size = 10
-    self.uri_encode_mode = 'hex-normal'
+  end
+end
+
     
       #
-  # A hash that associated a file extension with a mime type for use as the
-  # content type of responses.
+  # More advanced [] that does downcase comparison.
   #
-  ExtensionMimeTypes =
-    {
-      'rhtml' => 'text/html',
-      'html'  => 'text/html',
-      'htm'   => 'text/htm',
-      'jpg'   => 'image/jpeg',
-      'jpeg'  => 'image/jpeg',
-      'gif'   => 'image/gif',
-      'png'   => 'image/png',
-      'bmp'   => 'image/bmp',
-      'txt'   => 'text/plain',
-      'css'   => 'text/css',
-      'ico'   => 'image/x-icon',
-    }
+  def [](key)
+    begin
+      rv = self.fetch(key)
+    rescue IndexError
+      rv = nil
+    end
+    if (rv == nil)
+      begin
+        rv = self.dcase_hash[key.downcase]
+      rescue IndexError
+        rv = nil
+      end
+    end
     
-        info  = nil
-    stype = pkt[11,1].unpack('C')[0]
-    info  = process_elements(pkt, 12) if [IAX_TYPE_IAX, IAX_TYPE_CONTROL].include?(itype)
+            input_name = input.attributes['name'] ? input.attributes['name'].value : ''
+        input_value = input.attributes['value'] ? input.attributes['value'].value : ''
+        found_inputs[input_name] = input_value unless input_name.empty?
+      end
+      forms << found_inputs unless found_inputs.empty?
+    end
     
-              # Encodes the components field
-          #
-          # @return [String]
-          def encode_components
-            encoded = ''
+      #
+  # Dispatches the supplied request for a given connection.
+  #
+  def dispatch_request(cli, request)
+    # Is the client requesting keep-alive?
+    if ((request['Connection']) and
+       (request['Connection'].downcase == 'Keep-Alive'.downcase))
+      cli.keepalive = true
+    end
     
-              # Encodes the msg_type field
+    
+IAX_SUBTYPE_NEW     = 1
+IAX_SUBTYPE_PING    = 2
+IAX_SUBTYPE_PONG    = 3
+IAX_SUBTYPE_ANSWER  = 4
+IAX_SUBTYPE_ACK     = 4
+IAX_SUBTYPE_HANGUP  = 5
+IAX_SUBTYPE_REJECT  = 6
+IAX_SUBTYPE_ACCEPT  = 7
+IAX_SUBTYPE_AUTHREQ = 8
+IAX_SUBTYPE_AUTHREP = 9
+IAX_SUBTYPE_INVAL   = 10
+IAX_SUBTYPE_LAGRQ   = 11
+IAX_SUBTYPE_LAGRP   = 12
+IAX_SUBTYPE_REGREQ  = 13
+IAX_SUBTYPE_REGAUTH = 14
+IAX_SUBTYPE_REGACK  = 15
+IAX_SUBTYPE_REGREJ  = 16
+IAX_SUBTYPE_REGREL  = 17
+IAX_SUBTYPE_VNAK    = 18
+    
+              # Encodes the type
           #
           # @return [OpenSSL::ASN1::Integer]
-          def encode_msg_type
-            bn = OpenSSL::BN.new(msg_type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-              # Encodes the type field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_type
+          def encode_type(type)
             bn = OpenSSL::BN.new(type.to_s)
             int = OpenSSL::ASN1::Integer.new(bn)
     
-                self
-          end
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a Kerberos Checksum definition.
+        class Checksum < Element
     
-              # Decodes the value from an OpenSSL::ASN1::ASN1Data
+              # Decodes the renew_till field
           #
           # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
           # @return [Time]
-          def decode_value(input)
+          def decode_renew_till(input)
             input.value[0].value
           end
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a Kerberos KDC-REQ (response) data
+        # definition
+        class KdcResponse < Element
+          # @!attribute pvno
+          #   @return [Integer] The protocol version number
+          attr_accessor :pvno
+          # @!attribute msg_type
+          #   @return [Integer] The type of a protocol message
+          attr_accessor :msg_type
+          # @!attribute crealm
+          #   @return [String] The realm part of the client's principal identifier
+          attr_accessor :crealm
+          # @!attribute cname
+          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
+          attr_accessor :cname
+          # @!attribute ticket
+          #   @return [Rex::Proto::Kerberos::Model::Ticket] The issued ticket
+          attr_accessor :ticket
+          # @!attribute enc_part
+          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] The encrypted part of the response
+          attr_accessor :enc_part
+    
+              # Decodes the cname field
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Rex::Proto::Kerberos::Model::PrincipalName]
+          def decode_cname(input)
+            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
+          end
+    
+    # Commit file to wiki, overwriting previous versions of that file
+def commit_test_file(wiki, dir, filename, ext, content)
+  committer = Gollum::Committer.new(wiki, :message => 'Added testfile', :parent  => wiki.repo.head.commit)
+  committer.add_to_index(dir, filename, ext, content, true)
+    committer.after_commit do |committer, sha|
+      wiki.clear_cache
+      committer.update_working_dir(dir, filename, ext)
+    end
+  committer.commit
+end
+
+    
+      test 'remove page extentions' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'page', view.remove_page_extentions('page.wiki')
+    assert_equal 'page-wiki', view.remove_page_extentions('page-wiki.md')
+    assert_equal 'file.any_extention', view.remove_page_extentions('file.any_extention')
+  end
+    
+        # Test page_header_from_content(@content)
+    actual = @view.title
+    assert_equal '1 & 2', actual
+  end
+    
+      s.add_dependency 'gollum-lib', '~> 4.2', '>= 4.2.10'
+  s.add_dependency 'kramdown', '~> 1.9.0'
+  s.add_dependency 'sinatra', '~> 1.4', '>= 1.4.4'
+  s.add_dependency 'mustache', ['>= 0.99.5', '< 1.0.0']
+  s.add_dependency 'useragent', '~> 0.16.2'
+  s.add_dependency 'gemojione', '~> 3.2'
+    
+      node[:applications].each do |app, data|
+    template '/etc/monit.d/sidekiq_#{app}.monitrc' do 
+      owner 'root' 
+      group 'root' 
+      mode 0644 
+      source 'monitrc.conf.erb' 
+      variables({ 
+        :num_workers => worker_count,
+        :app_name => app, 
+        :rails_env => node[:environment][:framework_env] 
+      }) 
+    end
+    
+          def deliver(msg)
+        if msg.respond_to?(:deliver_now)
+          # Rails 4.2/5.0
+          msg.deliver_now
+        else
+          # Rails 3.2/4.0/4.1
+          msg.deliver
         end
       end
     end
-  end
-end
     
-      # if rss_url already in existing opml file, use that; otherwise, do a lookup
-  rss_url = nil
-  if File.exist?(OUTPUT_FILENAME)
-    xml = Nokogiri::XML(File.open(OUTPUT_FILENAME))
-    existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first
-    if existing_blog
-      rss_url = existing_blog.attr('xmlUrl')
-      puts '#{name}: ALREADY HAVE'
-    end
-  end
-    
-        def paragraphize(input)
-      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
-    end
-  end
-end
-    
-    Liquid::Template.register_tag('img', Jekyll::ImageTag)
-
-    
-      class RenderPartialTag < Liquid::Tag
-    include OctopressFilters
-    def initialize(tag_name, markup, tokens)
-      @file = nil
-      @raw = false
-      if markup =~ /^(\S+)\s?(\w+)?/
-        @file = $1.strip
-        @raw = $2 == 'raw'
+          def initialize
+        @entries = []
+        yield self if block_given?
       end
-      super
+    
+      # Copy a path.
+  #
+  # Files will be hardlinked if possible, but copied otherwise.
+  # Symlinks should be copied as symlinks.
+  def copy(source, destination)
+    logger.debug('Copying path', :source => source, :destination => destination)
+    directory = File.dirname(destination)
+    # lstat to follow symlinks
+    dstat = File.stat(directory) rescue nil
+    if dstat.nil?
+      FileUtils.mkdir_p(directory, :mode => 0755)
+    elsif dstat.directory?
+      # do nothing, it's already a directory!
+    else
+      # It exists and is not a directory. This is probably a user error or a bug.
+      readable_path = directory.gsub(staging_path, '')
+      logger.error('You wanted to copy a file into a directory, but that's not a directory, it's a file!', :path => readable_path, :stat => dstat)
+      raise FPM::InvalidPackageConfiguration, 'Tried to treat #{readable_path} like a directory, but it's a file!'
+    end
+    
+        # Build the packaging metadata files.
+    checksums = {}
+    self.files.each do |f|
+      path = staging_path(f)
+      if File.symlink?(path)
+        checksums[f] = '-'
+      elsif File.file?(path)
+        checksums[f] = Digest::SHA256.file(path).hexdigest
+      end
+    end
+    
+        # Got a good gem now (downloaded or otherwise)
+    #
+    # 1. unpack it into staging_path
+    # 2. take the metadata from it and update our wonderful package with it.
+    load_package_info(path_to_gem)
+    install_to_staging(path_to_gem)
+  end # def input
+    
+      def self.default_prefix
+    npm_prefix = safesystemout('npm', 'prefix', '-g').chomp
+    if npm_prefix.count('\n') > 0
+      raise FPM::InvalidPackageConfiguration, '`npm prefix -g` returned unexpected output.'
+    elsif !File.directory?(npm_prefix)
+      raise FPM::InvalidPackageConfiguration, '`npm prefix -g` returned a non-existent directory'
+    end
+    logger.info('Setting default npm install prefix', :prefix => npm_prefix)
+    npm_prefix
+  end
+    
+          scripts_path(filename).tap do |pkgscript|
+        logger.info('Writing pkg script', :source => filename, :target => pkgscript)
+        File.write(pkgscript, script(scriptname))
+        # scripts are required to be executable
+        File.chmod(0755, pkgscript)
+      end
+    end
+  end # def write_scripts
+    
+        if script?(:before_install) or script?(:after_install) or \
+        script?(:before_upgrade) or script?(:after_upgrade) or \
+        script?(:before_remove) or script?(:after_remove)
+      install_script = template('pacman/INSTALL.erb').result(binding)
+      install_script_file = build_path('.INSTALL')
+      File.write(install_script_file, install_script)
+    end
+    
+        scripts[:after_install] = template(File.join('pleaserun', 'scripts', 'after-install.sh')).result(binding)
+    scripts[:before_remove] = template(File.join('pleaserun', 'scripts', 'before-remove.sh')).result(binding)
+  end # def input
+    
+        begin
+      json_test_code = [
+        'try:',
+        '  import json',
+        'except ImportError:',
+        '  import simplejson as json'
+      ].join('\n')
+      safesystem('#{attributes[:python_bin]} -c '#{json_test_code}'')
+    rescue FPM::Util::ProcessFailed => e
+      logger.error('Your python environment is missing json support (either json or simplejson python module). I cannot continue without this.', :python => attributes[:python_bin], :error => e)
+      raise FPM::Util::ProcessFailed, 'Python (#{attributes[:python_bin]}) is missing simplejson or json modules.'
     end
