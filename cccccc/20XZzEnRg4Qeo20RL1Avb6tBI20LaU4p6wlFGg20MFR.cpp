690ee778be53dbce9530e6efdb287cb6f4586987
SUNsung <weatherius@gmail.com>
2019-01-25 12:17:09 +0200

    Rewrite [20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp] File in: 2019-01-25 12:17:09

--- cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
+++ cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
@@ -1,153 +1,157 @@
 
         
-        #include 'base/callback.h'
-#include 'base/location.h'
-#include 'base/single_thread_task_runner.h'
-#include 'uv.h'  // NOLINT(build/include)
+            const QString &getName() const { return name; }
     
-    #include 'atom/browser/api/atom_api_net.h'
-#include 'atom/browser/api/atom_api_url_request.h'
-#include 'atom/common/node_includes.h'
-#include 'native_mate/dictionary.h'
-    
-    class Event : public Wrappable<Event>, public content::WebContentsObserver {
- public:
-  static Handle<Event> Create(v8::Isolate* isolate);
-    }
-    
-    namespace api {
-class WebContents;
+    namespace Ui {
+    class SignVerifyMessageDialog;
 }
     
-    
-    {}  // namespace atom
+    #endif
 
     
-    void AutoUpdater::CheckForUpdates() {}
+        // Special handling for null case
+    // (needed because string comparison reads the null as end-of-string)
+    BOOST_TEST_MESSAGE(std::string('CheckParseTorReplyMapping(Null=\'\\0\')'));
+    auto ret = ParseTorReplyMapping('Null=\'\\0\'');
+    BOOST_CHECK_EQUAL(ret.size(), 1U);
+    auto r_it = ret.begin();
+    BOOST_CHECK_EQUAL(r_it->first, 'Null');
+    BOOST_CHECK_EQUAL(r_it->second.size(), 1U);
+    BOOST_CHECK_EQUAL(r_it->second[0], '\0');
     
-    #ifndef UPB_ENCODER_H_
-#define UPB_ENCODER_H_
+            void format(std::ostream& out, const char* fmtBegin,
+                    const char* fmtEnd, int ntrunc) const
+        {
+            assert(m_value);
+            assert(m_formatImpl);
+            m_formatImpl(out, fmtBegin, fmtEnd, ntrunc, m_value);
+        }
     
-    bool RunProtoCompiler(const string& filename,
-                      const string& plugin_specific_args,
-                      CommandLineInterface* cli, FileDescriptorProto* file) {
-  cli->SetInputsAreProtoPathRelative(true);
+        j0 = input[0];
+    j1 = input[1];
+    j2 = input[2];
+    j3 = input[3];
+    j4 = input[4];
+    j5 = input[5];
+    j6 = input[6];
+    j7 = input[7];
+    j8 = input[8];
+    j9 = input[9];
+    j10 = input[10];
+    j11 = input[11];
+    j12 = input[12];
+    j13 = input[13];
+    j14 = input[14];
+    j15 = input[15];
+    
+    IPC_MESSAGE_ROUTED4(ShellViewHostMsg_Call_Object_Method,
+                    int /* object id */,
+                    std::string /* type name */,
+                    std::string /* method name */,
+                    base::ListValue /* arguments */)
+    
+     private:
+  ObjectManager* object_manager_;
+    
+     protected:
+  ~NwAppClearAppCacheFunction() override;
+    
+    // Read through the first n keys repeatedly and check that they get
+// compacted (verified by checking the size of the key space).
+void AutoCompactTest::DoReads(int n) {
+  std::string value(kValueSize, 'x');
+  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
     }
     
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-namespace {
-    }
-    }
-    }
-    }
-    }
     
-    #include <google/protobuf/compiler/csharp/csharp_source_generator_base.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_names.h>
-#include <google/protobuf/compiler/csharp/csharp_options.h>
-    
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
-    
-      printer->Print(
-      '// This CPP symbol can be defined to use imports that match up to the framework\n'
-      '// imports needed when using CocoaPods.\n'
-      '#if !defined($cpp_symbol$)\n'
-      ' #define $cpp_symbol$ 0\n'
-      '#endif\n'
-      '\n'
-      '#if $cpp_symbol$\n',
-      'cpp_symbol', cpp_symbol);
-    
-      // Checks whether the currently seen ikey_ is needed for
-  // incremental (differential) snapshot and hence can't be dropped
-  // or seqnum be zero-ed out even if all other conditions for it are met.
-  inline bool ikeyNotNeededForIncrementalSnapshot();
-    
-    class MyMerge : public rocksdb::MergeOperator {
- public:
-  virtual bool FullMergeV2(const MergeOperationInput& merge_in,
-                           MergeOperationOutput* merge_out) const override {
-    merge_out->new_value.clear();
-    if (merge_in.existing_value != nullptr) {
-      merge_out->new_value.assign(merge_in.existing_value->data(),
-                                  merge_in.existing_value->size());
-    }
-    for (const rocksdb::Slice& m : merge_in.operand_list) {
-      fprintf(stderr, 'Merge(%s)\n', m.ToString().c_str());
-      // the compaction filter filters out bad values
-      assert(m.ToString() != 'bad');
-      merge_out->new_value.assign(m.data(), m.size());
-    }
-    return true;
-  }
-    }
-    
-      txn->Rollback();
-    
-          manager.reset(new internal::ThreadManager(b.threads));
-      for (std::size_t ti = 0; ti < pool.size(); ++ti) {
-        pool[ti] = std::thread(&RunInThread, &b, iters,
-                               static_cast<int>(ti + 1), manager.get());
-      }
-      RunInThread(&b, iters, 0, manager.get());
-      manager->WaitForAllThreads();
-      for (std::thread& thread : pool) thread.join();
-      internal::ThreadManager::Result results;
-      {
-        MutexLock l(manager->GetBenchmarkMutex());
-        results = manager->results;
-      }
-      manager.reset();
-      // Adjust real/manual time stats since they were reported per thread.
-      results.real_time_used /= b.threads;
-      results.manual_time_used /= b.threads;
-    
-    bool BenchmarkFamilies::FindBenchmarks(
-    const std::string& spec, std::vector<Benchmark::Instance>* benchmarks,
-    std::ostream* ErrStream) {
-  CHECK(ErrStream);
-  auto& Err = *ErrStream;
-  // Make regular expression out of command-line flag
-  std::string error_msg;
-  Regex re;
-  if (!re.Init(spec, &error_msg)) {
-    Err << 'Could not compile benchmark re: ' << error_msg << std::endl;
-    return false;
-  }
-    }
-    
-    std::string FormatString(const char* msg, ...) {
-  va_list args;
-  va_start(args, msg);
-  auto tmp = FormatString(msg, args);
-  va_end(args);
-  return tmp;
+    {  // When limit user key is prefix of start user key
+  ASSERT_EQ(IKey('foobar', 100, kTypeValue),
+            Shorten(IKey('foobar', 100, kTypeValue),
+                    IKey('foo', 200, kTypeValue)));
 }
     
-    std::string FormatString(const char* msg, va_list args);
-std::string FormatString(const char* msg, ...);
+    // Return the name of the log file with the specified number
+// in the db named by 'dbname'.  The result will be prefixed with
+// 'dbname'.
+std::string LogFileName(const std::string& dbname, uint64_t number);
     
-    #ifndef BENCHMARK_OS_WINDOWS
-#include <sys/time.h>
-#include <time.h>
-#endif
+      struct KeyComparator {
+    const InternalKeyComparator comparator;
+    explicit KeyComparator(const InternalKeyComparator& c) : comparator(c) { }
+    int operator()(const char* a, const char* b) const;
+  };
+  friend class MemTableIterator;
+  friend class MemTableBackwardIterator;
     
-    std::string FormatKV(std::string const& key, std::string const& value) {
-  return StringPrintF('\'%s\': \'%s\'', key.c_str(), value.c_str());
+      std::string Get(const std::string& k, const Snapshot* snapshot = nullptr) {
+    std::string result;
+    Status s = db_->Get(ReadOptions(), k, &result);
+    if (s.IsNotFound()) {
+      result = 'NOT_FOUND';
+    } else if (!s.ok()) {
+      result = s.ToString();
+    }
+    return result;
+  }
+    
+     private:
+  enum { kMaxHeight = 12 };
+    
+    #include 'db/dbformat.h'
+#include 'leveldb/db.h'
+    
+      sender.AddMessage(1, &mpd);
+  EXPECT_EQ(sender.Start(), common::ErrorCode::OK);
+  EXPECT_TRUE(sender.IsRunning());
+  EXPECT_TRUE(sender.enable_log());
+    
+    
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    // System gflags
+DEFINE_string(node_name, 'chassis', 'The chassis module name in proto');
+DEFINE_string(canbus_driver_name, 'canbus', 'Driver name.');
+    
+      x <<= 2;
+  x |= t;
+    
+    bool RadarState201::send_ext_info(const std::uint8_t* bytes,
+                                  int32_t length) const {
+  Byte t0(bytes + 5);
+  uint32_t x = t0.get_byte(5, 1);
+    }
+    
+    namespace apollo {
+namespace localization {
+namespace msf {
+    }
+    }
+    }
+    
+    namespace apollo {
+namespace hdmap {
+DEFINE_string(test_map_file,
+              'modules/map/data/sunnyvale_loop/base_map_test.bin',
+              'The test map file');
+DEFINE_string(
+    test_routing_file,
+    'modules/map/pnc_map/testdata/sample_sunnyvale_loop_routing.pb.txt',
+    'The test map file');
+    }
+    }
+    
+    
+    {  double length_2 = 50.0;
+  const LaneGraph &lane_graph_2 =
+      ObstacleClusters::GetLaneGraph(start_s, length_2, lane);
+  EXPECT_EQ(1, lane_graph_2.lane_sequence_size());
+  EXPECT_EQ(3, lane_graph_2.lane_sequence(0).lane_segment_size());
+  EXPECT_EQ('l9', lane_graph_2.lane_sequence(0).lane_segment(0).lane_id());
+  EXPECT_EQ('l18', lane_graph_2.lane_sequence(0).lane_segment(1).lane_id());
+  EXPECT_EQ('l21', lane_graph_2.lane_sequence(0).lane_segment(2).lane_id());
 }
-    
-    #define TRY_ACQUIRE_SHARED(...) \
-  THREAD_ANNOTATION_ATTRIBUTE__(try_acquire_shared_capability(__VA_ARGS__))
