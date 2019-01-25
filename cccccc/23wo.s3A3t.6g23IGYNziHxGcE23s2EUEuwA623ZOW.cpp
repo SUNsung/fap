73e85633fbbcc165adbf1d4a6bc638a4bc7d37e1
SUNsung <weatherius@gmail.com>
2019-01-25 16:06:15 +0200

    Rewrite [23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp] File in: 2019-01-25 16:06:14

--- cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
+++ cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
@@ -1,79 +1,157 @@
 
         
-        bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv) {
-  const base::CommandLine::StringType dashdash(2, '-');
-  bool block_args = false;
-  for (int i = 0; i < argc; ++i) {
-    if (argv[i] == dashdash)
-      break;
-    if (block_args) {
-      return false;
-    } else if (IsUrlArg(argv[i])) {
-      block_args = true;
-    }
+        
+    {}  // namespace nw
+
+    
+    // Get RenderView from current js context (only works under window context).
+content::RenderView* GetCurrentRenderView();
+content::RenderView* GetEnteredRenderView();
+    
+    void Clipboard::CallSync(const std::string& method,
+                         const base::ListValue& arguments,
+                         base::ListValue* result) {
+  if (method == 'Get') {
+    result->AppendString(GetText());
+  } else {
+    NOTREACHED() << 'Invalid call to Clipboard method:' << method
+                 << ' arguments:' << arguments;
   }
-  return true;
 }
     
-    #include 'base/command_line.h'
+    #include 'content/nw/src/api/base/base.h'
+#include 'ui/gfx/display_observer.h'
     
-    
-    {}  // namespace mate
-    
-    Net::~Net() {}
-    
-    #ifndef ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
-#define ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
-    
-    void SavePageHandler::OnDownloadCreated(content::DownloadManager* manager,
-                                        download::DownloadItem* item) {
-  // OnDownloadCreated is invoked during WebContents::SavePage, so the |item|
-  // here is the one stated by WebContents::SavePage.
-  item->AddObserver(this);
+    void Menu::Insert(MenuItem* menu_item, int pos) {
+  std::vector<MenuItem*>::iterator begin = menu_items.begin();
+  menu_items.insert(begin+pos,menu_item);
+  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    menu_item->UpdateKeys(gtk_accel_group);
+  }
+  gtk_menu_shell_insert(GTK_MENU_SHELL(menu_), menu_item->menu_item_, pos);
 }
     
-    AtomQuotaPermissionContext::AtomQuotaPermissionContext() {}
+        std::string tooltip;
+    if (option.GetString('tooltip', &tooltip))
+      SetTooltip(tooltip);
     
-      // Check if part is indented.
-  IndentType IsIndented(ColPartition* part);
-    
-      /**
-   * Returns the bounding rectangle of the current object at the given level.
-   * See comment on coordinate system above.
-   * Returns false if there is no such object at the current position.
-   * The returned bounding box is guaranteed to match the size and position
-   * of the image returned by GetBinaryImage, but may clip foreground pixels
-   * from a grey image. The padding argument to GetImage can be used to expand
-   * the image to include more foreground pixels. See GetImage below.
-   */
-  bool BoundingBox(PageIteratorLevel level,
-                   int* left, int* top, int* right, int* bottom) const;
-  bool BoundingBox(PageIteratorLevel level, const int padding,
-                   int* left, int* top, int* right, int* bottom) const;
-  /**
-   * Returns the bounding rectangle of the object in a coordinate system of the
-   * working image rectangle having its origin at (rect_left_, rect_top_) with
-   * respect to the original image and is scaled by a factor scale_.
-   */
-  bool BoundingBoxInternal(PageIteratorLevel level,
-                           int* left, int* top, int* right, int* bottom) const;
-    
-    namespace tesseract {
+    namespace {
     }
     
-    struct PARA;
+    Github Links:
     
-      /// Get enough parameters to be able to rebuild bounding boxes in the
-  /// original image (not just within the rectangle).
-  /// Left and top are enough with top-down coordinates, but
-  /// the height of the rectangle and the image are needed for bottom-up.
-  virtual void GetImageSizes(int* left, int* top, int* width, int* height,
-                             int* imagewidth, int* imageheight);
+    OPERATOR_SCHEMA(HalfToFloat)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .TensorInferenceFunction(
+        [](const OperatorDef& def, const vector<TensorShape>& in) {
+          vector<TensorShape> out;
+          const TensorShape& X = in[0];
+          out.push_back(X);
+          out[0].set_data_type(TensorProto_DataType_FLOAT);
+    }
     
-    // A CostFunc that takes the variance of step into account in the cost.
-int64_t DPPoint::CostWithVariance(const DPPoint* prev) {
-  if (prev == nullptr || prev == this) {
-    UpdateIfBetter(0, 1, nullptr, 0, 0, 0);
-    return 0;
+      // Staring from the seed center, we do radius search. And for partitions that
+  // have large overlaps with seed, we remove them from part_grid_ and add into
+  // parts_overlap. Note: this function may update the part_grid_, so if the
+  // caller is also running ColPartitionGridSearch, use the RepositionIterator
+  // to continue.
+  void SearchByOverlap(ColPartition* seed,
+                       GenericVector<ColPartition*>* parts_overlap);
+    
+    void PageIterator::Orientation(tesseract::Orientation *orientation,
+                               tesseract::WritingDirection *writing_direction,
+                               tesseract::TextlineOrder *textline_order,
+                               float *deskew_angle) const {
+  BLOCK* block = it_->block()->block;
+    }
+    
+      // Recognize the first part of the word.
+  recog_word_recursive(word);
+  // Recognize the second part of the word.
+  recog_word_recursive(word2);
+    
+    
+    {}  // namespace tesseract.
+    
+    
+    {}  // namespace tesseract
+    
+    		/*
+		Convert legacy user-aliases.cmd to new name user_aliases.cmd
+		*/
+		PathCombine(legacyUserAliasesPath, userConfigDirPath, L'user-aliases.cmd');
+		if (PathFileExists(legacyUserAliasesPath))
+		{
+			PathCombine(userAliasesPath, userConfigDirPath, L'user_aliases.cmd');
+    }
+    
+    class UnicodeSet;
+    
+    class PluralRules;
+    
+    
+    {  // Single-Delete diagnostics for exceptional situations
+  uint64_t num_single_del_fallthru = 0;
+  uint64_t num_single_del_mismatch = 0;
+};
+
+    
+    
+    {  // Sleep just until `num_bytes` is allowed.
+  uint64_t sleep_amount =
+      static_cast<uint64_t>(num_bytes /
+                            static_cast<long double>(delayed_write_rate_) *
+                            kMicrosPerSecond) +
+      sleep_debt;
+  last_refill_time_ = time_now + sleep_amount;
+  return sleep_amount;
+}
+    
+     public:
+  PosixMmapFile(const std::string& fname, int fd, size_t page_size,
+                const EnvOptions& options);
+  ~PosixMmapFile();
+    
+    std::string kDBPath = '/tmp/rocksdb_column_families_example';
+    
+    int main() {
+  Options options;
+  options.create_if_missing = true;
+  // Disable RocksDB background compaction.
+  options.compaction_style = kCompactionStyleNone;
+  // Small slowdown and stop trigger for experimental purpose.
+  options.level0_slowdown_writes_trigger = 3;
+  options.level0_stop_writes_trigger = 5;
+  options.IncreaseParallelism(5);
+  options.listeners.emplace_back(new FullCompactor(options));
+    }
+    
+      // Initialize pointer options for each column family
+  for (size_t i = 0; i < loaded_cf_descs.size(); ++i) {
+    auto* loaded_bbt_opt = reinterpret_cast<BlockBasedTableOptions*>(
+        loaded_cf_descs[0].options.table_factory->GetOptions());
+    // Expect the same as BlockBasedTableOptions will be loaded form file.
+    assert(loaded_bbt_opt->block_size == bbt_opts.block_size);
+    // However, block_cache needs to be manually initialized as documented
+    // in rocksdb/utilities/options_util.h.
+    loaded_bbt_opt->block_cache = cache;
   }
-    }
+  // In addition, as pointer options are initialized with default value,
+  // we need to properly initialized all the pointer options if non-defalut
+  // values are used before calling DB::Open().
+  assert(loaded_cf_descs[0].options.compaction_filter == nullptr);
+  loaded_cf_descs[0].options.compaction_filter = compaction_filter.get();
+    
+    #include <string>
+#include 'rocksdb/table.h'
+    
+      // Waits for all jobs to complete those
+  // that already started running and those that did not
+  // start yet. This ensures that everything that was thrown
+  // on the TP runs even though
+  // we may not have specified enough threads for the amount
+  // of jobs
+  virtual void WaitForJobsAndJoinAllThreads() = 0;
+    
+    #if defined(BOOST_ATOMIC_DETAIL_X86_HAS_CMPXCHG16B)
