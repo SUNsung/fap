86cbf02334f75f8a30332a90b63b49595aaf4040
SUNsung <weatherius@gmail.com>
2019-01-26 05:17:42 +0200

    Rewrite [06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py] File in: 2019-01-26 05:17:41

--- pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
+++ pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
@@ -1,154 +1,160 @@
 
         
-            self.noise_bxn = noise_bxn = tf.random_normal(size_bxn)
-    self.noise_bxn.set_shape([None, z_size])
-    self.sample_bxn = mean_bxn + tf.exp(0.5 * logvar_bxn) * noise_bxn
+        from mrjob.job import MRJob
     
-        The mean values for both initial conditions and the control inputs are
-    pushed through the model instead of sampling (as is done in
-    eval_model_runs_avg_epoch).
-    This is a quick and approximate version of estimating these values instead
-    of sampling from the posterior many times and then averaging those values of
-    interest.
-    
-      # Note that we put these 'truth' rates and input into this
-  # structure, the only data that is used in LFADS are the noisy
-  # data e.g. spike trains.  The rest is either for printing or posterity.
-  data = {'train_truth': rates_train,
-          'valid_truth': rates_valid,
-          'input_train_truth' : input_train,
-          'input_valid_truth' : inputs_valid,
-          'train_data' : noisy_data_train,
-          'valid_data' : noisy_data_valid,
-          'train_percentage' : train_percentage,
-          'nreplications' : nreplications,
-          'dt' : rnn['dt'],
-          'input_magnitude' : input_magnitude,
-          'input_times_train' : input_times_train,
-          'input_times_valid' : input_times_valid,
-          'P_sxn' : P_sxn,
-          'condition_labels_train' : condition_labels_train,
-          'condition_labels_valid' : condition_labels_valid,
-          'conversion_factor': 1.0 / rnn['conversion_factor']}
-  datasets[dataset_name] = data
-    
-    # just one dataset here
-datasets = {}
-dataset_name = 'dataset_N' + str(N)
-datasets[dataset_name] = data
-    
-      @property
-  def eos(self):
-    return self._eos
-    
-      return word_to_id
+        def set(self, results, query):
+        '''Set the result for the given query key in the cache.
     
     
-def main(_):
-  hparams = train_mask_gan.create_hparams()
-  log_dir = FLAGS.base_directory
-    
-    from .compat import is_py2, builtin_str, str
+class PrettyStream(EncodedStream):
+    '''In addition to :class:`EncodedStream` behaviour, this stream applies
+    content processing.
     
     
-def info():
-    '''Generate information for a bug report.'''
+def test_credentials_in_url(httpbin_both):
+    url = add_auth(httpbin_both.url + '/basic-auth/user/password',
+                   auth='user:password')
+    r = http('GET', url)
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+    
+def test_migrate_implicit_content_type():
+    config = MockEnvironment().config
+    
+    
+def test_unicode_form_item_verbose(httpbin):
+    r = http('--verbose', '--form',
+             'POST', httpbin.url + '/post', u'test=%s' % UNICODE)
+    assert HTTP_OK in r
+    assert UNICODE in r
+    
+    from .compat import urlparse, str, basestring
+from .cookies import extract_cookies_to_jar
+from ._internal_utils import to_native_string
+from .utils import parse_dict_header
+    
+    ``response``:
+    The response generated from a Request.
+'''
+HOOKS = ['response']
+    
+        def test_super_len_with_no__len__(self):
+        class LenFile(object):
+            def __init__(self):
+                self.len = 5
+    
+    # Documents to append as an appendix to all manuals.
+#latex_appendices = []
+    
+        Save previous value, yield, and then restore the previous value stored in
+    the environment variable 'env_name'.
+    
+    
+def make_setting_element(setting_data, app, fromdocname):
+    refnode = make_refnode(app.builder, fromdocname,
+                           todocname=setting_data['docname'],
+                           targetid=setting_data['refid'],
+                           child=nodes.Text(setting_data['setting_name']))
+    p = nodes.paragraph()
+    p += refnode
+    
+            if opts.logfile:
+            self.settings.set('LOG_ENABLED', True, priority='cmdline')
+            self.settings.set('LOG_FILE', opts.logfile, priority='cmdline')
+    
+        def _err(self, msg):
+        sys.stderr.write(msg + os.linesep)
+        self.exitcode = 1
+    
+    
+class Command(ScrapyCommand):
+    
+            if not assertion:
+            if self.min_bound == self.max_bound:
+                expected = self.min_bound
+            else:
+                expected = '%s..%s' % (self.min_bound, self.max_bound)
+    
+        :param key: name of cli constant
+    :return: value of constant for active os
+    '''
+    return CLI_DEFAULTS[key]
+    
+        def test_timeout(self):
+        self.assertTrue(self.timeout.timeout)
+        self.assertFalse(self.invalid.timeout)
+    
+        @certbot_util.patch_get_utility()
+    def test_multiple_names(self, mock_util):
+        mock_util().menu.return_value = (display_util.OK, 5)
+    
+        def test_eq(self):
+        self.assertTrue(self.vhost1b == self.vhost1)
+        self.assertFalse(self.vhost1 == self.vhost2)
+        self.assertEqual(str(self.vhost1b), str(self.vhost1))
+        self.assertFalse(self.vhost1b == 1234)
+    
+        def test_perform2(self):
+        # Avoid load module
+        self.sni.configurator.parser.modules.add('ssl_module')
+        self.sni.configurator.parser.modules.add('socache_shmcb_module')
+        acme_responses = []
+        for achall in self.achalls:
+            self.sni.add_chall(achall)
+            acme_responses.append(achall.response(self.auth_key))
+    
+    # If true, show URL addresses after external links.
+#man_show_urls = False
+    
+        long_description = README,
+    
+        # Get options and arguments.
     try:
-        platform_info = {
-            'system': platform.system(),
-            'release': platform.release(),
-        }
-    except IOError:
-        platform_info = {
-            'system': 'Unknown',
-            'release': 'Unknown',
-        }
+        opts, args = getopt.getopt(sys.argv[1:], _short_options, _options)
+    except getopt.GetoptError as e:
+        log.wtf('''
+    [Fatal] {}.
+    Try '{} --help' for more options.'''.format(e, script_name))
     
+        def extract(self, **kwargs):
+        for i in self.streams:
+            s = self.streams[i]
+            _, s['container'], s['size'] = url_info(s['url'])
+            s['src'] = [s['url']]
     
-@pytest.mark.parametrize(
-    'value, expected', (
-        (
-            '<http:/.../front.jpeg>; rel=front; type='image/jpeg'',
-            [{'url': 'http:/.../front.jpeg', 'rel': 'front', 'type': 'image/jpeg'}]
-        ),
-        (
-            '<http:/.../front.jpeg>',
-            [{'url': 'http:/.../front.jpeg'}]
-        ),
-        (
-            '<http:/.../front.jpeg>;',
-            [{'url': 'http:/.../front.jpeg'}]
-        ),
-        (
-            '<http:/.../front.jpeg>; type='image/jpeg',<http://.../back.jpeg>;',
-            [
-                {'url': 'http:/.../front.jpeg', 'type': 'image/jpeg'},
-                {'url': 'http://.../back.jpeg'}
-            ]
-        ),
-        (
-            '',
-            []
-        ),
-    ))
-def test_parse_header_links(value, expected):
-    assert parse_header_links(value) == expected
+    def cbs_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    '''Downloads CBS videos by URL.
+    '''
     
+    def rebuilt_url(url):
+    path = urllib.parse.urlparse(url).path
+    aid = path.split('/')[-1].split('_')[0]
+    return 'http://www.dailymotion.com/embed/video/{}?autoplay=1'.format(aid)
     
-    {    # Server Error.
-    500: ('internal_server_error', 'server_error', '/o\\', '✗'),
-    501: ('not_implemented',),
-    502: ('bad_gateway',),
-    503: ('service_unavailable', 'unavailable'),
-    504: ('gateway_timeout',),
-    505: ('http_version_not_supported', 'http_version'),
-    506: ('variant_also_negotiates',),
-    507: ('insufficient_storage',),
-    509: ('bandwidth_limit_exceeded', 'bandwidth'),
-    510: ('not_extended',),
-    511: ('network_authentication_required', 'network_auth', 'network_authentication'),
+    from ..common import *
+from .ckplayer import ckplayer_download
+    
+    recur_limit = 3
+    
+        print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls(urls, title, ext, size, output_dir, merge=False)
+    
+    # looks that flickr won't return urls for all sizes
+# we required in 'extras field without a acceptable header
+dummy_header = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'
 }
+def get_content_headered(url):
+    return get_content(url, dummy_header)
     
-    
-class AppidManager(object):
-    lock = threading.Lock()
-    
-    Here is a little overview over the most commonly used classes provided by
-this runtime:
-    
-        def owner(self):
-        '''
-        Return the login name of the file owner.
-        '''
-        import pwd
-        return pwd.getpwuid(self.stat().st_uid).pw_name
-    
-        def test_ignore_PYTHONPATH(self):
-        path = 'should_be_ignored'
-        self.run_ignoring_vars(''{}' not in sys.path'.format(path),
-                               PYTHONPATH=path)
-    
-        print('Unpickled records:')
-    pprint.pprint(memos)
-    
-    # Make a local copy of what we are going to send.
-with open('outgoing.msg', 'wb') as f:
-    f.write(bytes(msg))
-    
-    # Of course, there are lots of email messages that could break this simple
-# minded program, but it will handle the most common ones.
-
-    
-    def calculate(func, args):
-    result = func(*args)
-    return '%s says that %s%s = %s' % \
-        (current_process().name, func.__name__, args, result)
-    
-    def shorten_title(title):
-    m1 = re.search('[[0-9]*]', title)
-    m2 = re.search(''.*'', title)
-    if m1:
-        title = m1.group(0)
-    if m2:
-        title = ' '.join((title, m2.group(0)))   
-    return title[:50] + ' [...]'    
+        # Remember top of the previous nesting stack.
+    #
+    # The stack is always pushed/popped and not modified in place, so
+    # we can just do a shallow copy instead of copy.deepcopy.  Using
+    # deepcopy would slow down cpplint by ~28%.
+    if self.stack:
+      self.previous_stack_top = self.stack[-1]
+    else:
+      self.previous_stack_top = None
