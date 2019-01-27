6170b070c525e6c9be64efa2ea98fd046c372fe7
SUNsung <weatherius@gmail.com>
2019-01-27 07:49:30 +0200

    Rewrite [479NX67xNU806471jgr1loTtI47tb5KEp1DvYM47vps.py] File in: 2019-01-27 07:49:29

--- pyton/479NX67xNU806471jgr1loTtI47tb5KEp1DvYM47vps.py
+++ pyton/479NX67xNU806471jgr1loTtI47tb5KEp1DvYM47vps.py
@@ -1,45 +1,115 @@
 
         
-        model.compile(loss='categorical_crossentropy',
-              optimizer=Adam(lr=lr_schedule(0)),
-              metrics=['accuracy'])
-model.summary()
-print(model_type)
+        
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    '''Log in a registered user by adding the user id to the session.'''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
     
-    from .cifar import load_batch
-from ..utils.data_utils import get_file
-from .. import backend as K
-import numpy as np
-import os
     
-        return (x_train, y_train), (x_test, y_test)
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+    
+    from functools import partial
+from werkzeug.local import LocalStack, LocalProxy
+    
+        class _FakeSignal(object):
+        '''If blinker is unavailable, create a fake class with the same
+        interface that allows sending of signals but will fail with an
+        error on anything else.  Instead of doing anything on send, it
+        will just ignore the arguments and do nothing instead.
+        '''
+    
+        if 'json' in kwargs:
+        assert 'data' not in kwargs, (
+            'Client cannot provide both 'json' and 'data'.'
+        )
+    
+    
+class QuadraticProbing(HashTable):
+    '''
+        Basic Hash Table example with open addressing using Quadratic Probing 
+    '''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        def __solveDP(self, x, y):
+        if (x==-1):
+            return y+1
+        elif (y==-1):
+            return x+1
+        elif (self.dp[x][y]>-1):
+            return self.dp[x][y]
+        else:
+            if (self.A[x]==self.B[y]):
+                self.dp[x][y] = self.__solveDP(x-1,y-1)
+            else:
+                self.dp[x][y] = 1+min(self.__solveDP(x,y-1), self.__solveDP(x-1,y), self.__solveDP(x-1,y-1))
+    
+        for i in range(1, n+1):
+        dp[i][0] = True
+    
+            if self.depth == 1:
+            self.prediction = np.mean(y)
+            return
+    
+    #Root Mean Square Logarithmic Error
+def rmsle(predict, actual):
+    predict = np.array(predict)
+    actual = np.array(actual)
+    
+    
+def b_expo_mod(a, b, c):
+    res = 1
+    while b > 0:
+        if b&1:
+            res = ((res%c) * (a%c)) % c
+    
+        return res
+    
+    def getItemAtIndexZero(x):
+    return x[0]
+    
+    #A Python script to generate a single PDF document with all the tldr pages. It works by generating 
+#intermediate HTML files from existing md files using Python-markdown, applying desired formatting 
+#through CSS, and finally rendering them as PDF. There is no LaTeX dependency for generating the PDF.
+    
+        :param img: A list of images (each as a numpy array)
+    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
+    :param batch_size: How many images to include in each GPU processing batch.
+    :return: A list of tuples of found face locations in css (top, right, bottom, left) order
+    '''
+    def convert_cnn_detections_to_css(detections):
+        return [_trim_css_to_bounds(_rect_to_css(face.rect), images[0].shape) for face in detections]
+    
+    
+def test_image(image_to_check, model):
+    unknown_image = face_recognition.load_image_file(image_to_check)
+    face_locations = face_recognition.face_locations(unknown_image, number_of_times_to_upsample=0, model=model)
+    
+            print('I see someone named {}!'.format(name))
 
     
-        with gzip.open(paths[1], 'rb') as imgpath:
-        x_train = np.frombuffer(imgpath.read(), np.uint8,
-                                offset=16).reshape(len(y_train), 28, 28)
+    # Find all the faces and face encodings in the unknown image
+face_locations = face_recognition.face_locations(unknown_image)
+face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
     
+        def test_partial_face_locations(self):
+        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama_partial_face.jpg'))
+        detected_faces = api.face_locations(img)
     
-class L1L2(Regularizer):
-    '''Regularizer for L1 and L2 regularization.
+    test_requirements = [
+    'tox',
+    'flake8==2.6.0'
+]
     
-    
-def test_activity_regularization():
-    x_train, y_train = get_data()
-    for reg in [regularizers.l1(), regularizers.l2()]:
-        model = create_model(activity_regularizer=reg)
-        model.compile(loss='categorical_crossentropy', optimizer='sgd')
-        assert len(model.losses) == 1
-        model.train_on_batch(x_train, y_train)
-    
-        @classmethod
-    def from_config(cls, config):
-        return cls(**config)
-    
-    
-batch_size = 32
-num_classes = 10
-epochs = 100
-num_predictions = 20
-save_dir = '/tmp/saved_models'
-model_name = 'keras_cifar10_trained_model.h5'
+            file = request.files['file']
