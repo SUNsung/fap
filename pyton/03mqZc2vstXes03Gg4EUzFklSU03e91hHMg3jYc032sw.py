3b8c1e099289a22bbf580e1f88391d894ffcce77
SUNsung <weatherius@gmail.com>
2019-01-27 10:01:00 +0200

    Rewrite [03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py] File in: 2019-01-27 10:00:59

--- pyton/03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py
+++ pyton/03mqZc2vstXes03Gg4EUzFklSU03e91hHMg3jYc032sw.py
@@ -1,91 +1,63 @@
 
         
-        # Read lines from the linkcheck output file
-try:
-    with open('build/linkcheck/output.txt') as out:
-        output_lines = out.readlines()
-except IOError:
-    print('linkcheck output not found; please run linkcheck first.')
-    exit(1)
+        import json
+import re
     
-        def start_requests(self):
-        qargs = {'total': self.total, 'show': self.show}
-        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
-        return [scrapy.Request(url, dont_filter=True)]
     
-            editor = self.settings['EDITOR']
-        try:
-            spidercls = self.crawler_process.spider_loader.load(args[0])
-        except KeyError:
-            return self._err('Spider not found: %s' % args[0])
+def get_coub_data(html):
+    coub_data = r1(r'<script id=\'coubPageCoubJson\' type=\'text/json\'>([^<]+)</script>', html)
+    json_data = json.loads(coub_data)
+    return json_data
     
-        def process_spider_arguments(self, opts):
+        mime, ext, size = url_info(real_url)
     
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
-                          help='set spider argument (may be repeated)')
-        parser.add_option('-o', '--output', metavar='FILE',
-                          help='dump scraped items into FILE (use - for stdout)')
-        parser.add_option('-t', '--output-format', metavar='FORMAT',
-                          help='format to use for dumping items with -o')
+                if item.get(CONF_TEXT) is not None:
+                if isinstance(item.get(CONF_TEXT), template.Template):
+                    output[ATTR_MAIN_TEXT] = item[CONF_TEXT].async_render()
+                else:
+                    output[ATTR_MAIN_TEXT] = item.get(CONF_TEXT)
     
-        # Gloss the lips
-    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
-    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
-    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
-    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+            self._channel = self._create_channel()
+        client.add_connection_channel(self._channel)
     
-        # For a definition of each point index, see https://cdn-images-1.medium.com/max/1600/1*AbEg31EgkbXSQehuNJBlWg.png
-    if model == 'large':
-        return [{
-            'chin': points[0:17],
-            'left_eyebrow': points[17:22],
-            'right_eyebrow': points[22:27],
-            'nose_bridge': points[27:31],
-            'nose_tip': points[31:36],
-            'left_eye': points[36:42],
-            'right_eye': points[42:48],
-            'top_lip': points[48:55] + [points[64]] + [points[63]] + [points[62]] + [points[61]] + [points[60]],
-            'bottom_lip': points[54:60] + [points[48]] + [points[60]] + [points[67]] + [points[66]] + [points[65]] + [points[64]]
-        } for points in landmarks_as_tuples]
-    elif model == 'small':
-        return [{
-            'nose_tip': [points[4]],
-            'left_eye': points[2:4],
-            'right_eye': points[0:2],
-        } for points in landmarks_as_tuples]
-    else:
-        raise ValueError('Invalid landmarks model type. Supported models are ['small', 'large'].')
+            states = dict(state.attributes)
+        metric = '{}.{}'.format(prefix, state.domain)
+        tags = ['entity:{}'.format(state.entity_id)]
     
-        if os.path.isdir(image_to_check):
-        if cpus == 1:
-            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
-        else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
-    else:
-        test_image(image_to_check, model)
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/device_tracker.actiontec/
+'''
+import logging
+import re
+import telnetlib
+from collections import namedtuple
+import voluptuous as vol
     
-                face_names.append(name)
+    import homeassistant.helpers.config_validation as cv
+from homeassistant.components.device_tracker import (
+    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import (
+    CONF_HOST, CONF_PASSWORD, CONF_USERNAME, CONF_TYPE
+)
     
-    # Get a reference to the Raspberry Pi camera.
-# If this fails, make sure you have a camera connected to the RPi and that you
-# enabled your camera in raspi-config and rebooted first.
-camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-output = np.empty((240, 320, 3), dtype=np.uint8)
+                    _LOGGER.debug('Device %s is connected', mac)
+                self.last_results[mac] = name
+        except (KeyError, IndexError):
+            _LOGGER.exception('Router returned unexpected response')
+            return False
+        return True
     
-    # Loop through each face found in the unknown image
-for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-    # See if the face is a match for the known face(s)
-    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    _DEVICES_REGEX = re.compile(
+    r'(?P<mac>(([0-9a-f]{2}[:-]){5}([0-9a-f]{2})))\s'
+    r'(?P<ip>([0-9]{1,3}[\.]){3}[0-9]{1,3})\s+'
+    r'(?P<status>([^\s]+))\s+'
+    r'(?P<type>([^\s]+))\s+'
+    r'(?P<intf>([^\s]+))\s+'
+    r'(?P<hwintf>([^\s]+))\s+'
+    r'(?P<host>([^\s]+))')
     
-            if True in result:
-            [print_result(image_to_check, name, distance, show_distance) for is_match, name, distance in zip(result, known_names, distances) if is_match]
-        else:
-            print_result(image_to_check, 'unknown_person', None, show_distance)
-    
-    # 你需要一个2代以上的树莓派，并在树莓派上安装face_recognition，并连接上picamera摄像头
-# 并确保picamera这个模块已经安装（树莓派一般会内置安装）
-# 你可以参考这个教程配制你的树莓派：
-# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
+            result = self._retrieve_list_with_retry()
+        if result:
+            self._store_result(result)
+            return True
+        return False
