dc5eb0815ae14b4e61615f4e0cf4f797e7a2fa09
SUNsung <weatherius@gmail.com>
2019-01-28 01:04:12 +0200

    Rewrite [275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb] File in: 2019-01-28 01:04:11

--- ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
+++ ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
@@ -1,161 +1,208 @@
 
         
-          config.on_event :test_run_finished do
-    f.print_worst_offenders
-  end
-end
-
-    
-      matter = matter.join.chomp
-  content = \
-    if !input_hash['input'] || !input_hash['filter']
-      then input_hash['content']
-    else '{{ #{input_hash['input']} | ' \
-        '#{input_hash['filter']} }}'
-    end
-    
-        # Deletes a machine by UUID.
-    #
-    # The machine being deleted with this UUID must either be locked
-    # by this index or must be unlocked.
-    #
-    # @param [Entry] entry The entry to delete.
-    # @return [Boolean] true if delete is successful
-    def delete(entry)
-      return true if !entry.id
-    
-            # This is called as a last-minute hook that allows the configuration
-        # object to finalize itself before it will be put into use. This is
-        # a useful place to do some defaults in the case the user didn't
-        # configure something or so on.
-        #
-        # An example of where this sort of thing is used or has been used:
-        # the 'vm' configuration key uses this to make sure that at least
-        # one sub-VM has been defined: the default VM.
-        #
-        # The configuration object is expected to mutate itself.
-        def finalize!
-          # Default implementation is to do nothing.
-        end
-    
-              # Set all of our instance variables on the new class
-          [self, other].each do |obj|
-            obj.instance_variables.each do |key|
-              # Ignore keys that start with a double underscore. This allows
-              # configuration classes to still hold around internal state
-              # that isn't propagated.
-              if !key.to_s.start_with?('@__')
-                # Don't set the value if it is the unset value, either.
-                value = obj.instance_variable_get(key)
-                result.instance_variable_set(key, value) if value != UNSET_VALUE
-              end
+                  def name_and_id_index(options)
+            if options.key?('index')
+              options.delete('index') || ''
+            elsif @generate_indexed_names
+              @auto_index || ''
             end
           end
     
-    module Vagrant
-  module Plugin
-    module V2
-      # This is the superclass for all V2 plugins.
-      class Plugin
-        # Special marker that can be used for action hooks that matches
-        # all action sequences.
-        ALL_ACTIONS = :__all_actions__
+              content_is_options = content_or_options.is_a?(Hash)
+          if content_is_options
+            options.merge! content_or_options
+            @content = nil
+          else
+            @content = content_or_options
+          end
     
-        # Get a value by the given key.
-    #
-    # This will evaluate the block given to `register` and return the
-    # resulting value.
-    def get(key)
-      return nil if !@items.key?(key)
-      return @results_cache[key] if @results_cache.key?(key)
-      @results_cache[key] = @items[key].call
-    end
-    alias :[] :get
+    module ActionView
+  # This is the main entry point for rendering. It basically delegates
+  # to other objects like TemplateRenderer and PartialRenderer which
+  # actually renders the template.
+  #
+  # The Renderer will parse the options from the +render+ or +render_body+
+  # method and render a partial or a template based on the options. The
+  # +TemplateRenderer+ and +PartialRenderer+ objects are wrappers which do all
+  # the setup and logic necessary to render a view and a new object is created
+  # each time +render+ is called.
+  class Renderer
+    attr_accessor :lookup_context
     
-      def process_salmon
-    SalmonWorker.perform_async(@account.id, payload.force_encoding('UTF-8'))
+        open_dry_run_modal(agent)
+    click_on('Dry Run')
+    expect(page).to have_text('Dry Run started')
+    expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabLog']')
   end
 end
 
     
-      def subscription
-    @_subscription ||= @account.subscription(
-      api_subscription_url(@account.id)
-    )
+        it 'returns a label 'No' if any falsy value is given' do
+      [false, nil].each { |value|
+        label = yes_no(value)
+        expect(label).to be_html_safe
+        expect(Nokogiri(label).text).to eq 'No'
+      }
+    end
   end
     
-          if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: provider_id.capitalize) if is_navigational_format?
-      else
-        session['devise.#{provider}_data'] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
+      describe '#style_colors' do
+    it 'returns a css style-formated version of the scenario foreground and background colors' do
+      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
+    end
+    
+      let :new_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title', 'hidden' => true }
+    }
+  end
+    
+      # POST /resource/sign_in
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    respond_with resource, location: after_sign_in_path_for(resource)
+  end
+    
+      # Override prefixes to consider the scoped view.
+  # Notice we need to check for the request due to a bug in
+  # Action Controller tests that forces _prefixes to be
+  # loaded before even having a request object.
+  #
+  # This method should be public as it is in ActionPack
+  # itself. Changing its visibility may break other gems.
+  def _prefixes #:nodoc:
+    @_prefixes ||= if self.class.scoped_views? && request && devise_mapping
+      ['#{devise_mapping.scoped_path}/#{controller_name}'] + super
+    else
+      super
+    end
+  end
+    
+          # Stores the provided location to redirect the user after signing in.
+      # Useful in combination with the `stored_location_for` helper.
+      #
+      # Example:
+      #
+      #   store_location_for(:user, dashboard_path)
+      #   redirect_to user_facebook_omniauth_authorize_path
+      #
+      def store_location_for(resource_or_scope, location)
+        session_key = stored_location_key_for(resource_or_scope)
+        
+        path = extract_path_from_location(location)
+        session[session_key] = path if path
       end
-    end
+    
+            if Devise.activerecord51?
+          def clear_reset_password_token?
+            encrypted_password_changed = respond_to?(:will_save_change_to_encrypted_password?) && will_save_change_to_encrypted_password?
+            authentication_keys_changed = self.class.authentication_keys.any? do |attribute|
+              respond_to?('will_save_change_to_#{attribute}?') && send('will_save_change_to_#{attribute}?')
+            end
+    
+        def create
+      authorize :status, :update?
+    
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
   end
-    
-          it 'displays Float::INFINITY as Inf' do
-        format('%a', Float::INFINITY).should == 'Inf'
-        format('%a', -Float::INFINITY).should == '-Inf'
-      end
-    
-        t.wakeup
-    t.value.should == 5
-  end
-end
-    
-        $?.should be_an_instance_of Process::Status
-    $?.success?.should == true
-  end
-    
-      it 'does not raise an error on a tainted, frozen object' do
-    o = Object.new.taint.freeze
-    o.taint.should equal(o)
-  end
-    
-      it 'calls #to_path on second argument when passed ?d and a directory' do
-    p = mock('path')
-    p.should_receive(:to_path).and_return @dir
-    Kernel.test(?d, p)
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
   end
 end
     
-    class FixPhotosShareVisibilities < ActiveRecord::Migration[4.2]
-  class Photo < ApplicationRecord
-  end
-    
-      class FetchWebfinger < Base
-    def perform(*_args)
-      # don't do real discovery in cucumber
-    end
-  end
-end
-
-    
-        it 'generates a jasmine fixture', fixture: true do
-      session[:mobile_view] = true
-      get :new, format: :mobile
-      save_fixture(html_for('body'), 'conversations_new_mobile')
-    end
-  end
-end
-
-    
-    describe StatusMessagesController, :type => :controller do
-  describe '#bookmarklet' do
+    describe ConversationsController, :type => :controller do
+  describe '#index' do
     before do
-      sign_in bob, scope: :user
-    end
+      @person = alice.contacts.first.person
+      hash = {
+        :author => @person,
+        :participant_ids => [alice.person.id, @person.id],
+        :subject => 'not spam',
+        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
+      }
+      @conv1 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
     
-    describe ParticipationsController, :type => :controller do
-  before do
-    allow(@controller).to receive(:current_user).and_return(alice)
-    sign_in alice, scope: :user
-  end
-    
-          it 'returns an empty array for a post visible to the user' do
-        sign_in(bob, scope: :user)
-        get :index, params: {post_id: @post.id}, format: :json
-        expect(JSON.parse(response.body)).to eq([])
+        shared_examples 'on a visible post' do
+      it 'creates the participation' do
+        post :create, params: {post_id: @post.id}
+        expect(alice.participations.where(:target_id => @post.id)).to exist
+        expect(response.code).to eq('201')
       end
     end
+    
+          context 'resharing a reshared post' do
+        before do
+          FactoryGirl.create(:reshare, :root => @post, :author => bob.person)
+        end
+    
+      gem 'danger'
+end
+    
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
+          end
+        end
+    
+        def URIEncodeSingle(cc, result, index)
+      x = (cc >> 12) & 0xF;
+      y = (cc >> 6) & 63;
+      z = cc & 63;
+      octets = Array.new(3);
+      if (cc <= 0x007F)
+        octets[0] = cc;
+      elsif (cc <= 0x07FF)
+        octets[0] = y + 192;
+        octets[1] = z + 128;
+      else
+        octets[0] = x + 224;
+        octets[1] = y + 128;
+        octets[2] = z + 128;
+      end
+      return URIEncodeOctets(octets, result, index);
+    end
+    
+          def upload_dest
+        @upload_dest
+      end
+    
+          def partial(name)
+        if name == :author_template
+          self.class.partial('history_authors/#{@wiki.user_icons}')
+        else
+          super
+        end
+      end
+    
+        @wiki.clear_cache
+    assert_nil @wiki.paged('H', 'G')
+    page_2 = @wiki.paged('C', 'G/K')
+    assert_equal 'INITIAL\n\nSPAM2\n', page_2.raw_data
+    assert_equal 'def', page_2.version.message
+    assert_not_equal page_1.version.sha, page_2.version.sha
+  end
+    
+        # Test page_header_from_content(@content)
+    actual = @view.title
+    assert_equal '1 & 2', actual
+  end
