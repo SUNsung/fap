b1aedec01917f5a848c6c748e2628d037472ffa6
SUNsung <weatherius@gmail.com>
2019-01-28 04:35:22 +0200

    Rewrite [535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb] File in: 2019-01-28 04:35:21

--- ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
+++ ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
@@ -1,280 +1,116 @@
 
         
-        def content_for(file)
-  contents = File.read(file)
-  case file
-  when 'History.markdown'
-    converted_history(contents)
-  else
-    contents.gsub(%r!\A# .*\n\n?!, '')
+          it 'requires a URL or file uplaod' do
+    visit new_scenario_imports_path
+    click_on 'Start Import'
+    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
   end
+    
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
+    end
+    
+        it 'returns nil when the path cannot be followed' do
+      expect(Utils.value_at({ :foo => { :bar => :baz }}, 'foo.bing')).to be_nil
+    end
+  end
+    
+          if @email_domain_block.save
+        log_action :create, @email_domain_block
+        redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.created_msg')
+      else
+        render :new
+      end
+    end
+    
+        def destroy
+      authorize @report_note, :destroy?
+      @report_note.destroy!
+      redirect_to admin_report_path(@report_note.report_id), notice: I18n.t('admin.report_notes.destroyed_msg')
+    end
+    
+      def payload
+    @_payload ||= request.body.read
+  end
+    
+        render_empty
+  end
+    
+      def up
+    Photo.joins('INNER JOIN posts ON posts.guid = photos.status_message_guid')
+         .where(posts: {type: 'StatusMessage', public: true}).update_all(public: true)
+    
+      def navigate_to(page_name)
+    path = path_to(page_name)
+    if path.is_a?(Hash)
+      visit(path[:path])
+      await_elem = path[:special_elem]
+      find(await_elem.delete(:selector), await_elem)
+    else
+      visit(path)
+    end
+  end
+    
+    describe ConversationsController, :type => :controller do
+  describe '#index' do
+    before do
+      @person = alice.contacts.first.person
+      hash = {
+        :author => @person,
+        :participant_ids => [alice.person.id, @person.id],
+        :subject => 'not spam',
+        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
+      }
+      @conv1 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+    
+        context 'on my own post' do
+      before do
+        aspect_to_post = alice.aspects.where(:name => 'generic').first
+        @post = alice.post :status_message, :text => 'something', :to => aspect_to_post
+      end
+    
+    begin
+  require 'bundler'
+  Bundler::GemHelper.install_tasks
+rescue LoadError => e
+  $stderr.puts e
 end
     
-    require 'benchmark/ips'
-require 'jekyll'
-require 'json'
-    
-    module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
-    
-                  EM.start_server(
-                opts['host'],
-                opts['livereload_port'],
-                HttpAwareConnection,
-                opts
-              ) do |ws|
-                handle_websockets_event(ws)
-              end
-    
-              # This is too noisy even for --verbose, but uncomment if you need it for
-          # a specific WebSockets issue.  Adding ?LR-verbose=true onto the URL will
-          # enable logging on the client side.
-          # em_opts[:debug] = true
-    
-            def initialize(config)
-          @main_fallback_highlighter = config['highlighter'] || 'rouge'
-          @config = config['kramdown'] || {}
-          @highlighter = nil
-          setup
-        end
-    
-          def self.authors
-        ['lmirosevic', 'maschall']
+          def default_options
+        DEFAULT_OPTIONS
       end
     
-          it 'shellescapes the exclude_dirs correctly' do
-        directory = 'My Dir'
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: ['#{directory}'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --exclude-dir #{directory.shellescape}')
+            reaction
       end
     
-            it 'escapes spaces in list of files to process' do
-          file = 'path/to/my project/source code/AppDelegate.swift'
-          result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-                files: ['#{file}']
-            )
-          end').runner.execute(:test)
-    
-          it 'should shell escape keychain names when checking for installation' do
-        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
-        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
-    
-                expect(FastlaneCore::UI).to receive(:input).and_return('val ask')
-            expect(config[:item]).to eq('val ask')
-          end
-        end
-      end
+        it 'Returns nil when Referer header is invalid' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
+      expect(subject.referrer(env)).to be_nil
     end
   end
 end
 
     
-    if git.modified_files.include?('snapshot/lib/assets/SnapshotHelperXcode8.swift')
-  warn('You modified `SnapshotHelperXcode8.swift`, make sure to update the version number at the bottom of the file to notify users about the new helper file.')
-end
-    
-          it 'raises ArgumentError if passed negative value' do
-        -> { warn '', uplevel: -2 }.should raise_error(ArgumentError)
-        -> { warn '', uplevel: -100 }.should raise_error(ArgumentError)
-      end
-    
-          close_client(cli)
-    end
+    module RailsCommandHelpers
+  def framework_version?(version_string)
+    framework_version =~ /^#{version_string}/
   end
     
-        when IAX_TYPE_CONTROL
-      case stype
-      when IAX_CTRL_HANGUP
-        dprint('HANGUP')
-        self.client.send_ack(self)
-        self.state = :hangup
-    
-              # Encodes the authenticator field
-          #
-          # @return [String]
-          def encode_authenticator
-            authenticator.encode
-          end
-        end
-      end
-    end
-  end
-end
-    
-              # Encodes a Rex::Proto::Kerberos::Model::Checksum into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            elems = []
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_type], 0, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_checksum], 1, :CONTEXT_SPECIFIC)
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of an encrypted message.
-        class EncryptedData < Element
-          # @!attribute name_type
-          #   @return [Integer] The encryption algorithm
-          attr_accessor :etype
-          # @!attribute kvno
-          #   @return [Integer] The version number of the key
-          attr_accessor :kvno
-          # @!attribute cipher
-          #   @return [String] The enciphered text
-          attr_accessor :cipher
-    
-                pre_auth
-          end
-    
-              # Decodes the msg_type from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_msg_type(input)
-            input.value[0].value.to_i
-          end
-    
-            title 'Running Integration tests'
-        sh 'bundle exec bacon spec/integration.rb'
-      end
-    
-    (allow file-read-metadata)
-(allow file-read*
-  ; This is currenly only added because using `xcodebuild` to build a resource
-  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
-  ; needed, but for now it doesn’t seem like a real problem.
-  (literal '/')
-  (regex
-    ; TODO see if we can restrict this more, but it's going to be hard
-    #'^/Users/[^.]+/*'
-    ;#'^/Users/[^.]+/.netrc'
-    ;#'^/Users/[^.]+/.gemrc'
-    ;#'^/Users/[^.]+/.gem/*'
-    ;#'^/Users/[^.]+/Library/.*'
-    #'^/Library/*'
-    #'^/System/Library/*'
-    #'^/usr/lib/*'
-    #'^/usr/share/*'
-    #'^/private/*'
-    #'^/dev/*'
-    #'^<%= ruby_prefix %>'
-    #'^<%= pod_prefix %>'
-    #'^<%= xcode_app_path %>'
-    #'^<%= Pod::Config.instance.repos_dir %>'
-<% prefixes.each do |prefix| %>
-    #'^<%= prefix %>/*'
-<% end %>
-  )
-)
-    
-      GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
-    file package(gem, '.gem') => ['pkg/', '#{directory + '/' + gem}.gemspec'] do |f|
-      sh 'cd #{directory} && gem build #{gem}.gemspec'
-      mv directory + '/' + File.basename(f.name), f.name
+        def initialize(klass, name, options)
+      @klass = klass
+      @name = name
+      @options = options
     end
     
-    <script>
-  // reading
-  var es = new EventSource('/stream');
-  es.onmessage = function(e) { $('#chat').append(e.data + '\n') };
-    
-        def frame_class(frame)
-      if frame.filename =~ %r{lib/sinatra.*\.rb}
-        'framework'
-      elsif (defined?(Gem) && frame.filename.include?(Gem.dir)) ||
-            frame.filename =~ %r{/bin/(\w+)\z}
-        'system'
-      else
-        'app'
-      end
+        # Returns the pluralized form of the attachment name. e.g.
+    # 'avatars' for an attachment of :avatar
+    def attachment attachment, style_name
+      plural_cache.pluralize_symbol(attachment.name)
     end
-    
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   XSS and others
-    # Supported browsers:: Firefox 23+, Safari 7+, Chrome 25+, Opera 15+
-    #
-    # Description:: Content Security Policy, a mechanism web applications
-    #               can use to mitigate a broad class of content injection
-    #               vulnerabilities, such as cross-site scripting (XSS).
-    #               Content Security Policy is a declarative policy that lets
-    #               the authors (or server administrators) of a web application
-    #               inform the client about the sources from which the
-    #               application expects to load resources.
-    #
-    # More info::   W3C CSP Level 1 : https://www.w3.org/TR/CSP1/ (deprecated)
-    #               W3C CSP Level 2 : https://www.w3.org/TR/CSP2/ (current)
-    #               W3C CSP Level 3 : https://www.w3.org/TR/CSP3/ (draft)
-    #               https://developer.mozilla.org/en-US/docs/Web/Security/CSP
-    #               http://caniuse.com/#search=ContentSecurityPolicy
-    #               http://content-security-policy.com/
-    #               https://securityheaders.io
-    #               https://scotthelme.co.uk/csp-cheat-sheet/
-    #               http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-    #
-    # Sets the 'Content-Security-Policy[-Report-Only]' header.
-    #
-    # Options: ContentSecurityPolicy configuration is a complex topic with
-    #          several levels of support that has evolved over time.
-    #          See the W3C documentation and the links in the more info
-    #          section for CSP usage examples and best practices. The
-    #          CSP3 directives in the 'NO_ARG_DIRECTIVES' constant need to be
-    #          presented in the options hash with a boolean 'true' in order
-    #          to be used in a policy.
-    #
-    class ContentSecurityPolicy < Base
-      default_options default_src: :none, script_src: ''self'',
-                      img_src: ''self'', style_src: ''self'',
-                      connect_src: ''self'', report_only: false
-    
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
-        end
-        bad_cookies.empty?
-      end
-    
-        headers = get('/', {}, 'wants' => 'text/html').headers
-    expect(headers['Content-Security-Policy']).to eq('block-all_mixed_content; connect-src 'self'; default-src none; disown-opener; img-src 'self'; script-src 'self'; style-src 'self'; upgrade-insecure_requests')
-  end
-    
-    desc 'Initial setup for Octopress: copies the default theme into the path of Jekyll's generator. Rake install defaults to rake install[classic] to install a different theme run rake install[some_theme_name]'
-task :install, :theme do |t, args|
-  if File.directory?(source_dir) || File.directory?('sass')
-    abort('rake aborted!') if ask('A theme is already installed, proceeding will overwrite existing files. Are you sure?', ['y', 'n']) == 'n'
-  end
-  # copy theme into working Jekyll directories
-  theme = args.theme || 'classic'
-  puts '## Copying '+theme+' theme into ./#{source_dir} and ./sass'
-  mkdir_p source_dir
-  cp_r '#{themes_dir}/#{theme}/source/.', source_dir
-  mkdir_p 'sass'
-  cp_r '#{themes_dir}/#{theme}/sass/.', 'sass'
-  mkdir_p '#{source_dir}/#{posts_dir}'
-  mkdir_p public_dir
-end
-    
-    class SinatraStaticServer < Sinatra::Base
-    
-    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
