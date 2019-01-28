5b4d74d098ed7e5e65d350997c3d8c6a74630be4
SUNsung <weatherius@gmail.com>
2019-01-28 13:48:42 +0200

    Rewrite [29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb] File in: 2019-01-28 13:48:41

--- ruby/29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb
+++ ruby/29cB..NWEb2A29TpgSCO6oTs29RGl9e9esdBY29yVx.rb
@@ -1,20 +1,255 @@
 
         
-          require ARGV.shift
-  exit_status = LogStash::Runner.run('bin/logstash', ARGV)
-  exit(exit_status || 0)
+        # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
+    
+    DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
+    
+    # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
 end
 
     
-      def target_file
-    target_file = File.join(LogStash::Environment::LOGSTASH_HOME, 'plugins_package')
-    '#{target_file}#{file_extension}'
+    def file_contents(path)
+  return Pathname.new(path).read
+end
+    
+            def print_message(json_message)
+          msg = JSON.parse(json_message)
+          # Not sure what the 'url' command even does in LiveReload.  The spec is silent
+          # on its purpose.
+          Jekyll.logger.info 'LiveReload:', 'Browser URL: #{msg['url']}' if msg['command'] == 'url'
+        end
+    
+            def make_accessible(hash = @config)
+          hash.keys.each do |key|
+            hash[key.to_sym] = hash[key]
+            make_accessible(hash[key]) if hash[key].is_a?(Hash)
+          end
+        end
+    
+          it 'activates an existing user' do
+        users(:bob).deactivate!
+        visit admin_users_path
+        find(:css, 'a[href='/admin/users/#{users(:bob).id}/activate']').click
+        expect(page).to have_no_text('inactive')
+        users(:bob).reload
+        expect(users(:bob)).to be_active
+      end
+    end
   end
 end
 
     
-            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
+              @bar3 = Agents::DotBar.new(name: 'bar3').tap { |agent|
+            agent.user = users(:bob)
+            agent.sources << @bar2
+            agent.save!
+          },
+        ]
+        @foo.reload
+        @bar2.reload
     
-      it 'returns the pipeline id' do
-    expect(subject.pipeline_id).to eq(pipeline_id)
+        it 'works for queued jobs' do
+      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
+    end
   end
+    
+      describe '#values_at' do
+    it 'returns arrays of matching values' do
+      expect(Utils.values_at({ :foo => { :bar => :baz }}, 'foo.bar')).to eq(%w[baz])
+      expect(Utils.values_at({ :foo => [ { :bar => :baz }, { :bar => :bing } ]}, 'foo[*].bar')).to eq(%w[baz bing])
+      expect(Utils.values_at({ :foo => [ { :bar => :baz }, { :bar => :bing } ]}, 'foo[*].bar')).to eq(%w[baz bing])
+    end
+    
+      let :valid_options do
+    {
+      'name' => 'XKCD',
+      'expected_update_period_in_days' => '2',
+      'type' => 'html',
+      'url' => '{{ url | default: 'http://xkcd.com/' }}',
+      'mode' => 'on_change',
+      'extract' => old_extract,
+      'template' => old_template
+    }
+  end
+    
+          @log.level = 5
+      expect(@log).not_to be_valid
+      expect(@log).to have(1).error_on(:level)
+    
+      describe '#working?' do
+    it 'should not be working until the first event was received' do
+      expect(@checker).not_to be_working
+      @checker.last_receive_at = Time.now
+      expect(@checker).to be_working
+    end
+  end
+    
+              # Make sure we're only working with one VM if single target
+          if options[:single_target] && vms.length != 1
+            vm = @env.primary_vm
+            raise Errors::MultiVMTargetRequired if !vm
+            vms = [vm]
+          end
+    
+            # Mounts a shared folder via NFS. This assumes that the exports
+        # via the host are already done.
+        def mount_nfs(ip, folders)
+          raise BaseError, _key: :unsupported_nfs
+        end
+    
+            # This registers a plugin. This should _NEVER_ be called by the public
+        # and should only be called from within Vagrant. Vagrant will
+        # automatically register V1 plugins when a name is set on the
+        # plugin.
+        def register(plugin)
+          if !@registered.include?(plugin)
+            @logger.info('Registered plugin: #{plugin.name}')
+            @registered << plugin
+          end
+        end
+    
+            # This is the method called to provision the system. This method
+        # is expected to do whatever necessary to provision the system (create files,
+        # SSH, etc.)
+        def provision!
+        end
+    
+              # Determine if we require a local Vagrant environment. There are
+          # two cases that we require a local environment:
+          #
+          #   * We're asking for ANY/EVERY VM (no names given).
+          #
+          #   * We're asking for specific VMs, at least once of which
+          #     is NOT in the local machine index.
+          #
+          requires_local_env = false
+          requires_local_env = true if names.empty?
+          requires_local_env ||= names.any? { |n|
+            !@env.machine_index.include?(n)
+          }
+          raise Errors::NoEnvironmentError if requires_local_env && !@env.root_path
+    
+            # Merge another configuration object into this one. This assumes that
+        # the other object is the same class as this one. This should not
+        # mutate this object, but instead should return a new, merged object.
+        #
+        # The default implementation will simply iterate over the instance
+        # variables and merge them together, with this object overriding
+        # any conflicting instance variables of the older object. Instance
+        # variables starting with '__' (double underscores) will be ignored.
+        # This lets you set some sort of instance-specific state on your
+        # configuration keys without them being merged together later.
+        #
+        # @param [Object] other The other configuration object to merge from,
+        #   this must be the same type of object as this one.
+        # @return [Object] The merged object.
+        def merge(other)
+          result = self.class.new
+    
+            # This should return the state of the machine within this provider.
+        # The state must be an instance of {MachineState}. Please read the
+        # documentation of that class for more information.
+        #
+        # @return [MachineState]
+        def state
+          nil
+        end
+    
+        # Register a key with a lazy-loaded value.
+    #
+    # If a key with the given name already exists, it is overwritten.
+    def register(key, &block)
+      raise ArgumentError, 'block required' if !block_given?
+      @items[key] = block
+    end
+    
+          # Returns the path to a file for the given key.
+      #
+      # @param key [String]
+      # @return [String] The path to the cache file.
+      def path_to(key)
+        key = key.gsub(/[<>:\\|?*%]/) {|c| '%%%03d' % c.ord}
+        File.join(cache_location, key)
+      end
+    end
+  end
+end
+
+    
+          opts.on('-C', '--no-cache', 'Don't cache parsed Sass files.') do
+        @options[:for_engine][:cache] = false
+      end
+    
+      <a href='/'>Refresh page</a>
+    
+          private
+    
+        def initialize(options)
+      @strictly_ordered_queues = !!options[:strict]
+      @queues = options[:queues].map { |q| 'queue:#{q}' }
+      if @strictly_ordered_queues
+        @queues = @queues.uniq
+        @queues << TIMEOUT
+      end
+    end
+    
+        def self.job_hash_context(job_hash)
+      # If we're using a wrapper class, like ActiveJob, use the 'wrapped'
+      # attribute to expose the underlying thing.
+      klass = job_hash['wrapped'] || job_hash['class']
+      bid = job_hash['bid']
+      '#{klass} JID-#{job_hash['jid']}#{' BID-#{bid}' if bid}'
+    end
+    
+          def fake?
+        self.__test_mode == :fake
+      end
+    
+      config.vm.define 'freebsd10' do |freebsd10|
+    freebsd10.vm.box = 'tjay/freebsd-10.1'
+  end
+    
+      option '--workdir', 'WORKDIR',
+    'The directory you want fpm to do its work in, where 'work' is any file ' \
+    'copying, downloading, etc. Roughly any scratch space fpm needs to build ' \
+    'your package.', :default => Dir.tmpdir
+    
+        if @attributes.include?(:prefix)
+      installdir = staging_path(@attributes[:prefix])
+    else
+      installdir = staging_path
+    end
+    
+        # npm installs dependencies in the module itself, so if you do
+    # 'npm install express' it installs dependencies (like 'connect')
+    # to: node_modules/express/node_modules/connect/...
+    #
+    # To that end, I don't think we necessarily need to include
+    # any automatic dependency information since every 'npm install'
+    # is fully self-contained. That's why you don't see any bother, yet,
+    # to include the package's dependencies in here.
+    #
+    # It's possible someone will want to decouple that in the future,
+    # but I will wait for that feature request.
+  end
+    
+        # Final format of manifest
+    safesystem('pkgfmt', manifest_fn)
+    
+        # do channel-update if requested
+    if attributes[:pear_channel_update?]
+      channel = attributes[:pear_channel] || 'pear'
+      logger.info('Updating the channel', :channel => channel)
+      safesystem('pear', '-c', config, 'channel-update', channel)
+    end
