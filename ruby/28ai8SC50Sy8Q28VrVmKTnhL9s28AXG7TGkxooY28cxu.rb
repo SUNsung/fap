3e639b1194508a292bd4dab381b6d7dd6e7ab3d5
SUNsung <weatherius@gmail.com>
2019-01-28 15:31:09 +0200

    Rewrite [28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb] File in: 2019-01-28 15:31:09

--- ruby/28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb
+++ ruby/28ai8SC50Sy8Q28VrVmKTnhL9s28AXG7TGkxooY28cxu.rb
@@ -1,76 +1,83 @@
 
         
-          def require_enabled_api!
-    head 404 unless Setting.activity_api_enabled
-  end
-end
-
-    
-      def create
-    active_session = current_session
-    
-          ohai 'Migrating tap #{old_name} to #{new_name}...' if $stdout.tty?
-    
-    module NavigationHelpers
-  def path_to(page_name)
-    case page_name
-    when /^person_photos page$/
-      person_photos_path(@me.person)
-    when /^the home(?: )?page$/
-      stream_path
-    when /^the mobile path$/
-      force_mobile_path
-    when /^the user applications page$/
-      api_openid_connect_user_applications_path
-    when /^the tag page for '([^\']*)'$/
-      tag_path(Regexp.last_match(1))
-    when /^its ([\w ]+) page$/
-      send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path', @it)
-    when /^the mobile ([\w ]+) page$/
-      public_send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path', format: 'mobile')
-    when /^the ([\w ]+) page$/
-      public_send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path')
-    when /^my edit profile page$/
-      edit_profile_path
-    when /^my profile page$/
-      person_path(@me.person)
-    when /^my acceptance form page$/
-      invite_code_path(InvitationCode.first)
-    when /^the requestors profile$/
-      person_path(Request.where(recipient_id: @me.person.id).first.sender)
-    when /^'([^\']*)''s page$/
-      p = User.find_by_email(Regexp.last_match(1)).person
-      {path:         person_path(p),
-       # '#diaspora_handle' on desktop, '.description' on mobile
-       special_elem: {selector: '#diaspora_handle, .description', text: p.diaspora_handle}
-      }
-    when /^'([^\']*)''s photos page$/
-      p = User.find_by_email(Regexp.last_match(1)).person
-      person_photos_path p
-    when /^my account settings page$/
-      edit_user_path
-    when /^forgot password page$/
-      new_user_password_path
-    when %r{^'(/.*)'}
-      Regexp.last_match(1)
-    else
-      raise 'Can't find mapping from \'#{page_name}\' to a path.'
+            def initialize
+      @pages = {}
     end
-  end
     
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
+        def effective_path
+      @effective_path ||= effective_url.path
     end
   end
 end
 
     
-        context 'with valid parameters' do
-      it 'creates a user' do
-        expect {
-          get :create, params: valid_params
-        }.to change(User, :count).by(1)
+              node['data-language'] = 'typescript' if node['path'].try(:ends_with?, '.ts')
+          node['data-language'] = 'html' if node['path'].try(:ends_with?, '.html')
+          node['data-language'] = 'css' if node['path'].try(:ends_with?, '.css')
+          node['data-language'] = 'js' if node['path'].try(:ends_with?, '.js')
+          node['data-language'] = 'json' if node['path'].try(:ends_with?, '.json')
+          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip if node['language']
+          node['data-language'] ||= 'typescript' if node.content.start_with?('@')
+    
+            css('button.verbose', 'button.verbose + .l-verbose-section', 'a[id=top]', 'a[href='#top']', '.sidebar', 'br').remove
+    
+      it 'defaults number to a random value' do
+    lambda { srand }.should_not raise_error
+    srand.should_not == 0
+  end
+    
+      url 'http://swupdl.adobe.com/updates/oobe/aam20/mac/AdobeLightroom-#{version.major}.0/#{version}/setup.dmg'
+  name 'Adobe Photoshop Lightroom'
+  homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
+    
+    Then(/^the invalid (.+) release is ignored$/) do |filename|
+  test = 'ls -g #{TestApp.releases_path} | grep #{filename}'
+  _, _, status = vagrant_cli_command('ssh -c #{test.shellescape}')
+  expect(status).to be_success
+end
+    
+    Given(/^a linked file '(.*?)'$/) do |file|
+  # ignoring other linked files
+  TestApp.append_to_deploy_file('set :linked_files, ['#{file}']')
+end
+    
+        def servers
+      @servers ||= Servers.new
+    end
+    
+          private
+    
+    
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'paths'))
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'selectors'))
+    
+    Before do
+  gemfile = ENV['BUNDLE_GEMFILE'].to_s
+  ENV['BUNDLE_GEMFILE'] = File.join(Dir.pwd, gemfile) unless gemfile.start_with?(Dir.pwd)
+  @framework_version = nil
+end
+    
+      def migration_class_name
+    migration_name.camelize
+  end
+    
+        def geometry_string
+      begin
+        orientation = Paperclip.options[:use_exif_orientation] ?
+          '%[exif:orientation]' : '1'
+        Paperclip.run(
+          Paperclip.options[:is_windows] ? 'magick identify' : 'identify',
+          '-format '%wx%h,#{orientation}' :file', {
+            :file => '#{path}[0]'
+          }, {
+            :swallow_stderr => true
+          }
+        )
+      rescue Terrapin::ExitStatusError
+        ''
+      rescue Terrapin::CommandNotFoundError => e
+        raise_because_imagemagick_missing
       end
+    end
