5eb53283951dd454c8e6dcdea5b645b41e1ef258
SUNsung <weatherius@gmail.com>
2019-01-28 06:47:51 +0200

    Rewrite [53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py] File in: 2019-01-28 06:47:51

--- pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
+++ pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
@@ -1,216 +1,230 @@
 
         
-          Returns:
-    The word embeddings IDs, paths and counts
-  '''
-  features = tf.parse_example(
-      record, {
-          'x_embedding_id': tf.FixedLenFeature([1], dtype=tf.int64),
-          'y_embedding_id': tf.FixedLenFeature([1], dtype=tf.int64),
-          'nc_embedding_id': tf.FixedLenFeature([1], dtype=tf.int64),
-          'reprs': tf.FixedLenSequenceFeature(
-              shape=(), dtype=tf.string, allow_missing=True),
-          'counts': tf.FixedLenSequenceFeature(
-              shape=(), dtype=tf.int64, allow_missing=True),
-          'rel_id': tf.FixedLenFeature([1], dtype=tf.int64)
-      })
+            assert proc.expect([TIMEOUT, u'usage'])
     
-        Args:
-      x: The input tensor from which the mean and variance are computed,
-        via a linear transformation of x.  I.e.
-          mu = Wx + b, log(var) = Mx + c
-      z_size: The size of the distribution.
-      name:  The name to prefix to learned variables.
-      var_min (optional): Minimal variance allowed.  This is an additional
-        way to control the amount of information getting through the stochastic
-        layer.
+    
+@pytest.mark.parametrize('script, output', [
+    ('apt', invalid_operation('saerch')),
+    ('apt-get', invalid_operation('isntall')),
+    ('apt-cache', invalid_operation('rumove'))])
+def test_match(script, output):
+    assert match(Command(script, output))
+    
+            Did you mean `build`?
+'''
+    
+    ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+    
+    ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+    
+    - heroku_collaborator:
+    api_key: YOUR_API_KEY
+    user: '{{ item.user }}'
+    apps: '{{ item.apps | default(apps) }}'
+    suppress_invitation: '{{ item.suppress_invitation | default(suppress_invitation) }}'
+    state: '{{ item.state | default('present') }}'
+  with_items:
+    - { user: 'a.b@example.com' }
+    - { state: 'absent', user: 'b.c@example.com', suppress_invitation: false }
+    - { user: 'x.y@example.com', apps: ['heroku-example-app'] }
+'''
+    
+            argument_spec = dict(
+            name=dict(type='str', required=True),
+            state=dict(choices=['present', 'absent', 'enabled', 'disabled', 'offline'], default='present'),
+            im_mad_name=dict(type='str', default='kvm'),
+            vmm_mad_name=dict(type='str', default='kvm'),
+            cluster_id=dict(type='int', default=0),
+            cluster_name=dict(type='str'),
+            labels=dict(type='list'),
+            template=dict(type='dict', aliases=['attributes']),
+        )
+    
+        return module.exit_json(**result)
+    
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+    
+    def main():
+    
+        def test_include_single_quotes(self):
+        self.verify_fnmatch(''' + self.config_path + ''')
+    
+        for trials in range(5):
+        a = random.randrange(2, num - 1)
+        v = pow(a, s, num)
+        if v != 1:
+            i = 0
+            while v != (num - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i = i + 1
+                    v = (v ** 2) % num
+    return True
+    
+        publicKey, privateKey = generateKey(keySize)
+    print('\nWriting public key to file %s_pubkey.txt...' % name)
+    with open('%s_pubkey.txt' % name, 'w') as fo:
+        fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
+    
+    
+class DoubleHash(HashTable):
     '''
-    size_bxn = tf.stack([tf.shape(x_bxu)[0], z_size])
-    self.mean_bxn = mean_bxn = linear(x_bxu, z_size, name=(name+'/mean'))
-    logvar_bxn = linear(x_bxu, z_size, name=(name+'/logvar'))
-    if var_min > 0.0:
-      logvar_bxn = tf.log(tf.exp(logvar_bxn) + var_min)
-    self.logvar_bxn = logvar_bxn
-    
-        # STOCHASTIC LATENT VARIABLES, priors and posteriors
-    # (initial conditions g0, and control inputs, u_t)
-    # Note that zs represent all the stochastic latent variables.
-    with tf.variable_scope('z', reuse=False):
-      self.prior_zs_g0 = None
-      self.posterior_zs_g0 = None
-      self.g0s_val = None
-      if ic_dim > 0:
-        self.prior_zs_g0 = \
-            LearnableDiagonalGaussian(batch_size, ic_dim, name='prior_g0',
-                                      mean_init=0.0,
-                                      var_min=hps.ic_prior_var_min,
-                                      var_init=hps.ic_prior_var_scale,
-                                      var_max=hps.ic_prior_var_max)
-        ic_enc = tf.concat(axis=1, values=[ic_enc_fwd[-1], ic_enc_rev[0]])
-        ic_enc = tf.nn.dropout(ic_enc, keep_prob)
-        self.posterior_zs_g0 = \
-            DiagonalGaussianFromInput(ic_enc, ic_dim, 'ic_enc_2_post_g0',
-                                      var_min=hps.ic_post_var_min)
-        if kind in ['train', 'posterior_sample_and_average',
-                    'posterior_push_mean']:
-          zs_g0 = self.posterior_zs_g0
-        else:
-          zs_g0 = self.prior_zs_g0
-        if kind in ['train', 'posterior_sample_and_average', 'prior_sample']:
-          self.g0s_val = zs_g0.sample
-        else:
-          self.g0s_val = zs_g0.mean
-    
-      # Convert from figure to an numpy array width x height x 3 (last for RGB)
-  f.canvas.draw()
-  data = np.fromstring(f.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-  data_wxhx3 = data.reshape(f.canvas.get_width_height()[::-1] + (3,))
-  plt.close()
-    
-        if FLAGS.prefix_label and use_prefix:
-      label = sequence_example.context.feature['class'].int64_list.value[0]
-      review_words = [EOS_INDEX + 1 + label]
-    else:
-      review_words = []
-    review_words.extend([
-        f.int64_list.value[0]
-        for f in sequence_example.feature_lists.feature_list['token_id'].feature
-    ])
-    all_words.append(review_words)
-  return all_words
-    
-              if FLAGS.data_set == 'ptb':
-            # Statefulness for *evaluation* Generator.
-            for i, (c, h) in enumerate(model.eval_initial_state):
-              eval_feed[c] = gen_initial_state_eval[i].c
-              eval_feed[h] = gen_initial_state_eval[i].h
-    
-        for index, pred in zip(index_batch, pred_batch):
-      indices_predictions.append([str(id_to_word[index]), pred])
-    batch_of_indices_predictions.append(indices_predictions)
-  return batch_of_indices_predictions
-    
-          else:
-        raise NotImplementedError
-    
-        @property
-    def suffixes(self):
-        '''A list of the final component's suffixes, if any.'''
-        name = self.name
-        if name.endswith('.'):
-            return []
-        name = name.lstrip('.')
-        return ['.' + suffix for suffix in name.split('.')[1:]]
-    
-        Note that not all entries are resources.  Specifically, directories are
-    not considered resources.  Use `is_resource()` on each entry returned here
-    to check if it is a resource or not.
+        Hash Table example with open addressing and Double Hash
     '''
-    package = _get_package(package)
-    reader = _get_resource_reader(package)
-    if reader is not None:
-        return reader.contents()
-    # Is the package a namespace package?  By definition, namespace packages
-    # cannot have resources.  We could use _check_location() and catch the
-    # exception, but that's extra work, so just inline the check.
-    elif package.__spec__.origin is None or not package.__spec__.has_location:
-        return ()
-    else:
-        package_directory = Path(package.__spec__.origin).parent
-        return os.listdir(package_directory)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    try:
+	xrange		#Python 2
+except NameError:
+	xrange = range	#Python 3
+    
+    	Arguments:
+		i {[int]} -- [integer]
+	'''
+    
+    # Mock test below
+if False: # change to true to run this test case.
+    import sklearn.datasets as ds
+    dataset = ds.load_iris()
+    k = 3
+    heterogeneity = []
+    initial_centroids = get_initial_centroids(dataset['data'], k, seed=0)
+    centroids, cluster_assignment = kmeans(dataset['data'], k, initial_centroids, maxiter=400,
+                                        record_heterogeneity=heterogeneity, verbose=True)
+    plot_heterogeneity(heterogeneity, k)
 
     
-    dom = xml.dom.minidom.parseString(document)
+    # frequency taken from http://en.wikipedia.org/wiki/Letter_frequency
+englishLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97,
+                     'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25,
+                     'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36,
+                     'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29,
+                     'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10,
+                     'Z': 0.07}
+ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     
-        print('-' * 20)
+        if not app.config.edit_on_github_project:
+        warnings.warn('edit_on_github_project not specified')
+        return
+    if not doctree:
+        warnings.warn('doctree is None')
+        return
+    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
+    show_url = get_github_url(app, 'blob', path)
+    edit_url = get_github_url(app, 'edit', path)
     
-    p = Point(4.0, -3.2)
+    # The name of an image file (relative to this directory) to use as a favicon of
+# the docs.
+# This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#
+html_favicon = '_static/favicon.ico'
     
-        classifiers = proj_info['classifiers'],
+        def _queued_event_check(self, click_type, time_diff):
+        '''Generate a log message and returns true if timeout exceeded.'''
+        time_string = '{:d} {}'.format(
+            time_diff, 'second' if time_diff == 1 else 'seconds')
     
-    if __name__ == '__main__':
-    main()
+    CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Required(CONF_HOST, default=DEFAULT_HOST): cv.string,
+        vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+        vol.Optional(CONF_PREFIX, default=DEFAULT_PREFIX): cv.string,
+        vol.Optional(CONF_RATE, default=DEFAULT_RATE):
+            vol.All(vol.Coerce(int), vol.Range(min=1)),
+    }),
+}, extra=vol.ALLOW_EXTRA)
+    
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_HOST): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_TYPE, default=DEFAULT_TYPE): cv.string,
+})
+    
+    DEFAULT_IP = '192.168.1.1'
+    
+        def send_url_service(service):
+        '''Service to open url on devices.'''
+        send_url(api_key=api_key, device_id=device_id,
+                 device_ids=device_ids, device_names=device_names,
+                 url=service.data.get('url'))
+    
+    REQUIREMENTS = ['pykira==0.1.1']
+    
+        def run(self):
+        '''Run the loop of the LIRC interface thread.'''
+        import lirc
+        _LOGGER.debug('LIRC interface thread started')
+        while not self.stopped.isSet():
+            try:
+                code = lirc.nextcode()  # list; empty if no buttons pressed
+            except lirc.NextCodeError:
+                _LOGGER.warning('Error reading next code from LIRC')
+                code = None
+            # interpret result from python-lirc
+            if code:
+                code = code[0]
+                _LOGGER.info('Got new LIRC code %s', code)
+                self.hass.bus.fire(
+                    EVENT_IR_COMMAND_RECEIVED, {BUTTON_NAME: code})
+            else:
+                time.sleep(0.2)
+        lirc.deinit()
+        _LOGGER.debug('LIRC interface thread stopped')
 
     
-    from .theplatform import theplatform_download_by_pid
     
-        if re.match(re_str, url):
-        html = get_content(url)
-        title = match1(html, r'<title>(.+)丨(.+)</title>')  #title
-        
-        # player loaded via internal iframe
-        frame_url = re.search(r'<iframe src=\'(.+?)\'', html).group(1)
-        logging.debug('dilidili_download: %s' % frame_url)
-        
-        #https://player.005.tv:60000/?vid=a8760f03fd:a04808d307&v=yun&sign=a68f8110cacd892bc5b094c8e5348432
-        html = get_content(frame_url, headers=headers, decoded=False).decode('utf-8')
-        
-        match = re.search(r'(.+?)var video =(.+?);', html)
-        vid = match1(html, r'var vid='(.+)'')
-        hd2 = match1(html, r'var hd2='(.+)'')
-        typ = match1(html, r'var typ='(.+)'')
-        sign = match1(html, r'var sign='(.+)'')
-        tmsign = match1(html, r'tmsign=([A-Za-z0-9]+)')
-        ulk =  match1(html, r'var ulk='(.+)'')
+def datasets():
+    '''Retrieve the list of available dataset names.'''
+    return _DATASETS.keys()
     
-    def get_orig_video_source(api_key, pid, secret):
-    parsed = json.loads(get_content_headered(tmpl_api_call_video_info % (api_key, pid, secret)))
-    for stream in parsed['streams']['stream']:
-        if stream['type'] == 'orig':
-            return stream['_content'].replace('\\', '')
-    return None
+        # Histogram of ground-truth objects
+    gt_hist = np.zeros((len(classes)), dtype=np.int)
+    for entry in roidb:
+        gt_inds = np.where(
+            (entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
+        gt_classes = entry['gt_classes'][gt_inds]
+        gt_hist += np.histogram(gt_classes, bins=hist_bins)[0]
+    logger.debug('Ground-truth class histogram:')
+    for i, v in enumerate(gt_hist):
+        logger.debug(
+            '{:d}{:s}: {:d}'.format(
+                i, classes[i].rjust(char_len), v))
+    logger.debug('-' * char_len)
+    logger.debug(
+        '{:s}: {:d}'.format(
+            'total'.rjust(char_len), np.sum(gt_hist)))
+
     
-            extractor = Funshion()
-        for ep in meta['episodes']:
-            title = '{}_{}_{}'.format(drama_name, ep['num'], ep['name'])
-            extractor.download_by_vid(ep['id'], title=title, **kwargs)
-    else:
-        log.wtf('Unknown url pattern')
     
-                if title is None:
-                response = request.urlopen(request.Request(real_url))
-                if response.headers['content-disposition']:
-                    filename = parse.unquote(r1(r'filename='?(.+)'?', response.headers['content-disposition'])).split('.')
-                    title = ''.join(filename[:-1])
-        except: pass
+def _do_matlab_eval(json_dataset, salt, output_dir='output'):
+    import subprocess
+    logger.info('-----------------------------------------------------')
+    logger.info('Computing results with the official MATLAB eval code.')
+    logger.info('-----------------------------------------------------')
+    info = voc_info(json_dataset)
+    path = os.path.join(
+        cfg.ROOT_DIR, 'detectron', 'datasets', 'VOCdevkit-matlab-wrapper')
+    cmd = 'cd {} && '.format(path)
+    cmd += '{:s} -nodisplay -nodesktop '.format(cfg.MATLAB)
+    cmd += '-r 'dbstop if error; '
+    cmd += 'voc_eval(\'{:s}\',\'{:s}\',\'{:s}\',\'{:s}\'); quit;'' \
+       .format(info['devkit_path'], 'comp4' + salt, info['image_set'],
+               output_dir)
+    logger.info('Running:\n{}'.format(cmd))
+    subprocess.call(cmd, shell=True)
     
-        # Apply some eyeliner
-    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    
-        :param X_img_path: path to image to be recognized
-    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
-    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
-    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
-           of mis-classifying an unknown person as a known one.
-    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
-        For faces of unrecognized persons, the name 'unknown' will be returned.
-    '''
-    if not os.path.isfile(X_img_path) or os.path.splitext(X_img_path)[1][1:] not in ALLOWED_EXTENSIONS:
-        raise Exception('Invalid image path: {}'.format(X_img_path))
-    
-    print('I found {} face(s) in this photograph.'.format(len(face_locations)))
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-        # Find all the faces and face enqcodings in the frame of video
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
-    
-    # Loop through each face found in the unknown image
-for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-    # See if the face is a match for the known face(s)
-    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-    
-        # 图片上传失败，输出以下html代码
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
+            If used during training, then the output blobs will also include:
+          [labels, bbox_targets, bbox_inside_weights, bbox_outside_weights].
+        '''
+        k_max = cfg.FPN.RPN_MAX_LEVEL
+        k_min = cfg.FPN.RPN_MIN_LEVEL
