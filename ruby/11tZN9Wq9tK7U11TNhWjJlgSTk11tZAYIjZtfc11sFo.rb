896a1fb04d8c7bd19fd93365c6c1bffbd52e90b7
SUNsung <weatherius@gmail.com>
2019-01-28 20:56:52 +0200

    Rewrite [11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb] File in: 2019-01-28 20:56:52

--- ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
+++ ruby/11tZN9Wq9tK7U11TNhWjJlgSTk11tZAYIjZtfc11sFo.rb
@@ -1,84 +1,140 @@
 
         
-                if Pathname::BOTTLE_EXTNAME_RX === file.to_s
-          version = bottle_resolve_version(file) rescue file.version
-        else
-          version = file.version
-        end
-        next unless version
-        next unless (name = file.basename.to_s[/(.*)-(?:#{Regexp.escape(version)})/, 1])
+                      accept = if current_value.respond_to?(:call)
+                current_value.call(item)
+              else
+                Array(current_value).map(&:to_s).include?(value.to_s)
+              end
     
-      def hardware
-    'CPU: #{Hardware.cores_as_words}-core #{Hardware::CPU.bits}-bit #{Hardware::CPU.family}'
+    require 'action_view/helpers/tags/placeholderable'
+    
+    module ActionView
+  # This class defines the interface for a renderer. Each class that
+  # subclasses +AbstractRenderer+ is used by the base +Renderer+ class to
+  # render a specific type of object.
+  #
+  # The base +Renderer+ class uses its +render+ method to delegate to the
+  # renderers. These currently consist of
+  #
+  #   PartialRenderer - Used for rendering partials
+  #   TemplateRenderer - Used for rendering other types of templates
+  #   StreamingTemplateRenderer - Used for streaming
+  #
+  # Whenever the +render+ method is called on the base +Renderer+ class, a new
+  # renderer object of the correct type is created, and the +render+ method on
+  # that new object is called in turn. This abstracts the setup and rendering
+  # into a separate classes for partials and templates.
+  class AbstractRenderer #:nodoc:
+    delegate :find_template, :find_file, :template_exists?, :any_templates?, :with_fallbacks, :with_layout_format, :formats, to: :@lookup_context
+    
+    module Vagrant
+  # This class handles guest-OS specific interactions with a machine.
+  # It is primarily responsible for detecting the proper guest OS
+  # implementation and then delegating capabilities.
+  #
+  # Vagrant has many tasks which require specific guest OS knowledge.
+  # These are implemented using a guest/capability system. Various plugins
+  # register as 'guests' which determine the underlying OS of the system.
+  # Then, 'guest capabilities' register themselves for a specific OS (one
+  # or more), and these capabilities are called.
+  #
+  # Example capabilities might be 'mount_virtualbox_shared_folder' or
+  # 'configure_networks'.
+  #
+  # This system allows for maximum flexibility and pluginability for doing
+  # guest OS specific operations.
+  class Guest
+    include CapabilityHost
+    
+            # Initializes the system. Any subclasses MUST make sure this
+        # method is called on the parent. Therefore, if a subclass overrides
+        # `initialize`, then you must call `super`.
+        def initialize(vm)
+          @vm = vm
+        end
+    
+            # Sets a human-friendly description of the plugin.
+        #
+        # @param [String] value Description of the plugin.
+        # @return [String] Description of the plugin.
+        def self.description(value=UNSET_VALUE)
+          get_or_set(:description, value)
+        end
+    
+    describe 'Kernel.srand' do
+  it 'is a private method' do
+    Kernel.should have_private_instance_method(:srand)
   end
     
-          out = checks.send(method)
-      unless out.nil? || out.empty?
-        if first_warning
-          $stderr.puts <<-EOS.undent
-            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
-            with debugging if you file an issue. If everything you use Homebrew for is
-            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
-          EOS
+      desc 'Run sass-spec tests against the local code.'
+  task :spec do
+    require 'yaml'
+    sass_spec_options = YAML.load_file(scope('test/sass-spec.yml'))
+    enabled = sass_spec_options.delete(:enabled)
+    unless enabled
+      puts 'SassSpec tests are disabled.'
+      next
+    end
+    if ruby_version_at_least?('1.9.2')
+      old_load_path = $:.dup
+      begin
+        $:.unshift(File.join(File.dirname(__FILE__), 'lib'))
+        begin
+          require 'sass_spec'
+        rescue LoadError
+          puts 'You probably forgot to run: bundle exec rake'
+          raise
         end
-    
-    module Gitlab
-  module BackgroundMigration
-    class PopulateMergeRequestsLatestMergeRequestDiffId
-      BATCH_SIZE = 1_000
-    
-            def preload_pipeline_warnings
-          # This preloads the number of warnings for every pipeline, ensuring
-          # that Ci::Pipeline#has_warnings? doesn't execute any additional
-          # queries.
-          @pipeline.number_of_warnings
-        end
-    
-              waiter.jobs_remaining += 1
-        end
-    
-    Liquid::Template.register_tag('img', Jekyll::ImageTag)
-
-    
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
-          end
-        end
+        default_options = {
+          :spec_directory => SassSpec::SPEC_DIR,
+          :engine_adapter => SassEngineAdapter.new,
+          :generate => false,
+          :tap => false,
+          :skip => false,
+          :verbose => false,
+          :filter => '',
+          :limit => -1,
+          :unexpected_pass => false,
+          :nuke => false,
+        }
+        SassSpec::Runner.new(default_options.merge(sass_spec_options)).run || exit(1)
+      ensure
+        $:.replace(old_load_path)
       end
-      rtn
+    else
+      'Skipping sass-spec on ruby versions less than 1.9.2'
     end
   end
-    
-    Given /^I run a paperclip generator to add a paperclip '([^']*)' to the '([^']*)' model$/ do |attachment_name, model_name|
-  step %[I successfully run `rails generate paperclip #{model_name} #{attachment_name}`]
 end
     
-    When /^(?:|I )follow '([^']*)'$/ do |link|
-  click_link(link)
+          opts.on('-T', '--to FORMAT',
+        'The format to convert to. Can be scss or sass.',
+        'By default, this is inferred from the output filename.',
+        'If there is none, defaults to sass.') do |name|
+        @options[:to] = name.downcase.to_sym
+        unless [:scss, :sass].include?(@options[:to])
+          raise 'Unknown format for sass-convert --to: #{name}'
+        end
+      end
+    
+    class LogStash::PluginManager::Unpack < LogStash::PluginManager::PackCommand
+  option '--tgz', :flag, 'unpack a packaged tar.gz file', :default => !LogStash::Environment.windows?
+  option '--zip', :flag, 'unpack a packaged  zip file', :default => LogStash::Environment.windows?
+    
+      it 'returns the merged `ConfigPart#config_string`' do
+    expect(subject.config_string).to eq(ordered_config_parts.collect(&:text).join('\n'))
+  end
+    
+        desc 'Bootstrap all the VM's used for this tests'
+    task :setup, :platform do |t, args|
+      config   = PlatformConfig.new
+      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
+      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
+    
+          it 'list the plugin with his version' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name} \(\d+\.\d+.\d+\)/)
+      end
+    end
+  end
 end
-    
-        def type_from_mime_magic
-      @type_from_mime_magic ||= File.open(@filepath) do |file|
-        MimeMagic.by_magic(file).try(:type)
-      end
-    end
-    
-          [ scale_geometry, crop_geometry ]
-    end
-    
-        def add_required_validations
-      options = Paperclip::Attachment.default_options.deep_merge(@options)
-      if options[:validate_media_type] != false
-        name = @name
-        @klass.validates_media_type_spoof_detection name,
-          :if => ->(instance){ instance.send(name).dirty? }
-      end
-    end
