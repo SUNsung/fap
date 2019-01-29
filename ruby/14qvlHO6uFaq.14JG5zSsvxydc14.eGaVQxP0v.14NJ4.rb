a3027aeb331cc69aa4cdc7d497a8a8c2ec27dd73
SUNsung <weatherius@gmail.com>
2019-01-29 13:24:04 +0200

    Rewrite [14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb] File in: 2019-01-29 13:24:03

--- ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
+++ ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
@@ -1,280 +1,307 @@
 
         
-                  def tag_name(multiple = false, index = nil)
-            # a little duplication to construct less strings
-            case
-            when @object_name.empty?
-              '#{sanitized_method_name}#{multiple ? '[]' : ''}'
-            when index
-              '#{@object_name}[#{index}][#{sanitized_method_name}]#{multiple ? '[]' : ''}'
-            else
-              '#{@object_name}[#{sanitized_method_name}]#{multiple ? '[]' : ''}'
-            end
-          end
+                result = block.call(parameters.first || {}) # to always pass a hash
+        self.current_lane = original_lane
     
-            private
-    
-    module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class Label < Base # :nodoc:
-        class LabelBuilder # :nodoc:
-          attr_reader :object
-    
-            layout_definition = \
-          case _layout
-          when String
-            _layout.inspect
-          when Symbol
-            <<-RUBY
-              #{_layout}.tap do |layout|
-                return #{default_behavior} if layout.nil?
-                unless layout.is_a?(String) || !layout
-                  raise ArgumentError, 'Your layout method :#{_layout} returned \#{layout}. It ' \
-                    'should have returned a String, false, or nil'
-                end
-              end
-            RUBY
-          when Proc
-            define_method :_layout_from_proc, &_layout
-            protected :_layout_from_proc
-            <<-RUBY
-              result = _layout_from_proc(#{_layout.arity == 0 ? '' : 'self'})
-              return #{default_behavior} if result.nil?
-              result
-            RUBY
-          when false
-            nil
-          when true
-            raise ArgumentError, 'Layouts must be specified as a String, Symbol, Proc, false, or nil'
-          when nil
-            name_clause
-          end
-    
-          []
+          command_return = ActionCommandReturn.new(
+        return_value: action_return,
+        return_value_type: action_class_ref.return_type,
+        closure_argument_value: closure_argument_value
+      )
+      return command_return
     end
     
-    require 'rubygems'
-require 'rake'
-require 'rdoc'
-require 'date'
-require 'yaml'
+          it 'sets the platform to tvOS' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            carthage(
+              platform: 'tvOS'
+            )
+          end').runner.execute(:test)
     
-                # stream_file_data would free us from keeping livereload.js in memory
-            # but JRuby blocks on that call and never returns
-            send_data(reload_body)
-            close_connection_after_writing
-          else
-            body = 'This port only serves livereload.js over HTTP.\n'
-            headers = [
-              'HTTP/1.1 400 Bad Request',
-              'Content-Type: text/plain',
-              'Content-Length: #{body.bytesize}',
-              '',
-              '',
-            ].join('\r\n')
-            send_data(headers)
-            send_data(body)
-            close_connection_after_writing
-          end
-        end
-        # rubocop:enable Metrics/MethodLength
+            expect(result[1]).to start_with('security set-keychain-settings')
+        expect(result[1]).to include('-t 600')
+        expect(result[1]).to include('-l')
+        expect(result[1]).to include('-u')
+        expect(result[1]).to include('~/Library/Keychains/test.keychain')
+      end
+    
+          it 'handles no extension or extensions parameters' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.')
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
+      end
+    
+          # Verify that value is the type that we're expecting, if we are expecting a type
+      if data_type == Fastlane::Boolean
+        ensure_boolean_type_passes_validation(value)
+      else
+        ensure_generic_type_passes_validation(value)
+      end
+    
+            expect(FastlaneCore::CertChecker.install_wwdr_certificate).to be(true)
       end
     end
   end
 end
 
     
-    module Jekyll
-  module Deprecator
-    extend self
+              config = FastlaneCore::Configuration.create([name, other, platform], {})
+          config.set(:name, 'name1')
+          config.set(:other, 'other')
+          config.pop_values!
     
-            lounge.topic_id = post.topic.id
-        unless lounge.save
-          puts lounge.errors.full_messages
-          puts 'Failed to set the lounge description topic!'
-        end
+    # remove (double and single) quote pairs
+# un-double-double quote resulting string
+def simulate_windows_shell_unwrapping(string)
+  regex = /^('|')(([^'])(\S*)([^']))('|')$/
+  unless string.to_s.match(regex).nil?
+    string = string.to_s.match(regex)[2] # get only part in quotes
+    string.to_s.gsub!('''', ''') # remove double double quotes
+  end
+  return string
+end
     
-        Category.transaction do
-      staff.group_names = ['staff']
-      unless staff.save
-        puts staff.errors.full_messages
-        raise 'Failed to set permissions on the Staff category!'
-      end
-    
-      # Helper for use after calling send_*_instructions methods on a resource.
-  # If we are in paranoid mode, we always act as if the resource was valid
-  # and instructions were sent.
-  def successfully_sent?(resource)
-    notice = if Devise.paranoid
-      resource.errors.clear
-      :send_paranoid_instructions
-    elsif resource.errors.empty?
-      :send_instructions
+        # See {CapabilityHost#capability}
+    def capability(*args)
+      super
+    rescue Errors::CapabilityNotFound => e
+      raise Errors::GuestCapabilityNotFound,
+        cap: e.extra_data[:cap],
+        guest: name
+    rescue Errors::CapabilityInvalid => e
+      raise Errors::GuestCapabilityInvalid,
+        cap: e.extra_data[:cap],
+        guest: name
     end
     
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
-      end
+              # Default opts to a blank optionparser if none is given
+          opts ||= OptionParser.new
     
-          # Sign in a user bypassing the warden callbacks and stores the user
-      # straight in session. This option is useful in cases the user is already
-      # signed in, but we want to refresh the credentials in session.
-      #
-      # Examples:
-      #
-      #   bypass_sign_in @user, scope: :user
-      #   bypass_sign_in @user
-      def bypass_sign_in(resource, scope: nil)
-        scope ||= Devise::Mapping.find_scope!(resource)
-        expire_data_after_sign_in!
-        warden.session_serializer.store(resource, scope)
-      end
+              # Set all of our instance variables on the new class
+          [self, other].each do |obj|
+            obj.instance_variables.each do |key|
+              # Ignore keys that start with a double underscore. This allows
+              # configuration classes to still hold around internal state
+              # that isn't propagated.
+              if !key.to_s.start_with?('@__')
+                result.instance_variable_set(key, obj.instance_variable_get(key))
+              end
+            end
+          end
     
-              providers
-        end
+            # This contains all the configuration plugins by scope.
+        #
+        # @return [Hash<Symbol, Registry>]
+        attr_reader :configs
     
-            # This method will split the argv given into three parts: the
-        # flags to this command, the subcommand, and the flags to the
-        # subcommand. For example:
+            # This returns all the registered commands.
         #
-        #     -v status -h -v
-        #
-        # The above would yield 3 parts:
-        #
-        #     ['-v']
-        #     'status'
-        #     ['-h', '-v']
-        #
-        # These parts are useful because the first is a list of arguments
-        # given to the current command, the second is a subcommand, and the
-        # third are the commands given to the subcommand.
-        #
-        # @return [Array] The three parts.
-        def split_main_and_subcommand(argv)
-          # Initialize return variables
-          main_args   = nil
-          sub_command = nil
-          sub_args    = []
-    
-            # Called after the configuration is finalized and loaded to validate
-        # this object.
-        #
-        # @param [Machine] machine Access to the machine that is being
-        #   validated.
-        # @return [Hash]
-        def validate(machine)
-          return { self.to_s => _detected_errors }
-        end
-    
-            # This returns all the config classes for the various provisioners.
-        #
-        # @return [Registry]
-        def provisioner_configs
+        # @return [Registry<Symbol, Array<Proc, Hash>>]
+        def commands
           Registry.new.tap do |result|
             @registered.each do |plugin|
-              result.merge!(plugin.components.configs[:provisioner])
+              result.merge!(plugin.components.commands)
             end
           end
         end
     
-      it 'accepts a negative seed' do
-    srand(-17)
-    srand.should == -17
+      #
+  # Payload types were identified from xCAT-server source code (IPMI.pm)
+  #
+  PAYLOAD_IPMI = 0
+  PAYLOAD_SOL  = 1
+  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
+  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
+  PAYLOAD_RAKP1 = 0x12
+  PAYLOAD_RAKP2 = 0x13
+  PAYLOAD_RAKP3 = 0x14
+  PAYLOAD_RAKP4 = 0x15
+    
+      def self.create_ipmi_getchannel_probe
+    [   # Get Channel Authentication Capabilities
+      0x06, 0x00, 0xff, 0x07, # RMCP Header
+      0x00, 0x00, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x00, 0x09, 0x20, 0x18,
+      0xc8, 0x81, 0x00, 0x38, 0x8e, 0x04, 0xb5
+    ].pack('C*')
   end
     
-      it 'does not append line-end if last character is line-end' do
-    lambda {
-      $VERBOSE = true
-      warn('this is some simple text with line-end\n')
-    }.should output(nil, 'this is some simple text with line-end\n')
-  end
+              res = decode_kerb_response(data)
     
-      url 'http://swupdl.adobe.com/updates/oobe/aam20/mac/AdobeLightroom-#{version.major}.0/#{version}/setup.dmg'
-  name 'Adobe Photoshop Lightroom'
-  homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
+              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
+          #
+          # @return [String] encoded principal
+          def encode
+            encoded = ''
+            encoded << encode_name_type
+            encoded << [components.length].pack('N')
+            encoded << encode_realm
+            encoded << encode_components
     
-        private
+    module Rex
+  module Proto
+    module Kerberos
+      module CredentialCache
+        # This class provides a representation of credential times stored in the Kerberos Credential Cache.
+        class Time < Element
+          # @!attribute auth_time
+          #   @return [Integer]
+          attr_accessor :auth_time
+          # @!attribute start_time
+          #   @return [Integer]
+          attr_accessor :start_time
+          # @!attribute end_time
+          #   @return [Integer]
+          attr_accessor :end_time
+          # @!attribute renew_till
+          #   @return [Integer]
+          attr_accessor :renew_till
     
-            safe?(env) ||
-          valid_token?(session, env['HTTP_X_CSRF_TOKEN']) ||
-          valid_token?(session, Request.new(env).params[options[:authenticity_param]]) ||
-          ( options[:allow_if] && options[:allow_if].call(env) )
+                res = ''
+            case etype
+            when RC4_HMAC
+              res = encrypt_rc4_hmac(data, key, 5)
+            else
+              raise ::NotImplementedError, 'EncryptedData schema is not supported'
+            end
+    
+              # Decodes the etype from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_etype(input)
+            input.value[0].value.to_i
+          end
+    
+              # Decodes the type from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_type(input)
+            input.value[0].value.to_i
+          end
+    
+          def is_create_page
+        true
       end
     
-          def handle(hash)
-        was = hash.dup
-        hash.replace escape(hash)
-        was
+          def sidebar
+        if @sidebar.nil?
+          if page = @page.sidebar
+            @sidebar = page.text_data
+          else
+            @sidebar = false
+          end
+        end
+        @sidebar
       end
     
-      it 'should not override the header if already set' do
-    mock_app with_headers('Content-Security-Policy' => 'default-src: none')
-    expect(get('/', {}, 'wants' => 'text/html').headers['Content-Security-Policy']).to eq('default-src: none')
+    context 'Precious::Views::Editing' do
+  include Rack::Test::Methods
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    Precious::App.set(:gollum_path, @path)
+    FileUtils.cp_r File.join(examples, 'revert.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
   end
-end
-
     
-        def render(context)
-      if @img
-        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
-      else
-        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+        assert_equal 'header', header_2.raw_data
+    assert_equal 'footer', foot_2.raw_data
+    assert_equal 'def', foot_2.version.message
+    assert_not_equal foot_1.version.sha, foot_2.version.sha
+    assert_not_equal header_1.version.sha, header_2.version.sha
+    
+    #############################################################################
+#
+# Custom tasks (add your own tasks here)
+#
+#############################################################################
+    
+        class MapGollum
+      def initialize(base_path)
+        @mg = Rack::Builder.new do
+          
+          map '/#{base_path}' do
+            run Precious::App
+          end
+          map '/' do
+            run Proc.new { [302, { 'Location' => '/#{base_path}' }, []] }
+          end
+          map '/*' do
+            run Proc.new { [302, { 'Location' => '/#{base_path}' }, []] }
+          end
+          
+        end
+      end
+    
+    class PaperclipGenerator < ActiveRecord::Generators::Base
+  desc 'Create a migration to add paperclip-specific fields to your model. ' +
+       'The NAME argument is the name of your model, and the following ' +
+       'arguments are the name of the attachments'
+    
+        def type_from_file_contents
+      type_from_mime_magic || type_from_file_command
+    rescue Errno::ENOENT => e
+      Paperclip.log('Error while determining content type: #{e}')
+      SENSIBLE_DEFAULT
+    end
+    
+    # enable logging
+FPM::Util.send :module_function, :logger
+FPM::Util.logger.level = :info
+FPM::Util.logger.subscribe STDERR
+    
+        # Short circuit for a `fpm --version` or `fpm -v` short invocation that 
+    # is the user asking us for the version of fpm.
+    if run_args == [ '-v' ] || run_args == [ '--version' ]
+      puts FPM::VERSION
+      return 0
+    end
+    
+      # write all scripts to .scripts (tar and dir)
+  def write_scripts
+    scripts_path = File.join(staging_path, '.scripts')
+    target_scripts = [:before_install, :after_install, :before_remove, :after_remove]
+    if target_scripts.any? {|name| script?(name)}
+      ::Dir.mkdir(scripts_path)
+      target_scripts.each do |name|
+        next unless script?(name)
+        out = File.join(scripts_path, name.to_s)
+        logger.debug('Writing script', :source => name, :target => out)
+        File.write(out, script(name))
+        File.chmod(0755, out)
       end
     end
   end
-end
     
-          Dir.chdir(code_path) do
-        code = file.read
-        @filetype = file.extname.sub('.','') if @filetype.nil?
-        title = @title ? '#{@title} (#{file.basename})' : file.basename
-        url = '/#{code_dir}/#{@file}'
-        source = '<figure class='code'><figcaption><span>#{title}</span> <a href='#{url}'>download</a></figcaption>\n'
-        source += '#{HighlightCode::highlight(code, @filetype)}</figure>'
-        TemplateWrapper::safe_wrap(source)
-      end
+        # magic
+    header[TAR_MAGIC_START..TAR_MAGIC_END] = 'ustar\0' + '00'
+    
+      option '--postinstall-action', 'POSTINSTALL_ACTION',
+    'Post-install action provided in package metadata. ' \
+    'Optionally one of '#{POSTINSTALL_ACTIONS.join('', '')}'.' do |value|
+    if !POSTINSTALL_ACTIONS.include?(value)
+      raise ArgumentError, 'osxpkg-postinstall-action value of '#{value}' is invalid. ' \
+        'Must be one of #{POSTINSTALL_ACTIONS.join(', ')}'
     end
+    value
   end
     
-      # Condenses multiple spaces and tabs into a single space
-  def condense_spaces(input)
-    input.gsub(/\s{2,}/, ' ')
-  end
-    
-    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
-
-    
-    # Use this to fill in an entire form with data from a table. Example:
-#
-#   When I fill in the following:
-#     | Account Number | 5002       |
-#     | Expiry date    | 2009-11-01 |
-#     | Note           | Nice guy   |
-#     | Wants Email?   |            |
-#
-# TODO: Add support for checkbox, select og option
-# based on naming conventions.
-#
-When /^(?:|I )fill in the following:$/ do |fields|
-  fields.rows_hash.each do |name, value|
-    When %{I fill in '#{name}' with '#{value}'}
-  end
-end
-    
-    Before do
-  gemfile = ENV['BUNDLE_GEMFILE'].to_s
-  ENV['BUNDLE_GEMFILE'] = File.join(Dir.pwd, gemfile) unless gemfile.start_with?(Dir.pwd)
-  @framework_version = nil
-end
-    
-      argument :attachment_names, :required => true, :type => :array, :desc => 'The names of the attachment(s) to add.',
-           :banner => 'attachment_one attachment_two attachment_three ...'
-    
-        def self.names_for(klass)
-      instance.names_for(klass)
+      option '--compression', 'COMPRESSION', 'The compression type to use, must ' \
+    'be one of #{COMPRESSION_TYPES.join(', ')}.', :default => 'xz' do |value|
+    if !COMPRESSION_TYPES.include?(value)
+      raise ArgumentError, 'Pacman compression value of '#{value}' is invalid. ' \
+        'Must be one of #{COMPRESSION_TYPES.join(', ')}'
     end
+    value
+  end
+    
+      def architecture
+    case @architecture
+    when nil, 'native'
+      @architecture = %x{uname -p}.chomp
+    end
+    # 'all' is a valid arch according to
+    # http://www.bolthole.com/solaris/makeapackage.html
