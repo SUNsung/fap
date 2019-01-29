0276f277fbd9ee012dd79a980831ef07143057eb
SUNsung <weatherius@gmail.com>
2019-01-29 07:46:29 +0200

    Rewrite [51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb] File in: 2019-01-29 07:46:29

--- ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
+++ ruby/51XE1w6QQIVIs51wNOVn3Ah69k51VT4Z5QE4hUQ513I2.rb
@@ -1,226 +1,96 @@
 
         
-            option_names.any? do |name|
-      if option_defined? 'with-#{name}'
-        include? 'with-#{name}'
-      elsif option_defined? 'without-#{name}'
-        !include? 'without-#{name}'
-      else
-        false
-      end
-    end
-  end
+                result = Fastlane::FastFile.new.parse('lane :test do
+          add_git_tag ({
+            tag: '#{tag}',
+            message: '#{message}',
+            force: true
+          })
+        end').runner.execute(:test)
     
-        first_warning = true
-    methods.each do |method|
-      unless checks.respond_to?(method)
-        Homebrew.failed = true
-        puts 'No check available by the name: #{method}'
-        next
-      end
+          it 'Does not use pattern matching for tag name if so requested' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          changelog_from_git_commits()
+        end').runner.execute(:test)
     
-        # Exclude cache, logs, and repository, if they are located under the prefix.
-    [HOMEBREW_CACHE, HOMEBREW_LOGS, HOMEBREW_REPOSITORY].each do |dir|
-      dirs.delete dir.relative_path_from(HOMEBREW_PREFIX).to_s
-    end
-    dirs.delete 'etc'
-    dirs.delete 'var'
+    # Here be monkey patches
     
-            if msg = blacklisted?(query)
-          if count > 0
-            puts
-            puts 'If you meant #{query.inspect} precisely:'
-            puts
-          end
-          puts msg
-        elsif count == 0
-          puts 'No formula found for #{query.inspect}.'
-          begin
-            GitHub.print_pull_requests_matching(query)
-          rescue GitHub::Error => e
-            SEARCH_ERROR_QUEUE << e
-          end
-        end
-      end
-    end
-    
-        updated_taps = []
-    Tap.each do |tap|
-      next unless tap.git?
-      begin
-        reporter = Reporter.new(tap)
-      rescue Reporter::ReporterRevisionUnsetError => e
-        onoe e if ARGV.homebrew_developer?
-        next
-      end
-      if reporter.updated?
-        updated_taps << tap.name
-        hub.add(reporter)
-      end
-    end
-    
-        self.store(key, value) if (stored == false)
-    self.dcase_hash[key.downcase] = value
-  end
-    
-    # Default timings
-IAX_DEFAULT_REG_REFRESH = 60
-IAX_DEFAULT_TIMEOUT     = 10
-    
-    
-  def self.create_rakp_hmac_sha1_salt(con_sid, bmc_sid, con_rid, bmc_rid, bmc_gid, auth_level, username)
-    con_sid +
-    bmc_sid +
-    con_rid +
-    bmc_rid +
-    bmc_gid +
-    [ auth_level ].pack('C') +
-    [ username.length ].pack('C') +
-    username
-  end
-    
-              res = decode_kerb_response(data)
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a KRB_AP_REQ definition, containing the Kerberos protocol version number,
-        # the message type KRB_AP_REQ, an options field to indicate any options in use, and the ticket and authenticator
-        # themselves
-        class ApReq < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of the protocol message
-          attr_accessor :msg_type
-          # @!attribute options
-          #   @return [Integer] request options, affects processing
-          attr_accessor :options
-          # @!attribute ticket
-          #   @return [Rex::Proto::Kerberos::Model::Ticket] The ticket authenticating the client to the server
-          attr_accessor :ticket
-          # @!attribute authenticator
-          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] This contains the authenticator, which includes the
-          #   client's choice of a subkey
-          attr_accessor :authenticator
-    
-                int
-          end
-    
-              # Decodes the ctime field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_ctime(input)
-            input.value[0].value
-          end
-    
-        system_command '#{staged_path}/AdobePatchInstaller.app/Contents/MacOS/AdobePatchInstaller',
-                   args: [
-                           '--mode=silent',
-                         ],
-                   sudo: true
-  end
-    
-        remove_duplicates
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
-    
-    module NavigationHelpers
-  def path_to(page_name)
-    case page_name
-    when /^person_photos page$/
-      person_photos_path(@me.person)
-    when /^the home(?: )?page$/
-      stream_path
-    when /^the mobile path$/
-      force_mobile_path
-    when /^the user applications page$/
-      api_openid_connect_user_applications_path
-    when /^the tag page for '([^\']*)'$/
-      tag_path(Regexp.last_match(1))
-    when /^its ([\w ]+) page$/
-      send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path', @it)
-    when /^the mobile ([\w ]+) page$/
-      public_send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path', format: 'mobile')
-    when /^the ([\w ]+) page$/
-      public_send('#{Regexp.last_match(1).gsub(/\W+/, '_')}_path')
-    when /^my edit profile page$/
-      edit_profile_path
-    when /^my profile page$/
-      person_path(@me.person)
-    when /^my acceptance form page$/
-      invite_code_path(InvitationCode.first)
-    when /^the requestors profile$/
-      person_path(Request.where(recipient_id: @me.person.id).first.sender)
-    when /^'([^\']*)''s page$/
-      p = User.find_by_email(Regexp.last_match(1)).person
-      {path:         person_path(p),
-       # '#diaspora_handle' on desktop, '.description' on mobile
-       special_elem: {selector: '#diaspora_handle, .description', text: p.diaspora_handle}
-      }
-    when /^'([^\']*)''s photos page$/
-      p = User.find_by_email(Regexp.last_match(1)).person
-      person_photos_path p
-    when /^my account settings page$/
-      edit_user_path
-    when /^forgot password page$/
-      new_user_password_path
-    when %r{^'(/.*)'}
-      Regexp.last_match(1)
+      if FastlaneCore::CommandExecutor.which('grep')
+    if FastlaneCore::Helper.windows?
+      compare_string = simulate_windows_shell_unwrapping(compare_string)
     else
-      raise 'Can't find mapping from \'#{page_name}\' to a path.'
+      compare_string = simulate_normal_shell_unwrapping(compare_string)
     end
+    compare_command = 'grep 'foo' #{escaped}'
+    expected_compare_error = 'grep: ' + compare_string + ': No such file or directory'
+  elsif FastlaneCore::CommandExecutor.which('find')
+    compare_string = simulate_normal_shell_unwrapping(compare_string)
+    compare_string = compare_string.upcase
+    compare_command = 'find \'foo\' #{escaped}'
+    expected_compare_error = 'File not found - ' + compare_string
   end
     
-    module Workers
-  class PublishToHub < Base
-    def perform(*_args)
-      # don't publish to pubsubhubbub in cucumber
-    end
+    Then(/^file symlinks are created in the new release$/) do
+  TestApp.linked_files.each do |file|
+    run_vagrant_command(test_symlink_exists(TestApp.current_path.join(file)))
   end
+end
     
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
+    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
+  file_shared_path = TestApp.shared_path.join(file)
+  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
+  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
+end
     
-          it 'assigns @user' do
-        get :create, params: valid_params
-        expect(assigns(:user)).to be_truthy
-      end
+    module VagrantHelpers
+  extend self
     
-        worker_count.times do |count|
-      template '/data/#{app}/shared/config/sidekiq_#{count}.yml' do
-        owner node[:owner_name]
-        group node[:owner_name]
-        mode 0644
-        source 'sidekiq.yml.erb'
-        variables({
-          :require => '/data/#{app}/current'
-        })
-      end
-    end
+          attr_reader :validators
     
-            names.inject(Object) do |constant, name|
-          constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+    %i(git_strategy hg_strategy svn_strategy).each do |strategy|
+  validate(strategy) do |key, _value|
+    warn(
+      '[Deprecation Warning] #{key} is deprecated and will be removed in '\
+      'Capistrano 3.7.0.\n'\
+      'https://github.com/capistrano/capistrano/blob/master/UPGRADING-3.7.md'
+    )
+  end
+end
+    
+              inventory_unit.transaction do
+            if inventory_unit.update_attributes(inventory_unit_params)
+              fire
+              render :show, status: 200
+            else
+              invalid_resource!(inventory_unit)
+            end
+          end
         end
-      rescue NameError
-        super
+    
+            def show
+          authorize! :admin, ReturnAuthorization
+          @return_authorization = order.return_authorizations.accessible_by(current_ability, :read).find(params[:id])
+          respond_with(@return_authorization)
+        end
+    
+              state = @states.last
+          respond_with(@states) if stale?(state)
+        end
+    
+            def destroy
+          authorize! :destroy, stock_location
+          stock_location.destroy
+          respond_with(stock_location, status: 204)
+        end
+    
+            def taxonomy_params
+          if params[:taxonomy] && !params[:taxonomy].empty?
+            params.require(:taxonomy).permit(permitted_taxonomy_attributes)
+          else
+            {}
+          end
+        end
       end
     end
   end
 end
-    
-        def self.tid
-      Thread.current['sidekiq_tid'] ||= (Thread.current.object_id ^ ::Process.pid).to_s(36)
-    end
-    
-        def erb(content, options = {})
-      if content.kind_of? Symbol
-        unless respond_to?(:'_erb_#{content}')
-          src = ERB.new(File.read('#{Web.settings.views}/#{content}.erb')).src
-          WebAction.class_eval('def _erb_#{content}\n#{src}\n end')
-        end
-      end
