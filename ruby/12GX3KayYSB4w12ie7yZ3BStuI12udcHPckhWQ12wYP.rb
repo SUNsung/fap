c4e2f8447da0178a7200a719b329bd4969b26e37
SUNsung <weatherius@gmail.com>
2019-01-29 11:11:00 +0200

    Rewrite [12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb] File in: 2019-01-29 11:11:00

--- ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
+++ ruby/12GX3KayYSB4w12ie7yZ3BStuI12udcHPckhWQ12wYP.rb
@@ -1,59 +1,91 @@
 
         
-        if pathutil_relative == native_relative
-  Benchmark.ips do |x|
-    x.report('pathutil') { pathutil_relative }
-    x.report('native')   { native_relative }
-    x.compare!
+            lambda {
+      catch :blah do
+        throw :blah, :return_value, 2, 3, 4, 5
+      end
+    }.should raise_error(ArgumentError)
   end
-else
-  print 'PATHUTIL: '
-  puts pathutil_relative
-  print 'NATIVE:   '
-  puts native_relative
+    
+      it 'calls #write on $stderr if $VERBOSE is false' do
+    lambda {
+      $VERBOSE = false
+      warn('this is some simple text')
+    }.should output(nil, 'this is some simple text\n')
+  end
+    
+      def framework_major_version
+    framework_version.split('.').first.to_i
+  end
 end
+World(RailsCommandHelpers)
 
     
-              new_theme_name = args.join('_')
-          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
-    
-        group.add(moderator)
-    group.save
-    
-    # User for the smoke tests
-if ENV['SMOKE'] == '1'
-  UserEmail.seed do |ue|
-    ue.id = 0
-    ue.email = 'smoke_user@discourse.org'
-    ue.primary = true
-    ue.user_id = 0
+      def generate_migration
+    migration_template('paperclip_migration.rb.erb',
+                       'db/migrate/#{migration_file_name}',
+                       migration_version: migration_version)
   end
     
-          it 'Converts a string value for :commits_count' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          changelog_from_git_commits(commits_count: '10')
-        end').runner.execute(:test)
+      # Provides configurability to Paperclip. The options available are:
+  # * whiny: Will raise an error if Paperclip cannot process thumbnails of
+  #   an uploaded image. Defaults to true.
+  # * log: Logs progress to the Rails log. Uses ActiveRecord's logger, so honors
+  #   log levels, etc. Defaults to true.
+  # * command_path: Defines the path at which to find the command line
+  #   programs if they are not visible to Rails the system's search path. Defaults to
+  #   nil, which uses the first executable found in the user's search path.
+  # * use_exif_orientation: Whether to inspect EXIF data to determine an
+  #   image's orientation. Defaults to true.
+  def self.options
+    @options ||= {
+      command_path: nil,
+      content_type_mappings: {},
+      log: true,
+      log_command: true,
+      read_timeout: nil,
+      swallow_stderr: true,
+      use_exif_orientation: true,
+      whiny: true,
+      is_windows: Gem.win_platform?
+    }
+  end
     
-          self.default_value
+      option '--description', 'DESCRIPTION', 'Add a description for this package.' \
+    ' You can include '\\n' sequences to indicate newline breaks.',
+    :default => 'no description'
+  option '--url', 'URI', 'Add a url for this package.',
+    :default => 'http://example.com/no-uri-given'
+  option '--inputs', 'INPUTS_PATH',
+    'The path to a file containing a newline-separated list of ' \
+    'files and dirs to use as input.'
+    
+      #######################################
+  # The following methods are provided to
+  # easily override particular substitut-
+  # ions performed by to_s for subclasses
+  #######################################
+  def to_s_arch;        architecture.to_s;  end
+  def to_s_name;        name.to_s;          end
+  def to_s_fullversion; iteration ? '#{version}-#{iteration}' : '#{version}'; end
+  def to_s_version;     version.to_s;       end
+  def to_s_iteration;   iteration.to_s;     end
+  def to_s_epoch;       epoch.to_s;         end
+  def to_s_type;        type.to_s;          end
+  def to_s_extension;   type.to_s;          end
+  #######################################
+    
+      option '--group', 'GROUP',
+    'Set the group to GROUP in the prototype file.',
+    :default => 'root'
+    
+        libs = [ 'install.sh', 'install-path.sh', 'generate-cleanup.sh' ]
+    libs.each do |file|
+      base = staging_path(File.join(attributes[:prefix]))
+      File.write(File.join(base, file), template(File.join('pleaserun', file)).result(binding))
+      File.chmod(0755, File.join(base, file))
     end
-  end
-end
-
     
-          ENV[new_initial_revision_var] ||= ENV[old_initial_revision_var]
-      ENV[new_current_revision_var] ||= ENV[old_current_revision_var]
-    
-    task :permissions do
-  sh %{chmod -R a+rx bin}
-  sh %{chmod -R a+r .}
-  require 'shellwords'
-  Dir.glob('test/**/*_test.rb') do |file|
-    next if file =~ %r{^test/haml/spec/}
-    sh %{chmod a+rx #{file}}
-  end
-end
-    
-        # variable
-    # Script::Value
-    inherited_hash_reader :var
+          safesystem(attributes[:python_bin], 'setup.py', 'install', *flags)
+    end
+  end # def install_to_staging
