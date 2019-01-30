905ed0974e20c66011dfbbe757c5480533760a86
SUNsung <weatherius@gmail.com>
2019-01-30 08:06:28 +0200

    Rewrite [50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp] File in: 2019-01-30 08:06:27

--- cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
+++ cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
@@ -1,189 +1,391 @@
 
         
-        /*!
- * \brief The result holder of storage type of each NodeEntry in the graph.
- * \note Stored under graph.attrs['storage_type'], provided by Pass 'InferStorageType'
- *
- * \code
- *  Graph g = ApplyPass(src_graph, 'InferStorageType');
- *  const StorageVector& stypes = g.GetAttr<StorageTypeVector>('storage_type');
- *  // get storage type by entry id
- *  int entry_type = stypes[g.indexed_graph().entry_id(my_entry)];
- * \endcode
- *
- * \sa FInferStorageType
- */
-using StorageTypeVector = std::vector<int>;
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    #include <mxnet/op_attr_types.h>
-#include <mxnet/graph_attr_types.h>
-#include <mxnet/c_api.h>
-#include <nnvm/symbolic.h>
-#include <nnvm/op.h>
-#include <nnvm/graph.h>
-#include <vector>
-#include <atomic>
-#include <utility>
-#include <string>
-#include <unordered_map>
-    
-     public:
-  /*! \brief cuda kernel argument descriptor */
-  struct ArgType {
-    /*! \brief whether argument is NDArray */
-    bool is_ndarray;
-    /*! \brief whether argument is constant (input) */
-    bool is_const;
-    /*! \brief data type of argument */
-    mshadow::TypeFlag dtype;
-  };
-  /*! \brief Cuda kernel */
-  class Kernel {
-   public:
-    /*! \brief Launch the kernel */
-    void Launch(const Context& ctx, const std::vector<dmlc::any>& args,
-                uint32_t grid_dim_x, uint32_t grid_dim_y, uint32_t grid_dim_z,
-                uint32_t block_dim_x, uint32_t block_dim_y, uint32_t block_dim_z,
-                uint32_t shared_mem);
-    /*! \brief kernel interface signature */
-    const std::vector<ArgType>& signature() { return signature_; }
+    // Python number methods for PyBfloat16 objects.
+PyNumberMethods PyBfloat16_AsNumber = {
+    PyBfloat16_Add,       // nb_add
+    PyBfloat16_Subtract,  // nb_subtract
+    PyBfloat16_Multiply,  // nb_multiply
+#if PY_MAJOR_VERSION < 3
+    PyBfloat16_Divide,  // nb_divide
+#endif
+    nullptr,              // nb_remainder
+    nullptr,              // nb_divmod
+    nullptr,              // nb_power
+    PyBfloat16_Negative,  // nb_negative
+    nullptr,              // nb_positive
+    nullptr,              // nb_absolute
+    nullptr,              // nb_nonzero
+    nullptr,              // nb_invert
+    nullptr,              // nb_lshift
+    nullptr,              // nb_rshift
+    nullptr,              // nb_and
+    nullptr,              // nb_xor
+    nullptr,              // nb_or
+#if PY_MAJOR_VERSION < 3
+    nullptr,  // nb_coerce
+#endif
+    PyBfloat16_Int,  // nb_int
+#if PY_MAJOR_VERSION < 3
+    PyBfloat16_Int,  // nb_long
+#else
+    nullptr,  // reserved
+#endif
+    PyBfloat16_Float,  // nb_float
+#if PY_MAJOR_VERSION < 3
+    nullptr,  // nb_oct
+    nullptr,  // nb_hex
+#endif
     }
     
-      virtual void Backward(const OpContext &ctx,
-                        const std::vector<TBlob> &out_grad,
-                        const std::vector<TBlob> &in_data,
-                        const std::vector<TBlob> &out_data,
-                        const std::vector<OpReqType> &req,
-                        const std::vector<TBlob> &in_grad,
-                        const std::vector<TBlob> &aux_args) {
-    // Set mode before backward
-    caffe::CaffeMode::SetMode<xpu>();
-    using namespace mshadow;
-    using namespace mshadow::expr;
-    CHECK_EQ(out_grad.size(), param_.num_out);
-    for (int i = 0; i < param_.num_data; ++i)
-      CHECK(req[i] != kAddTo) << 'caffe doesn't accm diff on bottom data';
-    CHECK(in_data.size() == param_.num_data);
-    }
+    // Must be included first.
+#include 'tensorflow/python/lib/core/numpy.h'
     
-      /*! \brief internal next function, inlined for fater processing. */
-  inline bool Next_(void) {
-    if (!base_->Next()) return false;
-    const DataInst &src = base_->Value();
-    this->SetOutImg(src);
-    out_.data.resize(2);
-    out_.data[0] = outimg_;
-    out_.data[1] = src.data[1];
-    out_.index = src.index;
-    out_.extra_data = src.extra_data;
-    return true;
+    Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
+                       std::function<void()> destructor, PyObject** result) {
+  int size = 1;
+  for (int i = 0; i < dim_size; ++i) {
+    size *= dims[i];
   }
-  /*!
-   * \brief Set the output image, after augmentation and normalization.
-   * \param src The source image.
-   */
-  inline void SetOutImg(const DataInst &src) {
-    using namespace mshadow::expr;  // NOLINT(*)
+  if (dtype == DT_STRING || dtype == DT_RESOURCE || size == 0) {
+    return errors::FailedPrecondition(
+        'Cannot convert strings, resources, or empty Tensors.');
+  }
     }
     
-        template <>
-    /*static*/ NDArrayViewPtr NDArrayView::RandomNormal<float16>(const NDShape& shape, double mean, double stdDev, unsigned long seed, const DeviceDescriptor& device)
-    {
-        return NDArrayView::_RandomNormal<float16, half>(shape, mean, stdDev, seed, device);
+    
+    {}  // namespace tensorflow
+
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    #include <map>
+    
+      // Maps ordinal number to a list of cached executors for that ordinal.
+  // We key off of ordinal (instead of just looking up all fields in the
+  // StreamExecutorConfig) for a slight improvement in lookup time.
+  mutex mutex_;
+  std::map<int, Entry> cache_ GUARDED_BY(mutex_);
+    
+          // colon indicates that the argument starts with a URI scheme
+      if (c == ':') {
+        // it could also be a Windows filesystem path
+        if (p == arg + 1)
+          break;
     }
     
-        template <typename T>
-    inline void ValidateType(const Dictionary& dict, const std::wstring& typeValue, size_t currentVersion)
-    {
-        if (!dict.Contains(typeKey))
-        {
-            const auto& version = GetVersion(dict);
-            LogicError('Required key '%ls' is not found in the dictionary (%s).',
-                       typeKey.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
-        } 
-    }
-    
-            // this ensures that nobody will start writing to the model/checkpoint files, until
-        // everybody is done reading them.
-        DistributedCommunicatorPtr communicator = MPICommunicator();
-        communicator->Barrier();
-    
-        //
-    // Create NDMask for the 'sequences' if the 'sequences' do not have the same length.
-    // It returns null if all the 'sequences' have the same length.
-    //
-    template <typename T>
-    static NDMaskPtr CreateMask(size_t numElementsPerSample, const std::vector<std::vector<T>>& sequences, const std::vector<bool>& sequenceStartFlags, const DeviceDescriptor& device)
-    {
-        size_t numSequences = sequences.size();
-        std::vector<size_t> sequenceLengths(numSequences);
-        for (size_t i = 0; i < numSequences; ++i)
-            sequenceLengths[i] = sequences[i].size() / numElementsPerSample;
-    }
-    
-        private:
-        bool m_isReadOnly;
-        NDShape m_sampleShape;
-        std::vector<Axis> m_sampleDynamicAxes;
-        NDShape m_unpackedShape;
-    
-    #define NOMINMAX
-#include 'Windows.h' // for HANDLE
-    
-    size_t DataReader::GetNumParallelSequencesForFixingBPTTMode()
-{
-    size_t nNbr = 0;
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-    {
-        IDataReader* ptr = m_dataReaders[m_ioNames[i]];
-        if (nNbr == 0)
-            nNbr = ptr->GetNumParallelSequencesForFixingBPTTMode();
-        else if (nNbr != ptr->GetNumParallelSequencesForFixingBPTTMode())
-            LogicError('GetNumParallelSequences: number of slices in each minibatch not consistent for these streams');
-    }
-    return nNbr;
+    void BrowserWindow::UpdateDraggableRegions(
+    content::RenderFrameHost* rfh,
+    const std::vector<DraggableRegion>& regions) {
+  if (window_->has_frame())
+    return;
+  static_cast<NativeWindowViews*>(window_.get())
+      ->UpdateDraggableRegions(DraggableRegionsToSkRegion(regions));
 }
     
-    class Clock
-{
-public:
-    static long long GetTimeStamp();
-    static long long GetTicksPerSecond();
+    namespace atom {
+    }
+    
+    #endif  // ATOM_BROWSER_API_ATOM_API_TRAY_H_
+
+    
+     protected:
+  explicit Event(v8::Isolate* isolate);
+  ~Event() override;
+    
+    AboutProtocolHandler::~AboutProtocolHandler() {}
+    
+    namespace base {
+class TaskRunner;
+}
+    
+    namespace base {
+class ListValue;
+}
+    
+    
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
+}
+    
+    EventListener::EventListener(int id,
+  const base::WeakPtr<DispatcherHost>& dispatcher_host,
+  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+    }
+    
+    
+    {
+    {    if (zoom_controller) {
+      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
+      if (zoom_factor > content::kMaximumZoomFactor) {
+        zoom_factor = content::kMaximumZoomFactor;
+      }
+      if (zoom_factor < content::kMinimumZoomFactor) {
+        zoom_factor = content::kMinimumZoomFactor;
+      }
+      x *= zoom_factor;
+      y *= zoom_factor;
+    }
+    
+    Popup(x, y, rvh);
+  } else if (method == 'EnableShowEvent') {
+    arguments.GetBoolean(0, &enable_show_event_);
+  } else {
+    NOTREACHED() << 'Invalid call to Menu method:' << method
+                 << ' arguments:' << arguments;
+  }
+}
+    
+    
+    {  *accelerator = item->accelerator_;
+  return true;
+}
+    
+    void MenuItem::SetChecked(bool checked) {
+  // Set active will cause 'activate' to be emitted, so block here
+  block_active_ = true;
+  gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_item_), checked);
+  block_active_ = false;
+}
+    
+    class NwMenuGetNSStringFWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringFWithFixupFunction() {}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringFWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringFWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringFWithFixupFunction);
 };
     
-        // out_of_range.401
-    try
+    
     {
-        // try to use a an invalid array index
-        json::const_reference ref = j.at('/array/4'_json_pointer);
-    }
-    catch (json::out_of_range& e)
     {
-        std::cout << e.what() << '\n';
+    {}  // namespace internal
+}  // namespace protobuf
+}  // namespace google
+
+    
+    
+    {
+    {
+    {
+    {bool AnnotationMatchesSubstring(const string& file_content,
+                                const GeneratedCodeInfo::Annotation* annotation,
+                                const string& expected_text) {
+  std::vector<const GeneratedCodeInfo::Annotation*> annotations;
+  annotations.push_back(annotation);
+  return AtLeastOneAnnotationMatchesSubstring(file_content, annotations,
+                                              expected_text);
+}
+}  // namespace annotation_test_util
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
+    
+    TEST(CSharpEnumValue, PascalCasedPrefixStripping) {
+  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
+  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
+  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO_BAR'));
+  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO__BAR'));
+  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'FOO_BAR_BAZ'));
+  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'Foo_BarBaz'));
+  EXPECT_EQ('Bar', GetEnumValueName('FO_O', 'FOO_BAR'));
+  EXPECT_EQ('Bar', GetEnumValueName('FOO', 'F_O_O_BAR'));
+  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
+  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
+  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO'));
+  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO___'));
+  // Identifiers can't start with digits
+  EXPECT_EQ('_2Bar', GetEnumValueName('Foo', 'FOO_2_BAR'));
+  EXPECT_EQ('_2', GetEnumValueName('Foo', 'FOO___2'));
+}
+    
+    void ImmutableExtensionLiteGenerator::Generate(io::Printer* printer) {
+  std::map<string, string> vars;
+  const bool kUseImmutableNames = true;
+  InitTemplateVars(descriptor_, scope_, kUseImmutableNames, name_resolver_,
+                   &vars);
+  printer->Print(vars,
+      'public static final int $constant_name$ = $number$;\n');
     }
     
-    #include <folly/DefaultKeepAliveExecutor.h>
+    ImmutableGeneratorFactory::ImmutableGeneratorFactory(
+    Context* context) : context_(context) {
+}
+ImmutableGeneratorFactory::~ImmutableGeneratorFactory() {}
     
-      std::shared_ptr<T> get() const {
-    folly::hazptr_local<1> hazptr;
-    auto slots = hazptr[0].get_protected(slots_);
-    if (!slots) {
-      return nullptr;
+    string OneofGenerator::DescriptorName(void) const {
+  return variables_.find('name')->second;
+}
+    
+    bool GenerateCode(const CodeGeneratorRequest& request,
+    const CodeGenerator& generator, CodeGeneratorResponse* response,
+    string* error_msg) {
+  DescriptorPool pool;
+  for (int i = 0; i < request.proto_file_size(); i++) {
+    const FileDescriptor* file = pool.BuildFile(request.proto_file(i));
+    if (file == NULL) {
+      // BuildFile() already wrote an error message.
+      return false;
     }
-    return (slots->slots_)[AccessSpreader<>::current(kNumSlots)];
   }
+    }
     
     
-    {  T* get() const {
-    return p_;
-  }
-  T* operator->() const {
-    return p_;
-  }
-  explicit operator bool() const {
-    return p_ == nullptr ? false : true;
-  }
-  bool operator==(const counted_ptr<T, Atom>& p) const {
-    return get() == p.get();
-  }
+    {  // Now replace the '.' in the input with it.
+  string result;
+  result.reserve(strlen(input) + size - 3);
+  result.append(input, radix_pos);
+  result.append(temp + 1, size - 2);
+  result.append(radix_pos + 1);
+  return result;
+}
+    
+    
+    {	return userOnly;
+}
+    
+    int main(int argc, char **argv) {
+  int port = 9090;
+  ::apache::thrift::stdcxx::shared_ptr<ExtensionManagerHandler> handler(new ExtensionManagerHandler());
+  ::apache::thrift::stdcxx::shared_ptr<TProcessor> processor(new ExtensionManagerProcessor(handler));
+  ::apache::thrift::stdcxx::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
+  ::apache::thrift::stdcxx::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
+  ::apache::thrift::stdcxx::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+    }
+    
+    using namespace ::apache::thrift;
+using namespace ::apache::thrift::protocol;
+using namespace ::apache::thrift::transport;
+using namespace ::apache::thrift::server;
+    
+    // getSerializedQueryData() return an std::pair where pair->first is a string
+// which should serialize to pair->second. pair->second should
+// deserialize to pair->first. getSerializedQueryDataWithColumnOrder
+// returns a pair where pair->second is a tree that has a repeated column and
+// the child nodes are not in alphabetical order
+std::pair<JSON, QueryData> getSerializedQueryData();
+std::pair<JSON, QueryData> getSerializedQueryDataWithColumnOrder();
+std::pair<std::string, QueryData> getSerializedQueryDataJSON();
+    
+    /**
+ * @brief Represents a hardware driver type that SMART api can you use to query
+ * device information.
+ *
+ * @param driver name of SMART controller driver
+ * @param maxID max ID number of which disks on the controller is monitored
+ */
+struct hardwareDriver {
+  std::string driver;
+  size_t maxID;
 };
+    
+    class SystemCpuTests : public testing::Test {};
+    
+    namespace osquery {
+    }
+    
+    TEST_F(ScopeGuardTests, guard_is_called) {
+  auto guard_has_been_called = false;
+  {
+    auto guard = scope_guard::create(
+        [&guard_has_been_called]() { guard_has_been_called = true; });
+    ASSERT_FALSE(guard_has_been_called);
+  }
+  ASSERT_TRUE(guard_has_been_called);
+}
+    
+      /// Use the SQL implementation to parse a query string and return details
+  /// (name, type) about the columns.
+  virtual Status getQueryColumns(const std::string& query,
+                                 TableColumns& columns) const = 0;
+    
+    #include <gtest/gtest.h>
+    
+        PrintEnv();
+    SharedState shared(this);
+    std::vector<ThreadState*> threads(num_threads_);
+    for (uint32_t i = 0; i < num_threads_; i++) {
+      threads[i] = new ThreadState(i, &shared);
+      env->StartThread(ThreadBody, threads[i]);
+    }
+    {
+      MutexLock l(shared.GetMutex());
+      while (!shared.AllInitialized()) {
+        shared.GetCondVar()->Wait();
+      }
+      // Record start time
+      uint64_t start_time = env->NowMicros();
+    }
+    
+      // Set a snapshot at start of transaction by setting set_snapshot=true
+  txn_options.set_snapshot = true;
+  txn = txn_db->BeginTransaction(write_options, txn_options);
+    
+    
+    {}  // namespace rocksdb
+
+    
+    #include 'gtest/gtest.h'
+    
+    // System gflags
+DEFINE_string(node_name, 'chassis', 'The chassis module name in proto');
+DEFINE_string(canbus_driver_name, 'canbus', 'Driver name.');
+    
+    double Spline1dSeg::ThirdOrderDerivative(const double x) const {
+  return third_order_derivative_(x);
+}
+    
+    
+    {
+    {
+    {}  // namespace gem
+}  // namespace canbus
+}  // namespace apollo
+
+    
+    TEST_F(GemMessageManagerTest, GetSendProtocols) {
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Accelcmd67::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Brakecmd6b::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Globalcmd69::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Headlightcmd76::ID) !=
+              nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Horncmd78::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Shiftcmd65::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Steeringcmd6d::ID) !=
+              nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Turncmd63::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Wipercmd90::ID) != nullptr);
+}
