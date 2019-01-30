fd2cb33794bff31e0d8c160165dcda18a8fb5b38
SUNsung <weatherius@gmail.com>
2019-01-30 20:18:13 +0200

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2019-01-30 20:18:13

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,127 +1,321 @@
 
         
-        #include <dmlc/registry.h>
-#include <dmlc/parameter.h>
-#include <map>
-#include <vector>
-#include <string>
-#include <utility>
-#include './base.h'
-#include './operator.h'
+          v8::Local<v8::Value> URLRequest(v8::Isolate* isolate);
     
-    namespace mxnet {
-namespace op {
-    }
-    }
+      int AddEntry(const base::DictionaryValue& entry);
+  void RemoveEntry(int id);
     
-    template<>
-Operator* CreateOp<cpu>(CaffeOpParam param, int dtype) {
-  Operator *op = NULL;
-  switch (dtype) {
-  case mshadow::kFloat32:
-    op = new CaffeOp<cpu, float>(param);
-    break;
-  case mshadow::kFloat64:
-    op = new CaffeOp<cpu, double>(param);
-    break;
-  case mshadow::kFloat16:
-    LOG(FATAL) << 'float16 layer is not supported by caffe';
-    break;
-  default:
-    LOG(FATAL) << 'Unsupported type ' << dtype;
-  }
-  return op;
+      // event.PreventDefault().
+  void PreventDefault(v8::Isolate* isolate);
+    
+    bool SavePageHandler::Handle(const base::FilePath& full_path,
+                             const content::SavePageType& save_type) {
+  auto* download_manager = content::BrowserContext::GetDownloadManager(
+      web_contents_->GetBrowserContext());
+  download_manager->AddObserver(this);
+  // Chromium will create a 'foo_files' directory under the directory of saving
+  // page 'foo.html' for holding other resource files of 'foo.html'.
+  base::FilePath saved_main_directory_path = full_path.DirName().Append(
+      full_path.RemoveExtension().BaseName().value() +
+      FILE_PATH_LITERAL('_files'));
+  bool result =
+      web_contents_->SavePage(full_path, saved_main_directory_path, save_type);
+  download_manager->RemoveObserver(this);
+  // If initialization fails which means fail to create |DownloadItem|, we need
+  // to delete the |SavePageHandler| instance to avoid memory-leak.
+  if (!result)
+    delete this;
+  return result;
 }
     
-    #include '../common/utils.h'
+    
+    {}  // namespace atom
+
+    
+    #ifndef ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
+#define ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
+    
+    namespace in_app_purchase {
+    }
+    
+    URLRequestAboutJob::~URLRequestAboutJob() {}
+    
+    /** @brief Fills a Blob with values @f$ x \in [0, 1] @f$
+ *         such that @f$ \forall i \sum_j x_{ij} = 1 @f$.
+ */
+template <typename Dtype>
+class PositiveUnitballFiller : public Filler<Dtype> {
+ public:
+  explicit PositiveUnitballFiller(const FillerParameter& param)
+      : Filler<Dtype>(param) {}
+  virtual void Fill(Blob<Dtype>* blob) {
+    Dtype* data = blob->mutable_cpu_data();
+    DCHECK(blob->count());
+    caffe_rng_uniform<Dtype>(blob->count(), 0, 1, blob->mutable_cpu_data());
+    // We expect the filler to not be called very frequently, so we will
+    // just use a simple implementation
+    int dim = blob->count() / blob->shape(0);
+    CHECK(dim);
+    for (int i = 0; i < blob->shape(0); ++i) {
+      Dtype sum = 0;
+      for (int j = 0; j < dim; ++j) {
+        sum += data[i * dim + j];
+      }
+      for (int j = 0; j < dim; ++j) {
+        data[i * dim + j] /= sum;
+      }
+    }
+    CHECK_EQ(this->filler_param_.sparse(), -1)
+         << 'Sparsity not supported by this Filler.';
+  }
+};
+    
+    // Serialize LayerParameter to protocol buffer
+template <typename Dtype>
+void Layer<Dtype>::ToProto(LayerParameter* param, bool write_diff) {
+  param->Clear();
+  param->CopyFrom(layer_param_);
+  param->clear_blobs();
+  for (int i = 0; i < blobs_.size(); ++i) {
+    blobs_[i]->ToProto(param->add_blobs(), write_diff);
+  }
+}
+    
+      // Adds a creator.
+  static void AddCreator(const string& type, Creator creator) {
+    CreatorRegistry& registry = Registry();
+    CHECK_EQ(registry.count(type), 0)
+        << 'Layer type ' << type << ' already registered.';
+    registry[type] = creator;
+  }
+    
+    #endif  // CAFFE_BNLL_LAYER_HPP_
+
+    
+    
+    {  bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
+  cudnnPoolingDescriptor_t  pooling_desc_;
+  cudnnPoolingMode_t        mode_;
+};
+#endif
+    
+    #ifdef USE_CUDNN
+/**
+ * @brief CuDNN acceleration of ReLULayer.
+ */
+template <typename Dtype>
+class CuDNNReLULayer : public ReLULayer<Dtype> {
+ public:
+  explicit CuDNNReLULayer(const LayerParameter& param)
+      : ReLULayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNReLULayer();
+    }
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    /*
+Method:
+Start at vertex with minimum y (pick maximum x one if there are two).  
+We aim our 'lastDir' vector at (1.0, 0)
+We look at the two rays going off from our start vertex, and follow whichever
+has the smallest angle (in -Pi -> Pi) wrt lastDir ('rightest' turn)
+    
+    #include 'b2Triangle.h'
+#include 'stdio.h'
+#include <string.h>
+#include <limits.h>
+namespace b2ConvexDecomp {
+    }
+    
+    					if (m_afrgbaSource[uiPixel].fG != 0.0f)
+					{
+						m_pimageSource->m_numColorValues.fG++;
+						if (m_afrgbaSource[uiPixel].fG - 1.0f > 0.0f)
+						{
+							m_pimageSource->m_numOutOfRangeValues.fG++;
+						}
+					}
+					if (m_afrgbaSource[uiPixel].fB != 0.0f)
+					{
+						m_pimageSource->m_numColorValues.fB++;
+						if (m_afrgbaSource[uiPixel].fB - 1.0f > 0.0f)
+						{
+							m_pimageSource->m_numOutOfRangeValues.fB++;
+						}
+					}
+					// for formats with no alpha, set source alpha to 1
+					if (imageformat == Image::Format::ETC1 ||
+						imageformat == Image::Format::RGB8 ||
+						imageformat == Image::Format::SRGB8)
+					{
+						m_afrgbaSource[uiPixel].fA = 1.0f;
+					}
+    
+    
+    {	};
+    
+    
+  typedef struct  AF_LatinBlueRec_
+  {
+    AF_WidthRec  ref;
+    AF_WidthRec  shoot;
+    FT_Pos       ascender;
+    FT_Pos       descender;
+    FT_UInt      flags;
+    }
+    
+    // Constants and Macros used by all idct/dct functions
+#define DCT_CONST_BITS 14
+#define DCT_CONST_ROUNDING  (1 << (DCT_CONST_BITS - 1))
+    
+    void ClipperOffset::OffsetPoint(int j, int& k, JoinType jointype)
+{
+  //cross product ...
+  m_sinA = (m_normals[k].X * m_normals[j].Y - m_normals[j].X * m_normals[k].Y);
+  if (std::fabs(m_sinA * m_delta) < 1.0) 
+  {
+    //dot product ...
+    double cosA = (m_normals[k].X * m_normals[j].X + m_normals[j].Y * m_normals[k].Y ); 
+    if (cosA > 0) // angle => 0 degrees
+    {
+      m_destPoly.push_back(IntPoint(Round(m_srcPoly[j].X + m_normals[k].X * m_delta),
+        Round(m_srcPoly[j].Y + m_normals[k].Y * m_delta)));
+      return; 
+    }
+    //else angle => 180 degrees   
+  }
+  else if (m_sinA > 1.0) m_sinA = 1.0;
+  else if (m_sinA < -1.0) m_sinA = -1.0;
+    }
+    
+    //--------------------------------------------------------------------------------
+//
+//    region
+//
+//--------------------------------------------------------------------------------
+RegexMatcher &RegexMatcher::region(int64_t regionStart, int64_t regionLimit, int64_t startIndex, UErrorCode &status) {
+    if (U_FAILURE(status)) {
+        return *this;
+    }
+    }
+    
+        /**
+     * System registration hook.
+     */
+    static void registerIDs();
+    
+    int32_t SearchIterator::preceding(int32_t position, 
+                                      UErrorCode &status)
+{
+    if (U_FAILURE(status)) {
+        return USEARCH_DONE;
+    }
+    setOffset(position, status);
+    return handlePrev(position, status);
+}
+    
+    UnicodeString&
+SelectFormat::toPattern(UnicodeString& appendTo) {
+    if (0 == msgPattern.countParts()) {
+        appendTo.setToBogus();
+    } else {
+        appendTo.append(msgPattern.getPatternString());
+    }
+    return appendTo;
+}
+    
+    /**
+ * A representation an acceptable range of digit counts for integers.
+ */
+class U_I18N_API IntDigitCountRange : public UMemory {
+public:
+    /**
+     * No constraints: 0 up to INT32_MAX
+     */
+    IntDigitCountRange() : fMin(0), fMax(INT32_MAX) { }
+    IntDigitCountRange(int32_t min, int32_t max);
+    int32_t pin(int32_t digitCount) const;
+    int32_t getMax() const { return fMax; }
+    int32_t getMin() const { return fMin; }
+private:
+    int32_t fMin;
+    int32_t fMax;
+};
+    
+    
+    {    setBooleanAttribute(UDAT_PARSE_ALLOW_WHITESPACE, true, status);
+    setBooleanAttribute(UDAT_PARSE_ALLOW_NUMERIC, true, status);
+    setBooleanAttribute(UDAT_PARSE_PARTIAL_LITERAL_MATCH, true, status);
+    setBooleanAttribute(UDAT_PARSE_MULTIPLE_PATTERNS_FOR_MATCH, true, status);
+}
+    
+    #include 'unicode/utypes.h'
+    
+        int32_t cursor = cursorPos;
+    
+    int main() {
+    }
+    
+    using namespace std;
     
     
     {
-    {}}
+    {                if(prev->right == NULL){
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else{
+                    prev->right = NULL;
+                    res.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
     
-    struct ObjectData;
-struct Object;
     
-    void Config::ParseConfigFile(const std::string &filename, IniSettingMap &ini,
-                             Hdf &hdf, const bool is_system /* = true */) {
-  // We don't allow a filename of just '.ini'
-  if (boost::ends_with(filename, '.ini') && filename.length() > 4) {
-    Config::ParseIniFile(filename, ini, false, is_system);
-  } else {
-    // For now, assume anything else is an hdf file
-    // TODO(#5151773): Have a non-invasive warning if HDF file does not end
-    // .hdf
-    Config::ParseHdfFile(filename, hdf);
-  }
+    {
+    {
+    {            if(command.s == 'print')
+                res.push_back(command.node->val);
+            else{
+                assert(command.s == 'go');
+                if(command.node->right)
+                    stack.push(Command('go',command.node->right));
+                if(command.node->left)
+                    stack.push(Command('go',command.node->left));
+                stack.push(Command('print', command.node));
+            }
+        }
+        return res;
+    }
+};
+    
+    int main() {
+    }
+    
+    
+void print_vec(const vector<int>& vec){
+    for(int e: vec)
+        cout << e << ' ';
+    cout << endl;
 }
     
-      // Parse and process a .hdf string (e.g., -v)
-  static void ParseHdfString(const std::string &hdfStr, Hdf &hdf);
+    /// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
     
-      static std::string FormatNumber(const char* fmt, int64_t n);
-  static std::string FormatSize(int64_t size);
-  static std::string FormatTime(int64_t milliSeconds);
-    
-    String ArrayDirectory::path() {
-  if (!m_it) {
-    return empty_string();
-  }
+            stack.push(root);
+        while(!stack.empty()){
     }
-    
-    #ifndef HPHP_FILE_STREAM_WRAPPER_H
-#define HPHP_FILE_STREAM_WRAPPER_H
-    
-    void record_perf_mem_event(PerfEvent kind, const perf_event_sample* sample);
-    
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-    
-    using apollo::drivers::canbus::Byte;
-    
-    namespace apollo {
-namespace localization {
-namespace msf {
-    }
-    }
-    }
-    
-      auto geo_reference_node = header_node->FirstChildElement('geoReference');
-  if (!geo_reference_node) {
-    std::string err_msg = 'Error parsing header geoReoference attributes';
-    return Status(apollo::common::ErrorCode::HDMAP_DATA_ERROR, err_msg);
-  }
-  auto geo_text = geo_reference_node->FirstChild()->ToText();
-  if (!geo_text) {
-    std::string err_msg = 'Error parsing header geoReoference text';
-    return Status(apollo::common::ErrorCode::HDMAP_DATA_ERROR, err_msg);
-  }
-    
-    #include 'modules/canbus/vehicle/gem/protocol/accel_cmd_67.h'
-#include 'modules/canbus/vehicle/gem/protocol/accel_rpt_68.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_cmd_6b.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_1_70.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_2_71.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_3_72.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_rpt_6c.h'
-#include 'modules/canbus/vehicle/gem/protocol/date_time_rpt_83.h'
-#include 'modules/canbus/vehicle/gem/protocol/global_cmd_69.h'
-#include 'modules/canbus/vehicle/gem/protocol/global_rpt_6a.h'
-#include 'modules/canbus/vehicle/gem/protocol/headlight_cmd_76.h'
-#include 'modules/canbus/vehicle/gem/protocol/headlight_rpt_77.h'
-#include 'modules/canbus/vehicle/gem/protocol/horn_cmd_78.h'
-#include 'modules/canbus/vehicle/gem/protocol/horn_rpt_79.h'
-#include 'modules/canbus/vehicle/gem/protocol/lat_lon_heading_rpt_82.h'
-#include 'modules/canbus/vehicle/gem/protocol/parking_brake_status_rpt_80.h'
-#include 'modules/canbus/vehicle/gem/protocol/shift_cmd_65.h'
-#include 'modules/canbus/vehicle/gem/protocol/shift_rpt_66.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_cmd_6d.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_1_73.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_2_74.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_3_75.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_rpt_1_6e.h'
-#include 'modules/canbus/vehicle/gem/protocol/turn_cmd_63.h'
-#include 'modules/canbus/vehicle/gem/protocol/turn_rpt_64.h'
-#include 'modules/canbus/vehicle/gem/protocol/vehicle_speed_rpt_6f.h'
-#include 'modules/canbus/vehicle/gem/protocol/wheel_speed_rpt_7a.h'
-#include 'modules/canbus/vehicle/gem/protocol/wiper_cmd_90.h'
-#include 'modules/canbus/vehicle/gem/protocol/wiper_rpt_91.h'
-#include 'modules/canbus/vehicle/gem/protocol/yaw_rate_rpt_81.h'
