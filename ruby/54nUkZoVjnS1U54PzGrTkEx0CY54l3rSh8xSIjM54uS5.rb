c8b3fd626caf581ab3af138ad7cf0ea86425e56f
SUNsung <weatherius@gmail.com>
2019-01-30 17:31:30 +0200

    Rewrite [54nUkZoVjnS1U54PzGrTkEx0CY54l3rSh8xSIjM54uS5.rb] File in: 2019-01-30 17:31:29

--- ruby/54nUkZoVjnS1U54PzGrTkEx0CY54l3rSh8xSIjM54uS5.rb
+++ ruby/54nUkZoVjnS1U54PzGrTkEx0CY54l3rSh8xSIjM54uS5.rb
@@ -1,46 +1,98 @@
 
         
-          describe '.all' do
-    it 'uses the client to fetch all keys' do
-      mock_client_response(:list_keys, with: no_args) do
-        [
-          {
-            canDownload: false,
-            canRevoke: true,
-            keyId: 'some-key-id',
-            keyName: 'Test Key via fastlane',
-            servicesCount: 2
-          },
-          {
-            canDownload: true,
-            canRevoke: true,
-            keyId: 'B92NE4F7RG',
-            keyName: 'Test Key via browser',
-            servicesCount: 2
-          }
-        ]
+                # Executes a command and returns true if the command succeeded,
+        # and false otherwise. By default, this executes as a normal user,
+        # and it is up to the communicator implementation if they expose an
+        # option for running tests as an administrator.
+        #
+        # @see #execute
+        def test(command, opts=nil)
+        end
       end
-    
-          def initialize(gradle_path: nil)
-        self.gradle_path = gradle_path
-      end
-    
-            expect(result).to eq('/usr/local/bin/cloc  --by-file --xml  --out=/tmp/cloc.xml')
-      end
-    
-            expect(result).to eq('hg parent --template '{node|short}'')
-        expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER_REPOSITORY]).to eq('hg parent --template '{node|short}'')
-      end
-    
-    unless dups.empty?
-  puts '\nDuplicate links:'
-  dups.each do |link|
-    puts '- #{link}'
-    puts `grep -nr '#{link}' README.md`
+    end
   end
-  puts '\nDone with errors.'
-  exit(1)
 end
+
     
-      def gist_logs
-    raise FormulaUnspecifiedError if ARGV.resolved_formulae.length != 1
+            # This method is automatically called when the system is available (when
+        # Vagrant can successfully SSH into the machine) to give the system a chance
+        # to determine the distro and return a distro-specific system.
+        #
+        # If this method returns nil, then this instance is assumed to be
+        # the most specific guest implementation.
+        def distro_dispatch
+        end
+    
+              @registered.each do |plugin|
+            plugin.config.each do |key, klass|
+              result[key] = klass
+            end
+          end
+    
+            # Defines additional command line commands available by key. The key
+        # becomes the subcommand, so if you register a command 'foo' then
+        # 'vagrant foo' becomes available.
+        #
+        # @param [String] name Subcommand key.
+        def self.command(name=UNSET_VALUE, &block)
+          data[:command] ||= Registry.new
+    
+                  # Break so we don't find the next non flag and shift our
+              # main args.
+              break
+            end
+          end
+    
+            # This returns all the registered communicators.
+        #
+        # @return [Hash]
+        def communicators
+          Registry.new.tap do |result|
+            @registered.each do |plugin|
+              result.merge!(plugin.communicator)
+            end
+          end
+        end
+    
+            -> () {
+          format('%s', obj)
+        }.should raise_error(NoMethodError)
+      end
+    end
+    
+      it 'accepts a Float' do
+    sleep(0.1).should be_close(0, 2)
+  end
+    
+      it 'calls #to_int on seed' do
+    srand(3.8)
+    srand.should == 3
+    
+      it 'calls #to_path on second argument when passed ?e and a filename' do
+    p = mock('path')
+    p.should_receive(:to_path).and_return @file
+    Kernel.test(?e, p)
+  end
+    
+      it 'transfers control to the innermost catch block waiting for the same sympol' do
+    one = two = three = 0
+    catch :duplicate do
+      catch :duplicate do
+        catch :duplicate do
+          one = 1
+          throw :duplicate
+        end
+        two = 2
+        throw :duplicate
+      end
+      three = 3
+      throw :duplicate
+    end
+    [one, two, three].should == [1, 2, 3]
+  end
+    
+            -> { w.f4('foo', 0) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.warn_call_lineno}: warning: foo|)
+        -> { w.f4('foo', 1) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.f1_call_lineno}: warning: foo|)
+        -> { w.f4('foo', 2) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.f2_call_lineno}: warning: foo|)
+        -> { w.f4('foo', 3) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.f3_call_lineno}: warning: foo|)
+      end
