73beb26f8dbdb317657f1ed56d8f6a048cd3a031
SUNsung <weatherius@gmail.com>
2019-01-30 14:05:43 +0200

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2019-01-30 14:05:42

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,20 +1,140 @@
 
         
-            # Controls whether an action should be rendered using a layout.
-    # If you want to disable any <tt>layout</tt> settings for the
-    # current action so that it is rendered without a layout then
-    # either override this method in your controller to return false
-    # for that action or set the <tt>action_has_layout</tt> attribute
-    # to false before rendering.
-    def action_has_layout?
-      @_action_has_layout
-    end
-    
-        # Main render entry point shared by Action View and Action Controller.
-    def render(context, options)
-      if options.key?(:partial)
-        render_partial(context, options)
-      else
-        render_template(context, options)
+                  relation.update_all(update)
+        end
       end
     end
+  end
+end
+
+    
+          def cache_key_iid
+        if object.respond_to?(:noteable_id)
+          object.noteable_id
+        elsif object.respond_to?(:iid)
+          object.iid
+        else
+          raise(
+            TypeError,
+            'Instances of #{object.class} are not supported'
+          )
+        end
+      end
+    end
+  end
+end
+
+    
+            # Builds a PR from a GitHub API response.
+        #
+        # issue - An instance of `Sawyer::Resource` containing the PR details.
+        def self.from_api_response(pr)
+          assignee =
+            if pr.assignee
+              Representation::User.from_api_response(pr.assignee)
+            end
+    
+        pod 'ObjCPod', path: 'ObjCPod'
+    pod 'SwiftPod', path: 'SwiftPod'
+    pod 'MixedPod', path: 'MixedPod'
+    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
+    
+            self.description = <<-DESC
+          Shows the content of the pods cache as a YAML tree output, organized by pod.
+          If `NAME` is given, only the caches for that pod will be included in the output.
+        DESC
+    
+      def execute
+    signal_deprecation_warning_for_pack
+    
+          it 'returns true if the pipeline is a system pipeline' do
+        expect(subject.system?).to be_truthy
+      end
+    end
+    
+          it 'list the plugins with their versions' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose')
+        result.stdout.split('\n').each do |plugin|
+          expect(plugin).to match(/^logstash-\w+-\w+\s\(\d+\.\d+.\d+(.\w+)?\)/)
+        end
+      end
+    end
+    
+              context 'and a comment after the last element' do
+            let(:b_comment) { ' # comment b' }
+    
+          # Returns an array of all the when branches in the `case` statement.
+      #
+      # @return [Array<WhenNode>] an array of `when` nodes
+      def when_branches
+        node_parts[1...-1]
+      end
+    
+          # Checks whether any of the key value pairs in the `hash` literal are on
+      # the same line.
+      #
+      # @note A multiline `pair` is considered to be on the same line if it
+      #       shares any of its lines with another `pair`
+      #
+      # @return [Boolean] whether any `pair` nodes are on the same line
+      def pairs_on_same_line?
+        pairs.each_cons(2).any? { |first, second| first.same_line?(second) }
+      end
+    
+    module RuboCop
+  module AST
+    # A node extension for `kwsplat` nodes. This will be used in place of a
+    # plain  node when the builder constructs the AST, making its methods
+    # available to all `kwsplat` nodes within RuboCop.
+    class KeywordSplatNode < Node
+      include HashElementNode
+    
+          # Checks whether this `hash` element is on the same line as `other`.
+      #
+      # @note A multiline element is considered to be on the same line if it
+      #       shares any of its lines with `other`
+      #
+      # @return [Boolean] whether this element is on the same line as `other`
+      def same_line?(other)
+        loc.last_line == other.loc.line || loc.line == other.loc.last_line
+      end
+    
+    module RuboCop
+  module AST
+    # Common functionality for nodes that are parameterized:
+    # `send`, `super`, `zsuper`, `def`, `defs`
+    module ParameterizedNode
+      # Checks whether this node's arguments are wrapped in parentheses.
+      #
+      # @return [Boolean] whether this node's arguments are
+      #                   wrapped in parentheses
+      def parenthesized?
+        loc.end && loc.end.is?(')')
+      end
+    
+        # data tar.
+    tar_path(staging_path(''), datatar_path)
+    
+      def to_s(format=nil)
+    return super(format.nil? ? 'NAME-FULLVERSION.EXTENSION' : format)
+  end # def to_s
+end # class FPM::Package::FreeBSD
+
+    
+      option '--channel-update', :flag,
+    'call 'pear channel-update' prior to installation'
+    
+        # Add the tar compression flag if necessary
+    tar_compression_flag(input_path).tap do |flag|
+      args << flag unless flag.nil?
+    end
+    
+      # Output a zipfile.
+  def output(output_path)
+    output_check(output_path)
+    realpath = Pathname.new(output_path).realdirpath.to_s
+    ::Dir.chdir(staging_path) do
+      safesystem('zip', '-9r', realpath, '.')
+    end
+  end # def output
+end # class FPM::Package::Tar
