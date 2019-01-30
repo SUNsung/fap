d0086f7ca3fd382435636afa865fb7c9e6913088
SUNsung <weatherius@gmail.com>
2019-01-30 02:10:31 +0200

    Rewrite [31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb] File in: 2019-01-30 02:10:30

--- ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
+++ ruby/31iqlmGuGfzFQ31Xt1pW7KtG6g31M6NlOpRTtek31yHw.rb
@@ -1,136 +1,68 @@
 
         
-                private
+        module Vagrant
+  # This class handles guest-OS specific interactions with a machine.
+  # It is primarily responsible for detecting the proper guest OS
+  # implementation and then delegating capabilities.
+  #
+  # Vagrant has many tasks which require specific guest OS knowledge.
+  # These are implemented using a guest/capability system. Various plugins
+  # register as 'guests' which determine the underlying OS of the system.
+  # Then, 'guest capabilities' register themselves for a specific OS (one
+  # or more), and these capabilities are called.
+  #
+  # Example capabilities might be 'mount_virtualbox_shared_folder' or
+  # 'configure_networks'.
+  #
+  # This system allows for maximum flexibility and pluginability for doing
+  # guest OS specific operations.
+  class Guest
+    include CapabilityHost
     
-          []
+            # Upload a file to the remote machine.
+        #
+        # @param [String] from Path of the file locally to upload.
+        # @param [String] to Path of where to save the file on the remote
+        #   machine.
+        def upload(from, to)
+        end
+    
+        def status_params
+      params.require(:status).permit(:sensitive)
     end
     
-          TopicUser.change(@target.id, @topic.id, notification_level: TopicUser.notification_levels[:watching])
-    
-    describe ApplicationHelper do
-  describe '#icon_tag' do
-    it 'returns a Glyphicon icon element' do
-      icon = icon_tag('glyphicon-help')
-      expect(icon).to be_html_safe
-      expect(Nokogiri(icon).at('span.glyphicon.glyphicon-help')).to be_a Nokogiri::XML::Element
-    end
-    
-        it 'works for queued jobs' do
-      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
-    end
-  end
-    
-            context 'when the schema_version is less than 1' do
-          before do
-            valid_parsed_weather_agent_data[:keep_events_for] = 2
-            valid_parsed_data.delete(:schema_version)
-          end
-    
-      describe 'cleanup_failed_jobs!' do
-    before do
-      3.times do |i|
-        Delayed::Job.create(failed_at: Time.now - i.minutes)
-      end
-      @keep = Delayed::Job.order(:failed_at)[1]
-    end
-    
-        it 'always succeeds in sorting even if it finds pairs of incomparable objects' do
-      time = Time.now
-      tuples = [
-        [2,   'a', time - 1],  # 0
-        [1,   'b', nil],       # 1
-        [1,   'b', time],      # 2
-        ['2', nil, time],      # 3
-        [1,   nil, time],      # 4
-        [nil, 'a', time + 1],  # 5
-        [2,   'a', time],      # 6
-      ]
-      orders = [true, false, true]
-      expected = tuples.values_at(3, 6, 0, 4, 2, 1, 5)
-    
-        self.each_key { |k|
-      if (k.downcase == key.downcase)
-        self.store(k, value)
-        stored = true
-      end
-    }
-    
-              # Encodes the name_type field
-          #
-          # @return [String]
-          def encode_name_type
-            [name_type].pack('N')
-          end
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Crypto
-        module Rc4Hmac
-          # Decrypts the cipher using RC4-HMAC schema
-          #
-          # @param cipher [String] the data to decrypt
-          # @param key [String] the key to decrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the decrypted cipher
-          # @raise [RuntimeError] if decryption doesn't succeed
-          def decrypt_rc4_hmac(cipher, key, msg_type)
-            unless cipher && cipher.length > 16
-              raise ::RuntimeError, 'RC4-HMAC decryption failed'
-            end
-    
-              # Encodes the pvno field
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_pvno
-            bn = OpenSSL::BN.new(pvno.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-              # Rex::Proto::Kerberos::Model::Checksum decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'Checksum decoding not supported'
-          end
-    
-              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_pvno(input)
-            input.value[0].value.to_i
-          end
-    
-              private
-    
-      def render(context)
-    config_tag(context.registers[:site].config, @key, @tag, @classname)
+      def setting
+    @_setting ||= ::Web::Setting.where(user: current_user).first_or_initialize(user: current_user)
   end
 end
+
     
-            Dir.chdir(includes_dir) do
-          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
-          if choices.include?(file)
-            source = File.read(file)
-            partial = Liquid::Template.parse(source)
-            context.stack do
-              rtn = rtn + partial.render(context)
-            end
-          else
-            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
-          end
-        end
+          # Returns the path to a file for the given key.
+      #
+      # @param key [String]
+      # @return [String] The path to the cache file.
+      def path_to(key)
+        key = key.gsub(/[<>:\\|?*%]/) {|c| '%%%03d' % c.ord}
+        File.join(cache_location, key)
       end
-      rtn
     end
   end
+end
+
+    
+          opts.on('-T', '--to FORMAT',
+        'The format to convert to. Can be scss or sass.',
+        'By default, this is inferred from the output filename.',
+        'If there is none, defaults to sass.') do |name|
+        @options[:to] = name.downcase.to_sym
+        unless [:scss, :sass].include?(@options[:to])
+          raise 'Unknown format for sass-convert --to: #{name}'
+        end
+      end
+    
+    module Sass
+  module Importers
+    # The default importer, used for any strings found in the load path.
+    # Simply loads Sass files from the filesystem using the default logic.
+    class Filesystem < Base
+      attr_accessor :root
