1ea6c7bb6bfc6de159438cd778a3ed4548e42c3d
SUNsung <weatherius@gmail.com>
2019-01-30 20:53:07 +0200

    Rewrite [109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb] File in: 2019-01-30 20:53:07

--- ruby/109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb
+++ ruby/109OkO.vDEUVM10qDbGJ.0KGfM10Og0nTeBOEw610tnm.rb
@@ -1,131 +1,98 @@
 
         
-        DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
+                  expect(Scenario.where(guid: guid).count).to eq(2)
     
+    desc 'generate gemspec'
+task 'rack-protection.gemspec' do
+  require 'rack/protection/version'
+  content = File.binread 'rack-protection.gemspec'
     
-def pathutil_relative
-  Pathutil.new(DOC_PATH).relative_path_from(COL_PATH).to_s
-end
+          def default_options
+        DEFAULT_OPTIONS
+      end
     
-        To install Clojure you should install Leiningen:
-      brew install leiningen
-    and then follow the tutorial:
-      https://github.com/technomancy/leiningen/blob/stable/doc/TUTORIAL.md
-    EOS
-  when 'osmium' then <<-EOS.undent
-    The creator of Osmium requests that it not be packaged and that people
-    use the GitHub master branch instead.
-    EOS
-  when 'gfortran' then <<-EOS.undent
-    GNU Fortran is now provided as part of GCC, and can be installed with:
-      brew install gcc
-    EOS
-  when 'play' then <<-EOS.undent
-    Play 2.3 replaces the play command with activator:
-      brew install typesafe-activator
-    
-        if ARGV.ignore_deps?
-      @deps = []
-      @reqs = []
-    else
-      @deps = expand_deps
-      @reqs = expand_reqs
-    end
-  end
-    
-      # @private
-  def unused_options
-    @options - @args
-  end
-    
-        root.children.sort.each do |pn|
-      if pn.directory?
-        dirs << pn
-      elsif block_given? && yield(pn)
-        puts pn
-        other = 'other '
-      else
-        remaining_root_files << pn unless pn.basename.to_s == '.DS_Store'
+          def call(env)
+        status, headers, body = @app.call(env)
+        header = options[:report_only] ? 'Content-Security-Policy-Report-Only' : 'Content-Security-Policy'
+        headers[header] ||= csp_policy if html? headers
+        [status, headers, body]
       end
     end
+  end
+end
+
     
-              subtree, file = File.split(object['path'])
+    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
+Gem.use_paths(LogStash::Environment.logstash_gem_home)
     
-              # If we asked for reversed ordering, then reverse it
-          vms.reverse! if options[:reverse]
+        class << self
+      def elastic_pack_base_uri
+        env_url = ENV['LOGSTASH_PACK_URL']
+        (env_url.nil? || env_url.empty?) ? DEFAULT_PACK_URL : env_url
+      end
     
-            if rest.empty?
-          current_rule.children += child.children
-        else
-          rest.unshift Sass::Selector::Parent.new
-          child.parsed_rules = make_sseq(sseq.subject?, *rest)
-          current_rule << child
+          # Try to add the gems to the current gemfile and lock file, if successful
+      # both of them will be updated. This injector is similar to Bundler's own injector class
+      # minus the support for additionals source and doing local resolution only.
+      ::Bundler::LogstashInjector.inject!(pack)
+    
+      def validate_cache_location
+    cache_location = LogStash::Environment::CACHE_PATH
+    if File.exist?(cache_location)
+      puts('Directory #{cache_location} is going to be overwritten, do you want to continue? (Y/N)')
+      override = ( 'y' == STDIN.gets.strip.downcase ? true : false)
+      if override
+        FileUtils.rm_rf(cache_location)
+      else
+        puts('Unpack cancelled: file #{cache_location} already exists, please delete or move it')
+        exit
+      end
+    end
+  end
+end
+
+    
+      def execute
+    # Turn off any jar dependencies lookup when running with `--local`
+    ENV['JARS_SKIP'] = 'true' if local?
+    
+      it 'returns the sorted config parts' do
+    expect(subject.config_parts).to eq(ordered_config_parts)
+  end
+    
+    desc 'Watch the site and regenerate when it changes'
+task :watch do
+  raise '### You haven't set anything up yet. First run `rake install` to set up an Octopress theme.' unless File.directory?(source_dir)
+  puts 'Starting to watch source with Jekyll and Compass.'
+  system 'compass compile --css-dir #{source_dir}/stylesheets' unless File.exist?('#{source_dir}/stylesheets/screen.css')
+  jekyllPid = Process.spawn({'OCTOPRESS_ENV'=>'preview'}, 'jekyll build --watch')
+  compassPid = Process.spawn('compass watch')
+    
+        def render(context)
+      quote = paragraphize(super)
+      author = '<strong>#{@by.strip}</strong>' if @by
+      if @source
+        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
+        parts = []
+        url.each do |part|
+          if (parts + [part]).join('/').length < 32
+            parts << part
+          end
         end
-    
-        # function
-    # Sass::Callable
-    inherited_hash_reader :function
-    
-        context 'and a comment after the last element' do
-      let(:b_comment) { ' # comment b' }
-    
-          # Returns the value of this `hash` element.
-      #
-      # @note For keyword splats, this returns the whole node
-      #
-      # @return [Node] the value of the hash element
-      def value
-        node_parts[1]
+        source = parts.join('/')
+        source << '/&hellip;' unless source == @source
       end
-    
-    When /^(?:|I )fill in '([^']*)' with '([^']*)'$/ do |field, value|
-  fill_in(field, :with => value)
-end
-    
-      def generate_migration
-    migration_template('paperclip_migration.rb.erb',
-                       'db/migrate/#{migration_file_name}',
-                       migration_version: migration_version)
-  end
-    
-        # Returns the scaling and cropping geometries (in string-based ImageMagick format)
-    # neccessary to transform this Geometry into the Geometry given. If crop is true,
-    # then it is assumed the destination Geometry will be the exact final resolution.
-    # In this case, the source Geometry is scaled so that an image containing the
-    # destination Geometry would be completely filled by the source image, and any
-    # overhanging image would be cropped. Useful for square thumbnail images. The cropping
-    # is weighted at the center of the Geometry.
-    def transformation_to dst, crop = false
-      if crop
-        ratio = Geometry.new( dst.width / self.width, dst.height / self.height )
-        scale_geometry, scale = scaling(dst, ratio)
-        crop_geometry         = cropping(dst, ratio, scale)
+      if !@source.nil?
+        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
+      elsif !@title.nil?
+        cite = ' <cite>#{@title}</cite>'
+      end
+      blockquote = if @by.nil?
+        quote
+      elsif cite
+        '#{quote}<footer>#{author + cite}</footer>'
       else
-        scale_geometry        = dst.to_s
+        '#{quote}<footer>#{author}</footer>'
       end
-    
-        def geometry_string
-      begin
-        orientation = Paperclip.options[:use_exif_orientation] ?
-          '%[exif:orientation]' : '1'
-        Paperclip.run(
-          Paperclip.options[:is_windows] ? 'magick identify' : 'identify',
-          '-format '%wx%h,#{orientation}' :file', {
-            :file => '#{path}[0]'
-          }, {
-            :swallow_stderr => true
-          }
-        )
-      rescue Terrapin::ExitStatusError
-        ''
-      rescue Terrapin::CommandNotFoundError => e
-        raise_because_imagemagick_missing
-      end
-    end
-    
-        # Hash assignment of interpolations. Included only for compatibility,
-    # and is not intended for normal use.
-    def self.[]= name, block
-      define_method(name, &block)
-      @interpolators_cache = nil
+      '<blockquote>#{blockquote}</blockquote>'
     end
