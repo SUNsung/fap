5579124ecf64b227106188ea54868cfa28ccbab4
SUNsung <weatherius@gmail.com>
2019-01-30 07:32:34 +0200

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2019-01-30 07:32:33

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,59 +1,61 @@
 
         
-            it 'returns a label 'No' if a given agent is not working' do
-      stub(@agent).working? { false }
-      label = working(@agent)
-      expect(label).to be_html_safe
-      expect(Nokogiri(label).text).to eq 'No'
-    end
-  end
-    
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
-    end
-    
-        it 'unregisters disabled SchedulerAgents' do
-      @scheduler.schedule_scheduler_agents
-    
-            return a.casecmp(b) if a_length == 1 && b_length == 1
-        return 1 if a_length == 1
-        return -1 if b_length == 1
-    
-        def effective_path
-      @effective_path ||= effective_url.path
-    end
-  end
-end
-
-    
-            if mod
-          if name == 'Index'
-            return slug.split('/')[1..-2].join('/')
-          elsif name == 'Angular'
-            return slug.split('/').last.split('-').first
-          end
-        end
-    
-            css('ul.methods', 'ul.properties', 'ul.events').add_class('defs').each do |node|
-          node.css('> li > h3').each do |h3|
-            next if h3.content.present?
-            h3.content = h3.next_element.content
-            h3.next_element.remove
-          end
-        end
+              it 'allows to place name in any position' do
+        format('%+15.5<foo>f', foo: 10.952).should == '      +10.95200'
+        format('%+15<foo>.5f', foo: 10.952).should == '      +10.95200'
+        format('%+<foo>15.5f', foo: 10.952).should == '      +10.95200'
+        format('%<foo>+15.5f', foo: 10.952).should == '      +10.95200'
       end
+    
+      it 'returns true when passed ?R if the argument is readable by the real uid' do
+    Kernel.test(?R, @file).should be_true
+  end
+    
+        trace_var :$Kernel_trace_var_global, proc {|value| captured = value}
+    
+    describe 'main#define_method' do
+  before :each do
+    @code = 'define_method(:boom) { :bam }'
+  end
+    
+            self.description = <<-DESC
+          Shows the content of the pods cache as a YAML tree output, organized by pod.
+          If `NAME` is given, only the caches for that pod will be included in the output.
+        DESC
+    
+      def delete_target_file?
+    return true if overwrite?
+    puts('File #{target_file} exist, do you want to overwrite it? (Y/N)')
+    ( 'y' == STDIN.gets.strip.downcase ? true : false)
+  end
+    
+              it 'fails when installing a non logstash plugin' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
+            expect(command).not_to install_successfully
+          end
+    
+    shared_examples 'logstash list' do |logstash|
+  describe 'logstash-plugin list on #{logstash.hostname}' do
+    before(:all) do
+      logstash.install({:version => LOGSTASH_VERSION})
     end
-  end
-end
-
     
-      def unpin
-    path.unlink if pinned?
-    HOMEBREW_PINNED_KEGS.rmdir_if_possible
+        before do
+      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
+      # Logstash won't update when we have a pinned version in the gemfile so we remove them
+      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
+      expect(logstash).to have_installed?(plugin_name, previous_version)
+    end
+    
+      def self.source_root
+    @source_root ||= File.expand_path('../templates', __FILE__)
   end
     
-      zap trash: [
-               '~/Library/Application Support/Adobe/Lightroom',
-               '~/Library/Preferences/com.adobe.Lightroom#{version.major}.plist',
-             ]
+    # The base module that gets included in ActiveRecord::Base. See the
+# documentation for Paperclip::ClassMethods for more useful information.
+module Paperclip
+  extend Helpers
+  extend Logger
+  extend ProcessorHelpers
+    
+        private
