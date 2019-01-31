791f30a96a912955c0ea9f16c5cf4d734bbefa91
SUNsung <weatherius@gmail.com>
2019-01-31 06:26:36 +0200

    Rewrite [37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb] File in: 2019-01-31 06:26:35

--- ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
+++ ruby/37b6OVC4kn.s37Jn7rj9SnBEU37wlUxEnAfOo37zyQ.rb
@@ -1,38 +1,64 @@
 
         
-            when IAX_TYPE_IAX
+                private
     
+    module ActionView
+  # = Action View Railtie
+  class Railtie < Rails::Engine # :nodoc:
+    config.action_view = ActiveSupport::OrderedOptions.new
+    config.action_view.embed_authenticity_token_in_remote_forms = nil
+    config.action_view.debug_missing_translation = true
+    config.action_view.default_enforce_utf8 = nil
+    config.action_view.finalize_compiled_template_methods = true
     
-IAX_TYPE_VOICE   = 2
-IAX_TYPE_CONTROL = 4
-IAX_TYPE_IAX     = 6
-IAX_TYPE_DTMF_BEGIN = 1
-IAX_TYPE_DTMF_END   = 12
+          redirect_to admin_report_path(@report)
+    end
     
-              # Rex::Proto::Kerberos::Model::ApReq decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'AP-REQ decoding not supported'
-          end
+      def setting
+    @_setting ||= ::Web::Setting.where(user: current_user).first_or_initialize(user: current_user)
+  end
+end
+
     
-              # @!attribute key
-          #   @return [Integer] The type of encryption key
-          attr_accessor :type
-          # @!attribute value
-          #   @return [String] the key itself
-          attr_accessor :value
+      def save_file(path, content, mode='w')
+    dir = File.dirname(path)
+    FileUtils.mkdir_p(dir) unless File.directory?(dir)
+    File.open(path, mode) { |file| file.write(content) }
+  end
     
-              # Decodes a Rex::Proto::Kerberos::Model::KdcRequestBody from an
-          # OpenSSL::ASN1::Sequence
-          #
-          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode_asn1(input)
-            seq_values = input.value
+        def initialize(*args)
+      @s = StringScanner.new(*args)
+    end
     
-              # Decodes a Rex::Proto::Kerberos::Model::KdcResponse from an String
-          #
-          # @param input [String] the input to decode from
-          def decode_string(input)
-            asn1 = OpenSSL::ASN1.decode(input)
+    Given(/^a custom task to run in the event of a failure$/) do
+  safely_remove_file(TestApp.shared_path.join('failed'))
+  TestApp.copy_task_to_test_app('spec/support/tasks/failed.rake')
+end
+    
+        extend Forwardable
+    attr_reader :variables
+    def_delegators :variables,
+                   :set, :fetch, :fetch_for, :delete, :keys, :validate
+    
+          private
+    
+        def prefers_plain_text?(env)
+      !(Request.new(env).preferred_type('text/plain','text/html') == 'text/html') &&
+      [/curl/].index { |item| item =~ env['HTTP_USER_AGENT'] }
+    end
+    
+    desc 'generate documentation'
+task :doc => 'doc:all'
+    
+          def call(env)
+        status, headers, body = super
+        response = Rack::Response.new(body, status, headers)
+        request = Rack::Request.new(env)
+        remove_bad_cookies(request, response)
+        response.finish
+      end
+    
+      it 'sets a new csrf token for the session in env, even after a 'safe' request' do
+    get('/', {}, {})
+    expect(env['rack.session'][:csrf]).not_to be_nil
+  end
