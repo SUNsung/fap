de17cc2053b0e54b53430d28020e0b63eb6c650c
SUNsung <weatherius@gmail.com>
2019-02-01 01:10:07 +0200

    Rewrite [24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb] File in: 2019-02-01 01:10:01

--- ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
+++ ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
@@ -1,24 +1,37 @@
 
         
-          # Controllers inheriting DeviseController are advised to override this
-  # method so that other controllers inheriting from them would use
-  # existing translations.
-  def translation_scope
-    'devise.#{controller_name}'
-  end
-    
-    class BugTest < ActionDispatch::IntegrationTest
-  include Rack::Test::Methods
-  include Warden::Test::Helpers
-    
-          def _devise_route_context
-        @_devise_route_context ||= send(Devise.available_router_name)
-      end
-    end
+        def jekyll_run_output
+  if Paths.output_file.file?
+    then return Paths.output_file.read
   end
 end
-
     
-          selected_modules = modules.map(&:to_sym).uniq.sort_by do |s|
-        Devise::ALL.index(s) || -1  # follow Devise::ALL order
+                  # Notify blocked threads that EventMachine has started or shutdown
+              EM.schedule { @started_event.set }
+              EM.add_shutdown_hook { @stopped_event.set }
+    
+              @highlighter = begin
+            if @config.key?('enable_coderay') && @config['enable_coderay']
+              Jekyll::Deprecator.deprecation_message(
+                'You are using 'enable_coderay', ' \
+                'use syntax_highlighter: coderay in your configuration file.'
+              )
+    
+      namespace :install do
+    GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
+      desc 'Build and install #{gem} as local gem'
+      task gem => package(gem, '.gem') do
+        sh 'gem install #{package(gem, '.gem')}'
       end
+    end
+    
+      task :all => [:readmes, :index]
+end
+    
+            reaction
+      end
+    
+        it 'Reads referrer from Host header when Referer header is missing' do
+      env = {'HTTP_HOST' => 'foo.com'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
