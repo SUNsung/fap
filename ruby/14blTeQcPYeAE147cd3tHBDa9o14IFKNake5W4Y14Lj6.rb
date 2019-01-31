480786a70c0f7e8be470acf271385563a537e25c
SUNsung <weatherius@gmail.com>
2019-01-31 07:53:23 +0200

    Rewrite [14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb] File in: 2019-01-31 07:53:22

--- ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
+++ ruby/14blTeQcPYeAE147cd3tHBDa9o14IFKNake5W4Y14Lj6.rb
@@ -1,94 +1,159 @@
 
         
-            # Called internally to setup the runner object
+                  @object_name.sub!(/\[\]$/, '') || @object_name.sub!(/\[\]\]$/, ']')
+          @object = retrieve_object(options.delete(:object))
+          @skip_default_ids = options.delete(:skip_default_ids)
+          @allow_method_names_outside_object = options.delete(:allow_method_names_outside_object)
+          @options = options
+    
+    require 'action_view/helpers/tags/checkable'
+    
+                  yield item, value, text, default_html_options.merge(additional_html_options)
+            end.join.html_safe
+          end
+    
+            private
+    
+          # Creates a _layout method to be called by _default_layout .
+      #
+      # If a layout is not explicitly mentioned then look for a layout with the controller's name.
+      # if nothing is found then try same procedure to find super class's layout.
+      def _write_layout_method # :nodoc:
+        silence_redefinition_of_method(:_layout)
+    
+        # Render but returns a valid Rack body. If fibers are defined, we return
+    # a streaming body that renders the template piece by piece.
     #
-    
-          xcode_outdated = false
-      begin
-        unless FastlaneCore::Helper.xcode_at_least?(Fastlane::MINIMUM_XCODE_RELEASE)
-          xcode_outdated = true
-        end
-      rescue
-        # We don't care about exceptions here
-        # We'll land here if the user doesn't have Xcode at all for example
-        # which is fine for someone who uses fastlane just for Android project
-        # What we *do* care about is when someone links an old version of Xcode
+    # Note that partials are not supported to be rendered with streaming,
+    # so in such cases, we just wrap them in an array.
+    def render_body(context, options)
+      if options.key?(:partial)
+        [render_partial(context, options)]
+      else
+        StreamingTemplateRenderer.new(@lookup_context).render(context, options)
       end
-    
-          it 'adds publish_docset param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            publish_docset: true
-          )
-        end').runner.execute(:test)
-    
-          it 'Uses the provided pretty format to collect log messages' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          changelog_from_git_commits(pretty: '%s%n%b')
-        end').runner.execute(:test)
-    
-            expect(result[1]).to eq('security default-keychain -s /tmp/test.keychain')
-        expect(result[2]).to eq('security unlock-keychain -p testpassword /tmp/test.keychain')
-    
-          it 'should shell escape keychain names when checking for installation' do
-        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
-        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
-    
-    if git.modified_files.include?('snapshot/lib/assets/SnapshotHelper.swift')
-  warn('You modified `SnapshotHelper.swift`, make sure to update the version number at the bottom of the file to notify users about the new helper file.')
-end
-    
-          private
-    
-        def default_constraints(options)
-      @constraints = Hash.new
-      @constraints.merge!(options[:constraints]) if options[:constraints]
     end
     
-        logs
-  end
-    
-      def initialize(*paths)
-    @paths = parse(*paths)
-  end
-    
-      it 'accepts a Bignum as a seed' do
-    srand(0x12345678901234567890)
-    srand.should == 0x12345678901234567890
-  end
-    
-      it 'returns false when the command exits with a non-zero exit status' do
-    @object.system(ruby_cmd('exit 1')).should == false
-    
-    describe 'Kernel#test' do
-  before :all do
-    @file = File.dirname(__FILE__) + '/fixtures/classes.rb'
-    @dir = File.dirname(__FILE__) + '/fixtures'
-  end
-    
-        remove_duplicates
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
-    
-        it 'generates a jasmine fixture', fixture: true do
-      session[:mobile_view] = true
-      get :new, format: :mobile
-      save_fixture(html_for('body'), 'conversations_new_mobile')
+            # Executes a command and returns true if the command succeeded,
+        # and false otherwise. By default, this executes as a normal user,
+        # and it is up to the communicator implementation if they expose an
+        # option for running tests as an administrator.
+        #
+        # @see #execute
+        def test(command, opts=nil)
+        end
+      end
     end
   end
 end
 
     
-        it 'generates a jasmine fixture', :fixture => true do
-      get :bookmarklet
-      save_fixture(html_for('body'), 'bookmarklet')
+            # This is called as a last-minute hook that allows the configuration
+        # object to finalize itself before it will be put into use. This is
+        # a useful place to do some defaults in the case the user didn't
+        # configure something or so on.
+        #
+        # An example of where this sort of thing is used or has been used:
+        # the 'vm' configuration key uses this to make sure that at least
+        # one sub-VM has been defined: the default VM.
+        #
+        # The configuration object is expected to mutate itself.
+        def finalize!
+          # Default implementation is to do nothing.
+        end
+    
+              # Set all of our instance variables on the new class
+          [self, other].each do |obj|
+            obj.instance_variables.each do |key|
+              # Ignore keys that start with a double underscore. This allows
+              # configuration classes to still hold around internal state
+              # that isn't propagated.
+              if !key.to_s.start_with?('@__')
+                # Don't set the value if it is the unset value, either.
+                value = obj.instance_variable_get(key)
+                result.instance_variable_set(key, value) if value != UNSET_VALUE
+              end
+            end
+          end
+    
+      desc 'Prepares for a release'
+  task :pre_release do
+    unless File.exist?('../Specs')
+      raise 'Ensure that the specs repo exits in the `../Specs` location'
+    end
+  end
+    
+    abstract_target 'Abstract Target' do
+    use_modular_headers!
+    
+      desc 'Release all gems to rubygems'
+  task release: :build do
+    sh 'git tag -a -m \'Version #{version}\' v#{version}'
+    
+            def log_state_changes
+          if @order.previous_changes[:state]
+            @order.log_state_changes(
+              state_name: 'order',
+              old_state: @order.previous_changes[:state].first,
+              new_state: @order.previous_changes[:state].last
+            )
+          end
+        end
+    
+            def order
+          @order ||= Spree::Order.includes(:line_items).find_by!(number: order_id)
+          authorize! :update, @order, order_token
+        end
+    
+            def update
+          authorize! :update, @product_property
+    
+              if @product.persisted?
+            respond_with(@product, status: 201, default_template: :show)
+          else
+            invalid_resource!(@product)
+          end
+        end
+    
+            def update
+          @return_authorization = order.return_authorizations.accessible_by(current_ability, :update).find(params[:id])
+          if @return_authorization.update_attributes(return_authorization_params)
+            respond_with(@return_authorization, default_template: :show)
+          else
+            invalid_resource!(@return_authorization)
+          end
+        end
+    
+      get(/.+/) do
+    send_sinatra_file(request.path) {404}
+  end
+    
+        # Initializes a new CategoryIndex.
+    #
+    #  +base+         is the String path to the <source>.
+    #  +category_dir+ is the String path between <source> and the category folder.
+    #  +category+     is the category currently being processed.
+    def initialize(site, base, category_dir, category)
+      @site = site
+      @base = base
+      @dir  = category_dir
+      @name = 'index.html'
+      self.process(@name)
+      # Read the YAML data from the layout page.
+      self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
+      self.data['category']    = category
+      # Set the title for this page.
+      title_prefix             = site.config['category_title_prefix'] || 'Category: '
+      self.data['title']       = '#{title_prefix}#{category}'
+      # Set the meta-description for this page.
+      meta_description_prefix  = site.config['category_meta_description_prefix'] || 'Category: '
+      self.data['description'] = '#{meta_description_prefix}#{category}'
     end
     
-          it 'federates' do
-        allow_any_instance_of(Participation::Generator).to receive(:create!)
-        expect(Diaspora::Federation::Dispatcher).to receive(:defer_dispatch)
-        post_request!
-      end
+      class ImageTag < Liquid::Tag
+    @img = nil
+    
+        def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
