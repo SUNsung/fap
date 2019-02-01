eddc1ea23528120d6b52ad7e46c24b1e5cca04d9
SUNsung <weatherius@gmail.com>
2019-02-01 11:45:09 +0200

    Rewrite [00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp] File in: 2019-02-01 11:45:09

--- cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
+++ cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
@@ -1,231 +1,355 @@
 
         
         
+    {    QString appName;
+    QIcon appIcon;
+    QIcon trayAndWindowIcon;
+    QString titleAddText;
+};
+    
+        ~reverse_lock() {
+        templock.lock();
+        templock.swap(lock);
+    }
+    
+    /** Generate blocks (mine) */
+UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbaseScript, int nGenerate, uint64_t nMaxTries, bool keepScript);
+    
+    int secp256k1_ecdh(const secp256k1_context* ctx, unsigned char *result, const secp256k1_pubkey *point, const unsigned char *scalar) {
+    int ret = 0;
+    int overflow = 0;
+    secp256k1_gej res;
+    secp256k1_ge pt;
+    secp256k1_scalar s;
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(result != NULL);
+    ARG_CHECK(point != NULL);
+    ARG_CHECK(scalar != NULL);
+    }
+    
+    
     {
-    {  uint64_t nElemX = nElemFromDim(X);
-  uint64_t nElemW = nElemFromDim(W);
-  uint64_t nElemB = nElemFromDim(b);
-  c.flops = 2 * K * M * N + M * N;
-  c.bytes_read = (nElemX + nElemW + nElemB) * sizeof(X.data_type());
-  c.bytes_written = M * N * sizeof(X.data_type());
-  c.params_bytes = (K * N + N) * sizeof(X.data_type());
-  return c;
+    {        /* compute using ECDH function */
+        CHECK(secp256k1_ec_pubkey_create(ctx, &point[0], s_one) == 1);
+        CHECK(secp256k1_ecdh(ctx, output_ecdh, &point[0], s_b32) == 1);
+        /* compute 'explicitly' */
+        CHECK(secp256k1_ec_pubkey_create(ctx, &point[1], s_b32) == 1);
+        CHECK(secp256k1_ec_pubkey_serialize(ctx, point_ser, &point_ser_len, &point[1], SECP256K1_EC_COMPRESSED) == 1);
+        CHECK(point_ser_len == sizeof(point_ser));
+        secp256k1_sha256_initialize(&sha);
+        secp256k1_sha256_write(&sha, point_ser, point_ser_len);
+        secp256k1_sha256_finalize(&sha, output_ser);
+        /* compare */
+        CHECK(memcmp(output_ecdh, output_ser, sizeof(output_ser)) == 0);
+    }
 }
-} // namespace caffe2
+    
+    #include <boost/test/unit_test.hpp>
+    
+    
+//------------------------------------------------------------------------------
+inline void formatImpl(std::ostream& out, const char* fmt,
+                       const detail::FormatArg* formatters,
+                       int numFormatters)
+{
+    // Saved stream state
+    std::streamsize origWidth = out.width();
+    std::streamsize origPrecision = out.precision();
+    std::ios::fmtflags origFlags = out.flags();
+    char origFill = out.fill();
+    }
+    
+    
+    {
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace csharp
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
 
     
-    OPERATOR_SCHEMA(Im2Col)
+    // ----------------------------------------------------------------------
+// NoLocaleStrtod()
+//   This code will make you cry.
+// ----------------------------------------------------------------------
+    
+        struct Margin {
+        Margin() : left(0), right(0), top(0), bottom(0) {}
+        Margin(size_t left_, size_t right_, size_t top_, size_t bottom_)
+            : left(left_), right(right_), top(top_), bottom(bottom_) {}
+    }
+    
+        for (size_t i = 0; i < size.height; ++i)
+    {
+        const u8 * src0 = internal::getRowPtr(src0Base, src0Stride, i);
+        const u8 * src1 = internal::getRowPtr(src1Base, src1Stride, i);
+        u16 * dst = internal::getRowPtr((u16 *)dstBase, dstStride, i);
+        size_t j = 0;
+    }
+    
+    void bitwiseXor(const Size2D &size,
+                const u8 *src0Base, ptrdiff_t src0Stride,
+                const u8 *src1Base, ptrdiff_t src1Stride,
+                u8 *dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride, BitwiseXor());
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+#endif
+}
+    
+    void extract4(const Size2D &size,
+              const u8 * srcBase, ptrdiff_t srcStride,
+              u8 * dstBase, ptrdiff_t dstStride,
+              u32 coi)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+#ifndef __ANDROID__
+    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
+#endif
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+    }
+    
+        v1k8 = vextq_s16(d16_23, d24, 1);
+    q0 = vmaxq_s16(q0, vminq_s16(ak8, v1k8));
+    q1 = vminq_s16(q1, vmaxq_s16(bk8, v1k8));
+    
+                uint8x8_t v_maxmask = vceq_u8(v_src, v_maxval8);
+            uint8x8_t v_minmask = vceq_u8(v_src, v_minval8);
+            uint8x8_t v_mask = vorr_u8(v_maxmask, v_minmask);
+    
+    typedef void (* flipFunc)(const Size2D &size,
+                  const void * srcBase, ptrdiff_t srcStride,
+                  void * dstBase, ptrdiff_t dstStride,
+                  FLIP_MODE flipMode);
+    
+    namespace caffe2 {
+    }
+    
+    REGISTER_CPU_OPERATOR(
+    MergeSingleScalarFeatureTensors,
+    MergeSingleScalarFeatureTensorsOp<CPUContext>);
+OPERATOR_SCHEMA(MergeSingleScalarFeatureTensors)
+    .SetDoc(
+        'Merge given single-feature tensors with scalar features into one '
+        'multi-feature tensor.' +
+        doc)
+    .NumInputs([](int n) { return n >= 2 && n % 2 == 0; })
+    .NumOutputs(3)
+    .Input(0, 'in1', '')
+    .Input(1, 'in1_presence', '.presence')
+    .Output(0, 'out_lengths', '.lengths')
+    .Output(1, 'out_keys', '.keys')
+    .Output(2, 'out_values', '.values')
+    .Arg('feature_ids', 'feature ids');
+    
+    OPERATOR_SCHEMA(Glu)
     .NumInputs(1)
     .NumOutputs(1)
-    .SetDoc('The Im2Col operator from Matlab.')
-    .TensorInferenceFunction(
-        [](const OperatorDef& def, const vector<TensorShape>& in) {
-          ArgumentHelper helper(def);
-          auto pad = helper.GetSingleArgument<int>('pad', 0);
-          auto kernel_h = helper.GetSingleArgument<int>(
-              'kernel_h', helper.GetSingleArgument<int>('kernel', 0));
-          auto kernel_w = helper.GetSingleArgument<int>(
-              'kernel_w', helper.GetSingleArgument<int>('kernel', 0));
-          auto dilation_h = helper.GetSingleArgument<int>(
-              'dilation_h', helper.GetSingleArgument<int>('dilation', 1));
-          auto dilation_w = helper.GetSingleArgument<int>(
-              'dilation_w', helper.GetSingleArgument<int>('dilation', 1));
-          auto stride_h = helper.GetSingleArgument<int>(
-              'stride_h', helper.GetSingleArgument<int>('stride', 1));
-          auto stride_w = helper.GetSingleArgument<int>(
-              'stride_w', helper.GetSingleArgument<int>('stride', 1));
-          auto order = StringToStorageOrder(
-              helper.GetSingleArgument<string>('order', 'NCHW'));
-    }
+    .SetDoc(R'DOC(
+Applies gated linear unit to the input Tensor X. The output Y is half the size
+of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
+[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
+X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
+)DOC')
+    .Input(0, 'X', '1D input tensor')
+    .Output(0, 'Y', '1D output tensor');
     
-    namespace detail {
-/*! \brief Implementation of gradient statistics pair. Template specialisation
- * may be used to overload different gradients types e.g. low precision, high
- * precision, integer, floating point. */
-template <typename T>
-class GradientPairInternal {
-  /*! \brief gradient statistics */
-  T grad_;
-  /*! \brief second order gradient statistics */
-  T hess_;
-    }
-    }
-    
-    
-    {    // loop over all rows and fill column entries
-    // num_nonzeros[fid] = how many nonzeros have this feature accumulated so far?
-    std::vector<size_t> num_nonzeros;
-    num_nonzeros.resize(nfeature);
-    std::fill(num_nonzeros.begin(), num_nonzeros.end(), 0);
-    for (size_t rid = 0; rid < nrow; ++rid) {
-      const size_t ibegin = gmat.row_ptr[rid];
-      const size_t iend = gmat.row_ptr[rid + 1];
-      size_t fid = 0;
-      for (size_t i = ibegin; i < iend; ++i) {
-        const uint32_t bin_id = gmat.index[i];
-        while (bin_id >= gmat.cut.row_ptr[fid + 1]) {
-          ++fid;
-        }
-        if (type_[fid] == kDenseColumn) {
-          uint32_t* begin = &index_[boundary_[fid].index_begin];
-          begin[rid] = bin_id - index_base_[fid];
-        } else {
-          uint32_t* begin = &index_[boundary_[fid].index_begin];
-          begin[num_nonzeros[fid]] = bin_id - index_base_[fid];
-          row_ind_[boundary_[fid].row_ind_begin + num_nonzeros[fid]] = rid;
-          ++num_nonzeros[fid];
-        }
-      }
-    }
+    class GetFloatToHalfGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'HalfToFloat', '', vector<string>{GO(0)}, vector<string>{GI(0)});
   }
-    
-    
-    {  /*!
-   * \brief dump the model in the requested format
-   * \param fmap feature map that may help give interpretations of feature
-   * \param with_stats extra statistics while dumping model
-   * \param format the format to dump the model in
-   * \return a vector of dump for boosters.
-   */
-  virtual std::vector<std::string> DumpModel(const FeatureMap& fmap,
-                                             bool with_stats,
-                                             std::string format) const = 0;
-  /*!
-   * \brief create a gradient booster from given name
-   * \param name name of gradient booster
-   * \param cache_mats The cache data matrix of the Booster.
-   * \param base_margin The base margin of prediction.
-   * \return The created booster.
-   */
-  static GradientBooster* Create(
-      const std::string& name,
-      const std::vector<std::shared_ptr<DMatrix> >& cache_mats,
-      bst_float base_margin);
 };
+REGISTER_GRADIENT(FloatToHalf, GetFloatToHalfGradient);
     
-      std::vector<std::shared_ptr<DHTNode>> nodes_;
+              const TensorShape& X = in[0];
+          int N = 0, C = 0, H = 0, W = 0;
+          switch (order) {
+            case StorageOrder::NCHW:
+              N = X.dims(0);
+              C = X.dims(1);
+              H = X.dims(2);
+              W = X.dims(3);
+              break;
+            case StorageOrder::NHWC:
+              N = X.dims(0);
+              H = X.dims(1);
+              W = X.dims(2);
+              C = X.dims(3);
+              break;
+            default:
+              CAFFE_THROW('Unknown storage order: ', order);
+          }
     
     
-    {} // namespace aria2
-
-    
-        routingTable->setTaskQueue(taskQueue.get());
-    routingTable->setTaskFactory(taskFactory.get());
-    
-      virtual std::shared_ptr<DHTTask>
-  createPeerLookupTask(const std::shared_ptr<DownloadContext>& ctx,
-                       uint16_t tcpPort,
-                       const std::shared_ptr<PeerStorage>& peerStorage) = 0;
-    
-    std::shared_ptr<DHTTask>
-DHTTaskFactoryImpl::createNodeLookupTask(const unsigned char* targetID)
-{
-  auto task = std::make_shared<DHTNodeLookupTask>(targetID);
-  setCommonProperty(task);
-  return task;
+    {    return 0;
 }
     
-      std::chrono::seconds timeout_;
     
-    #include <memory>
+    {    return;
+}
     
-    #include 'DHTNode.h'
-#include 'util.h'
-#include 'a2functional.h'
+    #include <iostream>
+#include <vector>
+#include <cassert>
     
-    #endif // D_DHT_UNKNOWN_MESSAGE_H
-
+    
+// Another Classic Non-Recursive algorithm for inorder traversal
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+    }
+    
+            vector<int> res;
+        if(root == NULL)
+            return res;
+    
+    /// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
     
     
     {
-    {      // See how much iterations should be increased by
-      // Note: Avoid division by zero with max(seconds, 1ns).
-      double multiplier = min_time * 1.4 / std::max(seconds, 1e-9);
-      // If our last run was at least 10% of FLAGS_benchmark_min_time then we
-      // use the multiplier directly. Otherwise we use at most 10 times
-      // expansion.
-      // NOTE: When the last run was at least 10% of the min time the max
-      // expansion should be 14x.
-      bool is_significant = (seconds / min_time) > 0.1;
-      multiplier = is_significant ? multiplier : std::min(10.0, multiplier);
-      if (multiplier <= 1.0) multiplier = 2.0;
-      double next_iters = std::max(multiplier * iters, iters + 1.0);
-      if (next_iters > kMaxIterations) {
-        next_iters = kMaxIterations;
-      }
-      VLOG(3) << 'Next iters: ' << next_iters << ', ' << multiplier << '\n';
-      iters = static_cast<int>(next_iters + 0.5);
+    {        stack<TreeNode*> stack;
+        TreeNode* cur = root;
+        while(cur != NULL || !stack.empty()){
+            if(cur != NULL){
+                res.push_back(cur->val);
+                stack.push(cur);
+                cur = cur->left;
+            }
+            else{
+                cur = stack.top();
+                stack.pop();
+                cur = cur->right;
+            }
+        }
+        return res;
     }
-  }
-  // Calculate additional statistics
-  auto stat_reports = ComputeStats(reports);
-  if ((b.complexity != oNone) && b.last_benchmark_instance) {
-    auto additional_run_stats = ComputeBigO(*complexity_reports);
-    stat_reports.insert(stat_reports.end(), additional_run_stats.begin(),
-                        additional_run_stats.end());
-    complexity_reports->clear();
-  }
-    
-    // Information kept per benchmark we may want to run
-struct Benchmark::Instance {
-  std::string name;
-  Benchmark* benchmark;
-  ReportMode report_mode;
-  std::vector<int> arg;
-  TimeUnit time_unit;
-  int range_multiplier;
-  bool use_real_time;
-  bool use_manual_time;
-  BigO complexity;
-  BigOFunc* complexity_lambda;
-  UserCounters counters;
-  const std::vector<Statistics>* statistics;
-  bool last_benchmark_instance;
-  int repetitions;
-  double min_time;
-  size_t iterations;
-  int threads;  // Number of concurrent threads to us
 };
     
-    bool IsFlag(const char* str, const char* flag) {
-  return (ParseFlagValue(str, flag, true) != nullptr);
+    using namespace std;
+    
+    
+    
+    
+    {
+    {
+    {            TagNode tagNode = stack.top();
+            stack.pop();
+            cur = tagNode.node;
+            if(tagNode.isFirst == false){
+                tagNode.isFirst = true;
+                stack.push(tagNode);
+                cur = cur->right;
+            }
+            else{
+                res.push_back(cur->val);
+                cur = NULL;
+            };
+        }
+        return res;
+    }
+};
+    
+    
+// Non-Recursive
+// Using two stacks, Reverse the Preorder Traversal!
+//
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+    }
+    
+            TreeNode* p = root;
+        while(p != NULL || !stack.empty()){
+            if(p != NULL){
+                stack.push(p);
+                output.push(p);
+                p = p->right;
+            }
+            else{
+                p = stack.top();
+                stack.pop();
+                p = p->left;
+            }
+        }
+    
+    using namespace folly;
+    
+    #ifndef SKIP_RESERVE
+void BENCHFUN(reserve)(int iters, int size) {
+  auto const obj = randomObject<VECTOR::value_type>();
+  FOR_EACH_RANGE (i, 0, iters) {
+    VECTOR v(random(0U, 1U), obj);
+    v.reserve(size);
+  }
 }
+BENCHMARK_PARAM(BENCHFUN(reserve), 16)
+BENCHMARK_PARAM(BENCHFUN(reserve), 128)
+BENCHMARK_PARAM(BENCHFUN(reserve), 1024)
+#endif
     
-    vector<detail::BenchmarkResult> resultsFromFile(const std::string& filename) {
-  string content;
-  readFile(filename.c_str(), content);
-  vector<detail::BenchmarkResult> ret;
-  benchmarkResultsFromDynamic(parseJson(content), ret);
-  return ret;
-}
+    // ============================================================================
+// folly/test/GLogBenchmark.cpp                    relative  time/iter  iters/s
+// ============================================================================
+// skip_overhead                                               36.37ns   27.49M
+// dev_null_log_overhead                                        2.61us  382.57K
+// ============================================================================
     
+    exception_wrapper::VTable const exception_wrapper::SharedPtr::ops_{
+    copy_,
+    move_,
+    delete_,
+    throw_,
+    type_,
+    get_exception_,
+    get_exception_ptr_};
     
-    {} // namespace folly
-
-    
-      mutable AtomicStruct<PackedPtr, Atom> ptr_;
-    
-    template <class T, size_t kNumSlots = 64>
-class CoreCachedWeakPtr {
+    template <
+    class Iterator = const char*,
+    class Base = folly::Range<boost::u8_to_u32_iterator<Iterator>>>
+class UTF8Range : public Base {
  public:
-  explicit CoreCachedWeakPtr(const CoreCachedSharedPtr<T, kNumSlots>& p) {
-    for (auto slot : folly::enumerate(slots_)) {
-      *slot = p.slots_[slot.index];
-    }
-  }
-    }
-    
-    
-    {  T* get() const {
-    return p_;
-  }
-  T* operator->() const {
-    return p_;
-  }
-  explicit operator bool() const {
-    return p_ == nullptr ? false : true;
-  }
-  bool operator==(const counted_ptr<T, Atom>& p) const {
-    return get() == p.get();
-  }
+  /* implicit */ UTF8Range(const folly::Range<Iterator> baseRange)
+      : Base(
+            boost::u8_to_u32_iterator<Iterator>(
+                baseRange.begin(),
+                baseRange.begin(),
+                baseRange.end()),
+            boost::u8_to_u32_iterator<Iterator>(
+                baseRange.end(),
+                baseRange.begin(),
+                baseRange.end())) {}
+  /* implicit */ UTF8Range(const std::string& baseString)
+      : Base(folly::Range<Iterator>(baseString)) {}
 };
+    
+    
+    { private:
+  std::array<std::weak_ptr<T>, kNumSlots> slots_;
+};
+    
+    namespace {
+    }
