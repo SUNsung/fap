9d69257f34240acbd564bf0f5c7a166b154c3fe7
SUNsung <weatherius@gmail.com>
2019-02-01 16:34:25 +0200

    Rewrite [13vC5xs9ToW.13QBNxqVJ1neI13X4rZ2zLy.I13uVp.py] File in: 2019-02-01 16:34:25

--- pyton/13vC5xs9ToW.13QBNxqVJ1neI13X4rZ2zLy.I13uVp.py
+++ pyton/13vC5xs9ToW.13QBNxqVJ1neI13X4rZ2zLy.I13uVp.py
@@ -1,124 +1,111 @@
 
-def login_required(view):
-    '''View decorator that redirects anonymous users to the login page.'''
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
+        
+            def backwards(self, orm):
+        # Removing unique constraint on 'GroupCommitResolution', fields ['group_id', 'commit_id']
+        db.delete_unique('sentry_groupcommitresolution', ['group_id', 'commit_id'])
     
-    from flaskr.auth import login_required
-from flaskr.db import get_db
+        def backwards(self, orm):
+        'Write your backwards methods here.'
     
-    # read in SQL for populating test data
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
+        def backwards(self, orm):
+        # Removing unique constraint on 'ApiAuthorization', fields ['user', 'application']
+        db.delete_unique('sentry_apiauthorization', ['user_id', 'application_id'])
     
     
-def _lookup_req_object(name):
-    top = _request_ctx_stack.top
-    if top is None:
-        raise RuntimeError(_request_ctx_err_msg)
-    return getattr(top, name)
+def cityscapes_to_coco(cityscapes_id):
+    lookup = {
+        0: 0,  # ... background
+        1: 2,  # bicycle
+        2: 3,  # car
+        3: 1,  # person
+        4: 7,  # train
+        5: 8,  # truck
+        6: 4,  # motorcycle
+        7: 6,  # bus
+        8: -1,  # rider (-1 means rand init)
+    }
+    return lookup[cityscapes_id]
     
-        @app.teardown_appcontext
-    def teardown_app(error=None):
-        called.append('app')
+    # Optional dataset entry keys
+_IM_PREFIX = 'image_prefix'
+_DEVKIT_DIR = 'devkit_directory'
+_RAW_DIR = 'raw_dir'
     
-        # Arguments
-        model: A Keras model instance. To avoid OOM errors,
-            this model could have been built on CPU, for instance
-            (see usage example below).
-        gpus: Integer >= 2 or list of integers, number of GPUs or
-            list of GPU IDs on which to create model replicas.
-        cpu_merge: A boolean value to identify whether to force
-            merging model weights under the scope of the CPU or not.
-        cpu_relocation: A boolean value to identify whether to
-            create the model's weights under the scope of the CPU.
-            If the model is not defined under any preceding device
-            scope, you can still rescue it by activating this option.
+        # Get IoU overlap between each ex ROI and gt ROI
+    ex_gt_overlaps = box_utils.bbox_overlaps(
+        rois[ex_inds, :].astype(dtype=np.float32, copy=False),
+        rois[gt_inds, :].astype(dtype=np.float32, copy=False))
     
-    if K.backend() != 'tensorflow':
-    raise RuntimeError('This example can only run with the '
-                       'TensorFlow backend, '
-                       'because it requires TF-native augmentation APIs')
-    
+    def add_ResNet_roi_conv5_head_for_keypoints(
+    model, blob_in, dim_in, spatial_scale
+):
+    '''Add a ResNet 'conv5' / 'stage5' head for Mask R-CNN keypoint prediction.
     '''
-Note:
-batch_size is highly sensitive.
-Only 2 epochs are needed as the dataset is very small.
-'''
+    model.RoIFeatureTransform(
+        blob_in,
+        '_[pose]_pool5',
+        blob_rois='keypoint_rois',
+        method=cfg.KRCNN.ROI_XFORM_METHOD,
+        resolution=cfg.KRCNN.ROI_XFORM_RESOLUTION,
+        sampling_ratio=cfg.KRCNN.ROI_XFORM_SAMPLING_RATIO,
+        spatial_scale=spatial_scale
+    )
+    # Using the prefix '_[pose]_' to 'res5' enables initializing the head's
+    # parameters using pretrained 'res5' parameters if given (see
+    # utils.net.initialize_from_weights_file)
+    s, dim_in = ResNet.add_stage(
+        model,
+        '_[pose]_res5',
+        '_[pose]_pool5',
+        3,
+        dim_in,
+        2048,
+        512,
+        cfg.KRCNN.DILATION,
+        stride_init=int(cfg.KRCNN.ROI_XFORM_RESOLUTION / 7)
+    )
+    return s, 2048
     
-        # Check cryptography version
-    from cryptography import __version__ as cryptography_version
-    _check_cryptography(cryptography_version)
-except ImportError:
-    pass
+    from detectron.core.config import cfg
+from detectron.modeling.detector import DetectionModelHelper
+from detectron.roi_data.loader import RoIDataLoader
+import detectron.modeling.fast_rcnn_heads as fast_rcnn_heads
+import detectron.modeling.keypoint_rcnn_heads as keypoint_rcnn_heads
+import detectron.modeling.mask_rcnn_heads as mask_rcnn_heads
+import detectron.modeling.name_compat as name_compat
+import detectron.modeling.optimizer as optim
+import detectron.modeling.retinanet_heads as retinanet_heads
+import detectron.modeling.rfcn_heads as rfcn_heads
+import detectron.modeling.rpn_heads as rpn_heads
+import detectron.roi_data.minibatch as roi_data_minibatch
+import detectron.utils.c2 as c2_utils
     
-        Provide both the name and the version of the Python implementation
-    currently running. For example, on CPython 2.7.5 it will return
-    {'name': 'CPython', 'version': '2.7.5'}.
+    from detectron.core.config import cfg
+import detectron.utils.c2 as c2_utils
     
-        def rebuild_auth(self, prepared_request, response):
-        '''When being redirected we may want to strip authentication from the
-        request to avoid leaking credentials. This method intelligently removes
-        and reapplies authentication where possible to avoid credential loss.
-        '''
-        headers = prepared_request.headers
-        url = prepared_request.url
+    from detectron.datasets import json_dataset
+from detectron.datasets import roidb as roidb_utils
+from detectron.utils import blob as blob_utils
+import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
     
-        replace_chars = ' \'''
+        # Scale rois and format as (batch_idx, x1, y1, x2, y2)
+    sampled_rois = sampled_boxes * im_scale
+    repeated_batch_idx = batch_idx * blob_utils.ones((sampled_rois.shape[0], 1))
+    sampled_rois = np.hstack((repeated_batch_idx, sampled_rois))
     
-        def test_string_literals(self):
-        x = ''; y = ''; self.assertTrue(len(x) == 0 and x == y)
-        x = '\''; y = '''; self.assertTrue(len(x) == 1 and x == y and ord(x) == 39)
-        x = '''; y = '\''; self.assertTrue(len(x) == 1 and x == y and ord(x) == 34)
-        x = 'doesn't \'shrink\' does it'
-        y = 'doesn\'t 'shrink' does it'
-        self.assertTrue(len(x) == 24 and x == y)
-        x = 'does \'shrink\' doesn't it'
-        y = 'does 'shrink' doesn\'t it'
-        self.assertTrue(len(x) == 24 and x == y)
-        x = '''
-The 'quick'
-brown fox
-jumps over
-the 'lazy' dog.
-'''
-        y = '\nThe 'quick'\nbrown fox\njumps over\nthe \'lazy\' dog.\n'
-        self.assertEqual(x, y)
-        y = '''
-The 'quick'
-brown fox
-jumps over
-the 'lazy' dog.
-'''
-        self.assertEqual(x, y)
-        y = '\n\
-The \'quick\'\n\
-brown fox\n\
-jumps over\n\
-the 'lazy' dog.\n\
-'
-        self.assertEqual(x, y)
-        y = '\n\
-The \'quick\'\n\
-brown fox\n\
-jumps over\n\
-the \'lazy\' dog.\n\
-'
-        self.assertEqual(x, y)
+    logger = logging.getLogger(__name__)
     
-            self.assertEqual(total, 3)
-    
-    
-def main():
-    import io
-    import pprint
-    
-    document = '''\
-<slideshow>
-<title>Demo slideshow</title>
-<slide><title>Slide title</title>
-<point>This is a demo</point>
-<point>Of a program for processing slides</point>
-</slide>
+        net = core.Net('dequeue_net')
+    net.type = 'dag'
+    all_blobs = []
+    for gpu_id in range(cfg.NUM_GPUS):
+        with core.NameScope('gpu_{}'.format(gpu_id)):
+            with core.DeviceScope(muji.OnGPU(gpu_id)):
+                for blob_name in blob_names:
+                    blob = core.ScopedName(blob_name)
+                    all_blobs.append(blob)
+                    workspace.CreateBlob(blob)
+                    logger.info('Creating blob: {}'.format(blob))
+                net.DequeueBlobs(
+                    roi_data_loader._blobs_queue_name, blob_names)
+    logger.info('Protobuf:\n' + str(net.Proto()))
