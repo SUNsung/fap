274196674db38b993b88a1d83bc7f5a29240fc1f
SUNsung <weatherius@gmail.com>
2019-02-01 13:56:38 +0200

    Rewrite [26rxtjJc78mvM26brhkCzghaHc26RKkjnZ6IzNM26egk.py] File in: 2019-02-01 13:56:38

--- pyton/26rxtjJc78mvM26brhkCzghaHc26RKkjnZ6IzNM26egk.py
+++ pyton/26rxtjJc78mvM26brhkCzghaHc26RKkjnZ6IzNM26egk.py
@@ -1,140 +1,138 @@
 
         
-        
-if len(sys.argv) <= 1:
-    print('Specify the version number as parameter')
-    sys.exit()
-version = sys.argv[1]
-    
-    with io.open(README_FILE, encoding='utf-8') as f:
-    oldreadme = f.read()
+            def get_prep_lookup(self):
+        return [str(item) for item in self.rhs]
     
     
-def build_completion(opt_parser):
-    opts = [opt for group in opt_parser.option_groups
-            for opt in group.option_list]
-    opts_file = [opt for opt in opts if opt.metavar == 'FILE']
-    opts_dir = [opt for opt in opts if opt.metavar == 'DIR']
+class CreateExtension(Operation):
+    reversible = True
     
-        def test_youtube_channel_matching(self):
-        assertChannel = lambda url: self.assertMatch(url, ['youtube:channel'])
-        assertChannel('https://www.youtube.com/channel/HCtnHdj3df7iM')
-        assertChannel('https://www.youtube.com/channel/HCtnHdj3df7iM?feature=gb_ch_rec')
-        assertChannel('https://www.youtube.com/channel/HCtnHdj3df7iM/videos')
-    
-    
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
-    '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
-        else:
-            out = string.decode(encoding)
-    
-        return inner
-    
-        @pytest.fixture(autouse=True)
-    def setup(self):
-        '''LookupDict instance with 'bad_gateway' attribute.'''
-        self.lookup_dict = LookupDict('test')
-        self.lookup_dict.bad_gateway = 502
-    
-        @classmethod
-    def text_response_server(cls, text, request_timeout=0.5, **kwargs):
-        def text_response_handler(sock):
-            request_content = consume_socket_content(sock, timeout=request_timeout)
-            sock.send(text.encode('utf-8'))
-    
-                # Allow self-specified cert location.
-            if verify is not True:
-                cert_loc = verify
-    
-    
-class VersionedPackage(object):
-    def __init__(self, version):
-        self.__version__ = version
-    
-        def test_server_finishes_when_no_connections(self):
-        '''the server thread exits even if there are no connections'''
-        server = Server.basic_response_server()
-        with server:
-            pass
-    
-        def test_long_authinfo_in_url(self):
-        url = 'http://{}:{}@{}:9000/path?query#frag'.format(
-            'E8A3BE87-9E3F-4620-8858-95478E385B5B',
-            'EA770032-DA4D-4D84-8CE9-29C6D910BF1E',
-            'exactly-------------sixty-----------three------------characters',
+        def _get_session_key(self):
+        '''
+        Instead of generating a random string, generate a secure url-safe
+        base64-encoded string of data as our session key.
+        '''
+        return signing.dumps(
+            self._session, compress=True,
+            salt='django.contrib.sessions.backends.signed_cookies',
+            serializer=self.serializer,
         )
-        r = requests.Request('GET', url).prepare()
-        assert r.url == url
     
     
-def get_encoding_from_headers(headers):
-    '''Returns encodings from given HTTP Header Dict.
+class AbstractBaseSession(models.Model):
+    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
+    session_data = models.TextField(_('session data'))
+    expire_date = models.DateTimeField(_('expire date'), db_index=True)
+    
+    from django.contrib.sites.shortcuts import get_current_site
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.utils.http import http_date
+    
+        It can be used in place of a ReLU.
+    
+        x_train = np.empty((num_train_samples, 3, 32, 32), dtype='uint8')
+    y_train = np.empty((num_train_samples,), dtype='uint8')
+    
+        # Arguments
+        l1: Float; L1 regularization factor.
+        l2: Float; L2 regularization factor.
+    '''
     
     
-__licence__ = 'BSD (3 clause)'
+def test_objective_shapes_2d():
+    y_a = K.variable(np.random.random((6, 7)))
+    y_b = K.variable(np.random.random((6, 7)))
+    for obj in allobj:
+        objective_output = obj(y_a, y_b)
+        assert K.eval(objective_output).shape == (6,)
     
-        def get_actiontec_data(self):
-        '''Retrieve data from Actiontec MI424WR and return parsed result.'''
-        try:
-            telnet = telnetlib.Telnet(self.host)
-            telnet.read_until(b'Username: ')
-            telnet.write((self.username + '\n').encode('ascii'))
-            telnet.read_until(b'Password: ')
-            telnet.write((self.password + '\n').encode('ascii'))
-            prompt = telnet.read_until(
-                b'Wireless Broadband Router> ').split(b'\n')[-1]
-            telnet.write('firewall mac_cache_dump\n'.encode('ascii'))
-            telnet.write('\n'.encode('ascii'))
-            telnet.read_until(prompt)
-            leases_result = telnet.read_until(prompt).split(b'\n')[1:-1]
-            telnet.write('exit\n'.encode('ascii'))
-        except EOFError:
-            _LOGGER.exception('Unexpected response from router')
-            return
-        except ConnectionRefusedError:
-            _LOGGER.exception('Connection refused by router. Telnet enabled?')
-            return None
+    model.compile(loss='binary_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          validation_data=(x_test, y_test))
+
     
-            _LOGGER.debug('Nmap last results %s', self.last_results)
+    # Model creation using tensors from the get_next() graph node.
+inputs, targets = iterator.get_next()
+model_input = layers.Input(tensor=inputs)
+model_output = cnn_layers(model_input)
+train_model = keras.models.Model(inputs=model_input, outputs=model_output)
     
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/device_tracker.swisscom/
+        '''
+    
+        def setUp(self):
+        from acme.fields import Resource
+        self.field = Resource('x')
+    
+    # Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+]
+    
+    
+class AugeasConfigurator(common.Installer):
+    '''Base Augeas Configurator class.
+    
+    
+if __name__ == '__main__':
+    unittest.main()  # pragma: no cover
+
+    
+    def prepare_input(dirty):
+    '''
+    Prepare the plaintext by up-casing it
+    and separating repeated letters with X's
+    '''
+    
+    dirty = ''.join([c.upper() for c in dirty if c in string.ascii_letters])
+    clean = ''
+    
+    if len(dirty) < 2:
+        return dirty
+    
+                if new_key is None:
+                break
+    
+    The problem is :
+Given two strings A and B. Find the minimum number of operations to string B such that A = B. The permitted operations are removal,  insertion, and substitution.
 '''
-import logging
+from __future__ import print_function
     
-    import requests
-import voluptuous as vol
     
-            # Note that the double newline at the end of
-        # this string is required per the SSDP spec
-        resp_template = '''HTTP/1.1 200 OK
-CACHE-CONTROL: max-age=60
-EXT:
-LOCATION: http://{0}:{1}/description.xml
-SERVER: FreeRTOS/6.0.5, UPnP/1.0, IpBridge/0.1
-hue-bridgeid: 1234
-ST: urn:schemas-upnp-org:device:basic:1
-USN: uuid:Socket-1_0-221438K0100073::urn:schemas-upnp-org:device:basic:1
+if __name__ == '__main__':
+    whole_array = input('please input some numbers:')
+    array = SubArray(whole_array)
+    re = array.solve_sub_array()
+    print(('the results is:', re))
     
-        def __call__(self, method):
-        '''Decorate a function.'''
-        @wraps(method)
-        async def wrapper(view, request, *args, **kwargs):
-            '''Wrap a request handler with data validation.'''
-            data = None
-            try:
-                data = await request.json()
-            except ValueError:
-                if not self._allow_empty or \
-                   (await request.content.read()) != b'':
-                    _LOGGER.error('Invalid JSON received.')
-                    return view.json_message('Invalid JSON.', 400)
-                data = {}
+    Inputs:
+  - X , a 2D numpy array of features.
+  - k , number of clusters to create.
+  - initial_centroids , initial centroid values generated by utility function(mentioned in usage).
+  - maxiter , maximum number of iterations to process.
+  - heterogeneity , empty list that will be filled with hetrogeneity values if passed to kmeans func.
+    
+    # Note: This isn't exactly the same as a 'percent match'. The scale isn't linear. But you can assume that images with a
+# smaller distance are more similar to each other than ones with a larger distance.
+    
+        # If no faces are found in the image, return an empty result.
+    if len(X_face_locations) == 0:
+        return []
+    
+    print('I found {} face(s) in this photograph.'.format(len(face_locations)))
+    
+    # 初始化变量
+face_locations = []
+face_encodings = []
