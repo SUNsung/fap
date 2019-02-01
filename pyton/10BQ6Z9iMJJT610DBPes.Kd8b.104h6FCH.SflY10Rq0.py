2b4d3e2474a61d5de2efbdb5be1647b4839191f8
SUNsung <weatherius@gmail.com>
2019-02-01 09:16:40 +0200

    Rewrite [10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py] File in: 2019-02-01 09:16:39

--- pyton/10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py
+++ pyton/10BQ6Z9iMJJT610DBPes.Kd8b.104h6FCH.SflY10Rq0.py
@@ -1,232 +1,132 @@
 
         
         
-@classmethod
-def get_args(cls, dist, header=None):
+@pytest.fixture
+def runner(app):
+    '''A test runner for the app's Click commands.'''
+    return app.test_cli_runner()
+    
+        with client:
+        auth.logout()
+        assert 'user_id' not in session
+
+    
+            for line in _dump_loader_info(loader):
+            info.append('       %s' % line)
+    
+    
+def _render(template, context, app):
+    '''Renders the template and fires the signal'''
+    
+            :param force: Ignore the mimetype and always try to parse JSON.
+        :param silent: Silence parsing errors and return ``None``
+            instead.
+        :param cache: Store the parsed JSON to return for subsequent
+            calls.
+        '''
+        if cache and self._cached_json[silent] is not Ellipsis:
+            return self._cached_json[silent]
+    
+        set_init_version(version)
+    make_git_commit('Bump version number to %s', version)
+    make_git_tag(version)
+    build()
+    set_init_version(dev_version)
+    
+        try:
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            USERNAME,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
+
+    
+    
+class TestAutoContentTypeAndAcceptHeaders:
     '''
-    Yield write_script() argument tuples for a distribution's
-    console_scripts and gui_scripts entry points.
-    '''
-    if header is None:
-        header = cls.get_header()
-    spec = str(dist.as_requirement())
-    for type_ in 'console', 'gui':
-        group = type_ + '_scripts'
-        for name, ep in dist.get_entry_map(group).items():
-            # ensure_safe_name
-            if re.search(r'[\\/]', name):
-                raise ValueError('Path separators not allowed in script names')
-            script_text = TEMPLATE.format(
-                          ep.module_name, ep.attrs[0], '.'.join(ep.attrs),
-                          spec, group, name)
-            args = cls._get_script_args(type_, name, header, script_text)
-            for res in args:
-                yield res
+    Test that Accept and Content-Type correctly defaults to JSON,
+    but can still be overridden. The same with Content-Type when --form
+    -f is used.
     
     
-@pytest.fixture(params=[(python_3, False),
-                        (python_3, True),
-                        (python_2, False)])
-def proc(request, spawnu, TIMEOUT):
-    container, instant_mode = request.param
-    proc = spawnu(*container)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(init_zshrc.format(
-        u'--enable-experimental-instant-mode' if instant_mode else ''))
-    proc.sendline(u'zsh')
-    if instant_mode:
-        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
-    return proc
-    
-    match_output = '''
-Listing... Done
-heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
-resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
-squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
-unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
-'''
+class Response(object):
+    # noinspection PyDefaultArgument
+    def __init__(self, url, headers={}, status_code=200):
+        self.url = url
+        self.headers = CaseInsensitiveDict(headers)
+        self.status_code = status_code
     
     
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_get_new_command(brew_no_available_formula):
-    assert get_new_command(Command('brew install elsticsearch',
-                                   brew_no_available_formula))\
-        == 'brew install elasticsearch'
-    
-    no_such_subcommand = '''error: no such subcommand
-    
-      * glmnet-python
-  * scikit-learn (of course)
-    
-        for i, kk in enumerate(krange):
-        print('k = %i (%i out of %i)' % (kk, i + 1, len(krange)))
-        for algorithm in algorithms:
-            nbrs = neighbors.NearestNeighbors(n_neighbors=kk,
-                                              algorithm=algorithm,
-                                              leaf_size=leaf_size)
-            t0 = time()
-            nbrs.fit(X)
-            t1 = time()
-            nbrs.kneighbors(X)
-            t2 = time()
-    
-                gc.collect()
-            print('benchmarking orthogonal_mp (with Gram):', end='')
-            sys.stdout.flush()
-            tstart = time()
-            orthogonal_mp(X, y, precompute=True,
-                          n_nonzero_coefs=n_informative)
-            delta = time() - tstart
-            print('%0.3fs' % delta)
-            omp_gram[i_f, i_s] = delta
-    
-        results = defaultdict(lambda: [])
-    
-        ###########################################################################
-    # Perform benchmark
-    ###########################################################################
-    time_fit = collections.defaultdict(list)
-    time_transform = collections.defaultdict(list)
-    
-        target = utils.unescape(target).strip()
-    title = utils.unescape(title).strip()
-    config = inliner.document.settings.env.app.config
-    if config.issues_user_uri:
-        ref = config.issues_user_uri.format(user=target)
-    else:
-        ref = 'https://github.com/{0}'.format(target)
-    if has_explicit_title:
-        text = title
-    else:
-        text = '@{0}'.format(target)
+class AlexaFlashBriefingView(http.HomeAssistantView):
+    '''Handle Alexa Flash Briefing skill requests.'''
     
     
-def _do_python_eval(json_dataset, salt, output_dir='output'):
-    info = voc_info(json_dataset)
-    year = info['year']
-    anno_path = info['anno_path']
-    image_set_path = info['image_set_path']
-    devkit_path = info['devkit_path']
-    cachedir = os.path.join(devkit_path, 'annotations_cache')
-    aps = []
-    # The PASCAL VOC metric changed in 2010
-    use_07_metric = True if int(year) < 2010 else False
-    logger.info('VOC07 metric? ' + ('Yes' if use_07_metric else 'No'))
-    if not os.path.isdir(output_dir):
-        os.mkdir(output_dir)
-    for _, cls in enumerate(json_dataset.classes):
-        if cls == '__background__':
-            continue
-        filename = _get_voc_results_file_template(
-            json_dataset, salt).format(cls)
-        rec, prec, ap = voc_eval(
-            filename, anno_path, image_set_path, cls, cachedir, ovthresh=0.5,
-            use_07_metric=use_07_metric)
-        aps += [ap]
-        logger.info('AP for {} = {:.4f}'.format(cls, ap))
-        res_file = os.path.join(output_dir, cls + '_pr.pkl')
-        save_object({'rec': rec, 'prec': prec, 'ap': ap}, res_file)
-    logger.info('Mean AP = {:.4f}'.format(np.mean(aps)))
-    logger.info('~~~~~~~~')
-    logger.info('Results:')
-    for ap in aps:
-        logger.info('{:.3f}'.format(ap))
-    logger.info('{:.3f}'.format(np.mean(aps)))
-    logger.info('~~~~~~~~')
-    logger.info('')
-    logger.info('----------------------------------------------------------')
-    logger.info('Results computed with the **unofficial** Python eval code.')
-    logger.info('Results should be very close to the official MATLAB code.')
-    logger.info('Use `./tools/reval.py --matlab ...` for your paper.')
-    logger.info('-- Thanks, The Management')
-    logger.info('----------------------------------------------------------')
+def get_scanner(hass, config):
+    '''Validate the configuration and return an Actiontec scanner.'''
+    scanner = ActiontecDeviceScanner(config[DOMAIN])
+    return scanner if scanner.success_init else None
     
-        # ==========================================================================
-    # classification tower with logits and prob prediction
-    # ==========================================================================
-    for lvl in range(k_min, k_max + 1):
-        bl_in = blobs_in[k_max - lvl]  # blobs_in is in reversed order
-        # classification tower stack convolution starts
-        for nconv in range(cfg.RETINANET.NUM_CONVS):
-            suffix = 'n{}_fpn{}'.format(nconv, lvl)
-            dim_in, dim_out = dim_in, dim_in
-            if lvl == k_min:
-                bl_out = model.Conv(
-                    bl_in,
-                    'retnet_cls_conv_' + suffix,
-                    dim_in,
-                    dim_out,
-                    3,
-                    stride=1,
-                    pad=1,
-                    weight_init=('GaussianFill', {
-                        'std': 0.01
-                    }),
-                    bias_init=('ConstantFill', {
-                        'value': 0.
-                    })
-                )
-            else:
-                bl_out = model.ConvShared(
-                    bl_in,
-                    'retnet_cls_conv_' + suffix,
-                    dim_in,
-                    dim_out,
-                    3,
-                    stride=1,
-                    pad=1,
-                    weight='retnet_cls_conv_n{}_fpn{}_w'.format(nconv, k_min),
-                    bias='retnet_cls_conv_n{}_fpn{}_b'.format(nconv, k_min)
-                )
-            bl_in = model.Relu(bl_out, bl_out)
-            bl_feat = bl_in
-        # cls tower stack convolution ends. Add the logits layer now
-        if lvl == k_min:
-            retnet_cls_pred = model.Conv(
-                bl_feat,
-                'retnet_cls_pred_fpn{}'.format(lvl),
-                dim_in,
-                cls_pred_dim * A,
-                3,
-                pad=1,
-                stride=1,
-                weight_init=('GaussianFill', {
-                    'std': 0.01
-                }),
-                bias_init=bias_init
-            )
-        else:
-            retnet_cls_pred = model.ConvShared(
-                bl_feat,
-                'retnet_cls_pred_fpn{}'.format(lvl),
-                dim_in,
-                cls_pred_dim * A,
-                3,
-                pad=1,
-                stride=1,
-                weight='retnet_cls_pred_fpn{}_w'.format(k_min),
-                bias='retnet_cls_pred_fpn{}_b'.format(k_min)
-            )
-        if not model.train:
-            if cfg.RETINANET.SOFTMAX:
-                model.net.GroupSpatialSoftmax(
-                    retnet_cls_pred,
-                    'retnet_cls_prob_fpn{}'.format(lvl),
-                    num_classes=cls_pred_dim
-                )
-            else:
-                model.net.Sigmoid(
-                    retnet_cls_pred, 'retnet_cls_prob_fpn{}'.format(lvl)
-                )
-        if cfg.RETINANET.SHARE_CLS_BBOX_TOWER:
-            bbox_feat_list.append(bl_feat)
+            # doing a request
+        try:
+            res = requests.get(self._url, timeout=10, cookies={
+                'userid': self._userid
+            })
+        except requests.exceptions.Timeout:
+            _LOGGER.error(
+                'Connection to the router timed out at URL %s', self._url)
+            return False
+        if res.status_code != 200:
+            _LOGGER.error(
+                'Connection failed with http code %s', res.status_code)
+            return False
+        try:
+            result = res.json()
+        except ValueError:
+            # If json decoder could not parse the response
+            _LOGGER.error('Failed to parse response from router')
+            return False
     
-        if opts.profiler:
-        import cProfile
-        cProfile.runctx(
-            'loader_loop(roi_data_loader)', globals(), locals(),
-            sort='cumulative')
-    else:
-        loader_loop(roi_data_loader)
+        def __init__(self, config):
+        '''Initialize the scanner.'''
+        self.host = config[CONF_HOST]
+        self.last_results = {}
+    
+    _DEVICES_REGEX = re.compile(
+    r'(?P<mac>(([0-9a-f]{2}[:-]){5}([0-9a-f]{2})))\s'
+    r'(?P<ip>([0-9]{1,3}[\.]){3}[0-9]{1,3})\s+'
+    r'(?P<status>([^\s]+))\s+'
+    r'(?P<type>([^\s]+))\s+'
+    r'(?P<intf>([^\s]+))\s+'
+    r'(?P<hwintf>([^\s]+))\s+'
+    r'(?P<host>([^\s]+))')
+    
+            # Note that the double newline at the end of
+        # this string is required per the SSDP spec
+        resp_template = '''HTTP/1.1 200 OK
+CACHE-CONTROL: max-age=60
+EXT:
+LOCATION: http://{0}:{1}/description.xml
+SERVER: FreeRTOS/6.0.5, UPnP/1.0, IpBridge/0.1
+hue-bridgeid: 1234
+ST: urn:schemas-upnp-org:device:basic:1
+USN: uuid:Socket-1_0-221438K0100073::urn:schemas-upnp-org:device:basic:1
+    
+    REQUIREMENTS = ['watchdog==0.8.3']
+_LOGGER = logging.getLogger(__name__)
+    
+            hass.data[DOMAIN][platform][module_name] = module
+        for code in codes:
+            code_tuple = (code.get(CONF_NAME),
+                          code.get(CONF_DEVICE, STATE_UNKNOWN))
+            module.registerCode(code_tuple, code.get(CONF_CODE))
+    
+    This is the base platform to support LaMetric components:
+Notify, Light, Mediaplayer
+    
+        return True
