7719bd2740a854b09f16b9daf85fa1960436b825
SUNsung <weatherius@gmail.com>
2019-02-03 01:57:43 +0200

    Rewrite [02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py] File in: 2019-02-03 01:57:43

--- pyton/02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py
+++ pyton/02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py
@@ -1,87 +1,50 @@
 
         
-            def possible_scores(self):
-        '''Return a list of possible scores, taking Aces into account.'''
-        pass
+            # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+    return model
     
-        def reducer(self, key, values):
-        total = sum(values)
-        if total == 1:
-            yield key, total
+    from ..utils.data_utils import get_file
+import numpy as np
     
-    Options:
-  -h  This help text.
-  -q  Loggable output - no progress indicator
-  -qq No output except for errors
-  -d  Download only - do NOT install or unpack archives
-  -s  No-act. Perform ordering simulation
-  -y  Assume Yes to all queries and do not prompt
-  -f  Attempt to correct a system with broken dependencies in place
-  -m  Attempt to continue if archives are unlocatable
-  -u  Show a list of upgraded packages as well
-  -b  Build the source package after fetching it
-  -V  Show verbose version numbers
-  -c=? Read this configuration file
-  -o=? Set an arbitrary configuration option, eg -o dir::cache=/tmp
-See the apt-get(8), sources.list(5) and apt.conf(5) manual
-pages for more information and options.
-                       This APT has Super Cow Powers.
-'''
-apt_get_operations = ['update', 'upgrade', 'install', 'remove', 'autoremove',
-                      'purge', 'source', 'build-dep', 'dist-upgrade',
-                      'dselect-upgrade', 'clean', 'autoclean', 'check',
-                      'changelog', 'download']
-    
-    match_output = '''
-Listing... Done
-heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
-resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
-squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
-unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
-'''
+        def __call__(self, x):
+        regularization = 0.
+        if self.l1:
+            regularization += K.sum(self.l1 * K.abs(x))
+        if self.l2:
+            regularization += K.sum(self.l2 * K.square(x))
+        return regularization
     
     
-@pytest.mark.parametrize('command', [
-    Command('cargo buid', no_such_subcommand_old),
-    Command('cargo buils', no_such_subcommand)])
-def test_match(command):
-    assert match(command)
+def test_decode_predictions():
+    x = np.zeros((2, 1000))
+    x[0, 372] = 1.0
+    x[1, 549] = 1.0
+    outs = utils.decode_predictions(x, top=1)
+    scores = [out[0][2] for out in outs]
+    assert scores[0] == scores[1]
     
-    Note that the same result can also be achieved via a Lambda layer.
     
-        fpath = os.path.join(path, 'test')
-    x_test, y_test = load_batch(fpath, label_key=label_mode + '_labels')
+def test_imdb():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = imdb.load_data()
+        (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=40)
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        word_index = imdb.get_word_index()
+        assert isinstance(word_index, dict)
     
-        input_tensor = Input(shape=(data_dim,))
-    dummy_model = Model(input_tensor, dense_layer(input_tensor))
+    import keras
+from keras import losses
+from keras import backend as K
+from keras.utils.generic_utils import custom_object_scope
     
-        # Arguments
-        y: class vector to be converted into a matrix
-            (integers from 0 to num_classes).
-        num_classes: total number of classes.
-        dtype: The data type expected by the input, as a string
-            (`float32`, `float64`, `int32`...)
-    
-        target_devices = ['/cpu:0'] + ['/gpu:%d' % i for i in target_gpu_ids]
-    for device in target_devices:
-        if device not in available_devices:
-            raise ValueError(
-                'To call `multi_gpu_model` with `gpus=%s`, '
-                'we expect the following devices to be available: %s. '
-                'However this machine only has: %s. '
-                'Try reducing `gpus`.' % (gpus,
-                                          target_devices,
-                                          available_devices))
-    
-        # To prevent a slowdown,
-    # we find beforehand the arrays that need conversion.
-    feed = (model._feed_inputs +
-            model._feed_targets +
-            model._feed_sample_weights)
-    indices_for_conversion_to_dense = []
-    for i in range(len(feed)):
-        if issparse(fit_inputs[i]) and not K.is_sparse(feed[i]):
-            indices_for_conversion_to_dense.append(i)
+        model = create_multi_input_model_from(dummy_model, dummy_model)
+    model.compile(loss='categorical_crossentropy', optimizer='sgd')
+    assert len(model.losses) == 6
     
     print('Pad sequences (samples x time)')
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
@@ -91,331 +54,11 @@ print('x_test shape:', x_test.shape)
 y_train = np.array(y_train)
 y_test = np.array(y_test)
     
-        if state == 'absent':
-        if not snapshot_id:
-            module.fail_json(msg='snapshot_id must be set when state is absent')
-        try:
-            ec2.delete_snapshot(snapshot_id)
-        except boto.exception.BotoServerError as e:
-            # exception is raised if snapshot does not exist
-            if e.error_code == 'InvalidSnapshot.NotFound':
-                module.exit_json(changed=False)
-            else:
-                module.fail_json(msg='%s: %s' % (e.error_code, e.error_message))
+    train_model.compile(optimizer=keras.optimizers.RMSprop(lr=2e-3, decay=1e-5),
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'],
+                    target_tensors=[targets])
+train_model.summary()
     
-        '''Get an elasticache connection'''
-    try:
-        conn = connect_to_region(region_name=region, **aws_connect_kwargs)
-    except boto.exception.NoAuthHandlerFound as e:
-        module.fail_json(msg=e.message)
-    
-    
-DOCUMENTATION = '''
----
-module: iam_server_certificate_facts
-short_description: Retrieve the facts of a server certificate
-description:
-  - Retrieve the attributes of a server certificate
-version_added: '2.2'
-author: 'Allen Sanabria (@linuxdynasty)'
-requirements: [boto3, botocore]
-options:
-  name:
-    description:
-      - The name of the server certificate you are retrieving attributes for.
-    required: true
-extends_documentation_fragment:
-    - aws
-    - ec2
-'''
-    
-            # manage host state
-        desired_state = self.get_parameter('state')
-        if bool(host):
-            current_state = host.STATE
-            current_state_name = HOST_STATES(host.STATE).name
-        else:
-            current_state = HOST_ABSENT
-            current_state_name = 'ABSENT'
-    
-        module.exit_json(changed=changed, parameter=parameter_name, ansible_facts={'vertica_configuration': configuration_facts})
-    
-    
-def update_roles(role_facts, cursor, role,
-                 existing, required):
-    for assigned_role in set(existing) - set(required):
-        cursor.execute('revoke {0} from {1}'.format(assigned_role, role))
-    for assigned_role in set(required) - set(existing):
-        cursor.execute('grant {0} to {1}'.format(assigned_role, role))
-    
-    
-DOCUMENTATION = '''
----
-module: vertica_schema
-version_added: '2.0'
-short_description: Adds or removes Vertica database schema and roles.
-description:
-  - Adds or removes Vertica database schema and, optionally, roles
-    with schema access privileges.
-  - A schema will not be removed until all the objects have been dropped.
-  - In such a situation, if the module tries to remove the schema it
-    will fail and only remove roles created for the schema if they have
-    no dependencies.
-options:
-  name:
-    description:
-      - Name of the schema to add or remove.
-    required: true
-  usage_roles:
-    description:
-      - Comma separated list of roles to create and grant usage access to the schema.
-    aliases: ['usage_role']
-  create_roles:
-    description:
-      - Comma separated list of roles to create and grant usage and create access to the schema.
-    aliases: ['create_role']
-  owner:
-    description:
-      - Name of the user to set as owner of the schema.
-  state:
-    description:
-      - Whether to create C(present), or drop C(absent) a schema.
-    default: present
-    choices: ['present', 'absent']
-  db:
-    description:
-      - Name of the Vertica database.
-  cluster:
-    description:
-      - Name of the Vertica cluster.
-    default: localhost
-  port:
-    description:
-      - Vertica cluster port to connect to.
-    default: 5433
-  login_user:
-    description:
-      - The username used to authenticate with.
-    default: dbadmin
-  login_password:
-    description:
-      - The password used to authenticate with.
-notes:
-  - The default authentication assumes that you are either logging in as or sudo'ing
-    to the C(dbadmin) account on the host.
-  - This module uses C(pyodbc), a Python ODBC database adapter. You must ensure
-    that C(unixODBC) and C(pyodbc) is installed on the host and properly configured.
-  - Configuring C(unixODBC) for Vertica requires C(Driver = /opt/vertica/lib64/libverticaodbc.so)
-    to be added to the C(Vertica) section of either C(/etc/odbcinst.ini) or C($HOME/.odbcinst.ini)
-    and both C(ErrorMessagesPath = /opt/vertica/lib64) and C(DriverManagerEncoding = UTF-16)
-    to be added to the C(Driver) section of either C(/etc/vertica.ini) or C($HOME/.vertica.ini).
-requirements: [ 'unixODBC', 'pyodbc' ]
-author: 'Dariusz Owczarek (@dareko)'
-'''
-    
-    RETURN = r'''
-#
-'''
-    
-            if service is not None:
-            changed = client.modify_if_diff(name, ipa_hbacrule.get('memberservice_hbacsvc', []), service,
-                                            client.hbacrule_add_service,
-                                            client.hbacrule_remove_service, 'hbacsvc') or changed
-    
-    # Copyright (c) 2016, Loic Blot <loic.blot@unix-experience.fr>
-# Copyright (c) 2018, Ansible Project
-# Sponsored by Infopro Digital. http://www.infopro-digital.com/
-# Sponsored by E.T.A.I. http://www.etai.fr/
-#
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-    
-    from ansible.module_utils.basic import AnsibleModule
-    
-        #------------------------------------------------------------
-    # varying k
-    k_results_build = dict([(alg, np.zeros(len(krange)))
-                            for alg in algorithms])
-    k_results_query = dict([(alg, np.zeros(len(krange)))
-                            for alg in algorithms])
-    
-            plt.subplot(m, 2, i + 1)
-        plt.plot(list_n_samples, np.sqrt(elnet_results[:, j, 1]),
-                 label='ElasticNet')
-        plt.plot(list_n_samples, np.sqrt(sgd_results[:, j, 1]),
-                 label='SGDRegressor')
-        plt.plot(list_n_samples, np.sqrt(asgd_results[:, j, 1]),
-                 label='A-SGDRegressor')
-        plt.plot(list_n_samples, np.sqrt(ridge_results[:, j, 1]),
-                 label='Ridge')
-        plt.legend(prop={'size': 10})
-        plt.xlabel('n_train')
-        plt.ylabel('Time [sec]')
-        plt.title('Training time - %d features' % list_n_features[j])
-        i += 1
-    
-        Example: ::
-    
-    We selected two sets of two variables from the Boston housing data set
-as an illustration of what kind of analysis can be done with several
-outlier detection tools. For the purpose of visualization, we are working
-with two-dimensional examples, but one should be aware that things are
-not so trivial in high-dimension, as it will be pointed out.
-    
-            if self.continue_fail_count > 10:
-            # don't set network_stat to 'unknown', wait for check
-            # network_stat = 'unknown'
-            xlog.debug('report_connect_fail %s continue_fail_count:%d',
-                       self.type, self.continue_fail_count)
-            self.triger_check_network(True)
-    
-        Any state that has a semantic predicate edge is special; those states
-    are generated with if-then-else structures in a specialStateTransition()
-    which is generated by cyclicDFA template.
-    
-    '''
-    
-    def __init__(
-        self,
-        recognizer, decisionNumber,
-        eot, eof, min, max, accept, special, transition
-        ):
-        ## Which recognizer encloses this DFA?  Needed to check backtracking
-        self.recognizer = recognizer
-    
-            #type_ = ''
-        #size = 0
-    
-    	for video in tab.childNodes:
-		if re.search(contentid, video.attributes['link'].value):
-			url = video.attributes['flv'].value
-			break
-    
-        print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([video_url], title, ext, size, output_dir, merge=merge, headers = fake_headers)
-    
-        if title is None:
-      title = url[0]
-    
-        parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
-    parser.add_argument('-d', action='store', dest='directory')
-    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
-    parser.add_argument('--overwrite', action='store_true', default = False)    
-    results = parser.parse_args()
-    
-    
-def _raw_face_landmarks(face_image, face_locations=None, model='large'):
-    if face_locations is None:
-        face_locations = _raw_face_locations(face_image)
-    else:
-        face_locations = [_css_to_rect(face_location) for face_location in face_locations]
-    
-    # Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
-known_face_names = [
-    'Barack Obama',
-    'Joe Biden'
-]
-    
-        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
-    context = multiprocessing
-    if 'forkserver' in multiprocessing.get_all_start_methods():
-        context = multiprocessing.get_context('forkserver')
-    
-        # Let's trace out each facial feature in the image with a line!
-    for facial_feature in face_landmarks.keys():
-        d.line(face_landmarks[facial_feature], width=5)
-    
-    setup(
-    name='face_recognition',
-    version='1.2.3',
-    description='Recognize faces from Python or from the command line',
-    long_description=readme + '\n\n' + history,
-    author='Adam Geitgey',
-    author_email='ageitgey@gmail.com',
-    url='https://github.com/ageitgey/face_recognition',
-    packages=[
-        'face_recognition',
-    ],
-    package_dir={'face_recognition': 'face_recognition'},
-    package_data={
-        'face_recognition': ['models/*.dat']
-    },
-    entry_points={
-        'console_scripts': [
-            'face_recognition=face_recognition.face_recognition_cli:main',
-            'face_detection=face_recognition.face_detection_cli:main'
-        ]
-    },
-    install_requires=requirements,
-    license='MIT license',
-    zip_safe=False,
-    keywords='face_recognition',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
-
-    
-        def amount(self, val):
-        print(val, end=' ')
-        return self
-    
-        def detach(self, observer):
-        try:
-            self._observers.remove(observer)
-        except ValueError:
-            pass
-    
-    '''
-*What is this pattern about?
-This pattern aims to encapsulate each algorithm and allow them to be
-interchangeable. Separating algorithms allows the client to scale
-with larger and more complex algorithms, since the client and the
-strategies are kept independent of each other.
-    
-    ### OUTPUT ###
-# We have a lovely Cat
-# It says meow
-#
-# We have a lovely Dog
-# It says woof
-# ====================
-# We have a lovely Cat
-# It says meow
-# ====================
-# We have a lovely Cat
-# It says meow
-# ====================
-
-    
-    
-def main():
-    try:
-        import queue
-    except ImportError:  # python 2.x compatibility
-        import Queue as queue
-    
-        def get_current_time_as_html_fragment(self):
-        current_time = self.time_provider.now()
-        current_time_as_html_fragment = '<span class=\'tinyBoldText\'>{}</span>'.format(current_time)
-        return current_time_as_html_fragment
-'''
-    
-    ''
+    def print_title(title, pattern = '-'):
+    print('\n'.join(('', title, pattern * len(title)))) 
