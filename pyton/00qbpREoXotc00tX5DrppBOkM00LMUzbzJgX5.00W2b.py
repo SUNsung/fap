32299f98671acb517e78a484689d7dd0261a195d
SUNsung <weatherius@gmail.com>
2019-02-02 12:04:45 +0200

    Rewrite [00qbpREoXotc00tX5DrppBOkM00LMUzbzJgX5.00W2b.py] File in: 2019-02-02 12:04:44

--- pyton/00qbpREoXotc00tX5DrppBOkM00LMUzbzJgX5.00W2b.py
+++ pyton/00qbpREoXotc00tX5DrppBOkM00LMUzbzJgX5.00W2b.py
@@ -1,112 +1,123 @@
 
         
-            def delete(self, session_key=None):
+        
+MOD_SSL_CONF_DEST = 'options-ssl-nginx.conf'
+'''Name of the mod_ssl config file as saved in `IConfig.config_dir`.'''
+    
+        def setUp(self):
+        from acme.errors import MissingNonce
+        self.response = mock.MagicMock(headers={})
+        self.response.request.method = 'FOO'
+        self.error = MissingNonce(self.response)
+    
         '''
-        To delete, clear the session key and the underlying data structure
-        and set the modified flag so that the cookie is set on the client for
-        the current request.
-        '''
-        self._session_key = ''
-        self._session_cache = {}
-        self.modified = True
+    return_vars = {}
+    # Get list of words in the variable
+    a_opts = util.get_var_from_file(varname, filepath).split()
+    for i, v in enumerate(a_opts):
+        # Handle Define statements and make sure it has an argument
+        if v == '-D' and len(a_opts) >= i+2:
+            var_parts = a_opts[i+1].partition('=')
+            return_vars[var_parts[0]] = var_parts[2]
+        elif len(v) > 2 and v.startswith('-D'):
+            # Found var with no whitespace separator
+            var_parts = v[2:].partition('=')
+            return_vars[var_parts[0]] = var_parts[2]
+    return return_vars
     
-        dispatch = {
-        'call': trace_call,
-        'exception': trace_exception,
-        'return': trace_return,
-        'c_call': trace_pass,
-        'c_return': trace_pass,
-        'c_exception': trace_pass,
-        }
+            :param int rollback: Number of checkpoints to revert
     
+            self.assertEqual(None, self._call(self.vhosts))
     
-def main():
-    parser = ArgumentParser(description='''\
-Send the contents of a directory as a MIME message.
-Unless the -o option is given, the email is sent by forwarding to your local
-SMTP server, which then does the normal delivery process.  Your local machine
-must be running an SMTP server.
-''')
-    parser.add_argument('-d', '--directory',
-                        help='''Mail the contents of the specified directory,
-                        otherwise use the current directory.  Only the regular
-                        files in the directory are sent, and we don't recurse to
-                        subdirectories.''')
-    parser.add_argument('-o', '--output',
-                        metavar='FILE',
-                        help='''Print the composed message to FILE instead of
-                        sending the message to the SMTP server.''')
-    parser.add_argument('-s', '--sender', required=True,
-                        help='The value of the From: header (required)')
-    parser.add_argument('-r', '--recipient', required=True,
-                        action='append', metavar='RECIPIENT',
-                        default=[], dest='recipients',
-                        help='A To: header value (at least one required)')
-    args = parser.parse_args()
-    directory = args.directory
-    if not directory:
-        directory = '.'
-    # Create the message
-    msg = EmailMessage()
-    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
-    msg['To'] = ', '.join(args.recipients)
-    msg['From'] = args.sender
-    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+            self.assertEqual(mock_setup_cert.call_count, 2)
     
-        theplatform_download_by_pid(pid, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    # How to display URL addresses: 'footnote', 'no', or 'inline'.
+#texinfo_show_urls = 'footnote'
     
-            #type_, ext, size = url_info(url)
-        #print_info(site_info, title, type_, size)
-        #if not info_only:
-            #download_urls([url], title, ext, total_size=None, output_dir=output_dir, merge=merge)
+    PROJ_NAME = 'you-get'
+PACKAGE_NAME = 'you_get'
     
+    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    title = r1(r'<title>(.*)</title>', html)
+    assert title
+    id = r1(r'flvid\s*=\s*(\d+)', html)
+    assert id
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
     
-'''
-http://open.iqiyi.com/lib/player.html
-'''
-iqiyi_patterns = [r'(?:\'|\')(https?://dispatcher\.video\.qiyi\.com\/disp\/shareplayer\.swf\?.+?)(?:\'|\')',
-                  r'(?:\'|\')(https?://open\.iqiyi\.com\/developer\/player_js\/coopPlayerIndex\.html\?.+?)(?:\'|\')']
+            for i in html_json['sources']:
+            if 'src' in i:  #to avoid KeyError
+                if i['src'].startswith('https'):
+                    link_list.append((str(i['height']), i['src']))
     
-        title = match1(html, r'&title=([^&]+)')
+    #----------------------------------------------------------------------
+#helper
+#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
+def dictify(r,root=True):
+    if root:
+        return {r.tag : dictify(r, False)}
+    d=copy(r.attrib)
+    if r.text:
+        d['_text']=r.text
+    for x in r.findall('./*'):
+        if x.tag not in d:
+            d[x.tag]=[]
+        d[x.tag].append(dictify(x,False))
+    return d
     
-            page = get_content(url)
-        src = re.findall(r'src='(.+?)'', page)
-        js = [path for path in src if path.endswith('.js')]
+    	for video in tab.childNodes:
+		if re.search(contentid, video.attributes['link'].value):
+			url = video.attributes['flv'].value
+			break
     
-    site_info = 'Giphy.com'
-download = giphy_download
-download_playlist = playlist_not_supported('giphy')
-
+                # compare date_added
+            date_diff = None
+            dates = [release.date_added for release in releases]
+            if dates:
+                diff = (max(dates) - min(dates)).total_seconds()
+                if date_diff is None or diff > date_diff:
+                    date_diff = diff
     
+            for renv in dupe_release_envs:
+            release_id = renv['release_id']
+            organization_id = renv['organization_id']
+            environment_id = renv['environment_id']
+            renvs = list(
+                orm.ReleaseEnvironment.objects.filter(
+                    release_id=release_id,
+                    organization_id=organization_id,
+                    environment_id=environment_id,
+                ).order_by('first_seen')
+            )
+            to_renv = renvs[0]
+            from_renvs = renvs[1:]
+            last_seen = max([re.last_seen for re in renvs])
+            orm.ReleaseEnvironment.objects.filter(
+                id=to_renv.id,
+            ).update(last_seen=last_seen)
+            orm.ReleaseEnvironment.objects.filter(
+                id__in=[re.id for re in from_renvs],
+            ).delete()
     
-if __name__ == '__main__':
-    # Create our localizers
-    e, g = get_localizer(language='English'), get_localizer(language='Greek')
-    # Localize some text
-    for msgid in 'dog parrot cat bear'.split():
-        print(e.get(msgid), g.get(msgid))
-    
-    *TL;DR80
-Allows object composition to achieve the same code reuse as inheritance.
-'''
-    
-        def __init__(self, param):
-        # simple test to validate param value
-        if param in self._class_method_choices:
-            self.param = param
-        else:
-            raise ValueError('Invalid Value for Param: {0}'.format(param))
-    
-        def undo(self):
-        self.rename(self.dest, self.src)
-    
-    
-def execute_replacement2(self):
-    print(self.name + ' from execute 2')
-    
-    '''
-Lazily-evaluated property pattern in Python.
-    
-    
-class UnsupportedTransition(BaseException):
-    pass
+            # Adding model 'ApiAuthorization'
+        db.create_table(
+            'sentry_apiauthorization', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'application', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.ApiApplication'], null=True
+                    )
+                ), (
+                    'user', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.User']
+                    )
+                ), ('scopes', self.gf('django.db.models.fields.BigIntegerField')(default=None)), (
+                    'date_added',
+                    self.gf('django.db.models.fields.DateTimeField')()
+                ),
+            )
+        )
+        db.send_create_signal('sentry', ['ApiAuthorization'])
