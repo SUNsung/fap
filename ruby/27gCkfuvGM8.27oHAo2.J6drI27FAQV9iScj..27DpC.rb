9a52a65dce1f225f2e6068c5cc53555c168d9905
SUNsung <weatherius@gmail.com>
2019-02-02 16:42:02 +0200

    Rewrite [27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb] File in: 2019-02-02 16:42:01

--- ruby/27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb
+++ ruby/27gCkfuvGM8.27oHAo2.J6drI27FAQV9iScj..27DpC.rb
@@ -1,127 +1,157 @@
 
         
-                        @env.machine_names.each do |machine_name|
-                  if machine_name =~ regex
-                    machines << get_machine.call(machine_name)
-                  end
-                end
-    
-            # This is called early, before a machine is instantiated, to check
-        # if this provider is installed. This should return true or false.
-        #
-        # If the provider is not installed and Vagrant determines it is
-        # able to install this provider, then it will do so. Installation
-        # is done by calling Environment.install_provider.
-        #
-        # If Environment.can_install_provider? returns false, then an error
-        # will be shown to the user.
-        def self.installed?
-          # By default return true for backwards compat so all providers
-          # continue to work.
-          true
+                lounge.topic_id = post.topic.id
+        unless lounge.save
+          puts lounge.errors.full_messages
+          puts 'Failed to set the lounge description topic!'
         end
     
-        def __internal_state
-      {
-        items: @items,
-        results_cache: @results_cache
-      }
+      before do
+    login_as(user)
+  end
+    
+              @bar2 = Agents::DotBar.new(name: 'bar2').tap { |agent|
+            agent.user = users(:bob)
+            agent.sources << @foo
+            agent.propagate_immediately = true
+            agent.disabled = true
+            agent.save!
+          },
+    
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
+  end
+    
+              agent_diffs = scenario_import.agent_diffs
+          weather_agent_diff = agent_diffs[0]
+          trigger_agent_diff = agent_diffs[1]
+    
+      def guid_order(agent_list, agent_name)
+    agent_list.map{|a|a.guid}.sort.find_index(agents(agent_name).guid)
+  end
+end
+
+    
+        def ==(other)
+      other.is_a?(self.class) && filters == other.filters
+    end
+    
+        def initialize
+      super
+      initialize_stubs
+    end
+    
+        def log(msg)
+      puts '\r' + justify(msg)
+    end
+    
+            doc
+      end
     end
   end
 end
 
     
-      def load_logs(dir)
-    logs = {}
-    if dir.exist?
-      dir.children.sort.each do |file|
-        contents = file.size? ? file.read : 'empty log'
-        # small enough to avoid GitHub 'unicorn' page-load-timeout errors
-        max_file_size = 1_000_000
-        contents = truncate_text_to_approximate_size(contents, max_file_size, front_weight: 0.2)
-        logs[file.basename.to_s] = { content: contents }
+      protected
+    
+        if resource.errors.empty?
+      set_flash_message! :notice, :unlocked
+      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
+    else
+      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+    end
+  end
+    
+    module Devise
+  module Controllers
+    # Provide sign in and sign out functionality.
+    # Included by default in all controllers.
+    module SignInOut
+      # Return true if the given scope is signed in session. If no scope given, return
+      # true if any scope is signed in. This will run authentication hooks, which may
+      # cause exceptions to be thrown from this method; if you simply want to check
+      # if a scope has already previously been authenticated without running
+      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
+      def signed_in?(scope=nil)
+        [scope || Devise.mappings.keys].flatten.any? do |_scope|
+          warden.authenticate?(scope: _scope)
+        end
       end
-    end
-    raise 'No logs.' if logs.empty?
     
-      #
-  # HTTP GET request class wrapper.
-  #
-  class Get < Request
-    def initialize(uri = '/', proto = DefaultProtocol)
-      super('GET', uri, proto)
-    end
-  end
+        # Include the chosen devise modules in your model:
+    #
+    #   devise :database_authenticatable, :confirmable, :recoverable
+    #
+    # You can also give any of the devise configuration values in form of a hash,
+    # with specific values for this model. Please check your Devise initializer
+    # for a complete description on those values.
+    #
+    def devise(*modules)
+      options = modules.extract_options!.dup
     
-      # Returns parsed JavaScript blocks.
-  # The parsed version is a RKelly object that allows you to be able do advanced parsing.
-  #
-  # @see https://github.com/tenderlove/rkelly
-  # @return [Array<RKelly::Nodes::SourceElementsNode>]
-  def get_html_scripts
-    n = get_html_document
-    rkelly = RKelly::Parser.new
-    n.search('//script').map { |s| rkelly.parse(s.text) }
-  end
-    
-              case protocol
-          when 'tcp'
-            self.connection = create_tcp_connection
-          when 'udp'
-            raise ::NotImplementedError, 'Kerberos Client: UDP not supported'
-          else
-            raise ::RuntimeError, 'Kerberos Client: unknown transport protocol'
+                authentication_keys_changed || encrypted_password_changed
           end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Crypto
-        module Rc4Hmac
-          # Decrypts the cipher using RC4-HMAC schema
-          #
-          # @param cipher [String] the data to decrypt
-          # @param key [String] the key to decrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the decrypted cipher
-          # @raise [RuntimeError] if decryption doesn't succeed
-          def decrypt_rc4_hmac(cipher, key, msg_type)
-            unless cipher && cipher.length > 16
-              raise ::RuntimeError, 'RC4-HMAC decryption failed'
+        else
+          def clear_reset_password_token?
+            encrypted_password_changed = respond_to?(:encrypted_password_changed?) && encrypted_password_changed?
+            authentication_keys_changed = self.class.authentication_keys.any? do |attribute|
+              respond_to?('#{attribute}_changed?') && send('#{attribute}_changed?')
             end
     
-              # Decodes the Rex::Proto::Kerberos::Model::KrbError from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KrbError, invalid input'
-            end
+      # Examples
+  #-----------------------------------------------------------------------------#
     
-    Then(/^it creates the file with the remote_task prerequisite$/) do
-  TestApp.linked_files.each do |file|
-    run_vagrant_command(test_file_exists(TestApp.shared_path.join(file)))
-  end
-end
-    
-      def test_symlink_exists(path)
-    exists?('L', path)
-  end
-    
-      def run_vagrant_command(command)
-    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
-    return [stdout, stderr] if status.success?
-    raise VagrantSSHCommandError, status
-  end
-end
-    
-        def name
-      'cap'
+    When /^I append gems from Appraisal Gemfile$/ do
+  File.read(ENV['BUNDLE_GEMFILE']).split(/\n/).each do |line|
+    if line =~ /^gem '(?!rails|appraisal)/
+      append_to_gemfile line.strip
     end
+  end
+end
+    
+    When /^(?:|I )press '([^']*)'$/ do |button|
+  click_button(button)
+end
+    
+      def migration_version
+    if Rails.version.start_with? '5'
+      '[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]'
+    end
+  end
+end
+
+    
+    require 'erb'
+require 'digest'
+require 'tempfile'
+require 'paperclip/version'
+require 'paperclip/geometry_parser_factory'
+require 'paperclip/geometry_detector_factory'
+require 'paperclip/geometry'
+require 'paperclip/processor'
+require 'paperclip/processor_helpers'
+require 'paperclip/tempfile'
+require 'paperclip/thumbnail'
+require 'paperclip/interpolations/plural_cache'
+require 'paperclip/interpolations'
+require 'paperclip/tempfile_factory'
+require 'paperclip/style'
+require 'paperclip/attachment'
+require 'paperclip/storage'
+require 'paperclip/callbacks'
+require 'paperclip/file_command_content_type_detector'
+require 'paperclip/media_type_spoof_detector'
+require 'paperclip/content_type_detector'
+require 'paperclip/glue'
+require 'paperclip/errors'
+require 'paperclip/missing_attachment_styles'
+require 'paperclip/validators'
+require 'paperclip/logger'
+require 'paperclip/helpers'
+require 'paperclip/has_attached_file'
+require 'paperclip/attachment_registry'
+require 'paperclip/filename_cleaner'
+require 'paperclip/rails_environment'
