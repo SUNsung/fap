9e399a3ce2ab8034989fe8851eb41a455dbf02ea
SUNsung <weatherius@gmail.com>
2019-02-02 22:16:05 +0200

    Rewrite [35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp] File in: 2019-02-02 22:16:04

--- cccccc/35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp
+++ cccccc/35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp
@@ -1,157 +1,87 @@
 
         
-        #ifndef DIRECTIONAL_PREPOSITION
-#  define DIRECTIONAL_PREPOSITION(Word) PREPOSITION(Word)
-#endif
-    
-      case Demangle::Node::Kind::Class:
-  case Demangle::Node::Kind::Enum:
-  case Demangle::Node::Kind::Structure:
-  case Demangle::Node::Kind::OtherNominalType: {
-    if (node->getNumChildren() < 2)
-      return node;
+        namespace swig {
+namespace {
+    }
     }
     
     
-    {};
-    
-    		inline float * GetDecodedAlphas(void)
-		{
-			return m_afDecodedAlphas;
-		}
-    
-    		float fLeftGrayErrorSum = 0.0f;
-		float fRightGrayErrorSum = 0.0f;
-		float fTopGrayErrorSum = 0.0f;
-		float fBottomGrayErrorSum = 0.0f;
-    
-      FT_LOCAL( void )
-  af_latin_hints_link_segments( AF_GlyphHints  hints,
-                                FT_UInt        width_count,
-                                AF_WidthRec*   widths,
-                                AF_Dimension   dim );
-    
-       - Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
-    
-       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-    
-    //////////////////////////////////////////////////////////////////////
-    
-      const char* data = filename.data();
-  int data_len = filename.length();
-  bool base64 = false;
-  if (strncmp(data, 'data:', sizeof('data:') - 1)) {
-    return nullptr;
+    {  DCHECK(PyDict_Check(code_to_exc_type_map));
+  PyObject* key;
+  PyObject* value;
+  Py_ssize_t pos = 0;
+  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
+    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
+    singleton_->exc_types_[code] = value;
+    // The exception classes should also have the lifetime of the process, but
+    // incref just in case.
+    Py_INCREF(value);
   }
-  data += sizeof('data:') - 1;
-  data_len -= sizeof('data:') - 1;
-    
-    
-    {  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-};
-    
-    public:
-  static void Add(InfoVec& out, const char* name, const std::string& value);
-  static void AddServerStats(InfoVec& out, const char* name,
-                             const char* statsName = nullptr);
-    
-    #ifndef HPHP_FILE_STREAM_WRAPPER_H
-#define HPHP_FILE_STREAM_WRAPPER_H
-    
-      for (auto alphabet_size : test_cases) {
-    for (int i = 0; i < repetitions; i++) {
-      std::vector<int> input(num_elements);
-      std::generate(input.begin(), input.end(),
-        [=]() { return rand() % alphabet_size; });
-      CompressedBufferWriter cbw(alphabet_size);
-    }
-    }
-    
-    SEXP XGBoosterUpdateOneIter_R(SEXP handle, SEXP iter, SEXP dtrain) {
-  R_API_BEGIN();
-  CHECK_CALL(XGBoosterUpdateOneIter(R_ExternalPtrAddr(handle),
-                                  asInteger(iter),
-                                  R_ExternalPtrAddr(dtrain)));
-  R_API_END();
-  return R_NilValue;
 }
     
-        /** Creates an action with a Cardinal Spline array of points and tension.
-     * @code
-     * When this function bound to js or lua,the input params are changed.
-     * In js: var create(var t,var table).
-     * In lua: local create(local t, local table).
-     * @param duration In seconds.
-     * @param point An PointArray.
-     * @param tension Goodness of fit.
-     * @endcode
-     */
-    static CardinalSplineBy* create(float duration, PointArray* points, float tension);
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
     
-    bool DeccelAmplitude::initWithAction(Action *action, float duration)
-{
-    if (ActionInterval::initWithDuration(duration))
-    {
-        _rate = 1.0f;
-        _other = (ActionInterval*)(action);
-        action->retain();
-    }
+    
+    {}  // end namespace tensorflow
+    
+    string TryFindKernelClass(const string& serialized_node_def) {
+  tensorflow::NodeDef node_def;
+  if (!node_def.ParseFromString(serialized_node_def)) {
+    LOG(WARNING) << 'Error parsing node_def';
+    return '';
+  }
     }
     
-        /**
-    @brief Initializes the action with center position, number of twirls, amplitude, a grid size and duration.
-    @param duration Specify the duration of the Twirl action. It's a value in seconds.
-    @param gridSize Specify the size of the grid.
-    @param position Specify the center position of the twirl action.
-    @param twirls Specify the twirls count of the Twirl action.
-    @param amplitude Specify the amplitude of the Twirl action.
-    @return If the initialization success, return true; otherwise, return false.
-    */
-    bool initWithDuration(float duration, const Size& gridSize, const Vec2& position, unsigned int twirls, float amplitude);
     
-    
-    {private:
-    CC_DISALLOW_COPY_AND_ASSIGN(Sequence);
+    { private:
+  unsigned flags_;
 };
     
+    void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
+  // Prepare all the blobs.
+  GenericVector<BlobData> blobs;
+  for (int w = 0; w < words.size(); ++w) {
+    if (words[w].word->ratings != nullptr &&
+        words[w].word->ratings->get(0, 0) == nullptr) {
+      for (int s = 0; s < words[w].lang_words.size(); ++s) {
+        Tesseract* sub = s < sub_langs_.size() ? sub_langs_[s] : this;
+        const WERD_RES& word = *words[w].lang_words[s];
+        for (int b = 0; b < word.chopped_word->NumBlobs(); ++b) {
+          blobs.push_back(BlobData(b, sub, word));
+        }
+      }
+    }
+  }
+  // Pre-classify all the blobs.
+  if (tessedit_parallelize > 1) {
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(10)
+#endif  // _OPENMP
+    for (int b = 0; b < blobs.size(); ++b) {
+      *blobs[b].choices =
+          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    }
+  } else {
+    // TODO(AMD) parallelize this.
+    for (int b = 0; b < blobs.size(); ++b) {
+      *blobs[b].choices =
+          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    }
+  }
+}
     
-    {    /** how many times the animation is going to loop. 0 means animation is not animated. 1, animation is executed one time, ... */
-    unsigned int _loops;
     
-private:
-    CC_DISALLOW_COPY_AND_ASSIGN(Animation);
+    {  bool rword_indicates_list_item;
+  bool rword_likely_starts_idea;
+  bool rword_likely_ends_idea;
 };
     
-    NS_CC_END
-
-    
-    /**
- * PolygonInfo is an object holding the required data to display Sprites.
- * It can be a simple as a triangle, or as complex as a whole 3D mesh
- */
-class CC_DLL PolygonInfo
-{
-public:
-    /// @name Creators
-    /// @{
-    /**
-     * Creates an empty Polygon info
-     * @memberof PolygonInfo
-     * @return PolygonInfo object
-     */
-    PolygonInfo();
-    }
-    }
+      // Gets a VC object identified by its ID.
+  static ParamContent* GetParamContentById(int id);
