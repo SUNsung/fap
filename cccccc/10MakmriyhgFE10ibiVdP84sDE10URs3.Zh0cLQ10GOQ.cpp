ef02fd17da2d0a38fef6a587c77463f7300e4109
SUNsung <weatherius@gmail.com>
2019-02-03 10:08:33 +0200

    Rewrite [10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp] File in: 2019-02-03 10:08:32

--- cccccc/10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp
+++ cccccc/10MakmriyhgFE10ibiVdP84sDE10URs3.Zh0cLQ10GOQ.cpp
@@ -1,197 +1,264 @@
 
         
-          // If the conforming type is a class, add a class-constrained 'Self'
-  // parameter.
-  if (covariantSelf) {
-    auto paramTy = GenericTypeParamType::get(/*depth=*/0, /*index=*/0, ctx);
-    builder.addGenericParameter(paramTy);
-  }
-    
-            // Don't prune redundant type information from the base name if
-        // there is a corresponding property (either singular or plural).
-        StringRef removedText =
-          name.substr(nameWordRevIter.base().getPosition(),
-                      firstMatchingNameWordRevIter.base().getPosition());
-        if (textMatchesPropertyName(removedText, allPropertyNames))
-          return name;
-    
-    %# Ignore the following admonition; it applies to the resulting .cpp file only
-//// Automatically Generated From UnicodeExtendedGraphemeClusters.cpp.gyb.
-//// Do Not Edit Directly!
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
-    
-    
-    {public:
-  Darwin(const Driver &D, const llvm::Triple &Triple) : ToolChain(D, Triple) {}
-  ~Darwin() = default;
-  std::string sanitizerRuntimeLibName(StringRef Sanitizer,
-                                      bool shared = true) const override;
-};
-    
-    // Get render process host.
-RenderProcessHost* GetRenderProcessHost() {
-  RenderProcessHost* render_process_host = NULL;
-  std::vector<Shell*> windows = Shell::windows();
-  for (size_t i = 0; i < windows.size(); ++i) {
-    if (!windows[i]->is_devtools()) {
-      render_process_host = windows[i]->web_contents()->GetRenderProcessHost();
-      break;
-    }
-  }
+        class InAppPurchase : public mate::EventEmitter<InAppPurchase>,
+                      public in_app_purchase::TransactionObserver {
+ public:
+  static mate::Handle<InAppPurchase> Create(v8::Isolate* isolate);
     }
     
-      static void Call(content::Shell* shell,
-                   const std::string& method,
-                   const base::ListValue& arguments,
-                   base::ListValue* result,
-                   DispatcherHost* dispatcher_host);
+    namespace atom {
+    }
     
-    MenuItem::~MenuItem() {
-  Destroy();
+      // net::URLRequestJobFactory::ProtocolHandler:
+  net::URLRequestJob* MaybeCreateJob(
+      net::URLRequest* request,
+      net::NetworkDelegate* network_delegate) const override;
+  bool IsSafeRedirectTarget(const GURL& location) const override;
+    
+    
+    {}  // namespace asar
+    
+    io::ZeroCopyOutputStream* GeneratorContext::OpenForInsert(
+    const string& filename, const string& insertion_point) {
+  GOOGLE_LOG(FATAL) << 'This GeneratorContext does not support insertion.';
+  return NULL;  // make compiler happy
 }
     
-    void MenuItem::SetLabel(const std::string& label) {
-  label_ = label;
-  gtk_menu_item_set_label(GTK_MENU_ITEM(menu_item_), label.c_str());
+    ImmutableExtensionLiteGenerator::ImmutableExtensionLiteGenerator(
+    const FieldDescriptor* descriptor, Context* context)
+  : descriptor_(descriptor), context_(context),
+    name_resolver_(context->GetNameResolver()) {
+  if (descriptor_->extension_scope() != NULL) {
+    scope_ = name_resolver_->GetImmutableClassName(
+        descriptor_->extension_scope());
+  } else {
+    scope_ = name_resolver_->GetImmutableClassName(descriptor_->file());
+  }
 }
     
-    void NwAppClearCacheFunction::OnBrowsingDataRemoverDone() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  run_loop_.Quit();
-}
-    
-     protected:
-  ~NwClipboardGetListSyncFunction() override;
-    
-    #include 'extensions/browser/extension_function.h'
-    
-    bool NwObjDestroyFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  int id = 0;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+      printer->Print(
+    'java.lang.String[] descriptorData = {\n');
+  printer->Indent();
     }
     
-    enum GARBAGE_LEVEL
-{
-  G_NEVER_CRUNCH,
-  G_OK,
-  G_DODGY,
-  G_TERRIBLE
-};
+      // #import any headers for 'public imports' in the proto file.
+  {
+    ImportWriter import_writer(
+        options_.generate_for_named_framework,
+        options_.named_framework_to_proto_path_mappings_path,
+        is_bundled_proto_);
+    const string header_extension(kHeaderExtension);
+    for (int i = 0; i < file_->public_dependency_count(); i++) {
+      import_writer.AddFile(file_->public_dependency(i), header_extension);
+    }
+    import_writer.Print(printer);
+  }
     
-    namespace tesseract {
+      EXPECT_EXIT(TextFormatDecodeData::DecodeDataForString('', ''),
+              ::testing::KilledBySignal(SIGABRT),
+              'error: got empty string for making TextFormat data, input:');
+  EXPECT_EXIT(TextFormatDecodeData::DecodeDataForString('a', ''),
+              ::testing::KilledBySignal(SIGABRT),
+              'error: got empty string for making TextFormat data, input:');
+  EXPECT_EXIT(TextFormatDecodeData::DecodeDataForString('', 'a'),
+              ::testing::KilledBySignal(SIGABRT),
+              'error: got empty string for making TextFormat data, input:');
+    
+    // Returns a string identical to *input except that the character pointed to
+// by radix_pos (which should be '.') is replaced with the locale-specific
+// radix character.
+string LocalizeRadix(const char* input, const char* radix_pos) {
+  // Determine the locale-specific radix character by calling sprintf() to
+  // print the number 1.5, then stripping off the digits.  As far as I can
+  // tell, this is the only portable, thread-safe way to get the C library
+  // to divuldge the locale's radix character.  No, localeconv() is NOT
+  // thread-safe.
+  char temp[16];
+  int size = sprintf(temp, '%.1f', 1.5);
+  GOOGLE_CHECK_EQ(temp[0], '1');
+  GOOGLE_CHECK_EQ(temp[size-1], '5');
+  GOOGLE_CHECK_LE(size, 6);
     }
     
-    #include 'paramsd.h'
-#include <cstdio>            // for fclose, fopen, fprintf, sprintf, FILE
-#include <cstdlib>           // for atoi, strtod
-#include <cstring>           // for strcmp, strcspn, strlen, strncpy
-#include <map>               // for map, _Rb_tree_iterator, map<>::iterator
-#include <memory>            // for unique_ptr
-#include <utility>           // for pair
-#include 'genericvector.h'   // for GenericVector
-#include 'params.h'          // for ParamsVectors, StringParam, BoolParam
-#include 'scrollview.h'      // for SVEvent, ScrollView, SVET_POPUP
-#include 'svmnode.h'         // for SVMenuNode
-#include 'tesseractclass.h'  // for Tesseract
+    template <typename T> struct TypeTraits;
+template <> struct TypeTraits< u8> { typedef u16 wide;                     typedef  u8 unsign; typedef  uint8x16_t vec128; };
+template <> struct TypeTraits< s8> { typedef s16 wide;                     typedef  u8 unsign; typedef   int8x16_t vec128; };
+template <> struct TypeTraits<u16> { typedef u32 wide; typedef  u8 narrow; typedef u16 unsign; typedef  uint16x8_t vec128; };
+template <> struct TypeTraits<s16> { typedef s32 wide; typedef  s8 narrow; typedef u16 unsign; typedef   int16x8_t vec128; };
+template <> struct TypeTraits<u32> { typedef u64 wide; typedef u16 narrow; typedef u32 unsign; typedef  uint32x4_t vec128; };
+template <> struct TypeTraits<s32> { typedef s64 wide; typedef s16 narrow; typedef u32 unsign; typedef   int32x4_t vec128; };
+template <> struct TypeTraits<f32> { typedef f64 wide;                                         typedef float32x4_t vec128; };
     
+    #ifdef CAROTENE_NEON
     
-    {  // Restore the pointer to original blamer bundle and combine blamer
-  // information recorded in the splits.
-  if (orig_bb != nullptr) {
-    orig_bb->JoinBlames(*word->blamer_bundle, *word2->blamer_bundle,
-                        wordrec_debug_blamer);
-    delete word->blamer_bundle;
-    word->blamer_bundle = orig_bb;
-  }
-  word->SetupBoxWord();
-  word->reject_map.initialise(word->box_word->length());
-  delete word2;
-}
+    #define CONTSRC2 dstStride == src0Stride && \
+                 dstStride == src1Stride &&
+#define CONTSRC3 dstStride == src0Stride && \
+                 dstStride == src1Stride && \
+                 dstStride == src2Stride &&
+#define CONTSRC4 dstStride == src0Stride && \
+                 dstStride == src1Stride && \
+                 dstStride == src2Stride && \
+                 dstStride == src3Stride &&
     
-     protected:
-  /// Clone or other copy of the source Pix.
-  /// The pix will always be PixDestroy()ed on destruction of the class.
-  Pix*                 pix_;
-    
-      // Fits a line to the points, returning the fitted line as a pair of
-  // points, and the upper quartile error.
-  double Fit(ICOORD* pt1, ICOORD* pt2) {
-    return Fit(0, 0, pt1, pt2);
-  }
-  // Fits a line to the points, ignoring the skip_first initial points and the
-  // skip_last final points, returning the fitted line as a pair of points,
-  // and the upper quartile error.
-  double Fit(int skip_first, int skip_last, ICOORD* pt1, ICOORD* pt2);
-    
-      // Computes the maximum x and y value in the features.
-  static void ComputeSize(const GenericVector<WordFeature>& features,
-                          int* max_x, int* max_y);
-  // Draws the features in the given window.
-  static void Draw(const GenericVector<WordFeature>& features,
-                   ScrollView* window);
-    
-      // Adds an element with a weight of 1.
-  void add(double x, double y);
-  // Adds an element with a specified weight.
-  void add(double x, double y, double weight);
-  // Adds a whole LLSQ.
-  void add(const LLSQ& other);
-  // Deletes an element with a weight of 1.
-  void remove(double x, double y);
-  int32_t count() const {  // no of elements
-    return static_cast<int>(total_weight + 0.5);
-  }
-    
-    namespace b2ConvexDecomp {
-    }
-    
-    			if (fDecodedLength < 0.5f)
-			{
-				return 1.0f;
-			}
-			else if (fDecodedLength == 0.0f)
-			{
-				fDecodedX = 1.0f;
-				fDecodedY = 0.0f;
-				fDecodedZ = 0.0f;
-			}
-			else
-			{
-				fDecodedX /= fDecodedLength;
-				fDecodedY /= fDecodedLength;
-				fDecodedZ /= fDecodedLength;
-			}
-    
-    //  16384 * sqrt(2) * sin(kPi/9) * 2 / 3
-static const tran_high_t sinpi_1_9 = 5283;
-static const tran_high_t sinpi_2_9 = 9929;
-static const tran_high_t sinpi_3_9 = 13377;
-static const tran_high_t sinpi_4_9 = 15212;
+                    v_dst0 = vmlal_n_s16(v_dst0, vget_low_s16(t0_16s), kernelBase[5]);
+                v_dst0 = vmlal_n_s16(v_dst0, vget_low_s16(t1_16s), kernelBase[4]);
+                v_dst0 = vmlal_n_s16(v_dst0, vget_low_s16(t2_16s), kernelBase[3]);
     
     
     {
-    {}}
+    {            if (s[0] < 0 || s[1] < 0)//saturate in case of overflow ~ 4GB of non-zeros...
+            {
+                return 0x7fFFffFF;
+            }
+            result += (s[0] += s[1]);
+            if (s[0] < 0 || result < 0)
+            {
+                return 0x7fFFffFF;
+            }
+        }
+        for (; i < size.width; i++)
+            result += (src[i] != 0)?1:0;
+        if (result < 0)//saturate in case of overflow ~ 4GB of non-zeros...
+        {
+            return 0x7fFFffFF;
+        }
+    }
+    return result;
+#else
+    (void)_size;
+    (void)srcBase;
+    (void)srcStride;
     
-      // Can be used to generate or force a unimplemented opcode exception
-  void unimplemented();
+        Size2D size(_size);
+    if (srcStride == dstStride &&
+        srcStride == rng1Stride &&
+        srcStride == rng2Stride &&
+        srcStride == (ptrdiff_t)(size.width))
+    {
+        size.width *= size.height;
+        size.height = 1;
+    }
+    const size_t width = size.width & ~( 32/sizeof(T) - 1 );
     
-    #endif // HPHP_FILE_STREAM_WRAPPER_H
-
+      // Now, we have that n is odd and n >= 3.
     
-    req::ptr<Directory> GlobStreamWrapper::opendir(const String& path) {
-  const char* prefix = 'glob://';
-  const char* path_str = path.data();
-  int path_len = path.length();
+    /*!
+ * \file graph_attr_types.h
+ * \brief Data structures that can appear in graph attributes.
+ */
+#ifndef MXNET_GRAPH_ATTR_TYPES_H_
+#define MXNET_GRAPH_ATTR_TYPES_H_
+    
+    
+    { private:
+  friend class NDArray;
+  /*! \brief make constructor protected. */
+  Imperative() {
+    if (dmlc::GetEnv('MXNET_EXEC_BULK_EXEC_TRAIN', 1)) {
+      backward_bulk_size_ =  dmlc::GetEnv('MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN', 15);
+    }
+  }
+  /*! \brief find the input/output ndarrays that are needed for backward */
+  void GetBackwardDependency(
+      const nnvm::NodePtr& node,
+      uint32_t num_inputs, uint32_t num_outputs,
+      std::vector<bool> *p_save_inputs,
+      std::vector<bool> *p_save_outputs);
+  /*! \brief indicate whether is training. */
+#if DMLC_CXX11_THREAD_LOCAL
+  static thread_local bool is_train_;
+  static thread_local bool is_recording_;
+#else
+  static MX_THREAD_LOCAL bool is_train_;
+  static MX_THREAD_LOCAL bool is_recording_;
+#endif
+  /*! \brief node count used for naming */
+  std::atomic<uint64_t> node_count_{0};
+  /*! \brief variable count used for naming */
+  std::atomic<uint64_t> variable_count_{0};
+  /*! \brief default backward bulk size */
+  int backward_bulk_size_{0};
+};
+    
+    namespace mxnet {
+/*!
+ * \brief Operator interface.
+ *  Operator defines basic operation unit of optimized computation graph in mxnet.
+ *  This interface relies on pre-allocated memory in TBlob, the caller need to set
+ *  the memory region in TBlob correctly before calling Forward and Backward.
+ *
+ *  Operator is generated by OperatorProperty.
+ *  To add new operator(aka. layers of neural nets) to mxnet, developer need to create
+ *  a new OperatorProperty and its corresponding Operator.
+ *
+ * \sa TBlob, TShape, OperatorProperty
+ */
+class Operator {
+ public:
+  /*! \brief destructor */
+  virtual ~Operator() {}
+  /*!
+   * \brief perform a forward operation of Operator, save the output to TBlob.
+   * \param ctx runtime context available to this call
+   * \param in_data array of input data, it is const
+   * \param req the request types of saving operation, can only be kWriteTo or kWriteInplace.
+   * \param out_data array of output data, pointer is used to indicate that this is holder
+   *        the space of TBlob in out_data must be pre-allocated with InferShape
+   * \param aux_states Auxiliary states of operator. Normally operator doesn't
+   *        need, epecial case like Batch Norm requires.
+   * \sa OpReqType, OpContext
+   */
+  virtual void Forward(const OpContext &ctx,
+                       const std::vector<TBlob> &in_data,
+                       const std::vector<OpReqType> &req,
+                       const std::vector<TBlob> &out_data,
+                       const std::vector<TBlob> &aux_states) = 0;
+  /*!
+   * \brief Perform a Backward Operation, write gradient to the in_grad.
+   *
+   * \note
+   * Convention:
+   *   out_grad.size() == OperatorProperty.NumVisibleOutputs()
+   *   out_data.size() == OperatorProperty.NumOutputs()
+   * out_data can contain additional invisible returns that remembers the
+   * state carried from the Forward pass. For example mask in the dropout.
+   * The gradients are passed from visible returns in this function.
+   *
+   * \par
+   * Not all the TBlobs in the arguments will be available
+   * if you override the DeclareBackwardDependency of corresponding OperatorProperty class.
+   * Only the dependencies you declared will be available at corresponding position,
+   * the rest of the parameters are simply dummy where you will get a nullptr.
+   * You will be safe if you use the default DeclareBackwardDependency.
+   * But only declare what you need will give engine more chance for optimization.
+   *
+   * \param ctx runtime context available to this call
+   * \param out_grad the gradient value we get from of the Operator.
+   * \param in_data the array of input data.
+   * \param out_data the array of output data.
+   * \param req request types of the saving operation, can be all types.
+   * \param in_grad the array of gradient we need to write to.
+   * \param aux_states Auxiliary states of operator. Normally operator doesn't need
+   * \sa OperatorProperty, OpReqType, OpContext
+   */
+  virtual void Backward(const OpContext &ctx,
+                        const std::vector<TBlob> &out_grad,
+                        const std::vector<TBlob> &in_data,
+                        const std::vector<TBlob> &out_data,
+                        const std::vector<OpReqType> &req,
+                        const std::vector<TBlob> &in_grad,
+                        const std::vector<TBlob> &aux_states) {
+    LOG(FATAL) << 'Backward is not implemented';
+  }
+  /*! \return [Deprecated] execution type of the operator */
+  virtual ExecType exec_type() const final {  // NOLINT(*) exec_type has been moved to OperatorProperty
+    return ExecType::kSync;
+  }
+};
     }
     
-    ///////////////////////////////////////////////////////////////////////////////
+    
+    {
+    {}  // namespace rtc
+}  // namespace mxnet
