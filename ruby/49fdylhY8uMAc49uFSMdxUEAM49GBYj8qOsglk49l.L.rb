236d7c7eb0451a7604945b9cc65a7ecf0864b65c
SUNsung <weatherius@gmail.com>
2019-02-04 20:55:43 +0200

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2019-02-04 20:55:42

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,79 +1,38 @@
 
         
-        lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'capistrano/version'
+            def create
+      authorize ReportNote, :create?
     
-          def warn_set_scm_is_deprecated
-        $stderr.puts(<<-MESSAGE)
-[Deprecation Notice] `set :scm, #{scm_name.inspect}` is deprecated.
-To ensure your project is compatible with future versions of Capistrano,
-remove the :scm setting and instead add these lines to your Capfile after
-`require 'capistrano/deploy'`:
-    
-            true
-      end
-    
-          def roles_for(names)
-        options = extract_options(names)
-        s = Filter.new(:role, names).filter(servers_by_key.values)
-        s.select { |server| server.select?(options) }
-      end
-    
-    # This is basically a copy of the original bundler 'bundle' shim
-# with the addition of the loading of our Bundler patches that
-# modify Bundler's caching behaviour.
-    
-    require 'bootstrap/environment'
-    
-        def execute
-      temp_path = generate_temporary_path
-      packet_gem = Paquet::Gem.new(temp_path, LogStash::Environment::CACHE_PATH)
-    
-      def validate_cache_location
-    cache_location = LogStash::Environment::CACHE_PATH
-    if File.exist?(cache_location)
-      puts('Directory #{cache_location} is going to be overwritten, do you want to continue? (Y/N)')
-      override = ( 'y' == STDIN.gets.strip.downcase ? true : false)
-      if override
-        FileUtils.rm_rf(cache_location)
-      else
-        puts('Unpack cancelled: file #{cache_location} already exists, please delete or move it')
-        exit
-      end
-    end
+      def local_domain?
+    TagManager.instance.web_domain?(hub_topic_domain)
   end
-end
-
     
-      namespace :vm do
+        web_subscription = ::Web::PushSubscription.create!(
+      endpoint: subscription_params[:endpoint],
+      key_p256dh: subscription_params[:keys][:p256dh],
+      key_auth: subscription_params[:keys][:auth],
+      data: data,
+      user_id: active_session.user_id,
+      access_token_id: active_session.access_token_id
+    )
     
-    desc 'Clean up files.'
-task :clean do |t|
-  FileUtils.rm_rf 'doc'
-  FileUtils.rm_rf 'tmp'
-  FileUtils.rm_rf 'pkg'
-  FileUtils.rm_rf 'public'
-  FileUtils.rm 'test/debug.log' rescue nil
-  FileUtils.rm 'test/paperclip.db' rescue nil
-  Dir.glob('paperclip-*.gem').each{|f| FileUtils.rm f }
-end
-
-    
-    
-require 'uri'
-require 'cgi'
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'paths'))
-require File.expand_path(File.join(File.dirname(__FILE__), '..', 'support', 'selectors'))
-    
-    After do
-  ORIGINAL_BUNDLE_VARS.each_pair do |key, value|
-    ENV[key] = value
+        render_empty
   end
-end
     
-        private
+          spec['version'] = Bootstrap::VERSION
     
-        def register_new_attachment
-      Paperclip::AttachmentRegistry.register(@klass, @name, @options)
+        def pos
+      byte_to_str_pos @s.pos
     end
+    
+        alias log puts
+    
+        post '/edit/Mordor/Orc', :content => 'not so big smelly creatures',
+         :page                        => 'Orc', :path => 'Mordor', :message => 'minor edit'
+    assert_equal 'http://example.org/Mordor/Orc', last_response.headers['Location']
+    
+        @wiki = Gollum::Wiki.new(@path)
+    page  = @wiki.page('PG')
+    assert_equal '바뀐 text', utf8(page.raw_data)
+    assert_equal 'ghi', page.version.message
+  end
