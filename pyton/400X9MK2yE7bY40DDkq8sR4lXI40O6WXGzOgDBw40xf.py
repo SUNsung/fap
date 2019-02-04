393f060921560bc41ca7ca8ac934a1babb208616
SUNsung <weatherius@gmail.com>
2019-02-04 19:10:26 +0200

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2019-02-04 19:10:25

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,116 +1,75 @@
 
         
-            def __repr__(self):
-        return '<{0} {1}>'.format(type(self).__name__, str(self))
-
-    
-        def __init__(self, conversion, formatting, **kwargs):
-        super(PrettyStream, self).__init__(**kwargs)
-        self.formatting = formatting
-        self.conversion = conversion
-        self.mime = self.msg.content_type.split(';')[0]
-    
-        def run_tests(self):
-        import pytest
-        sys.exit(pytest.main(self.test_args))
-    
-        plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            BASIC_AUTH_HEADER_VALUE,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
-    
-    import pytest
-    
-        def test_request_body_from_file_by_path_no_field_name_allowed(
-            self, httpbin):
-        env = MockEnvironment(stdin_isatty=True)
-        r = http('POST', httpbin.url + '/post', 'field-name@' + FILE_PATH_ARG,
-                 env=env, error_exit_ok=True)
-        assert 'perhaps you meant --form?' in r.stderr
+            def process_query(self, query):
+        query = self.parse_query(query)
+        results = self.memory_cache.get(query)
+        if results is None:
+            results = self.reverse_index_cluster.process_search(query)
+            self.memory_cache.set(query, results)
+        return results
     
     
-DEFAULT_CONFIG_DIR = str(os.environ.get(
-    'HTTPIE_CONFIG_DIR',
-    os.path.expanduser('~/.httpie') if not is_windows else
-    os.path.expandvars(r'%APPDATA%\\httpie')
-))
-    
-        output_directory = 'pdfs' if results.directory is None else results.directory
-    
-                    for k in range(boxes.shape[0]):
-                    score = boxes[k, -1]
-                    mask = masks[:, :, k]
-                    pngname = os.path.join(
-                        'results',
-                        basename + '_' + clss + '_{}.png'.format(k))
-                    # write txt
-                    fid_txt.write('{} {} {}\n'.format(pngname, clss_id, score))
-                    # save mask
-                    cv2.imwrite(os.path.join(output_dir, pngname), mask * 255)
-    logger.info('Evaluating...')
-    cityscapes_eval.main([])
-    return None
-
-    
-        # Broacast anchors over shifts to enumerate all anchors at all positions
-    # in the (H, W) grid:
-    #   - add A cell anchors of shape (1, A, 4) to
-    #   - K shifts of shape (K, 1, 4) to get
-    #   - all shifted anchors of shape (K, A, 4)
-    #   - reshape to (K*A, 4) shifted anchors
-    A = num_cell_anchors
-    K = shifts.shape[0]
-    field_of_anchors = (
-        cell_anchors.reshape((1, A, 4)) +
-        shifts.reshape((1, K, 4)).transpose((1, 0, 2))
-    )
-    field_of_anchors = field_of_anchors.reshape((K * A, 4))
-    foa = FieldOfAnchors(
-        field_of_anchors=field_of_anchors.astype(np.float32),
-        num_cell_anchors=num_cell_anchors,
-        stride=stride,
-        field_size=field_size,
-        octave=octave,
-        aspect=aspect
-    )
-    _threadlocal_foa.cache[cache_key] = foa
-    return foa
+@pytest.mark.functional
+def test_refuse_with_confirmation(proc, TIMEOUT):
+    refuse_with_confirmation(proc, TIMEOUT)
+    history_not_changed(proc, TIMEOUT)
     
     
-def add_mask_rcnn_blobs(blobs, sampled_boxes, roidb, im_scale, batch_idx):
-    '''Add Mask R-CNN specific blobs to the input blob dictionary.'''
-    # Prepare the mask targets by associating one gt mask to each training roi
-    # that has a fg (non-bg) class label.
-    M = cfg.MRCNN.RESOLUTION
-    polys_gt_inds = np.where(
-        (roidb['gt_classes'] > 0) & (roidb['is_crowd'] == 0)
-    )[0]
-    polys_gt = [roidb['segms'][i] for i in polys_gt_inds]
-    boxes_from_polys = segm_utils.polys_to_boxes(polys_gt)
-    fg_inds = np.where(blobs['labels_int32'] > 0)[0]
-    roi_has_mask = blobs['labels_int32'].copy()
-    roi_has_mask[roi_has_mask > 0] = 1
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'tcsh')
+    proc.sendline(u'setenv PYTHONIOENCODING utf8')
+    proc.sendline(u'eval `thefuck --alias`')
+    return proc
     
-        net = core.Net('dequeue_net')
-    net.type = 'dag'
-    all_blobs = []
-    for gpu_id in range(cfg.NUM_GPUS):
-        with core.NameScope('gpu_{}'.format(gpu_id)):
-            with core.DeviceScope(muji.OnGPU(gpu_id)):
-                for blob_name in blob_names:
-                    blob = core.ScopedName(blob_name)
-                    all_blobs.append(blob)
-                    workspace.CreateBlob(blob)
-                    logger.info('Creating blob: {}'.format(blob))
-                net.DequeueBlobs(
-                    roi_data_loader._blobs_queue_name, blob_names)
-    logger.info('Protobuf:\n' + str(net.Proto()))
+    
+@pytest.mark.parametrize('command, packages, which', [
+    (Command('a_bad_cmd', 'a_bad_cmd: command not found'),
+     [], None),
+    (Command('vim', ''), [], None),
+    (Command('', ''), [], None),
+    (Command('vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim'),
+    (Command('sudo vim', 'vim: command not found'),
+     ['vim'], '/usr/bin/vim')])
+def test_not_match(mocker, command, packages, which):
+    mocker.patch('thefuck.rules.apt_get.which', return_value=which)
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
+    
+            if row1 == row2:
+            plaintext += table[row1*5+(col1-1)%5]
+            plaintext += table[row2*5+(col2-1)%5]
+        elif col1 == col2:
+            plaintext += table[((row1-1)%5)*5+col1]
+            plaintext += table[((row2-1)%5)*5+col2]
+        else: # rectangle
+            plaintext += table[row1*5+col2]
+            plaintext += table[row2*5+col1]
+    
+        print('Writing private key to file %s_privkey.txt...' % name)
+    with open('%s_privkey.txt' % name, 'w') as fo:
+        fo.write('%s,%s,%s' % (keySize, privateKey[0], privateKey[1]))
+    
+    The problem is  :
+Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
+Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
+'''
+from __future__ import print_function
+    
+    from matplotlib import pyplot as plt
+def plot_heterogeneity(heterogeneity, k):
+    plt.figure(figsize=(7,4))
+    plt.plot(heterogeneity, linewidth=4)
+    plt.xlabel('# Iterations')
+    plt.ylabel('Heterogeneity')
+    plt.title('Heterogeneity of clustering over time, K={0:d}'.format(k))
+    plt.rcParams.update({'font.size': 16})
+    plt.show()
+    
+        for uncommonLetter in ETAOIN[-6:]:
+        if uncommonLetter in freqOrder[-6:]:
+            matchScore += 1
