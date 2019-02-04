dc39eeef861b1f4df0a342143adb992c59fb9b54
SUNsung <weatherius@gmail.com>
2019-02-04 09:00:45 +0200

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2019-02-04 09:00:44

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,107 +1,194 @@
 
         
-          formula = ARGV.formulae.first
-  options = Options.create(ARGV.flags_only)
-  build   = Build.new(formula, options)
-  build.install
-rescue Exception => e
-  Marshal.dump(e, error_pipe)
-  error_pipe.close
-  exit! 1
+            it 'returns a label 'No' if any falsy value is given' do
+      [false, nil].each { |value|
+        label = yes_no(value)
+        expect(label).to be_html_safe
+        expect(Nokogiri(label).text).to eq 'No'
+      }
+    end
+  end
+    
+        it 'can not be turned off' do
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'true' }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
+  end
 end
 
     
-            if Pathname::BOTTLE_EXTNAME_RX === file.to_s
-          version = bottle_resolve_version(file) rescue file.version
-        else
-          version = file.version
+              existing_scenario.reload
+          expect(existing_scenario.guid).to eq(guid)
+          expect(existing_scenario.tag_fg_color).to eq(tag_fg_color)
+          expect(existing_scenario.tag_bg_color).to eq(tag_bg_color)
+          expect(existing_scenario.icon).to eq(icon)
+          expect(existing_scenario.description).to eq(description)
+          expect(existing_scenario.name).to eq(name)
+          expect(existing_scenario.source_url).to eq(source_url)
+          expect(existing_scenario.public).to be_falsey
         end
-        next unless version
-        next unless (name = file.basename.to_s[/(.*)-(?:#{Regexp.escape(version)})/, 1])
     
-    module Vagrant
-  # This class handles guest-OS specific interactions with a machine.
-  # It is primarily responsible for detecting the proper guest OS
-  # implementation and then delegating capabilities.
-  #
-  # Vagrant has many tasks which require specific guest OS knowledge.
-  # These are implemented using a guest/capability system. Various plugins
-  # register as 'guests' which determine the underlying OS of the system.
-  # Then, 'guest capabilities' register themselves for a specific OS (one
-  # or more), and these capabilities are called.
-  #
-  # Example capabilities might be 'mount_virtualbox_shared_folder' or
-  # 'configure_networks'.
-  #
-  # This system allows for maximum flexibility and pluginability for doing
-  # guest OS specific operations.
-  class Guest
-    include CapabilityHost
+      before(:each) do
+    stub_request(:get, /events.json$/).to_return(
+      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
+      :status => 200,
+      :headers => {'Content-Type' => 'text/json'}
+    )
+    stub_request(:get, /projects.json$/).to_return(
+      :body => JSON.dump([{name: 'test', id: 1234},{name: 'test1', id: 1235}]),
+      :status => 200,
+      :headers => {'Content-Type' => 'text/json'}
+    )
+    stub_request(:get, /02:00$/).to_return(
+      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
+      :status => 200,
+      :headers => {'Content-Type' => 'text/json'}
+    )
+    @valid_params = { :project_id => 6789 }
     
-            protected
+            a_split.each_with_index { |s, i| a_split[i] = s.to_i unless i == a_length - 1 }
+        b_split.each_with_index { |s, i| b_split[i] = s.to_i unless i == b_length - 1 }
     
-            # Called after the configuration is finalized and loaded to validate
-        # this object.
+        def add(path, content)
+      @pages[path] = content
+    end
+    
+        private
+    
+        module MultipleBaseUrls
+      def self.included(base)
+        base.extend ClassMethods
+      end
+    
+            css('.nav-index-section').each do |node|
+          node.content = node.content
+        end
+    
+      def pinnable?
+    !@f.installed_prefixes.empty?
+  end
+    
+      describe '#casks' do
+    it 'returns an empty array if there is no match' do
+      expect(subject.casks).to eq []
+    end
+  end
+    
+            # Sends a Kerberos Request over a tcp connection
         #
-        # @param [Environment] env Vagrant::Environment object of the
-        #   environment that this configuration has been loaded into. This
-        #   gives you convenient access to things like the the root path
-        #   and so on.
-        # @param [ErrorRecorder] errors
-        def validate(env, errors)
-        end
-      end
-    end
-  end
-end
-
-    
-            # Initializes the system. Any subclasses MUST make sure this
-        # method is called on the parent. Therefore, if a subclass overrides
-        # `initialize`, then you must call `super`.
-        def initialize(vm)
-          @vm = vm
+        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
+        # @return [Integer] the number of bytes sent
+        # @raise [RuntimeError] if the request can't be encoded
+        def send_request_tcp(req)
+          data = req.encode
+          length = [data.length].pack('N')
+          connection.put(length + data)
         end
     
-            # This is the method called to when the system is being destroyed
-        # and allows the provisioners to engage in any cleanup tasks necessary.
-        def cleanup
-        end
-      end
-    end
-  end
-end
-
+              private
     
-            # Returns the instance variables as a hash of key-value pairs.
-        def instance_variables_hash
-          instance_variables.inject({}) do |acc, iv|
-            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
-            acc
+              def initialize(options = {})
+            self.class.attributes.each do |attr|
+              if options.has_key?(attr)
+                m = (attr.to_s + '=').to_sym
+                self.send(m, options[attr])
+              end
+            end
           end
-        end
     
-    describe 'Kernel#taint' do
-  it 'returns self' do
-    o = Object.new
-    o.taint.should equal(o)
-  end
+              # Rex::Proto::Kerberos::Model::KdcResponse encoding isn't supported
+          #
+          # @raise [NotImplementedError]
+          def encode
+            raise ::NotImplementedError, 'KdcResponse encoding not supported'
+          end
     
-        after :each do
-      @tmp_file.close
-      rm_r @tmp_file
+              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an
+          # OpenSSL::ASN1::Sequence
+          #
+          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
+          def decode_asn1(input)
+            seq_values = input.value
+            self.type = decode_type(seq_values[0])
+            self.value = decode_value(seq_values[1])
+          end
+    
+          spec['main'] =
+          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
+          find_files.(Bootstrap.fonts_path) +
+          %w(assets/javascripts/bootstrap.js)
+    
+        def log_processed(name)
+      puts green '    #{name}'
     end
     
-    describe 'Kernel#warn' do
-  before :each do
-    @before_verbose = $VERBOSE
-    @before_separator = $/
-  end
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
     
-          def is_edit_page
-        true
+        it 'succeeds for notification dropdown' do
+      Timecop.travel(6.seconds.ago) do
+        @notification.touch
+      end
+      get :index, format: :json
+      expect(response).to be_success
+      response_json = JSON.parse(response.body)
+      note_html = Nokogiri::HTML(response_json['notification_list'][0]['also_commented']['note_html'])
+      timeago_content = note_html.css('time')[0]['data-time-ago']
+      expect(response_json['unread_count']).to be(1)
+      expect(response_json['unread_count_by_type']).to eq(
+        'also_commented'       => 1,
+        'comment_on_post'      => 0,
+        'contacts_birthday'    => 0,
+        'liked'                => 0,
+        'mentioned'            => 0,
+        'mentioned_in_comment' => 0,
+        'reshared'             => 0,
+        'started_sharing'      => 0
+      )
+      expect(timeago_content).to include(@notification.updated_at.iso8601)
+      expect(response.body).to match(/note_html/)
+    end
+    
+          it 'assigns @user' do
+        get :create, params: invalid_params
+        expect(assigns(:user)).not_to be_nil
       end
     
-        post '/edit/PG', :page => 'PG', :content => '바뀐 text', :message => 'ghi'
-    follow_redirect!
-    assert last_response.ok?
+          it 'creates a reshare' do
+        expect{
+          post_request!
+        }.to change(Reshare, :count).by(1)
+      end
+    
+    Liquid::Template.register_tag('gist', Jekyll::GistTag)
+Liquid::Template.register_tag('gistnocache', Jekyll::GistTagNoCache)
+
+    
+          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
+        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
+        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
+          @img['title']  = title
+          @img['alt']    = alt
+        else
+          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
+        end
+        @img['class'].gsub!(/'/, '') if @img['class']
+      end
+      super
+    end
+    
+          super
+    end
+    
+    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
+
+    
+        def initialize(tag_name, markup, tokens)
+      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
+      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
+      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
+      super
+    end
