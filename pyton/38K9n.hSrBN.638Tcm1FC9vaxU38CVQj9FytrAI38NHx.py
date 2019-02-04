b7a85a0414f997c973bc4480ea6b52378c40e52b
SUNsung <weatherius@gmail.com>
2019-02-04 16:47:31 +0200

    Rewrite [38K9n.hSrBN.638Tcm1FC9vaxU38CVQj9FytrAI38NHx.py] File in: 2019-02-04 16:47:31

--- pyton/38K9n.hSrBN.638Tcm1FC9vaxU38CVQj9FytrAI38NHx.py
+++ pyton/38K9n.hSrBN.638Tcm1FC9vaxU38CVQj9FytrAI38NHx.py
@@ -1,192 +1,110 @@
 
         
-                try:
-            matching_groups = conn.describe_cache_subnet_groups(group_name, max_records=100)
-            exists = len(matching_groups) > 0
-        except BotoServerError as e:
-            if e.error_code != 'CacheSubnetGroupNotFoundFault':
-                module.fail_json(msg=e.error_message)
+        
+@pytest.fixture(params=[(python_3, False),
+                        (python_3, True),
+                        (python_2, False)])
+def proc(request, spawnu, TIMEOUT):
+    container, instant_mode = request.param
+    proc = spawnu(*container)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(init_zshrc.format(
+        u'--enable-experimental-instant-mode' if instant_mode else ''))
+    proc.sendline(u'zsh')
+    if instant_mode:
+        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
+    return proc
     
-            # return
-        self.exit()
-    
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-    
-                update_roles(schema_facts, cursor, schema,
-                         schema_facts[schema_key]['usage_roles'], usage_roles,
-                         schema_facts[schema_key]['create_roles'], create_roles)
-            changed = True
-        if changed:
-            schema_facts.update(get_schema_facts(cursor, schema))
-        return changed
-    
-    
-DOCUMENTATION = '''
----
-module: ipa_dnszone
-author: Fran Fitzpatrick (@fxfitz)
-short_description: Manage FreeIPA DNS Zones
-description:
-- Add and delete an IPA DNS Zones using IPA API
-options:
-  zone_name:
-    description:
-    - The DNS zone name to which needs to be managed.
-    required: true
-  state:
-    description: State to ensure
-    required: false
-    default: present
-    choices: ['present', 'absent']
-extends_documentation_fragment: ipa.documentation
-version_added: '2.5'
+    match_output = '''
+Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
+Hit:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease
+Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
+Hit:4 http://security.ubuntu.com/ubuntu zesty-security InRelease
+Hit:5 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
+Hit:6 https://download.docker.com/linux/ubuntu zesty InRelease
+Hit:7 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
+Fetched 89.2 kB in 0s (122 kB/s)
+Reading package lists... Done
+Building dependency tree
+Reading state information... Done
+8 packages can be upgraded. Run 'apt list --upgradable' to see them.
 '''
     
-    '''
+                gc.collect()
+            print('max_iter', max_iter)
+            print('- benchmarking A-SGD')
+            clf = SGDRegressor(alpha=alpha / n_train, fit_intercept=False,
+                               max_iter=max_iter, learning_rate='invscaling',
+                               eta0=.002, power_t=0.05, tol=1e-3,
+                               average=(max_iter * n_train // 2))
+    
+        package is the name of the root module of the package
+    
+    The goal of this exercise is to train a linear classifier on text features
+that represent sequences of up to 3 consecutive characters so as to be
+recognize natural languages by using the frequencies of short character
+sequences as 'fingerprints'.
+    
+    # TASK: Build a vectorizer / classifier pipeline using the previous analyzer
+# the pipeline instance should stored in a variable named clf
+clf = Pipeline([
+    ('vec', vectorizer),
+    ('clf', Perceptron(tol=1e-3)),
+])
+    
+    # Now predict the value of the digit on the second half:
+expected = digits.target[n_samples // 2:]
+predicted = classifier.predict(data[n_samples // 2:])
+    
+    agglo = cluster.FeatureAgglomeration(connectivity=connectivity,
+                                     n_clusters=32)
+    
+            cert.sign(CertUtil.ca_privatekey, CertUtil.ca_digest)
+    
+    EVENT_NAME = 'flic_click'
+EVENT_DATA_NAME = 'button_name'
+EVENT_DATA_ADDRESS = 'button_address'
+EVENT_DATA_TYPE = 'click_type'
+EVENT_DATA_QUEUED_TIME = 'queued_time'
+    
+        def random_see(dev_id, name):
+        '''Randomize a sighting.'''
+        see(
+            dev_id=dev_id,
+            host_name=name,
+            gps=(hass.config.latitude + offset(),
+                 hass.config.longitude + offset()),
+            gps_accuracy=random.randrange(50, 150),
+            battery=random.randrange(10, 90)
+        )
+    
+    _LOGGER = logging.getLogger(__name__)
+    
+    import homeassistant.helpers.config_validation as cv
+from homeassistant.components import http
+from homeassistant.core import callback
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.entity_component import EntityComponent
+from homeassistant.util import Throttle
+    
+        return EventHandler(patterns, hass)
     
     
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
     
-    from scrapy.utils.conf import arglist_to_dict
-from scrapy.exceptions import UsageError
-    
-            sfile = sys.modules[spidercls.__module__].__file__
-        sfile = sfile.replace('.pyc', '.py')
-        self.exitcode = os.system('%s '%s'' % (editor, sfile))
-
-    
-        def run(self, args, opts):
-        settings = self.crawler_process.settings
-        if opts.get:
-            s = settings.get(opts.get)
-            if isinstance(s, BaseSettings):
-                print(json.dumps(s.copy_to_dict()))
-            else:
-                print(s)
-        elif opts.getbool:
-            print(settings.getbool(opts.getbool))
-        elif opts.getint:
-            print(settings.getint(opts.getint))
-        elif opts.getfloat:
-            print(settings.getfloat(opts.getfloat))
-        elif opts.getlist:
-            print(settings.getlist(opts.getlist))
-
-    
-        def __init__(self, crawler, spider_closed_callback):
-        self.crawler = crawler
-        self.settings = crawler.settings
-        self.signals = crawler.signals
-        self.logformatter = crawler.logformatter
-        self.slot = None
-        self.spider = None
-        self.running = False
-        self.paused = False
-        self.scheduler_cls = load_object(self.settings['SCHEDULER'])
-        downloader_cls = load_object(self.settings['DOWNLOADER'])
-        self.downloader = downloader_cls(crawler)
-        self.scraper = Scraper(crawler)
-        self._spider_closed_callback = spider_closed_callback
+    # Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file('biden.jpg')
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
     
     
-def test_1():
-    for o in [None, True, False, 0, 1, (1 << 6), (1 << 7) - 1, -1,
-              -((1 << 5) - 1), -(1 << 5)]:
-        check(1, o)
-    
-    url = pr['url']
-title = pr['title']
-body = pr['body']
-target_ref = pr['base']['ref']
-user_login = pr['user']['login']
-base_ref = pr['head']['ref']
-pr_repo_desc = '%s/%s' % (user_login, base_ref)
-    
-        clipboard_types = {'osx': init_osx_clipboard,
-                       'gtk': init_gtk_clipboard,
-                       'qt': init_qt_clipboard,
-                       'xclip': init_xclip_clipboard,
-                       'xsel': init_xsel_clipboard,
-                       'klipper': init_klipper_clipboard,
-                       'windows': init_windows_clipboard,
-                       'no': init_no_clipboard}
-    
-            exp = pd.Index([pd.Timestamp('2011-01-01 09:00', tz=tz),
-                        pd.Timestamp('2011-01-01 10:00'),
-                        pd.Timestamp('2011-01-01 11:00', tz=tz)],
-                       dtype=object)
-        tm.assert_index_equal(
-            idx.fillna(pd.Timestamp('2011-01-01 10:00')), exp)
-    
-            with pd.option_context('display.width', 300):
-            for idx, expected in zip([idx1, idx2, idx3, idx4, idx5],
-                                     [exp1, exp2, exp3, exp4, exp5]):
-                result = repr(pd.Series(idx))
-                assert result == expected
-    
-        def __call__(self, text, **kargs):
-        words = jieba.tokenize(text, mode='search')
-        token = Token()
-        for (w, start_pos, stop_pos) in words:
-            if not accepted_chars.match(w) and len(w) <= 1:
-                continue
-            token.original = token.text = w
-            token.pos = start_pos
-            token.startchar = start_pos
-            token.endchar = stop_pos
-            yield token
-    
-        def __init__(self):
-        self.graph = defaultdict(list)
-    
-        route = [None] * len(obs)
-    i = len(obs) - 1
-    while i >= 0:
-        route[i] = state
-        state = mem_path[i][state]
-        i -= 1
-    return (prob, route)
-
-    
-    USAGE = 'usage:    python extract_tags.py [file name] -k [top k]'
-    
-    n_topic = 10
-n_top_words = 25
-    
-    print(','.join(tags))
-    
-    print('speed %s bytes/second' % (len(content)/tm_cost))
-    
-    
-    
-        # set basic CLI commands
-    config.CLI_COMMANDS['infra'] = {
-        'description': 'Commands to manage the infrastructure',
-        'function': cmd_infra
-    }
-    config.CLI_COMMANDS['start'] = {
-        'description': 'Shorthand to start the infrastructure',
-        'function': cmd_infra
-    }
-    config.CLI_COMMANDS['web'] = {
-        'description': 'Commands to manage the Web dashboard',
-        'function': cmd_web
-    }
-    
-    # set up logger
-LOGGER = logging.getLogger(__name__)
-    
-    
-def test_environment():
-    env = aws_stack.Environment.from_json({'prefix': 'foobar1'})
-    assert env.prefix == 'foobar1'
-    env = aws_stack.Environment.from_string('foobar2')
-    assert env.prefix == 'foobar2'
-    
-        def test_get_container_name(self):
-        executor = lambda_executors.EXECUTOR_CONTAINERS_REUSE
-        name = executor.get_container_name('arn:aws:lambda:us-east-1:00000000:function:my_function_name')
-        self.assertEqual(name, 'localstack_lambda_arn_aws_lambda_us-east-1_00000000_function_my_function_name')
+import unittest
+import os
+import numpy as np
+from click.testing import CliRunner
