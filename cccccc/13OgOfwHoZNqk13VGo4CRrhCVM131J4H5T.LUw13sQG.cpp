ea97cc515cef346b1c319ba8ae9752df7928b83e
SUNsung <weatherius@gmail.com>
2019-02-04 06:22:55 +0200

    Rewrite [13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp] File in: 2019-02-04 06:22:54

--- cccccc/13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp
+++ cccccc/13OgOfwHoZNqk13VGo4CRrhCVM131J4H5T.LUw13sQG.cpp
@@ -1,158 +1,140 @@
 
         
-          // WindowListObserver:
-  void OnWindowAllClosed() override;
+          const OpRegistrationData* op_reg_data;
+  TF_RETURN_IF_ERROR(OpRegistry::Global()->LookUp(node.op(), &op_reg_data));
     
-    #include 'atom/browser/api/event_emitter.h'
-#include 'atom/browser/mac/in_app_purchase.h'
-#include 'atom/browser/mac/in_app_purchase_observer.h'
-#include 'atom/browser/mac/in_app_purchase_product.h'
-#include 'native_mate/handle.h'
+    // hidden_ops should be a list of Op names that should get a leading _
+// in the output. Prints the output to stdout.
+// Optional fourth argument is the name of the original C++ source file
+// where the ops' REGISTER_OP() calls reside.
+void PrintPythonOps(const OpList& ops, const ApiDefMap& api_defs,
+                    const std::vector<string>& hidden_ops, bool require_shapes,
+                    const string& source_file_name = '');
     
-    class Net : public mate::EventEmitter<Net> {
+    string DataTypeToPython(DataType dtype, const string& dtype_module);
+    
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/platform/mutex.h'
+#include 'tensorflow/python/lib/core/bfloat16.h'
+#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
+    
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/framework/types.pb.h'
+#include 'tensorflow/core/lib/core/status.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    #include <map>
+    
+    
+    {}  // namespace tensorflow
+    
+    #endif  // TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
+
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    namespace stream_executor {
+namespace cuda {
+    }
+    }
+    
+      // Maps ordinal number to a list of cached executors for that ordinal.
+  // We key off of ordinal (instead of just looking up all fields in the
+  // StreamExecutorConfig) for a slight improvement in lookup time.
+  mutex mutex_;
+  std::map<int, Entry> cache_ GUARDED_BY(mutex_);
+    
+      // Creates a 1d FFT plan with scratch allocator.
+  virtual std::unique_ptr<Plan> Create1dPlanWithScratchAllocator(
+      Stream *stream, uint64 num_x, Type type, bool in_place_fft,
+      ScratchAllocator *scratch_allocator) = 0;
+    
+    // Thread compatible.
+class CensusContext {
  public:
-  static v8::Local<v8::Value> Create(v8::Isolate* isolate);
+  CensusContext() : span_(::opencensus::trace::Span::BlankSpan()) {}
     }
     
-      // content::DownloadManager::Observer:
-  void OnDownloadCreated(content::DownloadManager* manager,
-                         download::DownloadItem* item) override;
+    #include <grpc/impl/codegen/port_platform.h>
     
-    namespace atom {
+      std::chrono::seconds timeout_;
+    
+      virtual bool isReply() const CXX11_OVERRIDE;
+    
+    class DHTRoutingTableSerializer {
+private:
+  int family_;
     }
     
-      virtual void OnError(const std::string& error,
-                       const int code,
-                       const std::string& domain) {}
+    class DHTTask;
     
-    
-    {}  // namespace atom
-    
-        UBool operator == (const ScriptSet &other) const;
-    ScriptSet & operator = (const ScriptSet &other);
-    
-    #define DOT               ((UChar)0x002E)
-#define SINGLE_QUOTE      ((UChar)0x0027)
-#define SLASH             ((UChar)0x002F)
-#define BACKSLASH         ((UChar)0x005C)
-#define SPACE             ((UChar)0x0020)
-#define TAB               ((UChar)0x0009)
-#define QUOTATION_MARK    ((UChar)0x0022)
-#define ASTERISK          ((UChar)0x002A)
-#define COMMA             ((UChar)0x002C)
-#define HYPHEN            ((UChar)0x002D)
-#define U_ZERO            ((UChar)0x0030)
-#define U_ONE             ((UChar)0x0031)
-#define U_TWO             ((UChar)0x0032)
-#define U_THREE           ((UChar)0x0033)
-#define U_FOUR            ((UChar)0x0034)
-#define U_FIVE            ((UChar)0x0035)
-#define U_SIX             ((UChar)0x0036)
-#define U_SEVEN           ((UChar)0x0037)
-#define U_EIGHT           ((UChar)0x0038)
-#define U_NINE            ((UChar)0x0039)
-#define COLON             ((UChar)0x003A)
-#define SEMI_COLON        ((UChar)0x003B)
-#define CAP_A             ((UChar)0x0041)
-#define CAP_B             ((UChar)0x0042)
-#define CAP_R             ((UChar)0x0052)
-#define CAP_Z             ((UChar)0x005A)
-#define LOWLINE           ((UChar)0x005F)
-#define LEFTBRACE         ((UChar)0x007B)
-#define RIGHTBRACE        ((UChar)0x007D)
-    
-    U_NAMESPACE_BEGIN
-    
-        // TimeZoneFormat in ICU4C only depends on a locale for now
-    if (fLocale != other.fLocale) {
-        delete fTimeZoneFormat;
-        fTimeZoneFormat = NULL; // forces lazy instantiation with the other locale
-        fLocale = other.fLocale;
-    }
-    
-    /**
- * Implement UnicodeMatcher
- */
-void StringMatcher::addMatchSetTo(UnicodeSet& toUnionTo) const {
-    UChar32 ch;
-    for (int32_t i=0; i<pattern.length(); i+=U16_LENGTH(ch)) {
-        ch = pattern.char32At(i);
-        const UnicodeMatcher* matcher = data->lookupMatcher(ch);
-        if (matcher == NULL) {
-            toUnionTo.add(ch);
-        } else {
-            matcher->addMatchSetTo(toUnionTo);
-        }
-    }
-}
-    
-    /**
- * Construct a StringReplacer that sets the emits the given output
- * text and sets the cursor to the given position.
- * @param theOutput text that will replace input text when the
- * replace() method is called.  May contain stand-in characters
- * that represent nested replacers.
- * @param theCursorPos cursor position that will be returned by
- * the replace() method
- * @param theData transliterator context object that translates
- * stand-in characters to UnicodeReplacer objects
- */
-StringReplacer::StringReplacer(const UnicodeString& theOutput,
-                               int32_t theCursorPos,
-                               const TransliterationRuleData* theData) {
-    output = theOutput;
-    cursorPos = theCursorPos;
-    hasCursor = TRUE;
-    data = theData;
-    isComplex = TRUE;
-}
-    
-    
-    {    Executor::KeepAlive<Executor> ka2 = std::move(ka); // conversion
-    EXPECT_FALSE(ka);
-    EXPECT_TRUE(ka2);
-    EXPECT_EQ(&exec, ka2.get());
-    EXPECT_EQ(1, exec.refCount);
-  }
-    
-    #include <glog/logging.h>
-    
-    vector<detail::BenchmarkResult> resultsFromFile(const std::string& filename) {
-  string content;
-  readFile(filename.c_str(), content);
-  vector<detail::BenchmarkResult> ret;
-  benchmarkResultsFromDynamic(parseJson(content), ret);
-  return ret;
-}
-    
-    bool Executor::keepAliveAcquire() {
-  return false;
-}
-    
-    namespace folly {
-    }
-    
-    #include <type_traits>
-    
-    TEST_F(SparseByteSetTest, each) {
-  for (auto c = lims::min(); c < lims::max(); ++c) {
-    EXPECT_TRUE(s.add(c));
-    EXPECT_TRUE(s.contains(c));
-  }
-  for (auto c = lims::min(); c < lims::max(); ++c) {
-    EXPECT_FALSE(s.add(c));
-    EXPECT_TRUE(s.contains(c));
-  }
-}
-    
-    #if BOOST_ATOMIC_THREAD_FENCE > 0
-BOOST_FORCEINLINE void atomic_thread_fence(memory_order order) BOOST_NOEXCEPT
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port,
+                                           const unsigned char* secret) const
 {
-    detail::thread_fence(order);
+  unsigned char src[DHT_ID_LENGTH + COMPACT_LEN_IPV6 + SECRET_SIZE];
+  memset(src, 0, sizeof(src));
+  int compactlen = bittorrent::packcompact(src + DHT_ID_LENGTH, ipaddr, port);
+  if (compactlen == 0) {
+    throw DL_ABORT_EX(fmt('Token generation failed: ipaddr=%s, port=%u',
+                          ipaddr.c_str(), port));
+  }
+  memcpy(src, infoHash, DHT_ID_LENGTH);
+  memcpy(src + DHT_ID_LENGTH + COMPACT_LEN_IPV6, secret, SECRET_SIZE);
+  unsigned char md[20];
+  message_digest::digest(md, sizeof(md), MessageDigest::sha1().get(), src,
+                         sizeof(src));
+  return std::string(&md[0], &md[sizeof(md)]);
 }
-#else
-BOOST_FORCEINLINE void atomic_thread_fence(memory_order) BOOST_NOEXCEPT
-{
-    detail::lockpool::thread_fence();
-}
-#endif
+    
+    DHTTokenUpdateCommand::~DHTTokenUpdateCommand() = default;
+    
+    #endif // D_DHT_TOKEN_UPDATE_COMMAND_H
+
+    
+    #include 'DHTNode.h'
+#include 'util.h'
+#include 'a2functional.h'
+    
+    public:
+  // _remoteNode is always null
+  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                    const unsigned char* data, size_t length,
+                    const std::string& ipaddr, uint16_t port);
+    
+      bool FilterMergeOperand(int level, const rocksdb::Slice& key,
+                          const rocksdb::Slice& existing_value) const override {
+    fprintf(stderr, 'FilterMerge(%s)\n', key.ToString().c_str());
+    ++merge_count_;
+    return existing_value == 'bad';
+  }
+    
+      const Snapshot* snapshot = txn->GetSnapshot();
+    
+      // close DB
+  delete cf;
+  delete db;
+    
+      ////////////////////////////////////////////////////////
+  //
+  // 'Read Committed' (Monotonic Atomic Views) Example
+  //   --Using multiple Snapshots
+  //
+  ////////////////////////////////////////////////////////
+    
+    #include <functional>
+    
+      // Restore rate limiter. Used to control transfer speed during restore. If
+  // this is not null, restore_rate_limit is ignored.
+  // Default: nullptr
+  std::shared_ptr<RateLimiter> restore_rate_limiter{nullptr};
