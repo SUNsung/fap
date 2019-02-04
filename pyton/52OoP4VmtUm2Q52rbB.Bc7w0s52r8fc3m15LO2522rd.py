e0956979d86298084b481c791c8eb1dee9a8fd09
SUNsung <weatherius@gmail.com>
2019-02-04 11:32:48 +0200

    Rewrite [52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py] File in: 2019-02-04 11:32:47

--- pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
+++ pyton/52OoP4VmtUm2Q52rbB.Bc7w0s52r8fc3m15LO2522rd.py
@@ -1,81 +1,191 @@
 
         
-            Returns:
-      The mean loss for the epoch.
+                if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {0} is already registered.'.format(username)
     
-        Returns:
-      a 3 tuple of costs:
-        (epoch total cost, epoch reconstruction cost, epoch KL cost)
-    '''
-    ops_to_eval = [self.cost, self.recon_cost, self.kl_cost]
-    collected_op_values = self.run_epoch(datasets, ops_to_eval, kind=kind,
-                                         keep_prob=1.0)
+        if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
     
-    flags = tf.app.flags
-flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
-                    'Directory for saving data.')
-flags.DEFINE_string('datafile_name', 'conditioned_rnn_data',
-                    'Name of data file for input case.')
-flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
-flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
-flags.DEFINE_integer('C', 400, 'Number of conditions')
-flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
-flags.DEFINE_float('train_percentage', 4.0/5.0,
-                   'Percentage of train vs validation trials')
-flags.DEFINE_integer('nreplications', 10,
-                     'Number of spikifications of the same underlying rates.')
-flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
-flags.DEFINE_float('x0_std', 1.0,
-                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
-flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
-flags.DEFINE_float('dt', 0.010, 'Time bin')
-flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
-FLAGS = flags.FLAGS
-    
-      full_name_stem = os.path.join(data_path, data_fname_stem)
-  for s, data_dict in dataset_dict.items():
-    write_data(full_name_stem + '_' + s, data_dict, compression=compression)
-    
-        Args:
-      filename: Vocabulary file name.
-    '''
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-    
-      Returns:
-    A decoder function with the required interface of `dynamic_rnn_decoder`
-    intended for training.
-  '''
-  with tf.name_scope(name, 'attention_decoder_fn_train', [
-      encoder_state, attention_keys, attention_values, attention_score_fn,
-      attention_construct_fn
-  ]):
-    pass
+        app.logger.info('\n'.join(info))
     
     
-class RedirectFallbackMiddleware(MiddlewareMixin):
-    # Defined as class-level attributes to be subclassing-friendly.
-    response_gone_class = HttpResponseGone
-    response_redirect_class = HttpResponsePermanentRedirect
+def _wrap_writer_for_text(fp, encoding):
+    try:
+        fp.write('')
+    except TypeError:
+        fp = io.TextIOWrapper(fp, encoding)
+    return fp
     
-    KEY_PREFIX = 'django.contrib.sessions.cache'
+        def check(self, value):
+        return (
+            isinstance(value, dict)
+            and len(value) == 1
+            and next(iter(value)) in self.serializer.tags
+        )
     
-        def flush(self):
-        '''
-        Remove the current session data from the database and regenerate the
-        key.
-        '''
-        self.clear()
-        self.delete(self.session_key)
-        self._session_key = None
+        before_render_template.send(app, template=template, context=context)
+    rv = template.render(context)
+    template_rendered.send(app, template=template, context=context)
+    return rv
+    
+        rv = parse_changelog()
+    
+    
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
+    proc.sendline(u'fish')
+    return proc
+    
+    # TODO: ensure that history changes.
 
     
+    apt-get is a simple command line interface for downloading and
+installing packages. The most frequently used commands are update
+and install.
     
-class SessionStore(SessionBase):
+    
+@pytest.mark.skipif(_is_not_okay_to_test(),
+                    reason='No need to run if there\'s no formula')
+def test_get_new_command(brew_no_available_formula):
+    assert get_new_command(Command('brew install elsticsearch',
+                                   brew_no_available_formula))\
+        == 'brew install elasticsearch'
+    
+        def __init__(self, groups, env=Environment(), **kwargs):
+        '''
+        :param groups: names of processor groups to be applied
+        :param env: Environment
+        :param kwargs: additional keyword arguments for processors
+    
+    
+def build_output_stream(args, env, request, response, output_options):
+    '''Build and return a chain of iterators over the `request`-`response`
+    exchange each of which yields `bytes` chunks.
+    
+        # If both `auth_parse` and `prompt_password` are set to `True`,
+    # and the value of `-a` lacks the password part,
+    # then the user will be prompted to type the password in.
+    prompt_password = True
+    
+        @staticmethod
+    def make_header(username, password):
+        credentials = u'%s:%s' % (username, password)
+        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
+        return 'Basic %s' % token
+    
+    
+with open(BIN_FILE_PATH, 'rb') as f:
+    BIN_FILE_CONTENT = f.read()
+    
+    
+@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
+def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
+    r = http(follow_flag, httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 1
+    assert HTTP_OK in r
+    
+        def test_request_body_from_file_by_path(self, httpbin):
+        r = http('--verbose',
+                 'POST', httpbin.url + '/post', '@' + FILE_PATH_ARG)
+        assert HTTP_OK in r
+        assert FILE_CONTENT in r, r
+        assert ''Content-Type': 'text/plain'' in r
+    
+        if args.auth_plugin:
+        session.auth = {
+            'type': args.auth_plugin.auth_type,
+            'raw_auth': args.auth_plugin.raw_auth,
+        }
+    elif session.auth:
+        kwargs['auth'] = session.auth
+    
+            return new_key
+
+    
+    try:
+	xrange		#Python 2
+except NameError:
+	xrange = range	#Python 3
+    
+        for i in range(1, n+1):
+        dp[i][0] = True
+    
+    def main():
     '''
-    Implement database session store.
+    In this demonstration we're generating a sample data set from the sin function in numpy.
+    We then train a decision tree on the data set and use the decision tree to predict the
+    label of 10 different test values. Then the mean squared error over this test is displayed.
     '''
-    def __init__(self, session_key=None):
-        super().__init__(session_key)
+    X = np.arange(-1., 1., 0.005)
+    y = np.sin(X)
+    
+    
+class Migration(DataMigration):
+    def forwards(self, orm):
+        db.commit_transaction()
+        try:
+            self._forwards(orm)
+        except Exception:
+            # Explicitly resume the transaction because
+            # South is going to try and roll it back, but when
+            # it can't find one, it'll error itself, masking
+            # the actual exception being raised
+            #
+            # See https://github.com/getsentry/sentry/issues/5035
+            db.start_transaction()
+            raise
+        db.start_transaction()
+    
+    
+class Migration(DataMigration):
+    def forwards(self, orm):
+        'Write your forwards methods here.'
+        db.commit_transaction()
+    
+        def backwards(self, orm):
+    
+            # Adding model 'ReprocessingReport'
+        db.create_table(
+            'sentry_reprocessingreport', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.Project']
+                    )
+                ), (
+                    'event_id',
+                    self.gf('django.db.models.fields.CharField')(max_length=32, null=True)
+                ), (
+                    'datetime',
+                    self.gf('django.db.models.fields.DateTimeField')()
+                ),
+            )
+        )
+        db.send_create_signal('sentry', ['ReprocessingReport'])
+    
+            db.start_transaction()
+    
+        def backwards(self, orm):
+        # Deleting field 'ApiGrant.scope_list'
+        db.delete_column('sentry_apigrant', 'scope_list')
+    
+        complete_apps = ['sentry']
+
+    
+            # Adding unique constraint on 'CommitAuthor', fields ['organization_id', 'external_id']
+        db.create_unique('sentry_commitauthor', ['organization_id', 'external_id'])
