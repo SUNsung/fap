f1db99dce1366ca3623e68d3dba7bd6d36ace49f
SUNsung <weatherius@gmail.com>
2019-02-04 17:01:21 +0200

    Rewrite [563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp] File in: 2019-02-04 17:01:19

--- cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
+++ cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
@@ -1,129 +1,103 @@
 
         
-        class AutoCompactTest {
- public:
-  std::string dbname_;
-  Cache* tiny_cache_;
-  Options options_;
-  DB* db_;
-    }
-    
-          for (int compact_start = 0; compact_start < N; compact_start += 10) {
-        for (int i = 0; i < N; i += 10) {
-          ASSERT_TRUE(Between(Size('', Key(i)), S1*i, S2*i));
-          ASSERT_TRUE(Between(Size('', Key(i)+'.suffix'), S1*(i+1), S2*(i+1)));
-          ASSERT_TRUE(Between(Size(Key(i), Key(i+10)), S1*10, S2*10));
-        }
-        ASSERT_TRUE(Between(Size('', Key(50)), S1*50, S2*50));
-        ASSERT_TRUE(Between(Size('', Key(50)+'.suffix'), S1*50, S2*50));
-    }
-    
-    void InternalKeyComparator::FindShortestSeparator(
-      std::string* start,
-      const Slice& limit) const {
-  // Attempt to shorten the user portion of the key
-  Slice user_start = ExtractUserKey(*start);
-  Slice user_limit = ExtractUserKey(limit);
-  std::string tmp(user_start.data(), user_start.size());
-  user_comparator_->FindShortestSeparator(&tmp, user_limit);
-  if (tmp.size() < user_start.size() &&
-      user_comparator_->Compare(user_start, tmp) < 0) {
-    // User key has become shorter physically, but larger logically.
-    // Tack on the earliest possible number to the shortened user key.
-    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
-    assert(this->Compare(*start, tmp) < 0);
-    assert(this->Compare(tmp, limit) < 0);
-    start->swap(tmp);
-  }
-}
-    
-    static std::string Shorten(const std::string& s, const std::string& l) {
-  std::string result = s;
-  InternalKeyComparator(BytewiseComparator()).FindShortestSeparator(&result, l);
-  return result;
-}
-    
-    #endif  // STORAGE_LEVELDB_DB_LOG_WRITER_H_
-
-    
-    #endif  // STORAGE_LEVELDB_DB_MEMTABLE_H_
-
-    
-    TEST(RecoveryTest, LogFileReuse) {
-  if (!CanAppend()) {
-    fprintf(stderr, 'skipping test because env does not support appending\n');
-    return;
-  }
-  for (int i = 0; i < 2; i++) {
-    ASSERT_OK(Put('foo', 'bar'));
-    if (i == 0) {
-      // Compact to ensure current log is empty
-      CompactMemTable();
-    }
-    Close();
-    ASSERT_EQ(1, NumLogs());
-    uint64_t number = FirstLogFile();
-    if (i == 0) {
-      ASSERT_EQ(0, FileSize(LogName(number)));
-    } else {
-      ASSERT_LT(0, FileSize(LogName(number)));
-    }
-    Open();
-    ASSERT_EQ(1, NumLogs());
-    ASSERT_EQ(number, FirstLogFile()) << 'did not reuse log file';
-    ASSERT_EQ('bar', Get('foo'));
-    Open();
-    ASSERT_EQ(1, NumLogs());
-    ASSERT_EQ(number, FirstLogFile()) << 'did not reuse log file';
-    ASSERT_EQ('bar', Get('foo'));
-  }
-}
-    
-    int main(int argc, char** argv) {
-  ParseCommandLineFlags(&argc, &argv, true);
-    }
-    
-    Status WriteBatchBase::DeleteRange(ColumnFamilyHandle* column_family,
-                                   const SliceParts& begin_key,
-                                   const SliceParts& end_key) {
-  std::string begin_key_buf, end_key_buf;
-  Slice begin_key_slice(begin_key, &begin_key_buf);
-  Slice end_key_slice(end_key, &end_key_buf);
-  return DeleteRange(column_family, begin_key_slice, end_key_slice);
-}
-    
-    std::unique_ptr<WriteControllerToken>
-WriteController::GetCompactionPressureToken() {
-  ++total_compaction_pressure_;
-  return std::unique_ptr<WriteControllerToken>(
-      new CompactionPressureToken(this));
+        ServiceGenerator* ImmutableGeneratorFactory::NewServiceGenerator(
+    const ServiceDescriptor* descriptor) const {
+  return new ImmutableServiceGenerator(descriptor, context_);
 }
     
     
-    {#if defined(OS_LINUX) || defined(OS_MACOSX) || defined(OS_AIX)
-  virtual size_t GetUniqueId(char* id, size_t max_size) const override;
+    {  EXPECT_EQ(expected, decode_data.Data());
+}
+    
+    void OneofGenerator::GenerateClearFunctionImplementation(io::Printer* printer) {
+  printer->Print(
+      variables_,
+      'void $owning_message_class$_Clear$capitalized_name$OneOfCase($owning_message_class$ *message) {\n'
+      '  GPBDescriptor *descriptor = [message descriptor];\n'
+      '  GPBOneofDescriptor *oneof = [descriptor.oneofs objectAtIndex:$raw_index$];\n'
+      '  GPBMaybeClearOneof(message, oneof, $index$, 0);\n'
+      '}\n');
+}
+    
+    #ifdef _WIN32
+#include <fcntl.h>
+#else
+#include <unistd.h>
 #endif
-  virtual void Hint(AccessPattern pattern) override;
-  virtual Status InvalidateCache(size_t offset, size_t length) override;
-  virtual bool use_direct_io() const override { return use_direct_io_; }
-  virtual size_t GetRequiredBufferAlignment() const override {
-    return logical_sector_size_;
-  }
+    
+      reflection_tester.SetPackedFieldsViaReflection(message);
+  reflection_tester.ExpectPackedFieldsSetViaReflection(*message);
+    
+        int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(int);
+    
+            int res = 1;
+        int index = nextDifferentCharacterIndex(nums, 1);
+        int i = 1;
+        while(index < nums.size()){
+            res ++;
+            nums[i++] = nums[index];
+            index = nextDifferentCharacterIndex(nums, index + 1);
+        }
+    
+    
+    {    return 0;
+}
+
+    
+    
+/// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
     
-    using namespace rocksdb;
     
-    #pragma once
+    {
+    {        return res;
+    }
+};
     
-      FlushBlockPolicy* NewFlushBlockPolicy(
-      const BlockBasedTableOptions& table_options,
-      const BlockBuilder& data_block_builder) const override;
+    using namespace std;
     
-      // Instead of creating a snapshot, take ownership of the input snapshot.
-  ManagedSnapshot(DB* db, const Snapshot* _snapshot);
     
-      // Submit a fire and forget jobs
-  // This allows to submit the same job multiple times
-  virtual void SubmitJob(const std::function<void()>&) = 0;
-  // This moves the function in for efficiency
-  virtual void SubmitJob(std::function<void()>&&) = 0;
+    {
+    {        if(node){
+            res.push_back(node->val);
+            preorderTraversal(node->left, res);
+            preorderTraversal(node->right, res);
+        }
+    }
+};
+    
+    public:
+    vector<int> preorderTraversal(TreeNode* root) {
+    }
+    
+    int main() {
+    }
+    
+    
+    {    return 0;
+}
+
+    
+    
+    {
+    {
+    {            if(command.s == 'print')
+                res.push_back(command.node->val);
+            else{
+                assert(command.s == 'go');
+                stack.push(Command('print', command.node));
+                if(command.node->right)
+                    stack.push(Command('go',command.node->right));
+                if(command.node->left)
+                    stack.push(Command('go',command.node->left));
+            }
+        }
+        return res;
+    }
+};
