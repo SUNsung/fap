0f5f4ccf992c69b5016963a27506a5c51de40ed3
SUNsung <weatherius@gmail.com>
2019-02-05 12:29:59 +0200

    Rewrite [53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py] File in: 2019-02-05 12:29:59

--- pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
+++ pyton/53t7wbqJnF.3c53v0IlOA2BT6o53O9tGqIJloVk53obS.py
@@ -1,58 +1,393 @@
 
         
-            def __init__(self, cards):
-        self.cards = cards
+          def logp(self, z=None):
+    '''Compute the log-likelihood under the distribution.
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
+      Returns:
+    The dataset structures, with the field alignment_matrix_cxf added.
+    This is # channels x npcs dimension
+'''
+  nchannels_all = 0
+  channel_idxs = {}
+  conditions_all = {}
+  nconditions_all = 0
+  for name, dataset in datasets.items():
+    cidxs = np.where(dataset['P_sxn'])[1] # non-zero entries in columns
+    channel_idxs[name] = [cidxs[0], cidxs[-1]+1]
+    nchannels_all += cidxs[-1]+1 - cidxs[0]
+    conditions_all[name] = np.unique(dataset['condition_labels_train'])
     
-    
-def get_response(args, config_dir):
-    '''Send the request and return a `request.Response`.'''
-    
-        def get_auth_plugin(self, auth_type):
-        return self.get_auth_plugin_mapping()[auth_type]
-    
-    
-def humanize_bytes(n, precision=2):
-    # Author: Doug Latornell
-    # Licence: MIT
-    # URL: http://code.activestate.com/recipes/577081/
-    '''Return a humanized string representation of a number of bytes.
+      dataset_dict = {}
+  fnames = os.listdir(data_path)
     
     
-tests_require = [
-    # Pytest needs to come last.
-    # https://bitbucket.org/pypa/setuptools/issue/196/
-    'pytest-httpbin',
-    'pytest',
-    'mock',
-]
+# For saving demo resources, use batch size 1 and step 1.
+BATCH_SIZE = 1
+NUM_TIMESTEPS = 1
+MAX_WORD_LEN = 50
     
-        def test_download_interrupted(self, httpbin_both):
-        devnull = open(os.devnull, 'w')
-        downloader = Downloader(output_file=devnull, progress_file=devnull)
-        downloader.start(Response(
-            url=httpbin_both.url + '/',
-            headers={'Content-Length': 5}
-        ))
-        downloader.chunk_downloaded(b'1234')
-        downloader.finish()
-        assert downloader.interrupted
+      # Question is correctly answered only if
+  # all predictions of the same question_id is correct
+  num_correct_answer = 0
+  previous_qid = None
+  correctly_answered = False
+  for predict, qid in zip(prediction_correctness, question_ids):
+    if qid != previous_qid:
+      previous_qid = qid
+      num_correct_answer += int(correctly_answered)
+      correctly_answered = True
+    correctly_answered = correctly_answered and predict
+  num_correct_answer += int(correctly_answered)
+    
+      for _, value in gen_ngrams_dict.iteritems():
+    total_ngrams_produced += value
+    
+      ## Encoder variables.
+  encoder_lstm_w_0 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'dis/encoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel'
+  ][0]
+  encoder_lstm_b_0 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'dis/encoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias'
+  ][0]
+  encoder_lstm_w_1 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'dis/encoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel'
+  ][0]
+  encoder_lstm_b_1 = [
+      v for v in tf.trainable_variables() if v.op.name ==
+      'dis/encoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias'
+  ][0]
+    
+    
+class BtreeGinExtension(CreateExtension):
+    
+            # No redirect was found. Return the response.
+        return response
+
+    
+        def load(self):
+        '''
+        Load the data from the key itself instead of fetching from some
+        external data store. Opposite of _get_session_key(), raise BadSignature
+        if signature fails.
+        '''
+        try:
+            return signing.loads(
+                self.session_key,
+                serializer=self.serializer,
+                # This doesn't handle non-default expiry dates, see #19201
+                max_age=settings.SESSION_COOKIE_AGE,
+                salt='django.contrib.sessions.backends.signed_cookies',
+            )
+        except Exception:
+            # BadSignature, ValueError, or unpickling exceptions. If any of
+            # these happen, reset the session.
+            self.create()
+        return {}
+    
+    
+def deserialize(config, custom_objects=None):
+    return deserialize_keras_object(config,
+                                    module_objects=globals(),
+                                    custom_objects=custom_objects,
+                                    printable_module_name='regularizer')
+    
+    
+def test_mnist():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        assert len(x_train) == len(y_train) == 60000
+        assert len(x_test) == len(y_test) == 10000
+    
+            if 0 < self.dropout + self.recurrent_dropout:
+            if training is None:
+                h._uses_learning_phase = True
+    
+    
+for seq_index in range(100):
+    # Take one sequence (part of the training set)
+    # for trying out decoding.
+    input_seq = encoder_input_data[seq_index: seq_index + 1]
+    decoded_sentence = decode_sequence(input_seq)
+    print('-')
+    print('Input sentence:', input_texts[seq_index])
+    print('Decoded sentence:', decoded_sentence)
 
     
     
-@pytest.mark.parametrize('follow_flag', ['--follow', '-F'])
-def test_follow_without_all_redirects_hidden(httpbin, follow_flag):
-    r = http(follow_flag, httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 1
-    assert HTTP_OK in r
+def cnn_layers(inputs):
+    x = layers.Conv2D(32, (3, 3),
+                      activation='relu', padding='valid')(inputs)
+    x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+    x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+    x = layers.MaxPooling2D(pool_size=(2, 2))(x)
+    x = layers.Flatten()(x)
+    x = layers.Dense(512, activation='relu')(x)
+    x = layers.Dropout(0.5)(x)
+    predictions = layers.Dense(num_classes,
+                               activation='softmax',
+                               name='x_train_out')(x)
+    return predictions
     
-        def test_non_existent_file_raises_parse_error(self, httpbin):
-        with pytest.raises(ParseError):
-            http('--form',
-                 'POST', httpbin.url + '/post', 'foo@/__does_not_exist__')
+    '''
+    
+    DOCUMENTATION = '''
+---
+module: oneandone_monitoring_policy
+short_description: Configure 1&1 monitoring policy.
+description:
+     - Create, remove, update monitoring policies
+       (and add/remove ports, processes, and servers).
+       This module has a dependency on 1and1 >= 1.0
+version_added: '2.5'
+options:
+  state:
+    description:
+      - Define a monitoring policy's state to create, remove, update.
+    required: false
+    default: present
+    choices: [ 'present', 'absent', 'update' ]
+  auth_token:
+    description:
+      - Authenticating API token provided by 1&1.
+    required: true
+  api_url:
+    description:
+      - Custom API URL. Overrides the
+        ONEANDONE_API_URL environement variable.
+    required: false
+  name:
+    description:
+      - Monitoring policy name used with present state. Used as identifier (id or name) when used with absent state. maxLength=128
+    required: true
+  monitoring_policy:
+    description:
+      - The identifier (id or name) of the monitoring policy used with update state.
+    required: true
+  agent:
+    description:
+      - Set true for using agent.
+    required: true
+  email:
+    description:
+      - User's email. maxLength=128
+    required: true
+  description:
+    description:
+      - Monitoring policy description. maxLength=256
+    required: false
+  thresholds:
+    description:
+      - Monitoring policy thresholds. Each of the suboptions have warning and critical,
+        which both have alert and value suboptions. Warning is used to set limits for
+        warning alerts, critical is used to set critical alerts. alert enables alert,
+        and value is used to advise when the value is exceeded.
+    required: true
+    suboptions:
+      cpu:
+        description:
+          - Consumption limits of CPU.
+        required: true
+      ram:
+        description:
+          - Consumption limits of RAM.
+        required: true
+      disk:
+        description:
+          - Consumption limits of hard disk.
+        required: true
+      internal_ping:
+        description:
+          - Response limits of internal ping.
+        required: true
+      transfer:
+        description:
+          - Consumption limits for transfer.
+        required: true
+  ports:
+    description:
+      - Array of ports that will be monitoring.
+    required: true
+    suboptions:
+      protocol:
+        description:
+          - Internet protocol.
+        choices: [ 'TCP', 'UDP' ]
+        required: true
+      port:
+        description:
+          - Port number. minimum=1, maximum=65535
+        required: true
+      alert_if:
+        description:
+          - Case of alert.
+        choices: [ 'RESPONDING', 'NOT_RESPONDING' ]
+        required: true
+      email_notification:
+        description:
+          - Set true for sending e-mail notifications.
+        required: true
+  processes:
+    description:
+      - Array of processes that will be monitoring.
+    required: true
+    suboptions:
+      process:
+        description:
+          - Name of the process. maxLength=50
+        required: true
+      alert_if:
+        description:
+          - Case of alert.
+        choices: [ 'RUNNING', 'NOT_RUNNING' ]
+        required: true
+  add_ports:
+    description:
+      - Ports to add to the monitoring policy.
+    required: false
+  add_processes:
+    description:
+      - Processes to add to the monitoring policy.
+    required: false
+  add_servers:
+    description:
+      - Servers to add to the monitoring policy.
+    required: false
+  remove_ports:
+    description:
+      - Ports to remove from the monitoring policy.
+    required: false
+  remove_processes:
+    description:
+      - Processes to remove from the monitoring policy.
+    required: false
+  remove_servers:
+    description:
+      - Servers to remove from the monitoring policy.
+    required: false
+  update_ports:
+    description:
+      - Ports to be updated on the monitoring policy.
+    required: false
+  update_processes:
+    description:
+      - Processes to be updated on the monitoring policy.
+    required: false
+  wait:
+    description:
+      - wait for the instance to be in state 'running' before returning
+    required: false
+    default: 'yes'
+    type: bool
+  wait_timeout:
+    description:
+      - how long before wait gives up, in seconds
+    default: 600
+  wait_interval:
+    description:
+      - Defines the number of seconds to wait when using the _wait_for methods
+    default: 5
+    
+    RETURN = '''
+zone:
+  description: DNS zone as returned by IPA API.
+  returned: always
+  type: dict
+'''
+    
+        def role_add_host(self, name, item):
+        return self.role_add_member(name=name, item={'host': item})
+    
+        if module.params['environment']:
+        params['deploy[rails_env]'] = module.params['environment']
+    
+        module = AnsibleModule(
+        argument_spec=dict(
+            token=dict(required=True, no_log=True),
+            environment=dict(required=True),
+            user=dict(required=False),
+            repo=dict(required=False),
+            revision=dict(required=False),
+            url=dict(required=False, default='https://api.honeybadger.io/v1/deploys'),
+            validate_certs=dict(default='yes', type='bool'),
+        ),
+        supports_check_mode=True
+    )
+    
+    
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            path=dict(required=True),
+            state=dict(default='present', choices=['present', 'followed', 'absent', 'unfollowed']),
+            name=dict(required=False, default=None, type='str'),
+            logtype=dict(required=False, default=None, type='str', aliases=['type'])
+        ),
+        supports_check_mode=True
+    )
+    
+        def __init__(self, arr):
+        # we need a list not a string, so do something to change the type
+        self.array = arr.split(',')
+        print(('the input array is:', self.array))
+    
+    def compute_heterogeneity(data, k, centroids, cluster_assignment):
+    
+    heterogeneity = 0.0
+    for i in range(k):
+        
+        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
+        member_data_points = data[cluster_assignment==i, :]
+        
+        if member_data_points.shape[0] > 0: # check if i-th cluster is non-empty
+            # Compute distances from centroid to data points (RHS only)
+            distances = pairwise_distances(member_data_points, [centroids[i]], metric='euclidean')
+            squared_distances = distances**2
+            heterogeneity += np.sum(squared_distances)
+        
+    return heterogeneity
+    
+    # Find all the faces in the image using a pre-trained convolutional neural network.
+# This method is more accurate than the default HOG model, but it's slower
+# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
+# this will use GPU acceleration and perform well.
+# See also: find_faces_in_picture.py
+face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
+    
+    # Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+    
+    # Load a sample picture and learn how to recognize it.
+print('Loading known face image(s)')
+obama_image = face_recognition.load_image_file('obama_small.jpg')
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    
+            if len(encodings) == 0:
+            click.echo('WARNING: No faces found in {}. Ignoring file.'.format(file))
+        else:
+            known_names.append(basename)
+            known_face_encodings.append(encodings[0])
+    
+            faces_to_compare = [
+            face_encoding_a2,
+            face_encoding_a3,
+            face_encoding_b1]
+    
+        # Print the location of each facial feature in this image
+    for facial_feature in face_landmarks.keys():
+        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
+    
+    requirements = [
+    'face_recognition_models>=0.3.0',
+    'Click>=6.0',
+    'dlib>=19.7',
+    'numpy',
+    'Pillow'
+]
