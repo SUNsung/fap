d799f4579eba30331309d02a3f336c9e9262c3dc
SUNsung <weatherius@gmail.com>
2019-02-05 06:11:03 +0200

    Rewrite [563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp] File in: 2019-02-05 06:11:03

--- cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
+++ cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
@@ -1,103 +1,131 @@
 
         
-        ServiceGenerator* ImmutableGeneratorFactory::NewServiceGenerator(
-    const ServiceDescriptor* descriptor) const {
-  return new ImmutableServiceGenerator(descriptor, context_);
+        OPERATOR_SCHEMA(EnforceFinite)
+    .NumInputs(1)
+    .NumOutputs(0)
+    .SetDoc(R'DOC(
+Raise if there is NaN or Inf values in the input tensor.
+)DOC')
+    .Input(0, 'input', 'Input tensor');
+    
+    **Result**
+    
+    
+    {} // namespace caffe2
+    
+    namespace grpc {
+    }
+    
+    #include 'src/cpp/ext/filters/census/context.h'
+    
+    
+    {}  // namespace grpc
+
+    
+    Aggregation BytesDistributionAggregation() {
+  return Aggregation::Distribution(BucketBoundaries::Explicit(
+      {0, 1024, 2048, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216,
+       67108864, 268435456, 1073741824, 4294967296}));
 }
     
     
-    {  EXPECT_EQ(expected, decode_data.Data());
+    {  std::vector<const protobuf::FieldDescriptor*> extensions;
+  descriptor_pool_->FindAllExtensions(desc, &extensions);
+  for (auto it = extensions.begin(); it != extensions.end(); it++) {
+    response->add_extension_number((*it)->number());
+  }
+  response->set_base_type_name(type);
+  return Status::OK;
 }
     
-    void OneofGenerator::GenerateClearFunctionImplementation(io::Printer* printer) {
-  printer->Print(
-      variables_,
-      'void $owning_message_class$_Clear$capitalized_name$OneOfCase($owning_message_class$ *message) {\n'
-      '  GPBDescriptor *descriptor = [message descriptor];\n'
-      '  GPBOneofDescriptor *oneof = [descriptor.oneofs objectAtIndex:$raw_index$];\n'
-      '  GPBMaybeClearOneof(message, oneof, $index$, 0);\n'
-      '}\n');
-}
     
-    #ifdef _WIN32
-#include <fcntl.h>
-#else
-#include <unistd.h>
+    {b2PolyNode* b2PolyNode::GetRightestConnection(b2Vec2& incomingDir){
+	b2Vec2 diff = position-incomingDir;
+	b2PolyNode temp(diff);
+	b2PolyNode* res = GetRightestConnection(&temp);
+	b2Assert(res);
+	return res;
+}
+}
+
+    
+    	// ----------------------------------------------------------------------------------------------------
+	// calculate the error for the block by summing the pixel errors
+	//
+	void Block4x4Encoding::CalcBlockError(void)
+	{
+		m_fError = 0.0f;
+    }
+    
+    //OutRec: contains a path in the clipping solution. Edges in the AEL will
+//carry a pointer to an OutRec when they are part of the clipping solution.
+struct OutRec {
+  int       Idx;
+  bool      IsHole;
+  bool      IsOpen;
+  OutRec   *FirstLeft;  //see comments in clipper.pas
+  PolyNode *PolyNd;
+  OutPt    *Pts;
+  OutPt    *BottomPt;
+};
+    
+    #define C_SUBFROM( res , a)\
+    do {(res).r = ADD32((res).r,(a).r);  (res).i = SUB32((res).i,(a).i); \
+    }while(0)
+    
+    #define MULT16_32_PX(a, b, Q) MULT16_32_PX_(a, b, Q, __FILE__, __LINE__)
+static OPUS_INLINE int MULT16_32_PX_(int a, opus_int64 b, int Q, char *file, int line)
+{
+   opus_int64 res;
+   if (!VERIFY_SHORT(a) || !VERIFY_INT(b))
+   {
+      fprintf (stderr, 'MULT16_32_P%d: inputs are not short+int: %d %d in %s: line %d\n\n', Q, (int)a, (int)b, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
 #endif
-    
-      reflection_tester.SetPackedFieldsViaReflection(message);
-  reflection_tester.ExpectPackedFieldsSetViaReflection(*message);
-    
-        int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr)/sizeof(int);
-    
-            int res = 1;
-        int index = nextDifferentCharacterIndex(nums, 1);
-        int i = 1;
-        while(index < nums.size()){
-            res ++;
-            nums[i++] = nums[index];
-            index = nextDifferentCharacterIndex(nums, index + 1);
-        }
-    
-    
-    {    return 0;
+   }
+   if (ABS32(b)>=((opus_int64)(1)<<(15+Q)))
+   {
+      fprintf (stderr, 'MULT16_32_Q%d: second operand too large: %d %d in %s: line %d\n\n', Q, (int)a, (int)b,file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = ((((opus_int64)a)*(opus_int64)b) + (((opus_val32)(1)<<Q)>>1))>> Q;
+   if (!VERIFY_INT(res))
+   {
+      fprintf (stderr, 'MULT16_32_P%d: output is not int: %d*%d=%d in %s: line %d\n\n', Q, (int)a, (int)b,(int)res, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   if (Q==15)
+      celt_mips+=4;
+   else
+      celt_mips+=5;
+   return res;
 }
-
     
+    class UnicodeSet;
     
-/// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+    U_NAMESPACE_END
     
-    
-    {
-    {        return res;
-    }
-};
-    
-    using namespace std;
-    
-    
-    {
-    {        if(node){
-            res.push_back(node->val);
-            preorderTraversal(node->left, res);
-            preorderTraversal(node->right, res);
-        }
-    }
-};
-    
-    public:
-    vector<int> preorderTraversal(TreeNode* root) {
-    }
-    
-    int main() {
+        /**
+     * Get maximum significant digits. INT32_MAX means no maximum.
+     */
+    int32_t getMax() const {
+        return fMax;
     }
     
     
-    {    return 0;
+    {    // ...and if they're equal, we compare times
+    if (millis < ruleMillis) return -1;
+    else if (millis > ruleMillis) return 1;
+    else return 0;
 }
-
+    
+    //----------------------------------------------------------------------
     
     
-    {
-    {
-    {            if(command.s == 'print')
-                res.push_back(command.node->val);
-            else{
-                assert(command.s == 'go');
-                stack.push(Command('print', command.node));
-                if(command.node->right)
-                    stack.push(Command('go',command.node->right));
-                if(command.node->left)
-                    stack.push(Command('go',command.node->left));
-            }
-        }
-        return res;
-    }
-};
+    {    return outLen;
+}
