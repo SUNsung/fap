1d5fdf98afc6936cfd385c09f26d30d5579de783
SUNsung <weatherius@gmail.com>
2019-02-05 09:35:28 +0200

    Rewrite [12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp] File in: 2019-02-05 09:35:27

--- cccccc/12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp
+++ cccccc/12JUHXCrFxkl212wK9Gxs3ULM2127mtJS9K2s.A12KvN.cpp
@@ -1,141 +1,264 @@
 
         
-        static const Message* GetCProtoInsidePyProtoStub(PyObject* msg) {
-  return NULL;
-}
-static Message* MutableCProtoInsidePyProtoStub(PyObject* msg) {
-  return NULL;
-}
+        #endif // BITCOIN_QT_NETWORKSTYLE_H
+
     
-    #include <google/protobuf/compiler/csharp/csharp_doc_comment.h>
-#include <google/protobuf/compiler/csharp/csharp_enum.h>
-#include <google/protobuf/compiler/csharp/csharp_helpers.h>
-#include <google/protobuf/compiler/csharp/csharp_options.h>
     
-    TEST(CSharpEnumValue, PascalCasedPrefixStripping) {
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'FOO__BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'FOO_BAR_BAZ'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'Foo_BarBaz'));
-  EXPECT_EQ('Bar', GetEnumValueName('FO_O', 'FOO_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('FOO', 'F_O_O_BAR'));
-  EXPECT_EQ('Bar', GetEnumValueName('Foo', 'BAR'));
-  EXPECT_EQ('BarBaz', GetEnumValueName('Foo', 'BAR_BAZ'));
-  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO'));
-  EXPECT_EQ('Foo', GetEnumValueName('Foo', 'FOO___'));
-  // Identifiers can't start with digits
-  EXPECT_EQ('_2Bar', GetEnumValueName('Foo', 'FOO_2_BAR'));
-  EXPECT_EQ('_2', GetEnumValueName('Foo', 'FOO___2'));
-}
+    {private Q_SLOTS:
+    /* sign message */
+    void on_addressBookButton_SM_clicked();
+    void on_pasteButton_SM_clicked();
+    void on_signMessageButton_SM_clicked();
+    void on_copySignatureButton_SM_clicked();
+    void on_clearButton_SM_clicked();
+    /* verify message */
+    void on_addressBookButton_VM_clicked();
+    void on_verifyMessageButton_VM_clicked();
+    void on_clearButton_VM_clicked();
+};
     
-        if (file_contains_extensions) {
-      printer->Print(
-          'static GPBExtensionDescription descriptions[] = {\n');
-      printer->Indent();
-      for (std::vector<ExtensionGenerator *>::iterator iter =
-               extension_generators_.begin();
-           iter != extension_generators_.end(); ++iter) {
-        (*iter)->GenerateStaticVariablesInitialization(printer);
-      }
-      for (std::vector<MessageGenerator *>::iterator iter =
-               message_generators_.begin();
-           iter != message_generators_.end(); ++iter) {
-        (*iter)->GenerateStaticVariablesInitialization(printer);
-      }
-      printer->Outdent();
-      printer->Print(
-          '};\n'
-          'for (size_t i = 0; i < sizeof(descriptions) / sizeof(descriptions[0]); ++i) {\n'
-          '  GPBExtensionDescriptor *extension =\n'
-          '      [[GPBExtensionDescriptor alloc] initWithExtensionDescription:&descriptions[i]];\n'
-          '  [registry addExtension:extension];\n'
-          '  [self globallyRegisterExtension:extension];\n'
-          '  [extension release];\n'
-          '}\n');
+    /** Generate blocks (mine) */
+UniValue generateBlocks(std::shared_ptr<CReserveScript> coinbaseScript, int nGenerate, uint64_t nMaxTries, bool keepScript);
+    
+    /**
+ * Changelog:
+ * - March 2013, Diederik Huys:    original version
+ * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
+ * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
+ */
+    
+    int secp256k1_ecdh(const secp256k1_context* ctx, unsigned char *result, const secp256k1_pubkey *point, const unsigned char *scalar) {
+    int ret = 0;
+    int overflow = 0;
+    secp256k1_gej res;
+    secp256k1_ge pt;
+    secp256k1_scalar s;
+    VERIFY_CHECK(ctx != NULL);
+    ARG_CHECK(result != NULL);
+    ARG_CHECK(point != NULL);
+    ARG_CHECK(scalar != NULL);
     }
     
-    #include <map>
-#include <string>
-    
-    #include <iostream>
-#include <set>
-    
-    static uint32 ComputeCRC32(const string &buf) {
-  uint32 x = ~0U;
-  for (int i = 0; i < buf.size(); ++i) {
-    unsigned char c = buf[i];
-    x = kCRC32Table[(x ^ c) & 0xff] ^ (x >> 8);
-  }
-  return ~x;
+    void run_recovery_tests(void) {
+    int i;
+    for (i = 0; i < count; i++) {
+        test_ecdsa_recovery_api();
+    }
+    for (i = 0; i < 64*count; i++) {
+        test_ecdsa_recovery_end_to_end();
+    }
+    test_ecdsa_recovery_edge_cases();
 }
     
-    float SafeDoubleToFloat(double value) {
-  if (value > std::numeric_limits<float>::max()) {
-    return std::numeric_limits<float>::infinity();
-  } else if (value < -std::numeric_limits<float>::max()) {
-    return -std::numeric_limits<float>::infinity();
-  } else {
-    return static_cast<float>(value);
-  }
-}
-    
-      uint64 result = 0;
-  for (; *ptr != '\0'; ptr++) {
-    int digit = DigitValue(*ptr);
-    if (digit < 0 || digit >= base) {
-      // The token provided by Tokenizer is invalid. i.e., 099 is an invalid
-      // token, but Tokenizer still think it's integer.
-      return false;
-    }
-    if (digit > max_value || result > (max_value - digit) / base) {
-      // Overflow.
-      return false;
-    }
-    result = result * base + digit;
-  }
-    
-    
-    {                      ' trailing block comment\n'
-                      ' line 2\n'
-                      ' line 3\n',
-                      {},
-                      ' leading block comment\n'
-                      ' line 2\n'
-                      ' line 3 '
-                    },
-    
-    namespace mxnet {
-/*! \brief runtime functions for NDArray */
-class Imperative {
- public:
-  /*! \brief */
-  class AGInfo {
-   public:
-    Context ctx;
-    OpReqType grad_req;
-    OpStatePtr state;
-    std::vector<NDArray> outputs;
-    std::vector<NDArray> out_grads;
-    bool fresh_out_grad;
-    }
-    }
-    }
-    
-      std::string TypeString() const override {
-    return 'CaffeLoss';
-  }
-    
-    When `num_parts` and `part_index` are provided, the data is split into `num_parts` partitions,
-and the iterator only reads the `part_index`-th partition. However, the partitions are not
-guaranteed to be even.
-    
-    BOOST_FORCEINLINE void pause() BOOST_NOEXCEPT
+    static bool CaseInsensitiveEqual(const std::string &s1, const std::string &s2)
 {
-#if defined(_MSC_VER) && (defined(_M_AMD64) || defined(_M_IX86))
-    _mm_pause();
-#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-    __asm__ __volatile__('pause;');
-#endif
+    if (s1.size() != s2.size()) return false;
+    for (size_t i = 0; i < s1.size(); ++i) {
+        char c1 = s1[i];
+        if (c1 >= 'A' && c1 <= 'Z') c1 -= ('A' - 'a');
+        char c2 = s2[i];
+        if (c2 >= 'A' && c2 <= 'Z') c2 -= ('A' - 'a');
+        if (c1 != c2) return false;
+    }
+    return true;
 }
+    
+    const std::vector<std::string>& UniValue::getKeys() const
+{
+    if (typ != VOBJ)
+        throw std::runtime_error('JSON value is not an object as expected');
+    return keys;
+}
+    
+    template<>
+void SetDataGradToBlob<mshadow::gpu, float>(caffeMemoryTypes memType,
+                            std::vector<::caffe::Blob<float>*>::iterator blob,
+                            std::vector<TBlob>::const_iterator itr) {
+  float *data_ptr = reinterpret_cast<float*>((*itr).dptr_);
+  if (memType == Data)
+    (*blob)->set_gpu_data(data_ptr);
+  else
+    MXCAFFEBLOB(*blob, float)->set_gpu_diff(data_ptr);
+}
+    
+    MXNET_REGISTER_OP_PROPERTY(CaffeLoss, CaffeLossProp)
+.describe('Caffe loss layer')
+.add_arguments(CaffeLossParam::__FIELDS__());
+    
+    
+    {
+    {
+    {  /*!
+   * \brief Worker threads.
+   */
+  std::vector<std::thread> worker_threads_;
+  /*!
+   * \brief Startup synchronization objects
+   */
+  std::list<std::shared_ptr<dmlc::ManualEvent>> ready_events_;
+  /*!
+   * \brief Disallow default construction.
+   */
+  ThreadPool() = delete;
+  /*!
+   * \brief Disallow copy construction and assignment.
+   */
+  DISALLOW_COPY_AND_ASSIGN(ThreadPool);
+};
+}  // namespace engine
+}  // namespace mxnet
+#endif  // MXNET_ENGINE_THREAD_POOL_H_
+
+    
+    
+    {
+    {/*! \brief typedef the factory function of data iterator */
+typedef std::function<ImageAugmenter *()> ImageAugmenterFactory;
+/*!
+ * \brief Registry entry for DataIterator factory functions.
+ */
+struct ImageAugmenterReg
+    : public dmlc::FunctionRegEntryBase<ImageAugmenterReg,
+                                        ImageAugmenterFactory> {
+};
+//--------------------------------------------------------------
+// The following part are API Registration of Iterators
+//--------------------------------------------------------------
+/*!
+ * \brief Macro to register image augmenter
+ *
+ * \code
+ * // example of registering a mnist iterator
+ * REGISTER_IMAGE_AUGMENTER(aug_default)
+ * .describe('default augmenter')
+ * .set_body([]() {
+ *     return new DefaultAugmenter();
+ *   });
+ * \endcode
+ */
+#define MXNET_REGISTER_IMAGE_AUGMENTER(name)                            \
+  DMLC_REGISTRY_REGISTER(::mxnet::io::ImageAugmenterReg, ImageAugmenterReg, name)
+}  // namespace io
+}  // namespace mxnet
+#endif  // MXNET_USE_OPENCV
+    
+    /*! \brief create a batch iterator from single instance iterator */
+class BatchLoader : public IIterator<TBlobBatch> {
+ public:
+  explicit BatchLoader(IIterator<DataInst> *base):
+    head_(1), num_overflow_(0), base_(base) {
+  }
+    }
+    
+      // intialize iterator loads data in
+  virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) {
+    param_.InitAllowUnknown(kwargs);
+    CHECK_EQ(param_.data_shape.ndim(), 1) << 'dimension of data_shape is expected to be 1';
+    CHECK_GT(param_.num_parts, 0) << 'number of parts should be positive';
+    CHECK_GE(param_.part_index, 0) << 'part index should be non-negative';
+    data_parser_.reset(dmlc::Parser<uint64_t>::Create(param_.data_libsvm.c_str(),
+                                                      param_.part_index,
+                                                      param_.num_parts, 'libsvm'));
+    if (param_.label_libsvm != 'NULL') {
+      label_parser_.reset(dmlc::Parser<uint64_t>::Create(param_.label_libsvm.c_str(),
+                                                         param_.part_index,
+                                                         param_.num_parts, 'libsvm'));
+      CHECK_GT(param_.label_shape.Size(), 1)
+        << 'label_shape is not expected to be (1,) when param_.label_libsvm is set.';
+    } else {
+      CHECK_EQ(param_.label_shape.Size(), 1)
+        << 'label_shape is expected to be (1,) when param_.label_libsvm is NULL';
+    }
+    // both data and label are of CSRStorage in libsvm format
+    if (param_.label_shape.Size() > 1) {
+      out_.data.resize(6);
+    } else {
+      // only data is of CSRStorage in libsvm format.
+      out_.data.resize(4);
+    }
+  }
+    
+    int main(int, char**)
+{
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    }
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+        ALLEGRO_LOCKED_REGION *locked_img = al_lock_bitmap(img, al_get_bitmap_format(img), ALLEGRO_LOCK_WRITEONLY);
+    if (!locked_img)
+    {
+        al_destroy_bitmap(img);
+        return false;
+    }
+    memcpy(locked_img->data, pixels, sizeof(int)*width*height);
+    al_unlock_bitmap(img);
+    
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+    }
+    
+    // Data
+static int const                    NUM_FRAMES_IN_FLIGHT = 3;
+static FrameContext                 g_frameContext[NUM_FRAMES_IN_FLIGHT] = {};
+static UINT                         g_frameIndex = 0;
+    
+    // Called by Init/NewFrame/Shutdown
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateFontsTexture();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyFontsTexture();
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyDeviceObjects();
+
+    
+    
+    {    // exception out_of_range.401
+    try
+    {
+        // try to write beyond the array limit
+        array.at(5) = 'sixth';
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+}
+
+    
+        // out_of_range.403
+    try
+    {
+        // try to use a JSON pointer to an nonexistent object key
+        json::const_reference ref = j.at('/foo'_json_pointer);
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+      Byte t1(bytes + 3);
+  int32_t t = t1.get_byte(4, 4);
+    
+    int ObjectExtendedInfo60D::object_id(const std::uint8_t* bytes,
+                                     int32_t length) const {
+  Byte t0(bytes);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    OutputType RadarState201::output_type(const std::uint8_t* bytes,
+                                      int32_t length) const {
+  Byte t0(bytes + 5);
+  uint32_t x = t0.get_byte(2, 2);
+    }
+    
+    #include 'modules/localization/msf/local_map/base_map/base_map_matrix.h'
+    
+    #include 'modules/planning/common/speed_limit.h'
+    
+    NodeWithRange::NodeWithRange(const TopoNode* node, const NodeSRange& range)
+    : NodeSRange(range), topo_node_(node) {}
