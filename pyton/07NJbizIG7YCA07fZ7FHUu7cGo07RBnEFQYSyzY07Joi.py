ea22c3cffb4ca2d55c072841f8ceec7c8c8ad531
SUNsung <weatherius@gmail.com>
2019-02-06 23:04:57 +0200

    Rewrite [07NJbizIG7YCA07fZ7FHUu7cGo07RBnEFQYSyzY07Joi.py] File in: 2019-02-06 23:04:56

--- pyton/07NJbizIG7YCA07fZ7FHUu7cGo07RBnEFQYSyzY07Joi.py
+++ pyton/07NJbizIG7YCA07fZ7FHUu7cGo07RBnEFQYSyzY07Joi.py
@@ -1,186 +1,98 @@
-    # Returns
-        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
-    '''
-    dirname = 'cifar-10-batches-py'
-    origin = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-    path = get_file(dirname, origin=origin, untar=True)
+
+        
+                    handler_result = self.handler(sock)
     
-        # Raises
-        ValueError: in case of invalid `label_mode`.
-    '''
-    if label_mode not in ['fine', 'coarse']:
-        raise ValueError('`label_mode` must be one of `'fine'`, `'coarse'`.')
-    
-        outputs1 = Lambda(lambda x: utils.preprocess_input(x, 'channels_last'),
-                      output_shape=x.shape[1:])(inputs)
-    model1 = Model(inputs, outputs1)
-    out1 = model1.predict(x)
-    x2 = np.transpose(x, (0, 3, 1, 2))
-    inputs2 = Input(shape=x2.shape[1:])
-    outputs2 = Lambda(lambda x: utils.preprocess_input(x, 'channels_first'),
-                      output_shape=x2.shape[1:])(inputs2)
-    model2 = Model(inputs2, outputs2)
-    out2 = model2.predict(x2)
-    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
-    
-    from __future__ import print_function
-import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
-    
+    try:
+    import simplejson as json
+except ImportError:
     import json
     
-        html = get_content(url)
-    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
-    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+        with server as (host, port):
+        url = 'http://{}:{}/'.format(host, port)
+        r = requests.get(url, auth=auth)
+        # Verify server didn't receive auth from us.
+        assert r.status_code == 200
+        assert len(r.history) == 0
+        close_server.set()
     
-    __all__ = ['ckplayer_download']
+        # First check whether no_proxy is defined. If it is, check that the URL
+    # we're getting isn't in the no_proxy list.
+    no_proxy_arg = no_proxy
+    if no_proxy is None:
+        no_proxy = get_proxy('no_proxy')
+    parsed = urlparse(url)
+    
+    from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import boto3_conn, camel_dict_to_snake_dict
+from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info
+    
+    from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import camel_dict_to_snake_dict, get_aws_connection_info, boto3_conn
+import json
+import datetime
+import sys
+import re
+    
+        elif state == 'present':
+        for param in ('name', 'agent', 'email', 'thresholds', 'ports', 'processes'):
+            if not module.params.get(param):
+                module.fail_json(
+                    msg='%s parameter is required for a new monitoring policy.' % param)
+        try:
+            (changed, monitoring_policy) = create_monitoring_policy(module, oneandone_conn)
+        except Exception as ex:
+            module.fail_json(msg=str(ex))
+    
+            # manage host state
+        desired_state = self.get_parameter('state')
+        if bool(host):
+            current_state = host.STATE
+            current_state_name = HOST_STATES(host.STATE).name
+        else:
+            current_state = HOST_ABSENT
+            current_state_name = 'ABSENT'
+    
+        # Insert state-specific attributes to body
+    if state == 'started':
+        for k in ('source_system', 'env', 'owner', 'description'):
+            v = module.params[k]
+            if v is not None:
+                body[k] = v
+    
+    import homeassistant.helpers.config_validation as cv
+import homeassistant.util.dt as dt_util
+from homeassistant.components.device_tracker import (
+    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+    
+        def __init__(self, config):
+        '''Initialize the scanner.'''
+        self.last_results = []
+        host = config[CONF_HOST]
+        self._url = 'http://{}/data/getConnectInfo.asp'.format(host)
+        self._loginurl = 'http://{}/goform/login'.format(host)
+    
+    import voluptuous as vol
+    
+        def __init__(self, hass, filename, add_timestamp):
+        '''Initialize the service.'''
+        self.filepath = os.path.join(hass.config.config_dir, filename)
+        self.add_timestamp = add_timestamp
     
     
-def cntv_download(url, **kwargs):
-    if re.match(r'http://tv\.cntv\.cn/video/(\w+)/(\w+)', url):
-        rid = match1(url, r'http://tv\.cntv\.cn/video/\w+/(\w+)')
-    elif re.match(r'http://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
-        rid = r1(r'var guid = '(\w+)'', get_content(url))
-    elif re.match(r'http://\w+\.cntv\.cn/(\w+/\w+/(classpage/video/)?)?\d+/\d+\.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/(\w+/)*VIDE\d+.shtml', url) or \
-         re.match(r'http://(\w+).cntv.cn/(\w+)/classpage/video/(\d+)/(\d+).shtml', url) or \
-         re.match(r'http://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/\d+/\d+/\d+/\w+.shtml', url): 
-        page = get_content(url)
-        rid = r1(r'videoCenterId','(\w+)'', page)
-        if rid is None:
-            guid = re.search(r'guid\s*=\s*'([0-9a-z]+)'', page).group(1)
-            rid = guid
-    elif re.match(r'http://xiyou.cntv.cn/v-[\w-]+\.html', url):
-        rid = r1(r'http://xiyou.cntv.cn/v-([\w-]+)\.html', url)
+def get_service(hass, config, discovery_info=None):
+    '''Get the GNTP notification service.'''
+    if config.get(CONF_APP_ICON) is None:
+        icon_file = os.path.join(os.path.dirname(__file__), '..', 'frontend',
+                                 'www_static', 'icons', 'favicon-192x192.png')
+        with open(icon_file, 'rb') as file:
+            app_icon = file.read()
     else:
-        raise NotImplementedError(url)
+        app_icon = config.get(CONF_APP_ICON)
     
-        @callback
-    def get(self, request, briefing_id):
-        '''Handle Alexa Flash Briefing request.'''
-        _LOGGER.debug('Received Alexa flash briefing request for: %s',
-                      briefing_id)
-    
-        def get_info_callback(items):
-        '''Add entities for already verified buttons.'''
-        addresses = items['bd_addr_of_verified_buttons'] or []
-        for address in addresses:
-            setup_button(hass, config, add_entities, client, address)
-    
-            devices = {}
-        for device in request.json()['status']:
-            try:
-                devices[device['Key']] = {
-                    'ip': device['IPAddress'],
-                    'mac': device['PhysAddress'],
-                    'host': device['Name'],
-                    'status': device['Active']
-                    }
-            except (KeyError, requests.exceptions.RequestException):
-                pass
-        return devices
-
-    
-                except requests.exceptions.ConnectionError:
-                _LOGGER.exception('ConnectionError occurred for %s', url)
-                hass.bus.fire(
-                    '{}_{}'.format(DOMAIN, DOWNLOAD_FAILED_EVENT), {
-                        'url': url,
-                        'filename': filename
-                        })
-    
-        def __call__(self, method):
-        '''Decorate a function.'''
-        @wraps(method)
-        async def wrapper(view, request, *args, **kwargs):
-            '''Wrap a request handler with data validation.'''
-            data = None
-            try:
-                data = await request.json()
-            except ValueError:
-                if not self._allow_empty or \
-                   (await request.content.read()) != b'':
-                    _LOGGER.error('Invalid JSON received.')
-                    return view.json_message('Invalid JSON.', 400)
-                data = {}
-    
-    
-def setup(hass, config):
-    '''Set up the LiteJet component.'''
-    from pylitejet import LiteJet
-    
-        logging.warning('Starting fetch with curl client')
-    curl_client = CurlAsyncHTTPClient()
-    curl_client.fetch('http://localhost:%d/' % options.port,
-                      callback=callback)
-    IOLoop.current().start()
-    
-        PATTERN = '''import_from< 'from' module_name='__future__' 'import' any >'''
-    
-                runpy.run_module(module, run_name='__main__', alter_sys=True)
-        elif mode == 'script':
-            with open(script) as f:
-                # Execute the script in our namespace instead of creating
-                # a new one so that something that tries to import __main__
-                # (e.g. the unittest module) will see names defined in the
-                # script instead of just those defined in this module.
-                global __file__
-                __file__ = script
-                # If __package__ is defined, imports may be incorrectly
-                # interpreted as relative to this module.
-                global __package__
-                del __package__
-                exec_in(f.read(), globals(), globals())
-    except SystemExit as e:
-        logging.basicConfig()
-        gen_log.info('Script exited with status %s', e.code)
-    except Exception as e:
-        logging.basicConfig()
-        gen_log.warning('Script exited with uncaught exception', exc_info=True)
-        # If an exception occurred at import time, the file with the error
-        # never made it into sys.modules and so we won't know to watch it.
-        # Just to make sure we've covered everything, walk the stack trace
-        # from the exception and watch every file.
-        for (filename, lineno, name, line) in traceback.extract_tb(sys.exc_info()[2]):
-            watch(filename)
-        if isinstance(e, SyntaxError):
-            # SyntaxErrors are special:  their innermost stack frame is fake
-            # so extract_tb won't see it and we have to get the filename
-            # from the exception object.
-            watch(e.filename)
-    else:
-        logging.basicConfig()
-        gen_log.info('Script exited normally')
-    # restore sys.argv so subsequent executions will include autoreload
-    sys.argv = original_argv
-    
-    
-class CurlAsyncHTTPClient(AsyncHTTPClient):
-    def initialize(  # type: ignore
-        self, max_clients: int = 10, defaults: Dict[str, Any] = None
-    ) -> None:
-        super(CurlAsyncHTTPClient, self).initialize(defaults=defaults)
-        self._multi = pycurl.CurlMulti()
-        self._multi.setopt(pycurl.M_TIMERFUNCTION, self._set_timeout)
-        self._multi.setopt(pycurl.M_SOCKETFUNCTION, self._handle_socket)
-        self._curls = [self._curl_create() for i in range(max_clients)]
-        self._free_list = self._curls[:]
-        self._requests = (
-            collections.deque()
-        )  # type: Deque[Tuple[HTTPRequest, Callable[[HTTPResponse], None], float]]
-        self._fds = {}  # type: Dict[int, int]
-        self._timeout = None  # type: Optional[object]
-    
-        def consume(self, count: int = None) -> str:
-        if count is None:
-            count = len(self.text) - self.pos
-        newpos = self.pos + count
-        self.line += self.text.count('\n', self.pos, newpos)
-        s = self.text[self.pos : newpos]
-        self.pos = newpos
-        return s
+                # Close socket
+            sock.close()
+        except socket.gaierror:
+            _LOGGER.error('Unable to connect to host %s', self._host)
+        except socket.error:
+            _LOGGER.exception('Failed to send data to Lannnouncer')
