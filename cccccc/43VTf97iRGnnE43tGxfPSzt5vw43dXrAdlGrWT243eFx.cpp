6773b9d66abf40525d137b8b51d9a591f57ed136
SUNsung <weatherius@gmail.com>
2019-02-06 05:22:03 +0200

    Rewrite [43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp] File in: 2019-02-06 05:22:02

--- cccccc/43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp
+++ cccccc/43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp
@@ -1,251 +1,117 @@
 
         
-          XGBOOST_DEVICE GradientPairInternal() : grad_(0), hess_(0) {}
-    
-    /*!
- * \brief Registry entry for linear updater.
- */
-struct LinearUpdaterReg
-    : public dmlc::FunctionRegEntryBase<LinearUpdaterReg,
-                                        std::function<LinearUpdater*()> > {};
-    
-    // Forward declarations
-namespace xgboost {
-class TreeUpdater;
-}
-    
-    #include <dmlc/registry.h>
-#include <functional>
-#include <vector>
-#include <utility>
-#include <string>
-#include './base.h'
-#include './data.h'
-#include './tree_model.h'
-#include '../../src/common/host_device_vector.h'
-    
-    template<typename IndexType>
-class DensifyParser : public dmlc::Parser<IndexType> {
- public:
-  DensifyParser(dmlc::Parser<IndexType>* parser, uint32_t num_col)
-      : parser_(parser), num_col_(num_col) {
-  }
-    }
-    
-    namespace xgboost {
-namespace gbm {
-/*! \brief model parameters */
-struct GBTreeModelParam : public dmlc::Parameter<GBTreeModelParam> {
-  /*! \brief number of trees */
-  int num_trees;
-  /*! \brief number of roots */
-  int num_roots;
-  /*! \brief number of features to be used by trees */
-  int num_feature;
-  /*! \brief pad this space, for backward compatibility reason.*/
-  int pad_32bit;
-  /*! \brief deprecated padding space. */
-  int64_t num_pbuffer_deprecated;
-  /*!
-   * \brief how many output group a single instance can produce
-   *  this affects the behavior of number of output we have:
-   *    suppose we have n instance and k group, output will be k * n
-   */
-  int num_output_group;
-  /*! \brief size of leaf vector needed in tree */
-  int size_leaf_vector;
-  /*! \brief reserved parameters */
-  int reserved[32];
-  /*! \brief constructor */
-  GBTreeModelParam() {
-    std::memset(this, 0, sizeof(GBTreeModelParam));
-    static_assert(sizeof(GBTreeModelParam) == (4 + 2 + 2 + 32) * sizeof(int),
-                  '64/32 bit compatibility issue');
-  }
-  // declare parameters, only declare those that need to be set.
-  DMLC_DECLARE_PARAMETER(GBTreeModelParam) {
-    DMLC_DECLARE_FIELD(num_output_group)
-        .set_lower_bound(1)
-        .set_default(1)
-        .describe(
-            'Number of output groups to be predicted,'
-            ' used for multi-class classification.');
-    DMLC_DECLARE_FIELD(num_roots).set_lower_bound(1).set_default(1).describe(
-        'Tree updater sequence.');
-    DMLC_DECLARE_FIELD(num_feature)
-        .set_lower_bound(0)
-        .describe('Number of features used for training and prediction.');
-    DMLC_DECLARE_FIELD(size_leaf_vector)
-        .set_lower_bound(0)
-        .set_default(0)
-        .describe('Reserved option for vector tree.');
-  }
-};
-    }
-    }
-    
-    namespace xgboost {
-namespace common {
-TEST(CompressedIterator, Test) {
-  ASSERT_TRUE(detail::SymbolBits(256) == 8);
-  ASSERT_TRUE(detail::SymbolBits(150) == 8);
-  std::vector<int> test_cases = {1, 3, 426, 21, 64, 256, 100000, INT32_MAX};
-  int num_elements = 1000;
-  int repetitions = 1000;
-  srand(9);
-    }
-    }
-    }
+        // Version constant.
+// This is either 0 for python, 1 for CPP V1, 2 for CPP V2.
+//
+// 0 is default and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+//
+// 1 is set with -DPYTHON_PROTO2_CPP_IMPL_V1 and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+// and
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=1
+//
+// 2 is set with -DPYTHON_PROTO2_CPP_IMPL_V2 and is equivalent to
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
+// and
+//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
+#ifdef PYTHON_PROTO2_CPP_IMPL_V1
+#error 'PYTHON_PROTO2_CPP_IMPL_V1 is no longer supported.'
+#else
+#ifdef PYTHON_PROTO2_CPP_IMPL_V2
+static int kImplVersion = 2;
+#else
+#ifdef PYTHON_PROTO2_PYTHON_IMPL
+static int kImplVersion = 0;
+#else
     
     
     {
-    {// use R's PRNG to replacd
-CustomGlobalRandomEngine::result_type
-CustomGlobalRandomEngine::operator()() {
-  return static_cast<result_type>(
-      std::floor(unif_rand() * CustomGlobalRandomEngine::max()));
-}
-}  // namespace common
-}  // namespace xgboost
+    {
+    {}  // namespace python
+}  // namespace protobuf
+}  // namespace google
 
     
-    
-    {
-    {    // Eject all bytes
-    int tmp_bytes =
-        static_cast<int>(std::ceil(static_cast<float>(stored_bits) / 8));
-    for (int j = 0; j < tmp_bytes; j++) {
-      int shift_bits = static_cast<int>(stored_bits) - (j + 1) * 8;
-      if (shift_bits >= 0) {
-        buffer[buffer_position] =
-            static_cast<CompressedByteT>(tmp >> shift_bits);
-      } else {
-        buffer[buffer_position] =
-            static_cast<CompressedByteT>(tmp << std::abs(shift_bits));
-      }
-      buffer_position++;
+    void SharedCodeGenerator::GenerateDescriptors(io::Printer* printer) {
+  // Embed the descriptor.  We simply serialize the entire FileDescriptorProto
+  // and embed it as a string literal, which is parsed and built into real
+  // descriptors at initialization time.  We unfortunately have to put it in
+  // a string literal, not a byte array, because apparently using a literal
+  // byte array causes the Java compiler to generate *instructions* to
+  // initialize each and every byte of the array, e.g. as if you typed:
+  //   b[0] = 123; b[1] = 456; b[2] = 789;
+  // This makes huge bytecode files and can easily hit the compiler's internal
+  // code size limits (error 'code to large').  String literals are apparently
+  // embedded raw, which is what we want.
+  FileDescriptorProto file_proto;
+  file_->CopyTo(&file_proto);
     }
+    
+      // write central directory
+  io::CodedOutputStream output(raw_output_);
+  for (int i = 0; i < num_entries; ++i) {
+    const string &filename = files_[i].name;
+    uint16 filename_size = filename.size();
+    uint32 crc32 = files_[i].crc32;
+    uint32 size = files_[i].size;
+    uint32 offset = files_[i].offset;
+    }
+    
+    
+    {  GOOGLE_LOG_IF(DFATAL, end - start != text.size() || *start == '-')
+    << ' Tokenizer::ParseFloat() passed text that could not have been'
+       ' tokenized as a float: ' << CEscape(text);
+  return result;
+}
+    
+    OPERATOR_SCHEMA(EnforceFinite)
+    .NumInputs(1)
+    .NumOutputs(0)
+    .SetDoc(R'DOC(
+Raise if there is NaN or Inf values in the input tensor.
+)DOC')
+    .Input(0, 'input', 'Input tensor');
+    
+    REGISTER_CPU_OPERATOR(
+    MergeSingleListFeatureTensorsGradient,
+    MergeSingleListOrMapFeatureTensorsGradientOp<CPUContext>);
+OPERATOR_SCHEMA(MergeSingleListFeatureTensorsGradient)
+    .SetDoc(
+        'Explode multi-feature tensors with list features into '
+        'single-feature tensors.' +
+        doc)
+    .NumInputs([](int n) { return n >= 3 && n % 2 == 1; })
+    .NumOutputs([](int n) { return n >= 1; })
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_presence', '.presence')
+    .Input(2, 'out_values_values', '.values.values_grad')
+    .Output(0, 'out1_values', '.values_grad');
+REGISTER_GRADIENT(
+    MergeSingleListFeatureTensors,
+    GetMergeSingleListFeatureTensorsGradient);
+    
+    template <typename T, class Context>
+class FlexibleTopKGradientOp : public Operator<Context> {
+ public:
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+    }
+    
+    OPERATOR_SCHEMA(GivenTensorFill)
+    .NumInputs(0, 1)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .SetDoc(R'DOC(
+This op fills an output tensor with the data specified by the *value* and *dtype* arguments.  The output tensor shape is specified by the *shape* argument. Beware, when using this argument *value* should have a value for every element of the *output*, as missing values will not be initialized automatically. If *input_as_shape* is set to *true*, then the *input* should be a 1D tensor containing the desired output shape (the dimensions specified in *extra_shape* will also be appended). In this case, the *shape* argument should **not** be set.
+    
+    class GetIm2ColGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'Col2Im',
+        '',
+        std::vector<string>{GO(0), I(0)},
+        std::vector<string>{GI(0)});
   }
 };
-    
-    /**
- * @class CanClientFactory
- * @brief CanClientFactory inherites apollo::common::util::Facotory.
- */
-class CanClientFactory
-    : public apollo::common::util::Factory<CANCardParameter::CANCardBrand,
-                                           CanClient> {
- public:
-  /**
-   * @brief Register the CAN clients of all brands. This function call the
-   *        Function apollo::common::util::Factory::Register() for all of the
-   *        CAN clients.
-   */
-  void RegisterCanClients();
-    }
-    
-      void RecvThreadFunc() {
-    using common::time::Clock;
-    using common::time::AsInt64;
-    using common::time::micros;
-    using common::ErrorCode;
-    AINFO << 'Receive thread starting...';
-    TestCanParam *param = param_ptr();
-    CanClient *client = param->can_client;
-    int64_t start = 0;
-    std::vector<CanFrame> buf;
-    }
-    
-    /**
- * @class EsdCanClient
- * @brief The class which defines a ESD CAN client which inherits CanClient.
- */
-class EsdCanClient : public CanClient {
- public:
-  /**
-   * @brief Initialize the ESD CAN client by specified CAN card parameters.
-   * @param parameter CAN card parameters to initialize the CAN client.
-   * @return If the initialization is successful.
-   */
-  bool Init(const CANCardParameter &parameter) override;
-    }
-    
-    
-    {
-    {
-    {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    /**
- * @class FakeCanClient
- * @brief The class which defines a fake CAN client which inherits CanClient.
- *        This fake CAN client is used for testing.
- */
-class FakeCanClient : public CanClient {
- public:
-  /// Interval of sleeping
-  static const int32_t USLEEP_INTERVAL = 10000;
-    }
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-    }
-    }
-    }
-    
-      AINFO << 'The canbus conf file is loaded: ' << FLAGS_sensor_conf_file;
-  ADEBUG << 'Canbus_conf:' << canbus_conf_.ShortDebugString();
-    
-    
-    {  std::atomic<int> refCount{0};
-};
-    
-    namespace folly {
-    }
-    
-    
-    {  std::string arguments;
-  ASSERT_TRUE(getTracepointArguments(
-      'folly', 'test_static_tracepoint_many_arg_types', 0, arguments));
-  std::array<int, 8> expected{{
-      sizeof(uint32_t),
-      sizeof(uint32_t),
-      sizeof(bool),
-      sizeof(char),
-      sizeof(short),
-      sizeof(long),
-      sizeof(float),
-      sizeof(double),
-  }};
-  checkTracepointArguments(arguments, expected);
-}
-    
-    
-    {void Executor::keepAliveRelease() {
-  LOG(FATAL) << __func__ << '() should not be called for folly::Executor types '
-             << 'which do not override keepAliveAcquire()';
-}
-} // namespace folly
-
-    
-    namespace folly {
-    }
-    
-      // Test for overflow.
-  if (std::numeric_limits<decltype(tv.tv_sec)>::max() >=
-      std::numeric_limits<int64_t>::max()) {
-    // Use our own type alias here rather than std::chrono::nanoseconds
-    // to ensure we have 64-bit rep type.
-    using nsec_i64 = std::chrono::duration<int64_t, std::nano>;
-    tv.tv_sec = std::numeric_limits<decltype(tv.tv_sec)>::max();
-    tv.tv_usec = std::numeric_limits<decltype(tv.tv_usec)>::max();
-    EXPECT_THROW(to<nsec_i64>(tv), std::range_error);
-    }
-    
-    using namespace std;
-using namespace folly;
+REGISTER_GRADIENT(Im2Col, GetIm2ColGradient);
