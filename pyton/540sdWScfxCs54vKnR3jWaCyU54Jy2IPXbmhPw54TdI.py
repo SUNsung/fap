52930da10d11b37f5239983f23d092df70b7ea31
SUNsung <weatherius@gmail.com>
2019-02-06 13:49:56 +0200

    Rewrite [540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py] File in: 2019-02-06 13:49:56

--- pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
+++ pyton/540sdWScfxCs54vKnR3jWaCyU54Jy2IPXbmhPw54TdI.py
@@ -1,74 +1,43 @@
 
         
-        
-@pytest.mark.functional
-def test_how_to_configure_alias(proc, TIMEOUT):
-    proc.sendline('unset -f fuck')
-    how_to_configure(proc, TIMEOUT)
-
+            OPERATOR = 0
+    SUPERVISOR = 1
+    DIRECTOR = 2
+    
+        def __init__(self, user_id, name, pass_hash):
+        self.user_id = user_id
+        self.name = name
+        self.pass_hash = pass_hash
+        self.friends_by_id = {}  # key: friend id, value: User
+        self.friend_ids_to_private_chats = {}  # key: friend id, value: private chats
+        self.group_chats_by_id = {}  # key: chat id, value: GroupChat
+        self.received_friend_requests_by_friend_id = {}  # key: friend id, value: AddRequest
+        self.sent_friend_requests_by_friend_id = {}  # key: friend id, value: AddRequest
     
     
-@pytest.mark.parametrize('command', [
-    Command('apt-cache search foo', ''),
-    Command('aptitude search foo', ''),
-    Command('apt search foo', ''),
-    Command('apt-get install foo', ''),
-    Command('apt-get source foo', ''),
-    Command('apt-get clean', ''),
-    Command('apt-get remove', ''),
-    Command('apt-get update', ''),
-    Command('sudo apt update', no_match_output)
-])
-def test_not_match(command):
-    assert not match(command)
+class Categorizer(object):
     
     
-@parametrize_extensions
-@parametrize_filename
-@parametrize_script
-def test_side_effect(ext, tar_error, filename, unquoted, quoted, script, fixed):
-    tar_error(unquoted.format(ext))
-    side_effect(Command(script.format(filename.format(ext)), ''), None)
-    assert set(os.listdir('.')) == {unquoted.format(ext), 'd'}
+class HitCounts(MRJob):
     
-        def __eq__(self, other):
-        return (
-            isinstance(other, self.__class__) and
-            self.keys == other.keys and
-            self.messages == other.messages and
-            self.strict == other.strict
-        )
+            (category1, 1), product4
+        (category1, 2), product1
+        (category1, 3), product2
+        (category2, 3), product1
+        (category2, 7), product3
+        '''
+        category, product_id = key
+        quantity = value
+        yield (category, quantity), product_id
     
-        def create(self):
-        # Because a cache can fail silently (e.g. memcache), we don't know if
-        # we are failing to create a new session because of a key collision or
-        # because the cache is missing. So we try for a (large) number of times
-        # and then raise an exception. That's the risk you shoulder if using
-        # cache backing.
-        for i in range(10000):
-            self._session_key = self._get_new_session_key()
-            try:
-                self.save(must_create=True)
-            except CreateError:
-                continue
-            self.modified = True
-            return
-        raise RuntimeError(
-            'Unable to create a new session key. '
-            'It is likely that the cache is unavailable.')
     
-        def create(self):
-        while True:
-            self._session_key = self._get_new_session_key()
-            try:
-                # Save immediately to ensure we have a unique entry in the
-                # database.
-                self.save(must_create=True)
-            except CreateError:
-                # Key wasn't unique. Try again.
-                continue
-            self.modified = True
-            return
+class BtreeGinExtension(CreateExtension):
+    
+    
+class RedirectFallbackMiddleware(MiddlewareMixin):
+    # Defined as class-level attributes to be subclassing-friendly.
+    response_gone_class = HttpResponseGone
+    response_redirect_class = HttpResponsePermanentRedirect
     
         def exists(self, session_key=None):
         '''
@@ -78,230 +47,58 @@ def test_side_effect(ext, tar_error, filename, unquoted, quoted, script, fixed):
         '''
         return False
     
-        def __str__(self):
-        return self.session_key
+        @classmethod
+    def get_session_store_class(cls):
+        from django.contrib.sessions.backends.db import SessionStore
+        return SessionStore
     
-        lastmod = None
-    all_sites_lastmod = True
-    urls = []
-    for site in maps:
-        try:
-            if callable(site):
-                site = site()
-            urls.extend(site.get_urls(page=page, site=req_site,
-                                      protocol=req_protocol))
-            if all_sites_lastmod:
-                site_lastmod = getattr(site, 'latest_lastmod', None)
-                if site_lastmod is not None:
-                    site_lastmod = (
-                        site_lastmod.utctimetuple() if isinstance(site_lastmod, datetime.datetime)
-                        else site_lastmod.timetuple()
-                    )
-                    lastmod = site_lastmod if lastmod is None else max(lastmod, site_lastmod)
-                else:
-                    all_sites_lastmod = False
-        except EmptyPage:
-            raise Http404('Page %s empty' % page)
-        except PageNotAnInteger:
-            raise Http404('No page '%s'' % page)
-    response = TemplateResponse(request, template_name, {'urlset': urls},
-                                content_type=content_type)
-    if all_sites_lastmod and lastmod is not None:
-        # if lastmod is defined for all sites, set header so as
-        # ConditionalGetMiddleware is able to send 304 NOT MODIFIED
-        response['Last-Modified'] = http_date(timegm(lastmod))
-    return response
+        sites = []  # all sections' sitemap URLs
+    for section, site in sitemaps.items():
+        # For each section label, add links of all pages of its sitemap
+        # (usually generated by the `sitemap` view).
+        if callable(site):
+            site = site()
+        protocol = req_protocol if site.protocol is None else site.protocol
+        sitemap_url = reverse(sitemap_url_name, kwargs={'section': section})
+        absolute_url = '%s://%s%s' % (protocol, req_site.domain, sitemap_url)
+        sites.append(absolute_url)
+        # Add links to all pages of the sitemap.
+        for page in range(2, site.paginator.num_pages + 1):
+            sites.append('%s?p=%s' % (absolute_url, page))
+    
+    
+class InvalidHeader(RequestException, ValueError):
+    '''The header value provided was somehow invalid.'''
+    
+    # Import encoding now, to avoid implicit import later.
+# Implicit import within threads may cause LookupError when standard library is in a ZIP,
+# such as in Embedded Python. See https://github.com/requests/requests/issues/3578.
+import encodings.idna
+    
+        for node in doctree.traverse(is_setting_index):
+        targetnode = get_setting_target(node)
+        assert isinstance(targetnode, nodes.target), 'Next node is not a target'
+    
+        def parse(self, response):
+        pass
 
     
+    del sys
+
     
-# Find the most recent snapshot
-def _get_snapshot_starttime(snap):
-    return datetime.datetime.strptime(snap.start_time, '%Y-%m-%dT%H:%M:%S.000Z')
+        def help(self):
+        '''An extensive help for the command. It will be shown when using the
+        'help' command. It can contain newlines, since not post-formatting will
+        be applied to its contents.
+        '''
+        return self.long_desc()
     
-    try:
-    import boto3
-    import botocore
-    HAS_BOTO3 = True
-except ImportError:
-    HAS_BOTO3 = False
+    from scrapy.commands import ScrapyCommand
+from scrapy.exceptions import UsageError
     
-    
-def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            instance_id=dict(type='str', required=True),
-            state=dict(type='str', default='present', choices=['absent', 'present']),
-            database_name=dict(type='str'),
-            configuration=dict(type='str', required=True),
-            node_count=dict(type='int', default=1),
-            instance_display_name=dict(type='str'),
-            force_instance_delete=dict(type='bool', default=False),
-            service_account_email=dict(type='str'),
-            credentials_file=dict(type='str'),
-            project_id=dict(type='str'),
-        ),
-    )
-    
-        if not module.params.get('api_url'):
-        oneandone_conn = oneandone.client.OneAndOneService(
-            api_token=module.params.get('auth_token'))
-    else:
-        oneandone_conn = oneandone.client.OneAndOneService(
-            api_token=module.params.get('auth_token'), api_url=module.params.get('api_url'))
-    
-    try:
-    from pyvcloud.schema.vcd.v1_5.schemas.vcloud.networkType import FirewallRuleType
-    from pyvcloud.schema.vcd.v1_5.schemas.vcloud.networkType import ProtocolsType
-except ImportError:
-    # normally set a flag here but it will be caught when testing for
-    # the existence of pyvcloud (see module_utils/vca.py).  This just
-    # protects against generating an exception at runtime
-    pass
-    
-    - name: creating a new schema with specific schema owner
-  vertica_schema: name=schema_name owner=dbowner db=db_name state=present
-    
-            if host is not None:
-            changed = client.modify_if_diff(name, ipa_hbacrule.get('memberhost_host', []), host,
-                                            client.hbacrule_add_host,
-                                            client.hbacrule_remove_host, 'host') or changed
-    
-        if running and state == 'unmonitored':
-        if module.check_mode:
-            module.exit_json(changed=True)
-        status = run_command('unmonitor')
-        if status in ['not monitored'] or 'unmonitor pending' in status:
-            module.exit_json(changed=True, name=name, state=state)
-        module.fail_json(msg='%s process not unmonitored' % name, status=status)
-    
-        def test_distant_exception(self):
-        def f():
-            1/0
-        def g():
-            f()
-        def h():
-            g()
-        def i():
-            h()
-        def j(p):
-            i()
-        f_ident = ident(f)
-        g_ident = ident(g)
-        h_ident = ident(h)
-        i_ident = ident(i)
-        j_ident = ident(j)
-        self.check_events(j, [(1, 'call', j_ident),
-                              (2, 'call', i_ident),
-                              (3, 'call', h_ident),
-                              (4, 'call', g_ident),
-                              (5, 'call', f_ident),
-                              (5, 'return', f_ident),
-                              (4, 'return', g_ident),
-                              (3, 'return', h_ident),
-                              (2, 'return', i_ident),
-                              (1, 'return', j_ident),
-                              ])
+        def short_desc(self):
+        return 'Print Scrapy version'
     
     
-def main():
-    parser = ArgumentParser(description='''\
-Send the contents of a directory as a MIME message.
-Unless the -o option is given, the email is sent by forwarding to your local
-SMTP server, which then does the normal delivery process.  Your local machine
-must be running an SMTP server.
-''')
-    parser.add_argument('-d', '--directory',
-                        help='''Mail the contents of the specified directory,
-                        otherwise use the current directory.  Only the regular
-                        files in the directory are sent, and we don't recurse to
-                        subdirectories.''')
-    parser.add_argument('-o', '--output',
-                        metavar='FILE',
-                        help='''Print the composed message to FILE instead of
-                        sending the message to the SMTP server.''')
-    parser.add_argument('-s', '--sender', required=True,
-                        help='The value of the From: header (required)')
-    parser.add_argument('-r', '--recipient', required=True,
-                        action='append', metavar='RECIPIENT',
-                        default=[], dest='recipients',
-                        help='A To: header value (at least one required)')
-    args = parser.parse_args()
-    directory = args.directory
-    if not directory:
-        directory = '.'
-    # Create the message
-    msg = EmailMessage()
-    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
-    msg['To'] = ', '.join(args.recipients)
-    msg['From'] = args.sender
-    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
-    
-        publicKey, privateKey = generateKey(keySize)
-    print('\nWriting public key to file %s_pubkey.txt...' % name)
-    with open('%s_pubkey.txt' % name, 'w') as fo:
-        fo.write('%d,%d,%d,%d' % (publicKey[0], publicKey[1], publicKey[2], publicKey[3]))
-    
-    
-class QuadraticProbing(HashTable):
-    '''
-        Basic Hash Table example with open addressing using Quadratic Probing 
-    '''
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-        def __prepare__(self, N = 0, M = 0):
-        self.dp = [[-1 for y in range(0,M)] for x in range(0,N)]
-    
-    def longestSub(ARRAY): 			#This function is recursive
-	
-	ARRAY_LENGTH = len(ARRAY)
-	if(ARRAY_LENGTH <= 1):  	#If the array contains only one element, we return it (it's the stop condition of recursion)
-		return ARRAY
-								#Else
-	PIVOT=ARRAY[0]
-	isFound=False
-	i=1
-	LONGEST_SUB=[]
-	while(not isFound and i<ARRAY_LENGTH):
-		if (ARRAY[i] < PIVOT):
-			isFound=True
-			TEMPORARY_ARRAY = [ element for element in ARRAY[i:] if element >= ARRAY[i] ]
-			TEMPORARY_ARRAY = longestSub(TEMPORARY_ARRAY)
-			if ( len(TEMPORARY_ARRAY) > len(LONGEST_SUB) ):
-				LONGEST_SUB = TEMPORARY_ARRAY
-		else:
-			i+=1
-    
-    
-class SubArray:
-    
-                if (arr[i-1] <= j):
-                dp[i][j] = dp[i][j] or dp[i-1][j-arr[i-1]]
-    
-                self.decision_boundary = X[best_split]
-            self.left = Decision_Tree(depth = self.depth - 1, min_leaf_size = self.min_leaf_size)
-            self.right = Decision_Tree(depth = self.depth - 1, min_leaf_size = self.min_leaf_size)
-            self.left.train(left_X, left_y)
-            self.right.train(right_X, right_y)
-        else:
-            self.prediction = np.mean(y)
-    
-        difference = predict - actual
-    numerator = np.sum(difference) / len(predict) 
-    denumerator =  np.sum(actual) / len(predict)
-    print(numerator)
-    print(denumerator)
-    
-    '''
-* Wondering how this method works !
-* It's pretty simple.
-* Let's say you need to calculate a ^ b
-* RULE 1 : a ^ b = (a*a) ^ (b/2) ---- example : 4 ^ 4 = (4*4) ^ (4/2) = 16 ^ 2
-* RULE 2 : IF b is ODD, then ---- a ^ b = a * (a ^ (b - 1)) :: where (b - 1) is even.
-* Once b is even, repeat the process to get a ^ b
-* Repeat the process till b = 1 OR b = 0, because a^1 = a AND a^0 = 1
-*
-* As far as the modulo is concerned,
-* the fact : (a*b) % c = ((a%c) * (b%c)) % c
-* Now apply RULE 1 OR 2 whichever is required.
-'''
+def _get_boto_connection():
+    from boto.s3.connection import S3Connection
