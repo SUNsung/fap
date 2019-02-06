647480bf5d546a7efccd7ff289c6fcb8f05f8192
SUNsung <weatherius@gmail.com>
2019-02-06 03:38:33 +0200

    Rewrite [37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp] File in: 2019-02-06 03:38:32

--- cccccc/37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp
+++ cccccc/37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp
@@ -1,38 +1,188 @@
 
         
-        TEST(FormatTest, InternalKeyShortestSuccessor) {
-  ASSERT_EQ(IKey('g', kMaxSequenceNumber, kValueTypeForSeek),
-            ShortSuccessor(IKey('foo', 100, kTypeValue)));
-  ASSERT_EQ(IKey('\xff\xff', 100, kTypeValue),
-            ShortSuccessor(IKey('\xff\xff', 100, kTypeValue)));
+        #include <unordered_map>
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    
+    {  if (!register_ufunc('equal', CompareUFunc<Bfloat16EqFunctor>,
+                      compare_types)) {
+    return false;
+  }
+  if (!register_ufunc('not_equal', CompareUFunc<Bfloat16NeFunctor>,
+                      compare_types)) {
+    return false;
+  }
+  if (!register_ufunc('less', CompareUFunc<Bfloat16LtFunctor>, compare_types)) {
+    return false;
+  }
+  if (!register_ufunc('greater', CompareUFunc<Bfloat16GtFunctor>,
+                      compare_types)) {
+    return false;
+  }
+  if (!register_ufunc('less_equal', CompareUFunc<Bfloat16LeFunctor>,
+                      compare_types)) {
+    return false;
+  }
+  if (!register_ufunc('greater_equal', CompareUFunc<Bfloat16GeFunctor>,
+                      compare_types)) {
+    return false;
+  }
+  return true;
 }
     
-    enum RecordType {
-  // Zero is reserved for preallocated files
-  kZeroType = 0,
+    #ifndef PyObject_HEAD
+struct _object;
+typedef _object PyObject;
+#endif
+    
+      // Return the current record contents.  Only valid after the preceding call
+  // to GetNext() returned true
+  string record() const { return record_; }
+  // Return the current offset in the file.
+  uint64 offset() const { return offset_; }
+    
+    
+    {
+    {}  // namespace cuda
+}  // namespace stream_executor
+
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+            // returns current per-minibatch variance momentum value.
+        double VarianceMomentumValueForMB(size_t minibatchSize) const
+        {
+            //TODO: According to my preliminary analysis, the second momentum variance scaling is different from momentum scaling; need to double check -- yuqing tang
+            return MomentumValueForMB(m_varianceMomentumSchedule, minibatchSize);
+        }
+    
+    #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Utils.h'
+#include 'ReaderShim.h'
+#include 'DataReader.h'
+    
+    
+    {        GetMatrix()->AssignValuesOf(*source.GetMatrix());
     }
     
-        // ReadPhysicalRecord may have only had an empty trailer remaining in its
-    // internal buffer. Calculate the offset of the next physical record now
-    // that it has returned, properly accounting for its header size.
-    uint64_t physical_record_offset =
-        end_of_buffer_offset_ - buffer_.size() - kHeaderSize - fragment.size();
+        // this returns the map directly (read-only) and will lazily initialize it for a given seed
+    const std::vector<INDEXTYPE>& operator()(size_t seed) // throw()
+    {
+        // if wrong seed then lazily recache the sequence
+        if (seed != currentseed && randomizationrange != randomizeDisable)
+        {
+            // test for numeric overflow
+            if (map.size() - 1 != (INDEXTYPE)(map.size() - 1))
+                RuntimeError('RandomOrdering: INDEXTYPE has too few bits for this corpus');
+            // 0, 1, 2...
+            foreach_index (t, map)
+                map[t] = (INDEXTYPE) t;
+    }
+    }
     
-      size_t DroppedBytes() const {
-    return report_.dropped_bytes_;
+    
+    {            // Set the sorted rk order to each url and 
+            // the urls are still in the original order
+            int rk = 0;
+            for (it = its0; it != its; it++)
+            {
+                urls[it->m_rank0].m_rank = rk++;
+            }
+        }
+    
+    struct ID3D10Device;
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_IMPL_API void     ImGui_ImplDX9_InvalidateDeviceObjects();
+IMGUI_IMPL_API bool     ImGui_ImplDX9_CreateDeviceObjects();
+
+    
+    IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window, bool install_callbacks);
+IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window, bool install_callbacks);
+IMGUI_IMPL_API void     ImGui_ImplGlfw_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplGlfw_NewFrame();
+    
+    int32 ImGui_Marmalade_PointerButtonEventCallback(void* system_data, void* user_data)
+{
+    // pEvent->m_Button is of type s3ePointerButton and indicates which mouse
+    // button was pressed.  For touchscreen this should always have the value
+    // S3E_POINTER_BUTTON_SELECT
+    s3ePointerEvent* pEvent = (s3ePointerEvent*)system_data;
+    }
+    
+    
+    {    // At this point note that we set ImGui::GetIO().Fonts->TexID to be == g_FontTexture, so clear both.
+    ImGuiIO& io = ImGui::GetIO();
+    IM_ASSERT(g_FontTexture == io.Fonts->TexID);
+    if (g_FontTexture)
+        g_FontTexture->Release();
+    g_FontTexture = NULL;
+    io.Fonts->TexID = NULL;
+}
+    
+    // GLFW
+#include <GLFW/glfw3.h>
+#ifdef _WIN32
+#undef APIENTRY
+#define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3native.h>   // for glfwGetWin32Window
+#endif
+#define GLFW_HAS_WINDOW_TOPMOST     (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3200) // 3.2+ GLFW_FLOATING
+#define GLFW_HAS_WINDOW_HOVERED     (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3300) // 3.3+ GLFW_HOVERED
+#define GLFW_HAS_WINDOW_ALPHA       (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3300) // 3.3+ glfwSetWindowOpacity
+#define GLFW_HAS_PER_MONITOR_DPI    (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3300) // 3.3+ glfwGetMonitorContentScale
+#define GLFW_HAS_VULKAN             (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3200) // 3.2+ glfwCreateWindowSurface
+    
+    
+    {  void setTimeout(std::chrono::seconds timeout)
+  {
+    timeout_ = std::move(timeout);
+  }
+};
+    
+      virtual void fillMessage(Dict* msgDict) CXX11_OVERRIDE;
+    
+    
+    {} // namespace aria2
+
+    
+        dispatcher->setTimeout(std::chrono::seconds(messageTimeout));
+    
+    
+    {} // namespace aria2
+    
+    
+    {} // namespace aria2
+
+    
+    class DHTTaskFactoryImpl : public DHTTaskFactory {
+private:
+  std::shared_ptr<DHTNode> localNode_;
+    }
+    
+    namespace aria2 {
+    }
+    
+      DHTTaskExecutor immediateTaskQueue_;
+    
+      // do nothing; we don't use this message as outgoing message.
+  virtual bool send() CXX11_OVERRIDE;
+    
+    #include <glog/logging.h>
+    
+      FunctionRef<int(int)> fref = lambda;
+  EXPECT_EQ(1005, fref(5));
+  EXPECT_EQ(1011, fref(6));
+  EXPECT_EQ(1018, fref(7));
+    
+    #include <type_traits>
+    
+    
+    {    return newlocal;
   }
     
-      Status EmitPhysicalRecord(RecordType type, const char* ptr, size_t length);
-    
-        SnapshotImpl* snapshot = new SnapshotImpl(sequence_number);
-    
-        // out_of_range.401
-    try
-    {
-        // try to use a an invalid array index
-        json::reference ref = j.at('/array/4'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+    #pragma once
