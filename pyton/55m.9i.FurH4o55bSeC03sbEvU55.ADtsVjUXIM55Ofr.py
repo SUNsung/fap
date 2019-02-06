2e946720a8791c2eb8d61cbcf4439ce7641eb421
SUNsung <weatherius@gmail.com>
2019-02-06 19:39:02 +0200

    Rewrite [55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py] File in: 2019-02-06 19:39:01

--- pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
+++ pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
@@ -1,163 +1,170 @@
 
         
-            if var_is_trainable:
-      z_logit_var_1xn = tf.exp(z_logvar_1xn)
-      z_var_1xn = tf.nn.sigmoid(z_logit_var_1xn)*(var_max-var_min) + var_min
-      z_logvar_1xn = tf.log(z_var_1xn)
+        
+def finalize_headers(headers):
+    final_headers = {}
+    for name, value in headers.items():
+        if value is not None:
     
-    def _plot_item(W, name, full_name, nspaces):
-  plt.figure()
-  if W.shape == ():
-    print(name, ': ', W)
-  elif W.shape[0] == 1:
-    plt.stem(W.T)
-    plt.title(full_name)
-  elif W.shape[1] == 1:
-    plt.stem(W)
-    plt.title(full_name)
-  else:
-    plt.imshow(np.abs(W), interpolation='nearest', cmap='jet');
-    plt.colorbar()
-    plt.title(full_name)
-    
-      Args:
-    data_dir: The directory from which to load the datasets.
-    data_filename_stem: The stem of the filename for the datasets.
-    
-      if FLAGS.noise_type == 'poisson':
-    noisy_data = spikify_data(rates, rng, rnn['dt'], rnn['max_firing_rate'])
-  elif FLAGS.noise_type == 'gaussian':
-    noisy_data = gaussify_data(rates, rng, rnn['dt'], rnn['max_firing_rate'])
-  else:
-    raise ValueError('Only noise types supported are poisson or gaussian')
-    
-    for inp in inputs_ph_t:
-  output, state = model.call(state, inp)
-  outputs_t.append(output)
-  states_t.append(state)
-    
-      Returns:
-    log_sum_exp of the arguments.
-  '''
-  m = tf.reduce_max(x_k)
-  x1_k = x_k - m
-  u_k = tf.exp(x1_k)
-  z = tf.reduce_sum(u_k)
-  return tf.log(z) + m
-    
-        batch_size, num_timesteps = self.shape
-    softmax = softmax.reshape((num_timesteps, batch_size, -1))
-    softmax = np.transpose(softmax, [1, 0, 2])
-    probs = np.array([[softmax[row, col, target_ids[row, col]]
-                       for col in range(num_timesteps)]
-                      for row in range(batch_size)])
-    print(probs)
-    return probs
-    
-        if word:
-      tokenized.append(word)
-    tokenized += special_end_tokens
-    
-      Args:
-    gen_logits:  Generator logits.
-    gen_labels:  Labels for the correct token.
-    dis_values:  Discriminator values Tensor of shape [batch_size,
-      sequence_length].
-    is_real_input:  Tensor indicating whether the label is present.
-    
-      Args;
-    hparams:  Hyperparameters for the MaskGAN.
-    inputs:  tf.int32 Tensor of the sequence input of shape [batch_size,
-      sequence_length].
-    present:  tf.bool Tensor indicating the presence or absence of the token
-      of shape [batch_size, sequence_length].
-    is_training:  Whether the model is training.
-    is_validating:  Whether the model is being run in validation mode for
-      calculating the perplexity.
-    reuse (Optional):  Whether to reuse the model.
+        def get_formatters_grouped(self):
+        groups = {}
+        for group_name, group in groupby(
+                self.get_formatters(),
+                key=lambda p: getattr(p, 'group_name', 'format')):
+            groups[group_name] = list(group)
+        return groups
     
     
-def generate_mask():
-  '''Generate the mask to be fed into the model.'''
-  if FLAGS.mask_strategy == 'random':
-    p = np.random.choice(
-        [True, False],
-        size=[FLAGS.batch_size, FLAGS.sequence_length],
-        p=[FLAGS.is_present_rate, 1. - FLAGS.is_present_rate])
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
     
     
-      '''
-      if attention_option == 'bahdanau':
-        # transform query
-        query = tf.matmul(query, query_w)
+def test_follow_all_output_options_used_for_redirects(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 3
+    assert HTTP_OK not in r
+    
+                return _r
+    
+        def __init__(self, headers):
+        '''Make a MockResponse for `cookielib` to read.
+    
+        with server as (host, port):
+        url = u'http://{}:{}'.format(host, port)
+        r = requests.get(url=url, allow_redirects=True)
+        assert r.status_code == 200
+        assert len(r.history) == 1
+        assert r.history[0].status_code == 301
+        assert redirect_request[0].startswith(b'GET /' + expected_path + b' HTTP/1.1')
+        assert r.url == u'{}/{}'.format(url, expected_path.decode('ascii'))
+    
+    Some codes have multiple names, and both upper- and lower-case versions of
+the names are allowed. For example, ``codes.ok``, ``codes.OK``, and
+``codes.okay`` all correspond to the HTTP status code 200.
+'''
+    
+        # First check whether no_proxy is defined. If it is, check that the URL
+    # we're getting isn't in the no_proxy list.
+    no_proxy_arg = no_proxy
+    if no_proxy is None:
+        no_proxy = get_proxy('no_proxy')
+    parsed = urlparse(url)
     
     
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'git help')
+def barplot_neighbors(Nrange=2 ** np.arange(1, 11),
+                      Drange=2 ** np.arange(7),
+                      krange=2 ** np.arange(10),
+                      N=1000,
+                      D=64,
+                      k=5,
+                      leaf_size=30,
+                      dataset='digits'):
+    algorithms = ('kd_tree', 'brute', 'ball_tree')
+    fiducial_values = {'N': N,
+                       'D': D,
+                       'k': k}
+    
+    # Learn a frontier for outlier detection with several classifiers
+xx1, yy1 = np.meshgrid(np.linspace(-8, 28, 500), np.linspace(3, 40, 500))
+xx2, yy2 = np.meshgrid(np.linspace(3, 10, 500), np.linspace(-5, 45, 500))
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    plt.figure(1)
+    clf.fit(X1)
+    Z1 = clf.decision_function(np.c_[xx1.ravel(), yy1.ravel()])
+    Z1 = Z1.reshape(xx1.shape)
+    legend1[clf_name] = plt.contour(
+        xx1, yy1, Z1, levels=[0], linewidths=2, colors=colors[i])
+    plt.figure(2)
+    clf.fit(X2)
+    Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel()])
+    Z2 = Z2.reshape(xx2.shape)
+    legend2[clf_name] = plt.contour(
+        xx2, yy2, Z2, levels=[0], linewidths=2, colors=colors[i])
+    
+    # Author: Kemal Eren <kemal@kemaleren.com>
+# License: BSD 3 clause
+    
+    The plots display firstly what a K-means algorithm would yield
+using three clusters. It is then shown what the effect of a bad
+initialization is on the classification process:
+By setting n_init to only 1 (default is 10), the amount of
+times that the algorithm will be run with different centroid
+seeds is reduced.
+The next plot displays what using eight clusters would deliver
+and finally the ground truth.
+    
+    from sklearn import cluster
+    
+    def os_constant(key):
+    # XXX TODO: In the future, this could return different constants
+    #           based on what OS we are running under.  To see an
+    #           approach to how to handle different OSes, see the
+    #           apache version of this file.  Currently, we do not
+    #           actually have any OS-specific constants on Nginx.
+    '''
+    Get a constant value for operating system
+    
+        def setUp(self):
+        from acme.errors import PollError
+        self.timeout = PollError(
+            exhausted=set([mock.sentinel.AR]),
+            updated={})
+        self.invalid = PollError(exhausted=set(), updated={
+            mock.sentinel.AR: mock.sentinel.AR2})
+    
+        def test_default_decoder(self):
+        from acme.fields import RFC3339Field
+        self.assertEqual(
+            self.decoded, RFC3339Field.default_decoder(self.encoded))
+    
+        # TODO: decoder should check that nonce is in the protected header
     
     
-@pytest.mark.parametrize('command, new_command, packages', [
-    (Command('vim', ''), 'sudo apt-get install vim && vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')]),
-    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')])])
-def test_get_new_command(mocker, command, new_command, packages):
-    mocker.patch('thefuck.rules.apt_get._get_packages',
-                 create=True, return_value=packages)
+UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-apache-conf-digest.txt'
+'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
     
+        def __init__(self, filep, path, addrs, ssl, enabled, name=None,
+                 aliases=None, modmacro=False, ancestor=None):
     
-@pytest.mark.parametrize('command', [
-    Command('apt-cache search foo', ''),
-    Command('aptitude search foo', ''),
-    Command('apt search foo', ''),
-    Command('apt-get install foo', ''),
-    Command('apt-get source foo', ''),
-    Command('apt-get clean', ''),
-    Command('apt-get remove', ''),
-    Command('apt-get update', ''),
-    Command('sudo apt update', no_match_output)
-])
-def test_not_match(command):
-    assert not match(command)
+        def test_view_config_changes_error(self):
+        self.config.reverter.view_config_changes = mock.Mock(
+            side_effect=errors.ReverterError)
+        self.assertRaises(errors.PluginError, self.config.view_config_changes)
     
+        @certbot_util.patch_get_utility()
+    def test_select_cancel(self, mock_util):
+        mock_util().checklist.return_value = (display_util.CANCEL, 'whatever')
+        vhs = select_vhost_multiple([self.vhosts[2], self.vhosts[3]])
+        self.assertFalse(vhs)
     
-@pytest.mark.parametrize('command, result', [
-    (Command('aws dynamdb scan', misspelled_command),
-     ['aws dynamodb scan']),
-    (Command('aws dynamodb scn', misspelled_subcommand),
-     ['aws dynamodb scan']),
-    (Command('aws dynamodb t-item',
-             misspelled_subcommand_with_multiple_options),
-     ['aws dynamodb put-item', 'aws dynamodb get-item'])])
-def test_get_new_command(command, result):
-    assert get_new_command(command) == result
-
+            self.vhost2 = VirtualHost(
+            'fp', 'vhp', set([self.addr2]), False, False, 'localhost')
     
+    # The language for content autogenerated by Sphinx. Refer to documentation
+# for a list of supported languages.
+#
+# This is also used if you do content translation via gettext catalogs.
+# Usually you set 'language' from the command line for these cases.
+language = 'en'
     
-@pytest.mark.parametrize('before, after', [
-    ('brew install sshfs',
-     'brew cask install osxfuse && brew install sshfs')])
-def test_get_new_command(before, after):
-    command = Command(before, output)
-    assert get_new_command(command) == after
-
+    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
+# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
+# positive matches at the risk of more false negatives.
     
+        # Load the uploaded image file
+    img = face_recognition.load_image_file(file_stream)
+    # Get face encodings for any faces in the uploaded image
+    unknown_face_encodings = face_recognition.face_encodings(img)
     
-def makeKeyFiles(name, keySize):
-    if os.path.exists('%s_pubkey.txt' % name) or os.path.exists('%s_privkey.txt' % name):
-        print('\nWARNING:')
-        print(''%s_pubkey.txt' or '%s_privkey.txt' already exists. \n'
-              'Use a different name or delete these files and re-run this program.' %
-              (name, name))
-        sys.exit()
+    # Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file('obama.jpg')
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
     
-            a += a
-        b >>= 1
+        face_found = False
+    is_obama = False
