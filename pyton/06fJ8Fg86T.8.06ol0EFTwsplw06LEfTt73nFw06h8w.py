43cebbdf292493060456e57541613daada7f0e48
SUNsung <weatherius@gmail.com>
2019-02-06 10:14:32 +0200

    Rewrite [06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py] File in: 2019-02-06 10:14:30

--- pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
+++ pyton/06fJ8Fg86T.8.06ol0EFTwsplw06LEfTt73nFw06h8w.py
@@ -1,120 +1,179 @@
 
         
-        
-class CallCenter(object):
+        '''
+requests._internal_utils
+~~~~~~~~~~~~~~
     
-        def __init__(self):
-        self.head = None
-        self.tail = None
+            Number:                    '#990000',        # class: 'm'
     
-        def _find_available_spot(self, vehicle):
-        '''Find an available spot where vehicle can fit, or return None'''
-        pass
+                # Consume content and release the original connection
+            # to allow our new request to reuse the same one.
+            r.content
+            r.close()
+            prep = r.request.copy()
+            extract_cookies_to_jar(prep._cookies, r.request, r.raw)
+            prep.prepare_cookies(prep._cookies)
     
+        return {
+        'platform': platform_info,
+        'implementation': implementation_info,
+        'system_ssl': system_ssl_info,
+        'using_pyopenssl': pyopenssl is not None,
+        'pyOpenSSL': pyopenssl_info,
+        'urllib3': urllib3_info,
+        'chardet': chardet_info,
+        'cryptography': cryptography_info,
+        'idna': idna_info,
+        'requests': {
+            'version': requests_version,
+        },
+    }
     
-class Categorizer(object):
+        close_server = threading.Event()
+    server = Server(digest_failed_response_handler, wait_to_close_event=close_server)
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer),
-            self.mr(mapper=self.mapper_sort,
-                    reducer=self.reducer_identity),
-        ]
+        def test_server_finishes_when_no_connections(self):
+        '''the server thread exits even if there are no connections'''
+        server = Server.basic_response_server()
+        with server:
+            pass
     
-        def __init__(self, url, contents, child_urls):
-        self.url = url
-        self.contents = contents
-        self.child_urls = child_urls
-        self.signature = self.create_signature()
-    
-    # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
-        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        loaded_model = keras.models.load_model(model_filename)
-        loaded_model.predict(np.random.rand(128, 2))
-    
-    data_dim = 5
-num_classes = 2
-batch_size = 10
-    
-        - Divide the model's input(s) into multiple sub-batches.
-    - Apply a model copy on each sub-batch. Every model copy
-        is executed on a dedicated GPU.
-    - Concatenate the results (on CPU) into one big batch.
-    
-    model = Sequential()
-model.add(Embedding(max_features, 128, input_length=maxlen))
-model.add(Bidirectional(LSTM(64)))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
-    
-        # Apply some eyeliner
-    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
-    
-    
-def test_image(image_to_check, model):
-    unknown_image = face_recognition.load_image_file(image_to_check)
-    face_locations = face_recognition.face_locations(unknown_image, number_of_times_to_upsample=0, model=model)
+        @pytest.mark.parametrize(
+        'content', (
+            # HTML5 meta charset attribute
+            '<meta charset='UTF-8'>',
+            # HTML4 pragma directive
+            '<meta http-equiv='Content-type' content='text/html;charset=UTF-8'>',
+            # XHTML 1.x served with text/html MIME type
+            '<meta http-equiv='Content-type' content='text/html;charset=UTF-8' />',
+            # XHTML 1.x served as XML
+            '<?xml version='1.0' encoding='UTF-8'?>',
+        ))
+    def test_pragmas(self, content):
+        encodings = get_encodings_from_content(content)
+        assert len(encodings) == 1
+        assert encodings[0] == 'UTF-8'
     
     
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+# -- Options for Texinfo output -------------------------------------------
     
-            # Now let's list all the faces we found in all 128 frames
-        for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
-            number_of_faces_in_frame = len(face_locations)
+            self.headers = CaseInsensitiveDict()
+        if headers:
+            for header in headers.items():
+                # Raise exception on invalid header value.
+                check_header_validity(header)
+                name, value = header
+                self.headers[to_native_string(name)] = value
     
-    # 你需要一个2代以上的树莓派，并在树莓派上安装face_recognition，并连接上picamera摄像头
-# 并确保picamera这个模块已经安装（树莓派一般会内置安装）
-# 你可以参考这个教程配制你的树莓派：
-# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
+        scikit_results = []
+    glmnet_results = []
+    n = 20
+    step = 500
+    n_features = 1000
+    n_informative = n_features / 10
+    n_test_samples = 1000
+    for i in range(1, n + 1):
+        print('==================')
+        print('Iteration %s of %s' % (i, n))
+        print('==================')
     
-        if len(unknown_face_encodings) > 0:
-        face_found = True
-        # 看看图片中的第一张脸是不是奥巴马
-        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
-        if match_results[0]:
-            is_obama = True
+    
+def plot_feature_errors(all_errors, batch_size, all_components, data):
+    plt.figure()
+    plot_results(all_components, all_errors['pca'], label='PCA')
+    plot_results(all_components, all_errors['ipca'],
+                 label='IncrementalPCA, bsize=%i' % batch_size)
+    plt.legend(loc='lower left')
+    plt.suptitle('Algorithm error vs. n_components\n'
+                 'LFW, size %i x %i' % data.shape)
+    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
+    plt.ylabel('Mean absolute error')
+    
+                gc.collect()
+            print('- benchmarking RidgeRegression')
+            clf = Ridge(alpha=alpha, fit_intercept=False)
+            tstart = time()
+            clf.fit(X_train, y_train)
+            ridge_results[i, j, 0] = mean_squared_error(clf.predict(X_test),
+                                                        y_test)
+            ridge_results[i, j, 1] = time() - tstart
+    
+    # Predict the result on some short new sentences:
+sentences = [
+    u'This is a language detection test.',
+    u'Ceci est un test de d\xe9tection de la langue.',
+    u'Dies ist ein Test, um die Sprache zu erkennen.',
+]
+predicted = clf.predict(sentences)
+    
+    exercise_dir = os.path.dirname(__file__)
+if exercise_dir == '':
+    exercise_dir = '.'
+    
+    plt.matshow(fit_data, cmap=plt.cm.Blues)
+plt.title('After biclustering; rearranged to show biclusters')
+    
+    # The data that we are interested in is made of 8x8 images of digits, let's
+# have a look at the first 4 images, stored in the `images` attribute of the
+# dataset.  If we were working from image files, we could load them using
+# matplotlib.pyplot.imread.  Note that each image must have the same size. For these
+# images, we know which digit they represent: it is given in the 'target' of
+# the dataset.
+images_and_labels = list(zip(digits.images, digits.target))
+for index, (image, label) in enumerate(images_and_labels[:4]):
+    plt.subplot(2, 4, index + 1)
+    plt.axis('off')
+    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    plt.title('Training: %i' % label)
+    
+    import matplotlib.pyplot as plt
+import numpy as np
+    
+    if platform.system() in ('FreeBSD', 'Darwin'):
+    server_root_tmp = FREEBSD_DARWIN_SERVER_ROOT
+else:
+    server_root_tmp = LINUX_SERVER_ROOT
+    
+    # The master toctree document.
+master_doc = 'index'
+    
+            '''
+        if isinstance(other, self.__class__):
+            return ((self.tup == other.tup) or
+                    (self.tup[0] == other.tup[0] and
+                     self.is_wildcard() and other.is_wildcard()))
+        return False
+    
+    
+class TlsSniPerformTest(util.ApacheTest):
+    '''Test the ApacheTlsSni01 challenge.'''
+    
+    # Language to be used for generating the HTML full-text search index.
+# Sphinx supports the following languages:
+#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
+#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
+#html_search_language = 'en'
     
         complete_apps = ['sentry']
+    symmetrical = True
 
     
-            # Adding field 'Environment.organization_id'
-        db.add_column(
+            # Changing field 'Environment.organization_id'
+        db.alter_column(
             'sentry_environment',
             'organization_id',
-            self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True),
-            keep_default=False
+            self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)
         )
     
-            for environment in RangeQuerySetWrapperWithProgressBar(orm.Environment.objects.all()):
-            try:
-                with transaction.atomic():
-                    orm.EnvironmentProject.objects.create(
-                        environment=environment, project_id=environment.project_id
-                    )
-            except IntegrityError:
-                pass
-    
-            # User chose to not deal with backwards NULL issues for 'ReleaseEnvironment.project_id'
-        raise RuntimeError(
-            'Cannot reverse this migration. 'ReleaseEnvironment.project_id' and its values cannot be restored.'
-        )
-    
-        def _forwards(self, orm):
-        'Write your forwards methods here.'
-        dupe_envs = orm.Environment.objects.values_list('name', 'organization_id')\
-                                           .annotate(ecount=models.Count('id'))\
-                                           .filter(ecount__gt=1)
+            for release_project in RangeQuerySetWrapperWithProgressBar(
+            orm.ReleaseProject.objects.all()
+        ):
+            orm.ReleaseProject.objects.filter(id=release_project.id).update(
+                new_groups=orm.Group.objects.filter(
+                    project_id=release_project.project_id,
+                    first_release_id=release_project.release_id,
+                ).count()
+            )
     
             # Adding field 'ApiKey.scope_list'
         db.add_column(
@@ -122,6 +181,25 @@ def test_image(image_to_check, model):
             'scope_list',
             self.gf('sentry.db.models.fields.array.ArrayField')(
                 of=('django.db.models.fields.TextField', [], {})
+            ),
+            keep_default=False
+        )
+    
+            # Adding unique constraint on 'VersionDSymFile', fields ['dsym_file', 'version', 'build']
+        db.create_unique('sentry_versiondsymfile', ['dsym_file_id', 'version', 'build'])
+    
+            # Deleting model 'ReleaseHeadCommit'
+        db.delete_table('sentry_releaseheadcommit')
+    
+    
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding field 'UserOption.organization'
+        db.add_column(
+            'sentry_useroption',
+            'organization',
+            self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['sentry.Organization'], null=True
             ),
             keep_default=False
         )
