21adbf3c885468357ba113b63aca8d25155beb8f
SUNsung <weatherius@gmail.com>
2019-02-06 11:18:18 +0200

    Rewrite [05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb] File in: 2019-02-06 11:18:17

--- ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
+++ ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
@@ -1,64 +1,169 @@
 
         
-            def puts(*args)
-      STDERR.puts *args unless @silence
-    end
+        require 'rubygems'
+require 'rake'
+require 'rdoc'
+require 'date'
+require 'yaml'
     
-      # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
+COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
     
-        remove_duplicates
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
+    # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
+
     
-        it 'returns a 401 for a private post when logged out' do
-      bob.like!(@message)
-      sign_out :user
-      get :index, params: {post_id: @message.id}, format: :json
-      expect(response.status).to eq(401)
+          #
+    
+        def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
     end
   end
+end
+
     
-        # @param options [{Symbol => Object}] The options hash. See
-    #   {file:SASS_REFERENCE.md#Options the Sass options documentation}.
-    # @param parent [Environment] See \{#parent}
-    def initialize(parent = nil, options = nil)
-      @parent = parent
-      @options = options || (parent && parent.options) || {}
-      @stack = @parent.nil? ? Sass::Stack.new : nil
-      @caller = nil
-      @content = nil
-      @filename = nil
-      @functions = nil
-      @mixins = nil
-      @selector = nil
-      @vars = nil
+        def sort_fn(a, b)
+      if (a.getbyte(0) >= 49 && a.getbyte(0) <= 57) || (b.getbyte(0) >= 49 && b.getbyte(0) <= 57)
+        a_split = a.split(SPLIT_INTS)
+        b_split = b.split(SPLIT_INTS)
+    
+        private
+    
+            css('.c10', '.showcase', '.showcase-content', '.l-main-section', 'div.div', 'div[flex]', 'code-tabs', 'md-card', 'md-card-content', 'div:not([class])', 'footer', '.card-row', '.card-row-container', 'figure', 'blockquote', 'exported', 'defined', 'div.ng-scope', '.code-example header', 'section.desc', '.row', '.dart-api-entry-main', '.main-content', 'section.summary', 'span.signature').each do |node|
+          node.before(node.children).remove
+        end
+    
+      ruby_version_is '2.6' do
+    it 'raises RuntimeError when `exception: true` is given and the command exits with a non-zero exit status' do
+      lambda { @object.system(ruby_cmd('exit 1'), exception: true) }.should raise_error(RuntimeError)
     end
     
-    module Sass::Exec
-  # The abstract base class for Sass executables.
-  class Base
-    # @param args [Array<String>] The command-line arguments
-    def initialize(args)
-      @args = args
-      @options = {}
-    end
-    
-        # Whether path is likely to be meant as the destination
-    # in a source:dest pair.
-    def probably_dest_dir?(path)
-      return false unless path
-      return false if colon_path?(path)
-      Sass::Util.glob(File.join(path, '*.s[ca]ss')).empty?
-    end
-    
-          # A hash from file extensions to the syntaxes for those extensions.
-      # The syntaxes must be `:sass` or `:scss`.
-      #
-      # This can be overridden by subclasses that want normal filesystem importing
-      # with unusual extensions.
-      #
-      # @return [{String => Symbol}]
-      def extensions
-        {'sass' => :sass, 'scss' => :scss}
+        lambda {
+      catch :blah do
+        throw :blah, :return_value, 2, 3, 4, 5
       end
+    }.should raise_error(ArgumentError)
+  end
+    
+      it 'raises ArgumentError if no block or proc is provided' do
+    lambda do
+      trace_var :$Kernel_trace_var_global
+    end.should raise_error(ArgumentError)
+  end
+end
+
+    
+          it 'prepends a message with specified line from the backtrace' do
+        w = KernelSpecs::WarnInNestedCall.new
+    
+        case itype
+    when IAX_TYPE_DTMF_BEGIN
+      self.dprint('DTMF BEG: #{pkt[11,1]}')
+      self.dtmf << pkt[11,1]
+    
+            # Receives a kerberos response through the connection
+        #
+        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] the kerberos
+        #   response message
+        # @raise [RuntimeError] if the connection isn't established, the transport protocol is unknown, not supported
+        #   or the response can't be parsed
+        # @raise [NotImplementedError] if the transport protocol isn't supported
+        def recv_response
+          if connection.nil?
+            raise ::RuntimeError, 'Kerberos Client: connection not established'
+          end
+    
+              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
+          #
+          # @return [String] encoded time
+          def encode
+            encoded = ''
+            encoded << encode_auth_time
+            encoded << encode_start_time
+            encoded << encode_end_time
+            encoded << encode_renew_time
+    
+                data_encrypt = Rex::Text::rand_text(8) + data
+    
+              # Encodes the type
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type(type)
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+              def self.decode(input)
+            elem = self.new
+            elem.decode(input)
+          end
+    
+              # Decodes the Rex::Proto::Kerberos::Model::EncKdcResponse from an input
+          #
+          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [self] if decoding succeeds
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode(input)
+            case input
+            when String
+              decode_string(input)
+            when OpenSSL::ASN1::ASN1Data
+              decode_asn1(input)
+            else
+              raise ::RuntimeError, 'Failed to decode EncKdcResponse, invalid input'
+            end
+    
+              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_asn1_pvno(input)
+            input.value[0].value.to_i
+          end
+    
+        private
+    
+    # enable logging
+FPM::Util.send :module_function, :logger
+FPM::Util.logger.level = :info
+FPM::Util.logger.subscribe STDERR
+    
+      # This class is raised when you try to output a package to a path
+  # whose containing directory does not exist.
+  class ParentDirectoryMissing < StandardError
+    def to_s
+      return 'Parent directory does not exist: #{File.dirname(super)} - cannot write to #{super}'
+    end # def to_s
+  end # class ParentDirectoryMissing
+    
+        copy_metadata(source, destination)
+  end # def copy
+    
+        if gem_files.length != 1
+      raise 'Unexpected number of gem files in #{download_dir},  #{gem_files.length} should be 1'
+    end
+    
+      # Extract name and version from PackageInfo XML
+  def extract_info(package)
+    build_path('expand').tap do |path|
+      doc = REXML::Document.new File.open(File.join(path, 'PackageInfo'))
+      pkginfo_elem = doc.elements['pkg-info']
+      identifier = pkginfo_elem.attribute('identifier').value
+      self.version = pkginfo_elem.attribute('version').value
+      # set name to the last dot element of the identifier
+      self.name = identifier.split('.').last
+      logger.info('inferring name #{self.name} from pkg-id #{identifier}')
+    end
+  end # def extract_info
+    
+        if attributes[:pear_data_dir]
+      logger.info('Setting data_dir', :data_dir => attributes[:pear_data_dir])
+      safesystem('pear', '-c', config, 'config-set', 'data_dir', '#{staging_path(installroot)}/#{attributes[:pear_data_dir]}')
+    end
+    
+    end
