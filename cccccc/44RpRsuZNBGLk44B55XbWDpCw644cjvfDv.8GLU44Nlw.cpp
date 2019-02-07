a8c1b2db1aa7d2a8a634173826a4bdfedd88e690
SUNsung <weatherius@gmail.com>
2019-02-07 19:20:17 +0200

    Rewrite [44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp] File in: 2019-02-07 19:20:17

--- cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
+++ cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
@@ -1,132 +1,45 @@
 
         
-        #endif // BITCOIN_QT_NETWORKSTYLE_H
-
-    
-    
-    {private:
-    Ui::OpenURIDialog *ui;
-};
-    
-    SECP256K1_INLINE static void secp256k1_fe_mul_inner(uint64_t *r, const uint64_t *a, const uint64_t * SECP256K1_RESTRICT b) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            r15:rcx = d
- *            r10-r14 = a0-a4
- *            rbx     = b
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
+        namespace internal {
     }
     
+      // Given directory = 'dir', relative_path = 'test.xml',
+  // returns 'dir/test.xml'.
+  // On Windows, uses \ as the separator rather than /.
+  static FilePath ConcatPaths(const FilePath& directory,
+                              const FilePath& relative_path);
     
-    {    /* Serialize/parse compact and verify/recover. */
-    extra[0] = 0;
-    CHECK(secp256k1_ecdsa_sign_recoverable(ctx, &rsignature[0], message, privkey, NULL, NULL) == 1);
-    CHECK(secp256k1_ecdsa_sign(ctx, &signature[0], message, privkey, NULL, NULL) == 1);
-    CHECK(secp256k1_ecdsa_sign_recoverable(ctx, &rsignature[4], message, privkey, NULL, NULL) == 1);
-    CHECK(secp256k1_ecdsa_sign_recoverable(ctx, &rsignature[1], message, privkey, NULL, extra) == 1);
-    extra[31] = 1;
-    CHECK(secp256k1_ecdsa_sign_recoverable(ctx, &rsignature[2], message, privkey, NULL, extra) == 1);
-    extra[31] = 0;
-    extra[0] = 1;
-    CHECK(secp256k1_ecdsa_sign_recoverable(ctx, &rsignature[3], message, privkey, NULL, extra) == 1);
-    CHECK(secp256k1_ecdsa_recoverable_signature_serialize_compact(ctx, sig, &recid, &rsignature[4]) == 1);
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(ctx, &signature[4], &rsignature[4]) == 1);
-    CHECK(memcmp(&signature[4], &signature[0], 64) == 0);
-    CHECK(secp256k1_ecdsa_verify(ctx, &signature[4], message, &pubkey) == 1);
-    memset(&rsignature[4], 0, sizeof(rsignature[4]));
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsignature[4], sig, recid) == 1);
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(ctx, &signature[4], &rsignature[4]) == 1);
-    CHECK(secp256k1_ecdsa_verify(ctx, &signature[4], message, &pubkey) == 1);
-    /* Parse compact (with recovery id) and recover. */
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsignature[4], sig, recid) == 1);
-    CHECK(secp256k1_ecdsa_recover(ctx, &recpubkey, &rsignature[4], message) == 1);
-    CHECK(memcmp(&pubkey, &recpubkey, sizeof(pubkey)) == 0);
-    /* Serialize/destroy/parse signature and verify again. */
-    CHECK(secp256k1_ecdsa_recoverable_signature_serialize_compact(ctx, sig, &recid, &rsignature[4]) == 1);
-    sig[secp256k1_rand_bits(6)] += 1 + secp256k1_rand_int(255);
-    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsignature[4], sig, recid) == 1);
-    CHECK(secp256k1_ecdsa_recoverable_signature_convert(ctx, &signature[4], &rsignature[4]) == 1);
-    CHECK(secp256k1_ecdsa_verify(ctx, &signature[4], message, &pubkey) == 0);
-    /* Recover again */
-    CHECK(secp256k1_ecdsa_recover(ctx, &recpubkey, &rsignature[4], message) == 0 ||
-          memcmp(&pubkey, &recpubkey, sizeof(pubkey)) != 0);
-}
+      // Copy an existing linked_ptr<>, adding ourselves to the list of references.
+  template <typename U> linked_ptr(linked_ptr<U> const& ptr) { copy(&ptr); }
+  linked_ptr(linked_ptr const& ptr) {  // NOLINT
+    assert(&ptr != this);
+    copy(&ptr);
+  }
     
-    static bool CaseInsensitiveEqual(const std::string &s1, const std::string &s2)
-{
-    if (s1.size() != s2.size()) return false;
-    for (size_t i = 0; i < s1.size(); ++i) {
-        char c1 = s1[i];
-        if (c1 >= 'A' && c1 <= 'Z') c1 -= ('A' - 'a');
-        char c2 = s2[i];
-        if (c2 >= 'A' && c2 <= 'Z') c2 -= ('A' - 'a');
-        if (c1 != c2) return false;
+        void ComputeCurrentValue() {
+      if (!AtEnd())
+        current_value_ = ParamType(*current1_, *current2_, *current3_,
+            *current4_, *current5_, *current6_);
     }
-    return true;
-}
+    bool AtEnd() const {
+      // We must report iterator past the end of the range when either of the
+      // component iterators has reached the end of its range.
+      return
+          current1_ == end1_ ||
+          current2_ == end2_ ||
+          current3_ == end3_ ||
+          current4_ == end4_ ||
+          current5_ == end5_ ||
+          current6_ == end6_;
+    }
     
-    #endif
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-    // Called by Init/NewFrame/Shutdown
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateFontsTexture();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyFontsTexture();
-IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateDeviceObjects();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyDeviceObjects();
-
-    
-    void ImGui_ImplDX11_NewFrame()
-{
-    if (!g_pFontSampler)
-        ImGui_ImplDX11_CreateDeviceObjects();
-}
-
-    
-                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox('Another Window', &show_another_window);
-    
-    class MockProtocolData : public ProtocolData<::apollo::canbus::ChassisDetail> {
- public:
-  static const int32_t ID = 0x111;
-  MockProtocolData() {}
-};
-    
-    #include 'glog/logging.h'
-#include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-#include 'modules/drivers/radar/conti_radar/protocol/const_vars.h'
-    
-    void ObjectGeneralInfo60B::Parse(const std::uint8_t* bytes, int32_t length,
-                                 ContiRadar* conti_radar) const {
-  int obj_id = object_id(bytes, length);
-  auto conti_obs = conti_radar->add_contiobs();
-  conti_obs->set_clusterortrack(false);
-  conti_obs->set_obstacle_id(obj_id);
-  conti_obs->set_longitude_dist(longitude_dist(bytes, length));
-  conti_obs->set_lateral_dist(lateral_dist(bytes, length));
-  conti_obs->set_longitude_vel(longitude_vel(bytes, length));
-  conti_obs->set_lateral_vel(lateral_vel(bytes, length));
-  conti_obs->set_rcs(rcs(bytes, length));
-  conti_obs->set_dynprop(dynprop(bytes, length));
-  double timestamp = apollo::common::time::Clock::NowInSeconds();
-  auto header = conti_obs->mutable_header();
-  header->CopyFrom(conti_radar->header());
-  header->set_timestamp_sec(timestamp);
-}
+    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+int Factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
+    }
     
     OutputType RadarState201::output_type(const std::uint8_t* bytes,
                                       int32_t length) const {
@@ -134,55 +47,57 @@ IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyDeviceObjects();
   uint32_t x = t0.get_byte(2, 2);
     }
     
-    Status HeaderXmlParser::Parse(const tinyxml2::XMLElement& xml_node,
-                              PbHeader* header) {
-  auto header_node = xml_node.FirstChildElement('header');
-  if (!header_node) {
-    std::string err_msg = 'xml data missing header';
-    return Status(apollo::common::ErrorCode::HDMAP_DATA_ERROR, err_msg);
+    TEST_F(SpeedLimitTest, SimpleSpeedLimitCreation) {
+  SpeedLimit simple_speed_limit;
+  EXPECT_TRUE(simple_speed_limit.speed_limit_points().empty());
+  EXPECT_EQ(speed_limit_.speed_limit_points().size(), 100);
+}
+    
+    /**
+ * @file : spline_1d_seg.cc
+ * @brief: polynomial smoothing spline segment
+ **/
+    
+    #include 'modules/canbus/vehicle/gem/protocol/accel_cmd_67.h'
+#include 'modules/canbus/vehicle/gem/protocol/brake_cmd_6b.h'
+#include 'modules/canbus/vehicle/gem/protocol/global_cmd_69.h'
+#include 'modules/canbus/vehicle/gem/protocol/headlight_cmd_76.h'
+#include 'modules/canbus/vehicle/gem/protocol/horn_cmd_78.h'
+#include 'modules/canbus/vehicle/gem/protocol/shift_cmd_65.h'
+#include 'modules/canbus/vehicle/gem/protocol/steering_cmd_6d.h'
+#include 'modules/canbus/vehicle/gem/protocol/turn_cmd_63.h'
+#include 'modules/canbus/vehicle/gem/protocol/wiper_cmd_90.h'
+    
+    [[noreturn]] void exception_wrapper::onNoExceptionError(
+    char const* const name) {
+  std::ios_base::Init ioinit_; // ensure std::cerr is alive
+  std::cerr << 'Cannot use `' << name
+            << '` with an empty folly::exception_wrapper' << std::endl;
+  std::terminate();
+}
+    
+      explicit Options(
+      Format format_ = Format::ZLIB,
+      int windowSize_ = 15,
+      int memLevel_ = 8,
+      int strategy_ = Z_DEFAULT_STRATEGY)
+      : format(format_),
+        windowSize(windowSize_),
+        memLevel(memLevel_),
+        strategy(strategy_) {}
+    
+    
+    {    return old_ptr;
   }
-  std::string rev_major;
-  std::string rev_minor;
-  std::string database_name;
-  std::string version;
-  std::string date;
-  double north = 0.0;
-  double south = 0.0;
-  double west = 0.0;
-  double east = 0.0;
-  std::string vendor;
-  int checker = UtilXmlParser::QueryStringAttribute(
-                            *header_node, 'revMajor', &rev_major);
-  checker += UtilXmlParser::QueryStringAttribute(
-                            *header_node, 'revMinor', &rev_minor);
-  checker += UtilXmlParser::QueryStringAttribute(
-                            *header_node, 'name', &database_name);
-  checker += UtilXmlParser::QueryStringAttribute(
-                            *header_node, 'version', &version);
-  checker += UtilXmlParser::QueryStringAttribute(
-                            *header_node, 'date', &date);
-  checker += header_node->QueryDoubleAttribute('north', &north);
-  checker += header_node->QueryDoubleAttribute('south', &south);
-  checker += header_node->QueryDoubleAttribute('east', &east);
-  checker += header_node->QueryDoubleAttribute('west', &west);
-  checker +=
-      UtilXmlParser::QueryStringAttribute(*header_node, 'vendor', &vendor);
+    
+    template <template <typename> class Atom = std::atomic>
+class counted_ptr_internals : public counted_ptr_base<Atom> {
+ public:
+  template <typename T, typename... Args>
+  static counted_ptr<T, Atom> make_ptr(Args&&... args) {
+    return make_counted<Atom, T>(std::forward<Args...>(args...));
+  }
+  template <typename T>
+  using CountedPtr = counted_ptr<T, Atom>;
+  typedef void counted_base;
     }
-    
-    
-    {
-    {
-    {}  // namespace adapter
-}  // namespace hdmap
-}  // namespace apollo
-
-    
-        double s_param = s - s0;
-    // linear extrapolation is handled internally in LatticeTrajectory1d;
-    // no worry about s_param > lat_trajectory.ParamLength() situation
-    double d = lat_trajectory.Evaluate(0, s_param);
-    double d_prime = lat_trajectory.Evaluate(1, s_param);
-    double d_pprime = lat_trajectory.Evaluate(2, s_param);
-    
-      const auto mat = kernel.kernel_matrix();
-  const auto offset = kernel.offset_matrix();
