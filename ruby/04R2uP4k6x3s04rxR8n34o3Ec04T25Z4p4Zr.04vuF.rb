1613ffcbb4c5354f16c1ecd19ffe4c71e608b592
SUNsung <weatherius@gmail.com>
2019-02-07 22:13:17 +0200

    Rewrite [04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb] File in: 2019-02-07 22:13:16

--- ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
+++ ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
@@ -1,72 +1,35 @@
 
         
-            # The stdlib recorded in the install receipt is used during dependency
-    # compatibility checks, so we only care about the stdlib that libraries
-    # link against.
-    keg.detect_cxx_stdlibs(:skip_executables => true)
+        namespace :test do
+  desc 'Run the ruby tests (without sass-spec)'
+  Rake::TestTask.new('ruby') do |t|
+    t.libs << 'test'
+    test_files = FileList[scope('test/**/*_test.rb')]
+    test_files.exclude(scope('test/rails/*'))
+    test_files.exclude(scope('test/plugins/*'))
+    t.test_files = test_files
+    t.warning = true
+    t.verbose = true
   end
     
-      def <<(o)
-    @settings << o
-    self
-  end
+            line_num = e.sass_line + 1 - line_offset
+        min = [line_num - 6, 0].max
+        section = e.sass_template.rstrip.split('\n')[min...line_num + 5]
+        return e.sass_backtrace_str if section.nil? || section.empty?
     
-      # True if a {Formula} is being built with {Formula.stable} instead of {Formula.devel} or {Formula.head}. This is the default.
-  # <pre>args << '--some-beta' if build.devel?</pre>
-  def stable?
-    !(head? || devel?)
-  end
+    Given /^I run a paperclip generator to add a paperclip '([^']*)' to the '([^']*)' model$/ do |attachment_name, model_name|
+  step %[I successfully run `rails generate paperclip #{model_name} #{attachment_name}`]
+end
     
-        hub = ReporterHub.new
-    updated = false
-    
-    # Exit cleanly from an early interrupt
-Signal.trap('INT') { exit 1 }
-    
-    require 'clamp'
-require 'pluginmanager/util'
-require 'pluginmanager/gemfile'
-require 'pluginmanager/install'
-require 'pluginmanager/remove'
-require 'pluginmanager/list'
-require 'pluginmanager/update'
-require 'pluginmanager/pack'
-require 'pluginmanager/unpack'
-require 'pluginmanager/generate'
-require 'pluginmanager/prepare_offline_pack'
-require 'pluginmanager/proxy_support'
-configure_proxy
-    
-            return nil
-      end
-    end
-  end
-end end end
-
-    
-          PluginManager.ui.info('Installing file: #{local_file}')
-      uncompressed_path = uncompress(local_file)
-      PluginManager.ui.debug('Pack uncompressed to #{uncompressed_path}')
-      pack = LogStash::PluginManager::PackInstaller::Pack.new(uncompressed_path)
-      raise PluginManager::InvalidPackError, 'The pack must contains at least one plugin' unless pack.valid?
-    
-              it 'fails when installing a non logstash plugin' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install  bundler')
-            expect(command).not_to install_successfully
-          end
-    
-          it 'list the plugins with their versions' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose')
-        result.stdout.split('\n').each do |plugin|
-          expect(plugin).to match(/^logstash-\w+-\w+\s\(\d+\.\d+.\d+(.\w+)?\)/)
-        end
-      end
+        def define_getters
+      define_instance_getter
+      define_class_getter
     end
     
-        context 'update a specific plugin' do
-      it 'has executed successfully' do
-        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
-        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
-        expect(logstash).not_to have_installed?(plugin_name, previous_version)
-      end
+        # Returns the dot+extension of the file. e.g. '.jpg' for 'file.jpg'
+    # If the style has a format defined, it will return the format instead
+    # of the actual extension. If the extension is empty, no dot is added.
+    def dotextension attachment, style_name
+      ext = extension(attachment, style_name)
+      ext.empty? ? ext : '.#{ext}'
     end
