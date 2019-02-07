870275db20b354f5145e75a4f65b9ebdd0996714
SUNsung <weatherius@gmail.com>
2019-02-08 01:52:02 +0200

    Rewrite [34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp] File in: 2019-02-08 01:52:01

--- cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
+++ cccccc/34PLB0sJVoLVQ34p1GX8l.ao.634ZsW9pDQdSQo34Qkh.cpp
@@ -1,202 +1,118 @@
 
         
-        
-    {}  // namespace tensorflow
+        #include <mxnet/op_attr_types.h>
+#include <mxnet/graph_attr_types.h>
+#include <mxnet/c_api.h>
+#include <nnvm/symbolic.h>
+#include <nnvm/op.h>
+#include <nnvm/graph.h>
+#include <vector>
+#include <atomic>
+#include <utility>
+#include <string>
+#include <unordered_map>
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #ifndef TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
-    
-    // Must be included first.
-#include 'tensorflow/python/lib/core/numpy.h'
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    
-    {
-    {}  // namespace cuda
-}  // namespace stream_executor
-
-    
-    
-    {
-    {}  // namespace cuda
-}  // namespace stream_executor
-    
-      // Iterates through loaded DSOs with DlIteratePhdrCallback to find the
-  // driver-interfacing DSO version number. Returns it as a string.
-  static port::StatusOr<DriverVersion> FindDsoVersion();
-    
-    // CUDAEvent wraps a CUevent in the platform-independent EventInterface
-// interface.
-class CUDAEvent : public internal::EventInterface {
+    /**!
+ * \brief Workaround for missing functions in ::caffe::Layer
+ * \warning Do not add or override any virtual functions in this class
+ * @tparam Dtype
+ */
+template <typename Dtype>
+class CaffeLayerFriend : public ::caffe::Layer<Dtype> {
+  explicit CaffeLayerFriend(const ::caffe::LayerParameter& param) = delete;
  public:
-  explicit CUDAEvent(CUDAExecutor* parent);
-    }
-    
-    #ifndef CONTENT_NW_SRC_API_APP_APP_H_
-#define CONTENT_NW_SRC_API_APP_APP_H_
-    
-    
-    {}  // namespace nw
-
-    
-    #include 'base/strings/string_piece.h'
-#include 'v8/include/v8.h'
-    
-    void Clipboard::CallSync(const std::string& method,
-                         const base::ListValue& arguments,
-                         base::ListValue* result) {
-  if (method == 'Get') {
-    result->AppendString(GetText());
-  } else {
-    NOTREACHED() << 'Invalid call to Clipboard method:' << method
-                 << ' arguments:' << arguments;
-  }
-}
-    
-    class ObjectManager;
-    
-    namespace ui {
-    }
-    
-     protected:
-  ~NwAppClearAppCacheFunction() override;
-    
-    #include 'extensions/browser/extension_function.h'
-    
-    template <typename Dtype>
-class LayerRegistry {
- public:
-  static ::caffe::Layer<Dtype> * CreateLayer(const ::caffe::LayerParameter& param) {
-    ::caffe::shared_ptr< ::caffe::Layer<Dtype> > ptr =
-      ::caffe::LayerRegistry<Dtype>::CreateLayer(param);
-    // avoid caffe::layer destructor, which deletes the weights layer owns
-    new ::caffe::shared_ptr< ::caffe::Layer<Dtype> >(ptr);
-    return ptr.get();
+  inline void SetPhase(::caffe::Phase p) {
+    this->phase_ = p;
   }
 };
     
     
-  bool ReadProtoFromTextContent(const std::string& text,
-                                ::google::protobuf::Message* proto) const {
-    bool success = google::protobuf::TextFormat::ParseFromString(text, proto);
-    return success;
+    {    for (int i = 0; i < param_.num_weight; ++i) {
+      if (i == 0)
+        res.push_back(std::to_string(i) + '_weight');
+      else
+        res.push_back(std::to_string(i) + '_bias');
+    }
+    return res;
   }
     
     
-MXNET_DLL int MXCVResize(NDArrayHandle src, const mx_uint w, const mx_uint h,
-                         const int interpolation, NDArrayHandle *out) {
-  API_BEGIN();
-  NDArray ndsrc = *static_cast<NDArray*>(src);
-  CHECK_EQ(ndsrc.shape().ndim(), 3);
-  CHECK_EQ(ndsrc.ctx(), Context::CPU());
-  CHECK_EQ(ndsrc.dtype(), mshadow::kUint8);
-    }
-    
-    namespace mxnet {
-namespace io {
-    }
-    }
-    
-     private:
-  std::shared_ptr<Cache> cache_;
-  uint32_t num_threads_;
-    
-    Status WriteBatchBase::Delete(const SliceParts& key) {
-  std::string key_buf;
-  Slice key_slice(key, &key_buf);
-  return Delete(key_slice);
-}
-    
-    std::unique_ptr<WriteControllerToken> WriteController::GetDelayToken(
-    uint64_t write_rate) {
-  total_delayed_++;
-  // Reset counters.
-  last_refill_time_ = 0;
-  bytes_left_ = 0;
-  set_delayed_write_rate(write_rate);
-  return std::unique_ptr<WriteControllerToken>(new DelayWriteToken(this));
-}
-    
-      virtual Status Write(uint64_t offset, const Slice& data) override;
-    
-      // close DB
-  delete cf;
-  delete db;
-    
-    // Take a default DBOptions 'base_options' in addition to a
-// map 'opts_map' of option name to option value to construct the new
-// DBOptions 'new_options'.
-//
-// Below are the instructions of how to config some non-primitive-typed
-// options in DBOptions:
-//
-// * rate_limiter_bytes_per_sec:
-//   RateLimiter can be configured directly by specifying its bytes_per_sec.
-//   [Example]:
-//   - Passing {'rate_limiter_bytes_per_sec', '1024'} is equivalent to
-//     passing NewGenericRateLimiter(1024) to rate_limiter_bytes_per_sec.
-//
-// @param base_options the default options of the output 'new_options'.
-// @param opts_map an option name to value map for specifying how 'new_options'
-//     should be set.
-// @param new_options the resulting options based on 'base_options' with the
-//     change specified in 'opts_map'.
-// @param input_strings_escaped when set to true, each escaped characters
-//     prefixed by '\' in the values of the opts_map will be further converted
-//     back to the raw string before assigning to the associated options.
-// @param ignore_unknown_options when set to true, unknown options are ignored
-//     instead of resulting in an unknown-option error.
-// @return Status::OK() on success.  Otherwise, a non-ok status indicating
-//     error will be returned, and 'new_options' will be set to 'base_options'.
-Status GetDBOptionsFromMap(
-    const DBOptions& base_options,
-    const std::unordered_map<std::string, std::string>& opts_map,
-    DBOptions* new_options, bool input_strings_escaped = false,
-    bool ignore_unknown_options = false);
-    
-      // Submit a fire and forget jobs
-  // This allows to submit the same job multiple times
-  virtual void SubmitJob(const std::function<void()>&) = 0;
-  // This moves the function in for efficiency
-  virtual void SubmitJob(std::function<void()>&&) = 0;
-    
-    
-    {    // exception out_of_range.401
-    try
     {
-        // try to write at a nonexisting key
-        object.at('the fast') = 'il rapido';
-    }
-    catch (json::out_of_range& e)
     {
-        std::cout << e.what() << '\n';
-    }
-}
+    {  /*!
+   * \brief Worker threads.
+   */
+  std::vector<std::thread> worker_threads_;
+  /*!
+   * \brief Startup synchronization objects
+   */
+  std::list<std::shared_ptr<dmlc::ManualEvent>> ready_events_;
+  /*!
+   * \brief Disallow default construction.
+   */
+  ThreadPool() = delete;
+  /*!
+   * \brief Disallow copy construction and assignment.
+   */
+  DISALLOW_COPY_AND_ASSIGN(ThreadPool);
+};
+}  // namespace engine
+}  // namespace mxnet
+#endif  // MXNET_ENGINE_THREAD_POOL_H_
 
     
+    #include './exec_pass.h'
     
-    // exception type_error.304
-    try
-    {
-        // use at() on a non-array type
-        json str = 'I am a string';
-        str.at(0) = 'Another string';
-    }
-    catch (json::type_error& e)
-    {
-        std::cout << e.what() << '\n';
+     private:
+  void create() {
+    CHECK(tensor_container_ == nullptr);
+    CHECK_EQ(this->dev_mask(), mshadow::cpu::kDevMask);
+    MSHADOW_TYPE_SWITCH(this->type_flag_, DType, {
+        auto tensor_container = new mshadow::TensorContainer<mshadow::cpu, 1, DType>(false);
+        tensor_container->Resize(mshadow::Shape1(shape_.Size()));
+        dptr_ = tensor_container->dptr_;
+        tensor_container_ = tensor_container;
+    });
+  }
+  void resize() {
+    MSHADOW_TYPE_SWITCH(this->type_flag_, DType, {
+        auto tensor_container =
+          (mshadow::TensorContainer<mshadow::cpu, 1, DType>*) tensor_container_;
+        tensor_container->Resize(mshadow::Shape1(shape_.Size()));
+    });
+  }
+  void release() {
+    MSHADOW_TYPE_SWITCH(this->type_flag_, DType, {
+        auto tensor_container =
+          (mshadow::TensorContainer<mshadow::cpu, 1, DType>*) tensor_container_;
+        delete tensor_container;
+    });
+  }
+    
+    using namespace std;
+    
+    
+int main() {
     }
     
-        // print values
-    std::cout << object << '\n';
-    std::cout << null << '\n';
+    // Three Way Quick Sort
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+public:
+    void sortColors(vector<int> &nums) {
+    }
+    }
+    
+            if(index == 0){
+            left = head->next;
+            return head;
+        }
+    
+    
+int main() {
+    }
+    
+    public:
+    vector<int> preorderTraversal(TreeNode* root) {
+    }
