0ddb7dd5da06512d60a161282005618d413b9f3f
SUNsung <weatherius@gmail.com>
2019-02-07 10:17:07 +0200

    Rewrite [24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb] File in: 2019-02-07 10:17:06

--- ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
+++ ruby/24QkGuNzt6knw24P5NOXAkIy5A24C.V8U6AerK.24NdH.rb
@@ -1,254 +1,65 @@
 
         
-            # Get rid of any info 'dir' files, so they don't conflict at the link stage
-    info_dir_file = @f.info + 'dir'
-    if info_dir_file.file? && !@f.skip_clean?(info_dir_file)
-      observe_file_removal info_dir_file
-    end
+            open_dry_run_modal(agent)
+    click_on('Dry Run')
+    expect(page).to have_text('Dry Run started')
+    expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabLog']')
+  end
+end
+
     
-            if Pathname::BOTTLE_EXTNAME_RX === file.to_s
-          version = bottle_resolve_version(file) rescue file.version
-        else
-          version = file.version
-        end
-        next unless version
-        next unless (name = file.basename.to_s[/(.*)-(?:#{Regexp.escape(version)})/, 1])
-    
-          out = checks.send(method)
-      unless out.nil? || out.empty?
-        if first_warning
-          $stderr.puts <<-EOS.undent
-            #{Tty.white}Please note that these warnings are just used to help the Homebrew maintainers
-            with debugging if you file an issue. If everything you use Homebrew for is
-            working fine: please don't worry and just ignore them. Thanks!#{Tty.reset}
-          EOS
-        end
-    
-      def dump_formula_report(key, title)
-    formulae = select_formula(key).sort.map do |name, new_name|
-      # Format list items of renamed formulae
-      if key == :R
-        name = pretty_installed(name) if installed?(name)
-        new_name = pretty_installed(new_name) if installed?(new_name)
-        '#{name} -> #{new_name}'
-      else
-        installed?(name) ? pretty_installed(name) : name
-      end
-    end
-    
-    module Gitlab
-  module Ci
-    module Pipeline
-      # Class for preloading data associated with pipelines such as commit
-      # authors.
-      class Preloader
-        def self.preload!(pipelines)
-          ##
-          # This preloads all commits at once, because `Ci::Pipeline#commit` is
-          # using a lazy batch loading, what results in only one batched Gitaly
-          # call.
-          #
-          pipelines.each(&:commit)
-    
-          def github_omniauth_provider
-        @github_omniauth_provider ||= Gitlab::Auth::OAuth::Provider.config_for('github').to_h
-      end
-    
-          def cache_key_iid
-        if object.respond_to?(:noteable_id)
-          object.noteable_id
-        elsif object.respond_to?(:iid)
-          object.iid
-        else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
-        end
-      end
+        it 'in the future' do
+      expect(relative_distance_of_time_in_words(Time.now+5.minutes)).to eq('in 5m')
     end
   end
 end
 
     
-    
-    {            # These fields are not displayed for LegacyDiffNote notes, so it
-            # doesn't really matter what we set them to.
-            a_mode: '100644',
-            b_mode: '100644',
-            new_file: false
-          }
-        end
-      end
+        it 'can be turned off' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
+      DefaultScenarioImporter.import(user)
     end
-  end
+    
+      # Setup the root logger with the Rails log level and the desired set of
+  # appenders. The list of appenders to use should be set in the environment
+  # specific configuration file.
+  #
+  # For example, in a production application you would not want to log to
+  # STDOUT, but you would want to send an email for 'error' and 'fatal'
+  # messages:
+  #
+  # => config/environments/production.rb
+  #
+  #     config.log_to = %w[file email]
+  #
+  # In development you would want to log to STDOUT and possibly to a file:
+  #
+  # => config/environments/development.rb
+  #
+  #     config.log_to = %w[stdout file]
+  #
+  Logging.logger.root.appenders = config.log_to unless config.log_to.empty?
+    
+    When /^I submit forgot password form$/ do
+  submit_forgot_password_form
 end
-
     
-              hash = {
-            iid: issue.number,
-            title: issue.title,
-            description: issue.body,
-            milestone_number: issue.milestone&.number,
-            state: issue.state == 'open' ? :opened : :closed,
-            assignees: issue.assignees.map do |u|
-              Representation::User.from_api_response(u)
-            end,
-            label_names: issue.labels.map(&:name),
-            author: user,
-            created_at: issue.created_at,
-            updated_at: issue.updated_at,
-            pull_request: issue.pull_request ? true : false
-          }
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
     
-            # Builds a user from a GitHub API response.
-        #
-        # user - An instance of `Sawyer::Resource` containing the user details.
-        def self.from_api_response(user)
-          new(id: user.id, login: user.login)
-        end
+        it 'generates a jasmine fixture', :fixture => true do
+      get :index, params: {conversation_id: @conv1.id}
+      save_fixture(html_for('body'), 'conversations_unread')
     
-          def action_for_rails(env)
-        controller = env[CONTROLLER_KEY]
-        action = '#{controller.class.name}##{controller.action_name}'
-    
-          def remember_cookie_values(resource)
-        options = { httponly: true }
-        options.merge!(forget_cookie_values(resource))
-        options.merge!(
-          value: resource.class.serialize_into_cookie(resource),
-          expires: resource.remember_expires_at
-        )
-      end
-    
-        if last_request_at.is_a? Integer
-      last_request_at = Time.at(last_request_at).utc
-    elsif last_request_at.is_a? String
-      last_request_at = Time.parse(last_request_at)
-    end
-    
-      it 'accepts a Rational' do
-    sleep(Rational(1, 9)).should be_close(0, 2)
-  end
-    
-      it 'accepts a negative seed' do
-    srand(-17)
-    srand.should == -17
-  end
-    
-      platform_is :windows do
-    it 'does expand shell variables when given multiples arguments' do
-      # See https://bugs.ruby-lang.org/issues/12231
-      lambda { @object.system('echo', @shell_var) }.should output_to_fd('foo\n')
-    end
-  end
-    
-        it 'returns the time at which the file was modified when passed ?M' do
-      Kernel.test(?M, @tmp_file).should == @tmp_file.mtime
-    end
-  end
-    
-      it 'returns the method name as symbol' do
-    eval(@code, TOPLEVEL_BINDING).should equal :boom
-  end
-end
-
-    
-        self.each_key { |k|
-      if (k.downcase == key.downcase)
-        self.store(k, value)
-        stored = true
-      end
-    }
+          # Example usage:
+      #   Sidekiq::Client.enqueue_to_in(:queue_name, 3.minutes, MyWorker, 'foo', 1, :bat => 'bar')
+      #
+      def enqueue_to_in(queue, interval, klass, *args)
+        int = interval.to_f
+        now = Time.now.to_f
+        ts = (int < 1_000_000_000 ? now + int : int)
     
     
-    {      # Add the item to the hash with logic to convert values to an array
-      # if so desired.
-      if (qstring.include?(var))
-        if (qstring[var].kind_of?(Array))
-          qstring[var] << val
-        else
-          curr = self.qstring[var]
-          qstring[var] = [ curr, val ]
-        end
-      else
-        qstring[var] = val
-      end
-    }
-    
-    
-  # open rmcpplus_request with cipherzero
-  def self.create_ipmi_session_open_cipher_zero_request(console_session_id)
-    head = [
-      0x06, 0x00, 0xff, 0x07,   # RMCP Header
-      0x06,                     # RMCP+ Authentication Type
-      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
-      0x00, 0x00, 0x00, 0x00,   # Session ID
-      0x00, 0x00, 0x00, 0x00    # Sequence Number
-    ].pack('C*')
-    
-                data_encrypt = Rex::Text::rand_text(8) + data
-    
-              # Decodes the last_req from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Array<Rex::Proto::Kerberos::Model::LastRequest>]
-          def decode_last_req(input)
-            last_requests = []
-            input.value[0].value.each do |last_request|
-              last_requests << Rex::Proto::Kerberos::Model::LastRequest.decode(last_request)
-            end
-    
-                seq = OpenSSL::ASN1::Sequence.new(elems)
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey
-          #
-          # @param input [String, OpenSSL::ASN1::Sequence] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::Sequence
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode EncryptionKey, invalid input'
-            end
-    
-              # Decodes the nonce field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_nonce(input)
-            input.value[0].value.to_i
-          end
-    
-              # Decodes the msg_type from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_msg_type(input)
-            input.value[0].value.to_i
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an
-          # OpenSSL::ASN1::Sequence
-          #
-          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
-          def decode_asn1(input)
-            seq_values = input.value
-            self.type = decode_type(seq_values[0])
-            self.value = decode_value(seq_values[1])
-          end
-    
-          def deny(env)
-        warn env, 'attack prevented by #{self.class}'
-        [options[:status], {'Content-Type' => 'text/plain'}, [options[:message]]]
-      end
-    
-      describe '#random_string' do
-    it 'outputs a string of 32 characters' do
-      expect(subject.random_string.length).to eq(32)
-    end
-  end
