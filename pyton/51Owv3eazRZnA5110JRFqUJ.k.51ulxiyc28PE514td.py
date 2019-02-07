89b77b20effbb17af6e688a8e163ebe76a6dee9a
SUNsung <weatherius@gmail.com>
2019-02-07 06:37:11 +0200

    Rewrite [51Owv3eazRZnA5110JRFqUJ.k.51ulxiyc28PE514td.py] File in: 2019-02-07 06:37:10

--- pyton/51Owv3eazRZnA5110JRFqUJ.k.51ulxiyc28PE514td.py
+++ pyton/51Owv3eazRZnA5110JRFqUJ.k.51ulxiyc28PE514td.py
@@ -1,109 +1,47 @@
 
         
-            @property
-    @abstractmethod
-    def value(self):
-        pass
+        def encode(plaintext, key):
+    table = generate_table(key)
+    plaintext = prepare_input(plaintext)
+    ciphertext = ''
     
-        def __init__(self, license_plate):
-        super(Motorcycle, self).__init__(VehicleSize.MOTORCYCLE, license_plate, spot_size=1)
-    
-        def extract_url(self, line):
-        '''Extract the generated url from the log line.'''
-        pass
-    
-    def get_marker(line):
-    matchlist = TAG_REGEX.findall(line)
-    if matchlist:
-        namematch = NAMED_A_TAG_REGEX.match(line)
-        if namematch:
-            return namematch.group(1) # group 0 is full match
-    
-    logger = logging.getLogger(__name__)
-    
-        # Find which gt ROI each ex ROI has max overlap with:
-    # this will be the ex ROI's gt target
-    gt_assignment = ex_gt_overlaps.argmax(axis=1)
-    gt_rois = rois[gt_inds[gt_assignment], :]
-    ex_rois = rois[ex_inds, :]
-    # Use class '1' for all boxes if using class_agnostic_bbox_reg
-    targets[ex_inds, 0] = (
-        1 if cfg.MODEL.CLS_AGNOSTIC_BBOX_REG else labels[ex_inds])
-    targets[ex_inds, 1:] = box_utils.bbox_transform_inv(
-        ex_rois, gt_rois, cfg.MODEL.BBOX_REG_WEIGHTS)
-    return targets
-    
-    
-def _do_matlab_eval(json_dataset, salt, output_dir='output'):
-    import subprocess
-    logger.info('-----------------------------------------------------')
-    logger.info('Computing results with the official MATLAB eval code.')
-    logger.info('-----------------------------------------------------')
-    info = voc_info(json_dataset)
-    path = os.path.join(
-        cfg.ROOT_DIR, 'detectron', 'datasets', 'VOCdevkit-matlab-wrapper')
-    cmd = 'cd {} && '.format(path)
-    cmd += '{:s} -nodisplay -nodesktop '.format(cfg.MATLAB)
-    cmd += '-r 'dbstop if error; '
-    cmd += 'voc_eval(\'{:s}\',\'{:s}\',\'{:s}\',\'{:s}\'); quit;'' \
-       .format(info['devkit_path'], 'comp4' + salt, info['image_set'],
-               output_dir)
-    logger.info('Running:\n{}'.format(cmd))
-    subprocess.call(cmd, shell=True)
-    
-    from __future__ import absolute_import
-from __future__ import division
+    The problem is :
+Given two strings A and B. Find the minimum number of operations to string B such that A = B. The permitted operations are removal,  insertion, and substitution.
+'''
 from __future__ import print_function
-from __future__ import unicode_literals
+    
+        difference = abs(predict - actual)
+    score = difference.mean()
+    
+        return res
+    
+        return matchScore
+    
+    # This is a very simple benchmark to give you an idea of how fast each step of face recognition will run on your system.
+# Notice that face detection gets very slow at large image sizes. So you might consider running face detection on a
+# scaled down version of your image and then running face encodings on the the full size image.
+    
+    known_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+    
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
     
-def _scale_enum(anchor, scales):
-    '''Enumerate a set of anchors for each scale wrt an anchor.'''
-    w, h, x_ctr, y_ctr = _whctrs(anchor)
-    ws = w * scales
-    hs = h * scales
-    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-    return anchors
+def face_landmarks(face_image, face_locations=None, model='large'):
+    '''
+    Given an image, returns a dict of face feature locations (eyes, nose, etc) for each face in the image
+    
+    # All done!
+input_movie.release()
+cv2.destroyAllWindows()
 
     
-    
-# octave and aspect fields are only used on RetinaNet. Octave corresponds to the
-# scale of the anchor and aspect denotes which aspect ratio is used in the range
-# of aspect ratios
-FieldOfAnchors = namedtuple(
-    'FieldOfAnchors', [
-        'field_of_anchors', 'num_cell_anchors', 'stride', 'field_size',
-        'octave', 'aspect'
-    ]
-)
-    
-    '''Construct minibatches for Mask R-CNN training when keypoints are enabled.
-Handles the minibatch blobs that are specific to training Mask R-CNN for
-keypoint detection. Other blobs that are generic to RPN or Fast/er R-CNN are
-handled by their respecitive roi_data modules.
-'''
-    
-    
-def get_minibatch(roidb):
-    '''Given a roidb, construct a minibatch sampled from it.'''
-    # We collect blobs from each image onto a list and then concat them into a
-    # single tensor, hence we initialize each blob to an empty list
-    blobs = {k: [] for k in get_minibatch_blob_names()}
-    # Get the input image blob, formatted for caffe2
-    im_blob, im_scales = _get_image_blob(roidb)
-    blobs['data'] = im_blob
-    if cfg.RPN.RPN_ON:
-        # RPN-only or end-to-end Faster/Mask R-CNN
-        valid = rpn_roi_data.add_rpn_blobs(blobs, im_scales, roidb)
-    elif cfg.RETINANET.RETINANET_ON:
-        im_width, im_height = im_blob.shape[3], im_blob.shape[2]
-        # im_width, im_height corresponds to the network input: padded image
-        # (if needed) width and height. We pass it as input and slice the data
-        # accordingly so that we don't need to use SampleAsOp
-        valid = retinanet_roi_data.add_retinanet_blobs(
-            blobs, im_scales, roidb, im_width, im_height
-        )
-    else:
-        # Fast R-CNN like models trained on precomputed proposals
-        valid = fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
-    return blobs, valid
+    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+# See http://pillow.readthedocs.io/ for more about PIL/Pillow
+pil_image = Image.fromarray(unknown_image)
+# Create a Pillow ImageDraw Draw instance to draw with
+draw = ImageDraw.Draw(pil_image)
