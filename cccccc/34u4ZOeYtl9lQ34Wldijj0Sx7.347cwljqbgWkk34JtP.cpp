6ef1563d3aaa289f6ede9510ff61b9eaf9e7872b
SUNsung <weatherius@gmail.com>
2019-02-08 00:11:05 +0200

    Rewrite [34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp] File in: 2019-02-08 00:11:04

--- cccccc/34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp
+++ cccccc/34u4ZOeYtl9lQ34Wldijj0Sx7.347cwljqbgWkk34JtP.cpp
@@ -1,127 +1,412 @@
-#include <sstream>
-    
-    TEST(JavaDocCommentTest, Escaping) {
-  EXPECT_EQ('foo /&#42; bar *&#47; baz', EscapeJavadoc('foo /* bar */ baz'));
-  EXPECT_EQ('foo /&#42;&#47; baz', EscapeJavadoc('foo /*/ baz'));
-  EXPECT_EQ('{&#64;foo}', EscapeJavadoc('{@foo}'));
-  EXPECT_EQ('&lt;i&gt;&amp;&lt;/i&gt;', EscapeJavadoc('<i>&</i>'));
-  EXPECT_EQ('foo&#92;u1234bar', EscapeJavadoc('foo\\u1234bar'));
-  EXPECT_EQ('&#64;deprecated', EscapeJavadoc('@deprecated'));
-}
-    
-    
-    {
-    {
-    {}  // namespace io
-}  // namespace protobuf
-}  // namespace google
-    
-      // Parsing halted on a '.'.  Perhaps we're in a different locale?  Let's
-  // try to replace the '.' with a locale-specific radix character and
-  // try again.
-  string localized = LocalizeRadix(text, temp_endptr);
-  const char* localized_cstr = localized.c_str();
-  char* localized_endptr;
-  result = strtod(localized_cstr, &localized_endptr);
-  if ((localized_endptr - localized_cstr) >
-      (temp_endptr - text)) {
-    // This attempt got further, so replacing the decimal must have helped.
-    // Update original_endptr to point at the right location.
-    if (original_endptr != NULL) {
-      // size_diff is non-zero if the localized radix has multiple bytes.
-      int size_diff = localized.size() - strlen(text);
-      // const_cast is necessary to match the strtod() interface.
-      *original_endptr = const_cast<char*>(
-        text + (localized_endptr - localized_cstr - size_diff));
+
+        
+          if (clangDiag.getID() == clang::diag::err_module_not_built &&
+      CurrentImport && clangDiag.getArgStdStr(0) == CurrentImport->getName()) {
+    SourceLoc loc = DiagLoc;
+    if (clangDiag.getLocation().isValid())
+      loc = resolveSourceLocation(clangDiag.getSourceManager(),
+                                  clangDiag.getLocation());
     }
+    
+      Optional<unsigned> selfIndex = None;
+    
+    char Mangle::getStandardTypeSubst(StringRef TypeName) {
+#define STANDARD_TYPE(KIND, MANGLING, TYPENAME)      \
+  if (TypeName == #TYPENAME) {                       \
+    return #MANGLING[0];                             \
+  }
+    }
+    
+      std::vector<std::thread> workers;
+  for (auto worker = size_t{0}; worker < num_threads; ++worker) {
+    workers.push_back(std::thread([&] {
+      try {
+        hphp_thread_init();
+        hphp_session_init(Treadmill::SessionKind::HHBBC);
+        SCOPE_EXIT {
+          hphp_context_exit();
+          hphp_session_exit();
+          hphp_thread_exit();
+        };
+    }
+    }
+    }
+    
+      bool operator==(SrcLoc o) const {
+    return start == o.start && past == o.past;
   }
     
-    namespace google {
-namespace protobuf {
-namespace io {
+          I_form_t i_formater {{
+                            lk,
+                            aa,
+                            imm >> 2,
+                            op
+                          }};
+    
+      Object createObject() const;
+    
+    void Config::ParseIniFile(const std::string &filename,
+                          const bool is_system /* = true */) {
+  IniSettingMap ini = IniSettingMap();
+  Config::ParseIniFile(filename, ini, false, is_system);
+}
+    
+    PlainDirectory::PlainDirectory(const String& path) {
+  m_dir = ::opendir(path.data());
+}
+    
+    struct FileStreamWrapper final : Stream::Wrapper {
+  static req::ptr<MemFile> openFromCache(const String& filename,
+                                         const String& mode);
+  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+  int access(const String& path, int mode) override {
+    return ::access(File::TranslatePath(path).data(), mode);
+  }
+  int stat(const String& path, struct stat* buf) override {
+    return ::stat(File::TranslatePath(path).data(), buf);
+  }
+  int lstat(const String& path, struct stat* buf) override {
+    return ::lstat(File::TranslatePath(path).data(), buf);
+  }
+  int unlink(const String& path) override;
+  int rename(const String& oldname, const String& newname) override;
+  int mkdir(const String& path, int mode, int options) override;
+  int rmdir(const String& path, int /*options*/) override {
+    ERROR_RAISE_WARNING(::rmdir(File::TranslatePath(path).data()));
+    return ret;
+  }
+  bool isNormalFileStream() const override { return true; }
+    }
+    
+    #include 'hphp/runtime/base/stream-wrapper.h'
+#include 'hphp/runtime/base/runtime-error.h'
+    
+    #include 'hphp/util/stack-trace.h'
+    
+      size_t BytesRead() const override {
+    return parser_->BytesRead();
+  }
+    
+    /*! \brief namespace of base64 decoding and encoding table */
+namespace base64 {
+const char DecodeTable[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  62,  // '+'
+  0, 0, 0,
+  63,  // '/'
+  52, 53, 54, 55, 56, 57, 58, 59, 60, 61,  // '0'-'9'
+  0, 0, 0, 0, 0, 0, 0,
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+  13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,  // 'A'-'Z'
+  0, 0, 0, 0, 0, 0,
+  26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+  39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,  // 'a'-'z'
+};
+static const char EncodeTable[] =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+}  // namespace base64
+/*! \brief the stream that reads from base64, note we take from file pointers */
+class Base64InStream: public dmlc::Stream {
+ public:
+  explicit Base64InStream(dmlc::Stream *fs) : reader_(256) {
+    reader_.set_stream(fs);
+    num_prev = 0; tmp_ch = 0;
+  }
+  /*!
+   * \brief initialize the stream position to beginning of next base64 stream
+   * call this function before actually start read
+   */
+  inline void InitPosition(void) {
+    // get a character
+    do {
+      tmp_ch = reader_.GetChar();
+    } while (isspace(tmp_ch));
+  }
+  /*! \brief whether current position is end of a base64 stream */
+  inline bool IsEOF(void) const {
+    return num_prev == 0 && (tmp_ch == EOF || isspace(tmp_ch));
+  }
+  virtual size_t Read(void *ptr, size_t size) {
+    using base64::DecodeTable;
+    if (size == 0) return 0;
+    // use tlen to record left size
+    size_t tlen = size;
+    unsigned char *cptr = static_cast<unsigned char*>(ptr);
+    // if anything left, load from previous buffered result
+    if (num_prev != 0) {
+      if (num_prev == 2) {
+        if (tlen >= 2) {
+          *cptr++ = buf_prev[0];
+          *cptr++ = buf_prev[1];
+          tlen -= 2;
+          num_prev = 0;
+        } else {
+          // assert tlen == 1
+          *cptr++ = buf_prev[0]; --tlen;
+          buf_prev[0] = buf_prev[1];
+          num_prev = 1;
+        }
+      } else {
+        // assert num_prev == 1
+        *cptr++ = buf_prev[0]; --tlen; num_prev = 0;
+      }
+    }
+    if (tlen == 0) return size;
+    int nvalue;
+    // note: everything goes with 4 bytes in Base64
+    // so we process 4 bytes a unit
+    while (tlen && tmp_ch != EOF && !isspace(tmp_ch)) {
+      // first byte
+      nvalue = DecodeTable[tmp_ch] << 18;
+      {
+        // second byte
+        tmp_ch = reader_.GetChar();
+        CHECK(tmp_ch != EOF && !isspace(tmp_ch)) << 'invalid base64 format';
+        nvalue |= DecodeTable[tmp_ch] << 12;
+        *cptr++ = (nvalue >> 16) & 0xFF; --tlen;
+        }
+      {
+        // third byte
+        tmp_ch = reader_.GetChar();
+        CHECK(tmp_ch != EOF && !isspace(tmp_ch)) << 'invalid base64 format';
+        // handle termination
+        if (tmp_ch == '=') {
+          tmp_ch = reader_.GetChar();
+          CHECK(tmp_ch == '=') << 'invalid base64 format';
+          tmp_ch = reader_.GetChar();
+          CHECK(tmp_ch == EOF || isspace(tmp_ch))
+              << 'invalid base64 format';
+          break;
+        }
+        nvalue |= DecodeTable[tmp_ch] << 6;
+        if (tlen) {
+          *cptr++ = (nvalue >> 8) & 0xFF; --tlen;
+        } else {
+          buf_prev[num_prev++] = (nvalue >> 8) & 0xFF;
+        }
+      }
+      {
+        // fourth byte
+        tmp_ch = reader_.GetChar();
+        CHECK(tmp_ch != EOF && !isspace(tmp_ch))
+            << 'invalid base64 format';
+        if (tmp_ch == '=') {
+          tmp_ch = reader_.GetChar();
+          CHECK(tmp_ch == EOF || isspace(tmp_ch))
+              << 'invalid base64 format';
+          break;
+        }
+        nvalue |= DecodeTable[tmp_ch];
+        if (tlen) {
+          *cptr++ = nvalue & 0xFF; --tlen;
+        } else {
+          buf_prev[num_prev ++] = nvalue & 0xFF;
+        }
+      }
+      // get next char
+      tmp_ch = reader_.GetChar();
+    }
+    if (kStrictCheck) {
+      CHECK_EQ(tlen, 0) << 'Base64InStream: read incomplete';
+    }
+    return size - tlen;
+  }
+  virtual void Write(const void *ptr, size_t size) {
+    LOG(FATAL) << 'Base64InStream do not support write';
+  }
+    }
+    
+    namespace xgboost {
+namespace data {
+/*!
+ * \brief Format specification of SparsePage.
+ */
+class SparsePageFormat {
+ public:
+  /*! \brief virtual destructor */
+  virtual ~SparsePageFormat() = default;
+  /*!
+   * \brief Load all the segments into page, advance fi to end of the block.
+   * \param page The data to read page into.
+   * \param fi the input stream of the file
+   * \return true of the loading as successful, false if end of file was reached
+   */
+  virtual bool Read(SparsePage* page, dmlc::SeekStream* fi) = 0;
+  /*!
+   * \brief read only the segments we are interested in, advance fi to end of the block.
+   * \param page The page to load the data into.
+   * \param fi the input stream of the file
+   * \param sorted_index_set sorted index of segments we are interested in
+   * \return true of the loading as successful, false if end of file was reached
+   */
+  virtual bool Read(SparsePage* page,
+                    dmlc::SeekStream* fi,
+                    const std::vector<bst_uint>& sorted_index_set) = 0;
+  /*!
+   * \brief save the data to fo, when a page was written.
+   * \param fo output stream
+   */
+  virtual void Write(const SparsePage& page, dmlc::Stream* fo) = 0;
+  /*!
+   * \brief Create sparse page of format.
+   * \return The created format functors.
+   */
+  static SparsePageFormat* Create(const std::string& name);
+  /*!
+   * \brief decide the format from cache prefix.
+   * \return pair of row format, column format type of the cache prefix.
+   */
+  static std::pair<std::string, std::string> DecideFormat(const std::string& cache_prefix);
+};
+    }
+    }
+    
+      /*!
+   * \brief add an element to a sketch
+   * \param x The element added to the sketch
+   * \param w The weight of the element.
+   */
+  inline void Push(DType x, RType w = 1) {
+    if (w == static_cast<RType>(0)) return;
+    if (inqueue.qtail == inqueue.queue.size()) {
+      // jump from lazy one value to limit_size * 2
+      if (inqueue.queue.size() == 1) {
+        inqueue.queue.resize(limit_size * 2);
+      } else {
+        temp.Reserve(limit_size * 2);
+        inqueue.MakeSummary(&temp);
+        // cleanup queue
+        inqueue.qtail = 0;
+        this->PushTemp();
+      }
+    }
+    inqueue.Push(x, w);
+  }
+    
+        // want to compute storage boundary for each feature
+    // using variants of prefix sum scan
+    boundary_.resize(nfeature);
+    size_t accum_index_ = 0;
+    size_t accum_row_ind_ = 0;
+    for (bst_uint fid = 0; fid < nfeature; ++fid) {
+      boundary_[fid].index_begin = accum_index_;
+      boundary_[fid].row_ind_begin = accum_row_ind_;
+      if (type_[fid] == kDenseColumn) {
+        accum_index_ += static_cast<size_t>(nrow);
+        accum_row_ind_ += static_cast<size_t>(nrow);
+      } else {
+        accum_index_ += feature_counts_[fid];
+        accum_row_ind_ += feature_counts_[fid];
+      }
+      boundary_[fid].index_end = accum_index_;
+      boundary_[fid].row_ind_end = accum_row_ind_;
+    }
+    
+    namespace xgboost {
+/*!
+ * \brief interface of gradient boosting model.
+ */
+class GradientBooster {
+ public:
+  /*! \brief virtual destructor */
+  virtual ~GradientBooster() = default;
+  /*!
+   * \brief set configuration from pair iterators.
+   * \param begin The beginning iterator.
+   * \param end The end iterator.
+   * \tparam PairIter iterator<std::pair<std::string, std::string> >
+   */
+  template<typename PairIter>
+  inline void Configure(PairIter begin, PairIter end);
+  /*!
+   * \brief Set the configuration of gradient boosting.
+   *  User must call configure once before InitModel and Training.
+   *
+   * \param cfg configurations on both training and model parameters.
+   */
+  virtual void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) = 0;
+  /*!
+   * \brief load model from stream
+   * \param fi input stream.
+   */
+  virtual void Load(dmlc::Stream* fi) = 0;
+  /*!
+   * \brief save model to stream.
+   * \param fo output stream
+   */
+  virtual void Save(dmlc::Stream* fo) const = 0;
+  /*!
+   * \brief whether the model allow lazy checkpoint
+   * return true if model is only updated in DoBoost
+   * after all Allreduce calls
+   */
+  virtual bool AllowLazyCheckPoint() const {
+    return false;
+  }
+  /*!
+   * \brief perform update to the model(boosting)
+   * \param p_fmat feature matrix that provide access to features
+   * \param in_gpair address of the gradient pair statistics of the data
+   * \param obj The objective function, optional, can be nullptr when use customized version
+   * the booster may change content of gpair
+   */
+  virtual void DoBoost(DMatrix* p_fmat,
+                       HostDeviceVector<GradientPair>* in_gpair,
+                       ObjFunction* obj = nullptr) = 0;
+    }
+    }
+    
+        Span<float const> cs (arr, arr + 16);
+    ASSERT_EQ (cs.size(), 16);
+    ASSERT_EQ (cs.data(), arr);
+    ASSERT_EQ (cs.data() + cs.size(), arr + 16);
+    
+    namespace osquery {
+namespace table_tests {
 namespace {
     }
     }
     }
+    
+    TEST_F(SchedulerTests, test_scheduler_reload) {
+  std::string config =
+      '{\'schedule\':{\'1\':{'
+      '\'query\':\'select * from processes\', \'interval\':1}}}';
+  auto backup_reload = FLAGS_schedule_reload;
     }
     
+    #include 'row.h'
     
-    {                            '',
-                            {
-                          ' detached comment\n'
-                            },
-                              ' leading comment\n'
-                            },
-    
-    
-    {}  // namespace guetzli
-    
-    namespace guetzli {
+    class ExtensionManager_extensions_result {
+ public:
     }
     
-    // Note about the CORRECT_LSB macro:
-// using 16bit fixed-point constants, we often compute products like:
-// p = (A*x + B*y + 32768) >> 16 by adding two sub-terms q = (A*x) >> 16
-// and r = (B*y) >> 16 together. Statistically, we have p = q + r + 1
-// in 3/4 of the cases. This can be easily seen from the relation:
-//   (a + b + 1) >> 1 = (a >> 1) + (b >> 1) + ((a|b)&1)
-// The approximation we are doing is replacing ((a|b)&1) by 1.
-// In practice, this is a slightly more involved because the constants A and B
-// have also been rounded compared to their exact floating point value.
-// However, all in all the correction is quite small, and CORRECT_LSB can
-// be defined empty if needed.
+      void call(ExtensionResponse& _return, const std::string& registry, const std::string& item, const ExtensionPluginRequest& request) {
+    // Your implementation goes here
+    printf('call\n');
+  }
     
-    constexpr int kDefaultMemlimitMB = 6000; // in MB
-    
-    // Decodes the parsed jpeg coefficients into an RGB image.
-// There can be only either 1 or 3 image components, in either case, an RGB
-// output image will be generated.
-// Only YUV420 and YUV444 sampling factors are supported.
-// Vector will be empty if a decoding error occurred.
-std::vector<uint8_t> DecodeJpegToRGB(const JPEGData& jpg);
-    
-    #endif  // GUETZLI_JPEG_DATA_ENCODER_H_
-
-    
-    // Writes DHT and SOS marker segments to out and fills in DC/AC Huffman tables
-// for each component of the image.
-bool BuildAndEncodeHuffmanCodes(const JPEGData& jpg, JPEGOutput out,
-                                std::vector<HuffmanCodeTable>* dc_huff_tables,
-                                std::vector<HuffmanCodeTable>* ac_huff_tables) {
-  const int ncomps = jpg.components.size();
-  dc_huff_tables->resize(ncomps);
-  ac_huff_tables->resize(ncomps);
+    QueryData genNFSShares(QueryContext& context) {
+  QueryData results;
     }
     
-                int complement = target - nums[i];
-            if(record.find(complement) != record.end()){
-                int res[] = {i, record[complement]};
-                return vector<int>(res, res + 2);
-            }
+    #include <osquery/core.h>
+#include <osquery/database.h>
+#include <osquery/filesystem/filesystem.h>
+#include <osquery/logger.h>
+#include <osquery/tables.h>
     
-        vector<int> nums1 = {1, 1, 2};
-    cout << Solution().removeDuplicates(nums1) << endl;
-    
-    // Three Way Quick Sort
-// Time Complexity: O(n)
-// Space Complexity: O(1)
-class Solution {
-public:
-    void sortColors(vector<int> &nums) {
+      STDMETHODIMP RegisterOutputCallbacks::QueryInterface(
+      THIS_ _In_ REFIID InterfaceId, _Out_ PVOID* Interface) {
+    *Interface = nullptr;
+    if (IsEqualIID(InterfaceId, __uuidof(IUnknown)) ||
+        IsEqualIID(InterfaceId, __uuidof(IDebugOutputCallbacks))) {
+      *Interface = (IDebugOutputCallbacks*)this;
+      AddRef();
+      return S_OK;
+    } else {
+      return E_NOINTERFACE;
     }
-    }
-    
-    using namespace std;
-    
-    private:
-    void __inorderTraversal(TreeNode* node, vector<int> &res){
-    }
-    
-    /// BFS
-/// Time Complexity: O(n), where n is the number of nodes in the tree
-/// Space Complexity: O(n)
-class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-    }
-    }
+  }
