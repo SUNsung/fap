c1d3d958773d57c6790670eb876e2068972c61a5
SUNsung <weatherius@gmail.com>
2019-02-08 04:25:33 +0200

    Rewrite [15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb] File in: 2019-02-08 04:25:33

--- ruby/15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb
+++ ruby/15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb
@@ -1,69 +1,43 @@
 
         
-                def importer_class
-          IssueAndLabelLinksImporter
-        end
-    
-              page.objects.each do |object|
-            next if already_imported?(object)
-    
-            def truncated_title
-          title.truncate(255)
-        end
-    
-    module Gitlab
-  module GithubImport
-    module Representation
-      class PullRequest
-        include ToHash
-        include ExposeAttribute
-    
-            # Builds a user using a Hash that was built from a JSON payload.
-        def self.from_json_hash(raw_hash)
-          new(Representation.symbolize_hash(raw_hash))
-        end
-    
-          def call(env)
-        transaction, retval = Transaction.run do
-          @app.call(env)
-        end
-    
-          emoji = CustomEmoji.find_or_initialize_by(domain: nil,
-                                                shortcode: @custom_emoji.shortcode)
-      emoji.image = @custom_emoji.image
-    
-        def set_report_note
-      @report_note = ReportNote.find(params[:id])
-    end
-  end
+        def custom_release_header_anchors(markdown)
+  header_regexp = %r!^(\d{1,2})\.(\d{1,2})\.(\d{1,2}) \/ \d{4}-\d{2}-\d{2}!
+  section_regexp = %r!^### \w+ \w+$!
+  markdown.split(%r!^##\s!).map do |release_notes|
+    _, major, minor, patch = *release_notes.match(header_regexp)
+    release_notes
+      .gsub(header_regexp, '\\0\n{: #v\\1-\\2-\\3}')
+      .gsub(section_regexp) { |section| '#{section}\n{: ##{slugify(section)}-v#{major}-#{minor}-#{patch}}' }
+  end.join('\n## ')
 end
-
     
-    module Admin
-  class ReportedStatusesController < BaseController
-    before_action :set_report
-    
-      def show
-    render_cached_json('api:v1:instances:activity:show', expires_in: 1.day) { activity }
-  end
-    
-      private
-    
-      context 'every shim script' do
-    it 'has valid bash syntax' do
-      # These have no file extension, but can be identified by their shebang.
-      (HOMEBREW_LIBRARY_PATH/'shims').find do |path|
-        next if path.directory?
-        next if path.symlink?
-        next unless path.executable?
-        next unless path.read(12) == '#!/bin/bash\n'
-    
-    
-    end
-  end
-end
-
-    
-            _render { content }
+      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
+    cmd = g.split('-').last
+    p.command(cmd.to_sym) do |c|
+      c.syntax cmd
+      c.action do
+        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
+          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
       end
     end
+  end
+    
+      it 'has no effect on immediate values' do
+    [nil, true, false].each do |v|
+      v.taint
+      v.tainted?.should == false
+    end
+  end
+    
+            # rubocop:disable Style/MethodMissing
+        def method_missing(key, value=nil)
+          if value
+            set(lvalue(key), value)
+          else
+            fetch(key)
+          end
+        end
+        # rubocop:enable Style/MethodMissing
+    
+            # Skip validation behavior if no validators are registered for this key
+        return super unless validators.key?(key)
