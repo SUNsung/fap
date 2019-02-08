146dbe0d6b4589dc130c24a7bacb49cc481f9fe6
SUNsung <weatherius@gmail.com>
2019-02-08 06:25:57 +0200

    Rewrite [27S6iSPuiMkrg27GNYQj7XbUwc27A6lWnp1LqXA27M7n.cpp] File in: 2019-02-08 06:25:56

--- cccccc/27S6iSPuiMkrg27GNYQj7XbUwc27A6lWnp1LqXA27M7n.cpp
+++ cccccc/27S6iSPuiMkrg27GNYQj7XbUwc27A6lWnp1LqXA27M7n.cpp
@@ -1,134 +1,162 @@
 
         
-        size_t num_threads = 31;
-size_t work_chunk  = 120;
+        #endif
+
     
-    //////////////////////////////////////////////////////////////////////
+    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
+/**
+ * Registers: rdx:rax = multiplication accumulator
+ *            r9:r8   = c
+ *            rcx:rbx = d
+ *            r10-r14 = a0-a4
+ *            r15     = M (0xfffffffffffff)
+ *            rdi     = r
+ *            rsi     = a / t?
+ */
+  uint64_t tmp1, tmp2, tmp3;
+__asm__ __volatile__(
+    'movq 0(%%rsi),%%r10\n'
+    'movq 8(%%rsi),%%r11\n'
+    'movq 16(%%rsi),%%r12\n'
+    'movq 24(%%rsi),%%r13\n'
+    'movq 32(%%rsi),%%r14\n'
+    'movq $0xfffffffffffff,%%r15\n'
+    }
     
-        VX_form_t vx_formater {{
-      xo,
-      static_cast<uint32_t>(rb),
-      static_cast<uint32_t>(ra),
-      static_cast<uint32_t>(rt),
-      op
-    }};
-    
-    
-    {private:
-  APCHandle m_handle;
-  APCHandle* m_arrayHandle;
-  CollectionType m_colType;
-};
-    
-      /**
-   * Prefer the Bind() over the GetFoo() as it makes ini_get() work too.
-   * These Bind()s should be used for ini settings. Specifically, they
-   * should be used when the bound setting is needed before the main ini
-   * processing pass. Unlike IniSetting::Bind, these bindings will fetch the
-   * value in an ini setting if it is set otherwise it will use the defValue.
-   */
-  static void Bind(bool& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const bool defValue = false,
-                   const bool prepend_hhvm = true);
-  static void Bind(const char*& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const char *defValue = nullptr,
-                   const bool prepend_hhvm = true);
-  static void Bind(std::string& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const std::string defValue = '',
-                   const bool prepend_hhvm = true);
-  static void Bind(char& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const char defValue = 0, const bool prepend_hhvm = true);
-  static void Bind(unsigned char& loc,const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const unsigned char defValue = 0,
-                   const bool prepend_hhvm = true);
-  static void Bind(int16_t& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const int16_t defValue = 0,
-                   const bool prepend_hhvm = true);
-  static void Bind(uint16_t& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const uint16_t defValue = 0,
-                   const bool prepend_hhvm = true);
-  static void Bind(int32_t& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const int32_t defValue = 0,
-                   const bool prepend_hhvm = true);
-  static void Bind(uint32_t& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const uint32_t defValue = 0,
-                   const bool prepend_hhvm = true);
-  static void Bind(int64_t& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const int64_t defValue = 0,
-                   const bool prepend_hhvm = true);
-  static void Bind(uint64_t& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const uint64_t defValue = 0,
-                   const bool prepend_hhvm = true);
-  static void Bind(double& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name = '',
-                   const double defValue = 0,
-                   const bool prepend_hhvm = true);
-  static void Bind(HackStrictOption& loc, const IniSettingMap &ini,
-                   const Hdf& config, const std::string& name,
-                   HackStrictOption def);
-  static void
-  Bind(std::vector<uint32_t>& loc, const IniSettingMap& ini,
-       const Hdf& config, const std::string& name = '',
-       const std::vector<uint32_t>& defValue = std::vector<uint32_t>(),
-       const bool prepend_hhvm = true);
-  static void
-  Bind(std::vector<std::string>& loc, const IniSettingMap& ini,
-       const Hdf& config, const std::string& name = '',
-       const std::vector<std::string>& defValue = std::vector<std::string>(),
-       const bool prepend_hhvm = true);
-  static void
-  Bind(std::unordered_map<std::string, int>& loc,
-       const IniSettingMap& ini, const Hdf& config,
-       const std::string& name = '',
-       const std::unordered_map<std::string, int>& defValue =
-         std::unordered_map<std::string, int>{},
-       const bool prepend_hhvm = true);
-  static void Bind(ConfigMap& loc, const IniSettingMap& ini, const Hdf& config,
-                   const std::string& name = '',
-                   const ConfigMap& defValue = ConfigMap(),
-                   const bool prepend_hhvm = true);
-  static void Bind(ConfigMapC& loc, const IniSettingMap& ini, const Hdf& config,
-                   const std::string& name = '',
-                   const ConfigMapC& defValue = ConfigMapC(),
-                   const bool prepend_hhvm = true);
-  static void Bind(ConfigSet& loc, const IniSettingMap& ini, const Hdf& config,
-                   const std::string& name = '',
-                   const ConfigSet& defValue = ConfigSet(),
-                   const bool prepend_hhvm = true);
-  static void Bind(ConfigSetC& loc, const IniSettingMap& ini, const Hdf& config,
-                   const std::string& name = '',
-                   const ConfigSetC& defValue = ConfigSetC(),
-                   const bool prepend_hhvm = true);
-  static void Bind(ConfigIMap& loc, const IniSettingMap& ini, const Hdf& config,
-                   const std::string& name = '',
-                   const ConfigIMap& defValue = ConfigIMap(),
-                   const bool prepend_hhvm = true);
-  static void Bind(ConfigFlatSet& loc, const IniSettingMap& ini,
-                   const Hdf& config, const std::string& name = '',
-                   const ConfigFlatSet& defValue = ConfigFlatSet(),
-                   const bool prepend_hhvm = true);
-    
-    
-    {  auto ret = m_it.second();
-  assertx(ret.isString());
-  ++m_it;
-  return Variant(HHVM_FN(basename)(ret.toString()));
+    static void secp256k1_gej_set_ge(secp256k1_gej *r, const secp256k1_ge *a) {
+   r->infinity = a->infinity;
+   r->x = a->x;
+   r->y = a->y;
+   secp256k1_fe_set_int(&r->z, 1);
 }
     
-    #ifndef HPHP_GLOB_STREAM_WRAPPER_H
-#define HPHP_GLOB_STREAM_WRAPPER_H
+    static void secp256k1_sha256_finalize(secp256k1_sha256_t *hash, unsigned char *out32) {
+    static const unsigned char pad[64] = {0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    uint32_t sizedesc[2];
+    uint32_t out[8];
+    int i = 0;
+    sizedesc[0] = BE32(hash->bytes >> 29);
+    sizedesc[1] = BE32(hash->bytes << 3);
+    secp256k1_sha256_write(hash, pad, 1 + ((119 - (hash->bytes % 64)) % 64));
+    secp256k1_sha256_write(hash, (const unsigned char*)sizedesc, 8);
+    for (i = 0; i < 8; i++) {
+        out[i] = BE32(hash->s[i]);
+        hash->s[i] = 0;
+    }
+    memcpy(out32, (const unsigned char*)out, 32);
+}
     
-    #include <folly/String.h>
+            secp256k1_ecmult_const(&res, &pt, &s);
+        secp256k1_ge_set_gej(&pt, &res);
+        /* Compute a hash of the point in compressed form
+         * Note we cannot use secp256k1_eckey_pubkey_serialize here since it does not
+         * expect its output to be secret and has a timing sidechannel. */
+        secp256k1_fe_normalize(&pt.x);
+        secp256k1_fe_normalize(&pt.y);
+        secp256k1_fe_get_b32(x, &pt.x);
+        y[0] = 0x02 | secp256k1_fe_is_odd(&pt.y);
     
-    #include 'hphp/util/struct-log.h'
+    void test_bad_scalar(void) {
+    unsigned char s_zero[32] = { 0 };
+    unsigned char s_overflow[32] = {
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe,
+        0xba, 0xae, 0xdc, 0xe6, 0xaf, 0x48, 0xa0, 0x3b,
+        0xbf, 0xd2, 0x5e, 0x8c, 0xd0, 0x36, 0x41, 0x41
+    };
+    unsigned char s_rand[32] = { 0 };
+    unsigned char output[32];
+    secp256k1_scalar rand;
+    secp256k1_pubkey point;
+    }
+    
+    static int recovery_test_nonce_function(unsigned char *nonce32, const unsigned char *msg32, const unsigned char *key32, const unsigned char *algo16, void *data, unsigned int counter) {
+    (void) msg32;
+    (void) key32;
+    (void) algo16;
+    (void) data;
+    }
+    
+    BOOST_AUTO_TEST_CASE(bip173_testvectors_valid)
+{
+    static const std::string CASES[] = {
+        'A12UEL5L',
+        'a12uel5l',
+        'an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs',
+        'abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw',
+        '11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j',
+        'split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w',
+        '?1ezyfcl',
+    };
+    for (const std::string& str : CASES) {
+        auto ret = bech32::Decode(str);
+        BOOST_CHECK(!ret.first.empty());
+        std::string recode = bech32::Encode(ret.first, ret.second);
+        BOOST_CHECK(!recode.empty());
+        BOOST_CHECK(CaseInsensitiveEqual(str, recode));
+    }
+}
+    
+    // To keep it as transparent as possible, the macros below have been generated
+// using python via the excellent cog.py code generation script.  This avoids
+// the need for a bunch of complex (but more general) preprocessor tricks as
+// used in boost.preprocessor.
+//
+// To rerun the code generation in place, use `cog.py -r tinyformat.h`
+// (see http://nedbatchelder.com/code/cog).  Alternatively you can just create
+// extra versions by hand.
+    
+    
+    {    CRIPEMD160();
+    CRIPEMD160& Write(const unsigned char* data, size_t len);
+    void Finalize(unsigned char hash[OUTPUT_SIZE]);
+    CRIPEMD160& Reset();
+};
+    
+    #include <grpc/support/port_platform.h>
+    
+    
+    {}  // namespace grpc
+    
+    constexpr size_t RpcServerStatsEncoding::kRpcServerStatsSize;
+constexpr size_t RpcServerStatsEncoding::kEncodeDecodeFailure;
+constexpr size_t RpcServerStatsEncoding::kVersionIdSize;
+constexpr size_t RpcServerStatsEncoding::kFieldIdSize;
+constexpr size_t RpcServerStatsEncoding::kVersionIdOffset;
+constexpr size_t RpcServerStatsEncoding::kVersionId;
+    
+    void SetCreateThreadPool(CreateThreadPoolFunc func) { g_ctp_impl = func; }
+    
+    class DynamicThreadPool final : public ThreadPoolInterface {
+ public:
+  explicit DynamicThreadPool(int reserve_threads);
+  ~DynamicThreadPool();
+    }
+    
+    #include <utility>
+    
+      for (auto alphabet_size : test_cases) {
+    for (int i = 0; i < repetitions; i++) {
+      std::vector<int> input(num_elements);
+      std::generate(input.begin(), input.end(),
+        [=]() { return rand() % alphabet_size; });
+      CompressedBufferWriter cbw(alphabet_size);
+    }
+    }
+    
+      inline static void LimitSizeLevel
+    (size_t maxn, double eps, size_t* out_nlevel, size_t* out_limit_size) {
+    size_t& nlevel = *out_nlevel;
+    size_t& limit_size = *out_limit_size;
+    nlevel = 1;
+    while (true) {
+      limit_size = static_cast<size_t>(ceil(nlevel / eps)) + 1;
+      size_t n = (1ULL << nlevel);
+      if (n * limit_size >= maxn) break;
+      ++nlevel;
+    }
+    // check invariant
+    size_t n = (1ULL << nlevel);
+    CHECK(n * limit_size >= maxn) << 'invalid init parameter';
+    CHECK(nlevel <= limit_size * eps) << 'invalid init parameter';
+  }
