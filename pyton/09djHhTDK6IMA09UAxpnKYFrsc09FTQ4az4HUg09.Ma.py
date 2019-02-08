9cc9822a8bf8ad6b09313d18278f1d2ee3b18f8b
SUNsung <weatherius@gmail.com>
2019-02-08 05:28:47 +0200

    Rewrite [09djHhTDK6IMA09UAxpnKYFrsc09FTQ4az4HUg09.Ma.py] File in: 2019-02-08 05:28:46

--- pyton/09djHhTDK6IMA09UAxpnKYFrsc09FTQ4az4HUg09.Ma.py
+++ pyton/09djHhTDK6IMA09UAxpnKYFrsc09FTQ4az4HUg09.Ma.py
@@ -1,81 +1,81 @@
 
         
-            def __init__(self, employee_id, name, rank, call_center):
-        self.employee_id = employee_id
-        self.name = name
-        self.rank = rank
-        self.call = None
-        self.call_center = call_center
+        
+@functools.lru_cache()
+def get_citext_oids(connection_alias):
+    '''Return citext array OIDs.'''
+    with connections[connection_alias].cursor() as cursor:
+        cursor.execute('SELECT typarray FROM pg_type WHERE typname = 'citext'')
+        return tuple(row[0] for row in cursor)
     
-        def override_category_budget(self, category, amount):
-        self.categories_to_budget_map[category] = amount
+    KEY_PREFIX = 'django.contrib.sessions.cache'
+    
+        def _get_session_from_db(self):
+        try:
+            return self.model.objects.get(
+                session_key=self.session_key,
+                expire_date__gt=timezone.now()
+            )
+        except (self.model.DoesNotExist, SuspiciousOperation) as e:
+            if isinstance(e, SuspiciousOperation):
+                logger = logging.getLogger('django.security.%s' % e.__class__.__name__)
+                logger.warning(str(e))
+            self._session_key = None
+    
+    
+class BaseSessionManager(models.Manager):
+    def encode(self, session_dict):
+        '''
+        Return the given session dictionary serialized and encoded as a string.
+        '''
+        session_store_class = self.model.get_session_store_class()
+        return session_store_class().encode(session_dict)
+    
+      # include_dict is modified during iteration, so we iterate over a copy of
+  # the keys.
+  header_keys = list(include_dict.keys())
+  for header in header_keys:
+    (same_module, common_path) = FilesBelongToSameModule(abs_filename, header)
+    fullpath = common_path + header
+    if same_module and UpdateIncludeState(fullpath, include_dict, io):
+      header_found = True
+    
+            _LOGGER.info('Scanner initialized')
+    
+            devices = {}
+        for device in request.json()['status']:
+            try:
+                devices[device['Key']] = {
+                    'ip': device['IPAddress'],
+                    'mac': device['PhysAddress'],
+                    'host': device['Name'],
+                    'status': device['Active']
+                    }
+            except (KeyError, requests.exceptions.RequestException):
+                pass
+        return devices
 
     
+    import voluptuous as vol
     
-class PersonServer(object):
+        def __init__(self, token, default_room, default_color, default_notify,
+                 default_format, host):
+        '''Initialize the service.'''
+        self._token = token
+        self._default_room = default_room
+        self._default_color = default_color
+        self._default_notify = default_notify
+        self._default_format = default_format
+        self._host = host
     
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
+        def backwards(self, orm):
+        'Write your backwards methods here.'
     
-    
-iv = key = [0x20, 0x15] + 14 * [0]
-    
-    filenames = {
-    'bin': 'youtube-dl',
-    'exe': 'youtube-dl.exe',
-    'tar': 'youtube-dl-%s.tar.gz' % version}
-build_dir = os.path.join('..', '..', 'build', version)
-for key, filename in filenames.items():
-    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
-    fn = os.path.join(build_dir, filename)
-    with open(fn, 'rb') as f:
-        data = f.read()
-    if not data:
-        raise ValueError('File %s is empty!' % fn)
-    sha256sum = hashlib.sha256(data).hexdigest()
-    new_version[key] = (url, sha256sum)
-    
-    versions_info = json.load(open('update/versions.json'))
-if 'signature' in versions_info:
-    del versions_info['signature']
-    
-        with io.open(outfile, 'w', encoding='utf-8') as outf:
-        outf.write(out)
-    
-        return ret
-    
-    
-def issue_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    ref = 'https://github.com/scrapy/scrapy/issues/' + text
-    set_classes(options)
-    node = nodes.reference(rawtext, 'issue ' + text, refuri=ref, **options)
-    return [node], []
-    
-    # Declare top-level shortcuts
-from scrapy.spiders import Spider
-from scrapy.http import Request, FormRequest
-from scrapy.selector import Selector
-from scrapy.item import Item, Field
-    
-        from scrapy.core.downloader.tls import ScrapyClientTLSOptions, DEFAULT_CIPHERS
-    
-            dfd.addBoth(lambda _: self.signals.send_catch_log_deferred(
-            signal=signals.spider_closed, spider=spider, reason=reason))
-        dfd.addErrback(log_failure('Error while sending spider_close signal'))
-    
-        return diff
+        complete_apps = ['sentry']
+    symmetrical = True
 
     
-    from matplotlib import pyplot as plt
-def plot_heterogeneity(heterogeneity, k):
-    plt.figure(figsize=(7,4))
-    plt.plot(heterogeneity, linewidth=4)
-    plt.xlabel('# Iterations')
-    plt.ylabel('Heterogeneity')
-    plt.title('Heterogeneity of clustering over time, K={0:d}'.format(k))
-    plt.rcParams.update({'font.size': 16})
-    plt.show()
+            # Adding unique constraint on 'CommitAuthor', fields ['organization_id', 'external_id']
+        db.create_unique('sentry_commitauthor', ['organization_id', 'external_id'])
+    
+        complete_apps = ['sentry']
