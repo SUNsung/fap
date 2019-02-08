9b7aeacffe43711c2b2f049f213d4106dc20dcc0
SUNsung <weatherius@gmail.com>
2019-02-08 13:02:12 +0200

    Rewrite [35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp] File in: 2019-02-08 13:02:11

--- cccccc/35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp
+++ cccccc/35WPATLx.r4Zk35cgK8ooqi2VE35oQo9cFXxj..35qdH.cpp
@@ -1,87 +1,168 @@
 
         
-        namespace swig {
-namespace {
-    }
-    }
-    
-    
-    {  DCHECK(PyDict_Check(code_to_exc_type_map));
-  PyObject* key;
-  PyObject* value;
-  Py_ssize_t pos = 0;
-  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
-    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
-    singleton_->exc_types_[code] = value;
-    // The exception classes should also have the lifetime of the process, but
-    // incref just in case.
-    Py_INCREF(value);
-  }
-}
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#ifndef TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
-    
-    
-    {}  // end namespace tensorflow
-    
-    string TryFindKernelClass(const string& serialized_node_def) {
-  tensorflow::NodeDef node_def;
-  if (!node_def.ParseFromString(serialized_node_def)) {
-    LOG(WARNING) << 'Error parsing node_def';
-    return '';
-  }
+        class InAppPurchase : public mate::EventEmitter<InAppPurchase>,
+                      public in_app_purchase::TransactionObserver {
+ public:
+  static mate::Handle<InAppPurchase> Create(v8::Isolate* isolate);
     }
     
+    class Net : public mate::EventEmitter<Net> {
+ public:
+  static v8::Local<v8::Value> Create(v8::Isolate* isolate);
+    }
     
-    { private:
-  unsigned flags_;
+    class Tray : public mate::TrackableObject<Tray>, public TrayIconObserver {
+ public:
+  static mate::WrappableBase* New(mate::Handle<NativeImage> image,
+                                  mate::Arguments* args);
+    }
+    
+    #ifndef ATOM_BROWSER_API_SAVE_PAGE_HANDLER_H_
+#define ATOM_BROWSER_API_SAVE_PAGE_HANDLER_H_
+    
+      int32_t weak_map_id_ = 0;
+    
+    
+    {  api::WebContents* api_web_contents_;
+  std::map<std::string, int> origin_counts_;
 };
     
-    void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
-  // Prepare all the blobs.
-  GenericVector<BlobData> blobs;
-  for (int w = 0; w < words.size(); ++w) {
-    if (words[w].word->ratings != nullptr &&
-        words[w].word->ratings->get(0, 0) == nullptr) {
-      for (int s = 0; s < words[w].lang_words.size(); ++s) {
-        Tesseract* sub = s < sub_langs_.size() ? sub_langs_[s] : this;
-        const WERD_RES& word = *words[w].lang_words[s];
-        for (int b = 0; b < word.chopped_word->NumBlobs(); ++b) {
-          blobs.push_back(BlobData(b, sub, word));
+    Delegate* AutoUpdater::delegate_ = nullptr;
+    
+    
+    {}  // namespace auto_updater
+    
+    #if defined(OS_LINUX)
+#include 'atom/browser/lib/power_observer_linux.h'
+#else
+#include 'base/power_monitor/power_observer.h'
+#endif  // defined(OS_LINUX)
+    
+    // --------------------------- Structures ---------------------------
+    
+    namespace atom {
+    }
+    
+    void URLRequestAboutJob::Start() {
+  base::ThreadTaskRunnerHandle::Get()->PostTask(
+      FROM_HERE, base::BindOnce(&URLRequestAboutJob::StartAsync,
+                                weak_ptr_factory_.GetWeakPtr()));
+}
+    
+            x -= 8;
+        if(x == colsn){
+            x -= cn;
         }
-      }
+        s16 pprevx[4], prevx[4], rowx[4], nextx[4], nnextx[4];
+        ptrdiff_t px = x / cn;
+        for( s32 k = 0; k < cn; k++ )
+        {
+            ptrdiff_t ploc;
+            ploc = internal::borderInterpolate(px-2, size.width, borderType);
+            pprevx[k] = ploc < 0 ? 5*borderValue :
+                                   srow4[ploc*cn+k] + srow3[ploc*cn+k] + srow2[ploc*cn+k] + srow1[ploc*cn+k] + srow0[ploc*cn+k];
     }
-  }
-  // Pre-classify all the blobs.
-  if (tessedit_parallelize > 1) {
-#ifdef _OPENMP
-#pragma omp parallel for num_threads(10)
-#endif  // _OPENMP
-    for (int b = 0; b < blobs.size(); ++b) {
-      *blobs[b].choices =
-          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    
+    #define ALPHA_QUAD(sgn, bits) { \
+                                  internal::prefetch(src + sj); \
+                                  union { vec128_4 v4; vec128_3 v3; } vals; \
+                                  vals.v4 = vld4q_##sgn##bits(src + sj); \
+                                  vst3q_##sgn##bits(dst3 + d3j, vals.v3); \
+                                  vst1q_##sgn##bits(dst1 + d1j, vals.v4.val[3]); \
+                              }
+    
+    namespace CAROTENE_NS {
     }
-  } else {
-    // TODO(AMD) parallelize this.
-    for (int b = 0; b < blobs.size(); ++b) {
-      *blobs[b].choices =
-          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    
+    s32 countNonZero(const Size2D &_size,
+                 const s32 * srcBase, ptrdiff_t srcStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    Size2D size(_size);
+    if (srcStride == (ptrdiff_t)(size.width))
+    {
+        size.width *= size.height;
+        size.height = 1;
     }
-  }
-}
+    size_t roiw4 = size.width & ~3u;
+    s32 result = 0;
+    for(size_t k = 0; k < size.height; ++k)
+    {
+        const u32* src = (const u32*)internal::getRowPtr( srcBase,  srcStride, k);
+        u32 i = 0;
+    }
+    }
     
+    #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Variable.h'
+#include 'CompositeFunction.h'
+#include 'Serialization.h'
+#include 'InputAndParamNodes.h'
     
-    {  bool rword_indicates_list_item;
-  bool rword_likely_starts_idea;
-  bool rword_likely_ends_idea;
-};
+    namespace Microsoft { namespace MSR { namespace CNTK {
+    }
+    }
+    }
     
-      // Gets a VC object identified by its ID.
-  static ParamContent* GetParamContentById(int id);
+            // add it to the respective node groups based on the tags
+        for (auto tag : node->GetTags())
+        {
+#if 1       // we keep this for a while (we already verified that our samples no longer use this)
+            // map legacy names
+            if      (tag == L'criteria') tag = L'criterion';
+            else if (tag == L'eval'    ) tag = L'evaluation';
+#endif
+            AddToNodeGroup(tag, node); // tag may be empty, or may have been set by array parameters
+        }
+    
+        virtual void CopyTo(ComputationNodeBasePtr nodeP, const std::wstring& newName, const CopyNodeFlags flags) const override
+    {
+        Base::CopyTo(nodeP, newName, flags);
+        if (flags & CopyNodeFlags::copyNodeValue)
+        {
+            auto node = dynamic_pointer_cast<DiagTimesNode<ElemType>>(nodeP);
+            node->m_innerproduct->SetValue(*m_innerproduct);
+            node->m_rightGradient->SetValue(*m_rightGradient);
+        }
+    }
+    // request matrices that are needed for gradient computation
+    virtual void RequestMatricesBeforeBackprop(MatrixPool& matrixPool)
+    {
+        Base::RequestMatricesBeforeBackprop(matrixPool);
+        RequestMatrixFromPool(m_innerproduct, matrixPool);
+        RequestMatrixFromPool(m_rightGradient, matrixPool);
+    }
+    
+    #if defined(_WIN32) || defined(__WIN32__)
+    #ifdef NAPA_EXAMPLE_API
+        #define NAPA_EXAMPLE_EXPORT __declspec(dllexport)
+    #else
+        #define NAPA_EXAMPLE_EXPORT __declspec(dllimport)
+    #endif
+#else
+    #define NAPA_EXAMPLE_EXPORT
+#endif
+    
+    #define CATCH_INTERNAL_CONFIG_COUNTER
+    
+    /// <summary> It sets the accessors for the given V8 function template object. </summary>
+#define NAPA_SET_ACCESSOR(functionTemplate, name, getter, setter) \
+    functionTemplate->InstanceTemplate()->SetAccessor(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), name), \
+                                                      getter, \
+                                                      setter)
+    
+        /// <summary> Get 'module' object of napa binding, which is napa-binding.node in Node.JS isolate or napa-binding from core-modules in Napa isolate. </summary>
+    /// <returns> 'module' object for napa binding (napajs/bin/napa-binding.node or napa.dll) </returns>
+    NAPA_BINDING_API v8::Local<v8::Object> GetModule();
+    
+            CHECK_ARG(isolate, args.Length() == 0, 'class \'%s\' doesn't accept any arguments in constructor.'', WrapType::exportName);
+        JS_ENSURE(isolate, args.IsConstructCall(), 'class \'%s\' allows constructor call only.', WrapType::exportName);
+    
+    namespace napa {
+namespace providers {
+    }
+    }
+    
+        protected:
