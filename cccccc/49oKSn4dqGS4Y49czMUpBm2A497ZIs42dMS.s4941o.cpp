150391c01d08a43682a27cc7cc7b9431e66a80f6
SUNsung <weatherius@gmail.com>
2019-02-08 09:47:10 +0200

    Rewrite [49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp] File in: 2019-02-08 09:47:09

--- cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
+++ cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
@@ -1,127 +1,259 @@
 
         
-                  if (!IsInplace) {
-            // Just swap both pointers if we allocated on the heap
-            to->ptr_ = from->ptr_;
+        
+    {    if (!ok) {
+      thread->stats.AddMessage('(snappy failure)');
+    } else {
+      char buf[100];
+      snprintf(buf, sizeof(buf), '(output: %.1f%%)',
+               (produced * 100.0) / bytes);
+      thread->stats.AddMessage(buf);
+      thread->stats.AddBytes(bytes);
+    }
+  }
+    
+      Status OpenCompactionOutputFile(CompactionState* compact);
+  Status FinishCompactionOutputFile(CompactionState* compact, Iterator* input);
+  Status InstallCompactionResults(CompactionState* compact)
+      EXCLUSIVE_LOCKS_REQUIRED(mutex_);
+    
+        Iterator* iterator2 = db_->NewIterator(read_options);
+    iterator2->SeekToFirst();
+    ASSERT_TRUE(!iterator2->Valid());
+    delete iterator2;
+    
+    Status DumpTable(Env* env, const std::string& fname, WritableFile* dst) {
+  uint64_t file_size;
+  RandomAccessFile* file = nullptr;
+  Table* table = nullptr;
+  Status s = env->GetFileSize(fname, &file_size);
+  if (s.ok()) {
+    s = env->NewRandomAccessFile(fname, &file);
+  }
+  if (s.ok()) {
+    // We use the default comparator, which may or may not match the
+    // comparator used in this database. However this should not cause
+    // problems since we only use Table operations that do not require
+    // any comparisons.  In particular, we do not call Seek or Prev.
+    s = Table::Open(Options(), file, file_size, &table);
+  }
+  if (!s.ok()) {
+    delete table;
+    delete file;
+    return s;
+  }
     }
     
-    /* optparse */
-#define OPTPARSE_IMPLEMENTATION
-#include 'helpers/optparse.h'
+      // Returns the physical offset of the last record returned by ReadRecord.
+  //
+  // Undefined before the first call to ReadRecord.
+  uint64_t LastRecordOffset();
     
-    #include 'HttpParser.h'
-#include 'AsyncSocketData.h'
-#include <functional>
+      std::string ManifestFileName() {
+    std::string current;
+    ASSERT_OK(ReadFileToString(env_, CurrentFileName(dbname_), &current));
+    size_t len = current.size();
+    if (len > 0 && current[len-1] == '\n') {
+      current.resize(len - 1);
+    }
+    return dbname_ + '/' + current;
+  }
     
-    #endif // ASYNCSOCKETDATA_H
+    TEST(SkipTest, Concurrent1) { RunConcurrent(1); }
+TEST(SkipTest, Concurrent2) { RunConcurrent(2); }
+TEST(SkipTest, Concurrent3) { RunConcurrent(3); }
+TEST(SkipTest, Concurrent4) { RunConcurrent(4); }
+TEST(SkipTest, Concurrent5) { RunConcurrent(5); }
+    
+    /*!
++ * \brief The result holder of dispatch mode of each Node in the graph.
++ * \note Stored under graph.attrs['dispatch_mode'], provided by Pass 'InferStorageType'
++ *
++ * \code
++ *  Graph g = ApplyPass(src_graph, 'InferStorageType');
++ *  const DispatchModeVector& dispatch_modes = g.GetAttr<DispatchModeVector>('dispatch_mode');
++ *  // get dispatch mode by entry node id
++ *  int node_type = dispatch_modes[nid];
++ * \endcode
++ *
++ * \sa FInferStorageType
++ */
+using DispatchModeVector = std::vector<DispatchMode>;
+    
+    
+    {  g.attrs['storage_id'] = std::make_shared<nnvm::any>(std::move(storage_id));
+  g.attrs['storage_inplace_index'] = std::make_shared<nnvm::any>(
+      std::move(storage_inplace_index));
+  g.attrs['addto_entry'] = std::make_shared<nnvm::any>(std::move(addto_entry));
+  g.attrs['skip_plus_node'] = std::make_shared<nnvm::any>(std::move(skip_plus_node));
+  return g;
+}
+    
+    /*! \brief create a batch iterator from single instance iterator */
+class BatchLoader : public IIterator<TBlobBatch> {
+ public:
+  explicit BatchLoader(IIterator<DataInst> *base):
+    head_(1), num_overflow_(0), base_(base) {
+  }
+    }
+    
+    When `label_libsvm` is set to the path to another LibSVM file,
+data is read from `data_libsvm` and label from `label_libsvm`.
+In this case, both data and label are stored in the csr format.
+If the label column in the `data_libsvm` file is ignored.
+    
+      ~PrefetcherIter() {
+    while (recycle_queue_.size() != 0) {
+      DataBatch *batch = recycle_queue_.front();
+      recycle_queue_.pop();
+      delete batch;
+    }
+    delete out_;
+    iter.Destroy();
+  }
+    
+    
+    {    RegexMatcher  m(this);
+    int32_t r = 0;
+    // Check m's status to make sure all is ok.
+    if (U_SUCCESS(m.fDeferredStatus)) {
+    	r = m.split(input, dest, destCapacity, status);
+    }
+    return r;
+}
+    
+    SearchIterator::SearchIterator(const UnicodeString &text, 
+                                     BreakIterator *breakiter) :
+                                     m_breakiterator_(breakiter),
+                                     m_text_(text)
+{
+    m_search_                     = (USearch *)uprv_malloc(sizeof(USearch));
+    m_search_->breakIter          = NULL;
+    m_search_->isOverlap          = FALSE;
+    m_search_->isCanonicalMatch   = FALSE;
+    m_search_->elementComparisonType = 0;
+    m_search_->isForwardSearching = TRUE;
+    m_search_->reset              = TRUE;
+    m_search_->matchedIndex       = USEARCH_DONE;
+    m_search_->matchedLength      = 0;
+    m_search_->text               = m_text_.getBuffer();
+    m_search_->textLength         = text.length();
+}
+    
+    #include 'unicode/messagepattern.h'
+#include 'unicode/rbnf.h'
+#include 'unicode/selfmt.h'
+#include 'unicode/uchar.h'
+#include 'unicode/ucnv_err.h'
+#include 'unicode/umsg.h'
+#include 'unicode/ustring.h'
+#include 'unicode/utypes.h'
+#include 'cmemory.h'
+#include 'messageimpl.h'
+#include 'patternprops.h'
+#include 'selfmtimpl.h'
+#include 'uassert.h'
+#include 'ustrfmt.h'
+#include 'util.h'
+#include 'uvector.h'
+    
+    #endif /* #if !UCONFIG_NO_BREAK_ITERATION */
 
     
+    #include 'unicode/unistr.h'
     
-    {        httpResponseData->onWritable = std::move(handler);
-        return this;
-    }
+            int32_t nsNameHash = nsName.hashCode();
+        // See if the numbering system is in the override list, if not, then add it.
+        NSOverride *cur = overrideList;
+        const SharedNumberFormat *snf = NULL;
+        UBool found = FALSE;
+        while ( cur && !found ) {
+            if ( cur->hash == nsNameHash ) {
+                snf = cur->snf;
+                found = TRUE;
+            }
+            cur = cur->next;
+        }
     
-     * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+    #endif   // #if !UCONFIG_NO_FORMATTING
+#endif   // SMPDTFST_H
+
     
-    //    return 0;
+        /** Initializes a Catmull Rom config with a capacity hint.
+     *
+     * @js NA
+     * @param capacity The size of the array.
+     * @return True.
+     */
+    bool initWithCapacity(ssize_t capacity);
     
-        /* Routes by method and url until handler found and said handler consumes the request by returning true.
-     * If a handler returns false, we keep searching for another match. If we cannot find a handler that
-     * a) matches the url and method and b) consume the request, then we fail and return false.
-     * In that case, a second pass where method changed to '*' to denote 'any' could be used to
-     * give such routes a chance. If second pass fails, we have an unhandled request and you may
-     * do whatever you want with your connection, such as close it, or respond with a fix message */
-    bool route(std::string_view method, std::string_view url, USERDATA &userData) {
-        /* Reset url parsing cache */
-        setUrl(url);
-        routeParameters.reset();
-    }
+    //
+// NOTE: Converting these macros into Templates is desirable, but please see
+// issue #16159 [https://github.com/cocos2d/cocos2d-x/pull/16159] for further info
+//
+#define EASERATE_TEMPLATE_IMPL(CLASSNAME, TWEEN_FUNC) \
+CLASSNAME* CLASSNAME::create(cocos2d::ActionInterval *action, float rate) \
+{ \
+    CLASSNAME *ease = new (std::nothrow) CLASSNAME(); \
+    if (ease) \
+    { \
+        if (ease->initWithAction(action, rate)) \
+            ease->autorelease(); \
+        else \
+            CC_SAFE_RELEASE_NULL(ease); \
+    } \
+    return ease; \
+} \
+CLASSNAME* CLASSNAME::clone() const \
+{ \
+    if(_inner) return CLASSNAME::create(_inner->clone(), _rate); \
+    return nullptr; \
+} \
+void CLASSNAME::update(float time) { \
+    _inner->update(TWEEN_FUNC(time, _rate)); \
+} \
+EaseRateAction* CLASSNAME::reverse() const { \
+    return CLASSNAME::create(_inner->reverse(), 1.f / _rate); \
+}
     
-    class Plus : public Expression 
-{
-    public:
-        Plus(Expression* left, Expression* right) :leftOperand(left), rightOperand(right) { } 
-        ~Plus() { delete leftOperand; delete rightOperand; }
-        int interpret() { return leftOperand->interpret() + rightOperand->interpret(); }
-    private:
-        Expression* leftOperand;
-        Expression* rightOperand;
-};
     
-    /********************************************************************************** 
-* 
-* Given a binary tree, return the level order traversal of its nodes' values. 
-* (ie, from left to right, level by level).
-* 
-* For example:
-* Given binary tree {3,9,20,#,#,15,7},
-* 
-*     3
-*    / \
-*   9  20
-*     /  \
-*    15   7
-* 
-* return its level order traversal as:
-* 
-* [
-*   [3],
-*   [9,20],
-*   [15,7]
-* ]
-* 
-* confused what '{1,#,2,3}' means? > read more on how binary tree is serialized on OJ.
-* 
-* OJ's Binary Tree Serialization:
-* 
-* The serialization of a binary tree follows a level order traversal, where '#' signifies 
-* a path terminator where no node exists below.
-* 
-* Here's an example:
-* 
-*    1
-*   / \
-*  2   3
-*     /
-*    4
-*     \
-*      5
-* 
-* The above binary tree is serialized as '{1,2,3,#,#,4,#,#,5}'. 
-* 
-*               
-**********************************************************************************/
+    /** Returns the numbers of actions that are running in a
+     *  certain target with a specific tag.
+     * Like getNumberOfRunningActionsInTarget Composable actions
+     * are counted as 1 action. Example:
+     * - If you are running 1 Sequence of 7 actions, it will return 1.
+     * - If you are running 7 Sequences of 2 actions, it will return 7.
+     *
+     * @param target    A certain target.
+     * @param tag       Tag that will be searched.
+     * @return  The numbers of actions that are running in a certain target
+     *          with a specific tag.
+     * @see getNumberOfRunningActionsInTarget
+     * @js NA
+     */
+    virtual size_t getNumberOfRunningActionsInTargetByTag(const Node *target, int tag);
     
-        auto constructor = NAPA_GET_PERSISTENT_CONSTRUCTOR(_exportName, PlusNumberWrap);
-    auto context = isolate->GetCurrentContext();
-    auto instance = constructor->NewInstance(context, argc, argv).ToLocalChecked();
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __CC_ANIMATION_CACHE_H__
+#define __CC_ANIMATION_CACHE_H__
     
-    inline void OutputAssertMessage(const char* condition, const char* file, int line, const char* format, ...) {
-    char message[MAX_ASSERT_MESSAGE_SIZE];
-    }
+    #include '2d/CCAtlasNode.h'
+#include 'renderer/CCTextureAtlas.h'
+#include 'base/CCDirector.h'
+#include 'renderer/CCTextureCache.h'
+#include 'renderer/CCRenderer.h'
+#include 'renderer/CCGLProgram.h'
     
-    /// <summary> Free memory using napa allocator from napa_allocator_set, which is using C runtime ::free if not called. </summary>
-/// <param name='pointer'> Pointer to memory to be freed. </param>
-/// <param name='size_hint'> Hint of size to deallocate. </param>
-EXTERN_C NAPA_API void napa_deallocate(void* pointer, size_t size_hint);
-    
-    #include <napa/assert.h>
-#include <napa/providers/logging.h>
-    
-        /// <summary> It sets the persistent constructor at the current V8 isolate. </summary>
-    /// <param name='name'> Unique constructor name. It's recommended to use the same name as module. </param>
-    /// <param name='constructor'> V8 persistent function to constructor V8 object. </param>
-    NAPA_API void SetPersistentConstructor(const char* name, v8::Local<v8::Function> constructor);
-    
-            /// <summary> Represents verboseness for logging. </summary>
-        enum class Verboseness {
-            Error = 0,
-            Warning,
-            Info,
-            Debug
-        };
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
