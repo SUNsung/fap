34df7a1d31ba0afc2487ef15841699b656d2570a
SUNsung <weatherius@gmail.com>
2019-02-09 15:14:12 +0200

    Rewrite [583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py] File in: 2019-02-09 15:14:11

--- pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
+++ pyton/583iMTkDnj7Ng58YkjFmvCMVU58pkRuxJF.pr658BEy.py
@@ -1,159 +1,45 @@
 
         
         
-seller_category_map = {}
-seller_category_map['Exxon'] = DefaultCategories.GAS
-seller_category_map['Target'] = DefaultCategories.SHOPPING
+init_bashrc = u'''echo '
+export SHELL=/bin/bash
+export PS1='$ '
+echo > $HISTFILE
+eval $(thefuck --alias {})
+echo 'instant mode ready: $THEFUCK_INSTANT_MODE'
+' > ~/.bashrc'''
+    
+    Commands:
+   update - Retrieve new lists of packages
+   upgrade - Perform an upgrade
+   install - Install new packages (pkg is libc6 not libc6.deb)
+   remove - Remove packages
+   autoremove - Remove automatically all unused packages
+   purge - Remove packages and config files
+   source - Download source archives
+   build-dep - Configure build-dependencies for source packages
+   dist-upgrade - Distribution upgrade, see apt-get(8)
+   dselect-upgrade - Follow dselect selections
+   clean - Erase downloaded archive files
+   autoclean - Erase old downloaded archive files
+   check - Verify that there are no broken dependencies
+   changelog - Download and display the changelog for the given package
+   download - Download the binary package into the current directory
     
     
-if __name__ == '__main__':
-    HitCounts.run()
-
-    
-    from mrjob.job import MRJob
-    
-        def remove(self, key):
-        hash_index = self._hash_function(key)
-        for index, item in enumerate(self.table[hash_index]):
-            if item.key == key:
-                del self.table[hash_index][index]
-                return
-        raise KeyError('Key not found')
-
-    
-        By default, it represents the actual environment.
-    All of the attributes can be overwritten though, which
-    is used by the test suite to simulate various scenarios.
-    
-        def iter_lines(self, chunk_size):
-        return ((line, b'\n') for line in self._orig.iter_lines(chunk_size))
-    
-    
-def write_stream(stream, outfile, flush):
-    '''Write the output stream.'''
-    try:
-        # Writing bytes so we use the buffer interface (Python 3).
-        buf = outfile.buffer
-    except AttributeError:
-        buf = outfile
-    
-    
-FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
-FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
-JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
-BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
-    
-    
-def test_default_headers_case_insensitive(httpbin):
-    '''
-    <https://github.com/jakubroztocil/httpie/issues/644>
-    '''
-    r = http(
-        '--debug',
-        '--print=H',
-        httpbin.url + '/post',
-        'CONTENT-TYPE:application/json-patch+json',
-        'a=b',
-    )
-    assert 'CONTENT-TYPE: application/json-patch+json' in r
-    assert 'Content-Type' not in r
-    
-    
-@mock.patch('httpie.core.get_response')
-def test_error_traceback(get_response):
-    exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    with raises(ConnectionError):
-        main(['--ignore-stdin', '--traceback', 'www.google.com'])
-    
-    
-def test_unicode_headers_verbose(httpbin):
-    # httpbin doesn't interpret utf8 headers
-    r = http('--verbose', httpbin.url + '/headers', u'Test:%s' % UNICODE)
-    assert HTTP_OK in r
-    assert UNICODE in r
-    
-        def test_upload_multiple_fields_with_the_same_name(self, httpbin):
-        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
-                 'test-file@%s' % FILE_PATH_ARG,
-                 'test-file@%s' % FILE_PATH_ARG)
-        assert HTTP_OK in r
-        assert r.count('Content-Disposition: form-data; name='test-file';'
-                       ' filename='%s'' % os.path.basename(FILE_PATH)) == 2
-        # Should be 4, but is 3 because httpbin
-        # doesn't seem to support filed field lists
-        assert r.count(FILE_CONTENT) in [3, 4]
-        assert r.count('Content-Type: text/plain') == 2
-    
-        def process_response(self, request, response):
-        # No need to check for a redirect for non-404 responses.
-        if response.status_code != 404:
-            return response
-    
-        def create(self):
-        '''
-        To create a new key, set the modified flag so that the cookie is set
-        on the client for the current request.
-        '''
-        self.modified = True
-    
-        return TemplateResponse(request, template_name, {'sitemaps': sites},
-                            content_type=content_type)
-    
-    ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-    
-        module.run_command_environ_update = dict(LANG='C', LC_ALL='C', LC_MESSAGES='C', LC_CTYPE='C')
-    Icinga2FeatureHelper(module).manage()
-    
-        if state == 'reloaded':
-        if module.check_mode:
-            module.exit_json(changed=True)
-        rc, out, err = module.run_command('%s reload' % MONIT)
-        if rc != 0:
-            module.fail_json(msg='monit reload failed', stdout=out, stderr=err)
-        wait_for_monit_to_stop_pending()
-        module.exit_json(changed=True, name=name, state=state)
-    
-        def glob(self, pattern):
-        '''Iterate over this subtree and yield all existing files (of any
-        kind, including directories) matching the given pattern.
-        '''
-        if not pattern:
-            raise ValueError('Unacceptable pattern: {!r}'.format(pattern))
-        pattern = self._flavour.casefold(pattern)
-        drv, root, pattern_parts = self._flavour.parse_parts((pattern,))
-        if drv or root:
-            raise NotImplementedError('Non-relative patterns are unsupported')
-        selector = _make_selector(tuple(pattern_parts))
-        for p in selector.select_from(self):
-            yield p
-    
-        def check_warnings_filters(self, cmdline_option, envvar, use_pywarning=False):
-        if use_pywarning:
-            code = ('import sys; from test.support import import_fresh_module; '
-                    'warnings = import_fresh_module('warnings', blocked=['_warnings']); ')
-        else:
-            code = 'import sys, warnings; '
-        code += ('print(' '.join('%s::%s' % (f[0], f[2].__name__) '
-                                'for f in warnings.filters))')
-        args = (sys.executable, '-W', cmdline_option, '-bb', '-c', code)
-        env = dict(os.environ)
-        env.pop('PYTHONDEVMODE', None)
-        env['PYTHONWARNINGS'] = envvar
-        proc = subprocess.run(args,
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.STDOUT,
-                              universal_newlines=True,
-                              env=env)
-        self.assertEqual(proc.returncode, 0, proc)
-        return proc.stdout.rstrip()
-    
-    # Import the email modules we'll need
-from email import policy
-from email.parser import BytesParser
+@pytest.mark.parametrize('command', [
+    Command('apt-cache search foo', ''),
+    Command('aptitude search foo', ''),
+    Command('apt search foo', ''),
+    Command('apt-get install foo', ''),
+    Command('apt-get source foo', ''),
+    Command('apt-get clean', ''),
+    Command('apt-get remove', ''),
+    Command('apt-get update', ''),
+    Command('sudo apt update', no_match_output)
+])
+def test_not_match(command):
+    assert not match(command)
     
     def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
     html = get_html(url)
@@ -163,25 +49,93 @@ from email.parser import BytesParser
     assert id
     baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
     
-        def prepare(self, **kwargs):
-        self.api_data = json.loads(get_content(self.__class__.ep.format(self.vid)))
-        self.title = self.api_data['title']
-        for s in self.api_data['video']:
-            for st in self.__class__.stream_types:
-                if st['map_to'] == s:
-                    urls = self.api_data['video'][s]
-                    src = [u['url'] for u in urls]
-                    stream_data = dict(src=src, size=0, container='mp4', video_profile=st['video_profile'])
-                    self.streams[st['id']] = stream_data
+            for i in html_json['sources']:
+            if 'src' in i:  #to avoid KeyError
+                if i['src'].startswith('https'):
+                    link_list.append((str(i['height']), i['src']))
     
-    def facebook_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url)
+            if self.tree.find('result').text != '1':
+            log.wtf('API result says failed!')
+            raise 
     
-    __all__ = ['flickr_download_main']
+        theplatform_download_by_pid(pid, title, output_dir=output_dir, merge=merge, info_only=info_only)
     
-                post_url = r1(r''(https://plus.google.com/[^/]+/posts/[^']*)'', html)
-            post_author = r1(r'/\+([^/]+)/posts', post_url)
-            if post_author:
-                post_url = 'https://plus.google.com/+%s/posts/%s' % (parse.quote(post_author), r1(r'posts/(.+)', post_url))
-            post_html = get_html(post_url, faker=True)
-            title = r1(r'<title[^>]*>([^<\n]+)', post_html)
+    __all__ = ['ckplayer_download']
+    
+    site_info = 'CNTV.com'
+download = cntv_download
+download_playlist = playlist_not_supported('cntv')
+
+    
+                print_info(site_info, title, type, size)
+            if not info_only:
+                download_urls([real_url], title, ext, size, output_dir, merge = merge)
+    
+        video_url = match1(html, r'filepath=(.+)&sec')
+    video_url = video_url.replace('&mid', '?mid')
+    
+        title = r1(r'<meta property='og:title' content='(.*?)'>', html)
+    
+    
+def compute_bbox_regression_targets(entry):
+    '''Compute bounding-box regression targets for an image.'''
+    # Indices of ground-truth ROIs
+    rois = entry['boxes']
+    overlaps = entry['max_overlaps']
+    labels = entry['max_classes']
+    gt_inds = np.where((entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
+    # Targets has format (class, tx, ty, tw, th)
+    targets = np.zeros((rois.shape[0], 5), dtype=np.float32)
+    if len(gt_inds) == 0:
+        # Bail if the image has no ground-truth ROIs
+        return targets
+    
+    from detectron.core.config import cfg
+from detectron.datasets import json_dataset
+from detectron.datasets import roidb as roidb_utils
+import detectron.modeling.FPN as fpn
+import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
+import detectron.utils.blob as blob_utils
+    
+        num_keypoints = gt_keypoints.shape[2]
+    sampled_keypoints = -np.ones(
+        (len(sampled_fg_rois), gt_keypoints.shape[1], num_keypoints),
+        dtype=gt_keypoints.dtype
+    )
+    for ii in range(len(sampled_fg_rois)):
+        ind = box_to_gt_ind_map[ii]
+        if ind >= 0:
+            sampled_keypoints[ii, :, :] = gt_keypoints[gt_inds[ind], :, :]
+            assert np.sum(sampled_keypoints[ii, 2, :]) > 0
+    
+    
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    logger = setup_logging(__name__)
+    logger.setLevel(logging.DEBUG)
+    logging.getLogger('detectron.roi_data.loader').setLevel(logging.INFO)
+    np.random.seed(cfg.RNG_SEED)
+    args = parse_args()
+    logger.info('Called with args:')
+    logger.info(args)
+    if args.cfg_file is not None:
+        merge_cfg_from_file(args.cfg_file)
+    if args.opts is not None:
+        merge_cfg_from_list(args.opts)
+    assert_and_infer_cfg()
+    logger.info('Running with config:')
+    logger.info(pprint.pformat(cfg))
+    main(args)
+
+    
+    file_name = args[0]
+    
+    import jieba
+import jieba.analyse
+from optparse import OptionParser
+    
+    if len(sys.argv)>2:
+    n_topic = int(sys.argv[2])
+    
+    
+content = open(file_name,'rb').read()
