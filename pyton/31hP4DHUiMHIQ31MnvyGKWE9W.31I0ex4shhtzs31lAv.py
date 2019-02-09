71a018c9a348cbe626a27c365429f8b035350497
SUNsung <weatherius@gmail.com>
2019-02-09 23:27:09 +0200

    Rewrite [31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py] File in: 2019-02-09 23:27:09

--- pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
+++ pyton/31hP4DHUiMHIQ31MnvyGKWE9W.31I0ex4shhtzs31lAv.py
@@ -1,114 +1,214 @@
 
         
-            print('20 newsgroups')
-    print('=============')
-    print('X_train.shape = {0}'.format(X_train.shape))
-    print('X_train.format = {0}'.format(X_train.format))
-    print('X_train.dtype = {0}'.format(X_train.dtype))
-    print('X_train density = {0}'
-          ''.format(X_train.nnz / np.product(X_train.shape)))
-    print('y_train {0}'.format(y_train.shape))
-    print('X_test {0}'.format(X_test.shape))
-    print('X_test.format = {0}'.format(X_test.format))
-    print('X_test.dtype = {0}'.format(X_test.dtype))
-    print('y_test {0}'.format(y_test.shape))
-    print()
+        
+class Hand(object):
+    
+        def _find_available_spot(self, vehicle):
+        '''Find an available spot where vehicle can fit, or return None'''
+        pass
     
     
-def variable_batch_size_comparison(data):
-    batch_sizes = [i.astype(int) for i in np.linspace(data.shape[0] // 10,
-                                                      data.shape[0], num=10)]
-    
-        import matplotlib.pyplot as plt
-    fig = plt.figure('scikit-learn OMP vs. LARS benchmark results')
-    for i, (label, timings) in enumerate(sorted(six.iteritems(results))):
-        ax = fig.add_subplot(1, 2, i+1)
-        vmax = max(1 - timings.min(), -1 + timings.max())
-        plt.matshow(timings, fignum=False, vmin=1 - vmax, vmax=1 + vmax)
-        ax.set_xticklabels([''] + [str(each) for each in samples_range])
-        ax.set_yticklabels([''] + [str(each) for each in features_range])
-        plt.xlabel('n_samples')
-        plt.ylabel('n_features')
-        plt.title(label)
-    
-    n_samples = np.logspace(.5, 3, 9)
-n_features = np.logspace(1, 3.5, 7)
-N_samples, N_features = np.meshgrid(n_samples,
-                                    n_features)
-scikits_time = np.zeros(N_samples.shape)
-scipy_time = np.zeros(N_samples.shape)
+class SpendingByCategory(MRJob):
     
     
-def bench_scikit_tree_regressor(X, Y):
-    '''Benchmark with scikit-learn decision tree regressor'''
+class Transaction(object):
     
-    # A regex that matches standard linkcheck output lines
-line_re = re.compile(u'(.*)\:\d+\:\s\[(.*)\]\s(?:(.*)\sto\s(.*)|(.*))')
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
     
-        def __init__(self, *a, **kw):
-        super(QPSSpider, self).__init__(*a, **kw)
-        if self.qps is not None:
-            self.qps = float(self.qps)
-            self.download_delay = 1 / self.qps
-        elif self.download_delay is not None:
-            self.download_delay = float(self.download_delay)
+        for i in range(1, 6):
+        fpath = os.path.join(path, 'data_batch_' + str(i))
+        (x_train[(i - 1) * 10000: i * 10000, :, :, :],
+         y_train[(i - 1) * 10000: i * 10000]) = load_batch(fpath)
     
-    from scrapy.utils.spider import iter_spider_classes
-from scrapy.commands import ScrapyCommand
-from scrapy.exceptions import UsageError
-from scrapy.utils.conf import arglist_to_dict
-from scrapy.utils.python import without_none_values
     
-        def run(self, args, opts):
-        settings = self.crawler_process.settings
-        if opts.get:
-            s = settings.get(opts.get)
-            if isinstance(s, BaseSettings):
-                print(json.dumps(s.copy_to_dict()))
-            else:
-                print(s)
-        elif opts.getbool:
-            print(settings.getbool(opts.getbool))
-        elif opts.getint:
-            print(settings.getint(opts.getint))
-        elif opts.getfloat:
-            print(settings.getfloat(opts.getfloat))
-        elif opts.getlist:
-            print(settings.getlist(opts.getlist))
+def l2(l=0.01):
+    return L1L2(l2=l)
+    
+    
+def test_regularization_shared_layer_in_different_models():
+    shared_dense = Dense(num_classes,
+                         kernel_regularizer=regularizers.l1(),
+                         activity_regularizer=regularizers.l1())
+    models = []
+    for _ in range(2):
+        input_tensor = Input(shape=(data_dim,))
+        unshared_dense = Dense(num_classes, kernel_regularizer=regularizers.l1())
+        out = unshared_dense(shared_dense(input_tensor))
+        models.append(Model(input_tensor, out))
+    
+        if isinstance(gpus, (list, tuple)):
+        if len(gpus) <= 1:
+            raise ValueError('For multi-gpu usage to be effective, '
+                             'call `multi_gpu_model` with `len(gpus) >= 2`. '
+                             'Received: `gpus=%s`' % gpus)
+        num_gpus = len(gpus)
+        target_gpu_ids = gpus
+    else:
+        if gpus <= 1:
+            raise ValueError('For multi-gpu usage to be effective, '
+                             'call `multi_gpu_model` with `gpus >= 2`. '
+                             'Received: `gpus=%d`' % gpus)
+        num_gpus = gpus
+        target_gpu_ids = range(num_gpus)
+    
+    seq.add(Conv3D(filters=1, kernel_size=(3, 3, 3),
+               activation='sigmoid',
+               padding='same', data_format='channels_last'))
+seq.compile(loss='binary_crossentropy', optimizer='adadelta')
+    
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import keras
+from keras.layers import Activation, Dense, Input
+from keras.layers import Conv2D, Flatten
+from keras.layers import Reshape, Conv2DTranspose
+from keras.models import Model
+from keras import backend as K
+from keras.datasets import mnist
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+    
+        print('LocalOutlierFactor processing...')
+    model = LocalOutlierFactor(n_neighbors=20)
+    tstart = time()
+    model.fit(X)
+    fit_time = time() - tstart
+    scoring = -model.negative_outlier_factor_  # the lower, the more normal
+    fpr, tpr, thresholds = roc_curve(y, scoring)
+    AUC = auc(fpr, tpr)
+    plt.plot(fpr, tpr, lw=1,
+             label=('ROC for %s (area = %0.3f, train-time: %0.2fs)'
+                    % (dataset_name, AUC, fit_time)))
+    
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
 
     
-        default_settings = {'LOG_ENABLED': False,
-                        'SPIDER_LOADER_WARN_ONLY': True}
+        # the training data folder must be passed as first argument
+    movie_reviews_data_folder = sys.argv[1]
+    dataset = load_files(movie_reviews_data_folder, shuffle=False)
+    print('n_samples: %d' % len(dataset.data))
     
-            general form:
-        @returns request(s)/item(s) [min=1 [max]]
     
-        long_description = README,
+def _count_nonzero_coefficients(estimator):
+    a = estimator.coef_.toarray()
+    return np.count_nonzero(a)
     
-        @staticmethod
-    def get_streams_by_id(account_number, video_id):
-        '''
-        int, int->list
-        
-        Get the height of the videos.
-        
-        Since brightcove is using 3 kinds of links: rtmp, http and https,
-        we will be using the HTTPS one to make it secure.
-        
-        If somehow akamaihd.net is blocked by the Great Fucking Wall,
-        change the 'startswith https' to http.
-        '''
-        endpoint = 'https://edge.api.brightcove.com/playback/v1/accounts/{account_number}/videos/{video_id}'.format(account_number = account_number, video_id = video_id)
-        fake_header_id = fake_headers
-        #is this somehow related to the time? Magic....
-        fake_header_id['Accept'] ='application/json;pk=BCpkADawqM1cc6wmJQC2tvoXZt4mrB7bFfi6zGt9QnOzprPZcGLE9OMGJwspQwKfuFYuCjAAJ53JdjI8zGFx1ll4rxhYJ255AXH1BQ10rnm34weknpfG-sippyQ'
+    First example
+-------------
+The first example illustrates how robust covariance estimation can help
+concentrating on a relevant cluster when another one exists. Here, many
+observations are confounded into one and break down the empirical covariance
+estimation.
+Of course, some screening tools would have pointed out the presence of two
+clusters (Support Vector Machines, Gaussian Mixture Models, univariate
+outlier detection, ...). But had it been a high-dimensional example, none
+of these could be applied that easily.
     
-        else:
-        titles = re.findall(r''name':'([^']*)'', html)
-        real_urls = [re.sub('\\\\/', '/', i) for i in re.findall(r''rawUrl':'([^']*)'', html)]
+    plt.show()
+
     
-    __all__ = ['facebook_download']
+    plt.show()
+
     
-    from ..common import *
+    iris = datasets.load_iris()
+X = iris.data
+y = iris.target
     
-            docid = r1('/file/d/([^/]+)', url)
+    # equal bins face
+regular_values = np.linspace(0, 256, n_clusters + 1)
+regular_labels = np.searchsorted(regular_values, face) - 1
+regular_values = .5 * (regular_values[1:] + regular_values[:-1])  # mean
+regular_face = np.choose(regular_labels.ravel(), regular_values, mode='clip')
+regular_face.shape = face.shape
+plt.figure(3, figsize=(3, 2.2))
+plt.imshow(regular_face, cmap=plt.cm.gray, vmin=vmin, vmax=vmax)
+    
+        def report_not_exist(self, appid, ip):
+        self.logger.debug('report_not_exist:%s %s', appid, ip)
+        th = threading.Thread(target=self.process_appid_not_exist, args=(appid, ip))
+        th.start()
+    
+        def report_ok(self):
+        self.network_stat = 'OK'
+        self.last_check_time = time.time()
+        self.continue_fail_count = 0
+    
+    URLFETCH_MAX = 2
+URLFETCH_MAXSIZE = 4*1024*1024
+URLFETCH_DEFLATE_MAXSIZE = 4*1024*1024
+URLFETCH_TIMEOUT = 60
+    
+    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
+    host = r1(r'host=([^&]*)', html)
+    assert host
+    type = r1(r'videofiletype=([^&]*)', html)
+    assert type
+    vid = r1(r'&stream_name=([^&]*)', html)
+    assert vid
+    dir_str = r1(r'&dir=([^&]*)', html).strip()
+    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
+    _, ext, size = url_info(url)
+    print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge = merge)
+    
+            self.prepare(vid = vid, title = title, **kwargs)
+    
+        return video_dict
+    
+    
+'''
+http://open.iqiyi.com/lib/player.html
+'''
+iqiyi_patterns = [r'(?:\'|\')(https?://dispatcher\.video\.qiyi\.com\/disp\/shareplayer\.swf\?.+?)(?:\'|\')',
+                  r'(?:\'|\')(https?://open\.iqiyi\.com\/developer\/player_js\/coopPlayerIndex\.html\?.+?)(?:\'|\')']
+    
+        # Display the results
+    for top, right, bottom, left in face_locations:
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+    
+    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
+# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
+# positive matches at the risk of more false negatives.
+    
+    # Load the jpg file into a numpy array
+image = face_recognition.load_image_file('biden.jpg')
+    
+        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
+    context = multiprocessing
+    if 'forkserver' in multiprocessing.get_all_start_methods():
+        context = multiprocessing.get_context('forkserver')
+    
+    # Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
+    
+    import face_recognition
+import picamera
+import numpy as np
+    
+        for unknown_encoding in unknown_encodings:
+        distances = face_recognition.face_distance(known_face_encodings, unknown_encoding)
+        result = list(distances <= tolerance)
