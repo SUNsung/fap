e93e830f9e2bb1f5a5c560127e89ff6fb3202c35
SUNsung <weatherius@gmail.com>
2019-02-10 06:02:15 +0200

    Rewrite [47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp] File in: 2019-02-10 06:02:14

--- cccccc/47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp
+++ cccccc/47UeI5vCg1F7Y47HxVH94NCGg47Wfes4Rw6Dks47OoC.cpp
@@ -1,395 +1,195 @@
 
         
-        bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv) {
-  const base::CommandLine::StringType dashdash(2, '-');
-  bool block_args = false;
-  for (int i = 0; i < argc; ++i) {
-    if (argv[i] == dashdash)
-      break;
-    if (block_args) {
-      return false;
-    } else if (IsUrlArg(argv[i])) {
-      block_args = true;
-    }
-  }
-  return true;
-}
+        // Get basic type definitions.
+#define IPC_MESSAGE_IMPL
+#include 'content/nw/src/common/common_message_generator.h'
     
-    #include 'base/command_line.h'
-    
-    void Event::RenderFrameDeleted(content::RenderFrameHost* rfh) {
-  if (sender_ != rfh)
-    return;
-  sender_ = nullptr;
-  message_ = nullptr;
-}
-    
-    namespace IPC {
-class Message;
-}
-    
-     private:
-  void OnMessageBoxCallback(DialogClosedCallback callback,
-                            const std::string& origin,
-                            int code,
-                            bool checkbox_checked);
-    
-    namespace atom {
-    }
-    
-      // net::URLRequestJobFactory::ProtocolHandler:
-  net::URLRequestJob* MaybeCreateJob(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const override;
-  bool IsSafeRedirectTarget(const GURL& location) const override;
+      static void Call(content::Shell* shell,
+                   const std::string& method,
+                   const base::ListValue& arguments,
+                   base::ListValue* result,
+                   DispatcherHost* dispatcher_host);
     
     
-    {
-    {
-    {}  // namespace python
-}  // namespace protobuf
-}  // namespace google
+    {}  // namespace nw
 
     
-      // Increments the reference count fo the current object.
-  // Should not be called when no object is held.
-  void inc() const { Py_INCREF(ptr_); }
+      scoped_ptr<base::Value> value_args(
+      converter->FromV8Value(args, isolate->GetCurrentContext()));
+  if (!value_args.get() ||
+      !value_args->IsType(base::Value::TYPE_LIST))
+    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
+        'Unable to convert 'args' passed to CallObjectMethod')));
     
-    io::ZeroCopyOutputStream*
-GeneratorContext::OpenForAppend(const string& filename) {
-  return NULL;
-}
+    // Get string from resource_id.
+base::StringPiece GetStringResource(int resource_id);
     
-    #include <sstream>
-    
-    #include <google/protobuf/testing/googletest.h>
-#include <gtest/gtest.h>
-#include <google/protobuf/testing/file.h>
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-    }
-    }
-    }
+    namespace nw {
     }
     
-    int ImmutableExtensionLiteGenerator::GenerateRegistrationCode(
-    io::Printer* printer) {
-  printer->Print(
-    'registry.add($scope$.$name$);\n',
-    'scope', scope_,
-    'name', UnderscoresToCamelCase(descriptor_));
-  return 7;
-}
-    
-    // Check whether there is any type defined in the proto file that has
-// the given class name.
-bool ClassNameResolver::HasConflictingClassName(
-    const FileDescriptor* file, const string& classname) {
-  for (int i = 0; i < file->enum_type_count(); i++) {
-    if (file->enum_type(i)->name() == classname) {
-      return true;
-    }
-  }
-  for (int i = 0; i < file->service_count(); i++) {
-    if (file->service(i)->name() == classname) {
-      return true;
-    }
-  }
-  for (int i = 0; i < file->message_type_count(); i++) {
-    if (MessageHasConflictingClassName(file->message_type(i), classname)) {
-      return true;
-    }
-  }
-  return false;
-}
-    
-        NDMask::NDMask(const NDShape& shape, Matrix<char>* matrix)
-        : m_device(AsDeviceDescriptor(matrix->GetDeviceId())), m_maskShape(shape)
-    {
-        m_matrixView = std::shared_ptr<Matrix<char>>(matrix, [](Matrix<char>* ptr) { delete ptr; });
-    }
+    #include 'content/nw/src/api/menuitem/menuitem.h'
     
     
-    {                    index = i;
-                    found = true;
-                }
-                else if (*currentp != (ElementType)0)
-                    RuntimeError('CopyDenseToOneHot: Cannot convert to onehot vector; contains value other than 0/1.');
-    
-    
-    {            // get the name for the reader we want to use, default to CNTKTextFormatReader
-            GetReaderProc getReaderProc = (GetReaderProc) Plugin::Load(readerType, GetReaderName(precision));
-            m_ioNames.push_back(ioName);
-            assert(getReaderProc != nullptr);
-            getReaderProc(&m_dataReaders[ioName]); // instantiates the reader with the default constructor (no config processed at this point)
-        }
-    }
-    else if (hasDeserializers)
-    {
-        wstring readerType = config(L'readerType', L'Cntk.Composite');
-    
-            int firstLbl = -1;
-        for (int ik = 0; ik < lbls.GetNumRows(); ik++)
-            if (lbls(ik, 0) != 0)
-            {
-                firstLbl = ik;
-                break;
-            }
-    
-    namespace Microsoft { namespace MSR { namespace CNTK {
-    }
-    }
-    }
-    
-    
-    {};
-    
-      xfer += oprot->writeFieldBegin('message', ::apache::thrift::protocol::T_STRING, 2);
-  xfer += oprot->writeString(this->message);
-  xfer += oprot->writeFieldEnd();
-    
-    // getSerializedRowColumnNames returns a vector of test column names that
-// are in alphabetical order. If unordered_and_repeated is true, the
-// vector includes a repeated column name and is in non-alphabetical order
-ColumnNames getSerializedRowColumnNames(bool unordered_and_repeated);
-    
-    const std::string kLocalDumpsRegKey =
-    'HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\Windows Error '
-    'Reporting\\LocalDumps\\DumpFolder';
-const std::string kSymbolPath =
-    'C:\\ProgramData\\dbg\\sym;'
-    'cache*C:\\ProgramData\\dbg\\sym;'
-    'srv*C:\\ProgramData\\dbg\\sym*https://msdl.microsoft.com/download/symbols';
-const unsigned long kNumStackFramesToLog = 10;
-const std::map<unsigned long long, std::string> kMinidumpTypeFlags = {
-    {0x00000000, 'MiniDumpNormal'},
-    {0x00000001, 'MiniDumpWithDataSegs'},
-    {0x00000002, 'MiniDumpWithFullMemory'},
-    {0x00000004, 'MiniDumpWithHandleData'},
-    {0x00000008, 'MiniDumpFilterMemory'},
-    {0x00000010, 'MiniDumpScanMemory'},
-    {0x00000020, 'MiniDumpWithUnloadedModules'},
-    {0x00000040, 'MiniDumpWithIndirectlyReferencedMemory'},
-    {0x00000080, 'MiniDumpFilterModulePaths'},
-    {0x00000100, 'MiniDumpWithProcessThreadData'},
-    {0x00000200, 'MiniDumpWithPrivateReadWriteMemory'},
-    {0x00000400, 'MiniDumpWithoutOptionalData'},
-    {0x00000800, 'MiniDumpWithFullMemoryInfo'},
-    {0x00001000, 'MiniDumpWithThreadInfo'},
-    {0x00002000, 'MiniDumpWithCodeSegs'},
-    {0x00004000, 'MiniDumpWithoutAuxiliaryState'},
-    {0x00008000, 'MiniDumpWithFullAuxiliaryState'},
-    {0x00010000, 'MiniDumpWithPrivateWriteCopyMemory'},
-    {0x00020000, 'MiniDumpIgnoreInaccessibleMemory'},
-    {0x00040000, 'MiniDumpWithTokenInformation'},
-    {0x00080000, 'MiniDumpWithModuleHeaders'},
-    {0x00100000, 'MiniDumpFilterTriage'},
-    {0x001fffff, 'MiniDumpValidTypeFlags'}};
-    
-    
-    {} // namespace
-    
-    TEST_F(ScopeGuardTests, example_time_measurement) {
-  auto duration = std::chrono::duration<double>{0};
-  {
-    auto guard = scope_guard::create(
-        [&duration, start = std::chrono::steady_clock::now()]() {
-          duration = std::chrono::steady_clock::now() - start;
-        });
-    std::this_thread::sleep_for(std::chrono::milliseconds{1});
-  }
-  EXPECT_GE(duration, std::chrono::milliseconds{1});
-}
-    
-          resp.content['disk_id'] = std::to_string(i);
-      // Change device type to driver_typerrr
-      resp.content['driver_type'] =
-          type->driver.substr(0, type->driver.length() - 1);
-    
-    #pragma once
-    
-    using namespace rocksdb;
-    
-    // Move all L0 files to target_level skipping compaction.
-// This operation succeeds only if the files in L0 have disjoint ranges; this
-// is guaranteed to happen, for instance, if keys are inserted in sorted
-// order. Furthermore, all levels between 1 and target_level must be empty.
-// If any of the above condition is violated, InvalidArgument will be
-// returned.
-Status PromoteL0(DB* db, ColumnFamilyHandle* column_family,
-                 int target_level = 1);
-    
-    namespace rocksdb {
-    }
-    
-    
-    { private:
-  DB* db_;
-  const Snapshot* snapshot_;
+    {  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.readAvailableTypes', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwClipboardReadAvailableTypesFunction);
 };
     
-      // Builds an openable snapshot of RocksDB on the same disk, which
-  // accepts an output directory on the same disk, and under the directory
-  // (1) hard-linked SST files pointing to existing live SST files
-  // SST files will be copied if output directory is on a different filesystem
-  // (2) a copied manifest files and other files
-  // The directory should not already exist and will be created by this API.
-  // The directory will be an absolute path
-  // log_size_for_flush: if the total log file size is equal or larger than
-  // this value, then a flush is triggered for all the column families. The
-  // default value is 0, which means flush is always triggered. If you move
-  // away from the default, the checkpoint may not contain up-to-date data
-  // if WAL writing is not always enabled.
-  // Flush will always trigger if it is 2PC.
-  virtual Status CreateCheckpoint(const std::string& checkpoint_dir,
-                                  uint64_t log_size_for_flush = 0);
+      char headerCompat[8];
+  memset(headerCompat, 0, sizeof(headerCompat));
+  // magic
+  headerCompat[0] = 0xa1u;
+  headerCompat[1] = 0xa2u;
+  // format ID
+  headerCompat[2] = 0x02u;
+  // version
+  headerCompat[6] = 0;
+  headerCompat[7] = 0x02u;
     
-    void Follow::stop()
+    
+    {  void serialize(const std::string& filename);
+};
+    
+    namespace aria2 {
+    }
+    
+      void update();
+    
+      virtual std::shared_ptr<DHTTask>
+  createPeerAnnounceTask(const unsigned char* infoHash) = 0;
+    
+    namespace aria2 {
+    }
+    
+    namespace aria2 {
+    }
+    
+    void DHTUnknownMessage::doReceivedAction() {}
+    
+    void DNSCache::remove(const std::string& hostname, uint16_t port)
 {
-    _target = nullptr;
-    Action::stop();
+  auto target = std::make_shared<CacheEntry>(hostname, port);
+  entries_.erase(target);
 }
     
-    ActionInterval* ActionEase::getInnerAction()
-{
-    return _inner;
-}
+      // Input statistics
+  // TODO(noetzli): The stats are incomplete. They are lacking everything
+  // consumed by MergeHelper.
+  uint64_t num_input_records = 0;
+  uint64_t num_input_deletion_records = 0;
+  uint64_t num_input_corrupt_records = 0;
+  uint64_t total_input_raw_key_bytes = 0;
+  uint64_t total_input_raw_value_bytes = 0;
     
-    ShatteredTiles3D* ShatteredTiles3D::create(float duration, const Size& gridSize, int range, bool shatterZ)
-{
-    ShatteredTiles3D *action = new (std::nothrow) ShatteredTiles3D();
-    }
+     public:
+  PosixSequentialFile(const std::string& fname, FILE* file, int fd,
+                      const EnvOptions& options);
+  virtual ~PosixSequentialFile();
     
-     @since v0.99.2
- */
-class CC_DLL ActionTween : public ActionInterval
-{
-public:
-    /** 
-     * @brief Create and initializes the action with the property name (key), and the from and to parameters.
-     * @param duration The duration of the ActionTween. It's a value in seconds.
-     * @param key The key of property which should be updated.
-     * @param from The value of the specified property when the action begin.
-     * @param to The value of the specified property when the action end.
-     * @return If the creation success, return a pointer of ActionTween; otherwise, return nil.
-     */
-    static ActionTween* create(float duration, const std::string& key, float from, float to);
-    }
-    
-    AnimationFrame* AnimationFrame::create(SpriteFrame* spriteFrame, float delayUnits, const ValueMap& userInfo)
-{
-    auto ret = new (std::nothrow) AnimationFrame();
-    if (ret && ret->initWithSpriteFrame(spriteFrame, delayUnits, userInfo))
-    {
-        ret->autorelease();
-    }
-    else
-    {
-        CC_SAFE_DELETE(ret);
-    }
-    return ret;
-}
-    
-        /** Returns a Animation that was previously added.
-     * If the name is not found it will return nil.
-     * You should retain the returned copy if you are going to use it.
-     *
-     * @return A Animation that was previously added. If the name is not found it will return nil.
-     */
-    Animation* getAnimation(const std::string& name);
-    /**
-     * @deprecated. Use getAnimation() instead
-     * @js NA
-     * @lua NA
-     */
-    CC_DEPRECATED_ATTRIBUTE Animation* animationByName(const std::string& name){ return getAnimation(name); }
-    
-    void AtlasNode::calculateMaxItems()
-{
-    Size s = _textureAtlas->getTexture()->getContentSize();
-    
-    if (_ignoreContentScaleFactor)
-    {
-        s = _textureAtlas->getTexture()->getContentSizeInPixels();
-    }
-    
-    _itemsPerColumn = (int)(s.height / _itemHeight);
-    _itemsPerRow = (int)(s.width / _itemWidth);
-}
-    
-    /**
- * PolygonInfo is an object holding the required data to display Sprites.
- * It can be a simple as a triangle, or as complex as a whole 3D mesh
- */
-class CC_DLL PolygonInfo
-{
-public:
-    /// @name Creators
-    /// @{
-    /**
-     * Creates an empty Polygon info
-     * @memberof PolygonInfo
-     * @return PolygonInfo object
-     */
-    PolygonInfo();
-    }
-    }
-    
-    #include 'modules/drivers/canbus/can_client/socket/socket_can_client_raw.h'
-    
-    #include 'modules/drivers/radar/conti_radar/protocol/cluster_general_info_701.h'
-    
-      x <<= 8;
-  x |= t;
-    
-    #include 'modules/planning/lattice/trajectory_generation/trajectory_combiner.h'
-    
-    void Spline1dSeg::SetParams(const std::vector<double>& params) {
-  SetSplineFunc(PolynomialXd(params));
-}
-    
-    void SplineSegKernel::CalculateSecondOrderDerivative(
-    const uint32_t num_params) {
-  kernel_second_order_derivative_ =
-      Eigen::MatrixXd::Zero(num_params, num_params);
-  for (int r = 2; r < kernel_second_order_derivative_.rows(); ++r) {
-    for (int c = 2; c < kernel_second_order_derivative_.cols(); ++c) {
-      kernel_second_order_derivative_(r, c) =
-          (r * r - r) * (c * c - c) / (r + c - 3.0);
+      static void CompactFiles(void* arg) {
+    std::unique_ptr<CompactionTask> task(
+        reinterpret_cast<CompactionTask*>(arg));
+    assert(task);
+    assert(task->db);
+    Status s = task->db->CompactFiles(
+        task->compact_options,
+        task->input_file_names,
+        task->output_level);
+    printf('CompactFiles() finished with status %s\n', s.ToString().c_str());
+    if (!s.ok() && !s.IsIOError() && task->retry_on_fail) {
+      // If a compaction task with its retry_on_fail=true failed,
+      // try to schedule another compaction in case the reason
+      // is not an IO error.
+      CompactionTask* new_task = task->compactor->PickCompaction(
+          task->db, task->column_family_name);
+      task->compactor->ScheduleCompaction(new_task);
     }
   }
-}
     
-    #include 'modules/canbus/vehicle/gem/protocol/accel_cmd_67.h'
-#include 'modules/canbus/vehicle/gem/protocol/accel_rpt_68.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_cmd_6b.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_1_70.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_2_71.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_motor_rpt_3_72.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_rpt_6c.h'
-#include 'modules/canbus/vehicle/gem/protocol/date_time_rpt_83.h'
-#include 'modules/canbus/vehicle/gem/protocol/global_cmd_69.h'
-#include 'modules/canbus/vehicle/gem/protocol/global_rpt_6a.h'
-#include 'modules/canbus/vehicle/gem/protocol/headlight_cmd_76.h'
-#include 'modules/canbus/vehicle/gem/protocol/headlight_rpt_77.h'
-#include 'modules/canbus/vehicle/gem/protocol/horn_cmd_78.h'
-#include 'modules/canbus/vehicle/gem/protocol/horn_rpt_79.h'
-#include 'modules/canbus/vehicle/gem/protocol/lat_lon_heading_rpt_82.h'
-#include 'modules/canbus/vehicle/gem/protocol/parking_brake_status_rpt_80.h'
-#include 'modules/canbus/vehicle/gem/protocol/shift_cmd_65.h'
-#include 'modules/canbus/vehicle/gem/protocol/shift_rpt_66.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_cmd_6d.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_1_73.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_2_74.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_motor_rpt_3_75.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_rpt_1_6e.h'
-#include 'modules/canbus/vehicle/gem/protocol/turn_cmd_63.h'
-#include 'modules/canbus/vehicle/gem/protocol/turn_rpt_64.h'
-#include 'modules/canbus/vehicle/gem/protocol/vehicle_speed_rpt_6f.h'
-#include 'modules/canbus/vehicle/gem/protocol/wheel_speed_rpt_7a.h'
-#include 'modules/canbus/vehicle/gem/protocol/wiper_cmd_90.h'
-#include 'modules/canbus/vehicle/gem/protocol/wiper_rpt_91.h'
-#include 'modules/canbus/vehicle/gem/protocol/yaw_rate_rpt_81.h'
+    class MyMerge : public rocksdb::MergeOperator {
+ public:
+  virtual bool FullMergeV2(const MergeOperationInput& merge_in,
+                           MergeOperationOutput* merge_out) const override {
+    merge_out->new_value.clear();
+    if (merge_in.existing_value != nullptr) {
+      merge_out->new_value.assign(merge_in.existing_value->data(),
+                                  merge_in.existing_value->size());
+    }
+    for (const rocksdb::Slice& m : merge_in.operand_list) {
+      fprintf(stderr, 'Merge(%s)\n', m.ToString().c_str());
+      // the compaction filter filters out bad values
+      assert(m.ToString() != 'bad');
+      merge_out->new_value.assign(m.data(), m.size());
+    }
+    return true;
+  }
+    }
+    
+      // Write a key OUTSIDE of this transaction.
+  // Does not affect txn since this is an unrelated key.  If we wrote key 'abc'
+  // here, the transaction would fail to commit.
+  s = db->Put(write_options, 'xyz', 'zzz');
+    
+    struct UndumpOptions {
+  // Database that we will load the dumped file into
+  std::string db_path;
+  // File location of the dumped file that will be loaded
+  std::string dump_location;
+  // Compact the db after loading the dumped file
+  bool compact_db = false;
+};
+    
+      // Set the number of background threads that will be executing the
+  // scheduled jobs.
+  virtual void SetBackgroundThreads(int num) = 0;
+  virtual int GetBackgroundThreads() = 0;
+    
+    
+    {}  // namespace rocksdb
+#endif  // !ROCKSDB_LITE
+
+    
+        vector<int> nums1 = {1, 1, 2};
+    cout << Solution().removeDuplicates(nums1) << endl;
+    
+    
+int main() {
+    }
+    
+            vector<int> res;
+        if( root == NULL )
+            return res;
+    
+    
+    {
+    {
+    {            if(cur != NULL){
+                stack.push(cur);
+                cur = cur->left;
+            }
+            else {
+                cur = stack.top();
+                stack.pop();
+                res.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+        return res;
+    }
+};
+    
+            while(!q.empty()){
+    }
+    
+    
+    {        vector<int> res;
+        preorderTraversal(root, res);
+        return res;
+    }
+    
+    private:
+    struct TagNode{
+        TreeNode* node;
+        bool isFirst;
+        TagNode(TreeNode* node): node(node), isFirst(false){}
+    };
