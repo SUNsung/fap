fae31b0ca69936ad83ab89b88c3f6ad101d0d13a
SUNsung <weatherius@gmail.com>
2019-02-10 23:26:36 +0200

    Rewrite [05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb] File in: 2019-02-10 23:26:36

--- ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
+++ ruby/05n5TbCmIUkjM05ogfomQYSrE05JPzEHuH0yg05aG1.rb
@@ -1,169 +1,100 @@
 
         
-        require 'rubygems'
-require 'rake'
-require 'rdoc'
-require 'date'
-require 'yaml'
+        describe ContactsController, :type => :controller do
+  describe '#index' do
+    before do
+      AppConfig.chat.enabled = true
+      @aspect = bob.aspects.create(:name => 'another aspect')
+      bob.share_with alice.person, @aspect
+      bob.share_with eve.person, @aspect
+      sign_in bob, scope: :user
+    end
     
-    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
-COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
+      end
     
-    # No trailing slash
-Benchmark.ips do |x|
-  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
-  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
-  x.compare!
-end
-
+    (deny default)
+EOS
     
-          #
+        pod 'ObjCPod', path: 'ObjCPod'
+    pod 'SwiftPod', path: 'SwiftPod'
+    pod 'MixedPod', path: 'MixedPod'
+    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
     
-        def defaults_deprecate_type(old, current)
-      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
-      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
-                        'type: #{current}'.'
+          def run
+        UI.puts report
+      end
+    
+    require_relative '../lib/bootstrap/environment'
+    
+    describe LogStash::Config::PipelineConfig do
+  let(:source) { LogStash::Config::Source::Local }
+  let(:pipeline_id) { :main }
+  let(:ordered_config_parts) do
+    [
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/1', 0, 0, 'input { generator1 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/2', 0, 0,  'input { generator2 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/3', 0, 0, 'input { generator3 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/4', 0, 0, 'input { generator4 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/5', 0, 0, 'input { generator5 }'),
+      org.logstash.common.SourceWithMetadata.new('file', '/tmp/6', 0, 0, 'input { generator6 }'),
+      org.logstash.common.SourceWithMetadata.new('string', 'config_string', 0, 0, 'input { generator1 }'),
+    ]
+  end
+    
+              it 'successfully install the plugin when verification is disabled' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest')
+          end
+    
+          it 'display a list of installed plugins' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --installed')
+        expect(result.stdout.split('\n').size).to be > 1
+      end
+    
+        it_behaves_like 'returns 404 HTTP status'
+    
+          expect(json_response['data'][0]).to have_type('credit_card')
+      expect(json_response['data'][0]).to have_relationships(:payment_method)
+      expect(json_response['data'][0]).to have_attribute(:last_digits)
+      expect(json_response['data'][0]).to have_attribute(:month)
+      expect(json_response['data'][0]).to have_attribute(:year)
+      expect(json_response['data'][0]).to have_attribute(:name)
     end
   end
-end
-
     
-        def sort_fn(a, b)
-      if (a.getbyte(0) >= 49 && a.getbyte(0) <= 57) || (b.getbyte(0) >= 49 && b.getbyte(0) <= 57)
-        a_split = a.split(SPLIT_INTS)
-        b_split = b.split(SPLIT_INTS)
-    
-        private
-    
-            css('.c10', '.showcase', '.showcase-content', '.l-main-section', 'div.div', 'div[flex]', 'code-tabs', 'md-card', 'md-card-content', 'div:not([class])', 'footer', '.card-row', '.card-row-container', 'figure', 'blockquote', 'exported', 'defined', 'div.ng-scope', '.code-example header', 'section.desc', '.row', '.dart-api-entry-main', '.main-content', 'section.summary', 'span.signature').each do |node|
-          node.before(node.children).remove
+            def load_order(lock = false)
+          @order = Spree::Order.lock(lock).find_by!(number: params[:id])
+          raise_insufficient_quantity and return if @order.insufficient_stock_lines.present?
+          @order.state = params[:state] if params[:state]
+          state_callback(:before)
         end
     
-      ruby_version_is '2.6' do
-    it 'raises RuntimeError when `exception: true` is given and the command exits with a non-zero exit status' do
-      lambda { @object.system(ruby_cmd('exit 1'), exception: true) }.should raise_error(RuntimeError)
-    end
+    module Sidekiq
+  module Generators # :nodoc:
+    class WorkerGenerator < ::Rails::Generators::NamedBase # :nodoc:
+      desc 'This generator creates a Sidekiq Worker in app/workers and a corresponding test'
     
-        lambda {
-      catch :blah do
-        throw :blah, :return_value, 2, 3, 4, 5
+        ##
+    # Define client-side middleware:
+    #
+    #   client = Sidekiq::Client.new
+    #   client.middleware do |chain|
+    #     chain.use MyClientMiddleware
+    #   end
+    #   client.push('class' => 'SomeWorker', 'args' => [1,2,3])
+    #
+    # All client instances default to the globally-defined
+    # Sidekiq.client_middleware but you can change as necessary.
+    #
+    def middleware(&block)
+      @chain ||= Sidekiq.client_middleware
+      if block_given?
+        @chain = @chain.dup
+        yield @chain
       end
-    }.should raise_error(ArgumentError)
-  end
-    
-      it 'raises ArgumentError if no block or proc is provided' do
-    lambda do
-      trace_var :$Kernel_trace_var_global
-    end.should raise_error(ArgumentError)
-  end
-end
-
-    
-          it 'prepends a message with specified line from the backtrace' do
-        w = KernelSpecs::WarnInNestedCall.new
-    
-        case itype
-    when IAX_TYPE_DTMF_BEGIN
-      self.dprint('DTMF BEG: #{pkt[11,1]}')
-      self.dtmf << pkt[11,1]
-    
-            # Receives a kerberos response through the connection
-        #
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] the kerberos
-        #   response message
-        # @raise [RuntimeError] if the connection isn't established, the transport protocol is unknown, not supported
-        #   or the response can't be parsed
-        # @raise [NotImplementedError] if the transport protocol isn't supported
-        def recv_response
-          if connection.nil?
-            raise ::RuntimeError, 'Kerberos Client: connection not established'
-          end
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
-    
-                data_encrypt = Rex::Text::rand_text(8) + data
-    
-              # Encodes the type
-          #
-          # @return [OpenSSL::ASN1::Integer]
-          def encode_type(type)
-            bn = OpenSSL::BN.new(type.to_s)
-            int = OpenSSL::ASN1::Integer.new(bn)
-    
-              def self.decode(input)
-            elem = self.new
-            elem.decode(input)
-          end
-    
-              # Decodes the Rex::Proto::Kerberos::Model::EncKdcResponse from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode EncKdcResponse, invalid input'
-            end
-    
-              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_asn1_pvno(input)
-            input.value[0].value.to_i
-          end
-    
-        private
-    
-    # enable logging
-FPM::Util.send :module_function, :logger
-FPM::Util.logger.level = :info
-FPM::Util.logger.subscribe STDERR
-    
-      # This class is raised when you try to output a package to a path
-  # whose containing directory does not exist.
-  class ParentDirectoryMissing < StandardError
-    def to_s
-      return 'Parent directory does not exist: #{File.dirname(super)} - cannot write to #{super}'
-    end # def to_s
-  end # class ParentDirectoryMissing
-    
-        copy_metadata(source, destination)
-  end # def copy
-    
-        if gem_files.length != 1
-      raise 'Unexpected number of gem files in #{download_dir},  #{gem_files.length} should be 1'
+      @chain
     end
     
-      # Extract name and version from PackageInfo XML
-  def extract_info(package)
-    build_path('expand').tap do |path|
-      doc = REXML::Document.new File.open(File.join(path, 'PackageInfo'))
-      pkginfo_elem = doc.elements['pkg-info']
-      identifier = pkginfo_elem.attribute('identifier').value
-      self.version = pkginfo_elem.attribute('version').value
-      # set name to the last dot element of the identifier
-      self.name = identifier.split('.').last
-      logger.info('inferring name #{self.name} from pkg-id #{identifier}')
-    end
-  end # def extract_info
-    
-        if attributes[:pear_data_dir]
-      logger.info('Setting data_dir', :data_dir => attributes[:pear_data_dir])
-      safesystem('pear', '-c', config, 'config-set', 'data_dir', '#{staging_path(installroot)}/#{attributes[:pear_data_dir]}')
-    end
-    
-    end
+          s = sessions
+      return unless s
