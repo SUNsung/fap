67cff760b7915d3c48e33546dce573eff3c79257
SUNsung <weatherius@gmail.com>
2019-02-10 11:36:41 +0200

    Create [40gmHGKyKNt0Q406fma5grX4uE40nCz1SWSuwY40NtJ.cpp] File in: 2019-02-10 11:36:40

---
+++ cccccc/40gmHGKyKNt0Q406fma5grX4uE40nCz1SWSuwY40NtJ.cpp
@@ -0,0 +1,26 @@
+  // atomic write
+  WriteBatch batch;
+  batch.Put(handles[0], Slice('key2'), Slice('value2'));
+  batch.Put(handles[1], Slice('key3'), Slice('value3'));
+  batch.Delete(handles[0], Slice('key'));
+  s = db->Write(WriteOptions(), &batch);
+  assert(s.ok());
+    
+      // Always pick a compaction which includes all files whenever possible.
+  CompactionTask* PickCompaction(
+      DB* db, const std::string& cf_name) override {
+    ColumnFamilyMetaData cf_meta;
+    db->GetColumnFamilyMetaData(&cf_meta);
+    }
+    
+    class FlushBlockBySizePolicyFactory : public FlushBlockPolicyFactory {
+ public:
+  FlushBlockBySizePolicyFactory() {}
+    }
+    
+    
+    {}  // namespace rocksdb
+
+    
+    namespace rocksdb {
+    }
