28b93a30ba72fae5039c115c77cffa74cd6ed516
SUNsung <weatherius@gmail.com>
2019-02-12 01:02:18 +0200

    Rewrite [02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py] File in: 2019-02-12 01:02:17

--- pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
+++ pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
@@ -1,173 +1,175 @@
 
         
         
-def with_confirmation(proc, TIMEOUT):
-    '''Ensures that command can be fixed when confirmation enabled.'''
-    _set_confirmation(proc, True)
+def load_word_embeddings(word_embeddings_dir, word_embeddings_file):
+  '''Loads pretrained word embeddings from a binary file and returns the matrix.
+    
+      T, N = vals_txn.shape
+  if n_to_plot > N:
+    n_to_plot = N
     
     
-@pytest.mark.functional
-def test_refuse_with_confirmation(proc, TIMEOUT):
-    refuse_with_confirmation(proc, TIMEOUT)
+def read_data(data_fname):
+  ''' Read saved data in HDF5 format.
+    
+    import numpy as np
+import tensorflow as tf
+    
+        self._id_to_word = []
+    self._word_to_id = {}
+    self._unk = -1
+    self._bos = -1
+    self._eos = -1
+    
+          if len(example) > num_steps:
+        final_x = example[:num_steps]
+        final_y = example[1:(num_steps + 1)]
+        w[i] = 1
+    
+      samples = []
+  for sequence_id in xrange(min(len(arr), max_num_to_print)):
+    sample = []
+    for i, index in enumerate(arr[sequence_id, :]):
+      if p[sequence_id, i] == 1:
+        sample.append(str(id_to_word[index]))
+      else:
+        sample.append('*' + str(id_to_word[index]))
+    buffer_str = ' '.join(sample)
+    samples.append(buffer_str)
+  return samples
+    
+        # rnn_vd derived from the same code base as rnn_zaremba.
+    elif (FLAGS.discriminator_model == 'rnn_zaremba' or
+          FLAGS.discriminator_model == 'rnn_vd'):
+      dis_variable_maps = variable_mapping.rnn_zaremba(hparams, model='dis')
+      dis_init_saver = tf.train.Saver(var_list=dis_variable_maps)
+      init_savers['dis_init_saver'] = dis_init_saver
     
     
-@pytest.mark.parametrize('command', [
-    Command('apt list --upgradable', no_match_output),
-    Command('sudo apt list --upgradable', no_match_output)
-])
-def test_not_match(command):
-    assert not match(command)
+DOCUMENTATION = '''
+---
+module: elasticache_snapshot
+short_description: Manage cache snapshots in Amazon Elasticache.
+description:
+  - Manage cache snapshots in Amazon Elasticache.
+  - Returns information about the specified snapshot.
+version_added: '2.3'
+author: 'Sloane Hertel (@s-hertel)'
+extends_documentation_fragment:
+  - aws
+  - ec2
+requirements: [ boto3, botocore ]
+options:
+  name:
+    description:
+      - The name of the snapshot we want to create, copy, delete
+    required: yes
+  state:
+    description:
+      - Actions that will create, destroy, or copy a snapshot.
+    choices: ['present', 'absent', 'copy']
+  replication_id:
+    description:
+      - The name of the existing replication group to make the snapshot.
+  cluster_id:
+    description:
+      - The name of an existing cache cluster in the replication group to make the snapshot.
+  target:
+    description:
+      - The name of a snapshot copy
+  bucket:
+    description:
+      - The s3 bucket to which the snapshot is exported
+'''
     
-    
-def _is_not_okay_to_test():
-    return 'elasticsearch' not in _get_formulas()
-    
-        # Input shape
-        2D tensor of shape (samples, n)
-    
-        # Returns
-        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
-    '''
-    dirname = 'cifar-10-batches-py'
-    origin = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-    path = get_file(dirname, origin=origin, untar=True)
-    
-    from ..utils.data_utils import get_file
-import numpy as np
-    
-    
-def get(identifier):
-    if identifier is None:
-        return None
-    if isinstance(identifier, dict):
-        return deserialize(identifier)
-    elif isinstance(identifier, six.string_types):
-        config = {'class_name': str(identifier), 'config': {}}
-        return deserialize(config)
-    elif callable(identifier):
-        return identifier
-    else:
-        raise ValueError('Could not interpret regularizer identifier: ' +
-                         str(identifier))
-
-    
-    
-def test_imdb():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = imdb.load_data()
-        (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=40)
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        word_index = imdb.get_word_index()
-        assert isinstance(word_index, dict)
-    
-            # Save model via the template model (which shares the same weights):
-        model.save('my_model.h5')
-    ```
-    
-            params = self._canonical_to_params(
-            weights=[
-                self.kernel_r,
-                self.kernel_z,
-                self.kernel_h,
-                self.recurrent_kernel_r,
-                self.recurrent_kernel_z,
-                self.recurrent_kernel_h,
-            ],
-            biases=[
-                self.bias_r_i,
-                self.bias_z_i,
-                self.bias_h_i,
-                self.bias_r,
-                self.bias_z,
-                self.bias_h,
-            ],
-        )
-        outputs, h = self._cudnn_gru(
-            inputs,
-            input_h=input_h,
-            params=params,
-            is_training=True)
-    
-        # Arguments
-      rotation: A float, the degree range for rotation (0 <= rotation < 180),
-          e.g. 3 for random image rotation between (-3.0, 3.0).
-      horizontal_flip: A boolean, whether to allow random horizontal flip,
-          e.g. true for 50% possibility to flip image horizontally.
-      vertical_flip: A boolean, whether to allow random vertical flip,
-          e.g. true for 50% possibility to flip image vertically.
-    
-    _options = [
-    'help',
-    'version',
-    'gui',
-    'force',
-    'playlists',
-]
-_short_options = 'hVgfl'
-    
-            if self.tree.find('result').text != '1':
-            log.wtf('API result says failed!')
-            raise 
-    
-    
-def cntv_download(url, **kwargs):
-    if re.match(r'http://tv\.cntv\.cn/video/(\w+)/(\w+)', url):
-        rid = match1(url, r'http://tv\.cntv\.cn/video/\w+/(\w+)')
-    elif re.match(r'http://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
-        rid = r1(r'var guid = '(\w+)'', get_content(url))
-    elif re.match(r'http://\w+\.cntv\.cn/(\w+/\w+/(classpage/video/)?)?\d+/\d+\.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/(\w+/)*VIDE\d+.shtml', url) or \
-         re.match(r'http://(\w+).cntv.cn/(\w+)/classpage/video/(\d+)/(\d+).shtml', url) or \
-         re.match(r'http://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/\d+/\d+/\d+/\w+.shtml', url): 
-        page = get_content(url)
-        rid = r1(r'videoCenterId','(\w+)'', page)
-        if rid is None:
-            guid = re.search(r'guid\s*=\s*'([0-9a-z]+)'', page).group(1)
-            rid = guid
-    elif re.match(r'http://xiyou.cntv.cn/v-[\w-]+\.html', url):
-        rid = r1(r'http://xiyou.cntv.cn/v-([\w-]+)\.html', url)
-    else:
-        raise NotImplementedError(url)
-    
-        try:
-        json_data = get_coub_data(html)
-        title, video_url, audio_url = get_title_and_urls(json_data)
-        video_file_name, video_file_path = get_file_path(merge, output_dir, title, video_url)
-        audio_file_name, audio_file_path = get_file_path(merge, output_dir, title, audio_url)
-        download_url(audio_url, merge, output_dir, title, info_only)
-        download_url(video_url, merge, output_dir, title, info_only)
-        if not info_only:
-            try:
-                fix_coub_video_file(video_file_path)
-                audio_duration = float(ffmpeg.ffprobe_get_media_duration(audio_file_path))
-                video_duration = float(ffmpeg.ffprobe_get_media_duration(video_file_path))
-                loop_file_path = get_loop_file_path(title, output_dir)
-                single_file_path = audio_file_path
-                if audio_duration > video_duration:
-                    write_loop_file(int(audio_duration / video_duration), loop_file_path, video_file_name)
+            elif desired_state == 'offline':
+            if current_state == HOST_ABSENT:
+                self.fail(msg='absent host cannot be placed in offline state')
+            elif current_state in [HOST_STATES.MONITORED, HOST_STATES.DISABLED]:
+                if one.host.status(host.ID, HOST_STATUS.OFFLINE):
+                    self.wait_for_host_state(host, [HOST_STATES.OFFLINE])
+                    result['changed'] = True
                 else:
-                    single_file_path = audio_file_path
-                    write_loop_file(int(video_duration / audio_duration), loop_file_path, audio_file_name)
+                    self.fail(msg='could not set host offline')
+            elif current_state in [HOST_STATES.OFFLINE]:
+                pass
+            else:
+                self.fail(msg='unknown host state %s, cowardly refusing to change state to offline' % current_state_name)
     
-    site_info = 'Douban.com'
-download = douban_download
-download_playlist = playlist_not_supported('douban')
+        def hbacrule_mod(self, name, item):
+        return self._post_json(method='hbacrule_mod', name=name, item=item)
+    
+        module = AnsibleModule(
+        argument_spec=dict(
+            component=dict(required=True, aliases=['name']),
+            version=dict(required=True),
+            token=dict(required=True, no_log=True),
+            state=dict(required=True, choices=['started', 'finished', 'failed']),
+            hosts=dict(required=False, default=[socket.gethostname()], aliases=['host']),
+            env=dict(required=False),
+            owner=dict(required=False),
+            description=dict(required=False),
+            message=dict(required=False),
+            source_system=dict(required=False, default='ansible'),
+            validate_certs=dict(default='yes', type='bool'),
+            url=dict(required=False, default='https://api.bigpanda.io'),
+        ),
+        supports_check_mode=True,
+    )
+    
+    from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.six.moves.urllib.parse import urlencode
+from ansible.module_utils._text import to_native
+from ansible.module_utils.urls import fetch_url
+    
+    # Apply monkey patches to fix issues in external libraries
+from . import _monkeypatches
+del _monkeypatches
+    
+        def long_desc(self):
+        '''A long description of the command. Return short description when not
+        available. It cannot contain newlines, since contents will be formatted
+        by optparser which removes newlines and wraps text.
+        '''
+        return self.short_desc()
+    
+            spidercls = DefaultSpider
+        spider_loader = self.crawler_process.spider_loader
+        if opts.spider:
+            spidercls = spider_loader.load(opts.spider)
+        else:
+            spidercls = spidercls_for_request(spider_loader, request, spidercls)
+        self.crawler_process.crawl(spidercls, start_requests=lambda: [request])
+        self.crawler_process.start()
 
     
+        def test_simple(self):
+        def f(p):
+            pass
+        f_ident = ident(f)
+        self.check_events(f, [(1, 'call', f_ident),
+                              (1, 'return', f_ident),
+                              ])
     
-'''
-http://open.iqiyi.com/lib/player.html
-'''
-iqiyi_patterns = [r'(?:\'|\')(https?://dispatcher\.video\.qiyi\.com\/disp\/shareplayer\.swf\?.+?)(?:\'|\')',
-                  r'(?:\'|\')(https?://open\.iqiyi\.com\/developer\/player_js\/coopPlayerIndex\.html\?.+?)(?:\'|\')']
     
-        print('Done!')
-    if failures:
-        print('Some downloads have failed:')
-        for fail in failures:
-            print('> ' + fail)
+def _check_location(package):
+    if package.__spec__.origin is None or not package.__spec__.has_location:
+        raise FileNotFoundError(f'Package has no location {package!r}')
+    
+    import os
+import email
+import mimetypes
+    
+    # register the Foo class; make `g()` and `_h()` accessible via proxy
+MyManager.register('Foo2', Foo, exposed=('g', '_h'))
+    
+    def worker(input, output):
+    for func, args in iter(input.get, 'STOP'):
+        result = calculate(func, args)
+        output.put(result)
+    
+            #Creating directory title page for current directory
+        with open('dir_title.html', 'w') as os_html:
+            os_html.write(header + dir_title)
+        
+        group.append(HTML('dir_title.html').render())
