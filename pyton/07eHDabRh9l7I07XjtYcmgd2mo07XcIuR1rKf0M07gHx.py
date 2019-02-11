482541131fdcf203158680562b9776421507e435
SUNsung <weatherius@gmail.com>
2019-02-11 20:48:42 +0200

    Rewrite [07eHDabRh9l7I07XjtYcmgd2mo07XcIuR1rKf0M07gHx.py] File in: 2019-02-11 20:48:42

--- pyton/07eHDabRh9l7I07XjtYcmgd2mo07XcIuR1rKf0M07gHx.py
+++ pyton/07eHDabRh9l7I07XjtYcmgd2mo07XcIuR1rKf0M07gHx.py
@@ -1,10 +1,38 @@
 
         
-            while s % 2 == 0:
-        s = s // 2
-        t += 1
+        
+with codecs.open(FILE_PATH, encoding='utf8') as f:
+    # Strip because we don't want new lines in the data so that we can
+    # easily count occurrences also when embedded in JSON (where the new
+    # line would be escaped).
+    FILE_CONTENT = f.read().strip()
     
     
-class SubArray:
+def test_follow_redirect_output_options(httpbin):
+    r = http('--check-status',
+             '--follow',
+             '--all',
+             '--print=h',
+             '--history-print=H',
+             httpbin.url + '/redirect/2')
+    assert r.count('GET /') == 2
+    assert 'HTTP/1.1 302 FOUND' not in r
+    assert HTTP_OK in r
     
-        dp = [[False for x in range(s+1)]for y in range(n+1)]
+        def test_upload_multiple_fields_with_the_same_name(self, httpbin):
+        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
+                 'test-file@%s' % FILE_PATH_ARG,
+                 'test-file@%s' % FILE_PATH_ARG)
+        assert HTTP_OK in r
+        assert r.count('Content-Disposition: form-data; name='test-file';'
+                       ' filename='%s'' % os.path.basename(FILE_PATH)) == 2
+        # Should be 4, but is 3 because httpbin
+        # doesn't seem to support filed field lists
+        assert r.count(FILE_CONTENT) in [3, 4]
+        assert r.count('Content-Type: text/plain') == 2
+    
+        session = Session(path)
+    session.load()
+    
+            #Required string to create directory title pages
+        dir_title = '<h2 class=titledir>' + operating_sys.capitalize() + '</h2></body></html>'
