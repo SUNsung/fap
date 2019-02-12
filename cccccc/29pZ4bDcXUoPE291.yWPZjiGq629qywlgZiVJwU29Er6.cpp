5cff0c3f6b0a2e2a0c23c223e4e813130430c60b
SUNsung <weatherius@gmail.com>
2019-02-13 01:53:00 +0200

    Rewrite [29pZ4bDcXUoPE291.yWPZjiGq629qywlgZiVJwU29Er6.cpp] File in: 2019-02-13 01:53:00

--- cccccc/29pZ4bDcXUoPE291.yWPZjiGq629qywlgZiVJwU29Er6.cpp
+++ cccccc/29pZ4bDcXUoPE291.yWPZjiGq629qywlgZiVJwU29Er6.cpp
@@ -1,103 +1,136 @@
 
         
-        
+        #include <google/protobuf/python/python_protobuf.h>
+    
+    void WriteEnumDocComment(io::Printer* printer, const EnumDescriptor* enumDescriptor) {
+    WriteDocCommentBody(printer, enumDescriptor);
+}
+void WriteEnumValueDocComment(io::Printer* printer, const EnumValueDescriptor* value) {
+    WriteDocCommentBody(printer, value);
+}
+    
+    // Author: liujisi@google.com (Pherl Liu)
+    
+    string ClassNameResolver::GetExtensionIdentifierName(
+    const FieldDescriptor* descriptor, bool immutable) {
+  return GetClassName(descriptor->containing_type(), immutable) + '.' +
+         descriptor->name();
+}
+    
+    #include <google/protobuf/compiler/java/java_helpers.h>
+#include <google/protobuf/compiler/java/java_name_resolver.h>
+#include <google/protobuf/compiler/code_generator.h>
+#include <google/protobuf/descriptor.pb.h>
+#include <google/protobuf/io/printer.h>
+#include <google/protobuf/io/zero_copy_stream.h>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/stubs/strutil.h>
+    
+        for (std::vector<ExtensionGenerator *>::iterator iter =
+             extension_generators_.begin();
+         iter != extension_generators_.end(); ++iter) {
+      (*iter)->GenerateMembersHeader(printer);
+    }
+    
+    
     {
-    {    if (last_executed < getUnixTime() - 592200) {
-      // Query has not run in the last week, expire results and interval.
-      deleteDatabaseValue(kQueries, saved_query);
-      deleteDatabaseValue(kQueries, saved_query + 'epoch');
-      deleteDatabaseValue(kPersistentSettings, 'interval.' + saved_query);
-      deleteDatabaseValue(kPersistentSettings, 'timestamp.' + saved_query);
-      VLOG(1) << 'Expiring results for scheduled query: ' << saved_query;
+    {        for (; dj < size.width; sj += 4, ++dj)
+        {
+            dst[dj] = src[sj + coi];
+        }
     }
-  }
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)coi;
+#endif
 }
     
-      TableColumns columns() const override {
-    return tc_columns_;
-  }
     
-    void runDecorators(DecorationPoint point,
-                   size_t time,
-                   const std::string& source) {
-  if (FLAGS_disable_decorators) {
-    return;
-  }
-    }
-    
-    /**
- * @brief Access the internal storage of the Decorator parser.
- *
- * The decoration set is a map of column name to value. It contains the opaque
- * set of decoration point results.
- *
- * Decorations are applied to log items before they are sent to the downstream
- * logging APIs: logString, logSnapshot, etc.
- *
- * @param results the output parameter to write decorations.
- */
-void getDecorations(std::map<std::string, std::string>& results);
-    
-    
-    {  return Status();
+    {    return 0;
+#endif
 }
     
-    #include <osquery/config/config.h>
-#include <osquery/config/tests/test_utils.h>
-#include <osquery/database.h>
-#include <osquery/registry.h>
-#include <osquery/system.h>
-    
-    #include <boost/filesystem/operations.hpp>
-#include <boost/property_tree/json_parser.hpp>
-#include <boost/property_tree/ptree.hpp>
-    
-    
-    {}  //  namespace rocksdb
-
-    
-    
-    {}  // namespace rocksdb
-    
-      // put and get from non-default column family
-  s = db->Put(WriteOptions(), handles[1], Slice('key'), Slice('value'));
-  assert(s.ok());
-  std::string value;
-  s = db->Get(ReadOptions(), handles[1], Slice('key'), &value);
-  assert(s.ok());
-    
-    class MyMerge : public rocksdb::MergeOperator {
- public:
-  virtual bool FullMergeV2(const MergeOperationInput& merge_in,
-                           MergeOperationOutput* merge_out) const override {
-    merge_out->new_value.clear();
-    if (merge_in.existing_value != nullptr) {
-      merge_out->new_value.assign(merge_in.existing_value->data(),
-                                  merge_in.existing_value->size());
+            for (; js < roiw_base; js += step_base, jd -= step_base)
+        {
+            prefetch(src + js);
     }
-    for (const rocksdb::Slice& m : merge_in.operand_list) {
-      fprintf(stderr, 'Merge(%s)\n', m.ToString().c_str());
-      // the compaction filter filters out bad values
-      assert(m.ToString() != 'bad');
-      merge_out->new_value.assign(m.data(), m.size());
-    }
-    return true;
+    
+    #include 'common.hpp'
+#include 'saturate_cast.hpp'
+#include 'separable_filter.hpp'
+    
+                uint8x8_t x0 = vld1_u8(v0 + x);
+            uint8x8_t x1 = vld1_u8(v1 + x);
+            uint8x8_t x2 = vld1_u8(v2 + x);
+            uint8x8_t x3 = vld1_u8(v3 + x);
+            uint8x8_t x4 = vld1_u8(v4 + x);
+            if(x) {
+                tcurr1 = tnext1;
+            }
+    
+    void BranchParams::decodeInstr(const PPC64Instr* const pinstr) {
+  const DecoderInfo dinfo = Decoder::GetDecoder().decode(pinstr);
+  switch (dinfo.opcode_name()) {
+    case OpcodeNames::op_b:
+    case OpcodeNames::op_bl:
+      assert(dinfo.form() == Form::kI);
+      defineBoBi(BranchConditions::Always);
+      break;
+    case OpcodeNames::op_bc:
+      assert(dinfo.form() == Form::kB);
+      B_form_t bform;
+      bform.instruction = dinfo.instruction_image();
+      m_bo = BranchParams::BO(bform.BO);
+      m_bi = BranchParams::BI(bform.BI);
+      break;
+    case OpcodeNames::op_bcctr:
+    case OpcodeNames::op_bcctrl:
+      assert(dinfo.form() == Form::kXL);
+      XL_form_t xlform;
+      xlform.instruction = dinfo.instruction_image();
+      m_bo = BranchParams::BO(xlform.BT);
+      m_bi = BranchParams::BI(xlform.BA);
+      break;
+    default:
+      assert(false && 'Not a valid conditional branch instruction');
+      // also possible: defineBoBi(BranchConditions::Always);
+      break;
   }
     }
     
-      // Do some reads and writes to key 'y'
-  s = txn->GetForUpdate(read_options, 'y', &value);
-  txn->Put('y', 'y');
     
-    int main() {
-  // open DB
-  Options options;
-  TransactionDBOptions txn_db_options;
-  options.create_if_missing = true;
-  TransactionDB* txn_db;
+    {
+    {    RNAME(v16); RNAME(v17); RNAME(v18); RNAME(v19); RNAME(v20); RNAME(v21);
+    RNAME(v22); RNAME(v23); RNAME(v24); RNAME(v25); RNAME(v26); RNAME(v27);
+    RNAME(v28); RNAME(v29);
+    return nullptr;
+  }
+ inline const char* regname(RegSF) {
+    return 'cr0';
+ }
+#undef RNAME
+}
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    namespace HPHP {
     }
     
-      std::string ToString(bool exclude_zero_counters = false) const;
+    #define ERROR_RAISE_WARNING(exp)        \
+  int ret = (exp);                      \
+  if (ret != 0) {                       \
+    raise_warning(                      \
+      '%s(): %s',                       \
+      __FUNCTION__,                     \
+      folly::errnoStr(errno).c_str()    \
+    );                                  \
+  }                                     \
     
-    
-    {}  // namespace rocksdb
+        // create values of different floating-point types
+    float n_float = 42.23;
+    float n_float_nan = 1.0f / 0.0f;
+    double n_double = 23.42;
