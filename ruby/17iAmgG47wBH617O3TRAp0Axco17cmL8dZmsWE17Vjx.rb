95dbc69a139c35addfbb73249650034fe9cbae27
SUNsung <weatherius@gmail.com>
2019-02-12 02:39:58 +0200

    Rewrite [17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb] File in: 2019-02-12 02:39:58

--- ruby/17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb
+++ ruby/17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb
@@ -1,248 +1,174 @@
 
         
-                  def retrieve_object(object)
-            if object
-              object
-            elsif @template_object.instance_variable_defined?('@#{@object_name}')
-              @template_object.instance_variable_get('@#{@object_name}')
-            end
-          rescue NameError
-            # As @object_name may contain the nested syntax (item[subobject]) we need to fallback to nil.
-            nil
+                MergeRequest
+          .where(id: start_id..stop_id)
+          .where(latest_merge_request_diff_id: nil)
+          .each_batch(of: BATCH_SIZE) do |relation|
+    
+            def sidekiq_worker_class
+          ImportDiffNoteWorker
+        end
+    
+            def representation_class
+          Representation::Issue
+        end
+    
+              lfs_objects.each do |object|
+            yield object
           end
-    
-              def render_collection_for(builder_class, &block)
-            options = @options.stringify_keys
-            rendered_collection = render_collection do |item, value, text, default_html_options|
-              builder = instantiate_builder(builder_class, item, value, text, default_html_options)
-    
-    def custom_release_header_anchors(markdown)
-  header_regexp = %r!^(\d{1,2})\.(\d{1,2})\.(\d{1,2}) \/ \d{4}-\d{2}-\d{2}!
-  section_regexp = %r!^### \w+ \w+$!
-  markdown.split(%r!^##\s!).map do |release_notes|
-    _, major, minor, patch = *release_notes.match(header_regexp)
-    release_notes
-      .gsub(header_regexp, '\\0\n{: #v\\1-\\2-\\3}')
-      .gsub(section_regexp) { |section| '#{section}\n{: ##{slugify(section)}-v#{major}-#{minor}-#{patch}}' }
-  end.join('\n## ')
-end
-    
-    #
-    
-        attr_reader :filters
-    
-        private
-    
-            css('.toplang', '#quickview', '.top').remove
-    
-        # The path used after confirmation.
-    def after_confirmation_path_for(resource_name, resource)
-      if signed_in?(resource_name)
-        signed_in_root_path(resource)
-      else
-        new_session_path(resource_name)
+        rescue StandardError => e
+          Rails.logger.error('The Lfs import process failed. #{e.message}')
+        end
       end
-    end
-    
-      # Sets the flash message with :key, using I18n. By default you are able
-  # to set up your messages using specific resource scope, and if no message is
-  # found we look to the default scope. Set the 'now' options key to a true
-  # value to populate the flash.now hash in lieu of the default flash hash (so
-  # the flash message will be available to the current action instead of the
-  # next action).
-  # Example (i18n locale file):
-  #
-  #   en:
-  #     devise:
-  #       passwords:
-  #         #default_scope_messages - only if resource_scope is not found
-  #         user:
-  #           #resource_scope_messages
-  #
-  # Please refer to README or en.yml locale file to check what messages are
-  # available.
-  def set_flash_message(key, kind, options = {})
-    message = find_message(kind, options)
-    if options[:now]
-      flash.now[key] = message if message.present?
-    else
-      flash[key] = message if message.present?
     end
   end
+end
+
     
-        def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
-    end
-    
-          def remember_expires_at
-        self.class.remember_for.from_now
+            Caching.set_includes?(already_imported_cache_key, id)
       end
+    
+    module Gitlab
+  module GithubImport
+    module Representation
+      class DiffNote
+        include ToHash
+        include ExposeAttribute
+    
+            # attributes - A Hash containing the raw PR details. The keys of this
+        #              Hash (and any nested hashes) must be symbols.
+        def initialize(attributes)
+          @attributes = attributes
+        end
     
     module Devise
-  module Models
-    # Timeoutable takes care of verifying whether a user session has already
-    # expired or not. When a session expires after the configured time, the user
-    # will be asked for credentials again, it means, they will be redirected
-    # to the sign in page.
-    #
-    # == Options
-    #
-    # Timeoutable adds the following options to devise_for:
-    #
-    #   * +timeout_in+: the interval to timeout the user session without activity.
-    #
-    # == Examples
-    #
-    #   user.timedout?(30.minutes.ago)
-    #
-    module Timeoutable
-      extend ActiveSupport::Concern
-    
-      it 'transfers control to the innermost catch block waiting for the same sympol' do
-    one = two = three = 0
-    catch :duplicate do
-      catch :duplicate do
-        catch :duplicate do
-          one = 1
-          throw :duplicate
-        end
-        two = 2
-        throw :duplicate
-      end
-      three = 3
-      throw :duplicate
-    end
-    [one, two, three].should == [1, 2, 3]
-  end
-    
-        desc 'Build and install all of the gems as local gems'
-    task :all => GEMS_AND_ROOT_DIRECTORIES.keys
-  end
-    
-          def real_token(session)
-        decode_token(session[:csrf])
-      end
-    
-              react_and_close(env, body) or [status, headers, body]
-        else
-          [status, headers, body]
+  module Controllers
+    # Provide sign in and sign out functionality.
+    # Included by default in all controllers.
+    module SignInOut
+      # Return true if the given scope is signed in session. If no scope given, return
+      # true if any scope is signed in. This will run authentication hooks, which may
+      # cause exceptions to be thrown from this method; if you simply want to check
+      # if a scope has already previously been authenticated without running
+      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
+      def signed_in?(scope=nil)
+        [scope || Devise.mappings.keys].flatten.any? do |_scope|
+          warden.authenticate?(scope: _scope)
         end
       end
     
-      describe '#referrer' do
-    it 'Reads referrer from Referer header' do
-      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/valid'}
-      expect(subject.referrer(env)).to eq('bar.com')
-    end
+    require 'uri'
     
-    require 'bundler/cli'
-require 'bundler/friendly_errors'
+                  define_method method do |resource_or_scope, *args|
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                router_name = Devise.mappings[scope].router_name
+                context = router_name ? send(router_name) : _devise_route_context
+                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
+              end
+            end
+          end
+        end
+      end
     
-          puts user_feedback_string_for('halting', args[:platform], machines, {'experimental' => experimental})
-      options = {:debug => ENV['LS_QA_DEBUG']}
+      uninstall_preflight do
+    system_command '#{HOMEBREW_PREFIX}/bin/brew', args: ['cask', 'uninstall', 'adobe-photoshop-lightroom600']
+  end
     
-          it 'list the plugin with his version' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose #{plugin_name}')
-        expect(result).to run_successfully_and_output(/^#{plugin_name} \(\d+\.\d+.\d+\)/)
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+          attr_reader :key, :default, :options
+    
+        assert_no_match /New/, last_response.body, ''New' link not blocked in pages template'
+    
+      test 'extracting paths from URLs' do
+    assert_nil extract_path('Eye-Of-Sauron')
+    assert_equal 'Mordor', extract_path('Mordor/Sauron')
+    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
+  end
+    
+      # replace name version and date
+  replace_header(head, :name)
+  replace_header(head, :version)
+  replace_header(head, :date)
+  #comment this out if your rubyforge_project has a different name
+  replace_header(head, :rubyforge_project)
+    
+      s.add_development_dependency 'rack-test', '~> 0.6.2'
+  s.add_development_dependency 'shoulda', '~> 3.5.0'
+  s.add_development_dependency 'minitest-reporters', '~> 0.14.16'
+  s.add_development_dependency 'twitter_cldr', '~> 3.2.0'
+  s.add_development_dependency 'mocha', '~> 1.1.0'
+  s.add_development_dependency 'test-unit', '~> 3.1.0'
+  s.add_development_dependency 'webrick', '~> 1.3.1'
+    
+      class Error < StandardError;
+  end
+    
+            context 'with nested resources' do
+          before { get '/api/v2/storefront/products?include=option_types,option_types.option_values' }
+    
+        it 'return JSON API payload of User and associations (default billing and shipping address)' do
+      expect(json_response['data']).to have_id(user.id.to_s)
+      expect(json_response['data']).to have_type('user')
+      expect(json_response['data']).to have_relationships(:default_shipping_address, :default_billing_address)
+    
+          it_behaves_like 'returns 200 HTTP status'
+    
+            it 'returns proper links data' do
+          expect(json_response['links']['self']).to include('/api/v2/storefront/taxons')
+          expect(json_response['links']['next']).to include('/api/v2/storefront/taxons?page=1')
+          expect(json_response['links']['prev']).to include('/api/v2/storefront/taxons?page=1')
+        end
       end
     end
+  end
+    
+              def serialize_resource(resource)
+            resource_serializer.new(
+              resource,
+              include: resource_includes,
+              sparse_fields: sparse_fields
+            ).serializable_hash
+          end
+    
+    desc 'Creates a sandbox application for simulating the Spree code in a deployed Rails app'
+task :sandbox do
+  Bundler.with_clean_env do
+    exec('lib/sandbox.sh')
   end
 end
 
     
-    run SinatraStaticServer
-
+      # Output a pkgbuild pkg.
+  def output(output_path)
+    output_check(output_path)
     
-        def render(context)
-      quote = paragraphize(super)
-      author = '<strong>#{@by.strip}</strong>' if @by
-      if @source
-        url = @source.match(/https?:\/\/(.+)/)[1].split('/')
-        parts = []
-        url.each do |part|
-          if (parts + [part]).join('/').length < 32
-            parts << part
-          end
-        end
-        source = parts.join('/')
-        source << '/&hellip;' unless source == @source
-      end
-      if !@source.nil?
-        cite = ' <cite><a href='#{@source}'>#{(@title || source)}</a></cite>'
-      elsif !@title.nil?
-        cite = ' <cite>#{@title}</cite>'
-      end
-      blockquote = if @by.nil?
-        quote
-      elsif cite
-        '#{quote}<footer>#{author + cite}</footer>'
-      else
-        '#{quote}<footer>#{author}</footer>'
-      end
-      '<blockquote>#{blockquote}</blockquote>'
+        if attributes[:pear_data_dir]
+      logger.info('Setting data_dir', :data_dir => attributes[:pear_data_dir])
+      safesystem('pear', '-c', config, 'config-set', 'data_dir', '#{staging_path(installroot)}/#{attributes[:pear_data_dir]}')
     end
     
-    class ConfigTag < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-    options = options.split(' ').map {|i| i.strip }
-    @key = options.slice!(0)
-    @tag = nil
-    @classname = nil
-    options.each do |option|
-      @tag = $1 if option =~ /tag:(\S+)/ 
-      @classname = $1 if option =~ /classname:(\S+)/
-    end
+      def iteration
+    return @iteration ? @iteration : 1
   end
     
-        def render(context)
-      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
-        gist, file = parts[1].strip, parts[2].strip
+        # Copy 'files' from builddir to :output/files
+    Find.find('files', 'manifests') do |path|
+      logger.info('Copying path: #{path}')
+      if File.directory?(path)
+        ::Dir.mkdir(File.join(params[:output], path))
       else
-        gist, file = @text.strip, ''
-      end
-      if gist.empty?
-        ''
-      else
-        script_url = script_url_for gist, file
-        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
-        html_output_for script_url, code
+        FileUtils.cp(path, File.join(params[:output], path))
       end
     end
+  end # def build!
     
-          Dir.chdir(file_path) do
-        contents = file.read
-        if contents =~ /\A-{3}.+[^\A]-{3}\n(.+)/m
-          contents = $1.lstrip
-        end
-        contents = pre_filter(contents)
-        if @raw
-          contents
-        else
-          partial = Liquid::Template.parse(contents)
-          context.stack do
-            partial.render(context)
-          end
-        end
-      end
+        # Unpack the tarball to the staging path
+    args = ['-cf', output_path, '-C', staging_path]
+    tar_compression_flag(output_path).tap do |flag|
+      args << flag unless flag.nil?
     end
-  end
-end
-    
-      # Helper for user lookup
-  def uid2user(uid)
-    begin
-      pwent = Etc.getpwuid(uid)
-      return pwent.name
-    rescue ArgumentError => e
-      # Invalid user id? No user? Return the uid.
-      logger.warn('Failed to find username for uid #{uid}')
-      return uid.to_s
-    end
-  end # def uid2user
-    
-        # Make one file. The installscript can unpack itself.
-    `cat #{install_script} #{payload} > #{output_path}`
-    FileUtils.chmod('+x', output_path)
-  end
+    args << '.'
