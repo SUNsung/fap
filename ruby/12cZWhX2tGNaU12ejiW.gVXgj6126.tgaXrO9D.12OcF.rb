3294b566beb97b3cf9f3e06229adb10aa77edb05
SUNsung <weatherius@gmail.com>
2019-02-12 22:59:11 +0200

    Rewrite [12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb] File in: 2019-02-12 22:59:09

--- ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
+++ ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
@@ -1,30 +1,85 @@
 
         
-            INVALID_PLUGINS_TO_EXPLICIT_PACK = IGNORE_GEMS_IN_PACK.collect { |name| /^#{name}/ } + [
-      /mixin/
-    ]
-    
-      it 'returns the merged `ConfigPart#config_string`' do
-    expect(subject.config_string).to eq(ordered_config_parts.collect(&:text).join('\n'))
+        task :permissions do
+  sh %{chmod -R a+rx bin}
+  sh %{chmod -R a+r .}
+  require 'shellwords'
+  Dir.glob('test/**/*_test.rb') do |file|
+    next if file =~ %r{^test/haml/spec/}
+    sh %{chmod a+rx #{file}}
   end
+end
     
-          options = {:debug => ENV['LS_QA_DEBUG']}
-      puts 'Destroying #{machines}'
-      LogStash::VagrantHelpers.destroy(machines, options)
-      puts 'Bootstrapping #{machines}'
-      LogStash::VagrantHelpers.bootstrap(machines, options)
+        # Returns a string representation of the Sass backtrace.
+    #
+    # @param default_filename [String] The filename to use for unknown files
+    # @see #sass_backtrace
+    # @return [String]
+    def sass_backtrace_str(default_filename = 'an unknown file')
+      lines = message.split('\n')
+      msg = lines[0] + lines[1..-1].
+        map {|l| '\n' + (' ' * 'Error: '.size) + l}.join
+      'Error: #{msg}' +
+        sass_backtrace.each_with_index.map do |entry, i|
+          '\n        #{i == 0 ? 'on' : 'from'} line #{entry[:line]}' +
+            ' of #{entry[:filename] || default_filename}' +
+            (entry[:mixin] ? ', in `#{entry[:mixin]}'' : '')
+        end.join
     end
     
-    shared_examples 'logstash list' do |logstash|
-  describe 'logstash-plugin list on #{logstash.hostname}' do
-    before(:all) do
-      logstash.install({:version => LOGSTASH_VERSION})
+        def miscellaneous(opts)
+      opts.separator ''
+      opts.separator 'Miscellaneous:'
+    
+        # @param args [Array<String>] The command-line arguments
+    def initialize(args, default_syntax)
+      super(args)
+      @options[:sourcemap] = :auto
+      @options[:for_engine] = {
+        :load_paths => default_sass_path
+      }
+      @default_syntax = default_syntax
     end
     
-        context 'update a specific plugin' do
-      it 'has executed successfully' do
-        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
-        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
-        expect(logstash).not_to have_installed?(plugin_name, previous_version)
+    Then(/^the default stage files are created$/) do
+  staging = TestApp.test_app_path.join('config/deploy/staging.rb')
+  production = TestApp.test_app_path.join('config/deploy/production.rb')
+  expect(File.exist?(staging)).to be true
+  expect(File.exist?(production)).to be true
+end
+    
+    Given(/^a linked file '(.*?)'$/) do |file|
+  # ignoring other linked files
+  TestApp.append_to_deploy_file('set :linked_files, ['#{file}']')
+end
+    
+        def role_properties_for(names, &block)
+      servers.role_properties_for(names, &block)
+    end
+    
+        worker_count.times do |count|
+      template '/data/#{app}/shared/config/sidekiq_#{count}.yml' do
+        owner node[:owner_name]
+        group node[:owner_name]
+        mode 0644
+        source 'sidekiq.yml.erb'
+        variables({
+          :require => '/data/#{app}/current'
+        })
       end
     end
+    
+          def perform(yml)
+        (target, method_name, args) = YAML.load(yml)
+        msg = target.public_send(method_name, *args)
+        # The email method can return nil, which causes ActionMailer to return
+        # an undeliverable empty message.
+        if msg
+          deliver(msg)
+        else
+          raise '#{target.name}##{method_name} returned an undeliverable mail object'
+        end
+      end
+    
+        def _erb(file, locals)
+      locals.each {|k, v| define_singleton_method(k){ v } unless (singleton_methods.include? k)} if locals
