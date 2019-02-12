86374ce4158b7648663a3da09014fc0beef1ac3e
SUNsung <weatherius@gmail.com>
2019-02-12 23:28:41 +0200

    Rewrite [52zcj3MHIQDUo52QHG9ZEK9E0I5271qAIETH9ms52d2a.py] File in: 2019-02-12 23:28:41

--- pyton/52zcj3MHIQDUo52QHG9ZEK9E0I5271qAIETH9ms52d2a.py
+++ pyton/52zcj3MHIQDUo52QHG9ZEK9E0I5271qAIETH9ms52d2a.py
@@ -1,153 +1,112 @@
 
         
-          def __init__(self, batch_size, z_size, mean, logvar):
-    '''Create a diagonal gaussian distribution.
+            @classmethod
+    def get_model_class(cls):
+        # Avoids a circular import and allows importing SessionStore when
+        # django.contrib.sessions is not in INSTALLED_APPS.
+        from django.contrib.sessions.models import Session
+        return Session
     
-    # Check to make sure the RNN is the one we used in the paper.
-if N == 50:
-  assert abs(rnn['W'][0,0] - 0.06239899) < 1e-8, 'Error in random seed?'
-  rem_check = nreplications * train_percentage
-  assert  abs(rem_check - int(rem_check)) < 1e-8, \
-    'Train percentage  * nreplications should be integral number.'
+        def make_uri(self, path):
+        # Under Windows, file URIs use the UTF-8 encoding.
+        drive = path.drive
+        if len(drive) == 2 and drive[1] == ':':
+            # It's a path on a local drive => 'file:///c:/a/b'
+            rest = path.as_posix()[2:].lstrip('/')
+            return 'file:///%s/%s' % (
+                drive, urlquote_from_bytes(rest.encode('utf-8')))
+        else:
+            # It's a path on a network drive => 'file://host/share/a/b'
+            return 'file:' + urlquote_from_bytes(path.as_posix().encode('utf-8'))
     
-    rates_train, rates_valid = split_list_by_inds(rates, train_inds, valid_inds)
-spikes_train, spikes_valid = split_list_by_inds(spikes, train_inds, valid_inds)
-condition_labels_train, condition_labels_valid = split_list_by_inds(
-    condition_labels, train_inds, valid_inds)
-ext_input_train, ext_input_valid = split_list_by_inds(
-    ext_input, train_inds, valid_inds)
+        # Modify function metadata to help with introspection and debugging
+    for method in (__new__, _make.__func__, _replace,
+                   __repr__, _asdict, __getnewargs__):
+        method.__qualname__ = f'{typename}.{method.__name__}'
     
-    def get_train_n_valid_inds(num_trials, train_fraction, nreplications):
-  '''Split the numbers between 0 and num_trials-1 into two portions for
-  training and validation, based on the train fraction.
-  Args:
-    num_trials: the number of trials
-    train_fraction: (e.g. .80)
-    nreplications: the number of spiking trials per initial condition
-  Returns:
-    a 2-tuple of two lists: the training indices and validation indices
-    '''
-  train_inds = []
-  valid_inds = []
-  for i in range(num_trials):
-    # This line divides up the trials so that within one initial condition,
-    # the randomness of spikifying the condition is shared among both
-    # training and validation data splits.
-    if (i % nreplications)+1 > train_fraction * nreplications:
-      valid_inds.append(i)
+            is_not_importable = False
+        is_namespace = False
+        tests = []
+        if os.path.isdir(os.path.abspath(start_dir)):
+            start_dir = os.path.abspath(start_dir)
+            if start_dir != top_level_dir:
+                is_not_importable = not os.path.isfile(os.path.join(start_dir, '__init__.py'))
+        else:
+            # support for discovery from dotted module names
+            try:
+                __import__(start_dir)
+            except ImportError:
+                is_not_importable = True
+            else:
+                the_module = sys.modules[start_dir]
+                top_part = start_dir.split('.')[0]
+                try:
+                    start_dir = os.path.abspath(
+                       os.path.dirname((the_module.__file__)))
+                except AttributeError:
+                    # look for namespace packages
+                    try:
+                        spec = the_module.__spec__
+                    except AttributeError:
+                        spec = None
+    
+            self.check_tokenize('''\
+async def f():
+    
+        @unittest.skipIf(interpreter_requires_environment(),
+                     'Cannot run -E tests when PYTHON env vars are required.')
+    def test_xoptions(self):
+        def get_xoptions(*args):
+            # use subprocess module directly because test.support.script_helper adds
+            # '-X faulthandler' to the command line
+            args = (sys.executable, '-E') + args
+            args += ('-c', 'import sys; print(sys._xoptions)')
+            out = subprocess.check_output(args)
+            opts = eval(out.splitlines()[0])
+            return opts
+    
+    from email.message import EmailMessage
+from email.headerregistry import Address
+from email.utils import make_msgid
+    
+        for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        if not os.path.isfile(path):
+            continue
+        # Guess the content type based on the file's extension.  Encoding
+        # will be ignored, although we should check for simple things like
+        # gzip'd or compressed files.
+        ctype, encoding = mimetypes.guess_type(path)
+        if ctype is None or encoding is not None:
+            # No guess could be made, or the file is encoded (compressed), so
+            # use a generic bag-of-bits type.
+            ctype = 'application/octet-stream'
+        maintype, subtype = ctype.split('/', 1)
+        with open(path, 'rb') as fp:
+            msg.add_attachment(fp.read(),
+                               maintype=maintype,
+                               subtype=subtype,
+                               filename=filename)
+    # Now send or store the message
+    if args.output:
+        with open(args.output, 'wb') as fp:
+            fp.write(msg.as_bytes(policy=SMTP))
     else:
-      train_inds.append(i)
+        with smtplib.SMTP('localhost') as s:
+            s.send_message(msg)
     
-      def encode(self, sentence):
-    '''Convert a sentence to a list of ids, with special tokens added.'''
-    word_ids = [self.word_to_id(cur_word) for cur_word in sentence.split()]
-    return np.array([self.bos] + word_ids + [self.eos], dtype=np.int32)
+    $ pip3 install scikit-learn
     
-      def __init__(self, test_data_name='wsc273'):
-    vocab_file = os.path.join(FLAGS.data_dir, 'vocab.txt')
-    self.vocab = utils.CharsVocabulary(vocab_file, 50)
-    assert test_data_name in ['pdp60', 'wsc273'], (
-        'Test data must be pdp60 or wsc273, got {}'.format(test_data_name))
-    self.test_data_name = test_data_name
+    try:
+    import face_recognition_models
+except Exception:
+    print('Please install `face_recognition_models` with this command before using `face_recognition`:\n')
+    print('pip install git+https://github.com/ageitgey/face_recognition_models')
+    quit()
     
-      Returns:
-    gen_train_op: Generator training op.
-  '''
-  del hparams
-  with tf.name_scope('train_generator'):
-    if FLAGS.generator_optimizer == 'sgd':
-      gen_optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    elif FLAGS.generator_optimizer == 'adam':
-      gen_optimizer = tf.train.AdamOptimizer(learning_rate)
-    else:
-      raise NotImplementedError
-    gen_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
-    ]
-    print('\nOptimizing Generator vars:')
-    for v in gen_vars:
-      print(v)
+    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
     
-        # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
-    app.add_url_rule('/', endpoint='index')
-    
-        # test that successful registration redirects to the login page
-    response = client.post(
-        '/auth/register', data={'username': 'a', 'password': 'a'}
-    )
-    assert 'http://localhost/auth/login' == response.headers['Location']
-    
-        def fake_init_db():
-        Recorder.called = True
-    
-    
-# context locals
-_request_ctx_stack = LocalStack()
-_app_ctx_stack = LocalStack()
-current_app = LocalProxy(_find_app)
-request = LocalProxy(partial(_lookup_req_object, 'request'))
-session = LocalProxy(partial(_lookup_req_object, 'session'))
-g = LocalProxy(partial(_lookup_app_object, 'g'))
-
-    
-    
-@LocalProxy
-def wsgi_errors_stream():
-    '''Find the most appropriate error stream for the application. If a request
-    is active, log to ``wsgi.errors``, otherwise use ``sys.stderr``.
-    
-    
-def _default_template_ctx_processor():
-    '''Default template context processor.  Injects `request`,
-    `session` and `g`.
-    '''
-    reqctx = _request_ctx_stack.top
-    appctx = _app_ctx_stack.top
-    rv = {}
-    if appctx is not None:
-        rv['g'] = appctx.g
-    if reqctx is not None:
-        rv['request'] = reqctx.request
-        rv['session'] = reqctx.session
-    return rv
-    
-    
-def parse_changelog():
-    with open('CHANGES.rst') as f:
-        lineiter = iter(f)
-        for line in lineiter:
-            match = re.search('^Version\s+(.*)', line.strip())
-    
-        def _IfExp(self, t):
-        self.write('(')
-        self.dispatch(t.body)
-        self.write(' if ')
-        self.dispatch(t.test)
-        self.write(' else ')
-        self.dispatch(t.orelse)
-        self.write(')')
-    
-            bz2f = BZ2File(BytesIO(), 'w')
-        try:
-            self.assertFalse(bz2f.readable())
-        finally:
-            bz2f.close()
-        self.assertRaises(ValueError, bz2f.readable)
-    
-    class DBPickler(pickle.Pickler):
-    
-    import os
-import smtplib
-# For guessing MIME type based on file name extension
-import mimetypes
-    
-    def test():
-    manager = MyManager()
-    manager.start()
-    
-            #
-        # Test error handling
-        #
+            if file.filename == '':
+            return redirect(request.url)
