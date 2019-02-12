a7d7623ac0a3dbdd8ec6b14dbf3a364cc681d3a5
SUNsung <weatherius@gmail.com>
2019-02-12 17:45:26 +0200

    Rewrite [56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp] File in: 2019-02-12 17:45:25

--- cccccc/56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp
+++ cccccc/56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp
@@ -1,90 +1,217 @@
 
         
-        //---------------------------------------------------------------------------------
-// The following part are API Registration of Operators
-// See also MXNET_REGISTER_SIMPLE_OP in operator_util.h for registering simple ops.
-//---------------------------------------------------------------------------------
-/*!
- * \brief Macro to register OperatorProperty
- *
- * \code
- * // example of registering a fully connected operator
- * REGISTER_OP_PROPERTY(FullyConnected, FullyConnectedOpProp)
- * .describe('Fully connected layer');
- *
- * \endcode
- */
-#define MXNET_REGISTER_OP_PROPERTY(name, OperatorPropertyType)          \
-  DMLC_REGISTRY_REGISTER(::mxnet::OperatorPropertyReg, OperatorPropertyReg, name) \
-  .set_body([]() { return new OperatorPropertyType(); })                \
-  .set_return_type('NDArray-or-Symbol') \
-  .check_name()
-    
-    #include <caffe/proto/caffe.pb.h>
-    
-    #include <caffe/util/io.hpp>
-namespace dmlc {
-namespace parameter {
-    }
+        Status ModelAnalyzer::GenerateReport(bool debug, bool assume_valid_feeds,
+                                     std::ostream& os) {
+  GraphProperties properties(item_);
+  TF_RETURN_IF_ERROR(properties.InferStatically(assume_valid_feeds));
     }
     
-      inline void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) {
-    std::vector<std::pair<std::string, std::string> > kwargs_left;
-    // init batch param, it could have similar param with
-    kwargs_left = param_.InitAllowUnknown(kwargs);
-    // Init space for out
-    out_.inst_index = new unsigned[param_.batch_size];
-    out_.batch_size = param_.batch_size;
-    out_.data.clear();
-    // init base iterator
-    base_->Init(kwargs);
+    #include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+    
+      friend class SubstitutionMap;
+    
+    // We'd like the dump routine to be present in all builds, but it's
+// a pretty large amount of code, most of which is not sensitive to the
+// actual key and value data.  If we try to have a common implementation,
+// we're left with the problem of describing the layout of a node when
+// that's technically instantiation-specific.  Redefining the struct here
+// is technically an aliasing violation, but we can just tell the compilers
+// that actually use TBAA that this is okay.
+typedef struct _Node Node LLVM_MAY_ALIAS;
+struct _Node {
+  // If you change the layout in the header, you'll need to change it here.
+  // (This comment is repeated there.)
+  Node *Left, *Right, *Further;
+};
+    
+    /// Determine whether the given word occurring after the given
+/// preposition results in a conflict that suppresses preposition
+/// splitting.
+static bool wordConflictsAfterPreposition(StringRef word,
+                                          StringRef preposition) {
+  if (camel_case::sameWordIgnoreFirstCase(preposition, 'with')) {
+    if (camel_case::sameWordIgnoreFirstCase(word, 'error') ||
+        camel_case::sameWordIgnoreFirstCase(word, 'no'))
+      return true;
+  }
+    }
+    
+      static CFPointeeInfo forRecord(bool isConst, const clang::RecordDecl *decl) {
+    assert(decl);
+    CFPointeeInfo info;
+    info.IsValid = true;
+    info.IsConst = isConst;
+    info.Decl = decl;
+    return info;
   }
     
-    BENCHMARK_PARAM(BENCHFUN(insertFront), 16)
-BENCHMARK_PARAM(BENCHFUN(insertFront), 128)
-BENCHMARK_PARAM(BENCHFUN(insertFront), 1024)
-BENCHMARK_PARAM(BENCHFUN(insertFront), 10240)
-BENCHMARK_PARAM(BENCHFUN(insertFront), 102400)
-BENCHMARK_PARAM(BENCHFUN(insertFront), 1024000)
+        /// Returns true if \p loc is inside one of Swift's synthetic buffers.
+    static bool isInSwiftBuffers(clang::FullSourceLoc loc) {
+      StringRef bufName = StringRef(loc.getManager().getBufferName(loc));
+      return bufName == ClangImporter::Implementation::moduleImportBufferName ||
+             bufName == ClangImporter::Implementation::bridgingHeaderBufferName;
+    }
     
-      FunctionRef<int(int, std::vector<int> const&)> variant6 = of;
-  EXPECT_EQ(100 + 6 * 20, variant6(20, {}));
-  EXPECT_EQ(100 + 6 * 20, variant6(20, {1, 2, 3}));
-  FunctionRef<int(int, std::vector<int> const&)> const cvariant6 = of;
-  EXPECT_EQ(100 + 6 * 20, cvariant6(20, {}));
-  EXPECT_EQ(100 + 6 * 20, cvariant6(20, {1, 2, 3}));
     
-      for (unsigned i = 0; i < iter; ++i) {
-    FB_LOG_EVERY_MS(INFO, -1) << 'every -1ms';
-  }
-    
-    vector<detail::BenchmarkResult> resultsFromFile(const std::string& filename) {
-  string content;
-  readFile(filename.c_str(), content);
-  vector<detail::BenchmarkResult> ret;
-  benchmarkResultsFromDynamic(parseJson(content), ret);
-  return ret;
+    {  llvm_unreachable('invalid class');
 }
     
-    #include <folly/GLog.h>
+    static bool CaseInsensitiveEqual(const std::string &s1, const std::string &s2)
+{
+    if (s1.size() != s2.size()) return false;
+    for (size_t i = 0; i < s1.size(); ++i) {
+        char c1 = s1[i];
+        if (c1 >= 'A' && c1 <= 'Z') c1 -= ('A' - 'a');
+        char c2 = s2[i];
+        if (c2 >= 'A' && c2 <= 'Z') c2 -= ('A' - 'a');
+        if (c1 != c2) return false;
+    }
+    return true;
+}
     
+        // Special handling for null case
+    // (needed because string comparison reads the null as end-of-string)
+    BOOST_TEST_MESSAGE(std::string('CheckParseTorReplyMapping(Null=\'\\0\')'));
+    auto ret = ParseTorReplyMapping('Null=\'\\0\'');
+    BOOST_CHECK_EQUAL(ret.size(), 1U);
+    auto r_it = ret.begin();
+    BOOST_CHECK_EQUAL(r_it->first, 'Null');
+    BOOST_CHECK_EQUAL(r_it->second.size(), 1U);
+    BOOST_CHECK_EQUAL(r_it->second[0], '\0');
     
-    {} // namespace folly
-
+    bool ParseInt64(const std::string& str, int64_t *out)
+{
+    if (!ParsePrechecks(str))
+        return false;
+    char *endp = NULL;
+    errno = 0; // strtoll will not set errno if valid
+    long long int n = strtoll(str.c_str(), &endp, 10);
+    if(out) *out = (int64_t)n;
+    // Note that strtoll returns a *long long int*, so even if strtol doesn't report a over/underflow
+    // we still have to check that the returned value is within the range of an *int64_t*.
+    return endp && *endp == 0 && !errno &&
+        n >= std::numeric_limits<int64_t>::min() &&
+        n <= std::numeric_limits<int64_t>::max();
+}
+    
+      inline int offset(const int n, const int c = 0, const int h = 0,
+      const int w = 0) const {
+    CHECK_GE(n, 0);
+    CHECK_LE(n, num());
+    CHECK_GE(channels(), 0);
+    CHECK_LE(c, channels());
+    CHECK_GE(height(), 0);
+    CHECK_LE(h, height());
+    CHECK_GE(width(), 0);
+    CHECK_LE(w, width());
+    return ((n * channels() + c) * height() + h) * width() + w;
+  }
     
       /**
-   * Shortcut to get a secure random value of integral type.
+   * @brief Using the CPU device, compute the gradients for any parameters and
+   *        for the bottom blobs if propagate_down is true.
    */
-  template <class T>
-  static typename std::enable_if<
-      std::is_integral<T>::value && !std::is_same<T, bool>::value,
-      T>::type
-  secureRandom() {
-    T val;
-    secureRandom(&val, sizeof(val));
-    return val;
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down,
+      const vector<Blob<Dtype>*>& bottom) = 0;
+  /**
+   * @brief Using the GPU device, compute the gradients for any parameters and
+   *        for the bottom blobs if propagate_down is true.
+   *        Fall back to Backward_cpu() if unavailable.
+   */
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down,
+      const vector<Blob<Dtype>*>& bottom) {
+    // LOG(WARNING) << 'Using CPU code as backup.';
+    Backward_cpu(top, propagate_down, bottom);
   }
     
-      void addWithPriority(Func f, int8_t priority) override {
-    executor_->addWithPriority(wrapFunc(std::move(f)), priority);
+    #include <vector>
+    
+    /**
+ * @brief Normalizes the input to have 0-mean and/or unit (1) variance across
+ *        the batch.
+ *
+ * This layer computes Batch Normalization as described in [1]. For each channel
+ * in the data (i.e. axis 1), it subtracts the mean and divides by the variance,
+ * where both statistics are computed across both spatial dimensions and across
+ * the different examples in the batch.
+ *
+ * By default, during training time, the network is computing global
+ * mean/variance statistics via a running average, which is then used at test
+ * time to allow deterministic outputs for each input. You can manually toggle
+ * whether the network is accumulating or using the statistics via the
+ * use_global_stats option. For reference, these statistics are kept in the
+ * layer's three blobs: (0) mean, (1) variance, and (2) moving average factor.
+ *
+ * Note that the original paper also included a per-channel learned bias and
+ * scaling factor. To implement this in Caffe, define a `ScaleLayer` configured
+ * with `bias_term: true` after each `BatchNormLayer` to handle both the bias
+ * and scaling factor.
+ *
+ * [1] S. Ioffe and C. Szegedy, 'Batch Normalization: Accelerating Deep Network
+ *     Training by Reducing Internal Covariate Shift.' arXiv preprint
+ *     arXiv:1502.03167 (2015).
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class BatchNormLayer : public Layer<Dtype> {
+ public:
+  explicit BatchNormLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    
+    { private:
+  struct pair_sort_first {
+    bool operator()(const std::pair<int, int> &left,
+                    const std::pair<int, int> &right) {
+      return left.first < right.first;
+    }
+  };
+  void check_batch_reindex(int initial_num, int final_num,
+                           const Dtype* ridx_data);
+};
+    
+      vector<cudnnTensorDescriptor_t> bottom_descs_, top_descs_;
+  cudnnTensorDescriptor_t bias_desc_;
+  cudnnFilterDescriptor_t filter_desc_;
+  vector<cudnnConvolutionDescriptor_t> conv_descs_;
+  int bottom_offset_, top_offset_, bias_offset_;
+    
+    namespace grpc {
+    }
+    
+    inline absl::string_view GetMethod(const grpc_slice* path) {
+  if (GRPC_SLICE_IS_EMPTY(*path)) {
+    return '';
   }
+  // Check for leading '/' and trim it if present.
+  return absl::StripPrefix(absl::string_view(reinterpret_cast<const char*>(
+                                                 GRPC_SLICE_START_PTR(*path)),
+                                             GRPC_SLICE_LENGTH(*path)),
+                           '/');
+}
+    
+    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
+constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
+constexpr size_t TraceContextEncoding::kVersionIdSize;
+constexpr size_t TraceContextEncoding::kFieldIdSize;
+constexpr size_t TraceContextEncoding::kVersionIdOffset;
+constexpr size_t TraceContextEncoding::kVersionId;
+    
+      const protobuf::Descriptor* desc =
+      descriptor_pool_->FindMessageTypeByName(type);
+  if (desc == nullptr) {
+    return Status(StatusCode::NOT_FOUND, 'Type not found.');
+  }
+    
+        // print values
+    std::cout << object << '\n';
+    std::cout << null << '\n';
