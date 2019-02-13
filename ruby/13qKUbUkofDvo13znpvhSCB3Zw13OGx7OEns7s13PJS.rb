d4c9c18d2bd05f121b02f17415c17b4529d4786c
SUNsung <weatherius@gmail.com>
2019-02-13 06:01:33 +0200

    Rewrite [13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb] File in: 2019-02-13 06:01:33

--- ruby/13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb
+++ ruby/13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb
@@ -1,13 +1,73 @@
 
         
-        class SinatraWorker
-  include Sidekiq::Worker
+          </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-        def initialize(options)
-      @strictly_ordered_queues = !!options[:strict]
-      @queues = options[:queues].map { |q| 'queue:#{q}' }
-      if @strictly_ordered_queues
-        @queues = @queues.uniq
-        @queues << TIMEOUT
+    #
+    
+    #
+def run_in_shell(*args)
+  p, output = Jekyll::Utils::Exec.run(*args)
+    
+    module Jekyll
+  module Commands
+    class NewTheme < Jekyll::Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:'new-theme') do |c|
+            c.syntax 'new-theme NAME'
+            c.description 'Creates a new Jekyll theme scaffold'
+            c.option 'code_of_conduct', \
+                     '-c', '--code-of-conduct', \
+                     'Include a Code of Conduct. (defaults to false)'
+    
+              # This is too noisy even for --verbose, but uncomment if you need it for
+          # a specific WebSockets issue.  Adding ?LR-verbose=true onto the URL will
+          # enable logging on the client side.
+          # em_opts[:debug] = true
+    
+            def initialize(config)
+          @main_fallback_highlighter = config['highlighter'] || 'rouge'
+          @config = config['kramdown'] || {}
+          @highlighter = nil
+          setup
+        end
+    
+    def scope(path)
+  File.join(File.dirname(__FILE__), path)
+end
+    
+        # The environment of the caller of this environment's mixin or function.
+    # @return {Environment?}
+    def caller
+      @caller || (@parent && @parent.caller)
+    end
+    
+          # @see Base#find_relative
+      def find_relative(name, base, options)
+        _find(File.dirname(base), name, options)
+      end
+    
+          # Custom destructuring method. This is used to normalize the branches
+      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
+      #
+      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
+      def node_parts
+        [self, self]
       end
     end
+  end
+end
