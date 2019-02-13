7a085c32a56925bdc8038d13eff8f6513e00e197
SUNsung <weatherius@gmail.com>
2019-02-13 14:03:39 +0200

    Rewrite [07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp] File in: 2019-02-13 14:03:38

--- cccccc/07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp
+++ cccccc/07QSE0k3QsAo07b8ztzVWfWoU07qhee4gS14iw07jhI.cpp
@@ -1,269 +1,29 @@
-#ifndef UINT32_MAX
-    #define UINT32_MAX (4294967295U)
-#endif
-    
-        void operator() (const typename VecTraits<s32>::vec64 & v_src0,
-                     const typename VecTraits<s32>::vec64 & v_src1,
-                     typename VecTraits<s32>::vec64 & v_dst) const
-    {
-        float32x2_t vs1 = vcvt_f32_s32(v_src0);
-        float32x2_t vs2 = vcvt_f32_s32(v_src1);
-    }
-    
-        uint16x8_t tprev = vdupq_n_u16(0x0);
-    uint16x8_t tcurr = tprev;
-    uint16x8_t tnext = tprev;
-    uint16x8_t t0, t1, t2;
-    if(cn == 1)
-    {
-        for( size_t y = 0; y < size.height; y++ )
-        {
-            const u8* srow0;
-            const u8* srow1 = internal::getRowPtr(srcBase, srcStride, y);
-            const u8* srow2;
-            u8* drow = internal::getRowPtr(dstBase, dstStride, y);
-            if (borderType == BORDER_MODE_REFLECT101) {
-                srow0 = internal::getRowPtr(srcBase, srcStride, y > 0 ? y-1 : 1);
-                srow2 = internal::getRowPtr(srcBase, srcStride, y < size.height-1 ? y+1 : size.height-2);
-            } else  if (borderType == BORDER_MODE_CONSTANT) {
-                srow0 = y > 0 ? internal::getRowPtr(srcBase, srcStride, y-1) : tmp;
-                srow2 =  y < size.height-1 ? internal::getRowPtr(srcBase, srcStride, y+1) : tmp;
-            } else { // BORDER_MODE_REFLECT || BORDER_MODE_REPLICATE
-                srow0 = internal::getRowPtr(srcBase, srcStride, y > 0 ? y-1 : 0);
-                srow2 = internal::getRowPtr(srcBase, srcStride, y < size.height-1 ? y+1 : size.height-1);
-            }
-    }
-    }
-    
-        // now track the edges (hysteresis thresholding)
-    while (stack_top > stack_bottom)
-    {
-        u8* m;
-        if ((size_t)(stack_top - stack_bottom) + 8u > maxsize)
-        {
-            ptrdiff_t sz = (ptrdiff_t)(stack_top - stack_bottom);
-            maxsize = maxsize * 3/2;
-            stack.resize(maxsize);
-            stack_bottom = &stack[0];
-            stack_top = stack_bottom + sz;
-        }
-    }
-    
-    #define COMBINE64(sgn,n) void combine##n(const Size2D &_size                                                \
-                                               FILL_LINES##n(FARG, sgn##64),                                \
-                                               sgn##64 * dstBase, ptrdiff_t dstStride)                      \
-{                                                                                                           \
-    internal::assertSupportedConfiguration();                                                               \
-    Size2D size(_size);                                                                                     \
-    if (CONTSRC##n                                                                                          \
-        dstStride == (ptrdiff_t)(size.width))                                                               \
-    {                                                                                                       \
-        size.width *= size.height;                                                                          \
-        size.height = 1;                                                                                    \
-    }                                                                                                       \
-    typedef internal::VecTraits<sgn##64, n>::vec64 vec64;                                                   \
-                                                                                                            \
-    for (size_t i = 0u; i < size.height; ++i)                                                               \
-    {                                                                                                       \
-        FILL_LINES##n(VROW, sgn##64)                                                                        \
-        sgn##64 * dst = internal::getRowPtr(dstBase, dstStride, i);                                         \
-        size_t sj = 0u, dj = 0u;                                                                            \
-                                                                                                            \
-        for (; sj < size.width; ++sj, dj += n)                                                              \
-        {                                                                                                   \
-            vec64 v_dst;                                                                                    \
-            FILL_LINES##n(VLD1, sgn##64)                                                                    \
-            vst##n##_##sgn##64(dst + dj, v_dst);                                                            \
-            /*FILL_LINES##n(SLD, sgn##64)*/                                                                 \
-        }                                                                                                   \
-    }                                                                                                       \
-}
-    
-    void assertSupportedConfiguration(bool parametersSupported)
-{
-    if (!isSupportedConfiguration()) {
-        std::cerr << 'internal error: attempted to use an unavailable function' << std::endl;
-        std::abort();
-    }
-    }
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(f32, u16, 8,
-     register float32x4_t vhalf asm ('q0') = vdupq_n_f32(0.5f);,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d2-d3}, [%[src]]                               \n\t'
-             'vadd.f32 q2, q1, q0                                     \n\t'
-             'vcvt.u32.f32 q3, q2                                     \n\t'
-             'vqmovn.u32 d8, q3                                       \n\t'
-             'vst1.16 {d8}, [%[dst]]                                  \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i),
-               [dst] 'r' (_dst + i),
-               'w' (vhalf)
-             : 'd2','d3','d4','d5','d6','d7','d8'
-         );
-         __asm__ (
-             'vld1.32 {d2-d3}, [%[src]]                               \n\t'
-             'vadd.f32 q2, q1, q0                                     \n\t'
-             'vcvt.u32.f32 q3, q2                                     \n\t'
-             'vqmovn.u32 d8, q3                                       \n\t'
-             'vst1.16 {d8}, [%[dst]]                                  \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i + 4),
-               'w' (vhalf)
-             : 'd2','d3','d4','d5','d6','d7','d8'
-         );
-     }
-})
-#else
-CVT_FUNC(f32, u16, 8,
-     float32x4_t vhalf = vdupq_n_f32(0.5f);,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         float32x4_t vline_f32 = vld1q_f32(_src + i);
-    }
-    }
-    
-    
-    {        for (; i < size.width; i++)
-            result += (src[i] != 0)?1:0;
-        if (result < 0)//saturate in case of overflow ~ 8GB of non-zeros...
-        {
-            return 0x7fFFffFF;
-        }
-    }
-    return result;
-#else
-    (void)_size;
-    (void)srcBase;
-    (void)srcStride;
-    
-                for( ; j < (s32)size.width - 3; j++, ptr++ )
-            {
-                s32 v = ptr[0];
-                const u8* tab = &threshold_tab[0] - v + 255;
-                s32 d = tab[ptr[pixel[0]]] | tab[ptr[pixel[8]]];
-    }
-    
-    void gaussianBlur5x5(const Size2D &size, s32 cn,
-                     const s32 * srcBase, ptrdiff_t srcStride,
-                     s32 * dstBase, ptrdiff_t dstStride,
-                     BORDER_MODE borderType, s32 borderValue, Margin borderMargin)
-{
-    internal::assertSupportedConfiguration(isGaussianBlur5x5Supported(size, cn, borderType));
-#ifdef CAROTENE_NEON
-    size_t colsn = size.width * cn;
-    }
-    
-    std::string tmppath() {
-  // TMPFILE is for manual test execution during which the user will specify
-  // the full temp file path using the environmental variable TMPFILE
-  const char* tmpfile = getenv('TMPFILE');
-  if (tmpfile) {
-    return std::string(tmpfile);
-  }
-    }
-    
-    SHOULD_NOT_DO_GRADIENT(EnforceFinite);
-    
-    // ##########################################################
-    
-    namespace caffe2 {
-    }
-    
-    
-    {          return out;
-        })
-    .Input(0, 'X', '4-tensor in NCHW or NHWC.')
-    .Output(
-        0,
-        'Y',
-        '4-tensor. For NCHW: N x (C x kH x kW) x outH x outW.'
-        'For NHWC: N x outH x outW x (kH x kW x C');
-    
-    	// Now set the registry keys
-	HKEY root = GetRootKey(opt);
-    
-        if (U_FAILURE(status)) {return FALSE;}
-    
-    class FCDUTF8NFDIterator : public NFDIterator {
-public:
-    FCDUTF8NFDIterator(const CollationData *data, const uint8_t *text, int32_t textLength)
-            : u8ci(data, FALSE, text, 0, textLength) {}
-protected:
-    virtual UChar32 nextRawCodePoint() {
-        UErrorCode errorCode = U_ZERO_ERROR;
-        return u8ci.nextCodePoint(errorCode);
-    }
-private:
-    FCDUTF8CollationIterator u8ci;
-};
-    
-    U_NAMESPACE_END
-    
-    #endif /* #if !UCONFIG_NO_COLLATION */
 
+        
+            bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     
-            // Don't need suffix processing here (as in number processing at the beginning of the function);
-        // the new fields being handled as numeric values (month, weekdays, quarters) should not have suffixes.
+    static int const                    NUM_BACK_BUFFERS = 3;
+static ID3D12Device*                g_pd3dDevice = NULL;
+static ID3D12DescriptorHeap*        g_pd3dRtvDescHeap = NULL;
+static ID3D12DescriptorHeap*        g_pd3dSrvDescHeap = NULL;
+static ID3D12CommandQueue*          g_pd3dCommandQueue = NULL;
+static ID3D12GraphicsCommandList*   g_pd3dCommandList = NULL;
+static ID3D12Fence*                 g_fence = NULL;
+static HANDLE                       g_fenceEvent = NULL;
+static UINT64                       g_fenceLastSignaledValue = 0;
+static IDXGISwapChain3*             g_pSwapChain = NULL;
+static HANDLE                       g_hSwapChainWaitableObject = NULL;
+static ID3D12Resource*              g_mainRenderTargetResource[NUM_BACK_BUFFERS] = {};
+static D3D12_CPU_DESCRIPTOR_HANDLE  g_mainRenderTargetDescriptor[NUM_BACK_BUFFERS] = {};
     
-    /**
- * Implement UnicodeMatcher
- */
-void StringMatcher::addMatchSetTo(UnicodeSet& toUnionTo) const {
-    UChar32 ch;
-    for (int32_t i=0; i<pattern.length(); i+=U16_LENGTH(ch)) {
-        ch = pattern.char32At(i);
-        const UnicodeMatcher* matcher = data->lookupMatcher(ch);
-        if (matcher == NULL) {
-            toUnionTo.add(ch);
-        } else {
-            matcher->addMatchSetTo(toUnionTo);
-        }
+        // the literals are everything from lit_start to q
+    *pending_literals = (q - lit_start);
+    
+            {
+            static char str0[128] = 'Hello, world!';
+            static int i0 = 123;
+            ImGui::InputText('input text', str0, IM_ARRAYSIZE(str0));
+            ImGui::SameLine(); ShowHelpMarker('USER:\nHold SHIFT or use mouse to select text.\n' 'CTRL+Left/Right to word jump.\n' 'CTRL+A or double-click to select all.\n' 'CTRL+X,CTRL+C,CTRL+V clipboard.\n' 'CTRL+Z,CTRL+Y undo/redo.\n' 'ESCAPE to revert.\n\nPROGRAMMER:\nYou can use the ImGuiInputTextFlags_CallbackResize facility if you need to wire InputText() to a dynamic string type. See misc/cpp/imgui_stdlib.h for an example (this is not demonstrated in imgui_demo.cpp).');
     }
-}
-    
-        /**
-     * Implement UnicodeFunctor
-     * @return a copy of the object.
-     */
-    virtual UnicodeFunctor* clone() const;
-    
-    
-    {  s = postCarve(uploadPath);
-  if (!s.ok()) {
-    VLOG(1) << 'Failed to post carve: ' << s.getMessage();
-    updateCarveValue(carveGuid_, 'status', 'DATA POST FAILED');
-    return;
-  }
-};
-    
-    TEST_F(OptionsConfigParserPluginTests, test_unknown_option) {
-  Config c;
-  std::map<std::string, std::string> update;
-    }
-    
-      boost::system::error_code ec;
-  if (!fs::is_regular_file(value, ec) || ec.value() != errc::success) {
-    return Status(1, value + ' is not a valid path');
-  }
-    
-    
-    {
-    {      // Re-encode the config key into JSON.
-      auto it = tree.doc().FindMember('config');
-      config['tls_plugin'] =
-          unescapeUnicode(it != tree.doc().MemberEnd() && it->value.IsString()
-                              ? it->value.GetString()
-                              : '');
-    } else {
-      config['tls_plugin'] = json;
-    }
-  }
