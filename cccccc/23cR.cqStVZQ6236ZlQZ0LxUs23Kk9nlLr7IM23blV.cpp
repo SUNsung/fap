239a3e974feeb16ee2604effa0dd3e5372473fd1
SUNsung <weatherius@gmail.com>
2019-02-13 17:52:58 +0200

    Rewrite [23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp] File in: 2019-02-13 17:52:57

--- cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
+++ cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
@@ -1,261 +1,150 @@
 
         
-        
-    {}  // namespace grpc
-
+        string DataTypeToPython(DataType dtype, const string& dtype_module);
     
-    #ifndef GRPC_CUSTOM_DEFAULT_THREAD_POOL
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-    #endif  // GPR_LINUX
-
+    REGISTER_OP('Ackermann')
+    .Output('ackermann: string')
+    .Doc(R'doc(
+Output a fact about the ackermann function.
+)doc');
     
-    		inline float GetError(void)
-		{
-			return m_pencoding->GetError();
-		}
-    
-    								float fPixelError;
-    
-    
-/** 16x32 multiply, followed by a 15-bit shift right and 32-bit add.
-    b must fit in 31 bits.
-    Result fits in 32 bits. */
-#undef MAC16_32_Q15
-#define MAC16_32_Q15(c, a, b) ADD32(c, MULT16_32_Q15(a, b))
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
     
-/** 16x32 multiplication, followed by a 15-bit shift right. Results fits in 32 bits */
-#undef MULT16_32_Q15
-static OPUS_INLINE opus_val32 MULT16_32_Q15_armv5e(opus_val16 a, opus_val32 b)
-{
-  int res;
-  __asm__(
-      '#MULT16_32_Q15\n\t'
-      'smulwb %0, %1, %2\n\t'
-      : '=r'(res)
-      : 'r'(b), 'r'(a)
-  );
-  return res<<1;
-}
-#define MULT16_32_Q15(a, b) (MULT16_32_Q15_armv5e(a, b))
+    {}  // namespace tensorflow
     
-    /*!
- * Initialize/reset the resampler state for a given pair of input/output sampling rates
-*/
-opus_int silk_resampler_init(
-    silk_resampler_state_struct *S,                 /* I/O  Resampler state                                             */
-    opus_int32                  Fs_Hz_in,           /* I    Input sampling rate (Hz)                                    */
-    opus_int32                  Fs_Hz_out,          /* I    Output sampling rate (Hz)                                   */
-    opus_int                    forEnc              /* I    If 1: encoder; if 0: decoder                                */
-);
+    // Safe container for an owned PyObject. On destruction, the reference count of
+// the contained object will be decremented.
+using Safe_PyObjectPtr = std::unique_ptr<PyObject, detail::PyDecrefDeleter>;
+Safe_PyObjectPtr make_safe(PyObject* o);
     
-    /**!
- * \brief Workaround for missing functions in ::caffe::Blob
- * \warning Do not add or override any virtual functions in this class
- * @tparam Dtype
- */
-template<class Dtype>
-class CaffeBlobFriend : public ::caffe::Blob<Dtype> {
- public:
-  inline void set_cpu_diff(Dtype* diff) {
-    CHECK(diff);
-    this->diff_->set_cpu_data(diff);
+      // Attempt to get the next record at 'current_offset()'. Populates status
+  // with OK on success, OUT_OF_RANGE for end of file, DATA_LOSS for some
+  // kinds of truncated reads, or another code for other errors
+  // (e.g., filesystem errors).
+  void GetNext(TF_Status* status);
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+      // Creates a 3d FFT plan.
+  virtual std::unique_ptr<Plan> Create3dPlan(Stream *stream, uint64 num_x,
+                                             uint64 num_y, uint64 num_z,
+                                             Type type, bool in_place_fft) = 0;
+    
+      /**
+   * @brief Infers the shape of transformed_blob will have when
+   *    the transformation is applied to the data.
+   *
+   * @param datum
+   *    Datum containing the data to be transformed.
+   */
+  vector<int> InferBlobShape(const Datum& datum);
+  /**
+   * @brief Infers the shape of transformed_blob will have when
+   *    the transformation is applied to the data.
+   *    It uses the first element to infer the shape of the blob.
+   *
+   * @param datum_vector
+   *    A vector of Datum containing the data to be transformed.
+   */
+  vector<int> InferBlobShape(const vector<Datum> & datum_vector);
+  /**
+   * @brief Infers the shape of transformed_blob will have when
+   *    the transformation is applied to the data.
+   *    It uses the first element to infer the shape of the blob.
+   *
+   * @param mat_vector
+   *    A vector of Mat containing the data to be transformed.
+   */
+#ifdef USE_OPENCV
+  vector<int> InferBlobShape(const vector<cv::Mat> & mat_vector);
+  /**
+   * @brief Infers the shape of transformed_blob will have when
+   *    the transformation is applied to the data.
+   *
+   * @param cv_img
+   *    cv::Mat containing the data to be transformed.
+   */
+  vector<int> InferBlobShape(const cv::Mat& cv_img);
+#endif  // USE_OPENCV
+    
+    // Serialize LayerParameter to protocol buffer
+template <typename Dtype>
+void Layer<Dtype>::ToProto(LayerParameter* param, bool write_diff) {
+  param->Clear();
+  param->CopyFrom(layer_param_);
+  param->clear_blobs();
+  for (int i = 0; i < blobs_.size(); ++i) {
+    blobs_[i]->ToProto(param->add_blobs(), write_diff);
   }
-    }
+}
     
-     protected:
-  IF_CHECK_TIMING(
-    static uint64_t GetTickCountMS() {
-      struct timeval tv;
-      gettimeofday(&tv, 0);
-      return uint64_t( tv.tv_sec ) * 1000 + tv.tv_usec / 1000;
-    }
-  )
+      int label_axis_, outer_num_, inner_num_;
     
-    
-    {
-    {
-    {  /*!
-   * \brief Worker threads.
-   */
-  std::vector<std::thread> worker_threads_;
-  /*!
-   * \brief Startup synchronization objects
-   */
-  std::list<std::shared_ptr<dmlc::ManualEvent>> ready_events_;
-  /*!
-   * \brief Disallow default construction.
-   */
-  ThreadPool() = delete;
-  /*!
-   * \brief Disallow copy construction and assignment.
-   */
-  DISALLOW_COPY_AND_ASSIGN(ThreadPool);
-};
-}  // namespace engine
-}  // namespace mxnet
-#endif  // MXNET_ENGINE_THREAD_POOL_H_
+    #endif  // CAFFE_ARGMAX_LAYER_HPP_
 
     
-    RemoveTransliterator::RemoveTransliterator() : Transliterator(UnicodeString(TRUE, ::CURR_ID, -1), 0) {}
+    
+    {}  // namespace caffe
     
     
-    {
-    {        if (matchlength > 0) {
-            // if matchlength is 0 we are at the start of the iteration
-            if (m_search_->isOverlap) {
-                offset ++;
-            }
-            else {
-                offset += matchlength;
-            }
-        }
-        return handleNext(offset, status);
-    }
-    return USEARCH_DONE;
-}
+    {}  // namespace caffe
     
-    /**
- * Given a set of encoded rules in startDay and startDayOfMonth, decode
- * them and set the startMode appropriately.  Do the same for endDay and
- * endDayOfMonth.  Upon entry, the day of week variables may be zero or
- * negative, in order to indicate special modes.  The day of month
- * variables may also be negative.  Upon exit, the mode variables will be
- * set, and the day of week and day of month variables will be positive.
- * This method also recognizes a startDay or endDay of zero as indicating
- * no DST.
- */
-void 
-SimpleTimeZone::decodeRules(UErrorCode& status)
-{
-    decodeStartRule(status);
-    decodeEndRule(status);
-}
+      bool handles_setup_;
+  cudnnHandle_t* handle_;
+  cudaStream_t*  stream_;
     
-    #include 'unicode/udat.h'
+    #endif  // CAFFE_CUDNN_LRN_LAYER_HPP_
+
     
-            for (oOutput=0; oOutput<output.length(); ) {
-            if (oOutput == cursorPos) {
-                // Record the position of the cursor
-                newStart = destLimit - destStart; // relative to start
-            }
-            UChar32 c = output.char32At(oOutput);
-            UnicodeReplacer* r = data->lookupReplacer(c);
-            if (r == NULL) {
-                // Accumulate straight (non-segment) text.
-                buf.append(c);
-            } else {
-                isComplex = TRUE;
-    }
-    }
+    int os_detect_blobs(const GenericVector<int>* allowed_scripts,
+                    BLOBNBOX_CLIST* blob_list,
+                    OSResults* osr,
+                    tesseract::Tesseract* tess);
     
-            ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-        ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-        ImGui::Checkbox('Another Window', &show_another_window);
+      /**
+   * Moves the iterator to the beginning of the text line.
+   * This class implements this functionality by moving it to the zero indexed
+   * blob of the first (leftmost) word of the row.
+   */
+  virtual void RestartRow();
     
-    static ImGui_ImplVulkanH_WindowData g_WindowData;
+      // blow away the copied chopped_word, as we want to work with
+  // the blobs from the input chopped_word so seam_arrays can be merged.
+  TWERD *chopped = word->chopped_word;
+  TWERD *chopped2 = new TWERD;
+  chopped2->blobs.reserve(chopped->NumBlobs() - split_pt);
+  for (int i = split_pt; i < chopped->NumBlobs(); ++i) {
+    chopped2->blobs.push_back(chopped->blobs[i]);
+  }
+  chopped->blobs.truncate(split_pt);
+  word->chopped_word = nullptr;
+  delete word2->chopped_word;
+  word2->chopped_word = nullptr;
     
-        // Main loop
-    MSG msg;
-    ZeroMemory(&msg, sizeof(msg));
-    while (msg.message != WM_QUIT)
-    {
-        // Poll and handle messages (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-            continue;
-        }
-    }
-    
-        const GLchar* vertex_shader_glsl_300_es =
-        'precision mediump float;\n'
-        'layout (location = 0) in vec2 Position;\n'
-        'layout (location = 1) in vec2 UV;\n'
-        'layout (location = 2) in vec4 Color;\n'
-        'uniform mat4 ProjMtx;\n'
-        'out vec2 Frag_UV;\n'
-        'out vec4 Frag_Color;\n'
-        'void main()\n'
-        '{\n'
-        '    Frag_UV = UV;\n'
-        '    Frag_Color = Color;\n'
-        '    gl_Position = ProjMtx * vec4(Position.xy,0,1);\n'
-        '}\n';
+    // Reads all boxes from the given filename.
+// Reads a specific target_page number if >= 0, or all pages otherwise.
+// Skips blanks if skip_blanks is true.
+// The UTF-8 label of the box is put in texts, and the full box definition as
+// a string is put in box_texts, with the corresponding page number in pages.
+// Each of the output vectors is optional (may be nullptr).
+// Returns false if no boxes are found.
+bool ReadAllBoxes(int target_page, bool skip_blanks, const STRING& filename,
+                  GenericVector<TBOX>* boxes,
+                  GenericVector<STRING>* texts,
+                  GenericVector<STRING>* box_texts,
+                  GenericVector<int>* pages);
     
     
-    {
-    {        throw invalid_argument('the input has no solution');
-    }
-};
-    
-    
-    {
-    {        return true;
-    }
-};
-    
-    
-    {
-    {        int index = 0;
-        for(int i = 0 ; i < count[0] ; i ++)
-            nums[index++] = 0;
-        for(int i = 0 ; i < count[1] ; i ++)
-            nums[index++] = 1;
-        for(int i = 0 ; i < count[2] ; i ++)
-            nums[index++] = 2;
-    }
-};
-    
-    // Three Way Quick Sort
-// Time Complexity: O(n)
-// Space Complexity: O(1)
-class Solution {
-public:
-    void sortColors(vector<int> &nums) {
-    }
-    }
-    
-            for(ListNode* cur = head ; cur != NULL ;){
-            if(cur->val < x){
-                prev1->next = cur;
-                cur = cur->next;
-                prev1 = prev1->next;
-                prev1->next = NULL;
-            }
-            else{
-                prev2->next = cur;
-                cur = cur->next;
-                prev2 = prev2->next;
-                prev2->next = NULL;
-            }
-        }
-    
-    
-    {        vector<int> res;
-        __inorderTraversal(root, res);
-        return res;
-    }
-    
-    
-// Another Classic Non-Recursive algorithm for inorder traversal
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-    }
-    
-            queue<pair<TreeNode*,int>> q;
-        q.push(make_pair(root, 0));
-    
-    /// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+    {}  // namespace tesseract
