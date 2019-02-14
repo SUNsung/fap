39994700d42ca4257c4cf610456a616e83c7cb7f
SUNsung <weatherius@gmail.com>
2019-02-14 02:41:25 +0200

    Rewrite [25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb] File in: 2019-02-14 02:41:24

--- ruby/25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb
+++ ruby/25fpI5Q2J4ih2254I7ZtAyNF6.25nklN9LYIPho25CcX.rb
@@ -1,80 +1,73 @@
 
         
-          describe '#style_colors' do
-    it 'returns a css style-formated version of the scenario foreground and background colors' do
-      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
-    end
+              self.current_lane = lane.to_sym
+      self.current_platform = (platform ? platform.to_sym : nil)
     
-      describe '#interpolate_jsonpaths' do
-    let(:payload) { { :there => { :world => 'WORLD' }, :works => 'should work' } }
+            cmd << ['-am #{message.shellescape}']
+        cmd << '--force' if options[:force]
+        cmd << '-s' if options[:sign]
+        cmd << tag.shellescape
+        cmd << options[:commit].to_s if options[:commit]
     
-      it 'accepts a Bignum as a seed' do
-    srand(0x12345678901234567890)
-    srand.should == 0x12345678901234567890
-  end
+          it 'adds clean_output param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            clean_output: true
+          )
+        end').runner.execute(:test)
     
-          it 'raises ArgumentError if passed negative value' do
-        -> { warn '', uplevel: -2 }.should raise_error(ArgumentError)
-        -> { warn '', uplevel: -100 }.should raise_error(ArgumentError)
+            expect(result).to \
+          eq('carthage update TestDependency')
       end
     
-    
-  def self.create_rakp_hmac_sha1_salt(con_sid, bmc_sid, con_rid, bmc_rid, bmc_gid, auth_level, username)
-    con_sid +
-    bmc_sid +
-    con_rid +
-    bmc_rid +
-    bmc_gid +
-    [ auth_level ].pack('C') +
-    [ username.length ].pack('C') +
-    username
-  end
-    
-            # Sends a kerberos request, and reads the response through the connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [<Rex::Proto::Kerberos::Model::KrbError, Rex::Proto::Kerberos::Model::KdcResponse>] The kerberos message
-        # @raise [RuntimeError] if the transport protocol is unknown or the response can't be parsed.
-        # @raise [NotImplementedError] if the transport protocol isn't supported
-        def send_recv(req)
-          send_request(req)
-          res = recv_response
-    
-              # Encodes the end_time field
-          #
-          # @return [String]
-          def encode_end_time
-            [end_time].pack('N')
-          end
-    
-                res = checksum + encrypted
-            res
-          end
+          it 'yields any error result' do
+        expect_command('ls', '-la', exitstatus: 1)
+        Fastlane::Actions.sh('ls', '-la') do |status, result|
+          expect(status.exitstatus).to eq(1)
+          expect(result).to be_empty
         end
       end
+    
+          @key = key
+      @env_name = env_name
+      @description = description
+      @short_option = short_option
+      @default_value = default_value
+      @default_value_dynamic = default_value_dynamic
+      @verify_block = verify_block
+      @is_string = is_string
+      @data_type = type
+      @data_type = String if type == :shell_string
+      @optional = optional
+      @conflicting_options = conflicting_options
+      @conflict_block = conflict_block
+      @deprecated = deprecated
+      @sensitive = sensitive
+      @code_gen_sensitive = code_gen_sensitive || sensitive
+      @allow_shell_conversion = (type == :shell_string)
+      @display_in_shell = display_in_shell
+      @skip_type_validation = skip_type_validation # sometimes we allow multiple types which causes type validation failures, e.g.: export_options in gym
+    
+            if mod
+          if name == 'Index'
+            return slug.split('/')[1..-2].join('/')
+          elsif name == 'Angular'
+            return slug.split('/').last.split('-').first
+          end
+        end
+    
+        it 'generates the contacts_json fixture', :fixture => true do
+      json = bob.contacts.map { |c|
+               ContactPresenter.new(c, bob).full_hash_with_person
+             }.to_json
+      save_fixture(json, 'contacts_json')
     end
   end
 end
-    
-              # Rex::Proto::Kerberos::Model::AuthorizationData decoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def decode(input)
-            raise ::NotImplementedError, 'Authorization Data decoding not supported'
-          end
-    
-            end
-      end
-    end
-  end
-end
-    
-              # Rex::Proto::Kerberos::Model::KdcResponse encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'KdcResponse encoding not supported'
-          end
+
     
         it 'generates a jasmine fixture', fixture: true do
       session[:mobile_view] = true
@@ -85,12 +78,44 @@ end
 end
 
     
-          get :update, params: {id: note.id, set_unread: 'false'}, format: :json
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
+    end
+  end
+end
+
     
-        shared_examples 'on a visible post' do
-      it 'creates the participation' do
-        post :create, params: {post_id: @post.id}
-        expect(alice.participations.where(:target_id => @post.id)).to exist
-        expect(response.code).to eq('201')
+        it 'supports a limit per_page parameter' do
+      2.times { FactoryGirl.create(:notification, :recipient => alice, :target => @post) }
+      get :index, params: {per_page: 2}
+      expect(assigns[:notifications].count).to eq(2)
+    end
+    
+              expect {
+            get :create, params: valid_params.merge(invite: {token: code.token})
+          }.to change { code.reload.count }.by(-1)
+        end
+    
+          it 'returns an empty array for a post visible to the user' do
+        sign_in(bob, scope: :user)
+        get :index, params: {post_id: @post.id}, format: :json
+        expect(JSON.parse(response.body)).to eq([])
       end
     end
+    
+          expect('.border-style-implied-left').to have_rule(rule)
+    end
+  end
+    
+          expect('.margin-all').to have_rule(rule)
+    end
+  end
+    
+          expect('.padding-alternate').to have_rule(rule)
+    end
+  end
