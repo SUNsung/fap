6005e6e1f1b4654e89610d7dff56e3734bc633a0
SUNsung <weatherius@gmail.com>
2019-02-14 09:22:33 +0200

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2019-02-14 09:22:32

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,90 +1,86 @@
 
         
-                tag = options[:tag] || '#{options[:grouping]}/#{lane_name}/#{options[:prefix]}#{options[:build_number]}#{options[:postfix]}'
-        message = options[:message] || '#{tag} (fastlane)'
+            it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
+  end
     
-          it 'works with name and password that contain spaces or `\'`' do
-        password = '\'test password\''
-        result = Fastlane::FastFile.new.parse('lane :test do
-          create_keychain ({
-            name: 'test.keychain',
-            password: '#{password}',
-          })
-        end').runner.execute(:test)
-    
-          context 'the `ignore_exit_status` option' do
-        context 'by default' do
-          it 'should raise if swiftlint completes with a non-zero exit status' do
-            allow(FastlaneCore::UI).to receive(:important)
-            expect(FastlaneCore::UI).to receive(:important).with(/If you want fastlane to continue anyway/)
-            # This is simulating the exception raised if the return code is non-zero
-            expect(Fastlane::Actions).to receive(:sh).and_raise('fake error')
-            expect(FastlaneCore::UI).to receive(:user_error!).with(/SwiftLint finished with errors/).and_call_original
-    
-        it 'shell-escapes environment variable values' do
-      message = 'A message'
-      path = '/usr/my local/bin'
-      command = command_from_args({ 'PATH' => path }, 'git', 'commit', '-m', message)
-      expect(command).to eq('PATH=#{path.shellescape} git commit -m #{message.shellescape}')
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
     end
     
-    if git.modified_files.include?('snapshot/lib/assets/SnapshotHelperXcode8.swift')
-  warn('You modified `SnapshotHelperXcode8.swift`, make sure to update the version number at the bottom of the file to notify users about the new helper file.')
+          it 'does not load workers specified in the :except option' do
+        agent_runner = AgentRunner.new(except: HuginnScheduler)
+        workers = agent_runner.send(:load_workers)
+        expect(workers.keys).to eq(['DelayedJobWorker'])
+    
+          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent)).to eq([@agent1])
+    end
+  end
 end
+
     
-        # Iterate over every machine in the index. The yielded {Entry} objects
-    # will NOT be locked, so you'll have to call {#get} manually to acquire
-    # the lock on them.
-    def each(reload=false)
-      if reload
-        @lock.synchronize do
-          with_index_lock do
-            unlocked_reload
-          end
-        end
-      end
+      describe '#sort_tuples!' do
+    let(:tuples) {
+      time = Time.now
+      [
+        [2, 'a', time - 1],  # 0
+        [2, 'b', time - 1],  # 1
+        [1, 'b', time - 1],  # 2
+        [1, 'b', time],      # 3
+        [1, 'a', time],      # 4
+        [2, 'a', time + 1],  # 5
+        [2, 'a', time],      # 6
+      ]
+    }
     
-            # Parses the options given an OptionParser instance.
-        #
-        # This is a convenience method that properly handles duping the
-        # originally argv array so that it is not destroyed.
-        #
-        # This method will also automatically detect '-h' and '--help'
-        # and print help. And if any invalid options are detected, the help
-        # will be printed, as well.
-        #
-        # If this method returns `nil`, then you should assume that help
-        # was printed and parsing failed.
-        def parse_options(opts=nil)
-          # Creating a shallow copy of the arguments so the OptionParser
-          # doesn't destroy the originals.
-          argv = @argv.dup
+    describe ConvertWebsiteAgentTemplateForMerge do
+  let :old_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
+    }
+  end
     
-            # Merge another configuration object into this one. This assumes that
-        # the other object is the same class as this one. This should not
-        # mutate this object, but instead should return a new, merged object.
-        #
-        # The default implementation will simply iterate over the instance
-        # variables and merge them together, with this object overriding
-        # any conflicting instance variables of the older object. Instance
-        # variables starting with '__' (double underscores) will be ignored.
-        # This lets you set some sort of instance-specific state on your
-        # configuration keys without them being merged together later.
-        #
-        # @param [Object] other The other configuration object to merge from,
-        #   this must be the same type of object as this one.
-        # @return [Object] The merged object.
-        def merge(other)
-          result = self.class.new
+      # if rss_url already in existing opml file, use that; otherwise, do a lookup
+  rss_url = nil
+  if File.exist?(OUTPUT_FILENAME)
+    xml = Nokogiri::XML(File.open(OUTPUT_FILENAME))
+    existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first
+    if existing_blog
+      rss_url = existing_blog.attr('xmlUrl')
+      puts '#{name}: ALREADY HAVE'
+    end
+  end
     
-            # Set the name of the plugin. The moment that this is called, the
-        # plugin will be registered and available. Before this is called, a
-        # plugin does not exist. The name must be unique among all installed
-        # plugins.
-        #
-        # @param [String] name Name of the plugin.
-        # @return [String] The name of the plugin.
-        def self.name(name=UNSET_VALUE)
-          # Get or set the value first, so we have a name for logging when
-          # we register.
-          result = get_or_set(:name, name)
+        def log_processing(name)
+      puts yellow '  #{File.basename(name)}'
+    end
+    
+        def setup_filters
+      @filters = cmdline_filters
+      @filters += @custom_filters if @custom_filters
+      @filters << Filter.new(:role, ENV['ROLES']) if ENV['ROLES']
+      @filters << Filter.new(:host, ENV['HOSTS']) if ENV['HOSTS']
+      fh = fetch_for(:filter, {}) || {}
+      @filters << Filter.new(:host, fh[:hosts]) if fh[:hosts]
+      @filters << Filter.new(:role, fh[:roles]) if fh[:roles]
+      @filters << Filter.new(:host, fh[:host]) if fh[:host]
+      @filters << Filter.new(:role, fh[:role]) if fh[:role]
+    end
+    
+    %i(git_strategy hg_strategy svn_strategy).each do |strategy|
+  validate(strategy) do |key, _value|
+    warn(
+      '[Deprecation Warning] #{key} is deprecated and will be removed in '\
+      'Capistrano 3.7.0.\n'\
+      'https://github.com/capistrano/capistrano/blob/master/UPGRADING-3.7.md'
+    )
+  end
+end
