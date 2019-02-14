9f3ccb18cd0a3ef622a4b928dd24fce16a98cac1
SUNsung <weatherius@gmail.com>
2019-02-15 01:47:12 +0200

    Rewrite [03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py] File in: 2019-02-15 01:47:12

--- pyton/03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py
+++ pyton/03tsrILecRwRM03wrPfIT3GhIQ03JiSIW2.BhI03cfk.py
@@ -1,115 +1,171 @@
 
         
-            def decode(self, value):
-        if value != self.resource_type:
-            raise jose.DeserializationError(
-                'Wrong resource type: {0} instead of {1}'.format(
-                    value, self.resource_type))
-        return value
+        
+def make_setting_element(setting_data, app, fromdocname):
+    refnode = make_refnode(app.builder, fromdocname,
+                           todocname=setting_data['docname'],
+                           targetid=setting_data['refid'],
+                           child=nodes.Text(setting_data['setting_name']))
+    p = nodes.paragraph()
+    p += refnode
+    
+        def run(self, args, opts):
+        if len(args) != 1:
+            raise UsageError()
+        filename = args[0]
+        if not os.path.exists(filename):
+            raise UsageError('File not found: %s\n' % filename)
+        try:
+            module = _import_file(filename)
+        except (ImportError, ValueError) as e:
+            raise UsageError('Unable to load %r: %s\n' % (filename, e))
+        spclasses = list(iter_spider_classes(module))
+        if not spclasses:
+            raise UsageError('No spider found in file: %s\n' % filename)
+        spidercls = spclasses.pop()
+    
+        def syntax(self):
+        return '[options]'
+    
+    else:
+    
+        from twisted.internet.ssl import AcceptableCiphers
+    from twisted.internet._sslverify import (ClientTLSOptions,
+                                             verifyHostname,
+                                             VerificationError)
+    try:
+        # XXX: this import would fail on Debian jessie with system installed
+        # service_identity library, due to lack of cryptography.x509 dependency
+        # See https://github.com/pyca/service_identity/issues/21
+        from service_identity.exceptions import CertificateError
+        verification_errors = (CertificateError, VerificationError)
+    except ImportError:
+        verification_errors = VerificationError
+    
+            dfd.addBoth(lambda _: logger.info('Spider closed (%(reason)s)',
+                                          {'reason': reason},
+                                          extra={'spider': spider}))
+    
+    
+class Signature(jose.Signature):
+    '''ACME-specific Signature. Uses ACME-specific Header for customer fields.'''
+    __slots__ = jose.Signature._orig_slots  # pylint: disable=no-member
+    
+    
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+sys.path.insert(0, os.path.abspath(os.path.join(here, '..')))
+    
+            self.config = util.get_apache_configurator(
+            self.config_path, self.vhost_path, self.config_dir, self.work_dir)
+    
+            with mock.patch('certbot.util.get_os_info') as mock_info:
+            for distro in entrypoint.OVERRIDE_CLASSES.keys():
+                mock_info.return_value = (distro, 'whatever')
+                self.assertEqual(entrypoint.get_configurator(),
+                                 entrypoint.OVERRIDE_CLASSES[distro])
+    
+            # Check to make sure challenge config path is included in apache config
+        self.assertEqual(
+            len(self.sni.configurator.parser.find_dir(
+                'Include', self.sni.challenge_conf)), 1)
+        self.assertEqual(len(responses), 1)
+        self.assertEqual(responses[0], response)
+    
+    # If true, links to the reST sources are added to the pages.
+#html_show_sourcelink = True
+    
+      Args:
+    clean_lines: A CleansedLines instance containing the file.
+    linenum: The number of the line to check.
+  Returns:
+    True if current line contains a function with 'override'
+    virt-specifier.
+  '''
+  # Scan back a few lines for start of current function
+  for i in xrange(linenum, max(-1, linenum - 10), -1):
+    match = Match(r'^([^()]*\w+)\(', clean_lines.elided[i])
+    if match:
+      # Look for 'override' after the matching closing parenthesis
+      line, _, closing_paren = CloseExpression(
+          clean_lines, i, len(match.group(1)))
+      return (closing_paren >= 0 and
+              Search(r'\boverride\b', line[closing_paren:]))
+  return False
+    
+    import os
+import sys
+import codecs
+import re
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+  def Done( self ):
+    return bool( self._response_future ) and self._response_future.done()
+    
+      # Start the next poll (only if the last poll didn't raise an exception)
+  return True
 
     
-    HEADER_ARGS = {'Strict-Transport-Security': HSTS_ARGS,
-               'Upgrade-Insecure-Requests': UIR_ARGS}
     
-        def test_nonexistent_generic(self):
-        with mock.patch('certbot.util.get_os_info') as mock_info:
-            mock_info.return_value = ('nonexistent', 'irrelevant')
-            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
-                mock_like.return_value = ['unknonwn']
-                self.assertEqual(entrypoint.get_configurator(),
-                                 configurator.ApacheConfigurator)
+  def _ThreadMain( self ):
+    while True:
+      time.sleep( self._ping_interval_seconds )
     
-        def test_perform2(self):
-        # Avoid load module
-        self.sni.configurator.parser.modules.add('ssl_module')
-        self.sni.configurator.parser.modules.add('socache_shmcb_module')
-        acme_responses = []
-        for achall in self.achalls:
-            self.sni.add_chall(achall)
-            acme_responses.append(achall.response(self.auth_key))
+      python_interpreter = _PathToPythonUsedDuringBuild()
+  if python_interpreter and utils.GetExecutable( python_interpreter ):
+    return python_interpreter
     
-        def __hash__(self):
-        try:
-            return self._hash
-        except AttributeError:
-            self._hash = hash(tuple(self._cparts))
-            return self._hash
+      AddToGroupMap( 'Include'  , preproc_group )
+  AddToGroupMap( 'Define'   , preproc_group )
+  AddToGroupMap( 'Macro'    , preproc_group )
+  AddToGroupMap( 'PreCondit', preproc_group )
     
-        def test_version(self):
-        version = ('Python %d.%d' % sys.version_info[:2]).encode('ascii')
-        for switch in '-V', '--version', '-VV':
-            rc, out, err = assert_python_ok(switch)
-            self.assertFalse(err.startswith(version))
-            self.assertTrue(out.startswith(version))
+      # 0 is not False
+  assert_that( _HandlePollResponse( 0, None ), equal_to( True ) )
     
-        @support.refcount_test
-    def test_refleaks_in___init__(self):
-        gettotalrefcount = support.get_attribute(sys, 'gettotalrefcount')
-        bzd = BZ2Decompressor()
-        refs_before = gettotalrefcount()
-        for i in range(100):
-            bzd.__init__()
-        self.assertAlmostEqual(gettotalrefcount() - refs_before, 0, delta=10)
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
     
-            results = [pool.apply_async(calculate, t) for t in TASKS]
-        imap_it = pool.imap(calculatestar, TASKS)
-        imap_unordered_it = pool.imap_unordered(calculatestar, TASKS)
+        return result
+
     
-    from ..common import *
+    def main():
+    for name, fn in [('sequential',
+                      functools.partial(download_urls_sequential, URLS)),
+                     ('processes',
+                      functools.partial(download_urls_with_executor,
+                                        URLS,
+                                        ProcessPoolExecutor(10))),
+                     ('threads',
+                      functools.partial(download_urls_with_executor,
+                                        URLS,
+                                        ThreadPoolExecutor(10)))]:
+        sys.stdout.write('%s: ' % name.ljust(12))
+        start = time.time()
+        url_map = fn()
+        sys.stdout.write('%.2f seconds (%d of %d downloaded)\n' %
+                         (time.time() - start, len(url_map), len(URLS)))
     
-            for i in html_json['sources']:
-            if 'src' in i:  #to avoid KeyError
-                if i['src'].startswith('https'):
-                    link_list.append((str(i['height']), i['src']))
-    
-            html = get_content(api_url)
-        self.tree = ET.ElementTree(ET.fromstring(html))
+    # Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [
+  ('index', 'futures.tex', u'futures Documentation',
+   u'Brian Quinlan', 'manual'),
+]
     
     
-def get_loop_file_path(title, output_dir):
-    return os.path.join(output_dir, get_output_filename([], title, 'txt', None, False))
-    
-        for quality in ['1080','720','480','380','240','144','auto']:
-        try:
-            real_url = info[quality][1]['url']
-            if real_url:
-                break
-        except KeyError:
-            pass
-    
-    yinyuetai_embed_patterns = [ 'player\.yinyuetai\.com/video/swf/(\d+)' ]
-    
-    # Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
-    
-    # Load some images to compare against
-known_obama_image = face_recognition.load_image_file('obama.jpg')
-known_biden_image = face_recognition.load_image_file('biden.jpg')
-    
-        # Display the resulting image
-    pil_image.show()
-    
-        # If no valid image file was uploaded, show the file upload form:
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
-    
-    
-def _trim_css_to_bounds(css, image_shape):
-    '''
-    Make sure a tuple in (top, right, bottom, left) order is within the bounds of the image.
-    
-        # Loop over each face found in the frame to see if it's someone we know.
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
-        name = '<Unknown Person>'
-    
-                frame_number = frame_count - 128 + frame_number_in_batch
-            print('I found {} face(s) in frame #{}.'.format(number_of_faces_in_frame, frame_number))
+def test_raw16():
+    check_raw(3, 1 << 5)
+    check_raw(3, (1 << 16) - 1)
