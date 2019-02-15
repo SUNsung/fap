1b62d8113e319bf2c0e78bc09291c1d2e792e58f
SUNsung <weatherius@gmail.com>
2019-02-15 12:09:22 +0200

    Rewrite [36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py] File in: 2019-02-15 12:09:21

--- pyton/36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py
+++ pyton/36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py
@@ -1,159 +1,325 @@
 
         
-        import pytest
-from flaskr.db import get_db
+        
+class HStoreExtension(CreateExtension):
     
-        indent = None
-    separators = (',', ':')
+        def flush(self):
+        '''
+        Remove the current session data from the database and regenerate the
+        key.
+        '''
+        self.clear()
+        self.delete(self.session_key)
+        self._session_key = None
+
     
-        def to_python(self, value):
-        return Markup(value)
+    from django.contrib.sessions.backends.base import (
+    CreateError, SessionBase, UpdateError,
+)
+from django.core.exceptions import SuspiciousOperation
+from django.db import DatabaseError, IntegrityError, router, transaction
+from django.utils import timezone
+from django.utils.functional import cached_property
     
-    :copyright: © 2010 by the Pallets team.
-:license: BSD, see LICENSE for more details.
+    '''
+requests._internal_utils
+~~~~~~~~~~~~~~
+    
+    import pytest
+from requests.compat import urljoin
+    
+            Currently, this closes the PoolManager and any active ProxyManager,
+        which closes any pooled connections.
+        '''
+        self.poolmanager.clear()
+        for proxy in self.proxy_manager.values():
+            proxy.clear()
+    
+            if algorithm is None:
+            _algorithm = 'MD5'
+        else:
+            _algorithm = algorithm.upper()
+        # lambdas assume digest modules are imported at the top level
+        if _algorithm == 'MD5' or _algorithm == 'MD5-SESS':
+            def md5_utf8(x):
+                if isinstance(x, str):
+                    x = x.encode('utf-8')
+                return hashlib.md5(x).hexdigest()
+            hash_utf8 = md5_utf8
+        elif _algorithm == 'SHA':
+            def sha_utf8(x):
+                if isinstance(x, str):
+                    x = x.encode('utf-8')
+                return hashlib.sha1(x).hexdigest()
+            hash_utf8 = sha_utf8
+        elif _algorithm == 'SHA-256':
+            def sha256_utf8(x):
+                if isinstance(x, str):
+                    x = x.encode('utf-8')
+                return hashlib.sha256(x).hexdigest()
+            hash_utf8 = sha256_utf8
+        elif _algorithm == 'SHA-512':
+            def sha512_utf8(x):
+                if isinstance(x, str):
+                    x = x.encode('utf-8')
+                return hashlib.sha512(x).hexdigest()
+            hash_utf8 = sha512_utf8
+    
+    # The scheme of the identifier. Typical schemes are ISBN or URL.
+#epub_scheme = ''
+    
+    
+def session():
+    '''
+    Returns a :class:`Session` for context-management.
+    
+    
+def default_headers():
+    '''
+    :rtype: requests.structures.CaseInsensitiveDict
+    '''
+    return CaseInsensitiveDict({
+        'User-Agent': default_user_agent(),
+        'Accept-Encoding': ', '.join(('gzip', 'deflate')),
+        'Accept': '*/*',
+        'Connection': 'keep-alive',
+    })
+    
+        def ensure_absent(self):
+        '''Ensures the rule and targets are absent'''
+        rule_description = self.rule.describe()
+        if not rule_description:
+            # Rule doesn't exist so don't need to delete
+            return
+        self.rule.delete()
+    
+    - debug: msg='{{ my_vm_eips.addresses | json_query(\'[?private_ip_address=='10.0.0.5']\') }}'
+    
+        if state == 'present':
+        for required in ['name', 'description', 'subnets']:
+            if not module.params.get(required):
+                module.fail_json(msg=str('Parameter %s required for state='present'' % required))
+    else:
+        for not_allowed in ['description', 'subnets']:
+            if module.params.get(not_allowed):
+                module.fail_json(msg=str('Parameter %s not allowed for state='absent'' % not_allowed))
+    
+            _check_mode(module, True)
+        firewall_policy = oneandone_conn.create_firewall_policy(
+            firewall_policy=firewall_policy_obj,
+            firewall_policy_rules=firewall_rules
+        )
+    
+            _thresholds = []
+        for treshold in thresholds:
+            key = treshold.keys()[0]
+            if key in threshold_entities:
+                _threshold = oneandone.client.Threshold(
+                    entity=key,
+                    warning_value=treshold[key]['warning']['value'],
+                    warning_alert=str(treshold[key]['warning']['alert']).lower(),
+                    critical_value=treshold[key]['critical']['value'],
+                    critical_alert=str(treshold[key]['critical']['alert']).lower())
+                _thresholds.append(_threshold)
+    
+            elif desired_state == 'offline':
+            if current_state == HOST_ABSENT:
+                self.fail(msg='absent host cannot be placed in offline state')
+            elif current_state in [HOST_STATES.MONITORED, HOST_STATES.DISABLED]:
+                if one.host.status(host.ID, HOST_STATUS.OFFLINE):
+                    self.wait_for_host_state(host, [HOST_STATES.OFFLINE])
+                    result['changed'] = True
+                else:
+                    self.fail(msg='could not set host offline')
+            elif current_state in [HOST_STATES.OFFLINE]:
+                pass
+            else:
+                self.fail(msg='unknown host state %s, cowardly refusing to change state to offline' % current_state_name)
+    
+    #example for a DNAT
+- hosts: localhost
+  connection: local
+  tasks:
+   - vca_nat:
+       instance_id: 'b15ff1e5-1024-4f55-889f-ea0209726282'
+       vdc_name: 'benz_ansible'
+       state: 'present'
+       nat_rules:
+         - rule_type: DNAT
+           original_ip: 203.0.113.23
+           original_port: 22
+           translated_ip: 192.0.2.42
+           translated_port: 22
+    
+    
+DOCUMENTATION = '''
+---
+module: ipa_dnszone
+author: Fran Fitzpatrick (@fxfitz)
+short_description: Manage FreeIPA DNS Zones
+description:
+- Add and delete an IPA DNS Zones using IPA API
+options:
+  zone_name:
+    description:
+    - The DNS zone name to which needs to be managed.
+    required: true
+  state:
+    description: State to ensure
+    required: false
+    default: present
+    choices: ['present', 'absent']
+extends_documentation_fragment: ipa.documentation
+version_added: '2.5'
 '''
     
-    
-class Environment(BaseEnvironment):
-    '''Works like a regular Jinja2 environment but has some additional
-    knowledge of how Flask's blueprint works so that it can prepend the
-    name of the blueprint to referenced templates if necessary.
-    '''
-    
-            The arguments passed to :meth:`as_view` are forwarded to the
-        constructor of the class.
-        '''
-        def view(*args, **kwargs):
-            self = view.view_class(*class_args, **class_kwargs)
-            return self.dispatch_request(*args, **kwargs)
-    
-        yield out
-    mp.undo()
-    
-    
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-    
-        return results
-    
-        for index, rule in updates:
-        if not module.check_mode:
-            rule = create_fw_rule(**rule)
-            fwservice.replace_FirewallRule_at(index, rule)
-        result['changed'] = True
-    
-    EXAMPLES = '''
-# Ensure rule to allow all users to access any host from any host
-- ipa_hbacrule:
-    name: allow_all
-    description: Allow all users to access any host from any host
-    hostcategory: all
-    servicecategory: all
-    usercategory: all
-    state: present
-    ipa_host: ipa.example.com
-    ipa_user: admin
-    ipa_pass: topsecret
-    
-    EXAMPLES = '''
-# Create groups based on the machine architecture
+    # Create nested groups
 - group_by:
-    key: machine_{{ ansible_machine }}
+    key: el{{ ansible_distribution_major_version }}-{{ ansible_architecture }}
+    parents:
+      - el{{ ansible_distribution_major_version }}
     
-        url = module.params.get('url')
+    - bigpanda:
+    component: '{{ deployment.component }}'
+    version: '{{ deployment.version }}'
+    token: '{{ deployment.token }}'
+    state: finished
+  delegate_to: localhost
+'''
     
-        def get_status():
-        '''Return the status of the process in monit, or the empty string if not present.'''
-        rc, out, err = module.run_command('%s %s' % (MONIT, SUMMARY_COMMAND), check_rc=True)
-        for line in out.split('\n'):
-            # Sample output lines:
-            # Process 'name'    Running
-            # Process 'name'    Running - restart pending
-            parts = parse(line.split())
-            if parts != '':
-                return parts
+        def test_default_encoder(self):
+        from acme.fields import RFC3339Field
+        self.assertEqual(
+            self.encoded, RFC3339Field.default_encoder(self.decoded))
     
-            # put all lines in the file into a Python list
-        strings = f.readlines()
-        
-        # above line leaves trailing newline characters; strip them out
-        strings = [x.strip(u'\n') for x in strings]
-        
-        # remove empty-lines and comments
-        strings = [x for x in strings if x and not x.startswith(u'#')]
-        
-        # insert empty string since all are being removed
-        strings.insert(0, u'')
-    
-        return ciphertext
-    
-    
-def im_detect_mask_hflip(model, im, target_scale, target_max_size, boxes):
-    '''Performs mask detection on the horizontally flipped image.
-    Function signature is the same as for im_detect_mask_aug.
-    '''
-    # Compute the masks for the flipped image
-    im_hf = im[:, ::-1, :]
-    boxes_hf = box_utils.flip_boxes(boxes, im.shape[1])
-    
-    
-def cityscapes_to_coco_all_random(cityscapes_id):
-    lookup = {
-        0: -1,  # ... background
-        1: -1,  # bicycle
-        2: -1,  # car
-        3: -1,  # person (ignore)
-        4: -1,  # train
-        5: -1,  # truck
-        6: -1,  # motorcycle
-        7: -1,  # bus
-        8: -1,  # rider (ignore)
-    }
-    return lookup[cityscapes_id]
+        @classmethod
+    # pylint: disable=arguments-differ,too-many-arguments
+    def sign(cls, payload, key, alg, nonce, url=None, kid=None):
+        # Per ACME spec, jwk and kid are mutually exclusive, so only include a
+        # jwk field if kid is not provided.
+        include_jwk = kid is None
+        return super(JWS, cls).sign(payload, key=key, alg=alg,
+                                    protect=frozenset(['nonce', 'url', 'kid', 'jwk', 'alg']),
+                                    nonce=nonce, url=url, kid=kid,
+                                    include_jwk=include_jwk)
 
     
     
-def add_stage(
-    model,
-    prefix,
-    blob_in,
-    n,
-    dim_in,
-    dim_out,
-    dim_inner,
-    dilation,
-    stride_init=2
-):
-    '''Add a ResNet stage to the model by stacking n residual blocks.'''
-    # e.g., prefix = res2
-    for i in range(n):
-        blob_in = add_residual_block(
-            model,
-            '{}_{}'.format(prefix, i),
-            blob_in,
-            dim_in,
-            dim_out,
-            dim_inner,
-            dilation,
-            stride_init,
-            # Not using inplace for the last block;
-            # it may be fetched externally or used by FPN
-            inplace_sum=i < n - 1
+class JWSTest(unittest.TestCase):
+    '''Tests for acme.jws.JWS.'''
+    
+    # Documents to append as an appendix to all manuals.
+#texinfo_appendices = []
+    
+            '''
+        error_files = self.aug.match('/augeas//error')
+    
+            self.assertEqual(len(matches), 1)
+        self.assertEqual(self.parser.get_arg(matches[0]), '1234')
+    
+        def test_nonexistent_generic(self):
+        with mock.patch('certbot.util.get_os_info') as mock_info:
+            mock_info.return_value = ('nonexistent', 'irrelevant')
+            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
+                mock_like.return_value = ['unknonwn']
+                self.assertEqual(entrypoint.get_configurator(),
+                                 configurator.ApacheConfigurator)
+    
+    
+class Migration(DataMigration):
+    def forwards(self, orm):
+        db.commit_transaction()
+        try:
+            self._forwards(orm)
+        except Exception:
+            # Explicitly resume the transaction because
+            # South is going to try and roll it back, but when
+            # it can't find one, it'll error itself, masking
+            # the actual exception being raised
+            #
+            # See https://github.com/getsentry/sentry/issues/5035
+            db.start_transaction()
+            raise
+        db.start_transaction()
+    
+            # Deleting model 'EnvironmentProject'
+        db.delete_table('sentry_environmentproject')
+    
+            # Adding field 'ApiToken.application'
+        db.add_column(
+            'sentry_apitoken',
+            'application',
+            self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                to=orm['sentry.ApiApplication'], null=True
+            ),
+            keep_default=False
         )
-        dim_in = dim_out
-    return blob_in, dim_in
     
     
-def keypoint_rcnn_frozen_features(model):
-    logger.warn('Deprecated: use `TRAIN.FREEZE_CONV_BODY: True` instead')
-    return build_generic_detection_model(
-        model,
-        get_func(cfg.MODEL.CONV_BODY),
-        add_roi_box_head_func=get_func(cfg.FAST_RCNN.ROI_BOX_HEAD),
-        add_roi_keypoint_head_func=get_func(cfg.KRCNN.ROI_KEYPOINTS_HEAD),
-        freeze_conv_body=True
+class Migration(DataMigration):
+    def forwards(self, orm):
+        db.commit_transaction()
+        try:
+            self._forwards(orm)
+        except Exception:
+            # Explicitly resume the transaction because
+            # South is going to try and roll it back, but when
+            # it can't find one, it'll error itself, masking
+            # the actual exception being raised
+            #
+            # See https://github.com/getsentry/sentry/issues/5035
+            db.start_transaction()
+            raise
+        db.start_transaction()
+    
+        def backwards(self, orm):
+        # Removing unique constraint on 'UserOption', fields ['user', 'organization', 'key']
+        db.delete_unique('sentry_useroption', ['user_id', 'organization_id', 'key'])
+    
+    
+def contains(name):
+    '''Determine if the dataset is in the catalog.'''
+    return name in _DATASETS.keys()
+    
+            return self.net.Conv(
+            blobs_in, blob_out, kernel=kernel, order=self.order, **kwargs
+        )
+    
+    
+def _add_fast_rcnn_head(
+    model, add_roi_box_head_func, blob_in, dim_in, spatial_scale_in
+):
+    '''Add a Fast R-CNN head to the model.'''
+    blob_frcn, dim_frcn = add_roi_box_head_func(
+        model, blob_in, dim_in, spatial_scale_in
     )
+    fast_rcnn_heads.add_fast_rcnn_outputs(model, blob_frcn, dim_frcn)
+    if model.train:
+        loss_gradients = fast_rcnn_heads.add_fast_rcnn_losses(model)
+    else:
+        loss_gradients = None
+    return loss_gradients
     
-    When renaming functions, it's generally a good idea to codemod existing yaml
-config files. An easy way to batch edit, by example, is a shell command like
+    from detectron.datasets import json_dataset
+from detectron.datasets import roidb as roidb_utils
+from detectron.utils import blob as blob_utils
+import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
+    
+    
+def compute_targets(ex_rois, gt_rois, weights=(1.0, 1.0, 1.0, 1.0)):
+    '''Compute bounding-box regression targets for an image.'''
+    return box_utils.bbox_transform_inv(ex_rois, gt_rois, weights).astype(
+        np.float32, copy=False
+    )
+
+    
+    
+def add_keypoint_rcnn_blobs(
+    blobs, roidb, fg_rois_per_image, fg_inds, im_scale, batch_idx
+):
+    '''Add Mask R-CNN keypoint specific blobs to the given blobs dictionary.'''
+    # Note: gt_inds must match how they're computed in
+    # datasets.json_dataset._merge_proposal_boxes_into_roidb
+    gt_inds = np.where(roidb['gt_classes'] > 0)[0]
+    max_overlaps = roidb['max_overlaps']
+    gt_keypoints = roidb['gt_keypoints']
