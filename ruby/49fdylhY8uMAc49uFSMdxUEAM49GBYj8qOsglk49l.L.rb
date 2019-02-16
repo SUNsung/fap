0f339c5552fb0d7f77cc218be547b0241b97b103
SUNsung <weatherius@gmail.com>
2019-02-16 09:30:17 +0200

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2019-02-16 09:30:16

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,152 +1,63 @@
 
         
-              agent = Agent.find_by(name: 'Test Weather Agent')
+                  add_default_name_and_id_for_value(tag_value, name_and_id)
+          options.delete('index')
+          options.delete('namespace')
+          options['for'] = name_and_id['id'] unless options.key?('for')
     
-              @bar3 = Agents::DotBar.new(name: 'bar3').tap { |agent|
-            agent.user = users(:bob)
-            agent.sources << @bar2
-            agent.save!
-          },
-        ]
-        @foo.reload
-        @bar2.reload
+            # This contains all the command plugins by name, and returns
+        # the command class and options. The command class is wrapped
+        # in a Proc so that it can be lazy loaded.
+        #
+        # @return [Registry<Symbol, Array<Proc, Hash>>]
+        attr_reader :commands
     
-    describe DefaultScenarioImporter do
-  let(:user) { users(:bob) }
-  describe '.import' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      mock(DefaultScenarioImporter).seed(is_a(User))
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'true' }
-      DefaultScenarioImporter.import(user)
+        # Get a value by the given key.
+    #
+    # This will evaluate the block given to `register` and return the
+    # resulting value.
+    def get(key)
+      return nil if !@items.key?(key)
+      return @results_cache[key] if @results_cache.key?(key)
+      @results_cache[key] = @items[key].call
+    end
+    alias :[] :get
+    
+      it 'ignores single-line hashes' do
+    expect_no_offenses('{a: 1, b: 2}')
+  end
+    
+        def get_gist_from_web(gist, file)
+      gist_url = get_gist_url_for(gist, file)
+      data     = get_web_content(gist_url)
+    
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+        flash_messages.each do |key, value|
+      flash[key] = value
+    end
+    respond_to do |wants|
+      wants.html { redirect_to url }
+      wants.json { render :json => {:redirect_to => url} }
+    end
+  end
+    
+        it 'has string keys' do
+      expect(hash.keys).to be_include('uid')
     end
     
-          it 'should be valid when the url points to a valid scenario' do
-        stub_request(:get, 'http://example.com/scenarios/1/export.json').to_return(:status => 200, :body => valid_data)
-        subject.url = 'http://example.com/scenarios/1/export.json'
-        expect(subject).to be_valid
-      end
-    end
-    
-    describe AgentsExporter do
-  describe '#as_json' do
-    let(:name) { 'My set of Agents' }
-    let(:description) { 'These Agents work together nicely!' }
-    let(:guid) { 'some-guid' }
-    let(:tag_fg_color) { '#ffffff' }
-    let(:tag_bg_color) { '#000000' }
-    let(:icon) { 'Camera' }
-    let(:source_url) { 'http://yourhuginn.com/scenarios/2/export.json' }
-    let(:agent_list) { [agents(:jane_weather_agent), agents(:jane_rain_notifier_agent)] }
-    let(:exporter) { AgentsExporter.new(
-      agents: agent_list, name: name, description: description,
-      source_url: source_url, guid: guid, tag_fg_color: tag_fg_color,
-      tag_bg_color: tag_bg_color, icon: icon) }
-    
-      describe '#log_for_agent' do
-    it 'creates AgentLogs' do
-      log = AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 4, :outbound_event => events(:jane_website_agent_event))
-      expect(log).not_to be_new_record
-      expect(log.agent).to eq(agents(:jane_website_agent))
-      expect(log.outbound_event).to eq(events(:jane_website_agent_event))
-      expect(log.message).to eq('some message')
-      expect(log.level).to eq(4)
-    end
-    
-        it 'should raise error when response has an error' do
-      stub(HTTParty).post { {'error' => {'message' => 'Sample error'}} }
-      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Sample error/)
+        it 'includes the origin (escaped) if one is provided' do
+      env['omniauth.origin'] = '/origin-example'
+      _, head, = *subject.call(env)
+      expect(head['Location']).to be_include('&origin=%2Forigin-example')
     end
   end
 end
 
     
-    When /^I (?:sign|log) in as '([^']*)'( on the mobile website)?$/ do |email, mobile|
-  @me = User.find_by_email(email)
-  @me.password ||= 'password'
-  automatic_login
-  confirm_login mobile
-end
-    
-    Then /^'([^']*)' should be post (\d+)$/ do |post_text, position|
-  stream_element_numbers_content(position).should have_content(post_text)
-end
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
-    end
-  end
-end
-
-    
-          it 'should remove participation' do
-        delete :destroy, params: {post_id: post.id}
-        expect(alice.participations.where(:target_id => post.id)).not_to exist
-        expect(response.code).to eq('200')
+        context 'custom paths' do
+      it 'uses a custom request_path if one is provided' do
+        @options = {:request_path => '/awesome'}
+        expect { strategy.call(make_env('/awesome')) }.to raise_error('Request Phase')
       end
-    end
-    
-    require 'rack/show_exceptions'
-    
-          def accepts?(env)
-        raise NotImplementedError, '#{self.class} implementation pending'
-      end
-    
-          def call(env)
-        status, headers, body = super
-        response = Rack::Response.new(body, status, headers)
-        request = Rack::Request.new(env)
-        remove_bad_cookies(request, response)
-        response.finish
-      end
-    
-          def escape_string(str)
-        str = @escaper.escape_url(str)        if @url
-        str = @escaper.escape_html(str)       if @html
-        str = @escaper.escape_javascript(str) if @javascript
-        str
-      end
-    end
-  end
-end
-
-    
-        it 'returns proper error message' do
-      expect(json_response['error']).to eq('You are not authorized to access this page.')
-    end
-  end
-end
-
-    
-          it_behaves_like 'returns 200 HTTP status'
-    
-        def label
-      Spree.t included_in_price? ? :including_tax : :excluding_tax,
-              scope: 'adjustment_labels.tax_rates',
-              name: name.presence || tax_category.name,
-              amount: amount_for_label
-    end
-    
-    desc 'Generates a dummy app for testing for every Spree engine'
-task :test_app do
-  SPREE_GEMS.each do |gem_name|
-    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
-      sh 'rake test_app'
-    end
-  end
-end
-    
-          def unprocessable_entity(message)
-        render plain: { exception: message }.to_json, status: 422
-      end
-    
-            def address_params
-          params.require(:address).permit(permitted_address_attributes)
-        end
