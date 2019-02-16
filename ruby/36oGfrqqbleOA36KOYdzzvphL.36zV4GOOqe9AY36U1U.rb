1881816e09408b2d563698a76c49d4ffa55a7753
SUNsung <weatherius@gmail.com>
2019-02-16 13:55:33 +0200

    Rewrite [36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb] File in: 2019-02-16 13:55:32

--- ruby/36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb
+++ ruby/36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb
@@ -1,86 +1,84 @@
 
         
-                @octokit.connection_options[:ssl] = { verify: verify_ssl }
+          #
+  # Initializes an HTTP packet header class that inherits from a Hash base
+  # class.
+  #
+  def initialize
+    self.dcase_hash = {}
     
-            def representation_class
-          Representation::Issue
-        end
+    IAX_IE_CALLED_NUMBER  = 1
+IAX_IE_CALLING_NUMBER = 2
+IAX_IE_AUTH_METHODS   = 3
+IAX_IE_CALLING_NAME   = 4
+IAX_IE_USERNAME       = 6
+IAX_IE_DESIRED_CODEC  = 9
+IAX_IE_ORIGINAL_DID   = 10
+IAX_IE_ACTUAL_CODECS  = 8
+IAX_IE_PROTO_VERSION  = 11
+IAX_IE_REG_REFRESH    = 19
+IAX_IE_CHALLENGE_DATA = 15
+IAX_IE_CHALLENGE_RESP = 16
+IAX_IE_APPARENT_ADDR  = 18
+IAX_IE_REGREJ_CAUSE   = 22
+IAX_IE_HANGUP_CAUSE   = 42
     
-            attr_reader :attributes
+      # Setup the root logger with the Rails log level and the desired set of
+  # appenders. The list of appenders to use should be set in the environment
+  # specific configuration file.
+  #
+  # For example, in a production application you would not want to log to
+  # STDOUT, but you would want to send an email for 'error' and 'fatal'
+  # messages:
+  #
+  # => config/environments/production.rb
+  #
+  #     config.log_to = %w[file email]
+  #
+  # In development you would want to log to STDOUT and possibly to a file:
+  #
+  # => config/environments/development.rb
+  #
+  #     config.log_to = %w[stdout file]
+  #
+  Logging.logger.root.appenders = config.log_to unless config.log_to.empty?
     
-        reversible(&method(:up_down))
+      class Notification < ApplicationRecord
   end
     
-              expect {
-            get :create, params: valid_params.merge(invite: {token: code.token})
-          }.to change { code.reload.count }.by(-1)
-        end
-    
-          it 'returns reshares without login' do
-        bob.reshare!(@post)
-        sign_out :user
-        get :index, params: {post_id: @post.id}, format: :json
-        expect(JSON.parse(response.body).map {|h| h['id'] }).to match_array(@post.reshares.map(&:id))
-      end
-    end
-  end
+    Then (/^I should see the 'getting started' contents$/) do
+  confirm_getting_started_contents
 end
 
     
-      namespace :install do
-    GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
-      desc 'Build and install #{gem} as local gem'
-      task gem => package(gem, '.gem') do
-        sh 'gem install #{package(gem, '.gem')}'
-      end
-    end
-    
-      at_exit { Application.run! if $!.nil? && Application.run? }
+    Then /^'([^']*)' should be post (\d+)$/ do |post_text, position|
+  stream_element_numbers_content(position).should have_content(post_text)
 end
     
-        post('/', {'csrf_param' => token}, 'rack.session' => {:csrf => token})
-    expect(last_response).to be_ok
-  end
+        context 'on a post you partecipate to' do
+      before { alice.participate! post }
     
-          def line_class(line)
-        if line =~ /^@@/
-          'gc'
-        elsif line =~ /^\+/
-          'gi'
-        elsif line =~ /^\-/
-          'gd'
-        else
-          ''
-        end
-      end
-    
-          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
-      def left_shift(int, shift)
-        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
-        # 1>>31, 2**32
-        (r & 2147483648) == 0 ? r : r - 4294967296
-      end
-    
-          def footer_format
-        has_footer && @footer.format.to_s
-      end
-    
-        get '/pages'
-    
-      include_examples 'multiline literal brace layout' do
-    let(:open) { '[' }
-    let(:close) { ']' }
-  end
-    
-    shared_examples_for 'multiline literal brace layout method argument' do
-  include MultilineLiteralBraceHelper
-    
-          # Returns the body of the `when` node.
-      #
-      # @return [Node, nil] the body of the `when` node
-      def body
-        node_parts[-1]
-      end
-    end
-  end
+    Given(/^a custom task to generate a file$/) do
+  TestApp.copy_task_to_test_app('spec/support/tasks/database.rake')
 end
+    
+      def test_file_exists(path)
+    exists?('f', path)
+  end
+    
+        def setup_filters
+      @filters = cmdline_filters
+      @filters += @custom_filters if @custom_filters
+      @filters << Filter.new(:role, ENV['ROLES']) if ENV['ROLES']
+      @filters << Filter.new(:host, ENV['HOSTS']) if ENV['HOSTS']
+      fh = fetch_for(:filter, {}) || {}
+      @filters << Filter.new(:host, fh[:hosts]) if fh[:hosts]
+      @filters << Filter.new(:role, fh[:roles]) if fh[:roles]
+      @filters << Filter.new(:host, fh[:host]) if fh[:host]
+      @filters << Filter.new(:role, fh[:role]) if fh[:role]
+    end
+    
+            print_deprecation_warnings_if_applicable
+    
+    set_if_empty :format, :airbrussh
+set_if_empty :log_level, :debug
