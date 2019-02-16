0b8643a4e7efd0a817d954e8d308545336d05135
SUNsung <weatherius@gmail.com>
2019-02-16 16:49:37 +0200

    Rewrite [02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb] File in: 2019-02-16 16:49:37

--- ruby/02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb
+++ ruby/02E9IIBW89llc023GnnFRJwi.02YKLvNJy8ARs02owu.rb
@@ -1,110 +1,80 @@
 
         
-                      if accept
-                html_options[option] = true
-              elsif option == :checked
-                html_options[option] = false
-              end
-            end
-    
-        # Main render entry point shared by Action View and Action Controller.
-    def render(context, options)
-      if options.key?(:partial)
-        render_partial(context, options)
-      else
-        render_template(context, options)
-      end
-    end
-    
-    module Docs
-  class PageDb
-    attr_reader :pages
-    
-        DOCUMENT_RGX = /\A(?:\s|(?:<!--.*?-->))*<(?:\!doctype|html)/i
-    
-            if mod
-          if name == 'Index'
-            return slug.split('/')[1..-2].join('/')
-          elsif name == 'Angular'
-            return slug.split('/').last.split('-').first
-          end
-        end
-    
-          # Returns an array of all the values in the `hash` literal.
-      #
-      # @return [Array<Node>] an array of values in the `hash` literal
-      def values
-        each_pair.map(&:value)
-      end
-    
-          # This is used for duck typing with `pair` nodes which also appear as
-      # `hash` elements.
-      #
-      # @return [false]
-      def hash_rocket?
-        false
-      end
-    
-    module RuboCop
-  module AST
-    # Common functionality for nodes that are parameterized:
-    # `send`, `super`, `zsuper`, `def`, `defs`
-    module ParameterizedNode
-      # Checks whether this node's arguments are wrapped in parentheses.
-      #
-      # @return [Boolean] whether this node's arguments are
-      #                   wrapped in parentheses
-      def parenthesized?
-        loc.end && loc.end.is?(')')
-      end
-    
-          locations = Array.new
-      while (data.code.to_i == 301 || data.code.to_i == 302)
-        data = handle_gist_redirecting(data)
-        break if locations.include? data.header['Location']
-        locations << data.header['Location']
-      end
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-      # Improved version of Liquid's truncate:
-  # - Doesn't cut in the middle of a word.
-  # - Uses typographically correct ellipsis (…) insted of '...'
-  def truncate(input, length)
-    if input.length > length && input[0..(length-1)] =~ /(.+)\b.+$/im
-      $1.strip + ' &hellip;'
+          def confirm_on_page(page_name)
+    if page_name == 'my profile page'
+      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
     else
-      input
+      expect(page).to have_path(path_to(page_name))
     end
   end
+end
     
-        # Note: didn't use `self.directories`.
-    # Pacman doesn't really record that information, to my knowledge.
+        it 'generates a jasmine fixture', fixture: true do
+      session[:mobile_view] = true
+      get :new, format: :mobile
+      save_fixture(html_for('body'), 'conversations_new_mobile')
+    end
+  end
+end
+
     
-        if File.exists?(params[:output])
-      # TODO(sissel): Allow folks to choose output?
-      logger.error('Puppet module directory '#{params[:output]}' already ' \
-                    'exists. Delete it or choose another output (-p flag)')
+        context 'on a post you do not partecipate to' do
+      it 'says it is an unprocessable request' do
+        delete :destroy, params: {post_id: post.id}
+        expect(response.code).to eq('422')
+      end
+    end
+  end
+end
+
+    
+    Gem::Specification.new do |gem|
+  gem.name          = 'capistrano'
+  gem.version       = Capistrano::VERSION
+  gem.authors       = ['Tom Clements', 'Lee Hambley']
+  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
+  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
+  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
+  gem.homepage      = 'http://capistranorb.com/'
+    
+    Then(/^the release is created$/) do
+  run_vagrant_command('ls -g #{TestApp.releases_path}')
+end
+    
+            filter = Object.new
+        def filter.filter(servers)
+          block.call(servers)
+        end
+      elsif !filter.respond_to? :filter
+        raise TypeError, 'Provided custom filter <#{filter.inspect}> does ' \
+                         'not have a public 'filter' method'
+      end
+      @custom_filters ||= []
+      @custom_filters << filter
     end
     
-        # use dir to set stuff up properly, mainly so I don't have to reimplement
-    # the chdir/prefix stuff special for zip.
-    dir = convert(FPM::Package::Dir)
-    if attributes[:chdir]
-      dir.attributes[:chdir] = File.join(build_path, attributes[:chdir])
+    task :add_template, [:name] do |t, args|
+  Dir.glob('README.*') do |file|
+    code = File.read(file)
+    if code =~ /^===.*#{args.name.capitalize}/
+      puts 'Already covered in #{file}'
     else
-      dir.attributes[:chdir] = build_path
+      template = code[/===[^\n]*Liquid.*index\.liquid<\/tt>[^\n]*/m]
+      if !template
+        puts 'Liquid not found in #{file}'
+      else
+        puts 'Adding section to #{file}'
+        template = template.gsub(/Liquid/, args.name.capitalize).gsub(/liquid/, args.name.downcase)
+        code.gsub! /^(\s*===.*CoffeeScript)/, '\n' << template << '\n\\1'
+        File.open(file, 'w') { |f| f << code }
+      end
     end
+  end
+end
     
-        args.flatten!.compact!
+          def session_key
+        @session_key ||= options[:session_key]
+      end
+    end
+  end
+end
