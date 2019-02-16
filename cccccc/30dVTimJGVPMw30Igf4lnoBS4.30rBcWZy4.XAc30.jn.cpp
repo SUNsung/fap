b21cf3e3fb9cc87358bef0927ce8513e89514a0b
SUNsung <weatherius@gmail.com>
2019-02-16 17:42:35 +0200

    Rewrite [30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp] File in: 2019-02-16 17:42:35

--- cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
+++ cccccc/30dVTimJGVPMw30Igf4lnoBS4.30rBcWZy4.XAc30.jn.cpp
@@ -1,426 +1,46 @@
 
         
-        // Must be included first
-#include 'tensorflow/python/lib/core/numpy.h'
+          /*
+   * Patch a branch to the correct target.
+   *
+   * It decodes the branch @jmp to decide whether it's an absolute branch or an
+   * offset branch and patches it properly.
+   */
+  static void patchBranch(CodeAddress jmp, CodeAddress dest);
+  static void patchAbsolute(CodeAddress jmp, CodeAddress dest);
     
-    #include <unordered_map>
-    
-      CostGraphDef cost_graph_analytical;
-  PredictCosts(&analytical_estimator_, &cost_graph_analytical,
-               &total_time_analytical_);
-  VLOG(1) << 'cost_graph_analytical size: '
-          << cost_graph_analytical.node_size();
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    
-    {  DCHECK(PyDict_Check(code_to_exc_type_map));
-  PyObject* key;
-  PyObject* value;
-  Py_ssize_t pos = 0;
-  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
-    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
-    singleton_->exc_types_[code] = value;
-    // The exception classes should also have the lifetime of the process, but
-    // incref just in case.
-    Py_INCREF(value);
-  }
+    APCHandle::Pair APCCollection::WrapArray(APCHandle::Pair inner,
+                                         CollectionType colType) {
+  auto const col = new APCCollection;
+  col->m_arrayHandle = inner.handle;
+  col->m_colType = colType;
+  return { &col->m_handle, inner.size + sizeof(APCCollection) };
 }
     
-    // Called by python code on initialization.
-//
-// 'trampoline' must represent a python function which has the
-// following signature:
-//   (string, list(ndarray)) | (string, list(EagerTensor)) ->
-//     ndarray | list(ndarray) | python scalar |
-//     EagerTensor | list(EagerTensor) | None
-//
-// The trampoline takes two arguments, the first is a string token
-// used by the python frontend's dispatching logic; the second is a
-// list of numpy ndarrays or EagerTensor objects. It can return a
-// single numpy ndarray, a list of numpy ndarrays, a python scalar, an
-// EagerTensor, a list of EagerTensors, or None.
-//
-// PyFunc requires inputs and outputs to be ndarrays. EagerPyFunc requires
-// inputs to be a list of EagerTensors and outputs to be an EagerTensor, a list
-// of EagerTensors, or None.
-//
-// The C++ runtime converts outputs back to Tensor objects.
-//
-// This function is called by script_ops.py during its module initialization.
-//
-// TODO(zhifengc): Support distributed runtime.
-void InitializePyTrampoline(PyObject* trampoline);
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #endif  // TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_DIAGNOSTICS_H_
-
-    
-      // Polls the CUDA platform for the event's current status.
-  Event::Status PollForStatus();
+      Object createObject() const;
     
     
-    {
-    {    return module;
-#endif
-  }
-}
-    
-    bool AnyMetadata::InternalIs(const Descriptor* descriptor) const {
-  const string type_url = type_url_->GetNoArena();
-  string full_name;
-  if (!ParseAnyTypeUrl(type_url, &full_name)) {
-    return false;
-  }
-  return full_name == descriptor->full_name();
-}
-    
-    
-    {  for (int i = 0; i < parts.size(); i++) {
-    string::size_type equals_pos = parts[i].find_first_of('=');
-    std::pair<string, string> value;
-    if (equals_pos == string::npos) {
-      value.first = parts[i];
-      value.second = '';
-    } else {
-      value.first = parts[i].substr(0, equals_pos);
-      value.second = parts[i].substr(equals_pos + 1);
+struct Config {
+  /*
+   * Normalizes hdf string names to their ini counterparts
+   *
+   * We have special handling for a few hdf strings such as those containing
+   * MySQL, Eval, IPv[4|6] and EnableHipHopSyntax
+   */
+  static std::string IniName(const Hdf& config,
+                             const bool prepend_hhvm = true);
+  static std::string IniName(const std::string& config,
+                             const bool prepend_hhvm = true);
     }
-    output->push_back(value);
+    
+    
+    {  /**
+   * Execute a debugger action.
+   */
+  virtual String debuggerVerb(const std::string& /*verb*/,
+                              const std::vector<std::string>& /*args*/) {
+    return String();
   }
-}
-    
-    
-    {
-    {
-    {
-    {}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-    #include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/plugin.h>
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/descriptor.pb.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream.h>
-#include <google/protobuf/stubs/strutil.h>
-    
-    SourceGeneratorBase::~SourceGeneratorBase() {
-}
-    
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
-    
-    // Strip package name from a descriptor's full name.
-// For example:
-//   Full name   : foo.Bar.Baz
-//   Package name: foo
-//   After strip : Bar.Baz
-string StripPackageName(const string& full_name,
-                        const FileDescriptor* file) {
-  if (file->package().empty()) {
-    return full_name;
-  } else {
-    // Strip package name
-    return full_name.substr(file->package().size() + 1);
-  }
-}
-    
-    CallCredentials::~CallCredentials() {}
-    
-    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H */
-
-    
-    
-    { private:
-  CensusContext context_;
-  // Metadata elements for tracing and census stats data.
-  grpc_linked_mdelem stats_bin_;
-  grpc_linked_mdelem tracing_bin_;
-  // Client method.
-  absl::string_view method_;
-  std::string qualified_method_;
-  grpc_slice path_;
-  // The recv trailing metadata callbacks.
-  grpc_metadata_batch* recv_trailing_metadata_;
-  grpc_closure* initial_on_done_recv_trailing_metadata_;
-  grpc_closure on_done_recv_trailing_metadata_;
-  // recv message
-  grpc_closure* initial_on_done_recv_message_;
-  grpc_closure on_done_recv_message_;
-  // Start time (for measuring latency).
-  absl::Time start_time_;
-  // Server elapsed time in nanoseconds.
-  uint64_t elapsed_time_;
-  // The received message--may be null.
-  grpc_core::OrphanablePtr<grpc_core::ByteStream>* recv_message_;
-  // Number of messages in this RPC.
-  uint64_t recv_message_count_;
-  uint64_t sent_message_count_;
-  // Buffer needed for grpc_slice to reference when adding trace context
-  // metatdata to outgoing message.
-  char tracing_buf_[kMaxTraceContextLen];
 };
     
-    MeasureInt64 RpcClientSentMessagesPerRpc() {
-  static const auto measure =
-      MeasureInt64::Register(kRpcClientSentMessagesPerRpcMeasureName,
-                             'Number of messages sent per RPC', kCount);
-  return measure;
-}
-    
-    // A CallData class will be created for every grpc call within a channel. It is
-// used to store data and methods specific to that call. CensusServerCallData is
-// thread-compatible, however typically only 1 thread should be interacting with
-// a call at a time.
-class CensusServerCallData : public CallData {
- public:
-  // Maximum size of server stats that are sent on the wire.
-  static constexpr uint32_t kMaxServerStatsLen = 16;
-    }
-    
-    const ViewDescriptor& ClientCompletedRpcsCumulative() {
-  const static ViewDescriptor descriptor =
-      ViewDescriptor()
-          .set_name('grpc.io/client/completed_rpcs/cumulative')
-          .set_measure(kRpcClientRoundtripLatencyMeasureName)
-          .set_aggregation(Aggregation::Count())
-          .add_column(ClientMethodTagKey())
-          .add_column(ClientStatusTagKey());
-  return descriptor;
-}
-    
-      Status GetFileContainingSymbol(
-      ServerContext* context, const grpc::string& symbol,
-      reflection::v1alpha::ServerReflectionResponse* response);
-    
-    #include <grpc/support/cpu.h>
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  FILE* fp;
-  fp = fopen('/proc/stat', 'r');
-  uint64_t user, nice, system, idle;
-  fscanf(fp, 'cpu %lu %lu %lu %lu', &user, &nice, &system, &idle);
-  fclose(fp);
-  busy = user + nice + system;
-  total = busy + idle;
-  return std::make_pair(busy, total);
-}
-    
-    bool GodotCollisionDispatcher::needsCollision(const btCollisionObject *body0, const btCollisionObject *body1) {
-	if (body0->getUserIndex() == CASTED_TYPE_AREA || body1->getUserIndex() == CASTED_TYPE_AREA) {
-		// Avoide area narrow phase
-		return false;
-	}
-	return btCollisionDispatcher::needsCollision(body0, body1);
-}
-    
-    public:
-	HingeJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Transform &frameA, const Transform &frameB);
-	HingeJointBullet(RigidBodyBullet *rbA, RigidBodyBullet *rbB, const Vector3 &pivotInA, const Vector3 &pivotInB, const Vector3 &axisInA, const Vector3 &axisInB);
-    
-    /**
-	@author AndreaCatania
-*/
-    
-    void MemoryPool::setup(uint32_t p_max_allocs) {
-    }
-    
-    Thread *Thread::create(ThreadCreateCallback p_callback, void *p_user, const Settings &p_settings) {
-    }
-    
-    				if (p_str[index] == '-' || (p_str[index] >= '0' && p_str[index] <= '9')) {
-					//a number
-					const CharType *rptr;
-					double number = String::to_double(&p_str[index], &rptr);
-					index += (rptr - &p_str[index]);
-					r_token.type = TK_NUMBER;
-					r_token.value = number;
-					return OK;
-    }
-    
-    #define FAIL_ON_ERROR(x) { DWORD ec; if ((ec = (x)) != ERROR_SUCCESS) { ShowErrorAndExit(ec, __WFUNCTION__, __LINE__); } }
-    
-    /*!
- * \brief assign the expression to out according to request
- * \param out the data to be assigned
- * \param req the assignment request
- * \param exp the expression
- * \tparam OType output type
- * \tparam Exp expression type
- */
-#define ASSIGN_DISPATCH(out, req, exp)  \
-  {                                     \
-    switch (req) {                      \
-      case kNullOp:                     \
-        break;                          \
-      case kWriteTo:                    \
-      case kWriteInplace:               \
-        (out) = (exp);                  \
-        break;                          \
-      case kAddTo:                      \
-        (out) += (exp);                 \
-        break;                          \
-      default:                          \
-        LOG(FATAL) << 'not reached';    \
-    }                                   \
-  }
-    
-    namespace mxnet {
-namespace op {
-template<>
-Operator *CreateOp<cpu>(CaffeLossParam param, int dtype) {
-  Operator *op = NULL;
-  switch (dtype) {
-  case mshadow::kFloat32:
-    op = new CaffeLoss<cpu, float>(param);
-    break;
-  case mshadow::kFloat64:
-    op = new CaffeLoss<cpu, double>(param);
-    break;
-  case mshadow::kFloat16:
-    LOG(FATAL) << 'float16 layer is not supported by caffe';
-    break;
-  default:
-    LOG(FATAL) << 'Unsupported type ' << dtype;
-  }
-  return op;
-}
-    }
-    }
-    
-    /*!
- * \brief Thread pool.
- */
-class ThreadPool {
- public:
-  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
-  struct SetReadyOnDestroy {
-    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
-      : event_(event) {
-    }
-    inline ~SetReadyOnDestroy() {
-      if (event_) {
-        event_->signal();
-      }
-    }
-    std::shared_ptr<dmlc::ManualEvent>  event_;
-  };
-    }
-    
-    /*!
- *  Copyright (c) 2015 by Contributors
- * \file inst_vector.h
- * \brief holder of a sequence of DataInst in CPU
- *        that are not necessarily of same shape
- */
-    
-    /*!
- *  Copyright (c) 2015 by Contributors
- * \file iter_batchloader.h
- * \brief define a batch adapter to create tblob batch
- */
-#ifndef MXNET_IO_ITER_BATCHLOADER_H_
-#define MXNET_IO_ITER_BATCHLOADER_H_
-    
-        /** Inserts a controlPoint at index.
-     *
-     * @js NA
-     * @param controlPoint A control point.
-     * @param index Insert the point to array in index.
-     */
-    void insertControlPoint(const Vec2& controlPoint, ssize_t index);
-    
-    
-    {// end of actions group
-/// @}
-    
-        //
-    // Override
-    //
-    virtual RotateBy* clone() const override;
-    virtual RotateBy* reverse() const override;
-    virtual void startWithTarget(Node *target) override;
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    RotateBy();
-    virtual ~RotateBy() {}
-    
-    
-    {
-    {
-    {            p.z = (r * ( 1 - cosBeta ) * cosTheta);// '100' didn't work for
-            p.x = p.z * sinf(rotateByYAxis) + p.x * cosf(rotateByYAxis);
-            p.z = p.z * cosf(rotateByYAxis) - p.x * sinf(rotateByYAxis);
-            p.z/=7;
-            //    Stop z coord from dropping beneath underlying page in a transition
-            // issue #751
-            if( p.z < 0.5f )
-            {
-                p.z = 0.5f;
-            }
-            
-            // Set new coords
-            p.x += getGridRect().origin.x;
-            setVertex(Vec2(i, j), p);
-            
-        }
-    }
-}
-    
-    NS_CC_END
-    
-    ActionTween* ActionTween::create(float duration, const std::string& key, float from, float to)
-{
-    ActionTween* ret = new (std::nothrow) ActionTween();
-    if (ret && ret->initWithDuration(duration, key, from, to))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    
-    delete ret;
-    return nullptr;
-}
-    
-    /**
- * @addtogroup actions
- * @{
- */
-    }
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+    IMPLEMENT_RESOURCE_ALLOCATION(PlainDirectory)
