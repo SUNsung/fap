43e72bcc748931bbee850570504d27d47d42070f
SUNsung <weatherius@gmail.com>
2019-02-16 04:17:11 +0200

    Rewrite [02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb] File in: 2019-02-16 04:17:10

--- ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
+++ ruby/02zNtQmWB1x202wJsZv3StAs02Y0.DPHZokZk02Hie.rb
@@ -1,99 +1,90 @@
 
         
-        describe JobsHelper do
-  let(:job) { Delayed::Job.new }
+          # Generate digests for assets URLs.
+  config.assets.digest = true
     
-        it 'defauls foreground and background colors' do
-      scenario.tag_fg_color = nil
-      scenario.tag_bg_color = nil
-      expect(style_colors(scenario)).to eq('color:#FFFFFF;background-color:#5BC0DE')
-    end
-  end
-    
-    describe DefaultScenarioImporter do
-  let(:user) { users(:bob) }
-  describe '.import' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      mock(DefaultScenarioImporter).seed(is_a(User))
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'true' }
-      DefaultScenarioImporter.import(user)
-    end
-    
-          context '#run_workers' do
-        it 'runs all the workers' do
-          mock.instance_of(HuginnScheduler).run!
-          mock.instance_of(DelayedJobWorker).run!
-          @agent_runner.send(:run_workers)
-        end
-    
-        it 'should raise an exception when encountering complex JSONPaths' do
-      expect { LiquidMigrator.convert_string('Received <$.content.text.*> from <$.content.name> .') }.
-        to raise_error('JSONPath '$.content.text.*' is too complex, please check your migration.')
-    end
-  end
-    
-        def url
-      @url ||= URL.parse request.base_url
-    end
-    
-      # Compile a file on disk to CSS.
-  #
-  # @raise [Sass::SyntaxError] if there's an error in the document
-  # @raise [Encoding::UndefinedConversionError] if the source encoding
-  #   cannot be converted to UTF-8
-  # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
-  #
-  # @overload compile_file(filename, options = {})
-  #   Return the compiled CSS rather than writing it to a file.
-  #
-  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
-  #   @param options [{Symbol => Object}] An options hash;
-  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
-  #   @return [String] The compiled CSS.
-  #
-  # @overload compile_file(filename, css_filename, options = {})
-  #   Write the compiled CSS to a file.
-  #
-  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
-  #   @param options [{Symbol => Object}] An options hash;
-  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
-  #   @param css_filename [String] The location to which to write the compiled CSS.
-  def self.compile_file(filename, *args)
-    options = args.last.is_a?(Hash) ? args.pop : {}
-    css_filename = args.shift
-    result = Sass::Engine.for_file(filename, options).render
-    if css_filename
-      options[:css_filename] ||= css_filename
-      open(css_filename, 'w') {|css_file| css_file.write(result)}
-      nil
-    else
-      result
+          # Custom destructuring method. This can be used to normalize
+      # destructuring for different variations of the node.
+      #
+      # In this case, the `def` node destructures into:
+      #
+      #   `method_name, arguments, body`
+      #
+      # while the `defs` node destructures into:
+      #
+      #   `receiver, method_name, arguments, body`
+      #
+      # so we reverse the destructured array to get the optional receiver
+      # at the end, where it can be discarded.
+      #
+      # @return [Array] the different parts of the `def` or `defs` node
+      def node_parts
+        to_a.reverse
+      end
     end
   end
 end
+
     
-          # @see Base#\_store
-      def _store(key, version, sha, contents)
-        compiled_filename = path_to(key)
-        FileUtils.mkdir_p(File.dirname(compiled_filename))
-        Sass::Util.atomic_create_and_write_file(compiled_filename) do |f|
-          f.puts(version)
-          f.puts(sha)
-          f.write(contents)
+          DOUBLE_SPLAT = '**'.freeze
+    
+          # Checks whether any argument of the node is a splat
+      # argument, i.e. `*splat`.
+      #
+      # @return [Boolean] whether the node is a splat argument
+      def splat_argument?
+        arguments? &&
+          (arguments.any?(&:splat_type?) || arguments.any?(&:restarg_type?))
+      end
+      alias rest_argument? splat_argument?
+    
+          # Calls the given block for each condition node in the `when` branch.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_condition
+        return conditions.to_enum(__method__) unless block_given?
+    
+            def collection_paginator
+          Spree::Api::Dependencies.storefront_collection_paginator.constantize
         end
-      rescue Errno::EACCES
-        # pass
-      end
     
-          opts.on('-i', '--in-place',
-        'Convert a file to its own syntax.',
-        'This can be used to update some deprecated syntax.') do
-        @options[:in_place] = true
-      end
+          private
     
-          opts.on('--precision NUMBER_OF_DIGITS', Integer,
-              'How many digits of precision to use when outputting decimal numbers.',
-              'Defaults to #{Sass::Script::Value::Number.precision}.') do |precision|
-        Sass::Script::Value::Number.precision = precision
-      end
+            def update
+          @image = scope.images.accessible_by(current_ability, :update).find(params[:id])
+          if @image.update_attributes(image_params)
+            respond_with(@image, default_template: :show)
+          else
+            invalid_resource!(@image)
+          end
+        end
+    
+            def destroy
+          @option_type = Spree::OptionType.accessible_by(current_ability, :destroy).find(params[:id])
+          @option_type.destroy
+          render plain: nil, status: 204
+        end
+    
+            def scope
+          @scope ||= if params[:option_type_id]
+                       Spree::OptionType.find(params[:option_type_id]).option_values.accessible_by(current_ability, :read)
+                     else
+                       Spree::OptionValue.accessible_by(current_ability, :read).load
+                     end
+        end
+    
+            def create
+          @order.validate_payments_attributes([payment_params])
+          @payment = @order.payments.build(payment_params)
+          if @payment.save
+            respond_with(@payment, status: 201, default_template: :show)
+          else
+            invalid_resource!(@payment)
+          end
+        end
+    
+            def authorize_product!
+          authorize! :read, @product
+        end
