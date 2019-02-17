d23799f08ed8c447e2c2d695704caddd9d549124
SUNsung <weatherius@gmail.com>
2019-02-17 10:13:08 +0200

    Rewrite [35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb] File in: 2019-02-17 10:13:07

--- ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
+++ ruby/35ZV5YGggPFOg358t5MMFGtJWU35gjKPBx6zfY635Zx3.rb
@@ -1,121 +1,13 @@
 
         
-        FORWARD_SLASH = '/'.freeze
+          it 'correctly sets notification level' do
+    moderator = Fabricate(:moderator)
     
-    Benchmark.ips do |x|
-  x.report('no body include?') { CONTENT_NOT_CONTAINING.include?('<body') }
-  x.report('no body regexp')   { CONTENT_NOT_CONTAINING =~ /<\s*body/ }
-  x.compare!
-end
-    
-          def print_summary(features)
-        @io.puts
-        print_stats(features, @options)
-        print_snippets(@options)
-        print_passing_wip(@options)
-      end
-    end
-  end
-end
-    
-    def file_contents(path)
-  return Pathname.new(path).read
-end
-    
-              # WebSockets requests will have a Connection: Upgrade header
-          if parser.http_method != 'GET' || parser.upgrade?
-            super
-          elsif parser.request_url =~ %r!^\/livereload.js!
-            headers = [
-              'HTTP/1.1 200 OK',
-              'Content-Type: application/javascript',
-              'Content-Length: #{reload_size}',
-              '',
-              '',
-            ].join('\r\n')
-            send_data(headers)
-    
-    Group.user_trust_level_change!(-1, TrustLevel[4])
-    
-            if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-        def ==(other)
-      other.name == name && other.path == path && other.type == type
-    end
-    
-            def store_pages(store)
-          instrument 'info.doc', msg: 'Building internal urls...'
-          with_internal_urls do
-            instrument 'info.doc', msg: 'Continuing...'
-            super
-          end
-        end
-    
-        def load_capybara_selenium
-      require 'capybara/dsl'
-      require 'selenium/webdriver'
-      Capybara.register_driver :chrome do |app|
-        options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
-        Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-      end
-      Capybara.javascript_driver = :chrome
-      Capybara.current_driver = :chrome
-      Capybara.run_server = false
-      Capybara
-    end
-    
-        def initialize(*args)
-      if args.empty?
-        super(*Array.new(9))
-      elsif args.length == 1 && args.first.is_a?(Hash)
-        args.first.assert_valid_keys URI::Generic::COMPONENT
-        super(*args.first.values_at(*URI::Generic::COMPONENT))
-      else
-        super
-      end
-    end
-    
-            name = name.split(':').first
-    
-            css('.nav-index-section').each do |node|
-          node.content = node.content
-        end
-    
-      it 'accepts and uses a seed of 0' do
-    srand(0)
-    srand.should == 0
-  end
-    
-      gem.add_development_dependency 'danger'
-  gem.add_development_dependency 'mocha'
-  gem.add_development_dependency 'rspec'
-  gem.add_development_dependency 'rubocop', '0.48.1'
-end
-
-    
-      at_exit do
-    if ENV['KEEP_RUNNING']
-      puts 'Vagrant vm will be left up because KEEP_RUNNING is set.'
-      puts 'Rerun without KEEP_RUNNING set to cleanup the vm.'
-    else
-      vagrant_cli_command('destroy -f')
-    end
-  end
-    
-          attr_reader :key, :default, :options
-    
-    MESSAGE
-      end
-    
-          def matches?(other)
-        # This matching logic must stay in sync with `Servers#add_host`.
-        hostname == other.hostname && port == other.port
-      end
-    
-          def servers_by_key
-        @servers_by_key ||= {}
-      end
+          if staff.topic_id.nil?
+        creator = PostCreator.new(Discourse.system_user,
+          raw: I18n.t('staff_category_description'),
+          title: I18n.t('category.topic_prefix', category: staff.name),
+          category: staff.name,
+          archetype: Archetype.default
+        )
+        post = creator.create
