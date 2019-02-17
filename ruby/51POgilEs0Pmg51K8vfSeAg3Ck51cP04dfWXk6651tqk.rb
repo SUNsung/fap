4f9ce6241c72f21732362c5fa0f40862d919aaad
SUNsung <weatherius@gmail.com>
2019-02-17 02:20:28 +0200

    Rewrite [51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb] File in: 2019-02-17 02:20:27

--- ruby/51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb
+++ ruby/51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb
@@ -1,124 +1,99 @@
 
         
-            def as_json
-      { name: name, path: path, type: type }
-    end
-  end
-end
-
+          find_files = ->(path) {
+    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
+      path if File.file?(path)
+    end.compact
+  }
     
-        def initialize
-      @pages = {}
-    end
-    
-            css('.code-example > h4').each do |node|
-          node['class'] = 'pre-title'
-        end
-    
-            css('code').each do |node|
-          node.inner_html = node.inner_html.squish
-        end
-    
-        # The content passed to this environment. If the content's environment isn't already
-    # read-only, it's made read-only.
-    #
-    # @see BaseEnvironment#content
-    #
-    # @return {[Array<Sass::Tree::Node>, ReadOnlyEnvironment]?} The content nodes and
-    #   the lexical environment of the content block.
-    #   Returns `nil` when there is no content in this environment.
-    def content
-      # Return the cached content from a previous invocation if any
-      return @content if @content_cached
-      # get the content with a read-write environment from the superclass
-      read_write_content = super
-      if read_write_content
-        tree, env = read_write_content
-        # make the content's environment read-only
-        if env && !env.is_a?(ReadOnlyEnvironment)
-          env = ReadOnlyEnvironment.new(env, env.options)
-        end
-        @content_cached = true
-        @content = [tree, env]
-      else
-        @content_cached = true
-        @content = nil
-      end
-    end
-  end
-    
-    module Sass::Exec
-  # The abstract base class for Sass executables.
-  class Base
-    # @param args [Array<String>] The command-line arguments
-    def initialize(args)
-      @args = args
-      @options = {}
+      def up_down(change)
+    change.up do
+      Mention.update_all(mentions_container_type: 'Post')
+      change_column :mentions, :mentions_container_type, :string, null: false
+      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
     end
     
-      def package(gem, ext='')
-    'pkg/#{gem}-#{source_version}' + ext
-  end
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
     
-          def html?(headers)
-        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
-        options[:html_types].include? header.last[/^\w+\/\w+/]
+    describe StatusMessagesController, :type => :controller do
+  describe '#bookmarklet' do
+    before do
+      sign_in bob, scope: :user
+    end
+    
+        it 'marks all notifications in the current filter as read' do
+      request.env['HTTP_REFERER'] = 'I wish I were spelled right'
+      FactoryGirl.create(:notification, recipient: alice, target: post)
+      FactoryGirl.create(:notification, recipient: alice, type: 'Notifications::StartedSharing')
+      expect(Notification.where(unread: true).count).to eq(2)
+      get :read_all, params: {type: 'started_sharing'}
+      expect(Notification.where(unread: true).count).to eq(1)
+    end
+    
+        context 'on a post you do not partecipate to' do
+      it 'says it is an unprocessable request' do
+        delete :destroy, params: {post_id: post.id}
+        expect(response.code).to eq('422')
       end
     end
   end
 end
 
     
-          def escape_string(str)
-        str = @escaper.escape_url(str)        if @url
-        str = @escaper.escape_html(str)       if @html
-        str = @escaper.escape_javascript(str) if @javascript
-        str
-      end
-    end
-  end
-end
-
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
     
-        it 'Returns nil when Referer header is missing and allow_empty_referrer is false' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      subject.options[:allow_empty_referrer] = false
-      expect(subject.referrer(env)).to be_nil
-    end
+    # Exit cleanly from an early interrupt
+Signal.trap('INT') { exit 1 }
     
-          def insert_before(oldklass, newklass, *args)
-        i = entries.index { |entry| entry.klass == newklass }
-        new_entry = i.nil? ? Entry.new(newklass, *args) : entries.delete_at(i)
-        i = entries.index { |entry| entry.klass == oldklass } || 0
-        entries.insert(i, new_entry)
+    class LogStash::PluginManager::Pack < LogStash::PluginManager::PackCommand
+  option '--tgz', :flag, 'compress package as a tar.gz file', :default => !LogStash::Environment.windows?
+  option '--zip', :flag, 'compress package as a zip file', :default => LogStash::Environment.windows?
+  option '--[no-]clean', :flag, 'clean up the generated dump of plugins', :default => true
+  option '--overwrite', :flag, 'Overwrite a previously generated package file', :default => false
+    
+        class << self
+      def elastic_pack_base_uri
+        env_url = ENV['LOGSTASH_PACK_URL']
+        (env_url.nil? || env_url.empty?) ? DEFAULT_PACK_URL : env_url
       end
     
-      module TestingClient
-    def raw_push(payloads)
-      if Sidekiq::Testing.fake?
-        payloads.each do |job|
-          job = Sidekiq.load_json(Sidekiq.dump_json(job))
-          job.merge!('enqueued_at' => Time.now.to_f) unless job['at']
-          Queues.push(job['queue'], job['class'], job)
+          options = {:debug => ENV['LS_QA_DEBUG']}
+      puts 'Destroying #{machines}'
+      LogStash::VagrantHelpers.destroy(machines, options)
+      puts 'Bootstrapping #{machines}'
+      LogStash::VagrantHelpers.bootstrap(machines, options)
+    end
+    
+          it 'list the plugins with their versions' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose')
+        result.stdout.split('\n').each do |plugin|
+          expect(plugin).to match(/^logstash-\w+-\w+\s\(\d+\.\d+.\d+(.\w+)?\)/)
         end
-        true
-      elsif Sidekiq::Testing.inline?
-        payloads.each do |job|
-          klass = Sidekiq::Testing.constantize(job['class'])
-          job['id'] ||= SecureRandom.hex(12)
-          job_hash = Sidekiq.load_json(Sidekiq.dump_json(job))
-          klass.process_job(job_hash)
-        end
-        true
-      else
-        super
       end
     end
-  end
     
-        def watchdog(last_words)
-      yield
-    rescue Exception => ex
-      handle_exception(ex, { context: last_words })
-      raise ex
-    end
+          def page_name
+        @name.gsub('-', ' ')
+      end
+    
+          def remove_page_extentions(page_path)
+        Gollum::Markup.formats.values.each do |format|
+          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
+        end
+        return page_path
+      end
+    
+          def title
+        'Home'
+      end
+    
+      if cfg = options[:config]
+    # If the path begins with a '/' it will be considered an absolute path,
+    # otherwise it will be relative to the CWD
+    cfg = File.join(Dir.getwd, cfg) unless cfg.slice(0) == File::SEPARATOR
+    require cfg
+  end
