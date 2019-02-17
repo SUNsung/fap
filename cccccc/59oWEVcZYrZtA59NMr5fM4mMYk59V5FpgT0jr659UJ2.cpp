7dd77442341440005fbbc29613065df0905d4166
SUNsung <weatherius@gmail.com>
2019-02-17 13:16:57 +0200

    Rewrite [59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp] File in: 2019-02-17 13:16:57

--- cccccc/59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp
+++ cccccc/59oWEVcZYrZtA59NMr5fM4mMYk59V5FpgT0jr659UJ2.cpp
@@ -1,242 +1,169 @@
 
         
-        #include <cstdint>      // for int32_t
-    
-    
-    {}  // namespace tesseract
+        
+    {}  // end namespace tensorflow
 
     
-    // Computes part of matrix.vector v = Wu. Computes N=16 results.
-// For details see PartialMatrixDotVector64 with N=16.
-static void PartialMatrixDotVector16(const int8_t* wi, const double* scales,
-                                     const int8_t* u, int num_in, int num_out,
-                                     double* v) {
-  // Register containing 16-bit ones for horizontal add with 16->32 bit
-  // conversion.
-  __m256i ones =
-      _mm256_set_epi16(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
-  __m256i shift_id = _mm256_set_epi32(0, 7, 6, 5, 4, 3, 2, 1);
-  // Initialize all the results to 0.
-  __m256i result0 = _mm256_setzero_si256();
-  __m256i result1 = _mm256_setzero_si256();
-  // Iterate over the input (u), one registerful at a time.
-  for (int j = 0; j < num_in;) {
-    __m256i inputs =
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(u + j));
-    // Inputs are processed in groups of kNumInputsPerGroup, replicated
-    // kNumInputGroups times.
-    for (int ig = 0; ig < kNumInputGroups && j < num_in;
-         ++ig, j += kNumInputsPerGroup) {
-      // Replicate the low 32 bits (4 inputs) 8 times.
-      __m256i rep_input =
-          _mm256_broadcastd_epi32(_mm256_castsi256_si128(inputs));
-      // Rotate the inputs in groups of 4, so the next 4 inputs are ready.
-      inputs = _mm256_permutevar8x32_epi32(inputs, shift_id);
-      __m256i weights, reps;
-      // Mul-add, with horizontal add of the 4 inputs to each of the results.
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result0);
-      MultiplyGroup(rep_input, ones, wi, weights, reps, result1);
-    }
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Register the bfloat16 numpy type.
+void RegisterNumpyBfloat16();
+    
+    Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
+                       std::function<void()> destructor, PyObject** result) {
+  int size = 1;
+  for (int i = 0; i < dim_size; ++i) {
+    size *= dims[i];
   }
-  ExtractResults(result0, shift_id, wi, scales, kNumOutputsPerRegister, v);
-  num_out -= kNumOutputsPerRegister;
-  ExtractResults(result1, shift_id, wi, scales,
-                 std::min(kNumOutputsPerRegister, num_out), v);
-}
-    
-    #if defined(X86_BUILD)
-#if defined(__GNUC__)
-#include <cpuid.h>
-#elif defined(_WIN32)
-#include <intrin.h>
-#endif
-#endif
-    
-    namespace tesseract {
-    }
-    
-    
-// Write all (changed_) parameters to a config file.
-void ParamsEditor::WriteParams(char *filename,
-                               bool changes_only) {
-  FILE *fp;                      // input file
-  char msg_str[255];
-                                 // if file exists
-  if ((fp = fopen (filename, 'rb')) != nullptr) {
-    fclose(fp);
-    sprintf (msg_str, 'Overwrite file ' '%s' '? (Y/N)', filename);
-    int a = sv_window_->ShowYesNoDialog(msg_str);
-    if (a == 'n') {
-      return;
-    }  // don't write
+  if (dtype == DT_STRING || dtype == DT_RESOURCE || size == 0) {
+    return errors::FailedPrecondition(
+        'Cannot convert strings, resources, or empty Tensors.');
   }
     }
     
+    // Creates a numpy array with shapes specified by dim_size and dims and content
+// in data. The array does not own the memory, and destructor will be called to
+// release it. If the status is not ok the caller is responsible for releasing
+// the memory.
+Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
+                       std::function<void()> destructor, PyObject** result);
     
-/**********************************************************************
- * split_word
- *
- * Split a given WERD_RES in place into two smaller words for recognition.
- * split_pt is the index of the first blob to go in the second word.
- * The underlying word is left alone, only the TWERD (and subsequent data)
- * are split up.  orig_blamer_bundle is set to the original blamer bundle,
- * and will now be owned by the caller.  New blamer bundles are forged for the
- * two pieces.
- **********************************************************************/
-void Tesseract::split_word(WERD_RES *word,
-                           int split_pt,
-                           WERD_RES **right_piece,
-                           BlamerBundle **orig_blamer_bundle) const {
-  ASSERT_HOST(split_pt >0 && split_pt < word->chopped_word->NumBlobs());
+    namespace tensorflow {
+namespace swig {
+    }
     }
     
-    struct Pix;
     
-    #include 'blread.h'
-#include <cstdio>       // for fclose, fopen, FILE
-#include 'host.h'       // for TRUE
-#include 'ocrblock.h'   // for BLOCK_IT, BLOCK, BLOCK_LIST (ptr only)
-#include 'scanutils.h'  // for tfscanf
-    
-    // logistic loss for probability regression task
-struct LogisticRegression {
-  // duplication is necessary, as __device__ specifier
-  // cannot be made conditional on template parameter
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return common::Sigmoid(x); }
-  XGBOOST_DEVICE static bool CheckLabel(bst_float x) { return x >= 0.0f && x <= 1.0f; }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    return predt - label;
-  }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    const float eps = 1e-16f;
-    return fmaxf(predt * (1.0f - predt), eps);
-  }
-  template <typename T>
-  static T PredTransform(T x) { return common::Sigmoid(x); }
-  template <typename T>
-  static T FirstOrderGradient(T predt, T label) { return predt - label; }
-  template <typename T>
-  static T SecondOrderGradient(T predt, T label) {
-    const T eps = T(1e-16f);
-    return std::max(predt * (T(1.0f) - predt), eps);
-  }
-  static bst_float ProbToMargin(bst_float base_score) {
-    CHECK(base_score > 0.0f && base_score < 1.0f)
-      << 'base_score must be in (0,1) for logistic loss';
-    return -logf(1.0f / base_score - 1.0f);
-  }
-  static const char* LabelErrorMsg() {
-    return 'label must be in [0,1] for logistic regression';
-  }
-  static const char* DefaultEvalMetric() { return 'rmse'; }
+    { private:
+  unsigned flags_;
 };
     
-    namespace rabit {
-namespace utils {
-extern 'C' {
-  void (*Printf)(const char *fmt, ...) = Rprintf;
-  void (*Assert)(int exp, const char *fmt, ...) = XGBoostAssert_R;
-  void (*Check)(int exp, const char *fmt, ...) = XGBoostCheck_R;
-  void (*Error)(const char *fmt, ...) = error;
-}
-}
-}
+        const QString &getAppName() const { return appName; }
+    const QIcon &getAppIcon() const { return appIcon; }
+    const QIcon &getTrayAndWindowIcon() const { return trayAndWindowIcon; }
+    const QString &getTitleAddText() const { return titleAddText; }
     
-    struct SplitEvaluatorReg
-    : public dmlc::FunctionRegEntryBase<SplitEvaluatorReg,
-        std::function<SplitEvaluator* (std::unique_ptr<SplitEvaluator>)> > {};
+    #include <QDialog>
     
-    ConsoleReporter::OutputOptions GetOutputOptions(bool force_no_color = false);
-    
-    void ColorPrintf(std::ostream& out, LogColor color, const char* fmt,
-                 va_list args);
-void ColorPrintf(std::ostream& out, LogColor color, const char* fmt, ...);
-    
-      if (result.error_occurred) {
-    printer(Out, COLOR_RED, 'ERROR OCCURRED: \'%s\'',
-            result.error_message.c_str());
-    printer(Out, COLOR_DEFAULT, '\n');
-    return;
-  }
-  // Format bytes per second
-  std::string rate;
-  if (result.bytes_per_second > 0) {
-    rate = StrCat(' ', HumanReadableNumber(result.bytes_per_second), 'B/s');
-  }
-    
-        // print the header
-    for (auto B = elements.begin(); B != elements.end();) {
-      Out << *B++;
-      if (B != elements.end()) Out << ',';
+    BOOST_AUTO_TEST_CASE(bip173_testvectors_invalid)
+{
+    static const std::string CASES[] = {
+        ' 1nwldj5',
+        '\x7f''1axkwrx',
+        '\x80''1eym55h',
+        'an84characterslonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1569pvx',
+        'pzry9x0s0muk',
+        '1pzry9x0s0muk',
+        'x1b4n0q5v',
+        'li1dgmt3',
+        'de1lg7wt\xff',
+        'A1G7SGD8',
+        '10a06t8',
+        '1qzzfhee',
+        'a12UEL5L',
+        'A12uEL5L',
+    };
+    for (const std::string& str : CASES) {
+        auto ret = bech32::Decode(str);
+        BOOST_CHECK(ret.first.empty());
     }
-    for (auto B = user_counter_names_.begin(); B != user_counter_names_.end();) {
-      Out << ',\'' << *B++ << '\'';
-    }
-    Out << '\n';
-    
-    void JSONReporter::ReportRuns(std::vector<Run> const& reports) {
-  if (reports.empty()) {
-    return;
-  }
-  std::string indent(4, ' ');
-  std::ostream& out = GetOutputStream();
-  if (!first_report_) {
-    out << ',\n';
-  }
-  first_report_ = false;
-    }
-    
-    
-    {}  // end namespace benchmark
-    
-    
-    {void SleepForSeconds(double seconds) {
-  SleepForMicroseconds(static_cast<int>(seconds * kNumMicrosPerSecond));
 }
-#endif  // BENCHMARK_OS_WINDOWS
-}  // end namespace benchmark
-
     
-      {
-    auto ka = getKeepAliveToken(exec);
-    EXPECT_TRUE(ka);
-    EXPECT_EQ(&exec, ka.get());
-    EXPECT_EQ(1, exec.refCount);
+        // Other valid inputs
+    CheckParseTorReplyMapping(
+        'Foo=Bar=Baz Spam=Eggs', {
+            {'Foo', 'Bar=Baz'},
+            {'Spam', 'Eggs'},
+        });
+    CheckParseTorReplyMapping(
+        'Foo=\'Bar=Baz\'', {
+            {'Foo', 'Bar=Baz'},
+        });
+    CheckParseTorReplyMapping(
+        'Foo=\'Bar Baz\'', {
+            {'Foo', 'Bar Baz'},
+        });
+    
+    const UniValue& UniValue::get_obj() const
+{
+    if (typ != VOBJ)
+        throw std::runtime_error('JSON value is not an object as expected');
+    return *this;
+}
+    
+    
+    {}  // namespace nwapi
+    
+    #include 'base/strings/string_piece.h'
+#include 'v8/include/v8.h'
+    
+    
+#include 'content/nw/src/api/event/event.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'ui/gfx/screen.h'
+    
+    namespace nw {
     }
     
-      FunctionRef<int(int)> variant1 = of;
-  EXPECT_EQ(100 + 1 * 15, variant1(15));
-  FunctionRef<int(int)> const cvariant1 = of;
-  EXPECT_EQ(100 + 1 * 15, cvariant1(15));
     
-      /**
-   * Create a new RNG, seeded with a good seed.
-   *
-   * Note that you should usually use ThreadLocalPRNG unless you need
-   * reproducibility (such as during a test), in which case you'd want
-   * to create a RNG with a good seed in production, and seed it yourself
-   * in test.
-   */
-  template <class RNG = DefaultGenerator, class /* EnableIf */ = ValidRNG<RNG>>
-  static RNG create();
-    
-    #include <folly/Range.h>
+    {
+  DECLARE_EXTENSION_FUNCTION('nw.App.setProxyConfig', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwAppSetProxyConfigFunction);
+};
     
     
-    {} // namespace uri_detail
+    {  return true;
+}
     
-      bool compare_exchange_weak(
-      SharedPtr& expected,
-      const SharedPtr& n,
-      std::memory_order mo = std::memory_order_seq_cst) noexcept {
-    return compare_exchange_weak(
-        expected, n, mo, detail::default_failure_memory_order(mo));
-  }
-  bool compare_exchange_weak(
-      SharedPtr& expected,
-      const SharedPtr& n,
-      std::memory_order success,
-      std::memory_order failure) /* noexcept */ {
-    auto newptr = get_newptr(n);
-    PackedPtr oldptr, expectedptr;
+    class NwObjCallObjectMethodSyncFunction : public NWSyncExtensionFunction {
+ public:
+  NwObjCallObjectMethodSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+        if(head == NULL){
+        cout<<'NULL'<<endl;
+        return;
+    }
+    
+    
+    {
+    {        if( node ){
+            __inorderTraversal(node->left, res);
+            res.push_back( node->val );
+            __inorderTraversal(node->right, res);
+        }
+    }
+};
+    
+    
+    {                if(node->left){
+                    q.push(node->left);
+                    new_level_num ++;
+                }
+                if(node->right){
+                    q.push(node->right);
+                    new_level_num ++;
+                }
+            }
+    
+    // Recursive
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+    }
+    }
+    
+            stack<TagNode> stack;
+        TreeNode* cur = root;
+        while(cur != NULL || !stack.empty()){
     }
