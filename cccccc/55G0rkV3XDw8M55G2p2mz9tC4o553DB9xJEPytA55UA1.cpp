34b5104329436070c2c43b8165b8b790aebfdd97
SUNsung <weatherius@gmail.com>
2019-02-19 01:44:25 +0200

    Rewrite [55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp] File in: 2019-02-19 01:44:24

--- cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
+++ cccccc/55G0rkV3XDw8M55G2p2mz9tC4o553DB9xJEPytA55UA1.cpp
@@ -1,229 +1,158 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+        #ifndef ATOM_APP_COMMAND_LINE_ARGS_H_
+#define ATOM_APP_COMMAND_LINE_ARGS_H_
     
-    // Hash function for PyBfloat16. We use the identity function, which is a weak
-// hash function.
-HashType PyBfloat16_Hash(PyObject* self) {
-  bfloat16 x = reinterpret_cast<PyBfloat16*>(self)->value;
-  return x.value;
-}
-    
-    void ImportNumpy() {
-  import_array1();
-}
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-      // Attempt to get the next record at 'current_offset()'. Populates status
-  // with OK on success, OUT_OF_RANGE for end of file, DATA_LOSS for some
-  // kinds of truncated reads, or another code for other errors
-  // (e.g., filesystem errors).
-  void GetNext(TF_Status* status);
-    
-    // FFT support interface -- this can be derived from a GPU executor when the
-// underlying platform has an FFT library implementation available. See
-// StreamExecutor::AsFft().
-//
-// This support interface is not generally thread-safe; it is only thread-safe
-// for the CUDA platform (cuFFT) usage; host side FFT support is known
-// thread-compatible, but not thread-safe.
-class FftSupport {
+    class InAppPurchase : public mate::EventEmitter<InAppPurchase>,
+                      public in_app_purchase::TransactionObserver {
  public:
-  virtual ~FftSupport() {}
+  static mate::Handle<InAppPurchase> Create(v8::Isolate* isolate);
     }
     
-    namespace content {
-class RenderProcessHost;
-class Shell;
-}
+    #include 'atom/browser/render_process_preferences.h'
+#include 'native_mate/handle.h'
+#include 'native_mate/wrappable.h'
+    
+    #include <vector>
+    
+    #include 'content/public/browser/web_contents_observer.h'
+#include 'native_mate/handle.h'
+#include 'native_mate/wrappable.h'
     
     
-    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  return item->is_checked_;
-}
-    
-       bool IsCommandIdChecked(int command_id) const override;
-   bool IsCommandIdEnabled(int command_id) const override;
-    
-    void Menu::Create(const base::DictionaryValue& option) {
-  is_menu_modified_ = true;
-  menu_delegate_.reset(new MenuDelegate(object_manager()));
-  menu_model_.reset(new ui::NwMenuModel(menu_delegate_.get()));
-    }
-    
-    
-    {  for (AppWindow* window : windows) {
-    if (window->NWCanClose())
-      window->GetBaseWindow()->Close();
-  }
-}
-    
-    NwObjCallObjectMethodSyncFunction::~NwObjCallObjectMethodSyncFunction() {
-}
-    
-    #  define EXPECT_DEBUG_DEATH(statement, regex) \
-  GTEST_EXECUTE_STATEMENT_(statement, regex)
-    
-    // A copyable object representing the result of a test part (i.e. an
-// assertion or an explicit FAIL(), ADD_FAILURE(), or SUCCESS()).
-//
-// Don't inherit from TestPartResult as its destructor is not virtual.
-class GTEST_API_ TestPartResult {
- public:
-  // The possible outcomes of a test part (i.e. an assertion or an
-  // explicit SUCCEED(), FAIL(), or ADD_FAILURE()).
-  enum Type {
-    kSuccess,          // Succeeded.
-    kNonFatalFailure,  // Failed but the test can continue.
-    kFatalFailure      // Failed and the test should be terminated.
-  };
-    }
-    
-    inline int RUN_ALL_TESTS() {
-  return ::testing::UnitTest::GetInstance()->Run();
-}
-    
-    
-    {
-    {    // Next, recurses (at compile time) with the tail of the test list.
-    return TypeParameterizedTestCase<Fixture, typename Tests::Tail, Types>
-        ::Register(prefix, case_name, SkipComma(test_names));
-  }
+    {  DISALLOW_COPY_AND_ASSIGN(AtomQuotaPermissionContext);
 };
     
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12>
-struct Types12 {
-  typedef T1 Head;
-  typedef Types11<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> Tail;
-};
+    void AutoUpdater::QuitAndInstall() {}
+#endif
     
-    #ifndef GTEST_SAMPLES_SAMPLE2_H_
-#define GTEST_SAMPLES_SAMPLE2_H_
-    
-     public:
-  // Gets the element in this node.
-  const E& element() const { return element_; }
-    
-    void ChannelArguments::SetSslTargetNameOverride(const grpc::string& name) {
-  SetString(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG, name);
+    void URLRequestAboutJob::Kill() {
+  weak_ptr_factory_.InvalidateWeakPtrs();
+  URLRequestJob::Kill();
 }
     
-    const ViewDescriptor& ServerSentMessagesPerRpcHour() {
-  const static ViewDescriptor descriptor =
-      HourDescriptor()
-          .set_name('grpc.io/server/sent_messages_per_rpc/hour')
-          .set_measure(kRpcServerSentMessagesPerRpcMeasureName)
-          .set_aggregation(CountDistributionAggregation())
-          .add_column(ServerMethodTagKey());
-  return descriptor;
+    Status serializeRow(const RowTyped& r,
+                    const ColumnNames& cols,
+                    JSON& doc,
+                    rj::Value& obj) {
+  DocAppenderVisitor visitor(doc, obj);
+    }
+    
+    namespace osquery {
+    }
+    
+    
+Extension_shutdown_result::~Extension_shutdown_result() throw() {
 }
     
-    Status ProtoServerReflection::ServerReflectionInfo(
-    ServerContext* context,
-    ServerReaderWriter<ServerReflectionResponse, ServerReflectionRequest>*
-        stream) {
-  ServerReflectionRequest request;
-  ServerReflectionResponse response;
-  Status status;
-  while (stream->Read(&request)) {
-    switch (request.message_request_case()) {
-      case ServerReflectionRequest::MessageRequestCase::kFileByFilename:
-        status = GetFileByName(context, request.file_by_filename(), &response);
-        break;
-      case ServerReflectionRequest::MessageRequestCase::kFileContainingSymbol:
-        status = GetFileContainingSymbol(
-            context, request.file_containing_symbol(), &response);
-        break;
-      case ServerReflectionRequest::MessageRequestCase::
-          kFileContainingExtension:
-        status = GetFileContainingExtension(
-            context, &request.file_containing_extension(), &response);
-        break;
-      case ServerReflectionRequest::MessageRequestCase::
-          kAllExtensionNumbersOfType:
-        status = GetAllExtensionNumbers(
-            context, request.all_extension_numbers_of_type(),
-            response.mutable_all_extension_numbers_response());
-        break;
-      case ServerReflectionRequest::MessageRequestCase::kListServices:
-        status =
-            ListService(context, response.mutable_list_services_response());
-        break;
-      default:
-        status = Status(StatusCode::UNIMPLEMENTED, '');
-    }
+      ExtensionManager_getQueryColumns_args args;
+  args.read(iprot);
+  iprot->readMessageEnd();
+  uint32_t bytes = iprot->getTransport()->readEnd();
+    
+    
+    
+    namespace osquery {
+namespace tables {
     }
     }
     
+      std::vector<std::string> line_exports;
+  unsigned int readonly = 0;
+  int options_index = -1;
     
-    {
-    {}  // namespace load_reporter
-}  // namespace grpc
+    TEST_F(SystemCpuTests, decodeMaskFromString_fail_0) {
+  auto exp = cpu::decodeMaskFromString('1-0');
+  ASSERT_TRUE(exp.isError());
+  ASSERT_EQ(exp.getErrorCode(), cpu::Error::IncorrectRange);
+}
     
-     private:
-  uint64_t NowMicrosMonotonic(Env* env);
+    void DHTRoutingTableSerializer::setLocalNode(
+    const std::shared_ptr<DHTNode>& localNode)
+{
+  localNode_ = localNode;
+}
     
-    // Example structure that describes a compaction task.
-struct CompactionTask {
-  CompactionTask(
-      DB* _db, Compactor* _compactor,
-      const std::string& _column_family_name,
-      const std::vector<std::string>& _input_file_names,
-      const int _output_level,
-      const CompactionOptions& _compact_options,
-      bool _retry_on_fail)
-          : db(_db),
-            compactor(_compactor),
-            column_family_name(_column_family_name),
-            input_file_names(_input_file_names),
-            output_level(_output_level),
-            compact_options(_compact_options),
-            retry_on_fail(_retry_on_fail) {}
-  DB* db;
-  Compactor* compactor;
-  const std::string& column_family_name;
-  std::vector<std::string> input_file_names;
-  int output_level;
-  CompactionOptions compact_options;
-  bool retry_on_fail;
-};
+    #include 'common.h'
     
-      std::vector<ColumnFamilyDescriptor> cf_descs;
-  cf_descs.push_back({kDefaultColumnFamilyName, ColumnFamilyOptions()});
-  cf_descs.push_back({'new_cf', ColumnFamilyOptions()});
+    namespace aria2 {
+    }
     
-    #pragma once
+    void DHTTaskQueueImpl::executeTask()
+{
+  A2_LOG_DEBUG('Updating periodicTaskQueue1');
+  periodicTaskQueue1_.update();
+  A2_LOG_DEBUG('Updating periodicTaskQueue2');
+  periodicTaskQueue2_.update();
+  A2_LOG_DEBUG('Updating immediateTaskQueue');
+  immediateTaskQueue_.update();
+}
     
-      BackupInfo() {}
-    
-    
-    {}  // namespace rocksdb
-#endif  // !ROCKSDB_LITE
-
-    
-      static LDBCommandExecuteResult Succeed(std::string msg) {
-    return LDBCommandExecuteResult(EXEC_SUCCEED, msg);
+    bool DHTTokenTracker::validateToken(const std::string& token,
+                                    const unsigned char* infoHash,
+                                    const std::string& ipaddr,
+                                    uint16_t port) const
+{
+  for (auto& elem : secret_) {
+    if (generateToken(infoHash, ipaddr, port, elem) == token) {
+      return true;
+    }
   }
-    
-    
-    {    // exception out_of_range.401
-    try
-    {
-        // try to write beyond the array limit
-        array.at(5) = 'sixth';
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+  return false;
 }
+    
+    bool DHTUnknownMessage::send() { return true; }
+    
+    using namespace std;
+    
+    private:
+    void __inorderTraversal(TreeNode* node, vector<int> &res){
+    }
+    
+    /// BFS
+/// No need to store level information in the queue :-)
+///
+/// Time Complexity: O(n), where n is the number of nodes in the tree
+/// Space Complexity: O(n)
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+    }
+    }
+    
+            vector<int> res;
+        if(root == NULL)
+            return res;
+    
+    #include <iostream>
+#include <vector>
+#include <stack>
+#include <cassert>
+    
+    
+    
+    #define SIZE    100
+    
+        shared_ptr<string> val_str = make_shared<string>('hello');
+    cache.set('test1', val_str); // update test1 and will del test2
+    ASSERT_EQ(cache.get('test1').get(), val_str.get());
+    ASSERT_EQ(dtor_num, 2);
+    
+    #include 'channel.h'
+    
+            if (!deflater)
+        {
+            ret = nghttp2_hd_deflate_new(&deflater, SW_HTTP2_DEFAULT_HEADER_TABLE_SIZE);
+            if (ret != 0)
+            {
+                swoole_php_error(E_WARNING, 'nghttp2_hd_deflate_init() failed with error: %s\n', nghttp2_strerror(ret));
+                return SW_ERR;
+            }
+            client->deflater = deflater;
+        }
+    
+        int i;
+    for (i = 0; i < WRITE_N; i++)
+    {
+        size = 10000 + rand() % 90000;
+    }
