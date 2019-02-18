a56d27ae399e0e52940a9d491d3bd0ceb2784922
SUNsung <weatherius@gmail.com>
2019-02-18 19:35:14 +0200

    Rewrite [506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb] File in: 2019-02-18 19:35:14

--- ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
+++ ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
@@ -1,44 +1,19 @@
 
         
-              if lounge.topic_id.nil?
-        creator = PostCreator.new(Discourse.system_user,
-          raw: I18n.t('vip_category_description'),
-          title: I18n.t('category.topic_prefix', category: lounge.name),
-          category: lounge.name,
-          archetype: Archetype.default,
-          skip_validations: true
-        )
-        post = creator.create
-    
-        def pattern_path(path)
-      return ::File.join(LOGSTASH_HOME, 'patterns', path)
-    end
-    
-    module LogStash
-  module PluginManager
+          def pinned_version
+    Keg.new(path.resolved_path).version if pinned?
   end
 end
+
     
-          def pack_uri(plugin_name)
-        url = '#{elastic_pack_base_uri}/#{plugin_name}/#{plugin_name}-#{LOGSTASH_VERSION}.#{PACK_EXTENSION}'
-        URI.parse(url)
+            -> { w.f4('', 0) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.warn_call_lineno}: warning: \n$|)
+        -> { w.f4(nil, 0) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.warn_call_lineno}: warning: \n$|)
       end
     
-        FileUtils.rm_rf(LogStash::Environment::CACHE_PATH)
-    validate_cache_location
-    archive_manager.extract(package_file, LogStash::Environment::CACHE_PATH)
-    puts('Unpacked at #{LogStash::Environment::CACHE_PATH}')
-    puts('The unpacked plugins can now be installed in local-only mode using bin/logstash-plugin install --local [plugin name]')
-  end
+    When /^I (?:log|sign) out$/ do
+  logout
+  step 'I go to the root page'
+end
     
-        desc 'Halt all VM's involved in the acceptance test round'
-    task :halt, :platform do |t, args|
-      config   = PlatformConfig.new
-      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
-      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
-    
-        context 'without a specific plugin' do
-      it 'display a list of plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list')
-        expect(result.stdout.split('\n').size).to be > 1
+            self
       end
