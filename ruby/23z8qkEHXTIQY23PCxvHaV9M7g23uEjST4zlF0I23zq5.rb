cc8b0e05c6669d4c01c394cff63cfc3d88e5c955
SUNsung <weatherius@gmail.com>
2019-02-18 20:35:17 +0200

    Rewrite [23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb] File in: 2019-02-18 20:35:16

--- ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
+++ ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
@@ -1,35 +1,150 @@
 
         
-        ```
-Executable Path: #{actual_path}
-```
-EOS
+              private
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   CSRF
+    # Supported browsers:: all
+    # More infos::         http://flask.pocoo.org/docs/0.10/security/#json-security
+    #                      http://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx
+    #
+    # JSON GET APIs are vulnerable to being embedded as JavaScript when the
+    # Array prototype has been patched to track data. Checks the referrer
+    # even on GET requests if the content type is JSON.
+    #
+    # If request includes Origin HTTP header, defers to HttpOrigin to determine
+    # if the request is safe. Please refer to the documentation for more info.
+    #
+    # The `:allow_if` option can be set to a proc to use custom allow/deny logic.
+    class JsonCsrf < Base
+      default_options :allow_if => nil
+    
+      describe '.random_token' do
+    it 'generates a base64 encoded 32 character string' do
+      expect(Base64.strict_decode64(token).length).to eq(32)
+    end
+  end
+end
+
+    
+          @right_diff_line_number = nil
+    
+          def title
+        '#{@page.title}'
       end
     
-    # Website =============================================================
-    
-          <div id='post'>
-        <h3 id='post-info'>POST</h3>
-        <% if req.POST and not req.POST.empty? %>
-          <table class='req'>
-            <tr>
-              <th>Variable</th>
-              <th>Value</th>
-            </tr>
-            <% req.POST.sort_by { |k, v| k.to_s }.each { |key, val| %>
-            <tr>
-              <td><%=h key %></td>
-              <td class='code'><div><%=h val.inspect %></div></td>
-            </tr>
-            <% } %>
-          </table>
-        <% else %>
-          <p class='no-data'>No POST data.</p>
-        <% end %>
-        <div class='clear'></div>
-      </div> <!-- /POST -->
-    <% end %>
-    
-          def set_token(session)
-        session[:csrf] ||= self.class.random_token
+          def next_link
+        label = 'Next &raquo;'
+        if @versions.size == Gollum::Page.per_page
+          link = '/history/#{@page.name}?page=#{@page_num+1}'
+          %(<a href='#{link}' hotkey='l'>#{label}</a>)
+        else
+          %(<span class='disabled'>#{label}</span>)
+        end
       end
+    end
+  end
+end
+
+    
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
+  end
+    
+    #############################################################################
+#
+# Helper functions
+#
+#############################################################################
+    
+        # Extract the path string that Gollum::Wiki expects
+    def extract_path(file_path)
+      return nil if file_path.nil?
+      last_slash = file_path.rindex('/')
+      if last_slash
+        file_path[0, last_slash]
+      end
+    end
+    
+      config.vm.define 'freebsd10' do |freebsd10|
+    freebsd10.vm.box = 'tjay/freebsd-10.1'
+  end
+    
+        script_errors = []
+    setscript = proc do |scriptname|
+      # 'self.send(scriptname) == self.before_install == --before-install
+      # Gets the path to the script
+      path = self.send(scriptname)
+      # Skip scripts not set
+      next if path.nil?
+    
+        # Find all leaf-like paths (files, symlink, empty directories, etc)
+    # Also trim the leading path such that '#{staging_path}/' is removed from
+    # the path before returning.
+    #
+    # Wrapping Find.find in an Enumerator is required for sane operation in ruby 1.8.7,
+    # but requires the 'backports' gem (which is used in other places in fpm)
+    return Enumerator.new { |y| Find.find(staging_path) { |path| y << path } } \
+      .select { |path| path != staging_path } \
+      .select { |path| is_leaf.call(path) } \
+      .collect { |path| path[staging_path.length + 1.. -1] }
+  end # def files
+    
+        # Query details about our now-installed package.
+    # We do this by using 'npm ls' with json + long enabled to query details
+    # about the installed package.
+    npm_ls_out = safesystemout(attributes[:npm_bin], 'ls', '--json', '--long', *npm_flags)
+    npm_ls = JSON.parse(npm_ls_out)
+    name, info = npm_ls['dependencies'].first
+    
+      option '--postinstall-action', 'POSTINSTALL_ACTION',
+    'Post-install action provided in package metadata. ' \
+    'Optionally one of '#{POSTINSTALL_ACTIONS.join('', '')}'.' do |value|
+    if !POSTINSTALL_ACTIONS.include?(value)
+      raise ArgumentError, 'osxpkg-postinstall-action value of '#{value}' is invalid. ' \
+        'Must be one of #{POSTINSTALL_ACTIONS.join(', ')}'
+    end
+    value
+  end
+    
+          # Allow user to review added dependencies
+      edit_file(build_path('#{name}.p5m.3')) if attributes[:edit?]
+    
+        # do channel-discover if required
+    if !attributes[:pear_channel].nil?
+      logger.info('Custom channel specified', :channel => attributes[:pear_channel])
+      channel_list = safesystemout('pear', '-c', config, 'list-channels')
+      if channel_list !~ /#{Regexp.quote(attributes[:pear_channel])}/
+        logger.info('Discovering new channel', :channel => attributes[:pear_channel])
+        safesystem('pear', '-c', config, 'channel-discover', attributes[:pear_channel])
+      end
+      input_package = '#{attributes[:pear_channel]}/#{input_package}'
+      logger.info('Prefixing package name with channel', :package => input_package)
+    end
+    
+        libs = [ 'install.sh', 'install-path.sh', 'generate-cleanup.sh' ]
+    libs.each do |file|
+      base = staging_path(File.join(attributes[:prefix]))
+      File.write(File.join(base, file), template(File.join('pleaserun', file)).result(binding))
+      File.chmod(0755, File.join(base, file))
+    end
+    
+        ::Dir.mkdir(File.join(builddir, 'manifests'))
+    manifests.each do |manifest|
+      dir = File.join(builddir, 'manifests', File.dirname(manifest))
+      logger.info('manifests targeting: #{dir}')
+      ::Dir.mkdir(dir) if !File.directory?(dir)
+    
+    # Support for self extracting sh files (.sh files)
+#
+# This class only supports output of packages.
+#
+# The sh package is a single sh file with a tar payload concatenated to the end.
+# The script can unpack the tarball to install it and call optional post install scripts.
+class FPM::Package::Sh < FPM::Package
