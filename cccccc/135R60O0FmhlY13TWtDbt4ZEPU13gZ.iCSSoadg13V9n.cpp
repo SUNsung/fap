d82fec392e42fa3ceac7ce4cd84c21b420599784
SUNsung <weatherius@gmail.com>
2019-02-18 03:22:09 +0200

    Rewrite [135R60O0FmhlY13TWtDbt4ZEPU13gZ.iCSSoadg13V9n.cpp] File in: 2019-02-18 03:22:08

--- cccccc/135R60O0FmhlY13TWtDbt4ZEPU13gZ.iCSSoadg13V9n.cpp
+++ cccccc/135R60O0FmhlY13TWtDbt4ZEPU13gZ.iCSSoadg13V9n.cpp
@@ -1,241 +1,77 @@
 
         
-        /** @class FadeOut
- * @brief Fades Out an object that implements the RGBAProtocol protocol. It modifies the opacity from 255 to 0.
- The 'reverse' of this action is FadeIn
-*/
-class CC_DLL FadeOut : public FadeTo
-{
-public:
-    /** 
-     * Creates the action.
-     * @param d Duration time, in seconds.
-     */
-    static FadeOut* create(float d);
+        #endif  // 0
+    
+    #if GTEST_HAS_STD_WSTRING
+template <>
+class UniversalTersePrinter<const wchar_t*> {
+ public:
+  static void Print(const wchar_t* str, ::std::ostream* os) {
+    if (str == NULL) {
+      *os << 'NULL';
+    } else {
+      UniversalPrint(::std::wstring(str), os);
     }
-    
-    void ActionManager::actionAllocWithHashElement(tHashElement *element)
-{
-    // 4 actions per Node by default
-    if (element->actions == nullptr)
-    {
-        element->actions = ccArrayNew(4);
-    }else 
-    if (element->actions->num == element->actions->max)
-    {
-        ccArrayDoubleCapacity(element->actions);
-    }
-    }
-    
-    /**
-@brief Progress to percentage.
-@details This action show the target node from current percentage to the specified percentage.
-        You should specify the destination percentage when creating the action.
-@since v0.99.1
-*/
-class CC_DLL ProgressTo : public ActionInterval
-{
-public:
-    /** 
-     * @brief Create and initializes with a duration and a destination percentage.
-     * @param duration Specify the duration of the ProgressTo action. It's a value in seconds.
-     * @param percent Specify the destination percentage.
-     * @return If the creation success, return a pointer of ProgressTo action; otherwise, return nil.
-     */
-    static ProgressTo* create(float duration, float percent);
-    }
-    
-    /**
-@brief The delegate class for ActionTween.
-@details If you want to use ActionTween on a node.
-        You should implement the node follow these steps:
-        1. The node should be inherit from ActionTweenDelegate.
-        2. Override the virtual method updateTweenAction in the node.
-    
-    void Animation::addSpriteFrame(SpriteFrame* spriteFrame)
-{
-    AnimationFrame *animFrame = AnimationFrame::create(spriteFrame, 1.0f, ValueMap());
-    _frames.pushBack(animFrame);
-    }
-    
-                if( ! spriteFrame ) {
-                CCLOG('cocos2d: AnimationCache: Animation '%s' refers to frame '%s' which is not currently in the SpriteFrameCache. This frame will not be added to the animation.', name.c_str(), spriteFrameName.c_str());
-    }
-    
-    
-    {// end of sprite_nodes group
-/// @}
-    
-    The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-    
-    private:
-    CC_DISALLOW_COPY_AND_ASSIGN(AtlasNode);
-    
-    unsigned int AutoPolygon::getSquareValue(unsigned int x, unsigned int y, const Rect& rect, float threshold)
-{
-    /*
-     checking the 2x2 pixel grid, assigning these values to each pixel, if not transparent
-     +---+---+
-     | 1 | 2 |
-     +---+---+
-     | 4 | 8 | <- current pixel (curx,cury)
-     +---+---+
-     */
-    unsigned int sv = 0;
-    //NOTE: due to the way we pick points from texture, rect needs to be smaller, otherwise it goes outside 1 pixel
-    auto fixedRect = Rect(rect.origin, rect.size-Size(2,2));
-    
-    Vec2 tl = Vec2(x-1, y-1);
-    sv += (fixedRect.containsPoint(tl) && getAlphaByPos(tl) > threshold)? 1 : 0;
-    Vec2 tr = Vec2(x, y-1);
-    sv += (fixedRect.containsPoint(tr) && getAlphaByPos(tr) > threshold)? 2 : 0;
-    Vec2 bl = Vec2(x-1, y);
-    sv += (fixedRect.containsPoint(bl) && getAlphaByPos(bl) > threshold)? 4 : 0;
-    Vec2 br = Vec2(x, y);
-    sv += (fixedRect.containsPoint(br) && getAlphaByPos(br) > threshold)? 8 : 0;
-    CCASSERT(sv != 0 && sv != 15, 'square value should not be 0, or 15');
-    return sv;
-}
-    
-    // That gcc wants both of these prototypes seems mysterious. VC, for
-// its part, can't decide which to use (another mystery). Matching of
-// template overloads: the final frontier.
-#ifndef COMPILER_MSVC
-template <typename T, size_t N>
-char (&ArraySizeHelper(const T (&array)[N]))[N];
+  }
+};
 #endif
     
-      CheckHandler& operator=(const CheckHandler&) = delete;
-  CheckHandler(const CheckHandler&) = delete;
-  CheckHandler() = delete;
+      // Gets the (mutable) vector of TestInfos in this TestCase.
+  std::vector<TestInfo*>& test_info_list() { return test_info_list_; }
+    
+      explicit tuple(GTEST_BY_REF_(T0) f0, GTEST_BY_REF_(T1) f1,
+      GTEST_BY_REF_(T2) f2, GTEST_BY_REF_(T3) f3, GTEST_BY_REF_(T4) f4,
+      GTEST_BY_REF_(T5) f5) : f0_(f0), f1_(f1), f2_(f2), f3_(f3), f4_(f4),
+      f5_(f5) {}
+    
+    // This provides interface PrimeTable that determines whether a number is a
+// prime and determines a next prime number. This interface is used
+// in Google Test samples demonstrating use of parameterized tests.
     
     
-    {}  // end namespace benchmark
-
-    
-    // NOTE: Wrappers for std::mutex and std::unique_lock are provided so that
-// we can annotate them with thread safety attributes and use the
-// -Wthread-safety warning with clang. The standard library types cannot be
-// used directly because they do not provided the required annotations.
-class CAPABILITY('mutex') Mutex {
- public:
-  Mutex() {}
-    }
-    
-    #endif  // BENCHMARK_RE_H_
-
-    
-    #include 'sleep.h'
-    
-      // init config and state
-  // After a CAN handle is created with canOpen() the CAN-ID filter is
-  // cleared
-  // (no CAN messages
-  // will pass the filter). To receive a CAN message with a certain CAN-ID
-  // or an
-  // NTCAN-Event with
-  // a certain Event-ID it is required to enable this ID in the handle
-  // filter as
-  // otherwise a
-  // received  message or event is discarded by the driver for this handle.
-  // 1. set receive message_id filter, ie white list
-  int32_t id_count = 0x800;
-  ret = canIdRegionAdd(dev_handler_, 0, &id_count);
-  if (ret != NTCAN_SUCCESS) {
-    AERROR << 'add receive msg id filter error code: ' << ret << ', '
-           << GetErrorString(ret);
-    return ErrorCode::CAN_CLIENT_ERROR_BASE;
-  }
-    
-    
-    {  if (static_cast<size_t>(*frame_num) != frames.size()) {
-    AERROR << 'frame num is incorrect.';
-    return ErrorCode::CAN_CLIENT_ERROR_FRAME_NUM;
-  }
-  for (size_t i = 0; i < frames.size(); ++i) {
-    ADEBUG << 'send frame i:' << i;
-    ADEBUG << frames[i].CanFrameString();
-    frame_info_ << frames[i].CanFrameString();
-  }
-  ++send_counter_;
-  return ErrorCode::OK;
+    {  // <TechnicalDetails>
+  //
+  // EXPECT_EQ(expected, actual) is the same as
+  //
+  //   EXPECT_TRUE((expected) == (actual))
+  //
+  // except that it will print both the expected value and the actual
+  // value when the assertion fails.  This is very helpful for
+  // debugging.  Therefore in this case EXPECT_EQ is preferred.
+  //
+  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
+  // and is thus more general.
+  //
+  // </TechnicalDetails>
 }
     
     
-    {
-    {
-    {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
+    {  return clone;
+}
     
-    TEST(HermesCanClient, receiver) {
-  CANCardParameter param;
-  param.set_brand(CANCardParameter::HERMES_CAN);
-  param.set_channel_id(CANCardParameter::CHANNEL_ID_ZERO);
-  HermesCanClient hermes_can;
-  EXPECT_TRUE(hermes_can.Init(param));
+    // Tests the Set method.
+TEST(MyString, Set) {
+  MyString s;
     }
     
-      /**
-   * @brief Send messages
-   * @param frames The messages to send.
-   * @param frame_num The amount of messages to send.
-   * @return The status of the sending action which is defined by
-   *         apollo::common::ErrorCode.
-   */
-  apollo::common::ErrorCode Send(const std::vector<CanFrame> &frames,
-                                 int32_t *const frame_num) override;
+      /// equals operator
+  bool operator==(const QueryLogItem& comp) const {
+    return (comp.results == results) && (comp.name == name);
+  }
     
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-#include 'modules/common/proto/error_code.pb.h'
-#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
-#include 'modules/drivers/canbus/can_comm/message_manager.h'
-    
-      ProtocolData<::apollo::canbus::ChassisDetail> mpd;
-  SenderMessage<::apollo::canbus::ChassisDetail> msg(1, &mpd);
-  EXPECT_FALSE(sender.NeedSend(msg, 1));
-  EXPECT_EQ(msg.message_id(), 1);
-  int32_t period = msg.curr_period();
-  msg.UpdateCurrPeriod(-50);
-  EXPECT_EQ(msg.curr_period(), period + 50);
-  EXPECT_EQ(msg.CanFrame().id, 1);
-    
-    const uint8_t BIT_MASK_1[] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
-const uint8_t BIT_MASK_0[] = {0xFE, 0xFD, 0xFB, 0xF7, 0xEF, 0xDF, 0xBF, 0x7F};
-const uint8_t RANG_MASK_1_L[] = {0x01, 0x03, 0x07, 0x0F,
-                                 0x1F, 0x3F, 0x7F, 0xFF};
-const uint8_t RANG_MASK_0_L[] = {0xFE, 0XFC, 0xF8, 0xF0,
-                                 0xE0, 0xC0, 0x80, 0x00};
-    
-      /**
-   * @brief Constructor which takes a reference to a one-byte unsigned integer.
-   * @param value The reference to a one-byte unsigned integer for construction.
-   */
-  Byte(const Byte &value);
-    
-    // System gflags
-DEFINE_string(sensor_node_name, '', 'Sensor node name.');
+    #endif
 
     
-    #ifndef GUETZLI_FAST_LOG_H_
-#define GUETZLI_FAST_LOG_H_
+      JSON doc = JSON::newArray();
+  auto arr1 = doc.getArray();
+  doc.copyFrom(r.first.doc(), arr1);
+  doc.push(arr1);
     
-      int verbose = 0;
-  int quality = kDefaultJPEGQuality;
-  int memlimit_mb = kDefaultMemlimitMB;
+    const std::string kTestQuery{'SELECT * FROM test_table'};
     
-    
-    {}  // namespace guetzli
-    
-    namespace guetzli {
-    }
-    
-    // Creates a JPEG from the rgb pixel data. Returns true on success. The given
-// quantization table must have 3 * kDCTBlockSize values.
-bool EncodeRGBToJpeg(const std::vector<uint8_t>& rgb, int w, int h,
-                     const int* quant, JPEGData* jpg);
-    
-    #endif  // GUETZLI_JPEG_DATA_READER_H_
+      // Get address of ProcessParameters struct
+  unsigned long long procParamsAddr = 0;
+  if (data->ReadPointersVirtual(
+          1, pebAddr + procParamsOffset, &procParamsAddr) != S_OK) {
+    return Status(1);
+  }
