10ae832d14b6c967a48cc62cd0392e6ac0f87b6c
SUNsung <weatherius@gmail.com>
2019-02-18 23:25:13 +0200

    Rewrite [55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb] File in: 2019-02-18 23:25:13

--- ruby/55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb
+++ ruby/55pAvbSC41m2551UGjz3tKqHk55cqpTvl6SZgQ55ahc.rb
@@ -1,73 +1,48 @@
 
         
-                  content_is_options = content_or_options.is_a?(Hash)
-          if content_is_options
-            options.merge! content_or_options
-            @content = nil
-          else
-            @content = content_or_options
-          end
+            puts('Generated at #{target_file}')
+  end
     
-            private
-    
-        # Render but returns a valid Rack body. If fibers are defined, we return
-    # a streaming body that renders the template piece by piece.
-    #
-    # Note that partials are not supported to be rendered with streaming,
-    # so in such cases, we just wrap them in an array.
-    def render_body(context, options)
-      if options.key?(:partial)
-        [render_partial(context, options)]
-      else
-        StreamingTemplateRenderer.new(@lookup_context).render(context, options)
+        class << self
+      def elastic_pack_base_uri
+        env_url = ENV['LOGSTASH_PACK_URL']
+        (env_url.nil? || env_url.empty?) ? DEFAULT_PACK_URL : env_url
       end
+    
+          PluginManager.ui.info('Install successful')
+    rescue ::Bundler::BundlerError => e
+      raise PluginManager::InstallError.new(e), 'An error occurred went installing plugins'
+    ensure
+      FileUtils.rm_rf(uncompressed_path) if uncompressed_path && Dir.exist?(uncompressed_path)
     end
     
-        # The content passed to this environment. This is naturally only set
-    # for mixin body environments with content passed in.
-    #
-    # @return {[Array<Sass::Tree::Node>, Environment]?} The content nodes and
-    #   the lexical environment of the content block.
-    def content
-      @content || (@parent && @parent.content)
-    end
+        FileUtils.rm_rf(LogStash::Environment::CACHE_PATH)
+    validate_cache_location
+    archive_manager.extract(package_file, LogStash::Environment::CACHE_PATH)
+    puts('Unpacked at #{LogStash::Environment::CACHE_PATH}')
+    puts('The unpacked plugins can now be installed in local-only mode using bin/logstash-plugin install --local [plugin name]')
+  end
     
-      s.author    = 'You'
-  s.email     = 'you@example.com'
-  s.homepage  = 'https://github.com/your-github-handle/<%= file_name %>'
-  s.license = 'BSD-3-Clause'
+        # force a disk sync before running bundler
+    gemfile.save
     
-          it 'returns proper links data' do
-        expect(json_response['links']['self']).to include('/api/v2/storefront/account/orders')
-        expect(json_response['links']['next']).to include('/api/v2/storefront/account/orders?page=1')
-        expect(json_response['links']['prev']).to include('/api/v2/storefront/account/orders?page=1')
+    module RuboCop
+  module AST
+    # Common functionality for nodes that are parameterized:
+    # `send`, `super`, `zsuper`, `def`, `defs`
+    module ParameterizedNode
+      # Checks whether this node's arguments are wrapped in parentheses.
+      #
+      # @return [Boolean] whether this node's arguments are
+      #                   wrapped in parentheses
+      def parenthesized?
+        loc.end && loc.end.is?(')')
       end
-    end
     
-        it 'return JSON API payload of User and associations (default billing and shipping address)' do
-      expect(json_response['data']).to have_id(user.id.to_s)
-      expect(json_response['data']).to have_type('user')
-      expect(json_response['data']).to have_relationships(:default_shipping_address, :default_billing_address)
-    
-        shared_examples 'apply coupon code' do
-      before { execute }
-    
-        context 'as a guest user with token' do
-      before { get '/api/v2/storefront/order_status/#{order.number}', headers: headers_order_token }
-    
-              def serialize_resource(resource)
-            resource_serializer.new(
-              resource,
-              include: resource_includes,
-              sparse_fields: sparse_fields
-            ).serializable_hash
-          end
-    
-              def serialize_order(order)
-            resource_serializer.new(order.reload, include: resource_includes, fields: sparse_fields).serializable_hash
-          end
-    
-            include Spree::Core::ControllerHelpers::Auth
-        include Spree::Core::ControllerHelpers::Order
-        # This before_action comes from Spree::Core::ControllerHelpers::Order
-        skip_before_action :set_current_order
+          # Calls the given block for each condition node in the `when` branch.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_condition
+        return conditions.to_enum(__method__) unless block_given?
