32dbf5cd1eb4f7d8209a32d04279d4632ac17b13
SUNsung <weatherius@gmail.com>
2019-02-19 09:01:48 +0200

    Rewrite [31H2QsEQTnDCk31EwrfqMUCZNE31Fw6nLvTOXew31wdg.py] File in: 2019-02-19 09:01:48

--- pyton/31H2QsEQTnDCk31EwrfqMUCZNE31Fw6nLvTOXew31wdg.py
+++ pyton/31H2QsEQTnDCk31EwrfqMUCZNE31Fw6nLvTOXew31wdg.py
@@ -1,103 +1,127 @@
 
         
-        
-def test_login(client, auth):
-    # test that viewing the page renders without template errors
-    assert client.get('/auth/login').status_code == 200
+            if self.hparams.hidden_layers == 1:
+      self.output = tf.matmul(tf.nn.tanh(h), self.weights2)
     
+        self.keep_prob = keep_prob = tf.placeholder(tf.float32, [], 'keep_prob')
+    self.batch_size = batch_size = int(hps.batch_size)
+    self.learning_rate = tf.Variable(float(hps.learning_rate_init),
+                                     trainable=False, name='learning_rate')
+    self.learning_rate_decay_op = self.learning_rate.assign(
+        self.learning_rate * hps.learning_rate_decay_factor)
     
-def test_init_db_command(runner, monkeypatch):
-    class Recorder(object):
-        called = False
+      model.train_model(datasets)
     
-    
-class FormDataRoutingRedirect(AssertionError):
-    '''This exception is raised by Flask in debug mode if it detects a
-    redirect caused by the routing system when the request method is not
-    GET, HEAD or OPTIONS.  Reasoning: form data will be dropped.
+      Args:
+    data_e: nexamples length list of NxT trials
+    dt: how often the data are sampled
+    max_firing_rate: the firing rate that is associated with a value of 1.0
+  Returns:
+    gauss_e: a list of length b of the data with noise.
     '''
     
-            Uses ``SESSION_COOKIE_DOMAIN`` if it is configured, otherwise
-        falls back to detecting the domain based on ``SERVER_NAME``.
+      return question_ids, sentences, labels
     
-            #out = subprocess.check_output(cmd, startupinfo=startupinfo)
-        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, startupinfo=startupinfo)
-        out, unused_err = process.communicate()
-        retcode = process.poll()
-        if retcode:
-            return out + '\n retcode:%s\n unused_err:%s\n' % (retcode, unused_err)
-    except Exception as e:
-        out = 'Exception:%r' % e
-    
-    ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
+          x[i] = final_x
+      y[i] = final_y
     
     
-    def __str__(self):
-        return 'NoViableAltException(%r!=[%r])' % (
-            self.unexpectedType, self.grammarDecisionDescription
-            )
-    __repr__ = __str__
+def convert_to_human_readable(id_to_word, arr, max_num_to_print):
+  '''Convert a np.array of indices into words using id_to_word dictionary.
+  Return max_num_to_print results.
+  '''
+  assert arr.ndim == 2
     
     
-        '''
-    # Strip off /files
-    file_path = vhost_path[6:]
-    internal_path = []
+def create_gen_train_op(hparams, learning_rate, gen_loss, global_step, mode):
+  '''Create Generator train op.'''
+  del hparams
+  with tf.name_scope('train_generator'):
+    if FLAGS.generator_optimizer == 'sgd':
+      gen_optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    elif FLAGS.generator_optimizer == 'adam':
+      gen_optimizer = tf.train.AdamOptimizer(learning_rate)
+    else:
+      raise NotImplementedError
+    gen_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
+    ]
+    print('Optimizing Generator vars.')
+    for v in gen_vars:
+      print(v)
+    if mode == 'MINIMIZE':
+      gen_grads = tf.gradients(gen_loss, gen_vars)
+    elif mode == 'MAXIMIZE':
+      gen_grads = tf.gradients(-gen_loss, gen_vars)
+    else:
+      raise ValueError('Must be one of 'MINIMIZE' or 'MAXIMIZE'')
+    gen_grads_clipped, _ = tf.clip_by_global_norm(gen_grads,
+                                                  FLAGS.grad_clipping)
+    gen_train_op = gen_optimizer.apply_gradients(
+        zip(gen_grads_clipped, gen_vars), global_step=global_step)
+    return gen_train_op, gen_grads_clipped, gen_vars
     
-        def setup_variables(self):
-        '''Set up variables for parser.'''
-        self.parser.variables.update(
-            {
-                'COMPLEX': '',
-                'tls_port': '1234',
-                'fnmatch_filename': 'test_fnmatch.conf',
-                'tls_port_str': '1234'
-            }
-        )
+        elif FLAGS.discriminator_model == 'cnn':
+      dis_variable_maps = variable_mapping.cnn()
+      dis_init_saver = tf.train.Saver(var_list=dis_variable_maps)
+      init_savers['dis_init_saver'] = dis_init_saver
     
-        def setUp(self):
-        self.base_dir = '/example_path'
-        self.vhosts = util.get_vh_truth(
-            self.base_dir, 'debian_apache_2_4/multiple_vhosts')
+      for t in ngrams_list:
+    key = hash_function(t)
+    if key in counts:
+      counts[key] += 1
+    else:
+      counts[key] = 1
+  return counts
+    
+      if FLAGS.dis_share_embedding:
+    assert hparams.dis_rnn_size == hparams.gen_rnn_size, (
+        'If you wish to share Discriminator/Generator embeddings, they must be'
+        ' same dimension.')
+    with tf.variable_scope('gen/rnn', reuse=True):
+      embedding = tf.get_variable('embedding',
+                                  [FLAGS.vocab_size, hparams.gen_rnn_size])
+    
+                _contents = _contents.replace(match.group(3), match.group(4))
+    else:
+        # We don't understand what the current line means!
+        print('Not Understood: ' + line)
+
+    
+    # Check minimum required Python version
+import sys
+if sys.version_info < (2, 7):
+    print('Scrapy %s requires Python 2.7' % __version__)
+    sys.exit(1)
+    
+        def long_desc(self):
+        return ('Edit a spider using the editor defined in the EDITOR environment'
+                ' variable or else the EDITOR setting')
     
     
-class TlsSniPerformTest(util.ApacheTest):
-    '''Test the ApacheTlsSni01 challenge.'''
+class Command(ScrapyCommand):
     
-    # The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-version = '0'
-# The full version, including alpha/beta/rc tags.
-release = '0'
+        def post_process(self, output):
+        occurrences = 0
+        for x in output:
+            if isinstance(x, self.obj_type):
+                occurrences += 1
     
     
-# Is a path a directory?
-# This follows symbolic links, so both islink() and isdir()
-# can be true for the same path on systems that support symlinks
-def isdir(s):
-    '''Return true if the pathname refers to an existing directory.'''
-    try:
-        st = os.stat(s)
-    except (OSError, ValueError):
-        return False
-    return stat.S_ISDIR(st.st_mode)
+class ScrapyHTTPClientFactory(HTTPClientFactory):
+    '''Scrapy implementation of the HTTPClientFactory overwriting the
+    serUrl method to make use of our Url object that cache the parse
+    result.
+    '''
     
-    # Function to return the operator module
-def get_operator_module():
-    return operator
     
-    while True:
-    line = input()
-    if line == '':
-        break
-    buffer += line
-    if sqlite3.complete_statement(buffer):
-        try:
-            buffer = buffer.strip()
-            cur.execute(buffer)
+class ShutdownRequest( BaseRequest ):
+  def __init__( self ):
+    super( ShutdownRequest, self ).__init__()
+    
+    import mock
+import requests
+    
+    def load_url(url, timeout):
+    kwargs = {'timeout': timeout} if sys.version_info >= (2, 6) else {}
+    return urlopen(url, **kwargs).read()
