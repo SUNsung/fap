653aa3a74f2026ef620c8c161e1c0f949eab5f4b
SUNsung <weatherius@gmail.com>
2019-02-19 04:22:03 +0200

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2019-02-19 04:22:02

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,30 +1,68 @@
 
         
-            def start_requests(self):
-        qargs = {'total': self.total, 'show': self.show}
-        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
-        return [scrapy.Request(url, dont_filter=True)]
+            releaser = GitHubReleaser()
     
-            spider_loader = self.crawler_process.spider_loader
+        with open('supportedsites.html', 'w', encoding='utf-8') as sitesf:
+        sitesf.write(template)
     
-        def run(self, args, opts):
-        if len(args) < 1:
-            raise UsageError()
-        elif len(args) > 1:
-            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
-        spname = args[0]
+    lazy_extractors_filename = sys.argv[1]
+if os.path.exists(lazy_extractors_filename):
+    os.remove(lazy_extractors_filename)
     
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-l', '--list', dest='list', action='store_true',
-            help='List available templates')
-        parser.add_option('-e', '--edit', dest='edit', action='store_true',
-            help='Edit spider after creating it')
-        parser.add_option('-d', '--dump', dest='dump', metavar='TEMPLATE',
-            help='Dump template to standard output')
-        parser.add_option('-t', '--template', dest='template', default='basic',
-            help='Uses a custom template.')
-        parser.add_option('--force', dest='force', action='store_true',
-            help='If the spider already exists, overwrite it with the template')
     
-    class Command(ScrapyCommand):
+total_bytes = 0
+    
+    # Allow direct execution
+import os
+import sys
+import unittest
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    
+            if 'skip' in test_case:
+            print_skipping(test_case['skip'])
+            return
+        for other_ie in other_ies:
+            if not other_ie.working():
+                print_skipping('test depends on %sIE, marked as not WORKING' % other_ie.ie_key())
+                return
+    
+        def setUp(self):
+        from acme.errors import MissingNonce
+        self.response = mock.MagicMock(headers={})
+        self.response.request.method = 'FOO'
+        self.error = MissingNonce(self.response)
+    
+        def test_encode(self):
+        self.assertEqual('x', self.field.encode('x'))
+    
+    # If false, no index is generated.
+#html_use_index = True
+    
+        '''
+    return_vars = {}
+    # Get list of words in the variable
+    a_opts = util.get_var_from_file(varname, filepath).split()
+    for i, v in enumerate(a_opts):
+        # Handle Define statements and make sure it has an argument
+        if v == '-D' and len(a_opts) >= i+2:
+            var_parts = a_opts[i+1].partition('=')
+            return_vars[var_parts[0]] = var_parts[2]
+        elif len(v) > 2 and v.startswith('-D'):
+            # Found var with no whitespace separator
+            var_parts = v[2:].partition('=')
+            return_vars[var_parts[0]] = var_parts[2]
+    return return_vars
+    
+            for addr in vhost.addrs:
+            for local_addr in self.addrs:
+                if (local_addr.get_addr() == addr.get_addr() and
+                        local_addr != addr and
+                        local_addr.get_addr() not in already_found):
+    
+            complex_vh = VirtualHost(
+            'fp', 'vhp',
+            set([Addr.fromstring('*:443'), Addr.fromstring('1.2.3.4:443')]),
+            False, False)
+        self.assertTrue(complex_vh.conflicts([self.addr1]))
+        self.assertTrue(complex_vh.conflicts([self.addr2]))
+        self.assertFalse(complex_vh.conflicts([self.addr_default]))
