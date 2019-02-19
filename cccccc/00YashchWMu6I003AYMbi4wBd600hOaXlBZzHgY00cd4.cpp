20269d672e2f05ab209baa6a9a80993bb4ce64fc
SUNsung <weatherius@gmail.com>
2019-02-19 09:31:37 +0200

    Rewrite [00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp] File in: 2019-02-19 09:31:37

--- cccccc/00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp
+++ cccccc/00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp
@@ -1,224 +1,165 @@
 
         
-        // We define the PY_ARRAY_UNIQUE_SYMBOL in this .cc file and provide an
-// ImportNumpy function to populate it.
-#define TF_IMPORT_NUMPY
+        namespace {
+    }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      // This is for complex to real or real to complex FFT, when the direction
-  // is implied.
-  template <typename FuncT, typename InputT, typename OutputT>
-  bool DoFftInternal(Stream *stream, fft::Plan *plan, FuncT cufft_exec,
-                     const DeviceMemory<InputT> &input,
-                     DeviceMemory<OutputT> *output);
-    
-    #include 'atom/app/uv_task_runner.h'
-    
-    // static
-void Net::BuildPrototype(v8::Isolate* isolate,
-                         v8::Local<v8::FunctionTemplate> prototype) {
-  prototype->SetClassName(mate::StringToV8(isolate, 'Net'));
-  mate::ObjectTemplateBuilder(isolate, prototype->PrototypeTemplate())
-      .SetProperty('URLRequest', &Net::URLRequest);
+    template <>
+template <typename T>
+bool EnforceFiniteOp<CPUContext>::DoRunWithType() {
+  EnforceOnCPU<T>(Input(0));
+  return true;
 }
     
-     protected:
-  Screen(v8::Isolate* isolate, display::Screen* screen);
-  ~Screen() override;
-    
-    #include 'atom/browser/api/save_page_handler.h'
-    
-    #ifndef ATOM_BROWSER_ATOM_JAVASCRIPT_DIALOG_MANAGER_H_
-#define ATOM_BROWSER_ATOM_JAVASCRIPT_DIALOG_MANAGER_H_
-    
-    #include 'base/macros.h'
-    
-    void FinishTransactionByDate(const std::string& date);
-    
-      // For extensions to chain together, the Root gets created even if there
-  // are no extensions.
-  printer->Print(
-      '#pragma mark - $root_class_name$\n'
-      '\n'
-      '/**\n'
-      ' * Exposes the extension registry for this file.\n'
-      ' *\n'
-      ' * The base class provides:\n'
-      ' * @code\n'
-      ' *   + (GPBExtensionRegistry *)extensionRegistry;\n'
-      ' * @endcode\n'
-      ' * which is a @c GPBExtensionRegistry that includes all the extensions defined by\n'
-      ' * this file and all files that it depends on.\n'
-      ' **/\n'
-      '@interface $root_class_name$ : GPBRootObject\n'
-      '@end\n'
-      '\n',
-      'root_class_name', root_class_name_);
-    
-    
-    {
-    {    // We expect escape sequences to have been validated separately.
-    default:   return '?';
+    template <>
+void GluOp<float, CPUContext>::ComputeGlu(
+    const int M,
+    const int split_dim,
+    const int N,
+    const float* Xdata,
+    float* Ydata) {
+  const int xStride = 2 * split_dim * N;
+  const int yStride = split_dim * N;
+  for (int i = 0; i < M; ++i) {
+    const int idx = i * xStride;
+    const int idy = i * yStride;
+    for (int j = 0; j < split_dim; ++j) {
+      const int jN = j * N;
+      const int jdx1 = idx + jN;
+      const int jdx2 = idx + (j + split_dim) * N;
+      const int jdy = idy + jN;
+      for (int k = 0; k < N; ++k) {
+        const float x1 = Xdata[jdx1 + k];
+        const float x2 = Xdata[jdx2 + k];
+        Ydata[jdy + k] = x1 * sigmoid(x2);
+      }
+    }
   }
 }
     
-    TEST_2D(TokenizerTest, DocComments, kDocCommentCases, kBlockSizes) {
-  // Set up the tokenizer.
-  TestInputStream input(kDocCommentCases_case.input.data(),
-                        kDocCommentCases_case.input.size(),
-                        kBlockSizes_case);
-  TestErrorCollector error_collector;
-  Tokenizer tokenizer(&input, &error_collector);
-    }
-    
-      // Reset the resolution of the processing image. TEST only function.
-  void SetResolution(const int resolution);
-    
-    namespace tesseract {
-    }
-    
-    // A rather hackish helper structure which can take any kind of parameter input
-// (defined by ParamType) and do a couple of common operations on them, like
-// comparisond or getting its value. It is used in the context of the
-// ParamsEditor as a bridge from the internal tesseract parameters to the
-// ones displayed by the ScrollView server.
-class ParamContent : public ELIST_LINK {
- public:
-  // Compare two VC objects by their name.
-  static int Compare(const void* v1, const void* v2);
-    }
-    
-      /// Get enough parameters to be able to rebuild bounding boxes in the
-  /// original image (not just within the rectangle).
-  /// Left and top are enough with top-down coordinates, but
-  /// the height of the rectangle and the image are needed for bottom-up.
-  virtual void GetImageSizes(int* left, int* top, int* width, int* height,
-                             int* imagewidth, int* imageheight);
-    
-    // Reads all boxes from the given filename.
-// Reads a specific target_page number if >= 0, or all pages otherwise.
-// Skips blanks if skip_blanks is true.
-// The UTF-8 label of the box is put in texts, and the full box definition as
-// a string is put in box_texts, with the corresponding page number in pages.
-// Each of the output vectors is optional (may be nullptr).
-// Returns false if no boxes are found.
-bool ReadAllBoxes(int target_page, bool skip_blanks, const STRING& filename,
-                  GenericVector<TBOX>* boxes,
-                  GenericVector<STRING>* texts,
-                  GenericVector<STRING>* box_texts,
-                  GenericVector<int>* pages);
-    
-    namespace tesseract {
-    }
-    
-    class TFile;
-    
-    ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcServerServerLatency();
-::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
-    
-    
-    { private:
-  CensusContext context_;
-  // server method
-  absl::string_view method_;
-  std::string qualified_method_;
-  grpc_slice path_;
-  // Pointer to the grpc_call element
-  grpc_call* gc_;
-  // Authorization context for the call.
-  grpc_auth_context* auth_context_;
-  // Metadata element for census stats.
-  grpc_linked_mdelem census_bin_;
-  // recv callback
-  grpc_metadata_batch* recv_initial_metadata_;
-  grpc_closure* initial_on_done_recv_initial_metadata_;
-  grpc_closure on_done_recv_initial_metadata_;
-  // recv message
-  grpc_closure* initial_on_done_recv_message_;
-  grpc_closure on_done_recv_message_;
-  absl::Time start_time_;
-  absl::Duration elapsed_time_;
-  grpc_core::OrphanablePtr<grpc_core::ByteStream>* recv_message_;
-  uint64_t recv_message_count_;
-  uint64_t sent_message_count_;
-  // Buffer needed for grpc_slice to reference it when adding metatdata to
-  // response.
-  char stats_buf_[kMaxServerStatsLen];
+    class GetFloatToHalfGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'HalfToFloat', '', vector<string>{GO(0)}, vector<string>{GI(0)});
+  }
 };
+REGISTER_GRADIENT(FloatToHalf, GetFloatToHalfGradient);
     
-    const ViewDescriptor& ClientReceivedMessagesPerRpcHour() {
+              const TensorShape& X = in[0];
+          int N = 0, C = 0, H = 0, W = 0;
+          switch (order) {
+            case StorageOrder::NCHW:
+              N = X.dims(0);
+              C = X.dims(1);
+              H = X.dims(2);
+              W = X.dims(3);
+              break;
+            case StorageOrder::NHWC:
+              N = X.dims(0);
+              H = X.dims(1);
+              W = X.dims(2);
+              C = X.dims(3);
+              break;
+            default:
+              CAFFE_THROW('Unknown storage order: ', order);
+          }
+    
+    CallCredentials::~CallCredentials() {}
+    
+    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
+  for (unsigned int i = 0; i < args_.size(); i++) {
+    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
+      return args_[i].value.string;
+    }
+  }
+  return '';
+}
+    
+    // TraceContextEncoding encapsulates the logic for encoding and decoding of
+// trace contexts.
+class TraceContextEncoding {
+ public:
+  // Size of encoded GrpcTraceContext. (16 + 8 + 1 + 4)
+  static constexpr size_t kGrpcTraceContextSize = 29;
+  // Error value.
+  static constexpr size_t kEncodeDecodeFailure = 0;
+    }
+    
+    const ViewDescriptor& ClientCompletedRpcsMinute() {
   const static ViewDescriptor descriptor =
-      HourDescriptor()
-          .set_name('grpc.io/client/received_messages_per_rpc/hour')
-          .set_measure(kRpcClientReceivedMessagesPerRpcMeasureName)
-          .set_aggregation(CountDistributionAggregation())
-          .add_column(ClientMethodTagKey());
+      MinuteDescriptor()
+          .set_name('grpc.io/client/completed_rpcs/minute')
+          .set_measure(kRpcClientRoundtripLatencyMeasureName)
+          .set_aggregation(Aggregation::Count())
+          .add_column(ClientMethodTagKey())
+          .add_column(ClientStatusTagKey());
   return descriptor;
 }
     
-    
-    {    if (!status.ok()) {
-      FillErrorResponse(status, response.mutable_error_response());
-    }
-    response.set_valid_host(request.host());
-    response.set_allocated_original_request(
-        new ServerReflectionRequest(request));
-    stream->Write(response);
+    // Force InitProtoReflectionServerBuilderPlugin() to be called at static
+// initialization time.
+struct StaticProtoReflectionPluginInitializer {
+  StaticProtoReflectionPluginInitializer() {
+    InitProtoReflectionServerBuilderPlugin();
   }
+} static_proto_reflection_plugin_initializer;
     
-          // Reset parameters that may be overridden below
-      num_ = FLAGS_num;
-      reads_ = (FLAGS_reads < 0 ? FLAGS_num : FLAGS_reads);
-      value_size_ = FLAGS_value_size;
-      entries_per_batch_ = 1;
-      write_options_ = WriteOptions();
-    
-    
-    {  FileLock* lock;
-  const std::string lockname = LockFileName(dbname);
-  result = env->LockFile(lockname, &lock);
-  if (result.ok()) {
-    uint64_t number;
-    FileType type;
-    for (size_t i = 0; i < filenames.size(); i++) {
-      if (ParseFileName(filenames[i], &number, &type) &&
-          type != kDBLockFile) {  // Lock file will be deleted at end
-        Status del = env->DeleteFile(dbname + '/' + filenames[i]);
-        if (result.ok() && !del.ok()) {
-          result = del;
-        }
-      }
+        virtual void UpdateArguments(ChannelArguments* args) override {
+      args->SetInt(name_, value_);
     }
-    env->UnlockFile(lock);  // Ignore error since state is already gone
-    env->DeleteFile(lockname);
-    env->DeleteDir(dbname);  // Ignore error in case dir contains other files
-  }
-  return result;
-}
+    virtual void UpdatePlugins(
+        std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {}
     
-      // Increase reference count.
-  void Ref() { ++refs_; }
+    CreateThreadPoolFunc g_ctp_impl = CreateDefaultThreadPoolImpl;
     
-    class SnapshotList {
+    #include 'src/cpp/server/load_reporter/get_cpu_stats.h'
+    
+      ////////////////////////////////////////////////////////
+  //
+  // 'Repeatable Read' (Snapshot Isolation) Example
+  //   -- Using a single Snapshot
+  //
+  ////////////////////////////////////////////////////////
+    
+      // Load the options file.
+  DBOptions loaded_db_opt;
+  std::vector<ColumnFamilyDescriptor> loaded_cf_descs;
+  s = LoadLatestOptions(kDBPath, Env::Default(), &loaded_db_opt,
+                        &loaded_cf_descs);
+  assert(s.ok());
+  assert(loaded_db_opt.create_if_missing == db_opt.create_if_missing);
+    
+      // Decide we want to revert the last write from this transaction.
+  txn->RollbackToSavePoint();
+    
+    #include <string>
+#include 'rocksdb/table.h'
+    
+    // Abstract handle to particular state of a DB.
+// A Snapshot is an immutable object and can therefore be safely
+// accessed from multiple threads without any external synchronization.
+//
+// To Create a Snapshot, call DB::GetSnapshot().
+// To Destroy a Snapshot, call DB::ReleaseSnapshot(snapshot).
+class Snapshot {
  public:
-  SnapshotList() : head_(0) {
-    head_.prev_ = &head_;
-    head_.next_ = &head_;
-  }
+  // returns Snapshot's sequence number
+  virtual SequenceNumber GetSequenceNumber() const = 0;
     }
     
-    using json = nlohmann::json;
+    /*
+ * ThreadPool is a component that will spawn N background threads that will
+ * be used to execute scheduled work, The number of background threads could
+ * be modified by calling SetBackgroundThreads().
+ * */
+class ThreadPool {
+ public:
+  virtual ~ThreadPool() {}
+    }
     
-        // print values
-    std::cout << object << '\n';
-    std::cout << null << '\n';
+    #include <string>
+#include 'rocksdb/status.h'
+    
+    
+    {}  // namespace rocksdb
+#endif  // ROCKSDB_LITE
