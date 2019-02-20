bac4c797d27cd2d2a7eb6dc394c543454cebfb2c
SUNsung <weatherius@gmail.com>
2019-02-20 21:57:06 +0200

    Rewrite [08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp] File in: 2019-02-20 21:57:06

--- cccccc/08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp
+++ cccccc/08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp
@@ -1,39 +1,256 @@
 
         
-        #include <grpc/support/port_platform.h>
+          // TransactionObserver:
+  void OnTransactionsUpdated(
+      const std::vector<in_app_purchase::Transaction>& transactions) override;
     
-    void FilterTrailingMetadata(grpc_metadata_batch* b, uint64_t* elapsed_time) {
-  if (b->idx.named.grpc_server_stats_bin != nullptr) {
-    ServerStatsDeserialize(
-        reinterpret_cast<const char*>(GRPC_SLICE_START_PTR(
-            GRPC_MDVALUE(b->idx.named.grpc_server_stats_bin->md))),
-        GRPC_SLICE_LENGTH(GRPC_MDVALUE(b->idx.named.grpc_server_stats_bin->md)),
-        elapsed_time);
-    grpc_metadata_batch_remove(b, b->idx.named.grpc_server_stats_bin);
-  }
-}
+    #ifndef ATOM_BROWSER_API_ATOM_API_SCREEN_H_
+#define ATOM_BROWSER_API_ATOM_API_SCREEN_H_
     
-    // These helper functions return the SpanContext and Span, respectively
-// associated with the census_context* stored by grpc. The user will need to
-// call this for manual propagation of tracing data.
-::opencensus::trace::SpanContext SpanContextFromCensusContext(
-    const census_context* ctxt);
-::opencensus::trace::Span SpanFromCensusContext(const census_context* ctxt);
+    class TrayIcon;
     
-    #include <grpc/impl/codegen/port_platform.h>
+    #ifndef ATOM_BROWSER_API_ATOM_API_WEB_REQUEST_H_
+#define ATOM_BROWSER_API_ATOM_API_WEB_REQUEST_H_
     
-    		/*
-		Convert legacy user-profile.cmd to new name user_profile.cmd
-		*/
-		PathCombine(legacyUserProfilePath, userConfigDirPath, L'user-profile.cmd');
-		if (PathFileExists(legacyUserProfilePath))
-		{
-			PathCombine(userProfilePath, userConfigDirPath, L'user_profile.cmd');
+    #ifndef ATOM_BROWSER_AUTO_UPDATER_H_
+#define ATOM_BROWSER_AUTO_UPDATER_H_
+    
+    
+    {}  // namespace in_app_purchase
+    
+    class AsarProtocolHandler : public net::URLRequestJobFactory::ProtocolHandler {
+ public:
+  explicit AsarProtocolHandler(
+      const scoped_refptr<base::TaskRunner>& file_task_runner);
+  ~AsarProtocolHandler() override;
     }
     
-        // read-only access
+      if (!bitmap_.get())
+    return;
+    
+    #ifndef BITCOIN_QT_NETWORKSTYLE_H
+#define BITCOIN_QT_NETWORKSTYLE_H
+    
+    private Q_SLOTS:
+    void on_selectFileButton_clicked();
     
     
-    {    std::cout << null << '\n';
-    std::cout << *res2.first << ' ' << std::boolalpha << res2.second << '\n';
+    {    QString name;
+    bool imagesOnButtons;
+    bool colorizeIcons;
+    bool useExtraSpacing;
+    QColor singleColor;
+    QColor textColor;
+    /* ... more to come later */
+};
+    
+    static int secp256k1_gej_eq_x_var(const secp256k1_fe *x, const secp256k1_gej *a) {
+    secp256k1_fe r, r2;
+    VERIFY_CHECK(!a->infinity);
+    secp256k1_fe_sqr(&r, &a->z); secp256k1_fe_mul(&r, &r, x);
+    r2 = a->x; secp256k1_fe_normalize_weak(&r2);
+    return secp256k1_fe_equal_var(&r, &r2);
 }
+    
+        secp256k1_context_set_error_callback(tctx, counting_illegal_callback_fn, &ecount);
+    secp256k1_context_set_illegal_callback(tctx, counting_illegal_callback_fn, &ecount);
+    CHECK(secp256k1_ec_pubkey_create(tctx, &point, s_one) == 1);
+    
+    const std::vector<UniValue>& UniValue::getValues() const
+{
+    if (typ != VOBJ && typ != VARR)
+        throw std::runtime_error('JSON value is not an object or array as expected');
+    return values;
+}
+    
+    /** Encode a Bech32 string. Returns the empty string in case of failure. */
+std::string Encode(const std::string& hrp, const std::vector<uint8_t>& values);
+    
+    done_hash:
+%ifndef LINUX
+    movdqa	xmm6,[rsp + _XMM_SAVE + 0*16]
+    movdqa	xmm7,[rsp + _XMM_SAVE + 1*16]
+    movdqa	xmm8,[rsp + _XMM_SAVE + 2*16]
+    movdqa	xmm9,[rsp + _XMM_SAVE + 3*16]
+    movdqa	xmm10,[rsp + _XMM_SAVE + 4*16]
+    movdqa	xmm11,[rsp + _XMM_SAVE + 5*16]
+    movdqa	xmm12,[rsp + _XMM_SAVE + 6*16]
+%endif
+    
+    // Creates a new client context that is by default a new root context.
+// If the current context is the default context then the newly created
+// span automatically becomes a root span. This should only be called with a
+// blank CensusContext as it overwrites it.
+void GenerateClientContext(absl::string_view method, CensusContext* ctxt,
+                           CensusContext* parent_ctx);
+    
+      protobuf::FileDescriptorProto file_desc_proto;
+  grpc::string data;
+  file_desc->CopyTo(&file_desc_proto);
+  file_desc_proto.SerializeToString(&data);
+  response->mutable_file_descriptor_response()->add_file_descriptor_proto(data);
+    
+    
+    {}  // namespace grpc
+    
+    #include <grpcpp/support/config.h>
+    
+    /*! \brief options in the registry to set inplace of operator */
+enum SimpleOpInplaceOption {
+  /*! \brief do not allow inplace in arguments */
+  kNoInplace,
+  /*! \brief in unary forward, allow inplace in with out */
+  kInplaceInOut,
+  /*! \brief in unary backward, allow inplace out_grad with in_grad */
+  kInplaceOutIn,
+  /*! \brief in binary forward, allow inplace left operand with out */
+  kInplaceLhsOut,
+  /*! \brief in binary backward, allow inplace out_grad with lhs_grad */
+  kInplaceOutLhs
+};
+    
+    /*! \brief Cuda runtime compile module. */
+class CudaModule {
+ private:
+  /*! \brief Structure for holding internal info. */
+  struct Chunk {
+    /*!
+     * \brief Constructs cuda module.
+     * \param source cuda source code.
+     * \param exports export symbols before mangling.
+     */
+    Chunk(const char* source,
+          const std::vector<std::string>& options,
+          const std::vector<std::string>& exports);
+    /*! \brief deconstrutor */
+    ~Chunk();
+    /*!
+     * \brief Get handle to cuda kernel from loaded module
+     * \param mangled_name mangled kernel name
+     * \param ctx context to run kernel on
+     * \return loaded function handle
+     */
+    CUfunction GetFunction(const std::string& mangled_name, const Context& ctx);
+    /*! \brief nvrtc program handle. */
+    nvrtcProgram prog_;
+    /*! \brief compiled cuda PTX */
+    char* ptx_;
+    /*! \brief lazily loaded cuda module */
+    std::unordered_map<int, CUmodule> mod_;
+    /*! \brief exported names */
+    std::unordered_set<std::string> exports_;
+  };
+  /*! \brief pointer to Chunk */
+  std::shared_ptr<Chunk> ptr_;
+    }
+    
+    
+    {  Engine::Get()->PushSync([=](RunContext ctx){
+      ndout.CheckAndAlloc();
+      cv::Mat buf(h, w, c == 3 ? CV_8UC3 : CV_8U, ndsrc.data().dptr_);
+      cv::Mat dst(top+h+bot, left+w+right, c == 3 ? CV_8UC3 : CV_8U, ndout.data().dptr_);
+      cv::copyMakeBorder(buf, dst, top, bot, left, right, type, cv::Scalar(value));
+      CHECK(!dst.empty());
+    }, ndout.ctx(), {ndsrc.var()}, {ndout.var()});
+  NDArray *tmp = new NDArray();
+  *tmp = ndout;
+  *out = tmp;
+  API_END();
+}
+
+    
+     protected:
+  /*! \brief batch parameters */
+  BatchParam param_;
+  /*! \brief output data */
+  TBlobBatch out_;
+  /*! \brief on first */
+  int head_;
+  /*! \brief number of overflow instances that readed in round_batch mode */
+  int num_overflow_;
+  /*! \brief tensor to hold data */
+  std::vector<TBlobContainer> data_;
+    
+    
+    {    if (label_parser_.get() != nullptr) {
+      while (label_ptr_ >= label_size_) {
+        CHECK(label_parser_->Next())
+            << 'Data LibSVM's row is smaller than the number of rows in label_libsvm';
+        label_ptr_ = 0;
+        label_size_ = label_parser_->Value().size;
+      }
+      CHECK_LT(label_ptr_, label_size_);
+      const auto label_row = label_parser_->Value()[label_ptr_++];
+      // data, indices and indptr
+      out_.data[3] = AsDataBlob(label_row);
+      out_.data[4] = AsIdxBlob(label_row);
+      out_.data[5] = AsIndPtrPlaceholder(label_row);
+    } else {
+      out_.data[3] = AsScalarLabelBlob(data_row);
+    }
+    return true;
+  }
+    
+    // data file
+DEFINE_string(sensor_conf_file, '', 'Sensor conf file');
+    
+    
+    {  int ret = x;
+  return ret;
+}
+    
+    int ClusterQualityInfo702::lateral_vel_rms(const std::uint8_t* bytes,
+                                           int32_t length) const {
+  Byte t0(bytes + 2);
+  int32_t x = t0.get_byte(0, 1);
+    }
+    
+    
+    {
+    {
+    {}  // namespace conti_radar
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    #include <vector>
+    
+    const TopoNode* NodeWithRange::GetTopoNode() const { return topo_node_; }
+    
+    #include 'modules/canbus/vehicle/gem/protocol/accel_cmd_67.h'
+#include 'modules/canbus/vehicle/gem/protocol/brake_cmd_6b.h'
+#include 'modules/canbus/vehicle/gem/protocol/global_cmd_69.h'
+#include 'modules/canbus/vehicle/gem/protocol/headlight_cmd_76.h'
+#include 'modules/canbus/vehicle/gem/protocol/horn_cmd_78.h'
+#include 'modules/canbus/vehicle/gem/protocol/shift_cmd_65.h'
+#include 'modules/canbus/vehicle/gem/protocol/steering_cmd_6d.h'
+#include 'modules/canbus/vehicle/gem/protocol/turn_cmd_63.h'
+#include 'modules/canbus/vehicle/gem/protocol/wiper_cmd_90.h'
+    
+    TEST_F(GemMessageManagerTest, GetSendProtocols) {
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Accelcmd67::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Brakecmd6b::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Globalcmd69::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Headlightcmd76::ID) !=
+              nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Horncmd78::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Shiftcmd65::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Steeringcmd6d::ID) !=
+              nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Turncmd63::ID) != nullptr);
+  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Wipercmd90::ID) != nullptr);
+}
+    
+    
+    { protected:
+  GemVehicleFactory gem_factory_;
+};
+    
+    // config detail: {'name': 'output_value', 'offset': 0.0, 'precision': 0.001,
+// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 39,
+// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
+double Accelrpt68::output_value(const std::uint8_t* bytes,
+                                int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(0, 8);
+    }
