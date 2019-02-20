e34d9a9faac294b18a63cc7c4eca6a704f5d0e85
SUNsung <weatherius@gmail.com>
2019-02-20 12:38:44 +0200

    Rewrite [26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py] File in: 2019-02-20 12:38:43

--- pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
+++ pyton/26mZ.fD4DvSZ626rY7riInPBBw26Po3dYfhGWb2264Lo.py
@@ -1,191 +1,184 @@
 
         
-        
-def with_confirmation(proc, TIMEOUT):
-    '''Ensures that command can be fixed when confirmation enabled.'''
-    _set_confirmation(proc, True)
+            else:
+        sys.stdout.write('.')
+    sys.stdout.flush()
+    
+    from youtube_dl.utils import intlist_to_bytes
+from youtube_dl.aes import aes_encrypt, key_expansion
+    
+    import datetime
+import io
+import json
+import textwrap
     
     
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
+def build_lazy_ie(ie, name):
+    valid_url = getattr(ie, '_VALID_URL', None)
+    s = ie_template.format(
+        name=name,
+        bases=', '.join(map(get_base_name, ie.__bases__)),
+        valid_url=valid_url,
+        module=ie.__module__)
+    if ie.suitable.__func__ is not InfoExtractor.suitable.__func__:
+        s += '\n' + getsource(ie.suitable)
+    if hasattr(ie, '_make_valid_url'):
+        # search extractors
+        s += make_valid_template.format(valid_url=ie._make_valid_url())
+    return s
     
-    no_match_output = '''
-Listing... Done
-'''
+        def test_youtube_playlist_matching(self):
+        assertPlaylist = lambda url: self.assertMatch(url, ['youtube:playlist'])
+        assertPlaylist('ECUl4u3cNGP61MdtwGTqZA0MreSaDybji8')
+        assertPlaylist('UUBABnxM4Ar9ten8Mdjj1j0Q')  # 585
+        assertPlaylist('PL63F0C78739B09958')
+        assertPlaylist('https://www.youtube.com/playlist?list=UUBABnxM4Ar9ten8Mdjj1j0Q')
+        assertPlaylist('https://www.youtube.com/course?list=ECUl4u3cNGP61MdtwGTqZA0MreSaDybji8')
+        assertPlaylist('https://www.youtube.com/playlist?list=PLwP_SiAcdui0KVebT0mU9Apz359a4ubsC')
+        assertPlaylist('https://www.youtube.com/watch?v=AV6J6_AeFEQ&playnext=1&list=PL4023E734DA416012')  # 668
+        self.assertFalse('youtube:playlist' in self.matching_ies('PLtS2H6bU1M'))
+        # Top tracks
+        assertPlaylist('https://www.youtube.com/playlist?list=MCUS.20142101')
+    
+            def strclass(cls):
+            '''From 2.7's unittest; 2.6 had _strclass so we can't import it.'''
+            return '%s.%s' % (cls.__module__, cls.__name__)
+    
+    import unittest
     
     
-@pytest.mark.parametrize('before, after', [
-    ('brew install sshfs',
-     'brew cask install osxfuse && brew install sshfs')])
-def test_get_new_command(before, after):
-    command = Command(before, output)
-    assert get_new_command(command) == after
-
-    
-        print('LocalOutlierFactor processing...')
-    model = LocalOutlierFactor(n_neighbors=20)
-    tstart = time()
-    model.fit(X)
-    fit_time = time() - tstart
-    scoring = -model.negative_outlier_factor_  # the lower, the more normal
-    fpr, tpr, thresholds = roc_curve(y, scoring)
-    AUC = auc(fpr, tpr)
-    plt.plot(fpr, tpr, lw=1,
-             label=('ROC for %s (area = %0.3f, train-time: %0.2fs)'
-                    % (dataset_name, AUC, fit_time)))
-    
-    
-def plot_results(X, y, label):
-    plt.plot(X, y, label=label, marker='o')
-    
-    def plot(func):
-    random_state = check_random_state(0)
-    one_core = []
-    multi_core = []
-    sample_sizes = range(1000, 6000, 1000)
-    
-        # the training data folder must be passed as first argument
-    movie_reviews_data_folder = sys.argv[1]
-    dataset = load_files(movie_reviews_data_folder, shuffle=False)
-    print('n_samples: %d' % len(dataset.data))
-    
-    The goal of this exercise is to train a linear classifier on text features
-that represent sequences of up to 3 consecutive characters so as to be
-recognize natural languages by using the frequencies of short character
-sequences as 'fingerprints'.
-    
-    # Plot the results (= shape of the data points cloud)
-plt.figure(1)  # two clusters
-plt.title('Outlier detection on a real data set (boston housing)')
-plt.scatter(X1[:, 0], X1[:, 1], color='black')
-bbox_args = dict(boxstyle='round', fc='0.8')
-arrow_args = dict(arrowstyle='->')
-plt.annotate('several confounded points', xy=(24, 19),
-             xycoords='data', textcoords='data',
-             xytext=(13, 10), bbox=bbox_args, arrowprops=arrow_args)
-plt.xlim((xx1.min(), xx1.max()))
-plt.ylim((yy1.min(), yy1.max()))
-plt.legend((legend1_values_list[0].collections[0],
-            legend1_values_list[1].collections[0],
-            legend1_values_list[2].collections[0]),
-           (legend1_keys_list[0], legend1_keys_list[1], legend1_keys_list[2]),
-           loc='upper center',
-           prop=matplotlib.font_manager.FontProperties(size=12))
-plt.ylabel('accessibility to radial highways')
-plt.xlabel('pupil-teacher ratio by town')
-    
-    from sklearn.datasets import make_biclusters
-from sklearn.datasets import samples_generator as sg
-from sklearn.cluster.bicluster import SpectralCoclustering
-from sklearn.metrics import consensus_score
-    
-    The verbose setting on the MiniBatchKMeans enables us to see that some
-clusters are reassigned during the successive calls to
-partial-fit. This is because the number of patches that they represent
-has become too low, and it is better to choose a random new
-cluster.
-'''
-print(__doc__)
-    
-        plot_clustering(X_red, clustering.labels_, '%s linkage' % linkage)
-    
-    for face_landmarks in face_landmarks_list:
-    pil_image = Image.fromarray(image)
-    d = ImageDraw.Draw(pil_image, 'RGBA')
-    
-        :param img: A list of images (each as a numpy array)
-    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
-    :return: A list of dlib 'rect' objects of found face locations
+def get_db():
+    '''Connect to the application's configured database. The connection
+    is unique for each request and will be reused if this is called
+    again.
     '''
-    return cnn_face_detector(images, number_of_times_to_upsample, batch_size=batch_size)
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
     
-    while True:
-    print('Capturing image.')
-    # Grab a single frame of video from the RPi camera as a numpy array
-    camera.capture(output, format='rgb')
+        Basic usage is outlined in the :ref:`testing` chapter.
+    '''
     
-        # Every 128 frames (the default batch size), batch process the list of frames to find faces
-    if len(frames) == 128:
-        batch_of_face_locations = face_recognition.batch_face_locations(frames, number_of_times_to_upsample=0)
     
-    setup(
-    name='face_recognition',
-    version='1.2.3',
-    description='Recognize faces from Python or from the command line',
-    long_description=readme + '\n\n' + history,
-    author='Adam Geitgey',
-    author_email='ageitgey@gmail.com',
-    url='https://github.com/ageitgey/face_recognition',
-    packages=[
-        'face_recognition',
-    ],
-    package_dir={'face_recognition': 'face_recognition'},
-    package_data={
-        'face_recognition': ['models/*.dat']
-    },
-    entry_points={
-        'console_scripts': [
-            'face_recognition=face_recognition.face_recognition_cli:main',
-            'face_detection=face_recognition.face_detection_cli:main'
-        ]
-    },
-    install_requires=requirements,
-    license='MIT license',
-    zip_safe=False,
-    keywords='face_recognition',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
-)
+class MethodView(with_metaclass(MethodViewType, View)):
+    '''A class-based view that dispatches request methods to the corresponding
+    class methods. For example, if you implement a ``get`` method, it will be
+    used to handle ``GET`` requests. ::
+    
+            # return Werkzeug's default when not in an app context
+        return super(Response, self).max_cookie_size
 
     
-    # 你需要一个2代以上的树莓派，并在树莓派上安装face_recognition，并连接上picamera摄像头
-# 并确保picamera这个模块已经安装（树莓派一般会内置安装）
-# 你可以参考这个教程配制你的树莓派：
-# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
     
-            # User chose to not deal with backwards NULL issues for 'Environment.project_id'
-        raise RuntimeError(
-            'Cannot reverse this migration. 'Environment.project_id' and its values cannot be restored.'
-        )
+CLOUD_CLIENT = 'google-cloud-spanner'
+CLOUD_CLIENT_MINIMUM_VERSION = '0.23.0'
+CLOUD_CLIENT_USER_AGENT = 'ansible-spanner-0.1'
     
-            for release_project in RangeQuerySetWrapperWithProgressBar(
-            orm.ReleaseProject.objects.all()
-        ):
-            orm.ReleaseProject.objects.filter(id=release_project.id).update(
-                new_groups=orm.Group.objects.filter(
-                    project_id=release_project.project_id,
-                    first_release_id=release_project.release_id,
-                ).count()
-            )
+            if add_servers:
+            monitoring_policy = _attach_monitoring_policy_server(module,
+                                                                 oneandone_conn,
+                                                                 monitoring_policy['id'],
+                                                                 add_servers)
+            _check_mode(module, monitoring_policy)
+            changed = True
     
-        def _forwards(self, orm):
-        'Write your forwards methods here.'
+            elif desired_state == 'offline':
+            if current_state == HOST_ABSENT:
+                self.fail(msg='absent host cannot be placed in offline state')
+            elif current_state in [HOST_STATES.MONITORED, HOST_STATES.DISABLED]:
+                if one.host.status(host.ID, HOST_STATUS.OFFLINE):
+                    self.wait_for_host_state(host, [HOST_STATES.OFFLINE])
+                    result['changed'] = True
+                else:
+                    self.fail(msg='could not set host offline')
+            elif current_state in [HOST_STATES.OFFLINE]:
+                pass
+            else:
+                self.fail(msg='unknown host state %s, cowardly refusing to change state to offline' % current_state_name)
     
-        def backwards(self, orm):
-        # Removing unique constraint on 'CommitAuthor', fields ['organization_id', 'external_id']
-        db.delete_unique('sentry_commitauthor', ['organization_id', 'external_id'])
     
-            # Adding field 'ReleaseFile.dist'
-        db.add_column(
-            'sentry_releasefile',
-            'dist',
-            self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                to=orm['sentry.Distribution'], null=True
-            ),
-            keep_default=False
-        )
+VALID_PROTO = ['Tcp', 'Udp', 'Icmp', 'Other', 'Any']
+VALID_RULE_KEYS = ['policy', 'is_enable', 'enable_logging', 'description',
+                   'dest_ip', 'dest_port', 'source_ip', 'source_port',
+                   'protocol']
+    
+    
+def validate_nat_rules(nat_rules):
+    for rule in nat_rules:
+        if not isinstance(rule, dict):
+            raise VcaError('nat rules must be a list of dictionaries, '
+                           'Please check', valid_keys=VALID_RULE_KEYS)
+    
+    
+def main():
+    argument_spec = ipa_argument_spec()
+    argument_spec.update(zone_name=dict(type='str', required=True),
+                         state=dict(type='str', default='present', choices=['present', 'absent']),
+                         )
+    
+    
+def ensure(module, client):
+    name = module.params['cn']
+    state = module.params['state']
+    
+    # Create groups like 'virt_kvm_host'
+- group_by:
+    key: virt_{{ ansible_virtualization_type }}_{{ ansible_virtualization_role }}
+    
+    The problem is  :
+Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
+Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
+'''
+from __future__ import print_function
+    
+      fileinfo = FileInfo(filename)
+  file_path_from_root = fileinfo.RepositoryName()
+  if _root:
+    suffix = os.sep
+    # On Windows using directory separator will leave us with
+    # 'bogus escape error' unless we properly escape regex.
+    if suffix == '\\':
+      suffix += '\\'
+    file_path_from_root = re.sub('^' + _root + suffix, '', file_path_from_root)
+  return re.sub(r'[^a-zA-Z0-9]', '_', file_path_from_root).upper() + '_'
+    
+    def line_length(filename):
+    return sum(1 for line in open(filename))
+    
+    *What does this example do?
+    
+    
+class MidnightTimeProvider(object):
+    '''
+    Class implemented as hard-coded stub (in contrast to configurable stub).
+    '''
+    
+        def __init__(self):
+        self.time_provider = datetime.datetime
+    
+        def __init__(self, text):
+        self._text = text
+    
+    
+class MobileView(object):
+    def show_index_page(self):
+        print('Displaying mobile index page')
+    
+    
+class TestData(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dec_obs = DecimalViewer()
+        cls.hex_obs = HexViewer()
+        cls.sub = Data('Data')
+        # inherited behavior already tested with TestSubject
+        cls.sub.attach(cls.dec_obs)
+        cls.sub.attach(cls.hex_obs)
+    
+        def test_initial_am_station(self):
+        station = self.radio.state.stations[self.radio.state.pos]
+        expected_station = '1250'
+        self.assertEqual(station, expected_station)
