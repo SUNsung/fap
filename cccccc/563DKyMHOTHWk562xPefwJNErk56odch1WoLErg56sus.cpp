3ad3c150416e479eb199f7ec026bdb972379347c
SUNsung <weatherius@gmail.com>
2019-02-21 02:20:10 +0200

    Rewrite [563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp] File in: 2019-02-21 02:20:10

--- cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
+++ cccccc/563DKyMHOTHWk562xPefwJNErk56odch1WoLErg56sus.cpp
@@ -1,131 +1,185 @@
 
         
-        OPERATOR_SCHEMA(EnforceFinite)
-    .NumInputs(1)
-    .NumOutputs(0)
-    .SetDoc(R'DOC(
-Raise if there is NaN or Inf values in the input tensor.
-)DOC')
-    .Input(0, 'input', 'Input tensor');
-    
-    **Result**
-    
-    
-    {} // namespace caffe2
-    
-    namespace grpc {
-    }
-    
-    #include 'src/cpp/ext/filters/census/context.h'
-    
-    
-    {}  // namespace grpc
+        
+    {}  // namespace atom
 
     
-    Aggregation BytesDistributionAggregation() {
-  return Aggregation::Distribution(BucketBoundaries::Explicit(
-      {0, 1024, 2048, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216,
-       67108864, 268435456, 1073741824, 4294967296}));
+      base::WeakPtrFactory<TrackableObjectBase> weak_factory_;
+    
+      // There is a new update which has been downloaded.
+  virtual void OnUpdateDownloaded(const std::string& release_notes,
+                                  const std::string& release_name,
+                                  const base::Time& release_date,
+                                  const std::string& update_url) {}
+    
+    
+    {}  // namespace atom
+    
+    #if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef _module = {
+  PyModuleDef_HEAD_INIT,
+  kModuleName,
+  kModuleDocstring,
+  -1,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+#define INITFUNC PyInit__api_implementation
+#define INITFUNC_ERRORVAL NULL
+#else
+#define INITFUNC init_api_implementation
+#define INITFUNC_ERRORVAL
+#endif
+    
+    #include <google/protobuf/python/python_protobuf.h>
+    
+    #include <google/protobuf/compiler/code_generator.h>
+    
+    
+    {  // Moving messages on two different arenas should lead to a copy.
+  *message2_on_arena = std::move(*message1_on_arena);
+  EXPECT_NE(nested, &message2_on_arena->optional_nested_message());
+  TestUtil::ExpectAllFieldsSet(*message1_on_arena);
+  TestUtil::ExpectAllFieldsSet(*message2_on_arena);
 }
     
+    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
     
-    {  std::vector<const protobuf::FieldDescriptor*> extensions;
-  descriptor_pool_->FindAllExtensions(desc, &extensions);
-  for (auto it = extensions.begin(); it != extensions.end(); it++) {
-    response->add_extension_number((*it)->number());
-  }
-  response->set_base_type_name(type);
-  return Status::OK;
-}
-    
-    
-    {b2PolyNode* b2PolyNode::GetRightestConnection(b2Vec2& incomingDir){
-	b2Vec2 diff = position-incomingDir;
-	b2PolyNode temp(diff);
-	b2PolyNode* res = GetRightestConnection(&temp);
-	b2Assert(res);
-	return res;
-}
-}
-
-    
-    	// ----------------------------------------------------------------------------------------------------
-	// calculate the error for the block by summing the pixel errors
-	//
-	void Block4x4Encoding::CalcBlockError(void)
-	{
-		m_fError = 0.0f;
+      // If there were any extensions or this file has any dependencies, output
+  // a registry to override to create the file specific registry.
+  if (file_contains_extensions || !deps_with_extensions.empty()) {
+    printer->Print(
+        '+ (GPBExtensionRegistry*)extensionRegistry {\n'
+        '  // This is called by +initialize so there is no need to worry\n'
+        '  // about thread safety and initialization of registry.\n'
+        '  static GPBExtensionRegistry* registry = nil;\n'
+        '  if (!registry) {\n'
+        '    GPB_DEBUG_CHECK_RUNTIME_VERSIONS();\n'
+        '    registry = [[GPBExtensionRegistry alloc] init];\n');
+    }
+    }
     }
     
-    //OutRec: contains a path in the clipping solution. Edges in the AEL will
-//carry a pointer to an OutRec when they are part of the clipping solution.
-struct OutRec {
-  int       Idx;
-  bool      IsHole;
-  bool      IsOpen;
-  OutRec   *FirstLeft;  //see comments in clipper.pas
-  PolyNode *PolyNd;
-  OutPt    *Pts;
-  OutPt    *BottomPt;
+    
+    {
+    {
+    {
+    {}  // namespace objectivec
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
+    
+      // write central directory
+  io::CodedOutputStream output(raw_output_);
+  for (int i = 0; i < num_entries; ++i) {
+    const string &filename = files_[i].name;
+    uint16 filename_size = filename.size();
+    uint32 crc32 = files_[i].crc32;
+    uint32 size = files_[i].size;
+    uint32 offset = files_[i].offset;
+    }
+    
+    GzipOutputStream::GzipOutputStream(ZeroCopyOutputStream* sub_stream,
+                                   const Options& options) {
+  Init(sub_stream, options);
+}
+    
+    class PAGE_RES_IT;
+class ROW;
+class WERD_RES;
+    
+    class OrientationDetector {
+ public:
+  OrientationDetector(const GenericVector<int>* allowed_scripts,
+                      OSResults* results);
+  bool detect_blob(BLOB_CHOICE_LIST* scores);
+  int get_orientation();
+ private:
+  OSResults* osr_;
+  const GenericVector<int>* allowed_scripts_;
 };
     
-    #define C_SUBFROM( res , a)\
-    do {(res).r = ADD32((res).r,(a).r);  (res).i = SUB32((res).i,(a).i); \
-    }while(0)
+      /**
+   * Returns a binary image of the current object at the given level.
+   * The position and size match the return from BoundingBoxInternal, and so
+   * this could be upscaled with respect to the original input image.
+   * Use pixDestroy to delete the image after use.
+   */
+  Pix* GetBinaryImage(PageIteratorLevel level) const;
     
-    #define MULT16_32_PX(a, b, Q) MULT16_32_PX_(a, b, Q, __FILE__, __LINE__)
-static OPUS_INLINE int MULT16_32_PX_(int a, opus_int64 b, int Q, char *file, int line)
-{
-   opus_int64 res;
-   if (!VERIFY_SHORT(a) || !VERIFY_INT(b))
-   {
-      fprintf (stderr, 'MULT16_32_P%d: inputs are not short+int: %d %d in %s: line %d\n\n', Q, (int)a, (int)b, file, line);
-#ifdef FIXED_DEBUG_ASSERT
-      celt_assert(0);
-#endif
-   }
-   if (ABS32(b)>=((opus_int64)(1)<<(15+Q)))
-   {
-      fprintf (stderr, 'MULT16_32_Q%d: second operand too large: %d %d in %s: line %d\n\n', Q, (int)a, (int)b,file, line);
-#ifdef FIXED_DEBUG_ASSERT
-      celt_assert(0);
-#endif
-   }
-   res = ((((opus_int64)a)*(opus_int64)b) + (((opus_val32)(1)<<Q)>>1))>> Q;
-   if (!VERIFY_INT(res))
-   {
-      fprintf (stderr, 'MULT16_32_P%d: output is not int: %d*%d=%d in %s: line %d\n\n', Q, (int)a, (int)b,(int)res, file, line);
-#ifdef FIXED_DEBUG_ASSERT
-      celt_assert(0);
-#endif
-   }
-   if (Q==15)
-      celt_mips+=4;
-   else
-      celt_mips+=5;
-   return res;
+    void BoxWord::CopyFrom(const BoxWord& src) {
+  bbox_ = src.bbox_;
+  length_ = src.length_;
+  boxes_.clear();
+  boxes_.reserve(length_);
+  for (int i = 0; i < length_; ++i)
+    boxes_.push_back(src.boxes_[i]);
 }
     
-    class UnicodeSet;
     
-    U_NAMESPACE_END
+    { private:
+  // The collection of images to put in the PDF.
+  Pixa* pixa_;
+  // The fonts used to draw text captions.
+  L_Bmf* fonts_;
+};
     
-        /**
-     * Get maximum significant digits. INT32_MAX means no maximum.
-     */
-    int32_t getMax() const {
-        return fMax;
+      // Adds a new point. Takes a copy - the pt doesn't need to stay in scope.
+  // Add must be called on points in sequence along the line.
+  void Add(const ICOORD& pt);
+  // Associates a half-width with the given point if a point overlaps the
+  // previous point by more than half the width, and its distance is further
+  // than the previous point, then the more distant point is ignored in the
+  // distance calculation. Useful for ignoring i dots and other diacritics.
+  void Add(const ICOORD& pt, int halfwidth);
+    
+    int main() {
     }
     
+    using namespace std;
     
-    {    // ...and if they're equal, we compare times
-    if (millis < ruleMillis) return -1;
-    else if (millis > ruleMillis) return 1;
-    else return 0;
-}
+    /// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
     
-    //----------------------------------------------------------------------
     
+    {
+    {        if( node ){
+            __inorderTraversal(node->left, res);
+            res.push_back( node->val );
+            __inorderTraversal(node->right, res);
+        }
+    }
+};
     
-    {    return outLen;
-}
+    int main() {
+    }
+    
+    // My Non-Recursive
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+    }
+    
+    int main() {
+    }
+    
+    public:
+    vector<int> postorderTraversal(TreeNode* root) {
+    }
