734946d5cf62661accb9509dee780c2a3f8ec7c4
SUNsung <weatherius@gmail.com>
2019-02-21 20:59:11 +0200

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2019-02-21 20:59:10

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,132 +1,101 @@
 
         
-        # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
-    
-        # NOTE: Only run one test per function otherwise you will have
-    # inf recursion
-    def test_include(self):
-        self.verify_fnmatch('test_fnmatch.?onf')
-    
-        def test_nonexistent_like(self):
-        with mock.patch('certbot.util.get_os_info') as mock_info:
-            mock_info.return_value = ('nonexistent', 'irrelevant')
-            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
-                for like in entrypoint.OVERRIDE_CLASSES.keys():
-                    mock_like.return_value = [like]
-                    self.assertEqual(entrypoint.get_configurator(),
-                                     entrypoint.OVERRIDE_CLASSES[like])
-    
-    # Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'certbot-apache', u'certbot-apache Documentation',
-     author, 'certbot-apache', 'One line description of project.',
-     'Miscellaneous'),
-]
-    
-    
-def cityscapes_to_coco_without_person_rider(cityscapes_id):
-    lookup = {
-        0: 0,  # ... background
-        1: 2,  # bicycle
-        2: 3,  # car
-        3: -1,  # person (ignore)
-        4: 7,  # train
-        5: 8,  # truck
-        6: 4,  # motorcycle
-        7: 6,  # bus
-        8: -1,  # rider (ignore)
-    }
-    return lookup[cityscapes_id]
-    
-        # Find which gt ROI each ex ROI has max overlap with:
-    # this will be the ex ROI's gt target
-    gt_assignment = ex_gt_overlaps.argmax(axis=1)
-    gt_rois = rois[gt_inds[gt_assignment], :]
-    ex_rois = rois[ex_inds, :]
-    # Use class '1' for all boxes if using class_agnostic_bbox_reg
-    targets[ex_inds, 0] = (
-        1 if cfg.MODEL.CLS_AGNOSTIC_BBOX_REG else labels[ex_inds])
-    targets[ex_inds, 1:] = box_utils.bbox_transform_inv(
-        ex_rois, gt_rois, cfg.MODEL.BBOX_REG_WEIGHTS)
-    return targets
-    
-    import logging
-import numpy as np
-import os
-import shutil
-import uuid
-    
-        blobs: a dict mapping from blob name to numpy ndarray
-    blob_prefix: name prefix to use for the FPN blobs
-    rois: the source rois as a 2D numpy array of shape (N, 5) where each row is
-      an roi and the columns encode (batch_idx, x1, y1, x2, y2)
-    target_lvls: numpy array of shape (N, ) indicating which FPN level each roi
-      in rois should be assigned to
-    lvl_min: the finest (highest resolution) FPN level (e.g., 2)
-    lvl_max: the coarest (lowest resolution) FPN level (e.g., 6)
-    '''
-    rois_idx_order = np.empty((0, ))
-    rois_stacked = np.zeros((0, 5), dtype=np.float32)  # for assert
-    for lvl in range(lvl_min, lvl_max + 1):
-        idx_lvl = np.where(target_lvls == lvl)[0]
-        blobs[blob_prefix + '_fpn' + str(lvl)] = rois[idx_lvl, :]
-        rois_idx_order = np.concatenate((rois_idx_order, idx_lvl))
-        rois_stacked = np.vstack(
-            [rois_stacked, blobs[blob_prefix + '_fpn' + str(lvl)]]
-        )
-    rois_idx_restore = np.argsort(rois_idx_order).astype(np.int32, copy=False)
-    blobs[blob_prefix + '_idx_restore_int32'] = rois_idx_restore
-    # Sanity check that restore order is correct
-    assert (rois_stacked[rois_idx_restore] == rois).all()
-    
-    
-def _build_forward_graph(model, single_gpu_build_func):
-    '''Construct the forward graph on each GPU.'''
-    all_loss_gradients = {}  # Will include loss gradients from all GPUs
-    # Build the model on each GPU with correct name and device scoping
-    for gpu_id in range(cfg.NUM_GPUS):
-        with c2_utils.NamedCudaScope(gpu_id):
-            all_loss_gradients.update(single_gpu_build_func(model))
-    return all_loss_gradients
-    
-    from detectron.core.config import cfg
-from detectron.modeling.generate_anchors import generate_anchors
-from detectron.utils.c2 import const_fill
-from detectron.utils.c2 import gauss_fill
-import detectron.modeling.FPN as FPN
-import detectron.utils.blob as blob_utils
-    
-    
-def compute_targets(ex_rois, gt_rois, weights=(1.0, 1.0, 1.0, 1.0)):
-    '''Compute bounding-box regression targets for an image.'''
-    return box_utils.bbox_transform_inv(ex_rois, gt_rois, weights).astype(
-        np.float32, copy=False
-    )
+        # write out the dataset
+write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
+print ('Saved to ', os.path.join(FLAGS.save_dir,
+                                 FLAGS.datafile_name + '_' + dataset_name))
 
     
-            # Find overlap between all foreground rois and the bounding boxes
-        # enclosing each segmentation
-        rois_fg = sampled_boxes[fg_inds]
-        overlaps_bbfg_bbpolys = box_utils.bbox_overlaps(
-            rois_fg.astype(np.float32, copy=False),
-            boxes_from_polys.astype(np.float32, copy=False)
-        )
-        # Map from each fg rois to the index of the mask with highest overlap
-        # (measured by bbox overlap)
-        fg_polys_inds = np.argmax(overlaps_bbfg_bbpolys, axis=1)
+      train_path = os.path.join(data_path, 'train_lm.tfrecords')
+  valid_path = os.path.join(data_path, 'test_lm.tfrecords')
     
-        result = False
+      samples = []
+  for sequence_id in xrange(min(len(arr), max_num_to_print)):
+    buffer_str = ' '.join(
+        [str(id_to_word[index]) for index in arr[sequence_id, :]])
+    samples.append(buffer_str)
+  return samples
+    
+      Args:
+    hparams:  Hyperparameters for the MaskGAN.
+    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
+    is_training:  Whether the model is training.
+    reuse (Optional):  Whether to reuse the model.
+    
+      if FLAGS.dis_share_embedding:
+    assert hparams.dis_rnn_size == hparams.gen_rnn_size, (
+        'If you wish to share Discriminator/Generator embeddings, they must be'
+        ' same dimension.')
+    with tf.variable_scope('gen/rnn', reuse=True):
+      embedding = tf.get_variable('embedding',
+                                  [FLAGS.vocab_size, hparams.gen_rnn_size])
+    
+    
+class Server(threading.Thread):
+    '''Dummy server using for unit testing'''
+    WAIT_EVENT_TIMEOUT = 5
+    
+            for attr, value in state.items():
+            setattr(self, attr, value)
+    
+    import sys
+    
+        If 'value' is None, do nothing'''
+    value_changed = value is not None
+    if value_changed:
+        old_value = os.environ.get(env_name)
+        os.environ[env_name] = value
     try:
-        ret = urlopen(url, timeout=2)
-        result = (ret.code == 200)
-    except HTTPError as e:
-        print(e, file=sys.stderr)
-    except URLError as e:
-        print(e, file=sys.stderr)
-    except timeout as e:
-        print(e, file=sys.stderr)
-    except Exception as e:
-        print(e, file=sys.stderr)
+        yield
+    finally:
+        if value_changed:
+            if old_value is None:
+                del os.environ[env_name]
+            else:
+                os.environ[env_name] = old_value
+    
+        def _colision_resolution(self, key, data=None):
+        new_key = self.hash_function(key + 1)
+    
+    from matplotlib import pyplot as plt
+def plot_heterogeneity(heterogeneity, k):
+    plt.figure(figsize=(7,4))
+    plt.plot(heterogeneity, linewidth=4)
+    plt.xlabel('# Iterations')
+    plt.ylabel('Heterogeneity')
+    plt.title('Heterogeneity of clustering over time, K={0:d}'.format(k))
+    plt.rcParams.update({'font.size': 16})
+    plt.show()
+    
+        log_predict = np.log(predict+1)
+    log_actual = np.log(actual+1)
+    
+    ### OUTPUT ###
+# Floor: One | Size: Big
+# Floor: More than One | Size: Small
+# Floor: One | Size: Big and fancy
+
+    
+    *References:
+http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+https://fkromer.github.io/python-pattern-references/design/#factory-method
+https://sourcemaking.com/design_patterns/factory_method
+    
+        def contribute(self):
+        self.blackboard.common_state['problems'] += random.randint(10, 20)
+        self.blackboard.common_state['suggestions'] += random.randint(10, 20)
+        self.blackboard.common_state['contributions'] += [self.__class__.__name__]
+        self.blackboard.common_state['progress'] += random.randint(10, 30)
+    
+    
+class Unit(object):
+    def __init__(self, HierachicalStateMachine):
+        self.hsm = HierachicalStateMachine
+    
+    
+class BusinessLogic(object):
+    ''' Business logic holding data store instances '''
+    
+    *TL;DR80
+Provides a simpler unified interface to a complex system.
+'''
