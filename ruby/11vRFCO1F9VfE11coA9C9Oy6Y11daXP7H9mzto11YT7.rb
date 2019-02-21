35279f188b4b35ab9a4696d78b0b0b7453f062ca
SUNsung <weatherius@gmail.com>
2019-02-21 08:41:58 +0200

    Rewrite [11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb] File in: 2019-02-21 08:41:57

--- ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
+++ ruby/11vRFCO1F9VfE11coA9C9Oy6Y11daXP7H9mzto11YT7.rb
@@ -1,144 +1,193 @@
 
         
-            def set_error(platform, block)
-      unless error_blocks[platform].nil?
-        UI.error('You defined multiple `error` blocks in your `Fastfile`. The last one being set will be used.')
+          Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
+    cmd = g.split('-').last
+    p.command(cmd.to_sym) do |c|
+      c.syntax cmd
+      c.action do
+        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
+          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
       end
-      error_blocks[platform] = block
     end
-    
-          begin
-        if xcode_outdated
-          # We have to raise that error within this `begin` block to show a nice user error without a stack trace
-          FastlaneCore::UI.user_error!('fastlane requires a minimum version of Xcode #{Fastlane::MINIMUM_XCODE_RELEASE}, please upgrade and make sure to use `sudo xcode-select -s /Applications/Xcode.app`')
-        end
-    
-          def self.example_code
-        [
-          'add_git_tag # simple tag with default values',
-          'add_git_tag(
-            grouping: 'fastlane-builds',
-            prefix: 'v',
-            postfix: '-RC1',
-            build_number: 123
-          )',
-          '# Alternatively, you can specify your own tag. Note that if you do specify a tag, all other arguments are ignored.
-          add_git_tag(
-            tag: 'my_custom_tag'
-          )'
-        ]
-      end
-    
-          it 'logs the command if verbose' do
-        with_verbose(true) do
-          allow(Fastlane::Actions).to receive(:sh).with(anything, { log: true }).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: 'foo.bar')
-          end').runner.execute(:test)
-        end
-      end
-    
-          context 'when specify strict option' do
-        it 'adds strict option' do
-          result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-              strict: true
-            )
-          end').runner.execute(:test)
-    
-          it 'yields command output' do
-        expect_command('ls', '-la', exitstatus: 1, output: 'Heeeelp! Something went wrong.')
-        Fastlane::Actions.sh('ls', '-la') do |status, result|
-          expect(status.exitstatus).to eq(1)
-          expect(result).to eq('Heeeelp! Something went wrong.')
-        end
-      end
-    
-      if FastlaneCore::CommandExecutor.which('grep')
-    if FastlaneCore::Helper.windows?
-      compare_string = simulate_windows_shell_unwrapping(compare_string)
-    else
-      compare_string = simulate_normal_shell_unwrapping(compare_string)
-    end
-    compare_command = 'grep 'foo' #{escaped}'
-    expected_compare_error = 'grep: ' + compare_string + ': No such file or directory'
-  elsif FastlaneCore::CommandExecutor.which('find')
-    compare_string = simulate_normal_shell_unwrapping(compare_string)
-    compare_string = compare_string.upcase
-    compare_command = 'find \'foo\' #{escaped}'
-    expected_compare_error = 'File not found - ' + compare_string
   end
     
-          sh <<-SH
-        git commit --allow-empty -a -m '#{source_version} release'  &&
-        git tag -s v#{source_version} -m '#{source_version} release'  &&
-        git push && (git push origin || true) &&
-        git push --tags && (git push origin --tags || true)
-      SH
+          def print_worst_offenders
+        @io.puts
+        @io.puts 'Worst offenders:'
+        @timings.sort_by { |_f, t| -t }.take(10).each do |(f, t)|
+          @io.puts '  #{t}s for #{f}'
+        end
+        @io.puts
+      end
+    
+    module Jekyll
+  module Converters
+    class Markdown
+      class KramdownParser
+        CODERAY_DEFAULTS = {
+          'css'               => 'style',
+          'bold_every'        => 10,
+          'line_numbers'      => 'inline',
+          'line_number_start' => 1,
+          'tab_width'         => 4,
+          'wrap'              => 'div',
+        }.freeze
+    
+            unless post && post.id
+          puts post.errors.full_messages if post
+          puts creator.errors.inspect
+          raise 'Failed to create description for trust level 3 lounge!'
+        end
+    
+            # Reset topic count because we don't count the description topic
+        DB.exec 'UPDATE categories SET topic_count = 0 WHERE id = #{staff.id}'
+      end
+    end
+  end
+end
+
+    
+        it 'allows to delete a user' do
+      visit admin_users_path
+      find(:css, 'a[href='/admin/users/#{users(:bob).id}']').click
+      expect(page).to have_text('User 'bob' was deleted.')
+      expect(page).to have_no_text('bob@example.com')
     end
     
-      <script type='text/javascript'>
-  //<!--
-  function toggle(id) {
-    var pre  = document.getElementById('pre-' + id);
-    var post = document.getElementById('post-' + id);
-    var context = document.getElementById('context-' + id);
+      it 'imports a scenario which requires a service' do
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
+    click_on 'Start Import'
+    check('I confirm that I want to import these Agents.')
+    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
+    expect(page).to have_text('Import successful!')
+  end
+end
+
+    
+      describe '#yes_no' do
+    it 'returns a label 'Yes' if any truthy value is given' do
+      [true, Object.new].each { |value|
+        label = yes_no(value)
+        expect(label).to be_html_safe
+        expect(Nokogiri(label).text).to eq 'Yes'
+      }
+    end
+    
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
+  end
+    
+          @agent1.update!(disabled: true)
+    
+      let :new_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title', 'hidden' => true }
     }
+  end
     
-            safe?(env) ||
-          valid_token?(session, env['HTTP_X_CSRF_TOKEN']) ||
-          valid_token?(session, Request.new(env).params[options[:authenticity_param]]) ||
-          ( options[:allow_if] && options[:allow_if].call(env) )
+        it 'requires a message' do
+      @log.message = ''
+      expect(@log).not_to be_valid
+      @log.message = nil
+      expect(@log).not_to be_valid
+      expect(@log).to have(1).error_on(:message)
+    end
+    
+    describe Agents::BoxcarAgent do
+  before(:each) do
+  @valid_params = {
+                    'user_credentials' => 'access_token',
+                    'title' => 'Sample Title',
+                    'body' => 'Sample Body'
+                  }
+  @checker = Agents::BoxcarAgent.new(:name => 'boxcartest', :options => @valid_params)
+  @checker.user = users(:bob)
+  @checker.save!
+    
+    require 'clamp'
+require 'pluginmanager/util'
+require 'pluginmanager/gemfile'
+require 'pluginmanager/install'
+require 'pluginmanager/remove'
+require 'pluginmanager/list'
+require 'pluginmanager/update'
+require 'pluginmanager/pack'
+require 'pluginmanager/unpack'
+require 'pluginmanager/generate'
+require 'pluginmanager/prepare_offline_pack'
+require 'pluginmanager/proxy_support'
+configure_proxy
+    
+            return nil
+      end
+    end
+  end
+end end end
+
+    
+        context 'without a specific plugin' do
+      it 'display a list of plugins' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list')
+        expect(result.stdout.split('\n').size).to be > 1
       end
     
-            DIRECTIVES.each do |d|
-          if options.key?(d)
-            directives << '#{d.to_s.sub(/_/, '-')} #{options[d]}'
+            expect(cop.highlights).to eq([close])
+        expect(cop.messages)
+          .to eq([described_class::SAME_LINE_MESSAGE])
+      end
+    
+          # Checks whether this node body is a void context.
+      #
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
+      end
+    
+          # Returns the iteration variable of the `for` loop.
+      #
+      # @return [Node] The iteration variable of the `for` loop
+      def variable
+        node_parts[0]
+      end
+    
+          # Custom destructuring method. This is used to normalize the branches
+      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
+      #
+      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
+      def node_parts
+        [self, self]
+      end
+    end
+  end
+end
+
+    
+          page.find('#option_type_option_values_attributes_0_name').set('color')
+      page.find('#option_type_option_values_attributes_0_presentation').set('black')
+      click_button 'Update'
+      expect(page).to have_content('successfully updated!')
+    
+    desc 'Generates a dummy app for testing for every Spree engine'
+task :test_app do
+  SPREE_GEMS.each do |gem_name|
+    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
+      sh 'rake test_app'
+    end
+  end
+end
+    
+              def render_order(result)
+            if result.success?
+              render_serialized_payload { serialized_current_order }
+            else
+              render_error_payload(result.error)
+            end
           end
-        end
     
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   CSRF
-    # Supported browsers:: all
-    # More infos::         http://flask.pocoo.org/docs/0.10/security/#json-security
-    #                      http://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx
-    #
-    # JSON GET APIs are vulnerable to being embedded as JavaScript when the
-    # Array prototype has been patched to track data. Checks the referrer
-    # even on GET requests if the content type is JSON.
-    #
-    # If request includes Origin HTTP header, defers to HttpOrigin to determine
-    # if the request is safe. Please refer to the documentation for more info.
-    #
-    # The `:allow_if` option can be set to a proc to use custom allow/deny logic.
-    class JsonCsrf < Base
-      default_options :allow_if => nil
-    
-          def deliver(msg)
-        if msg.respond_to?(:deliver_now)
-          # Rails 4.2/5.0
-          msg.deliver_now
-        else
-          # Rails 3.2/4.0/4.1
-          msg.deliver
-        end
-      end
-    end
-    
-          def retrieve
-        map(&:make_new)
-      end
-    
-          # Clear all queued jobs across all workers
-      def clear_all
-        Queues.clear_all
-      end
-    
-          def enable(*opts)
-        opts.each {|key| set(key, true) }
-      end
-    
-          erb(content, options)
-    end
+            self.line_item_options = []
