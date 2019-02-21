268628a1062b655d5f774b1be02fbdb3c36a43d2
SUNsung <weatherius@gmail.com>
2019-02-22 00:03:06 +0200

    Rewrite [141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp] File in: 2019-02-22 00:03:05

--- cccccc/141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp
+++ cccccc/141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp
@@ -1,133 +1,440 @@
 
         
-        #endif  // TENSORFLOW_PYTHON_FRAMEWORK_CPP_SHAPE_INFERENCE_H_
-
-    
-    #include 'tensorflow/core/framework/op.h'
+        class AckermannOp : public OpKernel {
+ public:
+  explicit AckermannOp(OpKernelConstruction* context) : OpKernel(context) {}
+    }
     
     
     {}  // namespace tensorflow
 
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    PyObject* MakePyString(const string& s) {
-  return PyString_FromString(s.c_str());
+    static void TensorReleaser_dealloc(PyObject* pself) {
+  TensorReleaser* self = reinterpret_cast<TensorReleaser*>(pself);
+  (*self->destructor)();
+  delete self->destructor;
+  TensorReleaserType.tp_free(pself);
 }
     
-    // Actually dereferences cached numpy arrays. REQUIRES being called while
-// holding the GIL.
-void ClearDecrefCache();
+    #endif  // TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_BRIDGE_H_
+
     
-    namespace tensorflow {
+    #include 'tensorflow/python/lib/core/py_exception_registry.h'
+    
+    #include <Python.h>
+    
+    Safe_TF_StatusPtr make_safe(TF_Status* status) {
+  return Safe_TF_StatusPtr(status);
+}
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+          embedded_test_server()->RegisterRequestHandler(base::Bind(
+          &NWWebViewTestBase::UserAgentResponseHandler,
+          kUserAgentRedirectResponsePath,
+          embedded_test_server()->GetURL(kRedirectResponseFullPath)));
+    
+    #if defined(COMPONENT_BUILD) && defined(WIN32)
+#define NW_HOOK_MAP(type, sym, fn) BASE_EXPORT type fn;
+#else
+#define NW_HOOK_MAP(type, sym, fn) extern type fn;
+#endif
+#include 'content/nw/src/common/node_hooks.h'
+#undef NW_HOOK_MAP
+    
+    // Request Shell's id for current render_view_host.
+IPC_SYNC_MESSAGE_ROUTED0_1(ShellViewHostMsg_GetShellId,
+                           int /* result */)
+    
+      // Post 'open' event.
+  static void EmitOpenEvent(const std::string& path);
+    
+    Base::Base(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+	   const std::string& extension_id)
+    : extension_id_(extension_id),
+      id_(id),
+      delay_destruction_(false),
+      pending_destruction_(false),
+      object_manager_(object_manager) {
+}
+    
+    v8::Handle<v8::Value> DeallocateObject(int routing_id,
+                                       int object_id) {
+  RenderThread::Get()->Send(new ShellViewHostMsg_Deallocate_Object(
+      routing_id, object_id));
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  return v8::Undefined(isolate);
+}
+    
+    // Call method of an object in browser and return the result.
+// function CallObjectMethod(id, type, method, args);
+v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
+                                           int object_id,
+                                           const std::string& type,
+                                           const std::string& method,
+                                           v8::Handle<v8::Value> args);
+    
+    
+    {}
+    
+    
+    {
+    {    if (zoom_controller) {
+      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
+      if (zoom_factor > content::kMaximumZoomFactor) {
+        zoom_factor = content::kMaximumZoomFactor;
+      }
+      if (zoom_factor < content::kMinimumZoomFactor) {
+        zoom_factor = content::kMinimumZoomFactor;
+      }
+      x *= zoom_factor;
+      y *= zoom_factor;
     }
     
-    Safe_TF_TensorPtr make_safe(TF_Tensor* tensor) {
-  return Safe_TF_TensorPtr(tensor);
+    Popup(x, y, rvh);
+  } else if (method == 'EnableShowEvent') {
+    arguments.GetBoolean(0, &enable_show_event_);
+  } else {
+    NOTREACHED() << 'Invalid call to Menu method:' << method
+                 << ' arguments:' << arguments;
+  }
 }
     
-    class RecordReader;
-    
-    			a_pMipmapImages[mip].paucEncodingBits = std::shared_ptr<unsigned char>(image.GetEncodingBits(), [](unsigned char *p) { delete[] p; });
-			a_pMipmapImages[mip].uiEncodingBitsBytes = image.GetEncodingBitsBytes();
-			a_pMipmapImages[mip].uiExtendedWidth = image.GetExtendedWidth();
-			a_pMipmapImages[mip].uiExtendedHeight = image.GetExtendedHeight();
-    
-    		for (unsigned int uiPixel = 0; uiPixel < PIXELS; uiPixel++)
-		{
-			m_afrgbaDecodedColors[uiPixel] = ColorFloatRGBA(-1.0f, -1.0f, -1.0f, -1.0f);
-			m_afDecodedAlphas[uiPixel] = -1.0f;
-		}
-    
-      The output buffer must be at least 5% larger than the input buffer
-  and can not be smaller than 66 bytes.
-    
-    /*Constants used by the entropy encoder/decoder.*/
-    
-    
-    {  SocketCanClientRaw socket_can_client;
-  EXPECT_TRUE(socket_can_client.Init(param));
-  EXPECT_EQ(socket_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
-  std::vector<CanFrame> frames;
-  int32_t num = 0;
-  EXPECT_EQ(socket_can_client.Send(frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  EXPECT_EQ(socket_can_client.Receive(&frames, &num),
-            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
-  CanFrame can_frame;
-  frames.push_back(can_frame);
-  EXPECT_EQ(socket_can_client.SendSingleFrame(frames),
-            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
-  socket_can_client.Stop();
-}
-    
-    void ClusterGeneralInfo701::Parse(const std::uint8_t* bytes, int32_t length,
-                                  ContiRadar* conti_radar) const {
-  auto obs = conti_radar->add_contiobs();
-  obs->set_clusterortrack(true);
-  obs->set_obstacle_id(obstacle_id(bytes, length));
-  obs->set_longitude_dist(longitude_dist(bytes, length));
-  obs->set_lateral_dist(lateral_dist(bytes, length));
-  obs->set_longitude_vel(longitude_vel(bytes, length));
-  obs->set_lateral_vel(lateral_vel(bytes, length));
-  obs->set_rcs(rcs(bytes, length));
-  obs->set_dynprop(dynprop(bytes, length));
-  double timestamp = apollo::common::time::Clock::NowInSeconds();
-  auto header = obs->mutable_header();
-  header->CopyFrom(conti_radar->header());
-  header->set_timestamp_sec(timestamp);
-}
-    
-    RcsThreshold RadarState201::rcs_threshold(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 7);
-  uint32_t x = t0.get_byte(2, 3);
+    bool MenuDelegate::GetIconForCommandId(int command_id,
+                                       gfx::Image* icon) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  if (item->icon_.IsEmpty())
+    return false;
     }
     
-    class SpeedLimitTest : public ::testing::Test {
+    class NwAppCrashBrowserFunction : public UIThreadExtensionFunction {
  public:
-  virtual void SetUp() {
-    speed_limit_.Clear();
-    for (int i = 0; i < 100; ++i) {
-      std::pair<double, double> sp;
-      sp.first = i * 1.0;
-      sp.second = (i % 2 == 0) ? 5.0 : 10.0;
-      speed_limit_.AppendSpeedLimit(sp.first, sp.second);
+  NwAppCrashBrowserFunction() {}
     }
+    
+    #include 'extensions/browser/extension_function.h'
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.create', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjCreateFunction);
+};
+    
+    int16_t word_blob_quality(WERD_RES *word, ROW *row);
+void reject_whole_page(PAGE_RES_IT &page_res_it);
+    
+    #include 'platform.h'  // for TESS_API
+    
+    void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
+  // Prepare all the blobs.
+  GenericVector<BlobData> blobs;
+  for (int w = 0; w < words.size(); ++w) {
+    if (words[w].word->ratings != nullptr &&
+        words[w].word->ratings->get(0, 0) == nullptr) {
+      for (int s = 0; s < words[w].lang_words.size(); ++s) {
+        Tesseract* sub = s < sub_langs_.size() ? sub_langs_[s] : this;
+        const WERD_RES& word = *words[w].lang_words[s];
+        for (int b = 0; b < word.chopped_word->NumBlobs(); ++b) {
+          blobs.push_back(BlobData(b, sub, word));
+        }
+      }
+    }
+  }
+  // Pre-classify all the blobs.
+  if (tessedit_parallelize > 1) {
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(10)
+#endif  // _OPENMP
+    for (int b = 0; b < blobs.size(); ++b) {
+      *blobs[b].choices =
+          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    }
+  } else {
+    // TODO(AMD) parallelize this.
+    for (int b = 0; b < blobs.size(); ++b) {
+      *blobs[b].choices =
+          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    }
+  }
+}
+    
+    
+    {  // Restore the pointer to original blamer bundle and combine blamer
+  // information recorded in the splits.
+  if (orig_bb != nullptr) {
+    orig_bb->JoinBlames(*word->blamer_bundle, *word2->blamer_bundle,
+                        wordrec_debug_blamer);
+    delete word->blamer_bundle;
+    word->blamer_bundle = orig_bb;
+  }
+  word->SetupBoxWord();
+  word->reject_map.initialise(word->box_word->length());
+  delete word2;
+}
+    
+    
+    {}  // namespace tesseract
+    
+     private:
+  // Simple struct to hold an ICOORD point and a halfwidth representing half
+  // the 'width' (supposedly approximately parallel to the direction of the
+  // line) of each point, such that distant points can be discarded when they
+  // overlap nearer points. (Think i dot and other diacritics or noise.)
+  struct PointWidth {
+    PointWidth() : pt(ICOORD(0, 0)), halfwidth(0) {}
+    PointWidth(const ICOORD& pt0, int halfwidth0)
+      : pt(pt0), halfwidth(halfwidth0) {}
+    }
+    
+    // Solve the dynamic programming problem for the given array of points, with
+// the given size and cost function.
+// Steps backwards are limited to being between min_step and max_step
+// inclusive.
+// The return value is the tail of the best path.
+DPPoint* DPPoint::Solve(int min_step, int max_step, bool debug,
+                        CostFunc cost_func, int size, DPPoint* points) {
+  if (size <= 0 || max_step < min_step || min_step >= size)
+    return nullptr;  // Degenerate, but not necessarily an error.
+  ASSERT_HOST(min_step > 0);  // Infinite loop possible if this is not true.
+  if (debug)
+    tprintf('min = %d, max=%d\n',
+            min_step, max_step);
+  // Evaluate the total cost at each point.
+  for (int i = 0; i < size; ++i) {
+    for (int offset = min_step; offset <= max_step; ++offset) {
+      DPPoint* prev = offset <= i ? points + i - offset : nullptr;
+      int64_t new_cost = (points[i].*cost_func)(prev);
+      if (points[i].best_prev_ != nullptr && offset > min_step * 2 &&
+          new_cost > points[i].total_cost_)
+        break;  // Find only the first minimum if going over twice the min.
+    }
+    points[i].total_cost_ += points[i].local_cost_;
+    if (debug) {
+      tprintf('At point %d, local cost=%d, total_cost=%d, steps=%d\n',
+              i, points[i].local_cost_, points[i].total_cost_,
+              points[i].total_steps_);
+    }
+  }
+  // Now find the end of the best path and return it.
+  int best_cost = points[size - 1].total_cost_;
+  int best_end = size - 1;
+  for (int end = best_end - 1; end >= size - min_step; --end) {
+    int cost = points[end].total_cost_;
+    if (cost < best_cost) {
+      best_cost = cost;
+      best_end = end;
+    }
+  }
+  return points + best_end;
+}
+    
+    	si.cb = sizeof(STARTUPINFO);
+#if USE_TASKBAR_API
+	si.lpTitle = appId;
+	si.dwFlags = STARTF_TITLEISAPPID;
+#endif
+	PROCESS_INFORMATION pi;
+	if (!CreateProcess(conEmuPath, args, NULL, NULL, false, 0, NULL, NULL, &si, &pi)) {
+		MessageBox(NULL, _T('Unable to create the ConEmu process!'), _T('Error'), MB_OK);
+		return;
+	}
+    
+    DHTReplaceNodeTask::DHTReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                                       const std::shared_ptr<DHTNode>& newNode)
+    : bucket_(bucket),
+      newNode_(newNode),
+      numRetry_(0),
+      timeout_(DHT_MESSAGE_TIMEOUT)
+{
+}
+    
+      virtual bool isReply() const CXX11_OVERRIDE;
+    
+        auto node = std::make_shared<DHTNode>(buf);
+    node->setIPAddress(peer.first);
+    node->setPort(peer.second);
+    // 4bytes reserved
+    readBytes(fp, buf, buf.size(), 4);
+    
+    void DHTRoutingTableSerializer::setNodes(
+    const std::vector<std::shared_ptr<DHTNode>>& nodes)
+{
+  nodes_ = nodes;
+}
+    
+    #include <vector>
+#include <string>
+#include <memory>
+    
+    #include 'common.h'
+    
+    namespace aria2 {
+    }
+    
+    std::shared_ptr<DHTTask>
+DHTTaskFactoryImpl::createPeerAnnounceTask(const unsigned char* infoHash)
+{
+  // TODO
+  return nullptr;
+}
+    
+      std::chrono::seconds timeout_;
+    
+    class DHTTaskQueue {
+public:
+  virtual ~DHTTaskQueue() = default;
+    }
+    
+    class DHTTaskQueueImpl : public DHTTaskQueue {
+private:
+  DHTTaskExecutor periodicTaskQueue1_;
+    }
+    
+    
+    {  void updateTokenSecret();
+};
+    
+    const std::string& DHTUnknownMessage::getMessageType() const { return UNKNOWN; }
+    
+    
+    {} // namespace aria2
+    
+    namespace aria2 {
+    }
+    
+    using apollo::common::ErrorCode;
+    
+    
+    {  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
+    if (conti_radar->contiobs(i).obstacle_id() == obj_id) {
+      auto obs = conti_radar->mutable_contiobs(i);
+      obs->set_longitude_dist_rms(
+          LINEAR_RMS[longitude_dist_rms(bytes, length)]);
+      obs->set_lateral_dist_rms(LINEAR_RMS[lateral_dist_rms(bytes, length)]);
+      obs->set_longitude_vel_rms(LINEAR_RMS[longitude_vel_rms(bytes, length)]);
+      obs->set_lateral_vel_rms(LINEAR_RMS[lateral_vel_rms(bytes, length)]);
+      obs->set_longitude_accel_rms(
+          LINEAR_RMS[longitude_accel_rms(bytes, length)]);
+      obs->set_lateral_accel_rms(LINEAR_RMS[lateral_accel_rms(bytes, length)]);
+      obs->set_oritation_angle_rms(
+          ANGLE_RMS[oritation_angle_rms(bytes, length)]);
+      obs->set_probexist(PROBOFEXIST[probexist(bytes, length)]);
+      obs->set_meas_state(meas_state(bytes, length));
+      break;
+    }
+  }
+}
+    
+    void SplineSegKernel::IntegratedTermMatrix(const uint32_t num_params,
+                                           const double x,
+                                           const std::string& type,
+                                           Eigen::MatrixXd* term_matrix) const {
+  if (term_matrix->rows() != term_matrix->cols() ||
+      term_matrix->rows() != static_cast<int>(num_params)) {
+    term_matrix->resize(num_params, num_params);
   }
     }
     
-      MatrixXd mat_golden(20, 10);
-  // clang-format off
-  mat_golden <<
-    -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-     1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-     1, -1,  0,  0,  0,  0,  0,  0,  0,  0,
-    -1,  1,  0,  0,  0,  0,  0,  0,  0,  0,
-     0,  1, -1,  0,  0,  0,  0,  0,  0,  0,
-     0, -1,  1,  0,  0,  0,  0,  0,  0,  0,
-     0,  0,  1, -1,  0,  0,  0,  0,  0,  0,
-     0,  0, -1,  1,  0,  0,  0,  0,  0,  0,
-     0,  0,  0,  1, -1,  0,  0,  0,  0,  0,
-     0,  0,  0, -1,  1,  0,  0,  0,  0,  0,
-     0,  0,  0,  0,  1, -1,  0,  0,  0,  0,
-     0,  0,  0,  0, -1,  1,  0,  0,  0,  0,
-     0,  0,  0,  0,  0,  1, -1,  0,  0,  0,
-     0,  0,  0,  0,  0, -1,  1,  0,  0,  0,
-     0,  0,  0,  0,  0,  0,  1, -1,  0,  0,
-     0,  0,  0,  0,  0,  0, -1,  1,  0,  0,
-     0,  0,  0,  0,  0,  0,  0,  1, -1,  0,
-     0,  0,  0,  0,  0,  0,  0, -1,  1,  0,
-     0,  0,  0,  0,  0,  0,  0,  0,  1, -1,
-     0,  0,  0,  0,  0,  0,  0,  0, -1,  1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
     
-    TEST_F(GemVehicleFactoryTest, InitMessageManager) {
-  EXPECT_TRUE(gem_factory_.CreateMessageManager() != nullptr);
+    {  double length_2 = 50.0;
+  const LaneGraph &lane_graph_2 =
+      ObstacleClusters::GetLaneGraph(start_s, length_2, lane);
+  EXPECT_EQ(1, lane_graph_2.lane_sequence_size());
+  EXPECT_EQ(3, lane_graph_2.lane_sequence(0).lane_segment_size());
+  EXPECT_EQ('l9', lane_graph_2.lane_sequence(0).lane_segment(0).lane_id());
+  EXPECT_EQ('l18', lane_graph_2.lane_sequence(0).lane_segment(1).lane_id());
+  EXPECT_EQ('l21', lane_graph_2.lane_sequence(0).lane_segment(2).lane_id());
+}
+    
+      Byte t1(bytes + 1);
+  int32_t t = t1.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
+    
+    // config detail: {'name': 'output_value', 'offset': 0.0, 'precision': 0.001,
+// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 39,
+// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
+double Brakerpt6c::output_value(const std::uint8_t* bytes,
+                                int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    #include <adapters/qt.h>
+    
+            yield(PRODUCER);
+    
+    public:
+    explicit LRUCache(size_t capacity)
+    {
+        cache_capacity = capacity;
+    }
+    
+    using namespace swoole;
+using namespace std;
+    
+        time_t start = time(nullptr);
+    while (i != 1000)
+    {
+        usleep(100);
+        
+        if ((time(nullptr) - start) > 3)
+        {
+            ASSERT_TRUE(false);
+        }
+    }
+    
+    int swAio_dispatch(const swAio_event *request)
+{
+    if (unlikely(!SwooleAIO.init))
+    {
+        swAio_init();
+    }
+    SwooleAIO.task_num++;
+    async_event *event = pool->dispatch(request);
+    return event->task_id;
+}
+    
+            for (int i = 0; i < 5; ++i)
+        {
+            std::string addr2 = Coroutine::gethostbyname('www.baidu.com', AF_INET);
+            ASSERT_NE(addr2, '');
+        }
+    
+    typedef struct
+{
+    uint32_t size;
+    uint32_t serial_num;
+    void* ptr;
+} pkg;
+    
+    #include <map>
+#include <unordered_map>
+    
+    
+    {    }
+    
+    void swoole_coroutine_signal_init()
+{
+    if (!signal_init)
+    {
+        signal_init = true;
+        swSignal_add(SIGCHLD, signal_handler);
+#ifdef HAVE_SIGNALFD
+        if (SwooleG.use_signalfd && !swReactor_handle_isset(SwooleG.main_reactor, SW_FD_SIGNAL))
+        {
+            swSignalfd_setup(SwooleG.main_reactor);
+        }
+#endif
+    }
 }
