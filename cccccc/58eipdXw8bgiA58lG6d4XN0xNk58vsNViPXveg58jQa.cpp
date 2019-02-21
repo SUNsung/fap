e43bcb3c9b880c3e5a16df29bbb8bdedde62632e
SUNsung <weatherius@gmail.com>
2019-02-21 06:38:35 +0200

    Rewrite [58eipdXw8bgiA58lG6d4XN0xNk58vsNViPXveg58jQa.cpp] File in: 2019-02-21 06:38:35

--- cccccc/58eipdXw8bgiA58lG6d4XN0xNk58vsNViPXveg58jQa.cpp
+++ cccccc/58eipdXw8bgiA58lG6d4XN0xNk58vsNViPXveg58jQa.cpp
@@ -1,234 +1,216 @@
-// Version constant.
-// This is either 0 for python, 1 for CPP V1, 2 for CPP V2.
-//
-// 0 is default and is equivalent to
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
-//
-// 1 is set with -DPYTHON_PROTO2_CPP_IMPL_V1 and is equivalent to
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
-// and
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=1
-//
-// 2 is set with -DPYTHON_PROTO2_CPP_IMPL_V2 and is equivalent to
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=cpp
-// and
-//   PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION_VERSION=2
-#ifdef PYTHON_PROTO2_CPP_IMPL_V1
-#error 'PYTHON_PROTO2_CPP_IMPL_V1 is no longer supported.'
-#else
-#ifdef PYTHON_PROTO2_CPP_IMPL_V2
-static int kImplVersion = 2;
-#else
-#ifdef PYTHON_PROTO2_PYTHON_IMPL
-static int kImplVersion = 0;
-#else
+
+        
+        namespace tensorflow {
+namespace swig {
+    }
+    }
     
-    bool AnyMetadata::InternalIs(const Descriptor* descriptor) const {
-  const string type_url = type_url_->GetNoArena();
-  string full_name;
-  if (!ParseAnyTypeUrl(type_url, &full_name)) {
-    return false;
-  }
-  return full_name == descriptor->full_name();
+    void GenerateLowerCaseOpName(const string& str, string* result);
+    
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/c/tf_status_helper.h'
+#include 'tensorflow/core/framework/tensor.h'
+#include 'tensorflow/python/lib/core/safe_ptr.h'
+    
+    // Mutex used to serialize accesses to cached vector of pointers to python
+// arrays to be dereferenced.
+static mutex* DelayedDecrefLock() {
+  static mutex* decref_lock = new mutex;
+  return decref_lock;
 }
     
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-namespace {
-    }
-    }
-    }
-    }
-    }
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/platform/logging.h'
     
-    MessageGenerator* ImmutableGeneratorFactory::NewMessageGenerator(
-    const Descriptor* descriptor) const {
-  if (HasDescriptorMethods(descriptor, context_->EnforceLite())) {
-    return new ImmutableMessageGenerator(descriptor, context_);
-  } else {
-    return new ImmutableMessageLiteGenerator(descriptor, context_);
-  }
-}
+      // Close the underlying file and release its resources.
+  void Close();
     
-    TEST_P(DynamicMessageTest, PackedFields) {
-  // Check that packed fields work properly.
-  Arena arena;
-  Message* message = packed_prototype_->New(GetParam()? &arena : NULL);
-  TestUtil::ReflectionTester reflection_tester(packed_descriptor_);
-    }
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    static inline int internalInflateInit2(
-    z_stream* zcontext, GzipInputStream::Format format) {
-  int windowBitsFormat = 0;
-  switch (format) {
-    case GzipInputStream::GZIP: windowBitsFormat = 16; break;
-    case GzipInputStream::AUTO: windowBitsFormat = 32; break;
-    case GzipInputStream::ZLIB: windowBitsFormat = 0; break;
-  }
-  return inflateInit2(zcontext, /* windowBits */15 | windowBitsFormat);
-}
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
+    #include 'base/logging.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/api_messages.h'
+#include 'content/public/renderer/render_view.h'
+#include 'content/public/renderer/render_thread.h'
+#include 'content/public/renderer/v8_value_converter.h'
+#include 'third_party/WebKit/public/web/WebView.h'
+#include 'third_party/WebKit/public/web/WebLocalFrame.h'
+#include 'ui/base/resource/resource_bundle.h'
     
-    {}  // namespace
-    
-    
-    
-    
-    {
-    {}}
-    
-          X_form_t x_formater {{
-                            rc,
-                            xop,
-                            static_cast<uint32_t>(rb),
-                            static_cast<uint32_t>(ra),
-                            static_cast<uint32_t>(rt),
-                            op
-                          }};
-    
-    
-    {  auto const makeShared = [&] () {
-    if (isVectorCollection(obj->collectionType())) {
-      return APCArray::MakeSharedVec(const_cast<ArrayData*>(array),
-                                     level,
-                                     unserializeObj);
-    }
-    return APCArray::MakeSharedDict(const_cast<ArrayData*>(array),
-                                    level,
-                                    unserializeObj);
-  };
-  return WrapArray(makeShared(), obj->collectionType());
-}
-    
-    
-    {private:
-  APCHandle m_handle;
-  APCHandle* m_arrayHandle;
-  CollectionType m_colType;
-};
-    
-    public:
-  virtual ~IDebuggable() {}
-    
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    #define ERROR_RAISE_WARNING(exp)        \
-  int ret = (exp);                      \
-  if (ret != 0) {                       \
-    raise_warning(                      \
-      '%s(): %s',                       \
-      __FUNCTION__,                     \
-      folly::errnoStr(errno).c_str()    \
-    );                                  \
-  }                                     \
-    
-    
-    {  // if the function was called via FCallBuiltin, we'll get a bogus name as
-  // the stack frame will be wrong
-  ActRec* ar = g_context->getStackFrame();
-  const char* fn = (ar != nullptr)
-    ? ar->func()->name()->data()
-    : 'OPTIMIZED_BUILTIN';
-  raise_warning('%s(%s): failed to open stream: '
-                'wrapper does not support stream open',
-                fn, filename.data());
-  return nullptr;
-}
-    
-    #ifndef HPHP_GLOB_STREAM_WRAPPER_H
-#define HPHP_GLOB_STREAM_WRAPPER_H
-    
-      bool valid() const { return !isClosed(); }
-    
-    #include 'hphp/util/perf-event.h'
-    
-    void logAHMSubMapWarning(folly::StringPiece mapName) {
-  StackTrace st;
-  logPerfWarning(
-    'AtomicHashMap overflow',
-    [&](StructuredLogEntry& cols) {
-      cols.setStr('map_name', mapName);
-      cols.setStackTrace('stack', st);
-    }
-  );
-}
-    
-    req::ptr<File> PhpStreamWrapper::openFD(const char *sFD) {
-  if (is_cli_mode()) {
-    raise_warning('Direct access to file descriptors is not '
-                  'available via remote unix server execution');
-    return nullptr;
-  }
-  if (RuntimeOption::ServerExecutionMode()) {
-    raise_warning('Direct access to file descriptors '
-                  'is only available from command-line');
-    return nullptr;
-  }
+    bool NwAppSetProxyConfigFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  net::ProxyConfigWithAnnotation config;
+  std::unique_ptr<nwapi::nw__app::SetProxyConfig::Params> params(
+      nwapi::nw__app::SetProxyConfig::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
     }
     
-      /**
-   * \brief Generate batch predictions for a given feature matrix. May use
-   * cached predictions if available instead of calculating from scratch.
-   *
-   * \param [in,out]  dmat        Feature matrix.
-   * \param [in,out]  out_preds   The output preds.
-   * \param           model       The model to predict from.
-   * \param           tree_begin  The tree begin index.
-   * \param           ntree_limit (Optional) The ntree limit. 0 means do not
-   * limit trees.
-   */
+    namespace caffe2 {
+    }
     
     
     { private:
-  RowBlock<IndexType> out_;
-  std::unique_ptr<Parser<IndexType> > parser_;
-  uint32_t num_col_;
-  std::vector<size_t> offset_;
-  std::vector<IndexType> dense_index_;
-  std::vector<xgboost::bst_float> dense_value_;
+  const int kNumTensorsPerInput = 3;
 };
     
     
-    {
-    {}  // namespace common
-}  // namespace xgboost
+<details>
     
-    SparsePageWriter::~SparsePageWriter() {
-  for (auto& queue : qworkers_) {
-    // use nullptr to signal termination.
-    std::shared_ptr<SparsePage> sig(nullptr);
-    queue.Push(std::move(sig));
-  }
-  for (auto& thread : workers_) {
-    thread->join();
-  }
+    
+    {  bool RunOnDevice() override;
+};
+    
+    workspace.ResetWorkspace()
+    
+    Missing features (represented by empty ranges) filled with default_value.
+    
+    OPERATOR_SCHEMA(GivenTensorDoubleFill)
+    .NumInputs(0, 1)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .Arg(
+        'values',
+        'The value for the elements of the output tensor.',
+        true /* required */)
+    .Arg(
+        'shape',
+        'The shape of the output tensor.'
+        'Cannot set the shape argument and pass in an input at the same time.')
+    .Arg(
+        'extra_shape',
+        'The additional dimensions appended at the end of the shape indicated'
+        'by the input blob.'
+        'Cannot set the extra_shape argument when there is no input blob.')
+    .Arg(
+        'input_as_shape',
+        '1D tensor containing the desired output shape. First input must be in CPU context.')
+    .TensorInferenceFunction(
+        FillerTensorInference<TensorProto_DataType_DOUBLE>);
+    
+    	FAIL_ON_ERROR(RegSetValue(command, L'', REG_SZ, commandStr, NULL));
+    
+    #if !UCONFIG_NO_TRANSLITERATION
+    
+    ScriptSet &ScriptSet::intersect(const ScriptSet &other) {
+    for (uint32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
+        bits[i] &= other.bits[i];
+    }
+    return *this;
 }
     
-      std::vector<size_t> feature_counts_;
-  std::vector<ColumnType> type_;
-  std::vector<uint32_t> index_;  // index_: may store smaller integers; needs padding
-  std::vector<size_t> row_ind_;
-  std::vector<ColumnBoundary> boundary_;
+    #include 'unicode/messagepattern.h'
+#include 'unicode/rbnf.h'
+#include 'unicode/selfmt.h'
+#include 'unicode/uchar.h'
+#include 'unicode/ucnv_err.h'
+#include 'unicode/umsg.h'
+#include 'unicode/ustring.h'
+#include 'unicode/utypes.h'
+#include 'cmemory.h'
+#include 'messageimpl.h'
+#include 'patternprops.h'
+#include 'selfmtimpl.h'
+#include 'uassert.h'
+#include 'ustrfmt.h'
+#include 'util.h'
+#include 'uvector.h'
     
-    int main()
+    #ifndef SMPDTFST_H
+#define SMPDTFST_H
+    
+    U_NAMESPACE_END
+    
+      static Data& getMutableData() { return data_; }
+    
+    const std::string& DHTResponseMessage::getType() const { return R; }
+    
+      virtual std::string toString() const CXX11_OVERRIDE;
+    
+        auto node = std::make_shared<DHTNode>(buf);
+    node->setIPAddress(peer.first);
+    node->setPort(peer.second);
+    // 4bytes reserved
+    readBytes(fp, buf, buf.size(), 4);
+    
+    #endif // D_DHT_ROUTING_TABLE_SERIALIZER_H
+
+    
+    #include 'LogFactory.h'
+#include 'Logger.h'
+#include 'util.h'
+#include 'DHTNode.h'
+#include 'DHTConnectionImpl.h'
+#include 'DHTRoutingTable.h'
+#include 'DHTMessageFactoryImpl.h'
+#include 'DHTMessageTracker.h'
+#include 'DHTMessageDispatcherImpl.h'
+#include 'DHTMessageReceiver.h'
+#include 'DHTTaskQueueImpl.h'
+#include 'DHTTaskFactoryImpl.h'
+#include 'DHTPeerAnnounceStorage.h'
+#include 'DHTTokenTracker.h'
+#include 'DHTInteractionCommand.h'
+#include 'DHTTokenUpdateCommand.h'
+#include 'DHTBucketRefreshCommand.h'
+#include 'DHTPeerAnnounceCommand.h'
+#include 'DHTEntryPointNameResolveCommand.h'
+#include 'DHTAutoSaveCommand.h'
+#include 'DHTTask.h'
+#include 'DHTRoutingTableDeserializer.h'
+#include 'DHTRegistry.h'
+#include 'DHTBucketRefreshTask.h'
+#include 'DHTMessageCallback.h'
+#include 'DHTMessageTrackerEntry.h'
+#include 'DHTMessageEntry.h'
+#include 'UDPTrackerClient.h'
+#include 'BtRegistry.h'
+#include 'prefs.h'
+#include 'Option.h'
+#include 'SocketCore.h'
+#include 'DlAbortEx.h'
+#include 'RecoverableException.h'
+#include 'a2functional.h'
+#include 'DownloadEngine.h'
+#include 'fmt.h'
+    
+    #include <algorithm>
+    
+    #include <vector>
+#include <deque>
+#include <memory>
+    
+    std::shared_ptr<DHTTask>
+DHTTaskFactoryImpl::createPingTask(const std::shared_ptr<DHTNode>& remoteNode,
+                                   int numRetry)
 {
-    // create JSON array
-    json array = {'first', '2nd', 'third', 'fourth'};
-    }
+  auto task = std::make_shared<DHTPingTask>(remoteNode, numRetry);
+  task->setTimeout(timeout_);
+  setCommonProperty(task);
+  return task;
+}
     
-        // out_of_range.403
-    try
-    {
-        // try to use a JSON pointer to an nonexistent object key
-        json::const_reference ref = j.at('/foo'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+    DHTTaskQueueImpl::~DHTTaskQueueImpl() = default;
     
-        // create a JSON number from number_integer_t
-    json::number_integer_t value_integer_t = -42;
-    json j_integer_t(value_integer_t);
+      // do nothing
+  virtual void doReceivedAction() CXX11_OVERRIDE;
+    
+    void DNSCache::markBad(const std::string& hostname, const std::string& ipaddr,
+                       uint16_t port)
+{
+  auto target = std::make_shared<CacheEntry>(hostname, port);
+  auto i = entries_.find(target);
+  if (i != entries_.end()) {
+    (*i)->markBad(ipaddr);
+  }
+}
