c2e8ae10709c80747048f005d25400d52864e4ad
SUNsung <weatherius@gmail.com>
2019-02-21 07:07:36 +0200

    Rewrite [42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb] File in: 2019-02-21 07:07:35

--- ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
+++ ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
@@ -1,149 +1,68 @@
 
         
-              # This method returns an HTML safe string similar to what <tt>Array#join</tt>
-      # would return. The array is flattened, and all items, including
-      # the supplied separator, are HTML escaped unless they are HTML
-      # safe, and the returned string is marked as HTML safe.
-      #
-      #   safe_join([raw('<p>foo</p>'), '<p>bar</p>'], '<br />')
-      #   # => '<p>foo</p>&lt;br /&gt;&lt;p&gt;bar&lt;/p&gt;'
-      #
-      #   safe_join([raw('<p>foo</p>'), raw('<p>bar</p>')], raw('<br />'))
-      #   # => '<p>foo</p><br /><p>bar</p>'
-      #
-      def safe_join(array, sep = $,)
-        sep = ERB::Util.unwrapped_html_escape(sep)
+                def preload_pipeline_warnings
+          # This preloads the number of warnings for every pipeline, ensuring
+          # that Ci::Pipeline#has_warnings? doesn't execute any additional
+          # queries.
+          @pipeline.number_of_warnings
+        end
     
-                # Prepend a hidden field to make sure something will be sent back to the
-            # server if all radio buttons are unchecked.
-            if options.fetch('include_hidden', true)
-              hidden_field + rendered_collection
-            else
-              rendered_collection
-            end
-          end
-    
-            class RadioButtonBuilder < Builder # :nodoc:
-          def radio_button(extra_html_options = {})
-            html_options = extra_html_options.merge(@input_html_options)
-            html_options[:skip_default_ids] = false
-            @template_object.radio_button(@object_name, @method_name, @value, html_options)
+            Redis::Cache.with do |redis|
+          redis.multi do |m|
+            m.sadd(key, value)
+            m.expire(key, timeout)
           end
         end
+      end
     
-          def instrument(name, **options) # :doc:
-        options[:identifier] ||= (@template && @template.identifier) || @path
+            collection = with_rate_limit { octokit.public_send(method, *args) }
+        next_url = octokit.last_response.rels[:next]
     
-          def perform(start_id, stop_id)
-        status_sql = Build
-          .where('ci_builds.commit_id = ci_stages.pipeline_id')
-          .where('ci_builds.stage = ci_stages.name')
-          .status_sql
-    
-            def representation_class
-          Representation::Issue
+            def execute
+          create_labels
         end
     
-            def id_for_already_imported_cache(note)
-          note.id
+            def line_code
+          diff_line = Gitlab::Diff::Parser.new.parse(diff_hunk.lines).to_a.last
+    
+        it 'works for running jobs' do
+      job.locked_at = Time.now
+      job.locked_by = 'test'
+      expect(status(job)).to eq('<span class='label label-info'>running</span>')
+    end
+    
+      describe '#style_colors' do
+    it 'returns a css style-formated version of the scenario foreground and background colors' do
+      expect(style_colors(scenario)).to eq('color:#AAAAAA;background-color:#000000')
+    end
+    
+        it 'respects an environment variable that specifies a path or URL to a different scenario' do
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
+    end
+    
+                expect(weather_agent_diff.name.current).to eq(agents(:bob_weather_agent).name)
+            expect(weather_agent_diff.name.incoming).to eq('a weather agent')
+            expect(weather_agent_diff.name.updated).to eq('a new name')
+            expect(weather_agent_diff.keep_events_for.current).to eq(45.days.to_i)
+            expect(weather_agent_diff.keep_events_for.incoming).to eq(5.days.to_i)
+            expect(weather_agent_diff.keep_events_for.updated).to eq(2.days.to_i.to_s)
+          end
         end
-      end
-    end
-  end
-end
-
     
-    module Gitlab
-  module GithubImport
-    # IssuableFinder can be used for caching and retrieving database IDs for
-    # issuable objects such as issues and pull requests. By caching these IDs we
-    # remove the need for running a lot of database queries when importing
-    # GitHub projects.
-    class IssuableFinder
-      attr_reader :project, :object
-    
-              new(hash)
-        end
-    
-      it 'renders the import form' do
-    visit new_scenario_imports_path
-    expect(page).to have_text('Import a Public Scenario')
-  end
-    
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
-    end
-    
-          it 'handles signals' do
-        @agent_runner.instance_variable_set(:@signal_queue, ['TERM'])
-        @agent_runner.run
-      end
-    end
-    
-      describe 'converting escaped JSONPath strings' do
-    it 'should work' do
-      expect(LiquidMigrator.convert_string('Weather looks like <$.conditions> according to the forecast at <$.pretty_date.time>')).to eq(
-                                    'Weather looks like {{conditions}} according to the forecast at {{pretty_date.time}}'
-      )
-    end
-    
-        @checker = Agents::BasecampAgent.new(:name => 'somename', :options => @valid_params)
-    @checker.service = services(:generic)
-    @checker.user = users(:jane)
-    @checker.save!
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
-    end
-  end
-end
-
-    
-    When /^I toggle nsfw posts$/ do
-  find('.toggle_nsfw_state', match: :first).click
-end
-    
-      def confirm_on_page(page_name)
-    if page_name == 'my profile page'
-      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
-    else
-      expect(page).to have_path(path_to(page_name))
-    end
-  end
-end
-    
-          it 'supports filtering by read/unread' do
-        FactoryGirl.create(:notification, :recipient => alice, :target => @post)
-        get :read_all
-        FactoryGirl.create(:notification, :recipient => alice, :target => @post)
-        get :index, params: {show: 'unread'}
-        expect(assigns[:notifications].count).to eq(1)
-      end
-    end
-    
-          # Returns the else branch of the `case` statement, if any.
-      #
-      # @return [Node] the else branch node of the `case` statement
-      # @return [nil] if the case statement does not have an else branch.
-      def else_branch
-        node_parts[-1]
+        context '#load_workers' do
+      before do
+        AgentRunner.class_variable_set(:@@agents, [HuginnScheduler, DelayedJobWorker])
       end
     
-          # Calls the given block for each `value` node in the `hash` literal.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_value
-        return pairs.map(&:value).to_enum unless block_given?
-    
-          # Calls the given block for each condition node in the `when` branch.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_condition
-        return conditions.to_enum(__method__) unless block_given?
+      describe 'up' do
+    it 'should update extract and template options for an existing WebsiteAgent' do
+      expect(agent.options).to include('extract' => old_extract,
+                                       'template' => old_template)
+      ConvertWebsiteAgentTemplateForMerge.new.up
+      agent.reload
+      expect(agent.options).to include('extract' => new_extract,
+                                       'template' => new_template)
+    end
+  end
