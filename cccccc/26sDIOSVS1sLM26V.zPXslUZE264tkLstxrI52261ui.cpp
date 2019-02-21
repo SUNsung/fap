9d9a083aa441ff3503a7a783778421f90fb2a136
SUNsung <weatherius@gmail.com>
2019-02-22 01:08:36 +0200

    Rewrite [26sDIOSVS1sLM26V.zPXslUZE264tkLstxrI52261ui.cpp] File in: 2019-02-22 01:08:35

--- cccccc/26sDIOSVS1sLM26V.zPXslUZE264tkLstxrI52261ui.cpp
+++ cccccc/26sDIOSVS1sLM26V.zPXslUZE264tkLstxrI52261ui.cpp
@@ -1,113 +1,353 @@
 
         
-        #include <google/protobuf/generated_message_util.h>
+        #include <string>
+#include <vector>
+#include 'tensorflow/core/framework/op_def.pb.h'
+#include 'tensorflow/core/framework/op_gen_lib.h'
+#include 'tensorflow/core/platform/types.h'
     
     
     {
-    {
-    {
-    {}  // namespace csharp
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
+    {    output() = 'A(m, 0) == A(m-1, 1)';
+  }
+};
     
-    // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
+    #ifndef TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
     
-      // -----------------------------------------------------------------
-  // Invoke internalBuildGeneratedFileFrom() to build the file.
-  printer->Print(
-    'com.google.protobuf.Descriptors.FileDescriptor\n'
-    '  .internalBuildGeneratedFileFrom(descriptorData,\n');
-  printer->Print(
-    '    new com.google.protobuf.Descriptors.FileDescriptor[] {\n');
+    namespace tensorflow {
     }
     
-      desired_output_for_decode = '_AbcdefghIJ';
-  expected = string('\xCA\x0', 2);
-  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
-                                                     desired_output_for_decode);
-  EXPECT_EQ(expected, result);
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    void OneofGenerator::GenerateClearFunctionDeclaration(io::Printer* printer) {
-  printer->Print(
-      variables_,
-      '/**\n'
-      ' * Clears whatever value was set for the oneof '$name$'.\n'
-      ' **/\n'
-      'void $owning_message_class$_Clear$capitalized_name$OneOfCase($owning_message_class$ *message);\n');
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Safe containers for an owned TFE_TensorHandle. On destruction, the handle
+// will be deleted by TFE_DeleteTensorHandle.
+using Safe_TFE_TensorHandlePtr =
+    std::unique_ptr<TFE_TensorHandle, detail::TFETensorHandleDeleter>;
+Safe_TFE_TensorHandlePtr make_safe(TFE_TensorHandle* handle);
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    #undef cv_hal_addWeighted8u
+#define cv_hal_addWeighted8u(src1, sz1, src2, sz2, dst, sz, w, h, scales) TEGRA_BINARYOPSCALE(CAROTENE_NS::u8, addWeighted, src1, sz1, src2, sz2, dst, sz, w, h, scales)
+#undef cv_hal_addWeighted8s
+#define cv_hal_addWeighted8s(src1, sz1, src2, sz2, dst, sz, w, h, scales) TEGRA_BINARYOPSCALE(CAROTENE_NS::s8, addWeighted, src1, sz1, src2, sz2, dst, sz, w, h, scales)
+#undef cv_hal_addWeighted16u
+#define cv_hal_addWeighted16u(src1, sz1, src2, sz2, dst, sz, w, h, scales) TEGRA_BINARYOPSCALE(CAROTENE_NS::u16, addWeighted, src1, sz1, src2, sz2, dst, sz, w, h, scales)
+#undef cv_hal_addWeighted16s
+#define cv_hal_addWeighted16s(src1, sz1, src2, sz2, dst, sz, w, h, scales) TEGRA_BINARYOPSCALE(CAROTENE_NS::s16, addWeighted, src1, sz1, src2, sz2, dst, sz, w, h, scales)
+#undef cv_hal_addWeighted32s
+#define cv_hal_addWeighted32s(src1, sz1, src2, sz2, dst, sz, w, h, scales) TEGRA_BINARYOPSCALE(CAROTENE_NS::s32, addWeighted, src1, sz1, src2, sz2, dst, sz, w, h, scales)
+//#undef cv_hal_addWeighted32f
+//#define cv_hal_addWeighted32f(src1, sz1, src2, sz2, dst, sz, w, h, scales) TEGRA_BINARYOPSCALE(CAROTENE_NS::f32, addWeighted, src1, sz1, src2, sz2, dst, sz, w, h, scales)
+//#undef cv_hal_addWeighted64f
+//#define cv_hal_addWeighted64f(src1, sz1, src2, sz2, dst, sz, w, h, scales) TEGRA_BINARYOPSCALE(CAROTENE_NS::f64, addWeighted, src1, sz1, src2, sz2, dst, sz, w, h, scales)
+    
+        for (size_t i = 0u; i < size.height; ++i)
+    {
+        const u8 * src = internal::getRowPtr(srcBase, srcStride, i);
+        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t sj = 0u, dj = 0u;
+    }
+    
+        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0, const typename internal::VecTraits<T>::vec64 & v_src1,
+              typename internal::VecTraits<T>::unsign::vec64 & v_dst) const
+    {
+        v_dst = internal::vceq(v_src0, v_src1);
+    }
+    
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
+CVT_FUNC(u16, u8, 16,
+,
+{
+     for (size_t i = 0; i < w; i += 16)
+     {
+         internal::prefetch(_src + i);
+         __asm__ (
+             'vld1.8 {d0-d1}, [%[src1]]                             \n\t'
+             'vqmovn.u16 d4, q0                                     \n\t'
+             'vld1.8 {d2-d3}, [%[src2]]                             \n\t'
+             'vqmovn.u16 d5, q1                                     \n\t'
+             'vst1.8 {d4-d5}, [%[dst]]                              \n\t'
+             : /*no output*/
+             : [src1] 'r' (_src + i),
+               [src2] 'r' (_src + i + 8),
+               [dst] 'r' (_dst + i + 0)
+             : 'd0','d1','d2','d3','d4','d5'
+         );
+     }
+})
+#else
+CVT_FUNC(u16, u8, 16,
+,
+{
+     for (size_t i = 0; i < w; i += 16)
+     {
+         internal::prefetch(_src + i);
+         uint16x8_t vline1_u16 = vld1q_u16(_src + i);
+         uint16x8_t vline2_u16 = vld1q_u16(_src + i + 8);
+    }
+    }
+    
+    
+    {        if (result < 0)
+        {
+            return 0x7fFFffFF;
+        }
+    }
+    return result;
+#else
+    (void)_size;
+    (void)srcBase;
+    (void)srcStride;
+    
+            for (; j < roiw_tail; j += step_tail, js += step_tail3, jd -= step_tail3)
+        {
+            vec64 v_src = vld3(src + js), v_dst;
+            v_dst.val[0] = vrev64(v_src.val[0]);
+            v_dst.val[1] = vrev64(v_src.val[1]);
+            v_dst.val[2] = vrev64(v_src.val[2]);
+    }
+    
+    
+    {                lane[colsn+k] = lane[idx_r1 + k];
+                lane[colsn+cn+k] = lane[idx_r2 + k];
+            }
+    
+    #include <arm_neon.h>
+    
+    
+    {
+    {                    tprev1 = vextq_s16(tnext1, tnext1, 6);
+                    tprev1 = vsetq_lane_s16(borderValue, tprev1, 0);
+                    tprev1 = vsetq_lane_s16(borderValue, tprev1, 1);
+                }
+                tppprev1 = tprev1;
+                continue;
+            }
+    
+    /**
+ * @brief Applies common transformations to the input data, such as
+ * scaling, mirroring, substracting the image mean...
+ */
+template <typename Dtype>
+class DataTransformer {
+ public:
+  explicit DataTransformer(const TransformationParameter& param, Phase phase);
+  virtual ~DataTransformer() {}
+    }
+    
+    /// @brief Fills a Blob with Gaussian-distributed values @f$ x = a @f$.
+template <typename Dtype>
+class GaussianFiller : public Filler<Dtype> {
+ public:
+  explicit GaussianFiller(const FillerParameter& param)
+      : Filler<Dtype>(param) {}
+  virtual void Fill(Blob<Dtype>* blob) {
+    Dtype* data = blob->mutable_cpu_data();
+    CHECK(blob->count());
+    caffe_rng_gaussian<Dtype>(blob->count(), Dtype(this->filler_param_.mean()),
+        Dtype(this->filler_param_.std()), blob->mutable_cpu_data());
+    int sparse = this->filler_param_.sparse();
+    CHECK_GE(sparse, -1);
+    if (sparse >= 0) {
+      // Sparse initialization is implemented for 'weight' blobs; i.e. matrices.
+      // These have num == channels == 1; width is number of inputs; height is
+      // number of outputs.  The 'sparse' variable specifies the mean number
+      // of non-zero input weights for a given output.
+      CHECK_GE(blob->num_axes(), 1);
+      const int num_outputs = blob->shape(0);
+      Dtype non_zero_probability = Dtype(sparse) / Dtype(num_outputs);
+      rand_vec_.reset(new SyncedMemory(blob->count() * sizeof(int)));
+      int* mask = reinterpret_cast<int*>(rand_vec_->mutable_cpu_data());
+      caffe_rng_bernoulli(blob->count(), non_zero_probability, mask);
+      for (int i = 0; i < blob->count(); ++i) {
+        data[i] *= mask[i];
+      }
+    }
+  }
+    }
+    
+      /* Should be tested when running loops to exit when requested. */
+  bool must_stop();
+    
+      /**
+   * Called by SetUp to initialize the weights associated with any top blobs in
+   * the loss function. Store non-zero loss weights in the diff blob.
+   */
+  inline void SetLossWeights(const vector<Blob<Dtype>*>& top) {
+    const int num_loss_weights = layer_param_.loss_weight_size();
+    if (num_loss_weights) {
+      CHECK_EQ(top.size(), num_loss_weights) << 'loss_weight must be '
+          'unspecified or specified once per top blob.';
+      for (int top_id = 0; top_id < top.size(); ++top_id) {
+        const Dtype loss_weight = layer_param_.loss_weight(top_id);
+        if (loss_weight == Dtype(0)) { continue; }
+        this->set_loss(top_id, loss_weight);
+        const int count = top[top_id]->count();
+        Dtype* loss_multiplier = top[top_id]->mutable_cpu_diff();
+        caffe_set(count, loss_weight, loss_multiplier);
+      }
+    }
+  }
+    
+    
+template <typename Dtype>
+class LayerRegisterer {
+ public:
+  LayerRegisterer(const string& type,
+                  shared_ptr<Layer<Dtype> > (*creator)(const LayerParameter&)) {
+    // LOG(INFO) << 'Registering layer type: ' << type;
+    LayerRegistry<Dtype>::AddCreator(type, creator);
+  }
+};
+    
+    #include 'caffe/layers/neuron_layer.hpp'
+    
+    #endif  // CAFFE_CONV_LAYER_HPP_
+
+    
+    #ifdef USE_CUDNN
+template <typename Dtype>
+class CuDNNLRNLayer : public LRNLayer<Dtype> {
+ public:
+  explicit CuDNNLRNLayer(const LayerParameter& param)
+      : LRNLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNLRNLayer();
+    }
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+        /*virtual*/ bool LearnerBase::Update(unordered_map<Parameter, NDArrayViewPtr>& gradientValues, size_t trainingSampleCount, bool sweepEnd) /*override*/
+    {
+        ReportTrainingParameterValue(m_learningRateSchedule, L'Learning rate');
+    }
+    
+        size_t NDMask::MaskedCount() const
+    {
+        auto maskMatrix = GetMatrix();
+        std::unique_ptr<char[]> maskData(maskMatrix->CopyToArray());
+        return std::count_if(maskData.get(), maskData.get() + maskMatrix->GetNumElements(), [](const char& val) {
+            return val == (char)MaskKind::Invalid;
+        });
+    }
+    
+            for (const auto& key : requiredKeys)
+        {
+            if (!dict.Contains(key))
+            {
+                 LogicError('Required key '%ls' is not found in the dictionary (%s).',
+                            key.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
+            }
+        }
+    
+            VariableFields(const NDShape& shape, VariableKind varType, ::CNTK::DataType type, const std::weak_ptr<Function>& ownerFunction, const NDArrayViewPtr& value, bool needsGradient, const std::vector<Axis>& dynamicAxes, bool isSparse, const std::wstring& name, const std::wstring& uid)
+            : m_shape(shape), m_varKind(varType), m_dataType(type), m_ownerFunction(ownerFunction), m_value(value), m_needsGradient(needsGradient), m_dynamicAxes(dynamicAxes), m_isSparse(isSparse), m_name(name), m_uid(uid), m_valueTimeStamp(0)
+        {
+            if (value && (type != value->GetDataType()))
+                InvalidArgument('The DataType of the Parameter/Constant Variable '%S' does not match the DataType of the associated Value', AsString().c_str());
+    }
+    
+    #pragma once
+    
+    #include <string>
+    
+    
+    {    Transliterator::_registerSpecialInverse(UNICODE_STRING_SIMPLE('Remove'),
+                                            UNICODE_STRING_SIMPLE('Null'), FALSE);
 }
     
-    
-Status DBImpl::InstallCompactionResults(CompactionState* compact) {
-  mutex_.AssertHeld();
-  Log(options_.info_log,  'Compacted %d@%d + %d@%d files => %lld bytes',
-      compact->compaction->num_input_files(0),
-      compact->compaction->level(),
-      compact->compaction->num_input_files(1),
-      compact->compaction->level() + 1,
-      static_cast<long long>(compact->total_bytes));
+    void
+RuleBasedCollator::writeSortKey(const UChar *s, int32_t length,
+                                SortKeyByteSink &sink, UErrorCode &errorCode) const {
+    if(U_FAILURE(errorCode)) { return; }
+    const UChar *limit = (length >= 0) ? s + length : NULL;
+    UBool numeric = settings->isNumeric();
+    CollationKeys::LevelCallback callback;
+    if(settings->dontCheckFCD()) {
+        UTF16CollationIterator iter(data, numeric, s, s, limit);
+        CollationKeys::writeSortKeyUpToQuaternary(iter, data->compressibleBytes, *settings,
+                                                  sink, Collation::PRIMARY_LEVEL,
+                                                  callback, TRUE, errorCode);
+    } else {
+        FCDUTF16CollationIterator iter(data, numeric, s, s, limit);
+        CollationKeys::writeSortKeyUpToQuaternary(iter, data->compressibleBytes, *settings,
+                                                  sink, Collation::PRIMARY_LEVEL,
+                                                  callback, TRUE, errorCode);
     }
-    
-    TEST(DBTest, IterSmallAndLargeMix) {
-  ASSERT_OK(Put('a', 'va'));
-  ASSERT_OK(Put('b', std::string(100000, 'b')));
-  ASSERT_OK(Put('c', 'vc'));
-  ASSERT_OK(Put('d', std::string(100000, 'd')));
-  ASSERT_OK(Put('e', std::string(100000, 'e')));
+    if(settings->getStrength() == UCOL_IDENTICAL) {
+        writeIdenticalLevel(s, limit, sink, errorCode);
     }
+    static const char terminator = 0;  // TERMINATOR_BYTE
+    sink.Append(&terminator, 1);
+}
     
-      // When start user key is prefix of limit user key
-  ASSERT_EQ(IKey('foo', 100, kTypeValue),
-            Shorten(IKey('foo', 100, kTypeValue),
-                    IKey('foobar', 200, kTypeValue)));
+    U_NAMESPACE_BEGIN
     
-    
-    {  // For fragments
-  kFirstType = 2,
-  kMiddleType = 3,
-  kLastType = 4
+    class U_I18N_API SharedCalendar : public SharedObject {
+public:
+    SharedCalendar(Calendar *calToAdopt) : ptr(calToAdopt) { }
+    virtual ~SharedCalendar();
+    const Calendar *get() const { return ptr; }
+    const Calendar *operator->() const { return ptr; }
+    const Calendar &operator*() const { return *ptr; }
+private:
+    Calendar *ptr;
+    SharedCalendar(const SharedCalendar &);
+    SharedCalendar &operator=(const SharedCalendar &);
 };
-static const int kMaxRecordType = kLastType;
     
-      // Fragment the record if necessary and emit it.  Note that if slice
-  // is empty, we still want to iterate once to emit a single
-  // zero-length record
-  Status s;
-  bool begin = true;
-  do {
-    const int leftover = kBlockSize - block_offset_;
-    assert(leftover >= 0);
-    if (leftover < kHeaderSize) {
-      // Switch to a new block
-      if (leftover > 0) {
-        // Fill the trailer (literal below relies on kHeaderSize being 7)
-        assert(kHeaderSize == 7);
-        dest_->Append(Slice('\x00\x00\x00\x00\x00\x00', leftover));
-      }
-      block_offset_ = 0;
+    class U_I18N_API SharedNumberFormat : public SharedObject {
+public:
+    SharedNumberFormat(NumberFormat *nfToAdopt) : ptr(nfToAdopt) { }
+    virtual ~SharedNumberFormat();
+    const NumberFormat *get() const { return ptr; }
+    const NumberFormat *operator->() const { return ptr; }
+    const NumberFormat &operator*() const { return *ptr; }
+private:
+    NumberFormat *ptr;
+    SharedNumberFormat(const SharedNumberFormat &);
+    SharedNumberFormat &operator=(const SharedNumberFormat &);
+};
+    
+        // Check for southern hemisphere.  We assume that the start and end
+    // month are different.
+    UBool southern = (startMonth > endMonth);
+    
+    // Not for public consumption; used by DateFormat
+SimpleDateFormat::SimpleDateFormat(EStyle timeStyle,
+                                   EStyle dateStyle,
+                                   const Locale& locale,
+                                   UErrorCode& status)
+:   fLocale(locale),
+    fSymbols(NULL),
+    fTimeZoneFormat(NULL),
+    fSharedNumberFormatters(NULL),
+    fCapitalizationBrkIter(NULL)
+{
+    initializeBooleanAttributes();
+    construct(timeStyle, dateStyle, fLocale, status);
+    if(U_SUCCESS(status)) {
+      initializeDefaultCentury();
     }
-    }
-    
-    #endif  // STORAGE_LEVELDB_DB_LOG_WRITER_H_
-
-    
-    namespace leveldb {
-    }
-    
-     public:
-  // Create a new SkipList object that will use 'cmp' for comparing keys,
-  // and will allocate memory using '*arena'.  Objects allocated in the arena
-  // must remain allocated for the lifetime of the skiplist object.
-  explicit SkipList(Comparator cmp, Arena* arena);
-    
-      SequenceNumber sequence_number() const { return sequence_number_; }
+}
