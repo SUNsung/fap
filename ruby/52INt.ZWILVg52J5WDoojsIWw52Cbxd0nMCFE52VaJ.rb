1df194244ebd044e5d5d475b256be49c3a5a255c
SUNsung <weatherius@gmail.com>
2019-02-22 02:56:23 +0200

    Rewrite [52INt.ZWILVg52J5WDoojsIWw52Cbxd0nMCFE52VaJ.rb] File in: 2019-02-22 02:56:23

--- ruby/52INt.ZWILVg52J5WDoojsIWw52Cbxd0nMCFE52VaJ.rb
+++ ruby/52INt.ZWILVg52J5WDoojsIWw52Cbxd0nMCFE52VaJ.rb
@@ -1,76 +1,75 @@
 
         
-            def_delegators :@s, :scan_until, :skip_until, :string
-    
-        while readable_io = IO.select([self_read])
-      signal = readable_io.first[0].gets.strip
-      handle_signal(launcher, signal)
-    end
-  #end
-rescue SystemExit => e
-  #Sidekiq.logger.error('Profiling...')
-  #result = RubyProf.stop
-  #printer = RubyProf::GraphHtmlPrinter.new(result)
-  #printer.print(File.new('output.html', 'w'), :min_percent => 1)
-  # normal
-rescue => e
-  raise e if $DEBUG
-  STDERR.puts e.message
-  STDERR.puts e.backtrace.join('\n')
-  exit 1
-end
-
-    
-        execute 'ensure-sidekiq-is-setup-with-monit' do 
-      command %Q{ 
-        monit reload 
-      } 
-    end
-    
-    get '/' do
-  stats = Sidekiq::Stats.new
-  @failed = stats.failed
-  @processed = stats.processed
-  @messages = $redis.lrange('sinkiq-example-messages', 0, -1)
-  erb :index
-end
-    
-          arr = Sidekiq.options[:lifecycle_events][event]
-      arr.reverse! if reverse
-      arr.each do |block|
-        begin
-          block.call
-        rescue => ex
-          handle_exception(ex, { context: 'Exception during Sidekiq lifecycle event.', event: event })
-          raise ex if reraise
-        end
-      end
-      arr.clear
-    end
-  end
-end
-
-    
-        def erb(content, options = {})
-      if content.kind_of? Symbol
-        unless respond_to?(:'_erb_#{content}')
-          src = ERB.new(File.read('#{Web.settings.views}/#{content}.erb')).src
-          WebAction.class_eval('def _erb_#{content}\n#{src}\n end')
-        end
-      end
-    
-        args += ['--bindir', bin_path]
-    ::FileUtils.mkdir_p(bin_path)
-    args << gem_path
-    safesystem(*args)
-    
-      def unpack_data_to
-    'files'
+        describe Admin::UsersController do
+  it 'requires to be signed in as an admin' do
+    login_as(users(:bob))
+    visit admin_users_path
+    expect(page).to have_text('Admin access required to view that page.')
   end
     
-          if !attributes[:python_setup_py_arguments].nil? and !attributes[:python_setup_py_arguments].empty?
-        # Add optional setup.py arguments
-        attributes[:python_setup_py_arguments].each do |a|
-          flags += [ a ]
+              @bar2 = Agents::DotBar.new(name: 'bar2').tap { |agent|
+            agent.user = users(:bob)
+            agent.sources << @foo
+            agent.propagate_immediately = true
+            agent.disabled = true
+            agent.save!
+          },
+    
+        it 'respects an environment variable that specifies a path or URL to a different scenario' do
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
+    end
+    
+          @agent2.delete
+    
+        it 'should convert the 'escape' method correctly' do
+      expect(LiquidMigrator.convert_string('Escaped: <escape $.content.name>\nNot escaped: <$.content.name>')).to eq(
+                                    'Escaped: {{content.name | uri_escape}}\nNot escaped: {{content.name}}'
+      )
+    end
+    
+          expect(Utils.unindent('Hello\n  I am indented')).to eq('Hello\n  I am indented')
+    
+        it 'should require the basecamp project_id' do
+      @checker.options['project_id'] = nil
+      expect(@checker).not_to be_valid
+    end
+    
+        it { is_expected.to have_valid_bash_syntax }
+  end
+    
+          rtn = ''
+      (context.environments.first['site'][@array_name] || []).each do |file|
+        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
+          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
         end
-      end
+    
+    Liquid::Template.register_tag('include_code', Jekyll::IncludeCodeTag)
+
+    
+        attr_writer :sessions
+    
+          help = '(#{type} only) #{help}'
+      @options << [flag, param, help, options, block]
+    end # def options
+    
+        # data tar.
+    tar_path(staging_path(''), datatar_path)
+    
+        safesystem(*install_args)
+    
+        # do channel-update if requested
+    if attributes[:pear_channel_update?]
+      channel = attributes[:pear_channel] || 'pear'
+      logger.info('Updating the channel', :channel => channel)
+      safesystem('pear', '-c', config, 'channel-update', channel)
+    end
+    
+    require 'pleaserun/cli'
+    
+        if File.exists?(params[:output])
+      # TODO(sissel): Allow folks to choose output?
+      logger.error('Puppet module directory '#{params[:output]}' already ' \
+                    'exists. Delete it or choose another output (-p flag)')
+    end
