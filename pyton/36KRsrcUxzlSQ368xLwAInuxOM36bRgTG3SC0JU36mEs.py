d48036d4e8aae2dcac705514c9d9b4dff9fa9050
SUNsung <weatherius@gmail.com>
2019-02-22 05:49:08 +0200

    Rewrite [36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py] File in: 2019-02-22 05:49:07

--- pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
+++ pyton/36KRsrcUxzlSQ368xLwAInuxOM36bRgTG3SC0JU36mEs.py
@@ -1,32 +1,68 @@
 
         
-        import optparse
-import os
-from os.path import dirname as dirn
-import sys
+        
+class CaseInsensitiveDict(MutableMapping):
+    '''A case-insensitive ``dict``-like object.
     
-    secret_msg = b'Secret message goes here'
+        def copy(self):
+        '''Return a copy of this RequestsCookieJar.'''
+        new_cj = RequestsCookieJar()
+        new_cj.set_policy(self.get_policy())
+        new_cj.update(self)
+        return new_cj
     
-    if __name__ == '__main__':
-    main()
+        proxy_keys = [
+        urlparts.scheme + '://' + urlparts.hostname,
+        urlparts.scheme,
+        'all://' + urlparts.hostname,
+        'all',
+    ]
+    proxy = None
+    for proxy_key in proxy_keys:
+        if proxy_key in proxies:
+            proxy = proxies[proxy_key]
+            break
+    
+            # Note that prepare_auth must be last to enable authentication schemes
+        # such as OAuth to work on a fully prepared request.
+    
+    
+def rev_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
+    ref = 'http://hg.scrapy.org/scrapy/changeset/' + text
+    set_classes(options)
+    node = nodes.reference(rawtext, 'r' + text, refuri=ref, **options)
+    return [node], []
 
     
+    # Declare top-level shortcuts
+from scrapy.spiders import Spider
+from scrapy.http import Request, FormRequest
+from scrapy.selector import Selector
+from scrapy.item import Item, Field
     
-def get_base_name(base):
-    if base is InfoExtractor:
-        return 'LazyLoadExtractor'
-    elif base is SearchInfoExtractor:
-        return 'LazyLoadSearchExtractor'
-    else:
-        return base.__name__
     
-        ies = sorted(youtube_dl.gen_extractors(), key=lambda i: i.IE_NAME.lower())
-    out = '# Supported sites\n' + ''.join(
-        ' - ' + md + '\n'
-        for md in gen_ies_md(ies))
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
     
-    # The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'default'
+        def _err(self, msg):
+        sys.stderr.write(msg + os.linesep)
+        self.exitcode = 1
     
-    rootDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
+class Command(ScrapyCommand):
+    
+        def post_process(self, output):
+        for x in output:
+            if isinstance(x, (BaseItem, dict)):
+                for arg in self.args:
+                    if not arg in x:
+                        raise ContractFail(''%s' field is missing' % arg)
+
+    
+        #Unless specified otherwise by the user, this is the default colorscheme
+    colorscheme = 'basic'
