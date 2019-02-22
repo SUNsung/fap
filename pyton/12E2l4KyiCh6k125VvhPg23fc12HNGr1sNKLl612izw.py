9f09b46a99662b371b8bafef246929bcc65c3bf7
SUNsung <weatherius@gmail.com>
2019-02-23 00:43:57 +0200

    Rewrite [12E2l4KyiCh6k125VvhPg23fc12HNGr1sNKLl612izw.py] File in: 2019-02-23 00:43:57

--- pyton/12E2l4KyiCh6k125VvhPg23fc12HNGr1sNKLl612izw.py
+++ pyton/12E2l4KyiCh6k125VvhPg23fc12HNGr1sNKLl612izw.py
@@ -1,51 +1,104 @@
 
         
-            def _remote_state(self):
-        '''Returns the remote state from AWS'''
-        description = self.rule.describe()
-        if not description:
-            return
-        return description['state']
-    
-        for app in module.params['apps']:
-        if app not in client.apps():
-            module.fail_json(msg='App {0} does not exist'.format(app))
+        from .cifar import load_batch
+from ..utils.data_utils import get_file
+from .. import backend as K
+import numpy as np
+import os
     
     
-def main():
-    HostModule().run_module()
-    
-        gateway = vca.get_gateway(vdc_name, gateway_name)
-    if not gateway:
-        module.fail_json(msg='Not able to find the gateway %s, please check '
-                             'the gateway_name param' % gateway_name)
-    
-        if not pyodbc_found:
-        module.fail_json(msg='The python pyodbc module is required.')
-    
-        module = AnsibleModule(
-        argument_spec=dict(
-            role=dict(required=True, aliases=['name']),
-            assigned_roles=dict(default=None, aliases=['assigned_role']),
-            state=dict(default='present', choices=['absent', 'present']),
-            db=dict(default=None),
-            cluster=dict(default='localhost'),
-            port=dict(default='5433'),
-            login_user=dict(default='dbadmin'),
-            login_password=dict(default=None, no_log=True),
-        ), supports_check_mode=True)
+def test_boston_housing():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
     
     
-def main():
-    argument_spec = ipa_argument_spec()
-    argument_spec.update(zone_name=dict(type='str', required=True),
-                         state=dict(type='str', default='present', choices=['present', 'absent']),
-                         )
+def test_regularization_shared_layer_in_different_models():
+    shared_dense = Dense(num_classes,
+                         kernel_regularizer=regularizers.l1(),
+                         activity_regularizer=regularizers.l1())
+    models = []
+    for _ in range(2):
+        input_tensor = Input(shape=(data_dim,))
+        unshared_dense = Dense(num_classes, kernel_regularizer=regularizers.l1())
+        out = unshared_dense(shared_dense(input_tensor))
+        models.append(Model(input_tensor, out))
     
+    # input image dimensions
+img_rows, img_cols = 28, 28
     
-def unfollow_log(module, le_path, logs):
-    ''' Unfollows one or more logs if followed. '''
+        n_features = 10
+    list_n_samples = np.linspace(100, 1000000, 5).astype(np.int)
+    lasso_results, lars_lasso_results = compute_bench(alpha, list_n_samples,
+                                            [n_features], precompute=True)
     
+        # TASK: print the mean and std for each candidate along with the parameter
+    # settings for all the candidates explored by grid search.
+    n_candidates = len(grid_search.cv_results_['params'])
+    for i in range(n_candidates):
+        print(i, 'params - %s; mean - %0.2f; std - %0.2f'
+                 % (grid_search.cv_results_['params'][i],
+                    grid_search.cv_results_['mean_test_score'][i],
+                    grid_search.cv_results_['std_test_score'][i]))
     
-def naughty_strings(filepath=FILEPATH):
-    '''Get the list of naughty_strings.
+        for line in input_file:
+        linestrip = line.strip()
+        if len(linestrip) == 0:
+            in_exercise_region = False
+        elif linestrip.startswith('# TASK:'):
+            in_exercise_region = True
+    
+    # Author: Kemal Eren <kemal@kemaleren.com>
+# License: BSD 3 clause
+    
+    for center_1, center_2 in zip(regular_values[:-1], regular_values[1:]):
+    plt.axvline(.5 * (center_1 + center_2), color='b', linestyle='--')
+    
+        if (has_actual_code and not has_question_marks):
+        linebuffer = clean_trailing_newlines(linebuffer)
+        write_with_harness(codefile, sourcefile, start_linenum, linebuffer)
+    return (line, linenum)
+    
+    for face_landmarks in face_landmarks_list:
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image, 'RGBA')
+    
+    # See how far apart the test image is from the known faces
+face_distances = face_recognition.face_distance(known_encodings, image_to_test_encoding)
+    
+        # Let's trace out each facial feature in the image with a line!
+    for facial_feature in face_landmarks.keys():
+        d.line(face_landmarks[facial_feature], width=5)
+    
+    # Create a random animal
+def random_animal():
+    '''Let's be dynamic!'''
+    return random.choice([Dog, Cat])()
+    
+        def __init__(self):
+        self.time_provider = datetime.datetime
+    
+    *TL;DR80
+Decouples an abstraction from its implementation.
+'''
+    
+        def test_publisher_shall_append_subscription_message_to_queue(cls):
+        ''' msg_queue ~ Provider.notify(msg) ~ Publisher.publish(msg) '''
+        expected_msg = 'expected msg'
+        pro = Provider()
+        pub = Publisher(pro)
+        Subscriber('sub name', pro)
+        cls.assertEqual(len(pro.msg_queue), 0)
+        pub.publish(expected_msg)
+        cls.assertEqual(len(pro.msg_queue), 1)
+        cls.assertEqual(pro.msg_queue[0], expected_msg)
+    
+        def test_shall_toggle_from_fm_to_am(self):
+        self.radio.toggle_amfm()
+        state = self.radio.state.name
+        expected_state_name = 'AM'
+        self.assertEqual(state, expected_state_name)
