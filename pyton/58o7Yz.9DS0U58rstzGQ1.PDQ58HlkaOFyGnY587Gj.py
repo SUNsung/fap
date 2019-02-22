04d172c0b2eff5e26c17d778c1de61cdac9c0c83
SUNsung <weatherius@gmail.com>
2019-02-22 11:53:08 +0200

    Rewrite [58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py] File in: 2019-02-22 11:53:07

--- pyton/58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py
+++ pyton/58o7Yz.9DS0U58rstzGQ1.PDQ58HlkaOFyGnY587Gj.py
@@ -1,78 +1,197 @@
 
         
-                account_number = match1(html, r'data-account='(\d+)'')
+                new_content = sock.recv(chunks)
+        if not new_content:
+            break
+    
+        if not isinstance(password, basestring):
+        warnings.warn(
+            'Non-string passwords will no longer be supported in Requests '
+            '3.0.0. Please convert the object you've passed in ({!r}) to '
+            'a string or bytes object in the near future to avoid '
+            'problems.'.format(password),
+            category=DeprecationWarning,
+        )
+        password = str(password)
+    # -- End Removal --
+    
+    elif is_py3:
+    from urllib.parse import urlparse, urlunparse, urljoin, urlsplit, urlencode, quote, unquote, quote_plus, unquote_plus, urldefrag
+    from urllib.request import parse_http_list, getproxies, proxy_bypass, proxy_bypass_environment, getproxies_environment
+    from http import cookiejar as cookielib
+    from http.cookies import Morsel
+    from io import StringIO
+    from collections import OrderedDict
+    from collections.abc import Callable, Mapping, MutableMapping
     
     
-def download_url(url, merge, output_dir, title, info_only):
-    mime, ext, size = url_info(url)
-    print_info(site_info, title, mime, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge)
-    
-    	html = get_html(url)
-	contentid = r1(r'<meta name='contentid' scheme='DMINSTR2' content='([^']+)' />', html)
-	vid = r1(r''demand_ehow_videoid':'([^']+)'', html)
-	assert vid
-    
-    # image size suffixes used in inline json 'key' field
-# listed in descending order
-size_suffixes = ['o', 'k', 'h', 'l', 'c', 'z', 'm', 'n', 's', 't', 'q', 'sq']
-    
-            basename = os.path.splitext(os.path.basename(im_name))[0]
-        txtname = os.path.join(output_dir, basename + 'pred.txt')
-        with open(txtname, 'w') as fid_txt:
-            if i % 10 == 0:
-                logger.info('i: {}: {}'.format(i, basename))
-            for j in range(1, len(all_segms)):
-                clss = json_dataset.classes[j]
-                clss_id = cityscapes_eval.name2label[clss].id
-                segms = all_segms[j][i]
-                boxes = all_boxes[j][i]
-                if segms == []:
-                    continue
-                masks = mask_util.decode(segms)
-    
-        def AddLosses(self, losses):
-        if not isinstance(losses, list):
-            losses = [losses]
-        # Conversion to str allows losses to include BlobReferences
-        losses = [c2_utils.UnscopeName(str(l)) for l in losses]
-        self.losses = list(set(self.losses + losses))
+def dispatch_hook(key, hooks, hook_data, **kwargs):
+    '''Dispatches a hook dictionary on a given piece of data.'''
+    hooks = hooks or {}
+    hooks = hooks.get(key)
+    if hooks:
+        if hasattr(hooks, '__call__'):
+            hooks = [hooks]
+        for hook in hooks:
+            _hook_data = hook(hook_data, **kwargs)
+            if _hook_data is not None:
+                hook_data = _hook_data
+    return hook_data
+
     
     
-def add_roi_Xconv1fc_gn_head(model, blob_in, dim_in, spatial_scale):
-    '''Add a X conv + 1fc head, with GroupNorm'''
-    hidden_dim = cfg.FAST_RCNN.CONV_HEAD_DIM
-    roi_size = cfg.FAST_RCNN.ROI_XFORM_RESOLUTION
-    roi_feat = model.RoIFeatureTransform(
-        blob_in, 'roi_feat',
-        blob_rois='rois',
-        method=cfg.FAST_RCNN.ROI_XFORM_METHOD,
-        resolution=roi_size,
-        sampling_ratio=cfg.FAST_RCNN.ROI_XFORM_SAMPLING_RATIO,
-        spatial_scale=spatial_scale
-    )
+@pytest.mark.parametrize(
+    'value, expected', (
+        (
+            CaseInsensitiveDict(),
+            None
+        ),
+        (
+            CaseInsensitiveDict({'content-type': 'application/json; charset=utf-8'}),
+            'utf-8'
+        ),
+        (
+            CaseInsensitiveDict({'content-type': 'text/plain'}),
+            'ISO-8859-1'
+        ),
+    ))
+def test_get_encoding_from_headers(value, expected):
+    assert get_encoding_from_headers(value) == expected
     
-        By default, this function will generate a data parallel model configured to
-    run on cfg.NUM_GPUS devices. However, you can restrict it to build a model
-    targeted to a specific GPU by specifying gpu_id. This is used by
-    optimizer.build_data_parallel_model() during test time.
+    
+    {    # Server Error.
+    500: ('internal_server_error', 'server_error', '/o\\', '✗'),
+    501: ('not_implemented',),
+    502: ('bad_gateway',),
+    503: ('service_unavailable', 'unavailable'),
+    504: ('gateway_timeout',),
+    505: ('http_version_not_supported', 'http_version'),
+    506: ('variant_also_negotiates',),
+    507: ('insufficient_storage',),
+    509: ('bandwidth_limit_exceeded', 'bandwidth'),
+    510: ('not_extended',),
+    511: ('network_authentication_required', 'network_auth', 'network_authentication'),
+}
+    
+    
+def rewind_body(prepared_request):
+    '''Move file pointer back to its recorded starting position
+    so it can be read again on redirect.
     '''
-    model = DetectionModelHelper(
-        name=model_type_func,
-        train=train,
-        num_classes=cfg.MODEL.NUM_CLASSES,
-        init_params=train
-    )
-    model.only_build_forward_pass = False
-    model.target_gpu_id = gpu_id
-    return get_func(model_type_func)(model)
+    body_seek = getattr(prepared_request.body, 'seek', None)
+    if body_seek is not None and isinstance(prepared_request._body_position, integer_types):
+        try:
+            body_seek(prepared_request._body_position)
+        except (IOError, OSError):
+            raise UnrewindableBodyError('An error occurred when rewinding request '
+                                        'body for redirect.')
+    else:
+        raise UnrewindableBodyError('Unable to rewind request body for redirect.')
+
     
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+    alpha = 0.1
+# alpha = 0.01
     
-    from caffe2.python import muji
+    if __name__ == '__main__':
+    list_n_samples = np.linspace(100, 10000, 5).astype(np.int)
+    list_n_features = [10, 100, 1000]
+    n_test = 1000
+    max_iter = 1000
+    noise = 0.1
+    alpha = 0.01
+    sgd_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
+    elnet_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
+    ridge_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
+    asgd_results = np.zeros((len(list_n_samples), len(list_n_features), 2))
+    for i, n_train in enumerate(list_n_samples):
+        for j, n_features in enumerate(list_n_features):
+            X, y, coef = make_regression(
+                n_samples=n_train + n_test, n_features=n_features,
+                noise=noise, coef=True)
     
-    logger = logging.getLogger(__name__)
+        fn = os.path.relpath(fn,
+                         start=os.path.dirname(__import__(package).__file__))
+    try:
+        lineno = inspect.getsourcelines(obj)[1]
+    except Exception:
+        lineno = ''
+    return url_fmt.format(revision=revision, package=package,
+                          path=fn, lineno=lineno)
+    
+    # Learn a frontier for outlier detection with several classifiers
+xx1, yy1 = np.meshgrid(np.linspace(-8, 28, 500), np.linspace(3, 40, 500))
+xx2, yy2 = np.meshgrid(np.linspace(3, 10, 500), np.linspace(-5, 45, 500))
+for i, (clf_name, clf) in enumerate(classifiers.items()):
+    plt.figure(1)
+    clf.fit(X1)
+    Z1 = clf.decision_function(np.c_[xx1.ravel(), yy1.ravel()])
+    Z1 = Z1.reshape(xx1.shape)
+    legend1[clf_name] = plt.contour(
+        xx1, yy1, Z1, levels=[0], linewidths=2, colors=colors[i])
+    plt.figure(2)
+    clf.fit(X2)
+    Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel()])
+    Z2 = Z2.reshape(xx2.shape)
+    legend2[clf_name] = plt.contour(
+        xx2, yy2, Z2, levels=[0], linewidths=2, colors=colors[i])
+    
+    '''
+print(__doc__)
+    
+    '''
+print(__doc__)
+    
+    X = list()
+y = list()
+for i, (phi, a) in enumerate([(.5, .15), (.5, .6), (.3, .2)]):
+    for _ in range(30):
+        phase_noise = .01 * np.random.normal()
+        amplitude_noise = .04 * np.random.normal()
+        additional_noise = 1 - 2 * np.random.rand(n_features)
+        # Make the noise sparse
+        additional_noise[np.abs(additional_noise) < .997] = 0
+    
+    plt.subplot(224)
+plt.scatter(X_filtered[:, 0], X_filtered[:, 1], c=y_pred)
+plt.title('Unevenly Sized Blobs')
+    
+        def report_not_exist(self, appid, ip):
+        self.logger.debug('report_not_exist:%s %s', appid, ip)
+        th = threading.Thread(target=self.process_appid_not_exist, args=(appid, ip))
+        th.start()
+    
+            import_command = 'security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ../../../../data/gae_proxy/CA.crt'# % certfile.decode('utf-8')
+        if exist_ca_sha1:
+            delete_ca_command = 'security delete-certificate -Z %s' % exist_ca_sha1
+            exec_command = '%s;%s' % (delete_ca_command, import_command)
+        else:
+            exec_command = import_command
+    
+    
+def best_server(probe_nat=False):
+    best_server = None
+    prober = new_pteredor(probe_nat=probe_nat)
+    prober.qualified = True
+    if not probe_nat:
+        prober.nat_type = 'unknown'
+        prober.rs_cone_flag = 0
+    
+            Using setter/getter methods is deprecated. Use o.channel instead.'''
+    
+    def generateKey(keySize):
+    print('Generating prime p...')
+    p = rabinMiller.generateLargePrime(keySize)
+    print('Generating prime q...')
+    q = rabinMiller.generateLargePrime(keySize)
+    n = p * q
+    
+    from .hash_table import HashTable
+from number_theory.prime_numbers import next_prime, check_prime
+    
+            while self.values[new_key] is not None \
+                and self.values[new_key] != key:
+            i += 1
+            new_key = self.hash_function(key + i*i) if not \
+                self.balanced_factor() >= self.lim_charge else None
+    
+    This is a pure Python implementation of Dynamic Programming solution to the longest increasing subsequence of a given sequence.
