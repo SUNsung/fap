1fa43ce79e76cd6e5525ebd4fc1cae9ddab1a1fc
SUNsung <weatherius@gmail.com>
2019-02-23 05:28:55 +0200

    Rewrite [59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp] File in: 2019-02-23 05:28:54

--- cccccc/59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp
+++ cccccc/59VY7al3tuVdQ59QSs1DfgtPzs59LVNVyu6mIeM59TsA.cpp
@@ -1,297 +1,191 @@
 
         
-          // If the requirement itself is non-generic, the synthetic signature
-  // is that of the conformance context.
-  if (!covariantSelf &&
-      reqSig->getGenericParams().size() == 1 &&
-      reqSig->getRequirements().size() == 1) {
-    syntheticSignature = conformanceDC->getGenericSignatureOfContext();
-    if (syntheticSignature) {
-      syntheticSignature = syntheticSignature->getCanonicalSignature();
-      syntheticEnvironment =
-        syntheticSignature->createGenericEnvironment();
-    }
+        #include 'atom/app/uv_task_runner.h'
+    
+    #include 'atom/browser/api/event_emitter.h'
+#include 'atom/browser/auto_updater.h'
+#include 'atom/browser/window_list_observer.h'
+#include 'native_mate/arguments.h'
+#include 'native_mate/handle.h'
+    
+    
+    {}  // namespace api
+    
+    #ifndef ATOM_BROWSER_NET_ABOUT_PROTOCOL_HANDLER_H_
+#define ATOM_BROWSER_NET_ABOUT_PROTOCOL_HANDLER_H_
+    
+      // net::URLRequestJobFactory::ProtocolHandler:
+  net::URLRequestJob* MaybeCreateJob(
+      net::URLRequest* request,
+      net::NetworkDelegate* network_delegate) const override;
+  bool IsSafeRedirectTarget(const GURL& location) const override;
+    
+    void OffScreenOutputDevice::Resize(const gfx::Size& pixel_size,
+                                   float scale_factor) {
+  if (viewport_pixel_size_ == pixel_size)
+    return;
+  viewport_pixel_size_ = pixel_size;
     }
     
-    SILLayout::SILLayout(CanGenericSignature Sig,
-                     ArrayRef<SILField> Fields)
-  : GenericSigAndFlags(Sig, getFlagsValue(anyMutable(Fields))),
-    NumFields(Fields.size())
-{
-#ifndef NDEBUG
-  verifyFields(Sig, Fields);
-#endif
-  auto FieldsMem = getTrailingObjects<SILField>();
-  for (unsigned i : indices(Fields)) {
-    new (FieldsMem + i) SILField(Fields[i]);
-  }
+    // Generate param traits write methods.
+#include 'ipc/param_traits_write_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+    
+    void App::SetProxyConfig(content::RenderProcessHost* render_process_host,
+                         const std::string& proxy_config) {
+  net::ProxyConfig config;
+  config.proxy_rules().ParseFromString(proxy_config);
+  net::URLRequestContextGetter* context_getter =
+    render_process_host->GetBrowserContext()->
+    GetRequestContextForRenderProcess(render_process_host->GetID());
+    }
+    
+    
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
 }
     
-    enum class ChildKind { Left, Right, Further, Root };
+      int id() const { return id_; }
+  std::string extension_id_;
+  ObjectManager* object_manager() const { return object_manager_.get(); }
     
-        // We can match a prefix so long as everything following the match is
-    // a number.
-    if (typeWord.startswith_lower(nameWord)) {
-      for (unsigned i = nameWord.size(), n = typeWord.size(); i != n; ++i) {
-        if (!clang::isDigit(typeWord[i])) return false;
-      }
+    // Get RenderView from current js context (only works under window context).
+content::RenderView* GetCurrentRenderView();
+content::RenderView* GetEnteredRenderView();
+    
+    #ifndef CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+#define CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
+    
+      if (type == 'separator') {
+    menu_item_ = gtk_separator_menu_item_new();
+  } else {
+    if (type == 'checkbox') {
+      menu_item_ = gtk_check_menu_item_new();
+      bool checked;
+      if (option.GetBoolean('checked', &checked))
+        SetChecked(checked);
+    } else {
+      menu_item_ = gtk_image_menu_item_new();
+      std::string icon;
+      if (option.GetString('icon', &icon))
+        SetIcon(icon);
+    }
     }
     
-    %{
-    }
-    
-      public:
-    ClangDiagRenderer(const clang::LangOptions &langOpts,
-                      clang::DiagnosticOptions *diagOpts,
-                      decltype(callback) fn)
-       : DiagnosticNoteRenderer(langOpts, diagOpts),
-         callback(fn) {}
-    
-    void swift::Demangle::Node::dump() {
-  std::string TreeStr = getNodeTreeAsString(this);
-  fputs(TreeStr.c_str(), stderr);
-}
-    
-    static const int base         = 36;
-static const int tmin         = 1;
-static const int tmax         = 26;
-static const int skew         = 38;
-static const int damp         = 700;
-static const int initial_bias = 72;
-static const uint32_t initial_n = 128;
-    
-        /*
-        Convert BGR image to YCrCb
-    */
-    void bgr2ycrcb(const Size2D &size,
-                   const u8 * srcBase, ptrdiff_t srcStride,
-                   u8 * dstBase, ptrdiff_t dstStride);
-    
-                int32x4_t va = vaddq_s32(lane0a, lane2a);
-            int32x4_t vb = vaddq_s32(lane0b, lane2b);
-            int32x4_t vc = vaddq_s32(lane0c, lane2c);
-            int32x4_t wa = vaddq_s32(va, lane1a);
-            int32x4_t wb = vaddq_s32(vb, lane1b);
-            int32x4_t wc = vaddq_s32(vc, lane1c);
-    
-                v_src0 = internal::vld1q(src0 + x);
-            v_src1 = internal::vld1q(src1 + x);
-            op(v_src0, v_src1, v_dst);
-            internal::vst1q(dst + x, v_dst);
-            x+=16;
-        }
-        if( x + 8 < width)
-        {
-            vec64  v_src0, v_src1;
-            uvec64 v_dst;
-    
-    void lshift(const Size2D &size,
-            const u8 * srcBase, ptrdiff_t srcStride,
-            s16 * dstBase, ptrdiff_t dstStride,
-            u32 shift)
-{
-    internal::assertSupportedConfiguration();
-    }
-    
-    
-    {} // namespace CAROTENE_NS
-
-    
-                for(; j < (ptrdiff_t)size.width - 16 - 3; j += 16, ptr += 16)
-            {
-                internal::prefetch(ptr);
-                internal::prefetch(ptr + pixel[0]);
-                internal::prefetch(ptr + pixel[2]);
-    }
-    
-    
-    {    minLocCount >>= 1;
-    maxLocCount >>= 1;
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)minVal;
-    (void)minLocPtr;
-    (void)minLocCount;
-    (void)minLocCapacity;
-    (void)maxVal;
-    (void)maxLocPtr;
-    (void)maxLocCount;
-    (void)maxLocCapacity;
-#endif
-}
-    
-            //left&right borders
-        if (borderType != BORDER_MODE_CONSTANT)
-            for (s32 k = 0; k < cn; ++k)
-            {
-                lane[-cn+k] = lane[idx_l1 + k];
-                lane[-cn-cn+k] = lane[idx_l2 + k];
-    }
-    
-                        tprev1 = vextq_s16(tnext1, tnext1, 6);
-                    tprev1 = vsetq_lane_s16(vgetq_lane_s16(tprev1, 3),tprev1, 0);
-                    tprev1 = vsetq_lane_s16(vgetq_lane_s16(tprev1, 2),tprev1, 1);
-                } else if (border == BORDER_MODE_REFLECT101) {
-                    tpprev1 = vextq_s16(tnext2, tnext2, 7);
-                    tpprev1 = vsetq_lane_s16(vgetq_lane_s16(tpprev1, 2),tpprev1, 0);
-    
-    int os_detect_blobs(const GenericVector<int>* allowed_scripts,
-                    BLOBNBOX_CLIST* blob_list,
-                    OSResults* osr,
-                    tesseract::Tesseract* tess);
-    
-    /**
- * Class to iterate over tesseract page structure, providing access to all
- * levels of the page hierarchy, without including any tesseract headers or
- * having to handle any tesseract structures.
- * WARNING! This class points to data held within the TessBaseAPI class, and
- * therefore can only be used while the TessBaseAPI class still exists and
- * has not been subjected to a call of Init, SetImage, Recognize, Clear, End
- * DetectOS, or anything else that changes the internal PAGE_RES.
- * See apitypes.h for the definition of PageIteratorLevel.
- * See also ResultIterator, derived from PageIterator, which adds in the
- * ability to access OCR output with text-specific methods.
- */
-    
-    #define VARDIR        'configs/' /*parameters files */
-#define MAX_ITEMS_IN_SUBMENU 30
-    
-      // Construct the cartesian product of the best_choices of word(1) and word2.
-  WERD_CHOICE_LIST joined_choices;
-  WERD_CHOICE_IT jc_it(&joined_choices);
-  WERD_CHOICE_IT bc1_it(&word->best_choices);
-  WERD_CHOICE_IT bc2_it(&word2->best_choices);
-  int num_word1_choices = word->best_choices.length();
-  int total_joined_choices = num_word1_choices;
-  // Nota Bene: For the main loop here, we operate only on the 2nd and greater
-  // word2 choices, and put them in the joined_choices list. The 1st word2
-  // choice gets added to the original word1 choices in-place after we have
-  // finished with them.
-  int bc2_index = 1;
-  for (bc2_it.forward(); !bc2_it.at_first(); bc2_it.forward(), ++bc2_index) {
-    if (total_joined_choices >= kTooManyAltChoices &&
-        bc2_index > kAltsPerPiece)
-      break;
-    int bc1_index = 0;
-    for (bc1_it.move_to_first(); bc1_index < num_word1_choices;
-        ++bc1_index, bc1_it.forward()) {
-      if (total_joined_choices >= kTooManyAltChoices &&
-          bc1_index > kAltsPerPiece)
-        break;
-      WERD_CHOICE *wc = new WERD_CHOICE(*bc1_it.data());
-      *wc += *bc2_it.data();
-      jc_it.add_after_then_move(wc);
-      ++total_joined_choices;
-    }
+      void extensions(InternalExtensionList& _return) {
+    // Your implementation goes here
+    printf('extensions\n');
   }
-  // Now that we've filled in as many alternates as we want, paste the best
-  // choice for word2 onto the original word alt_choices.
-  bc1_it.move_to_first();
-  bc2_it.move_to_first();
-  for (bc1_it.mark_cycle_pt(); !bc1_it.cycled_list(); bc1_it.forward()) {
-    *bc1_it.data() += *bc2_it.data();
-  }
-  bc1_it.move_to_last();
-  bc1_it.add_list_after(&joined_choices);
-    
-      // Returns the covariance.
-  double covariance() const {
-    if (total_weight > 0.0)
-      return (sigxy - sigx * sigy / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-  double x_variance() const {
-    if (total_weight > 0.0)
-      return (sigxx - sigx * sigx / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-  double y_variance() const {
-    if (total_weight > 0.0)
-      return (sigyy - sigy * sigy / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-    
-    struct Pix;
-struct TBLOB;
-struct TPOINT;
-    
-    /*!
- * \brief assign the expression to out according to request
- * \param out the data to be assigned
- * \param req the assignment request
- * \param exp the expression
- * \tparam OType output type
- * \tparam Exp expression type
- */
-#define ASSIGN_DISPATCH(out, req, exp)  \
-  {                                     \
-    switch (req) {                      \
-      case kNullOp:                     \
-        break;                          \
-      case kWriteTo:                    \
-      case kWriteInplace:               \
-        (out) = (exp);                  \
-        break;                          \
-      case kAddTo:                      \
-        (out) += (exp);                 \
-        break;                          \
-      default:                          \
-        LOG(FATAL) << 'not reached';    \
-    }                                   \
-  }
-    
-    // Declare Memory Type for Caffe blob
-enum caffeMemoryTypes {Data, Grad, Non};
-    
-    
-    {    CHECK_EQ(net_param.layer_size(), 1) << 'Prototxt' << value <<' more than a layer';
-    caffe::LayerParameter *layer_param = new caffe::LayerParameter(net_param.layer(0));
-    this->Get(head) = (*layer_param);
-  }
-    
-      Operator* CreateOperatorEx(Context ctx, std::vector<TShape> *in_shape,
-                             std::vector<int> *in_type) const override;
-    
-    
-    {  Engine::Get()->PushSync([=](RunContext ctx){
-      ndout.CheckAndAlloc();
-      cv::Mat buf(h, w, c == 3 ? CV_8UC3 : CV_8U, ndsrc.data().dptr_);
-      cv::Mat dst(top+h+bot, left+w+right, c == 3 ? CV_8UC3 : CV_8U, ndout.data().dptr_);
-      cv::copyMakeBorder(buf, dst, top, bot, left, right, type, cv::Scalar(value));
-      CHECK(!dst.empty());
-    }, ndout.ctx(), {ndsrc.var()}, {ndout.var()});
-  NDArray *tmp = new NDArray();
-  *tmp = ndout;
-  *out = tmp;
-  API_END();
-}
-
     
     
     {
-    {}}
-    
-    //////////////////////////////////////////////////////////////////////
-    
-    void Label::addJump(Assembler* a) {
-  if (m_address) return;
-  JumpInfo info;
-  info.a = a;
-  info.addr = a->codeBlock.frontier();
-  m_toPatch.push_back(info);
+    {    std::ostringstream oss;
+    std::copy(line.begin() + options_index,
+              line.end(),
+              std::ostream_iterator<std::string>(oss, ' '));
+    r['options'] = oss.str();
+    results.push_back(r);
+  }
 }
     
-    #include 'gtest/gtest.h'
     
-      x <<= 4;
-  x |= t;
+    {
+    {    // If we don't find a serial_number match, we assume this drive information
+    // can only be retrieved by explicitly passing driver information.
+    if (!matched) {
+      results.push_back(std::move(entry.second));
+    }
+  }
+}
+    
+    #include <osquery/utils/expected/expected.h>
+#include <osquery/utils/system/posix/errno.h>
+    
+    #include <osquery/config/config.h>
+#include <osquery/database.h>
+#include <osquery/flags.h>
+#include <osquery/sql.h>
+#include <osquery/system.h>
+#include <osquery/registry_factory.h>
+    
+      ~EbpfTracepoint();
     
     
-    { protected:
-  GemMessageManager manager_;
+    {    private:
+        redisAsyncContext * m_ctx;
+        QSocketNotifier * m_read;
+        QSocketNotifier * m_write;
 };
+    
+    
+    {    //1
+    ret = p.read(&p, buf, sizeof(buf));
+    if (ret < 0)
+    {
+        swSysError('read() failed.');
+    }
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world1', buf), 0);
+    //2
+    ret = p.read(&p, buf, sizeof(buf));
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world2', buf), 0);
+    //3
+    ret = p.read(&p, buf, sizeof(buf));
+    ASSERT_GT(ret, 0);
+    ASSERT_EQ(strcmp('hello world3', buf), 0);
+}
+    
+    TEST(lru_cache, memory_free)
+{
+    shared_ptr<lru_cache_test_class> val = make_shared<lru_cache_test_class>();
+    cache.set('test', val);
+    ASSERT_EQ(cache.get('test').get(), val.get());
+    val.reset();
+    ASSERT_EQ(dtor_num, 0);
+    cache.clear();
+    ASSERT_EQ(dtor_num, 1);
+}
+    
+            if (!deflater)
+        {
+            ret = nghttp2_hd_deflate_new(&deflater, SW_HTTP2_DEFAULT_HEADER_TABLE_SIZE);
+            if (ret != 0)
+            {
+                swoole_php_error(E_WARNING, 'nghttp2_hd_deflate_init() failed with error: %s\n', nghttp2_strerror(ret));
+                return SW_ERR;
+            }
+            client->deflater = deflater;
+        }
+    
+    TEST(cares, resolve_inet6)
+{
+    coro_test([](void *arg)
+    {
+        auto result = CAres::resolve('ipv6.sjtu.edu.cn', AF_INET6, 5);
+        ASSERT_EQ(result, '2001:da8:8000:1::80');
+    });
+}
+    
+        for (int i = 0; i < 1000; ++i)
+    {
+        auto ret = swAio_dispatch2(&event);
+        ASSERT_EQ(ret->object, event.object);
+        ASSERT_NE(ret->task_id, event.task_id);
+    }
+    
+        int ret3 = (int) (long) swHashMap_find(hm, (char *) SW_STRL('notfound'));
+    ASSERT_EQ(ret3, 0);
+    
+            memcpy(&tmp, (char*) recv_pkg.ptr + recv_pkg.size - 4, sizeof(tmp));
+        ASSERT_EQ(tmp, recv_pkg.serial_num);
+    
+    TEST(coroutine_channel, push_yield)
+{
+    Channel chan(1);
+    }
