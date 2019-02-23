c5179eb054806792d32b9f19379767ae5a79ba63
SUNsung <weatherius@gmail.com>
2019-02-23 11:56:17 +0200

    Rewrite [35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py] File in: 2019-02-23 11:56:17

--- pyton/35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py
+++ pyton/35W81UAsXkBv.35L6AQAF.AfQQ35a6f7sGmHPXU35Ui1.py
@@ -1,199 +1,130 @@
 
         
-        
-@bp.route('/')
-def index():
-    '''Show all the posts, most recent first.'''
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+            def remove_from_tail(self):
+        pass
     
     
-def get_db():
-    '''Connect to the application's configured database. The connection
-    is unique for each request and will be reused if this is called
-    again.
-    '''
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
+class PrivateChat(Chat):
     
-    # read in SQL for populating test data
-with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
-    _data_sql = f.read().decode('utf8')
+            Emit key value pairs of the form:
     
-        def to_json(self, value):
-        return http_date(value)
+        def _hash_function(self, key):
+        return key % self.size
     
-        Implements signals based on blinker if available, otherwise
-    falls silently back to a noop.
+    FISH_COMPLETION_FILE = 'youtube-dl.fish'
+FISH_COMPLETION_TEMPLATE = 'devscripts/fish-completion.in'
     
-        .. versionchanged:: 0.12
-       `app.test_client()` includes preset default environment, which can be
-       set after instantiation of the `app.test_client()` object in
-       `client.environ_base`.
+    print('WARNING: Lazy loading extractors is an experimental feature that may not always work', file=sys.stderr)
+    
+    with io.open(README_FILE, 'w', encoding='utf-8') as f:
+    f.write(header)
+    f.write(options)
+    f.write(footer)
+
+    
+    import io
+import optparse
+import os.path
+import re
     
     
-@pytest.mark.functional
-def test_how_to_configure_alias(proc, TIMEOUT):
-    proc.sendline(u'unfunction fuck')
-    how_to_configure(proc, TIMEOUT)
+def build_completion(opt_parser):
+    opts = [opt for group in opt_parser.option_groups
+            for opt in group.option_list]
+    opts_file = [opt for opt in opts if opt.metavar == 'FILE']
+    opts_dir = [opt for opt in opts if opt.metavar == 'DIR']
+    
+    
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    '''Log in a registered user by adding the user id to the session.'''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+    
+        with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    
+    
+# context locals
+_request_ctx_stack = LocalStack()
+_app_ctx_stack = LocalStack()
+current_app = LocalProxy(_find_app)
+request = LocalProxy(partial(_lookup_req_object, 'request'))
+session = LocalProxy(partial(_lookup_req_object, 'session'))
+g = LocalProxy(partial(_lookup_app_object, 'g'))
 
     
     
-@pytest.mark.parametrize('command', [
-    Command('aws dynamdb scan', misspelled_command),
-    Command('aws dynamodb scn', misspelled_subcommand),
-    Command('aws dynamodb t-item',
-            misspelled_subcommand_with_multiple_options)])
-def test_match(command):
-    assert match(command)
+class TagMarkup(JSONTag):
+    '''Serialize anything matching the :class:`~flask.Markup` API by
+    having a ``__html__`` method to the result of that method. Always
+    deserializes to an instance of :class:`~flask.Markup`.'''
     
-    You can download from:
-  https://osxfuse.github.io/
-Error: An unsatisfied requirement failed this build.'''
+            .. versionchanged:: 0.10
+           Raise a :exc:`BadRequest` error instead of returning an error
+           message as JSON. If you want that behavior you can add it by
+           subclassing.
     
+                datestr, codename = match.groups()
+            return version, parse_date(datestr), codename
     
-@parametrize_extensions
-@parametrize_filename
-@parametrize_script
-def test_get_new_command(ext, tar_error, filename, unquoted, quoted, script, fixed):
-    tar_error(unquoted.format(ext))
-    assert (get_new_command(Command(script.format(filename.format(ext)), ''))
-            == fixed.format(dir=quoted.format(''), filename=filename.format(ext)))
-
+    # If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+#
+# html_use_opensearch = ''
     
-                if is_short_sha(version) or \
-                    is_head_tag(version) or \
-                    is_short_sha_and_date(version):
-                # if projects are across multiple environments, allow 1 week difference
-                if projects_split_by_env and date_diff and date_diff < 604800:
-                    merge(to_release=to_release, from_releases=from_releases, sentry_models=orm)
-                    continue
-                # +/- 8 hours
-                if date_diff and date_diff > 28800:
-                    for release in releases:
-                        update_version(release, orm)
-                else:
-                    merge(to_release=to_release, from_releases=from_releases, sentry_models=orm)
-                continue
+        def scan_devices(self):
+        '''Scan for new devices and return a list with found device IDs.'''
+        self._update_info()
     
-        complete_apps = ['sentry']
-
+        def get_thomson_data(self):
+        '''Retrieve data from THOMSON and return parsed result.'''
+        try:
+            telnet = telnetlib.Telnet(self.host)
+            telnet.read_until(b'Username : ')
+            telnet.write((self.username + '\r\n').encode('ascii'))
+            telnet.read_until(b'Password : ')
+            telnet.write((self.password + '\r\n').encode('ascii'))
+            telnet.read_until(b'=>')
+            telnet.write(('hostmgr list\r\n').encode('ascii'))
+            devices_result = telnet.read_until(b'=>').split(b'\r\n')
+            telnet.write('exit\r\n'.encode('ascii'))
+        except EOFError:
+            _LOGGER.exception('Unexpected response from router')
+            return
+        except ConnectionRefusedError:
+            _LOGGER.exception(
+                'Connection refused by router. Telnet enabled?')
+            return
     
-        complete_apps = ['sentry']
-
+        def _retrieve_list_with_retry(self):
+        '''Retrieve the device list with a retry if token is invalid.
     
-        def _forwards(self, orm):
-        'Write your forwards methods here.'
-        dupe_envs = orm.Environment.objects.values_list('name', 'organization_id')\
-                                           .annotate(ecount=models.Count('id'))\
-                                           .filter(ecount__gt=1)
+            if data is not None:
+            body_message.update(data)
+            # Only one of text or attachment can be specified
+            if 'attachment' in body_message:
+                body_message.pop('text')
     
-            # Adding model 'ApiApplication'
-        db.create_table(
-            'sentry_apiapplication', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'client_id',
-                    self.gf('django.db.models.fields.CharField')(unique=True, max_length=64)
-                ), (
-                    'client_secret',
-                    self.gf('sentry.db.models.fields.encrypted.EncryptedTextField')()
-                ), (
-                    'owner', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.User']
-                    )
-                ),
-                ('name', self.gf('django.db.models.fields.CharField')(max_length=64, blank=True)), (
-                    'status',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(
-                        default=0, db_index=True
-                    )
-                ), (
-                    'allowed_origins',
-                    self.gf('django.db.models.fields.TextField')(null=True, blank=True)
-                ), ('redirect_uris', self.gf('django.db.models.fields.TextField')()), (
-                    'homepage_url',
-                    self.gf('django.db.models.fields.URLField')(max_length=200, null=True)
-                ), (
-                    'privacy_url',
-                    self.gf('django.db.models.fields.URLField')(max_length=200, null=True)
-                ), (
-                    'terms_url',
-                    self.gf('django.db.models.fields.URLField')(max_length=200, null=True)
-                ), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['ApiApplication'])
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/notify.file/
+'''
+import logging
+import os
     
-            # Adding field 'ApiKey.scope_list'
-        db.add_column(
-            'sentry_apikey',
-            'scope_list',
-            self.gf('sentry.db.models.fields.array.ArrayField')(
-                of=('django.db.models.fields.TextField', [], {})
-            ),
-            keep_default=False
-        )
+    import voluptuous as vol
     
-            users = orm.User.objects.exclude(
-            email__in=orm.UserEmail.objects.all().values_list('email', flat=True)
-        )
-    
-        REGISTRY = {}
-    
-    
-# In some very complex cases, it might be desirable to pull out the building
-# logic into another function (or a method on another class), rather than being
-# in the base class '__init__'. (This leaves you in the strange situation where
-# a concrete class does not have a useful constructor)
-    
-    
-class EnglishGetter(object):
-    
-        def __get__(self, obj, type_):
-        if obj is None:
-            return self
-        val = self.function(obj)
-        obj.__dict__[self.function.__name__] = val
-        return val
-    
-        def __exit__(self, Type, value, traceback):
-        if self.item is not None:
-            self._queue.put(self.item)
-            self.item = None
-    
-        def __init__(self):
-        self.time_provider = datetime.datetime
-    
-        def _abort_diagnostics(self):
-        return 'abort diagnostics'
-    
-    
-# Refined Abstraction
-class CircleShape(object):
-    def __init__(self, x, y, radius, drawing_api):
-        self._x = x
-        self._y = y
-        self._radius = radius
-        self._drawing_api = drawing_api
-    
-    
-class ProductModel(Model):
-    class Price(float):
-        '''A polymorphic way to pass a float with a particular
-        __str__ functionality.'''
+        client = messagebird.Client(config[CONF_API_KEY])
+    try:
+        # validates the api key
+        client.balance()
+    except messagebird.client.ErrorException:
+        _LOGGER.error('The specified MessageBird API key is invalid')
+        return None
