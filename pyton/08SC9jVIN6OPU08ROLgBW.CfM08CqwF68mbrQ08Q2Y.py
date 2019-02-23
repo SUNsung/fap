6b47023a8d201e3aac20f6f25abc2e6d1732294c
SUNsung <weatherius@gmail.com>
2019-02-24 00:09:13 +0200

    Rewrite [08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py] File in: 2019-02-24 00:09:12

--- pyton/08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py
+++ pyton/08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py
@@ -1,57 +1,185 @@
 
         
-        
-def commit_role(name, rawtext, text, lineno, inliner, options={}, content=[]):
-    ref = 'https://github.com/scrapy/scrapy/commit/' + text
-    set_classes(options)
-    node = nodes.reference(rawtext, 'commit ' + text, refuri=ref, **options)
-    return [node], []
+            This could be extended by having nested blocks, sorting them recursively
+    and flattening the end structure into a list of lines. Revision 2 maybe ^.^.
+'''
     
-        def __init__(self):
-        Resource.__init__(self)
-        self.concurrent = 0
-        self.tail = deque(maxlen=100)
-        self._reset_stats()
     
-        inproject = inside_project()
-    cmds = _get_commands_dict(settings, inproject)
-    cmdname = _pop_command_name(argv)
-    parser = optparse.OptionParser(formatter=optparse.TitledHelpFormatter(), \
-        conflict_handler='resolve')
-    if not cmdname:
-        _print_commands(settings, inproject)
-        sys.exit(0)
-    elif cmdname not in cmds:
-        _print_unknown_command(settings, cmdname, inproject)
-        sys.exit(2)
+class Formatting(object):
+    '''A delegate class that invokes the actual processors.'''
     
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
-                          help='set spider argument (may be repeated)')
-        parser.add_option('-o', '--output', metavar='FILE',
-                          help='dump scraped items into FILE (use - for stdout)')
-        parser.add_option('-t', '--output-format', metavar='FORMAT',
-                          help='format to use for dumping items with -o')
+        name = 'Digest HTTP auth'
+    auth_type = 'digest'
     
-    import scrapy
-from scrapy.commands import ScrapyCommand
-from scrapy.utils.template import render_templatefile, string_camelcase
-from scrapy.exceptions import UsageError
     
-        def syntax(self):
-        return '[options] <spider_file>'
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
     
-            assertion = (self.min_bound <= occurrences <= self.max_bound)
     
-            # If no credentials could be found anywhere,
-        # consider this an anonymous connection request by default;
-        # unless 'anon' was set explicitly (True/False).
-        anon = kw.get('anon')
-        if anon is None and not aws_access_key_id and not aws_secret_access_key:
-            kw['anon'] = True
-        self.anon = kw.get('anon')
+@mock.patch('httpie.core.get_response')
+def test_timeout(get_response):
+    def error(msg, *args, **kwargs):
+        global error_msg
+        error_msg = msg % args
     
-            dfd.addBoth(lambda _: self.signals.send_catch_log_deferred(
-            signal=signals.spider_closed, spider=spider, reason=reason))
-        dfd.addErrback(log_failure('Error while sending spider_close signal'))
+    
+class BaseConfigDict(dict):
+    
+        # https://en.wikipedia.org/wiki/Playfair_cipher#Description
+    for char1, char2 in chunker(ciphertext, 2):
+        row1, col1 = divmod(table.index(char1), 5)
+        row2, col2 = divmod(table.index(char2), 5)
+    
+    def generateKey(keySize):
+    print('Generating prime p...')
+    p = rabinMiller.generateLargePrime(keySize)
+    print('Generating prime q...')
+    q = rabinMiller.generateLargePrime(keySize)
+    n = p * q
+    
+            while self.values[new_key] is not None and self.values[new_key] != key:
+            new_key = self.__hash_double_function(key, data, i) if \
+                self.balanced_factor() >= self.lim_charge else None
+            if new_key is None: break 
+            else: i += 1
+    
+    	s = [7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22, \
+		5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20, \
+		4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23, \
+		6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 ]
+    
+    '''
+* Wondering how this method works !
+* It's pretty simple.
+* Let's say you need to calculate a ^ b
+* RULE 1 : a ^ b = (a*a) ^ (b/2) ---- example : 4 ^ 4 = (4*4) ^ (4/2) = 16 ^ 2
+* RULE 2 : IF b is ODD, then ---- a ^ b = a * (a ^ (b - 1)) :: where (b - 1) is even.
+* Once b is even, repeat the process to get a ^ b
+* Repeat the process till b = 1 OR b = 0, because a^1 = a AND a^0 = 1
+*
+* As far as the modulo is concerned,
+* the fact : (a*b) % c = ((a%c) * (b%c)) % c
+* Now apply RULE 1 OR 2 whichever is required.
+'''
+
+    
+    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    title = r1(r'<title>(.*)</title>', html)
+    assert title
+    id = r1(r'flvid\s*=\s*(\d+)', html)
+    assert id
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
+    
+    # If true, an OpenSearch description file will be output, and all pages will
+# contain a <link> tag referring to it.  The value of this option must be the
+# base URL from which the finished HTML is served.
+#
+# html_use_opensearch = ''
+    
+        def _queued_event_check(self, click_type, time_diff):
+        '''Generate a log message and returns true if timeout exceeded.'''
+        time_string = '{:d} {}'.format(
+            time_diff, 'second' if time_diff == 1 else 'seconds')
+    
+        def __init__(self, config):
+        '''Initialize the scanner.'''
+        self.host = config[CONF_HOST]
+        self.username = config[CONF_USERNAME]
+        self.password = config[CONF_PASSWORD]
+        self.last_results = []
+        data = self.get_actiontec_data()
+        self.success_init = data is not None
+        _LOGGER.info('canner initialized')
+    
+            return [device.mac for device in self.last_results]
+    
+    from homeassistant.components.device_tracker import (
+    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
+from homeassistant.const import CONF_HOST
+import homeassistant.helpers.config_validation as cv
+    
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/notify.command_line/
+'''
+import logging
+import subprocess
+    
+    CONF_TO = 'to'
+    
+        def send_message(self, message=None, **kwargs):
+        '''Send a message to a specified target.'''
+        from messagebird.client import ErrorException
+    
+    
+def get_service(hass, config, discovery_info=None):
+    '''Get the Mycroft notification service.'''
+    return MycroftNotificationService(
+        hass.data['mycroft'])
+    
+        # Display the resulting image
+    cv2.imshow('Video', frame)
+    
+    known_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+    
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
+    
+        :param img: An image (as a numpy array)
+    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
+    :param model: Which face detection model to use. 'hog' is less accurate but faster on CPUs. 'cnn' is a more accurate
+                  deep-learning model which is GPU/CUDA accelerated (if available). The default is 'hog'.
+    :return: A list of tuples of found face locations in css (top, right, bottom, left) order
+    '''
+    if model == 'cnn':
+        return [_trim_css_to_bounds(_rect_to_css(face.rect), img.shape) for face in _raw_face_locations(img, number_of_times_to_upsample, 'cnn')]
+    else:
+        return [_trim_css_to_bounds(_rect_to_css(face), img.shape) for face in _raw_face_locations(img, number_of_times_to_upsample, model)]
+    
+    # This is an example of running face recognition on a single image
+# and drawing a box around each person that was identified.
+    
+    
+def print_result(filename, name, distance, show_distance=False):
+    if show_distance:
+        print('{},{},{}'.format(filename, name, distance))
+    else:
+        print('{},{}'.format(filename, name))
+    
+            self.assertEqual(
+            set(face_landmarks[0].keys()),
+            set(['chin', 'left_eyebrow', 'right_eyebrow', 'nose_bridge',
+                 'nose_tip', 'left_eye', 'right_eye', 'top_lip',
+                 'bottom_lip']))
+        self.assertEqual(
+            face_landmarks[0]['chin'],
+            [(369, 220), (372, 254), (378, 289), (384, 322), (395, 353),
+             (414, 382), (437, 407), (464, 424), (495, 428), (527, 420),
+             (552, 399), (576, 372), (594, 344), (604, 314), (610, 282),
+             (613, 250), (615, 219)])
+    
+    import face_recognition
+import picamera
+import numpy as np
+    
+        # 图片上传失败，输出以下html代码
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method='POST' enctype='multipart/form-data'>
+      <input type='file' name='file'>
+      <input type='submit' value='Upload'>
+    </form>
+    '''
