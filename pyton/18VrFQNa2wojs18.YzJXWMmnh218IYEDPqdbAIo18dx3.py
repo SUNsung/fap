6046fdcffef0267291449c9d62c5e763f8dc6344
SUNsung <weatherius@gmail.com>
2019-02-23 06:32:06 +0200

    Rewrite [18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py] File in: 2019-02-23 06:32:06

--- pyton/18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py
+++ pyton/18VrFQNa2wojs18.YzJXWMmnh218IYEDPqdbAIo18dx3.py
@@ -1,205 +1,183 @@
 
         
-            return app
-
+        rates_train = nparray_and_transpose(rates_train)
+rates_valid = nparray_and_transpose(rates_valid)
+spikes_train = nparray_and_transpose(spikes_train)
+spikes_valid = nparray_and_transpose(spikes_valid)
+    
+      def id_to_word(self, cur_id):
+    if cur_id < self.size:
+      return self._id_to_word[cur_id]
+    return 'ERROR'
+    
+        # Add 'lstm/lstm_0/control_dependency' if you want to dump previous layer
+    # LSTM.
+    lstm_emb = sess.run(t['lstm/lstm_1/control_dependency'],
+                        feed_dict={t['char_inputs_in']: char_ids_inputs,
+                                   t['inputs_in']: inputs,
+                                   t['targets_in']: targets,
+                                   t['target_weights_in']: weights})
+    
+          if len(example) > num_steps:
+        final_x = example[:num_steps]
+        final_y = example[1:(num_steps + 1)]
+        w[i] = 1
     
     
-@pytest.mark.parametrize(('username', 'password', 'message'), (
-    ('', '', b'Username is required.'),
-    ('a', '', b'Password is required.'),
-    ('test', 'test', b'already registered'),
-))
-def test_register_validate_input(client, username, password, message):
-    response = client.post(
-        '/auth/register',
-        data={'username': username, 'password': password}
-    )
-    assert message in response.data
+def write_unmasked_log(log, id_to_word, sequence_eval):
+  '''Helper function for logging evaluated sequences without mask.'''
+  indices_arr = np.asarray(sequence_eval)
+  samples = helper.convert_to_human_readable(id_to_word, indices_arr,
+                                             FLAGS.batch_size)
+  for sample in samples:
+    log.write(sample + '\n')
+  log.flush()
+  return samples
     
     
-def test_author_required(app, client, auth):
-    # change the post author to another user
-    with app.app_context():
-        db = get_db()
-        db.execute('UPDATE post SET author_id = 2 WHERE id = 1')
-        db.commit()
+def convert_to_indices(sequences):
+  '''Convert a list of size [batch_size, sequence_length, vocab_size] to
+  a list of size [batch_size, sequence_length] where the vocab element is
+  denoted by the index.
+  '''
+  batch_of_indices = []
     
-        monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
-    result = runner.invoke(args=['init-db'])
-    assert 'Initialized' in result.output
-    assert Recorder.called
-
-    
-        def app_url_value_preprocessor(self, f):
-        '''Same as :meth:`url_value_preprocessor` but application wide.
-        '''
-        self.record_once(lambda s: s.app.url_value_preprocessors
-            .setdefault(None, []).append(f))
-        return f
-    
-            info.append('% 5d: trying loader of %s' % (
-            idx + 1, src_info))
+          avg_baseline = tf.reduce_sum(
+          baseline_missing_list[t] * baseline_list[t], keep_dims=True) / (
+              num_missing + eps)
+      baseline = tf.tile(avg_baseline, multiples=[FLAGS.batch_size / 2])
+      baselines.append(baseline)
     
     
-def _lookup_req_object(name):
-    top = _request_ctx_stack.top
-    if top is None:
-        raise RuntimeError(_request_ctx_err_msg)
-    return getattr(top, name)
+def create_critic_train_op(hparams, critic_loss, global_step):
+  '''Create Discriminator train op.'''
+  with tf.name_scope('train_critic'):
+    critic_optimizer = tf.train.AdamOptimizer(hparams.critic_learning_rate)
+    output_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('critic')
+    ]
     
-        :param directory: the directory where all the files are stored.
-    :param filename: the filename relative to that directory to
-                     download.
-    :param options: optional keyword arguments that are directly
-                    forwarded to :func:`send_file`.
+      else:
+    raise NotImplementedError
+    
+      if FLAGS.dis_share_embedding:
+    assert hparams.dis_rnn_size == hparams.gen_rnn_size, (
+        'If you wish to share Discriminator/Generator embeddings, they must be'
+        ' same dimension.')
+    with tf.variable_scope('gen/rnn', reuse=True):
+      embedding = tf.get_variable('embedding',
+                                  [FLAGS.vocab_size, hparams.gen_rnn_size])
+    
+    from ..common import *
+    
+        ep = 'http://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={}'
+    
+    def rebuilt_url(url):
+    path = urllib.parse.urlparse(url).path
+    aid = path.split('/')[-1].split('_')[0]
+    return 'http://www.dailymotion.com/embed/video/{}?autoplay=1'.format(aid)
+    
     '''
-    filename = safe_join(directory, filename)
-    if not os.path.isabs(filename):
-        filename = os.path.join(current_app.root_path, filename)
-    try:
-        if not os.path.isfile(filename):
-            raise NotFound()
-    except (TypeError, ValueError):
-        raise BadRequest()
-    options.setdefault('conditional', True)
-    return send_file(filename, **options)
-    
-    
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
-
-    
-        def dispatch_request(self, *args, **kwargs):
-        meth = getattr(self, request.method.lower(), None)
-    
-        request.addfinalizer(_reset_settings)
-    conf.settings.user_dir = Path('~/.thefuck')
-    return conf.settings
-    
-    
-@pytest.mark.parametrize('command, packages', [
-    (Command('vim', 'vim: command not found'),
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('sudo vim', 'vim: command not found'),
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('vim', 'The program 'vim' is currently not installed. You can install it by typing: sudo apt install vim'),
-     [('vim', 'main'), ('vim-tiny', 'main')])])
-def test_match(mocker, command, packages):
-    mocker.patch('thefuck.rules.apt_get.which', return_value=None)
-    mocker.patch('thefuck.rules.apt_get._get_packages',
-                 create=True, return_value=packages)
-    
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_get_new_command(brew_no_available_formula):
-    assert get_new_command(Command('brew install elsticsearch',
-                                   brew_no_available_formula))\
-        == 'brew install elasticsearch'
-    
-    # Subtracting pixel mean improves accuracy
-subtract_pixel_mean = True
-    
-    
-def load_data():
-    '''Loads CIFAR10 dataset.
-    
-        # Returns
-        List of input tensors.
-    '''
-    if not hasattr(tensor, '_keras_history'):
-        return tensor
-    
-    
-allobj = [losses.mean_squared_error,
-          losses.mean_absolute_error,
-          losses.mean_absolute_percentage_error,
-          losses.mean_squared_logarithmic_error,
-          losses.squared_hinge,
-          losses.hinge,
-          losses.categorical_crossentropy,
-          losses.binary_crossentropy,
-          losses.kullback_leibler_divergence,
-          losses.poisson,
-          losses.cosine_proximity,
-          losses.logcosh,
-          losses.categorical_hinge]
-    
-        if steps is not None:
-        for step in range(steps):
-            batch_outs = f(ins)
-            if isinstance(batch_outs, list):
-                if step == 0:
-                    for _ in enumerate(batch_outs):
-                        outs.append(0.)
-                for i, batch_out in enumerate(batch_outs):
-                    if i in stateful_metric_indices:
-                        outs[i] = float(batch_out)
-                    else:
-                        outs[i] += batch_out
-            else:
-                if step == 0:
-                    outs.append(0.)
-                outs[0] += batch_outs
-            if verbose == 1:
-                progbar.update(step + 1)
-        for i in range(len(outs)):
-            if i not in stateful_metric_indices:
-                outs[i] /= steps
-    else:
-        batches = make_batches(num_samples, batch_size)
-        index_array = np.arange(num_samples)
-        for batch_index, (batch_start, batch_end) in enumerate(batches):
-            batch_ids = index_array[batch_start:batch_end]
-            if isinstance(ins[-1], float):
-                # Do not slice the training phase flag.
-                ins_batch = slice_arrays(ins[:-1], batch_ids) + [ins[-1]]
-            else:
-                ins_batch = slice_arrays(ins, batch_ids)
-            for i in indices_for_conversion_to_dense:
-                ins_batch[i] = ins_batch[i].toarray()
-    
-            # Arguments
-            layer: layer instance.
-    
-    Gets to 0.89 test accuracy after 2 epochs.
-90s/epoch on Intel i5 2.4Ghz CPU.
-10s/epoch on Tesla K40 GPU.
+refer to http://open.youku.com/tools
 '''
-from __future__ import print_function
+youku_embed_patterns = [ 'youku\.com/v_show/id_([a-zA-Z0-9=]+)',
+                         'player\.youku\.com/player\.php/sid/([a-zA-Z0-9=]+)/v\.swf',
+                         'loader\.swf\?VideoIDS=([a-zA-Z0-9=]+)',
+                         'player\.youku\.com/embed/([a-zA-Z0-9=]+)',
+                         'YKU.Player\(\'[a-zA-Z0-9]+\',{ client_id: \'[a-zA-Z0-9]+\', vid: \'([a-zA-Z0-9]+)\''
+                       ]
+    }
     
-    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
-    host = r1(r'host=([^&]*)', html)
-    assert host
-    type = r1(r'videofiletype=([^&]*)', html)
-    assert type
-    vid = r1(r'&stream_name=([^&]*)', html)
-    assert vid
-    dir_str = r1(r'&dir=([^&]*)', html).strip()
-    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
-    _, ext, size = url_info(url)
-    print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
+    def giphy_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
     
-    from .theplatform import theplatform_download_by_pid
+            request.install_opener(request.build_opener(request.HTTPCookieProcessor()))
     
-    __all__ = ['ehow_download']
+            # Restart scan wizard
+        start_scanning(config, add_entities, client)
     
-    pattern_url_photoset = r'https?://www\.flickr\.com/photos/.+/(?:(?:sets)|(?:albums))?/([^/]+)'
-pattern_url_photostream = r'https?://www\.flickr\.com/photos/([^/]+)(?:/|(?:/page))?$'
-pattern_url_single_photo = r'https?://www\.flickr\.com/photos/[^/]+/(\d+)'
-pattern_url_gallery = r'https?://www\.flickr\.com/photos/[^/]+/galleries/(\d+)'
-pattern_url_group = r'https?://www\.flickr\.com/groups/([^/]+)'
-pattern_url_favorite = r'https?://www\.flickr\.com/photos/([^/]+)/favorites'
+            try:
+            data = [
+                ('user', self._username),
+                (self._type, self._password),
+            ]
+            res = requests.post(self._loginurl, data=data, timeout=10)
+        except requests.exceptions.Timeout:
+            _LOGGER.error(
+                'Connection to the router timed out at URL %s', self._url)
+            return False
+        if res.status_code != 200:
+            _LOGGER.error(
+                'Connection failed with http code %s', res.status_code)
+            return False
+        try:
+            self._userid = res.cookies['userid']
+            return True
+        except KeyError:
+            _LOGGER.error('Failed to log in to router')
+            return False
+    
+    
+def get_scanner(hass, config):
+    '''Validate the configuration and return a THOMSON scanner.'''
+    scanner = ThomsonDeviceScanner(config[DOMAIN])
+    
+        def __init__(self, token, default_room):
+        '''Initialize the service.'''
+        from ciscosparkapi import CiscoSparkAPI
+        self._default_room = default_room
+        self._token = token
+        self._spark = CiscoSparkAPI(access_token=self._token)
+    
+            resp = requests.get(BASE_API_URL, params=data, timeout=5)
+        if (resp.status_code != 200) or (resp.status_code != 201):
+            _LOGGER.error('Error %s : %s', resp.status_code, resp.text)
+
+    
+    For more details about this platform, please refer to the documentation
+https://home-assistant.io/components/demo/
+'''
+from homeassistant.components.notify import BaseNotificationService
+    
+            if data is not None:
+            body_message.update(data)
+            # Only one of text or attachment can be specified
+            if 'attachment' in body_message:
+                body_message.pop('text')
+    
+            _LOGGER.debug('Attempting to call Flock at %s', self._url)
+    
+    from homeassistant.components.notify import (
+    PLATFORM_SCHEMA, BaseNotificationService)
+from homeassistant.const import CONF_ACCESS_TOKEN, CONF_USERNAME
+import homeassistant.helpers.config_validation as cv
+    
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_ROOM): vol.Coerce(int),
+    vol.Required(CONF_TOKEN): cv.string,
+    vol.Optional(CONF_COLOR, default=DEFAULT_COLOR): vol.In(VALID_COLORS),
+    vol.Optional(CONF_FORMAT, default=DEFAULT_FORMAT): vol.In(VALID_FORMATS),
+    vol.Optional(CONF_HOST, default=DEFAULT_HOST): cv.string,
+    vol.Optional(CONF_NOTIFY, default=DEFAULT_NOTIFY): cv.boolean,
+})
+    
+    from homeassistant.components.notify import (
+    BaseNotificationService, PLATFORM_SCHEMA)
+from homeassistant.const import CONF_API_KEY, CONF_DEVICE
+from homeassistant.helpers import config_validation as cv
+    
+    
+if __name__ == '__main__':
+    sys.exit(main())
+
+    
+    ax = fig.add_axes((0.63 + 0.18, 0.1, 0.16, 0.8))
+y = np.row_stack((fnx(), fnx(), fnx()))
+x = np.arange(10)
+y1, y2, y3 = fnx(), fnx(), fnx()
+ax.stackplot(x, y1, y2, y3)
+ax.set_xticks([])
+ax.set_yticks([])
+    
+        def __call__(self, declarations_str, inherited=None):
+        ''' the given declarations to atomic properties
