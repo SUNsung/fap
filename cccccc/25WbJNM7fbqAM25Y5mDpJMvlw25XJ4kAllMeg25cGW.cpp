42cfa8fc578505d4528cf996cc86890f26641faa
SUNsung <weatherius@gmail.com>
2019-02-23 17:31:40 +0200

    Rewrite [25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp] File in: 2019-02-23 17:31:39

--- cccccc/25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp
+++ cccccc/25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp
@@ -1,328 +1,99 @@
 
         
-        
-    {  return false;
-}
-    
-    bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv);
-    
-    void AutoUpdater::OnError(const std::string& message) {
-  v8::Locker locker(isolate());
-  v8::HandleScope handle_scope(isolate());
-  auto error = v8::Exception::Error(mate::StringToV8(isolate(), message));
-  mate::EmitEvent(
-      isolate(), GetWrapper(), 'error',
-      error->ToObject(isolate()->GetCurrentContext()).ToLocalChecked(),
-      // Message is also emitted to keep compatibility with old code.
-      message);
-}
-    
-        // Downloadable Content Information
-    dict.Set('isDownloadable', val.downloadable);
-    
-    #endif  // ATOM_BROWSER_API_ATOM_API_RENDER_PROCESS_PREFERENCES_H_
-
-    
-    class AtomBrowserContext;
-    
-     protected:
-  explicit Event(v8::Isolate* isolate);
-  ~Event() override;
-    
-    
-    {}  // namespace api
-    
-    #ifndef ATOM_BROWSER_ATOM_JAVASCRIPT_DIALOG_MANAGER_H_
-#define ATOM_BROWSER_ATOM_JAVASCRIPT_DIALOG_MANAGER_H_
-    
-    void AutoUpdater::CheckForUpdates() {}
-    
-    #endif // BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
-
-    
-        if (a->infinity) {
-        return 0;
-    }
-    
-    std::string tmppath() {
-  // TMPFILE is for manual test execution during which the user will specify
-  // the full temp file path using the environmental variable TMPFILE
-  const char* tmpfile = getenv('TMPFILE');
-  if (tmpfile) {
-    return std::string(tmpfile);
-  }
-    }
-    
-    REGISTER_CPU_OPERATOR(EnforceFinite, EnforceFiniteOp<CPUContext>);
-    
-    ```
-    
-    RANGES dimensions description:
-1: represents list of examples within a batch
-2: represents list features
-3: two values which are start and length or a range (to be applied on DATA)
-    
-    template <>
-void GluOp<float, CPUContext>::ComputeGlu(
-    const int M,
-    const int split_dim,
-    const int N,
-    const float* Xdata,
-    float* Ydata) {
-  const int xStride = 2 * split_dim * N;
-  const int yStride = split_dim * N;
-  for (int i = 0; i < M; ++i) {
-    const int idx = i * xStride;
-    const int idy = i * yStride;
-    for (int j = 0; j < split_dim; ++j) {
-      const int jN = j * N;
-      const int jdx1 = idx + jN;
-      const int jdx2 = idx + (j + split_dim) * N;
-      const int jdy = idy + jN;
-      for (int k = 0; k < N; ++k) {
-        const float x1 = Xdata[jdx1 + k];
-        const float x2 = Xdata[jdx2 + k];
-        Ydata[jdy + k] = x1 * sigmoid(x2);
+        Contrast Example 2 with Example 1. For each data point per feature, the values
+are sorted by the corresponding KEY.
+)DOC')
+    .Input(0, 'DATA', 'Tensor of rank 1.')
+    .Input(
+        1,
+        'RANGES',
+        'Tensor of int32/int64 ranges, of dims (N, M, 2). '
+        'Where N is number of examples and M is a size of each example. '
+        'Last dimention represents a range in the format (start, lengths)')
+    .Input(2, 'KEY', 'Tensor of rank 1 and type int64.')
+    .Output(0, 'OUTPUT', '1-D tensor of size sum of range lengths')
+    .Arg('lengths', 'Expected lengths for ranges')
+    .TensorInferenceFunction([](const OperatorDef& def,
+                                const vector<TensorShape>& in) {
+      ArgumentHelper helper(def);
+      auto lengths = helper.GetRepeatedArgument<int>('lengths');
+      CAFFE_ENFORCE_EQ(in[0].dims_size(), 1, 'DATA should be 1-D tensor.');
+      CAFFE_ENFORCE_EQ(in[1].dims_size(), 3, 'RANGES should be 3-D tensor.');
+      if (in.size() > 2) {
+        CAFFE_ENFORCE_EQ(in[2].dims_size(), 1, 'KEY should be 1-D tensor.');
       }
+      CAFFE_ENFORCE_GT(lengths.size(), 0, 'lengths should be non-empty.');
+      std::vector<TensorShape> out(lengths.size());
+      for (int i = 0; i < lengths.size(); ++i) {
+        out[i].set_data_type(in[0].data_type());
+        out[i].add_dims(in[1].dims(0));
+        out[i].add_dims(lengths[i]);
+      }
+      return out;
+    });
+    
+        NDMask::NDMask(const NDShape& shape, const DeviceDescriptor& device)
+        : NDMask(shape, AllocateMatrix(shape, device))
+    {
+        if (shape.Rank() > 2)
+            LogicError('NDMask instance shaped '%S' with more than 2 axes is currently unsupported.', shape.AsString().c_str());
     }
-  }
-}
     
-                    op      = (int32_t)pat[fp->fPatIdx];
-                fp->fPatIdx++;
-                opType  = URX_TYPE(op);
-                opValue = URX_VAL(op);
-                U_ASSERT(opType == URX_STRING_LEN);
-                int32_t patternStringLen = opValue;  // Length of the string from the pattern.
+            static bool IsUDF(const FunctionPtr& f);
     
-    ScientificNumberFormatter::Style *ScientificNumberFormatter::SuperscriptStyle::clone() const {
-    return new ScientificNumberFormatter::SuperscriptStyle(*this);
-}
+        template <typename ElementType>
+    void Value::CopyVariableValueToCSCSparse(size_t sequenceLength, std::vector<SparseIndexType>& colStarts, std::vector<SparseIndexType>& rowIndices, std::vector<ElementType>& nonZeroValues, size_t& numNonZeroValues)
+    {
+        // All sanity check has been done in ValidateSparseCSCAndGetIndexSizes().
+        NDArrayViewPtr cpuView;
+        if (Device().Type() == DeviceKind::GPU)
+        {
+            // Todo: GPUSparseMatrix to CPUSparseMatrix is not implemented in matrix, as a workaround the dense matrix is used as intermediate presentation.
+            // However, it is possible that data value very close to 0 could treated as 0 after transformation between dense and sparse.
+            auto cpuDenseView = MakeSharedObject<NDArrayView>(GetDataType(), StorageFormat::Dense, Shape(), DeviceDescriptor::CPUDevice());
+            cpuDenseView->CopyFrom(*Data());
+            cpuView = MakeSharedObject<NDArrayView>(GetDataType(), GetStorageFormat(), Shape(), DeviceDescriptor::CPUDevice());
+            cpuView->CopyFrom(*cpuDenseView);
+        }
+        else
+            cpuView = Data();
+    }
     
-        UBool operator == (const ScriptSet &other) const;
-    ScriptSet & operator = (const ScriptSet &other);
+    using namespace std;
     
-    #if !UCONFIG_NO_BREAK_ITERATION
-    
-    U_NAMESPACE_END
-    
-    
-class U_I18N_API SharedDateFormatSymbols : public SharedObject {
-public:
-    SharedDateFormatSymbols(
-            const Locale &loc, const char *type, UErrorCode &status)
-            : dfs(loc, type, status) { }
-    virtual ~SharedDateFormatSymbols();
-    const DateFormatSymbols &get() const { return dfs; }
-private:
-    DateFormatSymbols dfs;
-    SharedDateFormatSymbols(const SharedDateFormatSymbols &);
-    SharedDateFormatSymbols &operator=(const SharedDateFormatSymbols &);
-};
-    
-    U_NAMESPACE_END
-    
-    static const UChar gZero[] = { 0x7A, 0x65, 0x72, 0x6F };
-static const UChar gOne[] = { 0x6F, 0x6E, 0x65 };
-static const UChar gTwo[] = { 0x74, 0x77, 0x6F };
-static const UChar gFew[] = { 0x66, 0x65, 0x77 };
-static const UChar gMany[] = { 0x6D, 0x61, 0x6E, 0x79 };
-static const UChar gOther[] = { 0x6F, 0x74, 0x68, 0x65, 0x72 };
-    
-    #include 'unicode/utypes.h'
-    
-      void onTimeout(const std::shared_ptr<DHTNode>& node);
-    
-      virtual ~DHTResponseMessage();
-    
-    public:
-  DHTRoutingTable(const std::shared_ptr<DHTNode>& localNode);
-    
-    #endif // D_DHT_ROUTING_TABLE_DESERIALIZER_H
-
-    
-    void DHTRoutingTableSerializer::serialize(const std::string& filename)
+    class ScopeTimer
 {
-  A2_LOG_INFO(fmt('Saving DHT routing table to %s.', filename.c_str()));
-  std::string filenameTemp = filename;
-  filenameTemp += '__temp';
-  BufferedFile fp(filenameTemp.c_str(), BufferedFile::WRITE);
-  if (!fp) {
-    throw DL_ABORT_EX(
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
-  }
-  char header[8];
-  memset(header, 0, sizeof(header));
-  // magic
-  header[0] = 0xa1u;
-  header[1] = 0xa2u;
-  // format ID
-  header[2] = 0x02u;
-  // version
-  header[6] = 0;
-  header[7] = 0x03u;
+    Timer m_aggregateTimer;
+    size_t m_verbosity;
+    std::string m_message;
     }
     
-    void DHTTaskExecutor::update()
-{
-  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
-                                  std::mem_fn(&DHTTask::finished)),
-                   execTasks_.end());
-  int r;
-  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
-    r = numConcurrent_ - execTasks_.size();
-  }
-  else {
-    r = 0;
-  }
-  while (r && !queue_.empty()) {
-    std::shared_ptr<DHTTask> task = queue_.front();
-    queue_.pop_front();
-    task->startup();
-    if (!task->finished()) {
-      execTasks_.push_back(task);
-      --r;
-    }
-  }
-  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
-                   static_cast<unsigned int>(getExecutingTaskSize()),
-                   static_cast<unsigned int>(getQueueSize())));
-}
-    
-    DHTTaskQueueImpl::~DHTTaskQueueImpl() = default;
-    
-    namespace aria2 {
-    }
-    
-    #include <memory>
-    
-    const std::string DHTUnknownMessage::UNKNOWN('unknown');
-    
-    
-    {  return Merge(column_family, key_slice, value_slice);
-}
-    
-    // Returns an Env that translates paths such that the root directory appears to
-// be chroot_dir. chroot_dir should refer to an existing directory.
-Env* NewChrootEnv(Env* base_env, const std::string& chroot_dir);
-    
-    #ifdef ROCKSDB_LIB_IO_POSIX
-#include 'env/io_posix.h'
-#include <errno.h>
-#include <fcntl.h>
-#include <algorithm>
-#if defined(OS_LINUX)
-#include <linux/fs.h>
-#endif
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/ioctl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#ifdef OS_LINUX
-#include <sys/statfs.h>
-#include <sys/syscall.h>
-#include <sys/sysmacros.h>
-#endif
-#include 'env/posix_logger.h'
-#include 'monitoring/iostats_context_imp.h'
-#include 'port/port.h'
-#include 'rocksdb/slice.h'
-#include 'util/coding.h'
-#include 'util/string_util.h'
-#include 'util/sync_point.h'
-    
-      // Set a snapshot at start of transaction by setting set_snapshot=true
-  txn_options.set_snapshot = true;
-  txn = txn_db->BeginTransaction(write_options, txn_options);
-    
-    
-    {  // time spent in open() and fopen().
-  uint64_t open_nanos;
-  // time spent in fallocate().
-  uint64_t allocate_nanos;
-  // time spent in write() and pwrite().
-  uint64_t write_nanos;
-  // time spent in read() and pread()
-  uint64_t read_nanos;
-  // time spent in sync_file_range().
-  uint64_t range_sync_nanos;
-  // time spent in fsync
-  uint64_t fsync_nanos;
-  // time spent in preparing write (fallocate etc).
-  uint64_t prepare_write_nanos;
-  // time spent in Logger::Logv().
-  uint64_t logger_nanos;
+    ///Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
     
-      // Insert to page cache
-  //
-  // page_key   Identifier to identify a page uniquely across restarts
-  // data       Page data
-  // size       Size of the page
-  virtual Status Insert(const Slice& key, const char* data,
-                        const size_t size) = 0;
+    using namespace std;
     
-    template <size_t... Indexes>
-int snprintf20Numbers(int i, index_sequence<Indexes...>) {
-  static_assert(20 == sizeof...(Indexes), 'Must have exactly 20 indexes');
-  return snprintf(
-      bigBuf.data(),
-      bigBuf.size(),
-      '%d %d %d %d %d'
-      '%d %d %d %d %d'
-      '%d %d %d %d %d'
-      '%d %d %d %d %d',
-      (i + static_cast<int>(Indexes))...);
+    using namespace std;
+    
+    
+    {                if(node->left){
+                    q.push(node->left);
+                    new_level_num ++;
+                }
+                if(node->right){
+                    q.push(node->right);
+                    new_level_num ++;
+                }
+            }
+    
+    using namespace std;
+    
+    using namespace std;
+    
+    
+    {    return 0;
 }
-    
-    vector<detail::BenchmarkResult> resultsFromFile(const std::string& filename) {
-  string content;
-  readFile(filename.c_str(), content);
-  vector<detail::BenchmarkResult> ret;
-  benchmarkResultsFromDynamic(parseJson(content), ret);
-  return ret;
-}
-    
-      ts = to<struct timespec>(createTimePoint<system_clock>(123ns));
-  EXPECT_EQ(0, ts.tv_sec);
-  EXPECT_EQ(123, ts.tv_nsec);
-    
-    /**
- * This functions is an extension point when FOLLY_HAVE_WEAK_SYMBOLS is true.
- * There is a default no-op implementation provided which can be overrided by
- * linking in a library which provides its own definition.
- *
- * @param codecType   The type of the codec for this counter.
- * @param codecName   The name of the codec for this counter. If the codecName
- *                    is empty it should be defaulted using the codecType.
- * @param level       Optionally the level used to construct the codec.
- * @param key         The key of the counter.
- * @param counterType The type of the counter.
- * @returns           A function to increment the counter for the given key and
- *                    type. It may be an empty folly::Function.
- */
-folly::Function<void(double)> makeCompressionCounterHandler(
-    folly::io::CodecType codecType,
-    folly::StringPiece codecName,
-    folly::Optional<int> level,
-    CompressionCounterKey key,
-    CompressionCounterType counterType);
-    
-    
-    {  /**
-   * The strategy parameter is used to tune the compression algorithm.
-   * Supported values:
-   * - Z_DEFAULT_STRATEGY: normal data
-   * - Z_FILTERED: data produced by a filter (or predictor)
-   * - Z_HUFFMAN_ONLY: force Huffman encoding only (no string match)
-   * - Z_RLE: limit match distances to one
-   * - Z_FIXED: prevents the use of dynamic Huffman codes
-   *
-   * The strategy parameter only affects the compression ratio but not the
-   * correctness of the compressed output.
-   */
-  int strategy;
-};
