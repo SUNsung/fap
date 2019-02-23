0e9128402f32badb8e87b96dab41fd296d7c72e9
SUNsung <weatherius@gmail.com>
2019-02-23 18:37:40 +0200

    Rewrite [32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb] File in: 2019-02-23 18:37:40

--- ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
+++ ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
@@ -1,110 +1,81 @@
 
         
-        ActiveRecord::Base.establish_connection( adapter: :sqlite3, database:  ':memory:')
-    
-    # Each time a record is set we check whether its session has already timed out
-# or not, based on last request time. If so, the record is logged out and
-# redirected to the sign in page. Also, each time the request comes and the
-# record is set, we set the last request time inside its scoped session to
-# verify timeout in the following request.
-Warden::Manager.after_set_user do |record, warden, options|
-  scope = options[:scope]
-  env   = warden.request.env
-    
-          # Configure default email options
-      def devise_mail(record, action, opts = {}, &block)
-        initialize_from_record(record)
-        mail headers_for(action, opts), &block
+        module ActionView #:nodoc:
+  # = Action View Raw Output Helper
+  module Helpers #:nodoc:
+    module OutputSafetyHelper
+      # This method outputs without escaping a string. Since escaping tags is
+      # now default, this can be used when you don't want Rails to automatically
+      # escape tags. This is not recommended if the data is coming from the user's
+      # input.
+      #
+      # For example:
+      #
+      #  raw @user.name
+      #  # => 'Jimmy <alert>Tables</alert>'
+      def raw(stringish)
+        stringish.to_s.html_safe
       end
     
-          module ClassMethods
-        # Create the cookie key using the record id and remember_token
-        def serialize_into_cookie(record)
-          [record.to_key, record.rememberable_value, Time.now.utc.to_f.to_s]
+              def serialize_resource(resource)
+            resource_serializer.new(
+              resource,
+              include: resource_includes,
+              sparse_fields: sparse_fields
+            ).serializable_hash
+          end
+    
+          before_action :set_content_type
+      before_action :load_user
+      before_action :authorize_for_order, if: proc { order_token.present? }
+      before_action :authenticate_user
+      before_action :load_user_roles
+    
+              if @product_property.update_attributes(product_property_params)
+            respond_with(@product_property, status: 200, default_template: :show)
+          else
+            invalid_resource!(@product_property)
+          end
         end
     
-    module Devise
-  module Models
-    # Timeoutable takes care of verifying whether a user session has already
-    # expired or not. When a session expires after the configured time, the user
-    # will be asked for credentials again, it means, they will be redirected
-    # to the sign in page.
-    #
-    # == Options
-    #
-    # Timeoutable adds the following options to devise_for:
-    #
-    #   * +timeout_in+: the interval to timeout the user session without activity.
-    #
-    # == Examples
-    #
-    #   user.timedout?(30.minutes.ago)
-    #
-    module Timeoutable
-      extend ActiveSupport::Concern
+    # Add our files (should be in the current directory):
+package.input('my-executable=/usr/bin/')
+package.input('my-library.so=/usr/lib/')
     
-          if @custom_emoji.update(resource_params)
-        log_action :update, @custom_emoji
-        flash[:notice] = I18n.t('admin.custom_emojis.updated_msg')
-      else
-        flash[:alert] =  I18n.t('admin.custom_emojis.update_failed_msg')
+      option '--package-name-prefix', 'PREFIX', 'Name to prefix the package ' \
+    'name with.', :default => 'node'
+    
+          if !attributes[:pacman_group].nil?
+        if attributes[:pacman_group] == 'root'
+          data_tar_flags += [ '--numeric-owner', '--group', '0' ]
+        else
+          data_tar_flags += [ '--group', attributes[:deb_group] ]
+        end
       end
-      redirect_to admin_custom_emojis_path(page: params[:page], **@filter_params)
+    end
+    return data_tar_flags
+  end # def data_tar_flags
+    
+          base = staging_path(File.join(attributes[:prefix], '#{platform.platform}/#{platform.target_version || 'default'}'))
+      target = File.join(base, 'files')
+      actions_script = File.join(base, 'install_actions.sh')
+      ::PleaseRun::Installer.install_files(platform, target, false)
+      ::PleaseRun::Installer.write_actions(platform, actions_script)
     end
     
-      def subscription
-    @_subscription ||= @account.subscription(
-      api_subscription_url(@account.id)
-    )
-  end
+          if !attributes[:python_install_data].nil?
+        flags += [ '--install-data', File.join(prefix, attributes[:python_install_data]) ]
+      elsif !attributes[:prefix].nil?
+        # prefix given, but not python_install_data, assume PREFIX/data
+        flags += [ '--install-data', File.join(prefix, 'data') ]
+      end
     
-      respond_to :json
-    
-      url 'http://swupdl.adobe.com/updates/oobe/aam20/mac/AdobeLightroom-#{version.major}.0/#{version}/setup.dmg'
-  name 'Adobe Photoshop Lightroom'
-  homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
-    
-          # Parse through mail to get the from/sender headers
-      mail = Mail.new(raw_message.split('\r\n\r\n', 2).first)
-      from_headers = {'from' => mail.from, 'sender' => mail.sender}
-      authenticated_domain = identity.server.find_authenticated_domain_from_headers(from_headers)
-    
-      def verified_email_required
-    if logged_in? && !current_user.verified?
-      redirect_to verify_path(:return_to => request.fullpath)
-    end
-  end
-    
-      def update
-    if @credential.update(params.require(:credential).permit(:name, :hold))
-      redirect_to_with_json [organization, @server, :credentials]
-    else
-      render_form_errors 'edit', @credential
-    end
-  end
-    
-      def index
-    if @server
-      @ip_pool_rules = @server.ip_pool_rules
-    else
-      @ip_pool_rules = organization.ip_pool_rules
-    end
-  end
-    
-      def cancel_hold
-    @message.cancel_hold
-    redirect_to_with_json organization_server_message_path(organization, @server, @message.id)
-  end
-    
-      def check
-    if @track_domain.check_dns
-      redirect_to_with_json [organization, @server, :track_domains], :notice => 'Your CNAME for #{@track_domain.full_name} looks good!'
-    else
-      redirect_to_with_json [organization, @server, :track_domains], :alert => 'There seems to be something wrong with your DNS record. Check documentation for information.'
-    end
-  end
-    
-        if @user == organization.owner
-      redirect_to_with_json [organization, :users], :alert => 'You cannot revoke the organization owner's access.'
-      return
-    end
+        args = [ tar_cmd,
+             '-C',
+             staging_path,
+             '-cf',
+             payload_tar,
+             '--owner=0',
+             '--group=0',
+             '--numeric-owner',
+             '.' ]
