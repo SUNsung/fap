31e56133c0844b019eee7ef517d2807e42846bd5
SUNsung <weatherius@gmail.com>
2019-02-24 12:37:31 +0200

    Rewrite [585Y5dkiKaef.586QI8pIYo8M58HGmF9n9oKTg58U.6.cpp] File in: 2019-02-24 12:37:31

--- cccccc/585Y5dkiKaef.586QI8pIYo8M58HGmF9n9oKTg58U.6.cpp
+++ cccccc/585Y5dkiKaef.586QI8pIYo8M58HGmF9n9oKTg58U.6.cpp
@@ -1,205 +1,118 @@
 
-    {  DISALLOW_COPY_AND_ASSIGN(TrackableObjectBase);
+        
+        
+    {  /** Pointer to the page_res owned by the API. */
+  PAGE_RES* page_res_;
+  /** Pointer to the Tesseract object owned by the API. */
+  Tesseract* tesseract_;
+  /**
+   * The iterator to the page_res_. Owned by this ResultIterator.
+   * A pointer just to avoid dragging in Tesseract includes.
+   */
+  PAGE_RES_IT* it_;
+  /**
+   * The current input WERD being iterated. If there is an output from OCR,
+   * then word_ is nullptr. Owned by the API
+   */
+  WERD* word_;
+  /** The length of the current word_. */
+  int word_length_;
+  /** The current blob index within the word. */
+  int blob_index_;
+  /**
+   * Iterator to the blobs within the word. If nullptr, then we are iterating
+   * OCR results in the box_word.
+   * Owned by this ResultIterator.
+   */
+  C_BLOB_IT* cblob_it_;
+  /** Control over what to include in bounding boxes. */
+  bool include_upper_dots_;
+  bool include_lower_dots_;
+  /** Parameters saved from the Thresholder. Needed to rebuild coordinates.*/
+  int scale_;
+  int scaled_yres_;
+  int rect_left_;
+  int rect_top_;
+  int rect_width_;
+  int rect_height_;
 };
     
+      // Try to adjust the blamer bundle.
+  if (orig_bb != nullptr) {
+    // TODO(rays) Looks like a leak to me.
+    // orig_bb should take, rather than copy.
+    word->blamer_bundle = new BlamerBundle();
+    word2->blamer_bundle = new BlamerBundle();
+    orig_bb->SplitBundle(chopped->blobs.back()->bounding_box().right(),
+                         word2->chopped_word->blobs[0]->bounding_box().left(),
+                         wordrec_debug_blamer,
+                         word->blamer_bundle, word2->blamer_bundle);
+  }
     
-    {}  // namespace atom
-
+    // Clip output boxes to input blob boxes for bounds that are within this
+// tolerance. Otherwise, the blob may be chopped and we have to just use
+// the word bounding box.
+const int kBoxClipTolerance = 2;
     
-      // There is an update available and it is being downloaded
-  virtual void OnUpdateAvailable() {}
+    class BLOCK;
+class WERD;
     
-    
-    {}  // namespace atom
-    
-    void PurchaseProduct(const std::string& productID,
-                     int quantity,
-                     const InAppPurchaseCallback& callback);
-    
-    
-    {  Product(const Product&);
-  Product();
-  ~Product();
-};
-    
-    // Converts a streamable value to an std::string.  A NULL pointer is
-// converted to '(null)'.  When the input value is a ::string,
-// ::std::string, ::wstring, or ::std::wstring object, each NUL
-// character in it is replaced with '\\0'.
-template <typename T>
-std::string StreamableToString(const T& streamable) {
-  return (Message() << streamable).GetString();
-}
-    
-    // Values() allows generating tests from explicitly specified list of
-// parameters.
-//
-// Synopsis:
-// Values(T v1, T v2, ..., T vN)
-//   - returns a generator producing sequences with elements v1, v2, ..., vN.
-//
-// For example, this instantiates tests from test case BarTest each
-// with values 'one', 'two', and 'three':
-//
-// INSTANTIATE_TEST_CASE_P(NumSequence, BarTest, Values('one', 'two', 'three'));
-//
-// This instantiates tests from test case BazTest each with values 1, 2, 3.5.
-// The exact type of values will depend on the type of parameter in BazTest.
-//
-// INSTANTIATE_TEST_CASE_P(FloatingNumbers, BazTest, Values(1, 2, 3.5));
-//
-// Currently, Values() supports from 1 to 50 parameters.
-//
-template <typename T1>
-internal::ValueArray1<T1> Values(T1 v1) {
-  return internal::ValueArray1<T1>(v1);
-}
-    
-    GTEST_API_ void PrintStringTo(const ::std::string&s, ::std::ostream* os);
-inline void PrintTo(const ::std::string& s, ::std::ostream* os) {
-  PrintStringTo(s, os);
-}
-    
-    
-    {  // The name of the source file where the test part took place, or
-  // '' if the source file is unknown.
-  std::string file_name_;
-  // The line in the source file where the test part took place, or -1
-  // if the line number is unknown.
-  int line_number_;
-  std::string summary_;  // The test failure summary.
-  std::string message_;  // The test failure message.
-};
-    
-      // Take over ownership of a raw pointer.  This should happen as soon as
-  // possible after the object is created.
-  explicit linked_ptr(T* ptr = NULL) { capture(ptr); }
-  ~linked_ptr() { depart(); }
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38, typename T39, typename T40,
-    typename T41, typename T42, typename T43>
-struct Types43 {
-  typedef T1 Head;
-  typedef Types42<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
-      T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28, T29,
-      T30, T31, T32, T33, T34, T35, T36, T37, T38, T39, T40, T41, T42,
-      T43> Tail;
-};
-    
-    
-    {}  // namespace internal
-    
-      virtual int GetNextPrime(int p) const {
-    for (int n = p + 1; n < is_prime_size_; n++) {
-      if (is_prime_[n]) return n;
-    }
-    }
-    
-    #define MXCAFFELAYER(__object$, __type$) \
-  (static_cast<mxnet::op::caffe::CaffeLayerFriend<__type$> *>(__object$))
-    
-    
-    { private:
-  CaffeLossParam param_;
-  ::caffe::Layer<Dtype> *caffeOp_;
-  Dtype grad_scale_;
-  std::vector< ::caffe::Blob<Dtype> *> bot_, top_;
-  std::vector<bool> flags_;
-  bool setup_;
-};  // class CaffeLoss
-    
-    namespace mxnet {
-namespace io {
-/*!
- * \brief OpenCV based Image augmenter,
- *  The augmenter can contain internal temp state.
- */
-class ImageAugmenter {
+    // A simple class to provide a dynamic programming solution to a class of
+// 1st-order problems in which the cost is dependent only on the current
+// step and the best cost to that step, with a possible special case
+// of using the variance of the steps, and only the top choice is required.
+// Useful for problems such as finding the optimal cut points in a fixed-pitch
+// (vertical or horizontal) situation.
+// Skeletal Example:
+// DPPoint* array = new DPPoint[width];
+// for (int i = 0; i < width; i++) {
+//   array[i].AddLocalCost(cost_at_i)
+// }
+// DPPoint* best_end = DPPoint::Solve(..., array);
+// while (best_end != nullptr) {
+//   int cut_index = best_end - array;
+//   best_end = best_end->best_prev();
+// }
+// delete [] array;
+class DPPoint {
  public:
-  /*!
-   *  \brief Initialize the Operator by setting the parameters
-   *  This function need to be called before all other functions.
-   *  \param kwargs the keyword arguments parameters
-   */
-  virtual void Init(const std::vector<std::pair<std::string, std::string> >& kwargs) = 0;
-  /*!
-   * \brief augment src image.
-   *   this function is not thread safe, and will only be called by one thread
-   *   however, it will tries to re-use memory space as much as possible
-   * \param src the source image
-   * \param prnd pointer to random number generator.
-   * \return The processed image.
-   */
-  virtual cv::Mat Process(const cv::Mat &src, std::vector<float> *label,
-                          common::RANDOM_ENGINE *prnd) = 0;
-  // virtual destructor
-  virtual ~ImageAugmenter() {}
-  /*!
-   * \brief factory function
-   * \param name Name of the augmenter
-   * \return The created augmenter.
-   */
-  static ImageAugmenter* Create(const std::string& name);
-};
-    }
+  // The cost function evaluates the total cost at this (excluding this's
+  // local_cost) and if it beats this's total_cost, then
+  // replace the appropriate values in this.
+  typedef int64_t (DPPoint::*CostFunc)(const DPPoint* prev);
     }
     
-    namespace mxnet {
-namespace io {
-/*!
- * \brief a vector of tensor with various shape
- *
- * data are stored in memory continuously
- */
-template<int dim, typename DType>
-class TensorVector {
- public:
-  TensorVector(void) {
-    this->Clear();
-  }
-  /*! \brief get the buffer to the i-th tensor */
-  inline mshadow::Tensor<cpu, dim, DType>
-  operator[](size_t i) const {
-    CHECK_LT(i + 1, offset_.size());
-    CHECK_EQ(shape_[i].Size(), offset_[i + 1] - offset_[i]);
-    return mshadow::Tensor<cpu, dim, DType>
-        ((DType*)dmlc::BeginPtr(content_) + offset_[i], shape_[i]);  // NOLINT(*)
-  }
-  inline mshadow::Tensor<cpu, dim, DType> Back() const {
-    return (*this)[Size() - 1];
-  }
-  inline size_t Size(void) const {
-    return shape_.size();
-  }
-  /*! \brief allocate space given the shape (data are copied) */
-  inline void Push(mshadow::Shape<dim> shape) {
-    shape_.push_back(shape);
-    offset_.push_back(offset_.back() + shape.Size());
-    content_.resize(offset_.back());
-  }
-  inline void Clear(void) {
-    offset_.clear();
-    offset_.push_back(0);
-    content_.clear();
-    shape_.clear();
-  }
-    }
-    }
-    }
+      Extension_call_pargs args;
+  args.registry = &registry;
+  args.item = &item;
+  args.request = &request;
+  args.write(oprot_);
     
-      virtual void BeforeFirst(void) {
-    if (param_.round_batch == 0 || num_overflow_ == 0) {
-      // otherise, we already called before first
-      base_->BeforeFirst();
+      // Flag for indicating found state utilizing hardware driver info.
+  bool found = false;
+  walk_func([&](const std::string& devname, hardwareDriver* type) {
+    // Get autodetected info..
+    auto resp = smartctl.getDevInfo(devname, '');
+    if (resp.err != NOERR) {
+      LOG(INFO) << 'There was an error retrieving drive information: '
+                << libsmartctl::errStr(resp.err);
+      // Don't return here, keep searching with fulltype.
     } else {
-      num_overflow_ = 0;
+      resp.content['device_name'] = devname;
+      results.push_back(std::move(resp.content));
     }
-    head_ = 1;
-  }
+    }
+    
+        // output changed array
+    std::cout << object << '\n';
+    
+        // out_of_range.402
+    try
+    {
+        // try to use the array index '-'
+        json::const_reference ref = j.at('/array/-'_json_pointer);
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
