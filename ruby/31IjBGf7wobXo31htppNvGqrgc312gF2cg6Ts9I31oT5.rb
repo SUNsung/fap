7d733fcd860f7ef184757f53ec25a6f376a927cc
SUNsung <weatherius@gmail.com>
2019-02-24 19:50:42 +0200

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2019-02-24 19:50:41

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,194 +1,82 @@
 
         
-            it 'returns a label 'No' if any falsy value is given' do
-      [false, nil].each { |value|
-        label = yes_no(value)
-        expect(label).to be_html_safe
-        expect(Nokogiri(label).text).to eq 'No'
-      }
-    end
-  end
+        WITH_JUST_LIQUID_VAR = <<-LIQUID.freeze
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
+pharetra tempus. et metus fermentum, eu cursus lorem, ac dictum enim.
+mattis. Curabitur vel dui et lacus rutrum suscipit et {{ title }} neque.
     
-        it 'can not be turned off' do
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'true' }
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
+  p.option 'destination', '-d', '--destination [DIR]',
+    'Destination directory (defaults to ./_site)'
+  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
+  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
+    'Plugins directory (defaults to ./_plugins)'
+  p.option 'layouts_dir', '--layouts DIR', String,
+    'Layouts directory (defaults to ./_layouts)'
+  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+    
+          def feature_element_timing_key(feature_element)
+        '\'#{feature_element.name}\' (#{feature_element.location})'
+      end
+    
+    def jekyll_run_status
+  if Paths.status_file.file?
+    then return Paths.status_file.read
+  end
+end
+    
+        def no_subcommand(args)
+      unless args.empty? ||
+          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
+        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
+                          Run `jekyll help` to find out more.'
+        abort
+      end
+    end
+    
+          # Reads a cache key.
+      #
+      # If the key exists and has a non-empty value its TTL is refreshed
+      # automatically.
+      #
+      # raw_key - The cache key to read.
+      # timeout - The new timeout of the key if the key is to be refreshed.
+      def self.read(raw_key, timeout: TIMEOUT)
+        key = cache_key_for(raw_key)
+        value = Redis::Cache.with { |redis| redis.get(key) }
+    
+              hash[:author] &&= Representation::User.from_json_hash(hash[:author])
+    
+    $stderr.puts <<DEPRECATION
+WARNING: Ruby Sass's Git repository is moving, and the old repository will be
+deled on 26 March 2019! Please update your Git URLs to point to the new
+repository at https://github.com/sass/ruby-sass.
+    
+      # An environment that can write to in-scope global variables, but doesn't
+  # create new variables in the global scope. Useful for top-level control
+  # directives.
+  class SemiGlobalEnvironment < Environment
+    def try_set_var(name, value)
+      @vars ||= {}
+      if @vars.include?(name)
+        @vars[name] = value
+        true
+      elsif @parent
+        @parent.try_set_var(name, value)
+      else
+        false
+      end
     end
   end
 end
 
     
-              existing_scenario.reload
-          expect(existing_scenario.guid).to eq(guid)
-          expect(existing_scenario.tag_fg_color).to eq(tag_fg_color)
-          expect(existing_scenario.tag_bg_color).to eq(tag_bg_color)
-          expect(existing_scenario.icon).to eq(icon)
-          expect(existing_scenario.description).to eq(description)
-          expect(existing_scenario.name).to eq(name)
-          expect(existing_scenario.source_url).to eq(source_url)
-          expect(existing_scenario.public).to be_falsey
-        end
+    module Jekyll
     
-      before(:each) do
-    stub_request(:get, /events.json$/).to_return(
-      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    stub_request(:get, /projects.json$/).to_return(
-      :body => JSON.dump([{name: 'test', id: 1234},{name: 'test1', id: 1235}]),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    stub_request(:get, /02:00$/).to_return(
-      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    @valid_params = { :project_id => 6789 }
-    
-            a_split.each_with_index { |s, i| a_split[i] = s.to_i unless i == a_length - 1 }
-        b_split.each_with_index { |s, i| b_split[i] = s.to_i unless i == b_length - 1 }
-    
-        def add(path, content)
-      @pages[path] = content
-    end
-    
-        private
-    
-        module MultipleBaseUrls
-      def self.included(base)
-        base.extend ClassMethods
-      end
-    
-            css('.nav-index-section').each do |node|
-          node.content = node.content
-        end
-    
-      def pinnable?
-    !@f.installed_prefixes.empty?
-  end
-    
-      describe '#casks' do
-    it 'returns an empty array if there is no match' do
-      expect(subject.casks).to eq []
+        def sizes
+      attrs = 'width='#{@sizes[0]}'' if @sizes[0]
+      attrs += ' height='#{@sizes[1]}'' if @sizes[1]
+      attrs
     end
   end
-    
-            # Sends a Kerberos Request over a tcp connection
-        #
-        # @param req [Rex::Proto::Kerberos::Model::KdcRequest] the request to send
-        # @return [Integer] the number of bytes sent
-        # @raise [RuntimeError] if the request can't be encoded
-        def send_request_tcp(req)
-          data = req.encode
-          length = [data.length].pack('N')
-          connection.put(length + data)
-        end
-    
-              private
-    
-              def initialize(options = {})
-            self.class.attributes.each do |attr|
-              if options.has_key?(attr)
-                m = (attr.to_s + '=').to_sym
-                self.send(m, options[attr])
-              end
-            end
-          end
-    
-              # Rex::Proto::Kerberos::Model::KdcResponse encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'KdcResponse encoding not supported'
-          end
-    
-              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey from an
-          # OpenSSL::ASN1::Sequence
-          #
-          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
-          def decode_asn1(input)
-            seq_values = input.value
-            self.type = decode_type(seq_values[0])
-            self.value = decode_value(seq_values[1])
-          end
-    
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
-    
-        def log_processed(name)
-      puts green '    #{name}'
-    end
-    
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
-    
-        it 'succeeds for notification dropdown' do
-      Timecop.travel(6.seconds.ago) do
-        @notification.touch
-      end
-      get :index, format: :json
-      expect(response).to be_success
-      response_json = JSON.parse(response.body)
-      note_html = Nokogiri::HTML(response_json['notification_list'][0]['also_commented']['note_html'])
-      timeago_content = note_html.css('time')[0]['data-time-ago']
-      expect(response_json['unread_count']).to be(1)
-      expect(response_json['unread_count_by_type']).to eq(
-        'also_commented'       => 1,
-        'comment_on_post'      => 0,
-        'contacts_birthday'    => 0,
-        'liked'                => 0,
-        'mentioned'            => 0,
-        'mentioned_in_comment' => 0,
-        'reshared'             => 0,
-        'started_sharing'      => 0
-      )
-      expect(timeago_content).to include(@notification.updated_at.iso8601)
-      expect(response.body).to match(/note_html/)
-    end
-    
-          it 'assigns @user' do
-        get :create, params: invalid_params
-        expect(assigns(:user)).not_to be_nil
-      end
-    
-          it 'creates a reshare' do
-        expect{
-          post_request!
-        }.to change(Reshare, :count).by(1)
-      end
-    
-    Liquid::Template.register_tag('gist', Jekyll::GistTag)
-Liquid::Template.register_tag('gistnocache', Jekyll::GistTagNoCache)
-
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
-        else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
-        end
-        @img['class'].gsub!(/'/, '') if @img['class']
-      end
-      super
-    end
-    
-          super
-    end
-    
-    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
-
-    
-        def initialize(tag_name, markup, tokens)
-      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
-      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
-      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
-      super
-    end
+end
