12e815387723acddc551b8201dec46256ec6d2be
SUNsung <weatherius@gmail.com>
2019-02-24 06:28:32 +0200

    Rewrite [22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp] File in: 2019-02-24 06:28:31

--- cccccc/22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp
+++ cccccc/22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp
@@ -1,185 +1,127 @@
 
         
-        /**
- * Virtual class encapsulate boost::thread for use in base class
- * The child class will acquire the ability to run a single thread,
- * by reimplementing the virtual function InternalThreadEntry.
- */
-class InternalThread {
- public:
-  InternalThread() : thread_() {}
-  virtual ~InternalThread();
-    }
+          // Run shape inference.
+  tensorflow::shape_inference::InferenceContext c(
+      graph_def_version, &node, op_reg_data->op_def, input_shapes,
+      input_tensors, input_tensor_as_shapes_protos,
+      input_handle_shapes_and_types);
+  TF_RETURN_IF_ERROR(c.construction_status());
     
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    #include <Python.h>
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    extern PyTypeObject TensorReleaserType;
+    
+        http://www.apache.org/licenses/LICENSE-2.0
     
     
-    {}  // namespace caffe
-    
-    #include 'caffe/layers/neuron_layer.hpp'
-#include 'caffe/layers/relu_layer.hpp'
-    
-    
-    {}  // namespace caffe
-    
-    
-    {		*a_ppaucEncodingBits = image.GetEncodingBits();
-		*a_puiEncodingBitsBytes = image.GetEncodingBitsBytes();
-		*a_puiExtendedWidth = image.GetExtendedWidth();
-		*a_puiExtendedHeight = image.GetExtendedHeight();
-		*a_piEncodingTime_ms = image.GetEncodingTimeMs();
-	}
-    
-    					if (imageformat == Image::Format::RG11 ||
-						imageformat == Image::Format::SIGNED_RG11)
-					{
-						m_afrgbaSource[uiPixel].fA = 1.0f;
-						m_afrgbaSource[uiPixel].fB = 0.0f;
-					}
-    
-    	// ----------------------------------------------------------------------------------------------------
-	// calculate source pixel averages for each 2x2 quadrant in a 4x4 block
-	// these are used to determine the averages for each of the 4 different halves (left, right, top, bottom)
-	// ignore pixels that have alpha == NAN (these are border pixels outside of the source image)
-	// weight the averages based on a pixel's alpha
-	//
-	void Block4x4Encoding_ETC1::CalculateSourceAverages(void)
-	{
-		static const bool DEBUG_PRINT = false;
-    }
-    
-    // Constants and Macros used by all idct/dct functions
-#define DCT_CONST_BITS 14
-#define DCT_CONST_ROUNDING  (1 << (DCT_CONST_BITS - 1))
-    
-    #define CLIPPER_VERSION '6.4.2'
-    
-    #endif /* KISS_FFT_GUTS_H */
-
-    
-    #endif /* OPUS_ARM_INLINE_MEDIA */
-    
-    #endif
-
-    
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-#include 'Utils.h'
-#include 'ReaderShim.h'
-#include 'DataReader.h'
-    
-            // (start, end) values in the current window to be reported.
-        std::pair<double, double> m_loss;
-        std::pair<double, double> m_metric;
-        std::pair<size_t, size_t> m_samples;
-        std::pair<size_t, size_t> m_updates;
-    
-    
-    {
-    {        return version;
-    }
-}
-
-    
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-#include <fstream>
-    
-        double ElapsedSeconds();
-    
-        virtual void /*ComputationNodeBase::*/ Validate(bool isFinalValidationPass) override
-    {
-        Base::Validate(isFinalValidationPass);
-        InferMBLayoutFromInputsForStandardCase(isFinalValidationPass);
-    }
-    
-        for (i = 1; i < 1024; i++)
-    {
-        uint32_t key = ((rand() % 19999) + 1) * 37;
-        int ret = (int) (long) swRbtree_find(tree, key);
-        ASSERT_GT(ret, 0);
-        lists.insert(key);
-    }
-    
-    Context::Context(size_t stack_size, coroutine_func_t fn, void* private_data) :
-        fn_(fn), stack_size_(stack_size), private_data_(private_data)
-{
-    if (-1 == getcontext(&ctx_))
-    {
-        swoole_throw_error(SW_ERROR_CO_GETCONTEXT_FAILED);
-        return;
-    }
-    }
-    
-                    active_sock[i] = sock[i];
-                auto sw_conn = swReactor_get(reactor, sock[i]);
-                sw_conn->object = &task;
-                sw_conn->removed = 0;
-            }
-            else if (ARES_GETSOCK_READABLE(bitmap, i))
-            {
-                // only readable
-                if (unlikely(reactor->add(reactor, sock[i], SW_FD_ARES | SW_EVENT_READ) < 0))
-                {
-                    return '';
-                }
-    
-    
-    {    cache.clear();
-    ASSERT_EQ(dtor_num, 4);
+    {  DCHECK(PyDict_Check(code_to_exc_type_map));
+  PyObject* key;
+  PyObject* value;
+  Py_ssize_t pos = 0;
+  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
+    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
+    singleton_->exc_types_[code] = value;
+    // The exception classes should also have the lifetime of the process, but
+    // incref just in case.
+    Py_INCREF(value);
+  }
 }
     
-    #include <unordered_map>
-#include <list>
-#include <utility>
-#include <memory>
-#include <time.h>
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-        inline bool is_full()
-    {
-        return data_queue.size() == capacity;
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    namespace tensorflow {
     }
     
-            // flow control
-        client->recv_window -= length;
-        stream->recv_window -= length;
-        if (length > 0)
+    // Safe container for an owned PyObject. On destruction, the reference count of
+// the contained object will be decremented.
+using Safe_PyObjectPtr = std::unique_ptr<PyObject, detail::PyDecrefDeleter>;
+Safe_PyObjectPtr make_safe(PyObject* o);
+    
+    // Returns the kernel class name required to execute <node_def> on the device
+// type of <node_def.device>, or an empty string if the kernel class is not
+// found or the device name is invalid.
+string TryFindKernelClass(const string& serialized_node_def);
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    class ExampleQt : public QObject {
+    }
+    
+            size_t size = cache_list.size();
+        if (size == cache_capacity && size > 0)
         {
-            if (client->recv_window < (SW_HTTP2_MAX_WINDOW_SIZE / 4))
-            {
-                http2_server_send_window_update(fd, 0, SW_HTTP2_MAX_WINDOW_SIZE - client->recv_window);
-                client->recv_window = SW_HTTP2_MAX_WINDOW_SIZE;
-            }
-            if (stream->recv_window < (SW_HTTP2_MAX_WINDOW_SIZE / 4))
-            {
-                http2_server_send_window_update(fd, stream_id, SW_HTTP2_MAX_WINDOW_SIZE - stream->recv_window);
-                stream->recv_window = SW_HTTP2_MAX_WINDOW_SIZE;
-            }
+            auto del = cache_list.back();
+            cache_map.erase(del.first);
+            cache_list.pop_back();
         }
     
-        pid_t server_pid = create_server();
-    
-    static int thread_onTask(swThreadPool *pool, void *task, int task_len)
+    void Channel::yield(enum opcode type)
 {
-    sw_atomic_long_t *n = (sw_atomic_long_t *) task;
-    sw_atomic_fetch_add(n, 1);
-    if (*n == N - 1)
+    Coroutine *co = Coroutine::get_current();
+    if (unlikely(!co))
     {
-        write(_pipe, (void*) n, sizeof(long));
+        swError('Channel::yield() must be called in the coroutine.');
     }
-    return SW_OK;
+    if (type == PRODUCER)
+    {
+        producer_queue.push_back(co);
+        swTraceLog(SW_TRACE_CHANNEL, 'producer cid=%ld', co->get_cid());
+    }
+    else
+    {
+        consumer_queue.push_back(co);
+        swTraceLog(SW_TRACE_CHANNEL, 'consumer cid=%ld', co->get_cid());
+    }
+    co->yield();
+}
+    
+    
+    {    while (1)
+    {
+        data = (int) (long) swHashMap_each(hm, &key);
+        if (!data)
+        {
+            break;
+        }
+    }
+    swHashMap_free(hm);
+}
+    
+    static pid_t create_server()
+{
+    pid_t pid;
+    swoole_shell_exec('php server/tcp.php', &pid, 1);
+    sleep(1); // wait 1s
+    return pid;
+}
+    
+    
+    {
+    {        make_pair([](void *arg)
+        {
+            auto chan = (Channel *) arg;
+            ASSERT_EQ(*(int *) chan->pop(), 1);
+            ASSERT_EQ(*(int *) chan->pop(), 1);
+        }, &chan)
+    });
 }
