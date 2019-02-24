f050647d073446bcb0c91d37e848bffc52354f5b
SUNsung <weatherius@gmail.com>
2019-02-24 03:19:27 +0200

    Rewrite [09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp] File in: 2019-02-24 03:19:26

--- cccccc/09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp
+++ cccccc/09ZO6LzQA6vnE091Kkt33QQXI09PCTnteicpUs09fuf.cpp
@@ -1,96 +1,83 @@
 
         
-        // Class to hold an array of bounding boxes for an output word and
-// the bounding box of the whole word.
-class BoxWord {
- public:
-  BoxWord();
-  explicit BoxWord(const BoxWord& src);
-  ~BoxWord() = default;
+        #include 'blobbox.h'             // for BLOBNBOX (ptr only), BlobSpecialText...
+#include 'equationdetectbase.h'  // for EquationDetectBase
+#include 'genericvector.h'       // for GenericVector
+#include 'tesseractclass.h'      // for Tesseract
+#include 'unichar.h'             // for UNICHAR_ID
+    
+    
+    {}  // namespace tesseract.
+
+    
+    class MutableIterator;
+    
+    // Update the other members if the cost is lower.
+void DPPoint::UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
+                             int32_t n, int32_t sig_x, int64_t sig_xsq) {
+  if (cost < total_cost_) {
+    total_cost_ = cost;
+    total_steps_ = steps;
+    best_prev_ = prev;
+    n_ = n;
+    sig_x_ = sig_x;
+    sig_xsq_ = sig_xsq;
+  }
+}
+    
+     private:
+  // Code common to different cost functions.
+    
+    #define MODULUS       128        /*range of directions */
+#define DIRBITS       7          //no of bits used
+#define DIRSCALE      1000       //length of vector
+    
+    #define SIZE    100
+    
+            void delRead() {
+            if (!m_read) return;
+            delete m_read;
+            m_read = 0;
+        }
+    
+        m_ctx = redisAsyncConnect('localhost', 6379);
+    
+        shared_ptr<string> val_str = make_shared<string>('hello');
+    cache.set('test1', val_str); // update test1 and will del test2
+    ASSERT_EQ(cache.get('test1').get(), val_str.get());
+    ASSERT_EQ(dtor_num, 2);
+    
+        inline void set(const std::string &key, const std::shared_ptr<void> &val, time_t expire = 0)
+    {
+        time_t expire_time;
     }
     
-      // Fits a line to the points, returning the fitted line as a pair of
-  // points, and the upper quartile error.
-  double Fit(ICOORD* pt1, ICOORD* pt2) {
-    return Fit(0, 0, pt1, pt2);
-  }
-  // Fits a line to the points, ignoring the skip_first initial points and the
-  // skip_last final points, returning the fitted line as a pair of points,
-  // and the upper quartile error.
-  double Fit(int skip_first, int skip_last, ICOORD* pt1, ICOORD* pt2);
-    
-      // Accessors.
-  int total_cost() const {
-    return total_cost_;
-  }
-  int Pathlength() const {
-    return total_steps_;
-  }
-  const DPPoint* best_prev() const {
-    return best_prev_;
-  }
-  void AddLocalCost(int new_cost) {
-    local_cost_ += new_cost;
-  }
-    
-      // Computes the maximum x and y value in the features.
-  static void ComputeSize(const GenericVector<WordFeature>& features,
-                          int* max_x, int* max_y);
-  // Draws the features in the given window.
-  static void Draw(const GenericVector<WordFeature>& features,
-                   ScrollView* window);
-    
-    #include <cstdint>      // for int32_t
-#include 'points.h'     // for FCOORD
-    
-    template <typename Generator1, typename Generator2, typename Generator3,
-    typename Generator4, typename Generator5, typename Generator6>
-internal::CartesianProductHolder6<Generator1, Generator2, Generator3,
-    Generator4, Generator5, Generator6> Combine(
-    const Generator1& g1, const Generator2& g2, const Generator3& g3,
-        const Generator4& g4, const Generator5& g5, const Generator6& g6) {
-  return internal::CartesianProductHolder6<Generator1, Generator2, Generator3,
-      Generator4, Generator5, Generator6>(
-      g1, g2, g3, g4, g5, g6);
-}
-    
-    // First, define a fixture class template.  It should be parameterized
-// by a type.  Remember to derive it from testing::Test.
-template <typename T>
-class FooTest : public testing::Test {
- public:
-  ...
-  typedef std::list<T> List;
-  static T shared_;
-  T value_;
-};
-    
-    template <GTEST_3_TYPENAMES_(T)>
-struct tuple_size<GTEST_3_TUPLE_(T) > {
-  static const int value = 3;
-};
-    
-    #include <grpcpp/impl/grpc_library.h>
-#include <grpcpp/security/credentials.h>
-    
-    constexpr size_t RpcServerStatsEncoding::kRpcServerStatsSize;
-constexpr size_t RpcServerStatsEncoding::kEncodeDecodeFailure;
-constexpr size_t RpcServerStatsEncoding::kVersionIdSize;
-constexpr size_t RpcServerStatsEncoding::kFieldIdSize;
-constexpr size_t RpcServerStatsEncoding::kVersionIdOffset;
-constexpr size_t RpcServerStatsEncoding::kVersionId;
-    
-    const ViewDescriptor& ClientServerLatencyMinute() {
-  const static ViewDescriptor descriptor =
-      MinuteDescriptor()
-          .set_name('grpc.io/client/server_latency/minute')
-          .set_measure(kRpcClientServerLatencyMeasureName)
-          .set_aggregation(MillisDistributionAggregation())
-          .add_column(ClientMethodTagKey());
-  return descriptor;
-}
-    
-    
+    bool Channel::push(void *data, double timeout)
+{
+    if (closed)
     {
-    {}  // namespace load_reporter
-}  // namespace grpc
+        return false;
+    }
+    if (is_full() || !producer_queue.empty())
+    {
+        timer_msg_t msg;
+        msg.error = false;
+        msg.timer = NULL;
+        if (timeout > 0)
+        {
+            long msec = (long) (timeout * 1000);
+            msg.chan = this;
+            msg.type = PRODUCER;
+            msg.co = Coroutine::get_current();
+            msg.timer = swTimer_add(&SwooleG.timer, msec, 0, &msg, timer_callback);
+        }
+    }
+    }
+    
+    void my_onConnect(swServer *serv, swDataHead *info)
+{
+    printf('PID=%d\tConnect fd=%d|from_id=%d\n', getpid(), info->fd, info->from_id);
+}
+    
+        swHashMap_del(hm, (char *) SW_STRL('willdel'));
+    swHashMap_update(hm, (char *) SW_STRL('willupadte'), (void *) (9999 * 5555));
