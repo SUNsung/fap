859925ba1db9c83ee8f3b6f796fa227abe919bea
SUNsung <weatherius@gmail.com>
2019-02-25 08:07:58 +0200

    Rewrite [291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp] File in: 2019-02-25 08:07:57

--- cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
+++ cccccc/291D0Imgs1k6Q29tQAVk8mfCDw29zVYFu00lDs629TDy.cpp
@@ -1,190 +1,119 @@
 
         
-        CacheImpl::ImplTy CacheImpl::create(StringRef Name, const CallBacks &CBs) {
-  return new DefaultCache(CBs);
+           void EmitMForm(const uint8_t op,
+                  const RegNumber rs,
+                  const RegNumber ra,
+                  const RegNumber rb,
+                  const uint8_t mb,
+                  const uint8_t me,
+                  const bool rc = 0) {
+    }
+    
+      if (ignore_default_configs) {
+    // Appending empty file to -c options to avoid loading defaults
+    ini_fd = get_tempfile_if_not_exists(ini_fd, ini_path);
+  } else {
+    // Should only include this default if not explicitly ignored.
+#ifdef PHP_DEFAULT_HDF
+    newargv.push_back('-c');
+    newargv.push_back(PHP_DEFAULT_HDF);
+#endif
+    }
+    
+    #include 'hphp/runtime/base/perf-warning.h'
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+        // Main loop
+    bool running = true;
+    while (running)
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        ALLEGRO_EVENT ev;
+        while (al_get_next_event(queue, &ev))
+        {
+            ImGui_ImplAllegro5_ProcessEvent(&ev);
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+                running = false;
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_RESIZE)
+            {
+                ImGui_ImplAllegro5_InvalidateDeviceObjects();
+                al_acknowledge_resize(display);
+                ImGui_ImplAllegro5_CreateDeviceObjects();
+            }
+        }
+    }
+    
+    
+    {        // Setup the debug report callback
+        VkDebugReportCallbackCreateInfoEXT debug_report_ci = {};
+        debug_report_ci.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+        debug_report_ci.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT | VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT;
+        debug_report_ci.pfnCallback = debug_report;
+        debug_report_ci.pUserData = NULL;
+        err = vkCreateDebugReportCallbackEXT(g_Instance, &debug_report_ci, g_Allocator, &g_DebugReport);
+        check_vk_result(err);
+#else
+        // Create Vulkan Instance without any debug feature
+        err = vkCreateInstance(&create_info, g_Allocator, &g_Instance);
+        check_vk_result(err);
+#endif
+    }
+    
+    
+    {    switch (msg)
+    {
+    case WM_SIZE:
+        if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+        {
+            CleanupRenderTarget();
+            g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+            CreateRenderTarget();
+        }
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
 }
     
-      assert(capacity % 16 == 0 && 'not allocating multiple of alignment');
-    
-    class TreePrinter {
-  llvm::raw_ostream &Out;
-  void (&PrintNodeData)(llvm::raw_ostream &out, void *node);
-  SmallString<40> Indent;
-public:
-  TreePrinter(llvm::raw_ostream &out,
-              void (&printNodeData)(llvm::raw_ostream &out, void *node))
-    : Out(out), PrintNodeData(printNodeData) {}
-    }
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
     
     
-    {  assert(*BufferPtr == '\r');
-  unsigned Bytes = 1;
-  if (BufferPtr != BufferEnd && *BufferPtr == '\n')
-    Bytes++;
-  return Bytes;
-}
-    
-      while ((!QueuedTasks.empty() && !SubtaskFailed) ||
-         !ExecutingTasks.empty()) {
-    // Enqueue additional tasks if we have additional tasks, we aren't already
-    // at the parallel limit, and no earlier subtasks have failed.
-    while (!SubtaskFailed && !QueuedTasks.empty() &&
-           ExecutingTasks.size() < MaxNumberOfParallelTasks) {
-      std::unique_ptr<DummyTask> T(QueuedTasks.front().release());
-      QueuedTasks.pop();
-    }
-    }
-    
-    const uint16_t swift::unicode::ExtendedGraphemeClusterNoBoundaryRulesMatrix[] = {
-% for row in get_extended_grapheme_cluster_rules_matrix(break_table):
-  ${row},
-% end
+    {  virtual bool finished() = 0;
 };
     
+    class DHTTask;
     
-    {    return name;
-  }
-    
-    #include 'ClangDiagnosticConsumer.h'
-#include 'ImporterImpl.h'
-#include 'swift/AST/ASTContext.h'
-#include 'swift/AST/DiagnosticEngine.h'
-#include 'swift/AST/DiagnosticsClangImporter.h'
-#include 'clang/AST/ASTContext.h'
-#include 'clang/Frontend/DiagnosticRenderer.h'
-#include 'clang/Frontend/FrontendDiagnostic.h'
-#include 'clang/Lex/LexDiagnostic.h'
-#include 'llvm/ADT/STLExtras.h'
-    
-    #define MAP_STDLIB_TYPE(C_TYPE_NAME, C_TYPE_KIND, C_TYPE_BITWIDTH,  \
-                        SWIFT_TYPE_NAME, CAN_BE_MISSING,            \
-                        C_NAME_MAPPING)                             \
-    MAP_TYPE(C_TYPE_NAME, C_TYPE_KIND, C_TYPE_BITWIDTH,             \
-             'Swift', SWIFT_TYPE_NAME, CAN_BE_MISSING,              \
-             C_NAME_MAPPING)
-    
-    namespace Ui {
-    class OpenURIDialog;
-}
-    
-    class TrafficGraphWidget : public QWidget
+    DHTTaskFactoryImpl::DHTTaskFactoryImpl()
+    : routingTable_(nullptr),
+      dispatcher_(nullptr),
+      factory_(nullptr),
+      taskQueue_(nullptr),
+      timeout_(DHT_MESSAGE_TIMEOUT)
 {
-    Q_OBJECT
-    }
+}
     
-    #ifndef SECP256K1_MODULE_ECDH_MAIN_H
-#define SECP256K1_MODULE_ECDH_MAIN_H
-    
-    #include 'include/secp256k1_recovery.h'
-    
-    BOOST_AUTO_TEST_CASE(bip173_testvectors_invalid)
+    void DHTTaskQueueImpl::addPeriodicTask1(const std::shared_ptr<DHTTask>& task)
 {
-    static const std::string CASES[] = {
-        ' 1nwldj5',
-        '\x7f''1axkwrx',
-        '\x80''1eym55h',
-        'an84characterslonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1569pvx',
-        'pzry9x0s0muk',
-        '1pzry9x0s0muk',
-        'x1b4n0q5v',
-        'li1dgmt3',
-        'de1lg7wt\xff',
-        'A1G7SGD8',
-        '10a06t8',
-        '1qzzfhee',
-        'a12UEL5L',
-        'A12uEL5L',
-    };
-    for (const std::string& str : CASES) {
-        auto ret = bech32::Decode(str);
-        BOOST_CHECK(ret.first.empty());
+  periodicTaskQueue1_.addTask(task);
+}
+    
+    
+    {} // namespace aria2
+    
+    namespace aria2 {
     }
-}
-    
-    #include <map>
-#include <string>
-#include <utility>
-    
-    const UniValue& UniValue::get_array() const
-{
-    if (typ != VARR)
-        throw std::runtime_error('JSON value is not an array as expected');
-    return *this;
-}
-    
-        UniValue v3;
-    BOOST_CHECK(v3.setNumStr('32482348723847471234'));
-    BOOST_CHECK_THROW(v3.get_int64(), std::runtime_error);
-    BOOST_CHECK(v3.setNumStr('1000'));
-    BOOST_CHECK_EQUAL(v3.get_int64(), 1000);
-    
-    /** Decode a Bech32 string. Returns (hrp, data). Empty hrp means failure. */
-std::pair<std::string, std::vector<uint8_t>> Decode(const std::string& str);
-    
-    
-    { protected:
-  /// The CAN client is started.
-  bool is_started_ = false;
-};
-    
-      /**
-   * @brief Receive messages
-   * @param frames The messages to receive.
-   * @param frame_num The amount of messages to receive.
-   * @return The status of the receiving action which is defined by
-   *         apollo::common::ErrorCode.
-   */
-  apollo::common::ErrorCode Receive(std::vector<CanFrame> *const frames,
-                                    int32_t *const frame_num) override;
-    
-    using apollo::common::ErrorCode;
-    
-    #endif  // MODULES_DRIVERS_CANBUS_CAN_CLIENT_CLIENT_HERMES_CAN_CLIENT_H
-    
-      // load conf
-  if (!common::util::GetProtoFromFile(FLAGS_sensor_conf_file, &canbus_conf_)) {
-    return OnError('Unable to load canbus conf file: ' +
-                   FLAGS_sensor_conf_file);
-  }
-    
-      void SwitchBlock(int block_x, int block_y,
-                   int factor_x, int factor_y) override;
-    
-    
-    {  // odd part
-  const int C1 = table[0];
-  const int C3 = table[2];
-  const int C5 = table[4];
-  const int C7 = table[6];
-  in[1] = DESCALE(C1 * b0 + C3 * b1 + C5 * b2 + C7 * b3);
-  in[3] = DESCALE(C3 * b0 - C7 * b1 - C1 * b2 - C5 * b3);
-  in[5] = DESCALE(C5 * b0 - C1 * b1 + C7 * b2 + C3 * b3);
-  in[7] = DESCALE(C7 * b0 - C5 * b1 + C3 * b2 - C1 * b3);
-}
-#undef DESCALE
-#undef LOAD_CST
-#undef LOAD
-#undef MULT
-#undef ADD
-#undef SUB
-#undef LSHIFT
-#undef STORE16
-#undef CORRECT_LSB
-#undef kTan1
-#undef kTan2
-#undef kTan3m1
-#undef k2Sqrt2
-#undef BUTTERFLY
-#undef COLUMN_DCT8
-    
-    #include 'guetzli/gamma_correct.h'
-    
-    namespace {
-    }
-    
-    #include <algorithm>
-#include <string.h>
-    
-    size_t HistogramEntropyCost(const JpegHistogram& histo,
-                            const uint8_t depths[256]);
-size_t HistogramHeaderCost(const JpegHistogram& histo);
