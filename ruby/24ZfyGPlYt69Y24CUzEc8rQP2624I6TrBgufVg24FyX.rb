4fe32d3820af7ea195db35627192760bb70829d3
SUNsung <weatherius@gmail.com>
2019-02-25 20:00:00 +0200

    Rewrite [24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb] File in: 2019-02-25 19:59:59

--- ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
+++ ruby/24ZfyGPlYt69Y24CUzEc8rQP2624I6TrBgufVg24FyX.rb
@@ -1,82 +1,64 @@
 
         
-          def remove_duplicates
-    where = 'WHERE s1.user_id = s2.user_id AND s1.shareable_id = s2.shareable_id AND '\
-      's1.shareable_type = s2.shareable_type AND s1.id > s2.id'
-    if AppConfig.postgres?
-      execute('DELETE FROM share_visibilities AS s1 USING share_visibilities AS s2 #{where}')
-    else
-      execute('DELETE s1 FROM share_visibilities s1, share_visibilities s2 #{where}')
+                # Reset topic count because we don't count the description topic
+        DB.exec 'UPDATE categories SET topic_count = 0 WHERE id = #{staff.id}'
+      end
     end
   end
 end
 
     
-      end
+            def representation_class
+          Representation::Issue
+        end
     
-      test 'remove page extentions' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'page', view.remove_page_extentions('page.wiki')
-    assert_equal 'page-wiki', view.remove_page_extentions('page-wiki.md')
-    assert_equal 'file.any_extention', view.remove_page_extentions('file.any_extention')
-  end
-    
-        # Test page_header_from_content(@content)
-    actual = @view.title
-    assert_equal '1 & 2', actual
-  end
-    
-    task :default => :test
-    
-          @CONF[:IRB_RC].call(irb.context) if @CONF[:IRB_RC]
-      @CONF[:MAIN_CONTEXT] = irb.context
-    
-        def not_found(msg = nil)
-      @message = msg || 'The requested page does not exist.'
-      status 404
-      return mustache :error
-    end
-    
-        it_behaves_like 'returns 404 HTTP status'
-  end
-    
-      shared_examples 'nested requested resources' do
-    it 'are returned' do
-      expect(json_response['included']).to be_present
-      expect(json_response['included']).not_to include(have_type('variant').and have_id(default_variant.id.to_s))
-      expect(json_response['included']).to include(have_type('option_type'))
-      expect(json_response['included']).to include(have_type('option_value'))
-    end
-  end
-    
-          context 'with existing order' do
-        it_behaves_like 'apply coupon code'
-      end
-    
-        context 'as a guest user with token' do
-      before { get '/api/v2/storefront/order_status/#{order.number}', headers: headers_order_token }
-    
-      shared_examples 'returns valid taxon resource JSON' do
-    it 'returns a valid taxon resource JSON response' do
-      expect(response.status).to eq(200)
-    
-              def resource_finder
-            Spree::Api::Dependencies.storefront_completed_order_finder.constantize
+                rows << {
+              label_id: label_id,
+              target_id: target_id,
+              target_type: issue.issuable_type,
+              created_at: time,
+              updated_at: time
+            }
           end
     
-          private
+      it 'truncates message to a reasonable length' do
+    log = AgentLog.new(:agent => agents(:jane_website_agent), :level => 3)
+    log.message = 'a' * 11_000
+    log.save!
+    expect(log.message.length).to eq(10_000)
+  end
     
-    desc 'clean the whole repository by removing all the generated files'
-task :clean do
-  rm_f  'Gemfile.lock'
-  rm_rf 'sandbox'
-  rm_rf 'pkg'
+        it 'should generate the correct specific tracking url' do
+      @checker.options['path'] = 'trackings/usps/9361289878905919630610'
+      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/trackings/usps/9361289878905919630610')
+    end
     
-              def find_spree_current_order
-            Spree::Api::Dependencies.storefront_current_order_finder.constantize.new.execute(
-              store: spree_current_store,
-              user: spree_current_user,
-              token: order_token,
-              currency: current_currency
-            )
-          end
+      it 'raises a TypeError when passed nil' do
+    lambda { sleep(nil)   }.should raise_error(TypeError)
+  end
+    
+      it 'raises ArgumentError if no block or proc is provided' do
+    lambda do
+      trace_var :$Kernel_trace_var_global
+    end.should raise_error(ArgumentError)
+  end
+end
+
+    
+      it 'creates a public method in TOPLEVEL_BINDING' do
+    eval @code, TOPLEVEL_BINDING
+    Object.should have_method :boom
+  end
+    
+          def react(env)
+        result = send(options[:reaction], env)
+        result if Array === result and result.size == 3
+      end
+    
+          def csp_policy
+        directives = []
+    
+      it 'accepts post form requests with masked authenticity_token field' do
+    post('/', {'authenticity_token' => masked_token}, 'rack.session' => session)
+    expect(last_response).to be_ok
+  end
