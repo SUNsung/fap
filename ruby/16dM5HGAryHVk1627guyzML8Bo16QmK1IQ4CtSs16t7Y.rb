3f0181b3e9598d4e36182b56469674779e097dac
SUNsung <weatherius@gmail.com>
2019-02-25 17:52:08 +0200

    Rewrite [16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb] File in: 2019-02-25 17:52:07

--- ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
+++ ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
@@ -1,8 +1,25 @@
 
         
-          require 'inch_by_inch/rake_task'
-  InchByInch::RakeTask.new
+        module ActionView #:nodoc:
+  # = Action View PathSet
+  #
+  # This class is used to store and access paths in Action View. A number of
+  # operations are defined so that you can search among the paths in this
+  # set and also perform operations on other +PathSet+ objects.
+  #
+  # A +LookupContext+ will use a +PathSet+ to store the paths in its context.
+  class PathSet #:nodoc:
+    include Enumerable
     
-          def plugins_string
-        UI::ErrorReport.plugins_string
+          def file_name
+        @template.identifier
+      end
+    
+          class Properties
+        def initialize
+          @properties = {}
+        end
+    
+          def servers_by_key
+        @servers_by_key ||= {}
       end
