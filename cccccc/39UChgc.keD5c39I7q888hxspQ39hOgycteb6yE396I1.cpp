28206a3abf156d32ba9884826966f030b897ec38
SUNsung <weatherius@gmail.com>
2019-02-25 15:01:48 +0200

    Rewrite [39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp] File in: 2019-02-25 15:01:47

--- cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
+++ cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
@@ -1,208 +1,90 @@
 
         
-            http://www.apache.org/licenses/LICENSE-2.0
-    
-    REGISTER_OP('Add').Doc(R'doc(
-An op to test that duplicate registrations don't override previously
-registered ops.
-)doc');
-    
-    namespace tensorflow {
+            UText **destText = (UText **)uprv_malloc(sizeof(UText*)*destCapacity);
+    if (destText == NULL) {
+        status = U_MEMORY_ALLOCATION_ERROR;
+        return 0;
+    }
+    int32_t i;
+    for (i = 0; i < destCapacity; i++) {
+        destText[i] = utext_openUnicodeString(NULL, &dest[i], &status);
     }
     
-        http://www.apache.org/licenses/LICENSE-2.0
+    #include 'unicode/utypes.h'
     
-    #include <functional>
-    
-    namespace tensorflow {
+    void SearchIterator::setMatchNotFound() 
+{
+    setMatchStart(USEARCH_DONE);
+    setMatchLength(0);
+    UErrorCode status = U_ZERO_ERROR;
+    // by default no errors should be returned here since offsets are within 
+    // range.
+    if (m_search_->isForwardSearching) {
+        setOffset(m_search_->textLength, status);
     }
-    
-    
-    {  tensorflow::DeviceNameUtils::ParsedName parsed_name;
-  if (!tensorflow::DeviceNameUtils::ParseFullName(node_def.device(),
-                                                  &parsed_name)) {
-    LOG(WARNING) << 'Failed to parse device from node_def: '
-                 << node_def.ShortDebugString();
-    return '';
-  }
-  string class_name = '';
-  tensorflow::FindKernelDef(tensorflow::DeviceType(parsed_name.type.c_str()),
-                            node_def, nullptr /* kernel_def */, &class_name)
-      .IgnoreError();
-  return class_name;
+    else {
+        setOffset(0, status);
+    }
 }
     
-    namespace stream_executor {
-namespace cuda {
-    }
-    }
+    #include 'unicode/messagepattern.h'
+#include 'unicode/rbnf.h'
+#include 'unicode/selfmt.h'
+#include 'unicode/uchar.h'
+#include 'unicode/ucnv_err.h'
+#include 'unicode/umsg.h'
+#include 'unicode/ustring.h'
+#include 'unicode/utypes.h'
+#include 'cmemory.h'
+#include 'messageimpl.h'
+#include 'patternprops.h'
+#include 'selfmtimpl.h'
+#include 'uassert.h'
+#include 'ustrfmt.h'
+#include 'util.h'
+#include 'uvector.h'
     
-    #include 'tensorflow/stream_executor/lib/stringprintf.h'
+    U_NAMESPACE_END
     
-      // Build a substitution map to replace the protocol's \c Self and the type
-  // parameters of the requirement into a combined context that provides the
-  // type parameters of the conformance context and the parameters of the
-  // requirement.
-  auto selfType = cast<GenericTypeParamType>(
-      proto->getSelfInterfaceType()->getCanonicalType());
+    #ifndef __SHARED_BREAKITERATOR_H__
+#define __SHARED_BREAKITERATOR_H__
     
-        if (node->Left) {
-      IndentScope ms(this, (childKind == ChildKind::Left ||
-                            childKind == ChildKind::Root) ? '  ' : '| ');
-      print(node->Left, ChildKind::Left);
-    }
+    #ifndef __SHARED_DATEFORMATSYMBOLS_H__
+#define __SHARED_DATEFORMATSYMBOLS_H__
     
-      IsFirstLine = true;
-  for (auto &Line : Lines) {
-    if (!IsFirstLine) {
-      Line = Line.drop_front(WhitespaceToTrim);
-    }
-    IsFirstLine = false;
-  }
-    
-    #include 'swift/Demangling/Demangle.h'
-#include 'swift/Demangling/Demangler.h'
-#include <cstdio>
-    
-    
-    
-    class b2Triangle{
-public:
-	float* x;
-    float* y;
-	b2Triangle();
-	b2Triangle(float32 x1, float32 y1, float32 x2, float32 y2, float32 x3, float32 y3);
-	~b2Triangle();
-	bool IsInside(float32 _x, float32 _y);
-	void Set(const b2Triangle& toMe);
+        /**
+     * Make this instance have no limit on significant digits.
+     */
+    void clear() {
+        fMin = 0;
+        fMax = INT32_MAX;
     }
     
-    
-    {		*a_piEncodingTime_ms = totalEncodingTime;
-	}
-    
-    /** 'RAW' macros, should not be used outside of this header file */
-#define SHR(a,shift) ((a) >> (shift))
-#define SHL(a,shift) SHL32(a,shift)
-#define PSHR(a,shift) (SHR((a)+((EXTEND32(1)<<((shift))>>1)),shift))
-#define SATURATE(x,a) (((x)>(a) ? (a) : (x)<-(a) ? -(a) : (x)))
-    
-    #if DMLC_ENABLE_STD_THREAD
-/*!
- * \brief A threaded writer to write sparse batch page to sharded files.
- */
-class SparsePageWriter {
- public:
-  /*!
-   * \brief constructor
-   * \param name_shards name of shard files.
-   * \param format_shards format of each shard.
-   * \param extra_buffer_capacity Extra buffer capacity before block.
-   */
-  explicit SparsePageWriter(
-      const std::vector<std::string>& name_shards,
-      const std::vector<std::string>& format_shards,
-      size_t extra_buffer_capacity);
-  /*! \brief destructor, will close the files automatically */
-  ~SparsePageWriter();
-  /*!
-   * \brief Push a write job to the writer.
-   * This function won't block,
-   * writing is done by another thread inside writer.
-   * \param page The page to be written
-   */
-  void PushWrite(std::shared_ptr<SparsePage>&& page);
-  /*!
-   * \brief Allocate a page to store results.
-   *  This function can block when the writer is too slow and buffer pages
-   *  have not yet been recycled.
-   * \param out_page Used to store the allocated pages.
-   */
-  void Alloc(std::shared_ptr<SparsePage>* out_page);
-    }
-    
-    #include <dmlc/omp.h>
-#include <xgboost/logging.h>
-#include <algorithm>
-#include '../common/math.h'
-    
-    
-    {
-    {}  // namespace common
-}  // namespace xgboost
-
-    
-      // Computes the score (negative loss) resulting from performing this split
-  virtual bst_float ComputeSplitScore(bst_uint nodeid,
-                                      bst_uint featureid,
-                                      const GradStats& left_stats,
-                                      const GradStats& right_stats,
-                                      bst_float left_weight,
-                                      bst_float right_weight) const = 0;
-    
-    
-    {
-    {} // namespace ev2
-} // namespace osquery
-
-    
-    #pragma once
-    
-    /**
- * @brief Basic publisher implementation.
- *
- * @details This is a basic publisher implementation which supports basic
- * subscription registering and event forwarding. The publisher will simply
- * forward events to all registered subscriptions. It will also keep the event
- * id counter for producers to rely on.
- */
-template <typename SubscriptionT>
-class SimplePublisher : public Publisher {
- public:
-  /**
-   * @brief SimplePublisher constructor.
-   *
-   * @param name Publisher name to be passed to ev2::Publisher::Publisher().
-   */
-  SimplePublisher(const std::string name) : Publisher(std::move(name)) {}
-  virtual ~SimplePublisher() = default;
-    }
-    
-    
-    {
-    {} // namespace ev2
-} // namespace osquery
-
-    
-    std::size_t NullSubscription::avail() const {
-  return 0;
+    int32_t
+IntDigitCountRange::pin(int32_t digitCount) const {
+    return digitCount < fMin ? fMin : (digitCount < fMax ? digitCount : fMax);
 }
     
-    /**
- * @brief A vector of column names associated with a query
- *
- * ColumnNames is a vector of the column names, in order, returned by a query.
- */
-using ColumnNames = std::vector<std::string>;
+    std::unique_ptr<WriteControllerToken> WriteController::GetDelayToken(
+    uint64_t write_rate) {
+  total_delayed_++;
+  // Reset counters.
+  last_refill_time_ = 0;
+  bytes_left_ = 0;
+  set_delayed_write_rate(write_rate);
+  return std::unique_ptr<WriteControllerToken>(new DelayWriteToken(this));
+}
     
-    class UsersTest : public testing::Test {
- protected:
-  void SetUp() override {
-    setUpEnvironment();
-  }
+    struct PosixMemoryMappedFileBuffer : public MemoryMappedFileBuffer {
+  PosixMemoryMappedFileBuffer(void* _base, size_t _length)
+      : MemoryMappedFileBuffer(_base, _length) {}
+  virtual ~PosixMemoryMappedFileBuffer();
 };
     
-      ExtensionManager_getQueryColumns_result result;
-  try {
-    iface_->getQueryColumns(result.success, args.sql);
-    result.__isset.success = true;
-  } catch (const std::exception& e) {
-    if (this->eventHandler_.get() != NULL) {
-      this->eventHandler_->handlerError(ctx, 'ExtensionManager.getQueryColumns');
-    }
-    }
+      const char* Name() const override { return 'MyFilter'; }
     
-      void deregisterExtension(ExtensionStatus& _return, const ExtensionRouteUUID uuid) {
-    // Your implementation goes here
-    printf('deregisterExtension\n');
-  }
+    
+    { private:
+  uint32_t number_success_backup;
+  uint32_t number_fail_backup;
+};
