6bfa34cbe833a6d6525181b074a5ff30c9252faf
SUNsung <weatherius@gmail.com>
2019-02-26 15:01:20 +0200

    Rewrite [58pLtkO6DQE1.58GJaeFKyNgIU58QECAKeleyE58LMz.cpp] File in: 2019-02-26 15:01:18

--- cccccc/58pLtkO6DQE1.58GJaeFKyNgIU58QECAKeleyE58LMz.cpp
+++ cccccc/58pLtkO6DQE1.58GJaeFKyNgIU58QECAKeleyE58LMz.cpp
@@ -1,182 +1,96 @@
 
         
-            TableBuilder* builder = new TableBuilder(options, file);
-    meta->smallest.DecodeFrom(iter->key());
-    for (; iter->Valid(); iter->Next()) {
-      Slice key = iter->key();
-      meta->largest.DecodeFrom(key);
-      builder->Add(key, iter->value());
+        int InternalKeyComparator::Compare(const Slice& akey, const Slice& bkey) const {
+  // Order by:
+  //    increasing user key (according to user-supplied comparator)
+  //    decreasing sequence number
+  //    decreasing type (though sequence# should be enough to disambiguate)
+  int r = user_comparator_->Compare(ExtractUserKey(akey), ExtractUserKey(bkey));
+  if (r == 0) {
+    const uint64_t anum = DecodeFixed64(akey.data() + akey.size() - 8);
+    const uint64_t bnum = DecodeFixed64(bkey.data() + bkey.size() - 8);
+    if (anum > bnum) {
+      r = -1;
+    } else if (anum < bnum) {
+      r = +1;
     }
-    
-    namespace leveldb {
-    }
-    
-    void InternalKeyComparator::FindShortestSeparator(
-      std::string* start,
-      const Slice& limit) const {
-  // Attempt to shorten the user portion of the key
-  Slice user_start = ExtractUserKey(*start);
-  Slice user_limit = ExtractUserKey(limit);
-  std::string tmp(user_start.data(), user_start.size());
-  user_comparator_->FindShortestSeparator(&tmp, user_limit);
-  if (tmp.size() < user_start.size() &&
-      user_comparator_->Compare(user_start, tmp) < 0) {
-    // User key has become shorter physically, but larger logically.
-    // Tack on the earliest possible number to the shortened user key.
-    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
-    assert(this->Compare(*start, tmp) < 0);
-    assert(this->Compare(tmp, limit) < 0);
-    start->swap(tmp);
   }
+  return r;
+}
+    
+    std::string LogFileName(const std::string& dbname, uint64_t number) {
+  assert(number > 0);
+  return MakeFileName(dbname, number, 'log');
+}
+    
+    class Repairer {
+ public:
+  Repairer(const std::string& dbname, const Options& options)
+      : dbname_(dbname),
+        env_(options.env),
+        icmp_(options.comparator),
+        ipolicy_(options.filter_policy),
+        options_(SanitizeOptions(dbname, &icmp_, &ipolicy_, options)),
+        owns_info_log_(options_.info_log != options.info_log),
+        owns_cache_(options_.block_cache != options.block_cache),
+        next_file_number_(1) {
+    // TableCache can be small since we expect each table to be opened once.
+    table_cache_ = new TableCache(dbname_, options_, 10);
+  }
+    }
+    
+      // Our data structure does not allow duplicate insertion
+  assert(x == nullptr || !Equal(key, x->key));
+    
+      static bool isInitialized6() { return data6_.initialized; }
+    
+      virtual const std::string& getType() const CXX11_OVERRIDE;
+    
+      // header
+  readBytes(fp, buf, buf.size(), 8);
+  if (memcmp(header, buf, 8) == 0) {
+    version = 3;
+  }
+  else if (memcmp(headerCompat, buf, 8) == 0) {
+    version = 2;
+  }
+  else {
+    throw DL_ABORT_EX(fmt('Failed to load DHT routing table from %s. cause:%s',
+                          filename.c_str(), 'bad header'));
+  }
+    
+      const std::vector<std::shared_ptr<DHTNode>>& getNodes() const
+  {
+    return nodes_;
+  }
+    
+    DHTSetup::DHTSetup() = default;
+    
+    DHTTaskExecutor::~DHTTaskExecutor() = default;
+    
+    
+    {  size_t getQueueSize() const { return queue_.size(); }
+};
+    
+    namespace aria2 {
+    }
+    
+    DHTTaskQueueImpl::DHTTaskQueueImpl()
+    : periodicTaskQueue1_(NUM_CONCURRENT_TASK),
+      periodicTaskQueue2_(NUM_CONCURRENT_TASK),
+      immediateTaskQueue_(NUM_CONCURRENT_TASK)
+{
+}
+    
+    #include 'DHTTaskQueue.h'
+#include 'DHTTaskExecutor.h'
+    
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port) const
+{
+  return generateToken(infoHash, ipaddr, port, secret_[0]);
 }
     
     
-    {}  // namespace leveldb
-    
-      // Compute the crc of the record type and the payload.
-  uint32_t crc = crc32c::Extend(type_crc_[t], ptr, n);
-  crc = crc32c::Mask(crc);                 // Adjust for storage
-  EncodeFixed32(buf, crc);
-    
-      Status OpenWithStatus(Options* options = nullptr) {
-    Close();
-    Options opts;
-    if (options != nullptr) {
-      opts = *options;
-    } else {
-      opts.reuse_logs = true;  // TODO(sanjay): test both ways
-      opts.create_if_missing = true;
-    }
-    if (opts.env == nullptr) {
-      opts.env = env_;
-    }
-    return DB::Open(opts, dbname_, &db_);
-  }
-    
-    
-    {    // Do not record a version edit for this conversion to a Table
-    // since ExtractMetaData() will also generate edits.
-    FileMetaData meta;
-    meta.number = next_file_number_++;
-    Iterator* iter = mem->NewIterator();
-    status = BuildTable(dbname_, env_, options_, table_cache_, iter, &meta);
-    delete iter;
-    mem->Unref();
-    mem = nullptr;
-    if (status.ok()) {
-      if (meta.file_size > 0) {
-        table_numbers_.push_back(meta.number);
-      }
-    }
-    Log(options_.info_log, 'Log #%llu: %d ops saved to Table #%llu %s',
-        (unsigned long long) log,
-        counter,
-        (unsigned long long) meta.number,
-        status.ToString().c_str());
-    return status;
-  }
-    
-      // Removes a SnapshotImpl from this list.
-  //
-  // The snapshot must have been created by calling New() on this list.
-  //
-  // The snapshot pointer should not be const, because its memory is
-  // deallocated. However, that would force us to change DB::ReleaseSnapshot(),
-  // which is in the API, and currently takes a const Snapshot.
-  void Delete(const SnapshotImpl* snapshot) {
-#if !defined(NDEBUG)
-    assert(snapshot->list_ == this);
-#endif  // !defined(NDEBUG)
-    snapshot->prev_->next_ = snapshot->next_;
-    snapshot->next_->prev_ = snapshot->prev_;
-    delete snapshot;
-  }
-    
-      void EmitXX2Form(const uint8_t op,
-                   const RegNumber t,
-                   const uint8_t uim,
-                   const RegNumber b,
-                   const uint16_t xo,
-                   const bool bx,
-                   const bool tx)  {
-    XX2_form_t xx2_formater {{
-      tx,
-      bx,
-      xo,
-      static_cast<uint32_t>(b),
-      static_cast<uint32_t>(uim & 0x3),
-      static_cast<uint32_t>(t),
-      op
-    }};
-    dword(xx2_formater.instruction);
-  }
-    
-    struct ObjectData;
-struct Object;
-    
-      const char* data = filename.data();
-  int data_len = filename.length();
-  bool base64 = false;
-  if (strncmp(data, 'data:', sizeof('data:') - 1)) {
-    return nullptr;
-  }
-  data += sizeof('data:') - 1;
-  data_len -= sizeof('data:') - 1;
-    
-    #ifndef HPHP_DATA_STREAM_WRAPPER_H
-#define HPHP_DATA_STREAM_WRAPPER_H
-    
-    int emulate_zend(int argc, char** argv) {
-  std::vector<std::string> newargv;
-    }
-    
-    
-    {    if (isPHP == php) {
-      callback(spath + ename, false);
-    }
-  }
-    
-    #include 'hphp/runtime/base/runtime-option.h'
-    
-    
-    {}
-    
-        // change element with key 'the bad'
-    object.at('the bad') = 'il cattivo';
-    
-    #include 'modules/drivers/canbus/can_client/socket/socket_can_client_raw.h'
-    
-    TEST(ByteTest, SetBit) {
-  unsigned char byte_value = 0xFF;
-  Byte value(&byte_value);
-  value.set_bit_0(1);
-  EXPECT_EQ(0xFD, value.get_byte());
-  value.set_bit_0(7);
-  EXPECT_EQ(0x7D, value.get_byte());
-  value.set_bit_1(7);
-  EXPECT_EQ(0xFD, value.get_byte());
-  value.set_value(0x77);
-  value.set_bit_1(0);
-  EXPECT_EQ(0x77, value.get_byte());
-    }
-    
-    #include 'modules/drivers/radar/conti_radar/protocol/object_quality_info_60c.h'
-#include 'modules/drivers/radar/conti_radar/protocol/const_vars.h'
-    
-    Eigen::MatrixXd SplineSegKernel::Kernel(const uint32_t num_params,
-                                        const double accumulated_x) {
-  if (num_params > reserved_order_ + 1) {
-    CalculateFx(num_params);
-  }
-  Eigen::MatrixXd term_matrix;
-  IntegratedTermMatrix(num_params, accumulated_x, 'fx', &term_matrix);
-  return kernel_fx_.block(0, 0, num_params, num_params)
-      .cwiseProduct(term_matrix);
-}
-    
-    namespace apollo {
-namespace routing {
-    }
-    }
-    
-    #include 'gtest/gtest.h'
+    {} // namespace aria2
