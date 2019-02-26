194d17f3d95eb85823f562db767f43bbc51e5755
SUNsung <weatherius@gmail.com>
2019-02-26 10:38:10 +0200

    Rewrite [04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb] File in: 2019-02-26 10:38:09

--- ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
+++ ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
@@ -1,35 +1,35 @@
 
         
-        namespace :test do
-  desc 'Run the ruby tests (without sass-spec)'
-  Rake::TestTask.new('ruby') do |t|
-    t.libs << 'test'
-    test_files = FileList[scope('test/**/*_test.rb')]
-    test_files.exclude(scope('test/rails/*'))
-    test_files.exclude(scope('test/plugins/*'))
-    t.test_files = test_files
-    t.warning = true
-    t.verbose = true
+          it 'accepts a Bignum as a seed' do
+    srand(0x12345678901234567890)
+    srand.should == 0x12345678901234567890
   end
     
-            line_num = e.sass_line + 1 - line_offset
-        min = [line_num - 6, 0].max
-        section = e.sass_template.rstrip.split('\n')[min...line_num + 5]
-        return e.sass_backtrace_str if section.nil? || section.empty?
+      ruby_version_is '2.6' do
+    it 'raises RuntimeError when `exception: true` is given and the command exits with a non-zero exit status' do
+      lambda { @object.system(ruby_cmd('exit 1'), exception: true) }.should raise_error(RuntimeError)
+    end
     
-    Given /^I run a paperclip generator to add a paperclip '([^']*)' to the '([^']*)' model$/ do |attachment_name, model_name|
-  step %[I successfully run `rails generate paperclip #{model_name} #{attachment_name}`]
+      caveats <<~EOS
+    Installation or Uninstallation may fail with Exit Code 19 (Conflicting Processes running) if Browsers, Safari Notification Service or SIMBL Services (e.g. Flashlight) are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Logs in /Library/Logs/Adobe/Installers if Installation or Uninstallation fails, to identifify the conflicting processes.
+  EOS
 end
+
     
-        def define_getters
-      define_instance_getter
-      define_class_getter
-    end
+      desc 'update main and version in bower.json'
+  task :generate do
+    require 'bootstrap-sass'
+    Dir.chdir Bootstrap.gem_path do
+      spec       = JSON.parse(File.read 'bower.json')
     
-        # Returns the dot+extension of the file. e.g. '.jpg' for 'file.jpg'
-    # If the style has a format defined, it will return the format instead
-    # of the actual extension. If the extension is empty, no dot is added.
-    def dotextension attachment, style_name
-      ext = extension(attachment, style_name)
-      ext.empty? ? ext : '.#{ext}'
-    end
+      test 'edit pages within sub-directories' do
+    post '/create', :content => 'big smelly creatures', :page => 'Orc',
+         :path               => 'Mordor', :format => 'markdown', :message => 'oooh, scary'
+    
+      s.test_files = s.files.select { |path| path =~ /^test\/test_.*\.rb/ }
+end
+
+    
+    # Set ruby to UTF-8 mode
+# This is required for Ruby 1.8.7 which gollum still supports.
+$KCODE = 'U' if RUBY_VERSION[0, 3] == '1.8'
