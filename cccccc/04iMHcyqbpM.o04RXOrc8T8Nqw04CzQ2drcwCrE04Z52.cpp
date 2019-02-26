144f8e66a08738e878e20650cc41a0c5f11a3b09
SUNsung <weatherius@gmail.com>
2019-02-26 17:41:34 +0200

    Rewrite [04iMHcyqbpM.o04RXOrc8T8Nqw04CzQ2drcwCrE04Z52.cpp] File in: 2019-02-26 17:41:33

--- cccccc/04iMHcyqbpM.o04RXOrc8T8Nqw04CzQ2drcwCrE04Z52.cpp
+++ cccccc/04iMHcyqbpM.o04RXOrc8T8Nqw04CzQ2drcwCrE04Z52.cpp
@@ -1,243 +1,229 @@
 
-    {} // namespace CAROTENE_NS
-
+        
+                    sgdUpdateNoise.TransferToDeviceIfNotThere(parameterMatrix->GetDeviceId(), true);
     
-    
+        size_t NDMask::MaskedCount() const
     {
-    {        for (; j < size.width; j++)
+        auto maskMatrix = GetMatrix();
+        std::unique_ptr<char[]> maskData(maskMatrix->CopyToArray());
+        return std::count_if(maskData.get(), maskData.get() + maskMatrix->GetNumElements(), [](const char& val) {
+            return val == (char)MaskKind::Invalid;
+        });
+    }
+    
+            ValuePtr DeepClone(bool readOnly) const override
         {
-            dst[j] = ~src[j];
+            if (m_isPacked)
+            {
+                std::shared_ptr<Microsoft::MSR::CNTK::MBLayout> packedLayoutCopy;
+                if (m_packedDataLayout)
+                {
+                    packedLayoutCopy = std::make_shared<Microsoft::MSR::CNTK::MBLayout>();
+                    packedLayoutCopy->CopyFrom(m_packedDataLayout);
+                }
+                return MakeSharedObject<PackedValue>(m_sampleShape, m_sampleDynamicAxes, m_packedData->DeepClone(readOnly), packedLayoutCopy, readOnly);
+            }
+            else
+                return Value::DeepClone(readOnly);
+        }
+    
+    
+    {        assert(m_fd == -1);
+        for (;;)
+        {
+            // opening a lock file
+            int fd = open(m_fileName.c_str(), O_WRONLY | O_CREAT, 0666);
+            if (fd < 0)
+                RuntimeError('Acquire: Failed to open lock file %s: %s.', m_fileName.c_str(), strerror(errno));
+            // locking it with the fcntl API
+            memset(&m_lock, 0, sizeof(m_lock));
+            m_lock.l_type = F_WRLCK;
+            // BUG: fcntl call with F_SETLKW doesn't always reliably detect when lock is released
+            // As a workaround, using alarm() for interupting fcntl if it waits more than 1 second
+            setupTimeout(1);
+            int r = fcntl(fd, wait ? F_SETLKW : F_SETLK, &m_lock);
+            if (errno == EINTR)
+            {
+                sleep(1);
+                // retrying in the case of signal or timeout
+                close(fd);
+                continue;
+            }
+            if (r != 0)
+            {
+                // acquire failed
+                close(fd);
+                umask(mask);
+                return false;
+            }
+            // we own the exclusive lock on file descriptor, but we need to double-check
+            // that the lock file wasn't deleted and/or re-created;
+            // checking this by comparing inode numbers
+            struct stat before, after;
+            fstat(fd, &before);
+            if (stat(m_fileName.c_str(), &after) != 0 || before.st_ino != after.st_ino)
+            {
+                // we have a race with 'unlink' call in Release()
+                // our lock is held to the previous instance of the file;
+                // this is not a problem, we just need to retry locking the new file
+                close(fd);
+                continue;
+            }
+            else
+            {
+                // lock acquired successfully
+                m_fd = fd;
+                umask(mask);
+                return true;
+            }
         }
     }
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
+    
+        virtual void /*ComputationNodeBase::*/ Validate(bool isFinalValidationPass) override
+    {
+        ReadOutVariable(); // read out the value once, with the purpose of validating the variableName
+        Base::Validate(isFinalValidationPass);
+        // this node does not hold mini-batch data
+        m_pMBLayout = nullptr;
+        // for now, anything this node returns is a scalar
+        SetDims(TensorShape(1), false);
+    }
+    
+      /*! \brief return corresponding element set given the node_id */
+  inline const Elem& operator[](unsigned node_id) const {
+    const Elem& e = elem_of_each_node_[node_id];
+    CHECK(e.begin != nullptr)
+        << 'access element that is not in the set';
+    return e;
+  }
+  // clear up things
+  inline void Clear() {
+    row_indices_.clear();
+    elem_of_each_node_.clear();
+  }
+  // initialize node id 0->everything
+  inline void Init() {
+    CHECK_EQ(elem_of_each_node_.size(), 0U);
+    }
+    
+    void SparsePageWriter::PushWrite(std::shared_ptr<SparsePage>&& page) {
+  qworkers_[clock_ptr_].Push(std::move(page));
+  clock_ptr_ = (clock_ptr_ + 1) % workers_.size();
 }
     
-    template <>
-inline void NormCanny<true>(const ptrdiff_t colscn, s16* _dx, s16* _dy, s32* _norm)
-{
-    ptrdiff_t j = 0;
-    if (colscn >= 8)
-    {
-        int16x8_t vx = vld1q_s16(_dx);
-        int16x8_t vy = vld1q_s16(_dy);
-    }
-    }
-    
-    /*!
- *  Aligns pointer by the certain number of bytes
- *
- *  This small inline function aligns the pointer by the certain number of bytes by shifting
- *  it forward by 0 or a positive offset.
- */
-template<typename T> inline T* alignPtr(T* ptr, size_t n=sizeof(T))
-{
-    return (T*)(((size_t)ptr + n-1) & -n);
+    SEXP XGBoosterPredict_R(SEXP handle, SEXP dmat, SEXP option_mask, SEXP ntree_limit) {
+  SEXP ret;
+  R_API_BEGIN();
+  bst_ulong olen;
+  const float *res;
+  CHECK_CALL(XGBoosterPredict(R_ExternalPtrAddr(handle),
+                            R_ExternalPtrAddr(dmat),
+                            asInteger(option_mask),
+                            asInteger(ntree_limit),
+                            &olen, &res));
+  ret = PROTECT(allocVector(REALSXP, olen));
+  for (size_t i = 0; i < olen; ++i) {
+    REAL(ret)[i] = res[i];
+  }
+  R_API_END();
+  UNPROTECT(1);
+  return ret;
 }
     
-        for (size_t i = 0; i < size.height; ++i)
+            for(int i = 0 ; i < nums.size() ; i ++)
+            for(int j = i + 1 ; j < nums.size() ; j ++)
+                if(nums[i] + nums[j] == target){
+                    int res[] = {i, j};
+                    return vector<int>(res, res + 2);
+                }
+    
+    
+    {    return;
+}
+    
+    
+void printBool(bool res){
+    cout << (res ? 'True' : 'False') << endl;
+}
+    
+    using namespace std;
+    
+    
+    {    return 0;
+}
+
+    
+    #include <iostream>
+#include <vector>
+#include <stack>
+    
+    
+    
+    
+    {    if (follow && valid)
     {
-        const u8 * src = internal::getRowPtr(srcBase, srcStride, i);
-        s16 * dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t j = 0;
+        follow->autorelease();
+        return follow;
     }
     
-                v_dst.val[0] = vcombine(vget_high(v_src.val[0]), vget_low(v_src.val[0]));
-            v_dst.val[1] = vcombine(vget_high(v_src.val[1]), vget_low(v_src.val[1]));
-            v_dst.val[2] = vcombine(vget_high(v_src.val[2]), vget_low(v_src.val[2]));
+    delete follow;
+    return nullptr;
     
-            // do vertical convolution
-        ptrdiff_t x = 0;
-        const ptrdiff_t bcols = y + 3 < rows ? cols : (cols - 8);
-        for( ; x <= bcols; x += 8 )
+}
+Follow* Follow::clone() const
+{
+    // no copy constructor
+    return Follow::createWithOffset(_followedNode, _offsetX,_offsetY,_worldRect);
+    
+}
+    
+    CardinalSplineTo* CardinalSplineTo::create(float duration, PointArray *points, float tension)
+{
+    CardinalSplineTo *ret = new (std::nothrow) CardinalSplineTo();
+    if (ret)
+    {
+        if (ret->initWithDuration(duration, points, tension))
         {
-            internal::prefetch(v0 + x);
-            internal::prefetch(v1 + x);
-            internal::prefetch(v2 + x);
-            internal::prefetch(v3 + x);
-            internal::prefetch(v4 + x);
-    }
-    
-    REGISTER_GRADIENT(Sub, GetSubGradient);
-    
-    template <>
-template <typename T>
-bool EnforceFiniteOp<CPUContext>::DoRunWithType() {
-  EnforceOnCPU<T>(Input(0));
-  return true;
-}
-    
-    
-    {} // namespace caffe2
-    
-    Contrast Example 2 with Example 1. For each data point per feature, the values
-are sorted by the corresponding KEY.
-)DOC')
-    .Input(0, 'DATA', 'Tensor of rank 1.')
-    .Input(
-        1,
-        'RANGES',
-        'Tensor of int32/int64 ranges, of dims (N, M, 2). '
-        'Where N is number of examples and M is a size of each example. '
-        'Last dimention represents a range in the format (start, lengths)')
-    .Input(2, 'KEY', 'Tensor of rank 1 and type int64.')
-    .Output(0, 'OUTPUT', '1-D tensor of size sum of range lengths')
-    .Arg('lengths', 'Expected lengths for ranges')
-    .TensorInferenceFunction([](const OperatorDef& def,
-                                const vector<TensorShape>& in) {
-      ArgumentHelper helper(def);
-      auto lengths = helper.GetRepeatedArgument<int>('lengths');
-      CAFFE_ENFORCE_EQ(in[0].dims_size(), 1, 'DATA should be 1-D tensor.');
-      CAFFE_ENFORCE_EQ(in[1].dims_size(), 3, 'RANGES should be 3-D tensor.');
-      if (in.size() > 2) {
-        CAFFE_ENFORCE_EQ(in[2].dims_size(), 1, 'KEY should be 1-D tensor.');
-      }
-      CAFFE_ENFORCE_GT(lengths.size(), 0, 'lengths should be non-empty.');
-      std::vector<TensorShape> out(lengths.size());
-      for (int i = 0; i < lengths.size(); ++i) {
-        out[i].set_data_type(in[0].data_type());
-        out[i].add_dims(in[1].dims(0));
-        out[i].add_dims(lengths[i]);
-      }
-      return out;
-    });
-    
-    OPERATOR_SCHEMA(Glu)
-    .NumInputs(1)
-    .NumOutputs(1)
-    .SetDoc(R'DOC(
-Applies gated linear unit to the input Tensor X. The output Y is half the size
-of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
-[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
-X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
-)DOC')
-    .Input(0, 'X', '1D input tensor')
-    .Output(0, 'Y', '1D output tensor');
-    
-              const int dkernel_h = dilation_h * (kernel_h - 1) + 1;
-          const int dkernel_w = dilation_w * (kernel_w - 1) + 1;
-          CAFFE_ENFORCE(H >= dkernel_h);
-          CAFFE_ENFORCE(W >= dkernel_w);
-          const int out_h = (H + 2 * pad - dkernel_h) / stride_h + 1;
-          const int out_w = (W + 2 * pad - dkernel_w) / stride_w + 1;
-    
-      /*
-   * \brief Set up caffeOp_ to infer weights & output shape
-   * \brief Initialize param_'s in & out dims
-   */
-  bool InferShape(std::vector<TShape> *in_shape,
-                  std::vector<TShape> *out_shape,
-                  std::vector<TShape> *aux_shape) const override {
-    if (caffeOp_ == NULL)
-      caffeOp_ = caffe::LayerRegistry<float>::CreateLayer(param_.prototxt);
-    using namespace mshadow;
-    using ::caffe::Blob;
-    using std::vector;
-    CHECK_GE(in_shape->size(), param_.num_data);
-    // Initialize emtryp bottom & top blobs for caffeop
-    vector<Blob<float> *> bot_blobs, top_blobs;
-    }
-    
-    /*!
- * Copyright (c) 2016 by Contributors
- * \file inplace_addto_detect_pass.cc
- * \brief Detect whether inplace addto operation is possible for certain op.
- */
-#include <mxnet/base.h>
-#include <mxnet/operator.h>
-#include <mxnet/op_attr_types.h>
-#include <nnvm/graph_attr_types.h>
-    
-    #include <dmlc/registry.h>
-    
-    namespace mxnet {
-namespace io {
-// iterator on image recordio
-class PrefetcherIter : public IIterator<DataBatch> {
- public:
-  explicit PrefetcherIter(IIterator<TBlobBatch>* base)
-      : loader_(base), out_(nullptr) {}
-    }
+            ret->autorelease();
+        }
+        else 
+        {
+            CC_SAFE_RELEASE_NULL(ret);
+        }
     }
     }
     
-    TEST_F(UsersTest, test_sanity) {
-  auto const rows = execute_query('select * from users');
-  ASSERT_GE(rows.size(), 1ul); // There must be at least one user
-  auto row_map = ValidatatioMap{
-      {'uid', NonNegativeInt},
-      {'uid_signed', IntType},
-      {'gid_signed', IntType},
-      {'description', NormalType},
-      {'shell', NonEmptyString},
-  };
-  if (isPlatform(PlatformType::TYPE_WINDOWS)) {
-    row_map.emplace('gid', IntType);
-    row_map.emplace('username', NormalType);
-  } else {
-    row_map.emplace('gid', NonNegativeInt);
-    row_map.emplace('username', NonEmptyString);
-  }
-  if (isPlatform(PlatformType::TYPE_WINDOWS)) {
-    row_map.emplace('directory', NormalType);
-  } else {
-    row_map.emplace('directory', NonEmptyString);
-  }
-  if (isPlatform(PlatformType::TYPE_WINDOWS)) {
-    row_map.emplace('type', NormalType);
-  }
-  if (isPlatform(PlatformType::TYPE_OSX)) {
-    row_map.emplace('uuid', ValidUUID);
-    row_map.emplace('is_hidden', IntType);
-  } else {
-    row_map.emplace('uuid', NormalType);
-  }
-  validate_rows(rows, row_map);
-}
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
     
-    namespace rj = rapidjson;
+        /**
+    @brief Get the amplitude of the effect.
+    @return Return the amplitude of the effect.
+    */
+    float getAmplitude() const { return _amplitude; }
+    /**
+    @brief Set the amplitude to the effect.
+    @param amplitude The value of amplitude will be set.
+    */
+    void setAmplitude(float amplitude) { _amplitude = amplitude; }
     
-    namespace osquery {
-    }
+            if ( frames.empty() )
+        {
+            CCLOG('cocos2d: AnimationCache: None of the frames for animation '%s' were found in the SpriteFrameCache. Animation is not being added to the Animation Cache.', anim.first.c_str());
+            continue;
+        }
+        else if ( frames.size() != frameNameSize )
+        {
+            CCLOG('cocos2d: AnimationCache: An animation in your dictionary refers to a frame which is not in the SpriteFrameCache. Some or all of the frames for the animation '%s' may be missing.', anim.first.c_str());
+        }
     
-    
-    {  return Status();
-}
-    
-    namespace osquery { namespace extensions {
-    }
-    }
-    
-    std::ostream& operator<<(std::ostream& out, const ExtensionCode::type& val) {
-  std::map<int, const char*>::const_iterator it = _ExtensionCode_VALUES_TO_NAMES.find(val);
-  if (it != _ExtensionCode_VALUES_TO_NAMES.end()) {
-    out << it->second;
-  } else {
-    out << static_cast<int>(val);
-  }
-  return out;
-}
-    
-    std::pair<JSON, QueryLogItem> getSerializedQueryLogItem() {
-  std::pair<JSON, QueryLogItem> p;
-  QueryLogItem i;
-  JSON doc = JSON::newObject();
-  auto dr = getSerializedDiffResults();
-  i.results = std::move(dr.second);
-  i.name = 'foobar';
-  i.calendar_time = 'Mon Aug 25 12:10:57 2014';
-  i.time = 1408993857;
-  i.identifier = 'foobaz';
-  i.epoch = 0L;
-  i.counter = 0L;
-    }
-    
-    // getSerializedRowColumnNames returns a vector of test column names that
-// are in alphabetical order. If unordered_and_repeated is true, the
-// vector includes a repeated column name and is in non-alphabetical order
-ColumnNames getSerializedRowColumnNames(bool unordered_and_repeated);
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
