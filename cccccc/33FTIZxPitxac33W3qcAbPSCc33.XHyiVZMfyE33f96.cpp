57ebebd1ed99b794e57f08779f48ed2745124474
SUNsung <weatherius@gmail.com>
2019-02-26 04:53:23 +0200

    Rewrite [33FTIZxPitxac33W3qcAbPSCc33.XHyiVZMfyE33f96.cpp] File in: 2019-02-26 04:53:23

--- cccccc/33FTIZxPitxac33W3qcAbPSCc33.XHyiVZMfyE33f96.cpp
+++ cccccc/33FTIZxPitxac33W3qcAbPSCc33.XHyiVZMfyE33f96.cpp
@@ -1,129 +1,223 @@
-std::string tmppath() {
-  // TMPFILE is for manual test execution during which the user will specify
-  // the full temp file path using the environmental variable TMPFILE
-  const char* tmpfile = getenv('TMPFILE');
-  if (tmpfile) {
-    return std::string(tmpfile);
-  }
-    }
-    
-    template <>
-template <typename T>
-bool EnforceFiniteOp<CPUContext>::DoRunWithType() {
-  EnforceOnCPU<T>(Input(0));
-  return true;
-}
-    
-        for (int inputIdx = 0; inputIdx < def_.input_size() / 2; ++inputIdx) {
-      input_blob_names.push_back(I(inputIdx * 2 + 1));
-      output_blob_names.push_back(GI(inputIdx * 2));
-    }
-    input_blob_names.push_back(GO(2));
-    
-    - https://github.com/caffe2/caffe2/blob/master/caffe2/operators/find_duplicate_elements_op.h
-- https://github.com/caffe2/caffe2/blob/master/caffe2/operators/find_duplicate_elements_op.cc
-    
-    
-    {} // namespace caffe2
-    
-    OPERATOR_SCHEMA(GivenTensorFill)
-    .NumInputs(0, 1)
-    .NumOutputs(1)
-    .AllowInplace({{0, 0}})
-    .SetDoc(R'DOC(
-This op fills an output tensor with the data specified by the *value* and *dtype* arguments.  The output tensor shape is specified by the *shape* argument. Beware, when using this argument *value* should have a value for every element of the *output*, as missing values will not be initialized automatically. If *input_as_shape* is set to *true*, then the *input* should be a 1D tensor containing the desired output shape (the dimensions specified in *extra_shape* will also be appended). In this case, the *shape* argument should **not** be set.
-    
-    
-    {REGISTER_CPU_OPERATOR(Glu, GluOp<float, CPUContext>);
-} // namespace caffe2
 
-    
-    #include 'DHTAbstractMessage.h'
-#include 'A2STR.h'
-#include 'ValueBase.h'
-    
-      uint32_t temp32;
-  uint64_t temp64;
-  // time
-  if (version == 2) {
-    READ_CHECK(fp, &temp32, sizeof(temp32));
-    serializedTime_.setTimeFromEpoch(ntohl(temp32));
-    // 4bytes reserved
-    readBytes(fp, buf, buf.size(), 4);
-  }
-  else {
-    READ_CHECK(fp, &temp64, sizeof(temp64));
-    serializedTime_.setTimeFromEpoch(ntoh64(temp64));
+        
+          inline int offset(const int n, const int c = 0, const int h = 0,
+      const int w = 0) const {
+    CHECK_GE(n, 0);
+    CHECK_LE(n, num());
+    CHECK_GE(channels(), 0);
+    CHECK_LE(c, channels());
+    CHECK_GE(height(), 0);
+    CHECK_LE(h, height());
+    CHECK_GE(width(), 0);
+    CHECK_LE(w, width());
+    return ((n * channels() + c) * height() + h) * width() + w;
   }
     
-    namespace aria2 {
+      /// @brief The spatial dimensions of a filter kernel.
+  Blob<int> kernel_shape_;
+  /// @brief The spatial dimensions of the stride.
+  Blob<int> stride_;
+  /// @brief The spatial dimensions of the padding.
+  Blob<int> pad_;
+  /// @brief The spatial dimensions of the dilation.
+  Blob<int> dilation_;
+  /// @brief The spatial dimensions of the convolution input.
+  Blob<int> conv_input_shape_;
+  /// @brief The spatial dimensions of the col_buffer.
+  vector<int> col_buffer_shape_;
+  /// @brief The spatial dimensions of the output.
+  vector<int> output_shape_;
+  const vector<int>* bottom_shape_;
+    
+    
+    {}  // namespace caffe
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    
+    {}  // namespace caffe
+    
+    #include 'caffe/layers/conv_layer.hpp'
+    
+    
+    {}  // namespace caffe
+    
+    #include <vector>
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    namespace caffe {
     }
     
-    class DHTTask;
-    
-      virtual std::shared_ptr<DHTTask>
-  createNodeLookupTask(const unsigned char* targetID) = 0;
-    
-    #endif // D_DHT_TASK_QUEUE_H
-
-    
-    namespace aria2 {
+    namespace caffe {
     }
     
-        head = Solution().swapPairs(head);
-    printLinkedList(head);
+    enum GARBAGE_LEVEL
+{
+  G_NEVER_CRUNCH,
+  G_OK,
+  G_DODGY,
+  G_TERRIBLE
+};
     
-    /// Two pointers
-/// Time Complexity:  O(n)
-/// Space Complexity: O(1)
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-    }
-    }
+      WERD_RES *word2 = new WERD_RES(*word);
+    
+    /**********************************************************************
+ * read_unlv_file
+ *
+ * Read a whole unlv zone file to make a list of blocks.
+ **********************************************************************/
+    
+    class BLOCK;
+class WERD;
     
     
-    {
-    {        int zero = -1;          // [0...zero] == 0
-        int two = nums.size();  // [two...n-1] == 2
-        for(int i = 0 ; i < two ; ){
-            if(nums[i] == 1)
-                i ++;
-            else if (nums[i] == 2)
-                swap( nums[i] , nums[--two]);
-            else{ // nums[i] == 0
-                assert(nums[i] == 0);
-                swap(nums[++zero] , nums[i++]);
-            }
+    {    ICOORD pt;
+    int halfwidth;
+  };
+  // Type holds the distance of each point from the fitted line and the point
+  // itself. Use of double allows integer distances from ICOORDs to be stored
+  // exactly, and also the floating point results from ConstrainedFit.
+  using DistPointPair = KDPairInc<double, ICOORD>;
+    
+      // Writes to the given file. Returns false in case of error.
+  bool Serialize(FILE* fp) const;
+  // Reads from the given file. Returns false in case of error.
+  // If swap is true, assumes a big/little-endian swap is needed.
+  bool DeSerialize(bool swap, FILE* fp);
+    
+            auto newTensorShape = AsTensorViewShape(newShape);
+        void* tensorView = nullptr;
+        switch (m_dataType)
+        {
+        case DataType::Float:
+            tensorView = new TensorView<float>(*(GetTensorView<float>()), newTensorShape);
+            break;
+        case DataType::Double:
+            tensorView = new TensorView<double>(*(GetTensorView<double>()), newTensorShape);
+            break;
+        case DataType::Float16:
+            tensorView = new TensorView<half>(*(GetTensorView<half>()), newTensorShape);
+            break;
+        case DataType::Int8:
+            tensorView = new TensorView<char>(*(GetTensorView<char>()), newTensorShape);
+            break;
+        case DataType::Int16:
+            tensorView = new TensorView<short>(*(GetTensorView<short>()), newTensorShape);
+            break;
+        default:
+            LogicError('NDArrayView::AsShape: Unsupported DataType %s', DataTypeName(m_dataType));
+            break;
+        }
+    
+    
+    {        const auto& type = dict[typeKey].Value<std::wstring>();
+        if (type != typeValue) 
+        {
+            const auto& version = GetVersion(dict);
+            LogicError('Unexpected '%ls':'%ls' in place of '%ls':'%ls' (%s).',
+                       typeKey.c_str(), type.c_str(), typeKey.c_str(), typeValue.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
         }
     }
-};
     
-    /// Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-    
-    public:
-    vector<int> inorderTraversal(TreeNode* root) {
-    }
-    
-            vector<int> res;
-        if(root == NULL)
-            return res;
-    
-    /// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-    
-    
+        // Releases the mutex
+    void Release()
     {
-    {        return res;
+        assert(m_handle != NULL);
+        int rc = 0;
+        rc = ::ReleaseMutex(m_handle);
+        if ((rc == RELEASEMUTEX_ERROR) && !std::uncaught_exception())
+        {
+            RuntimeError('Mutex Release: Failed to release mutex %s: %d', m_name.c_str(), ::GetLastError());
+        }
+        rc = ::CloseHandle(m_handle);
+        if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
+        {
+            RuntimeError('Mutex Release: Failed to close handler %s: %d', m_name.c_str(), ::GetLastError());
+        }
+        m_handle = NULL;
     }
-};
+    
+        // this returns the map directly (read-only) and will lazily initialize it for a given seed
+    const std::vector<INDEXTYPE>& operator()(size_t seed) // throw()
+    {
+        // if wrong seed then lazily recache the sequence
+        if (seed != currentseed && randomizationrange != randomizeDisable)
+        {
+            // test for numeric overflow
+            if (map.size() - 1 != (INDEXTYPE)(map.size() - 1))
+                RuntimeError('RandomOrdering: INDEXTYPE has too few bits for this corpus');
+            // 0, 1, 2...
+            foreach_index (t, map)
+                map[t] = (INDEXTYPE) t;
+    }
+    }
+    
+      // construct column matrix from GHistIndexMatrix
+  inline void Init(const GHistIndexMatrix& gmat,
+                double  sparse_threshold) {
+    const auto nfeature = static_cast<bst_uint>(gmat.cut.row_ptr.size() - 1);
+    const size_t nrow = gmat.row_ptr.size() - 1;
+    }
+    
+    TEST(Expected, CoroutineSuccess) {
+  auto r0 = []() -> Expected<int, Err> {
+    auto x = co_await f1();
+    EXPECT_EQ(7, x);
+    auto y = co_await f2(x);
+    EXPECT_EQ(2.0 * 7, y);
+    auto z = co_await f3(x, y);
+    EXPECT_EQ(int(2.0 * 7 + 7), *z);
+    co_return* z;
+  }();
+  EXPECT_TRUE(r0.hasValue());
+  EXPECT_EQ(21, *r0);
+}
+    
+    
+    {} // namespace folly
+    
+    fbstring exceptionStr(exception_wrapper const& ew) {
+  return ew.what();
+}
+    
+      /**
+   * Returns true 1/n of the time. If n == 0, always returns false
+   */
+  static bool secureOneIn(uint32_t n) {
+    SecureRNG<uint32_t> srng;
+    return rand32(0, n, srng) == 0;
+  }
+    
+    namespace folly {
+    }
+    
+    
+    {    ts.tv_sec = 0;
+    ts.tv_nsec = std::numeric_limits<int64_t>::max();
+    EXPECT_EQ(std::numeric_limits<int64_t>::max(), to<nsec_i64>(ts).count());
+    ts.tv_sec = 1;
+    ts.tv_nsec = std::numeric_limits<int64_t>::max() - std::nano::den;
+    EXPECT_EQ(std::numeric_limits<int64_t>::max(), to<nsec_i64>(ts).count());
+    ts.tv_sec = 1;
+    ts.tv_nsec = std::numeric_limits<int64_t>::max() - std::nano::den + 1;
+    EXPECT_THROW(to<nsec_i64>(ts), std::range_error);
+  }
+    
+    template <typename T>
+typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type
+prefixToStringLE(T prefix, uint64_t n = sizeof(T)) {
+  DCHECK_GT(n, 0);
+  DCHECK_LE(n, sizeof(T));
+  prefix = Endian::little(prefix);
+  std::string result;
+  result.resize(n);
+  memcpy(&result[0], &prefix, n);
+  return result;
+}
