abe9c5517ea789f5b084a70893bbb4871dcce261
SUNsung <weatherius@gmail.com>
2019-02-26 19:38:04 +0200

    Rewrite [02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py] File in: 2019-02-26 19:38:03

--- pyton/02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py
+++ pyton/02uplQct5W2OA02TV8C1AERDwc02QPCUkhdlGqM02eDD.py
@@ -1,64 +1,121 @@
 
         
-            # Instantiate model.
-    model = Model(inputs=inputs, outputs=outputs)
-    return model
+        rnn_a = generate_rnn(rnn_rngs[0], N, FLAGS.g, FLAGS.tau, FLAGS.dt,
+                     FLAGS.max_firing_rate)
+rnn_b = generate_rnn(rnn_rngs[1], N, FLAGS.g, FLAGS.tau, FLAGS.dt,
+                     FLAGS.max_firing_rate)
+rnns = [rnn_a, rnn_b]
     
-    from ..utils.data_utils import get_file
-import numpy as np
-    
-        def __call__(self, x):
-        regularization = 0.
-        if self.l1:
-            regularization += K.sum(self.l1 * K.abs(x))
-        if self.l2:
-            regularization += K.sum(self.l2 * K.square(x))
-        return regularization
-    
-    
-def test_decode_predictions():
-    x = np.zeros((2, 1000))
-    x[0, 372] = 1.0
-    x[1, 549] = 1.0
-    outs = utils.decode_predictions(x, top=1)
-    scores = [out[0][2] for out in outs]
-    assert scores[0] == scores[1]
+      '''
+  loss = tf.losses.sigmoid_cross_entropy(labels,
+                                         predictions,
+                                         weights=missing_tokens)
+  loss = tf.Print(
+      loss, [loss, labels, missing_tokens],
+      message='loss, labels, missing_tokens',
+      summarize=25,
+      first_n=25)
+  return loss
     
     
-def test_imdb():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = imdb.load_data()
-        (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=40)
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        word_index = imdb.get_word_index()
-        assert isinstance(word_index, dict)
+def convert_and_zip(id_to_word, sequences, predictions):
+  '''Helper function for printing or logging.  Retrieves list of sequences
+  and predictions and zips them together.
+  '''
+  indices = convert_to_indices(sequences)
     
-    import keras
-from keras import losses
-from keras import backend as K
-from keras.utils.generic_utils import custom_object_scope
     
-        model = create_multi_input_model_from(dummy_model, dummy_model)
-    model.compile(loss='categorical_crossentropy', optimizer='sgd')
-    assert len(model.losses) == 6
+def create_reinforce_gen_train_op(hparams, learning_rate, final_gen_reward,
+                                  averages_op, global_step):
+  '''Create the Generator train_op when using REINFORCE.
     
-    print('Pad sequences (samples x time)')
-x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
-x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
-y_train = np.array(y_train)
-y_test = np.array(y_test)
+    filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
+build_dir = os.path.join('..', '..', 'build', version)
+for key, filename in filenames.items():
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    fn = os.path.join(build_dir, filename)
+    with open(fn, 'rb') as f:
+        data = f.read()
+    if not data:
+        raise ValueError('File %s is empty!' % fn)
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
     
-    train_model.compile(optimizer=keras.optimizers.RMSprop(lr=2e-3, decay=1e-5),
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'],
-                    target_tensors=[targets])
-train_model.summary()
     
-    def print_title(title, pattern = '-'):
-    print('\n'.join(('', title, pattern * len(title)))) 
+# And add them to TestDownload
+for n, test_case in enumerate(defs):
+    tname = 'test_' + str(test_case['name'])
+    i = 1
+    while hasattr(TestDownload, tname):
+        tname = 'test_%s_%d' % (test_case['name'], i)
+        i += 1
+    test_method = generator(test_case, tname)
+    test_method.__name__ = str(tname)
+    ie_list = test_case.get('add_ie')
+    test_method.add_ie = ie_list and ','.join(ie_list)
+    setattr(TestDownload, test_method.__name__, test_method)
+    del test_method
+    
+        def _make_request(self):
+        # Weirdly enough, this doesn't seem to require authentication
+        data = [{
+            'request': {
+                'sinceRevision': 0
+            },
+            'action': 'http://linksys.com/jnap/devicelist/GetDevices'
+        }]
+        headers = {'X-JNAP-Action': 'http://linksys.com/jnap/core/Transaction'}
+        return requests.post('http://{}/JNAP/'.format(self.host),
+                             timeout=DEFAULT_TIMEOUT,
+                             headers=headers,
+                             json=data)
+
+    
+        return scanner if scanner.success_init else None
+    
+        def send_message(self, message='', **kwargs):
+        '''Send a message to a user.'''
+        from ciscosparkapi import SparkApiError
+        try:
+            title = ''
+            if kwargs.get(ATTR_TITLE) is not None:
+                title = kwargs.get(ATTR_TITLE) + ': '
+            self._spark.messages.create(roomId=self._default_room,
+                                        text=title + message)
+        except SparkApiError as api_error:
+            _LOGGER.error('Could not send CiscoSpark notification. Error: %s',
+                          api_error)
+
+    
+    _LOGGER = logging.getLogger(__name__)
+    
+        def _get_room(self, room):
+        '''Get Room object, creating it if necessary.'''
+        from hipnotify import Room
+        if room not in self._rooms:
+            self._rooms[room] = Room(
+                token=self._token, room_id=room, endpoint_url=self._host)
+        return self._rooms[room]
+    
+    
+if __name__ == '__main__':
+    print('Before subclassing: ')
+    for k in RegistryHolder.REGISTRY:
+        print(k)
+    
+    *TL;DR80
+Delays the eval of an expr until its value is needed and avoids repeated evals.
+'''
+    
+        data = Data()
+    
+        front_controller.dispatch_request(Request('desktop'))
+    front_controller.dispatch_request('mobile')
+    
+        def show_item_information(self, item_type, item_name, item_info):
+        '''Will look for item information by iterating over key,value pairs
+        yielded by item_info.items()'''
+        raise NotImplementedError
