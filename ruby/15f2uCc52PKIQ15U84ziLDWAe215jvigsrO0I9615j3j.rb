9cda9007e8052ee317eecf421d6147332fd0099e
SUNsung <weatherius@gmail.com>
2019-02-28 01:12:07 +0200

    Rewrite [15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb] File in: 2019-02-28 01:12:06

--- ruby/15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb
+++ ruby/15f2uCc52PKIQ15U84ziLDWAe215jvigsrO0I9615j3j.rb
@@ -1,112 +1,222 @@
 
         
-            t.wakeup
-    t.value.should == 5
+            def lanes
+      @lanes ||= {}
+    end
+    
+          it 'adds docset_feed_name param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            docset_feed_name: 'DocSet feed name'
+          )
+        end').runner.execute(:test)
+    
+            expect(result.size).to eq(4)
+        expect(result[0]).to eq('security create-keychain -p testpassword ~/Library/Keychains/test.keychain')
+    
+              result = Fastlane::FastFile.new.parse('lane :test do
+            swiftlint(
+              strict: true,
+              executable: '#{CUSTOM_EXECUTABLE_NAME}'
+            )
+          end').runner.execute(:test)
+    
+          if data_type == Array
+        return value.split(',') if value.kind_of?(String)
+      elsif data_type == Integer
+        return value.to_i if value.to_i.to_s == value.to_s
+      elsif data_type == Float
+        return value.to_f if value.to_f.to_s == value.to_s
+      elsif allow_shell_conversion
+        return value.shelljoin if value.kind_of?(Array)
+        return value.map { |k, v| '#{k.to_s.shellescape}=#{v.shellescape}' }.join(' ') if value.kind_of?(Hash)
+      elsif data_type != String
+        # Special treatment if the user specified true, false or YES, NO
+        # There is no boolean type, so we just do it here
+        if %w(YES yes true TRUE).include?(value)
+          return true
+        elsif %w(NO no false FALSE).include?(value)
+          return false
+        end
+      end
+    
+    # The * turns the array into a parameter list
+# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
+# We need to use that, because otherwise invocations like
+# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
+exec(*exec_arr)
+
+    
+        def initialize(machine, guests, capabilities)
+      @capabilities = capabilities
+      @guests       = guests
+      @machine      = machine
+    end
+    
+            # Executes a command and returns true if the command succeeded,
+        # and false otherwise. By default, this executes as a normal user,
+        # and it is up to the communicator implementation if they expose an
+        # option for running tests as an administrator.
+        #
+        # @see #execute
+        def test(command, opts=nil)
+        end
+      end
+    end
+  end
+end
+
+    
+            # Allows setting options from a hash. By default this simply calls
+        # the `#{key}=` method on the config class with the value, which is
+        # the expected behavior most of the time.
+        #
+        # This is expected to mutate itself.
+        #
+        # @param [Hash] options A hash of options to set on this configuration
+        #   key.
+        def set_options(options)
+          options.each do |key, value|
+            send('#{key}=', value)
+          end
+        end
+    
+            # This is called early, before a machine is instantiated, to check
+        # if this provider is usable. This should return true or false.
+        #
+        # If raise_error is true, then instead of returning false, this
+        # should raise an error with a helpful message about why this
+        # provider cannot be used.
+        #
+        # @param [Boolean] raise_error If true, raise exception if not usable.
+        # @return [Boolean]
+        def self.usable?(raise_error=false)
+          # Return true by default for backwards compat since this was
+          # introduced long after providers were being written.
+          true
+        end
+    
+            it 'does not leave a space at the start of negative numbers' do
+          format('% b', -10).should == '-1010'
+          format('% B', -10).should == '-1010'
+          format('% d', -112).should == '-112'
+          format('% i', -112).should == '-112'
+          format('% o', -87).should == '-127'
+          format('% u', -112).should == '-112'
+          format('% x', -196).should == '-c4'
+          format('% X', -196).should == '-C4'
+    
+      it 'raises a TypeError when passed a String' do
+    lambda { srand('7') }.should raise_error(TypeError)
   end
 end
     
-    describe 'main#define_method' do
-  before :each do
-    @code = 'define_method(:boom) { :bam }'
+      platform_is :windows do
+    it 'runs commands starting with any number of @ using shell' do
+      `#{ruby_cmd('p system 'does_not_exist'')} 2>NUL`.chomp.should == 'nil'
+      @object.system('@does_not_exist 2>NUL').should == false
+      @object.system('@@@#{ruby_cmd('exit 0')}').should == true
+    end
   end
+end
     
-    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
+        $Kernel_trace_var_global = 'foo'
+    
+    require 'bundler/cli'
+require 'bundler/friendly_errors'
+    
+    if $0 == __FILE__
+  begin
+    LogStash::PluginManager::Main.run('bin/logstash-plugin', ARGV)
+  rescue LogStash::PluginManager::Error => e
+    $stderr.puts(e.message)
+    exit(1)
+  end
+end
 
     
-        def get_web_content(url)
-      raw_uri           = URI.parse url
-      proxy             = ENV['http_proxy']
-      if proxy
-        proxy_uri       = URI.parse(proxy)
-        https           = Net::HTTP::Proxy(proxy_uri.host, proxy_uri.port).new raw_uri.host, raw_uri.port
-      else
-        https           = Net::HTTP.new raw_uri.host, raw_uri.port
+        desc 'Bootstrap all the VM's used for this tests'
+    task :setup, :platform do |t, args|
+      config   = PlatformConfig.new
+      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
+      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
+    
+        context 'with a specific plugin' do
+      let(:plugin_name) { 'logstash-input-stdin' }
+      it 'list the plugin and display the plugin name' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
       end
-      https.use_ssl     = true
-      https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      request           = Net::HTTP::Get.new raw_uri.request_uri
-      data              = https.request request
+    
+    require_relative 'lib/spree/core/version.rb'
+    
+    desc 'Generates a dummy app for testing for every Spree engine'
+task :test_app do
+  SPREE_GEMS.each do |gem_name|
+    Dir.chdir('#{File.dirname(__FILE__)}/#{gem_name}') do
+      sh 'rake test_app'
     end
   end
+end
     
-      # Used on the blog index to split posts on the <!--more--> marker
-  def excerpt(input)
-    if input.index(/<!--\s*more\s*-->/i)
-      input.split(/<!--\s*more\s*-->/i)[0]
-    else
-      input
+            def address_params
+          params.require(:address).permit(permitted_address_attributes)
+        end
+    
+              unless inventory_unit.respond_to?(can_event) &&
+              inventory_unit.send(can_event)
+            render plain: { exception: 'cannot transition to #{@event}' }.to_json,
+                   status: 200
+            false
+          end
+        end
+    
+        while readable_io = IO.select([self_read])
+      signal = readable_io.first[0].gets.strip
+      handle_signal(launcher, signal)
     end
-  end
+  #end
+rescue SystemExit => e
+  #Sidekiq.logger.error('Profiling...')
+  #result = RubyProf.stop
+  #printer = RubyProf::GraphHtmlPrinter.new(result)
+  #printer.print(File.new('output.html', 'w'), :min_percent => 1)
+  # normal
+rescue => e
+  raise e if $DEBUG
+  STDERR.puts e.message
+  STDERR.puts e.backtrace.join('\n')
+  exit 1
+end
+
     
-      def migration_name
-    'add_attachment_#{attachment_names.join('_')}_to_#{name.underscore.pluralize}'
-  end
-    
-        def raise_if_blank_file
-      if path.blank?
-        raise Errors::NotIdentifiedByImageMagickError.new('Cannot find the geometry of a file with a blank name')
+        ##
+    # Define client-side middleware:
+    #
+    #   client = Sidekiq::Client.new
+    #   client.middleware do |chain|
+    #     chain.use MyClientMiddleware
+    #   end
+    #   client.push('class' => 'SomeWorker', 'args' => [1,2,3])
+    #
+    # All client instances default to the globally-defined
+    # Sidekiq.client_middleware but you can change as necessary.
+    #
+    def middleware(&block)
+      @chain ||= Sidekiq.client_middleware
+      if block_given?
+        @chain = @chain.dup
+        yield @chain
       end
+      @chain
     end
     
-    # Example for adding special attributes
-package.attributes[:deb_group] = 'super-useful'
-package.attributes[:rpm_group] = 'super-useful'
+        def fire_event(event, options={})
+      reverse = options[:reverse]
+      reraise = options[:reraise]
     
-        script_errors = []
-    setscript = proc do |scriptname|
-      # 'self.send(scriptname) == self.before_install == --before-install
-      # Gets the path to the script
-      path = self.send(scriptname)
-      # Skip scripts not set
-      next if path.nil?
-    
-    # A directory package.
-#
-# This class supports both input and output. As a note, 'output' will
-# only emit the files, not any metadata. This is an effective way
-# to extract another package type.
-class FPM::Package::Dir < FPM::Package
-  private
-    
-        self.description = info['description']
-    # Supposedly you can upload a package for npm with no author/author email
-    # so I'm being safer with this. Author can also be a hash or a string
-    self.vendor = 'Unknown <unknown@unknown.unknown>'
-    if info.include?('author')
-      author_info = info['author']
-      # If a hash, assemble into a string
-      if author_info.respond_to? :fetch
-        self.vendor = sprintf('%s <%s>', author_info.fetch('name', 'unknown'),
-                              author_info.fetch('email', 'unknown@unknown.unknown'))
-      else
-        # Probably will need a better check for validity here
-        self.vendor = author_info unless author_info == ''
-      end
-    end
-    
-      private
-  # return the identifier by prepending the reverse-domain prefix
-  # to the package name, else return just the name
-  def identifier
-    identifier = name.dup
-    if self.attributes[:osxpkg_identifier_prefix]
-      identifier.insert(0, '#{self.attributes[:osxpkg_identifier_prefix]}.')
-    end
-    identifier
-  end # def identifier
-    
-        return @architecture
-  end # def architecture
-    
-        realpath = Pathname.new(input_path).realpath.to_s
-    ::Dir.chdir(build_path) do
-      safesystem('unzip', realpath)
-    end
-    
-        task(options.name) do |_, task_args|
-      block.call(*[options, task_args].first(block.arity)) if block_given?
-      abort('Must specify args') unless options.respond_to?(:args)
-      @args = options.delete_field(:args)
-      run_cli
-    end
-  end
+        def _erb(file, locals)
+      locals.each {|k, v| define_singleton_method(k){ v } unless (singleton_methods.include? k)} if locals
