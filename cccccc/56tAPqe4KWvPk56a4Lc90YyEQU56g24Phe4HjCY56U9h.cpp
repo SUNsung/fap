efa571699a858744cb67c66e5a388ab05760996f
SUNsung <weatherius@gmail.com>
2019-02-28 09:59:12 +0200

    Rewrite [56tAPqe4KWvPk56a4Lc90YyEQU56g24Phe4HjCY56U9h.cpp] File in: 2019-02-28 09:59:11

--- cccccc/56tAPqe4KWvPk56a4Lc90YyEQU56g24Phe4HjCY56U9h.cpp
+++ cccccc/56tAPqe4KWvPk56a4Lc90YyEQU56g24Phe4HjCY56U9h.cpp
@@ -1,216 +1,105 @@
 
         
-        namespace atom {
-    }
+          // The d'tor restores the previous test part result reporter.
+  virtual ~ScopedFakeTestPartResultReporter();
     
-    
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(Net);
-};
-    
-    #if defined(OS_LINUX)
-void PowerMonitor::BlockShutdown() {
-  PowerObserverLinux::BlockShutdown();
-}
-    
-    #include 'atom/browser/api/trackable_object.h'
-#include 'atom/browser/lib/power_observer.h'
-#include 'base/compiler_specific.h'
-#include 'native_mate/handle.h'
-#include 'ui/base/idle/idle.h'
-    
-    
-    {}  // namespace atom
-    
-      gfx::Point GetCursorScreenPoint();
-  display::Display GetPrimaryDisplay();
-  std::vector<display::Display> GetAllDisplays();
-  display::Display GetDisplayNearestPoint(const gfx::Point& point);
-  display::Display GetDisplayMatching(const gfx::Rect& match_rect);
-    
-    namespace mate {
-    }
-    
-    #include 'atom/browser/atom_quota_permission_context.h'
-    
-    #endif  // ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
-
-    
-      // There is an update available and it is being downloaded
-  virtual void OnUpdateAvailable() {}
-    
-    namespace grpc {
-    }
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
-    
-    bool ProtoServerReflectionPlugin::has_sync_methods() const {
-  if (reflection_service_) {
-    return reflection_service_->has_synchronous_methods();
+      // C'tor.  TestPartResult does NOT have a default constructor.
+  // Always use this constructor (with parameters) to create a
+  // TestPartResult object.
+  TestPartResult(Type a_type,
+                 const char* a_file_name,
+                 int a_line_number,
+                 const char* a_message)
+      : type_(a_type),
+        file_name_(a_file_name == NULL ? '' : a_file_name),
+        line_number_(a_line_number),
+        summary_(ExtractSummary(a_message)),
+        message_(a_message) {
   }
-  return false;
+    
+    // INTERNAL IMPLEMENTATION - DO NOT USE IN USER CODE.
+//
+// Expands to the name of the variable used to remember the names of
+// the defined tests in the given test case.
+# define GTEST_TYPED_TEST_CASE_P_STATE_(TestCaseName) \
+  gtest_typed_test_case_p_state_##TestCaseName##_
+    
+    // Internal macro for implementing {EXPECT|ASSERT}_PRED3.  Don't use
+// this in your code.
+#define GTEST_PRED3_(pred, v1, v2, v3, on_failure)\
+  GTEST_ASSERT_(::testing::AssertPred3Helper(#pred, \
+                                             #v1, \
+                                             #v2, \
+                                             #v3, \
+                                             pred, \
+                                             v1, \
+                                             v2, \
+                                             v3), on_failure)
+    
+    // Skips to the first non-space char after the first comma in 'str';
+// returns NULL if no comma is found in 'str'.
+inline const char* SkipComma(const char* str) {
+  const char* comma = strchr(str, ',');
+  if (comma == NULL) {
+    return NULL;
+  }
+  while (IsSpace(*(++comma))) {}
+  return comma;
 }
     
-    #ifndef GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
-#define GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
-    
-    #endif
+    #endif  // GTEST_SAMPLES_PRIME_TABLES_H_
 
+    
+    // A sample program demonstrating using Google C++ testing framework.
+//
+// Author: wan@google.com (Zhanyong Wan)
+    
+      static int allocated() { return allocated_; }
+    
+    // Sets the 0-terminated C string this MyString object
+// represents.
+void MyString::Set(const char* a_c_string) {
+  // Makes sure this works when c_string == c_string_
+  const char* const temp = MyString::CloneCString(a_c_string);
+  delete[] c_string_;
+  c_string_ = temp;
+}
+
+    
+            int length = 0;
+        for(ListNode* cur = dummyHead->next ; cur != NULL ; cur = cur->next)
+            length ++;
     
     
     {
-    {}}
-    
-         // GP Register cannot be greater than 31
-     assert(static_cast<uint32_t>(rb) < 32);
-     assert(static_cast<uint32_t>(ra) < 32);
-     assert(static_cast<uint32_t>(rb) < 32);
-    
-        if (!semi) {
-      // only media type (type/subtype,data)
-      ssize_t media_len = comma - data;
-      meta_len -= media_len;
-      data += media_len;
-    } else if (slash && slash < semi) {
-      // media type + param (type/subtype;param,data)
-      ssize_t media_len = semi - data;
-      meta_len -= media_len;
-      data += media_len;
-    } else {
-      // no media type (;base64,data)
-      if (semi != data // ex. foo;base64,data
-          || meta_len != sizeof(';base64') - 1 // ex. ;something,data
-          || memcmp(data, ';base64',
-                    sizeof(';base64') - 1)) { // ex. ;base65,data
-          raise_warning('rfc2397: invalid meta data');
-          return nullptr;
-        }
+    {        return true;
     }
+};
     
-    #ifndef HPHP_DATA_STREAM_WRAPPER_H
-#define HPHP_DATA_STREAM_WRAPPER_H
+            prev1->next = dummyHead2->next;
+        ListNode* ret = dummyHead1->next;
     
     
-    {    // If the -c option is specified without a -n, php behavior is to
-    // load the default ini/hdf
-    auto cb = [&newargv] (const char *filename) {
-      newargv.push_back('-c');
-      newargv.push_back(filename);
-    };
-    add_default_config_files_globbed(DEFAULT_CONFIG_DIR '/php*.ini', cb);
-    add_default_config_files_globbed(DEFAULT_CONFIG_DIR '/config*.hdf', cb);
-  }
-    
-      // implementing File
-  bool open(const String& filename, const String& mode) override;
-  bool close() override;
-  int64_t readImpl(char *buffer, int64_t length) override;
-  int getc() override;
-  int64_t writeImpl(const char *buffer, int64_t length) override;
-  bool seek(int64_t offset, int whence = SEEK_SET) override;
-  int64_t tell() override;
-  bool eof() override;
-  bool rewind() override;
-  bool flush() override;
-  bool truncate(int64_t size) override;
-    
-    template<typename F>
-void logPerfWarningImpl(folly::StringPiece event, int64_t priority,
-                        int64_t rate, F fillCols) {
-  auto const effectiveRate = rate * RuntimeOption::EvalPerfWarningSampleRate;
-  if (effectiveRate > std::numeric_limits<uint32_t>::max()) return;
-  if (!StructuredLog::coinflip(effectiveRate)) return;
-    }
-    
-    #include 'unicode/messagepattern.h'
-#include 'unicode/rbnf.h'
-#include 'unicode/selfmt.h'
-#include 'unicode/uchar.h'
-#include 'unicode/ucnv_err.h'
-#include 'unicode/umsg.h'
-#include 'unicode/ustring.h'
-#include 'unicode/utypes.h'
-#include 'cmemory.h'
-#include 'messageimpl.h'
-#include 'patternprops.h'
-#include 'selfmtimpl.h'
-#include 'uassert.h'
-#include 'ustrfmt.h'
-#include 'util.h'
-#include 'uvector.h'
-    
-    #include 'unicode/format.h'
-#include 'unicode/locid.h'
-#include 'unicode/parseerr.h'
-#include 'unicode/utypes.h'
-#include 'uvector.h'
-#include 'hash.h'
-    
-    SharedBreakIterator::SharedBreakIterator(
-        BreakIterator *biToAdopt) : ptr(biToAdopt) { }
-    
-    #ifndef __SHARED_NUMBERFORMAT_H__
-#define __SHARED_NUMBERFORMAT_H__
-    
-    #endif
+    {    return 0;
+}
 
     
-    void 
-SimpleTimeZone::setEndRule(int32_t month, int32_t dayOfMonth, 
-                           int32_t time, TimeMode mode, UErrorCode& status)
+    int main()
 {
-    setEndRule(month, dayOfMonth, 0, time, mode, status);
-}
-    
-    UnicodeSet *SimpleDateFormatStaticSets::getIgnorables(UDateFormatField fieldIndex)
-{
-    UErrorCode status = U_ZERO_ERROR;
-    umtx_initOnce(gSimpleDateFormatStaticSetsInitOnce, &smpdtfmt_initSets, status);
-    if (U_FAILURE(status)) {
-        return NULL;
+    // create a JSON value
+    json j =
+    {
+        {'number', 1}, {'string', 'foo'}, {'array', {1, 2}}
+    };
     }
     
-    switch (fieldIndex) {
-        case UDAT_YEAR_FIELD:
-        case UDAT_MONTH_FIELD:
-        case UDAT_DATE_FIELD:
-        case UDAT_STANDALONE_DAY_FIELD:
-        case UDAT_STANDALONE_MONTH_FIELD:
-            return gStaticSets->fDateIgnorables;
-            
-        case UDAT_HOUR_OF_DAY1_FIELD:
-        case UDAT_HOUR_OF_DAY0_FIELD:
-        case UDAT_MINUTE_FIELD:
-        case UDAT_SECOND_FIELD:
-        case UDAT_HOUR1_FIELD:
-        case UDAT_HOUR0_FIELD:
-            return gStaticSets->fTimeIgnorables;
-            
-        default:
-            return gStaticSets->fOtherIgnorables;
+        // out_of_range.402
+    try
+    {
+        // try to use the array index '-'
+        json::const_reference ref = j.at('/array/-'_json_pointer);
     }
-}
-    
-    static const UChar gZero[] = { 0x7A, 0x65, 0x72, 0x6F };
-static const UChar gOne[] = { 0x6F, 0x6E, 0x65 };
-static const UChar gTwo[] = { 0x74, 0x77, 0x6F };
-static const UChar gFew[] = { 0x66, 0x65, 0x77 };
-static const UChar gMany[] = { 0x6D, 0x61, 0x6E, 0x79 };
-static const UChar gOther[] = { 0x6F, 0x74, 0x68, 0x65, 0x72 };
-    
-    /**
- * Implement UnicodeReplacer
- */
-void StringReplacer::addReplacementSetTo(UnicodeSet& toUnionTo) const {
-    UChar32 ch;
-    for (int32_t i=0; i<output.length(); i+=U16_LENGTH(ch)) {
-    ch = output.char32At(i);
-    UnicodeReplacer* r = data->lookupReplacer(ch);
-    if (r == NULL) {
-        toUnionTo.add(ch);
-    } else {
-        r->addReplacementSetTo(toUnionTo);
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
     }
-    }
-}
