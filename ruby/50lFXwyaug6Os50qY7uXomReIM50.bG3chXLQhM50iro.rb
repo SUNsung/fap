7a888187a1b2bd8df4b31f2f8b704fe01373af81
SUNsung <weatherius@gmail.com>
2019-03-01 01:31:36 +0200

    Rewrite [50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb] File in: 2019-03-01 01:31:35

--- ruby/50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb
+++ ruby/50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb
@@ -1,44 +1,62 @@
 
         
-            it 'is turned off for existing instances of Huginn' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
-      DefaultScenarioImporter.import(user)
-    end
+              class MergeRequest < ActiveRecord::Base
+        self.table_name = 'merge_requests'
     
-          # users should be able to set price when importing orders via api
-      def permitted_line_item_attributes
-        if @current_user_roles.include?('admin')
-          super + [:price, :variant_id, :sku]
-        else
-          super
+            def preload_stages_warnings
+          # This preloads the number of warnings for every stage, ensuring
+          # that Ci::Stage#has_warnings? doesn't execute any additional
+          # queries.
+          @pipeline.stages.each { |stage| stage.number_of_warnings }
         end
       end
+    end
+  end
+end
+
     
-            def show
-          authorize! :read, @order, order_token
-          @address = find_address
-          respond_with(@address)
+              noteable_type =
+            if matches[:type] == 'pull'
+              'MergeRequest'
+            else
+              'Issue'
+            end
+    
+              format('%020e', 109.52).should == '000000001.095200e+02'
+          format('%020E', 109.52).should == '000000001.095200E+02'
+          format('%020f', 10.952).should == '0000000000010.952000'
+          format('%020g', 12.1234).should == '000000000000012.1234'
+          format('%020G', 12.1234).should == '000000000000012.1234'
+          format('%020a', 196).should == '0x000000000001.88p+7'
+          format('%020A', 196).should == '0X000000000001.88P+7'
         end
     
-            def cancel
-          @return_authorization = order.return_authorizations.accessible_by(current_ability, :update).find(params[:id])
-          if @return_authorization.cancel
-            respond_with @return_authorization, default_template: :show
-          else
-            invalid_resource!(@return_authorization)
-          end
-        end
+      it 'raises an ArgumentError when passed a negative duration' do
+    lambda { sleep(-0.1) }.should raise_error(ArgumentError)
+    lambda { sleep(-1) }.should raise_error(ArgumentError)
+  end
     
-            def update
-          @stock_item = StockItem.accessible_by(current_ability, :update).find(params[:id])
+      it 'returns the previous seed value' do
+    srand(10)
+    srand(20).should == 10
+  end
     
-            def update
-          authorize! :update, @store
-          if @store.update_attributes(store_params)
-            respond_with(@store, status: 200, default_template: :show)
-          else
-            invalid_resource!(@store)
-          end
-        end
+      after :each do
+    Object.send :remove_method, :boom
+  end
+    
+    group :development do
+  cp_gem 'claide',                'CLAide'
+  cp_gem 'cocoapods-core',        'Core'
+  cp_gem 'cocoapods-deintegrate', 'cocoapods-deintegrate'
+  cp_gem 'cocoapods-downloader',  'cocoapods-downloader'
+  cp_gem 'cocoapods-plugins',     'cocoapods-plugins'
+  cp_gem 'cocoapods-search',      'cocoapods-search'
+  cp_gem 'cocoapods-stats',       'cocoapods-stats'
+  cp_gem 'cocoapods-trunk',       'cocoapods-trunk'
+  cp_gem 'cocoapods-try',         'cocoapods-try'
+  cp_gem 'molinillo',             'Molinillo'
+  cp_gem 'nanaimo',               'Nanaimo'
+  cp_gem 'xcodeproj',             'Xcodeproj'
+    
+    (import 'mDNSResponder.sb')
