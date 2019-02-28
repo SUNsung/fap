3f25a03b10023e73820af42954c056b539121104
SUNsung <weatherius@gmail.com>
2019-02-28 18:43:56 +0200

    Rewrite [50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp] File in: 2019-02-28 18:43:56

--- cccccc/50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp
+++ cccccc/50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp
@@ -1,3 +1,86 @@
 
         
-        
+        #include <QDialog>
+    
+    /**
+ * Changelog:
+ * - March 2013, Diederik Huys:    original version
+ * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
+ * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
+ */
+    
+    static void secp256k1_ge_set_gej_zinv(secp256k1_ge *r, const secp256k1_gej *a, const secp256k1_fe *zi) {
+    secp256k1_fe zi2;
+    secp256k1_fe zi3;
+    secp256k1_fe_sqr(&zi2, zi);
+    secp256k1_fe_mul(&zi3, &zi2, zi);
+    secp256k1_fe_mul(&r->x, &a->x, &zi2);
+    secp256k1_fe_mul(&r->y, &a->y, &zi3);
+    r->infinity = a->infinity;
+}
+    
+    #define Round(a,b,c,d,e,f,g,h,k,w) do { \
+    uint32_t t1 = (h) + Sigma1(e) + Ch((e), (f), (g)) + (k) + (w); \
+    uint32_t t2 = Sigma0(a) + Maj((a), (b), (c)); \
+    (d) += t1; \
+    (h) = t1 + t2; \
+} while(0)
+    
+    #include <bech32.h>
+#include <test/test_bitcoin.h>
+    
+    const std::vector<std::string>& UniValue::getKeys() const
+{
+    if (typ != VOBJ)
+        throw std::runtime_error('JSON value is not an object as expected');
+    return keys;
+}
+    
+    workspace.ResetWorkspace()
+    
+    #include 'caffe2/core/logging.h'
+#include 'caffe2/core/operator.h'
+#include 'caffe2/utils/math.h'
+    
+    template <typename T, class Context>
+class BernoulliJSDOp final : public Operator<Context> {
+ public:
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
+};
+    
+    
+    {
+    {
+    {        for (;;) {
+          auto start = index.fetch_add(work_chunk);
+          auto const stop = std::min(start + work_chunk, inputs.size());
+          if (start >= stop) break;
+          for (auto i = start; i != stop; ++i) func(inputs[i]);
+        }
+      } catch (const std::exception& e) {
+        std::fprintf(stderr,
+          'worker thread exited with exception: %s\n', e.what());
+        failed = true;
+      }
+    }));
+  }
+    
+    void fillMap(BaseMap* map, const APCArray* ar) {
+  for (auto i = uint32_t{0}; i < ar->size(); ++i) {
+    map->set(*ar->getKey(i).asTypedValue(),
+             *ar->getValue(i)->toLocal().asTypedValue());
+  }
+}
+    
+    struct APCCollection {
+  static APCHandle::Pair Make(const ObjectData*,
+                              APCHandleLevel level,
+                              bool unserializeObj);
+  static void Delete(APCHandle*);
+    }
+    
+      bool valid() const { return !isClosed(); }
+    
+    #include <folly/Range.h>
