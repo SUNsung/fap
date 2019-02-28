b262bce2984c2d729d1d7cf93a5fdcfc5c8772ff
SUNsung <weatherius@gmail.com>
2019-02-28 16:19:05 +0200

    Rewrite [32Qayg4oxxQDg32tpv.7OGpY0E32CT6D5K2zx0g32Bf6.cpp] File in: 2019-02-28 16:19:04

--- cccccc/32Qayg4oxxQDg32tpv.7OGpY0E32CT6D5K2zx0g32Bf6.cpp
+++ cccccc/32Qayg4oxxQDg32tpv.7OGpY0E32CT6D5K2zx0g32Bf6.cpp
@@ -1,312 +1,98 @@
-#include 'src/cpp/ext/filters/census/context.h'
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CONTEXT_H
-    
-    MeasureInt64 RpcServerSentMessagesPerRpc() {
-  static const auto measure =
-      MeasureInt64::Register(kRpcServerSentMessagesPerRpcMeasureName,
-                             'Number of messages sent per RPC', kCount);
-  return measure;
+
+        
+        std::unique_ptr<WriteControllerToken>
+WriteController::GetCompactionPressureToken() {
+  ++total_compaction_pressure_;
+  return std::unique_ptr<WriteControllerToken>(
+      new CompactionPressureToken(this));
 }
     
-      Status GetFileContainingExtension(
-      ServerContext* context,
-      const reflection::v1alpha::ExtensionRequest* request,
-      reflection::v1alpha::ServerReflectionResponse* response);
-    
-    #include <condition_variable>
-#include <list>
-#include <memory>
-#include <mutex>
-#include <queue>
-    
-    #include <cstdio>
-    
-    TEST(CorruptionTest, TableFile) {
-  Build(100);
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
-  dbi->TEST_CompactMemTable();
-  dbi->TEST_CompactRange(0, nullptr, nullptr);
-  dbi->TEST_CompactRange(1, nullptr, nullptr);
-    }
-    
-      // Have we encountered a background error in paranoid mode?
-  Status bg_error_ GUARDED_BY(mutex_);
-    
-    
-    {
-    {}  // namespace log
-}  // namespace leveldb
-    
-        RecordType type;
-    const bool end = (left == fragment_length);
-    if (begin && end) {
-      type = kFullType;
-    } else if (begin) {
-      type = kFirstType;
-    } else if (end) {
-      type = kLastType;
-    } else {
-      type = kMiddleType;
-    }
-    
-      // Return an iterator that yields the contents of the memtable.
-  //
-  // The caller must ensure that the underlying MemTable remains live
-  // while the returned iterator is live.  The keys returned by this
-  // iterator are internal keys encoded by AppendInternalKey in the
-  // db/format.{h,cc} module.
-  Iterator* NewIterator();
-    
-    
-    { private:
-  port::Mutex mu_;
-  ReaderState state_ GUARDED_BY(mu_);
-  port::CondVar state_cv_ GUARDED_BY(mu_);
-};
-    
-    /*! \brief gradient statistics pair usually needed in gradient boosting */
-using GradientPair = detail::GradientPairInternal<float>;
-    
-    
-    { private:
-  RowBlock<IndexType> out_;
-  std::unique_ptr<Parser<IndexType> > parser_;
-  uint32_t num_col_;
-  std::vector<size_t> offset_;
-  std::vector<IndexType> dense_index_;
-  std::vector<xgboost::bst_float> dense_value_;
-};
-    
-    struct GBTreeModel {
-  explicit GBTreeModel(bst_float base_margin) : base_margin(base_margin) {}
-  void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) {
-    // initialize model parameters if not yet been initialized.
-    if (trees.size() == 0) {
-      param.InitAllowUnknown(cfg);
-    }
+    namespace rocksdb {
+static std::string IOErrorMsg(const std::string& context,
+                              const std::string& file_name) {
+  if (file_name.empty()) {
+    return context;
   }
-    }
-    
-      for (auto alphabet_size : test_cases) {
-    for (int i = 0; i < repetitions; i++) {
-      std::vector<int> input(num_elements);
-      std::generate(input.begin(), input.end(),
-        [=]() { return rand() % alphabet_size; });
-      CompressedBufferWriter cbw(alphabet_size);
-    }
-    }
-    
-    SEXP XGBoosterBoostOneIter_R(SEXP handle, SEXP dtrain, SEXP grad, SEXP hess) {
-  R_API_BEGIN();
-  CHECK_EQ(length(grad), length(hess))
-      << 'gradient and hess must have same length';
-  int len = length(grad);
-  std::vector<float> tgrad(len), thess(len);
-  #pragma omp parallel for schedule(static)
-  for (int j = 0; j < len; ++j) {
-    tgrad[j] = REAL(grad)[j];
-    thess[j] = REAL(hess)[j];
-  }
-  CHECK_CALL(XGBoosterBoostOneIter(R_ExternalPtrAddr(handle),
-                                 R_ExternalPtrAddr(dtrain),
-                                 BeginPtr(tgrad), BeginPtr(thess),
-                                 len));
-  R_API_END();
-  return R_NilValue;
+  return context + ': ' + file_name;
 }
+    }
     
     
-    {  /*! \brief push up temp */
-  inline void PushTemp() {
-    temp.Reserve(limit_size * 2);
-    for (size_t l = 1; true; ++l) {
-      this->InitLevel(l + 1);
-      // check if level l is empty
-      if (level[l].size == 0) {
-        level[l].SetPrune(temp, limit_size);
-        break;
-      } else {
-        // level 0 is actually temp space
-        level[0].SetPrune(temp, limit_size);
-        temp.SetCombine(level[0], level[l]);
-        if (temp.size > limit_size) {
-          // try next level
-          level[l].size = 0;
-        } else {
-          // if merged record is still smaller, no need to send to next level
-          level[l].CopyFrom(temp); break;
+    {    std::vector<std::string> input_file_names;
+    for (auto level : cf_meta.levels) {
+      for (auto file : level.files) {
+        if (file.being_compacted) {
+          return nullptr;
         }
+        input_file_names.push_back(file.name);
       }
     }
+    return new CompactionTask(
+        db, this, cf_name, input_file_names,
+        options_.num_levels - 1, compact_options_, false);
   }
-  /*! \brief get the summary after finalize */
-  inline void GetSummary(SummaryContainer *out) {
-    if (level.size() != 0) {
-      out->Reserve(limit_size * 2);
-    } else {
-      out->Reserve(inqueue.queue.size());
-    }
-    inqueue.MakeSummary(out);
-    if (level.size() != 0) {
-      level[0].SetPrune(*out, limit_size);
-      for (size_t l = 1; l < level.size(); ++l) {
-        if (level[l].size == 0) continue;
-        if (level[0].size == 0) {
-          level[0].CopyFrom(level[l]);
-        } else {
-          out->SetCombine(level[0], level[l]);
-          level[0].SetPrune(*out, limit_size);
-        }
-      }
-      out->CopyFrom(level[0]);
-    } else {
-      if (out->size > limit_size) {
-        temp.Reserve(limit_size);
-        temp.SetPrune(*out, limit_size);
-        out->CopyFrom(temp);
-      }
-    }
-  }
-  // used for debug, check if the sketch is valid
-  inline void CheckValid(RType eps) const {
-    for (size_t l = 1; l < level.size(); ++l) {
-      level[l].CheckValid(eps);
-    }
-  }
-  // initialize level space to at least nlevel
-  inline void InitLevel(size_t nlevel) {
-    if (level.size() >= nlevel) return;
-    data.resize(limit_size * nlevel);
-    level.resize(nlevel, Summary(nullptr, 0));
-    for (size_t l = 0; l < level.size(); ++l) {
-      level[l].data = dmlc::BeginPtr(data) + l * limit_size;
-    }
-  }
-  // input data queue
-  typename Summary::Queue inqueue;
-  // number of levels
-  size_t nlevel;
-  // size of summary in each level
-  size_t limit_size;
-  // the level of each summaries
-  std::vector<Summary> level;
-  // content of the summary
-  std::vector<Entry> data;
-  // temporal summary, used for temp-merge
-  SummaryContainer temp;
+    
+    #ifndef ROCKSDB_LITE
+// The following set of functions provide a way to construct RocksDB Options
+// from a string or a string-to-string map.  Here're the general rule of
+// setting option values from strings by type.  Some RocksDB types are also
+// supported in these APIs.  Please refer to the comment of the function itself
+// to find more information about how to config those RocksDB types.
+//
+// * Strings:
+//   Strings will be used as values directly without any truncating or
+//   trimming.
+//
+// * Booleans:
+//   - 'true' or '1' => true
+//   - 'false' or '0' => false.
+//   [Example]:
+//   - {'optimize_filters_for_hits', '1'} in GetColumnFamilyOptionsFromMap, or
+//   - 'optimize_filters_for_hits=true' in GetColumnFamilyOptionsFromString.
+//
+// * Integers:
+//   Integers are converted directly from string, in addition to the following
+//   units that we support:
+//   - 'k' or 'K' => 2^10
+//   - 'm' or 'M' => 2^20
+//   - 'g' or 'G' => 2^30
+//   - 't' or 'T' => 2^40  // only for unsigned int with sufficient bits.
+//   [Example]:
+//   - {'arena_block_size', '19G'} in GetColumnFamilyOptionsFromMap, or
+//   - 'arena_block_size=19G' in GetColumnFamilyOptionsFromString.
+//
+// * Doubles / Floating Points:
+//   Doubles / Floating Points are converted directly from string.  Note that
+//   currently we do not support units.
+//   [Example]:
+//   - {'hard_rate_limit', '2.1'} in GetColumnFamilyOptionsFromMap, or
+//   - 'hard_rate_limit=2.1' in GetColumnFamilyOptionsFromString.
+// * Array / Vectors:
+//   An array is specified by a list of values, where ':' is used as
+//   the delimiter to separate each value.
+//   [Example]:
+//   - {'compression_per_level', 'kNoCompression:kSnappyCompression'}
+//     in GetColumnFamilyOptionsFromMap, or
+//   - 'compression_per_level=kNoCompression:kSnappyCompression' in
+//     GetColumnFamilyOptionsFromMapString
+// * Enums:
+//   The valid values of each enum are identical to the names of its constants.
+//   [Example]:
+//   - CompressionType: valid values are 'kNoCompression',
+//     'kSnappyCompression', 'kZlibCompression', 'kBZip2Compression', ...
+//   - CompactionStyle: valid values are 'kCompactionStyleLevel',
+//     'kCompactionStyleUniversal', 'kCompactionStyleFIFO', and
+//     'kCompactionStyleNone'.
+//
+    
+    
+    {class DbUndumpTool {
+ public:
+  bool Run(const UndumpOptions& undump_options,
+           rocksdb::Options options = rocksdb::Options());
 };
+}  // namespace rocksdb
+#endif  // ROCKSDB_LITE
+
     
-    /*!
- * \brief Registry entry for tree updater.
- */
-struct GradientBoosterReg
-    : public dmlc::FunctionRegEntryBase<
-  GradientBoosterReg,
-  std::function<GradientBooster* (const std::vector<std::shared_ptr<DMatrix> > &cached_mats,
-                                  bst_float base_margin)> > {
-};
-    
-    // About GLSL version:
-// The 'glsl_version' initialization parameter should be NULL (default) or a '#version XXX' string.
-// On computer platform the GLSL version default to '#version 130'. On OpenGL ES 3 platform it defaults to '#version 300 es'
-// Only override if your GL version doesn't handle this GLSL version. See GLSL version table at the top of imgui_impl_opengl3.cpp.
-    
-        // Create an invisible mouse cursor
-    // Because al_hide_mouse_cursor() seems to mess up with the actual inputs..
-    ALLEGRO_BITMAP* mouse_cursor = al_create_bitmap(8,8);
-    g_MouseCursorInvisible = al_create_mouse_cursor(mouse_cursor, 0, 0);
-    al_destroy_bitmap(mouse_cursor);
-    
-    
-    {    return true;
-}
-    
-        io.KeyCtrl = s3eKeyboardGetState(s3eKeyLeftControl) == S3E_KEY_STATE_DOWN || s3eKeyboardGetState(s3eKeyRightControl) == S3E_KEY_STATE_DOWN;
-    io.KeyShift = s3eKeyboardGetState(s3eKeyLeftShift) == S3E_KEY_STATE_DOWN || s3eKeyboardGetState(s3eKeyRightShift) == S3E_KEY_STATE_DOWN;
-    io.KeyAlt = s3eKeyboardGetState(s3eKeyLeftAlt) == S3E_KEY_STATE_DOWN || s3eKeyboardGetState(s3eKeyRightAlt) == S3E_KEY_STATE_DOWN;
-    io.KeySuper = s3eKeyboardGetState(s3eKeyLeftWindows) == S3E_KEY_STATE_DOWN || s3eKeyboardGetState(s3eKeyRightWindows) == S3E_KEY_STATE_DOWN;
-    
-    
-    {
-    {            t0 = t1;
-            tp0 = tp1;
-        }
-    }
-    
-            // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin('Another Window', &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text('Hello from another window!');
-            if (ImGui::Button('Close Me'))
-                show_another_window = false;
-            ImGui::End();
-        }
-    
-    bool Follow::initWithTargetAndOffset(Node *followedNode, float xOffset,float yOffset,const Rect& rect)
-{
-    CCASSERT(followedNode != nullptr, 'FollowedNode can't be NULL');
-    if(followedNode == nullptr)
-    {
-        log('Follow::initWithTarget error: followedNode is nullptr!');
-        return false;
-    }
- 
-    followedNode->retain();
-    _followedNode = followedNode;
-    _worldRect = rect;
-    _boundarySet = !rect.equals(Rect::ZERO);
-    _boundaryFullyCovered = false;
-    }
-    
-    void Waves3D::update(float time)
-{
-    int i, j;
-    for (i = 0; i < _gridSize.width + 1; ++i)
-    {
-        for (j = 0; j < _gridSize.height + 1; ++j)
-        {
-            Vec3 v = getOriginalVertex(Vec2(i ,j));
-            v.z += (sinf((float)M_PI * time * _waves * 2 + (v.y+v.x) * 0.01f) * _amplitude * _amplitudeRate);
-            //CCLOG('v.z offset is %f\n', (sinf((float)M_PI * time * _waves * 2 + (v.y+v.x) * .01f) * _amplitude * _amplitudeRate));
-            setVertex(Vec2(i, j), v);
-        }
-    }
-}
-    
-        if (ret && ret->initWithFlipY(y))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    
-        coords.tl.x += (step.x / 2) * (1.0f - distance);
-    coords.tl.y -= (step.y / 2) * (1.0f - distance);
-    
-        // Overrides
-    void startWithTarget(Node *target) override;
-    void update(float dt) override;
-    ActionTween* reverse() const override;
-    ActionTween *clone() const override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    /** 
-     * @brief Initializes the action with the property name (key), and the from and to parameters.
-     * @param duration The duration of the ActionTween. It's a value in seconds.
-     * @param key The key of property which should be updated.
-     * @param from The value of the specified property when the action begin.
-     * @param to The value of the specified property when the action end.
-     * @return If the initialization success, return true; otherwise, return false.
-     */
-    bool initWithDuration(float duration, const std::string& key, float from, float to);
-    
-    unsigned char AutoPolygon::getAlphaByIndex(unsigned int i)
-{
-    return *(_data+i*4+3);
-}
-unsigned char AutoPolygon::getAlphaByPos(const Vec2& pos)
-{
-    return *(_data+((int)pos.y*_width+(int)pos.x)*4+3);
-}
+        // read-only access
