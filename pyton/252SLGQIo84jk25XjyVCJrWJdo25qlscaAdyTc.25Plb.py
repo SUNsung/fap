093e08433e5a1e471e1e797f333d0ebe5ca8b596
SUNsung <weatherius@gmail.com>
2019-03-01 23:25:10 +0200

    Rewrite [252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py] File in: 2019-03-01 23:25:10

--- pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
+++ pyton/252SLGQIo84jk25XjyVCJrWJdo25qlscaAdyTc.25Plb.py
@@ -1,180 +1,53 @@
 
         
-                request_line = '{method} {path}{query} HTTP/1.1'.format(
-            method=self._orig.method,
-            path=url.path or '/',
-            query='?' + url.query if url.query else ''
-        )
+            def init_poolmanager(self, *args, **kwargs):
+        kwargs['ssl_version'] = self._ssl_version
+        super(HTTPieHTTPAdapter, self).init_poolmanager(*args, **kwargs)
     
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
+        def get_converter(self, mime):
+        if is_valid_mime(mime):
+            for converter_class in plugin_manager.get_converters():
+                if converter_class.supports(mime):
+                    return converter_class(mime)
     
-        def convert(self, content_bytes):
-        raise NotImplementedError
+        message = BINARY_SUPPRESSED_NOTICE
     
     
-def test_default_headers_case_insensitive(httpbin):
-    '''
-    <https://github.com/jakubroztocil/httpie/issues/644>
-    '''
-    r = http(
-        '--debug',
-        '--print=H',
-        httpbin.url + '/post',
-        'CONTENT-TYPE:application/json-patch+json',
-        'a=b',
-    )
-    assert 'CONTENT-TYPE: application/json-patch+json' in r
-    assert 'Content-Type' not in r
-    
-    from utils import TESTS_ROOT
-    
-    
-def test_unicode_raw_json_item(httpbin):
+def test_unicode_raw_json_item_verbose(httpbin):
     r = http('--json', 'POST', httpbin.url + '/post',
              u'test:={ '%s' : [ '%s' ] }' % (UNICODE, UNICODE))
     assert HTTP_OK in r
     assert r.json['json'] == {'test': {UNICODE: [UNICODE]}}
     
-    
-def get_unique_filename(filename, exists=os.path.exists):
-    attempt = 0
-    while True:
-        suffix = '-' + str(attempt) if attempt > 0 else ''
-        try_filename = trim_filename_if_needed(filename, extra=len(suffix))
-        try_filename += suffix
-        if not exists(try_filename):
-            return try_filename
-        attempt += 1
-    
-        def delete(self, session_key=None):
-        super().delete(session_key)
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        self._cache.delete(self.cache_key_prefix + session_key)
-    
-        def save(self, session_key, session_dict, expire_date):
-        s = self.model(session_key, self.encode(session_dict), expire_date)
-        if session_dict:
-            s.save()
-        else:
-            s.delete()  # Clear sessions with no data.
-        return s
-    
-        def _finish(self, request):
-        self.concurrent -= 1
-        if not request.finished and not request._disconnected:
-            request.finish()
-    
-        # Max concurrency is limited by global CONCURRENT_REQUESTS setting
-    max_concurrent_requests = 8
-    # Requests per second goal
-    qps = None # same as: 1 / download_delay
-    download_delay = None
-    # time in seconds to delay server responses
-    latency = None
-    # number of slots to create
-    slots = 1
-    
-        def process_options(self, args, opts):
-        ScrapyCommand.process_options(self, args, opts)
-        try:
-            opts.spargs = arglist_to_dict(opts.spargs)
-        except ValueError:
-            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
-        if opts.output:
-            if opts.output == '-':
-                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
-            else:
-                self.settings.set('FEED_URI', opts.output, priority='cmdline')
-            feed_exporters = without_none_values(
-                self.settings.getwithbase('FEED_EXPORTERS'))
-            valid_output_formats = feed_exporters.keys()
-            if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
-            if opts.output_format not in valid_output_formats:
-                raise UsageError('Unrecognized output format '%s', set one'
-                                 ' using the '-t' switch or as a file extension'
-                                 ' from the supported list %s' % (opts.output_format,
-                                                                  tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
-    
-            editor = self.settings['EDITOR']
-        try:
-            spidercls = self.crawler_process.spider_loader.load(args[0])
-        except KeyError:
-            return self._err('Spider not found: %s' % args[0])
-    
-            dfd.addBoth(lambda _: logger.info('Spider closed (%(reason)s)',
-                                          {'reason': reason},
-                                          extra={'spider': spider}))
-    
-        if prober.nat_type in ('cone', 'restricted'):
-        usable = 'usable'
-    elif prober.nat_type == 'offline':
-        usable = 'unusable'
-    else:
-        usable = 'unknown'
-    
-    ## All tokens go to the parser (unless skip() is called in that rule)
-# on a particular 'channel'.  The parser tunes to a particular channel
-# so that whitespace etc... can go to the parser on a 'hidden' channel.
-DEFAULT_CHANNEL = 0
+    def get_extension(link):
+    extension = os.path.splitext(link)[1][1:]
+    if extension in ['pdf', 'html']:
+        return extension
+    if 'pdf' in extension:
+        return 'pdf'    
+    return 'pdf'    
     
     
-# Are two open files really referencing the same file?
-# (Not necessarily the same file descriptor!)
-def sameopenfile(fp1, fp2):
-    '''Test whether two open file objects reference the same file'''
-    s1 = os.fstat(fp1)
-    s2 = os.fstat(fp2)
-    return samestat(s1, s2)
+def testRaw():
+    check(b'\x96\xda\x00\x00\xda\x00\x01a\xda\x00\x02ab\xdb\x00\x00'
+          b'\x00\x00\xdb\x00\x00\x00\x01a\xdb\x00\x00\x00\x02ab',
+          (b'', b'a', b'ab', b'', b'a', b'ab'))
     
-        def test_output_newline(self):
-        # Issue 13119 Newline for print() should be \r\n on Windows.
-        code = '''if 1:
-            import sys
-            print(1)
-            print(2)
-            print(3, file=sys.stderr)
-            print(4, file=sys.stderr)'''
-        rc, out, err = assert_python_ok('-c', code)
+        dest = pathlib.Path('dist')
+    dest.mkdir(exist_ok=True)
     
-    '''Send the contents of a directory as a MIME message.'''
+        MARGIN_RATIOS = UNIT_RATIOS.copy()
+    MARGIN_RATIOS.update({
+        'none': ('pt', 0),
+    })
     
-            TASKS = [(mul, (i, 7)) for i in range(10)] + \
-                [(plus, (i, 8)) for i in range(10)]
+        @pytest.mark.parametrize('tz', ['US/Eastern', 'Asia/Tokyo'])
+    def test_fillna_datetime64(self, tz):
+        # GH 11343
+        idx = pd.DatetimeIndex(['2011-01-01 09:00', pd.NaT,
+                                '2011-01-01 11:00'])
     
-    def mul(a, b):
-    time.sleep(0.5*random.random())
-    return a * b
-    
-        while s % 2 == 0:
-        s = s // 2
-        t += 1
-    
-    
-class DoubleHash(HashTable):
-    '''
-        Hash Table example with open addressing and Double Hash
-    '''
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    # frequency taken from http://en.wikipedia.org/wiki/Letter_frequency
-englishLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97,
-                     'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25,
-                     'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36,
-                     'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29,
-                     'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10,
-                     'Z': 0.07}
-ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            exp4 = ('0   1 days\n'
+                '1   2 days\n'
+                '2   3 days\n'
+                'dtype: timedelta64[ns]')
