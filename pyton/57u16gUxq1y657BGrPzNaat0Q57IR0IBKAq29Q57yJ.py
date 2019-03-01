5f60c76545a06b4d9866f847e36fe26e46bbdf2e
SUNsung <weatherius@gmail.com>
2019-03-01 22:11:28 +0200

    Rewrite [57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py] File in: 2019-03-01 22:11:27

--- pyton/57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py
+++ pyton/57u16gUxq1y657BGrPzNaat0Q57IR0IBKAq29Q57yJ.py
@@ -1,45 +1,224 @@
 
         
-        HEADER_ARGS = {'Strict-Transport-Security': HSTS_ARGS}
+                batch_indices = epoch_indices[minibatch * self.hparams.batch_size:(
+            minibatch + 1) * self.hparams.batch_size]
+        batch_instances = [inputs[i] for i in batch_indices]
+    
+      # First generate all firing rates. in the next loop, generate all
+  # replications this allows the random state for rate generation to be
+  # independent of n_replications.
+  dataset_name = 'dataset_N' + str(N) + '_S' + str(S)
+  if S < N:
+    dataset_name += '_n' + str(n+1)
+    
+    # save down the network outputs (may be useful later)
+train_outputs_u, valid_outputs_u = split_list_by_inds(outs_e,
+                                                      train_inds,
+                                                      valid_inds)
+train_outputs_u = np.array(train_outputs_u)
+valid_outputs_u = np.array(valid_outputs_u)
+    
+      words, _ = list(zip(*count_pairs))
+  word_to_id = dict(zip(words, range(len(words))))
+  print('<eos>:', word_to_id['<eos>'])
+  global EOS_INDEX
+  EOS_INDEX = word_to_id['<eos>']
+    
+      tf.gfile.MakeDirs(FLAGS.output_path)
+  output_file = tf.gfile.GFile(
+      os.path.join(FLAGS.output_path, 'reviews.txt'), mode='w')
+    
+      And the policy for Generator G is the log-probability of taking action x2
+  given the past context.
+    
+    
+def create_gen_train_op(hparams, learning_rate, gen_loss, global_step, mode):
+  '''Create Generator train op.'''
+  del hparams
+  with tf.name_scope('train_generator'):
+    if FLAGS.generator_optimizer == 'sgd':
+      gen_optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    elif FLAGS.generator_optimizer == 'adam':
+      gen_optimizer = tf.train.AdamOptimizer(learning_rate)
+    else:
+      raise NotImplementedError
+    gen_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
+    ]
+    print('Optimizing Generator vars.')
+    for v in gen_vars:
+      print(v)
+    if mode == 'MINIMIZE':
+      gen_grads = tf.gradients(gen_loss, gen_vars)
+    elif mode == 'MAXIMIZE':
+      gen_grads = tf.gradients(-gen_loss, gen_vars)
+    else:
+      raise ValueError('Must be one of 'MINIMIZE' or 'MAXIMIZE'')
+    gen_grads_clipped, _ = tf.clip_by_global_norm(gen_grads,
+                                                  FLAGS.grad_clipping)
+    gen_train_op = gen_optimizer.apply_gradients(
+        zip(gen_grads_clipped, gen_vars), global_step=global_step)
+    return gen_train_op, gen_grads_clipped, gen_vars
+    
+        ## Discriminator Models.
+    if (FLAGS.discriminator_model == 'rnn_nas' or
+        FLAGS.discriminator_model == 'rnn_zaremba' or
+        FLAGS.discriminator_model == 'rnn_vd' or
+        FLAGS.discriminator_model == 'cnn'):
+      load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
+      print('Restoring Discriminator from %s.' % load_ckpt)
+      tf.logging.info('Restoring Discriminator from %s.' % load_ckpt)
+      dis_init_saver = init_savers['dis_init_saver']
+      dis_init_saver.restore(sess, load_ckpt)
+    
+    
+def hash_function(input_tuple):
+  '''Hash function for a tuple.'''
+  return hash(input_tuple)
+    
+      variable_mapping = {
+      str(model_str) + '/embedding':
+          decoder_embedding,
+      str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+          decoder_lstm_w_0,
+      str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+          decoder_lstm_b_0,
+      str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+          decoder_lstm_w_1,
+      str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+          decoder_lstm_b_1,
+      str(model_str) + '/softmax_b':
+          decoder_softmax_b
+  }
+  return variable_mapping
+    
+        def create(self):
+        while True:
+            self._session_key = self._get_new_session_key()
+            try:
+                # Save immediately to ensure we have a unique entry in the
+                # database.
+                self.save(must_create=True)
+            except CreateError:
+                # Key wasn't unique. Try again.
+                continue
+            self.modified = True
+            return
+    
+    
+class SessionStore(SessionBase):
+    
+    
+def x_robots_tag(func):
+    @wraps(func)
+    def inner(request, *args, **kwargs):
+        response = func(request, *args, **kwargs)
+        response['X-Robots-Tag'] = 'noindex, noodp, noarchive'
+        return response
+    return inner
+    
+    
+def test_mnist():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        assert len(x_train) == len(y_train) == 60000
+        assert len(x_test) == len(y_test) == 10000
+    
+    
+def test_regularization_shared_layer_in_different_models():
+    shared_dense = Dense(num_classes,
+                         kernel_regularizer=regularizers.l1(),
+                         activity_regularizer=regularizers.l1())
+    models = []
+    for _ in range(2):
+        input_tensor = Input(shape=(data_dim,))
+        unshared_dense = Dense(num_classes, kernel_regularizer=regularizers.l1())
+        out = unshared_dense(shared_dense(input_tensor))
+        models.append(Model(input_tensor, out))
+    
+    import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import SimpleRNN
+from keras import initializers
+from keras.optimizers import RMSprop
+    
+    x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+    
+    print('Loading data...')
+(x_train, y_train), (x_test, y_test) = reuters.load_data(num_words=max_words,
+                                                         test_split=0.2)
+print(len(x_train), 'train sequences')
+print(len(x_test), 'test sequences')
+    
+    from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import (ansible_dict_to_boto3_filter_list,
+                                      boto3_tag_list_to_ansible_dict,
+                                      camel_dict_to_snake_dict)
+try:
+    from botocore.exceptions import (BotoCoreError, ClientError)
+except ImportError:
+    pass  # caught by imported AnsibleAWSModule
+    
+        function_name = module.params.get('function_name')
+    if function_name:
+        lambda_facts[function_name] = {}
+        lambda_facts[function_name].update(config_details(client, module)[function_name])
+        lambda_facts[function_name].update(alias_details(client, module)[function_name])
+        lambda_facts[function_name].update(policy_details(client, module)[function_name])
+        lambda_facts[function_name].update(version_details(client, module)[function_name])
+        lambda_facts[function_name].update(mapping_details(client, module)[function_name])
+    else:
+        lambda_facts.update(config_details(client, module))
+    
+            if update_processes:
+            chk_changed = False
+            for update_process in update_processes:
+                if module.check_mode:
+                    chk_changed |= _modify_process(module,
+                                                   oneandone_conn,
+                                                   monitoring_policy['id'],
+                                                   update_process['id'],
+                                                   update_process)
+    
+    RETURN = '''# '''
+    
+    
+def unfollow_log(module, le_path, logs):
+    ''' Unfollows one or more logs if followed. '''
+    
+    test_face_landmarks = '''
+landmarks = face_recognition.face_landmarks(image, face_locations=face_locations)[0]
+'''
+    
+    import face_recognition
+from flask import Flask, jsonify, request, redirect
+    
+    
+if __name__ == '__main__':
+    main()
 
     
-    from certbot import errors
+    print('I found {} face(s) in this photograph.'.format(len(face_landmarks_list)))
     
-    # General information about the project.
-project = u'Face Recognition'
-copyright = u'2017, Adam Geitgey'
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-        # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-    
-    # Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
-known_face_names = [
-    'Barack Obama',
-    'Joe Biden'
-]
-    
-        function_parameters = zip(
-        images_to_check,
-        itertools.repeat(known_names),
-        itertools.repeat(known_face_encodings),
-        itertools.repeat(tolerance),
-        itertools.repeat(show_distance)
-    )
-    
-    # Show the picture
-pil_image.show()
-
-    
-    with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+        # 图片上传失败，输出以下html代码
+    return '''
+    <!doctype html>
+    <title>Is this a picture of Obama?</title>
+    <h1>Upload a picture and see if it's a picture of Obama!</h1>
+    <form method='POST' enctype='multipart/form-data'>
+      <input type='file' name='file'>
+      <input type='submit' value='Upload'>
+    </form>
+    '''
