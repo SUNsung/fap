1201ddac31bbe98ca9df8b6ee6c6173edd4e550c
SUNsung <weatherius@gmail.com>
2019-03-01 11:19:12 +0200

    Rewrite [17U4AveMAI0o.177Nj6PzLXaHw17HLb0WH8wO617rh3.cpp] File in: 2019-03-01 11:19:12

--- cccccc/17U4AveMAI0o.177Nj6PzLXaHw17HLb0WH8wO617rh3.cpp
+++ cccccc/17U4AveMAI0o.177Nj6PzLXaHw17HLb0WH8wO617rh3.cpp
@@ -1,378 +1,117 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+            /**
+     * System registration hook.
+     */
+    static void registerIDs();
     
-    struct Bfloat16EqFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a == b; }
-};
-struct Bfloat16NeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a != b; }
-};
-struct Bfloat16LtFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a < b; }
-};
-struct Bfloat16GtFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a > b; }
-};
-struct Bfloat16LeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a <= b; }
-};
-struct Bfloat16GeFunctor {
-  npy_bool operator()(bfloat16 a, bfloat16 b) { return a >= b; }
-};
+    #define LOW_A             ((UChar)0x0061)
+#define LOW_B             ((UChar)0x0062)
+#define LOW_C             ((UChar)0x0063)
+#define LOW_D             ((UChar)0x0064)
+#define LOW_E             ((UChar)0x0065)
+#define LOW_F             ((UChar)0x0066)
+#define LOW_G             ((UChar)0x0067)
+#define LOW_H             ((UChar)0x0068)
+#define LOW_I             ((UChar)0x0069)
+#define LOW_J             ((UChar)0x006a)
+#define LOW_K             ((UChar)0x006B)
+#define LOW_L             ((UChar)0x006C)
+#define LOW_M             ((UChar)0x006D)
+#define LOW_N             ((UChar)0x006E)
+#define LOW_O             ((UChar)0x006F)
+#define LOW_P             ((UChar)0x0070)
+#define LOW_Q             ((UChar)0x0071)
+#define LOW_R             ((UChar)0x0072)
+#define LOW_S             ((UChar)0x0073)
+#define LOW_T             ((UChar)0x0074)
+#define LOW_U             ((UChar)0x0075)
+#define LOW_V             ((UChar)0x0076)
+#define LOW_W             ((UChar)0x0077)
+#define LOW_X             ((UChar)0x0078)
+#define LOW_Y             ((UChar)0x0079)
+#define LOW_Z             ((UChar)0x007A)
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+    #include 'unicode/utypes.h'
+#include 'sharedobject.h'
+#include 'unicode/dtfmtsym.h'
     
-    #ifndef TENSORFLOW_PYTHON_LIB_CORE_SAFE_PTR_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_SAFE_PTR_H_
-    
-    #include 'tensorflow/c/c_api.h'
-#include 'tensorflow/core/lib/core/stringpiece.h'
-#include 'tensorflow/core/platform/macros.h'
-#include 'tensorflow/core/platform/types.h'
-    
-    #include 'tensorflow/core/framework/graph.pb.h'
-#include 'tensorflow/core/platform/types.h'
-    
-    Each feature has fixed lengths which are passed as lengths argument and a
-separate tensor will be produced for each feature.
-i.e. DATA.dim(1) = len(lengths) = NumOuptuts.
-    
-      // Iterate over the blobs inside to_block, and set the blobs that we want to
-  // process to BSTT_NONE. (By default, they should be BSTT_SKIP). The function
-  // returns 0 upon success.
-  int LabelSpecialText(TO_BLOCK* to_block);
-    
-    class TESS_API PageIterator {
- public:
-  /**
-   * page_res and tesseract come directly from the BaseAPI.
-   * The rectangle parameters are copied indirectly from the Thresholder,
-   * via the BaseAPI. They represent the coordinates of some rectangle in an
-   * original image (in top-left-origin coordinates) and therefore the top-left
-   * needs to be added to any output boxes in order to specify coordinates
-   * in the original image. See TessBaseAPI::SetRectangle.
-   * The scale and scaled_yres are in case the Thresholder scaled the image
-   * rectangle prior to thresholding. Any coordinates in tesseract's image
-   * must be divided by scale before adding (rect_left, rect_top).
-   * The scaled_yres indicates the effective resolution of the binary image
-   * that tesseract has been given by the Thresholder.
-   * After the constructor, Begin has already been called.
-   */
-  PageIterator(PAGE_RES* page_res, Tesseract* tesseract,
-               int scale, int scaled_yres,
-               int rect_left, int rect_top,
-               int rect_width, int rect_height);
-  virtual ~PageIterator();
-    }
-    
-      SVMenuNode* std_menu = svMenuRoot->AddChild ('Build Config File');
-    
-    // Update the other members if the cost is lower.
-void DPPoint::UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
-                             int32_t n, int32_t sig_x, int64_t sig_xsq) {
-  if (cost < total_cost_) {
-    total_cost_ = cost;
-    total_steps_ = steps;
-    best_prev_ = prev;
-    n_ = n;
-    sig_x_ = sig_x;
-    sig_xsq_ = sig_xsq;
-  }
-}
-    
-    
-    {    ::apache::thrift::TApplicationException x(e.what());
-    oprot->writeMessageBegin('call', ::apache::thrift::protocol::T_EXCEPTION, seqid);
-    x.write(oprot);
-    oprot->writeMessageEnd();
-    oprot->getTransport()->writeEnd();
-    oprot->getTransport()->flush();
-    return;
-  }
-    
-    
-    {};
-    
-      using ::apache::thrift::protocol::TProtocolException;
-    
-    typedef struct _ExtensionManager_getQueryColumns_presult__isset {
-  _ExtensionManager_getQueryColumns_presult__isset() : success(false) {}
-  bool success :1;
-} _ExtensionManager_getQueryColumns_presult__isset;
-    
-    
-    {};
-    
-      // Get an integral value, 0 or 1, for whether a syscall table pointer is modified.
-  auto f1 = osquery::readFile(kKernelSyscallAddrModifiedPath, content);
-  if (f1.ok()) {
-    boost::trim(content);
-    syscall_addr_modified = content;
-  } else {
-    VLOG(1) << 'Cannot read file: ' << kKernelSyscallAddrModifiedPath;
-    return results;
-  }
-    
-     private:
-  bool isNewCodeEnabled(const std::string& key);
-    
-      PerfOutput(PerfOutput const&) = delete;
-  PerfOutput& operator=(PerfOutput const&) = delete;
-    
-    
-    {
-    {} // namespace perf_event_open
-} // namespace osquery
-
-    
-    #include <osquery/utils/system/linux/ebpf/perf_output.h>
-    
-        const int nums[] = {0,4,3,0};
-    vector<int> nums_vec( nums, nums + sizeof(nums)/sizeof(int) );
-    int target = 0;
-    printVec(Solution().twoSum(nums_vec, target));
-    
-    using namespace std;
-    
-    
-    {
-    {        return res;
-    }
-};
-    
-    /// BFS
-/// No need to store level information in the queue :-)
-///
-/// Time Complexity: O(n), where n is the number of nodes in the tree
-/// Space Complexity: O(n)
-class Solution {
+    class U_I18N_API SharedNumberFormat : public SharedObject {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-    }
-    }
-    
-    #include <iostream>
-#include <vector>
-    
-    // My Non-Recursive
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-    }
-    
-    int main() {
-    }
-    
-    namespace {
-const int MAX_RETRY = 2;
-} // namespace
-    
-    std::string DHTResponseMessage::toString() const
-{
-  return fmt('dht response %s TransactionID=%s Remote:%s(%u), id=%s, v=%s, %s',
-             getMessageType().c_str(), util::toHex(getTransactionID()).c_str(),
-             getRemoteNode()->getIPAddress().c_str(),
-             getRemoteNode()->getPort(),
-             util::toHex(getRemoteNode()->getID(), DHT_ID_LENGTH).c_str(),
-             util::torrentPercentEncode(getVersion()).c_str(),
-             toStringOptional().c_str());
-}
-    
-    
-    {  static const std::string R;
+    SharedNumberFormat(NumberFormat *nfToAdopt) : ptr(nfToAdopt) { }
+    virtual ~SharedNumberFormat();
+    const NumberFormat *get() const { return ptr; }
+    const NumberFormat *operator->() const { return ptr; }
+    const NumberFormat &operator*() const { return *ptr; }
+private:
+    NumberFormat *ptr;
+    SharedNumberFormat(const SharedNumberFormat &);
+    SharedNumberFormat &operator=(const SharedNumberFormat &);
 };
     
-    namespace aria2 {
+        int unit;
+    double value;
+    
+    #include <cstring>
+#include <deque>
+#include <exception>
+#include <functional>
+#include <memory>
+#include <string>
+    
+    template <YGMarker M>
+struct MarkerData;
+    
+    #ifdef __cplusplus
+namespace facebook {
+namespace yoga {
+namespace enums {
+    }
+    }
     }
     
-    void DHTRoutingTableSerializer::serialize(const std::string& filename)
-{
-  A2_LOG_INFO(fmt('Saving DHT routing table to %s.', filename.c_str()));
-  std::string filenameTemp = filename;
-  filenameTemp += '__temp';
-  BufferedFile fp(filenameTemp.c_str(), BufferedFile::WRITE);
-  if (!fp) {
-    throw DL_ABORT_EX(
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
-  }
-  char header[8];
-  memset(header, 0, sizeof(header));
-  // magic
-  header[0] = 0xa1u;
-  header[1] = 0xa2u;
-  // format ID
-  header[2] = 0x02u;
-  // version
-  header[6] = 0;
-  header[7] = 0x03u;
+        Size(double width, double height)
+    : width(width)
+    , height(height)
+    {
     }
     
-    namespace aria2 {
-    }
+      /**
+   * This runs the closure in a scope with fbjni's classloader. This should be
+   * the same classloader as the rest of the application and thus anything
+   * running in the closure will have access to the same classes as in a normal
+   * java-create thread.
+   */
+  static void WithClassLoader(std::function<void()>&& runnable);
     
-    #include <algorithm>
-    
-      void addTask(const std::shared_ptr<DHTTask>& task) { queue_.push_back(task); }
-    
-    class DHTTokenUpdateCommand : public TimeBasedCommand {
-private:
-  DHTTokenTracker* tokenTracker_;
-    }
-    
-     private:
-  uint64_t NowMicrosMonotonic(Env* env);
-    
-    /*
- * ThreadPool is a component that will spawn N background threads that will
- * be used to execute scheduled work, The number of background threads could
- * be modified by calling SetBackgroundThreads().
- * */
-class ThreadPool {
- public:
-  virtual ~ThreadPool() {}
-    }
-    
-      // Restore rate limiter. Used to control transfer speed during restore. If
-  // this is not null, restore_rate_limit is ignored.
-  // Default: nullptr
-  std::shared_ptr<RateLimiter> restore_rate_limiter{nullptr};
-    
-      // Builds an openable snapshot of RocksDB on the same disk, which
-  // accepts an output directory on the same disk, and under the directory
-  // (1) hard-linked SST files pointing to existing live SST files
-  // SST files will be copied if output directory is on a different filesystem
-  // (2) a copied manifest files and other files
-  // The directory should not already exist and will be created by this API.
-  // The directory will be an absolute path
-  // log_size_for_flush: if the total log file size is equal or larger than
-  // this value, then a flush is triggered for all the column families. The
-  // default value is 0, which means flush is always triggered. If you move
-  // away from the default, the checkpoint may not contain up-to-date data
-  // if WAL writing is not always enabled.
-  // Flush will always trigger if it is 2PC.
-  virtual Status CreateCheckpoint(const std::string& checkpoint_dir,
-                                  uint64_t log_size_for_flush = 0);
-    
-    #include 'gtest/gtest.h'
-    
-      sender.AddMessage(1, &mpd);
-  EXPECT_EQ(sender.Start(), common::ErrorCode::OK);
-  EXPECT_TRUE(sender.IsRunning());
-  EXPECT_TRUE(sender.enable_log());
-    
-    #include 'gtest/gtest.h'
-    
-    TEST(ByteTest, SetValue) {
-  unsigned char byte_value = 0x1A;
-  Byte value(&byte_value);
-  value.set_value(0x06, 3, 3);
-  EXPECT_EQ(0x32, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x06, 0, 8);
-  EXPECT_EQ(0x06, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x06, 0, 10);
-  EXPECT_EQ(0x06, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x06, 1, 7);
-  EXPECT_EQ(0x0C, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x07, 1, 1);
-  EXPECT_EQ(0x1A, value.get_byte());
-  value.set_value(0x1A);
-  value.set_value(0x07, -1, 1);
-  EXPECT_EQ(0x1A, value.get_byte());
+    template <typename T> template <typename U>
+RefPtr<T>::operator RefPtr<U>() const {
+  static_assert(std::is_base_of<T, U>::value, 'Invalid static cast');
+  return assumeAlreadyReffedOrNull<U>(static_cast<U*>(m_ptr));
 }
     
-    double ClusterGeneralInfo701::longitude_dist(const std::uint8_t* bytes,
-                                             int32_t length) const {
-  Byte t0(bytes + 1);
-  uint32_t x = t0.get_byte(0, 8);
-    }
-    
-    #include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
+    #if ENABLE_FBASSERT
+#define FBASSERTMSGF(expr, msg, ...) !(expr) ? facebook::assertInternal('Assert (%s:%d): ' msg, __FILE__, __LINE__, ##__VA_ARGS__) : (void) 0
+#else
+#define FBASSERTMSGF(expr, msg, ...)
+#endif // ENABLE_FBASSERT
     
     
-    {
-    {  } else {
-    for (uint32_t r = 3; r < num_params; ++r) {
-      for (uint32_t c = 3; c < num_params; ++c) {
-        (*term_matrix)(r, c) = x_pow[r + c - 5];
-      }
-    }
-    (*term_matrix).block(0, 0, num_params, 3) =
-        Eigen::MatrixXd::Zero(num_params, 3);
-    (*term_matrix).block(0, 0, 3, num_params) =
-        Eigen::MatrixXd::Zero(3, num_params);
-  }
-}
-    
-    TEST_F(GemMessageManagerTest, GetRecvProtocols) {
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Accelrpt68::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Brakemotorrpt170::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Brakemotorrpt271::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Brakemotorrpt372::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Brakerpt6c::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Datetimerpt83::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Globalrpt6a::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Headlightrpt77::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Hornrpt79::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Latlonheadingrpt82::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(
-                  Parkingbrakestatusrpt80::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Shiftrpt66::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Steeringmotorrpt173::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Steeringmotorrpt274::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Steeringmotorrpt375::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Steeringrpt16e::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Turnrpt64::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Vehiclespeedrpt6f::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Wheelspeedrpt7a::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Wiperrpt91::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Yawraterpt81::ID) != nullptr);
-}
-    
-    namespace apollo {
-namespace canbus {
-    }
-    }
+    {} // namespace detail
     
     
-    {
-    {
-    {
-    {  double ret = x * 0.001000;
-  return ret;
-}
-}  // namespace gem
-}  // namespace canbus
-}  // namespace apollo
-
-    
-      Byte t2(bytes + 2);
-  t = t2.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-    using ::apollo::drivers::canbus::Byte;
+/**
+ * This needs to be called at library load time, typically in your JNI_OnLoad method.
+ *
+ * The intended use is to return the result of initialize() directly
+ * from JNI_OnLoad and to do nothing else there. Library specific
+ * initialization code should go in the function passed to initialize
+ * (which can be, and probably should be, a C++ lambda). This approach
+ * provides correct error handling and translation errors during
+ * initialization into Java exceptions when appropriate.
+ *
+ * Failure to call this will cause your code to crash in a remarkably
+ * unhelpful way (typically a segfault) while trying to handle an exception
+ * which occurs later.
+ */
+FBEXPORT jint initialize(JavaVM*, std::function<void()>&&) noexcept;
