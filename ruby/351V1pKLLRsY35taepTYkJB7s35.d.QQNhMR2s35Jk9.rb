741cd6294e19ba37bec607a4fdfeec8f8e66c7b5
SUNsung <weatherius@gmail.com>
2019-03-01 10:56:13 +0200

    Rewrite [351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb] File in: 2019-03-01 10:56:12

--- ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
+++ ruby/351V1pKLLRsY35taepTYkJB7s35.d.QQNhMR2s35Jk9.rb
@@ -1,140 +1,78 @@
 
         
-                  relation.update_all(update)
-        end
+              launch_event = builder.new_event(:launch)
+      post_thread = client.post_event(launch_event)
+      unless post_thread.nil?
+        @threads << post_thread
       end
     end
+    
+            [
+          'This will automatically tag your build with the following format: `<grouping>/<lane>/<prefix><build_number>`, where:'.markdown_preserve_newlines,
+          list,
+          'For example, for build 1234 in the 'appstore' lane, it will tag the commit with `builds/appstore/1234`.'
+        ].join('\n')
+      end
+    
+            expect(result).to eq('git tag -am #{message.shellescape} #{tag.shellescape}')
+      end
+    
+          it 'works with both select and exclude regex' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+              select_regex: /\.*m/,
+              exclude_regex: /Test/
+            )
+          end').runner.execute(:test)
+    
+          it 'yields command output' do
+        expect_command('ls', '-la', exitstatus: 1, output: 'Heeeelp! Something went wrong.')
+        Fastlane::Actions.sh('ls', '-la') do |status, result|
+          expect(status.exitstatus).to eq(1)
+          expect(result).to eq('Heeeelp! Something went wrong.')
+        end
+      end
+    
+          new_email = resource_params.fetch(:unconfirmed_email)
+    
+            redirect_to admin_report_path(@report), notice: I18n.t('admin.report_notes.created_msg')
+      else
+        @report_notes = @report.notes.latest
+        @report_history = @report.history
+        @form = Form::StatusBatch.new
+    
+      def require_enabled_api!
+    head 404 unless Setting.activity_api_enabled
   end
 end
 
     
-          def cache_key_iid
-        if object.respond_to?(:noteable_id)
-          object.noteable_id
-        elsif object.respond_to?(:iid)
-          object.iid
-        else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
-        end
-      end
-    end
+      def create
+    @web_subscription&.destroy!
+    
+        def_delegators :@s, :scan_until, :skip_until, :string
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
   end
-end
-
     
-            # Builds a PR from a GitHub API response.
-        #
-        # issue - An instance of `Sawyer::Resource` containing the PR details.
-        def self.from_api_response(pr)
-          assignee =
-            if pr.assignee
-              Representation::User.from_api_response(pr.assignee)
-            end
+    include Sidekiq::Util
     
-        pod 'ObjCPod', path: 'ObjCPod'
-    pod 'SwiftPod', path: 'SwiftPod'
-    pod 'MixedPod', path: 'MixedPod'
-    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
-    
-            self.description = <<-DESC
-          Shows the content of the pods cache as a YAML tree output, organized by pod.
-          If `NAME` is given, only the caches for that pod will be included in the output.
-        DESC
-    
-      def execute
-    signal_deprecation_warning_for_pack
-    
-          it 'returns true if the pipeline is a system pipeline' do
-        expect(subject.system?).to be_truthy
-      end
-    end
-    
-          it 'list the plugins with their versions' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose')
-        result.stdout.split('\n').each do |plugin|
-          expect(plugin).to match(/^logstash-\w+-\w+\s\(\d+\.\d+.\d+(.\w+)?\)/)
+          def requeue
+        Sidekiq.redis do |conn|
+          conn.rpush('queue:#{queue_name}', job)
         end
       end
     end
     
-              context 'and a comment after the last element' do
-            let(:b_comment) { ' # comment b' }
+    module Sidekiq
+  ##
+  # This module is part of Sidekiq core and not intended for extensions.
+  #
+  module Util
+    include ExceptionHandler
     
-          # Returns an array of all the when branches in the `case` statement.
-      #
-      # @return [Array<WhenNode>] an array of `when` nodes
-      def when_branches
-        node_parts[1...-1]
-      end
-    
-          # Checks whether any of the key value pairs in the `hash` literal are on
-      # the same line.
-      #
-      # @note A multiline `pair` is considered to be on the same line if it
-      #       shares any of its lines with another `pair`
-      #
-      # @return [Boolean] whether any `pair` nodes are on the same line
-      def pairs_on_same_line?
-        pairs.each_cons(2).any? { |first, second| first.same_line?(second) }
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `kwsplat` nodes. This will be used in place of a
-    # plain  node when the builder constructs the AST, making its methods
-    # available to all `kwsplat` nodes within RuboCop.
-    class KeywordSplatNode < Node
-      include HashElementNode
-    
-          # Checks whether this `hash` element is on the same line as `other`.
-      #
-      # @note A multiline element is considered to be on the same line if it
-      #       shares any of its lines with `other`
-      #
-      # @return [Boolean] whether this element is on the same line as `other`
-      def same_line?(other)
-        loc.last_line == other.loc.line || loc.line == other.loc.last_line
-      end
-    
-    module RuboCop
-  module AST
-    # Common functionality for nodes that are parameterized:
-    # `send`, `super`, `zsuper`, `def`, `defs`
-    module ParameterizedNode
-      # Checks whether this node's arguments are wrapped in parentheses.
-      #
-      # @return [Boolean] whether this node's arguments are
-      #                   wrapped in parentheses
-      def parenthesized?
-        loc.end && loc.end.is?(')')
-      end
-    
-        # data tar.
-    tar_path(staging_path(''), datatar_path)
-    
-      def to_s(format=nil)
-    return super(format.nil? ? 'NAME-FULLVERSION.EXTENSION' : format)
-  end # def to_s
-end # class FPM::Package::FreeBSD
-
-    
-      option '--channel-update', :flag,
-    'call 'pear channel-update' prior to installation'
-    
-        # Add the tar compression flag if necessary
-    tar_compression_flag(input_path).tap do |flag|
-      args << flag unless flag.nil?
+        def settings
+      Web.settings
     end
-    
-      # Output a zipfile.
-  def output(output_path)
-    output_check(output_path)
-    realpath = Pathname.new(output_path).realdirpath.to_s
-    ::Dir.chdir(staging_path) do
-      safesystem('zip', '-9r', realpath, '.')
-    end
-  end # def output
-end # class FPM::Package::Tar
