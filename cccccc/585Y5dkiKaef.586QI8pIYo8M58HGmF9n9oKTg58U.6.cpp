d38414dc4fe30d2f5dc02bfa85e56e52ab58b1ba
SUNsung <weatherius@gmail.com>
2019-03-01 18:44:02 +0200

    Rewrite [585Y5dkiKaef.586QI8pIYo8M58HGmF9n9oKTg58U.6.cpp] File in: 2019-03-01 18:44:02

--- cccccc/585Y5dkiKaef.586QI8pIYo8M58HGmF9n9oKTg58U.6.cpp
+++ cccccc/585Y5dkiKaef.586QI8pIYo8M58HGmF9n9oKTg58U.6.cpp
@@ -1,118 +1,270 @@
 
         
-        
-    {  /** Pointer to the page_res owned by the API. */
-  PAGE_RES* page_res_;
-  /** Pointer to the Tesseract object owned by the API. */
-  Tesseract* tesseract_;
-  /**
-   * The iterator to the page_res_. Owned by this ResultIterator.
-   * A pointer just to avoid dragging in Tesseract includes.
-   */
-  PAGE_RES_IT* it_;
-  /**
-   * The current input WERD being iterated. If there is an output from OCR,
-   * then word_ is nullptr. Owned by the API
-   */
-  WERD* word_;
-  /** The length of the current word_. */
-  int word_length_;
-  /** The current blob index within the word. */
-  int blob_index_;
-  /**
-   * Iterator to the blobs within the word. If nullptr, then we are iterating
-   * OCR results in the box_word.
-   * Owned by this ResultIterator.
-   */
-  C_BLOB_IT* cblob_it_;
-  /** Control over what to include in bounding boxes. */
-  bool include_upper_dots_;
-  bool include_lower_dots_;
-  /** Parameters saved from the Thresholder. Needed to rebuild coordinates.*/
-  int scale_;
-  int scaled_yres_;
-  int rect_left_;
-  int rect_top_;
-  int rect_width_;
-  int rect_height_;
+          // Deserializes rpc server stats from the incoming 'buf' into *time.  Returns
+  // number of bytes decoded. If the buffer is of insufficient size (it must be
+  // at least kRpcServerStatsSize bytes) or the encoding version or field ID are
+  // unrecognized, *time will be set to 0 and it will return
+  // kEncodeDecodeFailure. Inlined for performance reasons.
+  static size_t Decode(absl::string_view buf, uint64_t* time) {
+    if (buf.size() < kRpcServerStatsSize) {
+      *time = 0;
+      return kEncodeDecodeFailure;
+    }
+    }
+    
+      // implementation of ServerReflectionInfo(stream ServerReflectionRequest) rpc
+  // in ServerReflection service
+  Status ServerReflectionInfo(
+      ServerContext* context,
+      ServerReaderWriter<reflection::v1alpha::ServerReflectionResponse,
+                         reflection::v1alpha::ServerReflectionRequest>* stream)
+      override;
+    
+    DynamicThreadPool::DynamicThread::DynamicThread(DynamicThreadPool* pool)
+    : pool_(pool),
+      thd_('grpcpp_dynamic_pool',
+           [](void* th) {
+             static_cast<DynamicThreadPool::DynamicThread*>(th)->ThreadFunc();
+           },
+           this) {
+  thd_.Start();
+}
+DynamicThreadPool::DynamicThread::~DynamicThread() { thd_.Join(); }
+    
+    #ifndef GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+#define GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+    
+    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
+  uint64_t busy = 0, total = 0;
+  host_cpu_load_info_data_t cpuinfo;
+  mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
+  if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO,
+                      (host_info_t)&cpuinfo, &count) == KERN_SUCCESS) {
+    for (int i = 0; i < CPU_STATE_MAX; i++) total += cpuinfo.cpu_ticks[i];
+    busy = total - cpuinfo.cpu_ticks[CPU_STATE_IDLE];
+  }
+  return std::make_pair(busy, total);
+}
+    
+    
+    { private:
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
 };
     
-      // Try to adjust the blamer bundle.
-  if (orig_bb != nullptr) {
-    // TODO(rays) Looks like a leak to me.
-    // orig_bb should take, rather than copy.
-    word->blamer_bundle = new BlamerBundle();
-    word2->blamer_bundle = new BlamerBundle();
-    orig_bb->SplitBundle(chopped->blobs.back()->bounding_box().right(),
-                         word2->chopped_word->blobs[0]->bounding_box().left(),
-                         wordrec_debug_blamer,
-                         word->blamer_bundle, word2->blamer_bundle);
+    
+    {  inline void SetPrune(const WQSummary &src, size_t maxsize) {
+    if (src.size <= maxsize) {
+      this->CopyFrom(src); return;
+    }
+    const RType begin = src.data[0].rmax;
+    const RType range = src.data[src.size - 1].rmin - src.data[0].rmax;
+    const size_t n = maxsize - 1;
+    data[0] = src.data[0];
+    this->size = 1;
+    // lastidx is used to avoid duplicated records
+    size_t i = 1, lastidx = 0;
+    for (size_t k = 1; k < n; ++k) {
+      RType dx2 =  2 * ((k * range) / n + begin);
+      // find first i such that  d < (rmax[i+1] + rmin[i+1]) / 2
+      while (i < src.size - 1
+             && dx2 >= src.data[i + 1].rmax + src.data[i + 1].rmin) ++i;
+      CHECK(i != src.size - 1);
+      if (dx2 < src.data[i].RMinNext() + src.data[i + 1].RMaxPrev()) {
+        if (i != lastidx) {
+          data[size++] = src.data[i]; lastidx = i;
+        }
+      } else {
+        if (i + 1 != lastidx) {
+          data[size++] = src.data[i + 1]; lastidx = i + 1;
+        }
+      }
+    }
+    if (lastidx != src.size - 1) {
+      data[size++] = src.data[src.size - 1];
+    }
   }
+  /*!
+   * \brief set current summary to be merged summary of sa and sb
+   * \param sa first input summary to be merged
+   * \param sb second input summary to be merged
+   */
+  inline void SetCombine(const WQSummary &sa,
+                         const WQSummary &sb) {
+    if (sa.size == 0) {
+      this->CopyFrom(sb); return;
+    }
+    if (sb.size == 0) {
+      this->CopyFrom(sa); return;
+    }
+    CHECK(sa.size > 0 && sb.size > 0);
+    const Entry *a = sa.data, *a_end = sa.data + sa.size;
+    const Entry *b = sb.data, *b_end = sb.data + sb.size;
+    // extended rmin value
+    RType aprev_rmin = 0, bprev_rmin = 0;
+    Entry *dst = this->data;
+    while (a != a_end && b != b_end) {
+      // duplicated value entry
+      if (a->value == b->value) {
+        *dst = Entry(a->rmin + b->rmin,
+                     a->rmax + b->rmax,
+                     a->wmin + b->wmin, a->value);
+        aprev_rmin = a->RMinNext();
+        bprev_rmin = b->RMinNext();
+        ++dst; ++a; ++b;
+      } else if (a->value < b->value) {
+        *dst = Entry(a->rmin + bprev_rmin,
+                     a->rmax + b->RMaxPrev(),
+                     a->wmin, a->value);
+        aprev_rmin = a->RMinNext();
+        ++dst; ++a;
+      } else {
+        *dst = Entry(b->rmin + aprev_rmin,
+                     b->rmax + a->RMaxPrev(),
+                     b->wmin, b->value);
+        bprev_rmin = b->RMinNext();
+        ++dst; ++b;
+      }
+    }
+    if (a != a_end) {
+      RType brmax = (b_end - 1)->rmax;
+      do {
+        *dst = Entry(a->rmin + bprev_rmin, a->rmax + brmax, a->wmin, a->value);
+        ++dst; ++a;
+      } while (a != a_end);
+    }
+    if (b != b_end) {
+      RType armax = (a_end - 1)->rmax;
+      do {
+        *dst = Entry(b->rmin + aprev_rmin, b->rmax + armax, b->wmin, b->value);
+        ++dst; ++b;
+      } while (b != b_end);
+    }
+    this->size = dst - data;
+    const RType tol = 10;
+    RType err_mingap, err_maxgap, err_wgap;
+    this->FixError(&err_mingap, &err_maxgap, &err_wgap);
+    if (err_mingap > tol || err_maxgap > tol || err_wgap > tol) {
+      LOG(INFO) << 'mingap=' << err_mingap
+                << ', maxgap=' << err_maxgap
+                << ', wgap=' << err_wgap;
+    }
+    CHECK(size <= sa.size + sb.size) << 'bug in combine';
+  }
+  // helper function to print the current content of sketch
+  inline void Print() const {
+    for (size_t i = 0; i < this->size; ++i) {
+      LOG(CONSOLE) << '[' << i << '] rmin=' << data[i].rmin
+                   << ', rmax=' << data[i].rmax
+                   << ', wmin=' << data[i].wmin
+                   << ', v=' << data[i].value;
+    }
+  }
+  // try to fix rounding error
+  // and re-establish invariance
+  inline void FixError(RType *err_mingap,
+                       RType *err_maxgap,
+                       RType *err_wgap) const {
+    *err_mingap = 0;
+    *err_maxgap = 0;
+    *err_wgap = 0;
+    RType prev_rmin = 0, prev_rmax = 0;
+    for (size_t i = 0; i < this->size; ++i) {
+      if (data[i].rmin < prev_rmin) {
+        data[i].rmin = prev_rmin;
+        *err_mingap = std::max(*err_mingap, prev_rmin - data[i].rmin);
+      } else {
+        prev_rmin = data[i].rmin;
+      }
+      if (data[i].rmax < prev_rmax) {
+        data[i].rmax = prev_rmax;
+        *err_maxgap = std::max(*err_maxgap, prev_rmax - data[i].rmax);
+      }
+      RType rmin_next = data[i].RMinNext();
+      if (data[i].rmax < rmin_next) {
+        data[i].rmax = rmin_next;
+        *err_wgap = std::max(*err_wgap, data[i].rmax - rmin_next);
+      }
+      prev_rmax = data[i].rmax;
+    }
+  }
+  // check consistency of the summary
+  inline bool Check(const char *msg) const {
+    const float tol = 10.0f;
+    for (size_t i = 0; i < this->size; ++i) {
+      if (data[i].rmin + data[i].wmin > data[i].rmax + tol ||
+          data[i].rmin < -1e-6f || data[i].rmax < -1e-6f) {
+        LOG(INFO) << '---------- WQSummary::Check did not pass ----------';
+        this->Print();
+        return false;
+      }
+    }
+    return true;
+  }
+};
     
-    // Clip output boxes to input blob boxes for bounds that are within this
-// tolerance. Otherwise, the blob may be chopped and we have to just use
-// the word bounding box.
-const int kBoxClipTolerance = 2;
+    #include <dmlc/registry.h>
+#include <vector>
+#include <utility>
+#include <string>
+#include <functional>
+#include <memory>
+#include './base.h'
+#include './data.h'
+#include './objective.h'
+#include './feature_map.h'
+#include '../../src/common/host_device_vector.h'
     
-    class BLOCK;
-class WERD;
-    
-    // A simple class to provide a dynamic programming solution to a class of
-// 1st-order problems in which the cost is dependent only on the current
-// step and the best cost to that step, with a possible special case
-// of using the variance of the steps, and only the top choice is required.
-// Useful for problems such as finding the optimal cut points in a fixed-pitch
-// (vertical or horizontal) situation.
-// Skeletal Example:
-// DPPoint* array = new DPPoint[width];
-// for (int i = 0; i < width; i++) {
-//   array[i].AddLocalCost(cost_at_i)
-// }
-// DPPoint* best_end = DPPoint::Solve(..., array);
-// while (best_end != nullptr) {
-//   int cut_index = best_end - array;
-//   best_end = best_end->best_prev();
-// }
-// delete [] array;
-class DPPoint {
+    // Define a customized logistic regression objective in C++.
+// Implement the interface.
+class MyLogistic : public ObjFunction {
  public:
-  // The cost function evaluates the total cost at this (excluding this's
-  // local_cost) and if it beats this's total_cost, then
-  // replace the appropriate values in this.
-  typedef int64_t (DPPoint::*CostFunc)(const DPPoint* prev);
+  void Configure(const std::vector<std::pair<std::string, std::string> >& args) override {
+    param_.InitAllowUnknown(args);
+  }
+  void GetGradient(const HostDeviceVector<bst_float> &preds,
+                   const MetaInfo &info,
+                   int iter,
+                   HostDeviceVector<GradientPair> *out_gpair) override {
+    out_gpair->Resize(preds.Size());
+    const std::vector<bst_float>& preds_h = preds.HostVector();
+    std::vector<GradientPair>& out_gpair_h = out_gpair->HostVector();
+    const std::vector<bst_float>& labels_h = info.labels_.HostVector();
+    for (size_t i = 0; i < preds_h.size(); ++i) {
+      bst_float w = info.GetWeight(i);
+      // scale the negative examples!
+      if (labels_h[i] == 0.0f) w *= param_.scale_neg_weight;
+      // logistic transformation
+      bst_float p = 1.0f / (1.0f + std::exp(-preds_h[i]));
+      // this is the gradient
+      bst_float grad = (p - labels_h[i]) * w;
+      // this is the second order gradient
+      bst_float hess = p * (1.0f - p) * w;
+      out_gpair_h.at(i) = GradientPair(grad, hess);
+    }
+  }
+  const char* DefaultEvalMetric() const override {
+    return 'error';
+  }
+  void PredTransform(HostDeviceVector<bst_float> *io_preds) override {
+    // transform margin value to probability.
+    std::vector<bst_float> &preds = io_preds->HostVector();
+    for (size_t i = 0; i < preds.size(); ++i) {
+      preds[i] = 1.0f / (1.0f + std::exp(-preds[i]));
+    }
+  }
+  bst_float ProbToMargin(bst_float base_score) const override {
+    // transform probability to margin value
+    return -std::log(1.0f / base_score - 1.0f);
+  }
     }
     
-      Extension_call_pargs args;
-  args.registry = &registry;
-  args.item = &item;
-  args.request = &request;
-  args.write(oprot_);
-    
-      // Flag for indicating found state utilizing hardware driver info.
-  bool found = false;
-  walk_func([&](const std::string& devname, hardwareDriver* type) {
-    // Get autodetected info..
-    auto resp = smartctl.getDevInfo(devname, '');
-    if (resp.err != NOERR) {
-      LOG(INFO) << 'There was an error retrieving drive information: '
-                << libsmartctl::errStr(resp.err);
-      // Don't return here, keep searching with fulltype.
-    } else {
-      resp.content['device_name'] = devname;
-      results.push_back(std::move(resp.content));
-    }
-    }
-    
-        // output changed array
-    std::cout << object << '\n';
-    
-        // out_of_range.402
-    try
-    {
-        // try to use the array index '-'
-        json::const_reference ref = j.at('/array/-'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+        // the following call will not add an object, because there is already
+    // a value stored at key 'B'
+    auto res2 = null.emplace('B', 'c');
