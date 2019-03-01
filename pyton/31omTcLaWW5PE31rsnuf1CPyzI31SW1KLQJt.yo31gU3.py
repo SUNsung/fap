c8e4d2a93992b5bea84958986293dba0d3f766df
SUNsung <weatherius@gmail.com>
2019-03-01 09:00:39 +0200

    Rewrite [31omTcLaWW5PE31rsnuf1CPyzI31SW1KLQJt.yo31gU3.py] File in: 2019-03-01 09:00:38

--- pyton/31omTcLaWW5PE31rsnuf1CPyzI31SW1KLQJt.yo31gU3.py
+++ pyton/31omTcLaWW5PE31rsnuf1CPyzI31SW1KLQJt.yo31gU3.py
@@ -1,38 +1,16 @@
 
         
-            try:
-        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
-        iam = boto3_conn(module, conn_type='client', resource='iam', region=region, endpoint=ec2_url, **aws_connect_kwargs)
-    except botocore.exceptions.ClientError as e:
-        module.fail_json(msg='Boto3 Client Error - ' + str(e.msg))
+            result['delete_rule'] = list()
+    result['delete_rule_rc'] = list()
+    for rule in current_rules:
+        if rule not in desired_rules:
+            deletions += 1
+            if not module.check_mode:
+                result['delete_rule'].append(rule)
+                rc = gateway.del_nat_rule(**rule)
+                result['delete_rule_rc'].append(rc)
+            result['changed'] = True
+    result['rules_deleted'] = deletions
     
-    - oneandone_firewall_policy:
-    auth_token: oneandone_private_api_key
-    name: ansible-firewall-policy
-    description: Testing creation of firewall policies with ansible
-    rules:
-     -
-       protocol: TCP
-       port_from: 80
-       port_to: 80
-       source: 0.0.0.0
-    wait: true
-    wait_timeout: 500
     
-            _check_mode(module, True)
-        monitoring_policy = oneandone_conn.create_monitoring_policy(
-            monitoring_policy=_monitoring_policy,
-            thresholds=_thresholds,
-            ports=_ports,
-            processes=_processes
-        )
-    
-            change_applied = False
-        if self.state == 'present':
-            if rc != 0:
-                self.module.fail_json(msg='Failed to %s feature %s.'
-                                          ' icinga2 command returned %s' % (feature_enable_str,
-                                                                            self.feature_name,
-                                                                            out))
-    
-        le_path = module.get_bin_path('le', True, ['/usr/local/bin'])
+if __name__ == '__main__':
