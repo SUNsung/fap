1fcd15c0d08f3b4447446f2d415d7d09690b55cb
SUNsung <weatherius@gmail.com>
2019-03-01 14:13:50 +0200

    Rewrite [05GpPtcY5VPaU05.ovFDYA7f.05zu7qTFtWviw05VfI.py] File in: 2019-03-01 14:13:49

--- pyton/05GpPtcY5VPaU05.ovFDYA7f.05zu7qTFtWviw05VfI.py
+++ pyton/05GpPtcY5VPaU05.ovFDYA7f.05zu7qTFtWviw05VfI.py
@@ -1,96 +1,133 @@
 
-DOCUMENTATION = '''
----
-module: iam_server_certificate_facts
-short_description: Retrieve the facts of a server certificate
-description:
-  - Retrieve the attributes of a server certificate
-version_added: '2.2'
-author: 'Allen Sanabria (@linuxdynasty)'
-requirements: [boto3, botocore]
-options:
-  name:
-    description:
-      - The name of the server certificate you are retrieving attributes for.
-    required: true
-extends_documentation_fragment:
-    - aws
-    - ec2
-'''
+        
+            def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+        self.is_available = True
     
-        for (index, rule) in enumerate(desired_rules):
-        try:
-            if rule != current_rules[index]:
-                updates.append((index, rule))
-        except IndexError:
-            additions.append(rule)
+        def get(self, query):
+        '''Get the stored query result from the cache.
     
-    DOCUMENTATION = '''
----
-module: vca_nat
-short_description: add remove nat rules in a gateway  in a vca
-description:
-  - Adds or removes nat rules from a gateway in a vca environment
-version_added: '2.0'
-author: Peter Sprygada (@privateip)
-options:
-    purge_rules:
-      description:
-        - If set to true, it will delete all rules in the gateway that are not given as parameter to this module.
-      type: bool
-      default: false
-    nat_rules:
-      description:
-        - A list of rules to be added to the gateway, Please see examples on valid entries
-      required: True
-      default: false
-extends_documentation_fragment: vca.documentation
-'''
-    
-        try:
-        schema_facts = get_schema_facts(cursor)
-        user_facts = get_user_facts(cursor)
-        role_facts = get_role_facts(cursor)
-        configuration_facts = get_configuration_facts(cursor)
-        node_facts = get_node_facts(cursor)
-        module.exit_json(changed=False,
-                         ansible_facts={'vertica_schemas': schema_facts,
-                                        'vertica_users': user_facts,
-                                        'vertica_roles': role_facts,
-                                        'vertica_configuration': configuration_facts,
-                                        'vertica_nodes': node_facts})
-    except NotSupportedError as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-    except SystemExit:
-        # avoid catching this on python 2.4
-        raise
-    except Exception as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-    
-        def role_add_hostgroup(self, name, item):
-        return self.role_add_member(name=name, item={'hostgroup': item})
-    
-    EXAMPLES = '''
-# Create groups based on the machine architecture
-- group_by:
-    key: machine_{{ ansible_machine }}
+        def __init__(self, license_plate):
+        super(Motorcycle, self).__init__(VehicleSize.MOTORCYCLE, license_plate, spot_size=1)
     
     
-# ===========================================
-# Module execution.
-#
+class UnaccentExtension(CreateExtension):
+    
+        def exists(self, session_key=None):
+        '''
+        This method makes sense when you're talking to a shared resource, but
+        it doesn't matter when you're storing the information in the client's
+        cookie.
+        '''
+        return False
+    
+        objects = BaseSessionManager()
+    
+    '''
+    
+        if not HAS_BOTO3:
+        module.fail_json(msg='boto3 required for this module')
+    
+    from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.gcp import check_min_pkg_version, get_google_cloud_credentials
+    
+        fwservice = gateway._getFirewallService()
+    
+            if group is not None:
+            changed = client.modify_if_diff(name, ipa_role.get('member_group', []), group,
+                                            client.role_add_group,
+                                            client.role_remove_group) or changed
+        if host is not None:
+            changed = client.modify_if_diff(name, ipa_role.get('member_host', []), host,
+                                            client.role_add_host,
+                                            client.role_remove_host) or changed
+    
+        if module.params['user']:
+        params['deploy[local_username]'] = module.params['user']
+    
+    RETURN = '''# '''
+    
+    import time
+import re
     
     
-class BufferFull(UnpackException):
-    pass
+def delete(resource):
+    return request(resource, xml=None, method='DELETE')
+    
+    # Documents to append as an appendix to all manuals.
+#texinfo_appendices = []
+    
+    # This is a very simple benchmark to give you an idea of how fast each step of face recognition will run on your system.
+# Notice that face detection gets very slow at large image sizes. So you might consider running face detection on a
+# scaled down version of your image and then running face encodings on the the full size image.
+    
+    For example, if k=3, and the three closest face images to the given image in the training set are one image of Biden
+and two images of Obama, The result would be 'Obama'.
+    
+        # Display the resulting image
+    cv2.imshow('Video', frame)
+    
+        # Loop over each face found in the frame to see if it's someone we know.
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = '<Unknown Person>'
     
     
-def check(src, should, use_list=0):
-    assert unpackb(src, use_list=use_list) == should
+# Remove the drawing library from memory as per the Pillow docs
+del draw
     
-        N = len(files)
+            face_encoding_a1 = api.face_encodings(img_a1)[0]
+        face_encoding_a2 = api.face_encodings(img_a2)[0]
+        face_encoding_a3 = api.face_encodings(img_a3)[0]
+        face_encoding_b1 = api.face_encodings(img_b1)[0]
     
+    setup(
+    name='face_recognition',
+    version='1.2.3',
+    description='Recognize faces from Python or from the command line',
+    long_description=readme + '\n\n' + history,
+    author='Adam Geitgey',
+    author_email='ageitgey@gmail.com',
+    url='https://github.com/ageitgey/face_recognition',
+    packages=[
+        'face_recognition',
+    ],
+    package_dir={'face_recognition': 'face_recognition'},
+    package_data={
+        'face_recognition': ['models/*.dat']
+    },
+    entry_points={
+        'console_scripts': [
+            'face_recognition=face_recognition.face_recognition_cli:main',
+            'face_detection=face_recognition.face_detection_cli:main'
+        ]
+    },
+    install_requires=requirements,
+    license='MIT license',
+    zip_safe=False,
+    keywords='face_recognition',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
+
     
-def _executable_exists(name):
-    return subprocess.call([CHECK_CMD, name],
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
+        # 载入用户上传的图片
+    img = face_recognition.load_image_file(file_stream)
+    # 为用户上传的图片中的人脸编码
+    unknown_face_encodings = face_recognition.face_encodings(img)
