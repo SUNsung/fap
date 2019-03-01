0fd6be386502106d0779d07b3e4afd19e7f35343
SUNsung <weatherius@gmail.com>
2019-03-02 00:26:29 +0200

    Rewrite [51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb] File in: 2019-03-02 00:26:29

--- ruby/51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb
+++ ruby/51POgilEs0Pmg51K8vfSeAg3Ck51cP04dfWXk6651tqk.rb
@@ -1,99 +1,101 @@
 
         
-          find_files = ->(path) {
-    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
-      path if File.file?(path)
-    end.compact
-  }
+              GivenDailyLike.increment_for(user.id)
+      expect(value_for(user.id, dt)).to eq(2)
+      expect(limit_reached_for(user.id, dt)).to eq(true)
     
-      def up_down(change)
-    change.up do
-      Mention.update_all(mentions_container_type: 'Post')
-      change_column :mentions, :mentions_container_type, :string, null: false
-      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
-    end
+    UserEmail.seed do |ue|
+  ue.id = -1
+  ue.email = 'no_email'
+  ue.primary = true
+  ue.user_id = -1
+end
     
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
+            include ::EachBatch
+      end
     
-    describe StatusMessagesController, :type => :controller do
-  describe '#bookmarklet' do
-    before do
-      sign_in bob, scope: :user
-    end
-    
-        it 'marks all notifications in the current filter as read' do
-      request.env['HTTP_REFERER'] = 'I wish I were spelled right'
-      FactoryGirl.create(:notification, recipient: alice, target: post)
-      FactoryGirl.create(:notification, recipient: alice, type: 'Notifications::StartedSharing')
-      expect(Notification.where(unread: true).count).to eq(2)
-      get :read_all, params: {type: 'started_sharing'}
-      expect(Notification.where(unread: true).count).to eq(1)
-    end
-    
-        context 'on a post you do not partecipate to' do
-      it 'says it is an unprocessable request' do
-        delete :destroy, params: {post_id: post.id}
-        expect(response.code).to eq('422')
+            def preload_stages_warnings
+          # This preloads the number of warnings for every stage, ensuring
+          # that Ci::Stage#has_warnings? doesn't execute any additional
+          # queries.
+          @pipeline.stages.each { |stage| stage.number_of_warnings }
+        end
       end
     end
   end
 end
 
     
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
+          # Returns true if the given value is present in the set.
+      #
+      # raw_key - The key of the set to check.
+      # value - The value to check for.
+      def self.set_includes?(raw_key, value)
+        key = cache_key_for(raw_key)
     
-    # Exit cleanly from an early interrupt
-Signal.trap('INT') { exit 1 }
-    
-    class LogStash::PluginManager::Pack < LogStash::PluginManager::PackCommand
-  option '--tgz', :flag, 'compress package as a tar.gz file', :default => !LogStash::Environment.windows?
-  option '--zip', :flag, 'compress package as a zip file', :default => LogStash::Environment.windows?
-  option '--[no-]clean', :flag, 'clean up the generated dump of plugins', :default => true
-  option '--overwrite', :flag, 'Overwrite a previously generated package file', :default => false
-    
-        class << self
-      def elastic_pack_base_uri
-        env_url = ENV['LOGSTASH_PACK_URL']
-        (env_url.nil? || env_url.empty?) ? DEFAULT_PACK_URL : env_url
+          def milestones(*args)
+        each_object(:milestones, *args)
       end
     
-          options = {:debug => ENV['LS_QA_DEBUG']}
-      puts 'Destroying #{machines}'
-      LogStash::VagrantHelpers.destroy(machines, options)
-      puts 'Bootstrapping #{machines}'
-      LogStash::VagrantHelpers.bootstrap(machines, options)
-    end
-    
-          it 'list the plugins with their versions' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose')
-        result.stdout.split('\n').each do |plugin|
-          expect(plugin).to match(/^logstash-\w+-\w+\s\(\d+\.\d+.\d+(.\w+)?\)/)
+            # issue - An instance of `Gitlab::GithubImport::Representation::Issue`
+        # project - An instance of `Project`
+        # client - An instance of `Gitlab::GithubImport::Client`
+        def initialize(issue, project, client)
+          @issue = issue
+          @project = project
+          @client = client
+          @label_finder = LabelFinder.new(project)
         end
-      end
-    end
     
-          def page_name
-        @name.gsub('-', ' ')
-      end
-    
-          def remove_page_extentions(page_path)
-        Gollum::Markup.formats.values.each do |format|
-          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
+              waiter.jobs_remaining += 1
         end
-        return page_path
+    
+          private
+    
+                platform = target.platform_name
+            case platform
+            when :osx
+              execute_command 'xcodebuild -workspace '#{workspace_path}' -scheme '#{scheme_name}' clean build'
+            when :ios
+              test_flag = (scheme_name.start_with? 'Test') ? 'test' : ''
+    
+          def plugins_string
+        UI::ErrorReport.plugins_string
       end
     
-          def title
-        'Home'
-      end
+    Then(/^the release is created$/) do
+  run_vagrant_command('ls -g #{TestApp.releases_path}')
+end
     
-      if cfg = options[:config]
-    # If the path begins with a '/' it will be considered an absolute path,
-    # otherwise it will be relative to the CWD
-    cfg = File.join(Dir.getwd, cfg) unless cfg.slice(0) == File::SEPARATOR
-    require cfg
+    Given(/^I make (\d+) deployments$/) do |count|
+  step 'all linked files exists in shared path'
+    
+      def run_vagrant_command(command)
+    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
+    return [stdout, stderr] if status.success?
+    raise VagrantSSHCommandError, status
   end
+end
+    
+        def append(key, *values)
+      set(key, Array(fetch(key)).concat(values))
+    end
+    
+          def self.[](host)
+        host.is_a?(Server) ? host : new(host)
+      end
+    
+    set_if_empty :format, :airbrussh
+set_if_empty :log_level, :debug
+    
+          # Checks whether the `when` node has a `then` keyword.
+      #
+      # @return [Boolean] whether the `when` node has a `then` keyword
+      def then?
+        loc.begin && loc.begin.is?('then')
+      end
+    
+        def cop_enabled_at_line?(cop, line_number)
+      cop = cop.cop_name if cop.respond_to?(:cop_name)
+      disabled_line_ranges = cop_disabled_line_ranges[cop]
+      return true unless disabled_line_ranges
