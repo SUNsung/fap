097ec4a50c9a2b3de5c3009da5235f486ae1a071
SUNsung <weatherius@gmail.com>
2019-03-01 05:01:10 +0200

    Rewrite [51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb] File in: 2019-03-01 05:01:09

--- ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
+++ ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
@@ -1,45 +1,74 @@
 
         
-                if b_length > a_length
-          (b_length - a_length).times { a_split.insert(-2, 0) }
-        elsif a_length > b_length
-          (a_length - b_length).times { b_split.insert(-2, 0) }
-        end
-    
-        def assert_index(index)
-      i = index.is_a?(Integer) ? index : @filters.index(filter_const(index))
-      raise 'No such filter to insert: #{index}' unless i
-      i
+            def to_json
+      JSON.generate(as_json)
     end
   end
 end
 
     
-            css('code').each do |node|
-          node.inner_html = node.inner_html.squish
+          def additional_options
+        if self.class.internal_urls
+          super.merge! \
+            only: self.class.internal_urls.to_set,
+            only_patterns: nil,
+            skip: nil,
+            skip_patterns: nil,
+            skip_links: nil,
+            fixed_internal_urls: true
+        else
+          super
+        end
+      end
+    
+        private
+    
+              node.remove_attribute('path')
+          node.remove_attribute('region')
+          node.remove_attribute('linenums')
+          node.remove_attribute('title')
+          node.remove_attribute('language')
+          node.remove_attribute('hidecopy')
+          node.remove_attribute('class')
         end
     
-      gem 'rb-fsevent'
-  gem 'kicker'
-  gem 'awesome_print'
-  gem 'ruby-prof', :platforms => [:ruby]
+            name = name.split(':').first
+    
+            within('#table-filter') do
+          fill_in 'q_created_at_gt', with: '2018/01/01'
+          fill_in 'q_created_at_lt', with: '2018/06/30'
+          fill_in 'q_number_cont', with: 'R100'
+          select 'cart', from: 'q_state_eq'
+          select 'paid', from: 'q_payment_state_eq'
+          select 'pending', from: 'q_shipment_state_eq'
+          fill_in 'q_bill_address_firstname_start', with: 'John'
+          fill_in 'q_bill_address_lastname_start', with: 'Smith'
+          fill_in 'q_email_cont', with: 'john_smith@example.com'
+          fill_in 'q_line_items_variant_sku_eq', with: 'BAG-00001'
+          select 'Promo', from: 'q_promotions_id_in'
+          select 'Spree Test Store', from: 'q_store_id_in'
+          select 'spree', from: 'q_channel_eq'
+        end
+    
+        it 'allows admin to create a variant if there are option types' do
+      within_row(1) { click_icon :edit }
+    
+    desc 'Creates a sandbox application for simulating the Spree code in a deployed Rails app'
+task :sandbox do
+  Bundler.with_clean_env do
+    exec('lib/sandbox.sh')
+  end
 end
 
     
-      desc 'Check code against RuboCop rules'
-  task :rubocop do
-    sh 'bundle exec rubocop lib spec Rakefile'
+            def scope
+          if params[:product_id]
+            Spree::Product.friendly.find(params[:product_id])
+          elsif params[:variant_id]
+            Spree::Variant.find(params[:variant_id])
+          end
+        end
+      end
+    end
   end
-    
-      def pod_prefix
-    File.expand_path('../..', pod_bin)
-  end
-    
-                case platform
-            when 'iOS' then self.platform :ios, '10.0'
-            when 'macOS' then self.platform :macos, '10.10'
-            end
-    
-            self.arguments = [
-          CLAide::Argument.new('NAME', false),
-        ]
+end
