8ccb5b13bc17a2cf1e7399d9b40a2babecdef9dd
SUNsung <weatherius@gmail.com>
2019-03-01 10:10:45 +0200

    Rewrite [23k0dvN4mtGRA23oQxejSE6.U23e4ohH3SLbGY23SlE.cpp] File in: 2019-03-01 10:10:44

--- cccccc/23k0dvN4mtGRA23oQxejSE6.U23e4ohH3SLbGY23SlE.cpp
+++ cccccc/23k0dvN4mtGRA23oQxejSE6.U23e4ohH3SLbGY23SlE.cpp
@@ -1,213 +1,142 @@
 
         
-        STATISTIC(NumRequirementEnvironments, '# of requirement environments');
+        	b2Vec2 origDir(1.0f,0.0f);
+	b2Vec2* resultVecs = new b2Vec2[4*nNodes];// nodes may be visited more than once, unfortunately - change to growable array!
+	int32 nResultVecs = 0;
+	b2PolyNode* currentNode = &nodes[minYIndex];
+	b2PolyNode* startNode = currentNode;
+	b2Assert(currentNode->nConnected > 0);
+	b2PolyNode* nextNode = currentNode->GetRightestConnection(origDir);
+	if (!nextNode) goto CleanUp; // Borked, clean up our mess and return
+	resultVecs[0] = startNode->position;
+	++nResultVecs;
+	while (nextNode != startNode){
+		if (nResultVecs > 4*nNodes){
+			/*
+			printf('%d, %d, %d\n',(int)startNode,(int)currentNode,(int)nextNode);
+			printf('%f, %f -> %f, %f\n',currentNode->position.x,currentNode->position.y, nextNode->position.x, nextNode->position.y);
+				p->printFormatted();
+				printf('Dumping connection graph: \n');
+				for (int32 i=0; i<nNodes; ++i) {
+					printf('nodex[%d] = %f; nodey[%d] = %f;\n',i,nodes[i].position.x,i,nodes[i].position.y);
+					printf('//connected to\n');
+					for (int32 j=0; j<nodes[i].nConnected; ++j) {
+						printf('connx[%d][%d] = %f; conny[%d][%d] = %f;\n',i,j,nodes[i].connected[j]->position.x, i,j,nodes[i].connected[j]->position.y);
+					}
+				}
+				printf('Dumping results thus far: \n');
+				for (int32 i=0; i<nResultVecs; ++i) {
+					printf('x[%d]=map(%f,-3,3,0,width); y[%d] = map(%f,-3,3,height,0);\n',i,resultVecs[i].x,i,resultVecs[i].y);
+				}
+			//*/
+			b2Assert(false); //nodes should never be visited four times apiece (proof?), so we've probably hit a loop...crap
+		}
+		resultVecs[nResultVecs++] = nextNode->position;
+		b2PolyNode* oldNode = currentNode;
+		currentNode = nextNode;
+		//printf('Old node connections = %d; address %d\n',oldNode->nConnected, (int)oldNode);
+		//printf('Current node connections = %d; address %d\n',currentNode->nConnected, (int)currentNode);
+		nextNode = currentNode->GetRightestConnection(oldNode);
+		if (!nextNode) goto CleanUp; // There was a problem, so jump out of the loop and use whatever garbage we've generated so far
+		//printf('nextNode address: %d\n',(int)nextNode);
+	}
     
-    using namespace swift::sys;
-using llvm::StringRef;
+    		inline SourceAlphaMix GetSourceAlphaMix(void)
+		{
+			return m_sourcealphamix;
+		}
     
-        // If this is a skippable suffix, skip it and keep looking.
-    if (nameWordRevIter == nameWordRevIterBegin) {
-      if (auto withoutSuffix = skipTypeSuffix(typeName.Name)) {
-        typeName.Name = *withoutSuffix;
-        typeWords = camel_case::getWords(typeName.Name);
-        typeWordRevIter = typeWords.rbegin();
-        typeWordRevIterEnd = typeWords.rend();
-        continue;
-      }
-    }
+    		typedef struct
+		{
+			unsigned red2 : 4;
+			unsigned red1 : 4;
+			//
+			unsigned green2 : 4;
+			unsigned green1 : 4;
+			//
+			unsigned blue2 : 4;
+			unsigned blue1 : 4;
+			//
+			unsigned flip : 1;
+			unsigned diff : 1;
+			unsigned cw2 : 3;
+			unsigned cw1 : 3;
+			//
+			unsigned int selectors;
+		} Individual;
     
-        /// Returns true if \p loc is inside one of Swift's synthetic buffers.
-    static bool isInSwiftBuffers(clang::FullSourceLoc loc) {
-      StringRef bufName = StringRef(loc.getManager().getBufferName(loc));
-      return bufName == ClangImporter::Implementation::moduleImportBufferName ||
-             bufName == ClangImporter::Implementation::bridgingHeaderBufferName;
-    }
+    #if !defined(OPUS_HAVE_RTCD)
+#define OVERRIDE_OPUS_FFT (1)
     
-    // Bool() allows generating tests with parameters in a set of (false, true).
-//
-// Synopsis:
-// Bool()
-//   - returns a generator producing sequences with elements {false, true}.
-//
-// It is useful when testing code that depends on Boolean flags. Combinations
-// of multiple flags can be tested when several Bool()'s are combined using
-// Combine() function.
-//
-// In the following example all tests in the test case FlagDependentTest
-// will be instantiated twice with parameters false and true.
-//
-// class FlagDependentTest : public testing::TestWithParam<bool> {
-//   virtual void SetUp() {
-//     external_flag = GetParam();
-//   }
-// }
-// INSTANTIATE_TEST_CASE_P(BoolSequence, FlagDependentTest, Bool());
-//
-inline internal::ParamGenerator<bool> Bool() {
-  return Values(false, true);
-}
+       - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
     
-    // A copyable object representing the result of a test part (i.e. an
-// assertion or an explicit FAIL(), ADD_FAILURE(), or SUCCESS()).
-//
-// Don't inherit from TestPartResult as its destructor is not virtual.
-class GTEST_API_ TestPartResult {
- public:
-  // The possible outcomes of a test part (i.e. an assertion or an
-  // explicit SUCCEED(), FAIL(), or ADD_FAILURE()).
-  enum Type {
-    kSuccess,          // Succeeded.
-    kNonFatalFailure,  // Failed but the test can continue.
-    kFatalFailure      // Failed and the test should be terminated.
-  };
-    }
+       - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
     
-    #define GTEST_TEST_NO_THROW_(statement, fail) \
-  GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
-  if (::testing::internal::AlwaysTrue()) { \
-    try { \
-      GTEST_SUPPRESS_UNREACHABLE_CODE_WARNING_BELOW_(statement); \
-    } \
-    catch (...) { \
-      goto GTEST_CONCAT_TOKEN_(gtest_label_testnothrow_, __LINE__); \
-    } \
-  } else \
-    GTEST_CONCAT_TOKEN_(gtest_label_testnothrow_, __LINE__): \
-      fail('Expected: ' #statement ' doesn't throw an exception.\n' \
-           '  Actual: it throws.')
-    
-    // SameSizeTuplePrefixComparator<k, k>::Eq(t1, t2) returns true if the
-// first k fields of t1 equals the first k fields of t2.
-// SameSizeTuplePrefixComparator(k1, k2) would be a compiler error if
-// k1 != k2.
-template <int kSize1, int kSize2>
-struct SameSizeTuplePrefixComparator;
-    
-    
-    {    return -1;
+      CodeAddress base() const {
+    return codeBlock.base();
   }
     
-    
-    {Status startDistributed() {
-  if (!FLAGS_disable_distributed) {
-    Dispatcher::addService(std::make_shared<DistributedRunner>());
-    return Status(0, 'OK');
-  } else {
-    return Status(1, 'Distributed query service not enabled.');
-  }
-}
-} // namespace osquery
+    #endif // HPHP_DATA_STREAM_WRAPPER_H
 
     
-    #include <atomic>
-#include <condition_variable>
-#include <cstddef>
-#include <memory>
-#include <mutex>
-#include <queue>
-#include <string>
+    ///////////////////////////////////////////////////////////////////////////////
     
-     protected:
-  /**
-   * @brief Reconfigure the publisher to start forwarding events to a new
-   * ev2::Subscription.
-   *
-   * @details If necessary, specializations of ev2::SimplePublisher should
-   * override reconfigure() to setup any internal state necessary to start
-   * forwarding events to a new ev2::Subscription.
-   */
-  virtual ExpectedSuccess<Publisher::Error> reconfigure(
-      const std::shared_ptr<SubscriptionT>& sub) {
-    return Success();
-  }
+    int execute_program(int argc, char **argv);
     
-    const std::type_index& Subscription::pubType() const {
-  return pub_type_;
+    #include 'hphp/runtime/base/stream-wrapper.h'
+#include 'hphp/runtime/base/runtime-error.h'
+    
+      virtual ~ExtensionManager_query_args() throw();
+  std::string sql;
+    
+    void ExtensionStatus::__set_uuid(const ExtensionRouteUUID val) {
+  this->uuid = val;
 }
-    
-    
-    {    EXPECT_EQ(sub.avail(), 1);
-  };
-    
-    Status deserializeQueryDataJSON(const std::string& json, QueryDataSet& qd) {
-  rj::Document doc;
-  if (doc.Parse(json.c_str()).HasParseError()) {
-    return Status(1, 'Error serializing JSON');
-  }
-  return deserializeQueryData(doc, qd);
-}
-    
-      // Notifications
-  void error(const QString& msg);
-    
-    
-    {        emit connectionAboutToBeEdited(name);
-        m_connections.removeAll(connection);
-        m_connectionMapping.remove(connection);
-        removeRootItem(serverItem);
-        saveConfig();
-      });
-    
-    class HashKeyModel : public KeyModel<QPair<QByteArray, QByteArray>>
+std::ostream& operator<<(std::ostream& out, const ExtensionStatus& obj)
 {
-public:
-    HashKeyModel(QSharedPointer<RedisClient::Connection> connection,
-                 QByteArray fullPath, int dbIndex, long long ttl);
-    }
-    
-        static void UpdateConfigFromBinary(const BinaryFormat &, uint64_t, Config &/*config*/) {}
-    
-    bool RecognizeBinary(const char *file, ModelType &recognized) {
-  util::scoped_fd fd(util::OpenReadOrThrow(file));
-  if (!IsBinaryFormat(fd.get())) {
-    return false;
-  }
-  Parameters params;
-  ReadHeader(fd.get(), params);
-  recognized = params.fixed.model_type;
-  return true;
+  obj.printTo(out);
+  return out;
 }
     
-    /* Suppose 'foo bar' appears with zero backoff but there is no trigram
- * beginning with these words.  Then, when scoring 'foo bar', the model could
- * return out_state containing 'bar' or even null context if 'bar' also has no
- * backoff and is never followed by another word.  Then the backoff is set to
- * kNoExtensionBackoff.  If the n-gram might be extended, then out_state must
- * contain the full n-gram, in which case kExtensionBackoff is set.  In any
- * case, if an n-gram has non-zero backoff, the full state is returned so
- * backoff can be properly charged.
- * These differ only in sign bit because the backoff is in fact zero in either
- * case.
- */
-const float kNoExtensionBackoff = -0.0;
-const float kExtensionBackoff = 0.0;
-const uint64_t kNoExtensionQuant = 0;
-const uint64_t kExtensionQuant = 1;
     
-    template <class Range> struct SuffixLexicographicLess : public std::binary_function<const Range, const Range, bool> {
-  bool operator()(const Range first, const Range second) const {
-    for (const WordIndex *f = first.end() - 1, *s = second.end() - 1; f >= first.begin() && s >= second.begin(); --f, --s) {
-      if (*f < *s) return true;
-      if (*f > *s) return false;
+    {
+    {
+    {  return results;
+}
+}
+}
+
+    
+    namespace osquery {
     }
-    return first.size() < second.size();
-  }
+    
+    struct mei_response {
+  uint32_t maxlen;
+  uint8_t version;
 };
     
-        Proxy *operator->() { return &proxy_; }
-    const Proxy *operator->() const { return &proxy_; }
-    
-    
-    {  private:
-    util::scoped_memory memory_;
-    std::vector<const char*> map_;
-};
-    
-    boost::program_options::typed_value<std::string> *SizeOption(std::size_t &to, const char *default_value) {
-  return boost::program_options::value<std::string>()->notifier(SizeNotify(to))->default_value(default_value);
+    template <typename MessageType>
+PerfOutput<MessageType>::PerfOutput(PerfOutput&& other)
+    : size_(other.size_), fd_(other.fd_), data_ptr_(other.data_ptr_) {
+  other.fd_ = -1;
+  other.data_ptr_ = nullptr;
 }
     
-    namespace lm {
-namespace interpolate {
-    }
-    }
+    #include <osquery/utils/expected/expected.h>
+#include <osquery/utils/map_take.h>
+#include <osquery/utils/system/linux/cpu.h>
+#include <osquery/utils/system/linux/perf_event/perf_event.h>
+#include <osquery/utils/system/posix/errno.h>
+    
+      auto it = table_.find(inv_key);
+  if (it == table_.end()) {
+    auto const key = createKey(in_event.type, in_event.pid, in_event.tgid);
+    // As far as `return_value` is not used while the event is in the table_
+    // we can use it to preserve the counter value as the age of the event.
+    in_event.return_value = counter_;
+    table_.emplace(key, std::move(in_event));
+    return boost::none;
+  }
