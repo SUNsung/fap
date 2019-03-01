07881a25381efe5b8c61c2795181a0a6877fa3cc
SUNsung <weatherius@gmail.com>
2019-03-01 17:44:07 +0200

    Rewrite [55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp] File in: 2019-03-01 17:44:07

--- cccccc/55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp
+++ cccccc/55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp
@@ -1,304 +1,66 @@
 
         
-        #include 'atom/app/command_line_args.h'
-    
-    UvTaskRunner::UvTaskRunner(uv_loop_t* loop) : loop_(loop) {}
-    
-    
-    {}  // namespace api
-    
-    namespace atom {
+        namespace {
     }
     
-    Net::Net(v8::Isolate* isolate) {
-  Init(isolate);
+      // display::DisplayObserver:
+  void OnDisplayAdded(const display::Display& new_display) override;
+  void OnDisplayRemoved(const display::Display& old_display) override;
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t changed_metrics) override;
+    
+    #include 'atom/browser/api/trackable_object.h'
+#include 'atom/browser/ui/tray_icon.h'
+#include 'atom/browser/ui/tray_icon_observer.h'
+#include 'native_mate/handle.h'
+    
+    namespace IPC {
+class Message;
 }
     
-    namespace atom {
+    #if defined(OS_LINUX)
+typedef PowerObserverLinux PowerObserver;
+#else
+typedef base::PowerObserver PowerObserver;
+#endif  // defined(OS_LINUX)
+    
+    // --------------------------- Structures ---------------------------
+    
+    class AboutProtocolHandler : public net::URLRequestJobFactory::ProtocolHandler {
+ public:
+  AboutProtocolHandler();
+  ~AboutProtocolHandler() override;
     }
     
-    Screen::Screen(v8::Isolate* isolate, display::Screen* screen)
-    : screen_(screen) {
-  screen_->AddObserver(this);
-  Init(isolate);
+    OffScreenOutputDevice::OffScreenOutputDevice(bool transparent,
+                                             const OnPaintCallback& callback)
+    : transparent_(transparent), callback_(callback) {
+  DCHECK(!callback_.is_null());
 }
     
     
-    {  AtomFrameHostMsg_Message_Sync::WriteReplyParams(message_, result);
-  bool success = sender_->Send(message_);
-  message_ = nullptr;
-  sender_ = nullptr;
-  return success;
-}
-    
-      // content::WebContentsObserver implementations:
-  void RenderFrameDeleted(content::RenderFrameHost* rfh) override;
-  void RenderFrameHostChanged(content::RenderFrameHost* old_rfh,
-                              content::RenderFrameHost* new_rfh) override;
-  void FrameDeleted(content::RenderFrameHost* rfh) override;
-    
-    void SavePageHandler::OnDownloadUpdated(download::DownloadItem* item) {
-  if (item->IsDone()) {
-    v8::Isolate* isolate = v8::Isolate::GetCurrent();
-    v8::Locker locker(isolate);
-    v8::HandleScope handle_scope(isolate);
-    if (item->GetState() == download::DownloadItem::COMPLETE) {
-      callback_.Run(v8::Null(isolate));
-    } else {
-      v8::Local<v8::String> error_message =
-          v8::String::NewFromUtf8(isolate, 'Fail to save page');
-      callback_.Run(v8::Exception::Error(error_message));
-    }
-    Destroy(item);
-  }
-}
-    
-    #include 'atom/browser/atom_quota_permission_context.h'
-    
-    #include 'content/public/browser/quota_permission_context.h'
-#include 'content/public/common/storage_quota_params.h'
-    
-    
-    {}  // namespace atom
-    
-    /*!
- * \brief DataBatch of NDArray, returned by Iterator
- */
-struct DataBatch {
-  /*! \brief content of dense data, if this DataBatch is dense */
-  std::vector<NDArray> data;
-  /*! \brief index of image data */
-  std::vector<uint64_t> index;
-  /*! \brief extra data to be fed to the network */
-  std::string extra_data;
-  /*! \brief num of example padded to batch */
-  int num_batch_padd;
-};  // struct DataBatch
-    
-    // Initialization funciton called by caffeOp & caffeLoss
-template<typename Dtype>
-void InitCaffeBlobs(std::vector< ::caffe::Blob<Dtype>*>* v, int n_num) {
-  for (index_t i=0; i < n_num; ++i)
-    v->push_back(new ::caffe::Blob<Dtype>());
-}
-    
-    bool get_png_size(const unsigned char* data, mx_uint data_size, mx_uint *width, mx_uint *height) {
-  if (data[0] == 0x89 && data[1] == 0x50 && data[2] ==0x4E && data[3] == 0x47) {
-    unsigned char const* p = data + 16;
-    *width = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
-    p += 4;
-    *height = ((p[0]*256 + p[1])*256 + p[2])*256 + p[3];
-    return true;
-  } else {
-    return false;
-  }
-}
-    
-      void Start() override {
-    streams_.reset(new StreamManager<kMaxNumGpus, kNumStreamsPerGpu>());
-    task_queue_.reset(new dmlc::ConcurrentBlockingQueue<OprBlock*>());
-    io_task_queue_.reset(new dmlc::ConcurrentBlockingQueue<OprBlock*>());
-    thread_pool_.reset(new ThreadPool(kNumWorkingThreads,
-                                      [this](std::shared_ptr<dmlc::ManualEvent> ready_event) {
-                                        ThreadWorker(task_queue_, ready_event); },
-                                      true));
-    io_thread_pool_.reset(new ThreadPool(1,
-                                         [this](std::shared_ptr<dmlc::ManualEvent> ready_event) {
-                                           ThreadWorker(io_task_queue_, ready_event); },
-                                         true));
-  }
-    
-    Graph DetectInplaceAddTo(Graph g) {
-  nnvm::StorageVector storage_id =
-      g.MoveCopyAttr<nnvm::StorageVector>('storage_id');
-  std::vector<int> storage_inplace_index =
-      g.MoveCopyAttr<std::vector<int> >('storage_inplace_index');
-  static const Op* ewise_plus_op = Op::Get('_grad_add');
-  auto& idx = g.indexed_graph();
-  // reference cont.
-  std::vector<int> ref_count(idx.num_node_entries(), 0);
-  std::vector<int> addto_entry(idx.num_node_entries(), 0);
-  std::vector<int> skip_plus_node(idx.num_nodes(), 0);
-    }
-    
-        /**
-     * Implements {@link Transliterator#handleTransliterate}.
-     * @param text          the buffer holding transliterated and
-     *                      untransliterated text
-     * @param offset        the start and limit of the text, the position
-     *                      of the cursor, and the start and limit of transliteration.
-     * @param incremental   if true, assume more text may be coming after
-     *                      pos.contextLimit. Otherwise, assume the text is complete.
-     */
-    virtual void handleTransliterate(Replaceable& text, UTransPosition& offset,
-                                     UBool isIncremental) const;
-    
-    UBool
-CollationKeyByteSink::Resize(int32_t appendCapacity, int32_t length) {
-    if (buffer_ == NULL) {
-        return FALSE;  // allocation failed before already
-    }
-    int32_t newCapacity = 2 * capacity_;
-    int32_t altCapacity = length + 2 * appendCapacity;
-    if (newCapacity < altCapacity) {
-        newCapacity = altCapacity;
-    }
-    if (newCapacity < 200) {
-        newCapacity = 200;
-    }
-    uint8_t *newBuffer = key_.reallocate(newCapacity, length);
-    if (newBuffer == NULL) {
-        SetNotOk();
-        return FALSE;
-    }
-    buffer_ = reinterpret_cast<char *>(newBuffer);
-    capacity_ = newCapacity;
-    return TRUE;
-}
-    
-    #define LOW_A             ((UChar)0x0061)
-#define LOW_B             ((UChar)0x0062)
-#define LOW_C             ((UChar)0x0063)
-#define LOW_D             ((UChar)0x0064)
-#define LOW_E             ((UChar)0x0065)
-#define LOW_F             ((UChar)0x0066)
-#define LOW_G             ((UChar)0x0067)
-#define LOW_H             ((UChar)0x0068)
-#define LOW_I             ((UChar)0x0069)
-#define LOW_J             ((UChar)0x006a)
-#define LOW_K             ((UChar)0x006B)
-#define LOW_L             ((UChar)0x006C)
-#define LOW_M             ((UChar)0x006D)
-#define LOW_N             ((UChar)0x006E)
-#define LOW_O             ((UChar)0x006F)
-#define LOW_P             ((UChar)0x0070)
-#define LOW_Q             ((UChar)0x0071)
-#define LOW_R             ((UChar)0x0072)
-#define LOW_S             ((UChar)0x0073)
-#define LOW_T             ((UChar)0x0074)
-#define LOW_U             ((UChar)0x0075)
-#define LOW_V             ((UChar)0x0076)
-#define LOW_W             ((UChar)0x0077)
-#define LOW_X             ((UChar)0x0078)
-#define LOW_Y             ((UChar)0x0079)
-#define LOW_Z             ((UChar)0x007A)
-    
-    U_NAMESPACE_BEGIN
-    
-    #endif
-    
-    #ifndef __SHARED_CALENDAR_H__
-#define __SHARED_CALENDAR_H__
-    
-    #endif  // __SIGNIFICANTDIGITINTERVAL_H__
-
-    
-    UnicodeString &
-SmallIntFormatter::format(
-        int32_t smallPositiveValue,
-        const IntDigitCountRange &range,
-        UnicodeString &appendTo) {
-    int32_t digits = range.pin(gDigitCount[smallPositiveValue]);
-    }
-    
-    #include 'unicode/uobject.h'
-#include 'unicode/utypes.h'
-    
-        // There may be multiple strings in the data[] array which begin with
-    // the same prefix (e.g., Cerven and Cervenec (June and July) in Czech).
-    // We keep track of the longest match, and return that.  Note that this
-    // unfortunately requires us to test all array elements.
-    int32_t bestMatchLength = 0, bestMatch = -1;
-    UnicodeString bestMatchName;
-    
-    #endif  // !UCONFIG_NO_FORMATTING
-#endif  // __STANDARDPLURAL_H__
-
-    
-    
-    {        ImGui::Render();
-    }
-    
-    // Changelog:
-// - v0.10: Initial version. Added InputText() / InputTextMultiline() calls with std::string
-    
-    //---- Tip: You can add extra functions within the ImGui:: namespace, here or in your own headers files.
-/*
-namespace ImGui
-{
-    void MyFunction(const char* name, const MyMatrix44& v);
-}
-*/
-
-    
-    // About OpenGL function loaders: 
-// About OpenGL function loaders: modern OpenGL doesn't have a standard header file and requires individual function pointers to be loaded manually. 
-// Helper libraries are often used for this purpose! Here we are supporting a few common ones: gl3w, glew, glad. 
-// You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
-    
-    
-    {        // Create the constant buffer
-        {
-            D3D10_BUFFER_DESC desc;
-            desc.ByteWidth = sizeof(VERTEX_CONSTANT_BUFFER);
-            desc.Usage = D3D10_USAGE_DYNAMIC;
-            desc.BindFlags = D3D10_BIND_CONSTANT_BUFFER;
-            desc.CPUAccessFlags = D3D10_CPU_ACCESS_WRITE;
-            desc.MiscFlags = 0;
-            g_pd3dDevice->CreateBuffer(&desc, NULL, &g_pVertexConstantBuffer);
-        }
-    }
-    
-        ImGui_ImplDX12_CreateFontsTexture();
-    
-        // Backup the DX9 transform (DX9 documentation suggests that it is included in the StateBlock but it doesn't appear to)
-    D3DMATRIX last_world, last_view, last_projection;
-    g_pd3dDevice->GetTransform(D3DTS_WORLD, &last_world);
-    g_pd3dDevice->GetTransform(D3DTS_VIEW, &last_view);
-    g_pd3dDevice->GetTransform(D3DTS_PROJECTION, &last_projection);
-    
-    // CHANGELOG 
-// (minor and older changes stripped away, please see git history for details)
-//  2018-11-30: Misc: Setting up io.BackendRendererName so it can be displayed in the About Window.
-//  2018-08-03: OpenGL: Disabling/restoring GL_LIGHTING and GL_COLOR_MATERIAL to increase compatibility with legacy OpenGL applications.
-//  2018-06-08: Misc: Extracted imgui_impl_opengl2.cpp/.h away from the old combined GLFW/SDL+OpenGL2 examples.
-//  2018-06-08: OpenGL: Use draw_data->DisplayPos and draw_data->DisplaySize to setup projection matrix and clipping rectangle.
-//  2018-02-16: Misc: Obsoleted the io.RenderDrawListsFn callback and exposed ImGui_ImplGlfwGL2_RenderDrawData() in the .h file so you can call it yourself.
-//  2017-09-01: OpenGL: Save and restore current polygon mode.
-//  2016-09-10: OpenGL: Uploading font texture as RGBA32 to increase compatibility with users shaders (not ideal).
-//  2016-09-05: OpenGL: Fixed save and restore of current scissor rectangle.
-    
-        if (style.FrameBorderSize > 0.0f)
     {
-        window->DrawList->AddCircle(center+ImVec2(1,1), radius, GetColorU32(ImGuiCol_BorderShadow), 16, style.FrameBorderSize);
-        window->DrawList->AddCircle(center, radius, GetColorU32(ImGuiCol_Border), 16, style.FrameBorderSize);
-    }
-    
-      uint64_t sleep_debt = 0;
-  uint64_t time_since_last_refill = 0;
-  if (last_refill_time_ != 0) {
-    if (last_refill_time_ > time_now) {
-      sleep_debt = last_refill_time_ - time_now;
-    } else {
-      time_since_last_refill = time_now - last_refill_time_;
-      bytes_left_ +=
-          static_cast<uint64_t>(static_cast<double>(time_since_last_refill) /
-                                kMicrosPerSecond * delayed_write_rate_);
-      if (time_since_last_refill >= kRefillInterval &&
-          bytes_left_ > num_bytes) {
-        // If refill interval already passed and we have enough bytes
-        // return without extra sleeping.
-        last_refill_time_ = time_now;
-        bytes_left_ -= num_bytes;
-        return 0;
+    {
+    {      // Test write Symbol
+      std::vector<unsigned char> buffer2(
+        CompressedBufferWriter::CalculateBufferSize(input.size(),
+          alphabet_size));
+      for (int i = 0; i < input.size(); i++) {
+        cbw.WriteSymbol(buffer2.data(), input[i], i);
       }
+      CompressedIterator<int> ci2(buffer.data(), alphabet_size);
+      std::vector<int> output2(input.size());
+      for (int i = 0; i < input.size(); i++) {
+        output2[i] = ci2[i];
+      }
+      ASSERT_TRUE(input == output2);
     }
   }
+}
     
-      // number of single-deletes which do not meet a put
-  uint64_t num_single_del_fallthru;
-    
-    #include 'rocksdb/perf_level.h'
-    
-    using atomics::atomic_thread_fence;
-using atomics::atomic_signal_fence;
+    // implementing configure.
+template<typename PairIter>
+inline void GradientBooster::Configure(PairIter begin, PairIter end) {
+  std::vector<std::pair<std::string, std::string> > vec(begin, end);
+  this->Configure(vec);
+}
