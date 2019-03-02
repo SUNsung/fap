bc5619d08266935d843e1a48b7f06a5ea0becd0c
SUNsung <weatherius@gmail.com>
2019-03-02 12:40:28 +0200

    Rewrite [43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py] File in: 2019-03-02 12:40:28

--- pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
+++ pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
@@ -1,232 +1,140 @@
 
         
-                full_path = request.get_full_path()
-        current_site = get_current_site(request)
+        import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     
-        def create_model_instance(self, data):
-        '''
-        Return a new instance of the session model object, which represents the
-        current session state. Intended to be used for saving the session data
-        to the database.
-        '''
-        return self.model(
-            session_key=self._get_or_create_session_key(),
-            session_data=self.encode(data),
-            expire_date=self.get_expiry_date(),
-        )
+    print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
     
-    
-class SessionManager(BaseSessionManager):
-    use_in_migrations = True
-    
-        print('duration: %0.3fs' % delta)
-    print('rmse: %f' % rmse(Y_test, clf.predict(X_test)))
-    print('mean coef abs diff: %f' % abs(ref_coef - clf.coef_.ravel()).mean())
-    return delta
+    atom_template = atom_template.replace('@TIMESTAMP@', now_iso)
     
     
-def bench_scikit_tree_classifier(X, Y):
-    '''Benchmark with scikit-learn decision tree classifier'''
+class TestAgeRestriction(unittest.TestCase):
+    def _assert_restricted(self, url, filename, age, old_age=None):
+        self.assertTrue(_download_restricted(url, filename, old_age))
+        self.assertFalse(_download_restricted(url, filename, age))
     
-        if revision is None:
-        return
-    if domain not in ('py', 'pyx'):
-        return
-    if not info.get('module') or not info.get('fullname'):
-        return
+            self.extract(**kwargs)
     
-    
-def plot_influence(conf, mse_values, prediction_times, complexities):
     '''
-    Plot influence of model complexity on both accuracy and latency.
-    '''
-    plt.figure(figsize=(12, 6))
-    host = host_subplot(111, axes_class=Axes)
-    plt.subplots_adjust(right=0.75)
-    par1 = host.twinx()
-    host.set_xlabel('Model Complexity (%s)' % conf['complexity_label'])
-    y1_label = conf['prediction_performance_label']
-    y2_label = 'Time (s)'
-    host.set_ylabel(y1_label)
-    par1.set_ylabel(y2_label)
-    p1, = host.plot(complexities, mse_values, 'b-', label='prediction error')
-    p2, = par1.plot(complexities, prediction_times, 'r-',
-                    label='latency')
-    host.legend(loc='upper right')
-    host.axis['left'].label.set_color(p1.get_color())
-    par1.axis['right'].label.set_color(p2.get_color())
-    plt.title('Influence of Model Complexity - %s' % conf['estimator'].__name__)
-    plt.show()
+http://www.tudou.com/programs/view/html5embed.action?type=0&amp;code=3LS_URGvl54&amp;lcode=&amp;resourceId=0_06_05_99
+'''
+tudou_embed_patterns = [ 'tudou\.com[a-zA-Z0-9\/\?=\&\.\;]+code=([a-zA-Z0-9_-]+)\&',
+                         'www\.tudou\.com/v/([a-zA-Z0-9_-]+)/[^']*v\.swf'
+                       ]
     
-    The dataset is generated using the ``make_biclusters`` function, which
-creates a matrix of small values and implants bicluster with large
-values. The rows and columns are then shuffled and passed to the
-Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
-make biclusters contiguous shows how accurately the algorithm found
-the biclusters.
+        type, ext, size = url_info(urls[0], True)
+    size = urls_size(urls)
     
-    import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+    #----------------------------------------------------------------------
+def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    '''wrapper'''
+    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
+    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
+    hostname = urlparse(url).hostname
+    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
+        return False
+    upid = match1(url, r'.+/content/(\w+)')
     
-        def open(self, mode='r', buffering=-1, encoding=None,
-             errors=None, newline=None):
-        '''
-        Open the file pointed by this path and return a file object, as
-        the built-in open() function does.
-        '''
-        if self._closed:
-            self._raise_closed()
-        return io.open(self, mode, buffering, encoding, errors, newline,
-                       opener=self._opener)
-    
-    from email.message import EmailMessage
-from email.headerregistry import Address
-from email.utils import make_msgid
-    
-    
-def main():
-    parser = ArgumentParser(description='''\
-Send the contents of a directory as a MIME message.
-Unless the -o option is given, the email is sent by forwarding to your local
-SMTP server, which then does the normal delivery process.  Your local machine
-must be running an SMTP server.
-''')
-    parser.add_argument('-d', '--directory',
-                        help='''Mail the contents of the specified directory,
-                        otherwise use the current directory.  Only the regular
-                        files in the directory are sent, and we don't recurse to
-                        subdirectories.''')
-    parser.add_argument('-o', '--output',
-                        metavar='FILE',
-                        help='''Print the composed message to FILE instead of
-                        sending the message to the SMTP server.''')
-    parser.add_argument('-s', '--sender', required=True,
-                        help='The value of the From: header (required)')
-    parser.add_argument('-r', '--recipient', required=True,
-                        action='append', metavar='RECIPIENT',
-                        default=[], dest='recipients',
-                        help='A To: header value (at least one required)')
-    args = parser.parse_args()
-    directory = args.directory
-    if not directory:
-        directory = '.'
-    # Create the message
-    msg = EmailMessage()
-    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
-    msg['To'] = ', '.join(args.recipients)
-    msg['From'] = args.sender
-    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
-    
-            try:
-            print(list(pool.imap(f, list(range(10)))))
-        except ZeroDivisionError:
-            print('\tGot ZeroDivisionError as expected from list(pool.imap())')
+            if 'title' not in kwargs:
+            url = 'http://pv.funshion.com/v5/video/profile/?id={}&cl=mweb&uc=111'.format(self.vid)
+            meta = json.loads(get_content(url))
+            self.title = meta['name']
         else:
-            raise AssertionError('expected ZeroDivisionError')
+            self.title = kwargs['title']
     
-        def __new__(cls, name, bases, attrs):
-        new_cls = type.__new__(cls, name, bases, attrs)
-        '''
-            Here the name of the class is used as key but it could be any class
-            parameter.
-        '''
-        cls.REGISTRY[new_cls.__name__] = new_cls
-        return new_cls
-    
-    *What does this example do?
-In this example queue.Queue is used to create the pool (wrapped in a
-custom ObjectPool object to use with the with statement), and it is
-populated with strings.
-As we can see, the first string object put in 'yam' is USED by the
-with statement. But because it is released back into the pool
-afterwards it is reused by the explicit call to sample_queue.get().
-Same thing happens with 'sam', when the ObjectPool created insided the
-function is deleted (by the GC) and the object is returned.
+        def scan_completed_callback(scan_wizard, result, address, name):
+        '''Restart scan wizard to constantly check for new buttons.'''
+        if result == pyflic.ScanWizardResult.WizardSuccess:
+            _LOGGER.info('Found new button %s', address)
+        elif result != pyflic.ScanWizardResult.WizardFailedTimeout:
+            _LOGGER.warning(
+                'Failed to connect to button %s. Reason: %s', address, result)
     
     
-# example of graph usage
-graph = {'A': ['B', 'C'], 'B': ['C', 'D'], 'C': ['D'], 'D': ['C'], 'E': ['F'], 'F': ['C']}
+def get_scanner(hass, config):
+    '''Validate the configuration and return an Actiontec scanner.'''
+    scanner = ActiontecDeviceScanner(config[DOMAIN])
+    return scanner if scanner.success_init else None
+    
+            self.last_results = {}
+        response = self._make_request()
+        if response.status_code != 200:
+            _LOGGER.error(
+                'Got HTTP status code %d when getting device list',
+                response.status_code)
+            return False
+        try:
+            data = response.json()
+            result = data['responses'][0]
+            devices = result['output']['devices']
+            for device in devices:
+                macs = device['knownMACAddresses']
+                if not macs:
+                    _LOGGER.warning(
+                        'Skipping device without known MAC address')
+                    continue
+                mac = macs[-1]
+                connections = device['connections']
+                if not connections:
+                    _LOGGER.debug('Device %s is not connected', mac)
+                    continue
+    
+        def get_thomson_data(self):
+        '''Retrieve data from THOMSON and return parsed result.'''
+        try:
+            telnet = telnetlib.Telnet(self.host)
+            telnet.read_until(b'Username : ')
+            telnet.write((self.username + '\r\n').encode('ascii'))
+            telnet.read_until(b'Password : ')
+            telnet.write((self.password + '\r\n').encode('ascii'))
+            telnet.read_until(b'=>')
+            telnet.write(('hostmgr list\r\n').encode('ascii'))
+            devices_result = telnet.read_until(b'=>').split(b'\r\n')
+            telnet.write('exit\r\n'.encode('ascii'))
+        except EOFError:
+            _LOGGER.exception('Unexpected response from router')
+            return
+        except ConnectionRefusedError:
+            _LOGGER.exception(
+                'Connection refused by router. Telnet enabled?')
+            return
+    
+    CONF_PAGE_ACCESS_TOKEN = 'page_access_token'
+BASE_URL = 'https://graph.facebook.com/v2.6/me/messages'
+CREATE_BROADCAST_URL = 'https://graph.facebook.com/v2.11/me/message_creatives'
+SEND_BROADCAST_URL = 'https://graph.facebook.com/v2.11/me/broadcast_messages'
+    
+        def __init__(self, hass, filename, add_timestamp):
+        '''Initialize the service.'''
+        self.filepath = os.path.join(hass.config.config_dir, filename)
+        self.add_timestamp = add_timestamp
     
     
-class MobileView(object):
-    def show_index_page(self):
-        print('Displaying mobile index page')
+async def get_service(hass, config, discovery_info=None):
+    '''Get the Flock notification service.'''
+    access_token = config.get(CONF_ACCESS_TOKEN)
+    url = '{}{}'.format(_RESOURCE, access_token)
+    session = async_get_clientsession(hass)
     
-    '''Various network 'heads' for classification and bounding box prediction.
-    
-    
-def add_single_gpu_param_update_ops(model, gpu_id):
-    # Learning rate of 0 is a dummy value to be set properly at the
-    # start of training
-    lr = model.param_init_net.ConstantFill(
-        [], 'lr', shape=[1], value=0.0
-    )
-    one = model.param_init_net.ConstantFill(
-        [], 'one', shape=[1], value=1.0
-    )
-    wd = model.param_init_net.ConstantFill(
-        [], 'wd', shape=[1], value=cfg.SOLVER.WEIGHT_DECAY
-    )
-    # weight decay of GroupNorm's parameters
-    wd_gn = model.param_init_net.ConstantFill(
-        [], 'wd_gn', shape=[1], value=cfg.SOLVER.WEIGHT_DECAY_GN
-    )
-    for param in model.TrainableParams(gpu_id=gpu_id):
-        logger.debug('param ' + str(param) + ' will be updated')
-        param_grad = model.param_to_grad[param]
-        # Initialize momentum vector
-        param_momentum = model.param_init_net.ConstantFill(
-            [param], param + '_momentum', value=0.0
-        )
-        if param in model.biases:
-            # Special treatment for biases (mainly to match historical impl.
-            # details):
-            # (1) Do not apply weight decay
-            # (2) Use a 2x higher learning rate
-            model.Scale(param_grad, param_grad, scale=2.0)
-        elif param in model.gn_params:
-            # Special treatment for GroupNorm's parameters
-            model.WeightedSum([param_grad, one, param, wd_gn], param_grad)
-        elif cfg.SOLVER.WEIGHT_DECAY > 0:
-            # Apply weight decay to non-bias weights
-            model.WeightedSum([param_grad, one, param, wd], param_grad)
-        # Update param_grad and param_momentum in place
-        model.net.MomentumSGDUpdate(
-            [param_grad, param_momentum, lr, param],
-            [param_grad, param_momentum, param],
-            momentum=cfg.SOLVER.MOMENTUM
-        )
-
+                # Check response
+            buffer = sock.recv(1024)
+            if buffer != b'LANnouncer: OK':
+                _LOGGER.error('Error sending data to Lannnouncer: %s',
+                              buffer.decode())
     
     
-def add_fpn_retinanet_outputs(model, blobs_in, dim_in, spatial_scales):
-    '''RetinaNet head. For classification and box regression, we can chose to
-    have the same conv tower or a separate tower. 'bl_feat_list' stores the list
-    of feature blobs for bbox prediction. These blobs can be shared cls feature
-    blobs if we share the tower or else are independent blobs.
-    '''
-    dim_out = dim_in
-    k_max = cfg.FPN.RPN_MAX_LEVEL  # coarsest level of pyramid
-    k_min = cfg.FPN.RPN_MIN_LEVEL  # finest level of pyramid
-    A = len(cfg.RETINANET.ASPECT_RATIOS) * cfg.RETINANET.SCALES_PER_OCTAVE
-    
-        if len(data.shape) == 1:
-        ret = np.empty((count, ), dtype=data.dtype)
-        ret.fill(fill)
-        ret[inds] = data
-    else:
-        ret = np.empty((count, ) + data.shape[1:], dtype=data.dtype)
-        ret.fill(fill)
-        ret[inds, :] = data
-    return ret
-    
-    from detectron.core.config import cfg
-import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
-import detectron.roi_data.retinanet as retinanet_roi_data
-import detectron.roi_data.rpn as rpn_roi_data
-import detectron.utils.blob as blob_utils
-    
-        # Map up to original set of anchors
-    labels = data_utils.unmap(labels, total_anchors, inds_inside, fill=-1)
-    bbox_targets = data_utils.unmap(bbox_targets, total_anchors, inds_inside, fill=0)
+class MycroftNotificationService(BaseNotificationService):
+    '''The Mycroft Notification Service.'''
