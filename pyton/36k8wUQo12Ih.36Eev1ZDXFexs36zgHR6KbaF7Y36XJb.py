b18567f26c68211403a097528dff11c223f983de
SUNsung <weatherius@gmail.com>
2019-03-02 16:21:59 +0200

    Rewrite [36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py] File in: 2019-03-02 16:21:58

--- pyton/36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py
+++ pyton/36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py
@@ -1,75 +1,78 @@
-    Args:
-      z_t_bxu: sample to compute likelihood for at time t.
-      z_tm1_bxu (optional): sample condition probability of z_t upon.
-    
-    # Getting hard vs. easy data can be a little stochastic, so we set the seed.
-    
-    # write out the dataset
-write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
-print ('Saved to ', os.path.join(FLAGS.save_dir,
-                                 FLAGS.datafile_name + '_' + dataset_name))
+
+        
+            entry_points = {'console_scripts': proj_info['console_scripts']}
+)
 
     
-      Returns:
-    log_sum_exp of the arguments.
-  '''
-  m = tf.reduce_max(x_k)
-  x1_k = x_k - m
-  u_k = tf.exp(x1_k)
-  z = tf.reduce_sum(u_k)
-  return tf.log(z) + m
+    #----------------------------------------------------------------------
+def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+    '''wrapper'''
+    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
+    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
+    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
+    hostname = urlparse(url).hostname
+    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
+        return False
+    upid = match1(url, r'.+/content/(\w+)')
+    
+                print_info(site_info, title_i, ext, size)
+            if not info_only:
+                download_urls([real_url], title_i, ext, size, output_dir, merge = merge)
+    
+        m3u8_url = get_m3u8_url(stream_id)
+    
+        def assert_failure(self, res, code=None):
+        self.assertEqual(res.status, 200)
+        body = res.body
+        body = json.loads(body)
+        self.assertTrue('json' in body)
+        errors = body['json'].get('errors')
+        self.assertTrue(code in [x[0] for x in errors])
+        data = body['json'].get('data')
+        self.assertFalse(bool(data))
+    
+        def test_subdomain_not_in(self):
+        cfg = {'subdomains': []}
+        feature_state = self.world._make_state(cfg)
+        self.assertFalse(feature_state.is_enabled(subdomain='beta'))
+        self.assertFalse(feature_state.is_enabled(subdomain=''))
+    
+        def test_no_upgrade_oauth(self):
+        # When g.domain == g.oauth_domain we might send a cookie even though
+        # we're not using it for auth. Don't echo it back in responses.
+        c.oauth_user = self._account
+        self._setSessionCookie(days_old=60)
+        upgrade_cookie_security()
+        self.assertFalse(c.cookies[g.login_cookie].dirty)
+    
+        def test_submit_event(self):
+        self.patch_liveconfig('events_collector_submit_sample_rate', 1.0)
+        new_link = MagicMock(name='new_link')
+        context = MagicMock(name='context')
+        request = MagicMock(name='request')
+        request.ip = '1.2.3.4'
+        g.events.submit_event(new_link, context=context, request=request)
+    
+        def test_resize(self):
+        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
+                      height=800)
     
     
-def get_batch(generator, batch_size, num_steps, max_word_length, pad=False):
-  '''Read batches of input.'''
-  cur_stream = [None] * batch_size
+from r2.lib.utils.reddit_agent_parser import (
+    AlienBlueDetector,
+    BaconReaderDetector,
+    detect,
+    McRedditDetector,
+    NarwhalForRedditDetector,
+    ReaditDetector,
+    RedditAndroidDetector,
+    RedditIsFunDetector,
+    RedditIOSDetector,
+    RedditSyncDetector,
+    RelayForRedditDetector)
+from r2.tests import RedditTestCase
     
-        batch_size, num_timesteps = self.shape
-    softmax = softmax.reshape((num_timesteps, batch_size, -1))
-    softmax = np.transpose(softmax, [1, 0, 2])
-    probs = np.array([[softmax[row, col, target_ids[row, col]]
-                       for col in range(num_timesteps)]
-                      for row in range(batch_size)])
-    print(probs)
-    return probs
-    
-        self.bos_char = free_ids[0]  # <begin sentence>
-    self.eos_char = free_ids[1]  # <end sentence>
-    self.bow_char = free_ids[2]  # <begin word>
-    self.eow_char = free_ids[3]  # <end word>
-    self.pad_char = free_ids[4]  # <padding>
-    
-      Returns:
-    loss:  Scalar tf.float32 loss.
-    
-        ## Calculate the Advantages, A(s,a) = Q(s,a) - \hat{V}(s).
-    advantages = []
-    for t in xrange(FLAGS.sequence_length):
-      log_probability = log_probs_list[t]
-      cum_advantage = tf.zeros(shape=[FLAGS.batch_size / 2])
-    
-      # Common elements to Generator and Discriminator.
-  embedding = [
-      v for v in tf.trainable_variables()
-      if v.op.name == str(model) + '/rnn/embedding'
-  ][0]
-  lstm_w_0 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      str(model) + '/rnn/GenericMultiRNNCell/Cell0/Alien/rnn_builder/big_h_mat'
-  ][0]
-  lstm_b_0 = [
-      v for v in tf.trainable_variables()
-      if v.op.name == str(model) +
-      '/rnn/GenericMultiRNNCell/Cell0/Alien/rnn_builder/big_inputs_mat'
-  ][0]
-  lstm_w_1 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      str(model) + '/rnn/GenericMultiRNNCell/Cell1/Alien/rnn_builder/big_h_mat'
-  ][0]
-  lstm_b_1 = [
-      v for v in tf.trainable_variables()
-      if v.op.name == str(model) +
-      '/rnn/GenericMultiRNNCell/Cell1/Alien/rnn_builder/big_inputs_mat'
-  ][0]
+        def test_scb(self):
+        scb = stats.StringCountBuffer()
+        self.assertEquals([], list(scb.flush()))
