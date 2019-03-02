676fec972ca6d7db259813e660f2762f242dcbd4
SUNsung <weatherius@gmail.com>
2019-03-02 03:09:28 +0200

    Rewrite [20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp] File in: 2019-03-02 03:09:27

--- cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
+++ cccccc/20XZzEnRg4Qeo20RL1Avb6tBI20LaU4p6wlFGg20MFR.cpp
@@ -1,157 +1,395 @@
 
         
-            const QString &getName() const { return name; }
+            assert(info.name.size() < (2 << 10) && 'name failed sanity check');
     
-    namespace Ui {
-    class SignVerifyMessageDialog;
+    using namespace swift::sys;
+using llvm::StringRef;
+    
+    /// Match a word within a name to a word within a type.
+static bool matchNameWordToTypeWord(StringRef nameWord, StringRef typeWord) {
+  // If the name word is longer, there's no match.
+  if (nameWord.size() > typeWord.size()) return false;
+    }
+    
+    
+    
+    /// Maintain a set of known CF types.
+static bool isKnownCFTypeName(StringRef name) {
+  return std::binary_search(KnownCFTypes, KnownCFTypes + NumKnownCFTypes,
+                            name, SortByLengthComparator());
 }
     
-    #endif
-
+    // Import As Member -- attempt to import C global functions and variables as
+// members on types or instances.
     
-        // Special handling for null case
-    // (needed because string comparison reads the null as end-of-string)
-    BOOST_TEST_MESSAGE(std::string('CheckParseTorReplyMapping(Null=\'\\0\')'));
-    auto ret = ParseTorReplyMapping('Null=\'\\0\'');
-    BOOST_CHECK_EQUAL(ret.size(), 1U);
-    auto r_it = ret.begin();
-    BOOST_CHECK_EQUAL(r_it->first, 'Null');
-    BOOST_CHECK_EQUAL(r_it->second.size(), 1U);
-    BOOST_CHECK_EQUAL(r_it->second[0], '\0');
     
-            void format(std::ostream& out, const char* fmtBegin,
-                    const char* fmtEnd, int ntrunc) const
-        {
-            assert(m_value);
-            assert(m_formatImpl);
-            m_formatImpl(out, fmtBegin, fmtEnd, ntrunc, m_value);
+    {  // let h = b = the number of basic code points in the input
+  // copy them to the output in order...
+  size_t h = 0;
+  for (auto C : InputCodePoints) {
+    if (C < 0x80) {
+      ++h;
+      OutPunycode.push_back(C);
+    }
+    if (!isValidUnicodeScalar(C)) {
+      OutPunycode.clear();
+      return false;
+    }
+  }
+  size_t b = h;
+  // ...followed by a delimiter if b > 0
+  if (b > 0)
+    OutPunycode.push_back(delimiter);
+  
+  while (h < InputCodePoints.size()) {
+    // let m = the minimum code point >= n in the input
+    uint32_t m = 0x10FFFF;
+    for (auto codePoint : InputCodePoints) {
+      if (codePoint >= n && codePoint < m)
+        m = codePoint;
+    }
+    
+    delta = delta + (m - n) * (h + 1);
+    n = m;
+    for (auto c : InputCodePoints) {
+      if (c < n) ++delta;
+      if (c == n) {
+        int q = delta;
+        for (int k = base; ; k += base) {
+          int t = k <= bias ? tmin
+                : k >= bias + tmax ? tmax
+                : k - bias;
+          
+          if (q < t) break;
+          OutPunycode.push_back(digit_value(t + ((q - t) % (base - t))));
+          q = (q - t) / (base - t);
+        }
+        OutPunycode.push_back(digit_value(q));
+        bias = adapt(delta, h + 1, h == b);
+        delta = 0;
+        ++h;
+      }
+    }
+    ++delta; ++n;
+  }
+  return true;
+}
+    
+    // The following variables should remain static globals, since they
+// are used by debug editor, which uses a single Tesseract instance.
+//
+// Contains the mappings from unique VC ids to their actual pointers.
+static std::map<int, ParamContent*> vcMap;
+static int nrParams = 0;
+static int writeCommands[2];
+    
+      // This and other putatively are the same, so call the (permanent) callback
+  // for each blob index where the bounding boxes match.
+  // The callback is deleted on completion.
+  void ProcessMatchedBlobs(const TWERD& other, TessCallback1<int>* cb) const;
+    
+    
+    {  float x;
+  float y;
+  float dir;
+  int x_bucket;
+};
+    
+      // Setup the normalization transformation parameters.
+  // The normalizations applied to a blob are as follows:
+  // 1. An optional block layout rotation that was applied during layout
+  // analysis to make the textlines horizontal.
+  // 2. A normalization transformation (LocalNormTransform):
+  // Subtract the 'origin'
+  // Apply an x,y scaling.
+  // Apply an optional rotation.
+  // Add back a final translation.
+  // The origin is in the block-rotated space, and is usually something like
+  // the x-middle of the word at the baseline.
+  // 3. Zero or more further normalization transformations that are applied
+  // in sequence, with a similar pattern to the first normalization transform.
+  //
+  // A DENORM holds the parameters of a single normalization, and can execute
+  // both the LocalNormTransform (a forwards normalization), and the
+  // LocalDenormTransform which is an inverse transform or de-normalization.
+  // A DENORM may point to a predecessor DENORM, which is actually the earlier
+  // normalization, so the full normalization sequence involves executing all
+  // predecessors first and then the transform in 'this'.
+  // Let x be image co-ordinates and that we have normalization classes A, B, C
+  // where we first apply A then B then C to get normalized x':
+  // x' = CBAx
+  // Then the backwards (to original coordinates) would be:
+  // x = A^-1 B^-1 C^-1 x'
+  // and A = B->predecessor_ and B = C->predecessor_
+  // NormTransform executes all predecessors recursively, and then this.
+  // NormTransform would be used to transform an image-based feature to
+  // normalized space for use in a classifier
+  // DenormTransform inverts this and then all predecessors. It can be
+  // used to get back to the original image coordinates from normalized space.
+  // The LocalNormTransform member executes just the transformation
+  // in 'this' without the layout rotation or any predecessors. It would be
+  // used to run each successive normalization, eg the word normalization,
+  // and later the character normalization.
+    
+    		m_pblockParent = nullptr;
+    
+    		static unsigned int GetBytesPerBlock(Format a_format)
+		{
+			switch (a_format)
+			{
+			case Format::RGB8:
+			case Format::R11:
+			case Format::RGB8A1:
+				return 8;
+				break;
+    }
+    }
+    
+    #if defined (__cplusplus)
+extern 'C' {
+#endif
+    }
+    
+    /** 16x16 multiply-add where the result fits in 32 bits */
+#undef MAC16_16
+static OPUS_INLINE opus_val32 MAC16_16_armv5e(opus_val32 c, opus_val16 a,
+ opus_val16 b)
+{
+  int res;
+  __asm__(
+      '#MAC16_16\n\t'
+      'smlabb %0, %1, %2, %3;\n'
+      : '=r'(res)
+      : 'r'(a), 'r'(b), 'r'(c)
+  );
+  return res;
+}
+#define MAC16_16(c, a, b) (MAC16_16_armv5e(c, a, b))
+    
+    /** 16x32 multiplication, followed by a 16-bit shift right and 32-bit add.
+    Results fits in 32 bits */
+#define MAC16_32_Q16(c,a,b) ADD32((c),ADD32(MULT16_16((a),SHR((b),16)), SHR(MULT16_16SU((a),((b)&0x0000ffff)),16)))
+    
+    /* Adds two signed 32-bit values in a way that can overflow, while not relying on undefined behaviour
+   (just standard two's complement implementation-specific behaviour) */
+#define silk_ADD32_ovflw(a, b)              ((opus_int32)((opus_uint32)(a) + (opus_uint32)(b)))
+/* Subtractss two signed 32-bit values in a way that can overflow, while not relying on undefined behaviour
+   (just standard two's complement implementation-specific behaviour) */
+#define silk_SUB32_ovflw(a, b)              ((opus_int32)((opus_uint32)(a) - (opus_uint32)(b)))
+    
+    std::string InternalKey::DebugString() const {
+  std::string result;
+  ParsedInternalKey parsed;
+  if (ParseInternalKey(rep_, &parsed)) {
+    result = parsed.DebugString();
+  } else {
+    result = '(bad)';
+    result.append(EscapeString(rep_));
+  }
+  return result;
+}
+    
+    class FormatTest { };
+    
+    Status DumpFile(Env* env, const std::string& fname, WritableFile* dst) {
+  FileType ftype;
+  if (!GuessType(fname, &ftype)) {
+    return Status::InvalidArgument(fname + ': unknown file type');
+  }
+  switch (ftype) {
+    case kLogFile:         return DumpLog(env, fname, dst);
+    case kDescriptorFile:  return DumpDescriptor(env, fname, dst);
+    case kTableFile:       return DumpTable(env, fname, dst);
+    default:
+      break;
+  }
+  return Status::InvalidArgument(fname + ': not a dump-able file type');
+}
+    
+    // Header is checksum (4 bytes), length (2 bytes), type (1 byte).
+static const int kHeaderSize = 4 + 2 + 1;
+    
+    Writer::Writer(WritableFile* dest)
+    : dest_(dest),
+      block_offset_(0) {
+  InitTypeCrc(type_crc_);
+}
+    
+    #ifndef STORAGE_LEVELDB_DB_MEMTABLE_H_
+#define STORAGE_LEVELDB_DB_MEMTABLE_H_
+    
+    #include 'db/db_impl.h'
+#include 'db/filename.h'
+#include 'db/version_set.h'
+#include 'db/write_batch_internal.h'
+#include 'leveldb/db.h'
+#include 'leveldb/env.h'
+#include 'leveldb/write_batch.h'
+#include 'util/logging.h'
+#include 'util/testharness.h'
+#include 'util/testutil.h'
+    
+    #ifndef STORAGE_LEVELDB_DB_SKIPLIST_H_
+#define STORAGE_LEVELDB_DB_SKIPLIST_H_
+    
+          if (!iter.Valid()) {
+        break;
+      }
+    
+      std::vector<std::thread> workers;
+  for (auto worker = size_t{0}; worker < num_threads; ++worker) {
+    workers.push_back(std::thread([&] {
+      try {
+        hphp_thread_init();
+        hphp_session_init(Treadmill::SessionKind::HHBBC);
+        SCOPE_EXIT {
+          hphp_context_exit();
+          hphp_session_exit();
+          hphp_thread_exit();
+        };
+    }
+    }
+    }
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    #ifndef HPHP_FILE_STREAM_WRAPPER_H
+#define HPHP_FILE_STREAM_WRAPPER_H
+    
+    
+    {            Reset();
         }
     
-        j0 = input[0];
-    j1 = input[1];
-    j2 = input[2];
-    j3 = input[3];
-    j4 = input[4];
-    j5 = input[5];
-    j6 = input[6];
-    j7 = input[7];
-    j8 = input[8];
-    j9 = input[9];
-    j10 = input[10];
-    j11 = input[11];
-    j12 = input[12];
-    j13 = input[13];
-    j14 = input[14];
-    j15 = input[15];
+        template <typename T>
+    inline void ValidateType(const Dictionary& dict, const std::wstring& typeValue, size_t currentVersion)
+    {
+        if (!dict.Contains(typeKey))
+        {
+            const auto& version = GetVersion(dict);
+            LogicError('Required key '%ls' is not found in the dictionary (%s).',
+                       typeKey.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
+        } 
+    }
     
-    IPC_MESSAGE_ROUTED4(ShellViewHostMsg_Call_Object_Method,
-                    int /* object id */,
-                    std::string /* type name */,
-                    std::string /* method name */,
-                    base::ListValue /* arguments */)
+    public:
+    CrossProcessMutex(const std::string& name)
+        : m_fd(-1),
+          m_fileName('/var/lock/' + name)
+    {
+    }
     
-     private:
-  ObjectManager* object_manager_;
-    
-     protected:
-  ~NwAppClearAppCacheFunction() override;
-    
-    // Read through the first n keys repeatedly and check that they get
-// compacted (verified by checking the size of the key space).
-void AutoCompactTest::DoReads(int n) {
-  std::string value(kValueSize, 'x');
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
+    // Exception wrapper to include native call stack string
+template <class E>
+class ExceptionWithCallStack : public E, public IExceptionWithCallStackBase
+{
+public:
+    ExceptionWithCallStack(const std::string& msg, const std::string& callstack) :
+        E(msg), m_callStack(callstack)
+    { }
     }
     
     
-    {  // When limit user key is prefix of start user key
-  ASSERT_EQ(IKey('foobar', 100, kTypeValue),
-            Shorten(IKey('foobar', 100, kTypeValue),
-                    IKey('foo', 200, kTypeValue)));
+    {            sequenceStartFrame += numFrames;
+        }
+    
+    protected:
+    InputValueBase(DEVICEID_TYPE deviceId, const wstring& name, const TensorShape& sampleLayout, bool isSparse, const std::wstring axisName)
+        : Base(deviceId, name)
+    {
+        Init(sampleLayout, isSparse, axisName);
+    }
+    InputValueBase(DEVICEID_TYPE deviceId, const wstring& name, size_t rows, bool isSparse, const std::wstring axisName)
+        : InputValueBase(deviceId, name, TensorShape(rows), isSparse, axisName)
+    {
+    }
+    InputValueBase(DEVICEID_TYPE deviceId, const wstring& name, bool isSparse, const std::wstring axisName)
+        : InputValueBase(deviceId, name, TensorShape(), isSparse, axisName)
+    {
+    }
+    InputValueBase(const ScriptableObjects::IConfigRecordPtr configp, bool isSparse)
+        : Base(configp->Get(L'deviceId'), L'<placeholder>')
+    {
+        AttachInputsFromConfig(configp, this->GetExpectedNumInputs());
+        wstring axisName = L'';
+        // TODO This currently reads a ComputationNode object from a property, thereby bypassing 'normal' input handling.
+        // The passing of shapes represents a second graph that is 'overlaid' (and previously identical) to the data
+        // flow network. This needs to be solved on a more fundamental level.
+        // The proposed future change from fseide is as follows:
+        // (2) On BS level, dynamicAxis is an optional parameter that takes a DynamicAxis object--the alternative,
+        // passing a string, will be removed.
+        // (3) The dynamicAxis argument will become an actual m_inputs[] to the InputValue. I.e.InputValues are no
+        // longer leaves from the ComputationNetwork viewpoint. But they ARE leaves from the user / BS / NDL view, as
+        // the axis is not passed as a regular input.This way, the current special - casing can and will be removed;
+        // instead, the MBLayout propagation will happen automagically as part of regular ValidateNetwork().
+        if (configp->Exists(L'dynamicAxis'))
+        {
+            auto axisConfig = configp->Find(L'dynamicAxis');
+            if (axisConfig->Is<ComputationNodeBase>())
+            {
+                ComputationNodeBasePtr axis = configp->Get(L'dynamicAxis');
+                axisName = axis->GetName();
+            }
+            else
+            {
+                axisName = (const std::wstring&)*axisConfig;
+            }
+        }
+    }
+    
+    // TODO: can this be static?
+template <class ElemType>
+void Microsoft::MSR::CNTK::UpdateRunningAverage(ComputationNode<ElemType>& newInput,
+                                                TensorView<ElemType>& runningAverage, size_t& runningCount)
+{
+    FrameRange fr(newInput.GetMBLayout());
+    // Set gaps to zero, since we are reducing in time.
+    newInput.MaskMissingValueColumnsToZero(fr);
+    }
+    
+    
+    {    printf('DestroyContext()\n');
+    ImGui::DestroyContext();
+    return 0;
 }
-    
-    // Return the name of the log file with the specified number
-// in the db named by 'dbname'.  The result will be prefixed with
-// 'dbname'.
-std::string LogFileName(const std::string& dbname, uint64_t number);
-    
-      struct KeyComparator {
-    const InternalKeyComparator comparator;
-    explicit KeyComparator(const InternalKeyComparator& c) : comparator(c) { }
-    int operator()(const char* a, const char* b) const;
-  };
-  friend class MemTableIterator;
-  friend class MemTableBackwardIterator;
-    
-      std::string Get(const std::string& k, const Snapshot* snapshot = nullptr) {
-    std::string result;
-    Status s = db_->Get(ReadOptions(), k, &result);
-    if (s.IsNotFound()) {
-      result = 'NOT_FOUND';
-    } else if (!s.ok()) {
-      result = s.ToString();
-    }
-    return result;
-  }
-    
-     private:
-  enum { kMaxHeight = 12 };
-    
-    #include 'db/dbformat.h'
-#include 'leveldb/db.h'
-    
-      sender.AddMessage(1, &mpd);
-  EXPECT_EQ(sender.Start(), common::ErrorCode::OK);
-  EXPECT_TRUE(sender.IsRunning());
-  EXPECT_TRUE(sender.enable_log());
-    
-    
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
 
     
-    // System gflags
-DEFINE_string(node_name, 'chassis', 'The chassis module name in proto');
-DEFINE_string(canbus_driver_name, 'canbus', 'Driver name.');
+    #include <stdint.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_primitives.h>
+#include 'imgui.h'
+#include 'imgui_impl_allegro5.h'
     
-      x <<= 2;
-  x |= t;
+    #include 'imgui.h'
+#include 'imgui_impl_glfw.h'
+#include 'imgui_impl_opengl3.h'
+#include <stdio.h>
     
-    bool RadarState201::send_ext_info(const std::uint8_t* bytes,
-                                  int32_t length) const {
-  Byte t0(bytes + 5);
-  uint32_t x = t0.get_byte(5, 1);
-    }
-    
-    namespace apollo {
-namespace localization {
-namespace msf {
-    }
-    }
-    }
-    
-    namespace apollo {
-namespace hdmap {
-DEFINE_string(test_map_file,
-              'modules/map/data/sunnyvale_loop/base_map_test.bin',
-              'The test map file');
-DEFINE_string(
-    test_routing_file,
-    'modules/map/pnc_map/testdata/sample_sunnyvale_loop_routing.pb.txt',
-    'The test map file');
-    }
+    bool ImGui::InputText(const char* label, std::string* str, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* user_data)
+{
+    IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
+    flags |= ImGuiInputTextFlags_CallbackResize;
     }
     
     
-    {  double length_2 = 50.0;
-  const LaneGraph &lane_graph_2 =
-      ObstacleClusters::GetLaneGraph(start_s, length_2, lane);
-  EXPECT_EQ(1, lane_graph_2.lane_sequence_size());
-  EXPECT_EQ(3, lane_graph_2.lane_sequence(0).lane_segment_size());
-  EXPECT_EQ('l9', lane_graph_2.lane_sequence(0).lane_segment(0).lane_id());
-  EXPECT_EQ('l18', lane_graph_2.lane_sequence(0).lane_segment(1).lane_id());
-  EXPECT_EQ('l21', lane_graph_2.lane_sequence(0).lane_segment(2).lane_id());
+    {    ImGuiMouseCursor imgui_cursor = ImGui::GetMouseCursor();
+    if (imgui_cursor == ImGuiMouseCursor_None || io.MouseDrawCursor)
+    {
+        // Hide OS mouse cursor if imgui is drawing it or if it wants no cursor
+        glfwSetInputMode(g_Window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+    }
+    else
+    {
+        // Show OS mouse cursor
+        // FIXME-PLATFORM: Unfocused windows seems to fail changing the mouse cursor with GLFW 3.2, but 3.3 works here.
+        glfwSetCursor(g_Window, g_MouseCursors[imgui_cursor] ? g_MouseCursors[imgui_cursor] : g_MouseCursors[ImGuiMouseCursor_Arrow]);
+        glfwSetInputMode(g_Window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    }
 }
+    
+    // Allow compilation with old Windows SDK. MinGW doesn't have default _WIN32_WINNT/WINVER versions.
+#ifndef WM_MOUSEHWHEEL
+#define WM_MOUSEHWHEEL 0x020E
+#endif
+#ifndef DBT_DEVNODES_CHANGED
+#define DBT_DEVNODES_CHANGED 0x0007
+#endif
