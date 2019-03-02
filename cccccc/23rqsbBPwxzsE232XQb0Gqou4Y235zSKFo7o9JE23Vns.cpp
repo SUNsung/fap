8dab5855fd75110eb7857b9bc2a85dd758204781
SUNsung <weatherius@gmail.com>
2019-03-02 02:00:05 +0200

    Rewrite [23rqsbBPwxzsE232XQb0Gqou4Y235zSKFo7o9JE23Vns.cpp] File in: 2019-03-02 02:00:04

--- cccccc/23rqsbBPwxzsE232XQb0Gqou4Y235zSKFo7o9JE23Vns.cpp
+++ cccccc/23rqsbBPwxzsE232XQb0Gqou4Y235zSKFo7o9JE23Vns.cpp
@@ -1,92 +1,226 @@
 
         
-        #ifndef ATOM_APP_COMMAND_LINE_ARGS_H_
-#define ATOM_APP_COMMAND_LINE_ARGS_H_
+        bool swift::parseASTSection(SerializedModuleLoader *SML, StringRef buf,
+                            SmallVectorImpl<std::string> &foundModules) {
+  if (!serialization::isSerializedAST(buf))
+    return false;
+    }
     
-    // static
-void UvTaskRunner::OnClose(uv_handle_t* handle) {
-  delete reinterpret_cast<uv_timer_t*>(handle);
+    void CacheImpl::removeAll() {
+  cache_remove_all(static_cast<cache_t*>(Impl));
 }
     
-    class WebRequest : public mate::TrackableObject<WebRequest> {
- public:
-  static mate::Handle<WebRequest> Create(v8::Isolate* isolate,
-                                         AtomBrowserContext* browser_context);
-    }
+    #include 'swift/Demangling/Demangle.h'
+#include 'swift/Demangling/Demangler.h'
+#include <cstdio>
     
-      base::WeakPtrFactory<TrackableObjectBase> weak_factory_;
     
-    namespace atom {
-    }
-    
-    namespace atom {
-    }
-    
-    namespace Ui {
-    class SignVerifyMessageDialog;
+    {  return encodeToUTF8(OutCodePoints, OutUTF8);
 }
     
-    #endif // BITCOIN_BECH32_H
-
-    
-    /** A hasher class for RIPEMD-160. */
-class CRIPEMD160
-{
-private:
-    uint32_t s[5];
-    unsigned char buf[64];
-    uint64_t bytes;
+    extern 'C' {
+  PyMODINIT_FUNC INITFUNC() {
+#if PY_MAJOR_VERSION >= 3
+    PyObject *module = PyModule_Create(&_module);
+#else
+    PyObject *module = Py_InitModule3(
+        const_cast<char*>(kModuleName),
+        NULL,
+        const_cast<char*>(kModuleDocstring));
+#endif
+    if (module == NULL) {
+      return INITFUNC_ERRORVAL;
+    }
+    }
     }
     
-    namespace CAROTENE_NS {
-    using std::size_t;
-    using std::ptrdiff_t;
+    void WritePropertyDocComment(io::Printer* printer, const FieldDescriptor* field) {
+    WriteDocCommentBody(printer, field);
+}
+    
+    #include <google/protobuf/compiler/csharp/csharp_doc_comment.h>
+#include <google/protobuf/compiler/csharp/csharp_enum.h>
+#include <google/protobuf/compiler/csharp/csharp_helpers.h>
+#include <google/protobuf/compiler/csharp/csharp_options.h>
+    
+    string ClassNameResolver::GetDescriptorClassName(
+    const FileDescriptor* descriptor) {
+  return GetFileImmutableClassName(descriptor);
+}
+    
+      printer->Print(
+    'com.google.protobuf.Descriptors.FileDescriptor.'
+    'InternalDescriptorAssigner assigner =\n'
+    '    new com.google.protobuf.Descriptors.FileDescriptor.'
+    '    InternalDescriptorAssigner() {\n'
+    '      public com.google.protobuf.ExtensionRegistry assignDescriptors(\n'
+    '          com.google.protobuf.Descriptors.FileDescriptor root) {\n'
+    '        descriptor = root;\n'
+    // Custom options will be handled when immutable messages' outer class is
+    // loaded. Here we just return null and let custom options be unknown
+    // fields.
+    '        return null;\n'
+    '      }\n'
+    '    };\n');
+    
+      desired_output_for_decode = '_AbcdefghIJ';
+  expected = string('\xCA\x0', 2);
+  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
+                                                     desired_output_for_decode);
+  EXPECT_EQ(expected, result);
+    
+    void OneofGenerator::GenerateCaseEnum(io::Printer* printer) {
+  printer->Print(
+      variables_,
+      'typedef GPB_ENUM($enum_name$) {\n');
+  printer->Indent();
+  printer->Print(
+      variables_,
+      '$enum_name$_GPBUnsetOneOfCase = 0,\n');
+  string enum_name = variables_['enum_name'];
+  for (int j = 0; j < descriptor_->field_count(); j++) {
+    const FieldDescriptor* field = descriptor_->field(j);
+    string field_name = FieldNameCapitalized(field);
+    printer->Print(
+        '$enum_name$_$field_name$ = $field_number$,\n',
+        'enum_name', enum_name,
+        'field_name', field_name,
+        'field_number', SimpleItoa(field->number()));
+  }
+  printer->Outdent();
+  printer->Print(
+      '};\n'
+      '\n');
+}
+    
+    namespace google {
+namespace protobuf {
+namespace io {
+    }
+    }
     }
     
-        void operator() (const uint8x8_t & v_src0, const uint8x8_t & v_src1,
-                     uint8x8_t & v_dst) const
+    void MapLiteTestUtil::ExpectClear(const unittest::TestMapLite& message) {
+  MapTestUtilImpl::ExpectClear(message);
+}
+    
+    #include 'absl/strings/str_cat.h'
+#include 'absl/strings/string_view.h'
+#include 'opencensus/stats/stats.h'
+#include 'src/core/lib/surface/call.h'
+#include 'src/cpp/ext/filters/census/grpc_plugin.h'
+#include 'src/cpp/ext/filters/census/measures.h'
+    
+    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
+constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
+constexpr size_t TraceContextEncoding::kVersionIdSize;
+constexpr size_t TraceContextEncoding::kFieldIdSize;
+constexpr size_t TraceContextEncoding::kVersionIdOffset;
+constexpr size_t TraceContextEncoding::kVersionId;
+    
+    #include 'src/cpp/server/dynamic_thread_pool.h'
+    
+        private:
+        static Microsoft::MSR::CNTK::InputStreamDescription GetInputStreamDescription(const StreamInformation& s, const DeviceDescriptor& device)
+        {
+            assert(s.m_storageFormat == StorageFormat::Dense || s.m_storageFormat == StorageFormat::SparseCSC);
+            auto CNTKdeviceId = AsCNTKImplDeviceId(device);
+            auto CNTKMatrixType = s.m_storageFormat == StorageFormat::Dense ? Microsoft::MSR::CNTK::MatrixType::DENSE : Microsoft::MSR::CNTK::MatrixType::SPARSE;
+            auto CNTKMatrixFormat = AsCNTKImplMatrixFormat(s.m_storageFormat);
+            return Microsoft::MSR::CNTK::InputStreamDescription(s.m_name, CNTKdeviceId, CNTKMatrixType, CNTKMatrixFormat);
+        }
+    
+                    // Sequence dynamic axes are 'Ordered' as opposed to the batch axis which is unordered.
+                bool hasSequenceAxis = (std::find_if(sampleDynamicAxes.begin(), sampleDynamicAxes.end(), [](const Axis& axis) {return axis.IsOrdered(); }) != sampleDynamicAxes.end());
+                if (hasSequenceAxis)
+                    unpackedShape = unpackedShape.AppendShape({ packedDataLayout->GetNumTimeSteps() });
+                else if ((packedDataLayout->GetNumTimeSteps() != 1) || packedDataLayout->HasSequenceBeyondBegin())
+                    LogicError('A PackedValue object with no sequence dynamic axis, must have a layout with exactly one time step and no sequences beginning in the past.');
+    
+        // Acquires the mutex. If 'wait' is true and mutex is acquired by someone else then
+    // function waits until mutex is released
+    // Returns false if !wait and lock cannot be acquired, or in case of a system error that prevents us from acquiring the lock.
+    bool Acquire(bool wait)
     {
-        v_dst = veor_u8(v_src0, v_src1);
+        assert(m_handle == NULL);
+        m_handle = ::CreateMutexA(NULL /*security attr*/, FALSE /*bInitialOwner*/, m_name.c_str());
+        if (m_handle == NULL)
+        {
+            if (!wait)
+                return false;   // can't lock due to access permissions: lock already exists, consider not available
+            else
+                RuntimeError('Acquire: Failed to create named mutex %s: %d.', m_name.c_str(), GetLastError());
+        }
     }
     
-                // make extrapolation for the first elements
-            if (!x)
-            {
-                // make border
-                if (border == BORDER_MODE_CONSTANT)
-                    tcurr = v_border_x3;
-                else if (border == BORDER_MODE_REPLICATE)
-                    tcurr = vdupq_n_u16(vgetq_lane_u16(tnext, 0));
+    template <typename TVector>
+inline void RandomShuffleMT(TVector& v, std::mt19937_64& rng)
+{
+    foreach_index(currentLocation, v)
+    {
+        // Pick a random location and swap with current one
+        const size_t randomLocation = RandMT(0, v.size(), rng);
+        std::swap(v[currentLocation], v[randomLocation]);
+    }
+}
+    
+    public:
+    DeclareConstructorFromConfigWithNumInputs(SumColumnElementsNode);
+    SumColumnElementsNode(DEVICEID_TYPE deviceId, const wstring& name)
+        : Base(deviceId, name)
+    {
     }
     
-                // make extrapolation for the first elements
-            if (!x)
-            {
-                // make border
-                if (border == BORDER_MODE_CONSTANT)
-                    tcurr[0] = tcurr[1] = tcurr[2] = v_border;
-                else if (border == BORDER_MODE_REPLICATE)
-                {
-                    tcurr[0] = vdup_n_u8(vget_lane_u8(tnext[0], 0));
-                    tcurr[1] = vdup_n_u8(vget_lane_u8(tnext[1], 0));
-                    tcurr[2] = vdup_n_u8(vget_lane_u8(tnext[2], 0));
-                }
-    }
     
-    namespace caffe2 {
-namespace {
-REGISTER_CPU_OPERATOR(
-    FindDuplicateElements,
-    FindDuplicateElementsOp<CPUContext>);
+    {public:
+    SparseInputValue(DEVICEID_TYPE deviceId, const wstring& name)
+        : Base(deviceId, name, true, L'')
+    {
     }
+    SparseInputValue(DEVICEID_TYPE deviceId, const wstring& name, const wstring& dynamicAxisName)
+        : Base(deviceId, name, true, dynamicAxisName)
+    {
     }
-    
-    ```
-    
-    template <typename T, class Context>
-class BernoulliJSDGradientOp final : public Operator<Context> {
- public:
-  USE_SIMPLE_CTOR_DTOR(BernoulliJSDGradientOp);
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-  bool RunOnDevice() override;
+    SparseInputValue(DEVICEID_TYPE deviceId, const wstring& name, size_t rows, const wstring& dynamicAxisName)
+        : Base(deviceId, name, rows, true, dynamicAxisName)
+    {
+    }
+    SparseInputValue(DEVICEID_TYPE deviceId, const wstring& name, const TensorShape& imageLayout, const wstring& dynamicAxisName)
+        : Base(deviceId, name, imageLayout, true, dynamicAxisName)
+    {
+    }
+    SparseInputValue(const ScriptableObjects::IConfigRecordPtr configp)
+        : Base(configp, true)
+    {
+    }
 };
+    
+    template <class ElemType>
+void EpochAccumulatorNode<ElemType>::OnEpochStart()
+{
+    Reset();
+}
+    
+    class osqueryConstants {
+ public:
+  osqueryConstants();
+    }
+    
+      // Start looping through starting at the first options
+  // (so skip the exports)
+  for (auto iter = line.begin() + options_index; iter != line.end(); ++iter) {
+    if (iter->compare('-ro') == 0 || iter->compare('-o') == 0) {
+      readonly = 1;
+    }
+  }
+    
+      // Join results..
+  for (auto& entry : hw_info) {
+    bool matched = false;
+    for (auto& row : results) {
+      auto serial = row.find('serial_number');
+      if (serial == row.end()) {
+        continue;
+      }
+    }
+    }
