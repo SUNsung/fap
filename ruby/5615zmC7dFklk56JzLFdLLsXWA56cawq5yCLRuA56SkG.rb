db5513da36c51a762a54acaa413c0f9e42178e0f
SUNsung <weatherius@gmail.com>
2019-03-02 22:51:44 +0200

    Rewrite [5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb] File in: 2019-03-02 22:51:43

--- ruby/5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb
+++ ruby/5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb
@@ -1,163 +1,246 @@
 
         
-            gu = GroupUser.find_by(user_id: moderator.id, group_id: group.id)
-    expect(gu.notification_level).to eq(NotificationLevels.all[:regular])
+                  def checked?(value)
+            case value
+            when TrueClass, FalseClass
+              value == !!@checked_value
+            when NilClass
+              false
+            when String
+              value == @checked_value
+            else
+              if value.respond_to?(:include?)
+                value.include?(@checked_value)
+              else
+                value.to_i == @checked_value.to_i
+              end
+            end
+          end
+    
+            class RadioButtonBuilder < Builder # :nodoc:
+          def radio_button(extra_html_options = {})
+            html_options = extra_html_options.merge(@input_html_options)
+            html_options[:skip_default_ids] = false
+            @template_object.radio_button(@object_name, @method_name, @value, html_options)
+          end
+        end
+    
+                options = options.dup
+            options[:field_name]           = @method_name
+            options[:include_position]     = true
+            options[:prefix]             ||= @object_name
+            options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
+    
+    desc 'Open an irb session preloaded with this library'
+task :console do
+  sh 'irb -rubygems -r ./lib/#{name}.rb'
+end
+
+    
+      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
+  p.option 'destination', '-d', '--destination [DIR]',
+    'Destination directory (defaults to ./_site)'
+  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
+  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
+    'Plugins directory (defaults to ./_plugins)'
+  p.option 'layouts_dir', '--layouts DIR', String,
+    'Layouts directory (defaults to ./_layouts)'
+  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+    
+                # stream_file_data would free us from keeping livereload.js in memory
+            # but JRuby blocks on that call and never returns
+            send_data(reload_body)
+            close_connection_after_writing
+          else
+            body = 'This port only serves livereload.js over HTTP.\n'
+            headers = [
+              'HTTP/1.1 400 Bad Request',
+              'Content-Type: text/plain',
+              'Content-Length: #{body.bytesize}',
+              '',
+              '',
+            ].join('\r\n')
+            send_data(headers)
+            send_data(body)
+            close_connection_after_writing
+          end
+        end
+        # rubocop:enable Metrics/MethodLength
+      end
+    end
   end
+end
+
     
-    describe ScenarioImportsController do
-  let(:user) { users(:bob) }
+                @config['syntax_highlighter_opts'] = begin
+              strip_coderay_prefix(
+                @config['syntax_highlighter_opts'] \
+                  .merge(CODERAY_DEFAULTS) \
+                  .merge(@config['coderay'])
+              )
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
     
-          it 'generates a DOT script' do
-        expect(agents_dot(@agents)).to match(%r{
-          \A
-          digraph \x20 'Agent \x20 Event \x20 Flow' \{
-            node \[ [^\]]+ \];
-            edge \[ [^\]]+ \];
-            (?<foo>\w+) \[label=foo\];
-            \k<foo> -> (?<bar1>\w+) \[style=dashed\];
-            \k<foo> -> (?<bar2>\w+) \[color='\#999999'\];
-            \k<bar1> \[label=bar1\];
-            \k<bar2> \[label=bar2,style='rounded,dashed',color='\#999999',fontcolor='\#999999'\];
-            \k<bar2> -> (?<bar3>\w+) \[style=dashed,color='\#999999'\];
-            \k<bar3> \[label=bar3\];
-          \}
-          \z
-        }x)
+        def no_subcommand(args)
+      unless args.empty? ||
+          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
+        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
+                          Run `jekyll help` to find out more.'
+        abort
+      end
+    end
+    
+        # The path used after unlocking the resource
+    def after_unlock_path_for(resource)
+      new_session_path(resource)  if is_navigational_format?
+    end
+    
+      def app
+    Rails.application
+  end
+end
+
+    
+                  define_method method do |resource_or_scope, *args|
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                router_name = Devise.mappings[scope].router_name
+                context = router_name ? send(router_name) : _devise_route_context
+                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
+              end
+            end
+          end
+        end
       end
     
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+          def mailer_sender(mapping, sender = :from)
+        default_sender = default_params[sender]
+        if default_sender.present?
+          default_sender.respond_to?(:to_proc) ? instance_eval(&default_sender) : default_sender
+        elsif Devise.mailer_sender.is_a?(Proc)
+          Devise.mailer_sender.call(mapping.name)
+        else
+          Devise.mailer_sender
+        end
+      end
+    
+    require 'devise/hooks/timeoutable'
+    
+        def index
+      authorize :email_domain_block, :index?
+      @email_domain_blocks = EmailDomainBlock.page(params[:page])
     end
     
-        @agent1 = Agents::SchedulerAgent.new(name: 'Scheduler 1', options: { action: 'run', schedule: '*/1 * * * * *' }).tap { |a|
-      a.user = users(:bob)
-      a.save!
-    }
-    @agent2 = Agents::SchedulerAgent.new(name: 'Scheduler 2', options: { action: 'run', schedule: '*/1 * * * * *' }).tap { |a|
-      a.user = users(:bob)
-      a.save!
-    }
+    class Api::SubscriptionsController < Api::BaseController
+  before_action :set_account
+  respond_to :txt
+    
+        render json: @web_subscription, serializer: REST::WebPushSubscriptionSerializer
   end
     
-        it 'should raise error when invalid response arrives' do
-      stub(HTTParty).post { {'blah' => 'blah'} }
-      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Invalid response from Boxcar:/)
-    end
+        render_empty
+  end
     
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   CSRF
-    # Supported browsers:: all
-    # More infos::         http://en.wikipedia.org/wiki/Cross-site_request_forgery
-    #
-    # This middleware only accepts requests other than <tt>GET</tt>,
-    # <tt>HEAD</tt>, <tt>OPTIONS</tt>, <tt>TRACE</tt> if their given access
-    # token matches the token included in the session.
-    #
-    # It checks the <tt>X-CSRF-Token</tt> header and the <tt>POST</tt> form
-    # data.
-    #
-    # Compatible with the {rack-csrf}[https://rubygems.org/gems/rack_csrf] gem.
-    #
-    # == Options
-    #
-    # [<tt>:authenticity_param</tt>] the name of the param that should contain
-    #                                the token on a request. Default value:
-    #                                <tt>'authenticity_token'</tt>
-    #
-    # == Example: Forms application
-    #
-    # To show what the AuthenticityToken does, this section includes a sample
-    # program which shows two forms. One with, and one without a CSRF token
-    # The one without CSRF token field will get a 403 Forbidden response.
-    #
-    # Install the gem, then run the program:
-    #
-    #   gem install 'rack-protection'
-    #   ruby server.rb
-    #
-    # Here is <tt>server.rb</tt>:
-    #
-    #   require 'rack/protection'
-    #
-    #   app = Rack::Builder.app do
-    #     use Rack::Session::Cookie, secret: 'secret'
-    #     use Rack::Protection::AuthenticityToken
-    #
-    #     run -> (env) do
-    #       [200, {}, [
-    #         <<~EOS
-    #           <!DOCTYPE html>
-    #           <html lang='en'>
-    #           <head>
-    #             <meta charset='UTF-8' />
-    #             <title>rack-protection minimal example</title>
-    #           </head>
-    #           <body>
-    #             <h1>Without Authenticity Token</h1>
-    #             <p>This takes you to <tt>Forbidden</tt></p>
-    #             <form action='' method='post'>
-    #               <input type='text' name='foo' />
-    #               <input type='submit' />
-    #             </form>
-    #
-    #             <h1>With Authenticity Token</h1>
-    #             <p>This successfully takes you to back to this form.</p>
-    #             <form action='' method='post'>
-    #               <input type='hidden' name='authenticity_token' value='#{env['rack.session'][:csrf]}' />
-    #               <input type='text' name='foo' />
-    #               <input type='submit' />
-    #             </form>
-    #           </body>
-    #           </html>
-    #         EOS
-    #       ]]
-    #     end
-    #   end
-    #
-    #   Rack::Handler::WEBrick.run app
-    #
-    # == Example: Customize which POST parameter holds the token
-    #
-    # To customize the authenticity parameter for form data, use the
-    # <tt>:authenticity_param</tt> option:
-    #   use Rack::Protection::AuthenticityToken, authenticity_param: 'your_token_param_name'
-    class AuthenticityToken < Base
-      TOKEN_LENGTH = 32
+      def export_data
+    raise 'Override in controller'
+  end
     
-          def csp_policy
-        directives = []
+      def request_locale
+    preferred_locale || compatible_locale
+  end
     
-      describe '#random_string' do
-    it 'outputs a string of 32 characters' do
-      expect(subject.random_string.length).to eq(32)
+      def rate_limit_remaining
+    (api_throttle_data[:limit] - api_throttle_data[:count]).to_s
+  end
+    
+        change.down do
+      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
+      Mention.where(mentions_container_type: 'Comment').destroy_all
+      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
     end
   end
+end
+
     
-      it 'should allow changing the protection settings' do
-    mock_app do
-      use Rack::Protection::ContentSecurityPolicy, :default_src => 'none', :script_src => 'https://cdn.mybank.net', :style_src => 'https://cdn.mybank.net', :img_src => 'https://cdn.mybank.net', :connect_src => 'https://api.mybank.com', :frame_src => 'self', :font_src => 'https://cdn.mybank.net', :object_src => 'https://cdn.mybank.net', :media_src => 'https://cdn.mybank.net', :report_uri => '/my_amazing_csp_report_parser', :sandbox => 'allow-scripts'
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
+  end
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
+  end
+end
     
-      config.vm.define :smartos do |smartos|
-    smartos.vm.box = 'smartos-base1310-64-virtualbox-20130806.box'
-    smartos.vm.box_url = 'http://dlc-int.openindiana.org/aszeszo/vagrant/smartos-base1310-64-virtualbox-20130806.box'
+      def post_path_by_content(text)
+    p = Post.find_by_text(text)
+    post_path(p)
   end
     
-      def iteration
-    return @iteration || 1
-  end # def iteration
-    
-        # name prefixing is optional, if enabled, a name 'foo' will become
-    # 'python-foo' (depending on what the python_package_name_prefix is)
-    if attributes[:python_fix_name?]
-      self.name = fix_name(metadata['name'])
-    else
-      self.name = metadata['name']
+    describe ContactsController, :type => :controller do
+  describe '#index' do
+    before do
+      AppConfig.chat.enabled = true
+      @aspect = bob.aspects.create(:name => 'another aspect')
+      bob.share_with alice.person, @aspect
+      bob.share_with eve.person, @aspect
+      sign_in bob, scope: :user
     end
     
-        # Make one file. The installscript can unpack itself.
-    `cat #{install_script} #{payload} > #{output_path}`
-    FileUtils.chmod('+x', output_path)
+        desc 'Release all gems as packages'
+    task :all => [:test, :commit_version] + GEMS_AND_ROOT_DIRECTORIES.keys
   end
+end
+
+    
+    get '/stream', :provides => 'text/event-stream' do
+  stream :keep_open do |out|
+    connections << out
+    out.callback { connections.delete(out) }
+  end
+end
+    
+      trap('INT') {
+    [jekyllPid, compassPid].each { |pid| Process.kill(9, pid) rescue Errno::ESRCH }
+    exit 0
+  }
+    
+    # The project root directory
+$root = ::File.dirname(__FILE__)
+    
+        def paragraphize(input)
+      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
+    end
+  end
+end
+    
+          if File.symlink?(code_path)
+        return 'Code directory '#{code_path}' cannot be a symlink'
+      end
+    
+    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
+
+    
+        def initialize(options)
+      @strictly_ordered_queues = !!options[:strict]
+      @queues = options[:queues].map { |q| 'queue:#{q}' }
+      if @strictly_ordered_queues
+        @queues = @queues.uniq
+        @queues << TIMEOUT
+      end
+    end
+    
+          # Provide a call() method that returns the formatted message.
+      def call(severity, time, program_name, message)
+        '#{time.utc.iso8601(3)} #{::Process.pid} TID-#{Sidekiq::Logging.tid}#{context} #{severity}: #{message}\n'
+      end
+    
+          def enabled?
+        self.__test_mode != :disable
+      end
+    
+          erb(content, options)
+    end
