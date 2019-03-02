e8eca58e0ac3e5baef96cbdac1bf3106f62ebfeb
SUNsung <weatherius@gmail.com>
2019-03-02 04:05:02 +0200

    Rewrite [522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py] File in: 2019-03-02 04:05:01

--- pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
+++ pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
@@ -1,71 +1,142 @@
 
         
-        
-MIME_RE = re.compile(r'^[^/]+/[^/]+$')
+            def __init__(self):
+        self.people = {}  # key: person_id, value: person
+    
+        def __init__(self, query, results):
+        self.query = query
+        self.results = results
+    
+    # pick which RNN is used on each trial
+rnn_to_use = rng.randint(2, size=E)
+ext_input = np.repeat(np.expand_dims(rnn_to_use, axis=1), ntimesteps, axis=1)
+ext_input = np.expand_dims(ext_input, axis=2)  # these are 'a's' in the paper
+    
+            if pad:
+          break
+    
+      def __init__(self, vocab, model_name='lm01'):
+    self.vocab = vocab
+    self.log_dir = os.path.join(FLAGS.data_dir, model_name)
+    
+    from models import bidirectional_zaremba
+from models import cnn
+from models import critic_vd
+from models import feedforward
+from models import rnn
+from models import rnn_nas
+from models import rnn_vd
+from models import rnn_zaremba
+from models import seq2seq
+from models import seq2seq_nas
+from models import seq2seq_vd
+from models import seq2seq_zaremba
     
     
-@mock.patch('httpie.input.AuthCredentials._getpass',
-            new=lambda self, prompt: 'password')
-def test_password_prompt(httpbin):
-    r = http('--auth', 'user',
-             'GET', httpbin.url + '/basic-auth/user/password')
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
+def create_gen_train_op(hparams, learning_rate, gen_loss, global_step, mode):
+  '''Create Generator train op.'''
+  del hparams
+  with tf.name_scope('train_generator'):
+    if FLAGS.generator_optimizer == 'sgd':
+      gen_optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    elif FLAGS.generator_optimizer == 'adam':
+      gen_optimizer = tf.train.AdamOptimizer(learning_rate)
+    else:
+      raise NotImplementedError
+    gen_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('gen')
+    ]
+    print('Optimizing Generator vars.')
+    for v in gen_vars:
+      print(v)
+    if mode == 'MINIMIZE':
+      gen_grads = tf.gradients(gen_loss, gen_vars)
+    elif mode == 'MAXIMIZE':
+      gen_grads = tf.gradients(-gen_loss, gen_vars)
+    else:
+      raise ValueError('Must be one of 'MINIMIZE' or 'MAXIMIZE'')
+    gen_grads_clipped, _ = tf.clip_by_global_norm(gen_grads,
+                                                  FLAGS.grad_clipping)
+    gen_train_op = gen_optimizer.apply_gradients(
+        zip(gen_grads_clipped, gen_vars), global_step=global_step)
+    return gen_train_op, gen_grads_clipped, gen_vars
     
+      with tf.variable_scope('dis', reuse=reuse):
+    cell_fwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
+        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
+    cell_bwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
+        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
+    if FLAGS.zoneout_drop_prob > 0.0:
+      cell_fwd = zoneout.ZoneoutWrapper(
+          cell_fwd,
+          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
+          is_training=is_training)
+      cell_bwd = zoneout.ZoneoutWrapper(
+          cell_bwd,
+          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
+          is_training=is_training)
     
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
+            _thresholds = None
+    
+        ipa_dnszone = client.dnszone_find(zone_name)
+    
+        def role_remove_member(self, name, item):
+        return self._post_json(method='role_remove_member', name=name, item=item)
+    
+        # Build the deployment object we return
+    deployment = dict(token=token, url=url)
+    deployment.update(body)
+    if 'errorMessage' in deployment:
+        message = deployment.pop('errorMessage')
+        deployment['message'] = message
+    
+        return strings
 
     
-    from httpie import ExitStatus
-from httpie.core import main
+    For more details about this platform, please refer to the documentation at
+https://home-assistant.io/components/binary_sensor.flic/
+'''
+import logging
+import threading
     
+        def __init__(self, config):
+        '''Initialize the scanner.'''
+        self.host = config[CONF_HOST]
+        self.username = config[CONF_USERNAME]
+        self.password = config[CONF_PASSWORD]
+        self.last_results = []
+        data = self.get_actiontec_data()
+        self.success_init = data is not None
+        _LOGGER.info('canner initialized')
     
-def test_follow_all_output_options_used_for_redirects(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 3
-    assert HTTP_OK not in r
+        def _make_request(self):
+        # Weirdly enough, this doesn't seem to require authentication
+        data = [{
+            'request': {
+                'sinceRevision': 0
+            },
+            'action': 'http://linksys.com/jnap/devicelist/GetDevices'
+        }]
+        headers = {'X-JNAP-Action': 'http://linksys.com/jnap/core/Transaction'}
+        return requests.post('http://{}/JNAP/'.format(self.host),
+                             timeout=DEFAULT_TIMEOUT,
+                             headers=headers,
+                             json=data)
+
     
-        def test_upload_ok(self, httpbin):
-        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
-                 'test-file@%s' % FILE_PATH_ARG, 'foo=bar')
-        assert HTTP_OK in r
-        assert 'Content-Disposition: form-data; name='foo'' in r
-        assert 'Content-Disposition: form-data; name='test-file';' \
-               ' filename='%s'' % os.path.basename(FILE_PATH) in r
-        assert FILE_CONTENT in r
-        assert ''foo': 'bar'' in r
-        assert 'Content-Type: text/plain' in r
+    DEFAULT_NAME = 'clickatell'
     
-            '''
-        for name, value in request_headers.items():
+    from homeassistant.components.notify import (
+    ATTR_DATA, ATTR_TARGET, PLATFORM_SCHEMA, BaseNotificationService)
+from homeassistant.const import CONTENT_TYPE_JSON
+import homeassistant.helpers.config_validation as cv
     
-                if self.values.count(None) > 0:
-                new_key = self.hash_function(new_key + 1)
-            else:
-                new_key = None
-                break
-    
-    	startLength = len(bitString)
-	bitString += '1'
-	while len(bitString) % 512 != 448:
-		bitString += '0'
-	lastPart = format(startLength,'064b')
-	bitString += rearrange(lastPart[32:]) + rearrange(lastPart[:32]) 
-	return bitString
-    
-            if len(X) < 2 * self.min_leaf_size:
-            self.prediction = np.mean(y)
-            return
+        def send_message(self, message='', **kwargs):
+        '''Send a message to a file.'''
+        with open(self.filepath, 'a') as file:
+            if os.stat(self.filepath).st_size == 0:
+                title = '{} notifications (Log started: {})\n{}\n'.format(
+                    kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT),
+                    dt_util.utcnow().isoformat(),
+                    '-' * 80)
+                file.write(title)
