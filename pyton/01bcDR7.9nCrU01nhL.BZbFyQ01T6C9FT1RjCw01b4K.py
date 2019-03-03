4c53be4f36bb275820de96671106235c050bee3f
SUNsung <weatherius@gmail.com>
2019-03-03 16:03:59 +0200

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2019-03-03 16:03:58

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,258 +1,58 @@
 
         
-        entry_template = textwrap.dedent('''
-    <entry>
-        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
-        <title>New version @VERSION@</title>
-        <link href='http://rg3.github.io/youtube-dl' />
-        <content type='xhtml'>
-            <div xmlns='http://www.w3.org/1999/xhtml'>
-                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
-            </div>
-        </content>
-        <author>
-            <name>The youtube-dl maintainers</name>
-        </author>
-        <updated>@TIMESTAMP@</updated>
-    </entry>
-    ''')
-    
-        template = template.replace('@SITES@', textwrap.indent('\n'.join(ie_htmls), '\t'))
-    
-    
-def get_base_name(base):
-    if base is InfoExtractor:
-        return 'LazyLoadExtractor'
-    elif base is SearchInfoExtractor:
-        return 'LazyLoadSearchExtractor'
-    else:
-        return base.__name__
-    
-    header = oldreadme[:oldreadme.index('# OPTIONS')]
-footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
-    
-        def test_playlist_items_selection(self):
-        entries = [{
-            'id': compat_str(i),
-            'title': compat_str(i),
-            'url': TEST_URL,
-        } for i in range(1, 5)]
-        playlist = {
-            '_type': 'playlist',
-            'id': 'test',
-            'entries': entries,
-            'extractor': 'test:playlist',
-            'extractor_key': 'test:playlist',
-            'webpage_url': 'http://example.com',
-        }
-    
-    
-class TestAES(unittest.TestCase):
-    def setUp(self):
-        self.key = self.iv = [0x20, 0x15] + 14 * [0]
-        self.secret_msg = b'Secret message goes here'
-    
-        def test_youtube_extract(self):
-        assertExtractId = lambda url, id: self.assertEqual(YoutubeIE.extract_id(url), id)
-        assertExtractId('http://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch?&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch?feature=player_embedded&v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('https://www.youtube.com/watch_popup?v=BaW_jenozKc', 'BaW_jenozKc')
-        assertExtractId('http://www.youtube.com/watch?v=BaW_jenozKcsharePLED17F32AD9753930', 'BaW_jenozKc')
-        assertExtractId('BaW_jenozKc', 'BaW_jenozKc')
-    
-    
-from test.helper import FakeYDL
-from youtube_dl.cache import Cache
-    
-        def get_converter(self, mime):
-        if is_valid_mime(mime):
-            for converter_class in plugin_manager.get_converters():
-                if converter_class.supports(mime):
-                    return converter_class(mime)
-    
-    
-# bdist_wheel
-extras_require = {
-    # http://wheel.readthedocs.io/en/latest/#defining-conditional-dependencies
-    'python_version == '3.0' or python_version == '3.1'': ['argparse>=1.2.1'],
-    ':sys_platform == 'win32'': ['colorama>=0.2.4'],
-}
-    
-        '''
-    return path.replace('\\', '\\\\\\')
-    
-        plugin_manager.register(Plugin)
-    try:
-        r = http(
-            httpbin + BASIC_AUTH_URL,
-            '--auth-type',
-            Plugin.auth_type,
-            '--auth',
-            BASIC_AUTH_HEADER_VALUE,
-        )
-        assert HTTP_OK in r
-        assert r.json == AUTH_OK
-    finally:
-        plugin_manager.unregister(Plugin)
-    
-    
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
-    
-    
-def test_unicode_basic_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--verbose', '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/basic-auth/test/' + UNICODE)
-    
-        def test_upload_ok(self, httpbin):
-        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
-                 'test-file@%s' % FILE_PATH_ARG, 'foo=bar')
-        assert HTTP_OK in r
-        assert 'Content-Disposition: form-data; name='foo'' in r
-        assert 'Content-Disposition: form-data; name='test-file';' \
-               ' filename='%s'' % os.path.basename(FILE_PATH) in r
-        assert FILE_CONTENT in r
-        assert ''foo': 'bar'' in r
-        assert 'Content-Type: text/plain' in r
-    
-            X, Y, coef_ = make_regression(
-            n_samples=(i * step) + n_test_samples, n_features=n_features,
-            noise=0.1, n_informative=n_informative, coef=True)
-    
-        _tabulate(results, args.metrics, args.formats)
-    
-    import time
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-    
-    
-    
-if __name__ == '__main__':
-    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
-    # block to be able to use a multi-core grid search that also works under
-    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
-    # The multiprocessing module is used as the backend of joblib.Parallel
-    # that is used when n_jobs != 1 in GridSearchCV
-    
-    # TASK: Build a vectorizer / classifier pipeline using the previous analyzer
-# the pipeline instance should stored in a variable named clf
-clf = Pipeline([
-    ('vec', vectorizer),
-    ('clf', Perceptron(tol=1e-3)),
-])
-    
-    This example shows the reconstruction of an image from a set of parallel
-projections, acquired along different angles. Such a dataset is acquired in
-**computed tomography** (CT).
-    
-    
-def _check_location(package):
-    if package.__spec__.origin is None or not package.__spec__.has_location:
-        raise FileNotFoundError(f'Package has no location {package!r}')
-    
-        counter = 1
-    for part in msg.walk():
-        # multipart/* are just containers
-        if part.get_content_maintype() == 'multipart':
-            continue
-        # Applications should really sanitize the given filename so that an
-        # email message can't be used to overwrite important files
-        filename = part.get_filename()
-        if not filename:
-            ext = mimetypes.guess_extension(part.get_content_type())
-            if not ext:
-                # Use a generic bag-of-bits extension
-                ext = '.bin'
-            filename = 'part-%03d%s' % (counter, ext)
-        counter += 1
-        with open(os.path.join(args.directory, filename), 'wb') as fp:
-            fp.write(part.get_payload(decode=True))
-    
-                if buffer.lstrip().upper().startswith('SELECT'):
-                print(cur.fetchall())
-        except sqlite3.Error as e:
-            print('An error occurred:', e.args[0])
-        buffer = ''
-    
-        if P2only:
-        # use only the finest level
-        return blobs_fpn[-1], dim_fpn, spatial_scales_fpn[-1]
-    else:
-        # use all levels
-        return blobs_fpn, dim_fpn, spatial_scales_fpn
-    
-        def UpdateWorkspaceLr(self, cur_iter, new_lr):
-        '''Updates the model's current learning rate and the workspace (learning
-        rate and update history/momentum blobs).
-        '''
-        # The workspace is the one source of truth for the lr
-        # The lr is always the same on all GPUs
-        cur_lr = workspace.FetchBlob('gpu_0/lr')[0]
-        # There are no type conversions between the lr in Python and the lr in
-        # the GPU (both are float32), so exact comparision is ok
-        if cur_lr != new_lr:
-            ratio = _get_lr_change_ratio(cur_lr, new_lr)
-            if ratio > cfg.SOLVER.LOG_LR_CHANGE_THRESHOLD:
-                logger.info(
-                    'Changing learning rate {:.6f} -> {:.6f} at iter {:d}'.
-                    format(cur_lr, new_lr, cur_iter))
-            self._SetNewLr(cur_lr, new_lr)
-        return new_lr
-    
-    
-def _scale_enum(anchor, scales):
-    '''Enumerate a set of anchors for each scale wrt an anchor.'''
-    w, h, x_ctr, y_ctr = _whctrs(anchor)
-    ws = w * scales
-    hs = h * scales
-    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-    return anchors
+        def get_parallel_rotations():
+    mult90 = [0, np.pi/2, -np.pi/2, np.pi]
+    parallel_rotations = []
+    for euler in itertools.product(mult90, repeat=3):
+        canonical = mat2euler(euler2mat(euler))
+        canonical = np.round(canonical / (np.pi / 2))
+        if canonical[0] == -2:
+            canonical[0] = 2
+        if canonical[2] == -2:
+            canonical[2] = 2
+        canonical *= np.pi / 2
+        if all([(canonical != rot).any() for rot in parallel_rotations]):
+            parallel_rotations += [canonical]
+    assert len(parallel_rotations) == 24
+    return parallel_rotations
 
     
-        for i in range(cfg.KRCNN.NUM_STACKED_CONVS):
-        current = model.Conv(
-            current,
-            'conv_fcn' + str(i + 1),
-            dim_in,
-            hidden_dim,
-            kernel_size,
-            stride=1,
-            pad=pad_size,
-            weight_init=(cfg.KRCNN.CONV_INIT, {'std': 0.01}),
-            bias_init=('ConstantFill', {'value': 0.})
+    
+def robot_get_obs(sim):
+    '''Returns all joint positions and velocities associated with
+    a robot.
+    '''
+    if sim.data.qpos is not None and sim.model.joint_names:
+        names = [n for n in sim.model.joint_names if n.startswith('robot')]
+        return (
+            np.array([sim.data.get_joint_qpos(name) for name in names]),
+            np.array([sim.data.get_joint_qvel(name) for name in names]),
         )
-        current = model.Relu(current, current)
-        dim_in = hidden_dim
+    return np.zeros(0), np.zeros(0)
     
+    # http://stackoverflow.com/a/13653312
+def full_class_name(o):
+    module = o.__class__.__module__
+    if module is None or module == str.__class__.__module__:
+        return o.__class__.__name__
+    return module + '.' + o.__class__.__name__
     
-def get_field_of_anchors(
-    stride, anchor_sizes, anchor_aspect_ratios, octave=None, aspect=None
-):
-    global _threadlocal_foa
-    if not hasattr(_threadlocal_foa, 'cache'):
-        _threadlocal_foa.cache = {}
-    
-        return valid
-    
-        num_keypoints = gt_keypoints.shape[2]
-    sampled_keypoints = -np.ones(
-        (len(sampled_fg_rois), gt_keypoints.shape[1], num_keypoints),
-        dtype=gt_keypoints.dtype
-    )
-    for ii in range(len(sampled_fg_rois)):
-        ind = box_to_gt_ind_map[ii]
-        if ind >= 0:
-            sampled_keypoints[ii, :, :] = gt_keypoints[gt_inds[ind], :, :]
-            assert np.sum(sampled_keypoints[ii, 2, :]) > 0
+    if __name__ == '__main__':
+    setup(name='wtfpython',
+          version='0.2',
+          description='What the f*ck Python!',
+          author='Satwik Kansal',
+          maintainer='Satwik Kansal',
+          maintainer_email='satwikkansal@gmail.com',
+          url='https://github.com/satwikkansal/wtfpython',
+          platforms='any',
+          license='WTFPL 2.0',
+          long_description='An interesting collection of subtle & tricky Python Snippets'
+                           ' and features.',
+          keywords='wtfpython gotchas snippets tricky',
+          packages=find_packages(),
+          entry_points = {
+              'console_scripts': ['wtfpython = wtf_python.main:load_and_read']
+          },
+          classifiers=[
+              'Development Status :: 4 - Beta',
