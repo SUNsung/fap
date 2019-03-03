e931df03fa0508e13ae4bdabfd918d3327ddbd56
SUNsung <weatherius@gmail.com>
2019-03-03 12:58:01 +0200

    Rewrite [570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb] File in: 2019-03-03 12:58:00

--- ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
+++ ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
@@ -1,219 +1,54 @@
 
         
-            # The category for users with trust level 3 has been created.
-    # Add initial permissions and description. They can be changed later.
+          protected
     
-            return return_val
-      rescue => ex
-        Dir.chdir(path_to_use) do
-          # Provide error block exception without color code
-          begin
-            error_blocks[current_platform].call(current_lane, ex, parameters) if current_platform && error_blocks[current_platform]
-            error_blocks[nil].call(current_lane, ex, parameters) if error_blocks[nil]
-          rescue => error_block_exception
-            UI.error('An error occurred while executing the `error` block:')
-            UI.error(error_block_exception.to_s)
-            raise ex # raise the original error message
-          end
-        end
+    2) You are testing a Devise controller bypassing the router.
+   If so, you can explicitly tell Devise which mapping to use:
     
-            message = '#{grouping}/test/#{specified_build_number} (fastlane)'
-        tag = '#{grouping}/test/#{specified_build_number}'
-        expect(result).to eq('git tag -am #{message.shellescape} #{tag.shellescape}')
-      end
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
     
-          it 'adds docset_bundle_id param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            docset_bundle_id: 'com.bundle.id'
-          )
-        end').runner.execute(:test)
+        alias :name :singular
     
-        # Determines the defined data type of this ConfigItem
-    def data_type
-      if @data_type.kind_of?(Symbol)
-        nil
-      elsif @data_type
-        @data_type
-      else
-        (@is_string ? String : nil)
-      end
-    end
+            # Attempt to find a user by its reset_password_token to reset its
+        # password. If a user is found and token is still valid, reset its password and automatically
+        # try saving the record. If not user is found, returns a new user
+        # containing an error in reset_password_token attribute.
+        # Attributes must contain reset_password_token, password and confirmation
+        def reset_password_by_token(attributes={})
+          original_token       = attributes[:reset_password_token]
+          reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
     
-    class String
-  # CrossplatformShellwords
-  def shellescape
-    CrossplatformShellwords.shellescape(self)
-  end
-end
+    require 'devise/hooks/timeoutable'
     
-            it 'sets the data type correctly if `is_string` is set but the type is not specified' do
-          config_item = FastlaneCore::ConfigItem.new(key: :foo,
-                                                     description: 'foo',
-                                                     is_string: true)
+    module Admin
+  class ChangeEmailsController < BaseController
+    before_action :set_account
+    before_action :require_local_account!
     
-          it 'activates an existing user' do
-        users(:bob).deactivate!
-        visit admin_users_path
-        find(:css, 'a[href='/admin/users/#{users(:bob).id}/activate']').click
-        expect(page).to have_no_text('inactive')
-        users(:bob).reload
-        expect(users(:bob)).to be_active
-      end
-    end
-  end
-end
-
-    
-          open_dry_run_modal(formatting_agent)
-      find('.dry-run-event-sample').click
-      within(:css, '.modal .builder') do
-        expect(page).to have_text('Line 1\nLine 2\nLine 3')
-      end
-      click_on('Dry Run')
-      expect(page).to have_text('Line 1,Line 2,Line 3')
-      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
-    end
+      def update
+    response, status = process_push_request
+    render plain: response, status: status
   end
     
-        it 'is turned off for existing instances of Huginn' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
-      DefaultScenarioImporter.import(user)
-    end
-    
-        it 'for the afternoon' do
-      expect(@scheduler.send(:hour_to_schedule_name, 17)).to eq('5pm')
-    end
+      def encoded_challenge
+    HTMLEntities.new.encode(params['hub.challenge'])
   end
     
-        it 'html_safes the output unless :skip_safe is passed in' do
-      expect(Utils.jsonify({:foo => 'bar'})).to be_html_safe
-      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => false)).to be_html_safe
-      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => true)).not_to be_html_safe
+      private
+    
+      before_action :require_user!
+    
+      def default_locale
+    if ENV['DEFAULT_LOCALE'].present?
+      I18n.default_locale
+    else
+      request_locale || I18n.default_locale
     end
-  end
-    
-        it 'should generate the correct last checkpoint url' do
-      @checker.options['path'] = 'last_checkpoint/usps/9361289878905919630610'
-      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/last_checkpoint/usps/9361289878905919630610')
-    end
-  end
-    
-      describe '#complete_project_id' do
-    it 'should return a array of hashes' do
-      expect(@checker.complete_project_id).to eq [{text: 'test (1234)', id: 1234}, {text: 'test1 (1235)', id: 1235}]
-    end
-  end
-    
-      # POST /resource/unlock
-  def create
-    self.resource = resource_class.send_unlock_instructions(resource_params)
-    yield resource if block_given?
-    
-        def reset_password_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :reset_password_instructions, opts)
-    end
-    
-          # Sign out a given user or scope. This helper is useful for signing out a user
-      # after deleting accounts. Returns true if there was a logout and false if there
-      # is no user logged in on the referred scope
-      #
-      # Examples:
-      #
-      #   sign_out :user     # sign_out(scope)
-      #   sign_out @user     # sign_out(resource)
-      #
-      def sign_out(resource_or_scope=nil)
-        return sign_out_all_scopes unless resource_or_scope
-        scope = Devise::Mapping.find_scope!(resource_or_scope)
-        user = warden.user(scope: scope, run_callbacks: false) # If there is no user
-    
-    module Devise
-  # Responsible for handling devise mappings and routes configuration. Each
-  # resource configured by devise_for in routes is actually creating a mapping
-  # object. You can refer to devise_for in routes for usage options.
-  #
-  # The required value in devise_for is actually not used internally, but it's
-  # inflected to find all other values.
-  #
-  #   map.devise_for :users
-  #   mapping = Devise.mappings[:user]
-  #
-  #   mapping.name #=> :user
-  #   # is the scope used in controllers and warden, given in the route as :singular.
-  #
-  #   mapping.as   #=> 'users'
-  #   # how the mapping should be search in the path, given in the route as :as.
-  #
-  #   mapping.to   #=> User
-  #   # is the class to be loaded from routes, given in the route as :class_name.
-  #
-  #   mapping.modules  #=> [:authenticatable]
-  #   # is the modules included in the class
-  #
-  class Mapping #:nodoc:
-    attr_reader :singular, :scoped_path, :path, :controllers, :path_names,
-                :class_name, :sign_out_via, :format, :used_routes, :used_helpers,
-                :failure_app, :router_name
-    
-          def message
-        'The following attribute(s) is (are) missing on your model: #{@attributes.join(', ')}'
-      end
-    end
-    
-          # If the record is persisted, remove the remember token (but only if
-      # it exists), and save the record without validations.
-      def forget_me!
-        return unless persisted?
-        self.remember_token = nil if respond_to?(:remember_token)
-        self.remember_created_at = nil if self.class.expire_all_remember_me_on_sign_out
-        save(validate: false)
-      end
-    
-    module Devise
-  module Models
-    # Timeoutable takes care of verifying whether a user session has already
-    # expired or not. When a session expires after the configured time, the user
-    # will be asked for credentials again, it means, they will be redirected
-    # to the sign in page.
-    #
-    # == Options
-    #
-    # Timeoutable adds the following options to devise_for:
-    #
-    #   * +timeout_in+: the interval to timeout the user session without activity.
-    #
-    # == Examples
-    #
-    #   user.timedout?(30.minutes.ago)
-    #
-    module Timeoutable
-      extend ActiveSupport::Concern
-    
-        false
-  end
-    
-      def pinned_version
-    Keg.new(path.resolved_path).version if pinned?
-  end
-end
-
-    
-      context 'brew' do
-    subject { HOMEBREW_LIBRARY_PATH.parent.parent/'bin/brew' }
-    
-    describe 'Kernel#srand' do
-  it 'needs to be reviewed for spec completeness'
-end
-
-    
-      it 'raises #{frozen_error_class} on an untainted, frozen object' do
-    o = Object.new.freeze
-    lambda { o.taint }.should raise_error(frozen_error_class)
   end
