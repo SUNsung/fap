94556b0806f7f04609256bd3f41fd6c60e9200a1
SUNsung <weatherius@gmail.com>
2019-03-03 11:03:04 +0200

    Rewrite [43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp] File in: 2019-03-03 11:03:03

--- cccccc/43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp
+++ cccccc/43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp
@@ -1,216 +1,325 @@
 
         
-        /*!
- * \file graph_attr_types.h
- * \brief Data structures that can appear in graph attributes.
- */
-#ifndef MXNET_GRAPH_ATTR_TYPES_H_
-#define MXNET_GRAPH_ATTR_TYPES_H_
+            http://www.apache.org/licenses/LICENSE-2.0
     
+    #include <iomanip>
+#include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/tensor_shape.pb.h'
+#include 'tensorflow/core/grappler/costs/graph_properties.h'
+#include 'tensorflow/core/grappler/grappler_item.h'
     
-    { private:
-  friend class NDArray;
-  /*! \brief make constructor protected. */
-  Imperative() {
-    if (dmlc::GetEnv('MXNET_EXEC_BULK_EXEC_TRAIN', 1)) {
-      backward_bulk_size_ =  dmlc::GetEnv('MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN', 15);
-    }
+    // Actually dereferences cached numpy arrays. REQUIRES being called while
+// holding the GIL.
+void ClearDecrefCache() {
+  std::vector<void*> cache_copy;
+  {
+    mutex_lock ml(*DelayedDecrefLock());
+    cache_copy.swap(*DecrefCache());
   }
-  /*! \brief find the input/output ndarrays that are needed for backward */
-  void GetBackwardDependency(
-      const nnvm::NodePtr& node,
-      uint32_t num_inputs, uint32_t num_outputs,
-      std::vector<bool> *p_save_inputs,
-      std::vector<bool> *p_save_outputs);
-  /*! \brief indicate whether is training. */
-#if DMLC_CXX11_THREAD_LOCAL
-  static thread_local bool is_train_;
-  static thread_local bool is_recording_;
-#else
-  static MX_THREAD_LOCAL bool is_train_;
-  static MX_THREAD_LOCAL bool is_recording_;
+  for (void* obj : cache_copy) {
+    Py_DECREF(reinterpret_cast<PyObject*>(obj));
+  }
+}
+    
+    #include <functional>
+    
+    // Must be included first
+#include 'tensorflow/python/lib/core/numpy.h'
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    // Indicates a set of options for a device's usage, which generally must be
+// provided at StreamExecutor device-initialization time.
+//
+// These are intended to be useful-but-not-mandatorily-supported options for
+// using devices on the underlying platform. Presently, if the option requested
+// is not available on the target platform, a warning will be emitted.
+struct DeviceOptions {
+ public:
+  // When it is observed that more memory has to be allocated for thread stacks,
+  // this flag prevents it from ever being deallocated. Potentially saves
+  // thrashing the thread stack memory allocation, but at the potential cost of
+  // some memory space.
+  static const unsigned kDoNotReclaimStackAllocation = 0x1;
+    }
+    
+    void
+swift::trimLeadingWhitespaceFromLines(StringRef RawText,
+                                      unsigned WhitespaceToTrim,
+                                      SmallVectorImpl<StringRef> &OutLines) {
+  SmallVector<StringRef, 8> Lines;
+    }
+    
+    /// Classify a potential CF typedef.
+CFPointeeInfo
+CFPointeeInfo::classifyTypedef(const clang::TypedefNameDecl *typedefDecl) {
+  clang::QualType type = typedefDecl->getUnderlyingType();
+    }
+    
+    namespace swift {
+    }
+    
+    
+    {  llvm_unreachable('invalid class');
+}
+    
+    #define TEGRA_MULF(src1, sz1, src2, sz2, dst, sz, w, h, scale) \
+( \
+    CAROTENE_NS::isSupportedConfiguration() ? \
+    CAROTENE_NS::mul(CAROTENE_NS::Size2D(w, h), \
+                     src1, sz1, \
+                     src2, sz2, \
+                     dst, sz, \
+                     (float)scale), \
+    CV_HAL_ERROR_OK \
+    : CV_HAL_ERROR_NOT_IMPLEMENTED \
+)
+    
+            float32x4_t v_dst0f = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(v_src1))), v_beta),
+                                        v_alpha, vcvtq_f32_u32(vmovl_u16(vget_low_u16(v_src0))));
+        float32x4_t v_dst1f = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_high_u16(v_src1))), v_beta),
+                                        v_alpha, vcvtq_f32_u32(vmovl_u16(vget_high_u16(v_src0))));
+        uint16x8_t _v_dst = vcombine_u16(vmovn_u32(vcvtq_u32_f32(v_dst0f)),
+                                        vmovn_u32(vcvtq_u32_f32(v_dst1f)));
+    
+    void extract3(const Size2D &size,
+              const u8 * srcBase, ptrdiff_t srcStride,
+              u8 * dstBase, ptrdiff_t dstStride,
+              u32 coi)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+#ifndef __ANDROID__
+    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
 #endif
-  /*! \brief node count used for naming */
-  std::atomic<uint64_t> node_count_{0};
-  /*! \brief variable count used for naming */
-  std::atomic<uint64_t> variable_count_{0};
-  /*! \brief default backward bulk size */
-  int backward_bulk_size_{0};
-};
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+    }
+    
+        for (size_t i = 0u; i < size.height; i += 1)
+    {
+        const u8 * srcy = internal::getRowPtr(srcyBase, srcyStride, i);
+        const u8 * srcu = internal::getRowPtr(srcuBase, srcuStride, i);
+        const u8 * srcv = internal::getRowPtr(srcvBase, srcvStride, i);
+        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t syj = 0u, sj = 0u, dj = 0u;
+    }
+    
+    #include 'common.hpp'
+    
+    ptrdiff_t borderInterpolate(ptrdiff_t _p, size_t _len, BORDER_MODE borderType, size_t startMargin = 0, size_t endMargin = 0);
+    
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
+CVTS_FUNC1(s16, 16,
+    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
+    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
+{
+    for (size_t i = 0; i < w; i += 8)
+    {
+        internal::prefetch(_src + i);
+        __asm__ (
+            'vld1.16 {d4-d5}, [%[src]]                              \n\t'
+            'vmovl.s16 q3, d4                                       \n\t'
+            'vmovl.s16 q4, d5                                       \n\t'
+            'vcvt.f32.s32 q5, q3                                    \n\t'
+            'vcvt.f32.s32 q6, q4                                    \n\t'
+            'vmul.f32 q7, q5, q0                                    \n\t'
+            'vmul.f32 q8, q6, q0                                    \n\t'
+            'vadd.f32 q9, q7, q1                                    \n\t'
+            'vadd.f32 q10, q8, q1                                   \n\t'
+            'vcvt.s32.f32 q11, q9                                   \n\t'
+            'vcvt.s32.f32 q12, q10                                  \n\t'
+            'vqmovn.s32 d26, q11                                    \n\t'
+            'vqmovn.s32 d27, q12                                    \n\t'
+            'vst1.16 {d26-d27}, [%[dst]]                            \n\t'
+            : /*no output*/
+            : [src] 'r' (_src + i),
+              [dst] 'r' (_dst + i + 0),
+              'w' (vshift), 'w' (vscale)
+            : 'd6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27'
+        );
+    }
+})
+#else
+CVTS_FUNC1(s16, 16,
+    float32x4_t vscale = vdupq_n_f32((f32)alpha);
+    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
+{
+    for (size_t i = 0; i < w; i += 8)
+    {
+        internal::prefetch(_src + i);
+        int16x8_t vline = vld1q_s16(_src + i);
+        int32x4_t vline1_s32 = vmovl_s16(vget_low_s16 (vline));
+        int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline));
+        float32x4_t vline1_f32 = vcvtq_f32_s32(vline1_s32);
+        float32x4_t vline2_f32 = vcvtq_f32_s32(vline2_s32);
+        vline1_f32 = vmulq_f32(vline1_f32, vscale);
+        vline2_f32 = vmulq_f32(vline2_f32, vscale);
+        vline1_f32 = vaddq_f32(vline1_f32, vshift);
+        vline2_f32 = vaddq_f32(vline2_f32, vshift);
+        vline1_s32 = vcvtq_s32_f32(vline1_f32);
+        vline2_s32 = vcvtq_s32_f32(vline2_f32);
+        int16x4_t vRes1 = vqmovn_s32(vline1_s32);
+        int16x4_t vRes2 = vqmovn_s32(vline2_s32);
+        vst1q_s16(_dst + i, vcombine_s16(vRes1, vRes2));
+    }
+})
+#endif
+    
+                int32x2_t vnz1 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx1), vget_high_u32(vlx1)));
+            int32x2_t vnz2 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx2), vget_high_u32(vlx2)));
+    
+                vst1_u8((u8 *)&mask[0], vmovn_u16(v_mask));
     
     
     {
-    {}  // namespace exec
-}  // namespace mxnet
+    {        for (--jd; js < size.width; ++js, --jd)
+            dst[jd] = src[js];
+    }
+}
+    
+    		/*
+		Convert legacy user-profile.cmd to new name user_profile.cmd
+		*/
+		PathCombine(legacyUserProfilePath, userConfigDirPath, L'user-profile.cmd');
+		if (PathFileExists(legacyUserProfilePath))
+		{
+			PathCombine(userProfilePath, userConfigDirPath, L'user_profile.cmd');
+    }
+    
+    /*!
+ * \brief Thread pool.
+ */
+class ThreadPool {
+ public:
+  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
+  struct SetReadyOnDestroy {
+    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
+      : event_(event) {
+    }
+    inline ~SetReadyOnDestroy() {
+      if (event_) {
+        event_->signal();
+      }
+    }
+    std::shared_ptr<dmlc::ManualEvent>  event_;
+  };
+    }
+    
+    
+    {}  // namespace mxnet
 
     
-    #include '../common/utils.h'
-    
-    
-    {    if(group == UCOL_REORDER_CODE_DEFAULT) {
-        group = (UColReorderCode)(UCOL_REORDER_CODE_FIRST + defaultSettings.getMaxVariable());
+      void Init(const TVMArgs& args,
+            const std::vector<int>& const_loc,
+            std::vector<Engine::VarHandle>* const_vars,
+            std::vector<Engine::VarHandle>* mutate_vars) {
+    values_.clear();
+    type_codes_.clear();
+    values_.insert(values_.end(), args.values, args.values + args.size());
+    type_codes_.insert(
+        type_codes_.end(), args.type_codes, args.type_codes + args.size());
     }
-    uint32_t varTop = data->getLastPrimaryForGroup(group);
-    U_ASSERT(varTop != 0);
-    ownedSettings->setMaxVariable(value, defaultSettings.options, errorCode);
-    if(U_FAILURE(errorCode)) { return *this; }
-    ownedSettings->variableTop = varTop;
-    setFastLatinOptions(*ownedSettings);
-    if(value == UCOL_DEFAULT) {
-        setAttributeDefault(ATTR_VARIABLE_TOP);
+    
+    DMLC_REGISTER_PARAMETER(CropParam);
+    
+    namespace mxnet {
+namespace op {
+template<typename DType>
+class CuDNNLocalResponseNormOp : public Operator {
+ public:
+  explicit CuDNNLocalResponseNormOp(LRNParam param) {
+    param_ = param;
+    init_cudnn_ = false;
+    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
+  }
+    }
+    }
+    }
+    
+    
+    {
+    {        throw invalid_argument('the input has no solution');
+    }
+};
+    
+    /// Two pointers
+/// Time Complexity:  O(n)
+/// Space Complexity: O(1)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    }
+    }
+    
+    // Counting
+// Time Complexity: O(n)
+// Space Complexity: O(3)
+class Solution {
+public:
+    void sortColors(vector<int> &nums) {
+    }
+    }
+    
+    int main() {
+    }
+    
+     public:
+  RegisterOutputCallbacks(Row* r) {
+    this->r_ = r;
+  }
+    
+    #include <osquery/utils/system/linux/perf_event/perf_event.h>
+    
+    template <typename MessageType>
+void PerfOutput<MessageType>::forceUnload() {
+  auto const exp = unload();
+  if (exp.isError()) {
+    LOG(ERROR) << 'Could not unload perf event output point '
+               << boost::io::quoted(exp.getError().getMessage());
+  }
+}
+    
+      EXPECT_EQ(to_obj.size_, buf.size());
+  EXPECT_EQ(to_obj.fd_, 42);
+  EXPECT_EQ(to_obj.data_ptr_, static_cast<void*>(buf.data()));
+    
+    ExpectedSuccess<EbpfTracepoint::Error> EbpfTracepoint::unload() {
+  if (fd_ < 0) {
+    return Success{};
+  }
+  bool failed = false;
+  std::string err_msg;
+  int ret = ioctl(fd_, PERF_EVENT_IOC_DISABLE, 0);
+  if (ret < 0) {
+    failed = true;
+    err_msg += ' perf event disabling failed: \'';
+    err_msg += strerror(errno);
+    err_msg += '\'. ';
+  }
+  ret = close(fd_);
+  if (ret < 0) {
+    failed = true;
+    err_msg += ' file descriptor closed with error: \'';
+    err_msg += strerror(errno);
+    err_msg += '\'.';
+  }
+  fd_ = -1;
+  if (failed) {
+    return createError(Error::SystemError, 'EbpfTracepoint unload failed ')
+           << err_msg;
+  }
+  return Success{};
+}
+    
+    void EnterExitJoiner::drop_stuck_events() {
+  // As far as `table_` is relatively small we can afford to iterarte over it
+  // once in a kCounterLimit events in order to clean it up.
+  for (auto it = table_.begin(); it != table_.end();) {
+    if (it->second.return_value < 0) {
+      it = table_.erase(it);
     } else {
-        setAttributeExplicitly(ATTR_VARIABLE_TOP);
+      it->second.return_value -= kCounterLimit;
+      ++it;
     }
-    return *this;
-}
-    
-    UBool SearchIterator::operator==(const SearchIterator &that) const
-{
-    if (this == &that) {
-        return TRUE;
-    }
-    return (m_breakiterator_            == that.m_breakiterator_ &&
-            m_search_->isCanonicalMatch == that.m_search_->isCanonicalMatch &&
-            m_search_->isOverlap        == that.m_search_->isOverlap &&
-            m_search_->elementComparisonType == that.m_search_->elementComparisonType &&
-            m_search_->matchedIndex     == that.m_search_->matchedIndex &&
-            m_search_->matchedLength    == that.m_search_->matchedLength &&
-            m_search_->textLength       == that.m_search_->textLength &&
-            getOffset() == that.getOffset() &&
-            (uprv_memcmp(m_search_->text, that.m_search_->text, 
-                              m_search_->textLength * sizeof(UChar)) == 0));
-}
-    
-    
-#ifndef SELFMTIMPL
-#define SELFMTIMPL
-    
-    // -------------------------------------
-    
-    #ifdef U_USE_COLLATION_KEY_DEPRECATES
-// Create a copy of the byte array.
-uint8_t*
-CollationKey::toByteArray(int32_t& count) const
-{
-    uint8_t *result = (uint8_t*) uprv_malloc( sizeof(uint8_t) * fCount );
-    }
-    
-    /**
- * Implement UnicodeMatcher
- */
-UnicodeString& StringMatcher::toPattern(UnicodeString& result,
-                                        UBool escapeUnprintable) const
-{
-    result.truncate(0);
-    UnicodeString str, quoteBuf;
-    if (segmentNumber > 0) {
-        result.append((UChar)40); /*(*/
-    }
-    for (int32_t i=0; i<pattern.length(); ++i) {
-        UChar keyChar = pattern.charAt(i);
-        const UnicodeMatcher* m = data->lookupMatcher(keyChar);
-        if (m == 0) {
-            ICU_Utility::appendToRule(result, keyChar, FALSE, escapeUnprintable, quoteBuf);
-        } else {
-            ICU_Utility::appendToRule(result, m->toPattern(str, escapeUnprintable),
-                         TRUE, escapeUnprintable, quoteBuf);
-        }
-    }
-    if (segmentNumber > 0) {
-        result.append((UChar)41); /*)*/
-    }
-    // Flush quoteBuf out to result
-    ICU_Utility::appendToRule(result, -1,
-                              TRUE, escapeUnprintable, quoteBuf);
-    return result;
-}
-    
-    
-    {  if (action->second == Killswitch::refresh_) {
-    auto result = refresh();
-    if (result) {
-      return Status::success();
-    } else {
-      return Status::failure(result.getError().getMessage());
-    }
-  } else {
-    return KillswitchPlugin::call(request, response);
   }
 }
-    
-    GTEST_TEST(ErrorTest, createErrorFromOtherError) {
-  const auto firstMsg = std::string{'2018-06-28 08:13 451014'};
-    }
-    
-    GTEST_TEST(ExpectedTest, get_error_from_expected_with_value) {
-  auto action = []() {
-    auto expected = Expected<int, TestError>(228);
-    const auto& error = expected.getError();
-    boost::ignore_unused(error);
-  };
-#ifndef NDEBUG
-  ASSERT_DEATH(action(), 'Do not try to get error from Expected with value');
-#else
-  boost::ignore_unused(action);
-#endif
-}
-    
-    #include <osquery/utils/error/error.h>
-#include <osquery/utils/expected/expected.h>
-#include <sstream>
-#include <string>
-    
-    Expected<int, NativeEvent::Error> extractIdFromTheSystem(
-    fs::path const& full_event_path) {
-  auto const id_path = full_event_path / 'id';
-  auto id_in =
-      std::fstream(id_path.native(), std::ios_base::in | std::ios_base::binary);
-  auto id_str = std::string{};
-  if (id_in.is_open()) {
-    id_in >> id_str;
-  }
-  if (!id_in.is_open() || id_in.fail()) {
-    return createError(NativeEvent::Error::System,
-                       'Could not open linux event id file ')
-           << boost::io::quoted(id_path.string());
-  }
-  auto id_exp = tryTo<SystemEventId>(id_str);
-  if (id_exp.isError()) {
-    return createError(NativeEvent::Error::System,
-                       'Could not parse linux event id from the string ')
-           << boost::io::quoted(id_str);
-  }
-  return id_exp.get();
-}
-    
-    IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplGlfw_NewFrame();
-    
-                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text('counter = %d', counter);
-    
-    void ImGui_ImplFreeGLUT_MouseWheelFunc(int button, int dir, int x, int y)
-{
-    ImGuiIO& io = ImGui::GetIO();
-    io.MousePos = ImVec2((float)x, (float)y);
-    if (dir > 0)
-        io.MouseWheel += 1.0;
-    else if (dir < 0)
-        io.MouseWheel -= 1.0;
-    (void)button; // Unused
-}
-    
-    IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_Init();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_NewFrame();
-IMGUI_IMPL_API void     ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data);
-    
-        // Setup back-end capabilities flags
-    ImGuiIO& io = ImGui::GetIO();
-    io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;       // We can honor GetMouseCursor() values (optional)
-    io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;        // We can honor io.WantSetMousePos requests (optional, rarely used)
-    io.BackendPlatformName = 'imgui_impl_sdl';
