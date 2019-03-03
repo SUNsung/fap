3fd03d40a122ed6676a9a79bc2639c02fa05a76b
SUNsung <weatherius@gmail.com>
2019-03-03 06:52:05 +0200

    Rewrite [57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb] File in: 2019-03-03 06:52:04

--- ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
+++ ruby/57l6ZDw8YSaBI57EhPO8uNBSwE5734XNDboF4y657idf.rb
@@ -1,50 +1,41 @@
 
         
-                # This method is called if the underlying machine ID changes. Providers
-        # can use this method to load in new data for the actual backing
-        # machine or to realize that the machine is now gone (the ID can
-        # become `nil`). No parameters are given, since the underlying machine
-        # is simply the machine instance given to this object. And no
-        # return value is necessary.
-        def machine_id_changed
-        end
+              it 'left-justifies the result if specified with $ argument is negative' do
+        format('%1$*2$b', 10, -10).should == '1010      '
+        format('%1$*2$B', 10, -10).should == '1010      '
+        format('%1$*2$d', 112, -10).should == '112       '
+        format('%1$*2$i', 112, -10).should == '112       '
+        format('%1$*2$o', 87, -10).should == '127       '
+        format('%1$*2$u', 112, -10).should == '112       '
+        format('%1$*2$x', 196, -10).should == 'c4        '
+        format('%1$*2$X', 196, -10).should == 'C4        '
     
-        # Merge one registry with another and return a completely new
-    # registry. Note that the result cache is completely busted, so
-    # any gets on the new registry will result in a cache miss.
-    def merge(other)
-      self.class.new.tap do |result|
-        result.merge!(self)
-        result.merge!(other)
-      end
-    end
-    
-        def str_to_byte_pos(pos)
-      @s.string.slice(0, pos).bytesize
+      before :each do
+    ENV['TEST_SH_EXPANSION'] = 'foo'
+    @shell_var = '$TEST_SH_EXPANSION'
+    platform_is :windows do
+      @shell_var = '%TEST_SH_EXPANSION%'
     end
   end
-end
     
-      # Generate digests for assets URLs.
-  config.assets.digest = true
+      it 'creates a public method in script binding' do
+    eval @code, script_binding
+    Object.should have_method :boom
+  end
     
-    Liquid::Template.register_tag('img', Jekyll::ImageTag)
-
+      context 'expands focus buttons' do
+    it 'finds selectors' do
+      list = @buttons_list.map { |input| '#{input}:focus' }
+      list = list.join(', ')
+      ruleset = 'content: #{list};'
     
-          super
-    end
-    
-    module OctopressFilters
-  def self.pre_filter(page)
-    if page.ext.match('html|textile|markdown|md|haml|slim|xml')
-      input = BacktickCodeBlock::render_code_block(page.content)
-      page.content = input.gsub /(<figure.+?>.+?<\/figure>)/m do
-        TemplateWrapper::safe_wrap($1)
-      end
+      context 'called with arguments (1, $ratio: $golden-ratio)' do
+    it 'output the first value from the golden ratio scale' do
+      expect('.one-golden-ratio').to have_rule('font-size: 1.618em')
     end
   end
-  def self.post_filter(page)
-    if page.ext.match('html|textile|markdown|md|haml|slim|xml')
-      page.output = TemplateWrapper::unwrap(page.output)
-    end
+    
+    describe 'padding' do
+  before(:all) do
+    ParserSupport.parse_file('library/padding')
   end
