b6547cc47289472cb7695901543c6e88fe104a42
SUNsung <weatherius@gmail.com>
2019-03-03 06:30:35 +0200

    Rewrite [506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb] File in: 2019-03-03 06:30:35

--- ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
+++ ruby/506jqISCjehVY50H3sJgAktPto505x.GoihZ7kg50g6w.rb
@@ -1,19 +1,16 @@
 
         
-          def pinned_version
-    Keg.new(path.resolved_path).version if pinned?
-  end
-end
-
+              unless described_class ==
+             RuboCop::Cop::Layout::MultilineMethodDefinitionBraceLayout
+        context 'with a chained call on the closing brace' do
+          let(:suffix) { '.any?' }
+          let(:source) { construct(false, true) }
     
-            -> { w.f4('', 0) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.warn_call_lineno}: warning: \n$|)
-        -> { w.f4(nil, 0) }.should output(nil, %r|core/kernel/fixtures/classes.rb:#{w.warn_call_lineno}: warning: \n$|)
-      end
-    
-    When /^I (?:log|sign) out$/ do
-  logout
-  step 'I go to the root page'
-end
-    
-            self
+          # A shorthand for getting the first argument of the node.
+      # Equivalent to `arguments.first`.
+      #
+      # @return [Node, nil] the first argument of the node,
+      #                     or `nil` if there are no arguments
+      def first_argument
+        arguments[0]
       end
