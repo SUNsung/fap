a402fb83ed23f2999b74592c2e5765b5d29abc6e
SUNsung <weatherius@gmail.com>
2019-03-03 15:48:59 +0200

    Rewrite [50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb] File in: 2019-03-03 15:48:58

--- ruby/50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb
+++ ruby/50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb
@@ -1,62 +1,36 @@
 
         
-              class MergeRequest < ActiveRecord::Base
-        self.table_name = 'merge_requests'
-    
-            def preload_stages_warnings
-          # This preloads the number of warnings for every stage, ensuring
-          # that Ci::Stage#has_warnings? doesn't execute any additional
-          # queries.
-          @pipeline.stages.each { |stage| stage.number_of_warnings }
-        end
-      end
-    end
-  end
+        describe 'Kernel.sleep' do
+  it 'needs to be reviewed for spec completeness'
 end
 
     
-              noteable_type =
-            if matches[:type] == 'pull'
-              'MergeRequest'
-            else
-              'Issue'
-            end
-    
-              format('%020e', 109.52).should == '000000001.095200e+02'
-          format('%020E', 109.52).should == '000000001.095200E+02'
-          format('%020f', 10.952).should == '0000000000010.952000'
-          format('%020g', 12.1234).should == '000000000000012.1234'
-          format('%020G', 12.1234).should == '000000000000012.1234'
-          format('%020a', 196).should == '0x000000000001.88p+7'
-          format('%020A', 196).should == '0X000000000001.88P+7'
-        end
-    
-      it 'raises an ArgumentError when passed a negative duration' do
-    lambda { sleep(-0.1) }.should raise_error(ArgumentError)
-    lambda { sleep(-1) }.should raise_error(ArgumentError)
+      it 'raises a TypeError when passed nil' do
+    lambda { srand(nil) }.should raise_error(TypeError)
   end
     
-      it 'returns the previous seed value' do
-    srand(10)
-    srand(20).should == 10
+          def title
+        'Latest Changes (Globally)'
+      end
+    
+          def custom_path
+        '#{@base_url}#{@page_dir.empty? ? '' : '/'}#{@page_dir}'
+      end
+    
+      def app
+    Precious::App
+  end
+end
+    
+      test 'create sets the correct path for a relative path subdirectory' do
+    dir  = 'foodir'
+    name = '#{dir}/bar'
+    get '/create/#{name}'
+    assert_match(/\/#{dir}/, last_response.body)
+    assert_no_match(/[^\/]#{dir}/, last_response.body)
   end
     
-      after :each do
-    Object.send :remove_method, :boom
-  end
-    
-    group :development do
-  cp_gem 'claide',                'CLAide'
-  cp_gem 'cocoapods-core',        'Core'
-  cp_gem 'cocoapods-deintegrate', 'cocoapods-deintegrate'
-  cp_gem 'cocoapods-downloader',  'cocoapods-downloader'
-  cp_gem 'cocoapods-plugins',     'cocoapods-plugins'
-  cp_gem 'cocoapods-search',      'cocoapods-search'
-  cp_gem 'cocoapods-stats',       'cocoapods-stats'
-  cp_gem 'cocoapods-trunk',       'cocoapods-trunk'
-  cp_gem 'cocoapods-try',         'cocoapods-try'
-  cp_gem 'molinillo',             'Molinillo'
-  cp_gem 'nanaimo',               'Nanaimo'
-  cp_gem 'xcodeproj',             'Xcodeproj'
-    
-    (import 'mDNSResponder.sb')
+      class DuplicatePageError < Error
+    attr_accessor :dir
+    attr_accessor :existing_path
+    attr_accessor :attempted_path
