714607be6e992f4edc1574918a3dac6c19bfc98d
SUNsung <weatherius@gmail.com>
2019-03-04 08:56:03 +0200

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2019-03-04 08:56:02

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,214 +1,190 @@
 
         
-        void SILLayout::Profile(llvm::FoldingSetNodeID &id,
-                        CanGenericSignature Generics,
-                        ArrayRef<SILField> Fields) {
-  id.AddPointer(Generics.getPointer());
-  for (auto &field : Fields) {
-    id.AddPointer(field.getLoweredType().getPointer());
-    id.AddBoolean(field.isMutable());
-  }
-}
-
-    
-    
-    {    RawText = RawText.drop_front(Pos);
-    unsigned NewlineBytes = measureNewline(RawText);
-    RawText = RawText.drop_front(NewlineBytes);
-  }
-    
-    bool swift::canBeMemberName(StringRef identifier) {
-  return llvm::StringSwitch<bool>(identifier)
-    .Case('init', false)
-    .Case('Protocol', false)
-    .Case('self', false)
-    .Case('Type', false)
-    .Default(true);
-}
-    
-      IAMAccessorKind accessorKind = IAMAccessorKind::None;
-    
-    void InputAction::anchor() {}
-    
-    #include <google/protobuf/compiler/annotation_test_util.h>
-    
-    bool CodeGenerator::GenerateAll(
-    const std::vector<const FileDescriptor*>& files,
-    const string& parameter,
-    GeneratorContext* generator_context,
-    string* error) const {
-  // Default implemenation is just to call the per file method, and prefix any
-  // error string with the file to provide context.
-  bool succeeded = true;
-  for (int i = 0; i < files.size(); i++) {
-    const FileDescriptor* file = files[i];
-    succeeded = Generate(file, parameter, generator_context, error);
-    if (!succeeded && error && error->empty()) {
-      *error = 'Code generator returned false but provided no error '
-               'description.';
-    }
-    if (error && !error->empty()) {
-      *error = file->name() + ': ' + *error;
-      break;
-    }
-    if (!succeeded) {
-      break;
-    }
-  }
-  return succeeded;
-}
-    
-    #include <google/protobuf/compiler/java/java_doc_comment.h>
-    
-    // Get the Java Class style full name of a message.
-string ClassNameResolver::GetJavaClassFullName(
-    const string& name_without_package,
-    const FileDescriptor* file,
-    bool immutable) {
-  string result;
-  if (MultipleJavaFiles(file, immutable)) {
-    result = FileJavaPackage(file, immutable);
-    if (!result.empty()) result += '.';
-  } else {
-    result = GetClassName(file, immutable);
-    if (!result.empty()) result += '$';
-  }
-  result += StringReplace(name_without_package, '.', '$', true);
-  return result;
-}
-    
-      for (int i = 0; i < dependencies.size(); i++) {
-    const string& dependency = dependencies[i].second;
-    printer->Print(
-        '      $dependency$.getDescriptor(),\n',
-        'dependency', dependency);
-  }
-    
-    void OneofGenerator::GenerateClearFunctionImplementation(io::Printer* printer) {
-  printer->Print(
-      variables_,
-      'void $owning_message_class$_Clear$capitalized_name$OneOfCase($owning_message_class$ *message) {\n'
-      '  GPBDescriptor *descriptor = [message descriptor];\n'
-      '  GPBOneofDescriptor *oneof = [descriptor.oneofs objectAtIndex:$raw_index$];\n'
-      '  GPBMaybeClearOneof(message, oneof, $index$, 0);\n'
-      '}\n');
-}
-    
-    // Return a new iterator that converts internal keys (yielded by
-// '*internal_iter') that were live at the specified 'sequence' number
-// into appropriate user keys.
-Iterator* NewDBIterator(DBImpl* db,
-                        const Comparator* user_key_comparator,
-                        Iterator* internal_iter,
-                        SequenceNumber sequence,
-                        uint32_t seed);
-    
-     private:
-  // We construct a char array of the form:
-  //    klength  varint32               <-- start_
-  //    userkey  char[klength]          <-- kstart_
-  //    tag      uint64
-  //                                    <-- end_
-  // The array is a suitable MemTable key.
-  // The suffix starting with 'userkey' can be used as an InternalKey.
-  const char* start_;
-  const char* kstart_;
-  const char* end_;
-  char space_[200];      // Avoid allocation for short keys
-    
-    #include 'db/dbformat.h'
-#include 'db/filename.h'
-#include 'db/log_reader.h'
-#include 'db/version_edit.h'
-#include 'db/write_batch_internal.h'
-#include 'leveldb/env.h'
-#include 'leveldb/iterator.h'
-#include 'leveldb/options.h'
-#include 'leveldb/status.h'
-#include 'leveldb/table.h'
-#include 'leveldb/write_batch.h'
-#include 'util/logging.h'
-    
-    #endif  // STORAGE_LEVELDB_DB_FILENAME_H_
-
-    
-    
-    {  // No copying allowed
-  Reader(const Reader&);
-  void operator=(const Reader&);
+        
+    {
+    {   private:
+    friend class CudaModule;
+    /*!
+     * \brief constructor
+     * \param mod module of this kernel
+     * \param mangled_name mangled kernel name
+     * \param signature kernel argument signature
+     */
+    Kernel(const std::shared_ptr<Chunk>& mod,
+           const std::string& mangled_name,
+           const std::vector<ArgType>& signature);
+    /*! \brief mangled kernel name */
+    std::string mangled_name_;
+    /*! \brief kernel argument signature */
+    std::vector<ArgType> signature_;
+    /*! \brief module of this kernel */
+    std::shared_ptr<Chunk> mod_;
+    /*! \brief cached kernel function on each device */
+    std::unordered_map<int, CUfunction> func_;
+  };
+  /*!
+   * \brief CudaModule constructor
+   * \param source cuda source code.
+   * \param exports export symbols before mangling.
+   */
+  CudaModule(const char* source,
+             const std::vector<std::string>& options,
+             const std::vector<std::string>& exports)
+      : ptr_(std::make_shared<Chunk>(source, options, exports)) {}
+  /*!
+   * \brief Get cuda kernal from module by name
+   * \param name kernel name
+   * \param signature kernel signature
+   * \return shared pointer to cuda kernel
+   */
+  std::shared_ptr<Kernel> GetKernel(const std::string& name,
+                                    const std::vector<ArgType>& signature);
 };
     
-      Status AddRecord(const Slice& slice);
     
-    #include <string>
-#include 'leveldb/db.h'
-#include 'db/dbformat.h'
-#include 'db/skiplist.h'
-#include 'util/arena.h'
+    {
+    {      if (top_size > DATA) {
+        if (param_.flat) {
+          batch_data_ = TBlob(nullptr, mshadow::Shape2(batch_size_,
+                                                       channels_ * width_ * height_),
+                              cpu::kDevCPU, type_flag_);
+        } else {
+          batch_data_ = TBlob(nullptr, mxnet::TShape(top_[DATA]->shape().begin(),
+                                                     top_[DATA]->shape().end()),
+                              cpu::kDevCPU, type_flag_);
+        }
+      }
+      out_.data.clear();
+      if (top_size > LABEL) {
+          batch_label_ = TBlob(nullptr, mxnet::TShape(top_[LABEL]->shape().begin(),
+                                                      top_[LABEL]->shape().end()),
+                               cpu::kDevCPU, type_flag_);
+      }
+      out_.batch_size = batch_size_;
+    }
+  }
     
-      // Our data structure does not allow duplicate insertion
-  assert(x == nullptr || !Equal(key, x->key));
     
-    class SnapshotList {
+    {  g.attrs['storage_id'] = std::make_shared<nnvm::any>(std::move(storage_id));
+  g.attrs['storage_inplace_index'] = std::make_shared<nnvm::any>(
+      std::move(storage_inplace_index));
+  g.attrs['addto_entry'] = std::make_shared<nnvm::any>(std::move(addto_entry));
+  g.attrs['skip_plus_node'] = std::make_shared<nnvm::any>(std::move(skip_plus_node));
+  return g;
+}
+    
+    namespace mxnet {
+    }
+    
+    
+    {
+    {// for reverse seq
+DECL_SCALAR(DEVICE, Plus, EvalScalar_, false)
+DECL_SCALAR(DEVICE, Minus, EvalScalar_, false)
+DECL_SCALAR(DEVICE, Mul, EvalScalar_, false)
+DECL_SCALAR(DEVICE, Div, EvalScalar_, false)
+}  // namespace ndarray
+}  // namespace mxnet
+    
+    #include <algorithm>
+#include <vector>
+#include './bilinear_sampler-inl.h'
+namespace mxnet {
+namespace op {
+#if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 5
+template<typename DType>
+class CuDNNBilinearSamplerOp : public Operator {
  public:
-  SnapshotList() : head_(0) {
-    head_.prev_ = &head_;
-    head_.next_ = &head_;
+  explicit CuDNNBilinearSamplerOp(BilinearSamplerParam param) {
+    this->param_ = param;
+    init_cudnn_ = false;
+    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
+    sampler_ = CUDNN_SAMPLER_BILINEAR;
   }
     }
-    
-    namespace HPHP { struct UnitEmitter; }
-namespace HPHP { namespace HHBBC {
     }
     }
     
-    //////////////////////////////////////////////////////////////////////
-    
-        if (!slash && !semi) {
-      raise_warning('rfc2397: invalid meta data');
-      return nullptr;
-    }
-    
-      if (need_file) {
-    char tmp[] = '/tmp/php-wrap-XXXXXX';
-    int tmp_fd = mkstemp(tmp);
-    if (tmp_fd == -1) {
-      fprintf(stderr, 'Error: unable to open temporary file');
-      exit(EXIT_FAILURE);
-    }
-    if (program == nullptr) {
-      // If the program wasn't specified on the command-line, ala' -r,
-      // is no command-line parameter, read the PHP file from stdin.
-      std::string line;
-      while (std::getline(std::cin, line)) {
-        write(tmp_fd, line.c_str(), line.length());
-        write(tmp_fd, '\n', 1);
-      }
-    } else {
-      // -r omits the braces
-      write(tmp_fd, '<?\n', 3);
-      write(tmp_fd, program, strlen(program));
-    }
-    close(tmp_fd);
-    }
-    
-    #include 'hphp/runtime/base/glob-stream-wrapper.h'
-    
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////
-    }
-    
-    namespace HPHP {
-    }
-    
-    template<typename F>
-void logPerfWarning(folly::StringPiece event, F fillCols) {
-  logPerfWarningImpl(event, 1, kDefaultPerfWarningRate, fillCols);
-}
-template<typename F>
-void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols) {
-  logPerfWarningImpl(event, 1, rate, fillCols);
+    /**
+ * Factory method
+ */
+static Transliterator* RemoveTransliterator_create(const UnicodeString& /*ID*/,
+                                                   Transliterator::Token /*context*/) {
+    /* We don't need the ID or context. We just remove data */
+    return new RemoveTransliterator();
 }
     
-    namespace HPHP {
+    void
+SelectFormat::parseObject(const UnicodeString& /*source*/,
+                        Formattable& /*result*/,
+                        ParsePosition& pos) const
+{
+    // Parsing not supported.
+    pos.setErrorIndex(pos.getIndex());
+}
+    
+    #if !UCONFIG_NO_FORMATTING
+    
+    U_NAMESPACE_END
+    
+    uint8_t *CollationKey::reallocate(int32_t newCapacity, int32_t length) {
+    uint8_t *newBytes = static_cast<uint8_t *>(uprv_malloc(newCapacity));
+    if(newBytes == NULL) { return NULL; }
+    if(length > 0) {
+        uprv_memcpy(newBytes, getBytes(), length);
     }
+    if(fFlagAndLength < 0) { uprv_free(fUnion.fFields.fBytes); }
+    fUnion.fFields.fBytes = newBytes;
+    fUnion.fFields.fCapacity = newCapacity;
+    fFlagAndLength |= 0x80000000;
+    return newBytes;
+}
+    
+        /**
+     * Implement UnicodeFunctor
+     */
+    virtual void setData(const TransliterationRuleData*);
+    
+    
+void printBool(bool res){
+    cout << (res ? 'True' : 'False') << endl;
+}
+    
+    
+    {    return;
+}
+    
+    
+    {private:
+    int nextDifferentCharacterIndex(const vector<int> &nums, int p){
+        for( ; p < nums.size() ; p ++ )
+            if( nums[p] != nums[p - 1] )
+                break;
+        return p;
+    }
+};
+    
+    
+    {    return 0;
+}
+
+    
+    // Recursive
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+    }
+    }
+    
+    // Recursive
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+    }
+    }
+    
+    using namespace std;
