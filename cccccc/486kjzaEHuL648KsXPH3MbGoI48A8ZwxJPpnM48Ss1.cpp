7555ba4c4fffa02b8ae55022c59bd4fdaebd6947
SUNsung <weatherius@gmail.com>
2019-03-04 23:29:35 +0200

    Rewrite [486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp] File in: 2019-03-04 23:29:34

--- cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
+++ cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
@@ -1,267 +1,240 @@
 
         
-        template <typename T1, typename T2, typename T3, typename T4>
-internal::ValueArray4<T1, T2, T3, T4> Values(T1 v1, T2 v2, T3 v3, T4 v4) {
-  return internal::ValueArray4<T1, T2, T3, T4>(v1, v2, v3, v4);
-}
+            QColor TextColor() const { return textColor; }
+    QColor SingleColor() const { return singleColor; }
     
-    #if 0
-    
-    // INTERNAL IMPLEMENTATION - DO NOT USE IN USER CODE.
-// A class that enables one to stream messages to assertion macros
-class GTEST_API_ AssertHelper {
- public:
-  // Constructor.
-  AssertHelper(TestPartResult::Type type,
-               const char* file,
-               int line,
-               const char* message);
-  ~AssertHelper();
-    }
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-    typename T6, typename T7, typename T8, typename T9, typename T10,
-    typename T11, typename T12, typename T13, typename T14, typename T15,
-    typename T16, typename T17, typename T18, typename T19, typename T20,
-    typename T21, typename T22, typename T23, typename T24, typename T25,
-    typename T26, typename T27, typename T28, typename T29, typename T30,
-    typename T31, typename T32, typename T33, typename T34, typename T35,
-    typename T36, typename T37, typename T38, typename T39, typename T40,
-    typename T41, typename T42, typename T43, typename T44, typename T45,
-    typename T46, typename T47, typename T48, typename T49>
-class ValueArray49 {
- public:
-  ValueArray49(T1 v1, T2 v2, T3 v3, T4 v4, T5 v5, T6 v6, T7 v7, T8 v8, T9 v9,
-      T10 v10, T11 v11, T12 v12, T13 v13, T14 v14, T15 v15, T16 v16, T17 v17,
-      T18 v18, T19 v19, T20 v20, T21 v21, T22 v22, T23 v23, T24 v24, T25 v25,
-      T26 v26, T27 v27, T28 v28, T29 v29, T30 v30, T31 v31, T32 v32, T33 v33,
-      T34 v34, T35 v35, T36 v36, T37 v37, T38 v38, T39 v39, T40 v40, T41 v41,
-      T42 v42, T43 v43, T44 v44, T45 v45, T46 v46, T47 v47, T48 v48,
-      T49 v49) : v1_(v1), v2_(v2), v3_(v3), v4_(v4), v5_(v5), v6_(v6), v7_(v7),
-      v8_(v8), v9_(v9), v10_(v10), v11_(v11), v12_(v12), v13_(v13), v14_(v14),
-      v15_(v15), v16_(v16), v17_(v17), v18_(v18), v19_(v19), v20_(v20),
-      v21_(v21), v22_(v22), v23_(v23), v24_(v24), v25_(v25), v26_(v26),
-      v27_(v27), v28_(v28), v29_(v29), v30_(v30), v31_(v31), v32_(v32),
-      v33_(v33), v34_(v34), v35_(v35), v36_(v36), v37_(v37), v38_(v38),
-      v39_(v39), v40_(v40), v41_(v41), v42_(v42), v43_(v43), v44_(v44),
-      v45_(v45), v46_(v46), v47_(v47), v48_(v48), v49_(v49) {}
-    }
-    
-    // The template 'selector' struct TemplateSel<Tmpl> is used to
-// represent Tmpl, which must be a class template with one type
-// parameter, as a type.  TemplateSel<Tmpl>::Bind<T>::type is defined
-// as the type Tmpl<T>.  This allows us to actually instantiate the
-// template 'selected' by TemplateSel<Tmpl>.
-//
-// This trick is necessary for simulating typedef for class templates,
-// which C++ doesn't support directly.
-template <GTEST_TEMPLATE_ Tmpl>
-struct TemplateSel {
-  template <typename T>
-  struct Bind {
-    typedef Tmpl<T> type;
-  };
-};
-    
-    // Sets the 0-terminated C string this MyString object
-// represents.
-void MyString::Set(const char* a_c_string) {
-  // Makes sure this works when c_string == c_string_
-  const char* const temp = MyString::CloneCString(a_c_string);
-  delete[] c_string_;
-  c_string_ = temp;
-}
+    #endif // BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
 
     
-    class b2Triangle{
-public:
-	float* x;
-    float* y;
-	b2Triangle();
-	b2Triangle(float32 x1, float32 y1, float32 x2, float32 y2, float32 x3, float32 y3);
-	~b2Triangle();
-	bool IsInside(float32 _x, float32 _y);
-	void Set(const b2Triangle& toMe);
+        ~reverse_lock() {
+        templock.lock();
+        templock.swap(lock);
     }
     
-    #if !defined(OPUS_HAVE_RTCD)
-#define OVERRIDE_OPUS_FFT (1)
+    #include <string.h>
     
-    /** 16x32 multiplication, followed by a 16-bit shift right. Results fits in 32 bits */
-#undef MULT16_32_Q16
-static OPUS_INLINE opus_val32 MULT16_32_Q16_armv4(opus_val16 a, opus_val32 b)
-{
-  unsigned rd_lo;
-  int rd_hi;
-  __asm__(
-      '#MULT16_32_Q16\n\t'
-      'smull %0, %1, %2, %3\n\t'
-      : '=&r'(rd_lo), '=&r'(rd_hi)
-      : '%r'(b),'r'(a<<16)
-  );
-  return rd_hi;
-}
-#define MULT16_32_Q16(a, b) (MULT16_32_Q16_armv4(a, b))
+    #endif // BITCOIN_CRYPTO_RIPEMD160_H
+
     
-    /*The number of bits to output at a time.*/
-# define EC_SYM_BITS   (8)
-/*The total number of bits in each of the state registers.*/
-# define EC_CODE_BITS  (32)
-/*The maximum symbol value.*/
-# define EC_SYM_MAX    ((1U<<EC_SYM_BITS)-1)
-/*Bits to shift by to move a symbol into the high-order position.*/
-# define EC_CODE_SHIFT (EC_CODE_BITS-EC_SYM_BITS-1)
-/*Carry bit of the high-order range symbol.*/
-# define EC_CODE_TOP   (((opus_uint32)1U)<<(EC_CODE_BITS-1))
-/*Low-order bit of the high-order range symbol.*/
-# define EC_CODE_BOT   (EC_CODE_TOP>>EC_SYM_BITS)
-/*The number of bits available for the last, partial symbol in the code field.*/
-# define EC_CODE_EXTRA ((EC_CODE_BITS-2)%EC_SYM_BITS+1)
+        sub	rsp,STACK_SIZE
+%ifndef LINUX
+    movdqa	[rsp + _XMM_SAVE + 0*16],xmm6	
+    movdqa	[rsp + _XMM_SAVE + 1*16],xmm7
+    movdqa	[rsp + _XMM_SAVE + 2*16],xmm8	
+    movdqa	[rsp + _XMM_SAVE + 3*16],xmm9	
+    movdqa	[rsp + _XMM_SAVE + 4*16],xmm10
+    movdqa	[rsp + _XMM_SAVE + 5*16],xmm11
+    movdqa	[rsp + _XMM_SAVE + 6*16],xmm12
+%endif
+    
+    extern 'C' {
+  PyMODINIT_FUNC INITFUNC() {
+#if PY_MAJOR_VERSION >= 3
+    PyObject *module = PyModule_Create(&_module);
+#else
+    PyObject *module = Py_InitModule3(
+        const_cast<char*>(kModuleName),
+        NULL,
+        const_cast<char*>(kModuleDocstring));
 #endif
-
-    
-    
-    {    // Back up until we come to a non-combining char, determine whether
-    //  that char is a word char.
-    UBool prevCIsWord = FALSE;
-    for (;;) {
-        if (pos <= fLookStart) {
-            break;
-        }
-        UChar32 prevChar;
-        U16_PREV(inputBuf, fLookStart, pos, prevChar);
-        if (!(u_hasBinaryProperty(prevChar, UCHAR_GRAPHEME_EXTEND)
-              || u_charType(prevChar) == U_FORMAT_CHAR)) {
-            prevCIsWord = fPattern->fStaticSets[URX_ISWORD_SET]->contains(prevChar);
-            break;
-        }
+    if (module == NULL) {
+      return INITFUNC_ERRORVAL;
     }
-    isBoundary = cIsWord ^ prevCIsWord;
-    return isBoundary;
+    }
+    }
+    
+    bool AtLeastOneAnnotationMatchesSubstring(
+    const string& file_content,
+    const std::vector<const GeneratedCodeInfo::Annotation*>& annotations,
+    const string& expected_text) {
+  for (std::vector<const GeneratedCodeInfo::Annotation*>::const_iterator
+           i = annotations.begin(),
+           e = annotations.end();
+       i != e; ++i) {
+    const GeneratedCodeInfo::Annotation* annotation = *i;
+    uint32 begin = annotation->begin();
+    uint32 end = annotation->end();
+    if (end < begin || end > file_content.size()) {
+      return false;
+    }
+    if (file_content.substr(begin, end - begin) == expected_text) {
+      return true;
+    }
+  }
+  return false;
 }
     
     
-    {    } else if (fStartType == START_CHAR) {
-        printf('    First char of Match: ');
-        if (fInitialChar > 0x20) {
-                printf(''%s'\n', CStr(UnicodeString(fInitialChar))());
-            } else {
-                printf('%#x\n', fInitialChar);
-            }
-    }
-    
-    U_CAPI int32_t U_EXPORT2
-uhash_hashScriptSet(const UElement key);
-    
-    #endif
+    {
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace java
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
 
     
-    #ifndef __SMALLINTFORMATTER_H__
-#define __SMALLINTFORMATTER_H__
-    
-    #if !UCONFIG_NO_FORMATTING
-    
-    int32_t StandardPlural::indexFromString(const UnicodeString &keyword, UErrorCode &errorCode) {
-    if (U_FAILURE(errorCode)) { return OTHER; }
-    int32_t i = indexOrNegativeFromString(keyword);
-    if (i >= 0) {
-        return i;
-    } else {
-        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
-        return OTHER;
-    }
+    string ClassNameResolver::GetFileClassName(const FileDescriptor* file,
+                                           bool immutable) {
+  if (immutable) {
+    return GetFileImmutableClassName(file);
+  } else {
+    return 'Mutable' + GetFileImmutableClassName(file);
+  }
 }
     
-        /**
-     * @param keyword for example 'few' or 'other'
-     * @return the plural form corresponding to the keyword, or OTHER
-     */
-    static Form orOtherFromString(const UnicodeString &keyword) {
-        return static_cast<Form>(indexOrOtherIndexFromString(keyword));
+      desired_output_for_decode = 'abcdefghIJ';
+  expected = string('\x0A\x0', 2);
+  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
+                                                     desired_output_for_decode);
+  EXPECT_EQ(expected, result);
+    
+    // =========================================================================
+    
+      // '1e' is not a valid float, but if the tokenizer reads it, it will
+  // report an error but still return it as a valid token.  We need to
+  // accept anything the tokenizer could possibly return, error or not.
+  if (*end == 'e' || *end == 'E') {
+    ++end;
+    if (*end == '-' || *end == '+') ++end;
+  }
+    
+    void Config::ParseIniFile(const std::string &filename, IniSettingMap &ini,
+                          const bool constants_only /* = false */,
+                          const bool is_system /* = true */ ) {
+    std::ifstream ifs(filename);
+    std::string str((std::istreambuf_iterator<char>(ifs)),
+                    std::istreambuf_iterator<char>());
+    std::string with_includes;
+    Config::ReplaceIncludesWithIni(filename, str, with_includes);
+    Config::SetParsedIni(ini, with_includes, filename, constants_only,
+                         is_system);
+}
+    
+      if (comma != data) {
+    // we have meta
+    ssize_t meta_len = comma - data;
+    data_len -= meta_len;
+    char* semi = (char*)memchr(data, ';', meta_len);
+    char* slash = (char*)memchr(data, '/', meta_len);
     }
     
-    /**
- * UnicodeReplacer API
+    Variant PlainDirectory::read() {
+  struct dirent entry;
+  struct dirent *result;
+  int ret = readdir_r(m_dir, &entry, &result);
+  if (ret != 0 || !result) {
+    return false;
+  }
+  return String(entry.d_name, CopyString);
+}
+    
+    /*
+ * Attempt to log an entry to the perf warning service.
+ *
+ * If StructuredLog::enabled() returns false or this event is discarded by the
+ * effective sample rate, nothing will be logged. If both of those checks pass,
+ * fillCols will be passed a StructuredLogEntry& to populate, which will then
+ * be logged. The column names 'event_name' and 'priority' are reserved and
+ * will be overwritten is fillCols() sets them.
+ *
+ * The effective sample rate is determined by Eval.PerfWarningSampleRate * rate
+ * (or kDefaultPerfWarningRate for the overloads that don't take a rate). If
+ * the effective sample rate is 0, all events will be discarded.
  */
-UnicodeString& StringMatcher::toReplacerPattern(UnicodeString& rule,
-                                                UBool /*escapeUnprintable*/) const {
-    // assert(segmentNumber > 0);
-    rule.truncate(0);
-    rule.append((UChar)0x0024 /*$*/);
-    ICU_Utility::appendNumber(rule, segmentNumber, 10, 1);
-    return rule;
-}
+template<typename F>
+void logPerfWarning(folly::StringPiece event, F fillCols);
+template<typename F>
+void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
     
-        /**
-     * Remove any match data.  This must be called before performing a
-     * set of matches with this segment.
-     */
-    void resetMatch();
-    
-    
-    {    // exception out_of_range.401
-    try
-    {
-        // try to write beyond the array limit
-        array.at(5) = 'sixth';
+    int main() {
     }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+    
+        cout << endl;
+    
+                if(level == res.size())
+                res.push_back(vector<int>());
+            assert( level < res.size() );
+    
+            vector<vector<int>> res;
+        if(root == NULL)
+            return res;
+    
+    #include <iostream>
+#include <vector>
+#include <stack>
+#include <cassert>
+    
+    
+    {    return 0;
 }
 
     
-        // print values
-    std::cout << object << '\n';
-    std::cout << null << '\n';
-    
-      ProtocolData<::apollo::canbus::ChassisDetail> mpd;
-  SenderMessage<::apollo::canbus::ChassisDetail> msg(1, &mpd);
-  EXPECT_FALSE(sender.NeedSend(msg, 1));
-  EXPECT_EQ(msg.message_id(), 1);
-  int32_t period = msg.curr_period();
-  msg.UpdateCurrPeriod(-50);
-  EXPECT_EQ(msg.curr_period(), period + 50);
-  EXPECT_EQ(msg.CanFrame().id, 1);
-    
-    double ClusterGeneralInfo701::longitude_vel(const std::uint8_t* bytes,
-                                            int32_t length) const {
-  Byte t0(bytes + 4);
-  uint32_t x = t0.get_byte(0, 8);
+    public:
+    vector<int> postorderTraversal(TreeNode* root) {
     }
     
-      Byte t1(bytes + 3);
-  uint32_t t = t0.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-    
-    {  int ret = x;
-  return ret;
-}
-    
-    BaseMapMatrix::BaseMapMatrix() {}
-    
-    #include 'modules/common/util/file.h'
-#include 'modules/map/hdmap/hdmap_util.h'
-    
-    class ObstacleClustersTest : public KMLMapBasedTest {};
-    
-    TEST_F(GemVehicleFactoryTest, InitMessageManager) {
-  EXPECT_TRUE(gem_factory_.CreateMessageManager() != nullptr);
-}
-    
-        static BOOST_FORCEINLINE storage_type load(storage_type const volatile& storage, memory_order) BOOST_NOEXCEPT
-    {
-        storage_type value;
+    class ExtensionProcessorFactory : public ::apache::thrift::TProcessorFactory {
+ public:
+  ExtensionProcessorFactory(const ::apache::thrift::stdcxx::shared_ptr< ExtensionIfFactory >& handlerFactory) :
+      handlerFactory_(handlerFactory) {}
     }
     
-    BOOST_FORCEINLINE void pause() BOOST_NOEXCEPT
-{
-#if defined(_MSC_VER) && (defined(_M_AMD64) || defined(_M_IX86))
-    _mm_pause();
-#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-    __asm__ __volatile__('pause;');
-#endif
+      bool operator == (const ExtensionManager_getQueryColumns_result & rhs) const
+  {
+    if (!(success == rhs.success))
+      return false;
+    return true;
+  }
+  bool operator != (const ExtensionManager_getQueryColumns_result &rhs) const {
+    return !(*this == rhs);
+  }
+    
+    #include <fstream>
+    
+    void processDebugEngine(const std::string& fileName, Row& r) {
+  const unsigned long kSymbolOptions =
+      SYMOPT_CASE_INSENSITIVE & SYMOPT_UNDNAME & SYMOPT_LOAD_LINES &
+      SYMOPT_OMAP_FIND_NEAREST & SYMOPT_LOAD_ANYTHING &
+      SYMOPT_FAIL_CRITICAL_ERRORS & SYMOPT_AUTO_PUBLICS;
+    }
+    
+    inline void meta_tables(int nArg, char** azArg) {
+  auto tables = osquery::RegistryFactory::get().names('table');
+  std::sort(tables.begin(), tables.end());
+  for (const auto& table_name : tables) {
+    if (nArg == 1 || table_name.find(azArg[1]) == 0) {
+      printf('  => %s\n', table_name.c_str());
+    }
+  }
 }
+    
+      std::vector<std::string> storage_drivers;
+  getStorageCtlerClassDrivers(storage_drivers);
+    
+      // Author: @guliashvili
+  // Creation Time: 3/09/2018
+  bool isAppStartMonitorEnabled();
+    
+    TEST_F(PerfOutputTests, assigning_constructor) {
+  auto buf = std::array<char, 8>{};
+  auto from_obj = ebpf::PerfOutput<TestMessage>{};
+  from_obj.size_ = buf.size();
+  from_obj.fd_ = 42;
+  from_obj.data_ptr_ = static_cast<void*>(buf.data());
+    }
+    
+    Expected<EbpfTracepoint, EbpfTracepoint::Error> EbpfTracepoint::load(
+    tracing::NativeEvent system_event, ebpf::Program program) {
+  auto instance = EbpfTracepoint(std::move(system_event), std::move(program));
+    }
+    
+    ExpectedSuccess<LinuxProbesControl::Error> LinuxProbesControl::traceSetuid() {
+  return traceEnterAndExit(syscall::EventType::SetuidEnter);
+}
+    
+        // this part of the stack is for comm string, let's initialize it with a '\0'
+    {BPF_STX | BPF_DW | BPF_MEM  , BPF_REG_10, BPF_REG_1,  -32,  0},
+    {BPF_STX | BPF_DW | BPF_MEM  , BPF_REG_10, BPF_REG_1,  -24,  0},
