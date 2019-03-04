16d4cd02249239c86851f1dd5840e88dfebaeda2
SUNsung <weatherius@gmail.com>
2019-03-04 04:25:25 +0200

    Rewrite [18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py] File in: 2019-03-04 04:25:25

--- pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
+++ pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
@@ -1,68 +1,159 @@
 
         
-            @property
-    def body(self):
-        body = self._orig.body
-        if isinstance(body, str):
-            # Happens with JSON/form request data parsed from the command line.
-            body = body.encode('utf8')
-        return body or b''
+        # Direction codes used in the paths.
+DIRS = '_^V<>'
+DIR_TO_ID = {dir: did for did, dir in enumerate(DIRS)}
+    
+      LFADS is an unsupervised method to decompose time series data into
+  various factors, such as an initial condition, a generative
+  dynamical system, inferred inputs to that generator, and a low
+  dimensional description of the observed data, called the factors.
+  Additoinally, the observations have a noise model (in this case
+  Poisson), so a denoised version of the observations is also created
+  (e.g. underlying rates of a Poisson distribution given the observed
+  event counts).
+  '''
+    
+    # Controlling the size of the generator is one way to control complexity of
+# the dynamics (there is also l2, which will squeeze out unnecessary
+# dynamics also).  The modern deep learning approach is to make these cells
+# as large as tolerable (from a waiting perspective), and then regularize
+# them to death with drop out or whatever.  I don't know if this is correct
+# for the LFADS application or not.
+flags.DEFINE_integer('gen_dim', GEN_DIM,
+                     'Cell hidden size, generator.')
+# The weights of the generator cell by default set to scale at
+# ws/sqrt(#inputs), with ws=1.0.  You can change ws for
+# the input weights or the recurrent weights with these hyperparameters.
+flags.DEFINE_float('gen_cell_input_weight_scale', GEN_CELL_INPUT_WEIGHT_SCALE,
+                     'Input scaling for input weights in generator.')
+flags.DEFINE_float('gen_cell_rec_weight_scale', GEN_CELL_REC_WEIGHT_SCALE,
+                     'Input scaling for rec weights in generator.')
+    
+    flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'thits_data',
+                    'Name of data file for input case.')
+flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 100, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
+flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 40,
+                     'Number of noise replications of the same underlying rates.')
+flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
+flags.DEFINE_float('x0_std', 1.0,
+                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('input_magnitude', 20.0,
+                   'For the input case, what is the value of the input?')
+flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
+FLAGS = flags.FLAGS
+    
+    # save down the network outputs (may be useful later)
+train_outputs_u, valid_outputs_u = split_list_by_inds(outs_e,
+                                                      train_inds,
+                                                      valid_inds)
+train_outputs_u = np.array(train_outputs_u)
+valid_outputs_u = np.array(valid_outputs_u)
+    
+    # split into train and validation sets
+train_inds, valid_inds = get_train_n_valid_inds(E, train_percentage,
+                                                nreplications)
+    
+      Args:
+    vocab: Vocabulary. Contains vocabulary size and converts word to ids.
+  '''
+  assert FLAGS.save_dir, 'Must specify FLAGS.save_dir for dump_emb.'
+  inputs = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
+  targets = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
+  weights = np.ones([BATCH_SIZE, NUM_TIMESTEPS], np.float32)
+    
+    
+def _read_words(filename):
+  with tf.gfile.GFile(filename, 'r') as f:
+    return f.read().decode('utf-8').replace('\n', '<eos>').split()
+    
+    Launch command:
+  python generate_samples.py
+  --data_dir=/tmp/data/imdb  --data_set=imdb
+  --batch_size=256 --sequence_length=20 --base_directory=/tmp/imdb
+  --hparams='gen_rnn_size=650,dis_rnn_size=650,gen_num_layers=2,
+  gen_vd_keep_prob=1.0' --generator_model=seq2seq_vd
+  --discriminator_model=seq2seq_vd --is_present_rate=0.5
+  --maskgan_ckpt=/tmp/model.ckpt-45494
+  --seq2seq_share_embedding=True --dis_share_embedding=True
+  --attention_option=luong --mask_strategy=contiguous --baseline_method=critic
+  --number_epochs=4
+'''
+    
+      # Split the batch into half.  Use half for MC estimates for REINFORCE.
+  # Use the other half to establish a baseline.
+  elif FLAGS.baseline_method == 'dis_batch':
+    # TODO(liamfedus):  Recheck.
+    [rewards_half, baseline_half] = tf.split(
+        rewards, num_or_size_splits=2, axis=0)
+    [log_probs_half, _] = tf.split(log_probs, num_or_size_splits=2, axis=0)
+    [reward_present_half, baseline_present_half] = tf.split(
+        present, num_or_size_splits=2, axis=0)
+    
+            '''
+        super(AugeasConfigurator, self).recovery_routine()
+        # Need to reload configuration after these changes take effect
+        self.aug.load()
+    
+    from certbot_apache import configurator
+from certbot_apache import entrypoint
+    
+            complex_vh = VirtualHost(
+            'fp', 'vhp',
+            set([Addr.fromstring('*:443'), Addr.fromstring('1.2.3.4:443')]),
+            False, False)
+        self.assertTrue(complex_vh.conflicts([self.addr1]))
+        self.assertTrue(complex_vh.conflicts([self.addr2]))
+        self.assertFalse(complex_vh.conflicts([self.addr_default]))
+    
+            with mock.patch(
+            'certbot_apache.override_debian.DebianConfigurator.enable_mod'):
+            sni_responses = self.sni.perform()
+    
+            :param list ip_addrs: addresses of challenged domain
+            :class:`list` of type `~.obj.Addr`
+    
+    # If false, no module index is generated.
+#texinfo_domain_indices = True
+    
+        pil_image.show()
 
     
-        def format_body(self, content, mime):
-        '''Return processed `content`.
+        if len(unknown_face_encodings) > 0:
+        face_found = True
+        # See if the first face in the uploaded image matches the known face of Obama
+        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
+        if match_results[0]:
+            is_obama = True
     
-        def get_formatters_grouped(self):
-        groups = {}
-        for group_name, group in groupby(
-                self.get_formatters(),
-                key=lambda p: getattr(p, 'group_name', 'format')):
-            groups[group_name] = list(group)
-        return groups
+        if os.path.isdir(image_to_check):
+        if cpus == 1:
+            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
+        else:
+            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
+    else:
+        test_image(image_to_check, model)
     
+    # Open video file
+video_capture = cv2.VideoCapture('short_hamilton_clip.mp4')
     
-def load_json_preserve_order(s):
-    return json.loads(s, object_pairs_hook=OrderedDict)
+        for unknown_encoding in unknown_encodings:
+        distances = face_recognition.face_distance(known_face_encodings, unknown_encoding)
+        result = list(distances <= tolerance)
     
-    
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
-
-    
-    from httpie import __version__
-from httpie.compat import is_windows
-    
-    
-def write_loop_file(records_number, loop_file_path, file_name):
-    with open(loop_file_path, 'a') as file:
-        for i in range(records_number):
-            file.write('file '{}'\n'.format(file_name))
-    
-    def ehow_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-	
-	assert re.search(r'http://www.ehow.com/video_', url), 'URL you entered is not supported'
-    
-    
-def fantasy_download_by_id_channelId(id = 0, channelId = 0, output_dir = '.', merge = True, info_only = False,
-                                     **kwargs):
-    api_url = 'http://www.fantasy.tv/tv/playDetails.action?' \
-              'myChannelId=1&id={id}&channelId={channelId}&t={t}'.format(id = id,
-                                                                         channelId = channelId,
-                                                                         t = str(random.random())
-                                                                         )
-    html = get_content(api_url)
-    html = json.loads(html)
-    
-        @classmethod
-    def get_cdninfo(cls, hashid):
-        url = 'http://jobsfe.funshion.com/query/v1/mp4/{}.json'.format(hashid)
-        meta = json.loads(get_content(url, decoded=False).decode('utf8'))
-        return meta['playlist'][0]['urls']
+    # Create a PIL imagedraw object so we can draw on the picture
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
