ba299ddb6d431b4bff9964562278c68097c934b0
SUNsung <weatherius@gmail.com>
2019-03-04 13:24:13 +0200

    Rewrite [21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py] File in: 2019-03-04 13:24:12

--- pyton/21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py
+++ pyton/21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py
@@ -1,180 +1,156 @@
 
         
-            creds, params = get_google_cloud_credentials(module)
-    pubsub_client = pubsub.Client(project=params['project_id'], credentials=creds, use_gax=False)
-    pubsub_client.user_agent = 'ansible-pubsub-0.1'
+            if RESULT and ('info_dict' not in test or 'age_limit' not in test['info_dict'] or
+                   test['info_dict']['age_limit'] != 18):
+        print('\nPotential missing age_limit check: {0}'.format(test['name']))
+    
+    with open('update/versions.json', 'w') as jsonf:
+    json.dump(versions_info, jsonf, indent=4, sort_keys=True)
+
     
     
-def _remove_firewall_rule(module, oneandone_conn, firewall_id, rule_id):
+def get_base_name(base):
+    if base is InfoExtractor:
+        return 'LazyLoadExtractor'
+    elif base is SearchInfoExtractor:
+        return 'LazyLoadSearchExtractor'
+    else:
+        return base.__name__
+    
+    header = oldreadme[:oldreadme.index('# OPTIONS')]
+footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
+    
+    import io
+import optparse
+import os
+import sys
+    
+    
+from test.helper import FakeYDL
+from youtube_dl.cache import Cache
+    
+    
+if __name__ == '__main__':
+    unittest.main()
+
+    
+    
+def test_cce_one_hot():
+    y_a = K.variable(np.random.randint(0, 7, (5, 6)))
+    y_b = K.variable(np.random.random((5, 6, 7)))
+    objective_output = losses.sparse_categorical_crossentropy(y_a, y_b)
+    assert K.eval(objective_output).shape == (5, 6)
+    
+    
+def normalize(x, axis=-1, order=2):
+    '''Normalizes a Numpy array.
+    
+    # create two datasets one with digits below 5 and one with 5 and above
+x_train_lt5 = x_train[y_train < 5]
+y_train_lt5 = y_train[y_train < 5]
+x_test_lt5 = x_test[y_test < 5]
+y_test_lt5 = y_test[y_test < 5]
+    
+    Compares the performance of a simple MLP using two
+different activation functions: RELU and SELU
+on the Reuters newswire topic classification task.
+    
+    
+def mean_absolute_percentage_error(y_true, y_pred):
+    diff = K.abs((y_true - y_pred) / K.clip(K.abs(y_true),
+                                            K.epsilon(),
+                                            None))
+    return 100. * K.mean(diff, axis=-1)
+    
+        def testReadInto(self):
+        self.createTempFile()
+        with BZ2File(self.filename) as bz2f:
+            n = 128
+            b = bytearray(n)
+            self.assertEqual(bz2f.readinto(b), n)
+            self.assertEqual(b, self.TEXT[:n])
+            n = len(self.TEXT) - n
+            b = bytearray(len(self.TEXT))
+            self.assertEqual(bz2f.readinto(b), n)
+            self.assertEqual(b[:n], self.TEXT[-n:])
+    
+    
+if __name__ == '__main__':
+    main()
+
+    
+        counter = 1
+    for part in msg.walk():
+        # multipart/* are just containers
+        if part.get_content_maintype() == 'multipart':
+            continue
+        # Applications should really sanitize the given filename so that an
+        # email message can't be used to overwrite important files
+        filename = part.get_filename()
+        if not filename:
+            ext = mimetypes.guess_extension(part.get_content_type())
+            if not ext:
+                # Use a generic bag-of-bits extension
+                ext = '.bin'
+            filename = 'part-%03d%s' % (counter, ext)
+        counter += 1
+        with open(os.path.join(args.directory, filename), 'wb') as fp:
+            fp.write(part.get_payload(decode=True))
+    
+        def __init__(self):
+        self.trans = dict(dog='σκύλος', cat='γάτα')
+    
+    
+def main():
+    Jhon = Person('Jhon', 'Coder')
+    print(u'Name: {0}    Occupation: {1}'.format(Jhon.name, Jhon.occupation))
+    print(u'Before we access `relatives`:')
+    print(Jhon.__dict__)
+    print(u'Jhon's relatives: {0}'.format(Jhon.relatives))
+    print(u'After we've accessed `relatives`:')
+    print(Jhon.__dict__)
+    print(Jhon.parents)
+    print(Jhon.__dict__)
+    print(Jhon.parents)
+    print(Jhon.call_count2)
+    
+        value = 'default'
+    
+        def now(self):
+        current_time_is_always_midnight = '24:01'
+        return current_time_is_always_midnight
+
+    
+        def render(self):
+        return '<b>{}</b>'.format(self._wrapped.render())
+    
+    delim = text_type(args.delimiter)
+cutall = args.cutall
+hmm = args.hmm
+fp = open(args.filename, 'r') if args.filename else sys.stdin
+    
+    s = '此外，公司拟对全资子公司吉林欧亚置业有限公司增资4.3亿元，增资后，吉林欧亚置业注册资本由7000万元增加到5亿元。吉林欧亚置业主要经营范围为房地产开发及百货零售等业务。目前在建吉林欧亚城市商业综合体项目。2013年，实现营业收入0万元，实现净利润-139.13万元。'
+for x, w in jieba.analyse.extract_tags(s, withWeight=True):
+    print('%s %s' % (x, w))
+    
+    tags = jieba.analyse.extract_tags(content, topK=topK)
+    
+    
+    
+    # os.execv is broken on Windows and can't properly parse command line
+# arguments and executable name if they contain whitespaces. subprocess
+# fixes that behavior.
+_has_execv = sys.platform != 'win32'
+    
+        See `OptionParser.add_parse_callback`
     '''
-    Removes a rule from a firewall policy.
-    '''
-    try:
-        if module.check_mode:
-            rule = oneandone_conn.get_firewall_policy_rule(
-                firewall_id=firewall_id,
-                rule_id=rule_id)
-            if rule:
-                return True
-            return False
+    options.add_parse_callback(callback)
     
-        gateway = vca.get_gateway(vdc_name, gateway_name)
-    if not gateway:
-        module.fail_json(msg='Not able to find the gateway %s, please check '
-                             'the gateway_name param' % gateway_name)
+    For each function or class described in `tornado.platform.interface`,
+the appropriate platform-specific implementation exists in this module.
+Most code that needs access to this functionality should do e.g.::
     
-        module = AnsibleModule(
-        argument_spec=dict(
-            cluster=dict(default='localhost'),
-            port=dict(default='5433'),
-            db=dict(default=None),
-            login_user=dict(default='dbadmin'),
-            login_password=dict(default=None, no_log=True),
-        ), supports_check_mode=True)
-    
-        role = module.params['role']
-    assigned_roles = []
-    if module.params['assigned_roles']:
-        assigned_roles = module.params['assigned_roles'].split(',')
-        assigned_roles = filter(None, assigned_roles)
-    state = module.params['state']
-    db = ''
-    if module.params['db']:
-        db = module.params['db']
-    
-    
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['stableinterface'],
-                    'supported_by': 'core'}
-    
-    
-DOCUMENTATION = '''
----
-module: airbrake_deployment
-version_added: '1.2'
-author: 'Bruce Pennypacker (@bpennypacker)'
-short_description: Notify airbrake about app deployments
-description:
-   - Notify airbrake about app deployments (see http://help.airbrake.io/kb/api-2/deploy-tracking)
-options:
-  token:
-    description:
-      - API token.
-    required: true
-  environment:
-    description:
-      - The airbrake environment name, typically 'production', 'staging', etc.
-    required: true
-  user:
-    description:
-      - The username of the person doing the deployment
-    required: false
-  repo:
-    description:
-      - URL of the project repository
-    required: false
-  revision:
-    description:
-      - A hash, number, tag, or other identifier showing what revision was deployed
-    required: false
-  url:
-    description:
-      - Optional URL to submit the notification to. Use to send notifications to Airbrake-compliant tools like Errbit.
-    required: false
-    default: 'https://airbrake.io/deploys.txt'
-    version_added: '1.5'
-  validate_certs:
-    description:
-      - If C(no), SSL certificates for the target url will not be validated. This should only be used
-        on personally controlled sites using self-signed certificates.
-    required: false
-    default: 'yes'
-    type: bool
-    
-            if not query_log_status(module, le_path, log):
-            module.fail_json(msg='failed to follow '%s': %s' % (log, err.strip()))
-    
-        if running and state == 'stopped':
-        if module.check_mode:
-            module.exit_json(changed=True)
-        status = run_command('stop')
-        if status in ['not monitored'] or 'stop pending' in status:
-            module.exit_json(changed=True, name=name, state=state)
-        module.fail_json(msg='%s process not stopped' % name, status=status)
-    
-        def __init__(self, *a, **kw):
-        super(QPSSpider, self).__init__(*a, **kw)
-        if self.qps is not None:
-            self.qps = float(self.qps)
-            self.download_delay = 1 / self.qps
-        elif self.download_delay is not None:
-            self.download_delay = float(self.download_delay)
-    
-        # --- backwards compatibility for scrapy.conf.settings singleton ---
-    if settings is None and 'scrapy.conf' in sys.modules:
-        from scrapy import conf
-        if hasattr(conf, 'settings'):
-            settings = conf.settings
-    # ------------------------------------------------------------------
-    
-    from scrapy.commands import ScrapyCommand
-from scrapy.settings import BaseSettings
-    
-    from . import Contract
-    
-            # kept for old-style HTTP/1.0 downloader context twisted calls,
-        # e.g. connectSSL()
-        def getContext(self, hostname=None, port=None):
-            return self.getCertificateOptions().getContext()
-    
-            # If no credentials could be found anywhere,
-        # consider this an anonymous connection request by default;
-        # unless 'anon' was set explicitly (True/False).
-        anon = kw.get('anon')
-        if anon is None and not aws_access_key_id and not aws_secret_access_key:
-            kw['anon'] = True
-        self.anon = kw.get('anon')
-    
-    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
-# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
-# positive matches at the risk of more false negatives.
-    
-    print('I found {} face(s) in this photograph.'.format(len(face_locations)))
-    
-    
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    
-    
-def face_distance(face_encodings, face_to_compare):
-    '''
-    Given a list of face encodings, compare them to a known face encoding and get a euclidean distance
-    for each comparison face. The distance tells you how similar the faces are.
-    
-        process_this_frame = not process_this_frame
-    
-        def test_cnn_face_locations(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
-        detected_faces = api.face_locations(img, model='cnn')
-    
-        # Print the location of each facial feature in this image
-    for facial_feature in face_landmarks.keys():
-        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
-    
-    ax = fig.add_axes((0.45, 0.1, 0.16, 0.8))
-bar_data = [2.1, -00.8, 1.1, 2.5, -2.1, -0.5, -2.0, 1.5]
-ax.set_ylim(-3, 3)
-ax.set_xticks([])
-ax.set_yticks([])
-ax.bar(np.arange(len(bar_data)), bar_data)
-    
-            for prop, val in list(props.items()):
-            if val == 'inherit':
-                val = inherited.get(prop, 'initial')
-            if val == 'initial':
-                val = self.INITIAL_STYLE.get(prop)
-    
-        See :class:`Packer` for options.
-    '''
-    return Packer(**kwargs).pack(o)
+    Template expressions are surrounded by double curly braces: ``{{ ... }}``.
+The contents may be any python expression, which will be escaped according
+to the current autoescape setting and inserted into the output.  Other
+template directives use ``{% %}``.
