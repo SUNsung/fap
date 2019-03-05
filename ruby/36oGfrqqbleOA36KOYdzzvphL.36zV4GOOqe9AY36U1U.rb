e8a1f98ffcc3ede197f3c16fa32e8bcc8fb4c68e
SUNsung <weatherius@gmail.com>
2019-03-05 13:46:11 +0200

    Rewrite [36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb] File in: 2019-03-05 13:46:10

--- ruby/36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb
+++ ruby/36oGfrqqbleOA36KOYdzzvphL.36zV4GOOqe9AY36U1U.rb
@@ -1,84 +1,48 @@
 
         
-          #
-  # Initializes an HTTP packet header class that inherits from a Hash base
-  # class.
-  #
-  def initialize
-    self.dcase_hash = {}
-    
-    IAX_IE_CALLED_NUMBER  = 1
-IAX_IE_CALLING_NUMBER = 2
-IAX_IE_AUTH_METHODS   = 3
-IAX_IE_CALLING_NAME   = 4
-IAX_IE_USERNAME       = 6
-IAX_IE_DESIRED_CODEC  = 9
-IAX_IE_ORIGINAL_DID   = 10
-IAX_IE_ACTUAL_CODECS  = 8
-IAX_IE_PROTO_VERSION  = 11
-IAX_IE_REG_REFRESH    = 19
-IAX_IE_CHALLENGE_DATA = 15
-IAX_IE_CHALLENGE_RESP = 16
-IAX_IE_APPARENT_ADDR  = 18
-IAX_IE_REGREJ_CAUSE   = 22
-IAX_IE_HANGUP_CAUSE   = 42
-    
-      # Setup the root logger with the Rails log level and the desired set of
-  # appenders. The list of appenders to use should be set in the environment
-  # specific configuration file.
-  #
-  # For example, in a production application you would not want to log to
-  # STDOUT, but you would want to send an email for 'error' and 'fatal'
-  # messages:
-  #
-  # => config/environments/production.rb
-  #
-  #     config.log_to = %w[file email]
-  #
-  # In development you would want to log to STDOUT and possibly to a file:
-  #
-  # => config/environments/development.rb
-  #
-  #     config.log_to = %w[stdout file]
-  #
-  Logging.logger.root.appenders = config.log_to unless config.log_to.empty?
-    
-      class Notification < ApplicationRecord
-  end
-    
-    Then (/^I should see the 'getting started' contents$/) do
-  confirm_getting_started_contents
-end
-
-    
-    Then /^'([^']*)' should be post (\d+)$/ do |post_text, position|
-  stream_element_numbers_content(position).should have_content(post_text)
-end
-    
-        context 'on a post you partecipate to' do
-      before { alice.participate! post }
-    
-    Given(/^a custom task to generate a file$/) do
-  TestApp.copy_task_to_test_app('spec/support/tasks/database.rake')
-end
-    
-      def test_file_exists(path)
-    exists?('f', path)
-  end
-    
-        def setup_filters
-      @filters = cmdline_filters
-      @filters += @custom_filters if @custom_filters
-      @filters << Filter.new(:role, ENV['ROLES']) if ENV['ROLES']
-      @filters << Filter.new(:host, ENV['HOSTS']) if ENV['HOSTS']
-      fh = fetch_for(:filter, {}) || {}
-      @filters << Filter.new(:host, fh[:hosts]) if fh[:hosts]
-      @filters << Filter.new(:role, fh[:roles]) if fh[:roles]
-      @filters << Filter.new(:host, fh[:host]) if fh[:host]
-      @filters << Filter.new(:role, fh[:role]) if fh[:role]
+            def confirmation_instructions(record, token, opts={})
+      @token = token
+      devise_mail(record, :confirmation_instructions, opts)
     end
     
-            print_deprecation_warnings_if_applicable
+          def forget_cookie_values(resource)
+        Devise::Controllers::Rememberable.cookie_values.merge!(resource.rememberable_options)
+      end
     
-    set_if_empty :format, :airbrussh
-set_if_empty :log_level, :debug
+          # Sign out all active users or scopes. This helper is useful for signing out all roles
+      # in one click. This signs out ALL scopes in warden. Returns true if there was at least one logout
+      # and false if there was no user logged in on all scopes.
+      def sign_out_all_scopes(lock=true)
+        users = Devise.mappings.keys.map { |s| warden.user(scope: s, run_callbacks: false) }
+    
+        def default_constraints(options)
+      @constraints = Hash.new
+      @constraints.merge!(options[:constraints]) if options[:constraints]
+    end
+    
+          # Resets reset password token and send reset password instructions by email.
+      # Returns the token sent in the e-mail.
+      def send_reset_password_instructions
+        token = set_reset_password_token
+        send_reset_password_instructions_notification(token)
+    
+          def self.required_fields(klass)
+        []
+      end
+    
+      if rss_url && rss_url.length > 0
+    blogs.push(Struct::Blog.new(name, web_url, rss_url))
+  else
+    unavailable.push(Struct::Blog.new(name, web_url, rss_url))
+  end
+end
+    
+        # From asking people, it seems MacPorts does not have a `prefix` command, like
+    # Homebrew does, so make an educated guess:
+    if port_prefix = prefix_from_bin('port')
+      prefixes << port_prefix
+    end
+    
+    #{stack}
+#{executable_path}
+### Plugins
