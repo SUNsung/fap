ca373b48c4293b2ef8201c632798ae7a934310b3
SUNsung <weatherius@gmail.com>
2019-03-05 14:26:14 +0200

    Rewrite [586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp] File in: 2019-03-05 14:26:14

--- cccccc/586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp
+++ cccccc/586VzBtUyxckI58WqcVSAXLoEI58jweQAeADkCA583V4.cpp
@@ -1,368 +1,153 @@
 
         
         
-    {    QString appName;
-    QIcon appIcon;
-    QIcon trayAndWindowIcon;
-    QString titleAddText;
-};
-    
-    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            rcx:rbx = d
- *            r10-r14 = a0-a4
- *            r15     = M (0xfffffffffffff)
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
-    'movq $0xfffffffffffff,%%r15\n'
-    }
-    
-            secp256k1_ecmult_const(&res, &pt, &s);
-        secp256k1_ge_set_gej(&pt, &res);
-        /* Compute a hash of the point in compressed form
-         * Note we cannot use secp256k1_eckey_pubkey_serialize here since it does not
-         * expect its output to be secret and has a timing sidechannel. */
-        secp256k1_fe_normalize(&pt.x);
-        secp256k1_fe_normalize(&pt.y);
-        secp256k1_fe_get_b32(x, &pt.x);
-        y[0] = 0x02 | secp256k1_fe_is_odd(&pt.y);
-    
-        // A more complex valid grammar. PROTOCOLINFO accepts a VersionLine that
-    // takes a key=value pair followed by an OptArguments, making this valid.
-    // Because an OptArguments contains no semantic data, there is no point in
-    // parsing it.
-    CheckParseTorReplyMapping(
-        'SOME=args,here MORE optional=arguments  here', {
-            {'SOME', 'args,here'},
-        });
-    
-    ; ROTATE_ARGS
-; Rotate values of symbols a...h
-%macro ROTATE_ARGS 0
-%xdefine TMP_ h
-%xdefine h g
-%xdefine g f
-%xdefine f e
-%xdefine e d
-%xdefine d c
-%xdefine c b
-%xdefine b a
-%xdefine a TMP_
-%endm
-    
-      static void ClearCache(content::RenderProcessHost* render_view_host);
-  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
-                             const std::string& proxy_config);
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
-};
-    
-      content::RenderProcessHost* render_process_host = GetSenderWebContents()->GetMainFrame()->GetProcess();
-  net::URLRequestContextGetter* context_getter =
-    render_process_host->GetStoragePartition()->GetURLRequestContext();
-    
-      private:
-    bool ReadText(ClipboardData& data) {
-      DCHECK(data.type == TYPE_TEXT);
-      base::string16 text;
-      clipboard_->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
-      data.data.reset(new std::string(base::UTF16ToUTF8(text)));
-      return true;
-    }
-    
-    
     {
-  DECLARE_EXTENSION_FUNCTION('nw.Clipboard.getListSync', UNKNOWN)
- private:
-  DISALLOW_COPY_AND_ASSIGN(NwClipboardGetListSyncFunction);
-};
-    
-      // implement nw.Screen.initEventListeners()
-  class NwScreenInitEventListenersFunction: public NWSyncExtensionFunction {
-    public:
-      NwScreenInitEventListenersFunction();
-      bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    # define INSTANTIATE_TEST_CASE_P(prefix, test_case_name, generator) \
-  ::testing::internal::ParamGenerator<test_case_name::ParamType> \
-      gtest_##prefix##test_case_name##_EvalGenerator_() { return generator; } \
-  int gtest_##prefix##test_case_name##_dummy_ = \
-      ::testing::UnitTest::GetInstance()->parameterized_test_registry(). \
-          GetTestCasePatternHolder<test_case_name>(\
-              #test_case_name, __FILE__, __LINE__)->AddTestCaseInstantiation(\
-                  #prefix, \
-                  &gtest_##prefix##test_case_name##_EvalGenerator_, \
-                  __FILE__, __LINE__)
-    
-      // Same as above, but you can choose the interception scope of this object.
-  ScopedFakeTestPartResultReporter(InterceptMode intercept_mode,
-                                   TestPartResultArray* result);
-    
-      // Verifies that registered_tests match the test names in
-  // defined_test_names_; returns registered_tests if successful, or
-  // aborts the program otherwise.
-  const char* VerifyRegisteredTestNames(
-      const char* file, int line, const char* registered_tests);
-    
-      // Copy an existing linked_ptr<>, adding ourselves to the list of references.
-  template <typename U> linked_ptr(linked_ptr<U> const& ptr) { copy(&ptr); }
-  linked_ptr(linked_ptr const& ptr) {  // NOLINT
-    assert(&ptr != this);
-    copy(&ptr);
-  }
-    
-    
-template <typename T1, typename T2, typename T3, typename T4, typename T5>
-class CartesianProductGenerator5
-    : public ParamGeneratorInterface< ::std::tr1::tuple<T1, T2, T3, T4, T5> > {
- public:
-  typedef ::std::tr1::tuple<T1, T2, T3, T4, T5> ParamType;
-    }
-    
-      // Asserts that s.c_string() returns NULL.
-  //
-  // <TechnicalDetails>
-  //
-  // If we write NULL instead of
-  //
-  //   static_cast<const char *>(NULL)
-  //
-  // in this assertion, it will generate a warning on gcc 3.4.  The
-  // reason is that EXPECT_EQ needs to know the types of its
-  // arguments in order to print them when it fails.  Since NULL is
-  // #defined as 0, the compiler will use the formatter function for
-  // int to print it.  However, gcc thinks that NULL should be used as
-  // a pointer, not an int, and therefore complains.
-  //
-  // The root of the problem is C++'s lack of distinction between the
-  // integer number 0 and the null pointer constant.  Unfortunately,
-  // we have to live with this fact.
-  //
-  // </TechnicalDetails>
-  EXPECT_STREQ(NULL, s.c_string());
-    
-            bool IsInfinite() override;
-    
-        template <>
-    /*static*/ NDArrayViewPtr NDArrayView::RandomUniform<int16_t>(const NDShape& shape, double rangeBegin, double rangeEnd, unsigned long seed, const DeviceDescriptor& device)
-    {
-        return NDArrayView::_RandomUniform<int16_t, short>(shape, rangeBegin, rangeEnd, seed, device);
-    }
-    
-        // Make sure that the dictionary contains all required keys, and if it does, return version value
-    // from the dictionary.
-    template <typename T>
-    inline size_t ValidateDictionary(const Dictionary& dict, const std::vector<std::wstring>& requiredKeys, const std::wstring& typeValue, size_t currentVersion)
-    { 
-        const auto& version = GetVersion(dict);
-    }
-    
-            std::vector<size_t> sequenceLengths(numSequences);
-        size_t maxSequenceLength = 0;
-        auto dataType = sequences[0]->GetDataType();
-        auto storageFormat = sequences[0]->GetStorageFormat();
-        NDShape fullyDefinedSampleShape = sampleShape;
-        for (size_t i = 0; i < numSequences; ++i)
-        {
-            auto currentSequenceData = sequences[i];
-            if (currentSequenceData->GetDataType() != dataType)
-                InvalidArgument('Value::Create: The data for all sequences/samples must have the same data type');
-    }
-    
-        const NDShape& Variable::Shape() const
-    {
-        return m_dataFields->m_shape; 
-    }
-    
-    
-    {
-    {    private:
-        // Disallow copy and move construction and assignment
-        VariableFields(const VariableFields&) = delete; VariableFields& operator=(const VariableFields& other) = delete; VariableFields(VariableFields&&) = delete; VariableFields& operator=(VariableFields&&) = delete;
-    };
-}
+    {}  // namespace swig
+}  // namespace tensorflow
 
     
-    // GetData - Gets metadata from the specified section (into CPU memory)
-// sectionName - section name to retrieve data from
-// numRecords - number of records to read
-// data - pointer to data buffer, if NULL, dataBufferSize will be set to size of required buffer to accomidate request
-// dataBufferSize - [in] size of the databuffer in bytes
-//                  [out] size of buffer filled with data
-// recordStart - record to start reading from, defaults to zero (start of data)
-// returns: true if data remains to be read, false if the end of data was reached
-bool DataReader::GetData(const std::wstring& sectionName, size_t numRecords, void* data, size_t& dataBufferSize, size_t recordStart)
-{
-    bool bRet = true;
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-        bRet &= m_dataReaders[m_ioNames[i]]->GetData(sectionName, numRecords, data, dataBufferSize, recordStart);
-    return bRet;
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    #ifndef TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_BFLOAT16_H_
+    
+    // Destructor passed to TF_NewTensor when it reuses a numpy buffer. Stores a
+// pointer to the pyobj in a buffer to be dereferenced later when we're actually
+// holding the GIL.
+void DelayedNumpyDecref(void* data, size_t len, void* obj) {
+  mutex_lock ml(*DelayedDecrefLock());
+  DecrefCache()->push_back(obj);
+}
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+      // Return the current record contents.  Only valid after the preceding call
+  // to GetNext() returned true
+  string record() const { return record_; }
+  // Return the current offset in the file.
+  uint64 offset() const { return offset_; }
+    
+    bool UvTaskRunner::PostDelayedTask(const base::Location& from_here,
+                                   base::OnceClosure task,
+                                   base::TimeDelta delay) {
+  auto* timer = new uv_timer_t;
+  timer->data = this;
+  uv_timer_init(loop_, timer);
+  uv_timer_start(timer, UvTaskRunner::OnTimeout, delay.InMilliseconds(), 0);
+  tasks_[timer] = std::move(task);
+  return true;
+}
+    
+    #ifndef ATOM_BROWSER_API_ATOM_API_AUTO_UPDATER_H_
+#define ATOM_BROWSER_API_ATOM_API_AUTO_UPDATER_H_
+    
+    class AtomBrowserContext;
+    
+    void Event::FrameDeleted(content::RenderFrameHost* rfh) {
+  if (sender_ != rfh)
+    return;
+  sender_ = nullptr;
+  message_ = nullptr;
+}
+    
+      // Returns all objects in this class's weak map.
+  static std::vector<v8::Local<v8::Object>> GetAll(v8::Isolate* isolate) {
+    if (weak_map_)
+      return weak_map_->Values(isolate);
+    else
+      return std::vector<v8::Local<v8::Object>>();
+  }
+    
+    void AtomQuotaPermissionContext::RequestQuotaPermission(
+    const content::StorageQuotaParams& params,
+    int render_process_id,
+    const PermissionCallback& callback) {
+  callback.Run(response::QUOTA_PERMISSION_RESPONSE_ALLOW);
+}
+    
+    size_t num_threads = 31;
+size_t work_chunk  = 120;
+    
+      SrcLoc(SrcPos start, SrcPos past)
+    : start(start)
+    , past(past)
+  {}
+    
+    #ifndef HPHP_GLOB_STREAM_WRAPPER_H
+#define HPHP_GLOB_STREAM_WRAPPER_H
+    
+    namespace HPHP {
+    }
+    
+    void logAHMSubMapWarning(folly::StringPiece mapName) {
+  StackTrace st;
+  logPerfWarning(
+    'AtomicHashMap overflow',
+    [&](StructuredLogEntry& cols) {
+      cols.setStr('map_name', mapName);
+      cols.setStackTrace('stack', st);
+    }
+  );
 }
     
     
-    {    ~ScopeTimer()
-    {
-        if (m_verbosity > 2)
-        {
-            m_aggregateTimer.Stop();
-            double time = m_aggregateTimer.ElapsedSeconds();
-            fprintf(stderr, m_message.c_str(), time);
+    {    return 0;
+}
+    
+            for(int i = 0 ; i < nums.size() ; i ++){
+            unordered_map<int,int>::iterator iter = record.find(target - nums[i]);
+            if(iter != record.end() && iter->second != i){
+                int res[] = {i, iter->second};
+                return vector<int>(res, res + 2);
+            }
         }
-    }
-};
     
-        // initialize by reading a matrix from a text file
-    void InitFromFile(const std::wstring& initFromFilePath);
+            ListNode* tail = pre->next;
+        ListNode* left;
+        pre->next = reverse(pre->next, n - m, left);
+        tail->next = left;
     
-    ScientificNumberFormatter *ScientificNumberFormatter::createSuperscriptInstance(
-            DecimalFormat *fmtToAdopt, UErrorCode &status) {
-    return createInstance(fmtToAdopt, new SuperscriptStyle(), status);
-}
-    
-    UnicodeString&
-SelectFormat::format(const Formattable& obj,
-                   UnicodeString& appendTo,
-                   FieldPosition& pos,
-                   UErrorCode& status) const
-{
-    if (U_FAILURE(status)) {
-        return appendTo;
-    }
-    if (obj.getType() == Formattable::kString) {
-        return format(obj.getString(status), appendTo, pos, status);
-    } else {
-        status = U_ILLEGAL_ARGUMENT_ERROR;
-        return appendTo;
-    }
-}
-    
-    #ifndef __SHARED_CALENDAR_H__
-#define __SHARED_CALENDAR_H__
-    
-    class NumberFormat;
-    
-            // Determine day period.
-        DayPeriodRules::DayPeriod periodType;
-        if (hour == 0 && minute == 0 && second == 0 && ruleSet->hasMidnight()) {
-            periodType = DayPeriodRules::DAYPERIOD_MIDNIGHT;
-        } else if (hour == 12 && minute == 0 && second == 0 && ruleSet->hasNoon()) {
-            periodType = DayPeriodRules::DAYPERIOD_NOON;
-        } else {
-            periodType = ruleSet->getDayPeriodForHour(hour);
-        }
-    
-        // Handle a cursor preceding the output
-    if (hasCursor && cursor < 0) {
-        while (cursor++ < 0) {
-            ICU_Utility::appendToRule(rule, (UChar)0x0040 /*@*/, TRUE, escapeUnprintable, quoteBuf);
-        }
-        // Fall through and append '|' below
+    private:
+    void __inorderTraversal(TreeNode* node, vector<int> &res){
     }
     
-    // finish.
-void assembly_builder::visit(func_def_syntax &node)
-{
-	in_global = false;
-	
-	// check if multiple definition.
-	if (functions.count(node.name)) {
-		err.error(node.line, node.pos, 'Function '' + node.name + '' multiply defined. Skipping.');
-		error_flag = true;
-		return;
-	}
-	
-	FunctionType *FT = FunctionType::get(Type::getVoidTy(context), {}, false);
-	current_function = Function::Create(FT, GlobalValue::LinkageTypes::ExternalLinkage, node.name, module.get());
-	functions.emplace(node.name, current_function);
-    }
-    
-        input_func = Function::Create(FunctionType::get(Type::getVoidTy(module->getContext()), {}, false),
-                                  GlobalValue::LinkageTypes::ExternalLinkage,
-                                  'input',
-                                  module);
-    builder.SetInsertPoint(BasicBlock::Create(module->getContext(), 'entry', input_func));
-    builder.CreateCall(input_impl, {input_var});
-    builder.CreateRetVoid();
+                cur = stack.top();
+            stack.pop();
+            res.push_back(cur->val);
+            cur = cur->right;
     
     
+    {                if(node->left){
+                    q.push(node->left);
+                    new_level_num ++;
+                }
+                if(node->right){
+                    q.push(node->right);
+                    new_level_num ++;
+                }
+            }
     
-        rapidjson::StringBuffer s;
-    rapidjson::PrettyWriter<decltype(s)> writer(s);
-    c1_recognizer::syntax_tree::syntax_tree_serializer<decltype(writer)> serializer(writer);
-    serializer.serialize(*ast);
-    
-    PointArray* PointArray::create(ssize_t capacity)
-{
-    PointArray* pointArray = new (std::nothrow) PointArray();
-    if (pointArray && pointArray->initWithCapacity(capacity))
-    {
-        pointArray->autorelease();
-        return pointArray;
-    }
-    }
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-    __CCCallFuncND * __CCCallFuncND::create(Ref* selectorTarget, SEL_CallFuncND selector, void* d)
-{
-    __CCCallFuncND* ret = new (std::nothrow) __CCCallFuncND();
-    
-    if (ret && ret->initWithTarget(selectorTarget, selector, d))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    
-    CC_SAFE_DELETE(ret);
-    return nullptr;
-}
-    
-    // FIXME: Passing 'const O *' instead of 'const O&' because HASH_FIND_IT requires the address of a pointer
-// and, it is not possible to get the address of a reference
-size_t ActionManager::getNumberOfRunningActionsInTargetByTag(const Node *target,
-                                                             int tag)
-{
-    CCASSERT(tag != Action::INVALID_TAG, 'Invalid tag value!');
-    }
-    
-    
-    {    delete action;
-    return nullptr;
-}
-    
-    /**
- @brief This action simulates a page turn from the bottom right hand corner of the screen.
- 
- @details It's not much use by itself but is used by the PageTurnTransition.
-         Based on an original paper by L Hong et al.
-         http://www.parc.com/publication/1638/turning-pages-of-3d-electronic-books.html
-  
- @since v0.8.2
- */
-class CC_DLL PageTurn3D : public Grid3DAction
-{
-public:
-    /**
-     * @js NA 
-     */
-    virtual GridBase* getGrid() override;
-    }
-    
-    
+        TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+    vector<int> res = Solution().preorderTraversal(root);
+    print_vec(res);
