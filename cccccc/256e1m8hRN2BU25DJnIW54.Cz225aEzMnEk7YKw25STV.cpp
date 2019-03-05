e9f87678d31dd94e0e67424bd42380056f5d2284
SUNsung <weatherius@gmail.com>
2019-03-05 10:35:14 +0200

    Rewrite [256e1m8hRN2BU25DJnIW54.Cz225aEzMnEk7YKw25STV.cpp] File in: 2019-03-05 10:35:14

--- cccccc/256e1m8hRN2BU25DJnIW54.Cz225aEzMnEk7YKw25STV.cpp
+++ cccccc/256e1m8hRN2BU25DJnIW54.Cz225aEzMnEk7YKw25STV.cpp
@@ -1,266 +1,206 @@
 
         
         
+    {  name += UNLV_EXT;              //add extension
+  if ((pdfp = fopen (name.string (), 'rb')) == nullptr) {
+    return false;                //didn't read one
+  } else {
+    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
+                                 //make rect block
+      block = new BLOCK (name.string (), TRUE, 0, 0,
+                         (int16_t) x, (int16_t) (ysize - y - height),
+                         (int16_t) (x + width), (int16_t) (ysize - y));
+                                 //on end of list
+      block_it.add_to_end (block);
+    }
+    fclose(pdfp);
+  }
+  return true;
+}
+    
+      // Update the other members if the cost is lower.
+  void UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
+                      int32_t n, int32_t sig_x, int64_t sig_xsq);
+    
+        DIR128 & operator= (         //assign of int16_t
+    int16_t value) {               //value to assign
+      value %= MODULUS;          //modulo arithmetic
+      if (value < 0)
+        value += MODULUS;        //done properly
+      dir = (int8_t) value;
+      return *this;
+    }
+    int8_t operator- (             //subtraction
+      const DIR128 & minus) const//for signed result
+    {
+                                 //result
+      int16_t result = dir - minus.dir;
+    }
+    
+    // Sanitize db options.  The caller should delete result.info_log if
+// it is not equal to src.info_log.
+Options SanitizeOptions(const std::string& db,
+                        const InternalKeyComparator* icmp,
+                        const InternalFilterPolicy* ipolicy,
+                        const Options& src);
+    
+    std::string InternalKey::DebugString() const {
+  std::string result;
+  ParsedInternalKey parsed;
+  if (ParseInternalKey(rep_, &parsed)) {
+    result = parsed.DebugString();
+  } else {
+    result = '(bad)';
+    result.append(EscapeString(rep_));
+  }
+  return result;
+}
+    
+    // Called on every item found in a WriteBatch.
+class WriteBatchItemPrinter : public WriteBatch::Handler {
+ public:
+  WritableFile* dst_;
+  virtual void Put(const Slice& key, const Slice& value) {
+    std::string r = '  put '';
+    AppendEscapedStringTo(&r, key);
+    r += '' '';
+    AppendEscapedStringTo(&r, value);
+    r += ''\n';
+    dst_->Append(r);
+  }
+  virtual void Delete(const Slice& key) {
+    std::string r = '  del '';
+    AppendEscapedStringTo(&r, key);
+    r += ''\n';
+    dst_->Append(r);
+  }
+};
+    
+    Status SetCurrentFile(Env* env, const std::string& dbname,
+                      uint64_t descriptor_number) {
+  // Remove leading 'dbname/' and add newline to manifest file name
+  std::string manifest = DescriptorFileName(dbname, descriptor_number);
+  Slice contents = manifest;
+  assert(contents.starts_with(dbname + '/'));
+  contents.remove_prefix(dbname.size() + 1);
+  std::string tmp = TempFileName(dbname, descriptor_number);
+  Status s = WriteStringToFileSync(env, contents.ToString() + '\n', tmp);
+  if (s.ok()) {
+    s = env->RenameFile(tmp, CurrentFileName(dbname));
+  }
+  if (!s.ok()) {
+    env->DeleteFile(tmp);
+  }
+  return s;
+}
+    
+        if (type == kZeroType && length == 0) {
+      // Skip zero length record without reporting any drops since
+      // such records are produced by the mmap based writing code in
+      // env_posix.cc that preallocates file regions.
+      buffer_.clear();
+      return kBadRecord;
+    }
+    
+    TEST(LogTest, RandomRead) {
+  const int N = 500;
+  Random write_rnd(301);
+  for (int i = 0; i < N; i++) {
+    Write(RandomSkewedString(i, &write_rnd));
+  }
+  Random read_rnd(301);
+  for (int i = 0; i < N; i++) {
+    ASSERT_EQ(RandomSkewedString(i, &read_rnd), Read());
+  }
+  ASSERT_EQ('EOF', Read());
+}
+    
+    // Encode a suitable internal key target for 'target' and return it.
+// Uses *scratch as scratch space, and the returned pointer will point
+// into this scratch space.
+static const char* EncodeKey(std::string* scratch, const Slice& target) {
+  scratch->clear();
+  PutVarint32(scratch, target.size());
+  scratch->append(target.data(), target.size());
+  return scratch->data();
+}
+    
+        SequenceNumber max_sequence = 0;
+    for (size_t i = 0; i < tables_.size(); i++) {
+      if (max_sequence < tables_[i].max_sequence) {
+        max_sequence = tables_[i].max_sequence;
+      }
+    }
     
      private:
-  // Recursive copy function.
-  void crop_copy(const vector<Blob<Dtype>*>& bottom,
-               const vector<Blob<Dtype>*>& top,
-               const int* offsets,
-               vector<int> indices,
-               int cur_dim,
-               const Dtype* src_data,
-               Dtype* dest_data,
-               bool is_forward);
-    
-    namespace caffe {
-    }
-    
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of ReLULayer.
- */
-template <typename Dtype>
-class CuDNNReLULayer : public ReLULayer<Dtype> {
- public:
-  explicit CuDNNReLULayer(const LayerParameter& param)
-      : ReLULayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNReLULayer();
-    }
-    
-    #ifdef USE_CUDNN
-/**
- * @brief CuDNN acceleration of SigmoidLayer.
- */
-template <typename Dtype>
-class CuDNNSigmoidLayer : public SigmoidLayer<Dtype> {
- public:
-  explicit CuDNNSigmoidLayer(const LayerParameter& param)
-      : SigmoidLayer<Dtype>(param), handles_setup_(false) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNSigmoidLayer();
-    }
-    
-      // Has a background compaction been scheduled or is running?
-  bool background_compaction_scheduled_ GUARDED_BY(mutex_);
-    
-    namespace leveldb {
-    }
-    
-    // Header is checksum (4 bytes), length (2 bytes), type (1 byte).
-static const int kHeaderSize = 4 + 2 + 1;
+  enum { kMaxHeight = 12 };
     
     
     {
-    {    // Compare against model iterator
-    for (std::set<Key>::reverse_iterator model_iter = keys.rbegin();
-         model_iter != keys.rend();
-         ++model_iter) {
-      ASSERT_TRUE(iter.Valid());
-      ASSERT_EQ(*model_iter, iter.key());
-      iter.Prev();
+    {}}
+    
+      // Use reserved function linkage register
+  const ssize_t address = ssize_t(m_address);
+  a.limmediate(reg::r12, address, immt, immMayChange);
+    
+      /*
+   * Create an uncounted array if we can.
+   *
+   * If this collection is an OuterHandle, then we need to do a full check on
+   * this array for things like circularity.  If we're an InnerHandle, someone
+   * already checked that, but we want to check for whether it's uncounted to
+   * use a better representation.  For the OuterHandle case, we just delegate
+   * to APCArray below (which will do the full DataWalker pass).
+   */
+  if (level == APCHandleLevel::Inner && apcExtension::UseUncounted &&
+      !array->empty()) {
+    DataWalker walker(DataWalker::LookupFeature::HasObjectOrResource);
+    auto const features = walker.traverseData(const_cast<ArrayData*>(array));
+    assertx(!features.isCircular);
+    if (!features.hasObjectOrResource) {
+      auto const makeUncounted = [&] () {
+        if (isVectorCollection(obj->collectionType())) {
+          return APCArray::MakeUncountedVec(const_cast<ArrayData*>(array));
+        }
+        return APCArray::MakeUncountedDict(const_cast<ArrayData*>(array));
+      };
+      return WrapArray(
+        { makeUncounted(), getMemSize(array) + sizeof(APCTypedValue) },
+        obj->collectionType()
+      );
     }
-    ASSERT_TRUE(!iter.Valid());
   }
-}
     
+    int execute_program(int argc, char **argv);
     
-    {}  // namespace leveldb
+    #include 'hphp/util/perf-event.h'
     
-        if (U_SUCCESS(status)) {
-        reset();
-        while (find()) {
-            appendReplacement(dest, replacement, status);
-            if (U_FAILURE(status)) {
-                break;
+    /// Two pointers
+/// Time Complexity:  O(n)
+/// Space Complexity: O(1)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    }
+    }
+    
+    public:
+    vector<int> inorderTraversal(TreeNode* root) {
+    }
+    
+    int main() {
+    }
+    
+            TreeNode* p = root;
+        while(p != NULL || !stack.empty()){
+            if(p != NULL){
+                stack.push(p);
+                output.push(p);
+                p = p->right;
+            }
+            else{
+                p = stack.top();
+                stack.pop();
+                p = p->left;
             }
         }
-        appendTail(dest, status);
-    }
-    
-        if (U_FAILURE(status)) {
-        return NULL;
-    }
-    if (U_FAILURE(fDeferredStatus)) {
-        status = fDeferredStatus;
-        return NULL;
-    }
-    
-    UCollationResult
-RuleBasedCollator::compare(const UChar *left, int32_t leftLength,
-                           const UChar *right, int32_t rightLength,
-                           UErrorCode &errorCode) const {
-    if(U_FAILURE(errorCode)) { return UCOL_EQUAL; }
-    if((left == NULL && leftLength != 0) || (right == NULL && rightLength != 0)) {
-        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
-        return UCOL_EQUAL;
-    }
-    // Make sure both or neither strings have a known length.
-    // We do not optimize for mixed length/termination.
-    if(leftLength >= 0) {
-        if(rightLength < 0) { rightLength = u_strlen(right); }
-    } else {
-        if(rightLength >= 0) { leftLength = u_strlen(left); }
-    }
-    return doCompare(left, leftLength, right, rightLength, errorCode);
-}
-    
-    U_NAMESPACE_END
-    
-    UnicodeString&
-SelectFormat::toPattern(UnicodeString& appendTo) {
-    if (0 == msgPattern.countParts()) {
-        appendTo.setToBogus();
-    } else {
-        appendTo.append(msgPattern.getPatternString());
-    }
-    return appendTo;
-}
-    
-    U_NAMESPACE_BEGIN
-    
-    
-U_NAMESPACE_END
-    
-    #if !UCONFIG_NO_FORMATTING
-    
-        /**
-     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
-     *
-     * @param keyword for example 'few' or 'other'
-     * @return the plural form corresponding to the keyword
-     */
-    static Form fromString(const UnicodeString &keyword, UErrorCode &errorCode) {
-        return static_cast<Form>(indexFromString(keyword, errorCode));
-    }
-    
-     private:
-  StreamBufferReader reader_;
-  int tmp_ch;
-  int num_prev;
-  unsigned char buf_prev[2];
-  // whether we need to do strict check
-  static const bool kStrictCheck = false;
-};
-/*! \brief the stream that write to base64, note we take from file pointers */
-class Base64OutStream: public dmlc::Stream {
- public:
-  explicit Base64OutStream(dmlc::Stream *fp) : fp(fp) {
-    buf_top = 0;
-  }
-  virtual void Write(const void *ptr, size_t size) {
-    using base64::EncodeTable;
-    size_t tlen = size;
-    const unsigned char *cptr = static_cast<const unsigned char*>(ptr);
-    while (tlen) {
-      while (buf_top < 3  && tlen != 0) {
-        buf[++buf_top] = *cptr++; --tlen;
-      }
-      if (buf_top == 3) {
-        // flush 4 bytes out
-        PutChar(EncodeTable[buf[1] >> 2]);
-        PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
-        PutChar(EncodeTable[((buf[2] << 2) | (buf[3] >> 6)) & 0x3F]);
-        PutChar(EncodeTable[buf[3] & 0x3F]);
-        buf_top = 0;
-      }
-    }
-  }
-  virtual size_t Read(void *ptr, size_t size) {
-    LOG(FATAL) << 'Base64OutStream do not support read';
-    return 0;
-  }
-  /*!
-   * \brief finish writing of all current base64 stream, do some post processing
-   * \param endch character to put to end of stream, if it is EOF, then nothing will be done
-   */
-  inline void Finish(char endch = EOF) {
-    using base64::EncodeTable;
-    if (buf_top == 1) {
-      PutChar(EncodeTable[buf[1] >> 2]);
-      PutChar(EncodeTable[(buf[1] << 4) & 0x3F]);
-      PutChar('=');
-      PutChar('=');
-    }
-    if (buf_top == 2) {
-      PutChar(EncodeTable[buf[1] >> 2]);
-      PutChar(EncodeTable[((buf[1] << 4) | (buf[2] >> 4)) & 0x3F]);
-      PutChar(EncodeTable[(buf[2] << 2) & 0x3F]);
-      PutChar('=');
-    }
-    buf_top = 0;
-    if (endch != EOF) PutChar(endch);
-    this->Flush();
-  }
-    
-    
-    {
-    {
-    {  // base margin
-  bst_float base_margin;
-  // model parameter
-  GBTreeModelParam param;
-  /*! \brief vector of trees stored in the model */
-  std::vector<std::unique_ptr<RegTree> > trees;
-  /*! \brief for the update process, a place to keep the initial trees */
-  std::vector<std::unique_ptr<RegTree> > trees_to_update;
-  /*! \brief some information indicator of the tree, reserved */
-  std::vector<int> tree_info;
-};
-}  // namespace gbm
-}  // namespace xgboost
-
-    
-    // logistic loss, but predict un-transformed margin
-struct LogisticRaw : public LogisticRegression {
-  // duplication is necessary, as __device__ specifier
-  // cannot be made conditional on template parameter
-  XGBOOST_DEVICE static bst_float PredTransform(bst_float x) { return x; }
-  XGBOOST_DEVICE static bst_float FirstOrderGradient(bst_float predt, bst_float label) {
-    predt = common::Sigmoid(predt);
-    return predt - label;
-  }
-  XGBOOST_DEVICE static bst_float SecondOrderGradient(bst_float predt, bst_float label) {
-    const float eps = 1e-16f;
-    predt = common::Sigmoid(predt);
-    return fmaxf(predt * (1.0f - predt), eps);
-  }
-  template <typename T>
-    static T PredTransform(T x) { return x; }
-  template <typename T>
-    static T FirstOrderGradient(T predt, T label) {
-    predt = common::Sigmoid(predt);
-    return predt - label;
-  }
-  template <typename T>
-    static T SecondOrderGradient(T predt, T label) {
-    const T eps = T(1e-16f);
-    predt = common::Sigmoid(predt);
-    return std::max(predt * (T(1.0f) - predt), eps);
-  }
-  static const char* DefaultEvalMetric() { return 'auc'; }
-};
-    
-      virtual void PredictInteractionContributions(DMatrix* dmat,
-                           std::vector<bst_float>* out_contribs,
-                           unsigned ntree_limit, bool approximate) = 0;
-    
-    
-    {    // out_of_range.404
-    try
-    {
-        // try to use a JSON pointer that cannot be resolved
-        json::const_reference ref = j.at('/number/foo'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-}
