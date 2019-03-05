4943f18a02ab7a3337330a64b3cf80f6bf0e38d8
SUNsung <weatherius@gmail.com>
2019-03-05 23:55:59 +0200

    Rewrite [41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb] File in: 2019-03-05 23:55:59

--- ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
+++ ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
@@ -1,83 +1,45 @@
 
         
-        module Gitlab
-  module GithubImport
-    module Importer
-      class DiffNotesImporter
-        include ParallelScheduling
+                  @highlighter = begin
+            if @config.key?('enable_coderay') && @config['enable_coderay']
+              Jekyll::Deprecator.deprecation_message(
+                'You are using 'enable_coderay', ' \
+                'use syntax_highlighter: coderay in your configuration file.'
+              )
     
-            def collection_method
-          :issues
-        end
+      desc 'update main and version in bower.json'
+  task :generate do
+    require 'bootstrap-sass'
+    Dir.chdir Bootstrap.gem_path do
+      spec       = JSON.parse(File.read 'bower.json')
     
-                rows << {
-              label_id: label_id,
-              target_id: target_id,
-              target_type: issue.issuable_type,
-              created_at: time,
-              updated_at: time
-            }
-          end
-    
-        # This will detect the proper guest OS for the machine and set up
-    # the class to actually execute capabilities.
-    def detect!
-      guest_name = @machine.config.vm.guest
-      initialize_capabilities!(guest_name, @guests, @capabilities, @machine)
-    rescue Errors::CapabilityHostExplicitNotDetected => e
-      raise Errors::GuestExplicitNotDetected, value: e.extra_data[:value]
-    rescue Errors::CapabilityHostNotDetected
-      raise Errors::GuestNotDetected
-    end
-    
-            # Checks if the target machine is ready for communication. If this
-        # returns true, then all the other methods for communicating with
-        # the machine are expected to be functional.
-        #
-        # @return [Boolean]
-        def ready?
-          false
-        end
-    
-            # Helper method that will set a value if a value is given, or otherwise
-        # return the already set value.
-        #
-        # @param [Symbol] key Key for the data
-        # @param [Object] value Value to store.
-        # @return [Object] Stored value.
-        def self.get_or_set(key, value=UNSET_VALUE)
-          # If no value is to be set, then return the value we have already set
-          return data[key] if value.eql?(UNSET_VALUE)
-    
-            # This contains all the registered guest capabilities.
-        #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :guest_capabilities
-    
-      if rss_url.nil?
-    puts '#{name}: GETTING'
-    rss_check_url = 'http://ajax.googleapis.com/ajax/services/feed/lookup?v=1.0&q=#{web_url}'
-    uri = URI.parse(rss_check_url)
-    response = JSON.parse(Net::HTTP.get(uri))
-    rss_url = response['responseData']['url'] if response['responseData'] && response['responseData'].has_key?('url')
-    
-        context 'and a comment after the last element' do
-      let(:b_comment) { ' # comment b' }
-    
-          # Returns an array of all the when branches in the `case` statement.
-      #
-      # @return [Array<WhenNode>] an array of `when` nodes
-      def when_branches
-        node_parts[1...-1]
-      end
-    
-    
-    {      # Checks whether the `hash` literal is delimited by curly braces.
-      #
-      # @return [Boolean] whether the `hash` literal is enclosed in braces
-      def braces?
-        loc.end && loc.end.is?('}')
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
       end
     end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
   end
 end
+
+    
+    desc 'Start an IRB session with all necessary files required.'
+task :shell do |t|
+  chdir File.dirname(__FILE__)
+  exec 'irb -I lib/ -I lib/paperclip -r rubygems -r active_record -r tempfile -r init'
+end
+    
+    When /^(?:|I )follow '([^']*)'$/ do |link|
+  click_link(link)
+end
+    
+      def framework_major_version
+    framework_version.split('.').first.to_i
+  end
+end
+World(RailsCommandHelpers)
