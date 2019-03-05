9c78241aaccc0dfd31b85f0cd1fe69905bbd0980
SUNsung <weatherius@gmail.com>
2019-03-05 07:43:32 +0200

    Rewrite [18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py] File in: 2019-03-05 07:43:32

--- pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
+++ pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
@@ -1,159 +1,91 @@
 
         
-        # Direction codes used in the paths.
-DIRS = '_^V<>'
-DIR_TO_ID = {dir: did for did, dir in enumerate(DIRS)}
+            publicKey, privateKey = generateKey(keySize)
+    print('\nWriting public key to file %s_pubkey.txt...' % name)
+    with open('%s_pubkey.txt' % name, 'w') as fo:
+        fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
     
-      LFADS is an unsupervised method to decompose time series data into
-  various factors, such as an initial condition, a generative
-  dynamical system, inferred inputs to that generator, and a low
-  dimensional description of the observed data, called the factors.
-  Additoinally, the observations have a noise model (in this case
-  Poisson), so a denoised version of the observations is also created
-  (e.g. underlying rates of a Poisson distribution given the observed
-  event counts).
-  '''
+                if new_key is None:
+                break
     
-    # Controlling the size of the generator is one way to control complexity of
-# the dynamics (there is also l2, which will squeeze out unnecessary
-# dynamics also).  The modern deep learning approach is to make these cells
-# as large as tolerable (from a waiting perspective), and then regularize
-# them to death with drop out or whatever.  I don't know if this is correct
-# for the LFADS application or not.
-flags.DEFINE_integer('gen_dim', GEN_DIM,
-                     'Cell hidden size, generator.')
-# The weights of the generator cell by default set to scale at
-# ws/sqrt(#inputs), with ws=1.0.  You can change ws for
-# the input weights or the recurrent weights with these hyperparameters.
-flags.DEFINE_float('gen_cell_input_weight_scale', GEN_CELL_INPUT_WEIGHT_SCALE,
-                     'Input scaling for input weights in generator.')
-flags.DEFINE_float('gen_cell_rec_weight_scale', GEN_CELL_REC_WEIGHT_SCALE,
-                     'Input scaling for rec weights in generator.')
-    
-    flags = tf.app.flags
-flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
-                    'Directory for saving data.')
-flags.DEFINE_string('datafile_name', 'thits_data',
-                    'Name of data file for input case.')
-flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
-flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
-flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
-flags.DEFINE_integer('C', 100, 'Number of conditions')
-flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
-flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
-flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
-flags.DEFINE_float('train_percentage', 4.0/5.0,
-                   'Percentage of train vs validation trials')
-flags.DEFINE_integer('nreplications', 40,
-                     'Number of noise replications of the same underlying rates.')
-flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
-flags.DEFINE_float('x0_std', 1.0,
-                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
-flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
-flags.DEFINE_float('dt', 0.010, 'Time bin')
-flags.DEFINE_float('input_magnitude', 20.0,
-                   'For the input case, what is the value of the input?')
-flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
-FLAGS = flags.FLAGS
-    
-    # save down the network outputs (may be useful later)
-train_outputs_u, valid_outputs_u = split_list_by_inds(outs_e,
-                                                      train_inds,
-                                                      valid_inds)
-train_outputs_u = np.array(train_outputs_u)
-valid_outputs_u = np.array(valid_outputs_u)
-    
-    # split into train and validation sets
-train_inds, valid_inds = get_train_n_valid_inds(E, train_percentage,
-                                                nreplications)
-    
-      Args:
-    vocab: Vocabulary. Contains vocabulary size and converts word to ids.
-  '''
-  assert FLAGS.save_dir, 'Must specify FLAGS.save_dir for dump_emb.'
-  inputs = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
-  targets = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
-  weights = np.ones([BATCH_SIZE, NUM_TIMESTEPS], np.float32)
-    
-    
-def _read_words(filename):
-  with tf.gfile.GFile(filename, 'r') as f:
-    return f.read().decode('utf-8').replace('\n', '<eos>').split()
-    
-    Launch command:
-  python generate_samples.py
-  --data_dir=/tmp/data/imdb  --data_set=imdb
-  --batch_size=256 --sequence_length=20 --base_directory=/tmp/imdb
-  --hparams='gen_rnn_size=650,dis_rnn_size=650,gen_num_layers=2,
-  gen_vd_keep_prob=1.0' --generator_model=seq2seq_vd
-  --discriminator_model=seq2seq_vd --is_present_rate=0.5
-  --maskgan_ckpt=/tmp/model.ckpt-45494
-  --seq2seq_share_embedding=True --dis_share_embedding=True
-  --attention_option=luong --mask_strategy=contiguous --baseline_method=critic
-  --number_epochs=4
+    The problem is  :
+Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
+Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
 '''
+from __future__ import print_function
     
-      # Split the batch into half.  Use half for MC estimates for REINFORCE.
-  # Use the other half to establish a baseline.
-  elif FLAGS.baseline_method == 'dis_batch':
-    # TODO(liamfedus):  Recheck.
-    [rewards_half, baseline_half] = tf.split(
-        rewards, num_or_size_splits=2, axis=0)
-    [log_probs_half, _] = tf.split(log_probs, num_or_size_splits=2, axis=0)
-    [reward_present_half, baseline_present_half] = tf.split(
-        present, num_or_size_splits=2, axis=0)
+        def __init__(self, arr):
+        # we need a list not a string, so do something to change the type
+        self.array = arr.split(',')
+        print(('the input array is:', self.array))
     
-            '''
-        super(AugeasConfigurator, self).recovery_routine()
-        # Need to reload configuration after these changes take effect
-        self.aug.load()
+    ''' Here I implemented the scoring functions.
+    MAE, MSE, RMSE, RMSLE are included.
     
-    from certbot_apache import configurator
-from certbot_apache import entrypoint
+    def getFrequencyOrder(message):
+    letterToFreq = getLetterCount(message)
+    freqToLetter = {}
+    for letter in LETTERS:
+        if letterToFreq[letter] not in freqToLetter:
+            freqToLetter[letterToFreq[letter]] = [letter]
+        else:
+            freqToLetter[letterToFreq[letter]].append(letter)
     
-            complex_vh = VirtualHost(
-            'fp', 'vhp',
-            set([Addr.fromstring('*:443'), Addr.fromstring('1.2.3.4:443')]),
-            False, False)
-        self.assertTrue(complex_vh.conflicts([self.addr1]))
-        self.assertTrue(complex_vh.conflicts([self.addr2]))
-        self.assertFalse(complex_vh.conflicts([self.addr_default]))
+    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    title = r1(r'<title>(.*)</title>', html)
+    assert title
+    id = r1(r'flvid\s*=\s*(\d+)', html)
+    assert id
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
     
-            with mock.patch(
-            'certbot_apache.override_debian.DebianConfigurator.enable_mod'):
-            sni_responses = self.sni.perform()
+        if re.match(r'https?://movie', url):
+        title = match1(html, 'name='description' content='([^']+)')
+        tid = match1(url, 'trailer/(\d+)')
+        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
+        type, ext, size = url_info(real_url)
     
-            :param list ip_addrs: addresses of challenged domain
-            :class:`list` of type `~.obj.Addr`
+    __all__ = ['facebook_download']
     
-    # If false, no module index is generated.
-#texinfo_domain_indices = True
+    #----------------------------------------------------------------------
+def makeMimi(upid):
+    '''From http://cdn37.atwikiimg.com/sitescript/pub/dksitescript/FC2.site.js
+    Also com.hps.util.fc2.FC2EncrptUtil.makeMimiLocal
+    L110'''
+    strSeed = 'gGddgPfeaf_gzyr'
+    prehash = upid + '_' + strSeed
+    return md5(prehash.encode('utf-8')).hexdigest()
     
-        pil_image.show()
+    # (api_key, method, ext, page)
+tmpl_api_call = (
+    'https://api.flickr.com/services/rest?'
+    '&format=json&nojsoncallback=1'
+    # UNCOMMENT FOR TESTING
+    #'&per_page=5'
+    '&per_page=500'
+    # this parameter CANNOT take control of 'flickr.galleries.getPhotos'
+    # though the doc said it should.
+    # it's always considered to be 500
+    '&api_key=%s'
+    '&method=flickr.%s'
+    '&extras=url_sq,url_q,url_t,url_s,url_n,url_m,url_z,url_c,url_l,url_h,url_k,url_o,media'
+    '%s&page=%d'
+)
+    
+                if title is None:
+                response = request.urlopen(request.Request(real_url))
+                if response.headers['content-disposition']:
+                    filename = parse.unquote(r1(r'filename='?(.+)'?', response.headers['content-disposition'])).split('.')
+                    title = ''.join(filename[:-1])
+        except: pass
+    
+        def get_confidence(self):
+        context_conf = self.context_analyzer.get_confidence()
+        distrib_conf = self.distribution_analyzer.get_confidence()
+        return max(context_conf, distrib_conf)
 
     
-        if len(unknown_face_encodings) > 0:
-        face_found = True
-        # See if the first face in the uploaded image matches the known face of Obama
-        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
-        if match_results[0]:
-            is_obama = True
-    
-        if os.path.isdir(image_to_check):
-        if cpus == 1:
-            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
-        else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
-    else:
-        test_image(image_to_check, model)
-    
-    # Open video file
-video_capture = cv2.VideoCapture('short_hamilton_clip.mp4')
-    
-        for unknown_encoding in unknown_encodings:
-        distances = face_recognition.face_distance(known_face_encodings, unknown_encoding)
-        result = list(distances <= tolerance)
-    
-    # Create a PIL imagedraw object so we can draw on the picture
-pil_image = Image.fromarray(image)
-d = ImageDraw.Draw(pil_image)
+            After calling ``feed``, you can check the value of the ``done``
+        attribute to see if you need to continue feeding the
+        ``UniversalDetector`` more data, or if it has made a prediction
+        (in the ``result`` attribute).
