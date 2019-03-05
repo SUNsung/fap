a391da685f36c1e44f1674bda76fbf36e3213d6b
SUNsung <weatherius@gmail.com>
2019-03-05 03:14:01 +0200

    Rewrite [37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp] File in: 2019-03-05 03:14:00

--- cccccc/37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp
+++ cccccc/37j0tKrgMFuA637280y1oLSQsk37bjyTnYVy8CU37JaX.cpp
@@ -1,188 +1,180 @@
 
         
-        #include <unordered_map>
+          static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
     
-        http://www.apache.org/licenses/LICENSE-2.0
+     private:
+  atom::RenderProcessPreferences preferences_;
     
-    
-    {  if (!register_ufunc('equal', CompareUFunc<Bfloat16EqFunctor>,
-                      compare_types)) {
-    return false;
-  }
-  if (!register_ufunc('not_equal', CompareUFunc<Bfloat16NeFunctor>,
-                      compare_types)) {
-    return false;
-  }
-  if (!register_ufunc('less', CompareUFunc<Bfloat16LtFunctor>, compare_types)) {
-    return false;
-  }
-  if (!register_ufunc('greater', CompareUFunc<Bfloat16GtFunctor>,
-                      compare_types)) {
-    return false;
-  }
-  if (!register_ufunc('less_equal', CompareUFunc<Bfloat16LeFunctor>,
-                      compare_types)) {
-    return false;
-  }
-  if (!register_ufunc('greater_equal', CompareUFunc<Bfloat16GeFunctor>,
-                      compare_types)) {
-    return false;
-  }
-  return true;
-}
-    
-    #ifndef PyObject_HEAD
-struct _object;
-typedef _object PyObject;
-#endif
-    
-      // Return the current record contents.  Only valid after the preceding call
-  // to GetNext() returned true
-  string record() const { return record_; }
-  // Return the current offset in the file.
-  uint64 offset() const { return offset_; }
-    
-    
-    {
-    {}  // namespace cuda
-}  // namespace stream_executor
-
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-            // returns current per-minibatch variance momentum value.
-        double VarianceMomentumValueForMB(size_t minibatchSize) const
-        {
-            //TODO: According to my preliminary analysis, the second momentum variance scaling is different from momentum scaling; need to double check -- yuqing tang
-            return MomentumValueForMB(m_varianceMomentumSchedule, minibatchSize);
-        }
-    
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-#include 'Utils.h'
-#include 'ReaderShim.h'
-#include 'DataReader.h'
-    
-    
-    {        GetMatrix()->AssignValuesOf(*source.GetMatrix());
+    namespace atom {
     }
     
-        // this returns the map directly (read-only) and will lazily initialize it for a given seed
-    const std::vector<INDEXTYPE>& operator()(size_t seed) // throw()
-    {
-        // if wrong seed then lazily recache the sequence
-        if (seed != currentseed && randomizationrange != randomizeDisable)
-        {
-            // test for numeric overflow
-            if (map.size() - 1 != (INDEXTYPE)(map.size() - 1))
-                RuntimeError('RandomOrdering: INDEXTYPE has too few bits for this corpus');
-            // 0, 1, 2...
-            foreach_index (t, map)
-                map[t] = (INDEXTYPE) t;
-    }
+    #include <string>
+    
+    namespace atom {
     }
     
+    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
+                                       net::NetworkDelegate* network_delegate)
+    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
     
-    {            // Set the sorted rk order to each url and 
-            // the urls are still in the original order
-            int rk = 0;
-            for (it = its0; it != its; it++)
-            {
-                urls[it->m_rank0].m_rank = rk++;
-            }
-        }
-    
-    struct ID3D10Device;
-    
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_IMPL_API void     ImGui_ImplDX9_InvalidateDeviceObjects();
-IMGUI_IMPL_API bool     ImGui_ImplDX9_CreateDeviceObjects();
-
-    
-    IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window, bool install_callbacks);
-IMGUI_IMPL_API void     ImGui_ImplGlfw_Shutdown();
-IMGUI_IMPL_API void     ImGui_ImplGlfw_NewFrame();
-    
-    int32 ImGui_Marmalade_PointerButtonEventCallback(void* system_data, void* user_data)
-{
-    // pEvent->m_Button is of type s3ePointerButton and indicates which mouse
-    // button was pressed.  For touchscreen this should always have the value
-    // S3E_POINTER_BUTTON_SELECT
-    s3ePointerEvent* pEvent = (s3ePointerEvent*)system_data;
+    class URLRequestAboutJob : public net::URLRequestJob {
+ public:
+  URLRequestAboutJob(net::URLRequest*, net::NetworkDelegate*);
     }
     
-    
-    {    // At this point note that we set ImGui::GetIO().Fonts->TexID to be == g_FontTexture, so clear both.
-    ImGuiIO& io = ImGui::GetIO();
-    IM_ASSERT(g_FontTexture == io.Fonts->TexID);
-    if (g_FontTexture)
-        g_FontTexture->Release();
-    g_FontTexture = NULL;
-    io.Fonts->TexID = NULL;
-}
-    
-    // GLFW
-#include <GLFW/glfw3.h>
-#ifdef _WIN32
-#undef APIENTRY
-#define GLFW_EXPOSE_NATIVE_WIN32
-#include <GLFW/glfw3native.h>   // for glfwGetWin32Window
-#endif
-#define GLFW_HAS_WINDOW_TOPMOST     (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3200) // 3.2+ GLFW_FLOATING
-#define GLFW_HAS_WINDOW_HOVERED     (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3300) // 3.3+ GLFW_HOVERED
-#define GLFW_HAS_WINDOW_ALPHA       (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3300) // 3.3+ glfwSetWindowOpacity
-#define GLFW_HAS_PER_MONITOR_DPI    (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3300) // 3.3+ glfwGetMonitorContentScale
-#define GLFW_HAS_VULKAN             (GLFW_VERSION_MAJOR * 1000 + GLFW_VERSION_MINOR * 100 >= 3200) // 3.2+ glfwCreateWindowSurface
+    ```
     
     
-    {  void setTimeout(std::chrono::seconds timeout)
-  {
-    timeout_ = std::move(timeout);
-  }
+    {          return out;
+        });
+OPERATOR_SCHEMA(Float16ConstantFill)
+    .NumInputs(0)
+    .NumOutputs(1)
+    .TensorInferenceFunction(Float16FillerTensorInference)
+    .Arg('value', 'The value for the elements of the output tensor.')
+    .Arg('shape', 'The shape of the output tensor.')
+    .Output(
+        0,
+        'output',
+        'Output tensor of constant values specified by 'value'');
+    
+    OPERATOR_SCHEMA(Im2Col)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc('The Im2Col operator from Matlab.')
+    .TensorInferenceFunction(
+        [](const OperatorDef& def, const vector<TensorShape>& in) {
+          ArgumentHelper helper(def);
+          auto pad = helper.GetSingleArgument<int>('pad', 0);
+          auto kernel_h = helper.GetSingleArgument<int>(
+              'kernel_h', helper.GetSingleArgument<int>('kernel', 0));
+          auto kernel_w = helper.GetSingleArgument<int>(
+              'kernel_w', helper.GetSingleArgument<int>('kernel', 0));
+          auto dilation_h = helper.GetSingleArgument<int>(
+              'dilation_h', helper.GetSingleArgument<int>('dilation', 1));
+          auto dilation_w = helper.GetSingleArgument<int>(
+              'dilation_w', helper.GetSingleArgument<int>('dilation', 1));
+          auto stride_h = helper.GetSingleArgument<int>(
+              'stride_h', helper.GetSingleArgument<int>('stride', 1));
+          auto stride_w = helper.GetSingleArgument<int>(
+              'stride_w', helper.GetSingleArgument<int>('stride', 1));
+          auto order = StringToStorageOrder(
+              helper.GetSingleArgument<string>('order', 'NCHW'));
+    }
+    
+    template <typename T, class Context>
+class BernoulliJSDOp final : public Operator<Context> {
+ public:
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
 };
     
-      virtual void fillMessage(Dict* msgDict) CXX11_OVERRIDE;
+      grpc_error* Init(grpc_call_element* elem,
+                   const grpc_call_element_args* args) override;
     
+    MeasureDouble RpcServerServerLatency() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcServerServerLatencyMeasureName,
+      'Time between first byte of request received to last byte of response '
+      'sent, or terminal error',
+      kUnitMilliseconds);
+  return measure;
+}
     
-    {} // namespace aria2
-
+    ::opencensus::stats::MeasureInt64 RpcServerSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcServerReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcServerServerLatency();
+::opencensus::stats::MeasureInt64 RpcServerCompletedRpcs();
     
-        dispatcher->setTimeout(std::chrono::seconds(messageTimeout));
+    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_SERVER_FILTER_H
+#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_SERVER_FILTER_H
     
-    
-    {} // namespace aria2
-    
-    
-    {} // namespace aria2
-
-    
-    class DHTTaskFactoryImpl : public DHTTaskFactory {
-private:
-  std::shared_ptr<DHTNode> localNode_;
+    namespace grpc {
+namespace load_reporter {
+    }
     }
     
-    namespace aria2 {
-    }
-    
-      DHTTaskExecutor immediateTaskQueue_;
-    
-      // do nothing; we don't use this message as outgoing message.
-  virtual bool send() CXX11_OVERRIDE;
-    
-    #include <glog/logging.h>
-    
-      FunctionRef<int(int)> fref = lambda;
-  EXPECT_EQ(1005, fref(5));
-  EXPECT_EQ(1011, fref(6));
-  EXPECT_EQ(1018, fref(7));
-    
-    #include <type_traits>
+    #include <grpc/support/log.h>
     
     
-    {    return newlocal;
+    {  virtual void printTo(std::ostream& out) const;
+};
+    
+      // Get an integral value, 0 or 1, for whether a syscall table pointer is modified.
+  auto f1 = osquery::readFile(kKernelSyscallAddrModifiedPath, content);
+  if (f1.ok()) {
+    boost::trim(content);
+    syscall_addr_modified = content;
+  } else {
+    VLOG(1) << 'Cannot read file: ' << kKernelSyscallAddrModifiedPath;
+    return results;
   }
     
-    #pragma once
+      STDMETHODIMP RegisterOutputCallbacks::QueryInterface(
+      THIS_ _In_ REFIID InterfaceId, _Out_ PVOID* Interface) {
+    *Interface = nullptr;
+    if (IsEqualIID(InterfaceId, __uuidof(IUnknown)) ||
+        IsEqualIID(InterfaceId, __uuidof(IDebugOutputCallbacks))) {
+      *Interface = (IDebugOutputCallbacks*)this;
+      AddRef();
+      return S_OK;
+    } else {
+      return E_NOINTERFACE;
+    }
+  }
+    
+    
+    {
+    {struct mei_version {
+  uint32_t important_details[7];
+  uint16_t major;
+  uint16_t minor;
+  uint16_t hotfix;
+  uint16_t build;
+  uint16_t r_major;
+  uint16_t r_minor;
+  uint16_t r_hotfix;
+  uint16_t r_build;
+  uint16_t codes[6];
+};
+}
+}
+
+    
+    Expected<EbpfTracepoint, LinuxProbesControl::Error> createTracepointForSyscall(
+    syscall::EventType type, PerfEventCpuMap const& cpu_map) {
+  auto program_exp = genLinuxProgram(BPF_PROG_TYPE_TRACEPOINT, cpu_map, type);
+  if (program_exp.isError()) {
+    return createError(LinuxProbesControl::Error::SystemEbpf,
+                       'could not load program to track syscall ',
+                       program_exp.takeError())
+           << to<std::string>(type);
+  }
+  auto tracing_path_exp = toTracingPath(type);
+  if (tracing_path_exp.isError()) {
+    return createError(LinuxProbesControl::Error::InvalidArgument,
+                       '',
+                       tracing_path_exp.takeError());
+  }
+  auto sys_event_exp = tracing::NativeEvent::load(tracing_path_exp.take());
+  if (sys_event_exp.isError()) {
+    return createError(LinuxProbesControl::Error::SystemNativeEvent,
+                       'could not enable linux event for ',
+                       sys_event_exp.takeError())
+           << to<std::string>(type);
+  }
+  auto tracepoint_exp =
+      events::EbpfTracepoint::load(sys_event_exp.take(), program_exp.take());
+  if (tracepoint_exp.isError()) {
+    return createError(
+               LinuxProbesControl::Error::SystemTracepoint,
+               'could not attach tracing prograp to the native event of ',
+               tracepoint_exp.takeError())
+           << to<std::string>(type);
+  }
+  return tracepoint_exp.take();
+}
