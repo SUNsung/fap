df5e9ef2f03e125c2aded14f98f799d15b4168a2
SUNsung <weatherius@gmail.com>
2019-03-06 01:41:06 +0200

    Rewrite [57IIby4XLSllw57BQyaNx3yP9Q57gdCFOwvI2g.57gRO.py] File in: 2019-03-06 01:41:05

--- pyton/57IIby4XLSllw57BQyaNx3yP9Q57gdCFOwvI2g.57gRO.py
+++ pyton/57IIby4XLSllw57BQyaNx3yP9Q57gdCFOwvI2g.57gRO.py
@@ -1,120 +1,140 @@
-        if any([s_line.startswith(s) for s in ['* [', '- [']]):
-            if indent == last_indent:
-                blocks[-1].append(line)
-            else:
-                blocks.append([line])
-            last_indent = indent
-        else:
-            blocks.append([line])
-            last_indent = None
+
+        
+        
+def make_setting_element(setting_data, app, fromdocname):
+    refnode = make_refnode(app.builder, fromdocname,
+                           todocname=setting_data['docname'],
+                           targetid=setting_data['refid'],
+                           child=nodes.Text(setting_data['setting_name']))
+    p = nodes.paragraph()
+    p += refnode
+    
+    del sys
+
+    
+    from scrapy.utils.conf import arglist_to_dict
+from scrapy.exceptions import UsageError
+    
+            def __init__(self, method=SSL.SSLv23_METHOD):
+            self.method = method
+    
+        def gotHeaders(self, headers):
+        self.headers_time = time()
+        self.response_headers = headers
+    
+        def _download(self, request, spider):
+        slot = self.slot
+        slot.add_request(request)
+        def _on_success(response):
+            assert isinstance(response, (Response, Request))
+            if isinstance(response, Response):
+                response.request = request # tie request to response received
+                logkws = self.logformatter.crawled(request, response, spider)
+                logger.log(*logformatter_adapter(logkws), extra={'spider': spider})
+                self.signals.send_catch_log(signal=signals.response_received, \
+                    response=response, request=request, spider=spider)
+            return response
     
     
-class AbstractBaseSession(models.Model):
-    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
-    session_data = models.TextField(_('session data'))
-    expire_date = models.DateTimeField(_('expire date'), db_index=True)
+def main():
+    print('Making key files...')
+    makeKeyFiles('elgamal', 2048)
+    print('Key files generation successful')
     
-        For complete documentation on using Sessions in your code, consult
-    the sessions documentation that is shipped with Django (also available
-    on the Django Web site).
+            if row1 == row2:
+            plaintext += table[row1*5+(col1-1)%5]
+            plaintext += table[row2*5+(col2-1)%5]
+        elif col1 == col2:
+            plaintext += table[((row1-1)%5)*5+col1]
+            plaintext += table[((row2-1)%5)*5+col2]
+        else: # rectangle
+            plaintext += table[row1*5+col2]
+            plaintext += table[row2*5+col1]
+    
+    if __name__ == '__main__':
+    num = generateLargePrime()
+    print(('Prime number:', num))
+    print(('isPrime:', isPrime(num)))
+
+    
+        print('Calculating d that is mod inverse of e...')
+    d = cryptoMath.findModInverse(e, (p - 1) * (q - 1))
+    
+                if new_key is None:
+                break
+    
+    This is a pure Python implementation of Dynamic Programming solution to the longest increasing subsequence of a given sequence.
+    
+        return matchScore
+    
+    from setuptools import setup, find_packages
+setup(
+    name = proj_info['name'],
+    version = VERSION,
+    
+    #----------------------------------------------------------------------
+#helper
+#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
+def dictify(r,root=True):
+    if root:
+        return {r.tag : dictify(r, False)}
+    d=copy(r.attrib)
+    if r.text:
+        d['_text']=r.text
+    for x in r.findall('./*'):
+        if x.tag not in d:
+            d[x.tag]=[]
+        d[x.tag].append(dictify(x,False))
+    return d
+    
+    
+    {
+    {pattern_inline_title = r'<title>([^<]*)</title>'
+pattern_inline_api_key = r'api\.site_key\s*=\s*'([^']+)''
+pattern_inline_img_url = r''url':'([^']+)','key':'[^']+'}}'
+pattern_inline_NSID = r''nsid'\s*:\s*'([^']+)''
+pattern_inline_video_mark = r'('mediaType':'video')'
+    
     '''
-    objects = SessionManager()
+*What is this pattern about?
+Define a family of algorithms, encapsulate each one, and make them interchangeable.
+Strategy lets the algorithm vary independently from clients that use it.
     
     
-class DatabaseErrorWrapper:
-    '''
-    Context manager and decorator that reraises backend-specific database
-    exceptions using Django's common wrappers.
-    '''
+class EnglishGetter(object):
     
     
-def to_native_string(string, encoding='ascii'):
-    '''Given a string object, regardless of type, returns a representation of
-    that string in the native string type, encoding and decoding where
-    necessary. This assumes ASCII unless told otherwise.
-    '''
-    if isinstance(string, builtin_str):
-        out = string
-    else:
-        if is_py2:
-            out = string.encode(encoding)
-        else:
-            out = string.decode(encoding)
-    
-        # Copy is required
-    def copy(self):
-        return CaseInsensitiveDict(self._store.values())
-    
-                # Consume content and release the original connection
-            # to allow our new request to reuse the same one.
-            r.content
-            r.close()
-            prep = r.request.copy()
-            extract_cookies_to_jar(prep._cookies, r.request, r.raw)
-            prep.prepare_cookies(prep._cookies)
-    
-        def get_full_url(self):
-        # Only return the response's URL if the user hadn't set the Host
-        # header
-        if not self._r.headers.get('Host'):
-            return self._r.url
-        # If they did set it, retrieve it and reconstruct the expected domain
-        host = to_native_string(self._r.headers['Host'], encoding='utf-8')
-        parsed = urlparse(self._r.url)
-        # Reconstruct the URL as we expect it
-        return urlunparse([
-            parsed.scheme, host, parsed.path, parsed.params, parsed.query,
-            parsed.fragment
-        ])
-    
-    
-def info():
-    '''Generate information for a bug report.'''
-    try:
-        platform_info = {
-            'system': platform.system(),
-            'release': platform.release(),
+class HierachicalStateMachine(object):
+    def __init__(self):
+        self._active_state = Active(self)  # Unit.Inservice.Active()
+        self._standby_state = Standby(self)  # Unit.Inservice.Standby()
+        self._suspect_state = Suspect(self)  # Unit.OutOfService.Suspect()
+        self._failed_state = Failed(self)  # Unit.OutOfService.Failed()
+        self._current_state = self._standby_state
+        self.states = {
+            'active': self._active_state,
+            'standby': self._standby_state,
+            'suspect': self._suspect_state,
+            'failed': self._failed_state,
         }
-    except IOError:
-        platform_info = {
-            'system': 'Unknown',
-            'release': 'Unknown',
+        self.message_types = {
+            'fault trigger': self._current_state.on_fault_trigger,
+            'switchover': self._current_state.on_switchover,
+            'diagnostics passed': self._current_state.on_diagnostics_passed,
+            'diagnostics failed': self._current_state.on_diagnostics_failed,
+            'operator inservice': self._current_state.on_operator_inservice,
         }
     
-        def initialize_options(self):
-        TestCommand.initialize_options(self)
+        # high-level i.e. Abstraction specific
+    def scale(self, pct):
+        self._radius *= pct
+    
+        def show_item_information(self, item_name):
         try:
-            from multiprocessing import cpu_count
-            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
-        except (ImportError, NotImplementedError):
-            self.pytest_args = ['-n', '1', '--boxed']
-    
-    # If false, no index is generated.
-#html_use_index = True
-    
-        # Additional stuff for the LaTeX preamble.
-    #'preamble': '',
-    
-    ALL_SSL_OPTIONS_HASHES = [
-    '2086bca02db48daf93468332543c60ac6acdb6f0b58c7bfdf578a5d47092f82a',
-    '4844d36c9a0f587172d9fa10f4f1c9518e3bcfa1947379f155e16a70a728c21a',
-    '5a922826719981c0a234b1fbcd495f3213e49d2519e845ea0748ba513044b65b',
-    '4066b90268c03c9ba0201068eaa39abbc02acf9558bb45a788b630eb85dadf27',
-    'f175e2e7c673bd88d0aff8220735f385f916142c44aa83b09f1df88dd4767a88',
-    'cfdd7c18d2025836ea3307399f509cfb1ebf2612c87dd600a65da2a8e2f2797b',
-    '80720bd171ccdc2e6b917ded340defae66919e4624962396b992b7218a561791',
-    'c0c022ea6b8a51ecc8f1003d0a04af6c3f2bc1c3ce506b3c2dfc1f11ef931082',
-]
-'''SHA256 hashes of the contents of previous versions of all versions of MOD_SSL_CONF_SRC'''
-    
-            '''
-        if self.get_addr() == '_default_':
-            return 0
-        elif self.get_addr() == '*':
-            return 1
+            item_info = self.model.get(item_name)
+        except Exception:
+            item_type = self.model.item_type
+            self.view.item_not_found(item_type, item_name)
         else:
-            return 2
-    
-    # If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-#show_authors = False
+            item_type = self.model.item_type
+            self.view.show_item_information(item_type, item_name, item_info)
