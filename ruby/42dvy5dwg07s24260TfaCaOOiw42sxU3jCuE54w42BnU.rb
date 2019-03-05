f6479c6a9c02ce1f2fe710d236b0226b82e7b8fd
SUNsung <weatherius@gmail.com>
2019-03-05 12:53:13 +0200

    Rewrite [42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb] File in: 2019-03-05 12:53:13

--- ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
+++ ruby/42dvy5dwg07s24260TfaCaOOiw42sxU3jCuE54w42BnU.rb
@@ -1,3 +1,63 @@
 
         
-        
+            def subpath
+      @subpath ||= subpath_to(current_url)
+    end
+    
+        def effective_url
+      @effective_url ||= URL.parse super
+    end
+    
+            css('.openParens').each do |node|
+          node.parent.name = 'pre'
+          node.parent.content = node.parent.css('code, pre').map(&:content).join('\n')
+        end
+    
+      it 'does not raise an error on a tainted, frozen object' do
+    o = Object.new.taint.freeze
+    o.taint.should equal(o)
+  end
+    
+    describe 'Kernel#trace_var' do
+  before :each do
+    $Kernel_trace_var_global = nil
+  end
+    
+      # Code Quality
+  gem 'inch_by_inch'
+  gem 'rubocop'
+    
+      def developer_prefix
+    `xcode-select --print-path`.strip
+  end
+    
+          # Checks whether this case statement has an `else` branch.
+      #
+      # @return [Boolean] whether the `case` statement has an `else` branch
+      def else?
+        loc.else
+      end
+    end
+  end
+end
+
+    
+    module RuboCop
+  module AST
+    # A node extension for `def` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `def` nodes within RuboCop.
+    class DefNode < Node
+      include ParameterizedNode
+      include MethodIdentifierPredicates
+    
+            pairs.map(&:key).each do |key|
+          yield key
+        end
+    
+          # Returns the index of the `when` branch within the `case` statement.
+      #
+      # @return [Integer] the index of the `when` branch
+      def branch_index
+        parent.when_branches.index(self)
+      end
