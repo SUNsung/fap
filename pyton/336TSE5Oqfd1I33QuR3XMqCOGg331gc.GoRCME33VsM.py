b39e2881af3ca149764920b570818af0ebaf4c5a
SUNsung <weatherius@gmail.com>
2019-03-06 20:50:43 +0200

    Rewrite [336TSE5Oqfd1I33QuR3XMqCOGg331gc.GoRCME33VsM.py] File in: 2019-03-06 20:50:42

--- pyton/336TSE5Oqfd1I33QuR3XMqCOGg331gc.GoRCME33VsM.py
+++ pyton/336TSE5Oqfd1I33QuR3XMqCOGg331gc.GoRCME33VsM.py
@@ -1,80 +1,162 @@
 
         
-                for srcobj, loader in self._iter_loaders(template):
-            try:
-                rv = loader.get_source(environment, template)
-                if trv is None:
-                    trv = rv
-            except TemplateNotFound:
-                rv = None
-            attempts.append((loader, srcobj, rv))
-    
-        def on_json_loading_failed(self, e):
-        '''Called if :meth:`get_json` parsing fails and isn't silenced. If
-        this method returns a value, it is used as the return value for
-        :meth:`get_json`. The default implementation raises a
-        :class:`BadRequest` exception.
-    
-        if version in tags:
-        fail('Version '%s' is already tagged', version)
+            @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
     
     
-@pytest.fixture
-def modules_tmpdir(tmpdir, monkeypatch):
-    '''A tmpdir added to sys.path.'''
-    rv = tmpdir.mkdir('modules_tmpdir')
-    monkeypatch.syspath_prepend(str(rv))
-    return rv
+def init_app(app):
+    '''Register database functions with the Flask app. This is called by
+    the application factory.
+    '''
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
     
-        def _ClassDef(self, t):
-        self.write('\n')
-        for deco in t.decorator_list:
-            self.fill('@')
-            self.dispatch(deco)
-        self.fill('class '+t.name)
-        self.write('(')
-        comma = False
-        for e in t.bases:
-            if comma: self.write(', ')
-            else: comma = True
-            self.dispatch(e)
-        for e in t.keywords:
-            if comma: self.write(', ')
-            else: comma = True
-            self.dispatch(e)
-        self.write(')')
+        auth.login()
+    response = client.get('/')
+    assert b'test title' in response.data
+    assert b'by test on 2018-01-01' in response.data
+    assert b'test\nbody' in response.data
+    assert b'href='/1/update'' in response.data
     
-    try:
-    WindowsError
-except NameError:
-    pass
-else:
-    PYTHON2_EXCEPTIONS += ('WindowsError',)
     
-    __all__ = ['commonprefix', 'exists', 'getatime', 'getctime', 'getmtime',
-           'getsize', 'isdir', 'isfile', 'samefile', 'sameopenfile',
-           'samestat']
+def _lookup_req_object(name):
+    top = _request_ctx_stack.top
+    if top is None:
+        raise RuntimeError(_request_ctx_err_msg)
+    return getattr(top, name)
     
-    def handleSlideTitle(title):
-    print('<h2>%s</h2>' % getText(title.childNodes))
+        def to_json(self, value):
+        # JSON objects may only have string keys, so don't bother tagging the
+        # key here.
+        return dict((k, self.serializer.tag(v)) for k, v in iteritems(value))
     
-        # Get and print some more results
-    for i in range(len(TASKS2)):
-        print('\t', done_queue.get())
+        if not has_level_handler(logger):
+        logger.addHandler(default_handler)
     
-    setup_end_to_end = '''
-import face_recognition
+            from .debughelpers import explain_template_loading_attempts
+        explain_template_loading_attempts(self.app, template, attempts)
     
-        # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    
+def set_filename_version(filename, version_number, pattern):
+    changed = []
+    
+        @property
+    def cache_key(self):
+        return self.cache_key_prefix + self._get_or_create_session_key()
+    
+        def save(self, must_create=False):
+        '''
+        Save the current session data to the database. If 'must_create' is
+        True, raise a database error if the saving operation doesn't create a
+        new entry (as opposed to possibly updating an existing entry).
+        '''
+        if self.session_key is None:
+            return self.create()
+        data = self._get_session(no_load=must_create)
+        obj = self.create_model_instance(data)
+        using = router.db_for_write(self.model, instance=obj)
+        try:
+            with transaction.atomic(using=using):
+                obj.save(force_insert=must_create, force_update=not must_create, using=using)
+        except IntegrityError:
+            if must_create:
+                raise CreateError
+            raise
+        except DatabaseError:
+            if not must_create:
+                raise UpdateError
+            raise
+    
+    
+class BaseSessionManager(models.Manager):
+    def encode(self, session_dict):
+        '''
+        Return the given session dictionary serialized and encoded as a string.
+        '''
+        session_store_class = self.model.get_session_store_class()
+        return session_store_class().encode(session_dict)
+    
+        def _test_host(self, url):
+        try:
+            header = {
+                'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36',
+                'accept': 'application/json, text/javascript, */*; q=0.01',
+                'accept-encoding': 'gzip, deflate, sdch',
+                'accept-language': 'en-US,en;q=0.8,ja;q=0.6,zh-CN;q=0.4,zh;q=0.2',
+                'connection': 'keep-alive'
+                }
+            response = self.http_client.request('HEAD', url, header, '', read_payload=False)
+            if response:
+                return True
+        except Exception as e:
+            if __name__ == '__main__':
+                xlog.exception('test %s e:%r', url, e)
+    
+        global pteredor_is_running
+    pteredor_is_running = True
+    server_list = prober.eval_servers()
+    pteredor_is_running = False
+    
+    Each recognizer pulls its input from one of the stream classes below. Streams
+handle stuff like buffering, look-ahead and seeking.
+    
+    	# A list of CharStreamState objects that tracks the stream state
+        # values line, charPositionInLine, and p that can change as you
+        # move through the input stream.  Indexed from 0..markDepth-1.
+        self._markers = [ ]
+        self.lastMarker = None
+        self.markDepth = 0
+    
+            Using setter/getter methods is deprecated. Use o.channel instead.'''
+    
+            for i in html_json['sources']:
+            if 'src' in i:  #to avoid KeyError
+                if i['src'].startswith('https'):
+                    link_list.append((str(i['height']), i['src']))
+    
+        for quality in ['1080','720','480','380','240','144','auto']:
+        try:
+            real_url = info[quality][1]['url']
+            if real_url:
+                break
+        except KeyError:
+            pass
+    
+        vids = matchall(content, yinyuetai_embed_patterns)
+    for vid in vids:
+        found = True
+        yinyuetai_download_by_id(vid, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
+    
+    
+def huomaotv_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    room_id_pattern = r'huomao.com/(\d+)'
+    room_id = match1(url, room_id_pattern)
+    html = get_content(get_mobile_room_url(room_id))
+    
+        point = readme_soup.find_all('h1')[1]
+    
+        # Display the results
+    for top, right, bottom, left in face_locations:
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+    
+    
+def print_result(filename, location):
+    top, right, bottom, left = location
+    print('{},{},{},{},{}'.format(filename, top, right, bottom, left))
     
             # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
     
-    # Initialize some variables
-face_locations = []
-face_encodings = []
+    while True:
+    print('Capturing image.')
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format='rgb')
     
     # This code finds all faces in a list of images using the CNN model.
 #
@@ -89,161 +171,20 @@ face_encodings = []
 # OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
 # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
     
-    print('I found {} face(s) in this photograph.'.format(len(face_landmarks_list)))
+        name = 'Unknown'
     
-        # 图片上传失败，输出以下html代码
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
-    
-    
-def cityscapes_to_coco(cityscapes_id):
-    lookup = {
-        0: 0,  # ... background
-        1: 2,  # bicycle
-        2: 3,  # car
-        3: 1,  # person
-        4: 7,  # train
-        5: 8,  # truck
-        6: 4,  # motorcycle
-        7: 6,  # bus
-        8: -1,  # rider (-1 means rand init)
-    }
-    return lookup[cityscapes_id]
-    
-    
-def voc_info(json_dataset):
-    year = json_dataset.name[4:8]
-    image_set = json_dataset.name[9:]
-    devkit_path = get_devkit_dir(json_dataset.name)
-    assert os.path.exists(devkit_path), \
-        'Devkit directory {} not found'.format(devkit_path)
-    anno_path = os.path.join(
-        devkit_path, 'VOC' + year, 'Annotations', '{:s}.xml')
-    image_set_path = os.path.join(
-        devkit_path, 'VOC' + year, 'ImageSets', 'Main', image_set + '.txt')
-    return dict(
-        year=year,
-        image_set=image_set,
-        devkit_path=devkit_path,
-        anno_path=anno_path,
-        image_set_path=image_set_path)
-
-    
-    
-def get_retinanet_bias_init(model):
-    '''Initialize the biases for the conv ops that predict class probabilities.
-    Initialization is performed such that at the start of training, all
-    locations are predicted to be background with high probability
-    (e.g., ~0.99 = 1 - cfg.RETINANET.PRIOR_PROB). See the Focal Loss paper for
-    details.
-    '''
-    prior_prob = cfg.RETINANET.PRIOR_PROB
-    scales_per_octave = cfg.RETINANET.SCALES_PER_OCTAVE
-    aspect_ratios = len(cfg.RETINANET.ASPECT_RATIOS)
-    if cfg.RETINANET.SOFTMAX:
-        # Multiclass softmax case
-        bias = np.zeros((model.num_classes, 1), dtype=np.float32)
-        bias[0] = np.log(
-            (model.num_classes - 1) * (1 - prior_prob) / (prior_prob)
-        )
-        bias = np.vstack(
-            [bias for _ in range(scales_per_octave * aspect_ratios)]
-        )
-        bias_init = (
-            'GivenTensorFill', {
-                'values': bias.astype(dtype=np.float32)
-            }
-        )
+        if os.path.isdir(image_to_check):
+        if cpus == 1:
+            [test_image(image_file, known_names, known_face_encodings, tolerance, show_distance) for image_file in image_files_in_folder(image_to_check)]
+        else:
+            process_images_in_process_pool(image_files_in_folder(image_to_check), known_names, known_face_encodings, cpus, tolerance, show_distance)
     else:
-        # Per-class sigmoid (binary classification) case
-        bias_init = (
-            'ConstantFill', {
-                'value': -np.log((1 - prior_prob) / prior_prob)
-            }
-        )
-    return bias_init
+        test_image(image_to_check, known_names, known_face_encodings, tolerance, show_distance)
     
-        def forward(self, inputs, outputs):
-        '''See modeling.detector.GenerateProposalLabels for inputs/outputs
-        documentation.
-        '''
-        # During training we reuse the data loader code. We populate roidb
-        # entries on the fly using the rois generated by RPN.
-        # im_info: [[im_height, im_width, im_scale], ...]
-        rois = inputs[0].data
-        roidb = blob_utils.deserialize(inputs[1].data)
-        im_info = inputs[2].data
-        im_scales = im_info[:, 2]
-        output_blob_names = fast_rcnn_roi_data.get_fast_rcnn_blob_names()
-        # For historical consistency with the original Faster R-CNN
-        # implementation we are *not* filtering crowd proposals.
-        # This choice should be investigated in the future (it likely does
-        # not matter).
-        json_dataset.add_proposals(roidb, rois, im_scales, crowd_thresh=0)
-        roidb_utils.add_bbox_regression_targets(roidb)
-        blobs = {k: [] for k in output_blob_names}
-        fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
-        for i, k in enumerate(output_blob_names):
-            blob_utils.py_op_copy_blob(blobs[k], outputs[i])
-
+        def test_fd_command_line_interface_cnn_model(self):
+        target_string = 'obama.jpg'
+        runner = CliRunner()
+        image_file = os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg')
     
-    
-if __name__ == '__main__':
-    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
-    logger = setup_logging(__name__)
-    logger.setLevel(logging.DEBUG)
-    logging.getLogger('detectron.roi_data.loader').setLevel(logging.INFO)
-    np.random.seed(cfg.RNG_SEED)
-    args = parse_args()
-    logger.info('Called with args:')
-    logger.info(args)
-    if args.cfg_file is not None:
-        merge_cfg_from_file(args.cfg_file)
-    if args.opts is not None:
-        merge_cfg_from_list(args.opts)
-    assert_and_infer_cfg()
-    logger.info('Running with config:')
-    logger.info(pprint.pformat(cfg))
-    main(args)
-
-    
-    
-def testUnsignedInt():
-    check(b'\x99\xcc\x00\xcc\x80\xcc\xff\xcd\x00\x00\xcd\x80\x00'
-          b'\xcd\xff\xff\xce\x00\x00\x00\x00\xce\x80\x00\x00\x00'
-          b'\xce\xff\xff\xff\xff',
-          (0,
-           128,
-           255,
-           0,
-           32768,
-           65535,
-           0,
-           2147483648,
-           4294967295, ), )
-    
-    import platform
-import os
-import subprocess
-from .clipboards import (init_osx_clipboard,
-                         init_gtk_clipboard, init_qt_clipboard,
-                         init_xclip_clipboard, init_xsel_clipboard,
-                         init_klipper_clipboard, init_no_clipboard)
-from .windows import init_windows_clipboard
-    
-    
-def pack(o, stream, **kwargs):
-    '''
-    Pack object `o` and write it to `stream`
-    
-            exp4 = ('0   1 days\n'
-                '1   2 days\n'
-                '2   3 days\n'
-                'dtype: timedelta64[ns]')
+    # Load the jpg file into a numpy array
+image = face_recognition.load_image_file('two_people.jpg')
