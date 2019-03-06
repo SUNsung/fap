c3a4ff66cdce0b8037974ade6b904d4697bdd308
SUNsung <weatherius@gmail.com>
2019-03-06 12:52:32 +0200

    Rewrite [22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb] File in: 2019-03-06 12:52:32

--- ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
+++ ruby/22rkRZf38MArE22Trx1WIKFiAo22kuPgaBBrPkk22QKK.rb
@@ -1,167 +1,66 @@
 
         
-            if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+          describe 'converting escaped JSONPath strings' do
+    it 'should work' do
+      expect(LiquidMigrator.convert_string('Weather looks like <$.conditions> according to the forecast at <$.pretty_date.time>')).to eq(
+                                    'Weather looks like {{conditions}} according to the forecast at {{pretty_date.time}}'
+      )
+    end
+    
+        it 'should generate the correct last checkpoint url' do
+      @checker.options['path'] = 'last_checkpoint/usps/9361289878905919630610'
+      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/last_checkpoint/usps/9361289878905919630610')
     end
   end
     
-      ActiveSupport.run_load_hooks(:devise_controller, self)
+        it 'should raise error when response has an error' do
+      stub(HTTParty).post { {'error' => {'message' => 'Sample error'}} }
+      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Sample error/)
+    end
+  end
 end
 
     
-        def default_failure_app(options)
-      @failure_app = options[:failure_app] || Devise::FailureApp
-      if @failure_app.is_a?(String)
-        ref = Devise.ref(@failure_app)
-        @failure_app = lambda { |env| ref.get.call(env) }
-      end
-    end
+        private
     
-      # Returns parsed JavaScript blocks.
-  # The parsed version is a RKelly object that allows you to be able do advanced parsing.
-  #
-  # @see https://github.com/tenderlove/rkelly
-  # @return [Array<RKelly::Nodes::SourceElementsNode>]
-  def get_html_scripts
-    n = get_html_document
-    rkelly = RKelly::Parser.new
-    n.search('//script').map { |s| rkelly.parse(s.text) }
+      respond_to :json
+    
+      def rate_limit_limit
+    api_throttle_data[:limit].to_s
   end
     
-              sent = 0
-          case protocol
-          when 'tcp'
-            sent = send_request_tcp(req)
-          when 'udp'
-            sent = send_request_udp(req)
-          else
-            raise ::RuntimeError, 'Kerberos Client: unknown transport protocol'
-          end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Crypto
-        module Rc4Hmac
-          # Decrypts the cipher using RC4-HMAC schema
-          #
-          # @param cipher [String] the data to decrypt
-          # @param key [String] the key to decrypt
-          # @param msg_type [Integer] the message type
-          # @return [String] the decrypted cipher
-          # @raise [RuntimeError] if decryption doesn't succeed
-          def decrypt_rc4_hmac(cipher, key, msg_type)
-            unless cipher && cipher.length > 16
-              raise ::RuntimeError, 'RC4-HMAC decryption failed'
-            end
-    
-              # Encodes the Rex::Proto::Kerberos::Model::Element into an ASN.1 String. This
-          # method has been designed to be overridden by subclasses.
-          #
-          # @raise [NoMethodError]
-          def encode
-            raise ::NoMethodError, 'Method designed to be overridden'
-          end
-        end
-      end
-    end
-  end
+    desc 'Generate jekyll site'
+task :generate do
+  raise '### You haven't set anything up yet. First run `rake install` to set up an Octopress theme.' unless File.directory?(source_dir)
+  puts '## Generating Site with Jekyll'
+  system 'compass compile --css-dir #{source_dir}/stylesheets'
+  system 'jekyll build'
 end
     
-              # Decodes the crealm field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_crealm(input)
-            input.value[0].value
-          end
-    
-              # Decodes the ctime field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_ctime(input)
-            input.value[0].value
-          end
-    
-              # Rex::Proto::Kerberos::Model::LastRequest encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'LastRequest encoding not supported'
-          end
-    
-    # skip over blogs that aren't found
-unavailable = []
-fast_forwards = [
-  'Baidu Research',
-  'Booking.com',
-  'Fynd',
-  'Graphcool',
-  'LinkedIn',
-  'Medallia',
-  'OmniTI',
-  'Paperless Post',
-  'Pluralsight',
-  'Prolific Interactive',
-  'Quora',
-  'Robert Elder Software',
-  'Simple',
-  'SlideShare',
-  'SourceClear',
-  'Viget',
-  'Zalando',
-  'Zapier',
-  'Zynga',
-  'Dave Beazley',
-  'Edan Kwan',
-  'Grzegorz Gajos',
-  'Joe Armstrong',
-  'Kai Hendry',
-  'LiveOverflow'
-]
-    
-      find_files = ->(path) {
-    Find.find(Pathname.new(path).relative_path_from(Pathname.new Dir.pwd).to_s).map do |path|
-      path if File.file?(path)
-    end.compact
-  }
-    
-        def clear
-      @attachments = Hash.new { |h,k| h[k] = {} }
-    end
-    
-        def cropping dst, ratio, scale
-      if ratio.horizontal? || ratio.square?
-        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
-      else
-        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
+      if options.respond_to? 'keys'
+    options.each do |k,v|
+      unless v.nil?
+        v = v.join ',' if v.respond_to? 'join'
+        v = v.to_json if v.respond_to? 'keys'
+        output += ' data-#{k.sub'_','-'}='#{v}''
       end
     end
+  elsif options.respond_to? 'join'
+    output += ' data-value='#{config[key].join(',')}''
+  else
+    output += ' data-value='#{config[key]}''
+  end
+  output += '></#{tag}>'
+end
     
-        def geometry_string
-      begin
-        orientation = Paperclip.options[:use_exif_orientation] ?
-          '%[exif:orientation]' : '1'
-        Paperclip.run(
-          Paperclip.options[:is_windows] ? 'magick identify' : 'identify',
-          '-format '%wx%h,#{orientation}' :file', {
-            :file => '#{path}[0]'
-          }, {
-            :swallow_stderr => true
-          }
-        )
-      rescue Terrapin::ExitStatusError
-        ''
-      rescue Terrapin::CommandNotFoundError => e
-        raise_because_imagemagick_missing
-      end
-    end
+          rtn = ''
+      (context.environments.first['site'][@array_name] || []).each do |file|
+        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
+          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
+        end
     
-        # Returns the Rails.root constant.
-    def rails_root attachment, style_name
-      Rails.root
-    end
+      class VideoTag < Liquid::Tag
+    @video = nil
+    @poster = ''
+    @height = ''
+    @width = ''
