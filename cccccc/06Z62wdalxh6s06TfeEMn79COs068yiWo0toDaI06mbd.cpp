faefe9892786451beb75b6630d3225fedc6df842
SUNsung <weatherius@gmail.com>
2019-03-06 22:31:47 +0200

    Rewrite [06Z62wdalxh6s06TfeEMn79COs068yiWo0toDaI06mbd.cpp] File in: 2019-03-06 22:31:47

--- cccccc/06Z62wdalxh6s06TfeEMn79COs068yiWo0toDaI06mbd.cpp
+++ cccccc/06Z62wdalxh6s06TfeEMn79COs068yiWo0toDaI06mbd.cpp
@@ -1,279 +1,107 @@
 
         
-            /** Specify model role to use as ordinal value (defaults to Qt::UserRole) */
-    void setRole(int role);
+        		m_boolBorderPixels = m_pblockParent->HasBorderPixels();
     
-        while (outlen > 0) {
-        secp256k1_hmac_sha256_t hmac;
-        int now = outlen;
-        secp256k1_hmac_sha256_initialize(&hmac, rng->k, 32);
-        secp256k1_hmac_sha256_write(&hmac, rng->v, 32);
-        secp256k1_hmac_sha256_finalize(&hmac, rng->v);
-        if (now > 32) {
-            now = 32;
-        }
-        memcpy(out, rng->v, now);
-        out += now;
-        outlen -= now;
-    }
     
-        secp256k1_context_set_error_callback(tctx, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_illegal_callback(tctx, counting_illegal_callback_fn, &ecount);
-    CHECK(secp256k1_ec_pubkey_create(tctx, &point, s_one) == 1);
-    
-    #include <boost/test/unit_test.hpp>
-    
-    #include <iosfwd>
-#include <vector>
-#include 'gtest/internal/gtest-internal.h'
-#include 'gtest/internal/gtest-string.h'
-    
-      // Returns a copy of the FilePath with the case-insensitive extension removed.
-  // Example: FilePath('dir/file.exe').RemoveExtension('EXE') returns
-  // FilePath('dir/file'). If a case-insensitive extension is not
-  // found, returns a copy of the original FilePath.
-  FilePath RemoveExtension(const char* extension) const;
-    
-    // Constructs a failure message for Boolean assertions such as EXPECT_TRUE.
-GTEST_API_ std::string GetBoolAssertionFailureMessage(
-    const AssertionResult& assertion_result,
-    const char* expression_text,
-    const char* actual_predicate_value,
-    const char* expected_predicate_value);
-    
-    // Gets the content of the stringstream's buffer as an std::string.  Each '\0'
-// character in the buffer is replaced with '\\0'.
-GTEST_API_ std::string StringStreamToString(::std::stringstream* stream);
-    
-            // Return true if the update should use classic momentum and 
-        // false if the unit-gain momentum should be used instead.
-        bool UseUnitGainMomentum() const
-        {
-            return m_unitGain;
-        }
-    
-    namespace CNTK
+/** 16x32 multiplication, followed by a 15-bit shift right. Results fits in 32 bits */
+#undef MULT16_32_Q15
+static OPUS_INLINE opus_val32 MULT16_32_Q15_armv4(opus_val16 a, opus_val32 b)
 {
-    static Matrix<char>* AllocateMatrix(const NDShape& viewShape, const DeviceDescriptor& device)
-    {
-        auto matrixDims = GetMatrixDimensions(viewShape);
-        return new Matrix<char>(matrixDims.first, matrixDims.second, AsCNTKImplDeviceId(device));
-    }
-    }
-    
-    
-    {        const auto& type = dict[typeKey].Value<std::wstring>();
-        if (type != typeValue) 
-        {
-            const auto& version = GetVersion(dict);
-            LogicError('Unexpected '%ls':'%ls' in place of '%ls':'%ls' (%s).',
-                       typeKey.c_str(), type.c_str(), typeKey.c_str(), typeValue.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
-        }
-    }
-    
-        // release temp matrices that are only used by forward computation
-    // don't release matrices that need to be used in the gradient computation
-    virtual void ReleaseMatricesAfterForwardProp(MatrixPool& matrixPool)
-    {
-        Base::ReleaseMatricesAfterForwardProp(matrixPool);
-        ReleaseMatrixToPool(m_maxIndexes0, matrixPool);
-        ReleaseMatrixToPool(m_maxIndexes1, matrixPool);
-        ReleaseMatrixToPool(m_maxValues, matrixPool);
-    }
-    
-    Status KafkaTopicsConfigParserPlugin::update(const std::string& source,
-                                             const ParserConfig& config) {
-  auto topics = config.find(kKafkaTopicParserRootKey);
-  if (topics != config.end()) {
-    auto obj = data_.getObject();
-    data_.copyFrom(topics->second.doc(), obj);
-    data_.add(kKafkaTopicParserRootKey, obj);
-  }
-  return Status();
+  unsigned rd_lo;
+  int rd_hi;
+  __asm__(
+      '#MULT16_32_Q15\n\t'
+      'smull %0, %1, %2, %3\n\t'
+      : '=&r'(rd_lo), '=&r'(rd_hi)
+      : '%r'(b), 'r'(a<<16)
+  );
+  /*We intentionally don't OR in the high bit of rd_lo for speed.*/
+  return rd_hi<<1;
 }
+#define MULT16_32_Q15(a, b) (MULT16_32_Q15_armv4(a, b))
     
-      auto cv = config.find(kLoggerKey);
-  if (cv != config.end()) {
-    auto obj = data_.getObject();
-    data_.copyFrom(cv->second.doc(), obj);
-    data_.add(kLoggerKey, obj);
-  }
+    /** 16x32 multiplication, followed by a 16-bit shift right and 32-bit add.
+    Results fits in 32 bits */
+#define MAC16_32_Q16(c,a,b) ADD32((c),ADD32(MULT16_16((a),SHR((b),16)), SHR(MULT16_16SU((a),((b)&0x0000ffff)),16)))
     
-    #include <gtest/gtest.h>
-    
-    
-    {  int status = ::kill(nativeHandle(), SIGTERM);
-  return (status == 0);
-}
-    
-    
-    {  // Finally wait for a signal / interrupt to shutdown.
-  runner.waitForShutdown();
-  return 0;
-}
+    /*The number of bits to output at a time.*/
+# define EC_SYM_BITS   (8)
+/*The total number of bits in each of the state registers.*/
+# define EC_CODE_BITS  (32)
+/*The maximum symbol value.*/
+# define EC_SYM_MAX    ((1U<<EC_SYM_BITS)-1)
+/*Bits to shift by to move a symbol into the high-order position.*/
+# define EC_CODE_SHIFT (EC_CODE_BITS-EC_SYM_BITS-1)
+/*Carry bit of the high-order range symbol.*/
+# define EC_CODE_TOP   (((opus_uint32)1U)<<(EC_CODE_BITS-1))
+/*Low-order bit of the high-order range symbol.*/
+# define EC_CODE_BOT   (EC_CODE_TOP>>EC_SYM_BITS)
+/*The number of bits available for the last, partial symbol in the code field.*/
+# define EC_CODE_EXTRA ((EC_CODE_BITS-2)%EC_SYM_BITS+1)
+#endif
 
     
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-    
-    
-    {    return nullptr;
-}
-    
-    ActionInstant* Show::reverse() const
+    int main(int, char**)
 {
-    return Hide::create();
-}
-    
-        //
-    // Override
-    //
-    virtual RotateBy* clone() const override;
-    virtual RotateBy* reverse() const override;
-    virtual void startWithTarget(Node *target) override;
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    RotateBy();
-    virtual ~RotateBy() {}
-    
-                    if (_currentTarget->currentActionSalvaged)
-                {
-                    // The currentAction told the node to remove it. To prevent the action from
-                    // accidentally deallocating itself before finishing its step, we retained
-                    // it. Now that step is done, it's safe to release it.
-                    _currentTarget->currentAction->release();
-                } else
-                if (_currentTarget->currentAction->isDone())
-                {
-                    _currentTarget->currentAction->stop();
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
     }
     
+    // Called by Init/NewFrame/Shutdown
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateFontsTexture();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyFontsTexture();
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyDeviceObjects();
+
     
-    {
-    {
-    {            p.z = (r * ( 1 - cosBeta ) * cosTheta);// '100' didn't work for
-            p.x = p.z * sinf(rotateByYAxis) + p.x * cosf(rotateByYAxis);
-            p.z = p.z * cosf(rotateByYAxis) - p.x * sinf(rotateByYAxis);
-            p.z/=7;
-            //    Stop z coord from dropping beneath underlying page in a transition
-            // issue #751
-            if( p.z < 0.5f )
-            {
-                p.z = 0.5f;
-            }
-            
-            // Set new coords
-            p.x += getGridRect().origin.x;
-            setVertex(Vec2(i, j), p);
-            
-        }
-    }
+                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+    
+    
+    {    return true;
 }
     
-    NS_CC_END
-    
-    AtlasNode * AtlasNode::create(const std::string& tile, int tileWidth, int tileHeight, int itemsToRender)
+    // Render function.
+// (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
+void ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data)
 {
-    AtlasNode * ret = new (std::nothrow) AtlasNode();
-    if (ret->initWithTileFile(tile, tileWidth, tileHeight, itemsToRender))
-    {
-        ret->autorelease();
-        return ret;
+    // Avoid rendering when minimized
+    if (draw_data->DisplaySize.x <= 0.0f || draw_data->DisplaySize.y <= 0.0f)
+        return;
     }
-    CC_SAFE_DELETE(ret);
-    return nullptr;
+    
+    using ::apollo::canbus::ChassisDetail;
+    
+    TEST(ByteTest, ByteToString) {
+  unsigned char value = 0x34;
+  EXPECT_EQ('34', Byte::byte_to_hex(value));
+  EXPECT_EQ('00110100', Byte::byte_to_binary(value));
+  uint32_t int_value = 0xE13A;
+  EXPECT_EQ('E13A', Byte::byte_to_hex(int_value));
 }
     
-    BENCHMARK_RELATIVE(format_short_string_unsafe, iters) {
-  BenchmarkSuspender suspender;
-  auto const& shortString = getShortString();
-  while (iters--) {
-    fbstring out;
-    suspender.dismissing([&] { format(&out, shortString); });
+    TEST(TestPiecewiseLinearConstraint, add_second_derivative_boundary) {
+  PiecewiseLinearConstraint constraint(10, 0.1);
+  std::vector<uint32_t> index_list;
+  std::vector<double> lower_bound;
+  std::vector<double> upper_bound;
+  for (uint32_t i = 0; i < 10; ++i) {
+    index_list.push_back(i);
+    lower_bound.push_back(-4.0);
+    upper_bound.push_back(2.0);
   }
-}
-    
-    namespace {
-std::exception const* get_std_exception_(std::exception_ptr eptr) noexcept {
-  try {
-    std::rethrow_exception(eptr);
-  } catch (const std::exception& ex) {
-    return &ex;
-  } catch (...) {
-    return nullptr;
-  }
-}
-} // namespace
-    
-      /**
-   * Returns a random uint64_t in [min, max). If min == max, returns 0.
-   */
-  template <class RNG = ThreadLocalPRNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static uint64_t rand64(uint64_t min, uint64_t max, RNG&& rng) {
-    if (min == max) {
-      return 0;
     }
-    return std::uniform_int_distribution<uint64_t>(min, max - 1)(rng);
-  }
     
-        // Test the lower boundary of conversion to uint64_t nanoseconds
-    ts.tv_sec = 0;
-    ts.tv_nsec = 0;
-    EXPECT_EQ(0, to<nsec_u64>(ts).count());
-    ts.tv_sec = -1;
-    ts.tv_nsec = 0;
-    EXPECT_THROW(to<nsec_u64>(ts), std::range_error);
+      x <<= 16;
+  x >>= 16;
     
-    template <typename T>
-typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type
-prefixToStringLE(T prefix, uint64_t n = sizeof(T)) {
-  DCHECK_GT(n, 0);
-  DCHECK_LE(n, sizeof(T));
-  prefix = Endian::little(prefix);
-  std::string result;
-  result.resize(n);
-  memcpy(&result[0], &prefix, n);
-  return result;
-}
-    
-      /**
-   * 'The memLevel parameter specifies how much memory should be allocated for
-   * the internal compression state. memLevel=1 uses minimum memory but is slow
-   * and reduces compression ratio; memLevel=9 uses maximum memory for optimal
-   * speed. The default value is 8.'
-   */
-  int memLevel;
-    
-      bool is_lock_free() const noexcept {
-    // lock free unless more than EXTERNAL_OFFSET threads are
-    // contending and they all get unlucky and scheduled out during
-    // load().
-    //
-    // TODO: Could use a lock-free external map to fix this
-    // corner case.
-    return true;
-  }
-    
-    TEST_F(SparseByteSetTest, each_random) {
-  mt19937 rng;
-  uniform_int_distribution<uint16_t> dist{lims::min(), lims::max()};
-  set<uint8_t> added;
-  while (added.size() <= lims::max()) {
-    auto c = uint8_t(dist(rng));
-    EXPECT_EQ(added.count(c), s.contains(c));
-    EXPECT_EQ(!added.count(c), s.add(c));
-    added.insert(c);
-    EXPECT_TRUE(added.count(c)); // sanity
-    EXPECT_TRUE(s.contains(c));
-  }
-}
+    // config detail: {'name': 'torque_input', 'offset': 0.0, 'precision': 0.001,
+// 'len': 32, 'is_signed_var': True, 'physical_range':
+// '[-2147483.648|2147483.647]', 'bit': 39, 'type': 'double', 'order':
+// 'motorola', 'physical_unit': 'N-m'}
+double Brakemotorrpt372::torque_input(const std::uint8_t* bytes,
+                                      int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(0, 8);
+    }
