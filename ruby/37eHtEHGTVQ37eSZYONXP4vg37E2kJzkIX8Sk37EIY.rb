3982a52586009969400ce112004ac9ba45976022
SUNsung <weatherius@gmail.com>
2019-03-06 22:08:17 +0200

    Rewrite [37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb] File in: 2019-03-06 22:08:16

--- ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
+++ ruby/37eHtEHGTVQ37eSZYONXP4vg37E2kJzkIX8Sk37EIY.rb
@@ -1,221 +1,158 @@
 
         
-                def render
-          options = @options.stringify_keys
-          options['type']     = 'checkbox'
-          options['value']    = @checked_value
-          options['checked'] = 'checked' if input_checked?(options)
+              def request_count_counter
+        @request_counter ||= Gitlab::Metrics.counter(
+          :github_importer_request_count,
+          'The number of GitHub API calls performed when importing projects'
+        )
+      end
+    end
+  end
+end
+
     
-              content_is_options = content_or_options.is_a?(Hash)
-          if content_is_options
-            options.merge! content_or_options
-            @content = nil
-          else
-            @content = content_or_options
+            def importer_class
+          IssueAndLabelLinksImporter
+        end
+    
+          def cache_key_iid
+        if object.respond_to?(:noteable_id)
+          object.noteable_id
+        elsif object.respond_to?(:iid)
+          object.iid
+        else
+          raise(
+            TypeError,
+            'Instances of #{object.class} are not supported'
+          )
+        end
+      end
+    end
+  end
+end
+
+    
+        if authenticated && resource = warden.user(resource_name)
+      flash[:alert] = I18n.t('devise.failure.already_authenticated')
+      redirect_to after_sign_in_path_for(resource)
+    end
+  end
+    
+    Rails.application.routes.draw do
+  devise_for :users
+    
+                bypass_sign_in(user)
+          DEPRECATION
+          warden.session_serializer.store(resource, scope)
+        elsif warden.user(scope) == resource && !options.delete(:force)
+          # Do nothing. User already signed in and we are not forcing it.
+          true
+        else
+          warden.set_user(resource, options.merge!(scope: scope))
+        end
+      end
+    
+          def parse_uri(location)
+        location && URI.parse(location)
+      rescue URI::InvalidURIError
+        nil
+      end
+    
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+      #
+  # Move these into an IPMI stack or mixin at some point
+  #
+    
+        head + [data.length].pack('v') + data
+  end
+    
+            end
+      end
+    end
+  end
+end
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a principal, an asset (e.g., a
+        # workstation user or a network server) on a network.
+        class Element
+    
+              # Decodes a Rex::Proto::Kerberos::Model::EncKdcResponse
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode_asn1(input)
+            input.value[0].value.each do |val|
+              case val.tag
+              when 0
+                self.key = decode_key(val)
+              when 1
+                self.last_req = decode_last_req(val)
+              when 2
+                self.nonce = decode_nonce(val)
+              when 3
+                self.key_expiration = decode_key_expiration(val)
+              when 4
+                self.flags = decode_flags(val)
+              when 5
+                self.auth_time = decode_auth_time(val)
+              when 6
+                self.start_time = decode_start_time(val)
+              when 7
+                self.end_time = decode_end_time(val)
+              when 8
+                self.renew_till = decode_renew_till(val)
+              when 9
+                self.srealm = decode_srealm(val)
+              when 10
+                self.sname = decode_sname(val)
+              else
+                raise ::RuntimeError, 'Failed to decode ENC-KDC-RESPONSE SEQUENCE'
+              end
+            end
           end
     
-            def render
-          options = @options.stringify_keys
-          options['size'] = options['maxlength'] unless options.key?('size')
-          options['type'] ||= field_type
-          options['value'] = options.fetch('value') { value_before_type_cast } unless field_type == 'file'
-          add_default_name_and_id(options)
-          tag('input', options)
-        end
+              # Decodes a Rex::Proto::Kerberos::Model::EncryptionKey
+          #
+          # @param input [String, OpenSSL::ASN1::Sequence] the input to decode from
+          # @return [self] if decoding succeeds
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode(input)
+            case input
+            when String
+              decode_string(input)
+            when OpenSSL::ASN1::Sequence
+              decode_asn1(input)
+            else
+              raise ::RuntimeError, 'Failed to decode EncryptionKey, invalid input'
+            end
     
-    module ActionView #:nodoc:
-  # = Action View PathSet
-  #
-  # This class is used to store and access paths in Action View. A number of
-  # operations are defined so that you can search among the paths in this
-  # set and also perform operations on other +PathSet+ objects.
-  #
-  # A +LookupContext+ will use a +PathSet+ to store the paths in its context.
-  class PathSet #:nodoc:
-    include Enumerable
-    
-      it 'imports a scenario that does not exist yet' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This scenario has a few agents to get you started. Feel free to change them or delete them as you see fit!')
-    expect(page).not_to have_text('This Scenario already exists in your system.')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
-    
-        it 'can be turned off' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
-      DefaultScenarioImporter.import(user)
-    end
-    
-        it 'strips punctuation' do
-      expect(AgentsExporter.new(:name => 'foo,bar').filename).to eq('foo-bar.json')
-    end
-    
-      describe 'down' do
-    let :valid_options do
-      super().merge('extract' => new_extract,
-                    'template' => new_template)
-    end
-    
-      describe 'path request must exist' do
-    it 'should check that validation added if path does not exist' do
-      opts = @opts.tap { |o| o.delete('path') }
-      @checker = Agents::AftershipAgent.new(:name => 'tectonic', :options => opts)
-      @checker.user = users(:bob)
-      expect(@checker.save).to eq false
-      expect(@checker.errors.full_messages.first).to eq('You need to specify a path request')
-    end
-  end
-    
-        def initialize(name = nil, path = nil, type = nil)
-      self.name = name
-      self.path = path
-      self.type = type
-    
-        def initialize
-      @pages = {}
-    end
-    
-        DOCUMENT_RGX = /\A(?:\s|(?:<!--.*?-->))*<(?:\!doctype|html)/i
-    
-        def additional_options
-      {}
-    end
-    
-        class RateLimiter
-      attr_accessor :limit
-    
-        def subpath_from(url, options = nil)
-      self.class.parse(url).subpath_to(self, options)
-    end
-    
-            css('pre[language]').each do |node|
-          node['data-language'] = node['language'].sub(/\Ats/, 'typescript').strip
-          node['data-language'] = 'html' if node.content.start_with?('<')
-          node.remove_attribute('language')
-          node.remove_attribute('format')
-        end
-    
-        if successfully_sent?(resource)
-      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
-    end
-  end
-    
-      def failure
-    set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
-    redirect_to after_omniauth_failure_path_for(resource_name)
-  end
-    
-        if resource.errors.empty?
-      set_flash_message! :notice, :unlocked
-      respond_with_navigational(resource){ redirect_to after_unlock_path_for(resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
-    end
-  end
-    
-            warden.logout
-        expire_data_after_sign_out!
-        warden.clear_strategies_cache!
-        warden.lock! if lock
-    
-        if record.timedout?(last_request_at) &&
-        !env['devise.skip_timeout'] &&
-        !proxy.remember_me_is_active?(record)
-      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
-      throw :warden, scope: scope, message: :timeout
-    end
-    
-          def initialize_from_record(record)
-        @scope_name = Devise::Mapping.find_scope!(record)
-        @resource   = instance_variable_set('@#{devise_mapping.name}', record)
-      end
-    
-        alias :name :singular
-    
-        # Include the chosen devise modules in your model:
-    #
-    #   devise :database_authenticatable, :confirmable, :recoverable
-    #
-    # You can also give any of the devise configuration values in form of a hash,
-    # with specific values for this model. Please check your Devise initializer
-    # for a complete description on those values.
-    #
-    def devise(*modules)
-      options = modules.extract_options!.dup
-    
-      def hub_topic_params
-    @_hub_topic_params ||= Rails.application.routes.recognize_path(hub_topic_uri.path)
-  end
-    
-      def body
-    @_body ||= request.body.read
-  end
-    
-        render json: web_subscription, serializer: REST::WebPushSubscriptionSerializer
-  end
-    
-    # Declares a dependency to the git repo of CocoaPods gem. This declaration is
-# compatible with the local git repos feature of Bundler.
-#
-def cp_gem(name, repo_name, branch = 'master', path: false)
-  return gem name if SKIP_UNRELEASED_VERSIONS
-  opts = if path
-           { :path => '../#{repo_name}' }
-         else
-           url = 'https://github.com/CocoaPods/#{repo_name}.git'
-           { :git => url, :branch => branch }
-         end
-  gem name, opts
+    class Rack::Builder
+  include Sinatra::Delegator
 end
+
     
-        # From asking people, it seems MacPorts does not have a `prefix` command, like
-    # Homebrew does, so make an educated guess:
-    if port_prefix = prefix_from_bin('port')
-      prefixes << port_prefix
-    end
-    
-          def page_dir
-        @page_dir
+          def unmasked_token?(token)
+        token.length == TOKEN_LENGTH
       end
     
-          # Finds header node inside Nokogiri::HTML document.
-      #
-      def find_header_node(doc)
-        case @page.format
-          when :asciidoc
-            doc.css('div#gollum-root > h1:first-child')
-          when :org
-            doc.css('div#gollum-root > p.title:first-child')
-          when :pod
-            doc.css('div#gollum-root > a.dummyTopAnchor:first-child + h1')
-          when :rest
-            doc.css('div#gollum-root > div > div > h1:first-child')
-          else
-            doc.css('div#gollum-root > h1:first-child')
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
+          end
         end
+        bad_cookies.empty?
       end
-    
-      test 'displays_latest_changes' do
-    get('/latest_changes')
-    body = last_response.body
-    
-      setup do
-    @path = cloned_testpath('examples/revert.git')
-    @wiki = Gollum::Wiki.new(@path)
-    Precious::App.set(:gollum_path, @path)
-    Precious::App.set(:wiki_options, {})
-  end
-    
-    def rubyforge_project
-  name
-end
-    
-      s.require_paths = %w[lib]
