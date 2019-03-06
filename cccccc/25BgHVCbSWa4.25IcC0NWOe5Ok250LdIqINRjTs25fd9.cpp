9e945c22e8b402f2abbc83d97c05787f872fad83
SUNsung <weatherius@gmail.com>
2019-03-06 02:50:35 +0200

    Rewrite [25BgHVCbSWa4.25IcC0NWOe5Ok250LdIqINRjTs25fd9.cpp] File in: 2019-03-06 02:50:34

--- cccccc/25BgHVCbSWa4.25IcC0NWOe5Ok250LdIqINRjTs25fd9.cpp
+++ cccccc/25BgHVCbSWa4.25IcC0NWOe5Ok250LdIqINRjTs25fd9.cpp
@@ -1,219 +1,272 @@
 
         
-        #endif // BITCOIN_QT_PLATFORMSTYLE_H
+        REGISTER_OP('Invalid')
+    .Attr('invalid attr: int32')  // invalid since the name has a space.
+    .Doc(R'doc(
+An op to test that invalid ops do not successfully generate invalid python code.
+)doc');
     
-    namespace Ui {
-    class SignVerifyMessageDialog;
-}
+    // Register the bfloat16 numpy type.
+void RegisterNumpyBfloat16();
     
-    #endif // BITCOIN_REVERSELOCK_H
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/platform/mutex.h'
+#include 'tensorflow/python/lib/core/bfloat16.h'
+#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
+    
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/framework/types.pb.h'
+#include 'tensorflow/core/lib/core/status.h'
+    
+    
+    {}  // namespace tensorflow
 
     
-        // A more complex valid grammar. PROTOCOLINFO accepts a VersionLine that
-    // takes a key=value pair followed by an OptArguments, making this valid.
-    // Because an OptArguments contains no semantic data, there is no point in
-    // parsing it.
-    CheckParseTorReplyMapping(
-        'SOME=args,here MORE optional=arguments  here', {
-            {'SOME', 'args,here'},
-        });
+    class RandomAccessFile;
     
-    // Format at most ntrunc characters to the given stream.
-template<typename T>
-inline void formatTruncated(std::ostream& out, const T& value, int ntrunc)
-{
-    std::ostringstream tmp;
-    tmp << value;
-    std::string result = tmp.str();
-    out.write(result.c_str(), (std::min)(ntrunc, static_cast<int>(result.size())));
-}
-#define TINYFORMAT_DEFINE_FORMAT_TRUNCATED_CSTR(type)       \
-inline void formatTruncated(std::ostream& out, type* value, int ntrunc) \
-{                                                           \
-    std::streamsize len = 0;                                \
-    while(len < ntrunc && value[len] != 0)                  \
-        ++len;                                              \
-    out.write(value, len);                                  \
-}
-// Overload for const char* and char*.  Could overload for signed & unsigned
-// char too, but these are technically unneeded for printf compatibility.
-TINYFORMAT_DEFINE_FORMAT_TRUNCATED_CSTR(const char)
-TINYFORMAT_DEFINE_FORMAT_TRUNCATED_CSTR(char)
-#undef TINYFORMAT_DEFINE_FORMAT_TRUNCATED_CSTR
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include 'tensorflow/python/util/kernel_registry.h'
     
-            WriteLE32(c + 0, x0);
-        WriteLE32(c + 4, x1);
-        WriteLE32(c + 8, x2);
-        WriteLE32(c + 12, x3);
-        WriteLE32(c + 16, x4);
-        WriteLE32(c + 20, x5);
-        WriteLE32(c + 24, x6);
-        WriteLE32(c + 28, x7);
-        WriteLE32(c + 32, x8);
-        WriteLE32(c + 36, x9);
-        WriteLE32(c + 40, x10);
-        WriteLE32(c + 44, x11);
-        WriteLE32(c + 48, x12);
-        WriteLE32(c + 52, x13);
-        WriteLE32(c + 56, x14);
-        WriteLE32(c + 60, x15);
-    
-    
-    {    CRIPEMD160();
-    CRIPEMD160& Write(const unsigned char* data, size_t len);
-    void Finalize(unsigned char hash[OUTPUT_SIZE]);
-    CRIPEMD160& Reset();
-};
-    
-    // A CodeGenerator that captures the FileDescriptor it's passed as a
-// FileDescriptorProto.
-class DescriptorCapturingGenerator : public CodeGenerator {
- public:
-  // Does not own file; file must outlive the Generator.
-  explicit DescriptorCapturingGenerator(FileDescriptorProto* file)
-      : file_(file) {}
+    port::StatusOr<StreamExecutor*> ExecutorCache::Get(
+    const StreamExecutorConfig& config) {
+  Entry* entry = nullptr;
+  {
+    tf_shared_lock lock{mutex_};
+    auto it = cache_.find(config.ordinal);
+    if (it != cache_.end()) {
+      entry = &it->second;
+    } else {
+      return port::Status(port::error::NOT_FOUND,
+                          port::Printf('No executors registered for ordinal %d',
+                                       config.ordinal));
     }
-    
-    io::ZeroCopyOutputStream*
-GeneratorContext::OpenForAppend(const string& filename) {
-  return NULL;
-}
-    
-    TEST(MovableMessageTest, MoveConstructor) {
-  protobuf_unittest::TestAllTypes message1;
-  TestUtil::SetAllFields(&message1);
-  const auto* nested = &message1.optional_nested_message();
-    }
-    
-    
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace java
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
-    
-    
-    {  WriteFieldDocComment(printer, descriptor_);
-  if (descriptor_->is_repeated()) {
-    printer->Print(
-        vars,
-        'public static final\n'
-        '  com.google.protobuf.GeneratedMessageLite.GeneratedExtension<\n'
-        '    $containing_type$,\n'
-        '    $type$> $name$ = com.google.protobuf.GeneratedMessageLite\n'
-        '        .newRepeatedGeneratedExtension(\n'
-        '      $containing_type$.getDefaultInstance(),\n'
-        '      $prototype$,\n'
-        '      $enum_map$,\n'
-        '      $number$,\n'
-        '      com.google.protobuf.WireFormat.FieldType.$type_constant$,\n'
-        '      $packed$,\n'
-        '      $singular_type$.class);\n');
-  } else {
-    printer->Print(
-        vars,
-        'public static final\n'
-        '  com.google.protobuf.GeneratedMessageLite.GeneratedExtension<\n'
-        '    $containing_type$,\n'
-        '    $type$> $name$ = com.google.protobuf.GeneratedMessageLite\n'
-        '        .newSingularGeneratedExtension(\n'
-        '      $containing_type$.getDefaultInstance(),\n'
-        '      $default$,\n'
-        '      $prototype$,\n'
-        '      $enum_map$,\n'
-        '      $number$,\n'
-        '      com.google.protobuf.WireFormat.FieldType.$type_constant$,\n'
-        '      $singular_type$.class);\n');
   }
-  printer->Annotate('name', descriptor_);
+  tf_shared_lock lock{entry->configurations_mutex};
+  if (entry->configurations.empty()) {
+    return port::Status(
+        port::error::NOT_FOUND,
+        port::Printf('No executors registered for ordinal %d', config.ordinal));
+  }
+  for (const auto& iter : entry->configurations) {
+    if (iter.first.plugin_config == config.plugin_config &&
+        iter.first.device_options == config.device_options) {
+      VLOG(2) << 'hit in cache for device ordinal ' << config.ordinal;
+      return iter.second.get();
+    }
+  }
+  return port::Status(port::error::NOT_FOUND,
+                      'No executor found with a matching config.');
 }
     
-    SharedCodeGenerator::SharedCodeGenerator(const FileDescriptor* file,
-                                         const Options& options)
-    : name_resolver_(new ClassNameResolver), file_(file), options_(options) {}
+      // Returns a pointer to the described executor (if one with a matching config
+  // has been created), or a NOT_FOUND status.
+  port::StatusOr<StreamExecutor*> Get(const StreamExecutorConfig& config);
     
+    // Tell browser to delete a object.
+// function DeallocateObject(id);
+v8::Handle<v8::Value> DeallocateObject(int routing_id,
+                                       int object_id);
     
-    {  desired_output_for_decode = 'abcdefghIJz';
-  expected = string('\0abcdefghIJz\0', 13);
-  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
-                                                     desired_output_for_decode);
-  EXPECT_EQ(expected, result);
+    #include 'content/nw/src/api/clipboard/clipboard.h'
+    
+    bool NwMenuModel::HasIcons() const {
+  // Always return false, see the comment about |NwMenuModel|.
+  return false;
 }
     
-    TEST_F(DynamicMessageTest, Defaults) {
-  // Check that all default values are set correctly in the initial message.
-  TestUtil::ReflectionTester reflection_tester(descriptor_);
-  reflection_tester.ExpectClearViaReflection(*prototype_);
-}
-    
-    #endif  // HAVE_ZLIB
-
-    
-      // Test that line comments are ignored.
-  { 'foo // This is a comment\n'
-    'bar // This is another comment', {
-    { Tokenizer::TYPE_IDENTIFIER, 'foo', 0,  0,  3 },
-    { Tokenizer::TYPE_IDENTIFIER, 'bar', 1,  0,  3 },
-    { Tokenizer::TYPE_END       , ''   , 1, 30, 30 },
-  }},
-    
-    //--------------------------------------------------------------------------------
-//
-//    appendReplacement
-//
-//--------------------------------------------------------------------------------
-RegexMatcher &RegexMatcher::appendReplacement(UnicodeString &dest,
-                                              const UnicodeString &replacement,
-                                              UErrorCode &status) {
-    UText replacementText = UTEXT_INITIALIZER;
+    ui::KeyboardCode GetKeycodeFromText(std::string text){
+  ui::KeyboardCode retval = ui::VKEY_UNKNOWN;
+  if (text.size() != 0){
+    std::string upperText = base::ToUpperASCII(text);
+    std::string keyName = text;
+    bool found = false;
+    if (upperText.size() == 1){
+      char key = upperText[0];
+      if (key>='0' && key<='9'){//handle digital
+        keyName = 'Digit' + upperText;
+        found = true;
+      } else if (key>='A'&&key<='Z'){//handle alphabet
+        keyName = 'Key' + upperText;
+        found = true;
+      }
+    }
+    }
     }
     
-    U_CAPI UBool U_EXPORT2
-uhash_compareScriptSet(const UElement key1, const UElement key2);
+      if (type == 'separator') {
+    menu_item_ = gtk_separator_menu_item_new();
+  } else {
+    if (type == 'checkbox') {
+      menu_item_ = gtk_check_menu_item_new();
+      bool checked;
+      if (option.GetBoolean('checked', &checked))
+        SetChecked(checked);
+    } else {
+      menu_item_ = gtk_image_menu_item_new();
+      std::string icon;
+      if (option.GetString('icon', &icon))
+        SetIcon(icon);
+    }
+    }
     
-    UOBJECT_DEFINE_RTTI_IMPLEMENTATION(SelectFormat)
+      static void DoJob(AppWindowRegistry* registry, std::string id);
+ protected:
+  ~NwAppCloseAllWindowsFunction() override {}
     
-    #endif
-
+    bool NwClipboardGetListSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  std::unique_ptr<GetListSync::Params> params(GetListSync::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
+  std::unique_ptr<ClipboardReader> reader(new ClipboardReader());
+    }
     
-    class U_I18N_API SharedPluralRules : public SharedObject {
-public:
-    SharedPluralRules(PluralRules *prToAdopt) : ptr(prToAdopt) { }
-    virtual ~SharedPluralRules();
-    const PluralRules *operator->() const { return ptr; }
-    const PluralRules &operator*() const { return *ptr; }
-private:
-    PluralRules *ptr;
-    SharedPluralRules(const SharedPluralRules &);
-    SharedPluralRules &operator=(const SharedPluralRules &);
+     protected:
+  ~NwClipboardGetListSyncFunction() override;
+    
+    class NwObjCreateFunction : public NWSyncExtensionFunction {
+ public:
+  NwObjCreateFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+        webrtc::DesktopCaptureOptions options = webrtc::DesktopCaptureOptions::CreateDefault();
+    options.set_disable_effects(false);
+    
+    
+    {
+    {        throw invalid_argument('the input has no solution');
+    }
+};
+    
+                int complement = target - nums[i];
+            if(record.find(complement) != record.end()){
+                int res[] = {i, record[complement]};
+                return vector<int>(res, res + 2);
+            }
+    
+    
+    {
+    {        return true;
+    }
+};
+    
+        cout << endl;
+    
+            ListNode* dummyHead1 = new ListNode(-1);
+        ListNode* dummyHead2 = new ListNode(-1);
+        ListNode* prev1 = dummyHead1;
+        ListNode* prev2 = dummyHead2;
+    
+    
+    {
+    {
+    {            if(command.s == 'print')
+                res.push_back(command.node->val);
+            else{
+                assert(command.s == 'go');
+                if(command.node->right)
+                    stack.push(Command('go',command.node->right));
+                stack.push(Command('print', command.node));
+                if(command.node->left)
+                    stack.push(Command('go',command.node->left));
+            }
+        }
+        return res;
+    }
 };
     
     
-    {private:
-    int32_t fMax;
-    int32_t fMin;
+    {
+    {        return res;
+    }
 };
     
-    #endif  // __SMALLINTFORMATTER_H__
+    
+    {    return 0;
+}
 
     
-    // Bitwise comparison for the collation keys.
-UCollationResult
-CollationKey::compareTo(const CollationKey& target, UErrorCode &status) const
+            vector<int> res;
+        if(root == NULL)
+            return res;
+    
+            stack<TreeNode*> stack, output;
+    
+    CatmullRomTo* CatmullRomTo::clone() const
 {
-  if(U_SUCCESS(status)) {
-    const uint8_t *src = getBytes();
-    const uint8_t *tgt = target.getBytes();
-    }
+    // no copy constructor
+    auto a = new (std::nothrow) CatmullRomTo();
+    a->initWithDuration(this->_duration, this->_points->clone());
+    a->autorelease();
+    return a;
+}
+    
+    GridBase* Grid3DAction::getGrid()
+{
+    return Grid3D::create(_gridSize, _gridNodeTarget->getGridRect());
+}
+    
+        // On WP8 for variable argument lists longer than 10 items, use the other create functions or variadicCreate with NULL as the last argument
+    static Sequence* variadicCreate(FiniteTimeAction* item, ...);
+#else
+    static Sequence* create(FiniteTimeAction *action1, ...) CC_REQUIRES_NULL_TERMINATION;
+#endif
+    
+    ActionTween* ActionTween::create(float duration, const std::string& key, float from, float to)
+{
+    ActionTween* ret = new (std::nothrow) ActionTween();
+    if (ret && ret->initWithDuration(duration, key, from, to))
+    {
+        ret->autorelease();
+        return ret;
     }
     
-    #endif  // !UCONFIG_NO_FORMATTING
+    delete ret;
+    return nullptr;
+}
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __CCACTIONTWEEN_H__
+#define __CCACTIONTWEEN_H__
+    
+    NS_CC_END
 
     
-    namespace mars_boost {} namespace boost = mars_boost; namespace mars_boost {
-    }
+        /** Gets the total Delay units of the Animation. 
+     *
+     * @return The total Delay units of the Animation.
+     */
+    float getTotalDelayUnits() const { return _totalDelayUnits; };
+    
+    /** Sets the delay in seconds of the 'delay unit'.
+     *
+     * @param delayPerUnit The delay in seconds of the 'delay unit'.
+     */
+    void setDelayPerUnit(float delayPerUnit) { _delayPerUnit = delayPerUnit; };
+    
+    /** Gets the delay in seconds of the 'delay unit'.
+     * 
+     * @return The delay in seconds of the 'delay unit'.
+     */
+    float getDelayPerUnit() const { return _delayPerUnit; };
+    
+    NS_CC_END
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
