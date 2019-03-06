304fc9141dce877506de846529376c62da99a092
SUNsung <weatherius@gmail.com>
2019-03-06 07:05:33 +0200

    Rewrite [03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp] File in: 2019-03-06 07:05:32

--- cccccc/03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp
+++ cccccc/03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp
@@ -1,233 +1,195 @@
 
         
-            void minMaxLoc(const Size2D &size,
-                   const s16 * srcBase, ptrdiff_t srcStride,
-                   s16 &minVal, size_t &minCol, size_t &minRow,
-                   s16 &maxVal, size_t &maxCol, size_t &maxRow);
-    
-        enum COLOR_SPACE
+        
+    {    void operator() (const u8 * src0, const u8 * src1, u8 * dst) const
     {
-        COLOR_SPACE_BT601,
-        COLOR_SPACE_BT709
-    };
-    
-    #endif
-    
-    void combineUYVY(const Size2D &size,
-                 const u8 * srcyBase, ptrdiff_t srcyStride,
-                 const u8 * srcuBase, ptrdiff_t srcuStride,
-                 const u8 * srcvBase, ptrdiff_t srcvStride,
-                 u8 * dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-#ifndef __ANDROID__
-    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
-#endif
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+        dst[0] = src0[0] & src1[0];
     }
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s32, u8, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src1]]                              \n\t'
-             'vld1.32 {d2-d3}, [%[src2]]                              \n\t'
-             'vqmovun.s32 d4, q0                                      \n\t'
-             'vqmovun.s32 d5, q1                                      \n\t'
-             'vqmovn.u16  d6, q2                                      \n\t'
-             'vst1.8 {d6}, [%[dst]]                                   \n\t'
-             : /*no output*/
-             : [src1] 'r' (_src + i + 0),
-               [src2] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i)
-             : 'd0','d1','d2','d3','d4','d5','d6'
-         );
-     }
-})
-#else
-CVT_FUNC(s32, u8, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         int32x4_t vline1_s32 = vld1q_s32(_src + i);
-         int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
-    }
-    }
-    
-            for (; j < roiw_tail; j += step_tail, js += step_tail3, jd -= step_tail3)
-        {
-            vec64 v_src = vld3(src + js), v_dst;
-            v_dst.val[0] = vrev64(v_src.val[0]);
-            v_dst.val[1] = vrev64(v_src.val[1]);
-            v_dst.val[2] = vrev64(v_src.val[2]);
-    }
-    
-    #define INRANGEFUNC(T)                                       \
-void inRange(const Size2D &_size,                            \
-             const T * srcBase, ptrdiff_t srcStride,         \
-             const T * rng1Base, ptrdiff_t rng1Stride,       \
-             const T * rng2Base, ptrdiff_t rng2Stride,       \
-             u8 * dstBase, ptrdiff_t dstStride)              \
-{                                                            \
-    internal::assertSupportedConfiguration();                \
-    inRangeCheck(_size, srcBase, srcStride,                  \
-                 rng1Base, rng1Stride, rng2Base, rng2Stride, \
-                 dstBase, dstStride);                        \
-}
-#else
-#define INRANGEFUNC(T)                                       \
-void inRange(const Size2D &,                                 \
-             const T *, ptrdiff_t,                           \
-             const T *, ptrdiff_t,                           \
-             const T *, ptrdiff_t,                           \
-             u8 *, ptrdiff_t)                                \
-{                                                            \
-    internal::assertSupportedConfiguration();                \
-}
-#endif
-    
-        // the others
-    for (size_t i = 1; i < size.height ; ++i)
-    {
-        src = internal::getRowPtr(srcBase, srcStride, i);
-        f64 * prevSqSum = internal::getRowPtr(sqsumBase, sqsumStride, i - 1);
-        sqsum = internal::getRowPtr(sqsumBase, sqsumStride, i);
-    }
-    
-    namespace tesseract {
-class Tesseract;
-}
-    
-    void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
-  // Prepare all the blobs.
-  GenericVector<BlobData> blobs;
-  for (int w = 0; w < words.size(); ++w) {
-    if (words[w].word->ratings != nullptr &&
-        words[w].word->ratings->get(0, 0) == nullptr) {
-      for (int s = 0; s < words[w].lang_words.size(); ++s) {
-        Tesseract* sub = s < sub_langs_.size() ? sub_langs_[s] : this;
-        const WERD_RES& word = *words[w].lang_words[s];
-        for (int b = 0; b < word.chopped_word->NumBlobs(); ++b) {
-          blobs.push_back(BlobData(b, sub, word));
-        }
-      }
-    }
-  }
-  // Pre-classify all the blobs.
-  if (tessedit_parallelize > 1) {
-#ifdef _OPENMP
-#pragma omp parallel for num_threads(10)
-#endif  // _OPENMP
-    for (int b = 0; b < blobs.size(); ++b) {
-      *blobs[b].choices =
-          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
-    }
-  } else {
-    // TODO(AMD) parallelize this.
-    for (int b = 0; b < blobs.size(); ++b) {
-      *blobs[b].choices =
-          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
-    }
-  }
-}
-    
-      // blow away the copied chopped_word, as we want to work with
-  // the blobs from the input chopped_word so seam_arrays can be merged.
-  TWERD *chopped = word->chopped_word;
-  TWERD *chopped2 = new TWERD;
-  chopped2->blobs.reserve(chopped->NumBlobs() - split_pt);
-  for (int i = split_pt; i < chopped->NumBlobs(); ++i) {
-    chopped2->blobs.push_back(chopped->blobs[i]);
-  }
-  chopped->blobs.truncate(split_pt);
-  word->chopped_word = nullptr;
-  delete word2->chopped_word;
-  word2->chopped_word = nullptr;
-    
-      // Merges the boxes from start to end, not including end, and deletes
-  // the boxes between start and end.
-  void MergeBoxes(int start, int end);
-    
-    // A CostFunc that takes the variance of step into account in the cost.
-int64_t DPPoint::CostWithVariance(const DPPoint* prev) {
-  if (prev == nullptr || prev == this) {
-    UpdateIfBetter(0, 1, nullptr, 0, 0, 0);
-    return 0;
-  }
-    }
-    
-    
-    { private:
-  double total_weight;         // no of elements or sum of weights.
-  double sigx;                 // sum of x
-  double sigy;                 // sum of y
-  double sigxx;                // sum x squared
-  double sigxy;                // sum of xy
-  double sigyy;                // sum y squared
 };
     
-    # define INSTANTIATE_TEST_CASE_P(prefix, test_case_name, generator) \
-  ::testing::internal::ParamGenerator<test_case_name::ParamType> \
-      gtest_##prefix##test_case_name##_EvalGenerator_() { return generator; } \
-  int gtest_##prefix##test_case_name##_dummy_ = \
-      ::testing::UnitTest::GetInstance()->parameterized_test_registry(). \
-          GetTestCasePatternHolder<test_case_name>(\
-              #test_case_name, __FILE__, __LINE__)->AddTestCaseInstantiation(\
-                  #prefix, \
-                  &gtest_##prefix##test_case_name##_EvalGenerator_, \
-                  __FILE__, __LINE__)
     
-      // C'tor.  TestPartResult does NOT have a default constructor.
-  // Always use this constructor (with parameters) to create a
-  // TestPartResult object.
-  TestPartResult(Type a_type,
-                 const char* a_file_name,
-                 int a_line_number,
-                 const char* a_message)
-      : type_(a_type),
-        file_name_(a_file_name == NULL ? '' : a_file_name),
-        line_number_(a_line_number),
-        summary_(ExtractSummary(a_message)),
-        message_(a_message) {
-  }
+    {            v_src = vld4q_u8(src + sj + 64);
+            vst1q_u8(dst + dj + 16, v_src.val[coi]);
+        }
+#endif
     
-    #endif  // GTEST_INCLUDE_GTEST_INTERNAL_GTEST_FILEPATH_H_
-
+    
+    {            v_y = vld2q_u8(srcy + syj + 32);
+            v_dst.val[0] = vld1q_u8(srcu + sj + 16);
+            v_dst.val[1] = v_y.val[0];
+            v_dst.val[2] = vld1q_u8(srcv + sj + 16);
+            v_dst.val[3] = v_y.val[1];
+            vst4q_u8(dst + dj + 64, v_dst);
+        }
+#endif
+    
+             int32x4_t vline_s32_lo = vmovl_s16(vget_low_s16(vline_s16));
+         int32x4_t vline_s32_hi = vmovl_s16(vget_high_s16(vline_s16));
+    
+            if(i + 2 <= size.width)
+        {
+            float32x2_t vres = vmul_f32(vld1_f32(src0 + i), vld1_f32(src1 + i));
+            result += vget_lane_f32(vres, 0) + vget_lane_f32(vres, 1);
+            i += 2;
+        }
+    
+                    uint8x16_t v0 = vld1q_u8(ptr);
+                int8x16_t v1 = vreinterpretq_s8_u8(veorq_u8(vqsubq_u8(v0, t), delta));
+                int8x16_t v2 = vreinterpretq_s8_u8(veorq_u8(vqaddq_u8(v0, t), delta));
+    
+        for (size_t i = 0; i < size.height; ++i)
+    {
+        const T * src = getRowPtr((const T *)srcBase, srcStride, i);
+        T * dst = getRowPtr((T *)dstBase, dstStride, (flipMode & FLIP_VERTICAL_MODE) != 0 ? size.height - i - 1 : i);
+        size_t j = 0, js = 0, jd = size.width * 3;
+    }
+    
+    #include 'common.hpp'
+    
+    #include 'common.hpp'
+    
+    namespace CAROTENE_NS { namespace internal {
+    }
+    }
+    
+    // Helper method to convert an orientation index to its value in degrees.
+// The value represents the amount of clockwise rotation in degrees that must be
+// applied for the text to be upright (readable).
+TESS_API int OrientationIdToValue(const int& id);
+    
+    #include 'allheaders.h'
+    
+      // Copying a DENORM is allowed.
+  DENORM(const DENORM &);
+  DENORM& operator=(const DENORM&);
+  ~DENORM();
+    
+      // Gets the text streamed to this object so far as an std::string.
+  // Each '\0' character in the buffer is replaced with '\\0'.
+  //
+  // INTERNAL IMPLEMENTATION - DO NOT USE IN A USER PROGRAM.
+  std::string GetString() const;
+    
+    ]]
+    
+      // Same as above, but you can choose the interception scope of this object.
+  ScopedFakeTestPartResultReporter(InterceptMode intercept_mode,
+                                   TestPartResultArray* result);
+    
+    // The empty template list.
+struct Templates0 {};
+    
+    using ::testing::EmptyTestEventListener;
+using ::testing::InitGoogleTest;
+using ::testing::Test;
+using ::testing::TestCase;
+using ::testing::TestEventListeners;
+using ::testing::TestInfo;
+using ::testing::TestPartResult;
+using ::testing::UnitTest;
     
       const size_t len = strlen(a_c_string);
   char* const clone = new char[ len + 1 ];
   memcpy(clone, a_c_string, len + 1);
     
+    // A sample program demonstrating using Google C++ testing framework.
+//
+// Author: wan@google.com (Zhanyong Wan)
     
-    {  if (s.ok() && meta->file_size > 0) {
-    // Keep it
-  } else {
-    env->DeleteFile(fname);
-  }
-  return s;
+    // Tests the c'tor that accepts a C string.
+TEST(MyString, ConstructorFromCString) {
+  const MyString s(kHelloString);
+  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
+  EXPECT_EQ(sizeof(kHelloString)/sizeof(kHelloString[0]) - 1,
+            s.Length());
 }
     
-    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
-#define STORAGE_LEVELDB_DB_DB_ITER_H_
+    void DBImpl::DeleteObsoleteFiles() {
+  mutex_.AssertHeld();
+    }
     
-    // Return the name of the sstable with the specified number
-// in the db named by 'dbname'.  The result will be prefixed with
-// 'dbname'.
-std::string TableFileName(const std::string& dbname, uint64_t number);
+    #ifndef STORAGE_LEVELDB_DB_LOG_FORMAT_H_
+#define STORAGE_LEVELDB_DB_LOG_FORMAT_H_
     
     namespace leveldb {
 namespace log {
     }
     }
     
-      void StartReadingAt(uint64_t initial_offset) {
-    delete reader_;
-    reader_ = new Reader(&source_, &report_, true/*checksum*/, initial_offset);
+    
+    {}  // namespace leveldb
+    
+      // If a seek to internal key 'k' in specified file finds an entry,
+  // call (*handle_result)(arg, found_key, found_value).
+  Status Get(const ReadOptions& options,
+             uint64_t file_number,
+             uint64_t file_size,
+             const Slice& k,
+             void* arg,
+             void (*handle_result)(void*, const Slice&, const Slice&));
+    
+    
+    {  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
+  server.serve();
+  return 0;
+}
+    
+    // Saved resource information for the beginning of an operation
+#ifdef WIN32
+struct rusage {
+  FILETIME ru_utime;
+  FILETIME ru_stime;
+};
+#endif
+    
+      // Join results..
+  for (auto& entry : hw_info) {
+    bool matched = false;
+    for (auto& row : results) {
+      auto serial = row.find('serial_number');
+      if (serial == row.end()) {
+        continue;
+      }
+    }
+    }
+    
+    TEST_F(PerfOutputTests,
+       impl_consumeWrappedMessagesFromCircularBuffer_simply_wrapped) {
+  using WrappedMessage = ebpf::PerfOutput<TestMessage>::WrappedMessage;
+  auto const test_size = std::size_t{9};
+  auto buf =
+      std::vector<ebpf::impl::ByteType>(sizeof(WrappedMessage) * test_size, 0);
+  auto buf_ptr = &buf[0];
+  for (std::size_t i = 0; i < test_size; ++i) {
+    auto wrapped = WrappedMessage{};
+    wrapped.msg.a_ = i + 1;
+    wrapped.msg.b_ = i * 2 + 2;
+    wrapped.msg.c_ = 'y';
+    wrapped.msg.d_ = 'x';
+    wrapped.size = sizeof(TestMessage);
+    wrapped.header.type = PERF_RECORD_SAMPLE;
+    wrapped.header.size = sizeof(WrappedMessage);
+    auto const wrapped_ptr =
+        reinterpret_cast<ebpf::impl::ByteType const*>(&wrapped);
+    std::copy(wrapped_ptr, wrapped_ptr + sizeof(WrappedMessage), buf_ptr);
+    buf_ptr += sizeof(WrappedMessage);
   }
+  auto dst = std::vector<TestMessage>{};
+  auto status =
+      ebpf::impl::consumeWrappedMessagesFromCircularBuffer<WrappedMessage>(
+          &buf[0],
+          sizeof(WrappedMessage),
+          buf.size() + sizeof(WrappedMessage),
+          buf.size(),
+          dst);
+  ASSERT_FALSE(status.isError()) << status.getError().getMessage();
+  ASSERT_EQ(dst.size(), test_size);
+  for (std::size_t i = 0; i < test_size; ++i) {
+    EXPECT_EQ(dst[i].c_, 'y') << i;
+    EXPECT_EQ(dst[i].d_, 'x');
+  }
+  EXPECT_EQ(dst.back().a_, 1);
+  EXPECT_EQ(dst.back().b_, 2);
+  EXPECT_EQ(dst[0].a_, 2);
+  EXPECT_EQ(dst[0].b_, 4);
+}
