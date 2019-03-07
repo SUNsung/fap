1b691992fb1a7e9dcdd1f510c1d19146153a6ea8
SUNsung <weatherius@gmail.com>
2019-03-07 23:10:45 +0200

    Rewrite [55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp] File in: 2019-03-07 23:10:45

--- cccccc/55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp
+++ cccccc/55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp
@@ -1,66 +1,84 @@
 
         
-        namespace {
+                    if (accumulatedMetric && m_samples.second > 0)
+            {
+                m_metric.second = accumulatedMetric->AsScalar<double>();
+            }
+    
+            static const size_t s_serializationVersion = 0;
+    
+        // Releases the mutex
+    void Release()
+    {
+        assert(m_handle != NULL);
+        int rc = 0;
+        rc = ::ReleaseMutex(m_handle);
+        if ((rc == RELEASEMUTEX_ERROR) && !std::uncaught_exception())
+        {
+            RuntimeError('Mutex Release: Failed to release mutex %s: %d', m_name.c_str(), ::GetLastError());
+        }
+        rc = ::CloseHandle(m_handle);
+        if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
+        {
+            RuntimeError('Mutex Release: Failed to close handler %s: %d', m_name.c_str(), ::GetLastError());
+        }
+        m_handle = NULL;
     }
-    
-      // display::DisplayObserver:
-  void OnDisplayAdded(const display::Display& new_display) override;
-  void OnDisplayRemoved(const display::Display& old_display) override;
-  void OnDisplayMetricsChanged(const display::Display& display,
-                               uint32_t changed_metrics) override;
-    
-    #include 'atom/browser/api/trackable_object.h'
-#include 'atom/browser/ui/tray_icon.h'
-#include 'atom/browser/ui/tray_icon_observer.h'
-#include 'native_mate/handle.h'
-    
-    namespace IPC {
-class Message;
-}
-    
-    #if defined(OS_LINUX)
-typedef PowerObserverLinux PowerObserver;
-#else
-typedef base::PowerObserver PowerObserver;
-#endif  // defined(OS_LINUX)
-    
-    // --------------------------- Structures ---------------------------
-    
-    class AboutProtocolHandler : public net::URLRequestJobFactory::ProtocolHandler {
- public:
-  AboutProtocolHandler();
-  ~AboutProtocolHandler() override;
-    }
-    
-    OffScreenOutputDevice::OffScreenOutputDevice(bool transparent,
-                                             const OnPaintCallback& callback)
-    : transparent_(transparent), callback_(callback) {
-  DCHECK(!callback_.is_null());
-}
     
     
     {
     {
-    {      // Test write Symbol
-      std::vector<unsigned char> buffer2(
-        CompressedBufferWriter::CalculateBufferSize(input.size(),
-          alphabet_size));
-      for (int i = 0; i < input.size(); i++) {
-        cbw.WriteSymbol(buffer2.data(), input[i], i);
-      }
-      CompressedIterator<int> ci2(buffer.data(), alphabet_size);
-      std::vector<int> output2(input.size());
-      for (int i = 0; i < input.size(); i++) {
-        output2[i] = ci2[i];
-      }
-      ASSERT_TRUE(input == output2);
-    }
-  }
-}
+    {}}}
+
     
-    // implementing configure.
-template<typename PairIter>
-inline void GradientBooster::Configure(PairIter begin, PairIter end) {
-  std::vector<std::pair<std::string, std::string> > vec(begin, end);
-  this->Configure(vec);
+    #include <string>
+    
+    
+    {        sliceInputGrad += sliceOutputGrad; // here the assumption is that sliceOutputGrad is a row vector
+    }
+    
+            // create a vector with the correct number of timesteps(shapeXT[2]) containing the sequence count (shapeXT[1])
+        numSequencesForFrame = vector<size_t>(shapeXT[2], shapeXT[1]);
+        m_transposedOutput->RNNForward(*m_transposedInput, paramW, shapeXT[0], shapeYT[0], numSequencesForFrame, m_rnnAttributes, *m_reserve, *m_workspace);
+    
+    #include <folly/io/Cursor.h>
+#include <folly/io/IOBuf.h>
+#include <folly/lang/Bits.h>
+    
+     private:
+  // Matches packed_sync_pointer.  Must be > max number of local
+  // counts.  This is the max number of threads that can access this
+  // atomic_shared_ptr at once before we start blocking.
+  static constexpr unsigned EXTERNAL_OFFSET{0x2000};
+  // Bit signifying aliased constructor
+  static constexpr unsigned ALIASED_PTR{0x4000};
+    
+    // This temporarily forwards tuple and some tuple functions from std::tr1 to
+// std:: to make it possible to migrate from std::tr1.
+//
+// TODO(crbug.com/829773): Remove this file when the transition is complete.
+    
+    
+    {
+    {}  // namespace internal
+}  // namespace v8
+
+    
+      Node* const fast_iter_result_map =
+      LoadContextElement(native_context, Context::ITERATOR_RESULT_MAP_INDEX);
+    
+    // ES6 #sec-boolean.prototype.tostring
+TF_BUILTIN(BooleanPrototypeToString, CodeStubAssembler) {
+  Node* context = Parameter(Descriptor::kContext);
+  Node* receiver = Parameter(Descriptor::kReceiver);
+    }
+    
+      explicit LazyBuiltinsAssembler(compiler::CodeAssemblerState* state)
+      : CodeStubAssembler(state) {}
+    
+    // ES6 #sec-math.cos
+TF_BUILTIN(MathCos, MathBuiltinsAssembler) {
+  Node* context = Parameter(Descriptor::kContext);
+  Node* x = Parameter(Descriptor::kX);
+  MathUnaryOperation(context, x, &CodeStubAssembler::Float64Cos);
 }
