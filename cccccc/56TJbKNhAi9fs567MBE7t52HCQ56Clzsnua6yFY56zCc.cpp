a2ce115a4e3232ec82d315207e6e72423879c8d3
SUNsung <weatherius@gmail.com>
2019-03-07 14:53:17 +0200

    Rewrite [56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp] File in: 2019-03-07 14:53:11

--- cccccc/56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp
+++ cccccc/56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp
@@ -1,338 +1,167 @@
 
         
-            http://www.apache.org/licenses/LICENSE-2.0
+        //////////////////////////////////////////////////////////////////////
     
-    // Binary arithmetic operators on PyBfloat16 values.
-#define BFLOAT16_BINOP(name, op)                                  \
-  PyObject* PyBfloat16_##name(PyObject* a, PyObject* b) {         \
-    bfloat16 x, y;                                                \
-    if (!AsBfloat16(a, &x) || !AsBfloat16(b, &y)) return nullptr; \
-    bfloat16 z = x op y;                                          \
-    return PyBfloat16_FromBfloat16(z).release();                  \
+    //////////////////////////////////////////////////////////////////////
+    
+        // GP Register cannot be greater than 31
+    assert(static_cast<uint32_t>(s) < 32);
+    assert(static_cast<uint32_t>(ra) < 32);
+    assert(static_cast<uint32_t>(rb) < 32);
+    
+    Variant PlainDirectory::read() {
+  struct dirent entry;
+  struct dirent *result;
+  int ret = readdir_r(m_dir, &entry, &result);
+  if (ret != 0 || !result) {
+    return false;
   }
-BFLOAT16_BINOP(Add, +)
-BFLOAT16_BINOP(Subtract, -)
-BFLOAT16_BINOP(Multiply, *)
-BFLOAT16_BINOP(Divide, /)
-#undef BFLOAT16_BINOP
+  return String(entry.d_name, CopyString);
+}
+    
+    #ifndef HPHP_GLOB_STREAM_WRAPPER_H
+#define HPHP_GLOB_STREAM_WRAPPER_H
     
     
-    {}  // namespace tensorflow
+    {  logAHMSubMapWarning(mapName);
+}
     
-    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
-// a copy.
-Status TensorToNdarray(const Tensor& t, PyObject** ret);
+    #include <folly/String.h>
     
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    
-    {  DCHECK(PyDict_Check(code_to_exc_type_map));
-  PyObject* key;
-  PyObject* value;
-  Py_ssize_t pos = 0;
-  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
-    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
-    singleton_->exc_types_[code] = value;
-    // The exception classes should also have the lifetime of the process, but
-    // incref just in case.
-    Py_INCREF(value);
+      char *end = nullptr;
+  long nFD = strtol(sFD, &end, 10);
+  if ((sFD == end) || (*end != '\0')) {
+    raise_warning('php://fd/ stream must be specified in the form '
+                  'php://fd/<orig fd>');
+    return nullptr;
   }
-}
-    
-      // Close the underlying file and release its resources.
-  void Close();
-    
-    #endif  // TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_PLATFORM_ID_H_
-
-    
-    ExecutorCache::Entry::~Entry() {
-  mutex_lock lock{configurations_mutex};
-  configurations.clear();
-}
-    
-    #ifndef TENSORFLOW_STREAM_EXECUTOR_FFT_H_
-#define TENSORFLOW_STREAM_EXECUTOR_FFT_H_
-    
-    namespace {
-    }
-    
-    // static
-void UvTaskRunner::OnClose(uv_handle_t* handle) {
-  delete reinterpret_cast<uv_timer_t*>(handle);
-}
-    
-    void BrowserWindow::UpdateDraggableRegions(
-    content::RenderFrameHost* rfh,
-    const std::vector<DraggableRegion>& regions) {
-  if (window_->has_frame())
-    return;
-  static_cast<NativeWindowViews*>(window_.get())
-      ->UpdateDraggableRegions(DraggableRegionsToSkRegion(regions));
-}
-    
-    class Event : public Wrappable<Event>, public content::WebContentsObserver {
- public:
-  static Handle<Event> Create(v8::Isolate* isolate);
-    }
-    
-    namespace api {
-class WebContents;
-}
-    
-    namespace auto_updater {
-    }
-    
-      // net::URLRequestJobFactory::ProtocolHandler:
-  net::URLRequestJob* MaybeCreateJob(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const override;
-  bool IsSafeRedirectTarget(const GURL& location) const override;
-    
-    #include <string>
-    
-    void OffscreenViewProxy::SetObserver(OffscreenViewProxyObserver* observer) {
-  if (observer_) {
-    observer_->ProxyViewDestroyed(this);
+  long dtablesize = getdtablesize();
+  if ((nFD < 0) || (nFD >= dtablesize)) {
+    raise_warning('The file descriptors must be non-negative numbers '
+                  'smaller than %ld', dtablesize);
+    return nullptr;
   }
-  observer_ = observer;
-}
     
-    /**
- * @brief Abstract base class that factors out the BLAS code common to
- *        ConvolutionLayer and DeconvolutionLayer.
- */
-template <typename Dtype>
-class BaseConvolutionLayer : public Layer<Dtype> {
- public:
-  explicit BaseConvolutionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+    #include 'unicode/scientificnumberformatter.h'
+#include 'unicode/dcfmtsym.h'
+#include 'unicode/fpositer.h'
+#include 'unicode/utf16.h'
+#include 'unicode/uniset.h'
+#include 'decfmtst.h'
+#include 'unicode/decimfmt.h'
+    
+    int32_t SearchIterator::first(UErrorCode &status)
+{
+    if (U_FAILURE(status)) {
+        return USEARCH_DONE;
     }
-    
-      /**
-   * @brief Computes the error gradient w.r.t. the reordered input.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient
-   *        with respect to the outputs
-   *   -# @f$ (M \times ...) @f$:
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to concatenated outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2):
-   *   - @f$ \frac{\partial E}{\partial y} @f$ is de-indexed (summing where
-   *     required) back to the input x_1
-   *   - This layer cannot backprop to x_2, i.e. propagate_down[1] must be
-   *     false.
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    {}  // namespace caffe
-    
-    template <typename Dtype>
-class CropLayer : public Layer<Dtype> {
- public:
-  explicit CropLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-                           const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-                            const vector<bool>& propagate_down,
-                            const vector<Blob<Dtype>*>& bottom);
-    
-    	if (!streqi(cmderStart.c_str(), L''))
-	{
-		swprintf_s(args, L'%s /dir \'%s\'', args, cmderStart.c_str());
-	}
-    
-    
-    {  for (size_t i = 0; i < nindptr; ++i) {
-    col_ptr_[i] = static_cast<size_t>(p_indptr[i]);
-  }
-  #pragma omp parallel for schedule(static)
-  for (int64_t i = 0; i < static_cast<int64_t>(ndata); ++i) {
-    indices_[i] = static_cast<unsigned>(p_indices[i]);
-    data_[i] = static_cast<float>(p_data[i]);
-  }
-  DMatrixHandle handle;
-  CHECK_CALL(XGDMatrixCreateFromCSCEx(BeginPtr(col_ptr_), BeginPtr(indices_),
-                                      BeginPtr(data_), nindptr, ndata,
-                                      nrow, &handle));
-  ret = PROTECT(R_MakeExternalPtr(handle, R_NilValue, R_NilValue));
-  R_RegisterCFinalizerEx(ret, _DMatrixFinalizer, TRUE);
-  R_API_END();
-  UNPROTECT(1);
-  return ret;
-}
-    
-    /*! \brief a column storage, to be used with ApplySplit. Note that each
-    bin id is stored as index[i] + index_base. */
-class Column {
- public:
-  Column(ColumnType type, const uint32_t* index, uint32_t index_base,
-         const size_t* row_ind, size_t len)
-      : type_(type),
-        index_(index),
-        index_base_(index_base),
-        row_ind_(row_ind),
-        len_(len) {}
-  size_t Size() const { return len_; }
-  uint32_t GetGlobalBinIdx(size_t idx) const { return index_base_ + index_[idx]; }
-  uint32_t GetFeatureBinIdx(size_t idx) const { return index_[idx]; }
-  // column.GetFeatureBinIdx(idx) + column.GetBaseIdx(idx) ==
-  // column.GetGlobalBinIdx(idx)
-  uint32_t GetBaseIdx() const { return index_base_; }
-  ColumnType GetType() const { return type_; }
-  size_t GetRowIdx(size_t idx) const {
-    return type_ == ColumnType::kDenseColumn ? idx : row_ind_[idx];
-  }
-  bool IsMissing(size_t idx) const {
-    return index_[idx] == std::numeric_limits<uint32_t>::max();
-  }
-  const size_t* GetRowData() const { return row_ind_; }
-    }
-    
-    int main() {
-    }
-    
-            if(nums.size() == 0)
-            return 0;
-    
-    
-void printArr(const vector<int>& vec){
-    for(int e: vec)
-        cout << e << ' ';
-    cout << endl;
+    setOffset(0, status);
+    return handleNext(0, status);
 }
     
     
-    {        vector<int> res;
-        __inorderTraversal(root, res);
-        return res;
-    }
-    
-    private:
-    struct Command{
-        string s;   // go, print
-        TreeNode* node;
-        Command(string s, TreeNode* node): s(s), node(node){}
-    };
-    
-    
-    {                if(node->left){
-                    q.push(node->left);
-                    new_level_num ++;
-                }
-                if(node->right){
-                    q.push(node->right);
-                    new_level_num ++;
-                }
-            }
-    
-        TreeNode* root = new TreeNode(1);
-    root->right = new TreeNode(2);
-    root->right->left = new TreeNode(3);
-    vector<int> res = Solution().preorderTraversal(root);
-    print_vec(res);
-    
-    
-    {    // exception out_of_range.401
-    try
-    {
-        // try to write beyond the array limit
-        array.at(5) = 'sixth';
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-}
-
-    
-        // out_of_range.403
-    try
-    {
-        // try to use a JSON pointer to an nonexistent object key
-        json::const_reference ref = j.at('/foo'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-        // create an array from std::vector
-    std::vector<int> c_vector {1, 2, 3, 4};
-    json j_vec(c_vector);
-    
-        // add values
-    auto res1 = object.emplace('three', 3);
-    null.emplace('A', 'a');
-    null.emplace('B', 'b');
-    
-    using apollo::common::ErrorCode;
-    
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-#include 'modules/common/proto/error_code.pb.h'
-#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
-#include 'modules/drivers/canbus/can_comm/protocol_data.h'
-    
-    class MockMessageManager
-    : public MessageManager<::apollo::canbus::ChassisDetail> {
- public:
-  MockMessageManager() {
-    AddRecvProtocolData<MockProtocolData, true>();
-    AddSendProtocolData<MockProtocolData, true>();
-  }
+    {    BreakIterator *get() const { return ptr; }
+    BreakIterator *operator->() const { return ptr; }
+    BreakIterator &operator*() const { return *ptr; }
+private:
+    BreakIterator *ptr;
+    SharedBreakIterator(const SharedBreakIterator &);
+    SharedBreakIterator &operator=(const SharedBreakIterator &);
 };
     
-    TEST(ByteTest, SetBit) {
-  unsigned char byte_value = 0xFF;
-  Byte value(&byte_value);
-  value.set_bit_0(1);
-  EXPECT_EQ(0xFD, value.get_byte());
-  value.set_bit_0(7);
-  EXPECT_EQ(0x7D, value.get_byte());
-  value.set_bit_1(7);
-  EXPECT_EQ(0xFD, value.get_byte());
-  value.set_value(0x77);
-  value.set_bit_1(0);
-  EXPECT_EQ(0x77, value.get_byte());
-    }
+    class U_I18N_API SharedNumberFormat : public SharedObject {
+public:
+    SharedNumberFormat(NumberFormat *nfToAdopt) : ptr(nfToAdopt) { }
+    virtual ~SharedNumberFormat();
+    const NumberFormat *get() const { return ptr; }
+    const NumberFormat *operator->() const { return ptr; }
+    const NumberFormat &operator*() const { return *ptr; }
+private:
+    NumberFormat *ptr;
+    SharedNumberFormat(const SharedNumberFormat &);
+    SharedNumberFormat &operator=(const SharedNumberFormat &);
+};
     
+    class U_I18N_API SharedPluralRules : public SharedObject {
+public:
+    SharedPluralRules(PluralRules *prToAdopt) : ptr(prToAdopt) { }
+    virtual ~SharedPluralRules();
+    const PluralRules *operator->() const { return ptr; }
+    const PluralRules &operator*() const { return *ptr; }
+private:
+    PluralRules *ptr;
+    SharedPluralRules(const SharedPluralRules &);
+    SharedPluralRules &operator=(const SharedPluralRules &);
+};
     
-    {
-    {
-    {}  // namespace conti_radar
-}  // namespace drivers
-}  // namespace apollo
+    #endif  // __SIGNIFICANTDIGITINTERVAL_H__
 
     
-      x <<= 5;
-  x |= t;
     
-    Eigen::MatrixXd SplineSegKernel::SecondOrderDerivativeKernel(
-    const uint32_t num_params, const double accumulated_x) {
-  if (num_params > reserved_order_ + 1) {
-    CalculateSecondOrderDerivative(num_params);
-  }
-  Eigen::MatrixXd term_matrix;
-  IntegratedTermMatrix(num_params, accumulated_x, 'second_order', &term_matrix);
-  return kernel_second_order_derivative_.block(0, 0, num_params, num_params)
-      .cwiseProduct(term_matrix);
-}
+/**
+ * A formatter for small, positive integers.
+ */
+class U_I18N_API SmallIntFormatter : public UMemory {
+public:
+    /**
+     * Estimates the actual digit count needed to format positiveValue
+     * using the given range of digit counts.
+     * Returns a value that is at least the actual digit count needed.
+     *
+     * @param positiveValue the value to format
+     * @param range the acceptable range of digit counts.
+     */
+    static int32_t estimateDigitCount(
+            int32_t positiveValue, const IntDigitCountRange &range);
+    }
     
-    #include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
+    U_NAMESPACE_END
+    
+        /**
+     * @param keyword for example 'few' or 'other'
+     * @return the plural form corresponding to the keyword, or OTHER
+     */
+    static Form orOtherFromString(const UnicodeString &keyword) {
+        return static_cast<Form>(indexOrOtherIndexFromString(keyword));
+    }
+    
+            // Get the function pointer (required for any extensions)
+        auto vkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(g_Instance, 'vkCreateDebugReportCallbackEXT');
+        IM_ASSERT(vkCreateDebugReportCallbackEXT != NULL);
+    
+    #include 'imgui.h'      // IMGUI_API, ImFontAtlas
+    
+            // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!), 
+        // you may need to backup/reset/restore current shader using the commented lines below.
+        //GLint last_program; 
+        //glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
+        //glUseProgram(0);
+        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+        //glUseProgram(last_program);
+    
+        // Main loop
+    bool done = false;
+    while (!done)
+    {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                done = true;
+            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+                done = true;
+        }
+    }
+    
+            ID3D10Texture2D *pTexture = NULL;
+        D3D10_SUBRESOURCE_DATA subResource;
+        subResource.pSysMem = pixels;
+        subResource.SysMemPitch = desc.Width * 4;
+        subResource.SysMemSlicePitch = 0;
+        g_pd3dDevice->CreateTexture2D(&desc, &subResource, &pTexture);
