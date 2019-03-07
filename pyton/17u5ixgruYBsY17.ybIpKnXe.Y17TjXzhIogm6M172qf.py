5f1a664482d0dc17dfd8f49064643284055dec1a
SUNsung <weatherius@gmail.com>
2019-03-07 11:23:19 +0200

    Rewrite [17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py] File in: 2019-03-07 11:23:19

--- pyton/17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py
+++ pyton/17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py
@@ -1,3 +1,103 @@
 
         
-        
+          T, N = vals_txn.shape
+  if n_to_plot > N:
+    n_to_plot = N
+    
+    # CONTROLLER
+# This parameter critically controls whether or not there is a controller
+# (along with controller encoders placed into the LFADS graph.  If CO_DIM >
+# 1, that means there is a 1 dimensional controller outputs, if equal to 0,
+# then no controller.
+flags.DEFINE_integer('co_dim', CO_DIM,
+    'Number of control net outputs (>0 builds that graph).')
+    
+      def decode(self, cur_ids):
+    '''Convert a list of ids to a sentence, with space inserted.'''
+    return ' '.join([self.id_to_word(cur_id) for cur_id in cur_ids])
+    
+      counter = collections.Counter(data)
+  count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+    
+    
+def create_discriminator(hparams,
+                         sequence,
+                         is_training,
+                         reuse=None,
+                         initial_state=None,
+                         inputs=None,
+                         present=None):
+  '''Create the Discriminator model specified by the FLAGS and hparams.
+    
+    '''Model optimization.'''
+    
+          elif FLAGS.generator_model.startswith('seq2seq'):
+        load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
+        print('Restoring Generator from %s.' % load_ckpt)
+        tf.logging.info('Restoring Generator from %s.' % load_ckpt)
+        gen_encoder_init_saver = init_savers['gen_encoder_init_saver']
+        gen_decoder_init_saver = init_savers['gen_decoder_init_saver']
+        gen_encoder_init_saver.restore(sess, load_ckpt)
+        gen_decoder_init_saver.restore(sess, load_ckpt)
+    
+    
+def test_objective_shapes_3d():
+    y_a = K.variable(np.random.random((5, 6, 7)))
+    y_b = K.variable(np.random.random((5, 6, 7)))
+    for obj in allobj:
+        objective_output = obj(y_a, y_b)
+        assert K.eval(objective_output).shape == (5, 6)
+    
+    Introduced in TensorFlow 1.3, the Dataset API is now the
+standard method for loading data into TensorFlow models.
+A Dataset is a sequence of elements, which are themselves
+composed of tf.Tensor components. For more details, see:
+https://www.tensorflow.org/programmers_guide/datasets
+    
+    batch_size = 32
+num_classes = 10
+epochs = 200
+hidden_units = 100
+    
+    # input image dimensions
+img_rows, img_cols = 28, 28
+# number of convolutional filters to use
+filters = 32
+# size of pooling area for max pooling
+pool_size = 2
+# convolution kernel size
+kernel_size = 3
+    
+                # If the layer returns tensors from its inputs, unmodified,
+            # we copy them to avoid loss of tensor metadata.
+            output_ls = to_list(output)
+            inputs_ls = to_list(inputs)
+            output_ls_copy = []
+            for x in output_ls:
+                if x in inputs_ls:
+                    x = K.identity(x)
+                output_ls_copy.append(x)
+            output = unpack_singleton(output_ls_copy)
+    
+        centers = rand_center(data, 3)
+    print(centers)
+    
+    parser = ArgumentParser(usage='%s -m jieba [options] filename' % sys.executable, description='Jieba command line interface.', epilog='If no filename specified, use STDIN instead.')
+parser.add_argument('-d', '--delimiter', metavar='DELIM', default=' / ',
+                    nargs='?', const=' ',
+                    help='use DELIM instead of ' / ' for word delimiter; or a space if it is used without DELIM')
+parser.add_argument('-p', '--pos', metavar='DELIM', nargs='?', const='_',
+                    help='enable POS tagging; if DELIM is specified, use DELIM instead of '_' for POS delimiter')
+parser.add_argument('-D', '--dict', help='use DICT as dictionary')
+parser.add_argument('-u', '--user-dict',
+                    help='use USER_DICT together with the default dictionary or DICT (if specified)')
+parser.add_argument('-a', '--cut-all',
+                    action='store_true', dest='cutall', default=False,
+                    help='full pattern cutting (ignored with POS tagging)')
+parser.add_argument('-n', '--no-hmm', dest='hmm', action='store_false',
+                    default=True, help='don't use the Hidden Markov Model')
+parser.add_argument('-q', '--quiet', action='store_true', default=False,
+                    help='don't print loading messages to stderr')
+parser.add_argument('-V', '--version', action='version',
+                    version='Jieba ' + jieba.__version__)
+parser.add_argument('filename', nargs='?', help='input file')
