722087fb0234cba14680e1dac999fe13240a8a00
SUNsung <weatherius@gmail.com>
2019-03-08 12:14:56 +0200

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2019-03-08 12:14:55

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,219 +1,185 @@
 
         
-            def __hash_double_function(self, key, data, increment):
-        return (increment * self.__hash_function_2(key, data)) % self.size_table
-    
-        def keys(self):
-        return self._keys
-    
-    The problem is  :
-Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
-Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
-'''
-from __future__ import print_function
-    
-        dp = [[False for x in range(s+1)]for y in range(n+1)]
-    
-    # A dictionary with options for the search language support, empty by default.
-# 'ja' uses this config value.
-# 'zh' user can custom change `jieba` dictionary path.
-#
-# html_search_options = {'type': 'default'}
-    
-        def __init__(self, hass, client, address, timeout, ignored_click_types):
-        '''Initialize the flic button.'''
-        import pyflic
-    
-            try:
-            data = [
-                ('user', self._username),
-                (self._type, self._password),
-            ]
-            res = requests.post(self._loginurl, data=data, timeout=10)
-        except requests.exceptions.Timeout:
-            _LOGGER.error(
-                'Connection to the router timed out at URL %s', self._url)
-            return False
-        if res.status_code != 200:
-            _LOGGER.error(
-                'Connection failed with http code %s', res.status_code)
-            return False
-        try:
-            self._userid = res.cookies['userid']
-            return True
-        except KeyError:
-            _LOGGER.error('Failed to log in to router')
-            return False
-    
-        return scanner if scanner.success_init else None
-    
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.host = config[CONF_HOST]
-        self.username = config[CONF_USERNAME]
-        self.password = config[CONF_PASSWORD]
-        self.last_results = {}
-    
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_API_KEY): cv.string,
-    vol.Required(CONF_RECIPIENT): cv.string,
-})
+        # Allow direct execution
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     
     
-class CommandLineNotificationService(BaseNotificationService):
-    '''Implement the notification service for the Command Line service.'''
+    {    # Options that need a file parameter
+    'download-archive': ['--require-parameter'],
+    'cookies': ['--require-parameter'],
+    'load-info': ['--require-parameter'],
+    'batch-file': ['--require-parameter'],
+}
     
-        def send_message(self, message='', **kwargs):
-        '''Send a message to a user.'''
-        kwargs['message'] = message
-        self.hass.bus.fire(EVENT_NOTIFY, kwargs)
-
+        def test_cmdline_umlauts(self):
+        p = subprocess.Popen(
+            [sys.executable, 'youtube_dl/__main__.py', encodeArgument('ä'), '--version'],
+            cwd=rootDir, stdout=_DEV_NULL, stderr=subprocess.PIPE)
+        _, stderr = p.communicate()
+        self.assertFalse(stderr)
     
-        def __init__(self, hass, filename, add_timestamp):
-        '''Initialize the service.'''
-        self.filepath = os.path.join(hass.config.config_dir, filename)
-        self.add_timestamp = add_timestamp
+        author = proj_info['author'],
+    author_email = proj_info['author_email'],
+    url = proj_info['url'],
+    license = proj_info['license'],
     
-        async def async_send_message(self, message, **kwargs):
-        '''Send the message to the user.'''
-        payload = {'text': message}
+                elif opt in ('-V', '--version'):
+                # Display version.
+                log.println('you-get:', log.BOLD)
+                log.println('    version:  {}'.format(__version__))
+                if head is not None:
+                    log.println('    branch:   {}\n    commit:   {}'.format(*head))
+                else:
+                    log.println('    branch:   {}\n    commit:   {}'.format('(stable)', '(tag v{})'.format(__version__)))
     
-        def send_message(self, message='', **kwargs):
-        '''Send a message to a user.'''
-        _LOGGER.debug('Sending to: %s, %s', self._recipient, str(self._device))
-        data = {
-            'secret': self._secret,
-            'to': self._recipient,
-            'device': self._device,
-            'payload': message,
-        }
+    __all__ = ['baomihua_download', 'baomihua_download_by_id']
     
+    class BokeCC(VideoExtractor):
+    name = 'BokeCC'
     
-from homeassistant.components.notify import BaseNotificationService
+    import re
     
-        def backwards(self, orm):
-    
-            # User chose to not deal with backwards NULL issues for 'Environment.project_id'
-        raise RuntimeError(
-            'Cannot reverse this migration. 'Environment.project_id' and its values cannot be restored.'
-        )
-    
-            for release_project in RangeQuerySetWrapperWithProgressBar(
-            orm.ReleaseProject.objects.all()
-        ):
-            orm.ReleaseProject.objects.filter(id=release_project.id).update(
-                new_groups=orm.Group.objects.filter(
-                    project_id=release_project.project_id,
-                    first_release_id=release_project.release_id,
-                ).count()
-            )
-    
-            # Deleting field 'ApiToken.application'
-        db.delete_column('sentry_apitoken', 'application_id')
+        title = match1(html, r'<title>([^<]{1,9999})</title>')
     
     
-def evaluate_masks(
-    json_dataset,
-    all_boxes,
-    all_segms,
-    output_dir,
-    use_salt=True,
-    cleanup=False
-):
-    if cfg.CLUSTER.ON_CLUSTER:
-        # On the cluster avoid saving these files in the job directory
-        output_dir = '/tmp'
-    res_file = os.path.join(
-        output_dir, 'segmentations_' + json_dataset.name + '_results')
-    if use_salt:
-        res_file += '_{}'.format(str(uuid.uuid4()))
-    res_file += '.json'
+def icourses_playlist_new(url, page=None):
+    # 2 helpers using same interface in the js code
+    def to_chap(course_id, chap_id, mod):
+        ep = 'http://www.icourses.cn/jpk/viewCharacterDetail2.action?courseId={}&characId={}&mod={}'
+        req = post_content(ep.format(course_id, chap_id, mod), post_data={})
+        return req
+    
+    for face_location in face_locations:
+    
+            if file.filename == '':
+            return redirect(request.url)
+    
+        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
+    context = multiprocessing
+    if 'forkserver' in multiprocessing.get_all_start_methods():
+        context = multiprocessing.get_context('forkserver')
+    
+            # Now let's list all the faces we found in all 128 frames
+        for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
+            number_of_faces_in_frame = len(face_locations)
+    
+        def test_fd_command_line_interface_cnn_model(self):
+        target_string = 'obama.jpg'
+        runner = CliRunner()
+        image_file = os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg')
     
     
-def get_devkit_dir(name):
-    '''Retrieve the devkit dir for the dataset.'''
-    return _DATASETS[name][_DEVKIT_DIR]
+def caesar_decode(n=0, text=''):
+  return caesar_encode(-n, text)
     
+    '''As a simple example of Python Fire, a Widget serves no clear purpose.'''
     
-# ---------------------------------------------------------------------------- #
-# Box heads
-# ---------------------------------------------------------------------------- #
+    '''Tests for the widget module.'''
     
-    
-def add_single_scale_rpn_losses(model):
-    '''Add losses for a single scale RPN model (i.e., no FPN).'''
-    # Spatially narrow the full-sized RPN label arrays to match the feature map
-    # shape
-    model.net.SpatialNarrowAs(
-        ['rpn_labels_int32_wide', 'rpn_cls_logits'], 'rpn_labels_int32'
-    )
-    for key in ('targets', 'inside_weights', 'outside_weights'):
-        model.net.SpatialNarrowAs(
-            ['rpn_bbox_' + key + '_wide', 'rpn_bbox_pred'], 'rpn_bbox_' + key
-        )
-    loss_rpn_cls = model.net.SigmoidCrossEntropyLoss(
-        ['rpn_cls_logits', 'rpn_labels_int32'],
-        'loss_rpn_cls',
-        scale=model.GetLossScale()
-    )
-    loss_rpn_bbox = model.net.SmoothL1Loss(
-        [
-            'rpn_bbox_pred', 'rpn_bbox_targets', 'rpn_bbox_inside_weights',
-            'rpn_bbox_outside_weights'
-        ],
-        'loss_rpn_bbox',
-        beta=1. / 9.,
-        scale=model.GetLossScale()
-    )
-    loss_gradients = blob_utils.get_loss_gradients(
-        model, [loss_rpn_cls, loss_rpn_bbox]
-    )
-    model.AddLosses(['loss_rpn_cls', 'loss_rpn_bbox'])
-    return loss_gradients
-
-    
-    
-# octave and aspect fields are only used on RetinaNet. Octave corresponds to the
-# scale of the anchor and aspect denotes which aspect ratio is used in the range
-# of aspect ratios
-FieldOfAnchors = namedtuple(
-    'FieldOfAnchors', [
-        'field_of_anchors', 'num_cell_anchors', 'stride', 'field_size',
-        'octave', 'aspect'
-    ]
-)
-    
-    '''Construct minibatches for Mask R-CNN training when keypoints are enabled.
-Handles the minibatch blobs that are specific to training Mask R-CNN for
-keypoint detection. Other blobs that are generic to RPN or Fast/er R-CNN are
-handled by their respecitive roi_data modules.
-'''
-    
-    
-def get_minibatch_blob_names(is_training=True):
-    '''Return blob names in the order in which they are read by the data loader.
-    '''
-    # data blob: holds a batch of N images, each with 3 channels
-    blob_names = ['data']
-    if cfg.RPN.RPN_ON:
-        # RPN-only or end-to-end Faster R-CNN
-        blob_names += rpn_roi_data.get_rpn_blob_names(is_training=is_training)
-    elif cfg.RETINANET.RETINANET_ON:
-        blob_names += retinanet_roi_data.get_retinanet_blob_names(
-            is_training=is_training
-        )
+      Args:
+    fn: The function to be used for parsing arguments.
+    *arguments: The arguments for which to use the parse fn. If none are listed,
+      then this will set the default parse function.
+  Returns:
+    The decorated function, which now has metadata telling Fire how to perform.
+  '''
+  def _Decorator(func):
+    parse_fns = GetParseFns(func)
+    if not arguments:
+      parse_fns['default'] = fn
     else:
-        # Fast R-CNN like models trained on precomputed proposals
-        blob_names += fast_rcnn_roi_data.get_fast_rcnn_blob_names(
-            is_training=is_training
-        )
-    return blob_names
+      for argument in arguments:
+        parse_fns['named'][argument] = fn
+    _SetMetadata(func, FIRE_PARSE_FNS, parse_fns)
+    return func
     
-    # Example usage:
-# data_loader_benchmark.par \
-#   NUM_GPUS 2 \
-#   TRAIN.DATASETS '('voc_2007_trainval',)' \
-#   TRAIN.PROPOSAL_FILES /path/to/voc_2007_trainval/proposals.pkl \
-#   DATA_LOADER.NUM_THREADS 4 \
-#   DATA_LOADER.MINIBATCH_QUEUE_SIZE 64 \
-#   DATA_LOADER.BLOBS_QUEUE_CAPACITY 8
+    import mock
+    
+      def test_one_line_simple_whitespace(self):
+    docstring = '''
+      A simple one line docstring.
+    '''
+    docstring_info = docstrings.parse(docstring)
+    expected_docstring_info = DocstringInfo(
+        summary='A simple one line docstring.',
+    )
+    self.assertEqual(docstring_info, expected_docstring_info)
+    
+      def testTabCompletionWithDict(self):
+    actions = {'multiply': lambda a, b: a * b}
+    completion_script = fire.Fire(
+        actions, command=['--', '--completion'], name='actCLI')
+    self.assertIn('actCLI', completion_script)
+    self.assertIn('multiply', completion_script)
+    
+      This method generates `branch_factor` branches for each sequence in the beam,
+  where each branch extends the event sequence by `num_steps` steps (via calls
+  to `generate_step_fn`). The resulting beam is returned.
+    
+      def testSleeper_SleepUntil(self):
+    # Burn in.
+    for _ in range(10):
+      concurrency.Sleeper().sleep(.01)
+    
+    
+def extract_state(batched_states, i):
+  '''Extracts a single state from a batch of states.
+    
+      def test_Unbatch(self):
+    unbatched_states = state_util.unbatch(self._batched_states, batch_size=2)
+    
+      Returns:
+    A merged tf.HParams object with the hyperparameters from both `hparams_1`
+    and `hparams_2`.
+  '''
+  hparams_map = hparams_1.values()
+  hparams_map.update(hparams_2.values())
+  return tf.contrib.training.HParams(**hparams_map)
+    
+        for text in TEXTS:
+        doc = nlp(text)
+        relations = extract_currency_relations(doc)
+        for r1, r2 in relations:
+            print('{:<10}\t{}\t{}'.format(r1.text, r2.ent_type_, r2.text))
+    
+    import plac
+from spacy.lang.en import English
+from spacy.tokens import Doc, Span
+from spacy import displacy
+from pathlib import Path
+    
+        doc = nlp(text)
+    print('Pipeline', nlp.pipe_names)  # pipeline contains component name
+    print('Tokens', [t.text for t in doc])  # company names from the list are merged
+    print('Doc has_tech_org', doc._.has_tech_org)  # Doc contains tech orgs
+    print('Token 0 is_tech_org', doc[0]._.is_tech_org)  # 'Alphabet Inc.' is a tech org
+    print('Token 1 is_tech_org', doc[1]._.is_tech_org)  # 'is' is not
+    print('Entities', [(e.text, e.label_) for e in doc.ents])  # all orgs are entities
+    
+        print('Building Tensorboard Projector metadata for ({}) vectors: {}'.format(vector_count, out_meta_file))
+    
+    
+def test():
+    with virtualenv(VENV_DIR):
+        with lcd(path.dirname(__file__)):
+            local('py.test -x spacy/tests')
+
+    
+    
+def get_model_links(compat):
+    links = {}
+    data_path = get_data_path()
+    if data_path:
+        models = [p for p in data_path.iterdir() if is_model_path(p)]
+        for model in models:
+            meta_path = Path(model) / 'meta.json'
+            if not meta_path.exists():
+                continue
+            meta = read_json(meta_path)
+            link = model.parts[-1]
+            name = meta['lang'] + '_' + meta['name']
+            links[link] = {'name': name, 'version': meta['version'],
+                           'compat': is_compat(compat, name, meta['version'])}
+    return links
