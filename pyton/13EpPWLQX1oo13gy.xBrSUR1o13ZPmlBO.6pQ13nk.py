5c2ef68c6603795eb4a27e76f6265ec6a7c9a37f
SUNsung <weatherius@gmail.com>
2019-03-08 08:49:36 +0200

    Rewrite [13EpPWLQX1oo13gy.xBrSUR1o13ZPmlBO.6pQ13nk.py] File in: 2019-03-08 08:49:35

--- pyton/13EpPWLQX1oo13gy.xBrSUR1o13ZPmlBO.6pQ13nk.py
+++ pyton/13EpPWLQX1oo13gy.xBrSUR1o13ZPmlBO.6pQ13nk.py
@@ -1,179 +1,155 @@
 
         
-          labels = tf.squeeze(curr_features['rel_id'], [-1])
-  return labels
+        from ..common import *
+import urllib.parse
     
-      # Save the path embeddings
-  path_vector_filename = os.path.join(
-      embeddings_base_path, '%d_path_vectors' % model.lstm_output_dim)
-  with open(path_vector_filename, 'w') as f_out:
-    np.save(f_out, path_matrix)
-    
-      # Visualize this in the morning.
-  all_data_nxtc = np.zeros([nchannels_all, ntime * nconditions_all])
-  for name, dataset in datasets.items():
-    cidx_s = channel_idxs[name][0]
-    cidx_f = channel_idxs[name][1]
-    for cname in conditions_all[name]:
-      cidxs = np.argwhere(all_conditions_list == cname)
-      if cidxs.shape[0] > 0:
-        cidx = cidxs[0][0]
-        all_tidxs = np.arange(0, ntime+1) + cidx*ntime
-        all_data_nxtc[cidx_s:cidx_f, all_tidxs[0]:all_tidxs[-1]] = \
-            avg_data_all[name][cname].T
-    
-      Returns:
-    In the equation, y = x W + b, returns the pair (W, b).
-  '''
-    
-      def encode_chars(self, sentence):
-    chars_ids = [self.word_to_char_ids(cur_word)
-                 for cur_word in sentence.split()]
-    return np.vstack([self.bos_chars] + chars_ids + [self.eos_chars])
-    
-      for batch in range(num_batches):
-    x = np.zeros([batch_size, num_steps], dtype=np.int32)
-    y = np.zeros([batch_size, num_steps], dtype=np.int32)
-    w = np.zeros([batch_size, num_steps], dtype=np.float)
-    
-    
-def wasserstein_discriminator_loss(real_values, fake_values):
-  '''Wasserstein discriminator loss.
-    
-      And the policy for Generator G is the log-probability of taking action x2
-  given the past context.
-    
-    
-class HTTPieHTTPAdapter(HTTPAdapter):
-    
-        def __str__(self):
-        defaults = dict(type(self).__dict__)
-        actual = dict(defaults)
-        actual.update(self.__dict__)
-        actual['config'] = self.config
-        return repr_dict_nice(dict(
-            (key, value)
-            for key, value in actual.items()
-            if not key.startswith('_'))
-        )
-    
-    
-def get_stream_type(env, args):
-    '''Pick the right stream type based on `env` and `args`.
-    Wrap it in a partial with the type-specific args so that
-    we don't need to think what stream we are dealing with.
-    
-        def get_auth(self, username=None, password=None):
-        '''
-        If `auth_parse` is set to `True`, then `username`
-        and `password` contain the parsed credentials.
-    
-    
-ENTRY_POINT_NAMES = [
-    'httpie.plugins.auth.v1',
-    'httpie.plugins.formatter.v1',
-    'httpie.plugins.converter.v1',
-    'httpie.plugins.transport.v1',
-]
-    
-        def test_POST_no_data_no_auto_headers(self, httpbin):
-        # JSON headers shouldn't be automatically set for POST with no data.
-        r = http('POST', httpbin.url + '/post')
-        assert HTTP_OK in r
-        assert ''Accept': '*/*'' in r
-        assert ''Content-Type': 'application/json' not in r
-    
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
-    
-    
-def test_follow_all_output_options_used_for_redirects(httpbin):
-    r = http('--check-status',
-             '--follow',
-             '--all',
-             '--print=H',
-             httpbin.url + '/redirect/2')
-    assert r.count('GET /') == 3
-    assert HTTP_OK not in r
-    
-        def run(self):
-        while not self._should_stop.is_set():
-            if self.status.has_finished:
-                self.sum_up()
-                break
-    
-                    _filename = newfilename
-    
-    
-root = Root()
-factory = Site(root)
-reactor.listenTCP(8880, factory)
-reactor.run()
+    site_info = 'ehow.com'
+download = ehow_download
+download_playlist = playlist_not_supported('ehow')
 
     
-            if opts.loglevel:
-            self.settings.set('LOG_ENABLED', True, priority='cmdline')
-            self.settings.set('LOG_LEVEL', opts.loglevel, priority='cmdline')
+        def get_actiontec_data(self):
+        '''Retrieve data from Actiontec MI424WR and return parsed result.'''
+        try:
+            telnet = telnetlib.Telnet(self.host)
+            telnet.read_until(b'Username: ')
+            telnet.write((self.username + '\n').encode('ascii'))
+            telnet.read_until(b'Password: ')
+            telnet.write((self.password + '\n').encode('ascii'))
+            prompt = telnet.read_until(
+                b'Wireless Broadband Router> ').split(b'\n')[-1]
+            telnet.write('firewall mac_cache_dump\n'.encode('ascii'))
+            telnet.write('\n'.encode('ascii'))
+            telnet.read_until(prompt)
+            leases_result = telnet.read_until(prompt).split(b'\n')[1:-1]
+            telnet.write('exit\n'.encode('ascii'))
+        except EOFError:
+            _LOGGER.exception('Unexpected response from router')
+            return
+        except ConnectionRefusedError:
+            _LOGGER.exception('Connection refused by router. Telnet enabled?')
+            return None
     
-                        if not cb:
-                        logger.error('Cannot find a rule that matches %(url)r in spider: %(spider)s',
-                                 {'url': response.url, 'spider': spider.name})
-                        return
-                else:
-                    cb = 'parse'
+            # parsing response
+        for info in result:
+            mac = info['macAddr']
+            name = info['hostName']
+            # No address = no item :)
+            if mac is None:
+                continue
     
     
-class ReturnsContract(Contract):
-    ''' Contract to check the output of a callback
+class ThomsonDeviceScanner(DeviceScanner):
+    '''This class queries a router running THOMSON firmware.'''
     
-            self._signer = None
-        if is_botocore():
-            import botocore.auth
-            import botocore.credentials
-            kw.pop('anon', None)
-            if kw:
-                raise TypeError('Unexpected keyword arguments: %s' % kw)
-            if not self.anon:
-                SignerCls = botocore.auth.AUTH_TYPE_MAPS['s3']
-                self._signer = SignerCls(botocore.credentials.Credentials(
-                    aws_access_key_id, aws_secret_access_key))
-        else:
-            _S3Connection = _get_boto_connection()
-            try:
-                self.conn = _S3Connection(
-                    aws_access_key_id, aws_secret_access_key, **kw)
-            except Exception as ex:
-                raise NotConfigured(str(ex))
+    _LOGGER = logging.getLogger(__name__)
     
-        # Gloss the lips
-    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
-    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
-    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
-    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
     
-        # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+class DemoNotificationService(BaseNotificationService):
+    '''Implement demo notification service.'''
     
-    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
-# See http://pillow.readthedocs.io/ for more about PIL/Pillow
-pil_image = Image.fromarray(unknown_image)
-# Create a Pillow ImageDraw Draw instance to draw with
-draw = ImageDraw.Draw(pil_image)
+    _LOGGER = logging.getLogger(__name__)
     
-    test_requirements = [
-    'tox',
-    'flake8==2.6.0'
-]
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_APP_NAME, default=DEFAULT_APP_NAME): cv.string,
+    vol.Optional(CONF_APP_ICON): vol.Url,
+    vol.Optional(CONF_HOSTNAME, default=DEFAULT_HOST): cv.string,
+    vol.Optional(CONF_PASSWORD): cv.string,
+    vol.Optional(CONF_PORT, default=DEFAULT_PORT): cv.port,
+})
     
-    # 你需要一个2代以上的树莓派，并在树莓派上安装face_recognition，并连接上picamera摄像头
-# 并确保picamera这个模块已经安装（树莓派一般会内置安装）
-# 你可以参考这个教程配制你的树莓派：
-# https://gist.github.com/ageitgey/1ac8dbe8572f3f533df6269dab35df65
+        #Writing names of all directories inside 'pages' to a list
+    for os_dir in os.listdir(loc):
+        oslist.append(os_dir)
+    
+    
+def basic_gn_shortcut(model, prefix, blob_in, dim_in, dim_out, stride):
+    if dim_in == dim_out:
+        return blob_in
+    
+            kernel_size = up_scale * 2
+        bil_filt = upsample_filt(kernel_size)
+    
+    '''Handle mapping from old network building function names to new names.
+    
+    
+def get_retinanet_bias_init(model):
+    '''Initialize the biases for the conv ops that predict class probabilities.
+    Initialization is performed such that at the start of training, all
+    locations are predicted to be background with high probability
+    (e.g., ~0.99 = 1 - cfg.RETINANET.PRIOR_PROB). See the Focal Loss paper for
+    details.
+    '''
+    prior_prob = cfg.RETINANET.PRIOR_PROB
+    scales_per_octave = cfg.RETINANET.SCALES_PER_OCTAVE
+    aspect_ratios = len(cfg.RETINANET.ASPECT_RATIOS)
+    if cfg.RETINANET.SOFTMAX:
+        # Multiclass softmax case
+        bias = np.zeros((model.num_classes, 1), dtype=np.float32)
+        bias[0] = np.log(
+            (model.num_classes - 1) * (1 - prior_prob) / (prior_prob)
+        )
+        bias = np.vstack(
+            [bias for _ in range(scales_per_octave * aspect_ratios)]
+        )
+        bias_init = (
+            'GivenTensorFill', {
+                'values': bias.astype(dtype=np.float32)
+            }
+        )
+    else:
+        # Per-class sigmoid (binary classification) case
+        bias_init = (
+            'ConstantFill', {
+                'value': -np.log((1 - prior_prob) / prior_prob)
+            }
+        )
+    return bias_init
+    
+        def forward(self, inputs, outputs):
+        '''See modeling.detector.GenerateProposalLabels for inputs/outputs
+        documentation.
+        '''
+        # During training we reuse the data loader code. We populate roidb
+        # entries on the fly using the rois generated by RPN.
+        # im_info: [[im_height, im_width, im_scale], ...]
+        rois = inputs[0].data
+        roidb = blob_utils.deserialize(inputs[1].data)
+        im_info = inputs[2].data
+        im_scales = im_info[:, 2]
+        output_blob_names = fast_rcnn_roi_data.get_fast_rcnn_blob_names()
+        # For historical consistency with the original Faster R-CNN
+        # implementation we are *not* filtering crowd proposals.
+        # This choice should be investigated in the future (it likely does
+        # not matter).
+        json_dataset.add_proposals(roidb, rois, im_scales, crowd_thresh=0)
+        roidb_utils.add_bbox_regression_targets(roidb)
+        blobs = {k: [] for k in output_blob_names}
+        fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
+        for i, k in enumerate(output_blob_names):
+            blob_utils.py_op_copy_blob(blobs[k], outputs[i])
+
+    
+        retnet_roi_fg_bbox_locs -> for the bbox regression, since we are only
+                               interested in regressing on fg bboxes which are
+                               M in number and the output prediction of the network
+                               is of shape N x (A * 4) x H x W
+                               (in case of non class-specific bbox), so we
+                               store the locations of positive fg boxes in this
+                               blob retnet_roi_fg_bbox_locs of shape M x 4 where
+                               each row looks like: [img_id, anchor_id, x_loc, y_loc]
+    '''
+    # im_info: (height, width, image scale)
+    blob_names = ['im_info']
+    assert cfg.FPN.FPN_ON, 'RetinaNet uses FPN for dense detection'
+    # Same format as RPN blobs, but one per FPN level
+    if is_training:
+        blob_names += ['retnet_fg_num', 'retnet_bg_num']
+        for lvl in range(cfg.FPN.RPN_MIN_LEVEL, cfg.FPN.RPN_MAX_LEVEL + 1):
+            suffix = 'fpn{}'.format(lvl)
+            blob_names += [
+                'retnet_cls_labels_' + suffix,
+                'retnet_roi_bbox_targets_' + suffix,
+                'retnet_roi_fg_bbox_locs_' + suffix,
+            ]
+    return blob_names
