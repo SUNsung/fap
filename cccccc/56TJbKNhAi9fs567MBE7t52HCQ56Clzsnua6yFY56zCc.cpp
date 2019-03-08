8fa846a007a4649b43ff42f889bd399c08c1ae8f
SUNsung <weatherius@gmail.com>
2019-03-08 09:55:35 +0200

    Rewrite [56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp] File in: 2019-03-08 09:55:35

--- cccccc/56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp
+++ cccccc/56TJbKNhAi9fs567MBE7t52HCQ56Clzsnua6yFY56zCc.cpp
@@ -1,167 +1,138 @@
 
         
-        //////////////////////////////////////////////////////////////////////
+          static void ClearCache(content::RenderProcessHost* render_view_host);
+  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
+                             const std::string& proxy_config);
     
-    //////////////////////////////////////////////////////////////////////
     
-        // GP Register cannot be greater than 31
-    assert(static_cast<uint32_t>(s) < 32);
-    assert(static_cast<uint32_t>(ra) < 32);
-    assert(static_cast<uint32_t>(rb) < 32);
-    
-    Variant PlainDirectory::read() {
-  struct dirent entry;
-  struct dirent *result;
-  int ret = readdir_r(m_dir, &entry, &result);
-  if (ret != 0 || !result) {
-    return false;
-  }
-  return String(entry.d_name, CopyString);
+void Base::Call(const std::string& method, const base::ListValue& arguments,
+                content::RenderFrameHost* rvh) {
+  NOTREACHED() << 'Uncatched call in Base'
+               << ' method:' << method
+               << ' arguments:' << arguments;
 }
     
-    #ifndef HPHP_GLOB_STREAM_WRAPPER_H
-#define HPHP_GLOB_STREAM_WRAPPER_H
-    
-    
-    {  logAHMSubMapWarning(mapName);
-}
-    
-    #include <folly/String.h>
-    
-      char *end = nullptr;
-  long nFD = strtol(sFD, &end, 10);
-  if ((sFD == end) || (*end != '\0')) {
-    raise_warning('php://fd/ stream must be specified in the form '
-                  'php://fd/<orig fd>');
-    return nullptr;
-  }
-  long dtablesize = getdtablesize();
-  if ((nFD < 0) || (nFD >= dtablesize)) {
-    raise_warning('The file descriptors must be non-negative numbers '
-                  'smaller than %ld', dtablesize);
-    return nullptr;
-  }
-    
-    #include 'unicode/scientificnumberformatter.h'
-#include 'unicode/dcfmtsym.h'
-#include 'unicode/fpositer.h'
-#include 'unicode/utf16.h'
-#include 'unicode/uniset.h'
-#include 'decfmtst.h'
-#include 'unicode/decimfmt.h'
-    
-    int32_t SearchIterator::first(UErrorCode &status)
-{
-    if (U_FAILURE(status)) {
-        return USEARCH_DONE;
-    }
-    setOffset(0, status);
-    return handleNext(0, status);
+    namespace content {
+class RenderFrameHost;
 }
     
     
-    {    BreakIterator *get() const { return ptr; }
-    BreakIterator *operator->() const { return ptr; }
-    BreakIterator &operator*() const { return *ptr; }
-private:
-    BreakIterator *ptr;
-    SharedBreakIterator(const SharedBreakIterator &);
-    SharedBreakIterator &operator=(const SharedBreakIterator &);
-};
-    
-    class U_I18N_API SharedNumberFormat : public SharedObject {
-public:
-    SharedNumberFormat(NumberFormat *nfToAdopt) : ptr(nfToAdopt) { }
-    virtual ~SharedNumberFormat();
-    const NumberFormat *get() const { return ptr; }
-    const NumberFormat *operator->() const { return ptr; }
-    const NumberFormat &operator*() const { return *ptr; }
-private:
-    NumberFormat *ptr;
-    SharedNumberFormat(const SharedNumberFormat &);
-    SharedNumberFormat &operator=(const SharedNumberFormat &);
-};
-    
-    class U_I18N_API SharedPluralRules : public SharedObject {
-public:
-    SharedPluralRules(PluralRules *prToAdopt) : ptr(prToAdopt) { }
-    virtual ~SharedPluralRules();
-    const PluralRules *operator->() const { return ptr; }
-    const PluralRules &operator*() const { return *ptr; }
-private:
-    PluralRules *ptr;
-    SharedPluralRules(const SharedPluralRules &);
-    SharedPluralRules &operator=(const SharedPluralRules &);
-};
-    
-    #endif  // __SIGNIFICANTDIGITINTERVAL_H__
+    {}  // namespace nwapi
 
     
+    Menu::~Menu() {
+  Destroy();
+}
     
-/**
- * A formatter for small, positive integers.
- */
-class U_I18N_API SmallIntFormatter : public UMemory {
-public:
-    /**
-     * Estimates the actual digit count needed to format positiveValue
-     * using the given range of digit counts.
-     * Returns a value that is at least the actual digit count needed.
-     *
-     * @param positiveValue the value to format
-     * @param range the acceptable range of digit counts.
-     */
-    static int32_t estimateDigitCount(
-            int32_t positiveValue, const IntDigitCountRange &range);
+    void MenuItem::SetSubmenu(Menu* sub_menu) {
+  submenu_ = sub_menu;
+  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    sub_menu->UpdateKeys(gtk_accel_group);
+  }
+  if (sub_menu == NULL)
+    gtk_menu_item_remove_submenu(GTK_MENU_ITEM(menu_item_));
+  else
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item_), sub_menu->menu_);
+}
+    
+      option.GetString('type', &type_);
+  option.GetString('label', &label_);
+  option.GetString('tooltip', &tooltip_);
+  option.GetBoolean('checked', &is_checked_);
+  option.GetBoolean('enabled', &is_enabled_);
+    
+    ExtensionFunction::ResponseAction
+NwAppCrashBrowserFunction::Run() {
+  int* ptr = nullptr;
+  *ptr = 1;
+  return RespondNow(NoArguments());
+}
+    
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.App.clearAppCache', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwAppClearAppCacheFunction);
+};
+    
+    namespace extensions {
     }
     
-    U_NAMESPACE_END
+    #include <vector>
     
-        /**
-     * @param keyword for example 'few' or 'other'
-     * @return the plural form corresponding to the keyword, or OTHER
-     */
-    static Form orOtherFromString(const UnicodeString &keyword) {
-        return static_cast<Form>(indexOrOtherIndexFromString(keyword));
-    }
+    NwObjAllocateIdFunction::NwObjAllocateIdFunction() {
+}
     
-            // Get the function pointer (required for any extensions)
-        auto vkCreateDebugReportCallbackEXT = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(g_Instance, 'vkCreateDebugReportCallbackEXT');
-        IM_ASSERT(vkCreateDebugReportCallbackEXT != NULL);
     
-    #include 'imgui.h'      // IMGUI_API, ImFontAtlas
+    {  bool isChild() {
+    return pid == 0;
+  }
+};
     
-            // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!), 
-        // you may need to backup/reset/restore current shader using the commented lines below.
-        //GLint last_program; 
-        //glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
-        //glUseProgram(0);
-        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-        //glUseProgram(last_program);
     
-        // Main loop
-    bool done = false;
-    while (!done)
+    {} // namespace caffe2
+
+    
+    **Result**
+    
+    workspace.RunOperatorOnce(op)
+print('Out:\n', workspace.FetchBlob('out'))
+    
+    #include 'caffe2/operators/glu_op.h'
+    
+    class GetIm2ColGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'Col2Im',
+        '',
+        std::vector<string>{GO(0), I(0)},
+        std::vector<string>{GI(0)});
+  }
+};
+REGISTER_GRADIENT(Im2Col, GetIm2ColGradient);
+    
+    
     {
-        // Poll and handle events (inputs, window resize, etc.)
-        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
-        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
-        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                done = true;
-            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
-                done = true;
-        }
-    }
+    {}}
     
-            ID3D10Texture2D *pTexture = NULL;
-        D3D10_SUBRESOURCE_DATA subResource;
-        subResource.pSysMem = pixels;
-        subResource.SysMemPitch = desc.Width * 4;
-        subResource.SysMemSlicePitch = 0;
-        g_pd3dDevice->CreateTexture2D(&desc, &subResource, &pTexture);
+    void Assembler::addi(const Reg64& rt, const Reg64& ra, Immed imm) {
+  assert(imm.fits(HPHP::sz::word) && 'Immediate is too big');
+  EmitDForm(14, rn(rt), rn(ra), imm.w());
+}
+    
+      void EmitXX2Form(const uint8_t op,
+                   const RegNumber t,
+                   const uint8_t uim,
+                   const RegNumber b,
+                   const uint16_t xo,
+                   const bool bx,
+                   const bool tx)  {
+    XX2_form_t xx2_formater {{
+      tx,
+      bx,
+      xo,
+      static_cast<uint32_t>(b),
+      static_cast<uint32_t>(uim & 0x3),
+      static_cast<uint32_t>(t),
+      op
+    }};
+    dword(xx2_formater.instruction);
+  }
+    
+    IMPLEMENT_RESOURCE_ALLOCATION(PlainDirectory)
+    
+      // implementing File
+  bool open(const String& filename, const String& mode) override;
+  bool close() override;
+  int64_t readImpl(char *buffer, int64_t length) override;
+  int getc() override;
+  int64_t writeImpl(const char *buffer, int64_t length) override;
+  bool seek(int64_t offset, int whence = SEEK_SET) override;
+  int64_t tell() override;
+  bool eof() override;
+  bool rewind() override;
+  bool flush() override;
+  bool truncate(int64_t size) override;
+    
+    namespace HPHP {
+    }
