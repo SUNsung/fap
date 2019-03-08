ad8898c672bb9d6200b53234c9608ffefbd6de1b
SUNsung <weatherius@gmail.com>
2019-03-08 17:44:28 +0200

    Rewrite [59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb] File in: 2019-03-08 17:44:27

--- ruby/59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb
+++ ruby/59XR3FbQDHGZM59hxa7jULiZ.w59H6OsdF9DX5Y59fog.rb
@@ -1,38 +1,90 @@
 
         
-                context 'with nested resources' do
-          before { get '/api/v2/storefront/products/#{product.id}?include=option_types,option_types.option_values' }
-    
-          it_behaves_like 'returns valid cart JSON'
+                platform_lanes.each do |lane_name, lane|
+          all << [platform, lane_name].reject(&:nil?).join(' ') unless lane.is_private
+        end
+      end
+      all
     end
     
-          it 'returns account data with included default billing address' do
-        expect(json_response['included']).to    include(have_type('address'))
-        expect(json_response['included'][0]).to eq(Spree::V2::Storefront::AddressSerializer.new(user.billing_address).as_json['data'])
+    module Fastlane
+  # Handles receiving commands from the socket server, finding the Action to be invoked,
+  # invoking it, and returning any return values
+  class SocketServerActionCommandExecutor < CommandExecutor
+    attr_accessor :runner
+    attr_accessor :actions_requiring_special_handling
+    
+            cmd << ['-am #{message.shellescape}']
+        cmd << '--force' if options[:force]
+        cmd << '-s' if options[:sign]
+        cmd << tag.shellescape
+        cmd << options[:commit].to_s if options[:commit]
+    
+          it 'handles the extensions parameter with multiple elements correctly' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          ensure_no_debug_code(text: 'pry', path: '.', extensions: ['m', 'h'])
+        end').runner.execute(:test)
+        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --include=\\*.{m,h}')
       end
+    
+            it 'executes the correct git command' do
+          allow(Fastlane::Actions).to receive(:sh).with('git add *.h *.m', anything).and_return('')
+          result = Fastlane::FastFile.new.parse('lane :test do
+            git_add(path: #{path}, shell_escape: false)
+          end').runner.execute(:test)
+        end
+      end
+    
+        it 'raises when no argument passed' do
+      expect do
+        command_from_args
+      end.to raise_error(ArgumentError)
     end
     
-          it 'with success' do
-        expect(order.line_items.count).to eq(2)
-        expect(order.line_items.last.variant).to eq(variant)
-        expect(order.line_items.last.quantity).to eq(5)
-        expect(json_response['included']).to include(have_type('variant').and(have_id(variant.id.to_s)))
-      end
+    exec_arr = ['fastlane', tool_name] + ARGV
     
-          it 'returns country by iso' do
-        expect(json_response['data']).to have_id(country.id.to_s)
-        expect(json_response['data']).to have_attribute(:iso).with_value(country.iso)
-        expect(json_response['data']).to have_attribute(:iso3).with_value(country.iso3)
-        expect(json_response['data']).to have_attribute(:iso_name).with_value(country.iso_name)
-        expect(json_response['data']).to have_attribute(:name).with_value(country.name)
-        expect(json_response['data']).to have_attribute(:default).with_value(country == Spree::Country.default)
-        expect(json_response['data']).to have_attribute(:states_required).with_value(country.states_required)
-        expect(json_response['data']).to have_attribute(:zipcode_required).with_value(country.zipcode_required)
-      end
-    end
+      def prefixes
+    prefixes = ['/bin', '/usr/bin', '/usr/libexec', xcode_app_path]
+    prefixes << `brew --prefix`.strip unless `which brew`.strip.empty?
     
-        context 'as a guest user with token' do
-      before { get '/api/v2/storefront/order_status/#{order.number}', headers: headers_order_token }
+          private
     
-                resource
+    Liquid::Template.register_tag('gist', Jekyll::GistTag)
+Liquid::Template.register_tag('gistnocache', Jekyll::GistTagNoCache)
+
+    
+    Liquid::Template.register_tag('img', Jekyll::ImageTag)
+
+    
+            Dir.chdir(includes_dir) do
+          choices = Dir['**/*'].reject { |x| File.symlink?(x) }
+          if choices.include?(file)
+            source = File.read(file)
+            partial = Liquid::Template.parse(source)
+            context.stack do
+              rtn = rtn + partial.render(context)
+            end
+          else
+            rtn = rtn + 'Included file '#{file}' not found in _includes directory'
           end
+        end
+      end
+      rtn
+    end
+  end
+    
+    class PaperclipGenerator < ActiveRecord::Generators::Base
+  desc 'Create a migration to add paperclip-specific fields to your model. ' +
+       'The NAME argument is the name of your model, and the following ' +
+       'arguments are the name of the attachments'
+    
+    # The base module that gets included in ActiveRecord::Base. See the
+# documentation for Paperclip::ClassMethods for more useful information.
+module Paperclip
+  extend Helpers
+  extend Logger
+  extend ProcessorHelpers
+    
+        def self.names_for(klass)
+      instance.names_for(klass)
+    end
