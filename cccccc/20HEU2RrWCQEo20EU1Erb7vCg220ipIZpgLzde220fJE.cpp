cbcb073b805f178923f245704f873edb974566d7
SUNsung <weatherius@gmail.com>
2019-03-09 00:59:03 +0200

    Rewrite [20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp] File in: 2019-03-09 00:59:03

--- cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
+++ cccccc/20HEU2RrWCQEo20EU1Erb7vCg220ipIZpgLzde220fJE.cpp
@@ -1,190 +1,297 @@
 
         
-        
-    {
-    {   private:
-    friend class CudaModule;
-    /*!
-     * \brief constructor
-     * \param mod module of this kernel
-     * \param mangled_name mangled kernel name
-     * \param signature kernel argument signature
-     */
-    Kernel(const std::shared_ptr<Chunk>& mod,
-           const std::string& mangled_name,
-           const std::vector<ArgType>& signature);
-    /*! \brief mangled kernel name */
-    std::string mangled_name_;
-    /*! \brief kernel argument signature */
-    std::vector<ArgType> signature_;
-    /*! \brief module of this kernel */
-    std::shared_ptr<Chunk> mod_;
-    /*! \brief cached kernel function on each device */
-    std::unordered_map<int, CUfunction> func_;
-  };
-  /*!
-   * \brief CudaModule constructor
-   * \param source cuda source code.
-   * \param exports export symbols before mangling.
-   */
-  CudaModule(const char* source,
-             const std::vector<std::string>& options,
-             const std::vector<std::string>& exports)
-      : ptr_(std::make_shared<Chunk>(source, options, exports)) {}
-  /*!
-   * \brief Get cuda kernal from module by name
-   * \param name kernel name
-   * \param signature kernel signature
-   * \return shared pointer to cuda kernel
-   */
-  std::shared_ptr<Kernel> GetKernel(const std::string& name,
-                                    const std::vector<ArgType>& signature);
-};
+         protected:
+  // Print: def Function(parameters):
+  void AddDefLine(const string& function_name, const string& parameters);
+  void AddDefLine(const string& parameters);
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    REGISTER_OP('Invalid')
+    .Attr('invalid attr: int32')  // invalid since the name has a space.
+    .Doc(R'doc(
+An op to test that invalid ops do not successfully generate invalid python code.
+)doc');
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
     
-    {
-    {      if (top_size > DATA) {
-        if (param_.flat) {
-          batch_data_ = TBlob(nullptr, mshadow::Shape2(batch_size_,
-                                                       channels_ * width_ * height_),
-                              cpu::kDevCPU, type_flag_);
-        } else {
-          batch_data_ = TBlob(nullptr, mxnet::TShape(top_[DATA]->shape().begin(),
-                                                     top_[DATA]->shape().end()),
-                              cpu::kDevCPU, type_flag_);
-        }
-      }
-      out_.data.clear();
-      if (top_size > LABEL) {
-          batch_label_ = TBlob(nullptr, mxnet::TShape(top_[LABEL]->shape().begin(),
-                                                      top_[LABEL]->shape().end()),
-                               cpu::kDevCPU, type_flag_);
-      }
-      out_.batch_size = batch_size_;
-    }
-  }
+    {}  // namespace tensorflow
     
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    {  g.attrs['storage_id'] = std::make_shared<nnvm::any>(std::move(storage_id));
-  g.attrs['storage_inplace_index'] = std::make_shared<nnvm::any>(
-      std::move(storage_inplace_index));
-  g.attrs['addto_entry'] = std::make_shared<nnvm::any>(std::move(addto_entry));
-  g.attrs['skip_plus_node'] = std::make_shared<nnvm::any>(std::move(skip_plus_node));
-  return g;
-}
-    
-    namespace mxnet {
-    }
-    
-    
-    {
-    {// for reverse seq
-DECL_SCALAR(DEVICE, Plus, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Minus, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Mul, EvalScalar_, false)
-DECL_SCALAR(DEVICE, Div, EvalScalar_, false)
-}  // namespace ndarray
-}  // namespace mxnet
-    
-    #include <algorithm>
-#include <vector>
-#include './bilinear_sampler-inl.h'
-namespace mxnet {
-namespace op {
-#if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 5
-template<typename DType>
-class CuDNNBilinearSamplerOp : public Operator {
- public:
-  explicit CuDNNBilinearSamplerOp(BilinearSamplerParam param) {
-    this->param_ = param;
-    init_cudnn_ = false;
-    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
-    sampler_ = CUDNN_SAMPLER_BILINEAR;
-  }
-    }
-    }
-    }
-    
-    /**
- * Factory method
- */
-static Transliterator* RemoveTransliterator_create(const UnicodeString& /*ID*/,
-                                                   Transliterator::Token /*context*/) {
-    /* We don't need the ID or context. We just remove data */
-    return new RemoveTransliterator();
-}
-    
-    void
-SelectFormat::parseObject(const UnicodeString& /*source*/,
-                        Formattable& /*result*/,
-                        ParsePosition& pos) const
-{
-    // Parsing not supported.
-    pos.setErrorIndex(pos.getIndex());
-}
-    
-    #if !UCONFIG_NO_FORMATTING
-    
-    U_NAMESPACE_END
-    
-    uint8_t *CollationKey::reallocate(int32_t newCapacity, int32_t length) {
-    uint8_t *newBytes = static_cast<uint8_t *>(uprv_malloc(newCapacity));
-    if(newBytes == NULL) { return NULL; }
-    if(length > 0) {
-        uprv_memcpy(newBytes, getBytes(), length);
-    }
-    if(fFlagAndLength < 0) { uprv_free(fUnion.fFields.fBytes); }
-    fUnion.fFields.fBytes = newBytes;
-    fUnion.fFields.fCapacity = newCapacity;
-    fFlagAndLength |= 0x80000000;
-    return newBytes;
-}
-    
-        /**
-     * Implement UnicodeFunctor
-     */
-    virtual void setData(const TransliterationRuleData*);
-    
-    
-void printBool(bool res){
-    cout << (res ? 'True' : 'False') << endl;
-}
-    
-    
-    {    return;
-}
-    
-    
-    {private:
-    int nextDifferentCharacterIndex(const vector<int> &nums, int p){
-        for( ; p < nums.size() ; p ++ )
-            if( nums[p] != nums[p - 1] )
-                break;
-        return p;
-    }
-};
-    
-    
-    {    return 0;
-}
+    #endif  // TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
 
     
-    // Recursive
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-public:
-    vector<int> preorderTraversal(TreeNode* root) {
-    }
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+            wr = src_width / dst_width
+        hr = src_height / dst_height
+    */
+    bool isResizeNearestNeighborSupported(const Size2D &ssize, u32 elemSize);
+    void resizeNearestNeighbor(const Size2D &ssize, const Size2D &dsize,
+                               const void * srcBase, ptrdiff_t srcStride,
+                               void * dstBase, ptrdiff_t dstStride,
+                               f32 wr, f32 hr, u32 elemSize);
+    
+        void operator() (const typename internal::VecTraits<T>::vec128 & v_src0,
+                     const typename internal::VecTraits<T>::vec128 & v_src1,
+                     typename internal::VecTraits<T>::vec128 & v_dst) const
+    {
+        v_dst = internal::vqaddq(v_src0, v_src1);
     }
     
-    // Recursive
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-public:
-    vector<int> postorderTraversal(TreeNode* root) {
-    }
+        std::vector<u8> _tmp;
+    u8 *tmp = 0;
+    if (borderType == BORDER_MODE_CONSTANT)
+    {
+        _tmp.assign(colsn + 2*cn, borderValue);
+        tmp = &_tmp[cn];
     }
     
-    using namespace std;
+    namespace CAROTENE_NS {
+    }
+    
+    void assertSupportedConfiguration(bool parametersSupported = true);
+    
+                uint32x4_t vs4 = vpaddlq_u16(vpaddlq_u8(vs));
+            uint32x2_t vs2 = vadd_u32(vget_low_u32(vs4), vget_high_u32(vs4));
+    
+    
+    {    minLocCount >>= 1;
+    maxLocCount >>= 1;
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)minVal;
+    (void)minLocPtr;
+    (void)minLocCount;
+    (void)minLocCapacity;
+    (void)maxVal;
+    (void)maxLocPtr;
+    (void)maxLocCount;
+    (void)maxLocCapacity;
+#endif
+}
+    
+    
+    {
+    {
+    {             vec128  vs = internal::vld1q( src + i);
+             vec128 vr1 = internal::vld1q(rng1 + i);
+             vec128 vr2 = internal::vld1q(rng2 + i);
+            uvec128 vd1 = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
+                     vs = internal::vld1q( src + i + 16/sizeof(T));
+                    vr1 = internal::vld1q(rng1 + i + 16/sizeof(T));
+                    vr2 = internal::vld1q(rng2 + i + 16/sizeof(T));
+            uvec128 vd2 = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
+            vnst(dst + i, vd1, vd2);
+        }
+        vtail<T, sizeof(T)>::inRange(src, rng1, rng2, dst, i, size.width);
+        for( ; i < size.width; i++ )
+            dst[i] = (u8)(-(rng1[i] <= src[i] && src[i] <= rng2[i]));
+    }
+}
+    
+    namespace tesseract {
+class Tesseract;
+}
+    
+    class PARA_LIST;
+class ParagraphModel;
+    
+    // Setter for the value.
+void ParamContent::SetValue(const char* val) {
+// TODO (wanke) Test if the values actually are properly converted.
+// (Quickly visible impacts?)
+  changed_ = true;
+  if (param_type_ == VT_INTEGER) {
+    iIt->set_value(atoi(val));
+  } else if (param_type_ == VT_BOOLEAN) {
+    bIt->set_value(atoi(val));
+  } else if (param_type_ == VT_DOUBLE) {
+    dIt->set_value(strtod(val, nullptr));
+  } else if (param_type_ == VT_STRING) {
+    sIt->set_value(val);
+  }
+}
+    
+      // Adds the given pix to the set of pages in the PDF file, with the given
+  // caption added to the top.
+  void AddPix(const Pix* pix, const char* caption) {
+    int depth = pixGetDepth(const_cast<Pix*>(pix));
+    int color = depth < 8 ? 1 : (depth > 8 ? 0x00ff0000 : 0x80);
+    Pix* pix_debug = pixAddSingleTextblock(
+        const_cast<Pix*>(pix), fonts_, caption, color, L_ADD_BELOW, nullptr);
+    pixaAddPix(pixa_, pix_debug, L_INSERT);
+  }
+    
+      // Solve the dynamic programming problem for the given array of points, with
+  // the given size and cost function.
+  // Steps backwards are limited to being between min_step and max_step
+  // inclusive.
+  // The return value is the tail of the best path.
+  static DPPoint* Solve(int min_step, int max_step, bool debug,
+                        CostFunc cost_func, int size, DPPoint* points);
+    
+      void PopulateCache() {
+    Random rnd(1);
+    for (int64_t i = 0; i < FLAGS_cache_size; i++) {
+      uint64_t rand_key = rnd.Next() % FLAGS_max_key;
+      // Cast uint64* to be char*, data would be copied to cache
+      Slice key(reinterpret_cast<char*>(&rand_key), 8);
+      // do insert
+      cache_->Insert(key, new char[10], 1, &deleter);
+    }
+  }
+    
+    #include 'rocksdb/status.h'
+    
+      // open DB with two column families
+  std::vector<ColumnFamilyDescriptor> column_families;
+  // have to open default column family
+  column_families.push_back(ColumnFamilyDescriptor(
+      kDefaultColumnFamilyName, ColumnFamilyOptions()));
+  // open the new one, too
+  column_families.push_back(ColumnFamilyDescriptor(
+      'new_cf', ColumnFamilyOptions()));
+  std::vector<ColumnFamilyHandle*> handles;
+  s = DB::Open(DBOptions(), kDBPath, column_families, &handles, &db);
+  assert(s.ok());
+    
+    
+    {  const char* Name() const override { return 'MyMerge'; }
+};
+    
+    #endif  // ROCKSDB_LITE
+
+    
+    // Take a default ColumnFamilyOptions 'base_options' in addition to a
+// map 'opts_map' of option name to option value to construct the new
+// ColumnFamilyOptions 'new_options'.
+//
+// Below are the instructions of how to config some non-primitive-typed
+// options in ColumnFOptions:
+//
+// * table_factory:
+//   table_factory can be configured using our custom nested-option syntax.
+//
+//   {option_a=value_a; option_b=value_b; option_c=value_c; ... }
+//
+//   A nested option is enclosed by two curly braces, within which there are
+//   multiple option assignments.  Each assignment is of the form
+//   'variable_name=value;'.
+//
+//   Currently we support the following types of TableFactory:
+//   - BlockBasedTableFactory:
+//     Use name 'block_based_table_factory' to initialize table_factory with
+//     BlockBasedTableFactory.  Its BlockBasedTableFactoryOptions can be
+//     configured using the nested-option syntax.
+//     [Example]:
+//     * {'block_based_table_factory', '{block_cache=1M;block_size=4k;}'}
+//       is equivalent to assigning table_factory with a BlockBasedTableFactory
+//       that has 1M LRU block-cache with block size equals to 4k:
+//         ColumnFamilyOptions cf_opt;
+//         BlockBasedTableOptions blk_opt;
+//         blk_opt.block_cache = NewLRUCache(1 * 1024 * 1024);
+//         blk_opt.block_size = 4 * 1024;
+//         cf_opt.table_factory.reset(NewBlockBasedTableFactory(blk_opt));
+//   - PlainTableFactory:
+//     Use name 'plain_table_factory' to initialize table_factory with
+//     PlainTableFactory.  Its PlainTableFactoryOptions can be configured using
+//     the nested-option syntax.
+//     [Example]:
+//     * {'plain_table_factory', '{user_key_len=66;bloom_bits_per_key=20;}'}
+//
+// * memtable_factory:
+//   Use 'memtable' to config memtable_factory.  Here are the supported
+//   memtable factories:
+//   - SkipList:
+//     Pass 'skip_list:<lookahead>' to config memtable to use SkipList,
+//     or simply 'skip_list' to use the default SkipList.
+//     [Example]:
+//     * {'memtable', 'skip_list:5'} is equivalent to setting
+//       memtable to SkipListFactory(5).
+//   - PrefixHash:
+//     Pass 'prfix_hash:<hash_bucket_count>' to config memtable
+//     to use PrefixHash, or simply 'prefix_hash' to use the default
+//     PrefixHash.
+//     [Example]:
+//     * {'memtable', 'prefix_hash:1000'} is equivalent to setting
+//       memtable to NewHashSkipListRepFactory(hash_bucket_count).
+//   - HashLinkedList:
+//     Pass 'hash_linkedlist:<hash_bucket_count>' to config memtable
+//     to use HashLinkedList, or simply 'hash_linkedlist' to use the default
+//     HashLinkedList.
+//     [Example]:
+//     * {'memtable', 'hash_linkedlist:1000'} is equivalent to
+//       setting memtable to NewHashLinkListRepFactory(1000).
+//   - VectorRepFactory:
+//     Pass 'vector:<count>' to config memtable to use VectorRepFactory,
+//     or simply 'vector' to use the default Vector memtable.
+//     [Example]:
+//     * {'memtable', 'vector:1024'} is equivalent to setting memtable
+//       to VectorRepFactory(1024).
+//   - HashCuckooRepFactory:
+//     Pass 'cuckoo:<write_buffer_size>' to use HashCuckooRepFactory with the
+//     specified write buffer size, or simply 'cuckoo' to use the default
+//     HashCuckooRepFactory.
+//     [Example]:
+//     * {'memtable', 'cuckoo:1024'} is equivalent to setting memtable
+//       to NewHashCuckooRepFactory(1024).
+//
+//  * compression_opts:
+//    Use 'compression_opts' to config compression_opts.  The value format
+//    is of the form '<window_bits>:<level>:<strategy>:<max_dict_bytes>'.
+//    [Example]:
+//    * {'compression_opts', '4:5:6:7'} is equivalent to setting:
+//        ColumnFamilyOptions cf_opt;
+//        cf_opt.compression_opts.window_bits = 4;
+//        cf_opt.compression_opts.level = 5;
+//        cf_opt.compression_opts.strategy = 6;
+//        cf_opt.compression_opts.max_dict_bytes = 7;
+//
+// @param base_options the default options of the output 'new_options'.
+// @param opts_map an option name to value map for specifying how 'new_options'
+//     should be set.
+// @param new_options the resulting options based on 'base_options' with the
+//     change specified in 'opts_map'.
+// @param input_strings_escaped when set to true, each escaped characters
+//     prefixed by '\' in the values of the opts_map will be further converted
+//     back to the raw string before assigning to the associated options.
+// @param ignore_unknown_options when set to true, unknown options are ignored
+//     instead of resulting in an unknown-option error.
+// @return Status::OK() on success.  Otherwise, a non-ok status indicating
+//     error will be returned, and 'new_options' will be set to 'base_options'.
+Status GetColumnFamilyOptionsFromMap(
+    const ColumnFamilyOptions& base_options,
+    const std::unordered_map<std::string, std::string>& opts_map,
+    ColumnFamilyOptions* new_options, bool input_strings_escaped = false,
+    bool ignore_unknown_options = false);
+    
+    namespace rocksdb {
+    }
+    
+      // Only used if share_table_files is set to true. If true, will consider that
+  // backups can come from different databases, hence a sst is not uniquely
+  // identifed by its name, but by the triple (file name, crc32, file length)
+  // Default: false
+  // Note: this is an experimental option, and you'll need to set it manually
+  // *turn it on only if you know what you're doing*
+  bool share_files_with_checksum;
