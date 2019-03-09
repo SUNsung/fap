be26527e61011e105225b361aa9e0de9b1fb1dcb
SUNsung <weatherius@gmail.com>
2019-03-09 08:44:27 +0200

    Rewrite [40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp] File in: 2019-03-09 08:44:27

--- cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
+++ cccccc/40VwWK4vNz1s40GzuzK05rrNQ40.jD5ztvdQY40wZo.cpp
@@ -1,45 +1,302 @@
 
         
-          if (*BufferPtr == '\n')
-    return 1;
+                ValidateType<T>(dict, typeValue, currentVersion);
     
-    %# Ignore the following admonition; it applies to the resulting .cpp file only
-//// Automatically Generated From UnicodeExtendedGraphemeClusters.cpp.gyb.
-//// Do Not Edit Directly!
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Swift.org open source project
-//
-// Copyright (c) 2014 - 2017 Apple Inc. and the Swift project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See https://swift.org/LICENSE.txt for license information
-// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
-//
-//===----------------------------------------------------------------------===//
+            dict[versionKey] = CurrentVersion();
+        dict[typeKey] = s_variableTypeValue;
+        dict[uidKey] = Uid();
+        dict[kindKey] = static_cast<size_t>(Kind());
+        dict[dataTypeKey] = static_cast<size_t>(GetDataType());
+        const auto& dynamicAxes = DynamicAxes();
+        vector<DictionaryValue> dictionaryValueVector; 
+        dictionaryValueVector.reserve(dynamicAxes.size());
+        for (const auto& axis : dynamicAxes)
+            dictionaryValueVector.push_back(axis);
     
-    // Import As Member -- attempt to import C global functions and variables as
-// members on types or instances.
-    
-    using namespace swift::driver;
-using namespace llvm::opt;
-    
-    
+        static void setupTimeout(int seconds)
     {
-    {
-    {} // end namespace toolchains
-} // end namespace driver
-} // end namespace swift
-    
-      WERD_RES *word2 = new WERD_RES(*word);
-    
-    // A collection of DocumentData that knows roughly how much memory it is using.
-// Note that while it supports background read-ahead, it assumes that a single
-// thread is accessing documents, ie it is not safe for multiple threads to
-// access different documents in parallel, as one may de-cache the other's
-// content.
-class DocumentCache {
- public:
-  explicit DocumentCache(int64_t max_memory);
-  ~DocumentCache();
+        struct sigaction action = {};
+        action.sa_handler = &CrossProcessMutex::noOpAlarmHandler;
+        sigaction(SIGALRM, &action, NULL);
+        alarm(seconds);
     }
+    
+    template <class ElemType>
+class InputValue : public InputValueBase<ElemType>, public IdentityTransformerNode
+{
+    typedef InputValueBase<ElemType> Base; UsingComputationNodeMembersBoilerplate;
+    static const std::wstring TypeName() { return L'InputValue'; }
+    }
+    
+    void appendAttribute(CharString &s, char letter, UColAttributeValue value,
+                     UErrorCode &errorCode) {
+    if(U_FAILURE(errorCode)) { return; }
+    if(!s.isEmpty()) {
+        s.append('_', errorCode);
+    }
+    static const char *valueChars = '1234...........IXO..SN..LU......';
+    s.append(letter, errorCode);
+    s.append(valueChars[value], errorCode);
+}
+    
+    
+    
+    //-------------------------------------------------------------------------------
+//
+//  ScriptSet - A bit set representing a set of scripts.
+//
+//              This class was originally used exclusively with script sets appearing
+//              as part of the spoof check whole script confusable binary data. Its
+//              use has since become more general, but the continued use to wrap
+//              prebuilt binary data does constrain the design.
+//
+//-------------------------------------------------------------------------------
+class U_I18N_API ScriptSet: public UMemory {
+  public:
+    ScriptSet();
+    ScriptSet(const ScriptSet &other);
+    ~ScriptSet();
+    }
+    
+    int32_t SearchIterator::getMatchedLength() const
+{
+    return m_search_->matchedLength;
+}
+    
+void SearchIterator::getMatchedText(UnicodeString &result) const
+{
+    int32_t matchedindex  = m_search_->matchedIndex;
+    int32_t     matchedlength = m_search_->matchedLength;
+    if (matchedindex != USEARCH_DONE && matchedlength != 0) {
+        result.setTo(m_search_->text + matchedindex, matchedlength); 
+    }
+    else {
+        result.remove();
+    }
+}
+    
+void SearchIterator::setBreakIterator(BreakIterator *breakiter, 
+                                      UErrorCode &status)
+{
+    if (U_SUCCESS(status)) {
+#if 0
+        m_search_->breakIter = NULL;
+        // the c++ breakiterator may not make use of ubreakiterator.
+        // so we'll have to keep track of it ourselves.
+#else
+        // Well, gee... the Constructors that take a BreakIterator
+        // all cast the BreakIterator to a UBreakIterator and
+        // pass it to the corresponding usearch_openFromXXX
+        // routine, so there's no reason not to do this.
+        //
+        // Besides, a UBreakIterator is a BreakIterator, so
+        // any subclass of BreakIterator should work fine here...
+        m_search_->breakIter = (UBreakIterator *) breakiter;
+#endif
+        
+        m_breakiterator_ = breakiter;
+    }
+}
+    
+const BreakIterator * SearchIterator::getBreakIterator(void) const
+{
+    return m_breakiterator_;
+}
+    
+    
+    {    BreakIterator *get() const { return ptr; }
+    BreakIterator *operator->() const { return ptr; }
+    BreakIterator &operator*() const { return *ptr; }
+private:
+    BreakIterator *ptr;
+    SharedBreakIterator(const SharedBreakIterator &);
+    SharedBreakIterator &operator=(const SharedBreakIterator &);
+};
+    
+        // if the mode is day-of-week-in-month, calculate the day-of-month from it
+    case DOW_IN_MONTH_MODE:
+        // In this case ruleDay is the day-of-week-in-month (this code is using
+        // the dayOfWeek and dayOfMonth parameters to figure out the day-of-week
+        // of the first day of the month, so it's trusting that they're really
+        // consistent with each other)
+        if (ruleDay > 0)
+            ruleDayOfMonth = 1 + (ruleDay - 1) * 7 +
+                (7 + ruleDayOfWeek - (dayOfWeek - dayOfMonth + 1)) % 7;
+        
+        // if ruleDay is negative (we assume it's not zero here), we have to do
+        // the same calculation figuring backward from the last day of the month.
+        else
+        {
+            // (again, this code is trusting that dayOfWeek and dayOfMonth are
+            // consistent with each other here, since we're using them to figure
+            // the day of week of the first of the month)
+            ruleDayOfMonth = monthLen + (ruleDay + 1) * 7 -
+                (7 + (dayOfWeek + monthLen - dayOfMonth) - ruleDayOfWeek) % 7;
+        }
+        break;
+    
+    
+    {    // Always emit at least '0'
+    if (digits == 0) {
+        return appendTo.append((UChar) 0x30);
+    }
+    return appendTo.append(gDigits, ((smallPositiveValue  + 1) << 2) - digits, digits);
+}
+    
+    static void U_CALLCONV smpdtfmt_initSets(UErrorCode &status) {
+    ucln_i18n_registerCleanup(UCLN_I18N_SMPDTFMT, smpdtfmt_cleanup);
+    U_ASSERT(gStaticSets == NULL);
+    gStaticSets = new SimpleDateFormatStaticSets(status);
+    if (gStaticSets == NULL) {
+        status = U_MEMORY_ALLOCATION_ERROR;
+        return;
+    }
+}
+    
+    static const UChar gZero[] = { 0x7A, 0x65, 0x72, 0x6F };
+static const UChar gOne[] = { 0x6F, 0x6E, 0x65 };
+static const UChar gTwo[] = { 0x74, 0x77, 0x6F };
+static const UChar gFew[] = { 0x66, 0x65, 0x77 };
+static const UChar gMany[] = { 0x6D, 0x61, 0x6E, 0x79 };
+static const UChar gOther[] = { 0x6F, 0x74, 0x68, 0x65, 0x72 };
+    
+        /**
+     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
+     *
+     * @param keyword for example 'few' or 'other'
+     * @return the plural form corresponding to the keyword
+     */
+    static Form fromString(const UnicodeString &keyword, UErrorCode &errorCode) {
+        return static_cast<Form>(indexFromString(keyword, errorCode));
+    }
+    
+    
+    {    printf('DestroyContext()\n');
+    ImGui::DestroyContext();
+    return 0;
+}
+
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+        // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+    
+    
+    {    return stb__out - out;
+}
+
+    
+    #include 'imgui.h'
+#include 'imgui_impl_sdl.h'
+#include 'imgui_impl_vulkan.h'
+#include <stdio.h>          // printf, fprintf
+#include <stdlib.h>         // abort
+#include <SDL.h>
+#include <SDL_vulkan.h>
+#include <vulkan/vulkan.h>
+    
+    std::shared_ptr<DHTNode> DHTRoutingTable::getNode(const unsigned char* nodeID,
+                                                  const std::string& ipaddr,
+                                                  uint16_t port) const
+{
+  std::shared_ptr<DHTBucket> bucket = getBucketFor(nodeID);
+  return bucket->getNode(nodeID, ipaddr, port);
+}
+    
+      std::shared_ptr<DHTNode> localNode_;
+    
+    #endif // D_DHT_SETUP_H
+
+    
+    class DHTTask {
+public:
+  virtual ~DHTTask() = default;
+    }
+    
+    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createPeerLookupTask(
+    const std::shared_ptr<DownloadContext>& ctx, uint16_t tcpPort,
+    const std::shared_ptr<PeerStorage>& peerStorage)
+{
+  auto task = std::make_shared<DHTPeerLookupTask>(ctx, tcpPort);
+  // TODO this may be not freed by RequestGroup::releaseRuntimeResource()
+  task->setPeerStorage(peerStorage);
+  setCommonProperty(task);
+  return task;
+}
+    
+      virtual void
+  addPeriodicTask1(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+    
+    
+    {#if CC_ENABLE_SCRIPT_BINDING
+    ccScriptType _scriptType;         ///< type of script binding, lua or javascript
+#endif
+private:
+    CC_DISALLOW_COPY_AND_ASSIGN(Action);
+};
+    
+    // FIXME: Passing 'const O *' instead of 'const O&' because HASH_FIND_IT requires the address of a pointer
+// and, it is not possible to get the address of a reference
+ssize_t ActionManager::getNumberOfRunningActionsInTarget(const Node *target) const
+{
+    tHashElement *element = nullptr;
+    HASH_FIND_PTR(_targets, &target, element);
+    if (element)
+    {
+        return element->actions ? element->actions->num : 0;
+    }
+    }
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    bool SplitCols::initWithDuration(float duration, unsigned int cols)
+{
+    _cols = cols;
+    return TiledGrid3DAction::initWithDuration(duration, Size(cols, 1));
+}
+    
+        /**
+    @brief Show part of the tile.
+    @param pos The position index of the tile should be shown.
+    @param distance The percentage that the tile should be shown.
+    */
+    virtual void transformTile(const Vec2& pos, float distance);
+    
+    
+    {}
+    
+    /**
+ * @addtogroup _2d
+ * @{
+ */
+    }
+    
+    AtlasNode::AtlasNode()
+: _itemsPerRow(0)
+, _itemsPerColumn(0)
+, _itemWidth(0)
+, _itemHeight(0)
+, _textureAtlas(nullptr)
+, _isOpacityModifyRGB(false)
+, _quadsToDraw(0)
+, _uniformColor(0)
+, _ignoreContentScaleFactor(false)
+{
+}
+    
+        /** Chars per row. */
+    int    _itemsPerRow;
+    /** Chars per column. */
+    int    _itemsPerColumn;
