0984780951c63bbeef97f3a4198dccd1a8b869b1
SUNsung <weatherius@gmail.com>
2019-03-09 09:06:49 +0200

    Rewrite [23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp] File in: 2019-03-09 09:06:49

--- cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
+++ cccccc/23wo.s3A3t.6g23IGYNziHxGcE23s2EUEuwA623ZOW.cpp
@@ -1,243 +1,82 @@
 
         
-        
-    {}  // namespace mate
-    
-    namespace atom {
-    }
-    
-    namespace atom {
-    }
-    
-    #endif  // ATOM_BROWSER_LIB_POWER_OBSERVER_H_
-
-    
-    #include 'base/callback.h'
-    
-    #include 'base/threading/thread_task_runner_handle.h'
+        /*!
+ * \brief Macro to register linear updater.
+ */
+#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
+  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
+      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
+          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
+              Name)
     
     
-    {  // Produce the generic signature and environment.
-  // FIXME: Pass in a source location for the conformance, perhaps? It seems
-  // like this could fail.
-  syntheticSignature =
-    std::move(builder).computeGenericSignature(SourceLoc());
-  syntheticEnvironment = syntheticSignature->createGenericEnvironment();
-}
-
-    
-      // An AST section consists of one or more AST modules, optionally with
-  // headers. Iterate over all AST modules.
-  while (!buf.empty()) {
-    auto info = serialization::validateSerializedAST(buf);
-    }
-    
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-      static CFPointeeInfo forRecord(bool isConst, const clang::RecordDecl *decl) {
-    assert(decl);
-    CFPointeeInfo info;
-    info.IsValid = true;
-    info.IsConst = isConst;
-    info.Decl = decl;
-    return info;
-  }
-    
-    static void printNode(DemanglerPrinter &Out, const Node *node, unsigned depth) {
-  // Indent two spaces per depth.
-  for (unsigned i = 0; i < depth * 2; ++i) {
-    Out << ' ';
-  }
-  if (!node) {
-    Out << '<<NULL>>';
-    return;
-  }
-  Out << 'kind=' << getNodeKindString(node->getKind());
-  if (node->hasText()) {
-    Out << ', text=\'' << node->getText() << '\'';
-  }
-  if (node->hasIndex()) {
-    Out << ', index=' << node->getIndex();
-  }
-  Out << '\n';
-  for (auto &child : *node) {
-    printNode(Out, child, depth + 1);
-  }
-}
-    
-    static const int base         = 36;
-static const int tmin         = 1;
-static const int tmax         = 26;
-static const int skew         = 38;
-static const int damp         = 700;
-static const int initial_bias = 72;
-static const uint32_t initial_n = 128;
-    
-    namespace swift {
-namespace driver {
-namespace toolchains {
-    }
-    }
-    }
-    
-    // This is initialized with a default, stub implementation.
-// If python-google.protobuf.cc is loaded, the function pointer is overridden
-// with a full implementation.
-const Message* (*GetCProtoInsidePyProtoPtr)(PyObject* msg) =
-    GetCProtoInsidePyProtoStub;
-Message* (*MutableCProtoInsidePyProtoPtr)(PyObject* msg) =
-    MutableCProtoInsidePyProtoStub;
-    
-    bool CodeGenerator::GenerateAll(
-    const std::vector<const FileDescriptor*>& files,
-    const string& parameter,
-    GeneratorContext* generator_context,
-    string* error) const {
-  // Default implemenation is just to call the per file method, and prefix any
-  // error string with the file to provide context.
-  bool succeeded = true;
-  for (int i = 0; i < files.size(); i++) {
-    const FileDescriptor* file = files[i];
-    succeeded = Generate(file, parameter, generator_context, error);
-    if (!succeeded && error && error->empty()) {
-      *error = 'Code generator returned false but provided no error '
-               'description.';
-    }
-    if (error && !error->empty()) {
-      *error = file->name() + ': ' + *error;
-      break;
-    }
-    if (!succeeded) {
-      break;
-    }
-  }
-  return succeeded;
-}
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace java {
-    }
-    }
-    }
-    }
-    
-    // Get the name of an enum's Java class without package name prefix.
-string ClassNameWithoutPackage(const EnumDescriptor* descriptor,
-                               bool immutable) {
-  // Doesn't append 'Mutable' for enum type's name.
-  const Descriptor* message_descriptor = descriptor->containing_type();
-  if (message_descriptor == NULL) {
-    return descriptor->name();
+    {
+    {void SparsePageWriter::Alloc(std::shared_ptr<SparsePage>* out_page) {
+  CHECK(*out_page == nullptr);
+  if (num_free_buffer_ != 0) {
+    out_page->reset(new SparsePage());
+    --num_free_buffer_;
   } else {
-    return ClassNameWithoutPackage(message_descriptor, immutable) +
-           '.' + descriptor->name();
+    CHECK(qrecycle_.Pop(out_page));
   }
 }
+}  // namespace data
+}  // namespace xgboost
     
-    void OneofGenerator::GenerateClearFunctionImplementation(io::Printer* printer) {
-  printer->Print(
-      variables_,
-      'void $owning_message_class$_Clear$capitalized_name$OneOfCase($owning_message_class$ *message) {\n'
-      '  GPBDescriptor *descriptor = [message descriptor];\n'
-      '  GPBOneofDescriptor *oneof = [descriptor.oneofs objectAtIndex:$raw_index$];\n'
-      '  GPBMaybeClearOneof(message, oneof, $index$, 0);\n'
-      '}\n');
-}
+        uint16_t port;
+    auto connection = make_unique<DHTConnectionImpl>(family);
+    {
+      port = e->getBtRegistry()->getUdpPort();
+      const std::string& addr = e->getOption()->get(
+          family == AF_INET ? PREF_DHT_LISTEN_ADDR : PREF_DHT_LISTEN_ADDR6);
+      // If UDP port is already used, use the same port
+      // number. Normally IPv4 port is available, then IPv6 port is
+      // (especially for port >= 1024). We don't loose much by doing
+      // this. We did the same thing in TCP socket. See BtSetup.cc.
+      bool rv;
+      if (port == 0) {
+        auto sgl =
+            util::parseIntSegments(e->getOption()->get(PREF_DHT_LISTEN_PORT));
+        sgl.normalize();
+        rv = connection->bind(port, addr, sgl);
+      }
+      else {
+        rv = connection->bind(port, addr);
+      }
+      if (!rv) {
+        throw DL_ABORT_EX('Error occurred while binding UDP port for DHT');
+      }
+      localNode->setPort(port);
+    }
+    A2_LOG_DEBUG(fmt('Initialized local node ID=%s',
+                     util::toHex(localNode->getID(), DHT_ID_LENGTH).c_str()));
+    auto tracker = std::make_shared<DHTMessageTracker>();
+    auto routingTable = make_unique<DHTRoutingTable>(localNode);
+    auto factory = make_unique<DHTMessageFactoryImpl>(family);
+    auto dispatcher = make_unique<DHTMessageDispatcherImpl>(tracker);
+    auto receiver = make_unique<DHTMessageReceiver>(tracker);
+    auto taskQueue = make_unique<DHTTaskQueueImpl>();
+    auto taskFactory = make_unique<DHTTaskFactoryImpl>();
+    auto peerAnnounceStorage = make_unique<DHTPeerAnnounceStorage>();
+    auto tokenTracker = make_unique<DHTTokenTracker>();
+    // For now, UDPTrackerClient was enabled along with DHT
+    auto udpTrackerClient = std::make_shared<UDPTrackerClient>();
+    const auto messageTimeout =
+        e->getOption()->getAsInt(PREF_DHT_MESSAGE_TIMEOUT);
+    // wiring up
+    tracker->setRoutingTable(routingTable.get());
+    tracker->setMessageFactory(factory.get());
     
-    #include <iostream>
-#include <set>
     
-    static uint32 ComputeCRC32(const string &buf) {
-  uint32 x = ~0U;
-  for (int i = 0; i < buf.size(); ++i) {
-    unsigned char c = buf[i];
-    x = kCRC32Table[(x ^ c) & 0xff] ^ (x >> 8);
+    {} // namespace aria2
+    
+    public:
+  DHTTaskQueueImpl();
+    
+    DNSCache& DNSCache::operator=(const DNSCache& c)
+{
+  if (this != &c) {
+    entries_ = c.entries_;
   }
-  return ~x;
+  return *this;
 }
-    
-    //
-// Follow
-//
-Follow::~Follow()
-{
-    CC_SAFE_RELEASE(_followedNode);
-}
-    
-    CardinalSplineTo* CardinalSplineTo::clone() const
-{
-    // no copy constructor
-    auto a = new (std::nothrow) CardinalSplineTo();
-    a->initWithDuration(this->_duration, this->_points->clone(), this->_tension);
-    a->autorelease();
-    return a;
-}
-    
-        if (targetGrid && targetGrid->getReuseGrid() > 0)
-    {
-        if (targetGrid->isActive() && targetGrid->getGridSize().width == _gridSize.width
-            && targetGrid->getGridSize().height == _gridSize.height)
-        {
-            targetGrid->reuse();
-        }
-        else
-        {
-            CCASSERT(0, 'Invalid grid parameters!');
-        }
-    }
-    else
-    {
-        if (targetGrid && targetGrid->isActive())
-        {
-            targetGrid->setActive(false);
-        }
-    }
-    
-    
-/** @class ResizeBy
-* @brief Resize a Node object by a Size. Works on all nodes where setContentSize is effective. But it's mostly useful for nodes where 9-slice is enabled
-*/
-class CC_DLL ResizeBy : public ActionInterval 
-{
-public:
-    /**
-    * Creates the action.
-    *
-    * @param duration Duration time, in seconds.
-    * @param deltaSize The delta size.
-    * @return An autoreleased ResizeBy object.
-    */
-    static ResizeBy* create(float duration, const cocos2d::Size& deltaSize);
-    
-    //
-    // Overrides
-    //
-    virtual ResizeBy* clone() const override;
-    virtual ResizeBy* reverse() const  override;
-    virtual void startWithTarget(Node *target) override;
-    /**
-    * @param time in seconds
-    */
-    virtual void update(float time) override;
-    }
-    
-        /** 
-     * @brief Initializes with a duration and destination percentage. 
-     * @param duration Specify the duration of the ProgressTo action. It's a value in seconds.
-     * @param percent Specify the destination percentage.
-     * @return If the creation success, return true; otherwise, return false.
-     */
-    bool initWithDuration(float duration, float percent);
-    
-                if( ! spriteFrame ) {
-                CCLOG('cocos2d: AnimationCache: Animation '%s' refers to frame '%s' which is not currently in the SpriteFrameCache. This frame will not be added to the animation.', name.c_str(), spriteFrameName.c_str());
-    }
-    
-    http://www.cocos2d-x.org
