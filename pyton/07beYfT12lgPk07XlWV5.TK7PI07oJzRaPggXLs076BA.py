d97ee79c39a95601d355bbb16403e424c5181133
SUNsung <weatherius@gmail.com>
2019-03-10 14:31:05 +0200

    Rewrite [07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py] File in: 2019-03-10 14:31:04

--- pyton/07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py
+++ pyton/07beYfT12lgPk07XlWV5.TK7PI07oJzRaPggXLs076BA.py
@@ -1,155 +1,42 @@
 
         
-        # Check to make sure the RNN is the one we used in the paper.
-if N == 50:
-  assert abs(rnn['W'][0,0] - 0.06239899) < 1e-8, 'Error in random seed?'
-  rem_check = nreplications * train_percentage
-  assert  abs(rem_check - int(rem_check)) < 1e-8, \
-    'Train percentage  * nreplications should be integral number.'
+            @staticmethod
+    def generate_cert_keyfile():
+        xlog.info('generate certs's key file:%s', CertUtil.cert_keyfile)
+        pkey = OpenSSL.crypto.PKey()
+        pkey.generate_key(OpenSSL.crypto.TYPE_RSA, 2048)
+        with open(CertUtil.cert_keyfile, 'wb') as fp:
+            fp.write(OpenSSL.crypto.dump_privatekey(OpenSSL.crypto.FILETYPE_PEM, pkey))
+            fp.write(OpenSSL.crypto.dump_publickey(OpenSSL.crypto.FILETYPE_PEM, pkey))
+        CertUtil.cert_publickey = pkey
     
-    rates_train, rates_valid = split_list_by_inds(rates, train_inds, valid_inds)
-spikes_train, spikes_valid = split_list_by_inds(spikes, train_inds, valid_inds)
-condition_labels_train, condition_labels_valid = split_list_by_inds(
-    condition_labels, train_inds, valid_inds)
-ext_input_train, ext_input_valid = split_list_by_inds(
-    ext_input, train_inds, valid_inds)
+    log_file = os.path.join(data_path, 'ipv6_tunnel.log')
     
-        if FLAGS.prefix_label and use_prefix:
-      label = sequence_example.context.feature['class'].int64_list.value[0]
-      review_words = [EOS_INDEX + 1 + label]
-    else:
-      review_words = []
-    review_words.extend([
-        f.int64_list.value[0]
-        for f in sequence_example.feature_lists.feature_list['token_id'].feature
-    ])
-    all_words.append(review_words)
-  return all_words
+        m = re.match(r'(\d+)\.(\d+)(\.(\d+))?(b(\d+))?', version_str)
+    if m is None:
+        raise ValueError('Bad version string %r' % version_str)
     
-      # If there are no real or fake entries in the batch, we assign an average
-  # value of zero.
-  real_avg = tf.where(tf.equal(real_count, 0), zero_tensor, real_avg)
-  fake_avg = tf.where(tf.equal(fake_count, 0), zero_tensor, fake_avg)
+    EOF = -1
     
-      Returns:
-    values:  tf.float32 Tensor of predictions of shape [batch_size,
-      sequence_length]
-  '''
-  if FLAGS.baseline_method == 'critic':
-    if FLAGS.discriminator_model == 'seq2seq_vd':
-      values = critic_vd.critic_seq2seq_vd_derivative(
-          hparams, sequence, is_training, reuse=reuse)
-    else:
-      raise NotImplementedError
-  else:
-    raise NotImplementedError
-  return values
+    PROJ_METADATA = '%s.json' % PROJ_NAME
+    
+            self.title = match1(html, r'<meta property='og:title' content='([^']*)'')
+    
+    bokecc_download_by_id = site.download_by_id
 
     
+    from .theplatform import theplatform_download_by_pid
     
-@pytest.fixture(params=[(python_3, False),
-                        (python_3, True),
-                        (python_2, False)])
-def proc(request, spawnu, TIMEOUT):
-    container, instant_mode = request.param
-    proc = spawnu(*container)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(init_bashrc.format(
-        u'--enable-experimental-instant-mode' if instant_mode else ''))
-    proc.sendline(u'bash')
-    if instant_mode:
-        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
-    return proc
+        #if dictify(e)['ckplayer']['info'][0]['title'][0]['_text'].strip():  #duration
+        #video_dict['title'] = dictify(e)['ckplayer']['info'][0]['title'][0]['_text'].strip()
     
-    containers = (('thefuck/python3-fish',
-               u'''FROM python:3
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'),
-              ('thefuck/python2-fish',
-               u'''FROM python:2
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'))
+        fc2video_download_by_upid(upid, output_dir, merge, info_only)
     
-    
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'tcsh')
-    proc.sendline(u'setenv PYTHONIOENCODING utf8')
-    proc.sendline(u'eval `thefuck --alias`')
-    return proc
-    
-    
-@pytest.mark.parametrize('script, output', [
-    ('apt', invalid_operation('saerch')),
-    ('apt-get', invalid_operation('isntall')),
-    ('apt-cache', invalid_operation('rumove'))])
-def test_match(script, output):
-    assert match(Command(script, output))
-    
-            if 0 < self.recurrent_dropout < 1.:
-            h_tm1_i = h_tm1 * rec_dp_mask[0]
-            h_tm1_f = h_tm1 * rec_dp_mask[1]
-            h_tm1_c = h_tm1 * rec_dp_mask[2]
-            h_tm1_o = h_tm1 * rec_dp_mask[3]
-        else:
-            h_tm1_i = h_tm1
-            h_tm1_f = h_tm1
-            h_tm1_c = h_tm1
-            h_tm1_o = h_tm1
-    
-        # Arguments
-        units: Positive integer, dimensionality of the output space.
-        kernel_initializer: Initializer for the `kernel` weights matrix,
-            used for the linear transformation of the inputs.
-            (see [initializers](../initializers.md)).
-        recurrent_initializer: Initializer for the `recurrent_kernel`
-            weights matrix,
-            used for the linear transformation of the recurrent state.
-            (see [initializers](../initializers.md)).
-        bias_initializer: Initializer for the bias vector
-            (see [initializers](../initializers.md)).
-        kernel_regularizer: Regularizer function applied to
-            the `kernel` weights matrix
-            (see [regularizer](../regularizers.md)).
-        recurrent_regularizer: Regularizer function applied to
-            the `recurrent_kernel` weights matrix
-            (see [regularizer](../regularizers.md)).
-        bias_regularizer: Regularizer function applied to the bias vector
-            (see [regularizer](../regularizers.md)).
-        activity_regularizer: Regularizer function applied to
-            the output of the layer (its 'activation').
-            (see [regularizer](../regularizers.md)).
-        kernel_constraint: Constraint function applied to
-            the `kernel` weights matrix
-            (see [constraints](../constraints.md)).
-        recurrent_constraint: Constraint function applied to
-            the `recurrent_kernel` weights matrix
-            (see [constraints](../constraints.md)).
-        bias_constraint: Constraint function applied to the bias vector
-            (see [constraints](../constraints.md)).
-        return_sequences: Boolean. Whether to return the last output.
-            in the output sequence, or the full sequence.
-        return_state: Boolean. Whether to return the last state
-            in addition to the output.
-        stateful: Boolean (default False). If True, the last state
-            for each sample at index i in a batch will be used as initial
-            state for the sample of index i in the following batch.
-    '''
-    
-    '''
-Note:
-batch_size is highly sensitive.
-Only 2 epochs are needed as the dataset is very small.
-'''
+        html = get_content(url)
+    uuid_pattern = r''([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})''
+    id = r1(r'var vid='([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})'', html)
+    if id is None:
+        video_pattern = r''vid'\s*:\s*' + uuid_pattern
+        id = match1(html, video_pattern)
+    assert id, 'can't find video info'
+    return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
