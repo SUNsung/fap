32ade720357797eaaf242d3cf1df0b07fa351442
SUNsung <weatherius@gmail.com>
2019-03-10 20:22:28 +0200

    Rewrite [39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py] File in: 2019-03-10 20:22:28

--- pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
+++ pyton/39lwQMHeXxq3c3937.sL03JJ4A39vyOg4loOG7g39j1f.py
@@ -1,114 +1,80 @@
 
         
-            def ensure_present(self, enabled=True):
-        '''Ensures the rule and targets are present and synced'''
-        rule_description = self.rule.describe()
-        if rule_description:
-            # Rule exists so update rule, targets and state
-            self._sync_rule(enabled)
-            self._sync_targets()
-            self._sync_state(enabled)
-        else:
-            # Rule does not exist, so create new rule and targets
-            self._create(enabled)
+            def test_b_observers_shall_be_attachable(cls):
+        cls.s.attach(cls.dec_obs)
+        cls.assertEqual(isinstance(cls.s._observers[0], DecimalViewer), True)
+        cls.assertEqual(len(cls.s._observers), 1)
+        cls.s.attach(cls.hex_obs)
+        cls.assertEqual(isinstance(cls.s._observers[1], HexViewer), True)
+        cls.assertEqual(len(cls.s._observers), 2)
+    
+        def test_extended_property_values_cloning(self):
+        sample_object_1 = self.prototype.clone()
+        sample_object_1.some_value = 'test string'
+        sample_object_2 = self.prototype.clone()
+        self.assertRaises(AttributeError, lambda: sample_object_2.some_value)
     
     
-def copy(module, connection, name, target, bucket):
-    ''' Copy an Elasticache backup. '''
-    try:
-        response = connection.copy_snapshot(SourceSnapshotName=name,
-                                            TargetSnapshotName=target,
-                                            TargetBucket=bucket)
-        changed = True
-    except botocore.exceptions.ClientError as e:
-        module.fail_json(msg='Unable to copy the snapshot.', exception=traceback.format_exc())
-    return response, changed
-    
-    from __future__ import absolute_import, division, print_function
-__metaclass__ = type
-    
-        lambda_facts = dict()
+class OutOfData(UnpackException):
+    pass
     
     
-if __name__ == '__main__':
-    main()
-
+# List of frequently used SAS date and datetime formats
+# http://support.sas.com/documentation/cdl/en/etsug/60372/HTML/default/viewer.htm#etsug_intervals_sect009.htm
+# https://github.com/epam/parso/blob/master/src/main/java/com/epam/parso/impl/SasFileConstants.java
+sas_date_formats = ('DATE', 'DAY', 'DDMMYY', 'DOWNAME', 'JULDAY', 'JULIAN',
+                    'MMDDYY', 'MMYY', 'MMYYC', 'MMYYD', 'MMYYP', 'MMYYS',
+                    'MMYYN', 'MONNAME', 'MONTH', 'MONYY', 'QTR', 'QTRR',
+                    'NENGO', 'WEEKDATE', 'WEEKDATX', 'WEEKDAY', 'WEEKV',
+                    'WORDDATE', 'WORDDATX', 'YEAR', 'YYMM', 'YYMMC', 'YYMMD',
+                    'YYMMP', 'YYMMS', 'YYMMN', 'YYMON', 'YYMMDD', 'YYQ',
+                    'YYQC', 'YYQD', 'YYQP', 'YYQS', 'YYQN', 'YYQR', 'YYQRC',
+                    'YYQRD', 'YYQRP', 'YYQRS', 'YYQRN',
+                    'YYMMDDP', 'YYMMDDC', 'E8601DA', 'YYMMDDN', 'MMDDYYC',
+                    'MMDDYYS', 'MMDDYYD', 'YYMMDDS', 'B8601DA', 'DDMMYYN',
+                    'YYMMDDD', 'DDMMYYB', 'DDMMYYP', 'MMDDYYP', 'YYMMDDB',
+                    'MMDDYYN', 'DDMMYYC', 'DDMMYYD', 'DDMMYYS',
+                    'MINGUO')
     
-            for _port in ports:
-            monitoring_policy_port = oneandone.client.Port(
-                protocol=_port['protocol'],
-                port=_port['port'],
-                alert_if=_port['alert_if'],
-                email_notification=_port['email_notification']
-            )
-            monitoring_policy_ports.append(monitoring_policy_port)
     
+def testSignedInt():
+    check(b'\x99\xd0\x00\xd0\x80\xd0\xff\xd1\x00\x00\xd1\x80\x00'
+          b'\xd1\xff\xff\xd2\x00\x00\x00\x00\xd2\x80\x00\x00\x00'
+          b'\xd2\xff\xff\xff\xff', (0,
+                                    -128,
+                                    -1,
+                                    0,
+                                    -32768,
+                                    -1,
+                                    0,
+                                    -2147483648,
+                                    -1, ))
     
-DOCUMENTATION = '''
----
-module: vertica_facts
-version_added: '2.0'
-short_description: Gathers Vertica database facts.
-description:
-  - Gathers Vertica database facts.
-options:
-  cluster:
-    description:
-      - Name of the cluster running the schema.
-    default: localhost
-  port:
-    description:
-      Database port to connect to.
-    default: 5433
-  db:
-    description:
-      - Name of the database running the schema.
-  login_user:
-    description:
-      - The username used to authenticate with.
-    default: dbadmin
-  login_password:
-    description:
-      - The password used to authenticate with.
-notes:
-  - The default authentication assumes that you are either logging in as or sudo'ing
-    to the C(dbadmin) account on the host.
-  - This module uses C(pyodbc), a Python ODBC database adapter. You must ensure
-    that C(unixODBC) and C(pyodbc) is installed on the host and properly configured.
-  - Configuring C(unixODBC) for Vertica requires C(Driver = /opt/vertica/lib64/libverticaodbc.so)
-    to be added to the C(Vertica) section of either C(/etc/odbcinst.ini) or C($HOME/.odbcinst.ini)
-    and both C(ErrorMessagesPath = /opt/vertica/lib64) and C(DriverManagerEncoding = UTF-16)
-    to be added to the C(Driver) section of either C(/etc/vertica.ini) or C($HOME/.vertica.ini).
-requirements: [ 'unixODBC', 'pyodbc' ]
-author: 'Dariusz Owczarek (@dareko)'
-'''
+    print('\n=== Pull Request #%s ===' % pr_num)
     
-        try:
-        client.login(
-            username=module.params['ipa_user'],
-            password=module.params['ipa_pass']
-        )
-        changed, zone = ensure(module, client)
-        module.exit_json(changed=changed, zone=zone)
-    except Exception as e:
-        module.fail_json(msg=to_native(e))
+            # 3. TODO: resolve other font-relative units
+        for side in self.SIDES:
+            prop = 'border-{side}-width'.format(side=side)
+            if prop in props:
+                props[prop] = self.size_to_pt(
+                    props[prop], em_pt=font_size,
+                    conversions=self.BORDER_WIDTH_RATIOS)
+            for prop in ['margin-{side}'.format(side=side),
+                         'padding-{side}'.format(side=side)]:
+                if prop in props:
+                    # TODO: support %
+                    props[prop] = self.size_to_pt(
+                        props[prop], em_pt=font_size,
+                        conversions=self.MARGIN_RATIOS)
     
-    - bigpanda:
-    component: myapp
-    version: '1.3'
-    token: '{{ bigpanda_token }}'
-    state: finished
+            exp = pd.Index([pd.Timestamp('2011-01-01 09:00', tz=tz),
+                        pd.Timestamp('2011-01-01 10:00'),
+                        pd.Timestamp('2011-01-01 11:00', tz=tz)],
+                       dtype=object)
+        tm.assert_index_equal(
+            idx.fillna(pd.Timestamp('2011-01-01 10:00')), exp)
     
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.six.moves.urllib.parse import urlencode
-from ansible.module_utils._text import to_native
-from ansible.module_utils.urls import fetch_url
+    seg_list = jieba.cut('我来到北京清华大学', cut_all=True)
+print('Full Mode: ' + '/ '.join(seg_list))  # 全模式
     
-    # Check whether nginx is running
-- name: check nginx process
-  sensu_check:
-    name: nginx_running
-    command: /etc/sensu/plugins/processes/check-procs.rb -f /var/run/nginx.pid
-    handlers: default
-    subscribers: nginx
-    interval: 60
+    print('speed' , len(content)/tm_cost, ' bytes/second')
