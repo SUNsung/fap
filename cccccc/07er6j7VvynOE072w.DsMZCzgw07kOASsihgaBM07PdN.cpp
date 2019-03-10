3cdd3b78e850fa89023c1b6260f63c353e560c5b
SUNsung <weatherius@gmail.com>
2019-03-10 22:52:30 +0200

    Rewrite [07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp] File in: 2019-03-10 22:52:30

--- cccccc/07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp
+++ cccccc/07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp
@@ -1,182 +1,167 @@
 
         
-          uv_loop_t* loop_;
-    
-      int AddEntry(const base::DictionaryValue& entry);
-  void RemoveEntry(int id);
-    
-    
-    {}  // namespace api
-    
-    #include 'atom/browser/api/trackable_object.h'
-#include 'atom/browser/net/atom_network_delegate.h'
-#include 'native_mate/arguments.h'
-#include 'native_mate/handle.h'
-    
-    namespace atom {
+            ~reverse_lock() {
+        templock.lock();
+        templock.swap(lock);
     }
     
-    #ifndef ATOM_BROWSER_NET_URL_REQUEST_ABOUT_JOB_H_
-#define ATOM_BROWSER_NET_URL_REQUEST_ABOUT_JOB_H_
+    /**
+ * Changelog:
+ * - March 2013, Diederik Huys:    original version
+ * - November 2014, Pieter Wuille: updated to use Peter Dettman's parallel multiplication algorithm
+ * - December 2014, Pieter Wuille: converted from YASM to GCC inline assembly
+ */
     
+            secp256k1_ecmult_const(&res, &pt, &s);
+        secp256k1_ge_set_gej(&pt, &res);
+        /* Compute a hash of the point in compressed form
+         * Note we cannot use secp256k1_eckey_pubkey_serialize here since it does not
+         * expect its output to be secret and has a timing sidechannel. */
+        secp256k1_fe_normalize(&pt.x);
+        secp256k1_fe_normalize(&pt.y);
+        secp256k1_fe_get_b32(x, &pt.x);
+        y[0] = 0x02 | secp256k1_fe_is_odd(&pt.y);
     
-    {  // Verify that the size of the key space not touched by the reads
-  // is pretty much unchanged.
-  const int64_t final_other_size = Size(Key(n), Key(kCount));
-  ASSERT_LE(final_other_size, initial_other_size + 1048576);
-  ASSERT_GE(final_other_size, initial_other_size/5 - 1048576);
-}
-    
-      void RunBenchmark(int n, Slice name,
-                    void (Benchmark::*method)(ThreadState*)) {
-    SharedState shared(n);
+    /** A hasher class for RIPEMD-160. */
+class CRIPEMD160
+{
+private:
+    uint32_t s[5];
+    unsigned char buf[64];
+    uint64_t bytes;
     }
-    
-    #ifndef STORAGE_LEVELDB_DB_DB_IMPL_H_
-#define STORAGE_LEVELDB_DB_DB_IMPL_H_
-    
-    // Append the serialization of 'key' to *result.
-void AppendInternalKey(std::string* result, const ParsedInternalKey& key);
-    
-    // Return the name of the log file with the specified number
-// in the db named by 'dbname'.  The result will be prefixed with
-// 'dbname'.
-std::string LogFileName(const std::string& dbname, uint64_t number);
-    
-    class StdoutPrinter : public WritableFile {
- public:
-  virtual Status Append(const Slice& data) {
-    fwrite(data.data(), 1, data.size(), stdout);
-    return Status::OK();
-  }
-  virtual Status Close() { return Status::OK(); }
-  virtual Status Flush() { return Status::OK(); }
-  virtual Status Sync() { return Status::OK(); }
-};
     
     
     {
-    {    if (!s.ok()) {
-      assert(table == nullptr);
-      delete file;
-      // We do not cache error results so that if the error is transient,
-      // or somebody repairs the file, we recover automatically.
-    } else {
-      TableAndFile* tf = new TableAndFile;
-      tf->file = file;
-      tf->table = table;
-      *handle = cache_->Insert(key, tf, 1, &DeleteEntry);
-    }
-  }
-  return s;
+    {}}
+    
+    void Config::Bind(HackStrictOption& loc, const IniSettingMap& ini,
+                  const Hdf& config, const std::string& name /* = '' */,
+                  HackStrictOption def) {
+  // Currently this doens't bind to ini_get since it is hard to thread through
+  // an enum
+  loc = GetHackStrictOption(ini, config, name, def);
 }
     
-    #include <caffe/proto/caffe.pb.h>
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+    }
     
-    MXNET_REGISTER_IO_ITER(CaffeDataIter)
-.describe('Create MxNet iterator for a Caffe data layer.')
-.add_arguments(CaffeDataParam::__FIELDS__())
-.add_arguments(PrefetcherParam::__FIELDS__())
-.set_body([]() {
-    return new CaffeDataIterWrapper();
-});
-    
-    namespace mxnet {
-namespace io {
-/*! \return the parameter of default augmenter */
-std::vector<dmlc::ParamFieldInfo> ListDefaultAugParams();
-std::vector<dmlc::ParamFieldInfo> ListDefaultDetAugParams();
-}  // namespace io
-}  // namespace mxnet
-#endif  // MXNET_IO_IMAGE_AUGMENTER_H_
-
-    
-     protected:
-  /*! \brief prefetcher parameters */
-  PrefetcherParam param_;
-  /*! \brief backend thread */
-  dmlc::ThreadedIter<DataBatch> iter;
-  /*! \brief internal batch loader */
-  std::unique_ptr<IIterator<TBlobBatch> > loader_;
-    
-    
-    {  // sorted position of constant arguments
-  std::vector<int> const_loc;
-  for (int i = 0; i < num_const; ++i) {
-    const_loc.push_back(wrap_args[i + 3].operator int());
+    Variant PlainDirectory::read() {
+  struct dirent entry;
+  struct dirent *result;
+  int ret = readdir_r(m_dir, &entry, &result);
+  if (ret != 0 || !result) {
+    return false;
   }
-  std::sort(const_loc.begin(), const_loc.end());
-  // wrapped function
-  // This is the function that called by the user.
-  auto wrapped = [f, fset_stream, const_loc](TVMArgs args, TVMRetValue* rv) {
-    std::shared_ptr<TVMFunctor> func =
-      std::make_shared<TVMFunctor>(f, fset_stream);
-    std::vector<Engine::VarHandle> const_vars, mutate_vars;
-    func->Init(args, const_loc, &const_vars, &mutate_vars);
-    Engine *engine = Engine::Get();
-    engine->DeduplicateVarHandle(&const_vars, &mutate_vars);
-    engine->PushSync([func](RunContext ctx) {
-        func->Run(ctx);
-      }, func->ctx(), const_vars, mutate_vars);
-  };
-  *wrap_rv = PackedFunc(wrapped);
+  return String(entry.d_name, CopyString);
 }
     
-      ~CuDNNLocalResponseNormOp() {
-    if (init_cudnn_) {
-      CUDNN_CALL(cudnnDestroyLRNDescriptor(lrn_desc_));
-      CUDNN_CALL(cudnnDestroyTensorDescriptor(shape_desc_));
-    }
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
+    
+    /*
+ * Attempt to log an entry to the perf warning service.
+ *
+ * If StructuredLog::enabled() returns false or this event is discarded by the
+ * effective sample rate, nothing will be logged. If both of those checks pass,
+ * fillCols will be passed a StructuredLogEntry& to populate, which will then
+ * be logged. The column names 'event_name' and 'priority' are reserved and
+ * will be overwritten is fillCols() sets them.
+ *
+ * The effective sample rate is determined by Eval.PerfWarningSampleRate * rate
+ * (or kDefaultPerfWarningRate for the overloads that don't take a rate). If
+ * the effective sample rate is 0, all events will be discarded.
+ */
+template<typename F>
+void logPerfWarning(folly::StringPiece event, F fillCols);
+template<typename F>
+void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
+    
+      if (!strcasecmp(req, 'stdin')) {
+    return make_from(BuiltinFiles::GetSTDIN());
+  }
+  if (!strcasecmp(req, 'stdout')) {
+    return make_from(BuiltinFiles::GetSTDOUT());
+  }
+  if (!strcasecmp(req, 'stderr')) {
+    return make_from(BuiltinFiles::GetSTDERR());
+  }
+  if (!strncasecmp(req, 'fd/', sizeof('fd/') - 1)) {
+    return openFD(req + sizeof('fd/') - 1);
+  }
+  if (!strncasecmp(req, 'filter/', sizeof('filter/') - 1)) {
+    return phpStreamOpenFilter(req + sizeof('filter') - 1, mode,
+                               options, context);
   }
     
     
     {
-    {      Engine::Get()->PushSync(
-          [=](RunContext rctx) {
-            for (size_t i = 0, out_idx = 0; i < arrs.size(); i++) {
-              if (arrs[i].storage_type() == kDefaultStorage ||
-                  arrs[i].storage_type() == kUndefinedStorage)
-                continue;
-              if (output_tags.count(tags[i]) > 0) {
-                outputs[out_idx].SparseUpdateChunk(arrs[i]);
-                out_idx++;
-              }
-            }
-            ctx.async_on_complete();
-          },
-          ctx.run_ctx.ctx, vars, vars2, FnProperty::kNormal, 0,
-          'CustomOperator');
-    });
-    cv_.notify_all();
-  }
-    
-    
-    {  setup(lm_path, trie_path);
+    {        _target->setPosition(clampf(tempPos.x, _leftBoundary, _rightBoundary),
+                                   clampf(tempPos.y, _bottomBoundary, _topBoundary));
+    }
+    else
+    {
+        _target->setPosition(_halfScreenSize - _followedNode->getPosition());
+    }
 }
     
-        /**
-     * Constructs a comparator capable of comparing two n-grams.
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+    NS_CC_END
+    
+    /**
+ * @addtogroup actions
+ * @{
+ */
+    }
+    
+        /** Gets the units of time the frame takes.
      *
-     * @param order Number of words in each n-gram
+     * @return The units of time the frame takes.
      */
-    explicit Comparator(std::size_t order) : order_(order) {}
+    float getDelayUnits() const { return _delayUnits; };
     
-    template <class Payload> class NGramStreams : public util::stream::GenericStreams<NGramStream<Payload> > {
-  private:
-    typedef util::stream::GenericStreams<NGramStream<Payload> > P;
-  public:
-    NGramStreams() : P() {}
-    NGramStreams(const util::stream::ChainPositions &positions) : P(positions) {}
-};
+    /** Sets the units of time the frame takes.
+     *
+     * @param delayUnits The units of time the frame takes.
+     */
+    void setDelayUnits(float delayUnits) { _delayUnits = delayUnits; };
     
-    class Renumber {
-  public:
-    // Assumes the array is large enough to map all words and stays alive while
-    // the thread is active.
-    Renumber(const WordIndex *new_numbers, std::size_t order)
-      : new_numbers_(new_numbers), order_(order) {}
+    /** @brief Gets user information
+     * A AnimationFrameDisplayedNotification notification will be broadcast when the frame is displayed with this dictionary as UserInfo. 
+     * If UserInfo is nil, then no notification will be broadcast.
+     *
+     * @return A dictionary as UserInfo
+     */
+    const ValueMap& getUserInfo() const { return _userInfo; };
+    ValueMap& getUserInfo() { return _userInfo; };
+    
+    /** Sets user information.
+     * @param userInfo A dictionary as UserInfo.
+     */
+    void setUserInfo(const ValueMap& userInfo)
+    {
+        _userInfo = userInfo;
     }
     
-    boost::program_options::typed_value<std::string> *SizeOption(std::size_t &to, const char *default_value) {
-  return boost::program_options::value<std::string>()->notifier(SizeNotify(to))->default_value(default_value);
-}
+    // Overrides
+    virtual AnimationFrame *clone() const override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    /**
+     * @js ctor
+     */
+    AnimationFrame();
+    /**
+     * @js NA
+     * @lua NA
+     */
+    virtual ~AnimationFrame();
+    
+    /** initializes the animation frame with a spriteframe, number of delay units and a notification user info */
+    bool initWithSpriteFrame(SpriteFrame* spriteFrame, float delayUnits, const ValueMap& userInfo);
+    
+    http://www.cocos2d-x.org
