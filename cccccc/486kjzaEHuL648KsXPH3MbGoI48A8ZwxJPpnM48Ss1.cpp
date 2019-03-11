9f5d005b18be8fcdac6161577c01c988ae3e3488
SUNsung <weatherius@gmail.com>
2019-03-11 18:47:05 +0200

    Rewrite [486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp] File in: 2019-03-11 18:47:05

--- cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
+++ cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
@@ -1,240 +1,185 @@
 
         
-            QColor TextColor() const { return textColor; }
-    QColor SingleColor() const { return singleColor; }
-    
-    #endif // BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
-
-    
-        ~reverse_lock() {
-        templock.lock();
-        templock.swap(lock);
-    }
-    
-    #include <string.h>
-    
-    #endif // BITCOIN_CRYPTO_RIPEMD160_H
-
-    
-        sub	rsp,STACK_SIZE
-%ifndef LINUX
-    movdqa	[rsp + _XMM_SAVE + 0*16],xmm6	
-    movdqa	[rsp + _XMM_SAVE + 1*16],xmm7
-    movdqa	[rsp + _XMM_SAVE + 2*16],xmm8	
-    movdqa	[rsp + _XMM_SAVE + 3*16],xmm9	
-    movdqa	[rsp + _XMM_SAVE + 4*16],xmm10
-    movdqa	[rsp + _XMM_SAVE + 5*16],xmm11
-    movdqa	[rsp + _XMM_SAVE + 6*16],xmm12
-%endif
-    
-    extern 'C' {
-  PyMODINIT_FUNC INITFUNC() {
-#if PY_MAJOR_VERSION >= 3
-    PyObject *module = PyModule_Create(&_module);
-#else
-    PyObject *module = Py_InitModule3(
-        const_cast<char*>(kModuleName),
-        NULL,
-        const_cast<char*>(kModuleDocstring));
-#endif
-    if (module == NULL) {
-      return INITFUNC_ERRORVAL;
-    }
-    }
-    }
-    
-    bool AtLeastOneAnnotationMatchesSubstring(
-    const string& file_content,
-    const std::vector<const GeneratedCodeInfo::Annotation*>& annotations,
-    const string& expected_text) {
-  for (std::vector<const GeneratedCodeInfo::Annotation*>::const_iterator
-           i = annotations.begin(),
-           e = annotations.end();
-       i != e; ++i) {
-    const GeneratedCodeInfo::Annotation* annotation = *i;
-    uint32 begin = annotation->begin();
-    uint32 end = annotation->end();
-    if (end < begin || end > file_content.size()) {
-      return false;
-    }
-    if (file_content.substr(begin, end - begin) == expected_text) {
-      return true;
+          if (conformanceSig) {
+    for (auto &rawReq : conformanceSig->getRequirements()) {
+      if (auto req = rawReq.subst(conformanceToSyntheticTypeFn,
+                                  conformanceToSyntheticConformanceFn))
+        builder.addRequirement(*req, source, nullptr);
     }
   }
-  return false;
-}
     
     
-    {
-    {
-    {
-    {
-    {}  // namespace
-}  // namespace java
-}  // namespace compiler
-}  // namespace protobuf
-}  // namespace google
-
+    {  if (!wasInline) delete[] oldAllocation;
+}  
     
-    string ClassNameResolver::GetFileClassName(const FileDescriptor* file,
-                                           bool immutable) {
-  if (immutable) {
-    return GetFileImmutableClassName(file);
-  } else {
-    return 'Mutable' + GetFileImmutableClassName(file);
-  }
-}
-    
-      desired_output_for_decode = 'abcdefghIJ';
-  expected = string('\x0A\x0', 2);
-  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
-                                                     desired_output_for_decode);
-  EXPECT_EQ(expected, result);
-    
-    // =========================================================================
-    
-      // '1e' is not a valid float, but if the tokenizer reads it, it will
-  // report an error but still return it as a valid token.  We need to
-  // accept anything the tokenizer could possibly return, error or not.
-  if (*end == 'e' || *end == 'E') {
-    ++end;
-    if (*end == '-' || *end == '+') ++end;
-  }
-    
-    void Config::ParseIniFile(const std::string &filename, IniSettingMap &ini,
-                          const bool constants_only /* = false */,
-                          const bool is_system /* = true */ ) {
-    std::ifstream ifs(filename);
-    std::string str((std::istreambuf_iterator<char>(ifs)),
-                    std::istreambuf_iterator<char>());
-    std::string with_includes;
-    Config::ReplaceIncludesWithIni(filename, str, with_includes);
-    Config::SetParsedIni(ini, with_includes, filename, constants_only,
-                         is_system);
-}
-    
-      if (comma != data) {
-    // we have meta
-    ssize_t meta_len = comma - data;
-    data_len -= meta_len;
-    char* semi = (char*)memchr(data, ';', meta_len);
-    char* slash = (char*)memchr(data, '/', meta_len);
+    namespace {
     }
     
-    Variant PlainDirectory::read() {
-  struct dirent entry;
-  struct dirent *result;
-  int ret = readdir_r(m_dir, &entry, &result);
-  if (ret != 0 || !result) {
-    return false;
-  }
-  return String(entry.d_name, CopyString);
-}
-    
-    /*
- * Attempt to log an entry to the perf warning service.
- *
- * If StructuredLog::enabled() returns false or this event is discarded by the
- * effective sample rate, nothing will be logged. If both of those checks pass,
- * fillCols will be passed a StructuredLogEntry& to populate, which will then
- * be logged. The column names 'event_name' and 'priority' are reserved and
- * will be overwritten is fillCols() sets them.
- *
- * The effective sample rate is determined by Eval.PerfWarningSampleRate * rate
- * (or kDefaultPerfWarningRate for the overloads that don't take a rate). If
- * the effective sample rate is 0, all events will be discarded.
- */
-template<typename F>
-void logPerfWarning(folly::StringPiece event, F fillCols);
-template<typename F>
-void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
-    
-    int main() {
+    namespace nwapi {
     }
     
-        cout << endl;
+    #include 'base/command_line.h'
+#include 'base/memory/ptr_util.h'
+#include 'base/task/post_task.h'
+#include 'content/public/browser/browser_task_traits.h'
+#include 'chrome/browser/browsing_data/browsing_data_appcache_helper.h'
+#include 'chrome/browser/browsing_data/browsing_data_helper.h'
+#include 'chrome/browser/devtools/devtools_window.h'
+#include 'chrome/browser/extensions/devtools_util.h'
+#include 'chrome/browser/extensions/extension_service.h'
+#include 'content/nw/src/api/nw_app.h'
+#include 'content/nw/src/nw_base.h'
+#include 'content/public/browser/render_frame_host.h'
+#include 'content/public/browser/render_process_host.h'
+#include 'content/public/browser/render_view_host.h'
+#include 'content/public/browser/storage_partition.h'
+#include 'content/public/browser/web_contents.h'
+#include 'extensions/browser/app_window/app_window.h'
+#include 'extensions/browser/app_window/app_window_registry.h'
+#include 'extensions/browser/app_window/native_app_window.h'
+#include 'extensions/browser/extension_system.h'
+#include 'extensions/common/error_utils.h'
+#include 'net/proxy_resolution/proxy_config.h'
+#include 'net/proxy_resolution/proxy_config_service_fixed.h'
+#include 'net/proxy_resolution/proxy_resolution_service.h'
+#include 'net/url_request/url_request_context.h'
+#include 'net/url_request/url_request_context_getter.h'
+#include 'net/traffic_annotation/network_traffic_annotation_test_helper.h'
     
-                if(level == res.size())
-                res.push_back(vector<int>());
-            assert( level < res.size() );
-    
-            vector<vector<int>> res;
-        if(root == NULL)
-            return res;
-    
-    #include <iostream>
-#include <vector>
-#include <stack>
-#include <cassert>
-    
-    
-    {    return 0;
-}
-
-    
-    public:
-    vector<int> postorderTraversal(TreeNode* root) {
-    }
-    
-    class ExtensionProcessorFactory : public ::apache::thrift::TProcessorFactory {
+    class NwClipboardClearSyncFunction : public NWSyncExtensionFunction {
  public:
-  ExtensionProcessorFactory(const ::apache::thrift::stdcxx::shared_ptr< ExtensionIfFactory >& handlerFactory) :
-      handlerFactory_(handlerFactory) {}
+  NwClipboardClearSyncFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-      bool operator == (const ExtensionManager_getQueryColumns_result & rhs) const
-  {
-    if (!(success == rhs.success))
-      return false;
-    return true;
-  }
-  bool operator != (const ExtensionManager_getQueryColumns_result &rhs) const {
-    return !(*this == rhs);
-  }
-    
-    #include <fstream>
-    
-    void processDebugEngine(const std::string& fileName, Row& r) {
-  const unsigned long kSymbolOptions =
-      SYMOPT_CASE_INSENSITIVE & SYMOPT_UNDNAME & SYMOPT_LOAD_LINES &
-      SYMOPT_OMAP_FIND_NEAREST & SYMOPT_LOAD_ANYTHING &
-      SYMOPT_FAIL_CRITICAL_ERRORS & SYMOPT_AUTO_PUBLICS;
-    }
-    
-    inline void meta_tables(int nArg, char** azArg) {
-  auto tables = osquery::RegistryFactory::get().names('table');
-  std::sort(tables.begin(), tables.end());
-  for (const auto& table_name : tables) {
-    if (nArg == 1 || table_name.find(azArg[1]) == 0) {
-      printf('  => %s\n', table_name.c_str());
-    }
-  }
+    NwObjCallObjectMethodSyncFunction::NwObjCallObjectMethodSyncFunction() {
 }
     
-      std::vector<std::string> storage_drivers;
-  getStorageCtlerClassDrivers(storage_drivers);
+        // following code is modified from `DesktopCaptureChooseDesktopMediaFunctionBase::OnPickerDialogResults`
+    // in chrome/browser/extensions/api/desktop_capture/desktop_capture_base.cc
     
-      // Author: @guliashvili
-  // Creation Time: 3/09/2018
-  bool isAppStartMonitorEnabled();
-    
-    TEST_F(PerfOutputTests, assigning_constructor) {
-  auto buf = std::array<char, 8>{};
-  auto from_obj = ebpf::PerfOutput<TestMessage>{};
-  from_obj.size_ = buf.size();
-  from_obj.fd_ = 42;
-  from_obj.data_ptr_ = static_cast<void*>(buf.data());
+      // implement nw.Screen.isMonitorStarted()
+  class NwScreenIsMonitorStartedFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenIsMonitorStartedFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
     
-    Expected<EbpfTracepoint, EbpfTracepoint::Error> EbpfTracepoint::load(
-    tracing::NativeEvent system_event, ebpf::Program program) {
-  auto instance = EbpfTracepoint(std::move(system_event), std::move(program));
+    
+    {};
+    
+    	// ----------------------------------------------------------------------------------------------------
+	// calculate the error between the source pixel and the decoded pixel
+	// the error amount is base on the error metric
+	//
+	float Block4x4Encoding::CalcPixelError(ColorFloatRGBA a_frgbaDecodedColor, float a_fDecodedAlpha,
+											ColorFloatRGBA a_frgbaSourcePixel)
+	{
     }
     
-    ExpectedSuccess<LinuxProbesControl::Error> LinuxProbesControl::traceSetuid() {
-  return traceEnterAndExit(syscall::EventType::SetuidEnter);
-}
+      /* the `latin' writing system */
     
-        // this part of the stack is for comm string, let's initialize it with a '\0'
-    {BPF_STX | BPF_DW | BPF_MEM  , BPF_REG_10, BPF_REG_1,  -32,  0},
-    {BPF_STX | BPF_DW | BPF_MEM  , BPF_REG_10, BPF_REG_1,  -24,  0},
+    class Clipper : public virtual ClipperBase
+{
+public:
+  Clipper(int initOptions = 0);
+  bool Execute(ClipType clipType,
+      Paths &solution,
+      PolyFillType fillType = pftEvenOdd);
+  bool Execute(ClipType clipType,
+      Paths &solution,
+      PolyFillType subjFillType,
+      PolyFillType clipFillType);
+  bool Execute(ClipType clipType,
+      PolyTree &polytree,
+      PolyFillType fillType = pftEvenOdd);
+  bool Execute(ClipType clipType,
+      PolyTree &polytree,
+      PolyFillType subjFillType,
+      PolyFillType clipFillType);
+  bool ReverseSolution() { return m_ReverseOutput; };
+  void ReverseSolution(bool value) {m_ReverseOutput = value;};
+  bool StrictlySimple() {return m_StrictSimple;};
+  void StrictlySimple(bool value) {m_StrictSimple = value;};
+  //set the callback function for z value filling on intersections (otherwise Z is 0)
+#ifdef use_xyz
+  void ZFillFunction(ZFillCallback zFillFunc);
+#endif
+protected:
+  virtual bool ExecuteInternal();
+private:
+  JoinList         m_Joins;
+  JoinList         m_GhostJoins;
+  IntersectList    m_IntersectList;
+  ClipType         m_ClipType;
+  typedef std::list<cInt> MaximaList;
+  MaximaList       m_Maxima;
+  TEdge           *m_SortedEdges;
+  bool             m_ExecuteLocked;
+  PolyFillType     m_ClipFillType;
+  PolyFillType     m_SubjFillType;
+  bool             m_ReverseOutput;
+  bool             m_UsingPolyTree; 
+  bool             m_StrictSimple;
+#ifdef use_xyz
+  ZFillCallback   m_ZFill; //custom callback 
+#endif
+  void SetWindingCount(TEdge& edge);
+  bool IsEvenOddFillType(const TEdge& edge) const;
+  bool IsEvenOddAltFillType(const TEdge& edge) const;
+  void InsertLocalMinimaIntoAEL(const cInt botY);
+  void InsertEdgeIntoAEL(TEdge *edge, TEdge* startEdge);
+  void AddEdgeToSEL(TEdge *edge);
+  bool PopEdgeFromSEL(TEdge *&edge);
+  void CopyAELToSEL();
+  void DeleteFromSEL(TEdge *e);
+  void SwapPositionsInSEL(TEdge *edge1, TEdge *edge2);
+  bool IsContributing(const TEdge& edge) const;
+  bool IsTopHorz(const cInt XPos);
+  void DoMaxima(TEdge *e);
+  void ProcessHorizontals();
+  void ProcessHorizontal(TEdge *horzEdge);
+  void AddLocalMaxPoly(TEdge *e1, TEdge *e2, const IntPoint &pt);
+  OutPt* AddLocalMinPoly(TEdge *e1, TEdge *e2, const IntPoint &pt);
+  OutRec* GetOutRec(int idx);
+  void AppendPolygon(TEdge *e1, TEdge *e2);
+  void IntersectEdges(TEdge *e1, TEdge *e2, IntPoint &pt);
+  OutPt* AddOutPt(TEdge *e, const IntPoint &pt);
+  OutPt* GetLastOutPt(TEdge *e);
+  bool ProcessIntersections(const cInt topY);
+  void BuildIntersectList(const cInt topY);
+  void ProcessIntersectList();
+  void ProcessEdgesAtTopOfScanbeam(const cInt topY);
+  void BuildResult(Paths& polys);
+  void BuildResult2(PolyTree& polytree);
+  void SetHoleState(TEdge *e, OutRec *outrec);
+  void DisposeIntersectNodes();
+  bool FixupIntersectionOrder();
+  void FixupOutPolygon(OutRec &outrec);
+  void FixupOutPolyline(OutRec &outrec);
+  bool IsHole(TEdge *e);
+  bool FindOwnerFromSplitRecs(OutRec &outRec, OutRec *&currOrfl);
+  void FixHoleLinkage(OutRec &outrec);
+  void AddJoin(OutPt *op1, OutPt *op2, const IntPoint offPt);
+  void ClearJoins();
+  void ClearGhostJoins();
+  void AddGhostJoin(OutPt *op, const IntPoint offPt);
+  bool JoinPoints(Join *j, OutRec* outRec1, OutRec* outRec2);
+  void JoinCommonEdges();
+  void DoSimplePolygons();
+  void FixupFirstLefts1(OutRec* OldOutRec, OutRec* NewOutRec);
+  void FixupFirstLefts2(OutRec* InnerOutRec, OutRec* OuterOutRec);
+  void FixupFirstLefts3(OutRec* OldOutRec, OutRec* NewOutRec);
+#ifdef use_xyz
+  void SetZ(IntPoint& pt, TEdge& e1, TEdge& e2);
+#endif
+};
+//------------------------------------------------------------------------------
+    
+    #define C_MUL4(m,a,b) C_MUL(m,a,b)
+    
+    /** 32x32 multiplication, followed by a 31-bit shift right. Results fits in 32 bits */
+#undef MULT32_32_Q31
+#define MULT32_32_Q31(a,b) (opus_val32)((((opus_int64)(a)) * ((opus_int64)(b)))>>31)
+    
+    /* Fixed point macros */
