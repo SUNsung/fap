61d402a61759edeaf1c0651e984e142fc731a1b6
SUNsung <weatherius@gmail.com>
2019-03-11 05:58:52 +0200

    Rewrite [02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py] File in: 2019-03-11 05:58:51

--- pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
+++ pyton/02eLRJaq01TP.02ThLKLoGFoU02X9qDKJTTQM02z0U.py
@@ -1,53 +1,216 @@
 
         
-        # Dependency labels used in the paths.
-DEPLABELS = [
-    'PAD',     'UNK',       'ROOT',    'abbrev',    'acomp', 'advcl',
-    'advmod',  'agent',     'amod',    'appos',     'attr',  'aux',
-    'auxpass', 'cc',        'ccomp',   'complm',    'conj',  'cop',
-    'csubj',   'csubjpass', 'dep',     'det',       'dobj',  'expl',
-    'infmod',  'iobj',      'mark',    'mwe',       'nc',    'neg',
-    'nn',      'npadvmod',  'nsubj',   'nsubjpass', 'num',   'number',
-    'p',       'parataxis', 'partmod', 'pcomp',     'pobj',  'poss',
-    'preconj', 'predet',    'prep',    'prepc',     'prt',   'ps',
-    'purpcl',  'quantmod',  'rcmod',   'ref',       'rel',   'suffix',
-    'title',   'tmod',      'xcomp',   'xsubj',
-]
+        
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'thefuck --alias > ~/.config/fish/config.fish')
+    proc.sendline(u'fish')
+    return proc
     
-      S = P_sxn.shape[0]
-  data_e = normalize_rates(data_e, E, S)
+    Options:
+  -h  This help text.
+  -q  Loggable output - no progress indicator
+  -qq No output except for errors
+  -d  Download only - do NOT install or unpack archives
+  -s  No-act. Perform ordering simulation
+  -y  Assume Yes to all queries and do not prompt
+  -f  Attempt to correct a system with broken dependencies in place
+  -m  Attempt to continue if archives are unlocatable
+  -u  Show a list of upgraded packages as well
+  -b  Build the source package after fetching it
+  -V  Show verbose version numbers
+  -c=? Read this configuration file
+  -o=? Set an arbitrary configuration option, eg -o dir::cache=/tmp
+See the apt-get(8), sources.list(5) and apt.conf(5) manual
+pages for more information and options.
+                       This APT has Super Cow Powers.
+'''
+apt_get_operations = ['update', 'upgrade', 'install', 'remove', 'autoremove',
+                      'purge', 'source', 'build-dep', 'dist-upgrade',
+                      'dselect-upgrade', 'clean', 'autoclean', 'check',
+                      'changelog', 'download']
     
-        tf.logging.info('Recovering Graph %s', gd_file)
-    t = {}
-    [t['states_init'], t['lstm/lstm_0/control_dependency'],
-     t['lstm/lstm_1/control_dependency'], t['softmax_out'], t['class_ids_out'],
-     t['class_weights_out'], t['log_perplexity_out'], t['inputs_in'],
-     t['targets_in'], t['target_weights_in'], t['char_inputs_in'],
-     t['all_embs'], t['softmax_weights'], t['global_step']
-    ] = tf.import_graph_def(gd, {}, ['states_init',
-                                     'lstm/lstm_0/control_dependency:0',
-                                     'lstm/lstm_1/control_dependency:0',
-                                     'softmax_out:0',
-                                     'class_ids_out:0',
-                                     'class_weights_out:0',
-                                     'log_perplexity_out:0',
-                                     'inputs_in:0',
-                                     'targets_in:0',
-                                     'target_weights_in:0',
-                                     'char_inputs_in:0',
-                                     'all_embs_out:0',
-                                     'Reshape_3:0',
-                                     'global_step:0'], name='')
+        # Retrieve any AWS settings from the environment.
+    region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
+    if not region:
+        module.fail_json(msg=str('Either region or AWS_REGION or EC2_REGION environment variable or boto config aws_region or ec2_region must be set.'))
     
-    '''Losses for Generator and Discriminator.'''
+    RETURN = '''
+---
+lambda_facts:
+    description: lambda facts
+    returned: success
+    type: dict
+lambda_facts.function:
+    description: lambda function list
+    returned: success
+    type: dict
+lambda_facts.function.TheName:
+    description: lambda function information, including event, mapping, and version information
+    returned: success
+    type: dict
+'''
     
-      # The unique ngrams in the training set.
-  unique_ngrams_in_train = 0.
+    - oneandone_moitoring_policy:
+    auth_token: oneandone_private_api_key
+    name: ansible monitoring policy
+    description: Testing creation of a monitoring policy with ansible
+    email: your@emailaddress.com
+    agent: true
+    thresholds:
+     -
+       cpu:
+         warning:
+           value: 80
+           alert: false
+         critical:
+           value: 92
+           alert: false
+     -
+       ram:
+         warning:
+           value: 80
+           alert: false
+         critical:
+           value: 90
+           alert: false
+     -
+       disk:
+         warning:
+           value: 80
+           alert: false
+         critical:
+           value: 90
+           alert: false
+     -
+       internal_ping:
+         warning:
+           value: 50
+           alert: false
+         critical:
+           value: 100
+           alert: false
+     -
+       transfer:
+         warning:
+           value: 1000
+           alert: false
+         critical:
+           value: 2000
+           alert: false
+    ports:
+     -
+       protocol: TCP
+       port: 22
+       alert_if: RESPONDING
+       email_notification: false
+    processes:
+     -
+       process: test
+       alert_if: NOT_RUNNING
+       email_notification: false
+    wait: true
     
-        Returns:
-      A tuple (done, next state, next input, emit output, next context state)
-      where:
     
-        if not FLAGS.dis_share_embedding:
-      embedding = tf.get_variable('embedding',
-                                  [FLAGS.vocab_size, hparams.dis_rnn_size])
+if __name__ == '__main__':
+    main()
+
+    
+    
+DOCUMENTATION = '''
+---
+module: honeybadger_deployment
+author: 'Benjamin Curtis (@stympy)'
+version_added: '2.2'
+short_description: Notify Honeybadger.io about app deployments
+description:
+   - Notify Honeybadger.io about app deployments (see http://docs.honeybadger.io/article/188-deployment-tracking)
+options:
+  token:
+    description:
+      - API token.
+    required: true
+  environment:
+    description:
+      - The environment name, typically 'production', 'staging', etc.
+    required: true
+  user:
+    description:
+      - The username of the person doing the deployment
+  repo:
+    description:
+      - URL of the project repository
+  revision:
+    description:
+      - A hash, number, tag, or other identifier showing what revision was deployed
+  url:
+    description:
+      - Optional URL to submit the notification to.
+    default: 'https://api.honeybadger.io/v1/deploys'
+  validate_certs:
+    description:
+      - If C(no), SSL certificates for the target url will not be validated. This should only be used
+        on personally controlled sites using self-signed certificates.
+    type: bool
+    default: 'yes'
+    
+        for log in logs:
+        if query_log_status(module, le_path, log):
+            continue
+    
+    import urllib.request, urllib.parse
+from ..common import *
+    
+        type, ext, size = url_info(video_url, headers=fake_headers)
+    
+        print_info(site_info, title, 'm3u8', float('inf'))
+    
+        def basic_extract(self):
+        self.get_title()
+        self.get_flashvars()
+        api_req_url = '{}?{}'.format(self.flashvars['IService'], parse.urlencode(self.flashvars))
+        self.api_req(api_req_url)
+    
+            # 更新聚类中心
+        log.info(centers)
+        for i in range(k):
+            data_i = data[ret[:, 0] == i]  # 标签为 i 的样本
+            centers[i, :] = np.mean(data_i, axis=0)  # 按类别过滤样本
+    
+    
+def hard_sigmoid(x):
+    '''
+    x = 0.                  x < -2.5
+      = 1.                  x > 2.5
+      = 0.2 * x + 0.5       otherwise
+    '''
+    x = (0.2 * x) + 0.5
+    x = tf.clip_by_value(x, 0., 1.)
+    return x
+    
+    
+def crelu(x, axis=-1):
+    '''Concatenated ReLU
+    '''
+    return tf.nn.crelu(x, axis=axis)
+    
+        with tf.variable_scope(name or 'attention_for_rnn', reuse=reuse):
+        a = permute(x, [0, 2, 1])  # [batch_size, n_input, n_step]
+        a = tf.reshape(a, [-1, n_step])  # [batch_size*n_input, n_step]
+    
+        Args:
+        x(tf.Tensor):
+        kernel_size(int or list of int):
+        out_channels(int):
+        act_fn(function):
+        strides(int or list of int):
+        padding(str):
+        name(str):
+        reuse(bool):
+    
+    References：
+    [1509.01626] Character-level Convolutional Networks for Text Classification https://arxiv.org/abs/1509.01626
+'''
