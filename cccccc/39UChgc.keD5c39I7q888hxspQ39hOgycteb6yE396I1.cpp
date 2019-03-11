70f5f4485fdddff95f981cc2433594cc1470abfe
SUNsung <weatherius@gmail.com>
2019-03-11 20:30:54 +0200

    Rewrite [39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp] File in: 2019-03-11 20:30:53

--- cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
+++ cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
@@ -1,90 +1,321 @@
 
         
-            UText **destText = (UText **)uprv_malloc(sizeof(UText*)*destCapacity);
-    if (destText == NULL) {
-        status = U_MEMORY_ALLOCATION_ERROR;
-        return 0;
-    }
-    int32_t i;
-    for (i = 0; i < destCapacity; i++) {
-        destText[i] = utext_openUnicodeString(NULL, &dest[i], &status);
+          /**
+   * @brief Applies the same transformation defined in the data layer's
+   * transform_param block to all the num images in a input_blob.
+   *
+   * @param input_blob
+   *    A Blob containing the data to be transformed. It applies the same
+   *    transformation to all the num images in the blob.
+   * @param transformed_blob
+   *    This is destination blob, it will contain as many images as the
+   *    input blob. It can be part of top blob's data.
+   */
+  void Transform(Blob<Dtype>* input_blob, Blob<Dtype>* transformed_blob);
+    
+      /// @brief The spatial dimensions of the input.
+  inline int input_shape(int i) {
+    return (*bottom_shape_)[channel_axis_ + i];
+  }
+  // reverse_dimensions should return true iff we are implementing deconv, so
+  // that conv helpers know which dimensions are which.
+  virtual bool reverse_dimensions() = 0;
+  // Compute height_out_ and width_out_ from other parameters.
+  virtual void compute_output_shape() = 0;
+    
+    #include <vector>
+    
+    /**
+ * @brief Computes a sum of two input Blobs, with the shape of the latter Blob
+ *        'broadcast' to match the shape of the former. Equivalent to tiling
+ *        the latter Blob, then computing the elementwise sum.
+ *
+ * The second input may be omitted, in which case it's learned as a parameter
+ * of the layer. Note: in case bias and scaling are desired, both operations can
+ * be handled by `ScaleLayer` configured with `bias_term: true`.
+ */
+template <typename Dtype>
+class BiasLayer : public Layer<Dtype> {
+ public:
+  explicit BiasLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
     }
     
-    #include 'unicode/utypes.h'
+    #include 'caffe/layers/neuron_layer.hpp'
     
-    void SearchIterator::setMatchNotFound() 
-{
-    setMatchStart(USEARCH_DONE);
-    setMatchLength(0);
-    UErrorCode status = U_ZERO_ERROR;
-    // by default no errors should be returned here since offsets are within 
-    // range.
-    if (m_search_->isForwardSearching) {
-        setOffset(m_search_->textLength, status);
+    /**
+ * @brief Takes a Blob and crop it, to the shape specified by the second input
+ *  Blob, across all dimensions after the specified axis.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+    
+    #include 'caffe/layers/lrn_layer.hpp'
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
+  for (unsigned int i = 0; i < args_.size(); i++) {
+    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
+      return args_[i].value.string;
     }
-    else {
-        setOffset(0, status);
-    }
+  }
+  return '';
 }
     
-    #include 'unicode/messagepattern.h'
-#include 'unicode/rbnf.h'
-#include 'unicode/selfmt.h'
-#include 'unicode/uchar.h'
-#include 'unicode/ucnv_err.h'
-#include 'unicode/umsg.h'
-#include 'unicode/ustring.h'
-#include 'unicode/utypes.h'
-#include 'cmemory.h'
-#include 'messageimpl.h'
-#include 'patternprops.h'
-#include 'selfmtimpl.h'
-#include 'uassert.h'
-#include 'ustrfmt.h'
-#include 'util.h'
-#include 'uvector.h'
-    
-    U_NAMESPACE_END
-    
-    #ifndef __SHARED_BREAKITERATOR_H__
-#define __SHARED_BREAKITERATOR_H__
-    
-    #ifndef __SHARED_DATEFORMATSYMBOLS_H__
-#define __SHARED_DATEFORMATSYMBOLS_H__
-    
-        /**
-     * Make this instance have no limit on significant digits.
-     */
-    void clear() {
-        fMin = 0;
-        fMax = INT32_MAX;
-    }
-    
-    int32_t
-IntDigitCountRange::pin(int32_t digitCount) const {
-    return digitCount < fMin ? fMin : (digitCount < fMax ? digitCount : fMax);
-}
-    
-    std::unique_ptr<WriteControllerToken> WriteController::GetDelayToken(
-    uint64_t write_rate) {
-  total_delayed_++;
-  // Reset counters.
-  last_refill_time_ = 0;
-  bytes_left_ = 0;
-  set_delayed_write_rate(write_rate);
-  return std::unique_ptr<WriteControllerToken>(new DelayWriteToken(this));
-}
-    
-    struct PosixMemoryMappedFileBuffer : public MemoryMappedFileBuffer {
-  PosixMemoryMappedFileBuffer(void* _base, size_t _length)
-      : MemoryMappedFileBuffer(_base, _length) {}
-  virtual ~PosixMemoryMappedFileBuffer();
+    class CensusChannelData : public ChannelData {
+ public:
+  grpc_error* Init(grpc_channel_element* elem,
+                   grpc_channel_element_args* args) override;
 };
     
-      const char* Name() const override { return 'MyFilter'; }
+    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CLIENT_FILTER_H */
+
     
+    void SetCreateThreadPool(CreateThreadPoolFunc func) { g_ctp_impl = func; }
     
-    { private:
-  uint32_t number_success_backup;
-  uint32_t number_fail_backup;
+    #include <utility>
+    
+    /*!
++ * \brief The result holder of dispatch mode of each Node in the graph.
++ * \note Stored under graph.attrs['dispatch_mode'], provided by Pass 'InferStorageType'
++ *
++ * \code
++ *  Graph g = ApplyPass(src_graph, 'InferStorageType');
++ *  const DispatchModeVector& dispatch_modes = g.GetAttr<DispatchModeVector>('dispatch_mode');
++ *  // get dispatch mode by entry node id
++ *  int node_type = dispatch_modes[nid];
++ * \endcode
++ *
++ * \sa FInferStorageType
++ */
+using DispatchModeVector = std::vector<DispatchMode>;
+    
+    // Initialization funciton called by caffeOp & caffeLoss
+template<typename Dtype>
+void InitCaffeBlobs(std::vector< ::caffe::Blob<Dtype>*>* v, int n_num) {
+  for (index_t i=0; i < n_num; ++i)
+    v->push_back(new ::caffe::Blob<Dtype>());
+}
+    
+    Graph DetectInplaceAddTo(Graph g) {
+  nnvm::StorageVector storage_id =
+      g.MoveCopyAttr<nnvm::StorageVector>('storage_id');
+  std::vector<int> storage_inplace_index =
+      g.MoveCopyAttr<std::vector<int> >('storage_inplace_index');
+  static const Op* ewise_plus_op = Op::Get('_grad_add');
+  auto& idx = g.indexed_graph();
+  // reference cont.
+  std::vector<int> ref_count(idx.num_node_entries(), 0);
+  std::vector<int> addto_entry(idx.num_node_entries(), 0);
+  std::vector<int> skip_plus_node(idx.num_nodes(), 0);
+    }
+    
+      /*! \brief internal next function, inlined for fater processing. */
+  inline bool Next_(void) {
+    if (!base_->Next()) return false;
+    const DataInst &src = base_->Value();
+    this->SetOutImg(src);
+    out_.data.resize(2);
+    out_.data[0] = outimg_;
+    out_.data[1] = src.data[1];
+    out_.index = src.index;
+    out_.extra_data = src.extra_data;
+    return true;
+  }
+  /*!
+   * \brief Set the output image, after augmentation and normalization.
+   * \param src The source image.
+   */
+  inline void SetOutImg(const DataInst &src) {
+    using namespace mshadow::expr;  // NOLINT(*)
+    mshadow::Tensor<cpu, 3> data = src.data[0].get<cpu, 3, real_t>();
+    }
+    
+    template<>
+void EvalRandom<DEVICE, GaussianDistribution>(
+    const real_t &mu,
+    const real_t &sigma,
+    const Resource &resource,
+    TBlob *ret,
+    RunContext ctx) {
+  typedef DEVICE xpu;
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  switch (ret->type_flag_) {
+  case mshadow::kFloat32:
+    {
+      mshadow::Random<xpu, float> *prnd = resource.get_random<xpu, float>(s);
+      mshadow::Tensor<xpu, 2, float> tmp = ret->FlatTo2D<xpu, float>(s);
+      prnd->SampleGaussian(&tmp, float(mu), float(sigma));  // NOLINT(*)
+      break;
+    }
+  case mshadow::kFloat64:
+    {
+      mshadow::Random<xpu, double> *prnd = resource.get_random<xpu, double>(s);
+      mshadow::Tensor<xpu, 2, double> tmp = ret->FlatTo2D<xpu, double>(s);
+      prnd->SampleGaussian(&tmp, double(mu), double(sigma));  // NOLINT(*)
+      break;
+    }
+  default:
+    LOG(FATAL) << 'Random only support float32 and float64';
+  }
+}
+    
+    namespace mxnet {
+namespace op {
+    }
+    }
+    
+    Crop the 2nd and 3rd dim of input data, with the corresponding size of h_w or
+with width and height of the second input symbol, i.e., with one input, we need h_w to
+specify the crop height and width, otherwise the second input symbol's size will be used
+)code' ADD_FILELINE)
+    
+      CustomOpPropCreator Find(const std::string &op_type) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    auto it = registry_.find(op_type);
+    if (it != registry_.end()) return it->second;
+    return nullptr;
+  }
+    
+    MXNET_REGISTER_OP_PROPERTY(_Native, NativeOpProp)
+.describe('Stub for implementing an operator implemented in native frontend language.')
+.add_argument('data', 'NDArray-or-Symbol[]', 'Input data for the custom operator.')
+.add_arguments(NativeOpParam::__FIELDS__());
+    
+    /*!
+ * \brief Macro to register linear updater.
+ */
+#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
+  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
+      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
+          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
+              Name)
+    
+    template<typename IndexType>
+class DensifyParser : public dmlc::Parser<IndexType> {
+ public:
+  DensifyParser(dmlc::Parser<IndexType>* parser, uint32_t num_col)
+      : parser_(parser), num_col_(num_col) {
+  }
+    }
+    
+    #include <dmlc/registry.h>
+#include <vector>
+#include <utility>
+#include <string>
+#include <functional>
+#include './data.h'
+#include './base.h'
+#include '../../src/common/host_device_vector.h'
+    
+    /// Brute Force
+/// Time Complexity: O(n^2)
+/// Space Complexity: O(1)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    }
+    }
+    
+        cout << endl;
+    
+            stack<char> stack;
+        for( int i = 0 ; i < s.size() ; i ++ )
+            if( s[i] == '(' || s[i] == '{' || s[i] == '[')
+                stack.push(s[i]);
+            else{
+    }
+    }
+    
+    int main() {
+    }
+    
+    /// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+    
+    
+    {
+    {
+    {            cur = stack.top();
+            stack.pop();
+            cur = cur->right;
+        }
+        return res;
+    }
+};
+    
+    
+    
+            stack.push(root);
+        while(!stack.empty()){
+    }
+    
+    #include 'gtest/gtest.h'
+    
+    #include 'modules/drivers/canbus/can_comm/message_manager.h'
+    
+    
+    {  Byte t1(bytes + 3);
+  uint32_t t = t1.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
+  double ret = x * CLUSTER_DIST_RES + CLUSTER_DIST_LAT_MIN;
+  return ret;
+}
+    
+    
+    {  int ret = x;
+  return ret;
+}
+    
+      Byte t1(bytes + 2);
+  uint32_t t = t1.get_byte(6, 2);
+  x <<= 2;
+  x |= t;
+    
+    TEST_F(SpeedLimitTest, GetSpeedLimitByS) {
+  EXPECT_EQ(speed_limit_.speed_limit_points().size(), 100);
+  double s = 0.0;
+  const double ds = 0.01;
+  while (s < 99.0) {
+    double v_limit = speed_limit_.GetSpeedLimitByS(s);
+    }
+    }
+    
+    #include 'modules/prediction/container/obstacles/obstacle_clusters.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // config detail: {'name': 'torque_output', 'offset': 0.0, 'precision': 0.001,
+// 'len': 32, 'is_signed_var': True, 'physical_range':
+// '[-2147483.648|2147483.647]', 'bit': 7, 'type': 'double', 'order':
+// 'motorola', 'physical_unit': 'N-m'}
+double Brakemotorrpt372::torque_output(const std::uint8_t* bytes,
+                                       int32_t length) const {
+  Byte t0(bytes + 0);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+      Byte t1(bytes + 1);
+  int32_t t = t1.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
