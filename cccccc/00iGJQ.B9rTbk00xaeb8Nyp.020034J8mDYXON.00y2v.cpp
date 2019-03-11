e1599c0b5eef64c008549e9dde45f4df40e27519
SUNsung <weatherius@gmail.com>
2019-03-11 02:31:33 +0200

    Rewrite [00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp] File in: 2019-03-11 02:31:32

--- cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
+++ cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
@@ -1,163 +1,85 @@
 
         
-          // This is a substitution function from the generic parameters of the
-  // conforming type to the synthetic environment.
-  //
-  // For structs, enums and protocols, this is a 1:1 mapping; for classes,
-  // we increase the depth of each generic parameter by 1 so that we can
-  // introduce a class-bound 'Self' parameter.
-  //
-  // This is a raw function rather than a substitution map because we need to
-  // keep generic parameters as generic, even if the conformanceSig (the best
-  // way to create the substitution map) equates them to concrete types.
-  auto conformanceToSyntheticTypeFn = [&](SubstitutableType *type) {
-    auto *genericParam = cast<GenericTypeParamType>(type);
-    if (covariantSelf) {
-      return GenericTypeParamType::get(genericParam->getDepth() + 1,
-                                       genericParam->getIndex(), ctx);
+        U_NAMESPACE_BEGIN
+    
+    class U_I18N_API SharedNumberFormat : public SharedObject {
+public:
+    SharedNumberFormat(NumberFormat *nfToAdopt) : ptr(nfToAdopt) { }
+    virtual ~SharedNumberFormat();
+    const NumberFormat *get() const { return ptr; }
+    const NumberFormat *operator->() const { return ptr; }
+    const NumberFormat &operator*() const { return *ptr; }
+private:
+    NumberFormat *ptr;
+    SharedNumberFormat(const SharedNumberFormat &);
+    SharedNumberFormat &operator=(const SharedNumberFormat &);
+};
+    
+    /**
+ * A representation an acceptable range of digit counts for integers.
+ */
+class U_I18N_API IntDigitCountRange : public UMemory {
+public:
+    /**
+     * No constraints: 0 up to INT32_MAX
+     */
+    IntDigitCountRange() : fMin(0), fMax(INT32_MAX) { }
+    IntDigitCountRange(int32_t min, int32_t max);
+    int32_t pin(int32_t digitCount) const;
+    int32_t getMax() const { return fMax; }
+    int32_t getMin() const { return fMin; }
+private:
+    int32_t fMin;
+    int32_t fMax;
+};
+    
+            // Fallback schedule:
+        // Midnight/Noon -> General Periods -> AM/PM.
+    
+    U_NAMESPACE_END
+    
+        /**
+     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
+     *
+     * @param keyword for example 'few' or 'other'
+     * @return the plural form corresponding to the keyword
+     */
+    static Form fromString(const UnicodeString &keyword, UErrorCode &errorCode) {
+        return static_cast<Form>(indexFromString(keyword, errorCode));
+    }
+    
+        /**
+     * UnicodeFunctor API.  Cast 'this' to a UnicodeMatcher* pointer
+     * and return the pointer.
+     * @return the UnicodeMatcher point.
+     */
+    virtual UnicodeMatcher* toMatcher() const;
+    
+    #include 'modules/canbus/proto/chassis_detail.pb.h'
+#include 'modules/drivers/canbus/can_comm/protocol_data.h'
+    
+    double ClusterGeneralInfo701::longitude_dist(const std::uint8_t* bytes,
+                                             int32_t length) const {
+  Byte t0(bytes + 1);
+  uint32_t x = t0.get_byte(0, 8);
+    }
+    
+      MatrixXd offset_golden(10, 1);
+  offset_golden << 0, 0, -80, 0, -160, 0, -240, 0, 0, 0;
+    
+    namespace apollo {
+namespace prediction {
     }
     }
     
-    using namespace swift;
-    
-      char *oldBegin = Begin;
-  char *oldEnd = End;
-  std::size_t oldSize = (std::size_t) (oldEnd - oldBegin);
-    
-        StringRef Line = RawText.substr(0, Pos);
-    Lines.push_back(Line);
-    if (!IsFirstLine) {
-      size_t NonWhitespacePos = RawText.find_first_not_of(' ');
-      if (NonWhitespacePos != StringRef::npos)
-        WhitespaceToTrim =
-            std::min(WhitespaceToTrim,
-                     static_cast<unsigned>(NonWhitespacePos));
-    }
-    IsFirstLine = false;
-    
-      bool isVoid() const {
-    assert(isValid());
-    return Decl.isNull();
-  }
-    
-    
-    {      unsigned ID;
-      if (auto *activeDiag = info.dyn_cast<const clang::Diagnostic *>())
-        ID = activeDiag->getID();
-      else
-        ID = info.get<const clang::StoredDiagnostic *>()->getID();
-      return ID == clang::diag::note_module_import_here ||
-             ID == clang::diag::err_module_not_built;
-    }
-    
-    bool Punycode::decodePunycodeUTF8(StringRef InputPunycode,
-                                  std::string &OutUTF8) {
-  std::vector<uint32_t> OutCodePoints;
-  if (!decodePunycode(InputPunycode, OutCodePoints))
-    return false;
-    }
-    
-    SourceGeneratorBase::SourceGeneratorBase(const FileDescriptor* descriptor,
-                                         const Options *options)
-    : descriptor_(descriptor), options_(options) {
+    void Brakerpt6c::Parse(const std::uint8_t* bytes, int32_t length,
+                       ChassisDetail* chassis) const {
+  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_manual_input(
+      manual_input(bytes, length));
+  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_commanded_value(
+      commanded_value(bytes, length));
+  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_output_value(
+      output_value(bytes, length));
+  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_brake_on_off(
+      brake_on_off(bytes, length));
 }
-    
-    ServiceGenerator* ImmutableGeneratorFactory::NewServiceGenerator(
-    const ServiceDescriptor* descriptor) const {
-  return new ImmutableServiceGenerator(descriptor, context_);
-}
-    
-    string ClassNameResolver::GetFileImmutableClassName(
-    const FileDescriptor* file) {
-  string& class_name = file_immutable_outer_class_names_[file];
-  if (class_name.empty()) {
-    if (file->options().has_java_outer_classname()) {
-      class_name = file->options().java_outer_classname();
-    } else {
-      class_name = GetFileDefaultImmutableClassName(file);
-      if (HasConflictingClassName(file, class_name)) {
-        class_name += kOuterClassNameSuffix;
-      }
-    }
-  }
-  return class_name;
-}
-    
-    namespace google {
-namespace protobuf {
-namespace io {
-    }
-    }
-    }
-    
-      // Initialize it.  Now we're initialized.
-  message.MutableExtension(unittest::TestRequired::single)->set_a(1);
-  message.MutableExtension(unittest::TestRequired::single)->set_b(2);
-  message.MutableExtension(unittest::TestRequired::single)->set_c(3);
-  EXPECT_TRUE(ReflectionOps::IsInitialized(message));
-    
-        // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-    
-        // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
-    // - Read 'misc/fonts/README.txt' for more instructions and details.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    //io.Fonts->AddFontDefault();
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Roboto-Medium.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/Cousine-Regular.ttf', 15.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/DroidSans.ttf', 16.0f);
-    //io.Fonts->AddFontFromFileTTF('../../misc/fonts/ProggyTiny.ttf', 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF('c:\\Windows\\Fonts\\ArialUni.ttf', 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
-    
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
-    
-        IDXGIFactory4* dxgiFactory = NULL;
-    g_pSwapChain->GetParent(IID_PPV_ARGS(&dxgiFactory));
-    
-                ImGui::Begin('Hello, world!');                          // Create a window called 'Hello, world!' and append into it.
-    
-    #ifdef _MSC_VER
-#pragma warning (disable: 4505) // unreferenced local function has been removed (stb stuff)
-#endif
-    
-    bool ImGui::InputText(const char* label, std::string* str, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* user_data)
-{
-    IM_ASSERT((flags & ImGuiInputTextFlags_CallbackResize) == 0);
-    flags |= ImGuiInputTextFlags_CallbackResize;
-    }
-    
-    #include 'imgui.h'      // IMGUI_API, ImFontAtlas
-    
-        // Copy and convert all vertices into a single contiguous buffer
-    ImDrawVert* vtx_dst = NULL;
-    ImDrawIdx* idx_dst = NULL;
-    g_pVB->Map(D3D10_MAP_WRITE_DISCARD, 0, (void**)&vtx_dst);
-    g_pIB->Map(D3D10_MAP_WRITE_DISCARD, 0, (void**)&idx_dst);
-    for (int n = 0; n < draw_data->CmdListsCount; n++)
-    {
-        const ImDrawList* cmd_list = draw_data->CmdLists[n];
-        memcpy(vtx_dst, cmd_list->VtxBuffer.Data, cmd_list->VtxBuffer.Size * sizeof(ImDrawVert));
-        memcpy(idx_dst, cmd_list->IdxBuffer.Data, cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx));
-        vtx_dst += cmd_list->VtxBuffer.Size;
-        idx_dst += cmd_list->IdxBuffer.Size;
-    }
-    g_pVB->Unmap();
-    g_pIB->Unmap();
