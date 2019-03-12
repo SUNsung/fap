3d80b2689a41cc3358d78df6c82350b44ab55773
SUNsung <weatherius@gmail.com>
2019-03-12 05:41:33 +0200

    Rewrite [19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py] File in: 2019-03-12 05:41:33

--- pyton/19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py
+++ pyton/19ddcELMfFTDk19FElcMMzI2DM19hefjfRtYRbs199fj.py
@@ -1,4 +1,6 @@
-        if any([s_line.startswith(s) for s in ['* [', '- [']]):
+
+        
+                if any([s_line.startswith(s) for s in ['* [', '- [']]):
             if indent == last_indent:
                 blocks[-1].append(line)
             else:
@@ -8,97 +10,167 @@
             blocks.append([line])
             last_indent = None
     
-        def __init__(self, employee_id, name, rank, call_center):
-        self.employee_id = employee_id
-        self.name = name
-        self.rank = rank
-        self.call = None
-        self.call_center = call_center
-    
-        def add_friend_request(self, from_user_id, to_user_id):
-        pass
-    
-        def reducer(self, key, values):
-        '''Sum values for each key.
-    
-    __all__ = ['cbs_download']
-    
-                    moz_ec_name = search_dict(sym_to_name, 'mozEcName')
-                push = search_dict(sym_to_name, 'push')
-                patt = '{}\.{}\('(.+?)'\)'.format(moz_ec_name, push)
-                ec_list = re.findall(patt, code)
-                [magic_list.append(sym_to_name[ec]) for ec in ec_list]
-        return magic_list
+    print('Enter the PKCS1 private key, followed by a blank line:')
+privkey = b''
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    if line == '':
+        break
+    privkey += line.encode('ascii') + b'\n'
+privkey = rsa.PrivateKey.load_pkcs1(privkey)
     
     
-def get_scanner(hass, config):
-    '''Validate the configuration and return an Actiontec scanner.'''
-    scanner = ActiontecDeviceScanner(config[DOMAIN])
-    return scanner if scanner.success_init else None
-    
-            devices = {}
-        for device in devices_result:
-            match = _DEVICES_REGEX.search(device.decode('utf-8'))
-            if match:
-                devices[match.group('ip')] = {
-                    'ip': match.group('ip'),
-                    'mac': match.group('mac').upper(),
-                    'host': match.group('host'),
-                    'status': match.group('status')
-                    }
-        return devices
+if __name__ == '__main__':
+    main()
 
     
-        def send_message(self, message='', **kwargs):
-        '''Send a message to a user.'''
-        from ciscosparkapi import SparkApiError
-        try:
-            title = ''
-            if kwargs.get(ATTR_TITLE) is not None:
-                title = kwargs.get(ATTR_TITLE) + ': '
-            self._spark.messages.create(roomId=self._default_room,
-                                        text=title + message)
-        except SparkApiError as api_error:
-            _LOGGER.error('Could not send CiscoSpark notification. Error: %s',
-                          api_error)
+        readme = re.sub(r'(?s)^.*?(?=# DESCRIPTION)', '', readme)
+    readme = re.sub(r'\s+youtube-dl \[OPTIONS\] URL \[URL\.\.\.\]', '', readme)
+    readme = PREFIX + readme
+    
+        template = template.replace('{{fileopts}}', '|'.join(fileopts))
+    template = template.replace('{{diropts}}', '|'.join(diropts))
+    template = template.replace('{{flags}}', ' '.join(flags))
+    
+    
+MIME_RE = re.compile(r'^[^/]+/[^/]+$')
+    
+        # Will be set to the raw value of `-a` (if provided) before
+    # `get_auth()` gets called.
+    raw_auth = None
+    
+        def get_formatters_grouped(self):
+        groups = {}
+        for group_name, group in groupby(
+                self.get_formatters(),
+                key=lambda p: getattr(p, 'group_name', 'format')):
+            groups[group_name] = list(group)
+        return groups
+    
+        '''
+    return path.replace('\\', '\\\\\\')
+    
+    
+def test_migrate_implicit_content_type():
+    config = MockEnvironment().config
+    
+        def test_GET_no_data_no_auto_headers(self, httpbin):
+        # https://github.com/jakubroztocil/httpie/issues/62
+        r = http('GET', httpbin.url + '/headers')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == '*/*'
+        assert 'Content-Type' not in r.json['headers']
+    
+        exc = Timeout('Request timed out')
+    exc.request = Request(method='GET', url='http://www.google.com')
+    get_response.side_effect = exc
+    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
+    assert ret == ExitStatus.ERROR_TIMEOUT
+    assert error_msg == 'Request timed out (30s).'
 
     
-        @property
-    def targets(self):
-        '''Return a dictionary of registered targets.'''
-        return {'test target name': 'test target id'}
+        requests_session.cookies = session.cookies
     
-                if self.add_timestamp:
-                text = '{} {}\n'.format(dt_util.utcnow().isoformat(), message)
-            else:
-                text = '{}\n'.format(message)
-            file.write(text)
-
-    
-    from homeassistant.components.notify import (
-    PLATFORM_SCHEMA, BaseNotificationService)
-from homeassistant.const import CONF_ACCESS_TOKEN
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-import homeassistant.helpers.config_validation as cv
-    
-            if resp.status_code == 400:
-            _LOGGER.error('At least one parameter is missing')
-        elif resp.status_code == 402:
-            _LOGGER.error('Too much SMS send in a few time')
-        elif resp.status_code == 403:
-            _LOGGER.error('Wrong Username/Password')
-        elif resp.status_code == 500:
-            _LOGGER.error('Server error, try later')
-
+    '''
+from __future__ import division
+import os
+import re
+import sys
+import errno
+import mimetypes
+import threading
+from time import sleep, time
+from mailbox import Message
     
     
-_LOGGER = logging.getLogger(__name__)
-_RESOURCE = 'https://llamalab.com/automate/cloud/message'
+def unicode_is_ascii(u_string):
+    '''Determine if unicode string only contains ASCII characters.
     
-            for target in targets:
-            try:
-                self.client.message_create(
-                    self.sender, target, message, {'reference': 'HA'})
-            except ErrorException as exception:
-                _LOGGER.error('Failed to notify %s: %s', target, exception)
-                continue
+    
+class ChunkedEncodingError(RequestException):
+    '''The server declared chunked encoding but sent an invalid chunk.'''
+    
+            Keyword:                   'bold #004461',   # class: 'k'
+        Keyword.Constant:          'bold #004461',   # class: 'kc'
+        Keyword.Declaration:       'bold #004461',   # class: 'kd'
+        Keyword.Namespace:         'bold #004461',   # class: 'kn'
+        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
+        Keyword.Reserved:          'bold #004461',   # class: 'kr'
+        Keyword.Type:              'bold #004461',   # class: 'kt'
+    
+            self.cert_verify(conn, request.url, verify, cert)
+        url = self.request_url(request, proxies)
+        self.add_headers(request, stream=stream, timeout=timeout, verify=verify, cert=cert, proxies=proxies)
+    
+                # Consume content and release the original connection
+            # to allow our new request to reuse the same one.
+            r.content
+            r.close()
+            prep = r.request.copy()
+            extract_cookies_to_jar(prep._cookies, r.request, r.raw)
+            prep.prepare_cookies(prep._cookies)
+    
+        def iteritems(self):
+        '''Dict-like iteritems() that returns an iterator of name-value tuples
+        from the jar.
+    
+        return request('post', url, data=data, json=json, **kwargs)
+    
+    
+class AppidManager(object):
+    lock = threading.Lock()
+    
+    
+def best_server(probe_nat=False):
+    best_server = None
+    prober = new_pteredor(probe_nat=probe_nat)
+    prober.qualified = True
+    if not probe_nat:
+        prober.nat_type = 'unknown'
+        prober.rs_cone_flag = 0
+    
+    RecognitionException are generated, when a recognizer encounters incorrect
+or unexpected input.
+    
+    
+    def getDescription(self):
+        return 'n/a'
+    
+        __str__ = toString
+    __repr__ = toString
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+        def solve_sub_array(self):
+        rear = [int(self.array[0])]*len(self.array)
+        sum_value = [int(self.array[0])]*len(self.array)
+        for i in range(1, len(self.array)):
+            sum_value[i] = max(int(self.array[i]) + sum_value[i-1], int(self.array[i]))
+            rear[i] = max(sum_value[i], rear[i-1])
+        return rear[len(self.array)-1]
+    
+                self.decision_boundary = X[best_split]
+            self.left = Decision_Tree(depth = self.depth - 1, min_leaf_size = self.min_leaf_size)
+            self.right = Decision_Tree(depth = self.depth - 1, min_leaf_size = self.min_leaf_size)
+            self.left.train(left_X, left_y)
+            self.right.train(right_X, right_y)
+        else:
+            self.prediction = np.mean(y)
+    
+        difference = log_predict - log_actual
+    square_diff = np.square(difference)
+    mean_square_diff = square_diff.mean()
+    
+        return letterCount
+    
+    min_length = 8
+max_length = 16
+password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
+print('Password: ' + password)
+print('[ If you are thinking of using this passsword, You better save it. ]')
