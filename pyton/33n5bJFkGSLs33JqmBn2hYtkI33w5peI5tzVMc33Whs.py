cb04ebc893c8fc4a364c761a0dc113399fc78740
SUNsung <weatherius@gmail.com>
2019-03-12 03:10:28 +0200

    Rewrite [33n5bJFkGSLs33JqmBn2hYtkI33w5peI5tzVMc33Whs.py] File in: 2019-03-12 03:10:28

--- pyton/33n5bJFkGSLs33JqmBn2hYtkI33w5peI5tzVMc33Whs.py
+++ pyton/33n5bJFkGSLs33JqmBn2hYtkI33w5peI5tzVMc33Whs.py
@@ -1,86 +1,90 @@
 
         
-                (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, category, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
-    
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
-    
-        def set(self, results, query):
-        '''Set the result for the given query key in the cache.
-    
-        def load(self):
-        try:
-            session_data = self._cache.get(self.cache_key)
-        except Exception:
-            # Some backends (e.g. memcache) raise an exception on invalid
-            # cache keys. If this happens, reset the session. See #17810.
-            session_data = None
-        if session_data is not None:
-            return session_data
-        self._session_key = None
-        return {}
-    
-        For complete documentation on using Sessions in your code, consult
-    the sessions documentation that is shipped with Django (also available
-    on the Django Web site).
+            See :class:`Packer` for options.
     '''
-    objects = SessionManager()
+    packer = Packer(**kwargs)
+    stream.write(packer.pack(o))
     
-    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
-    host = r1(r'host=([^&]*)', html)
-    assert host
-    type = r1(r'videofiletype=([^&]*)', html)
-    assert type
-    vid = r1(r'&stream_name=([^&]*)', html)
-    assert vid
-    dir_str = r1(r'&dir=([^&]*)', html).strip()
-    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
-    _, ext, size = url_info(url)
-    print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
+            exp = pd.DatetimeIndex(['2011-01-01 09:00', '2011-01-01 10:00',
+                                '2011-01-01 11:00'], tz=tz)
+        tm.assert_index_equal(
+            idx.fillna(pd.Timestamp('2011-01-01 10:00', tz=tz)), exp)
     
-    def cbs_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    '''Downloads CBS videos by URL.
-    '''
+            self.reloadtask = None
+        if reload:
+            self.reloadtask = asyncio.ensure_future(self.watcher())
+        else:
+            self.loadscript()
     
-        print_info(site_info, title, mime, size)
-    if not info_only:
-        download_urls([real_url], title, ext, size, output_dir=output_dir, merge=merge)
+        def run(self, f, is_request):
+        if self.max_size:
+            r = f.request if is_request else f.response
+            try:
+                expected_size = http1.expected_http_body_size(
+                    f.request, f.response if not is_request else None
+                )
+            except exceptions.HttpException:
+                f.reply.kill()
+                return
+            if expected_size and not r.raw_content and not (0 <= expected_size <= self.max_size):
+                # r.stream may already be a callable, which we want to preserve.
+                r.stream = r.stream or True
+                ctx.log.info('Streaming {} {}'.format('response from' if not is_request else 'request to', f.request.host))
     
-    site_info = 'Douban.com'
-download = douban_download
-download_playlist = playlist_not_supported('douban')
+            if log.log_tier(ctx.options.termlog_verbosity) >= log.log_tier(e.level):
+            click.secho(
+                e.msg,
+                file=outfile,
+                fg=dict(error='red', warn='yellow',
+                        alert='magenta').get(e.level),
+                dim=(e.level == 'debug'),
+                err=(e.level == 'error')
+            )
 
     
-        vids = matchall(content, yinyuetai_embed_patterns)
-    for vid in vids:
-        found = True
-        yinyuetai_download_by_id(vid, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
     
-        @classmethod
-    def funshion_decrypt_str(cls, a_str, coeff):
-        # r'.{27}0' pattern, untested
-        if len(a_str) == 28 and a_str[-1] == '0':
-            data_bytes = base64.b64decode(a_str[:27] + '=')
-            clear = cls.funshion_decrypt(data_bytes, coeff)
-            return binascii.hexlify(clear.encode('utf8')).upper()
+    class TextChunk(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self.keyword = (self._io.read_bytes_term(0, False, True, True)).decode(u'iso8859-1')
+            self.text = (self._io.read_bytes_full()).decode(u'iso8859-1')
     
-                post_url = r1(r''(https://plus.google.com/[^/]+/posts/[^']*)'', html)
-            post_author = r1(r'/\+([^/]+)/posts', post_url)
-            if post_author:
-                post_url = 'https://plus.google.com/+%s/posts/%s' % (parse.quote(post_author), r1(r'posts/(.+)', post_url))
-            post_html = get_html(post_url, faker=True)
-            title = r1(r'<title[^>]*>([^<\n]+)', post_html)
+    		page.addToken(0x05, 'UmCallerID')
+		page.addToken(0x06, 'UmUserNotes')
+		page.addToken(0x07, 'UmAttDuration')
+		page.addToken(0x08, 'UmAttOrder')
+		page.addToken(0x09, 'ConversationId')
+		page.addToken(0x0A, 'ConversationIndex')
+		page.addToken(0x0B, 'LastVerbExecuted')
+		page.addToken(0x0C, 'LastVerbExecutionTime')
+		page.addToken(0x0D, 'ReceivedAsBcc')
+		page.addToken(0x0E, 'Sender')
+		page.addToken(0x0F, 'CalendarType')
+		page.addToken(0x10, 'IsLeapMonth')
+		page.addToken(0x11, 'AccountId')
+		page.addToken(0x12, 'FirstDayOfWeek')
+		page.addToken(0x13, 'MeetingMessageType')
+		self.codePages.append(page)
+		# endregion
+    
+    @pytest.mark.usefixtures('resetSettings')
+@pytest.mark.usefixtures('resetTempSettings')
+class TestAnnounce:
+    def testSenderInfo(self, announcer):
+        sender_info = announcer.sender_info
+        assert sender_info['port'] > 0
+        assert len(sender_info['peer_id']) == 20
+        assert sender_info['rev'] > 0
+    
+    
+def toOpensslPublickey(publickey):
+    publickey_bin = btctools.encode_pubkey(publickey, 'bin')
+    publickey_bin = publickey_bin[1:]
+    publickey_openssl = '\x02\xca\x00 ' + publickey_bin[:32] + '\x00 ' + publickey_bin[32:]
+    return publickey_openssl
+
+    
+        def tell(self):
+        return self.pos
