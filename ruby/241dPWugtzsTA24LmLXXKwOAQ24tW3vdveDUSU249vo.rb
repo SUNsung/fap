dedc5774f2b0069334036018e47922d1c1c9e25c
SUNsung <weatherius@gmail.com>
2019-03-12 04:52:19 +0200

    Rewrite [241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb] File in: 2019-03-12 04:52:18

--- ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
+++ ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
@@ -1,164 +1,200 @@
 
         
-              #
+                unless post && post.id
+          puts post.errors.full_messages if post
+          puts creator.errors.inspect
+          raise 'Failed to create description for trust level 3 lounge!'
+        end
     
-            def connect(websocket, handshake)
-          @connections_count += 1
-          if @connections_count == 1
-            message = 'Browser connected'
-            message += ' over SSL/TLS' if handshake.secure?
-            Jekyll.logger.info 'LiveReload:', message
-          end
-          websocket.send(
-            JSON.dump(
-              :command    => 'hello',
-              :protocols  => ['http://livereload.com/protocols/official-7'],
-              :serverName => 'jekyll'
-            )
+        Category.transaction do
+      staff.group_names = ['staff']
+      unless staff.save
+        puts staff.errors.full_messages
+        raise 'Failed to set permissions on the Staff category!'
+      end
+    
+          case command.method_name
+      when 'sh'
+        error_callback = proc { |string_value| closure_argument_value = string_value }
+        command_param = parameter_map[:command]
+        log_param = parameter_map[:log]
+        action_return = Fastlane::FastFile.sh(command_param, log: log_param, error_callback: error_callback)
+      end
+    
+            [
+          'This will automatically tag your build with the following format: `<grouping>/<lane>/<prefix><build_number>`, where:'.markdown_preserve_newlines,
+          list,
+          'For example, for build 1234 in the 'appstore' lane, it will tag the commit with `builds/appstore/1234`.'
+        ].join('\n')
+      end
+    
+          it 'adds docset_feed_url param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            docset_feed_url: 'http://docset-feed-url.com'
           )
+        end').runner.execute(:test)
     
-              page.objects.each do |object|
-            next if already_imported?(object)
+            context 'with given path to oclint' do
+          let(:result) do
+            Fastlane::FastFile.new.parse('lane :test do
+              oclint(
+                compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+                oclint_path: 'test/bin/oclint'
+              )
+            end').runner.execute(:test)
+          end
+          let(:command) { 'cd #{File.expand_path('.').shellescape} && test/bin/oclint -report-type=html -o=oclint_report.html' }
     
-              user = Representation::User.from_api_response(note.user) if note.user
-          hash = {
-            noteable_type: noteable_type,
-            noteable_id: matches[:iid].to_i,
-            author: user,
-            note: note.body,
-            created_at: note.created_at,
-            updated_at: note.updated_at,
-            github_id: note.id
-          }
+            if b_length > a_length
+          (b_length - a_length).times { a_split.insert(-2, 0) }
+        elsif a_length > b_length
+          (a_length - b_length).times { b_split.insert(-2, 0) }
+        end
     
-          def action_for_grape(env)
-        endpoint = env[ENDPOINT_KEY]
-        route = endpoint.route rescue nil
+          private
     
-      class ShareVisibility < ApplicationRecord
-  end
+        delegate :puts, :print, :tty?, to: :$stdout
     
-      class Notification < ApplicationRecord
-  end
+            css('.file').each do |node|
+          node.content = node.content.strip
+        end
     
-    When /^I toggle all nsfw posts$/ do
-  all('a.toggle_nsfw_state').each &:click
-end
+          private
     
-    RSpec::Matchers.define :have_path do |expected|
-  match do |actual|
-    await_condition { actual.current_path == expected }
-  end
+            css('.nav-index-section').each do |node|
+          node.content = node.content
+        end
     
-      def confirm_on_page(page_name)
-    if page_name == 'my profile page'
-      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
+      def process_push_request
+    case hub_mode
+    when 'subscribe'
+      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
+    when 'unsubscribe'
+      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
     else
-      expect(page).to have_path(path_to(page_name))
+      ['Unknown mode: #{hub_mode}', 422]
     end
   end
-end
     
-        it 'generates the contacts_json fixture', :fixture => true do
-      json = bob.contacts.map { |c|
-               ContactPresenter.new(c, bob).full_hash_with_person
-             }.to_json
-      save_fixture(json, 'contacts_json')
-    end
-  end
-end
-
+    class Api::Web::SettingsController < Api::Web::BaseController
+  respond_to :json
     
-          it 'returns a 404 for a post not visible to the user' do
-        sign_in(eve, scope: :user)
-        expect {
-          get :index, params: {post_id: @post.id}, format: :json
-        }.to raise_error(ActiveRecord::RecordNotFound)
-      end
-    
-    Given /^I run a paperclip generator to add a paperclip '([^']*)' to the '([^']*)' model$/ do |attachment_name, model_name|
-  step %[I successfully run `rails generate paperclip #{model_name} #{attachment_name}`]
-end
-    
-    When /^(?:|I )fill in '([^']*)' for '([^']*)'$/ do |value, field|
-  fill_in(field, :with => value)
-end
-    
-      # Provides configurability to Paperclip. The options available are:
-  # * whiny: Will raise an error if Paperclip cannot process thumbnails of
-  #   an uploaded image. Defaults to true.
-  # * log: Logs progress to the Rails log. Uses ActiveRecord's logger, so honors
-  #   log levels, etc. Defaults to true.
-  # * command_path: Defines the path at which to find the command line
-  #   programs if they are not visible to Rails the system's search path. Defaults to
-  #   nil, which uses the first executable found in the user's search path.
-  # * use_exif_orientation: Whether to inspect EXIF data to determine an
-  #   image's orientation. Defaults to true.
-  def self.options
-    @options ||= {
-      command_path: nil,
-      content_type_mappings: {},
-      log: true,
-      log_command: true,
-      read_timeout: nil,
-      swallow_stderr: true,
-      use_exif_orientation: true,
-      whiny: true,
-      is_windows: Gem.win_platform?
-    }
+        $?.should be_an_instance_of Process::Status
+    $?.success?.should == true
+    $?.exitstatus.should == 0
   end
     
-        def calculated_type_matches
-      possible_types.select do |content_type|
-        content_type == type_from_file_contents
-      end
+        trace_var :$Kernel_trace_var_global do |value|
+      captured = value
     end
     
-        private
+    gemspec
     
-    # Set some attributes
-package.name = 'my-service'
-package.version = '1.0'
+      task :check_submodules do
+    title 'Ensuring submodules are initialized'
+    submodule_help_msg = 'Run git submodule update --init --recursive to ensure submodules are initialized'
+    raise submodule_help_msg if `git submodule status`.split('\n').any? do |line|
+      line.start_with? '-'
+    end
+  end
     
-        def validate
-      # Make sure the user has passed '-s' and '-t' flags
-      mandatory(@command.input_type,
-                'Missing required -s flag. What package source did you want?')
-      mandatory(@command.output_type,
-                'Missing required -t flag. What package output did you want?')
+        # From asking people, it seems MacPorts does not have a `prefix` command, like
+    # Homebrew does, so make an educated guess:
+    if port_prefix = prefix_from_bin('port')
+      prefixes << port_prefix
+    end
     
-          # Move pkginfo to the front, if it exists.
-      for i in (0..entries.length)
-        if(entries[i] == '.PKGINFO')
-          entries[i] = entries[0]
-          entries[0] = '.PKGINFO'
-          break
+          def response
+        return @response if defined? @response
+    
+            # rubocop:disable Style/MethodMissing
+        def method_missing(key, value=nil)
+          if value
+            set(lvalue(key), value)
+          else
+            fetch(key)
+          end
+        end
+        # rubocop:enable Style/MethodMissing
+    
+          private
+    
+    module Capistrano
+  class Configuration
+    # Decorates a Variables object to additionally perform an optional set of
+    # user-supplied validation rules. Each rule for a given key is invoked
+    # immediately whenever `set` is called with a value for that key.
+    #
+    # If `set` is called with a callable value or a block, validation is not
+    # performed immediately. Instead, the validation rules are invoked the first
+    # time `fetch` is used to access the value.
+    #
+    # A rule is simply a block that accepts two arguments: key and value. It is
+    # up to the rule to raise an exception when it deems the value is invalid
+    # (or just print a warning).
+    #
+    # Rules can be registered using the DSL like this:
+    #
+    #   validate(:my_key) do |key, value|
+    #     # rule goes here
+    #   end
+    #
+    class ValidatedVariables < SimpleDelegator
+      include Capistrano::ProcHelpers
+    
+    set_if_empty :pty, false
+    
+          def preference_field_tag(name, value, options)
+        case options[:type]
+        when :integer
+          text_field_tag(name, value, preference_field_options(options))
+        when :boolean
+          hidden_field_tag(name, 0, id: '#{name}_hidden') +
+            check_box_tag(name, 1, value, preference_field_options(options))
+        when :string
+          text_field_tag(name, value, preference_field_options(options))
+        when :password
+          password_field_tag(name, value, preference_field_options(options))
+        when :text
+          text_area_tag(name, value, preference_field_options(options))
+        else
+          text_field_tag(name, value, preference_field_options(options))
         end
       end
     
-      def to_s(format=nil)
-    return super(format.nil? ? 'NAME-VERSION.EXTENSION' : format)
-  end # def to_s
+            def show
+          @inventory_unit = inventory_unit
+          respond_with(@inventory_unit)
+        end
     
-      def parse_install_script(path)
-    global_lines = []
-    look_for = Set.new(['pre_install', 'post_install',
-                        'pre_upgrade', 'post_upgrade',
-                        'pre_remove', 'post_remove'])
-    functions = {}
-    look_for.each do |fname|
-      functions[fname] = []
+            def create
+          authorize! :create, Spree::OptionValue
+          @option_value = scope.new(option_value_params)
+          if @option_value.save
+            render :show, status: 201
+          else
+            invalid_resource!(@option_value)
+          end
+        end
+    
+          def insert_before(oldklass, newklass, *args)
+        i = entries.index { |entry| entry.klass == newklass }
+        new_entry = i.nil? ? Entry.new(newklass, *args) : entries.delete_at(i)
+        i = entries.index { |entry| entry.klass == oldklass } || 0
+        entries.insert(i, new_entry)
+      end
+    
+        def identity
+      @@identity ||= '#{hostname}:#{$$}:#{process_nonce}'
     end
     
-      def output(output_path)
-    output_check(output_path)
+    require 'rack/builder'
+require 'rack/file'
+require 'rack/session/cookie'
     
-      # Output a zipfile.
-  def output(output_path)
-    output_check(output_path)
-    realpath = Pathname.new(output_path).realdirpath.to_s
-    ::Dir.chdir(staging_path) do
-      safesystem('zip', '-9r', realpath, '.')
+          indifferent_hash
     end
-  end # def output
-end # class FPM::Package::Tar
