05e4950a84611650d703ca8ef6c8064cd7436e55
SUNsung <weatherius@gmail.com>
2019-03-12 05:19:59 +0200

    Rewrite [41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb] File in: 2019-03-12 05:19:58

--- ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
+++ ruby/41wN759FK1PmQ41zKmyeyuOHSk41emFwlUDjjb2414gM.rb
@@ -1,45 +1,115 @@
 
         
-                  @highlighter = begin
-            if @config.key?('enable_coderay') && @config['enable_coderay']
-              Jekyll::Deprecator.deprecation_message(
-                'You are using 'enable_coderay', ' \
-                'use syntax_highlighter: coderay in your configuration file.'
-              )
+              xcode_outdated = false
+      begin
+        unless FastlaneCore::Helper.xcode_at_least?(Fastlane::MINIMUM_XCODE_RELEASE)
+          xcode_outdated = true
+        end
+      rescue
+        # We don't care about exceptions here
+        # We'll land here if the user doesn't have Xcode at all for example
+        # which is fine for someone who uses fastlane just for Android project
+        # What we *do* care about is when someone links an old version of Xcode
+      end
     
-      desc 'update main and version in bower.json'
-  task :generate do
-    require 'bootstrap-sass'
-    Dir.chdir Bootstrap.gem_path do
-      spec       = JSON.parse(File.read 'bower.json')
+            expect(result).to eq('appledoc --project-name \'Project Name\' --project-company \'Company\' --docset-atom-filename \'DocSet atom feed filename\' --exit-threshold \'2\' input/dir')
+      end
     
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
+            expect(result).to eq('carthage bootstrap --platform Mac')
+      end
+    
+          it 'works given the path to compile_commands.json' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json'
+            )
+          end').runner.execute(:test)
+    
+                result = Fastlane::FastFile.new.parse('lane :test do
+              swiftlint(ignore_exit_status: true)
+            end').runner.execute(:test)
+          end
+        end
+    
+          # We need to explicity test against Fastlane::Boolean, TrueClass/FalseClass
+      if value.class != FalseClass && value.class != TrueClass
+        UI.user_error!(''#{self.key}' value must be either `true` or `false`! Found #{value.class} instead.')
       end
     end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
+    
+        str = str.dup
+    
+    # remove all double quotes completely
+def simulate_normal_shell_unwrapping(string)
+  string.gsub!(''', '')
+  regex = /^(')(\S*)(')$/
+  unless string.to_s.match(regex).nil?
+    string = string.to_s.match(regex)[2] # get only part in quotes
   end
+  return string
 end
+    
+    # The * turns the array into a parameter list
+# This is using the form of exec which takes a variable parameter list, e.g. `exec(command, param1, param2, ...)`
+# We need to use that, because otherwise invocations like
+# `spaceauth -u user@fastlane.tools` would recognize '-u user@fastlane.tools' as a single parameter and throw errors
+exec(*exec_arr)
 
     
-    desc 'Start an IRB session with all necessary files required.'
-task :shell do |t|
-  chdir File.dirname(__FILE__)
-  exec 'irb -I lib/ -I lib/paperclip -r rubygems -r active_record -r tempfile -r init'
-end
+      # GET /resource/sign_in
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    yield resource if block_given?
+    respond_with(resource, serialize_options(resource))
+  end
     
-    When /^(?:|I )follow '([^']*)'$/ do |link|
-  click_link(link)
-end
+    require 'rack/test'
+require 'action_controller/railtie'
+require 'active_record'
+require 'devise/rails/routes'
+require 'devise/rails/warden_compat'
     
-      def framework_major_version
-    framework_version.split('.').first.to_i
+          # Sign in a user bypassing the warden callbacks and stores the user
+      # straight in session. This option is useful in cases the user is already
+      # signed in, but we want to refresh the credentials in session.
+      #
+      # Examples:
+      #
+      #   bypass_sign_in @user, scope: :user
+      #   bypass_sign_in @user
+      def bypass_sign_in(resource, scope: nil)
+        scope ||= Devise::Mapping.find_scope!(resource)
+        expire_data_after_sign_in!
+        warden.session_serializer.store(resource, scope)
+      end
+    
+          # Resets reset password token and send reset password instructions by email.
+      # Returns the token sent in the e-mail.
+      def send_reset_password_instructions
+        token = set_reset_password_token
+        send_reset_password_instructions_notification(token)
+    
+          resources :taxons, only: [:index]
+    
+            def find_payment
+          @payment = @order.payments.find_by!(number: params[:id])
+        end
+    
+    module Sidekiq
+  module Generators # :nodoc:
+    class WorkerGenerator < ::Rails::Generators::NamedBase # :nodoc:
+      desc 'This generator creates a Sidekiq Worker in app/workers and a corresponding test'
+    
+        def match(request_method, path)
+      case matcher
+      when String
+        {} if path == matcher
+      else
+        if path_match = path.match(matcher)
+          Hash[path_match.names.map(&:to_sym).zip(path_match.captures)]
+        end
+      end
+    end
   end
 end
-World(RailsCommandHelpers)
