59c0b9c013ae10f28d67f52e8a66166825d43b99
SUNsung <weatherius@gmail.com>
2019-03-12 02:24:54 +0200

    Rewrite [55ASxbma8RXQw55UbKrEIGXkwE553ZU8f1.rcY55AC7.py] File in: 2019-03-12 02:24:53

--- pyton/55ASxbma8RXQw55UbKrEIGXkwE553ZU8f1.rcY55AC7.py
+++ pyton/55ASxbma8RXQw55UbKrEIGXkwE553ZU8f1.rcY55AC7.py
@@ -1,228 +1,61 @@
 
         
-                (2016-01, shopping), 25
-        (2016-01, shopping), 100
-        (2016-01, gas), 50
-        '''
-        timestamp, category, amount = line.split('\t')
-        period = self. extract_year_month(timestamp)
-        if period == self.current_year_month():
-            yield (period, category), amount
+            def __init__(self, first_user, second_user):
+        super(PrivateChat, self).__init__()
+        self.users.append(first_user)
+        self.users.append(second_user)
     
-        def get(self, key):
-        hash_index = self._hash_function(key)
-        for item in self.table[hash_index]:
-            if item.key == key:
-                return item.value
-        raise KeyError('Key not found')
     
-            When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.map[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(query, results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
-
+class VehicleSize(Enum):
     
-      d2 = d.copy()   # shallow copy is fine by assumption of d being shallow
-  def jsonify_bool(boolean_value):
-    if boolean_value:
-      return 'true'
+        def test_continuation(self):
+        # Balancing continuation
+        self.check_roundtrip('a = (3,4, \n'
+                             '5,6)\n'
+                             'y = [3, 4,\n'
+                             '5]\n'
+                             'z = {'a': 5,\n'
+                             ''b':15, 'c':True}\n'
+                             'x = len(y) + 5 - a[\n'
+                             '3] - a[2]\n'
+                             '+ len(z) - z[\n'
+                             ''b']\n')
+    
+    def capture(*args, **kw):
+    '''capture all positional and keyword arguments'''
+    return args, kw
+    
+        def test_basic_exception(self):
+        def f(p):
+            1/0
+        f_ident = ident(f)
+        self.check_events(f, [(1, 'call', f_ident),
+                              (1, 'return', f_ident),
+                              ])
+    
+        Note that not all entries are resources.  Specifically, directories are
+    not considered resources.  Use `is_resource()` on each entry returned here
+    to check if it is a resource or not.
+    '''
+    package = _get_package(package)
+    reader = _get_resource_reader(package)
+    if reader is not None:
+        return reader.contents()
+    # Is the package a namespace package?  By definition, namespace packages
+    # cannot have resources.  We could use _check_location() and catch the
+    # exception, but that's extra work, so just inline the check.
+    elif package.__spec__.origin is None or not package.__spec__.has_location:
+        return ()
     else:
-      return 'false'
-    
-    data_train_truth, data_valid_truth = split_list_by_inds(truth_data_e,
-                                                        train_inds,
-                                                        valid_inds)
-data_train_spiking, data_valid_spiking = split_list_by_inds(spiking_data_e,
-                                                            train_inds,
-                                                            valid_inds)
-    
-      # Apply only for missing elements.
-  zeros = tf.zeros_like(present, dtype=tf.float32)
-  log_probs = tf.where(present, zeros, log_probs)
-  rewards = tf.where(present, zeros, rewards)
-    
-    '''Simple bidirectional model definitions.'''
-    
-        '''
-    # Serialize JSON data, if needed.
-    data = args.data
-    auto_json = data and not args.form
-    if (args.json or auto_json) and isinstance(data, dict):
-        if data:
-            data = json.dumps(data)
-        else:
-            # We need to set data to an empty string to prevent requests
-            # from assigning an empty list to `response.request.data`.
-            data = ''
-    
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
-    
-    
-BINARY_SUPPRESSED_NOTICE = (
-    b'\n'
-    b'+-----------------------------------------+\n'
-    b'| NOTE: binary data not shown in terminal |\n'
-    b'+-----------------------------------------+'
-)
-    
-    
-@pytest.mark.skipif(not has_docutils(), reason='docutils not installed')
-@pytest.mark.parametrize('filename', filenames)
-def test_rst_file_syntax(filename):
-    p = subprocess.Popen(
-        ['rst2pseudoxml.py', '--report=1', '--exit-status=1', filename],
-        stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE
-    )
-    err = p.communicate()[1]
-    assert p.returncode == 0, err.decode('utf8')
+        package_directory = Path(package.__spec__.origin).parent
+        return os.listdir(package_directory)
 
     
-        while True:
-        more_to_read = select.select([sock], [], [], timeout)[0]
-        if not more_to_read:
-            break
+                env = os.environ.copy()
+            # C locale gives ASCII locale encoding, but Python uses UTF-8
+            # to parse the command line arguments on Mac OS X and Android.
+            env['LC_ALL'] = 'C'
     
-        if cryptography_version < [1, 3, 4]:
-        warning = 'Old version of cryptography ({}) may cause slowdown.'.format(cryptography_version)
-        warnings.warn(warning, RequestsDependencyWarning)
-    
-        pyopenssl_info = {
-        'version': None,
-        'openssl_version': '',
-    }
-    if OpenSSL:
-        pyopenssl_info = {
-            'version': OpenSSL.__version__,
-            'openssl_version': '%x' % OpenSSL.SSL.OPENSSL_VERSION_NUMBER,
-        }
-    cryptography_info = {
-        'version': getattr(cryptography, '__version__', ''),
-    }
-    idna_info = {
-        'version': getattr(idna, '__version__', ''),
-    }
-    
-        def test_precedence(self):
-        content = '''
-        <?xml version='1.0' encoding='XML'?>
-        <meta charset='HTML5'>
-        <meta http-equiv='Content-type' content='text/html;charset=HTML4' />
-        '''.strip()
-        assert get_encodings_from_content(content) == ['HTML5', 'HTML4', 'XML']
-    
-        def doc(code):
-        names = ', '.join('``%s``' % n for n in _codes[code])
-        return '* %d: %s' % (code, names)
-    
-            :param url: URL for the new :class:`Request` object.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        '''
-    
-        for n_samples in sample_sizes:
-        X = random_state.rand(n_samples, 300)
-    
-                tstart = time()
-            clf.fit(X_train, y_train)
-            sgd_results[i, j, 0] = mean_squared_error(clf.predict(X_test),
-                                                      y_test)
-            sgd_results[i, j, 1] = time() - tstart
-    
-        try:
-        fn = inspect.getsourcefile(obj)
-    except Exception:
-        fn = None
-    if not fn:
-        try:
-            fn = inspect.getsourcefile(sys.modules[obj.__module__])
-        except Exception:
-            fn = None
-    if not fn:
-        return
-    
-    __version__ = '0.2.0'
-__author__ = 'Steven Loria'
-__license__ = 'MIT'
-    
-    import sys
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-    
-    from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import Perceptron
-from sklearn.pipeline import Pipeline
-from sklearn.datasets import load_files
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-    
-    Sentiment analysis can be casted as a binary text classification problem,
-that is fitting a linear classifier on features extracted from the text
-of the user messages so as to guess wether the opinion of the author is
-positive or negative.
-    
-    legend2_values_list = list(legend2.values())
-legend2_keys_list = list(legend2.keys())
-    
-    images_and_predictions = list(zip(digits.images[n_samples // 2:], predicted))
-for index, (image, prediction) in enumerate(images_and_predictions[:4]):
-    plt.subplot(2, 4, index + 5)
-    plt.axis('off')
-    plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
-    plt.title('Prediction: %i' % prediction)
-    
-    These images how similar features are merged together using
-feature agglomeration.
-'''
-print(__doc__)
-    
-    # Theme options are theme-specific and customize the look and feel of a
-# theme further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-    
-        # Make the eyebrows into a nightmare
-    d.polygon(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 128))
-    d.polygon(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 128))
-    d.line(face_landmarks['left_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    d.line(face_landmarks['right_eyebrow'], fill=(68, 54, 39, 150), width=5)
-    
-    # The model was trained in a way that faces with a distance of 0.6 or less should be a match. But if you want to
-# be more strict, you can look for a smaller face distance. For example, using a 0.55 cutoff would reduce false
-# positive matches at the risk of more false negatives.
-    
-    
-def test_image(image_to_check, model):
-    unknown_image = face_recognition.load_image_file(image_to_check)
-    face_locations = face_recognition.face_locations(unknown_image, number_of_times_to_upsample=0, model=model)
-    
-    from setuptools import setup
+    # In a real program you'd get the filename from the arguments.
+with open('outgoing.msg', 'rb') as fp:
+    msg = BytesParser(policy=policy.default).parse(fp)
