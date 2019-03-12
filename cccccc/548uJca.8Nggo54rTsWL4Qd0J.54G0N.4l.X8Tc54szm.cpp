17fdd8f3935f7b51e8f1bd39fbdfcc607b0507a2
SUNsung <weatherius@gmail.com>
2019-03-12 19:19:31 +0200

    Rewrite [548uJca.8Nggo54rTsWL4Qd0J.54G0N.4l.X8Tc54szm.cpp] File in: 2019-03-12 19:19:30

--- cccccc/548uJca.8Nggo54rTsWL4Qd0J.54G0N.4l.X8Tc54szm.cpp
+++ cccccc/548uJca.8Nggo54rTsWL4Qd0J.54G0N.4l.X8Tc54szm.cpp
@@ -1,15 +1,51 @@
 
         
-        // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
-//   can only really compare the output against a golden value, which is a
-//   fairly tedious and fragile testing strategy.  If we want to go that route,
-//   it probably makes sense to bite the bullet and write a test that compares
-//   the whole generated output for unittest.proto against a golden value, with
-//   a very simple script that can be run to regenerate it with the latest code.
-//   This would mean that updates to the golden file would have to be included
-//   in any change to the code generator, which would actually be fairly useful
-//   as it allows the reviewer to see clearly how the generated code is
-//   changing.
+        Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+      // From constructor arguments
+  const OpDef& op_def_;
+  const ApiDef& api_def_;
+  const string function_name_;
+  const int num_outs_;
+    
+    REGISTER_OP('Add').Doc(R'doc(
+An op to test that duplicate registrations don't override previously
+registered ops.
+)doc');
+    
+    #include <vector>
+    
+    // Creates a numpy array with shapes specified by dim_size and dims and content
+// in data. The array does not own the memory, and destructor will be called to
+// release it. If the status is not ok the caller is responsible for releasing
+// the memory.
+Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
+                       std::function<void()> destructor, PyObject** result);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    PyObject* PyExceptionRegistry::Lookup(TF_Code code) {
+  DCHECK(singleton_ != nullptr) << 'Must call PyExceptionRegistry::Init() '
+                                   'before PyExceptionRegistry::Lookup()';
+  DCHECK_NE(code, TF_OK);
+  DCHECK(singleton_->exc_types_.find(code) != singleton_->exc_types_.end())
+      << 'Unknown error code passed to PyExceptionRegistry::Lookup: ' << code;
+  return singleton_->exc_types_[code];
+}
+    
+    #include 'tensorflow/core/framework/tensor.h'
+#include 'tensorflow/core/lib/core/status.h'
     
     #include <google/protobuf/compiler/java/java_context.h>
 #include <google/protobuf/compiler/java/java_enum_field.h>
@@ -21,144 +57,329 @@
 #include <google/protobuf/compiler/java/java_message_lite.h>
 #include <google/protobuf/compiler/java/java_service.h>
     
-        for (std::vector<ExtensionGenerator *>::iterator iter =
-             extension_generators_.begin();
-         iter != extension_generators_.end(); ++iter) {
-      (*iter)->GenerateMembersHeader(printer);
-    }
+    // Author: kenton@google.com (Kenton Varda)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
     
-    const Status Status::OK = Status();
-const Status Status::CANCELLED = Status(error::CANCELLED, '');
-const Status Status::UNKNOWN = Status(error::UNKNOWN, '');
-    
-    
-    {///////////////////////////////////////////////////////////////////////////////
+    Status& Status::operator=(const Status& other) {
+  error_code_ = other.error_code_;
+  error_message_ = other.error_message_;
+  return *this;
 }
     
-    #include <sys/types.h>
-#include <tuple>
+      close(1);
+  dup2(original_stdout_, 1);
+  original_stdout_ = -1;
     
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////
+    #include <google/protobuf/io/gzip_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+    
+    // Reads all boxes from the given filename.
+// Reads a specific target_page number if >= 0, or all pages otherwise.
+// Skips blanks if skip_blanks is true.
+// The UTF-8 label of the box is put in texts, and the full box definition as
+// a string is put in box_texts, with the corresponding page number in pages.
+// Each of the output vectors is optional (may be nullptr).
+// Returns false if no boxes are found.
+bool ReadAllBoxes(int target_page, bool skip_blanks, const STRING& filename,
+                  GenericVector<TBOX>* boxes,
+                  GenericVector<STRING>* texts,
+                  GenericVector<STRING>* box_texts,
+                  GenericVector<int>* pages);
+    
+    // This and other putatively are the same, so call the (permanent) callback
+// for each blob index where the bounding boxes match.
+// The callback is deleted on completion.
+void BoxWord::ProcessMatchedBlobs(const TWERD& other,
+                                  TessCallback1<int>* cb) const {
+  for (int i = 0; i < length_ && i < other.NumBlobs(); ++i) {
+    TBOX blob_box = other.blobs[i]->bounding_box();
+    if (blob_box == boxes_[i])
+      cb->Run(i);
+  }
+  delete cb;
+}
+    
+    // Class to hold a Pixa collection of debug images with captions and save them
+// to a PDF file.
+class DebugPixa {
+ public:
+  // TODO(rays) add another constructor with size control.
+  DebugPixa() {
+    pixa_ = pixaCreate(0);
+    fonts_ = bmfCreate(nullptr, 14);
+  }
+  // If the filename_ has been set and there are any debug images, they are
+  // written to the set filename_.
+  ~DebugPixa() {
+    pixaDestroy(&pixa_);
+    bmfDestroy(&fonts_);
+  }
     }
     
-    // This is a helpful data structure to define parameters
-// You do not have to use it.
-// see http://dmlc-core.readthedocs.org/en/latest/parameter.html
-// for introduction of this module.
-struct MyLogisticParam : public dmlc::Parameter<MyLogisticParam> {
-  float scale_neg_weight;
-  // declare parameters
-  DMLC_DECLARE_PARAMETER(MyLogisticParam) {
-    DMLC_DECLARE_FIELD(scale_neg_weight).set_default(1.0f).set_lower_bound(0.0f)
-        .describe('Scale the weight of negative examples by this factor');
-  }
+      // Update the other members if the cost is lower.
+  void UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
+                      int32_t n, int32_t sig_x, int64_t sig_xsq);
+    
+    
+    { private:
+  double total_weight;         // no of elements or sum of weights.
+  double sigx;                 // sum of x
+  double sigy;                 // sum of y
+  double sigxx;                // sum x squared
+  double sigxy;                // sum of xy
+  double sigyy;                // sum y squared
 };
     
-        const auto ngroup = static_cast<bst_omp_uint>(gptr.size() - 1);
-    #pragma omp parallel
+    
+    {  private:
+    int8_t dir;                    //a direction
+};
+#endif
+
+    
+      Pix* pix() const {
+    return pix_;
+  }
+  void set_pix(Pix* pix) {
+    pix_ = pix;
+  }
+  bool inverse() const {
+    return inverse_;
+  }
+  void set_inverse(bool value) {
+    inverse_ = value;
+  }
+  const DENORM* RootDenorm() const {
+    if (predecessor_ != nullptr)
+      return predecessor_->RootDenorm();
+    return this;
+  }
+  const DENORM* predecessor() const {
+    return predecessor_;
+  }
+  // Accessors - perhaps should not be needed.
+  float x_scale() const {
+    return x_scale_;
+  }
+  float y_scale() const {
+    return y_scale_;
+  }
+  const BLOCK* block() const {
+    return block_;
+  }
+  void set_block(const BLOCK* block) {
+    block_ = block;
+  }
+    
+    int main(int argc, char** argv) {
+  return leveldb::test::RunAllTests();
+}
+
+    
+    Status DumpDescriptor(Env* env, const std::string& fname, WritableFile* dst) {
+  return PrintLogContents(env, fname, VersionEditPrinter, dst);
+}
+    
+    
     {
-      // parall construct, declare random number generator here, so that each
-      // thread use its own random number generator, seed by thread id and current iteration
-      common::RandomEngine rnd(iter * 1111 + omp_get_thread_num());
+    {}  // namespace
+}  // namespace leveldb
+    
+      // If memtable contains a value for key, store it in *value and return true.
+  // If memtable contains a deletion for key, store a NotFound() error
+  // in *status and return true.
+  // Else, return false.
+  bool Get(const LookupKey& key, std::string* value, Status* s);
+    
+    TableCache::TableCache(const std::string& dbname,
+                       const Options& options,
+                       int entries)
+    : env_(options.env),
+      dbname_(dbname),
+      options_(options),
+      cache_(NewLRUCache(entries)) {
+}
+    
+            virtual const std::unordered_set<StreamInformation>& StreamInfos() override { return m_streamInfos; }
+    
+            auto currentWorkerNumSamples = m_prevMinibatchNumSamples;
+        auto prevTotalNumSamples = TotalNumberOfSamplesSeen();
+    
+        static void setupTimeout(int seconds)
+    {
+        struct sigaction action = {};
+        action.sa_handler = &CrossProcessMutex::noOpAlarmHandler;
+        sigaction(SIGALRM, &action, NULL);
+        alarm(seconds);
     }
+    
+    namespace Microsoft { namespace MSR { namespace CNTK {
+    }
+    }
+    }
+    
+            for (auto &qu : m_queryUrls)
+        {
+            std::vector<Url>& urls = qu.m_urls;
+            // Urls are pre-sorted in descending order of gains.
+            typename std::vector<Url>::iterator its = m_urlSorter.begin(), it = urls.begin();
+            typename std::vector<Url>::iterator its0 = its;
+            its = std::copy(it, urls.end(), its);
+            std::sort(its0, its);
+    }
+    
+    class DHTNode;
+class DHTRoutingTable;
+class DHTTaskQueue;
+class DHTTaskFactory;
+class DHTPeerAnnounceStorage;
+class DHTTokenTracker;
+class DHTMessageDispatcher;
+class DHTMessageReceiver;
+class DHTMessageFactory;
+    
+      bool addGoodNode(const std::shared_ptr<DHTNode>& node);
+    
+      // localnode
+  // 8bytes reserved
+  readBytes(fp, buf, buf.size(), 8);
+  // localnode ID
+  readBytes(fp, buf, buf.size(), DHT_ID_LENGTH);
+  auto localNode = std::make_shared<DHTNode>(buf);
+  // 4bytes reserved
+  readBytes(fp, buf, buf.size(), 4);
+    
+      ~DHTRoutingTableDeserializer();
+    
+    
+    {  // Returns two vector of Commands.  First one contains regular
+  // commands.  Secod one contains so called routine commands, which
+  // executed once per event poll returns.
+  std::pair<std::vector<std::unique_ptr<Command>>,
+            std::vector<std::unique_ptr<Command>>>
+  setup(DownloadEngine* e, int family);
+};
+    
+    
+    {} // namespace aria2
+    
+    void DHTTaskExecutor::update()
+{
+  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
+                                  std::mem_fn(&DHTTask::finished)),
+                   execTasks_.end());
+  int r;
+  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
+    r = numConcurrent_ - execTasks_.size();
+  }
+  else {
+    r = 0;
+  }
+  while (r && !queue_.empty()) {
+    std::shared_ptr<DHTTask> task = queue_.front();
+    queue_.pop_front();
+    task->startup();
+    if (!task->finished()) {
+      execTasks_.push_back(task);
+      --r;
+    }
+  }
+  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
+                   static_cast<unsigned int>(getExecutingTaskSize()),
+                   static_cast<unsigned int>(getQueueSize())));
+}
+    
+      size_t getExecutingTaskSize() const { return execTasks_.size(); }
+    
+    class DHTTask;
+    
+      virtual void
+  addPeriodicTask2(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+    
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port) const
+{
+  return generateToken(infoHash, ipaddr, port, secret_[0]);
+}
+    
+      virtual ~DHTTokenUpdateCommand();
     
     public:
-    vector<int> inorderTraversal(TreeNode* root) {
+  // _remoteNode is always null
+  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                    const unsigned char* data, size_t length,
+                    const std::string& ipaddr, uint16_t port);
+    
+    // State shared by all concurrent executions of the same benchmark.
+class SharedState {
+ public:
+  explicit SharedState(CacheBench* cache_bench)
+      : cv_(&mu_),
+        num_threads_(FLAGS_threads),
+        num_initialized_(0),
+        start_(false),
+        num_done_(0),
+        cache_bench_(cache_bench) {
+  }
     }
     
+    #include 'rocksdb/env.h'
     
-    {
-    {
-    {            if(cur != NULL){
-                stack.push(cur);
-                cur = cur->left;
-            }
-            else {
-                cur = stack.top();
-                stack.pop();
-                res.push_back(cur->val);
-                cur = cur->right;
-            }
-        }
-        return res;
-    }
+      // close db
+  for (auto handle : handles) {
+    delete handle;
+  }
+  delete db;
+    
+      // Write a key OUTSIDE of this transaction.
+  // Does not affect txn since this is an unrelated key.  If we wrote key 'abc'
+  // here, the transaction would fail to commit.
+  s = db->Put(write_options, 'xyz', 'zzz');
+    
+    // Verify the checksum of file
+Status VerifySstFileChecksum(const Options& options,
+                             const EnvOptions& env_options,
+                             const std::string& file_path);
+#endif  // ROCKSDB_LITE
+    
+    struct DumpOptions {
+  // Database that will be dumped
+  std::string db_path;
+  // File location that will contain dump output
+  std::string dump_location;
+  // Don't include db information header in the dump
+  bool anonymous = false;
 };
     
-    #include <iostream>
-#include <vector>
-#include <stack>
-    
-            queue<pair<TreeNode*,int>> q;
-        q.push(make_pair(root, 0));
-    
-    
-    {                if(node->left){
-                    q.push(node->left);
-                    new_level_num ++;
-                }
-                if(node->right){
-                    q.push(node->right);
-                    new_level_num ++;
-                }
-            }
-    
-    
-void print_vec(const vector<int>& vec){
-    for(int e: vec)
-        cout << e << ' ';
-    cout << endl;
-}
-    
-            TreeNode* cur = root;
-        while(cur != NULL){
-            if(cur->left == NULL){
-                res.push_back(cur->val);
-                cur = cur->right;
-            }
-            else{
-                TreeNode* prev = cur->left;
-                while(prev->right != NULL && prev->right != cur)
-                    prev = prev->right;
+    namespace rocksdb {
+namespace experimental {
     }
     }
     
-    /**
-@brief SplitCols action.
-@details Split the target node in many columns.
-        Then move out some columns from top, move out the other columns from bottom.
-*/
-class CC_DLL SplitCols : public TiledGrid3DAction
-{
-public:
+      // The name of the DB instance where the thread is currently
+  // involved with.  It would be set to empty string if the thread
+  // does not involve in any DB operation.
+  const std::string db_name;
     
-    /** 
-     * @brief Create the action with the number of columns and the duration.
-     * @param duration Specify the duration of the SplitCols action. It's a value in seconds.
-     * @param cols Specify the columns count should be split.
-     * @return If the creation success, return a pointer of SplitCols action; otherwise, return nil.
-     */
-    static SplitCols* create(float duration, unsigned int cols);
+      // Comparator used to define the order of keys in the table.
+  // Default: a comparator that uses lexicographic byte-wise ordering
+  //
+  // REQUIRES: The client must ensure that the comparator supplied
+  // here has the same name and orders keys *exactly* the same as the
+  // comparator provided to previous open calls on the same DB.
+  const Comparator* comparator;
+    
+    
+    // exception type_error.304
+    try
+    {
+        // use at() on a non-object type
+        json str = 'I am a string';
+        str.at('the good') = 'Another string';
     }
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-                case 1:
-            case 5:
-            case 13:
-                /* going UP with these cases:
-                 1          5           13
-                 +---+---+  +---+---+  +---+---+ 
-                 | 1 |   |  | 1 |   |  | 1 |   | 
-                 +---+---+  +---+---+  +---+---+ 
-                 |   |   |  | 4 |   |  | 4 | 8 | 
-                 +---+---+  +---+---+  +---+---+
-                 */
-                stepx = 0;
-                stepy = -1;
-                break;
+    catch (json::type_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
