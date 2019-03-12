dc8a1b20a752cca434f9c3f8e6beaa85c9c2773a
SUNsung <weatherius@gmail.com>
2019-03-12 14:02:54 +0200

    Rewrite [25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp] File in: 2019-03-12 14:02:53

--- cccccc/25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp
+++ cccccc/25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp
@@ -1,99 +1,298 @@
 
         
-        Contrast Example 2 with Example 1. For each data point per feature, the values
-are sorted by the corresponding KEY.
-)DOC')
-    .Input(0, 'DATA', 'Tensor of rank 1.')
-    .Input(
-        1,
-        'RANGES',
-        'Tensor of int32/int64 ranges, of dims (N, M, 2). '
-        'Where N is number of examples and M is a size of each example. '
-        'Last dimention represents a range in the format (start, lengths)')
-    .Input(2, 'KEY', 'Tensor of rank 1 and type int64.')
-    .Output(0, 'OUTPUT', '1-D tensor of size sum of range lengths')
-    .Arg('lengths', 'Expected lengths for ranges')
-    .TensorInferenceFunction([](const OperatorDef& def,
-                                const vector<TensorShape>& in) {
-      ArgumentHelper helper(def);
-      auto lengths = helper.GetRepeatedArgument<int>('lengths');
-      CAFFE_ENFORCE_EQ(in[0].dims_size(), 1, 'DATA should be 1-D tensor.');
-      CAFFE_ENFORCE_EQ(in[1].dims_size(), 3, 'RANGES should be 3-D tensor.');
-      if (in.size() > 2) {
-        CAFFE_ENFORCE_EQ(in[2].dims_size(), 1, 'KEY should be 1-D tensor.');
-      }
-      CAFFE_ENFORCE_GT(lengths.size(), 0, 'lengths should be non-empty.');
-      std::vector<TensorShape> out(lengths.size());
-      for (int i = 0; i < lengths.size(); ++i) {
-        out[i].set_data_type(in[0].data_type());
-        out[i].add_dims(in[1].dims(0));
-        out[i].add_dims(lengths[i]);
-      }
-      return out;
-    });
+        #include 'ExtensionManager.h'
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
     
-        NDMask::NDMask(const NDShape& shape, const DeviceDescriptor& device)
-        : NDMask(shape, AllocateMatrix(shape, device))
-    {
-        if (shape.Rank() > 2)
-            LogicError('NDMask instance shaped '%S' with more than 2 axes is currently unsupported.', shape.AsString().c_str());
-    }
+    typedef struct _ExtensionException__isset {
+  _ExtensionException__isset() : code(false), message(false), uuid(false) {}
+  bool code :1;
+  bool message :1;
+  bool uuid :1;
+} _ExtensionException__isset;
     
-            static bool IsUDF(const FunctionPtr& f);
-    
-        template <typename ElementType>
-    void Value::CopyVariableValueToCSCSparse(size_t sequenceLength, std::vector<SparseIndexType>& colStarts, std::vector<SparseIndexType>& rowIndices, std::vector<ElementType>& nonZeroValues, size_t& numNonZeroValues)
-    {
-        // All sanity check has been done in ValidateSparseCSCAndGetIndexSizes().
-        NDArrayViewPtr cpuView;
-        if (Device().Type() == DeviceKind::GPU)
-        {
-            // Todo: GPUSparseMatrix to CPUSparseMatrix is not implemented in matrix, as a workaround the dense matrix is used as intermediate presentation.
-            // However, it is possible that data value very close to 0 could treated as 0 after transformation between dense and sparse.
-            auto cpuDenseView = MakeSharedObject<NDArrayView>(GetDataType(), StorageFormat::Dense, Shape(), DeviceDescriptor::CPUDevice());
-            cpuDenseView->CopyFrom(*Data());
-            cpuView = MakeSharedObject<NDArrayView>(GetDataType(), GetStorageFormat(), Shape(), DeviceDescriptor::CPUDevice());
-            cpuView->CopyFrom(*cpuDenseView);
-        }
-        else
-            cpuView = Data();
-    }
-    
-    using namespace std;
-    
-    class ScopeTimer
-{
-    Timer m_aggregateTimer;
-    size_t m_verbosity;
-    std::string m_message;
-    }
-    
-    ///Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    /**
+ * @brief Represents a hardware driver type that SMART api can you use to query
+ * device information.
+ *
+ * @param driver name of SMART controller driver
+ * @param maxID max ID number of which disks on the controller is monitored
+ */
+struct hardwareDriver {
+  std::string driver;
+  size_t maxID;
 };
     
-    using namespace std;
+      // Get the hash value for the kernel's .text memory segment
+  auto f2 = osquery::readFile(kKernelTextHashPath, content);
+  if (f2.ok()) {
+    boost::trim(content);
+    text_segment_hash = content;
+  } else {
+    VLOG(1) << 'Cannot read file: ' << kKernelTextHashPath;
+    return results;
+  }
     
-    using namespace std;
-    
-    
-    {                if(node->left){
-                    q.push(node->left);
-                    new_level_num ++;
-                }
-                if(node->right){
-                    q.push(node->right);
-                    new_level_num ++;
-                }
-            }
-    
-    using namespace std;
-    
-    using namespace std;
+    // If the following flag is set, then command execution stops
+// at an error if we are not interactive.
+static int bail_on_error = 0;
     
     
-    {    return 0;
+    {      if (entry.first == serial->second) {
+        matched = true;
+        row['disk_id'] = entry.second['disk_id'];
+        row['driver_type'] = entry.second['driver_type'];
+      }
+    }
+    
+    namespace osquery {
+namespace perf_event_open {
+    }
+    }
+    
+      DHTRegistry();
+    
+    void DHTReplaceNodeTask::sendMessage()
+{
+  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
+  if (!questionableNode) {
+    setFinished(true);
+  }
+  else {
+    getMessageDispatcher()->addMessageToQueue(
+        getMessageFactory()->createPingMessage(questionableNode), timeout_,
+        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
+  }
 }
+    
+      void onReceived(const DHTPingReplyMessage* message);
+    
+    DHTResponseMessage::DHTResponseMessage(
+    const std::shared_ptr<DHTNode>& localNode,
+    const std::shared_ptr<DHTNode>& remoteNode,
+    const std::string& transactionID)
+    : DHTAbstractMessage(localNode, remoteNode, transactionID)
+{
+}
+    
+    std::shared_ptr<DHTNode> DHTRoutingTable::getNode(const unsigned char* nodeID,
+                                                  const std::string& ipaddr,
+                                                  uint16_t port) const
+{
+  std::shared_ptr<DHTBucket> bucket = getBucketFor(nodeID);
+  return bucket->getNode(nodeID, ipaddr, port);
+}
+    
+    #include <cstring>
+#include <cassert>
+#include <cstdio>
+#include <utility>
+    
+    #endif // D_DHT_TASK_H
+
+    
+    
+    {} // namespace aria2
+
+    
+    public:
+  DHTTokenTracker();
+    
+    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
+                                             std::chrono::seconds interval)
+    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
+{
+}
+    
+    const std::string& DNSCache::find(const std::string& hostname,
+                                  uint16_t port) const
+{
+  auto target = std::make_shared<CacheEntry>(hostname, port);
+  auto i = entries_.find(target);
+  if (i == entries_.end()) {
+    return A2STR::NIL;
+  }
+  else {
+    return (*i)->getGoodAddr();
+  }
+}
+    
+        bool add(const std::string& addr);
+    
+      uint64_t single_refill_amount =
+      delayed_write_rate_ * kRefillInterval / kMicrosPerSecond;
+  if (bytes_left_ + single_refill_amount >= num_bytes) {
+    // Wait until a refill interval
+    // Never trigger expire for less than one refill interval to avoid to get
+    // time.
+    bytes_left_ = bytes_left_ + single_refill_amount - num_bytes;
+    last_refill_time_ = time_now + kRefillInterval;
+    return kRefillInterval + sleep_debt;
+  }
+    
+    #pragma once
+    
+    #include 'rocksdb/db.h'
+#include 'rocksdb/slice.h'
+#include 'rocksdb/options.h'
+    
+    // Example structure that describes a compaction task.
+struct CompactionTask {
+  CompactionTask(
+      DB* _db, Compactor* _compactor,
+      const std::string& _column_family_name,
+      const std::vector<std::string>& _input_file_names,
+      const int _output_level,
+      const CompactionOptions& _compact_options,
+      bool _retry_on_fail)
+          : db(_db),
+            compactor(_compactor),
+            column_family_name(_column_family_name),
+            input_file_names(_input_file_names),
+            output_level(_output_level),
+            compact_options(_compact_options),
+            retry_on_fail(_retry_on_fail) {}
+  DB* db;
+  Compactor* compactor;
+  const std::string& column_family_name;
+  std::vector<std::string> input_file_names;
+  int output_level;
+  CompactionOptions compact_options;
+  bool retry_on_fail;
+};
+    
+      // Commit.  Since the snapshot was advanced, the write done outside of the
+  // transaction does not prevent this transaction from Committing.
+  s = txn->Commit();
+  assert(s.ok());
+  delete txn;
+  // Clear snapshot from read options since it is no longer valid
+  read_options.snapshot = nullptr;
+    
+    #include 'rocksdb/db.h'
+    
+      // A list of properties that describe some details about the current
+  // operation.  Same field in op_properties[] might have different
+  // meanings for different operations.
+  uint64_t op_properties[kNumOperationProperties];
+    
+      // Get the number of jobs scheduled in the ThreadPool queue.
+  virtual unsigned int GetQueueLen() const = 0;
+    
+    namespace rocksdb {
+    }
+    
+    TEST(ByteTest, SetBit) {
+  unsigned char byte_value = 0xFF;
+  Byte value(&byte_value);
+  value.set_bit_0(1);
+  EXPECT_EQ(0xFD, value.get_byte());
+  value.set_bit_0(7);
+  EXPECT_EQ(0x7D, value.get_byte());
+  value.set_bit_1(7);
+  EXPECT_EQ(0xFD, value.get_byte());
+  value.set_value(0x77);
+  value.set_bit_1(0);
+  EXPECT_EQ(0x77, value.get_byte());
+    }
+    
+    ObjectExtendedInfo60D::ObjectExtendedInfo60D() {}
+const uint32_t ObjectExtendedInfo60D::ID = 0x60D;
+    
+        auto it_lower = std::lower_bound(
+        speed_limit_.speed_limit_points().begin(),
+        speed_limit_.speed_limit_points().end(), s,
+        [](const std::pair<double, double>& point, const double curr_s) {
+          return point.first < curr_s;
+        });
+    
+      } else if (type == 'second_order') {
+    for (uint32_t r = 2; r < num_params; ++r) {
+      for (uint32_t c = 2; c < num_params; ++c) {
+        (*term_matrix)(r, c) = x_pow[r + c - 3];
+      }
+    }
+    (*term_matrix).block(0, 0, num_params, 2) =
+        Eigen::MatrixXd::Zero(num_params, 2);
+    (*term_matrix).block(0, 0, 2, num_params) =
+        Eigen::MatrixXd::Zero(2, num_params);
+    
+    #include 'modules/canbus/vehicle/gem/gem_vehicle_factory.h'
+    
+      Byte t1(bytes + 3);
+  int32_t t = t1.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
+    
+        for (i = 1; i < 20000; i++)
+    {
+        uint32_t key = i * 37;
+        swRbtree_insert(tree, key, (void *) (long) (i * 8));
+    }
+    
+    #endif
+
+    
+            void addWrite() {
+            if (m_write) return;
+            m_write = new QSocketNotifier(m_ctx->c.fd, QSocketNotifier::Write, 0);
+            connect(m_write, SIGNAL(activated(int)), this, SLOT(write()));
+        }
+    
+        Q_OBJECT
+    
+    namespace swoole
+{
+class Channel
+{
+public:
+    enum opcode
+    {
+        PRODUCER = 1,
+        CONSUMER = 2,
+    };
+    }
+    }
+    
+    typedef struct
+{
+    int fd;
+    int key;
+} swFdInfo;
+    
+    
+    {
+    {        if (i % 100 == 0)
+        {
+            usleep(10);
+        }
+    }
+}
+    
+    #include <gtest/gtest.h>
+#include <initializer_list>
+#include <utility>
+    
+    
+    {    void *alloc(size_t _size)
+    {
+        if (_size >= _callback_buffer->size)
+        {
+            size_t new_size = _callback_buffer->size * 2;
+            while (new_size < _size + 1)
+            {
+                new_size *= 2;
+            }
+            if (swString_extend(_callback_buffer, new_size) < 0)
+            {
+                abort();
+            }
+        }
+        length = _size;
+        buffer = _callback_buffer->str;
+        ((char *) buffer)[_size] = '\0';
+        return buffer;
+    }
+};
