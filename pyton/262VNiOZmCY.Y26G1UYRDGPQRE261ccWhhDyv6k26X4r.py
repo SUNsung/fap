baf35642dfed4735fdf9767dcf2643bbc6f71d08
SUNsung <weatherius@gmail.com>
2019-03-12 10:10:57 +0200

    Rewrite [262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py] File in: 2019-03-12 10:10:56

--- pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
+++ pyton/262VNiOZmCY.Y26G1UYRDGPQRE261ccWhhDyv6k26X4r.py
@@ -1,30 +1,168 @@
 
         
-            def __init__(self, request):
-        exc = request.routing_exception
-        buf = ['A request was sent to this URL (%s) but a redirect was '
-               'issued automatically by the routing system to '%s'.'
-               % (request.url, exc.new_url)]
+        
+@bp.before_app_request
+def load_logged_in_user():
+    '''If a user id is stored in the session, load the user object from
+    the database into ``g.user``.'''
+    user_id = session.get('user_id')
     
-            current = current.parent
-    
-        Implements the WSGI wrappers (request and response).
-    
-    
-def test_app_tearing_down_with_handled_exception_by_app_handler(app, client):
-    app.config['PROPAGATE_EXCEPTIONS'] = True
-    cleanup_stuff = []
-    
-        module = AnsibleModule(argument_spec, supports_check_mode=True)
+        if db is not None:
+        db.close()
     
     
-def main():
-    module = AnsibleModule(
-        argument_spec=dict(
-            path=dict(required=True),
-            state=dict(default='present', choices=['present', 'followed', 'absent', 'unfollowed']),
-            name=dict(required=False, default=None, type='str'),
-            logtype=dict(required=False, default=None, type='str', aliases=['type'])
-        ),
-        supports_check_mode=True
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
     )
+    assert message in response.data
+    
+        auth.login()
+    # current user can't modify other user's post
+    assert client.post('/1/update').status_code == 403
+    assert client.post('/1/delete').status_code == 403
+    # current user doesn't see edit link
+    assert b'href='/1/update'' not in client.get('/').data
+    
+            :param name: the optional name of the test, otherwise the
+                     function name will be used.
+        '''
+        def register_template(state):
+            state.app.jinja_env.tests[name or f.__name__] = f
+        self.record_once(register_template)
+    
+        If you configure your own :class:`logging.StreamHandler`, you may want to
+    use this for the stream. If you are using file or dict configuration and
+    can't import this directly, you can refer to it as
+    ``ext://flask.logging.wsgi_errors_stream``.
+    '''
+    return request.environ['wsgi.errors'] if request else sys.stderr
+    
+        def list_paths(self):
+        '''Utility method to list all the paths in the jar.'''
+        paths = []
+        for cookie in iter(self):
+            if cookie.path not in paths:
+                paths.append(cookie.path)
+        return paths
+    
+            # Persist cookies
+        if r.history:
+    
+    In both cases, only 10% of the features are informative.
+'''
+import numpy as np
+import gc
+from time import time
+from sklearn.datasets.samples_generator import make_regression
+    
+        link = nodes.reference(text=text, refuri=ref, **options)
+    return [link], []
+    
+        # split the dataset in training and test set:
+    docs_train, docs_test, y_train, y_test = train_test_split(
+        dataset.data, dataset.target, test_size=0.25, random_state=None)
+    
+    plt.figure(2)  # 'banana' shape
+plt.title('Outlier detection on a real data set (boston housing)')
+plt.scatter(X2[:, 0], X2[:, 1], color='black')
+plt.xlim((xx2.min(), xx2.max()))
+plt.ylim((yy2.min(), yy2.max()))
+plt.legend((legend2_values_list[0].collections[0],
+            legend2_values_list[1].collections[0],
+            legend2_values_list[2].collections[0]),
+           (legend2_keys_list[0], legend2_keys_list[1], legend2_keys_list[2]),
+           loc='upper center',
+           prop=matplotlib.font_manager.FontProperties(size=12))
+plt.ylabel('% lower status of the population')
+plt.xlabel('average number of rooms per dwelling')
+    
+    The dataset is generated using the ``make_biclusters`` function, which
+creates a matrix of small values and implants bicluster with large
+values. The rows and columns are then shuffled and passed to the
+Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
+make biclusters contiguous shows how accurately the algorithm found
+the biclusters.
+    
+    
+# Plot the distances
+for index, metric in enumerate(['cosine', 'euclidean', 'cityblock']):
+    avg_dist = np.zeros((n_clusters, n_clusters))
+    plt.figure(figsize=(5, 4.5))
+    for i in range(n_clusters):
+        for j in range(n_clusters):
+            avg_dist[i, j] = pairwise_distances(X[y == i], X[y == j],
+                                                metric=metric).mean()
+    avg_dist /= avg_dist.max()
+    for i in range(n_clusters):
+        for j in range(n_clusters):
+            plt.text(i, j, '%5.3f' % avg_dist[i, j],
+                     verticalalignment='center',
+                     horizontalalignment='center')
+    
+    iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+    
+    agglo = cluster.FeatureAgglomeration(connectivity=connectivity,
+                                     n_clusters=32)
+    
+    def nudge_images(X, y):
+    # Having a larger dataset shows more clearly the behavior of the
+    # methods, but we multiply the size of the dataset only by 2, as the
+    # cost of the hierarchical clustering methods are strongly
+    # super-linear in n_samples
+    shift = lambda x: ndimage.shift(x.reshape((8, 8)),
+                                  .3 * np.random.normal(size=2),
+                                  mode='constant',
+                                  ).ravel()
+    X = np.concatenate([X, np.apply_along_axis(shift, 1, X)])
+    Y = np.concatenate([y, y], axis=0)
+    return X, Y
+    
+    import numpy as np
+import matplotlib.pyplot as plt
+    
+    for face_landmarks in face_landmarks_list:
+    pil_image = Image.fromarray(image)
+    d = ImageDraw.Draw(pil_image, 'RGBA')
+    
+    # Note: This isn't exactly the same as a 'percent match'. The scale isn't linear. But you can assume that images with a
+# smaller distance are more similar to each other than ones with a larger distance.
+    
+    
+@app.route('/', methods=['GET', 'POST'])
+def upload_image():
+    # Check if a valid image file was uploaded
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+    
+    
+@click.command()
+@click.argument('image_to_check')
+@click.option('--cpus', default=1, help='number of CPU cores to use in parallel. -1 means 'use all in system'')
+@click.option('--model', default='hog', help='Which face detection model to use. Options are 'hog' or 'cnn'.')
+def main(image_to_check, cpus, model):
+    # Multi-core processing only supported on Python 3.4 or greater
+    if (sys.version_info < (3, 4)) and cpus != 1:
+        click.echo('WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!')
+        cpus = 1
+    
+        # Bail out when the video file ends
+    if not ret:
+        break
+    
+    requirements = [
+    'face_recognition_models>=0.3.0',
+    'Click>=6.0',
+    'dlib>=19.7',
+    'numpy',
+    'Pillow'
+]
