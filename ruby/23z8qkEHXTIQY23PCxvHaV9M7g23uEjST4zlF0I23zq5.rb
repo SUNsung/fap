ca03396b76b9d19b7659f5d4345e458ca397b50c
SUNsung <weatherius@gmail.com>
2019-03-13 21:54:13 +0200

    Rewrite [23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb] File in: 2019-03-13 21:54:12

--- ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
+++ ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
@@ -1,156 +1,192 @@
 
         
-            # @param filter_platform: Filter, to only show the lanes of a given platform
-    # @return an array of lanes (platform lane_name) to print them out to the user
-    def available_lanes(filter_platform = nil)
-      all = []
-      lanes.each do |platform, platform_lanes|
-        next if filter_platform && filter_platform.to_s != platform.to_s # skip actions that don't match
+              it 'works with select regex when regex is string' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+              select_regex: \'\/AppDelegate\'
+            )
+          end').runner.execute(:test)
     
-        def display_user_error!(e, message)
-      if FastlaneCore::Globals.verbose?
-        # with stack trace
-        reraise_formatted!(e, message)
-      else
-        # without stack trace
-        action_completed(@program[:name], status: FastlaneCore::ActionCompletionStatus::USER_ERROR, exception: e)
-        abort('\n[!] #{message}'.red)
-      end
+          description = '**DEPRECATED!**'
+    
+    shelljoin_testcases = [
+  {
+    'it' => '(#1) on array with entry with space',
+    'it_result' => {
+      'windows' => 'wraps this entry in double quotes',
+      'other'   => 'escapes the space in this entry'
+    },
+    'input' => ['a', 'b c', 'd'],
+    'expect' => {
+      'windows' => 'a 'b c' d',
+      'other'   => 'a b\ c d'
+    }
+  },
+  {
+    'it' => '(#2) on array with entry with string wrapped in double quotes and space',
+    'it_result' => {
+      'windows' => 'wraps the entry with space in quote, and doubles the double quotes',
+      'other'   => 'escapes the double quotes and escapes the space'
+    },
+    'input' => ['a', ''b' c', 'd'],
+    'expect' => {
+      'windows' => 'a '''b'' c' d',
+      'other'   => 'a \'b\'\ c d'
+    }
+  },
+  {
+    'it' => '(#3) on array with entry with string wrapped in single quotes and space',
+    'it_result' => {
+      'windows' => 'no changes',
+      'other'   => 'escapes the single quotes and space'
+    },
+    'input' => ['a', ''b' c', 'd'],
+    'expect' => {
+      'windows' => 'a \''b' c\' d',
+      'other'   => 'a \\'b\\'\\ c d'
+    }
+  },
+  # https://github.com/ruby/ruby/blob/ac543abe91d7325ace7254f635f34e71e1faaf2e/test/test_shellwords.rb#L67-L68
+  {
+    'it' => '(#4) on array with entry that is `$$`',
+    'it_result' => {
+      'windows' => 'the result includes the process id',
+      'other'   => 'the result includes the process id'
+    },
+    'input' => ['ps', '-p', $$],
+    'expect' => {
+      'windows' => 'ps -p #{$$}',
+      'other'   => 'ps -p #{$$}'
+    }
+  }
+]
+    
+    module Cert
+  class CommandsGenerator
+    include Commander::Methods
+    
+        # The path used after sending unlock password instructions
+    def after_sending_unlock_instructions_path_for(resource)
+      new_session_path(resource) if is_navigational_format?
     end
     
-            inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
-        pseudocommand = 'git log --pretty=\'%B\' #{inner_command.shellescape}...HEAD'
-        expect(result).to eq(pseudocommand)
-      end
+      # Attempt to find the mapped route for devise based on request path
+  def devise_mapping
+    @devise_mapping ||= request.env['devise.mapping']
+  end
     
-            expect(result[1]).to start_with('security set-keychain-settings')
-        expect(result[1]).to include('-t 600')
-        expect(result[1]).to include('-l')
-        expect(result[1]).to include('-u')
-        expect(result[1]).to include('~/Library/Keychains/test.keychain')
-      end
-    
-          context 'with valid path to compile_commands.json' do
-        context 'with no path to oclint' do
-          let(:result) do
-            Fastlane::FastFile.new.parse('lane :test do
-              oclint( compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json' )
-            end').runner.execute(:test)
-          end
-          let(:command) { 'cd #{File.expand_path('.').shellescape} && oclint -report-type=html -o=oclint_report.html' }
-    
-                expect do
-              Fastlane::FastFile.new.parse('lane :test do
-                swiftlint(ignore_exit_status: false)
-              end').runner.execute(:test)
-            end.to raise_error(/SwiftLint finished with errors/)
-          end
-        end
-      end
-    
-          it 'should shell escape keychain names when checking for installation' do
-        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
-        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
-    
-    describe 'monkey patch of Array.shelljoin (via CrossplatformShellwords)' do
-  describe 'on Windows' do
-    before(:each) do
-      allow(FastlaneCore::Helper).to receive(:windows?).and_return(true)
-    end
-    
-        # This will detect the proper guest OS for the machine and set up
-    # the class to actually execute capabilities.
-    def detect!
-      guest_name = @machine.config.vm.guest
-      initialize_capabilities!(guest_name, @guests, @capabilities, @machine)
-    rescue Errors::CapabilityHostExplicitNotDetected => e
-      raise Errors::GuestExplicitNotDetected, value: e.extra_data[:value]
-    rescue Errors::CapabilityHostNotDetected
-      raise Errors::GuestNotDetected
-    end
-    
-            # Returns the instance variables as a hash of key-value pairs.
-        def instance_variables_hash
-          instance_variables.inject({}) do |acc, iv|
-            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
-            acc
-          end
-        end
-    
-            def initialize
-          @logger = Log4r::Logger.new('vagrant::plugin::v1::manager')
-          @registered = []
-        end
-    
-    module Vagrant
-  module Plugin
-    module V1
-      # This is the superclass for all V1 plugins.
-      class Plugin
-        # Special marker that can be used for action hooks that matches
-        # all action sequences.
-        ALL_ACTIONS = :__all_actions__
-    
-              # First determine the proper array of VMs.
-          machines = []
-          if names.length > 0
-            names.each do |name|
-              if pattern = name[/^\/(.+?)\/$/, 1]
-                @logger.debug('Finding machines that match regex: #{pattern}')
-    
-            # This should return the state of the machine within this provider.
-        # The state must be an instance of {MachineState}. Please read the
-        # documentation of that class for more information.
-        #
-        # @return [MachineState]
-        def state
-          nil
-        end
-    
-      caveats <<~EOS
-    Installation or Uninstallation may fail with Exit Code 19 (Conflicting Processes running) if Browsers, Safari Notification Service or SIMBL Services (e.g. Flashlight) are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Logs in /Library/Logs/Adobe/Installers if Installation or Uninstallation fails, to identifify the conflicting processes.
-  EOS
-end
-
-    
-          ServerKey = Struct.new(:hostname, :port)
-    
-          def set(key, value=nil, &block)
-        @trusted_keys << key if trusted? && !@trusted_keys.include?(key)
-        remember_location(key)
-        values[key] = block || value
-        trace_set(key)
-        values[key]
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `case` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `case` nodes within RuboCop.
-    class CaseNode < Node
-      include ConditionalNode
-    
-          # A shorthand for getting the first argument of the node.
-      # Equivalent to `arguments.first`.
-      #
-      # @return [Node, nil] the first argument of the node,
-      #                     or `nil` if there are no arguments
-      def first_argument
-        arguments[0]
-      end
-    
-        def handle_switch(cop_names, names, disabled, extras, comment)
-      cop_names.each do |name|
-        names[name] ||= 0
-        if disabled
-          names[name] += 1
-        elsif names[name] > 0
-          names[name] -= 1
+                bypass_sign_in(user)
+          DEPRECATION
+          warden.session_serializer.store(resource, scope)
+        elsif warden.user(scope) == resource && !options.delete(:force)
+          # Do nothing. User already signed in and we are not forcing it.
+          true
         else
-          extras << [comment, name]
+          warden.set_user(resource, options.merge!(scope: scope))
+        end
+      end
+    
+      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
+     options[:store] != false && !env['devise.skip_timeoutable']
+    last_request_at = warden.session(scope)['last_request_at']
+    
+          # Checks whether the user session has expired based on configured time.
+      def timedout?(last_access)
+        !timeout_in.nil? && last_access && last_access <= timeout_in.ago
+      end
+    
+            # This is the method called to 'prepare' the provisioner. This is called
+        # before any actions are run by the action runner (see {Vagrant::Actions::Runner}).
+        # This can be used to setup shared folders, forward ports, etc. Whatever is
+        # necessary on a 'meta' level.
+        #
+        # No return value is expected.
+        def prepare
+        end
+    
+              return [main_args, sub_command, sub_args]
         end
       end
     end
   end
 end
+
+    
+            # This is called as a last-minute hook that allows the configuration
+        # object to finalize itself before it will be put into use. This is
+        # a useful place to do some defaults in the case the user didn't
+        # configure something or so on.
+        #
+        # An example of where this sort of thing is used or has been used:
+        # the 'vm' configuration key uses this to make sure that at least
+        # one sub-VM has been defined: the default VM.
+        #
+        # The configuration object is expected to mutate itself.
+        def finalize!
+          # Default implementation is to do nothing.
+        end
+    
+    When /^I (?:log|sign) out manually$/ do
+  manual_logout
+end
+    
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
+    
+          it 'supports filtering by read/unread' do
+        FactoryGirl.create(:notification, :recipient => alice, :target => @post)
+        get :read_all
+        FactoryGirl.create(:notification, :recipient => alice, :target => @post)
+        get :index, params: {show: 'unread'}
+        expect(assigns[:notifications].count).to eq(1)
+      end
+    end
+    
+          it 'returns an empty array for a post visible to the user' do
+        sign_in(bob, scope: :user)
+        get :index, params: {post_id: @post.id}, format: :json
+        expect(JSON.parse(response.body)).to eq([])
+      end
+    end
+    
+    desc 'Default: run unit tests.'
+task :default => [:clean, :all]
+    
+    Given /^I add this snippet to config\/application.rb:$/ do |snippet|
+  file_name = 'config/application.rb'
+  cd('.') do
+    content = File.read(file_name)
+    File.open(file_name, 'w') {|f| f << content.sub(/class Application < Rails::Application.*$/, 'class Application < Rails::Application\n#{snippet}\n')}
+  end
+end
+    
+    After do
+  ORIGINAL_BUNDLE_VARS.each_pair do |key, value|
+    ENV[key] = value
+  end
+end
+    
+      def generate_migration
+    migration_template('paperclip_migration.rb.erb',
+                       'db/migrate/#{migration_file_name}',
+                       migration_version: migration_version)
+  end
+    
+    require 'mimemagic'
+require 'mimemagic/overlay'
+require 'logger'
+require 'terrapin'
+    
+        def possible_types
+      MIME::Types.type_for(@filepath).collect(&:content_type)
+    end
+    
+        def scaling dst, ratio
+      if ratio.horizontal? || ratio.square?
+        [ '%dx' % dst.width, ratio.width ]
+      else
+        [ 'x%d' % dst.height, ratio.height ]
+      end
+    end
