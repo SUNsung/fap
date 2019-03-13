9101717e3d9cfd64def54332d9189e1e6613d262
SUNsung <weatherius@gmail.com>
2019-03-13 10:01:12 +0200

    Rewrite [36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py] File in: 2019-03-13 10:01:12

--- pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
+++ pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
@@ -1,109 +1,54 @@
 
         
-          dim = embeddings.shape[1]
-    
-    # write out the dataset
-write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
-print ('Saved to ', os.path.join(FLAGS.save_dir,
-                                 FLAGS.datafile_name + '_' + dataset_name))
-
-    
-          scorings.append(dict(
-          correctness=correctness,
-          sentence=sentence,
-          joint_prob=joint_prob,
-          word_probs=word_probs))
-    scoring_mode = 'full' if self.test_data_name == 'pdp60' else 'partial'
-    return utils.compare_substitutions(
-        self.question_ids, scorings, scoring_mode)
-    
-      # Generator rewards are log-probabilities.
-  eps = tf.constant(1e-7, tf.float32)
-  dis_predictions = tf.nn.sigmoid(dis_predictions)
-  rewards = tf.log(dis_predictions + eps)
-    
-        elif (FLAGS.discriminator_model == 'bidirectional_zaremba' or
-          FLAGS.discriminator_model == 'bidirectional_vd'):
-      assert FLAGS.language_model_ckpt_dir_reversed is not None, (
-          'Need a reversed directory to fill in the backward components.')
-      load_fwd_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-      load_bwd_ckpt = tf.train.latest_checkpoint(
-          FLAGS.language_model_ckpt_dir_reversed)
-      print('Restoring Discriminator from %s and %s.' % (load_fwd_ckpt,
-                                                         load_bwd_ckpt))
-      tf.logging.info('Restoring Discriminator from %s and %s.' %
-                      (load_fwd_ckpt, load_bwd_ckpt))
-      dis_fwd_init_saver = init_savers['dis_fwd_init_saver']
-      dis_bwd_init_saver = init_savers['dis_bwd_init_saver']
-      dis_fwd_init_saver.restore(sess, load_fwd_ckpt)
-      dis_bwd_init_saver.restore(sess, load_bwd_ckpt)
-    
-    import logging
-from tornado.curl_httpclient import CurlAsyncHTTPClient
-from tornado.simple_httpclient import SimpleAsyncHTTPClient
-from tornado.ioloop import IOLoop
-from tornado.options import define, options, parse_command_line
-from tornado.web import RequestHandler, Application
+            >>> humanize_bytes(1)
+    '1 B'
+    >>> humanize_bytes(1024, precision=1)
+    '1.0 kB'
+    >>> humanize_bytes(1024 * 123, precision=1)
+    '123.0 kB'
+    >>> humanize_bytes(1024 * 12342, precision=1)
+    '12.1 MB'
+    >>> humanize_bytes(1024 * 12342, precision=2)
+    '12.05 MB'
+    >>> humanize_bytes(1024 * 1234, precision=2)
+    '1.21 MB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
+    '1.31 GB'
+    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
+    '1.3 GB'
     
     
-if __name__ == '__main__':
-    main()
-
+def test_auth_plugin_require_auth_false(httpbin):
     
-        PATTERN = '''import_from< 'from' module_name='__future__' 'import' any >'''
     
-    Most applications should not access this module directly.  Instead,
-pass the keyword argument ``autoreload=True`` to the
-`tornado.web.Application` constructor (or ``debug=True``, which
-enables this setting and several others).  This will enable autoreload
-mode as well as checking for changes to templates and static
-resources.  Note that restarting is a destructive operation and any
-requests in progress will be aborted when the process restarts.  (If
-you want to disable autoreload while using other debug-mode features,
-pass both ``debug=True`` and ``autoreload=False``).
+@mock.patch('httpie.core.get_response')
+def test_error(get_response):
+    def error(msg, *args, **kwargs):
+        global error_msg
+        error_msg = msg % args
     
-        def _handle_events(self, fd: int, events: int) -> None:
-        fileobj, handler_func = self.handlers[fd]
-        handler_func(fileobj, events)
+        def test_request_body_from_file_by_path_no_field_name_allowed(
+            self, httpbin):
+        env = MockEnvironment(stdin_isatty=True)
+        r = http('POST', httpbin.url + '/post', 'field-name@' + FILE_PATH_ARG,
+                 env=env, error_exit_ok=True)
+        assert 'perhaps you meant --form?' in r.stderr
     
-        def feed(self, byte_str, num_bytes):
-        if self._done:
-            return
+        def create_model_instance(self, data):
+        '''
+        Return a new instance of the session model object, which represents the
+        current session state. Intended to be used for saving the session data
+        to the database.
+        '''
+        return self.model(
+            session_key=self._get_or_create_session_key(),
+            session_data=self.encode(data),
+            expire_date=self.get_expiry_date(),
+        )
     
-    # Character Mapping Table:
-Latin2_HungarianCharToOrderMap = (
-255,255,255,255,255,255,255,255,255,255,254,255,255,254,255,255,  # 00
-255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,  # 10
-253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,  # 20
-252,252,252,252,252,252,252,252,252,252,253,253,253,253,253,253,  # 30
-253, 28, 40, 54, 45, 32, 50, 49, 38, 39, 53, 36, 41, 34, 35, 47,
- 46, 71, 43, 33, 37, 57, 48, 64, 68, 55, 52,253,253,253,253,253,
-253,  2, 18, 26, 17,  1, 27, 12, 20,  9, 22,  7,  6, 13,  4,  8,
- 23, 67, 10,  5,  3, 21, 19, 65, 62, 16, 11,253,253,253,253,253,
-159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,
-175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,
-191,192,193,194,195,196,197, 75,198,199,200,201,202,203,204,205,
- 79,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,
-221, 51, 81,222, 78,223,224,225,226, 44,227,228,229, 61,230,231,
-232,233,234, 58,235, 66, 59,236,237,238, 60, 69, 63,239,240,241,
- 82, 14, 74,242, 70, 80,243, 72,244, 15, 83, 77, 84, 30, 76, 85,
-245,246,247, 25, 73, 42, 24,248,249,250, 31, 56, 29,251,252,253,
-)
-    
-        def reset(self):
-        super(MultiByteCharSetProber, self).reset()
-        if self.coding_sm:
-            self.coding_sm.reset()
-        if self.distribution_analyzer:
-            self.distribution_analyzer.reset()
-        self._last_char = [0, 0]
-    
-    EUCJP_ST = (
-          3,     4,     3,     5,MachineState.START,MachineState.ERROR,MachineState.ERROR,MachineState.ERROR,#00-07
-     MachineState.ERROR,MachineState.ERROR,MachineState.ERROR,MachineState.ERROR,MachineState.ITS_ME,MachineState.ITS_ME,MachineState.ITS_ME,MachineState.ITS_ME,#08-0f
-     MachineState.ITS_ME,MachineState.ITS_ME,MachineState.START,MachineState.ERROR,MachineState.START,MachineState.ERROR,MachineState.ERROR,MachineState.ERROR,#10-17
-     MachineState.ERROR,MachineState.ERROR,MachineState.START,MachineState.ERROR,MachineState.ERROR,MachineState.ERROR,     3,MachineState.ERROR,#18-1f
-          3,MachineState.ERROR,MachineState.ERROR,MachineState.ERROR,MachineState.START,MachineState.START,MachineState.START,MachineState.START#20-27
-)
-    
-        .. code::
+    def dedent(line, indent_depth):
+    if line.startswith(' ' * indent_depth):
+        return line[indent_depth:]
+    if line.startswith('\t'):
+        return line[1:]
+    return line
