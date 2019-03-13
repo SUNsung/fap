d5255be86bd17d397006a07efb4784b73a915c51
SUNsung <weatherius@gmail.com>
2019-03-13 05:01:01 +0200

    Rewrite [33RDKAgVxpsek33SB2XUl6tiug33OKP2YXSgoNc33F.J.rb] File in: 2019-03-13 05:01:01

--- ruby/33RDKAgVxpsek33SB2XUl6tiug33OKP2YXSgoNc33F.J.rb
+++ ruby/33RDKAgVxpsek33SB2XUl6tiug33OKP2YXSgoNc33F.J.rb
@@ -1,118 +1,59 @@
 
         
-            def password_change(record, opts={})
-      devise_mail(record, :password_change, opts)
-    end
+          # Compile a Sass or SCSS string to CSS.
+  # Defaults to SCSS.
+  #
+  # @param contents [String] The contents of the Sass file.
+  # @param options [{Symbol => Object}] An options hash;
+  #   see {file:SASS_REFERENCE.md#Options the Sass options documentation}
+  # @raise [Sass::SyntaxError] if there's an error in the document
+  # @raise [Encoding::UndefinedConversionError] if the source encoding
+  #   cannot be converted to UTF-8
+  # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
+  def self.compile(contents, options = {})
+    options[:syntax] ||= :scss
+    Engine.new(contents, options).to_css
   end
-end
-
     
-    module Devise
-  module Controllers
-    # Provide sign in and sign out functionality.
-    # Included by default in all controllers.
-    module SignInOut
-      # Return true if the given scope is signed in session. If no scope given, return
-      # true if any scope is signed in. This will run authentication hooks, which may
-      # cause exceptions to be thrown from this method; if you simply want to check
-      # if a scope has already previously been authenticated without running
-      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
-      def signed_in?(scope=nil)
-        [scope || Devise.mappings.keys].flatten.any? do |_scope|
-          warden.authenticate?(scope: _scope)
-        end
-      end
-    
-          def add_fragment_back_to_path(uri, path)
-        [path, uri.fragment].compact.join('#')
-      end
-    end
-  end
-end
-
-    
-    module Devise
-  module Mailers
-    module Helpers
-      extend ActiveSupport::Concern
-    
-          accessors.each do |accessor|
-        mod.class_eval <<-METHOD, __FILE__, __LINE__ + 1
-          def #{accessor}
-            if defined?(@#{accessor})
-              @#{accessor}
-            elsif superclass.respond_to?(:#{accessor})
-              superclass.#{accessor}
-            else
-              Devise.#{accessor}
-            end
+          ext = @options[:from]
+      Sass::Util.glob('#{@options[:input]}/**/*.#{ext}') do |f|
+        output =
+          if @options[:in_place]
+            f
+          elsif @options[:output]
+            output_name = f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
+            output_name[0...@options[:input].size] = @options[:output]
+            output_name
+          else
+            f.gsub(/\.(c|sa|sc|le)ss$/, '.#{@options[:to]}')
           end
     
-        def log_status(status)
-      puts bold status
+    # component must be String
+    def URIEncodeComponent(componentString)
+      Encode(componentString, :unescapePredicate);
     end
     
-      # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+          attr_reader :name, :path
     
-          # Returns the body of the `for` loop.
-      #
-      # @return [Node, nil] The body of the `for` loop.
-      def body
-        node_parts[2]
-      end
-    end
-  end
-end
-
-    
-          # Calls the given block for each `value` node in the `hash` literal.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_value
-        return pairs.map(&:value).to_enum unless block_given?
-    
-          # Calls the given block for each condition node in the `when` branch.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_condition
-        return conditions.to_enum(__method__) unless block_given?
-    
-          # the per_page_dropdown is used on index pages like orders, products, promotions etc.
-      # this method generates the select_tag
-      def per_page_dropdown
-        # there is a config setting for admin_products_per_page, only for the orders page
-        if @products && per_page_default = Spree::Config.admin_products_per_page
-          per_page_options = []
-          5.times do |amount|
-            per_page_options << (amount + 1) * Spree::Config.admin_products_per_page
-          end
+          def has_footer
+        if @footer
+          @footer.formatted_data.strip.empty? ? false : true
         else
-          per_page_default = Spree::Config.admin_orders_per_page
-          per_page_options = %w{15 30 45 60}
+          @footer = (@page.footer || false)
+          !!@footer
         end
+      end
     
-      s.add_development_dependency 'capybara-accessible'
-end
-
-    
-      def for_each_gem
-    SPREE_GEMS.each do |gem_name|
-      yield 'pkg/spree_#{gem_name}-#{version}.gem'
-    end
-    yield 'pkg/spree-#{version}.gem'
+      test 'extracting paths from URLs' do
+    assert_nil extract_path('Eye-Of-Sauron')
+    assert_equal 'Mordor', extract_path('Mordor/Sauron')
+    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
   end
     
-              inventory_unit.transaction do
-            if inventory_unit.update_attributes(inventory_unit_params)
-              fire
-              render :show, status: 200
-            else
-              invalid_resource!(inventory_unit)
-            end
-          end
-        end
+    def utf8(str)
+  str.respond_to?(:force_encoding) ? str.force_encoding('utf-8') : str
+end
+    
+        def sanitize_empty_params(param)
+      [nil, ''].include?(param) ? nil : CGI.unescape(param)
+    end
