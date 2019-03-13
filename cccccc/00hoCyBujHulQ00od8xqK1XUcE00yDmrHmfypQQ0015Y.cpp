ae05b8a4e6c03bcd26529fe1f905b11cee9085ba
SUNsung <weatherius@gmail.com>
2019-03-13 11:04:36 +0200

    Rewrite [00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp] File in: 2019-03-13 11:04:35

--- cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
+++ cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
@@ -1,70 +1,125 @@
-BENCHMARK_RELATIVE(bigFormat_format, iters) {
-  BenchmarkSuspender suspender;
-  char* p;
-  auto writeToBuf = [&p](StringPiece sp) mutable {
-    memcpy(p, sp.data(), sp.size());
-    p += sp.size();
-  };
+
+        
+            if (info.bytes > buf.size()) {
+      llvm::dbgs() << 'AST section too small.\n';
+      return false;
     }
     
-    
-    {  FunctionRef<int(int, std::vector<int> const&)> variant6const = cof;
-  EXPECT_EQ(100 + 6 * 21, variant6const(21, {}));
-  FunctionRef<int(int, std::vector<int> const&)> const cvariant6const = cof;
-  EXPECT_EQ(100 + 6 * 21, cvariant6const(21, {}));
-}
-    
-    vector<detail::BenchmarkResult> resultsFromFile(const std::string& filename) {
-  string content;
-  readFile(filename.c_str(), content);
-  vector<detail::BenchmarkResult> ret;
-  benchmarkResultsFromDynamic(parseJson(content), ret);
-  return ret;
-}
-    
-    #pragma once
-    
-      bool compare_exchange_weak(
-      SharedPtr& expected,
-      const SharedPtr& n,
-      std::memory_order mo = std::memory_order_seq_cst) noexcept {
-    return compare_exchange_weak(
-        expected, n, mo, detail::default_failure_memory_order(mo));
+      DefaultCacheKey CKey(Key, &DCache.CBs);
+  auto Entry = DCache.Entries.find(CKey);
+  if (Entry != DCache.Entries.end()) {
+    if (Entry->second == Value)
+      return;
+    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
+    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
+    DCache.Entries.erase(Entry);
   }
-  bool compare_exchange_weak(
-      SharedPtr& expected,
-      const SharedPtr& n,
-      std::memory_order success,
-      std::memory_order failure) /* noexcept */ {
-    auto newptr = get_newptr(n);
-    PackedPtr oldptr, expectedptr;
+    
+    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
+  int Ret = cache_get_and_retain(static_cast<cache_t*>(Impl),
+                                 const_cast<void*>(Key), Value_out);
+  return Ret == 0;
+}
+    
+      struct IndentScope {
+    TreePrinter *Printer;
+    size_t OldLength;
+    IndentScope(TreePrinter *printer, StringRef indent)
+        : Printer(printer), OldLength(printer->Indent.size()) {
+      Printer->Indent += indent;
+    }
+    ~IndentScope() { Printer->Indent.resize(OldLength); }
+  };
+    
+    static bool encodeToUTF8(const std::vector<uint32_t> &Scalars,
+                         std::string &OutUTF8) {
+  for (auto S : Scalars) {
+    if (!isValidUnicodeScalar(S)) {
+      OutUTF8.clear();
+      return false;
+    }
+    if (S >= 0xD800 && S < 0xD880)
+      S -= 0xD800;
+    }
     }
     
-        virtual void testCaseStarting( TestCaseInfo const& testInfo ) CATCH_OVERRIDE {
-        for( Reporters::const_iterator it = m_reporters.begin(), itEnd = m_reporters.end();
-                it != itEnd;
-                ++it )
-            (*it)->testCaseStarting( testInfo );
+    void CompileJobAction::anchor() {}
+    
+    class LLVM_LIBRARY_VISIBILITY Darwin : public ToolChain {
+protected:
+  InvocationInfo constructInvocation(const InterpretJobAction &job,
+                                     const JobContext &context) const override;
+  InvocationInfo constructInvocation(const LinkJobAction &job,
+                                     const JobContext &context) const override;
     }
     
-    #pragma once
+    int orientation_and_script_detection(STRING& filename,
+                                     OSResults*,
+                                     tesseract::Tesseract*);
+    
+    void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
+  // Prepare all the blobs.
+  GenericVector<BlobData> blobs;
+  for (int w = 0; w < words.size(); ++w) {
+    if (words[w].word->ratings != nullptr &&
+        words[w].word->ratings->get(0, 0) == nullptr) {
+      for (int s = 0; s < words[w].lang_words.size(); ++s) {
+        Tesseract* sub = s < sub_langs_.size() ? sub_langs_[s] : this;
+        const WERD_RES& word = *words[w].lang_words[s];
+        for (int b = 0; b < word.chopped_word->NumBlobs(); ++b) {
+          blobs.push_back(BlobData(b, sub, word));
+        }
+      }
+    }
+  }
+  // Pre-classify all the blobs.
+  if (tessedit_parallelize > 1) {
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(10)
+#endif  // _OPENMP
+    for (int b = 0; b < blobs.size(); ++b) {
+      *blobs[b].choices =
+          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    }
+  } else {
+    // TODO(AMD) parallelize this.
+    for (int b = 0; b < blobs.size(); ++b) {
+      *blobs[b].choices =
+          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
+    }
+  }
+}
     
     
     {
-    {    /// <summary> std::make_shared using Napa default allocator. </summary>
-    template <typename T, typename... Args>
-    std::shared_ptr<T> MakeShared(Args&&... args) {
-        return AllocateShared<T>(
-            GetDefaultAllocator(), 
-            std::forward<Args>(args)...);
+    {    if (amount[tag.string()] == 1) {
+      other->AddChild(vc->GetName(), vc->GetId(), vc->GetValue().string(),
+                      vc->GetDescription());
+    } else {  // More than one would use this submenu -> create submenu.
+      SVMenuNode* sv = mr->AddChild(tag.string());
+      if ((amount[tag.string()] <= MAX_ITEMS_IN_SUBMENU) ||
+          (amount[tag2.string()] <= 1)) {
+        sv->AddChild(vc->GetName(), vc->GetId(),
+                     vc->GetValue().string(), vc->GetDescription());
+      } else {  // Make subsubmenus.
+        SVMenuNode* sv2 = sv->AddChild(tag2.string());
+        sv2->AddChild(vc->GetName(), vc->GetId(),
+                      vc->GetValue().string(), vc->GetDescription());
+      }
     }
-}
+  }
+  return mr;
 }
     
-            auto functionTemplate = v8::FunctionTemplate::New(isolate, callback);
-        auto function = functionTemplate->GetFunction();
-        auto functionName = v8::String::NewFromUtf8(isolate, name);
-        function->SetName(functionName);
+    // Factory to build a BoxWord from a TWERD using the DENORMs on each blob to
+// switch back to original image coordinates.
+BoxWord* BoxWord::CopyFromNormalized(TWERD* tessword) {
+  BoxWord* boxword = new BoxWord();
+  // Count the blobs.
+  boxword->length_ = tessword->NumBlobs();
+  // Allocate memory.
+  boxword->boxes_.reserve(boxword->length_);
+    }
     
-        /// <summary> Exports a getter function for retrieves the configured logging provider. </summary>
-    NAPA_API LoggingProvider& GetLoggingProvider();
+    
+    {}  // namespace tesseract
