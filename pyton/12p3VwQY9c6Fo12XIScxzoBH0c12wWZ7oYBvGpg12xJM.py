bc522639ff23c186e70936b233e142a3628de076
SUNsung <weatherius@gmail.com>
2019-03-13 03:28:30 +0200

    Rewrite [12p3VwQY9c6Fo12XIScxzoBH0c12wWZ7oYBvGpg12xJM.py] File in: 2019-03-13 03:28:29

--- pyton/12p3VwQY9c6Fo12XIScxzoBH0c12wWZ7oYBvGpg12xJM.py
+++ pyton/12p3VwQY9c6Fo12XIScxzoBH0c12wWZ7oYBvGpg12xJM.py
@@ -1,126 +1,93 @@
 
         
-            def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
+        
+def test_mnist():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        assert len(x_train) == len(y_train) == 60000
+        assert len(x_test) == len(y_test) == 10000
     
-        def add_link_to_crawl(self, url):
-        '''Add the given link to `links_to_crawl`.'''
-        pass
+    from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
     
-      if ext_input_bxtxi is not None:
-    input_title += ' External Input'
-    plot_time_series(ext_input_bxtxi, n_to_plot=n_to_plot, color='b',
-                     scale=scale, title=col_title + input_title)
+    
+if K.backend() != 'tensorflow':
+    raise RuntimeError('This example can only run with the TensorFlow backend,'
+                       ' because it requires the Datset API, which is not'
+                       ' supported on other platforms.')
+    
+    # Display the 1st 8 corrupted and denoised images
+rows, cols = 10, 30
+num = rows * cols
+imgs = np.concatenate([x_test[:num], x_test_noisy[:num], x_decoded[:num]])
+imgs = imgs.reshape((rows * 3, cols, image_size, image_size))
+imgs = np.vstack(np.split(imgs, rows, axis=1))
+imgs = imgs.reshape((rows * 3, -1, image_size, image_size))
+imgs = np.vstack([np.hstack(i) for i in imgs])
+imgs = (imgs * 255).astype(np.uint8)
+plt.figure()
+plt.axis('off')
+plt.title('Original images: top rows, '
+          'Corrupted Input: middle rows, '
+          'Denoised Input:  third rows')
+plt.imshow(imgs, interpolation='none', cmap='gray')
+Image.fromarray(imgs).save('corrupted_and_denoised.png')
+plt.show()
+
     
     
-# Initial condition generation, and condition label generation.  This
-# happens outside of the dataset loop, so that all datasets have the
-# same conditions, which is similar to a neurophys setup.
-condition_number = 0
-x0s = []
-condition_labels = []
-for c in range(C):
-  x0 = FLAGS.x0_std * rng.randn(N, 1)
-  x0s.append(np.tile(x0, nreplications)) # replicate x0 nreplications times
-  # replicate the condition label nreplications times
-  for ns in range(nreplications):
-    condition_labels.append(condition_number)
-  condition_number += 1
-x0s = np.concatenate(x0s, axis=1)
+def mean_squared_logarithmic_error(y_true, y_pred):
+    first_log = K.log(K.clip(y_pred, K.epsilon(), None) + 1.)
+    second_log = K.log(K.clip(y_true, K.epsilon(), None) + 1.)
+    return K.mean(K.square(first_log - second_log), axis=-1)
     
-      return data_e, x0s, inputs_e
+        cmd = cmds[cmdname]
+    parser.usage = 'scrapy %s %s' % (cmdname, cmd.syntax())
+    parser.description = cmd.long_desc()
+    settings.setdict(cmd.default_settings, priority='command')
+    cmd.settings = settings
+    cmd.add_options(parser)
+    opts, args = parser.parse_args(args=argv[1:])
+    _run_print_help(parser, cmd.process_options, args, opts)
     
-      # Note the use of get_variable vs. tf.Variable.  this is because get_variable
-  # does not allow the initialization of the variable with a value.
-  if normalized:
-    w_collections = [tf.GraphKeys.GLOBAL_VARIABLES, 'norm-variables']
-    if collections:
-      w_collections += collections
-    if mat_init_value is not None:
-      w = tf.Variable(mat_init_value, name=wname, collections=w_collections,
-                      trainable=trainable)
-    else:
-      w = tf.get_variable(wname, [in_size, out_size], initializer=mat_init,
-                          collections=w_collections, trainable=trainable)
-    w = tf.nn.l2_normalize(w, dim=0) # x W, so xW_j = \sum_i x_bi W_ij
-  else:
-    w_collections = [tf.GraphKeys.GLOBAL_VARIABLES]
-    if collections:
-      w_collections += collections
-    if mat_init_value is not None:
-      w = tf.Variable(mat_init_value, name=wname, collections=w_collections,
-                      trainable=trainable)
-    else:
-      w = tf.get_variable(wname, [in_size, out_size], initializer=mat_init,
-                          collections=w_collections, trainable=trainable)
-  b = None
-  if do_bias:
-    b_collections = [tf.GraphKeys.GLOBAL_VARIABLES]
-    if collections:
-      b_collections += collections
-    bname = (name + '/b') if name else '/b'
-    if bias_init_value is None:
-      b = tf.get_variable(bname, [1, out_size],
-                          initializer=tf.zeros_initializer(),
-                          collections=b_collections,
-                          trainable=trainable)
-    else:
-      b = tf.Variable(bias_init_value, name=bname,
-                      collections=b_collections,
-                      trainable=trainable)
+            if opts.logfile:
+            self.settings.set('LOG_ENABLED', True, priority='cmdline')
+            self.settings.set('LOG_FILE', opts.logfile, priority='cmdline')
     
-    import tensorflow as tf
     
-      # Unstack Tensors into lists.
-  rewards_list = tf.unstack(rewards, axis=1)
-  log_probs_list = tf.unstack(log_probs, axis=1)
-  missing = 1. - tf.cast(present, tf.float32)
-  missing_list = tf.unstack(missing, axis=1)
+class _BenchSpider(scrapy.Spider):
+    '''A spider that follows all links'''
+    name = 'follow'
+    total = 10000
+    show = 20
+    baseurl = 'http://localhost:8998'
+    link_extractor = LinkExtractor()
     
-    '''Model optimization.'''
     
-      Args:
-    session:  Current tf.Session.
-    percent_real_update: tf.assign operation.
-    new_rate: tf.placeholder for the new rate.
-    current_rate: Percent of tokens that are currently real.  Fake tokens
-      are the ones being imputed by the Generator.
-  '''
-  session.run(percent_real_update, feed_dict={new_rate: current_rate})
+class Command(ScrapyCommand):
     
-        publicKey, privateKey = generateKey(keySize)
-    print('\nWriting public key to file %s_pubkey.txt...' % name)
-    with open('%s_pubkey.txt' % name, 'w') as fo:
-        fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
+        def short_desc(self):
+        return 'Edit spider'
     
-        def _colision_resolution(self, key, data=None):
-        i = 1
-        new_key = self.hash_function(key + i*i)
+        def _find_template(self, template):
+        template_file = join(self.templates_dir, '%s.tmpl' % template)
+        if exists(template_file):
+            return template_file
+        print('Unable to find template: %s\n' % template)
+        print('Use 'scrapy genspider --list' to see all available templates.')
     
-            print('Enter the first string: ', end='')
-        S1 = raw_input().strip()
-    
-        def extract(self, **kwargs):
-        for i in self.streams:
-            s = self.streams[i]
-            _, s['container'], s['size'] = url_info(s['url'])
-            s['src'] = [s['url']]
-    
-    from ..common import *
-from ..extractor import VideoExtractor
-import xml.etree.ElementTree as ET
-    
-        html = get_content(rebuilt_url(url))
-    info = json.loads(match1(html, r'qualities':({.+?}),''))
-    title = match1(html, r''video_title'\s*:\s*'([^']+)'') or \
-            match1(html, r''title'\s*:\s*'([^']+)'')
-    title = unicodize(title)
-    
-        title = match1(html, r'&title=([^&]+)')
-    
-    def fetch_photo_url_list(url, size):
-    for pattern in url_patterns:
-        # FIXME: fix multiple matching since the match group is dropped
-        if match1(url, pattern[0]):
-            return fetch_photo_url_list_impl(url, size, *pattern[1:])
-    raise NotImplementedError('Flickr extractor is not supported for %s.' % url)
+            # Fixes Twisted 11.1.0+ support as HTTPClientFactory is expected
+        # to have _disconnectedDeferred. See Twisted r32329.
+        # As Scrapy implements it's own logic to handle redirects is not
+        # needed to add the callback _waitForDisconnect.
+        # Specifically this avoids the AttributeError exception when
+        # clientConnectionFailed method is called.
+        self._disconnectedDeferred = defer.Deferred()
