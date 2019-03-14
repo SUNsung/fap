5ac8c09a952f3193a0862e8012e34301db8933e6
SUNsung <weatherius@gmail.com>
2019-03-14 19:31:34 +0200

    Rewrite [58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py] File in: 2019-03-14 19:31:33

--- pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
+++ pyton/58sz5WQhefAAM580dcN1Bl1Ep258siKEfdRFEFk58KKg.py
@@ -1,108 +1,57 @@
 
         
-            def get_converter(self, mime):
-        if is_valid_mime(mime):
-            for converter_class in plugin_manager.get_converters():
-                if converter_class.supports(mime):
-                    return converter_class(mime)
+        from ansible.module_utils.aws.core import AnsibleAWSModule
+from ansible.module_utils.ec2 import boto3_conn, camel_dict_to_snake_dict
+from ansible.module_utils.ec2 import ec2_argument_spec, get_aws_connection_info
     
-            def get_auth(self, username=None, password=None):
-            assert username is None
-            assert password is None
-            assert self.raw_auth == BASIC_AUTH_HEADER_VALUE
-            return basic_auth(self.raw_auth)
+    database_name:
+    description: Name of database.
+    returned: When database name is specified
+    type: str
+    sample: 'mydatabase'
     
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
+        def role_del(self, name):
+        return self._post_json(method='role_del', name=name)
+    
+        def manage(self):
+        rc, out = self._exec(['list'])
+        if rc != 0:
+            self.module.fail_json(msg='Unable to list icinga2 features. '
+                                      'Ensure icinga2 is installed and present in binary path.')
     
     
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
     
-        if args.auth_plugin:
-        session.auth = {
-            'type': args.auth_plugin.auth_type,
-            'raw_auth': args.auth_plugin.raw_auth,
-        }
-    elif session.auth:
-        kwargs['auth'] = session.auth
+    import urllib
     
-    # A dictionary with options for the search language support, empty by default.
-# 'ja' uses this config value.
-# 'zh' user can custom change `jieba` dictionary path.
-#
-# html_search_options = {'type': 'default'}
-    
-            # Restart scan wizard
-        start_scanning(config, add_entities, client)
-    
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.host = config[CONF_HOST]
-        self.username = config[CONF_USERNAME]
-        self.password = config[CONF_PASSWORD]
-        self.last_results = []
-        data = self.get_actiontec_data()
-        self.success_init = data is not None
-        _LOGGER.info('canner initialized')
-    
-    _LOGGER = logging.getLogger(__name__)
-    
-                    _LOGGER.error('UPNP Responder socket exception occurred: %s',
-                              ex.__str__)
-                # without the following continue, a second exception occurs
-                # because the data object has not been initialized
-                continue
-    
-    _LOGGER = logging.getLogger(__name__)
-    
-        @property
-    def targets(self):
-        '''Return a dictionary of registered targets.'''
-        return {'test target name': 'test target id'}
-    
-            if data is not None:
-            body_message.update(data)
-            # Only one of text or attachment can be specified
-            if 'attachment' in body_message:
-                body_message.pop('text')
+        elif 'subject' in url:
+        titles = re.findall(r'data-title='([^']*)'>', html)
+        song_id = re.findall(r'<li class='song-item' id='([^']*)'', html)
+        song_ssid = re.findall(r'data-ssid='([^']*)'', html)
+        get_song_url = 'http://music.douban.com/j/songlist/get_song_url'
     
     
-CONF_APP_NAME = 'app_name'
-CONF_APP_ICON = 'app_icon'
-CONF_HOSTNAME = 'hostname'
-    
-        return current, hidden_dim
-
-    
-    Flexible network configuration is achieved by specifying the function name that
-builds a network module (e.g., the name of the conv backbone or the mask roi
-head). However we may wish to change names over time without breaking previous
-config files. This module provides backwards naming compatibility by providing
-a mapping from the old name to the new name.
-    
-    
-def add_fast_rcnn_blobs(blobs, im_scales, roidb):
-    '''Add blobs needed for training Fast R-CNN style models.'''
-    # Sample training RoIs from each image and append them to the blob lists
-    for im_i, entry in enumerate(roidb):
-        frcn_blobs = _sample_rois(entry, im_scales[im_i], im_i)
-        for k, v in frcn_blobs.items():
-            blobs[k].append(v)
-    # Concat the training blob lists into tensors
-    for k, v in blobs.items():
-        if isinstance(v, list) and len(v) > 0:
-            blobs[k] = np.concatenate(v)
-    # Add FPN multilevel training RoIs, if configured
-    if cfg.FPN.FPN_ON and cfg.FPN.MULTILEVEL_ROIS:
-        _add_multilevel_rois(blobs)
+def url_save_icourses(url, filepath, bar, total_size, dyn_callback=None, is_part=False, max_size=0, headers=None):
+    def dyn_update_url(received):
+        if callable(dyn_callback):
+            logging.debug('Calling callback %s for new URL from %s' % (dyn_callback.__name__, received))
+            return dyn_callback(received)
+    if bar is None:
+        bar = DummyProgressBar()
+    if os.path.exists(filepath):
+        if not force:
+            if not is_part:
+                bar.done()
+                print('Skipping %s: file already exists' % tr(os.path.basename(filepath)))
+            else:
+                filesize = os.path.getsize(filepath)
+                bar.update_received(filesize)
+            return
+        else:
+            if not is_part:
+                bar.done()
+                print('Overwriting %s' % os.path.basename(filepath), '...')
+    elif not os.path.exists(os.path.dirname(filepath)):
+        os.mkdir(os.path.dirname(filepath))
