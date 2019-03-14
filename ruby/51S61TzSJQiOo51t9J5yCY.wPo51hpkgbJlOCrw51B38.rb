3c708f6db763269267e1b07a6da7277d3a4c7d76
SUNsung <weatherius@gmail.com>
2019-03-14 22:22:43 +0200

    Rewrite [51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb] File in: 2019-03-14 22:22:42

--- ruby/51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb
+++ ruby/51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb
@@ -1,144 +1,62 @@
 
         
-        describe GivenDailyLike do
-    
-          if staff.topic_id.nil?
-        creator = PostCreator.new(Discourse.system_user,
-          raw: I18n.t('staff_category_description'),
-          title: I18n.t('category.topic_prefix', category: staff.name),
-          category: staff.name,
-          archetype: Archetype.default
-        )
-        post = creator.create
-    
-          def perform(start_id, stop_id)
-        update = '
-          latest_merge_request_diff_id = (
-            SELECT MAX(id)
-            FROM merge_request_diffs
-            WHERE merge_requests.id = merge_request_diffs.merge_request_id
-          )'.squish
-    
-    module Gitlab
-  module Ci
-    module Pipeline
-      # Class for preloading data associated with pipelines such as commit
-      # authors.
-      class Preloader
-        def self.preload!(pipelines)
-          ##
-          # This preloads all commits at once, because `Ci::Pipeline#commit` is
-          # using a lazy batch loading, what results in only one batched Gitaly
-          # call.
-          #
-          pipelines.each(&:commit)
-    
-          def raise_or_wait_for_rate_limit
-        rate_limit_counter.increment
-    
-            def sidekiq_worker_class
-          ImportDiffNoteWorker
-        end
-    
-              Gitlab::Database.bulk_insert(LabelLink.table_name, rows)
-        end
-    
-            def representation_class
-          Representation::LfsObject
-        end
-    
-          def cache_key_iid
-        if object.respond_to?(:noteable_id)
-          object.noteable_id
-        elsif object.respond_to?(:iid)
-          object.iid
+              def perform(yml)
+        (target, method_name, args) = YAML.load(yml)
+        msg = target.public_send(method_name, *args)
+        # The email method can return nil, which causes ActionMailer to return
+        # an undeliverable empty message.
+        if msg
+          deliver(msg)
         else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
+          raise '#{target.name}##{method_name} returned an undeliverable mail object'
         end
       end
-    end
-  end
-end
-
     
-              hash[:state] = hash[:state].to_sym
-          hash[:assignees].map! { |u| Representation::User.from_json_hash(u) }
-          hash[:author] &&= Representation::User.from_json_hash(hash[:author])
-    
-      it 'requires a URL or file uplaod' do
-    visit new_scenario_imports_path
-    click_on 'Start Import'
-    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
-  end
-    
-      describe '#relative_distance_of_time_in_words' do
-    it 'in the past' do
-      expect(relative_distance_of_time_in_words(Time.now-5.minutes)).to eq('5m ago')
-    end
-    
-              # Already exists
-          expect(weather_agent_diff.agent).to eq(agents(:bob_weather_agent))
-          valid_parsed_weather_agent_data.each do |key, value|
-            next if key == :type
-            expect(weather_agent_diff.send(key).current).to eq(agents(:bob_weather_agent).send(key))
-          end
-    
-          context '#restart_dead_workers' do
-        before do
-          mock.instance_of(HuginnScheduler).run!
-          mock.instance_of(DelayedJobWorker).run!
-          @agent_runner.send(:run_workers)
-    
-    describe HuginnScheduler do
-  before(:each) do
-    @rufus_scheduler = Rufus::Scheduler.new
-    @scheduler = HuginnScheduler.new
-    stub(@scheduler).setup {}
-    @scheduler.setup!(@rufus_scheduler, Mutex.new)
-  end
-    
-      describe 'migrating the 'make_message' format' do
-    it 'should work' do
-      expect(LiquidMigrator.convert_make_message('<message>')).to eq('{{message}}')
-      expect(LiquidMigrator.convert_make_message('<new.message>')).to eq('{{new.message}}')
-      expect(LiquidMigrator.convert_make_message('Hello <world>. How is <nested.life>')).to eq('Hello {{world}}. How is {{nested.life}}')
-    end
-  end
-    
-      describe '#working?' do
-    it 'should not be working until the first event was received' do
-      expect(@checker).not_to be_working
-      @checker.last_receive_at = Time.now
-      expect(@checker).to be_working
-    end
-  end
-    
-          unless root?
-        raise Invalid, 'missing name' if !name || name.empty?
-        raise Invalid, 'missing path' if !path || path.empty?
-        raise Invalid, 'missing type' if !type || type.empty?
-      end
-    end
-    
-              workspace_path = 'Examples.xcworkspace'
-          workspace = Xcodeproj::Workspace.new_from_xcworkspace(workspace_path)
-          workspace.schemes.each do |scheme_name, project_path|
-            next if scheme_name == 'Pods'
-            next if project_path.end_with? 'Pods.xcodeproj'
-            puts '    Building scheme: #{scheme_name}'
-    
-            def initialize(argv)
-          @pod_name = argv.shift_argument
-          @short_output = argv.flag?('short')
-          super
+            begin
+          File.open(fp.path, 'a') { |tmpfp| fp.reopen(tmpfp) }
+          fp.sync = true
+          nr += 1
+        rescue IOError, Errno::EBADF
+          # not much we can do...
         end
+      end
+      nr
+    rescue RuntimeError => ex
+      # RuntimeError: ObjectSpace is disabled; each_object will only work with Class, pass -X+O to enable
+      puts 'Unable to reopen logs: #{ex.message}'
+    end
     
-    ```
-#{plugins_string}
-```
-#{markdown_podfile}
-EOS
+        # Add any facets or actuators that are needed.
+    # TODO(jcraig): add manpage actuator to enable install wo/ man pages
+    
+        scripts[:after_install] = template(File.join('pleaserun', 'scripts', 'after-install.sh')).result(binding)
+    scripts[:before_remove] = template(File.join('pleaserun', 'scripts', 'before-remove.sh')).result(binding)
+  end # def input
+    
+        # If this is a hard-link, there's no metadata to copy.
+    # If this is a symlink, what it points to hasn't been copied yet.
+    return if source_stat.ino == dest_stat.ino || dest_stat.symlink?
+    
+      failure_message do |actual|
+    return 'Expected #{actual} to be a Tmuxinator::Pane' unless is_pane
+    
+        def self.parse_settings(args)
+      settings = args.select { |x| x.match(/.*=.*/) }
+      args.reject! { |x| x.match(/.*=.*/) }
+    
+          # $TMUXINATOR_CONFIG (and create directory) or ''.
+      def environment
+        environment = ENV['TMUXINATOR_CONFIG']
+        return '' if environment.to_s.empty? # variable is unset (nil) or blank
+        FileUtils::mkdir_p(environment) unless File.directory?(environment)
+        environment
+      end
+    
+        let(:tmux_part) { project.tmux }
+  end
+    
+        no_commands do
+      def new_project(name)
+        project_file = find_project_file(name, options[:local])
+        Kernel.system('$EDITOR #{project_file}') || doctor
       end
