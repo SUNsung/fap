227160339fba374e00c4438833fecbafa68ebe63
SUNsung <weatherius@gmail.com>
2019-03-15 21:58:58 +0200

    Rewrite [09lYkxlt4HzqI097WCOh0aAejE096yUcXAYjKMU09iCo.py] File in: 2019-03-15 21:58:57

--- pyton/09lYkxlt4HzqI097WCOh0aAejE096yUcXAYjKMU09iCo.py
+++ pyton/09lYkxlt4HzqI097WCOh0aAejE096yUcXAYjKMU09iCo.py
@@ -1,187 +1,169 @@
 
         
-            # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
-    app.add_url_rule('/', endpoint='index')
+          with open(embedding_file) as f_in:
+    embeddings = np.load(f_in)
     
-    import click
-from flask import current_app, g
-from flask.cli import with_appcontext
+    flags = tf.app.flags
+flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
+                    'Directory for saving data.')
+flags.DEFINE_string('datafile_name', 'itb_rnn',
+                    'Name of data file for input case.')
+flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
+flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
+flags.DEFINE_integer('C', 800, 'Number of conditions')
+flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
+flags.DEFINE_float('train_percentage', 4.0/5.0,
+                   'Percentage of train vs validation trials')
+flags.DEFINE_integer('nreplications', 5,
+                     'Number of spikifications of the same underlying rates.')
+flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
+flags.DEFINE_float('dt', 0.010, 'Time bin')
+flags.DEFINE_float('max_firing_rate', 30.0,
+                   'Map 1.0 of RNN to a spikes per second')
+flags.DEFINE_float('u_std', 0.25,
+                   'Std dev of input to integration to bound model')
+flags.DEFINE_string('checkpoint_path', 'SAMPLE_CHECKPOINT',
+                    '''Path to directory with checkpoints of model
+                    trained on integration to bound task. Currently this
+                    is a placeholder which tells the code to grab the
+                    checkpoint that is provided with the code
+                    (in /trained_itb/..). If you have your own checkpoint
+                    you would like to restore, you would point it to
+                    that path.''')
+FLAGS = flags.FLAGS
     
+      def _score_patches(self, word_patches):
+    '''Score a 2D matrix of word_patches and stitch results together.'''
+    batch_size, num_timesteps = self.shape
+    nrow, ncol = len(word_patches), len(word_patches[0])
+    max_len = num_timesteps * ncol
+    probs = np.zeros([0, max_len])  # accumulate results into this.
     
-class AuthActions(object):
-    def __init__(self, client):
-        self._client = client
+    '''IMDB data loader and helpers.'''
     
-        auth.login()
-    response = client.get('/')
-    assert b'test title' in response.data
-    assert b'by test on 2018-01-01' in response.data
-    assert b'test\nbody' in response.data
-    assert b'href='/1/update'' in response.data
+      Args:
+    gen_logits:  Generator logits.
+    gen_labels:  Labels for the correct token.
+    dis_values:  Discriminator values Tensor of shape [batch_size,
+      sequence_length].
+    is_real_input:  Tensor indicating whether the label is present.
     
-        def check(self, value):
-        return isinstance(value, UUID)
-    
-        def dispatch_request(self, *args, **kwargs):
-        meth = getattr(self, request.method.lower(), None)
-    
-    
-class JSONMixin(object):
-    '''Common mixin for both request and response objects to provide JSON
-    parsing capabilities.
-    
-    
-if __name__ == '__main__':
-    main()
+      return variable_mapping
 
     
-        params['api_key'] = module.params['token']
+    # ZoneoutWrapper.
+from regularization import zoneout
     
-    # Create a random animal
-def random_animal():
-    '''Let's be dynamic!'''
-    return random.choice([Dog, Cat])()
+    # Ignore noisy twisted deprecation warnings
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
+del warnings
     
-        @lazy_property2
-    def parents(self):
-        self.call_count2 += 1
-        return 'Father and mother'
+        def help(self):
+        '''An extensive help for the command. It will be shown when using the
+        'help' command. It can contain newlines, since not post-formatting will
+        be applied to its contents.
+        '''
+        return self.long_desc()
     
-        def product_list(self):
-        return self.data['products'].keys()
+        def __enter__(self):
+        from scrapy.utils.test import get_testenv
+        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
+        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
+                                     env=get_testenv())
+        self.proc.stdout.readline()
     
-    ### OUTPUT ###
-# Ellipse: 1
-# Ellipse: 2
-# Ellipse: 3
-# Ellipse: 4
-
-    
-    *References:
-https://sourcemaking.com/design_patterns/decorator
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-    
-        if P2only:
-        # use only the finest level
-        return blobs_fpn[-1], dim_fpn, spatial_scales_fpn[-1]
-    else:
-        # use all levels
-        return blobs_fpn, dim_fpn, spatial_scales_fpn
+            assertion = (self.min_bound <= occurrences <= self.max_bound)
     
     
-def add_fpn_retinanet_outputs(model, blobs_in, dim_in, spatial_scales):
-    '''RetinaNet head. For classification and box regression, we can chose to
-    have the same conv tower or a separate tower. 'bl_feat_list' stores the list
-    of feature blobs for bbox prediction. These blobs can be shared cls feature
-    blobs if we share the tower or else are independent blobs.
-    '''
-    dim_out = dim_in
-    k_max = cfg.FPN.RPN_MAX_LEVEL  # coarsest level of pyramid
-    k_min = cfg.FPN.RPN_MIN_LEVEL  # finest level of pyramid
-    A = len(cfg.RETINANET.ASPECT_RATIOS) * cfg.RETINANET.SCALES_PER_OCTAVE
+logger = logging.getLogger(__name__)
     
-    from detectron.core.config import cfg
-import detectron.modeling.FPN as fpn
-import detectron.roi_data.keypoint_rcnn as keypoint_rcnn_roi_data
-import detectron.roi_data.mask_rcnn as mask_rcnn_roi_data
-import detectron.utils.blob as blob_utils
-import detectron.utils.boxes as box_utils
+    from twisted.web.client import HTTPClientFactory
+from twisted.web.http import HTTPClient
+from twisted.internet import defer
     
-        heats, weights = keypoint_utils.keypoints_to_heatmap_labels(
-        sampled_keypoints, sampled_fg_rois
-    )
+        def _format_cookie(self, cookie):
+        # build cookie string
+        cookie_str = '%s=%s' % (cookie['name'], cookie['value'])
     
+    if args.quiet:
+    jieba.setLogLevel(60)
+if args.pos:
+    import jieba.posseg
+    posdelim = args.pos
+    def cutfunc(sentence, _, HMM=True):
+        for w, f in jieba.posseg.cut(sentence, HMM):
+            yield w + posdelim + f
+else:
+    cutfunc = jieba.cut
     
-def get_minibatch(roidb):
-    '''Given a roidb, construct a minibatch sampled from it.'''
-    # We collect blobs from each image onto a list and then concat them into a
-    # single tensor, hence we initialize each blob to an empty list
-    blobs = {k: [] for k in get_minibatch_blob_names()}
-    # Get the input image blob, formatted for caffe2
-    im_blob, im_scales = _get_image_blob(roidb)
-    blobs['data'] = im_blob
-    if cfg.RPN.RPN_ON:
-        # RPN-only or end-to-end Faster/Mask R-CNN
-        valid = rpn_roi_data.add_rpn_blobs(blobs, im_scales, roidb)
-    elif cfg.RETINANET.RETINANET_ON:
-        im_width, im_height = im_blob.shape[3], im_blob.shape[2]
-        # im_width, im_height corresponds to the network input: padded image
-        # (if needed) width and height. We pass it as input and slice the data
-        # accordingly so that we don't need to use SampleAsOp
-        valid = retinanet_roi_data.add_retinanet_blobs(
-            blobs, im_scales, roidb, im_width, im_height
-        )
-    else:
-        # Fast R-CNN like models trained on precomputed proposals
-        valid = fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
-    return blobs, valid
+    from __future__ import absolute_import, unicode_literals
+import sys
+from operator import itemgetter
+from collections import defaultdict
+import jieba.posseg
+from .tfidf import KeywordExtractor
+from .._compat import *
     
+        def __init__(self):
+        self._total_rel = None
+        self._rel_sample = None
+        self._need_to_skip_char_num = None
+        self._last_char_order = None
+        self._done = None
+        self.reset()
     
-class UndirectWeightedGraph:
-    d = 0.85
+        @property
+    def charset_name(self):
+        return 'ISO-8859-1'
     
-            for y in obs_states:
-            prob, state = max((V[t - 1][y0] + trans_p[y0].get(y, MIN_INF) +
-                               emit_p[y].get(obs[t], MIN_FLOAT), y0) for y0 in prev_states)
-            V[t][y] = prob
-            mem_path[t][y] = state
+    # To be accurate, the length of class 6 can be either 2 or 4.
+# But it is not necessary to discriminate between the two since
+# it is used for frequency analysis only, and we are validating
+# each code range there as well. So it is safe to set it to be
+# 2 here.
+GB2312_CHAR_LEN_TABLE = (0, 1, 1, 1, 1, 1, 2)
     
-    s = '此外，公司拟对全资子公司吉林欧亚置业有限公司增资4.3亿元，增资后，吉林欧亚置业注册资本由7000万元增加到5亿元。吉林欧亚置业主要经营范围为房地产开发及百货零售等业务。目前在建吉林欧亚城市商业综合体项目。2013年，实现营业收入0万元，实现净利润-139.13万元。'
-for x, w in jieba.analyse.extract_tags(s, withWeight=True):
-    print('%s %s' % (x, w))
+            charset_name = self._model['charset_name']
+        if self.state == ProbingState.DETECTING:
+            if self._total_seqs > self.SB_ENOUGH_REL_THRESHOLD:
+                confidence = self.get_confidence()
+                if confidence > self.POSITIVE_SHORTCUT_THRESHOLD:
+                    self.logger.debug('%s confidence = %s, we have a winner',
+                                      charset_name, confidence)
+                    self._state = ProbingState.FOUND_IT
+                elif confidence < self.NEGATIVE_SHORTCUT_THRESHOLD:
+                    self.logger.debug('%s confidence = %s, below negative '
+                                      'shortcut threshhold %s', charset_name,
+                                      confidence,
+                                      self.NEGATIVE_SHORTCUT_THRESHOLD)
+                    self._state = ProbingState.NOT_ME
     
-        def transform(self, node, results):
-        self.found_future_import = True
-        return self.new_future_import(node)
-    
-        IOLoop.instance().start()
-    
-    For more information on application-level routing see docs for `~.web.Application`.
-    
-        def test_oauth10a_redirect(self):
-        response = self.fetch('/oauth10a/client/login', follow_redirects=False)
-        self.assertEqual(response.code, 302)
-        self.assertTrue(
-            response.headers['Location'].endswith(
-                '/oauth1/server/authorize?oauth_token=zxcv'
-            )
-        )
-        # the cookie is base64('zxcv')|base64('1234')
-        self.assertTrue(
-            '_oauth_request_token='enhjdg==|MTIzNA==''
-            in response.headers['Set-Cookie'],
-            response.headers['Set-Cookie'],
-        )
-    
-    # Char to FreqOrder table ,
-JIS_TABLE_SIZE = 4368
-    
-    class EUCJPContextAnalysis(JapaneseContextAnalysis):
-    def get_order(self, byte_str):
-        if not byte_str:
-            return -1, 1
-        # find out current char's byte length
-        first_char = byte_str[0]
-        if (first_char == 0x8E) or (0xA1 <= first_char <= 0xFE):
-            char_len = 2
-        elif first_char == 0x8F:
-            char_len = 3
-        else:
-            char_len = 1
+        def reset(self):
+        super(SingleByteCharSetProber, self).reset()
+        # char order of last character
+        self._last_order = 255
+        self._seq_counters = [0] * SequenceLikelihood.get_num_categories()
+        self._total_seqs = 0
+        self._total_char = 0
+        # characters that fall in our sampling range
+        self._freq_char = 0
     
         def get_confidence(self):
-        return self.distribution_analyzer.get_confidence()
+        context_conf = self.context_analyzer.get_confidence()
+        distrib_conf = self.distribution_analyzer.get_confidence()
+        return max(context_conf, distrib_conf)
 
     
-    GB2312_SM_MODEL = {'class_table': GB2312_CLS,
-                   'class_factor': 7,
-                   'state_table': GB2312_ST,
-                   'char_len_table': GB2312_CHAR_LEN_TABLE,
-                   'name': 'GB2312'}
+        def feed(self, byte_str):
+        for c in byte_str:
+            coding_state = self.coding_sm.next_state(c)
+            if coding_state == MachineState.ERROR:
+                self._state = ProbingState.NOT_ME
+                break
+            elif coding_state == MachineState.ITS_ME:
+                self._state = ProbingState.FOUND_IT
+                break
+            elif coding_state == MachineState.START:
+                if self.coding_sm.get_current_charlen() >= 2:
+                    self._num_mb_chars += 1
