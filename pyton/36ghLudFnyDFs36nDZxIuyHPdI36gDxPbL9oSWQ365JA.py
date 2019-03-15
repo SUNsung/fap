8a597c3966a69477d5ff253983308101fa0e89d9
SUNsung <weatherius@gmail.com>
2019-03-15 19:09:45 +0200

    Rewrite [36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py] File in: 2019-03-15 19:09:45

--- pyton/36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py
+++ pyton/36ghLudFnyDFs36nDZxIuyHPdI36gDxPbL9oSWQ365JA.py
@@ -1,200 +1,22 @@
 
         
-                    gc.collect()
-            print('- benchmarking Lasso')
-            clf = Lasso(alpha=alpha, fit_intercept=False,
-                        precompute=precompute)
-            tstart = time()
-            clf.fit(X, Y)
-            lasso_results.append(time() - tstart)
+        # We used to check for high-bit characters, but after much discussion we
+# decided those were OK, as long as they were in UTF-8 and didn't represent
+# hard-coded international strings, which belong in a separate i18n file.
     
-        for n_components in [i.astype(int) for i in
-                         np.linspace(data.shape[1] // 10,
-                                     data.shape[1], num=4)]:
-        all_times = defaultdict(list)
-        all_errors = defaultdict(list)
-        pca = PCA(n_components=n_components)
-        rpca = PCA(n_components=n_components, svd_solver='randomized',
-                   random_state=1999)
-        results_dict = {k: benchmark(est, data) for k, est in [('pca', pca),
-                                                               ('rpca', rpca)]}
+    *Where is the pattern used practically?
+    
+        def test_relatives_not_in_properties(self):
+        self.assertNotIn('relatives', self.John.__dict__)
     
     
-def rbf_kernels(X, n_jobs):
-    return pairwise_kernels(X, metric='rbf', n_jobs=n_jobs, gamma=0.1)
-    
-        xx = range(0, n * step, step)
-    plt.figure('scikit-learn tree benchmark results')
-    plt.subplot(211)
-    plt.title('Learning with varying number of samples')
-    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
-    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
-    plt.legend(loc='upper left')
-    plt.xlabel('number of samples')
-    plt.ylabel('Time (s)')
-    
-    
-if __name__ == '__main__':
-    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
-    # block to be able to use a multi-core grid search that also works under
-    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
-    # The multiprocessing module is used as the backend of joblib.Parallel
-    # that is used when n_jobs != 1 in GridSearchCV
-    
-    from sklearn.cluster import AgglomerativeClustering
-from sklearn.neighbors import kneighbors_graph
-    
-    These images how similar features are merged together using
-feature agglomeration.
-'''
-print(__doc__)
-    
-    #----------------------------------------------------------------------
-# 2D embedding of the digits dataset
-print('Computing embedding')
-X_red = manifold.SpectralEmbedding(n_components=2).fit_transform(X)
-print('Done.')
-    
-        publicKey, privateKey = generateKey(keySize)
-    print('\nWriting public key to file %s_pubkey.txt...' % name)
-    with open('%s_pubkey.txt' % name, 'w') as fo:
-        fo.write('%d,%d,%d,%d' % (publicKey[0], publicKey[1], publicKey[2], publicKey[3]))
-    
-            while self.values[new_key] is not None \
-                and self.values[new_key] != key:
-            i += 1
-            new_key = self.hash_function(key + i*i) if not \
-                self.balanced_factor() >= self.lim_charge else None
-    
-    
-class SubArray:
-    
-    	currPos = 0
-	while currPos < len(bitString):
-		currPart = bitString[currPos:currPos+512]
-		mySplits = []
-		for i in range(16):
-			mySplits.append(int(rearrange(currPart[32*i:32*i+32]),2))
-		yield mySplits
-		currPos += 512
-    
-    def main():
-    '''
-    In this demonstration we're generating a sample data set from the sin function in numpy.
-    We then train a decision tree on the data set and use the decision tree to predict the
-    label of 10 different test values. Then the mean squared error over this test is displayed.
-    '''
-    X = np.arange(-1., 1., 0.005)
-    y = np.sin(X)
-    
-    Requirements:
-  - sklearn
-  - numpy
-  - matplotlib
-    
-    '''
-* Wondering how this method works !
-* It's pretty simple.
-* Let's say you need to calculate a ^ b
-* RULE 1 : a ^ b = (a*a) ^ (b/2) ---- example : 4 ^ 4 = (4*4) ^ (4/2) = 16 ^ 2
-* RULE 2 : IF b is ODD, then ---- a ^ b = a * (a ^ (b - 1)) :: where (b - 1) is even.
-* Once b is even, repeat the process to get a ^ b
-* Repeat the process till b = 1 OR b = 0, because a^1 = a AND a^0 = 1
-*
-* As far as the modulo is concerned,
-* the fact : (a*b) % c = ((a%c) * (b%c)) % c
-* Now apply RULE 1 OR 2 whichever is required.
-'''
-
-    
-    
-def random_letters(ctbi, i):
-  pass  # Put your code here...
-    
-                load_tests = None
-            tests = None
-            name = self._get_name_from_path(full_path)
-            try:
-                package = self._get_module_from_name(name)
-            except case.SkipTest as e:
-                return _make_skipped_test(name, e, self.suiteClass), False
-            except:
-                error_case, error_message = \
-                    _make_failed_import_test(name, self.suiteClass)
-                self.errors.append(error_message)
-                return error_case, False
-            else:
-                load_tests = getattr(package, 'load_tests', None)
-                # Mark this package as being in load_tests (possibly ;))
-                self._loading_packages.add(name)
-                try:
-                    tests = self.loadTestsFromModule(package, pattern=pattern)
-                    if load_tests is not None:
-                        # loadTestsFromModule(package) has loaded tests for us.
-                        return tests, False
-                    return tests, True
-                finally:
-                    self._loading_packages.discard(name)
-        else:
-            return None, False
-    
-    PYTHON3_OSERROR_EXCEPTIONS = (
-    'BrokenPipeError',
-    'ChildProcessError',
-    'ConnectionAbortedError',
-    'ConnectionError',
-    'ConnectionRefusedError',
-    'ConnectionResetError',
-    'FileExistsError',
-    'FileNotFoundError',
-    'InterruptedError',
-    'IsADirectoryError',
-    'NotADirectoryError',
-    'PermissionError',
-    'ProcessLookupError',
-    'TimeoutError',
-)
-    
-    
-def open_binary(package: Package, resource: Resource) -> BinaryIO:
-    '''Return a file-like object opened for binary reading of the resource.'''
-    resource = _normalize_path(resource)
-    package = _get_package(package)
-    reader = _get_resource_reader(package)
-    if reader is not None:
-        return reader.open_resource(resource)
-    _check_location(package)
-    absolute_package_path = os.path.abspath(package.__spec__.origin)
-    package_path = os.path.dirname(absolute_package_path)
-    full_path = os.path.join(package_path, resource)
-    try:
-        return open(full_path, mode='rb')
-    except OSError:
-        # Just assume the loader is a resource loader; all the relevant
-        # importlib.machinery loaders are and an AttributeError for
-        # get_data() will make it clear what is needed from the loader.
-        loader = cast(ResourceLoader, package.__spec__.loader)
-        data = None
-        if hasattr(package.__spec__.loader, 'get_data'):
-            with suppress(OSError):
-                data = loader.get_data(full_path)
-        if data is None:
-            package_name = package.__spec__.name
-            message = '{!r} resource not found in {!r}'.format(
-                resource, package_name)
-            raise FileNotFoundError(message)
-        else:
-            return BytesIO(data)
-    
-    # Create the base text message.
-msg = EmailMessage()
-msg['Subject'] = 'Ayons asperges pour le déjeuner'
-msg['From'] = Address('Pepé Le Pew', 'pepe', 'example.com')
-msg['To'] = (Address('Penelope Pussycat', 'penelope', 'example.com'),
-             Address('Fabrette Pussycat', 'fabrette', 'example.com'))
-msg.set_content('''\
-Salut!
-    
-    
-if __name__ == '__main__':
-    main()
+class TestDispatcherFeatures(unittest.TestCase):
+    def setUp(self):
+        self.dispatcher = PrototypeDispatcher()
+        self.prototype = Prototype()
+        c = self.prototype.clone()
+        a = self.prototype.clone(value='a-value', ext_value='E')
+        b = self.prototype.clone(value='b-value', diff=True)
+        self.dispatcher.register_object('A', a)
+        self.dispatcher.register_object('B', b)
+        self.dispatcher.register_object('C', c)
