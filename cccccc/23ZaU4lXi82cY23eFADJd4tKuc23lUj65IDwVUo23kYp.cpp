ecc3006cf6d619c980e95cfb974c8a77aa9e6621
SUNsung <weatherius@gmail.com>
2019-03-15 16:52:12 +0200

    Rewrite [23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp] File in: 2019-03-15 16:52:11

--- cccccc/23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp
+++ cccccc/23ZaU4lXi82cY23eFADJd4tKuc23lUj65IDwVUo23kYp.cpp
@@ -1,384 +1,65 @@
 
         
-        using namespace swift::sys;
-using llvm::StringRef;
+        #ifndef GTEST_INCLUDE_GTEST_GTEST_SPI_H_
+#define GTEST_INCLUDE_GTEST_GTEST_SPI_H_
     
-    void swift::printOpaquePrefixMap(raw_ostream &out, void *_root,
-                         void (*printNodeData)(raw_ostream &out, void *node)) {
-  auto root = reinterpret_cast<Node*>(_root);
-  if (!root) {
-    out << '(empty)\n';
-    return;
-  }
-  TreePrinter(out, *printNodeData).print(root, ChildKind::Root);
-}
+    // Now the tricky part: you need to register all test patterns before
+// you can instantiate them.  The first argument of the macro is the
+// test case name; the rest are the names of the tests in this test
+// case.
+REGISTER_TYPED_TEST_CASE_P(FooTest,
+                           DoesBlah, HasPropertyA);
     
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-      if (auto subTypedef = type->getAs<clang::TypedefType>()) {
-    if (classifyTypedef(subTypedef->getDecl()))
-      return forTypedef(subTypedef->getDecl());
-    return forInvalid();
-  }
-    
-    namespace swift {
-namespace importer {
-    }
-    }
-    
-      bool isImportAsMember() const { return bool(effectiveDC); }
-    
-    
-    {
-    {    switch (Bytes) {
-    case 1:
-      OutUTF8.push_back(S);
-      break;
-    case 2: {
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xC0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      break;
-    }
-    case 3: {
-      uint8_t Byte3 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xE0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      OutUTF8.push_back(Byte3);
-      break;
-    }
-    case 4: {
-      uint8_t Byte4 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte3 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xF0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      OutUTF8.push_back(Byte3);
-      OutUTF8.push_back(Byte4);
-      break;
-    }
-    }
-  }
-  return true;
-}
-    
-     public:
-  /*! \brief cuda kernel argument descriptor */
-  struct ArgType {
-    /*! \brief whether argument is NDArray */
-    bool is_ndarray;
-    /*! \brief whether argument is constant (input) */
-    bool is_const;
-    /*! \brief data type of argument */
-    mshadow::TypeFlag dtype;
-  };
-  /*! \brief Cuda kernel */
-  class Kernel {
-   public:
-    /*! \brief Launch the kernel */
-    void Launch(const Context& ctx, const std::vector<dmlc::any>& args,
-                uint32_t grid_dim_x, uint32_t grid_dim_y, uint32_t grid_dim_z,
-                uint32_t block_dim_x, uint32_t block_dim_y, uint32_t block_dim_z,
-                uint32_t shared_mem);
-    /*! \brief kernel interface signature */
-    const std::vector<ArgType>& signature() { return signature_; }
-    }
-    
-    /**
- * \brief The interface to convert mxnet's tensor to caffe's blob
- * \brief called in caffe_operator_inl.h
- */
-template<typename Device, typename Dtype>
-void TBlob2CaffeBlob(caffeMemoryTypes memType,
-                     typename std::vector< ::caffe::Blob<Dtype>*>::iterator blob,
-                     typename std::vector<TBlob>::const_iterator tblob,
-                     int n = 1) {
-  for (int i = 0; i < n; ++i, ++blob, ++tblob) {
-    (*blob)->Reshape(TShape2Vector((*tblob).shape_));
-    SetDataGradToBlob<Device, Dtype>(memType, blob, tblob);
-  }
-}
-    
-    template<>
-Operator* CreateOp<cpu>(CaffeOpParam param, int dtype) {
-  Operator *op = NULL;
-  switch (dtype) {
-  case mshadow::kFloat32:
-    op = new CaffeOp<cpu, float>(param);
-    break;
-  case mshadow::kFloat64:
-    op = new CaffeOp<cpu, double>(param);
-    break;
-  case mshadow::kFloat16:
-    LOG(FATAL) << 'float16 layer is not supported by caffe';
-    break;
-  default:
-    LOG(FATAL) << 'Unsupported type ' << dtype;
-  }
-  return op;
-}
-    
-    
-    {
-    {}  // namespace exec
-}  // namespace mxnet
-
-    
-      virtual void BeforeFirst(void) {
-    if (param_.round_batch == 0 || num_overflow_ == 0) {
-      // otherise, we already called before first
-      base_->BeforeFirst();
-    } else {
-      num_overflow_ = 0;
-    }
-    head_ = 1;
-  }
-    
-    void Assembler::mtspr(const SpecialReg spr, const Reg64& rs) {
-  EmitXFXForm(31, rn(rs), spr, 467);
-}
-    
-    
-    {private:
-  APCHandle m_handle;
-  APCHandle* m_arrayHandle;
-  CollectionType m_colType;
+    // A concrete DeathTestFactory implementation for normal use.
+class DefaultDeathTestFactory : public DeathTestFactory {
+ public:
+  virtual bool Create(const char* statement, const RE* regex,
+                      const char* file, int line, DeathTest** test);
 };
     
-    void Config::ParseIniFile(const std::string &filename,
-                          const bool is_system /* = true */) {
-  IniSettingMap ini = IniSettingMap();
-  Config::ParseIniFile(filename, ini, false, is_system);
+      // Compares two C strings, ignoring case.  Returns true iff they
+  // have the same content.
+  //
+  // Unlike strcasecmp(), this function can handle NULL argument(s).
+  // A NULL C string is considered different to any non-NULL C string,
+  // including the empty string.
+  static bool CaseInsensitiveCStringEquals(const char* lhs,
+                                           const char* rhs);
+    
+      virtual int GetNextPrime(int p) const {
+    for (int n = p + 1; n < is_prime_size_; n++) {
+      if (is_prime_[n]) return n;
+    }
+    }
+    
+        const QueueNode<E>* const old_head = head_;
+    head_ = head_->next_;
+    size_--;
+    if (size_ == 0) {
+      last_ = NULL;
+    }
+    
+    
+    {    // exception out_of_range.401
+    try
+    {
+        // try to write beyond the array limit
+        array.at(5) = 'sixth';
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
 }
+
     
-    public:
-  static void Add(InfoVec& out, const char* name, const std::string& value);
-  static void AddServerStats(InfoVec& out, const char* name,
-                             const char* statsName = nullptr);
-    
-    struct FileStreamWrapper final : Stream::Wrapper {
-  static req::ptr<MemFile> openFromCache(const String& filename,
-                                         const String& mode);
-  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-  int access(const String& path, int mode) override {
-    return ::access(File::TranslatePath(path).data(), mode);
-  }
-  int stat(const String& path, struct stat* buf) override {
-    return ::stat(File::TranslatePath(path).data(), buf);
-  }
-  int lstat(const String& path, struct stat* buf) override {
-    return ::lstat(File::TranslatePath(path).data(), buf);
-  }
-  int unlink(const String& path) override;
-  int rename(const String& oldname, const String& newname) override;
-  int mkdir(const String& path, int mode, int options) override;
-  int rmdir(const String& path, int /*options*/) override {
-    ERROR_RAISE_WARNING(::rmdir(File::TranslatePath(path).data()));
-    return ret;
-  }
-  bool isNormalFileStream() const override { return true; }
-    }
-    
-    
-    {        return const_cast<int8_t*>(DataBuffer<int8_t>());
-    }
-    
-    
-    {            // Determine unpacked shape
-            m_unpackedShape = GetUnpackedShape(sampleShape, sampleDynamicAxes, packedDataLayout);
-        }
-    
-            Dictionary initConfig;
-        initConfig[InitializerTypeAttributeName] = initializerTypeName;
-        initConfig[ScaleAttributeName] = scale;
-        // Initializers are sometimes created as default arguments in python.
-        // If the value for an automatically-selected seed is assigned here, 
-        // subsequent calls to SetFixedRandomSeed will be ignored.
-        initConfig[RandomSeedAttributeName] = (size_t)seed;        
-        return initConfig;
-    }
-    
-    static ParameterInitializer CreateInitializer(const std::wstring& initializerTypeName, double scale, int outputRank, int filterRank, unsigned long seed)
+        // out_of_range.109
+    try
     {
-        if (scale <= 0)
-            InvalidArgument('CreateInitializer: scale value for initializer '%S' cannot be 0.', 
-                initializerTypeName.c_str());
-    
-                // Validate that each of the dynamic axes are unique
-            std::unordered_set<Axis> uniqueDynamicAxis;
-            for (auto& currentDynamicAxis : dynamicAxes)
-            {
-                auto retVal = uniqueDynamicAxis.insert(currentDynamicAxis);
-                if (!retVal.second)
-                    InvalidArgument('Dynamic axis named %S is specified more than once for Variable '%S'', currentDynamicAxis.Name().c_str(), AsString().c_str());
-            }
-    
-            if (::WaitForSingleObject(m_handle, wait ? INFINITE : 0) != WAIT_OBJECT_0)
-        {
-            // failed to acquire
-            int rc = ::CloseHandle(m_handle);
-            if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
-            {
-                RuntimeError('Acquire: Handler close failure with error code %d', ::GetLastError());
-            }
-            m_handle = NULL;
-            return false;
-        }
-    
-    #pragma once
-#ifndef _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
-#endif
-#ifdef _WIN32
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif // NOMINMAX
-#pragma comment(lib, 'Dbghelp.lib')
-#else
-#include <execinfo.h>
-#include <cxxabi.h>
-#endif
-    
-    
+        // try to use an array index that is not a number
+        json::const_reference ref = j.at('/array/one'_json_pointer);
+    }
+    catch (json::parse_error& e)
     {
-    {            if (map.size() > RAND_MAX * (size_t) RAND_MAX)
-                RuntimeError('RandomOrdering: too large training set: need to change to different random generator!');
-            srand((unsigned int) seed);
-            size_t retries = 0;
-            foreach_index (t, map)
-            {
-                for (int tries = 0; tries < 5; tries++)
-                {
-                    // swap current pos with a random position
-                    // Random positions are limited to t+randomizationrange.
-                    // This ensures some locality suitable for paging with a sliding window.
-                    const size_t tbegin = max((size_t) t, randomizationrange / 2) - randomizationrange / 2; // range of window  --TODO: use bounds() function above
-                    const size_t tend = min(t + randomizationrange / 2, map.size());
-                    assert(tend >= tbegin);                  // (guard against potential numeric-wraparound bug)
-                    const size_t trand = rand(tbegin, tend); // random number within windows
-                    assert((size_t) t <= trand + randomizationrange / 2 && trand < (size_t) t + randomizationrange / 2);
-                    // if range condition is fulfilled then swap
-                    if (trand <= map[t] + randomizationrange / 2 && map[t] < trand + randomizationrange / 2 && (size_t) t <= map[trand] + randomizationrange / 2 && map[trand] < (size_t) t + randomizationrange / 2)
-                    {
-                        std::swap(map[t], map[trand]);
-                        break;
-                    }
-                    // but don't multi-swap stuff out of its range (for swapping positions that have been swapped before)
-                    // instead, try again with a different random number
-                    retries++;
-                }
-            }
-            fprintf(stderr, 'RandomOrdering: %lu retries for %lu elements (%.1f%%) to ensure window condition\n', (unsigned long) retries, (unsigned long) map.size(), 100.0 * retries / map.size());
-            // ensure the window condition
-            foreach_index (t, map)
-                assert((size_t) t <= map[t] + randomizationrange / 2 && map[t] < (size_t) t + randomizationrange / 2);
-#if 0 // and a live check since I don't trust myself here yet
-            foreach_index (t, map) if (!((size_t) t <= map[t] + randomizationrange/2 && map[t] < (size_t) t + randomizationrange/2))
-            {
-                fprintf (stderr, 'RandomOrdering: windowing condition violated %d -> %d\n', t, map[t]);
-                LogicError('RandomOrdering: windowing condition violated');
-            }
-#endif
-#if 0 // test whether it is indeed a unique complete sequence
-            auto map2 = map;
-            ::sort (map2.begin(), map2.end());
-            foreach_index (t, map2) assert (map2[t] == (size_t) t);
-#endif
-            fprintf(stderr, 'RandomOrdering: recached sequence for seed %d: %d, %d, ...\n', (int) seed, (int) map[0], (int) map[1]);
-            currentseed = seed;
-        }
-        return map; // caller can now access it through operator[]
+        std::cout << e.what() << '\n';
     }
-    
-    public:
-    ScopeTimer(size_t verbosity, const std::string& message)
-        : m_verbosity(verbosity), m_message(message)
-    {
-        if (m_verbosity > 2)
-        {
-            m_aggregateTimer.Start();
-        }
-    }
-    
-            if (
-            // Test condition 1.
-            (dimsA.size() == 3 && dimsA[0] == 1 && dimsA[1] == 1) &&
-            // Test condition 2.
-            (dimsB.size() == 2 && dimsB[1] == 1) &&
-            (dimsC.size() == 2 && dimsC[1] == 1) &&
-            // Test condition 3. and condition 4.
-            (dimsB[0] == dimsC[0] && dimsB[0] == dimsA[2])
-            )
-        {
-            // for error messages
-            string dimsBstring = string(Input(1)->GetSampleLayout());
-            string dimsCstring = string(Input(2)->GetSampleLayout());
-    }
-    
-    void BENCHFUN(sizeCtor)(int iters, int size) {
-  FOR_EACH_RANGE (i, 0, iters) {
-    VECTOR v(size);
-    doNotOptimizeAway(&v);
-  }
-}
-BENCHMARK_PARAM(BENCHFUN(sizeCtor), 16)
-BENCHMARK_PARAM(BENCHFUN(sizeCtor), 128)
-BENCHMARK_PARAM(BENCHFUN(sizeCtor), 1024)
-    
-    #include <boost/regex/pending/unicode_iterator.hpp>
-    
-    /*
- * This is an implementation of the std::atomic_shared_ptr TS
- * http://en.cppreference.com/w/cpp/experimental/atomic_shared_ptr
- * https://isocpp.org/files/papers/N4162.pdf
- *
- * AFAIK, the only other implementation is Anthony Williams from
- * Just::thread library:
- *
- * https://bitbucket.org/anthonyw/atomic_shared_ptr
- *
- * implementation details:
- *
- * Basically, three things need to be atomically exchanged to make this work:
- * * the local count
- * * the pointer to the control block
- * * the aliased pointer, if any.
- *
- * The Williams version does it with DWcas: 32 bits for local count, 64
- * bits for control block ptr, and he changes the shared_ptr
- * implementation to also store the aliased pointers using a linked list
- * like structure, and provides 32-bit index accessors to them (like
- * IndexedMemPool trick).
- *
- * This version instead stores the 48 bits of address, plus 16 bits of
- * local count in a single 8byte pointer.  This avoids 'lock cmpxchg16b',
- * which is much slower than 'lock xchg' in the normal 'store' case.  In
- * the less-common aliased pointer scenaro, we just allocate it in a new
- * block, and store a pointer to that instead.
- *
- * Note that even if we only want to use the 3-bits of pointer alignment,
- * this trick should still work - Any more than 4 concurrent accesses
- * will have to go to an external map count instead (slower, but lots of
- * concurrent access will be slow anyway due to bouncing cachelines).
- *
- * As a perf optimization, we currently batch up local count and only
- * move it global every once in a while.  This means load() is usually
- * only a single atomic operation, instead of 3.  For this trick to work,
- * we probably need at least 8 bits to make batching worth it.
- */
