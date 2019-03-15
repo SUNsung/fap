e43bfe880f4b5a3c1d8985ee0298d29c65fcdfaa
SUNsung <weatherius@gmail.com>
2019-03-15 10:55:50 +0200

    Rewrite [36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py] File in: 2019-03-15 10:55:50

--- pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
+++ pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
@@ -1,54 +1,175 @@
 
         
-            >>> humanize_bytes(1)
-    '1 B'
-    >>> humanize_bytes(1024, precision=1)
-    '1.0 kB'
-    >>> humanize_bytes(1024 * 123, precision=1)
-    '123.0 kB'
-    >>> humanize_bytes(1024 * 12342, precision=1)
-    '12.1 MB'
-    >>> humanize_bytes(1024 * 12342, precision=2)
-    '12.05 MB'
-    >>> humanize_bytes(1024 * 1234, precision=2)
-    '1.21 MB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=2)
-    '1.31 GB'
-    >>> humanize_bytes(1024 * 1234 * 1111, precision=1)
-    '1.3 GB'
+          print(header)
+    
+        # Define the loss function and the optimization algorithm
+    self.cross_entropies = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        logits=self.scores, labels=tf.reduce_mean(self.batch_labels))
+    self.cost = tf.reduce_sum(self.cross_entropies, name='cost')
+    self.global_step = tf.Variable(0, name='global_step', trainable=False)
+    self.optimizer = tf.train.AdamOptimizer()
+    self.train_op = self.optimizer.minimize(self.cost,
+                                            global_step=self.global_step)
+    
+      Returns:
+    datasets: a dataset dictionary, with one name->data dictionary pair for
+    each dataset file.
+  '''
+  print('Reading data from ', data_dir)
+  datasets = utils.read_datasets(data_dir, data_filename_stem)
+  for k, data_dict in datasets.items():
+    datasets[k] = clean_data_dict(data_dict)
+    
+    model = IntegrationToBoundModel(N)
+inputs_ph_t = [tf.placeholder(tf.float32,
+                              shape=[None, 1]) for _ in range(ntimesteps)]
+state = tf.zeros([batch_size, N])
+saver = tf.train.Saver()
+    
+      Args:
+    list_of_lists: List of lists.
+    
+    '''Generate samples from the MaskGAN.
+    
+      # Averages for real and fake token values.
+  real = tf.mul(values, present)
+  fake = tf.mul(values, missing)
+  real_avg = tf.reduce_sum(real) / real_count
+  fake_avg = tf.reduce_sum(fake) / fake_count
+    
+        ## Calculate the Advantages, A(s,a) = Q(s,a) - \hat{V}(s).
+    advantages = []
+    for t in xrange(FLAGS.sequence_length):
+      log_probability = log_probs_list[t]
+      cum_advantage = tf.zeros(shape=[FLAGS.batch_size / 2])
+    
+      # *Total* number of n-grams produced by the generator.
+  total_ngrams_produced = 0
+    
+      # Standard variable mappings.
+  variable_mapping = {
+      'gen/encoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+          encoder_lstm_w_0,
+      'gen/encoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+          encoder_lstm_b_0,
+      'gen/encoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+          encoder_lstm_w_1,
+      'gen/encoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+          encoder_lstm_b_1,
+      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+          decoder_lstm_w_0,
+      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+          decoder_lstm_b_0,
+      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+          decoder_lstm_w_1,
+      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+          decoder_lstm_b_1
+  }
+    
+      Returns:
+    A decoder function with the required interface of `dynamic_rnn_decoder`
+    intended for inference.
+  '''
+  with tf.name_scope(name, 'attention_decoder_fn_inference', [
+      output_fn, encoder_state, attention_keys, attention_values,
+      attention_score_fn, attention_construct_fn, embeddings,
+      start_of_sequence_id, end_of_sequence_id, maximum_length,
+      num_decoder_symbols, dtype
+  ]):
+    start_of_sequence_id = tf.convert_to_tensor(start_of_sequence_id, dtype)
+    end_of_sequence_id = tf.convert_to_tensor(end_of_sequence_id, dtype)
+    maximum_length = tf.convert_to_tensor(maximum_length, dtype)
+    num_decoder_symbols = tf.convert_to_tensor(num_decoder_symbols, dtype)
+    encoder_info = tf.contrib.framework.nest.flatten(encoder_state)[0]
+    batch_size = encoder_info.get_shape()[0].value
+    if output_fn is None:
+      output_fn = lambda x: x
+    if batch_size is None:
+      batch_size = tf.shape(encoder_info)[0]
+    
+          # Prediction is linear output for Discriminator.
+      predictions = tf.contrib.layers.linear(outputs, 1, scope=vs)
+    
+    First, we fix a training set and increase the number of
+samples. Then we plot the computation time as function of
+the number of samples.
+    
+    plt.figure()
+for dataset_name in datasets:
+    # loading and vectorization
+    print('loading data')
+    if dataset_name in ['http', 'smtp', 'SA', 'SF']:
+        dataset = fetch_kddcup99(subset=dataset_name, percent10=True,
+                                 random_state=random_state)
+        X = dataset.data
+        y = dataset.target
+    
+        plt.subplots_adjust(hspace=.30)
     
     
-def test_auth_plugin_require_auth_false(httpbin):
+def benchmark_influence(conf):
+    '''
+    Benchmark influence of :changing_param: on both MSE and latency.
+    '''
+    prediction_times = []
+    prediction_powers = []
+    complexities = []
+    for param_value in conf['changing_param_values']:
+        conf['tuned_params'][conf['changing_param']] = param_value
+        estimator = conf['estimator'](**conf['tuned_params'])
+        print('Benchmarking %s' % estimator)
+        estimator.fit(conf['data']['X_train'], conf['data']['y_train'])
+        conf['postfit_hook'](estimator)
+        complexity = conf['complexity_computer'](estimator)
+        complexities.append(complexity)
+        start_time = time.time()
+        for _ in range(conf['n_samples']):
+            y_pred = estimator.predict(conf['data']['X_test'])
+        elapsed_time = (time.time() - start_time) / float(conf['n_samples'])
+        prediction_times.append(elapsed_time)
+        pred_score = conf['prediction_performance_computer'](
+            conf['data']['y_test'], y_pred)
+        prediction_powers.append(pred_score)
+        print('Complexity: %d | %s: %.4f | Pred. Time: %fs\n' % (
+            complexity, conf['prediction_performance_label'], pred_score,
+            elapsed_time))
+    return prediction_powers, prediction_times, complexities
     
+    plt.matshow(data, cmap=plt.cm.Blues)
+plt.title('Original dataset')
     
-@mock.patch('httpie.core.get_response')
-def test_error(get_response):
-    def error(msg, *args, **kwargs):
-        global error_msg
-        error_msg = msg % args
+    X = list()
+y = list()
+for i, (phi, a) in enumerate([(.5, .15), (.5, .6), (.3, .2)]):
+    for _ in range(30):
+        phase_noise = .01 * np.random.normal()
+        amplitude_noise = .04 * np.random.normal()
+        additional_noise = 1 - 2 * np.random.rand(n_features)
+        # Make the noise sparse
+        additional_noise[np.abs(additional_noise) < .997] = 0
     
-        def test_request_body_from_file_by_path_no_field_name_allowed(
-            self, httpbin):
-        env = MockEnvironment(stdin_isatty=True)
-        r = http('POST', httpbin.url + '/post', 'field-name@' + FILE_PATH_ARG,
-                 env=env, error_exit_ok=True)
-        assert 'perhaps you meant --form?' in r.stderr
+        X, y = dataset
     
-        def create_model_instance(self, data):
-        '''
-        Return a new instance of the session model object, which represents the
-        current session state. Intended to be used for saving the session data
-        to the database.
-        '''
-        return self.model(
-            session_key=self._get_or_create_session_key(),
-            session_data=self.encode(data),
-            expire_date=self.get_expiry_date(),
-        )
+            while self.values[new_key] is not None and self.values[new_key] != key:
+            new_key = self.__hash_double_function(key, data, i) if \
+                self.balanced_factor() >= self.lim_charge else None
+            if new_key is None: break 
+            else: i += 1
     
-    def dedent(line, indent_depth):
-    if line.startswith(' ' * indent_depth):
-        return line[indent_depth:]
-    if line.startswith('\t'):
-        return line[1:]
-    return line
+        def _colision_resolution(self, key, data=None):
+        i = 1
+        new_key = self.hash_function(key + i*i)
+    
+    '''
+* Wondering how this method works !
+* It's pretty simple.
+* Let's say you need to calculate a ^ b
+* RULE 1 : a ^ b = (a*a) ^ (b/2) ---- example : 4 ^ 4 = (4*4) ^ (4/2) = 16 ^ 2
+* RULE 2 : IF b is ODD, then ---- a ^ b = a * (a ^ (b - 1)) :: where (b - 1) is even.
+* Once b is even, repeat the process to get a ^ b
+* Repeat the process till b = 1 OR b = 0, because a^1 = a AND a^0 = 1
+*
+* As far as the modulo is concerned,
+* the fact : (a*b) % c = ((a%c) * (b%c)) % c
+* Now apply RULE 1 OR 2 whichever is required.
+'''
