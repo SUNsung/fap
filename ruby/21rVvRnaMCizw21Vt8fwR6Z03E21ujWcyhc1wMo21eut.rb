313f14a1a2d49390e1d0493bc76c6790ed7df3ed
SUNsung <weatherius@gmail.com>
2019-03-15 07:43:31 +0200

    Rewrite [21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb] File in: 2019-03-15 07:43:30

--- ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
+++ ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
@@ -1,75 +1,100 @@
 
         
-        User.seed do |u|
-  u.id = -1
-  u.name = 'system'
-  u.username = 'system'
-  u.username_lower = 'system'
-  u.password = SecureRandom.hex
-  u.active = true
-  u.admin = true
-  u.moderator = true
-  u.approved = true
-  u.trust_level = TrustLevel[4]
-end
-    
-      # DELETE /resource/sign_out
-  def destroy
-    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :notice, :signed_out if signed_out
-    yield if block_given?
-    respond_to_on_destroy
-  end
-    
-        def reset_password_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :reset_password_instructions, opts)
+              it 'allows a single array to be passed to support older Fastlane syntax' do
+        expect_command('ls -la /tmp')
+        Fastlane::Actions.sh(['ls -la', '/tmp'])
+      end
     end
     
-          def extract_path_from_location(location)
-        uri = parse_uri(location)
+        # Determines the defined data type of this ConfigItem
+    def data_type
+      if @data_type.kind_of?(Symbol)
+        nil
+      elsif @data_type
+        @data_type
+      else
+        (@is_string ? String : nil)
+      end
+    end
     
-                authentication_keys_changed || encrypted_password_changed
-          end
-        end
+              values = {
+              foo: '',
+              bar: ''
+          }
+    
+    # test shellescape Windows implementation directly
+describe 'WindowsShellwords#shellescape' do
+  os = 'windows'
+  shellescape_testcases.each do |testcase|
+    it testcase['it'] + ': ' + testcase['it_result'][os] do
+      str = testcase['str']
+      escaped = WindowsShellwords.shellescape(str)
+    
+          # Sets a cache key to the given value.
+      #
+      # key - The cache key to write.
+      # value - The value to set.
+      # timeout - The time after which the cache key should expire.
+      def self.write(raw_key, value, timeout: TIMEOUT)
+        key = cache_key_for(raw_key)
+    
+          # Returns the database ID for the object.
+      #
+      # This method will return `nil` if no ID could be found.
+      def database_id
+        val = Caching.read(cache_key)
+    
+            # Builds a new note using a Hash that was built from a JSON payload.
+        def self.from_json_hash(raw_hash)
+          hash = Representation.symbolize_hash(raw_hash)
+          hash[:author] &&= Representation::User.from_json_hash(hash[:author])
+    
+        # Short circuit for a `fpm --version` or `fpm -v` short invocation that 
+    # is the user asking us for the version of fpm.
+    if run_args == [ '-v' ] || run_args == [ '--version' ]
+      puts FPM::VERSION
+      return 0
+    end
+    
+        header = extension_header = ''
+    data = extension_data = ''
+    record_length = extension_length = 0
+    empty_records = 0
+    
+        npm_flags = []
+    settings.each do |key, value|
+      # npm lets you specify settings in a .npmrc but the same key=value settings
+      # are valid as '--key value' command arguments to npm. Woo!
+      logger.debug('Configuring npm', key => value)
+      npm_flags += [ '--#{key}', value ]
+    end
+    
+      # Output a pkgbuild pkg.
+  def output(output_path)
+    output_check(output_path)
+    
+        # Licenses could include more than one.
+    # Speaking of just taking the first entry of the field:
+    # A crude thing to do, but I suppose it's better than nothing.
+    # -- Daniel Haskin, 3/24/2015
+    self.license = control['license'][0] || self.license
+    
+      option '--channel', 'CHANNEL_URL',
+    'The pear channel url to use instead of the default.'
     
       private
+  def input(command)
+    platforms = [
+      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
+      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
+      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
+      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
+      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
+    ]
+    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
+                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
+                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
+                             'log_directory', 'log_file_stderr', 'log_file_stdout']
     
-      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-=end
-    
-          def string_to_code(string)
-        # sha bytes
-        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
-        # Thanks donpark's IdenticonUtil.java for this.
-        # Match the following Java code
-        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
-        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
-    
-          def page_dir
-        @page_dir
-      end
-    
-        assert_no_match /Edit Page/,             last_response.body, ''Edit Page' link not blocked in compare template'
-    assert_no_match /Revert Changes/,        last_response.body, ''Revert Changes' link not blocked in compare template'
-  end
-    
-      test 'edit returns nil for non-existant page' do
-    # post '/edit' fails. post '/edit/' works.
-    page = 'not-real-page'
-    path = '/'
-    post '/edit/', :content => 'edit_msg',
-         :page              => page, :path => path, :message => ''
-    page_e = @wiki.paged(page, path)
-    assert_equal nil, page_e
-  end
+        # Write the scripts, too.
+    write_scripts
