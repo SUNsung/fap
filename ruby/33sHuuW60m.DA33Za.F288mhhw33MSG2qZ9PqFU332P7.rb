2674927860d915a886fa30f21a5ea88d01e762fb
SUNsung <weatherius@gmail.com>
2019-03-16 01:00:55 +0200

    Rewrite [33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb] File in: 2019-03-16 01:00:54

--- ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
+++ ruby/33sHuuW60m.DA33Za.F288mhhw33MSG2qZ9PqFU332P7.rb
@@ -1,83 +1,31 @@
 
         
-              # Converts the array to a comma-separated sentence where the last element is
-      # joined by the connector word. This is the html_safe-aware version of
-      # ActiveSupport's {Array#to_sentence}[http://api.rubyonrails.org/classes/Array.html#method-i-to_sentence].
-      #
-      def to_sentence(array, options = {})
-        options.assert_valid_keys(:words_connector, :two_words_connector, :last_word_connector, :locale)
+        Then(/^directories referenced in :linked_files are created in shared$/) do
+  dirs = TestApp.linked_files.map { |path| TestApp.shared_path.join(path).dirname }
+  dirs.each do |dir|
+    run_vagrant_command(test_dir_exists(dir))
+  end
+end
     
-            private
+    # The project root directory
+$root = ::File.dirname(__FILE__)
     
-          def _find_all(path, prefixes, args, outside_app)
-        prefixes = [prefixes] if String === prefixes
-        prefixes.each do |prefix|
-          paths.each do |resolver|
-            if outside_app
-              templates = resolver.find_all_anywhere(path, prefix, *args)
-            else
-              templates = resolver.find_all(path, prefix, *args)
-            end
-            return templates unless templates.empty?
-          end
-        end
-        []
-      end
-    
-        Category.transaction do
-      lounge.group_names = ['trust_level_3']
-      unless lounge.save
-        puts lounge.errors.full_messages
-        raise 'Failed to set permissions on trust level 3 lounge category!'
-      end
-    
-        # This will reload the data without locking the index. It is assumed
-    # the caller with lock the index outside of this call.
-    #
-    # @param [File] f
-    def unlocked_reload
-      return if !@index_file.file?
-    
-            # This is called to upgrade this V1 config to V2. The parameter given
-        # is the full V2 configuration object, so you can do anything to it
-        # that you want.
-        #
-        # No return value is expected, modifications should be made directly
-        # to the new V2 object.
-        #
-        # @param [V2::Root] new
-        def upgrade(new)
-        end
-    
-            # Helper method that will set a value if a value is given, or otherwise
-        # return the already set value.
-        #
-        # @param [Symbol] key Key for the data
-        # @param [Object] value Value to store.
-        # @return [Object] Stored value.
-        def self.get_or_set(key, value=UNSET_VALUE)
-          # If no value is to be set, then return the value we have already set
-          return data[key] if value.eql?(UNSET_VALUE)
-    
-            # Initialize the provider to represent the given machine.
-        #
-        # @param [Vagrant::Machine] machine The machine that this provider
-        #   is responsible for.
-        def initialize(machine)
-        end
-    
-    @@ layout
-<html>
-  <head>
-    <title>Sinatra + Sidekiq</title>
-    <body>
-      <%= yield %>
-    </body>
-</html>
-    
-        def safe_thread(name, &block)
-      Thread.new do
-        Thread.current['sidekiq_label'] = name
-        watchdog(name, &block)
-      end
+    class ConfigTag < Liquid::Tag
+  def initialize(tag_name, options, tokens)
+    super
+    options = options.split(' ').map {|i| i.strip }
+    @key = options.slice!(0)
+    @tag = nil
+    @classname = nil
+    options.each do |option|
+      @tag = $1 if option =~ /tag:(\S+)/ 
+      @classname = $1 if option =~ /classname:(\S+)/
     end
+  end
+    
+          locations = Array.new
+      while (data.code.to_i == 301 || data.code.to_i == 302)
+        data = handle_gist_redirecting(data)
+        break if locations.include? data.header['Location']
+        locations << data.header['Location']
+      end
