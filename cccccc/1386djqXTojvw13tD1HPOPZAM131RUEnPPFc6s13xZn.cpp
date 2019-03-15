af848c5946c55e3a59c02ac7840ad4112631f04c
SUNsung <weatherius@gmail.com>
2019-03-15 03:52:02 +0200

    Rewrite [1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp] File in: 2019-03-15 03:52:01

--- cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
+++ cccccc/1386djqXTojvw13tD1HPOPZAM131RUEnPPFc6s13xZn.cpp
@@ -1,449 +1,112 @@
 
         
         
-    {  DISALLOW_COPY_AND_ASSIGN(Screen);
-};
-    
-    class WebRequest : public mate::TrackableObject<WebRequest> {
- public:
-  static mate::Handle<WebRequest> Create(v8::Isolate* isolate,
-                                         AtomBrowserContext* browser_context);
-    }
-    
-    #if defined(OS_LINUX)
-typedef PowerObserverLinux PowerObserver;
-#else
-typedef base::PowerObserver PowerObserver;
-#endif  // defined(OS_LINUX)
-    
-    namespace in_app_purchase {
-    }
-    
-    
-    {  // Following flags are set by FakeDesktopMediaPicker when it's created and
-  // deleted.
-  bool picker_created;
-  bool picker_deleted;
-};
-    
-    #define CONTENT_IMPLEMENTATION 1
-#include 'content/common/content_export.h'
-    
-    namespace content {
-class RenderFrameHost;
-}
-    
-      std::string type;
-    
-    ui::KeyboardCode GetKeycodeFromText(std::string text){
-  ui::KeyboardCode retval = ui::VKEY_UNKNOWN;
-  if (text.size() != 0){
-    std::string upperText = base::ToUpperASCII(text);
-    std::string keyName = text;
-    bool found = false;
-    if (upperText.size() == 1){
-      char key = upperText[0];
-      if (key>='0' && key<='9'){//handle digital
-        keyName = 'Digit' + upperText;
-        found = true;
-      } else if (key>='A'&&key<='Z'){//handle alphabet
-        keyName = 'Key' + upperText;
-        found = true;
-      }
-    }
-    }
-    }
-    
-    
-    {  gtk_widget_show(menu_item_);
-  g_object_ref_sink(G_OBJECT(menu_item_));
-} 
-    
-      option.GetString('type', &type_);
-  option.GetString('label', &label_);
-  option.GetString('tooltip', &tooltip_);
-  option.GetBoolean('checked', &is_checked_);
-  option.GetBoolean('enabled', &is_enabled_);
-    
-    NwClipboardClearSyncFunction::NwClipboardClearSyncFunction() {
-    }
-    
-    #include <vector>
-    
-     protected:
-  ~NwObjCallObjectMethodFunction() override;
-    
-    #include <google/protobuf/generated_message_util.h>
-    
-    void FindAnnotationsOnPath(
-    const GeneratedCodeInfo& info, const string& source_file,
-    const std::vector<int>& path,
-    std::vector<const GeneratedCodeInfo::Annotation*>* annotations) {
-  for (int i = 0; i < info.annotation_size(); ++i) {
-    const GeneratedCodeInfo::Annotation* annotation = &info.annotation(i);
-    if (annotation->source_file() != source_file ||
-        annotation->path_size() != path.size()) {
-      continue;
-    }
-    int node = 0;
-    for (; node < path.size(); ++node) {
-      if (annotation->path(node) != path[node]) {
-        break;
-      }
-    }
-    if (node == path.size()) {
-      annotations->push_back(annotation);
-    }
+// Called on every log record (each one of which is a WriteBatch)
+// found in a kLogFile.
+static void WriteBatchPrinter(uint64_t pos, Slice record, WritableFile* dst) {
+  std::string r = '--- offset ';
+  AppendNumberTo(&r, pos);
+  r += '; ';
+  if (record.size() < 12) {
+    r += 'log record length ';
+    AppendNumberTo(&r, record.size());
+    r += ' is too small\n';
+    dst->Append(r);
+    return;
+  }
+  WriteBatch batch;
+  WriteBatchInternal::SetContents(&batch, record);
+  r += 'sequence ';
+  AppendNumberTo(&r, WriteBatchInternal::Sequence(&batch));
+  r.push_back('\n');
+  dst->Append(r);
+  WriteBatchItemPrinter batch_item_printer;
+  batch_item_printer.dst_ = dst;
+  Status s = batch.Iterate(&batch_item_printer);
+  if (!s.ok()) {
+    dst->Append('  error: ' + s.ToString() + '\n');
   }
 }
     
-      protobuf_unittest::TestAllTypes message2;
+    namespace leveldb {
+    }
     
-    #include <memory>
-    
-    #include <google/protobuf/compiler/java/java_context.h>
-#include <google/protobuf/compiler/java/java_enum_field.h>
-#include <google/protobuf/compiler/java/java_extension.h>
-#include <google/protobuf/compiler/java/java_extension_lite.h>
-#include <google/protobuf/compiler/java/java_field.h>
-#include <google/protobuf/compiler/java/java_helpers.h>
-#include <google/protobuf/compiler/java/java_message.h>
-#include <google/protobuf/compiler/java/java_message_lite.h>
-#include <google/protobuf/compiler/java/java_service.h>
-    
-    // Check whether a given message or its nested types has the given class name.
-bool MessageHasConflictingClassName(const Descriptor* message,
-                                    const string& classname) {
-  if (message->name() == classname) return true;
-  for (int i = 0; i < message->nested_type_count(); ++i) {
-    if (MessageHasConflictingClassName(message->nested_type(i), classname)) {
-      return true;
+    bool HandleDumpCommand(Env* env, char** files, int num) {
+  StdoutPrinter printer;
+  bool ok = true;
+  for (int i = 0; i < num; i++) {
+    Status s = DumpFile(env, files[i], &printer);
+    if (!s.ok()) {
+      fprintf(stderr, '%s\n', s.ToString().c_str());
+      ok = false;
     }
   }
-  for (int i = 0; i < message->enum_type_count(); ++i) {
-    if (message->enum_type(i)->name() == classname) {
-      return true;
-    }
-  }
-  return false;
+  return ok;
 }
     
-    FileGenerator::FileGenerator(const FileDescriptor *file, const Options& options)
-    : file_(file),
-      root_class_name_(FileClassName(file)),
-      is_bundled_proto_(IsProtobufLibraryBundledProtoFile(file)),
-      options_(options) {
-  for (int i = 0; i < file_->enum_type_count(); i++) {
-    EnumGenerator *generator = new EnumGenerator(file_->enum_type(i));
-    enum_generators_.push_back(generator);
-  }
-  for (int i = 0; i < file_->message_type_count(); i++) {
-    MessageGenerator *generator =
-        new MessageGenerator(root_class_name_, file_->message_type(i), options_);
-    message_generators_.push_back(generator);
-  }
-  for (int i = 0; i < file_->extension_count(); i++) {
-    ExtensionGenerator *generator =
-        new ExtensionGenerator(root_class_name_, file_->extension(i));
-    extension_generators_.push_back(generator);
-  }
-}
-    
-      // Check default values.
-  const Descriptor* descriptor = message->GetDescriptor();
-  const Reflection* reflection = message->GetReflection();
-  EXPECT_EQ(0, reflection->GetInt32(
-      *message, descriptor->FindFieldByName('foo_int')));
-  EXPECT_EQ('', reflection->GetString(
-      *message, descriptor->FindFieldByName('foo_string')));
-  EXPECT_EQ('', reflection->GetString(
-      *message, descriptor->FindFieldByName('foo_cord')));
-  EXPECT_EQ('', reflection->GetString(
-      *message, descriptor->FindFieldByName('foo_string_piece')));
-  EXPECT_EQ('', reflection->GetString(
-      *message, descriptor->FindFieldByName('foo_bytes')));
-  EXPECT_EQ(unittest::TestOneof2::FOO, reflection->GetEnum(
-      *message, descriptor->FindFieldByName('foo_enum'))->number());
-  const Descriptor* nested_descriptor;
-  const Message* nested_prototype;
-  nested_descriptor =
-      pool_.FindMessageTypeByName('protobuf_unittest.TestOneof2.NestedMessage');
-  nested_prototype = factory_.GetPrototype(nested_descriptor);
-  EXPECT_EQ(nested_prototype,
-            &reflection->GetMessage(
-                *message, descriptor->FindFieldByName('foo_message')));
-  const Descriptor* foogroup_descriptor;
-  const Message* foogroup_prototype;
-  foogroup_descriptor =
-      pool_.FindMessageTypeByName('protobuf_unittest.TestOneof2.FooGroup');
-  foogroup_prototype = factory_.GetPrototype(foogroup_descriptor);
-  EXPECT_EQ(foogroup_prototype,
-            &reflection->GetMessage(
-                *message, descriptor->FindFieldByName('foogroup')));
-  EXPECT_NE(foogroup_prototype,
-            &reflection->GetMessage(
-                *message, descriptor->FindFieldByName('foo_lazy_message')));
-  EXPECT_EQ(5, reflection->GetInt32(
-      *message, descriptor->FindFieldByName('bar_int')));
-  EXPECT_EQ('STRING', reflection->GetString(
-      *message, descriptor->FindFieldByName('bar_string')));
-  EXPECT_EQ('CORD', reflection->GetString(
-      *message, descriptor->FindFieldByName('bar_cord')));
-  EXPECT_EQ('SPIECE', reflection->GetString(
-      *message, descriptor->FindFieldByName('bar_string_piece')));
-  EXPECT_EQ('BYTES', reflection->GetString(
-      *message, descriptor->FindFieldByName('bar_bytes')));
-  EXPECT_EQ(unittest::TestOneof2::BAR, reflection->GetEnum(
-      *message, descriptor->FindFieldByName('bar_enum'))->number());
-    
-          if (OctalDigit::InClass(*ptr)) {
-        // An octal escape.  May one, two, or three digits.
-        int code = DigitValue(*ptr);
-        if (OctalDigit::InClass(ptr[1])) {
-          ++ptr;
-          code = code * 8 + DigitValue(*ptr);
-        }
-        if (OctalDigit::InClass(ptr[1])) {
-          ++ptr;
-          code = code * 8 + DigitValue(*ptr);
-        }
-        output->push_back(static_cast<char>(code));
-    }
+      // crc32c values for all supported record types.  These are
+  // pre-computed to reduce the overhead of computing the crc of the
+  // record type stored in the header.
+  uint32_t type_crc_[kMaxRecordType + 1];
     
     
-    {  // There should be no more input.
-  EXPECT_FALSE(tokenizer.Next());
-  // There should be no errors.
-  EXPECT_TRUE(error_collector.text_.empty());
-}
-    
-    
-    {        // these are measured in elements
-        size_t left, right, top, bottom;
-    };
-    
-    
-    {        vs1 = vmla_f32(vget_low(vgamma), vs1, vget_low(valpha));
-        vs1 = vmla_f32(vs1, vs2, vget_low(vbeta));
-        v_dst = vcvt_u32_f32(vs1);
-    }
-    
-    namespace CAROTENE_NS {
-    }
-    
-    
-    {                    // make border
-                        if (borderType == BORDER_MODE_CONSTANT)
-                        {
-                            tcurr = vsetq_lane_u16(borderValue, tcurr, 7);
-                        }
-                        else if (borderType == BORDER_MODE_REFLECT101)
-                        {
-                            tcurr = vsetq_lane_u16(vgetq_lane_u16(tcurr, 1),tcurr, 7);
-                        }
-                        else // borderType == BORDER_MODE_REFLECT || borderType == BORDER_MODE_REPLICATE
-                        {
-                            tcurr = vsetq_lane_u16(vgetq_lane_u16(tcurr, 0),tcurr, 7);
-                        }
-                    continue;
-                }
-    
-                vst1q_s32(_norm + j + 4, normh);
-            vst1q_s32(_norm + j + 0, norml);
-    
-    SPLIT4ALPHA(u,8)
-    
-    
-    {            v_y = vld2q_u8(srcy + syj + 32);
-            v_dst.val[0] = vld1q_u8(srcu + sj + 16);
-            v_dst.val[1] = v_y.val[0];
-            v_dst.val[2] = vld1q_u8(srcv + sj + 16);
-            v_dst.val[3] = v_y.val[1];
-            vst4q_u8(dst + dj + 64, v_dst);
-        }
-#endif
-    
-            if (i < roiw2)
-        {
-            internal::prefetch(src + i);
-            uint64x2_t vln1 = vld1q_u64((const u64*)(src + i));
-    }
-    
-        v1k0 = vextq_s16(d8_15, d16_23, 1);
-    q0 = vmaxq_s16(q0, vminq_s16(ak0, v1k0));
-    q1 = vminq_s16(q1, vmaxq_s16(bk0, v1k0));
-    
-    class PAGE_RES_IT;
-class ROW;
-class WERD_RES;
-    
-      writeCommands[1] = nrParams+2;
-  std_menu->AddChild ('changed_ Parameters Only', writeCommands[1],
-                      paramfile.string(), 'Config file name?');
-    
-      // Try to adjust the blamer bundle.
-  if (orig_bb != nullptr) {
-    // TODO(rays) Looks like a leak to me.
-    // orig_bb should take, rather than copy.
-    word->blamer_bundle = new BlamerBundle();
-    word2->blamer_bundle = new BlamerBundle();
-    orig_bb->SplitBundle(chopped->blobs.back()->bounding_box().right(),
-                         word2->chopped_word->blobs[0]->bounding_box().left(),
-                         wordrec_debug_blamer,
-                         word->blamer_bundle, word2->blamer_bundle);
-  }
-    
-      /// SetImage makes a copy of all the image data, so it may be deleted
-  /// immediately after this call.
-  /// Greyscale of 8 and color of 24 or 32 bits per pixel may be given.
-  /// Palette color images will not work properly and must be converted to
-  /// 24 bit.
-  /// Binary images of 1 bit per pixel may also be given but they must be
-  /// byte packed with the MSB of the first byte being the first pixel, and a
-  /// one pixel is WHITE. For binary images set bytes_per_pixel=0.
-  void SetImage(const unsigned char* imagedata, int width, int height,
-                int bytes_per_pixel, int bytes_per_line);
-    
-      // Constrained fit with a supplied direction vector. Finds the best line_pt,
-  // that is one of the supplied points having the median cross product with
-  // direction, ignoring points that have a cross product outside of the range
-  // [min_dist, max_dist]. Returns the resulting error metric using the same
-  // reduced set of points.
-  // *Makes use of floating point arithmetic*
-  double ConstrainedFit(const FCOORD& direction,
-                        double min_dist, double max_dist,
-                        bool debug, ICOORD* line_pt);
-    
-    
-    {  int delta = this - prev;
-  int32_t n = prev->n_ + 1;
-  int32_t sig_x = prev->sig_x_ + delta;
-  int64_t sig_xsq = prev->sig_xsq_ + delta * delta;
-  int64_t cost = (sig_xsq - sig_x * sig_x / n) / n;
-  cost += prev->total_cost_;
-  UpdateIfBetter(cost, prev->total_steps_ + 1, prev, n, sig_x, sig_xsq);
-  return cost;
-}
-    
-            template <typename ElementType>
-        void Update(const Parameter& parameter, const NDArrayViewPtr& gradientValue, const NDArrayViewPtr& smoothedGradientValue, size_t trainingSampleCount) const;
-    
-            // (start, end) values in the current window to be reported.
-        std::pair<double, double> m_loss;
-        std::pair<double, double> m_metric;
-        std::pair<size_t, size_t> m_samples;
-        std::pair<size_t, size_t> m_updates;
-    
-            static bool IsUDF(const Dictionary& dict);
-    
-        private:
-        PackedValue(const NDShape& sampleShape, const std::vector<Axis>& sampleDynamicAxes, const NDArrayViewPtr& packedData, const std::shared_ptr<Microsoft::MSR::CNTK::MBLayout>& packedDataLayout, bool isReadOnly)
-            : Value(nullptr), m_isPacked(true), m_sampleShape(sampleShape), m_sampleDynamicAxes(sampleDynamicAxes), m_packedData(packedData), m_packedDataLayout(packedDataLayout), m_isReadOnly(isReadOnly)
-        {
-            // Determine unpacked shape
-            m_unpackedShape = GetUnpackedShape(sampleShape, sampleDynamicAxes, packedDataLayout);
-        }
-    
-        ParameterInitializer GlorotNormalInitializer(double scale, int outputRank, int filterRank, unsigned long seed)
     {
-        return CreateInitializer(Microsoft::MSR::CNTK::GlorotNormalInitializerTypeName, scale, outputRank, filterRank, seed);
+    {  void ArchiveFile(const std::string& fname) {
+    // Move into another directory.  E.g., for
+    //    dir/foo
+    // rename to
+    //    dir/lost/foo
+    const char* slash = strrchr(fname.c_str(), '/');
+    std::string new_dir;
+    if (slash != nullptr) {
+      new_dir.assign(fname.data(), slash - fname.data());
     }
-    
-            std::wstring AsString() const;
-        std::shared_ptr<VariableFields> Clone() const;
-        FunctionPtr Owner() const;
-    
-    
-    {        assert(m_fd == -1);
-        for (;;)
-        {
-            // opening a lock file
-            int fd = open(m_fileName.c_str(), O_WRONLY | O_CREAT, 0666);
-            if (fd < 0)
-                RuntimeError('Acquire: Failed to open lock file %s: %s.', m_fileName.c_str(), strerror(errno));
-            // locking it with the fcntl API
-            memset(&m_lock, 0, sizeof(m_lock));
-            m_lock.l_type = F_WRLCK;
-            // BUG: fcntl call with F_SETLKW doesn't always reliably detect when lock is released
-            // As a workaround, using alarm() for interupting fcntl if it waits more than 1 second
-            setupTimeout(1);
-            int r = fcntl(fd, wait ? F_SETLKW : F_SETLK, &m_lock);
-            if (errno == EINTR)
-            {
-                sleep(1);
-                // retrying in the case of signal or timeout
-                close(fd);
-                continue;
-            }
-            if (r != 0)
-            {
-                // acquire failed
-                close(fd);
-                umask(mask);
-                return false;
-            }
-            // we own the exclusive lock on file descriptor, but we need to double-check
-            // that the lock file wasn't deleted and/or re-created;
-            // checking this by comparing inode numbers
-            struct stat before, after;
-            fstat(fd, &before);
-            if (stat(m_fileName.c_str(), &after) != 0 || before.st_ino != after.st_ino)
-            {
-                // we have a race with 'unlink' call in Release()
-                // our lock is held to the previous instance of the file;
-                // this is not a problem, we just need to retry locking the new file
-                close(fd);
-                continue;
-            }
-            else
-            {
-                // lock acquired successfully
-                m_fd = fd;
-                umask(mask);
-                return true;
-            }
-        }
-    }
-    
-    
-    {    std::string GetCallStack(size_t skipLevels = 0, bool makeFunctionNamesStandOut = false);
+    new_dir.append('/lost');
+    env_->CreateDir(new_dir);  // Ignore error
+    std::string new_file = new_dir;
+    new_file.append('/');
+    new_file.append((slash == nullptr) ? fname.c_str() : slash + 1);
+    Status s = env_->RenameFile(fname, new_file);
+    Log(options_.info_log, 'Archiving %s: %s\n',
+        fname.c_str(), s.ToString().c_str());
+  }
 };
+}  // namespace
     
-    #include 'Basics.h'
-#include <stdio.h>
-#include <vector>
-#include <algorithm>
-#include <random>
+      int level_;
+  uint64_t max_output_file_size_;
+  Version* input_version_;
+  VersionEdit edit_;
     
-        void Start();
-    void Stop();
-    void Restart();
-    
-    SEXP XGDMatrixCreateFromCSC_R(SEXP indptr,
-                              SEXP indices,
-                              SEXP data,
-                              SEXP num_row) {
-  SEXP ret;
-  R_API_BEGIN();
-  const int *p_indptr = INTEGER(indptr);
-  const int *p_indices = INTEGER(indices);
-  const double *p_data = REAL(data);
-  size_t nindptr = static_cast<size_t>(length(indptr));
-  size_t ndata = static_cast<size_t>(length(data));
-  size_t nrow = static_cast<size_t>(INTEGER(num_row)[0]);
-  std::vector<size_t> col_ptr_(nindptr);
-  std::vector<unsigned> indices_(ndata);
-  std::vector<float> data_(ndata);
+    namespace leveldb {
     }
     
-    #include <dmlc/registry.h>
-#include <vector>
-#include <utility>
-#include <string>
-#include <functional>
-#include <memory>
-#include './base.h'
-#include './data.h'
-#include './objective.h'
-#include './feature_map.h'
-#include '../../src/common/host_device_vector.h'
+    
+    {
+    {}}
+    
+    void Assembler::bl(int32_t offset) {
+  EmitIForm(18, uint32_t(offset), 0, 1);
+}
+    
+    void fillMap(BaseMap* map, const APCArray* ar) {
+  for (auto i = uint32_t{0}; i < ar->size(); ++i) {
+    map->set(*ar->getKey(i).asTypedValue(),
+             *ar->getValue(i)->toLocal().asTypedValue());
+  }
+}
+    
+    #include <folly/ScopeGuard.h>
+    
+    #include 'hphp/runtime/base/file.h'
+#include 'hphp/runtime/base/mem-file.h'
+#include 'hphp/runtime/base/stream-wrapper.h'
+#include <folly/String.h>
+#include <folly/portability/SysStat.h>
+#include <folly/portability/Unistd.h>
+    
+    struct GlobStreamWrapper final : Stream::Wrapper {
+  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+  req::ptr<Directory> opendir(const String& path) override;
+};
