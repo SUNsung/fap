1d428f87904181a6c28817cef36b6924d4a9c8f7
SUNsung <weatherius@gmail.com>
2019-03-16 09:17:23 +0200

    Rewrite [56J5EYkSP3aGM56wpAkc9d6mtQ56O9GWMLlTRQU561Ke.cpp] File in: 2019-03-16 09:17:22

--- cccccc/56J5EYkSP3aGM56wpAkc9d6mtQ56O9GWMLlTRQU561Ke.cpp
+++ cccccc/56J5EYkSP3aGM56wpAkc9d6mtQ56O9GWMLlTRQU561Ke.cpp
@@ -1,36 +1,266 @@
-  EXPECT_EQ(false, refl->HasField(*message, optional_int32));
-  refl->SetInt32(message, optional_int32, 42);
-  EXPECT_EQ(true, refl->HasField(*message, optional_int32));
-  refl->SetInt32(message, optional_int32, 0);
-  EXPECT_EQ(false, refl->HasField(*message, optional_int32));
+
+        
+        
+    {}
     
-    void GzipInputStream::DoNextOutput(const void** data, int* size) {
-  *data = output_position_;
-  *size = ((uintptr_t)zcontext_.next_out) - ((uintptr_t)output_position_);
-  output_position_ = zcontext_.next_out;
+        assert(info.name.size() < (2 << 10) && 'name failed sanity check');
+    
+    #include 'swift/Basic/DiverseList.h'
+#include 'swift/Basic/DiverseStack.h'
+using namespace swift;
+    
+      struct IndentScope {
+    TreePrinter *Printer;
+    size_t OldLength;
+    IndentScope(TreePrinter *printer, StringRef indent)
+        : Printer(printer), OldLength(printer->Indent.size()) {
+      Printer->Indent += indent;
+    }
+    ~IndentScope() { Printer->Indent.resize(OldLength); }
+  };
+    
+    
+    {  return scratch.copyString(result);
 }
     
-    #include <grpcpp/support/channel_arguments.h>
+        void emitDiagnosticLoc(clang::FullSourceLoc Loc, clang::PresumedLoc PLoc,
+                           clang::DiagnosticsEngine::Level Level,
+                           ArrayRef<clang::CharSourceRange> Ranges) override {}
     
-    namespace grpc {
+    #endif
+
+    
+    // Max number of scripts in ICU + 'NULL' + Japanese and Korean + Fraktur
+const int kMaxNumberOfScripts = 116 + 1 + 2 + 1;
+    
+    struct BlobData {
+  BlobData() : blob(nullptr), choices(nullptr) {}
+  BlobData(int index, Tesseract* tess, const WERD_RES& word)
+    : blob(word.chopped_word->blobs[index]),
+      tesseract(tess),
+      choices(&(*word.ratings)(index, index)) {}
     }
     
-    using grpc::Status;
-using grpc::StatusCode;
-using grpc::reflection::v1alpha::ErrorResponse;
-using grpc::reflection::v1alpha::ExtensionNumberResponse;
-using grpc::reflection::v1alpha::ExtensionRequest;
-using grpc::reflection::v1alpha::FileDescriptorResponse;
-using grpc::reflection::v1alpha::ListServiceResponse;
-using grpc::reflection::v1alpha::ServerReflectionRequest;
-using grpc::reflection::v1alpha::ServerReflectionResponse;
-using grpc::reflection::v1alpha::ServiceResponse;
+    #include <cstdio>    // for FILE
+#include 'strngs.h'  // for STRING
     
-    #ifndef HPHP_DATA_STREAM_WRAPPER_H
-#define HPHP_DATA_STREAM_WRAPPER_H
     
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////
+    {    ICOORD pt;
+    int halfwidth;
+  };
+  // Type holds the distance of each point from the fitted line and the point
+  // itself. Use of double allows integer distances from ICOORDs to be stored
+  // exactly, and also the floating point results from ConstrainedFit.
+  using DistPointPair = KDPairInc<double, ICOORD>;
+    
+    #include 'dppoint.h'
+#include 'errcode.h'
+#include 'tprintf.h'
+    
+    #ifndef TESSERACT_CCSTRUCT_DPPOINT_H_
+#define TESSERACT_CCSTRUCT_DPPOINT_H_
+    
+     private:
+  // Saves the given Pix as a PNG-encoded string and destroys it.
+  static void SetPixInternal(Pix* pix, GenericVector<char>* image_data);
+  // Returns the Pix image for the image_data. Must be pixDestroyed after use.
+  static Pix* GetPixInternal(const GenericVector<char>& image_data);
+  // Parses the text string as a box file and adds any discovered boxes that
+  // match the page number. Returns false on error.
+  bool AddBoxes(const char* box_text);
+    
+      // Returns the covariance.
+  double covariance() const {
+    if (total_weight > 0.0)
+      return (sigxy - sigx * sigy / total_weight) / total_weight;
+    else
+      return 0.0;
+  }
+  double x_variance() const {
+    if (total_weight > 0.0)
+      return (sigxx - sigx * sigx / total_weight) / total_weight;
+    else
+      return 0.0;
+  }
+  double y_variance() const {
+    if (total_weight > 0.0)
+      return (sigyy - sigy * sigy / total_weight) / total_weight;
+    else
+      return 0.0;
+  }
+    
+    #define MODULUS       128        /*range of directions */
+#define DIRBITS       7          //no of bits used
+#define DIRSCALE      1000       //length of vector
+    
+            ListNode* delNode = p->next;
+        p->next = delNode->next;
+        delete delNode;
+    
+    using namespace std;
+    
+    
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <stdexcept>
+    
+    // Recursive
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+    }
     }
     
-    #endif // incl_HPHP_OUTPUT_FILE_H_
+    #include <iostream>
+#include <vector>
+#include <stack>
+    
+    
+/// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+    
+    
+    {
+    {        return res;
+    }
+};
+    
+            queue<pair<TreeNode*,int>> q;
+        q.push(make_pair(root, 0));
+    
+    
+    {    return 0;
+}
+
+    
+    #include <iostream>
+#include <vector>
+#include <stack>
+#include <cassert>
+    
+            vector<int> res;
+        if(root == NULL)
+            return res;
+    
+    
+    {
+    {
+    {            TagNode tagNode = stack.top();
+            stack.pop();
+            cur = tagNode.node;
+            if(tagNode.isFirst == false){
+                tagNode.isFirst = true;
+                stack.push(tagNode);
+                cur = cur->right;
+            }
+            else{
+                res.push_back(cur->val);
+                cur = NULL;
+            };
+        }
+        return res;
+    }
+};
+    
+    
+    {
+    {        return res;
+    }
+};
+    
+    
+  while (true)
+  {
+    xfer += iprot->readFieldBegin(fname, ftype, fid);
+    if (ftype == ::apache::thrift::protocol::T_STOP) {
+      break;
+    }
+    xfer += iprot->skip(ftype);
+    xfer += iprot->readFieldEnd();
+  }
+    
+      xfer += iprot->readStructBegin(fname);
+    
+    
+    {};
+    
+    #include 'Extension.h'
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+    
+    class osqueryConstants {
+ public:
+  osqueryConstants();
+    }
+    
+    int _kExtensionCodeValues[] = {
+  ExtensionCode::EXT_SUCCESS,
+  ExtensionCode::EXT_FAILED,
+  ExtensionCode::EXT_FATAL
+};
+const char* _kExtensionCodeNames[] = {
+  'EXT_SUCCESS',
+  'EXT_FAILED',
+  'EXT_FATAL'
+};
+const std::map<int, const char*> _ExtensionCode_VALUES_TO_NAMES(::apache::thrift::TEnumIterator(3, _kExtensionCodeValues, _kExtensionCodeNames), ::apache::thrift::TEnumIterator(-1, NULL, NULL));
+    
+          auto cant_id = smartctl.cantIdDev(devname, full_type);
+      if (cant_id.err != NOERR) {
+        LOG(INFO) << 'Error while trying to identify device: '
+                  << libsmartctl::errStr(cant_id.err);
+        continue;
+      }
+      // If device is not identifiable, the type is invalid, skip..
+      if (cant_id.content) {
+        continue;
+      }
+    
+    namespace apollo {
+namespace drivers {
+namespace canbus {
+namespace can {
+    }
+    }
+    }
+    }
+    
+    
+    {
+    {
+    {}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    TEST(ByteTest, SetGetHighLowBit) {
+  unsigned char byte_value = 0x37;
+  Byte value(&byte_value);
+  value.set_value_high_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
+  value.set_value_low_4_bits(0x0B);
+  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
+  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
+}
+    
+      Byte t1(bytes + 2);
+  int32_t t = t1.get_byte(6, 2);
+    
+    void Brakemotorrpt372::Parse(const std::uint8_t* bytes, int32_t length,
+                             ChassisDetail* chassis) const {
+  chassis->mutable_gem()->mutable_brake_motor_rpt_3_72()->set_torque_output(
+      torque_output(bytes, length));
+  chassis->mutable_gem()->mutable_brake_motor_rpt_3_72()->set_torque_input(
+      torque_input(bytes, length));
+}
