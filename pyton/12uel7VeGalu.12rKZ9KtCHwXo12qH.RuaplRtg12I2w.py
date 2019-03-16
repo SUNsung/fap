40241cc09bd1eb771a7cce8d8ac903d74b14aca0
SUNsung <weatherius@gmail.com>
2019-03-16 03:04:26 +0200

    Rewrite [12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py] File in: 2019-03-16 03:04:26

--- pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
+++ pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
@@ -1,203 +1,79 @@
 
         
-          # Plotting
-  f = plt.figure(figsize=(18,20), tight_layout=True)
-  plot_lfads_timeseries(train_bxtxd, train_model_vals,
-                        train_ext_input_bxtxi,
-                        truth_bxtxn=train_truth_bxtxd,
-                        conversion_factor=cf, bidx=bidx,
-                        output_dist=output_dist, col_title='Train')
-  plot_lfads_timeseries(valid_bxtxd, valid_model_vals,
-                        valid_ext_input_bxtxi,
-                        truth_bxtxn=valid_truth_bxtxd,
-                        conversion_factor=cf, bidx=bidx,
-                        output_dist=output_dist,
-                        subplot_cidx=1, col_title='Valid')
+            with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
+        loss = MSE_MAE_loss(0.3)
+        inputs = keras.layers.Input((2,))
+        outputs = keras.layers.Dense(1, name='model_output')(inputs)
+        model = keras.models.Model(inputs, outputs)
+        model.compile(optimizer='sgd', loss={'model_output': loss})
+        model.fit(np.random.rand(256, 2), np.random.rand(256, 1))
+        model.save(model_filename)
     
-      def call(self, h_tm1_bxn, u_bx1):
-    act_t_bxn = tf.matmul(h_tm1_bxn, self.Wh_nxn) + self.b_1xn + u_bx1 * self.Bu_1xn
-    h_t_bxn = tf.nn.tanh(act_t_bxn)
-    z_t = tf.nn.xw_plus_b(h_t_bxn, self.Wro_nxo, self.bro_o)
-    return z_t, h_t_bxn
+        E.g. if your `batch_size` is 64 and you use `gpus=2`,
+    then we will divide the input into 2 sub-batches of 32 samples,
+    process each sub-batch on one GPU, then return the full
+    batch of 64 processed samples.
     
-    import os
-import h5py
-import numpy as np
-from six.moves import xrange
+    learning_rate = 1e-6
+clip_norm = 1.0
     
-      # In the data workup in the paper, Chethan did intra condition
-  # averaging, so let's do that here.
-  avg_data_all = {}
-  for name, conditions in conditions_all.items():
-    dataset = datasets[name]
-    avg_data_all[name] = {}
-    for cname in conditions:
-      td_idxs = np.argwhere(np.array(dataset['condition_labels_train'])==cname)
-      data = np.squeeze(dataset['train_data'][td_idxs,:,:], axis=1)
-      avg_data = np.mean(data, axis=0)
-      avg_data_all[name][cname] = avg_data
-    
-      Args:
-    vocab: Vocabulary. Contains vocabulary size and converts word to ids.
-  '''
-  assert FLAGS.save_dir, 'Must specify FLAGS.save_dir for dump_emb.'
-  inputs = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
-  targets = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
-  weights = np.ones([BATCH_SIZE, NUM_TIMESTEPS], np.float32)
-    
-    '''Generate samples from the MaskGAN.
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+    x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
     
     
-@pytest.fixture
-def app():
-    '''Create and configure a new app instance for each test.'''
-    # create a temporary file to isolate the database for each test
-    db_fd, db_path = tempfile.mkstemp()
-    # create the app with common test config
-    app = create_app({
-        'TESTING': True,
-        'DATABASE': db_path,
-    })
+def mean_absolute_percentage_error(y_true, y_pred):
+    diff = K.abs((y_true - y_pred) / K.clip(K.abs(y_true),
+                                            K.epsilon(),
+                                            None))
+    return 100. * K.mean(diff, axis=-1)
     
-    signals_available = False
-try:
-    from blinker import Namespace
-    signals_available = True
-except ImportError:
-    class Namespace(object):
-        def signal(self, name, doc=None):
-            return _FakeSignal(name, doc)
-    
-    from jinja2 import BaseLoader, Environment as BaseEnvironment, \
-     TemplateNotFound
-    
-        @property
-    def blueprint(self):
-        '''The name of the current blueprint'''
-        if self.url_rule and '.' in self.url_rule.endpoint:
-            return self.url_rule.endpoint.rsplit('.', 1)[0]
-    
-    model.fit(x_train, y_train,
-          batch_size=batch_size,
-          callbacks=[tensorboard],
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+    _DEVICES_REGEX = re.compile(
+    r'(?P<mac>(([0-9a-f]{2}[:-]){5}([0-9a-f]{2})))\s'
+    r'(?P<ip>([0-9]{1,3}[\.]){3}[0-9]{1,3})\s+'
+    r'(?P<status>([^\s]+))\s+'
+    r'(?P<type>([^\s]+))\s+'
+    r'(?P<intf>([^\s]+))\s+'
+    r'(?P<hwintf>([^\s]+))\s+'
+    r'(?P<host>([^\s]+))')
     
     
-def test_objective_shapes_3d():
-    y_a = K.variable(np.random.random((5, 6, 7)))
-    y_b = K.variable(np.random.random((5, 6, 7)))
-    for obj in allobj:
-        objective_output = obj(y_a, y_b)
-        assert K.eval(objective_output).shape == (5, 6)
+def get_service(hass, config, discovery_info=None):
+    '''Get the demo notification service.'''
+    return DemoNotificationService(hass)
     
-        ```python
-    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
-    > labels
-    array([0, 2, 1, 2, 0])
-    # `to_categorical` converts this into a matrix with as many
-    # columns as there are classes. The number of rows
-    # stays the same.
-    > to_categorical(labels)
-    array([[ 1.,  0.,  0.],
-           [ 0.,  0.,  1.],
-           [ 0.,  1.,  0.],
-           [ 0.,  0.,  1.],
-           [ 1.,  0.,  0.]], dtype=float32)
-    ```
+    from homeassistant.components.notify import (
+    ATTR_TITLE, ATTR_TITLE_DEFAULT, PLATFORM_SCHEMA, BaseNotificationService)
+from homeassistant.const import CONF_PASSWORD, CONF_PORT
+import homeassistant.helpers.config_validation as cv
+    
+    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_API_KEY): cv.string,
+    vol.Optional(CONF_SENDER, default='HA'):
+        vol.All(cv.string, vol.Match(r'^(\+?[1-9]\d{1,14}|\w{1,11})$')),
+})
+    
+                # Target is email, send directly, don't use a target object.
+            # This also seems to work to send to all devices in own account.
+            if ttype == 'email':
+                self._push_data(message, title, data, self.pushbullet, tname)
+                _LOGGER.info('Sent notification to email %s', tname)
+                continue
+    
+    image = face_recognition.load_image_file('{}')
+face_locations = face_recognition.face_locations(image)
+'''
+    
+    
+def face_landmarks(face_image, face_locations=None, model='large'):
     '''
+    Given an image, returns a dict of face feature locations (eyes, nose, etc) for each face in the image
     
-    train_model.compile(optimizer=keras.optimizers.RMSprop(lr=2e-3, decay=1e-5),
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'],
-                    target_tensors=[targets])
-train_model.summary()
-    
-        t = now()
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              verbose=1,
-              validation_data=(x_test, y_test))
-    print('Training time: %s' % (now() - t))
-    score = model.evaluate(x_test, y_test, verbose=0)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])
-    
-    
-class PackException(Exception):
-    pass
-    
-    
-def testUnsignedInt():
-    check(b'\x99\xcc\x00\xcc\x80\xcc\xff\xcd\x00\x00\xcd\x80\x00'
-          b'\xcd\xff\xff\xce\x00\x00\x00\x00\xce\x80\x00\x00\x00'
-          b'\xce\xff\xff\xff\xff',
-          (0,
-           128,
-           255,
-           0,
-           32768,
-           65535,
-           0,
-           2147483648,
-           4294967295, ), )
-    
-            exp1 = '''TimedeltaIndex([], dtype='timedelta64[ns]', freq='D')'''
-    
-        extract_tags = textrank
-
-    
-    jieba.analyse.set_idf_path('../extra_dict/idf.txt.big');
-    
-    if len(sys.argv)>1:
-    default_encoding = sys.argv[1]
-    
-    url = sys.argv[1]
-content = open(url,'rb').read()
-t1 = time.time()
-words = '/ '.join(jieba.cut(content))
-    
-            # return its order if it is hiragana
-        if len(byte_str) > 1:
-            second_char = byte_str[1]
-            if (first_char == 0xA4) and (0xA1 <= second_char <= 0xF3):
-                return second_char - 0xA1, char_len
-    
-        def get_confidence(self):
-        if self.state == ProbingState.NOT_ME:
-            return 0.01
-    
-        def get_confidence(self):
-        if self.state == ProbingState.NOT_ME:
-            return 0.01
-    
-    EUCJP_SM_MODEL = {'class_table': EUCJP_CLS,
-                  'class_factor': 6,
-                  'state_table': EUCJP_ST,
-                  'char_len_table': EUCJP_CHAR_LEN_TABLE,
-                  'name': 'EUC-JP'}
-    
-        def get_confidence(self):
-        r = 0.01
-        if self._total_seqs > 0:
-            r = ((1.0 * self._seq_counters[SequenceLikelihood.POSITIVE]) /
-                 self._total_seqs / self._model['typical_positive_ratio'])
-            r = r * self._freq_char / self._total_char
-            if r >= 1.0:
-                r = 0.99
-        return r
-
-    
-        def get_confidence(self):
-        context_conf = self.context_analyzer.get_confidence()
-        distrib_conf = self.distribution_analyzer.get_confidence()
-        return max(context_conf, distrib_conf)
+            # Now let's list all the faces we found in all 128 frames
+        for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
+            number_of_faces_in_frame = len(face_locations)
