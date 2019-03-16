fca65ea383cf586a4d7ea36f2a57bd9d315a9010
SUNsung <weatherius@gmail.com>
2019-03-16 06:43:55 +0200

    Rewrite [51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb] File in: 2019-03-16 06:43:55

--- ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
+++ ruby/51a2JCnGdj9Yw51jTCAWDNqoY251DCvAcuYBm.s517hO.rb
@@ -1,257 +1,111 @@
 
         
-        def global_require
-  JSON.pretty_generate(DATA)
+            def insert(index, *names)
+      @filters.insert assert_index(index), *filter_const(names)
+    end
+    
+    module Docs
+  class Entry
+    class Invalid < StandardError; end
+    
+        def to_json
+      JSON.generate(as_json)
+    end
+  end
+end
+
+    
+        def load_capybara_selenium
+      require 'capybara/dsl'
+      require 'selenium/webdriver'
+      Capybara.register_driver :chrome do |app|
+        options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+        Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+      end
+      Capybara.javascript_driver = :chrome
+      Capybara.current_driver = :chrome
+      Capybara.run_server = false
+      Capybara
+    end
+    
+            css('.l-sub-section', '.alert', '.banner').each do |node|
+          node.name = 'blockquote'
+        end
+    
+            subtitle = at_css('.hero-subtitle').try(:content)
+        breadcrumbs = css('.breadcrumbs li').map(&:content)[2..-2]
+    
+        desc 'Release all gems as packages'
+    task :all => [:test, :commit_version] + GEMS_AND_ROOT_DIRECTORIES.keys
+  end
+end
+
+    
+      at_exit { Application.run! if $!.nil? && Application.run? }
 end
     
-    require 'benchmark/ips'
-require 'pathutil'
+          def masked_token?(token)
+        token.length == TOKEN_LENGTH * 2
+      end
     
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+          DIRECTIVES = %i(base_uri child_src connect_src default_src
+                      font_src form_action frame_ancestors frame_src
+                      img_src manifest_src media_src object_src
+                      plugin_types referrer reflected_xss report_to
+                      report_uri require_sri_for sandbox script_src
+                      style_src worker_src).freeze
     
-    #
-    
-            def handle_websockets_event(websocket)
-          websocket.onopen { |handshake| connect(websocket, handshake) }
-          websocket.onclose { disconnect(websocket) }
-          websocket.onmessage { |msg| print_message(msg) }
-          websocket.onerror { |error| log_error(error) }
+          def escape(object)
+        case object
+        when Hash   then escape_hash(object)
+        when Array  then object.map { |o| escape(o) }
+        when String then escape_string(object)
+        when Tempfile then object
+        else nil
         end
+      end
     
-            # Setup and normalize the configuration:
-        #   * Create Kramdown if it doesn't exist.
-        #   * Set syntax_highlighter, detecting enable_coderay and merging
-        #       highlighter if none.
-        #   * Merge kramdown[coderay] into syntax_highlighter_opts stripping coderay_.
-        #   * Make sure `syntax_highlighter_opts` exists.
+        EMPTY_TYPE = 'inode/x-empty'
+    SENSIBLE_DEFAULT = 'application/octet-stream'
     
-              hosts
-        end
-    
-            # This is called as a last-minute hook that allows the configuration
-        # object to finalize itself before it will be put into use. This is
-        # a useful place to do some defaults in the case the user didn't
-        # configure something or so on.
-        #
-        # An example of where this sort of thing is used or has been used:
-        # the 'vm' configuration key uses this to make sure that at least
-        # one sub-VM has been defined: the default VM.
-        #
-        # The configuration object is expected to mutate itself.
-        def finalize!
-          # Default implementation is to do nothing.
-        end
-    
-            # This is called early, before a machine is instantiated, to check
-        # if this provider is installed. This should return true or false.
-        #
-        # If the provider is not installed and Vagrant determines it is
-        # able to install this provider, then it will do so. Installation
-        # is done by calling Environment.install_provider.
-        #
-        # If Environment.can_install_provider? returns false, then an error
-        # will be shown to the user.
-        def self.installed?
-          # By default return true for backwards compat so all providers
-          # continue to work.
-          true
-        end
-    
-            it 'cuts excessive digits and keeps only 6 ones' do
-          format('%#{f}', 1.123456789).should == '1.123457#{exp}+00'
-        end
-    
-      it 'raises an ArgumentError when passed a negative duration' do
-    lambda { sleep(-0.1) }.should raise_error(ArgumentError)
-    lambda { sleep(-1) }.should raise_error(ArgumentError)
-  end
-    
-        it 'raises Errno::ENOENT when `exception: true` is given and the specified command does not exist' do
-      lambda { @object.system('feature_14386', exception: true) }.should raise_error(Errno::ENOENT)
-    end
-  end
-    
-      it 'sets the tainted bit' do
-    o = Object.new
-    o.taint
-    o.tainted?.should == true
-  end
-    
-      it 'hooks assignments to a global variable' do
-    captured = nil
-    
-    # The module that contains everything Sass-related:
-#
-# * {Sass::Engine} is the class used to render Sass/SCSS within Ruby code.
-# * {Sass::Plugin} is interfaces with web frameworks (Rails and Merb in particular).
-# * {Sass::SyntaxError} is raised when Sass encounters an error.
-# * {Sass::CSS} handles conversion of CSS to Sass.
-#
-# Also see the {file:SASS_REFERENCE.md full Sass reference}.
-module Sass
-  class << self
-    # @private
-    attr_accessor :tests_running
-  end
-    
-        # Flatten rules so that
-    #
-    #     foo
-    #       bar
-    #         color: red
-    #
-    # becomes
-    #
-    #     foo bar
-    #       color: red
-    #
-    # and
-    #
-    #     foo
-    #       &.bar
-    #         color: blue
-    #
-    # becomes
-    #
-    #     foo.bar
-    #       color: blue
-    #
-    # @param root [Tree::Node] The parent node
-    def flatten_rules(root)
-      root.children.each do |child|
-        case child
-        when Tree::RuleNode
-          flatten_rule(child)
-        when Tree::DirectiveNode
-          flatten_rules(child)
-        end
+        def geometry_string
+      begin
+        orientation = Paperclip.options[:use_exif_orientation] ?
+          '%[exif:orientation]' : '1'
+        Paperclip.run(
+          Paperclip.options[:is_windows] ? 'magick identify' : 'identify',
+          '-format '%wx%h,#{orientation}' :file', {
+            :file => '#{path}[0]'
+          }, {
+            :swallow_stderr => true
+          }
+        )
+      rescue Terrapin::ExitStatusError
+        ''
+      rescue Terrapin::CommandNotFoundError => e
+        raise_because_imagemagick_missing
       end
     end
     
-          opts.on('--trace', :NONE, 'Show a full Ruby stack trace on error') do
-        @options[:trace] = true
-      end
-    end
-    
-          if @options[:sourcemap] != :none && @options[:sourcemap_filename]
-        relative_sourcemap_path = Sass::Util.relative_path_from(
-          @options[:sourcemap_filename], Sass::Util.pathname(@options[:output_filename]).dirname)
-        rendered, mapping = engine.render_with_sourcemap(relative_sourcemap_path.to_s)
-        write_output(rendered, output)
-        write_output(
-          mapping.to_json(
-            :type => @options[:sourcemap],
-            :css_path => @options[:output_filename],
-            :sourcemap_path => @options[:sourcemap_filename]) + '\n',
-          @options[:sourcemap_filename])
+        # Returns a the attachment hash.  See Paperclip::Attachment#hash_key for
+    # more details.
+    def hash attachment=nil, style_name=nil
+      if attachment && style_name
+        attachment.hash_key(style_name)
       else
-        write_output(engine.render, output)
-      end
-    rescue Sass::SyntaxError => e
-      write_output(Sass::SyntaxError.exception_to_css(e), output) if output.is_a?(String)
-      raise e
-    ensure
-      output.close if output.is_a? File
-    end
-    
-      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
-  gem.executables   = %w(cap capify)
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ['lib']
-    
-        def dry_run?
-      fetch(:sshkit_backend) == SSHKit::Backend::Printer
-    end
-    
-          def print_deprecation_warnings_if_applicable
-        if using_default_scm?
-          warn_add_git_to_capfile unless scm_plugin_installed?
-        elsif built_in_scm_name?
-          warn_set_scm_is_deprecated
-        elsif third_party_scm_name?
-          warn_third_party_scm_must_be_upgraded
-        end
-      end
-    
-          private
-    
-          # A shorthand for getting the first argument of the node.
-      # Equivalent to `arguments.first`.
-      #
-      # @return [Node, nil] the first argument of the node,
-      #                     or `nil` if there are no arguments
-      def first_argument
-        arguments[0]
-      end
-    
-    desc 'Watch the site and regenerate when it changes'
-task :watch do
-  raise '### You haven't set anything up yet. First run `rake install` to set up an Octopress theme.' unless File.directory?(source_dir)
-  puts 'Starting to watch source with Jekyll and Compass.'
-  system 'compass compile --css-dir #{source_dir}/stylesheets' unless File.exist?('#{source_dir}/stylesheets/screen.css')
-  jekyllPid = Process.spawn({'OCTOPRESS_ENV'=>'preview'}, 'jekyll build --watch')
-  compassPid = Process.spawn('compass watch')
-    
-      get(/.+/) do
-    send_sinatra_file(request.path) {404}
-  end
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-      if options.respond_to? 'keys'
-    options.each do |k,v|
-      unless v.nil?
-        v = v.join ',' if v.respond_to? 'join'
-        v = v.to_json if v.respond_to? 'keys'
-        output += ' data-#{k.sub'_','-'}='#{v}''
+        super()
       end
     end
-  elsif options.respond_to? 'join'
-    output += ' data-value='#{config[key].join(',')}''
-  else
-    output += ' data-value='#{config[key]}''
-  end
-  output += '></#{tag}>'
-end
     
-          if File.symlink?(includes_dir)
-        return 'Includes directory '#{includes_dir}' cannot be a symlink'
+        bin_dir = attributes[:pear_bin_dir] || 'usr/bin'
+    logger.info('Setting bin_dir', :bin_dir => bin_dir)
+    safesystem('pear', '-c', config, 'config-set', 'bin_dir', bin_dir)
+    
+        if i < args.size
+      if args[i].kind_of?(Hash)
+        opts = Hash[args[i].map {|k,v| [k.to_sym, v]} ]
+        i += 1
       end
-    
-        def render(context)
-      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
-      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
-      file = code_path + @file
-    
-      # Escapes CDATA sections in post content
-  def cdata_escape(input)
-    input.gsub(/<!\[CDATA\[/, '&lt;![CDATA[').gsub(/\]\]>/, ']]&gt;')
-  end
-    
-    Liquid::Template.register_tag('video', Jekyll::VideoTag)
+    else
+      opts = Hash[]
+    end
