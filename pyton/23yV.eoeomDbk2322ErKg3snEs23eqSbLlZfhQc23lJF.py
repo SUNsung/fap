5d637c0f584a69b1c29150ea2bb729cebe822f82
SUNsung <weatherius@gmail.com>
2019-03-17 18:31:11 +0200

    Rewrite [23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py] File in: 2019-03-17 18:31:11

--- pyton/23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py
+++ pyton/23yV.eoeomDbk2322ErKg3snEs23eqSbLlZfhQc23lJF.py
@@ -1,203 +1,44 @@
 
         
-          LFADS generates a number of outputs for each examples, and these are all
-  saved.  They are:
-    The mean and variance of the prior of g0.
-    The control inputs (if enabled)
-    The initial conditions, g0, for all examples.
-    The generator states for all time.
-    The factors for all time.
-    The output distribution parameters (e.g. rates) for all time.
-    
-    rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
-rnn_rngs = [np.random.RandomState(seed=FLAGS.synth_data_seed+1),
-            np.random.RandomState(seed=FLAGS.synth_data_seed+2)]
-T = FLAGS.T
-C = FLAGS.C
-N = FLAGS.N
-nreplications = FLAGS.nreplications
-E = nreplications * C
-train_percentage = FLAGS.train_percentage
-ntimesteps = int(T / FLAGS.dt)
-    
-      Returns:
-    A length T list of numpy tensors with shape BxT.
-  '''
-    
-      def encode_chars(self, sentence):
-    chars_ids = [self.word_to_char_ids(cur_word)
-                 for cur_word in sentence.split()]
-    return np.vstack([self.bos_chars] + chars_ids + [self.eos_chars])
-    
-    import collections
-import os
-# Dependency imports
-import numpy as np
-    
-      for key, _ in gen_ngrams_dict.iteritems():
-    if key in train_ngrams_dict:
-      unique_ngrams_in_train += 1
-  return float(unique_ngrams_in_train) / float(total_ngrams_produced)
-
+        # See how far apart the test image is from the known faces
+face_distances = face_recognition.face_distance(known_encodings, image_to_test_encoding)
     
     
-def dis_fwd_bidirectional(hparams):
-  '''Returns the *forward* PTB Variable name to MaskGAN Variable dictionary
-  mapping.  This is a highly restrictive function just for testing. This is for
-  the bidirectional_zaremba discriminator.
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
     
+    known_faces = [
+    lmm_face_encoding,
+    al_face_encoding
+]
     
-@pytest.fixture
-def no_memoize(monkeypatch):
-    monkeypatch.setattr('thefuck.utils.memoize.disabled', True)
+    # This code finds all faces in a list of images using the CNN model.
+#
+# This demo is for the _special case_ when you need to find faces in LOTS of images very quickly and all the images
+# are the exact same size. This is common in video processing applications where you have lots of video frames
+# to process.
+#
+# If you are processing a lot of images and using a GPU with CUDA, batch processing can be ~3x faster then processing
+# single images at a time. But if you aren't using a GPU, then batch processing isn't going to be very helpful.
+#
+# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read the video file.
+# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
+# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
     
+            result = runner.invoke(face_detection_cli.main, args=[image_file])
+        self.assertEqual(result.exit_code, 0)
+        parts = result.output.split(',')
+        self.assertTrue('obama.jpg' in parts[0])
+        self.assertEqual(len(parts), 5)
     
-def how_to_configure(proc, TIMEOUT):
-    proc.sendline(u'fuck')
-    assert proc.expect([TIMEOUT, u'alias isn't configured'])
-
+    test_requirements = [
+    'tox',
+    'flake8==2.6.0'
+]
     
-    You can download from:
-  https://osxfuse.github.io/
-Error: An unsatisfied requirement failed this build.'''
+            # Deleting model 'DSymApp'
+        db.delete_table('sentry_dsymapp')
     
-        def __init__(self, mime):
-        self.mime = mime
-    
-    
-@mock.patch('httpie.core.get_response')
-def test_error_traceback(get_response):
-    exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    with raises(ConnectionError):
-        main(['--ignore-stdin', '--traceback', 'www.google.com'])
-    
-    
-def test_unicode_basic_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--verbose', '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/basic-auth/test/' + UNICODE)
-    
-    # Run training, with or without data augmentation.
-if not data_augmentation:
-    print('Not using data augmentation.')
-    model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=epochs,
-              validation_data=(x_test, y_test),
-              shuffle=True,
-              callbacks=callbacks)
-else:
-    print('Using real-time data augmentation.')
-    # This will do preprocessing and realtime data augmentation:
-    datagen = ImageDataGenerator(
-        # set input mean to 0 over the dataset
-        featurewise_center=False,
-        # set each sample mean to 0
-        samplewise_center=False,
-        # divide inputs by std of dataset
-        featurewise_std_normalization=False,
-        # divide each input by its std
-        samplewise_std_normalization=False,
-        # apply ZCA whitening
-        zca_whitening=False,
-        # epsilon for ZCA whitening
-        zca_epsilon=1e-06,
-        # randomly rotate images in the range (deg 0 to 180)
-        rotation_range=0,
-        # randomly shift images horizontally
-        width_shift_range=0.1,
-        # randomly shift images vertically
-        height_shift_range=0.1,
-        # set range for random shear
-        shear_range=0.,
-        # set range for random zoom
-        zoom_range=0.,
-        # set range for random channel shifts
-        channel_shift_range=0.,
-        # set mode for filling points outside the input boundaries
-        fill_mode='nearest',
-        # value used for fill_mode = 'constant'
-        cval=0.,
-        # randomly flip images
-        horizontal_flip=True,
-        # randomly flip images
-        vertical_flip=False,
-        # set rescaling factor (applied before any other transformation)
-        rescale=None,
-        # set function that will be applied on each input
-        preprocessing_function=None,
-        # image data format, either 'channels_first' or 'channels_last'
-        data_format=None,
-        # fraction of images reserved for validation (strictly between 0 and 1)
-        validation_split=0.0)
-    
-        fpath = os.path.join(path, 'train')
-    x_train, y_train = load_batch(fpath, label_key=label_mode + '_labels')
-    
-        model._check_trainable_weights_consistency()
-    if hasattr(model, '_collected_trainable_weights'):
-        trainable_count = count_params(model._collected_trainable_weights)
-    else:
-        trainable_count = count_params(model.trainable_weights)
-    
-    
-def test_decode_predictions():
-    x = np.zeros((2, 1000))
-    x[0, 372] = 1.0
-    x[1, 549] = 1.0
-    outs = utils.decode_predictions(x, top=1)
-    scores = [out[0][2] for out in outs]
-    assert scores[0] == scores[1]
-    
-    
-def test_regularization_shared_layer():
-    dense_layer = Dense(num_classes,
-                        kernel_regularizer=regularizers.l1(),
-                        activity_regularizer=regularizers.l1())
-    
-    
-def to_categorical(y, num_classes=None, dtype='float32'):
-    '''Converts a class vector (integers) to binary class matrix.
-    
-        def _process_batch(self, inputs, initial_state):
-        import tensorflow as tf
-        inputs = tf.transpose(inputs, (1, 0, 2))
-        input_h = initial_state[0]
-        input_h = tf.expand_dims(input_h, axis=0)
-    
-            def get_linux_ca_sha1(nss_path):
-            commonname = 'GoAgent XX-Net - GoAgent' #TODO: here should be GoAgent - XX-Net
-    
-                except NoViableAltException, re:
-                self.reportError(re)
-                self.recover(re) # throw out current char and try again
-    
-            Using setter/getter methods is deprecated. Use o.type instead.'''
-    
-            :raises .errors.PluginError: If there is a problem with the input or
-            the function is unable to correctly revert the configuration
-    
-            achall = self.achalls[0]
-        self.sni.add_chall(achall)
-        response = self.achalls[0].response(self.auth_key)
-        mock_setup_cert = mock.MagicMock(return_value=response)
-        # pylint: disable=protected-access
-        self.sni._setup_challenge_cert = mock_setup_cert
-    
-    # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
-    
-        Snippets without code (only comments) or containing lines starting with ??? should not yeld files,
-    but the counter for naming snippets should still increment.
-    '''
-    parser = argparse.ArgumentParser(description='Split md file into plain text and code blocks')
-    parser.add_argument('sourcefile',
-                        help='which file to read')
-    parser.add_argument('targetfile',
-                        help='where to put plain text')
-    parser.add_argument('codedir',
-                        help='where to put codeblocks')
-    args = parser.parse_args()
+        complete_apps = ['sentry']
+    symmetrical = True
