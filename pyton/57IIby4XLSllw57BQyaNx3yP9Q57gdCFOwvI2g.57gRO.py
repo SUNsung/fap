0d6a24506c9e6f7234ce8c0242ae57cdbe9d5799
SUNsung <weatherius@gmail.com>
2019-03-17 11:36:33 +0200

    Rewrite [57IIby4XLSllw57BQyaNx3yP9Q57gdCFOwvI2g.57gRO.py] File in: 2019-03-17 11:36:32

--- pyton/57IIby4XLSllw57BQyaNx3yP9Q57gdCFOwvI2g.57gRO.py
+++ pyton/57IIby4XLSllw57BQyaNx3yP9Q57gdCFOwvI2g.57gRO.py
@@ -1,140 +1,51 @@
 
         
-        
-def make_setting_element(setting_data, app, fromdocname):
-    refnode = make_refnode(app.builder, fromdocname,
-                           todocname=setting_data['docname'],
-                           targetid=setting_data['refid'],
-                           child=nodes.Text(setting_data['setting_name']))
-    p = nodes.paragraph()
-    p += refnode
+            if not opts and not args:
+        # Display help.
+        print(_help)
+        # Enter GUI mode.
+        #from .gui import gui_main
+        #gui_main()
+    else:
+        conf = {}
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):
+                # Display help.
+                print(_help)
     
-    del sys
-
+        html = get_content(url)
+    pid = match1(html, r'video\.settings\.pid\s*=\s*\'([^\']+)\'')
+    title = match1(html, r'video\.settings\.title\s*=\s*\'([^\']+)\'')
     
-    from scrapy.utils.conf import arglist_to_dict
-from scrapy.exceptions import UsageError
+        ep = 'http://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={}'
     
-            def __init__(self, method=SSL.SSLv23_METHOD):
-            self.method = method
-    
-        def gotHeaders(self, headers):
-        self.headers_time = time()
-        self.response_headers = headers
-    
-        def _download(self, request, spider):
-        slot = self.slot
-        slot.add_request(request)
-        def _on_success(response):
-            assert isinstance(response, (Response, Request))
-            if isinstance(response, Response):
-                response.request = request # tie request to response received
-                logkws = self.logformatter.crawled(request, response, spider)
-                logger.log(*logformatter_adapter(logkws), extra={'spider': spider})
-                self.signals.send_catch_log(signal=signals.response_received, \
-                    response=response, request=request, spider=spider)
-            return response
-    
-    
-def main():
-    print('Making key files...')
-    makeKeyFiles('elgamal', 2048)
-    print('Key files generation successful')
-    
-            if row1 == row2:
-            plaintext += table[row1*5+(col1-1)%5]
-            plaintext += table[row2*5+(col2-1)%5]
-        elif col1 == col2:
-            plaintext += table[((row1-1)%5)*5+col1]
-            plaintext += table[((row2-1)%5)*5+col2]
-        else: # rectangle
-            plaintext += table[row1*5+col2]
-            plaintext += table[row2*5+col1]
-    
-    if __name__ == '__main__':
-    num = generateLargePrime()
-    print(('Prime number:', num))
-    print(('isPrime:', isPrime(num)))
-
-    
-        print('Calculating d that is mod inverse of e...')
-    d = cryptoMath.findModInverse(e, (p - 1) * (q - 1))
-    
-                if new_key is None:
-                break
-    
-    This is a pure Python implementation of Dynamic Programming solution to the longest increasing subsequence of a given sequence.
-    
-        return matchScore
-    
-    from setuptools import setup, find_packages
-setup(
-    name = proj_info['name'],
-    version = VERSION,
-    
-    #----------------------------------------------------------------------
-#helper
-#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
-def dictify(r,root=True):
-    if root:
-        return {r.tag : dictify(r, False)}
-    d=copy(r.attrib)
-    if r.text:
-        d['_text']=r.text
-    for x in r.findall('./*'):
-        if x.tag not in d:
-            d[x.tag]=[]
-        d[x.tag].append(dictify(x,False))
-    return d
-    
-    
-    {
-    {pattern_inline_title = r'<title>([^<]*)</title>'
-pattern_inline_api_key = r'api\.site_key\s*=\s*'([^']+)''
-pattern_inline_img_url = r''url':'([^']+)','key':'[^']+'}}'
-pattern_inline_NSID = r''nsid'\s*:\s*'([^']+)''
-pattern_inline_video_mark = r'('mediaType':'video')'
-    
-    '''
-*What is this pattern about?
-Define a family of algorithms, encapsulate each one, and make them interchangeable.
-Strategy lets the algorithm vary independently from clients that use it.
-    
-    
-class EnglishGetter(object):
-    
-    
-class HierachicalStateMachine(object):
-    def __init__(self):
-        self._active_state = Active(self)  # Unit.Inservice.Active()
-        self._standby_state = Standby(self)  # Unit.Inservice.Standby()
-        self._suspect_state = Suspect(self)  # Unit.OutOfService.Suspect()
-        self._failed_state = Failed(self)  # Unit.OutOfService.Failed()
-        self._current_state = self._standby_state
-        self.states = {
-            'active': self._active_state,
-            'standby': self._standby_state,
-            'suspect': self._suspect_state,
-            'failed': self._failed_state,
-        }
-        self.message_types = {
-            'fault trigger': self._current_state.on_fault_trigger,
-            'switchover': self._current_state.on_switchover,
-            'diagnostics passed': self._current_state.on_diagnostics_passed,
-            'diagnostics failed': self._current_state.on_diagnostics_failed,
-            'operator inservice': self._current_state.on_operator_inservice,
-        }
-    
-        # high-level i.e. Abstraction specific
-    def scale(self, pct):
-        self._radius *= pct
-    
-        def show_item_information(self, item_name):
-        try:
-            item_info = self.model.get(item_name)
-        except Exception:
-            item_type = self.model.item_type
-            self.view.item_not_found(item_type, item_name)
+        # Try harder, check all iframes
+    if 'recur_lv' not in kwargs or kwargs['recur_lv'] < recur_limit:
+        r = kwargs.get('recur_lv')
+        if r is None:
+            r = 1
         else:
-            item_type = self.model.item_type
-            self.view.show_item_information(item_type, item_name, item_info)
+            r += 1
+        iframes = matchall(content, [r'<iframe.+?src=(?:\'|\')(.+?)(?:\'|\')'])
+        for iframe in iframes:
+            if not iframe.startswith('http'):
+                src = urllib.parse.urljoin(url, iframe)
+            else:
+                src = iframe
+            found = embed_download(src, output_dir=output_dir, merge=merge, info_only=info_only, recur_lv=r, **kwargs)
+            if found:
+                return True
+    
+    
+def huomaotv_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    room_id_pattern = r'huomao.com/(\d+)'
+    room_id = match1(url, room_id_pattern)
+    html = get_content(get_mobile_room_url(room_id))
+    
+    
+def icourses_playlist_download(url, output_dir='.', **kwargs):
+    page_type_patt = r'showSectionNode\(this,(\d+),(\d+)\)'
+    resid_courseid_patt = r'changeforvideo\(\'(\d+)\',\'(\d+)\',\'(\d+)\'\)'
+    ep = 'http://www.icourses.cn/jpk/viewCharacterDetail.action?sectionId={}&courseId={}'
+    change_for_video_ip = 'http://www.icourses.cn/jpk/changeforVideo.action?resId={}&courseId={}'
+    video_list = []
