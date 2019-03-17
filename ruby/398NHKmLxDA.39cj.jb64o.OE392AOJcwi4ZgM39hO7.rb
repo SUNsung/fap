522272d885aacb33cbb1587fe5aafb0716b3300e
SUNsung <weatherius@gmail.com>
2019-03-17 03:08:51 +0200

    Rewrite [398NHKmLxDA.39cj.jb64o.OE392AOJcwi4ZgM39hO7.rb] File in: 2019-03-17 03:08:50

--- ruby/398NHKmLxDA.39cj.jb64o.OE392AOJcwi4ZgM39hO7.rb
+++ ruby/398NHKmLxDA.39cj.jb64o.OE392AOJcwi4ZgM39hO7.rb
@@ -1,157 +1,128 @@
 
         
-                  def render_collection
-            @collection.map do |item|
-              value = value_for_collection(item, @value_method)
-              text  = value_for_collection(item, @text_method)
-              default_html_options = default_html_options_for_collection(item, value)
-              additional_html_options = option_html_attributes(item)
-    
-    require 'action_view/helpers/tags/placeholderable'
-    
-          cattr_accessor :type_klass
-    
-        class ClearHelpersTest < ActiveSupport::TestCase
-      def setup
-        @controller = HelperyTestController.new
+                # Reset topic count because we don't count the description topic
+        DB.exec 'UPDATE categories SET topic_count = 0 WHERE id = #{lounge.id}'
       end
-    
-    
-  #
-  # Payload types were copied from xCAT-server source code (IPMI.pm)
-  #
-  RMCP_ERRORS = {
-    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
-    2 => 'Invalid Session ID', #this shouldn't occur...
-    3 => 'Invalid payload type',#shouldn't occur..
-    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
-    5 => 'Invalid integrity algorithm', #same as above
-    6 => 'No matching authentication payload',
-    7 => 'No matching integrity payload',
-    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
-    9 => 'Invalid role',
-    0xa => 'Unauthorised role or privilege level requested',
-    0xb => 'Insufficient resources to create a session at the requested role',
-    0xc => 'Invalid username length',
-    0xd => 'Unauthorized name',
-    0xe => 'Unauthorized GUID',
-    0xf => 'Invalid integrity check value',
-    0x10 => 'Invalid confidentiality algorithm',
-    0x11 => 'No cipher suite match with proposed security algorithms',
-    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
-  }
-    
-              # Encodes the auth_time field
-          #
-          # @return [String]
-          def encode_auth_time
-            [auth_time].pack('N')
-          end
-    
-                checksum = cipher[0, 16]
-            data = cipher[16, cipher.length - 1]
-    
-              # Decodes the pa_data from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Array<Rex::Proto::Kerberos::Model::PreAuthData>]
-          def decode_asn1_pa_data(input)
-            pre_auth = []
-            input.value[0].value.each do |pre_auth_data|
-              pre_auth << Rex::Proto::Kerberos::Model::PreAuthData.decode(pre_auth_data)
-            end
-    
-              # Decodes the till field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_till(input)
-            input.value[0].value
-          end
-    
-          def link_to_with_icon(icon_name, text, url, options = {})
-        options[:class] = (options[:class].to_s + ' icon-link with-tip action-#{icon_name}').strip
-        options[:title] = text if options[:no_text]
-        text = options[:no_text] ? '' : content_tag(:span, text, class: 'text')
-        options.delete(:no_text)
-        if icon_name
-          icon = content_tag(:span, '', class: '#{'mr-2' unless text.empty?} icon icon-#{icon_name}')
-          text.insert(0, icon + ' ')
-        end
-        link_to(text.html_safe, url, options)
-      end
-    
-                it 'adds variant to order just fine' do
-              select2_search tote.name, from: Spree.t(:name_or_sku)
-              within('table.stock-levels') do
-                fill_in 'stock_item_quantity', with: 1
-                click_icon :add
-              end
-    
-    describe 'Shipments', type: :feature do
-  stub_authorization!
-    
-              def render_order(result)
-            if result.success?
-              render_serialized_payload { serialized_current_order }
-            else
-              render_error_payload(result.error)
-            end
-          end
-    
-            def address_params
-          params.require(:address).permit(permitted_address_attributes)
-        end
-    
-                if @order.completed? || @order.next
-              state_callback(:after)
-              respond_with(@order, default_template: 'spree/api/v1/orders/show')
-            else
-              respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
-            end
-          else
-            invalid_resource!(@order)
-          end
-        end
-    
-    run SinatraStaticServer
+    end
+  end
+end
 
     
-        # Outputs a single category as an <a> link.
-    #
-    #  +category+ is a category string to format as an <a> link
-    #
-    # Returns string
-    #
-    def category_link(category)
-      dir = @context.registers[:site].config['category_dir']
-      '<a class='category' href='/#{dir}/#{category.to_url}/'>#{category}</a>'
-    end
+      run_vagrant_command(symlinked?(TestApp.current_path, previous_release_path))
+end
     
-        def html_output_for(script_url, code)
-      code = CGI.escapeHTML code
-      <<-HTML
-<div><script src='#{script_url}'></script>
-<noscript><pre><code>#{code}</code></pre></noscript></div>
-      HTML
-    end
+          def warn_add_git_to_capfile
+        $stderr.puts(<<-MESSAGE)
+[Deprecation Notice] Future versions of Capistrano will not load the Git SCM
+plugin by default. To silence this deprecation warning, add the following to
+your Capfile after `require 'capistrano/deploy'`:
     
-        def self.clear
-      instance.clear
-    end
+          def add_host(host, properties={})
+        new_host = Server[host]
+        new_host.port = properties[:port] if properties.key?(:port)
+        # This matching logic must stay in sync with `Server#matches?`.
+        key = ServerKey.new(new_host.hostname, new_host.port)
+        existing = servers_by_key[key]
+        if existing
+          existing.user = new_host.user if new_host.user
+          existing.with(properties)
+        else
+          servers_by_key[key] = new_host.with(properties)
+        end
+      end
     
-          original_extension = extension(attachment, style_name)
-      style = attachment.styles[style_name.to_s.to_sym]
-      if style && style[:format]
-        style[:format].to_s
-      elsif extensions_for_mime_type.include? original_extension
-        original_extension
-      elsif !extensions_for_mime_type.empty?
-        extensions_for_mime_type.first
+    module LogStash module PluginManager
+  class SpecificationHelpers
+    WILDCARD = '*'
+    WILDCARD_INTO_RE = '.*'
+    
+    module LogStash module PluginManager module PackFetchStrategy
+  class Repository
+    DEFAULT_PACK_URL = 'https://artifacts.elastic.co/downloads/logstash-plugins'
+    PACK_EXTENSION = 'zip'
+    
+      def validate_cache_location
+    cache_location = LogStash::Environment::CACHE_PATH
+    if File.exist?(cache_location)
+      puts('Directory #{cache_location} is going to be overwritten, do you want to continue? (Y/N)')
+      override = ( 'y' == STDIN.gets.strip.downcase ? true : false)
+      if override
+        FileUtils.rm_rf(cache_location)
       else
-        # It's possible, though unlikely, that the mime type is not in the
-        # database, so just use the part after the '/' in the mime type as the
-        # extension.
-        %r{/([^/]*)\z}.match(attachment.content_type)[1]
+        puts('Unpack cancelled: file #{cache_location} already exists, please delete or move it')
+        exit
       end
     end
+  end
+end
+
+    
+        after(:all) do
+      logstash.uninstall
+    end
+    
+    dir = File.dirname(File.expand_path(__FILE__))
+$LOAD_PATH.unshift(File.join(dir, '..', 'lib'))
+$LOAD_PATH.unshift(dir)
+    
+      s.add_development_dependency 'rack-test', '~> 0.6.2'
+  s.add_development_dependency 'shoulda', '~> 3.5.0'
+  s.add_development_dependency 'minitest-reporters', '~> 0.14.16'
+  s.add_development_dependency 'twitter_cldr', '~> 3.2.0'
+  s.add_development_dependency 'mocha', '~> 1.1.0'
+  s.add_development_dependency 'test-unit', '~> 3.1.0'
+  s.add_development_dependency 'webrick', '~> 1.3.1'
+    
+          expect(page).to have_content('R200')
+      expect(page).to have_content('incomplete@example.com')
+    end
+    
+            def find_product
+          super(params[:id])
+        end
+    
+            def scope
+          if params[:country_id]
+            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
+            @country.states.accessible_by(current_ability, :read).order('name ASC')
+          else
+            State.accessible_by(current_ability, :read).order('name ASC')
+          end
+        end
+      end
+    end
+  end
+end
+
+    
+      <h3>Messages</h3>
+  <% @messages.each do |msg| %>
+    <p><%= msg %></p>
+  <% end %>
+
+    
+          def requeue
+        Sidekiq.redis do |conn|
+          conn.rpush('queue:#{queue_name}', job)
+        end
+      end
+    end
+    
+          ObjectSpace.each_object(File) do |fp|
+        begin
+          if !fp.closed? && fp.stat.file? && fp.sync && (fp.fcntl(Fcntl::F_GETFL) & append_flags) == append_flags
+            to_reopen << fp
+          end
+        rescue IOError, Errno::EBADF
+        end
+      end
+    
+    module Sidekiq
+    
+    module Sidekiq
+  class Web
+    ROOT = File.expand_path('#{File.dirname(__FILE__)}/../../web')
+    VIEWS = '#{ROOT}/views'
+    LOCALES = ['#{ROOT}/locales']
+    LAYOUT = '#{VIEWS}/layout.erb'
+    ASSETS = '#{ROOT}/assets'
