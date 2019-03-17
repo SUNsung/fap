f0adc86ce7e0121a17450c7f8fe83b1b6bcf2354
SUNsung <weatherius@gmail.com>
2019-03-17 17:02:29 +0200

    Rewrite [12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb] File in: 2019-03-17 17:02:28

--- ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
+++ ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
@@ -1,85 +1,99 @@
 
         
-        task :permissions do
-  sh %{chmod -R a+rx bin}
-  sh %{chmod -R a+r .}
-  require 'shellwords'
-  Dir.glob('test/**/*_test.rb') do |file|
-    next if file =~ %r{^test/haml/spec/}
-    sh %{chmod a+rx #{file}}
-  end
-end
+              GivenDailyLike.increment_for(user.id)
+      expect(value_for(user.id, dt)).to eq(1)
+      expect(limit_reached_for(user.id, dt)).to eq(false)
     
-        # Returns a string representation of the Sass backtrace.
-    #
-    # @param default_filename [String] The filename to use for unknown files
-    # @see #sass_backtrace
-    # @return [String]
-    def sass_backtrace_str(default_filename = 'an unknown file')
-      lines = message.split('\n')
-      msg = lines[0] + lines[1..-1].
-        map {|l| '\n' + (' ' * 'Error: '.size) + l}.join
-      'Error: #{msg}' +
-        sass_backtrace.each_with_index.map do |entry, i|
-          '\n        #{i == 0 ? 'on' : 'from'} line #{entry[:line]}' +
-            ' of #{entry[:filename] || default_filename}' +
-            (entry[:mixin] ? ', in `#{entry[:mixin]}'' : '')
-        end.join
-    end
+        group.add(moderator)
+    group.save
     
-        def miscellaneous(opts)
-      opts.separator ''
-      opts.separator 'Miscellaneous:'
+    module Vagrant
+  # This class handles guest-OS specific interactions with a machine.
+  # It is primarily responsible for detecting the proper guest OS
+  # implementation and then delegating capabilities.
+  #
+  # Vagrant has many tasks which require specific guest OS knowledge.
+  # These are implemented using a guest/capability system. Various plugins
+  # register as 'guests' which determine the underlying OS of the system.
+  # Then, 'guest capabilities' register themselves for a specific OS (one
+  # or more), and these capabilities are called.
+  #
+  # Example capabilities might be 'mount_virtualbox_shared_folder' or
+  # 'configure_networks'.
+  #
+  # This system allows for maximum flexibility and pluginability for doing
+  # guest OS specific operations.
+  class Guest
+    include CapabilityHost
     
-        # @param args [Array<String>] The command-line arguments
-    def initialize(args, default_syntax)
-      super(args)
-      @options[:sourcemap] = :auto
-      @options[:for_engine] = {
-        :load_paths => default_sass_path
-      }
-      @default_syntax = default_syntax
-    end
-    
-    Then(/^the default stage files are created$/) do
-  staging = TestApp.test_app_path.join('config/deploy/staging.rb')
-  production = TestApp.test_app_path.join('config/deploy/production.rb')
-  expect(File.exist?(staging)).to be true
-  expect(File.exist?(production)).to be true
-end
-    
-    Given(/^a linked file '(.*?)'$/) do |file|
-  # ignoring other linked files
-  TestApp.append_to_deploy_file('set :linked_files, ['#{file}']')
-end
-    
-        def role_properties_for(names, &block)
-      servers.role_properties_for(names, &block)
-    end
-    
-        worker_count.times do |count|
-      template '/data/#{app}/shared/config/sidekiq_#{count}.yml' do
-        owner node[:owner_name]
-        group node[:owner_name]
-        mode 0644
-        source 'sidekiq.yml.erb'
-        variables({
-          :require => '/data/#{app}/current'
-        })
-      end
-    end
-    
-          def perform(yml)
-        (target, method_name, args) = YAML.load(yml)
-        msg = target.public_send(method_name, *args)
-        # The email method can return nil, which causes ActionMailer to return
-        # an undeliverable empty message.
-        if msg
-          deliver(msg)
-        else
-          raise '#{target.name}##{method_name} returned an undeliverable mail object'
+            # Initializes the system. Any subclasses MUST make sure this
+        # method is called on the parent. Therefore, if a subclass overrides
+        # `initialize`, then you must call `super`.
+        def initialize(vm)
+          @vm = vm
         end
+    
+            # This method is expected to return a class that is used for
+        # configuring the provisioner. This return value is expected to be
+        # a subclass of {Config}.
+        #
+        # @return [Config]
+        def self.config_class
+        end
+    
+            # This method is called if the underlying machine ID changes. Providers
+        # can use this method to load in new data for the actual backing
+        # machine or to realize that the machine is now gone (the ID can
+        # become `nil`). No parameters are given, since the underlying machine
+        # is simply the machine instance given to this object. And no
+        # return value is necessary.
+        def machine_id_changed
+        end
+    
+        @statuses = @account.statuses.permitted_for(@account, signed_request_account)
+    @statuses = params[:min_id].present? ? @statuses.paginate_by_min_id(LIMIT, params[:min_id]).reverse : @statuses.paginate_by_max_id(LIMIT, params[:max_id])
+    @statuses = cache_collection(@statuses, Status)
+  end
+    
+        def destroy
+      authorize @report_note, :destroy?
+      @report_note.destroy!
+      redirect_to admin_report_path(@report_note.report_id), notice: I18n.t('admin.report_notes.destroyed_msg')
+    end
+    
+      def payload
+    @_payload ||= request.body.read
+  end
+    
+      def show
+    render_cached_json('api:v1:instances:activity:show', expires_in: 1.day) { activity }
+  end
+    
+        web_subscription = ::Web::PushSubscription.create!(
+      endpoint: subscription_params[:endpoint],
+      key_p256dh: subscription_params[:keys][:p256dh],
+      key_auth: subscription_params[:keys][:auth],
+      data: data,
+      user_id: active_session.user_id,
+      access_token_id: active_session.access_token_id
+    )
+    
+        define_method provider do
+      @user = User.find_for_oauth(request.env['omniauth.auth'], current_user)
+    
+      def default_locale
+    if ENV['DEFAULT_LOCALE'].present?
+      I18n.default_locale
+    else
+      request_locale || I18n.default_locale
+    end
+  end
+    
+          return false
+    end
+    
+          def css # custom css
+        @css
       end
     
-        def _erb(file, locals)
-      locals.each {|k, v| define_singleton_method(k){ v } unless (singleton_methods.include? k)} if locals
+        assert_no_match /New/, last_response.body, ''New' link not blocked in pages template'
