4a92f93ed962c0f9eee6d58922070e9b55c9aa68
SUNsung <weatherius@gmail.com>
2019-03-18 19:57:14 +0200

    Rewrite [26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb] File in: 2019-03-18 19:57:13

--- ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
+++ ruby/26NrgZdbDZydw26bF3GTx0FdM2261BwP.aswsr226lzJ.rb
@@ -1,119 +1,115 @@
 
         
-              GivenDailyLike.decrement_for(user.id)
-      expect(value_for(user.id, dt)).to eq(0)
-      expect(limit_reached_for(user.id, dt)).to eq(false)
+        desc 'Open an irb session preloaded with this library'
+task :console do
+  sh 'irb -rubygems -r ./lib/#{name}.rb'
+end
+
+    
+    Jekyll::PluginManager.require_from_bundler
+    
+            def handle_websockets_event(websocket)
+          websocket.onopen { |handshake| connect(websocket, handshake) }
+          websocket.onclose { disconnect(websocket) }
+          websocket.onmessage { |msg| print_message(msg) }
+          websocket.onerror { |error| log_error(error) }
+        end
+    
+                rows << {
+              label_id: label_id,
+              target_id: target_id,
+              target_type: issue.issuable_type,
+              created_at: time,
+              updated_at: time
+            }
+          end
+    
+          # project - An instance of `Project`.
+      # object - The object to look up or set a database ID for.
+      def initialize(project, object)
+        @project = project
+        @object = object
+      end
+    
+            waiter
+      end
+    
+              user = Representation::User.from_api_response(note.user) if note.user
+          hash = {
+            noteable_type: 'MergeRequest',
+            noteable_id: matches[:iid].to_i,
+            file_path: note.path,
+            commit_id: note.commit_id,
+            diff_hunk: note.diff_hunk,
+            author: user,
+            note: note.body,
+            created_at: note.created_at,
+            updated_at: note.updated_at,
+            github_id: note.id
+          }
+    
+        def add(path, content)
+      @pages[path] = content
     end
     
-          def cache_key_iid
-        if object.respond_to?(:noteable_id)
-          object.noteable_id
-        elsif object.respond_to?(:iid)
-          object.iid
-        else
-          raise(
-            TypeError,
-            'Instances of #{object.class} are not supported'
-          )
-        end
+        def error?
+      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
+    end
+    
+          def initial_urls
+        return super unless self.class.internal_urls
+        @initial_urls ||= self.class.internal_urls.map(&method(:url_for)).freeze
       end
+    
+        def log(msg)
+      puts '\r' + justify(msg)
+    end
+    
+        it 'returns the last access time for the provided file when passed ?A' do
+      Kernel.test(?A, @tmp_file).should == @tmp_file.atime
+    end
+    
+        context 'opening brace on same line as first element' do
+      it 'allows closing brace on same line from last element' do
+        expect_no_offenses(construct(false, false))
+      end
+    
+    module RuboCop
+  module AST
+    # A node extension for `case` nodes. This will be used in place of a plain
+    # node when the builder constructs the AST, making its methods available
+    # to all `case` nodes within RuboCop.
+    class CaseNode < Node
+      include ConditionalNode
+    
+    module RuboCop
+  module AST
+    # A node extension for `kwsplat` nodes. This will be used in place of a
+    # plain  node when the builder constructs the AST, making its methods
+    # available to all `kwsplat` nodes within RuboCop.
+    class KeywordSplatNode < Node
+      include HashElementNode
+    
+          private_constant :HashElementDelta
     end
   end
 end
 
     
-              hash[:author] &&= Representation::User.from_json_hash(hash[:author])
-    
-            # attributes - A Hash containing the user details. The keys of this
-        #              Hash (and any nested hashes) must be symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-      end
-    end
-  end
-end
-
-    
-          def initialize(app)
-        @app = app
-      end
-    
-    module Vagrant
-  # This class handles guest-OS specific interactions with a machine.
-  # It is primarily responsible for detecting the proper guest OS
-  # implementation and then delegating capabilities.
-  #
-  # Vagrant has many tasks which require specific guest OS knowledge.
-  # These are implemented using a guest/capability system. Various plugins
-  # register as 'guests' which determine the underlying OS of the system.
-  # Then, 'guest capabilities' register themselves for a specific OS (one
-  # or more), and these capabilities are called.
-  #
-  # Example capabilities might be 'mount_virtualbox_shared_folder' or
-  # 'configure_networks'.
-  #
-  # This system allows for maximum flexibility and pluginability for doing
-  # guest OS specific operations.
-  class Guest
-    include CapabilityHost
-    
-              # First determine the proper array of VMs.
-          vms = []
-          if names.length > 0
-            names.each do |name|
-              if pattern = name[/^\/(.+?)\/$/, 1]
-                # This is a regular expression name, so we convert to a regular
-                # expression and allow that sort of matching.
-                regex = Regexp.new(pattern)
-    
-            # Halt the machine. This method should gracefully shut down the
-        # operating system. This method will cause `vagrant halt` and associated
-        # commands to _block_, meaning that if the machine doesn't halt
-        # in a reasonable amount of time, this method should just return.
-        #
-        # If when this method returns, the machine's state isn't 'powered_off,'
-        # Vagrant will proceed to forcefully shut the machine down.
-        def halt
-          raise BaseError, _key: :unsupported_halt
-        end
-    
-              # Creating a shallow copy of the arguments so the OptionParser
-          # doesn't destroy the originals.
-          argv = @argv.dup
-    
-    module Vagrant
-  module Plugin
-    module V2
-      # This class maintains a list of all the registered plugins as well
-      # as provides methods that allow querying all registered components of
-      # those plugins as a single unit.
-      class Manager
-        attr_reader :registered
-    
-          spec['main'] =
-          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
-          find_files.(Bootstrap.fonts_path) +
-          %w(assets/javascripts/bootstrap.js)
-    
-      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
-    @logger     = Logger.new
-    @repo       = repo
-    @branch     = branch || 'master'
-    @branch_sha = get_branch_sha
-    @cache_path = cache_path
-    @repo_url   = 'https://github.com/#@repo'
-    @save_to    = {
-        js:    'assets/javascripts/bootstrap',
-        scss:  'assets/stylesheets/bootstrap',
-        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
-  end
-    
-          # If the importer is based on files on the local filesystem
-      # this method should return folders which should be watched
-      # for changes.
+          # Checks whether any argument of the node is a splat
+      # argument, i.e. `*splat`.
       #
-      # @return [Array<String>] List of absolute paths of directories to watch
-      def directories_to_watch
-        []
+      # @return [Boolean] whether the node is a splat argument
+      def splat_argument?
+        arguments? &&
+          (arguments.any?(&:splat_type?) || arguments.any?(&:restarg_type?))
       end
+      alias rest_argument? splat_argument?
+    
+          def remove(klass)
+        entries.delete_if { |entry| entry.klass == klass }
+      end
+    
+        def request
+      @request ||= ::Rack::Request.new(env)
+    end
