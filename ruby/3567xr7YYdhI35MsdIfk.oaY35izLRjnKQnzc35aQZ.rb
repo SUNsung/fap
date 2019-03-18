ee81c33acbd2851a933ac8466efe8ed6b010af4d
SUNsung <weatherius@gmail.com>
2019-03-18 03:53:25 +0200

    Rewrite [3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb] File in: 2019-03-18 03:53:24

--- ruby/3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb
+++ ruby/3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb
@@ -1,87 +1,44 @@
 
         
-            if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
-    end
+          # Configure static asset server for tests with Cache-Control for performance.
+  if config.respond_to?(:serve_static_files)
+    # rails >= 4.2
+    config.serve_static_files = true
+  elsif config.respond_to?(:serve_static_assets)
+    # rails < 4.2
+    config.serve_static_assets = true
+  end
+  config.static_cache_control = 'public, max-age=3600'
+    
+      def ruby_prefix
+    RbConfig::CONFIG['prefix']
   end
     
-      task :index do
-    doc = File.read('README.md')
-    file = 'doc/rack-protection-readme.md'
-    Dir.mkdir 'doc' unless File.directory? 'doc'
-    puts 'writing #{file}'
-    File.open(file, 'w') { |f| f << doc }
+          page.find('#option_type_option_values_attributes_0_name').set('color')
+      page.find('#option_type_option_values_attributes_0_presentation').set('black')
+      click_button 'Update'
+      expect(page).to have_content('successfully updated!')
+    
+              def ensure_order
+            raise ActiveRecord::RecordNotFound if spree_current_order.nil?
+          end
+    
+              state = @states.last
+          respond_with(@states) if stale?(state)
+        end
+    
+        def each_definition
+      @attachments.each do |klass, attachments|
+        attachments.each do |name, options|
+          yield klass, name, options
+        end
+      end
+    end
+    
+        # scale to the requested geometry and preserve the aspect ratio
+    def scale_to(new_geometry)
+      scale = [new_geometry.width.to_f / self.width.to_f , new_geometry.height.to_f / self.height.to_f].min
+      Paperclip::Geometry.new((self.width * scale).round, (self.height * scale).round)
+    end
   end
-    
-            safe?(env) ||
-          valid_token?(session, env['HTTP_X_CSRF_TOKEN']) ||
-          valid_token?(session, Request.new(env).params[options[:authenticity_param]]) ||
-          ( options[:allow_if] && options[:allow_if].call(env) )
-      end
-    
-          def redirect(env)
-        request = Request.new(env)
-        warn env, 'attack prevented by #{self.class}'
-        [302, {'Content-Type' => 'text/html', 'Location' => request.path}, []]
-      end
-    
-          def initialize(*)
-        super
-    
-        # This allows packages to define flags for the fpm command line
-    def option(flag, param, help, options={}, &block)
-      @options ||= []
-      if !flag.is_a?(Array)
-        flag = [flag]
-      end
-    
-        wordsize = case @architecture
-    when nil, 'native'
-      %x{getconf LONG_BIT}.chomp # 'native' is current arch
-    when 'amd64'
-      '64'
-    when 'i386'
-      '32'
-    else
-      %x{getconf LONG_BIT}.chomp # default to native, the current arch
-    end
-    
-      private
-  def input(package)
-    # Notes:
-    # * npm respects PREFIX
-    settings = {
-      'cache' => build_path('npm_cache'),
-      'loglevel' => 'warn',
-      'global' => 'true'
-    }
-    
-        File.write(build_path('description'), self.description + '\n')
-    
-      # The directory we create should just be the name of the package as the
-  # module name
-  def default_output
-    name
-  end # def default_output
-    
-        # use dir to set stuff up properly, mainly so I don't have to reimplement
-    # the chdir/prefix stuff special for tar.
-    dir = convert(FPM::Package::Dir)
-    if attributes[:chdir]
-      dir.attributes[:chdir] = File.join(build_path, attributes[:chdir])
-    else
-      dir.attributes[:chdir] = build_path
-    end
-    
-      # Input a zipfile.
-  def input(input_path)
-    # use part of the filename as the package name
-    self.name = File.extname(input_path)[1..-1]
+end
