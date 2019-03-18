80c0964e4e4c71cb8e1303e1b99bc3ca1c82a140
SUNsung <weatherius@gmail.com>
2019-03-18 23:41:29 +0200

    Rewrite [40gmHGKyKNt0Q406fma5grX4uE40nCz1SWSuwY40NtJ.cpp] File in: 2019-03-18 23:41:29

--- cccccc/40gmHGKyKNt0Q406fma5grX4uE40nCz1SWSuwY40NtJ.cpp
+++ cccccc/40gmHGKyKNt0Q406fma5grX4uE40nCz1SWSuwY40NtJ.cpp
@@ -1,26 +1,230 @@
 
         
-        
-    {
-    {
-    {        for (;;) {
-          auto start = index.fetch_add(work_chunk);
-          auto const stop = std::min(start + work_chunk, inputs.size());
-          if (start >= stop) break;
-          for (auto i = start; i != stop; ++i) func(inputs[i]);
-        }
-      } catch (const std::exception& e) {
-        std::fprintf(stderr,
-          'worker thread exited with exception: %s\n', e.what());
-        failed = true;
+        #endif // BITCOIN_QT_NETWORKSTYLE_H
+
+    
+    static void secp256k1_hmac_sha256_finalize(secp256k1_hmac_sha256_t *hash, unsigned char *out32) {
+    unsigned char temp[32];
+    secp256k1_sha256_finalize(&hash->inner, temp);
+    secp256k1_sha256_write(&hash->outer, temp, 32);
+    memset(temp, 0, 32);
+    secp256k1_sha256_finalize(&hash->outer, out32);
+}
+    
+    #ifndef BITCOIN_BECH32_H
+#define BITCOIN_BECH32_H
+    
+      /**
+   * @brief Applies the same transformation defined in the data layer's
+   * transform_param block to all the num images in a input_blob.
+   *
+   * @param input_blob
+   *    A Blob containing the data to be transformed. It applies the same
+   *    transformation to all the num images in the blob.
+   * @param transformed_blob
+   *    This is destination blob, it will contain as many images as the
+   *    input blob. It can be part of top blob's data.
+   */
+  void Transform(Blob<Dtype>* input_blob, Blob<Dtype>* transformed_blob);
+    
+    
+    {  static string LayerTypeListString() {
+    vector<string> layer_types = LayerTypeList();
+    string layer_types_str;
+    for (vector<string>::iterator iter = layer_types.begin();
+         iter != layer_types.end(); ++iter) {
+      if (iter != layer_types.begin()) {
+        layer_types_str += ', ';
       }
-    }));
+      layer_types_str += *iter;
+    }
+    return layer_types_str;
   }
+};
     
-    #include 'hphp/runtime/base/apc-handle.h'
     
-    #ifndef HPHP_DATA_STREAM_WRAPPER_H
-#define HPHP_DATA_STREAM_WRAPPER_H
+    {  /**
+   * @brief Computes the error gradient w.r.t. the absolute value inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x} =
+   *            \mathrm{sign}(x) \frac{\partial E}{\partial y}
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
     
-    #include 'hphp/runtime/base/stream-wrapper.h'
-#include 'hphp/runtime/base/runtime-error.h'
+    namespace caffe {
+    }
+    
+    #ifdef USE_CUDNN
+template <typename Dtype>
+class CuDNNLCNLayer : public LRNLayer<Dtype> {
+ public:
+  explicit CuDNNLCNLayer(const LayerParameter& param)
+      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
+        tempData1(NULL), tempData2(NULL) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNLCNLayer();
+    }
+    
+    namespace caffe {
+    }
+    
+    
+        case URX_BACKSLASH_X:
+            //  Match a Grapheme, as defined by Unicode TR 29.
+            //  Differs slightly from Perl, which consumes combining marks independently
+            //    of context.
+            {
+    }
+    
+    Locale
+RuleBasedCollator::getLocale(ULocDataLocaleType type, UErrorCode& errorCode) const {
+    if(U_FAILURE(errorCode)) {
+        return Locale::getRoot();
+    }
+    switch(type) {
+    case ULOC_ACTUAL_LOCALE:
+        return actualLocaleIsSameAsValid ? validLocale : tailoring->actualLocale;
+    case ULOC_VALID_LOCALE:
+        return validLocale;
+    case ULOC_REQUESTED_LOCALE:
+    default:
+        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
+        return Locale::getRoot();
+    }
+}
+    
+    U_NAMESPACE_BEGIN
+    
+        /**
+     * Get maximum significant digits. 0 means no minimum.
+     */
+    int32_t getMin() const {
+        return fMin;
+    }
+    
+    
+/**
+ * A formatter for small, positive integers.
+ */
+class U_I18N_API SmallIntFormatter : public UMemory {
+public:
+    /**
+     * Estimates the actual digit count needed to format positiveValue
+     * using the given range of digit counts.
+     * Returns a value that is at least the actual digit count needed.
+     *
+     * @param positiveValue the value to format
+     * @param range the acceptable range of digit counts.
+     */
+    static int32_t estimateDigitCount(
+            int32_t positiveValue, const IntDigitCountRange &range);
+    }
+    
+    
+//------------------------------------------------------------------------------
+//
+//   smpdtfmt_cleanup     Memory cleanup function, free/delete all
+//                      cached memory.  Called by ICU's u_cleanup() function.
+//
+//------------------------------------------------------------------------------
+UBool
+SimpleDateFormatStaticSets::cleanup(void)
+{
+    delete gStaticSets;
+    gStaticSets = NULL;
+    gSimpleDateFormatStaticSetsInitOnce.reset();
+    return TRUE;
+}
+    
+        /**
+     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
+     *
+     * @param keyword for example 'few' or 'other'
+     * @return the index of the plural form corresponding to the keyword
+     */
+    static int32_t indexFromString(const char *keyword, UErrorCode &errorCode);
+    
+    /**
+ * UnicodeFunctor API.  Cast 'this' to a UnicodeMatcher* pointer
+ * and return the pointer.
+ */
+UnicodeMatcher* StringMatcher::toMatcher() const {
+  StringMatcher  *nonconst_this = const_cast<StringMatcher *>(this);
+  UnicodeMatcher *nonconst_base = static_cast<UnicodeMatcher *>(nonconst_this);
+  
+  return nonconst_base;
+}
+    
+          CompressedIterator<int> ci(buffer.data(), alphabet_size);
+      std::vector<int> output(input.size());
+      for (int i = 0; i < input.size(); i++) {
+        output[i] = ci[i];
+      }
+    
+    /*!
+ * \brief Registry entry for tree updater.
+ */
+struct GradientBoosterReg
+    : public dmlc::FunctionRegEntryBase<
+  GradientBoosterReg,
+  std::function<GradientBooster* (const std::vector<std::shared_ptr<DMatrix> > &cached_mats,
+                                  bst_float base_margin)> > {
+};
+    
+        // Remove the assertion on batch.index, which can be null in the case that the data in this
+    // batch is entirely sparse. Although it's true that this indicates a likely issue with the
+    // user's data workflows, passing XGBoost entirely sparse data should not cause it to fail.
+    // See https://github.com/dmlc/xgboost/issues/1827 for complete detail.
+    // CHECK(batch.index != nullptr);
+    
+    class IExpressionCommand
+{
+public:
+    virtual CalculationManager::CommandType GetCommandType() const = 0;
+    virtual void Accept(_In_ ISerializeCommandVisitor &commandVisitor) = 0;
+};
+    
+        String^ navCategoryItemAutomationNameFormat = resProvider.GetResourceString(L'NavCategoryItem_AutomationNameFormat');
+    
+            private:
+            // IConverterDataLoader
+            void LoadData() override;
+            std::vector<UnitConversionManager::Category> LoadOrderedCategories() override;
+            std::vector<UnitConversionManager::Unit> LoadOrderedUnits(const UnitConversionManager::Category& c) override;
+            std::unordered_map<UnitConversionManager::Unit, UnitConversionManager::ConversionData, UnitConversionManager::UnitHash> LoadOrderedRatios(const UnitConversionManager::Unit& unit) override;
+            bool SupportsCategory(const UnitConversionManager::Category& target) override;
+            // IConverterDataLoader
+    
+    #include 'Views/CalculatorProgrammerBitFlipPanel.g.h'
+#include 'Controls/FlipButtons.h'
+#include 'Converters/BitFlipAutomationNameConverter.h'
+#include 'Converters/BooleanNegationConverter.h'
+#include 'Converters/VisibilityNegationConverter.h'
+#include 'CalcViewModel/StandardCalculatorViewModel.h'
+    
+            property CalculatorApp::ViewModel::StandardCalculatorViewModel^ Model
+        {
+            CalculatorApp::ViewModel::StandardCalculatorViewModel^ get() {
+                return static_cast<CalculatorApp::ViewModel::StandardCalculatorViewModel^>(this->DataContext);
+            }
+        }
+    
+    #pragma once
