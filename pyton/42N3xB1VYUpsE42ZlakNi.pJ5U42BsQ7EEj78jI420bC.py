db4a232d984831977c5d2940935d1eb2e0ca504e
SUNsung <weatherius@gmail.com>
2019-03-18 13:49:47 +0200

    Rewrite [42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py] File in: 2019-03-18 13:49:46

--- pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
+++ pyton/42N3xB1VYUpsE42ZlakNi.pJ5U42BsQ7EEj78jI420bC.py
@@ -1,22 +1,124 @@
 
         
-        
-def get_github_url(app, view, path):
-    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
-    return (
-        github_fmt.format(app.config.edit_on_github_project, view,
-                          app.config.edit_on_github_branch,
-                          app.config.edit_on_github_src_path, path))
+        from mrjob.job import MRJob
     
-                uid = item.get(CONF_UID)
-            if uid is None:
-                uid = str(uuid.uuid4())
-            output[ATTR_UID] = uid
+        def __init__(self, pages, data_store, reverse_index_queue, doc_index_queue):
+        self.pages = pages
+        self.data_store = data_store
+        self.reverse_index_queue = reverse_index_queue
+        self.doc_index_queue = doc_index_queue
     
-    import homeassistant.helpers.config_validation as cv
-import homeassistant.util.dt as dt_util
-from homeassistant.components.device_tracker import (
-    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+        ie_htmls = []
+    for ie in youtube_dl.list_extractors(age_limit=None):
+        ie_html = '<b>{}</b>'.format(ie.IE_NAME)
+        ie_desc = getattr(ie, 'IE_DESC', None)
+        if ie_desc is False:
+            continue
+        elif ie_desc is not None:
+            ie_html += ': {}'.format(ie.IE_DESC)
+        if not ie.working():
+            ie_html += ' (Currently broken)'
+        ie_htmls.append('<li>{}</li>'.format(ie_html))
     
-        observe()
+    import io
+import optparse
+import re
+    
+    
+# find the correct sorting and add the required base classes so that sublcasses
+# can be correctly created
+classes = _ALL_CLASSES[:-1]
+ordered_cls = []
+while classes:
+    for c in classes[:]:
+        bases = set(c.__bases__) - set((object, InfoExtractor, SearchInfoExtractor))
+        stop = False
+        for b in bases:
+            if b not in classes and b not in ordered_cls:
+                if b.__name__ == 'GenericIE':
+                    exit()
+                classes.insert(0, b)
+                stop = True
+        if stop:
+            break
+        if all(b in ordered_cls for b in bases):
+            ordered_cls.append(c)
+            classes.remove(c)
+            break
+ordered_cls.append(_ALL_CLASSES[-1])
+    
+    header = oldreadme[:oldreadme.index('# OPTIONS')]
+footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
+    
+    
+if __name__ == '__main__':
+    unittest.main()
+
+    
+            If `sign` is incorrect, /validate call throws an HTTP 556 error
+        '''
+        logger = WarningLogger()
+        ie = IqiyiIEWithCredentials(FakeYDL({'logger': logger}))
+        ie._login()
+        self.assertTrue('unable to log in:' in logger.messages[0])
+    
+        def test_socks5(self):
+        self.assertTrue(isinstance(self._get_ip('socks5'), compat_str))
+    
+        # register the database commands
+    from flaskr import db
+    db.init_app(app)
+    
+        return render_template('auth/login.html')
+    
+        def fake_init_db():
+        Recorder.called = True
+    
+        def add_url_rule(self, rule, endpoint=None, view_func=None, **options):
+        '''A helper method to register a rule (and optionally a view function)
+        to the application.  The endpoint is automatically prefixed with the
+        blueprint's name.
+        '''
+        if self.url_prefix is not None:
+            if rule:
+                rule = '/'.join((
+                    self.url_prefix.rstrip('/'), rule.lstrip('/')))
+            else:
+                rule = self.url_prefix
+        options.setdefault('subdomain', self.subdomain)
+        if endpoint is None:
+            endpoint = _endpoint_from_view_func(view_func)
+        defaults = self.url_defaults
+        if 'defaults' in options:
+            defaults = dict(defaults, **options.pop('defaults'))
+        self.app.add_url_rule(rule, '%s.%s' % (self.blueprint.name, endpoint),
+                              view_func, defaults=defaults, **options)
+    
+                resp = app.response_class()
+            if not session_interface.is_null_session(sess):
+                session_interface.save_session(app, sess, resp)
+            headers = resp.get_wsgi_headers(c.request.environ)
+            self.cookie_jar.extract_wsgi(c.request.environ, headers)
+    
+            for i in range(n):  # 对每个数据
+            i_score = np.inf
+            i_label = -1
+            for j in range(k):  # 与每个质心比较
+                s_ij = score(data[i], centers[j])
+                if s_ij < i_score:
+                    i_score = s_ij
+                    i_label = j
+    
+    
+def linear(x):
+    ''''''
+    return x
+    
+        def __init__(self, l1=0., l2=0.):
+        '''
+        Args:
+            l1(float): L1 正则化的系数
+            l2(float): L2 正则化的系数
+        '''
+        self.l1 = np.asarray(l1, dtype=np.float32)
+        self.l2 = np.asarray(l2, dtype=np.float32)
