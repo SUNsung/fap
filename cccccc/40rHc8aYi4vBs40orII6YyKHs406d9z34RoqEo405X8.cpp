a59c260872736eff4f4f180851fdbe53a00159ad
SUNsung <weatherius@gmail.com>
2019-03-19 18:28:56 +0200

    Rewrite [40rHc8aYi4vBs40orII6YyKHs406d9z34RoqEo405X8.cpp] File in: 2019-03-19 18:28:56

--- cccccc/40rHc8aYi4vBs40orII6YyKHs406d9z34RoqEo405X8.cpp
+++ cccccc/40rHc8aYi4vBs40orII6YyKHs406d9z34RoqEo405X8.cpp
@@ -1,32 +1,165 @@
 
         
-        void ExtensionStatus::__set_uuid(const ExtensionRouteUUID val) {
-  this->uuid = val;
-}
-std::ostream& operator<<(std::ostream& out, const ExtensionStatus& obj)
-{
-  obj.printTo(out);
-  return out;
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+#define TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+    
+    class AckermannOp : public OpKernel {
+ public:
+  explicit AckermannOp(OpKernelConstruction* context) : OpKernel(context) {}
+    }
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    // Must be included first.
+#include 'tensorflow/python/lib/core/numpy.h'
+    
+    
+    {}  // namespace tensorflow
+
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+     private:
+  PyRecordReader();
+    
+    // Functions for getting information about kernels registered in the binary.
+#ifndef TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
+#define TENSORFLOW_PYTHON_UTIL_KERNEL_REGISTRY_H_
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+    }
+    
+        // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, 'Dear ImGui GLFW+OpenGL3 example', NULL, NULL);
+    if (window == NULL)
+        return 1;
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+    
+        IMGUI_API bool BuildFontAtlas(ImFontAtlas* atlas, unsigned int extra_flags = 0);
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+    // DirectX
+#include <stdio.h>
+#include <d3d10_1.h>
+#include <d3d10.h>
+#include <d3dcompiler.h>
+#ifdef _MSC_VER
+#pragma comment(lib, 'd3dcompiler') // Automatically link with d3dcompiler.lib as we are using D3DCompile() below.
+#endif
+    
+    
+    {    // At this point note that we set ImGui::GetIO().Fonts->TexID to be == g_FontTexture, so clear both.
+    ImGuiIO& io = ImGui::GetIO();
+    IM_ASSERT(g_FontTexture == io.Fonts->TexID);
+    if (g_FontTexture)
+        g_FontTexture->Release();
+    g_FontTexture = NULL;
+    io.Fonts->TexID = NULL;
 }
     
-      auto wrapped = WrappedMessage{};
-  wrapped.msg.a_ = 1;
-  wrapped.msg.b_ = 2;
-  wrapped.msg.c_ = 't';
-  wrapped.msg.d_ = 'i';
-  wrapped.size = sizeof(TestMessage);
-  wrapped.header.type = PERF_RECORD_SAMPLE;
-  wrapped.header.size = sizeof(WrappedMessage);
-  auto const wrapped_ptr =
-      reinterpret_cast<ebpf::impl::ByteType const*>(&wrapped);
-  std::copy(wrapped_ptr, wrapped_ptr + sizeof(WrappedMessage), &buf[0] + tail);
     
-    Expected<LinuxProbesControl, LinuxProbesControl::Error>
-LinuxProbesControl::spawn() {
-  auto exp = setMemoryLockSystemLimit();
-  if (exp.isError()) {
-    return createError(Error::SystemUnknown,
-                       'failed to set appropriate memory lock limits',
-                       exp.takeError());
+    {  SocketCanClientRaw socket_can_client;
+  EXPECT_TRUE(socket_can_client.Init(param));
+  EXPECT_EQ(socket_can_client.Start(), ErrorCode::CAN_CLIENT_ERROR_BASE);
+  std::vector<CanFrame> frames;
+  int32_t num = 0;
+  EXPECT_EQ(socket_can_client.Send(frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  EXPECT_EQ(socket_can_client.Receive(&frames, &num),
+            ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED);
+  CanFrame can_frame;
+  frames.push_back(can_frame);
+  EXPECT_EQ(socket_can_client.SendSingleFrame(frames),
+            ErrorCode::CAN_CLIENT_ERROR_SEND_FAILED);
+  socket_can_client.Stop();
+}
+    
+    #include 'gtest/gtest.h'
+    
+    // Canbus gflags
+DEFINE_double(sensor_freq, 100,
+              'Sensor feedback timer frequency -- 0 means event trigger.');
+    
+      x <<= 3;
+  x |= t;
+    
+    
+    {  switch (x) {
+    case 0x0:
+      return OUTPUT_TYPE_NONE;
+    case 0x1:
+      return OUTPUT_TYPE_OBJECTS;
+    case 0x2:
+      return OUTPUT_TYPE_CLUSTERS;
+    default:
+      return OUTPUT_TYPE_ERROR;
+  }
+}
+    
+    TEST(TestPiecewiseLinearConstraint, add_derivative_boundary) {
+  PiecewiseLinearConstraint constraint(10, 0.1);
+  std::vector<uint32_t> index_list;
+  std::vector<double> lower_bound;
+  std::vector<double> upper_bound;
+  for (uint32_t i = 0; i < 10; ++i) {
+    index_list.push_back(i);
+    lower_bound.push_back(1.0);
+    upper_bound.push_back(100.0);
   }
     }
+    
+    
+    {
+    {}  // namespace planning
+}  // namespace apollo
+
+    
+    Eigen::MatrixXd SplineSegKernel::NthDerivativeKernel(
+    const uint32_t n, const uint32_t num_params, const double accumulated_x) {
+  if (n == 1) {
+    return DerivativeKernel(num_params, accumulated_x);
+  } else if (n == 2) {
+    return SecondOrderDerivativeKernel(num_params, accumulated_x);
+  } else if (n == 3) {
+    return ThirdOrderDerivativeKernel(num_params, accumulated_x);
+  } else {
+    return Eigen::MatrixXd::Zero(num_params, num_params);
+  }
+}
+    
+    
+    {  acc.Parse(bytes, length, &chassis_detail);
+  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().manual_input(), 0.258);
+  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().commanded_value(),
+                   0.772);
+  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().output_value(), 4.37);
+}
