42cfc0d52f1ad8fd3de062365715d45cca436d46
SUNsung <weatherius@gmail.com>
2019-03-19 04:30:22 +0200

    Rewrite [40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp] File in: 2019-03-19 04:30:21

--- cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
+++ cccccc/40QYyPXAmEtaU40EWGZ40VGOJw405jRc.z3EezI40t8Y.cpp
@@ -1,310 +1,190 @@
 
         
-          // Convert input shapes.
-  std::vector<TensorShapeProto> input_shapes;
-  std::vector<
-      std::unique_ptr<std::vector<std::pair<TensorShapeProto, DataType>>>>
-      input_handle_shapes_and_types;
-  input_shapes.resize(input_serialized_shapes.size());
-  input_handle_shapes_and_types.resize(input_serialized_shapes.size());
-  CppShapeInferenceResult tmp;
-  for (int i = 0; i < input_serialized_shapes.size(); ++i) {
-    tmp.Clear();
-    if (!tmp.ParseFromString(input_serialized_shapes[i])) {
-      return errors::InvalidArgument(
-          'Error parsing shape proto during cpp shape inference');
-    }
+        namespace atom {
     }
     
-    // Get the python wrappers for a list of ops in a OpList.
-// `op_list_buf` should be a pointer to a buffer containing
-// the binary encoded OpList proto, and `op_list_len` should be the
-// length of that buffer.
-string GetPythonWrappers(const char* op_list_buf, size_t op_list_len);
     
-    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
-// a copy.
-Status TensorToNdarray(const Tensor& t, PyObject** ret);
+    {}  // namespace mate
     
-    // Creates a numpy array with shapes specified by dim_size and dims and content
-// in data. The array does not own the memory, and destructor will be called to
-// release it. If the status is not ok the caller is responsible for releasing
-// the memory.
-Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
-                       std::function<void()> destructor, PyObject** result);
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      // Creates a batched FFT plan.
-  //
-  // stream:          The GPU stream in which the FFT runs.
-  // rank:            Dimensionality of the transform (1, 2, or 3).
-  // elem_count:      Array of size rank, describing the size of each dimension.
-  // input_embed, output_embed:
-  //                  Pointer of size rank that indicates the storage dimensions
-  //                  of the input/output data in memory. If set to null_ptr all
-  //                  other advanced data layout parameters are ignored.
-  // input_stride:    Indicates the distance (number of elements; same below)
-  //                  between two successive input elements.
-  // input_distance:  Indicates the distance between the first element of two
-  //                  consecutive signals in a batch of the input data.
-  // output_stride:   Indicates the distance between two successive output
-  //                  elements.
-  // output_distance: Indicates the distance between the first element of two
-  //                  consecutive signals in a batch of the output data.
-  virtual std::unique_ptr<Plan> CreateBatchedPlan(
-      Stream *stream, int rank, uint64 *elem_count, uint64 *input_embed,
-      uint64 input_stride, uint64 input_distance, uint64 *output_embed,
-      uint64 output_stride, uint64 output_distance, Type type,
-      bool in_place_fft, int batch_count) = 0;
-    
-      /**
-   * @brief Compute the volume of a slice; i.e., the product of dimensions
-   *        among a range of axes.
-   *
-   * @param start_axis The first axis to include in the slice.
-   *
-   * @param end_axis The first axis to exclude from the slice.
-   */
-  inline int count(int start_axis, int end_axis) const {
-    CHECK_LE(start_axis, end_axis);
-    CHECK_GE(start_axis, 0);
-    CHECK_GE(end_axis, 0);
-    CHECK_LE(start_axis, num_axes());
-    CHECK_LE(end_axis, num_axes());
-    int count = 1;
-    for (int i = start_axis; i < end_axis; ++i) {
-      count *= shape(i);
-    }
-    return count;
-  }
-  /**
-   * @brief Compute the volume of a slice spanning from a particular first
-   *        axis to the final axis.
-   *
-   * @param start_axis The first axis to include in the slice.
-   */
-  inline int count(int start_axis) const {
-    return count(start_axis, num_axes());
-  }
-    
-    /**
- * @brief Get a specific filler from the specification given in FillerParameter.
- *
- * Ideally this would be replaced by a factory pattern, but we will leave it
- * this way for now.
- */
-template <typename Dtype>
-Filler<Dtype>* GetFiller(const FillerParameter& param) {
-  const std::string& type = param.type();
-  if (type == 'constant') {
-    return new ConstantFiller<Dtype>(param);
-  } else if (type == 'gaussian') {
-    return new GaussianFiller<Dtype>(param);
-  } else if (type == 'positive_unitball') {
-    return new PositiveUnitballFiller<Dtype>(param);
-  } else if (type == 'uniform') {
-    return new UniformFiller<Dtype>(param);
-  } else if (type == 'xavier') {
-    return new XavierFiller<Dtype>(param);
-  } else if (type == 'msra') {
-    return new MSRAFiller<Dtype>(param);
-  } else if (type == 'bilinear') {
-    return new BilinearFiller<Dtype>(param);
-  } else {
-    CHECK(false) << 'Unknown filler name: ' << param.type();
-  }
-  return (Filler<Dtype>*)(NULL);
-}
-    
-      /**
-   * Caffe's thread local state will be initialized using the current
-   * thread values, e.g. device id, solver index etc. The random seed
-   * is initialized using caffe_rng_rand.
-   */
-  void StartInternalThread();
-    
-      /**
-   * @brief Adjust the shapes of top blobs and internal buffers to accommodate
-   *        the shapes of the bottom blobs.
-   *
-   * @param bottom the input blobs, with the requested input shapes
-   * @param top the top blobs, which should be reshaped as needed
-   *
-   * This method should reshape top blobs as needed according to the shapes
-   * of the bottom (input) blobs, as well as reshaping any internal buffers
-   * and making any other necessary adjustments so that the layer can
-   * accommodate the bottom blobs.
-   */
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) = 0;
-    
-    #endif  // CAFFE_ACCURACY_LAYER_HPP_
+    #endif  // ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
 
     
-      /**
-   * @brief Computes the error gradient w.r.t. the reordered input.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient
-   *        with respect to the outputs
-   *   -# @f$ (M \times ...) @f$:
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to concatenated outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2):
-   *   - @f$ \frac{\partial E}{\partial y} @f$ is de-indexed (summing where
-   *     required) back to the input x_1
-   *   - This layer cannot backprop to x_2, i.e. propagate_down[1] must be
-   *     false.
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    /**
- * @brief Computes @f$ y = x + \log(1 + \exp(-x)) @f$ if @f$ x > 0 @f$;
- *        @f$ y = \log(1 + \exp(x)) @f$ otherwise.
- *
- * @param bottom input Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the inputs @f$ x @f$
- * @param top output Blob vector (length 1)
- *   -# @f$ (N \times C \times H \times W) @f$
- *      the computed outputs @f$
- *      y = \left\{
- *         \begin{array}{ll}
- *            x + \log(1 + \exp(-x)) & \mbox{if } x > 0 \\
- *            \log(1 + \exp(x)) & \mbox{otherwise}
- *         \end{array} \right.
- *      @f$
- */
-template <typename Dtype>
-class BNLLLayer : public NeuronLayer<Dtype> {
- public:
-  explicit BNLLLayer(const LayerParameter& param)
-      : NeuronLayer<Dtype>(param) {}
-    }
+    namespace atom {
     }
     
-      Blob<int> offsets;
-  Blob<int> src_strides_;
-  Blob<int> dest_strides_;
+    int LaunchProgram(const StringVector& relauncher_args,
+                  const StringVector& argv) {
+  base::LaunchOptions options;
+  base::Process process =
+      base::LaunchProcess(ArgvToCommandLineString(argv), options);
+  return process.IsValid() ? 0 : 1;
+}
     
     
-    {}  // namespace caffe
+    {  int result = 0;
+  RenderThread::Get()->Send(new ShellViewHostMsg_AllocateId(
+      routing_id,
+      &result));
+  return scope.Escape(v8::Integer::New(isolate, result));
+}
     
-    void StartCmder(std::wstring  path = L'', bool is_single_mode = false, std::wstring taskName = L'', std::wstring cfgRoot = L'', bool use_user_cfg = true)
-{
-#if USE_TASKBAR_API
-	wchar_t appId[MAX_PATH] = { 0 };
+    static KeyMap keymap = {
+  {'`'    , 'Backquote'},
+  {'\\'   , 'Backslash'},
+  {'['    , 'BracketLeft'},
+  {']'    , 'BracketRight'},
+  {','    , 'Comma'},
+  {'='    , 'Equal'},
+  {'-'    , 'Minus'},
+  {'.'    , 'Period'},
+  {'''    , 'Quote'},
+  {';'    , 'Semicolon'},
+  {'/'    , 'Slash'},
+  {'\n'   , 'Enter'},
+  {'\t'   , 'Tab'},
+  {'UP'   , 'ArrowUp'},
+  {'DOWN' , 'ArrowDown'},
+  {'LEFT' , 'ArrowLeft'},
+  {'RIGHT', 'ArrowRight'},
+  {'ESC'  , 'Escape'},
+  {'MEDIANEXTTRACK', 'MediaTrackNext'},
+  {'MEDIAPREVTRACK', 'MediaTrackPrevious'}
+};
+    
+    
+    {
+} // namespace extensions
 #endif
-	wchar_t exeDir[MAX_PATH] = { 0 };
-	wchar_t icoPath[MAX_PATH] = { 0 };
-	wchar_t cfgPath[MAX_PATH] = { 0 };
-	wchar_t backupCfgPath[MAX_PATH] = { 0 };
-	wchar_t cpuCfgPath[MAX_PATH] = { 0 };
-	wchar_t userCfgPath[MAX_PATH] = { 0 };
-	wchar_t defaultCfgPath[MAX_PATH] = { 0 };
-	wchar_t conEmuPath[MAX_PATH] = { 0 };
-	wchar_t configDirPath[MAX_PATH] = { 0 };
-	wchar_t userConfigDirPath[MAX_PATH] = { 0 };
-	wchar_t userBinDirPath[MAX_PATH] = { 0 };
-	wchar_t userProfiledDirPath[MAX_PATH] = { 0 };
-	wchar_t userProfilePath[MAX_PATH] = { 0 };
-	wchar_t legacyUserProfilePath[MAX_PATH] = { 0 };
-	wchar_t userAliasesPath[MAX_PATH] = { 0 };
-	wchar_t legacyUserAliasesPath[MAX_PATH] = { 0 };
-	wchar_t args[MAX_PATH * 2 + 256] = { 0 };
-	wchar_t userConEmuCfgPath[MAX_PATH] = { 0 };
-    }
-    
-            // determine all leaves and their dependents
-        dependentSet = set<ComputationNodeBasePtr>(inputNodes.begin(), inputNodes.end()); // start with the specified inputs
-        bool iterate = true;
-        // determine all leaves and their dependents
-        // this needs outer loop to handle cyclic dependencies (PastValue) properly
-        while (iterate)
-        {
-            iterate = false;
-            for (let& node : allInputs)
-            {
-                // add parameters that are to be cloned to dependent set
-                if (parameterTreatment != ParameterTreatment::shared && node->Is<IFreezable>())
-                    dependentSet.insert(node);
-                // if at least one input is in the dependent set then this node is, too
-                else
-                    for (let& input : node->GetInputs())
-                        if (dependentSet.find(input) != dependentSet.end() && dependentSet.find(node) == dependentSet.end())
-                        {
-                            dependentSet.insert(node);
-                            iterate = true;
-                        }
-            }
-        }
-    
-        virtual bool InputUsedInComputingInputNodesGradients(size_t /*childIndex*/) const override {
-        return false;
-    }
+
     
     
-    {private:
-    wstring m_propertyName;
+    {  DECLARE_EXTENSION_FUNCTION('nw.Obj.callObjectMethodAsync', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwObjCallObjectMethodAsyncFunction);
 };
     
     
-    {        for (const auto& pair : rootLookupMap)
-        {
-            if (groupMap.find(pair.second) == groupMap.end())
-                InvalidArgument('lookup root should be group key');
-        }
-        m_aliasLookup = rootLookupMap;
-    }
+    {
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace csharp
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+
     
-    
-    {    RegexMatcher  m(this);
-    int32_t r = 0;
-    // Check m's status to make sure all is ok.
-    if (U_SUCCESS(m.fDeferredStatus)) {
-    	r = m.split(input, dest, destCapacity, status);
-    }
-    return r;
-}
-    
-        UnicodeString &displayScripts(UnicodeString &dest) const; // append script names to dest string.
-    ScriptSet & parseScripts(const UnicodeString &scriptsString, UErrorCode &status);  // Replaces ScriptSet contents.
-    
-    void
-SelectFormat::applyPattern(const UnicodeString& newPattern, UErrorCode& status) {
-    if (U_FAILURE(status)) {
-      return;
-    }
-    }
-    
-    U_NAMESPACE_BEGIN
-    
-    int32_t StandardPlural::indexFromString(const char *keyword, UErrorCode &errorCode) {
-    if (U_FAILURE(errorCode)) { return OTHER; }
-    int32_t i = indexOrNegativeFromString(keyword);
-    if (i >= 0) {
-        return i;
+        if (deps_with_extensions.empty()) {
+      printer->Print(
+          '// None of the imports (direct or indirect) defined extensions, so no need to add\n'
+          '// them to this registry.\n');
     } else {
-        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
-        return OTHER;
+      printer->Print(
+          '// Merge in the imports (direct or indirect) that defined extensions.\n');
+      for (std::vector<const FileDescriptor *>::iterator iter =
+               deps_with_extensions.begin();
+           iter != deps_with_extensions.end(); ++iter) {
+        const string root_class_name(FileClassName((*iter)));
+        printer->Print(
+            '[registry addExtensions:[$dependency$ extensionRegistry]];\n',
+            'dependency', root_class_name);
+      }
     }
+    
+    void OneofGenerator::GenerateCaseEnum(io::Printer* printer) {
+  printer->Print(
+      variables_,
+      'typedef GPB_ENUM($enum_name$) {\n');
+  printer->Indent();
+  printer->Print(
+      variables_,
+      '$enum_name$_GPBUnsetOneOfCase = 0,\n');
+  string enum_name = variables_['enum_name'];
+  for (int j = 0; j < descriptor_->field_count(); j++) {
+    const FieldDescriptor* field = descriptor_->field(j);
+    string field_name = FieldNameCapitalized(field);
+    printer->Print(
+        '$enum_name$_$field_name$ = $field_number$,\n',
+        'enum_name', enum_name,
+        'field_name', field_name,
+        'field_number', SimpleItoa(field->number()));
+  }
+  printer->Outdent();
+  printer->Print(
+      '};\n'
+      '\n');
 }
     
+    #else
     
-    {    /**
-     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
-     *
-     * @param keyword for example 'few' or 'other'
-     * @return the index of the plural form corresponding to the keyword
-     */
-    static int32_t indexFromString(const UnicodeString &keyword, UErrorCode &errorCode);
-};
+                    if( stack.size() == 0 )
+                    return false;
+    
+    int main() {
+    }
+    
+    // Recursive
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+    }
+    }
+    
+    
+    {            res[level].push_back(node->val);
+            if(node->left)
+                q.push(make_pair(node->left, level + 1 ));
+            if(node->right)
+                q.push(make_pair(node->right, level + 1 ));
+        }
+    
+            vector<int> res;
+        if(root == NULL)
+            return res;
+    
+    // Recursive
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+    }
+    }
+    
+    namespace CalcEngine
+{
+    class Number
+    {
+    public:
+        Number() noexcept;
+        Number(int32_t sign, int32_t exp, std::vector<uint32_t> const& mantissa) noexcept;
+    }
+    }
+    
+    namespace CalcEngine::RationalMath
+{
+    Rational Frac(Rational const& rat);
+    Rational Integer(Rational const& rat);
+    }
+    
+                property Windows::UI::Xaml::Visibility AppBarVisibility
+            {
+                Windows::UI::Xaml::Visibility get()
+                {
+                    return CalculatorApp::Common::NavCategory::IsCalculatorViewMode(Mode)
+                        ? Windows::UI::Xaml::Visibility::Visible
+                        : Windows::UI::Xaml::Visibility::Collapsed;
+                }
+            }
+    
+    void LiveRegionHost::Announce(NarratorAnnouncement^ announcement)
+{
+    if (m_host == nullptr)
+    {
+        m_host = ref new TextBlock();
+        AutomationProperties::SetLiveSetting(m_host, AutomationLiveSetting::Assertive);
+    }
+    }
