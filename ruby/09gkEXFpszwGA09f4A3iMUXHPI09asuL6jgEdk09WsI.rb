9c3b9bd7f2bc136bc49b39da11203fccdcd365ea
SUNsung <weatherius@gmail.com>
2019-03-20 01:26:00 +0200

    Rewrite [09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb] File in: 2019-03-20 01:26:00

--- ruby/09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb
+++ ruby/09gkEXFpszwGA09f4A3iMUXHPI09asuL6jgEdk09WsI.rb
@@ -1,59 +1,138 @@
 
         
-                if options[:bootstrap]
-          template 'config/initializers/simple_form_bootstrap.rb'
-        elsif options[:foundation]
-          template 'config/initializers/simple_form_foundation.rb'
-        end
-    
-      config.wrappers :horizontal_form, tag: 'div', class: 'row', hint_class: :field_with_hint, error_class: :error, valid_class: :valid do |b|
-    b.use :html5
-    b.use :placeholder
-    b.optional :maxlength
-    b.optional :minlength
-    b.optional :pattern
-    b.optional :min_max
-    b.optional :readonly
-    
-          def simple_fields_for(record_name, record_object = nil, options = {}, &block)
-        options, record_object = record_object, nil if record_object.is_a?(Hash) && record_object.extractable_options?
-        options[:builder] ||= SimpleForm::FormBuilder
-    
-            '#{html_escape(options[:error_prefix])} #{html_escape(text)}'.lstrip.html_safe
+            context '(de)activating users' do
+      it 'does not show deactivation buttons for the current user' do
+        visit admin_users_path
+        expect(page).to have_no_css('a[href='/admin/users/#{users(:jane).id}/deactivate']')
       end
     
-          def label_input(wrapper_options = nil)
-        if options[:label] == false
-          deprecated_component(:input, wrapper_options)
-        else
-          deprecated_component(:label, wrapper_options) + deprecated_component(:input, wrapper_options)
-        end
+        it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
+    end
+  end
+    
+      describe '.seed' do
+    it 'imports a set of agents to get the user going when they are first created' do
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
+    
+          describe '#import' do
+        it 'uses the existing scenario, updating its data' do
+          expect {
+            scenario_import.import(:skip_agents => true)
+            expect(scenario_import.scenario).to eq(existing_scenario)
+          }.not_to change { users(:bob).scenarios.count }
+    
+      describe '#hour_to_schedule_name' do
+    it 'for 0h' do
+      expect(@scheduler.send(:hour_to_schedule_name, 0)).to eq('midnight')
+    end
+    
+      let :agent do
+    Agents::WebsiteAgent.create!(
+      user: users(:bob),
+      name: 'xkcd',
+      options: valid_options,
+      keep_events_for: 2.days
+    )
+  end
+    
+      describe '#helpers' do
+    it 'should return the correct request header' do
+      expect(@checker.send(:request_options)).to eq({:headers => {'aftership-api-key' => '800deeaf-e285-9d62-bc90-j999c1973cc9', 'Content-Type'=>'application/json'}})
+    end
+    
+        it 'should provide the since attribute after the first run' do
+      time = (Time.now-1.minute).iso8601
+      @checker.memory[:last_event] = time
+      @checker.save
+      expect(@checker.reload.send(:query_parameters)).to eq({:query => {:since => time}})
+    end
+  end
+    
+      describe '#receive' do
+    it 'sends a message' do
+      stub(HTTParty).post { {'id' => 1, 'message' => 'blah', 'title' => 'blah','source_name' => 'Custom Notification'} }
+      @checker.receive([@event])
+    end
+    
+        alias log puts
+    
+      # Use a different cache store in production.
+  # config.cache_store = :mem_cache_store
+    
+      def down
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id
+  end
+    
+      def up_down(change)
+    change.up do
+      Mention.update_all(mentions_container_type: 'Post')
+      change_column :mentions, :mentions_container_type, :string, null: false
+      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
+    end
+    
+    When /^I put in my password in '([^']*)'$/ do |field|
+ step %(I fill in '#{field}' with '#{@me.password}')
+end
+    
+      def navigate_to(page_name)
+    path = path_to(page_name)
+    if path.is_a?(Hash)
+      visit(path[:path])
+      await_elem = path[:special_elem]
+      find(await_elem.delete(:selector), await_elem)
+    else
+      visit(path)
+    end
+  end
+    
+          sign_in alice, scope: :user
+    end
+    
+        context 'on my own post' do
+      before do
+        aspect_to_post = alice.aspects.where(:name => 'generic').first
+        @post = alice.post :status_message, :text => 'something', :to => aspect_to_post
       end
     
-            def i18n_scope
-          SimpleForm.i18n_scope
-        end
-      end
+    package = FPM::Package::Dir.new
     
-          def attribute_validators
-        object.class.validators_on(attribute_name)
-      end
+      TAR_CHUNK_SIZE = 512
+  TAR_TYPEFLAG_OFFSET = 156
+  TAR_NAME_OFFSET_START = 0
+  TAR_NAME_OFFSET_END = 99
+  TAR_LENGTH_OFFSET_START = 124
+  TAR_LENGTH_OFFSET_END = 135
+  TAR_CHECKSUM_OFFSET_START = 148
+  TAR_CHECKSUM_OFFSET_END = 155
+  TAR_MAGIC_START = 257
+  TAR_MAGIC_END = 264
+  TAR_UID_START = 108
+  TAR_UID_END = 115
+  TAR_GID_START = 116
+  TAR_GID_END = 123
+  TAR_UNAME_START = 265
+  TAR_UNAME_END = 296
+  TAR_GNAME_START = 297
+  TAR_GNAME_END = 328
+  TAR_MAJOR_START = 329
+  TAR_MAJOR_END = 336
+  TAR_MINOR_START = 337
+  TAR_MINOR_END = 344
     
-            if collection_translated || collection_classes.include?(Array)
-          { label: :first, value: :second }
-        elsif collection_includes_basic_objects?(collection_classes)
-          { label: :to_s, value: :to_s }
-        else
-          detect_method_from_class(collection_classes)
-        end
-      end
+      option '--channel', 'CHANNEL_URL',
+    'The pear channel url to use instead of the default.'
     
-      context 'raise-out environment' do
-    before do
-      @rack_env = ENV['RACK_ENV']
-      ENV['RACK_ENV'] = 'test'
+        args = [ '-B', build_path('build-info'), '-c', build_path('comment'), '-d', build_path('description'), '-f', build_path('packlist'), '-I', '/opt/local', '-p', staging_path,  '-U', '#{cwd}/#{name}-#{self.version}-#{iteration}.tgz' ]
+    safesystem('pkg_create', *args)
     
-          it 'respects SCRIPT_NAME (a.k.a. BaseURI)' do
-        response = strategy.call(make_env('/auth/test', 'SCRIPT_NAME' => '/sub_uri'))
-        expect(response[1]['Location']).to eq('/sub_uri/auth/test/callback')
-      end
+        libs = [ 'install.sh', 'install-path.sh', 'generate-cleanup.sh' ]
+    libs.each do |file|
+      base = staging_path(File.join(attributes[:prefix]))
+      File.write(File.join(base, file), template(File.join('pleaserun', file)).result(binding))
+      File.chmod(0755, File.join(base, file))
+    end
