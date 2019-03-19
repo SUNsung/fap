e7a8c05ffa6d0f3aad9deb0b8d4e660fd7c369d9
SUNsung <weatherius@gmail.com>
2019-03-19 20:36:13 +0200

    Rewrite [570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb] File in: 2019-03-19 20:36:11

--- ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
+++ ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
@@ -1,134 +1,94 @@
 
         
-            alias_method :blank?, :empty?
-    
-        def root?
-      path == 'index'
-    end
-    
-        def error?
-      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
-    end
-    
-        def request_all(url, &block)
-      raise NotImplementedError
-    end
-    
-          # Forgets the given resource by deleting a cookie
-      def forget_me(resource)
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.forget_me!
-        cookies.delete(remember_key(resource, scope), forget_cookie_values(resource))
+                expect(result).to eq('appledoc --project-name \'Project Name\' --project-company \'Company\' --docset-fallback-url \'http://docset-fallback-url.com\' --exit-threshold \'2\' input/dir')
       end
     
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-                if class_mod.respond_to?(:available_configs)
-              available_configs = class_mod.available_configs
-              available_configs.each do |config|
-                next unless options.key?(config)
-                send(:'#{config}=', options.delete(config))
-              end
-            end
-          end
-    
-          # Checks whether the user session has expired based on configured time.
-      def timedout?(last_access)
-        !timeout_in.nil? && last_access && last_access <= timeout_in.ago
-      end
-    
-        if self.junk_headers
-      while str.length < 4096
-        if self.fold
-          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}:\r\n\t#{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
-        else
-          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}: #{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
+              expect(result).to eq('SCRIPT_INPUT_FILE_COUNT=3 SCRIPT_INPUT_FILE_0=AppDelegate.swift SCRIPT_INPUT_FILE_1=path/to/project/src/Model.swift SCRIPT_INPUT_FILE_2=path/to/project/test/Test.swift swiftlint lint --use-script-input-files')
         end
       end
+    
+        # Returns an updated value type (if necessary)
+    def auto_convert_value(value)
+      return nil if value.nil?
+    
+          it 'should shell escape keychain names when checking for installation' do
+        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
+    
+            def sidekiq_worker_class
+          ImportNoteWorker
+        end
+    
+          def call(env)
+        transaction, retval = Transaction.run do
+          @app.call(env)
+        end
+    
+            a_length = a_split.length
+        b_length = b_split.length
+    
+        def inheritable_copy
+      self.class.new @filters
     end
     
-        # If a procedure was passed, mount the resource with it.
-    if (opts['Proc'])
-      mount(name, Handler::Proc, false, opts['Proc'], opts['VirtualDirectory'])
-    else
-      raise ArgumentError, 'You must specify a procedure.'
-    end
+      def pin_at(version)
+    HOMEBREW_PINNED_KEGS.mkpath
+    version_path = @f.rack/version
+    path.make_relative_symlink(version_path) unless pinned? || !version_path.exist?
   end
     
+          # Checks whether this case statement has an `else` branch.
       #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
-    
-                res = checksum + encrypted
-            res
-          end
-        end
+      # @return [Boolean] whether the `case` statement has an `else` branch
+      def else?
+        loc.else
       end
     end
   end
 end
+
     
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a KRB_AP_REQ definition, containing the Kerberos protocol version number,
-        # the message type KRB_AP_REQ, an options field to indicate any options in use, and the ticket and authenticator
-        # themselves
-        class ApReq < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of the protocol message
-          attr_accessor :msg_type
-          # @!attribute options
-          #   @return [Integer] request options, affects processing
-          attr_accessor :options
-          # @!attribute ticket
-          #   @return [Rex::Proto::Kerberos::Model::Ticket] The ticket authenticating the client to the server
-          attr_accessor :ticket
-          # @!attribute authenticator
-          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] This contains the authenticator, which includes the
-          #   client's choice of a subkey
-          attr_accessor :authenticator
+            def delimiter_delta
+          return 0 if first.same_line?(second)
+          return 0 if first.delimiter != second.delimiter
     
-              # Encodes the data
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_data(data)
-            OpenSSL::ASN1::OctetString.new(data)
-          end
-        end
-      end
+          dir = if File.directory?(file_or_dir)
+              file_or_dir
+            else
+              File.dirname(file_or_dir)
+            end
+      @path_cache[dir] ||= ConfigLoader.configuration_file_for(dir)
+      path = @path_cache[dir]
+      @object_cache[path] ||= begin
+                                print 'For #{dir}: ' if ConfigLoader.debug?
+                                ConfigLoader.configuration_from_file(path)
+                              end
     end
   end
 end
+
     
-        def initialize(plugins_to_package, target)
-      @plugins_to_package = Array(plugins_to_package)
-      @target = target
+    get '/' do
+  stats = Sidekiq::Stats.new
+  @failed = stats.failed
+  @processed = stats.processed
+  @messages = $redis.lrange('sinkiq-example-messages', 0, -1)
+  erb :index
+end
     
-          # Try to add the gems to the current gemfile and lock file, if successful
-      # both of them will be updated. This injector is similar to Bundler's own injector class
-      # minus the support for additionals source and doing local resolution only.
-      ::Bundler::LogstashInjector.inject!(pack)
+          # Pop out a single job and perform it
+      def perform_one
+        raise(EmptyQueueError, 'perform_one called with empty job queue') if jobs.empty?
+        next_job = jobs.first
+        Queues.delete_for(next_job['jid'], queue, self.to_s)
+        process_job(next_job)
+      end
     
-          options = {:debug => ENV['LS_QA_DEBUG']}
-      puts 'Destroying #{machines}'
-      LogStash::VagrantHelpers.destroy(machines, options)
-      puts 'Bootstrapping #{machines}'
-      LogStash::VagrantHelpers.bootstrap(machines, options)
+            run WebApplication.new(klass)
+      end
+    end
+  end
+    
+        def redirect(location)
+      throw :halt, [302, { 'Location' => '#{request.base_url}#{location}' }, []]
     end
