016b0cf18c0c5154766fcaa5211e23e69c97445c
SUNsung <weatherius@gmail.com>
2019-03-20 10:01:12 +0200

    Rewrite [275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb] File in: 2019-03-20 10:01:11

--- ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
+++ ruby/275xafRmvMevU27inhwmvKQcQ27e3bX79PcVQg27gyz.rb
@@ -1,52 +1,40 @@
 
         
-              def is_create_page
-        false
+              def parse_uri(location)
+        location && URI.parse(location)
+      rescue URI::InvalidURIError
+        nil
       end
     
-    desc 'Build and install'
-task :install => :build do
-  sh 'gem install --local --no-ri --no-rdoc pkg/#{name}-#{version}.gem'
+    # Each time a record is set we check whether its session has already timed out
+# or not, based on last request time. If so, the record is logged out and
+# redirected to the sign in page. Also, each time the request comes and the
+# record is set, we set the last request time inside its scoped session to
+# verify timeout in the following request.
+Warden::Manager.after_set_user do |record, warden, options|
+  scope = options[:scope]
+  env   = warden.request.env
+    
+        def default_used_helpers(options)
+      singularizer = lambda { |s| s.to_s.singularize.to_sym }
+    
+            Devise::Models.config(self, :reset_password_keys, :reset_password_within, :sign_in_after_reset_password)
+      end
+    end
+  end
 end
+
     
-        COP_NAME_PATTERN = '([A-Z]\w+/)?(?:[A-Z]\w+)'.freeze
-    COP_NAMES_PATTERN = '(?:#{COP_NAME_PATTERN} , )*#{COP_NAME_PATTERN}'.freeze
-    COPS_PATTERN = '(all|#{COP_NAMES_PATTERN})'.freeze
+          # Checks whether the user session has expired based on configured time.
+      def timedout?(last_access)
+        !timeout_in.nil? && last_access && last_access <= timeout_in.ago
+      end
     
-    # Create two output packages!
-output_packages = []
-output_packages << pleaserun.convert(FPM::Package::RPM)
-output_packages << pleaserun.convert(FPM::Package::Deb)
+      def prev_page
+    account_outbox_url(@account, page: true, min_id: @statuses.first.id) unless @statuses.empty?
+  end
     
-        # The only way to get npm to respect the 'prefix' setting appears to
-    # be to set the --global flag.
-    #install_args << '--global'
-    install_args += npm_flags
+            log_action :change_email, @user
     
-      option '--bin-dir', 'BIN_DIR',
-    'Directory to put binaries in'
-    
-      private
-  def input(command)
-    platforms = [
-      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
-      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
-      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
-      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
-      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
-    ]
-    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
-                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
-                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
-                             'log_directory', 'log_file_stderr', 'log_file_stdout']
-    
-      def build!(params)
-    # TODO(sissel): Support these somehow, perhaps with execs and files.
-    self.scripts.each do |name, path|
-      case name
-        when 'pre-install'
-        when 'post-install'
-        when 'pre-uninstall'
-        when 'post-uninstall'
-      end # case name
-    end # self.scripts.each
+        # Mobile devices do not support regular notifications, so we enable push notifications by default
+    alerts_enabled = active_session.detection.device.mobile? || active_session.detection.device.tablet?
