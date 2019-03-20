8b0fd9a66f5d21052a5c0e1837a95990cc09feb6
SUNsung <weatherius@gmail.com>
2019-03-20 22:31:53 +0200

    Rewrite [479NX67xNU806471jgr1loTtI47tb5KEp1DvYM47vps.py] File in: 2019-03-20 22:31:52

--- pyton/479NX67xNU806471jgr1loTtI47tb5KEp1DvYM47vps.py
+++ pyton/479NX67xNU806471jgr1loTtI47tb5KEp1DvYM47vps.py
@@ -1,136 +1,71 @@
 
         
-            with open('README.md', 'w+') as sorted_file:
-        # Then all of the blocks are sorted individually
-        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
-        # And the result is written back to README.md
-        sorted_file.write(''.join(blocks))
+        
+def add_error(line_num, message):
+    '''adds an error to the dynamic error list'''
+    err = '(L{:03d}) {}'.format(line_num + 1, message)
+    errors.append(err)
     
-    from flask import Flask
-    
-    from flaskr.auth import login_required
-from flaskr.db import get_db
-    
-    
-def test_init_db_command(runner, monkeypatch):
-    class Recorder(object):
-        called = False
-    
-            if not current_app.config['JSON_AS_ASCII']:
-            kwargs.setdefault('ensure_ascii', False)
+            exist_ca_sha1 = get_exist_ca_sha1()
+        if exist_ca_sha1 == ca_hash:
+            xlog.info('GoAgent CA exist')
+            return
     
     
-class TagUUID(JSONTag):
-    __slots__ = ()
-    key = ' u'
-    
-        while current:
-        if any(handler.level <= level for handler in current.handlers):
-            return True
-    
-            if (
-            not kwargs and len(args) == 1
-            and isinstance(args[0], (EnvironBuilder, dict))
-        ):
-            environ = self.environ_base.copy()
-    
-    
-class RangeMinValueValidator(MinValueValidator):
-    def compare(self, a, b):
-        return a.lower is None or a.lower < b
-    message = _('Ensure that this range is completely greater than or equal to %(limit_value)s.')
-
-    
-    KEY_PREFIX = 'django.contrib.sessions.cached_db'
-    
-        def exists(self, session_key):
-        return self.model.objects.filter(session_key=session_key).exists()
-    
-    from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
-    
-        if args.plot is not None:
-        max_val = getattr(args, args.plot)
-        if args.plot in ('classes', 'samples'):
-            min_val = 2
-        else:
-            min_val = 0
-        steps = np.linspace(min_val, max_val, num=args.n_steps + 1)[1:]
-        if args.plot in ('classes', 'samples'):
-            steps = np.unique(np.round(steps).astype(int))
-        setattr(args, args.plot, steps)
-    
-        X = get_data(N, DD, dataset)
-    
-                tstart = time()
-            clf.fit(X_train, y_train)
-            asgd_results[i, j, 0] = mean_squared_error(clf.predict(X_test),
-                                                       y_test)
-            asgd_results[i, j, 1] = time() - tstart
-    
-    
-def _linkcode_resolve(domain, info, package, url_fmt, revision):
-    '''Determine a link to online source for a class/method/function
-    
-    
-def setup(app):
-    # Format template for issues URI
-    # e.g. 'https://github.com/sloria/marshmallow/issues/{issue}
-    app.add_config_value('issues_uri', default=None, rebuild='html')
-    # Shortcut for GitHub, e.g. 'sloria/marshmallow'
-    app.add_config_value('issues_github_path', default=None, rebuild='html')
-    # Format template for user profile URI
-    # e.g. 'https://github.com/{user}'
-    app.add_config_value('issues_user_uri', default=None, rebuild='html')
-    app.add_role('issue', issue_role)
-    app.add_role('user', user_role)
-
-    
-        input_file = open(os.path.join(exercise_dir, f))
-    output_file = open(os.path.join(skeleton_dir, f), 'w')
-    
-    # Learn a frontier for outlier detection with several classifiers
-xx1, yy1 = np.meshgrid(np.linspace(-8, 28, 500), np.linspace(3, 40, 500))
-xx2, yy2 = np.meshgrid(np.linspace(3, 10, 500), np.linspace(-5, 45, 500))
-for i, (clf_name, clf) in enumerate(classifiers.items()):
-    plt.figure(1)
-    clf.fit(X1)
-    Z1 = clf.decision_function(np.c_[xx1.ravel(), yy1.ravel()])
-    Z1 = Z1.reshape(xx1.shape)
-    legend1[clf_name] = plt.contour(
-        xx1, yy1, Z1, levels=[0], linewidths=2, colors=colors[i])
-    plt.figure(2)
-    clf.fit(X2)
-    Z2 = clf.decision_function(np.c_[xx2.ravel(), yy2.ravel()])
-    Z2 = Z2.reshape(xx2.shape)
-    legend2[clf_name] = plt.contour(
-        xx2, yy2, Z2, levels=[0], linewidths=2, colors=colors[i])
-    
-        n_dir : int
-        number of angles at which projections are acquired.
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-        if os.path.isdir(image_to_check):
-        if cpus == 1:
-            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
-        else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
+def is_ok(ip=None):
+    if not ip:
+        return IPv4.is_ok() or IPv6.is_ok()
+    elif '.' in ip:
+        return IPv4.is_ok()
     else:
-        test_image(image_to_check, model)
+        return IPv6.is_ok()
     
-    # PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
+            self.decisionNumber = decisionNumber
     
-        def test_fd_command_line_interface_options(self):
-        target_string = 'Show this message and exit.'
-        runner = CliRunner()
-        help_result = runner.invoke(face_detection_cli.main, ['--help'])
-        self.assertEqual(help_result.exit_code, 0)
-        self.assertTrue(target_string in help_result.output)
+            for i, op in enumerate(rewrites[:before]):
+            if op is None:
+                # ignore deleted
+                continue
+            if op.__class__ == kind:
+                yield i, op
+    
+        def __init__(self, hass, filename, add_timestamp):
+        '''Initialize the service.'''
+        self.filepath = os.path.join(hass.config.config_dir, filename)
+        self.add_timestamp = add_timestamp
+    
+        def __init__(self, hass, host, port):
+        '''Initialize the service.'''
+        self._hass = hass
+        self._host = host
+        self._port = port
+    
+    DEPENDENCIES = ['mycroft']
+    
+            title = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
+    
+    
+class DemoProvider(Provider):
+    '''Demo speech API provider.'''
+    
+        def __init__(self, unpacked, extra):
+        self.unpacked = unpacked
+        self.extra = extra
+    
+    
+def testSignedInt():
+    check(b'\x99\xd0\x00\xd0\x80\xd0\xff\xd1\x00\x00\xd1\x80\x00'
+          b'\xd1\xff\xff\xd2\x00\x00\x00\x00\xd2\x80\x00\x00\x00'
+          b'\xd2\xff\xff\xff\xff', (0,
+                                    -128,
+                                    -1,
+                                    0,
+                                    -32768,
+                                    -1,
+                                    0,
+                                    -2147483648,
+                                    -1, ))
+    
+    plt.figtext(
+    0.05, 0.2, r'$y_{it} = \beta^{\prime} x_{it} + \mu_{i} + \epsilon_{it}$',
+    size=16, color='#5a89a4')
