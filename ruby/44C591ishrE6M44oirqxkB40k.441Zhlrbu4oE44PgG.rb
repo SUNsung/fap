819df77a9b4a7218f3f7529cd13290bd08ae5297
SUNsung <weatherius@gmail.com>
2019-03-20 18:48:13 +0200

    Rewrite [44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb] File in: 2019-03-20 18:48:12

--- ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
+++ ruby/44C591ishrE6M44oirqxkB40k.441Zhlrbu4oE44PgG.rb
@@ -1,76 +1,144 @@
 
         
-                result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            grouping: '#{grouping}',
-            build_number: #{specified_build_number},
-          })
-        end').runner.execute(:test)
-    
-            expect(result).to eq('appledoc --project-name \'Project Name\' --project-company \'Company\' --exit-threshold \'2\' #{input_dir.shellescape}')
-      end
-    
-            context 'when command is archive' do
-          it 'adds one framework' do
-            result = Fastlane::FastFile.new.parse('lane :test do
-                carthage(command: '#{command}', frameworks: ['myframework'])
-              end').runner.execute(:test)
-            expect(result).to eq('carthage archive myframework')
-          end
-    
-          it 'handles the exclude_dirs parameter with a single element correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: ['.bundle'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --exclude-dir .bundle')
-      end
-    
-        describe 'shell escaping' do
-      let(:keychain_name) { 'keychain with spaces.keychain' }
-      let(:shell_escaped_name) { keychain_name.shellescape }
-      let(:name_regex) { Regexp.new(Regexp.escape(shell_escaped_name)) }
-    
-    module Docs
-  class Filter < ::HTML::Pipeline::Filter
-    def css(*args)
-      doc.css(*args)
-    end
-    
-        alias_method :insert_before, :insert
-    
-        def error?
-      code == 0 || code != 404 && code != 403 && code >= 400 && code <= 599
-    end
-    
-            # Remove root-level <div>
-        while div = at_css('h1 + div')
-          div.before(div.children)
-          div.remove
+                # This is what is called on the class to actually execute it. Any
+        # subclasses should implement this method and do any option parsing
+        # and validation here.
+        def execute
         end
     
-      # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
+            def initialize(env, config)
+          @env    = env
+          @config = config
+        end
+    
+            log_action :change_email, @user
+    
+    module Admin
+  class ReportNotesController < BaseController
+    before_action :set_report_note, only: [:destroy]
+    
+      def payload
+    @_payload ||= request.body.read
   end
     
-          def remove_domain_from_uri(uri)
-        [uri.path.sub(/\A\/+/, '/'), uri.query].compact.join('?')
-      end
+      before_action :require_user!
     
-          module ClassMethods
-        # Create the cookie key using the record id and remember_token
-        def serialize_into_cookie(record)
-          [record.to_key, record.rememberable_value, Time.now.utc.to_f.to_s]
+      def compatible_locale
+    http_accept_language.compatible_language_from(available_locales)
+  end
+    
+    ###
+#
+# Runtime extension of the HTTP clients that connect to the server.
+#
+###
+module ServerClient
+    
+    
+end
+end
+end
+
+    
+                res = checksum + encrypted
+            res
+          end
         end
-    
-    require 'devise/hooks/timeoutable'
-    
-          # Returns the operator for the `kwsplat` as a string.
-      #
-      # @return [String] the double splat operator
-      def operator
-        DOUBLE_SPLAT
       end
+    end
+  end
+end
+    
+              # Rex::Proto::Kerberos::Model::AuthorizationData decoding isn't supported
+          #
+          # @raise [NotImplementedError]
+          def decode(input)
+            raise ::NotImplementedError, 'Authorization Data decoding not supported'
+          end
+    
+              def initialize(options = {})
+            self.class.attributes.each do |attr|
+              if options.has_key?(attr)
+                m = (attr.to_s + '=').to_sym
+                self.send(m, options[attr])
+              end
+            end
+          end
+    
+      # Generate digests for assets URLs.
+  config.assets.digest = true
+    
+    ::Bundler.with_friendly_errors do
+  ::Bundler::CLI.start(ARGV, :debug => true)
+end
+
+    
+            if specs.size > 0
+          specs
+        else
+          raise LogStash::PluginManager::PluginNotFoundError, 'Cannot find plugins matching: `#{plugin_pattern}`'
+        end
+      end.flatten
+    end
+    
+          def get_installer_for(plugin_name)
+        uri = pack_uri(plugin_name)
+    
+      # We compare the before the update and after the update
+  def display_updated_plugins(previous_gem_specs_map)
+    update_count = 0
+    find_latest_gem_specs.values.each do |spec|
+      name = spec.name.downcase
+      if previous_gem_specs_map.has_key?(name)
+        if spec.version != previous_gem_specs_map[name].version
+          puts('Updated #{spec.name} #{previous_gem_specs_map[name].version.to_s} to #{spec.version.to_s}')
+          update_count += 1
+        end
+      else
+        puts('Installed #{spec.name} #{spec.version.to_s}')
+        update_count += 1
+      end
+    end
+    
+      describe 'on #{logstash.hostname}' do
+    context 'with a direct internet connection' do
+      context 'when the plugin exist' do
+        context 'from a local `.GEM` file' do
+          let(:gem_name) { 'logstash-filter-qatest-0.1.1.gem' }
+          let(:gem_path_on_vagrant) { '/tmp/#{gem_name}' }
+          before(:each) do
+            logstash.download('https://rubygems.org/gems/#{gem_name}', gem_path_on_vagrant)
+          end
+    
+    # usage rake new_post[my-new-post] or rake new_post['my new post'] or rake new_post (defaults to 'new-post')
+desc 'Begin a new post in #{source_dir}/#{posts_dir}'
+task :new_post, :title do |t, args|
+  if args.title
+    title = args.title
+  else
+    title = get_stdin('Enter a title for your post: ')
+  end
+  raise '### You haven't set anything up yet. First run `rake install` to set up an Octopress theme.' unless File.directory?(source_dir)
+  mkdir_p '#{source_dir}/#{posts_dir}'
+  filename = '#{source_dir}/#{posts_dir}/#{Time.now.strftime('%Y-%m-%d')}-#{title.to_url}.#{new_post_ext}'
+  if File.exist?(filename)
+    abort('rake aborted!') if ask('#{filename} already exists. Do you want to overwrite?', ['y', 'n']) == 'n'
+  end
+  puts 'Creating new post: #{filename}'
+  open(filename, 'w') do |post|
+    post.puts '---'
+    post.puts 'layout: post'
+    post.puts 'title: \'#{title.gsub(/&/,'&amp;')}\''
+    post.puts 'date: #{Time.now.strftime('%Y-%m-%d %H:%M:%S %z')}'
+    post.puts 'comments: true'
+    post.puts 'categories: '
+    post.puts '---'
+  end
+end
+    
+    end
+    
+        def render(context)
+      code_dir = (context.registers[:site].config['code_dir'].sub(/^\//,'') || 'downloads/code')
+      code_path = (Pathname.new(context.registers[:site].source) + code_dir).expand_path
+      file = code_path + @file
