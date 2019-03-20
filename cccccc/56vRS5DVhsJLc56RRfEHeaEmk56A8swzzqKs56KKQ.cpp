8fdb78a269650340c4c4c91212869118d24b299f
SUNsung <weatherius@gmail.com>
2019-03-20 15:22:06 +0200

    Rewrite [56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp] File in: 2019-03-20 15:22:05

--- cccccc/56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp
+++ cccccc/56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp
@@ -1,283 +1,129 @@
 
         
-        
-    { private:
-  RowBlock<IndexType> out_;
-  std::unique_ptr<Parser<IndexType> > parser_;
-  uint32_t num_col_;
-  std::vector<size_t> offset_;
-  std::vector<IndexType> dense_index_;
-  std::vector<xgboost::bst_float> dense_value_;
-};
+        std::string DescriptorFileName(const std::string& dbname, uint64_t number) {
+  assert(number > 0);
+  char buf[100];
+  snprintf(buf, sizeof(buf), '/MANIFEST-%06llu',
+           static_cast<unsigned long long>(number));
+  return dbname + buf;
+}
     
-    SparsePageWriter::SparsePageWriter(
-    const std::vector<std::string>& name_shards,
-    const std::vector<std::string>& format_shards,
-    size_t extra_buffer_capacity)
-    : num_free_buffer_(extra_buffer_capacity + name_shards.size()),
-      clock_ptr_(0),
-      workers_(name_shards.size()),
-      qworkers_(name_shards.size()) {
-  CHECK_EQ(name_shards.size(), format_shards.size());
-  // start writer threads
-  for (size_t i = 0; i < name_shards.size(); ++i) {
-    std::string name_shard = name_shards[i];
-    std::string format_shard = format_shards[i];
-    auto* wqueue = &qworkers_[i];
-    workers_[i].reset(new std::thread(
-        [this, name_shard, format_shard, wqueue] () {
-          std::unique_ptr<dmlc::Stream> fo(
-              dmlc::Stream::Create(name_shard.c_str(), 'w'));
-          std::unique_ptr<SparsePageFormat> fmt(
-              SparsePageFormat::Create(format_shard));
-          fo->Write(format_shard);
-          std::shared_ptr<SparsePage> page;
-          while (wqueue->Pop(&page)) {
-            if (page == nullptr) break;
-            fmt->Write(*page, fo.get());
-            qrecycle_.Push(std::move(page));
-          }
-          fo.reset(nullptr);
-          LOG(CONSOLE) << 'SparsePage::Writer Finished writing to ' << name_shard;
-        }));
+    
+    {  // Errors
+  static const char* errors[] = {
+    '',
+    'foo',
+    'foo-dx-100.log',
+    '.log',
+    '',
+    'manifest',
+    'CURREN',
+    'CURRENTX',
+    'MANIFES',
+    'MANIFEST',
+    'MANIFEST-',
+    'XMANIFEST-3',
+    'MANIFEST-3x',
+    'LOC',
+    'LOCKx',
+    'LO',
+    'LOGx',
+    '18446744073709551616.log',
+    '184467440737095516150.log',
+    '100',
+    '100.',
+    '100.lop'
+  };
+  for (int i = 0; i < sizeof(errors) / sizeof(errors[0]); i++) {
+    std::string f = errors[i];
+    ASSERT_TRUE(!ParseFileName(f, &number, &type)) << f;
   }
 }
     
-    #endif  // XGBOOST_OBJECTIVE_REGRESSION_LOSS_H_
-
-    
-    
-    {    // loop over all rows and fill column entries
-    // num_nonzeros[fid] = how many nonzeros have this feature accumulated so far?
-    std::vector<size_t> num_nonzeros;
-    num_nonzeros.resize(nfeature);
-    std::fill(num_nonzeros.begin(), num_nonzeros.end(), 0);
-    for (size_t rid = 0; rid < nrow; ++rid) {
-      const size_t ibegin = gmat.row_ptr[rid];
-      const size_t iend = gmat.row_ptr[rid + 1];
-      size_t fid = 0;
-      for (size_t i = ibegin; i < iend; ++i) {
-        const uint32_t bin_id = gmat.index[i];
-        while (bin_id >= gmat.cut.row_ptr[fid + 1]) {
-          ++fid;
-        }
-        if (type_[fid] == kDenseColumn) {
-          uint32_t* begin = &index_[boundary_[fid].index_begin];
-          begin[rid] = bin_id - index_base_[fid];
+          case kMiddleType:
+        if (!in_fragmented_record) {
+          ReportCorruption(fragment.size(),
+                           'missing start of fragmented record(1)');
         } else {
-          uint32_t* begin = &index_[boundary_[fid].index_begin];
-          begin[num_nonzeros[fid]] = bin_id - index_base_[fid];
-          row_ind_[boundary_[fid].row_ind_begin + num_nonzeros[fid]] = rid;
-          ++num_nonzeros[fid];
+          scratch->append(fragment.data(), fragment.size());
         }
-      }
-    }
-  }
-    
-    namespace xgboost {
-/*!
- * \brief interface of gradient boosting model.
- */
-class GradientBooster {
- public:
-  /*! \brief virtual destructor */
-  virtual ~GradientBooster() = default;
-  /*!
-   * \brief set configuration from pair iterators.
-   * \param begin The beginning iterator.
-   * \param end The end iterator.
-   * \tparam PairIter iterator<std::pair<std::string, std::string> >
-   */
-  template<typename PairIter>
-  inline void Configure(PairIter begin, PairIter end);
-  /*!
-   * \brief Set the configuration of gradient boosting.
-   *  User must call configure once before InitModel and Training.
-   *
-   * \param cfg configurations on both training and model parameters.
-   */
-  virtual void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) = 0;
-  /*!
-   * \brief load model from stream
-   * \param fi input stream.
-   */
-  virtual void Load(dmlc::Stream* fi) = 0;
-  /*!
-   * \brief save model to stream.
-   * \param fo output stream
-   */
-  virtual void Save(dmlc::Stream* fo) const = 0;
-  /*!
-   * \brief whether the model allow lazy checkpoint
-   * return true if model is only updated in DoBoost
-   * after all Allreduce calls
-   */
-  virtual bool AllowLazyCheckPoint() const {
-    return false;
-  }
-  /*!
-   * \brief perform update to the model(boosting)
-   * \param p_fmat feature matrix that provide access to features
-   * \param in_gpair address of the gradient pair statistics of the data
-   * \param obj The objective function, optional, can be nullptr when use customized version
-   * the booster may change content of gpair
-   */
-  virtual void DoBoost(DMatrix* p_fmat,
-                       HostDeviceVector<GradientPair>* in_gpair,
-                       ObjFunction* obj = nullptr) = 0;
-    }
-    }
-    
-    
-    {  /*!
-   * \brief transform prediction values, this is only called when Eval is called,
-   *  usually it redirect to PredTransform
-   * \param io_preds prediction values, saves to this vector as well
-   */
-  virtual void EvalTransform(HostDeviceVector<bst_float> *io_preds) {
-    this->PredTransform(io_preds);
-  }
-  /*!
-   * \brief transform probability value back to margin
-   * this is used to transform user-set base_score back to margin
-   * used by gradient boosting
-   * \return transformed value
-   */
-  virtual bst_float ProbToMargin(bst_float base_score) const {
-    return base_score;
-  }
-  /*!
-   * \brief Create an objective function according to name.
-   * \param name Name of the objective.
-   */
-  static ObjFunction* Create(const std::string& name);
-};
-    
-    template<typename DType>
-inline void CompressArray<DType>::Write(dmlc::Stream* fo) {
-  encoded_chunks_.clear();
-  encoded_chunks_.push_back(0);
-  for (size_t i = 0; i < out_buffer_.size(); ++i) {
-    encoded_chunks_.push_back(encoded_chunks_.back() + out_buffer_[i].length());
-  }
-  fo->Write(raw_chunks_);
-  fo->Write(encoded_chunks_);
-  for (const std::string& buf : out_buffer_) {
-    fo->Write(dmlc::BeginPtr(buf), buf.length());
-  }
-}
-    
-    
-    {};
-    
-    void swap(InternalExtensionInfo &a, InternalExtensionInfo &b) {
-  using ::std::swap;
-  swap(a.name, b.name);
-  swap(a.version, b.version);
-  swap(a.sdk_version, b.sdk_version);
-  swap(a.min_sdk_version, b.min_sdk_version);
-  swap(a.__isset, b.__isset);
-}
+        break;
     
     
     {
-    {  ASSERT_EQ(kEnrollHostDetails.count('osquery_info'), 1U);
-  auto osquery_info = SQL::selectAllFrom('osquery_info');
-  ASSERT_EQ(osquery_info.size(), 1U);
-  ASSERT_EQ(osquery_info[0].count('uuid'), 1U);
-  ASSERT_TRUE(obj.HasMember('host_details'));
-  ASSERT_TRUE(obj['host_details'].HasMember('osquery_info'));
-  ASSERT_TRUE(obj['host_details']['osquery_info'].HasMember('uuid'));
-  ASSERT_TRUE(obj['host_details']['osquery_info']['uuid'].IsString());
-  value = obj['host_details']['osquery_info']['uuid'].GetString();
-  EXPECT_EQ(osquery_info[0]['uuid'], value);
-}
-}
-
+    {}  // namespace log
+}  // namespace leveldb
     
+    namespace leveldb {
+    }
     
-    {
-    {} // namespace events
-} // namespace osquery
-
+      std::string comparator_;
+  uint64_t log_number_;
+  uint64_t prev_log_number_;
+  uint64_t next_file_number_;
+  SequenceNumber last_sequence_;
+  bool has_comparator_;
+  bool has_log_number_;
+  bool has_prev_log_number_;
+  bool has_next_file_number_;
+  bool has_last_sequence_;
     
-    TEST_F(PerfOutputTests, assigning_constructor) {
-  auto buf = std::array<char, 8>{};
-  auto from_obj = ebpf::PerfOutput<TestMessage>{};
-  from_obj.size_ = buf.size();
-  from_obj.fd_ = 42;
-  from_obj.data_ptr_ = static_cast<void*>(buf.data());
+        head = Solution().removeNthFromEnd(head, 2);
+    printLinkedList(head);
+    
+    // Using Stack
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    bool isValid(string s) {
+    }
     }
     
     
-    {}  //  namespace rocksdb
-
-    
-      uint64_t single_refill_amount =
-      delayed_write_rate_ * kRefillInterval / kMicrosPerSecond;
-  if (bytes_left_ + single_refill_amount >= num_bytes) {
-    // Wait until a refill interval
-    // Never trigger expire for less than one refill interval to avoid to get
-    // time.
-    bytes_left_ = bytes_left_ + single_refill_amount - num_bytes;
-    last_refill_time_ = time_now + kRefillInterval;
-    return kRefillInterval + sleep_debt;
-  }
-    
-    #include <string>
-    
-    // For non linux platform, the following macros are used only as place
-// holder.
-#if !(defined OS_LINUX) && !(defined CYGWIN) && !(defined OS_AIX)
-#define POSIX_FADV_NORMAL 0     /* [MC1] no further special treatment */
-#define POSIX_FADV_RANDOM 1     /* [MC1] expect random page refs */
-#define POSIX_FADV_SEQUENTIAL 2 /* [MC1] expect sequential page refs */
-#define POSIX_FADV_WILLNEED 3   /* [MC1] will need these pages */
-#define POSIX_FADV_DONTNEED 4   /* [MC1] dont need these pages */
-#endif
-    
-      int ret = system('rm -rf /tmp/rocksmergetest');
-  if (ret != 0) {
-    fprintf(stderr, 'Error deleting /tmp/rocksmergetest, code: %d\n', ret);
-    return ret;
-  }
-  rocksdb::Options options;
-  options.create_if_missing = true;
-  options.merge_operator.reset(new MyMerge);
-  options.compaction_filter = &filter;
-  status = rocksdb::DB::Open(options, '/tmp/rocksmergetest', &raw_db);
-  assert(status.ok());
-  std::unique_ptr<rocksdb::DB> db(raw_db);
-    
-      delete txn;
-  // Clear snapshot from read options since it is no longer valid
-  read_options.snapshot = nullptr;
-  snapshot = nullptr;
-    
-      // A list of properties that describe some details about the current
-  // operation.  Same field in op_properties[] might have different
-  // meanings for different operations.
-  uint64_t op_properties[kNumOperationProperties];
-    
-    
-    { protected:
-  explicit DBWithTTL(DB* db) : StackableDB(db) {}
+    {
+    {
+    {            if(cur != NULL){
+                stack.push(cur);
+                cur = cur->left;
+            }
+            else {
+                cur = stack.top();
+                stack.pop();
+                res.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+        return res;
+    }
 };
     
-    void AccelAmplitude::startWithTarget(Node *target)
-{
-    ActionInterval::startWithTarget(target);
-    _other->startWithTarget(target);
-}
+                TreeNode* node = q.front().first;
+            int level = q.front().second;
+            q.pop();
     
-        //
-    // Overrides
-    //
-    virtual CallFuncN* clone() const override;
-    virtual void execute() override;
+    private:
+    struct Command{
+        string s;   // go, print
+        TreeNode* node;
+        Command(string s, TreeNode* node): s(s), node(node){}
+    };
     
-CC_CONSTRUCTOR_ACCESS:
-    CallFuncN():_functionN(nullptr){}
-    virtual ~CallFuncN(){}
+    int main() {
+    }
     
-    #endif /* __CCACTIONTWEEN_H__ */
+            stack<TreeNode*> stack;
+        TreeNode* cur = root;
+        while(cur != NULL || !stack.empty()){
+            while(cur != NULL){
+                res.push_back(cur->val);
+                stack.push(cur);
+                cur = cur->left;
+            }
+    }
+    
+    // PreOrder Morris Traversal
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(1)
+class Solution {
+    }
