7cc8ee3e5f1cb4b370e2465c7166fa5bf6a4e117
SUNsung <weatherius@gmail.com>
2019-03-20 12:03:22 +0200

    Rewrite [22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp] File in: 2019-03-20 12:03:21

--- cccccc/22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp
+++ cccccc/22TcYAD7gv.Tw221w68kJzvb2s22NgJDvs0R7ZE22Dkj.cpp
@@ -1,15 +1,20 @@
 
         
-          // Run shape inference.
-  tensorflow::shape_inference::InferenceContext c(
-      graph_def_version, &node, op_reg_data->op_def, input_shapes,
-      input_tensors, input_tensor_as_shapes_protos,
-      input_handle_shapes_and_types);
-  TF_RETURN_IF_ERROR(c.construction_status());
+        #include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
     
         http://www.apache.org/licenses/LICENSE-2.0
     
-    #include <Python.h>
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+    // We define the PY_ARRAY_UNIQUE_SYMBOL in this .cc file and provide an
+// ImportNumpy function to populate it.
+#define TF_IMPORT_NUMPY
+    
+    #ifndef TENSORFLOW_PYTHON_LIB_IO_PY_RECORD_READER_H_
+#define TENSORFLOW_PYTHON_LIB_IO_PY_RECORD_READER_H_
     
     Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an 'AS IS' BASIS,
@@ -18,110 +23,180 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
     
-    extern PyTypeObject TensorReleaserType;
+    #include <algorithm>
+#include <string>
+#include <vector>
     
-        http://www.apache.org/licenses/LICENSE-2.0
     
-    
-    {  DCHECK(PyDict_Check(code_to_exc_type_map));
-  PyObject* key;
-  PyObject* value;
-  Py_ssize_t pos = 0;
-  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
-    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
-    singleton_->exc_types_[code] = value;
-    // The exception classes should also have the lifetime of the process, but
-    // incref just in case.
-    Py_INCREF(value);
+// Write all (changed_) parameters to a config file.
+void ParamsEditor::WriteParams(char *filename,
+                               bool changes_only) {
+  FILE *fp;                      // input file
+  char msg_str[255];
+                                 // if file exists
+  if ((fp = fopen (filename, 'rb')) != nullptr) {
+    fclose(fp);
+    sprintf (msg_str, 'Overwrite file ' '%s' '? (Y/N)', filename);
+    int a = sv_window_->ShowYesNoDialog(msg_str);
+    if (a == 'n') {
+      return;
+    }  // don't write
   }
+    }
+    
+      // Clean up the bounding boxes from the polygonal approximation by
+  // expanding slightly, then clipping to the blobs from the original_word
+  // that overlap. If not null, the block provides the inverse rotation.
+  void ClipToOriginalWord(const BLOCK* block, WERD* original_word);
+    
+    // A simple class to provide a dynamic programming solution to a class of
+// 1st-order problems in which the cost is dependent only on the current
+// step and the best cost to that step, with a possible special case
+// of using the variance of the steps, and only the top choice is required.
+// Useful for problems such as finding the optimal cut points in a fixed-pitch
+// (vertical or horizontal) situation.
+// Skeletal Example:
+// DPPoint* array = new DPPoint[width];
+// for (int i = 0; i < width; i++) {
+//   array[i].AddLocalCost(cost_at_i)
+// }
+// DPPoint* best_end = DPPoint::Solve(..., array);
+// while (best_end != nullptr) {
+//   int cut_index = best_end - array;
+//   best_end = best_end->best_prev();
+// }
+// delete [] array;
+class DPPoint {
+ public:
+  // The cost function evaluates the total cost at this (excluding this's
+  // local_cost) and if it beats this's total_cost, then
+  // replace the appropriate values in this.
+  typedef int64_t (DPPoint::*CostFunc)(const DPPoint* prev);
+    }
+    
+            m_combinedTrainingFunction->Save(tempModelFile);
+        std::wstring trainerStateCheckpointFilePath = GetTrainerStateCheckpointFilePath(modelFilePath);
+        std::wstring tempCheckpointFile = trainerStateCheckpointFilePath + L'.tmp';
+    
+        virtual const char * CallStack() const override { return m_callStack.c_str(); }
+    
+    #include 'DHTAbstractTask.h'
+#include 'a2time.h'
+    
+    class DHTNode;
+class DHTBucket;
+class DHTTaskQueue;
+class DHTTaskFactory;
+class DHTBucketTreeNode;
+    
+    
+    {  const int clen = bittorrent::getCompactLength(family_);
+  // nodes
+  for (std::vector<std::shared_ptr<DHTNode>>::const_iterator i = nodes_.begin(),
+                                                             eoi = nodes_.end();
+       i != eoi; ++i) {
+    const std::shared_ptr<DHTNode>& node = *i;
+    // Write IP address + port in Compact IP-address/port info form.
+    unsigned char compactPeer[COMPACT_LEN_IPV6];
+    int compactlen = bittorrent::packcompact(compactPeer, node->getIPAddress(),
+                                             node->getPort());
+    if (compactlen != clen) {
+      memset(compactPeer, 0, clen);
+    }
+    uint8_t clen1 = clen;
+    // 1byte compact peer format length
+    WRITE_CHECK(fp, &clen1, sizeof(clen1));
+    // 7bytes reserved
+    WRITE_CHECK(fp, zero, 7);
+    // clen bytes compact peer
+    WRITE_CHECK(fp, compactPeer, static_cast<size_t>(clen));
+    // 24-clen bytes reserved
+    WRITE_CHECK(fp, zero, static_cast<size_t>(24 - clen));
+    // 20bytes: node ID
+    WRITE_CHECK(fp, node->getID(), DHT_ID_LENGTH);
+    // 4bytes reserved
+    WRITE_CHECK(fp, zero, 4);
+  }
+  if (fp.close() == EOF) {
+    throw DL_ABORT_EX(
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
+  }
+  if (!File(filenameTemp).renameTo(filename)) {
+    throw DL_ABORT_EX(
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
+  }
+  A2_LOG_INFO('DHT routing table was saved successfully');
 }
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    namespace tensorflow {
+    class DHTTaskFactoryImpl : public DHTTaskFactory {
+private:
+  std::shared_ptr<DHTNode> localNode_;
     }
     
-    // Safe container for an owned PyObject. On destruction, the reference count of
-// the contained object will be decremented.
-using Safe_PyObjectPtr = std::unique_ptr<PyObject, detail::PyDecrefDeleter>;
-Safe_PyObjectPtr make_safe(PyObject* o);
     
-    // Returns the kernel class name required to execute <node_def> on the device
-// type of <node_def.device>, or an empty string if the kernel class is not
-// found or the device name is invalid.
-string TryFindKernelClass(const string& serialized_node_def);
+    {} // namespace aria2
     
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    class ExampleQt : public QObject {
+    class DHTUnknownMessage : public DHTMessage {
+private:
+  unsigned char* data_;
+  size_t length_;
+  std::string ipaddr_;
+  uint16_t port_;
     }
     
-            size_t size = cache_list.size();
-        if (size == cache_capacity && size > 0)
-        {
-            auto del = cache_list.back();
-            cache_map.erase(del.first);
-            cache_list.pop_back();
-        }
+    #include 'gtest/gtest.h'
     
-    void Channel::yield(enum opcode type)
-{
-    Coroutine *co = Coroutine::get_current();
-    if (unlikely(!co))
-    {
-        swError('Channel::yield() must be called in the coroutine.');
-    }
-    if (type == PRODUCER)
-    {
-        producer_queue.push_back(co);
-        swTraceLog(SW_TRACE_CHANNEL, 'producer cid=%ld', co->get_cid());
-    }
-    else
-    {
-        consumer_queue.push_back(co);
-        swTraceLog(SW_TRACE_CHANNEL, 'consumer cid=%ld', co->get_cid());
-    }
-    co->yield();
+    TEST(ByteTest, GetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  EXPECT_EQ(0x05, value.get_byte(1, 3));
+  EXPECT_EQ(0x01, value.get_byte(1, 1));
+  EXPECT_EQ(0x00, value.get_byte(8, 1));
+  EXPECT_EQ(0x00, value.get_byte(-1, 1));
+  EXPECT_EQ(0x1A, value.get_byte(0, 10));
 }
     
+      x <<= 3;
+  x |= t;
     
-    {    while (1)
+      MatrixXd offset_golden(10, 1);
+  offset_golden << 0, -40, -80, -120, -160, -200, -240, -280, -320, -360;
+    
+    
+    {  // Report Messages
+  AddRecvProtocolData<Accelrpt68, true>();
+  AddRecvProtocolData<Brakemotorrpt170, true>();
+  AddRecvProtocolData<Brakemotorrpt271, true>();
+  AddRecvProtocolData<Brakemotorrpt372, true>();
+  AddRecvProtocolData<Brakerpt6c, true>();
+  AddRecvProtocolData<Datetimerpt83, true>();
+  AddRecvProtocolData<Globalrpt6a, true>();
+  AddRecvProtocolData<Headlightrpt77, true>();
+  AddRecvProtocolData<Hornrpt79, true>();
+  AddRecvProtocolData<Latlonheadingrpt82, true>();
+  AddRecvProtocolData<Parkingbrakestatusrpt80, true>();
+  AddRecvProtocolData<Shiftrpt66, true>();
+  AddRecvProtocolData<Steeringmotorrpt173, true>();
+  AddRecvProtocolData<Steeringmotorrpt274, true>();
+  AddRecvProtocolData<Steeringmotorrpt375, true>();
+  AddRecvProtocolData<Steeringrpt16e, true>();
+  AddRecvProtocolData<Turnrpt64, true>();
+  AddRecvProtocolData<Vehiclespeedrpt6f, true>();
+  AddRecvProtocolData<Wheelspeedrpt7a, true>();
+  AddRecvProtocolData<Wiperrpt91, true>();
+  AddRecvProtocolData<Yawraterpt81, true>();
+}
+    
+    using ::apollo::drivers::canbus::Byte;
+    
+    
     {
-        data = (int) (long) swHashMap_each(hm, &key);
-        if (!data)
-        {
-            break;
-        }
-    }
-    swHashMap_free(hm);
-}
-    
-    static pid_t create_server()
-{
-    pid_t pid;
-    swoole_shell_exec('php server/tcp.php', &pid, 1);
-    sleep(1); // wait 1s
-    return pid;
-}
-    
-    
     {
-    {        make_pair([](void *arg)
-        {
-            auto chan = (Channel *) arg;
-            ASSERT_EQ(*(int *) chan->pop(), 1);
-            ASSERT_EQ(*(int *) chan->pop(), 1);
-        }, &chan)
-    });
+    {
+    {  Brake_rpt_6c::Brake_on_offType ret =
+      static_cast<Brake_rpt_6c::Brake_on_offType>(x);
+  return ret;
 }
+}  // namespace gem
+}  // namespace canbus
+}  // namespace apollo
