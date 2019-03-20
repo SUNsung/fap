3fc8a844a73986e355afc403514554205fd15185
SUNsung <weatherius@gmail.com>
2019-03-20 17:48:48 +0200

    Rewrite [43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py] File in: 2019-03-20 17:48:47

--- pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
+++ pyton/43LwG7mFNeNs43u9TUJxrZ1pE43NHDddSHVe1643CjV.py
@@ -1,350 +1,51 @@
 
         
-        
-@pytest.mark.parametrize('path', (
-    '/create',
-    '/1/update',
-))
-def test_create_update_validate(client, auth, path):
-    auth.login()
-    response = client.post(path, data={'title': '', 'body': ''})
-    assert b'Title is required.' in response.data
-    
-    
-def explain_ignored_app_run():
-    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
-        warn(Warning('Silently ignoring app.run() because the '
-                     'application is run from the flask command line '
-                     'executable.  Consider putting app.run() behind an '
-                     'if __name__ == '__main__' guard to silence this '
-                     'warning.'), stacklevel=3)
+            return strings
 
     
-    
-def load(fp, **kwargs):
-    '''Like :func:`loads` but reads from a file object.
+    def prepare_input(dirty):
     '''
-    _load_arg_defaults(kwargs)
-    if not PY2:
-        fp = _wrap_reader_for_text(fp, kwargs.pop('encoding', None) or 'utf-8')
-    return _json.load(fp, **kwargs)
+    Prepare the plaintext by up-casing it
+    and separating repeated letters with X's
+    '''
     
+    dirty = ''.join([c.upper() for c in dirty if c in string.ascii_letters])
+    clean = ''
     
-class RedirectFallbackMiddleware(MiddlewareMixin):
-    # Defined as class-level attributes to be subclassing-friendly.
-    response_gone_class = HttpResponseGone
-    response_redirect_class = HttpResponsePermanentRedirect
+    if len(dirty) < 2:
+        return dirty
     
-        def save(self, must_create=False):
-        super().save(must_create)
-        self._cache.set(self.cache_key, self._session, self.get_expiry_age())
+        publicKey, privateKey = generateKey(keySize)
+    print('\nWriting public key to file %s_pubkey.txt...' % name)
+    with open('%s_pubkey.txt' % name, 'w') as fo:
+        fo.write('%s,%s,%s' % (keySize, publicKey[0], publicKey[1]))
     
-        def save(self, must_create=False):
-        '''
-        Save the current session data to the database. If 'must_create' is
-        True, raise a database error if the saving operation doesn't create a
-        new entry (as opposed to possibly updating an existing entry).
-        '''
-        if self.session_key is None:
-            return self.create()
-        data = self._get_session(no_load=must_create)
-        obj = self.create_model_instance(data)
-        using = router.db_for_write(self.model, instance=obj)
-        try:
-            with transaction.atomic(using=using):
-                obj.save(force_insert=must_create, force_update=not must_create, using=using)
-        except IntegrityError:
-            if must_create:
-                raise CreateError
-            raise
-        except DatabaseError:
-            if not must_create:
-                raise UpdateError
-            raise
-    
-        def save(self, session_key, session_dict, expire_date):
-        s = self.model(session_key, self.encode(session_dict), expire_date)
-        if session_dict:
-            s.save()
+        def __solveDP(self, x, y):
+        if (x==-1):
+            return y+1
+        elif (y==-1):
+            return x+1
+        elif (self.dp[x][y]>-1):
+            return self.dp[x][y]
         else:
-            s.delete()  # Clear sessions with no data.
-        return s
-    
-        try:
-        desired_rules = validate_nat_rules(nat_rules)
-    except VcaError as e:
-        module.fail_json(msg=e.message)
-    
-        module = AnsibleModule(
-        argument_spec=dict(
-            token=dict(required=True, no_log=True),
-            environment=dict(required=True),
-            user=dict(required=False),
-            repo=dict(required=False),
-            revision=dict(required=False),
-            url=dict(required=False, default='https://api.airbrake.io/deploys.txt'),
-            validate_certs=dict(default='yes', type='bool'),
-        ),
-        supports_check_mode=True
-    )
-    
-        if module.params['repo']:
-        params['deploy[repository]'] = module.params['repo']
-    
-    
-- name: Remove device from CA Spectrum
-  local_action:
-    module: spectrum_device
-    device: '{{ ansible_host }}'
-    landscape: '{{ landscape_handle }}'
-    oneclick_url: http://oneclick.example.com:8080
-    oneclick_user: username
-    oneclick_password: password
-    use_proxy: no
-    state: absent
-'''
-    
-    print(__doc__)
-    
-    score(y_test, clf.predict(X_test), 'dense model')
-benchmark_dense_predict()
-clf.sparsify()
-score(y_test, clf.predict(X_test), 'sparse model')
-benchmark_sparse_predict()
-
-    
-        xx = np.arange(start_dim, start_dim + n * step, step)
-    plt.subplot(212)
-    plt.title('Learning in high dimensional spaces')
-    plt.plot(xx, scikit_classifier_results, 'g-', label='classification')
-    plt.plot(xx, scikit_regressor_results, 'r-', label='regression')
-    plt.legend(loc='upper left')
-    plt.xlabel('number of dimensions')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
-
-    
-    Sentiment analysis can be casted as a binary text classification problem,
-that is fitting a linear classifier on features extracted from the text
-of the user messages so as to guess wether the opinion of the author is
-positive or negative.
-    
-    
-if __name__ == '__main__':
-    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
-    # block to be able to use a multi-core grid search that also works under
-    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
-    # The multiprocessing module is used as the backend of joblib.Parallel
-    # that is used when n_jobs != 1 in GridSearchCV
-    
-            if not in_exercise_region or linestrip.startswith('#'):
-            output_file.write(line)
-    
-    # Unevenly sized blobs
-X_filtered = np.vstack((X[y == 0][:500], X[y == 1][:100], X[y == 2][:10]))
-y_pred = KMeans(n_clusters=3,
-                random_state=random_state).fit_predict(X_filtered)
-    
-    
-def make_data(random_state, n_samples_per_center, grid_size, scale):
-    random_state = check_random_state(random_state)
-    centers = np.array([[i, j]
-                        for i in range(grid_size)
-                        for j in range(grid_size)])
-    n_clusters_true, n_features = centers.shape
-    
-    
-class Signature(jose.Signature):
-    '''ACME-specific Signature. Uses ACME-specific Header for customer fields.'''
-    __slots__ = jose.Signature._orig_slots  # pylint: disable=no-member
-    
-        def test_bad_parse(self):
-        # pylint: disable=protected-access
-        self.config.parser.parse_file(os.path.join(
-            self.config.parser.root, 'conf-available', 'bad_conf_file.conf'))
-        self.assertRaises(
-            errors.PluginError, self.config.check_parsing_errors, 'httpd.aug')
-    
-        def test_include_fullpath(self):
-        self.verify_fnmatch(os.path.join(self.config_path,
-                                         'test_fnmatch.conf'))
-    
-            with mock.patch('certbot.util.get_os_info') as mock_info:
-            for distro in entrypoint.OVERRIDE_CLASSES.keys():
-                mock_info.return_value = (distro, 'whatever')
-                self.assertEqual(entrypoint.get_configurator(),
-                                 entrypoint.OVERRIDE_CLASSES[distro])
-    
-        def test_comparison(self):
-        # Comparison
-        self.check_tokenize('if 1 < 1 > 1 == 1 >= 5 <= 0x15 <= 0x12 != '
-                            '1 and 5 in 1 not in 1 is 1 or 5 is not 1: pass', '''\
-    NAME       'if'          (1, 0) (1, 2)
-    NUMBER     '1'           (1, 3) (1, 4)
-    OP         '<'           (1, 5) (1, 6)
-    NUMBER     '1'           (1, 7) (1, 8)
-    OP         '>'           (1, 9) (1, 10)
-    NUMBER     '1'           (1, 11) (1, 12)
-    OP         '=='          (1, 13) (1, 15)
-    NUMBER     '1'           (1, 16) (1, 17)
-    OP         '>='          (1, 18) (1, 20)
-    NUMBER     '5'           (1, 21) (1, 22)
-    OP         '<='          (1, 23) (1, 25)
-    NUMBER     '0x15'        (1, 26) (1, 30)
-    OP         '<='          (1, 31) (1, 33)
-    NUMBER     '0x12'        (1, 34) (1, 38)
-    OP         '!='          (1, 39) (1, 41)
-    NUMBER     '1'           (1, 42) (1, 43)
-    NAME       'and'         (1, 44) (1, 47)
-    NUMBER     '5'           (1, 48) (1, 49)
-    NAME       'in'          (1, 50) (1, 52)
-    NUMBER     '1'           (1, 53) (1, 54)
-    NAME       'not'         (1, 55) (1, 58)
-    NAME       'in'          (1, 59) (1, 61)
-    NUMBER     '1'           (1, 62) (1, 63)
-    NAME       'is'          (1, 64) (1, 66)
-    NUMBER     '1'           (1, 67) (1, 68)
-    NAME       'or'          (1, 69) (1, 71)
-    NUMBER     '5'           (1, 72) (1, 73)
-    NAME       'is'          (1, 74) (1, 76)
-    NAME       'not'         (1, 77) (1, 80)
-    NUMBER     '1'           (1, 81) (1, 82)
-    OP         ':'           (1, 82) (1, 83)
-    NAME       'pass'        (1, 84) (1, 88)
-    ''')
-    
-        def test_unknown_options(self):
-        rc, out, err = assert_python_failure('-E', '-z')
-        self.assertIn(b'Unknown option: -z', err)
-        self.assertEqual(err.splitlines().count(b'Unknown option: -z'), 1)
-        self.assertEqual(b'', out)
-        # Add 'without='-E'' to prevent _assert_python to append -E
-        # to env_vars and change the output of stderr
-        rc, out, err = assert_python_failure('-z', without='-E')
-        self.assertIn(b'Unknown option: -z', err)
-        self.assertEqual(err.splitlines().count(b'Unknown option: -z'), 1)
-        self.assertEqual(b'', out)
-        rc, out, err = assert_python_failure('-a', '-z', without='-E')
-        self.assertIn(b'Unknown option: -a', err)
-        # only the first unknown option is reported
-        self.assertNotIn(b'Unknown option: -z', err)
-        self.assertEqual(err.splitlines().count(b'Unknown option: -a'), 1)
-        self.assertEqual(b'', out)
-    
-            TASKS = [(mul, (i, 7)) for i in range(10)] + \
-                [(plus, (i, 8)) for i in range(10)]
-    
-    from setuptools import setup, find_packages
-setup(
-    name = proj_info['name'],
-    version = VERSION,
-    
-    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
-    host = r1(r'host=([^&]*)', html)
-    assert host
-    type = r1(r'videofiletype=([^&]*)', html)
-    assert type
-    vid = r1(r'&stream_name=([^&]*)', html)
-    assert vid
-    dir_str = r1(r'&dir=([^&]*)', html).strip()
-    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
-    _, ext, size = url_info(url)
-    print_info(site_info, title, type, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge = merge)
-    
-    site_info = 'CBS.com'
-download = cbs_download
-download_playlist = playlist_not_supported('cbs')
-
-    
-    from xml.etree import cElementTree as ET
-from copy import copy
-from ..common import *
-#----------------------------------------------------------------------
-def ckplayer_get_info_by_xml(ckinfo):
-    '''str->dict
-    Information for CKPlayer API content.'''
-    e = ET.XML(ckinfo)
-    video_dict = {'title': '',
-                  #'duration': 0,
-                  'links': [],
-                  'size': 0,
-                  'flashvars': '',}
-    dictified = dictify(e)['ckplayer']
-    if 'info' in dictified:
-        if '_text' in dictified['info'][0]['title'][0]:  #title
-            video_dict['title'] = dictified['info'][0]['title'][0]['_text'].strip()
-    
-    
-def cntv_download(url, **kwargs):
-    if re.match(r'http://tv\.cntv\.cn/video/(\w+)/(\w+)', url):
-        rid = match1(url, r'http://tv\.cntv\.cn/video/\w+/(\w+)')
-    elif re.match(r'http://tv\.cctv\.com/\d+/\d+/\d+/\w+.shtml', url):
-        rid = r1(r'var guid = '(\w+)'', get_content(url))
-    elif re.match(r'http://\w+\.cntv\.cn/(\w+/\w+/(classpage/video/)?)?\d+/\d+\.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/(\w+/)*VIDE\d+.shtml', url) or \
-         re.match(r'http://(\w+).cntv.cn/(\w+)/classpage/video/(\d+)/(\d+).shtml', url) or \
-         re.match(r'http://\w+.cctv.com/\d+/\d+/\d+/\w+.shtml', url) or \
-         re.match(r'http://\w+.cntv.cn/\d+/\d+/\d+/\w+.shtml', url): 
-        page = get_content(url)
-        rid = r1(r'videoCenterId','(\w+)'', page)
-        if rid is None:
-            guid = re.search(r'guid\s*=\s*'([0-9a-z]+)'', page).group(1)
-            rid = guid
-    elif re.match(r'http://xiyou.cntv.cn/v-[\w-]+\.html', url):
-        rid = r1(r'http://xiyou.cntv.cn/v-([\w-]+)\.html', url)
-    else:
-        raise NotImplementedError(url)
-    
-            for i in range(len(titles)):
-            title = titles[i]
-            datas = {
-                'sid': song_id[i],
-                'ssid': song_ssid[i]
-            }
-            post_params = urllib.parse.urlencode(datas).encode('utf-8')
-            try:
-                resp = urllib.request.urlopen(get_song_url, post_params)
-                resp_data = json.loads(resp.read().decode('utf-8'))
-                real_url = resp_data['r']
-                type, ext, size = url_info(real_url)
-                print_info(site_info, title, type, size)
-            except:
-                pass
-    
-            page = get_content(url)
-        src = re.findall(r'src='(.+?)'', page)
-        js = [path for path in src if path.endswith('.js')]
-    
-        m3u8_url = get_m3u8_url(stream_id)
-    
-            uuid = re.search(r'uuid\s*:\s*\'?(\w+)\'?', flashvar_str).group(1)
-        other = re.search(r'other\s*:\s*'(.*?)'', flashvar_str).group(1)
-        isvc = re.search(r'IService\s*:\s*\'(.+?)\'', flashvar_str).group(1)
+            if (self.A[x]==self.B[y]):
+                self.dp[x][y] = self.__solveDP(x-1,y-1)
+            else:
+                self.dp[x][y] = 1+min(self.__solveDP(x,y-1), self.__solveDP(x-1,y), self.__solveDP(x-1,y-1))
     
     '''
-Lazily-evaluated property pattern in Python.
+The number of partitions of a number n into at least k parts equals the number of partitions into exactly k parts
+plus the number of partitions into at least k-1 parts. Subtracting 1 from each part of a partition of n into k parts
+gives a partition of n-k into k parts. These two facts together are used for this algorithm.
+'''
+def partition(m):
+	memo = [[0 for _ in xrange(m)] for _ in xrange(m+1)]
+	for i in xrange(m+1):
+		memo[i][0] = 1
     
-        def clone(self, **attrs):
-        '''Clone a prototype and update inner attributes dictionary'''
-        # Python in Practice, Mark Summerfield
-        obj = self.__class__()
-        obj.__dict__.update(attrs)
-        return obj
     
-        @classmethod
-    def setUpClass(self):
-        self.radio = Radio()
-    
-        def test_cat_greek_localization(self):
-        self.assertEqual(self.g.get('cat'), 'γάτα')
-    
-        def test_relatives_not_in_properties(self):
-        self.assertNotIn('relatives', self.John.__dict__)
-    
-        # sample_queue.put('sam')
-    # test_object(sample_queue)
-    # print('Outside func: {}'.format(sample_queue.get()))
+def b_expo_mod(a, b, c):
+    res = 0
+    while b > 0:
+        if b&1:
+            res = ((res%c) + (a%c)) % c
