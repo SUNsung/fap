6553782bca970a6ac9ae6b4c3036e696d48cd41e
SUNsung <weatherius@gmail.com>
2019-03-21 23:14:06 +0200

    Rewrite [425QxF4ursIj642pNtEC7oTN4M42k4xh.OZKRCY42Woo.py] File in: 2019-03-21 23:14:06

--- pyton/425QxF4ursIj642pNtEC7oTN4M42k4xh.OZKRCY42Woo.py
+++ pyton/425QxF4ursIj642pNtEC7oTN4M42k4xh.OZKRCY42Woo.py
@@ -1,169 +1,140 @@
 
         
-        P_nxn = rng.randn(N, N) / np.sqrt(N)
-    
-      Args:
-    gd_file: GraphDef proto text file.
-    ckpt_file: TensorFlow Checkpoint file.
-    
-      Returns:
-    predictions:  tf.float32 Tensor of predictions of shape [batch_size,
-      sequence_length]
-  '''
-  if FLAGS.discriminator_model == 'cnn':
-    predictions = cnn.discriminator(
-        hparams, sequence, is_training=is_training, reuse=reuse)
-  elif FLAGS.discriminator_model == 'fnn':
-    predictions = feedforward.discriminator(
-        hparams, sequence, is_training=is_training, reuse=reuse)
-  elif FLAGS.discriminator_model == 'rnn':
-    predictions = rnn.discriminator(
-        hparams, sequence, is_training=is_training, reuse=reuse)
-  elif FLAGS.discriminator_model == 'bidirectional':
-    predictions = bidirectional.discriminator(
-        hparams, sequence, is_training=is_training, reuse=reuse)
-  elif FLAGS.discriminator_model == 'bidirectional_zaremba':
-    predictions = bidirectional_zaremba.discriminator(
-        hparams, sequence, is_training=is_training, reuse=reuse)
-  elif FLAGS.discriminator_model == 'seq2seq_vd':
-    predictions = seq2seq_vd.discriminator(
-        hparams,
-        inputs,
-        present,
-        sequence,
-        is_training=is_training,
-        reuse=reuse)
-  elif FLAGS.discriminator_model == 'rnn_zaremba':
-    predictions = rnn_zaremba.discriminator(
-        hparams, sequence, is_training=is_training, reuse=reuse)
-  elif FLAGS.discriminator_model == 'rnn_nas':
-    predictions = rnn_nas.discriminator(
-        hparams, sequence, is_training=is_training, reuse=reuse)
-  elif FLAGS.discriminator_model == 'rnn_vd':
-    predictions = rnn_vd.discriminator(
-        hparams,
-        sequence,
-        is_training=is_training,
-        reuse=reuse,
-        initial_state=initial_state)
-  elif FLAGS.discriminator_model == 'bidirectional_vd':
-    predictions = bidirectional_vd.discriminator(
-        hparams,
-        sequence,
-        is_training=is_training,
-        reuse=reuse,
-        initial_state=initial_state)
-  else:
-    raise NotImplementedError
-  return predictions
-    
-    from httpie.compat import is_windows
-from httpie.config import DEFAULT_CONFIG_DIR, Config
-    
-        def get_converter(self, mime):
-        if is_valid_mime(mime):
-            for converter_class in plugin_manager.get_converters():
-                if converter_class.supports(mime):
-                    return converter_class(mime)
-    
-            Use `self.raw_auth` to access the raw value passed through
-        `--auth, -a`.
-    
-        '''
-    return path.replace('\\', '\\\\\\')
+        from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
     
     
-def has_docutils():
-    try:
-        # noinspection PyUnresolvedReferences
-        import docutils
-        return True
-    except ImportError:
-        return False
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username.'),
+    ('test', 'a', b'Incorrect password.'),
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
+    
+        assert 'closed' in str(e)
     
     
-def test_unicode_basic_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--verbose', '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/basic-auth/test/' + UNICODE)
+class TaggedJSONSerializer(object):
+    '''Serializer that uses a tag system to compactly represent objects that
+    are not JSON types. Passed as the intermediate serializer to
+    :class:`itsdangerous.Serializer`.
     
-        def test_upload_ok(self, httpbin):
-        r = http('--form', '--verbose', 'POST', httpbin.url + '/post',
-                 'test-file@%s' % FILE_PATH_ARG, 'foo=bar')
-        assert HTTP_OK in r
-        assert 'Content-Disposition: form-data; name='foo'' in r
-        assert 'Content-Disposition: form-data; name='test-file';' \
-               ' filename='%s'' % os.path.basename(FILE_PATH) in r
-        assert FILE_CONTENT in r
-        assert ''foo': 'bar'' in r
-        assert 'Content-Type: text/plain' in r
+            if not current.propagate:
+            break
     
-                value = value.decode('utf8')
-            if name == 'User-Agent' and value.startswith('HTTPie/'):
-                continue
+            :param cli: Command object to invoke. Default is the app's
+            :attr:`~flask.app.Flask.cli` group.
+        :param args: List of strings to invoke the command with.
     
-        def __init__(self, get_response=None):
-        if not apps.is_installed('django.contrib.sites'):
-            raise ImproperlyConfigured(
-                'You cannot use RedirectFallbackMiddleware when '
-                'django.contrib.sites is not installed.'
-            )
-        super().__init__(get_response)
     
-        def save(self, must_create=False):
-        '''
-        To save, get the session key as a securely signed string and then set
-        the modified flag so that the cookie is set on the client for the
-        current request.
-        '''
-        self._session_key = self._get_session_key()
-        self.modified = True
+class MethodView(with_metaclass(MethodViewType, View)):
+    '''A class-based view that dispatches request methods to the corresponding
+    class methods. For example, if you implement a ``get`` method, it will be
+    used to handle ``GET`` requests. ::
     
-        lastmod = None
-    all_sites_lastmod = True
-    urls = []
-    for site in maps:
-        try:
-            if callable(site):
-                site = site()
-            urls.extend(site.get_urls(page=page, site=req_site,
-                                      protocol=req_protocol))
-            if all_sites_lastmod:
-                site_lastmod = getattr(site, 'latest_lastmod', None)
-                if site_lastmod is not None:
-                    site_lastmod = (
-                        site_lastmod.utctimetuple() if isinstance(site_lastmod, datetime.datetime)
-                        else site_lastmod.timetuple()
-                    )
-                    lastmod = site_lastmod if lastmod is None else max(lastmod, site_lastmod)
-                else:
-                    all_sites_lastmod = False
-        except EmptyPage:
-            raise Http404('Page %s empty' % page)
-        except PageNotAnInteger:
-            raise Http404('No page '%s'' % page)
-    response = TemplateResponse(request, template_name, {'urlset': urls},
-                                content_type=content_type)
-    if all_sites_lastmod and lastmod is not None:
-        # if lastmod is defined for all sites, set header so as
-        # ConditionalGetMiddleware is able to send 304 NOT MODIFIED
-        response['Last-Modified'] = http_date(timegm(lastmod))
-    return response
+    
+@pytest.mark.parametrize('command, new_command', [
+    (Command('cargo buid', no_such_subcommand_old), 'cargo build'),
+    (Command('cargo buils', no_such_subcommand), 'cargo build')])
+def test_get_new_command(command, new_command):
+    assert get_new_command(command) == new_command
 
     
-        def run(self, args, opts):
-        # load contracts
-        contracts = build_component_list(self.settings.getwithbase('SPIDER_CONTRACTS'))
-        conman = ContractsManager(load_object(c) for c in contracts)
-        runner = TextTestRunner(verbosity=2 if opts.verbose else 1)
-        result = TextTestResult(runner.stream, runner.descriptions, runner.verbosity)
+        return out
     
-        def short_desc(self):
-        return 'Run a spider'
+        def __init__(self, data=None, **kwargs):
+        self._store = OrderedDict()
+        if data is None:
+            data = {}
+        self.update(data, **kwargs)
     
-        def short_desc(self):
-        return 'Edit spider'
+        @possible_keys
+    def test_getitem(self, key):
+        assert self.case_insensitive_dict[key] == 'application/json'
+    
+        def get_connection(self, url, proxies=None):
+        '''Returns a urllib3 connection for the given URL. This should not be
+        called from user code, and is only exposed for use when subclassing the
+        :class:`HTTPAdapter <requests.adapters.HTTPAdapter>`.
     
     
-class Command(ScrapyCommand):
+class TestGuessFilename:
+    
+        def put_targets(self, targets):
+        '''Creates or updates the provided targets on the rule in AWS'''
+        if not targets:
+            return
+        request = {
+            'Rule': self.name,
+            'Targets': self._targets_request(targets),
+        }
+        try:
+            response = self.client.put_targets(**request)
+        except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
+            self.module.fail_json_aws(e, msg='Could not create/update rule targets for rule %s' % self.name)
+        self.changed = True
+        return response
+    
+        cert_name = module.params.get('name')
+    results = get_server_certs(iam, cert_name)
+    module.exit_json(results=results)
+    
+            elif desired_state == 'offline':
+            if current_state == HOST_ABSENT:
+                self.fail(msg='absent host cannot be placed in offline state')
+            elif current_state in [HOST_STATES.MONITORED, HOST_STATES.DISABLED]:
+                if one.host.status(host.ID, HOST_STATUS.OFFLINE):
+                    self.wait_for_host_state(host, [HOST_STATES.OFFLINE])
+                    result['changed'] = True
+                else:
+                    self.fail(msg='could not set host offline')
+            elif current_state in [HOST_STATES.OFFLINE]:
+                pass
+            else:
+                self.fail(msg='unknown host state %s, cowardly refusing to change state to offline' % current_state_name)
+    
+    # Copyright: (c) 2015, VMware, Inc. All Rights Reserved.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+    
+    
+def nat_rules_to_dict(nat_rules):
+    result = []
+    for rule in nat_rules:
+        gw_rule = rule.get_GatewayNatRule()
+        result.append(
+            dict(
+                rule_type=rule.get_RuleType().lower(),
+                original_ip=gw_rule.get_OriginalIp().lower(),
+                original_port=(gw_rule.get_OriginalPort().lower() or 'any'),
+                translated_ip=gw_rule.get_TranslatedIp().lower(),
+                translated_port=(gw_rule.get_TranslatedPort().lower() or 'any'),
+                protocol=(gw_rule.get_Protocol().lower() or 'any')
+            )
+        )
+    return result
+    
+        try:
+        client.login(
+            username=module.params['ipa_user'],
+            password=module.params['ipa_pass']
+        )
+        changed, zone = ensure(module, client)
+        module.exit_json(changed=changed, zone=zone)
+    except Exception as e:
+        module.fail_json(msg=to_native(e))
+    
+        # Send the data to airbrake
+    data = urlencode(params)
+    response, info = fetch_url(module, url, data=data)
+    if info['status'] == 200:
+        module.exit_json(changed=True)
+    else:
+        module.fail_json(msg='HTTP result code: %d connecting to %s' % (info['status'], url))
+    
+            # If feature is already in good state, just exit
+        if (re.search('Disabled features:.* %s[ \n]' % self.feature_name, out) and self.state == 'absent') or \
+                (re.search('Enabled features:.* %s[ \n]' % self.feature_name, out) and self.state == 'present'):
+            self.module.exit_json(changed=False)
