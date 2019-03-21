00b63bab52fa695fded0ea436cbbeaa14b21f9e3
SUNsung <weatherius@gmail.com>
2019-03-21 21:31:42 +0200

    Rewrite [50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py] File in: 2019-03-21 21:31:41

--- pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
+++ pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
@@ -1,177 +1,182 @@
 
         
-            Returns:
-    A tuple with 6 float values:
-      (total cost of the epoch, epoch reconstruction cost,
-       epoch kl cost, KL weight used this training epoch,
-       total l2 cost on generator, and the corresponding weight).
-    '''
-    ops_to_eval = [self.cost, self.recon_cost,
-                   self.kl_cost, self.kl_weight,
-                   self.l2_cost, self.l2_weight,
-                   self.train_op]
-    collected_op_values = self.run_epoch(datasets, ops_to_eval, kind='train')
+                if args:
+            if 'gui' in conf and conf['gui']:
+                # Enter GUI mode.
+                from .gui import gui_main
+                gui_main(*args, **conf)
+            else:
+                # Enter console mode.
+                from .console import console_main
+                console_main(*args, **conf)
     
-      plt.subplot(nrows,2,13+subplot_cidx)
-  plt.imshow(means_nxt, aspect='auto', interpolation='nearest')
-  plt.title(col_title + ' Means')
-    
-    # DATA
-flags.DEFINE_string('data_dir', DATA_DIR, 'Data for training')
-flags.DEFINE_string('data_filename_stem', DATA_FILENAME_STEM,
-                    'Filename stem for data dictionaries.')
-flags.DEFINE_string('lfads_save_dir', LFADS_SAVE_DIR, 'model save dir')
-flags.DEFINE_string('checkpoint_pb_load_name', CHECKPOINT_PB_LOAD_NAME,
-                    'Name of checkpoint files, use 'checkpoint_lve' for best \
-                    error')
-flags.DEFINE_string('checkpoint_name', CHECKPOINT_NAME,
-                    'Name of checkpoint files (.ckpt appended)')
-flags.DEFINE_string('output_filename_stem', OUTPUT_FILENAME_STEM,
-                    'Name of output file (postfix will be added)')
-flags.DEFINE_string('device', DEVICE,
-                    'Which device to use (default: \'gpu:0\', can also be \
-                    \'cpu:0\', \'gpu:1\', etc)')
-flags.DEFINE_string('csv_log', CSV_LOG,
-                    'Name of file to keep running log of fit likelihoods, \
-                    etc (.csv appended)')
-flags.DEFINE_integer('max_ckpt_to_keep', MAX_CKPT_TO_KEEP,
-                 'Max # of checkpoints to keep (rolling)')
-flags.DEFINE_integer('ps_nexamples_to_process', PS_NEXAMPLES_TO_PROCESS,
-                 'Number of examples to process for posterior sample and \
-                 average (not number of samples to average over).')
-flags.DEFINE_integer('max_ckpt_to_keep_lve', MAX_CKPT_TO_KEEP_LVE,
-                 'Max # of checkpoints to keep for lowest validation error \
-                 models (rolling)')
-flags.DEFINE_integer('ext_input_dim', EXT_INPUT_DIM, 'Dimension of external \
-inputs')
-flags.DEFINE_integer('num_steps_for_gen_ic', NUM_STEPS_FOR_GEN_IC,
-                     'Number of steps to train the generator initial conditon.')
-    
-      # Split the data, inputs, labels and times into train vs. validation.
-  rates_train, rates_valid = \
-      split_list_by_inds(rates, train_inds, valid_inds)
-  noisy_data_train, noisy_data_valid = \
-      split_list_by_inds(noisy_data, train_inds, valid_inds)
-  input_train, inputs_valid = \
-      split_list_by_inds(inputs, train_inds, valid_inds)
-  condition_labels_train, condition_labels_valid = \
-      split_list_by_inds(condition_labels, train_inds, valid_inds)
-  input_times_train, input_times_valid = \
-      split_list_by_inds(input_times, train_inds, valid_inds)
-    
-      try:
-    with h5py.File(data_fname, 'r') as hf:
-      data_dict = {k: np.array(v) for k, v in hf.items()}
-      return data_dict
-  except IOError:
-    print('Cannot open %s for reading.' % data_fname)
-    raise
-    
-      Args:
-    sentence: Sentence words.
-    vocab: Vocabulary. Contains max word chard id length and converts between
-        words and ids.
-  '''
-  targets = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
-  weights = np.ones([BATCH_SIZE, NUM_TIMESTEPS], np.float32)
-    
-      def evaluate(self):
-    '''Evaluate the current ensemble.'''
-    # Attach word probabilities and correctness label to each substitution
-    ensembled_probs = sum(self.all_probs) / len(self.all_probs)
-    scorings = []
-    for i, sentence in enumerate(self.sentences):
-      correctness = self.labels[i]
-      word_probs = ensembled_probs[i, :len(sentence)]
-      joint_prob = np.prod(word_probs, dtype=np.float64)
-    
-      Returns:
-    Tuple of the (sequence, logits, log_probs) of the Generator.   Sequence
-      and logits have shape [batch_size, sequence_length, vocab_size].  The
-      log_probs will have shape [batch_size, sequence_length].  Log_probs
-      corresponds to the log probability of selecting the words.
-  '''
-  if FLAGS.generator_model == 'rnn':
-    (sequence, logits, log_probs, initial_state, final_state) = rnn.generator(
-        hparams,
-        inputs,
-        targets,
-        present,
-        is_training=is_training,
-        is_validating=is_validating,
-        reuse=reuse)
-  elif FLAGS.generator_model == 'rnn_zaremba':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = rnn_zaremba.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_zaremba':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq_zaremba.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'rnn_nas':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = rnn_nas.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_nas':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq_nas.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_vd':
-    (sequence, logits, log_probs, initial_state, final_state,
-     encoder_states) = seq2seq_vd.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  else:
-    raise NotImplementedError
-  return (sequence, logits, log_probs, initial_state, final_state,
-          encoder_states)
+    from ..common import *
     
     
-def percent_unique_ngrams_in_train(train_ngrams_dict, gen_ngrams_dict):
-  '''Compute the percent of ngrams generated by the model that are
-  present in the training text and are unique.'''
+class CNTV(VideoExtractor):
+    name = 'CNTV.com'
+    stream_types = [
+        {'id': '1', 'video_profile': '1280x720_2000kb/s', 'map_to': 'chapters4'},
+        {'id': '2', 'video_profile': '1280x720_1200kb/s', 'map_to': 'chapters3'},
+        {'id': '3', 'video_profile': '640x360_850kb/s', 'map_to': 'chapters2'},
+        {'id': '4', 'video_profile': '480x270_450kb/s', 'map_to': 'chapters'},
+        {'id': '5', 'video_profile': '320x180_200kb/s', 'map_to': 'lowChapters'},
+    ]
     
-    def makeKeyFiles(name, keySize):
-    if os.path.exists('%s_pubkey.txt' % (name)) or os.path.exists('%s_privkey.txt' % (name)):
-        print('\nWARNING:')
-        print(''%s_pubkey.txt' or '%s_privkey.txt' already exists. \nUse a different name or delete these files and re-run this program.' % (name, name))
-        sys.exit()
+        html = get_content(rebuilt_url(url))
+    info = json.loads(match1(html, r'qualities':({.+?}),''))
+    title = match1(html, r''video_title'\s*:\s*'([^']+)'') or \
+            match1(html, r''title'\s*:\s*'([^']+)'')
+    title = unicodize(title)
+    
+    	html = get_html(url)
+	contentid = r1(r'<meta name='contentid' scheme='DMINSTR2' content='([^']+)' />', html)
+	vid = r1(r''demand_ehow_videoid':'([^']+)'', html)
+	assert vid
+    
+    __all__ = ['huomaotv_download']
+    
+    
+def run(args):
+    '''Handle ensure config commandline script.'''
+    parser = argparse.ArgumentParser(
+        description=('Ensure a Home Assistant config exists, '
+                     'creates one if necessary.'))
+    parser.add_argument(
+        '-c', '--config',
+        metavar='path_to_config_dir',
+        default=config_util.get_default_config_dir(),
+        help='Directory that contains the Home Assistant configuration')
+    parser.add_argument(
+        '--script',
+        choices=['ensure_config'])
+    
+    from homeassistant.core import CoreState
+from homeassistant.setup import async_setup_component
+import homeassistant.components.automation as automation
+    
+    
+def test_setitem_callable():
+    # GH 12533
+    s = pd.Series([1, 2, 3, 4], index=list('ABCD'))
+    s[lambda x: 'A'] = -1
+    tm.assert_series_equal(s, pd.Series([-1, 2, 3, 4], index=list('ABCD')))
+    
+            # create target S3 bucket
+        s3_resource.create_bucket(Bucket=TEST_BUCKET_NAME)
+    
+    DDB_KINESIS_STREAM_NAME_PREFIX = '__ddb_stream_'
+    
+    ES_URL = aws_stack.get_local_service_url('elasticsearch')
+TEST_INDEX = 'megacorp'
+TEST_DOC_ID = 1
+COMMON_HEADERS = {
+    'content-type': 'application/json',
+    'Accept-encoding': 'identity'
+}
+TEST_DOMAIN_NAME = 'test_es_domain_1'
+TEST_ENDPOINT_URL = 'http://localhost:4571'
+    
+      def run_tester(self, tester):
+    with self.test_session() as sess:
+      ops = tester.create_model()
+      init_op = tf.group(tf.global_variables_initializer(),
+                         tf.local_variables_initializer())
+      sess.run(init_op)
+      output_result = sess.run(ops)
+      tester.check_output(output_result)
+    
+      def _run_strip_accents(self, text):
+    '''Strips accents from a piece of text.'''
+    text = unicodedata.normalize('NFD', text)
+    output = []
+    for char in text:
+      cat = unicodedata.category(char)
+      if cat == 'Mn':
+        continue
+      output.append(char)
+    return ''.join(output)
+    
+      if len(from_shape) == 3:
+    batch_size = from_shape[0]
+    from_seq_length = from_shape[1]
+    to_seq_length = to_shape[1]
+  elif len(from_shape) == 2:
+    if (batch_size is None or from_seq_length is None or to_seq_length is None):
+      raise ValueError(
+          'When passing in rank 2 tensors to attention_layer, the values '
+          'for `batch_size`, `from_seq_length`, and `to_seq_length` '
+          'must all be specified.')
+    
+        self.assertAllEqual(
+        tokenizer.tokenize(u' \tHeLLo!how  \n Are yoU?  '),
+        ['hello', '!', 'how', 'are', 'you', '?'])
+    self.assertAllEqual(tokenizer.tokenize(u'H\u00E9llo'), ['hello'])
+    
+    Revision ID: 6b5be7e0a0ef
+Revises: 5ec5c84ba61e
+Create Date: 2017-11-02 20:42:13.356360
+    
+        @require_admin
+    def get(self):
+        page = request.args.get('page', 1, type=int)
+        page_size = request.args.get('page_size', 25, type=int)
+        return paginate(self.current_org.events, page, page_size, serialize_event)
+
+    
+            self.record_event({
+            'action': 'edit',
+            'object_id': self.current_org.id,
+            'object_type': 'settings',
+            'new_values': new_values,
+            'previous_values': previous_values
+        })
+    
+        return required_level <= group_level
+    
+            return json_data, error
+    
+        def _get_value(self, dct, dots):
+        for key in dots.split('.'):
+            if dct is not None and key in dct:
+                dct = dct.get(key)
+            else:
+                dct = None
+        return dct
+    
+            response = self.make_request('delete', path, user=user)
+        self.assertEqual(response.status_code, 200)
+    
+    
+    
+        # This part is to make a new line when the process is finished.
+    if progress >= 1:
+        progress = 1
+        status = '\r\n'
+    
+                # Last layer which is the logits for classes
+            logits = tf.contrib.layers.conv2d(net, num_classes, [1, 1], activation_fn=None, scope='fc4')
+    
+        ############################################
+    ############ Run the Session ###############
+    ############################################
+    session_conf = tf.ConfigProto(
+        allow_soft_placement=FLAGS.allow_soft_placement,
+        log_device_placement=FLAGS.log_device_placement)
+    sess = tf.Session(graph=graph, config=session_conf)
+    
+        with sess.as_default():
+        # Run the saver.
+        # 'max_to_keep' flag determines the maximum number of models that the tensorflow save and keep. default by TensorFlow = 5.
+        saver = tf.train.Saver(max_to_keep=FLAGS.max_num_checkpoint)
+    
+    # The tutorials in this section is just a start for going into TensorFlow world.
+# The TensorFlow flags are used for having a more user friendly environment.
+    
+    ##########################################################
+######## Initialization using other variables ############
+##########################################################
+    
+    #######################
+### Necessary Flags ###
+#######################
