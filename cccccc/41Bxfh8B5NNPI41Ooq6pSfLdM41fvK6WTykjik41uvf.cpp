e6b4954995549a1fc9cdfe7577d54b8c2ee15da3
SUNsung <weatherius@gmail.com>
2019-03-21 02:37:34 +0200

    Rewrite [41Bxfh8B5NNPI41Ooq6pSfLdM41fvK6WTykjik41uvf.cpp] File in: 2019-03-21 02:37:34

--- cccccc/41Bxfh8B5NNPI41Ooq6pSfLdM41fvK6WTykjik41uvf.cpp
+++ cccccc/41Bxfh8B5NNPI41Ooq6pSfLdM41fvK6WTykjik41uvf.cpp
@@ -1,179 +1,119 @@
 
         
-              // colon indicates that the argument starts with a URI scheme
-      if (c == ':') {
-        // it could also be a Windows filesystem path
-        if (p == arg + 1)
-          break;
+            void setModel(WalletModel *model);
+    void setAddress_SM(const QString &address);
+    void setAddress_VM(const QString &address);
+    
+    #ifndef SECP256K1_MODULE_ECDH_MAIN_H
+#define SECP256K1_MODULE_ECDH_MAIN_H
+    
+    void test_bad_scalar(void) {
+    unsigned char s_zero[32] = { 0 };
+    unsigned char s_overflow[32] = {
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe,
+        0xba, 0xae, 0xdc, 0xe6, 0xaf, 0x48, 0xa0, 0x3b,
+        0xbf, 0xd2, 0x5e, 0x8c, 0xd0, 0x36, 0x41, 0x41
+    };
+    unsigned char s_rand[32] = { 0 };
+    unsigned char output[32];
+    secp256k1_scalar rand;
+    secp256k1_pubkey point;
     }
     
-    #include 'base/command_line.h'
+        CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 0));
+    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
+    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 1));
+    CHECK(secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
+    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 2));
+    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
+    CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sig64, 3));
+    CHECK(!secp256k1_ecdsa_recover(ctx, &pubkey, &rsig, msg32));
     
-    #include 'base/callback.h'
-#include 'base/location.h'
-#include 'base/single_thread_task_runner.h'
-#include 'uv.h'  // NOLINT(build/include)
+    #include <stdint.h>
+#include <string>
+#include <vector>
     
+    #define QUARTERROUND(a,b,c,d) \
+  a += b; d = rotl32(d ^ a, 16); \
+  c += d; b = rotl32(b ^ c, 12); \
+  a += b; d = rotl32(d ^ a, 8); \
+  c += d; b = rotl32(b ^ c, 7);
     
-    {}  // namespace atom
-    
-     protected:
-  WebRequest(v8::Isolate* isolate, AtomBrowserContext* browser_context);
-  ~WebRequest() override;
-    
-    void AutoUpdater::SetDelegate(Delegate* delegate) {
-  delegate_ = delegate;
+    void RemoveTransliterator::handleTransliterate(Replaceable& text, UTransPosition& index,
+                                               UBool /*isIncremental*/) const {
+    // Our caller (filteredTransliterate) has already narrowed us
+    // to an unfiltered run.  Delete it.
+    UnicodeString empty;
+    text.handleReplaceBetween(index.start, index.limit, empty);
+    int32_t len = index.limit - index.start;
+    index.contextLimit -= len;
+    index.limit -= len;
 }
+U_NAMESPACE_END
     
-    #ifndef ATOM_BROWSER_LIB_POWER_OBSERVER_H_
-#define ATOM_BROWSER_LIB_POWER_OBSERVER_H_
-    
-    void FinishAllTransactions(void);
-    
-    
-    {}  // namespace in_app_purchase
-    
-    #include 'atom/browser/net/url_request_about_job.h'
-    
-      static vector<string> LayerTypeList() {
-    CreatorRegistry& registry = Registry();
-    vector<string> layer_types;
-    for (typename CreatorRegistry::iterator iter = registry.begin();
-         iter != registry.end(); ++iter) {
-      layer_types.push_back(iter->first);
+    UnicodeString&
+SelectFormat::format(const Formattable& obj,
+                   UnicodeString& appendTo,
+                   FieldPosition& pos,
+                   UErrorCode& status) const
+{
+    if (U_FAILURE(status)) {
+        return appendTo;
     }
-    return layer_types;
-  }
-    
-      virtual inline const char* type() const { return 'Accuracy'; }
-  virtual inline int ExactNumBottomBlobs() const { return 2; }
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-                           const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-                            const vector<bool>& propagate_down,
-                            const vector<Blob<Dtype>*>& bottom);
-    
-    
-struct NULLDeleter {template<typename T> void operator()(T*){}};
-    
-    /*!
- * Copyright (c) 2016 by Contributors
- * \file inplace_addto_detect_pass.cc
- * \brief Detect whether inplace addto operation is possible for certain op.
- */
-#include <mxnet/base.h>
-#include <mxnet/operator.h>
-#include <mxnet/op_attr_types.h>
-#include <nnvm/graph_attr_types.h>
-    
-    intptr_t VMTOC::getPtrVector() {
-  always_assert(m_tocvector != nullptr);
-  return reinterpret_cast<intptr_t>(m_tocvector->base() + INT16_MAX + 1);
+    if (obj.getType() == Formattable::kString) {
+        return format(obj.getString(status), appendTo, pos, status);
+    } else {
+        status = U_ILLEGAL_ARGUMENT_ERROR;
+        return appendTo;
+    }
 }
     
-        D_form_t d_formater {{
-                          static_cast<uint32_t>(imm),
-                          static_cast<uint32_t>(ra),
-                          static_cast<uint32_t>(rt),
-                          op
-                         }};
+    U_NAMESPACE_END
     
-    APCHandle::Pair APCCollection::WrapArray(APCHandle::Pair inner,
-                                         CollectionType colType) {
-  auto const col = new APCCollection;
-  col->m_arrayHandle = inner.handle;
-  col->m_colType = colType;
-  return { &col->m_handle, inner.size + sizeof(APCCollection) };
-}
-    
-    #include 'hphp/runtime/base/type-string.h'
-    
-    
-    { private:
-  RowBlock<IndexType> out_;
-  std::unique_ptr<Parser<IndexType> > parser_;
-  uint32_t num_col_;
-  std::vector<size_t> offset_;
-  std::vector<IndexType> dense_index_;
-  std::vector<xgboost::bst_float> dense_value_;
+    class U_I18N_API SharedNumberFormat : public SharedObject {
+public:
+    SharedNumberFormat(NumberFormat *nfToAdopt) : ptr(nfToAdopt) { }
+    virtual ~SharedNumberFormat();
+    const NumberFormat *get() const { return ptr; }
+    const NumberFormat *operator->() const { return ptr; }
+    const NumberFormat &operator*() const { return *ptr; }
+private:
+    NumberFormat *ptr;
+    SharedNumberFormat(const SharedNumberFormat &);
+    SharedNumberFormat &operator=(const SharedNumberFormat &);
 };
     
-    /*!
- * \brief Quantile sketch use WQSummary
- * \tparam DType type of data content
- * \tparam RType type of rank
+    U_NAMESPACE_BEGIN
+    
+        /**
+     * Get maximum significant digits. INT32_MAX means no maximum.
+     */
+    int32_t getMax() const {
+        return fMax;
+    }
+    
+    #ifndef __SMALLINTFORMATTER_H__
+#define __SMALLINTFORMATTER_H__
+    
+    U_NAMESPACE_BEGIN
+    
+        /**
+     * @param keyword for example 'few' or 'other'
+     * @return the plural form corresponding to the keyword, or OTHER
+     */
+    static Form orOtherFromString(const char *keyword) {
+        return static_cast<Form>(indexOrOtherIndexFromString(keyword));
+    }
+    
+    /**
+ * UnicodeReplacer API
  */
-template<typename DType, typename RType = unsigned>
-class WQuantileSketch :
-      public QuantileSketchTemplate<DType, RType, WQSummary<DType, RType> > {
-};
-    
-    
-    {
-    {}  // namespace obj
-}  // namespace xgboost
-
-    
-      void OperateCache(ThreadState* thread) {
-    for (uint64_t i = 0; i < FLAGS_ops_per_thread; i++) {
-      uint64_t rand_key = thread->rnd.Next() % FLAGS_max_key;
-      // Cast uint64* to be char*, data would be copied to cache
-      Slice key(reinterpret_cast<char*>(&rand_key), 8);
-      int32_t prob_op = thread->rnd.Uniform(100);
-      if (prob_op >= 0 && prob_op < FLAGS_insert_percent) {
-        // do insert
-        cache_->Insert(key, new char[10], 1, &deleter);
-      } else if (prob_op -= FLAGS_insert_percent &&
-                 prob_op < FLAGS_lookup_percent) {
-        // do lookup
-        auto handle = cache_->Lookup(key);
-        if (handle) {
-          cache_->Release(handle);
-        }
-      } else if (prob_op -= FLAGS_lookup_percent &&
-                 prob_op < FLAGS_erase_percent) {
-        // do erase
-        cache_->Erase(key);
-      }
-    }
-  }
-    
-      // Will be called while on the write thread before the write executes.  If
-  // this function returns a non-OK status, the write will be aborted and this
-  // status will be returned to the caller of DB::Write().
-  virtual Status Callback(DB* db) = 0;
-    
-      uint64_t single_refill_amount =
-      delayed_write_rate_ * kRefillInterval / kMicrosPerSecond;
-  if (bytes_left_ + single_refill_amount >= num_bytes) {
-    // Wait until a refill interval
-    // Never trigger expire for less than one refill interval to avoid to get
-    // time.
-    bytes_left_ = bytes_left_ + single_refill_amount - num_bytes;
-    last_refill_time_ = time_now + kRefillInterval;
-    return kRefillInterval + sleep_debt;
-  }
-    
-      delete db;
-    
-    #pragma once
-#ifndef ROCKSDB_LITE
-    
-      ~ManagedSnapshot();
-    
-      // A list of properties that describe some details about the current
-  // operation.  Same field in op_properties[] might have different
-  // meanings for different operations.
-  uint64_t op_properties[kNumOperationProperties];
-    
-    namespace rocksdb {
-    }
-    
-      // If non-NULL, use the specified filter policy to reduce disk reads.
-  // Many applications will benefit from passing the result of
-  // NewBloomFilterPolicy() here.
-  //
-  // Default: NULL
-  const FilterPolicy* filter_policy;
+UnicodeString& StringMatcher::toReplacerPattern(UnicodeString& rule,
+                                                UBool /*escapeUnprintable*/) const {
+    // assert(segmentNumber > 0);
+    rule.truncate(0);
+    rule.append((UChar)0x0024 /*$*/);
+    ICU_Utility::appendNumber(rule, segmentNumber, 10, 1);
+    return rule;
+}
