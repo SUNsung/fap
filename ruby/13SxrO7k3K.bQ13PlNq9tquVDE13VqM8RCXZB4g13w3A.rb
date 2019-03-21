f1e39c47719f0137799eeaa1d281d919abcaa92b
SUNsung <weatherius@gmail.com>
2019-03-21 03:13:01 +0200

    Rewrite [13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb] File in: 2019-03-21 03:13:01

--- ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
+++ ruby/13SxrO7k3K.bQ13PlNq9tquVDE13VqM8RCXZB4g13w3A.rb
@@ -1,202 +1,59 @@
 
         
-        # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
+          def translation_scope
+    'devise.sessions'
+  end
     
-    $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+          # Sign out a given user or scope. This helper is useful for signing out a user
+      # after deleting accounts. Returns true if there was a logout and false if there
+      # is no user logged in on the referred scope
+      #
+      # Examples:
+      #
+      #   sign_out :user     # sign_out(scope)
+      #   sign_out @user     # sign_out(resource)
+      #
+      def sign_out(resource_or_scope=nil)
+        return sign_out_all_scopes unless resource_or_scope
+        scope = Devise::Mapping.find_scope!(resource_or_scope)
+        user = warden.user(scope: scope, run_callbacks: false) # If there is no user
     
-      def self.source_dir; SOURCE_DIR; end
-end
+    require 'uri'
     
-        def defaults_deprecate_type(old, current)
-      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
-      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
-                        'type: #{current}'.'
+              include mod
+        end
+    
+      # Under Phusion Passenger smart spawning, we need to reopen all IO streams
+  # after workers have forked.
+  #
+  # The rolling file appender uses shared file locks to ensure that only one
+  # process will roll the log file. Each process writing to the file must have
+  # its own open file descriptor for `flock` to function properly. Reopening
+  # the file descriptors after forking ensures that each worker has a unique
+  # file descriptor.
+  if defined? PhusionPassenger
+    PhusionPassenger.on_event(:starting_worker_process) do |forked|
+      Logging.reopen if forked
     end
   end
 end
-
     
-            lounge.topic_id = post.topic.id
-        unless lounge.save
-          puts lounge.errors.full_messages
-          puts 'Failed to set the lounge description topic!'
-        end
-    
-          @launch_event_sent = true
-      builder = AnalyticsEventBuilder.new(
-        p_hash: launch_context.p_hash,
-        session_id: session_id,
-        action_name: nil,
-        fastlane_client_language: launch_context.fastlane_client_language
-      )
-    
-          context 'without parameters' do
-        it 'executes the correct git command' do
-          allow(Fastlane::Actions).to receive(:sh).with('git add .', anything).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add
-          end').runner.execute(:test)
-        end
-      end
-    
-          it 'works with both select and exclude regex' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            oclint(
-              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-              select_regex: /\.*m/,
-              exclude_regex: /Test/
-            )
-          end').runner.execute(:test)
-    
-    exec_arr = ['fastlane', tool_name] + ARGV
-    
-        # Description formatted to work well as page title when viewing gist
-    if f.core_formula?
-      descr = '#{f.name} on #{OS_VERSION} - Homebrew build logs'
-    else
-      descr = '#{f.name} (#{f.full_name}) on #{OS_VERSION} - Homebrew build logs'
+      def up_down(change)
+    change.up do
+      Mention.update_all(mentions_container_type: 'Post')
+      change_column :mentions, :mentions_container_type, :string, null: false
+      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
     end
-    url = create_gist(files, descr)
     
-        cmd_paths = PATH.new(ENV['PATH']).append(Tap.cmd_directories) unless path
-    path ||= which('brew-#{cmd}', cmd_paths)
-    path ||= which('brew-#{cmd}.rb', cmd_paths)
-    
-    When /^I (?:log|sign) out manually on the mobile website$/ do
-  manual_logout_mobile
+    When /^I toggle nsfw posts$/ do
+  find('.toggle_nsfw_state', match: :first).click
 end
     
-    When /^I (?:like|unlike) the post '([^']*)' in the stream$/ do |post_text|
-  like_stream_post(post_text)
-end
-    
-    Given /^a user named '([^\']*)' with email '([^\']*)'$/ do |name, email|
-  first, last = name.split
-  user = create_user(:email => email, :username => '#{first}_#{last}')
-  user.profile.update_attributes!(:first_name => first, :last_name => last) if first
-end
-    
-        shared_examples 'on a visible post' do
-      it 'creates the participation' do
-        post :create, params: {post_id: @post.id}
-        expect(alice.participations.where(:target_id => @post.id)).to exist
-        expect(response.code).to eq('201')
+          it 'succeeds on mobile' do
+        eve.share_with(alice.person, eve.aspects.first)
+        get :index, format: :mobile
+        expect(response).to be_success
       end
     end
     
-      def exists?(type, path)
-    %Q{[ -#{type} '#{path}' ]}
-  end
-    
-        def install_plugin(plugin, load_hooks: true, load_immediately: false)
-      installer.install(plugin,
-                        load_hooks: load_hooks,
-                        load_immediately: load_immediately)
-    end
-    
-          class Properties
-        def initialize
-          @properties = {}
-        end
-    
-          # Runs all validation rules registered for the given key against the
-      # user-supplied value for that variable. If no validator raises an
-      # exception, the value is assumed to be valid.
-      def assert_valid_now(key, value)
-        validators[key].each do |validator|
-          validator.call(key, value)
-        end
-      end
-    
-          # Internal use only.
-      def peek(key, default=nil, &block)
-        value = fetch_for(key, default, &block)
-        while callable_without_parameters?(value)
-          value = (values[key] = value.call)
-        end
-        value
-      end
-    
-        # @return [String] the ruby version string bundler uses to craft its gem path
-    def gem_ruby_version
-      RbConfig::CONFIG['ruby_version']
-    end
-    
-    module LogStash
-  module PluginManager
-  end
-end
-    
-      # We compare the before the update and after the update
-  def display_updated_plugins(previous_gem_specs_map)
-    update_count = 0
-    find_latest_gem_specs.values.each do |spec|
-      name = spec.name.downcase
-      if previous_gem_specs_map.has_key?(name)
-        if spec.version != previous_gem_specs_map[name].version
-          puts('Updated #{spec.name} #{previous_gem_specs_map[name].version.to_s} to #{spec.version.to_s}')
-          update_count += 1
-        end
-      else
-        puts('Installed #{spec.name} #{spec.version.to_s}')
-        update_count += 1
-      end
-    end
-    
-    describe LogStash::Config::PipelineConfig do
-  let(:source) { LogStash::Config::Source::Local }
-  let(:pipeline_id) { :main }
-  let(:ordered_config_parts) do
-    [
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/1', 0, 0, 'input { generator1 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/2', 0, 0,  'input { generator2 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/3', 0, 0, 'input { generator3 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/4', 0, 0, 'input { generator4 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/5', 0, 0, 'input { generator5 }'),
-      org.logstash.common.SourceWithMetadata.new('file', '/tmp/6', 0, 0, 'input { generator6 }'),
-      org.logstash.common.SourceWithMetadata.new('string', 'config_string', 0, 0, 'input { generator1 }'),
-    ]
-  end
-    
-    shared_examples 'logstash list' do |logstash|
-  describe 'logstash-plugin list on #{logstash.hostname}' do
-    before(:all) do
-      logstash.install({:version => LOGSTASH_VERSION})
-    end
-    
-          it 'detects closing brace on separate line from last element' do
-        inspect_source(source)
-    
-    module RuboCop
-  module AST
-    # A node extension for `def` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `def` nodes within RuboCop.
-    class DefNode < Node
-      include ParameterizedNode
-      include MethodIdentifierPredicates
-    
-            self
-      end
-    
-          # A helper class for comparing the positions of different parts of a
-      # `pair` node.
-      class HashElementDelta
-        def initialize(first, second)
-          @first = first
-          @second = second
-    
-    module RailsCommandHelpers
-  def framework_version?(version_string)
-    framework_version =~ /^#{version_string}/
-  end
-    
-        def each_definition
-      @attachments.each do |klass, attachments|
-        attachments.each do |name, options|
-          yield klass, name, options
-        end
-      end
-    end
+              code = InvitationCode.create(user: bob)
