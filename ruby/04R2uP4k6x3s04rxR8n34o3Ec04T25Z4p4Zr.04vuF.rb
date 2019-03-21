c6340cad007af84f4977c147b88da122ba814d58
SUNsung <weatherius@gmail.com>
2019-03-22 01:04:07 +0200

    Rewrite [04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb] File in: 2019-03-22 01:04:06

--- ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
+++ ruby/04R2uP4k6x3s04rxR8n34o3Ec04T25Z4p4Zr.04vuF.rb
@@ -1,67 +1,60 @@
 
         
-            def execute(command: nil, target_object: nil)
-      action_name = command.method_name
-      action_class_ref = class_ref_for_action(named: action_name)
-      parameter_map = {}
-      closure_argument_value = nil
+                return a.casecmp(b) if a_length == 1 && b_length == 1
+        return 1 if a_length == 1
+        return -1 if b_length == 1
     
-          File.write(new_path, '1')
-      false
+        def as_json
+      @pages
     end
     
-        def rescue_file_error(e)
-      # We're also printing the new-lines, as otherwise the message is not very visible in-between the error and the stack trace
-      puts('')
-      FastlaneCore::UI.important('Error accessing file, this might be due to fastlane's directory handling')
-      FastlaneCore::UI.important('Check out https://docs.fastlane.tools/advanced/#directory-behavior for more details')
-      puts('')
-      raise e
+        def root_path?
+      root_path.present? && root_path != '/'
     end
     
-            expect(result).to eq('appledoc --project-name \'Project Name\' --project-company \'Company\' --use-single-star --keep-intermediate-files --search-undocumented-doc --exit-threshold \'2\' input/dir')
+          def inherited(subclass)
+        super
+        subclass.params = params.deep_dup
+        subclass.headers = headers.deep_dup
+        subclass.force_gzip = force_gzip
       end
-    
-            inner_command = 'git describe --tags `git rev-list --tags --max-count=1`'
-        pseudocommand = 'git log --pretty=\'%B\' #{inner_command.shellescape}...HEAD --no-merges'
-        expect(result).to eq(pseudocommand)
-      end
-    
-          it 'handles the extensions parameter with a single element correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', extensions: ['m'])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --include=\\*.m')
-      end
-    
-          context 'when specify list of files to process' do
-        it 'adds use script input files option and environment variables' do
-          result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-              files: ['AppDelegate.swift', 'path/to/project/src/Model.swift', 'path/to/project/test/Test.swift']
-            )
-          end').runner.execute(:test)
-    
-      it 'has no effect on immediate values' do
-    [nil, true, false].each do |v|
-      v.taint
-      v.tainted?.should == false
     end
-  end
     
-      platform_is_not :windows do
-    it 'returns true when passed ?l if the argument is a symlink' do
-      link = tmp('file_symlink.lnk')
-      File.symlink(@file, link)
-      begin
-        Kernel.test(?l, link).should be_true
-      ensure
-        rm_r link
+            css('a.is-button > h3').each do |node|
+          node.parent.content = node.content
+        end
+    
+            if mod
+          if name == 'Index'
+            return slug.split('/')[1..-2].join('/')
+          elsif name == 'Angular'
+            return slug.split('/').last.split('-').first
+          end
+        end
+    
+    require 'bundler/cli'
+require 'bundler/friendly_errors'
+    
+      def validate_target_file
+    if File.exist?(target_file)
+      if  delete_target_file?
+        File.delete(target_file)
+      else
+        signal_error('Package creation cancelled, a previously generated package exist at location: #{target_file}, move this file to safe place and run the command again')
       end
     end
   end
     
-        pod 'ObjCPod', path: 'ObjCPod'
-    pod 'SwiftPod', path: 'SwiftPod'
-    pod 'MixedPod', path: 'MixedPod'
-    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
+            return nil
+      end
+    end
+  end
+end end end
+
+    
+        before do
+      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
+      # Logstash won't update when we have a pinned version in the gemfile so we remove them
+      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
+      expect(logstash).to have_installed?(plugin_name, previous_version)
+    end
