93606cba4d05b61154c3171ae752221eccc11d93
SUNsung <weatherius@gmail.com>
2019-03-22 10:29:30 +0200

    Rewrite [05KlaQIp7ZVsU05NhJKHYgYKdg055PBW6uNizik053uY.rb] File in: 2019-03-22 10:29:30

--- ruby/05KlaQIp7ZVsU05NhJKHYgYKdg055PBW6uNizik053uY.rb
+++ ruby/05KlaQIp7ZVsU05NhJKHYgYKdg055PBW6uNizik053uY.rb
@@ -1,159 +1,101 @@
 
         
-              GivenDailyLike.increment_for(user.id)
-      expect(value_for(user.id, dt)).to eq(2)
-      expect(limit_reached_for(user.id, dt)).to eq(true)
+                  def hidden_field
+            hidden_name = @html_options[:name] || hidden_field_name
+            @template_object.hidden_field_tag(hidden_name, '', id: nil)
+          end
     
-        Category.transaction do
-      staff.group_names = ['staff']
-      unless staff.save
-        puts staff.errors.full_messages
-        raise 'Failed to set permissions on the Staff category!'
-      end
+            10.times do |x|
+          controller = WithString.new
+          controller.define_singleton_method :index do
+            render template: ActionView::Template::Text.new('Hello string!'), locals: { :'x#{x}' => :omg }
+          end
+          controller.process(:index)
+        end
     
-      def up
-    Photo.joins('INNER JOIN posts ON posts.guid = photos.status_message_guid')
-         .where(posts: {type: 'StatusMessage', public: true}).update_all(public: true)
-    
-    When /^I fill out change password section with my password and '([^']*)' and '([^']*)'$/ do |new_pass, confirm_pass|
-  fill_change_password_section(@me.password, new_pass, confirm_pass)
-end
-    
-    World(NavigationHelpers)
-
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-        it 'marks all notifications in the current filter as read' do
-      request.env['HTTP_REFERER'] = 'I wish I were spelled right'
-      FactoryGirl.create(:notification, recipient: alice, target: post)
-      FactoryGirl.create(:notification, recipient: alice, type: 'Notifications::StartedSharing')
-      expect(Notification.where(unread: true).count).to eq(2)
-      get :read_all, params: {type: 'started_sharing'}
-      expect(Notification.where(unread: true).count).to eq(1)
+        def replace(index, name)
+      @filters[assert_index(index)] = filter_const(name)
     end
     
-          it 'returns an array of reshares for a post' do
-        bob.reshare!(@post)
-        get :index, params: {post_id: @post.id}, format: :json
-        expect(JSON.parse(response.body).map {|h| h['id'] }).to eq(@post.reshares.map(&:id))
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
       end
+    end
     
-          opts.on('-F', '--from FORMAT',
-        'The format to convert from. Can be css, scss, sass.',
-        'By default, this is inferred from the input filename.',
-        'If there is none, defaults to css.') do |name|
-        @options[:from] = name.downcase.to_sym
-        raise 'sass-convert no longer supports LessCSS.' if @options[:from] == :less
-        unless [:css, :scss, :sass].include?(@options[:from])
-          raise 'Unknown format for sass-convert --from: #{name}'
+        def log(msg)
+      puts '\r' + justify(msg)
+    end
+    
+            css('th h3').each do |node|
+          node.name = 'span'
         end
-      end
     
-          if @default_syntax == :sass
-        opts.on('--scss',
-                'Use the CSS-superset SCSS syntax.') do
-          @options[:for_engine][:syntax] = :scss
+            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
+          next if node.previous.try(:content).present? || node.next.try(:content).present?
+          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
+          node.content = node.content.strip
+          node.name = 'pre' if node.content =~ /\s/
+          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
         end
-      else
-        opts.on('--sass',
-                'Use the indented Sass syntax.') do
-          @options[:for_engine][:syntax] = :sass
+    
+      # Sets the resource creating an instance variable
+  def resource=(new_resource)
+    instance_variable_set(:'@#{resource_name}', new_resource)
+  end
+    
+    class BugTest < ActionDispatch::IntegrationTest
+  include Rack::Test::Methods
+  include Warden::Test::Helpers
+    
+          # Sign out all active users or scopes. This helper is useful for signing out all roles
+      # in one click. This signs out ALL scopes in warden. Returns true if there was at least one logout
+      # and false if there was no user logged in on all scopes.
+      def sign_out_all_scopes(lock=true)
+        users = Devise.mappings.keys.map { |s| warden.user(scope: s, run_callbacks: false) }
+    
+            # Returns the instance variables as a hash of key-value pairs.
+        def instance_variables_hash
+          instance_variables.inject({}) do |acc, iv|
+            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
+            acc
+          end
         end
-      end
     
-          # @see Base#watched_file?
-      def watched_file?(filename)
-        # Check against the root with symlinks resolved, since Listen
-        # returns fully-resolved paths.
-        filename =~ /\.s[ac]ss$/ && filename.start_with?(@real_root + File::SEPARATOR)
-      end
+            # This method is expected to return a class that is used for
+        # configuring the provisioner. This return value is expected to be
+        # a subclass of {Config}.
+        #
+        # @return [Config]
+        def self.config_class
+        end
     
-      gem.add_development_dependency 'danger'
-  gem.add_development_dependency 'mocha'
-  gem.add_development_dependency 'rspec'
-  gem.add_development_dependency 'rubocop', '0.48.1'
-end
-
+            # Parses the options given an OptionParser instance.
+        #
+        # This is a convenience method that properly handles duping the
+        # originally argv array so that it is not destroyed.
+        #
+        # This method will also automatically detect '-h' and '--help'
+        # and print help. And if any invalid options are detected, the help
+        # will be printed, as well.
+        #
+        # If this method returns `nil`, then you should assume that help
+        # was printed and parsing failed.
+        def parse_options(opts=nil)
+          # make sure optparse doesn't use POSIXLY_CORRECT parsing
+          ENV['POSIXLY_CORRECT'] = nil
     
-    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
-  file_shared_path = TestApp.shared_path.join(file)
-  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
-  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
-end
+      # Specifies the header that your server uses for sending files.
+  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
     
-      def test_symlink_exists(path)
-    exists?('L', path)
+      # The test environment is used exclusively to run your application's
+  # test suite. You never need to work with it otherwise. Remember that
+  # your test database is 'scratch space' for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+    
+      def test_font_helper_with_suffix_sharp
+    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
   end
-    
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   XSS and others
-    # Supported browsers:: Firefox 23+, Safari 7+, Chrome 25+, Opera 15+
-    #
-    # Description:: Content Security Policy, a mechanism web applications
-    #               can use to mitigate a broad class of content injection
-    #               vulnerabilities, such as cross-site scripting (XSS).
-    #               Content Security Policy is a declarative policy that lets
-    #               the authors (or server administrators) of a web application
-    #               inform the client about the sources from which the
-    #               application expects to load resources.
-    #
-    # More info::   W3C CSP Level 1 : https://www.w3.org/TR/CSP1/ (deprecated)
-    #               W3C CSP Level 2 : https://www.w3.org/TR/CSP2/ (current)
-    #               W3C CSP Level 3 : https://www.w3.org/TR/CSP3/ (draft)
-    #               https://developer.mozilla.org/en-US/docs/Web/Security/CSP
-    #               http://caniuse.com/#search=ContentSecurityPolicy
-    #               http://content-security-policy.com/
-    #               https://securityheaders.io
-    #               https://scotthelme.co.uk/csp-cheat-sheet/
-    #               http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-    #
-    # Sets the 'Content-Security-Policy[-Report-Only]' header.
-    #
-    # Options: ContentSecurityPolicy configuration is a complex topic with
-    #          several levels of support that has evolved over time.
-    #          See the W3C documentation and the links in the more info
-    #          section for CSP usage examples and best practices. The
-    #          CSP3 directives in the 'NO_ARG_DIRECTIVES' constant need to be
-    #          presented in the options hash with a boolean 'true' in order
-    #          to be used in a policy.
-    #
-    class ContentSecurityPolicy < Base
-      default_options default_src: :none, script_src: ''self'',
-                      img_src: ''self'', style_src: ''self'',
-                      connect_src: ''self'', report_only: false
-    
-      it 'should not set the Content Security Policy for other content types' do
-    headers = get('/', {}, 'wants' => 'text/foo').headers
-    expect(headers['Content-Security-Policy']).to be_nil
-    expect(headers['Content-Security-Policy-Report-Only']).to be_nil
-  end
-    
-          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
-      def left_shift(int, shift)
-        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
-        # 1>>31, 2**32
-        (r & 2147483648) == 0 ? r : r - 4294967296
-      end
-    
-        get '/pages'
-    
-    context 'Precious::Helpers' do
-  include Precious::Helpers
-    
-      teardown do
-    FileUtils.rm_rf(@path)
-  end
-end
-
-    
-      test 'uri encode' do
-    c = '한글'
-    assert_equal '%ED%95%9C%EA%B8%80', encodeURIComponent(c)
-    assert_equal '%ED%95%9C%EA%B8%80', CGI::escape(c)
-  end
-end
