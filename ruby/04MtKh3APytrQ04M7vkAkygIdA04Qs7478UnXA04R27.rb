a769d1e41f327b046189321496058c5f30f3630e
SUNsung <weatherius@gmail.com>
2019-03-22 17:50:14 +0200

    Rewrite [04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb] File in: 2019-03-22 17:50:12

--- ruby/04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb
+++ ruby/04MtKh3APytrQ04M7vkAkygIdA04Qs7478UnXA04R27.rb
@@ -1,98 +1,41 @@
 
         
-                  new_theme_name = args.join('_')
-          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
-          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
+          describe '#flag?' do
+    let(:argv) { ['--foo', '-bq', '--bar'] }
     
-          def string_to_code(string)
-        # sha bytes
-        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
-        # Thanks donpark's IdenticonUtil.java for this.
-        # Match the following Java code
-        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
-        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
+          spec['main'] =
+          find_files.(File.join(Bootstrap.stylesheets_path, '_bootstrap.scss')) +
+          find_files.(Bootstrap.fonts_path) +
+          %w(assets/javascripts/bootstrap.js)
     
-      teardown do
-    FileUtils.rm_r(File.join(File.dirname(__FILE__), *%w[examples test.git]))
-  end
+    class NodeMincerTest < Minitest::Test
+  DUMMY_PATH = 'test/dummy_node_mincer'
     
-    opts = OptionParser.new do |opts|
-  # define program name (although this defaults to the name of the file, just in case...)
-  opts.program_name = 'gollum'
+    Gem::Specification.new do |gem|
+  gem.name          = 'capistrano'
+  gem.version       = Capistrano::VERSION
+  gem.authors       = ['Tom Clements', 'Lee Hambley']
+  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
+  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
+  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
+  gem.homepage      = 'http://capistranorb.com/'
     
-  # set basic info for the '--help' command (options will be appended automatically from the below definitions)
-  opts.banner = '
-  Gollum is a multi-format Wiki Engine/API/Frontend.
+    module VagrantHelpers
+  extend self
     
-  Usage:
-      gollum [options] [git-repo]
-    
-  Arguments:
-      [git-repo]                     Path to the git repository being served. If not specified, current working directory is used.
-  
-  Notes:
-      Paths for all options are relative to <git-repo> unless absolute.
-      This message is only a basic description. For more information, please visit:
-          https://github.com/gollum/gollum
-  
-  OPTIONS'
-  
-  # define gollum options  
-  opts.separator ''
-  opts.separator '  Major:'
-  
-  opts.on('-h', '--host [HOST]', 'Specify the hostname or IP address to listen on. Default: '0.0.0.0'.') do |host|
-    options[:bind] = host
-  end
-  opts.on('-p', '--port [PORT]', 'Specify the port to bind Gollum with. Default: '4567'.') do |port|
-    begin
-      # don't use 'port.to_i' here... it doesn't raise errors which might result in a nice confusion later on
-      options[:port] = Integer(port)
-    rescue ArgumentError
-      puts 'Error: '#{port}' is not a valid port number.'
-      exit 1
-    end
-  end
-  opts.on('-c', '--config [FILE]', 'Specify path to the Gollum's configuration file.') do |file|
-    options[:config] = file
-  end
-  opts.on('-r', '--ref [REF]', 'Specify the branch to serve. Default: 'master'.') do |ref|
-    wiki_options[:ref] = ref
-  end
-  opts.on('-a', '--adapter [ADAPTER]', 'Launch Gollum using a specific git adapter. Default: 'grit'.') do |adapter|
-    Gollum::GIT_ADAPTER = adapter
-  end
-  opts.on('--bare', 'Declare '<git-repo>' to be bare. This is only necessary when using the grit adapter.') do
-    wiki_options[:repo_is_bare] = true
-  end
-  opts.on('-b', '--base-path [PATH]', 'Specify the leading portion of all Gollum URLs (path info). Default: '/'.',
-    'Example: setting this to '/wiki' will make the wiki accessible under 'http://localhost:4567/wiki/'.') do |base_path|
-      
-    # first trim a leading slash, if any
-    base_path.sub!(/^\/+/, '')
-    
-      node[:applications].each do |app, data|
-    template '/etc/monit.d/sidekiq_#{app}.monitrc' do 
-      owner 'root' 
-      group 'root' 
-      mode 0644 
-      source 'monitrc.conf.erb' 
-      variables({ 
-        :num_workers => worker_count,
-        :app_name => app, 
-        :rails_env => node[:environment][:framework_env] 
-      }) 
+        def installer
+      @installer ||= PluginInstaller.new
     end
     
-      <h3>Messages</h3>
-  <% @messages.each do |msg| %>
-    <p><%= msg %></p>
-  <% end %>
-
-    
-          def insert_after(oldklass, newklass, *args)
-        i = entries.index { |entry| entry.klass == newklass }
-        new_entry = i.nil? ? Entry.new(newklass, *args) : entries.delete_at(i)
-        i = entries.index { |entry| entry.klass == oldklass } || entries.count - 1
-        entries.insert(i+1, new_entry)
+          # rubocop:disable Security/MarshalLoad
+      def add_role(role, hosts, options={})
+        options_deepcopy = Marshal.dump(options.merge(roles: role))
+        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
       end
+      # rubocop:enable Security/MarshalLoad
+    
+          attr_reader :validators
+    
+        def post(path, &block)
+      route(POST, path, &block)
+    end
