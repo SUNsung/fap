51a4a8133ad1fdd510e6aade0b1e5c3224c4ee7f
SUNsung <weatherius@gmail.com>
2019-03-22 14:09:41 +0200

    Rewrite [00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp] File in: 2019-03-22 14:09:40

--- cccccc/00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp
+++ cccccc/00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp
@@ -1,167 +1,309 @@
 
         
-          gfx::Point GetCursorScreenPoint();
-  display::Display GetPrimaryDisplay();
-  std::vector<display::Display> GetAllDisplays();
-  display::Display GetDisplayNearestPoint(const gfx::Point& point);
-  display::Display GetDisplayMatching(const gfx::Rect& match_rect);
+        Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-    namespace atom {
+    
+    {}  // namespace tensorflow
+
+    
+    #include 'tensorflow/python/lib/core/py_exception_registry.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    
+    {}  // namespace tensorflow
+
+    
+    // Safe containers for an owned TF_Status. On destruction, the handle
+// will be deleted by TF_DeleteStatus.
+using Safe_TF_StatusPtr = std::unique_ptr<TF_Status, detail::TFStatusDeleter>;
+Safe_TF_StatusPtr make_safe(TF_Status* status);
+    
+    #include 'tensorflow/core/framework/node_def.pb.h'
+#include 'tensorflow/core/framework/node_def_util.h'
+#include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+#include 'tensorflow/core/framework/types.h'
+#include 'tensorflow/core/lib/core/status.h'
+#include 'tensorflow/core/util/device_name_utils.h'
+    
+      // Synchronize with spinlocks.
+  static const unsigned kScheduleSpin = 0x02;
+  // Synchronize with spinlocks that also call CPU yield instructions.
+  static const unsigned kScheduleYield = 0x04;
+  // Synchronize with a 'synchronization primitive' (e.g. mutex).
+  static const unsigned kScheduleBlockingSync = 0x08;
+    
+    
+    {    // Vector of cached {config, executor} pairs.
+    std::vector<
+        std::pair<StreamExecutorConfig, std::unique_ptr<StreamExecutor>>>
+        configurations GUARDED_BY(configurations_mutex);
+  };
+    
+    namespace fft {
+    }
+    
+    #include 'base/basictypes.h'
+#include '../dispatcher_host.h'
+    
+      virtual void Call(const std::string& method,
+                    const base::ListValue& arguments,
+                    content::RenderFrameHost* rvh = nullptr);
+  virtual void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result);
+    
+    // Call method of an object in browser and return the result.
+// function CallObjectMethod(id, type, method, args);
+v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
+                                           int object_id,
+                                           const std::string& type,
+                                           const std::string& method,
+                                           v8::Handle<v8::Value> args);
+    
+    class BaseEvent {
+  friend class EventListener;
+  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
     }
     
     
-    {}  // namespace atom
+    {}  // namespace nw
     
-    void FinishTransactionByDate(const std::string& date);
+      nw::Package* package = nw::package();
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  base::CommandLine::StringVector args = command_line->GetArgs();
+  base::CommandLine::StringVector argv = command_line->original_argv();
     
-      // Downloadable Content Information
-  bool downloadable = false;
+    bool NwObjCallObjectMethodSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
+    }
     
-    #include <string>
+        void operator() (const typename VecTraits<T>::vec64 & v_src0,
+                     const typename VecTraits<T>::vec64 & v_src1,
+                     typename VecTraits<T>::vec64 & v_dst) const
+    {
+        typename VecTraits<wtype>::vec128 vr;
+        wideAdd(vmovl(v_src0), vmovl(v_src1), vr);
+    }
     
-    void OffscreenViewProxy::RemoveObserver() {
-  observer_ = nullptr;
+                    int32x4_t va = vaddq_s32(lane0a, lane2a);
+                int32x4_t vb = vaddq_s32(lane0b, lane2b);
+                int32x4_t wa = vaddq_s32(va, lane1a);
+                int32x4_t wb = vaddq_s32(vb, lane1b);
+    
+    void Canny3x3L2(const Size2D &size,
+                const u8 * srcBase, ptrdiff_t srcStride,
+                u8 * dstBase, ptrdiff_t dstStride,
+                f64 low_thresh, f64 high_thresh,
+                Margin borderMargin)
+{
+    internal::assertSupportedConfiguration(isCanny3x3Supported(size));
+#ifdef CAROTENE_NEON
+    Canny3x3<true, false>(size, 1,
+                          srcBase, srcStride,
+                          dstBase, dstStride,
+                          NULL, 0,
+                          NULL, 0,
+                          low_thresh, high_thresh,
+                          borderMargin);
+#else
+    (void)size;
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)low_thresh;
+    (void)high_thresh;
+    (void)borderMargin;
+#endif
 }
     
-      // Releases ownership of the object without decrementing the reference count.
-  // The caller now owns the returned reference.
-  PyObjectStruct* release() {
-    PyObject* p = ptr_;
-    ptr_ = NULL;
-    return p;
+    #include 'common.hpp'
+    
+    #else
+    
+                uint32x4_t vs4 = vpaddlq_u16(vpaddlq_u8(vs));
+            uint32x2_t vs2 = vadd_u32(vget_low_u32(vs4), vget_high_u32(vs4));
+    
+                    uint16x8_t vSum_3_1 = vaddq_u16(vLane3.val[0], vLane1.val[0]);
+                uint16x8_t vSum_4_2 = vaddq_u16(vLane3.val[1], vLane1.val[1]);
+                uint16x8_t vSum_5_6 = vaddq_u16(vLane3.val[2], vLane1.val[2]);
+    
+    class BLOBNBOX;
+class BLOBNBOX_CLIST;
+class BLOB_CHOICE_LIST;
+class STRING;
+class TO_BLOCK_LIST;
+class UNICHARSET;
+template <typename T> class GenericVector;
+    
+    // Main entry point for Paragraph Detection Algorithm.
+//
+// Given a set of equally spaced textlines (described by row_infos),
+// Split them into paragraphs.  See http://goto/paragraphstalk
+//
+// Output:
+//   row_owners - one pointer for each row, to the paragraph it belongs to.
+//   paragraphs - this is the actual list of PARA objects.
+//   models - the list of paragraph models referenced by the PARA objects.
+//            caller is responsible for deleting the models.
+void DetectParagraphs(int debug_level,
+                      GenericVector<RowInfo> *row_infos,
+                      GenericVector<PARA *> *row_owners,
+                      PARA_LIST *paragraphs,
+                      GenericVector<ParagraphModel *> *models);
+    
+    
+    {  name += UNLV_EXT;              //add extension
+  if ((pdfp = fopen (name.string (), 'rb')) == nullptr) {
+    return false;                //didn't read one
+  } else {
+    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
+                                 //make rect block
+      block = new BLOCK (name.string (), TRUE, 0, 0,
+                         (int16_t) x, (int16_t) (ysize - y - height),
+                         (int16_t) (x + width), (int16_t) (ysize - y));
+                                 //on end of list
+      block_it.add_to_end (block);
+    }
+    fclose(pdfp);
   }
+  return true;
+}
     
-    static void WriteDocCommentBodyForLocation(
-    io::Printer* printer, const SourceLocation& location) {
-  string comments = location.leading_comments.empty() ?
-      location.trailing_comments : location.leading_comments;
-  if (!comments.empty()) {
-    // TODO(kenton):  Ideally we should parse the comment text as Markdown and
-    //   write it back as HTML, but this requires a Markdown parser.  For now
-    //   we just use <pre> to get fixed-width text formatting.
-    }
-    }
+    template <typename T> class GenericVector;
+template <typename T> class GenericVector;
     
-      desired_output_for_decode = 'ABCD__EfghI_j';
-  expected = string('\x64\x80\xC5\xA1\x0', 5);
-  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
-                                                     desired_output_for_decode);
-  EXPECT_EQ(expected, result);
+      // Computes and returns the squared evaluation metric for a line fit.
+  double EvaluateLineFit();
     
-      // Check default values.
-  const Descriptor* descriptor = message->GetDescriptor();
-  const Reflection* reflection = message->GetReflection();
-  EXPECT_EQ(0, reflection->GetInt32(
-      *message, descriptor->FindFieldByName('foo_int')));
-  EXPECT_EQ('', reflection->GetString(
-      *message, descriptor->FindFieldByName('foo_string')));
-  EXPECT_EQ('', reflection->GetString(
-      *message, descriptor->FindFieldByName('foo_cord')));
-  EXPECT_EQ('', reflection->GetString(
-      *message, descriptor->FindFieldByName('foo_string_piece')));
-  EXPECT_EQ('', reflection->GetString(
-      *message, descriptor->FindFieldByName('foo_bytes')));
-  EXPECT_EQ(unittest::TestOneof2::FOO, reflection->GetEnum(
-      *message, descriptor->FindFieldByName('foo_enum'))->number());
-  const Descriptor* nested_descriptor;
-  const Message* nested_prototype;
-  nested_descriptor =
-      pool_.FindMessageTypeByName('protobuf_unittest.TestOneof2.NestedMessage');
-  nested_prototype = factory_.GetPrototype(nested_descriptor);
-  EXPECT_EQ(nested_prototype,
-            &reflection->GetMessage(
-                *message, descriptor->FindFieldByName('foo_message')));
-  const Descriptor* foogroup_descriptor;
-  const Message* foogroup_prototype;
-  foogroup_descriptor =
-      pool_.FindMessageTypeByName('protobuf_unittest.TestOneof2.FooGroup');
-  foogroup_prototype = factory_.GetPrototype(foogroup_descriptor);
-  EXPECT_EQ(foogroup_prototype,
-            &reflection->GetMessage(
-                *message, descriptor->FindFieldByName('foogroup')));
-  EXPECT_NE(foogroup_prototype,
-            &reflection->GetMessage(
-                *message, descriptor->FindFieldByName('foo_lazy_message')));
-  EXPECT_EQ(5, reflection->GetInt32(
-      *message, descriptor->FindFieldByName('bar_int')));
-  EXPECT_EQ('STRING', reflection->GetString(
-      *message, descriptor->FindFieldByName('bar_string')));
-  EXPECT_EQ('CORD', reflection->GetString(
-      *message, descriptor->FindFieldByName('bar_cord')));
-  EXPECT_EQ('SPIECE', reflection->GetString(
-      *message, descriptor->FindFieldByName('bar_string_piece')));
-  EXPECT_EQ('BYTES', reflection->GetString(
-      *message, descriptor->FindFieldByName('bar_bytes')));
-  EXPECT_EQ(unittest::TestOneof2::BAR, reflection->GetEnum(
-      *message, descriptor->FindFieldByName('bar_enum'))->number());
     
-    const std::string& DHTResponseMessage::getType() const { return R; }
+    {  int delta = this - prev;
+  int32_t n = prev->n_ + 1;
+  int32_t sig_x = prev->sig_x_ + delta;
+  int64_t sig_xsq = prev->sig_xsq_ + delta * delta;
+  int64_t cost = (sig_xsq - sig_x * sig_x / n) / n;
+  cost += prev->total_cost_;
+  UpdateIfBetter(cost, prev->total_steps_ + 1, prev, n, sig_x, sig_xsq);
+  return cost;
+}
     
-      virtual std::shared_ptr<DHTTask>
-  createPeerAnnounceTask(const unsigned char* infoHash) = 0;
-    
-    namespace aria2 {
+    class DLLSYM DIR128
+{
+  public:
+    DIR128() = default;
     }
     
+    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
+::opencensus::stats::MeasureDouble RpcClientServerLatency();
+::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
     
-    {} // namespace aria2
+    constexpr size_t RpcServerStatsEncoding::kRpcServerStatsSize;
+constexpr size_t RpcServerStatsEncoding::kEncodeDecodeFailure;
+constexpr size_t RpcServerStatsEncoding::kVersionIdSize;
+constexpr size_t RpcServerStatsEncoding::kFieldIdSize;
+constexpr size_t RpcServerStatsEncoding::kVersionIdOffset;
+constexpr size_t RpcServerStatsEncoding::kVersionId;
     
-        deleteLinkedList(head);
+    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_RPC_ENCODING_H */
+
     
-                    char match;
-                if( s[i] == ')' )
-                    match = '(';
-                else if( s[i] == ']' )
-                    match = '[';
-                else{
-                    assert( s[i] == '}' );
-                    match = '{';
-                }
+      Status GetAllExtensionNumbers(
+      ServerContext* context, const grpc::string& type,
+      reflection::v1alpha::ExtensionNumberResponse* response);
     
-        vector<int> vec2 = {2};
-    Solution().sortColors(vec2);
-    printArr(vec2);
     
-            ListNode* pre = dummyHead;
-        for(int i = 0; i < m - 1; i ++){
-            pre = pre->next
+    {                //    Save state to this URX_LB_CONT op, so failure to match will repeat the loop.
+                //      (successful match will cause a FAIL out of the loop altogether.)
+                fp = StateSave(fp, fp->fPatIdx-4, status);
+                fp->fInputIdx = lbStartIdx;
+            }
+            break;
+    
+    
+    {    switch(attr) {
+    case UCOL_FRENCH_COLLATION:
+        ownedSettings->setFlag(CollationSettings::BACKWARD_SECONDARY, value,
+                               defaultSettings.options, errorCode);
+        break;
+    case UCOL_ALTERNATE_HANDLING:
+        ownedSettings->setAlternateHandling(value, defaultSettings.options, errorCode);
+        break;
+    case UCOL_CASE_FIRST:
+        ownedSettings->setCaseFirst(value, defaultSettings.options, errorCode);
+        break;
+    case UCOL_CASE_LEVEL:
+        ownedSettings->setFlag(CollationSettings::CASE_LEVEL, value,
+                               defaultSettings.options, errorCode);
+        break;
+    case UCOL_NORMALIZATION_MODE:
+        ownedSettings->setFlag(CollationSettings::CHECK_FCD, value,
+                               defaultSettings.options, errorCode);
+        break;
+    case UCOL_STRENGTH:
+        ownedSettings->setStrength(value, defaultSettings.options, errorCode);
+        break;
+    case UCOL_HIRAGANA_QUATERNARY_MODE:
+        // Deprecated attribute. Check for valid values but do not change anything.
+        if(value != UCOL_OFF && value != UCOL_ON && value != UCOL_DEFAULT) {
+            errorCode = U_ILLEGAL_ARGUMENT_ERROR;
         }
+        break;
+    case UCOL_NUMERIC_COLLATION:
+        ownedSettings->setFlag(CollationSettings::NUMERIC, value, defaultSettings.options, errorCode);
+        break;
+    default:
+        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
+        break;
+    }
+    if(U_FAILURE(errorCode)) { return; }
+    setFastLatinOptions(*ownedSettings);
+    if(value == UCOL_DEFAULT) {
+        setAttributeDefault(attr);
+    } else {
+        setAttributeExplicitly(attr);
+    }
+}
     
     
-    {        vector<int> res;
-        __inorderTraversal(root, res);
-        return res;
-    }
+    {    BreakIterator *get() const { return ptr; }
+    BreakIterator *operator->() const { return ptr; }
+    BreakIterator &operator*() const { return *ptr; }
+private:
+    BreakIterator *ptr;
+    SharedBreakIterator(const SharedBreakIterator &);
+    SharedBreakIterator &operator=(const SharedBreakIterator &);
+};
     
-    private:
-    struct Command{
-        string s;   // go, print
-        TreeNode* node;
-        Command(string s, TreeNode* node): s(s), node(node){}
-    };
+    #include 'unicode/utypes.h'
     
-    /// BFS
-/// No need to store level information in the queue :-)
-///
-/// Time Complexity: O(n), where n is the number of nodes in the tree
-/// Space Complexity: O(n)
-class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
+    int32_t StandardPlural::indexFromString(const char *keyword, UErrorCode &errorCode) {
+    if (U_FAILURE(errorCode)) { return OTHER; }
+    int32_t i = indexOrNegativeFromString(keyword);
+    if (i >= 0) {
+        return i;
+    } else {
+        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
+        return OTHER;
     }
-    }
+}
     
-    // Recursive
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-public:
-    vector<int> preorderTraversal(TreeNode* root) {
-    }
-    }
-    
-    // PreOrder Morris Traversal
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(1)
-class Solution {
-    }
+    U_NAMESPACE_BEGIN
