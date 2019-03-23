fe97fa88ca6bd9990a8fd43c43eb35a6459ff5c9
SUNsung <weatherius@gmail.com>
2019-03-23 05:32:23 +0200

    Rewrite [3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb] File in: 2019-03-23 05:32:22

--- ruby/3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb
+++ ruby/3567xr7YYdhI35MsdIfk.oaY35izLRjnKQnzc35aQZ.rb
@@ -1,44 +1,106 @@
 
         
-          # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
-  end
-  config.static_cache_control = 'public, max-age=3600'
+        def custom_release_header_anchors(markdown)
+  header_regexp = %r!^(\d{1,2})\.(\d{1,2})\.(\d{1,2}) \/ \d{4}-\d{2}-\d{2}!
+  section_regexp = %r!^### \w+ \w+$!
+  markdown.split(%r!^##\s!).map do |release_notes|
+    _, major, minor, patch = *release_notes.match(header_regexp)
+    release_notes
+      .gsub(header_regexp, '\\0\n{: #v\\1-\\2-\\3}')
+      .gsub(section_regexp) { |section| '#{section}\n{: ##{slugify(section)}-v#{major}-#{minor}-#{patch}}' }
+  end.join('\n## ')
+end
     
-      def ruby_prefix
-    RbConfig::CONFIG['prefix']
-  end
+    CONTENT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-          page.find('#option_type_option_values_attributes_0_name').set('color')
-      page.find('#option_type_option_values_attributes_0_presentation').set('black')
-      click_button 'Update'
-      expect(page).to have_content('successfully updated!')
-    
-              def ensure_order
-            raise ActiveRecord::RecordNotFound if spree_current_order.nil?
+                Jekyll.logger.debug 'LiveReload:', 'Reloading #{p.url}'
+            Jekyll.logger.debug '', json_message
+            @websockets.each { |ws| ws.send(json_message) }
           end
-    
-              state = @states.last
-          respond_with(@states) if stale?(state)
         end
     
-        def each_definition
-      @attachments.each do |klass, attachments|
-        attachments.each do |name, options|
-          yield klass, name, options
+        def show_message
+      UI.message('Sending anonymous analytics information')
+      UI.message('Learn more at https://docs.fastlane.tools/#metrics')
+      UI.message('No personal or sensitive data is sent.')
+      UI.message('You can disable this by adding `opt_out_usage` at the top of your Fastfile')
+    end
+    
+            it 'executes the correct git command' do
+          allow(Fastlane::Actions).to receive(:sh).with('git add #{path.shellescape}', anything).and_return('')
+          result = Fastlane::FastFile.new.parse('lane :test do
+            git_add(path: '#{path}')
+          end').runner.execute(:test)
         end
       end
+    
+          it 'works given the path to compile_commands.json' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json'
+            )
+          end').runner.execute(:test)
+    
+            it 'supports autocorrect mode option' do
+          result = Fastlane::FastFile.new.parse('lane :test do
+            swiftlint(
+              mode: :autocorrect
+            )
+          end').runner.execute(:test)
+    
+          it 'should shell escape keychain names when checking for installation' do
+        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
+        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
+    
+        private
+    
+        def replace(index, name)
+      @filters[assert_index(index)] = filter_const(name)
     end
     
-        # scale to the requested geometry and preserve the aspect ratio
-    def scale_to(new_geometry)
-      scale = [new_geometry.width.to_f / self.width.to_f , new_geometry.height.to_f / self.height.to_f].min
-      Paperclip::Geometry.new((self.width * scale).round, (self.height * scale).round)
-    end
+    module Docs
+  class Entry
+    class Invalid < StandardError; end
+    
+        delegate :empty?, :blank?, to: :pages
+    
+      def path
+    HOMEBREW_PINNED_KEGS/@f.name
   end
-end
+    
+      describe '#userpaths?' do
+    it 'returns true if the environment contains :userpaths' do
+      env << :userpaths
+      expect(env).to use_userpaths
+    end
+    
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
+          end
+        end
+    
+          def stack
+        UI::ErrorReport.stack
+      end
