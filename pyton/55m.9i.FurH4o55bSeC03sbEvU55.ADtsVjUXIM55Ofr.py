c637d5542ff763a7714b36d51f66ea0b85733e95
SUNsung <weatherius@gmail.com>
2019-03-23 19:37:31 +0200

    Rewrite [55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py] File in: 2019-03-23 19:37:31

--- pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
+++ pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
@@ -1,33 +1,97 @@
 
         
-            print('After subclassing: ')
-    for k in RegistryHolder.REGISTRY:
-        print(k)
+            n_samples = 2000
+    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
+    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
+                                           list_n_features, precompute=False)
+    plt.subplot(212)
+    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
+    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
+    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
+    plt.legend(loc='upper left')
+    plt.xlabel('number of features')
+    plt.ylabel('Time (s)')
+    plt.axis('tight')
+    plt.show()
+
     
-        print('rm1: {0}'.format(rm1))
-    print('rm2: {0}'.format(rm2))
-    print('rm3: {0}'.format(rm3))
+    n_samples = np.logspace(.5, 3, 9)
+n_features = np.logspace(1, 3.5, 7)
+N_samples, N_features = np.meshgrid(n_samples,
+                                    n_features)
+scikits_time = np.zeros(N_samples.shape)
+scipy_time = np.zeros(N_samples.shape)
     
-        @lazy_property
-    def relatives(self):
-        # Get all relatives, let's assume that it costs much time.
-        relatives = 'Many relatives.'
-        return relatives
-    
-    
-class Standby(Inservice):
-    def __init__(self, HierachicalStateMachine):
-        self._hsm = HierachicalStateMachine
-    
-    
-class Ui(object):
-    ''' UI interaction class '''
+        # Plot results
+    i = 0
+    m = len(list_n_features)
+    plt.figure('scikit-learn SGD regression benchmark results',
+               figsize=(5 * 2, 4 * m))
+    for j in range(m):
+        plt.subplot(m, 2, i + 1)
+        plt.plot(list_n_samples, np.sqrt(elnet_results[:, j, 0]),
+                 label='ElasticNet')
+        plt.plot(list_n_samples, np.sqrt(sgd_results[:, j, 0]),
+                 label='SGDRegressor')
+        plt.plot(list_n_samples, np.sqrt(asgd_results[:, j, 0]),
+                 label='A-SGDRegressor')
+        plt.plot(list_n_samples, np.sqrt(ridge_results[:, j, 0]),
+                 label='Ridge')
+        plt.legend(prop={'size': 10})
+        plt.xlabel('n_train')
+        plt.ylabel('RMSE')
+        plt.title('Test error - %d features' % list_n_features[j])
+        i += 1
     
     
 if __name__ == '__main__':
-    main()
+    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
+    # block to be able to use a multi-core grid search that also works under
+    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
+    # The multiprocessing module is used as the backend of joblib.Parallel
+    # that is used when n_jobs != 1 in GridSearchCV
     
-        def test_initial_am_station(self):
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
+    legend1_values_list = list(legend1.values())
+legend1_keys_list = list(legend1.keys())
+    
+    colors = cycle('bgrcmykbgrcmykbgrcmykbgrcmyk')
+for k, col in zip(range(n_clusters_), colors):
+    my_members = labels == k
+    cluster_center = cluster_centers[k]
+    plt.plot(X[my_members, 0], X[my_members, 1], col + '.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+             markeredgecolor='k', markersize=14)
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+
+    
+    
+    
+    
+    def LB(self, k):
+        '''Look backwards k tokens on-channel tokens'''
+    
+    CLI_DEFAULTS = dict(
+    server_root=server_root_tmp,
+    ctl='nginx',
+)
+'''CLI defaults.'''
+    
+    from acme import test_util
+    
+            '''
+        if self.get_addr() == '_default_':
+            return 0
+        elif self.get_addr() == '*':
+            return 1
+        else:
+            return 2
+    
+        def test_recovery_routine_error(self):
+        self.config.reverter.recovery_routine = mock.Mock(
+            side_effect=errors.ReverterError)
+    
+    
+def _SetCountingStyle(level):
+  '''Sets the module's counting options.'''
+  _cpplint_state.SetCountingStyle(level)
