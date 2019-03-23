b3277e5f4b318fb8e727d73f4071951587f76e4c
SUNsung <weatherius@gmail.com>
2019-03-23 20:17:19 +0200

    Rewrite [50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py] File in: 2019-03-23 20:17:18

--- pyton/50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py
+++ pyton/50UMOII5X8i1c50XTec8ggru250.dtUghjbDuE50rs4.py
@@ -1,129 +1,97 @@
 
         
-        
-class BlackJackCard(Card):
-    
-        def mapper(self, _, line):
-        yield line, 1
-    
-      if prefix_words.find('<S>') != 0:
-    prefix_words = '<S> ' + prefix_words
-    
-      def __init__(self, test_data_name='wsc273'):
-    vocab_file = os.path.join(FLAGS.data_dir, 'vocab.txt')
-    self.vocab = utils.CharsVocabulary(vocab_file, 50)
-    assert test_data_name in ['pdp60', 'wsc273'], (
-        'Test data must be pdp60 or wsc273, got {}'.format(test_data_name))
-    self.test_data_name = test_data_name
+            READY = 0
+    IN_PROGRESS = 1
+    COMPLETE = 2
     
     
-def compare_substitutions(question_ids, scorings, mode='full'):
-  '''Return accuracy by comparing two consecutive scorings.'''
-  prediction_correctness = []
-  # Compare two consecutive substitutions
-  for i in range(len(scorings) // 2):
-    scoring1, scoring2 = scorings[2*i: 2*i+2]
-    if mode == 'partial':  # fix joint prob into partial prob
-      _convert_to_partial(scoring1, scoring2)
-    
-    from models import bidirectional_zaremba
-from models import cnn
-from models import critic_vd
-from models import feedforward
-from models import rnn
-from models import rnn_nas
-from models import rnn_vd
-from models import rnn_zaremba
-from models import seq2seq
-from models import seq2seq_nas
-from models import seq2seq_vd
-from models import seq2seq_zaremba
-    
-        elif FLAGS.discriminator_model == 'seq2seq_vd':
-      load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-      print('Restoring Discriminator from %s.' % load_ckpt)
-      tf.logging.info('Restoring Discriminator from %s.' % load_ckpt)
-      dis_encoder_init_saver = init_savers['dis_encoder_init_saver']
-      dis_decoder_init_saver = init_savers['dis_decoder_init_saver']
-      dis_encoder_init_saver.restore(sess, load_ckpt)
-      dis_decoder_init_saver.restore(sess, load_ckpt)
-    
-        All keys are expected to be strings. The structure remembers the
-    case of the last key to be set, and ``iter(instance)``,
-    ``keys()``, ``items()``, ``iterkeys()``, and ``iteritems()``
-    will contain case-sensitive keys. However, querying and contains
-    testing is case insensitive::
-    
-    from .__version__ import __title__, __description__, __url__, __version__
-from .__version__ import __build__, __author__, __author_email__, __license__
-from .__version__ import __copyright__, __cake__
-    
-                        for header, value in request.headers.items():
-                        low_conn.putheader(header, value)
+class DefaultCategories(Enum):
     
     
-def info():
-    '''Generate information for a bug report.'''
-    try:
-        platform_info = {
-            'system': platform.system(),
-            'release': platform.release(),
-        }
-    except IOError:
-        platform_info = {
-            'system': 'Unknown',
-            'release': 'Unknown',
-        }
+class RemoveDuplicateUrls(MRJob):
     
-            try:
-            self.hooks[event].remove(hook)
-            return True
-        except ValueError:
-            return False
+        def __init__(self, pages, data_store, reverse_index_queue, doc_index_queue):
+        self.pages = pages
+        self.data_store = data_store
+        self.reverse_index_queue = reverse_index_queue
+        self.doc_index_queue = doc_index_queue
     
     
-def write_with_harness(codefile, sourcefile, start_linenum, linebuffer):
-    '''write output with additional lines to make code likely compilable'''
-    # add commonly used headers, so that lines can likely compile.
-    # This is work in progress, the main issue remains handling class
-    # declarations in in-function code differently
-    with io.open(codefile, 'w') as code_filehandle:
-        code_filehandle.write('''\
-#include<stdio.h>      // by md-split
-#include<stdlib.h>     // by md-split
-#include<tuple>        // by md-split
-#include<utility>      // by md-split
-#include<limits>       // by md-split
-#include<functional>   // by md-split
-#include<string>       // by md-split
-#include<map>          // by md-split
-#include<iostream>     // by md-split
-#include<vector>       // by md-split
-#include<algorithm>    // by md-split
-#include<memory>       // by md-split
-using namespace std;   // by md-split
-// %s : %s
-''' % (sourcefile, start_linenum))
-        # TODO: if not toplevel code, wrap inside class
-        for codeline in linebuffer:
-            code_filehandle.write(codeline)
+def repr_dict_nice(d):
+    def prepare_dict(d):
+        for k, v in d.items():
+            if isinstance(v, dict):
+                v = dict(prepare_dict(v))
+            elif isinstance(v, bytes):
+                v = v.decode('utf8')
+            elif not isinstance(v, (int, str)):
+                v = repr(v)
+            yield k, v
+    return json.dumps(
+        dict(prepare_dict(d)),
+        indent=4, sort_keys=True,
+    )
     
-    # One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'face_recognition',
-     u'Face Recognition Documentation',
-     [u'Adam Geitgey'], 1)
-]
+        try:
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            USERNAME,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
+
     
-    Algorithm Description:
-The knn classifier is first trained on a set of labeled (known) faces and can then predict the person
-in an unknown image by finding the k most similar faces (images with closet face-features under eucledian distance)
-in its training set, and performing a majority vote (possibly weighted) on their label.
+    from httpie import ExitStatus
+from utils import http, HTTP_OK
     
-    # Find all the faces in the image using a pre-trained convolutional neural network.
-# This method is more accurate than the default HOG model, but it's slower
-# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
-# this will use GPU acceleration and perform well.
-# See also: find_faces_in_picture.py
-face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
+    
+class AjaxCrawlMiddleware(object):
+    '''
+    Handle 'AJAX crawlable' pages marked as crawlable via meta tag.
+    For more info see https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
+    '''
+    
+        def backwards(self, orm):
+    
+    
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding model 'ProcessingIssue'
+        db.create_table(
+            'sentry_processingissue', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.Project']
+                    )
+                ), (
+                    'checksum',
+                    self.gf('django.db.models.fields.CharField')(max_length=40, db_index=True)
+                ), ('type', self.gf('django.db.models.fields.CharField')(max_length=30)),
+                ('data', self.gf('sentry.db.models.fields.gzippeddict.GzippedDictField')()), (
+                    'datetime',
+                    self.gf('django.db.models.fields.DateTimeField')()
+                ),
+            )
+        )
+        db.send_create_signal('sentry', ['ProcessingIssue'])
+    
+            db.start_transaction()
+    
+        complete_apps = ['sentry']
+
+    
+            # Adding unique constraint on 'Distribution', fields ['release', 'name']
+        db.create_unique('sentry_distribution', ['release_id', 'name'])
+    
+        def backwards(self, orm):
+        # Removing unique constraint on 'UserOption', fields ['user', 'organization', 'key']
+        db.delete_unique('sentry_useroption', ['user_id', 'organization_id', 'key'])
