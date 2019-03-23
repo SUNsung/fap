8f4bca9414713ab0462dfd7acaaffbaa00b5a9cd
SUNsung <weatherius@gmail.com>
2019-03-23 17:30:37 +0200

    Rewrite [03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py] File in: 2019-03-23 17:30:37

--- pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
+++ pyton/03NJT4GWLMGQ03BYtRcbFsi7k0346CMsQ0nZM037hR.py
@@ -1,135 +1,49 @@
 
         
         
-class Message(object):
+def load_data():
+    '''Loads CIFAR10 dataset.
     
-        def reducer(self, key, values):
-        total = sum(values)
-        if total == 1:
-            yield key, total
-    
-    
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'git help')
-    
-    
-@pytest.mark.parametrize('script, output', [
-    ('vim', invalid_operation('vim')),
-    ('apt-get', '')])
-def test_not_match(script, output):
-    assert not match(Command(script, output))
-    
-    no_match_output = '''
-Hit:1 http://us.archive.ubuntu.com/ubuntu zesty InRelease
-Get:2 http://us.archive.ubuntu.com/ubuntu zesty-updates InRelease [89.2 kB]
-Get:3 http://us.archive.ubuntu.com/ubuntu zesty-backports InRelease [89.2 kB]
-Get:4 http://security.ubuntu.com/ubuntu zesty-security InRelease [89.2 kB]
-Hit:5 https://cli-assets.heroku.com/branches/stable/apt ./ InRelease
-Hit:6 http://ppa.launchpad.net/ubuntu-mozilla-daily/ppa/ubuntu zesty InRelease
-Hit:7 https://download.docker.com/linux/ubuntu zesty InRelease
-Get:8 http://us.archive.ubuntu.com/ubuntu zesty-updates/main i386 Packages [232 kB]
-Get:9 http://us.archive.ubuntu.com/ubuntu zesty-updates/main amd64 Packages [235 kB]
-Get:10 http://us.archive.ubuntu.com/ubuntu zesty-updates/main amd64 DEP-11 Metadata [55.2 kB]
-Get:11 http://us.archive.ubuntu.com/ubuntu zesty-updates/main DEP-11 64x64 Icons [32.3 kB]
-Get:12 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe amd64 Packages [156 kB]
-Get:13 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe i386 Packages [156 kB]
-Get:14 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe amd64 DEP-11 Metadata [175 kB]
-Get:15 http://us.archive.ubuntu.com/ubuntu zesty-updates/universe DEP-11 64x64 Icons [253 kB]
-Get:16 http://us.archive.ubuntu.com/ubuntu zesty-updates/multiverse amd64 DEP-11 Metadata [5,840 B]
-Get:17 http://us.archive.ubuntu.com/ubuntu zesty-backports/universe amd64 DEP-11 Metadata [4,588 B]
-Get:18 http://security.ubuntu.com/ubuntu zesty-security/main amd64 DEP-11 Metadata [12.7 kB]
-Get:19 http://security.ubuntu.com/ubuntu zesty-security/main DEP-11 64x64 Icons [17.6 kB]
-Get:20 http://security.ubuntu.com/ubuntu zesty-security/universe amd64 DEP-11 Metadata [21.6 kB]
-Get:21 http://security.ubuntu.com/ubuntu zesty-security/universe DEP-11 64x64 Icons [47.7 kB]
-Get:22 http://security.ubuntu.com/ubuntu zesty-security/multiverse amd64 DEP-11 Metadata [208 B]
-Fetched 1,673 kB in 0s (1,716 kB/s)
-Reading package lists... Done
-Building dependency tree
-Reading state information... Done
-All packages are up to date.
-'''
-    
-    
-@pytest.mark.parametrize('command', [
-    Command('apt list --upgradable', no_match_output),
-    Command('sudo apt list --upgradable', no_match_output)
-])
-def test_not_match(command):
-    assert not match(command)
-    
-    
-@pytest.mark.parametrize('command, result', [
-    (Command('aws dynamdb scan', misspelled_command),
-     ['aws dynamodb scan']),
-    (Command('aws dynamodb scn', misspelled_subcommand),
-     ['aws dynamodb scan']),
-    (Command('aws dynamodb t-item',
-             misspelled_subcommand_with_multiple_options),
-     ['aws dynamodb put-item', 'aws dynamodb get-item'])])
-def test_get_new_command(command, result):
-    assert get_new_command(command) == result
+        return (x_train, y_train), (x_test, y_test)
 
     
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_match(brew_no_available_formula, brew_already_installed,
-               brew_install_no_argument):
-    assert match(Command('brew install elsticsearch',
-                         brew_no_available_formula))
-    assert not match(Command('brew install git',
-                             brew_already_installed))
-    assert not match(Command('brew install', brew_install_no_argument))
-    
-    \tDid you mean `build`?
-'''
+        with gzip.open(paths[1], 'rb') as imgpath:
+        x_train = np.frombuffer(imgpath.read(), np.uint8,
+                                offset=16).reshape(len(y_train), 28, 28)
     
     
-@parametrize_extensions
-@parametrize_filename
-@parametrize_script
-def test_match(ext, tar_error, filename, unquoted, quoted, script, fixed):
-    tar_error(unquoted.format(ext))
-    assert match(Command(script.format(filename.format(ext)), ''))
+def get(identifier):
+    if identifier is None:
+        return None
+    if isinstance(identifier, dict):
+        return deserialize(identifier)
+    elif isinstance(identifier, six.string_types):
+        config = {'class_name': str(identifier), 'config': {}}
+        return deserialize(config)
+    elif callable(identifier):
+        return identifier
+    else:
+        raise ValueError('Could not interpret regularizer identifier: ' +
+                         str(identifier))
+
     
-        def get(self):
-        if len(self.config.GAE_APPIDS):
-            if len(self.working_appid_list) == 0:
-                time_to_reset = 600 - (time.time() - self.last_reset_time)
-                if time_to_reset > 0:
-                    self.logger.warn('all appid out of quota, wait %d seconds to reset', time_to_reset)
-                    time.sleep(time_to_reset)
-                    return None
-                else:
-                    self.logger.warn('reset appid')
-                    self.reset_appid()
-    
-            def get_exist_ca_sha1():
-            args = ['security', 'find-certificate', '-Z', '-a', '-c', commonname]
-            output = subprocess.check_output(args)
-            for line in output.splitlines(True):
-                if len(line) == 53 and line.startswith('SHA-1 hash:'):
-                    sha1_hash = line[12:52]
-                    return sha1_hash
-    
-        return out
-    
-                # Is there a bitwise operation to do this?
-            if v == 0xFFFF:
-                v = -1
-    
-            return link_list
-    
-        urls = matchall(content, netease_embed_patterns)
-    for url in urls:
-        found = True
-        netease_download(url, output_dir=output_dir, merge=merge, info_only=info_only)
-    
-        type, ext, size = url_info(url[0], True)
-    size = urls_size(url)
-    
-    __all__ = ['huomaotv_download']
-    
-    from ..common import *
+    plt.plot(range(epochs),
+         history_model1.history['val_loss'],
+         'g-',
+         label='Network 1 Val Loss')
+plt.plot(range(epochs),
+         history_model2.history['val_loss'],
+         'r-',
+         label='Network 2 Val Loss')
+plt.plot(range(epochs),
+         history_model1.history['loss'],
+         'g--',
+         label='Network 1 Loss')
+plt.plot(range(epochs),
+         history_model2.history['loss'],
+         'r--',
+         label='Network 2 Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('comparison_of_networks.png')
