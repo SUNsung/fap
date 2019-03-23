2774122eb4bbdc1679b48b38a9c1ace4ecbafde4
SUNsung <weatherius@gmail.com>
2019-03-23 16:51:24 +0200

    Rewrite [12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb] File in: 2019-03-23 16:51:24

--- ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
+++ ruby/12cZWhX2tGNaU12ejiW.gVXgj6126.tgaXrO9D.12OcF.rb
@@ -1,99 +1,199 @@
 
         
-              GivenDailyLike.increment_for(user.id)
-      expect(value_for(user.id, dt)).to eq(1)
-      expect(limit_reached_for(user.id, dt)).to eq(false)
+        module ActionView
+  module Helpers
+    module Tags # :nodoc:
+      class DateSelect < Base # :nodoc:
+        def initialize(object_name, method_name, template_object, options, html_options)
+          @html_options = html_options
     
-        group.add(moderator)
-    group.save
-    
-    module Vagrant
-  # This class handles guest-OS specific interactions with a machine.
-  # It is primarily responsible for detecting the proper guest OS
-  # implementation and then delegating capabilities.
-  #
-  # Vagrant has many tasks which require specific guest OS knowledge.
-  # These are implemented using a guest/capability system. Various plugins
-  # register as 'guests' which determine the underlying OS of the system.
-  # Then, 'guest capabilities' register themselves for a specific OS (one
-  # or more), and these capabilities are called.
-  #
-  # Example capabilities might be 'mount_virtualbox_shared_folder' or
-  # 'configure_networks'.
-  #
-  # This system allows for maximum flexibility and pluginability for doing
-  # guest OS specific operations.
-  class Guest
-    include CapabilityHost
-    
-            # Initializes the system. Any subclasses MUST make sure this
-        # method is called on the parent. Therefore, if a subclass overrides
-        # `initialize`, then you must call `super`.
-        def initialize(vm)
-          @vm = vm
-        end
-    
-            # This method is expected to return a class that is used for
-        # configuring the provisioner. This return value is expected to be
-        # a subclass of {Config}.
-        #
-        # @return [Config]
-        def self.config_class
-        end
-    
-            # This method is called if the underlying machine ID changes. Providers
-        # can use this method to load in new data for the actual backing
-        # machine or to realize that the machine is now gone (the ID can
-        # become `nil`). No parameters are given, since the underlying machine
-        # is simply the machine instance given to this object. And no
-        # return value is necessary.
-        def machine_id_changed
-        end
-    
-        @statuses = @account.statuses.permitted_for(@account, signed_request_account)
-    @statuses = params[:min_id].present? ? @statuses.paginate_by_min_id(LIMIT, params[:min_id]).reverse : @statuses.paginate_by_max_id(LIMIT, params[:max_id])
-    @statuses = cache_collection(@statuses, Status)
-  end
-    
-        def destroy
-      authorize @report_note, :destroy?
-      @report_note.destroy!
-      redirect_to admin_report_path(@report_note.report_id), notice: I18n.t('admin.report_notes.destroyed_msg')
-    end
-    
-      def payload
-    @_payload ||= request.body.read
-  end
-    
-      def show
-    render_cached_json('api:v1:instances:activity:show', expires_in: 1.day) { activity }
-  end
-    
-        web_subscription = ::Web::PushSubscription.create!(
-      endpoint: subscription_params[:endpoint],
-      key_p256dh: subscription_params[:keys][:p256dh],
-      key_auth: subscription_params[:keys][:auth],
-      data: data,
-      user_id: active_session.user_id,
-      access_token_id: active_session.access_token_id
-    )
-    
-        define_method provider do
-      @user = User.find_for_oauth(request.env['omniauth.auth'], current_user)
-    
-      def default_locale
-    if ENV['DEFAULT_LOCALE'].present?
-      I18n.default_locale
-    else
-      request_locale || I18n.default_locale
-    end
-  end
-    
-          return false
-    end
-    
-          def css # custom css
-        @css
+        def find_all_with_query(query) # :nodoc:
+      paths.each do |resolver|
+        templates = resolver.find_all_with_query(query)
+        return templates unless templates.empty?
       end
     
-        assert_no_match /New/, last_response.body, ''New' link not blocked in pages template'
+          def file_name
+        @template.identifier
+      end
+    
+          def self.delegate_to(klass)
+        self.type_klass = klass
+      end
+    
+        class RespondToActionController < AbstractController::Base
+      def index() self.response_body = 'success' end
+    
+      protected
+    
+    class TestController < ApplicationController
+  include Rails.application.routes.url_helpers
+    
+    module Devise
+  module Controllers
+    # A module that may be optionally included in a controller in order
+    # to provide remember me behavior. Useful when signing in is done
+    # through a callback, like in OmniAuth.
+    module Rememberable
+      # Return default cookie values retrieved from session options.
+      def self.cookie_values
+        Rails.configuration.session_options.slice(:path, :domain, :secure)
+      end
+    
+          # Set up a subject doing an I18n lookup. At first, it attempts to set a subject
+      # based on the current mapping:
+      #
+      #   en:
+      #     devise:
+      #       mailer:
+      #         confirmation_instructions:
+      #           user_subject: '...'
+      #
+      # If one does not exist, it fallbacks to ActionMailer default:
+      #
+      #   en:
+      #     devise:
+      #       mailer:
+      #         confirmation_instructions:
+      #           subject: '...'
+      #
+      def subject_for(key)
+        I18n.t(:'#{devise_mapping.name}_subject', scope: [:devise, :mailer, key],
+          default: [:subject, key.to_s.humanize])
+      end
+    end
+  end
+end
+
+    
+    module Devise
+  module Models
+    # Timeoutable takes care of verifying whether a user session has already
+    # expired or not. When a session expires after the configured time, the user
+    # will be asked for credentials again, it means, they will be redirected
+    # to the sign in page.
+    #
+    # == Options
+    #
+    # Timeoutable adds the following options to devise_for:
+    #
+    #   * +timeout_in+: the interval to timeout the user session without activity.
+    #
+    # == Examples
+    #
+    #   user.timedout?(30.minutes.ago)
+    #
+    module Timeoutable
+      extend ActiveSupport::Concern
+    
+            # This is called as a last-minute hook that allows the configuration
+        # object to finalize itself before it will be put into use. This is
+        # a useful place to do some defaults in the case the user didn't
+        # configure something or so on.
+        #
+        # An example of where this sort of thing is used or has been used:
+        # the 'vm' configuration key uses this to make sure that at least
+        # one sub-VM has been defined: the default VM.
+        #
+        # The configuration object is expected to mutate itself.
+        def finalize!
+          # Default implementation is to do nothing.
+        end
+    
+            include Vagrant::Util
+    
+            # This is called early, before a machine is instantiated, to check
+        # if this provider is installed. This should return true or false.
+        #
+        # If the provider is not installed and Vagrant determines it is
+        # able to install this provider, then it will do so. Installation
+        # is done by calling Environment.install_provider.
+        #
+        # If Environment.can_install_provider? returns false, then an error
+        # will be shown to the user.
+        def self.installed?
+          # By default return true for backwards compat so all providers
+          # continue to work.
+          true
+        end
+    
+      def maxwidth_or_default
+    (params[:maxwidth].presence || 400).to_i
+  end
+    
+        render_empty
+  end
+    
+      def set_locale
+    I18n.locale = default_locale
+    I18n.locale = current_user.locale if user_signed_in?
+  rescue I18n::InvalidLocale
+    I18n.locale = default_locale
+  end
+    
+    
+    
+        <div id='backtrace' class='condensed'>
+      <h3>BACKTRACE</h3>
+      <p><a href='#' id='expando'
+            onclick='toggleBacktrace(); return false'>(expand)</a></p>
+      <p id='nav'><strong>JUMP TO:</strong>
+         <% unless bad_request?(exception) %>
+            <a href='#get-info'>GET</a>
+            <a href='#post-info'>POST</a>
+         <% end %>
+         <a href='#cookie-info'>COOKIES</a>
+         <a href='#env-info'>ENV</a>
+      </p>
+      <div class='clear'></div>
+    
+          def session_key
+        @session_key ||= options[:session_key]
+      end
+    end
+  end
+end
+
+    
+            if @javascript and not @escaper.respond_to? :escape_javascript
+          fail('Use EscapeUtils for JavaScript escaping.')
+        end
+      end
+    
+    # component must be String
+def encodeURIComponent(componentString)
+  ::URI::URIEncodeComponent(componentString)
+end
+    
+      teardown do
+    FileUtils.rm_r(File.join(File.dirname(__FILE__), *%w[examples test.git]))
+  end
+    
+    #############################################################################
+#
+# Custom tasks (add your own tasks here)
+#
+#############################################################################
+    
+    # Read command line options into `options` hash
+begin
+  opts.parse!
+rescue OptionParser::InvalidOption
+  puts 'gollum: #{$!.message}'
+  puts 'gollum: try 'gollum --help' for more information'
+  exit
+end
+    
+      def self.assets_path
+    ::File.expand_path('gollum/public', ::File.dirname(__FILE__))
+  end
+    
+      config.vm.define :smartos do |smartos|
+    smartos.vm.box = 'smartos-base1310-64-virtualbox-20130806.box'
+    smartos.vm.box_url = 'http://dlc-int.openindiana.org/aszeszo/vagrant/smartos-base1310-64-virtualbox-20130806.box'
+  end
+    
+        rc_args = []
+    
+      # Raised if safesystem cannot find the program to run.
+  class ExecutableNotFound < StandardError; end
