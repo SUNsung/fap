f92680d42f84e4c3c3e7acb759b5ba0c144b2522
SUNsung <weatherius@gmail.com>
2019-03-24 01:11:24 +0200

    Rewrite [25BgHVCbSWa4.25IcC0NWOe5Ok250LdIqINRjTs25fd9.cpp] File in: 2019-03-24 01:11:24

--- cccccc/25BgHVCbSWa4.25IcC0NWOe5Ok250LdIqINRjTs25fd9.cpp
+++ cccccc/25BgHVCbSWa4.25IcC0NWOe5Ok250LdIqINRjTs25fd9.cpp
@@ -1,272 +1,238 @@
 
         
-        REGISTER_OP('Invalid')
-    .Attr('invalid attr: int32')  // invalid since the name has a space.
-    .Doc(R'doc(
-An op to test that invalid ops do not successfully generate invalid python code.
-)doc');
+        using namespace internal;
     
-    // Register the bfloat16 numpy type.
-void RegisterNumpyBfloat16();
-    
-    #include 'tensorflow/c/c_api.h'
-#include 'tensorflow/core/lib/core/errors.h'
-#include 'tensorflow/core/platform/mutex.h'
-#include 'tensorflow/python/lib/core/bfloat16.h'
-#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
-    
-    #include 'tensorflow/c/c_api.h'
-#include 'tensorflow/core/framework/types.pb.h'
-#include 'tensorflow/core/lib/core/status.h'
-    
-    
-    {}  // namespace tensorflow
-
-    
-    class RandomAccessFile;
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#include 'tensorflow/python/util/kernel_registry.h'
-    
-    port::StatusOr<StreamExecutor*> ExecutorCache::Get(
-    const StreamExecutorConfig& config) {
-  Entry* entry = nullptr;
-  {
-    tf_shared_lock lock{mutex_};
-    auto it = cache_.find(config.ordinal);
-    if (it != cache_.end()) {
-      entry = &it->second;
-    } else {
-      return port::Status(port::error::NOT_FOUND,
-                          port::Printf('No executors registered for ordinal %d',
-                                       config.ordinal));
-    }
-  }
-  tf_shared_lock lock{entry->configurations_mutex};
-  if (entry->configurations.empty()) {
-    return port::Status(
-        port::error::NOT_FOUND,
-        port::Printf('No executors registered for ordinal %d', config.ordinal));
-  }
-  for (const auto& iter : entry->configurations) {
-    if (iter.first.plugin_config == config.plugin_config &&
-        iter.first.device_options == config.device_options) {
-      VLOG(2) << 'hit in cache for device ordinal ' << config.ordinal;
-      return iter.second.get();
-    }
-  }
-  return port::Status(port::error::NOT_FOUND,
-                      'No executor found with a matching config.');
-}
-    
-      // Returns a pointer to the described executor (if one with a matching config
-  // has been created), or a NOT_FOUND status.
-  port::StatusOr<StreamExecutor*> Get(const StreamExecutorConfig& config);
-    
-    // Tell browser to delete a object.
-// function DeallocateObject(id);
-v8::Handle<v8::Value> DeallocateObject(int routing_id,
-                                       int object_id);
-    
-    #include 'content/nw/src/api/clipboard/clipboard.h'
-    
-    bool NwMenuModel::HasIcons() const {
-  // Always return false, see the comment about |NwMenuModel|.
-  return false;
-}
-    
-    ui::KeyboardCode GetKeycodeFromText(std::string text){
-  ui::KeyboardCode retval = ui::VKEY_UNKNOWN;
-  if (text.size() != 0){
-    std::string upperText = base::ToUpperASCII(text);
-    std::string keyName = text;
-    bool found = false;
-    if (upperText.size() == 1){
-      char key = upperText[0];
-      if (key>='0' && key<='9'){//handle digital
-        keyName = 'Digit' + upperText;
-        found = true;
-      } else if (key>='A'&&key<='Z'){//handle alphabet
-        keyName = 'Key' + upperText;
-        found = true;
-      }
-    }
-    }
-    }
-    
-      if (type == 'separator') {
-    menu_item_ = gtk_separator_menu_item_new();
-  } else {
-    if (type == 'checkbox') {
-      menu_item_ = gtk_check_menu_item_new();
-      bool checked;
-      if (option.GetBoolean('checked', &checked))
-        SetChecked(checked);
-    } else {
-      menu_item_ = gtk_image_menu_item_new();
-      std::string icon;
-      if (option.GetString('icon', &icon))
-        SetIcon(icon);
-    }
-    }
-    
-      static void DoJob(AppWindowRegistry* registry, std::string id);
- protected:
-  ~NwAppCloseAllWindowsFunction() override {}
-    
-    bool NwClipboardGetListSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  std::unique_ptr<GetListSync::Params> params(GetListSync::Params::Create(*args_));
-  EXTENSION_FUNCTION_VALIDATE(params.get());
-  std::unique_ptr<ClipboardReader> reader(new ClipboardReader());
-    }
-    
-     protected:
-  ~NwClipboardGetListSyncFunction() override;
-    
-    class NwObjCreateFunction : public NWSyncExtensionFunction {
- public:
-  NwObjCreateFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-        webrtc::DesktopCaptureOptions options = webrtc::DesktopCaptureOptions::CreateDefault();
-    options.set_disable_effects(false);
-    
-    
-    {
-    {        throw invalid_argument('the input has no solution');
-    }
-};
-    
-                int complement = target - nums[i];
-            if(record.find(complement) != record.end()){
-                int res[] = {i, record[complement]};
-                return vector<int>(res, res + 2);
-            }
-    
-    
-    {
-    {        return true;
-    }
-};
-    
-        cout << endl;
-    
-            ListNode* dummyHead1 = new ListNode(-1);
-        ListNode* dummyHead2 = new ListNode(-1);
-        ListNode* prev1 = dummyHead1;
-        ListNode* prev2 = dummyHead2;
-    
-    
-    {
-    {
-    {            if(command.s == 'print')
-                res.push_back(command.node->val);
-            else{
-                assert(command.s == 'go');
-                if(command.node->right)
-                    stack.push(Command('go',command.node->right));
-                stack.push(Command('print', command.node));
-                if(command.node->left)
-                    stack.push(Command('go',command.node->left));
-            }
-        }
-        return res;
-    }
-};
-    
-    
-    {
-    {        return res;
-    }
-};
-    
-    
-    {    return 0;
-}
-
-    
-            vector<int> res;
-        if(root == NULL)
-            return res;
-    
-            stack<TreeNode*> stack, output;
-    
-    CatmullRomTo* CatmullRomTo::clone() const
+    void bitwiseOr(const Size2D &size,
+               const u8 *src0Base, ptrdiff_t src0Stride,
+               const u8 *src1Base, ptrdiff_t src1Stride,
+               u8 *dstBase, ptrdiff_t dstStride)
 {
-    // no copy constructor
-    auto a = new (std::nothrow) CatmullRomTo();
-    a->initWithDuration(this->_duration, this->_points->clone());
-    a->autorelease();
-    return a;
-}
-    
-    GridBase* Grid3DAction::getGrid()
-{
-    return Grid3D::create(_gridSize, _gridNodeTarget->getGridRect());
-}
-    
-        // On WP8 for variable argument lists longer than 10 items, use the other create functions or variadicCreate with NULL as the last argument
-    static Sequence* variadicCreate(FiniteTimeAction* item, ...);
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride, BitwiseOr());
 #else
-    static Sequence* create(FiniteTimeAction *action1, ...) CC_REQUIRES_NULL_TERMINATION;
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
 #endif
-    
-    ActionTween* ActionTween::create(float duration, const std::string& key, float from, float to)
-{
-    ActionTween* ret = new (std::nothrow) ActionTween();
-    if (ret && ret->initWithDuration(duration, key, from, to))
-    {
-        ret->autorelease();
-        return ret;
-    }
-    
-    delete ret;
-    return nullptr;
 }
     
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __CCACTIONTWEEN_H__
-#define __CCACTIONTWEEN_H__
+    #ifndef __ANDROID__
+        for (; dj < roiw32; sj += 96, dj += 32)
+        {
+            internal::prefetch(src + sj);
+    }
     
-    NS_CC_END
+        for (size_t i = 0u; i < size.height; ++i)
+    {
+        const u8 * srcy = internal::getRowPtr(srcyBase, srcyStride, i);
+        const u8 * srcu = internal::getRowPtr(srcuBase, srcuStride, i);
+        const u8 * srcv = internal::getRowPtr(srcvBase, srcvStride, i);
+        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t syj = 0u, sj = 0u, dj = 0u;
+    }
+    
+    ptrdiff_t borderInterpolate(ptrdiff_t _p, size_t _len, BORDER_MODE borderType, size_t startMargin, size_t endMargin)
+{
+    ptrdiff_t p = _p + (ptrdiff_t)startMargin;
+    size_t len = _len + startMargin + endMargin;
+    if( (size_t)p < len )
+        return _p;
+    else if( borderType == BORDER_MODE_REPLICATE )
+        p = p < 0 ? 0 : (ptrdiff_t)len - 1;
+    else if( borderType == BORDER_MODE_REFLECT || borderType == BORDER_MODE_REFLECT101 )
+    {
+        s32 delta = borderType == BORDER_MODE_REFLECT101;
+        if( len == 1 )
+            return 0;
+        do
+        {
+            if( p < 0 )
+                p = -p - 1 + delta;
+            else
+                p = (ptrdiff_t)len - 1 - (p - (ptrdiff_t)len) - delta;
+        }
+        while( (size_t)p >= len );
+    }
+    else if( borderType == BORDER_MODE_WRAP )
+    {
+        if( p < 0 )
+            p -= ((p-(ptrdiff_t)len+1)/(ptrdiff_t)len)*(ptrdiff_t)len;
+        if( p >= (ptrdiff_t)len )
+            p %= (ptrdiff_t)len;
+    }
+    else if( borderType == BORDER_MODE_CONSTANT )
+        p = -1;
+    else
+        internal::assertSupportedConfiguration(false);
+    return p - (ptrdiff_t)startMargin;
+}
+    
+        for (size_t i = 0; i < size.height; ++i)
+    {
+        const s16 * src = internal::getRowPtr(srcBase, srcStride, i);
+        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t j = 0;
+    }
+    
+    
+    {            float32x2_t vres = vpadd_f32(vget_low_f32(v_sum),vget_high_f32(v_sum));
+            result += vget_lane_f32(vres, 0) + vget_lane_f32(vres, 1);
+        }
+    
+            uint16x8_t el8shr0 = vmull_u8(vsrc, vsrc);
+        uint16x8_t el8shr1 = vextq_u16(v_zero8, el8shr0, 7);
+    
+    inline float32x2_t vrecp_f32(float32x2_t val)
+{
+    float32x2_t reciprocal = vrecpe_f32(val);
+    reciprocal = vmul_f32(vrecps_f32(val, reciprocal), reciprocal);
+    reciprocal = vmul_f32(vrecps_f32(val, reciprocal), reciprocal);
+    return reciprocal;
+}
+    
+                    if (border == BORDER_MODE_CONSTANT && x4 < 0)
+                    prevx = borderValue;
+                else
+                    prevx = (srow2 ? srow2[x4] : borderValue) + srow1[x4] + (srow0 ? srow0[x4] : borderValue);
+    
+    
+    {  TBLOB* blob;
+  Tesseract* tesseract;
+  BLOB_CHOICE_LIST** choices;
+};
+    
+    #endif  // TESSERACT_CCMAIN_PARAGRAPHS_H_
 
     
-        /** Gets the total Delay units of the Animation. 
-     *
-     * @return The total Delay units of the Animation.
-     */
-    float getTotalDelayUnits() const { return _totalDelayUnits; };
+    // ReadNextBox factors out the code to interpret a line of a box
+// file so that applybox and unicharset_extractor interpret the same way.
+// This function returns the next valid box file utf8 string and coords
+// and returns true, or false on eof (and closes the file).
+// It ignores the utf8 file signature ByteOrderMark (U+FEFF=EF BB BF), checks
+// for valid utf-8 and allows space or tab between fields.
+// utf8_str is set with the unichar string, and bounding box with the box.
+// If there are page numbers in the file, it reads them all.
+bool ReadNextBox(int *line_number, FILE* box_file,
+                 STRING* utf8_str, TBOX* bounding_box);
+// As ReadNextBox above, but get a specific page number. (0-based)
+// Use -1 to read any page number. Files without page number all
+// read as if they are page 0.
+bool ReadNextBox(int target_page, int *line_number, FILE* box_file,
+                 STRING* utf8_str, TBOX* bounding_box);
     
-    /** Sets the delay in seconds of the 'delay unit'.
-     *
-     * @param delayPerUnit The delay in seconds of the 'delay unit'.
-     */
-    void setDelayPerUnit(float delayPerUnit) { _delayPerUnit = delayPerUnit; };
+      // Sets the destination filename and enables images to be written to a PDF
+  // on destruction.
+  void WritePDF(const char* filename) {
+    if (pixaGetCount(pixa_) > 0) {
+      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
+      pixaClear(pixa_);
+    }
+  }
     
-    /** Gets the delay in seconds of the 'delay unit'.
-     * 
-     * @return The delay in seconds of the 'delay unit'.
-     */
-    float getDelayPerUnit() const { return _delayPerUnit; };
     
-    NS_CC_END
+    {  int delta = this - prev;
+  int32_t n = prev->n_ + 1;
+  int32_t sig_x = prev->sig_x_ + delta;
+  int64_t sig_xsq = prev->sig_xsq_ + delta * delta;
+  int64_t cost = (sig_xsq - sig_x * sig_x / n) / n;
+  cost += prev->total_cost_;
+  UpdateIfBetter(cost, prev->total_steps_ + 1, prev, n, sig_x, sig_xsq);
+  return cost;
+}
     
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+      // Returns the direction of the fitted line as a unit vector, using the
+  // least mean squared perpendicular distance. The line runs through the
+  // mean_point, i.e. a point p on the line is given by:
+  // p = mean_point() + lambda * vector_fit() for some real number lambda.
+  // Note that the result (0<=x<=1, -1<=y<=-1) is directionally ambiguous
+  // and may be negated without changing its meaning, since a line is only
+  // unique to a range of pi radians.
+  // Modernists prefer to think of this as an Eigenvalue problem, but
+  // Pearson had the simple solution in 1901.
+  //
+  // Note that this is equivalent to returning the Principal Component in PCA,
+  // or the eigenvector corresponding to the largest eigenvalue in the
+  // covariance matrix.
+  FCOORD vector_fit() const;
+    
+    namespace dmlc {
+namespace data {
+    }
+    }
+    
+    // implementing configure.
+template<typename PairIter>
+inline void ObjFunction::Configure(PairIter begin, PairIter end) {
+  std::vector<std::pair<std::string, std::string> > vec(begin, end);
+  this->Configure(vec);
+}
+    
+      void Write(const SparsePage& page, dmlc::Stream* fo) override {
+    const auto& offset_vec = page.offset.HostVector();
+    const auto& data_vec = page.data.HostVector();
+    CHECK(offset_vec.size() != 0 && offset_vec[0] == 0);
+    CHECK_EQ(offset_vec.back(), data_vec.size());
+    fo->Write(offset_vec);
+    min_index_ = page.base_rowid;
+    fo->Write(&min_index_, sizeof(min_index_));
+    index_.data.resize(data_vec.size());
+    value_.data.resize(data_vec.size());
+    }
+    
+    XGBOOST_REGISTER_OBJECTIVE(LambdaRankObjMAP, 'rank:map')
+.describe('LambdaRank with MAP as objective.')
+.set_body([]() { return new LambdaRankObjMAP(); });
+    
+        // Remove the assertion on batch.index, which can be null in the case that the data in this
+    // batch is entirely sparse. Although it's true that this indicates a likely issue with the
+    // user's data workflows, passing XGBoost entirely sparse data should not cause it to fail.
+    // See https://github.com/dmlc/xgboost/issues/1827 for complete detail.
+    // CHECK(batch.index != nullptr);
+    
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+    
+    IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_Init();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data);
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+        // Setup viewport
+    D3DVIEWPORT9 vp;
+    vp.X = vp.Y = 0;
+    vp.Width = (DWORD)draw_data->DisplaySize.x;
+    vp.Height = (DWORD)draw_data->DisplaySize.y;
+    vp.MinZ = 0.0f;
+    vp.MaxZ = 1.0f;
+    g_pd3dDevice->SetViewport(&vp);
+    
+    namespace CalculatorApp
+{
+    [Windows::Foundation::Metadata::WebHostHidden]
+    public ref class CalculatorProgrammerDisplayPanel sealed
+    {
+    public:
+        CalculatorProgrammerDisplayPanel();
+    }
+    }
+    
+    //
+// CalculatorScientificAngleButtons.xaml.h
+// Declaration of the CalculatorScientificAngleButtons class
+//
