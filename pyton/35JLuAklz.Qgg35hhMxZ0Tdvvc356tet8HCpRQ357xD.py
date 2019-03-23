4119196ebe4d43a4088ad0ebde82e9265603d71b
SUNsung <weatherius@gmail.com>
2019-03-23 03:39:53 +0200

    Rewrite [35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py] File in: 2019-03-23 03:39:53

--- pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
+++ pyton/35JLuAklz.Qgg35hhMxZ0Tdvvc356tet8HCpRQ357xD.py
@@ -1,127 +1,87 @@
 
         
-        
-class HStoreExtension(CreateExtension):
+            '''
+    is_windows = is_windows
+    config_dir = DEFAULT_CONFIG_DIR
+    stdin = sys.stdin
+    stdin_isatty = stdin.isatty()
+    stdin_encoding = None
+    stdout = sys.stdout
+    stdout_isatty = stdout.isatty()
+    stdout_encoding = None
+    stderr = sys.stderr
+    stderr_isatty = stderr.isatty()
+    colors = 256
+    if not is_windows:
+        if curses:
+            try:
+                curses.setupterm()
+                colors = curses.tigetnum('colors')
+            except curses.error:
+                pass
+    else:
+        # noinspection PyUnresolvedReferences
+        import colorama.initialise
+        stdout = colorama.initialise.wrap_stream(
+            stdout, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        stderr = colorama.initialise.wrap_stream(
+            stderr, convert=None, strip=None,
+            autoreset=True, wrap=True
+        )
+        del colorama
+    
+            request_line = '{method} {path}{query} HTTP/1.1'.format(
+            method=self._orig.method,
+            path=url.path or '/',
+            query='?' + url.query if url.query else ''
+        )
+    
+        Suitable regular HTTP responses.
+    
+        def convert(self, content_bytes):
+        raise NotImplementedError
     
         try:
-        citext_oids = get_citext_oids(connection.alias)
-        array_type = psycopg2.extensions.new_array_type(citext_oids, 'citext[]', psycopg2.STRING)
-        psycopg2.extensions.register_type(array_type, None)
-    except ProgrammingError:
-        # citext is not available on the database.
-        #
-        # The same comments in the except block of the above call to
-        # register_hstore() also apply here.
-        pass
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            USERNAME,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
 
     
-        def __eq__(self, other):
-        return (
-            isinstance(other, self.__class__) and
-            self.keys == other.keys and
-            self.messages == other.messages and
-            self.strict == other.strict
-        )
+    known_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
     
-        def save(self, must_create=False):
-        if self.session_key is None:
-            return self.create()
-        if must_create:
-            func = self._cache.add
-        elif self._cache.get(self.cache_key) is not None:
-            func = self._cache.set
-        else:
-            raise UpdateError
-        result = func(self.cache_key,
-                      self._get_session(no_load=must_create),
-                      self.get_expiry_age())
-        if must_create and not result:
-            raise CreateError
-    
-        publicKey = (keySize, e_1, e_2, p)
-    privateKey = (keySize, d)
-    
-            if row1 == row2:
-            plaintext += table[row1*5+(col1-1)%5]
-            plaintext += table[row2*5+(col2-1)%5]
-        elif col1 == col2:
-            plaintext += table[((row1-1)%5)*5+col1]
-            plaintext += table[((row2-1)%5)*5+col2]
-        else: # rectangle
-            plaintext += table[row1*5+col2]
-            plaintext += table[row2*5+col1]
-    
-        for i in range(1, n+1):
-        for j in range(1, s+1):
-            dp[i][j]= dp[i][j-1]
-    
-        score = np.sqrt(mean_square_diff)
-    
-    
-def b_expo_mod(a, b, c):
-    res = 1
-    while b > 0:
-        if b&1:
-            res = ((res%c) * (a%c)) % c
-    
-        # Perform mask detection on the horizontally flipped image
-    if cfg.TEST.MASK_AUG.H_FLIP:
-        masks_hf = im_detect_mask_hflip(
-            model, im, cfg.TEST.SCALE, cfg.TEST.MAX_SIZE, boxes
-        )
-        masks_ts.append(masks_hf)
-    
-    # mapping coco categories to cityscapes (our converted json) id
-# cityscapes
-# INFO roidb.py: 220: 1       bicycle: 7286
-# INFO roidb.py: 220: 2           car: 53684
-# INFO roidb.py: 220: 3        person: 35704
-# INFO roidb.py: 220: 4         train: 336
-# INFO roidb.py: 220: 5         truck: 964
-# INFO roidb.py: 220: 6    motorcycle: 1468
-# INFO roidb.py: 220: 7           bus: 758
-# INFO roidb.py: 220: 8         rider: 3504
-    
-    from detectron.core.config import cfg
-from detectron.modeling.generate_anchors import generate_anchors
-from detectron.utils.c2 import const_fill
-from detectron.utils.c2 import gauss_fill
-import detectron.modeling.FPN as FPN
-import detectron.utils.blob as blob_utils
-    
-        # Base Fast R-CNN blobs
-    blob_dict = dict(
-        labels_int32=sampled_labels.astype(np.int32, copy=False),
-        rois=sampled_rois,
-        bbox_targets=bbox_targets,
-        bbox_inside_weights=bbox_inside_weights,
-        bbox_outside_weights=bbox_outside_weights
-    )
-    
-    
-def get_minibatch_blob_names(is_training=True):
-    '''Return blob names in the order in which they are read by the data loader.
+        :param faces: List of face encodings to compare
+    :param face_to_compare: A face encoding to compare against
+    :return: A numpy ndarray with the distance for each face in the same order as the 'faces' array
     '''
-    # data blob: holds a batch of N images, each with 3 channels
-    blob_names = ['data']
-    if cfg.RPN.RPN_ON:
-        # RPN-only or end-to-end Faster R-CNN
-        blob_names += rpn_roi_data.get_rpn_blob_names(is_training=is_training)
-    elif cfg.RETINANET.RETINANET_ON:
-        blob_names += retinanet_roi_data.get_retinanet_blob_names(
-            is_training=is_training
-        )
-    else:
-        # Fast R-CNN like models trained on precomputed proposals
-        blob_names += fast_rcnn_roi_data.get_fast_rcnn_blob_names(
-            is_training=is_training
-        )
-    return blob_names
+    if len(face_encodings) == 0:
+        return np.empty((0))
     
-        if opts.profiler:
-        import cProfile
-        cProfile.runctx(
-            'loader_loop(roi_data_loader)', globals(), locals(),
-            sort='cumulative')
-    else:
-        loader_loop(roi_data_loader)
+        # Write the resulting image to the output video file
+    print('Writing frame {} / {}'.format(frame_number, length))
+    output_movie.write(frame)
+    
+            # Draw a box around the face
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+    
+    # Get a reference to the Raspberry Pi camera.
+# If this fails, make sure you have a camera connected to the RPi and that you
+# enabled your camera in raspi-config and rebooted first.
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
+    
+    # 提示：运行本案例需要安装Flask，你可以用下面的代码安装Flask
+# $ pip3 install flask
