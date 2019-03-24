8a85ca47d8939ec545916aac365e8aef0a3fd690
SUNsung <weatherius@gmail.com>
2019-03-24 15:54:20 +0200

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2019-03-24 15:54:19

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,119 +1,307 @@
 
         
-        REGISTER_GRADIENT(Sub, GetSubGradient);
-    
-              const TensorShape& X = in[0];
-          int N = 0, C = 0, H = 0, W = 0;
-          switch (order) {
-            case StorageOrder::NCHW:
-              N = X.dims(0);
-              C = X.dims(1);
-              H = X.dims(2);
-              W = X.dims(3);
-              break;
-            case StorageOrder::NHWC:
-              N = X.dims(0);
-              H = X.dims(1);
-              W = X.dims(2);
-              C = X.dims(3);
-              break;
-            default:
-              CAFFE_THROW('Unknown storage order: ', order);
-          }
-    
-    namespace mxnet {
-namespace rtc {
+        namespace swig {
+namespace {
     }
     }
     
-    // Initialization funciton called by caffeOp & caffeLoss
-template<typename Dtype>
-void InitCaffeBlobs(std::vector< ::caffe::Blob<Dtype>*>* v, int n_num) {
-  for (index_t i=0; i < n_num; ++i)
-    v->push_back(new ::caffe::Blob<Dtype>());
-}
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    /*!
- * Copyright (c) 2015 by Contributors
- * \file caffe_data_iter.cc
- * \brief register mnist iterator
-*/
-#include <sys/time.h>
-#include <caffe/proto/caffe.pb.h>
-#include <dmlc/parameter.h>
-#include <atomic>
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-      for (uint32_t nid = 0; nid < idx.num_nodes(); ++nid) {
-    const auto& inode = idx[nid];
-    if (inode.source->op() != ewise_plus_op) continue;
-    int sid = storage_id[idx.entry_id(inode.inputs[0])];
-    if (sid != storage_id[idx.entry_id(nid, 0)]) continue;
-    if (idx[inode.inputs[0].node_id].source->is_variable()) continue;
-    if (idx[inode.inputs[1].node_id].source->is_variable()) continue;
-    uint32_t eid_rhs  = idx.entry_id(inode.inputs[1]);
-    if (ref_count[eid_rhs] != 1) continue;
-    if (inode.inputs[0].node_id >= inode.inputs[1].node_id) continue;
-    // TODO(haibin) support inplace addto for Dynamic Storage
-    if (storage_id[eid_rhs] == kDynamicStorageID) continue;
-    CHECK_NE(storage_id[eid_rhs], sid);
-    storage_id[eid_rhs] = sid;
-    addto_entry[eid_rhs] = 1;
-    storage_inplace_index[eid_rhs] = -1;
-    skip_plus_node[nid] = 1;
-  }
+    #include 'tensorflow/python/grappler/model_analyzer.h'
     
-    namespace mxnet {
-    }
+    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
+// a copy.
+Status TensorToNdarray(const Tensor& t, PyObject** ret);
+    
+    #ifndef PyObject_HEAD
+struct _object;
+typedef _object PyObject;
+#endif
+    
+    #ifndef TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_PLATFORM_ID_H_
+#define TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_PLATFORM_ID_H_
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
     
-// Wrap a TVM function to a function that invokes MXNet's Engine
-// It does two things: call the engine properly
-// set up the NDArray to DLTensor during invocation.
-void WrapAsyncCall(TVMArgs wrap_args, TVMRetValue* wrap_rv) {
-  PackedFunc f = wrap_args[0];
-  PackedFunc fset_stream =  wrap_args[1];
-  int num_const = wrap_args[2];
-    }
-    
-    /*!
- * Copyright (c) 2015 by Contributors
- * \file native_op.cc
- * \brief
- * \author Junyuan Xie
-*/
-#include './native_op-inl.h'
-    
-    void DHTRoutingTableSerializer::setLocalNode(
-    const std::shared_ptr<DHTNode>& localNode)
-{
-  localNode_ = localNode;
-}
-    
-    #include 'common.h'
-    
-      virtual std::shared_ptr<DHTTask>
-  createPeerAnnounceTask(const unsigned char* infoHash) = 0;
-    
-    #include <cstring>
-    
-    
-    {} // namespace aria2
+    {  return Begin + oldSize;
+}  
 
     
-    const std::string& DNSCache::find(const std::string& hostname,
-                                  uint16_t port) const
-{
-  auto target = std::make_shared<CacheEntry>(hostname, port);
-  auto i = entries_.find(target);
-  if (i == entries_.end()) {
-    return A2STR::NIL;
-  }
-  else {
-    return (*i)->getGoodAddr();
-  }
+    namespace {
+  class ClangDiagRenderer final : public clang::DiagnosticNoteRenderer {
+    const llvm::function_ref<void(clang::FullSourceLoc,
+                                  clang::DiagnosticsEngine::Level,
+                                  StringRef)> callback;
+    }
+    }
+    
+    
+    {  llvm_unreachable('invalid class');
 }
     
-      typedef std::set<std::shared_ptr<CacheEntry>,
-                   DerefLess<std::shared_ptr<CacheEntry>>>
-      CacheEntrySet;
-  CacheEntrySet entries_;
+    class LLVM_LIBRARY_VISIBILITY GenericUnix : public ToolChain {
+protected:
+  InvocationInfo constructInvocation(const InterpretJobAction &job,
+                                     const JobContext &context) const override;
+  InvocationInfo constructInvocation(const AutolinkExtractJobAction &job,
+                                     const JobContext &context) const override;
+    }
+    
+    #endif // BITCOIN_QT_NETWORKSTYLE_H
+
+    
+            friend void vformat(std::ostream& out, const char* fmt,
+                            const FormatList& list);
+    
+    #define COMBINE64(sgn,n) void combine##n(const Size2D &_size                                                \
+                                               FILL_LINES##n(FARG, sgn##64),                                \
+                                               sgn##64 * dstBase, ptrdiff_t dstStride)                      \
+{                                                                                                           \
+    internal::assertSupportedConfiguration();                                                               \
+    Size2D size(_size);                                                                                     \
+    if (CONTSRC##n                                                                                          \
+        dstStride == (ptrdiff_t)(size.width))                                                               \
+    {                                                                                                       \
+        size.width *= size.height;                                                                          \
+        size.height = 1;                                                                                    \
+    }                                                                                                       \
+    typedef internal::VecTraits<sgn##64, n>::vec64 vec64;                                                   \
+                                                                                                            \
+    for (size_t i = 0u; i < size.height; ++i)                                                               \
+    {                                                                                                       \
+        FILL_LINES##n(VROW, sgn##64)                                                                        \
+        sgn##64 * dst = internal::getRowPtr(dstBase, dstStride, i);                                         \
+        size_t sj = 0u, dj = 0u;                                                                            \
+                                                                                                            \
+        for (; sj < size.width; ++sj, dj += n)                                                              \
+        {                                                                                                   \
+            vec64 v_dst;                                                                                    \
+            FILL_LINES##n(VLD1, sgn##64)                                                                    \
+            vst##n##_##sgn##64(dst + dj, v_dst);                                                            \
+            /*FILL_LINES##n(SLD, sgn##64)*/                                                                 \
+        }                                                                                                   \
+    }                                                                                                       \
+}
+    
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
+CVT_FUNC(s32, f32, 8,
+,
+{
+     for (size_t i = 0; i < w; i += 8)
+     {
+         internal::prefetch(_src + i);
+         __asm__ (
+             'vld1.32 {d0-d1}, [%[src]]                              \n\t'
+             'vcvt.f32.s32 q1, q0                                    \n\t'
+             'vst1.32 {d2-d3}, [%[dst]]                              \n\t'
+             : /*no output*/
+             : [src] 'r' (_src + i),
+               [dst] 'r' (_dst + i)
+             : 'd0','d1','d2','d3'//,'d4','d5'
+         );
+         __asm__ (
+             'vld1.32 {d0-d1}, [%[src]]                              \n\t'
+             'vcvt.f32.s32 q1, q0                                    \n\t'
+             'vst1.32 {d2-d3}, [%[dst]]                              \n\t'
+             : /*no output*/
+             : [src] 'r' (_src + i + 4),
+               [dst] 'r' (_dst + i + 4)
+             : 'd0','d1','d2','d3'//,'d4','d5'
+         );
+     }
+})
+#else
+CVT_FUNC(s32, f32, 8,
+,
+{
+     for (size_t i = 0; i < w; i += 8)
+     {
+         internal::prefetch(_src + i);
+         int32x4_t vline_s32 = vld1q_s32(_src + i);
+         float32x4_t vline_f32 = vcvtq_f32_s32(vline_s32);
+         vst1q_f32(_dst + i, vline_f32);
+    }
+    }
+    
+            for ( ; x < width; ++x)
+        {
+            // make extrapolation for the last elements
+            if (x + 1 >= width)
+            {
+                if (border == BORDER_MODE_CONSTANT)
+                {
+                    nextx[0] = borderValue;
+                    nextx[1] = borderValue;
+                    nextx[2] = borderValue;
+                }
+                else if (border == BORDER_MODE_REPLICATE)
+                {
+                    nextx[0] = srow0[x];
+                    nextx[1] = srow1[x];
+                    nextx[2] = srow2[x];
+                }
+            }
+            else
+            {
+                nextx[0] = srow0 ? srow0[x + 1] : borderValue;
+                nextx[1] =         srow1[x + 1]              ;
+                nextx[2] = srow2 ? srow2[x + 1] : borderValue;
+            }
+    }
+    
+                uint32x4_t vs4 = vpaddlq_u16(vpaddlq_u8(vs));
+            uint32x2_t vs2 = vadd_u32(vget_low_u32(vs4), vget_high_u32(vs4));
+    
+        v0k8 = vextq_s16(d8_15, d16_23, 7);
+    ak8 = vminq_s16(ak8, v0k8);
+    bk8 = vmaxq_s16(bk8, v0k8);
+    
+    void OneofGenerator::GenerateClearFunctionDeclaration(io::Printer* printer) {
+  printer->Print(
+      variables_,
+      '/**\n'
+      ' * Clears whatever value was set for the oneof '$name$'.\n'
+      ' **/\n'
+      'void $owning_message_class$_Clear$capitalized_name$OneOfCase($owning_message_class$ *message);\n');
+}
+    
+    void MapLiteTestUtil::ModifyMapFields(unittest::TestMapLite* message) {
+  MapTestUtilImpl::ModifyMapFields<unittest::MapEnumLite,
+                                   unittest::MAP_ENUM_FOO_LITE>(message);
+}
+    
+    TEST(StatusOr, TestPointerAssignmentStatusOk) {
+  const int kI = 0;
+  StatusOr<const int*> source(&kI);
+  StatusOr<const int*> target;
+  target = source;
+  EXPECT_EQ(source.status(), target.status());
+  EXPECT_EQ(source.ValueOrDie(), target.ValueOrDie());
+}
+    
+    TEST(TemplateUtilTest, TestTemplateTypeEquals) {
+  // Check that the TemplateTypeEquals works correctly.
+  bool value = false;
+    }
+    
+    int main(int argc, char** argv) {
+  if (argc != 4) {
+    printf('This script converts the MNIST dataset to the leveldb format used\n'
+           'by caffe to train a siamese network.\n'
+           'Usage:\n'
+           '    convert_mnist_data input_image_file input_label_file '
+           'output_db_file\n'
+           'The MNIST dataset could be downloaded at\n'
+           '    http://yann.lecun.com/exdb/mnist/\n'
+           'You should gunzip them after downloading.\n');
+  } else {
+    google::InitGoogleLogging(argv[0]);
+    convert_dataset(argv[1], argv[2], argv[3]);
+  }
+  return 0;
+}
+#else
+int main(int argc, char** argv) {
+  LOG(FATAL) << 'This example requires LevelDB; compile with USE_LEVELDB.';
+}
+#endif  // USE_LEVELDB
+
+    
+      /** Will not return until the internal thread has exited. */
+  void StopInternalThread();
+    
+    #include <vector>
+    
+     protected:
+  // Helper functions that abstract away the column buffer and gemm arguments.
+  // The last argument in forward_cpu_gemm is so that we can skip the im2col if
+  // we just called weight_cpu_gemm with the same input.
+  void forward_cpu_gemm(const Dtype* input, const Dtype* weights,
+      Dtype* output, bool skip_im2col = false);
+  void forward_cpu_bias(Dtype* output, const Dtype* bias);
+  void backward_cpu_gemm(const Dtype* input, const Dtype* weights,
+      Dtype* output);
+  void weight_cpu_gemm(const Dtype* input, const Dtype* output, Dtype*
+      weights);
+  void backward_cpu_bias(Dtype* bias, const Dtype* input);
+    
+    
+    {  // extra temporarary variables is used to carry out sums/broadcasting
+  // using BLAS
+  Blob<Dtype> batch_sum_multiplier_;
+  Blob<Dtype> num_by_chans_;
+  Blob<Dtype> spatial_sum_multiplier_;
+};
+    
+    #include 'caffe/layers/loss_layer.hpp'
+    
+    
+    {  bool handles_setup_;
+  cudnnHandle_t             handle_;
+  cudnnTensorDescriptor_t bottom_desc_;
+  cudnnTensorDescriptor_t top_desc_;
+  cudnnActivationDescriptor_t activ_desc_;
+};
+#endif
+    
+    #endif  // CAFFE_CUDNN_SOFTMAX_LAYER_HPP_
+
+    
+    #include 'gtest/gtest.h'
+    
+    #include 'modules/drivers/canbus/sensor_gflags.h'
+    
+    
+    {  double ret = x * OBJECT_WIDTH_RES;
+  return ret;
+}
+    
+    unsigned int BaseMapMatrix::LoadBinary(unsigned char* buf) { return 0; }
+    
+    class SpeedLimitTest : public ::testing::Test {
+ public:
+  virtual void SetUp() {
+    speed_limit_.Clear();
+    for (int i = 0; i < 100; ++i) {
+      std::pair<double, double> sp;
+      sp.first = i * 1.0;
+      sp.second = (i % 2 == 0) ? 5.0 : 10.0;
+      speed_limit_.AppendSpeedLimit(sp.first, sp.second);
+    }
+  }
+    }
+    
+    TEST(TestPiecewiseLinearConstraint, add_second_derivative_boundary) {
+  PiecewiseLinearConstraint constraint(10, 0.1);
+  std::vector<uint32_t> index_list;
+  std::vector<double> lower_bound;
+  std::vector<double> upper_bound;
+  for (uint32_t i = 0; i < 10; ++i) {
+    index_list.push_back(i);
+    lower_bound.push_back(-4.0);
+    upper_bound.push_back(2.0);
+  }
+    }
+    
+    TEST_F(ObstacleClustersTest, ObstacleClusters) {
+  auto lane = PredictionMap::LaneById('l9');
+  double start_s = 99.0;
+  double length = 100.0;
+    }
+    
+    #include 'modules/canbus/vehicle/gem/gem_message_manager.h'
+    
+    
+    {  double ret = x * 0.001000;
+  return ret;
+}
