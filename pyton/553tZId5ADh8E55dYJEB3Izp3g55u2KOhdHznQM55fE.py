12f2fdb4bf8d32a4550dbb76ad4eaea1afd10063
SUNsung <weatherius@gmail.com>
2019-03-24 03:54:25 +0200

    Rewrite [553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py] File in: 2019-03-24 03:54:24

--- pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
+++ pyton/553tZId5ADh8E55dYJEB3Izp3g55u2KOhdHznQM55fE.py
@@ -1,53 +1,21 @@
 
         
-        
-def test_regularization_shared_layer():
-    dense_layer = Dense(num_classes,
-                        kernel_regularizer=regularizers.l1(),
-                        activity_regularizer=regularizers.l1())
+                    if ret[i, 0] != i_label:  # 样本的类别发生了改变
+                changed = True
     
-    This script loads the s2s.h5 model saved by lstm_seq2seq.py and generates
-sequences from it.  It assumes that no changes have been made (for example:
-latent_dim is unchanged, and the input data and model architecture are unchanged).
     
-    # Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
-    
-    # Find all the faces in the image using a pre-trained convolutional neural network.
-# This method is more accurate than the default HOG model, but it's slower
-# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
-# this will use GPU acceleration and perform well.
-# See also: find_faces_in_picture.py
-face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
-    
-        face_found = False
-    is_obama = False
-    
-        :param known_face_encodings: A list of known face encodings
-    :param face_encoding_to_check: A single face encoding to compare against the list
-    :param tolerance: How much distance between faces to consider it a match. Lower is more strict. 0.6 is typical best performance.
-    :return: A list of True/False values indicating which known_face_encodings match the face encoding to check
+def relu6(x):
     '''
-    return list(face_distance(known_face_encodings, face_encoding_to_check) <= tolerance)
+    `o = min(max(x, 0), 6)`
+    '''
+    return tf.nn.relu6(x)
+    
+                # x = FlattenLayer(x)
+            x = DenseLayer(x, n_units=1, act=tf.nn.sigmoid)
+            o = x.outputs
+    
+            return self._call(x)
 
     
-    # This code finds all faces in a list of images using the CNN model.
-#
-# This demo is for the _special case_ when you need to find faces in LOTS of images very quickly and all the images
-# are the exact same size. This is common in video processing applications where you have lots of video frames
-# to process.
-#
-# If you are processing a lot of images and using a GPU with CUDA, batch processing can be ~3x faster then processing
-# single images at a time. But if you aren't using a GPU, then batch processing isn't going to be very helpful.
-#
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read the video file.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-    
-            for detected_faces in batched_detected_faces:
-            self.assertEqual(len(detected_faces), 1)
-            self.assertEqual(detected_faces[0].rect.top(), 154)
-            self.assertEqual(detected_faces[0].rect.bottom(), 390)
-    
-                with open('htmlout.html', 'w') as out:
-                out.write(header)
+            with tf.variable_scope('transform'):
+            W_T, b_T = get_wb([n_input, n_input], b_initializer=tf.initializers.constant(carry_bias))
