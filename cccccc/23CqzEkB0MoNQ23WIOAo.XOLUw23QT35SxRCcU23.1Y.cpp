83352aa8cf8bdc77589b4c6db88fb88cf6266df7
SUNsung <weatherius@gmail.com>
2019-03-25 16:10:45 +0200

    Rewrite [23CqzEkB0MoNQ23WIOAo.XOLUw23QT35SxRCcU23.1Y.cpp] File in: 2019-03-25 16:10:45

--- cccccc/23CqzEkB0MoNQ23WIOAo.XOLUw23QT35SxRCcU23.1Y.cpp
+++ cccccc/23CqzEkB0MoNQ23WIOAo.XOLUw23QT35SxRCcU23.1Y.cpp
@@ -1,366 +1,246 @@
 
         
-        REGISTER_OP('ShapelessOp');
+        )DOC')
+    .Input(0, 'X', '*(type: Tensor`<float>`)* Input tensor.')
+    .Output(0, 'Y', '*(type: Tensor`<float>`)* Output tensor.');
     
-    #include 'tensorflow/core/framework/tensor.h'
-#include 'tensorflow/core/lib/core/status.h'
     
-    #endif  // TENSORFLOW_PYTHON_LIB_CORE_PY_SEQ_TENSOR_H_
+    {          return out;
+        })
+    .Input(0, 'X', '4-tensor in NCHW or NHWC.')
+    .Output(
+        0,
+        'Y',
+        '4-tensor. For NCHW: N x (C x kH x kW) x outH x outW.'
+        'For NHWC: N x outH x outW x (kH x kW x C');
+    
+    namespace caffe2 {
+    }
+    
+      // Check if part from seed2 label: with low math density and left indented. We
+  // are using two checks:
+  // 1. If its left is aligned with any coordinates in indented_texts_left,
+  // which we assume have been sorted.
+  // 2. If its foreground density is over foreground_density_th.
+  bool CheckForSeed2(
+      const GenericVector<int>& indented_texts_left,
+      const float foreground_density_th,
+      ColPartition* part);
+    
+    #endif  // TESSERACT_CCSTRUCT_DEBUGPIXA_H_
 
     
-    namespace stream_executor {
-    }
-    
-    port::Status CUDAEvent::Init() {
-  return CUDADriver::CreateEvent(parent_->cuda_context(), &cuda_event_,
-                                 CUDADriver::EventFlags::kDisableTiming);
-}
-    
-    scoped_refptr<TracingController::TraceDataEndpoint> GetTraceDataEndpoint(
-    const base::FilePath& path,
-    const CompletionCallback& callback) {
-  base::FilePath result_file_path = path;
-  if (result_file_path.empty() && !base::CreateTemporaryFile(&result_file_path))
-    LOG(ERROR) << 'Creating temporary file failed';
-    }
-    
-    #include 'atom/browser/api/event_emitter.h'
-#include 'atom/browser/mac/in_app_purchase.h'
-#include 'atom/browser/mac/in_app_purchase_observer.h'
-#include 'atom/browser/mac/in_app_purchase_product.h'
-#include 'native_mate/handle.h'
-    
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-     private:
-  // Replyer for the synchronous messages.
-  content::RenderFrameHost* sender_ = nullptr;
-  IPC::Message* message_ = nullptr;
-    
-    #include 'components/download/public/common/download_item.h'
-#include 'content/public/browser/download_manager.h'
-#include 'content/public/browser/save_page_type.h'
-#include 'v8/include/v8.h'
-    
-    #include 'content/public/browser/javascript_dialog_manager.h'
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(AtomQuotaPermissionContext);
-};
-    
-    void SILLayout::Profile(llvm::FoldingSetNodeID &id,
-                        CanGenericSignature Generics,
-                        ArrayRef<SILField> Fields) {
-  id.AddPointer(Generics.getPointer());
-  for (auto &field : Fields) {
-    id.AddPointer(field.getLoweredType().getPointer());
-    id.AddBoolean(field.isMutable());
+      // Accessors.
+  int total_cost() const {
+    return total_cost_;
   }
-}
+  int Pathlength() const {
+    return total_steps_;
+  }
+  const DPPoint* best_prev() const {
+    return best_prev_;
+  }
+  void AddLocalCost(int new_cost) {
+    local_cost_ += new_cost;
+  }
+    
+    
+#endif  // TESSERACT_CCSTRUCT_LINLSQ_H_
 
     
-    
-    {  for (auto Entry : DCache.Entries) {
-    DCache.CBs.keyDestroyCB(Entry.first.Key, nullptr);
-    DCache.CBs.valueReleaseCB(Entry.second, nullptr);
-  }
-  DCache.Entries.clear();
-}
-    
-    using namespace swift::sys;
-using llvm::StringRef;
-    
-    #include 'swift/Basic/DiverseList.h'
-#include 'swift/Basic/DiverseStack.h'
-using namespace swift;
-    
-    DummyTaskQueue::~DummyTaskQueue() = default;
-    
-      // Instance members
-  IAMResult(DeclName declName, unsigned selfIdx, EffectiveClangContext dc)
-      : name(declName), selfIndex(selfIdx), effectiveDC(dc) {}
-    
-    // Generate constructors.
-#include 'ipc/struct_constructor_macros.h'
-#include 'content/nw/src/common/common_message_generator.h'
-    
-    void App::ClearCache(content::RenderProcessHost* render_process_host) {
-  render_process_host->Send(new ShellViewMsg_ClearCache());
-  nw::RemoveHttpDiskCache(render_process_host->GetBrowserContext(),
-                          render_process_host->GetID());
-}
-    
-      static void ClearCache(content::RenderProcessHost* render_view_host);
-  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
-                             const std::string& proxy_config);
-    
-    namespace nw {
-    }
-    
-      scoped_ptr<base::Value> value_args(
-      converter->FromV8Value(args, isolate->GetCurrentContext()));
-  if (!value_args.get() ||
-      !value_args->IsType(base::Value::TYPE_LIST))
-    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
-        'Unable to convert 'args' passed to CallObjectMethodSync')));
-    
-    // Get RenderView from current js context (only works under window context).
-content::RenderView* GetCurrentRenderView();
-content::RenderView* GetEnteredRenderView();
-    
-    
-    {  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  if (!item)
-    return false;
-  return item->is_modified_;
-}
-    
-    void PointMenuPositionFunc(GtkMenu* menu,
-                           int* x,
-                           int* y,
-                           gboolean* push_in,
-                           gpointer userdata) {
-  *push_in = TRUE;
-    }
-    
-    
-    {  submenu_ = menu;
-}
-    
-      // Ignore first non-switch arg if it's not a standalone package.
-  bool ignore_arg = !package->self_extract();
-  for (unsigned i = 1; i < argv.size(); ++i) {
-    if (ignore_arg && args.size() && argv[i] == args[0]) {
-      ignore_arg = false;
-      continue;
-    }
-    }
-    
-    
-    {      return true;
-    }
-    
-      bool NwScreenIsMonitorStartedFunction::RunNWSync(base::ListValue* response, std::string* error) {
-    response->AppendBoolean(NwDesktopCaptureMonitor::GetInstance()->IsStarted());
-    return true;
-  }
-    
-    // Used to print a pointer that is neither a char pointer nor a member
-// pointer, when the user doesn't define PrintTo() for it.  (A member
-// variable pointer or member function pointer doesn't really point to
-// a location in the address space.  Their representation is
-// implementation-defined.  Therefore they will be printed as raw
-// bytes.)
-template <typename T>
-void DefaultPrintTo(IsNotContainer /* dummy */,
-                    true_type /* is a pointer */,
-                    T* p, ::std::ostream* os) {
-  if (p == NULL) {
-    *os << 'NULL';
-  } else {
-    // C++ doesn't allow casting from a function pointer to any object
-    // pointer.
-    //
-    // IsTrue() silences warnings: 'Condition is always true',
-    // 'unreachable code'.
-    if (IsTrue(ImplicitlyConvertible<T*, const void*>::value)) {
-      // T is not a function type.  We just call << to print p,
-      // relying on ADL to pick up user-defined << for their pointer
-      // types, if any.
-      *os << p;
+    #if GTEST_OS_SYMBIAN
+  // These are needed as the Nokia Symbian Compiler cannot decide between
+  // const T& and const T* in a function template. The Nokia compiler _can_
+  // decide between class template specializations for T and T*, so a
+  // tr1::type_traits-like is_pointer works, and we can overload on that.
+  template <typename T>
+  inline void StreamHelper(internal::true_type /*is_pointer*/, T* pointer) {
+    if (pointer == NULL) {
+      *ss_ << '(null)';
     } else {
-      // T is a function type, so '*os << p' doesn't do what we want
-      // (it just prints p as bool).  We want to print p as a const
-      // void*.  However, we cannot cast it to const void* directly,
-      // even using reinterpret_cast, as earlier versions of gcc
-      // (e.g. 3.4.5) cannot compile the cast when p is a function
-      // pointer.  Casting to UInt64 first solves the problem.
-      *os << reinterpret_cast<const void*>(
-          reinterpret_cast<internal::UInt64>(p));
+      *ss_ << pointer;
     }
   }
+  template <typename T>
+  inline void StreamHelper(internal::false_type /*is_pointer*/,
+                           const T& value) {
+    // See the comments in Message& operator <<(const T&) above for why
+    // we need this using statement.
+    using ::operator <<;
+    *ss_ << value;
+  }
+#endif  // GTEST_OS_SYMBIAN
+    
+    // Helper function for printing a tuple.  T must be instantiated with
+// a tuple type.
+template <typename T>
+void PrintTupleTo(const T& t, ::std::ostream* os) {
+  *os << '(';
+  TuplePrefixPrinter< ::std::tr1::tuple_size<T>::value>::
+      PrintPrefixTo(t, os);
+  *os << ')';
 }
     
-    // A helper class for implementing EXPECT_FATAL_FAILURE() and
-// EXPECT_NONFATAL_FAILURE().  Its destructor verifies that the given
-// TestPartResultArray contains exactly one failure that has the given
-// type and contains the given substring.  If that's not the case, a
-// non-fatal failure will be generated.
-class GTEST_API_ SingleFailureChecker {
+      // Verifies that registered_tests match the test names in
+  // defined_test_names_; returns registered_tests if successful, or
+  // aborts the program otherwise.
+  const char* VerifyRegisteredTestNames(
+      const char* file, int line, const char* registered_tests);
+    
+    template <class Generator1, class Generator2, class Generator3,
+    class Generator4, class Generator5, class Generator6, class Generator7,
+    class Generator8, class Generator9>
+class CartesianProductHolder9 {
  public:
-  // The constructor remembers the arguments.
-  SingleFailureChecker(const TestPartResultArray* results,
-                       TestPartResult::Type type,
-                       const string& substr);
-  ~SingleFailureChecker();
- private:
-  const TestPartResultArray* const results_;
-  const TestPartResult::Type type_;
-  const string substr_;
+CartesianProductHolder9(const Generator1& g1, const Generator2& g2,
+    const Generator3& g3, const Generator4& g4, const Generator5& g5,
+    const Generator6& g6, const Generator7& g7, const Generator8& g8,
+    const Generator9& g9)
+      : g1_(g1), g2_(g2), g3_(g3), g4_(g4), g5_(g5), g6_(g6), g7_(g7), g8_(g8),
+          g9_(g9) {}
+  template <typename T1, typename T2, typename T3, typename T4, typename T5,
+      typename T6, typename T7, typename T8, typename T9>
+  operator ParamGenerator< ::std::tr1::tuple<T1, T2, T3, T4, T5, T6, T7, T8,
+      T9> >() const {
+    return ParamGenerator< ::std::tr1::tuple<T1, T2, T3, T4, T5, T6, T7, T8,
+        T9> >(
+        new CartesianProductGenerator9<T1, T2, T3, T4, T5, T6, T7, T8, T9>(
+        static_cast<ParamGenerator<T1> >(g1_),
+        static_cast<ParamGenerator<T2> >(g2_),
+        static_cast<ParamGenerator<T3> >(g3_),
+        static_cast<ParamGenerator<T4> >(g4_),
+        static_cast<ParamGenerator<T5> >(g5_),
+        static_cast<ParamGenerator<T6> >(g6_),
+        static_cast<ParamGenerator<T7> >(g7_),
+        static_cast<ParamGenerator<T8> >(g8_),
+        static_cast<ParamGenerator<T9> >(g9_)));
+  }
     }
     
     
-    {  const T1 v1_;
-  const T2 v2_;
-  const T3 v3_;
-  const T4 v4_;
-  const T5 v5_;
-  const T6 v6_;
-  const T7 v7_;
-  const T8 v8_;
-  const T9 v9_;
-  const T10 v10_;
-  const T11 v11_;
-  const T12 v12_;
-  const T13 v13_;
-  const T14 v14_;
-  const T15 v15_;
-  const T16 v16_;
-  const T17 v17_;
-  const T18 v18_;
-  const T19 v19_;
-  const T20 v20_;
-  const T21 v21_;
-  const T22 v22_;
-  const T23 v23_;
-  const T24 v24_;
-  const T25 v25_;
-  const T26 v26_;
-  const T27 v27_;
-  const T28 v28_;
-  const T29 v29_;
-  const T30 v30_;
-  const T31 v31_;
-  const T32 v32_;
-  const T33 v33_;
-  const T34 v34_;
-  const T35 v35_;
-  const T36 v36_;
-  const T37 v37_;
-  const T38 v38_;
-  const T39 v39_;
-  const T40 v40_;
-  const T41 v41_;
-  const T42 v42_;
-  const T43 v43_;
-  const T44 v44_;
-  const T45 v45_;
-  const T46 v46_;
-  const T47 v47_;
-  const T48 v48_;
-  const T49 v49_;
-  const T50 v50_;
+    {    return -1;
+  }
+    
+        E* element = new E(old_head->element());
+    delete old_head;
+    
+    class DBImpl;
+    
+    
+    {
+    {}  // namespace log
+}  // namespace leveldb
+    
+    Reader::~Reader() {
+  delete[] backing_store_;
+}
+    
+    TEST(LogTest, ReadWrite) {
+  Write('foo');
+  Write('bar');
+  Write('');
+  Write('xxxx');
+  ASSERT_EQ('foo', Read());
+  ASSERT_EQ('bar', Read());
+  ASSERT_EQ('', Read());
+  ASSERT_EQ('xxxx', Read());
+  ASSERT_EQ('EOF', Read());
+  ASSERT_EQ('EOF', Read());  // Make sure reads at eof work
+}
+    
+    class Env;
+    
+    class VersionSet;
+    
+      VersionEdit edit;
+  for (int i = 0; i < 4; i++) {
+    TestEncodeDecode(edit);
+    edit.AddFile(3, kBig + 300 + i, kBig + 400 + i,
+                 InternalKey('foo', kBig + 500 + i, kTypeValue),
+                 InternalKey('zoo', kBig + 600 + i, kTypeDeletion));
+    edit.DeleteFile(4, kBig + 700 + i);
+    edit.SetCompactPointer(i, InternalKey('x', kBig + 900 + i, kTypeValue));
+  }
+    
+      void Add(const char* smallest, const char* largest,
+           SequenceNumber smallest_seq = 100,
+           SequenceNumber largest_seq = 100) {
+    FileMetaData* f = new FileMetaData;
+    f->number = files_.size() + 1;
+    f->smallest = InternalKey(smallest, smallest_seq, kTypeValue);
+    f->largest = InternalKey(largest, largest_seq, kTypeValue);
+    files_.push_back(f);
+  }
+    
+    size_t num_threads = 31;
+size_t work_chunk  = 120;
+    
+    void Config::ParseIniFile(const std::string &filename, IniSettingMap &ini,
+                          const bool constants_only /* = false */,
+                          const bool is_system /* = true */ ) {
+    std::ifstream ifs(filename);
+    std::string str((std::istreambuf_iterator<char>(ifs)),
+                    std::istreambuf_iterator<char>());
+    std::string with_includes;
+    Config::ReplaceIncludesWithIni(filename, str, with_includes);
+    Config::SetParsedIni(ini, with_includes, filename, constants_only,
+                         is_system);
+}
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+    }
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+    }
+    
+    void logAHMSubMapWarning(folly::StringPiece mapName) {
+  StackTrace st;
+  logPerfWarning(
+    'AtomicHashMap overflow',
+    [&](StructuredLogEntry& cols) {
+      cols.setStr('map_name', mapName);
+      cols.setStackTrace('stack', st);
+    }
+  );
+}
+    
+    
+void printVec(const vector<int>& vec){
+    for(int e: vec)
+        cout << e << ' ';
+    cout << endl;
+}
+    
+    ///Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
     
-    $for i [[
-$range j 1..i
-$range k 2..i
-template <$for j, [[GTEST_TEMPLATE_ T$j]]>
-struct Templates$i {
-  typedef TemplateSel<T1> Head;
-  typedef Templates$(i-1)<$for k, [[T$k]]> Tail;
+                    if( stack.size() == 0 )
+                    return false;
+    
+    /// LinkedList Test Helper Functions
+ListNode* createLinkedList(int arr[], int n){
+    }
+    
+            if(nums.size() == 0)
+            return 0;
+    
+    
+int main() {
+    }
+    
+    #include <iostream>
+#include <vector>
+    
+    /// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-    
-     public:
-  // Gets the element in this node.
-  const E& element() const { return element_; }
-    
-    const AuthProperty AuthPropertyIterator::operator*() {
-  return std::pair<grpc::string_ref, grpc::string_ref>(
-      property_->name,
-      grpc::string_ref(property_->value, property_->value_length));
-}
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
-    
-    MeasureInt64 RpcClientSentMessagesPerRpc() {
-  static const auto measure =
-      MeasureInt64::Register(kRpcClientSentMessagesPerRpcMeasureName,
-                             'Number of messages sent per RPC', kCount);
-  return measure;
-}
-    
-    ViewDescriptor MinuteDescriptor() {
-  auto descriptor = ViewDescriptor();
-  SetAggregationWindow(AggregationWindow::Interval(absl::Minutes(1)),
-                       &descriptor);
-  return descriptor;
-}
-    
-        virtual void UpdateArguments(ChannelArguments* args) override {
-      args->SetString(name_, value_);
-    }
-    virtual void UpdatePlugins(
-        std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {}
-    
-    #include <grpc/support/cpu.h>
-    
-    class DynamicThreadPool final : public ThreadPoolInterface {
- public:
-  explicit DynamicThreadPool(int reserve_threads);
-  ~DynamicThreadPool();
-    }
-    
-    
-    {  QueryLogItem second_item;
-  getDecorations(second_item.decorations);
-  ASSERT_EQ(second_item.decorations.size(), 2U);
-}
-    
-    namespace rj = rapidjson;
-    
-    TEST_F(PacksTests, test_restriction_population) {
-  // Require that all potential restrictions are populated before being checked.
-  auto doc = getExamplePacksConfig();
-  const auto& packs = doc.doc()['packs'];
-  Pack fpack('fake_pack', packs['restricted_pack']);
-    }
-    
-    #include <Objbase.h>
-#include <Windows.h>
-    
-      /// Mark for delete, subscriptions.
-  void removeSubscriptions(const std::string& subscriber) override;
-    
-    REGISTER_EXTERNAL(ExampleConfigPlugin, 'config', 'example');
-REGISTER_EXTERNAL(ExampleTable, 'table', 'example');
-REGISTER_EXTERNAL(ComplexExampleTable, 'table', 'complex_example');
-    
-    // buf size must be 8 bytes, every time, we receive only one frame
-ErrorCode SocketCanClientRaw::Receive(std::vector<CanFrame> *const frames,
-                                      int32_t *const frame_num) {
-  if (!is_started_) {
-    AERROR << 'Nvidia can client is not init! Please init first!';
-    return ErrorCode::CAN_CLIENT_ERROR_RECV_FAILED;
-  }
-    }
-    
-      sender.AddMessage(1, &mpd);
-  EXPECT_EQ(sender.Start(), common::ErrorCode::OK);
-  EXPECT_TRUE(sender.IsRunning());
-  EXPECT_TRUE(sender.enable_log());
-    
-    /**
- * @file
- * @brief The class of ProtocolData
- */
-    
-    uint8_t Byte::get_byte(const int32_t start_pos, const int32_t length) const {
-  if (start_pos > BYTE_LENGTH - 1 || start_pos < 0 || length < 1) {
-    return 0x00;
-  }
-  int32_t end_pos = std::min(start_pos + length - 1, BYTE_LENGTH - 1);
-  int32_t real_len = end_pos + 1 - start_pos;
-  uint8_t result = *value_ >> start_pos;
-  result &= RANG_MASK_1_L[real_len - 1];
-  return result;
-}
-    
-    #include <string>
-    
-    #include <cstdint>
