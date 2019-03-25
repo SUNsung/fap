343405ddc22c5e07f54adca60238eb59b242e7f5
SUNsung <weatherius@gmail.com>
2019-03-25 18:11:00 +0200

    Rewrite [53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py] File in: 2019-03-25 18:10:59

--- pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
+++ pyton/53wdC.W5QjTLI53uEskFTVYW4I53KPqLGg1La2I53laL.py
@@ -1,134 +1,105 @@
 
         
-            Returns:
-      The cost for the current batch.
+        
+class SessionStore(SessionBase):
     '''
-    cost, _ = session.run([self.cost, self.train_op],
-                          feed_dict={self.instances: batch_instances})
-    
-        Args:
-      hparams: the hyper-parameters.
-      lemma_embeddings: word embeddings for the path-based component.
-      instance: string tensor containing the input instance
+    Implement database session store.
     '''
-    self.hparams = hparams
-    self.lemma_embeddings = lemma_embeddings
-    self.instance = instance
-    self.vocab_size, self.lemma_dim = self.lemma_embeddings.shape
+    def __init__(self, session_key=None):
+        super().__init__(session_key)
     
-        self._word_char_ids = np.zeros([num_words, max_word_length], dtype=np.int32)
+    from django.contrib.sites.shortcuts import get_current_site
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from django.http import Http404
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.utils.http import http_date
     
-      for key, _ in gen_ngrams_dict.iteritems():
-    if key in train_ngrams_dict:
-      unique_ngrams_in_train += 1
-  return float(unique_ngrams_in_train) / float(total_ngrams_produced)
-
+            This property is cached so that only one database query occurs when
+        rendering ModelChoiceFields.
+        '''
+        id_ = self.field.widget.attrs.get('id') or self.auto_id
+        attrs = {'id': id_} if id_ else {}
+        attrs = self.build_widget_attrs(attrs)
+        return [
+            BoundWidget(self.field.widget, widget, self.form.renderer)
+            for widget in self.field.widget.subwidgets(self.html_name, self.value(), attrs=attrs)
+        ]
     
-    
-def load_data(label_mode='fine'):
-    '''Loads CIFAR100 dataset.
-    
-    
-def test_mnist():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        assert len(x_train) == len(y_train) == 60000
-        assert len(x_test) == len(y_test) == 10000
-    
-        Example 2 - Training models with weights merge on CPU using cpu_relocation
-    
-            if rotation > 0:
-            angle_rad = rotation * 3.141592653589793 / 180.0
-            angles = tf.random_uniform([batch_size], -angle_rad, angle_rad)
-            f = tf.contrib.image.angles_to_projective_transforms(angles,
-                                                                 height, width)
-            transforms.append(f)
-    
-    Gets to 0.89 test accuracy after 2 epochs.
-90s/epoch on Intel i5 2.4Ghz CPU.
-10s/epoch on Tesla K40 GPU.
-'''
-from __future__ import print_function
-    
-    x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-    
-    # Save the model weights.
-weight_path = os.path.join(tempfile.gettempdir(), 'saved_wt.h5')
-train_model.save_weights(weight_path)
-    
-        def __eq__(self, other):
-        if isinstance(other, Mapping):
-            other = CaseInsensitiveDict(other)
+            if state == 'absent':
+            if exists:
+                conn.delete_cache_subnet_group(group_name)
+                changed = True
         else:
-            return NotImplemented
-        # Compare insensitively
-        return dict(self.lower_items()) == dict(other.lower_items())
+            if not exists:
+                new_group = conn.create_cache_subnet_group(group_name, cache_subnet_group_description=group_description, subnet_ids=group_subnets)
+                changed = True
+            else:
+                changed_group = conn.modify_cache_subnet_group(group_name, cache_subnet_group_description=group_description, subnet_ids=group_subnets)
+                changed = True
     
-        builtin_str = str
-    str = str
-    bytes = bytes
-    basestring = (str, bytes)
-    numeric_types = (int, float)
-    integer_types = (int,)
-
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
     
-        def get_origin_req_host(self):
-        return self.get_host()
     
-    '''
-requests.hooks
-~~~~~~~~~~~~~~
+def validate_fw_rules(fw_rules):
+    for rule in fw_rules:
+        for k in rule.keys():
+            if k not in VALID_RULE_KEYS:
+                raise VcaError('%s is not a valid key in fw rules, please '
+                               'check above..' % k, valid_keys=VALID_RULE_KEYS)
+    
+        gateway = vca.get_gateway(vdc_name, gateway_name)
+    if not gateway:
+        module.fail_json(msg='Not able to find the gateway %s, please check '
+                             'the gateway_name param' % gateway_name)
+    
+    from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+    
+        module_hbacrule = get_hbacrule_dict(description=module.params['description'],
+                                        hostcategory=hostcategory,
+                                        ipaenabledflag=ipaenabledflag,
+                                        servicecategory=servicecategory,
+                                        sourcehostcategory=sourcehostcategory,
+                                        usercategory=usercategory)
+    ipa_hbacrule = client.hbacrule_find(name=name)
+    
+        context['show_on_github_url'] = show_url
+    context['edit_on_github_url'] = edit_url
     
     # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Requests.tex', u'Requests Documentation',
-     u'Kenneth Reitz', 'manual'),
+    (master_doc, 'home-assistant.tex', 'Home Assistant Documentation',
+     'Home Assistant Team', 'manual'),
 ]
     
-    site_info = 'CBS.com'
-download = cbs_download
-download_playlist = playlist_not_supported('cbs')
+        # Gloss the lips
+    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
+    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+    
+        :param img_path: path to image to be recognized
+    :param predictions: results of the predict function
+    :return:
+    '''
+    pil_image = Image.open(img_path).convert('RGB')
+    draw = ImageDraw.Draw(pil_image)
+    
+    
+def batch_face_locations(images, number_of_times_to_upsample=1, batch_size=128):
+    '''
+    Returns an 2d array of bounding boxes of human faces in a image using the cnn face detector
+    If you are using a GPU, this can give you much faster results since the GPU
+    can process batches of images at once. If you aren't using a GPU, you don't need this function.
+    
+    # You can also save a copy of the new image to disk if you want by uncommenting this line
+# pil_image.save('image_with_boxes.jpg')
 
     
-    #----------------------------------------------------------------------
-def ckplayer_download_by_xml(ckinfo, output_dir = '.', merge = False, info_only = False, **kwargs):
-    #Info XML
-    video_info = ckplayer_get_info_by_xml(ckinfo)
-    
-    try:
-        title = kwargs['title']
-    except:
-        title = ''
-    type_ = ''
-    size = 0
-    
-    if len(video_info['links']) > 0:  #has link
-        type_, _ext, size = url_info(video_info['links'][0])  #use 1st to determine type, ext
-    
-    if 'size' in video_info:
-        size = int(video_info['size'])
-    else:
-        for i in video_info['links'][1:]:  #save 1st one
-            size += url_info(i)[2]
-    
-    print_info(site_info, title, type_, size)
-    if not info_only:
-        download_urls(video_info['links'], title, _ext, size, output_dir=output_dir, merge=merge)
-    
-    '''
-http://www.tudou.com/programs/view/html5embed.action?type=0&amp;code=3LS_URGvl54&amp;lcode=&amp;resourceId=0_06_05_99
-'''
-tudou_embed_patterns = [ 'tudou\.com[a-zA-Z0-9\/\?=\&\.\;]+code=([a-zA-Z0-9_-]+)\&',
-                         'www\.tudou\.com/v/([a-zA-Z0-9_-]+)/[^']*v\.swf'
-                       ]
+            self.assertEqual(len(detected_faces), 1)
+        self.assertAlmostEqual(detected_faces[0].rect.top(), 144, delta=25)
+        self.assertAlmostEqual(detected_faces[0].rect.bottom(), 389, delta=25)
