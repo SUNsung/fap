6ba7bfe686fb6d81a1e9001d8c82ddd38d4fa305
SUNsung <weatherius@gmail.com>
2019-03-25 02:14:52 +0200

    Rewrite [11QVl9WJ6eDZM11FSFsFlMEybA11gez1BRNt5Do11XC1.cpp] File in: 2019-03-25 02:14:52

--- cccccc/11QVl9WJ6eDZM11FSFsFlMEybA11gez1BRNt5Do11XC1.cpp
+++ cccccc/11QVl9WJ6eDZM11FSFsFlMEybA11gez1BRNt5Do11XC1.cpp
@@ -1,257 +1,259 @@
-bool IsUrlArg(const base::CommandLine::CharType* arg) {
-  // the first character must be a letter for this to be a URL
-  auto c = *arg;
-  if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
-    for (auto* p = arg + 1; *p; ++p) {
-      c = *p;
-    }
-    }
-    }
+
+        
+        void SILLayout::Profile(llvm::FoldingSetNodeID &id,
+                        CanGenericSignature Generics,
+                        ArrayRef<SILField> Fields) {
+  id.AddPointer(Generics.getPointer());
+  for (auto &field : Fields) {
+    id.AddPointer(field.getLoweredType().getPointer());
+    id.AddBoolean(field.isMutable());
+  }
+}
+
     
-    #include 'atom/browser/api/event_emitter.h'
-#include 'atom/browser/mac/in_app_purchase.h'
-#include 'atom/browser/mac/in_app_purchase_observer.h'
-#include 'atom/browser/mac/in_app_purchase_product.h'
-#include 'native_mate/handle.h'
     
-      gfx::Point GetCursorScreenPoint();
-  display::Display GetPrimaryDisplay();
-  std::vector<display::Display> GetAllDisplays();
-  display::Display GetDisplayNearestPoint(const gfx::Point& point);
-  display::Display GetDisplayMatching(const gfx::Rect& match_rect);
-    
-    #ifndef ATOM_BROWSER_API_EVENT_H_
-#define ATOM_BROWSER_API_EVENT_H_
-    
-      canvas_.reset();
-  bitmap_.reset(new SkBitmap);
-  bitmap_->allocN32Pixels(viewport_pixel_size_.width(),
-                          viewport_pixel_size_.height(), !transparent_);
-  if (bitmap_->drawsNothing()) {
-    NOTREACHED();
-    bitmap_.reset();
-    return;
+    {    return name;
   }
     
-    void OffscreenViewProxy::RemoveObserver() {
-  observer_ = nullptr;
+    bool Mangle::needsPunycodeEncoding(StringRef str) {
+  for (unsigned char c : str) {
+    if (!isValidSymbolChar(c))
+      return true;
+  }
+  return false;
 }
     
-      Extension_ping_pargs args;
-  args.write(oprot_);
+    const char *Demangle::getNodeKindString(swift::Demangle::Node::Kind k) {
+  switch (k) {
+#define NODE(ID)                                                               \
+  case Node::Kind::ID:                                                         \
+    return #ID;
+#include 'swift/Demangling/DemangleNodes.def'
+  }
+  return 'Demangle::Node::Kind::???';
+}
     
-    int main(int argc, char **argv) {
-  int port = 9090;
-  ::apache::thrift::stdcxx::shared_ptr<ExtensionManagerHandler> handler(new ExtensionManagerHandler());
-  ::apache::thrift::stdcxx::shared_ptr<TProcessor> processor(new ExtensionManagerProcessor(handler));
-  ::apache::thrift::stdcxx::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  ::apache::thrift::stdcxx::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  ::apache::thrift::stdcxx::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+    static const int base         = 36;
+static const int tmin         = 1;
+static const int tmax         = 26;
+static const int skew         = 38;
+static const int damp         = 700;
+static const int initial_bias = 72;
+static const uint32_t initial_n = 128;
+    
+    void REPLJobAction::anchor() {}
+    
+      // Try to close all windows (then will cause whole app to quit).
+  static void CloseAllWindows(bool force = false, bool quit = false);
+    
+    
+    {  template<typename T> bool RemoveListener() {
+    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
+    if (i!=listerners_.end()) {
+      delete i->second;
+      listerners_.erase(i);
+      return true;
     }
-    
-    
-    {  virtual void printTo(std::ostream& out) const;
+    return false;
+  }
+private:
+  DISALLOW_COPY_AND_ASSIGN(EventListener);
 };
     
-      if (types.size() > 1) {
-    LOG(WARNING) << 'Found more than 1 hardware storage controller: ' << types;
-    return;
-  }
-    
-    #include <osquery/utils/system/linux/perf_event/perf_event.h>
-    
-    
-    {
-    {} // namespace perf_event_open
-} // namespace osquery
-
-    
-    TEST_F(PerfOutputTests,
-       impl_consumeWrappedMessagesFromCircularBuffer_splited_record_wrapping) {
-  using WrappedMessage = ebpf::PerfOutput<TestMessage>::WrappedMessage;
-  auto const test_size = std::size_t{3};
-  auto buf = std::vector<ebpf::impl::ByteType>(
-      sizeof(WrappedMessage) * test_size + 154, 0);
-  auto const first_part_size = 8;
-  auto const tail = buf.size() - sizeof(WrappedMessage) - first_part_size;
-  auto const head = tail + sizeof(WrappedMessage) * test_size;
-    }
-    
-    #include <osquery/utils/expected/expected.h>
-    
-        while ((n = (node_t*) swHeap_pop(pq)))
-    {
-        ASSERT_EQ(_map[n->val], n->pri);
-        free(n);
-    }
-    
-    TEST(rbtree, insert)
-{
-    swRbtree *tree = swRbtree_new();
-    int i;
-    std::set<uint32_t> lists;
+    bool MenuDelegate::GetIconForCommandId(int command_id,
+                                       gfx::Image* icon) const {
+  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
+  if (!item)
+    return false;
+  if (item->icon_.IsEmpty())
+    return false;
     }
     
     
-    {    uint32_t protect_page = get_protect_stack_page();
-    if (protect_page)
-    {
-        if (protect_stack(stack_, stack_size, protect_page))
-        {
-            protect_page_ = protect_page;
-        }
-    }
-}
+    {}  // namespace nwapi
     
-    class RedisQtAdapter : public QObject {
-    }
-    
-    
-    {            ASSERT_EQ(addr1, addr3);
-            ASSERT_EQ(addr2, addr4);
-        }
-    
-    
-    {    ASSERT_GT(cid, 0);
-    ASSERT_EQ(cid, _cid);
-}
-    
-            ZEND_HASH_FOREACH_VAL(Z_ARRVAL_P(pv_param_arr), tmp) {
-    }
-    
-    
-template<typename ConsumerType>
-class Action : public clang::PluginASTAction {
+      static void DoJob(AppWindowRegistry* registry, std::string id);
  protected:
-  clang::ASTConsumer *CreateASTConsumer(clang::CompilerInstance &CI,
-                                        llvm::StringRef InFile) {
-    return new ConsumerType(CI.getDiagnostics(), CI.getSourceManager(), args_);
+  ~NwAppCloseAllWindowsFunction() override {}
+    
+     protected:
+  ~NwClipboardSetListSyncFunction() override;
+    
+    class NwMenuGetNSStringWithFixupFunction : public NWSyncExtensionFunction {
+ public:
+  NwMenuGetNSStringWithFixupFunction(){}
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    
+ protected:
+  ~NwMenuGetNSStringWithFixupFunction() override {}
+    
+  DECLARE_EXTENSION_FUNCTION('nw.Menu.getNSStringWithFixup', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwMenuGetNSStringWithFixupFunction);
+};
+    
+      // Called when the |display|'s bound has changed.
+  void NwScreenDisplayObserver::OnDisplayMetricsChanged(const display::Display& display,
+    uint32_t changed_metrics) {
+    std::unique_ptr<base::ListValue> args = 
+      nwapi::nw__screen::OnDisplayBoundsChanged::Create(*ConvertGfxDisplay(display),
+                                                        changed_metrics);
+    DispatchEvent(
+      events::HistogramValue::UNKNOWN, 
+      nwapi::nw__screen::OnDisplayBoundsChanged::kEventName,
+      std::move(args));
+  }
+    
+      // implement nw.Screen.startMonitor()
+  class NwScreenStartMonitorFunction : public NWSyncExtensionFunction {
+  public:
+    NwScreenStartMonitorFunction();
+    bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    // Returns a newly created InternalRunDeathTestFlag object with fields
+// initialized from the GTEST_FLAG(internal_run_death_test) flag if
+// the flag is specified; otherwise returns NULL.
+InternalRunDeathTestFlag* ParseInternalRunDeathTestFlag();
+    
+      // Returns true if pathname describes a directory in the file-system
+  // that exists.
+  bool DirectoryExists() const;
+    
+    #include 'gtest/internal/gtest-port.h'
+    
+    # define GTEST_TEMPLATE_ template <typename T> class
+    
+    // Implementation #2 pre-calculates the primes and stores the result
+// in an array.
+class PreCalculatedPrimeTable : public PrimeTable {
+ public:
+  // 'max' specifies the maximum number the prime table holds.
+  explicit PreCalculatedPrimeTable(int max)
+      : is_prime_size_(max + 1), is_prime_(new bool[max + 1]) {
+    CalculatePrimesUpTo(max);
+  }
+  virtual ~PreCalculatedPrimeTable() { delete[] is_prime_; }
+    }
+    
+    
+    {  EXPECT_EQ(0u, s.Length());
+}
+    
+      // D'tor.  Clears the queue.
+  ~Queue() { Clear(); }
+    
+    
+    {  // No copying allowed
+  LookupKey(const LookupKey&);
+  void operator=(const LookupKey&);
+};
+    
+    std::string SSTTableFileName(const std::string& dbname, uint64_t number) {
+  assert(number > 0);
+  return MakeFileName(dbname, number, 'sst');
+}
+    
+    int main(int argc, char** argv) {
+  leveldb::Env* env = leveldb::Env::Default();
+  bool ok = true;
+  if (argc < 2) {
+    Usage();
+    ok = false;
+  } else {
+    std::string command = argv[1];
+    if (command == 'dump') {
+      ok = leveldb::HandleDumpCommand(env, argv+2, argc-2);
+    } else {
+      Usage();
+      ok = false;
+    }
+  }
+  return (ok ? 0 : 1);
+}
+
+    
+          case kMiddleType:
+        if (!in_fragmented_record) {
+          ReportCorruption(fragment.size(),
+                           'missing start of fragmented record(1)');
+        } else {
+          scratch->append(fragment.data(), fragment.size());
+        }
+        break;
+    
+    class Writer {
+ public:
+  // Create a writer that will append data to '*dest'.
+  // '*dest' must be initially empty.
+  // '*dest' must remain live while this Writer is in use.
+  explicit Writer(WritableFile* dest);
+    }
+    
+      Iterator* NewTableIterator(const FileMetaData& meta) {
+    // Same as compaction iterators: if paranoid_checks are on, turn
+    // on checksum verification.
+    ReadOptions r;
+    r.verify_checksums = options_.paranoid_checks;
+    return table_cache_->NewIterator(r, meta.number, meta.file_size);
+  }
+    
+    Iterator* TableCache::NewIterator(const ReadOptions& options,
+                                  uint64_t file_number,
+                                  uint64_t file_size,
+                                  Table** tableptr) {
+  if (tableptr != nullptr) {
+    *tableptr = nullptr;
   }
     }
     
-    #include <stdio.h>
+      VersionEdit edit;
+  for (int i = 0; i < 4; i++) {
+    TestEncodeDecode(edit);
+    edit.AddFile(3, kBig + 300 + i, kBig + 400 + i,
+                 InternalKey('foo', kBig + 500 + i, kTypeValue),
+                 InternalKey('zoo', kBig + 600 + i, kTypeDeletion));
+    edit.DeleteFile(4, kBig + 700 + i);
+    edit.SetCompactPointer(i, InternalKey('x', kBig + 900 + i, kTypeValue));
+  }
     
-      // TODO(ishell): use constants from Descriptor once the JSFunction linkage
-  // arguments are reordered.
-  Node* argc = Parameter(Descriptor::kJSActualArgumentsCount);
-  Node* context = Parameter(Descriptor::kContext);
-  Node* new_target = Parameter(Descriptor::kJSNewTarget);
-  GotoIf(Word32Equal(argc, Int32Constant(0)), &runtime);
+      static void SetContents(WriteBatch* batch, const Slice& contents);
     
-    void LazyBuiltinsAssembler::GenerateTailCallToReturnedCode(
-    Runtime::FunctionId function_id, TNode<JSFunction> function) {
-  TNode<Context> context = CAST(Parameter(Descriptor::kContext));
-  TNode<Code> code = CAST(CallRuntime(function_id, context, function));
-  GenerateTailCallToJSCode(code, function);
+    namespace php {
+struct Program;
+}
+struct Index;
+    
+      if (UNLIKELY(m_arrayHandle->kind() == APCKind::SerializedVec ||
+               m_arrayHandle->kind() == APCKind::SerializedDict)) {
+    return createFromSerialized(m_colType, m_arrayHandle);
+  }
+    
+    
+    {  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+};
+    
+    
+    {///////////////////////////////////////////////////////////////////////////////
 }
     
-      Node* MathPow(Node* context, Node* base, Node* exponent);
+      // only accept paths with the glob:// prefix
+  if (strncmp(path_str, prefix, strlen(prefix)) != 0) {
+    return nullptr;
+  }
     
-    
-    {  Node* value = ToThisValue(context, receiver, PrimitiveType::kSymbol,
-                            'Symbol.prototype.description');
-  Node* result = LoadObjectField(value, Symbol::kNameOffset);
-  Return(result);
+    template<typename F>
+void logPerfWarning(folly::StringPiece event, F fillCols) {
+  logPerfWarningImpl(event, 1, kDefaultPerfWarningRate, fillCols);
+}
+template<typename F>
+void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols) {
+  logPerfWarningImpl(event, 1, rate, fillCols);
 }
     
-    #endif  // V8_BUILTINS_BUILTINS_TEST_GEN_H_
-
+    #include 'hphp/util/stack-trace.h'
     
-        uint64_t *write_to_;
     
-    BinaryFormat::BinaryFormat(const Config &config)
-  : write_method_(config.write_method), write_mmap_(config.write_mmap), load_method_(config.load_method),
-    header_size_(kInvalidSize), vocab_size_(kInvalidSize), vocab_string_offset_(kInvalidOffset) {}
-    
-    void Usage(const char *name, const char *default_mem) {
-  std::cerr << 'Usage: ' << name << ' [-u log10_unknown_probability] [-s] [-i] [-w mmap|after] [-p probing_multiplier] [-T trie_temporary] [-S trie_building_mem] [-q bits] [-b bits] [-a bits] [type] input.arpa [output.mmap]\n\n'
-'-u sets the log10 probability for <unk> if the ARPA file does not have one.\n'
-'   Default is -100.  The ARPA file will always take precedence.\n'
-'-s allows models to be built even if they do not have <s> and </s>.\n'
-'-i allows buggy models from IRSTLM by mapping positive log probability to 0.\n'
-'-w mmap|after determines how writing is done.\n'
-'   mmap maps the binary file and writes to it.  Default for trie.\n'
-'   after allocates anonymous memory, builds, and writes.  Default for probing.\n'
-'-r \'order1.arpa order2 order3 order4\' adds lower-order rest costs from these\n'
-'   model files.  order1.arpa must be an ARPA file.  All others may be ARPA or\n'
-'   the same data structure as being built.  All files must have the same\n'
-'   vocabulary.  For probing, the unigrams must be in the same order.\n\n'
-'type is either probing or trie.  Default is probing.\n\n'
-'probing uses a probing hash table.  It is the fastest but uses the most memory.\n'
-'-p sets the space multiplier and must be >1.0.  The default is 1.5.\n\n'
-'trie is a straightforward trie with bit-level packing.  It uses the least\n'
-'memory and is still faster than SRI or IRST.  Building the trie format uses an\n'
-'on-disk sort to save memory.\n'
-'-T is the temporary directory prefix.  Default is the output file name.\n'
-'-S determines memory use for sorting.  Default is ' << default_mem << '.  This is compatible\n'
-'   with GNU sort.  The number is followed by a unit: \% for percent of physical\n'
-'   memory, b for bytes, K for Kilobytes, M for megabytes, then G,T,P,E,Z,Y.  \n'
-'   Default unit is K for Kilobytes.\n'
-'-q turns quantization on and sets the number of bits (e.g. -q 8).\n'
-'-b sets backoff quantization bits.  Requires -q and defaults to that value.\n'
-'-a compresses pointers using an array of offsets.  The parameter is the\n'
-'   maximum number of bits encoded by the array.  Memory is minimized subject\n'
-'   to the maximum, so pick 255 to minimize memory.\n\n'
-'-h print this help message.\n\n'
-'Get a memory estimate by passing an ARPA file without an output file name.\n';
-  exit(1);
+    {  return nullptr;
 }
-    
-        static std::size_t OrderFromSize(std::size_t size) {
-      std::size_t ret = (size - sizeof(Payload)) / sizeof(WordIndex);
-      assert(size == TotalSize(ret));
-      return ret;
-    }
-    
-        Proxy &operator*() { return proxy_; }
-    const Proxy &operator*() const { return proxy_; }
-    
-    
-  // Quantization options.  Only effective for QuantTrieModel.  One value is
-  // reserved for each of prob and backoff, so 2^bits - 1 buckets will be used
-  // to quantize (and one of the remaining backoffs will be 0).
-  uint8_t prob_bits, backoff_bits;
-    
-        virtual void Add(WordIndex index, const StringPiece &str) = 0;
-    
-    	const QMap<char, Type> singleLetterTokens_ = {
-		{ '(', Type::LeftParenthesis },
-		{ ')', Type::RightParenthesis },
-		{ '{', Type::LeftBrace },
-		{ '}', Type::RightBrace },
-		{ '[', Type::LeftBracket },
-		{ ']', Type::RightBracket },
-		{ ':', Type::Colon },
-		{ ';', Type::Semicolon },
-		{ ',', Type::Comma },
-		{ '.', Type::Dot },
-		{ '#', Type::Number },
-		{ '+', Type::Plus },
-		{ '-', Type::Minus },
-		{ '=', Type::Equals },
-		{ '&', Type::And },
-		{ '|', Type::Or },
-	};
-    
-    class ConstUtf8String;
-    
-    #include <ostream>
-#include <QtCore/QCoreApplication>
-#include 'codegen/common/logging.h'
-    
-    constexpr int kErrorOutputPathExpected      = 902;
-constexpr int kErrorInputPathExpected       = 903;
-constexpr int kErrorSingleInputPathExpected = 904;
-constexpr int kErrorWorkingPathExpected     = 905;
-    
-    	QFileInfo srcFile(options_.inputPath);
-	QString dstFilePath = dir.absolutePath() + '/lang_auto';
-    
-    
-    {
-    {} // namespace numbers
-} // namespace codegen
-
-    
-    	bool writeHeader();
-	bool writeSource();
