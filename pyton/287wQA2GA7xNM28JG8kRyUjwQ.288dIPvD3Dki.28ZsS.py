5e1eb3132d963e20095c5f27aa0942d6da7ee8df
SUNsung <weatherius@gmail.com>
2019-03-25 11:06:37 +0200

    Rewrite [287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py] File in: 2019-03-25 11:06:37

--- pyton/287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py
+++ pyton/287wQA2GA7xNM28JG8kRyUjwQ.288dIPvD3Dki.28ZsS.py
@@ -1,236 +1,39 @@
 
         
-        
-def load_all_pairs(records):
-  '''Reads TensorFlow examples from a RecordReader and returns the word pairs.
+          if mat_init_value is not None and mat_init_value.shape != (in_size, out_size):
+    raise ValueError(
+        'Provided mat_init_value must have shape [%d, %d].'%(in_size, out_size))
+  if bias_init_value is not None and bias_init_value.shape != (1,out_size):
+    raise ValueError(
+        'Provided bias_init_value must have shape [1,%d].'%(out_size,))
     
-      # First generate all firing rates. in the next loop, generate all
-  # replications this allows the random state for rate generation to be
-  # independent of n_replications.
-  dataset_name = 'dataset_N' + str(N) + '_S' + str(S)
-  if S < N:
-    dataset_name += '_n' + str(n+1)
+        for word in self._id_to_word:
+      chars_set |= set(word)
     
-    # not the best way to do this but E is small enough
-rates = []
-spikes = []
-for trial in xrange(E):
-  if rnn_to_use[trial] == 0:
-    rates.append(rates_a[trial])
-    spikes.append(spikes_a[trial])
-  else:
-    rates.append(rates_b[trial])
-    spikes.append(spikes_b[trial])
+      softmax_weights = sess.run(t['softmax_weights'])
+  fname = FLAGS.save_dir + '/embeddings_softmax.npy'
+  with tf.gfile.Open(fname, mode='w') as f:
+    np.save(f, softmax_weights)
+  sys.stderr.write('Finished softmax weights\n')
     
-      Returns:
-    values_bxtxn: The BxTxN numpy tensor.
-  '''
+      real_labels = tf.ones([FLAGS.batch_size, FLAGS.sequence_length])
+  dis_loss_real = tf.losses.sigmoid_cross_entropy(
+      real_labels, real_predictions, weights=missing)
+  dis_loss_fake = tf.losses.sigmoid_cross_entropy(
+      targets_present, fake_predictions, weights=missing)
     
-        global_word_ids = []
-    current_idx = 0
-    for word_ids in ids:
-      current_size = len(word_ids) - 1  # without <BOS> symbol
-      cur_ids = np.arange(current_idx, current_idx + current_size)
-      global_word_ids.append(cur_ids)
-      current_idx += current_size
-    
-      '''
-  zero_tensor = tf.constant(0., dtype=tf.float32, shape=[])
-    
-    import tensorflow as tf
-    
-      return attn
-    
-    
-@pytest.mark.parametrize('script, output', [
-    ('apt', invalid_operation('saerch')),
-    ('apt-get', invalid_operation('isntall')),
-    ('apt-cache', invalid_operation('rumove'))])
-def test_match(script, output):
-    assert match(Command(script, output))
-    
-    
-def test_match():
-    command = Command('brew install sshfs', output)
-    assert match(command)
-    
-    
-@pytest.mark.parametrize('command, new_command', [
-    (Command('cargo buid', no_such_subcommand_old), 'cargo build'),
-    (Command('cargo buils', no_such_subcommand), 'cargo build')])
-def test_get_new_command(command, new_command):
-    assert get_new_command(command) == new_command
-
-    
-                # the (n+1)th request fails
-            with pytest.raises(requests.exceptions.ConnectionError):
-                r = requests.get(server_url)
-    
-        def test_precedence(self):
-        content = '''
-        <?xml version='1.0' encoding='XML'?>
-        <meta charset='HTML5'>
-        <meta http-equiv='Content-type' content='text/html;charset=HTML4' />
-        '''.strip()
-        assert get_encodings_from_content(content) == ['HTML5', 'HTML4', 'XML']
-    
-    # Model version
-# Orig paper: version = 1 (ResNet v1), Improved ResNet: version = 2 (ResNet v2)
-version = 1
-    
-    
-def create_multi_input_model_from(layer1, layer2):
-    input_1 = Input(shape=(data_dim,))
-    input_2 = Input(shape=(data_dim,))
-    out1 = layer1(input_1)
-    out2 = layer2(input_2)
-    out = Average()([out1, out2])
-    model = Model([input_1, input_2], out)
-    model.add_loss(K.mean(out2))
-    model.add_loss(1)
-    model.add_loss(1)
-    return model
-    
-        module = AnsibleAWSModule(argument_spec=argument_spec, supports_check_mode=False)
-    service_mgr = CloudFrontOriginAccessIdentityServiceManager(module)
-    validation_mgr = CloudFrontOriginAccessIdentityValidationManager(module)
-    
-        def enable(self):
-        '''Enables the rule in AWS'''
-        try:
-            response = self.client.enable_rule(Name=self.name)
-        except (botocore.exceptions.BotoCoreError, botocore.exceptions.ClientError) as e:
-            self.module.fail_json_aws(e, msg='Could not enable rule %s' % self.name)
-        self.changed = True
-        return response
-    
-        def describe_gateways(self, ip_address):
-        response = self.ec2.describe_customer_gateways(
-            DryRun=False,
-            Filters=[
-                {
-                    'Name': 'state',
-                    'Values': [
-                        'available',
-                    ]
-                },
-                {
-                    'Name': 'ip-address',
-                    'Values': [
-                        ip_address,
-                    ]
-                }
-            ]
-        )
-        return response
-    
-    # Gather facts about all launch configurations
-- ec2_lc_facts:
-    
-    import time
-import datetime
-    
-                        if verbosity >= 3:
-                        result['original_policy'] = original_policy
-    
-    
-DOCUMENTATION = '''
----
-module: elasticache_subnet_group
-version_added: '2.0'
-short_description: manage Elasticache subnet groups
-description:
-     - Creates, modifies, and deletes Elasticache subnet groups. This module has a dependency on python-boto >= 2.5.
-options:
-  state:
-    description:
-      - Specifies whether the subnet should be present or absent.
-    required: true
-    default: present
-    choices: [ 'present' , 'absent' ]
-  name:
-    description:
-      - Database subnet group identifier.
-    required: true
-  description:
-    description:
-      - Elasticache subnet group description. Only set when a new group is added.
-  subnets:
-    description:
-      - List of subnet IDs that make up the Elasticache subnet group.
-author: 'Tim Mahoney (@timmahoney)'
-extends_documentation_fragment:
-    - aws
-    - ec2
-'''
-    
-    
-DOCUMENTATION = '''
----
-module: iam_role_facts
-short_description: Gather information on IAM roles
-description:
-    - Gathers information about IAM roles
-version_added: '2.5'
-requirements: [ boto3 ]
-author:
-    - 'Will Thames (@willthames)'
-options:
-    name:
-        description:
-            - Name of a role to search for
-            - Mutually exclusive with C(prefix)
-        aliases:
-            - role_name
-    path_prefix:
-        description:
-            - Prefix of role I(path) to restrict IAM role search for
-            - Mutually exclusive with C(name)
-extends_documentation_fragment:
-  - aws
-  - ec2
-'''
-    
-        return results
-    
-        def client(self, resource='lambda'):
-        return self.resource_client[resource]
-    
-    
-def fix_return(node):
-    '''
-    fixup returned dictionary
-    
-    
-def main():
-    argument_spec = ec2_argument_spec()
-    argument_spec.update(dict(
-        state=dict(required=True, choices=['present', 'absent']),
-        group_name=dict(required=True, aliases=['name']),
-        group_description=dict(required=False, aliases=['description']),
-        group_subnets=dict(required=False, aliases=['subnets'], type='list'),
-    ))
-    module = AnsibleModule(argument_spec=argument_spec)
-    
-        return _split_aug_path(vhost_path)[0]
-    
-        :ivar aug: Augeas object
-    :type aug: :class:`augeas.Augeas`
-    
-    import mock
-    
-        def test_mod_config(self):
-        z_domains = []
-        for achall in self.achalls:
-            self.sni.add_chall(achall)
-            z_domain = achall.response(self.auth_key).z_domain
-            z_domains.append(set([z_domain.decode('ascii')]))
-    
-            self.challenge_conf = os.path.join(
-            self.configurator.conf('challenge-location'),
-            'le_tls_sni_01_cert_challenge.conf')
-    
-    
-def fantasy_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    if 'fantasy.tv' not in url:
-        raise Exception('Wrong place!')
+          # seq2seq_vd derived from the same code base as seq2seq_zaremba.
+      elif (FLAGS.generator_model == 'seq2seq_zaremba' or
+            FLAGS.generator_model == 'seq2seq_vd'):
+        # Encoder.
+        gen_encoder_variable_maps = variable_mapping.gen_encoder_seq2seq(
+            hparams)
+        gen_encoder_init_saver = tf.train.Saver(
+            var_list=gen_encoder_variable_maps)
+        # Decoder.
+        gen_decoder_variable_maps = variable_mapping.gen_decoder_seq2seq(
+            hparams)
+        gen_decoder_init_saver = tf.train.Saver(
+            var_list=gen_decoder_variable_maps)
+        init_savers['gen_encoder_init_saver'] = gen_encoder_init_saver
+        init_savers['gen_decoder_init_saver'] = gen_decoder_init_saver
