b829f399d8334f6d1de89511f1fdbbcccd53bc5b
SUNsung <weatherius@gmail.com>
2019-03-26 09:14:25 +0200

    Rewrite [29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb] File in: 2019-03-26 09:14:24

--- ruby/29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb
+++ ruby/29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb
@@ -1,173 +1,175 @@
 
         
-                def initialize(object_name, method_name, template_object, checked_value, unchecked_value, options)
-          @checked_value   = checked_value
-          @unchecked_value = unchecked_value
-          super(object_name, method_name, template_object, options)
-        end
-    
-            def render(&block)
-          options = @options.stringify_keys
-          tag_value = options.delete('value')
-          name_and_id = options.dup
-    
-    require 'active_support/core_ext/module/attribute_accessors'
-    
-          it 'updates an existing user' do
-        visit edit_admin_user_path(users(:bob))
-        check 'Admin'
-        click_on 'Update User'
-        expect(page).to have_text('User 'bob' was successfully updated.')
-        visit edit_admin_user_path(users(:bob))
-        expect(page).to have_checked_field('Admin')
-      end
-    
-        it 'returns a correct icon tag for GitHub' do
-      icon = omniauth_provider_icon(:github)
-      expect(icon).to be_html_safe
-      elem = Nokogiri(icon).at('i.fa.fa-github')
-      expect(elem).to be_a Nokogiri::XML::Element
+            def limit_reached_for(user_id, date)
+      GivenDailyLike.find_for(user_id, date).pluck(:limit_reached)[0] || false
     end
     
-        it 'works for running jobs' do
-      job.locked_at = Time.now
-      job.locked_by = 'test'
-      expect(status(job)).to eq('<span class='label label-info'>running</span>')
-    end
+      #
+  # Transmits a response and adds the appropriate headers.
+  #
+  def send_response(response)
+    # Set the connection to close or keep-alive depending on what the client
+    # can support.
+    response['Connection'] = (keepalive) ? 'Keep-Alive' : 'close'
     
-    describe ConvertWebsiteAgentTemplateForMerge do
-  let :old_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
-    }
+      def timestamp
+    (( ::Time.now - self.itime) * 1000.0 ).to_i & 0xffffffff
   end
     
-      describe 'validating' do
-    before do
-      expect(@checker).to be_valid
+    require 'rex/proto/ipmi/utils'
+    
+                k3 = OpenSSL::HMAC.digest('MD5', k1, checksum)
+    
+              # Encodes the checksum field
+          #
+          # @return [OpenSSL::ASN1::OctetString]
+          def encode_checksum
+            OpenSSL::ASN1::OctetString.new(checksum)
+          end
+        end
+      end
+    end
+  end
+end
+    
+              # Encodes the Rex::Proto::Kerberos::Model::Element into an ASN.1 String. This
+          # method has been designed to be overridden by subclasses.
+          #
+          # @raise [NoMethodError]
+          def encode
+            raise ::NoMethodError, 'Method designed to be overridden'
+          end
+        end
+      end
+    end
+  end
+end
+    
+                res = ''
+            case etype
+            when RC4_HMAC
+              res = decrypt_rc4_hmac(cipher, key, msg_type)
+              raise ::RuntimeError, 'EncryptedData failed to decrypt' if res.length < 8
+              res = res[8, res.length - 1]
+            else
+              raise ::NotImplementedError, 'EncryptedData schema is not supported'
+            end
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a Kerberos KRB-ERROR (response error)
+        # message definition.
+        class KrbError < Element
+          # @!attribute pvno
+          #   @return [Integer] The protocol version number
+          attr_accessor :pvno
+          # @!attribute msg_type
+          #   @return [Integer] The type of a protocol message
+          attr_accessor :msg_type
+          # @!attribute ctime
+          #   @return [Time] The current time of the client's host
+          attr_accessor :ctime
+          # @!attribute cusec
+          #   @return [Integer] The microseconds part of the client timestamp
+          attr_accessor :cusec
+          # @!attribute stime
+          #   @return [Time] The current time of the server
+          attr_accessor :stime
+          # @!attribute susec
+          #   @return [Integer] The microseconds part of the server timestamp
+          attr_accessor :susec
+          # @!attribute error_code
+          #   @return [Integer] The error request returned by kerberos or the server when a request fails
+          attr_accessor :error_code
+          # @!attribute crealm
+          #   @return [String] The realm part of the client's principal identifier
+          attr_accessor :crealm
+          # @!attribute cname
+          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
+          attr_accessor :cname
+          # @!attribute realm
+          #   @return [String] The realm part of the server's principal identifier
+          attr_accessor :realm
+          # @!attribute sname
+          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
+          attr_accessor :sname
+          # @!attribute e_data
+          #   @return [String] additional data about the error (ASN.1 encoded data)
+          attr_accessor :e_data
+    
+    rescue LoadError, NameError => e
+  $stderr.puts '\033[0;31m' \
+    '[!] Some Rake tasks haven been disabled because the environment' \
+    ' couldn’t be loaded. Be sure to run `rake bootstrap` first or use the ' \
+    'VERBOSE environment variable to see errors.\e[0m'
+  if ENV['VERBOSE']
+    $stderr.puts e.message
+    $stderr.puts e.backtrace
+    $stderr.puts
+  end
+end
+    
+    LogStash::Bundler.setup!
+    
+    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
+Gem.use_paths(LogStash::Environment.logstash_gem_home)
+    
+          packet_gem.pack
+    
+              it 'allow to install a specific version' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version 0.1.0 logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest', '0.1.0')
+          end
+        end
+      end
+    
+    shared_examples 'logstash update' do |logstash|
+  describe 'logstash-plugin update on #{logstash.hostname}' do
+    before :each do
+      logstash.install({:version => LOGSTASH_VERSION})
     end
     
-    module Docs
-  class Entry
-    class Invalid < StandardError; end
+          def datepicker_field_value(date)
+        unless date.blank?
+          l(date, format: Spree.t('date_picker.format', default: '%Y/%m/%d'))
+        end
+      end
     
-        def initialize(machine, guests, capabilities)
-      @capabilities = capabilities
-      @guests       = guests
-      @machine      = machine
-    end
-    
-            # Helper method that will set a value if a value is given, or otherwise
-        # return the already set value.
-        #
-        # @param [Symbol] key Key for the data
-        # @param [Object] value Value to store.
-        # @return [Object] Stored value.
-        def self.get_or_set(key, value=UNSET_VALUE)
-          # If no value is to be set, then return the value we have already set
-          return data[key] if value.eql?(UNSET_VALUE)
-    
-              # Make sure we're only working with one VM if single target
-          if options[:single_target] && machines.length != 1
-            @logger.debug('Using primary machine since single target')
-            primary_name = @env.primary_machine_name
-            raise Errors::MultiVMTargetRequired if !primary_name
-            machines = [get_machine.call(primary_name)]
+                expect(order.shipments.count).to eq(1)
+            expect(order.shipments.last.backordered?).to eq(false)
+            expect(order.shipments.first.inventory_units_for(product.master).sum(&:quantity)).to eq(2)
+            expect(order.shipments.first.stock_location.id).to eq(stock_location2.id)
           end
     
-              results
-        end
-    
-            last_simple_subject = rest.empty? && sseq.subject?
-        if current_rule.nil? || first_sseq(current_rule).members != firsts ||
-            !!first_sseq(current_rule).subject? != !!last_simple_subject
-          current_rule = Tree::RuleNode.new([])
-          current_rule.parsed_rules = make_sseq(last_simple_subject, *firsts)
-        end
-    
-        # The name of the file in which the exception was raised.
-    # This could be `nil` if no filename is available.
-    #
-    # @return [String, nil]
-    def sass_filename
-      sass_backtrace.first[:filename]
-    end
-    
-            if e.is_a?(Sass::SyntaxError)
-          $stderr.puts e.sass_backtrace_str('standard input')
-        else
-          $stderr.print '#{e.class}: ' unless e.class == RuntimeError
-          $stderr.puts e.message.to_s
-        end
-        $stderr.puts '  Use --trace for backtrace.'
-    
-          # Find a Sass file, if it exists.
-      #
-      # This is the primary entry point of the Importer.
-      # It corresponds directly to an `@import` statement in Sass.
-      # It should do three basic things:
-      #
-      # * Determine if the URI is in this importer's format.
-      #   If not, return nil.
-      # * Determine if the file indicated by the URI actually exists and is readable.
-      #   If not, return nil.
-      # * Read the file and place the contents in a {Sass::Engine}.
-      #   Return that engine.
-      #
-      # If this importer's format allows for file extensions,
-      # it should treat them the same way as the default {Filesystem} importer.
-      # If the URI explicitly has a `.sass` or `.scss` filename,
-      # the importer should look for that exact file
-      # and import it as the syntax indicated.
-      # If it doesn't exist, the importer should return nil.
-      #
-      # If the URI doesn't have either of these extensions,
-      # the importer should look for files with the extensions.
-      # If no such files exist, it should return nil.
-      #
-      # The {Sass::Engine} to be returned should be passed `options`,
-      # with a few modifications. `:syntax` should be set appropriately,
-      # `:filename` should be set to `uri`,
-      # and `:importer` should be set to this importer.
-      #
-      # @param uri [String] The URI to import.
-      # @param options [{Symbol => Object}] Options for the Sass file
-      #   containing the `@import` that's currently being resolved.
-      #   This is safe for subclasses to modify destructively.
-      #   Callers should only pass in a value they don't mind being destructively modified.
-      # @return [Sass::Engine, nil] An Engine containing the imported file,
-      #   or nil if it couldn't be found or was in the wrong format.
-      def find(uri, options)
-        Sass::Util.abstract(self)
-      end
-    
-            sourcemap_pathname = Sass::Util.cleanpath(sourcemap_directory)
-        begin
-          Sass::Util.file_uri_from_path(
-            Sass::Util.relative_path_from(file_pathname, sourcemap_pathname))
-        rescue ArgumentError # when a relative path cannot be constructed
-          Sass::Util.file_uri_from_path(file_pathname)
-        end
-      end
-    
-    desc 'Clean up files.'
-task :clean do |t|
-  FileUtils.rm_rf 'doc'
-  FileUtils.rm_rf 'tmp'
-  FileUtils.rm_rf 'pkg'
-  FileUtils.rm_rf 'public'
-  FileUtils.rm 'test/debug.log' rescue nil
-  FileUtils.rm 'test/paperclip.db' rescue nil
-  Dir.glob('paperclip-*.gem').each{|f| FileUtils.rm f }
-end
-
-    
-        # Swaps the height and width if necessary
-    def auto_orient
-      if EXIF_ROTATED_ORIENTATION_VALUES.include?(@orientation)
-        @height, @width = @width, @height
-        @orientation -= 4
+      context 'moving variants between shipments', js: true do
+    before do
+      create(:stock_location, name: 'LA')
+      visit spree.admin_orders_path
+      within_row(1) do
+        click_link 'R100'
       end
     end
+    
+              def find_spree_current_order
+            Spree::Api::Dependencies.storefront_current_order_finder.constantize.new.execute(
+              store: spree_current_store,
+              user: spree_current_user,
+              token: order_token,
+              currency: current_currency
+            )
+          end
+    
+          def variants_associations
+        [{ option_values: :option_type }, :default_price, :images]
+      end
+    
+            def line_items_attributes
+          { line_items_attributes: {
+            id: params[:id],
+            quantity: params[:line_item][:quantity],
+            options: line_item_params[:options] || {}
+          } }
+        end
