9f8a086c59263a3a94a85033c71bf3ab425e2432
SUNsung <weatherius@gmail.com>
2019-03-26 06:52:54 +0200

    Rewrite [12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb] File in: 2019-03-26 06:52:53

--- ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
+++ ruby/12L7AUtMXf1rA12BIMDZbGxxg12GTUCwWZrbLI126ns.rb
@@ -1,93 +1,7 @@
 
         
-                cmd << ['-am #{message.shellescape}']
-        cmd << '--force' if options[:force]
-        cmd << '-s' if options[:sign]
-        cmd << tag.shellescape
-        cmd << options[:commit].to_s if options[:commit]
-    
-          it 'accepts an input path with spaces' do
-        input_dir = 'input/dir with spaces/file'
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: '#{input_dir}'
-          )
-        end').runner.execute(:test)
-    
-          it 'Uses the provided pretty format to collect log messages' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          changelog_from_git_commits(pretty: '%s%n%b')
-        end').runner.execute(:test)
-    
-          it 'passes a list' do
-        expect_command('git', 'commit')
-        Fastlane::Actions.sh('git', 'commit')
-      end
-    
-            it 'does not auto convert Array values to Strings if not allowed' do
-          config_item = FastlaneCore::ConfigItem.new(key: :xcargs,
-                                                     description: 'xcargs',
-                                                     type: String)
-    
-    require_relative 'options'
-require_relative 'runner'
-    
-      describe '#scenario_label' do
-    it 'creates a scenario label with the scenario name' do
-      expect(scenario_label(scenario)).to eq(
-        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Scene</span>'
-      )
-    end
-    
-          context '#run_workers' do
-        it 'runs all the workers' do
-          mock.instance_of(HuginnScheduler).run!
-          mock.instance_of(DelayedJobWorker).run!
-          @agent_runner.send(:run_workers)
-        end
-    
-      it 'replaces invalid byte sequences in a message' do
-    log = AgentLog.new(:agent => agents(:jane_website_agent), level: 3)
-    log.message = '\u{3042}\xffA\x95'
-    expect { log.save! }.not_to raise_error
-    expect(log.message).to eq('\u{3042}<ff>A\<95>')
-  end
-    
-      describe '#check' do
-    it 'should check that initial run creates an event' do
-      @checker.memory[:last_updated_at] = '2016-03-15T14:01:05+00:00'
-      expect { @checker.check }.to change { Event.count }.by(1)
-    end
-  end
-end
-
-    
-    describe Agents::BasecampAgent do
-  it_behaves_like Oauthable
-    
-            a_length = a_split.length
-        b_length = b_split.length
-    
-        def base_url
-      context[:base_url]
-    end
-    
-        def initialize
-      @pages = {}
-    end
-    
-          def fetch_redirections
-        result = {}
-        with_filters 'apply_base_url', 'container', 'normalize_urls', 'internal_urls' do
-          build_pages do |page|
-            next if page[:response_effective_path] == page[:response_path]
-            result[page[:response_path].downcase] = page[:response_effective_path]
-          end
-        end
-        result
-      end
+            def create
+      authorize :email_domain_block, :create?
     
         def set_report_note
       @report_note = ReportNote.find(params[:id])
@@ -96,103 +10,84 @@ end
 end
 
     
-        render_empty
+    class Api::OEmbedController < Api::BaseController
+  respond_to :json
+    
+      respond_to :json
+    
+        @web_subscription = ::Web::PushSubscription.create!(
+      endpoint: subscription_params[:endpoint],
+      key_p256dh: subscription_params[:keys][:p256dh],
+      key_auth: subscription_params[:keys][:auth],
+      data: data_params,
+      user_id: current_user.id,
+      access_token_id: doorkeeper_token.id
+    )
+    
+        # default chunk sizes (if chunked is used)
+    self.chunk_min_size = 1
+    self.chunk_max_size = 10
+    
+      def timestamp
+    (( ::Time.now - self.itime) * 1000.0 ).to_i & 0xffffffff
   end
     
-    module ExportControllerConcern
-  extend ActiveSupport::Concern
     
-      def rate_limit_reset
-    (request_time + reset_period_offset).iso8601(6)
-  end
+  # open rmcpplus_request with cipherzero
+  def self.create_ipmi_session_open_cipher_zero_request(console_session_id)
+    head = [
+      0x06, 0x00, 0xff, 0x07,   # RMCP Header
+      0x06,                     # RMCP+ Authentication Type
+      PAYLOAD_RMCPPLUSOPEN_REQ, # Payload Type
+      0x00, 0x00, 0x00, 0x00,   # Session ID
+      0x00, 0x00, 0x00, 0x00    # Sequence Number
+    ].pack('C*')
     
-      #
-  # More advanced [] that does downcase comparison.
-  #
-  def [](key)
-    begin
-      rv = self.fetch(key)
-    rescue IndexError
-      rv = nil
-    end
-    if (rv == nil)
-      begin
-        rv = self.dcase_hash[key.downcase]
-      rescue IndexError
-        rv = nil
-      end
-    end
-    
-          close_client(cli)
-    end
-  end
-    
-      attr_accessor :client
-  attr_accessor :oseq, :iseq
-  attr_accessor :scall, :dcall
-  attr_accessor :codec, :state
-  attr_accessor :ring_start, :ring_finish
-  attr_accessor :itime
-  attr_accessor :queue
-  attr_accessor :audio_hook
-  attr_accessor :audio_buff
-  attr_accessor :time_limit
-  attr_accessor :busy
-    
-              # Encodes a Rex::Proto::Kerberos::Model::AuthorizationData into an ASN.1 String
+              # Encrypts the cipher using RC4-HMAC schema
           #
-          # @return [String]
-          def encode
-            seqs = []
-            elements.each do |elem|
-              elems = []
-              type_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_type(elem[:type])], 0, :CONTEXT_SPECIFIC)
-              elems << type_asn1
-              data_asn1 = OpenSSL::ASN1::ASN1Data.new([encode_data(elem[:data])], 1, :CONTEXT_SPECIFIC)
-              elems << data_asn1
-              seqs << OpenSSL::ASN1::Sequence.new(elems)
-            end
+          # @param data [String] the data to encrypt
+          # @param key [String] the key to encrypt
+          # @param msg_type [Integer] the message type
+          # @return [String] the encrypted data
+          def encrypt_rc4_hmac(data, key, msg_type)
+            k1 = OpenSSL::HMAC.digest('MD5', key, [msg_type].pack('V'))
     
-              # Decodes a Rex::Proto::Kerberos::Model::KdcRequest
+              # Decodes a Rex::Proto::Kerberos::Model::KdcRequestBody from an
+          # OpenSSL::ASN1::Sequence
           #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @param input [OpenSSL::ASN1::Sequence] the input to decode from
           # @raise [RuntimeError] if decoding doesn't succeed
           def decode_asn1(input)
-            input.value[0].value.each do |val|
-              case val.tag
-              when 1
-                self.pvno = decode_asn1_pvno(val)
-              when 2
-                self.msg_type = decode_asn1_msg_type(val)
-              when 3
-                self.pa_data  = decode_asn1_pa_data(val)
-              when 4
-                self.req_body = decode_asn1_req_body(val)
-              else
-                raise ::RuntimeError, 'Failed to decode KdcRequest SEQUENCE'
-              end
+            seq_values = input.value
+    
+              # Decodes a Rex::Proto::Kerberos::Model::LastRequest
+          #
+          # @param input [String, OpenSSL::ASN1::Sequence] the input to decode from
+          # @return [self] if decoding succeeds
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode(input)
+            case input
+            when String
+              decode_string(input)
+            when OpenSSL::ASN1::Sequence
+              decode_asn1(input)
+            else
+              raise ::RuntimeError, 'Failed to decode LastRequest, invalid input'
             end
-          end
     
-              # Decodes the msg_type from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_msg_type(input)
-            input.value[0].value.to_i
-          end
+    When /^I submit forgot password form$/ do
+  submit_forgot_password_form
+end
     
-              # Decodes the ctime field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_ctime(input)
-            input.value[0].value
-          end
+    World(NavigationHelpers)
+
     
-              # Rex::Proto::Kerberos::Model::LastRequest encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'LastRequest encoding not supported'
-          end
+        context 'on my own post' do
+      before do
+        aspect_to_post = alice.aspects.where(:name => 'generic').first
+        @post = alice.post :status_message, :text => 'something', :to => aspect_to_post
+      end
+    
+              state = @states.last
+          respond_with(@states) if stale?(state)
+        end
