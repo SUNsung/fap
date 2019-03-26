97de7c43e3e576b6a4ed6c1907e75678aaf757e7
SUNsung <weatherius@gmail.com>
2019-03-26 17:35:05 +0200

    Rewrite [56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb] File in: 2019-03-26 17:35:05

--- ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
+++ ruby/56ufcSuGd78Ho561rusYThfe525643E5V7bkDrc56Fb6.rb
@@ -1,165 +1,149 @@
 
         
-        module Gitlab
-  module BackgroundMigration
-    class PopulateMergeRequestsLatestMergeRequestDiffId
-      BATCH_SIZE = 1_000
+            def at_xpath(*args)
+      doc.at_xpath(*args)
+    end
     
-    module Gitlab
-  module Ci
-    module Pipeline
-      # Class for preloading data associated with pipelines such as commit
-      # authors.
-      class Preloader
-        def self.preload!(pipelines)
-          ##
-          # This preloads all commits at once, because `Ci::Pipeline#commit` is
-          # using a lazy batch loading, what results in only one batched Gitaly
-          # call.
+        def assert_index(index)
+      i = index.is_a?(Integer) ? index : @filters.index(filter_const(index))
+      raise 'No such filter to insert: #{index}' unless i
+      i
+    end
+  end
+end
+
+    
+        private
+    
+            if mod
+          if name == 'Index'
+            return slug.split('/')[1..-2].join('/')
+          elsif name == 'Angular'
+            return slug.split('/').last.split('-').first
+          end
+        end
+    
+        if self.junk_headers
+      while str.length < 4096
+        if self.fold
+          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}:\r\n\t#{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
+        else
+          str << 'X-#{Rex::Text.rand_text_alphanumeric(rand(30) + 5)}: #{Rex::Text.rand_text_alphanumeric(rand(1024) + 1)}\r\n'
+        end
+      end
+    end
+    
+        # Send it off.
+    put(response.to_s)
+  end
+    
+    IAX_CTRL_HANGUP   = 1
+IAX_CTRL_RINGING  = 3
+IAX_CTRL_ANSWER   = 4
+IAX_CTRL_BUSY     = 5
+IAX_CTRL_PROGRESS = 14
+IAX_CTRL_PROCEED  = 15
+    
+        data =
+    [   # Maximum access
+      0x00, 0x00,
+      # Reserved
+      0x00, 0x00
+    ].pack('C*') +
+    console_session_id +
+    [
+      0x00, 0x00, 0x00, 0x08,
+      0x01, 0x00, 0x00, 0x00,
+      0x01, 0x00, 0x00, 0x08,
+      # HMAC-SHA1
+      0x01, 0x00, 0x00, 0x00,
+      0x02, 0x00, 0x00, 0x08,
+      # AES Encryption
+      0x01, 0x00, 0x00, 0x00
+    ].pack('C*')
+    
+              # Encodes the pvno field
           #
-          pipelines.each(&:commit)
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_pvno
+            bn = OpenSSL::BN.new(pvno.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
     
-          # Reads a cache key.
-      #
-      # If the key exists and has a non-empty value its TTL is refreshed
-      # automatically.
-      #
-      # raw_key - The cache key to read.
-      # timeout - The new timeout of the key if the key is to be refreshed.
-      def self.read(raw_key, timeout: TIMEOUT)
-        key = cache_key_for(raw_key)
-        value = Redis::Cache.with { |redis| redis.get(key) }
+              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_pvno(input)
+            input.value[0].value.to_i
+          end
     
-            def representation_class
-          Representation::Issue
+              # Decodes the key_type from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_type(input)
+            input.value[0].value.to_i
+          end
+    
+            fields = object.preferences.keys.map do |key|
+          if object.has_preference?(key)
+            form.label('preferred_#{key}', Spree.t(key) + ': ') +
+              preference_field_for(form, 'preferred_#{key}', type: object.preference_type(key))
+          end
         end
-    
-            def collection_method
-          :lfs_objects
-        end
-    
-          # Associates the given database ID with the current object.
-      #
-      # database_id - The ID of the corresponding database row.
-      def cache_database_id(database_id)
-        Caching.write(cache_key, database_id)
+        safe_join(fields, '<br />'.html_safe)
       end
     
-          # Imports all the objects in sequence in the current thread.
-      def sequential_import
-        each_object_to_import do |object|
-          repr = representation_class.from_api_response(object)
-    
-              new(hash)
-        end
-    
-          def parse_uri(location)
-        location && URI.parse(location)
-      rescue URI::InvalidURIError
-        nil
-      end
-    
-        def initialize(name, options) #:nodoc:
-      @scoped_path = options[:as] ? '#{options[:as]}/#{name}' : name.to_s
-      @singular = (options[:singular] || @scoped_path.tr('/', '_').singularize).to_sym
-    
-        def pos
-      byte_to_str_pos @s.pos
-    end
-    
-        def log_file_info(s)
-      puts '    #{magenta s}'
-    end
-    
-          # Install the gems to make them available locally when bundler does his local resolution
-      post_install_messages = []
-      pack.gems.each do |packed_gem|
-        PluginManager.ui.debug('Installing, #{packed_gem.name}, version: #{packed_gem.version} file: #{packed_gem.file}')
-        post_install_messages << LogStash::PluginManager::GemInstaller::install(packed_gem.file, packed_gem.plugin?)
-      end
-    
-        context 'update a specific plugin' do
-      it 'has executed successfully' do
-        cmd = logstash.run_command_in_path('bin/logstash-plugin update --no-verify #{plugin_name}')
-        expect(cmd.stdout).to match(/Updating #{plugin_name}/)
-        expect(logstash).not_to have_installed?(plugin_name, previous_version)
+      context 'shipping an order', js: true do
+    before do
+      visit spree.admin_orders_path
+      within_row(1) do
+        click_link 'R100'
       end
     end
     
-          # Returns the collection the `for` loop is iterating over.
-      #
-      # @return [Node] The collection the `for` loop is iterating over
-      def collection
-        node_parts[1]
-      end
-    
-          # Custom destructuring method. This is used to normalize the branches
-      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
-      #
-      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
-      def node_parts
-        [self, self]
+              def serialized_current_order
+            serialize_order(spree_current_order)
+          end
+        end
       end
     end
   end
 end
 
     
-    module RuboCop
-  module AST
-    # Common functionality for nodes that can be used as hash elements:
-    # `pair`, `kwsplat`
-    module HashElementNode
-      # Returns the key of this `hash` element.
-      #
-      # @note For keyword splats, this returns the whole node
-      #
-      # @return [Node] the key of the hash element
-      def key
-        node_parts[0]
-      end
+            def order_params
+          if params[:order]
+            normalize_params
+            params.require(:order).permit(permitted_order_attributes)
+          else
+            {}
+          end
+        end
     
-        def extra_enabled_comments
-      extra_enabled_comments_with_names([], {})
-    end
-    
-      s.add_dependency 'bootstrap',       '~> 4.1.3'
-  s.add_dependency 'glyphicons',      '~> 1.0.2'
-  s.add_dependency 'jquery-rails',    '~> 4.3'
-  s.add_dependency 'jquery-ui-rails', '~> 6.0.1'
-  s.add_dependency 'select2-rails',   '3.5.9.1' # 3.5.9.2 breaks several specs
-end
+    Liquid::Template.register_tag('blockquote', Jekyll::Blockquote)
 
     
-          def authenticate_user
-        return if @current_api_user
-    
-            def address_params
-          params.require(:address).permit(permitted_address_attributes)
-        end
-    
-            def advance
-          authorize! :update, @order, order_token
-          while @order.next; end
-          respond_with(@order, default_template: 'spree/api/v1/orders/show', status: 200)
-        end
-    
-        worker_count.times do |count|
-      template '/data/#{app}/shared/config/sidekiq_#{count}.yml' do
-        owner node[:owner_name]
-        group node[:owner_name]
-        mode 0644
-        source 'sidekiq.yml.erb'
-        variables({
-          :require => '/data/#{app}/current'
-        })
-      end
+        # Outputs a single category as an <a> link.
+    #
+    #  +category+ is a category string to format as an <a> link
+    #
+    # Returns string
+    #
+    def category_link(category)
+      dir = @context.registers[:site].config['category_dir']
+      '<a class='category' href='/#{dir}/#{category.to_url}/'>#{category}</a>'
     end
     
+          unless file.file?
+        return 'File #{file} could not be found'
+      end
     
-    # By leaving this as a class method, it can be pluggable and used by the Manager actor. Making it
-    # an instance method will make it async to the Fetcher actor
-    def self.bulk_requeue(inprogress, options)
-      return if inprogress.empty?
-    
-        private
+      # Used on the blog index to split posts on the <!--more--> marker
+  def excerpt(input)
+    if input.index(/<!--\s*more\s*-->/i)
+      input.split(/<!--\s*more\s*-->/i)[0]
+    else
+      input
+    end
+  end
