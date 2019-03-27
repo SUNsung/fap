c75856054d9c34606f0585fb427aa8a20cb75628
SUNsung <weatherius@gmail.com>
2019-03-27 09:40:00 +0200

    Rewrite [01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb] File in: 2019-03-27 09:39:59

--- ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
+++ ruby/01svmYlRW7kqk01PdPN7ROsCdU01p9RPcI9ESO601r2G.rb
@@ -1,119 +1,34 @@
 
         
-                # Parses the options given an OptionParser instance.
-        #
-        # This is a convenience method that properly handles duping the
-        # originally argv array so that it is not destroyed.
-        #
-        # This method will also automatically detect '-h' and '--help'
-        # and print help. And if any invalid options are detected, the help
-        # will be printed, as well.
-        #
-        # If this method returns `nil`, then you should assume that help
-        # was printed and parsing failed.
-        def parse_options(opts=nil)
-          # Creating a shallow copy of the arguments so the OptionParser
-          # doesn't destroy the originals.
-          argv = @argv.dup
-    
-            # This is the method called to 'prepare' the provisioner. This is called
-        # before any actions are run by the action runner (see {Vagrant::Actions::Runner}).
-        # This can be used to setup shared folders, forward ports, etc. Whatever is
-        # necessary on a 'meta' level.
-        #
-        # No return value is expected.
-        def prepare
-        end
-    
-              nil
-        end
-    
-    class FormulaPin
-  def initialize(f)
-    @f = f
+        begin
+  require 'cucumber/rake/task'
+  Cucumber::Rake::Task.new(:features) do |t|
+    t.profile = 'travis'
   end
-    
-          def accepts?(env)
-        raise NotImplementedError, '#{self.class} implementation pending'
-      end
-    
-          DIRECTIVES = %i(base_uri child_src connect_src default_src
-                      font_src form_action frame_ancestors frame_src
-                      img_src manifest_src media_src object_src
-                      plugin_types referrer reflected_xss report_to
-                      report_uri require_sri_for sandbox script_src
-                      style_src worker_src).freeze
-    
-          def call(env)
-        status, headers, body = super
-        response = Rack::Response.new(body, status, headers)
-        request = Rack::Request.new(env)
-        remove_bad_cookies(request, response)
-        response.finish
-      end
-    
-      it 'should not override the header if already set' do
-    mock_app with_headers('Content-Security-Policy' => 'default-src: none')
-    expect(get('/', {}, 'wants' => 'text/html').headers['Content-Security-Policy']).to eq('default-src: none')
+  Cucumber::Rake::Task.new(:'features:html', 'Run Cucumber features and produce HTML output') do |t|
+    t.profile = 'html_report'
+  end
+rescue LoadError
+  desc 'Cucumber rake task not available'
+  task :features do
+    abort 'Cucumber rake task is not available. Be sure to install cucumber as a gem or plugin'
   end
 end
-
     
-          validate_plugins!
-    end
+            def initialize(_opts)
+          # If EventMachine SSL support on Windows ever gets better, the code below will
+          # set up the reactor to handle SSL
+          #
+          # @ssl_enabled = opts['ssl_cert'] && opts['ssl_key']
+          # if @ssl_enabled
+          #   em_opts[:tls_options] = {
+          #   :private_key_file => Jekyll.sanitized_path(opts['source'], opts['ssl_key']),
+          #   :cert_chain_file  => Jekyll.sanitized_path(opts['source'], opts['ssl_cert'])
+          #   }
+          #   em_opts[:secure] = true
+          # end
     
-            return nil
-      end
-    end
+      def pinned_version
+    Keg.new(path.resolved_path).version if pinned?
   end
-end end end
-
-    
-        before do
-      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
-      # Logstash won't update when we have a pinned version in the gemfile so we remove them
-      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
-      expect(logstash).to have_installed?(plugin_name, previous_version)
-    end
-    
-    # Example for adding special attributes
-package.attributes[:deb_group] = 'super-useful'
-package.attributes[:rpm_group] = 'super-useful'
-    
-              # directories have a magic string inserted into their name
-          full_record_path = extension_header[TAR_NAME_OFFSET_START..TAR_NAME_OFFSET_END].delete('\0')
-          full_record_path = add_paxstring(full_record_path)
-    
-      def add_path(tar, tar_path, path)
-    stat = File.lstat(path)
-    if stat.directory?
-      tar.mkdir(tar_path, stat.mode)
-    elsif stat.symlink?
-      tar.add_symlink(tar_path, File.readlink(path), stat.mode)
-    else
-      tar.add_file_simple(tar_path, stat.mode, stat.size) do |io|
-        File.open(path) do |fd|
-          chunk = nil
-          size = 0
-          while chunk = fd.read(16384) do
-            size += io.write(chunk)
-          end
-          if size != stat.size
-            raise 'Failed to add #{path} to the archive; expected to ' +
-                  'write #{stat.size} bytes, only wrote #{size}'
-          end
-        end
-      end # tar.tar.add_file_simple
-    end
-  end # def add_path
-    
-        self.name = control['pkgname'][0]
-    
-        File.write(build_path('description'), self.description + '\n')
-    
-          if !attributes[:python_setup_py_arguments].nil? and !attributes[:python_setup_py_arguments].empty?
-        # Add optional setup.py arguments
-        attributes[:python_setup_py_arguments].each do |a|
-          flags += [ a ]
-        end
-      end
+end
