2714b75dca6fa45efd466511b6c60b51b14de245
SUNsung <weatherius@gmail.com>
2019-03-27 05:14:21 +0200

    Rewrite [35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py] File in: 2019-03-27 05:14:21

--- pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
+++ pyton/35I9Vdajhzwi235gMdW1jQ3YGs35Ejdu6LUC6Rc35gBh.py
@@ -1,3 +1,120 @@
 
         
         
+class BtreeGinExtension(CreateExtension):
+    
+            r = None
+        try:
+            r = Redirect.objects.get(site=current_site, old_path=full_path)
+        except Redirect.DoesNotExist:
+            pass
+        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
+            try:
+                r = Redirect.objects.get(
+                    site=current_site,
+                    old_path=request.get_full_path(force_append_slash=True),
+                )
+            except Redirect.DoesNotExist:
+                pass
+        if r is not None:
+            if r.new_path == '':
+                return self.response_gone_class()
+            return self.response_redirect_class(r.new_path)
+    
+        def delete(self, session_key=None):
+        if session_key is None:
+            if self.session_key is None:
+                return
+            session_key = self.session_key
+        self._cache.delete(self.cache_key_prefix + session_key)
+    
+        def delete(self, session_key=None):
+        super().delete(session_key)
+        if session_key is None:
+            if self.session_key is None:
+                return
+            session_key = self.session_key
+        self._cache.delete(self.cache_key_prefix + session_key)
+    
+            exist_ca_sha1 = get_exist_ca_sha1()
+        if exist_ca_sha1 == ca_hash:
+            xlog.info('GoAgent CA exist')
+            return
+    
+            self.in_fd = open('ipv6_list.txt', 'r')
+        self.out_fd = open(
+            os.path.join(module_data_path, 'ipv6_list.txt'),
+            'w'
+        )
+    
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    
+        
+    def getStart(self):
+        return self.start
+    
+            if row1 == row2:
+            plaintext += table[row1*5+(col1-1)%5]
+            plaintext += table[row2*5+(col2-1)%5]
+        elif col1 == col2:
+            plaintext += table[((row1-1)%5)*5+col1]
+            plaintext += table[((row2-1)%5)*5+col2]
+        else: # rectangle
+            plaintext += table[row1*5+col2]
+            plaintext += table[row2*5+col1]
+    
+    
+def b_expo(a, b):
+    res = 1
+    while b > 0:
+        if b&1:
+            res *= a
+    
+    
+# ALTERNATIVE METHODS  
+# ctbi= characters that must be in password
+# i= how many letters or characters the password length will be 
+def password_generator(ctbi, i):
+  # Password generator = full boot with random_number, random_letters, and random_character FUNCTIONS
+  pass  # Put your code here...
+    
+    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html(url)
+    title = r1(r'<title>(.*)</title>', html)
+    assert title
+    id = r1(r'flvid\s*=\s*(\d+)', html)
+    assert id
+    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
+    
+    __all__ = ['giphy_download']
+    
+    class Imgur(VideoExtractor):
+    name = 'Imgur'
+    
+        # 读取文件
+    for f in files_list:
+        txt = open(f).read()
+        # words = word_tokenize(txt)
+        words = tokenizer.tokenize(txt)
+        # creating inverted index data structure
+        for word in words:
+            word = word_clean(word)  # 单词清洗
+            if word not in index:
+                index[word] = {f}
+            else:
+                index[word].add(f)
+    
+    
+# TODO(huay)
+def conv1d():
+    ''''''
+    
+        return g
+    
+    
+def foo():
+    print('foo')
+    
+            y.shape == [128, 64, 32]
+        y = permute(x, [2, 1, 0])
+        y.shape == [32, 64, 128]
