98f3064819d5af748c6ab92128db338a8c3f3d26
SUNsung <weatherius@gmail.com>
2019-03-27 15:43:39 +0200

    Rewrite [14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py] File in: 2019-03-27 15:43:38

--- pyton/14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py
+++ pyton/14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py
@@ -1,284 +1,127 @@
 
         
-          print ('loading data from ' + data_path + ' with stem ' + data_fname_stem)
-  for fname in fnames:
-    if fname.startswith(data_fname_stem):
-      data_dict = read_data(os.path.join(data_path,fname))
-      idx = len(data_fname_stem) + 1
-      key = fname[idx:]
-      data_dict['data_dim'] = data_dict['train_data'].shape[2]
-      data_dict['num_steps'] = data_dict['train_data'].shape[1]
-      dataset_dict[key] = data_dict
+          # Four initially random vectors for the special tokens: <PAD>, <UNK>, <X>, <Y>
+  special_embeddings = np.random.normal(0, 0.1, (4, dim))
+  embeddings = np.vstack((special_embeddings, embeddings))
+  embeddings = embeddings.astype(np.float32)
     
-        batch_size, num_timesteps = self.shape
-    softmax = softmax.reshape((num_timesteps, batch_size, -1))
-    softmax = np.transpose(softmax, [1, 0, 2])
-    probs = np.array([[softmax[row, col, target_ids[row, col]]
-                       for col in range(num_timesteps)]
-                      for row in range(batch_size)])
-    print(probs)
-    return probs
     
-        self._char_set = chars_set
-    num_words = len(self._id_to_word)
+# Note that with N small, (as it is 25 above), the finite size effects
+# will have pretty dramatic effects on the dynamics of the random RNN.
+# If you want more complex dynamics, you'll have to run the script a
+# lot, or increase N (or g).
     
-      wasserstein_loss = real_avg - fake_avg
-  return wasserstein_loss
     
-        for index, pred, crossent in zip(ind_batch, pred_batch, crossent_batch):
-      metrics.append([str(id_to_word[index]), pred, crossent])
+data = { 'train_truth': data_train_truth,
+         'valid_truth': data_valid_truth,
+         'train_data' : data_train_spiking,
+         'valid_data' : data_valid_spiking,
+         'train_percentage' : train_percentage,
+         'nreplications' : nreplications,
+         'dt' : FLAGS.dt,
+         'u_std' : FLAGS.u_std,
+         'max_firing_rate': FLAGS.max_firing_rate,
+         'train_inputs_u': train_inputs_u,
+         'valid_inputs_u': valid_inputs_u,
+         'train_outputs_u': train_outputs_u,
+         'valid_outputs_u': valid_outputs_u,
+         'conversion_factor' : FLAGS.max_firing_rate/(1.0/FLAGS.dt) }
     
-      Returns:
-    Tuple of the (sequence, logits, log_probs) of the Generator.   Sequence
-      and logits have shape [batch_size, sequence_length, vocab_size].  The
-      log_probs will have shape [batch_size, sequence_length].  Log_probs
-      corresponds to the log probability of selecting the words.
-  '''
-  if FLAGS.generator_model == 'rnn':
-    (sequence, logits, log_probs, initial_state, final_state) = rnn.generator(
-        hparams,
-        inputs,
-        targets,
-        present,
-        is_training=is_training,
-        is_validating=is_validating,
-        reuse=reuse)
-  elif FLAGS.generator_model == 'rnn_zaremba':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = rnn_zaremba.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_zaremba':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq_zaremba.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'rnn_nas':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = rnn_nas.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_nas':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq_nas.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_vd':
-    (sequence, logits, log_probs, initial_state, final_state,
-     encoder_states) = seq2seq_vd.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
+      if FLAGS.mode == 'eval':
+    dataset = data_utils.LM1BDataset(FLAGS.input_data, vocab)
+    _EvalModel(dataset)
+  elif FLAGS.mode == 'sample':
+    _SampleModel(FLAGS.prefix, vocab)
+  elif FLAGS.mode == 'dump_emb':
+    _DumpEmb(vocab)
+  elif FLAGS.mode == 'dump_lstm_emb':
+    _DumpSentenceEmbedding(FLAGS.sentence, vocab)
   else:
-    raise NotImplementedError
-  return (sequence, logits, log_probs, initial_state, final_state,
-          encoder_states)
+    raise Exception('Mode not supported.')
     
-            # init attention
-        attention = _init_attention(encoder_state)
-      else:
-        # construct attention
-        attention = attention_construct_fn(cell_output, attention_keys,
-                                           attention_values)
-        cell_output = attention
+      def assign_probs(self, sentences, test_data_name='wsc273'):
+    '''Return prediction accuracy using this LM for a test.'''
     
-    print()
-
+        prediction_correctness.append(
+        (scoring2['joint_prob'] > scoring1['joint_prob']) ==
+         scoring2['correctness'])
     
-    
-if len(sys.argv) <= 1:
-    print('Specify the version number as parameter')
-    sys.exit()
-version = sys.argv[1]
-    
-    entry_template = textwrap.dedent('''
-    <entry>
-        <id>https://yt-dl.org/feed/youtube-dl-updates-feed/youtube-dl-@VERSION@</id>
-        <title>New version @VERSION@</title>
-        <link href='http://rg3.github.io/youtube-dl' />
-        <content type='xhtml'>
-            <div xmlns='http://www.w3.org/1999/xhtml'>
-                Downloads available at <a href='https://yt-dl.org/downloads/@VERSION@/'>https://yt-dl.org/downloads/@VERSION@/</a>
-            </div>
-        </content>
-        <author>
-            <name>The youtube-dl maintainers</name>
-        </author>
-        <updated>@TIMESTAMP@</updated>
-    </entry>
-    ''')
-    
-        bug_text = re.search(
-        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
-    dev_text = re.search(
-        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
-        readme).group(1)
+      Args:
+    hparams:  Hyperparameters for the MaskGAN.
+    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
+    is_training:  Whether the model is training.
+    reuse (Optional):  Whether to reuse the model.
     
     
-def build_lazy_ie(ie, name):
-    valid_url = getattr(ie, '_VALID_URL', None)
-    s = ie_template.format(
-        name=name,
-        bases=', '.join(map(get_base_name, ie.__bases__)),
-        valid_url=valid_url,
-        module=ie.__module__)
-    if ie.suitable.__func__ is not InfoExtractor.suitable.__func__:
-        s += '\n' + getsource(ie.suitable)
-    if hasattr(ie, '_make_valid_url'):
-        # search extractors
-        s += make_valid_template.format(valid_url=ie._make_valid_url())
-    return s
-    
-    youtube\-dl \- download videos from youtube.com or other video platforms
-    
-            ydl = YDL({'format': 'best [filesize = 1000] [width!=450]'})
-        ydl.process_ie_result(info_dict)
-        downloaded = ydl.downloaded_info_dicts[0]
-        self.assertEqual(downloaded['format_id'], 'C')
-    
-        def test_facebook_matching(self):
-        self.assertTrue(FacebookIE.suitable('https://www.facebook.com/Shiniknoh#!/photo.php?v=10153317450565268'))
-        self.assertTrue(FacebookIE.suitable('https://www.facebook.com/cindyweather?fref=ts#!/photo.php?v=10152183998945793'))
+def create_critic_train_op(hparams, critic_loss, global_step):
+  '''Create Discriminator train op.'''
+  with tf.name_scope('train_critic'):
+    critic_optimizer = tf.train.AdamOptimizer(hparams.critic_learning_rate)
+    output_vars = [
+        v for v in tf.trainable_variables() if v.op.name.startswith('critic')
+    ]
     
     
-class TestCache(unittest.TestCase):
-    def setUp(self):
-        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
-        _mkdir(TESTDATA_DIR)
-        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
-        self.tearDown()
+def gen_encoder_seq2seq_nas(hparams):
+  '''Returns the NAS Variable name to MaskGAN Variable
+  dictionary mapping.  This is a highly restrictive function just for testing.
+  This is for the *unidirecitional* seq2seq_nas encoder.
     
-        def test_template(self):
-        ie = youtube_dl.extractor.get_info_extractor(test_case['name'])()
-        other_ies = [get_info_extractor(ie_key)() for ie_key in test_case.get('add_ie', [])]
-        is_playlist = any(k.startswith('playlist') for k in test_case)
-        test_cases = test_case.get(
-            'playlist', [] if is_playlist else [test_case])
+      with tf.variable_scope('dis', reuse=reuse):
+    cell_fwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
+        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
+    cell_bwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
+        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
+    if FLAGS.zoneout_drop_prob > 0.0:
+      cell_fwd = zoneout.ZoneoutWrapper(
+          cell_fwd,
+          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
+          is_training=is_training)
+      cell_bwd = zoneout.ZoneoutWrapper(
+          cell_bwd,
+          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
+          is_training=is_training)
     
-    from __future__ import print_function
-import keras
-from keras.layers import Dense, Conv2D, BatchNormalization, Activation
-from keras.layers import AveragePooling2D, Input, Flatten
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras.callbacks import ReduceLROnPlateau
-from keras.preprocessing.image import ImageDataGenerator
-from keras.regularizers import l2
-from keras import backend as K
-from keras.models import Model
-from keras.datasets import cifar10
+    from .cifar import load_batch
+from ..utils.data_utils import get_file
+from .. import backend as K
 import numpy as np
 import os
     
-        y_train = np.reshape(y_train, (len(y_train), 1))
-    y_test = np.reshape(y_test, (len(y_test), 1))
+        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
+        loss = MSE_MAE_loss(0.3)
+        inputs = keras.layers.Input((2,))
+        outputs = keras.layers.Dense(1, name='model_output')(inputs)
+        model = keras.models.Model(inputs, outputs)
+        model.compile(optimizer='sgd', loss={'model_output': loss})
+        model.fit(np.random.rand(256, 2), np.random.rand(256, 1))
+        model.save(model_filename)
     
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    xint = x.astype('int32')
-    assert utils.preprocess_input(x).shape == x.shape
-    assert utils.preprocess_input(xint).shape == xint.shape
+    batch_size = 128
+num_classes = 10
+epochs = 20
     
         # Returns
-        A normalized copy of the array.
+        Tensor with one scalar loss entry per sample.
     '''
-    l2 = np.atleast_1d(np.linalg.norm(x, order, axis))
-    l2[l2 == 0] = 1
-    return x / np.expand_dims(l2, axis)
+    def _logcosh(x):
+        return x + K.softplus(-2. * x) - K.log(2.)
+    return K.mean(_logcosh(y_pred - y_true), axis=-1)
+    
+    *TL;DR80
+Delays the eval of an expr until its value is needed and avoids repeated evals.
+'''
+    
+    ### OUTPUT ###
+# [{'objectb': 'b-value'}, {'default': 'default'}, {'objecta': 'a-value'}]
 
     
-        def _canonical_to_params(self, weights, biases):
-        import tensorflow as tf
-        weights = [tf.reshape(x, (-1,)) for x in weights]
-        biases = [tf.reshape(x, (-1,)) for x in biases]
-        return tf.concat(weights + biases, 0)
+        def _send_diagnostics_request(self):
+        return 'send diagnostic request'
     
-    print('Build model...')
-model = Sequential()
-    
-        def __init__(self, *a, **kw):
-        super(QPSSpider, self).__init__(*a, **kw)
-        if self.qps is not None:
-            self.qps = float(self.qps)
-            self.download_delay = 1 / self.qps
-        elif self.download_delay is not None:
-            self.download_delay = float(self.download_delay)
-    
-        def _print_response(self, response, opts):
-        if opts.headers:
-            self._print_headers(response.request.headers, b'>')
-            print('>')
-            self._print_headers(response.headers, b'<')
-        else:
-            self._print_bytes(response.body)
-    
-        def long_desc(self):
-        return 'Run the spider defined in the given file'
-    
-        class ScrapyClientContextFactory(ClientContextFactory):
-        'A SSL context factory which is more permissive against SSL bugs.'
-        # see https://github.com/scrapy/scrapy/issues/82
-        # and https://github.com/scrapy/scrapy/issues/26
-        # and https://github.com/scrapy/scrapy/issues/981
-    
-    from scrapy.exceptions import NotConfigured
-from scrapy.utils.httpobj import urlparse_cached
-from scrapy.utils.boto import is_botocore
-from .http import HTTPDownloadHandler
-    
-        def _build_response(self, body, request):
-        request.meta['download_latency'] = self.headers_time-self.start_time
-        status = int(self.status)
-        headers = Headers(self.response_headers)
-        respcls = responsetypes.from_args(headers=headers, url=self._url)
-        return respcls(url=self._url, status=status, headers=headers, body=body)
-    
-            while not self._needs_backout(spider):
-            if not self._next_request_from_scheduler(spider):
-                break
-    
-    try:
-    # compatible for python2
-    from urllib2 import urlopen
-    from urllib2 import HTTPError
-    from urllib2 import URLError
-except ImportError:
-    # compatible for python3
-    from urllib.request import urlopen
-    from urllib.error import HTTPError
-    from urllib.error import URLError
+    '''
+*What is this pattern about?
+The Decorator pattern is used to dynamically add a new feature to an
+object without changing its implementation. It differs from
+inheritance because the new feature is added only to that particular
+object, not to the entire subclass.
