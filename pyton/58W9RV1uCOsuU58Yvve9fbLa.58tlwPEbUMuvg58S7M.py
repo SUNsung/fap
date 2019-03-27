c064c2abc1cd45e07dfa94fffb0245ef5207587e
SUNsung <weatherius@gmail.com>
2019-03-27 02:49:05 +0200

    Rewrite [58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py] File in: 2019-03-27 02:49:04

--- pyton/58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py
+++ pyton/58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py
@@ -1,197 +1,98 @@
 
         
-        python_2 = (u'thefuck/python2-bash',
-            u'FROM python:2',
-            u'sh')
-    
-    containers = (('thefuck/python3-fish',
-               u'''FROM python:3
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'),
-              ('thefuck/python2-fish',
-               u'''FROM python:2
-                   # Use jessie-backports since it has the fish package. See here for details:
-                   # https://github.com/tianon/docker-brew-debian/blob/88ae21052affd8a14553bb969f9d41c464032122/jessie/backports/Dockerfile
-                   RUN awk '$1 ~ '^deb' { $3 = $3 '-backports'; print; exit }' /etc/apt/sources.list > /etc/apt/sources.list.d/backports.list
-                   RUN apt-get update
-                   RUN apt-get install -yy fish''',
-               u'fish'))
-    
-    
-@pytest.mark.parametrize('command, new_command, packages', [
-    (Command('vim', ''), 'sudo apt-get install vim && vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')]),
-    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
-     [('vim', 'main'), ('vim-tiny', 'main')]),
-    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
-     [('imagemagick', 'main'),
-      ('graphicsmagick-imagemagick-compat', 'universe')])])
-def test_get_new_command(mocker, command, new_command, packages):
-    mocker.patch('thefuck.rules.apt_get._get_packages',
-                 create=True, return_value=packages)
-    
-      aws help
-  aws <command> help
-  aws <command> <subcommand> help
-aws: error: argument operation: Invalid choice, valid choices are:
-    
-    
-@pytest.mark.parametrize('command, new_command', [
-    (Command('cargo buid', no_such_subcommand_old), 'cargo build'),
-    (Command('cargo buils', no_such_subcommand), 'cargo build')])
-def test_get_new_command(command, new_command):
-    assert get_new_command(command) == new_command
-
-    
-        with gzip.open(paths[0], 'rb') as lbpath:
-        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-    
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    inputs = Input(shape=x.shape)
-    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
-    model = Model(inputs, outputs)
-    assert model.predict(x[np.newaxis])[0].shape == x.shape
-    
-        ```python
-    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
-    > labels
-    array([0, 2, 1, 2, 0])
-    # `to_categorical` converts this into a matrix with as many
-    # columns as there are classes. The number of rows
-    # stays the same.
-    > to_categorical(labels)
-    array([[ 1.,  0.,  0.],
-           [ 0.,  0.,  1.],
-           [ 0.,  1.,  0.],
-           [ 0.,  0.,  1.],
-           [ 1.,  0.,  0.]], dtype=float32)
-    ```
-    '''
-    
-            if 0 < self.dropout + self.recurrent_dropout:
-            if training is None:
-                h._uses_learning_phase = True
-    
-    # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-    
-    # Train the autoencoder
-autoencoder.fit(x_train_noisy,
-                x_train,
-                validation_data=(x_test_noisy, x_test),
-                epochs=30,
-                batch_size=batch_size)
-    
-    # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-    
-    history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_split=0.1)
-score = model.evaluate(x_test, y_test,
-                       batch_size=batch_size, verbose=1)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
-
-    
-            This method is the reverse of `get_config`,
-        capable of instantiating the same layer from the config
-        dictionary. It does not handle layer connectivity
-        (handled by Network), nor weights (handled by `set_weights`).
-    
-    UPDATED_MOD_SSL_CONF_DIGEST = '.updated-options-ssl-nginx-conf-digest.txt'
-'''Name of the hash of the updated or informed mod_ssl_conf as saved in `IConfig.config_dir`.'''
-    
-    
+        
 if __name__ == '__main__':
-    unittest.main()  # pragma: no cover
+    main()
 
     
-    # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+    import os
+import numpy as np
+from sklearn import metrics
+import tensorflow as tf
     
-            '''
-        super(AugeasConfigurator, self).rollback_checkpoints(rollback)
-        self.aug.load()
+    rates_train = nparray_and_transpose(rates_train)
+rates_valid = nparray_and_transpose(rates_valid)
+spikes_train = nparray_and_transpose(spikes_train)
+spikes_valid = nparray_and_transpose(spikes_valid)
+    
+      else:
+    return
 
     
-        def test_include_fullpath_trailing_slash(self):
-        self.verify_fnmatch(self.config_path + '//')
     
-            complex_vh = VirtualHost(
-            'fp', 'vhp',
-            set([Addr.fromstring('*:443'), Addr.fromstring('1.2.3.4:443')]),
-            False, False)
-        self.assertTrue(complex_vh.conflicts([self.addr1]))
-        self.assertTrue(complex_vh.conflicts([self.addr2]))
-        self.assertFalse(complex_vh.conflicts([self.addr_default]))
+def find_all_ngrams(dataset, n):
+  '''Generate a list of all ngrams.'''
+  return zip(*[dataset[i:] for i in xrange(n)])
     
-    # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+      if not FLAGS.seq2seq_share_embedding:
+    variable_mapping = {
+        str(model_str) + '/embedding':
+            encoder_embedding,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+            encoder_lstm_w_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+            encoder_lstm_b_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+            encoder_lstm_w_1,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+            encoder_lstm_b_1
+    }
+  else:
+    variable_mapping = {
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
+            encoder_lstm_w_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
+            encoder_lstm_b_0,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
+            encoder_lstm_w_1,
+        str(model_str) + '/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
+            encoder_lstm_b_1
+    }
+  return variable_mapping
     
-        # If no valid image file was uploaded, show the file upload form:
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
+        state_fwd = cell_fwd.zero_state(FLAGS.batch_size, tf.float32)
+    state_bwd = cell_bwd.zero_state(FLAGS.batch_size, tf.float32)
+    
+        return out
+    
+        @pytest.mark.parametrize(
+        'other, result', (
+            ({'AccePT': 'application/json'}, True),
+            ({}, False),
+            (None, False)
+        )
+    )
+    def test_instance_equality(self, other, result):
+        assert (self.case_insensitive_dict == other) is result
+    
+            Keyword:                   'bold #004461',   # class: 'k'
+        Keyword.Constant:          'bold #004461',   # class: 'kc'
+        Keyword.Declaration:       'bold #004461',   # class: 'kd'
+        Keyword.Namespace:         'bold #004461',   # class: 'kn'
+        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
+        Keyword.Reserved:          'bold #004461',   # class: 'kr'
+        Keyword.Type:              'bold #004461',   # class: 'kt'
+    
+        if cryptography_version < [1, 3, 4]:
+        warning = 'Old version of cryptography ({}) may cause slowdown.'.format(cryptography_version)
+        warnings.warn(warning, RequestsDependencyWarning)
+    
+        authstr = 'Basic ' + to_native_string(
+        b64encode(b':'.join((username, password))).strip()
+    )
     
     
-def batch_face_locations(images, number_of_times_to_upsample=1, batch_size=128):
-    '''
-    Returns an 2d array of bounding boxes of human faces in a image using the cnn face detector
-    If you are using a GPU, this can give you much faster results since the GPU
-    can process batches of images at once. If you aren't using a GPU, you don't need this function.
-    
-    # Initialize some variables
-face_locations = []
-face_encodings = []
-    
-        # Multi-core processing only supported on Python 3.4 or greater
-    if (sys.version_info < (3, 4)) and cpus != 1:
-        click.echo('WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!')
-        cpus = 1
-    
-            self.assertEqual(len(face_landmarks), 1)
-        self.assertEqual(face_landmarks[0].num_parts, 68)
-        self.assertEqual((example_landmark.x, example_landmark.y), (552, 399))
-    
-        # Print the location of each facial feature in this image
-    for facial_feature in face_landmarks.keys():
-        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
+def default_hooks():
+    return {event: [] for event in HOOKS}
     
     
-class Cat(object):
-    def speak(self):
-        return 'meow'
+class VersionedPackage(object):
+    def __init__(self, version):
+        self.__version__ = version
     
     
-print(graph1.find_path('A', 'D'))
-print(graph1.find_all_path('A', 'D'))
-print(graph1.find_shortest_path('A', 'D'))
-    
-        for shape in shapes:
-        shape.scale(2.5)
-        shape.draw()
-    
-        def test_shall_toggle_from_fm_to_am(self):
-        self.radio.toggle_amfm()
-        state = self.radio.state.name
-        expected_state_name = 'AM'
-        self.assertEqual(state, expected_state_name)
+def test_chunked_upload():
+    '''can safely send generators'''
+    close_server = threading.Event()
+    server = Server.basic_response_server(wait_to_close_event=close_server)
+    data = iter([b'a', b'b', b'c'])
