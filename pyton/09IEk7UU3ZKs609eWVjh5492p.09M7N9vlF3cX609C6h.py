bbddbf5b608261ee13f010078391830d13175312
SUNsung <weatherius@gmail.com>
2019-03-27 12:49:30 +0200

    Rewrite [09IEk7UU3ZKs609eWVjh5492p.09M7N9vlF3cX609C6h.py] File in: 2019-03-27 12:49:29

--- pyton/09IEk7UU3ZKs609eWVjh5492p.09M7N9vlF3cX609C6h.py
+++ pyton/09IEk7UU3ZKs609eWVjh5492p.09M7N9vlF3cX609C6h.py
@@ -1,101 +1,74 @@
 
         
-            parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
-    parser.add_argument('-d', action='store', dest='directory')
-    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
-    parser.add_argument('--overwrite', action='store_true', default = False)    
-    results = parser.parse_args()
-    
-            active_clients = [client for client in data.values() if
-                          client['status']]
-        self.last_results = active_clients
-        return True
-    
-    from homeassistant.components.notify import (
-    PLATFORM_SCHEMA, BaseNotificationService, ATTR_TITLE)
-from homeassistant.const import (CONF_TOKEN)
-import homeassistant.helpers.config_validation as cv
-    
-    import voluptuous as vol
-    
-        def __init__(self, username, access_token):
-        '''Initialize the service.'''
-        from freesms import FreeClient
-        self.free_client = FreeClient(username, access_token)
-    
-    test_encode_face = '''
-encoding = face_recognition.face_encodings(image, known_face_locations=face_locations)[0]
-'''
+            def test_kid_serialize(self):
+        from acme.jws import JWS
+        jws = JWS.sign(payload=b'foo', key=self.privkey,
+                       alg=jose.RS256, nonce=self.nonce,
+                       url=self.url, kid=self.kid)
+        self.assertEqual(jws.signature.combined.nonce, self.nonce)
+        self.assertEqual(jws.signature.combined.url, self.url)
+        self.assertEqual(jws.signature.combined.kid, self.kid)
+        self.assertEqual(jws.signature.combined.jwk, None)
+        # TODO: check that nonce is in protected header
     
     
-def face_distance(face_encodings, face_to_compare):
-    '''
-    Given a list of face encodings, compare them to a known face encoding and get a euclidean distance
-    for each comparison face. The distance tells you how similar the faces are.
+class AugeasConfiguratorTest(util.ApacheTest):
+    '''Test for Augeas Configurator base class.'''
     
-            face_encoding_a1 = api.face_encodings(img_a1)[0]
-        face_encoding_a2 = api.face_encodings(img_a2)[0]
-        face_encoding_a3 = api.face_encodings(img_a3)[0]
-        face_encoding_b1 = api.face_encodings(img_b1)[0]
+        def setUp(self):  # pylint: disable=arguments-differ
+        super(ComplexParserTest, self).setUp(
+            'complex_parsing', 'complex_parsing')
     
+        def test_nonexistent_like(self):
+        with mock.patch('certbot.util.get_os_info') as mock_info:
+            mock_info.return_value = ('nonexistent', 'irrelevant')
+            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
+                for like in entrypoint.OVERRIDE_CLASSES.keys():
+                    mock_like.return_value = [like]
+                    self.assertEqual(entrypoint.get_configurator(),
+                                     entrypoint.OVERRIDE_CLASSES[like])
     
-def detect_faces_in_image(file_stream):
-    # 用face_recognition.face_encodings(img)接口提前把奥巴马人脸的编码录入
-    known_face_encoding = [-0.09634063,  0.12095481, -0.00436332, -0.07643753,  0.0080383,
-                            0.01902981, -0.07184699, -0.09383309,  0.18518871, -0.09588896,
-                            0.23951106,  0.0986533 , -0.22114635, -0.1363683 ,  0.04405268,
-                            0.11574756, -0.19899382, -0.09597053, -0.11969153, -0.12277931,
-                            0.03416885, -0.00267565,  0.09203379,  0.04713435, -0.12731361,
-                           -0.35371891, -0.0503444 , -0.17841317, -0.00310897, -0.09844551,
-                           -0.06910533, -0.00503746, -0.18466514, -0.09851682,  0.02903969,
-                           -0.02174894,  0.02261871,  0.0032102 ,  0.20312519,  0.02999607,
-                           -0.11646006,  0.09432904,  0.02774341,  0.22102901,  0.26725179,
-                            0.06896867, -0.00490024, -0.09441824,  0.11115381, -0.22592428,
-                            0.06230862,  0.16559327,  0.06232892,  0.03458837,  0.09459756,
-                           -0.18777156,  0.00654241,  0.08582542, -0.13578284,  0.0150229 ,
-                            0.00670836, -0.08195844, -0.04346499,  0.03347827,  0.20310158,
-                            0.09987706, -0.12370517, -0.06683611,  0.12704916, -0.02160804,
-                            0.00984683,  0.00766284, -0.18980607, -0.19641446, -0.22800779,
-                            0.09010898,  0.39178532,  0.18818057, -0.20875394,  0.03097027,
-                           -0.21300618,  0.02532415,  0.07938635,  0.01000703, -0.07719778,
-                           -0.12651891, -0.04318593,  0.06219772,  0.09163868,  0.05039065,
-                           -0.04922386,  0.21839413, -0.02394437,  0.06173781,  0.0292527 ,
-                            0.06160797, -0.15553983, -0.02440624, -0.17509389, -0.0630486 ,
-                            0.01428208, -0.03637431,  0.03971229,  0.13983178, -0.23006812,
-                            0.04999552,  0.0108454 , -0.03970895,  0.02501768,  0.08157793,
-                           -0.03224047, -0.04502571,  0.0556995 , -0.24374914,  0.25514284,
-                            0.24795187,  0.04060191,  0.17597422,  0.07966681,  0.01920104,
-                           -0.01194376, -0.02300822, -0.17204897, -0.0596558 ,  0.05307484,
-                            0.07417042,  0.07126575,  0.00209804]
-    
-    
-class SASIndex(object):
-    row_size_index = 0
-    column_size_index = 1
-    subheader_counts_index = 2
-    column_text_index = 3
-    column_name_index = 4
-    column_attributes_index = 5
-    format_and_label_index = 6
-    column_list_index = 7
-    data_subheader_index = 8
-    
-    ax = fig.add_axes((0.63 + 0.18, 0.1, 0.16, 0.8))
-y = np.row_stack((fnx(), fnx(), fnx()))
-x = np.arange(10)
-y1, y2, y3 = fnx(), fnx(), fnx()
-ax.stackplot(x, y1, y2, y3)
-ax.set_xticks([])
-ax.set_yticks([])
-    
-    dump = pack
-dumps = packb
+        config_path = config_util.ensure_config_exists(config_dir)
+    print('Configuration file:', config_path)
+    return 0
 
     
-            exp = pd.DatetimeIndex(['2011-01-01 09:00', '2011-01-01 10:00',
-                                '2011-01-01 11:00'], tz=tz)
-        tm.assert_index_equal(
-            idx.fillna(pd.Timestamp('2011-01-01 10:00', tz=tz)), exp)
     
-        s = pd.Series([1, 2, -1, 4])
-    s[s < 0] = inc
+@asyncio.coroutine
+def test_if_fires_on_hass_shutdown(hass):
+    '''Test the firing when HASS starts.'''
+    calls = async_mock_service(hass, 'test', 'automation')
+    hass.state = CoreState.not_running
+    
+        # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(small_frame, model='cnn')
+    
+    # Find all the faces in the image using a pre-trained convolutional neural network.
+# This method is more accurate than the default HOG model, but it's slower
+# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
+# this will use GPU acceleration and perform well.
+# See also: find_faces_in_picture.py
+face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
+    
+        # Load the uploaded image file
+    img = face_recognition.load_image_file(file_stream)
+    # Get face encodings for any faces in the uploaded image
+    unknown_face_encodings = face_recognition.face_encodings(img)
+    
+    
+def _rect_to_css(rect):
+    '''
+    Convert a dlib 'rect' object to a plain tuple in (top, right, bottom, left) order
+    
+        # Print the location of each facial feature in this image
+    for facial_feature in face_landmarks.keys():
+        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
+    
+    from setuptools import setup
+    
+        # 讲识别结果以json键值对的数据结构输出
+    result = {
+        'face_found_in_image': face_found,
+        'is_picture_of_obama': is_obama
+    }
+    return jsonify(result)
