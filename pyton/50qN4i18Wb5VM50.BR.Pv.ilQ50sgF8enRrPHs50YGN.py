554f226a6deee691d6f3b610de79017c8a01b10b
SUNsung <weatherius@gmail.com>
2019-03-27 13:56:02 +0200

    Rewrite [50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py] File in: 2019-03-27 13:56:01

--- pyton/50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py
+++ pyton/50qN4i18Wb5VM50.BR.Pv.ilQ50sgF8enRrPHs50YGN.py
@@ -1,192 +1,147 @@
 
         
-        
-@click.command('init-db')
-@with_appcontext
-def init_db_command():
-    '''Clear existing data and create new tables.'''
-    init_db()
-    click.echo('Initialized the database.')
+        # Fail if the server certificate name was not found
+- iam_server_certificate_facts:
+    name: production-cert
+  register: server_cert
+  failed_when: '{{ server_cert.results | length == 0 }}'
+'''
     
-        # login request set the user_id in the session
-    # check that the user is loaded from the session
-    with client:
-        client.get('/')
-        assert session['user_id'] == 1
-        assert g.user['username'] == 'test'
-    
-            if triple is None:
-            detail = 'no match'
+        json_output = {}
+    if mod_params['view'] == 'topics':
+        json_output['topics'] = list_func(pubsub_client.list_topics())
+    elif mod_params['view'] == 'subscriptions':
+        if mod_params['topic']:
+            t = pubsub_client.topic(mod_params['topic'])
+            json_output['subscriptions'] = list_func(t.list_subscriptions())
         else:
-            detail = 'found (%r)' % (triple[1] or '<string>')
-            total_found += 1
-        info.append('       -> %s' % detail)
+            json_output['subscriptions'] = list_func(pubsub_client.list_subscriptions())
+    
+        elif state == 'present':
+        for param in ('name', 'rules'):
+            if not module.params.get(param):
+                module.fail_json(
+                    msg='%s parameter is required for new firewall policies.' % param)
+        try:
+            (changed, firewall_policy) = create_firewall_policy(module, oneandone_conn)
+        except Exception as e:
+            module.fail_json(msg=str(e))
+    
+            # apply properties
+        if desired_state == 'present':
+            if current_state == HOST_ABSENT:
+                self.allocate_host()
+                host = self.get_host_by_name(host_name)
+                self.wait_for_host_state(host, [HOST_STATES.MONITORED])
+            elif current_state in [HOST_STATES.ERROR, HOST_STATES.MONITORING_ERROR]:
+                self.fail(msg='invalid host state %s' % current_state_name)
+    
+        if module.params['user']:
+        params['deploy[local_username]'] = module.params['user']
+    
+        module.exit_json(changed=False, msg='logs(s) already followed')
+    
+        if event == 'annotation':
+        if not msg:
+            module.fail_json(msg='msg required for annotation events')
+        try:
+            send_annotation_event(module, key, msg, annotated_by, level, instance_id, event_epoch)
+        except Exception as e:
+            module.fail_json(msg='unable to sent annotation event: %s' % to_native(e),
+                             exception=traceback.format_exc())
     
     
-def get_git_tags():
-    return set(
-        Popen(['git', 'tag'], stdout=PIPE).communicate()[0].splitlines()
-    )
-    
-        @app.teardown_request
-    def teardown_req(error=None):
-        called.append('request')
-    
-    
-@pytest.mark.functional
-def test_with_confirmation(proc, TIMEOUT):
-    with_confirmation(proc, TIMEOUT)
-    
-    
-@pytest.mark.parametrize('command', [
-    Command('cargo buid', no_such_subcommand_old),
-    Command('cargo buils', no_such_subcommand)])
-def test_match(command):
-    assert match(command)
-    
-        @staticmethod
-    def make_header(username, password):
-        credentials = u'%s:%s' % (username, password)
-        token = b64encode(credentials.encode('utf8')).strip().decode('latin1')
-        return 'Basic %s' % token
-    
-        def unregister(self, plugin):
-        self._plugins.remove(plugin)
-    
-    USERNAME = 'user'
-PASSWORD = 'password'
-# Basic auth encoded `USERNAME` and `PASSWORD`
-# noinspection SpellCheckingInspection
-BASIC_AUTH_HEADER_VALUE = 'Basic dXNlcjpwYXNzd29yZA=='
-BASIC_AUTH_URL = '/basic-auth/{0}/{1}'.format(USERNAME, PASSWORD)
-AUTH_OK = {'authenticated': True, 'user': USERNAME}
-    
-    
-def rst_filenames():
-    for root, dirnames, filenames in os.walk(os.path.dirname(TESTS_ROOT)):
-        if '.tox' not in root:
-            for filename in fnmatch.filter(filenames, '*.rst'):
-                yield os.path.join(root, filename)
-    
-    
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
-    
-    
-def test_unicode_basic_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--verbose', '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/basic-auth/test/' + UNICODE)
-    
-        def failed(self):
-        self._progress_reporter.stop()
-    
-        for i in range(1, 6):
-        fpath = os.path.join(path, 'data_batch_' + str(i))
-        (x_train[(i - 1) * 10000: i * 10000, :, :, :],
-         y_train[(i - 1) * 10000: i * 10000]) = load_batch(fpath)
-    
-        if K.image_data_format() == 'channels_last':
-        x_train = x_train.transpose(0, 2, 3, 1)
-        x_test = x_test.transpose(0, 2, 3, 1)
-    
-    
-def get(identifier):
-    if identifier is None:
+def linkcode_resolve(domain, info):
+    '''Determine the URL corresponding to Python object.'''
+    if domain != 'py':
         return None
-    if isinstance(identifier, dict):
-        return deserialize(identifier)
-    elif isinstance(identifier, six.string_types):
-        config = {'class_name': str(identifier), 'config': {}}
-        return deserialize(config)
-    elif callable(identifier):
-        return identifier
+    modname = info['module']
+    fullname = info['fullname']
+    submod = sys.modules.get(modname)
+    if submod is None:
+        return None
+    obj = submod
+    for part in fullname.split('.'):
+        try:
+            obj = getattr(obj, part)
+        except:
+            return None
+    try:
+        fn = inspect.getsourcefile(obj)
+    except:
+        fn = None
+    if not fn:
+        return None
+    try:
+        source, lineno = inspect.findsource(obj)
+    except:
+        lineno = None
+    if lineno:
+        linespec = '#L%d' % (lineno + 1)
     else:
-        raise ValueError('Could not interpret regularizer identifier: ' +
-                         str(identifier))
+        linespec = ''
+    index = fn.find('/homeassistant/')
+    if index == -1:
+        index = 0
+    
+    
+# Concrete Buildings
+class House(Building):
+    def build_floor(self):
+        self.floor = 'One'
+    
+        sample_queue.put('yam')
+    with ObjectPool(sample_queue) as obj:
+        print('Inside with: {}'.format(obj))
+    print('Outside with: {}'.format(sample_queue.get()))
+    
+    This is useful as it make it easier to derive new kinds of objects,
+when instances of the class have only a few different combinations of
+state, and when instantiation is expensive.
+    
+            if start == end:
+            return path
+        shortest = None
+        for node in self.graph.get(start, []):
+            if node not in path:
+                newpath = self.find_shortest_path(node, end, path[:])
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
+    
+    
+class Failed(OutOfService):
+    '''No need to override any method.'''
+    
+        def test_publisher_shall_append_subscription_message_to_queue(cls):
+        ''' msg_queue ~ Provider.notify(msg) ~ Publisher.publish(msg) '''
+        expected_msg = 'expected msg'
+        pro = Provider()
+        pub = Publisher(pro)
+        Subscriber('sub name', pro)
+        cls.assertEqual(len(pro.msg_queue), 0)
+        pub.publish(expected_msg)
+        cls.assertEqual(len(pro.msg_queue), 1)
+        cls.assertEqual(pro.msg_queue[0], expected_msg)
+    
+        def test_shall_toggle_from_fm_to_am(self):
+        self.radio.toggle_amfm()
+        state = self.radio.state.name
+        expected_state_name = 'AM'
+        self.assertEqual(state, expected_state_name)
 
     
-        ```python
-    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
-    > labels
-    array([0, 2, 1, 2, 0])
-    # `to_categorical` converts this into a matrix with as many
-    # columns as there are classes. The number of rows
-    # stays the same.
-    > to_categorical(labels)
-    array([[ 1.,  0.,  0.],
-           [ 0.,  0.,  1.],
-           [ 0.,  1.,  0.],
-           [ 0.,  0.,  1.],
-           [ 1.,  0.,  0.]], dtype=float32)
-    ```
-    '''
+            Parameters
+        ----------
+        declarations_str : str
+            A list of CSS declarations
+        inherited : dict, optional
+            Atomic properties indicating the inherited style context in which
+            declarations_str is to be resolved. ``inherited`` should already
+            be resolved, i.e. valid output of this method.
     
-            if 0 < self.dropout < 1.:
-            inputs_i = inputs * dp_mask[0]
-            inputs_f = inputs * dp_mask[1]
-            inputs_c = inputs * dp_mask[2]
-            inputs_o = inputs * dp_mask[3]
-        else:
-            inputs_i = inputs
-            inputs_f = inputs
-            inputs_c = inputs
-            inputs_o = inputs
-    
-    Denoising is one of the classic applications of autoencoders.
-The denoising process removes unwanted noise that corrupted the
-true signal.
-    
-    print('Convert class vector to binary class matrix '
-      '(for use with categorical_crossentropy)')
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-print('y_train shape:', y_train.shape)
-print('y_test shape:', y_test.shape)
-    
-                    log.println('    platform: {}'.format(platform.platform()))
-                log.println('    python:   {}'.format(sys.version.split('\n')[0]))
-    
-    def get_orig_video_source(api_key, pid, secret):
-    parsed = json.loads(get_content_headered(tmpl_api_call_video_info % (api_key, pid, secret)))
-    for stream in parsed['streams']['stream']:
-        if stream['type'] == 'orig':
-            return stream['_content'].replace('\\', '')
-    return None
-    
-            for p in js_path:
-            if 'mtool' in p or 'mcore' in p:
-                js_text = get_content(p)
-                hit = re.search(r'\(\'(.+?)\',(\d+),(\d+),\'(.+?)\'\.split\(\'\|\'\),\d+,\{\}\)', js_text)
-    
-    def ifeng_download_by_id(id, title = None, output_dir = '.', merge = True, info_only = False):
-    assert r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', id), id
-    url = 'http://vxml.ifengimg.com/video_info_new/%s/%s/%s.xml' % (id[-2], id[-2:], id)
-    xml = get_html(url, 'utf-8')
-    title = r1(r'Name='([^']+)'', xml)
-    title = unescape_html(title)
-    url = r1(r'VideoPlayUrl='([^']+)'', xml)
-    from random import randint
-    r = randint(10, 19)
-    url = url.replace('http://wideo.ifeng.com/', 'http://ips.ifeng.com/wideo.ifeng.com/')
-    type, ext, size = url_info(url)
-    
-    class Imgur(VideoExtractor):
-    name = 'Imgur'
-    
-        stream_types = [
-        {'id': 'video'},
-        {'id': 'audio'},
-        {'id': 'slides'}
-    ]
-    
-        Each code snipped should get additional C++ code around it to help compile the line in context, with
-    some heuristic guessing of what is needed around. The wrapping code should have a token in each line allowing
-    other tools to filter out these lines
-    
-            #Sorting all filenames in the directory, to maintain the order of the PDF
-        allmd.sort()
+            exp = pd.DatetimeIndex(['2011-01-01 09:00', '2011-01-01 10:00',
+                                '2011-01-01 11:00'])
+        tm.assert_index_equal(
+            idx.fillna(pd.Timestamp('2011-01-01 10:00')), exp)
