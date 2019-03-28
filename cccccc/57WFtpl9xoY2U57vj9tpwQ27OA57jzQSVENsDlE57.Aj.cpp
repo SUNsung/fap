7eeb56651f440a4a6c2abcf40ded0c4b0736a7c4
SUNsung <weatherius@gmail.com>
2019-03-28 12:45:16 +0200

    Rewrite [57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp] File in: 2019-03-28 12:45:15

--- cccccc/57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp
+++ cccccc/57WFtpl9xoY2U57vj9tpwQ27OA57jzQSVENsDlE57.Aj.cpp
@@ -1,91 +1,289 @@
 
         
-         private:
-  scoped_refptr<AtomBrowserContext> browser_context_;
+        #include 'content/nw/src/api/api_messages.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'content/nw/src/api/shortcut/global_shortcut_listener.h'
+#include 'content/nw/src/api/shortcut/shortcut.h'
+#include 'content/nw/src/breakpad_linux.h'
+#include 'content/nw/src/browser/native_window.h'
+#include 'content/nw/src/browser/net_disk_cache_remover.h'
+#include 'content/nw/src/nw_package.h'
+#include 'content/nw/src/nw_shell.h'
+#include 'content/nw/src/shell_browser_context.h'
+#include 'content/common/view_messages.h'
+#include 'content/public/browser/browser_thread.h'
+#include 'content/public/browser/web_contents.h'
+#include 'content/public/browser/render_process_host.h'
+#include 'net/proxy/proxy_config.h'
+#include 'net/proxy/proxy_config_service_fixed.h'
+#include 'net/proxy/proxy_service.h'
+#include 'net/url_request/url_request_context.h'
+#include 'net/url_request/url_request_context_getter.h'
     
-    void Event::SetSenderAndMessage(content::RenderFrameHost* sender,
-                                IPC::Message* message) {
-  DCHECK(!sender_);
-  DCHECK(!message_);
-  sender_ = sender;
-  message_ = message;
-    }
+      // Post 'open' event.
+  static void EmitOpenEvent(const std::string& path);
     
-     private:
-  // Replyer for the synchronous messages.
-  content::RenderFrameHost* sender_ = nullptr;
-  IPC::Message* message_ = nullptr;
+    // Call method of an object in browser.
+// function CallObjectMethod(id, type, method, args);
+v8::Handle<v8::Value> CallObjectMethod(int routing_id,
+                                       int object_id,
+                                       const std::string& type,
+                                       const std::string& method,
+                                       v8::Handle<v8::Value> args);
     
-     private:
-  void OnMessageBoxCallback(DialogClosedCallback callback,
-                            const std::string& origin,
-                            int code,
-                            bool checkbox_checked);
     
-    namespace auto_updater {
-    }
+    {  DISALLOW_COPY_AND_ASSIGN(Clipboard);
+};
     
-    #include 'base/macros.h'
     
-    net::URLRequestJob* AboutProtocolHandler::MaybeCreateJob(
-    net::URLRequest* request,
-    net::NetworkDelegate* network_delegate) const {
-  return new URLRequestAboutJob(request, network_delegate);
+    {}
+    
+    Menu::Menu(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+           const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
+  Create(option);
 }
     
-    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
-                                       net::NetworkDelegate* network_delegate)
-    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
-    
-    // The argument separating arguments intended for the relauncher process from
-// those intended for the relaunched process. '---' is chosen instead of '--'
-// because CommandLine interprets '--' as meaning 'end of switches', but
-// for many purposes, the relauncher process' CommandLine ought to interpret
-// arguments intended for the relaunched process, to get the correct settings
-// for such things as logging and the user-data-dir in case it affects crash
-// reporting.
-extern const CharType* kRelauncherArgSeparator;
-    
-    Github Links:
-    
-    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC1DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
-    
-    
-    {    if (!status.ok()) {
-      FillErrorResponse(status, response.mutable_error_response());
-    }
-    response.set_valid_host(request.host());
-    response.set_allocated_original_request(
-        new ServerReflectionRequest(request));
-    stream->Write(response);
-  }
-    
-      // Add the full names of registered services
-  void SetServiceList(const std::vector<grpc::string>* services);
-    
-    namespace grpc {
-namespace load_reporter {
-    }
+    void Menu::Create(const base::DictionaryValue& option) {
+  is_menu_modified_ = true;
+  menu_delegate_.reset(new MenuDelegate(object_manager()));
+  menu_model_.reset(new ui::NwMenuModel(menu_delegate_.get()));
     }
     
+    class NwAppCrashBrowserFunction : public UIThreadExtensionFunction {
+ public:
+  NwAppCrashBrowserFunction() {}
+    }
     
-    {    auto opndCmds = opndCmd.GetCommands();
-    unsigned int opndCmdSize;
-    opndCmds->GetSize(&opndCmdSize);
-    m_dataWriter->WriteUInt32(opndCmdSize);
-    for (unsigned int j = 0; j < opndCmdSize; ++j)
+    NwObjCallObjectMethodSyncFunction::~NwObjCallObjectMethodSyncFunction() {
+}
+    
+    
+    {    struct KeypointStore {
+        virtual void push(f32 kpX, f32 kpY, f32 kpSize, f32 kpAngle=-1, f32 kpResponse=0, s32 kpOctave=0, s32 kpClass_id=-1) = 0;
+        virtual ~KeypointStore() {};
+    };
+}
+    
+        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0,
+                     const typename internal::VecTraits<T>::vec64 & v_src1,
+                     typename internal::VecTraits<T>::vec64 & v_dst) const
     {
-        int eachOpndcmd;
-        opndCmds->GetAt(j, &eachOpndcmd);
-        m_dataWriter->WriteInt32(eachOpndcmd);
+        typename internal::VecTraits<T>::vec64 v_min = internal::vmin(v_src0, v_src1);
+        typename internal::VecTraits<T>::vec64 v_max = internal::vmax(v_src0, v_src1);
+        v_dst = internal::vqsub(v_max, v_min);
     }
+    
+        void operator() (const typename VecTraits<T>::vec128 & v_src0,
+                     const typename VecTraits<T>::vec128 & v_src1,
+                     typename VecTraits<T>::vec128 & v_dst) const
+    {
+        typename VecTraits<wtype>::vec128 vrl, vrh;
+        wideAdd(vmovl( vget_low(v_src0)), vmovl( vget_low(v_src1)), vrl);
+        wideAdd(vmovl(vget_high(v_src0)), vmovl(vget_high(v_src1)), vrh);
+    }
+    
+        for (ptrdiff_t y = 0; y < height; ++y)
+    {
+        const u8 * srow0 = y == 0 && border == BORDER_MODE_CONSTANT ? NULL : internal::getRowPtr(srcBase, srcStride, std::max<ptrdiff_t>(y - 1, 0));
+        const u8 * srow1 = internal::getRowPtr(srcBase, srcStride, y);
+        const u8 * srow2 = y + 1 == height && border == BORDER_MODE_CONSTANT ? NULL : internal::getRowPtr(srcBase, srcStride, std::min(y + 1, height - 1));
+        u8 * drow = internal::getRowPtr(dstBase, dstStride, y);
+    }
+    
+    template <typename T>
+inline T *getRowPtr(T *base, ptrdiff_t stride, size_t row)
+{
+    char *baseRaw = const_cast<char *>(reinterpret_cast<const char *>(base));
+    return reinterpret_cast<T *>(baseRaw + ptrdiff_t(row) * stride);
 }
     
-            property bool IsErrorVisualState
-        {
-            bool get();
-            void set(bool value);
-        }
+        minLocCapacity <<= 1;
+    maxLocCapacity <<= 1;
+    
+                // make shift
+            if (x)
+            {
+                tprev = tcurr;
+                tcurr = tnext;
+            }
+    
+                t0 = vsubq_s16(vqaddq_s16(t0, t2), tc);
+            tc = tcnext;
+    
+    
+    {} // namespace
+    
+    <summary> <b>Example</b> </summary>
+    
+    
+    {}
+
+    
+    		static const unsigned int ROWS = 4;
+		static const unsigned int COLUMNS = 4;
+		static const unsigned int PIXELS = ROWS * COLUMNS;
+		static const float LUMA_WEIGHT;
+		static const float CHROMA_BLUE_WEIGHT;
+    
+    	// ----------------------------------------------------------------------------------------------------
+	// algorithm:
+	// create a source average color for the Left, Right, Top and Bottom halves using the 8 pixels in each half
+	// note: the 'gray line' is the line of equal delta RGB that goes thru the average color
+	// for each half:
+	//		see how close each of the 8 pixels are to the 'gray line' that goes thru the source average color
+	//		create an error value that is the sum of the distances from the gray line
+	// h_error is the sum of Left and Right errors
+	// v_error is the sum of Top and Bottom errors
+	//
+	void Block4x4Encoding_ETC1::CalculateMostLikelyFlip(void)
+	{
+		static const bool DEBUG_PRINT = false;
+    }
+    
+    static OPUS_INLINE short MULT16_16_P13(int a, int b)
+{
+   opus_int64 res;
+   if (!VERIFY_SHORT(a) || !VERIFY_SHORT(b))
+   {
+      fprintf (stderr, 'MULT16_16_P13: inputs are not short: %d %d\n', a, b);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = ((opus_int64)a)*b;
+   res += 4096;
+   if (!VERIFY_INT(res))
+   {
+      fprintf (stderr, 'MULT16_16_P13: overflow: %d*%d=%d\n', a, b, (int)res);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res >>= 13;
+   if (!VERIFY_SHORT(res))
+   {
+      fprintf (stderr, 'MULT16_16_P13: output is not short: %d*%d=%d\n', a, b, (int)res);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips+=4;
+   return res;
+}
+static OPUS_INLINE short MULT16_16_P14(int a, int b)
+{
+   opus_int64 res;
+   if (!VERIFY_SHORT(a) || !VERIFY_SHORT(b))
+   {
+      fprintf (stderr, 'MULT16_16_P14: inputs are not short: %d %d\n', a, b);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = ((opus_int64)a)*b;
+   res += 8192;
+   if (!VERIFY_INT(res))
+   {
+      fprintf (stderr, 'MULT16_16_P14: overflow: %d*%d=%d\n', a, b, (int)res);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res >>= 14;
+   if (!VERIFY_SHORT(res))
+   {
+      fprintf (stderr, 'MULT16_16_P14: output is not short: %d*%d=%d\n', a, b, (int)res);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips+=4;
+   return res;
+}
+static OPUS_INLINE short MULT16_16_P15(int a, int b)
+{
+   opus_int64 res;
+   if (!VERIFY_SHORT(a) || !VERIFY_SHORT(b))
+   {
+      fprintf (stderr, 'MULT16_16_P15: inputs are not short: %d %d\n', a, b);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = ((opus_int64)a)*b;
+   res += 16384;
+   if (!VERIFY_INT(res))
+   {
+      fprintf (stderr, 'MULT16_16_P15: overflow: %d*%d=%d\n', a, b, (int)res);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res >>= 15;
+   if (!VERIFY_SHORT(res))
+   {
+      fprintf (stderr, 'MULT16_16_P15: output is not short: %d*%d=%d\n', a, b, (int)res);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips+=2;
+   return res;
+}
+    
+    /*!
+* Downsample 2x, mediocre quality
+*/
+void silk_resampler_down2(
+    opus_int32                  *S,                 /* I/O  State vector [ 2 ]                                          */
+    opus_int16                  *out,               /* O    Output signal [ len ]                                       */
+    const opus_int16            *in,                /* I    Input signal [ floor(len/2) ]                               */
+    opus_int32                  inLen               /* I    Number of input samples                                     */
+);
+    
+    
+    {    return head;
+}
+    
+    
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <stdexcept>
+    
+    int main() {
+    }
+    
+    using namespace std;
+    
+                if(level == res.size())
+                res.push_back(vector<int>());
+            assert( level < res.size() );
+    
+    public:
+    vector<int> postorderTraversal(TreeNode* root) {
+    }
+    
+    using namespace std;
+    
+    #include <iostream>
+#include <vector>
+#include <stack>
+    
+    #include <string>
+#include <openpose/core/macros.hpp>
+    
+        const auto FACE_NUMBER_PARTS = 70u;
+    #define FACE_PAIRS_RENDER_GPU \
+        0,1,  1,2,  2,3,  3,4,  4,5,  5,6,  6,7,  7,8,  8,9,  9,10,  10,11,  11,12,  12,13,  13,14,  14,15,  15,16,  17,18,  18,19,  19,20, \
+        20,21,  22,23,  23,24,  24,25,  25,26,  27,28,  28,29,  29,30,  31,32,  32,33,  33,34,  34,35,  36,37,  37,38,  38,39,  39,40,  40,41, \
+        41,36,  42,43,  43,44,  44,45,  45,46,  46,47,  47,42,  48,49,  49,50,  50,51,  51,52,  52,53,  53,54,  54,55,  55,56,  56,57,  57,58, \
+        58,59,  59,48,  60,61,  61,62,  62,63,  63,64,  64,65,  65,66,  66,67,  67,60
+    #define FACE_SCALES_RENDER_GPU 1
+    const std::vector<unsigned int> FACE_PAIRS_RENDER {FACE_PAIRS_RENDER_GPU};
+    #define FACE_COLORS_RENDER_GPU 255.f,    255.f,    255.f
+    const std::vector<float> FACE_COLORS_RENDER{FACE_COLORS_RENDER_GPU};
+    const std::vector<float> FACE_SCALES_RENDER{FACE_SCALES_RENDER_GPU};
