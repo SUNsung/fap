cdc0dde6e5bd7e6f2d38895405978fe2a2b64bd0
SUNsung <weatherius@gmail.com>
2019-03-28 22:41:36 +0200

    Rewrite [38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb] File in: 2019-03-28 22:41:35

--- ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
+++ ruby/38XwGFlnu91fk380KMU7J5z7aw38bPu2R47tICg38C5s.rb
@@ -1,174 +1,112 @@
 
         
-            group.add(moderator)
-    group.save
+        #############################################################################
+#
+# Standard tasks
+#
+#############################################################################
     
-      #
-  # HTTP 200/OK response class wrapper.
-  #
-  class OK < Response
-    def initialize(message = 'OK', proto = DefaultProtocol)
-      super(200, message, proto)
-    end
-  end
-    
-    # Default timings
-IAX_DEFAULT_REG_REFRESH = 60
-IAX_DEFAULT_TIMEOUT     = 10
-    
-                encoded
-          end
-    
-                decrypted
-          end
-    
-              # Encodes the Rex::Proto::Kerberos::Model::ApReq into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            elems = []
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_pvno], 0, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_msg_type], 1, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_options], 2, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_ticket], 3, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_authenticator], 4, :CONTEXT_SPECIFIC)
-            seq = OpenSSL::ASN1::Sequence.new(elems)
-    
-              # Encodes the value field
-          #
-          # @return [OpenSSL::ASN1::OctetString]
-          def encode_value
-            OpenSSL::ASN1::OctetString.new(value)
-          end
-        end
-      end
-    end
-  end
+    def graceful_require
+  Jekyll::External.require_with_graceful_fail('json')
+  JSON.pretty_generate(DATA)
 end
     
-              # Encodes the pa_data field
-          #
-          # @return [String]
-          def encode_pa_data
-            elems = []
-            pa_data.each do |data|
-              elems << data.encode
-            end
+            # For a description of the protocol see
+        # http://feedback.livereload.com/knowledgebase/articles/86174-livereload-protocol
+        def reload(pages)
+          pages.each do |p|
+            json_message = JSON.dump(
+              :command => 'reload',
+              :path    => p.url,
+              :liveCSS => true
+            )
     
-              # Encodes the Rex::Proto::Kerberos::Model::KdcRequestBody into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            elems = []
-    
-    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
-#   licensed under the Affero General Public License version 3 or later.  See
-#   the COPYRIGHT file.
-    
-        it 'returns likes for a public post without login' do
-      post = alice.post(:status_message, text: 'hey', public: true)
-      bob.like!(post)
-      sign_out :user
-      get :index, params: {post_id: post.id}, format: :json
-      expect(JSON.parse(response.body).map {|h| h['id'] }).to match_array(post.likes.map(&:id))
+        def email_changed(record, opts={})
+      devise_mail(record, :email_changed, opts)
     end
     
-    describe ResharesController, :type => :controller do
-  describe '#create' do
-    let(:post_request!) {
-      post :create, params: {root_guid: @post_guid}, format: :json
-    }
-    
-    @@ index
-  <h1>Sinatra + Sidekiq Example</h1>
-  <h2>Failed: <%= @failed %></h2>
-  <h2>Processed: <%= @processed %></h2>
-    
-          normed = normalize_item(items)
-      payloads = items['args'].map do |args|
-        copy = normed.merge('args' => args, 'jid' => SecureRandom.hex(12), 'enqueued_at' => Time.now.to_f)
-        result = process_single(items['class'], copy)
-        result ? result : nil
-      end.compact
-    
-      module Worker
-    ##
-    # The Sidekiq testing infrastructure overrides perform_async
-    # so that it does not actually touch the network.  Instead it
-    # stores the asynchronous jobs in a per-class array so that
-    # their presence/absence can be asserted by your tests.
-    #
-    # This is similar to ActionMailer's :test delivery_method and its
-    # ActionMailer::Base.deliveries array.
-    #
-    # Example:
-    #
-    #   require 'sidekiq/testing'
-    #
-    #   assert_equal 0, HardWorker.jobs.size
-    #   HardWorker.perform_async(:something)
-    #   assert_equal 1, HardWorker.jobs.size
-    #   assert_equal :something, HardWorker.jobs[0]['args'][0]
-    #
-    #   assert_equal 0, Sidekiq::Extensions::DelayedMailer.jobs.size
-    #   MyMailer.delay.send_welcome_email('foo@example.com')
-    #   assert_equal 1, Sidekiq::Extensions::DelayedMailer.jobs.size
-    #
-    # You can also clear and drain all workers' jobs:
-    #
-    #   assert_equal 0, Sidekiq::Extensions::DelayedMailer.jobs.size
-    #   assert_equal 0, Sidekiq::Extensions::DelayedModel.jobs.size
-    #
-    #   MyMailer.delay.send_welcome_email('foo@example.com')
-    #   MyModel.delay.do_something_hard
-    #
-    #   assert_equal 1, Sidekiq::Extensions::DelayedMailer.jobs.size
-    #   assert_equal 1, Sidekiq::Extensions::DelayedModel.jobs.size
-    #
-    #   Sidekiq::Worker.clear_all # or .drain_all
-    #
-    #   assert_equal 0, Sidekiq::Extensions::DelayedMailer.jobs.size
-    #   assert_equal 0, Sidekiq::Extensions::DelayedModel.jobs.size
-    #
-    # This can be useful to make sure jobs don't linger between tests:
-    #
-    #   RSpec.configure do |config|
-    #     config.before(:each) do
-    #       Sidekiq::Worker.clear_all
-    #     end
-    #   end
-    #
-    # or for acceptance testing, i.e. with cucumber:
-    #
-    #   AfterStep do
-    #     Sidekiq::Worker.drain_all
-    #   end
-    #
-    #   When I sign up as 'foo@example.com'
-    #   Then I should receive a welcome email to 'foo@example.com'
-    #
-    module ClassMethods
-    
-          arr = Sidekiq.options[:lifecycle_events][event]
-      arr.reverse! if reverse
-      arr.each do |block|
-        begin
-          block.call
-        rescue => ex
-          handle_exception(ex, { context: 'Exception during Sidekiq lifecycle event.', event: event })
-          raise ex if reraise
-        end
+          # Set up a subject doing an I18n lookup. At first, it attempts to set a subject
+      # based on the current mapping:
+      #
+      #   en:
+      #     devise:
+      #       mailer:
+      #         confirmation_instructions:
+      #           user_subject: '...'
+      #
+      # If one does not exist, it fallbacks to ActionMailer default:
+      #
+      #   en:
+      #     devise:
+      #       mailer:
+      #         confirmation_instructions:
+      #           subject: '...'
+      #
+      def subject_for(key)
+        I18n.t(:'#{devise_mapping.name}_subject', scope: [:devise, :mailer, key],
+          default: [:subject, key.to_s.humanize])
       end
-      arr.clear
     end
   end
 end
 
     
-        def erb(content, options = {})
-      if content.kind_of? Symbol
-        unless respond_to?(:'_erb_#{content}')
-          src = ERB.new(File.read('#{Web.settings.views}/#{content}.erb')).src
-          WebAction.class_eval('def _erb_#{content}\n#{src}\n end')
+    require 'devise/models/authenticatable'
+
+    
+            within('.table-active-filters') do
+          expect(page).to have_content('Start: 2018/01/01')
+          expect(page).to have_content('Stop: 2018/06/30')
+          expect(page).to have_content('Order: R100')
+          expect(page).to have_content('Status: cart')
+          expect(page).to have_content('Payment State: paid')
+          expect(page).to have_content('Shipment State: pending')
+          expect(page).to have_content('First Name Begins With: John')
+          expect(page).to have_content('Last Name Begins With: Smith')
+          expect(page).to have_content('Email: john_smith@example.com')
+          expect(page).to have_content('SKU: BAG-00001')
+          expect(page).to have_content('Promotion: Promo')
+          expect(page).to have_content('Store: Spree Test Store')
+          expect(page).to have_content('Channel: spree')
         end
       end
+    end
+  end
+end
+
+    
+                  expect(order.shipments.count).to eq(1)
+              expect(order.shipments.first.inventory_units_for(product.master).sum(&:quantity)).to eq(2)
+              expect(order.shipments.first.stock_location.id).to eq(stock_location.id)
+            end
+          end
+    
+        def flash_messages(opts = {})
+      ignore_types = ['order_completed'].concat(Array(opts[:ignore_types]).map(&:to_s) || [])
+    
+      def for_each_gem
+    SPREE_GEMS.each do |gem_name|
+      yield 'pkg/spree_#{gem_name}-#{version}.gem'
+    end
+    yield 'pkg/spree-#{version}.gem'
+  end
+    
+          rescue_from ActionController::ParameterMissing, with: :error_during_processing
+      rescue_from ActiveRecord::RecordInvalid, with: :error_during_processing
+      rescue_from ActiveRecord::RecordNotFound, with: :not_found
+      rescue_from CanCan::AccessDenied, with: :unauthorized
+      rescue_from Spree::Core::GatewayError, with: :gateway_error
+    
+              state = @states.last
+          respond_with(@states) if stale?(state)
+        end
+    
+    When /^(?:|I )choose '([^']*)'$/ do |field|
+  choose(field)
+end
+    
+        def raise_because_imagemagick_missing
+      raise Errors::CommandNotFoundError.new('Could not run the `identify` command. Please install ImageMagick.')
+    end
+  end
+end
