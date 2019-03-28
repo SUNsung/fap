81e236b7952131457ef703b3c22bd48991f79b37
SUNsung <weatherius@gmail.com>
2019-03-28 14:17:03 +0200

    Rewrite [09IEk7UU3ZKs609eWVjh5492p.09M7N9vlF3cX609C6h.py] File in: 2019-03-28 14:17:03

--- pyton/09IEk7UU3ZKs609eWVjh5492p.09M7N9vlF3cX609C6h.py
+++ pyton/09IEk7UU3ZKs609eWVjh5492p.09M7N9vlF3cX609C6h.py
@@ -1,74 +1,84 @@
 
         
-            def test_kid_serialize(self):
-        from acme.jws import JWS
-        jws = JWS.sign(payload=b'foo', key=self.privkey,
-                       alg=jose.RS256, nonce=self.nonce,
-                       url=self.url, kid=self.kid)
-        self.assertEqual(jws.signature.combined.nonce, self.nonce)
-        self.assertEqual(jws.signature.combined.url, self.url)
-        self.assertEqual(jws.signature.combined.kid, self.kid)
-        self.assertEqual(jws.signature.combined.jwk, None)
-        # TODO: check that nonce is in protected header
+            if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+    
+        def before_request(self, f):
+        '''Like :meth:`Flask.before_request` but for a blueprint.  This function
+        is only executed before each request that is handled by a function of
+        that blueprint.
+        '''
+        self.record_once(lambda s: s.app.before_request_funcs
+            .setdefault(self.name, []).append(f))
+        return f
+    
+    This typically means that you attempted to use functionality that needed
+to interface with the current application object in some way. To solve
+this, set up an application context with app.app_context().  See the
+documentation for more information.\
+'''
     
     
-class AugeasConfiguratorTest(util.ApacheTest):
-    '''Test for Augeas Configurator base class.'''
-    
-        def setUp(self):  # pylint: disable=arguments-differ
-        super(ComplexParserTest, self).setUp(
-            'complex_parsing', 'complex_parsing')
-    
-        def test_nonexistent_like(self):
-        with mock.patch('certbot.util.get_os_info') as mock_info:
-            mock_info.return_value = ('nonexistent', 'irrelevant')
-            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
-                for like in entrypoint.OVERRIDE_CLASSES.keys():
-                    mock_like.return_value = [like]
-                    self.assertEqual(entrypoint.get_configurator(),
-                                     entrypoint.OVERRIDE_CLASSES[like])
-    
-        config_path = config_util.ensure_config_exists(config_dir)
-    print('Configuration file:', config_path)
-    return 0
-
-    
-    
-@asyncio.coroutine
-def test_if_fires_on_hass_shutdown(hass):
-    '''Test the firing when HASS starts.'''
-    calls = async_mock_service(hass, 'test', 'automation')
-    hass.state = CoreState.not_running
-    
-        # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(small_frame, model='cnn')
-    
-    # Find all the faces in the image using a pre-trained convolutional neural network.
-# This method is more accurate than the default HOG model, but it's slower
-# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
-# this will use GPU acceleration and perform well.
-# See also: find_faces_in_picture.py
-face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
-    
-        # Load the uploaded image file
-    img = face_recognition.load_image_file(file_stream)
-    # Get face encodings for any faces in the uploaded image
-    unknown_face_encodings = face_recognition.face_encodings(img)
-    
-    
-def _rect_to_css(rect):
+class FlaskCliRunner(CliRunner):
+    '''A :class:`~click.testing.CliRunner` for testing a Flask app's
+    CLI commands. Typically created using
+    :meth:`~flask.Flask.test_cli_runner`. See :ref:`testing-cli`.
     '''
-    Convert a dlib 'rect' object to a plain tuple in (top, right, bottom, left) order
+    def __init__(self, app, **kwargs):
+        self.app = app
+        super(FlaskCliRunner, self).__init__(**kwargs)
     
-        # Print the location of each facial feature in this image
-    for facial_feature in face_landmarks.keys():
-        print('The {} in this face has the following points: {}'.format(facial_feature, face_landmarks[facial_feature]))
+        :copyright: © 2010 by the Pallets team.
+    :license: BSD, see LICENSE for more details.
+'''
     
-    from setuptools import setup
+        def __init__(self):
+        self.tokenizer = self.postokenizer = jieba.posseg.dt
+        self.stop_words = self.STOP_WORDS.copy()
+        self.pos_filt = frozenset(('ns', 'n', 'vn', 'v'))
+        self.span = 5
     
-        # 讲识别结果以json键值对的数据结构输出
-    result = {
-        'face_found_in_image': face_found,
-        'is_picture_of_obama': is_obama
-    }
-    return jsonify(result)
+    import jieba
+import jieba.analyse
+from optparse import OptionParser
+    
+    jieba.enable_parallel()
+    
+    
+def get_serializer(serializer):
+    ''' Return requested serializer '''
+    if serializer == 'json':
+        return JSONSerializer
+    if serializer == 'pickle':
+        return PickleSerializer
+    if serializer == 'yaml' and yaml is not None:
+        return YAMLSerializer
+    if serializer == 'yaml' and yaml is None:
+        logger.warning('You must have PyYAML installed to use YAML as the serializer.'
+                       'Switching to JSON as the serializer.')
+    return JSONSerializer
+    
+        @property
+    def frames_count(self):
+        ''' Return current frames count '''
+        retval = len(self.data)
+        logger.trace(retval)
+        return retval
+    
+            self.add_frame_separator()
+        self.set_mainframe_single_tab_style()
+        parent.add(self, text=self.tabname.title())
+        logger.debug('Initialized %s', self.__class__.__name__,)
+    
+            self.input_spec = InputSpec(ndim=ndim)
+    
+        def debug_monitor(self, update_secs=2):
+        ''' Debug tool for monitoring queues '''
+        thread = threading.Thread(target=self.debug_queue_sizes,
+                                  args=(update_secs, ))
+        thread.daemon = True
+        thread.start()
