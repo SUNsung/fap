949dc092d3d50cc3a9653da1eee9353e59940730
SUNsung <weatherius@gmail.com>
2019-03-28 20:43:27 +0200

    Rewrite [588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py] File in: 2019-03-28 20:43:27

--- pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
+++ pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
@@ -1,64 +1,119 @@
 
         
-            All keys are expected to be strings. The structure remembers the
-    case of the last key to be set, and ``iter(instance)``,
-    ``keys()``, ``items()``, ``iterkeys()``, and ``iteritems()``
-    will contain case-sensitive keys. However, querying and contains
-    testing is case insensitive::
+          if output_dist == 'poisson':
+    rates = means = conversion_factor * model_vals['output_dist_params']
+    plot_time_series(rates, bidx, n_to_plot=n_to_plot, scale=scale,
+                     title=col_title + ' rates (LFADS - red, Truth - black)')
+  elif output_dist == 'gaussian':
+    means_vars = model_vals['output_dist_params']
+    means, vars = np.split(means_vars,2, axis=2) # bxtxn
+    stds = np.sqrt(vars)
+    plot_time_series(means, bidx, n_to_plot=n_to_plot, scale=scale,
+                     title=col_title + ' means (LFADS - red, Truth - black)')
+    plot_time_series(means+stds, bidx, n_to_plot=n_to_plot, scale=scale,
+                     color='c')
+    plot_time_series(means-stds, bidx, n_to_plot=n_to_plot, scale=scale,
+                     color='c')
+  else:
+    assert 'NIY'
     
+      Returns:
+    The length B list of TxN numpy tensors.
+  '''
+  T = len(values_t_bxn)
+  B, N = values_t_bxn[0].shape
+  values_b_txn = []
+  for b in range(B):
+    values_pb_txn = np.zeros([T,N])
+    for t in range(T):
+      values_pb_txn[t,:] = values_t_bxn[t][b,:]
+    values_b_txn.append(values_pb_txn)
     
-        return Server(text_response_handler, **kwargs)
+      def __init__(self, test_data_name='wsc273'):
+    vocab_file = os.path.join(FLAGS.data_dir, 'vocab.txt')
+    self.vocab = utils.CharsVocabulary(vocab_file, 50)
+    assert test_data_name in ['pdp60', 'wsc273'], (
+        'Test data must be pdp60 or wsc273, got {}'.format(test_data_name))
+    self.test_data_name = test_data_name
     
-        def __call__(self, r):
-        # Initialize per-thread state, if needed
-        self.init_per_thread_state()
-        # If we have a saved nonce, skip the 401
-        if self._thread_local.last_nonce:
-            r.headers['Authorization'] = self.build_digest_header(r.method, r.url)
+      Returns:
+    patches: A 2D matrix,
+      each entry is a matrix of shape (batch_size, num_timesteps).
+  '''
+  preprocessed = [['<S>']+sentence+['</S>'] for sentence in sentences]
+  max_len = max([len(sent) for sent in preprocessed])
+    
+      for batch in range(num_batches):
+    x = np.zeros([batch_size, num_steps], dtype=np.int32)
+    y = np.zeros([batch_size, num_steps], dtype=np.int32)
+    w = np.zeros([batch_size, num_steps], dtype=np.float)
+    
+    from .hash_table import HashTable
+from number_theory.prime_numbers import next_prime, check_prime
+    
+    if __name__ == '__main__':
         try:
-            self._thread_local.pos = r.body.tell()
-        except AttributeError:
-            # In the case of HTTPDigestAuth being reused and the body of
-            # the previous request was a file-like object, pos has the
-            # file position of the previous body. Ensure it's set to
-            # None.
-            self._thread_local.pos = None
-        r.register_hook('response', self.handle_401)
-        r.register_hook('response', self.handle_redirect)
-        self._thread_local.num_401_calls = 1
+            raw_input          # Python 2
+        except NameError:
+            raw_input = input  # Python 3
+    
+    The problem is  :
+Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
+Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
+'''
+from __future__ import print_function
+    
+        difference = predict - actual
+    square_diff = np.square(difference)
+    mean_square_diff = square_diff.mean()
+    score = np.sqrt(mean_square_diff)
+    return score
     
     
-def test_system_ssl():
-    '''Verify we're actually setting system_ssl when it should be available.'''
-    assert info()['system_ssl']['version'] != ''
+# ALTERNATIVE METHODS  
+# ctbi= characters that must be in password
+# i= how many letters or characters the password length will be 
+def password_generator(ctbi, i):
+  # Password generator = full boot with random_number, random_letters, and random_character FUNCTIONS
+  pass  # Put your code here...
     
-            server = Server.basic_response_server(requests_to_handle=requests_to_handle)
     
-        return scanner if scanner.success_init else None
+def save_data(file_name, distances, press_times):
+    pf = pandas.DataFrame({'Distance': distances, 'Press_time': press_times})
+    # print(pf)
+    pf.to_csv(file_name, index=False, sep=',')
     
-    REQUIREMENTS = ['dweepy==0.3.0']
+        # 优先获取执行文件目录的配置文件
+    here = sys.path[0]
+    for file in os.listdir(here):
+        if re.match(r'(.+)\.json', file):
+            file_name = os.path.join(here, file)
+            with open(file_name, 'r') as f:
+                print('Load config file from {}'.format(file_name))
+                return json.load(f)
     
-    CONF_HOURS_TO_SHOW = 'hours_to_show'
-CONF_REFRESH = 'refresh'
-ATTR_HOURS_TO_SHOW = CONF_HOURS_TO_SHOW
-ATTR_REFRESH = CONF_REFRESH
+    update = True
     
-                try:
-                kwargs['data'] = self._schema(data)
-            except vol.Invalid as err:
-                _LOGGER.error('Data does not match schema: %s', err)
-                return view.json_message(
-                    'Message format incorrect: {}'.format(err), 400)
-    
-    def generateKey(keySize):
-    print('Generating prime p...')
-    p = rabinMiller.generateLargePrime(keySize)
-    print('Generating prime q...')
-    q = rabinMiller.generateLargePrime(keySize)
-    n = p * q
-    
-                if new_key is None:
-                break
-    
-    def anagram(myword):
-    return word_bysig[signature(myword)]
+    '''
+# === 思路 ===
+# 核心：每次落稳之后截图，根据截图算出棋子的坐标和下一个块顶面的中点坐标，
+#      根据两个点的距离乘以一个时间系数获得长按的时间
+# 识别棋子：靠棋子的颜色来识别位置，通过截图发现最下面一行大概是一条
+           直线，就从上往下一行一行遍历，比较颜色（颜色用了一个区间来比较）
+           找到最下面的那一行的所有点，然后求个中点，求好之后再让 Y 轴坐标
+           减小棋子底盘的一半高度从而得到中心点的坐标
+# 识别棋盘：靠底色和方块的色差来做，从分数之下的位置开始，一行一行扫描，
+           由于圆形的块最顶上是一条线，方形的上面大概是一个点，所以就
+           用类似识别棋子的做法多识别了几个点求中点，这时候得到了块中点的 X
+           轴坐标，这时候假设现在棋子在当前块的中心，根据一个通过截图获取的
+           固定的角度来推出中点的 Y 坐标
+# 最后：根据两点的坐标算距离乘以系数来获取长按时间（似乎可以直接用 X 轴距离）
+'''
+import os
+import shutil
+import time
+import math
+import random
+import json
+from PIL import Image, ImageDraw
+import wda
