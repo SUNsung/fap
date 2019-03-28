f9f171c0a0ca1ef3cae2ab142f860be9af5ce280
SUNsung <weatherius@gmail.com>
2019-03-28 22:20:58 +0200

    Rewrite [55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py] File in: 2019-03-28 22:20:57

--- pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
+++ pyton/55KT35nxgx2qQ55sPA2X4n5H8Y557rJJJpR4QrU55vJe.py
@@ -1,147 +1,180 @@
 
         
-        
-def l1(l=0.01):
-    return L1L2(l1=l)
+            n_features = 10
+    list_n_samples = np.linspace(100, 1000000, 5).astype(np.int)
+    lasso_results, lars_lasso_results = compute_bench(alpha, list_n_samples,
+                                            [n_features], precompute=True)
+    
+                std = X_train.std(axis=0)
+            mean = X_train.mean(axis=0)
+            X_train = (X_train - mean) / std
+            X_test = (X_test - mean) / std
     
     
-def test_reuters():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = reuters.load_data()
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        assert len(x_train) + len(x_test) == 11228
-        (x_train, y_train), (x_test, y_test) = reuters.load_data(maxlen=10)
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        word_index = reuters.get_word_index()
-        assert isinstance(word_index, dict)
-    
-    loss, acc = test_model.evaluate(x_test, y_test, num_classes)
-print('\nTest accuracy: {0}'.format(acc))
+def setup(app):
+    # Format template for issues URI
+    # e.g. 'https://github.com/sloria/marshmallow/issues/{issue}
+    app.add_config_value('issues_uri', default=None, rebuild='html')
+    # Shortcut for GitHub, e.g. 'sloria/marshmallow'
+    app.add_config_value('issues_github_path', default=None, rebuild='html')
+    # Format template for user profile URI
+    # e.g. 'https://github.com/{user}'
+    app.add_config_value('issues_user_uri', default=None, rebuild='html')
+    app.add_role('issue', issue_role)
+    app.add_role('user', user_role)
 
     
-    print('Convert class vector to binary class matrix '
-      '(for use with categorical_crossentropy)')
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-print('y_train shape:', y_train.shape)
-print('y_test shape:', y_test.shape)
+    import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
     
-        for i in range(n_dense - 1):
-        model.add(Dense(dense_units, kernel_initializer=kernel_initializer))
-        model.add(Activation(activation))
-        model.add(dropout(dropout_rate))
+    We generate data from three groups of waveforms. Two of the waveforms
+(waveform 1 and waveform 2) are proportional one to the other. The cosine
+distance is invariant to a scaling of the data, as a result, it cannot
+distinguish these two waveforms. Thus even with no noise, clustering
+using this distance will not separate out waveform 1 and 2.
     
+    agglo = cluster.FeatureAgglomeration(connectivity=connectivity,
+                                     n_clusters=32)
     
-def get(identifier):
-    '''Get the `identifier` loss function.
-    
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/device_tracker.actiontec/
+    The dataset used for evaluation is a 2D grid of isotropic Gaussian
+clusters widely spaced.
 '''
-import logging
-import re
-import telnetlib
-from collections import namedtuple
-import voluptuous as vol
+print(__doc__)
     
-        def get_device_name(self, device):
-        '''Return the name of the given device or None if we don't know.'''
-        if not self.last_results:
-            return None
-        for client in self.last_results:
-            if client['mac'] == device:
-                return client['host']
-        return None
+        # Max concurrency is limited by global CONCURRENT_REQUESTS setting
+    max_concurrent_requests = 8
+    # Requests per second goal
+    qps = None # same as: 1 / download_delay
+    download_delay = None
+    # time in seconds to delay server responses
+    latency = None
+    # number of slots to create
+    slots = 1
     
-        def get_device_name(self, device):
-        '''Return the name of the given device or None if we don't know.'''
-        if not self.last_results:
-            return None
-        for client in self.last_results:
-            if client['mac'] == device:
-                return client['host']
-        return None
+    # Ignore noisy twisted deprecation warnings
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
+del warnings
     
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.host = config[CONF_HOST]
-        self.username = config[CONF_USERNAME]
-        self.password = config[CONF_PASSWORD]
+        def short_desc(self):
+        return 'Check spider contracts'
     
-    import voluptuous as vol
+        spider = None
+    items = {}
+    requests = {}
     
-    from homeassistant.const import (CONF_COMMAND, CONF_NAME)
-from homeassistant.components.notify import (
-    BaseNotificationService, PLATFORM_SCHEMA)
-import homeassistant.helpers.config_validation as cv
+        requires_project = False
+    default_settings = {'SPIDER_LOADER_WARN_ONLY': True}
     
-            _LOGGER.debug('Attempting to call Flock at %s', self._url)
+        def short_desc(self):
+        return 'Get settings values'
     
+        name = 'scrapes'
     
-def get_service(hass, config, discovery_info=None):
-    '''Get the GNTP notification service.'''
-    if config.get(CONF_APP_ICON) is None:
-        icon_file = os.path.join(os.path.dirname(__file__), '..', 'frontend',
-                                 'www_static', 'icons', 'favicon-192x192.png')
-        with open(icon_file, 'rb') as file:
-            app_icon = file.read()
-    else:
-        app_icon = config.get(CONF_APP_ICON)
+                # trustRoot set to platformTrust() will use the platform's root CAs.
+            #
+            # This means that a website like https://www.cacert.org will be rejected
+            # by default, since CAcert.org CA certificate is seldom shipped.
+            return optionsForClientTLS(hostname.decode('ascii'),
+                                       trustRoot=platformTrust(),
+                                       extraCertificateOptions={
+                                            'method': self._ssl_method,
+                                       })
     
-        def send_message(self, message='', **kwargs):
-        '''Send a message to Lannouncer.'''
-        data = kwargs.get(ATTR_DATA)
-        if data is not None and ATTR_METHOD in data:
-            method = data.get(ATTR_METHOD)
-        else:
-            method = ATTR_METHOD_DEFAULT
-    
-    
-def get_service(hass, config, discovery_info=None):
-    '''Get the Pushetta notification service.'''
-    api_key = config[CONF_API_KEY]
-    channel_name = config[CONF_CHANNEL_NAME]
-    send_test_msg = config[CONF_SEND_TEST_MSG]
-    
-    
-class SASIndex(object):
-    row_size_index = 0
-    column_size_index = 1
-    subheader_counts_index = 2
-    column_text_index = 3
-    column_name_index = 4
-    column_attributes_index = 5
-    format_and_label_index = 6
-    column_list_index = 7
-    data_subheader_index = 8
-    
-    
-def test_3():
-    for o in [1 << 8, (1 << 16) - 1, -((1 << 7) + 1), -(1 << 15)]:
-        check(3, o)
-    
-            In a future version may generate parsed tokens from tinycss/tinycss2
+        def _has_ajax_crawlable_variant(self, response):
         '''
-        for decl in declarations_str.split(';'):
-            if not decl.strip():
-                continue
-            prop, sep, val = decl.partition(':')
-            prop = prop.strip().lower()
-            # TODO: don't lowercase case sensitive parts of values (strings)
-            val = val.strip().lower()
-            if sep:
-                yield prop, val
-            else:
-                warnings.warn('Ill-formatted attribute: expected a colon '
-                              'in {decl!r}'.format(decl=decl), CSSWarning)
+        Return True if a page without hash fragment could be 'AJAX crawlable'
+        according to https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
+        '''
+        body = response.text[:self.lookup_bytes]
+        return _has_ajaxcrawlable_meta(body)
+    
+    def makeKeyFiles(name, keySize):
+    if os.path.exists('%s_pubkey.txt' % (name)) or os.path.exists('%s_privkey.txt' % (name)):
+        print('\nWARNING:')
+        print(''%s_pubkey.txt' or '%s_privkey.txt' already exists. \nUse a different name or delete these files and re-run this program.' % (name, name))
+        sys.exit()
+    
+        for i in range(1, n+1):
+        for j in range(1, s+1):
+            dp[i][j]= dp[i][j-1]
+    
+        freqPairs = list(freqToLetter.items())
+    freqPairs.sort(key = getItemAtIndexZero, reverse = True)
+    
+        def test_invalid_registrations(self):
+        msg_prefix = 'Invalid first argument to `register()`: '
+        msg_suffix = (
+            '. Use either `@register(some_class)` or plain `@register` on an '
+            'annotated function.'
+        )
+        @functools.singledispatch
+        def i(arg):
+            return 'base'
+        with self.assertRaises(TypeError) as exc:
+            @i.register(42)
+            def _(arg):
+                return 'I annotated with a non-type'
+        self.assertTrue(str(exc.exception).startswith(msg_prefix + '42'))
+        self.assertTrue(str(exc.exception).endswith(msg_suffix))
+        with self.assertRaises(TypeError) as exc:
+            @i.register
+            def _(arg):
+                return 'I forgot to annotate'
+        self.assertTrue(str(exc.exception).startswith(msg_prefix +
+            '<function TestSingleDispatch.test_invalid_registrations.<locals>._'
+        ))
+        self.assertTrue(str(exc.exception).endswith(msg_suffix))
+    
+        def testReadBadFile(self):
+        self.createTempFile(streams=0, suffix=self.BAD_DATA)
+        with BZ2File(self.filename) as bz2f:
+            self.assertRaises(OSError, bz2f.read)
+    
+        # Load the records from the pickle data stream.
+    file.seek(0)
+    memos = DBUnpickler(file, conn).load()
+    
+    dom = xml.dom.minidom.parseString(document)
+    
+            for i in range(n):  # 对每个数据
+            i_score = np.inf
+            i_label = -1
+            for j in range(k):  # 与每个质心比较
+                s_ij = score(data[i], centers[j])
+                if s_ij < i_score:
+                    i_score = s_ij
+                    i_label = j
+    
+        search_txt = 'html a z'
+    ret = search(search_txt, inverse_index, word_freq)
+    # print(ret)
+    printy(ret)
+    r''' 
+    ['html']
+         ./data\b.txt
+         ./data\c.txt
+    ['a']
+         ./data\b.txt
+         ./data\a.txt
+    ['z']
+         -
+    ['a', 'html']
+         ./data\b.txt
+    ['html', 'z']
+         -
+    ['a', 'z']
+         -
+    ['a', 'html', 'z']
+         -
+    '''
 
     
-            exp = pd.DatetimeIndex(['2011-01-01 09:00', '2011-01-01 10:00',
-                                '2011-01-01 11:00'], tz=tz)
-        tm.assert_index_equal(
-            idx.fillna(pd.Timestamp('2011-01-01 10:00', tz=tz)), exp)
+            # u_tilde(u~): context to question attended query vectors
+        u_tilde = tf.matmul(softmax(S), u)  # [N, T, d]
