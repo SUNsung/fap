697b165847a2cf8216b6e063bc49ad56819f045a
SUNsung <weatherius@gmail.com>
2019-03-29 21:37:54 +0200

    Rewrite [43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp] File in: 2019-03-29 21:37:54

--- cccccc/43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp
+++ cccccc/43VTf97iRGnnE43tGxfPSzt5vw43dXrAdlGrWT243eFx.cpp
@@ -1,325 +1,118 @@
 
         
-            http://www.apache.org/licenses/LICENSE-2.0
+          image_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
+  label_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
+  image_file.read(reinterpret_cast<char*>(&num_items), 4);
+  num_items = swap_endian(num_items);
+  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
+  num_labels = swap_endian(num_labels);
+  CHECK_EQ(num_items, num_labels);
+  image_file.read(reinterpret_cast<char*>(&rows), 4);
+  rows = swap_endian(rows);
+  image_file.read(reinterpret_cast<char*>(&cols), 4);
+  cols = swap_endian(cols);
     
-    #include <iomanip>
-#include 'tensorflow/core/framework/op.h'
-#include 'tensorflow/core/framework/tensor_shape.pb.h'
-#include 'tensorflow/core/grappler/costs/graph_properties.h'
-#include 'tensorflow/core/grappler/grappler_item.h'
+    #define INSTANTIATE_LAYER_GPU_FORWARD(classname) \
+  template void classname<float>::Forward_gpu( \
+      const std::vector<Blob<float>*>& bottom, \
+      const std::vector<Blob<float>*>& top); \
+  template void classname<double>::Forward_gpu( \
+      const std::vector<Blob<double>*>& bottom, \
+      const std::vector<Blob<double>*>& top);
     
-    // Actually dereferences cached numpy arrays. REQUIRES being called while
-// holding the GIL.
-void ClearDecrefCache() {
-  std::vector<void*> cache_copy;
-  {
-    mutex_lock ml(*DelayedDecrefLock());
-    cache_copy.swap(*DecrefCache());
-  }
-  for (void* obj : cache_copy) {
-    Py_DECREF(reinterpret_cast<PyObject*>(obj));
-  }
-}
+      /**
+   * @brief Initialize the Random number generations if needed by the
+   *    transformation.
+   */
+  void InitRand();
     
-    #include <functional>
+     private:
+  // Recursive copy function.
+  void crop_copy(const vector<Blob<Dtype>*>& bottom,
+               const vector<Blob<Dtype>*>& top,
+               const int* offsets,
+               vector<int> indices,
+               int cur_dim,
+               const Dtype* src_data,
+               Dtype* dest_data,
+               bool is_forward);
     
-    // Must be included first
-#include 'tensorflow/python/lib/core/numpy.h'
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    // Indicates a set of options for a device's usage, which generally must be
-// provided at StreamExecutor device-initialization time.
-//
-// These are intended to be useful-but-not-mandatorily-supported options for
-// using devices on the underlying platform. Presently, if the option requested
-// is not available on the target platform, a warning will be emitted.
-struct DeviceOptions {
- public:
-  // When it is observed that more memory has to be allocated for thread stacks,
-  // this flag prevents it from ever being deallocated. Potentially saves
-  // thrashing the thread stack memory allocation, but at the potential cost of
-  // some memory space.
-  static const unsigned kDoNotReclaimStackAllocation = 0x1;
-    }
-    
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-    /// Classify a potential CF typedef.
-CFPointeeInfo
-CFPointeeInfo::classifyTypedef(const clang::TypedefNameDecl *typedefDecl) {
-  clang::QualType type = typedefDecl->getUnderlyingType();
-    }
-    
-    namespace swift {
-    }
-    
-    
-    {  llvm_unreachable('invalid class');
-}
-    
-    #define TEGRA_MULF(src1, sz1, src2, sz2, dst, sz, w, h, scale) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-    CAROTENE_NS::mul(CAROTENE_NS::Size2D(w, h), \
-                     src1, sz1, \
-                     src2, sz2, \
-                     dst, sz, \
-                     (float)scale), \
-    CV_HAL_ERROR_OK \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
-    
-            float32x4_t v_dst0f = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_low_u16(v_src1))), v_beta),
-                                        v_alpha, vcvtq_f32_u32(vmovl_u16(vget_low_u16(v_src0))));
-        float32x4_t v_dst1f = vmlaq_f32(vmulq_f32(vcvtq_f32_u32(vmovl_u16(vget_high_u16(v_src1))), v_beta),
-                                        v_alpha, vcvtq_f32_u32(vmovl_u16(vget_high_u16(v_src0))));
-        uint16x8_t _v_dst = vcombine_u16(vmovn_u32(vcvtq_u32_f32(v_dst0f)),
-                                        vmovn_u32(vcvtq_u32_f32(v_dst1f)));
-    
-    void extract3(const Size2D &size,
-              const u8 * srcBase, ptrdiff_t srcStride,
-              u8 * dstBase, ptrdiff_t dstStride,
-              u32 coi)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-#ifndef __ANDROID__
-    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
-#endif
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
-    
-        for (size_t i = 0u; i < size.height; i += 1)
-    {
-        const u8 * srcy = internal::getRowPtr(srcyBase, srcyStride, i);
-        const u8 * srcu = internal::getRowPtr(srcuBase, srcuStride, i);
-        const u8 * srcv = internal::getRowPtr(srcvBase, srcvStride, i);
-        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t syj = 0u, sj = 0u, dj = 0u;
-    }
-    
-    #include 'common.hpp'
-    
-    ptrdiff_t borderInterpolate(ptrdiff_t _p, size_t _len, BORDER_MODE borderType, size_t startMargin = 0, size_t endMargin = 0);
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC1(s16, 16,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.16 {d4-d5}, [%[src]]                              \n\t'
-            'vmovl.s16 q3, d4                                       \n\t'
-            'vmovl.s16 q4, d5                                       \n\t'
-            'vcvt.f32.s32 q5, q3                                    \n\t'
-            'vcvt.f32.s32 q6, q4                                    \n\t'
-            'vmul.f32 q7, q5, q0                                    \n\t'
-            'vmul.f32 q8, q6, q0                                    \n\t'
-            'vadd.f32 q9, q7, q1                                    \n\t'
-            'vadd.f32 q10, q8, q1                                   \n\t'
-            'vcvt.s32.f32 q11, q9                                   \n\t'
-            'vcvt.s32.f32 q12, q10                                  \n\t'
-            'vqmovn.s32 d26, q11                                    \n\t'
-            'vqmovn.s32 d27, q12                                    \n\t'
-            'vst1.16 {d26-d27}, [%[dst]]                            \n\t'
-            : /*no output*/
-            : [src] 'r' (_src + i),
-              [dst] 'r' (_dst + i + 0),
-              'w' (vshift), 'w' (vscale)
-            : 'd6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27'
-        );
-    }
-})
-#else
-CVTS_FUNC1(s16, 16,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        int16x8_t vline = vld1q_s16(_src + i);
-        int32x4_t vline1_s32 = vmovl_s16(vget_low_s16 (vline));
-        int32x4_t vline2_s32 = vmovl_s16(vget_high_s16(vline));
-        float32x4_t vline1_f32 = vcvtq_f32_s32(vline1_s32);
-        float32x4_t vline2_f32 = vcvtq_f32_s32(vline2_s32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        int16x4_t vRes1 = vqmovn_s32(vline1_s32);
-        int16x4_t vRes2 = vqmovn_s32(vline2_s32);
-        vst1q_s16(_dst + i, vcombine_s16(vRes1, vRes2));
-    }
-})
-#endif
-    
-                int32x2_t vnz1 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx1), vget_high_u32(vlx1)));
-            int32x2_t vnz2 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx2), vget_high_u32(vlx2)));
-    
-                vst1_u8((u8 *)&mask[0], vmovn_u16(v_mask));
-    
-    
-    {
-    {        for (--jd; js < size.width; ++js, --jd)
-            dst[jd] = src[js];
-    }
-}
-    
-    		/*
-		Convert legacy user-profile.cmd to new name user_profile.cmd
-		*/
-		PathCombine(legacyUserProfilePath, userConfigDirPath, L'user-profile.cmd');
-		if (PathFileExists(legacyUserProfilePath))
-		{
-			PathCombine(userProfilePath, userConfigDirPath, L'user_profile.cmd');
-    }
-    
-    /*!
- * \brief Thread pool.
- */
-class ThreadPool {
- public:
-  /*! \brief Signal event upon destruction, even for exceptions (RAII) */
-  struct SetReadyOnDestroy {
-    explicit inline SetReadyOnDestroy(const std::shared_ptr<dmlc::ManualEvent>& event)
-      : event_(event) {
-    }
-    inline ~SetReadyOnDestroy() {
-      if (event_) {
-        event_->signal();
-      }
-    }
-    std::shared_ptr<dmlc::ManualEvent>  event_;
-  };
-    }
-    
-    
-    {}  // namespace mxnet
+    #endif  // CAFFE_CUDNN_POOLING_LAYER_HPP_
 
     
-      void Init(const TVMArgs& args,
-            const std::vector<int>& const_loc,
-            std::vector<Engine::VarHandle>* const_vars,
-            std::vector<Engine::VarHandle>* mutate_vars) {
-    values_.clear();
-    type_codes_.clear();
-    values_.insert(values_.end(), args.values, args.values + args.size());
-    type_codes_.insert(
-        type_codes_.end(), args.type_codes, args.type_codes + args.size());
-    }
+    #include <vector>
     
-    DMLC_REGISTER_PARAMETER(CropParam);
-    
-    namespace mxnet {
-namespace op {
-template<typename DType>
-class CuDNNLocalResponseNormOp : public Operator {
- public:
-  explicit CuDNNLocalResponseNormOp(LRNParam param) {
-    param_ = param;
-    init_cudnn_ = false;
-    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
-  }
-    }
-    }
-    }
-    
-    
-    {
-    {        throw invalid_argument('the input has no solution');
-    }
+    enum GARBAGE_LEVEL
+{
+  G_NEVER_CRUNCH,
+  G_OK,
+  G_DODGY,
+  G_TERRIBLE
 };
     
-    /// Two pointers
-/// Time Complexity:  O(n)
-/// Space Complexity: O(1)
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-    }
-    }
-    
-    // Counting
-// Time Complexity: O(n)
-// Space Complexity: O(3)
-class Solution {
-public:
-    void sortColors(vector<int> &nums) {
-    }
+    struct BlobData {
+  BlobData() : blob(nullptr), choices(nullptr) {}
+  BlobData(int index, Tesseract* tess, const WERD_RES& word)
+    : blob(word.chopped_word->blobs[index]),
+      tesseract(tess),
+      choices(&(*word.ratings)(index, index)) {}
     }
     
-    int main() {
+    class DLLSYM DIR128
+{
+  public:
+    DIR128() = default;
     }
     
-     public:
-  RegisterOutputCallbacks(Row* r) {
-    this->r_ = r;
-  }
+      // Fills in the x-height range accepted by the given unichar_id in blob
+  // coordinates, given its bounding box in the usual baseline-normalized
+  // coordinates, with some initial crude x-height estimate (such as word
+  // size) and this denoting the transformation that was used.
+  // Also returns the amount the character must have shifted up or down.
+  void XHeightRange(int unichar_id, const UNICHARSET& unicharset,
+                    const TBOX& bbox,
+                    float* min_xht,
+                    float* max_xht,
+                    float* yshift) const;
     
-    #include <osquery/utils/system/linux/perf_event/perf_event.h>
+    // Computes the Otsu threshold(s) for the given image rectangle, making one
+// for each channel. Each channel is always one byte per pixel.
+// Returns an array of threshold values and an array of hi_values, such
+// that a pixel value >threshold[channel] is considered foreground if
+// hi_values[channel] is 0 or background if 1. A hi_value of -1 indicates
+// that there is no apparent foreground. At least one hi_value will not be -1.
+// Delete thresholds and hi_values with delete [] after use.
+// The return value is the number of channels in the input image, being
+// the size of the output thresholds and hi_values arrays.
+int OtsuThreshold(Pix* src_pix, int left, int top, int width, int height,
+                  int** thresholds, int** hi_values);
     
-    template <typename MessageType>
-void PerfOutput<MessageType>::forceUnload() {
-  auto const exp = unload();
-  if (exp.isError()) {
-    LOG(ERROR) << 'Could not unload perf event output point '
-               << boost::io::quoted(exp.getError().getMessage());
-  }
-}
     
-      EXPECT_EQ(to_obj.size_, buf.size());
-  EXPECT_EQ(to_obj.fd_, 42);
-  EXPECT_EQ(to_obj.data_ptr_, static_cast<void*>(buf.data()));
+    {  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+};
     
-    ExpectedSuccess<EbpfTracepoint::Error> EbpfTracepoint::unload() {
-  if (fd_ < 0) {
-    return Success{};
-  }
-  bool failed = false;
-  std::string err_msg;
-  int ret = ioctl(fd_, PERF_EVENT_IOC_DISABLE, 0);
-  if (ret < 0) {
-    failed = true;
-    err_msg += ' perf event disabling failed: \'';
-    err_msg += strerror(errno);
-    err_msg += '\'. ';
-  }
-  ret = close(fd_);
-  if (ret < 0) {
-    failed = true;
-    err_msg += ' file descriptor closed with error: \'';
-    err_msg += strerror(errno);
-    err_msg += '\'.';
-  }
-  fd_ = -1;
-  if (failed) {
-    return createError(Error::SystemError, 'EbpfTracepoint unload failed ')
-           << err_msg;
-  }
-  return Success{};
-}
+      // implementing File
+  bool open(const String& filename, const String& mode) override;
+  bool close() override;
     
-    void EnterExitJoiner::drop_stuck_events() {
-  // As far as `table_` is relatively small we can afford to iterarte over it
-  // once in a kCounterLimit events in order to clean it up.
-  for (auto it = table_.begin(); it != table_.end();) {
-    if (it->second.return_value < 0) {
-      it = table_.erase(it);
-    } else {
-      it->second.return_value -= kCounterLimit;
-      ++it;
-    }
-  }
-}
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+        // Select Present Mode
+#ifdef IMGUI_UNLIMITED_FRAME_RATE
+    VkPresentModeKHR present_modes[] = { VK_PRESENT_MODE_MAILBOX_KHR, VK_PRESENT_MODE_IMMEDIATE_KHR, VK_PRESENT_MODE_FIFO_KHR };
+#else
+    VkPresentModeKHR present_modes[] = { VK_PRESENT_MODE_FIFO_KHR };
+#endif
+    wd->PresentMode = ImGui_ImplVulkanH_SelectPresentMode(g_PhysicalDevice, wd->Surface, &present_modes[0], IM_ARRAYSIZE(present_modes));
+    //printf('[vulkan] Selected PresentMode = %d\n', wd->PresentMode);
+    
+    #define STB__SCRAMBLE(h)   (((h) + ((h) >> 16)) & mask)
+    
+    #pragma once
