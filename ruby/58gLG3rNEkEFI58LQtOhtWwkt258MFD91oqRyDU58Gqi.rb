6765413f43dbcd90a135847d56650ce141290ba6
SUNsung <weatherius@gmail.com>
2019-03-29 17:33:22 +0200

    Rewrite [58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb] File in: 2019-03-29 17:33:21

--- ruby/58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb
+++ ruby/58gLG3rNEkEFI58LQtOhtWwkt258MFD91oqRyDU58Gqi.rb
@@ -1,103 +1,138 @@
 
         
-          # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-    clean_up_passwords(resource)
-    yield resource if block_given?
-    respond_with(resource, serialize_options(resource))
+              @terminal_width = if !tty?
+        nil
+      elsif ENV['COLUMNS']
+        ENV['COLUMNS'].to_i
+      else
+        `stty size`.scan(/\d+/).last.to_i
+      end
+    rescue
+      @terminal_width = nil
+    end
+  end
+end
+
+    
+            css('pre').each do |node|
+          node.content = node.content.strip
+    
+            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
+          node.before(node.children).remove
+        end
+    
+        users.all?(&:blank?)
   end
     
-          # Remembers the given resource by setting up a cookie
-      def remember_me(resource)
-        return if request.env['devise.skip_storage']
-        scope = Devise::Mapping.find_scope!(resource)
-        resource.remember_me!
-        cookies.signed[remember_key(resource, scope)] = remember_cookie_values(resource)
-      end
+    gemfile(true) do
+  source 'https://rubygems.org'
+  # Activate the gem you are reporting the issue against.
+  gem 'rails', '~> 4.2.0'
+  gem 'devise', '~> 4.0'
+  gem 'sqlite3'
+  gem 'byebug'
+end
     
-          def parse_uri(location)
-        location && URI.parse(location)
-      rescue URI::InvalidURIError
-        nil
-      end
+            if options[:bypass]
+          ActiveSupport::Deprecation.warn(<<-DEPRECATION.strip_heredoc, caller)
+          [Devise] bypass option is deprecated and it will be removed in future version of Devise.
+          Please use bypass_sign_in method instead.
+          Example:
     
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
+          def extract_path_from_location(location)
+        uri = parse_uri(location)
+    
+            routes.each do |module_name, actions|
+          [:path, :url].each do |path_or_url|
+            actions.each do |action|
+              action = action ? '#{action}_' : ''
+              method = :'#{action}#{module_name}_#{path_or_url}'
+    
+    module Devise
+  # Responsible for handling devise mappings and routes configuration. Each
+  # resource configured by devise_for in routes is actually creating a mapping
+  # object. You can refer to devise_for in routes for usage options.
+  #
+  # The required value in devise_for is actually not used internally, but it's
+  # inflected to find all other values.
+  #
+  #   map.devise_for :users
+  #   mapping = Devise.mappings[:user]
+  #
+  #   mapping.name #=> :user
+  #   # is the scope used in controllers and warden, given in the route as :singular.
+  #
+  #   mapping.as   #=> 'users'
+  #   # how the mapping should be search in the path, given in the route as :as.
+  #
+  #   mapping.to   #=> User
+  #   # is the class to be loaded from routes, given in the route as :class_name.
+  #
+  #   mapping.modules  #=> [:authenticatable]
+  #   # is the modules included in the class
+  #
+  class Mapping #:nodoc:
+    attr_reader :singular, :scoped_path, :path, :controllers, :path_names,
+                :class_name, :sign_out_via, :format, :used_routes, :used_helpers,
+                :failure_app, :router_name
+    
+            # Removes reset_password token
+        def clear_reset_password_token
+          self.reset_password_token = nil
+          self.reset_password_sent_at = nil
         end
-      end
     
-        # Receives an object and find a scope for it. If a scope cannot be found,
-    # raises an error. If a symbol is given, it's considered to be the scope.
-    def self.find_scope!(obj)
-      obj = obj.devise_scope if obj.respond_to?(:devise_scope)
-      case obj
-      when String, Symbol
-        return obj.to_sym
-      when Class
-        Devise.mappings.each_value { |m| return m.name if obj <= m.to }
+          module ClassMethods
+        # Create the cookie key using the record id and remember_token
+        def serialize_into_cookie(record)
+          [record.to_key, record.rememberable_value, Time.now.utc.to_f.to_s]
+        end
+    
+      # String arguments should be evaluated in the context of the caller.
+  it 'accepts a String argument instead of a Proc or block' do
+    trace_var :$Kernel_trace_var_global, '$Kernel_trace_var_extra = true'
+    
+    # when launched as a script, not require'd, (currently from bin/logstash and bin/logstash-plugin) the first
+# argument is the path of a Ruby file to require and a LogStash::Runner class is expected to be
+# defined and exposing the LogStash::Runner#main instance method which will be called with the current ARGV
+# currently lib/logstash/runner.rb and lib/pluginmanager/main.rb are called using this.
+if $0 == __FILE__
+  LogStash::Bundler.setup!({:without => [:build, :development]})
+  require_relative 'patches/jar_dependencies'
+    
+          Dir.glob(::File.join(temp_path, '*.gem')).each do |gem_file|
+        filename = ::File.basename(gem_file)
+    
+      it 'returns the sorted config parts' do
+    expect(subject.config_parts).to eq(ordered_config_parts)
+  end
+    
+        def URIEncodeSingle(cc, result, index)
+      x = (cc >> 12) & 0xF;
+      y = (cc >> 6) & 63;
+      z = cc & 63;
+      octets = Array.new(3);
+      if (cc <= 0x007F)
+        octets[0] = cc;
+      elsif (cc <= 0x07FF)
+        octets[0] = y + 192;
+        octets[1] = z + 128;
       else
-        Devise.mappings.each_value { |m| return m.name if obj.is_a?(m.to) }
+        octets[0] = x + 224;
+        octets[1] = y + 128;
+        octets[2] = z + 128;
       end
-    
-        # Creates configuration values for Devise and for the given module.
-    #
-    #   Devise::Models.config(Devise::Models::DatabaseAuthenticatable, :stretches)
-    #
-    # The line above creates:
-    #
-    #   1) An accessor called Devise.stretches, which value is used by default;
-    #
-    #   2) Some class methods for your model Model.stretches and Model.stretches=
-    #      which have higher priority than Devise.stretches;
-    #
-    #   3) And an instance method stretches.
-    #
-    # To add the class methods you need to have a module ClassMethods defined
-    # inside the given class.
-    #
-    def self.config(mod, *accessors) #:nodoc:
-      class << mod; attr_accessor :available_configs; end
-      mod.available_configs = accessors
-    
-            it_behaves_like 'returns 400 HTTP status'
-      end
-    
-      describe 'checkout#advance' do
-    let(:execute) { patch '/api/v2/storefront/checkout/advance', headers: headers }
-    
-              it 'returns products sorted by updated_at with descending order' do
-            expect(json_response['data'].count).to      eq Spree::Product.count
-            expect(json_response['data'].pluck(:id)).to eq Spree::Product.order(updated_at: :desc).pluck(:id).map(&:to_s)
-          end
-        end
-      end
+      return URIEncodeOctets(octets, result, index);
     end
     
-        context 'by ids' do
-      before { get '/api/v2/storefront/taxons?ids=#{taxons.map(&:id).join(',')}' }
+          def use_identicon
+        @wiki.user_icons == 'identicon'
+      end
     
-              def serialize_resource(resource)
-            resource_serializer.new(
-              resource,
-              include: resource_includes,
-              sparse_fields: sparse_fields
-            ).serializable_hash
-          end
+      teardown do
+    FileUtils.rm_r(File.join(File.dirname(__FILE__), *%w[examples test.git]))
+  end
     
-        match 'v:api/*path', to: redirect { |params, request|
-      format = '.#{params[:format]}' unless params[:format].blank?
-      query  = '?#{request.query_string}' unless request.query_string.blank?
-    }
-    
-          private
-    
-              def serialize_order(order)
-            resource_serializer.new(order.reload, include: resource_includes, fields: sparse_fields).serializable_hash
-          end
+    # Set ruby to UTF-8 mode
+# This is required for Ruby 1.8.7 which gollum still supports.
+$KCODE = 'U' if RUBY_VERSION[0, 3] == '1.8'
