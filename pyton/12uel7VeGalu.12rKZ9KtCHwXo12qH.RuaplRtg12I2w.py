579b4bda23cd81098d79e5c892ea94097e4755a0
SUNsung <weatherius@gmail.com>
2019-03-29 07:30:42 +0200

    Rewrite [12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py] File in: 2019-03-29 07:30:42

--- pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
+++ pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
@@ -1,79 +1,47 @@
 
         
-            with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        loss = MSE_MAE_loss(0.3)
-        inputs = keras.layers.Input((2,))
-        outputs = keras.layers.Dense(1, name='model_output')(inputs)
-        model = keras.models.Model(inputs, outputs)
-        model.compile(optimizer='sgd', loss={'model_output': loss})
-        model.fit(np.random.rand(256, 2), np.random.rand(256, 1))
-        model.save(model_filename)
+        
+def get_serializer_from_ext(ext):
+    ''' Get the sertializer from filename extension '''
+    if ext == '.json':
+        return JSONSerializer
+    if ext == '.p':
+        return PickleSerializer
+    if ext in ('.yaml', '.yml') and yaml is not None:
+        return YAMLSerializer
+    if ext in ('.yaml', '.yml') and yaml is None:
+        logger.warning('You must have PyYAML installed to use YAML as the serializer.\n'
+                       'Switching to JSON as the serializer.')
+    return JSONSerializer
+
     
-        E.g. if your `batch_size` is 64 and you use `gpus=2`,
-    then we will divide the input into 2 sub-batches of 32 samples,
-    process each sub-batch on one GPU, then return the full
-    batch of 64 processed samples.
+        def __init__(self, image, alignments, original_roi=None):
+        logger.debug('Initializing %s: (alignments: %s, original_roi: %s)',
+                     self.__class__.__name__, alignments, original_roi)
+        self.image = image
+        self.alignments = alignments
+        self.roi = original_roi
+        self.colors = {1: (255, 0, 0),
+                       2: (0, 255, 0),
+                       3: (0, 0, 255),
+                       4: (255, 255, 0),
+                       5: (255, 0, 255),
+                       6: (0, 255, 255)}
+        logger.debug('Initialized %s', self.__class__.__name__)
     
-    learning_rate = 1e-6
-clip_norm = 1.0
+        def get_full_frame_name(self, frame):
+        ''' Return a frame with extension for when the extension is
+            not known '''
+        retval = next(key for key in self.data.keys()
+                      if key.startswith(frame))
+        logger.trace('Requested: '%s', Returning: '%s'', frame, retval)
+        return retval
     
-    x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-    
-    
-def mean_absolute_percentage_error(y_true, y_pred):
-    diff = K.abs((y_true - y_pred) / K.clip(K.abs(y_true),
-                                            K.epsilon(),
-                                            None))
-    return 100. * K.mean(diff, axis=-1)
-    
-    _DEVICES_REGEX = re.compile(
-    r'(?P<mac>(([0-9a-f]{2}[:-]){5}([0-9a-f]{2})))\s'
-    r'(?P<ip>([0-9]{1,3}[\.]){3}[0-9]{1,3})\s+'
-    r'(?P<status>([^\s]+))\s+'
-    r'(?P<type>([^\s]+))\s+'
-    r'(?P<intf>([^\s]+))\s+'
-    r'(?P<hwintf>([^\s]+))\s+'
-    r'(?P<host>([^\s]+))')
-    
-    
-def get_service(hass, config, discovery_info=None):
-    '''Get the demo notification service.'''
-    return DemoNotificationService(hass)
-    
-    from homeassistant.components.notify import (
-    ATTR_TITLE, ATTR_TITLE_DEFAULT, PLATFORM_SCHEMA, BaseNotificationService)
-from homeassistant.const import CONF_PASSWORD, CONF_PORT
-import homeassistant.helpers.config_validation as cv
-    
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_API_KEY): cv.string,
-    vol.Optional(CONF_SENDER, default='HA'):
-        vol.All(cv.string, vol.Match(r'^(\+?[1-9]\d{1,14}|\w{1,11})$')),
-})
-    
-                # Target is email, send directly, don't use a target object.
-            # This also seems to work to send to all devices in own account.
-            if ttype == 'email':
-                self._push_data(message, title, data, self.pushbullet, tname)
-                _LOGGER.info('Sent notification to email %s', tname)
-                continue
-    
-    image = face_recognition.load_image_file('{}')
-face_locations = face_recognition.face_locations(image)
-'''
-    
-    
-def face_landmarks(face_image, face_locations=None, model='large'):
-    '''
-    Given an image, returns a dict of face feature locations (eyes, nose, etc) for each face in the image
-    
-            # Now let's list all the faces we found in all 128 frames
-        for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
-            number_of_faces_in_frame = len(face_locations)
+        def add_options_info(self):
+        ''' Add the info bar '''
+        logger.debug('Adding options info')
+        lblinfo = ttk.Label(self.optsframe,
+                            textvariable=self.vars['info'],
+                            anchor=tk.W,
+                            width=70)
+        lblinfo.pack(side=tk.LEFT, padx=5, pady=5, anchor=tk.W)
