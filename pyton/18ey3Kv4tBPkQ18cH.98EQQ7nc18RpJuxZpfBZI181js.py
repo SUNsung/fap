11958028096f26dc7cee44fada5066a345a0fd57
SUNsung <weatherius@gmail.com>
2019-03-29 10:43:31 +0200

    Rewrite [18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py] File in: 2019-03-29 10:43:30

--- pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
+++ pyton/18ey3Kv4tBPkQ18cH.98EQQ7nc18RpJuxZpfBZI181js.py
@@ -1,62 +1,87 @@
 
         
-          def __init__(self, hparams, lemma_embeddings, instance):
-    '''Initialize the LexNET classifier.
-    
-    
-def variable_summaries(var, name):
-  '''Attach a lot of summaries to a Tensor.'''
-  mean = tf.reduce_mean(var)
-  tf.summary.scalar('mean/' + name, mean)
-  with tf.name_scope('stddev'):
-    stddev = tf.sqrt(tf.reduce_sum(tf.square(var - mean)))
-  tf.summary.scalar('sttdev/' + name, stddev)
-  tf.summary.scalar('max/' + name, tf.reduce_max(var))
-  tf.summary.scalar('min/' + name, tf.reduce_min(var))
-  tf.summary.histogram(name, var)
-    
-      # Exponential Moving Average baseline.
-  elif FLAGS.baseline_method == 'ema':
-    # TODO(liamfedus): Recheck.
-    # Lists of rewards and Log probabilities of the actions taken only for
-    # missing tokens.
-    ema = tf.train.ExponentialMovingAverage(decay=hparams.baseline_decay)
-    maintain_averages_op = ema.apply(rewards_list)
-    
-    
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'GroupCommitResolution'
-        db.create_table(
-            'sentry_groupcommitresolution', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'group_id',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
-                ), (
-                    'commit_id',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
-                ), (
-                    'datetime', self.gf('django.db.models.fields.DateTimeField')(
-                        db_index=True
-                    )
-                ),
-            )
+            def __eq__(self, other):
+        return (
+            isinstance(other, self.__class__) and
+            self.keys == other.keys and
+            self.messages == other.messages and
+            self.strict == other.strict
         )
-        db.send_create_signal('sentry', ['GroupCommitResolution'])
     
-            for name, organization_id in dupe_envs:
-            envs = list(
-                orm.Environment.objects.filter(
-                    name=name,
-                    organization_id=organization_id,
-                ).order_by('date_added')
-            )
-            to_env = envs[0]
-            from_envs = envs[1:]
+        if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
     
-            # Adding unique constraint on 'ReleaseHeadCommit', fields ['repository_id', 'release']
-        db.create_unique('sentry_releaseheadcommit', ['repository_id', 'release_id'])
+    print('Evaluate IRNN...')
+model = Sequential()
+model.add(SimpleRNN(hidden_units,
+                    kernel_initializer=initializers.RandomNormal(stddev=0.001),
+                    recurrent_initializer=initializers.Identity(gain=1.0),
+                    activation='relu',
+                    input_shape=x_train.shape[1:]))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+rmsprop = RMSprop(lr=learning_rate)
+model.compile(loss='categorical_crossentropy',
+              optimizer=rmsprop,
+              metrics=['accuracy'])
+    
+    plt.plot(range(epochs),
+         history_model1.history['val_loss'],
+         'g-',
+         label='Network 1 Val Loss')
+plt.plot(range(epochs),
+         history_model2.history['val_loss'],
+         'r-',
+         label='Network 2 Val Loss')
+plt.plot(range(epochs),
+         history_model1.history['loss'],
+         'g--',
+         label='Network 1 Loss')
+plt.plot(range(epochs),
+         history_model2.history['loss'],
+         'r--',
+         label='Network 2 Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('comparison_of_networks.png')
+
+    
+            # Raises
+            AttributeError: if the layer is connected to
+            more than one incoming layers.
+        '''
+        if not self._inbound_nodes:
+            raise AttributeError('The layer has never been called '
+                                 'and thus has no defined input shape.')
+        all_input_shapes = set(
+            [str(node.input_shapes) for node in self._inbound_nodes])
+        if len(all_input_shapes) == 1:
+            input_shapes = self._inbound_nodes[0].input_shapes
+            return unpack_singleton(input_shapes)
+        else:
+            raise AttributeError('The layer '' + str(self.name) +
+                                 ' has multiple inbound nodes, '
+                                 'with different input shapes. Hence '
+                                 'the notion of 'input shape' is '
+                                 'ill-defined for the layer. '
+                                 'Use `get_input_shape_at(node_index)` '
+                                 'instead.')
+    
+        def transform(self, node, results):
+        self.found_future_import = True
+        return self.new_future_import(node)
+    
+    
+def filter_whitespace(mode: str, text: str) -> str:
+    '''Transform whitespace in ``text`` according to ``mode``.
+    
+        def test_oauth10a_request_parameters(self):
+        response = self.fetch('/oauth10a/client/request_params')
+        response.rethrow()
+        parsed = json_decode(response.body)
+        self.assertEqual(parsed['oauth_consumer_key'], 'asdf')
+        self.assertEqual(parsed['oauth_token'], 'uiop')
+        self.assertTrue('oauth_nonce' in parsed)
+        self.assertTrue('oauth_signature' in parsed)
