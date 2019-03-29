38b65ea8cbd7e1b697005ea08f471c66e2760c3f
SUNsung <weatherius@gmail.com>
2019-03-30 00:12:58 +0200

    Rewrite [55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb] File in: 2019-03-30 00:12:58

--- ruby/55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb
+++ ruby/55wZfxgJ9VEhQ5503O4Szi84G655RCVO.3LGRIM55OcY.rb
@@ -1,133 +1,57 @@
 
         
-              dt = Date.today
-      freeze_time dt
+        When /^I (?:like|unlike) the post '([^']*)' in the stream$/ do |post_text|
+  like_stream_post(post_text)
+end
     
-        if successfully_sent?(resource)
-      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
+          it 'should not be catched when it is unexpected' do
+        @target = alice.post :status_message, text: 'AWESOME', to: @alices_aspect.id
+        allow(alice).to receive(:like!).and_raise('something')
+        allow(@controller).to receive(:current_user).and_return(alice)
+        expect { post :create, params: like_hash, format: :json }.to raise_error('something')
+      end
     end
   end
     
-      def after_omniauth_failure_path_for(scope)
-    new_session_path(scope)
-  end
-    
-      # GET /resource/unlock?unlock_token=abcdef
-  def show
-    self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
-    yield resource if block_given?
-    
-    module Devise
-  module Mailers
-    module Helpers
-      extend ActiveSupport::Concern
-    
-          private
-    
-          # Extra data to store with the index entry. This can be anything
-      # and is treated like a general global state bag.
-      #
-      # @return [Hash]
-      attr_accessor :extra_data
-    
-              hosts
-        end
-    
-                @logger.info('With machine: #{machine.name} (#{machine.provider.inspect})')
-            yield machine
-    
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
+        it 'should redirect back in the mobile version if it has > 0 notifications' do
+      FactoryGirl.create(:notification, recipient: alice, type: 'Notifications::StartedSharing')
+      get :read_all, params: {type: 'liked'}, format: :mobile
+      expect(response).to redirect_to(notifications_path)
     end
     
-        # Outputs a single category as an <a> link.
-    #
-    #  +category+ is a category string to format as an <a> link
-    #
-    # Returns string
-    #
-    def category_link(category)
-      dir = @context.registers[:site].config['category_dir']
-      '<a class='category' href='/#{dir}/#{category.to_url}/'>#{category}</a>'
+        def pattern_path(path)
+      return ::File.join(LOGSTASH_HOME, 'patterns', path)
     end
     
-    def config_tag(config, key, tag=nil, classname=nil)
-  options     = key.split('.').map { |k| config[k] }.last #reference objects with dot notation
-  tag       ||= 'div'
-  classname ||= key.sub(/_/, '-').sub(/\./, '-')
-  output      = '<#{tag} class='#{classname}''
-    
-        def poster
-      'poster='#{@poster}'' if @poster
-    end
-    
-      after { FileUtils.rm_rf(public_path) }
-    
-        context 'when file has been cached' do
-      before { uploader.cache!(test_file) }
-    
-        def self.certificates_from_file(path)
-      data = File.read(path)
-      pattern = Regexp.compile('-+BEGIN CERTIFICATE-+\n(?:[^-]*\n)+-+END CERTIFICATE-+\n', Regexp::MULTILINE)
-      list = []
-      data.scan(pattern){|match| list << OpenSSL::X509::Certificate.new(match)}
-      list
-    end
-    
-    puts 'Connected to #{uri}.'
-puts 'Usage:'
-puts '    Fluent::Engine.root_agent.event_router.match('some.tag') : get an output plugin instance'
-puts '    Fluent::Engine.root_agent.inputs[i]                      : get input plugin instances'
-puts '    Fluent::Plugin::OUTPUT_REGISTRY.lookup(name)             : load output plugin class (use this if you get DRb::DRbUnknown)'
-puts ''
-    
-            def each_line(line)
-          line.chomp!
-          vals = line.split('\t')
-    
-          def shutdown
-        super
-      end
+        class Main < Clamp::Command
+      subcommand 'list', 'List all installed Logstash plugins', LogStash::PluginManager::List
+      subcommand 'install', 'Install a Logstash plugin', LogStash::PluginManager::Install
+      subcommand 'remove', 'Remove a Logstash plugin', LogStash::PluginManager::Remove
+      subcommand 'update', 'Update a plugin', LogStash::PluginManager::Update
+      subcommand 'pack', 'Package currently installed plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Pack
+      subcommand 'unpack', 'Unpack packaged plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Unpack
+      subcommand 'generate', 'Create the foundation for a new plugin', LogStash::PluginManager::Generate
+      subcommand 'uninstall', 'Uninstall a plugin. Deprecated: Please use remove instead', LogStash::PluginManager::Remove
+      subcommand 'prepare-offline-pack', 'Create an archive of specified plugins to use for offline installation', LogStash::PluginManager::PrepareOfflinePack
     end
   end
 end
-
     
-            formatter_params = {}
-        FORMATTER_PARAMS.each do |older, newer|
-          next unless newer
-          if conf.has_key?(older)
-            formatter_params[newer] = conf[older]
-          end
-        end
-        unless formatter_params.empty?
-          conf.elements << Fluent::Config::Element.new('format', '', formatter_params, [])
-        end
-      end
+          prepare_package(explicit_plugins_specs, temp_path)
+      LogStash::Util::Zip.compress(temp_path, @target)
+    ensure
+      FileUtils.rm_rf(temp_path)
     end
-  end
-end
-
     
-    module Fluent
-  module Compat
-    class Input < Fluent::Plugin::Input
-      # TODO: warn when deprecated
+          def get_installer_for(plugin_name)
+        uri = pack_uri(plugin_name)
+    
+          post_install_messages.compact.each do |message|
+        PluginManager.ui.info(message)
+      end
+    
+        def user_feedback_string_for(action, platform, machines, options={})
+      experimental_string = options['experimental'] ? 'experimental' : 'non experimental'
+      message  = '#{action} all #{experimental_string} VM's defined in acceptance/Vagrantfile'
+      '#{message} for #{platform}: #{machines}' if !platform.nil?
+    end
