873b57d27fcc5ea5719d23c7553a3ea7a1c5cb82
SUNsung <weatherius@gmail.com>
2019-03-30 04:55:26 +0200

    Rewrite [486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb] File in: 2019-03-30 04:55:25

--- ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
+++ ruby/486eWRxbXcwk487aCPQDTQSPU48SztTIoUju6k48ig.rb
@@ -1,59 +1,71 @@
 
         
-              def perform(start_id, stop_id)
-        update = '
-          latest_merge_request_diff_id = (
-            SELECT MAX(id)
-            FROM merge_request_diffs
-            WHERE merge_requests.id = merge_request_diffs.merge_request_id
-          )'.squish
+              # A helper class for comparing the positions of different parts of a
+      # `pair` node.
+      class HashElementDelta
+        def initialize(first, second)
+          @first = first
+          @second = second
     
-          # Sets multiple keys to a given value.
+          # Checks whether the `when` node has a `then` keyword.
       #
-      # mapping - A Hash mapping the cache keys to their values.
-      # timeout - The time after which the cache key should expire.
-      def self.write_multiple(mapping, timeout: TIMEOUT)
-        Redis::Cache.with do |redis|
-          redis.multi do |multi|
-            mapping.each do |raw_key, value|
-              multi.set(cache_key_for(raw_key), value, ex: timeout)
-            end
-          end
-        end
+      # @return [Boolean] whether the `when` node has a `then` keyword
+      def then?
+        loc.begin && loc.begin.is?('then')
       end
     
-          # Iterates over all of the objects for the given method (e.g. `:labels`).
-      #
-      # method - The method to send to Octokit for querying data.
-      # args - Any arguments to pass to the Octokit method.
-      def each_object(method, *args, &block)
-        return to_enum(__method__, method, *args) unless block_given?
+          it 'renders selected filters' do
+        click_on 'Filter'
     
-            def sidekiq_worker_class
-          ImportDiffNoteWorker
+      context 'moving variants between shipments', js: true do
+    before do
+      create(:stock_location, name: 'LA')
+      visit spree.admin_orders_path
+      within_row(1) do
+        click_link 'R100'
+      end
+    end
+    
+          if simple_current_order.nil? || simple_current_order.item_count.zero?
+        text = '<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{Spree.t('empty')})'
+        css_class = 'empty'
+      else
+        text = '<span class='glyphicon glyphicon-shopping-cart'></span> #{text}: (#{simple_current_order.item_count})
+                <span class='amount'>#{simple_current_order.display_total.to_html}</span>'
+        css_class = 'full'
+      end
+    
+      SPREE_GEMS.each do |gem_name|
+    rm_f  '#{gem_name}/Gemfile.lock'
+    rm_rf '#{gem_name}/pkg'
+    rm_rf '#{gem_name}/spec/dummy'
+  end
+end
+    
+                if @order.completed? || @order.next
+              state_callback(:after)
+              respond_with(@order, default_template: 'spree/api/v1/orders/show')
+            else
+              respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
+            end
+          else
+            invalid_resource!(@order)
+          end
         end
     
-            # issue - An instance of `Gitlab::GithubImport::Representation::Issue`
-        # project - An instance of `Project`
-        # client - An instance of `Gitlab::GithubImport::Client`
-        def initialize(issue, project, client)
-          @issue = issue
-          @project = project
-          @client = client
-          @label_finder = LabelFinder.new(project)
+              @products = @products.distinct.page(params[:page]).per(params[:per_page])
+          expires_in 15.minutes, public: true
+          headers['Surrogate-Control'] = 'max-age=#{15.minutes}'
+          respond_with(@products)
         end
     
-            def collection_method
-          :issues_comments
-        end
-    
-              new(hash)
-        end
-    
-            # attributes - A Hash containing the user details. The keys of this
-        #              Hash (and any nested hashes) must be symbols.
-        def initialize(attributes)
-          @attributes = attributes
+            def scope
+          if params[:country_id]
+            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
+            @country.states.accessible_by(current_ability, :read).order('name ASC')
+          else
+            State.accessible_by(current_ability, :read).order('name ASC')
+          end
         end
       end
     end
@@ -61,66 +73,17 @@
 end
 
     
-        def fragment_url_string?(str)
-      str[0] == '#'
+            def update
+          @stock_item = StockItem.accessible_by(current_ability, :update).find(params[:id])
+    
+            def show
+          @stock_movement = scope.find(params[:id])
+          respond_with(@stock_movement)
+        end
+    
+        def params
+      indifferent_hash = Hash.new {|hash,key| hash[key.to_s] if Symbol === key }
+    
+        def post(path, &block)
+      route(POST, path, &block)
     end
-    
-      def insert(index, *paths)
-    @paths = parse(*@paths.insert(index, *paths))
-    self
-  end
-    
-        context 'when there are no named arguments' do
-      let(:argv) { [] }
-    
-      it 'raises #{frozen_error_class} on an untainted, frozen object' do
-    o = Object.new.freeze
-    lambda { o.taint }.should raise_error(frozen_error_class)
-  end
-    
-    describe 'main#define_method' do
-  before :each do
-    @code = 'define_method(:boom) { :bam }'
-  end
-    
-      def pod_prefix
-    File.expand_path('../..', pod_bin)
-  end
-    
-        def explicitly_declared_plugins_specs
-      @plugins_to_package.collect do |plugin_pattern|
-        specs = SpecificationHelpers.find_by_name_with_wildcards(plugin_pattern)
-    
-        puts('Packaging plugins for offline usage')
-    
-      get(/.+/) do
-    send_sinatra_file(request.path) {404}
-  end
-    
-      class IncludeArrayTag < Liquid::Tag
-    Syntax = /(#{Liquid::QuotedFragment}+)/
-    def initialize(tag_name, markup, tokens)
-      if markup =~ Syntax
-        @array_name = $1
-      else
-        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
-      end
-    
-      # Summary is used on the Archive pages to return the first block of content from a post.
-  def summary(input)
-    if input.index(/\n\n/)
-      input.split(/\n\n/)[0]
-    else
-      input
-    end
-  end
-    
-    When /^(?:|I )fill in '([^']*)' for '([^']*)'$/ do |value, field|
-  fill_in(field, :with => value)
-end
-    
-        def raise_because_imagemagick_missing
-      raise Errors::CommandNotFoundError.new('Could not run the `identify` command. Please install ImageMagick.')
-    end
-  end
-end
