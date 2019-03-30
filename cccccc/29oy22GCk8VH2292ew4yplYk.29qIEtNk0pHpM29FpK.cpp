e0b83759947563b8491d6809e432cb27d016a115
SUNsung <weatherius@gmail.com>
2019-03-30 23:56:50 +0200

    Rewrite [29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp] File in: 2019-03-30 23:56:49

--- cccccc/29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp
+++ cccccc/29oy22GCk8VH2292ew4yplYk.29qIEtNk0pHpM29FpK.cpp
@@ -1,270 +1,173 @@
 
         
-        bool UvTaskRunner::PostDelayedTask(const base::Location& from_here,
-                                   base::OnceClosure task,
-                                   base::TimeDelta delay) {
-  auto* timer = new uv_timer_t;
-  timer->data = this;
-  uv_timer_init(loop_, timer);
-  uv_timer_start(timer, UvTaskRunner::OnTimeout, delay.InMilliseconds(), 0);
-  tasks_[timer] = std::move(task);
-  return true;
-}
+          // Generated output should contain 3, 6, or 9 fractional digits.
+  EXPECT_EQ('1s', TimeUtil::ToString(TimeUtil::SecondsToDuration(1)));
+  EXPECT_EQ('0.010s', TimeUtil::ToString(TimeUtil::MillisecondsToDuration(10)));
+  EXPECT_EQ('0.000010s',
+            TimeUtil::ToString(TimeUtil::MicrosecondsToDuration(10)));
+  EXPECT_EQ('0.000000010s',
+            TimeUtil::ToString(TimeUtil::NanosecondsToDuration(10)));
     
-    class AutoUpdater : public mate::EventEmitter<AutoUpdater>,
-                    public auto_updater::Delegate,
-                    public WindowListObserver {
- public:
-  static mate::Handle<AutoUpdater> Create(v8::Isolate* isolate);
-    }
-    
-    // All instances of TrackableObject will be kept in a weak map and can be got
-// from its ID.
-template <typename T>
-class TrackableObject : public TrackableObjectBase,
-                        public mate::EventEmitter<T> {
- public:
-  // Mark the JS object as destroyed.
-  void MarkDestroyed() {
-    v8::Local<v8::Object> wrapper = Wrappable<T>::GetWrapper();
-    if (!wrapper.IsEmpty()) {
-      wrapper->SetAlignedPointerInInternalField(0, nullptr);
+      // Sets the destination filename and enables images to be written to a PDF
+  // on destruction.
+  void WritePDF(const char* filename) {
+    if (pixaGetCount(pixa_) > 0) {
+      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
+      pixaClear(pixa_);
     }
   }
+    
+    // A simple class to provide a dynamic programming solution to a class of
+// 1st-order problems in which the cost is dependent only on the current
+// step and the best cost to that step, with a possible special case
+// of using the variance of the steps, and only the top choice is required.
+// Useful for problems such as finding the optimal cut points in a fixed-pitch
+// (vertical or horizontal) situation.
+// Skeletal Example:
+// DPPoint* array = new DPPoint[width];
+// for (int i = 0; i < width; i++) {
+//   array[i].AddLocalCost(cost_at_i)
+// }
+// DPPoint* best_end = DPPoint::Solve(..., array);
+// while (best_end != nullptr) {
+//   int cut_index = best_end - array;
+//   best_end = best_end->best_prev();
+// }
+// delete [] array;
+class DPPoint {
+ public:
+  // The cost function evaluates the total cost at this (excluding this's
+  // local_cost) and if it beats this's total_cost, then
+  // replace the appropriate values in this.
+  typedef int64_t (DPPoint::*CostFunc)(const DPPoint* prev);
     }
     
+    class LLSQ {
+ public:
+  LLSQ() {  // constructor
+    clear();  // set to zeros
+  }
+  void clear();  // initialize
+    }
     
-    {}  // namespace atom
+    STRING ParagraphModel::ToString() const {
+  char buffer[200];
+  const STRING &alignment = ParagraphJustificationToString(justification_);
+  snprintf(buffer, sizeof(buffer),
+           'margin: %d, first_indent: %d, body_indent: %d, alignment: %s',
+           margin_, first_indent_, body_indent_, alignment.string());
+  return STRING(buffer);
+}
 
     
-    namespace atom {
+    ELISTIZEH(PARA)
+    
+    // Computes the histogram for the given image rectangle, and the given
+// single channel. Each channel is always one byte per pixel.
+// Histogram is always a kHistogramSize(256) element array to count
+// occurrences of each pixel value.
+void HistogramRect(Pix* src_pix, int channel,
+                   int left, int top, int width, int height,
+                   int* histogram);
+    
+        REJ & operator= (            //assign REJ
+    const REJ & source) {        //from this
+      flags1 = source.flags1;
+      flags2 = source.flags2;
+      return *this;
     }
     
-    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
-                                       net::NetworkDelegate* network_delegate)
-    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
+    #include 'leveldb/status.h'
     
-    #endif // BITCOIN_REVERSELOCK_H
-
-    
-    
-static void secp256k1_gej_add_ge(secp256k1_gej *r, const secp256k1_gej *a, const secp256k1_ge *b) {
-    /* Operations: 7 mul, 5 sqr, 4 normalize, 21 mul_int/add/negate/cmov */
-    static const secp256k1_fe fe_1 = SECP256K1_FE_CONST(0, 0, 0, 0, 0, 0, 0, 1);
-    secp256k1_fe zz, u1, u2, s1, s2, t, tt, m, n, q, rr;
-    secp256k1_fe m_alt, rr_alt;
-    int infinity, degenerate;
-    VERIFY_CHECK(!b->infinity);
-    VERIFY_CHECK(a->infinity == 0 || a->infinity == 1);
+    namespace leveldb {
     }
     
-    void test_ecdsa_recovery_api(void) {
-    /* Setup contexts that just count errors */
-    secp256k1_context *none = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
-    secp256k1_context *sign = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
-    secp256k1_context *vrfy = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
-    secp256k1_context *both = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
-    secp256k1_pubkey pubkey;
-    secp256k1_pubkey recpubkey;
-    secp256k1_ecdsa_signature normal_sig;
-    secp256k1_ecdsa_recoverable_signature recsig;
-    unsigned char privkey[32] = { 1 };
-    unsigned char message[32] = { 2 };
-    int32_t ecount = 0;
-    int recid = 0;
-    unsigned char sig[74];
-    unsigned char zero_privkey[32] = { 0 };
-    unsigned char over_privkey[32] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-    }
+      fname = InfoLogFileName('foo');
+  ASSERT_EQ('foo/', std::string(fname.data(), 4));
+  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
+  ASSERT_EQ(0, number);
+  ASSERT_EQ(kInfoLogFile, type);
     
-    // Define for C++11 variadic templates which make the code shorter & more
-// general.  If you don't define this, C++11 support is autodetected below.
-#define TINYFORMAT_USE_VARIADIC_TEMPLATES
+     private:
+  ~MemTable();  // Private since only Unref() should be used to delete it
     
-    
-    {} // namespace bech32
-    
-    
-    {
-    {        : '+r'(s), '+r'(chunk), '+r'(blocks), '=r'(a), '=r'(b), '=r'(c), '=r'(d), /* e = chunk */ '=r'(f), '=r'(g), '=r'(h), '=r'(y0), '=r'(y1), '=r'(y2), '=r'(tbl), '+m'(inp_end), '+m'(inp), '+m'(xfer)
-        : 'm'(K256), 'm'(FLIP_MASK), 'm'(SHUF_00BA), 'm'(SHUF_DC00)
-        : 'cc', 'memory', 'xmm0', 'xmm1', 'xmm2', 'xmm3', 'xmm4', 'xmm5', 'xmm6', 'xmm7', 'xmm8', 'xmm9', 'xmm10', 'xmm11', 'xmm12'
-   );
-}
-}
-    
-                if(cn > 1)
-            {
-                for(size_t j = 0, jn = 0; j < size.width; ++j, jn += cn)
-                {
-                    size_t maxIdx = jn;
-                    for(s32 k = 1; k < cn; ++k)
-                        if(_norm[jn + k] > _norm[maxIdx]) maxIdx = jn + k;
-                    _norm[j] = _norm[maxIdx];
-                    _dx[j] = _dx[maxIdx];
-                    _dy[j] = _dy[maxIdx];
-                }
-            }
-    
-    #define SPLIT_QUAD(sgn, bits, n) { \
-                                     internal::prefetch(src + sj); \
-                                     SPLIT_ASM##n(sgn, bits) \
-                                 }
-    
-        for (size_t i = 0u; i < size.height; ++i)
-    {
-        const u8 * srcy = internal::getRowPtr(srcyBase, srcyStride, i);
-        const u8 * srcu = internal::getRowPtr(srcuBase, srcuStride, i);
-        const u8 * srcv = internal::getRowPtr(srcvBase, srcvStride, i);
-        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t syj = 0u, sj = 0u, dj = 0u;
-    }
-    
-    ptrdiff_t borderInterpolate(ptrdiff_t _p, size_t _len, BORDER_MODE borderType, size_t startMargin, size_t endMargin)
-{
-    ptrdiff_t p = _p + (ptrdiff_t)startMargin;
-    size_t len = _len + startMargin + endMargin;
-    if( (size_t)p < len )
-        return _p;
-    else if( borderType == BORDER_MODE_REPLICATE )
-        p = p < 0 ? 0 : (ptrdiff_t)len - 1;
-    else if( borderType == BORDER_MODE_REFLECT || borderType == BORDER_MODE_REFLECT101 )
-    {
-        s32 delta = borderType == BORDER_MODE_REFLECT101;
-        if( len == 1 )
-            return 0;
-        do
-        {
-            if( p < 0 )
-                p = -p - 1 + delta;
-            else
-                p = (ptrdiff_t)len - 1 - (p - (ptrdiff_t)len) - delta;
-        }
-        while( (size_t)p >= len );
-    }
-    else if( borderType == BORDER_MODE_WRAP )
-    {
-        if( p < 0 )
-            p -= ((p-(ptrdiff_t)len+1)/(ptrdiff_t)len)*(ptrdiff_t)len;
-        if( p >= (ptrdiff_t)len )
-            p %= (ptrdiff_t)len;
-    }
-    else if( borderType == BORDER_MODE_CONSTANT )
-        p = -1;
-    else
-        internal::assertSupportedConfiguration(false);
-    return p - (ptrdiff_t)startMargin;
-}
-    
-    #include <cstddef>
-#include <cstdlib>
-#include <algorithm>
-    
-    
-    {    return 0;
-#endif
-}
-    
-        v1k8 = vextq_s16(d8_15, d16_23, 4);
-    ak8 = vminq_s16(ak8, v1k8);
-    bk8 = vmaxq_s16(bk8, v1k8);
-    
-        if (flipMode == FLIP_VERTICAL_MODE)
-    {
-        for (size_t y = 0; y < size.height; ++y)
-        {
-            const u8 * src_row = internal::getRowPtr(srcBase, srcStride, y);
-            u8 * dst_row = internal::getRowPtr(dstBase, dstStride, size.height - y - 1);
-    }
-    }
-    
-        for( ptrdiff_t y = 0; y < rows; y++ )
-    {
-        const u8* v0 = 0;
-        const u8* v1 = internal::getRowPtr(srcBase, srcStride, y);
-        const u8* v2 = 0;
-        // make border
-        if (border == BORDER_MODE_REFLECT101) {
-            v0 = internal::getRowPtr(srcBase, srcStride, y > 0 ? y-1 : y+1);
-            v2 = internal::getRowPtr(srcBase, srcStride, y < rows-1 ? y+1 : rows > 1 ? rows-2 : 0);
-        } else  if (border == BORDER_MODE_CONSTANT) {
-            v0 = y > 0 ? internal::getRowPtr(srcBase, srcStride, y-1) : tmp;
-            v2 = y < rows-1 ? internal::getRowPtr(srcBase, srcStride, y+1) : tmp;
+      while (msg == nullptr && GetVarint32(&input, &tag)) {
+    switch (tag) {
+      case kComparator:
+        if (GetLengthPrefixedSlice(&input, &str)) {
+          comparator_ = str.ToString();
+          has_comparator_ = true;
         } else {
-            v0 = internal::getRowPtr(srcBase, srcStride, y > 0 ? y-1 : 0);
-            v2 = internal::getRowPtr(srcBase, srcStride, y < rows-1 ? y+1 : rows > 0 ? rows-1 : 0);
+          msg = 'comparator name';
         }
-        s16* drow = internal::getRowPtr(dstBase, dstStride, y);
+        break;
+    }
     }
     
-    //////////////////////////////////////////////////////////////////////
+    namespace leveldb {
+    }
     
-      // for assert purposes
-  DEBUG_ONLY CodeAddress li64StartPos = frontier();
+      bool isValid() const { return past != SrcPos{0,0}; }
     
-    #define CONTAINER_CONFIG_BODY(T, METHOD) \
-T Config::Get##METHOD(const IniSetting::Map& ini, const Hdf& config, \
-                      const std::string& name /* = '' */, \
-                      const T& defValue /* = T() */, \
-                      const bool prepend_hhvm /* = true */) { \
-  auto ini_name = IniName(name, prepend_hhvm); \
-  Hdf hdf = name != '' ? config[name] : config; \
-  T ini_ret, hdf_ret; \
-  auto value = ini_iterate(ini, ini_name); \
-  if (value.isArray() || value.isObject()) { \
-    ini_on_update(value.toVariant(), ini_ret); \
-    /** Make sure that even if we have an ini value, that if we also **/ \
-    /** have an hdf value, that it maintains its edge as beating out **/ \
-    /** ini                                                          **/ \
-    if (hdf.exists() && !hdf.isEmpty()) { \
-      hdf.configGet(hdf_ret); \
-      if (hdf_ret != ini_ret) { \
-        ini_ret = hdf_ret; \
-        IniSetting::SetSystem(ini_name, ini_get(ini_ret)); \
-      } \
-    } \
-    return ini_ret; \
-  } \
-  if (hdf.exists() && !hdf.isEmpty()) { \
-    hdf.configGet(hdf_ret); \
-    return hdf_ret; \
-  } \
-  return defValue; \
-} \
-void Config::Bind(T& loc, const IniSetting::Map& ini, const Hdf& config, \
-                  const std::string& name /* = '' */, \
-                  const T& defValue /* = T() */, \
-                  const bool prepend_hhvm /* = true */) { \
-  loc = Get##METHOD(ini, config, name, defValue, prepend_hhvm); \
-  IniSetting::Bind(IniSetting::CORE, IniSetting::PHP_INI_SYSTEM, \
-                   IniName(name, prepend_hhvm), &loc); \
+    
+struct Config {
+  /*
+   * Normalizes hdf string names to their ini counterparts
+   *
+   * We have special handling for a few hdf strings such as those containing
+   * MySQL, Eval, IPv[4|6] and EnableHipHopSyntax
+   */
+  static std::string IniName(const Hdf& config,
+                             const bool prepend_hhvm = true);
+  static std::string IniName(const std::string& config,
+                             const bool prepend_hhvm = true);
+    }
+    
+         */
+    bool initWithTargetAndOffset(Node *followedNode,float xOffset,float yOffset,const Rect& rect = Rect::ZERO);
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+    
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+        /** 
+    @brief Initializes an action with duration and grid size.
+    @param gridSize Specify the grid size of the FlipX3D action.
+    @param duration Specify the duration of the FlipX3D action. It's a value in seconds.
+    @return If the initialization success, return true; otherwise, return false.
+    */
+    virtual bool initWithSize(const Size& gridSize, float duration);
+    
+    void __CCCallFuncND::execute()
+{
+    if (_callFuncND)
+    {
+        (_selectorTarget->*_callFuncND)(_target, _data);
+    }
 }
     
-    #include 'hphp/runtime/base/file.h'
-#include 'hphp/runtime/base/stream-wrapper.h'
-#include 'hphp/runtime/base/stream-wrapper-registry.h'
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
     
-    #include <vector>
-#include <string>
-#include <iostream>
-#include <sstream>
+    Animation* Animation::create(const Vector<AnimationFrame*>& arrayOfAnimationFrameNames, float delayPerUnit, unsigned int loops /* = 1 */)
+{
+    Animation *animation = new (std::nothrow) Animation();
+    animation->initWithAnimationFrames(arrayOfAnimationFrameNames, delayPerUnit, loops);
+    animation->autorelease();
+    return animation;
+}
     
-    #define ERROR_RAISE_WARNING(exp)        \
-  int ret = (exp);                      \
-  if (ret != 0) {                       \
-    raise_warning(                      \
-      '%s(): %s',                       \
-      __FUNCTION__,                     \
-      folly::errnoStr(errno).c_str()    \
-    );                                  \
-  }                                     \
     
-    req::ptr<File>
-GlobStreamWrapper::open(const String& filename, const String& /*mode*/,
-                        int /*options*/,
-                        const req::ptr<StreamContext>& /*context*/) {
-  // Can't open a glob as a file, it's meant to be opened as a directory
-    }
+class Animation;
