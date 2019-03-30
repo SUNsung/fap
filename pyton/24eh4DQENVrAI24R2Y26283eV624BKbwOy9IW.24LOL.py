fab396fdaee0aaa9dc5bd91bf735ebbba80cf46b
SUNsung <weatherius@gmail.com>
2019-03-30 19:37:32 +0200

    Rewrite [24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py] File in: 2019-03-30 19:37:31

--- pyton/24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py
+++ pyton/24eh4DQENVrAI24R2Y26283eV624BKbwOy9IW.24LOL.py
@@ -1,89 +1,111 @@
 
         
-            Args:
-      session: The current TensorFlow session.
-      num_steps: The number of steps in each epoch.
+            return out
     
-      if ext_input_bxtxi is not None:
-    input_title += ' External Input'
-    plot_time_series(ext_input_bxtxi, n_to_plot=n_to_plot, color='b',
-                     scale=scale, title=col_title + input_title)
+    '''
+requests.exceptions
+~~~~~~~~~~~~~~~~~~~
     
-        tf.logging.info('Recovering Graph %s', gd_file)
-    t = {}
-    [t['states_init'], t['lstm/lstm_0/control_dependency'],
-     t['lstm/lstm_1/control_dependency'], t['softmax_out'], t['class_ids_out'],
-     t['class_weights_out'], t['log_perplexity_out'], t['inputs_in'],
-     t['targets_in'], t['target_weights_in'], t['char_inputs_in'],
-     t['all_embs'], t['softmax_weights'], t['global_step']
-    ] = tf.import_graph_def(gd, {}, ['states_init',
-                                     'lstm/lstm_0/control_dependency:0',
-                                     'lstm/lstm_1/control_dependency:0',
-                                     'softmax_out:0',
-                                     'class_ids_out:0',
-                                     'class_weights_out:0',
-                                     'log_perplexity_out:0',
-                                     'inputs_in:0',
-                                     'targets_in:0',
-                                     'target_weights_in:0',
-                                     'char_inputs_in:0',
-                                     'all_embs_out:0',
-                                     'Reshape_3:0',
-                                     'global_step:0'], name='')
+    import pytest
+from requests.compat import urljoin
     
-        self.bos_char = free_ids[0]  # <begin sentence>
-    self.eos_char = free_ids[1]  # <end sentence>
-    self.bow_char = free_ids[2]  # <begin word>
-    self.eow_char = free_ids[3]  # <end word>
-    self.pad_char = free_ids[4]  # <padding>
+    from . import utils
+from . import packages
+from .models import Request, Response, PreparedRequest
+from .api import request, get, head, post, patch, put, delete, options
+from .sessions import session, Session
+from .status_codes import codes
+from .exceptions import (
+    RequestException, Timeout, URLRequired,
+    TooManyRedirects, HTTPError, ConnectionError,
+    FileModeWarning, ConnectTimeout, ReadTimeout
+)
     
-      Yields:
-    Pairs of the batched data, each a matrix of shape [batch_size, num_steps].
-    The second element of the tuple is the same data time-shifted to the
-    right by one.
     
-      Args:
-    real_values: Value given by the Wasserstein Discriminator to real data.
-    fake_values: Value given by the Wasserstein Discriminator to fake data.
-    
-      samples = []
-  for sequence_id in xrange(min(len(arr), max_num_to_print)):
-    buffer_str = ' '.join(
-        [str(id_to_word[index]) for index in arr[sequence_id, :]])
-    samples.append(buffer_str)
-  return samples
-    
-        def get_device_name(self, device):
-        '''Return the name of the device with the given MAC address.'''
-        name = next((
-            result.name for result in self.last_results
-            if result.mac == device), None)
-        return name
-    
-    import homeassistant.helpers.config_validation as cv
-from homeassistant.components.device_tracker import (
-    DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
-from homeassistant.const import CONF_HOST
-    
-        def send_message(self, message='', **kwargs):
-        '''Send a message to a user.'''
-        data = {
-            'apiKey': self.api_key,
-            'to': self.recipient,
-            'content': message,
-        }
-    
-    from homeassistant.const import (CONF_COMMAND, CONF_NAME)
-from homeassistant.components.notify import (
-    BaseNotificationService, PLATFORM_SCHEMA)
-import homeassistant.helpers.config_validation as cv
-    
-        def send_message(self, message='', **kwargs):
-        '''Send a message to a user.'''
-        kwargs['message'] = message
-        self.hass.bus.fire(EVENT_NOTIFY, kwargs)
+def test_idna_with_version_attribute(mocker):
+    '''Verify we're actually setting idna version when it should be available.'''
+    mocker.patch('requests.help.idna', new=VersionedPackage('2.6'))
+    assert info()['idna'] == {'version': '2.6'}
 
     
+        # Client Error.
+    400: ('bad_request', 'bad'),
+    401: ('unauthorized',),
+    402: ('payment_required', 'payment'),
+    403: ('forbidden',),
+    404: ('not_found', '-o-'),
+    405: ('method_not_allowed', 'not_allowed'),
+    406: ('not_acceptable',),
+    407: ('proxy_authentication_required', 'proxy_auth', 'proxy_authentication'),
+    408: ('request_timeout', 'timeout'),
+    409: ('conflict',),
+    410: ('gone',),
+    411: ('length_required',),
+    412: ('precondition_failed', 'precondition'),
+    413: ('request_entity_too_large',),
+    414: ('request_uri_too_large',),
+    415: ('unsupported_media_type', 'unsupported_media', 'media_type'),
+    416: ('requested_range_not_satisfiable', 'requested_range', 'range_not_satisfiable'),
+    417: ('expectation_failed',),
+    418: ('im_a_teapot', 'teapot', 'i_am_a_teapot'),
+    421: ('misdirected_request',),
+    422: ('unprocessable_entity', 'unprocessable'),
+    423: ('locked',),
+    424: ('failed_dependency', 'dependency'),
+    425: ('unordered_collection', 'unordered'),
+    426: ('upgrade_required', 'upgrade'),
+    428: ('precondition_required', 'precondition'),
+    429: ('too_many_requests', 'too_many'),
+    431: ('header_fields_too_large', 'fields_too_large'),
+    444: ('no_response', 'none'),
+    449: ('retry_with', 'retry'),
+    450: ('blocked_by_windows_parental_controls', 'parental_controls'),
+    451: ('unavailable_for_legal_reasons', 'legal_reasons'),
+    499: ('client_closed_request',),
     
-class FileNotificationService(BaseNotificationService):
-    '''Implement the notification service for the File service.'''
+    def check_live_url(url):
+    
+    
+class BufferFull(UnpackException):
+    pass
+    
+    
+def match(obj, buf):
+    assert packb(obj) == buf
+    assert unpackb(buf, use_list=0) == obj
+    
+            try:
+            # qtpy is a small abstraction layer that lets you write
+            # applications using a single api call to either PyQt or PySide
+            # https://pypi.org/project/QtPy
+            import qtpy  # noqa
+        except ImportError:
+            # If qtpy isn't installed, fall back on importing PyQt5, or PyQt5
+            try:
+                import PyQt5  # noqa
+            except ImportError:
+                try:
+                    import PyQt4  # noqa
+                except ImportError:
+                    pass  # fail fast for all non-ImportError exceptions.
+                else:
+                    return init_qt_clipboard()
+            else:
+                return init_qt_clipboard()
+            pass
+        else:
+            return init_qt_clipboard()
+    
+        result = s[lambda x: [True, False, True, True]]
+    tm.assert_series_equal(result, s.iloc[[0, 2, 3]])
+    
+    import random
+import signal
+import subprocess
+    
+    
+class _IncludeBlock(_Node):
+    def __init__(self, name: str, reader: '_TemplateReader', line: int) -> None:
+        self.name = name
+        self.template_name = reader.name
+        self.line = line
