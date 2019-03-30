b6b19e1f7a1e479efd317649d7ab6a50b4f1fbdf
SUNsung <weatherius@gmail.com>
2019-03-30 21:31:31 +0200

    Rewrite [17lopW5s4riN217..2hrDCuXYM17gUWJ3EfFYjs17maA.cpp] File in: 2019-03-30 21:31:30

--- cccccc/17lopW5s4riN217..2hrDCuXYM17gUWJ3EfFYjs17maA.cpp
+++ cccccc/17lopW5s4riN217..2hrDCuXYM17gUWJ3EfFYjs17maA.cpp
@@ -1,42 +1,31 @@
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #include <map>
-    
-    Safe_PyObjectPtr make_safe(PyObject* object) {
-  return Safe_PyObjectPtr(object);
+
+        
+        
+    {  DefaultCacheKey CKey(const_cast<void*>(Key), &DCache.CBs);
+  auto Entry = DCache.Entries.find(CKey);
+  if (Entry != DCache.Entries.end()) {
+    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
+    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
+    DCache.Entries.erase(Entry);
+    return true;
+  }
+  return false;
 }
     
-    string TryFindKernelClass(const string& serialized_node_def) {
-  tensorflow::NodeDef node_def;
-  if (!node_def.ParseFromString(serialized_node_def)) {
-    LOG(WARNING) << 'Error parsing node_def';
-    return '';
-  }
+    #include 'swift/Basic/Cache.h'
+#include 'llvm/ADT/SmallString.h'
+#include <cache.h>
+    
+        StringRef Line = RawText.substr(0, Pos);
+    Lines.push_back(Line);
+    if (!IsFirstLine) {
+      size_t NonWhitespacePos = RawText.find_first_not_of(' ');
+      if (NonWhitespacePos != StringRef::npos)
+        WhitespaceToTrim =
+            std::min(WhitespaceToTrim,
+                     static_cast<unsigned>(NonWhitespacePos));
     }
-    
-    // Contains device-level options that can be specified at a platform level.
-// Example usage:
-//    auto device_options = DeviceOptions::Default();
-    
-      char *oldAllocation = Allocated;
-  char *oldBegin = Begin;
-  std::size_t oldSize = (std::size_t) (End - oldBegin);
+    IsFirstLine = false;
     
     StringRef importer::getCFTypeName(
             const clang::TypedefNameDecl *decl) {
@@ -48,398 +37,94 @@ limitations under the License.
     }
     }
     
-      static CFPointeeInfo forRecord(bool isConst, const clang::RecordDecl *decl) {
-    assert(decl);
-    CFPointeeInfo info;
-    info.IsValid = true;
-    info.IsConst = isConst;
-    info.Decl = decl;
-    return info;
+    #ifndef SWIFT_IMPORTER_IMPORT_AS_MEMBER_INFERENCE_H
+#define SWIFT_IMPORTER_IMPORT_AS_MEMBER_INFERENCE_H
+    
+    std::string Mangle::translateOperator(StringRef Op) {
+  std::string Encoded;
+  for (char ch : Op) {
+    Encoded.push_back(translateOperatorChar(ch));
   }
-    
-    char Mangle::getStandardTypeSubst(StringRef TypeName) {
-#define STANDARD_TYPE(KIND, MANGLING, TYPENAME)      \
-  if (TypeName == #TYPENAME) {                       \
-    return #MANGLING[0];                             \
-  }
-    }
-    
-    SECP256K1_INLINE static void secp256k1_fe_sqr_inner(uint64_t *r, const uint64_t *a) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            rcx:rbx = d
- *            r10-r14 = a0-a4
- *            r15     = M (0xfffffffffffff)
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
-    'movq $0xfffffffffffff,%%r15\n'
-    }
-    
-        secp256k1_context_set_error_callback(tctx, counting_illegal_callback_fn, &ecount);
-    secp256k1_context_set_illegal_callback(tctx, counting_illegal_callback_fn, &ecount);
-    CHECK(secp256k1_ec_pubkey_create(tctx, &point, s_one) == 1);
-    
-    const std::vector<UniValue>& UniValue::getValues() const
-{
-    if (typ != VOBJ && typ != VARR)
-        throw std::runtime_error('JSON value is not an object or array as expected');
-    return values;
+  return Encoded;
 }
     
-    ChaCha20::ChaCha20(const unsigned char* k, size_t keylen)
-{
-    SetKey(k, keylen);
-}
+            virtual const std::unordered_set<StreamInformation>& StreamInfos() override { return m_streamInfos; }
     
-    ; ROTATE_ARGS
-; Rotate values of symbols a...h
-%macro ROTATE_ARGS 0
-%xdefine TMP_ h
-%xdefine h g
-%xdefine g f
-%xdefine f e
-%xdefine e d
-%xdefine d c
-%xdefine c b
-%xdefine b a
-%xdefine a TMP_
-%endm
-    
-    namespace grpc {
-    }
-    
-    inline absl::string_view GetMethod(const grpc_slice* path) {
-  if (GRPC_SLICE_IS_EMPTY(*path)) {
-    return '';
-  }
-  // Check for leading '/' and trim it if present.
-  return absl::StripPrefix(absl::string_view(reinterpret_cast<const char*>(
-                                                 GRPC_SLICE_START_PTR(*path)),
-                                             GRPC_SLICE_LENGTH(*path)),
-                           '/');
-}
-    
-      // Offset and value for currently supported version ID.
-  static constexpr size_t kVersionIdOffset = 0;
-  static constexpr size_t kVersionId = 0;
-    
-    
-    {  const protobuf::FileDescriptor* file_desc =
-      descriptor_pool_->FindFileByName(filename);
-  if (file_desc == nullptr) {
-    return Status(StatusCode::NOT_FOUND, 'File not found.');
-  }
-  std::unordered_set<grpc::string> seen_files;
-  FillFileDescriptorResponse(file_desc, response, &seen_files);
-  return Status::OK;
-}
-    
-        virtual void UpdateArguments(ChannelArguments* args) override {
-      args->SetInt(name_, value_);
-    }
-    virtual void UpdatePlugins(
-        std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {}
-    
-    DynamicThreadPool::DynamicThread::DynamicThread(DynamicThreadPool* pool)
-    : pool_(pool),
-      thd_('grpcpp_dynamic_pool',
-           [](void* th) {
-             static_cast<DynamicThreadPool::DynamicThread*>(th)->ThreadFunc();
-           },
-           this) {
-  thd_.Start();
-}
-DynamicThreadPool::DynamicThread::~DynamicThread() { thd_.Join(); }
-    
-      void Add(const std::function<void()>& callback) override;
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  gpr_log(GPR_ERROR,
-          'Platforms other than Linux, Windows, and MacOS are not supported.');
-  return std::make_pair(busy, total);
-}
-    
-    TEST(LogTest, ReadPastEnd) {
-  CheckOffsetPastEndReturnsNoRecords(5);
-}
-    
-      // Format the header
-  char buf[kHeaderSize];
-  buf[4] = static_cast<char>(n & 0xff);
-  buf[5] = static_cast<char>(n >> 8);
-  buf[6] = static_cast<char>(t);
-    
-    void MemTable::Add(SequenceNumber s, ValueType type,
-                   const Slice& key,
-                   const Slice& value) {
-  // Format of an entry is concatenation of:
-  //  key_size     : varint32 of internal_key.size()
-  //  key bytes    : char[internal_key.size()]
-  //  value_size   : varint32 of value.size()
-  //  value bytes  : char[value.size()]
-  size_t key_size = key.size();
-  size_t val_size = value.size();
-  size_t internal_key_size = key_size + 8;
-  const size_t encoded_len =
-      VarintLength(internal_key_size) + internal_key_size +
-      VarintLength(val_size) + val_size;
-  char* buf = arena_.Allocate(encoded_len);
-  char* p = EncodeVarint32(buf, internal_key_size);
-  memcpy(p, key.data(), key_size);
-  p += key_size;
-  EncodeFixed64(p, (s << 8) | type);
-  p += 8;
-  p = EncodeVarint32(p, val_size);
-  memcpy(p, value.data(), val_size);
-  assert(p + val_size == buf + encoded_len);
-  table_.Insert(buf);
-}
-    
-      KeyComparator comparator_;
-  int refs_;
-  Arena arena_;
-  Table table_;
-    
-      // Return an iterator for the specified file number (the corresponding
-  // file length must be exactly 'file_size' bytes).  If 'tableptr' is
-  // non-null, also sets '*tableptr' to point to the Table object
-  // underlying the returned iterator, or to nullptr if no Table object
-  // underlies the returned iterator.  The returned '*tableptr' object is owned
-  // by the cache and should not be deleted, and is valid for as long as the
-  // returned iterator is live.
-  Iterator* NewIterator(const ReadOptions& options,
-                        uint64_t file_number,
-                        uint64_t file_size,
-                        Table** tableptr = nullptr);
-    
-    void VersionEdit::EncodeTo(std::string* dst) const {
-  if (has_comparator_) {
-    PutVarint32(dst, kComparator);
-    PutLengthPrefixedSlice(dst, comparator_);
-  }
-  if (has_log_number_) {
-    PutVarint32(dst, kLogNumber);
-    PutVarint64(dst, log_number_);
-  }
-  if (has_prev_log_number_) {
-    PutVarint32(dst, kPrevLogNumber);
-    PutVarint64(dst, prev_log_number_);
-  }
-  if (has_next_file_number_) {
-    PutVarint32(dst, kNextFileNumber);
-    PutVarint64(dst, next_file_number_);
-  }
-  if (has_last_sequence_) {
-    PutVarint32(dst, kLastSequence);
-    PutVarint64(dst, last_sequence_);
-  }
-    }
-    
-    class VersionEdit {
- public:
-  VersionEdit() { Clear(); }
-  ~VersionEdit() { }
-    }
-    
-      VersionEdit edit;
-  for (int i = 0; i < 4; i++) {
-    TestEncodeDecode(edit);
-    edit.AddFile(3, kBig + 300 + i, kBig + 400 + i,
-                 InternalKey('foo', kBig + 500 + i, kTypeValue),
-                 InternalKey('zoo', kBig + 600 + i, kTypeDeletion));
-    edit.DeleteFile(4, kBig + 700 + i);
-    edit.SetCompactPointer(i, InternalKey('x', kBig + 900 + i, kTypeValue));
-  }
-    
-      bool empty() const {
-    return codeBlock.empty();
-  }
-    
-    
-    {  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-};
-    
-    
-    {  /**
-   * Execute a debugger action.
-   */
-  virtual String debuggerVerb(const std::string& /*verb*/,
-                              const std::vector<std::string>& /*args*/) {
-    return String();
-  }
-};
-    
-    #endif
-
-    
-    template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, F fillCols);
-template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
-    
-    namespace xgboost {
-/*!
- * \brief interface of linear updater
- */
-class LinearUpdater {
- public:
-  /*! \brief virtual destructor */
-  virtual ~LinearUpdater() = default;
-  /*!
-   * \brief Initialize the updater with given arguments.
-   * \param args arguments to the objective function.
-   */
-  virtual void Init(
-      const std::vector<std::pair<std::string, std::string> >& args) = 0;
-    }
-    }
-    
-      void BeforeFirst() override {
-    parser_->BeforeFirst();
-  }
-    
-    
+        template <typename T>
+    inline void ValidateType(const Dictionary& dict, const std::wstring& typeValue, size_t currentVersion)
     {
-    {void SparsePageWriter::Alloc(std::shared_ptr<SparsePage>* out_page) {
-  CHECK(*out_page == nullptr);
-  if (num_free_buffer_ != 0) {
-    out_page->reset(new SparsePage());
-    --num_free_buffer_;
-  } else {
-    CHECK(qrecycle_.Pop(out_page));
-  }
-}
-}  // namespace data
-}  // namespace xgboost
-    
-    
-    {
-    {}  // namespace obj
-}  // namespace xgboost
-    
-    
-    {
-    {}  // namespace common
-}  // namespace xgboost
-
-    
-    
-    {  /*! \brief push up temp */
-  inline void PushTemp() {
-    temp.Reserve(limit_size * 2);
-    for (size_t l = 1; true; ++l) {
-      this->InitLevel(l + 1);
-      // check if level l is empty
-      if (level[l].size == 0) {
-        level[l].SetPrune(temp, limit_size);
-        break;
-      } else {
-        // level 0 is actually temp space
-        level[0].SetPrune(temp, limit_size);
-        temp.SetCombine(level[0], level[l]);
-        if (temp.size > limit_size) {
-          // try next level
-          level[l].size = 0;
-        } else {
-          // if merged record is still smaller, no need to send to next level
-          level[l].CopyFrom(temp); break;
-        }
-      }
+        if (!dict.Contains(typeKey))
+        {
+            const auto& version = GetVersion(dict);
+            LogicError('Required key '%ls' is not found in the dictionary (%s).',
+                       typeKey.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
+        } 
     }
-  }
-  /*! \brief get the summary after finalize */
-  inline void GetSummary(SummaryContainer *out) {
-    if (level.size() != 0) {
-      out->Reserve(limit_size * 2);
-    } else {
-      out->Reserve(inqueue.queue.size());
-    }
-    inqueue.MakeSummary(out);
-    if (level.size() != 0) {
-      level[0].SetPrune(*out, limit_size);
-      for (size_t l = 1; l < level.size(); ++l) {
-        if (level[l].size == 0) continue;
-        if (level[0].size == 0) {
-          level[0].CopyFrom(level[l]);
-        } else {
-          out->SetCombine(level[0], level[l]);
-          level[0].SetPrune(*out, limit_size);
-        }
-      }
-      out->CopyFrom(level[0]);
-    } else {
-      if (out->size > limit_size) {
-        temp.Reserve(limit_size);
-        temp.SetPrune(*out, limit_size);
-        out->CopyFrom(temp);
-      }
-    }
-  }
-  // used for debug, check if the sketch is valid
-  inline void CheckValid(RType eps) const {
-    for (size_t l = 1; l < level.size(); ++l) {
-      level[l].CheckValid(eps);
-    }
-  }
-  // initialize level space to at least nlevel
-  inline void InitLevel(size_t nlevel) {
-    if (level.size() >= nlevel) return;
-    data.resize(limit_size * nlevel);
-    level.resize(nlevel, Summary(nullptr, 0));
-    for (size_t l = 0; l < level.size(); ++l) {
-      level[l].data = dmlc::BeginPtr(data) + l * limit_size;
-    }
-  }
-  // input data queue
-  typename Summary::Queue inqueue;
-  // number of levels
-  size_t nlevel;
-  // size of summary in each level
-  size_t limit_size;
-  // the level of each summaries
-  std::vector<Summary> level;
-  // content of the summary
-  std::vector<Entry> data;
-  // temporal summary, used for temp-merge
-  SummaryContainer temp;
-};
     
-    /*!
- * \brief Macro to register gradient booster.
- *
- * \code
- * // example of registering a objective ndcg@k
- * XGBOOST_REGISTER_GBM(GBTree, 'gbtree')
- * .describe('Boosting tree ensembles.')
- * .set_body([]() {
- *     return new GradientBooster<TStats>();
- *   });
- * \endcode
- */
-#define XGBOOST_REGISTER_GBM(UniqueId, Name)                            \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::GradientBoosterReg &          \
-  __make_ ## GradientBoosterReg ## _ ## UniqueId ## __ =                \
-      ::dmlc::Registry< ::xgboost::GradientBoosterReg>::Get()->__REGISTER__(Name)
+            static bool IsUDF(const Dictionary& dict);
     
-    float SimpleDMatrix::GetColDensity(size_t cidx) {
-  size_t column_size = 0;
-  // Use whatever version of column batches already exists
-  if (sorted_column_page_) {
-    auto batch = this->GetSortedColumnBatches();
-    column_size = (*batch.begin())[cidx].size();
-  } else {
-    auto batch = this->GetColumnBatches();
-    column_size = (*batch.begin())[cidx].size();
+            const NDShape& Shape() const override { return m_unpackedShape; }
+        DeviceDescriptor Device() const override { return m_isPacked ? m_packedData->Device() : Value::Device(); }
+        DataType GetDataType() const override { return m_isPacked ? m_packedData->GetDataType() : Value::GetDataType(); }
+        StorageFormat GetStorageFormat() const override { return m_isPacked? m_packedData->GetStorageFormat() : Value::GetStorageFormat(); }
+        bool IsReadOnly() const override { return m_isPacked ? m_packedData->IsReadOnly() : Value::IsReadOnly(); }
+    
+                // Validate that each of the dynamic axes are unique
+            std::unordered_set<Axis> uniqueDynamicAxis;
+            for (auto& currentDynamicAxis : dynamicAxes)
+            {
+                auto retVal = uniqueDynamicAxis.insert(currentDynamicAxis);
+                if (!retVal.second)
+                    InvalidArgument('Dynamic axis named %S is specified more than once for Variable '%S'', currentDynamicAxis.Name().c_str(), AsString().c_str());
+            }
+    
+    // -----------------------------------------------------------------------
+// InputValue (/*no input*/)
+// an input value (typically fed by a DataReader)
+// this covers two types: (regular vs. image)
+// TODO: There is still debate whether an InputValue without layout makes sense.
+// -----------------------------------------------------------------------
+    
+        size_t rank = runningAverage.GetShape().GetRank();
+    auto input = newInput.ValueTensorFor(rank, fr);
+    
+    // Return the source of the generated service file.
+grpc::string GenerateServiceSource(grpc_generator::File *file,
+                                   const grpc_generator::Service *service,
+                                   grpc_go_generator::Parameters *parameters);
+    
+      bool generate() {
+    std::string code;
+    code += std::string('// ') + FlatBuffersGeneratedWarning() +
+            '\n\ninclude \'flatbuffers.lobster\'\n\n';
+    for (auto it = parser_.enums_.vec.begin(); it != parser_.enums_.vec.end();
+         ++it) {
+      auto &enum_def = **it;
+      GenEnum(enum_def, &code);
+    }
+    for (auto it = parser_.structs_.vec.begin();
+         it != parser_.structs_.vec.end(); ++it) {
+      auto &struct_def = **it;
+      GenStructPreDecl(struct_def, &code);
+    }
+    for (auto it = parser_.structs_.vec.begin();
+         it != parser_.structs_.vec.end(); ++it) {
+      auto &struct_def = **it;
+      GenStruct(struct_def, &code);
+    }
+    return SaveFile((path_ + file_name_ + '_generated.lobster').c_str(),
+                    code, false);
   }
+    
+    
+#ifndef FLATBUFFERS_GENERATED_NAMESPACETEST2_NAMESPACEA_H_
+#define FLATBUFFERS_GENERATED_NAMESPACETEST2_NAMESPACEA_H_
+    
+    #include 'flatbuffers/idl.h'
+#include 'test_init.h'
+    
+    class ScalarReferenceResult {
+ private:
+  ScalarReferenceResult(const char *_type, RegexMatcher::MatchResult _matched)
+      : type(_type), matched(_matched) {}
     }
