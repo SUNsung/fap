eaf5a879edc92680158540ef4147cb3ed52a1855
SUNsung <weatherius@gmail.com>
2019-03-30 07:29:03 +0200

    Rewrite [50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py] File in: 2019-03-30 07:29:02

--- pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
+++ pyton/50aB2XmA8igI501Oofm.gOgRA50e2BwxDVIL4I50mtd.py
@@ -1,107 +1,250 @@
 
         
-            '''
-    is_windows = is_windows
-    config_dir = DEFAULT_CONFIG_DIR
-    stdin = sys.stdin
-    stdin_isatty = stdin.isatty()
-    stdin_encoding = None
-    stdout = sys.stdout
-    stdout_isatty = stdout.isatty()
-    stdout_encoding = None
-    stderr = sys.stderr
-    stderr_isatty = stderr.isatty()
-    colors = 256
-    if not is_windows:
-        if curses:
-            try:
-                curses.setupterm()
-                colors = curses.tigetnum('colors')
-            except curses.error:
-                pass
-    else:
-        # noinspection PyUnresolvedReferences
-        import colorama.initialise
-        stdout = colorama.initialise.wrap_stream(
-            stdout, convert=None, strip=None,
-            autoreset=True, wrap=True
-        )
-        stderr = colorama.initialise.wrap_stream(
-            stderr, convert=None, strip=None,
-            autoreset=True, wrap=True
-        )
-        del colorama
+            total_cost_ph = tf.placeholder(tf.float32, shape=[], name='total_cost_ph')
+    self.cost_t_summ = tf.summary.scalar('Total cost (train)', total_cost_ph,
+                                         collections=['train_summaries'])
+    self.cost_v_summ = tf.summary.scalar('Total cost (valid)', total_cost_ph,
+                                         collections=['valid_summaries'])
     
-        def load_installed_plugins(self):
-        for entry_point_name in ENTRY_POINT_NAMES:
-            for entry_point in iter_entry_points(entry_point_name):
-                plugin = entry_point.load()
-                plugin.package_name = entry_point.dist.key
-                self.register(entry_point.load())
+    from utils import write_datasets
+from synthetic_data_utils import normalize_rates
+from synthetic_data_utils import get_train_n_valid_inds, nparray_and_transpose
+from synthetic_data_utils import spikify_data, split_list_by_inds
+    
+      if identity_if_possible and in_size == out_size:
+    return (tf.constant(np.eye(in_size).astype(np.float32)),
+            tf.zeros(in_size))
+    
+          softmax = sess.run(t['softmax_out'],
+                         feed_dict={t['char_inputs_in']: char_ids_inputs,
+                                    t['inputs_in']: inputs,
+                                    t['targets_in']: targets,
+                                    t['target_weights_in']: weights})
+    
+      def _score_patches(self, word_patches):
+    '''Score a 2D matrix of word_patches and stitch results together.'''
+    batch_size, num_timesteps = self.shape
+    nrow, ncol = len(word_patches), len(word_patches[0])
+    max_len = num_timesteps * ncol
+    probs = np.zeros([0, max_len])  # accumulate results into this.
     
     
-def repr_dict_nice(d):
-    def prepare_dict(d):
-        for k, v in d.items():
-            if isinstance(v, dict):
-                v = dict(prepare_dict(v))
-            elif isinstance(v, bytes):
-                v = v.decode('utf8')
-            elif not isinstance(v, (int, str)):
-                v = repr(v)
-            yield k, v
-    return json.dumps(
-        dict(prepare_dict(d)),
-        indent=4, sort_keys=True,
-    )
+def print_and_log(log, id_to_word, sequence_eval, max_num_to_print=5):
+  '''Helper function for printing and logging evaluated sequences.'''
+  indices_eval = convert_to_indices(sequence_eval)
+  indices_arr = np.asarray(indices_eval)
+  samples = convert_to_human_readable(id_to_word, indices_arr, max_num_to_print)
+    
+      # The unique ngrams in the training set.
+  unique_ngrams_in_train = 0.
+    
+      # Common elements to Generator and Discriminator.
+  if not FLAGS.dis_share_embedding or model != 'dis':
+    embedding = [
+        v for v in tf.trainable_variables()
+        if v.op.name == str(model) + '/rnn/embedding'
+    ][0]
+  lstm_w_0 = [
+      v for v in tf.trainable_variables() if v.op.name == str(model) +
+      '/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel'
+  ][0]
+  lstm_b_0 = [
+      v for v in tf.trainable_variables() if v.op.name == str(model) +
+      '/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias'
+  ][0]
+  lstm_w_1 = [
+      v for v in tf.trainable_variables() if v.op.name == str(model) +
+      '/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel'
+  ][0]
+  lstm_b_1 = [
+      v for v in tf.trainable_variables() if v.op.name == str(model) +
+      '/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias'
+  ][0]
+    
+      with tf.variable_scope('dis', reuse=reuse):
+    cell_fwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
+        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
+    cell_bwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
+        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
+    if FLAGS.zoneout_drop_prob > 0.0:
+      cell_fwd = zoneout.ZoneoutWrapper(
+          cell_fwd,
+          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
+          is_training=is_training)
+      cell_bwd = zoneout.ZoneoutWrapper(
+          cell_bwd,
+          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
+          is_training=is_training)
+    
+    from flask import Flask
+    
+        Validates that the username is not already taken. Hashes the
+    password for security.
+    '''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
     
     
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    '''Delete a post.
     
-    ward = AgglomerativeClustering(n_clusters=3, linkage='ward')
+        assert 'closed' in str(e)
     
-    plt.matshow(np.outer(np.sort(model.row_labels_) + 1,
-                     np.sort(model.column_labels_) + 1),
-            cmap=plt.cm.Blues)
-plt.title('Checkerboard structure of rearranged data')
-    
-    Two consequences of imposing a connectivity can be seen. First clustering
-with a connectivity matrix is much faster.
-    
-    print(__doc__)
-from time import time
-    
-        # print(index)
-    # print(word_freq)
-    return index, word_freq
-    
-    from ...utils import get_shape, get_w
-    
-    
-def highway_dense(x, act_fn=relu, carry_bias=-1.0, name=None):
-    '''用于全连接层的 highway
-    Input shape:  [batch_size, n_input]
-    Output shape: [batch_size, n_input]
-    
-        def __init__(self, l1=0., l2=0.):
+        def before_request(self, f):
+        '''Like :meth:`Flask.before_request` but for a blueprint.  This function
+        is only executed before each request that is handled by a function of
+        that blueprint.
         '''
-        Args:
-            l1(float): L1 正则化的系数
-            l2(float): L2 正则化的系数
-        '''
-        self.l1 = np.asarray(l1, dtype=np.float32)
-        self.l2 = np.asarray(l2, dtype=np.float32)
+        self.record_once(lambda s: s.app.before_request_funcs
+            .setdefault(self.name, []).append(f))
+        return f
     
-        def __init__(self, unpacked, extra):
-        self.unpacked = unpacked
-        self.extra = extra
+    This typically means that you attempted to use functionality that needed
+an active HTTP request.  Consult the documentation on testing for
+information about how to avoid this problem.\
+'''
+_app_ctx_err_msg = '''\
+Working outside of application context.
+    
+                for key in http_method_funcs:
+                if hasattr(cls, key):
+                    methods.add(key.upper())
+    
+        monkeypatch.setattr(pkgutil, 'get_loader', get_loader)
+    
+    # List all EIP addresses for a VM.
+- ec2_eip_facts:
+    filters:
+       instance-id: i-123456789
+  register: my_vm_eips
+    
+            if state == 'absent' and user in heroku_collaborator_list:
+            if not module.check_mode:
+                heroku_app.remove_collaborator(user)
+            affected_apps += [app]
+            result_state = True
+        elif state == 'present' and user not in heroku_collaborator_list:
+            if not module.check_mode:
+                heroku_app.add_collaborator(user_id_or_email=user, silent=module.params['suppress_invitation'])
+            affected_apps += [app]
+            result_state = True
+    
+            for rule in rules:
+            firewall_rule = oneandone.client.FirewallPolicyRule(
+                protocol=rule['protocol'],
+                port_from=rule['port_from'],
+                port_to=rule['port_to'],
+                source=rule['source'])
+            firewall_rules.append(firewall_rule)
+    
+    DOCUMENTATION = '''
+---
+module: vca_fw
+short_description: add remove firewall rules in a gateway  in a vca
+description:
+  - Adds or removes firewall rules from a gateway in a vca environment
+version_added: '2.0'
+author:
+- Peter Sprygada (@privateip)
+options:
+    fw_rules:
+      description:
+        - A list of firewall rules to be added to the gateway, Please see examples on valid entries
+      required: True
+      default: false
+extends_documentation_fragment: vca.documentation
+'''
+    
+            if servicegroup is not None:
+            changed = client.modify_if_diff(name, ipa_hbacrule.get('memberservice_hbacsvcgroup', []),
+                                            servicegroup,
+                                            client.hbacrule_add_service,
+                                            client.hbacrule_remove_service, 'hbacsvcgroup') or changed
+    
+            if module.check_mode:
+            module.exit_json(changed=True)
+    
+            return ''
+    
+        xml = '''<?xml version='1.0' encoding='UTF-8'?>
+        <rs:model-request throttlesize='5'
+        xmlns:rs='http://www.ca.com/spectrum/restful/schema/request'
+        xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+        xsi:schemaLocation='http://www.ca.com/spectrum/restful/schema/request ../../../xsd/Request.xsd'>
+            <rs:target-models>
+            <rs:models-search>
+                <rs:search-criteria xmlns='http://www.ca.com/spectrum/restful/schema/filter'>
+                    <action-models>
+                        <filtered-models>
+                            <and>
+                                <equals>
+                                    <model-type>SearchManager</model-type>
+                                </equals>
+                                <greater-than>
+                                    <attribute id='0x129fa'>
+                                        <value>{mh_min}</value>
+                                    </attribute>
+                                </greater-than>
+                                <less-than>
+                                    <attribute id='0x129fa'>
+                                        <value>{mh_max}</value>
+                                    </attribute>
+                                </less-than>
+                            </and>
+                        </filtered-models>
+                        <action>FIND_DEV_MODELS_BY_IP</action>
+                        <attribute id='AttributeID.NETWORK_ADDRESS'>
+                            <value>{search_ip}</value>
+                        </attribute>
+                    </action-models>
+                </rs:search-criteria>
+            </rs:models-search>
+            </rs:target-models>
+            <rs:requested-attribute id='0x12d7f' /> <!--Network Address-->
+        </rs:model-request>
+        '''.format(search_ip=device_ip, mh_min=landscape_min, mh_max=landscape_max)
+    
+    CLI_DEFAULTS = dict(
+    server_root=server_root_tmp,
+    ctl='nginx',
+)
+'''CLI defaults.'''
+    
+        def setUp(self):
+        from acme.errors import MissingNonce
+        self.response = mock.MagicMock(headers={})
+        self.response.request.method = 'FOO'
+        self.error = MissingNonce(self.response)
     
     
-def parse_args(args=None):
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('version', type=str, help='Pandas version (0.23.0)')
-    return parser.parse_args(args)
+class Fixed(jose.Field):
+    '''Fixed field.'''
+    
+    
+class JWS(jose.JWS):
+    '''ACME-specific JWS. Includes none, url, and kid in protected header.'''
+    signature_cls = Signature
+    __slots__ = jose.JWS._orig_slots  # pylint: disable=no-member
+    
+    # If false, no index is generated.
+#html_use_index = True
+    
+            self.assertEqual(self.vhosts[5], self._call(self.vhosts))
+    
+        def test_same_server(self):
+        from certbot_apache.obj import VirtualHost
+        no_name1 = VirtualHost(
+            'fp', 'vhp', set([self.addr1]), False, False, None)
+        no_name2 = VirtualHost(
+            'fp', 'vhp', set([self.addr2]), False, False, None)
+        no_name3 = VirtualHost(
+            'fp', 'vhp', set([self.addr_default]),
+            False, False, None)
+        no_name4 = VirtualHost(
+            'fp', 'vhp', set([self.addr2, self.addr_default]),
+            False, False, None)
