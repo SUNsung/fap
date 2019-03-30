025e22391c08a0d377096085fdaa8e88b9b0f430
SUNsung <weatherius@gmail.com>
2019-03-30 12:40:02 +0200

    Rewrite [371lqYdo6Klw6375qX1wGV1lAo375iIRXuyLQDk37fUP.py] File in: 2019-03-30 12:40:01

--- pyton/371lqYdo6Klw6375qX1wGV1lAo375iIRXuyLQDk37fUP.py
+++ pyton/371lqYdo6Klw6375qX1wGV1lAo375iIRXuyLQDk37fUP.py
@@ -1,166 +1,120 @@
 
         
-            def notify_call_completed(self, call):
-        pass
+        
+class BadNonceTest(unittest.TestCase):
+    '''Tests for acme.errors.BadNonce.'''
     
-        HEART = 0
-    DIAMOND = 1
-    CLUBS = 2
-    SPADE = 3
+        # TODO: decoder should check that nonce is in the protected header
     
-        def reducer(self, key, values):
-        '''Sum values for each key.
+    OLD_REWRITE_HTTPS_ARGS = [
+    ['^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[L,QSA,R=permanent]'],
+    ['^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[END,QSA,R=permanent]']]
     
+            return all_names
     
-class HashTable(object):
+        def test_basic_ifdefine(self):
+        self.assertEqual(len(self.parser.find_dir('VAR_DIRECTIVE')), 2)
+        self.assertEqual(len(self.parser.find_dir('INVALID_VAR_DIRECTIVE')), 0)
     
-        def _targets_request(self, targets):
-        '''Formats each target for the request'''
-        targets_request = []
-        for target in targets:
-            target_request = {
-                'Id': target['id'],
-                'Arn': target['arn']
-            }
-            if 'input' in target:
-                target_request['Input'] = target['input']
-            if 'input_path' in target:
-                target_request['InputPath'] = target['input_path']
-            if 'role_arn' in target:
-                target_request['RoleArn'] = target['role_arn']
-            if 'ecs_parameters' in target:
-                target_request['EcsParameters'] = {}
-                ecs_parameters = target['ecs_parameters']
-                if 'task_definition_arn' in target['ecs_parameters']:
-                    target_request['EcsParameters']['TaskDefinitionArn'] = ecs_parameters['task_definition_arn']
-                if 'task_count' in target['ecs_parameters']:
-                    target_request['EcsParameters']['TaskCount'] = ecs_parameters['task_count']
-            targets_request.append(target_request)
-        return targets_request
+    class EntryPointTest(unittest.TestCase):
+    '''Entrypoint tests'''
     
-    try:
-    from ast import literal_eval
-    HAS_PYTHON26 = True
-except ImportError:
-    HAS_PYTHON26 = False
+        def load_config(self):
+        '''Loads the next configuration for the plugin to test'''
+        config = super(Proxy, self).load_config()
+        self._all_names, self._test_names = _get_names(config)
     
-            for rule in rules:
-            firewall_rule = oneandone.client.FirewallPolicyRule(
-                protocol=rule['protocol'],
-                port_from=rule['port_from'],
-                port_to=rule['port_to'],
-                source=rule['source'])
-            firewall_rules.append(firewall_rule)
+      files_belong_to_same_module = filename_cc.endswith(filename_h)
+  common_path = ''
+  if files_belong_to_same_module:
+    common_path = filename_cc[:-len(filename_h)]
+  return files_belong_to_same_module, common_path
+    
+    # All the missing Fares -> assume median of their respective class
+if len(test_df.Fare[ test_df.Fare.isnull() ]) > 0:
+    median_fare = np.zeros(3)
+    for f in range(0,3):                                              # loop 0 to 2
+        median_fare[f] = test_df[ test_df.Pclass == f+1 ]['Fare'].dropna().median()
+    for f in range(0,3):                                              # loop 0 to 2
+        test_df.loc[ (test_df.Fare.isnull()) & (test_df.Pclass == f+1 ), 'Fare'] = median_fare[f]
     
     
-def present(configuration_facts, cursor, parameter_name, current_value):
-    parameter_key = parameter_name.lower()
-    changed = False
-    if current_value and current_value.lower() != configuration_facts[parameter_key]['current_value'].lower():
-        cursor.execute('select set_config_parameter('{0}', '{1}')'.format(parameter_name, current_value))
-        changed = True
-    if changed:
-        configuration_facts.update(get_configuration_facts(cursor, parameter_name))
-    return changed
+def batch_iter(data, batch_size, num_epochs):
+    '''
+    Generates a batch iterator for a dataset.
+    '''
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_data = data[shuffle_indices]
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
     
-        try:
-        dsn = (
-            'Driver=Vertica;'
-            'Server=%s;'
-            'Port=%s;'
-            'Database=%s;'
-            'User=%s;'
-            'Password=%s;'
-            'ConnectionLoadBalance=%s'
-        ) % (module.params['cluster'], module.params['port'], db,
-             module.params['login_user'], module.params['login_password'], 'true')
-        db_conn = pyodbc.connect(dsn, autocommit=True)
-        cursor = db_conn.cursor()
-    except Exception as e:
-        module.fail_json(msg='Unable to connect to database: %s.' % to_native(e), exception=traceback.format_exc())
+        x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
     
-        try:
-        schema_facts = get_schema_facts(cursor)
-        if module.check_mode:
-            changed = not check(schema_facts, schema, usage_roles, create_roles, owner)
-        elif state == 'absent':
-            try:
-                changed = absent(schema_facts, cursor, schema, usage_roles, create_roles)
-            except pyodbc.Error as e:
-                module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-        elif state == 'present':
-            try:
-                changed = present(schema_facts, cursor, schema, usage_roles, create_roles, owner)
-            except pyodbc.Error as e:
-                module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-    except NotSupportedError as e:
-        module.fail_json(msg=to_native(e), ansible_facts={'vertica_schemas': schema_facts})
-    except CannotDropError as e:
-        module.fail_json(msg=to_native(e), ansible_facts={'vertica_schemas': schema_facts})
-    except SystemExit:
-        # avoid catching this on python 2.4
-        raise
-    except Exception as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-    
-    # Copyright (c) 2016, Loic Blot <loic.blot@unix-experience.fr>
-# Copyright (c) 2018, Ansible Project
-# Sponsored by Infopro Digital. http://www.infopro-digital.com/
-# Sponsored by E.T.A.I. http://www.etai.fr/
-#
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-    
-    import time
-import re
-    
-    
-def best_server(probe_nat=False):
-    best_server = None
-    prober = new_pteredor(probe_nat=probe_nat)
-    prober.qualified = True
-    if not probe_nat:
-        prober.nat_type = 'unknown'
-        prober.rs_cone_flag = 0
-    
-    
-def gae_application(environ, start_response):
-    cookie = environ.get('HTTP_COOKIE', '')
-    options = environ.get('HTTP_X_GOA_OPTIONS', '')
-    if environ['REQUEST_METHOD'] == 'GET' and not cookie:
-        if '204' in environ['QUERY_STRING']:
-            start_response('204 No Content', [])
-            yield ''
+        # load weights
+    if weights == 'imagenet':
+        print('K.image_dim_ordering:', K.image_dim_ordering())
+        if K.image_dim_ordering() == 'th':
+            if include_top:
+                weights_path = get_file('vgg19_weights_th_dim_ordering_th_kernels.h5',
+                                        TH_WEIGHTS_PATH,
+                                        cache_subdir='models')
+            else:
+                weights_path = get_file('vgg19_weights_th_dim_ordering_th_kernels_notop.h5',
+                                        TH_WEIGHTS_PATH_NO_TOP,
+                                        cache_subdir='models')
+            model.load_weights(weights_path)
+            if K.backend() == 'tensorflow':
+                warnings.warn('You are using the TensorFlow backend, yet you '
+                              'are using the Theano '
+                              'image dimension ordering convention '
+                              '(`image_dim_ordering='th'`). '
+                              'For best performance, set '
+                              '`image_dim_ordering='tf'` in '
+                              'your Keras config '
+                              'at ~/.keras/keras.json.')
+                convert_all_kernels_in_model(model)
         else:
-            timestamp = long(os.environ['CURRENT_VERSION_ID'].split('.')[1])/2**28
-            ctime = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(timestamp+8*3600))
-            html = u'GoAgent Python Server %s \u5df2\u7ecf\u5728\u5de5\u4f5c\u4e86\uff0c\u90e8\u7f72\u65f6\u95f4 %s\n' % (__version__, ctime)
-            start_response('200 OK', [('Content-Type', 'text/plain; charset=utf-8')])
-            yield html.encode('utf8')
-        raise StopIteration
+            if include_top:
+                weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels.h5',
+                                        TF_WEIGHTS_PATH,
+                                        cache_subdir='models')
+            else:
+                weights_path = get_file('vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                                        TF_WEIGHTS_PATH_NO_TOP,
+                                        cache_subdir='models')
+            model.load_weights(weights_path)
+            if K.backend() == 'theano':
+                convert_all_kernels_in_model(model)
+    return model
     
-            raise NotImplementedError
+        # The expression below can be re-written in a more C style as
+    # follows :
+    #
+    # out_shape    = [0,0]
+    # out_shape[0] = (img_shape[0]+tile_spacing[0])*tile_shape[0] -
+    #                tile_spacing[0]
+    # out_shape[1] = (img_shape[1]+tile_spacing[1])*tile_shape[1] -
+    #                tile_spacing[1]
+    out_shape = [
+        (ishp + tsp) * tshp - tsp
+        for ishp, tshp, tsp in zip(img_shape, tile_shape, tile_spacing)
+    ]
     
-    def setCharPositionInLine(self, pos):
-        '''@brief Set the column of the tokens first character,
+    import numpy
+import theano
+from theano import config
+import theano.tensor as T
+from theano.tensor.nnet import categorical_crossentropy
     
-    For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/browser/
-'''
-import voluptuous as vol
-    
-            now = dt_util.now()
-        actiontec_data = self.get_actiontec_data()
-        if not actiontec_data:
-            return False
-        self.last_results = [Device(data['mac'], name, now)
-                             for name, data in actiontec_data.items()
-                             if data['timevalid'] > -60]
-        _LOGGER.info('Scan successful')
-        return True
-    
-            self.success_init = self._update_info()
-        _LOGGER.info('Scanner initialized')
-    
-            options = self._options
-    
-            return web.Response(text=resp_text, content_type='text/xml')
+    # Generate the components of the polynomial
+full_range = T.arange(max_coefficients_supported)
+outputs_info = np.zeros((), dtype=theano.config.floatX)
