18d796cbf446b28769492ef521049f5125a8f7bb
SUNsung <weatherius@gmail.com>
2019-04-01 01:34:00 +0300

    Rewrite [48JnRaiYoVeug48XkHHtnagcGM48SB9K0j0.boI48MJz.cpp] File in: 2019-04-01 01:33:59

--- cccccc/48JnRaiYoVeug48XkHHtnagcGM48SB9K0j0.boI48MJz.cpp
+++ cccccc/48JnRaiYoVeug48XkHHtnagcGM48SB9K0j0.boI48MJz.cpp
@@ -1,153 +1,158 @@
 
         
-        #include <dmlc/omp.h>
-#include <xgboost/logging.h>
-#include <algorithm>
-#include '../common/math.h'
-    
-    namespace xgboost {
-namespace common {
-TEST(CompressedIterator, Test) {
-  ASSERT_TRUE(detail::SymbolBits(256) == 8);
-  ASSERT_TRUE(detail::SymbolBits(150) == 8);
-  std::vector<int> test_cases = {1, 3, 426, 21, 64, 256, 100000, INT32_MAX};
-  int num_elements = 1000;
-  int repetitions = 1000;
-  srand(9);
+          caffe::Datum datum;
+  datum.set_channels(2);  // one channel for each image in the pair
+  datum.set_height(rows);
+  datum.set_width(cols);
+  LOG(INFO) << 'A total of ' << num_items << ' items.';
+  LOG(INFO) << 'Rows: ' << rows << ' Cols: ' << cols;
+  for (int itemid = 0; itemid < num_items; ++itemid) {
+    int i = caffe::caffe_rng_rand() % num_items;  // pick a random  pair
+    int j = caffe::caffe_rng_rand() % num_items;
+    read_image(&image_file, &label_file, i, rows, cols,
+        pixels, &label_i);
+    read_image(&image_file, &label_file, j, rows, cols,
+        pixels + (rows * cols), &label_j);
+    datum.set_data(pixels, 2*rows*cols);
+    if (label_i  == label_j) {
+      datum.set_label(1);
+    } else {
+      datum.set_label(0);
     }
-    }
-    }
-    
-    SEXP XGBoosterDumpModel_R(SEXP handle, SEXP fmap, SEXP with_stats, SEXP dump_format) {
-  SEXP out;
-  R_API_BEGIN();
-  bst_ulong olen;
-  const char **res;
-  const char *fmt = CHAR(asChar(dump_format));
-  CHECK_CALL(XGBoosterDumpModelEx(R_ExternalPtrAddr(handle),
-                                CHAR(asChar(fmap)),
-                                asInteger(with_stats),
-                                fmt,
-                                &olen, &res));
-  out = PROTECT(allocVector(STRSXP, olen));
-  if (!strcmp('json', fmt)) {
-    std::stringstream stream;
-    stream <<  '[\n';
-    for (size_t i = 0; i < olen; ++i) {
-      stream << res[i];
-      if (i < olen - 1) {
-        stream << ',\n';
-      } else {
-        stream << '\n';
-      }
-    }
-    stream <<  ']';
-    SET_STRING_ELT(out, 0, mkChar(stream.str().c_str()));
-  } else {
-    for (size_t i = 0; i < olen; ++i) {
-      std::stringstream stream;
-      stream <<  'booster[' << i <<']\n' << res[i];
-      SET_STRING_ELT(out, i, mkChar(stream.str().c_str()));
-    }
+    datum.SerializeToString(&value);
+    std::string key_str = caffe::format_int(itemid, 8);
+    db->Put(leveldb::WriteOptions(), key_str, value);
   }
-  R_API_END();
-  UNPROTECT(1);
-  return out;
-}
     
-      bool Read(SparsePage* page, dmlc::SeekStream* fi) override {
-    auto& offset_vec = page->offset.HostVector();
-    auto& data_vec = page->data.HostVector();
-    if (!fi->Read(&(offset_vec))) return false;
-    CHECK_NE(offset_vec.size(), 0) << 'Invalid SparsePage file';
-    this->LoadIndexValue(fi);
-    }
-    
-      oprot->writeMessageBegin('getQueryColumns', ::apache::thrift::protocol::T_REPLY, seqid);
-  result.write(oprot);
-  oprot->writeMessageEnd();
-  bytes = oprot->getTransport()->writeEnd();
-  oprot->getTransport()->flush();
-    
-    using namespace ::apache::thrift;
-using namespace ::apache::thrift::protocol;
-using namespace ::apache::thrift::transport;
-using namespace ::apache::thrift::server;
-    
-    namespace osquery { namespace extensions {
-    }
-    }
-    
-      xfer += iprot->readStructEnd();
+    /*!
+@brief Fills a Blob with coefficients for bilinear interpolation.
     
     /**
- * @brief Queries SMART devices on the system by autodetection and explicit
- * storage controller arguments.
+ * @brief Computes @f$ y = |x| @f$
  *
- * @param client libsmartctl client
- * @param walk_func function that walks the system devices and runs the handler
- * function on each device
- * @param results reference to QueryData to store results in
+ * @param bottom input Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the inputs @f$ x @f$
+ * @param top output Blob vector (length 1)
+ *   -# @f$ (N \times C \times H \times W) @f$
+ *      the computed outputs @f$ y = |x| @f$
  */
-void querySmartDevices(
-    libsmartctl::ClientInterface& client,
-    std::function<void(
-        std::function<void(const std::string&, hardwareDriver*)>)> walk_func,
-    QueryData& results);
-    
-    /**
- * @brief Intel HECI GUID for the update application.
- *
- * This GUID can be used on every OS over the HECI protocol to inspect the
- * FW version.
- */
-const std::vector<uint8_t> kMEIUpdateGUID{
-    232, 205, 157, 48, 177, 204, 98, 64, 143, 120, 96, 1, 21, 163, 67, 39,
-};
-    
-    Expected<int, PosixError> syscall(struct perf_event_attr* attr,
-                                  pid_t pid,
-                                  int cpu,
-                                  int group_fd,
-                                  unsigned long const flags);
-    
-    #include <gtest/gtest.h>
-    
-    Expected<EbpfTracepoint, LinuxProbesControl::Error> createTracepointForSyscall(
-    syscall::EventType type, PerfEventCpuMap const& cpu_map) {
-  auto program_exp = genLinuxProgram(BPF_PROG_TYPE_TRACEPOINT, cpu_map, type);
-  if (program_exp.isError()) {
-    return createError(LinuxProbesControl::Error::SystemEbpf,
-                       'could not load program to track syscall ',
-                       program_exp.takeError())
-           << to<std::string>(type);
-  }
-  auto tracing_path_exp = toTracingPath(type);
-  if (tracing_path_exp.isError()) {
-    return createError(LinuxProbesControl::Error::InvalidArgument,
-                       '',
-                       tracing_path_exp.takeError());
-  }
-  auto sys_event_exp = tracing::NativeEvent::load(tracing_path_exp.take());
-  if (sys_event_exp.isError()) {
-    return createError(LinuxProbesControl::Error::SystemNativeEvent,
-                       'could not enable linux event for ',
-                       sys_event_exp.takeError())
-           << to<std::string>(type);
-  }
-  auto tracepoint_exp =
-      events::EbpfTracepoint::load(sys_event_exp.take(), program_exp.take());
-  if (tracepoint_exp.isError()) {
-    return createError(
-               LinuxProbesControl::Error::SystemTracepoint,
-               'could not attach tracing prograp to the native event of ',
-               tracepoint_exp.takeError())
-           << to<std::string>(type);
-  }
-  return tracepoint_exp.take();
-}
-    
-    class EnterExitJoiner {
+template <typename Dtype>
+class AbsValLayer : public NeuronLayer<Dtype> {
  public:
-  boost::optional<Event> join(Event event);
+  explicit AbsValLayer(const LayerParameter& param)
+      : NeuronLayer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    /**
+ * @brief Compute the index of the @f$ K @f$ max values for each datum across
+ *        all dimensions @f$ (C \times H \times W) @f$.
+ *
+ * Intended for use after a classification layer to produce a prediction.
+ * If parameter out_max_val is set to true, output is a vector of pairs
+ * (max_ind, max_val) for each image. The axis parameter specifies an axis
+ * along which to maximise.
+ *
+ * NOTE: does not implement Backwards operation.
+ */
+template <typename Dtype>
+class ArgMaxLayer : public Layer<Dtype> {
+ public:
+  /**
+   * @param param provides ArgMaxParameter argmax_param,
+   *     with ArgMaxLayer options:
+   *   - top_k (\b optional uint, default 1).
+   *     the number @f$ K @f$ of maximal items to output.
+   *   - out_max_val (\b optional bool, default false).
+   *     if set, output a vector of pairs (max_ind, max_val) unless axis is set then
+   *     output max_val along the specified axis.
+   *   - axis (\b optional int).
+   *     if set, maximise along the specified axis else maximise the flattened
+   *     trailing dimensions for each index of the first / num dimension.
+   */
+  explicit ArgMaxLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    template <typename Dtype>
+class BasePrefetchingDataLayer :
+    public BaseDataLayer<Dtype>, public InternalThread {
+ public:
+  explicit BasePrefetchingDataLayer(const LayerParameter& param);
+  // LayerSetUp: implements common data layer setup functionality, and calls
+  // DataLayerSetUp to do special data layer setup for individual layer types.
+  // This method may not be overridden.
+  void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    namespace caffe {
+    }
+    
+    namespace caffe {
+    }
+    
+     protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    
+    {}  // namespace caffe
+    
+    
+    {
+    {
+    {            std::shared_ptr<std::vector<UnitConversionManager::Category>> m_categoryList;
+            std::shared_ptr<UnitConversionManager::CategoryToUnitVectorMap> m_categoryToUnits;
+            std::shared_ptr<UnitConversionManager::UnitToUnitToConversionDataMap> m_ratioMap;
+            Platform::String^ m_currentRegionCode;
+        };
+    }
+}
+
+    
+    void HistoryList::HistoryList_Loaded(Object^ sender, RoutedEventArgs^ e)
+{
+    // When transitioning between docked and undocked view states, the history list is
+    // unloaded and then loaded, so we attempt to create the titlebarhelper every time
+    // we are loaded, letting the util function check if we are docked or not.
+    m_titleBarHelper = TitleBarHelper::CreateTitleBarHelperIfNotDocked(CustomTitleBar);
+}
+    
+    
+    {
+    {        e->Handled = true;
+    }
+}
+    
+    namespace CalculatorApp
+{
+    [Windows::Foundation::Metadata::WebHostHidden]
+    public ref class Memory sealed
+    {
+    public:
+        Memory();
+        property CalculatorApp::ViewModel::StandardCalculatorViewModel^ Model
+        {
+            CalculatorApp::ViewModel::StandardCalculatorViewModel^ get() {
+                return static_cast<CalculatorApp::ViewModel::StandardCalculatorViewModel^>(this->DataContext);
+            }
+        }
+    }
     }
