7ec2e4424d1071a6bdf7f965cc70076579a44485
SUNsung <weatherius@gmail.com>
2019-03-31 21:40:30 +0300

    Rewrite [26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp] File in: 2019-03-31 21:40:30

--- cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
+++ cccccc/26ywK4izpTCX226pXO1xXcx2BU26i7DhOQKBIVc265aY.cpp
@@ -1,115 +1,123 @@
 
         
-        #undef BODY_ARG
-#undef BODY_NONE
+          int num_words;
+  TBOX lword_box;     // in normalized (horiz text rows) space
+  TBOX rword_box;     // in normalized (horiz text rows) space
     
-    struct APCCollection {
-  static APCHandle::Pair Make(const ObjectData*,
-                              APCHandleLevel level,
-                              bool unserializeObj);
-  static void Delete(APCHandle*);
+      // Deletes the box with the given index, and shuffles up the rest.
+  // Recomputes the bounding box.
+  void DeleteBox(int index);
+    
+    namespace tesseract {
     }
     
-      static void ParseIniFile(const std::string &filename,
-                           const bool is_system = true);
-  static void ParseIniFile(const std::string &filename, IniSettingMap &ini,
-                           const bool constants_only = false,
-                           const bool is_system = true);
+    template <typename T> class GenericVector;
     
-    template <typename F>
-void find(const std::string &root, const std::string& path, bool php,
-          const F& callback) {
-  auto spath = path.empty() || !isDirSeparator(path[0]) ?
-    path : path.substr(1);
-    }
+    ELISTIZE(PARA)
     
-    ///////////////////////////////////////////////////////////////////////////////
-    
-    #include <folly/String.h>
-    
-    DHTReplaceNodeTask::~DHTReplaceNodeTask() = default;
-    
-    
-    {  void setTaskFactory(DHTTaskFactory* taskFactory);
-};
-    
-    namespace aria2 {
-    }
-    
-    void DHTRoutingTableSerializer::setLocalNode(
-    const std::shared_ptr<DHTNode>& localNode)
-{
-  localNode_ = localNode;
-}
-    
-    class DHTTaskFactory {
-public:
-  virtual ~DHTTaskFactory() = default;
-    }
-    
-    DHTTaskFactoryImpl::DHTTaskFactoryImpl()
-    : routingTable_(nullptr),
-      dispatcher_(nullptr),
-      factory_(nullptr),
-      taskQueue_(nullptr),
-      timeout_(DHT_MESSAGE_TIMEOUT)
-{
-}
-    
-      virtual std::shared_ptr<DHTTask> createPeerLookupTask(
-      const std::shared_ptr<DownloadContext>& ctx, uint16_t tcpPort,
-      const std::shared_ptr<PeerStorage>& peerStorage) CXX11_OVERRIDE;
-    
-    namespace {
-const size_t NUM_CONCURRENT_TASK = 15;
-} // namespace
-    
-    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
-                                           const std::string& ipaddr,
-                                           uint16_t port,
-                                           const unsigned char* secret) const
-{
-  unsigned char src[DHT_ID_LENGTH + COMPACT_LEN_IPV6 + SECRET_SIZE];
-  memset(src, 0, sizeof(src));
-  int compactlen = bittorrent::packcompact(src + DHT_ID_LENGTH, ipaddr, port);
-  if (compactlen == 0) {
-    throw DL_ABORT_EX(fmt('Token generation failed: ipaddr=%s, port=%u',
-                          ipaddr.c_str(), port));
+      // Simple accessors.
+  bool empty() const {
+    return heap_.empty();
   }
-  memcpy(src, infoHash, DHT_ID_LENGTH);
-  memcpy(src + DHT_ID_LENGTH + COMPACT_LEN_IPV6, secret, SECRET_SIZE);
-  unsigned char md[20];
-  message_digest::digest(md, sizeof(md), MessageDigest::sha1().get(), src,
-                         sizeof(src));
-  return std::string(&md[0], &md[sizeof(md)]);
+  int size() const {
+    return heap_.size();
+  }
+  int size_reserved() const {
+    return heap_.size_reserved();
+  }
+  void clear() {
+    // Clear truncates to 0 to keep the number reserved in tact.
+    heap_.truncate(0);
+  }
+  // Provides access to the underlying vector.
+  // Caution! any changes that modify the keys will invalidate the heap!
+  GenericVector<Pair>* heap() {
+    return &heap_;
+  }
+  // Provides read-only access to an element of the underlying vector.
+  const Pair& get(int index) const {
+    return heap_[index];
+  }
+    
+        //
+    // Override
+    //
+    virtual Speed* clone() const override;
+    virtual Speed* reverse() const override;
+    virtual void startWithTarget(Node* target) override;
+    virtual void stop() override;
+    /**
+     * @param dt in seconds.
+     */
+    virtual void step(float dt) override;
+    /** Return true if the action has finished.
+     *
+     * @return Is true if the action has finished.
+     */
+    virtual bool isDone() const  override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    Speed();
+    virtual ~Speed(void);
+    /** Initializes the action. */
+    bool initWithAction(ActionInterval *action, float speed);
+    
+    ssize_t PointArray::count() const
+{
+    return _controlPoints.size();
 }
     
-    public:
-  DNSCache();
-  DNSCache(const DNSCache& c);
-  ~DNSCache();
-    
-    
-    {    static BOOST_FORCEINLINE storage_type fetch_xor(storage_type volatile& storage, storage_type v, memory_order order) BOOST_NOEXCEPT
-    {
-        switch (order)
-        {
-        case memory_order_relaxed:
-            v = static_cast< storage_type >(BOOST_ATOMIC_INTERLOCKED_XOR8_RELAXED(&storage, v));
-            break;
-        case memory_order_consume:
-        case memory_order_acquire:
-            v = static_cast< storage_type >(BOOST_ATOMIC_INTERLOCKED_XOR8_ACQUIRE(&storage, v));
-            break;
-        case memory_order_release:
-            v = static_cast< storage_type >(BOOST_ATOMIC_INTERLOCKED_XOR8_RELEASE(&storage, v));
-            break;
-        case memory_order_acq_rel:
-        case memory_order_seq_cst:
-        default:
-            v = static_cast< storage_type >(BOOST_ATOMIC_INTERLOCKED_XOR8(&storage, v));
-            break;
-        }
-        return v;
+    CallFuncN * CallFuncN::clone() const
+{
+    // no copy constructor
+    auto a = new (std::nothrow) CallFuncN();
     }
+    
+        /** Removes an action given its tag and the target.
+     *
+     * @param tag       The action's tag.
+     * @param target    A certain target.
+     */
+    virtual void removeActionByTag(int tag, Node *target);
+    
+    /** Removes all actions given its tag and the target.
+     *
+     * @param tag       The actions' tag.
+     * @param target    A certain target.
+     * @js NA
+     */
+    virtual void removeAllActionsByTag(int tag, Node *target);
+    
+    AnimationCache* AnimationCache::s_sharedAnimationCache = nullptr;
+    
+        /** Initializes an AtlasNode  with an Atlas file the width and height of each item and the quantity of items to render*/
+    bool initWithTileFile(const std::string& tile, int tileWidth, int tileHeight, int itemsToRender);
+    
+    /** Initializes an AtlasNode  with a texture the width and height of each item measured in points and the quantity of items to render*/
+    bool initWithTexture(Texture2D* texture, int tileWidth, int tileHeight, int itemsToRender);
+    
+      x <<= 4;
+  x |= t;
+    
+    #include 'modules/drivers/canbus/common/byte.h'
+#include 'modules/drivers/canbus/common/canbus_consts.h'
+    
+    void Spline1dSeg::SetParams(const std::vector<double>& params) {
+  SetSplineFunc(PolynomialXd(params));
+}
+    
+    Eigen::MatrixXd SplineSegKernel::Kernel(const uint32_t num_params,
+                                        const double accumulated_x) {
+  if (num_params > reserved_order_ + 1) {
+    CalculateFx(num_params);
+  }
+  Eigen::MatrixXd term_matrix;
+  IntegratedTermMatrix(num_params, accumulated_x, 'fx', &term_matrix);
+  return kernel_fx_.block(0, 0, num_params, num_params)
+      .cwiseProduct(term_matrix);
+}
+    
+    class Accelrpt68Test : public ::testing::Test {
+ public:
+  virtual void SetUp() {}
 };
