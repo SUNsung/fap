c893bea37e1ca79f5371a0798115970ad4706d15
SUNsung <weatherius@gmail.com>
2019-03-31 13:58:54 +0300

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2019-03-31 13:58:53

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,86 +1,131 @@
 
         
-            it 'creates a scenario label with the given text' do
-      expect(scenario_label(scenario, 'Other')).to eq(
-        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
-      )
+            it 'opens the dry run modal even when clicking on the refresh icon' do
+      visit edit_agent_path(agent)
+      find('.agent-dry-run-button span.glyphicon').click
+      expect(page).to have_text('Event to send (Optional)')
+    end
+    
+              @bar2 = Agents::DotBar.new(name: 'bar2').tap { |agent|
+            agent.user = users(:bob)
+            agent.sources << @foo
+            agent.propagate_immediately = true
+            agent.disabled = true
+            agent.save!
+          },
+    
+          describe '#generate_diff' do
+        it 'should check if the agent requires a service' do
+          agent_diffs = services_scenario_import.agent_diffs
+          basecamp_agent_diff = agent_diffs[0]
+          expect(basecamp_agent_diff.requires_service?).to eq(true)
+        end
+    
+          it 'runs until stop is called' do
+        mock.instance_of(Rufus::Scheduler).join
+        Thread.new { while @agent_runner.instance_variable_get(:@running) != false do sleep 0.1; @agent_runner.stop end }
+        @agent_runner.run
+      end
+    
+      describe 'up' do
+    it 'should update extract and template options for an existing WebsiteAgent' do
+      expect(agent.options).to include('extract' => old_extract,
+                                       'template' => old_template)
+      ConvertWebsiteAgentTemplateForMerge.new.up
+      agent.reload
+      expect(agent.options).to include('extract' => new_extract,
+                                       'template' => new_template)
     end
   end
     
-        it 'is turned off for existing instances of Huginn' do
-      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
-      DefaultScenarioImporter.import(user)
+      it 'replaces invalid byte sequences in a message' do
+    log = AgentLog.new(:agent => agents(:jane_website_agent), level: 3)
+    log.message = '\u{3042}\xffA\x95'
+    expect { log.save! }.not_to raise_error
+    expect(log.message).to eq('\u{3042}<ff>A\<95>')
+  end
+    
+        it 'should provide the since attribute after the first run' do
+      time = (Time.now-1.minute).iso8601
+      @checker.memory[:last_event] = time
+      @checker.save
+      expect(@checker.reload.send(:query_parameters)).to eq({:query => {:since => time}})
     end
+  end
     
-          it 'does not load workers specified in the :except option' do
-        agent_runner = AgentRunner.new(except: HuginnScheduler)
-        workers = agent_runner.send(:load_workers)
-        expect(workers.keys).to eq(['DelayedJobWorker'])
-    
-          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent)).to eq([@agent1])
+        it 'should raise error when response has an error' do
+      stub(HTTParty).post { {'error' => {'message' => 'Sample error'}} }
+      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Sample error/)
     end
   end
 end
 
     
-      describe '#sort_tuples!' do
-    let(:tuples) {
-      time = Time.now
-      [
-        [2, 'a', time - 1],  # 0
-        [2, 'b', time - 1],  # 1
-        [1, 'b', time - 1],  # 2
-        [1, 'b', time],      # 3
-        [1, 'a', time],      # 4
-        [2, 'a', time + 1],  # 5
-        [2, 'a', time],      # 6
-      ]
-    }
+            return a.casecmp(b) if a_length == 1 && b_length == 1
+        return 1 if a_length == 1
+        return -1 if b_length == 1
     
-    describe ConvertWebsiteAgentTemplateForMerge do
-  let :old_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
-    }
+        def push(*names)
+      @filters.push *filter_const(names)
+    end
+    
+            def destroy
+          authorize! :destroy, @product
+          @product.destroy
+          respond_with(@product, status: 204)
+        end
+    
+            private
+    
+      config.vm.define :smartos do |smartos|
+    smartos.vm.box = 'smartos-base1310-64-virtualbox-20130806.box'
+    smartos.vm.box_url = 'http://dlc-int.openindiana.org/aszeszo/vagrant/smartos-base1310-64-virtualbox-20130806.box'
   end
     
-      # if rss_url already in existing opml file, use that; otherwise, do a lookup
-  rss_url = nil
-  if File.exist?(OUTPUT_FILENAME)
-    xml = Nokogiri::XML(File.open(OUTPUT_FILENAME))
-    existing_blog = xml.xpath('//outline[@htmlUrl='#{web_url}']').first
-    if existing_blog
-      rss_url = existing_blog.attr('xmlUrl')
-      puts '#{name}: ALREADY HAVE'
+          # Load the script into memory.
+      input.scripts[scriptname] = File.read(path)
     end
+    
+        pkgdata = {
+      'arch' => architecture,
+      'name' => name,
+      'version' => pkg_version,
+      'comment' => description,
+      'desc' => description,
+      'origin' => pkg_origin,
+      'maintainer' => maintainer,
+      'www' => url,
+      # prefix is required, but it doesn't seem to matter
+      'prefix' => '/',
+    }
+    
+      def set_default_prefix
+    attributes[:prefix] = self.class.default_prefix
+    attributes[:prefix_given?] = true
   end
     
-        def log_processing(name)
-      puts yellow '  #{File.basename(name)}'
-    end
+    # Use an OS X pkg built with pkgbuild.
+#
+# Supports input and output. Requires pkgbuild and (for input) pkgutil, part of a
+# standard OS X install in 10.7 and higher.
+class FPM::Package::OSXpkg < FPM::Package
     
-        def setup_filters
-      @filters = cmdline_filters
-      @filters += @custom_filters if @custom_filters
-      @filters << Filter.new(:role, ENV['ROLES']) if ENV['ROLES']
-      @filters << Filter.new(:host, ENV['HOSTS']) if ENV['HOSTS']
-      fh = fetch_for(:filter, {}) || {}
-      @filters << Filter.new(:host, fh[:hosts]) if fh[:hosts]
-      @filters << Filter.new(:role, fh[:roles]) if fh[:roles]
-      @filters << Filter.new(:host, fh[:host]) if fh[:host]
-      @filters << Filter.new(:role, fh[:role]) if fh[:role]
-    end
+        # Some setup.py's assume $PWD == current directory of setup.py, so let's
+    # chdir first.
+    ::Dir.chdir(project_dir) do
+      flags = [ '--root', staging_path ]
+      if !attributes[:python_install_lib].nil?
+        flags += [ '--install-lib', File.join(prefix, attributes[:python_install_lib]) ]
+      elsif !attributes[:prefix].nil?
+        # setup.py install --prefix PREFIX still installs libs to
+        # PREFIX/lib64/python2.7/site-packages/
+        # but we really want something saner.
+        #
+        # since prefix is given, but not python_install_lib, assume PREFIX/lib
+        flags += [ '--install-lib', File.join(prefix, 'lib') ]
+      end
     
-    %i(git_strategy hg_strategy svn_strategy).each do |strategy|
-  validate(strategy) do |key, _value|
-    warn(
-      '[Deprecation Warning] #{key} is deprecated and will be removed in '\
-      'Capistrano 3.7.0.\n'\
-      'https://github.com/capistrano/capistrano/blob/master/UPGRADING-3.7.md'
-    )
+          self
+    end # def add_symlink
   end
 end
