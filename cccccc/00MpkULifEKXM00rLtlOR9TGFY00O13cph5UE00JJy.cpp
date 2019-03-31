336b9486942026c6842fe85edc31b407574088c3
SUNsung <weatherius@gmail.com>
2019-03-31 12:07:31 +0300

    Rewrite [00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp] File in: 2019-03-31 12:07:30

--- cccccc/00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp
+++ cccccc/00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp
@@ -1,309 +1,139 @@
 
         
-        Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    
-    {}  // namespace tensorflow
-
-    
-    #include 'tensorflow/python/lib/core/py_exception_registry.h'
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    
-    {}  // namespace tensorflow
-
-    
-    // Safe containers for an owned TF_Status. On destruction, the handle
-// will be deleted by TF_DeleteStatus.
-using Safe_TF_StatusPtr = std::unique_ptr<TF_Status, detail::TFStatusDeleter>;
-Safe_TF_StatusPtr make_safe(TF_Status* status);
-    
-    #include 'tensorflow/core/framework/node_def.pb.h'
-#include 'tensorflow/core/framework/node_def_util.h'
-#include 'tensorflow/core/framework/op.h'
-#include 'tensorflow/core/framework/op_kernel.h'
-#include 'tensorflow/core/framework/types.h'
-#include 'tensorflow/core/lib/core/status.h'
-#include 'tensorflow/core/util/device_name_utils.h'
-    
-      // Synchronize with spinlocks.
-  static const unsigned kScheduleSpin = 0x02;
-  // Synchronize with spinlocks that also call CPU yield instructions.
-  static const unsigned kScheduleYield = 0x04;
-  // Synchronize with a 'synchronization primitive' (e.g. mutex).
-  static const unsigned kScheduleBlockingSync = 0x08;
-    
-    
-    {    // Vector of cached {config, executor} pairs.
-    std::vector<
-        std::pair<StreamExecutorConfig, std::unique_ptr<StreamExecutor>>>
-        configurations GUARDED_BY(configurations_mutex);
-  };
-    
-    namespace fft {
-    }
-    
-    #include 'base/basictypes.h'
-#include '../dispatcher_host.h'
-    
-      virtual void Call(const std::string& method,
-                    const base::ListValue& arguments,
-                    content::RenderFrameHost* rvh = nullptr);
-  virtual void CallSync(const std::string& method,
-                        const base::ListValue& arguments,
-                        base::ListValue* result);
-    
-    // Call method of an object in browser and return the result.
-// function CallObjectMethod(id, type, method, args);
-v8::Handle<v8::Value> CallObjectMethodSync(int routing_id,
-                                           int object_id,
-                                           const std::string& type,
-                                           const std::string& method,
-                                           v8::Handle<v8::Value> args);
-    
-    class BaseEvent {
-  friend class EventListener;
-  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
-    }
-    
-    
-    {}  // namespace nw
-    
-      nw::Package* package = nw::package();
-  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  base::CommandLine::StringVector args = command_line->GetArgs();
-  base::CommandLine::StringVector argv = command_line->original_argv();
-    
-    bool NwObjCallObjectMethodSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  base::ListValue* arguments = nullptr;
-  int id = 0;
-  std::string type, method;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
-  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
-  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
-  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
-    }
-    
-        void operator() (const typename VecTraits<T>::vec64 & v_src0,
-                     const typename VecTraits<T>::vec64 & v_src1,
-                     typename VecTraits<T>::vec64 & v_dst) const
-    {
-        typename VecTraits<wtype>::vec128 vr;
-        wideAdd(vmovl(v_src0), vmovl(v_src1), vr);
-    }
-    
-                    int32x4_t va = vaddq_s32(lane0a, lane2a);
-                int32x4_t vb = vaddq_s32(lane0b, lane2b);
-                int32x4_t wa = vaddq_s32(va, lane1a);
-                int32x4_t wb = vaddq_s32(vb, lane1b);
-    
-    void Canny3x3L2(const Size2D &size,
-                const u8 * srcBase, ptrdiff_t srcStride,
-                u8 * dstBase, ptrdiff_t dstStride,
-                f64 low_thresh, f64 high_thresh,
-                Margin borderMargin)
-{
-    internal::assertSupportedConfiguration(isCanny3x3Supported(size));
-#ifdef CAROTENE_NEON
-    Canny3x3<true, false>(size, 1,
-                          srcBase, srcStride,
-                          dstBase, dstStride,
-                          NULL, 0,
-                          NULL, 0,
-                          low_thresh, high_thresh,
-                          borderMargin);
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)dstBase;
-    (void)dstStride;
-    (void)low_thresh;
-    (void)high_thresh;
-    (void)borderMargin;
-#endif
-}
-    
-    #include 'common.hpp'
-    
-    #else
-    
-                uint32x4_t vs4 = vpaddlq_u16(vpaddlq_u8(vs));
-            uint32x2_t vs2 = vadd_u32(vget_low_u32(vs4), vget_high_u32(vs4));
-    
-                    uint16x8_t vSum_3_1 = vaddq_u16(vLane3.val[0], vLane1.val[0]);
-                uint16x8_t vSum_4_2 = vaddq_u16(vLane3.val[1], vLane1.val[1]);
-                uint16x8_t vSum_5_6 = vaddq_u16(vLane3.val[2], vLane1.val[2]);
-    
-    class BLOBNBOX;
-class BLOBNBOX_CLIST;
-class BLOB_CHOICE_LIST;
-class STRING;
-class TO_BLOCK_LIST;
-class UNICHARSET;
-template <typename T> class GenericVector;
-    
-    // Main entry point for Paragraph Detection Algorithm.
-//
-// Given a set of equally spaced textlines (described by row_infos),
-// Split them into paragraphs.  See http://goto/paragraphstalk
-//
-// Output:
-//   row_owners - one pointer for each row, to the paragraph it belongs to.
-//   paragraphs - this is the actual list of PARA objects.
-//   models - the list of paragraph models referenced by the PARA objects.
-//            caller is responsible for deleting the models.
-void DetectParagraphs(int debug_level,
-                      GenericVector<RowInfo> *row_infos,
-                      GenericVector<PARA *> *row_owners,
-                      PARA_LIST *paragraphs,
-                      GenericVector<ParagraphModel *> *models);
-    
-    
-    {  name += UNLV_EXT;              //add extension
-  if ((pdfp = fopen (name.string (), 'rb')) == nullptr) {
-    return false;                //didn't read one
-  } else {
-    while (tfscanf(pdfp, '%d %d %d %d %*s', &x, &y, &width, &height) >= 4) {
-                                 //make rect block
-      block = new BLOCK (name.string (), TRUE, 0, 0,
-                         (int16_t) x, (int16_t) (ysize - y - height),
-                         (int16_t) (x + width), (int16_t) (ysize - y));
-                                 //on end of list
-      block_it.add_to_end (block);
-    }
-    fclose(pdfp);
+          // Calculate the depth at which the requirement's generic parameters
+  // appear in the synthetic signature.
+  unsigned depth = 0;
+  if (covariantSelf) {
+    depth++;
   }
-  return true;
-}
+  if (conformanceSig) {
+    depth += conformanceSig->getGenericParams().back()->getDepth() + 1;
+  }
     
-    template <typename T> class GenericVector;
-template <typename T> class GenericVector;
+      /// The number of conformance requirements, cached to avoid constantly
+  /// recomputing it on conformance-buffer access.
+  const unsigned numConformanceRequirements : 31;
     
-      // Computes and returns the squared evaluation metric for a line fit.
-  double EvaluateLineFit();
-    
-    
-    {  int delta = this - prev;
-  int32_t n = prev->n_ + 1;
-  int32_t sig_x = prev->sig_x_ + delta;
-  int64_t sig_xsq = prev->sig_xsq_ + delta * delta;
-  int64_t cost = (sig_xsq - sig_x * sig_x / n) / n;
-  cost += prev->total_cost_;
-  UpdateIfBetter(cost, prev->total_steps_ + 1, prev, n, sig_x, sig_xsq);
-  return cost;
-}
-    
-    class DLLSYM DIR128
-{
-  public:
-    DIR128() = default;
+        if (info.status == serialization::Status::Valid) {
+      assert(info.bytes != 0);
+      if (!info.name.empty()) {
+        StringRef moduleData = buf.substr(0, info.bytes);
+        std::unique_ptr<llvm::MemoryBuffer> bitstream(
+          llvm::MemoryBuffer::getMemBuffer(moduleData, info.name, false));
+    }
     }
     
-    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
-::opencensus::stats::MeasureDouble RpcClientServerLatency();
-::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
-    
-    constexpr size_t RpcServerStatsEncoding::kRpcServerStatsSize;
-constexpr size_t RpcServerStatsEncoding::kEncodeDecodeFailure;
-constexpr size_t RpcServerStatsEncoding::kVersionIdSize;
-constexpr size_t RpcServerStatsEncoding::kFieldIdSize;
-constexpr size_t RpcServerStatsEncoding::kVersionIdOffset;
-constexpr size_t RpcServerStatsEncoding::kVersionId;
-    
-    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_RPC_ENCODING_H */
+    #undef VERB
+#undef DIRECTIONAL_PREPOSITION
+#undef PREPOSITION
 
     
-      Status GetAllExtensionNumbers(
-      ServerContext* context, const grpc::string& type,
-      reflection::v1alpha::ExtensionNumberResponse* response);
+    
+    {}%
     
     
-    {                //    Save state to this URX_LB_CONT op, so failure to match will repeat the loop.
-                //      (successful match will cause a FAIL out of the loop altogether.)
-                fp = StateSave(fp, fp->fPatIdx-4, status);
-                fp->fInputIdx = lbStartIdx;
-            }
-            break;
-    
-    
-    {    switch(attr) {
-    case UCOL_FRENCH_COLLATION:
-        ownedSettings->setFlag(CollationSettings::BACKWARD_SECONDARY, value,
-                               defaultSettings.options, errorCode);
-        break;
-    case UCOL_ALTERNATE_HANDLING:
-        ownedSettings->setAlternateHandling(value, defaultSettings.options, errorCode);
-        break;
-    case UCOL_CASE_FIRST:
-        ownedSettings->setCaseFirst(value, defaultSettings.options, errorCode);
-        break;
-    case UCOL_CASE_LEVEL:
-        ownedSettings->setFlag(CollationSettings::CASE_LEVEL, value,
-                               defaultSettings.options, errorCode);
-        break;
-    case UCOL_NORMALIZATION_MODE:
-        ownedSettings->setFlag(CollationSettings::CHECK_FCD, value,
-                               defaultSettings.options, errorCode);
-        break;
-    case UCOL_STRENGTH:
-        ownedSettings->setStrength(value, defaultSettings.options, errorCode);
-        break;
-    case UCOL_HIRAGANA_QUATERNARY_MODE:
-        // Deprecated attribute. Check for valid values but do not change anything.
-        if(value != UCOL_OFF && value != UCOL_ON && value != UCOL_DEFAULT) {
-            errorCode = U_ILLEGAL_ARGUMENT_ERROR;
+    {    // Must be 'const' or nothing.
+    clang::Qualifiers quals = pointee.getQualifiers();
+    bool isConst = quals.hasConst();
+    quals.removeConst();
+    if (quals.empty()) {
+      if (auto record = pointee->getAs<clang::RecordType>()) {
+        auto recordDecl = record->getDecl();
+        if (recordDecl->hasAttr<clang::ObjCBridgeAttr>() ||
+            recordDecl->hasAttr<clang::ObjCBridgeMutableAttr>() ||
+            recordDecl->hasAttr<clang::ObjCBridgeRelatedAttr>() ||
+            isKnownCFTypeName(typedefDecl->getName())) {
+          return forRecord(isConst, record->getDecl());
         }
-        break;
-    case UCOL_NUMERIC_COLLATION:
-        ownedSettings->setFlag(CollationSettings::NUMERIC, value, defaultSettings.options, errorCode);
-        break;
-    default:
-        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
-        break;
+      } else if (pointee->isVoidType()) {
+        if (typedefDecl->hasAttr<clang::ObjCBridgeAttr>() ||
+            isKnownCFTypeName(typedefDecl->getName())) {
+          return isConst ? forConstVoid() : forVoid();
+        }
+      }
     }
-    if(U_FAILURE(errorCode)) { return; }
-    setFastLatinOptions(*ownedSettings);
-    if(value == UCOL_DEFAULT) {
-        setAttributeDefault(attr);
-    } else {
-        setAttributeExplicitly(attr);
+  }
+    
+    class CFPointeeInfo {
+  bool IsValid;
+  bool IsConst;
+  llvm::PointerUnion<const clang::RecordDecl *, const clang::TypedefNameDecl *>
+      Decl;
+  CFPointeeInfo() = default;
     }
-}
+    
+    bool Punycode::decodePunycodeUTF8(StringRef InputPunycode,
+                                  std::string &OutUTF8) {
+  std::vector<uint32_t> OutCodePoints;
+  if (!decodePunycode(InputPunycode, OutCodePoints))
+    return false;
+    }
+    
+    void GenerateDSYMJobAction::anchor() {}
     
     
-    {    BreakIterator *get() const { return ptr; }
-    BreakIterator *operator->() const { return ptr; }
-    BreakIterator &operator*() const { return *ptr; }
-private:
-    BreakIterator *ptr;
-    SharedBreakIterator(const SharedBreakIterator &);
-    SharedBreakIterator &operator=(const SharedBreakIterator &);
+    {public:
+  Darwin(const Driver &D, const llvm::Triple &Triple) : ToolChain(D, Triple) {}
+  ~Darwin() = default;
+  std::string sanitizerRuntimeLibName(StringRef Sanitizer,
+                                      bool shared = true) const override;
 };
     
-    #include 'unicode/utypes.h'
+      virtual void PrintDefaultValueString(std::ostream &os) const {  // NOLINT(*)
+    std::string s;
+    caffe::NetParameter np;
+    // Avoid wasting time making a copy -- just push in out default object's pointer
+    np.mutable_layer()->AddAllocated(const_cast<::caffe::LayerParameter *>(&default_value_));
+    google::protobuf::TextFormat::PrintToString(np, &s);
+    np.mutable_layer()->ReleaseLast();
+    os << '\'' << s << '\'';
+  }
     
-    int32_t StandardPlural::indexFromString(const char *keyword, UErrorCode &errorCode) {
-    if (U_FAILURE(errorCode)) { return OTHER; }
-    int32_t i = indexOrNegativeFromString(keyword);
-    if (i >= 0) {
-        return i;
-    } else {
-        errorCode = U_ILLEGAL_ARGUMENT_ERROR;
-        return OTHER;
+    /*!
+ * \brief Moore-Penrose pseudoinverse of the Khatri-Rao product
+ *
+ * Given input matrices A_1, ..., A_n, of shape (l_1, k), ..., (l_n, k) respectively, the pseudoinverse of the Khatri-Rao product is
+ *
+ *   pinv(A_1 khatri-rao A_2 khatri-rao ... khatri-rao A_n) =
+ *     ((A_1^T A_1) hadamard-dot ... hadamard-dot (A_n^T A_n))
+ *     (A_1 khatri-rao ... khatri-rao A_n)^T
+ *
+ * As the first term of the r.h.s is a square matrix, the result is always of the same shape as the transpose of the Khatri-Rao product of the input matrices. The input argument ts_arr could contain the original input matrices, or transposed ones.
+ *
+ * \param out result matrix
+ * \param ts_arr vector of input matrices
+ * \param input_transposed if every input matrices is transposed
+ */
+template <typename DType>
+inline void inv_khatri_rao
+  (Tensor<cpu, 2, DType> out,
+  const std::vector<Tensor<cpu, 2, DType> > &ts_arr,
+  bool input_transposed = false) {
+  CHECK_GE(ts_arr.size(), 1) << 'Input tensor array must be non-empty';
     }
-}
     
-    U_NAMESPACE_BEGIN
+    Crop the 2nd and 3rd dim of input data, with the corresponding size of h_w or
+with width and height of the second input symbol, i.e., with one input, we need h_w to
+specify the crop height and width, otherwise the second input symbol's size will be used
+)code' ADD_FILELINE)
+    
+    #include <algorithm>
+#include <vector>
+#include './spatial_transformer-inl.h'
+namespace mxnet {
+namespace op {
+#if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 5
+template<typename DType>
+class CuDNNSpatialTransformerOp : public Operator {
+ public:
+  explicit CuDNNSpatialTransformerOp(SpatialTransformerParam param) {
+    this->param_ = param;
+    init_cudnn_ = false;
+    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
+    if (param_.sampler_type == st::kBilinear) {
+      sampler_ = CUDNN_SAMPLER_BILINEAR;
+    }
+  }
+    }
+    }
+    }
