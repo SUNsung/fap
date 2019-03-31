3777ec65bcd8bf50cfbb8a72b27e5fff29b2379a
SUNsung <weatherius@gmail.com>
2019-03-31 23:44:29 +0300

    Rewrite [11H7zUYJoqu7U11sXfU5DZoNGY11a3by9xxVWhw11IN.cpp] File in: 2019-03-31 23:44:28

--- cccccc/11H7zUYJoqu7U11sXfU5DZoNGY11a3by9xxVWhw11IN.cpp
+++ cccccc/11H7zUYJoqu7U11sXfU5DZoNGY11a3by9xxVWhw11IN.cpp
@@ -1,86 +1,208 @@
 
         
-        #include <google/protobuf/compiler/objectivec/objectivec_file.h>
-#include <google/protobuf/compiler/objectivec/objectivec_enum.h>
-#include <google/protobuf/compiler/objectivec/objectivec_extension.h>
-#include <google/protobuf/compiler/objectivec/objectivec_message.h>
-#include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/io/printer.h>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
-#include <google/protobuf/stubs/stl_util.h>
-#include <google/protobuf/stubs/strutil.h>
-#include <algorithm> // std::find()
-#include <iostream>
-#include <sstream>
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    void MapLiteTestUtil::SetMapFields(unittest::TestMapLite* message) {
-  MapTestUtilImpl::SetMapFields<unittest::MapEnumLite,
-                                unittest::MAP_ENUM_BAR_LITE,
-                                unittest::MAP_ENUM_BAZ_LITE>(message);
+    ModelAnalyzer::ModelAnalyzer(const GrapplerItem& item) : item_(item) {}
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    #include 'tensorflow/c/c_api.h'
+#include 'tensorflow/core/lib/core/errors.h'
+#include 'tensorflow/core/platform/mutex.h'
+#include 'tensorflow/python/lib/core/bfloat16.h'
+#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
+    
+    
+    {
+    {}  // namespace swig
+}  // namespace tensorflow
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    // Request Shell's id for current render_view_host.
+IPC_SYNC_MESSAGE_ROUTED0_1(ShellViewHostMsg_GetShellId,
+                           int /* result */)
+    
+    #ifndef CONTENT_NW_SRC_API_APP_APP_H_
+#define CONTENT_NW_SRC_API_APP_APP_H_
+    
+    #endif  // CONTENT_NW_SRC_API_BASE_BASE_H_
+
+    
+    
+    {
+    {    if (zoom_controller) {
+      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
+      if (zoom_factor > content::kMaximumZoomFactor) {
+        zoom_factor = content::kMaximumZoomFactor;
+      }
+      if (zoom_factor < content::kMinimumZoomFactor) {
+        zoom_factor = content::kMinimumZoomFactor;
+      }
+      x *= zoom_factor;
+      y *= zoom_factor;
+    }
+    
+    Popup(x, y, rvh);
+  } else if (method == 'EnableShowEvent') {
+    arguments.GetBoolean(0, &enable_show_event_);
+  } else {
+    NOTREACHED() << 'Invalid call to Menu method:' << method
+                 << ' arguments:' << arguments;
+  }
 }
     
-            virtual Dictionary GetCheckpointState() const override;
-        virtual void RestoreFromCheckpoint(const Dictionary& checkpoint) override;
-    
-    
-    {        return std::tuple<ElementType *, SparseIndexType *, SparseIndexType *, size_t, size_t, size_t>(reinterpret_cast<ElementType*>(blockValues), blockId2Col, col2BlockId, numBlocks, numRows, numCols);
+    ui::KeyboardCode GetKeycodeFromText(std::string text){
+  ui::KeyboardCode retval = ui::VKEY_UNKNOWN;
+  if (text.size() != 0){
+    std::string upperText = base::ToUpperASCII(text);
+    std::string keyName = text;
+    bool found = false;
+    if (upperText.size() == 1){
+      char key = upperText[0];
+      if (key>='0' && key<='9'){//handle digital
+        keyName = 'Digit' + upperText;
+        found = true;
+      } else if (key>='A'&&key<='Z'){//handle alphabet
+        keyName = 'Key' + upperText;
+        found = true;
+      }
+    }
+    }
     }
     
-            size_t numSequences = oneHotSequences.size();
-        NDShape valueDataShape = sampleShape.AppendShape({ maxSequenceLength, numSequences });
-        size_t numCSCCols = valueDataShape.SubShape(1).TotalSize() + 1;
-        std::vector<SparseIndexType> colStarts(numCSCCols);
-        std::vector<ElementType> nonZeroValues;
-        std::vector<SparseIndexType> rowIndices;
-        for (size_t i = 0; i < numSequences; ++i)
-        {
-            size_t currentSequenceNumCols = oneHotSequences[i].size();
-            size_t j = 0;
-            for (; j < currentSequenceNumCols; ++j)
-            {
-                colStarts[(i * maxSequenceNumCols) + j] = (SparseIndexType)nonZeroValues.size();
-                size_t oneHotIdx = oneHotSequences[i][j];
-                if ((oneHotIdx & OneHotSkip) == OneHotSkip) // note that OneHotSkip used to be (size_t)-1, and later changed to (uint32_t)-1. Both are supported
-                {
-                    nonZeroValues.push_back(0);
-                    rowIndices.push_back(0);
-                }
-                else
-                {
-                    nonZeroValues.push_back(1);
-                    if (oneHotIdx >= dimension)
-                        InvalidArgument('Value::Create: one-hot index value (%zu) exceeds vocabulary size (%zu).', oneHotSequences[i][j], dimension);
-                    rowIndices.push_back((SparseIndexType)(oneHotSequences[i][j]));
-                }
-            }
-    }
+    // Then, use the TEST_P macro to define as many parameterized tests
+// for this fixture as you want. The _P suffix is for 'parameterized'
+// or 'pattern', whichever you prefer to think.
     
-    // process the special-nodes parameters
-void ComputationNetwork::ProcessSpecialNodes(const ScriptableObjects::IConfigRecord& config, std::deque<ComputationNodeBasePtr>& workList)
+      // Signals that the death test did not die as expected.
+  virtual void Abort(AbortReason reason) = 0;
+    
+    
+    {  template <class Tuple>
+  static GTEST_BY_REF_(GTEST_TUPLE_ELEMENT_(8, Tuple))
+  ConstField(const Tuple& t) { return t.f8_; }
+};
+    
+    #endif  // GTEST_SAMPLES_PRIME_TABLES_H_
+
+    
+      void setTaskQueue(DHTTaskQueue* taskQueue);
+    
+    class DHTNode;
+class DHTRoutingTable;
+class DHTMessageDispatcher;
+class DHTMessageFactory;
+class DHTTaskQueue;
+class DHTAbstractTask;
+    
+    class DHTTask;
+    
+    void DHTTokenUpdateCommand::process()
 {
-    for (let& id : config.GetMemberIds())
+  try {
+    tokenTracker_->updateTokenSecret();
+  }
+  catch (RecoverableException& e) {
+    A2_LOG_ERROR_EX(EX_EXCEPTION_CAUGHT, e);
+  }
+}
+    
+    void DNSCache::remove(const std::string& hostname, uint16_t port)
+{
+  auto target = std::make_shared<CacheEntry>(hostname, port);
+  entries_.erase(target);
+}
+    
+    
     {
-        let pos = id.find(L'Nodes');
-        if (pos == wstring::npos || pos != id.size() - 5)  // special node name = node-group name + L'Nodes'
-            continue;
-        let nodeGroup = id.substr(0, id.size() - 5);
-        if (nodeGroupNames.find(nodeGroup) == nodeGroupNames.end())
-            continue;
+    {
+    {
+    {}  // namespace can
+}  // namespace canbus
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    TEST(ByteTest, GetValue) {
+  unsigned char byte_value = 0x1A;
+  Byte value(&byte_value);
+  EXPECT_EQ(0x05, value.get_byte(1, 3));
+  EXPECT_EQ(0x01, value.get_byte(1, 1));
+  EXPECT_EQ(0x00, value.get_byte(8, 1));
+  EXPECT_EQ(0x00, value.get_byte(-1, 1));
+  EXPECT_EQ(0x1A, value.get_byte(0, 10));
+}
+    
+    DEFINE_string(adapter_config_filename, 'modules/canbus/conf/adapter.conf',
+              'The adapter config file');
+    
+    void ClusterQualityInfo702::Parse(const std::uint8_t* bytes, int32_t length,
+                                  ContiRadar* conti_radar) const {
+  int id = target_id(bytes, length);
+  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
+    if (conti_radar->contiobs(i).obstacle_id() == id) {
+      auto conti_obs = conti_radar->mutable_contiobs(i);
+      conti_obs->set_longitude_dist_rms(
+          LINEAR_RMS[longitude_dist_rms(bytes, length)]);
+      conti_obs->set_lateral_dist_rms(
+          LINEAR_RMS[lateral_dist_rms(bytes, length)]);
+      conti_obs->set_longitude_vel_rms(
+          LINEAR_RMS[longitude_vel_rms(bytes, length)]);
+      conti_obs->set_lateral_vel_rms(
+          LINEAR_RMS[lateral_vel_rms(bytes, length)]);
+      conti_obs->set_probexist(PROBOFEXIST[pdh0(bytes, length)]);
+      switch (invalid_state(bytes, length)) {
+        case 0x01:
+        case 0x02:
+        case 0x03:
+        case 0x06:
+        case 0x07:
+        case 0x0E:
+          conti_obs->set_probexist(PROBOFEXIST[0]);
+        default:
+          break;
+      }
+      switch (ambig_state(bytes, length)) {
+        case 0x00:
+        case 0x01:
+        case 0x02:
+          conti_obs->set_probexist(PROBOFEXIST[0]);
+        default:
+          break;
+      }
     }
+  }
+}
+    
+    
+    {  switch (x) {
+    case 0x0:
+      return OUTPUT_TYPE_NONE;
+    case 0x1:
+      return OUTPUT_TYPE_OBJECTS;
+    case 0x2:
+      return OUTPUT_TYPE_CLUSTERS;
+    default:
+      return OUTPUT_TYPE_ERROR;
+  }
+}
+    
+    // config detail: {'name': 'output_value', 'offset': 0.0, 'precision': 0.001,
+// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 39,
+// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
+double Accelrpt68::output_value(const std::uint8_t* bytes,
+                                int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(0, 8);
     }
     
-        virtual void /*ComputationNodeNonLooping::*/ ForwardPropNonLooping() override
-    {
-        FrameRange fr(InputRef(0).GetMBLayout());
-        InputRef(0).ValueFor(fr).VectorMax(*m_maxIndexes0, *m_maxValues, true);
-        InputRef(1).ValueFor(fr).VectorMax(*m_maxIndexes1, *m_maxValues, true, m_topK);
-        MaskMissingColumnsToZero(*m_maxIndexes0, InputRef(0).GetMBLayout(), fr);
-        MaskMissingColumnsToZero(*m_maxIndexes1, InputRef(1).GetMBLayout(), fr);
-        Value().AssignNumOfDiff(*m_maxIndexes0, *m_maxIndexes1, m_topK > 1);
-#if NANCHECK
-        Value().HasNan('ClassificationError');
-#endif
-#if DUMPOUTPUT
-        Value().Print('ClassificationErrorNode');
-#endif
-    }
+      Byte t3(bytes + 7);
+  t = t3.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
