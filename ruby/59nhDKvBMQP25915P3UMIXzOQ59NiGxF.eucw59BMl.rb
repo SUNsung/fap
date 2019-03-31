e0308c50dffb9f35e0c7ccab4a2abfb5c1ee2e37
SUNsung <weatherius@gmail.com>
2019-03-31 06:48:20 +0300

    Rewrite [59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb] File in: 2019-03-31 06:48:19

--- ruby/59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb
+++ ruby/59nhDKvBMQP25915P3UMIXzOQ59NiGxF.eucw59BMl.rb
@@ -1,35 +1,105 @@
 
         
-            def as_json
-      @pages
+            def action_completed(completion_context: nil)
     end
     
-        def terminal_width
-      return @terminal_width if defined? @terminal_width
-    
-            css('code code').each do |node|
-          node.before(node.children).remove
-        end
-    
-            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
-          next if node.previous.try(:content).present? || node.next.try(:content).present?
-          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
-          node.content = node.content.strip
-          node.name = 'pre' if node.content =~ /\s/
-          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
-        end
-    
-        it 'returns false if the given string is not a switch' do
-      %w[b ns bar --bar -n a bad arg].each do |s|
-        expect(subject.switch?(s)).to be false
+            UI.message('Adding git tag '#{tag}' 🎯.')
+        Actions.sh(cmd.join(' '))
       end
+    
+          before :each do
+        Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER] = build_number
+      end
+    
+          it 'Does not use pattern matching for tag name if so requested' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          changelog_from_git_commits()
+        end').runner.execute(:test)
+    
+          it 'works with single quote in rule name' do
+        rule = 'CoveredSwitchStatementsDon'tNeedDefault'
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+              enable_rules: [\'#{rule}\'],
+              disable_rules: [\'#{rule}\']
+            )
+          end').runner.execute(:test)
+    
+        it 'shelljoins multiple args' do
+      message = 'A message'
+      command = command_from_args('git', 'commit', '-m', message)
+      expect(command).to eq('git commit -m #{message.shellescape}')
     end
+    
+    puts('[WARNING] You are calling #{tool_name} directly. Usage of the tool name without the `fastlane` prefix is deprecated in fastlane 2.0'.yellow)
+puts('Please update your scripts to use `fastlane #{tool_name} #{full_params}` instead.'.yellow)
+    
+    class Devise::ConfirmationsController < DeviseController
+  # GET /resource/confirmation/new
+  def new
+    self.resource = resource_class.new
   end
     
-    ENV['COCOAPODS_DISABLE_STATS'] = 'true'
+      # Check if there is no signed in user before doing the sign out.
+  #
+  # If there is no signed in user, it will set the flash message and redirect
+  # to the after_sign_out path.
+  def verify_signed_out_user
+    if all_signed_out?
+      set_flash_message! :notice, :already_signed_out
+    
+    Rails.application.initialize!
+    
+          protected
+    
+                  define_method method do |resource_or_scope, *args|
+                scope = Devise::Mapping.find_scope!(resource_or_scope)
+                router_name = Devise.mappings[scope].router_name
+                context = router_name ? send(router_name) : _devise_route_context
+                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
+              end
+            end
+          end
+        end
+      end
+    
+          def headers_for(action, opts)
+        headers = {
+          subject: subject_for(action),
+          to: resource.email,
+          from: mailer_sender(devise_mapping),
+          reply_to: mailer_reply_to(devise_mapping),
+          template_path: template_paths,
+          template_name: action
+        }.merge(opts)
+    
+              if recoverable.persisted?
+            if recoverable.reset_password_period_valid?
+              recoverable.reset_password(attributes[:password], attributes[:password_confirmation])
+            else
+              recoverable.errors.add(:reset_password_token, :expired)
+            end
+          end
+    
+      def process_salmon
+    SalmonWorker.perform_async(@account.id, payload.force_encoding('UTF-8'))
+  end
+end
 
     
-    # SPECS ===============================================================
+      def load_export
+    @export = Export.new(current_account)
+  end
+    
+      context 'every shim script' do
+    it 'has valid bash syntax' do
+      # These have no file extension, but can be identified by their shebang.
+      (HOMEBREW_LIBRARY_PATH/'shims').find do |path|
+        next if path.directory?
+        next if path.symlink?
+        next unless path.executable?
+        next unless path.read(12) == '#!/bin/bash\n'
     
         if run? && ARGV.any?
       require 'optparse'
@@ -44,34 +114,76 @@
     end
   end
     
-          # Essentially the inverse of +mask_token+.
-      def unmask_token(masked_token)
-        # Split the token into the one-time pad and the encrypted
-        # value and decrypt it
-        token_length = masked_token.length / 2
-        one_time_pad = masked_token[0...token_length]
-        encrypted_token = masked_token[token_length..-1]
-        xor_byte_strings(one_time_pad, encrypted_token)
+            if unmasked_token?(token)
+          compare_with_real_token token, session
+    
+          def default_options
+        DEFAULT_OPTIONS
       end
     
-          def accepts?(env)
-        cookie_header = env['HTTP_COOKIE']
-        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
-        cookies.each do |k, v|
-          if k == session_key && Array(v).size > 1
-            bad_cookies << k
-          elsif k != session_key && Rack::Utils.unescape(k) == session_key
-            bad_cookies << k
-          end
-        end
-        bad_cookies.empty?
+          def call(env)
+        request  = Request.new(env)
+        get_was  = handle(request.GET)
+        post_was = handle(request.POST) rescue nil
+        app.call env
+      ensure
+        request.GET.replace  get_was  if get_was
+        request.POST.replace post_was if post_was
       end
     
-            if has_vector?(request, headers)
-          warn env, 'attack prevented by #{self.class}'
+        headers = get('/', {}, 'wants' => 'text/html').headers
+    expect(headers['Content-Security-Policy']).to eq('connect-src https://api.mybank.com; default-src none; font-src https://cdn.mybank.net; frame-src self; img-src https://cdn.mybank.net; media-src https://cdn.mybank.net; object-src https://cdn.mybank.net; report-uri /my_amazing_csp_report_parser; sandbox allow-scripts; script-src https://cdn.mybank.net; style-src https://cdn.mybank.net')
+    expect(headers['Content-Security-Policy-Report-Only']).to be_nil
+  end
     
-      it 'allows for a custom authenticity token param' do
-    mock_app do
-      use Rack::Protection::AuthenticityToken, :authenticity_param => 'csrf_param'
-      run proc { |e| [200, {'Content-Type' => 'text/plain'}, ['hi']] }
+    def commit_details
+  { :message => 'Did something at #{Time.now}',
+    :name    => 'Tom Preston-Werner',
+    :email   => 'tom@github.com' }
+end
+    
+        # Title is based on file name when h1_title is false.
+    actual = @view.title
+    assert_equal 'H1', title
+  end
+end
+
+    
+        # Remove all slashes from the start of string.
+    # Remove all double slashes
+    def clean_url url
+      return url if url.nil?
+      url.gsub('%2F', '/').gsub(/^\/+/, '').gsub('//', '/')
     end
+    
+          def field_container(model, method, options = {}, &block)
+        css_classes = options[:class].to_a
+        css_classes << 'field'
+        css_classes << 'withError' if error_message_on(model, method).present?
+        content_tag(
+          :div, capture(&block),
+          options.merge(class: css_classes.join(' '), id: '#{model}_#{method}_field')
+        )
+      end
+    
+    task default: :test
+    
+          def find_product(id)
+        @product = product_scope.friendly.distinct(false).find(id.to_s)
+      rescue ActiveRecord::RecordNotFound
+        @product = product_scope.find_by(id: id)
+        not_found unless @product
+      end
+    
+            private
+    
+            def stock_location
+          render 'spree/api/v1/shared/stock_location_required', status: 422 and return unless params[:stock_location_id]
+          @stock_location ||= StockLocation.accessible_by(current_ability, :read).find(params[:stock_location_id])
+        end
+    
+            def index
+          authorize! :read, StockMovement
+          @stock_movements = scope.ransack(params[:q]).result.page(params[:page]).per(params[:per_page])
+          respond_with(@stock_movements)
+        end
