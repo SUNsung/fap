bed6153902502280af206aee6e2be87ba4c91b00
SUNsung <weatherius@gmail.com>
2019-03-31 15:18:04 +0300

    Rewrite [17lopW5s4riN217..2hrDCuXYM17gUWJ3EfFYjs17maA.cpp] File in: 2019-03-31 15:18:04

--- cccccc/17lopW5s4riN217..2hrDCuXYM17gUWJ3EfFYjs17maA.cpp
+++ cccccc/17lopW5s4riN217..2hrDCuXYM17gUWJ3EfFYjs17maA.cpp
@@ -1,130 +1,219 @@
 
         
-        
-    {  DefaultCacheKey CKey(const_cast<void*>(Key), &DCache.CBs);
-  auto Entry = DCache.Entries.find(CKey);
-  if (Entry != DCache.Entries.end()) {
-    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
-    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
-    DCache.Entries.erase(Entry);
-    return true;
-  }
-  return false;
-}
+          // Open leveldb
+  leveldb::DB* db;
+  leveldb::Options options;
+  options.create_if_missing = true;
+  options.error_if_exists = true;
+  leveldb::Status status = leveldb::DB::Open(
+      options, db_filename, &db);
+  CHECK(status.ok()) << 'Failed to open leveldb ' << db_filename
+      << '. Is it already existing?';
     
-    #include 'swift/Basic/Cache.h'
-#include 'llvm/ADT/SmallString.h'
-#include <cache.h>
+      /// @brief Scale the blob data by a constant factor.
+  void scale_data(Dtype scale_factor);
+  /// @brief Scale the blob diff by a constant factor.
+  void scale_diff(Dtype scale_factor);
     
-        StringRef Line = RawText.substr(0, Pos);
-    Lines.push_back(Line);
-    if (!IsFirstLine) {
-      size_t NonWhitespacePos = RawText.find_first_not_of(' ');
-      if (NonWhitespacePos != StringRef::npos)
-        WhitespaceToTrim =
-            std::min(WhitespaceToTrim,
-                     static_cast<unsigned>(NonWhitespacePos));
+      /**
+   * Caffe's thread local state will be initialized using the current
+   * thread values, e.g. device id, solver index etc. The random seed
+   * is initialized using caffe_rng_rand.
+   */
+  void StartInternalThread();
+    
+    #include 'caffe/layers/loss_layer.hpp'
+    
+      // algorithms for forward and backwards convolutions
+  cudnnConvolutionFwdAlgo_t *fwd_algo_;
+  cudnnConvolutionBwdFilterAlgo_t *bwd_filter_algo_;
+  cudnnConvolutionBwdDataAlgo_t *bwd_data_algo_;
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+     protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+    SHOULD_NOT_DO_GRADIENT(EnforceFinite);
+    
+    REGISTER_CPU_OPERATOR(
+    MergeSingleListFeatureTensors,
+    MergeSingleListFeatureTensorsOp<CPUContext>);
+OPERATOR_SCHEMA(MergeSingleListFeatureTensors)
+    .SetDoc(
+        'Merge given single-feature tensors with list features into one '
+        'multi-feature tensor.' +
+        doc)
+    .NumInputs([](int n) { return n >= 3 && n % 3 == 0; })
+    .NumOutputs(4)
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_values', '.values')
+    .Input(2, 'in1_presence', '.presence')
+    .Output(0, 'out_lengths', '.lengths')
+    .Output(1, 'out_keys', '.keys')
+    .Output(2, 'out_values_lengths', '.values.lengths')
+    .Output(3, 'out_values_values', '.values.values')
+    .Arg('feature_ids', 'feature ids');
+    
+    OPERATOR_SCHEMA(Glu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc(R'DOC(
+Applies gated linear unit to the input Tensor X. The output Y is half the size
+of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
+[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
+X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
+)DOC')
+    .Input(0, 'X', '1D input tensor')
+    .Output(0, 'Y', '1D output tensor');
+    
+    
+    {        Clear();
     }
-    IsFirstLine = false;
     
-    StringRef importer::getCFTypeName(
-            const clang::TypedefNameDecl *decl) {
-  if (auto pointee = CFPointeeInfo::classifyTypedef(decl)) {
-    auto name = decl->getName();
-    if (pointee.isRecord() || pointee.isTypedef())
-      if (name.endswith(SWIFT_CFTYPE_SUFFIX))
-        return name.drop_back(strlen(SWIFT_CFTYPE_SUFFIX));
-    }
-    }
+                // The assumption here is that a sequence always start at 0 (no invaid mark at the beginning),
+            // and ends at the first invalid mask. 
+            // Therefore, no need to check NDMask again.
+            // And the sequences has been resized to match the number of sequences and the length of each sequence in the Value object.
     
-    #ifndef SWIFT_IMPORTER_IMPORT_AS_MEMBER_INFERENCE_H
-#define SWIFT_IMPORTER_IMPORT_AS_MEMBER_INFERENCE_H
-    
-    std::string Mangle::translateOperator(StringRef Op) {
-  std::string Encoded;
-  for (char ch : Op) {
-    Encoded.push_back(translateOperatorChar(ch));
-  }
-  return Encoded;
-}
-    
-            virtual const std::unordered_set<StreamInformation>& StreamInfos() override { return m_streamInfos; }
-    
-        template <typename T>
-    inline void ValidateType(const Dictionary& dict, const std::wstring& typeValue, size_t currentVersion)
+        Constant Constant::CloneAs(DataType dataType) const
     {
-        if (!dict.Contains(typeKey))
-        {
-            const auto& version = GetVersion(dict);
-            LogicError('Required key '%ls' is not found in the dictionary (%s).',
-                       typeKey.c_str(), GetVersionsString<T>(currentVersion, version).c_str());
-        } 
+        if (dataType != DataType::Double)
+            InvalidArgument('Constant::Clone: Cannot clone Constant '%S' with DataType '%s' to DataType '%s'.', AsString().c_str(), DataTypeName(GetDataType()), DataTypeName(dataType));
     }
     
-            static bool IsUDF(const Dictionary& dict);
+    // Rand based on Mersenne Twister.
+// We use our own distribution in order to match baselines between different operating systems,
+// because uniform_distribution is not guaranteed to provide the same numbers on different platforms.
+// TODO: Switching to Boost would eliminate this problem.
+static inline size_t RandMT(const size_t begin, const size_t end, std::mt19937_64& rng)
+{
+    const size_t randomNumber = rng();
+    return begin + randomNumber % (end - begin);
+}
     
-            const NDShape& Shape() const override { return m_unpackedShape; }
-        DeviceDescriptor Device() const override { return m_isPacked ? m_packedData->Device() : Value::Device(); }
-        DataType GetDataType() const override { return m_isPacked ? m_packedData->GetDataType() : Value::GetDataType(); }
-        StorageFormat GetStorageFormat() const override { return m_isPacked? m_packedData->GetStorageFormat() : Value::GetStorageFormat(); }
-        bool IsReadOnly() const override { return m_isPacked ? m_packedData->IsReadOnly() : Value::IsReadOnly(); }
+        deque<ComputationNodeBasePtr> workList;
     
-                // Validate that each of the dynamic axes are unique
-            std::unordered_set<Axis> uniqueDynamicAxis;
-            for (auto& currentDynamicAxis : dynamicAxes)
-            {
-                auto retVal = uniqueDynamicAxis.insert(currentDynamicAxis);
-                if (!retVal.second)
-                    InvalidArgument('Dynamic axis named %S is specified more than once for Variable '%S'', currentDynamicAxis.Name().c_str(), AsString().c_str());
-            }
     
-    // -----------------------------------------------------------------------
-// InputValue (/*no input*/)
-// an input value (typically fed by a DataReader)
-// this covers two types: (regular vs. image)
-// TODO: There is still debate whether an InputValue without layout makes sense.
-// -----------------------------------------------------------------------
-    
-        size_t rank = runningAverage.GetShape().GetRank();
-    auto input = newInput.ValueTensorFor(rank, fr);
-    
-    // Return the source of the generated service file.
-grpc::string GenerateServiceSource(grpc_generator::File *file,
-                                   const grpc_generator::Service *service,
-                                   grpc_go_generator::Parameters *parameters);
-    
-      bool generate() {
-    std::string code;
-    code += std::string('// ') + FlatBuffersGeneratedWarning() +
-            '\n\ninclude \'flatbuffers.lobster\'\n\n';
-    for (auto it = parser_.enums_.vec.begin(); it != parser_.enums_.vec.end();
-         ++it) {
-      auto &enum_def = **it;
-      GenEnum(enum_def, &code);
+    {        // resize the temporaries to their proper size
+        size_t cols = Input(0)->Value().GetNumCols();
+        m_maxIndexes0->Resize(1, cols);
+        m_maxIndexes1->Resize(1, cols);
+        m_maxValues->Resize(1, cols);
     }
-    for (auto it = parser_.structs_.vec.begin();
-         it != parser_.structs_.vec.end(); ++it) {
-      auto &struct_def = **it;
-      GenStructPreDecl(struct_def, &code);
-    }
-    for (auto it = parser_.structs_.vec.begin();
-         it != parser_.structs_.vec.end(); ++it) {
-      auto &struct_def = **it;
-      GenStruct(struct_def, &code);
-    }
-    return SaveFile((path_ + file_name_ + '_generated.lobster').c_str(),
-                    code, false);
+    
+            // This changes the data from 'minibatch paking' in InputRef(0).Value() to 'dense CuDNN packing' in m_transposedInput
+        this->PackSequencesForCuDNN(InputRef(1).Value(), *m_transposedInput, numSequencesForFrame);
+    
+    #include <vector>
+#include <memory>
+    
+          X_form_t x_formater {{
+                            rc,
+                            xop,
+                            static_cast<uint32_t>(rb),
+                            static_cast<uint32_t>(ra),
+                            static_cast<uint32_t>(rt),
+                            op
+                          }};
+    
+      if (UNLIKELY(m_arrayHandle->kind() == APCKind::SerializedVec ||
+               m_arrayHandle->kind() == APCKind::SerializedDict)) {
+    return createFromSerialized(m_colType, m_arrayHandle);
   }
     
+      static void SetParsedIni(IniSettingMap &ini, const std::string confStr,
+                           const std::string &filename, bool constants_only,
+                           bool is_system);
     
-#ifndef FLATBUFFERS_GENERATED_NAMESPACETEST2_NAMESPACEA_H_
-#define FLATBUFFERS_GENERATED_NAMESPACETEST2_NAMESPACEA_H_
+    #include <folly/ScopeGuard.h>
     
-    #include 'flatbuffers/idl.h'
-#include 'test_init.h'
+    #ifndef HPHP_DATA_STREAM_WRAPPER_H
+#define HPHP_DATA_STREAM_WRAPPER_H
     
-    class ScalarReferenceResult {
- private:
-  ScalarReferenceResult(const char *_type, RegexMatcher::MatchResult _matched)
-      : type(_type), matched(_matched) {}
+    
+    {  /**
+   * Execute a debugger action.
+   */
+  virtual String debuggerVerb(const std::string& /*verb*/,
+                              const std::vector<std::string>& /*args*/) {
+    return String();
+  }
+};
+    
+    #endif
+
+    
+    
+    {  StructuredLogEntry cols;
+  fillCols(cols);
+  cols.setStr('event_name', event);
+  cols.setInt('priority', priority);
+  StructuredLog::log('hhvm_perf_warning', cols);
+}
+    
+    namespace HPHP {
     }
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+    }
+    
+    
+void printVec(const vector<int>& vec){
+    for(int e: vec)
+        cout << e << ' ';
+    cout << endl;
+}
+    
+    
+void printArr(const vector<int>& vec){
+    for(int e: vec)
+        cout << e << ' ';
+    cout << endl;
+}
+    
+    int main() {
+    }
+    
+    #ifndef V8_HEAP_STORE_BUFFER_INL_H_
+#define V8_HEAP_STORE_BUFFER_INL_H_
+    
+    void StoreBuffer::DeleteDuringGarbageCollection(StoreBuffer* store_buffer,
+                                                Address start, Address end) {
+  // In GC the store buffer has to be empty at any time.
+  DCHECK(store_buffer->Empty());
+  DCHECK(store_buffer->mode() != StoreBuffer::NOT_IN_GC);
+  Page* page = Page::FromAddress(start);
+  if (end) {
+    RememberedSet<OLD_TO_NEW>::RemoveRange(page, start, end,
+                                           SlotSet::PREFREE_EMPTY_BUCKETS);
+  } else {
+    RememberedSet<OLD_TO_NEW>::Remove(page, start);
+  }
+}
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(IterabilityTask);
+};
+    
+    
+    {  // Caller SP-relative.
+  static constexpr int kParam0Offset = -2 * kSystemPointerSize;
+  static constexpr int kReceiverOffset = -1 * kSystemPointerSize;
+};
+    
+    #define SSSE3_INSTRUCTION_LIST(V) \
+  V(phaddd, 66, 0F, 38, 02)       \
+  V(phaddw, 66, 0F, 38, 01)       \
+  V(pshufb, 66, 0F, 38, 00)       \
+  V(psignb, 66, 0F, 38, 08)       \
+  V(psignw, 66, 0F, 38, 09)       \
+  V(psignd, 66, 0F, 38, 0A)
