ab39e0fdddeea0b39ef397fd6ed2a154688f1289
SUNsung <weatherius@gmail.com>
2019-04-01 19:35:31 +0300

    Rewrite [55oFHuWk69o9Y553ynTUX3FIg55JgU8Qh3IQY55a8X.cpp] File in: 2019-04-01 19:35:30

--- cccccc/55oFHuWk69o9Y553ynTUX3FIg55JgU8Qh3IQY55a8X.cpp
+++ cccccc/55oFHuWk69o9Y553ynTUX3FIg55JgU8Qh3IQY55a8X.cpp
@@ -1,292 +1,225 @@
 
         
-            const QString &getAppName() const { return appName; }
-    const QIcon &getAppIcon() const { return appIcon; }
-    const QIcon &getTrayAndWindowIcon() const { return trayAndWindowIcon; }
-    const QString &getTitleAddText() const { return titleAddText; }
+        
+    {        if (specialized)
+          return ProtocolConformanceRef(specialized);
+      }
     
-        ~reverse_lock() {
-        templock.lock();
-        templock.swap(lock);
+    #include 'swift/Basic/Cache.h'
+#include 'llvm/ADT/DenseMap.h'
+#include 'llvm/Support/Mutex.h'
+    
+      // If the base name is vacuous or is a keyword and there are two or
+  // fewer words in the base name, don't split.
+  auto newBaseName = baseName.substr(0, endOfBaseName);
+  {
+    auto newWords = camel_case::getWords(newBaseName);
+    auto newWordsIter = newWords.begin();
+    bool isKeyword = !canBeMemberName(*newWordsIter);
+    bool isVacuous = isVacuousName(*newWordsIter);
+    if (isKeyword || isVacuous) {
+      // Just one word?
+      ++newWordsIter;
+      if (newWordsIter == newWords.end()) return false;
+    }
     }
     
-    /** Perform one SHA-256 transformation, processing 16 big endian 32-bit words. */
-static void secp256k1_sha256_transform(uint32_t* s, const uint32_t* chunk) {
-    uint32_t a = s[0], b = s[1], c = s[2], d = s[3], e = s[4], f = s[5], g = s[6], h = s[7];
-    uint32_t w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15;
+    void GeneratePCHJobAction::anchor() {}
+
+    
+      /**
+   * @brief Set the data_ shared_ptr to point to the SyncedMemory holding the
+   *        data_ of Blob other -- useful in Layer%s which simply perform a copy
+   *        in their Forward pass.
+   *
+   * This deallocates the SyncedMemory holding this Blob's data_, as
+   * shared_ptr calls its destructor when reset with the '=' operator.
+   */
+  void ShareData(const Blob& other);
+  /**
+   * @brief Set the diff_ shared_ptr to point to the SyncedMemory holding the
+   *        diff_ of Blob other -- useful in Layer%s which simply perform a copy
+   *        in their Forward pass.
+   *
+   * This deallocates the SyncedMemory holding this Blob's diff_, as
+   * shared_ptr calls its destructor when reset with the '=' operator.
+   */
+  void ShareDiff(const Blob& other);
+    
+    // Convert macro to string
+#define STRINGIFY(m) #m
+#define AS_STRING(m) STRINGIFY(m)
+    
+    namespace caffe {
     }
-    
-    #include 'include/secp256k1_ecdh.h'
-#include 'ecmult_const_impl.h'
-    
-    
-    {    /* cleanup */
-    secp256k1_context_destroy(none);
-    secp256k1_context_destroy(sign);
-    secp256k1_context_destroy(vrfy);
-    secp256k1_context_destroy(both);
-}
-    
-    // Based on the public domain implementation 'merged' by D. J. Bernstein
-// See https://cr.yp.to/chacha.html.
     
      protected:
   /**
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the predictions @f$ x @f$, a Blob with values in
-   *      @f$ [-\infty, +\infty] @f$ indicating the predicted score for each of
-   *      the @f$ K = CHW @f$ classes. Each @f$ x_n @f$ is mapped to a predicted
-   *      label @f$ \hat{l}_n @f$ given by its maximal index:
-   *      @f$ \hat{l}_n = \arg\max\limits_k x_{nk} @f$
-   *   -# @f$ (N \times 1 \times 1 \times 1) @f$
-   *      the labels @f$ l @f$, an integer-valued Blob with values
-   *      @f$ l_n \in [0, 1, 2, ..., K - 1] @f$
-   *      indicating the correct class label among the @f$ K @f$ classes
+   * @param bottom input Blob vector (length 2+)
+   *   -# @f$ (N \times ...) @f$
+   *      the inputs @f$ x_1 @f$
+   *   -# @f$ (M) @f$
+   *      the inputs @f$ x_2 @f$
    * @param top output Blob vector (length 1)
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      the computed accuracy: @f$
-   *        \frac{1}{N} \sum\limits_{n=1}^N \delta\{ \hat{l}_n = l_n \}
-   *      @f$, where @f$
-   *      \delta\{\mathrm{condition}\} = \left\{
-   *         \begin{array}{lr}
-   *            1 & \mbox{if condition} \\
-   *            0 & \mbox{otherwise}
-   *         \end{array} \right.
+   *   -# @f$ (M \times ...) @f$:
+   *      the reindexed array @f$
+   *        y = x_1[x_2]
    *      @f$
    */
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
-    }
     
-    #include <vector>
-    
-    
-    {}  // namespace caffe
-    
-    #endif  // CAFFE_CONCAT_LAYER_HPP_
+    #endif  // CAFFE_CONV_LAYER_HPP_
 
     
+            const std::unordered_map<StreamInformation, MinibatchData>& GetNextMinibatch(
+            size_t minibatchSizeInSamples,
+            size_t minibatchSizeInSequences,
+            size_t numberOfWorkers,
+            size_t workerRank,
+            const DeviceDescriptor& device = DeviceDescriptor::UseDefaultDevice()) override;
     
-    {} // namespace caffe2
-
-    
-    namespace caffe2 {
+        void Trainer::AddProgressWriters(const std::vector<ProgressWriterPtr>& progressWriters)
+    {
+        for (auto& learner : m_parameterLearners->ParameterLearners()) 
+        {
+            learner->AddProgressWriters(progressWriters);
+        }
+        m_progressWriters.insert(progressWriters.begin(), progressWriters.end());
     }
     
-    const AuthProperty AuthPropertyIterator::operator*() {
-  return std::pair<grpc::string_ref, grpc::string_ref>(
-      property_->name,
-      grpc::string_ref(property_->value, property_->value_length));
-}
-    
-    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
-  for (unsigned int i = 0; i < args_.size(); i++) {
-    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
-      return args_[i].value.string;
-    }
-  }
-  return '';
-}
-    
-    bool ProtoServerReflectionPlugin::has_async_methods() const {
-  if (reflection_service_) {
-    return reflection_service_->has_async_methods();
-  }
-  return false;
-}
-    
-    
-    {}  // namespace grpc
-    
-    #include <grpc/support/log.h>
-    
-    namespace grpc {
-namespace load_reporter {
-    }
+    // Exception wrapper to include native call stack string
+template <class E>
+class ExceptionWithCallStack : public E, public IExceptionWithCallStackBase
+{
+public:
+    ExceptionWithCallStack(const std::string& msg, const std::string& callstack) :
+        E(msg), m_callStack(callstack)
+    { }
     }
     
-    // Read through the first n keys repeatedly and check that they get
-// compacted (verified by checking the size of the key space).
-void AutoCompactTest::DoReads(int n) {
-  std::string value(kValueSize, 'x');
-  DBImpl* dbi = reinterpret_cast<DBImpl*>(db_);
+        double ElapsedSeconds();
+    
+                // replace input if needed
+            let iter = replacements.find(input);
+            if (iter != replacements.end())
+            {
+                assert(input->GetEnvironmentPtr()); // must be in some network if mapped
+                input = iter->second;
+                numRelinked++;
+                node->SetInput(i, input);
+            }
+    
+    template <class ElemType>
+EpochAccumulatorNode<ElemType>::EpochAccumulatorNode(DEVICEID_TYPE deviceId, const wstring& name)
+    : Base(deviceId, name), m_numSamples(0)
+{
+    m_accumulator = make_shared<Matrix<ElemType>>(deviceId);
+}
+    
+    protected:
+    vector<MemRequestInfo<float>> m_memRequestInfoFloatVec; 
+    vector<MemRequestInfo<double>> m_memRequestInfoDoubleVec;
+    vector<MemRequestInfo<half>> m_memRequestInfoHalfVec;
+    set<DEVICEID_TYPE> m_deviceIDSet; 
+    int m_stepCounter; 
+    
+    void DateCalculator::RaiseLiveRegionChangedAutomationEvent(_In_ bool isDateDiffMode)
+{
+    TextBlock^ resultTextBlock = (isDateDiffMode ? DateDiffAllUnitsResultLabel : DateResultLabel);
+    String^ automationName = AutomationProperties::GetName(resultTextBlock);
+    TextBlockAutomationPeer::FromElement(resultTextBlock)->RaiseAutomationEvent(AutomationEvents::LiveRegionChanged);
+}
+    
+    using namespace CalculatorApp::Common;
+using namespace Windows::Storage::Streams;
+    
+    //
+// CalculatorStandardOperators.xaml.h
+// Declaration of the CalculatorStandardOperators class
+//
+    
+            CalculatorApp::Calculator^ m_calculator;
+        CalculatorApp::UnitConverter^ m_converter;
+        CalculatorApp::DateCalculator^ m_dateCalculator;
+        Windows::Foundation::EventRegistrationToken _windowSizeEventToken;
+        Windows::Foundation::EventRegistrationToken m_hardwareButtonsBackPressedToken;
+        Windows::Foundation::EventRegistrationToken m_colorValuesChangedToken;
+        CalculatorApp::ViewModel::ApplicationViewModel^ m_model;
+        Windows::UI::ViewManagement::UISettings^ m_uiSettings;
+    
+        private:
+        Windows::UI::Xaml::Controls::MenuFlyout^ m_memoryItemFlyout;
+        Windows::Foundation::Rect m_visibleBounds;
+        Windows::Foundation::Rect m_coreBounds;
+        bool m_isErrorVisualState;
+    
+      void sendMessage();
+    
+      void setTaskQueue(DHTTaskQueue* taskQueue);
+    
+    
+    {} // namespace aria2
+    
+        uint16_t port;
+    auto connection = make_unique<DHTConnectionImpl>(family);
+    {
+      port = e->getBtRegistry()->getUdpPort();
+      const std::string& addr = e->getOption()->get(
+          family == AF_INET ? PREF_DHT_LISTEN_ADDR : PREF_DHT_LISTEN_ADDR6);
+      // If UDP port is already used, use the same port
+      // number. Normally IPv4 port is available, then IPv6 port is
+      // (especially for port >= 1024). We don't loose much by doing
+      // this. We did the same thing in TCP socket. See BtSetup.cc.
+      bool rv;
+      if (port == 0) {
+        auto sgl =
+            util::parseIntSegments(e->getOption()->get(PREF_DHT_LISTEN_PORT));
+        sgl.normalize();
+        rv = connection->bind(port, addr, sgl);
+      }
+      else {
+        rv = connection->bind(port, addr);
+      }
+      if (!rv) {
+        throw DL_ABORT_EX('Error occurred while binding UDP port for DHT');
+      }
+      localNode->setPort(port);
     }
+    A2_LOG_DEBUG(fmt('Initialized local node ID=%s',
+                     util::toHex(localNode->getID(), DHT_ID_LENGTH).c_str()));
+    auto tracker = std::make_shared<DHTMessageTracker>();
+    auto routingTable = make_unique<DHTRoutingTable>(localNode);
+    auto factory = make_unique<DHTMessageFactoryImpl>(family);
+    auto dispatcher = make_unique<DHTMessageDispatcherImpl>(tracker);
+    auto receiver = make_unique<DHTMessageReceiver>(tracker);
+    auto taskQueue = make_unique<DHTTaskQueueImpl>();
+    auto taskFactory = make_unique<DHTTaskFactoryImpl>();
+    auto peerAnnounceStorage = make_unique<DHTPeerAnnounceStorage>();
+    auto tokenTracker = make_unique<DHTTokenTracker>();
+    // For now, UDPTrackerClient was enabled along with DHT
+    auto udpTrackerClient = std::make_shared<UDPTrackerClient>();
+    const auto messageTimeout =
+        e->getOption()->getAsInt(PREF_DHT_MESSAGE_TIMEOUT);
+    // wiring up
+    tracker->setRoutingTable(routingTable.get());
+    tracker->setMessageFactory(factory.get());
     
-    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
-#define STORAGE_LEVELDB_DB_DB_ITER_H_
+    #include 'common.h'
     
-    void InternalKeyComparator::FindShortestSeparator(
-      std::string* start,
-      const Slice& limit) const {
-  // Attempt to shorten the user portion of the key
-  Slice user_start = ExtractUserKey(*start);
-  Slice user_limit = ExtractUserKey(limit);
-  std::string tmp(user_start.data(), user_start.size());
-  user_comparator_->FindShortestSeparator(&tmp, user_limit);
-  if (tmp.size() < user_start.size() &&
-      user_comparator_->Compare(user_start, tmp) < 0) {
-    // User key has become shorter physically, but larger logically.
-    // Tack on the earliest possible number to the shortened user key.
-    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
-    assert(this->Compare(*start, tmp) < 0);
-    assert(this->Compare(tmp, limit) < 0);
-    start->swap(tmp);
-  }
-}
-    
-    // Filter policy wrapper that converts from internal keys to user keys
-class InternalFilterPolicy : public FilterPolicy {
- private:
-  const FilterPolicy* const user_policy_;
- public:
-  explicit InternalFilterPolicy(const FilterPolicy* p) : user_policy_(p) { }
-  virtual const char* Name() const;
-  virtual void CreateFilter(const Slice* keys, int n, std::string* dst) const;
-  virtual bool KeyMayMatch(const Slice& key, const Slice& filter) const;
-};
-    
-    // Header is checksum (4 bytes), length (2 bytes), type (1 byte).
-static const int kHeaderSize = 4 + 2 + 1;
-    
-    
-    {  // Write the header and the payload
-  Status s = dest_->Append(Slice(buf, kHeaderSize));
-  if (s.ok()) {
-    s = dest_->Append(Slice(ptr, n));
-    if (s.ok()) {
-      s = dest_->Flush();
+    class DHTTaskQueue {
+public:
+  virtual ~DHTTaskQueue() = default;
     }
-  }
-  block_offset_ += kHeaderSize + n;
-  return s;
-}
-    
-      void SetComparatorName(const Slice& name) {
-    has_comparator_ = true;
-    comparator_ = name.ToString();
-  }
-  void SetLogNumber(uint64_t num) {
-    has_log_number_ = true;
-    log_number_ = num;
-  }
-  void SetPrevLogNumber(uint64_t num) {
-    has_prev_log_number_ = true;
-    prev_log_number_ = num;
-  }
-  void SetNextFile(uint64_t num) {
-    has_next_file_number_ = true;
-    next_file_number_ = num;
-  }
-  void SetLastSequence(SequenceNumber seq) {
-    has_last_sequence_ = true;
-    last_sequence_ = seq;
-  }
-  void SetCompactPointer(int level, const InternalKey& key) {
-    compact_pointers_.push_back(std::make_pair(level, key));
-  }
-    
-    int main(int argc, char** argv) {
-  return leveldb::test::RunAllTests();
-}
-
-    
-    
-#endif  // STORAGE_LEVELDB_DB_WRITE_BATCH_INTERNAL_H_
-
     
     public:
-    vector<int> preorderTraversal(TreeNode* root) {
-    }
+  DHTTaskQueueImpl();
     
-    
-    // exception type_error.304
-    try
-    {
-        // use at() on a non-object type
-        json str = 'I am a string';
-        str.at('the good') = 'Another string';
-    }
-    catch (json::type_error& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-        /* Sets the Eye value of the Camera. 
-     * 
-     * @param eye The Eye value of the Camera.
-     * @js NA
-     */
-    void setEye(const Vec3 &eye);
-    void setEye(float x, float y, float z);
-    /* Returns the Eye value of the Camera. 
-     *
-     * @return The Eye value of the Camera.
-     * @js NA
-     */
-    const Vec3& getEye() const { return _eye; }
-    /* Sets the Center value of the Camera. 
-     *
-     * @param center The Center value of the Camera.
-     * @js NA
-     */
-    void setCenter(const Vec3 &center);
-    /* Returns the Center value of the Camera. 
-     *
-     * @return The Center value of the Camera.
-     * @js NA
-     */
-    const Vec3& getCenter() const { return _center; }
-    /* Sets the Up value of the Camera. 
-     *
-     * @param up The Up value of the Camera.
-     * @js NA
-     */
-    void setUp(const Vec3 &up);
-    /* Returns the Up value of the Camera. 
-     *
-     * @return The Up value of the Camera.
-     * @js NA
-     */
-    const Vec3& getUp() const { return _up; }
-    
-    
-    {        p = current;
-    }
-    
-    CallFunc * CallFunc::reverse() const
+    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
+                                           const std::string& ipaddr,
+                                           uint16_t port) const
 {
-    // no reverse here, just return a clone
-    return this->clone();
+  return generateToken(infoHash, ipaddr, port, secret_[0]);
 }
-    
-    
-    {
-    {
-    {            if ((action->getFlags() & flags) != 0 && action->getOriginalTarget() == target)
-            {
-                removeActionAtIndex(i, element);
-                --limit;
-            }
-            else
-            {
-                ++i;
-            }
-        }
-    }
-}
-    
-        /** Gets an action given its tag an a target.
-     *
-     * @param tag       The action's tag.
-     * @param target    A certain target.
-     * @return  The Action the with the given tag.
-     */
-    virtual Action* getActionByTag(int tag, const Node *target) const;
-    
-                p.y = ( R + ay - ( r * (1 - cosBeta) * sinTheta));
-    
-        _tiles = (struct Tile *)new Tile[_tilesCount];
-    Tile *tileArray = (Tile*) _tiles;
