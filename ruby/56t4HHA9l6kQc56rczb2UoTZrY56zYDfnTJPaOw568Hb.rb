4f9c53be19f0a80ad78fe8f086e8e8bbc6abe190
SUNsung <weatherius@gmail.com>
2019-04-01 13:38:58 +0300

    Rewrite [56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb] File in: 2019-04-01 13:38:57

--- ruby/56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb
+++ ruby/56t4HHA9l6kQc56rczb2UoTZrY56zYDfnTJPaOw568Hb.rb
@@ -1,150 +1,126 @@
 
         
-            def unlock_instructions(record, token, opts={})
-      @token = token
-      devise_mail(record, :unlock_instructions, opts)
+            def value_for(user_id, date)
+      GivenDailyLike.find_for(user_id, date).pluck(:likes_given)[0] || 0
     end
     
-        get '/'
-    assert last_response.ok?
-  end
-    
-              path
-        end
+          it 'deactivates an existing user' do
+        visit admin_users_path
+        expect(page).to have_no_text('inactive')
+        find(:css, 'a[href='/admin/users/#{users(:bob).id}/deactivate']').click
+        expect(page).to have_text('inactive')
+        users(:bob).reload
+        expect(users(:bob)).not_to be_active
       end
     
-          def template_paths
-        template_path = _prefixes.dup
-        template_path.unshift '#{@devise_mapping.scoped_path}/mailer' if self.class.scoped_views?
-        template_path
-      end
+      context 'with associated agents' do
+    let!(:bob_scheduler_agent) {
+      Agents::SchedulerAgent.create!(
+        user: users(:bob),
+        name: 'Example Scheduler',
+        options: {
+          'action' => 'run',
+          'schedule' => '0 * * * *'
+        },
+      )
+    }
     
-            # The token is only valid if:
-        # 1. we have a date
-        # 2. the current time does not pass the expiry period
-        # 3. the record has a remember_created_at date
-        # 4. the token date is bigger than the remember_created_at
-        # 5. the token matches
-        generated_at.is_a?(Time) &&
-         (self.class.remember_for.ago < generated_at) &&
-         (generated_at > (remember_created_at || Time.now).utc) &&
-         Devise.secure_compare(rememberable_value, token)
-      end
-    
-      def process_push_request
-    case hub_mode
-    when 'subscribe'
-      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
-    when 'unsubscribe'
-      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
-    else
-      ['Unknown mode: #{hub_mode}', 422]
+        it 'strips punctuation' do
+      expect(AgentsExporter.new(:name => 'foo,bar').filename).to eq('foo-bar.json')
     end
-  end
     
-      def subscription
-    @_subscription ||= @account.subscription(
-      api_subscription_url(@account.id)
-    )
-  end
+    describe LiquidMigrator do
+  describe 'converting JSONPath strings' do
+    it 'should work' do
+      expect(LiquidMigrator.convert_string('$.data', true)).to eq('{{data}}')
+      expect(LiquidMigrator.convert_string('$.data.test', true)).to eq('{{data.test}}')
+      expect(LiquidMigrator.convert_string('$first_title', true)).to eq('{{first_title}}')
+    end
     
-          if @user.persisted?
-        sign_in_and_redirect @user, event: :authentication
-        set_flash_message(:notice, :success, kind: provider_id.capitalize) if is_navigational_format?
-      else
-        session['devise.#{provider}_data'] = request.env['omniauth.auth']
-        redirect_to new_user_registration_url
-      end
+        it 'accepts objects as well as strings' do
+      log = AgentLog.log_for_agent(agents(:jane_website_agent), events(:bob_website_agent_event).payload)
+      expect(log.message).to include(''title'=>'foo'')
     end
   end
     
-      def set_rate_limit_headers
-    apply_header_limit
-    apply_header_remaining
-    apply_header_reset
-  end
+        def sort_fn(a, b)
+      if (a.getbyte(0) >= 49 && a.getbyte(0) <= 57) || (b.getbyte(0) >= 49 && b.getbyte(0) <= 57)
+        a_split = a.split(SPLIT_INTS)
+        b_split = b.split(SPLIT_INTS)
     
-          # @see Base#\_retrieve
-      def _retrieve(key, version, sha)
-        return unless File.readable?(path_to(key))
-        begin
-          File.open(path_to(key), 'rb') do |f|
-            if f.readline('\n').strip == version && f.readline('\n').strip == sha
-              return f.read
-            end
+        def name=(value)
+      @name = value.try :strip
+    end
+    
+        delegate :empty?, :blank?, to: :pages
+    
+          str.truncate(max_length).ljust(max_length) << tag.to_s
+    end
+    
+        def translation_scope
+      'devise.unlocks'
+    end
+end
+
+    
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
+    end
+    
+          def mailer_reply_to(mapping)
+        mailer_sender(mapping, :reply_to)
+      end
+    
+              def #{accessor}=(value)
+            @#{accessor} = value
           end
-          File.unlink path_to(key)
-        rescue Errno::ENOENT
-          # Already deleted. Race condition?
-        end
-        nil
-      rescue EOFError, TypeError, ArgumentError => e
-        Sass::Util.sass_warn 'Warning. Error encountered while reading cache #{path_to(key)}: #{e}'
+        METHOD
       end
-    
-        # Create a {Sass::Selector::CommaSequence} containing only a single
-    # {Sass::Selector::Sequence}.
-    #
-    # @param sseqs [Array<Sass::Selector::Sequence, String>]
-    # @return [Sass::Selector::CommaSequence]
-    def make_seq(*sseqs)
-      make_cseq(Sass::Selector::Sequence.new(sseqs))
     end
     
-        # The name of the file in which the exception was raised.
-    # This could be `nil` if no filename is available.
-    #
-    # @return [String, nil]
-    def sass_filename
-      sass_backtrace.first[:filename]
+      # A identifier representing the license. Any string is fine.
+  attr_accessor :license
+    
+          until(file.eof?() || empty_records == 2)
+    
+      dont_obsolete_paths = []
+  option '--dont-obsolete', 'DONT_OBSOLETE_PATH',
+    'A file path for which to 'dont-obsolete' in the built PackageInfo. ' \
+    'Can be specified multiple times.' do |path|
+      dont_obsolete_paths << path
     end
     
-        # @param args [Array<String>] The command-line arguments
-    def initialize(args, default_syntax)
-      super(args)
-      @options[:sourcemap] = :auto
-      @options[:for_engine] = {
-        :load_paths => default_sass_path
-      }
-      @default_syntax = default_syntax
+        File.write(build_path('packlist'), files.sort.join('\n'))
+    
+        scripts[:after_install] = template(File.join('pleaserun', 'scripts', 'after-install.sh')).result(binding)
+    scripts[:before_remove] = template(File.join('pleaserun', 'scripts', 'before-remove.sh')).result(binding)
+  end # def input
+    
+        if File.exists?(params[:output])
+      # TODO(sissel): Allow folks to choose output?
+      logger.error('Puppet module directory '#{params[:output]}' already ' \
+                    'exists. Delete it or choose another output (-p flag)')
     end
     
-    Gem::Specification.new do |gem|
-  gem.name          = 'capistrano'
-  gem.version       = Capistrano::VERSION
-  gem.authors       = ['Tom Clements', 'Lee Hambley']
-  gem.email         = ['seenmyfate@gmail.com', 'lee.hambley@gmail.com']
-  gem.description   = 'Capistrano is a utility and framework for executing commands in parallel on multiple remote machines, via SSH.'
-  gem.summary       = 'Capistrano - Welcome to easy deployment with Ruby over SSH'
-  gem.homepage      = 'http://capistranorb.com/'
+      # Returns the path to the tar file containing the packed up staging directory
+  def payload
+    payload_tar = build_path('payload.tar')
+    logger.info('Creating payload tar ', :path => payload_tar)
     
-        def dry_run
-      ['--dry-run', '-n',
-       'Do a dry run without executing actions',
-       lambda do |_value|
-         Configuration.env.set(:sshkit_backend, SSHKit::Backend::Printer)
-       end]
-    end
-    
-          # rubocop:disable Security/MarshalLoad
-      def add_role(role, hosts, options={})
-        options_deepcopy = Marshal.dump(options.merge(roles: role))
-        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
+          case
+      when value.is_a?(String), value.is_a?(Symbol)
+        %W(--#{opt} #{value})
+      when value.is_a?(Array)
+        value.map { |v| %W(--#{opt} #{v}) }
+      when value.is_a?(TrueClass)
+        '--#{opt}'
+      when value.is_a?(FalseClass)
+        '--no-#{opt}'
+      else
+        fail TypeError, 'Unexpected type: #{value.class}'
       end
-      # rubocop:enable Security/MarshalLoad
-    
-            value_to_evaluate = block || value
-    
-          it 'autocorrects closing brace on same line from last element' do
-        new_source = autocorrect_source(construct(true, false))
-    
-          # Checks whether this node body is a void context.
-      # Always `true` for `for`.
-      #
-      # @return [true] whether the `for` node body is a void context
-      def void_context?
-        true
-      end
-    
-              delta(first.key.loc, second.key.loc, alignment)
-        end
+    end
+  end
