9ab6921c4d379a5959a80f2553f4e74a4b5664cc
SUNsung <weatherius@gmail.com>
2019-04-02 13:16:01 +0300

    Rewrite [45QzYtMDMh9tI457EFHsjqVSo45htAUaxkfQWw45YwY.py] File in: 2019-04-02 13:16:00

--- pyton/45QzYtMDMh9tI457EFHsjqVSo45htAUaxkfQWw45YwY.py
+++ pyton/45QzYtMDMh9tI457EFHsjqVSo45htAUaxkfQWw45YwY.py
@@ -1,273 +1,245 @@
 
         
         
-@pytest.fixture(params=[(python_3, False),
-                        (python_3, True),
-                        (python_2, False)])
-def proc(request, spawnu, TIMEOUT):
-    container, instant_mode = request.param
-    proc = spawnu(*container)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(init_bashrc.format(
-        u'--enable-experimental-instant-mode' if instant_mode else ''))
-    proc.sendline(u'bash')
-    if instant_mode:
-        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
-    return proc
-    
-    Options:
-  -h  This help text.
-  -q  Loggable output - no progress indicator
-  -qq No output except for errors
-  -d  Download only - do NOT install or unpack archives
-  -s  No-act. Perform ordering simulation
-  -y  Assume Yes to all queries and do not prompt
-  -f  Attempt to correct a system with broken dependencies in place
-  -m  Attempt to continue if archives are unlocatable
-  -u  Show a list of upgraded packages as well
-  -b  Build the source package after fetching it
-  -V  Show verbose version numbers
-  -c=? Read this configuration file
-  -o=? Set an arbitrary configuration option, eg -o dir::cache=/tmp
-See the apt-get(8), sources.list(5) and apt.conf(5) manual
-pages for more information and options.
-                       This APT has Super Cow Powers.
-'''
-apt_get_operations = ['update', 'upgrade', 'install', 'remove', 'autoremove',
-                      'purge', 'source', 'build-dep', 'dist-upgrade',
-                      'dselect-upgrade', 'clean', 'autoclean', 'check',
-                      'changelog', 'download']
+def test_logout(client, auth):
+    auth.login()
     
     
-@pytest.mark.parametrize('command', [
-    Command('apt list --upgradable', no_match_output),
-    Command('sudo apt list --upgradable', no_match_output)
-])
-def test_not_match(command):
-    assert not match(command)
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
     
     
-@pytest.mark.parametrize('command, result', [
-    (Command('aws dynamdb scan', misspelled_command),
-     ['aws dynamodb scan']),
-    (Command('aws dynamodb scn', misspelled_subcommand),
-     ['aws dynamodb scan']),
-    (Command('aws dynamodb t-item',
-             misspelled_subcommand_with_multiple_options),
-     ['aws dynamodb put-item', 'aws dynamodb get-item'])])
-def test_get_new_command(command, result):
-    assert get_new_command(command) == result
-
-    
-    
-@pytest.mark.parametrize('script, output', [
-    ('brew link sshfs', output),
-    ('cat output', output),
-    ('brew install sshfs', '')])
-def test_not_match(script, output):
-    command = Command(script, output)
-    assert not match(command)
-    
-    
-@pytest.mark.skipif(_is_not_okay_to_test(),
-                    reason='No need to run if there\'s no formula')
-def test_get_new_command(brew_no_available_formula):
-    assert get_new_command(Command('brew install elsticsearch',
-                                   brew_no_available_formula))\
-        == 'brew install elasticsearch'
-    
-    
-class Header(jose.Header):
-    '''ACME-specific JOSE Header. Implements nonce, kid, and url.
+class FormDataRoutingRedirect(AssertionError):
+    '''This exception is raised by Flask in debug mode if it detects a
+    redirect caused by the routing system when the request method is not
+    GET, HEAD or OPTIONS.  Reasoning: form data will be dropped.
     '''
-    nonce = jose.Field('nonce', omitempty=True, encoder=jose.encode_b64jose)
-    kid = jose.Field('kid', omitempty=True)
-    url = jose.Field('url', omitempty=True)
-    
-        def test_kid_serialize(self):
-        from acme.jws import JWS
-        jws = JWS.sign(payload=b'foo', key=self.privkey,
-                       alg=jose.RS256, nonce=self.nonce,
-                       url=self.url, kid=self.kid)
-        self.assertEqual(jws.signature.combined.nonce, self.nonce)
-        self.assertEqual(jws.signature.combined.url, self.url)
-        self.assertEqual(jws.signature.combined.kid, self.kid)
-        self.assertEqual(jws.signature.combined.jwk, None)
-        # TODO: check that nonce is in protected header
     
     
-class AugeasConfigurator(common.Installer):
-    '''Base Augeas Configurator class.
+def _find_app():
+    top = _app_ctx_stack.top
+    if top is None:
+        raise RuntimeError(_app_ctx_err_msg)
+    return top.app
     
-            =========================================  =====
-        ``127.0.0.1:\*.conflicts(127.0.0.1:443)``  True
-        ``127.0.0.1:443.conflicts(127.0.0.1:\*)``  False
-        ``\*:443.conflicts(\*:80)``                False
-        ``_default_:443.conflicts(\*:443)``        True
-        =========================================  =====
+        If there is no handler for the logger's effective level, add a
+    :class:`~logging.StreamHandler` for
+    :func:`~flask.logging.wsgi_errors_stream` with a basic format.
+    '''
+    logger = logging.getLogger('flask.app')
     
     
-# I have written my code naively same as definition of primitive root
-# however every time I run this program, memory exceeded...
-# so I used 4.80 Algorithm in Handbook of Applied Cryptography(CRC Press, ISBN : 0-8493-8523-7, October 1996)
-# and it seems to run nicely!
-def primitiveRoot(p_val):
-    print('Generating primitive root of p')
-    while True:
-        g = random.randrange(3,p_val)
-        if pow(g, 2, p_val) == 1:
+class Environment(BaseEnvironment):
+    '''Works like a regular Jinja2 environment but has some additional
+    knowledge of how Flask's blueprint works so that it can prepend the
+    name of the blueprint to referenced templates if necessary.
+    '''
+    
+        paths = []
+    for fname in files:
+        paths.append(get_file(fname,
+                              origin=base + fname,
+                              cache_subdir=dirname))
+    
+    
+# Aliases.
+    
+        y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
+    
+        E.g. if your `batch_size` is 64 and you use `gpus=2`,
+    then we will divide the input into 2 sub-batches of 32 samples,
+    process each sub-batch on one GPU, then return the full
+    batch of 64 processed samples.
+    
+    model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+    
+        return out
+    
+        def test_copy(self):
+        copy = self.case_insensitive_dict.copy()
+        assert copy is not self.case_insensitive_dict
+        assert copy == self.case_insensitive_dict
+    
+        return {
+        'platform': platform_info,
+        'implementation': implementation_info,
+        'system_ssl': system_ssl_info,
+        'using_pyopenssl': pyopenssl is not None,
+        'pyOpenSSL': pyopenssl_info,
+        'urllib3': urllib3_info,
+        'chardet': chardet_info,
+        'cryptography': cryptography_info,
+        'idna': idna_info,
+        'requests': {
+            'version': requests_version,
+        },
+    }
+    
+                # Look for requests environment configuration and be compatible
+            # with cURL.
+            if verify is True or verify is None:
+                verify = (os.environ.get('REQUESTS_CA_BUNDLE') or
+                          os.environ.get('CURL_CA_BUNDLE'))
+    
+        :param value: the header value to unquote.
+    :rtype: str
+    '''
+    if value and value[0] == value[-1] == ''':
+        # this is not the real unquoting, but fixing this so that the
+        # RFC is met will result in bugs with internet explorer and
+        # probably some other browsers as well.  IE for example is
+        # uploading files with 'C:\foo\bar.txt' as filename
+        value = value[1:-1]
+    
+        Code is a simple port of what is already in the /scripts directory
+    
+            cert.sign(CertUtil.ca_privatekey, CertUtil.ca_digest)
+    
+            if response.status not in self.config.check_ip_accept_status:
+            return False
+    
+    
+#try:
+#    from code.default.gae_proxy.local.config import config
+#except:
+#    from code.default.gae_proxy.local.config import config
+from config import config
+    
+        if __password__ and __password__ != kwargs.get('password', ''):
+        start_response('403 Forbidden', [('Content-Type', 'text/html')])
+        yield message_html('403 Wrong password', 'Wrong password(%r)' % kwargs.get('password', ''), 'GoAgent proxy.ini password is wrong!')
+        raise StopIteration
+    
+        def __init__(self, child_parts):
+        self.child_parts = child_parts
+        if child_parts:
+            self.successor = _make_selector(child_parts)
+            self.dironly = True
+        else:
+            self.successor = _TerminatingSelector()
+            self.dironly = False
+    
+        for type, token, start, end, line in token_generator:
+        if type == ENDMARKER:
+            break
+        # Ignore the new line on the last line if the input lacks one
+        if missing_trailing_nl and type == NEWLINE and end[0] == num_lines:
             continue
-        if pow(g, p_val, p_val) == 1:
-            continue
-        return g
+        type = tok_name[type]
+        result.append(f'    {type:10} {token!r:13} {start} {end}')
     
-    This is a pure Python implementation of Dynamic Programming solution to the longest increasing subsequence of a given sequence.
+        @bigmemtest(size=_4G + 100, memuse=2)
+    def testCompress4G(self, size):
+        # 'Test BZ2Compressor.compress()/flush() with >4GiB input'
+        bz2c = BZ2Compressor()
+        data = b'x' * size
+        try:
+            compressed = bz2c.compress(data)
+            compressed += bz2c.flush()
+        finally:
+            data = None  # Release memory
+        data = bz2.decompress(compressed)
+        try:
+            self.assertEqual(len(data), size)
+            self.assertEqual(len(data.strip(b'x')), 0)
+        finally:
+            data = None
+    
+        def __str__(self):
+        return self.state
+    
+    *What does this example do?
+The code shows a way to localize words in two languages: English and
+Greek. 'getLocalizer' is the factory method that constructs a
+localizer depending on the language chosen. The localizer object will
+be an instance from a different class according to the language
+localized. However, the main code does not have to worry about which
+localizer will be instantiated, since the method 'get' will be called
+in the same way independently of the language.
     
     
 if __name__ == '__main__':
-    whole_array = input('please input some numbers:')
-    array = SubArray(whole_array)
-    re = array.solve_sub_array()
-    print(('the results is:', re))
+    main()
     
     
-__all__ = [
-    'Package',
-    'Resource',
-    'contents',
-    'is_resource',
-    'open_binary',
-    'open_text',
-    'path',
-    'read_binary',
-    'read_text',
-    ]
-    
-        def test_set_pycache_prefix(self):
-        # sys.pycache_prefix can be set from either -X pycache_prefix or
-        # PYTHONPYCACHEPREFIX env var, with the former taking precedence.
-        NO_VALUE = object()  # `-X pycache_prefix` with no `=PATH`
-        cases = [
-            # (PYTHONPYCACHEPREFIX, -X pycache_prefix, sys.pycache_prefix)
-            (None, None, None),
-            ('foo', None, 'foo'),
-            (None, 'bar', 'bar'),
-            ('foo', 'bar', 'bar'),
-            ('foo', '', None),
-            ('foo', NO_VALUE, None),
-        ]
-        for envval, opt, expected in cases:
-            exp_clause = 'is None' if expected is None else f'== '{expected}''
-            code = f'import sys; sys.exit(not sys.pycache_prefix {exp_clause})'
-            args = ['-c', code]
-            env = {} if envval is None else {'PYTHONPYCACHEPREFIX': envval}
-            if opt is NO_VALUE:
-                args[:0] = ['-X', 'pycache_prefix']
-            elif opt is not None:
-                args[:0] = ['-X', f'pycache_prefix={opt}']
-            with self.subTest(envval=envval, opt=opt):
-                with support.temp_cwd():
-                    assert_python_ok(*args, **env)
-    
-    # Add the html version.  This converts the message into a multipart/alternative
-# container, with the original text message as the first part and the new html
-# message as the second part.
-asparagus_cid = make_msgid()
-msg.add_alternative('''\
-<html>
-  <head></head>
-  <body>
-    <p>Salut!</p>
-    <p>Cela ressemble à un excellent
-        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
-            recipie
-        </a> déjeuner.
-    </p>
-    <img src='cid:{asparagus_cid}' />
-  </body>
-</html>
-'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
-# note that we needed to peel the <> off the msgid for use in the html.
-    
-    def handleSlideTitle(title):
-    print('<h2>%s</h2>' % getText(title.childNodes))
-    
-    print('Enter your SQL commands to execute in sqlite3.')
-print('Enter a blank line to exit.')
-    
-    import os, json, imp
-here = os.path.abspath(os.path.dirname(__file__))
-proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
-try:
-    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
-except:
-    README = ''
-CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
-VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
-    
-    def baomihua_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-    html = get_html(url)
-    title = r1(r'<title>(.*)</title>', html)
-    assert title
-    id = r1(r'flvid\s*=\s*(\d+)', html)
-    assert id
-    baomihua_download_by_id(id, title, output_dir=output_dir, merge=merge, info_only=info_only)
-    
-    	type, ext, size = url_info(url)
-	print_info(site_info, title, type, size)
-	
-	if not info_only:
-		download_urls([url], title, ext, size, output_dir, merge = merge)
+class Prototype(object):
     
     
-def get_mobile_room_url(room_id):
-    return 'http://www.huomao.com/mobile/mob_live/%s' % room_id
+# example of graph usage
+graph = {'A': ['B', 'C'], 'B': ['C', 'D'], 'C': ['D'], 'D': ['C'], 'E': ['F'], 'F': ['C']}
     
-        def to_sec(course_id, chap_id, mod):
-        ep = 'http://www.icourses.cn/jpk/viewCharacterDetail2.action?courseId={}&characId={}&mod={}'
-        req = post_content(ep.format(course_id, chap_id, mod), post_data={})
-        return req
-    
-        html = get_content(url)
-    uuid_pattern = r''([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})''
-    id = r1(r'var vid='([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})'', html)
-    if id is None:
-        video_pattern = r''vid'\s*:\s*' + uuid_pattern
-        id = match1(html, video_pattern)
-    assert id, 'can't find video info'
-    return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
+        def on_switchover(self):
+        super(Standby, self).on_switchover()  # message ignored
+        self._hsm._next_state('active')
     
     
-# def get_all_subset(tokens: list):
-#     '''获取 tokens 的所有非空子集'''
-#     tokens = set(tokens)
-#
-#     ret = []
-#     for i in range(1, len(tokens) + 1):
-#         ret.extend(list(combinations(tokens, i)))
-#
-#     return ret
+class TabletView(object):
+    def show_index_page(self):
+        print('Displaying tablet index page')
     
     
-def relu(x):
-    '''ReLU
-    `o = max(0., x)`
-    '''
-    return tf.nn.relu(x)
+class Proxy:
+    def __init__(self):
+        self.busy = 'No'
+        self.sales = None
     
-            o = tf.nn.conv2d(x, W, strides=strides, padding=padding) + b
-        o = act_fn(o)
+        def test_parents(self):
+        for _ in range(2):
+            self.assertEqual(self.John.parents, 'Father and mother')
+        self.assertEqual(self.John.call_count2, 1)
+
     
-        def __call__(self, x):
-        '''
-        Args:
-            x: 注意 x.dtype == float32
-        '''
-        # x = tf.cast(x, dtype=tf.float32)  # 交给外部处理
-        loss_regularization = 0.
-        if self.l1:
-            loss_regularization += tf.reduce_sum(self.l1 * tf.abs(x))
-        if self.l2:
-            loss_regularization += tf.reduce_sum(self.l2 * tf.square(x))
-        return loss_regularization
+        def test_car_adapter_shall_make_loud_noise(self):
+        car = Car()
+        car_adapter = Adapter(car, make_noise=car.make_noise)
+        noise = car_adapter.make_noise(1)
+        expected_noise = 'vroom!'
+        self.assertEqual(noise, expected_noise)
+    
+        result = False
+    try:
+        ret = urlopen(url, timeout=2)
+        result = (ret.code == 200)
+    except HTTPError as e:
+        print(e, file=sys.stderr)
+    except URLError as e:
+        print(e, file=sys.stderr)
+    except timeout as e:
+        print(e, file=sys.stderr)
+    except Exception as e:
+        print(e, file=sys.stderr)
+    
+    
+class BufferFull(UnpackException):
+    pass
+    
+    
+def testSimpleValue():
+    check(b'\x93\xc0\xc2\xc3', (None, False, True, ))
+    
+            exp1 = '''Series([], dtype: timedelta64[ns])'''
+    
+    
+def test_setitem_callable():
+    # GH 12533
+    s = pd.Series([1, 2, 3, 4], index=list('ABCD'))
+    s[lambda x: 'A'] = -1
+    tm.assert_series_equal(s, pd.Series([-1, 2, 3, 4], index=list('ABCD')))
