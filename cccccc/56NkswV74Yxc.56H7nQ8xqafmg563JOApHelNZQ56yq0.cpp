d4254b693433434a827499f307f6a93c872069c8
SUNsung <weatherius@gmail.com>
2019-04-02 10:11:59 +0300

    Rewrite [56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp] File in: 2019-04-02 10:11:59

--- cccccc/56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp
+++ cccccc/56NkswV74Yxc.56H7nQ8xqafmg563JOApHelNZQ56yq0.cpp
@@ -1,148 +1,273 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+        
+    {}  // namespace atom
     
-    
-    {
-    {    output() = 'A(m, 0) == A(m-1, 1)';
-  }
-};
-    
-    namespace tensorflow {
+    namespace atom {
     }
     
-    // Must be included first
-#include 'tensorflow/python/lib/core/numpy.h'
+      v8::Local<v8::Value> URLRequest(v8::Isolate* isolate);
     
-    #include 'tensorflow/core/framework/tensor.h'
-#include 'tensorflow/core/lib/core/status.h'
-    
-    struct TFTensorDeleter {
-  void operator()(TF_Tensor* p) const { TF_DeleteTensor(p); }
-};
-    
-    namespace stream_executor {
-namespace cuda {
-    }
-    }
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      unsigned flags() const { return flags_; }
-    
-    void add(const Size2D &size,
-         const u8 * src0Base, ptrdiff_t src0Stride,
-         const s16 * src1Base, ptrdiff_t src1Stride,
-         s16 *dstBase, ptrdiff_t dstStride,
-         CONVERT_POLICY policy)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    size_t roiw16 = size.width >= 15 ? size.width - 15 : 0;
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
-    
-    namespace {
-    }
-    
-            for (; i < roiw4; i += 4 )
-        {
-            internal::prefetch(src + i);
-            float32x4_t vln = vld1q_f32(src + i);
-            int32x4_t vnz = vreinterpretq_s32_u32(vmvnq_u32(vceqq_f32(vln, vc0)));
-            vs = vqaddq_s32(vs, vnz);
-        }
-    
-    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
-inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
-inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
-    
-      /**
-   * @brief Applies the transformation defined in the data layer's
-   * transform_param block to a vector of Datum.
-   *
-   * @param datum_vector
-   *    A vector of Datum containing the data to be transformed.
-   * @param transformed_blob
-   *    This is destination blob. It can be part of top blob's data if
-   *    set_cpu_data() is used. See memory_layer.cpp for an example.
-   */
-  void Transform(const vector<Datum> & datum_vector,
-                Blob<Dtype>* transformed_blob);
-    
-    
-    { private:
-  DISABLE_COPY_AND_ASSIGN(Layer);
-};  // class Layer
-    
-    namespace caffe {
-    }
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/data_transformer.hpp'
-#include 'caffe/internal_thread.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-#include 'caffe/util/blocking_queue.hpp'
-    
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    DelayWriteToken::~DelayWriteToken() {
-  controller_->total_delayed_--;
-  assert(controller_->total_delayed_.load() >= 0);
+    void Event::RenderFrameDeleted(content::RenderFrameHost* rfh) {
+  if (sender_ != rfh)
+    return;
+  sender_ = nullptr;
+  message_ = nullptr;
 }
     
-      // close DB
-  delete cf;
-  delete db;
+      base::WeakPtrFactory<TrackableObjectBase> weak_factory_;
     
-      int ret = system('rm -rf /tmp/rocksmergetest');
-  if (ret != 0) {
-    fprintf(stderr, 'Error deleting /tmp/rocksmergetest, code: %d\n', ret);
-    return ret;
-  }
-  rocksdb::Options options;
-  options.create_if_missing = true;
-  options.merge_operator.reset(new MyMerge);
-  options.compaction_filter = &filter;
-  status = rocksdb::DB::Open(options, '/tmp/rocksmergetest', &raw_db);
-  assert(status.ok());
-  std::unique_ptr<rocksdb::DB> db(raw_db);
     
-      // destroy and open DB
-  DB* db;
-  Status s = DestroyDB(kDBPath, Options(db_opt, cf_descs[0].options));
-  assert(s.ok());
-  s = DB::Open(Options(db_opt, cf_descs[0].options), kDBPath, &db);
-  assert(s.ok());
+    {}  // namespace atom
+
     
-    /*
- * ThreadPool is a component that will spawn N background threads that will
- * be used to execute scheduled work, The number of background threads could
- * be modified by calling SetBackgroundThreads().
- * */
-class ThreadPool {
- public:
-  virtual ~ThreadPool() {}
+      // URLRequestJob:
+  void Start() override;
+  void Kill() override;
+  bool GetMimeType(std::string* mime_type) const override;
+    
+      int parent_pid = getppid();
+    
+      // Join an existing circle.
+  void join(linked_ptr_internal const* ptr)
+      GTEST_LOCK_EXCLUDED_(g_linked_ptr_mutex) {
+    MutexLock lock(&g_linked_ptr_mutex);
     }
     
-    namespace rocksdb {
+    
+    {  // <TechnicalDetails>
+  //
+  // EXPECT_EQ(expected, actual) is the same as
+  //
+  //   EXPECT_TRUE((expected) == (actual))
+  //
+  // except that it will print both the expected value and the actual
+  // value when the assertion fails.  This is very helpful for
+  // debugging.  Therefore in this case EXPECT_EQ is preferred.
+  //
+  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
+  // and is thus more general.
+  //
+  // </TechnicalDetails>
+}
+    
+    
+    {	CleanUp:
+	
+	float32* xres = new float32[nResultVecs];
+	float32* yres = new float32[nResultVecs];
+	for (int32 i=0; i<nResultVecs; ++i){
+		xres[i] = resultVecs[i].x;
+		yres[i] = resultVecs[i].y;
+	}
+	b2Polygon retval(xres,yres,nResultVecs);
+	delete[] resultVecs;
+	delete[] yres;
+	delete[] xres;
+	delete[] nodes;
+	return retval;
+}
+    
+    
+    {	}
+    
+    		static const unsigned int ROWS = 4;
+		static const unsigned int COLUMNS = 4;
+		static const unsigned int PIXELS = ROWS * COLUMNS;
+    
+    
+#define AF_LATIN_BLUE_ACTIVE      ( 1U << 0 ) /* zone height is <= 3/4px   */
+#define AF_LATIN_BLUE_TOP         ( 1U << 1 ) /* we have a top blue zone   */
+#define AF_LATIN_BLUE_SUB_TOP     ( 1U << 2 ) /* we have a subscript top   */
+                                              /* blue zone                 */
+#define AF_LATIN_BLUE_NEUTRAL     ( 1U << 3 ) /* we have neutral blue zone */
+#define AF_LATIN_BLUE_ADJUSTMENT  ( 1U << 4 ) /* used for scale adjustment */
+                                              /* optimization              */
+    
+    #ifdef FIXED_DEBUG
+#include 'fixed_debug.h'
+#else
+    
+    #define ADD32(a, b) ADD32_(a, b, __FILE__, __LINE__)
+static OPUS_INLINE int ADD32_(opus_int64 a, opus_int64 b, char *file, int line)
+{
+   opus_int64 res;
+   if (!VERIFY_INT(a) || !VERIFY_INT(b))
+   {
+      fprintf (stderr, 'ADD32: inputs are not int: %d %d in %s: line %d\n', (int)a, (int)b, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = a+b;
+   if (!VERIFY_INT(res))
+   {
+      fprintf (stderr, 'ADD32: output is not int: %d in %s: line %d\n', (int)res, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips+=2;
+   return res;
+}
+    
+    #define silk_LIMIT( a, limit1, limit2)      ((limit1) > (limit2) ? ((a) > (limit1) ? (limit1) : ((a) < (limit2) ? (limit2) : (a))) \
+                                                                 : ((a) > (limit2) ? (limit2) : ((a) < (limit1) ? (limit1) : (a))))
+    
+    using namespace Platform;
+using namespace Platform::Collections;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Globalization;
+using namespace Windows::Globalization::DateTimeFormatting;
+using namespace Windows::System::UserProfile;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::ViewManagement;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Automation;
+using namespace Windows::UI::Xaml::Automation::Peers;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+    
+    NavCategoryGroup^ NavCategoryGroup::CreateCalculatorCategory()
+{
+    return ref new NavCategoryGroup(s_categoryGroupManifest.at(0));
+}
+    
+    
+    {    if (FAILED(hr))
+    {
+        return LocalizationService::GetNarratorReadableString(fallbackExpression);
+    }
+    else
+    {
+        return ref new String(accExpression.str().c_str());
+    }
+}
+
+    
+    TitleBarHelper::TitleBarHelper(_In_ CoreApplicationViewTitleBar^ coreTitleBar, _In_ FrameworkElement^ customTitleBar) :
+    m_coreTitleBar(coreTitleBar),
+    m_customTitleBar(customTitleBar)
+{
+    RegisterForLayoutChanged();
+    RegisterForVisibilityChanged();
+    SetCustomTitleBar();
+}
+    
+    //
+// CalculatorStandardOperators.xaml.h
+// Declaration of the CalculatorStandardOperators class
+//
+    
+    
+    {    // When the user clears the history list in the overlay view and presses enter, the clickedItem is nullptr
+    if (clickedItem != nullptr)
+    {
+        historyVM->ShowItem(clickedItem);
+    }
+}
+    
+        private:
+        Windows::Foundation::Rect m_visibleBounds;
+        Windows::Foundation::Rect m_coreBounds;
+        void ListView_ItemClick(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::Controls::ItemClickEventArgs^ e);
+        void OnDeleteMenuItemClicked(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::RoutedEventArgs^ e);
+        void OnDeleteSwipeInvoked(_In_ Microsoft::UI::Xaml::Controls::SwipeItem^ sender, _In_ Microsoft::UI::Xaml::Controls::SwipeItemInvokedEventArgs^ e);
+    
+    void Memory::OnContextRequested(Windows::UI::Xaml::UIElement^ sender, Windows::UI::Xaml::Input::ContextRequestedEventArgs^ e)
+{
+    // Walk up the tree to find the ListViewItem.
+    // There may not be one if the click wasn't on an item.
+    auto requestedElement = safe_cast<FrameworkElement^>(e->OriginalSource);
+    while ((requestedElement != sender) && !dynamic_cast<ListViewItem^>(requestedElement))
+    {
+        requestedElement = safe_cast<FrameworkElement^>(VisualTreeHelper::GetParent(requestedElement));
+    }
+    }
+    
+    #include 'pch.h'
+#include 'MemoryListItem.xaml.h'
+#include 'Controls/CalculatorButton.h'
+    
+    ActionCamera * ActionCamera::reverse() const
+{
+    // FIXME: This conversion isn't safe.
+    return (ActionCamera*)ReverseTime::create(const_cast<ActionCamera*>(this));
+}
+    
+    /* Implementation of CardinalSplineTo
+ */
+    
+    
+    {    CC_SAFE_DELETE(easeRateAction);
+    return nullptr;
+}
+    
+    bool AccelAmplitude::initWithAction(Action *action, float duration)
+{
+    if (ActionInterval::initWithDuration(duration))
+    {
+        _rate = 1.0f;
+        _other = (ActionInterval*)(action);
+        action->retain();
+    }
+    }
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    NS_CC_BEGIN
+    
+    ActionTween *ActionTween::clone() const
+{
+    return ActionTween::create(_duration, _key, _from, _to);
+}
+    
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+    AnimationFrame* AnimationFrame::create(SpriteFrame* spriteFrame, float delayUnits, const ValueMap& userInfo)
+{
+    auto ret = new (std::nothrow) AnimationFrame();
+    if (ret && ret->initWithSpriteFrame(spriteFrame, delayUnits, userInfo))
+    {
+        ret->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(ret);
+    }
+    return ret;
+}
+    
+    void SplineSegKernel::CalculateThirdOrderDerivative(const uint32_t num_params) {
+  kernel_third_order_derivative_ =
+      Eigen::MatrixXd::Zero(num_params, num_params);
+  for (int r = 3; r < kernel_third_order_derivative_.rows(); ++r) {
+    for (int c = 3; c < kernel_third_order_derivative_.cols(); ++c) {
+      kernel_third_order_derivative_(r, c) =
+          (r * r - r) * (r - 2) * (c * c - c) * (c - 2) / (r + c - 5.0);
+    }
+  }
+}
+    
+    using ::apollo::drivers::canbus::Byte;
+    
+    // config detail: {'name': 'motor_temperature', 'offset': -40.0,
+// 'precision': 1.0, 'len': 16, 'is_signed_var': True, 'physical_range':
+// '[-32808|32727]', 'bit': 23, 'type': 'int', 'order': 'motorola',
+// 'physical_unit': 'deg C'}
+int Brakemotorrpt271::motor_temperature(const std::uint8_t* bytes,
+                                        int32_t length) const {
+  Byte t0(bytes + 2);
+  int32_t x = t0.get_byte(0, 8);
     }
