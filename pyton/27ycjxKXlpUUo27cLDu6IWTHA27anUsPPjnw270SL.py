648a326615cce3fe06ab7006b8146f78c572fb52
SUNsung <weatherius@gmail.com>
2019-04-02 16:38:29 +0300

    Rewrite [27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py] File in: 2019-04-02 16:38:29

--- pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
+++ pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
@@ -1,89 +1,147 @@
 
         
-                e.g.:
-        @returns request
-        @returns request 2
-        @returns request 2 10
-        @returns request 0 10
-    '''
+        with open('update/LATEST_VERSION', 'w') as f:
+    f.write(version)
     
-            def getContext(self, hostname=None, port=None):
-            ctx = ClientContextFactory.getContext(self)
-            # Enable all workarounds to SSL bugs as documented by
-            # https://www.openssl.org/docs/manmaster/man3/SSL_CTX_set_options.html
-            ctx.set_options(SSL.OP_ALL)
-            return ctx
+    
+# Import youtube_dl
+ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
+sys.path.insert(0, ROOT_DIR)
+import youtube_dl
+    
+        def test_cbc_encrypt(self):
+        data = bytes_to_intlist(self.secret_msg)
+        encrypted = intlist_to_bytes(aes_cbc_encrypt(data, self.key, self.iv))
+        self.assertEqual(
+            encrypted,
+            b'\x97\x92+\xe5\x0b\xc3\x18\x91ky9m&\xb3\xb5@\xe6'\xc2\x96.\xc8u\x88\xab9-[\x9e|\xf1\xcd')
+    
+    
+if __name__ == '__main__':
+    unittest.main()
 
     
-        delimiter = b'\n'
-    
-    
-def _FormatYcmdDebugInfo( ycmd ):
-  python = ycmd[ 'python' ]
-  clang = ycmd[ 'clang' ]
-  message = ( 'Server Python interpreter: {0}\n'
-              'Server Python version: {1}\n'
-              'Server has Clang support compiled in: {2}\n'
-              'Clang version: {3}\n'.format( python[ 'executable' ],
-                                             python[ 'version' ],
-                                             clang[ 'has_support' ],
-                                             clang[ 'version' ] ) )
-  extra_conf = ycmd[ 'extra_conf' ]
-  extra_conf_path = extra_conf[ 'path' ]
-  if not extra_conf_path:
-    message += 'No extra configuration file found\n'
-  elif not extra_conf[ 'is_loaded' ]:
-    message += ( 'Extra configuration file found but not loaded\n'
-                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
-  else:
-    message += ( 'Extra configuration file found and loaded\n'
-                 'Extra configuration path: {0}\n'.format( extra_conf_path ) )
-  return message
-    
-    from ycm.client.base_request import BaseRequest
-    
-    
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-      _assert_rejects( f, 'This is a Taco' )
-  _assert_rejects( f, 'This is a Burrito' )
-    
-    Local worker thread:
-- reads work ids from the 'Work Ids' queue and looks up the corresponding
-  WorkItem from the 'Work Items' dict: if the work item has been cancelled then
-  it is simply removed from the dict, otherwise it is repackaged as a
-  _CallItem and put in the 'Call Q'. New _CallItems are put in the 'Call Q'
-  until 'Call Q' is full. NOTE: the size of the 'Call Q' is kept small because
-  calls placed in the 'Call Q' can no longer be cancelled with Future.cancel().
-- reads _ResultItems from 'Result Q', updates the future stored in the
-  'Work Items' dict and deletes the dict entry
-    
-        return '{0:.3g} {1}'.format(
-        size / float(1 << (order * 10)),
-        suffixes[order]
-    )
-    
-    
-def rewrite_build(service):
-    if 'dockerfile' in service:
-        service['build'] = {
-            'context': service.pop('build'),
-            'dockerfile': service.pop('dockerfile'),
+            jsi = JSInterpreter('''
+        function f() {
+            var x = '/*';
+            var y = 1 /* comment */ + 2;
+            return y;
         }
+        ''')
+        self.assertEqual(jsi.call_function('f'), 3)
     
-        @pytest.mark.skipif(IS_WINDOWS_PLATFORM, reason='posix separator')
-    def test_multiple_path_from_env(self):
-        with mock.patch.dict(os.environ):
-            os.environ['COMPOSE_FILE'] = 'one.yml:two.yml'
-            environment = Environment.from_env_file('.')
-            assert get_config_path_from_options(
-                '.', {}, environment
-            ) == ['one.yml', 'two.yml']
+    n_samples, n_features = 5000, 300
+X = np.random.randn(n_samples, n_features)
+inds = np.arange(n_samples)
+np.random.shuffle(inds)
+X[inds[int(n_features / 1.2):]] = 0  # sparsify input
+print('input data sparsity: %f' % sparsity_ratio(X))
+coef = 3 * np.random.randn(n_features)
+inds = np.arange(n_features)
+np.random.shuffle(inds)
+coef[inds[n_features // 2:]] = 0  # sparsify coef
+print('true coef sparsity: %f' % sparsity_ratio(coef))
+y = np.dot(X, coef)
     
-            _, args, _ = mock_logging.error.mock_calls[0]
-        assert 'Windows named pipe error: I use weird characters \xe9 (code: 9999)' == args[0]
+    
+def issue_role(name, rawtext, text, lineno,
+               inliner, options=None, content=None):
+    '''Sphinx role for linking to an issue. Must have
+    `issues_uri` or `issues_github_path` configured in ``conf.py``.
+    
+    X = np.array(X)
+y = np.array(y)
+    
+    import numpy as np
+import matplotlib.pyplot as plt
+    
+    The first plot shows the best inertia reached for each combination
+of the model (``KMeans`` or ``MiniBatchKMeans``) and the init method
+(``init='random'`` or ``init='kmeans++'``) for increasing values of the
+``n_init`` parameter that controls the number of initializations.
+    
+    # Anisotropicly distributed data
+random_state = 170
+X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
+transformation = [[0.6, -0.6], [-0.4, 0.8]]
+X_aniso = np.dot(X, transformation)
+aniso = (X_aniso, y)
+    
+    
+class BadNonceTest(unittest.TestCase):
+    '''Tests for acme.errors.BadNonce.'''
+    
+            # Placeholder for augeas
+        self.aug = None
+    
+        # NOTE: Only run one test per function otherwise you will have
+    # inf recursion
+    def test_include(self):
+        self.verify_fnmatch('test_fnmatch.?onf')
+    
+            api_url = self.API_ENDPOINT + \
+            'servlet/playinfo?vid={vid}&m=0'.format(vid = vid)  #return XML
+    
+        if re.match(r'https?://movie', url):
+        title = match1(html, 'name='description' content='([^']+)')
+        tid = match1(url, 'trailer/(\d+)')
+        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
+        type, ext, size = url_info(real_url)
+    
+    def get_photo_id(url, page):
+    return match1(url, pattern_url_single_photo)
+    
+        type, ext, size = url_info(url[0], True)
+    size = urls_size(url)
+    
+        stream_types = [
+        {'id': 'video'},
+        {'id': 'audio'},
+        {'id': 'slides'}
+    ]
+    
+    import face_recognition
+    
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
+    
+        if os.path.isdir(image_to_check):
+        if cpus == 1:
+            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
+        else:
+            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
+    else:
+        test_image(image_to_check, model)
+    
+        # Label the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        if not name:
+            continue
+    
+    frames = []
+frame_count = 0
+    
+    # Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+known_face_names = [
+    'Barack Obama',
+    'Joe Biden'
+]
+    
+            for detected_faces in batched_detected_faces:
+            self.assertEqual(len(detected_faces), 1)
+            self.assertEqual(detected_faces[0].rect.top(), 154)
+            self.assertEqual(detected_faces[0].rect.bottom(), 390)
+    
+    # Find all facial features in all the faces in the image
+face_landmarks_list = face_recognition.face_landmarks(image)
+    
+            if match[0]:
+            name = 'Barack Obama'
+    
+            #Conversion of md to HTML
+        for md in allmd:
