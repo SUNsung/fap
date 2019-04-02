6b91b806a15bd1bdb5c055dfa9a154c08f22fb38
SUNsung <weatherius@gmail.com>
2019-04-02 18:10:30 +0300

    Rewrite [09djHhTDK6IMA09UAxpnKYFrsc09FTQ4az4HUg09.Ma.py] File in: 2019-04-02 18:10:29

--- pyton/09djHhTDK6IMA09UAxpnKYFrsc09FTQ4az4HUg09.Ma.py
+++ pyton/09djHhTDK6IMA09UAxpnKYFrsc09FTQ4az4HUg09.Ma.py
@@ -1,72 +1,172 @@
 
         
-            dp = [[False for x in range(s+1)]for y in range(n+1)]
+            def __call__(self, x):
+        regularization = 0.
+        if self.l1:
+            regularization += K.sum(self.l1 * K.abs(x))
+        if self.l2:
+            regularization += K.sum(self.l2 * K.square(x))
+        return regularization
     
-    def reformatHex(i):
-	'''[summary]
-	Converts the given integer into 8-digit hex number.
+    
+def test_boston_housing():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
     
     
+def create_model(kernel_regularizer=None, activity_regularizer=None):
+    model = Sequential()
+    model.add(Dense(num_classes,
+                    kernel_regularizer=kernel_regularizer,
+                    activity_regularizer=activity_regularizer,
+                    input_shape=(data_dim,)))
+    return model
+    
+            self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.recurrent_regularizer = regularizers.get(recurrent_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+    
+    outputs = Activation('sigmoid', name='decoder_output')(x)
+    
+    Gets to 98.40% test accuracy after 20 epochs
+(there is *a lot* of margin for parameter tuning).
+2 seconds per epoch on a K520 GPU.
 '''
-* Wondering how this method works !
-* It's pretty simple.
-* Let's say you need to calculate a ^ b
-* RULE 1 : a * b = (a+a) * (b/2) ---- example : 4 * 4 = (4+4) * (4/2) = 8 * 2
-* RULE 2 : IF b is ODD, then ---- a * b = a + (a * (b - 1)) :: where (b - 1) is even.
-* Once b is even, repeat the process to get a * b
-* Repeat the process till b = 1 OR b = 0, because a*1 = a AND a*0 = 0
-*
-* As far as the modulo is concerned,
-* the fact : (a+b) % c = ((a%c) + (b%c)) % c
-* Now apply RULE 1 OR 2, whichever is required.
-'''
+    
+    model1 = create_network(num_classes=num_classes, **network1)
+history_model1 = model1.fit(x_train,
+                            y_train,
+                            batch_size=batch_size,
+                            epochs=epochs,
+                            verbose=1,
+                            validation_split=0.1)
+    
+        # Display the results
+    for top, right, bottom, left in face_locations:
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+    
+    
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
+        :param img: A list of images (each as a numpy array)
+    :param number_of_times_to_upsample: How many times to upsample the image looking for faces. Higher numbers find smaller faces.
+    :param batch_size: How many images to include in each GPU processing batch.
+    :return: A list of tuples of found face locations in css (top, right, bottom, left) order
+    '''
+    def convert_cnn_detections_to_css(detections):
+        return [_trim_css_to_bounds(_rect_to_css(face.rect), images[0].shape) for face in detections]
+    
+    # Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
 
     
-    letters = [letter for letter in string.ascii_letters]
-digits = [digit for digit in string.digits]
-symbols = [symbol for symbol in string.punctuation]
-chars = letters + digits + symbols
-random.shuffle(chars)
+        # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(output)
+    print('Found {} faces in image.'.format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
     
+    # Find all facial features in all the faces in the image
+face_landmarks_list = face_recognition.face_landmarks(image)
     
-# ---------------------------------------------------------------------------- #
-# Functions for bolting FPN onto a backbone architectures
-# ---------------------------------------------------------------------------- #
+    setup(
+    name='face_recognition',
+    version='1.2.3',
+    description='Recognize faces from Python or from the command line',
+    long_description=readme + '\n\n' + history,
+    author='Adam Geitgey',
+    author_email='ageitgey@gmail.com',
+    url='https://github.com/ageitgey/face_recognition',
+    packages=[
+        'face_recognition',
+    ],
+    package_dir={'face_recognition': 'face_recognition'},
+    package_data={
+        'face_recognition': ['models/*.dat']
+    },
+    entry_points={
+        'console_scripts': [
+            'face_recognition=face_recognition.face_recognition_cli:main',
+            'face_detection=face_recognition.face_detection_cli:main'
+        ]
+    },
+    install_requires=requirements,
+    license='MIT license',
+    zip_safe=False,
+    keywords='face_recognition',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
+
     
-        return model.Relu(s, s)
+        # 将每一个人脸与已知样本图片比对
+    for face_encoding in face_encodings:
+        # 看是否属于奥巴马或者拜登
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = '<Unknown Person>'
     
-            Output blobs: [rois_fpn<min>, ..., rois_rpn<max>, rois,
-                       rois_idx_restore]
-          - rois_fpn<i> are the RPN proposals for FPN level i
-          - rois_idx_restore is a permutation on the concatenation of all
-            rois_fpn<i>, i=min...max, such that when applied the RPN RoIs are
-            restored to their original order in the input blobs.
+        # 载入用户上传的图片
+    img = face_recognition.load_image_file(file_stream)
+    # 为用户上传的图片中的人脸编码
+    unknown_face_encodings = face_recognition.face_encodings(img)
     
-    import numpy as np
+        filenames = []
+    for (dirpath, dnames, fnames) in os.walk(path):
+        for fname in fnames:
+            if fname.endswith('.md'):
+                filenames.append(os.sep.join([dirpath, fname]))
     
-    def add_generic_rpn_outputs(model, blob_in, dim_in, spatial_scale_in):
-    '''Add RPN outputs (objectness classification and bounding box regression)
-    to an RPN model. Abstracts away the use of FPN.
+    from ycm.client.base_request import BaseRequest
+    
+    import time
+from threading import Thread
+from ycm.client.base_request import BaseRequest
+    
+        Args:
+        executor_reference: A weakref.ref to the ProcessPoolExecutor that owns
+            this thread. Used to determine if the ProcessPoolExecutor has been
+            garbage collected and that this function can exit.
+        process: A list of the multiprocessing.Process instances used as
+            workers.
+        pending_work_items: A dict mapping work ids to _WorkItems e.g.
+            {5: <_WorkItem...>, 6: <_WorkItem...>, ...}
+        work_ids_queue: A queue.Queue of work ids e.g. Queue([5, 6, ...]).
+        call_queue: A multiprocessing.Queue that will be filled with _CallItems
+            derived from _WorkItems for processing by the process workers.
+        result_queue: A multiprocessing.Queue of _ResultItems generated by the
+            process workers.
     '''
-    loss_gradients = None
-    if cfg.FPN.FPN_ON:
-        # Delegate to the FPN module
-        FPN.add_fpn_rpn_outputs(model, blob_in, dim_in, spatial_scale_in)
-        if cfg.MODEL.FASTER_RCNN:
-            # CollectAndDistributeFpnRpnProposals also labels proposals when in
-            # training mode
-            model.CollectAndDistributeFpnRpnProposals()
-        if model.train:
-            loss_gradients = FPN.add_fpn_rpn_losses(model)
-    else:
-        # Not using FPN, add RPN to a single scale
-        add_single_scale_rpn_outputs(model, blob_in, dim_in, spatial_scale_in)
-        if model.train:
-            loss_gradients = add_single_scale_rpn_losses(model)
-    return loss_gradients
-    
-        sampled_fg_rois *= im_scale
-    repeated_batch_idx = batch_idx * blob_utils.ones(
-        (sampled_fg_rois.shape[0], 1)
-    )
-    sampled_fg_rois = np.hstack((repeated_batch_idx, sampled_fg_rois))
+    nb_shutdown_processes = [0]
+    def shutdown_one_process():
+        '''Tell a worker to terminate, which will in turn wake us again'''
+        call_queue.put(None)
+        nb_shutdown_processes[0] += 1
+    while True:
+        _add_call_item_to_queue(pending_work_items,
+                                work_ids_queue,
+                                call_queue)
