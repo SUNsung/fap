751488d1d6c7b80852eb90eda6bca1f0f0db8be7
SUNsung <weatherius@gmail.com>
2019-04-02 12:28:37 +0300

    Rewrite [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2019-04-02 12:28:37

--- pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -1,110 +1,81 @@
 
         
-            self.instances_to_load = tf.placeholder(dtype=tf.string, shape=[None])
-    self.labels_to_load = lexnet_common.load_all_labels(self.instances_to_load)
-    self.pairs_to_load = lexnet_common.load_all_pairs(self.instances_to_load)
+        
+def l1_l2(l1=0.01, l2=0.01):
+    return L1L2(l1=l1, l2=l2)
     
-    model = IntegrationToBoundModel(N)
-inputs_ph_t = [tf.placeholder(tf.float32,
-                              shape=[None, 1]) for _ in range(ntimesteps)]
-state = tf.zeros([batch_size, N])
-saver = tf.train.Saver()
+        outputs1 = Lambda(lambda x: utils.preprocess_input(x, 'channels_last'),
+                      output_shape=x.shape[1:])(inputs)
+    model1 = Model(inputs, outputs1)
+    out1 = model1.predict(x)
+    x2 = np.transpose(x, (0, 3, 1, 2))
+    inputs2 = Input(shape=x2.shape[1:])
+    outputs2 = Lambda(lambda x: utils.preprocess_input(x, 'channels_first'),
+                      output_shape=x2.shape[1:])(inputs2)
+    model2 = Model(inputs2, outputs2)
+    out2 = model2.predict(x2)
+    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
     
-      Returns:
-    In the equation, y = x W + b, returns the tensorflow op that yields y.
-  '''
-  in_size = int(x.get_shape()[1]) # from Dimension(10) -> 10
-  stddev = alpha/np.sqrt(float(in_size))
-  mat_init = tf.random_normal_initializer(0.0, stddev)
-  wname = (name + '/W') if name else '/W'
     
-        # Loop through the 2D matrix of word_patches and score each.
-    for i, row in enumerate(word_patches):
-      print('Reset RNN states.')
-      self.reset()  # reset states before processing each row.
-      row_probs = np.zeros([batch_size, 0])
-      for j, word_patch in enumerate(row):
-        print('Processing patch '
-              '({}, {}) / ({}, {})'.format(i+1, j+1, nrow, ncol))
-        patch_probs = (self._score(word_patch) if word_patch else
-                       np.zeros([batch_size, num_timesteps]))
-        row_probs = np.concatenate([row_probs, patch_probs], 1)
-      probs = np.concatenate([probs, row_probs], 0)
-    return probs
+def test_fashion_mnist():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+        assert len(x_train) == len(y_train) == 60000
+        assert len(x_test) == len(y_test) == 10000
     
-      if epoch_size_override:
-    epoch_size = epoch_size_override
-  else:
-    epoch_size = (batch_len - 1) // num_steps
+    # convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
     
-    def roundtrip(filename, output=sys.stdout):
-    with open(filename, 'rb') as pyfile:
-        encoding = tokenize.detect_encoding(pyfile.readline)[0]
-    with open(filename, 'r', encoding=encoding) as pyfile:
-        source = pyfile.read()
-    tree = compile(source, filename, 'exec', ast.PyCF_ONLY_AST)
-    Unparser(tree, output)
+    # Stack of Transposed Conv2D blocks
+# Notes:
+# 1) Use Batch Normalization before ReLU on deep networks
+# 2) Use UpSampling2D as alternative to strides>1
+# - faster but not as good as strides>1
+for filters in layer_filters[::-1]:
+    x = Conv2DTranspose(filters=filters,
+                        kernel_size=kernel_size,
+                        strides=2,
+                        activation='relu',
+                        padding='same')(x)
     
-    IMPORT_MAPPING.update({
-    'cPickle': 'pickle',
-    '_elementtree': 'xml.etree.ElementTree',
-    'FileDialog': 'tkinter.filedialog',
-    'SimpleDialog': 'tkinter.simpledialog',
-    'DocXMLRPCServer': 'xmlrpc.server',
-    'SimpleHTTPServer': 'http.server',
-    'CGIHTTPServer': 'http.server',
-    # For compatibility with broken pickles saved in old Python 3 versions
-    'UserDict': 'collections',
-    'UserList': 'collections',
-    'UserString': 'collections',
-    'whichdb': 'dbm',
-    'StringIO':  'io',
-    'cStringIO': 'io',
-})
+        def test_bad_save_checkpoint(self):
+        self.config.reverter.add_to_checkpoint = mock.Mock(
+            side_effect=errors.ReverterError)
+        self.config.parser.add_dir(
+            self.vh_truth[0].path, 'Test', 'bad_save_ckpt')
+        self.assertRaises(errors.PluginError, self.config.save)
     
-        def testSeekBackwardsAcrossStreams(self):
-        self.createTempFile(streams=2)
-        with BZ2File(self.filename) as bz2f:
-            readto = len(self.TEXT) + 100
-            while readto > 0:
-                readto -= len(bz2f.read(readto))
-            bz2f.seek(-150, 1)
-            self.assertEqual(bz2f.read(), self.TEXT[100-150:] + self.TEXT)
     
-        counter = 1
-    for part in msg.walk():
-        # multipart/* are just containers
-        if part.get_content_maintype() == 'multipart':
-            continue
-        # Applications should really sanitize the given filename so that an
-        # email message can't be used to overwrite important files
-        filename = part.get_filename()
-        if not filename:
-            ext = mimetypes.guess_extension(part.get_content_type())
-            if not ext:
-                # Use a generic bag-of-bits extension
-                ext = '.bin'
-            filename = 'part-%03d%s' % (counter, ext)
-        counter += 1
-        with open(os.path.join(args.directory, filename), 'wb') as fp:
-            fp.write(part.get_payload(decode=True))
+class SelectVhostMultiTest(unittest.TestCase):
+    '''Tests for certbot_apache.display_ops.select_vhost_multiple.'''
     
-    from ..common import *
-from ..extractor import VideoExtractor
+    import mock
     
-            if title is None:
-            self.title = '_'.join([i.text for i in self.tree.iterfind('video/videomarks/videomark/markdesc')])
-        else:
-            self.title = title
+            self.vhost1b = VirtualHost(
+            'filep', 'vh_path', set([self.addr1]), False, False, 'localhost')
     
-    from ..common import *
+            # XXX: Deleting all of this is kind of scary unless the test
+        #      instances really each have a complete configuration!
+        shutil.rmtree('/etc/nginx')
+        shutil.copytree(server_root, '/etc/nginx', symlinks=True)
     
-    site_info = 'ehow.com'
-download = ehow_download
-download_playlist = playlist_not_supported('ehow')
+    
+def test_1():
+    for o in [None, True, False, 0, 1, (1 << 6), (1 << 7) - 1, -1,
+              -((1 << 5) - 1), -(1 << 5)]:
+        check(1, o)
+    
+            # object
+        exp = pd.Index([pd.Timestamp('2011-01-01 09:00', tz=tz),
+                        'x',
+                        pd.Timestamp('2011-01-01 11:00', tz=tz)],
+                       dtype=object)
+        tm.assert_index_equal(idx.fillna('x'), exp)
 
     
-        if title is None:
-      title = url[0]
-    
-            i = 1
+        result = s[lambda x: [True, False, True, True]]
+    tm.assert_series_equal(result, s.iloc[[0, 2, 3]])
