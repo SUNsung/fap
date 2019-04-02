a703f15baddc143432207345b6492c17d9519214
SUNsung <weatherius@gmail.com>
2019-04-02 03:35:07 +0300

    Rewrite [00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp] File in: 2019-04-02 03:35:06

--- cccccc/00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp
+++ cccccc/00MpkULifEKXM00rLtlOR9TGFY00O13cph5UE00JJy.cpp
@@ -1,139 +1,228 @@
 
         
-          // Calculate the depth at which the requirement's generic parameters
-  // appear in the synthetic signature.
-  unsigned depth = 0;
-  if (covariantSelf) {
-    depth++;
-  }
-  if (conformanceSig) {
-    depth += conformanceSig->getGenericParams().back()->getDepth() + 1;
-  }
-    
-      /// The number of conformance requirements, cached to avoid constantly
-  /// recomputing it on conformance-buffer access.
-  const unsigned numConformanceRequirements : 31;
-    
-        if (info.status == serialization::Status::Valid) {
-      assert(info.bytes != 0);
-      if (!info.name.empty()) {
-        StringRef moduleData = buf.substr(0, info.bytes);
-        std::unique_ptr<llvm::MemoryBuffer> bitstream(
-          llvm::MemoryBuffer::getMemBuffer(moduleData, info.name, false));
+          // If the requirement itself is non-generic, the synthetic signature
+  // is that of the conformance context.
+  if (!covariantSelf &&
+      reqSig->getGenericParams().size() == 1 &&
+      reqSig->getRequirements().size() == 1) {
+    syntheticSignature = conformanceDC->getGenericSignatureOfContext();
+    if (syntheticSignature) {
+      syntheticSignature = syntheticSignature->getCanonicalSignature();
+      syntheticEnvironment =
+        syntheticSignature->createGenericEnvironment();
     }
     }
     
-    #undef VERB
-#undef DIRECTIONAL_PREPOSITION
-#undef PREPOSITION
+    void absDiff(const Size2D &size,
+             const f32 * src0Base, ptrdiff_t src0Stride,
+             const f32 * src1Base, ptrdiff_t src1Stride,
+             f32 * dstBase, ptrdiff_t dstStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    internal::vtransform(size,
+                         src0Base, src0Stride,
+                         src1Base, src1Stride,
+                         dstBase, dstStride, AbsDiff<f32>());
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+#endif
+}
+    
+        f32 alpha, beta, gamma;
+    float32x4_t valpha, vbeta, vgamma;
+    wAdd(f32 _alpha, f32 _beta, f32 _gamma):
+        alpha(_alpha), beta(_beta), gamma(_gamma)
+    {
+        valpha = vdupq_n_f32(_alpha);
+        vbeta = vdupq_n_f32(_beta);
+        vgamma = vdupq_n_f32(_gamma + 0.5);
+    }
+    
+                if(!x) {
+                tcurr = tnext;
+    }
+    
+        for (size_t i = 0u; i < size.height; ++i)
+    {
+        const u8 * srcy = internal::getRowPtr(srcyBase, srcyStride, i);
+        const u8 * srcu = internal::getRowPtr(srcuBase, srcuStride, i);
+        const u8 * srcv = internal::getRowPtr(srcvBase, srcvStride, i);
+        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t syj = 0u, sj = 0u, dj = 0u;
+    }
+    
+    #include <cstddef>
+#include <cstdlib>
+#include <algorithm>
+    
+    template <>
+void rshiftConst<0>(const Size2D &size,
+                    const s16 * srcBase, ptrdiff_t srcStride,
+                    u8 * dstBase, ptrdiff_t dstStride,
+                    CONVERT_POLICY cpolicy)
+{
+    size_t roiw16 = size.width >= 15 ? size.width - 15 : 0;
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+    }
+    
+    #if !defined(__aarch64__) && defined(__GNUC__) && defined(__arm__)
+CVTS_FUNC1(u8, 16,
+    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
+    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
+{
+    for (size_t i = 0; i < w; i += 16)
+    {
+        internal::prefetch(_src + i);
+        __asm__ (
+            'vld1.8 {d4-d5}, [%[src]]                              \n\t'
+            'vmovl.u8 q3, d4                                       \n\t'
+            'vmovl.u8 q4, d5                                       \n\t'
+            'vmovl.u16 q5, d6                                      \n\t'
+            'vmovl.u16 q6, d7                                      \n\t'
+            'vmovl.u16 q7, d8                                      \n\t'
+            'vmovl.u16 q8, d9                                      \n\t'
+            'vcvt.f32.u32 q9, q5                                   \n\t'
+            'vcvt.f32.u32 q10, q6                                  \n\t'
+            'vcvt.f32.u32 q11, q7                                  \n\t'
+            'vcvt.f32.u32 q12, q8                                  \n\t'
+            'vmul.f32 q13, q9, q0                                  \n\t'
+            'vmul.f32 q14, q10, q0                                 \n\t'
+            'vmul.f32 q15, q11, q0                                 \n\t'
+            'vmul.f32 q2, q12, q0                                  \n\t'
+            'vadd.f32 q3, q13, q1                                  \n\t'
+            'vadd.f32 q4, q14, q1                                  \n\t'
+            'vadd.f32 q5, q15, q1                                  \n\t'
+            'vadd.f32 q6, q2, q1                                   \n\t'
+            'vcvt.s32.f32 q7, q3                                   \n\t'
+            'vcvt.s32.f32 q8, q4                                   \n\t'
+            'vcvt.s32.f32 q9, q5                                   \n\t'
+            'vcvt.s32.f32 q10, q6                                  \n\t'
+            'vqmovun.s32 d22, q7                                   \n\t'
+            'vqmovun.s32 d23, q8                                   \n\t'
+            'vqmovun.s32 d24, q9                                   \n\t'
+            'vqmovun.s32 d25, q10                                  \n\t'
+            'vqmovn.u16 d26, q11                                   \n\t'
+            'vqmovn.u16 d27, q12                                   \n\t'
+            'vst1.8 {d26-d27}, [%[dst1]]                           \n\t'
+            : /*no output*/
+            : [src] 'r' (_src + i),
+              [dst1] 'r' (_dst + i + 0),
+              'w'  (vscale), 'w' (vshift)
+            : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25','d26','d27','d28','d29','d30','d31'
+        );
+    }
+})
+#else
+CVTS_FUNC1(u8, 16,
+    float32x4_t vscale = vdupq_n_f32((f32)alpha);
+    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
+{
+    for (size_t i = 0; i < w; i += 16)
+    {
+        internal::prefetch(_src + i);
+        uint8x16_t vline = vld1q_u8(_src + i);
+        uint16x8_t vline1_u16 = vmovl_u8(vget_low_u8 (vline));
+        uint16x8_t vline2_u16 = vmovl_u8(vget_high_u8(vline));
+        uint32x4_t vline1_u32 = vmovl_u16(vget_low_u16 (vline1_u16));
+        uint32x4_t vline2_u32 = vmovl_u16(vget_high_u16(vline1_u16));
+        uint32x4_t vline3_u32 = vmovl_u16(vget_low_u16 (vline2_u16));
+        uint32x4_t vline4_u32 = vmovl_u16(vget_high_u16(vline2_u16));
+        float32x4_t vline1_f32 = vcvtq_f32_u32(vline1_u32);
+        float32x4_t vline2_f32 = vcvtq_f32_u32(vline2_u32);
+        float32x4_t vline3_f32 = vcvtq_f32_u32(vline3_u32);
+        float32x4_t vline4_f32 = vcvtq_f32_u32(vline4_u32);
+        vline1_f32 = vmulq_f32(vline1_f32, vscale);
+        vline2_f32 = vmulq_f32(vline2_f32, vscale);
+        vline3_f32 = vmulq_f32(vline3_f32, vscale);
+        vline4_f32 = vmulq_f32(vline4_f32, vscale);
+        vline1_f32 = vaddq_f32(vline1_f32, vshift);
+        vline2_f32 = vaddq_f32(vline2_f32, vshift);
+        vline3_f32 = vaddq_f32(vline3_f32, vshift);
+        vline4_f32 = vaddq_f32(vline4_f32, vshift);
+        int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
+        int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
+        int32x4_t vline3_s32 = vcvtq_s32_f32(vline3_f32);
+        int32x4_t vline4_s32 = vcvtq_s32_f32(vline4_f32);
+        uint16x8_t vRes1_u16 = vcombine_u16(vqmovun_s32(vline1_s32), vqmovun_s32(vline2_s32));
+        uint16x8_t vRes2_u16 = vcombine_u16(vqmovun_s32(vline3_s32), vqmovun_s32(vline4_s32));
+        vst1q_u8(_dst + i, vcombine_u8(vqmovn_u16(vRes1_u16), vqmovn_u16(vRes2_u16)));
+    }
+})
+#endif
+    
+            if (s[0] < 0 || s[1] < 0)//saturate in case of overflow ~ 8GB of non-zeros...
+        {
+            return 0x7fFFffFF;
+        }
+        result += (s[0] += s[1]);
+        if (s[0] < 0 || result < 0)
+        {
+            return 0x7fFFffFF;
+        }
+    
+        ptrdiff_t idx_l1 = internal::borderInterpolate(-1, size.width, borderType, borderMargin.left, borderMargin.right) * cn;
+    ptrdiff_t idx_l2 = internal::borderInterpolate(-2, size.width, borderType, borderMargin.left, borderMargin.right) * cn;
+    ptrdiff_t idx_r1 = internal::borderInterpolate(size.width + 0, size.width, borderType, borderMargin.left, borderMargin.right) * cn;
+    ptrdiff_t idx_r2 = internal::borderInterpolate(size.width + 1, size.width, borderType, borderMargin.left, borderMargin.right) * cn;
+    
+    
+    {
+    {} // namespace test
+} // namespace c10d
 
     
+    Example 2 (with KEY):
+DATA  = [1, 2, 3, 4, 5, 6, 7, 8]
+KEY   = [0, 1, 3, 2, 1, 0, 1, 0]
+RANGES = [
+  [
+    [2, 4],
+    [0, 2],
+  ],
+  [
+    [0, 0],
+    [6, 2],
+  ]
+]
+lengths = [4, 2]
+OUTPUT[0] = [[6, 5, 4, 3], [0, 0, 0, 0]]
+OUTPUT[1] = [[1, 2], [8, 7]]
     
-    {}%
+    Github Links:
+- https://github.com/caffe2/caffe2/blob/master/caffe2/operators/given_tensor_fill_op.h
+- https://github.com/caffe2/caffe2/blob/master/caffe2/operators/given_tensor_fill_op.cc
     
-    
-    {    // Must be 'const' or nothing.
-    clang::Qualifiers quals = pointee.getQualifiers();
-    bool isConst = quals.hasConst();
-    quals.removeConst();
-    if (quals.empty()) {
-      if (auto record = pointee->getAs<clang::RecordType>()) {
-        auto recordDecl = record->getDecl();
-        if (recordDecl->hasAttr<clang::ObjCBridgeAttr>() ||
-            recordDecl->hasAttr<clang::ObjCBridgeMutableAttr>() ||
-            recordDecl->hasAttr<clang::ObjCBridgeRelatedAttr>() ||
-            isKnownCFTypeName(typedefDecl->getName())) {
-          return forRecord(isConst, record->getDecl());
-        }
-      } else if (pointee->isVoidType()) {
-        if (typedefDecl->hasAttr<clang::ObjCBridgeAttr>() ||
-            isKnownCFTypeName(typedefDecl->getName())) {
-          return isConst ? forConstVoid() : forVoid();
-        }
-      }
-    }
-  }
-    
-    class CFPointeeInfo {
-  bool IsValid;
-  bool IsConst;
-  llvm::PointerUnion<const clang::RecordDecl *, const clang::TypedefNameDecl *>
-      Decl;
-  CFPointeeInfo() = default;
-    }
-    
-    bool Punycode::decodePunycodeUTF8(StringRef InputPunycode,
-                                  std::string &OutUTF8) {
-  std::vector<uint32_t> OutCodePoints;
-  if (!decodePunycode(InputPunycode, OutCodePoints))
-    return false;
-    }
-    
-    void GenerateDSYMJobAction::anchor() {}
-    
-    
-    {public:
-  Darwin(const Driver &D, const llvm::Triple &Triple) : ToolChain(D, Triple) {}
-  ~Darwin() = default;
-  std::string sanitizerRuntimeLibName(StringRef Sanitizer,
-                                      bool shared = true) const override;
+    class IOperatorCommand : public IExpressionCommand
+{
+public:
+    virtual void SetCommand(int command) = 0;
 };
     
-      virtual void PrintDefaultValueString(std::ostream &os) const {  // NOLINT(*)
-    std::string s;
-    caffe::NetParameter np;
-    // Avoid wasting time making a copy -- just push in out default object's pointer
-    np.mutable_layer()->AddAllocated(const_cast<::caffe::LayerParameter *>(&default_value_));
-    google::protobuf::TextFormat::PrintToString(np, &s);
-    np.mutable_layer()->ReleaseLast();
-    os << '\'' << s << '\'';
-  }
+    #include 'Views/CalculatorProgrammerOperators.g.h'
+#include 'Controls/RadixButton.h'
+#include 'CalcViewModel/StandardCalculatorViewModel.h'
     
-    /*!
- * \brief Moore-Penrose pseudoinverse of the Khatri-Rao product
- *
- * Given input matrices A_1, ..., A_n, of shape (l_1, k), ..., (l_n, k) respectively, the pseudoinverse of the Khatri-Rao product is
- *
- *   pinv(A_1 khatri-rao A_2 khatri-rao ... khatri-rao A_n) =
- *     ((A_1^T A_1) hadamard-dot ... hadamard-dot (A_n^T A_n))
- *     (A_1 khatri-rao ... khatri-rao A_n)^T
- *
- * As the first term of the r.h.s is a square matrix, the result is always of the same shape as the transpose of the Khatri-Rao product of the input matrices. The input argument ts_arr could contain the original input matrices, or transposed ones.
- *
- * \param out result matrix
- * \param ts_arr vector of input matrices
- * \param input_transposed if every input matrices is transposed
- */
-template <typename DType>
-inline void inv_khatri_rao
-  (Tensor<cpu, 2, DType> out,
-  const std::vector<Tensor<cpu, 2, DType> > &ts_arr,
-  bool input_transposed = false) {
-  CHECK_GE(ts_arr.size(), 1) << 'Input tensor array must be non-empty';
-    }
+        private:
+        void FromDate_DateChanged(_In_ Windows::UI::Xaml::Controls::CalendarDatePicker^ sender, _In_ Windows::UI::Xaml::Controls::CalendarDatePickerDateChangedEventArgs^ e);
+        void ToDate_DateChanged(_In_ Windows::UI::Xaml::Controls::CalendarDatePicker^ sender, _In_ Windows::UI::Xaml::Controls::CalendarDatePickerDateChangedEventArgs^ e);
+        void AddSubtract_DateChanged(_In_ Windows::UI::Xaml::Controls::CalendarDatePicker^ sender, _In_ Windows::UI::Xaml::Controls::CalendarDatePickerDateChangedEventArgs^ e);
+        void OffsetValue_Changed(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::Controls::SelectionChangedEventArgs^ e);
+        void OnCopyMenuItemClicked(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::RoutedEventArgs^ e);
+        void OnLoaded(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::RoutedEventArgs^ e);
+        void DateCalcOption_Changed(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::Controls::SelectionChangedEventArgs^ e);
+        void AddSubtractDateGrid_Loaded(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::RoutedEventArgs^ e);
+        void AddSubtractOption_Checked(_In_ Platform::Object^ sender, _In_ Windows::UI::Xaml::RoutedEventArgs^ e);
+        void ReselectCalendarDate(_In_ Windows::UI::Xaml::Controls::CalendarDatePicker^ calendarDatePicker, Windows::Foundation::DateTime dateTime);
+        void OffsetDropDownClosed(_In_ Platform::Object^ sender, _In_ Platform::Object^ e);
+        void CalendarFlyoutClosed(_In_ Platform::Object^ sender, _In_ Platform::Object^ e);
+        void RaiseLiveRegionChangedAutomationEvent(_In_ bool isDateDiffMode);
     
-    Crop the 2nd and 3rd dim of input data, with the corresponding size of h_w or
-with width and height of the second input symbol, i.e., with one input, we need h_w to
-specify the crop height and width, otherwise the second input symbol's size will be used
-)code' ADD_FILELINE)
-    
-    #include <algorithm>
-#include <vector>
-#include './spatial_transformer-inl.h'
-namespace mxnet {
-namespace op {
-#if defined(__CUDACC__) && MXNET_USE_CUDNN == 1 && CUDNN_MAJOR >= 5
-template<typename DType>
-class CuDNNSpatialTransformerOp : public Operator {
- public:
-  explicit CuDNNSpatialTransformerOp(SpatialTransformerParam param) {
-    this->param_ = param;
-    init_cudnn_ = false;
-    dtype_ = mshadow::DataType<DType>::kCudnnFlag;
-    if (param_.sampler_type == st::kBilinear) {
-      sampler_ = CUDNN_SAMPLER_BILINEAR;
-    }
-  }
-    }
-    }
-    }
+            void HistoryList_Loaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+        void HistoryList_Unloaded(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
