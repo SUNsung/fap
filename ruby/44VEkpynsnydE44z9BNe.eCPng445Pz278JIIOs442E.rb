35e4d3d25f69f0094842c41b0c441efa21c62a35
SUNsung <weatherius@gmail.com>
2019-04-02 17:44:44 +0300

    Rewrite [44VEkpynsnydE44z9BNe.eCPng445Pz278JIIOs442E.rb] File in: 2019-04-02 17:44:44

--- ruby/44VEkpynsnydE44z9BNe.eCPng445Pz278JIIOs442E.rb
+++ ruby/44VEkpynsnydE44z9BNe.eCPng445Pz278JIIOs442E.rb
@@ -1,86 +1,123 @@
 
         
-            # This returns whether the guest is ready to work. If this returns
-    # `false`, then {#detect!} should be called in order to detect the
-    # guest OS.
-    #
-    # @return [Boolean]
-    def ready?
-      !!capability_host_chain
+            def suggest_ruby_reinstall(e)
+      ui = FastlaneCore::UI
+      ui.error('-----------------------------------------------------------------------')
+      ui.error(e.to_s)
+      ui.error('')
+      ui.error('SSL errors can be caused by various components on your local machine.')
+      if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.1')
+        ui.error('Apple has recently changed their servers to require TLS 1.2, which may')
+        ui.error('not be available to your system installed Ruby (#{RUBY_VERSION})')
+      end
+      ui.error('')
+      ui.error('The best solution is to use the self-contained fastlane version.')
+      ui.error('Which ships with a bundled OpenSSL,ruby and all gems - so you don't depend on system libraries')
+      ui.error(' - Use Homebrew')
+      ui.error('    - update brew with `brew update`')
+      ui.error('    - install fastlane using:')
+      ui.error('      - `brew cask install fastlane`')
+      ui.error(' - Use One-Click-Installer:')
+      ui.error('    - download fastlane at https://download.fastlane.tools')
+      ui.error('    - extract the archive and double click the `install`')
+      ui.error('-----------------------------------------------------------')
+      ui.error('for more details on ways to install fastlane please refer the documentation:')
+      ui.error('-----------------------------------------------------------')
+      ui.error('        🚀       https://docs.fastlane.tools          🚀   ')
+      ui.error('-----------------------------------------------------------')
+      ui.error('')
+      ui.error('You can also install a new version of Ruby')
+      ui.error('')
+      ui.error('- Make sure OpenSSL is installed with Homebrew: `brew update && brew upgrade openssl`')
+      ui.error('- If you use system Ruby:')
+      ui.error('  - Run `brew update && brew install ruby`')
+      ui.error('- If you use rbenv with ruby-build:')
+      ui.error('  - Run `brew update && brew upgrade ruby-build && rbenv install 2.3.1`')
+      ui.error('  - Run `rbenv global 2.3.1` to make it the new global default Ruby version')
+      ui.error('- If you use rvm:')
+      ui.error('  - First run `rvm osx-ssl-certs update all`')
+      ui.error('  - Then run `rvm reinstall ruby-2.3.1 --with-openssl-dir=/usr/local`')
+      ui.error('')
+      ui.error('If that doesn't fix your issue, please google for the following error message:')
+      ui.error('  '#{e}'')
+      ui.error('-----------------------------------------------------------------------')
     end
-  end
-end
-
     
-              if !@machine_locks[id]
-            raise 'Unlocked write on machine: #{id}'
-          end
+          optional = false if optional.nil?
+      sensitive = false if sensitive.nil?
     
-            # Defines an additionally available guest implementation with
-        # the given key.
+          global_option('--verbose') { FastlaneCore::Globals.verbose = true }
+    
+            # This method is expected to return a class that is used for
+        # configuring the provisioner. This return value is expected to be
+        # a subclass of {Config}.
         #
-        # @param [String] name Name of the guest.
-        def self.guest(name=UNSET_VALUE, &block)
-          data[:guests] ||= Registry.new
-    
-            # This is the method called to 'prepare' the provisioner. This is called
-        # before any actions are run by the action runner (see {Vagrant::Actions::Runner}).
-        # This can be used to setup shared folders, forward ports, etc. Whatever is
-        # necessary on a 'meta' level.
-        #
-        # No return value is expected.
-        def prepare
+        # @return [Config]
+        def self.config_class
         end
     
-            # This contains all the registered host capabilities.
+            # Capture all bad configuration calls and save them for an error
+        # message later during validation.
+        def method_missing(name, *args, &block)
+          return super if @__finalized
+    
+            # Defines additional command line commands available by key. The key
+        # becomes the subcommand, so if you register a command 'foo' then
+        # 'vagrant foo' becomes available.
         #
-        # @return [Hash<Symbol, Registry>]
-        attr_reader :host_capabilities
+        # @param [String] name Subcommand key.
+        def self.command(name, **opts, &block)
+          # Validate the name of the command
+          if name.to_s !~ /^[-a-z0-9]+$/i
+            raise InvalidCommandName, 'Commands can only contain letters, numbers, and hyphens'
+          end
     
-        def log_processing(name)
-      puts yellow '  #{File.basename(name)}'
-    end
-    
-      # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both thread web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
-  config.eager_load = true
-    
-      # Configure static asset server for tests with Cache-Control for performance.
-  if config.respond_to?(:serve_static_files)
-    # rails >= 4.2
-    config.serve_static_files = true
-  elsif config.respond_to?(:serve_static_assets)
-    # rails < 4.2
-    config.serve_static_assets = true
-  end
-  config.static_cache_control = 'public, max-age=3600'
-    
-    ::Bundler.with_friendly_errors do
-  ::Bundler::CLI.start(ARGV, :debug => true)
+      caveats <<~EOS
+    Installation or Uninstallation may fail with Exit Code 19 (Conflicting Processes running) if Browsers, Safari Notification Service or SIMBL Services (e.g. Flashlight) are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Logs in /Library/Logs/Adobe/Installers if Installation or Uninstallation fails, to identifify the conflicting processes.
+  EOS
 end
 
     
-          def get_installer_for(plugin_name)
-        uri = pack_uri(plugin_name)
-    
-        FileUtils.rm_rf(LogStash::Environment::CACHE_PATH)
-    validate_cache_location
-    archive_manager.extract(package_file, LogStash::Environment::CACHE_PATH)
-    puts('Unpacked at #{LogStash::Environment::CACHE_PATH}')
-    puts('The unpacked plugins can now be installed in local-only mode using bin/logstash-plugin install --local [plugin name]')
+      # Compile a file on disk to CSS.
+  #
+  # @raise [Sass::SyntaxError] if there's an error in the document
+  # @raise [Encoding::UndefinedConversionError] if the source encoding
+  #   cannot be converted to UTF-8
+  # @raise [ArgumentError] if the document uses an unknown encoding with `@charset`
+  #
+  # @overload compile_file(filename, options = {})
+  #   Return the compiled CSS rather than writing it to a file.
+  #
+  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
+  #   @param options [{Symbol => Object}] An options hash;
+  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
+  #   @return [String] The compiled CSS.
+  #
+  # @overload compile_file(filename, css_filename, options = {})
+  #   Write the compiled CSS to a file.
+  #
+  #   @param filename [String] The path to the Sass, SCSS, or CSS file on disk.
+  #   @param options [{Symbol => Object}] An options hash;
+  #     see {file:SASS_REFERENCE.md#Options the Sass options documentation}
+  #   @param css_filename [String] The location to which to write the compiled CSS.
+  def self.compile_file(filename, *args)
+    options = args.last.is_a?(Hash) ? args.pop : {}
+    css_filename = args.shift
+    result = Sass::Engine.for_file(filename, options).render
+    if css_filename
+      options[:css_filename] ||= css_filename
+      open(css_filename, 'w') {|css_file| css_file.write(result)}
+      nil
+    else
+      result
+    end
   end
+end
     
-        context 'without a specific plugin' do
-      it 'display a list of plugins' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list')
-        expect(result.stdout.split('\n').size).to be > 1
-      end
-    
-        before do
-      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
-      # Logstash won't update when we have a pinned version in the gemfile so we remove them
-      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
-      expect(logstash).to have_installed?(plugin_name, previous_version)
+        # The name of the mixin in which the error occurred.
+    # This could be `nil` if the error occurred outside a mixin.
+    #
+    # @return [String]
+    def sass_mixin
+      sass_backtrace.first[:mixin]
     end
