2458126bb5de130bd66d923dae644d33be2b2834
SUNsung <weatherius@gmail.com>
2019-04-03 22:46:39 +0300

    Rewrite [38k12fUwHb42s38PRNtpHTbyQA38EurpJTwlg1Y38sk4.cpp] File in: 2019-04-03 22:46:38

--- cccccc/38k12fUwHb42s38PRNtpHTbyQA38EurpJTwlg1Y38sk4.cpp
+++ cccccc/38k12fUwHb42s38PRNtpHTbyQA38EurpJTwlg1Y38sk4.cpp
@@ -1,282 +1,213 @@
 
         
-        Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+            // Setup Platform/Renderer bindings
+    ImGui_Marmalade_Init(true);
     
-    #endif  // TENSORFLOW_PYTHON_LIB_CORE_PY_FUNC_H_
-
+                if (ImGui::Button('Button'))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text('counter = %d', counter);
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    #include 'tensorflow/stream_executor/lib/status.h'
-#include 'tensorflow/stream_executor/lib/statusor.h'
-#include 'tensorflow/stream_executor/platform/mutex.h'
-#include 'tensorflow/stream_executor/stream_executor_pimpl.h'
-    
-      // Delegate implementations.
-  void OnError(const std::string& error) override;
-  void OnError(const std::string& message,
-               const int code,
-               const std::string& domain) override;
-  void OnCheckingForUpdate() override;
-  void OnUpdateAvailable() override;
-  void OnUpdateNotAvailable() override;
-  void OnUpdateDownloaded(const std::string& release_notes,
-                          const std::string& release_name,
-                          const base::Time& release_date,
-                          const std::string& update_url) override;
-    
-     private:
-  void OnMessageBoxCallback(DialogClosedCallback callback,
-                            const std::string& origin,
-                            int code,
-                            bool checkbox_checked);
-    
-    Delegate* AutoUpdater::GetDelegate() {
-  return delegate_;
-}
-    
-      // Post 'open' event.
-  static void EmitOpenEvent(const std::string& path);
-    
-    class ObjectManager;
-    
-    EventListener::EventListener(int id,
-  const base::WeakPtr<DispatcherHost>& dispatcher_host,
-  const base::DictionaryValue& option) : Base(id, dispatcher_host, option) {
+        // Create the pixel shader
+    {
+        static const char* pixelShader =
+            'struct PS_INPUT\
+            {\
+            float4 pos : SV_POSITION;\
+            float4 col : COLOR0;\
+            float2 uv  : TEXCOORD0;\
+            };\
+            sampler sampler0;\
+            Texture2D texture0;\
+            \
+            float4 main(PS_INPUT input) : SV_Target\
+            {\
+            float4 out_col = input.col * texture0.Sample(sampler0, input.uv); \
+            return out_col; \
+            }';
     }
     
-      template<typename T> T* AddListener() {
-    std::map<int, BaseEvent*>::iterator i = listerners_.find(T::id);
-    if (i==listerners_.end()) {
-      T* listener_object = new T(this);
-      listerners_[T::id] = listener_object;
-      return listener_object;
-    }
-    return NULL;
-  }
+    // **DO NOT USE THIS CODE IF YOUR CODE/ENGINE IS USING MODERN OPENGL (SHADERS, VBO, VAO, etc.)**
+// **Prefer using the code in imgui_impl_opengl3.cpp**
+// This code is mostly provided as a reference to learn how ImGui integration works, because it is shorter to read.
+// If your code is using GL3+ context or any semi modern OpenGL calls, using this is likely to make everything more
+// complicated, will require your code to reset every single OpenGL attributes to their initial state, and might
+// confuse your GPU driver.
+// The GL2 code is unable to reset attributes or even call e.g. 'glUseProgram(0)' because they don't exist in that API.
     
-    #include 'content/nw/src/api/menuitem/menuitem.h'
+    // About GLSL version:
+//  The 'glsl_version' initialization parameter should be NULL (default) or a '#version XXX' string.
+//  On computer platform the GLSL version default to '#version 130'. On OpenGL ES 3 platform it defaults to '#version 300 es'
+//  Only override if your GL version doesn't handle this GLSL version. See GLSL version table at the top of imgui_impl_opengl3.cpp.
     
-    #if defined(OS_WIN) || defined(OS_LINUX)
-bool MenuItem::AcceleratorPressed(const ui::Accelerator& accelerator) {
-#if defined(OS_WIN)
-  if (meta_down_flag_) {
-    if ((::GetKeyState(VK_APPS) & 0x8000) != 0x8000) {
-      return true;
-    }
-  }
-#endif
-  OnClick();
-  return true;
-}
-    
-    class NwClipboardReadAvailableTypesFunction : public NWSyncExtensionFunction {
- public:
-  NwClipboardReadAvailableTypesFunction();
-  bool RunNWSync(base::ListValue* response, std::string* error) override;
-    }
-    
-    
-    {  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
-  manager->OnAllocateObject(id, type, *options, extension_id());
-  return true;
-}
-    
-      // Called when the |display|'s bound has changed.
-  void NwScreenDisplayObserver::OnDisplayMetricsChanged(const display::Display& display,
-    uint32_t changed_metrics) {
-    std::unique_ptr<base::ListValue> args = 
-      nwapi::nw__screen::OnDisplayBoundsChanged::Create(*ConvertGfxDisplay(display),
-                                                        changed_metrics);
-    DispatchEvent(
-      events::HistogramValue::UNKNOWN, 
-      nwapi::nw__screen::OnDisplayBoundsChanged::kEventName,
-      std::move(args));
-  }
-    
-      // Create template
-  std::vector<char> tmp(256);
-  auto len = snprintf(tmp.data(), tmp.size(), '%s/testXXXXXX', tmpdir);
-  tmp.resize(len);
-    
-    OPERATOR_SCHEMA(EnforceFinite)
-    .NumInputs(1)
-    .NumOutputs(0)
-    .SetDoc(R'DOC(
-Raise if there is NaN or Inf values in the input tensor.
-)DOC')
-    .Input(0, 'input', 'Input tensor');
-    
-    OPERATOR_SCHEMA(GatherRangesToDense)
-    .NumInputs(2, 3)
-    .NumOutputs(1, INT_MAX)
-    .SetDoc(R'DOC(
-Given DATA tensor of rank 1, and RANGES tensor of rank 3, gather values
-corresponding to each range into a separate output tensor. If the optional input
-KEY tensor is also given, the output will be sorted by KEY for each example.
-    
-    #include 'caffe2/operators/glu_op.h'
-    
-    template <typename T, class Context>
-class BernoulliJSDGradientOp final : public Operator<Context> {
- public:
-  USE_SIMPLE_CTOR_DTOR(BernoulliJSDGradientOp);
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-  bool RunOnDevice() override;
-};
-    
-    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
-REGISTER_CUDA_OPERATOR(
-    LC1DGradient,
-    LocallyConnectedGradientOp<float, CUDAContext>);
-    
-    template <$for j, [[typename Generator$j]]>
-internal::CartesianProductHolder$i<$for j, [[Generator$j]]> Combine(
-    $for j, [[const Generator$j& g$j]]) {
-  return internal::CartesianProductHolder$i<$for j, [[Generator$j]]>(
-      $for j, [[g$j]]);
-}
-    
-    // DeathTest is a class that hides much of the complexity of the
-// GTEST_DEATH_TEST_ macro.  It is abstract; its static Create method
-// returns a concrete class that depends on the prevailing death test
-// style, as defined by the --gtest_death_test_style and/or
-// --gtest_internal_run_death_test flags.
-    
-      // Returns true if pathname describes a root directory. (Windows has one
-  // root directory per disk drive.)
-  bool IsRootDirectory() const;
-    
-    template <GTEST_10_TYPENAMES_(T)>
-struct TupleElement<true, 1, GTEST_10_TUPLE_(T) > {
-  typedef T1 type;
-};
-    
-      // Returns true iff n is a prime number.
-  virtual bool IsPrime(int n) const = 0;
-    
-    // Tests the c'tor that accepts a C string.
-TEST(MyString, ConstructorFromCString) {
-  const MyString s(kHelloString);
-  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
-  EXPECT_EQ(sizeof(kHelloString)/sizeof(kHelloString[0]) - 1,
-            s.Length());
-}
-    
-      int32_t rseqid = 0;
-  std::string fname;
-  ::apache::thrift::protocol::TMessageType mtype;
-    
-    using namespace ::apache::thrift;
-using namespace ::apache::thrift::protocol;
-using namespace ::apache::thrift::transport;
-using namespace ::apache::thrift::server;
-    
-      std::vector<std::string> line_exports;
-  unsigned int readonly = 0;
-  int options_index = -1;
-    
-    #include <osquery/core.h>
-#include <osquery/database.h>
-#include <osquery/filesystem/filesystem.h>
-#include <osquery/logger.h>
-#include <osquery/tables.h>
-    
-    struct mei_response {
-  uint32_t maxlen;
-  uint8_t version;
-};
-    
-    void DHTRoutingTable::dropNode(const std::shared_ptr<DHTNode>& node)
+    static void ImGui_ImplGlfw_UpdateGamepads()
 {
-  getBucketFor(node)->dropNode(node);
-}
-/*
-  void DHTRoutingTable::moveBucketHead(const std::shared_ptr<DHTNode>& node)
-  {
-  getBucketFor(node)->moveToHead(node);
-  }
-*/
-void DHTRoutingTable::moveBucketTail(const std::shared_ptr<DHTNode>& node)
+    ImGuiIO& io = ImGui::GetIO();
+    memset(io.NavInputs, 0, sizeof(io.NavInputs));
+    if ((io.ConfigFlags & ImGuiConfigFlags_NavEnableGamepad) == 0)
+        return;
+    }
+    
+            // Rendering
+        ImGui::Render();
+        g_pd3dDevice->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+        g_pd3dDevice->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
+        ImGui_ImplDX10_RenderDrawData(ImGui::GetDrawData());
+    
+    void DHTReplaceNodeTask::onReceived(const DHTPingReplyMessage* message)
 {
-  getBucketFor(node)->moveToTail(node);
+  A2_LOG_INFO(fmt('ReplaceNode: Ping reply received from %s.',
+                  message->getRemoteNode()->toString().c_str()));
+  setFinished(true);
 }
     
-      Time getSerializedTime() const { return serializedTime_; }
+      virtual const std::string& getType() const CXX11_OVERRIDE;
     
-    #include 'common.h'
-    
-      void addTask(const std::shared_ptr<DHTTask>& task) { queue_.push_back(task); }
-    
-    public:
-  DHTTaskQueueImpl();
-    
-    class DHTTokenTracker {
+    class DHTRoutingTable {
 private:
-  static const size_t SECRET_SIZE = 4;
+  std::shared_ptr<DHTNode> localNode_;
     }
     
-    void DHTTokenUpdateCommand::preProcess()
-{
-  if (getDownloadEngine()->getRequestGroupMan()->downloadFinished() ||
-      getDownloadEngine()->isHaltRequested()) {
-    enableExit();
+    
+    {  const int clen = bittorrent::getCompactLength(family_);
+  // nodes
+  for (std::vector<std::shared_ptr<DHTNode>>::const_iterator i = nodes_.begin(),
+                                                             eoi = nodes_.end();
+       i != eoi; ++i) {
+    const std::shared_ptr<DHTNode>& node = *i;
+    // Write IP address + port in Compact IP-address/port info form.
+    unsigned char compactPeer[COMPACT_LEN_IPV6];
+    int compactlen = bittorrent::packcompact(compactPeer, node->getIPAddress(),
+                                             node->getPort());
+    if (compactlen != clen) {
+      memset(compactPeer, 0, clen);
+    }
+    uint8_t clen1 = clen;
+    // 1byte compact peer format length
+    WRITE_CHECK(fp, &clen1, sizeof(clen1));
+    // 7bytes reserved
+    WRITE_CHECK(fp, zero, 7);
+    // clen bytes compact peer
+    WRITE_CHECK(fp, compactPeer, static_cast<size_t>(clen));
+    // 24-clen bytes reserved
+    WRITE_CHECK(fp, zero, static_cast<size_t>(24 - clen));
+    // 20bytes: node ID
+    WRITE_CHECK(fp, node->getID(), DHT_ID_LENGTH);
+    // 4bytes reserved
+    WRITE_CHECK(fp, zero, 4);
   }
+  if (fp.close() == EOF) {
+    throw DL_ABORT_EX(
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
+  }
+  if (!File(filenameTemp).renameTo(filename)) {
+    throw DL_ABORT_EX(
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));
+  }
+  A2_LOG_INFO('DHT routing table was saved successfully');
 }
     
-      virtual ~DHTUnknownMessage();
+      void update();
     
-        Number::Number(int32_t sign, int32_t exp, vector<uint32_t> const& mantissa) noexcept :
-        m_sign{ sign },
-        m_exp{ exp },
-        m_mantissa{ mantissa }
-    {}
+      // returns 'unknown'
+  virtual const std::string& getMessageType() const CXX11_OVERRIDE;
     
-            *this = Rational{ lhsRat };
-        destroyrat(lhsRat);
     
-            case IDC_RSHF:
+    {  return xfer;
+}
+    
+    
+    {};
+    
+    using namespace  ::osquery::extensions;
+    
+    
+    {};
+    
+      xfer += oprot->writeFieldBegin('response', ::apache::thrift::protocol::T_LIST, 2);
+  {
+    xfer += oprot->writeListBegin(::apache::thrift::protocol::T_MAP, static_cast<uint32_t>(this->response.size()));
+    std::vector<std::map<std::string, std::string> > ::const_iterator _iter18;
+    for (_iter18 = this->response.begin(); _iter18 != this->response.end(); ++_iter18)
+    {
+      {
+        xfer += oprot->writeMapBegin(::apache::thrift::protocol::T_STRING, ::apache::thrift::protocol::T_STRING, static_cast<uint32_t>((*_iter18).size()));
+        std::map<std::string, std::string> ::const_iterator _iter19;
+        for (_iter19 = (*_iter18).begin(); _iter19 != (*_iter18).end(); ++_iter19)
         {
-            if (m_fIntegerMode && result >= m_dwWordBitWidth) // Lsh/Rsh >= than current word size is always 0
-            {
-                throw CALC_E_NORESULT;
-            }
+          xfer += oprot->writeString(_iter19->first);
+          xfer += oprot->writeString(_iter19->second);
+        }
+        xfer += oprot->writeMapEnd();
+      }
+    }
+    xfer += oprot->writeListEnd();
+  }
+  xfer += oprot->writeFieldEnd();
+    
+    /**
+ * @brief Represents a hardware driver type that SMART api can you use to query
+ * device information.
+ *
+ * @param driver name of SMART controller driver
+ * @param maxID max ID number of which disks on the controller is monitored
+ */
+struct hardwareDriver {
+  std::string driver;
+  size_t maxID;
+};
+    
+    void tableCompletionFunction(char const* prefix, linenoiseCompletions* lc) {
+  auto tables = osquery::RegistryFactory::get().names('table');
+  size_t index = 0;
     }
     
     
-    {    Rational Sinh(Rational const& rat);
-    Rational Cosh(Rational const& rat);
-    Rational Tanh(Rational const& rat);
-    Rational ASinh(Rational const& rat);
-    Rational ACosh(Rational const& rat);
-    Rational ATanh(Rational const& rat);
+    {  return stream;
 }
+    
+    #pragma once
+    
+    
+    {
+    {} // namespace table_tests
+} // namespace osquery
 
     
-    #include 'pch.h'
-#include 'LiveRegionHost.h'
+    class kernelExtensions : public testing::Test {
+ protected:
+  void SetUp() override {
+    setUpEnvironment();
+  }
+};
     
-    INarratorAnnouncementHost^ NarratorAnnouncementHostFactory::s_hostProducer;
-vector<INarratorAnnouncementHost^> NarratorAnnouncementHostFactory::s_hosts;
+    TEST_F(kernelIntegrity, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from kernel_integrity');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'sycall_addr_modified', IntType}
+  //      {'text_segment_hash', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
     
-    using namespace CalculatorApp::Common::Automation;
-using namespace Windows::Foundation::Metadata;
-using namespace Windows::UI::Xaml::Automation;
-using namespace Windows::UI::Xaml::Automation::Peers;
-using namespace Windows::UI::Xaml::Controls;
+    TEST_F(KernelModules, test_sanity) {
+  QueryData data = execute_query('select * from kernel_modules');
+  ASSERT_GT(data.size(), 0ul);
+  ValidatatioMap row_map = {
+      {'name', NonEmptyString},
+      {'size', NonNegativeInt},
+      {'used_by', NonEmptyString},
+      {'status', NonEmptyString},
+      {'address', NonNegativeInt},
+  };
+  validate_rows(data, row_map);
+}
