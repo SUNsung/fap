d9d17aaa495d7412942b9162383531c864b2e0da
SUNsung <weatherius@gmail.com>
2019-04-03 21:16:18 +0300

    Rewrite [5386cZk0f9OEc53IGOAo0jrIXo53FDoPQRRt6NU53V1c.py] File in: 2019-04-03 21:16:17

--- pyton/5386cZk0f9OEc53IGOAo0jrIXo53FDoPQRRt6NU53V1c.py
+++ pyton/5386cZk0f9OEc53IGOAo0jrIXo53FDoPQRRt6NU53V1c.py
@@ -1,116 +1,141 @@
 
         
-            @property
-    def encoding(self):
-        return 'utf8'
+        # 128  --> 0.77094
+# 256  --> 0.85710
+# 512  --> 0.92635
+# 1024 --> 0.97130
+# 2048 --> 0.99431
+#
+# Ideal Distribution Ratio = 0.92635 / (1-0.92635) = 12.58
+# Random Distribution Ration = 512 / (2965+62+83+86-512) = 0.191
+#
+# Typical Distribution Ratio, 25% of IDR
     
+    win1250HungarianCharToOrderMap = (
+255,255,255,255,255,255,255,255,255,255,254,255,255,254,255,255,  # 00
+255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,  # 10
+253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,  # 20
+252,252,252,252,252,252,252,252,252,252,253,253,253,253,253,253,  # 30
+253, 28, 40, 54, 45, 32, 50, 49, 38, 39, 53, 36, 41, 34, 35, 47,
+ 46, 72, 43, 33, 37, 57, 48, 64, 68, 55, 52,253,253,253,253,253,
+253,  2, 18, 26, 17,  1, 27, 12, 20,  9, 22,  7,  6, 13,  4,  8,
+ 23, 67, 10,  5,  3, 21, 19, 65, 62, 16, 11,253,253,253,253,253,
+161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,
+177,178,179,180, 78,181, 69,182,183,184,185,186,187,188,189,190,
+191,192,193,194,195,196,197, 76,198,199,200,201,202,203,204,205,
+ 81,206,207,208,209,210,211,212,213,214,215,216,217,218,219,220,
+221, 51, 83,222, 80,223,224,225,226, 44,227,228,229, 61,230,231,
+232,233,234, 58,235, 66, 59,236,237,238, 60, 70, 63,239,240,241,
+ 84, 14, 75,242, 71, 82,243, 73,244, 15, 85, 79, 86, 30, 77, 87,
+245,246,247, 25, 74, 42, 24,248,249,250, 31, 56, 29,251,252,253,
+)
     
-def repr_dict_nice(d):
-    def prepare_dict(d):
-        for k, v in d.items():
-            if isinstance(v, dict):
-                v = dict(prepare_dict(v))
-            elif isinstance(v, bytes):
-                v = v.decode('utf8')
-            elif not isinstance(v, (int, str)):
-                v = repr(v)
-            yield k, v
-    return json.dumps(
-        dict(prepare_dict(d)),
-        indent=4, sort_keys=True,
-    )
+            self._last_char[0] = byte_str[-1]
     
-        '''
+    SJIS_CLS = (
+    1,1,1,1,1,1,1,1,  # 00 - 07
+    1,1,1,1,1,1,0,0,  # 08 - 0f
+    1,1,1,1,1,1,1,1,  # 10 - 17
+    1,1,1,0,1,1,1,1,  # 18 - 1f
+    1,1,1,1,1,1,1,1,  # 20 - 27
+    1,1,1,1,1,1,1,1,  # 28 - 2f
+    1,1,1,1,1,1,1,1,  # 30 - 37
+    1,1,1,1,1,1,1,1,  # 38 - 3f
+    2,2,2,2,2,2,2,2,  # 40 - 47
+    2,2,2,2,2,2,2,2,  # 48 - 4f
+    2,2,2,2,2,2,2,2,  # 50 - 57
+    2,2,2,2,2,2,2,2,  # 58 - 5f
+    2,2,2,2,2,2,2,2,  # 60 - 67
+    2,2,2,2,2,2,2,2,  # 68 - 6f
+    2,2,2,2,2,2,2,2,  # 70 - 77
+    2,2,2,2,2,2,2,1,  # 78 - 7f
+    3,3,3,3,3,2,2,3,  # 80 - 87
+    3,3,3,3,3,3,3,3,  # 88 - 8f
+    3,3,3,3,3,3,3,3,  # 90 - 97
+    3,3,3,3,3,3,3,3,  # 98 - 9f
+    #0xa0 is illegal in sjis encoding, but some pages does
+    #contain such byte. We need to be more error forgiven.
+    2,2,2,2,2,2,2,2,  # a0 - a7
+    2,2,2,2,2,2,2,2,  # a8 - af
+    2,2,2,2,2,2,2,2,  # b0 - b7
+    2,2,2,2,2,2,2,2,  # b8 - bf
+    2,2,2,2,2,2,2,2,  # c0 - c7
+    2,2,2,2,2,2,2,2,  # c8 - cf
+    2,2,2,2,2,2,2,2,  # d0 - d7
+    2,2,2,2,2,2,2,2,  # d8 - df
+    3,3,3,3,3,3,3,3,  # e0 - e7
+    3,3,3,3,3,4,4,4,  # e8 - ef
+    3,3,3,3,3,3,3,3,  # f0 - f7
+    3,3,3,3,3,0,0,0)  # f8 - ff
     
+    from .mbcharsetprober import MultiByteCharSetProber
+from .codingstatemachine import CodingStateMachine
+from .chardistribution import SJISDistributionAnalysis
+from .jpcntx import SJISContextAnalysis
+from .mbcssm import SJIS_SM_MODEL
+from .enums import ProbingState, MachineState
     
-def test_unicode_digest_auth(httpbin):
-    # it doesn't really authenticate us because httpbin
-    # doesn't interpret the utf8-encoded auth
-    http('--auth-type=digest',
-         '--auth', u'test:%s' % UNICODE,
-         httpbin.url + u'/digest-auth/auth/test/' + UNICODE)
+        def test_del_msg_failure_with_null_msg(self):
+        '''Del_msg fails: Returns 200 and does not set del_on_recipient.'''
+        message = MagicMock(spec=Message)
+        message.name = 'msg_3'
+        message.to_id = self.id
+        message.del_on_recipient = False
+    
+        @patch('r2.lib.authorize.interaction.Bid._new')
+    def test_auth_freebie_transaction(self, _new):
+        '''Test auth_freebie_transaction'''
+        link = Mock(spec=Link)
+        link._id = 99
+        amount = 100
+        campaign_id = 99
+    
+    import unittest
+    
+        def test_filtered_modules_do_not_include_filtered_sources(self):
+        test_files = ['foo.js', 'bar.js']
+        filtered_files = ['baz.js', 'qux.js']
+        all_files = test_files + filtered_files
+        filter_module = TestModule('filter_module', *filtered_files)
+        test_module = TestModule('test_module', filter_module=filter_module, *all_files)
+        self.assertEqual(test_module.build(), concat_sources(test_files))
 
     
-    from httpie import __version__
-from httpie.compat import is_windows
+        def test_ftue_nocreate(self):
+        request = MagicMock()
+        context = MagicMock()
+        request.cookies = {}
+        loid = LoId.load(request, context, create=False)
+        self.assertFalse(loid.new)
+        self.assertFalse(loid.serializable)
+        loid.save()
+        self.assertFalse(bool(context.cookies.add.called))
     
-                    if not speed:
-                    eta = '-:--:--'
-                else:
-                    s = int((self.status.total_size - downloaded) / speed)
-                    h, s = divmod(s, 60 * 60)
-                    m, s = divmod(s, 60)
-                    eta = '{0}:{1:0>2}:{2:0>2}'.format(h, m, s)
-    
-        for node in doctree.traverse(settingslist_node):
-        settings_list = nodes.bullet_list()
-        settings_list.extend([make_setting_element(d, app, fromdocname)
-                              for d in sorted(env.scrapy_all_settings,
-                                              key=itemgetter('setting_name'))
-                              if fromdocname != d['docname']])
-        node.replace_self(settings_list)
-    
-    # Ignore noisy twisted deprecation warnings
-import warnings
-warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
-del warnings
-    
-            if opts.nolog:
-            self.settings.set('LOG_ENABLED', False, priority='cmdline')
-    
-                tested_methods = conman.tested_methods_from_spidercls(spidercls)
-            if opts.list:
-                for method in tested_methods:
-                    contract_reqs[spidercls.name].append(method)
-            elif tested_methods:
-                self.crawler_process.crawl(spidercls)
-    
-            self.crawler_process.crawl(spname, **opts.spargs)
-        self.crawler_process.start()
-    
-        requires_project = True
-    default_settings = {'LOG_ENABLED': False}
-    
-            general form:
-        @returns request(s)/item(s) [min=1 [max]]
+            post = Link(is_self=True, selftext='''
+Some text here.
+https://example.com
+https://reddit.com/a.jpg/b''')
+        url = _get_scrape_url(post)
+        self.assertEqual(url, 'https://example.com')
+
     
     
-class S3DownloadHandler(object):
     
-    image = face_recognition.load_image_file('{}')
-'''
+        @patch('r2.models.Subreddit.user_subreddits')
+    def test_multi_logged_in(self, user_subreddits):
+        user_subreddits.return_value = subscriptions
+        multi = MultiReddit(path='/user/test/m/multi_test', srs=multi_subreddits)
+        srnames = srnames_from_site(self.logged_in, multi)
     
-    # Find all the faces in the image using the default HOG-based model.
-# This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# See also: find_faces_in_picture_cnn.py
-face_locations = face_recognition.face_locations(image)
+        def test_resize(self):
+        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
+                      height=800)
+        for width in (108, 216, 320, 640, 960, 1080):
+            url = self.provider.resize_image(image, width)
+            self.assertEqual(url, 'https://example.com/a.jpg?w=%d' % width)
     
-        # If no valid image file was uploaded, show the file upload form:
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
+    from r2.lib.providers.image_resizing.unsplashit import UnsplashitImageResizingProvider
     
-                face_names.append(name)
-    
-        # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(output)
-    print('Found {} faces in image.'.format(len(face_locations)))
-    face_encodings = face_recognition.face_encodings(output, face_locations)
-    
-            self.assertEqual(
-            set(face_landmarks[0].keys()),
-            set(['chin', 'left_eyebrow', 'right_eyebrow', 'nose_bridge',
-                 'nose_tip', 'left_eye', 'right_eye', 'top_lip',
-                 'bottom_lip']))
-        self.assertEqual(
-            face_landmarks[0]['chin'],
-            [(369, 220), (372, 254), (378, 289), (384, 322), (395, 353),
-             (414, 382), (437, 407), (464, 424), (495, 428), (527, 420),
-             (552, 399), (576, 372), (594, 344), (604, 314), (610, 282),
-             (613, 250), (615, 219)])
+        def test_invalid_comment(self):
+        testcase = '<!--[if IE 6]>WHAT YEAR IS IT?<![endif]-->'
+        self.assertFragmentRaises(testcase, SoupUnexpectedCommentError)
