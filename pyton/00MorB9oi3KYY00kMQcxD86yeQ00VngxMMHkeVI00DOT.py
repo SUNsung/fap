62f5f089cf2778bf8a833bbd3ecefe95b874c7d8
SUNsung <weatherius@gmail.com>
2019-04-04 01:20:45 +0300

    Rewrite [00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py] File in: 2019-04-04 01:20:44

--- pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
+++ pyton/00MorB9oi3KYY00kMQcxD86yeQ00VngxMMHkeVI00DOT.py
@@ -1,196 +1,179 @@
 
         
-            # Distributional noun compound
-    if self.hparams.input in ['dist-nc', 'integrated-nc']:
-      network_input += self.relata_dim
+          Args:
+    records: a record list with TensorFlow examples.
     
-      def __create_computation_graph__(self):
-    '''Initialize the model and define the graph.'''
-    self.lstm_input_dim = sum([self.hparams.lemma_dim, self.hparams.pos_dim,
-                               self.hparams.dep_dim, self.hparams.dir_dim])
-    self.lstm_output_dim = self.lstm_input_dim
+    from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
     
-      @property
-  def unk(self):
-    return self._unk
+      if epoch_size == 0:
+    raise ValueError('epoch_size == 0, decrease batch_size or num_steps')
     
-      # *Total* number of n-grams produced by the generator.
-  total_ngrams_produced = 0
+        Args:
+      hparams:  Hyperparameters for the MaskGAN.
+      data: Data to evaluate.
+      id_to_word: Dictionary of indices to words.
+      log_dir: Log directory.
+      output_file:  Output file for the samples.
+  '''
+  # Boolean indicating operational mode.
+  is_training = False
     
-        scikit_results = []
-    glmnet_results = []
-    n = 20
-    step = 500
-    n_features = 1000
-    n_informative = n_features / 10
-    n_test_samples = 1000
-    for i in range(1, n + 1):
-        print('==================')
-        print('Iteration %s of %s' % (i, n))
-        print('==================')
+      Args:
+    gen_logits: Generator logits.
+    gen_labels:  Labels for the correct token.
+    dis_predictions:  Discriminator predictions.
+    is_real_input:  Tensor indicating whether the label is present.
     
-        from sklearn.tree import DecisionTreeRegressor
+      for sequence in sequences:
+    indices = []
+    for embedding in sequence:
+      indices.append(np.argmax(embedding))
+    batch_of_indices.append(indices)
+  return batch_of_indices
     
+      with tf.variable_scope('dis', reuse=reuse):
+    cell_fwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
+        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
+    cell_bwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
+        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
+    if FLAGS.zoneout_drop_prob > 0.0:
+      cell_fwd = zoneout.ZoneoutWrapper(
+          cell_fwd,
+          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
+          is_training=is_training)
+      cell_bwd = zoneout.ZoneoutWrapper(
+          cell_bwd,
+          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
+          is_training=is_training)
     
-if __name__ == '__main__':
-    # NOTE: we put the following in a 'if __name__ == '__main__'' protected
-    # block to be able to use a multi-core grid search that also works under
-    # Windows, see: http://docs.python.org/library/multiprocessing.html#windows
-    # The multiprocessing module is used as the backend of joblib.Parallel
-    # that is used when n_jobs != 1 in GridSearchCV
+        def process_response(self, request, response):
+        # No need to check for a redirect for non-404 responses.
+        if response.status_code != 404:
+            return response
     
-    n_clusters = (4, 3)
-data, rows, columns = make_checkerboard(
-    shape=(300, 300), n_clusters=n_clusters, noise=10,
-    shuffle=False, random_state=0)
+        def delete(self, session_key=None):
+        if session_key is None:
+            if self.session_key is None:
+                return
+            session_key = self.session_key
+        self._cache.delete(self.cache_key_prefix + session_key)
     
-    import numpy as np
-from matplotlib import pyplot as plt
+        def save(self, must_create=False):
+        super().save(must_create)
+        self._cache.set(self.cache_key, self._session, self.get_expiry_age())
     
-    estimators = [('k_means_iris_8', KMeans(n_clusters=8)),
-              ('k_means_iris_3', KMeans(n_clusters=3)),
-              ('k_means_iris_bad_init', KMeans(n_clusters=3, n_init=1,
-                                               init='random'))]
+        def create(self):
+        while True:
+            self._session_key = self._get_new_session_key()
+            try:
+                # Save immediately to ensure we have a unique entry in the
+                # database.
+                self.save(must_create=True)
+            except CreateError:
+                # Key wasn't unique. Try again.
+                continue
+            self.modified = True
+            return
     
-    X_restored = agglo.inverse_transform(X_reduced)
-images_restored = np.reshape(X_restored, images.shape)
-plt.figure(1, figsize=(4, 3.5))
-plt.clf()
-plt.subplots_adjust(left=.01, right=.99, bottom=.01, top=.91)
-for i in range(4):
-    plt.subplot(3, 4, i + 1)
-    plt.imshow(images[i], cmap=plt.cm.gray, vmax=16, interpolation='nearest')
-    plt.xticks(())
-    plt.yticks(())
-    if i == 1:
-        plt.title('Original data')
-    plt.subplot(3, 4, 4 + i + 1)
-    plt.imshow(images_restored[i], cmap=plt.cm.gray, vmax=16,
-               interpolation='nearest')
-    if i == 1:
-        plt.title('Agglomerated data')
-    plt.xticks(())
-    plt.yticks(())
-    
-    # histogram
-plt.figure(4, figsize=(3, 2.2))
-plt.clf()
-plt.axes([.01, .01, .98, .98])
-plt.hist(X, bins=256, color='.5', edgecolor='.5')
-plt.yticks(())
-plt.xticks(regular_values)
-values = np.sort(values)
-for center_1, center_2 in zip(values[:-1], values[1:]):
-    plt.axvline(.5 * (center_1 + center_2), color='b')
-    
-    plt.subplot(224)
-plt.scatter(X_filtered[:, 0], X_filtered[:, 1], c=y_pred)
-plt.title('Unevenly Sized Blobs')
-    
-    # The following bandwidth can be automatically detected using
-bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
-    
-    # Scrapy version
-import pkgutil
-__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
-version_info = tuple(int(v) if v.isdigit() else v
-                     for v in __version__.split('.'))
-del pkgutil
-    
-        def short_desc(self):
-        return 'Run a spider'
-    
-            spidercls = DefaultSpider
-        spider_loader = self.crawler_process.spider_loader
-        if opts.spider:
-            spidercls = spider_loader.load(opts.spider)
-        else:
-            spidercls = spidercls_for_request(spider_loader, request, spidercls)
-        self.crawler_process.crawl(spidercls, start_requests=lambda: [request])
-        self.crawler_process.start()
-
-    
-        def syntax(self):
-        return '[options] <spider_file>'
-    
-    
-    class ScrapyClientTLSOptions(ClientTLSOptions):
+            label_suffix overrides the form's label_suffix.
         '''
-        SSL Client connection creator ignoring certificate verification errors
-        (for genuinely invalid certificates or bugs in verification code).
-    
-    def clean_pdf_link(link):
-    if 'arxiv' in link:
-        link = link.replace('abs', 'pdf')   
-        if not(link.endswith('.pdf')):
-            link = '.'.join((link, 'pdf'))
-    
-    def cut(sentence):
-    sentence = strdecode(sentence)
-    blocks = re_han.split(sentence)
-    for blk in blocks:
-        if re_han.match(blk):
-            for word in __cut(blk):
-                if word not in Force_Split_Words:
-                    yield word
+        contents = contents or self.label
+        if label_suffix is None:
+            label_suffix = (self.field.label_suffix if self.field.label_suffix is not None
+                            else self.form.label_suffix)
+        # Only add the suffix if the label does not end in punctuation.
+        # Translators: If found as last label character, these punctuation
+        # characters will prevent the default label_suffix to be appended to the label
+        if label_suffix and contents and contents[-1] not in _(':?.!'):
+            contents = format_html('{}{}', contents, label_suffix)
+        widget = self.field.widget
+        id_ = widget.attrs.get('id') or self.auto_id
+        if id_:
+            id_for_label = widget.id_for_label(id_)
+            if id_for_label:
+                attrs = {**(attrs or {}), 'for': id_for_label}
+            if self.field.required and hasattr(self.form, 'required_css_class'):
+                attrs = attrs or {}
+                if 'class' in attrs:
+                    attrs['class'] += ' ' + self.form.required_css_class
                 else:
-                    for c in word:
-                        yield c
+                    attrs['class'] = self.form.required_css_class
+            attrs = flatatt(attrs) if attrs else ''
+            contents = format_html('<label{}>{}</label>', attrs, contents)
         else:
-            tmp = re_skip.split(blk)
-            for x in tmp:
-                if x:
-                    yield x
+            contents = conditional_escape(contents)
+        return mark_safe(contents)
+    
+        def __repr__(self):
+        return '<{0} {1}>'.format(type(self).__name__, str(self))
 
     
+        def iter_body(self, chunk_size=1):
+        return self._orig.iter_content(chunk_size=chunk_size)
     
-def get_top_states(t_state_v, K=4):
-    return sorted(t_state_v, key=t_state_v.__getitem__, reverse=True)[:K]
-    
-    print(','.join(tags))
-
-    
-    t2 = time.time()
-tm_cost = t2-t1
-    
-        def test_merge_sort(self):
-        merge_sort = MergeSort()
+        # Will be set to the raw value of `-a` (if provided) before
+    # `get_auth()` gets called.
+    raw_auth = None
     
     
-def main():
-    test = TestSelectionSort()
-    selection_sort = SelectionSort()
-    test.test_selection_sort(selection_sort.sort)
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
+    
+            def get_auth(self, username=None, password=None):
+            assert self.raw_auth is None
+            assert username is None
+            assert password is None
+            return basic_auth()
+    
+    
+def test_default_options_overwrite(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['json'] == {'foo': 'bar'}
+    
+        good_nonce = jose.encode_b64jose(b'foo')
+    wrong_nonce = u'F'
+    # Following just makes sure wrong_nonce is wrong
     try:
-        test.test_selection_sort(selection_sort.sort_recursive)
-        test.test_selection_sort(selection_sort.sor_iterative_alt)
-    except NameError:
-        # Alternate solutions are only defined
-        # in the solutions file
-        pass
+        jose.b64decode(wrong_nonce)
+    except (ValueError, TypeError):
+        assert True
+    else:
+        assert False  # pragma: no cover
     
-        def get_sorted_stack(self, stack, numbers):
-        for x in numbers:
-            stack.push(x)
-        sorted_stack = stack.sort()
-        return sorted_stack
+    # Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
     
-            print('Test: More than one element')
-        stack = Stack()
-        stack.push(1)
-        stack.push(2)
-        stack.push(3)
-        assert_equal(stack.pop(), 3)
-        assert_equal(stack.peek(), 2)
-        assert_equal(stack.pop(), 2)
-        assert_equal(stack.peek(), 1)
-        assert_equal(stack.is_empty(), False)
-        assert_equal(stack.pop(), 1)
-        assert_equal(stack.peek(), None)
-        assert_equal(stack.is_empty(), True)
     
-    		print('Test: insert checking with in order traversal')
-		expectVal = [7, 10, 25, 30, 38, 40, 50, 60, 70, 80]
-		assert_equal(myTree.printInOrder(), expectVal)
-		expectVal = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91]
-		assert_equal(myTree2.printInOrder(), expectVal)
+def get_file_path(vhost_path):
+    '''Get file path from augeas_vhost_path.
+    
+    from certbot_apache.tests import util
+    
+    注意 x 经过 Highway 之后维度应该保持不变
+    
+    
+class L1L2Regularizer(object):
+    '''L1 L2 正则化
+    
+    
+def get_w(shape,
+          w_initializer=truncated_normal,
+          w_regularizer=l2_regularizer,
+          name=None):
+    name = name or 'W'
+    W = tf.get_variable(name, shape, dtype=tf_float, initializer=w_initializer,
+                        regularizer=w_regularizer)
+    return W
+    
+    import tensorflow as tf
+import keras.backend as K
