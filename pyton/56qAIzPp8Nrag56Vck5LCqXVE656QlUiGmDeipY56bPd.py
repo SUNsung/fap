e2e453a09dad763d7c0fb2280b3eabaac2fdd8e6
SUNsung <weatherius@gmail.com>
2019-04-04 11:37:42 +0300

    Rewrite [56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py] File in: 2019-04-04 11:37:42

--- pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
+++ pyton/56qAIzPp8Nrag56Vck5LCqXVE656QlUiGmDeipY56bPd.py
@@ -1,94 +1,146 @@
 
         
-          If my_dataset (filename) stem is in the directory, the read routine will try
-  and load it.  The datasets dictionary will then look like
-  dataset['first_day'] -> (first day data dictionary)
-  dataset['second_day'] -> (first day data dictionary)
+            auto_json = args.data and not args.form
+    if args.json or auto_json:
+        default_headers['Accept'] = JSON_ACCEPT
+        if args.json or (auto_json and args.data):
+            default_headers['Content-Type'] = JSON_CONTENT_TYPE
     
-    flags = tf.app.flags
-flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
-                    'Directory for saving data.')
-flags.DEFINE_string('datafile_name', 'thits_data',
-                    'Name of data file for input case.')
-flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
-flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
-flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
-flags.DEFINE_integer('C', 100, 'Number of conditions')
-flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
-flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
-flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
-flags.DEFINE_float('train_percentage', 4.0/5.0,
-                   'Percentage of train vs validation trials')
-flags.DEFINE_integer('nreplications', 40,
-                     'Number of noise replications of the same underlying rates.')
-flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
-flags.DEFINE_float('x0_std', 1.0,
-                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
-flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
-flags.DEFINE_float('dt', 0.010, 'Time bin')
-flags.DEFINE_float('input_magnitude', 20.0,
-                   'For the input case, what is the value of the input?')
-flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
-FLAGS = flags.FLAGS
-    
-      Args:
-    gd_file: GraphDef proto text file.
-    ckpt_file: TensorFlow Checkpoint file.
-    
-          scorings.append(dict(
-          correctness=correctness,
-          sentence=sentence,
-          joint_prob=joint_prob,
-          word_probs=word_probs))
-    scoring_mode = 'full' if self.test_data_name == 'pdp60' else 'partial'
-    return utils.compare_substitutions(
-        self.question_ids, scorings, scoring_mode)
-    
-                handler_result = self.handler(sock)
-    
-            return response
-    
-    '''
-requests.compat
-~~~~~~~~~~~~~~~
-    
-        Wraps CookieJar.clear(), is O(n).
-    '''
-    clearables = []
-    for cookie in cookiejar:
-        if cookie.name != name:
-            continue
-        if domain is not None and domain != cookie.domain:
-            continue
-        if path is not None and path != cookie.path:
-            continue
-        clearables.append((cookie.domain, cookie.path, cookie.name))
-    
-    def _init():
-    for code, titles in _codes.items():
-        for title in titles:
-            setattr(codes, title, code)
-            if not title.startswith(('\\', '/')):
-                setattr(codes, title.upper(), code)
+        def iter_body(self):
+        # Read the whole body before prettifying it,
+        # but bail out immediately if the body is binary.
+        converter = None
+        body = bytearray()
     
     
-def get_netrc_auth(url, raise_errors=False):
-    '''Returns the Requests tuple auth for a given url from netrc.'''
+ENTRY_POINT_NAMES = [
+    'httpie.plugins.auth.v1',
+    'httpie.plugins.formatter.v1',
+    'httpie.plugins.converter.v1',
+    'httpie.plugins.transport.v1',
+]
     
-        def deregister_hook(self, event, hook):
-        '''Deregister a previously registered hook.
-        Returns True if the hook existed, False if not.
+        def test_GET_no_data_no_auto_headers(self, httpbin):
+        # https://github.com/jakubroztocil/httpie/issues/62
+        r = http('GET', httpbin.url + '/headers')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == '*/*'
+        assert 'Content-Type' not in r.json['headers']
+    
+    
+def test_unicode_raw_json_item(httpbin):
+    r = http('--json', 'POST', httpbin.url + '/post',
+             u'test:={ '%s' : [ '%s' ] }' % (UNICODE, UNICODE))
+    assert HTTP_OK in r
+    assert r.json['json'] == {'test': {UNICODE: [UNICODE]}}
+    
+        def test_request_body_from_file_by_path_no_field_name_allowed(
+            self, httpbin):
+        env = MockEnvironment(stdin_isatty=True)
+        r = http('POST', httpbin.url + '/post', 'field-name@' + FILE_PATH_ARG,
+                 env=env, error_exit_ok=True)
+        assert 'perhaps you meant --form?' in r.stderr
+    
+        def delete(self):
+        try:
+            os.unlink(self.path)
+        except OSError as e:
+            if e.errno != errno.ENOENT:
+                raise
+    
         '''
+    from .client import get_requests_kwargs, dump_request
+    if os.path.sep in session_name:
+        path = os.path.expanduser(session_name)
+    else:
+        hostname = (args.headers.get('Host', None)
+                    or urlsplit(args.url).netloc.split('@')[-1])
+        if not hostname:
+            # HACK/FIXME: httpie-unixsocket's URLs have no hostname.
+            hostname = 'localhost'
     
-        return diff
+        msg = Message('Content-Disposition: %s' % content_disposition)
+    filename = msg.get_filename()
+    if filename:
+        # Basic sanitation.
+        filename = os.path.basename(filename).lstrip('.').strip()
+        if filename:
+            return filename
+    
+            start = time.time()
+        func(X, n_jobs=-1)
+        multi_core.append(time.time() - start)
+    
+                gc.collect()
+            print('- benchmarking SGD')
+            clf = SGDRegressor(alpha=alpha / n_train, fit_intercept=False,
+                               max_iter=max_iter, learning_rate='invscaling',
+                               eta0=.01, power_t=0.25, tol=1e-3)
+    
+        try:
+        fn = inspect.getsourcefile(obj)
+    except Exception:
+        fn = None
+    if not fn:
+        try:
+            fn = inspect.getsourcefile(sys.modules[obj.__module__])
+        except Exception:
+            fn = None
+    if not fn:
+        return
+    
+    
+def generate_data(case, sparse=False):
+    '''Generate regression/classification data.'''
+    bunch = None
+    if case == 'regression':
+        bunch = datasets.load_boston()
+    elif case == 'classification':
+        bunch = datasets.fetch_20newsgroups_vectorized(subset='all')
+    X, y = shuffle(bunch.data, bunch.target)
+    offset = int(X.shape[0] * 0.8)
+    X_train, y_train = X[:offset], y[:offset]
+    X_test, y_test = X[offset:], y[offset:]
+    if sparse:
+        X_train = csr_matrix(X_train)
+        X_test = csr_matrix(X_test)
+    else:
+        X_train = np.array(X_train)
+        X_test = np.array(X_test)
+    y_test = np.array(y_test)
+    y_train = np.array(y_train)
+    data = {'X_train': X_train, 'X_test': X_test, 'y_train': y_train,
+            'y_test': y_test}
+    return data
+    
+    model = SpectralBiclustering(n_clusters=n_clusters, method='log',
+                             random_state=0)
+model.fit(data)
+score = consensus_score(model.biclusters_,
+                        (rows[:, row_idx], columns[:, col_idx]))
+    
+    print('consensus score: {:.3f}'.format(score))
+    
+    
+X, y = nudge_images(X, y)
+    
+    *References:
+http://stackoverflow.com/questions/1514120/python-implementation-of-the-object-pool-design-pattern
+https://sourcemaking.com/design_patterns/object_pool
+    
+        def test_extended_properties(self):
+        print(u'John's relatives: {0}'.format(self.John.relatives))
+        self.assertDictEqual(
+            {'name': 'John', 'occupation': 'Coder', 'relatives': 'Many relatives.', 'call_count2': 0},
+            self.John.__dict__,
+        )
+    
+        # if not sample_queue.empty():
 
     
-    # frequency taken from http://en.wikipedia.org/wiki/Letter_frequency
-englishLetterFreq = {'E': 12.70, 'T': 9.06, 'A': 8.17, 'O': 7.51, 'I': 6.97,
-                     'N': 6.75, 'S': 6.33, 'H': 6.09, 'R': 5.99, 'D': 4.25,
-                     'L': 4.03, 'C': 2.78, 'U': 2.76, 'M': 2.41, 'W': 2.36,
-                     'F': 2.23, 'G': 2.02, 'Y': 1.97, 'P': 1.93, 'B': 1.29,
-                     'V': 0.98, 'K': 0.77, 'J': 0.15, 'X': 0.15, 'Q': 0.10,
-                     'Z': 0.07}
-ETAOIN = 'ETAOINSHRDLCUMWFGYPBVKJXQZ'
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        def test_human_adapter_shall_make_noise(self):
+        human = Human()
+        human_adapter = Adapter(human, make_noise=human.speak)
+        noise = human_adapter.make_noise()
+        expected_noise = ''hello''
+        self.assertEqual(noise, expected_noise)
