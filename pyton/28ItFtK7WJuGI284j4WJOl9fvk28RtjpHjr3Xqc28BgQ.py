6842c1e3922959502f954f3f53a01c7a2c8f10b7
SUNsung <weatherius@gmail.com>
2019-04-04 22:42:37 +0300

    Rewrite [28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py] File in: 2019-04-04 22:42:36

--- pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
+++ pyton/28ItFtK7WJuGI284j4WJOl9fvk28RtjpHjr3Xqc28BgQ.py
@@ -1,124 +1,53 @@
 
         
-        flags = tf.app.flags
-flags.DEFINE_string('save_dir', '/tmp/' + DATA_DIR + '/',
-                    'Directory for saving data.')
-flags.DEFINE_string('datafile_name', 'thits_data',
-                    'Name of data file for input case.')
-flags.DEFINE_string('noise_type', 'poisson', 'Noise type for data.')
-flags.DEFINE_integer('synth_data_seed', 5, 'Random seed for RNN generation.')
-flags.DEFINE_float('T', 1.0, 'Time in seconds to generate.')
-flags.DEFINE_integer('C', 100, 'Number of conditions')
-flags.DEFINE_integer('N', 50, 'Number of units for the RNN')
-flags.DEFINE_integer('S', 50, 'Number of sampled units from RNN')
-flags.DEFINE_integer('npcs', 10, 'Number of PCS for multi-session case.')
-flags.DEFINE_float('train_percentage', 4.0/5.0,
-                   'Percentage of train vs validation trials')
-flags.DEFINE_integer('nreplications', 40,
-                     'Number of noise replications of the same underlying rates.')
-flags.DEFINE_float('g', 1.5, 'Complexity of dynamics')
-flags.DEFINE_float('x0_std', 1.0,
-                   'Volume from which to pull initial conditions (affects diversity of dynamics.')
-flags.DEFINE_float('tau', 0.025, 'Time constant of RNN')
-flags.DEFINE_float('dt', 0.010, 'Time bin')
-flags.DEFINE_float('input_magnitude', 20.0,
-                   'For the input case, what is the value of the input?')
-flags.DEFINE_float('max_firing_rate', 30.0, 'Map 1.0 of RNN to a spikes per second')
-FLAGS = flags.FLAGS
+            # I and J are used interchangeably to allow
+    # us to use a 5x5 table (25 letters)
+    alphabet = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
+    # we're using a list instead of a '2d' array because it makes the math 
+    # for setting up the table and doing the actual encoding/decoding simpler
+    table = []
     
-    # write out the dataset
-write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
-print ('Saved to ', os.path.join(FLAGS.save_dir,
-                                 FLAGS.datafile_name + '_' + dataset_name))
-
+        def bulk_insert(self, values):
+        i = 1
+        self.__aux_list = values
+        for value in values:
+            self.insert_data(value)
+            self._step_by_step(i)
+            i += 1
     
+            print('****************** Testing Edit Distance DP Algorithm ******************')
+        print()
     
-def split_list_by_inds(data, inds1, inds2):
-  '''Take the data, a list, and split it up based on the indices in inds1 and
-  inds2.
-  Args:
-    data: the list of data to split
-    inds1, the first list of indices
-    inds2, the second list of indices
-  Returns: a 2-tuple of two lists.
-  '''
-  if data is None or len(data) == 0:
-    return [], []
-  else:
-    dout1 = [data[i] for i in inds1]
-    dout2 = [data[i] for i in inds2]
-    return dout1, dout2
+    '''
+The number of partitions of a number n into at least k parts equals the number of partitions into exactly k parts
+plus the number of partitions into at least k-1 parts. Subtracting 1 from each part of a partition of n into k parts
+gives a partition of n-k into k parts. These two facts together are used for this algorithm.
+'''
+def partition(m):
+	memo = [[0 for _ in xrange(m)] for _ in xrange(m+1)]
+	for i in xrange(m+1):
+		memo[i][0] = 1
     
-      # Averages for real and fake token values.
-  real = tf.mul(values, present)
-  fake = tf.mul(values, missing)
-  real_avg = tf.reduce_sum(real) / real_count
-  fake_avg = tf.reduce_sum(fake) / fake_count
-    
-        r_t = log D(x_t|x_0,x_1,...x_{t-1})
+    	TEMPORARY_ARRAY = [ element for element in ARRAY[1:] if element >= PIVOT ]
+	TEMPORARY_ARRAY = [PIVOT] + longestSub(TEMPORARY_ARRAY)
+	if ( len(TEMPORARY_ARRAY) > len(LONGEST_SUB) ):
+		return TEMPORARY_ARRAY
+	else:
+		return LONGEST_SUB
     
     
-def construct_ngrams_dict(ngrams_list):
-  '''Construct a ngram dictionary which maps an ngram tuple to the number
-  of times it appears in the text.'''
-  counts = {}
+class SubArray:
     
-    import tensorflow as tf
+    def reformatHex(i):
+	'''[summary]
+	Converts the given integer into 8-digit hex number.
     
+    * @author chinmoy159
+* @version 1.0 dated 10/08/2017
+'''
     
-    {    # Options that need a file parameter
-    'download-archive': ['--require-parameter'],
-    'cookies': ['--require-parameter'],
-    'load-info': ['--require-parameter'],
-    'batch-file': ['--require-parameter'],
-}
-    
-    ZSH_COMPLETION_FILE = 'youtube-dl.zsh'
-ZSH_COMPLETION_TEMPLATE = 'devscripts/zsh-completion.in'
-    
-        if not app.config.edit_on_github_project:
-        warnings.warn('edit_on_github_project not specified')
-        return
-    if not doctree:
-        warnings.warn('doctree is None')
-        return
-    path = os.path.relpath(doctree.get('source'), app.builder.srcdir)
-    show_url = get_github_url(app, 'blob', path)
-    edit_url = get_github_url(app, 'edit', path)
-    
-    # General information about the project.
-project = PROJECT_NAME
-copyright = PROJECT_COPYRIGHT
-author = PROJECT_AUTHOR
-    
-        @property
-    def targets(self):
-        '''Return a dictionary of registered targets.'''
-        return {'test target name': 'test target id'}
-    
-            # non-broadcast message
-        else:
-            for target in targets:
-                # If the target starts with a '+', it's a phone number,
-                # otherwise it's a user id.
-                if target.startswith('+'):
-                    recipient = {'phone_number': target}
-                else:
-                    recipient = {'id': target}
-    
-        def __init__(self, username, access_token):
-        '''Initialize the service.'''
-        from freesms import FreeClient
-        self.free_client = FreeClient(username, access_token)
-    
-        def send_message(self, message='', **kwargs):
-        '''Send a message to a user.'''
-        _LOGGER.debug('Sending to: %s, %s', self._recipient, str(self._device))
-        data = {
-            'secret': self._secret,
-            'to': self._recipient,
-            'device': self._device,
-            'payload': message,
-        }
-    
-    DEPENDENCIES = ['mycroft']
+    letters = [letter for letter in string.ascii_letters]
+digits = [digit for digit in string.digits]
+symbols = [symbol for symbol in string.punctuation]
+chars = letters + digits + symbols
+random.shuffle(chars)
