fd4c8731eec4734c024108210efb5e02de22d317
SUNsung <weatherius@gmail.com>
2019-04-04 09:37:05 +0300

    Rewrite [49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb] File in: 2019-04-04 09:37:04

--- ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
+++ ruby/49LWFspIchO7U49XHqs9LQ3gw249bkftXqhWEJM491h..rb
@@ -1,93 +1,33 @@
 
         
-          def ring_time
-    (self.ring_finish || Time.now).to_i - self.ring_start.to_i
-  end
+            BUNDLE_DIR = ::File.join(LOGSTASH_HOME, 'vendor', 'bundle')
+    GEMFILE_PATH = ::File.join(LOGSTASH_HOME, 'Gemfile')
+    LOCAL_GEM_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'local_gems')
+    CACHE_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'cache')
+    LOCKFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile.lock'))
+    GEMFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile'))
     
-              case msg_type
-          when Rex::Proto::Kerberos::Model::KRB_ERROR
-            res = Rex::Proto::Kerberos::Model::KrbError.decode(asn1)
-          when Rex::Proto::Kerberos::Model::AS_REP
-            res = Rex::Proto::Kerberos::Model::KdcResponse.decode(asn1)
-          when Rex::Proto::Kerberos::Model::TGS_REP
-            res = Rex::Proto::Kerberos::Model::KdcResponse.decode(asn1)
-          else
-            raise ::RuntimeError, 'Kerberos Client: Unknown response'
-          end
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Time into an String
-          #
-          # @return [String] encoded time
-          def encode
-            encoded = ''
-            encoded << encode_auth_time
-            encoded << encode_start_time
-            encoded << encode_end_time
-            encoded << encode_renew_time
-    
-                decode_asn1(asn1)
-          end
-    
-              # Decodes the enc_part
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::EncryptedData]
-          def decode_enc_part(input)
-            Rex::Proto::Kerberos::Model::EncryptedData.decode(input.value[0])
-          end
+            if explicit_plugins.any? { |spec| filename =~ /^#{spec.name}/ }
+          FileUtils.mv(gem_file, ::File.join(explicit_path, filename))
+        else
+          FileUtils.mv(gem_file, ::File.join(dependencies_path, filename))
         end
       end
     end
-  end
-end
     
-          # Wraps page formatted data to Nokogiri::HTML document.
-      #
-      def build_document(content)
-        Nokogiri::HTML::fragment(%{<div id='gollum-root'>} + content.to_s + %{</div>}, 'UTF-8')
-      end
-    
-      test 'extracting paths from URLs' do
-    assert_nil extract_path('Eye-Of-Sauron')
-    assert_equal 'Mordor', extract_path('Mordor/Sauron')
-    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
-  end
-    
-        @view = Precious::Views::Page.new
-    @view.instance_variable_set :@page, page
-    @view.instance_variable_set :@content, page.formatted_data
-    @view.instance_variable_set :@h1_title, true
-    
-        page = @wiki.page('k')
-    assert_equal '한글 text', utf8(page.raw_data)
-    assert_equal 'def', page.version.message
-  end
-    
-          ws  = WorkSpace.new(binding)
-      irb = Irb.new(ws)
-    
-      s.require_paths = %w[lib]
-    
-      # for now
-  worker_count = 1
-    
-    class SinatraWorker
-  include Sidekiq::Worker
-    
-        UnitOfWork = Struct.new(:queue, :job) do
-      def acknowledge
-        # nothing to do
-      end
-    
-        def redis(&block)
-      Sidekiq.redis(&block)
+        before do
+      logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version #{previous_version} #{plugin_name}')
+      # Logstash won't update when we have a pinned version in the gemfile so we remove them
+      logstash.replace_in_gemfile(',[[:space:]]'0.1.0'', '')
+      expect(logstash).to have_installed?(plugin_name, previous_version)
     end
     
-          if file.kind_of?(String)
-        ERB.new(file).result(binding)
-      else
-        send(:'_erb_#{file}')
-      end
+        def pane_index
+      index + tab.project.pane_base_index
     end
-  end
-end
+    
+      describe '#get_pane_base_index' do
+    it 'extracts pane-base-index from the global tmux window options' do
+      allow_any_instance_of(Kernel).to receive(:`).
+        with(Regexp.new('tmux.+ show-window-option -g pane-base-index')).
+        and_return('pane-base-index 3\n')
