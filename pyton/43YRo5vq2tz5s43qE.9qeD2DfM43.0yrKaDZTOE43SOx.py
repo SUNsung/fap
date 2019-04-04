3d747c1a2d7e5bc010d431596e22191fc034c3ad
SUNsung <weatherius@gmail.com>
2019-04-04 21:02:05 +0300

    Rewrite [43YRo5vq2tz5s43qE.9qeD2DfM43.0yrKaDZTOE43SOx.py] File in: 2019-04-04 21:02:02

--- pyton/43YRo5vq2tz5s43qE.9qeD2DfM43.0yrKaDZTOE43SOx.py
+++ pyton/43YRo5vq2tz5s43qE.9qeD2DfM43.0yrKaDZTOE43SOx.py
@@ -1,212 +1,150 @@
 
         
-        
-class CallState(Enum):
+            def take_call(self, call):
+        '''Assume the employee will always successfully take the call.'''
+        self.call = call
+        self.call.employee = self
+        self.call.state = CallState.IN_PROGRESS
     
-        def deal_card(self):
-        try:
-            card = self.cards[self.deal_index]
-            card.is_available = False
-            self.deal_index += 1
-        except IndexError:
-            return None
-        return card
+        def __init__(self, key, value):
+        self.key = key
+        self.value = value
     
-        def steps(self):
-        '''Run the map and reduce steps.'''
-        return [
-            self.mr(mapper=self.mapper,
-                    reducer=self.reducer)
-        ]
+        def crawl(self):
+        while True:
+            page = self.data_store.extract_max_priority_page()
+            if page is None:
+                break
+            if self.data_store.crawled_similar(page.signature):
+                self.data_store.reduce_priority_link_to_crawl(page.url)
+            else:
+                self.crawl_page(page)
+            page = self.data_store.extract_max_priority_page()
+
     
-            (2016-01, url0), 1
-        (2016-01, url0), 1
-        (2016-01, url1), 1
-        '''
-        url = self.extract_url(line)
-        period = self.extract_year_month(line)
-        yield (period, url), 1
-    
-    
-class LookupService(object):
-    
-    
-class Page(object):
-    
-          # And finally, divide pair_path_embeddings by num_paths element-wise.
-      self.pair_path_embeddings = tf.div(
-          self.pair_path_embeddings, self.num_paths)
-      vec_inputs.append(self.pair_path_embeddings)
-    
-      if output_dist == 'poisson':
-    rates = means = conversion_factor * model_vals['output_dist_params']
-    plot_time_series(rates, bidx, n_to_plot=n_to_plot, scale=scale,
-                     title=col_title + ' rates (LFADS - red, Truth - black)')
-  elif output_dist == 'gaussian':
-    means_vars = model_vals['output_dist_params']
-    means, vars = np.split(means_vars,2, axis=2) # bxtxn
-    stds = np.sqrt(vars)
-    plot_time_series(means, bidx, n_to_plot=n_to_plot, scale=scale,
-                     title=col_title + ' means (LFADS - red, Truth - black)')
-    plot_time_series(means+stds, bidx, n_to_plot=n_to_plot, scale=scale,
-                     color='c')
-    plot_time_series(means-stds, bidx, n_to_plot=n_to_plot, scale=scale,
-                     color='c')
-  else:
-    assert 'NIY'
-    
-      Args:
-    sentence: Sentence words.
-    vocab: Vocabulary. Contains max word chard id length and converts between
-        words and ids.
-  '''
-  targets = np.zeros([BATCH_SIZE, NUM_TIMESTEPS], np.int32)
-  weights = np.ones([BATCH_SIZE, NUM_TIMESTEPS], np.float32)
-    
-      Args:
-    values: Value given by the Wasserstein Discriminator of shape [batch_size,
-      sequence_length] to an imputed batch (real and fake).
-    is_real_input: tf.bool Tensor of shape [batch_size, sequence_length]. If
-      true, it indicates that the label is known.
-    
-    
-def zip_seq_pred_crossent(id_to_word, sequences, predictions, cross_entropy):
-  '''Zip together the sequences, predictions, cross entropy.'''
-  indices = convert_to_indices(sequences)
-    
-      # Unstack Tensors into lists.
-  rewards_list = tf.unstack(rewards, axis=1)
-  log_probs_list = tf.unstack(log_probs, axis=1)
-  missing = 1. - tf.cast(present, tf.float32)
-  missing_list = tf.unstack(missing, axis=1)
-    
-        # Maximize reward.
-    gen_grads = tf.gradients(-final_gen_reward, gen_vars)
-    gen_grads_clipped, _ = tf.clip_by_global_norm(gen_grads,
-                                                  FLAGS.grad_clipping)
-    maximize_op = gen_optimizer.apply_gradients(
-        zip(gen_grads_clipped, gen_vars), global_step=global_step)
-    
-            if row1 == row2:
-            plaintext += table[row1*5+(col1-1)%5]
-            plaintext += table[row2*5+(col2-1)%5]
-        elif col1 == col2:
-            plaintext += table[((row1-1)%5)*5+col1]
-            plaintext += table[((row2-1)%5)*5+col2]
-        else: # rectangle
-            plaintext += table[row1*5+col2]
-            plaintext += table[row2*5+col1]
-    
-    	if len(sys.argv) == 1:
-		try:
-			n = int(raw_input('Enter a number: '))
-			print(partition(n))
-		except ValueError:
-			print('Please enter a number.')
-	else:
-		try:
-			n = int(sys.argv[1])
-			print(partition(n))
-		except ValueError:
-			print('Please pass a number.')
-    
-            if self.depth == 1:
-            self.prediction = np.mean(y)
-            return
-    
-        log_predict = np.log(predict+1)
-    log_actual = np.log(actual+1)
-    
-    '''
-* Wondering how this method works !
-* It's pretty simple.
-* Let's say you need to calculate a ^ b
-* RULE 1 : a ^ b = (a*a) ^ (b/2) ---- example : 4 ^ 4 = (4*4) ^ (4/2) = 16 ^ 2
-* RULE 2 : IF b is ODD, then ---- a ^ b = a * (a ^ (b - 1)) :: where (b - 1) is even.
-* Once b is even, repeat the process to get a ^ b
-* Repeat the process till b = 1 OR b = 0, because a^1 = a AND a^0 = 1
-*
-* As far as the modulo is concerned,
-* the fact : (a*b) % c = ((a%c) * (b%c)) % c
-* Now apply RULE 1 OR 2 whichever is required.
+    Options:
+  -h  This help text.
+  -q  Loggable output - no progress indicator
+  -qq No output except for errors
+  -d  Download only - do NOT install or unpack archives
+  -s  No-act. Perform ordering simulation
+  -y  Assume Yes to all queries and do not prompt
+  -f  Attempt to correct a system with broken dependencies in place
+  -m  Attempt to continue if archives are unlocatable
+  -u  Show a list of upgraded packages as well
+  -b  Build the source package after fetching it
+  -V  Show verbose version numbers
+  -c=? Read this configuration file
+  -o=? Set an arbitrary configuration option, eg -o dir::cache=/tmp
+See the apt-get(8), sources.list(5) and apt.conf(5) manual
+pages for more information and options.
+                       This APT has Super Cow Powers.
 '''
-
+apt_get_operations = ['update', 'upgrade', 'install', 'remove', 'autoremove',
+                      'purge', 'source', 'build-dep', 'dist-upgrade',
+                      'dselect-upgrade', 'clean', 'autoclean', 'check',
+                      'changelog', 'download']
     
-        freqOrder = []
-    for freqPair in freqPairs:
-        freqOrder.append(freqPair[1])
+        kwargs = {
+        'stream': True,
+        'method': args.method.lower(),
+        'url': args.url,
+        'headers': headers,
+        'data': data,
+        'verify': {
+            'yes': True,
+            'true': True,
+            'no': False,
+            'false': False,
+        }.get(args.verify.lower(), args.verify),
+        'cert': cert,
+        'timeout': args.timeout,
+        'auth': args.auth,
+        'proxies': {p.key: p.value for p in args.proxy},
+        'files': args.files,
+        'allow_redirects': args.follow,
+        'params': args.params,
+    }
+    
+            # Keyword arguments > stream.encoding > default utf8
+        if self.stdin_encoding is None:
+            self.stdin_encoding = getattr(
+                self.stdin, 'encoding', None) or 'utf8'
+        if self.stdout_encoding is None:
+            actual_stdout = self.stdout
+            if is_windows:
+                # noinspection PyUnresolvedReferences
+                from colorama import AnsiToWin32
+                if isinstance(self.stdout, AnsiToWin32):
+                    actual_stdout = self.stdout.wrapped
+            self.stdout_encoding = getattr(
+                actual_stdout, 'encoding', None) or 'utf8'
     
     
-def get_scanner(hass, config):
-    '''Return the Swisscom device scanner.'''
-    scanner = SwisscomDeviceScanner(config[DOMAIN])
+def test_default_options(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http(httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['form'] == {'foo': 'bar'}
     
-                if self.add_timestamp:
-                text = '{} {}\n'.format(dt_util.utcnow().isoformat(), message)
-            else:
-                text = '{}\n'.format(message)
-            file.write(text)
-
+        def test_GET_with_data_auto_JSON_headers(self, httpbin):
+        # JSON headers should automatically be set also for GET with data.
+        r = http('POST', httpbin.url + '/post', 'a=b')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == JSON_ACCEPT
+        assert r.json['headers']['Content-Type'] == 'application/json'
     
-        def _get_room(self, room):
-        '''Get Room object, creating it if necessary.'''
-        from hipnotify import Room
-        if room not in self._rooms:
-            self._rooms[room] = Room(
-                token=self._token, room_id=room, endpoint_url=self._host)
-        return self._rooms[room]
-    
-    _LOGGER = logging.getLogger(__name__)
-    
-        def assert_same_dict(self, data, expected_data, prefix=None):
-        prefix = prefix or []
-        for k in set(data.keys() + expected_data.keys()):
-            current_prefix = prefix + [k]
-            want = expected_data.get(k)
-            got = data.get(k)
-            if isinstance(want, dict) and isinstance(got, dict):
-                self.assert_same_dict(got, want, prefix=current_prefix)
-            else:
-                self.assertEqual(
-                    got, want,
-                    'Mismatch for %s: %r != %r' % (
-                        '.'.join(current_prefix), got, want
+        def load(self):
+        try:
+            with open(self.path, 'rt') as f:
+                try:
+                    data = json.load(f)
+                except ValueError as e:
+                    raise ValueError(
+                        'Invalid %s JSON: %s [%s]' %
+                        (type(self).__name__, str(e), self.path)
                     )
-                )
+                self.update(data)
+        except IOError as e:
+            if e.errno != errno.ENOENT:
+                raise
     
-            get_or_create_customer_profile(self.user)
+    
+SESSIONS_DIR_NAME = 'sessions'
+DEFAULT_SESSIONS_DIR = os.path.join(DEFAULT_CONFIG_DIR, SESSIONS_DIR_NAME)
+VALID_SESSION_NAME_PATTERN = re.compile('^[a-zA-Z0-9_.-]+$')
+# Request headers starting with these prefixes won't be stored in sessions.
+# They are specific to each request.
+# http://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Requests
+SESSION_IGNORED_HEADER_PREFIXES = ['Content-', 'If-']
+    
+                with open('htmlout.html', 'w') as out:
+                out.write(header)
+    
+            a = softmax(tf.matmul(x, W) + b)  # attention
+        o = tf.multiply(x, a)
+    
+    from ..utils import get_wb
+from ..activations import relu
+from ..activations import linear
     
     
-class TestModule(js.Module):
-    def get_default_source(self, source):
-        return TestFileSource(source)
+def multi_highway_conv2d(x, kernel_size, n_layer,
+                         act_fn=relu,
+                         strides=1,
+                         padding='SAME',
+                         carry_bias=-1.0,
+                         name=None):
+    '''多层 highway_conv2d'''
+    if isinstance(kernel_size, int):
+        kernel_size = [kernel_size] * n_layer
     
-            perm_set = TestPermissionSet()
-        self.assertTrue(perm_set.is_valid())
-        perm_set['x'] = True
-        self.assertTrue(perm_set.is_valid())
-        perm_set[perm_set.ALL] = True
-        self.assertTrue(perm_set.is_valid())
-        perm_set['z'] = True
-        self.assertFalse(perm_set.is_valid())
-    
-        def test_no_resize(self):
-        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
-                      height=800)
-        url = self.provider.resize_image(image)
-        self.assertEqual(url, 'http://s3.amazonaws.com/a.jpg')
-    
-        def test_narwhal_detector(self):
-        user_agent = 'narwhal-iOS/2306 by det0ur'
-        outs = detect(user_agent)
-        self.assertEqual(outs['browser']['name'],
-                         NarwhalForRedditDetector.name)
-    
-        def assert_invalid(self, body, header, error, **expected):
-        expected.setdefault('global_version', -1)
-        expected.setdefault('version', -1)
-        expected.setdefault('platform', None)
-        expected.setdefault('has_mac', False)
-        expected['success'] = False
-        expected['error'] = error
-        return self._assert_validity(body, header, **expected)
+    # 可以通过相同的方式获取每个单词以及任一个 n-gram 的向量
+print(model.wv['hello'])
+print(model.wv['<h'])
+'''
+[-0.03481839  0.00606661  0.02581969  0.00188777  0.0325358 ]
+[ 0.04481247 -0.1784363  -0.03192253  0.07162753  0.16744071]
+'''
+print()
