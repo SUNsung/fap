3c2693cf06c0ab7fb86f0c694261485163b69dbe
SUNsung <weatherius@gmail.com>
2019-04-05 08:23:25 +0300

    Rewrite [58Q4eWZC1eotw58q9gU5RV8cp.58cEBcQ0Jvgw58col.cpp] File in: 2019-04-05 08:23:24

--- cccccc/58Q4eWZC1eotw58q9gU5RV8cp.58cEBcQ0Jvgw58col.cpp
+++ cccccc/58Q4eWZC1eotw58q9gU5RV8cp.58cEBcQ0Jvgw58col.cpp
@@ -1,237 +1,272 @@
 
         
         
-    {        return true;
-      }
+    {  std::move(self->tasks_[timer]).Run();
+  self->tasks_.erase(timer);
+  uv_timer_stop(timer);
+  uv_close(reinterpret_cast<uv_handle_t*>(timer), UvTaskRunner::OnClose);
+}
     
-    #include 'atom/browser/native_window_views.h'
+    namespace api {
+    }
     
-      v8::Local<v8::Value> URLRequest(v8::Isolate* isolate);
     
-     private:
-  atom::RenderProcessPreferences preferences_;
-    
-    #include 'atom/browser/api/event_emitter.h'
-#include 'native_mate/handle.h'
-#include 'ui/display/display_observer.h'
-#include 'ui/display/screen.h'
-    
-    #include 'base/macros.h'
-#include 'build/build_config.h'
-#include 'native_mate/arguments.h'
+    {}  // namespace api
     
     
     {}  // namespace atom
+    
+    void AutoUpdater::CheckForUpdates() {}
+    
+    #ifndef ATOM_BROWSER_LIB_POWER_OBSERVER_H_
+#define ATOM_BROWSER_LIB_POWER_OBSERVER_H_
+    
+    #endif  // ATOM_BROWSER_NET_ABOUT_PROTOCOL_HANDLER_H_
 
     
-    void URLRequestAboutJob::StartAsync() {
-  NotifyHeadersComplete();
-}
+    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
+                                       net::NetworkDelegate* network_delegate)
+    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
     
-    void OffscreenViewProxy::OnEvent(ui::Event* event) {
-  if (view_) {
-    view_->OnEvent(event);
+      if (transparent_) {
+    bitmap_->eraseColor(SK_ColorTRANSPARENT);
+  } else {
+    bitmap_->eraseColor(SK_ColorWHITE);
   }
-}
     
-      PROCESS_BASIC_INFORMATION pbi;
-  LONG status =
-      NtQueryInformationProcess(handle, ProcessBasicInformation, &pbi,
-                                sizeof(PROCESS_BASIC_INFORMATION), NULL);
-  if (!NT_SUCCESS(status)) {
-    LOG(ERROR) << 'NtQueryInformationProcess failed';
+    // In the relauncher process, performs the necessary synchronization steps
+// with the parent by setting up a kqueue to watch for it to exit, writing a
+// byte to the pipe, and then waiting for the exit notification on the kqueue.
+// If anything fails, this logs a message and returns immediately. In those
+// situations, it can be assumed that something went wrong with the parent
+// process and the best recovery approach is to attempt relaunch anyway.
+void RelauncherSynchronizeWithParent();
+    
+    #ifndef ATOM_BROWSER_UI_COCOA_VIEWS_DELEGATE_MAC_H_
+#define ATOM_BROWSER_UI_COCOA_VIEWS_DELEGATE_MAC_H_
+    
+    #ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+    
+    // static
+void App::EmitReopenEvent() {
+  std::set<RenderProcessHost*> rphs;
+  std::set<RenderProcessHost*>::iterator it;
+    }
+    
+      int id() const { return id_; }
+  std::string extension_id_;
+  ObjectManager* object_manager() const { return object_manager_.get(); }
+    
+    class Clipboard : public Base {
+ public:
+  Clipboard(int id,
+            const base::WeakPtr<DispatcherHost>& dispatcher_host,
+            const base::DictionaryValue& option);
+  ~Clipboard() override;
+    }
+    
+    #include 'content/nw/src/api/menu/menu_delegate.h'
+    
+      GdkRectangle screen_rect;
+  gdk_screen_get_monitor_geometry(screen, monitor, &screen_rect);
+    
+    aura::Window* Menu::GetActiveNativeView(content::RenderFrameHost* rfh) {
+  content::WebContents* web_contents =
+    content::WebContents::FromRenderFrameHost(rfh);
+  if (!web_contents) {
+    LOG(ERROR) << 'Menu: couldn't find WebContents';
     return NULL;
   }
+  return web_contents->GetFullscreenRenderWidgetHostView()
+             ? web_contents->GetFullscreenRenderWidgetHostView()
+                   ->GetNativeView()
+             : web_contents->GetNativeView();
+}
     
-      Begin = new char[capacity];
-  EndOfAllocation = Begin + capacity;
-  End = Begin + oldSize + needed;
-  std::memcpy(Begin, oldBegin, oldSize);
+    namespace extensions {
+class AppWindowRegistry;
+class ExtensionService;
+    }
     
-      static CFPointeeInfo forRecord(bool isConst, const clang::RecordDecl *decl) {
-    assert(decl);
-    CFPointeeInfo info;
-    info.IsValid = true;
-    info.IsConst = isConst;
-    info.Decl = decl;
-    return info;
-  }
     
-    enum class IAMAccessorKind : uint8_t { None, Getter, Setter };
+    {} // namespace extensions
+#endif
+
     
-    void BackendJobAction::anchor() {}
+    bool NwObjCallObjectMethodSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  base::ListValue* arguments = nullptr;
+  int id = 0;
+  std::string type, method;
+  EXTENSION_FUNCTION_VALIDATE(args_->GetInteger(0, &id));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(1, &type));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetString(2, &method));
+  EXTENSION_FUNCTION_VALIDATE(args_->GetList(3, &arguments));
+    }
     
-    int32_t SearchIterator::next(UErrorCode &status)
+    template <typename T>
+struct AbsDiff
 {
-    if (U_SUCCESS(status)) {
-        int32_t offset = getOffset();
-        int32_t matchindex  = m_search_->matchedIndex;
-        int32_t     matchlength = m_search_->matchedLength;
-        m_search_->reset = FALSE;
-        if (m_search_->isForwardSearching == TRUE) {
-            int32_t textlength = m_search_->textLength;
-            if (offset == textlength || matchindex == textlength || 
-                (matchindex != USEARCH_DONE && 
-                matchindex + matchlength >= textlength)) {
-                // not enough characters to match
-                setMatchNotFound();
-                return USEARCH_DONE; 
-            }
-        }
-        else {
-            // switching direction. 
-            // if matchedIndex == USEARCH_DONE, it means that either a 
-            // setOffset has been called or that previous ran off the text
-            // string. the iterator would have been set to offset 0 if a 
-            // match is not found.
-            m_search_->isForwardSearching = TRUE;
-            if (m_search_->matchedIndex != USEARCH_DONE) {
-                // there's no need to set the collation element iterator
-                // the next call to next will set the offset.
-                return matchindex;
-            }
-        }
-    }
+    typedef T type;
     }
     
-    #ifndef __SHARED_CALENDAR_H__
-#define __SHARED_CALENDAR_H__
     
+#include 'common.hpp'
+#include 'vtransform.hpp'
     
-    {        // Use subFormat() to format a repeated pattern character
-        // when a different pattern or non-pattern character is seen
-        if (ch != prevCh && count > 0) {
-            subFormat(appendTo, prevCh, count, capitalizationContext, fieldNum++, handler, *workCal, mutableNFs, status);
-            count = 0;
-        }
-        if (ch == QUOTE) {
-            // Consecutive single quotes are a single quote literal,
-            // either outside of quotes or between quotes
-            if ((i+1) < fPattern.length() && fPattern[i+1] == QUOTE) {
-                appendTo += (UChar)QUOTE;
-                ++i;
-            } else {
-                inQuote = ! inQuote;
-            }
-        }
-        else if (!inQuote && isSyntaxChar(ch)) {
-            // ch is a date-time pattern character to be interpreted
-            // by subFormat(); count the number of times it is repeated
-            prevCh = ch;
-            ++count;
-        }
-        else {
-            // Append quoted characters and unquoted non-pattern characters
-            appendTo += ch;
-        }
-    }
-    
-    #ifndef SMPDTFST_H
-#define SMPDTFST_H
-    
-        /**
-     * @param keyword for example 'few' or 'other'
-     * @return the index of the plural form corresponding to the keyword, or a negative value
-     */
-    static int32_t indexOrNegativeFromString(const char *keyword);
-    
-    // NOTE: the original code used the same class for the `reverse()` method
-EASERATE_TEMPLATE_IMPL(EaseIn, tweenfunc::easeIn);
-EASERATE_TEMPLATE_IMPL(EaseOut, tweenfunc::easeOut);
-EASERATE_TEMPLATE_IMPL(EaseInOut, tweenfunc::easeInOut);
-    
-    bool CallFuncN::initWithTarget(Ref* selectorTarget, SEL_CallFuncN selector)
+    void add(const Size2D &size,
+         const s16 * src0Base, ptrdiff_t src0Stride,
+         const s16 * src1Base, ptrdiff_t src1Stride,
+         s16 *dstBase, ptrdiff_t dstStride,
+         CONVERT_POLICY policy)
 {
-    if (CallFunc::initWithTarget(selectorTarget))
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+        if (policy == CONVERT_POLICY_SATURATE)
     {
-        _callFuncN = selector;
-        return true;
+        internal::vtransform(size,
+                             src0Base, src0Stride,
+                             src1Base, src1Stride,
+                             dstBase, dstStride,
+                             AddSaturate<s16, s32>());
     }
+    else
+    {
+        internal::vtransform(size,
+                             src0Base, src0Stride,
+                             src1Base, src1Stride,
+                             dstBase, dstStride,
+                             AddWrap<s16, s32>());
+    }
+#else
+    (void)size;
+    (void)src0Base;
+    (void)src0Stride;
+    (void)src1Base;
+    (void)src1Stride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)policy;
+#endif
+}
+    
+        void operator() (const typename VecTraits<s32>::vec64 & v_src0,
+                     const typename VecTraits<s32>::vec64 & v_src1,
+                     typename VecTraits<s32>::vec64 & v_dst) const
+    {
+        float32x2_t vs1 = vcvt_f32_s32(v_src0);
+        float32x2_t vs2 = vcvt_f32_s32(v_src1);
     }
     
-    /** @class FlipY
-* @brief Flips the sprite vertically.
-* @since v0.99.0
-*/
-class CC_DLL FlipY : public ActionInstant
+                float32x4_t lane0b = vld1q_f32(laneB + x - cn);
+            float32x4_t lane2b = vld1q_f32(laneB + x + cn);
+            float32x4_t lane1b = vld1q_f32(laneB + x);
+    
+    void extract3(const Size2D &size,
+              const u8 * srcBase, ptrdiff_t srcStride,
+              u8 * dstBase, ptrdiff_t dstStride,
+              u32 coi)
 {
-public:
-    /** Create the action.
-     *
-     * @param y Flips the sprite vertically if true.
-     * @return An autoreleased FlipY object.
-     */
-    static FlipY * create(bool y);
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+#ifndef __ANDROID__
+    size_t roiw32 = size.width >= 31 ? size.width - 31 : 0;
+#endif
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
     }
     
+    bool isSupportedConfiguration()
+{
+#ifdef CAROTENE_NEON
+    return true;
+#else
+    return false;
+#endif
+}
     
-    /** Returns the numbers of actions that are running in a
-     *  certain target with a specific tag.
-     * Like getNumberOfRunningActionsInTarget Composable actions
-     * are counted as 1 action. Example:
-     * - If you are running 1 Sequence of 7 actions, it will return 1.
-     * - If you are running 7 Sequences of 2 actions, it will return 7.
-     *
-     * @param target    A certain target.
-     * @param tag       Tag that will be searched.
-     * @return  The numbers of actions that are running in a certain target
-     *          with a specific tag.
-     * @see getNumberOfRunningActionsInTarget
-     * @js NA
-     */
-    virtual size_t getNumberOfRunningActionsInTargetByTag(const Node *target, int tag);
-    
-        /**
-    @brief Create an action with duration, grid size.
-    @param duration Specify the duration of the PageTurn3D action. It's a value in seconds.
-    @param gridSize Specify the size of the grid.
-    @return If the creation success, return a pointer of PageTurn3D action; otherwise, return nil.
-    */
-    static PageTurn3D* create(float duration, const Size& gridSize);
-    
-    using apollo::common::ErrorCode;
-    
-    using apollo::common::ErrorCode;
-    
-    double ClusterGeneralInfo701::lateral_vel(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 5);
-  uint32_t x = t0.get_byte(0, 6);
+            while(i + 16 <= size.width)
+        {
+            size_t lim = std::min(i + DOT_UINT_BLOCKSIZE, size.width) - 16;
     }
     
-    int ObjectExtendedInfo60D::obstacle_class(const std::uint8_t* bytes,
-                                          int32_t length) const {
-  Byte t0(bytes + 3);
-  int32_t x = t0.get_byte(0, 3);
+    inline uint8x8_t vqtbl1_u8 (uint8x16_t a, uint8x8_t b)
+{
+#ifdef __aarch64__
+    // AArch64 supports this natively
+    return ::vqtbl1_u8(a, b);
+#else
+    union { uint8x16_t v; uint8x8x2_t w; } u = { a };
+    return vtbl2_u8(u.w, b);
+#endif
+}
+    
+    
+    {    Rational Sinh(Rational const& rat);
+    Rational Cosh(Rational const& rat);
+    Rational Tanh(Rational const& rat);
+    Rational ASinh(Rational const& rat);
+    Rational ACosh(Rational const& rat);
+    Rational ATanh(Rational const& rat);
+}
+
+    
+    void LiveRegionHost::Announce(NarratorAnnouncement^ announcement)
+{
+    if (m_host == nullptr)
+    {
+        m_host = ref new TextBlock();
+        AutomationProperties::SetLiveSetting(m_host, AutomationLiveSetting::Assertive);
+    }
     }
     
+                static void OnVirtualKeyControlShiftChordPropertyChanged(
+                Windows::UI::Xaml::DependencyObject^ target,
+                MyVirtualKey oldValue,
+                MyVirtualKey newValue);
     
-    {  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
-    if (conti_radar->contiobs(i).obstacle_id() == obj_id) {
-      auto obs = conti_radar->mutable_contiobs(i);
-      obs->set_longitude_dist_rms(
-          LINEAR_RMS[longitude_dist_rms(bytes, length)]);
-      obs->set_lateral_dist_rms(LINEAR_RMS[lateral_dist_rms(bytes, length)]);
-      obs->set_longitude_vel_rms(LINEAR_RMS[longitude_vel_rms(bytes, length)]);
-      obs->set_lateral_vel_rms(LINEAR_RMS[lateral_vel_rms(bytes, length)]);
-      obs->set_longitude_accel_rms(
-          LINEAR_RMS[longitude_accel_rms(bytes, length)]);
-      obs->set_lateral_accel_rms(LINEAR_RMS[lateral_accel_rms(bytes, length)]);
-      obs->set_oritation_angle_rms(
-          ANGLE_RMS[oritation_angle_rms(bytes, length)]);
-      obs->set_probexist(PROBOFEXIST[probexist(bytes, length)]);
-      obs->set_meas_state(meas_state(bytes, length));
-      break;
+    
+    {        Windows::Globalization::Fonts::LanguageFontGroup^ m_fontGroup;
+        Platform::String^ m_language;
+        Windows::UI::Xaml::FlowDirection m_flowDirection;
+        bool m_overrideFontApiValues;
+        Platform::String^ m_fontFamilyOverride;
+        Windows::UI::Text::FontWeight m_fontWeightOverride;
+        double m_uiTextFontScaleFactorOverride;
+        double m_uiCaptionFontScaleFactorOverride;
+    };
+    
+      uint64_t sleep_debt = 0;
+  uint64_t time_since_last_refill = 0;
+  if (last_refill_time_ != 0) {
+    if (last_refill_time_ > time_now) {
+      sleep_debt = last_refill_time_ - time_now;
+    } else {
+      time_since_last_refill = time_now - last_refill_time_;
+      bytes_left_ +=
+          static_cast<uint64_t>(static_cast<double>(time_since_last_refill) /
+                                kMicrosPerSecond * delayed_write_rate_);
+      if (time_since_last_refill >= kRefillInterval &&
+          bytes_left_ > num_bytes) {
+        // If refill interval already passed and we have enough bytes
+        // return without extra sleeping.
+        last_refill_time_ = time_now;
+        bytes_left_ -= num_bytes;
+        return 0;
+      }
     }
   }
-}
+    
+    #include <string>
+    
+      MyFilter filter;
+    
+      delete db;
+    
+      // Set a new snapshot in the transaction
+  txn->SetSnapshot();
+  txn->SetSavePoint();
+  read_options.snapshot = txn_db->GetSnapshot();
     
     
-    {  acc.Parse(bytes, length, &chassis_detail);
-  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().manual_input(), 0.258);
-  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().commanded_value(),
-                   0.772);
-  EXPECT_DOUBLE_EQ(chassis_detail.gem().accel_rpt_68().output_value(), 4.37);
-}
-    
-    using ::apollo::drivers::canbus::Byte;
+    {}  // namespace rocksdb
