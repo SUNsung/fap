8ffad24f649a00fd8ac30e06678e16dab105d928
SUNsung <weatherius@gmail.com>
2019-04-05 04:22:58 +0300

    Rewrite [11H7zUYJoqu7U11sXfU5DZoNGY11a3by9xxVWhw11IN.cpp] File in: 2019-04-05 04:22:58

--- cccccc/11H7zUYJoqu7U11sXfU5DZoNGY11a3by9xxVWhw11IN.cpp
+++ cccccc/11H7zUYJoqu7U11sXfU5DZoNGY11a3by9xxVWhw11IN.cpp
@@ -1,208 +1,107 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+        data:
+ [8. 2. 1. 1. 7. 8. 1.]
+indices:
+ [3 5 6]
     
-    ModelAnalyzer::ModelAnalyzer(const GrapplerItem& item) : item_(item) {}
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    #include 'tensorflow/c/c_api.h'
-#include 'tensorflow/core/lib/core/errors.h'
-#include 'tensorflow/core/platform/mutex.h'
-#include 'tensorflow/python/lib/core/bfloat16.h'
-#include 'tensorflow/python/lib/core/ndarray_tensor_bridge.h'
-    
-    
-    {
-    {}  // namespace swig
-}  // namespace tensorflow
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    // Request Shell's id for current render_view_host.
-IPC_SYNC_MESSAGE_ROUTED0_1(ShellViewHostMsg_GetShellId,
-                           int /* result */)
-    
-    #ifndef CONTENT_NW_SRC_API_APP_APP_H_
-#define CONTENT_NW_SRC_API_APP_APP_H_
-    
-    #endif  // CONTENT_NW_SRC_API_BASE_BASE_H_
+    #endif // CAFFE2_OPERATORS_JSD_OP_H_
 
     
-    
-    {
-    {    if (zoom_controller) {
-      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
-      if (zoom_factor > content::kMaximumZoomFactor) {
-        zoom_factor = content::kMaximumZoomFactor;
-      }
-      if (zoom_factor < content::kMinimumZoomFactor) {
-        zoom_factor = content::kMinimumZoomFactor;
-      }
-      x *= zoom_factor;
-      y *= zoom_factor;
-    }
-    
-    Popup(x, y, rvh);
-  } else if (method == 'EnableShowEvent') {
-    arguments.GetBoolean(0, &enable_show_event_);
-  } else {
-    NOTREACHED() << 'Invalid call to Menu method:' << method
-                 << ' arguments:' << arguments;
-  }
-}
-    
-    ui::KeyboardCode GetKeycodeFromText(std::string text){
-  ui::KeyboardCode retval = ui::VKEY_UNKNOWN;
-  if (text.size() != 0){
-    std::string upperText = base::ToUpperASCII(text);
-    std::string keyName = text;
-    bool found = false;
-    if (upperText.size() == 1){
-      char key = upperText[0];
-      if (key>='0' && key<='9'){//handle digital
-        keyName = 'Digit' + upperText;
-        found = true;
-      } else if (key>='A'&&key<='Z'){//handle alphabet
-        keyName = 'Key' + upperText;
-        found = true;
-      }
-    }
-    }
-    }
-    
-    // Then, use the TEST_P macro to define as many parameterized tests
-// for this fixture as you want. The _P suffix is for 'parameterized'
-// or 'pattern', whichever you prefer to think.
-    
-      // Signals that the death test did not die as expected.
-  virtual void Abort(AbortReason reason) = 0;
-    
-    
-    {  template <class Tuple>
-  static GTEST_BY_REF_(GTEST_TUPLE_ELEMENT_(8, Tuple))
-  ConstField(const Tuple& t) { return t.f8_; }
-};
-    
-    #endif  // GTEST_SAMPLES_PRIME_TABLES_H_
+    #endif  // TESSERACT_CCSTRUCT_DEBUGPIXA_H_
 
     
-      void setTaskQueue(DHTTaskQueue* taskQueue);
+      // Returns the direction of the fitted line as a unit vector, using the
+  // least mean squared perpendicular distance. The line runs through the
+  // mean_point, i.e. a point p on the line is given by:
+  // p = mean_point() + lambda * vector_fit() for some real number lambda.
+  // Note that the result (0<=x<=1, -1<=y<=-1) is directionally ambiguous
+  // and may be negated without changing its meaning, since a line is only
+  // unique to a range of pi radians.
+  // Modernists prefer to think of this as an Eigenvalue problem, but
+  // Pearson had the simple solution in 1901.
+  //
+  // Note that this is equivalent to returning the Principal Component in PCA,
+  // or the eigenvector corresponding to the largest eigenvalue in the
+  // covariance matrix.
+  FCOORD vector_fit() const;
     
-    class DHTNode;
-class DHTRoutingTable;
-class DHTMessageDispatcher;
-class DHTMessageFactory;
-class DHTTaskQueue;
-class DHTAbstractTask;
-    
-    class DHTTask;
-    
-    void DHTTokenUpdateCommand::process()
-{
-  try {
-    tokenTracker_->updateTokenSecret();
-  }
-  catch (RecoverableException& e) {
-    A2_LOG_ERROR_EX(EX_EXCEPTION_CAUGHT, e);
-  }
-}
-    
-    void DNSCache::remove(const std::string& hostname, uint16_t port)
-{
-  auto target = std::make_shared<CacheEntry>(hostname, port);
-  entries_.erase(target);
-}
-    
-    
-    {
-    {
-    {
-    {}  // namespace can
-}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
-    
-    TEST(ByteTest, GetValue) {
-  unsigned char byte_value = 0x1A;
-  Byte value(&byte_value);
-  EXPECT_EQ(0x05, value.get_byte(1, 3));
-  EXPECT_EQ(0x01, value.get_byte(1, 1));
-  EXPECT_EQ(0x00, value.get_byte(8, 1));
-  EXPECT_EQ(0x00, value.get_byte(-1, 1));
-  EXPECT_EQ(0x1A, value.get_byte(0, 10));
-}
-    
-    DEFINE_string(adapter_config_filename, 'modules/canbus/conf/adapter.conf',
-              'The adapter config file');
-    
-    void ClusterQualityInfo702::Parse(const std::uint8_t* bytes, int32_t length,
-                                  ContiRadar* conti_radar) const {
-  int id = target_id(bytes, length);
-  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
-    if (conti_radar->contiobs(i).obstacle_id() == id) {
-      auto conti_obs = conti_radar->mutable_contiobs(i);
-      conti_obs->set_longitude_dist_rms(
-          LINEAR_RMS[longitude_dist_rms(bytes, length)]);
-      conti_obs->set_lateral_dist_rms(
-          LINEAR_RMS[lateral_dist_rms(bytes, length)]);
-      conti_obs->set_longitude_vel_rms(
-          LINEAR_RMS[longitude_vel_rms(bytes, length)]);
-      conti_obs->set_lateral_vel_rms(
-          LINEAR_RMS[lateral_vel_rms(bytes, length)]);
-      conti_obs->set_probexist(PROBOFEXIST[pdh0(bytes, length)]);
-      switch (invalid_state(bytes, length)) {
-        case 0x01:
-        case 0x02:
-        case 0x03:
-        case 0x06:
-        case 0x07:
-        case 0x0E:
-          conti_obs->set_probexist(PROBOFEXIST[0]);
-        default:
-          break;
-      }
-      switch (ambig_state(bytes, length)) {
-        case 0x00:
-        case 0x01:
-        case 0x02:
-          conti_obs->set_probexist(PROBOFEXIST[0]);
-        default:
-          break;
-      }
-    }
-  }
-}
-    
-    
-    {  switch (x) {
-    case 0x0:
-      return OUTPUT_TYPE_NONE;
-    case 0x1:
-      return OUTPUT_TYPE_OBJECTS;
-    case 0x2:
-      return OUTPUT_TYPE_CLUSTERS;
+    bool ParagraphModel::ValidBodyLine(int lmargin, int lindent,
+                                   int rindent, int rmargin) const {
+  switch (justification_) {
+    case JUSTIFICATION_LEFT:
+      return NearlyEqual(lmargin + lindent, margin_ + body_indent_,
+                         tolerance_);
+    case JUSTIFICATION_RIGHT:
+      return NearlyEqual(rmargin + rindent, margin_ + body_indent_,
+                         tolerance_);
+    case JUSTIFICATION_CENTER:
+      return NearlyEqual(lindent, rindent, tolerance_ * 2);
     default:
-      return OUTPUT_TYPE_ERROR;
+      // shouldn't happen
+      return false;
   }
 }
     
-    // config detail: {'name': 'output_value', 'offset': 0.0, 'precision': 0.001,
-// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 39,
-// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
-double Accelrpt68::output_value(const std::uint8_t* bytes,
-                                int32_t length) const {
-  Byte t0(bytes + 4);
-  int32_t x = t0.get_byte(0, 8);
-    }
+    void Assembler::rlwinm(const Reg64& ra, const Reg64& rs, uint8_t sh, uint8_t mb,
+                       uint16_t me, bool rc) {
+  EmitMForm(21, rn(rs), rn(ra), rn(sh), mb, me, rc);
+}
     
-      Byte t3(bytes + 7);
-  t = t3.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
+    public:
+  static void Add(InfoVec& out, const char* name, const std::string& value);
+  static void AddServerStats(InfoVec& out, const char* name,
+                             const char* statsName = nullptr);
+    
+      path_str += strlen(prefix);
+  path_len -= strlen(prefix);
+    
+    
+    {};
+    
+    using namespace ::apache::thrift;
+using namespace ::apache::thrift::protocol;
+using namespace ::apache::thrift::transport;
+using namespace ::apache::thrift::server;
+    
+    
+  while (true)
+  {
+    xfer += iprot->readFieldBegin(fname, ftype, fid);
+    if (ftype == ::apache::thrift::protocol::T_STOP) {
+      break;
+    }
+    switch (fid)
+    {
+      case 1:
+        if (ftype == ::apache::thrift::protocol::T_I32) {
+          xfer += iprot->readI32(this->code);
+          this->__isset.code = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      case 2:
+        if (ftype == ::apache::thrift::protocol::T_STRING) {
+          xfer += iprot->readString(this->message);
+          this->__isset.message = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      case 3:
+        if (ftype == ::apache::thrift::protocol::T_I64) {
+          xfer += iprot->readI64(this->uuid);
+          this->__isset.uuid = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      default:
+        xfer += iprot->skip(ftype);
+        break;
+    }
+    xfer += iprot->readFieldEnd();
+  }
+    
+      void __set_code(const int32_t val);
