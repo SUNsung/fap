a313c8269bccba50798d039f4ccefe4a15041ade
SUNsung <weatherius@gmail.com>
2019-04-06 01:58:56 +0300

    Rewrite [44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp] File in: 2019-04-06 01:58:55

--- cccccc/44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp
+++ cccccc/44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp
@@ -1,105 +1,202 @@
 
         
-          /** The vector that indicates whether each top blob has a non-zero weight in
-   *  the objective function. */
-  vector<Dtype> loss_;
+        namespace atom {
+    }
     
-      // If there are two top blobs, then the second blob will contain
-  // accuracies per class.
-  virtual inline int MinTopBlobs() const { return 1; }
-  virtual inline int MaxTopBlobs() const { return 2; }
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
+    
+    namespace atom {
+    }
+    
+    #include 'atom/browser/api/event_emitter.h'
+#include 'native_mate/handle.h'
+#include 'ui/display/display_observer.h'
+#include 'ui/display/screen.h'
+    
+    Delegate* AutoUpdater::GetDelegate() {
+  return delegate_;
+}
+    
+    #ifndef ATOM_BROWSER_LIB_POWER_OBSERVER_H_
+#define ATOM_BROWSER_LIB_POWER_OBSERVER_H_
+    
+    
+    {  DISALLOW_COPY_AND_ASSIGN(URLRequestAboutJob);
+};
+    
+    SHOULD_NOT_DO_GRADIENT(EnforceFinite);
+    
+    workspace.ResetWorkspace()
+    
+    )DOC')
+    .Input(0, 'X', '*(type: Tensor`<float>`)* Input tensor.')
+    .Output(0, 'Y', '*(type: Tensor`<float>`)* Output tensor.');
+    
+    template <typename T, class Context>
+class BernoulliJSDOp final : public Operator<Context> {
+ public:
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
+};
+    
+    #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Utils.h'
+#include 'ReaderShim.h'
+#include 'DataReader.h'
+    
+    
+    {
+    {    private:
+        static bool IsNativeUDF(const Dictionary& dict);
+    };
+}
+
+    
+        std::wstring VariableFields::AsString() const
+    {
+        std::wstringstream wss;
+        wss << VariableKindName(m_varKind) << '('';
+        if (m_name != L'')
+            wss << m_name;
+        else
+            wss << m_uid;
+        bool reverse = Internal::IsReversingTensorShapesInErrorMessagesEnabled();
+        if (reverse)
+            wss << '', ' << DynamicAxesAsString(m_dynamicAxes, reverse) << ', ' << m_shape.AsString() << ')';
+        else
+            wss << '', ' << m_shape.AsString() << ', ' << DynamicAxesAsString(m_dynamicAxes, reverse) << ')';
+        return wss.str();
+    }
+    
+    
+    {            if ((m_varKind == VariableKind::Parameter) || (m_varKind == VariableKind::Constant))
+            {
+                if (m_shape.HasFreeDimension())
+                    InvalidArgument('Parameter/Constant '%S' has invalid shape '%S'; it is illegal for a Parameter/Constant to have a FreeDimension.', AsString().c_str(), m_shape.AsString().c_str());
+            }
+        }
+    
+    template <typename TVector>
+inline void RandomShuffleMT(TVector& v, std::mt19937_64& rng)
+{
+    foreach_index(currentLocation, v)
+    {
+        // Pick a random location and swap with current one
+        const size_t randomLocation = RandMT(0, v.size(), rng);
+        std::swap(v[currentLocation], v[randomLocation]);
+    }
+}
+    
+    
+    {    ~ScopeTimer()
+    {
+        if (m_verbosity > 2)
+        {
+            m_aggregateTimer.Stop();
+            double time = m_aggregateTimer.ElapsedSeconds();
+            fprintf(stderr, m_message.c_str(), time);
+        }
+    }
+};
+    
+            // traverse children: append them to the end of the work list
+        // In case of model editing, map inputs.
+        for (size_t i = 0; i < node->GetNumInputs(); i++)
+        {
+            auto input = node->Input(i);
+    }
+    
+    
+    {    virtual void Load(File& fstream, size_t modelVersion) override
+    {
+        Base::Load(fstream, modelVersion);
+        fstream >> m_isOutputSparse;
+    }
+private:
+    bool m_isOutputSparse;
+};
     
     /**
- * @brief Compute the index of the @f$ K @f$ max values for each datum across
- *        all dimensions @f$ (C \times H \times W) @f$.
- *
- * Intended for use after a classification layer to produce a prediction.
- * If parameter out_max_val is set to true, output is a vector of pairs
- * (max_ind, max_val) for each image. The axis parameter specifies an axis
- * along which to maximise.
- *
- * NOTE: does not implement Backwards operation.
- */
-template <typename Dtype>
-class ArgMaxLayer : public Layer<Dtype> {
- public:
-  /**
-   * @param param provides ArgMaxParameter argmax_param,
-   *     with ArgMaxLayer options:
-   *   - top_k (\b optional uint, default 1).
-   *     the number @f$ K @f$ of maximal items to output.
-   *   - out_max_val (\b optional bool, default false).
-   *     if set, output a vector of pairs (max_ind, max_val) unless axis is set then
-   *     output max_val along the specified axis.
-   *   - axis (\b optional int).
-   *     if set, maximise along the specified axis else maximise the flattened
-   *     trailing dimensions for each index of the first / num dimension.
-   */
-  explicit ArgMaxLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+@brief Waves action.
+@details This action is used for take effect on the target node as waves.
+        You can control the effect by these parameters:
+        duration, grid size, waves count, amplitude,
+        whether waves on horizontal and whether waves on vertical.
+*/
+class CC_DLL Waves : public Grid3DAction
+{
+public:
+    /**
+    @brief Create the action with amplitude, horizontal sin, vertical sin, grid size, waves count and duration.
+    @param duration Specify the duration of the Waves action. It's a value in seconds.
+    @param gridSize Specify the size of the grid.
+    @param waves Specify the waves count of the Waves action.
+    @param amplitude Specify the amplitude of the Waves action.
+    @param horizontal Specify whether waves on horizontal.
+    @param vertical Specify whether waves on vertical.
+    @return If the creation success, return a pointer of Waves action; otherwise, return nil.
+    */
+    static Waves* create(float duration, const Size& gridSize, unsigned int waves, float amplitude, bool horizontal, bool vertical);
     }
     
-     protected:
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    {}  // namespace caffe
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    // Gets the content of the stringstream's buffer as an std::string.  Each '\0'
-// character in the buffer is replaced with '\\0'.
-GTEST_API_ std::string StringStreamToString(::std::stringstream* stream);
-    
-      template <GTEST_10_TYPENAMES_(U)>
-  tuple(const GTEST_10_TUPLE_(U)& t) : f0_(t.f0_), f1_(t.f1_), f2_(t.f2_),
-      f3_(t.f3_), f4_(t.f4_), f5_(t.f5_), f6_(t.f6_), f7_(t.f7_), f8_(t.f8_),
-      f9_(t.f9_) {}
-    
-     private:
-  // Creates a node with a given element value.  The next pointer is
-  // set to NULL.
-  explicit QueueNode(const E& an_element) : element_(an_element), next_(NULL) {}
-    
-    int64_t VMTOC::allocTOC(int64_t target) {
-  folly::MSLGuard g{s_TOC};
-  HPHP::Address addr = m_tocvector->frontier();
-  m_tocvector->qword(target);
-  return addr - (m_tocvector->base() + INT16_MAX + 1);
-}
-    
-    namespace HPHP {
-///////////////////////////////////////////////////////////////////////////////n
+    /** @class FlipY
+* @brief Flips the sprite vertically.
+* @since v0.99.0
+*/
+class CC_DLL FlipY : public ActionInstant
+{
+public:
+    /** Create the action.
+     *
+     * @param y Flips the sprite vertically if true.
+     * @return An autoreleased FlipY object.
+     */
+    static FlipY * create(bool y);
     }
     
-    Variant PlainDirectory::read() {
-  struct dirent entry;
-  struct dirent *result;
-  int ret = readdir_r(m_dir, &entry, &result);
-  if (ret != 0 || !result) {
-    return false;
-  }
-  return String(entry.d_name, CopyString);
+    
+    {
+    {
+    {            if (action->getTag() == (int)tag && action->getOriginalTarget() == target)
+            {
+                removeActionAtIndex(i, element);
+                --limit;
+            }
+            else
+            {
+                ++i;
+            }
+        }
+    }
 }
     
-    template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, F fillCols) {
-  logPerfWarningImpl(event, 0, kDefaultPerfWarningRate, fillCols);
-}
-template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, int64_t rate, F fillCols) {
-  logPerfWarningImpl(event, 0, rate, fillCols);
-}
+        /**
+     * @brief Initializes the action with a duration, a 'from' percentage and a 'to' percentage.
+     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
+     * @param fromPercentage Specify the source percentage.
+     * @param toPercentage Specify the destination percentage.
+     * @return If the creation success, return true; otherwise, return false.
+     */
+    bool initWithDuration(float duration, float fromPercentage, float toPercentage);
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __CCACTIONTWEEN_H__
+#define __CCACTIONTWEEN_H__
+    
+        /** Adds a frame with a texture and a rect. Internally it will create a SpriteFrame and it will add it.
+     * The frame will be added with one 'delay unit'.
+     * Added to facilitate the migration from v0.8 to v0.9.
+     * @param pobTexture A frame with a texture.
+     * @param rect The Texture of rect.
+     */
+    void addSpriteFrameWithTexture(Texture2D* pobTexture, const Rect& rect);
