0a28676ca8e5e25b6ce96164d120c4a4e24fd538
SUNsung <weatherius@gmail.com>
2019-04-05 13:26:04 +0300

    Rewrite [22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb] File in: 2019-04-05 13:26:04

--- ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
+++ ruby/22SAvzSus8cGY22h94XY9Vy4UU22TJqz..NItbM227ZW.rb
@@ -1,138 +1,27 @@
 
         
-              it 'requires the passwords to match when changing them' do
-        visit edit_admin_user_path(users(:bob))
-        fill_in 'Password', with: '12345678'
-        fill_in 'Password confirmation', with: 'no_match'
-        click_on 'Update User'
-        expect(page).to have_text('Password confirmation doesn't match')
-      end
-    end
-    
-        class Agents::DotBar < Agent
-      cannot_be_scheduled!
-    
-        it 'cleans up old logs when there are more than log_length' do
-      stub(AgentLog).log_length { 4 }
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 1')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 2')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 3')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 4')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 4')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 1')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 5')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 5')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 2')
-      AgentLog.log_for_agent(agents(:jane_website_agent), 'message 6')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').first.message).to eq('message 6')
-      expect(agents(:jane_website_agent).logs.order('agent_logs.id desc').last.message).to eq('message 3')
-    end
-    
-      describe 'helpers' do
-    it 'should generate a correct request options hash' do
-      expect(@checker.send(:request_options)).to eq({headers: {'User-Agent' => 'Huginn - https://github.com/huginn/huginn', 'Authorization' => 'Bearer '1234token''}})
-    end
-    
-      describe '#receive' do
-    it 'sends a message' do
-      stub(HTTParty).post { {'id' => 1, 'message' => 'blah', 'title' => 'blah','source_name' => 'Custom Notification'} }
-      @checker.receive([@event])
-    end
-    
-      describe '#switch?' do
-    let(:argv) { ['-ns', '-i', '--bar', '-a-bad-arg'] }
-    
-        it 'returns false if the environment does not contain :std' do
-      expect(env).not_to be_std
-    end
-  end
-    
-      #
-  # Updates the command parts for this specific packet type.
-  #
-  def update_cmd_parts(str)
-    if (md = str.match(/^(.+?)\s+(.+?)\s+HTTP\/(.+?)\r?\n?$/i))
-      self.method  = md[1]
-      self.raw_uri = URI.decode(md[2])
-      self.proto   = md[3]
-    
-              # Encodes a Rex::Proto::Kerberos::Model::Checksum into an ASN.1 String
-          #
-          # @return [String]
-          def encode
-            elems = []
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_type], 0, :CONTEXT_SPECIFIC)
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_checksum], 1, :CONTEXT_SPECIFIC)
-    
-              # Rex::Proto::Kerberos::Model::KdcResponse encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'KdcResponse encoding not supported'
-          end
-    
-        if processes.stdout.lines.any? { |line| line =~ %r{^\d+\t\d\tcom.apple.SafariNotificationAgent$} }
-      system_command '/usr/bin/killall', args: ['-kill', 'SafariNotificationAgent']
-    end
-    
-            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
-    
-        def execute
-      raise PluginManager::FileNotFoundError, 'Can't file local file #{local_file}' unless ::File.exist?(local_file)
-      raise PluginManager::InvalidPackError, 'Invalid format, the pack must be in zip format' unless valid_format?(local_file)
-    
-        # any errors will be logged to $stderr by invoke!
-    # Bundler cannot update and clean gems in one operation so we have to call the CLI twice.
-    options = {:update => plugins, :rubygems_source => gemfile.gemset.sources}
-    options[:local] = true if local?
-    output = LogStash::Bundler.invoke!(options)
-    # We currently dont removed unused gems from the logstash installation
-    # see: https://github.com/elastic/logstash/issues/6339
-    # output = LogStash::Bundler.invoke!(:clean => true)
-    display_updated_plugins(previous_gem_specs_map)
-  rescue => exception
-    gemfile.restore!
-    report_exception('Updated Aborted', exception)
-  ensure
-    display_bundler_output(output)
-  end
-    
-            context 'when fetching a gem from rubygems' do
-    
-          it 'list the plugins with their versions' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose')
-        result.stdout.split('\n').each do |plugin|
-          expect(plugin).to match(/^logstash-\w+-\w+\s\(\d+\.\d+.\d+(.\w+)?\)/)
-        end
-      end
-    end
-    
-      argument :attachment_names, :required => true, :type => :array, :desc => 'The names of the attachment(s) to add.',
-           :banner => 'attachment_one attachment_two attachment_three ...'
-    
-        def self.clear
-      instance.clear
-    end
-    
-        def type_from_mime_magic
-      @type_from_mime_magic ||= File.open(@filepath) do |file|
-        MimeMagic.by_magic(file).try(:type)
-      end
-    end
-    
-        # Returns the id of the instance in a split path form. e.g. returns
-    # 000/001/234 for an id of 1234.
-    def id_partition attachment, style_name
-      case id = attachment.instance.id
-      when Integer
-        if id < ID_PARTITION_LIMIT
-          ('%09d'.freeze % id).scan(/\d{3}/).join('/'.freeze)
-        else
-          ('%012d'.freeze % id).scan(/\d{3}/).join('/'.freeze)
-        end
-      when String
-        id.scan(/.{3}/).first(3).join('/'.freeze)
+            def add(entry)
+      if entry.is_a? Array
+        entry.each(&method(:add))
       else
-        nil
+        add_entry(entry) unless entry.root?
       end
+    end
+    
+        attr_accessor :name, :type, :path
+    
+            def store_pages(store)
+          instrument 'info.doc', msg: 'Fetching redirections...'
+          with_redirections do
+            instrument 'info.doc', msg: 'Continuing...'
+            super
+          end
+        end
+    
+        def terminal_width
+      return @terminal_width if defined? @terminal_width
+    
+          updated_note = Notification.find(note.id)
+      expect(updated_note.unread).to eq(true)
+      expect(updated_note.updated_at.iso8601).to eq(note.updated_at.iso8601)
     end
