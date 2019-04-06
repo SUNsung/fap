baa82ab52b1d234d7554438cbd4ee5cc8a1c951c
SUNsung <weatherius@gmail.com>
2019-04-06 10:22:01 +0300

    Rewrite [486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp] File in: 2019-04-06 10:22:00

--- cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
+++ cccccc/486kjzaEHuL648KsXPH3MbGoI48A8ZwxJPpnM48Ss1.cpp
@@ -1,281 +1,153 @@
 
         
-        #include <map>
-    
-    namespace atom {
-    }
-    
-    class AsarProtocolHandler : public net::URLRequestJobFactory::ProtocolHandler {
- public:
-  explicit AsarProtocolHandler(
-      const scoped_refptr<base::TaskRunner>& file_task_runner);
-  ~AsarProtocolHandler() override;
-    }
-    
-    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
-                                       net::NetworkDelegate* network_delegate)
-    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
-    
-    namespace atom {
-    }
-    
-        void setModel(WalletModel *model);
-    void setAddress_SM(const QString &address);
-    void setAddress_VM(const QString &address);
-    
-        /** In:
-     *    Eric Brier and Marc Joye, Weierstrass Elliptic Curves and Side-Channel Attacks.
-     *    In D. Naccache and P. Paillier, Eds., Public Key Cryptography, vol. 2274 of Lecture Notes in Computer Science, pages 335-345. Springer-Verlag, 2002.
-     *  we find as solution for a unified addition/doubling formula:
-     *    lambda = ((x1 + x2)^2 - x1 * x2 + a) / (y1 + y2), with a = 0 for secp256k1's curve equation.
-     *    x3 = lambda^2 - (x1 + x2)
-     *    2*y3 = lambda * (x1 + x2 - 2 * x3) - (y1 + y2).
-     *
-     *  Substituting x_i = Xi / Zi^2 and yi = Yi / Zi^3, for i=1,2,3, gives:
-     *    U1 = X1*Z2^2, U2 = X2*Z1^2
-     *    S1 = Y1*Z2^3, S2 = Y2*Z1^3
-     *    Z = Z1*Z2
-     *    T = U1+U2
-     *    M = S1+S2
-     *    Q = T*M^2
-     *    R = T^2-U1*U2
-     *    X3 = 4*(R^2-Q)
-     *    Y3 = 4*(R*(3*Q-2*R^2)-M^4)
-     *    Z3 = 2*M*Z
-     *  (Note that the paper uses xi = Xi / Zi and yi = Yi / Zi instead.)
-     *
-     *  This formula has the benefit of being the same for both addition
-     *  of distinct points and doubling. However, it breaks down in the
-     *  case that either point is infinity, or that y1 = -y2. We handle
-     *  these cases in the following ways:
-     *
-     *    - If b is infinity we simply bail by means of a VERIFY_CHECK.
-     *
-     *    - If a is infinity, we detect this, and at the end of the
-     *      computation replace the result (which will be meaningless,
-     *      but we compute to be constant-time) with b.x : b.y : 1.
-     *
-     *    - If a = -b, we have y1 = -y2, which is a degenerate case.
-     *      But here the answer is infinity, so we simply set the
-     *      infinity flag of the result, overriding the computed values
-     *      without even needing to cmov.
-     *
-     *    - If y1 = -y2 but x1 != x2, which does occur thanks to certain
-     *      properties of our curve (specifically, 1 has nontrivial cube
-     *      roots in our field, and the curve equation has no x coefficient)
-     *      then the answer is not infinity but also not given by the above
-     *      equation. In this case, we cmov in place an alternate expression
-     *      for lambda. Specifically (y1 - y2)/(x1 - x2). Where both these
-     *      expressions for lambda are defined, they are equal, and can be
-     *      obtained from each other by multiplication by (y1 + y2)/(y1 + y2)
-     *      then substitution of x^3 + 7 for y^2 (using the curve equation).
-     *      For all pairs of nonzero points (a, b) at least one is defined,
-     *      so this covers everything.
-     */
-    
-    static void secp256k1_hmac_sha256_initialize(secp256k1_hmac_sha256_t *hash, const unsigned char *key, size_t keylen) {
-    int n;
-    unsigned char rkey[64];
-    if (keylen <= 64) {
-        memcpy(rkey, key, keylen);
-        memset(rkey + keylen, 0, 64 - keylen);
-    } else {
-        secp256k1_sha256_t sha256;
-        secp256k1_sha256_initialize(&sha256);
-        secp256k1_sha256_write(&sha256, key, keylen);
-        secp256k1_sha256_finalize(&sha256, rkey);
-        memset(rkey + 32, 0, 32);
-    }
-    }
-    
-        s_one[31] = 1;
-    /* Check against pubkey creation when the basepoint is the generator */
-    for (i = 0; i < 100; ++i) {
-        secp256k1_sha256_t sha;
-        unsigned char s_b32[32];
-        unsigned char output_ecdh[32];
-        unsigned char output_ser[32];
-        unsigned char point_ser[33];
-        size_t point_ser_len = sizeof(point_ser);
-        secp256k1_scalar s;
-    }
-    
-    void test_ecdsa_recovery_api(void) {
-    /* Setup contexts that just count errors */
-    secp256k1_context *none = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
-    secp256k1_context *sign = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
-    secp256k1_context *vrfy = secp256k1_context_create(SECP256K1_CONTEXT_VERIFY);
-    secp256k1_context *both = secp256k1_context_create(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY);
-    secp256k1_pubkey pubkey;
-    secp256k1_pubkey recpubkey;
-    secp256k1_ecdsa_signature normal_sig;
-    secp256k1_ecdsa_recoverable_signature recsig;
-    unsigned char privkey[32] = { 1 };
-    unsigned char message[32] = { 2 };
-    int32_t ecount = 0;
-    int recid = 0;
-    unsigned char sig[74];
-    unsigned char zero_privkey[32] = { 0 };
-    unsigned char over_privkey[32] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-                                       0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-    }
-    
-    BOOST_AUTO_TEST_CASE(bip173_testvectors_valid)
-{
-    static const std::string CASES[] = {
-        'A12UEL5L',
-        'a12uel5l',
-        'an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs',
-        'abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw',
-        '11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j',
-        'split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w',
-        '?1ezyfcl',
-    };
-    for (const std::string& str : CASES) {
-        auto ret = bech32::Decode(str);
-        BOOST_CHECK(!ret.first.empty());
-        std::string recode = bech32::Encode(ret.first, ret.second);
-        BOOST_CHECK(!recode.empty());
-        BOOST_CHECK(CaseInsensitiveEqual(str, recode));
-    }
-}
-    
-    #ifndef BITCOIN_BECH32_H
-#define BITCOIN_BECH32_H
-    
-    
-  bool ReadProtoFromTextContent(const std::string& text,
-                                ::google::protobuf::Message* proto) const {
-    bool success = google::protobuf::TextFormat::ParseFromString(text, proto);
-    return success;
-  }
-    
-    /*!
- * Copyright (c) 2015 by Contributors
- * \file kvstore.cc
- * \brief implement kv_store
- */
-#include <mxnet/kvstore.h>
-#include <stdlib.h>
-#include <dmlc/logging.h>
-#include './kvstore_local.h'
-    
-    
-    {    Tensor<gpu, 4, DType> data = in_data[bs::kData].get<gpu, 4, DType>(s);
-    Tensor<gpu, 4, DType> grid = in_data[bs::kGrid].get<gpu, 4, DType>(s);
-    Tensor<gpu, 4, DType> grid_tmp = out_data[bs::kTmp].get<gpu, 4, DType>(s);
-    Tensor<gpu, 4, DType> out = out_data[bs::kOut].get<gpu, 4, DType>(s);
-    // grid_tmp : (batch, h, w, 2)
-    grid_tmp = transpose(grid, Shape4(0, 2, 3, 1));
-    if (!init_cudnn_) {
-     Init(s, in_data, out_data);
-    }
-    CHECK_EQ(data.CheckContiguous(), true);
-    CHECK_EQ(out.CheckContiguous(), true);
-    CHECK_EQ(grid_tmp.CheckContiguous(), true);
-    typename DataType<DType>::ScaleType alpha = 1.0f;
-    typename DataType<DType>::ScaleType beta = 0.0f;
-    CUDNN_CALL(cudnnSpatialTfSamplerForward(s->dnn_handle_,
-                                            st_desc_,
-                                            &alpha,
-                                            in_desc_,
-                                            data.dptr_,
-                                            grid_tmp.dptr_,
-                                            &beta,
-                                            out_desc_,
-                                            out.dptr_));
-  }
-    
-    /*!
- * Copyright (c) 2015 by Contributors
- * \file cudnn_lrn-inl.h
- * \brief
- * \author Bing Xu
-*/
-    
-    DEFINE_int32(threads, 16, 'Number of concurrent threads to run.');
-DEFINE_int64(cache_size, 8 * KB * KB,
-             'Number of bytes to use as a cache of uncompressed data.');
-DEFINE_int32(num_shard_bits, 4, 'shard_bits.');
-    
-    
-    {}  // namespace rocksdb
-    
-      ////////////////////////////////////////////////////////
-  //
-  // 'Read Committed' (Monotonic Atomic Views) Example
-  //   --Using multiple Snapshots
-  //
-  ////////////////////////////////////////////////////////
-    
-    class FlushBlockBySizePolicyFactory : public FlushBlockPolicyFactory {
- public:
-  FlushBlockBySizePolicyFactory() {}
-    }
-    
-    
-    {}  // namespace rocksdb
+        #endif  // STORAGE_LEVELDB_DB_DB_ITER_H_
 
     
-      // The type used to refer to a thread operation.
-  // A thread operation describes high-level action of a thread.
-  // Examples include compaction and flush.
-  enum OperationType : int {
-    OP_UNKNOWN = 0,
-    OP_COMPACTION,
-    OP_FLUSH,
-    NUM_OP_TYPES
-  };
     
-    class LDBCommandExecuteResult {
-public:
-  enum State {
-    EXEC_NOT_STARTED = 0, EXEC_SUCCEED = 1, EXEC_FAILED = 2,
-  };
+// Owned filenames have the form:
+//    dbname/CURRENT
+//    dbname/LOCK
+//    dbname/LOG
+//    dbname/LOG.old
+//    dbname/MANIFEST-[0-9]+
+//    dbname/[0-9]+.(log|sst|ldb)
+bool ParseFileName(const std::string& filename,
+                   uint64_t* number,
+                   FileType* type) {
+  Slice rest(filename);
+  if (rest == 'CURRENT') {
+    *number = 0;
+    *type = kCurrentFile;
+  } else if (rest == 'LOCK') {
+    *number = 0;
+    *type = kDBLockFile;
+  } else if (rest == 'LOG' || rest == 'LOG.old') {
+    *number = 0;
+    *type = kInfoLogFile;
+  } else if (rest.starts_with('MANIFEST-')) {
+    rest.remove_prefix(strlen('MANIFEST-'));
+    uint64_t num;
+    if (!ConsumeDecimalNumber(&rest, &num)) {
+      return false;
     }
-    
-    Register Register::FromParameterIndex(int index, int parameter_count) {
-  DCHECK_GE(index, 0);
-  DCHECK_LT(index, parameter_count);
-  int register_index = kLastParamRegisterIndex - parameter_count + index + 1;
-  DCHECK_LT(register_index, 0);
-  return Register(register_index);
-}
-    
-      // Returns the register which holds the current context object.
-  static Register current_context();
-  bool is_current_context() const;
-    
-    
-void HandlerTableBuilder::SetHandlerTarget(int handler_id, size_t offset) {
-  DCHECK(Smi::IsValid(offset));  // Encoding of handler table requires this.
-  entries_[handler_id].offset_target = offset;
-}
-    
-    namespace interpreter {
+    if (!rest.empty()) {
+      return false;
     }
-    
-    
-    {  return task;
-}
-    
-    bool TraceBufferRingBuffer::Flush() {
-  base::MutexGuard guard(&mutex_);
-  // This flushes all the traces stored in the buffer.
-  if (!is_empty_) {
-    for (size_t i = NextChunkIndex(chunk_index_);; i = NextChunkIndex(i)) {
-      if (auto& chunk = chunks_[i]) {
-        for (size_t j = 0; j < chunk->size(); ++j) {
-          trace_writer_->AppendTraceEvent(chunk->GetEventAt(j));
-        }
-      }
-      if (i == chunk_index_) break;
+    *type = kDescriptorFile;
+    *number = num;
+  } else {
+    // Avoid strtoull() to keep filename format independent of the
+    // current locale
+    uint64_t num;
+    if (!ConsumeDecimalNumber(&rest, &num)) {
+      return false;
     }
+    Slice suffix = rest;
+    if (suffix == Slice('.log')) {
+      *type = kLogFile;
+    } else if (suffix == Slice('.sst') || suffix == Slice('.ldb')) {
+      *type = kTableFile;
+    } else if (suffix == Slice('.dbtmp')) {
+      *type = kTempFile;
+    } else {
+      return false;
+    }
+    *number = num;
   }
-  trace_writer_->Flush();
-  // This resets the trace buffer.
-  is_empty_ = true;
   return true;
 }
+    
+    unsigned int Reader::ReadPhysicalRecord(Slice* result) {
+  while (true) {
+    if (buffer_.size() < kHeaderSize) {
+      if (!eof_) {
+        // Last read was a full read, so this is a trailer to skip
+        buffer_.clear();
+        Status status = file_->Read(kBlockSize, &buffer_, backing_store_);
+        end_of_buffer_offset_ += buffer_.size();
+        if (!status.ok()) {
+          buffer_.clear();
+          ReportDrop(kBlockSize, status);
+          eof_ = true;
+          return kEof;
+        } else if (buffer_.size() < kBlockSize) {
+          eof_ = true;
+        }
+        continue;
+      } else {
+        // Note that if buffer_ is non-empty, we have a truncated header at the
+        // end of the file, which can be caused by the writer crashing in the
+        // middle of writing the header. Instead of considering this an error,
+        // just report EOF.
+        buffer_.clear();
+        return kEof;
+      }
+    }
+    }
+    }
+    
+    TEST(LogTest, UnexpectedFullType) {
+  Write('foo');
+  Write('bar');
+  SetByte(6, kFirstType);
+  FixChecksum(0, 3);
+  ASSERT_EQ('bar', Read());
+  ASSERT_EQ('EOF', Read());
+  ASSERT_EQ(3, DroppedBytes());
+  ASSERT_EQ('OK', MatchError('partial record without end'));
+}
+    
+    namespace leveldb {
+namespace log {
+    }
+    }
+    
+    #include 'db/table_cache.h'
+    
+    #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Utils.h'
+    
+            bool IsPacked() const { return m_isPacked; }
+    
+    
+    {    ~CrossProcessMutex()
+    {
+        if (m_handle != NULL)
+        {
+            Release();
+        }
+    }
+};
+    
+    size_t DataReader::GetCurrentSamplePosition()
+{
+    // BUGBUG: composition of old readers is not supported.
+    // Returning just for the last reader.
+    return m_dataReaders[m_ioNames.back()]->GetCurrentSamplePosition();
+}
+    
+    // base class that we can catch, independent of the type parameter
+struct /*interface*/ IExceptionWithCallStackBase
+{
+    virtual const char * CallStack() const = 0;
+    virtual ~IExceptionWithCallStackBase() noexcept = default;
+};
+    
+        double ElapsedSeconds();
+    
+        virtual void /*ComputationNodeBase::*/ Validate(bool isFinalValidationPass) override
+    {
+        ValidateBinaryReduce(isFinalValidationPass);
+    }
+    
+        template <class ElemType>
+    void RequestRelease(shared_ptr<Matrix<ElemType>> *pMatrixPtr)
+    {
+        auto memInfo = GetMemInfo(pMatrixPtr);
+        if (memInfo != nullptr)
+        {
+            memInfo->SetReleaseStep(m_stepCounter);
+        }
+        m_stepCounter++; 
+    }
