3e19e93c5c0b10f899c2debc02ef5e2a8429b7ee
SUNsung <weatherius@gmail.com>
2019-04-06 09:39:33 +0300

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2019-04-06 09:39:33

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,50 +1,19 @@
 
         
-          # Print rows
-  for i, label1 in enumerate(short_labels):
-    row = '%{0}s '.format(columnwidth) % label1[:10]
-    for j in range(len(short_labels)):
-      value = int(cm[i, j]) if not np.isnan(cm[i, j]) else 0
-      cell = ' %{0}d '.format(10) % value
-      row += cell + ' '
-    print(row)
+        def get_orig_video_source(api_key, pid, secret):
+    parsed = json.loads(get_content_headered(tmpl_api_call_video_info % (api_key, pid, secret)))
+    for stream in parsed['streams']['stream']:
+        if stream['type'] == 'orig':
+            return stream['_content'].replace('\\', '')
+    return None
     
-          self.weights1 = tf.get_variable(
-          'W1',
-          shape=[network_input, hidden_dim],
-          dtype=tf.float32)
-      self.bias1 = tf.get_variable(
-          'b1',
-          shape=[hidden_dim],
-          dtype=tf.float32)
-    
-      Args:
-    model: The trained path-based model.
-    session: The current TensorFlow session.
-    instances: All the train, test and validation instances.
-    
-    # write out the dataset
-write_datasets(FLAGS.save_dir, FLAGS.datafile_name, datasets)
-print ('Saved to ', os.path.join(FLAGS.save_dir,
-                                 FLAGS.datafile_name + '_' + dataset_name))
+    site_info = 'Giphy.com'
+download = giphy_download
+download_playlist = playlist_not_supported('giphy')
 
     
-      Args:
-    data_fname: The filename of teh file in which to write the data.
-    data_dict:  The dictionary of data to write. The keys are strings
-      and the values are numpy arrays.
-    use_json (optional): human readable format for simple items
-    compression (optional): The compression to use for h5py (disabled by
-      default because the library borks on scalars, otherwise try 'gzip').
-  '''
+        return re.search(patt, page).group(1)
     
-        for i in range(batch_size):
-      data_index = batch * batch_size + i
-      example = raw_data[data_index]
-    
-    
-def convert_to_human_readable(id_to_word, arr, max_num_to_print):
-  '''Convert a np.array of indices into words using id_to_word dictionary.
-  Return max_num_to_print results.
-  '''
-  assert arr.ndim == 2
+    from ..common import *
+from ..extractor import VideoExtractor
+from .universal import *
