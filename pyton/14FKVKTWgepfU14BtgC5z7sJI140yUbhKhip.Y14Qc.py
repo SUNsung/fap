2c16fa932dbb05c31c9f067705802749c3db7ba2
SUNsung <weatherius@gmail.com>
2019-04-06 10:49:05 +0300

    Rewrite [14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py] File in: 2019-04-06 10:49:04

--- pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
+++ pyton/14FKVKTWgepfU14BtgC5z7sJI140yUbhKhip.Y14Qc.py
@@ -1,81 +1,72 @@
 
         
-        
-def l1_l2(l1=0.01, l2=0.01):
-    return L1L2(l1=l1, l2=l2)
+          # Concatenate the edge components to create a path tensor:
+  # [max_paths_per_ins, max_path_length, 4]
+  lemmas = _word_dropout(
+      tf.reshape(features['lemmas'], [-1, max_path_len]), input_keep_prob)
     
-        outputs1 = Lambda(lambda x: utils.preprocess_input(x, 'channels_last'),
-                      output_shape=x.shape[1:])(inputs)
-    model1 = Model(inputs, outputs1)
-    out1 = model1.predict(x)
-    x2 = np.transpose(x, (0, 3, 1, 2))
-    inputs2 = Input(shape=x2.shape[1:])
-    outputs2 = Lambda(lambda x: utils.preprocess_input(x, 'channels_first'),
-                      output_shape=x2.shape[1:])(inputs2)
-    model2 = Model(inputs2, outputs2)
-    out2 = model2.predict(x2)
-    assert_allclose(out1, out2.transpose(0, 2, 3, 1))
+          if not samples:
+        samples = [sample]
+        char_ids_samples = [sample_char_ids]
+      sent += vocab.id_to_word(samples[0]) + ' '
+      sys.stderr.write('%s\n' % sent)
     
+      def _score(self, word_patch):
+    '''Score a matrix of shape (batch_size, num_timesteps+1) str tokens.'''
+    word_ids = np.array(
+        [[self.vocab.word_to_id(word) for word in row]
+         for row in word_patch])
+    char_ids = np.array(
+        [[self.vocab.word_to_char_ids(word) for word in row]
+         for row in word_patch])
+    print('Probs for \n{}\n='.format(np.array(word_patch)[:, 1:]))
     
-def test_fashion_mnist():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-        assert len(x_train) == len(y_train) == 60000
-        assert len(x_test) == len(y_test) == 10000
+      train_data = _read_words(train_path)
+  valid_data = _read_words(valid_path)
+  return train_data, valid_data
     
-    # convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+      if epoch_size == 0:
+    raise ValueError('epoch_size == 0, decrease batch_size or num_steps')
     
-    # Stack of Transposed Conv2D blocks
-# Notes:
-# 1) Use Batch Normalization before ReLU on deep networks
-# 2) Use UpSampling2D as alternative to strides>1
-# - faster but not as good as strides>1
-for filters in layer_filters[::-1]:
-    x = Conv2DTranspose(filters=filters,
-                        kernel_size=kernel_size,
-                        strides=2,
-                        activation='relu',
-                        padding='same')(x)
+      Returns:
+    loss:  Scalar tf.float32 loss.
     
-        def test_bad_save_checkpoint(self):
-        self.config.reverter.add_to_checkpoint = mock.Mock(
-            side_effect=errors.ReverterError)
-        self.config.parser.add_dir(
-            self.vh_truth[0].path, 'Test', 'bad_save_ckpt')
-        self.assertRaises(errors.PluginError, self.config.save)
+        elif (FLAGS.discriminator_model == 'bidirectional_zaremba' or
+          FLAGS.discriminator_model == 'bidirectional_vd'):
+      assert FLAGS.language_model_ckpt_dir_reversed is not None, (
+          'Need a reversed directory to fill in the backward components.')
+      load_fwd_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
+      load_bwd_ckpt = tf.train.latest_checkpoint(
+          FLAGS.language_model_ckpt_dir_reversed)
+      print('Restoring Discriminator from %s and %s.' % (load_fwd_ckpt,
+                                                         load_bwd_ckpt))
+      tf.logging.info('Restoring Discriminator from %s and %s.' %
+                      (load_fwd_ckpt, load_bwd_ckpt))
+      dis_fwd_init_saver = init_savers['dis_fwd_init_saver']
+      dis_bwd_init_saver = init_savers['dis_bwd_init_saver']
+      dis_fwd_init_saver.restore(sess, load_fwd_ckpt)
+      dis_bwd_init_saver.restore(sess, load_bwd_ckpt)
     
+        The main difference between this decoder function and the `decoder_fn` in
+    `attention_decoder_fn_train` is how `next_cell_input` is calculated. In
+    decoder function we calculate the next input by applying an argmax across
+    the feature dimension of the output from the decoder. This is a
+    greedy-search approach. (Bahdanau et al., 2014) & (Sutskever et al., 2014)
+    use beam-search instead.
     
-class SelectVhostMultiTest(unittest.TestCase):
-    '''Tests for certbot_apache.display_ops.select_vhost_multiple.'''
+    # ZoneoutWrapper.
+from regularization import zoneout
     
-    import mock
+    import rsa
+import json
+from binascii import hexlify
     
-            self.vhost1b = VirtualHost(
-            'filep', 'vh_path', set([self.addr1]), False, False, 'localhost')
+        # Get the version from youtube_dl/version.py without importing the package
+    exec(compile(open('youtube_dl/version.py').read(),
+                 'youtube_dl/version.py', 'exec'))
     
-            # XXX: Deleting all of this is kind of scary unless the test
-        #      instances really each have a complete configuration!
-        shutil.rmtree('/etc/nginx')
-        shutil.copytree(server_root, '/etc/nginx', symlinks=True)
+    PREFIX = r'''%YOUTUBE-DL(1)
     
-    
-def test_1():
-    for o in [None, True, False, 0, 1, (1 << 6), (1 << 7) - 1, -1,
-              -((1 << 5) - 1), -(1 << 5)]:
-        check(1, o)
-    
-            # object
-        exp = pd.Index([pd.Timestamp('2011-01-01 09:00', tz=tz),
-                        'x',
-                        pd.Timestamp('2011-01-01 11:00', tz=tz)],
-                       dtype=object)
-        tm.assert_index_equal(idx.fillna('x'), exp)
-
-    
-        result = s[lambda x: [True, False, True, True]]
-    tm.assert_series_equal(result, s.iloc[[0, 2, 3]])
+        def setUp(self):
+        if self._SKIP_SOCKS_TEST:
+            return
