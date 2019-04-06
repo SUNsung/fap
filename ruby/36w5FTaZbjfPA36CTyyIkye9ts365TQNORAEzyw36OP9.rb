9d8c33045ad5bb4be1991869363d8cdd6a6a9dc4
SUNsung <weatherius@gmail.com>
2019-04-06 13:05:22 +0300

    Rewrite [36w5FTaZbjfPA36CTyyIkye9ts365TQNORAEzyw36OP9.rb] File in: 2019-04-06 13:05:22

--- ruby/36w5FTaZbjfPA36CTyyIkye9ts365TQNORAEzyw36OP9.rb
+++ ruby/36w5FTaZbjfPA36CTyyIkye9ts365TQNORAEzyw36OP9.rb
@@ -1,145 +1,100 @@
 
         
-            resp.body =
-      '<html><head>' +
-      '<title>404 Not Found</title>' +
-      '</head><body>' +
-      '<h1>Not found</h1>' +
-      'The requested URL #{html_escape(request.resource)} was not found on this server.<p><hr>' +
-      '</body></html>'
+        def docs_name
+  '#{name}-docs'
+end
     
+    # For this pull request, which changes Page#dir
+# https://github.com/jekyll/jekyll/pull/4403
     
-IAX_SUBTYPE_NEW     = 1
-IAX_SUBTYPE_PING    = 2
-IAX_SUBTYPE_PONG    = 3
-IAX_SUBTYPE_ANSWER  = 4
-IAX_SUBTYPE_ACK     = 4
-IAX_SUBTYPE_HANGUP  = 5
-IAX_SUBTYPE_REJECT  = 6
-IAX_SUBTYPE_ACCEPT  = 7
-IAX_SUBTYPE_AUTHREQ = 8
-IAX_SUBTYPE_AUTHREP = 9
-IAX_SUBTYPE_INVAL   = 10
-IAX_SUBTYPE_LAGRQ   = 11
-IAX_SUBTYPE_LAGRP   = 12
-IAX_SUBTYPE_REGREQ  = 13
-IAX_SUBTYPE_REGAUTH = 14
-IAX_SUBTYPE_REGACK  = 15
-IAX_SUBTYPE_REGREJ  = 16
-IAX_SUBTYPE_REGREL  = 17
-IAX_SUBTYPE_VNAK    = 18
-    
-      #
-  # Move these into an IPMI stack or mixin at some point
-  #
-    
-              # Decodes the enc_part
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Model::EncryptedData]
-          def decode_enc_part(input)
-            Rex::Proto::Kerberos::Model::EncryptedData.decode(input.value[0])
-          end
+            def stop
+          # There is only one EventMachine instance per Ruby process so stopping
+          # it here will stop the reactor thread we have running.
+          EM.stop if EM.reactor_running?
+          Jekyll.logger.debug 'LiveReload Server:', 'halted'
         end
+    
+    describe GivenDailyLike do
+    
+            css('.example-title + pre').each do |node|
+          node['name'] = node.previous_element.content.strip
+          node.previous_element.remove
+        end
+    
+            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
+          next if node.previous.try(:content).present? || node.next.try(:content).present?
+          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
+          node.content = node.content.strip
+          node.name = 'pre' if node.content =~ /\s/
+          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
+        end
+    
+          def has_sidebar
+        @sidebar = (@page.sidebar || false) if @sidebar.nil? && @page
+        !!@sidebar
       end
-    end
+    
+          attr_reader :page, :content
+    
+      test 'extracting paths from URLs' do
+    assert_nil extract_path('Eye-Of-Sauron')
+    assert_equal 'Mordor', extract_path('Mordor/Sauron')
+    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
   end
-end
     
-              # Rex::Proto::Kerberos::Model::KrbError encoding isn't supported
-          #
-          # @raise [NotImplementedError]
-          def encode
-            raise ::NotImplementedError, 'KrbError encoding not supported'
-          end
+        @wiki.update_page(@wiki.page('k'), nil, nil, '다른 text', {})
+    @wiki = Gollum::Wiki.new(@path)
+    page  = @wiki.page('k')
+    assert_equal '다른 text', utf8(page.raw_data)
     
-        def dry_run
-      ['--dry-run', '-n',
-       'Do a dry run without executing actions',
-       lambda do |_value|
-         Configuration.env.set(:sshkit_backend, SSHKit::Backend::Printer)
-       end]
-    end
-    
-          # Checks whether this case statement has an `else` branch.
-      #
-      # @return [Boolean] whether the `case` statement has an `else` branch
-      def else?
-        loc.else
       end
-    end
-  end
 end
 
     
-          # Checks whether this node body is a void context.
-      #
-      # @return [Boolean] whether the `def` node body is a void context
-      def void_context?
-        method?(:initialize) || assignment_method?
-      end
-    
-          # Custom destructuring method. This is used to normalize the branches
-      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
-      #
-      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
-      def node_parts
-        [self, self]
-      end
-    end
-  end
-end
-
-    
-          # Returns the value of this `hash` element.
-      #
-      # @note For keyword splats, this returns the whole node
-      #
-      # @return [Node] the value of the hash element
-      def value
-        node_parts[1]
-      end
-    
-    $redis = Redis.new
-    
-        # This reopens ALL logfiles in the process that have been rotated
-    # using logrotate(8) (without copytruncate) or similar tools.
-    # A +File+ object is considered for reopening if it is:
-    #   1) opened with the O_APPEND and O_WRONLY flags
-    #   2) the current open file handle does not match its original open path
-    #   3) unbuffered (as far as userspace buffering goes, not O_SYNC)
-    # Returns the number of files reopened
-    def self.reopen_logs
-      to_reopen = []
-      append_flags = File::WRONLY | File::APPEND
-    
-        def app
-      @app ||= build
-    end
-    
-            _render { content }
-      end
-    end
-    
-    # Use this to fill in an entire form with data from a table. Example:
-#
-#   When I fill in the following:
-#     | Account Number | 5002       |
-#     | Expiry date    | 2009-11-01 |
-#     | Note           | Nice guy   |
-#     | Wants Email?   |            |
-#
-# TODO: Add support for checkbox, select og option
-# based on naming conventions.
-#
-When /^(?:|I )fill in the following:$/ do |fields|
-  fields.rows_hash.each do |name, value|
-    When %{I fill in '#{name}' with '#{value}'}
+    desc 'Test the paperclip plugin under all supported Rails versions.'
+task :all do |t|
+  if ENV['BUNDLE_GEMFILE']
+    exec('rake spec && cucumber')
+  else
+    exec('rm -f gemfiles/*.lock')
+    Rake::Task['appraisal:gemfiles'].execute
+    Rake::Task['appraisal:install'].execute
+    exec('rake appraisal')
   end
 end
     
-    Before do
-  gemfile = ENV['BUNDLE_GEMFILE'].to_s
-  ENV['BUNDLE_GEMFILE'] = File.join(Dir.pwd, gemfile) unless gemfile.start_with?(Dir.pwd)
-  @framework_version = nil
-end
+      steps %{
+    And I turn off class caching
+    And I write to 'Gemfile' with:
+      '''
+      source 'http://rubygems.org'
+      gem 'rails', '#{framework_version}'
+      gem 'sqlite3', :platform => [:ruby, :rbx]
+      gem 'activerecord-jdbcsqlite3-adapter', :platform => :jruby
+      gem 'jruby-openssl', :platform => :jruby
+      gem 'capybara'
+      gem 'gherkin'
+      gem 'aws-sdk-s3'
+      gem 'racc', :platform => :rbx
+      gem 'rubysl', :platform => :rbx
+      '''
+    And I remove turbolinks
+    And I comment out lines that contain 'action_mailer' in 'config/environments/*.rb'
+    And I empty the application.js file
+    And I configure the application to use 'paperclip' from this project
+  }
+    
+    module RailsCommandHelpers
+  def framework_version?(version_string)
+    framework_version =~ /^#{version_string}/
+  end
+    
+      def generate_migration
+    migration_template('paperclip_migration.rb.erb',
+                       'db/migrate/#{migration_file_name}',
+                       migration_version: migration_version)
+  end
+    
+        def register_new_attachment
+      Paperclip::AttachmentRegistry.register(@klass, @name, @options)
+    end
