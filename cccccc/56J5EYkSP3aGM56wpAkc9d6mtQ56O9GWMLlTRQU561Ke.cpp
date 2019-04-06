8a3534c7c1581e3276c3c9e9e8ff11d79feebb4f
SUNsung <weatherius@gmail.com>
2019-04-06 19:22:55 +0300

    Rewrite [56J5EYkSP3aGM56wpAkc9d6mtQ56O9GWMLlTRQU561Ke.cpp] File in: 2019-04-06 19:22:54

--- cccccc/56J5EYkSP3aGM56wpAkc9d6mtQ56O9GWMLlTRQU561Ke.cpp
+++ cccccc/56J5EYkSP3aGM56wpAkc9d6mtQ56O9GWMLlTRQU561Ke.cpp
@@ -1,266 +1,154 @@
 
         
-        
-    {}
-    
-        assert(info.name.size() < (2 << 10) && 'name failed sanity check');
-    
-    #include 'swift/Basic/DiverseList.h'
-#include 'swift/Basic/DiverseStack.h'
-using namespace swift;
-    
-      struct IndentScope {
-    TreePrinter *Printer;
-    size_t OldLength;
-    IndentScope(TreePrinter *printer, StringRef indent)
-        : Printer(printer), OldLength(printer->Indent.size()) {
-      Printer->Indent += indent;
-    }
-    ~IndentScope() { Printer->Indent.resize(OldLength); }
-  };
-    
-    
-    {  return scratch.copyString(result);
-}
-    
-        void emitDiagnosticLoc(clang::FullSourceLoc Loc, clang::PresumedLoc PLoc,
-                           clang::DiagnosticsEngine::Level Level,
-                           ArrayRef<clang::CharSourceRange> Ranges) override {}
-    
-    #endif
-
-    
-    // Max number of scripts in ICU + 'NULL' + Japanese and Korean + Fraktur
-const int kMaxNumberOfScripts = 116 + 1 + 2 + 1;
-    
-    struct BlobData {
-  BlobData() : blob(nullptr), choices(nullptr) {}
-  BlobData(int index, Tesseract* tess, const WERD_RES& word)
-    : blob(word.chopped_word->blobs[index]),
-      tesseract(tess),
-      choices(&(*word.ratings)(index, index)) {}
-    }
-    
-    #include <cstdio>    // for FILE
-#include 'strngs.h'  // for STRING
-    
-    
-    {    ICOORD pt;
-    int halfwidth;
-  };
-  // Type holds the distance of each point from the fitted line and the point
-  // itself. Use of double allows integer distances from ICOORDs to be stored
-  // exactly, and also the floating point results from ConstrainedFit.
-  using DistPointPair = KDPairInc<double, ICOORD>;
-    
-    #include 'dppoint.h'
-#include 'errcode.h'
-#include 'tprintf.h'
-    
-    #ifndef TESSERACT_CCSTRUCT_DPPOINT_H_
-#define TESSERACT_CCSTRUCT_DPPOINT_H_
-    
-     private:
-  // Saves the given Pix as a PNG-encoded string and destroys it.
-  static void SetPixInternal(Pix* pix, GenericVector<char>* image_data);
-  // Returns the Pix image for the image_data. Must be pixDestroyed after use.
-  static Pix* GetPixInternal(const GenericVector<char>& image_data);
-  // Parses the text string as a box file and adds any discovered boxes that
-  // match the page number. Returns false on error.
-  bool AddBoxes(const char* box_text);
-    
-      // Returns the covariance.
-  double covariance() const {
-    if (total_weight > 0.0)
-      return (sigxy - sigx * sigy / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-  double x_variance() const {
-    if (total_weight > 0.0)
-      return (sigxx - sigx * sigx / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-  double y_variance() const {
-    if (total_weight > 0.0)
-      return (sigyy - sigy * sigy / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-    
-    #define MODULUS       128        /*range of directions */
-#define DIRBITS       7          //no of bits used
-#define DIRSCALE      1000       //length of vector
-    
-            ListNode* delNode = p->next;
-        p->next = delNode->next;
-        delete delNode;
-    
-    using namespace std;
-    
-    
-#include <iostream>
-#include <vector>
-#include <cassert>
-#include <stdexcept>
+                    cur = stack.top();
+            stack.pop();
+            res.push_back(cur->val);
+            cur = cur->right;
     
     // Recursive
 // Time Complexity: O(n), n is the node number in the tree
 // Space Complexity: O(h), h is the height of the tree
 class Solution {
 public:
-    vector<int> inorderTraversal(TreeNode* root) {
+    vector<int> preorderTraversal(TreeNode* root) {
     }
     }
     
-    #include <iostream>
-#include <vector>
-#include <stack>
+    using namespace std;
     
-    
-/// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-    
-    
-    {
-    {        return res;
-    }
-};
-    
-            queue<pair<TreeNode*,int>> q;
-        q.push(make_pair(root, 0));
-    
-    
-    {    return 0;
-}
-
-    
-    #include <iostream>
-#include <vector>
-#include <stack>
-#include <cassert>
-    
-            vector<int> res;
-        if(root == NULL)
-            return res;
-    
-    
-    {
-    {
-    {            TagNode tagNode = stack.top();
-            stack.pop();
-            cur = tagNode.node;
-            if(tagNode.isFirst == false){
-                tagNode.isFirst = true;
-                stack.push(tagNode);
-                cur = cur->right;
+            size_t numSequences = oneHotSequences.size();
+        NDShape valueDataShape = sampleShape.AppendShape({ maxSequenceLength, numSequences });
+        size_t numCSCCols = valueDataShape.SubShape(1).TotalSize() + 1;
+        std::vector<SparseIndexType> colStarts(numCSCCols);
+        std::vector<ElementType> nonZeroValues;
+        std::vector<SparseIndexType> rowIndices;
+        for (size_t i = 0; i < numSequences; ++i)
+        {
+            size_t currentSequenceNumCols = oneHotSequences[i].size();
+            size_t j = 0;
+            for (; j < currentSequenceNumCols; ++j)
+            {
+                colStarts[(i * maxSequenceNumCols) + j] = (SparseIndexType)nonZeroValues.size();
+                size_t oneHotIdx = oneHotSequences[i][j];
+                if ((oneHotIdx & OneHotSkip) == OneHotSkip) // note that OneHotSkip used to be (size_t)-1, and later changed to (uint32_t)-1. Both are supported
+                {
+                    nonZeroValues.push_back(0);
+                    rowIndices.push_back(0);
+                }
+                else
+                {
+                    nonZeroValues.push_back(1);
+                    if (oneHotIdx >= dimension)
+                        InvalidArgument('Value::Create: one-hot index value (%zu) exceeds vocabulary size (%zu).', oneHotSequences[i][j], dimension);
+                    rowIndices.push_back((SparseIndexType)(oneHotSequences[i][j]));
+                }
             }
-            else{
-                res.push_back(cur->val);
-                cur = NULL;
-            };
-        }
-        return res;
     }
-};
     
+            // Now we have three kinds of nodes:
+        //  - unmodified nodes that will be shared with the old network
+        //  - modified nodes (user edits and their parents)
+        //  - original nodes that are no longer referenced
+        // The new network will be constructed to have the same roots as the original.
     
+        virtual void /*ComputationNode::*/ BackpropTo(const size_t /*inputIndex*/, const FrameRange& fr) override
     {
-    {        return res;
-    }
-};
-    
-    
-  while (true)
-  {
-    xfer += iprot->readFieldBegin(fname, ftype, fid);
-    if (ftype == ::apache::thrift::protocol::T_STOP) {
-      break;
-    }
-    xfer += iprot->skip(ftype);
-    xfer += iprot->readFieldEnd();
-  }
-    
-      xfer += iprot->readStructBegin(fname);
-    
-    
-    {};
-    
-    #include 'Extension.h'
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
-#include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TBufferTransports.h>
-    
-    class osqueryConstants {
- public:
-  osqueryConstants();
+        auto sliceInputGrad  = InputRef(0).GradientFor(fr);
+        auto sliceOutputGrad =           GradientFor(fr);
     }
     
-    int _kExtensionCodeValues[] = {
-  ExtensionCode::EXT_SUCCESS,
-  ExtensionCode::EXT_FAILED,
-  ExtensionCode::EXT_FATAL
-};
-const char* _kExtensionCodeNames[] = {
-  'EXT_SUCCESS',
-  'EXT_FAILED',
-  'EXT_FATAL'
-};
-const std::map<int, const char*> _ExtensionCode_VALUES_TO_NAMES(::apache::thrift::TEnumIterator(3, _kExtensionCodeValues, _kExtensionCodeNames), ::apache::thrift::TEnumIterator(-1, NULL, NULL));
-    
-          auto cant_id = smartctl.cantIdDev(devname, full_type);
-      if (cant_id.err != NOERR) {
-        LOG(INFO) << 'Error while trying to identify device: '
-                  << libsmartctl::errStr(cant_id.err);
-        continue;
-      }
-      // If device is not identifiable, the type is invalid, skip..
-      if (cant_id.content) {
-        continue;
-      }
-    
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-namespace can {
-    }
+    namespace Microsoft { namespace MSR { namespace CNTK {
     }
     }
     }
     
+        if (fMatch == FALSE) {
+        status = U_REGEX_INVALID_STATE;
+        return utext_replace(dest, destLen, destLen, NULL, 0, &status);
+    }
+    if (groupNum < 0 || groupNum > fPattern->fGroupMap->size()) {
+        status = U_INDEX_OUTOFBOUNDS_ERROR;
+        return utext_replace(dest, destLen, destLen, NULL, 0, &status);
+    }
     
-    {
-    {
-    {}  // namespace canbus
-}  // namespace drivers
-}  // namespace apollo
-
     
-    TEST(ByteTest, SetGetHighLowBit) {
-  unsigned char byte_value = 0x37;
-  Byte value(&byte_value);
-  value.set_value_high_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
-  value.set_value_low_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
+    {}  // namespace
+    
+    U_CAPI UBool U_EXPORT2
+uhash_compareScriptSet(const UElement key1, const UElement key2);
+    
+    void
+SimpleTimeZone::getOffsetFromLocal(UDate date, int32_t nonExistingTimeOpt, int32_t duplicatedTimeOpt,
+                                   int32_t& rawOffsetGMT, int32_t& savingsDST, UErrorCode& status) const {
+    if (U_FAILURE(status)) {
+        return;
+    }
+    }
+    
+        // are we comparing different lengths?
+    int32_t minLength = getLength();
+    int32_t targetLength = target.getLength();
+    if (minLength < targetLength) {
+        result = UCOL_LESS;
+    } else if (minLength == targetLength) {
+        result = UCOL_EQUAL;
+    } else {
+        minLength = targetLength;
+        result = UCOL_GREATER;
+    }
+    
+    /**
+ * Union the set of all characters that may output by this object
+ * into the given set.
+ * @param toUnionTo the set into which to union the output characters
+ */
+void StringMatcher::addReplacementSetTo(UnicodeSet& /*toUnionTo*/) const {
+    // The output of this replacer varies; it is the source text between
+    // matchStart and matchLimit.  Since this varies depending on the
+    // input text, we can't compute it here.  We can either do nothing
+    // or we can add ALL characters to the set.  It's probably more useful
+    // to do nothing.
 }
     
-      Byte t1(bytes + 2);
-  int32_t t = t1.get_byte(6, 2);
-    
-    void Brakemotorrpt372::Parse(const std::uint8_t* bytes, int32_t length,
-                             ChassisDetail* chassis) const {
-  chassis->mutable_gem()->mutable_brake_motor_rpt_3_72()->set_torque_output(
-      torque_output(bytes, length));
-  chassis->mutable_gem()->mutable_brake_motor_rpt_3_72()->set_torque_input(
-      torque_input(bytes, length));
+    /**
+ * Construct a StringReplacer that sets the emits the given output
+ * text and does not modify the cursor.
+ * @param theOutput text that will replace input text when the
+ * replace() method is called.  May contain stand-in characters
+ * that represent nested replacers.
+ * @param theData transliterator context object that translates
+ * stand-in characters to UnicodeReplacer objects
+ */
+StringReplacer::StringReplacer(const UnicodeString& theOutput,
+                               const TransliterationRuleData* theData) {
+    output = theOutput;
+    cursorPos = 0;
+    hasCursor = FALSE;
+    data = theData;
+    isComplex = TRUE;
 }
+    
+    using json = nlohmann::json;
+    
+    
+    // out_of_range.106
+    try
+    {
+        // try to use an array index with leading '0'
+        json::reference ref = j.at('/array/01'_json_pointer);
+    }
+    catch (json::parse_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+        // out_of_range.401
+    try
+    {
+        // try to use a an invalid array index
+        json::const_reference ref = j.at('/array/4'_json_pointer);
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
