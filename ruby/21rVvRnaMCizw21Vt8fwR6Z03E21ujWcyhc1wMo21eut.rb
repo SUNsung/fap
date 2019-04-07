7aaefec1f4e9182fcdf7b59862cfde9068877787
SUNsung <weatherius@gmail.com>
2019-04-07 18:58:56 +0300

    Rewrite [21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb] File in: 2019-04-07 18:58:56

--- ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
+++ ruby/21rVvRnaMCizw21Vt8fwR6Z03E21ujWcyhc1wMo21eut.rb
@@ -1,90 +1,82 @@
 
         
-        describe Admin::UsersController do
-  it 'requires to be signed in as an admin' do
-    login_as(users(:bob))
-    visit admin_users_path
-    expect(page).to have_text('Admin access required to view that page.')
-  end
+                def render
+          error_wrapping(datetime_selector(@options, @html_options).send('select_#{select_type}').html_safe)
+        end
     
-      it 'imports a scenario which requires a service' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
-    click_on 'Start Import'
-    check('I confirm that I want to import these Agents.')
-    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
-    expect(page).to have_text('Import successful!')
+          def test_helpers_with_symbol
+        @controller.process(:with_symbol)
+        assert_equal 'I respond to bare_a: true', @controller.response_body
+      end
+    
+        class WithProcReturningNil < WithString
+      layout proc { nil }
+    
+        select_agent_type('Trigger Agent')
+    fill_in(:agent_name, with: 'Test Trigger Agent')
+    click_on('Toggle View')
+    
+        it 'can be turned off' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
+      DefaultScenarioImporter.import(user)
+    end
+    
+    describe AgentsExporter do
+  describe '#as_json' do
+    let(:name) { 'My set of Agents' }
+    let(:description) { 'These Agents work together nicely!' }
+    let(:guid) { 'some-guid' }
+    let(:tag_fg_color) { '#ffffff' }
+    let(:tag_bg_color) { '#000000' }
+    let(:icon) { 'Camera' }
+    let(:source_url) { 'http://yourhuginn.com/scenarios/2/export.json' }
+    let(:agent_list) { [agents(:jane_weather_agent), agents(:jane_rain_notifier_agent)] }
+    let(:exporter) { AgentsExporter.new(
+      agents: agent_list, name: name, description: description,
+      source_url: source_url, guid: guid, tag_fg_color: tag_fg_color,
+      tag_bg_color: tag_bg_color, icon: icon) }
+    
+        it 'work with set FAILED_JOBS_TO_KEEP env variable' do
+      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(-1)
+      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(0)
+      expect(@keep.id).to eq(Delayed::Job.order(:failed_at)[0].id)
+    end
+    
+      describe '#sort_tuples!' do
+    let(:tuples) {
+      time = Time.now
+      [
+        [2, 'a', time - 1],  # 0
+        [2, 'b', time - 1],  # 1
+        [1, 'b', time - 1],  # 2
+        [1, 'b', time],      # 3
+        [1, 'a', time],      # 4
+        [2, 'a', time + 1],  # 5
+        [2, 'a', time],      # 6
+      ]
+    }
+    
+        def definitions_for(klass)
+      parent_classes = klass.ancestors.reverse
+      parent_classes.each_with_object({}) do |ancestor, inherited_definitions|
+        inherited_definitions.deep_merge! @attachments[ancestor]
+      end
+    end
   end
 end
 
     
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+        # Returns a String describing the file's content type
+    def detect
+      if blank_name?
+        SENSIBLE_DEFAULT
+      elsif empty_file?
+        EMPTY_TYPE
+      elsif calculated_type_matches.any?
+        calculated_type_matches.first
+      else
+        type_from_file_contents || SENSIBLE_DEFAULT
+      end.to_s
     end
-    
-        it 'optionally supports treating values that start with '$' as raw JSONPath' do
-      expect(Utils.interpolate_jsonpaths('$.there.world', payload)).to eq('$.there.world')
-      expect(Utils.interpolate_jsonpaths('$.there.world', payload, :leading_dollarsign_is_jsonpath => true)).to eq('WORLD')
-    end
-  end
-    
-      describe 'down' do
-    let :valid_options do
-      super().merge('extract' => new_extract,
-                    'template' => new_template)
-    end
-    
-      describe '#check' do
-    it 'should not emit events on its first run' do
-      expect { @checker.check }.to change { Event.count }.by(0)
-      expect(@checker.memory[:last_event]).to eq '2014-04-17T10:25:31.000+02:00'
-    end
-    it 'should check that initial run creates an event' do
-      @checker.memory[:last_event] = '2014-04-17T10:25:31.000+02:00'
-      expect { @checker.check }.to change { Event.count }.by(1)
-    end
-  end
-    
-        it 'should raise error when response says unauthorized' do
-      stub(HTTParty).post { {'Response' => 'Not authorized'} }
-      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Not authorized/)
-    end
-    
-        if successfully_sent?(resource)
-      respond_with({}, location: after_sending_unlock_instructions_path_for(resource))
-    else
-      respond_with(resource)
-    end
-  end
-    
-          def parse_uri(location)
-        location && URI.parse(location)
-      rescue URI::InvalidURIError
-        nil
-      end
-    
-    module Devise
-  module Models
-    # Timeoutable takes care of verifying whether a user session has already
-    # expired or not. When a session expires after the configured time, the user
-    # will be asked for credentials again, it means, they will be redirected
-    # to the sign in page.
-    #
-    # == Options
-    #
-    # Timeoutable adds the following options to devise_for:
-    #
-    #   * +timeout_in+: the interval to timeout the user session without activity.
-    #
-    # == Examples
-    #
-    #   user.timedout?(30.minutes.ago)
-    #
-    module Timeoutable
-      extend ActiveSupport::Concern
-    
-        it 'returns false if the environment does not contain :userpaths' do
-      expect(env).not_to use_userpaths
-    end
-  end
