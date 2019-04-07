33e10ed1632f167d7f810c6b771c648eeaf03a0a
SUNsung <weatherius@gmail.com>
2019-04-08 00:41:43 +0300

    Rewrite [36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py] File in: 2019-04-08 00:41:43

--- pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
+++ pyton/36A27QymTRMO236gH5ObwlJME36rmcWSRkahoE36Kel.py
@@ -1,175 +1,154 @@
 
         
-          print(header)
+        
+    {    # Options that need a file parameter
+    'download-archive': ['--require-parameter'],
+    'cookies': ['--require-parameter'],
+    'load-info': ['--require-parameter'],
+    'batch-file': ['--require-parameter'],
+}
     
-        # Define the loss function and the optimization algorithm
-    self.cross_entropies = tf.nn.sparse_softmax_cross_entropy_with_logits(
-        logits=self.scores, labels=tf.reduce_mean(self.batch_labels))
-    self.cost = tf.reduce_sum(self.cross_entropies, name='cost')
-    self.global_step = tf.Variable(0, name='global_step', trainable=False)
-    self.optimizer = tf.train.AdamOptimizer()
-    self.train_op = self.optimizer.minimize(self.cost,
-                                            global_step=self.global_step)
+        with io.open(infile, encoding='utf-8') as inf:
+        issue_template_tmpl = inf.read()
     
-      Returns:
-    datasets: a dataset dictionary, with one name->data dictionary pair for
-    each dataset file.
-  '''
-  print('Reading data from ', data_dir)
-  datasets = utils.read_datasets(data_dir, data_filename_stem)
-  for k, data_dict in datasets.items():
-    datasets[k] = clean_data_dict(data_dict)
+        outfile, = args
     
-    model = IntegrationToBoundModel(N)
-inputs_ph_t = [tf.placeholder(tf.float32,
-                              shape=[None, 1]) for _ in range(ntimesteps)]
-state = tf.zeros([batch_size, N])
-saver = tf.train.Saver()
+        readme = re.sub(r'(?s)^.*?(?=# DESCRIPTION)', '', readme)
+    readme = re.sub(r'\s+youtube-dl \[OPTIONS\] URL \[URL\.\.\.\]', '', readme)
+    readme = PREFIX + readme
     
-      Args:
-    list_of_lists: List of lists.
+    # The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+from youtube_dl.version import __version__
+version = __version__
+# The full version, including alpha/beta/rc tags.
+release = version
     
-    '''Generate samples from the MaskGAN.
+        def test_cbc_encrypt(self):
+        data = bytes_to_intlist(self.secret_msg)
+        encrypted = intlist_to_bytes(aes_cbc_encrypt(data, self.key, self.iv))
+        self.assertEqual(
+            encrypted,
+            b'\x97\x92+\xe5\x0b\xc3\x18\x91ky9m&\xb3\xb5@\xe6'\xc2\x96.\xc8u\x88\xab9-[\x9e|\xf1\xcd')
     
-      # Averages for real and fake token values.
-  real = tf.mul(values, present)
-  fake = tf.mul(values, missing)
-  real_avg = tf.reduce_sum(real) / real_count
-  fake_avg = tf.reduce_sum(fake) / fake_count
+        fpath = os.path.join(path, 'test_batch')
+    x_test, y_test = load_batch(fpath)
     
-        ## Calculate the Advantages, A(s,a) = Q(s,a) - \hat{V}(s).
-    advantages = []
-    for t in xrange(FLAGS.sequence_length):
-      log_probability = log_probs_list[t]
-      cum_advantage = tf.zeros(shape=[FLAGS.batch_size / 2])
+        # Deduplicate output names to handle Siamese networks.
+    occurrences = {}
+    for n in model.output_names:
+        if n not in occurrences:
+            occurrences[n] = 1
+        else:
+            occurrences[n] += 1
+    conflict_counter = {n: 0 for n, count in occurrences.items() if count > 1}
+    output_names = []
+    for n in model.output_names:
+        if n in conflict_counter:
+            conflict_counter[n] += 1
+            n += '_%d' % conflict_counter[n]
+        output_names.append(n)
     
-      # *Total* number of n-grams produced by the generator.
-  total_ngrams_produced = 0
-    
-      # Standard variable mappings.
-  variable_mapping = {
-      'gen/encoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
-          encoder_lstm_w_0,
-      'gen/encoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
-          encoder_lstm_b_0,
-      'gen/encoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
-          encoder_lstm_w_1,
-      'gen/encoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
-          encoder_lstm_b_1,
-      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/kernel':
-          decoder_lstm_w_0,
-      'gen/decoder/rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias':
-          decoder_lstm_b_0,
-      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/kernel':
-          decoder_lstm_w_1,
-      'gen/decoder/rnn/multi_rnn_cell/cell_1/basic_lstm_cell/bias':
-          decoder_lstm_b_1
-  }
-    
-      Returns:
-    A decoder function with the required interface of `dynamic_rnn_decoder`
-    intended for inference.
-  '''
-  with tf.name_scope(name, 'attention_decoder_fn_inference', [
-      output_fn, encoder_state, attention_keys, attention_values,
-      attention_score_fn, attention_construct_fn, embeddings,
-      start_of_sequence_id, end_of_sequence_id, maximum_length,
-      num_decoder_symbols, dtype
-  ]):
-    start_of_sequence_id = tf.convert_to_tensor(start_of_sequence_id, dtype)
-    end_of_sequence_id = tf.convert_to_tensor(end_of_sequence_id, dtype)
-    maximum_length = tf.convert_to_tensor(maximum_length, dtype)
-    num_decoder_symbols = tf.convert_to_tensor(num_decoder_symbols, dtype)
-    encoder_info = tf.contrib.framework.nest.flatten(encoder_state)[0]
-    batch_size = encoder_info.get_shape()[0].value
-    if output_fn is None:
-      output_fn = lambda x: x
-    if batch_size is None:
-      batch_size = tf.shape(encoder_info)[0]
-    
-          # Prediction is linear output for Discriminator.
-      predictions = tf.contrib.layers.linear(outputs, 1, scope=vs)
-    
-    First, we fix a training set and increase the number of
-samples. Then we plot the computation time as function of
-the number of samples.
-    
-    plt.figure()
-for dataset_name in datasets:
-    # loading and vectorization
-    print('loading data')
-    if dataset_name in ['http', 'smtp', 'SA', 'SF']:
-        dataset = fetch_kddcup99(subset=dataset_name, percent10=True,
-                                 random_state=random_state)
-        X = dataset.data
-        y = dataset.target
-    
-        plt.subplots_adjust(hspace=.30)
-    
-    
-def benchmark_influence(conf):
-    '''
-    Benchmark influence of :changing_param: on both MSE and latency.
-    '''
-    prediction_times = []
-    prediction_powers = []
-    complexities = []
-    for param_value in conf['changing_param_values']:
-        conf['tuned_params'][conf['changing_param']] = param_value
-        estimator = conf['estimator'](**conf['tuned_params'])
-        print('Benchmarking %s' % estimator)
-        estimator.fit(conf['data']['X_train'], conf['data']['y_train'])
-        conf['postfit_hook'](estimator)
-        complexity = conf['complexity_computer'](estimator)
-        complexities.append(complexity)
-        start_time = time.time()
-        for _ in range(conf['n_samples']):
-            y_pred = estimator.predict(conf['data']['X_test'])
-        elapsed_time = (time.time() - start_time) / float(conf['n_samples'])
-        prediction_times.append(elapsed_time)
-        pred_score = conf['prediction_performance_computer'](
-            conf['data']['y_test'], y_pred)
-        prediction_powers.append(pred_score)
-        print('Complexity: %d | %s: %.4f | Pred. Time: %fs\n' % (
-            complexity, conf['prediction_performance_label'], pred_score,
-            elapsed_time))
-    return prediction_powers, prediction_times, complexities
-    
-    plt.matshow(data, cmap=plt.cm.Blues)
-plt.title('Original dataset')
-    
-    X = list()
-y = list()
-for i, (phi, a) in enumerate([(.5, .15), (.5, .6), (.3, .2)]):
-    for _ in range(30):
-        phase_noise = .01 * np.random.normal()
-        amplitude_noise = .04 * np.random.normal()
-        additional_noise = 1 - 2 * np.random.rand(n_features)
-        # Make the noise sparse
-        additional_noise[np.abs(additional_noise) < .997] = 0
-    
-        X, y = dataset
-    
-            while self.values[new_key] is not None and self.values[new_key] != key:
-            new_key = self.__hash_double_function(key, data, i) if \
-                self.balanced_factor() >= self.lim_charge else None
-            if new_key is None: break 
-            else: i += 1
-    
-        def _colision_resolution(self, key, data=None):
-        i = 1
-        new_key = self.hash_function(key + i*i)
-    
-    '''
-* Wondering how this method works !
-* It's pretty simple.
-* Let's say you need to calculate a ^ b
-* RULE 1 : a ^ b = (a*a) ^ (b/2) ---- example : 4 ^ 4 = (4*4) ^ (4/2) = 16 ^ 2
-* RULE 2 : IF b is ODD, then ---- a ^ b = a * (a ^ (b - 1)) :: where (b - 1) is even.
-* Once b is even, repeat the process to get a ^ b
-* Repeat the process till b = 1 OR b = 0, because a^1 = a AND a^0 = 1
-*
-* As far as the modulo is concerned,
-* the fact : (a*b) % c = ((a%c) * (b%c)) % c
-* Now apply RULE 1 OR 2 whichever is required.
+    This example has modular design. The encoder, decoder and autoencoder
+are 3 models that share weights. For example, after training the
+autoencoder, the encoder can be used to  generate latent vectors
+of input data for low-dim visualization like PCA or TSNE.
 '''
+    
+    x_train = x_train.reshape(x_train.shape[0], -1, 1)
+x_test = x_test.reshape(x_test.shape[0], -1, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+    
+    # transfer: train dense layers for new classification task [5..9]
+train_model(model,
+            (x_train_gte5, y_train_gte5),
+            (x_test_gte5, y_test_gte5), num_classes)
+
+    
+    
+def categorical_hinge(y_true, y_pred):
+    pos = K.sum(y_true * y_pred, axis=-1)
+    neg = K.max((1. - y_true) * y_pred, axis=-1)
+    return K.maximum(0., neg - pos + 1.)
+    
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
+    
+    
+class OutOfData(UnpackException):
+    pass
+    
+    
+def check(src, should, use_list=0):
+    assert unpackb(src, use_list=use_list) == should
+    
+        def __init__(self, lang_filter=None):
+        super(MultiByteCharSetProber, self).__init__(lang_filter=lang_filter)
+        self.distribution_analyzer = None
+        self.coding_sm = None
+        self._last_char = [0, 0]
+    
+    # UTF-8
+    
+    
+class SingleByteCharSetProber(CharSetProber):
+    SAMPLE_SIZE = 64
+    SB_ENOUGH_REL_THRESHOLD = 1024  #  0.25 * SAMPLE_SIZE^2
+    POSITIVE_SHORTCUT_THRESHOLD = 0.95
+    NEGATIVE_SHORTCUT_THRESHOLD = 0.05
+    
+    
+class SJISProber(MultiByteCharSetProber):
+    def __init__(self):
+        super(SJISProber, self).__init__()
+        self.coding_sm = CodingStateMachine(SJIS_SM_MODEL)
+        self.distribution_analyzer = SJISDistributionAnalysis()
+        self.context_analyzer = SJISContextAnalysis()
+        self.reset()
+    
+        @property
+    def language(self):
+        return 'Japanese'
+    
+        def get_confidence(self):
+        unlike = 0.99
+        if self._num_mb_chars < 6:
+            unlike *= self.ONE_CHAR_PROB ** self._num_mb_chars
+            return 1.0 - unlike
+        else:
+            return unlike
+
+    
+        description='A complete wechat personal account api',
+    long_description=long_description,
