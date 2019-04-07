3757b4484d1931cd0997d8ecade666b327addf6d
SUNsung <weatherius@gmail.com>
2019-04-07 04:03:26 +0300

    Rewrite [14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb] File in: 2019-04-07 04:03:26

--- ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
+++ ruby/14qvlHO6uFaq.14JG5zSsvxydc14.eGaVQxP0v.14NJ4.rb
@@ -1,51 +1,157 @@
 
         
-                class << self
-          def field_type
-            @field_type ||= name.split('::').last.sub('Field', '').downcase
-          end
+                  relation.update_all(update)
         end
-    
-        if resource.errors.empty?
-      set_flash_message!(:notice, :confirmed)
-      respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
-    else
-      respond_with_navigational(resource.errors, status: :unprocessable_entity){ render :new }
+      end
     end
-  end
-    
-      def app
-    Rails.application
   end
 end
 
     
-          module ClassMethods
-        # Create the cookie key using the record id and remember_token
-        def serialize_into_cookie(record)
-          [record.to_key, record.rememberable_value, Time.now.utc.to_f.to_s]
+            def representation_class
+          Representation::LfsObject
         end
     
-          def error_message_on(object, method, _options = {})
-        object = convert_to_model(object)
-        obj = object.respond_to?(:errors) ? object : instance_variable_get('@#{object}')
+              hash[:state] = hash[:state].to_sym
+          hash[:assignees].map! { |u| Representation::User.from_json_hash(u) }
+          hash[:author] &&= Representation::User.from_json_hash(hash[:author])
     
-            options[:class] = 'sidebar-menu-item d-block w-100'
-        options[:class] << ' selected' if is_selected
-        content_tag(:li, options) do
-          link_to(link_text, url, class: '#{'text-success' if is_selected} py-1 px-3 d-block sidebar-submenu-item')
+          def stub(path, &block)
+        @stubs[path] = block
+        @stubs
+      end
+    end
+    
+      preflight do
+    processes = system_command '/bin/launchctl', args: ['list']
+    
+            private
+    
+    task :add_template, [:name] do |t, args|
+  Dir.glob('README.*') do |file|
+    code = File.read(file)
+    if code =~ /^===.*#{args.name.capitalize}/
+      puts 'Already covered in #{file}'
+    else
+      template = code[/===[^\n]*Liquid.*index\.liquid<\/tt>[^\n]*/m]
+      if !template
+        puts 'Liquid not found in #{file}'
+      else
+        puts 'Adding section to #{file}'
+        template = template.gsub(/Liquid/, args.name.capitalize).gsub(/liquid/, args.name.downcase)
+        code.gsub! /^(\s*===.*CoffeeScript)/, '\n' << template << '\n\\1'
+        File.open(file, 'w') { |f| f << code }
+      end
+    end
+  end
+end
+    
+    
+    
+        # we assume that the first file that requires 'sinatra' is the
+    # app_file. all other path related options are calculated based
+    # on this path by default.
+    set :app_file, caller_files.first || $0
+    
+      table td.code       {width:750px}
+  table td.code div   {width:750px;overflow:hidden}
+</style>
+</head>
+<body>
+  <div id='wrap'>
+    <div id='header'>
+      <img src='<%= env['SCRIPT_NAME'] %>/__sinatra__/500.png' alt='application error' height='161' width='313' />
+      <div id='summary'>
+        <h1><strong><%=h exception.class %></strong> at <strong><%=h path %>
+          </strong></h1>
+        <h2><%=h exception.message %></h2>
+        <ul>
+          <li class='first'><strong>file:</strong> <code>
+            <%=h frames.first.filename.split('/').last %></code></li>
+          <li><strong>location:</strong> <code><%=h frames.first.function %>
+            </code></li>
+          <li class='last'><strong>line:
+            </strong> <%=h frames.first.lineno %></li>
+        </ul>
+      </div>
+      <div class='clear'></div>
+    </div>
+    
+              compare_with_real_token token, session
+    
+      it 'should not override the header if already set' do
+    mock_app with_headers('Content-Security-Policy' => 'default-src: none')
+    expect(get('/', {}, 'wants' => 'text/html').headers['Content-Security-Policy']).to eq('default-src: none')
+  end
+end
+
+    
+    ENV['GEM_HOME'] = ENV['GEM_PATH'] = LogStash::Environment.logstash_gem_home
+Gem.use_paths(LogStash::Environment.logstash_gem_home)
+    
+          packet_gem.pack
+    
+          PluginManager.ui.info('Installing file: #{local_file}')
+      uncompressed_path = uncompress(local_file)
+      PluginManager.ui.debug('Pack uncompressed to #{uncompressed_path}')
+      pack = LogStash::PluginManager::PackInstaller::Pack.new(uncompressed_path)
+      raise PluginManager::InvalidPackError, 'The pack must contains at least one plugin' unless pack.valid?
+    
+      def validate_cache_location
+    cache_location = LogStash::Environment::CACHE_PATH
+    if File.exist?(cache_location)
+      puts('Directory #{cache_location} is going to be overwritten, do you want to continue? (Y/N)')
+      override = ( 'y' == STDIN.gets.strip.downcase ? true : false)
+      if override
+        FileUtils.rm_rf(cache_location)
+      else
+        puts('Unpack cancelled: file #{cache_location} already exists, please delete or move it')
+        exit
+      end
+    end
+  end
+end
+
+    
+      parameter '[PLUGIN] ...', 'Plugin name(s) to upgrade to latest version', :attribute_name => :plugins_arg
+  option '--[no-]verify', :flag, 'verify plugin validity before installation', :default => true
+  option '--local', :flag, 'force local-only plugin update. see bin/logstash-plugin package|unpack', :default => false
+    
+              it 'allow to install a specific version' do
+            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify --version 0.1.0 logstash-filter-qatest')
+            expect(command).to install_successfully
+            expect(logstash).to have_installed?('logstash-filter-qatest', '0.1.0')
+          end
         end
       end
     
-      describe 'listing orders' do
-    it 'lists existing orders' do
-      within_row(1) do
-        expect(column_text(2)).to eq 'R100'
-        expect(find('td:nth-child(3)')).to have_css '.badge-considered_risky'
-        expect(column_text(4)).to eq 'cart'
-      end
     
-          page.find('#option_type_option_values_attributes_0_name').set('color')
-      page.find('#option_type_option_values_attributes_0_presentation').set('black')
-      click_button 'Update'
-      expect(page).to have_content('successfully updated!')
+===============================================
+ Error for category_generator.rb plugin
+-----------------------------------------------
+ No 'category_index.html' in source/_layouts/
+ Perhaps you haven't installed a theme yet.
+===============================================
+    
+        def render(context)
+      if parts = @text.match(/([a-zA-Z\d]*) (.*)/)
+        gist, file = parts[1].strip, parts[2].strip
+      else
+        gist, file = @text.strip, ''
+      end
+      if gist.empty?
+        ''
+      else
+        script_url = script_url_for gist, file
+        code       = get_cached_gist(gist, file) || get_gist_from_web(gist, file)
+        html_output_for script_url, code
+      end
+    end
+    
+          super
+    end
+    
+      # Checks for excerpts (helpful for template conditionals)
+  def has_excerpt(input)
+    input =~ /<!--\s*more\s*-->/i ? true : false
+  end
