983160ba7de9a8ac9e21eb6164b692012ba6f4c0
SUNsung <weatherius@gmail.com>
2019-04-07 17:35:47 +0300

    Rewrite [05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb] File in: 2019-04-07 17:35:47

--- ruby/05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb
+++ ruby/05MTOjAlE1UnQ05jNTilX8Tf8605eUsAeskfWdE05t4c.rb
@@ -1,173 +1,132 @@
 
         
-                    @template_object.label(@object_name, @sanitized_attribute_name, @text, html_options, &block)
+            it 'allows to delete a user' do
+      visit admin_users_path
+      find(:css, 'a[href='/admin/users/#{users(:bob).id}']').click
+      expect(page).to have_text('User 'bob' was deleted.')
+      expect(page).to have_no_text('bob@example.com')
+    end
+    
+        it 'does not output links to other agents outside of the incoming set' do
+      Link.create!(:source_id => agents(:jane_weather_agent).id, :receiver_id => agents(:jane_website_agent).id)
+      Link.create!(:source_id => agents(:jane_website_agent).id, :receiver_id => agents(:jane_rain_notifier_agent).id)
+    
+        it 'should raise an exception when encountering complex JSONPaths' do
+      @agent.options['username_path'] = '$.very.complex[*]'
+      expect { LiquidMigrator.convert_all_agent_options(@agent) }.
+        to raise_error('JSONPath '$.very.complex[*]' is too complex, please check your migration.')
+    end
+    
+      let :new_extract do
+    {
+      'url' => { 'css' => '#comic img', 'value' => '@src' },
+      'title' => { 'css' => '#comic img', 'value' => '@alt' },
+      'hovertext' => { 'css' => '#comic img', 'value' => '@title', 'hidden' => true }
+    }
+  end
+    
+      # Update version.rb file with BOOTSTRAP_SHA
+  def store_version
+    path    = 'lib/bootstrap-sass/version.rb'
+    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
+    File.open(path, 'w') { |f| f.write(content) }
+  end
+end
+
+    
+        def pos=(i)
+      @s.pos = str_to_byte_pos i
+      i
+    end
+    
+      # Use a different cache store in production.
+  # config.cache_store = :mem_cache_store
+    
+      def test_image_helper
+    assert_match %r(url\(['']?/assets/apple-touch-icon-144-precomposed.*png['']?\)), @css
+  end
+    
+          # Calls the given block for each `key` node in the `hash` literal.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_key
+        return pairs.map(&:key).to_enum unless block_given?
+    
+          # Custom destructuring method. This is used to normalize the branches
+      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
+      #
+      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
+      def node_parts
+        [self, self]
+      end
+    end
+  end
+end
+
+    
+          # Checks whether this `hash` element is on the same line as `other`.
+      #
+      # @note A multiline element is considered to be on the same line if it
+      #       shares any of its lines with `other`
+      #
+      # @return [Boolean] whether this element is on the same line as `other`
+      def same_line?(other)
+        loc.last_line == other.loc.line || loc.line == other.loc.last_line
+      end
+    
+          # A shorthand for getting the first argument of the node.
+      # Equivalent to `arguments.first`.
+      #
+      # @return [Node, nil] the first argument of the node,
+      #                     or `nil` if there are no arguments
+      def first_argument
+        arguments[0]
+      end
+    
+            if obj && obj.errors[method].present?
+          errors = safe_join(obj.errors[method], '<br />'.html_safe)
+          content_tag(:span, errors, class: 'formError')
+        else
+          ''
+        end
+      end
+    
+          within('#sidebar') { click_link 'Variants' }
+      expect(page).to have_content('To add variants, you must first define')
+    end
+    
+              def spree_current_order
+            @spree_current_order ||= find_spree_current_order
+          end
+    
+            def create
+          authorize! :create, Product
+          params[:product][:available_on] ||= Time.current
+          set_up_shipping_category
+    
+            def scope
+          if params[:country_id]
+            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
+            @country.states.accessible_by(current_ability, :read).order('name ASC')
+          else
+            State.accessible_by(current_ability, :read).order('name ASC')
           end
         end
-    
-              def datetime_selector(options, html_options)
-            datetime = options.fetch(:selected) { value || default_datetime(options) }
-            @auto_index ||= nil
-    
-        %w(<< concat push insert unshift).each do |method|
-      class_eval <<-METHOD, __FILE__, __LINE__ + 1
-        def #{method}(*args)
-          paths.#{method}(*typecast(args))
-        end
-      METHOD
-    end
-    
-    User.seed do |u|
-  u.id = -1
-  u.name = 'system'
-  u.username = 'system'
-  u.username_lower = 'system'
-  u.password = SecureRandom.hex
-  u.active = true
-  u.admin = true
-  u.moderator = true
-  u.approved = true
-  u.trust_level = TrustLevel[4]
-end
-    
-            # Reset topic count because we don't count the description topic
-        DB.exec 'UPDATE categories SET topic_count = 0 WHERE id = #{staff.id}'
       end
     end
   end
 end
 
     
-              relation.update_all(update)
+          def deliver(msg)
+        if msg.respond_to?(:deliver_now)
+          # Rails 4.2/5.0
+          msg.deliver_now
+        else
+          # Rails 3.2/4.0/4.1
+          msg.deliver
         end
       end
     end
-  end
-end
-
-    
-          # Reads a cache key.
-      #
-      # If the key exists and has a non-empty value its TTL is refreshed
-      # automatically.
-      #
-      # raw_key - The cache key to read.
-      # timeout - The new timeout of the key if the key is to be refreshed.
-      def self.read(raw_key, timeout: TIMEOUT)
-        key = cache_key_for(raw_key)
-        value = Redis::Cache.with { |redis| redis.get(key) }
-    
-            def find_target_id
-          GithubImport::IssuableFinder.new(project, issue).database_id
-        end
-      end
-    end
-  end
-end
-
-    
-            def collection_method
-          :lfs_objects
-        end
-    
-    module Gitlab
-  module GithubImport
-    # IssuableFinder can be used for caching and retrieving database IDs for
-    # issuable objects such as issues and pull requests. By caching these IDs we
-    # remove the need for running a lot of database queries when importing
-    # GitHub projects.
-    class IssuableFinder
-      attr_reader :project, :object
-    
-            # attributes - A Hash containing the raw note details. The keys of this
-        #              Hash must be Symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-    
-    module Gitlab
-  module GithubImport
-    module Representation
-      class Issue
-        include ToHash
-        include ExposeAttribute
-    
-            if controller.content_type == 'text/html'
-          action
-        else
-          '#{action} (#{controller.content_type})'
-        end
-      end
-    
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-          def mailer_sender(mapping, sender = :from)
-        default_sender = default_params[sender]
-        if default_sender.present?
-          default_sender.respond_to?(:to_proc) ? instance_eval(&default_sender) : default_sender
-        elsif Devise.mailer_sender.is_a?(Proc)
-          Devise.mailer_sender.call(mapping.name)
-        else
-          Devise.mailer_sender
-        end
-      end
-    
-              def #{accessor}=(value)
-            @#{accessor} = value
-          end
-        METHOD
-      end
-    end
-    
-    module Devise
-  module Models
-    # Timeoutable takes care of verifying whether a user session has already
-    # expired or not. When a session expires after the configured time, the user
-    # will be asked for credentials again, it means, they will be redirected
-    # to the sign in page.
-    #
-    # == Options
-    #
-    # Timeoutable adds the following options to devise_for:
-    #
-    #   * +timeout_in+: the interval to timeout the user session without activity.
-    #
-    # == Examples
-    #
-    #   user.timedout?(30.minutes.ago)
-    #
-    module Timeoutable
-      extend ActiveSupport::Concern
-    
-        # function
-    # Sass::Callable
-    inherited_hash_reader :function
-    
-            if e.is_a?(Sass::SyntaxError)
-          $stderr.puts e.sass_backtrace_str('standard input')
-        else
-          $stderr.print '#{e.class}: ' unless e.class == RuntimeError
-          $stderr.puts e.message.to_s
-        end
-        $stderr.puts '  Use --trace for backtrace.'
-    
-          opts.on(
-        '--indent NUM',
-        'How many spaces to use for each level of indentation. Defaults to 2.',
-        ''t' means use hard tabs.'
-      ) do |indent|
-        if indent == 't'
-          @options[:for_tree][:indent] = '\t'
-        else
-          @options[:for_tree][:indent] = ' ' * indent.to_i
-        end
-      end
-    
-    # This class is the parent of all packages.
-# If you want to implement an FPM package type, you'll inherit from this.
-class FPM::Package
-  include FPM::Util
-  include Cabin::Inspectable
