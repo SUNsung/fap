881303812dfa2115563e2f6a9972431b41227adf
SUNsung <weatherius@gmail.com>
2019-04-07 21:42:44 +0300

    Rewrite [5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb] File in: 2019-04-07 21:42:43

--- ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
+++ ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
@@ -1,322 +1,174 @@
 
         
-              before :each do
-        Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::BUILD_NUMBER] = build_number
-      end
+          it 'no errors without a user' do
+    expect(-> { GivenDailyLike.increment_for(nil) }).not_to raise_error
+    expect(-> { GivenDailyLike.decrement_for(nil) }).not_to raise_error
+  end
     
-            expect(result).to eq('carthage build --no-skip-current')
-      end
+    describe GroupUser do
     
-            it 'executes the correct git command' do
-          allow(Fastlane::Actions).to receive(:sh).with('git add #{path.shellescape}', anything).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: '#{path}')
+        def show_message
+      UI.message('Sending anonymous analytics information')
+      UI.message('Learn more at https://docs.fastlane.tools/#metrics')
+      UI.message('No personal or sensitive data is sent.')
+      UI.message('You can disable this by adding `opt_out_usage` at the top of your Fastfile')
+    end
+    
+          it 'sets the command to build' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            carthage(command: 'build')
           end').runner.execute(:test)
+    
+    # To avoid 'PR & Runs' for which tests don't pass, we want to make spec errors more visible
+# The code below will run on Circle, parses the results in JSON and posts them to the PR as comment
+containing_dir = ENV['CIRCLE_TEST_REPORTS'] || '.' # for local testing
+file_path = File.join(containing_dir, 'rspec', 'fastlane-junit-results.xml')
+    
+      #
+  # If there were CGI parameters in the URI, this will hold a hash of each
+  # variable to value.  If there is more than one value for a given variable,
+  # an array of each value is returned.
+  #
+  def qstring
+    self.uri_parts['QueryString']
+  end
+    
+      #
+  # Move these into an IPMI stack or mixin at some point
+  #
+    
+              # Encrypts the cipher using RC4-HMAC schema
+          #
+          # @param data [String] the data to encrypt
+          # @param key [String] the key to encrypt
+          # @param msg_type [Integer] the message type
+          # @return [String] the encrypted data
+          def encrypt_rc4_hmac(data, key, msg_type)
+            k1 = OpenSSL::HMAC.digest('MD5', key, [msg_type].pack('V'))
+    
+              # Encodes the authenticator field
+          #
+          # @return [String]
+          def encode_authenticator
+            authenticator.encode
+          end
         end
       end
+    end
+  end
+end
     
-            context 'with given path to oclint' do
-          let(:result) do
-            Fastlane::FastFile.new.parse('lane :test do
-              oclint(
-                compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
-                oclint_path: 'test/bin/oclint'
-              )
-            end').runner.execute(:test)
-          end
-          let(:command) { 'cd #{File.expand_path('.').shellescape} && test/bin/oclint -report-type=html -o=oclint_report.html' }
-    
-    # Windows implementation
-module WindowsShellwords
-  def shellescape(str)
-    str = str.to_s
-    
-    shellescape_testcases = [
-  # baseline
-  {
-    'it' => '(#1) on simple string',
-    'it_result' => {
-      'windows' => 'doesn't change it',
-      'other'   => 'doesn't change it'
-    },
-    'str' => 'normal_string_without_spaces',
-    'expect' => {
-      'windows' => 'normal_string_without_spaces',
-      'other'   => 'normal_string_without_spaces'
-    }
-  },
-  {
-    'it' => '(#2) on empty string',
-    'it_result' => {
-      'windows' => 'wraps it in double quotes',
-      'other'   => 'wraps it in single quotes'
-    },
-    'str' => '',
-    'expect' => {
-      'windows' => '''',
-      'other'   => '\'\''
-    }
-  },
-  # spaces
-  {
-    'it' => '(#3) on string with spaces',
-    'it_result' => {
-      'windows' => 'wraps it in double quotes',
-      'other'   => 'escapes spaces with <backslash>'
-    },
-    'str' => 'string with spaces',
-    'expect' => {
-      'windows' => ''string with spaces'',
-      'other'   => 'string\ with\ spaces'
-    }
-  },
-  # double quotes
-  {
-    'it' => '(#4) on simple string that is already wrapped in double quotes',
-    'it_result' => {
-      'windows' => 'doesn't touch it',
-      'other'   => 'escapes the double quotes with <backslash>'
-    },
-    'str' => ''normal_string_without_spaces'',
-    'expect' => {
-      'windows' => ''normal_string_without_spaces'',
-      'other'   => '\'normal_string_without_spaces\''
-    }
-  },
-  {
-    'it' => '(#5) on string with spaces that is already wrapped in double quotes',
-    'it_result' => {
-      'windows' => 'wraps in double quotes and duplicates existing double quotes',
-      'other'   => 'escapes the double quotes and spaces with <backslash>'
-    },
-    'str' => ''string with spaces already wrapped in double quotes'',
-    'expect' => {
-      'windows' => ''''string with spaces already wrapped in double quotes'''',
-      'other'   => '\'string\ with\ spaces\ already\ wrapped\ in\ double\ quotes\''
-    }
-  },
-  {
-    'it' => '(#6) on string with spaces and double quotes',
-    'it_result' => {
-      'windows' => 'wraps in double quotes and duplicates existing double quotes',
-      'other'   => 'escapes the double quotes and spaces with <backslash>'
-    },
-    'str' => 'string with spaces and 'double' quotes',
-    'expect' => {
-      'windows' => ''string with spaces and ''double'' quotes'',
-      'other'   => 'string\ with\ spaces\ and\ \'double\'\ quotes'
-    }
-  },
-  # https://github.com/ruby/ruby/blob/ac543abe91d7325ace7254f635f34e71e1faaf2e/test/test_shellwords.rb#L64-L65
-  {
-    'it' => '(#7) on simple int',
-    'it_result' => {
-      'windows' => 'doesn't change it',
-      'other'   => 'doesn't change it'
-    },
-    'str' => 3,
-    'expect' => {
-      'windows' => '3',
-      'other'   => '3'
-    }
-  },
-  # single quotes
-  {
-    'it' => '(#8) on simple string that is already wrapped in single quotes',
-    'it_result' => {
-      'windows' => 'doesn't touch it',
-      'other'   => 'escapes the single quotes with <backslash>'
-    },
-    'str' => ''normal_string_without_spaces'',
-    'expect' => {
-      'windows' => ''normal_string_without_spaces'',
-      'other'   => '\\'normal_string_without_spaces\\''
-    }
-  },
-  {
-    'it' => '(#9) on string with spaces that is already wrapped in single quotes',
-    'it_result' => {
-      'windows' => 'wraps in double quotes',
-      'other'   => 'escapes the single quotes and spaces with <backslash>'
-    },
-    'str' => ''string with spaces already wrapped in single quotes'',
-    'expect' => {
-      'windows' => '\''string with spaces already wrapped in single quotes'\'',
-      'other'   => '\\'string\\ with\\ spaces\\ already\\ wrapped\\ in\\ single\\ quotes\\''
-    }
-  },
-  {
-    'it' => '(#10) string with spaces and single quotes',
-    'it_result' => {
-      'windows' => 'wraps in double quotes and leaves single quotes',
-      'other'   => 'escapes the single quotes and spaces with <backslash>'
-    },
-    'str' => 'string with spaces and 'single' quotes',
-    'expect' => {
-      'windows' => '\'string with spaces and 'single' quotes\'',
-      'other'   => 'string\ with\ spaces\ and\ \\\'single\\\'\ quotes'
-    }
-  },
-  {
-    'it' => '(#11) string with spaces and <backslash>',
-    'it_result' => {
-      'windows' => 'wraps in double quotes and escapes the backslash with backslash',
-      'other'   => 'escapes the spaces and the backslash (which in results in quite a lot of them)'
-    },
-    'str' => 'string with spaces and \\ in it',
-    'expect' => {
-      'windows' => '\'string with spaces and \\ in it\'',
-      'other'   => 'string\\ with\\ spaces\\ and\\ \\\\\\ in\\ it'
-    }
-  },
-  {
-    'it' => '(#12) string with spaces and <slash>',
-    'it_result' => {
-      'windows' => 'wraps in double quotes',
-      'other'   => 'escapes the spaces'
-    },
-    'str' => 'string with spaces and / in it',
-    'expect' => {
-      'windows' =>  '\'string with spaces and / in it\'',
-      'other'   => 'string\\ with\\ spaces\\ and\\ /\\ in\\ it'
-    }
-  },
-  {
-    'it' => '(#13) string with spaces and parens',
-    'it_result' => {
-      'windows' => 'wraps in double quotes',
-      'other'   => 'escapes the spaces and parens'
-    },
-    'str' => 'string with spaces and (parens) in it',
-    'expect' => {
-      'windows' => '\'string with spaces and (parens) in it\'',
-      'other'   => 'string\\ with\\ spaces\\ and\\ \\(parens\\)\\ in\\ it'
-    }
-  },
-  {
-    'it' => '(#14) string with spaces, single quotes and parens',
-    'it_result' => {
-      'windows' => 'wraps in double quotes',
-      'other'   => 'escapes the spaces, single quotes and parens'
-    },
-    'str' => 'string with spaces and 'quotes' (and parens) in it',
-    'expect' => {
-      'windows' => '\'string with spaces and 'quotes' (and parens) in it\'',
-      'other'   => 'string\\ with\\ spaces\\ and\\ \\'quotes\\'\\ \\(and\\ parens\\)\\ in\\ it'
-    }
-  }
-]
-    
-              # Handle the case that argv was empty or didn't contain any subcommand
-          main_args = argv.dup if main_args.nil?
-    
-              # Set all of our instance variables on the new class
-          [self, other].each do |obj|
-            obj.instance_variables.each do |key|
-              # Ignore keys that start with a double underscore. This allows
-              # configuration classes to still hold around internal state
-              # that isn't propagated.
-              if !key.to_s.start_with?('@__')
-                result.instance_variable_set(key, obj.instance_variable_get(key))
+                seq_values.each do |val|
+              case val.tag
+              when 0
+                self.options = decode_options(val)
+              when 1
+                self.cname = decode_cname(val)
+              when 2
+                self.realm = decode_realm(val)
+              when 3
+                self.sname = decode_sname(val)
+              when 4
+                self.from = decode_from(val)
+              when 5
+                self.till = decode_till(val)
+              when 6
+                self.rtime = decode_rtime(val)
+              when 7
+                self.nonce = decode_nonce(val)
+              when 8
+                self.etype = decode_etype(val)
+              when 10
+                self.enc_auth_data = decode_enc_auth_data(val)
+              else
+                raise ::RuntimeError, 'Failed to decode KdcRequestBody SEQUENCE'
               end
             end
           end
     
-            # This registers a plugin. This should _NEVER_ be called by the public
-        # and should only be called from within Vagrant. Vagrant will
-        # automatically register V1 plugins when a name is set on the
-        # plugin.
-        def register(plugin)
-          if !@registered.include?(plugin)
-            @logger.info('Registered plugin: #{plugin.name}')
-            @registered << plugin
-          end
-        end
-    
-            # Registers additional provisioners to be available.
-        #
-        # @param [String] name Name of the provisioner.
-        def self.provisioner(name=UNSET_VALUE, &block)
-          data[:provisioners] ||= Registry.new
-    
-            # Helper method that will set a value if a value is given, or otherwise
-        # return the already set value.
-        #
-        # @param [Symbol] key Key for the data
-        # @param [Object] value Value to store.
-        # @return [Object] Stored value.
-        def self.get_or_set(key, value=UNSET_VALUE)
-          # If no value is to be set, then return the value we have already set
-          return data[key] if value.eql?(UNSET_VALUE)
-    
-        # Merge one registry with another and return a completely new
-    # registry. Note that the result cache is completely busted, so
-    # any gets on the new registry will result in a cache miss.
-    def merge(other)
-      self.class.new.tap do |result|
-        result.merge!(self)
-        result.merge!(other)
-      end
-    end
-    
-    $stderr.puts <<DEPRECATION
-WARNING: Ruby Sass's Git repository is moving, and the old repository will be
-deled on 26 March 2019! Please update your Git URLs to point to the new
-repository at https://github.com/sass/ruby-sass.
-    
-        class << self
-      # Returns an error report for an exception in CSS format.
+          # A shorthand for getting the last argument of the node.
+      # Equivalent to `arguments.last`.
       #
-      # @param e [Exception]
-      # @param line_offset [Integer] The number of the first line of the Sass template.
-      # @return [String] The error report
-      # @raise [Exception] `e`, if the
-      #   {file:SASS_REFERENCE.md#full_exception-option `:full_exception`} option
-      #   is set to false.
-      def exception_to_css(e, line_offset = 1)
-        header = header_string(e, line_offset)
-    
-        # Prints a status message about performing the given action,
-    # colored using the given color (via terminal escapes) if possible.
-    #
-    # @param name [#to_s] A short name for the action being performed.
-    #   Shouldn't be longer than 11 characters.
-    # @param color [Symbol] The name of the color to use for this action.
-    #   Can be `:red`, `:green`, or `:yellow`.
-    def puts_action(name, color, arg)
-      return if @options[:for_engine][:quiet]
-      printf color(color, '%11s %s\n'), name, arg
-      STDOUT.flush
-    end
-    
-          if !@options[:update] && !@options[:watch] &&
-          @args.first && colon_path?(@args.first)
-        if @args.size == 1
-          @args = split_colon_path(@args.first)
-        else
-          @fake_update = true
-          @options[:update] = true
-        end
+      # @return [Node, nil] the last argument of the node,
+      #                     or `nil` if there are no arguments
+      def last_argument
+        arguments[-1]
       end
-      load_compass if @options[:compass]
-      return interactive if @options[:interactive]
-      return watch_or_update if @options[:watch] || @options[:update]
-      super
     
-      def xcode_app_path
-    File.expand_path('../..', developer_prefix)
+            self
+      end
+    
+          def page_name
+        @name.gsub('-', ' ')
+      end
+    
+      teardown do
+    FileUtils.rm_r(File.join(File.dirname(__FILE__), *%w[examples test.git]))
   end
     
-          attr_reader :page, :content
+        @wiki.clear_cache
+    assert_nil @wiki.paged('H', 'G')
+    page_2 = @wiki.paged('C', 'G/K')
+    assert_equal 'INITIAL\n\nSPAM2\n', page_2.raw_data
+    assert_equal 'def', page_2.version.message
+    assert_not_equal page_1.version.sha, page_2.version.sha
+  end
     
-        # don't use h1 or it will be promoted to replace file name
-    # which doesn't generate a normal header link
-    @wiki.write_page(page, :markdown, '## ' + text,
-                     { :name => 'user1', :email => 'user1' });
+        def initialize(dir, existing, attempted, message = nil)
+      @dir            = dir
+      @existing_path  = existing
+      @attempted_path = attempted
+      super(message || 'Cannot write #{@dir}/#{@attempted_path}, found #{@dir}/#{@existing_path}.')
+    end
+  end
+end
     
-      test 'transliteration' do
-    # we transliterate only when adapter is grit
-    return if defined?(Gollum::GIT_ADAPTER) && Gollum::GIT_ADAPTER != 'grit'
+    __END__
     
-          ws  = WorkSpace.new(binding)
-      irb = Irb.new(ws)
+          def deliver(msg)
+        if msg.respond_to?(:deliver_now)
+          # Rails 4.2/5.0
+          msg.deliver_now
+        else
+          # Rails 3.2/4.0/4.1
+          msg.deliver
+        end
+      end
+    end
+    
+        return [osname, osversion, arch, wordsize].join(':')
+  end
+    
+        control_contents = File.read(pkginfo)
+    FileUtils.rm(pkginfo)
+    FileUtils.rm(mtree)
+    
+        return @architecture
+  end # def architecture
+    
+      # mknod is __xmknod in glibc a wrapper around mknod to handle
+  # various stat struct formats. See bits/stat.h in glibc source
+  begin
+    attach_function :mknod, :mknod, [:string, :uint, :ulong], :int
+  rescue FFI::NotFoundError
+    # glibc/io/xmknod.c int __xmknod (int vers, const char *path, mode_t mode, dev_t *dev)
+    attach_function :xmknod, :__xmknod, [:int, :string, :uint, :pointer], :int
+  end
+    
+        initialize_with { Tmuxinator::Project.new(file) }
+  end
+    
+        context 'with alias' do
+      it 'renders the tmux config' do
+        rendered = project_with_alias.render
+        expect(rendered).to_not be_empty
+        expect(rendered).to include('alias_is_working')
+      end
+    end
+  end
+    
+      describe '#home' do
+    it 'is ~/.tmuxinator' do
+      expect(described_class.home).to eq '#{ENV['HOME']}/.tmuxinator'
+    end
+  end
