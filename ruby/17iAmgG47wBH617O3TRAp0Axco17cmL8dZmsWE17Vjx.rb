a282e481bd5641a8be06e059787efe0b195cb78b
SUNsung <weatherius@gmail.com>
2019-04-07 19:29:19 +0300

    Rewrite [17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb] File in: 2019-04-07 19:29:19

--- ruby/17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb
+++ ruby/17iAmgG47wBH617O3TRAp0Axco17cmL8dZmsWE17Vjx.rb
@@ -1,174 +1,98 @@
 
         
-                MergeRequest
-          .where(id: start_id..stop_id)
-          .where(latest_merge_request_diff_id: nil)
-          .each_batch(of: BATCH_SIZE) do |relation|
+          it_behaves_like :object_equal, :equal?
     
-            def sidekiq_worker_class
-          ImportDiffNoteWorker
-        end
+        ScratchPad.recorded.should == [:singleton_method_added, :new_singleton_method]
+  end
     
-            def representation_class
-          Representation::Issue
-        end
+        def get_empty_binding
+      binding
+    end
     
-              lfs_objects.each do |object|
-            yield object
-          end
-        rescue StandardError => e
-          Rails.logger.error('The Lfs import process failed. #{e.message}')
-        end
-      end
+      # Update version.rb file with BOOTSTRAP_SHA
+  def store_version
+    path    = 'lib/bootstrap-sass/version.rb'
+    content = File.read(path).sub(/BOOTSTRAP_SHA\s*=\s*[''][\w]+['']/, 'BOOTSTRAP_SHA = '#@branch_sha'')
+    File.open(path, 'w') { |f| f.write(content) }
+  end
+end
+
+    
+        def pos
+      byte_to_str_pos @s.pos
+    end
+    
+        def silence_log
+      @silence = true
+      yield
+    ensure
+      @silence = false
     end
   end
 end
 
     
-            Caching.set_includes?(already_imported_cache_key, id)
-      end
-    
-    module Gitlab
-  module GithubImport
-    module Representation
-      class DiffNote
-        include ToHash
-        include ExposeAttribute
-    
-            # attributes - A Hash containing the raw PR details. The keys of this
-        #              Hash (and any nested hashes) must be symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-    
-    module Devise
-  module Controllers
-    # Provide sign in and sign out functionality.
-    # Included by default in all controllers.
-    module SignInOut
-      # Return true if the given scope is signed in session. If no scope given, return
-      # true if any scope is signed in. This will run authentication hooks, which may
-      # cause exceptions to be thrown from this method; if you simply want to check
-      # if a scope has already previously been authenticated without running
-      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
-      def signed_in?(scope=nil)
-        [scope || Devise.mappings.keys].flatten.any? do |_scope|
-          warden.authenticate?(scope: _scope)
-        end
-      end
-    
-    require 'uri'
-    
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
-        end
-      end
-    
-      uninstall_preflight do
-    system_command '#{HOMEBREW_PREFIX}/bin/brew', args: ['cask', 'uninstall', 'adobe-photoshop-lightroom600']
-  end
-    
-      # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
-    
-          attr_reader :key, :default, :options
-    
-        assert_no_match /New/, last_response.body, ''New' link not blocked in pages template'
-    
-      test 'extracting paths from URLs' do
-    assert_nil extract_path('Eye-Of-Sauron')
-    assert_equal 'Mordor', extract_path('Mordor/Sauron')
-    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
-  end
-    
-      # replace name version and date
-  replace_header(head, :name)
-  replace_header(head, :version)
-  replace_header(head, :date)
-  #comment this out if your rubyforge_project has a different name
-  replace_header(head, :rubyforge_project)
-    
-      s.add_development_dependency 'rack-test', '~> 0.6.2'
-  s.add_development_dependency 'shoulda', '~> 3.5.0'
-  s.add_development_dependency 'minitest-reporters', '~> 0.14.16'
-  s.add_development_dependency 'twitter_cldr', '~> 3.2.0'
-  s.add_development_dependency 'mocha', '~> 1.1.0'
-  s.add_development_dependency 'test-unit', '~> 3.1.0'
-  s.add_development_dependency 'webrick', '~> 1.3.1'
-    
-      class Error < StandardError;
-  end
-    
-            context 'with nested resources' do
-          before { get '/api/v2/storefront/products?include=option_types,option_types.option_values' }
-    
-        it 'return JSON API payload of User and associations (default billing and shipping address)' do
-      expect(json_response['data']).to have_id(user.id.to_s)
-      expect(json_response['data']).to have_type('user')
-      expect(json_response['data']).to have_relationships(:default_shipping_address, :default_billing_address)
-    
-          it_behaves_like 'returns 200 HTTP status'
-    
-            it 'returns proper links data' do
-          expect(json_response['links']['self']).to include('/api/v2/storefront/taxons')
-          expect(json_response['links']['next']).to include('/api/v2/storefront/taxons?page=1')
-          expect(json_response['links']['prev']).to include('/api/v2/storefront/taxons?page=1')
-        end
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
       end
     end
-  end
-    
-              def serialize_resource(resource)
-            resource_serializer.new(
-              resource,
-              include: resource_includes,
-              sparse_fields: sparse_fields
-            ).serializable_hash
-          end
-    
-    desc 'Creates a sandbox application for simulating the Spree code in a deployed Rails app'
-task :sandbox do
-  Bundler.with_clean_env do
-    exec('lib/sandbox.sh')
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
   end
 end
 
     
-      # Output a pkgbuild pkg.
-  def output(output_path)
-    output_check(output_path)
+    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
+  file_shared_path = TestApp.shared_path.join(file)
+  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
+  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
+end
     
-        if attributes[:pear_data_dir]
-      logger.info('Setting data_dir', :data_dir => attributes[:pear_data_dir])
-      safesystem('pear', '-c', config, 'config-set', 'data_dir', '#{staging_path(installroot)}/#{attributes[:pear_data_dir]}')
-    end
-    
-      def iteration
-    return @iteration ? @iteration : 1
-  end
-    
-        # Copy 'files' from builddir to :output/files
-    Find.find('files', 'manifests') do |path|
-      logger.info('Copying path: #{path}')
-      if File.directory?(path)
-        ::Dir.mkdir(File.join(params[:output], path))
-      else
-        FileUtils.cp(path, File.join(params[:output], path))
+          # The body of the method definition.
+      #
+      # @note this can be either a `begin` node, if the method body contains
+      #       multiple expressions, or any other node, if it contains a single
+      #       expression.
+      #
+      # @return [Node] the body of the method definition
+      def body
+        node_parts[0]
       end
-    end
-  end # def build!
     
-        # Unpack the tarball to the staging path
-    args = ['-cf', output_path, '-C', staging_path]
-    tar_compression_flag(output_path).tap do |flag|
-      args << flag unless flag.nil?
-    end
-    args << '.'
+        def each_mentioned_cop
+      each_directive do |comment, cop_names, disabled|
+        comment_line_number = comment.loc.expression.line
+        single_line = !comment_only_line?(comment_line_number)
+    
+        # Generate a package manifest.
+    pkg_generate = safesystemout('pkgsend', 'generate', '#{staging_path}')
+    File.write(build_path('#{name}.p5m.1'), pkg_generate)
+    
+      # The list of supported compression types. Default is xz (LZMA2)
+  COMPRESSION_TYPES = [ 'gz', 'bzip2', 'xz', 'none' ]
+    
+      private
+  def input(command)
+    platforms = [
+      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
+      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
+      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
+      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
+      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
+    ]
+    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
+                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
+                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
+                             'log_directory', 'log_file_stderr', 'log_file_stdout']
+    
+          def self.has_issues_with_add_symlink?
+        return !::Gem::Package::TarWriter.public_instance_methods.include?(:add_symlink)
+      end
+    end # module TarWriter
+  end # module Issues
+end # module FPM
