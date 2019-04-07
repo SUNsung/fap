fc6562d9df4c4f761286726b1239e38a5d437afd
SUNsung <weatherius@gmail.com>
2019-04-07 14:52:52 +0300

    Rewrite [54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb] File in: 2019-04-07 14:52:51

--- ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
+++ ruby/54QzmrZe8ypSE54dHbY0D7nKv254KuoodoOMYs54CNw.rb
@@ -1,131 +1,57 @@
 
         
-            it 'opens the dry run modal even when clicking on the refresh icon' do
-      visit edit_agent_path(agent)
-      find('.agent-dry-run-button span.glyphicon').click
-      expect(page).to have_text('Event to send (Optional)')
+          describe '#highlighted?' do
+    it 'understands hl=6-8' do
+      stub(params).[](:hl) { '6-8' }
+      expect((1..10).select { |i| highlighted?(i) }).to eq [6, 7, 8]
     end
     
-              @bar2 = Agents::DotBar.new(name: 'bar2').tap { |agent|
-            agent.user = users(:bob)
-            agent.sources << @foo
-            agent.propagate_immediately = true
-            agent.disabled = true
-            agent.save!
-          },
+    describe JobsHelper do
+  let(:job) { Delayed::Job.new }
     
-          describe '#generate_diff' do
-        it 'should check if the agent requires a service' do
-          agent_diffs = services_scenario_import.agent_diffs
-          basecamp_agent_diff = agent_diffs[0]
-          expect(basecamp_agent_diff.requires_service?).to eq(true)
-        end
+          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent).sort_by(&:id)).to eq([@agent1, @agent2])
+    end
     
-          it 'runs until stop is called' do
-        mock.instance_of(Rufus::Scheduler).join
-        Thread.new { while @agent_runner.instance_variable_get(:@running) != false do sleep 0.1; @agent_runner.stop end }
-        @agent_runner.run
+      context '#parse_duration' do
+    it 'works with correct arguments' do
+      expect(Utils.parse_duration('2.days')).to eq(2.days)
+      expect(Utils.parse_duration('2.seconds')).to eq(2)
+      expect(Utils.parse_duration('2')).to eq(2)
+    end
+    
+        @checker = Agents::BasecampAgent.new(:name => 'somename', :options => @valid_params)
+    @checker.service = services(:generic)
+    @checker.user = users(:jane)
+    @checker.save!
+    
+    class Ray
+  def initialize(org, dir)
+    @org = org
+    @dir = dir
+  end
+    
+        it 'lets a user destroy their like' do
+      current_user = controller.send(:current_user)
+      expect(current_user).to receive(:retract).with(@like)
+    
+    Given /^(?:|I )am on (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
+    
+        def definitions_for(klass)
+      parent_classes = klass.ancestors.reverse
+      parent_classes.each_with_object({}) do |ancestor, inherited_definitions|
+        inherited_definitions.deep_merge! @attachments[ancestor]
       end
-    
-      describe 'up' do
-    it 'should update extract and template options for an existing WebsiteAgent' do
-      expect(agent.options).to include('extract' => old_extract,
-                                       'template' => old_template)
-      ConvertWebsiteAgentTemplateForMerge.new.up
-      agent.reload
-      expect(agent.options).to include('extract' => new_extract,
-                                       'template' => new_template)
-    end
-  end
-    
-      it 'replaces invalid byte sequences in a message' do
-    log = AgentLog.new(:agent => agents(:jane_website_agent), level: 3)
-    log.message = '\u{3042}\xffA\x95'
-    expect { log.save! }.not_to raise_error
-    expect(log.message).to eq('\u{3042}<ff>A\<95>')
-  end
-    
-        it 'should provide the since attribute after the first run' do
-      time = (Time.now-1.minute).iso8601
-      @checker.memory[:last_event] = time
-      @checker.save
-      expect(@checker.reload.send(:query_parameters)).to eq({:query => {:since => time}})
-    end
-  end
-    
-        it 'should raise error when response has an error' do
-      stub(HTTParty).post { {'error' => {'message' => 'Sample error'}} }
-      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Sample error/)
     end
   end
 end
 
     
-            return a.casecmp(b) if a_length == 1 && b_length == 1
-        return 1 if a_length == 1
-        return -1 if b_length == 1
-    
-        def push(*names)
-      @filters.push *filter_const(names)
+        # Returns the extension of the file. e.g. 'jpg' for 'file.jpg'
+    # If the style has a format defined, it will return the format instead
+    # of the actual extension.
+    def extension attachment, style_name
+      ((style = attachment.styles[style_name.to_s.to_sym]) && style[:format]) ||
+        File.extname(attachment.original_filename).sub(/\A\.+/, ''.freeze)
     end
-    
-            def destroy
-          authorize! :destroy, @product
-          @product.destroy
-          respond_with(@product, status: 204)
-        end
-    
-            private
-    
-      config.vm.define :smartos do |smartos|
-    smartos.vm.box = 'smartos-base1310-64-virtualbox-20130806.box'
-    smartos.vm.box_url = 'http://dlc-int.openindiana.org/aszeszo/vagrant/smartos-base1310-64-virtualbox-20130806.box'
-  end
-    
-          # Load the script into memory.
-      input.scripts[scriptname] = File.read(path)
-    end
-    
-        pkgdata = {
-      'arch' => architecture,
-      'name' => name,
-      'version' => pkg_version,
-      'comment' => description,
-      'desc' => description,
-      'origin' => pkg_origin,
-      'maintainer' => maintainer,
-      'www' => url,
-      # prefix is required, but it doesn't seem to matter
-      'prefix' => '/',
-    }
-    
-      def set_default_prefix
-    attributes[:prefix] = self.class.default_prefix
-    attributes[:prefix_given?] = true
-  end
-    
-    # Use an OS X pkg built with pkgbuild.
-#
-# Supports input and output. Requires pkgbuild and (for input) pkgutil, part of a
-# standard OS X install in 10.7 and higher.
-class FPM::Package::OSXpkg < FPM::Package
-    
-        # Some setup.py's assume $PWD == current directory of setup.py, so let's
-    # chdir first.
-    ::Dir.chdir(project_dir) do
-      flags = [ '--root', staging_path ]
-      if !attributes[:python_install_lib].nil?
-        flags += [ '--install-lib', File.join(prefix, attributes[:python_install_lib]) ]
-      elsif !attributes[:prefix].nil?
-        # setup.py install --prefix PREFIX still installs libs to
-        # PREFIX/lib64/python2.7/site-packages/
-        # but we really want something saner.
-        #
-        # since prefix is given, but not python_install_lib, assume PREFIX/lib
-        flags += [ '--install-lib', File.join(prefix, 'lib') ]
-      end
-    
-          self
-    end # def add_symlink
-  end
-end
