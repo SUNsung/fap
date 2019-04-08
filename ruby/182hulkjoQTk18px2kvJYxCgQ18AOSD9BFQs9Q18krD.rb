b3e39882e6a6bc458cf38e404c26540999cdf815
SUNsung <weatherius@gmail.com>
2019-04-08 11:22:02 +0300

    Rewrite [182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb] File in: 2019-04-08 11:22:02

--- ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
+++ ruby/182hulkjoQTk18px2kvJYxCgQ18AOSD9BFQs9Q18krD.rb
@@ -1,103 +1,67 @@
 
         
-          before_action :require_user!
-    
-      def preferred_locale
-    http_accept_language.preferred_language_from(available_locales)
-  end
-    
-      def request_time
-    @_request_time ||= Time.now.utc
-  end
-    
-        self.busy = false
-    self.dtmf = ''
-  end
-    
-    IAX_IE_CALLED_NUMBER  = 1
-IAX_IE_CALLING_NUMBER = 2
-IAX_IE_AUTH_METHODS   = 3
-IAX_IE_CALLING_NAME   = 4
-IAX_IE_USERNAME       = 6
-IAX_IE_DESIRED_CODEC  = 9
-IAX_IE_ORIGINAL_DID   = 10
-IAX_IE_ACTUAL_CODECS  = 8
-IAX_IE_PROTO_VERSION  = 11
-IAX_IE_REG_REFRESH    = 19
-IAX_IE_CHALLENGE_DATA = 15
-IAX_IE_CHALLENGE_RESP = 16
-IAX_IE_APPARENT_ADDR  = 18
-IAX_IE_REGREJ_CAUSE   = 22
-IAX_IE_HANGUP_CAUSE   = 42
-    
-                components.each do |c|
-              encoded << [c.length].pack('N')
-              encoded << c
-            end
-    
-              private
-    
-              # Decodes the msg_type from an OpenSSL::ASN1::ASN1Data
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_msg_type(input)
-            input.value[0].value.to_i
-          end
-    
-        desc 'List all undocumented methods and classes.'
-    task :undocumented do
-      opts = ENV['YARD_OPTS'] || ''
-      ENV['YARD_OPTS'] = opts.dup + <<OPTS
- --list --tag comment --hide-tag comment --query '
-  object.docstring.blank? &&
-  !(object.type == :method && object.is_alias?)'
-OPTS
-      Rake::Task['yard'].execute
-    end
-    
-          opts.on('-q', '--quiet', 'Silence warnings and status messages during conversion.') do |bool|
-        @options[:for_engine][:quiet] = bool
-      end
-    
-          # Creates a new filesystem importer that imports files relative to a given path.
-      #
-      # @param root [String] The root path.
-      #   This importer will import files relative to this path.
-      def initialize(root)
-        @root = File.expand_path(root)
-        @real_root = Sass::Util.realpath(@root).to_s
-        @same_name_warnings = Set.new
-      end
-    
-          def encode_token(token)
-        Base64.strict_encode64(token)
-      end
-    
-      describe '#random_string' do
-    it 'outputs a string of 32 characters' do
-      expect(subject.random_string.length).to eq(32)
+            it 'creates a scenario label with the given text' do
+      expect(scenario_label(scenario, 'Other')).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Other</span>'
+      )
     end
   end
     
-        def URIAddEncodedOctetToBuffer(octet, result, index)
-      result[index] = 37; # Char code of '%'.
-      index         += 1
-      result[index] = @@hexCharCodeArray[octet >> 4];
-      index         += 1
-      result[index] = @@hexCharCodeArray[octet & 0x0F];
-      index += 1
-      return index;
+        describe 'data' do
+      it 'should be invalid with invalid data' do
+        subject.data = invalid_data
+        expect(subject).not_to be_valid
+        expect(subject).to have(1).error_on(:base)
+    
+          it 'runs until stop is called' do
+        mock.instance_of(Rufus::Scheduler).join
+        Thread.new { while @agent_runner.instance_variable_get(:@running) != false do sleep 0.1; @agent_runner.stop end }
+        @agent_runner.run
+      end
+    
+        Thread.abort_on_exception = @taoe
+    $stdout, $stderr = @oso, @ose
+  end
+    
+        it 'html_safes the output unless :skip_safe is passed in' do
+      expect(Utils.jsonify({:foo => 'bar'})).to be_html_safe
+      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => false)).to be_html_safe
+      expect(Utils.jsonify({:foo => 'bar'}, :skip_safe => true)).not_to be_html_safe
     end
+  end
     
-          attr_reader :page, :content
-    
-    desc 'Build and install'
-task :install => :build do
-  sh 'gem install --local --no-ri --no-rdoc pkg/#{name}-#{version}.gem'
+        it 'should revert extract and template options for an updated WebsiteAgent' do
+      expect(agent.options).to include('extract' => new_extract,
+                                       'template' => new_template)
+      ConvertWebsiteAgentTemplateForMerge.new.down
+      agent.reload
+      expect(agent.options).to include('extract' => reverted_extract,
+                                       'template' => reverted_template)
+    end
+  end
 end
+
     
-      class DuplicatePageError < Error
-    attr_accessor :dir
-    attr_accessor :existing_path
-    attr_accessor :attempted_path
+        # See {CapabilityHost#capability}
+    def capability(*args)
+      super
+    rescue Errors::CapabilityNotFound => e
+      raise Errors::GuestCapabilityNotFound,
+        cap: e.extra_data[:cap],
+        guest: name
+    rescue Errors::CapabilityInvalid => e
+      raise Errors::GuestCapabilityInvalid,
+        cap: e.extra_data[:cap],
+        guest: name
+    end
+    
+        # Get a value by the given key.
+    #
+    # This will evaluate the block given to `register` and return the
+    # resulting value.
+    def get(key)
+      return nil if !@items.key?(key)
+      return @results_cache[key] if @results_cache.key?(key)
+      @results_cache[key] = @items[key].call
+    end
+    alias :[] :get
