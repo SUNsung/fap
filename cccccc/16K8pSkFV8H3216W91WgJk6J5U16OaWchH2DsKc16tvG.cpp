e50d57ebff3df94f8c8fd767b58f1bb97f7ea552
SUNsung <weatherius@gmail.com>
2019-04-09 04:01:05 +0300

    Rewrite [16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp] File in: 2019-04-09 04:01:04

--- cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
+++ cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
@@ -1,442 +1,173 @@
 
         
-        #include <grpcpp/security/auth_context.h>
+        #if GTEST_HAS_DEATH_TEST
     
-    #include <grpc/support/port_platform.h>
+    #define EXPECT_NONFATAL_FAILURE_ON_ALL_THREADS(statement, substr) \
+  do {\
+    ::testing::TestPartResultArray gtest_failures;\
+    ::testing::internal::SingleFailureChecker gtest_checker(\
+        &gtest_failures, ::testing::TestPartResult::kNonFatalFailure, \
+        (substr));\
+    {\
+      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
+          ::testing::ScopedFakeTestPartResultReporter::INTERCEPT_ALL_THREADS, \
+          &gtest_failures);\
+      if (::testing::internal::AlwaysTrue()) { statement; }\
+    }\
+  } while (::testing::internal::AlwaysFalse())
     
-      void Destroy(grpc_call_element* elem, const grpc_call_final_info* final_info,
-               grpc_closure* then_call_closure) override;
+    
+    {
+    {}  // namespace internal
+}  // namespace testing
+    
+    #include 'sample1.h'
+    
+    int Water::allocated_ = 0;
     
     
-    {  const protobuf::FileDescriptor* file_desc =
-      descriptor_pool_->FindFileContainingSymbol(symbol);
-  if (file_desc == nullptr) {
-    return Status(StatusCode::NOT_FOUND, 'Symbol not found.');
-  }
-  std::unordered_set<grpc::string> seen_files;
-  FillFileDescriptorResponse(file_desc, response, &seen_files);
-  return Status::OK;
-}
-    
-    #if !defined(GPR_LINUX) && !defined(GPR_WINDOWS) && !defined(GPR_APPLE)
-    
-    #endif  // GPR_WINDOWS
+#endif  // GTEST_SAMPLES_SAMPLE2_H_
 
     
-    TEST(AutoCompactTest, ReadHalf) {
-  DoReads(kCount/2);
-}
+    #include <dmlc/registry.h>
+#include <vector>
+#include <utility>
+#include <string>
+#include <functional>
+#include <memory>
+#include './base.h'
+#include './data.h'
+#include './objective.h'
+#include './feature_map.h'
+#include '../../src/common/host_device_vector.h'
     
-      fname = TempFileName('tmp', 999);
-  ASSERT_EQ('tmp/', std::string(fname.data(), 4));
-  ASSERT_TRUE(ParseFileName(fname.c_str() + 4, &number, &type));
-  ASSERT_EQ(999, number);
-  ASSERT_EQ(kTempFile, type);
     
-    namespace leveldb {
-namespace log {
-    }
-    }
-    
-    Status TableCache::Get(const ReadOptions& options,
-                       uint64_t file_number,
-                       uint64_t file_size,
-                       const Slice& k,
-                       void* arg,
-                       void (*saver)(void*, const Slice&, const Slice&)) {
-  Cache::Handle* handle = nullptr;
-  Status s = FindTable(file_number, file_size, &handle);
-  if (s.ok()) {
-    Table* t = reinterpret_cast<TableAndFile*>(cache_->Value(handle))->table;
-    s = t->InternalGet(options, k, arg, saver);
-    cache_->Release(handle);
-  }
-  return s;
-}
-    
-    class TableCache {
- public:
-  TableCache(const std::string& dbname, const Options& options, int entries);
-  ~TableCache();
-    }
-    
-    U_NAMESPACE_BEGIN
-    
-    //----------------------------------------------------------------------------
-//
-//  ScriptSet implementation
-//
-//----------------------------------------------------------------------------
-ScriptSet::ScriptSet() {
-    for (uint32_t i=0; i<UPRV_LENGTHOF(bits); i++) {
-        bits[i] = 0;
-    }
-}
-    
-    U_NAMESPACE_END
-    
-        /**
-     * Get maximum significant digits. INT32_MAX means no maximum.
-     */
-    int32_t getMax() const {
-        return fMax;
-    }
-    
-    U_NAMESPACE_BEGIN
-    
-        /**
-     * Returns TRUE if this class can format positiveValue using
-     * the given range of digit counts.
-     *
-     * @param positiveValue the value to format
-     * @param range the acceptable range of digit counts.
-     */
-    static UBool canFormat(
-            int32_t positiveValue, const IntDigitCountRange &range);
-    
-    /**
- * UnicodeFunctor API.  Cast 'this' to a UnicodeReplacer* pointer
- * and return the pointer.
+    {/*!
+ * \brief Macro to register objective function.
+ *
+ * \code
+ * // example of registering a objective
+ * XGBOOST_REGISTER_OBJECTIVE(LinearRegression, 'reg:linear')
+ * .describe('Linear regression objective')
+ * .set_body([]() {
+ *     return new RegLossObj(LossType::kLinearSquare);
+ *   });
+ * \endcode
  */
-UnicodeReplacer* StringMatcher::toReplacer() const {
-  StringMatcher  *nonconst_this = const_cast<StringMatcher *>(this);
-  UnicodeReplacer *nonconst_base = static_cast<UnicodeReplacer *>(nonconst_this);
-  
-  return nonconst_base;
-}
-    
-      virtual ~Extension_call_args() throw();
-  std::string registry;
-  std::string item;
-  ExtensionPluginRequest request;
-    
-    typedef struct _ExtensionManager_registerExtension_presult__isset {
-  _ExtensionManager_registerExtension_presult__isset() : success(false) {}
-  bool success :1;
-} _ExtensionManager_registerExtension_presult__isset;
-    
-      _ExtensionStatus__isset __isset;
-    
-    
-    {
-    {
-    {    osquery::Row r;
-    for (i = 0; i < nArg; ++i) {
-      if (azCol[i] != nullptr) {
-        r[std::string(azCol[i])] = (azArg[i] == nullptr)
-                                       ? osquery::FLAGS_nullvalue
-                                       : std::string(azArg[i]);
-      }
-    }
-    osquery::computeRowLengths(r, p->prettyPrint->lengths);
-    p->prettyPrint->results.push_back(r);
-    break;
-  }
-  case MODE_Line: {
-    int w = 5;
-    if (azArg == nullptr) {
-      break;
-    }
-    for (i = 0; i < nArg; i++) {
-      int len = strlen30(azCol[i] != nullptr ? azCol[i] : '');
-      if (len > w) {
-        w = len;
-      }
-    }
-    if (p->cnt++ > 0) {
-      fprintf(p->out, '\n');
-    }
-    for (i = 0; i < nArg; i++) {
-      fprintf(p->out,
-              '%*s = %s\n',
-              w,
-              azCol[i],
-              azArg[i] != nullptr ? azArg[i] : p->nullvalue);
-    }
-    break;
-  }
-  case MODE_Column: {
-    if (p->cnt++ == 0) {
-      for (i = 0; i < nArg; i++) {
-        int w;
-        if (i < ArraySize(p->colWidth)) {
-          w = p->colWidth[i];
-        } else {
-          w = 0;
-        }
-        if (w == 0) {
-          w = strlen30(azCol[i] != nullptr ? azCol[i] : '');
-          if (w < 10) {
-            w = 10;
-          }
-          int n = strlen30((azArg != nullptr) && (azArg[i] != nullptr)
-                               ? azArg[i]
-                               : p->nullvalue);
-          if (w < n) {
-            w = n;
-          }
-        }
-        if (i < ArraySize(p->actualWidth)) {
-          p->actualWidth[i] = w;
-        }
-        if (p->showHeader != 0) {
-          if (w < 0) {
-            fprintf(p->out,
-                    '%*.*s%s',
-                    -w,
-                    -w,
-                    azCol[i],
-                    i == nArg - 1 ? '\n' : '  ');
-          } else {
-            fprintf(p->out,
-                    '%-*.*s%s',
-                    w,
-                    w,
-                    azCol[i],
-                    i == nArg - 1 ? '\n' : '  ');
-          }
-        }
-      }
-      if (p->showHeader != 0) {
-        for (i = 0; i < nArg; i++) {
-          int w;
-          if (i < ArraySize(p->actualWidth)) {
-            w = p->actualWidth[i];
-            if (w < 0) {
-              w = -w;
-            }
-          } else {
-            w = 10;
-          }
-          fprintf(p->out,
-                  '%-*.*s%s',
-                  w,
-                  w,
-                  '-----------------------------------'
-                  '----------------------------------------------------------',
-                  i == nArg - 1 ? '\n' : '  ');
-        }
-      }
-    }
-    if (azArg == nullptr) {
-      break;
-    }
-    for (i = 0; i < nArg; i++) {
-      int w;
-      if (i < ArraySize(p->actualWidth)) {
-        w = p->actualWidth[i];
-      } else {
-        w = 10;
-      }
-      if (i == 1 && (p->aiIndent != nullptr) && (p->pStmt != nullptr)) {
-        if (p->iIndent < p->nIndent) {
-          fprintf(p->out, '%*.s', p->aiIndent[p->iIndent], '');
-        }
-        p->iIndent++;
-      }
-      if (w < 0) {
-        fprintf(p->out,
-                '%*.*s%s',
-                -w,
-                -w,
-                azArg[i] != nullptr ? azArg[i] : p->nullvalue,
-                i == nArg - 1 ? '\n' : '  ');
-      } else {
-        fprintf(p->out,
-                '%-*.*s%s',
-                w,
-                w,
-                azArg[i] != nullptr ? azArg[i] : p->nullvalue,
-                i == nArg - 1 ? '\n' : '  ');
-      }
-    }
-    break;
-  }
-  case MODE_Semi:
-  case MODE_List: {
-    if (p->cnt++ == 0 && (p->showHeader != 0)) {
-      for (i = 0; i < nArg; i++) {
-        fprintf(p->out, '%s%s', azCol[i], i == nArg - 1 ? '\n' : p->separator);
-      }
-    }
-    if (azArg == nullptr) {
-      break;
-    }
-    for (i = 0; i < nArg; i++) {
-      const char* z = azArg[i];
-      if (z == nullptr) {
-        z = p->nullvalue;
-      }
-      fprintf(p->out, '%s', z);
-      if (i < nArg - 1) {
-        fprintf(p->out, '%s', p->separator);
-      } else if (p->mode == MODE_Semi) {
-        fprintf(p->out, ';\n');
-      } else {
-        fprintf(p->out, '\n');
-      }
-    }
-    break;
-  }
-  case MODE_Csv: {
-    if (p->cnt++ == 0 && (p->showHeader != 0)) {
-      for (i = 0; i < nArg; i++) {
-        output_csv(p,
-                   azCol[i] != nullptr ? azCol[i] : '',
-                   static_cast<int>(i < nArg - 1));
-      }
-      fprintf(p->out, '\n');
-    }
-    if (azArg == nullptr) {
-      break;
-    }
-    for (i = 0; i < nArg; i++) {
-      output_csv(p, azArg[i], static_cast<int>(i < nArg - 1));
-    }
-    fprintf(p->out, '\n');
-    break;
-  }
-  }
-  return 0;
-}
-    
-    #pragma once
-    
-    
-    {
-    {} // namespace perf_event_open
-} // namespace osquery
+#define XGBOOST_REGISTER_OBJECTIVE(UniqueId, Name)                      \
+  static DMLC_ATTRIBUTE_UNUSED ::xgboost::ObjFunctionReg &              \
+  __make_ ## ObjFunctionReg ## _ ## UniqueId ## __ =                    \
+      ::dmlc::Registry< ::xgboost::ObjFunctionReg>::Get()->__REGISTER__(Name)
+}  // namespace xgboost
+#endif  // XGBOOST_OBJECTIVE_H_
 
     
-    #include <osquery/utils/expected/expected.h>
-#include <osquery/utils/system/posix/errno.h>
-    
-    EnterExitJoiner::KeyType createKey(EventType const type,
-                                   __s32 const pid,
-                                   __s32 const tgid) {
-  auto key = EnterExitJoiner::KeyType(static_cast<std::uint32_t>(pid));
-  key <<= 32;
-  key |= static_cast<std::uint32_t>(tgid);
-  key <<= 32;
-  key |= static_cast<std::uint32_t>(type);
-  return key;
-}
-    
-        {BPF_JMP | BPF_K | BPF_CALL  ,          0,         0,    0,  BPF_FUNC_get_current_pid_tgid},
-    {BPF_STX | BPF_W | BPF_MEM   , BPF_REG_10, BPF_REG_0,  -36,  0}, // Event.body.pid
-    {BPF_ALU64 | BPF_K | BPF_RSH ,          0,         0,    0,  32},
-    {BPF_STX | BPF_W | BPF_MEM   , BPF_REG_10, BPF_REG_0,  -32,  0}, // Event.body.tgid
-    
-    #pragma once
-    
-    #include 'rocksdb/db.h'
-#include 'rocksdb/slice.h'
-#include 'rocksdb/options.h'
-    
-      // Initialize pointer options for each column family
-  for (size_t i = 0; i < loaded_cf_descs.size(); ++i) {
-    auto* loaded_bbt_opt = reinterpret_cast<BlockBasedTableOptions*>(
-        loaded_cf_descs[0].options.table_factory->GetOptions());
-    // Expect the same as BlockBasedTableOptions will be loaded form file.
-    assert(loaded_bbt_opt->block_size == bbt_opts.block_size);
-    // However, block_cache needs to be manually initialized as documented
-    // in rocksdb/utilities/options_util.h.
-    loaded_bbt_opt->block_cache = cache;
-  }
-  // In addition, as pointer options are initialized with default value,
-  // we need to properly initialized all the pointer options if non-defalut
-  // values are used before calling DB::Open().
-  assert(loaded_cf_descs[0].options.compaction_filter == nullptr);
-  loaded_cf_descs[0].options.compaction_filter = compaction_filter.get();
-    
-    class DbDumpTool {
+    template<typename StorageIndex>
+class SparsePageLZ4Format : public SparsePageFormat {
  public:
-  bool Run(const DumpOptions& dump_options,
-           rocksdb::Options options = rocksdb::Options());
-};
+  explicit SparsePageLZ4Format(bool use_lz4_hc)
+      : use_lz4_hc_(use_lz4_hc) {
+    raw_bytes_ = raw_bytes_value_ = raw_bytes_index_ = 0;
+    encoded_bytes_value_ = encoded_bytes_index_ = 0;
+    nthread_ = dmlc::GetEnv('XGBOOST_LZ4_DECODE_NTHREAD', 4);
+    nthread_write_ = dmlc::GetEnv('XGBOOST_LZ4_COMPRESS_NTHREAD', 12);
+  }
+  virtual ~SparsePageLZ4Format() {
+    size_t encoded_bytes = raw_bytes_ +  encoded_bytes_value_ + encoded_bytes_index_;
+    raw_bytes_ += raw_bytes_value_ + raw_bytes_index_;
+    if (raw_bytes_ != 0) {
+      LOG(CONSOLE) << 'raw_bytes=' << raw_bytes_
+                   << ', encoded_bytes=' << encoded_bytes
+                   << ', ratio=' << double(encoded_bytes) / raw_bytes_
+                   << ', ratio-index=' << double(encoded_bytes_index_) /raw_bytes_index_
+                   << ', ratio-value=' << double(encoded_bytes_value_) /raw_bytes_value_;
+    }
+  }
+    }
     
-    
-    {
-    {}  // namespace experimental
-}  // namespace rocksdb
-
-    
-     virtual void SetTtl(int32_t ttl) = 0;
-    
-    
-    {CC_CONSTRUCTOR_ACCESS:
-    FlipY3D() {}
-    virtual ~FlipY3D() {}
-private:
-    CC_DISALLOW_COPY_AND_ASSIGN(FlipY3D);
-};
-    
-        /** Sets the inner action.
-     *
-     * @param action The inner action.
+    class LambdaRankObjMAP : public LambdaRankObj {
+ protected:
+  struct MAPStats {
+    /*! \brief the accumulated precision */
+    float ap_acc;
+    /*!
+     * \brief the accumulated precision,
+     *   assuming a positive instance is missing
      */
-    void setInnerAction(FiniteTimeAction *action)
-    {
-        if (_innerAction != action)
-        {
-            CC_SAFE_RETAIN(action);
-            CC_SAFE_RELEASE(_innerAction);
-            _innerAction = action;
-        }
-    }
-    
-    /**
- @brief This action simulates a page turn from the bottom right hand corner of the screen.
- 
- @details It's not much use by itself but is used by the PageTurnTransition.
-         Based on an original paper by L Hong et al.
-         http://www.parc.com/publication/1638/turning-pages-of-3d-electronic-books.html
-  
- @since v0.8.2
- */
-class CC_DLL PageTurn3D : public Grid3DAction
-{
-public:
-    /**
-     * @js NA 
+    float ap_acc_miss;
+    /*!
+     * \brief the accumulated precision,
+     * assuming that one more positive instance is inserted ahead
      */
-    virtual GridBase* getGrid() override;
+    float ap_acc_add;
+    /* \brief the accumulated positive instance count */
+    float hits;
+    MAPStats() = default;
+    MAPStats(float ap_acc, float ap_acc_miss, float ap_acc_add, float hits)
+        : ap_acc(ap_acc), ap_acc_miss(ap_acc_miss), ap_acc_add(ap_acc_add), hits(hits) {}
+  };
+  /*!
+   * \brief Obtain the delta MAP if trying to switch the positions of instances in index1 or index2
+   *        in sorted triples
+   * \param sorted_list the list containing entry information
+   * \param index1,index2 the instances switched
+   * \param map_stats a vector containing the accumulated precisions for each position in a list
+   */
+  inline bst_float GetLambdaMAP(const std::vector<ListEntry> &sorted_list,
+                                int index1, int index2,
+                                std::vector<MAPStats> *p_map_stats) {
+    std::vector<MAPStats> &map_stats = *p_map_stats;
+    if (index1 == index2 || map_stats[map_stats.size() - 1].hits == 0) {
+      return 0.0f;
     }
-    
-    
-    {    for (int i = 0; i < _gridSize.width; ++i)
-    {
-        for ( int j = 0; j < _gridSize.height; ++j)
-        {
-            tileArray->position.set((float)i, (float)j);
-            tileArray->startPosition.set((float)i, (float)j);
-            tileArray->delta = getDelta(Size(i, j));
-            ++tileArray;
-        }
+    if (index1 > index2) std::swap(index1, index2);
+    bst_float original = map_stats[index2].ap_acc;
+    if (index1 != 0) original -= map_stats[index1 - 1].ap_acc;
+    bst_float changed = 0;
+    bst_float label1 = sorted_list[index1].label > 0.0f ? 1.0f : 0.0f;
+    bst_float label2 = sorted_list[index2].label > 0.0f ? 1.0f : 0.0f;
+    if (label1 == label2) {
+      return 0.0;
+    } else if (label1 < label2) {
+      changed += map_stats[index2 - 1].ap_acc_add - map_stats[index1].ap_acc_add;
+      changed += (map_stats[index1].hits + 1.0f) / (index1 + 1);
+    } else {
+      changed += map_stats[index2 - 1].ap_acc_miss - map_stats[index1].ap_acc_miss;
+      changed += map_stats[index2].hits / (index2 + 1);
     }
-}
-    
-    bool ActionTween::initWithDuration(float duration, const std::string& key, float from, float to)
-{
-    if (ActionInterval::initWithDuration(duration))
-    {
-        _key    = key;
-        _to       = to;
-        _from     = from;
-        return true;
+    bst_float ans = (changed - original) / (map_stats[map_stats.size() - 1].hits);
+    if (ans < 0) ans = -ans;
+    return ans;
+  }
+  /*
+   * \brief obtain preprocessing results for calculating delta MAP
+   * \param sorted_list the list containing entry information
+   * \param map_stats a vector containing the accumulated precisions for each position in a list
+   */
+  inline void GetMAPStats(const std::vector<ListEntry> &sorted_list,
+                          std::vector<MAPStats> *p_map_acc) {
+    std::vector<MAPStats> &map_acc = *p_map_acc;
+    map_acc.resize(sorted_list.size());
+    bst_float hit = 0, acc1 = 0, acc2 = 0, acc3 = 0;
+    for (size_t i = 1; i <= sorted_list.size(); ++i) {
+      if (sorted_list[i - 1].label > 0.0f) {
+        hit++;
+        acc1 += hit / i;
+        acc2 += (hit - 1) / i;
+        acc3 += (hit + 1) / i;
+      }
+      map_acc[i - 1] = MAPStats(acc1, acc2, acc3, hit);
     }
+  }
+  void GetLambdaWeight(const std::vector<ListEntry> &sorted_list,
+                       std::vector<LambdaPair> *io_pairs) override {
+    std::vector<LambdaPair> &pairs = *io_pairs;
+    std::vector<MAPStats> map_stats;
+    GetMAPStats(sorted_list, &map_stats);
+    for (auto & pair : pairs) {
+      pair.weight *=
+          GetLambdaMAP(sorted_list, pair.pos_index,
+                       pair.neg_index, &map_stats);
     }
-    
-    void AnimationCache::parseVersion2(const ValueMap& animations)
-{
-    SpriteFrameCache *frameCache = SpriteFrameCache::getInstance();
-    }
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
+  }
+};
