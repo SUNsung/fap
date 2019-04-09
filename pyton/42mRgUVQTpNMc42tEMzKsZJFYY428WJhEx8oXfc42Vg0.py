550aee305210d6a95e94dd19c2df2e0ae5637584
SUNsung <weatherius@gmail.com>
2019-04-10 01:06:53 +0300

    Rewrite [42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py] File in: 2019-04-10 01:06:52

--- pyton/42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py
+++ pyton/42mRgUVQTpNMc42tEMzKsZJFYY428WJhEx8oXfc42Vg0.py
@@ -1,138 +1,44 @@
 
         
-        tensorboard = TensorBoard(batch_size=batch_size,
-                          embeddings_freq=1,
-                          embeddings_layer_names=['features'],
-                          embeddings_metadata='metadata.tsv',
-                          embeddings_data=x_test)
+        
+class LinkedList(object):
+    
+        def bfs(self, source, dest):
+        # Use self.visited_ids to track visited nodes
+        # Use self.lookup to translate a person_id to a Person
+        pass
+
+    
+        def set(self, key, value):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                item.value = value
+                return
+        self.table[hash_index].append(Item(key, value))
+    
+        y_train = np.reshape(y_train, (len(y_train), 1))
+    y_test = np.reshape(y_test, (len(y_test), 1))
     
     
-def serialize(loss):
-    return serialize_keras_object(loss)
+def create_multi_input_model_from(layer1, layer2):
+    input_1 = Input(shape=(data_dim,))
+    input_2 = Input(shape=(data_dim,))
+    out1 = layer1(input_1)
+    out2 = layer2(input_2)
+    out = Average()([out1, out2])
+    model = Model([input_1, input_2], out)
+    model.add_loss(K.mean(out2))
+    model.add_loss(1)
+    model.add_loss(1)
+    return model
     
-            # Arguments
-            node_index: Integer, index of the node
-                from which to retrieve the attribute.
-                E.g. `node_index=0` will correspond to the
-                first time the layer was called.
+    autoencoder.compile(loss='mse', optimizer='adam')
     
-    Typical output
---------------
-    
-    for f in solutions:
-    if not f.endswith('.py'):
-        continue
-    
-    '''
-=========================================================
-Feature agglomeration
-=========================================================
-    
-    '''
-print(__doc__)
-    
-        platforms = 'any',
-    zip_safe = True,
-    include_package_data = True,
-    
-    def main(**kwargs):
-    '''Main entry point.
-    you-get (legacy)
-    '''
-    from .common import main
-    main(**kwargs)
-    
-    from ..common import *
-from ..extractor import VideoExtractor
-    
-        for quality in ['1080','720','480','380','240','144','auto']:
-        try:
-            real_url = info[quality][1]['url']
-            if real_url:
-                break
-        except KeyError:
-            pass
-    
-    def ifeng_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-# old pattern /uuid.shtml
-# now it could be #uuid
-    id = r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', url)
-    if id:
-        return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
-    
-        def prepare(self, **kwargs):
-        if re.search(r'imgur\.com/a/', self.url):
-            # album
-            content = get_content(self.url)
-            album = match1(content, r'album\s*:\s*({.*}),') or \
-                    match1(content, r'image\s*:\s*({.*}),')
-            album = json.loads(album)
-            count = album['album_images']['count']
-            images = album['album_images']['images']
-            ext = images[0]['ext']
-            self.streams = {
-                'original': {
-                    'src': ['http://i.imgur.com/%s%s' % (i['hash'], ext)
-                            for i in images],
-                    'size': sum([i['size'] for i in images]),
-                    'container': ext[1:]
-                },
-                'thumbnail': {
-                    'src': ['http://i.imgur.com/%ss%s' % (i['hash'], '.jpg')
-                            for i in images],
-                    'container': 'jpg'
-                }
-            }
-            self.title = album['title']
-    
-    *References:
-bottle
-https://github.com/bottlepy/bottle/blob/cafc15419cbb4a6cb748e6ecdccf92893bb25ce5/bottle.py#L270
-django
-https://github.com/django/django/blob/ffd18732f3ee9e6f0374aff9ccf350d85187fac2/django/utils/functional.py#L19
-pip
-https://github.com/pypa/pip/blob/cb75cca785629e15efb46c35903827b3eae13481/pip/utils/__init__.py#L821
-pyramimd
-https://github.com/Pylons/pyramid/blob/7909e9503cdfc6f6e84d2c7ace1d3c03ca1d8b73/pyramid/decorator.py#L4
-werkzeug
-https://github.com/pallets/werkzeug/blob/5a2bf35441006d832ab1ed5a31963cbc366c99ac/werkzeug/utils.py#L35
-    
-        def clone(self, **attrs):
-        '''Clone a prototype and update inner attributes dictionary'''
-        # Python in Practice, Mark Summerfield
-        obj = self.__class__()
-        obj.__dict__.update(attrs)
-        return obj
-    
-    
-class Ui(object):
-    ''' UI interaction class '''
-    
-        # high-level i.e. Abstraction specific
-    def scale(self, pct):
-        self._radius *= pct
-    
-        def add(self, graphic):
-        self.graphics.append(graphic)
-    
-    
-class Request(object):
-    ''' request '''
-    
-        def test_subscriber_shall_be_detachable_from_subscriptions(cls):
-        subscription = 'sub msg'
-        pro = Provider()
-        sub = Subscriber('sub name', pro)
-        sub.subscribe(subscription)
-        cls.assertEqual(len(pro.subscribers[subscription]), 1)
-        sub.unsubscribe(subscription)
-        cls.assertEqual(len(pro.subscribers[subscription]), 0)
-    
-    
-class AdapterTest(unittest.TestCase):
-    def test_dog_adapter_shall_make_noise(self):
-        dog = Dog()
-        dog_adapter = Adapter(dog, make_noise=dog.bark)
-        noise = dog_adapter.make_noise()
-        expected_noise = 'woof!'
-        self.assertEqual(noise, expected_noise)
+    import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+from keras.layers import SimpleRNN
+from keras import initializers
+from keras.optimizers import RMSprop
