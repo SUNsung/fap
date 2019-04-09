22200b593f9273a046b6a18e3111d852e1b23ac9
SUNsung <weatherius@gmail.com>
2019-04-09 20:39:06 +0300

    Create [05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb] File in: 2019-04-09 20:39:05

---
+++ ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
@@ -0,0 +1,162 @@
+def local_require
+  require 'json'
+  JSON.pretty_generate(DATA)
+end
+    
+    # -------------------------------------------------------------------
+# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
+# -------------------------------------------------------------------
+    
+    $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+    
+        def process(args)
+      arg_is_present? args, '--server', 'The --server command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--serve', 'The --serve command has been replaced by the \
+                          'serve' subcommand.'
+      arg_is_present? args, '--no-server', 'To build Jekyll without launching a server, \
+                          use the 'build' subcommand.'
+      arg_is_present? args, '--auto', 'The switch '--auto' has been replaced with \
+                          '--watch'.'
+      arg_is_present? args, '--no-auto', 'To disable auto-replication, simply leave off \
+                          the '--watch' switch.'
+      arg_is_present? args, '--pygments', 'The 'pygments'settings has been removed in \
+                          favour of 'highlighter'.'
+      arg_is_present? args, '--paginate', 'The 'paginate' setting can only be set in \
+                          your config files.'
+      arg_is_present? args, '--url', 'The 'url' setting can only be set in your \
+                          config files.'
+      no_subcommand(args)
+    end
+    
+              new(hash)
+        end
+    
+              hash = {
+            iid: issue.number,
+            title: issue.title,
+            description: issue.body,
+            milestone_number: issue.milestone&.number,
+            state: issue.state == 'open' ? :opened : :closed,
+            assignees: issue.assignees.map do |u|
+              Representation::User.from_api_response(u)
+            end,
+            label_names: issue.labels.map(&:name),
+            author: user,
+            created_at: issue.created_at,
+            updated_at: issue.updated_at,
+            pull_request: issue.pull_request ? true : false
+          }
+    
+              hash[:author] &&= Representation::User.from_json_hash(hash[:author])
+    
+        def inheritable_copy
+      self.class.new @filters
+    end
+    
+        private
+    
+            css('a.is-button > h3').each do |node|
+          node.parent.content = node.content
+        end
+    
+          def root
+        css('.nav-index-group').each do |node|
+          if heading = node.at_css('.nav-index-group-heading')
+            heading.name = 'h2'
+          end
+          node.parent.before(node.children)
+        end
+    
+            css('.toplang', '#quickview', '.top').remove
+    
+        execute 'INSERT INTO share_visibilities (user_id, shareable_id, shareable_type) ' \
+            'SELECT post_visibility.user_id, photos.id, 'Photo' FROM photos ' \
+            'INNER JOIN posts ON posts.guid = photos.status_message_guid AND posts.type = 'StatusMessage' ' \
+            'LEFT OUTER JOIN share_visibilities ON share_visibilities.shareable_id = photos.id ' \
+            'INNER JOIN share_visibilities AS post_visibility ON post_visibility.shareable_id = posts.id ' \
+            'WHERE photos.public = false AND share_visibilities.shareable_id IS NULL ' \
+            'AND post_visibility.shareable_type = 'Post''
+  end
+    
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
+  end
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
+  end
+end
+    
+        it 'generates the contacts_json fixture', :fixture => true do
+      json = bob.contacts.map { |c|
+               ContactPresenter.new(c, bob).full_hash_with_person
+             }.to_json
+      save_fixture(json, 'contacts_json')
+    end
+  end
+end
+
+    
+        context 'with an authenticated user' do
+      before do
+        sign_in(bob, scope: :user)
+        allow(@controller).to receive(:current_user).and_return(bob)
+      end
+    
+          def string_to_code(string)
+        # sha bytes
+        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
+        # Thanks donpark's IdenticonUtil.java for this.
+        # Match the following Java code
+        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
+        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
+    
+          def has_sidebar
+        if @sidebar
+          @sidebar.formatted_data.strip.empty? ? false : true
+        else
+          @sidebar = (@page.sidebar || false)
+          !!@sidebar
+        end
+      end
+    
+      setup do
+    @path = cloned_testpath('examples/revert.git')
+    @wiki = Gollum::Wiki.new(@path)
+    Precious::App.set(:gollum_path, @path)
+    Precious::App.set(:wiki_options, {allow_editing: true})
+  end
+    
+    
+    
+    def name
+  @name ||= Dir['*.gemspec'].first.split('.').first
+end
+    
+        # This allows packages to define flags for the fpm command line
+    def option(flag, param, help, options={}, &block)
+      @options ||= []
+      if !flag.is_a?(Array)
+        flag = [flag]
+      end
+    
+    # A pleaserun package.
+#
+# This does not currently support 'output'
+class FPM::Package::PleaseRun < FPM::Package
+  # TODO(sissel): Implement flags.
+    
+        ::Dir.mkdir(File.join(builddir, 'manifests'))
+    manifests.each do |manifest|
+      dir = File.join(builddir, 'manifests', File.dirname(manifest))
+      logger.info('manifests targeting: #{dir}')
+      ::Dir.mkdir(dir) if !File.directory?(dir)
+    
+        unless safesystem(*args)
+      raise 'Command failed while creating payload tar: #{args}'
+    end
+    payload_tar
+  end
+    
+        safesystem('tar', *args)
+  end # def output
