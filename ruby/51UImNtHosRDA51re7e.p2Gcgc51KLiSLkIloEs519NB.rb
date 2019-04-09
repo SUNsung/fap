3a904487148d267e2757b83ecdf6e03acdfb01a2
SUNsung <weatherius@gmail.com>
2019-04-10 02:57:33 +0300

    Rewrite [51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb] File in: 2019-04-10 02:57:33

--- ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
+++ ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
@@ -1,93 +1,39 @@
 
         
-        module ActionView
-  module Helpers
-    module Tags # :nodoc:
-      class CollectionRadioButtons < Base # :nodoc:
-        include CollectionHelpers
-    
-                case options[:default]
-            when nil
-              Time.current
-            when Date, Time
-              options[:default]
-            else
-              default = options[:default].dup
-    
-    module ActionView #:nodoc:
-  # = Action View PathSet
-  #
-  # This class is used to store and access paths in Action View. A number of
-  # operations are defined so that you can search among the paths in this
-  # set and also perform operations on other +PathSet+ objects.
-  #
-  # A +LookupContext+ will use a +PathSet+ to store the paths in its context.
-  class PathSet #:nodoc:
-    include Enumerable
-    
-          def with_block
-        render inline: 'Hello <%= helpery_test %>'
-      end
-    
-            context 'when command is bootstrap' do
-          let(:command) { 'bootstrap' }
-    
-            context 'when enabled' do
-          it 'should not raise if swiftlint completes with a non-zero exit status' do
-            allow(FastlaneCore::UI).to receive(:important)
-            expect(FastlaneCore::UI).to receive(:important).with(/fastlane will continue/)
-            # This is simulating the exception raised if the return code is non-zero
-            expect(Fastlane::Actions).to receive(:sh).and_raise('fake error')
-            expect(FastlaneCore::UI).to_not(receive(:user_error!))
-    
-    # To avoid 'PR & Runs' for which tests don't pass, we want to make spec errors more visible
-# The code below will run on Circle, parses the results in JSON and posts them to the PR as comment
-containing_dir = ENV['CIRCLE_TEST_REPORTS'] || '.' # for local testing
-file_path = File.join(containing_dir, 'rspec', 'fastlane-junit-results.xml')
-    
-    module Vagrant
-  # This class handles guest-OS specific interactions with a machine.
-  # It is primarily responsible for detecting the proper guest OS
-  # implementation and then delegating capabilities.
-  #
-  # Vagrant has many tasks which require specific guest OS knowledge.
-  # These are implemented using a guest/capability system. Various plugins
-  # register as 'guests' which determine the underlying OS of the system.
-  # Then, 'guest capabilities' register themselves for a specific OS (one
-  # or more), and these capabilities are called.
-  #
-  # Example capabilities might be 'mount_virtualbox_shared_folder' or
-  # 'configure_networks'.
-  #
-  # This system allows for maximum flexibility and pluginability for doing
-  # guest OS specific operations.
-  class Guest
-    include CapabilityHost
-    
-        # Accesses a machine by UUID and returns a {MachineIndex::Entry}
-    #
-    # The entry returned is locked and can't be read again or updated by
-    # this process or any other. To unlock the machine, call {#release}
-    # with the entry.
-    #
-    # You can only {#set} an entry (update) when the lock is held.
-    #
-    # @param [String] uuid UUID for the machine to access.
-    # @return [MachineIndex::Entry]
-    def get(uuid)
-      entry = nil
-    
-            # Mounts a shared folder via NFS. This assumes that the exports
-        # via the host are already done.
-        def mount_nfs(ip, folders)
-          raise BaseError, _key: :unsupported_nfs
-        end
-    
-        # Iterate over the keyspace.
-    def each(&block)
-      @items.each do |key, _|
-        yield key, get(key)
-      end
+          describe '#hook_on_project_start' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_start' }
     end
+  end
+  describe '#hook_on_project_first_start' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_first_start' }
+    end
+  end
+  describe '#hook_on_project_restart' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_restart' }
+    end
+  end
+  describe '#hook_on_project_exit' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_exit' }
+    end
+  end
+  describe '#hook_on_project_stop' do
+    it_should_behave_like 'a project hook' do
+      let(:hook_name) { 'on_project_stop' }
+    end
+  end
+end
+
     
-                eye = Vec.new(px, py, -1.0).vnormalize
+      describe '#get_pane_base_index' do
+    it 'extracts pane-base-index from the global tmux window options' do
+      allow_any_instance_of(Kernel).to receive(:`).
+        with(Regexp.new('tmux.+ show-window-option -g pane-base-index')).
+        and_return('pane-base-index 3\n')
+    
+        def send_pane_command(cmd, window_index, _pane_index)
+      send_keys(cmd, window_index)
+    end
