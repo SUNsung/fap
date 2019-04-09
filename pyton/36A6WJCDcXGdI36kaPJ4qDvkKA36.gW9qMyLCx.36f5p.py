f2943c2df330ec6bee236a10d42a96be9c5514ad
SUNsung <weatherius@gmail.com>
2019-04-09 10:07:39 +0300

    Rewrite [36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py] File in: 2019-04-09 10:07:38

--- pyton/36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py
+++ pyton/36A6WJCDcXGdI36kaPJ4qDvkKA36.gW9qMyLCx.36f5p.py
@@ -1,177 +1,87 @@
 
         
-        
-FORM_CONTENT_TYPE = 'application/x-www-form-urlencoded; charset=utf-8'
-JSON_CONTENT_TYPE = 'application/json'
-JSON_ACCEPT = '{0}, */*'.format(JSON_CONTENT_TYPE)
-DEFAULT_UA = 'HTTPie/%s' % __version__
+            def remove_user(self, user_id):
+        pass
     
-        def __init__(self, env=Environment(), **kwargs):
-    
-        package_name = '(builtin)'
+        def override_category_budget(self, category, amount):
+        self.categories_to_budget_map[category] = amount
+
     
     
-class PluginManager(object):
-    
-        '''
-    args = httpie.cli.parser.parse_args(args=[url], env=MockEnvironment())
-    assert args.auth
-    assert args.auth.username == 'username'
-    assert args.auth.password == ''
-    
-        class Plugin(AuthPlugin):
-        auth_type = 'test-require-false-yet-provided'
-        auth_require = False
-    
-        def test_GET_explicit_JSON_explicit_headers(self, httpbin):
-        r = http('--json', 'GET', httpbin.url + '/headers',
-                 'Accept:application/xml',
-                 'Content-Type:application/xml')
-        assert HTTP_OK in r
-        assert ''Accept': 'application/xml'' in r
-        assert ''Content-Type': 'application/xml'' in r
+if __name__ == '__main__':
+    HitCounts.run()
+
     
     
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
-    assert HTTP_OK in r
+if __name__ == '__main__':
+    SalesRanker.run()
+
+    
+        def steps(self):
+        '''Run the map and reduce steps.'''
+        return [
+            self.mr(mapper=self.mapper,
+                    reducer=self.reducer)
+        ]
+    
+        def process_query(self, query):
+        query = self.parse_query(query)
+        results = self.memory_cache.get(query)
+        if results is None:
+            results = self.reverse_index_cluster.process_search(query)
+            self.memory_cache.set(query, results)
+        return results
+    
+    KEY_PREFIX = 'django.contrib.sessions.cached_db'
+    
+        def create(self):
+        while True:
+            self._session_key = self._get_new_session_key()
+            try:
+                # Save immediately to ensure we have a unique entry in the
+                # database.
+                self.save(must_create=True)
+            except CreateError:
+                # Key wasn't unique. Try again.
+                continue
+            self.modified = True
+            return
     
     
-# ConcreteImplementor 1/2
-class DrawingAPI1(object):
-    def draw_circle(self, x, y, radius):
-        print('API1.circle at {}:{} radius {}'.format(x, y, radius))
+def _check_cryptography(cryptography_version):
+    # cryptography < 1.3.4
+    try:
+        cryptography_version = list(map(int, cryptography_version.split('.')))
+    except ValueError:
+        return
+    
+                        # Receive the response from the server
+                    try:
+                        # For Python 2.7, use buffering of HTTP responses
+                        r = low_conn.getresponse(buffering=True)
+                    except TypeError:
+                        # For compatibility with Python 3.3+
+                        r = low_conn.getresponse()
+    
+            assert r.status_code == 200
+        assert len(r.history) == 2
+        assert r.history[0].request.url == url
+    
+        # Gloss the lips
+    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
+    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
+    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
+    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
     
     
-class Graphic:
-    def render(self):
-        raise NotImplementedError('You should implement this.')
+def print_result(filename, location):
+    top, right, bottom, left = location
+    print('{},{},{},{},{}'.format(filename, top, right, bottom, left))
     
-        def dispatch(self, request):
-        if request.type == Request.mobile_type:
-            self.mobile_view.show_index_page()
-        elif request.type == Request.tablet_type:
-            self.tablet_view.show_index_page()
-        else:
-            print('cant dispatch the request')
+            print('I see someone named {}!'.format(name))
+
     
-    
-class TestSubject(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.s = Subject()
-        cls.dec_obs = DecimalViewer()
-        cls.hex_obs = HexViewer()
-    
-        def test_subscriber_shall_be_attachable_to_subscriptions(cls):
-        subscription = 'sub msg'
-        pro = Provider()
-        cls.assertEqual(len(pro.subscribers), 0)
-        sub = Subscriber('sub name', pro)
-        sub.subscribe(subscription)
-        cls.assertEqual(len(pro.subscribers[subscription]), 1)
-    
-        def test_bear_eng_localization(self):
-        self.assertEqual(self.e.get('bear'), 'bear')
-    
-        def test_cat_adapter_shall_make_noise(self):
-        cat = Cat()
-        cat_adapter = Adapter(cat, make_noise=cat.meow)
-        noise = cat_adapter.make_noise()
-        expected_noise = 'meow!'
-        self.assertEqual(noise, expected_noise)
-    
-        def __cut_DAG_NO_HMM(self, sentence):
-        DAG = self.tokenizer.get_DAG(sentence)
-        route = {}
-        self.tokenizer.calc(sentence, DAG, route)
-        x = 0
-        N = len(sentence)
-        buf = ''
-        while x < N:
-            y = route[x][1] + 1
-            l_word = sentence[x:y]
-            if re_eng1.match(l_word):
-                buf += l_word
-                x = y
-            else:
-                if buf:
-                    yield pair(buf, 'eng')
-                    buf = ''
-                yield pair(l_word, self.word_tag_tab.get(l_word, 'x'))
-                x = y
-        if buf:
-            yield pair(buf, 'eng')
-            buf = ''
-    
-    
-def viterbi(obs, states, start_p, trans_p, emit_p):
-    V = [{}]  # tabular
-    mem_path = [{}]
-    all_states = trans_p.keys()
-    for y in states.get(obs[0], all_states):  # init
-        V[0][y] = start_p[y] + emit_p[y].get(obs[0], MIN_FLOAT)
-        mem_path[0][y] = ''
-    for t in xrange(1, len(obs)):
-        V.append({})
-        mem_path.append({})
-        #prev_states = get_top_states(V[t-1])
-        prev_states = [
-            x for x in mem_path[t - 1].keys() if len(trans_p[x]) > 0]
-    
-    if len(sys.argv)>2:
-    n_topic = int(sys.argv[2])
-    
-        def testCutAll(self):
-        for content in test_contents:
-            result = jieba.cut(content, cut_all=True)
-            assert isinstance(result, types.GeneratorType), 'Test CutAll Generator error'
-            result = list(result)
-            assert isinstance(result, list), 'Test CutAll error on content: %s' % content
-            print(' , '.join(result), file=sys.stderr)
-        print('testCutAll', file=sys.stderr)
-    
-    
-def backup_file(directory, filename):
-    ''' Backup a given file by appending .bk to the end '''
-    logger.trace('Backing up: '%s'', filename)
-    origfile = os.path.join(directory, filename)
-    backupfile = origfile + '.bk'
-    if os.path.exists(backupfile):
-        logger.trace('Removing existing file: '%s'', backup_file)
-        os.remove(backupfile)
-    if os.path.exists(origfile):
-        logger.trace('Renaming: '%s' to '%s'', origfile, backup_file)
-        os.rename(origfile, backupfile)
-    
-    
-class Model(OriginalModel):
-    ''' Improved Autoeencoder Model '''
-    def __init__(self, *args, **kwargs):
-        logger.debug('Initializing %s: (args: %s, kwargs: %s',
-                     self.__class__.__name__, args, kwargs)
-        kwargs['input_shape'] = (64, 64, 3)
-        kwargs['encoder_dim'] = 1024
-        self.kernel_initializer = RandomNormal(0, 0.02)
-        super().__init__(*args, **kwargs)
-        logger.debug('Initialized %s', self.__class__.__name__)
-    
-            print('Test: Two or more element stack (general case)')
-        num_items = 10
-        numbers = [randint(0, 10) for x in range(num_items)]
-        sorted_stack = self.get_sorted_stack(stack, numbers)
-        sorted_numbers = []
-        for _ in range(num_items):
-            sorted_numbers.append(sorted_stack.pop())
-        assert_equal(sorted_numbers, sorted(numbers, reverse=True))
-    
-    def main():
-    testing = TestBinaryTree()
-    testing.test_insert_traversals()
-    testing.test_max_min_nodes()
-    testing.test_delete()
-    
-if __name__=='__main__':
-    main()
+        # Multi-core processing only supported on Python 3.4 or greater
+    if (sys.version_info < (3, 4)) and cpus != 1:
+        click.echo('WARNING: Multi-processing support requires Python 3.4 or greater. Falling back to single-threaded processing!')
+        cpus = 1
