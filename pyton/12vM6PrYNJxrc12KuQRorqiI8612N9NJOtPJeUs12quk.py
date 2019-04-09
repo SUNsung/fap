ac4cd93888b8c0cbedb9352760921a00b03f6616
SUNsung <weatherius@gmail.com>
2019-04-09 21:37:38 +0300

    Rewrite [12vM6PrYNJxrc12KuQRorqiI8612N9NJOtPJeUs12quk.py] File in: 2019-04-09 21:37:38

--- pyton/12vM6PrYNJxrc12KuQRorqiI8612N9NJOtPJeUs12quk.py
+++ pyton/12vM6PrYNJxrc12KuQRorqiI8612N9NJOtPJeUs12quk.py
@@ -1,308 +1,105 @@
 
         
-            ```python
-    # Consider an array of 5 labels out of a set of 3 classes {0, 1, 2}:
-    > labels
-    array([0, 2, 1, 2, 0])
-    # `to_categorical` converts this into a matrix with as many
-    # columns as there are classes. The number of rows
-    # stays the same.
-    > to_categorical(labels)
-    array([[ 1.,  0.,  0.],
-           [ 0.,  0.,  1.],
-           [ 0.,  1.,  0.],
-           [ 0.,  0.,  1.],
-           [ 1.,  0.,  0.]], dtype=float32)
-    ```
+            def save(self, must_create=False):
+        '''
+        To save, get the session key as a securely signed string and then set
+        the modified flag so that the cookie is set on the client for the
+        current request.
+        '''
+        self._session_key = self._get_session_key()
+        self.modified = True
+    
+    
+class SessionManager(BaseSessionManager):
+    use_in_migrations = True
+    
+        app.add_node(settingslist_node)
+    app.add_directive('settingslist', SettingsListDirective)
+    
+        default_settings = {
+        'LOG_LEVEL': 'INFO',
+        'LOGSTATS_INTERVAL': 1,
+        'CLOSESPIDER_TIMEOUT': 10,
+    }
+    
+    
+class Command(ScrapyCommand):
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('--spider', dest='spider', default=None,
+            help='use this spider without looking for one')
+        parser.add_option('-a', dest='spargs', action='append', default=[], metavar='NAME=VALUE',
+            help='set spider argument (may be repeated)')
+        parser.add_option('--pipelines', action='store_true',
+            help='process items through pipelines')
+        parser.add_option('--nolinks', dest='nolinks', action='store_true',
+            help='don't show links to follow (extracted requests)')
+        parser.add_option('--noitems', dest='noitems', action='store_true',
+            help='don't show scraped items')
+        parser.add_option('--nocolour', dest='nocolour', action='store_true',
+            help='avoid using pygments to colorize the output')
+        parser.add_option('-r', '--rules', dest='rules', action='store_true',
+            help='use CrawlSpider rules to discover the callback')
+        parser.add_option('-c', '--callback', dest='callback',
+            help='use this callback for parsing, instead looking for a callback')
+        parser.add_option('-m', '--meta', dest='meta',
+            help='inject extra meta into the Request, it must be a valid raw json string')
+        parser.add_option('-d', '--depth', dest='depth', type='int', default=1,
+            help='maximum depth for parsing requests [default: %default]')
+        parser.add_option('-v', '--verbose', dest='verbose', action='store_true',
+            help='print each depth level one by one')
+    
+            e.g.:
+        @returns request
+        @returns request 2
+        @returns request 2 10
+        @returns request 0 10
     '''
     
-    x_train = x_train.reshape(x_train.shape[0], -1, 1)
-x_test = x_test.reshape(x_test.shape[0], -1, 1)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+            cookies = [self._format_cookie(x) for x in cookie_list]
+        headers = {'Set-Cookie': cookies}
+        response = Response(request.url, headers=headers)
     
-    # input image dimensions
-img_rows, img_cols = 28, 28
-# number of convolutional filters to use
-filters = 32
-# size of pooling area for max pooling
-pool_size = 2
-# convolution kernel size
-kernel_size = 3
+    from . import case, suite, util
     
-    print('Building model...')
-model = Sequential()
-model.add(Dense(512, input_shape=(max_words,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
-    
-        # Arguments
-        y_true: tensor of true targets.
-        y_pred: tensor of predicted targets.
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-        result = dict(changed=False, rules_purged=0)
-    
-        ipa_dnszone = client.dnszone_find(zone_name)
-    
-                if re.search('already enabled', out) is None:
-                change_applied = True
-        else:
-            if rc == 0:
-                change_applied = True
-            # RC is not 0 for this already disabled feature, handle it as no change applied
-            elif re.search('Cannot disable feature '%s'. Target file .* does not exist' % self.feature_name, out):
-                change_applied = False
-            else:
-                self.module.fail_json(msg='Failed to disable feature. Command returns %s' % out)
-    
-        MONIT = module.get_bin_path('monit', True)
-    
-    
-FILEPATH = os.path.join(
-    os.path.abspath(os.path.dirname(os.path.dirname(__file__))), 'blns.txt')
-'''Path to the file'''
-    
-                self.ip_manager.report_connect_fail(ip, force_remove=True)
-    
-    import simple_http_client
-from xlog import getLogger
-xlog = getLogger('gae_proxy')
-    
-    # [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    
-            LA(1) is not what we are looking for.  If LA(2) has the right token,
-        however, then assume LA(1) is some extra spurious token.  Delete it
-        and LA(2) as if we were doing a normal match(), which advances the
-        input.
-    
-    The problem is  :
-Given an ARRAY, to find the longest and increasing sub ARRAY in that given ARRAY and return it.
-Example: [10, 22, 9, 33, 21, 50, 41, 60, 80] as input will return [10, 22, 33, 41, 60, 80] as output
-'''
-from __future__ import print_function
-    
-    
-class SubArray:
-    
-        def _AugAssign(self, t):
-        self.fill()
-        self.dispatch(t.target)
-        self.write(' '+self.binop[t.op.__class__.__name__]+'= ')
-        self.dispatch(t.value)
-    
-        # Test an invalid call (bpo-34125)
-    def test_unbound_method_invalid_args(self):
-        kwargs = {}
+        def test_exception_in_except_clause(self):
         def f(p):
-            dict.get(print, 42, **kwargs)
+            1/0
+        def g(p):
+            try:
+                f(p)
+            except:
+                try: f(p)
+                except: pass
         f_ident = ident(f)
-        self.check_events(f, [(1, 'call', f_ident),
-                              (1, 'return', f_ident)])
+        g_ident = ident(g)
+        self.check_events(g, [(1, 'call', g_ident),
+                              (2, 'call', f_ident),
+                              (2, 'return', f_ident),
+                              (3, 'call', f_ident),
+                              (3, 'return', f_ident),
+                              (1, 'return', g_ident),
+                              ])
     
+    import os
+import subprocess
+import sys
+import sysconfig
+import tempfile
+import unittest
+from test import support
+from test.support.script_helper import (
+    spawn_python, kill_python, assert_python_ok, assert_python_failure,
+    interpreter_requires_environment
+)
     
-# Are two filenames really pointing to the same file?
-def samefile(f1, f2):
-    '''Test whether two pathnames reference the same actual file'''
-    s1 = os.stat(f1)
-    s2 = os.stat(f2)
-    return samestat(s1, s2)
+        def testSeekForwardAcrossStreams(self):
+        self.createTempFile(streams=2)
+        with BZ2File(self.filename) as bz2f:
+            self.assertRaises(TypeError, bz2f.seek)
+            bz2f.seek(len(self.TEXT) + 150)
+            self.assertEqual(bz2f.read(), self.TEXT[150:])
     
-        @unittest.skipUnless((sys.platform == 'darwin' or
-                support.is_android), 'test specific to Mac OS X and Android')
-    def test_osx_android_utf8(self):
-        def check_output(text):
-            decoded = text.decode('utf-8', 'surrogateescape')
-            expected = ascii(decoded).encode('ascii') + b'\n'
-    
-    # Create the base text message.
-msg = EmailMessage()
-msg['Subject'] = 'Ayons asperges pour le déjeuner'
-msg['From'] = Address('Pepé Le Pew', 'pepe', 'example.com')
-msg['To'] = (Address('Penelope Pussycat', 'penelope', 'example.com'),
-             Address('Fabrette Pussycat', 'fabrette', 'example.com'))
-msg.set_content('''\
-Salut!
-    
-        f1 = manager.Foo1()
-    f1.f()
-    f1.g()
-    assert not hasattr(f1, '_h')
-    assert sorted(f1._exposed_) == sorted(['f', 'g'])
-    
-        ep = 'http://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={}'
-    
-        for quality in ['1080','720','480','380','240','144','auto']:
-        try:
-            real_url = info[quality][1]['url']
-            if real_url:
-                break
-        except KeyError:
-            pass
-    
-        elif 'subject' in url:
-        titles = re.findall(r'data-title='([^']*)'>', html)
-        song_id = re.findall(r'<li class='song-item' id='([^']*)'', html)
-        song_ssid = re.findall(r'data-ssid='([^']*)'', html)
-        get_song_url = 'http://music.douban.com/j/songlist/get_song_url'
-    
-        fc2video_download_by_upid(upid, output_dir, merge, info_only)
-    
-    import json
-import urllib.parse
-import base64
-import binascii
-import re
-    
-        def basic_extract(self):
-        self.get_title()
-        self.get_flashvars()
-        api_req_url = '{}?{}'.format(self.flashvars['IService'], parse.urlencode(self.flashvars))
-        self.api_req(api_req_url)
-    
-    from ..common import *
-    
-    
-def _test():
-    ''''''
-    file_path = r'./data.txt'
-    
-            W, b = get_wb([n_step, n_step])
-        a = softmax(tf.matmul(a, W) + b)
-        a = tf.reshape(a, [-1, n_input, n_step])  # [batch_size, n_input, n_step]
-    
-            with tf.variable_scope('transform'):
-            W_T, b_T = get_wb([n_input, n_input], b_initializer=tf.initializers.constant(carry_bias))
-    
-    import tensorflow as tf
-import keras.backend as K
-    
-    # 构建 FastText 模型
-sentences = [['Hello', 'World', '!'], ['I', 'am', 'huay', '.']]
-min_ngrams, max_ngrams = 2, 4  # ngrams 范围
-model = FastText(sentences, size=5, min_count=1, min_n=min_ngrams, max_n=max_ngrams)
-    
-    
-def filter_for_training(roidb):
-    '''Remove roidb entries that have no usable RoIs based on config settings.
-    '''
-    def is_valid(entry):
-        # Valid images have:
-        #   (1) At least one foreground RoI OR
-        #   (2) At least one background RoI
-        overlaps = entry['max_overlaps']
-        # find boxes with sufficient overlap
-        fg_inds = np.where(overlaps >= cfg.TRAIN.FG_THRESH)[0]
-        # Select background RoIs as those within [BG_THRESH_LO, BG_THRESH_HI)
-        bg_inds = np.where((overlaps < cfg.TRAIN.BG_THRESH_HI) &
-                           (overlaps >= cfg.TRAIN.BG_THRESH_LO))[0]
-        # image is only valid if such boxes exist
-        valid = len(fg_inds) > 0 or len(bg_inds) > 0
-        if cfg.MODEL.KEYPOINTS_ON:
-            # If we're training for keypoints, exclude images with no keypoints
-            valid = valid and entry['has_visible_keypoints']
-        return valid
-    
-        # Check if we need the P6 feature map
-    if not cfg.FPN.EXTRA_CONV_LEVELS and max_level == HIGHEST_BACKBONE_LVL + 1:
-        # Original FPN P6 level implementation from our CVPR'17 FPN paper
-        P6_blob_in = blobs_fpn[0]
-        P6_name = P6_blob_in + '_subsampled_2x'
-        # Use max pooling to simulate stride 2 subsampling
-        P6_blob = model.MaxPool(P6_blob_in, P6_name, kernel=1, pad=0, stride=2)
-        blobs_fpn.insert(0, P6_blob)
-        spatial_scales.insert(0, spatial_scales[0] * 0.5)
-    
-    ... -> RoI ----\                               /-> box cls output -> cls loss
-                -> RoIFeatureXform -> box head
-... -> Feature /                               \-> box reg output -> reg loss
-       Map
-    
-    from caffe2.python import muji
-    
-    
-def add_fpn_retinanet_outputs(model, blobs_in, dim_in, spatial_scales):
-    '''RetinaNet head. For classification and box regression, we can chose to
-    have the same conv tower or a separate tower. 'bl_feat_list' stores the list
-    of feature blobs for bbox prediction. These blobs can be shared cls feature
-    blobs if we share the tower or else are independent blobs.
-    '''
-    dim_out = dim_in
-    k_max = cfg.FPN.RPN_MAX_LEVEL  # coarsest level of pyramid
-    k_min = cfg.FPN.RPN_MIN_LEVEL  # finest level of pyramid
-    A = len(cfg.RETINANET.ASPECT_RATIOS) * cfg.RETINANET.SCALES_PER_OCTAVE
-    
-    
-def collect(inputs, is_training):
-    cfg_key = 'TRAIN' if is_training else 'TEST'
-    post_nms_topN = cfg[cfg_key].RPN_POST_NMS_TOP_N
-    k_max = cfg.FPN.RPN_MAX_LEVEL
-    k_min = cfg.FPN.RPN_MIN_LEVEL
-    num_lvls = k_max - k_min + 1
-    roi_inputs = inputs[:num_lvls]
-    score_inputs = inputs[num_lvls:]
-    if is_training:
-        score_inputs = score_inputs[:-2]
-    
-    
-def compute_targets(ex_rois, gt_rois, weights=(1.0, 1.0, 1.0, 1.0)):
-    '''Compute bounding-box regression targets for an image.'''
-    return box_utils.bbox_transform_inv(ex_rois, gt_rois, weights).astype(
-        np.float32, copy=False
-    )
-
-    
-        def _distribute_rois_over_fpn_levels(rois_blob_name):
-        '''Distribute rois over the different FPN levels.'''
-        # Get target level for each roi
-        # Recall blob rois are in (batch_idx, x1, y1, x2, y2) format, hence take
-        # the box coordinates from columns 1:5
-        target_lvls = fpn.map_rois_to_fpn_levels(
-            blobs[rois_blob_name][:, 1:5], lvl_min, lvl_max
-        )
-        # Add per FPN level roi blobs named like: <rois_blob_name>_fpn<lvl>
-        fpn.add_multilevel_roi_blobs(
-            blobs, rois_blob_name, blobs[rois_blob_name], target_lvls, lvl_min,
-            lvl_max
-        )
-    
-        filenames = []
-    for (dirpath, dnames, fnames) in os.walk(path):
-        for fname in fnames:
-            if fname.endswith('.md'):
-                filenames.append(os.sep.join([dirpath, fname]))
+    from email.message import EmailMessage
+from email.policy import SMTP
