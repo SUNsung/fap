37e8d7da75684a540e023dcd01d341673376c3be
SUNsung <weatherius@gmail.com>
2019-04-10 13:33:48 +0300

    Rewrite [22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py] File in: 2019-04-10 13:33:48

--- pyton/22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py
+++ pyton/22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py
@@ -1,64 +1,202 @@
 
         
-        
-class Vehicle(metaclass=ABCMeta):
+                    self.handler_results.append(handler_result)
     
-            When updating an entry, updates its position to the front of the LRU list.
-        If the entry is new and the cache is at capacity, removes the oldest entry
-        before the new entry is added.
-        '''
-        node = self.map[query]
-        if node is not None:
-            # Key exists in cache, update the value
-            node.results = results
-            self.linked_list.move_to_front(node)
-        else:
-            # Key does not exist in cache
-            if self.size == self.MAX_SIZE:
-                # Remove the oldest entry from the linked list and lookup
-                self.lookup.pop(self.linked_list.tail.query, None)
-                self.linked_list.remove_from_tail()
-            else:
-                self.size += 1
-            # Add the new key and value
-            new_node = Node(query, results)
-            self.linked_list.append_to_front(new_node)
-            self.lookup[query] = new_node
+            Keyword:                   'bold #004461',   # class: 'k'
+        Keyword.Constant:          'bold #004461',   # class: 'kc'
+        Keyword.Declaration:       'bold #004461',   # class: 'kd'
+        Keyword.Namespace:         'bold #004461',   # class: 'kn'
+        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
+        Keyword.Reserved:          'bold #004461',   # class: 'kr'
+        Keyword.Type:              'bold #004461',   # class: 'kt'
+    
+    
+def info():
+    '''Generate information for a bug report.'''
+    try:
+        platform_info = {
+            'system': platform.system(),
+            'release': platform.release(),
+        }
+    except IOError:
+        platform_info = {
+            'system': 'Unknown',
+            'release': 'Unknown',
+        }
+    
+    # TODO: response is the only one
+    
+        def prepare_content_length(self, body):
+        '''Prepare Content-Length header based on request method and body'''
+        if body is not None:
+            length = super_len(body)
+            if length:
+                # If length exists, set it. Otherwise, we fallback
+                # to Transfer-Encoding: chunked.
+                self.headers['Content-Length'] = builtin_str(length)
+        elif self.method not in ('GET', 'HEAD') and self.headers.get('Content-Length') is None:
+            # Set Content-Length to 0 for methods that can have a body
+            # but don't provide one. (i.e. not GET or HEAD)
+            self.headers['Content-Length'] = '0'
+    
+                try:
+                self.working_appid_list.remove(appid)
+            except:
+                pass
+    
+        @staticmethod
+    def import_ca(certfile):
+        xlog.debug('Importing CA')
+        commonname = 'GoAgent XX-Net - GoAgent' #TODO: here should be GoAgent - XX-Net
+        if sys.platform.startswith('win'):
+            CertUtil.import_windows_ca(certfile)
+        elif sys.platform == 'darwin':
+            CertUtil.import_mac_ca(commonname, certfile)
+        elif sys.platform.startswith('linux'):
+            CertUtil.import_linux_ca(commonname, certfile)
+            CertUtil.import_linux_firefox_ca(commonname, certfile)
+            #CertUtil.import_ubuntu_system_ca(commonname, certfile) # we don't need install CA to system root, special user is enough
+    
+            if self.continue_fail_count > 10:
+            # don't set network_stat to 'unknown', wait for check
+            # network_stat = 'unknown'
+            xlog.debug('report_connect_fail %s continue_fail_count:%d',
+                       self.type, self.continue_fail_count)
+            self.triger_check_network(True)
+    
+        value = lp[1].strip()
+    return value
 
     
-    def print_title(title, pattern = '-'):
-    print('\n'.join(('', title, pattern * len(title)))) 
+        deadline = URLFETCH_TIMEOUT
+    validate_certificate = bool(int(kwargs.get('validate', 0)))
+    accept_encoding = headers.get('Accept-Encoding', '')
+    errors = []
+    for i in xrange(int(kwargs.get('fetchmax', URLFETCH_MAX))):
+        try:
+            response = urlfetch.fetch(url, payload, fetchmethod, headers, allow_truncated=False, follow_redirects=False, deadline=deadline, validate_certificate=validate_certificate)
+            break
+        except apiproxy_errors.OverQuotaError as e:
+            time.sleep(5)
+        except urlfetch.DeadlineExceededError as e:
+            errors.append('%r, deadline=%s' % (e, deadline))
+            logging.error('DeadlineExceededError(deadline=%s, url=%r)', deadline, url)
+            time.sleep(1)
+            deadline = URLFETCH_TIMEOUT * 2
+        except urlfetch.DownloadError as e:
+            errors.append('%r, deadline=%s' % (e, deadline))
+            logging.error('DownloadError(deadline=%s, url=%r)', deadline, url)
+            time.sleep(1)
+            deadline = URLFETCH_TIMEOUT * 2
+        except urlfetch.ResponseTooLargeError as e:
+            errors.append('%r, deadline=%s' % (e, deadline))
+            response = e.response
+            logging.error('ResponseTooLargeError(deadline=%s, url=%r) response(%r)', deadline, url, response)
+            m = re.search(r'=\s*(\d+)-', headers.get('Range') or headers.get('range') or '')
+            if m is None:
+                headers['Range'] = 'bytes=0-%d' % int(kwargs.get('fetchmaxsize', URLFETCH_MAXSIZE))
+            else:
+                headers.pop('Range', '')
+                headers.pop('range', '')
+                start = int(m.group(1))
+                headers['Range'] = 'bytes=%s-%d' % (start, start+int(kwargs.get('fetchmaxsize', URLFETCH_MAXSIZE)))
+            deadline = URLFETCH_TIMEOUT * 2
+        except urlfetch.SSLCertificateError as e:
+            errors.append('%r, should validate=0 ?' % e)
+            logging.error('%r, deadline=%s', e, deadline)
+        except Exception as e:
+            errors.append(str(e))
+            if i == 0 and method == 'GET':
+                deadline = URLFETCH_TIMEOUT * 2
+    else:
+        start_response('500 Internal Server Error', [('Content-Type', 'text/html')])
+        error_string = '<br />\n'.join(errors)
+        if not error_string:
+            logurl = 'https://appengine.google.com/logs?&app_id=%s' % os.environ['APPLICATION_ID']
+            error_string = 'Internal Server Error. <p/>try <a href='javascript:window.location.reload(true);'>refresh</a> or goto <a href='%s' target='_blank'>appengine.google.com</a> for details' % logurl
+        yield message_html('502 Urlfetch Error', 'Python Urlfetch Error: %r' % method,  error_string)
+        raise StopIteration
     
-    from homeassistant.components.device_tracker import DOMAIN
+        '''
+    return_vars = {}
+    # Get list of words in the variable
+    a_opts = util.get_var_from_file(varname, filepath).split()
+    for i, v in enumerate(a_opts):
+        # Handle Define statements and make sure it has an argument
+        if v == '-D' and len(a_opts) >= i+2:
+            var_parts = a_opts[i+1].partition('=')
+            return_vars[var_parts[0]] = var_parts[2]
+        elif len(v) > 2 and v.startswith('-D'):
+            # Found var with no whitespace separator
+            var_parts = v[2:].partition('=')
+            return_vars[var_parts[0]] = var_parts[2]
+    return return_vars
     
-            self._userid = None
+            self.assertEqual(None, self._call(self.vhosts))
     
-    _DEVICES_REGEX = re.compile(
-    r'(?P<mac>(([0-9a-f]{2}[:-]){5}([0-9a-f]{2})))\s'
-    r'(?P<ip>([0-9]{1,3}[\.]){3}[0-9]{1,3})\s+'
-    r'(?P<status>([^\s]+))\s+'
-    r'(?P<type>([^\s]+))\s+'
-    r'(?P<intf>([^\s]+))\s+'
-    r'(?P<hwintf>([^\s]+))\s+'
-    r'(?P<host>([^\s]+))')
+        def test_repr(self):
+        self.assertEqual(repr(self.addr2), 'certbot_apache.obj.Addr(('127.0.0.1', '443'))')
+    
+        def test_printer_buttons(self):
+        '''Test buttons whose commands use printer function.'''
+        dialog = self.dialog
+        button_sources = [(dialog.py_license, license, 'license'),
+                          (dialog.py_copyright, copyright, 'copyright'),
+                          (dialog.py_credits, credits, 'credits')]
     
     
-def get_service(hass, config, discovery_info=None):
-    '''Get the Command Line notification service.'''
-    command = config[CONF_COMMAND]
+def main():
+    import io
+    import pprint
     
-    For more details about this platform, please refer to the documentation
-https://home-assistant.io/components/demo/
+    # Create the base text message.
+msg = EmailMessage()
+msg['Subject'] = 'Ayons asperges pour le déjeuner'
+msg['From'] = Address('Pepé Le Pew', 'pepe', 'example.com')
+msg['To'] = (Address('Penelope Pussycat', 'penelope', 'example.com'),
+             Address('Fabrette Pussycat', 'fabrette', 'example.com'))
+msg.set_content('''\
+Salut!
+    
+    
+def get_github_url(app, view, path):
+    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
+    return (
+        github_fmt.format(app.config.edit_on_github_project, view,
+                          app.config.edit_on_github_branch,
+                          app.config.edit_on_github_src_path, path))
+    
+    
+def load_data(file_path):
+    '''加载数据
+        源数据格式为多行，每行为两个浮点数，分别表示 (x,y)
+    '''
+    data = []
+    with open(file_path, 'r', encoding='utf-8') as fr:
+        for line in fr.read().splitlines():
+            line_float = list(map(float, line.split('\t')))
+            data.append(line_float)
+    data = np.array(data)
+    return data
+    
+            o = tf.nn.conv2d(x, W, strides=strides, padding=padding) + b
+        o = act_fn(o)
+    
+    
+def repeat(x, n):
+    '''
+    Examples:
+        x.shape == [batch_size, n_input]
+        x = repeat(x, n_step)
+        x.shape == [batch_size, n_step, n_input]
+    
+    to perform the renaming:
+  head_builder.add_roi_2mlp_head => fast_rcnn_heads.add_roi_2mlp_head
 '''
-from homeassistant.components.notify import BaseNotificationService
     
-    import homeassistant.util.dt as dt_util
-from homeassistant.components.notify import (
-    ATTR_TITLE, ATTR_TITLE_DEFAULT, PLATFORM_SCHEMA, BaseNotificationService)
-from homeassistant.const import CONF_FILENAME
-import homeassistant.helpers.config_validation as cv
     
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_USERNAME): cv.string,
-    vol.Required(CONF_ACCESS_TOKEN): cv.string,
-})
+def compute_targets(ex_rois, gt_rois, weights=(1.0, 1.0, 1.0, 1.0)):
+    '''Compute bounding-box regression targets for an image.'''
+    return box_utils.bbox_transform_inv(ex_rois, gt_rois, weights).astype(
+        np.float32, copy=False
+    )
