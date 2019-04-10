4f58fe6f5d5055a2b2ff080d1c2717c7665ad3b4
SUNsung <weatherius@gmail.com>
2019-04-10 04:47:59 +0300

    Rewrite [13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb] File in: 2019-04-10 04:47:58

--- ruby/13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb
+++ ruby/13qKUbUkofDvo13znpvhSCB3Zw13OGx7OEns7s13PJS.rb
@@ -1,120 +1,211 @@
 
         
-            Thread.pass until running
-    Thread.pass while t.status and t.status != 'sleep'
+            def limit_reached_for(user_id, date)
+      GivenDailyLike.find_for(user_id, date).pluck(:limit_reached)[0] || false
+    end
     
-        if run? && ARGV.any?
-      require 'optparse'
-      OptionParser.new { |op|
-        op.on('-p port',   'set the port (default is 4567)')                { |val| set :port, Integer(val) }
-        op.on('-o addr',   'set the host (default is #{bind})')             { |val| set :bind, val }
-        op.on('-e env',    'set the environment (default is development)')  { |val| set :environment, val.to_sym }
-        op.on('-s server', 'specify rack server/handler (default is thin)') { |val| set :server, val }
-        op.on('-q',        'turn on quiet mode (default is off)')           {       set :quiet, true }
-        op.on('-x',        'turn on the mutex lock (default is off)')       {       set :lock, true }
-      }.parse!(ARGV.dup)
+      describe '.seed' do
+    it 'imports a set of agents to get the user going when they are first created' do
+      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+    end
+    
+              agent_diffs = scenario_import.agent_diffs
+    
+        it 'updates Agents' last_error_log_at when an error is logged' do
+      AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 3, :outbound_event => events(:jane_website_agent_event))
+      expect(agents(:jane_website_agent).reload.last_error_log_at).to be_nil
+    
+      before(:each) do
+    stub_request(:get, /events.json$/).to_return(
+      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
+      :status => 200,
+      :headers => {'Content-Type' => 'text/json'}
+    )
+    stub_request(:get, /projects.json$/).to_return(
+      :body => JSON.dump([{name: 'test', id: 1234},{name: 'test1', id: 1235}]),
+      :status => 200,
+      :headers => {'Content-Type' => 'text/json'}
+    )
+    stub_request(:get, /02:00$/).to_return(
+      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
+      :status => 200,
+      :headers => {'Content-Type' => 'text/json'}
+    )
+    @valid_params = { :project_id => 6789 }
+    
+    protected
+    
+    
+  #
+  # Payload types were copied from xCAT-server source code (IPMI.pm)
+  #
+  RMCP_ERRORS = {
+    1 => 'Insufficient resources to create new session (wait for existing sessions to timeout)',
+    2 => 'Invalid Session ID', #this shouldn't occur...
+    3 => 'Invalid payload type',#shouldn't occur..
+    4 => 'Invalid authentication algorithm', #if this happens, we need to enhance our mechanism for detecting supported auth algorithms
+    5 => 'Invalid integrity algorithm', #same as above
+    6 => 'No matching authentication payload',
+    7 => 'No matching integrity payload',
+    8 => 'Inactive Session ID', #this suggests the session was timed out while trying to negotiate, shouldn't happen
+    9 => 'Invalid role',
+    0xa => 'Unauthorised role or privilege level requested',
+    0xb => 'Insufficient resources to create a session at the requested role',
+    0xc => 'Invalid username length',
+    0xd => 'Unauthorized name',
+    0xe => 'Unauthorized GUID',
+    0xf => 'Invalid integrity check value',
+    0x10 => 'Invalid confidentiality algorithm',
+    0x11 => 'No cipher suite match with proposed security algorithms',
+    0x12 => 'Illegal or unrecognized parameter', #have never observed this, would most likely mean a bug in xCAT or IPMI device
+  }
+    
+            end
+      end
+    end
+  end
+end
+
+    
+    module Rex
+  module Proto
+    module Kerberos
+      module Model
+        # This class provides a representation of a Kerberos KDC-REQ-BODY (request body) data
+        # definition
+        class KdcRequestBody < Element
+          # @!attribute options
+          #   @return [Integer] The ticket flags
+          attr_accessor :options
+          # @!attribute cname
+          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
+          attr_accessor :cname
+          # @!attribute realm
+          #   @return [String] The realm part of the server's principal identifier
+          attr_accessor :realm
+          # @!attribute sname
+          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the server's identity
+          attr_accessor :sname
+          # @!attribute from
+          #   @return [Time] Start time when the ticket is to be postdated
+          attr_accessor :from
+          # @!attribute till
+          #   @return [Time] Expiration date requested by the client
+          attr_accessor :till
+          # @!attribute rtime
+          #   @return [Time] Optional requested renew-till time
+          attr_accessor :rtime
+          # @!attribute nonce
+          #   @return [Integer] random number
+          attr_accessor :nonce
+          # @!attribute etype
+          #   @return [Array<Integer>] The desired encryption algorithm to be used in the response
+          attr_accessor :etype
+          # @!attribute enc_auth_data
+          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] An encoding of the desired authorization-data encrypted
+          attr_accessor :enc_auth_data
+    
+              # Decodes the pvno from an OpenSSL::ASN1::ASN1Data
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Integer]
+          def decode_pvno(input)
+            input.value[0].value.to_i
+          end
+    
+              # Decodes a Rex::Proto::Kerberos::Model::LastReque from an String
+          #
+          # @param input [String] the input to decode from
+          def decode_string(input)
+            asn1 = OpenSSL::ASN1.decode(input)
+    
+      it 'is unaffected by overriding object_id' do
+    o1 = mock('object')
+    o1.stub!(:object_id).and_return(10)
+    o2 = mock('object')
+    o2.stub!(:object_id).and_return(10)
+    o1.equal?(o2).should be_false
+  end
+    
+      it 'raises a TypeError when defining methods on numerics' do
+    lambda do
+      (1.0).instance_eval { def foo; end }
+    end.should raise_error(TypeError)
+    lambda do
+      (1 << 64).instance_eval { def foo; end }
+    end.should raise_error(TypeError)
+  end
+    
+      it 'passes any arguments to the block' do
+    Object.new.instance_exec(1,2) {|one, two| one + two}.should == 3
+  end
+    
+    describe 'BasicObject#method_missing' do
+  it_behaves_like :method_missing_class, nil, BasicObject
+end
+    
+      describe 'when invoked using Kernel#send' do
+    it 'returns true if other is not identical to self' do
+      a = Object.new
+      b = Object.new
+      a.send(:!=, b).should be_true
+    end
+    
+      it 'uses the __FILE__ that is passed in' do
+    bind = BindingSpecs::Demo.new(1).get_binding
+    bind.eval('__FILE__', '(test)').should == '(test)'
+  end
+    
+      it 'returns true when a regular local variable is defined' do
+    foo = 10
+    binding.local_variable_defined?(:foo).should == true
+  end
+    
+      if config.log_to.include? 'file'
+    # Configure an appender that will write log events to a file.
+    if AppConfig.environment.logging.logrotate.enable?
+      # The file will be rolled on a daily basis, and the rolled files will be kept
+      # the configured number of days. Older files will be deleted. The default pattern
+      # layout is used when formatting log events into strings.
+      Logging.appenders.rolling_file('file',
+                                     filename:      config.paths['log'].first,
+                                     keep:          AppConfig.environment.logging.logrotate.days.to_i,
+                                     age:           'daily',
+                                     truncate:      false,
+                                     auto_flushing: true,
+                                     layout:        layout
+                                    )
+    else
+      # No file rolling, use logrotate to roll the logfile.
+      Logging.appenders.file('file',
+                             filename:      config.paths['log'].first,
+                             truncate:      false,
+                             auto_flushing: true,
+                             layout:        layout
+                            )
     end
   end
     
-        <div id='backtrace' class='condensed'>
-      <h3>BACKTRACE</h3>
-      <p><a href='#' id='expando'
-            onclick='toggleBacktrace(); return false'>(expand)</a></p>
-      <p id='nav'><strong>JUMP TO:</strong>
-         <% unless bad_request?(exception) %>
-            <a href='#get-info'>GET</a>
-            <a href='#post-info'>POST</a>
-         <% end %>
-         <a href='#cookie-info'>COOKIES</a>
-         <a href='#env-info'>ENV</a>
-      </p>
-      <div class='clear'></div>
+        change.down do
+      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
+      Mention.where(mentions_container_type: 'Comment').destroy_all
+      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
+    end
+  end
+end
+
     
-      # fetch data
-  fields = {
-    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
-    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
-    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
-  }
+      describe '#new' do
+    before do
+      sign_in alice, scope: :user
+    end
     
-          def mask_authenticity_token(session)
-        token = set_token(session)
-        mask_token(token)
+        context 'on my own post' do
+      before do
+        aspect_to_post = alice.aspects.where(:name => 'generic').first
+        @post = alice.post :status_message, :text => 'something', :to => aspect_to_post
       end
     
-    module Rack
-  module Protection
-    class Base
-      DEFAULT_OPTIONS = {
-        :reaction    => :default_reaction, :logging   => true,
-        :message     => 'Forbidden',       :encryptor => Digest::SHA1,
-        :session_key => 'rack.session',    :status    => 403,
-        :allow_empty_referrer => true,
-        :report_key           => 'protection.failed',
-        :html_types           => %w[text/html application/xhtml text/xml application/xml]
-      }
-    
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   XSS and others
-    # Supported browsers:: Firefox 23+, Safari 7+, Chrome 25+, Opera 15+
-    #
-    # Description:: Content Security Policy, a mechanism web applications
-    #               can use to mitigate a broad class of content injection
-    #               vulnerabilities, such as cross-site scripting (XSS).
-    #               Content Security Policy is a declarative policy that lets
-    #               the authors (or server administrators) of a web application
-    #               inform the client about the sources from which the
-    #               application expects to load resources.
-    #
-    # More info::   W3C CSP Level 1 : https://www.w3.org/TR/CSP1/ (deprecated)
-    #               W3C CSP Level 2 : https://www.w3.org/TR/CSP2/ (current)
-    #               W3C CSP Level 3 : https://www.w3.org/TR/CSP3/ (draft)
-    #               https://developer.mozilla.org/en-US/docs/Web/Security/CSP
-    #               http://caniuse.com/#search=ContentSecurityPolicy
-    #               http://content-security-policy.com/
-    #               https://securityheaders.io
-    #               https://scotthelme.co.uk/csp-cheat-sheet/
-    #               http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-    #
-    # Sets the 'Content-Security-Policy[-Report-Only]' header.
-    #
-    # Options: ContentSecurityPolicy configuration is a complex topic with
-    #          several levels of support that has evolved over time.
-    #          See the W3C documentation and the links in the more info
-    #          section for CSP usage examples and best practices. The
-    #          CSP3 directives in the 'NO_ARG_DIRECTIVES' constant need to be
-    #          presented in the options hash with a boolean 'true' in order
-    #          to be used in a policy.
-    #
-    class ContentSecurityPolicy < Base
-      default_options default_src: :none, script_src: ''self'',
-                      img_src: ''self'', style_src: ''self'',
-                      connect_src: ''self'', report_only: false
-    
-          def call(env)
-        request  = Request.new(env)
-        get_was  = handle(request.GET)
-        post_was = handle(request.POST) rescue nil
-        app.call env
-      ensure
-        request.GET.replace  get_was  if get_was
-        request.POST.replace post_was if post_was
-      end
-    
-          def create_worker_file
-        template 'worker.rb.erb', File.join('app/workers', class_path, '#{file_name}_worker.rb')
-      end
-    
-          def perform(yml)
-        (target, method_name, args) = YAML.load(yml)
-        msg = target.public_send(method_name, *args)
-        # The email method can return nil, which causes ActionMailer to return
-        # an undeliverable empty message.
-        if msg
-          deliver(msg)
-        else
-          raise '#{target.name}##{method_name} returned an undeliverable mail object'
-        end
-      end
+        desc 'Build and install all of the gems as local gems'
+    task :all => GEMS_AND_ROOT_DIRECTORIES.keys
+  end
