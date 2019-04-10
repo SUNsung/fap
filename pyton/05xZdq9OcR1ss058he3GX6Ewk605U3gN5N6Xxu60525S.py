f5da898f75a8b3bf33c4ba3f07434282e9644401
SUNsung <weatherius@gmail.com>
2019-04-10 03:18:40 +0300

    Rewrite [05xZdq9OcR1ss058he3GX6Ewk605U3gN5N6Xxu60525S.py] File in: 2019-04-10 03:18:40

---
+++ pyton/05xZdq9OcR1ss058he3GX6Ewk605U3gN5N6Xxu60525S.py
@@ -0,0 +1,210 @@
+
+        
+        from mrjob.job import MRJob
+    
+    
+class PersonServer(object):
+    
+            if error is None:
+            # the name is available, store it in the database and go to
+            # the login page
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+    
+        return g.db
+    
+        :copyright: © 2010 by the Pallets team.
+    :license: BSD, see LICENSE for more details.
+'''
+    
+    
+@pytest.fixture
+def site_packages(modules_tmpdir, monkeypatch):
+    '''Create a fake site-packages.'''
+    rv = modules_tmpdir \
+        .mkdir('lib') \
+        .mkdir('python{x[0]}.{x[1]}'.format(x=sys.version_info)) \
+        .mkdir('site-packages')
+    monkeypatch.syspath_prepend(str(rv))
+    return rv
+    
+        try:
+        oids, array_oids = get_hstore_oids(connection.alias)
+        register_hstore(connection.connection, globally=True, oid=oids, array_oid=array_oids)
+    except ProgrammingError:
+        # Hstore is not available on the database.
+        #
+        # If someone tries to create an hstore field it will error there.
+        # This is necessary as someone may be using PSQL without extensions
+        # installed but be using other features of contrib.postgres.
+        #
+        # This is also needed in order to create the connection in order to
+        # install the hstore extension.
+        pass
+    
+        def _reset_stats(self):
+        self.tail.clear()
+        self.start = self.lastmark = self.lasttime = time()
+    
+        def run(self, args, opts):
+        if len(args) < 1:
+            raise UsageError()
+        elif len(args) > 1:
+            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
+        spname = args[0]
+    
+        def short_desc(self):
+        return 'Print Scrapy version'
+    
+            try:
+            self.min_bound = int(self.args[1])
+        except IndexError:
+            self.min_bound = 1
+    
+    
+def get_im_dir(name):
+    '''Retrieve the image directory for the dataset.'''
+    return _DATASETS[name][_IM_DIR]
+    
+        # Histogram of ground-truth objects
+    gt_hist = np.zeros((len(classes)), dtype=np.int)
+    for entry in roidb:
+        gt_inds = np.where(
+            (entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
+        gt_classes = entry['gt_classes'][gt_inds]
+        gt_hist += np.histogram(gt_classes, bins=hist_bins)[0]
+    logger.debug('Ground-truth class histogram:')
+    for i, v in enumerate(gt_hist):
+        logger.debug(
+            '{:d}{:s}: {:d}'.format(
+                i, classes[i].rjust(char_len), v))
+    logger.debug('-' * char_len)
+    logger.debug(
+        '{:s}: {:d}'.format(
+            'total'.rjust(char_len), np.sum(gt_hist)))
+
+    
+    from detectron.core.config import cfg
+from detectron.datasets.dataset_catalog import get_devkit_dir
+from detectron.datasets.voc_eval import voc_eval
+from detectron.utils.io import save_object
+    
+    
+def add_roi_Xconv1fc_head(model, blob_in, dim_in, spatial_scale):
+    '''Add a X conv + 1fc head, as a reference if not using GroupNorm'''
+    hidden_dim = cfg.FAST_RCNN.CONV_HEAD_DIM
+    roi_size = cfg.FAST_RCNN.ROI_XFORM_RESOLUTION
+    roi_feat = model.RoIFeatureTransform(
+        blob_in,
+        'roi_feat',
+        blob_rois='rois',
+        method=cfg.FAST_RCNN.ROI_XFORM_METHOD,
+        resolution=roi_size,
+        sampling_ratio=cfg.FAST_RCNN.ROI_XFORM_SAMPLING_RATIO,
+        spatial_scale=spatial_scale
+    )
+    
+    
+_RENAME = {
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up4convs':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up4convs',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0upshare':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v0upshare',
+    # Removed 'ResNet_' from the name because it wasn't relevent
+    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0up':
+        'mask_rcnn_heads.mask_rcnn_fcn_head_v0up',
+    # Removed head_builder module in favor of the more specific fast_rcnn name
+    'head_builder.add_roi_2mlp_head':
+        'fast_rcnn_heads.add_roi_2mlp_head',
+}
+    
+        if cfg.MODEL.FASTER_RCNN:
+        if model.train:
+            # Add op that generates training labels for in-network RPN proposals
+            model.GenerateProposalLabels(['rpn_rois', 'roidb', 'im_info'])
+        else:
+            # Alias rois to rpn_rois for inference
+            model.net.Alias('rpn_rois', 'rois')
+    
+        ind_kp = gt_inds[roidb['box_to_gt_ind_map']]
+    within_box = _within_box(gt_keypoints[ind_kp, :, :], roidb['boxes'])
+    vis_kp = gt_keypoints[ind_kp, 2, :] > 0
+    is_visible = np.sum(np.logical_and(vis_kp, within_box), axis=1) > 0
+    kp_fg_inds = np.where(
+        np.logical_and(max_overlaps >= cfg.TRAIN.FG_THRESH, is_visible)
+    )[0]
+    
+    
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    logger = setup_logging(__name__)
+    logger.setLevel(logging.DEBUG)
+    logging.getLogger('detectron.roi_data.loader').setLevel(logging.INFO)
+    np.random.seed(cfg.RNG_SEED)
+    args = parse_args()
+    logger.info('Called with args:')
+    logger.info(args)
+    if args.cfg_file is not None:
+        merge_cfg_from_file(args.cfg_file)
+    if args.opts is not None:
+        merge_cfg_from_list(args.opts)
+    assert_and_infer_cfg()
+    logger.info('Running with config:')
+    logger.info(pprint.pformat(cfg))
+    main(args)
+
+    
+    _logger = logging.getLogger( __name__ )
+    
+      post_vim_message.assert_has_exact_calls( [
+    call( 'On the first day of Christmas, my VimScript gave to me',
+          warning=False,
+          truncate=True ),
+    call( 'A test file in a Command-T', warning=False, truncate=True ),
+    call( 'On the second day of Christmas, my VimScript gave to me',
+          warning=False,
+          truncate=True ),
+    call( 'Two popup menus, and a test file in a Command-T',
+          warning=False,
+          truncate=True ),
+  ] )
+
+    
+      opts = _JavaFilter( { 'regex' : '.*taco.*',
+                        'level' : 'warning' } )
+  f = _CreateFilterForTypes( opts, [ 'java' ] )
+    
+        This worker is run in a separate process.
+    
+    if __name__ == '__main__':
+    main()
+
+    
+        route = [None] * len(obs)
+    i = len(obs) - 1
+    while i >= 0:
+        route[i] = state
+        state = mem_path[i][state]
+        i -= 1
+    return (prob, route)
+
+    
+    content = open(file_name, 'rb').read()
+    
+    parser = OptionParser(USAGE)
+parser.add_option('-k', dest='topK')
+opt, args = parser.parse_args()
+    
+    if len(sys.argv)>1:
+    default_encoding = sys.argv[1]
+    
+            return True
