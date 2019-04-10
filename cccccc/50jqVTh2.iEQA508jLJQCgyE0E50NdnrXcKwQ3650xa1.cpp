b2c756252d434dace2ae2517e420e67d0788c4dc
SUNsung <weatherius@gmail.com>
2019-04-10 10:44:34 +0300

    Rewrite [50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp] File in: 2019-04-10 10:44:34

--- cccccc/50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp
+++ cccccc/50jqVTh2.iEQA508jLJQCgyE0E50NdnrXcKwQ3650xa1.cpp
@@ -1,253 +1,295 @@
 
         
-        SILLayout::SILLayout(CanGenericSignature Sig,
-                     ArrayRef<SILField> Fields)
-  : GenericSigAndFlags(Sig, getFlagsValue(anyMutable(Fields))),
-    NumFields(Fields.size())
-{
-#ifndef NDEBUG
-  verifyFields(Sig, Fields);
-#endif
-  auto FieldsMem = getTrailingObjects<SILField>();
-  for (unsigned i : indices(Fields)) {
-    new (FieldsMem + i) SILField(Fields[i]);
-  }
-}
-    
-        if (node->Further || node->Right) {
-      IndentScope ms(this, (childKind == ChildKind::Right ||
-                            childKind == ChildKind::Further ||
-                            childKind == ChildKind::Root) ? '  ' : '| ');
-    }
-    
-      while (!RawText.empty()) {
-    size_t Pos = RawText.find_first_of('\n\r');
-    if (Pos == StringRef::npos)
-      Pos = RawText.size();
-    }
-    
-    std::string Mangle::translateOperator(StringRef Op) {
-  std::string Encoded;
-  for (char ch : Op) {
-    Encoded.push_back(translateOperatorChar(ch));
-  }
-  return Encoded;
-}
-    
-    
-    {  // -- Build the decoded string as UTF32 first because we need random access.
-  uint32_t n = initial_n;
-  int i = 0;
-  int bias = initial_bias;
-  /// let output = an empty string indexed from 0
-  // consume all code points before the last delimiter (if there is one)
-  //  and copy them to output,
-  size_t lastDelimiter = InputPunycode.find_last_of(delimiter);
-  if (lastDelimiter != StringRef::npos) {
-    for (char c : InputPunycode.slice(0, lastDelimiter)) {
-      // fail on any non-basic code point
-      if (static_cast<unsigned char>(c) > 0x7f)
-        return true;
-      OutCodePoints.push_back(c);
-    }
-    // if more than zero code points were consumed then consume one more
-    //  (which will be the last delimiter)
-    InputPunycode =
-        InputPunycode.slice(lastDelimiter + 1, InputPunycode.size());
-  }
-  
-  while (!InputPunycode.empty()) {
-    int oldi = i;
-    int w = 1;
-    for (int k = base; ; k += base) {
-      // consume a code point, or fail if there was none to consume
-      if (InputPunycode.empty())
-        return true;
-      char codePoint = InputPunycode.front();
-      InputPunycode = InputPunycode.slice(1, InputPunycode.size());
-      // let digit = the code point's digit-value, fail if it has none
-      int digit = digit_index(codePoint);
-      if (digit < 0)
-        return true;
-      
-      i = i + digit * w;
-      int t = k <= bias ? tmin
-            : k >= bias + tmax ? tmax
-            : k - bias;
-      if (digit < t)
-        break;
-      w = w * (base - t);
-    }
-    bias = adapt(i - oldi, OutCodePoints.size() + 1, oldi == 0);
-    n = n + i / (OutCodePoints.size() + 1);
-    i = i % (OutCodePoints.size() + 1);
-    // if n is a basic code point then fail
-    if (n < 0x80)
-      return true;
-    // insert n into output at position i
-    OutCodePoints.insert(OutCodePoints.begin() + i, n);
-    i++;
-  }
-  
-  return true;
-}
-    
-            for(int i = 0 ; i < nums.size() ; i ++)
-            for(int j = i + 1 ; j < nums.size() ; j ++)
-                if(nums[i] + nums[j] == target){
-                    int res[] = {i, j};
-                    return vector<int>(res, res + 2);
-                }
-    
-    
-    {private:
-    int nextDifferentCharacterIndex(const vector<int> &nums, int p){
-        for( ; p < nums.size() ; p ++ )
-            if( nums[p] != nums[p - 1] )
-                break;
-        return p;
-    }
-};
-    
-    // Recursive
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-public:
-    vector<int> preorderTraversal(TreeNode* root) {
-    }
-    }
-    
-    using namespace std;
-    
-    class IExpressionCommand
-{
-public:
-    virtual CalculationManager::CommandType GetCommandType() const = 0;
-    virtual void Accept(_In_ ISerializeCommandVisitor &commandVisitor) = 0;
-};
-    
-    // vvv THESE CONSTANTS SHOULD NEVER CHANGE vvv
-static constexpr int STANDARD_ID    = 0;
-static constexpr int SCIENTIFIC_ID  = 1;
-static constexpr int PROGRAMMER_ID  = 2;
-static constexpr int DATE_ID        = 3;
-static constexpr int VOLUME_ID      = 4;
-static constexpr int LENGTH_ID      = 5;
-static constexpr int WEIGHT_ID      = 6;
-static constexpr int TEMPERATURE_ID = 7;
-static constexpr int ENERGY_ID      = 8;
-static constexpr int AREA_ID        = 9;
-static constexpr int SPEED_ID       = 10;
-static constexpr int TIME_ID        = 11;
-static constexpr int POWER_ID       = 12;
-static constexpr int DATA_ID        = 13;
-static constexpr int PRESSURE_ID    = 14;
-static constexpr int ANGLE_ID       = 15;
-static constexpr int CURRENCY_ID    = 16;
-// ^^^ THESE CONSTANTS SHOULD NEVER CHANGE ^^^
-    
-        const auto& localizationSettings = LocalizationSettings::GetInstance();
-    wchar_t ch0 = localizationSettings.GetDigitSymbolFromEnUsDigit('0');
-    wchar_t ch1 = localizationSettings.GetDigitSymbolFromEnUsDigit('1');
-    
-    #include 'Views/CalculatorProgrammerOperators.g.h'
-#include 'Controls/RadixButton.h'
-#include 'CalcViewModel/StandardCalculatorViewModel.h'
-    
-    #include 'Converters/BooleanNegationConverter.h'
-#include 'Converters/VisibilityNegationConverter.h'
-#include 'Views/DateCalculator.g.h'
-    
-    namespace CalculatorApp
-{
-    [Windows::Foundation::Metadata::WebHostHidden]
-    public ref class Memory sealed
+            enum BORDER_MODE
     {
-    public:
-        Memory();
-        property CalculatorApp::ViewModel::StandardCalculatorViewModel^ Model
-        {
-            CalculatorApp::ViewModel::StandardCalculatorViewModel^ get() {
-                return static_cast<CalculatorApp::ViewModel::StandardCalculatorViewModel^>(this->DataContext);
-            }
+        BORDER_MODE_UNDEFINED,
+        BORDER_MODE_CONSTANT,
+        BORDER_MODE_REPLICATE,
+        BORDER_MODE_REFLECT,
+        BORDER_MODE_REFLECT101,
+        BORDER_MODE_WRAP
+    };
+    
+    template <int shift>
+void accumulateSquareConst(const Size2D &size,
+                           const u8 *srcBase, ptrdiff_t srcStride,
+                           s16 *dstBase, ptrdiff_t dstStride)
+{
+    size_t roiw16 = size.width >= 15 ? size.width - 15 : 0;
+    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
+    }
+    
+            std::swap(lanea, laneA);
+        std::swap(laneb, laneB);
+    }
+    //last line
+    if(i < size.height)
+    {
+        //vertical convolution
+        ptrdiff_t idx_rm1 = internal::borderInterpolate(i - 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+        ptrdiff_t idx_rp1 = internal::borderInterpolate(i + 1, size.height, borderType, borderMargin.top, borderMargin.bottom);
+    
+    
+    {
+    {
+    {            v_src0 = internal::vld1(src0 + x);
+            v_src1 = internal::vld1(src1 + x);
+            op(v_src0, v_src1, v_dst);
+            internal::vst1(dst + x, v_dst);
+            x+=8;
         }
     }
+};
+    
+             int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
+         int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
+         int16x4_t vline1_s16 = vqmovn_s32(vline1_s32);
+         int16x4_t vline2_s16 = vqmovn_s32(vline2_s32);
+    
+    
+    {
+    {
+    {                for (; j < size.width; j++)
+                {
+                    dst[j] = src[j] >= 0 ? 0 : 255;
+                }
+            }
+        }
+        else
+        {
+            for (size_t i = 0; i < size.height; ++i)
+            {
+                u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
+                std::memset(dst, 0, sizeof(u8) * size.width);
+            }
+        }
+        return;
     }
     
-    #endif //__CCCAMERA_ACTION_H__
+    #define CVTS_FUNC1(T1, SIMD_SIZE, CVTSINIT, CVTSROW)                             \
+    void convertScale(const Size2D &,                                            \
+                      const T1 *, ptrdiff_t,                                     \
+                      T1 *, ptrdiff_t,                                           \
+                      f64, f64)                                                  \
+    {                                                                            \
+        internal::assertSupportedConfiguration();                                \
+    }
+    
+    
+    {        for (; i < size.width; i++)
+            result += (src[i] < std::numeric_limits<double>::min() && src[i] > -std::numeric_limits<double>::min())?0:1;
+        if (result < 0)
+        {
+            return 0x7fFFffFF;
+        }
+    }
+    return result;
+#else
+    (void)_size;
+    (void)srcBase;
+    (void)srcStride;
+    
+            //left&right borders
+        if (borderType != BORDER_MODE_CONSTANT)
+            for (s32 k = 0; k < cn; ++k)
+            {
+                lane[-cn+k] = lane[idx_l1 + k];
+                lane[-cn-cn+k] = lane[idx_l2 + k];
+    }
+    
+        for(size_t j = 0; j < size.height; ++j)
+    {
+        const T *  src = internal::getRowPtr( srcBase,  srcStride, j);
+        const T * rng1 = internal::getRowPtr(rng1Base, rng1Stride, j);
+        const T * rng2 = internal::getRowPtr(rng2Base, rng2Stride, j);
+             u8 *  dst = internal::getRowPtr( dstBase,  dstStride, j);
+        size_t i = 0;
+        for( ; i < width; i += 32/sizeof(T) )
+        {
+            internal::prefetch(src + i);
+            internal::prefetch(rng1 + i);
+            internal::prefetch(rng2 + i);
+    }
+    }
+    
+        vector<int> nums1 = {1, 1, 2};
+    cout << Solution().removeDuplicates(nums1) << endl;
+    
+            vector<int> res;
+        if( root == NULL )
+            return res;
+    
+            TreeNode* cur = root;
+        while(cur != NULL){
+    }
+    
+    
+    {
+    {
+    {            if(command.s == 'print')
+                res.push_back(command.node->val);
+            else{
+                assert(command.s == 'go');
+                if(command.node->right)
+                    stack.push(Command('go',command.node->right));
+                if(command.node->left)
+                    stack.push(Command('go',command.node->left));
+                stack.push(Command('print', command.node));
+            }
+        }
+        return res;
+    }
+};
+    
+    
+void print_vec(const vector<int>& vec){
+    for(int e: vec)
+        cout << e << ' ';
+    cout << endl;
+}
+    
+    /**
+ * Factory method
+ */
+static Transliterator* RemoveTransliterator_create(const UnicodeString& /*ID*/,
+                                                   Transliterator::Token /*context*/) {
+    /* We don't need the ID or context. We just remove data */
+    return new RemoveTransliterator();
+}
+    
+    uint32_t
+RuleBasedCollator::setVariableTop(const UnicodeString &varTop, UErrorCode &errorCode) {
+    return setVariableTop(varTop.getBuffer(), varTop.length(), errorCode);
+}
+    
+    U_NAMESPACE_END
+    
+    #ifndef __SHARED_DATEFORMATSYMBOLS_H__
+#define __SHARED_DATEFORMATSYMBOLS_H__
+    
+        /**
+     * Get maximum significant digits. 0 means no minimum.
+     */
+    int32_t getMin() const {
+        return fMin;
+    }
+    
+    void 
+SimpleTimeZone::setEndRule(int32_t month, int32_t dayOfMonth, 
+                           int32_t time, TimeMode mode, UErrorCode& status)
+{
+    setEndRule(month, dayOfMonth, 0, time, mode, status);
+}
+    
+    /**
+ * A representation an acceptable range of digit counts for integers.
+ */
+class U_I18N_API IntDigitCountRange : public UMemory {
+public:
+    /**
+     * No constraints: 0 up to INT32_MAX
+     */
+    IntDigitCountRange() : fMin(0), fMax(INT32_MAX) { }
+    IntDigitCountRange(int32_t min, int32_t max);
+    int32_t pin(int32_t digitCount) const;
+    int32_t getMax() const { return fMax; }
+    int32_t getMin() const { return fMin; }
+private:
+    int32_t fMin;
+    int32_t fMax;
+};
+    
+    
+SimpleDateFormatStaticSets::~SimpleDateFormatStaticSets() {
+    delete fDateIgnorables;  fDateIgnorables = NULL;
+    delete fTimeIgnorables;  fTimeIgnorables = NULL;
+    delete fOtherIgnorables; fOtherIgnorables = NULL;
+}
+    
+    
+    {    AutomationProperties::SetName(m_host, announcement->Announcement);
+    AutomationPeer^ peer = FrameworkElementAutomationPeer::FromElement(m_host);
+    if (peer != nullptr)
+    {
+        peer->RaiseAutomationEvent(AutomationEvents::LiveRegionChanged);
+    }
+}
 
     
-        /**
-    @brief Initializes the action with center position, radius, number of waves, amplitude, a grid size and duration.
-    @param duration Specify the duration of the Ripple3D action. It's a value in seconds.
-    @param gridSize Specify the size of the grid.
-    @param position Specify the center position of the ripple effect.
-    @param radius Specify the radius of the ripple effect.
-    @param waves Specify the waves count of the ripple effect.
-    @param amplitude Specify the amplitude of the ripple effect.
-    @return If the initialization success, return true; otherwise, return false.
-     */
-    bool initWithDuration(float duration, const Size& gridSize, const Vec2& position, float radius, unsigned int waves, float amplitude);
+        case CustomPeers::AutomationNotificationKind::ActionCompleted:
+        return StandardPeers::AutomationNotificationKind::ActionCompleted;
     
-    void CallFunc::update(float time)
-{
-    ActionInstant::update(time);
-    this->execute();
-}
-    
-    
-    {
-    {
-    {            if (action->getTag() == (int)tag && action->getOriginalTarget() == target)
-            {
-                removeActionAtIndex(i, element);
-                break;
-            }
+            if (id.compare(L'sGrouping') == 0)
+        {
+            // The following groupings are the onces that CalcEngine supports.
+            //   0;0             0x000          - no grouping
+            //   3;0             0x003          - group every 3 digits
+            //   3;2;0           0x023          - group 1st 3 and then every 2 digits
+            //   4;0             0x004          - group every 4 digits
+            //   5;3;2;0         0x235          - group 5, then 3, then every 2
+            wstring numberGroupingString = localizationSettings.GetNumberGroupingStr();
+            return numberGroupingString;
         }
-    }
+    
+                static void OnVirtualKeyControlShiftChordPropertyChanged(
+                Windows::UI::Xaml::DependencyObject^ target,
+                MyVirtualKey oldValue,
+                MyVirtualKey newValue);
+    
+    
+    {
+    {    private:
+        std::wstring m_activityName;
+        winrt::Windows::Foundation::Diagnostics::LoggingChannel m_channel;
+        winrt::Windows::Foundation::Diagnostics::LoggingFields m_fields;
+        winrt::Windows::Foundation::Diagnostics::LoggingActivity m_activity;
+    };
 }
+
     
-        /**
-    @brief Create an action with duration, grid size.
-    @param duration Specify the duration of the PageTurn3D action. It's a value in seconds.
-    @param gridSize Specify the size of the grid.
-    @return If the creation success, return a pointer of PageTurn3D action; otherwise, return nil.
-    */
-    static PageTurn3D* create(float duration, const Size& gridSize);
+      std::atomic<int> total_stopped_;
+  std::atomic<int> total_delayed_;
+  std::atomic<int> total_compaction_pressure_;
+  uint64_t bytes_left_;
+  uint64_t last_refill_time_;
+  // write rate set when initialization or by `DBImpl::SetDBOptions`
+  uint64_t max_delayed_write_rate_;
+  // current write rate
+  uint64_t delayed_write_rate_;
     
-        // Override
-    virtual ShatteredTiles3D* clone() const override;
-    virtual void update(float time) override;
+    #if !defined(ROCKSDB_LITE) && !defined(OS_WIN)
     
-CC_CONSTRUCTOR_ACCESS:
-    ShatteredTiles3D() {}
-    virtual ~ShatteredTiles3D() {}
+      // atomic write
+  WriteBatch batch;
+  batch.Put(handles[0], Slice('key2'), Slice('value2'));
+  batch.Put(handles[1], Slice('key3'), Slice('value3'));
+  batch.Delete(handles[0], Slice('key'));
+  s = db->Write(WriteOptions(), &batch);
+  assert(s.ok());
     
-        // Overrides
-    void startWithTarget(Node *target) override;
-    void update(float dt) override;
-    ActionTween* reverse() const override;
-    ActionTween *clone() const override;
+      // Initialize pointer options for each column family
+  for (size_t i = 0; i < loaded_cf_descs.size(); ++i) {
+    auto* loaded_bbt_opt = reinterpret_cast<BlockBasedTableOptions*>(
+        loaded_cf_descs[0].options.table_factory->GetOptions());
+    // Expect the same as BlockBasedTableOptions will be loaded form file.
+    assert(loaded_bbt_opt->block_size == bbt_opts.block_size);
+    // However, block_cache needs to be manually initialized as documented
+    // in rocksdb/utilities/options_util.h.
+    loaded_bbt_opt->block_cache = cache;
+  }
+  // In addition, as pointer options are initialized with default value,
+  // we need to properly initialized all the pointer options if non-defalut
+  // values are used before calling DB::Open().
+  assert(loaded_cf_descs[0].options.compaction_filter == nullptr);
+  loaded_cf_descs[0].options.compaction_filter = compaction_filter.get();
     
-CC_CONSTRUCTOR_ACCESS:
-    /** 
-     * @brief Initializes the action with the property name (key), and the from and to parameters.
-     * @param duration The duration of the ActionTween. It's a value in seconds.
-     * @param key The key of property which should be updated.
-     * @param from The value of the specified property when the action begin.
-     * @param to The value of the specified property when the action end.
-     * @return If the initialization success, return true; otherwise, return false.
-     */
-    bool initWithDuration(float duration, const std::string& key, float from, float to);
+      // open DB
+  Status s = DB::Open(options, kDBPath, &db);
+  assert(s.ok());
     
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __CC_ANIMATION_CACHE_H__
-#define __CC_ANIMATION_CACHE_H__
+      // Write a key OUTSIDE of this transaction.
+  // Does not affect txn since this is an unrelated key.  If we wrote key 'abc'
+  // here, the transaction would fail to commit.
+  s = txn_db->Put(write_options, 'xyz', 'zzz');
+    
+      ~ManagedSnapshot();
