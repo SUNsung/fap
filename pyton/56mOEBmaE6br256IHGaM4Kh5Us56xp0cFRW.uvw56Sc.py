d72871a6c7c34dd6d0d62b6d285f3d94d2d5310a
SUNsung <weatherius@gmail.com>
2019-04-10 20:05:18 +0300

    Rewrite [56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py] File in: 2019-04-10 20:05:18

--- pyton/56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py
+++ pyton/56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py
@@ -1,43 +1,122 @@
 
         
-            # Returns
-        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+            else:
+        sys.stdout.write('.')
+    sys.stdout.flush()
+    
+        infile, outfile = args
+    
+    if isinstance(helptext, bytes):
+    helptext = helptext.decode('utf-8')
+    
+        outfile, = args
+    
+        def test_cmdline_umlauts(self):
+        p = subprocess.Popen(
+            [sys.executable, 'youtube_dl/__main__.py', encodeArgument('ä'), '--version'],
+            cwd=rootDir, stdout=_DEV_NULL, stderr=subprocess.PIPE)
+        _, stderr = p.communicate()
+        self.assertFalse(stderr)
+    
+            If `sign` is incorrect, /validate call throws an HTTP 556 error
+        '''
+        logger = WarningLogger()
+        ie = IqiyiIEWithCredentials(FakeYDL({'logger': logger}))
+        ie._login()
+        self.assertTrue('unable to log in:' in logger.messages[0])
+    
+        elif args.form and not args.files:
+        # If sending files, `requests` will set
+        # the `Content-Type` for us.
+        default_headers['Content-Type'] = FORM_CONTENT_TYPE
+    return default_headers
+    
+        http://docs.python-requests.org/en/latest/user/advanced/#transport-adapters
+    
+        def register(self, *plugins):
+        for plugin in plugins:
+            self._plugins.append(plugin)
+    
+        '''
+    abbrevs = [
+        (1 << 50, 'PB'),
+        (1 << 40, 'TB'),
+        (1 << 30, 'GB'),
+        (1 << 20, 'MB'),
+        (1 << 10, 'kB'),
+        (1, 'B')
+    ]
+    
+    
+@pytest.mark.parametrize('argument_name', ['--auth-type', '-A'])
+def test_digest_auth(httpbin_both, argument_name):
+    r = http(argument_name + '=digest', '--auth=user:password',
+             'GET', httpbin_both.url + '/digest-auth/auth/user/password')
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+    
+def test_default_options(httpbin):
+    env = MockEnvironment()
+    env.config['default_options'] = ['--form']
+    env.config.save()
+    r = http(httpbin.url + '/post', 'foo=bar', env=env)
+    assert r.json['form'] == {'foo': 'bar'}
+    
+        def test_POST_with_data_auto_JSON_headers(self, httpbin):
+        r = http('POST', httpbin.url + '/post', 'a=b')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == JSON_ACCEPT
+        assert r.json['headers']['Content-Type'] == 'application/json'
+    
+        def load(self):
+        super(Config, self).load()
+        self._migrate_implicit_content_type()
+    
+        if last_snapshot_min_age > 0:
+        try:
+            current_snapshots = ec2.get_all_snapshots(filters={'volume_id': volume_id})
+        except boto.exception.BotoServerError as e:
+            module.fail_json(msg='%s: %s' % (e.error_code, e.error_message))
+    
+        try:
+        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
+        iam = boto3_conn(module, conn_type='client', resource='iam', region=region, endpoint=ec2_url, **aws_connect_kwargs)
+    except botocore.exceptions.ClientError as e:
+        module.fail_json(msg='Boto3 Client Error - ' + str(e.msg))
+    
+            try:
+            lambda_facts.update(function_list=client.list_functions(**params)['Functions'])
+        except ClientError as e:
+            if e.response['Error']['Code'] == 'ResourceNotFoundException':
+                lambda_facts.update(function_list=[])
+            else:
+                module.fail_json_aws(e, msg='Trying to get function list')
+    
+        module : AnsibleModule object
+    oneandone_conn: authenticated oneandone object
     '''
-    dirname = 'cifar-10-batches-py'
-    origin = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
-    path = get_file(dirname, origin=origin, untar=True)
+    try:
+        mp_id = module.params.get('name')
+        monitoring_policy_id = get_monitoring_policy(oneandone_conn, mp_id)
+        if module.check_mode:
+            if monitoring_policy_id is None:
+                _check_mode(module, False)
+            _check_mode(module, True)
+        monitoring_policy = oneandone_conn.delete_monitoring_policy(monitoring_policy_id)
     
-        return (x_train, y_train), (x_test, y_test)
-
+    from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.six.moves.urllib.parse import urlencode
+from ansible.module_utils._text import to_native
+from ansible.module_utils.urls import fetch_url
     
-    import six
-from . import backend as K
-from .utils.generic_utils import serialize_keras_object
-from .utils.generic_utils import deserialize_keras_object
+        if module.check_mode:
+        device = dict(
+            model_handle=None,
+            address=device_ip,
+            landscape='0x%x' % int(module.params.get('landscape'), 16))
+        module.exit_json(changed=True, device=device)
     
-    
-def test_preprocess_input():
-    # Test image batch with float and int image input
-    x = np.random.uniform(0, 255, (2, 10, 10, 3))
-    xint = x.astype('int32')
-    assert utils.preprocess_input(x).shape == x.shape
-    assert utils.preprocess_input(xint).shape == xint.shape
-    
-    if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
-    
-    # Generate corrupted MNIST images by adding noise with normal dist
-# centered at 0.5 and std=0.5
-noise = np.random.normal(loc=0.5, scale=0.5, size=x_train.shape)
-x_train_noisy = x_train + noise
-noise = np.random.normal(loc=0.5, scale=0.5, size=x_test.shape)
-x_test_noisy = x_test + noise
-    
-    num_classes = np.max(y_train) + 1
-print(num_classes, 'classes')
+        if temperature_unit != ha_unit:
+        temperature = convert_temperature(
+            temperature, temperature_unit, ha_unit)
