47d57d527cb3a77c7215edee915e03b1b2801b4d
SUNsung <weatherius@gmail.com>
2019-04-11 00:01:52 +0300

    Rewrite [007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py] File in: 2019-04-11 00:01:51

--- pyton/007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py
+++ pyton/007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py
@@ -1,41 +1,132 @@
 
         
-            def appid_exist(self, appids):
-        for appid in appids.split('|'):
-            if appid == '':
-                continue
-            if appid in self.config.GAE_APPIDS:
-                return True
-        return False
+        
+@pytest.fixture
+def client(app):
+    '''A test client for the app.'''
+    return app.test_client()
     
     
-IPv4 = CheckNetwork('IPv4')
-IPv4.urls = [
-            'https://www.microsoft.com',
-            'https://www.apple.com',
-            'https://code.jquery.com',
-            'https://cdn.bootcss.com',
-            'https://cdnjs.cloudflare.com']
-IPv4.triger_check_network()
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/update',
+    '/1/delete',
+))
+def test_login_required(client, path):
+    response = client.post(path)
+    assert response.headers['Location'] == 'http://localhost/auth/login'
     
-        for qualified, server, _, _ in server_list:
-        if qualified:
-            best_server = server[0]
-            break
-    log = Log()
-    if best_server:
-        log.write('best server is: %s.' % best_server)
+    
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+    
+        def __init__(self, serializer):
+        '''Create a tagger for the given serializer.'''
+        self.serializer = serializer
+    
+            The arguments passed to :meth:`as_view` are forwarded to the
+        constructor of the class.
+        '''
+        def view(*args, **kwargs):
+            self = view.view_class(*class_args, **class_kwargs)
+            return self.dispatch_request(*args, **kwargs)
+    
+            # In debug mode we're replacing the files multidict with an ad-hoc
+        # subclass that raises a different error for key errors.
+        if (
+            current_app
+            and current_app.debug
+            and self.mimetype != 'multipart/form-data'
+            and not self.files
+        ):
+            from .debughelpers import attach_enctype_error_multidict
+            attach_enctype_error_multidict(self)
+    
+        This provides for tests where a system has custom loaders, e.g. Google App
+    Engine's HardenedModulesHook, which have neither the `get_filename` method
+    nor the `archive` attribute.
+    
+        try:
+        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
+        iam = boto3_conn(module, conn_type='client', resource='iam', region=region, endpoint=ec2_url, **aws_connect_kwargs)
+    except botocore.exceptions.ClientError as e:
+        module.fail_json(msg='Boto3 Client Error - ' + str(e.msg))
+    
+        module = AnsibleAWSModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+        mutually_exclusive=[],
+        required_together=[]
+    )
+    
+            changed = False
+    
+        changed = False
+    if state == 'present':
+        if not ipa_dnszone:
+            changed = True
+            if not module.check_mode:
+                client.dnszone_add(zone_name=zone_name)
+        else:
+            changed = False
     else:
-        xlog.warning('no server detected, return default: teredo.remlab.net.')
-        log.write('no server detected, return default: teredo.remlab.net.')
-        best_server = 'teredo.remlab.net'
-    log.close()
-    return best_server
+        if ipa_dnszone:
+            changed = True
+            if not module.check_mode:
+                client.dnszone_del(zone_name=zone_name)
     
-        一般 K-Mean 算法的终止条件有如下几个：
-        1. 所有样本的类别不再改变
-        2. 达到最大迭代次数
-        3. 精度达到要求（？）
+        def hbacrule_remove_service(self, name, item):
+        return self._post_json(method='hbacrule_remove_service', name=name, item=item)
     
-        with tf.variable_scope(name or 'conv2d', reuse=reuse):
-        W, b = get_wb(kernel_shape)
+    # Verify that we compute the same anchors as Shaoqing's matlab implementation:
+#
+#    >> load output/rpn_cachedir/faster_rcnn_VOC2007_ZF_stage1_rpn/anchors.mat
+#    >> anchors
+#
+#    anchors =
+#
+#       -83   -39   100    56
+#      -175   -87   192   104
+#      -359  -183   376   200
+#       -55   -55    72    72
+#      -119  -119   136   136
+#      -247  -247   264   264
+#       -35   -79    52    96
+#       -79  -167    96   184
+#      -167  -343   184   360
+    
+    
+def add_fpn_retinanet_outputs(model, blobs_in, dim_in, spatial_scales):
+    '''RetinaNet head. For classification and box regression, we can chose to
+    have the same conv tower or a separate tower. 'bl_feat_list' stores the list
+    of feature blobs for bbox prediction. These blobs can be shared cls feature
+    blobs if we share the tower or else are independent blobs.
+    '''
+    dim_out = dim_in
+    k_max = cfg.FPN.RPN_MAX_LEVEL  # coarsest level of pyramid
+    k_min = cfg.FPN.RPN_MIN_LEVEL  # finest level of pyramid
+    A = len(cfg.RETINANET.ASPECT_RATIOS) * cfg.RETINANET.SCALES_PER_OCTAVE
+    
+        if not model.train or cfg.MODEL.FASTER_RCNN:
+        # Proposals are needed during:
+        #  1) inference (== not model.train) for RPN only and Faster R-CNN
+        #  OR
+        #  2) training for Faster R-CNN
+        # Otherwise (== training for RPN only), proposals are not needed
+        model.net.Sigmoid('rpn_cls_logits', 'rpn_cls_probs')
+        model.GenerateProposals(
+            ['rpn_cls_probs', 'rpn_bbox_pred', 'im_info'],
+            ['rpn_rois', 'rpn_roi_probs'],
+            anchors=anchors,
+            spatial_scale=spatial_scale
+        )
+    
+    from detectron.core.config import assert_and_infer_cfg
+from detectron.core.config import cfg
+from detectron.core.config import merge_cfg_from_file
+from detectron.core.config import merge_cfg_from_list
+from detectron.datasets.roidb import combined_roidb_for_training
+from detectron.roi_data.loader import RoIDataLoader
+from detectron.utils.logging import setup_logging
+from detectron.utils.timer import Timer
