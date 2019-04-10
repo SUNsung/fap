176bdbd6f8dda81256a6529057ff2cdb7c8cc2ef
SUNsung <weatherius@gmail.com>
2019-04-10 16:01:31 +0300

    Rewrite [00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp] File in: 2019-04-10 16:01:30

--- cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
+++ cccccc/00hoCyBujHulQ00od8xqK1XUcE00yDmrHmfypQQ0015Y.cpp
@@ -1,125 +1,95 @@
 
         
-            if (info.bytes > buf.size()) {
-      llvm::dbgs() << 'AST section too small.\n';
-      return false;
-    }
-    
-      DefaultCacheKey CKey(Key, &DCache.CBs);
-  auto Entry = DCache.Entries.find(CKey);
-  if (Entry != DCache.Entries.end()) {
-    if (Entry->second == Value)
-      return;
-    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
-    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
-    DCache.Entries.erase(Entry);
-  }
-    
-    bool CacheImpl::getAndRetain(const void *Key, void **Value_out) {
-  int Ret = cache_get_and_retain(static_cast<cache_t*>(Impl),
-                                 const_cast<void*>(Key), Value_out);
-  return Ret == 0;
-}
-    
-      struct IndentScope {
-    TreePrinter *Printer;
-    size_t OldLength;
-    IndentScope(TreePrinter *printer, StringRef indent)
-        : Printer(printer), OldLength(printer->Indent.size()) {
-      Printer->Indent += indent;
-    }
-    ~IndentScope() { Printer->Indent.resize(OldLength); }
-  };
-    
-    static bool encodeToUTF8(const std::vector<uint32_t> &Scalars,
-                         std::string &OutUTF8) {
-  for (auto S : Scalars) {
-    if (!isValidUnicodeScalar(S)) {
-      OutUTF8.clear();
-      return false;
-    }
-    if (S >= 0xD800 && S < 0xD880)
-      S -= 0xD800;
-    }
-    }
-    
-    void CompileJobAction::anchor() {}
-    
-    class LLVM_LIBRARY_VISIBILITY Darwin : public ToolChain {
-protected:
-  InvocationInfo constructInvocation(const InterpretJobAction &job,
-                                     const JobContext &context) const override;
-  InvocationInfo constructInvocation(const LinkJobAction &job,
-                                     const JobContext &context) const override;
-    }
-    
-    int orientation_and_script_detection(STRING& filename,
-                                     OSResults*,
-                                     tesseract::Tesseract*);
-    
-    void Tesseract::PrerecAllWordsPar(const GenericVector<WordData>& words) {
-  // Prepare all the blobs.
-  GenericVector<BlobData> blobs;
-  for (int w = 0; w < words.size(); ++w) {
-    if (words[w].word->ratings != nullptr &&
-        words[w].word->ratings->get(0, 0) == nullptr) {
-      for (int s = 0; s < words[w].lang_words.size(); ++s) {
-        Tesseract* sub = s < sub_langs_.size() ? sub_langs_[s] : this;
-        const WERD_RES& word = *words[w].lang_words[s];
-        for (int b = 0; b < word.chopped_word->NumBlobs(); ++b) {
-          blobs.push_back(BlobData(b, sub, word));
-        }
-      }
-    }
-  }
-  // Pre-classify all the blobs.
-  if (tessedit_parallelize > 1) {
-#ifdef _OPENMP
-#pragma omp parallel for num_threads(10)
-#endif  // _OPENMP
-    for (int b = 0; b < blobs.size(); ++b) {
-      *blobs[b].choices =
-          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
-    }
-  } else {
-    // TODO(AMD) parallelize this.
-    for (int b = 0; b < blobs.size(); ++b) {
-      *blobs[b].choices =
-          blobs[b].tesseract->classify_blob(blobs[b].blob, 'par', White, nullptr);
-    }
-  }
-}
+        // TODO(kenton):  It's hard to write a robust test of the doc comments -- we
+//   can only really compare the output against a golden value, which is a
+//   fairly tedious and fragile testing strategy.  If we want to go that route,
+//   it probably makes sense to bite the bullet and write a test that compares
+//   the whole generated output for unittest.proto against a golden value, with
+//   a very simple script that can be run to regenerate it with the latest code.
+//   This would mean that updates to the golden file would have to be included
+//   in any change to the code generator, which would actually be fairly useful
+//   as it allows the reviewer to see clearly how the generated code is
+//   changing.
     
     
     {
-    {    if (amount[tag.string()] == 1) {
-      other->AddChild(vc->GetName(), vc->GetId(), vc->GetValue().string(),
-                      vc->GetDescription());
-    } else {  // More than one would use this submenu -> create submenu.
-      SVMenuNode* sv = mr->AddChild(tag.string());
-      if ((amount[tag.string()] <= MAX_ITEMS_IN_SUBMENU) ||
-          (amount[tag2.string()] <= 1)) {
-        sv->AddChild(vc->GetName(), vc->GetId(),
-                     vc->GetValue().string(), vc->GetDescription());
-      } else {  // Make subsubmenus.
-        SVMenuNode* sv2 = sv->AddChild(tag2.string());
-        sv2->AddChild(vc->GetName(), vc->GetId(),
-                      vc->GetValue().string(), vc->GetDescription());
-      }
-    }
+    {    reflection->MutableUnknownFields(message)->Clear();
   }
-  return mr;
-}
+ private:
+  virtual bool ShouldBeClear(const FieldDescriptor *field) = 0;
+};
     
-    // Factory to build a BoxWord from a TWERD using the DENORMs on each blob to
-// switch back to original image coordinates.
-BoxWord* BoxWord::CopyFromNormalized(TWERD* tessword) {
-  BoxWord* boxword = new BoxWord();
-  // Count the blobs.
-  boxword->length_ = tessword->NumBlobs();
-  // Allocate memory.
-  boxword->boxes_.reserve(boxword->length_);
+        for (int i = 0; i < dataset.payload_size(); i++) {
+      message->ParseFromString(dataset.payload(i));
+      GogoDataStripper stripper;
+      stripper.StripMessage(message);
+      dataset.set_payload(i, message->SerializeAsString());
+    }
+    
+        if (dataset.message_name() == 'benchmarks.proto3.GoogleMessage1') {
+      message = new benchmarks::proto3::GoogleMessage1;
+    } else if (dataset.message_name() == 'benchmarks.proto2.GoogleMessage1') {
+      message = new benchmarks::proto2::GoogleMessage1;
+    } else if (dataset.message_name() == 'benchmarks.proto2.GoogleMessage2') {
+      message = new benchmarks::proto2::GoogleMessage2;
+    } else if (dataset.message_name() ==
+        'benchmarks.google_message3.GoogleMessage3') {
+      message = new benchmarks::google_message3::GoogleMessage3;
+    } else if (dataset.message_name() ==
+        'benchmarks.google_message4.GoogleMessage4') {
+      message = new benchmarks::google_message4::GoogleMessage4;
+    } else {
+      std::cerr << 'Unknown message type: ' << dataset.message_name();
+      exit(1);
     }
     
     
-    {}  // namespace tesseract
+    {        return dict[versionKey].Value<size_t>();
+    }
+    
+                auto scale = initConfig[ScaleAttributeName].Value<double>();
+            int outputRank = DefaultParamInitOutputRank, filterRank = DefaultParamInitFilterRank;
+            if (initializerType != Microsoft::MSR::CNTK::UniformInitializerTypeName && 
+                initializerType != Microsoft::MSR::CNTK::TruncNormalInitializerTypeName)
+            {
+                outputRank = initConfig[OutputRankAttributeName].Value<int>();
+                filterRank = initConfig[FilterRankAttributeName].Value<int>();
+    }
+    
+        int m_fd;               // file descriptor
+    std::string m_fileName; // lock file name
+    struct flock m_lock;    // fnctl lock structure
+    
+    #pragma once
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
+#endif
+#ifdef _WIN32
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif // NOMINMAX
+#pragma comment(lib, 'Dbghelp.lib')
+#else
+#include <execinfo.h>
+#include <cxxabi.h>
+#endif
+    
+    
+    {
+    {
+    {}}}
+
+    
+            if (Input(1)->HasMBLayout())
+        {
+            // infer rows1 as rows0
+            Input(1)->ValidateInferInputDimsFrom(TensorShape(rows0));
+            SetDims(TensorShape(rows0), true);
+        }
+        else // multiplying two straight matrices
+        {
+            size_t cols1 = Input(1)->GetAsMatrixNumCols();
+            // infer rows1 as rows0
+            Input(1)->ValidateInferInputDimsFrom(TensorShape(rows0, cols1));
+            SetDims(TensorShape(rows0, cols1), false);
+        }
