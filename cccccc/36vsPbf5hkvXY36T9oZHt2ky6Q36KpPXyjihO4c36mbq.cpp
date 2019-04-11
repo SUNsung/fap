4c9dc5eb229a255777bfccec189957a997e0e864
SUNsung <weatherius@gmail.com>
2019-04-11 12:17:46 +0300

    Rewrite [36vsPbf5hkvXY36T9oZHt2ky6Q36KpPXyjihO4c36mbq.cpp] File in: 2019-04-11 12:17:45

--- cccccc/36vsPbf5hkvXY36T9oZHt2ky6Q36KpPXyjihO4c36mbq.cpp
+++ cccccc/36vsPbf5hkvXY36T9oZHt2ky6Q36KpPXyjihO4c36mbq.cpp
@@ -1,105 +1,196 @@
 
         
-        #endif  // GRPC_INTERNAL_CPP_EXT_PROTO_SERVER_REFLECTION_H
-
-    
-    
-	// ----------------------------------------------------------------------------------------------------
-	//
-    
-    	void EncodeMipmaps(float *a_pafSourceRGBA,
-		unsigned int a_uiSourceWidth,
-		unsigned int a_uiSourceHeight,
-		Image::Format a_format,
-		ErrorMetric a_eErrMetric,
-		float a_fEffort,
-		unsigned int a_uiJobs,
-		unsigned int a_uiMaxJobs,
-		unsigned int a_uiMaxMipmaps,
-		unsigned int a_uiMipFilterFlags,
-		RawImage* a_pMipmaps,
-		int *a_piEncodingTime_ms, bool a_bVerboseOutput = false);
-    
-    
-    {		switch (m_pencoding->GetMode())
-		{
-		case Block4x4Encoding::MODE_ETC1:
-			return 'ETC1';
-		case Block4x4Encoding::MODE_T:
-			return 'T';
-		case Block4x4Encoding::MODE_H:
-			return 'H';
-		case Block4x4Encoding::MODE_PLANAR:
-			return 'PLANAR';
-		default:
-			return '???';
-		}
-	}
-    
-    #include 'EtcBlock4x4EncodingBits.h'
-#include 'EtcBlock4x4.h'
-    
-    		Block4x4Encoding(void);
-		//virtual ~Block4x4Encoding(void) =0;
-		virtual ~Block4x4Encoding(void) {}
-		virtual void InitFromSource(Block4x4 *a_pblockParent,
-									ColorFloatRGBA *a_pafrgbaSource,
-    
-    		static const unsigned int BYTES_PER_BLOCK = 16;
-		static const unsigned int SELECTOR_BYTES = 12;
-    
-    		// use the best of each half
-		IndividualTrys::Try *ptryBest1 = trys.m_half1.m_ptryBest;
-		IndividualTrys::Try *ptryBest2 = trys.m_half2.m_ptryBest;
-		encodingTry.m_fError = trys.m_half1.m_ptryBest->m_fError + trys.m_half2.m_ptryBest->m_fError;
-    
-    #include 'vpx_dsp/vpx_dsp_common.h'
-    
-    #   define C_MULBYSCALAR( c, s ) \
-      do{ (c).r =  S_MUL( (c).r , s ) ;\
-          (c).i =  S_MUL( (c).i , s ) ; }while(0)
-    
-    #define QCONST16(x,bits) (x)
-#define QCONST32(x,bits) (x)
-    
-      void options(InternalOptionList& _return) {
-    // Your implementation goes here
-    printf('options\n');
+          // Releases ownership of the object without decrementing the reference count.
+  // The caller now owns the returned reference.
+  PyObjectStruct* release() {
+    PyObject* p = ptr_;
+    ptr_ = NULL;
+    return p;
   }
     
+      decode_data.AddString(1, 'abcdefghIJ', 'abcdefghIJ');
+  decode_data.AddString(3, 'abcdefghIJ', '_AbcdefghIJ');
+  decode_data.AddString(2, 'abcdefghIJ', 'Abcd_EfghIJ');
+  decode_data.AddString(4, 'abcdefghIJ', 'ABCD__EfghI_j');
+  decode_data.AddString(1000,
+                        'longFieldNameIsLooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong1000',
+                        'long_field_name_is_looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong_1000');
     
-    {
-    {    std::ostringstream oss;
-    std::copy(line.begin() + options_index,
-              line.end(),
-              std::ostream_iterator<std::string>(oss, ' '));
-    r['options'] = oss.str();
-    results.push_back(r);
-  }
+    #include <google/protobuf/stubs/logging.h>
+#include <google/protobuf/stubs/common.h>
+    
+    void MapLiteTestUtil::SetMapFields(unittest::TestMapLite* message) {
+  MapTestUtilImpl::SetMapFields<unittest::MapEnumLite,
+                                unittest::MAP_ENUM_BAR_LITE,
+                                unittest::MAP_ENUM_BAZ_LITE>(message);
 }
     
-      enum class IsEnabledError {
-    CallFailed = 1,
-    IncorrectResponseFormat = 2,
-    IncorrectValue = 3
-  };
-  Expected<bool, Killswitch::IsEnabledError> isEnabled(const std::string& key);
+    std::string ReadFile(const std::string& name) {
+  std::ifstream file(name.c_str());
+  GOOGLE_CHECK(file.is_open()) << 'Couldn't find file ''
+      << name
+      << '', please make sure you are running this command from the benchmarks'
+      << ' directory.\n';
+  return std::string((std::istreambuf_iterator<char>(file)),
+                     std::istreambuf_iterator<char>());
+}
+    
+        std::vector<std::pair<string,string>> option_pairs;
+    ParseGeneratorParameter(parameter, &option_pairs);
+    
+      {
+    // Read the existing address book.
+    fstream input(argv[1], ios::in | ios::binary);
+    if (!input) {
+      cout << argv[1] << ': File not found.  Creating a new file.' << endl;
+    } else if (!address_book.ParseFromIstream(&input)) {
+      cerr << 'Failed to parse address book.' << endl;
+      return -1;
+    }
+  }
+    
+    We add our current vertex to the list.
+We check the seg from current vertex to next vertex for intersections
+  - if no intersections, follow to next vertex and continue
+  - if intersections, pick one with minimum distance
+    - if more than one, pick one with 'rightest' next point (two possibilities for each)
+    
+      FT_LOCAL( void )
+  af_latin_hints_link_segments( AF_GlyphHints  hints,
+                                FT_UInt        width_count,
+                                AF_WidthRec*   widths,
+                                AF_Dimension   dim );
+    
+    #include 'opus_types.h'
+#include 'opus_defines.h'
+    
+    #define opus_ifft(_st, _fin, _fout, arch) \
+   ((void)(arch), opus_ifft_neon(_st, _fin, _fout))
+    
+    /** Arithmetic shift-right of a 16-bit value */
+#define SHR16(a,shift) ((a) >> (shift))
+/** Arithmetic shift-left of a 16-bit value */
+#define SHL16(a,shift) ((opus_int16)((opus_uint16)(a)<<(shift)))
+/** Arithmetic shift-right of a 32-bit value */
+#define SHR32(a,shift) ((a) >> (shift))
+/** Arithmetic shift-left of a 32-bit value */
+#define SHL32(a,shift) ((opus_int32)((opus_uint32)(a)<<(shift)))
+    
+    #include <regex>
+#include <iostream>
+    
+    using namespace CalculatorApp::Common::Automation;
+using namespace Platform;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Automation;
+using namespace Windows::UI::Xaml::Automation::Peers;
+    
+      // When an actor (column family) requests a stop token, all writes will be
+  // stopped until the stop token is released (deleted)
+  std::unique_ptr<WriteControllerToken> GetStopToken();
+  // When an actor (column family) requests a delay token, total delay for all
+  // writes to the DB will be controlled under the delayed write rate. Every
+  // write needs to call GetDelay() with number of bytes writing to the DB,
+  // which returns number of microseconds to sleep.
+  std::unique_ptr<WriteControllerToken> GetDelayToken(
+      uint64_t delayed_write_rate);
+  // When an actor (column family) requests a moderate token, compaction
+  // threads will be increased
+  std::unique_ptr<WriteControllerToken> GetCompactionPressureToken();
+    
+    int main() {
+  Options options;
+  options.create_if_missing = true;
+  // Disable RocksDB background compaction.
+  options.compaction_style = kCompactionStyleNone;
+  // Small slowdown and stop trigger for experimental purpose.
+  options.level0_slowdown_writes_trigger = 3;
+  options.level0_stop_writes_trigger = 5;
+  options.IncreaseParallelism(5);
+  options.listeners.emplace_back(new FullCompactor(options));
+    }
+    
+      const char* Name() const override { return 'MyFilter'; }
+    
+      // Write a key OUTSIDE of transaction
+  db->Put(write_options, 'abc', 'xyz');
     
     
-    {  // This value is used by EnterExitJoiner, final return value of the syscall
-  // is placed here as a result of join().
-  // Also this member is used by EnterExitJoiner to preserve the age of the
-  // event.
-  __s32 return_value;
+    {  virtual ~TransactionDBMutexFactory() {}
 };
     
-        // out_of_range.401
-    try
+    /*
+ * Class:     org_rocksdb_BackupableDBOptions
+ * Method:    backupLogFiles
+ * Signature: (J)Z
+ */
+jboolean Java_org_rocksdb_BackupableDBOptions_backupLogFiles(JNIEnv* /*env*/,
+                                                             jobject /*jobj*/,
+                                                             jlong jhandle) {
+  auto* bopt = reinterpret_cast<rocksdb::BackupableDBOptions*>(jhandle);
+  return bopt->backup_log_files;
+}
+    
+        if (ret)
     {
-        // try to use a an invalid array index
-        json::reference ref = j.at('/array/4'_json_pointer);
+        ret->autorelease();
     }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
+    
+    
+/** @class JumpBy
+ * @brief Moves a Node object simulating a parabolic jump movement by modifying it's position attribute.
+*/
+class CC_DLL JumpBy : public ActionInterval
+{
+public:
+    /** 
+     * Creates the action.
+     * @param duration Duration time, in seconds.
+     * @param position The jumping distance.
+     * @param height The jumping height.
+     * @param jumps The jumping times.
+     * @return An autoreleased JumpBy object.
+     */
+    static JumpBy* create(float duration, const Vec2& position, float height, int jumps);
     }
+    
+        tHashElement *element = nullptr;
+    HASH_FIND_PTR(_targets, &target, element);
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    /**
+ @brief This action simulates a page turn from the bottom right hand corner of the screen.
+ 
+ @details It's not much use by itself but is used by the PageTurnTransition.
+         Based on an original paper by L Hong et al.
+         http://www.parc.com/publication/1638/turning-pages-of-3d-electronic-books.html
+  
+ @since v0.8.2
+ */
+class CC_DLL PageTurn3D : public Grid3DAction
+{
+public:
+    /**
+     * @js NA 
+     */
+    virtual GridBase* getGrid() override;
+    }
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __CC_ANIMATION_CACHE_H__
+#define __CC_ANIMATION_CACHE_H__
+    
+        _quadsToDraw = itemsToRender;
