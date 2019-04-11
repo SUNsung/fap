f482f90a30917888b751979b2ca1d4dc3b62ee9e
SUNsung <weatherius@gmail.com>
2019-04-11 14:20:11 +0300

    Rewrite [17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py] File in: 2019-04-11 14:20:10

--- pyton/17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py
+++ pyton/17u5ixgruYBsY17.ybIpKnXe.Y17TjXzhIogm6M172qf.py
@@ -1,45 +1,188 @@
 
         
-            def shuffle(self):
-        pass
-
+        import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     
     
-if __name__ == '__main__':
-    HitCounts.run()
-
+def main():
+    parser = optparse.OptionParser(usage='%prog INFILE OUTFILE')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error('Expected an input and an output filename')
     
-        def __str__(self):
-        defaults = dict(type(self).__dict__)
-        actual = dict(defaults)
-        actual.update(self.__dict__)
-        actual['config'] = self.config
-        return repr_dict_nice(dict(
-            (key, value)
-            for key, value in actual.items()
-            if not key.startswith('_'))
+    ie_template = '''
+class {name}({bases}):
+    _VALID_URL = {valid_url!r}
+    _module = '{module}'
+'''
+    
+        diropts = []
+    for opt in opts_dir:
+        if opt._short_opts:
+            diropts.extend(opt._short_opts)
+        if opt._long_opts:
+            diropts.extend(opt._long_opts)
+    
+        def test_comments(self):
+        'Skipping: Not yet fully implemented'
+        return
+        jsi = JSInterpreter('''
+        function x() {
+            var x = /* 1 + */ 2;
+            var y = /* 30
+            * 40 */ 50;
+            return x + y;
+        }
+        ''')
+        self.assertEqual(jsi.call_function('x'), 52)
+    
+        def test_socks5(self):
+        self.assertTrue(isinstance(self._get_ip('socks5'), compat_str))
+    
+    
+@functools.lru_cache()
+def get_hstore_oids(connection_alias):
+    '''Return hstore and hstore array OIDs.'''
+    with connections[connection_alias].cursor() as cursor:
+        cursor.execute(
+            'SELECT t.oid, typarray '
+            'FROM pg_type t '
+            'JOIN pg_namespace ns ON typnamespace = ns.oid '
+            'WHERE typname = 'hstore''
         )
+        oids = []
+        array_oids = []
+        for row in cursor:
+            oids.append(row[0])
+            array_oids.append(row[1])
+        return tuple(oids), tuple(array_oids)
     
-        '''
-    # The value that should be passed to --auth-type
-    # to use this auth plugin. Eg. 'my-auth'
-    auth_type = None
+        # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    '''
+    dirname = 'cifar-10-batches-py'
+    origin = 'https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz'
+    path = get_file(dirname, origin=origin, untar=True)
+    
+        # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
     
     
-def test_default_options_overwrite(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http('--json', httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['json'] == {'foo': 'bar'}
+class L1L2(Regularizer):
+    '''Regularizer for L1 and L2 regularization.
     
-    error_msg = None
     
-        def test_non_existent_file_raises_parse_error(self, httpbin):
-        with pytest.raises(ParseError):
-            http('--form',
-                 'POST', httpbin.url + '/post', 'foo@/__does_not_exist__')
+def test_reuters():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = reuters.load_data()
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        assert len(x_train) + len(x_test) == 11228
+        (x_train, y_train), (x_test, y_test) = reuters.load_data(maxlen=10)
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        word_index = reuters.get_word_index()
+        assert isinstance(word_index, dict)
     
-        def _get_path(self):
-        '''Return the config file path without side-effects.'''
-        raise NotImplementedError()
+        def recurrent_conv(self, x, w):
+        conv_out = K.conv2d(x, w, strides=(1, 1),
+                            padding='same',
+                            data_format=self.data_format)
+        return conv_out
+    
+    Introduced in TensorFlow 1.3, the Dataset API is now the
+standard method for loading data into TensorFlow models.
+A Dataset is a sequence of elements, which are themselves
+composed of tf.Tensor components. For more details, see:
+https://www.tensorflow.org/programmers_guide/datasets
+    
+    image_size = x_train.shape[1]
+x_train = np.reshape(x_train, [-1, image_size, image_size, 1])
+x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
+x_train = x_train.astype('float32') / 255
+x_test = x_test.astype('float32') / 255
+    
+    scores = model.evaluate(x_test, y_test, verbose=0)
+print('IRNN test score:', scores[0])
+print('IRNN test accuracy:', scores[1])
+
+    
+    Get to 99.8% test accuracy after 5 epochs
+for the first five digits classifier
+and 99.2% for the last five digits after transfer + fine-tuning.
+'''
+    
+        def proxy_headers(self, proxy):
+        '''Returns a dictionary of the headers to add to any request sent
+        through a proxy. This works with urllib3 magic to ensure that they are
+        correctly sent to the proxy, rather than in a tunnelled request if
+        CONNECT is being used.
+    
+    
+class HTTPDigestAuth(AuthBase):
+    '''Attaches HTTP Digest Authentication to the given Request object.'''
+    
+        with server as (host, port):
+        url = 'http://{}:{}/path/to/thing/#view=edit&token=hunter2'.format(host, port)
+        r = requests.get(url)
+        raw_request = r.content
+    
+        # Client Error.
+    400: ('bad_request', 'bad'),
+    401: ('unauthorized',),
+    402: ('payment_required', 'payment'),
+    403: ('forbidden',),
+    404: ('not_found', '-o-'),
+    405: ('method_not_allowed', 'not_allowed'),
+    406: ('not_acceptable',),
+    407: ('proxy_authentication_required', 'proxy_auth', 'proxy_authentication'),
+    408: ('request_timeout', 'timeout'),
+    409: ('conflict',),
+    410: ('gone',),
+    411: ('length_required',),
+    412: ('precondition_failed', 'precondition'),
+    413: ('request_entity_too_large',),
+    414: ('request_uri_too_large',),
+    415: ('unsupported_media_type', 'unsupported_media', 'media_type'),
+    416: ('requested_range_not_satisfiable', 'requested_range', 'range_not_satisfiable'),
+    417: ('expectation_failed',),
+    418: ('im_a_teapot', 'teapot', 'i_am_a_teapot'),
+    421: ('misdirected_request',),
+    422: ('unprocessable_entity', 'unprocessable'),
+    423: ('locked',),
+    424: ('failed_dependency', 'dependency'),
+    425: ('unordered_collection', 'unordered'),
+    426: ('upgrade_required', 'upgrade'),
+    428: ('precondition_required', 'precondition'),
+    429: ('too_many_requests', 'too_many'),
+    431: ('header_fields_too_large', 'fields_too_large'),
+    444: ('no_response', 'none'),
+    449: ('retry_with', 'retry'),
+    450: ('blocked_by_windows_parental_controls', 'parental_controls'),
+    451: ('unavailable_for_legal_reasons', 'legal_reasons'),
+    499: ('client_closed_request',),
+    
+    
+class Session(SessionRedirectMixin):
+    '''A Requests session.
+    
+        :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    '''
+    
+        parser = argparse.ArgumentParser(description = 'Download all the PDF/HTML links into README.md')
+    parser.add_argument('-d', action='store', dest='directory')
+    parser.add_argument('--no-html', action='store_true', dest='nohtml', default = False)
+    parser.add_argument('--overwrite', action='store_true', default = False)    
+    results = parser.parse_args()
+    
+    def main(loc, colorscheme):
