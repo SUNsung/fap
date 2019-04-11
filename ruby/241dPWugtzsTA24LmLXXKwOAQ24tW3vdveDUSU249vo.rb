73dabcd8e6d8a04cc27db95d7576726c420865aa
SUNsung <weatherius@gmail.com>
2019-04-11 05:26:52 +0300

    Rewrite [241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb] File in: 2019-04-11 05:26:52

--- ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
+++ ruby/241dPWugtzsTA24LmLXXKwOAQ24tW3vdveDUSU249vo.rb
@@ -1,248 +1,152 @@
 
         
-          </body>
-</html>
-HTML
-CONTENT_NOT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+            # The path used after resending confirmation instructions.
+    def after_resending_confirmation_instructions_path_for(resource_name)
+      is_navigational_format? ? new_session_path(resource_name) : '/'
+    end
     
-        def execute(command: nil, target_object: nil)
-      action_name = command.method_name
-      action_class_ref = class_ref_for_action(named: action_name)
-      parameter_map = {}
-      closure_argument_value = nil
+    require 'minitest/autorun'
     
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            prefix: '#{prefix}',
-          })
-        end').runner.execute(:test)
-    
-            it 'executes the correct git command' do
-          allow(Fastlane::Actions).to receive(:sh).with('git add #{path}', anything).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: '#{path}')
-          end').runner.execute(:test)
+    module Devise
+  module Controllers
+    # Provide sign in and sign out functionality.
+    # Included by default in all controllers.
+    module SignInOut
+      # Return true if the given scope is signed in session. If no scope given, return
+      # true if any scope is signed in. This will run authentication hooks, which may
+      # cause exceptions to be thrown from this method; if you simply want to check
+      # if a scope has already previously been authenticated without running
+      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
+      def signed_in?(scope=nil)
+        [scope || Devise.mappings.keys].flatten.any? do |_scope|
+          warden.authenticate?(scope: _scope)
         end
       end
     
-    def clamp(f)
-  i = f * 255.5
-  if i > 255.0 then
-    i = 255.0
-  end
-  if i < 0.0 then
-    i = 0.0
-  end
-  i.to_i
-end
+    module Devise
+  module Models
+    # Rememberable manages generating and clearing token for remembering the user
+    # from a saved cookie. Rememberable also has utility methods for dealing
+    # with serializing the user into the cookie and back from the cookie, trying
+    # to lookup the record based on the saved information.
+    # You probably wouldn't use rememberable methods directly, they are used
+    # mostly internally for handling the remember token.
+    #
+    # == Options
+    #
+    # Rememberable adds the following options in devise_for:
+    #
+    #   * +remember_for+: the time you want the user will be remembered without
+    #     asking for credentials. After this time the user will be blocked and
+    #     will have to enter their credentials again. This configuration is also
+    #     used to calculate the expires time for the cookie created to remember
+    #     the user. By default remember_for is 2.weeks.
+    #
+    #   * +extend_remember_period+: if true, extends the user's remember period
+    #     when remembered via cookie. False by default.
+    #
+    #   * +rememberable_options+: configuration options passed to the created cookie.
+    #
+    # == Examples
+    #
+    #   User.find(1).remember_me!  # regenerating the token
+    #   User.find(1).forget_me!    # clearing the token
+    #
+    #   # generating info to put into cookies
+    #   User.serialize_into_cookie(user)
+    #
+    #   # lookup the user based on the incoming cookie information
+    #   User.serialize_from_cookie(cookie_string)
+    module Rememberable
+      extend ActiveSupport::Concern
     
-    answer = to_array(solution).map do |p|
-  to_string(p)
-end
-    
-    mkpieces
-mkboard
-$p[4] = [$p[4][0]]
-$pnum = (0...$p.length).to_a
-setpiece([],0)
-
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
+      ruby_version_is '2.6' do
+    it 'can be used with jit' do
+      ruby_exe('p :OK', options: '--enable=jit 2>&1').chomp.should == ':OK'
+      ruby_exe('p :OK', options: '--disable=jit 2>&1').chomp.should == ':OK'
+      ruby_exe('p :OK', options: '--enable-jit 2>&1').chomp.should == ':OK'
+      ruby_exe('p :OK', options: '--disable-jit 2>&1').chomp.should == ':OK'
     end
   end
-end
-
     
-    Then /^I should have (\d+) nsfw posts$/ do |num_posts|
-  page.should have_css('.nsfw-shield', count: num_posts.to_i)
-end
+      it 'is not called for instance methods' do
+    ScratchPad.record []
     
-      gem.licenses      = ['MIT']
-    
-        def set_if_empty(key, value=nil, &block)
-      set(key, value, &block) unless keys.include?(key)
+        def get_binding
+      get_binding_and_line[0]
     end
     
-          def response
-        return @response if defined? @response
-    
-          def using_default_scm?
-        return @using_default_scm if defined? @using_default_scm
-        @using_default_scm = (fetch(:scm) == DEFAULT_GIT)
-      end
-    
-          def with(properties)
-        properties.each { |key, value| add_property(key, value) }
-        self
-      end
-    
-          def add_host(host, properties={})
-        new_host = Server[host]
-        new_host.port = properties[:port] if properties.key?(:port)
-        # This matching logic must stay in sync with `Server#matches?`.
-        key = ServerKey.new(new_host.hostname, new_host.port)
-        existing = servers_by_key[key]
-        if existing
-          existing.user = new_host.user if new_host.user
-          existing.with(properties)
-        else
-          servers_by_key[key] = new_host.with(properties)
-        end
-      end
-    
-    # We use a special :_default_git value so that SCMResolver can tell whether the
-# default has been replaced by the user via `set`.
-set_if_empty :scm, Capistrano::Configuration::SCMResolver::DEFAULT_GIT
-set_if_empty :branch, 'master'
-set_if_empty :deploy_to, -> { '/var/www/#{fetch(:application)}' }
-set_if_empty :tmp_dir, '/tmp'
-    
-          def string_to_code(string)
-        # sha bytes
-        b = [Digest::SHA1.hexdigest(string)[0, 20]].pack('H*').bytes.to_a
-        # Thanks donpark's IdenticonUtil.java for this.
-        # Match the following Java code
-        # ((b[0] & 0xFF) << 24) | ((b[1] & 0xFF) << 16) |
-        #	 ((b[2] & 0xFF) << 8) | (b[3] & 0xFF)
-    
-          # Returns page content without title if it was extracted.
-      #
-      def content_without_page_header(content)
-        doc = build_document(content)
-          if @h1_title
-            title = find_header_node(doc)
-            title.remove unless title.empty?
-          end
-        # .inner_html will cause href escaping on UTF-8
-        doc.css('div#gollum-root').children.to_xml(@@to_xml)
-      end
-    end
-  end
-end
-
-    
-      test 'clean path with leading slash' do
-    assert_equal '/Mordor', clean_path('/Mordor')
+        bind.local_variable_defined?(:foo).should == true
   end
     
-          def preference_field_for(form, field, options)
-        case options[:type]
-        when :integer
-          form.text_field(field, preference_field_options(options))
-        when :boolean
-          form.check_box(field, preference_field_options(options))
-        when :string
-          form.text_field(field, preference_field_options(options))
-        when :password
-          form.password_field(field, preference_field_options(options))
-        when :text
-          form.text_area(field, preference_field_options(options))
-        else
-          form.text_field(field, preference_field_options(options))
-        end
-      end
+      # staged_path not available in Installer/Uninstall Stanza, workaround by nesting with preflight/postflight
+  # see https://github.com/Homebrew/homebrew-cask/pull/8887
+  # and https://github.com/Homebrew/homebrew-cask-versions/pull/296
     
-          within_row(1) do
-        expect(page).to have_content(order1.number)
-      end
-    
-    module Spree
-  module Api
-    class BaseController < ActionController::Base
-      protect_from_forgery unless: -> { request.format.json? || request.format.xml? }
-    
-            def scope
-          if params[:country_id]
-            @country = Country.accessible_by(current_ability, :read).find(params[:country_id])
-            @country.states.accessible_by(current_ability, :read).order('name ASC')
-          else
-            State.accessible_by(current_ability, :read).order('name ASC')
-          end
-        end
-      end
-    end
+      # Configure static asset server for tests with Cache-Control for performance.
+  if config.respond_to?(:serve_static_files)
+    # rails >= 4.2
+    config.serve_static_files = true
+  elsif config.respond_to?(:serve_static_assets)
+    # rails < 4.2
+    config.serve_static_assets = true
   end
-end
-
+  config.static_cache_control = 'public, max-age=3600'
     
-                result = next_service.call(order: spree_current_order)
+        remove_duplicates
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
     
-        platforms.each do |platform|
-      logger.info('Generating service manifest.', :platform => platform.class.name)
-      platform.program = command.first
-      platform.name = attributes[:pleaserun_name]
-      platform.args = command[1..-1]
-      platform.description = if attributes[:description_given?]
-        attributes[:description]
-      else
-        platform.name
-      end
-      pleaserun_attributes.each do |attribute_name|
-        attribute = 'pleaserun_#{attribute_name}'.to_sym
-        if attributes.has_key?(attribute) and not attributes[attribute].nil?
-          platform.send('#{attribute_name}=', attributes[attribute])
-        end
-      end
+    #   Copyright (c) 2010-2011, Diaspora Inc.  This file is
+#   licensed under the Affero General Public License version 3 or later.  See
+#   the COPYRIGHT file.
     
-        # Copy 'files' from builddir to :output/files
-    Find.find('files', 'manifests') do |path|
-      logger.info('Copying path: #{path}')
-      if File.directory?(path)
-        ::Dir.mkdir(File.join(params[:output], path))
-      else
-        FileUtils.cp(path, File.join(params[:output], path))
+          @conv2 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv2.id)
+             .increase_unread(alice)
+    
+        context 'on a post from a contact' do
+      before do
+        aspect_to_post = bob.aspects.where(:name => 'generic').first
+        @post = bob.post :status_message, :text => 'something', :to => aspect_to_post
       end
-    end
-  end # def build!
     
       def auth_session_error(exception)
     Rails.logger.info 'AuthSessionError: #{exception.class}: #{exception.message}'
     redirect_to login_path(:return_to => request.fullpath)
   end
     
-      def new
-    @credential = @server.credentials.build
-  end
-    
-      before_action do
-    if params[:server_id]
-      @server = organization.servers.present.find_by_permalink!(params[:server_id])
-      params[:id] && @domain = @server.domains.find_by_uuid!(params[:id])
+      def create
+    @route = @server.routes.build(safe_params)
+    if @route.save
+      redirect_to_with_json [organization, @server, :routes]
     else
-      params[:id] && @domain = organization.domains.find_by_uuid!(params[:id])
+      render_form_errors 'new', @route
     end
   end
     
-      def new
-    @route = @server.routes.build
-  end
-    
+      def update
+    extra_params = [:spam_threshold, :spam_failure_threshold, :postmaster_address]
+    extra_params += [:send_limit, :allow_sender, :log_smtp_data, :outbound_spam_threshold, :message_retention_days, :raw_message_retention_days, :raw_message_retention_size] if current_user.admin?
+    if @server.update(safe_params(*extra_params))
+      redirect_to_with_json organization_server_path(organization, @server), :notice => 'Server settings have been updated'
+    else
+      render_form_errors 'edit', @server
     end
-
-    
-      def destroy
-    @webhook.destroy
-    redirect_to_with_json [organization, @server, :webhooks]
   end
     
-    task :spec
+      def toggle_ssl
+    @track_domain.update(:ssl_enabled => !@track_domain.ssl_enabled)
+    redirect_to_with_json [organization, @server, :track_domains], :notice => 'SSL settings for #{@track_domain.full_name} updated successfully.'
+  end
     
-            if presenter
-          embeds = { env: env }
-          embeds[:version] = env[Grape::Env::API_VERSION] if env[Grape::Env::API_VERSION]
-          presented_message = presenter.represent(presented_message, embeds).serializable_hash
+          address_endpoints = server.address_endpoints.order(:address).to_a
+      if address_endpoints.present?
+        s << '<optgroup label='Address Endpoints'>'
+        for endpoint in address_endpoints
+          value = '#{endpoint.class}##{endpoint.uuid}'
+          selected = value == selected_value ? 'selected='selected'' : ''
+          s << '<option value='#{value}' #{selected}>#{endpoint.address}</option>'
         end
+        s << '</optgroup>'
+      end
