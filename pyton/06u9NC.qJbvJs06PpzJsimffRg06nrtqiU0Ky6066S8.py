bbac88bae8e10882070c8f48e20d3b81f2f86856
SUNsung <weatherius@gmail.com>
2019-04-11 23:51:51 +0300

    Rewrite [06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py] File in: 2019-04-11 23:51:51

--- pyton/06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py
+++ pyton/06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py
@@ -1,162 +1,39 @@
 
         
-        containers = (('thefuck/python3-tcsh',
-               u'''FROM python:3
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'),
-              ('thefuck/python2-tcsh',
-               u'''FROM python:2
-                   RUN apt-get update
-                   RUN apt-get install -yy tcsh''',
-               u'tcsh'))
+            def send_friend_request(self, friend_id):
+        pass
     
+        def remove_link_to_crawl(self, url):
+        '''Remove the given link from `links_to_crawl`.'''
+        pass
     
-@pytest.mark.parametrize('command', [
-    Command('apt-cache search foo', ''),
-    Command('aptitude search foo', ''),
-    Command('apt search foo', ''),
-    Command('apt-get install foo', ''),
-    Command('apt-get source foo', ''),
-    Command('apt-get clean', ''),
-    Command('apt-get remove', ''),
-    Command('apt-get update', ''),
-    Command('sudo apt update', no_match_output)
-])
-def test_not_match(command):
-    assert not match(command)
-    
-    no_match_output = '''
-Listing... Done
-'''
-    
-        good_nonce = jose.encode_b64jose(b'foo')
-    wrong_nonce = u'F'
-    # Following just makes sure wrong_nonce is wrong
-    try:
-        jose.b64decode(wrong_nonce)
-    except (ValueError, TypeError):
-        assert True
-    else:
-        assert False  # pragma: no cover
-    
-        '''
-    return_vars = {}
-    # Get list of words in the variable
-    a_opts = util.get_var_from_file(varname, filepath).split()
-    for i, v in enumerate(a_opts):
-        # Handle Define statements and make sure it has an argument
-        if v == '-D' and len(a_opts) >= i+2:
-            var_parts = a_opts[i+1].partition('=')
-            return_vars[var_parts[0]] = var_parts[2]
-        elif len(v) > 2 and v.startswith('-D'):
-            # Found var with no whitespace separator
-            var_parts = v[2:].partition('=')
-            return_vars[var_parts[0]] = var_parts[2]
-    return return_vars
-    
-    MANAGED_COMMENT = 'DO NOT REMOVE - Managed by Certbot'
-MANAGED_COMMENT_ID = MANAGED_COMMENT+', VirtualHost id: {0}'
-'''Managed by Certbot comments and the VirtualHost identification template'''
+        def parse(self, response):
+        pass
 
     
-            self.assertTrue(self.vhost1.conflicts([self.addr2]))
-        self.assertFalse(self.vhost1.conflicts([self.addr_default]))
+            parser.add_option_group(group)
     
-            self.configurator.parser.add_include(
-            self.configurator.parser.loc['default'], self.challenge_conf)
-        self.configurator.reverter.register_file_creation(
-            True, self.challenge_conf)
+        def long_desc(self):
+        return ('Edit a spider using the editor defined in the EDITOR environment'
+                ' variable or else the EDITOR setting')
     
-            return new_key
+    from scrapy.commands import ScrapyCommand
+from scrapy.http import Request
+from scrapy.exceptions import UsageError
+from scrapy.utils.datatypes import SequenceExclude
+from scrapy.utils.spider import spidercls_for_request, DefaultSpider
     
+                        if not cb:
+                        logger.error('Cannot find a rule that matches %(url)r in spider: %(spider)s',
+                                 {'url': response.url, 'spider': spider.name})
+                        return
+                else:
+                    cb = 'parse'
     
-class QuadraticProbing(HashTable):
-    '''
-        Basic Hash Table example with open addressing using Quadratic Probing 
-    '''
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    def longestSub(ARRAY): 			#This function is recursive
-	
-	ARRAY_LENGTH = len(ARRAY)
-	if(ARRAY_LENGTH <= 1):  	#If the array contains only one element, we return it (it's the stop condition of recursion)
-		return ARRAY
-								#Else
-	PIVOT=ARRAY[0]
-	isFound=False
-	i=1
-	LONGEST_SUB=[]
-	while(not isFound and i<ARRAY_LENGTH):
-		if (ARRAY[i] < PIVOT):
-			isFound=True
-			TEMPORARY_ARRAY = [ element for element in ARRAY[i:] if element >= ARRAY[i] ]
-			TEMPORARY_ARRAY = longestSub(TEMPORARY_ARRAY)
-			if ( len(TEMPORARY_ARRAY) > len(LONGEST_SUB) ):
-				LONGEST_SUB = TEMPORARY_ARRAY
-		else:
-			i+=1
-    
-    	hexrep = format(i,'08x')
-	thing = ''
-	for i in [3,2,1,0]:
-		thing += hexrep[2*i:2*i+2]
-	return thing
-    
-      3. find centroids and clusters using kmeans function.
-  
-        centroids, cluster_assignment = kmeans(
-            X, 
-            k, 
-            initial_centroids, 
-            maxiter=400,
-            record_heterogeneity=heterogeneity, 
-            verbose=True # whether to print logs in console or not.(default=False)
-            )
-  
-  
-  4. Plot the loss function, hetrogeneity values for every iteration saved in hetrogeneity list.
-        plot_heterogeneity(
-            heterogeneity, 
-            k
-        )
-  
-  5. Have fun..
-  
-'''
-from __future__ import print_function
-from sklearn.metrics import pairwise_distances
-import numpy as np
-    
-        def _update_info(self):
-        '''Check for connected devices.'''
-        _LOGGER.info('Checking Linksys Smart Wifi')
-    
-        def get_device_name(self, device):
-        '''Return the name of the given device or None if we don't know.'''
-        if self.mac2name is None:
-            result = self._retrieve_list_with_retry()
-            if result:
-                hosts = [x for x in result
-                         if 'mac' in x and 'name' in x]
-                mac2name_list = [
-                    (x['mac'].upper(), x['name']) for x in hosts]
-                self.mac2name = dict(mac2name_list)
-            else:
-                # Error, handled in the _retrieve_list_with_retry
-                return
-        return self.mac2name.get(device.upper(), None)
-    
-        def _get_room(self, room):
-        '''Get Room object, creating it if necessary.'''
-        from hipnotify import Room
-        if room not in self._rooms:
-            self._rooms[room] = Room(
-                token=self._token, room_id=room, endpoint_url=self._host)
-        return self._rooms[room]
-    
-    _LOGGER = logging.getLogger(__name__)
-    
-      Args:
-    line: A line of a string.
+        # Stripping scripts and comments is slow (about 20x slower than
+    # just checking if a string is in text); this is a quick fail-fast
+    # path that should work for most pages.
+    if 'fragment' not in text:
+        return False
+    if 'content' not in text:
+        return False
