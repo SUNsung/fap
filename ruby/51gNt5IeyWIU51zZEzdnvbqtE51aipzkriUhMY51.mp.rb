4cf049b7ad30d7d95beb91c4c76b4cfeebd1829f
SUNsung <weatherius@gmail.com>
2019-04-12 02:24:34 +0300

    Rewrite [51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb] File in: 2019-04-12 02:24:33

--- ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
+++ ruby/51gNt5IeyWIU51zZEzdnvbqtE51aipzkriUhMY51.mp.rb
@@ -1,117 +1,21 @@
 
         
-              it 'deactivates an existing user' do
-        visit admin_users_path
-        expect(page).to have_no_text('inactive')
-        find(:css, 'a[href='/admin/users/#{users(:bob).id}/deactivate']').click
-        expect(page).to have_text('inactive')
-        users(:bob).reload
-        expect(users(:bob)).not_to be_active
-      end
-    
-      describe 'DotHelper::DotDrawer' do
-    describe '#id' do
-      it 'properly escapes double quotaion and backslash' do
-        expect(DotHelper::DotDrawer.draw(foo: '') {
-          id('hello\\'')
-        }).to eq(''hello\\\\\\''')
-      end
-    end
-  end
-end
-
-    
-              weather_agent = existing_scenario.agents.find_by(:guid => 'a-weather-agent')
-          expect(weather_agent.name).to eq('updated name')
-          expect(weather_agent.schedule).to eq('6pm')
-          expect(weather_agent.keep_events_for).to eq(2.days.to_i)
-          expect(weather_agent).not_to be_disabled
-          expect(weather_agent.options).to eq(weather_agent_options.merge('api_key' => 'foo'))
-        end
-    
-    describe ConvertWebsiteAgentTemplateForMerge do
-  let :old_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title' }
-    }
-  end
-    
-      describe '#working?' do
-    it 'should not be working until the first event was received' do
-      expect(@checker).not_to be_working
-      @checker.last_receive_at = Time.now
-      expect(@checker).to be_working
-    end
-  end
-    
-      def up_down(change)
-    change.up do
-      Mention.update_all(mentions_container_type: 'Post')
-      change_column :mentions, :mentions_container_type, :string, null: false
-      Notification.where(type: 'Notifications::Mentioned').update_all(type: 'Notifications::MentionedInPost')
+            def last?
+      index == tab.panes.length - 1
     end
     
-    When /^I toggle all nsfw posts$/ do
-  all('a.toggle_nsfw_state').each &:click
-end
+        it 'renders the template' do
+      expect(File).to receive(:read).at_least(:once) { 'wemux ls 2>/dev/null' }
     
-          # Install the gems to make them available locally when bundler does his local resolution
-      post_install_messages = []
-      pack.gems.each do |packed_gem|
-        PluginManager.ui.debug('Installing, #{packed_gem.name}, version: #{packed_gem.version} file: #{packed_gem.file}')
-        post_install_messages << LogStash::PluginManager::GemInstaller::install(packed_gem.file, packed_gem.plugin?)
-      end
+          @custom_name = options[:custom_name]
     
-        # remove any version constrain from the Gemfile so the plugin(s) can be updated to latest version
-    # calling update without requirements will remove any previous requirements
-    plugins = plugins_to_update(previous_gem_specs_map)
-    # Skipping the major version validation when using a local cache as we can have situations
-    # without internet connection.
-    filtered_plugins = plugins.map { |plugin| gemfile.find(plugin) }
-      .compact
-      .reject { |plugin| REJECTED_OPTIONS.any? { |key| plugin.options.has_key?(key) } }
-      .each   { |plugin| gemfile.update(plugin.name) }
-    
-        desc 'Halt all VM's involved in the acceptance test round'
-    task :halt, :platform do |t, args|
-      config   = PlatformConfig.new
-      experimental = (ENV['LS_QA_EXPERIMENTAL_OS'].to_s.downcase || 'false') == 'true'
-      machines = config.select_names_for(args[:platform], {'experimental' => experimental})
-    
-        mkdir_p page_dir
-    file = '#{page_dir}/#{filename}.#{extension}'
-    if File.exist?(file)
-      abort('rake aborted!') if ask('#{file} already exists. Do you want to overwrite?', ['y', 'n']) == 'n'
-    end
-    puts 'Creating new page: #{file}'
-    open(file, 'w') do |page|
-      page.puts '---'
-      page.puts 'layout: page'
-      page.puts 'title: \'#{title}\''
-      page.puts 'date: #{Time.now.strftime('%Y-%m-%d %H:%M')}'
-      page.puts 'comments: true'
-      page.puts 'sharing: true'
-      page.puts 'footer: true'
-      page.puts '---'
-    end
-  else
-    puts 'Syntax error: #{args.filename} contains unsupported characters'
-  end
-end
-    
-        # Loops through the list of category pages and processes each one.
-    def write_category_indexes
-      if self.layouts.key? 'category_index'
-        dir = self.config['category_dir'] || 'categories'
-        self.categories.keys.each do |category|
-          self.write_category_index(File.join(dir, category.to_url), category)
-        end
-    
-      # Extracts raw content DIV from template, used for page description as {{ content }}
-  # contains complete sub-template code on main page level
-  def raw_content(input)
-    /<div class='entry-content'>(?<content>[\s\S]*?)<\/div>\s*<(footer|\/article)>/ =~ input
-    return (content.nil?) ? input : content
-  end
+      s.add_development_dependency 'activesupport', '< 5.0.0' # Please see issue #432
+  s.add_development_dependency 'awesome_print', '~> 1.2'
+  s.add_development_dependency 'bundler', '>= 1.3'
+  s.add_development_dependency 'coveralls', '~> 0.8'
+  s.add_development_dependency 'factory_bot', '~> 4.8'
+  s.add_development_dependency 'pry', '~> 0.10'
+  s.add_development_dependency 'rake', '~> 10.4'
+  s.add_development_dependency 'rspec', '~> 3.3'
+  s.add_development_dependency 'rubocop', '~> 0.48.1'
+  s.add_development_dependency 'simplecov', '~> 0.16'
