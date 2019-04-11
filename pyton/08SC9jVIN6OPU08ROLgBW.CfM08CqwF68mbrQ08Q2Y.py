e065629c4a8d866f61db31cfee639a540d063cf7
SUNsung <weatherius@gmail.com>
2019-04-12 00:40:07 +0300

    Rewrite [08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py] File in: 2019-04-12 00:40:06

--- pyton/08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py
+++ pyton/08SC9jVIN6OPU08ROLgBW.CfM08CqwF68mbrQ08Q2Y.py
@@ -1,172 +1,66 @@
 
         
-        DEPLABEL_TO_ID = {label: lid for lid, label in enumerate(DEPLABELS)}
+        
+def rainbow():
+    cs = ['cyan', 'yellow', 'green', 'magenta', 'red', 'blue',
+          'intense_cyan', 'intense_yellow', 'intense_green',
+          'intense_magenta', 'intense_red', 'intense_blue']
     
-    def _plot_item(W, name, full_name, nspaces):
-  plt.figure()
-  if W.shape == ():
-    print(name, ': ', W)
-  elif W.shape[0] == 1:
-    plt.stem(W.T)
-    plt.title(full_name)
-  elif W.shape[1] == 1:
-    plt.stem(W)
-    plt.title(full_name)
-  else:
-    plt.imshow(np.abs(W), interpolation='nearest', cmap='jet');
-    plt.colorbar()
-    plt.title(full_name)
-    
-      Yields:
-    Pairs of the batched data, each a matrix of shape [batch_size, num_steps].
-    The second element of the tuple is the same data time-shifted to the
-    right by one. The third is a set of weights with 1 indicating a word was
-    present and 0 not.
-    
-      for sequence in sequences:
-    indices = []
-    for embedding in sequence:
-      indices.append(np.argmax(embedding))
-    batch_of_indices.append(indices)
-  return batch_of_indices
-    
-      Args:
-    hparams:  Hyperparameters for the MaskGAN.
-    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
-    is_training:  Whether the model is training.
-    reuse (Optional):  Whether to reuse the model.
+        tls_attr_name = 'PROTOCOL_{}'.format(compose_tls_version)
+    if not hasattr(ssl, tls_attr_name):
+        log.warn(
+            'The '{}' protocol is unavailable. You may need to update your '
+            'version of Python or OpenSSL. Falling back to TLSv1 (default).'
+            .format(compose_tls_version)
+        )
+        return None
     
     
-def create_dis_train_op(hparams, dis_loss, global_step):
-  '''Create Discriminator train op.'''
-  with tf.name_scope('train_discriminator'):
-    dis_optimizer = tf.train.AdamOptimizer(hparams.dis_learning_rate)
-    dis_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('dis')
-    ]
-    if FLAGS.dis_update_share_embedding and FLAGS.dis_share_embedding:
-      shared_embedding = [
-          v for v in tf.trainable_variables()
-          if v.op.name == 'gen/decoder/rnn/embedding'
-      ][0]
-      dis_vars.append(shared_embedding)
-    print('\nOptimizing Discriminator vars:')
-    for v in dis_vars:
-      print(v)
-    dis_grads = tf.gradients(dis_loss, dis_vars)
-    dis_grads_clipped, _ = tf.clip_by_global_norm(dis_grads,
-                                                  FLAGS.grad_clipping)
-    dis_train_op = dis_optimizer.apply_gradients(
-        zip(dis_grads_clipped, dis_vars), global_step=global_step)
-    return dis_train_op, dis_grads_clipped, dis_vars
-    
-        elif FLAGS.discriminator_model == 'seq2seq_vd':
-      load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-      print('Restoring Discriminator from %s.' % load_ckpt)
-      tf.logging.info('Restoring Discriminator from %s.' % load_ckpt)
-      dis_encoder_init_saver = init_savers['dis_encoder_init_saver']
-      dis_decoder_init_saver = init_savers['dis_decoder_init_saver']
-      dis_encoder_init_saver.restore(sess, load_ckpt)
-      dis_decoder_init_saver.restore(sess, load_ckpt)
-    
-        with tf.variable_scope('rnn') as vs:
-      outputs, _, _ = tf.contrib.rnn.static_bidirectional_rnn(
-          cell_fwd, cell_bwd, rnn_inputs, state_fwd, state_bwd, scope=vs)
-    
-        def __init__(self, debuglevel=0):
-        self._init_github_account()
-        https_handler = make_HTTPS_handler({}, debuglevel=debuglevel)
-        self._opener = compat_urllib_request.build_opener(https_handler)
+def get_service_names(links):
+    return [link.split(':')[0] for link in links]
     
     
-def openssl_encode(algo, key, iv):
-    cmd = ['openssl', 'enc', '-e', '-' + algo, '-K', hex_str(key), '-iv', hex_str(iv)]
-    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, _ = prog.communicate(secret_msg)
-    return out
+class VolumeConfigChangedError(ConfigurationError):
+    def __init__(self, local, property_name, local_value, remote_value):
+        super(VolumeConfigChangedError, self).__init__(
+            'Configuration for volume {vol_name} specifies {property_name} '
+            '{local_value}, but a volume with the same name uses a different '
+            '{property_name} ({remote_value}). If you wish to use the new '
+            'configuration, please remove the existing volume '{full_name}' '
+            'first:\n$ docker volume rm {full_name}'.format(
+                vol_name=local.name, property_name=property_name,
+                local_value=local_value, remote_value=remote_value,
+                full_name=local.true_name
+            )
+        )
     
-    new_version = {}
+            with pytest.raises(ConfigurationError):
+            net.ensure()
     
+    import os
     
-class TestIqiyiSDKInterpreter(unittest.TestCase):
-    def test_iqiyi_sdk_interpreter(self):
-        '''
-        Test the functionality of IqiyiSDKInterpreter by trying to log in
+            assert fake_log.error.call_count == 1
+        assert '123' in fake_log.error.call_args[0][0]
     
-      * dynamodb
-'''
+        def test_nosigning_register(self):
+        res = self.do_register(
+            headers={
+                signing.SIGNATURE_UA_HEADER: None,
+                signing.SIGNATURE_BODY_HEADER: None,
+            },
+            expect_errors=True,
+        )
+        self.assert_403_response(res, 'signing.ua.invalid.invalid_format')
     
+        # IE<8 XSS
+    def test_invalid_function(self):
+        testcase = u'*{color:expression(alert(1));}'
+        self.assertInvalid(testcase)
     
-def test_match():
-    command = Command('brew install sshfs', output)
-    assert match(command)
+        def test_attr_whitelist(self):
+        testcase = '<div><a><a><em onclick='alert(1)'>FOO!</em></a></a></div>'
+        self.assertFragmentRaises(testcase, SoupUnsupportedAttrError)
     
-    
-def _is_not_okay_to_test():
-    return 'elasticsearch' not in _get_formulas()
-    
-        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
-    plt.plot(sample_sizes, one_core, label='one core')
-    plt.plot(sample_sizes, multi_core, label='multi core')
-    plt.xlabel('n_samples')
-    plt.ylabel('Time (s)')
-    plt.title('Parallel %s' % func.__name__)
-    plt.legend()
-    
-        gc.collect()
-    
-        class_name = info['fullname'].split('.')[0]
-    if type(class_name) != str:
-        # Python 2 only
-        class_name = class_name.encode('utf-8')
-    module = __import__(info['module'], fromlist=[class_name])
-    obj = attrgetter(info['fullname'])(module)
-    
-        for line in input_file:
-        linestrip = line.strip()
-        if len(linestrip) == 0:
-            in_exercise_region = False
-        elif linestrip.startswith('# TASK:'):
-            in_exercise_region = True
-    
-    from sklearn.datasets import make_checkerboard
-from sklearn.datasets import samples_generator as sg
-from sklearn.cluster.bicluster import SpectralBiclustering
-from sklearn.metrics import consensus_score
-    
-    from sklearn.cluster import AgglomerativeClustering
-from sklearn.metrics import pairwise_distances
-    
-        def report_out_of_quota(self, appid):
-        self.logger.warn('report_out_of_quota:%s', appid)
-        with self.lock:
-            if appid not in self.out_of_quota_appids:
-                self.out_of_quota_appids.append(appid)
-            try:
-                self.working_appid_list.remove(appid)
-            except:
-                pass
-    
-            if not any(os.path.isfile('%s/certutil' % x) for x in os.environ['PATH'].split(os.pathsep)):
-            xlog.warn('please install *libnss3-tools* package to import GoAgent root ca')
-            return False
-    
-        def __init__(self, input=None):
-        Exception.__init__(self)
-    
-        if len(unknown_face_encodings) > 0:
-        face_found = True
-        # See if the first face in the uploaded image matches the known face of Obama
-        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
-        if match_results[0]:
-            is_obama = True
-    
-        :param css:  plain tuple representation of the rect in (top, right, bottom, left) order
-    :param image_shape: numpy shape of the image array
-    :return: a trimmed plain tuple representation of the rect in (top, right, bottom, left) order
-    '''
-    return max(css[0], 0), min(css[1], image_shape[1]), min(css[2], image_shape[0]), max(css[3], 0)
-    
-    # Create a PIL imagedraw object so we can draw on the picture
-pil_image = Image.fromarray(image)
-d = ImageDraw.Draw(pil_image)
+        def test_no_extension(self):
+        u = UrlParser('http://example.com/a')
+        self.assertEquals('', u.path_extension())
