e8e00c57d72ecbd7e491fb192c270968e9c87261
SUNsung <weatherius@gmail.com>
2019-04-11 17:17:19 +0300

    Rewrite [285pdbLTvRI5s28GmRzf8mXYAU28cu4sHxDV.p628OzJ.cpp] File in: 2019-04-11 17:17:19

--- cccccc/285pdbLTvRI5s28GmRzf8mXYAU28cu4sHxDV.p628OzJ.cpp
+++ cccccc/285pdbLTvRI5s28GmRzf8mXYAU28cu4sHxDV.p628OzJ.cpp
@@ -1,32 +1,165 @@
 
         
-                // before version 1, there was no distributed state per se. Instead, the external state
-        // contained a dictionary of worker-specific external states.
-        if (version == 0)
-        {
-            auto key = externalState.Contains(localWorkerId) ? localWorkerId : mainWorkerId;
-            return externalState[key].Value<Dictionary>();
-        }
+        private:
+    NetworkStyle(const QString &appName, const int iconColorHueShift, const int iconColorSaturationReduction, const char *titleAddText);
     
-    void DataReader::CopyMBLayoutTo(MBLayoutPtr pMBLayout)
+    BOOST_AUTO_TEST_CASE(bip173_testvectors_valid)
 {
-    // BUGBUG: This copies all data reader's layout info on top of each other, keeping only the last one; likely not what was intended.
-    for (size_t i = 0; i < m_ioNames.size(); i++)
-        m_dataReaders[m_ioNames[i]]->CopyMBLayoutTo(pMBLayout);
+    static const std::string CASES[] = {
+        'A12UEL5L',
+        'a12uel5l',
+        'an83characterlonghumanreadablepartthatcontainsthenumber1andtheexcludedcharactersbio1tt5tgs',
+        'abcdef1qpzry9x8gf2tvdw0s3jn54khce6mua7lmqqqxw',
+        '11qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqc8247j',
+        'split1checkupstagehandshakeupstreamerranterredcaperred2y9e3w',
+        '?1ezyfcl',
+    };
+    for (const std::string& str : CASES) {
+        auto ret = bech32::Decode(str);
+        BOOST_CHECK(!ret.first.empty());
+        std::string recode = bech32::Encode(ret.first, ret.second);
+        BOOST_CHECK(!recode.empty());
+        BOOST_CHECK(CaseInsensitiveEqual(str, recode));
+    }
 }
     
-    // base class that we can catch, independent of the type parameter
-struct /*interface*/ IExceptionWithCallStackBase
+    const std::string& UniValue::get_str() const
 {
-    virtual const char * CallStack() const = 0;
-    virtual ~IExceptionWithCallStackBase() noexcept = default;
-};
+    if (typ != VSTR)
+        throw std::runtime_error('JSON value is not a string as expected');
+    return getValStr();
+}
     
-    class ScopeTimer
-{
-    Timer m_aggregateTimer;
-    size_t m_verbosity;
-    std::string m_message;
+        if (!bytes) return;
+    
+    TegraBinaryOp_Invoker(cmpEQ, cmpEQ)
+TegraBinaryOp_Invoker(cmpNE, cmpNE)
+TegraBinaryOp_Invoker(cmpGT, cmpGT)
+TegraBinaryOp_Invoker(cmpGE, cmpGE)
+TegraGenOp_Invoker(cmpLT, cmpGT, 2, 1, 0, RANGE_DATA(ST, src2_data, src2_step), src2_step, \
+                                          RANGE_DATA(ST, src1_data, src1_step), src1_step, \
+                                          RANGE_DATA(DT, dst1_data, dst1_step), dst1_step)
+TegraGenOp_Invoker(cmpLE, cmpGE, 2, 1, 0, RANGE_DATA(ST, src2_data, src2_step), src2_step, \
+                                          RANGE_DATA(ST, src1_data, src1_step), src1_step, \
+                                          RANGE_DATA(DT, dst1_data, dst1_step), dst1_step)
+#define TEGRA_CMP(type, src1, sz1, src2, sz2, dst, sz, w, h, op) \
+( \
+    CAROTENE_NS::isSupportedConfiguration() ? \
+        ((op) == cv::CMP_EQ) ? \
+        parallel_for_(Range(0, h), \
+        TegraGenOp_cmpEQ_Invoker<const type, CAROTENE_NS::u8>(src1, sz1, src2, sz2, dst, sz, w, h), \
+        (w * h) / static_cast<double>(1<<16)), \
+        CV_HAL_ERROR_OK : \
+        ((op) == cv::CMP_NE) ? \
+        parallel_for_(Range(0, h), \
+        TegraGenOp_cmpNE_Invoker<const type, CAROTENE_NS::u8>(src1, sz1, src2, sz2, dst, sz, w, h), \
+        (w * h) / static_cast<double>(1<<16)), \
+        CV_HAL_ERROR_OK : \
+        ((op) == cv::CMP_GT) ? \
+        parallel_for_(Range(0, h), \
+        TegraGenOp_cmpGT_Invoker<const type, CAROTENE_NS::u8>(src1, sz1, src2, sz2, dst, sz, w, h), \
+        (w * h) / static_cast<double>(1<<16)), \
+        CV_HAL_ERROR_OK : \
+        ((op) == cv::CMP_GE) ? \
+        parallel_for_(Range(0, h), \
+        TegraGenOp_cmpGE_Invoker<const type, CAROTENE_NS::u8>(src1, sz1, src2, sz2, dst, sz, w, h), \
+        (w * h) / static_cast<double>(1<<16)), \
+        CV_HAL_ERROR_OK : \
+        ((op) == cv::CMP_LT) ? \
+        parallel_for_(Range(0, h), \
+        TegraGenOp_cmpLT_Invoker<const type, CAROTENE_NS::u8>(src1, sz1, src2, sz2, dst, sz, w, h), \
+        (w * h) / static_cast<double>(1<<16)), \
+        CV_HAL_ERROR_OK : \
+        ((op) == cv::CMP_LE) ? \
+        parallel_for_(Range(0, h), \
+        TegraGenOp_cmpLE_Invoker<const type, CAROTENE_NS::u8>(src1, sz1, src2, sz2, dst, sz, w, h), \
+        (w * h) / static_cast<double>(1<<16)), \
+        CV_HAL_ERROR_OK : \
+        CV_HAL_ERROR_NOT_IMPLEMENTED \
+    : CV_HAL_ERROR_NOT_IMPLEMENTED \
+)
+    
+        void operator() (const typename VecTraits<s32>::vec64 & v_src0,
+                     const typename VecTraits<s32>::vec64 & v_src1,
+                     typename VecTraits<s32>::vec64 & v_dst) const
+    {
+        float32x2_t vs1 = vcvt_f32_s32(v_src0);
+        float32x2_t vs2 = vcvt_f32_s32(v_src1);
     }
     
-    #include 'LinearAlgebraNodes.h'
+        for (size_t i = 0u; i < size.height; ++i)
+    {
+        const u8 * src = internal::getRowPtr(srcBase, srcStride, i);
+        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t sj = 0u, dj = 0u;
+    }
+    
+        for (size_t i = 0; i < size.height; ++i)
+    {
+        const u8 * src = internal::getRowPtr(srcBase, srcStride, i);
+        s16 * dst = internal::getRowPtr(dstBase, dstStride, i);
+        size_t j = 0;
+    }
+    
+                for (; i <= lim; i += 16)
+            {
+                internal::prefetch(src0 + i);
+                internal::prefetch(src1 + i);
+    }
+    
+    
+    {            vec128 v_src = vld1q(src + js);
+            vec128 v_dst = vrev64q(v_src);
+            v_dst = vcombine(vget_high(v_dst), vget_low(v_dst));
+            vst1q(dst + jd - step_base, v_dst);
+        }
+        for (; js < roiw_tail; js += step_tail, jd -= step_tail)
+        {
+            vec64 v_src = vld1(src + js);
+            vst1(dst + jd - step_tail, vrev64(v_src));
+        }
+    
+    #include 'vtransform.hpp'
+    
+    // Overloaded PrintTo() for tuples of various arities.  We support
+// tuples of up-to 10 fields.  The following implementation works
+// regardless of whether tr1::tuple is implemented using the
+// non-standard variadic template feature or not.
+    
+    // 4-ary predicate assertion macros.
+#define EXPECT_PRED_FORMAT4(pred_format, v1, v2, v3, v4) \
+  GTEST_PRED_FORMAT4_(pred_format, v1, v2, v3, v4, GTEST_NONFATAL_FAILURE_)
+#define EXPECT_PRED4(pred, v1, v2, v3, v4) \
+  GTEST_PRED4_(pred, v1, v2, v3, v4, GTEST_NONFATAL_FAILURE_)
+#define ASSERT_PRED_FORMAT4(pred_format, v1, v2, v3, v4) \
+  GTEST_PRED_FORMAT4_(pred_format, v1, v2, v3, v4, GTEST_FATAL_FAILURE_)
+#define ASSERT_PRED4(pred, v1, v2, v3, v4) \
+  GTEST_PRED4_(pred, v1, v2, v3, v4, GTEST_FATAL_FAILURE_)
+    
+    
+    { private:
+  String();  // Not meant to be instantiated.
+};  // class String
+    
+    // The TypeList template makes it possible to use either a single type
+// or a Types<...> list in TYPED_TEST_CASE() and
+// INSTANTIATE_TYPED_TEST_CASE_P().
+    
+      // If we are given the --check_for_leaks command line flag, installs the
+  // leak checker.
+  if (check_for_leaks) {
+    TestEventListeners& listeners = UnitTest::GetInstance()->listeners();
+    }
+    
+    
+// A simple string class.
+class MyString {
+ private:
+  const char* c_string_;
+  const MyString& operator=(const MyString& rhs);
+    }
+    
+      // Set should work when the input pointer is the same as the one
+  // already in the MyString object.
+  s.Set(s.c_string());
+  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
