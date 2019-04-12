e4f383ff80450086ad0008cae988a12a77c9bd18
SUNsung <weatherius@gmail.com>
2019-04-12 12:17:34 +0300

    Rewrite [50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp] File in: 2019-04-12 12:17:34

--- cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
+++ cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
@@ -1,168 +1,206 @@
 
         
-          std::vector<string> output;
-  string input_tensors_needed_out;
-  tensorflow::Status status = RunCppShapeInferenceImpl(
-      graph_def_version, serialized_node_def, input_serialized_shapes,
-      input_constant_tensor_values_v, input_constant_tensor_as_shape_values,
-      &output, &input_tensors_needed_out);
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+#define TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
     
-    #include <vector>
-#include 'tensorflow/c/tf_status_helper.h'
-#include 'tensorflow/core/platform/types.h'
+     protected:
+  // Print: def Function(parameters):
+  void AddDefLine(const string& function_name, const string& parameters);
+  void AddDefLine(const string& parameters);
     
-    #include <string>
-#include <vector>
-#include 'tensorflow/core/framework/op_def.pb.h'
-#include 'tensorflow/core/framework/op_gen_lib.h'
-#include 'tensorflow/core/platform/types.h'
-    
-    #ifndef TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_H_
-#define TENSORFLOW_PYTHON_LIB_CORE_NDARRAY_TENSOR_H_
-    
-    
-    {  // Maps error codes to the corresponding Python exception type.
-  std::map<TF_Code, PyObject*> exc_types_;
-};
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-      static const unsigned kMask = 0xf;  // Mask of all available flags.
-    
-    	// ################################################################################
-	// Block4x4EncodingBits_A8
-	// Encoding bits for the A portion of RGBA8
-	// ################################################################################
-    
-    		// use the best of each half
-		IndividualTrys::Try *ptryBest1 = trys.m_half1.m_ptryBest;
-		IndividualTrys::Try *ptryBest2 = trys.m_half2.m_ptryBest;
-		encodingTry.m_fError = trys.m_half1.m_ptryBest->m_fError + trys.m_half2.m_ptryBest->m_fError;
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
     
-    {#if defined (__cplusplus)
+    {  return Status::OK();
 }
-#endif
     
-    #if defined(OPUS_ARM_INLINE_ASM)
-#include 'arm/kiss_fft_armv4.h'
-#endif
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    /*!
- * Initialize/reset the resampler state for a given pair of input/output sampling rates
-*/
-opus_int silk_resampler_init(
-    silk_resampler_state_struct *S,                 /* I/O  Resampler state                                             */
-    opus_int32                  Fs_Hz_in,           /* I    Input sampling rate (Hz)                                    */
-    opus_int32                  Fs_Hz_out,          /* I    Output sampling rate (Hz)                                   */
-    opus_int                    forEnc              /* I    If 1: encoder; if 0: decoder                                */
-);
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    #include 'opencensus/stats/stats.h'
-#include 'src/cpp/ext/filters/census/grpc_plugin.h'
-    
-    namespace grpc {
+    namespace tensorflow {
     }
     
-    Status ProtoServerReflection::GetAllExtensionNumbers(
-    ServerContext* context, const grpc::string& type,
-    ExtensionNumberResponse* response) {
-  if (descriptor_pool_ == nullptr) {
-    return Status::CANCELLED;
+    // Safe containers for an owned TF_Tensor. On destruction, the tensor will be
+// deleted by TF_DeleteTensor.
+using Safe_TF_TensorPtr = std::unique_ptr<TF_Tensor, detail::TFTensorDeleter>;
+Safe_TF_TensorPtr make_safe(TF_Tensor* tensor);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#include 'tensorflow/python/util/kernel_registry.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+      // Acquire the per-Entry mutex without holding the map mutex. Initializing
+  // an Executor may be expensive, so we want to allow concurrent
+  // initialization of different entries.
+  mutex_lock lock{entry->configurations_mutex};
+  for (const auto& iter : entry->configurations) {
+    if (iter.first.plugin_config == config.plugin_config &&
+        iter.first.device_options == config.device_options) {
+      VLOG(2) << 'hit in cache';
+      return iter.second.get();
+    }
   }
-    }
     
-      void FillErrorResponse(const Status& status,
-                         reflection::v1alpha::ErrorResponse* error_response);
-    
-    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
-    const grpc::string& name, int value) {
-  class IntOption final : public ServerBuilderOption {
-   public:
-    IntOption(const grpc::string& name, int value)
-        : name_(name), value_(value) {}
-    }
-    }
-    
-    
-    {}  // namespace grpc
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  gpr_log(GPR_ERROR,
-          'Platforms other than Linux, Windows, and MacOS are not supported.');
-  return std::make_pair(busy, total);
-}
-    
-    grpc::string LoadRecordKey::GetClientIpBytes() const {
-  if (client_ip_hex_.empty()) {
-    return '';
-  } else if (client_ip_hex_.size() == kIpv4AddressLength) {
-    uint32_t ip_bytes;
-    if (sscanf(client_ip_hex_.c_str(), '%x', &ip_bytes) != 1) {
-      gpr_log(GPR_ERROR,
-              'Can't parse client IP (%s) from a hex string to an integer.',
-              client_ip_hex_.c_str());
-      return '';
-    }
-    ip_bytes = grpc_htonl(ip_bytes);
-    return grpc::string(reinterpret_cast<const char*>(&ip_bytes),
-                        sizeof(ip_bytes));
-  } else if (client_ip_hex_.size() == kIpv6AddressLength) {
-    uint32_t ip_bytes[4];
-    for (size_t i = 0; i < 4; ++i) {
-      if (sscanf(client_ip_hex_.substr(i * 8, (i + 1) * 8).c_str(), '%x',
-                 ip_bytes + i) != 1) {
-        gpr_log(
-            GPR_ERROR,
-            'Can't parse client IP part (%s) from a hex string to an integer.',
-            client_ip_hex_.substr(i * 8, (i + 1) * 8).c_str());
-        return '';
-      }
-      ip_bytes[i] = grpc_htonl(ip_bytes[i]);
-    }
-    return grpc::string(reinterpret_cast<const char*>(ip_bytes),
-                        sizeof(ip_bytes));
-  } else {
-    GPR_UNREACHABLE_CODE(return '');
-  }
-}
-    
-    class MyFilter : public rocksdb::CompactionFilter {
+    // FFT support interface -- this can be derived from a GPU executor when the
+// underlying platform has an FFT library implementation available. See
+// StreamExecutor::AsFft().
+//
+// This support interface is not generally thread-safe; it is only thread-safe
+// for the CUDA platform (cuFFT) usage; host side FFT support is known
+// thread-compatible, but not thread-safe.
+class FftSupport {
  public:
-  bool Filter(int level, const rocksdb::Slice& key,
-              const rocksdb::Slice& existing_value, std::string* new_value,
-              bool* value_changed) const override {
-    fprintf(stderr, 'Filter(%s)\n', key.ToString().c_str());
-    ++count_;
-    assert(*value_changed == false);
-    return false;
+  virtual ~FftSupport() {}
+    }
+    
+        // out_of_range.109
+    try
+    {
+        // try to use an array index that is not a number
+        json::const_reference ref = j.at('/array/one'_json_pointer);
+    }
+    catch (json::parse_error& e)
+    {
+        std::cout << e.what() << '\n';
+    }
+    
+    void ObjectExtendedInfo60D::Parse(const std::uint8_t* bytes, int32_t length,
+                                  ContiRadar* conti_radar) const {
+  int obj_id = object_id(bytes, length);
+  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
+    if (conti_radar->contiobs(i).obstacle_id() == obj_id) {
+      auto obs = conti_radar->mutable_contiobs(i);
+      obs->set_longitude_accel(longitude_accel(bytes, length));
+      obs->set_lateral_accel(lateral_accel(bytes, length));
+      obs->set_oritation_angle(oritation_angle(bytes, length));
+      obs->set_length(object_length(bytes, length));
+      obs->set_width(object_width(bytes, length));
+      obs->set_obstacle_class(obstacle_class(bytes, length));
+      break;
+    }
   }
+  // auto conti_obs = conti_radar->mutable_contiobs(object_id(bytes, length));
+}
+    
+    TEST(TestPiecewiseLinearKernel, add_second_order_derivative_matrix) {
+  PiecewiseLinearKernel kernel(10, 0.1);
+  const double init_derivative = 5.0;
     }
     
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    {class DbUndumpTool {
- public:
-  bool Run(const UndumpOptions& undump_options,
-           rocksdb::Options options = rocksdb::Options());
-};
-}  // namespace rocksdb
-#endif  // ROCKSDB_LITE
-
-    
-    namespace rocksdb {
+    namespace apollo {
+namespace canbus {
+namespace gem {
+    }
+    }
     }
     
-      // Comparator used to define the order of keys in the table.
-  // Default: a comparator that uses lexicographic byte-wise ordering
-  //
-  // REQUIRES: The client must ensure that the comparator supplied
-  // here has the same name and orders keys *exactly* the same as the
-  // comparator provided to previous open calls on the same DB.
-  const Comparator* comparator;
+    // config detail: {'name': 'usr_can_read_errors', 'offset': 0.0,
+// 'precision': 1.0, 'len': 16, 'is_signed_var': False, 'physical_range':
+// '[0|65535]', 'bit': 55, 'type': 'int', 'order': 'motorola', 'physical_unit':
+// ''}
+int Globalrpt6a::usr_can_read_errors(const std::uint8_t* bytes,
+                                     int32_t length) const {
+  Byte t0(bytes + 6);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    Context::~Context()
+{
+    if (stack_)
+    {
+        swTraceLog(SW_TRACE_COROUTINE, 'free stack: ptr=%p', stack_);
+        if (protect_page_)
+        {
+            unprotect_stack(stack_, protect_page_);
+        }
+    }
+    }
+    
+        signals:
+        void finished();
+    
+            //保存长度值
+        memcpy(ptr, &size, sizeof(size));
+        //在指针末尾保存一个串号
+        memcpy((char*) ptr + size - 4, &(send_pkg.serial_num), sizeof(send_pkg.serial_num));
+    
+    TEST(coroutine_base, yield_resume)
+{
+    long _cid;
+    long cid = Coroutine::create([](void *arg)
+    {
+        long cid = Coroutine::get_current_cid();
+        Coroutine *co = Coroutine::get_by_cid(cid);
+        co->yield();
+        *(long *) arg = Coroutine::get_current_cid();
+    }, &_cid);
+    }
+    
+        coro_test({
+        make_pair([](void *arg)
+        {
+            auto chan = (Channel *) arg;
+    }
+    }
+    
+        virtual void onStart() = 0;
+    virtual void onShutdown() = 0;
+    virtual void onWorkerStart(int worker_id) = 0;
+    virtual void onWorkerStop(int worker_id) = 0;
+    virtual void onReceive(int fd, const DataBuffer &data) = 0;
+    virtual void onConnect(int fd) = 0;
+    virtual void onClose(int fd) = 0;
+    virtual void onPacket(const DataBuffer &, ClientInfo &) = 0;
+    virtual void onPipeMessage(int src_worker_id, const DataBuffer &) = 0;
+    virtual void onTask(int, int, const DataBuffer &) = 0;
+    virtual void onFinish(int, const DataBuffer &) = 0;
+    
+        long buf;
+    read(_pipe, (void*) &buf, sizeof(buf));
+    close(_pipe);
+    
+        swSignalfd_init();
+    swSignal_add(SIGUSR1,sig_usr1);
+    swSignalfd_setup(SwooleG.main_reactor);
