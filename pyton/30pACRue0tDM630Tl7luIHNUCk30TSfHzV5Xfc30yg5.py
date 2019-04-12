8fe2105bf308be896f5640617f56d1f562bceed6
SUNsung <weatherius@gmail.com>
2019-04-12 04:25:28 +0300

    Rewrite [30pACRue0tDM630Tl7luIHNUCk30TSfHzV5Xfc30yg5.py] File in: 2019-04-12 04:25:27

--- pyton/30pACRue0tDM630Tl7luIHNUCk30TSfHzV5Xfc30yg5.py
+++ pyton/30pACRue0tDM630Tl7luIHNUCk30TSfHzV5Xfc30yg5.py
@@ -1,24 +1,45 @@
 
         
-            def score(self):
-        min_over = sys.MAXSIZE
-        max_under = -sys.MAXSIZE
-        for score in self.possible_scores():
-            if self.BLACKJACK < score < min_over:
-                min_over = score
-            elif max_under < score <= self.BLACKJACK:
-                max_under = score
-        return max_under if max_under != -sys.MAXSIZE else min_over
+            def extract_url(self, line):
+        '''Extract the generated url from the log line.'''
+        pass
     
+        def crawl_page(self, page):
+        for url in page.child_urls:
+            self.data_store.add_link_to_crawl(url)
+        self.reverse_index_queue.generate(page)
+        self.doc_index_queue.generate(page)
+        self.data_store.remove_link_to_crawl(page.url)
+        self.data_store.insert_crawled_link(page.url, page.signature)
     
-class SpendingByCategory(MRJob):
+        def __init__(self, handler=None, host='localhost', port=0, requests_to_handle=1, wait_to_close_event=None):
+        super(Server, self).__init__()
     
-        def mapper(self, _, line):
-        yield line, 1
+            Literal:                   '#000000',        # class: 'l'
+        Literal.Date:              '#000000',        # class: 'ld'
     
+                sock.close()
     
-class PersonServer(object):
+            >>> from_key_val_list([('key', 'val')])
+        OrderedDict([('key', 'val')])
+        >>> from_key_val_list('string')
+        ValueError: cannot encode objects that are not 2-tuples
+        >>> from_key_val_list({'key': 'val'})
+        OrderedDict([('key', 'val')])
     
-        def __init__(self, key, value):
-        self.key = key
-        self.value = value
+                if ret[i, 0] != i_label:  # 样本的类别发生了改变
+                changed = True
+    
+        Args:
+        x(tf.Tensor):
+        n_unit(int): 
+        act_fn:
+        name(str):
+        reuse(bool):
+    '''
+    # n_input = tf.shape(x)[-1]  # err: need int but tensor
+    n_input = int(x.get_shape()[-1])
+    with tf.variable_scope(name or 'dense', reuse=reuse):
+        W, b = get_wb([n_input, n_unit])
+        o = act_fn(tf.matmul(x, W) + b)
+    return o
