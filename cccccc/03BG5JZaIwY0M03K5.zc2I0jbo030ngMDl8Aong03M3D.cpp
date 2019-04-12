ba42c32dccb52fe437a9bbdfb0b9a63df715f01a
SUNsung <weatherius@gmail.com>
2019-04-12 20:17:11 +0300

    Rewrite [03BG5JZaIwY0M03K5.zc2I0jbo030ngMDl8Aong03M3D.cpp] File in: 2019-04-12 20:17:10

--- cccccc/03BG5JZaIwY0M03K5.zc2I0jbo030ngMDl8Aong03M3D.cpp
+++ cccccc/03BG5JZaIwY0M03K5.zc2I0jbo030ngMDl8Aong03M3D.cpp
@@ -1,251 +1,185 @@
 
         
-        #include <carotene/definitions.hpp>
-#include <stdint.h>
-#include <cstddef>
-    
-    #endif
-    
-        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0, const typename internal::VecTraits<T>::vec64 & v_src1,
-              typename internal::VecTraits<T>::unsign::vec64 & v_dst) const
-    {
-        v_dst = internal::vcge(v_src0, v_src1);
-    }
-    
-    #endif
+        
+    {}  // namespace auto_updater
 
     
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(f32, s32, 8,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.32 {d4-d5}, [%[src1]]                              \n\t'
-            'vld1.32 {d6-d7}, [%[src2]]                              \n\t'
-            'vmul.f32 q4, q2, q0                                     \n\t'
-            'vmul.f32 q5, q3, q0                                     \n\t'
-            'vadd.f32 q6, q4, q1                                     \n\t'
-            'vadd.f32 q7, q5, q1                                     \n\t'
-            'vcvt.s32.f32 q4, q6                                     \n\t'
-            'vcvt.s32.f32 q5, q7                                     \n\t'
-            'vst1.32 {d8-d9},   [%[dst1]]                            \n\t'
-            'vst1.32 {d10-d11}, [%[dst2]]                            \n\t'
-            : //no output
-            : [src1] 'r' (_src + i),
-              [src2] 'r' (_src + i + 4),
-              [dst1] 'r' (_dst + i),
-              [dst2] 'r' (_dst + i + 4),
-              'w' (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15'
-        );
-    }
-})
-#else
-CVTS_FUNC(f32, s32, 8,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        float32x4_t vline1_f32 = vld1q_f32(_src + i + 0);
-        float32x4_t vline2_f32 = vld1q_f32(_src + i + 4);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        vst1q_s32(_dst + i + 0, vline1_s32);
-        vst1q_s32(_dst + i + 4, vline2_s32);
-    }
-})
-#endif
+    #endif  // ATOM_BROWSER_NET_ASAR_ASAR_PROTOCOL_HANDLER_H_
+
     
-    
-    {
-    {            if (s[0] < 0 || s[1] < 0)//saturate in case of overflow ~ 4GB of non-zeros...
-            {
-                return 0x7fFFffFF;
-            }
-            result += (s[0] += s[1]);
-            if (s[0] < 0 || result < 0)
-            {
-                return 0x7fFFffFF;
-            }
-        }
-        for (; i < size.width; i++)
-            result += (src[i] != 0)?1:0;
-        if (result < 0)//saturate in case of overflow ~ 4GB of non-zeros...
-        {
-            return 0x7fFFffFF;
-        }
-    }
-    return result;
-#else
-    (void)_size;
-    (void)srcBase;
-    (void)srcStride;
-    
-    #define DOT_FLOAT_BLOCKSIZE (1 << 13)
-    f64 result = 0.0;
-    for (size_t row = 0; row < size.height; ++row)
-    {
-        const f32 * src0 = internal::getRowPtr(src0Base, src0Stride, row);
-        const f32 * src1 = internal::getRowPtr(src1Base, src1Stride, row);
-    }
-    
-    namespace CAROTENE_NS {
-    }
-    
-    void CensusClientCallData::OnDoneRecvMessageCb(void* user_data,
-                                               grpc_error* error) {
-  grpc_call_element* elem = reinterpret_cast<grpc_call_element*>(user_data);
-  CensusClientCallData* calld =
-      reinterpret_cast<CensusClientCallData*>(elem->call_data);
-  CensusChannelData* channeld =
-      reinterpret_cast<CensusChannelData*>(elem->channel_data);
-  GPR_ASSERT(calld != nullptr);
-  GPR_ASSERT(channeld != nullptr);
-  // Stream messages are no longer valid after receiving trailing metadata.
-  if ((*calld->recv_message_) != nullptr) {
-    calld->recv_message_count_++;
-  }
-  GRPC_CLOSURE_RUN(calld->initial_on_done_recv_message_, GRPC_ERROR_REF(error));
-}
-    
-    // Deserialize the incoming SpanContext and generate a new server context based
-// on that. This new span will never be a root span. This should only be called
-// with a blank CensusContext as it overwrites it.
-void GenerateServerContext(absl::string_view tracing, absl::string_view stats,
-                           absl::string_view primary_role,
-                           absl::string_view method, CensusContext* context);
-    
-      // Serializes a GrpcTraceContext into the provided buffer. Returns the number
-  // of bytes serialized into the buffer. If the buffer is not of sufficient
-  // size (it must be at least kGrpcTraceContextSize bytes) it will drop
-  // everything and return 0 bytes serialized. Inlined for performance reasons.
-  static size_t Encode(const GrpcTraceContext& tc, char* buf, size_t buf_size) {
-    if (buf_size < kGrpcTraceContextSize) {
-      return kEncodeDecodeFailure;
-    }
-    buf[kVersionIdOffset] = kVersionId;
-    buf[kTraceIdOffset] = kTraceIdField;
-    memcpy(&buf[kTraceIdOffset + 1], tc.trace_id,
-           opencensus::trace::TraceId::kSize);
-    buf[kSpanIdOffset] = kSpanIdField;
-    memcpy(&buf[kSpanIdOffset + 1], tc.span_id,
-           opencensus::trace::SpanId::kSize);
-    buf[kTraceOptionsOffset] = kTraceOptionsField;
-    memcpy(&buf[kTraceOptionsOffset + 1], tc.trace_options,
-           opencensus::trace::TraceOptions::kSize);
-    return kGrpcTraceContextSize;
+      // PID 1 identifies init. launchd, that is. launchd never starts the
+  // relauncher process directly, having this parent_pid means that the parent
+  // already exited and launchd 'inherited' the relauncher as its child.
+  // There's no reason to synchronize with launchd.
+  if (parent_pid == 1) {
+    LOG(ERROR) << 'unexpected parent_pid';
+    return;
   }
     
-    // A CallData class will be created for every grpc call within a channel. It is
-// used to store data and methods specific to that call. CensusServerCallData is
-// thread-compatible, however typically only 1 thread should be interacting with
-// a call at a time.
-class CensusServerCallData : public CallData {
- public:
-  // Maximum size of server stats that are sent on the wire.
-  static constexpr uint32_t kMaxServerStatsLen = 16;
-    }
     
-    Status ProtoServerReflection::GetFileContainingExtension(
-    ServerContext* context, const ExtensionRequest* request,
-    ServerReflectionResponse* response) {
-  if (descriptor_pool_ == nullptr) {
-    return Status::CANCELLED;
-  }
-    }
-    
-    #include 'src/cpp/server/load_reporter/get_cpu_stats.h'
-    
-    /*!
-* \brief Maximum ndim supported for special operators like broadcasting with non contiguous lhs/rhs
-*/
-#define MXNET_SPECIAL_MAX_NDIM 5
-    
-    /**
- * \brief The class sets caffe's mode before doing forward/backward
- * \tparam xpu The device that the op will be executed on.
- */
-class CaffeMode {
- public:
-  template<typename xpu> static void SetMode();
+    { private:
+  DISALLOW_COPY_AND_ASSIGN(ViewsDelegateMac);
 };
     
-    // specialize define for Layer Parameter
-template<>
-class FieldEntry<caffe::LayerParameter>
-    : public FieldEntryBase<FieldEntry<caffe::LayerParameter>, caffe::LayerParameter> {
- public:
-  // parent class
-  typedef FieldEntryBase<FieldEntry<caffe::LayerParameter>, caffe::LayerParameter> Parent;
+    
+    {}  // namespace nwapi
+    
+    v8::Handle<v8::Value> CallObjectMethod(int routing_id,
+                                       int object_id,
+                                       const std::string& type,
+                                       const std::string& method,
+                                       v8::Handle<v8::Value> args) {
+  v8::Isolate* isolate = v8::Isolate::GetCurrent();
+  scoped_ptr<V8ValueConverter> converter(V8ValueConverter::create());
     }
     
-      virtual bool Next(void) {
-    if (out_ != nullptr) {
-      recycle_queue_.push(out_); out_ = nullptr;
-    }
-    // do recycle
-    if (recycle_queue_.size() == param_.prefetch_buffer) {
-      DataBatch *old_batch =  recycle_queue_.front();
-      // can be more efficient on engine
-      for (NDArray& arr : old_batch->data) {
-        arr.WaitToWrite();
+    #ifndef CONTENT_NW_SRC_API_BINDINGS_COMMON_H_
+#define CONTENT_NW_SRC_API_BINDINGS_COMMON_H_
+    
+      GtkRequisition menu_req;
+  gtk_widget_size_request(GTK_WIDGET(menu), &menu_req);
+  GdkScreen* screen;
+  gdk_display_get_pointer(gdk_display_get_default(), &screen, NULL, NULL, NULL);
+  gint monitor = gdk_screen_get_monitor_at_point(screen, *x, *y);
+    
+    MenuItem::MenuItem(int id,
+                   const base::WeakPtr<ObjectManager>& object_manager,
+                   const base::DictionaryValue& option,
+                   const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id) {
+  Create(option);
+}
+    
+    
+    {  for (AppWindow* window : windows) {
+    if (window->NWCanClose())
+      window->GetBaseWindow()->Close();
+  }
+}
+    
+     protected:
+  ~NwAppGetArgvSyncFunction() override;
+    
+          if (!(data.raw.get() && *(data.raw))) {
+        if (data.type == TYPE_PNG) {
+          encoded_image_base64.insert(0, kPNGDataUriPrefix);
+        } else {
+          DCHECK(data.type == TYPE_JPEG);
+          encoded_image_base64.insert(0, kJPEGDataUriPrefix);
+        }
       }
-      recycle_queue_.pop();
-      iter.Recycle(&old_batch);
-    }
-    return iter.Next(&out_);
-  }
-  virtual const DataBatch &Value(void) const {
-    return *out_;
-  }
     
-    
-    {        ImGui::Render();
+    namespace extensions {
     }
     
-    // Use if you want to reset your rendering device without losing ImGui state.
-IMGUI_IMPL_API void     ImGui_ImplDX9_InvalidateDeviceObjects();
-IMGUI_IMPL_API bool     ImGui_ImplDX9_CreateDeviceObjects();
+    
+    {  nw::ObjectManager* manager = nw::ObjectManager::Get(browser_context());
+  manager->OnAllocateObject(id, type, *options, extension_id());
+  return true;
+}
+    
+    void OneofGenerator::GeneratePublicCasePropertyDeclaration(
+    io::Printer* printer) {
+  printer->Print(
+      variables_,
+      '$comments$'
+      '@property(nonatomic, readonly) $enum_name$ $name$OneOfCase;\n'
+      '\n');
+}
+    
+    // Verify that ByteSink is subclassable and Flush() overridable.
+class FlushingByteSink : public StringByteSink {
+ public:
+  explicit FlushingByteSink(string* dest) : StringByteSink(dest) {}
+  virtual void Flush() { Append('z', 1); }
+ private:
+  GOOGLE_DISALLOW_EVIL_CONSTRUCTORS(FlushingByteSink);
+};
+    
+    string Status::ToString() const {
+  if (error_code_ == error::OK) {
+    return 'OK';
+  } else {
+    if (error_message_.empty()) {
+      return error::CodeEnumToString(error_code_);
+    } else {
+      return error::CodeEnumToString(error_code_) + ':' +
+          error_message_;
+    }
+  }
+}
+    
+    TEST(StatusOr, TestAssignmentStatusOk) {
+  const int kI = 4;
+  StatusOr<int> source(kI);
+  StatusOr<int> target;
+  target = source;
+  EXPECT_EQ(source.status(), target.status());
+  EXPECT_EQ(source.ValueOrDie(), target.ValueOrDie());
+}
+    
+    using google::protobuf::io::FileInputStream;
+using google::protobuf::io::GzipInputStream;
+    
+    // Author: brianolson@google.com (Brian Olson)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+//
+// Test program to verify that GzipOutputStream is compatible with command line
+// gzip or java.util.zip.GzipOutputStream
+//
+// Reads data on standard input and writes compressed gzip stream to standard
+// output.
+    
+    class ExtensionStripper {
+ public:
+  static void StripFile(FileDescriptorProto *file) {
+    for (int i = 0; i < file->mutable_message_type()->size(); i++) {
+      StripMessage(file->mutable_message_type(i));
+    }
+    file->mutable_extension()->Clear();
+  }
+ private:
+  static void StripMessage(DescriptorProto *message_type) {
+    message_type->mutable_extension()->Clear();
+    message_type->clear_extension_range();
+    for (int i = 0; i < message_type->mutable_nested_type()->size(); i++) {
+      StripMessage(message_type->mutable_nested_type(i));
+    }
+  }
+};
+    
+      {
+    // Write the new address book back to disk.
+    fstream output(argv[1], ios::out | ios::trunc | ios::binary);
+    if (!address_book.SerializeToOstream(&output)) {
+      cerr << 'Failed to write address book.' << endl;
+      return -1;
+    }
+  }
+    
+    bool DHTRoutingTable::addNode(const std::shared_ptr<DHTNode>& node)
+{
+  return addNode(node, false);
+}
+    
+      void moveBucketHead(const std::shared_ptr<DHTNode>& node);
+    
+    #define READ_CHECK(fp, ptr, count)                                             \
+  if (fp.read((ptr), (count)) != (count)) {                                    \
+    throw DL_ABORT_EX('Failed to load DHT routing table.');                    \
+  }
+    
+    class DHTNode;
+    
+    namespace aria2 {
+    }
+    
+    #include <memory>
+    
+    
+    {} // namespace aria2
 
     
-    // Implemented features:
-//  [X] Platform: Clipboard support.
-//  [X] Platform: Gamepad support. Enable with 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad'.
-//  [x] Platform: Mouse cursor shape and visibility. Disable with 'io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange'. FIXME: 3 cursors types are missing from GLFW.
-//  [X] Platform: Keyboard arrays indexed using GLFW_KEY_* codes, e.g. ImGui::IsKeyPressed(GLFW_KEY_SPACE).
-    
-        // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
-    
-    
-    {            ImGui::Text('Application average %.3f ms/frame (%.1f FPS)', 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-    
-            // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin('Another Window', &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text('Hello from another window!');
-            if (ImGui::Button('Close Me'))
-                show_another_window = false;
-            ImGui::End();
-        }
-    
-                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox('Another Window', &show_another_window);
+    namespace aria2 {
+    }
