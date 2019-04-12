46d6a0da8d52f5624aeabe5b3c929c4b469fc927
SUNsung <weatherius@gmail.com>
2019-04-12 11:10:34 +0300

    Rewrite [02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp] File in: 2019-04-12 11:10:34

--- cccccc/02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp
+++ cccccc/02vkU2nDm69A02kUEzm8sXi8Q02R19fXTyxxDE02ax8.cpp
@@ -1,313 +1,150 @@
 
         
-        using namespace swift::sys;
-using llvm::StringRef;
-    
-    #ifndef VERB
-#  define VERB(Word)
-#endif
-    
-    #if __has_attribute(may_alias) || LLVM_GNUC_PREREQ(3, 0, 0)
-#define LLVM_MAY_ALIAS __attribute__((may_alias))
-#else
-#define LLVM_MAY_ALIAS
-#endif
-    
-      if (camel_case::getFirstWord(baseName) == 'set') {
-    StringRef newBaseName = ::omitNeedlessWords(
-                              baseName,
-                              contextType,
-                              NameRole::Property,
-                              allPropertyNames,
-                              scratch);
-    if (newBaseName != baseName) {
-      baseName = newBaseName;
-      anyChanges = true;
+        namespace atom {
     }
-  }
     
-      static CFPointeeInfo forTypedef(const clang::TypedefNameDecl *decl) {
-    assert(decl);
-    CFPointeeInfo info;
-    info.IsValid = true;
-    info.IsConst = false;
-    info.Decl = decl;
-    return info;
-  }
+      // content::JavaScriptDialogManager implementations.
+  void RunJavaScriptDialog(content::WebContents* web_contents,
+                           content::RenderFrameHost* rfh,
+                           content::JavaScriptDialogType dialog_type,
+                           const base::string16& message_text,
+                           const base::string16& default_prompt_text,
+                           DialogClosedCallback callback,
+                           bool* did_suppress_message) override;
+  void RunBeforeUnloadDialog(content::WebContents* web_contents,
+                             content::RenderFrameHost* rfh,
+                             bool is_reload,
+                             DialogClosedCallback callback) override;
+  void CancelDialogs(content::WebContents* web_contents,
+                     bool reset_state) override;
     
-    namespace clang {
-class Sema;
-class NamedDecl;
-class TypeDecl;
-class FunctionDecl;
+      // There is a new update which has been downloaded.
+  virtual void OnUpdateDownloaded(const std::string& release_notes,
+                                  const std::string& release_name,
+                                  const base::Time& release_date,
+                                  const std::string& update_url) {}
+    
+    #ifndef ATOM_BROWSER_LIB_POWER_OBSERVER_H_
+#define ATOM_BROWSER_LIB_POWER_OBSERVER_H_
+    
+    
+    {}  // namespace asar
+    
+    void OffScreenOutputDevice::Resize(const gfx::Size& pixel_size,
+                                   float scale_factor) {
+  if (viewport_pixel_size_ == pixel_size)
+    return;
+  viewport_pixel_size_ = pixel_size;
+    }
+    
+    OffscreenViewProxy::~OffscreenViewProxy() {
+  if (observer_) {
+    observer_->ProxyViewDestroyed(this);
+  }
 }
     
-      // Returns the mode: running on CPU or GPU.
-  inline static Brew mode() { return Get().mode_; }
-  // The setters for the variables
-  // Sets the mode. It is recommended that you don't change the mode halfway
-  // into the program since that may cause allocation of pinned memory being
-  // freed in a non-pinned way, which may cause problems - I haven't verified
-  // it personally but better to note it here in the header file.
-  inline static void set_mode(Brew mode) { Get().mode_ = mode; }
-  // Sets the random seed of both boost and curand
-  static void set_random_seed(const unsigned int seed);
-  // Sets the device. Since we have cublas and curand stuff, set device also
-  // requires us to reset those values.
-  static void SetDevice(const int device_id);
-  // Prints the current GPU status.
-  static void DeviceQuery();
-  // Check if specified device is available
-  static bool CheckDevice(const int device_id);
-  // Search from start_id to the highest possible device ordinal,
-  // return the ordinal of the first available device.
-  static int FindDevice(const int start_id = 0);
-  // Parallel training
-  inline static int solver_count() { return Get().solver_count_; }
-  inline static void set_solver_count(int val) { Get().solver_count_ = val; }
-  inline static int solver_rank() { return Get().solver_rank_; }
-  inline static void set_solver_rank(int val) { Get().solver_rank_ = val; }
-  inline static bool multiprocess() { return Get().multiprocess_; }
-  inline static void set_multiprocess(bool val) { Get().multiprocess_ = val; }
-  inline static bool root_solver() { return Get().solver_rank_ == 0; }
+    namespace Ui {
+    class SignVerifyMessageDialog;
+}
     
-     protected:
-   /**
-   * @brief Generates a random integer from Uniform({0, 1, ..., n-1}).
-   *
-   * @param n
-   *    The upperbound (exclusive) value of the random number.
-   * @return
-   *    A uniformly random integer value from ({0, 1, ..., n-1}).
-   */
-  virtual int Rand(int n);
+    static void secp256k1_gej_neg(secp256k1_gej *r, const secp256k1_gej *a) {
+    r->infinity = a->infinity;
+    r->x = a->x;
+    r->y = a->y;
+    r->z = a->z;
+    secp256k1_fe_normalize_weak(&r->y);
+    secp256k1_fe_negate(&r->y, &r->y, 1);
+}
+    
+        secp256k1_context_set_error_callback(tctx, counting_illegal_callback_fn, &ecount);
+    secp256k1_context_set_illegal_callback(tctx, counting_illegal_callback_fn, &ecount);
+    CHECK(secp256k1_ec_pubkey_create(tctx, &point, s_one) == 1);
     
     
-    {  static string LayerTypeListString() {
-    vector<string> layer_types = LayerTypeList();
-    string layer_types_str;
-    for (vector<string>::iterator iter = layer_types.begin();
-         iter != layer_types.end(); ++iter) {
-      if (iter != layer_types.begin()) {
-        layer_types_str += ', ';
-      }
-      layer_types_str += *iter;
+    {    secp256k1_ecdsa_recoverable_signature_load(ctx, &r, &s, &recid, signature);
+    VERIFY_CHECK(recid >= 0 && recid < 4);  /* should have been caught in parse_compact */
+    secp256k1_scalar_set_b32(&m, msg32, NULL);
+    if (secp256k1_ecdsa_sig_recover(&ctx->ecmult_ctx, &r, &s, &q, &m, recid)) {
+        secp256k1_pubkey_save(pubkey, &q);
+        return 1;
+    } else {
+        memset(pubkey, 0, sizeof(*pubkey));
+        return 0;
     }
-    return layer_types_str;
-  }
-};
+}
     
-    /**
- * @brief Abstract base class that factors out the BLAS code common to
- *        ConvolutionLayer and DeconvolutionLayer.
- */
-template <typename Dtype>
-class BaseConvolutionLayer : public Layer<Dtype> {
+    
+    {    /* Test r/s equal to zero */
+    {
+        /* (1,1) encoded in DER. */
+        unsigned char sigcder[8] = {0x30, 0x06, 0x02, 0x01, 0x01, 0x02, 0x01, 0x01};
+        unsigned char sigc64[64] = {
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01,
+        };
+        secp256k1_pubkey pubkeyc;
+        CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sigc64, 0) == 1);
+        CHECK(secp256k1_ecdsa_recover(ctx, &pubkeyc, &rsig, msg32) == 1);
+        CHECK(secp256k1_ecdsa_signature_parse_der(ctx, &sig, sigcder, sizeof(sigcder)) == 1);
+        CHECK(secp256k1_ecdsa_verify(ctx, &sig, msg32, &pubkeyc) == 1);
+        sigcder[4] = 0;
+        sigc64[31] = 0;
+        CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sigc64, 0) == 1);
+        CHECK(secp256k1_ecdsa_recover(ctx, &pubkeyb, &rsig, msg32) == 0);
+        CHECK(secp256k1_ecdsa_signature_parse_der(ctx, &sig, sigcder, sizeof(sigcder)) == 1);
+        CHECK(secp256k1_ecdsa_verify(ctx, &sig, msg32, &pubkeyc) == 0);
+        sigcder[4] = 1;
+        sigcder[7] = 0;
+        sigc64[31] = 1;
+        sigc64[63] = 0;
+        CHECK(secp256k1_ecdsa_recoverable_signature_parse_compact(ctx, &rsig, sigc64, 0) == 1);
+        CHECK(secp256k1_ecdsa_recover(ctx, &pubkeyb, &rsig, msg32) == 0);
+        CHECK(secp256k1_ecdsa_signature_parse_der(ctx, &sig, sigcder, sizeof(sigcder)) == 1);
+        CHECK(secp256k1_ecdsa_verify(ctx, &sig, msg32, &pubkeyc) == 0);
+    }
+}
+    
+    static void CheckParseTorReplyMapping(std::string input, std::map<std::string,std::string> expected)
+{
+    BOOST_TEST_MESSAGE(std::string('CheckParseTorReplyMapping(') + input + ')');
+    auto ret = ParseTorReplyMapping(input);
+    BOOST_CHECK_EQUAL(ret.size(), expected.size());
+    auto r_it = ret.begin();
+    auto e_it = expected.begin();
+    while (r_it != ret.end() && e_it != expected.end()) {
+        BOOST_CHECK_EQUAL(r_it->first, e_it->first);
+        BOOST_CHECK_EQUAL(r_it->second, e_it->second);
+        r_it++;
+        e_it++;
+    }
+}
+    
+    #ifndef BITCOIN_BECH32_H
+#define BITCOIN_BECH32_H
+    
+    #include 'rocksdb/slice.h'
+#include 'rocksdb/status.h'
+    
+    class StopWriteToken : public WriteControllerToken {
  public:
-  explicit BaseConvolutionLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-      virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {}
-    
-    
-    { private:
-  struct pair_sort_first {
-    bool operator()(const std::pair<int, int> &left,
-                    const std::pair<int, int> &right) {
-      return left.first < right.first;
-    }
-  };
-  void check_batch_reindex(int initial_num, int final_num,
-                           const Dtype* ridx_data);
+  explicit StopWriteToken(WriteController* controller)
+      : WriteControllerToken(controller) {}
+  virtual ~StopWriteToken();
 };
     
-     protected:
-  /// @copydoc BNLLLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+      MyFilter filter;
     
-    #include 'caffe/layers/lrn_layer.hpp'
+      // Start a transaction
+  Transaction* txn = txn_db->BeginTransaction(write_options);
+  assert(txn);
     
-    
-    {}  // namespace caffe
-    
-    namespace caffe {
-    }
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    bool AuthPropertyIterator::operator==(const AuthPropertyIterator& rhs) const {
-  if (property_ == nullptr || rhs.property_ == nullptr) {
-    return property_ == rhs.property_;
-  } else {
-    return index_ == rhs.index_;
-  }
-}
-    
-    
-    {}  // namespace grpc
-
-    
-    // Serializes the outgoing trace context. Field IDs are 1 byte followed by
-// field data. A 1 byte version ID is always encoded first.
-size_t TraceContextSerialize(const ::opencensus::trace::SpanContext& context,
-                             char* tracing_buf, size_t tracing_buf_size);
-    
-    namespace grpc {
-    }
-    
-    namespace grpc {
-    }
-    
-    #ifndef GRPC_INTERNAL_CPP_EXT_PROTO_SERVER_REFLECTION_H
-#define GRPC_INTERNAL_CPP_EXT_PROTO_SERVER_REFLECTION_H
-    
-    #include <grpcpp/impl/channel_argument_option.h>
-    
-    /**
- * \brief The class sets caffe's mode before doing forward/backward
- * \tparam xpu The device that the op will be executed on.
- */
-class CaffeMode {
- public:
-  template<typename xpu> static void SetMode();
-};
-    
-     protected:
-  /*! \brief prefetcher parameters */
-  PrefetcherParam param_;
-  /*! \brief backend thread */
-  dmlc::ThreadedIter<DataBatch> iter;
-  /*! \brief internal batch loader */
-  std::unique_ptr<IIterator<TBlobBatch> > loader_;
-    
-    namespace mxnet {
-namespace kvstore {
-    }
-    }
-    
-    /*!
- * \brief Computes row-wise Kronecker product
- *
- * Given input matrices, this function computes the Kronecker product
- * row-wise. E.g. if the input matrices  are of shape (3, 2), (3, 4),
- * (3, 5), the result matrix will be of shape (3, 2 * 4 * 5), which is
- * (3, 40).
- *
- * \param out result matrix
- * \param ts_arr vector of input matrices
- */
-template <typename DType>
-inline void row_wise_kronecker
-  (Tensor<cpu, 2, DType> out,
-  const std::vector<Tensor<cpu, 2, DType> > &ts_arr) {
-  CHECK_GE(ts_arr.size(), 1) << 'The input matrices must be non-empty.';
-    }
-    
-     private:
-  inline void Init(mshadow::Stream<gpu> *s,
-                   const std::vector<TBlob> &in_data,
-                   const std::vector<TBlob> &out_data) {
-    using namespace mshadow;
-    #if CUDNN_MAJOR >= 5
-    format_ = CUDNN_TENSOR_NCHW;
-    #endif
-    CHECK_EQ(in_data.size(), 2U);
-    CHECK_EQ(out_data.size(), 3U);
-    if (!init_cudnn_) {
-      init_cudnn_ = true;
-      Tensor<gpu, 4, DType> data = in_data[st::kData].get<gpu, 4, DType>(s);
-      Tensor<gpu, 4, DType> out = out_data[st::kOut].get<gpu, 4, DType>(s);
-      CUDNN_CALL(cudnnCreateSpatialTransformerDescriptor(&st_desc_));
-      CUDNN_CALL(cudnnCreateTensorDescriptor(&in_desc_));
-      CUDNN_CALL(cudnnCreateTensorDescriptor(&out_desc_));
-      CUDNN_CALL(cudnnSetTensor4dDescriptor(in_desc_,
-                                            format_,
-                                            dtype_,
-                                            data.size(0),
-                                            data.size(1),
-                                            data.size(2),
-                                            data.size(3)));
-      CUDNN_CALL(cudnnSetTensor4dDescriptor(out_desc_,
-                                            format_,
-                                            dtype_,
-                                            out.size(0),
-                                            out.size(1),
-                                            out.size(2),
-                                            out.size(3)));
-      if (param_.sampler_type == st::kBilinear) {
-        int dim[] = {static_cast<int>(out.size(0)), static_cast<int>(out.size(1)),
-                     static_cast<int>(out.size(2)), static_cast<int>(out.size(3))};
-        CUDNN_CALL(cudnnSetSpatialTransformerNdDescriptor(st_desc_,
-                                                          sampler_,
-                                                          dtype_,
-                                                          4,
-                                                          dim));
-      }
-    }
-  }
-    
-    /*!
- * Copyright (c) 2015 by Contributors
- * \file native_op.cc
- * \brief
- * \author Junyuan Xie
-*/
-#include './native_op-inl.h'
-    
-    void Assembler::unimplemented(){
-  //Emit a instruction with invalid opcode 0x0
-  EmitDForm(0, rn(0), rn(0), 0);
-}
-    
-         // GP Register cannot be greater than 31
-     assert(static_cast<uint32_t>(ra) < 32);
-     assert(static_cast<uint32_t>(rt) < 32);
-     assert(static_cast<uint16_t>(imm << 14) == 0);
-    
-      const char* data = filename.data();
-  int data_len = filename.length();
-  bool base64 = false;
-  if (strncmp(data, 'data:', sizeof('data:') - 1)) {
-    return nullptr;
-  }
-  data += sizeof('data:') - 1;
-  data_len -= sizeof('data:') - 1;
-    
-    #endif // incl_HPHP_OUTPUT_FILE_H_
-
-    
-    #include 'hphp/runtime/base/perf-warning.h'
-    
-    constexpr int64_t kDefaultPerfWarningRate = 100;
-    
-    
-    {  xfer += oprot->writeFieldStop();
-  xfer += oprot->writeStructEnd();
-  return xfer;
-}
-    
-    #pragma once
-    
-      auto enter_event = events::syscall::Event{};
-  enter_event.type = events::syscall::EventType::SetuidEnter;
-  enter_event.pid = 45;
-  enter_event.tgid = 146;
-  enter_event.body.setuid_enter.arg_uid = 48;
-  enter_event.body.setuid_enter.uid = 49;
-  enter_event.body.setuid_enter.gid = 50;
-  enter_event.return_value = -1;
+      static Status Open(const DBOptions& db_options, const std::string& dbname,
+                     const std::vector<ColumnFamilyDescriptor>& column_families,
+                     std::vector<ColumnFamilyHandle*>* handles,
+                     OptimisticTransactionDB** dbptr);
