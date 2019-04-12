f107db2a28e1856b5f762b9b71a6fa799dee2688
SUNsung <weatherius@gmail.com>
2019-04-13 00:47:31 +0300

    Rewrite [55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp] File in: 2019-04-13 00:47:31

--- cccccc/55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp
+++ cccccc/55SS3Usst6zgk55Koyc33lmTtE55CsU1h6mo4w55l4..cpp
@@ -1,84 +1,275 @@
 
         
-                    if (accumulatedMetric && m_samples.second > 0)
-            {
-                m_metric.second = accumulatedMetric->AsScalar<double>();
-            }
-    
-            static const size_t s_serializationVersion = 0;
-    
-        // Releases the mutex
-    void Release()
-    {
-        assert(m_handle != NULL);
-        int rc = 0;
-        rc = ::ReleaseMutex(m_handle);
-        if ((rc == RELEASEMUTEX_ERROR) && !std::uncaught_exception())
-        {
-            RuntimeError('Mutex Release: Failed to release mutex %s: %d', m_name.c_str(), ::GetLastError());
-        }
-        rc = ::CloseHandle(m_handle);
-        if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
-        {
-            RuntimeError('Mutex Release: Failed to close handler %s: %d', m_name.c_str(), ::GetLastError());
-        }
-        m_handle = NULL;
-    }
-    
-    
-    {
-    {
-    {}}}
+        
+    {}  // namespace atom
 
     
-    #include <string>
+    #if defined(OS_LINUX)
+typedef PowerObserverLinux PowerObserver;
+#else
+typedef base::PowerObserver PowerObserver;
+#endif  // defined(OS_LINUX)
+    
+      base::WeakPtrFactory<URLRequestAboutJob> weak_ptr_factory_;
+    
+    namespace content {
+struct MainFunctionParams;
+}
     
     
-    {        sliceInputGrad += sliceOutputGrad; // here the assumption is that sliceOutputGrad is a row vector
+    {  if (event.filter != EVFILT_PROC || event.fflags != NOTE_EXIT ||
+      event.ident != static_cast<uintptr_t>(parent_pid)) {
+    LOG(ERROR) << 'kevent (monitor): unexpected event, filter ' << event.filter
+               << ', fflags ' << event.fflags << ', ident ' << event.ident;
+    return;
+  }
+}
+    
+    #include 'ui/views/views_delegate.h'
+    
+    private:
+    reverse_lock(reverse_lock const&);
+    reverse_lock& operator=(reverse_lock const&);
+    
+    static void secp256k1_hmac_sha256_initialize(secp256k1_hmac_sha256_t *hash, const unsigned char *key, size_t keylen) {
+    int n;
+    unsigned char rkey[64];
+    if (keylen <= 64) {
+        memcpy(rkey, key, keylen);
+        memset(rkey + keylen, 0, 64 - keylen);
+    } else {
+        secp256k1_sha256_t sha256;
+        secp256k1_sha256_initialize(&sha256);
+        secp256k1_sha256_write(&sha256, key, keylen);
+        secp256k1_sha256_finalize(&sha256, rkey);
+        memset(rkey + 32, 0, 32);
+    }
     }
     
-            // create a vector with the correct number of timesteps(shapeXT[2]) containing the sequence count (shapeXT[1])
-        numSequencesForFrame = vector<size_t>(shapeXT[2], shapeXT[1]);
-        m_transposedOutput->RNNForward(*m_transposedInput, paramW, shapeXT[0], shapeYT[0], numSequencesForFrame, m_rnnAttributes, *m_reserve, *m_workspace);
+        struct Margin {
+        Margin() : left(0), right(0), top(0), bottom(0) {}
+        Margin(size_t left_, size_t right_, size_t top_, size_t bottom_)
+            : left(left_), right(right_), top(top_), bottom(bottom_) {}
+    }
     
-    #include <folly/io/Cursor.h>
-#include <folly/io/IOBuf.h>
-#include <folly/lang/Bits.h>
+    template <bool L2gradient, bool externalSobel>
+inline void Canny3x3(const Size2D &size, s32 cn,
+                     const u8 * srcBase, ptrdiff_t srcStride,
+                     u8 * dstBase, ptrdiff_t dstStride,
+                     s16 * dxBase, ptrdiff_t dxStride,
+                     s16 * dyBase, ptrdiff_t dyStride,
+                     f64 low_thresh, f64 high_thresh,
+                     Margin borderMargin)
+{
+    s32 low, high;
+    prepareThresh<L2gradient>(low_thresh, high_thresh, low, high);
+    }
     
-     private:
-  // Matches packed_sync_pointer.  Must be > max number of local
-  // counts.  This is the max number of threads that can access this
-  // atomic_shared_ptr at once before we start blocking.
-  static constexpr unsigned EXTERNAL_OFFSET{0x2000};
-  // Bit signifying aliased constructor
-  static constexpr unsigned ALIASED_PTR{0x4000};
+            for (; sj < roiw8; sj += 8, syj += 16, dj += 32)
+        {
+            uint8x8x2_t v_y = vld2_u8(srcy + syj);
+            uint8x8x4_t v_dst;
+            v_dst.val[0] = v_y.val[0];
+            v_dst.val[1] = vld1_u8(srcu + sj);
+            v_dst.val[2] = v_y.val[1];
+            v_dst.val[3] = vld1_u8(srcv + sj);
+            vst4_u8(dst + dj, v_dst);
+        }
     
-    // This temporarily forwards tuple and some tuple functions from std::tr1 to
-// std:: to make it possible to migrate from std::tr1.
-//
-// TODO(crbug.com/829773): Remove this file when the transition is complete.
+             float32x4_t vline1_f32 = vcvtq_f32_u32(vline1_u32);
+         float32x4_t vline2_f32 = vcvtq_f32_u32(vline2_u32);
+         float32x4_t vline3_f32 = vcvtq_f32_u32(vline3_u32);
+         float32x4_t vline4_f32 = vcvtq_f32_u32(vline4_u32);
     
-    
+    s32 countNonZero(const Size2D &_size,
+                 const u8 * srcBase, ptrdiff_t srcStride)
+{
+    internal::assertSupportedConfiguration();
+#ifdef CAROTENE_NEON
+    Size2D size(_size);
+    if (srcStride == (ptrdiff_t)(size.width))
     {
-    {}  // namespace internal
-}  // namespace v8
-
-    
-      Node* const fast_iter_result_map =
-      LoadContextElement(native_context, Context::ITERATOR_RESULT_MAP_INDEX);
-    
-    // ES6 #sec-boolean.prototype.tostring
-TF_BUILTIN(BooleanPrototypeToString, CodeStubAssembler) {
-  Node* context = Parameter(Descriptor::kContext);
-  Node* receiver = Parameter(Descriptor::kReceiver);
+        size.width *= size.height;
+        size.height = 1;
+    }
+    size_t roiw16 = size.width & ~15u;
+    s32 result = 0;
+    for(size_t k = 0; k < size.height; ++k)
+    {
+        const u8* src = internal::getRowPtr( srcBase,  srcStride, k);
+        size_t i = 0;
+    }
     }
     
-      explicit LazyBuiltinsAssembler(compiler::CodeAssemblerState* state)
-      : CodeStubAssembler(state) {}
+      image_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2051) << 'Incorrect image file magic.';
+  label_file.read(reinterpret_cast<char*>(&magic), 4);
+  magic = swap_endian(magic);
+  CHECK_EQ(magic, 2049) << 'Incorrect label file magic.';
+  image_file.read(reinterpret_cast<char*>(&num_items), 4);
+  num_items = swap_endian(num_items);
+  label_file.read(reinterpret_cast<char*>(&num_labels), 4);
+  num_labels = swap_endian(num_labels);
+  CHECK_EQ(num_items, num_labels);
+  image_file.read(reinterpret_cast<char*>(&rows), 4);
+  rows = swap_endian(rows);
+  image_file.read(reinterpret_cast<char*>(&cols), 4);
+  cols = swap_endian(cols);
     
-    // ES6 #sec-math.cos
-TF_BUILTIN(MathCos, MathBuiltinsAssembler) {
-  Node* context = Parameter(Descriptor::kContext);
-  Node* x = Parameter(Descriptor::kX);
-  MathUnaryOperation(context, x, &CodeStubAssembler::Float64Cos);
+    /**
+ * @brief Get a specific filler from the specification given in FillerParameter.
+ *
+ * Ideally this would be replaced by a factory pattern, but we will leave it
+ * this way for now.
+ */
+template <typename Dtype>
+Filler<Dtype>* GetFiller(const FillerParameter& param) {
+  const std::string& type = param.type();
+  if (type == 'constant') {
+    return new ConstantFiller<Dtype>(param);
+  } else if (type == 'gaussian') {
+    return new GaussianFiller<Dtype>(param);
+  } else if (type == 'positive_unitball') {
+    return new PositiveUnitballFiller<Dtype>(param);
+  } else if (type == 'uniform') {
+    return new UniformFiller<Dtype>(param);
+  } else if (type == 'xavier') {
+    return new XavierFiller<Dtype>(param);
+  } else if (type == 'msra') {
+    return new MSRAFiller<Dtype>(param);
+  } else if (type == 'bilinear') {
+    return new BilinearFiller<Dtype>(param);
+  } else {
+    CHECK(false) << 'Unknown filler name: ' << param.type();
+  }
+  return (Filler<Dtype>*)(NULL);
+}
+    
+      /* Should be tested when running loops to exit when requested. */
+  bool must_stop();
+    
+      // Adds a creator.
+  static void AddCreator(const string& type, Creator creator) {
+    CreatorRegistry& registry = Registry();
+    CHECK_EQ(registry.count(type), 0)
+        << 'Layer type ' << type << ' already registered.';
+    registry[type] = creator;
+  }
+    
+    #include <vector>
+    
+     protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    
+      virtual inline const char* type() const { return 'BatchReindex'; }
+  virtual inline int ExactNumBottomBlobs() const { return 2; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+    
+    /**
+ * @brief Computes a sum of two input Blobs, with the shape of the latter Blob
+ *        'broadcast' to match the shape of the former. Equivalent to tiling
+ *        the latter Blob, then computing the elementwise sum.
+ *
+ * The second input may be omitted, in which case it's learned as a parameter
+ * of the layer. Note: in case bias and scaling are desired, both operations can
+ * be handled by `ScaleLayer` configured with `bias_term: true`.
+ */
+template <typename Dtype>
+class BiasLayer : public Layer<Dtype> {
+ public:
+  explicit BiasLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    /**
+ * @brief Takes at least two Blob%s and concatenates them along either the num
+ *        or channel dimension, outputting the result.
+ */
+template <typename Dtype>
+class ConcatLayer : public Layer<Dtype> {
+ public:
+  explicit ConcatLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    }
+    
+    #include 'caffe/layers/base_conv_layer.hpp'
+    
+    namespace caffe {
+    }
+    
+    TEST(AutoCompactTest, ReadHalf) {
+  DoReads(kCount/2);
+}
+    
+    
+    {    if (s.ok()) {
+      // Verify that the table is usable
+      Iterator* it = table_cache->NewIterator(ReadOptions(),
+                                              meta->number,
+                                              meta->file_size);
+      s = it->status();
+      delete it;
+    }
+  }
+    
+    #include <stdint.h>
+#include 'leveldb/db.h'
+#include 'db/dbformat.h'
+    
+    std::string LockFileName(const std::string& dbname) {
+  return dbname + '/LOCK';
+}
+    
+    #include <stdint.h>
+#include <string>
+#include 'leveldb/slice.h'
+#include 'leveldb/status.h'
+#include 'port/port.h'
+    
+    // Header is checksum (4 bytes), length (2 bytes), type (1 byte).
+static const int kHeaderSize = 4 + 2 + 1;
+    
+    
+    {      counter++;
+      if (empty) {
+        empty = false;
+        t.meta.smallest.DecodeFrom(key);
+      }
+      t.meta.largest.DecodeFrom(key);
+      if (parsed.sequence > t.max_sequence) {
+        t.max_sequence = parsed.sequence;
+      }
+    }
+    if (!iter->status().ok()) {
+      status = iter->status();
+    }
+    delete iter;
+    Log(options_.info_log, 'Table #%llu: %d entries %s',
+        (unsigned long long) t.meta.number,
+        counter,
+        status.ToString().c_str());
+    
+    inline
+static void DBSynchronize(kyotocabinet::TreeDB* db_)
+{
+  // Synchronize will flush writes to disk
+  if (!db_->synchronize()) {
+    fprintf(stderr, 'synchronize error: %s\n', db_->error().name());
+  }
 }
