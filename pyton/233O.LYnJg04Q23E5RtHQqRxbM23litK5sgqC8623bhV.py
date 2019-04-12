4cc7329dad2e2180aaa03138df3c695fd03e234a
SUNsung <weatherius@gmail.com>
2019-04-12 12:40:20 +0300

    Rewrite [233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py] File in: 2019-04-12 12:40:18

--- pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
+++ pyton/233O.LYnJg04Q23E5RtHQqRxbM23litK5sgqC8623bhV.py
@@ -1,206 +1,57 @@
 
         
-            Returns:
-      A dictionary with the averaged outputs of the model decoder, namely:
-        prior g0 mean, prior g0 variance, approx. posterior mean, approx
-        posterior mean, the generator initial conditions, the control inputs (if
-        enabled), the state of the generator, the factors, and the output
-        distribution parameters, e.g. (rates or mean and variances).
+            def test_error_content_length(self):
+        # Issue #16088: standard error responses should have a content-length
+        self.con.request('NOTFOUND', '/')
+        res = self.con.getresponse()
+        self.assertEqual(res.status, HTTPStatus.NOT_FOUND)
+    
+            expected = [loader.suiteClass([MyTestCase('test')])]
+        self.assertEqual(list(suite), expected)
+    
+    class BaseRotatingHandler(logging.FileHandler):
     '''
-    hps = self.hps
-    batch_size = hps.batch_size
-    E, T, D  = data_extxd.shape
-    E_to_process = hps.ps_nexamples_to_process
-    if E_to_process > E:
-      E_to_process = E
-    
-      # Split the data, inputs, labels and times into train vs. validation.
-  rates_train, rates_valid = \
-      split_list_by_inds(rates, train_inds, valid_inds)
-  noisy_data_train, noisy_data_valid = \
-      split_list_by_inds(noisy_data, train_inds, valid_inds)
-  input_train, inputs_valid = \
-      split_list_by_inds(inputs, train_inds, valid_inds)
-  condition_labels_train, condition_labels_valid = \
-      split_list_by_inds(condition_labels, train_inds, valid_inds)
-  input_times_train, input_times_valid = \
-      split_list_by_inds(input_times, train_inds, valid_inds)
-    
-    model = IntegrationToBoundModel(N)
-inputs_ph_t = [tf.placeholder(tf.float32,
-                              shape=[None, 1]) for _ in range(ntimesteps)]
-state = tf.zeros([batch_size, N])
-saver = tf.train.Saver()
-    
-      wasserstein_loss = real_avg - fake_avg
-  return wasserstein_loss
-
-    
-        elif FLAGS.discriminator_model == 'seq2seq_vd':
-      load_ckpt = tf.train.latest_checkpoint(FLAGS.language_model_ckpt_dir)
-      print('Restoring Discriminator from %s.' % load_ckpt)
-      tf.logging.info('Restoring Discriminator from %s.' % load_ckpt)
-      dis_encoder_init_saver = init_savers['dis_encoder_init_saver']
-      dis_decoder_init_saver = init_savers['dis_decoder_init_saver']
-      dis_encoder_init_saver.restore(sess, load_ckpt)
-      dis_decoder_init_saver.restore(sess, load_ckpt)
-    
-    This typically means that you attempted to use functionality that needed
-an active HTTP request.  Consult the documentation on testing for
-information about how to avoid this problem.\
-'''
-_app_ctx_err_msg = '''\
-Working outside of application context.
-    
-    
-def tojson_filter(obj, **kwargs):
-    return Markup(htmlsafe_dumps(obj, **kwargs))
-
-    
-        def to_python(self, value):
-        key = next(iter(value))
-        return {key[:-2]: value[key]}
-    
-    
-def render_template(template_name_or_list, **context):
-    '''Renders a template from the template folder with the given
-    context.
-    
-        def _load_form_data(self):
-        RequestBase._load_form_data(self)
-    
-    
-@pytest.fixture
-def app_ctx(app):
-    with app.app_context() as ctx:
-        yield ctx
-    
-        # Raises
-        ValueError: in case of invalid `label_mode`.
+    Base class for handlers that rotate log files at a certain point.
+    Not meant to be instantiated directly.  Instead, use RotatingFileHandler
+    or TimedRotatingFileHandler.
     '''
-    if label_mode not in ['fine', 'coarse']:
-        raise ValueError('`label_mode` must be one of `'fine'`, `'coarse'`.')
+    def __init__(self, filename, mode, encoding=None, delay=False):
+        '''
+        Use the specified filename for streamed logging
+        '''
+        logging.FileHandler.__init__(self, filename, mode, encoding, delay)
+        self.mode = mode
+        self.encoding = encoding
+        self.namer = None
+        self.rotator = None
     
-        def get_config(self):
-        return {'l1': float(self.l1),
-                'l2': float(self.l2)}
     
+# Does a path exist?
+# This is false for dangling symbolic links on systems that support them.
+def exists(path):
+    '''Test whether a path exists.  Returns False for broken symbolic links'''
+    try:
+        os.stat(path)
+    except (OSError, ValueError):
+        return False
+    return True
     
-def test_fashion_mnist():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-        assert len(x_train) == len(y_train) == 60000
-        assert len(x_test) == len(y_test) == 10000
+    --Pepé
+''')
     
-        # Arguments
-        y: class vector to be converted into a matrix
-            (integers from 0 to num_classes).
-        num_classes: total number of classes.
-        dtype: The data type expected by the input, as a string
-            (`float32`, `float64`, `int32`...)
+            print('Empty input')
+        assert_equal(func([]), [])
     
-        # Place a copy of the model on each GPU,
-    # each getting a slice of the inputs.
-    for i, gpu_id in enumerate(target_gpu_ids):
-        with tf.device('/gpu:%d' % gpu_id):
-            with tf.name_scope('replica_%d' % gpu_id):
-                inputs = []
-                # Retrieve a slice of the input.
-                for x in model.inputs:
-                    # In-place input splitting which is not only
-                    # 5% ~ 12% faster but also less GPU memory
-                    # duplication.
-                    with tf.device(x.device):
-                        input_shape = K.int_shape(x)[1:]
-                        slice_i = Lambda(get_slice,
-                                         output_shape=input_shape,
-                                         arguments={'i': i,
-                                                    'parts': num_gpus})(x)
-                        inputs.append(slice_i)
+        # TODO: It would be better if we had unit tests for each
+    # method in addition to the following end-to-end test
+    def test_end_to_end(self):
+        print('Test: Empty stack')
+        stack = Stack()
+        assert_equal(stack.peek(), None)
+        assert_equal(stack.pop(), None)
     
-        @property
-    def dilation_rate(self):
-        return self.cell.dilation_rate
+        def __repr__(self):
+        return str(self.obj) + ': ' + str(self.key)
     
-    # The data, split between train and test sets:
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-    
-        # Sampling loop for a batch of sequences
-    # (to simplify, here we assume a batch of size 1).
-    stop_condition = False
-    decoded_sentence = ''
-    while not stop_condition:
-        output_tokens, h, c = decoder_model.predict(
-            [target_seq] + states_value)
-    
-    import os, json, imp
-here = os.path.abspath(os.path.dirname(__file__))
-proj_info = json.loads(open(os.path.join(here, PROJ_METADATA), encoding='utf-8').read())
-try:
-    README = open(os.path.join(here, 'README.rst'), encoding='utf-8').read()
-except:
-    README = ''
-CHANGELOG = open(os.path.join(here, 'CHANGELOG.rst'), encoding='utf-8').read()
-VERSION = imp.load_source('version', os.path.join(here, 'src/%s/version.py' % PACKAGE_NAME)).__version__
-    
-    from ..common import *
-from ..extractor import VideoExtractor
-    
-        if re.match(re_str, url):
-        html = get_content(url)
-        title = match1(html, r'<title>(.+)丨(.+)</title>')  #title
-        
-        # player loaded via internal iframe
-        frame_url = re.search(r'<iframe src=\'(.+?)\'', html).group(1)
-        logging.debug('dilidili_download: %s' % frame_url)
-        
-        #https://player.005.tv:60000/?vid=a8760f03fd:a04808d307&v=yun&sign=a68f8110cacd892bc5b094c8e5348432
-        html = get_content(frame_url, headers=headers, decoded=False).decode('utf-8')
-        
-        match = re.search(r'(.+?)var video =(.+?);', html)
-        vid = match1(html, r'var vid='(.+)'')
-        hd2 = match1(html, r'var hd2='(.+)'')
-        typ = match1(html, r'var typ='(.+)'')
-        sign = match1(html, r'var sign='(.+)'')
-        tmsign = match1(html, r'tmsign=([A-Za-z0-9]+)')
-        ulk =  match1(html, r'var ulk='(.+)'')
-    
-    __all__ = ['douban_download']
-    
-    	html = get_html(url)
-	contentid = r1(r'<meta name='contentid' scheme='DMINSTR2' content='([^']+)' />', html)
-	vid = r1(r''demand_ehow_videoid':'([^']+)'', html)
-	assert vid
-    
-        sd_urls = list(set([
-        unicodize(str.replace(i, '\\/', '/'))
-        for i in re.findall(r'sd_src_no_ratelimit:'([^']*)'', html)
-    ]))
-    hd_urls = list(set([
-        unicodize(str.replace(i, '\\/', '/'))
-        for i in re.findall(r'hd_src_no_ratelimit:'([^']*)'', html)
-    ]))
-    urls = hd_urls if hd_urls else sd_urls
-    
-    site_info = 'Giphy.com'
-download = giphy_download
-download_playlist = playlist_not_supported('giphy')
-
-    
-      Returns:
-    string with replacements made (or original string if no replacements)
-  '''
-  if pattern not in _regexp_compile_cache:
-    _regexp_compile_cache[pattern] = sre_compile.compile(pattern)
-  return _regexp_compile_cache[pattern].sub(rep, s)
-    
-        if len(sys.argv) == 2:
-        main(sys.argv[1])
-    else:
-        print('Choose one path as argument one')
+    for i in range(0, 21050, 1000):
+    open('ci.song.%s.json' % i, 'w').write(json.dumps(cis[i:i+1000], indent=2, ensure_ascii=False))
