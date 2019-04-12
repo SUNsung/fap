a02055744f3e0b8b8dfb27b9279ff770949a1121
SUNsung <weatherius@gmail.com>
2019-04-12 13:59:06 +0300

    Rewrite [44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp] File in: 2019-04-12 13:59:06

--- cccccc/44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp
+++ cccccc/44XO8jvXCh3Rw44XS0ptbIsNp244aLyaQc23Bek44WvB.cpp
@@ -1,202 +1,135 @@
 
         
-        namespace atom {
+          if (_stricmp(info->szDll, 'iojs.exe') != 0 &&
+      _stricmp(info->szDll, 'node.exe') != 0)
+    return NULL;
+    
+    #include <string>
+    
+    
+#include 'content/nw/src/api/event/event.h'
+#include 'base/values.h'
+#include 'content/nw/src/api/dispatcher_host.h'
+#include 'ui/gfx/screen.h'
+    
+    class BaseEvent {
+  friend class EventListener;
+  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
     }
     
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-    namespace atom {
+    bool NwClipboardReadAvailableTypesFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  bool contains_filenames;
+  std::vector<base::string16> types;
+  clipboard->ReadAvailableTypes(ui::CLIPBOARD_TYPE_COPY_PASTE, &types, &contains_filenames);
+  for(std::vector<base::string16>::iterator it = types.begin(); it != types.end(); it++) {
+    if (base::EqualsASCII(*it, ui::kMimeTypeText)) {
+      response->Append(base::WrapUnique(new base::Value(ToString(TYPE_TEXT))));
+    } else if (base::EqualsASCII(*it, ui::kMimeTypeHTML)) {
+      response->Append(base::WrapUnique(new base::Value(ToString(TYPE_HTML))));
+    } else if (base::EqualsASCII(*it, ui::kMimeTypeRTF)) {
+      response->Append(base::WrapUnique(new base::Value(ToString(TYPE_RTF))));
+    } else if (base::EqualsASCII(*it, ui::kMimeTypePNG)) {
+      response->Append(base::WrapUnique(new base::Value(ToString(TYPE_PNG))));
+      response->Append(base::WrapUnique(new base::Value(ToString(TYPE_JPEG))));
     }
-    
-    #include 'atom/browser/api/event_emitter.h'
-#include 'native_mate/handle.h'
-#include 'ui/display/display_observer.h'
-#include 'ui/display/screen.h'
-    
-    Delegate* AutoUpdater::GetDelegate() {
-  return delegate_;
+  }
+  return true;
 }
     
-    #ifndef ATOM_BROWSER_LIB_POWER_OBSERVER_H_
-#define ATOM_BROWSER_LIB_POWER_OBSERVER_H_
+    REGISTER_CPU_OPERATOR(EnforceFinite, EnforceFiniteOp<CPUContext>);
     
+    class GetMergeSingleListFeatureTensorsGradient : public GradientMakerBase {
+  using GradientMakerBase::GradientMakerBase;
+  vector<OperatorDef> GetGradientDefs() override {
+    vector<string> input_blob_names{};
+    vector<string> output_blob_names{};
+    }
+    }
     
-    {  DISALLOW_COPY_AND_ASSIGN(URLRequestAboutJob);
+    </details>
+    
+    // Like ASSERT_DEATH, but continues on to successive tests in the
+// test case, if any:
+# define EXPECT_DEATH(statement, regex) \
+    EXPECT_EXIT(statement, ::testing::internal::ExitedUnsuccessfully, regex)
+    
+    #if 0
+    
+      // Smart pointer members.
+  void reset(T* ptr = NULL) {
+    depart();
+    capture(ptr);
+  }
+  T* get() const { return value_; }
+  T* operator->() const { return value_; }
+  T& operator*() const { return *value_; }
+    
+    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
+    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
+    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
+    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
+    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
+    GTEST_TEMPLATE_ T16, GTEST_TEMPLATE_ T17, GTEST_TEMPLATE_ T18,
+    GTEST_TEMPLATE_ T19, GTEST_TEMPLATE_ T20, GTEST_TEMPLATE_ T21,
+    GTEST_TEMPLATE_ T22, GTEST_TEMPLATE_ T23, GTEST_TEMPLATE_ T24,
+    GTEST_TEMPLATE_ T25, GTEST_TEMPLATE_ T26, GTEST_TEMPLATE_ T27,
+    GTEST_TEMPLATE_ T28, GTEST_TEMPLATE_ T29, GTEST_TEMPLATE_ T30,
+    GTEST_TEMPLATE_ T31, GTEST_TEMPLATE_ T32, GTEST_TEMPLATE_ T33,
+    GTEST_TEMPLATE_ T34, GTEST_TEMPLATE_ T35>
+struct Templates35 {
+  typedef TemplateSel<T1> Head;
+  typedef Templates34<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+      T15, T16, T17, T18, T19, T20, T21, T22, T23, T24, T25, T26, T27, T28,
+      T29, T30, T31, T32, T33, T34, T35> Tail;
 };
     
-    SHOULD_NOT_DO_GRADIENT(EnforceFinite);
+      virtual bool IsPrime(int n) const {
+    return 0 <= n && n < is_prime_size_ && is_prime_[n];
+  }
     
-    workspace.ResetWorkspace()
+    // Returns n! (the factorial of n).  For negative n, n! is defined to be 1.
+int Factorial(int n) {
+  int result = 1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;
+  }
+    }
     
-    )DOC')
-    .Input(0, 'X', '*(type: Tensor`<float>`)* Input tensor.')
-    .Output(0, 'Y', '*(type: Tensor`<float>`)* Output tensor.');
+    #include 'sample2.h'
     
-    template <typename T, class Context>
-class BernoulliJSDOp final : public Operator<Context> {
- public:
-  USE_SIMPLE_CTOR_DTOR(BernoulliJSDOp);
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-  bool RunOnDevice() override;
+    
+    {  /*!
+   * \brief Create a tree updater given name
+   * \param name Name of the tree updater.
+   */
+  static TreeUpdater* Create(const std::string& name);
 };
     
-    #include 'stdafx.h'
-#include 'CNTKLibrary.h'
-#include 'Utils.h'
-#include 'ReaderShim.h'
-#include 'DataReader.h'
     
-    
-    {
-    {    private:
-        static bool IsNativeUDF(const Dictionary& dict);
-    };
-}
-
-    
-        std::wstring VariableFields::AsString() const
-    {
-        std::wstringstream wss;
-        wss << VariableKindName(m_varKind) << '('';
-        if (m_name != L'')
-            wss << m_name;
-        else
-            wss << m_uid;
-        bool reverse = Internal::IsReversingTensorShapesInErrorMessagesEnabled();
-        if (reverse)
-            wss << '', ' << DynamicAxesAsString(m_dynamicAxes, reverse) << ', ' << m_shape.AsString() << ')';
-        else
-            wss << '', ' << m_shape.AsString() << ', ' << DynamicAxesAsString(m_dynamicAxes, reverse) << ')';
-        return wss.str();
-    }
-    
-    
-    {            if ((m_varKind == VariableKind::Parameter) || (m_varKind == VariableKind::Constant))
-            {
-                if (m_shape.HasFreeDimension())
-                    InvalidArgument('Parameter/Constant '%S' has invalid shape '%S'; it is illegal for a Parameter/Constant to have a FreeDimension.', AsString().c_str(), m_shape.AsString().c_str());
-            }
-        }
-    
-    template <typename TVector>
-inline void RandomShuffleMT(TVector& v, std::mt19937_64& rng)
-{
-    foreach_index(currentLocation, v)
-    {
-        // Pick a random location and swap with current one
-        const size_t randomLocation = RandMT(0, v.size(), rng);
-        std::swap(v[currentLocation], v[randomLocation]);
-    }
-}
-    
-    
-    {    ~ScopeTimer()
-    {
-        if (m_verbosity > 2)
-        {
-            m_aggregateTimer.Stop();
-            double time = m_aggregateTimer.ElapsedSeconds();
-            fprintf(stderr, m_message.c_str(), time);
-        }
-    }
+    { private:
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
 };
     
-            // traverse children: append them to the end of the work list
-        // In case of model editing, map inputs.
-        for (size_t i = 0; i < node->GetNumInputs(); i++)
-        {
-            auto input = node->Input(i);
-    }
     
-    
-    {    virtual void Load(File& fstream, size_t modelVersion) override
-    {
-        Base::Load(fstream, modelVersion);
-        fstream >> m_isOutputSparse;
+    {    inline size_t Size() const {
+      return end - begin;
     }
-private:
-    bool m_isOutputSparse;
-};
-    
-    /**
-@brief Waves action.
-@details This action is used for take effect on the target node as waves.
-        You can control the effect by these parameters:
-        duration, grid size, waves count, amplitude,
-        whether waves on horizontal and whether waves on vertical.
-*/
-class CC_DLL Waves : public Grid3DAction
-{
-public:
-    /**
-    @brief Create the action with amplitude, horizontal sin, vertical sin, grid size, waves count and duration.
-    @param duration Specify the duration of the Waves action. It's a value in seconds.
-    @param gridSize Specify the size of the grid.
-    @param waves Specify the waves count of the Waves action.
-    @param amplitude Specify the amplitude of the Waves action.
-    @param horizontal Specify whether waves on horizontal.
-    @param vertical Specify whether waves on vertical.
-    @return If the creation success, return a pointer of Waves action; otherwise, return nil.
-    */
-    static Waves* create(float duration, const Size& gridSize, unsigned int waves, float amplitude, bool horizontal, bool vertical);
-    }
-    
-    /** @class FlipY
-* @brief Flips the sprite vertically.
-* @since v0.99.0
-*/
-class CC_DLL FlipY : public ActionInstant
-{
-public:
-    /** Create the action.
-     *
-     * @param y Flips the sprite vertically if true.
-     * @return An autoreleased FlipY object.
-     */
-    static FlipY * create(bool y);
-    }
+  };
+  /* \brief specifies how to split a rowset into two */
+  struct Split {
+    std::vector<size_t> left;
+    std::vector<size_t> right;
+  };
     
     
     {
-    {
-    {            if (action->getTag() == (int)tag && action->getOriginalTarget() == target)
-            {
-                removeActionAtIndex(i, element);
-                --limit;
-            }
-            else
-            {
-                ++i;
-            }
-        }
-    }
-}
-    
-        /**
-     * @brief Initializes the action with a duration, a 'from' percentage and a 'to' percentage.
-     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
-     * @param fromPercentage Specify the source percentage.
-     * @param toPercentage Specify the destination percentage.
-     * @return If the creation success, return true; otherwise, return false.
-     */
-    bool initWithDuration(float duration, float fromPercentage, float toPercentage);
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __CCACTIONTWEEN_H__
-#define __CCACTIONTWEEN_H__
-    
-        /** Adds a frame with a texture and a rect. Internally it will create a SpriteFrame and it will add it.
-     * The frame will be added with one 'delay unit'.
-     * Added to facilitate the migration from v0.8 to v0.9.
-     * @param pobTexture A frame with a texture.
-     * @param rect The Texture of rect.
-     */
-    void addSpriteFrameWithTexture(Texture2D* pobTexture, const Rect& rect);
+    {}  // namespace obj
+}  // namespace xgboost
