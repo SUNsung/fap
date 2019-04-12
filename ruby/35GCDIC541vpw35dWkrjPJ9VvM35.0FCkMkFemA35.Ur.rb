677805c08ed5964207ef9a0ed21528a39eef0dd3
SUNsung <weatherius@gmail.com>
2019-04-12 21:46:40 +0300

    Rewrite [35GCDIC541vpw35dWkrjPJ9VvM35.0FCkMkFemA35.Ur.rb] File in: 2019-04-12 21:46:39

--- ruby/35GCDIC541vpw35dWkrjPJ9VvM35.0FCkMkFemA35.Ur.rb
+++ ruby/35GCDIC541vpw35dWkrjPJ9VvM35.0FCkMkFemA35.Ur.rb
@@ -1,80 +1,42 @@
-def linkify_prs(markdown)
-  markdown.gsub(%r!(?<\!&)#(\d+)!) do |word|
-    '[#{word}]({{ site.repository }}/issues/#{word.delete('#')})'
-  end
-end
-    
-    # For this pull request, which changes Page#dir
-# https://github.com/jekyll/jekyll/pull/4403
-    
-          #
-    
-              # This is too noisy even for --verbose, but uncomment if you need it for
-          # a specific WebSockets issue.  Adding ?LR-verbose=true onto the URL will
-          # enable logging on the client side.
-          # em_opts[:debug] = true
-    
-          def fallback_data
-        @fallback_data ||= {}
-      end
+
+        
+            def url
+      @url ||= URL.parse request.base_url
     end
-  end
+    
+            css('.note h3', '.warning h3').each do |node|
+          node.before('<p><strong>#{node.inner_html}</strong></p>').remove
+        end
+    
+      end
 end
 
     
-    group :development do
-  cp_gem 'claide',                'CLAide'
-  cp_gem 'cocoapods-core',        'Core'
-  cp_gem 'cocoapods-deintegrate', 'cocoapods-deintegrate'
-  cp_gem 'cocoapods-downloader',  'cocoapods-downloader'
-  cp_gem 'cocoapods-plugins',     'cocoapods-plugins'
-  cp_gem 'cocoapods-search',      'cocoapods-search'
-  cp_gem 'cocoapods-stats',       'cocoapods-stats'
-  cp_gem 'cocoapods-trunk',       'cocoapods-trunk'
-  cp_gem 'cocoapods-try',         'cocoapods-try'
-  cp_gem 'molinillo',             'Molinillo'
-  cp_gem 'nanaimo',               'Nanaimo'
-  cp_gem 'xcodeproj',             'Xcodeproj'
+          def remove(klass)
+        entries.delete_if { |entry| entry.klass == klass }
+      end
     
-      node[:applications].each do |app, data|
-    template '/etc/monit.d/sidekiq_#{app}.monitrc' do 
-      owner 'root' 
-      group 'root' 
-      mode 0644 
-      source 'monitrc.conf.erb' 
-      variables({ 
-        :num_workers => worker_count,
-        :app_name => app, 
-        :rails_env => node[:environment][:framework_env] 
-      }) 
-    end
+            middlewares.each {|middleware, block| use(*middleware, &block) }
     
-    require 'fpm'
-require 'tmpdir'
-require 'fpm/package/pleaserun'
+        def _erb(file, locals)
+      locals.each {|k, v| define_singleton_method(k){ v } unless (singleton_methods.include? k)} if locals
     
-      # Any other attributes specific to this package.
-  # This is where you'd put rpm, deb, or other specific attributes.
-  attr_accessor :attributes
+    When /^(?:|I )select '([^']*)' from '([^']*)'$/ do |value, field|
+  select(value, :from => field)
+end
     
-        # Query details about our now-installed package.
-    # We do this by using 'npm ls' with json + long enabled to query details
-    # about the installed package.
-    npm_ls_out = safesystemout(attributes[:npm_bin], 'ls', '--json', '--long', *npm_flags)
-    npm_ls = JSON.parse(npm_ls_out)
-    name, info = npm_ls['dependencies'].first
-    
-      def iteration
-    return @iteration ? @iteration : 1
+      def framework_major_version
+    framework_version.split('.').first.to_i
   end
+end
+World(RailsCommandHelpers)
+
     
-      def default_output
-    v = version
-    v = '#{epoch}:#{v}' if epoch
-    if iteration
-      '#{name}_#{v}-#{iteration}_#{architecture}.#{type}'
-    else
-      '#{name}_#{v}_#{architecture}.#{type}'
+        def self.register(klass, attachment_name, attachment_options)
+      instance.register(klass, attachment_name, attachment_options)
     end
-  end # def default_output
-end # class FPM::Deb
+    
+        # Returns the smaller of the two dimensions
+    def smaller
+      [height, width].min
+    end
