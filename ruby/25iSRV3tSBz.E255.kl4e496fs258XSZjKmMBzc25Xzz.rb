5b1d663e568466fd406b692a8edcc17d87b333e9
SUNsung <weatherius@gmail.com>
2019-04-13 17:14:54 +0300

    Rewrite [25iSRV3tSBz.E255.kl4e496fs258XSZjKmMBzc25Xzz.rb] File in: 2019-04-13 17:14:54

--- ruby/25iSRV3tSBz.E255.kl4e496fs258XSZjKmMBzc25Xzz.rb
+++ ruby/25iSRV3tSBz.E255.kl4e496fs258XSZjKmMBzc25Xzz.rb
@@ -1,267 +1,151 @@
 
         
-                  def label(label_html_options = {}, &block)
-            html_options = @input_html_options.slice(:index, :namespace).merge(label_html_options)
-            html_options[:for] ||= @input_html_options[:id] if @input_html_options[:id]
+                  expect(weather_agent.name).to eq('a weather agent')
+          expect(weather_agent.schedule).to eq('5pm')
+          expect(weather_agent.keep_events_for).to eq(14.days)
+          expect(weather_agent.propagate_immediately).to be_falsey
+          expect(weather_agent).to be_disabled
+          expect(weather_agent.memory).to be_empty
+          expect(weather_agent.options).to eq(weather_agent_options)
     
-              def field_type
-            self.class.field_type
+        it 'should convert the 'escape' method correctly' do
+      expect(LiquidMigrator.convert_string('Escaped: <escape $.content.name>\nNot escaped: <$.content.name>')).to eq(
+                                    'Escaped: {{content.name | uri_escape}}\nNot escaped: {{content.name}}'
+      )
+    end
+    
+        it 'should revert extract and template options for an updated WebsiteAgent' do
+      expect(agent.options).to include('extract' => new_extract,
+                                       'template' => new_template)
+      ConvertWebsiteAgentTemplateForMerge.new.down
+      agent.reload
+      expect(agent.options).to include('extract' => reverted_extract,
+                                       'template' => reverted_template)
+    end
+  end
+end
+
+    
+    describe Agents::AftershipAgent do
+  before do
+    
+        def replace(index, name)
+      @filters[assert_index(index)] = filter_const(name)
+    end
+    
+        DOCUMENT_RGX = /\A(?:\s|(?:<!--.*?-->))*<(?:\!doctype|html)/i
+    
+          max_length = if tag = str.slice!(/ \[.+\]\z/)
+        terminal_width - tag.length
+      else
+        terminal_width
+      end
+    
+            css('.filetree .children').each do |node|
+          node.css('.file').each do |n|
+            n.content = '  #{n.content}'
           end
+        end
+    
+            css('.nav-index-section').each do |node|
+          node.content = node.content
+        end
+    
+    namespace :bower do
+    
+        def log_processed(name)
+      puts green '    #{name}'
+    end
+    
+        it 'returns an array of likes for a post' do
+      bob.like!(@message)
+      get :index, params: {post_id: @message.id}
+      expect(JSON.parse(response.body).map {|h| h['id'] }).to match_array(@message.likes.map(&:id))
+    end
+    
+        it 'succeeds for notification dropdown' do
+      Timecop.travel(6.seconds.ago) do
+        @notification.touch
+      end
+      get :index, format: :json
+      expect(response).to be_success
+      response_json = JSON.parse(response.body)
+      note_html = Nokogiri::HTML(response_json['notification_list'][0]['also_commented']['note_html'])
+      timeago_content = note_html.css('time')[0]['data-time-ago']
+      expect(response_json['unread_count']).to be(1)
+      expect(response_json['unread_count_by_type']).to eq(
+        'also_commented'       => 1,
+        'comment_on_post'      => 0,
+        'contacts_birthday'    => 0,
+        'liked'                => 0,
+        'mentioned'            => 0,
+        'mentioned_in_comment' => 0,
+        'reshared'             => 0,
+        'started_sharing'      => 0
+      )
+      expect(timeago_content).to include(@notification.updated_at.iso8601)
+      expect(response.body).to match(/note_html/)
+    end
+    
+        def self.find_by_name_with_wildcards(pattern)
+      re = transform_pattern_into_re(pattern)
+      ::Gem::Specification.find_all.select do |specification|
+        specification.name =~ re
+      end
+    end
+    
+            if Utils::HttpClient.remote_file_exist?(uri)
+          PluginManager.ui.debug('Found package at: #{uri}')
+          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
+        else
+          PluginManager.ui.debug('Package not found at: #{uri}')
+          return nil
+        end
+      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
+        # This probably means there is a firewall in place of the proxy is not correctly configured.
+        # So lets skip this strategy but log a meaningful errors.
+        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
+    
+          # Try to add the gems to the current gemfile and lock file, if successful
+      # both of them will be updated. This injector is similar to Bundler's own injector class
+      # minus the support for additionals source and doing local resolution only.
+      ::Bundler::LogstashInjector.inject!(pack)
+    
+          it 'returns true if the pipeline is a system pipeline' do
+        expect(subject.system?).to be_truthy
+      end
+    end
+    
+          context 'when the plugin doesnt exist' do
+        it 'fails to install and report an error' do
+          command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-output-impossible-plugin')
+          expect(command.stderr).to match(/Plugin not found, aborting/)
+        end
       end
     end
   end
 end
 
     
-        %w(<< concat push insert unshift).each do |method|
-      class_eval <<-METHOD, __FILE__, __LINE__ + 1
-        def #{method}(*args)
-          paths.#{method}(*typecast(args))
-        end
-      METHOD
+          it 'list the plugin with his version' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name} \(\d+\.\d+.\d+\)/)
+      end
     end
+  end
+end
+
     
-    def siteify_file(file, overrides_front_matter = {})
-  abort 'You seem to have misplaced your #{file} file. I can haz?' unless File.exist?(file)
-  title = begin
-            File.read(file).match(%r!\A# (.*)$!)[1]
-          rescue NoMethodError
-            File.basename(file, '.*').downcase.capitalize
+            def delta(first, second, alignment = :left)
+          case alignment
+          when :left
+            first.column - second.column
+          when :right
+            first.last_column - second.last_column
+          else
+            0
           end
-  slug  = File.basename(file, '.markdown').downcase
-  front_matter = {
-    'title'     => title,
-    'permalink' => '/docs/#{slug}/',
-    'note'      => 'This file is autogenerated. Edit /#{file} instead.',
-  }.merge(overrides_front_matter)
-  contents = '#{front_matter.to_yaml}---\n\n#{content_for(file)}'
-  File.write('#{docs_folder}/_docs/#{slug}.md', contents)
-end
-    
-    # Sample contents
-WITHOUT_LIQUID = <<-TEXT.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. Etiam bibendum magna et metus fermentum, eu cursus lorem
-mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
-    
-    require 'benchmark/ips'
-require 'jekyll'
-require 'json'
-    
-    require 'benchmark/ips'
-require 'pathutil'
-    
-    module Jekyll
-  module Commands
-    class NewTheme < Jekyll::Command
-      class << self
-        def init_with_program(prog)
-          prog.command(:'new-theme') do |c|
-            c.syntax 'new-theme NAME'
-            c.description 'Creates a new Jekyll theme scaffold'
-            c.option 'code_of_conduct', \
-                     '-c', '--code-of-conduct', \
-                     'Include a Code of Conduct. (defaults to false)'
-    
-          def site
-        @site_drop ||= SiteDrop.new(@obj)
-      end
-    
-          def self.available_options
-        [
-          FastlaneCore::ConfigItem.new(key: :tag,
-                                       env_name: 'FL_GIT_TAG_TAG',
-                                       description: 'Define your own tag text. This will replace all other parameters',
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :grouping,
-                                       env_name: 'FL_GIT_TAG_GROUPING',
-                                       description: 'Is used to keep your tags organised under one 'folder'',
-                                       default_value: 'builds'),
-          FastlaneCore::ConfigItem.new(key: :prefix,
-                                       env_name: 'FL_GIT_TAG_PREFIX',
-                                       description: 'Anything you want to put in front of the version number (e.g. 'v')',
-                                       default_value: ''),
-          FastlaneCore::ConfigItem.new(key: :postfix,
-                                       env_name: 'FL_GIT_TAG_POSTFIX',
-                                       description: 'Anything you want to put at the end of the version number (e.g. '-RC1')',
-                                       default_value: ''),
-          FastlaneCore::ConfigItem.new(key: :build_number,
-                                       env_name: 'FL_GIT_TAG_BUILD_NUMBER',
-                                       description: 'The build number. Defaults to the result of increment_build_number if you\'re using it',
-                                       default_value: Actions.lane_context[Actions::SharedValues::BUILD_NUMBER],
-                                       default_value_dynamic: true,
-                                       is_string: false),
-          FastlaneCore::ConfigItem.new(key: :message,
-                                       env_name: 'FL_GIT_TAG_MESSAGE',
-                                       description: 'The tag message. Defaults to the tag's name',
-                                       default_value_dynamic: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :commit,
-                                       env_name: 'FL_GIT_TAG_COMMIT',
-                                       description: 'The commit or object where the tag will be set. Defaults to the current HEAD',
-                                       default_value_dynamic: true,
-                                       optional: true),
-          FastlaneCore::ConfigItem.new(key: :force,
-                                       env_name: 'FL_GIT_TAG_FORCE',
-                                       description: 'Force adding the tag',
-                                       optional: true,
-                                       is_string: false,
-                                       default_value: false),
-          FastlaneCore::ConfigItem.new(key: :sign,
-                                       env_name: 'FL_GIT_TAG_SIGN',
-                                       description: 'Make a GPG-signed tag, using the default e-mail address's key',
-                                       optional: true,
-                                       is_string: false,
-                                       default_value: false)
-        ]
-      end
-    
-            it 'executes the correct git command' do
-          allow(Fastlane::Actions).to receive(:sh).with('git add #{path.shellescape}', anything).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: '#{path}')
-          end').runner.execute(:test)
         end
-      end
     
-    class String
-  # CrossplatformShellwords
-  def shellescape
-    CrossplatformShellwords.shellescape(self)
-  end
-end
-    
-          it 'uses the correct command to import it' do
-        # We have to execute *something* using ` since otherwise we set expectations to `nil`, which is not healthy
-        `ls`
-    
-    if git.modified_files.include?('snapshot/lib/assets/SnapshotHelperXcode8.swift')
-  warn('You modified `SnapshotHelperXcode8.swift`, make sure to update the version number at the bottom of the file to notify users about the new helper file.')
-end
-    
-    module Cert
-  class CommandsGenerator
-    include Commander::Methods
-    
-      # GET /resource/unlock?unlock_token=abcdef
-  def show
-    self.resource = resource_class.unlock_access_by_token(params[:unlock_token])
-    yield resource if block_given?
-    
-      ActiveSupport.run_load_hooks(:devise_controller, self)
-end
-
-    
-    class ApplicationController < ActionController::Base
-end
-    
-          def remember_key(resource, scope)
-        resource.rememberable_options.fetch(:key, 'remember_#{scope}_token')
-      end
-    end
-  end
-end
-
-    
-    module Devise
-  module Controllers
-    # Provide the ability to store a location.
-    # Used to redirect back to a desired path after sign in.
-    # Included by default in all controllers.
-    module StoreLocation
-      # Returns and delete (if it's navigational format) the url stored in the session for
-      # the given scope. Useful for giving redirect backs after sign up:
-      #
-      # Example:
-      #
-      #   redirect_to stored_location_for(:user) || root_path
-      #
-      def stored_location_for(resource_or_scope)
-        session_key = stored_location_key_for(resource_or_scope)
-    
-          protected
-    
-        # Include the chosen devise modules in your model:
-    #
-    #   devise :database_authenticatable, :confirmable, :recoverable
-    #
-    # You can also give any of the devise configuration values in form of a hash,
-    # with specific values for this model. Please check your Devise initializer
-    # for a complete description on those values.
-    #
-    def devise(*modules)
-      options = modules.extract_options!.dup
-    
-          protected
-    
-        ShareVisibility.joins('INNER JOIN photos ON photos.id = share_visibilities.shareable_id')
-                   .where(shareable_type: 'Photo', photos: {public: true}).delete_all
-    
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
-    end
-    
-        it 'generates a jasmine fixture', fixture: true do
-      session[:mobile_view] = true
-      get :new, format: :mobile
-      save_fixture(html_for('body'), 'conversations_new_mobile')
-    end
-  end
-end
-
-    
-        context 'on a post you do not partecipate to' do
-      it 'says it is an unprocessable request' do
-        delete :destroy, params: {post_id: post.id}
-        expect(response.code).to eq('422')
-      end
-    end
-  end
-end
-
-    
-    $stderr.puts <<DEPRECATION
-WARNING: Ruby Sass's Git repository is moving, and the old repository will be
-deled on 26 March 2019! Please update your Git URLs to point to the new
-repository at https://github.com/sass/ruby-sass.
-    
-      # This class handles the parsing and compilation of the Sass template.
-  # Example usage:
-  #
-  #     template = File.read('stylesheets/sassy.sass')
-  #     sass_engine = Sass::Engine.new(template)
-  #     output = sass_engine.render
-  #     puts output
-  class Engine
-    @@old_property_deprecation = Deprecation.new
-    
-        # Tells optparse how to parse the arguments.
-    #
-    # @param opts [OptionParser]
-    def set_opts(opts)
-      opts.banner = <<END
-Usage: sass-convert [options] [INPUT] [OUTPUT]
-    
-          opts.on('--watch', 'Watch files or directories for changes.',
-                         'The location of the generated CSS can be set using a colon:',
-                         '  #{@default_syntax} --watch input.#{@default_syntax}:output.css',
-                         '  #{@default_syntax} --watch input-dir:output-dir') do
-        @options[:watch] = true
-      end
-    
-          # A string representation of the importer.
-      # Should be overridden by subclasses.
-      #
-      # This is used to help debugging,
-      # and should usually just show the load path encapsulated by this importer.
-      #
-      # @return [String]
-      def to_s
-        Sass::Util.abstract(self)
-      end
+        def each_directive
+      return if processed_source.comments.nil?
