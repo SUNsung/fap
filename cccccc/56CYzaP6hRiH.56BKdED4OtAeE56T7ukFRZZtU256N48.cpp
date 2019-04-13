db241fe9ff3f8463d63cbcd0d01f2973092a33f7
SUNsung <weatherius@gmail.com>
2019-04-14 02:01:56 +0300

    Rewrite [56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp] File in: 2019-04-14 02:01:55

--- cccccc/56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp
+++ cccccc/56CYzaP6hRiH.56BKdED4OtAeE56T7ukFRZZtU256N48.cpp
@@ -1,279 +1,174 @@
 
         
-        void DiverseStackBase::pushNewStorageSlow(std::size_t needed) {
-  bool wasInline = isAllocatedInline();
+        #include <google/protobuf/stubs/logging.h>
+#include <google/protobuf/stubs/common.h>
+    
+    #include <google/protobuf/stubs/common.h>
+#include <gtest/gtest.h>
+    
+    #include 'addressbook.pb.h'
+    
+    // Iterates though all people in the AddressBook and prints info about them.
+void ListPeople(const tutorial::AddressBook& address_book) {
+  for (int i = 0; i < address_book.people_size(); i++) {
+    const tutorial::Person& person = address_book.people(i);
+    }
     }
     
     
-    
-    /// Translate the given operator character into its mangled form.
-///
-/// Current operator characters:   @/=-+*%<>!&|^~ and the special operator '..'
-char Mangle::translateOperatorChar(char op) {
-  switch (op) {
-    case '&': return 'a'; // 'and'
-    case '@': return 'c'; // 'commercial at sign'
-    case '/': return 'd'; // 'divide'
-    case '=': return 'e'; // 'equal'
-    case '>': return 'g'; // 'greater'
-    case '<': return 'l'; // 'less'
-    case '*': return 'm'; // 'multiply'
-    case '!': return 'n'; // 'negate'
-    case '|': return 'o'; // 'or'
-    case '+': return 'p'; // 'plus'
-    case '?': return 'q'; // 'question'
-    case '%': return 'r'; // 'remainder'
-    case '-': return 's'; // 'subtract'
-    case '~': return 't'; // 'tilde'
-    case '^': return 'x'; // 'xor'
-    case '.': return 'z'; // 'zperiod' (the z is silent)
-    default:
-      return op;
-  }
-}
-    
-    bool Punycode::decodePunycodeUTF8(StringRef InputPunycode,
-                                  std::string &OutUTF8) {
-  std::vector<uint32_t> OutCodePoints;
-  if (!decodePunycode(InputPunycode, OutCodePoints))
-    return false;
-    }
-    
-    void JobAction::anchor() {}
-    
-    #define TEGRA_SUB(src1, sz1, src2, sz2, dst, sz, w, h) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-    CAROTENE_NS::sub(CAROTENE_NS::Size2D(w, h), \
-                     src1, sz1, \
-                     src2, sz2, \
-                     dst, sz, \
-                     CAROTENE_NS::CONVERT_POLICY_SATURATE), /*Original addition use saturated operator*/ \
-                                                            /*so use the same from CAROTENE*/ \
-    CV_HAL_ERROR_OK \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
-    
-    void add(const Size2D &size,
-         const f32 * src0Base, ptrdiff_t src0Stride,
-         const f32 * src1Base, ptrdiff_t src1Stride,
-         f32 * dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride,
-                         AddWrap<f32, f32>());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
-}
-    
-    
-    {    void operator() (const u32 * src0, const u32 * src1, u32 * dst) const
-    {
-        dst[0] = saturate_cast<u32>(alpha*src0[0] + beta*src1[0] + gamma);
-    }
+    {  // No copying allowed
+  LookupKey(const LookupKey&);
+  void operator=(const LookupKey&);
 };
     
-    
-    {    void operator() (const u8 * src0, const u8 * src1, u8 * dst) const
-    {
-        dst[0] = src0[0] ^ src1[0];
-    }
-};
-    
-            for ( ; x < width; ++x)
-        {
-            // make extrapolation for the last elements
-            if (x + 1 >= width)
-            {
-                if (border == BORDER_MODE_CONSTANT)
-                    nextx = borderValue * 3;
-                else if (border == BORDER_MODE_REPLICATE)
-                    nextx = srow2[x] + srow1[x] + srow0[x];
-            }
-            else
-                nextx = (srow2 ? srow2[x + 1] : borderValue) +
-                                 srow1[x + 1] +
-                        (srow0 ? srow0[x + 1] : borderValue);
-    }
-    
-    #define SPLIT_QUAD(sgn, bits, n) { \
-                                     internal::prefetch(src + sj); \
-                                     vec128 v_src = vld##n##q_##sgn##bits(src + sj); \
-                                     FILL_LINES##n(VST1Q, sgn##bits) \
-                                 }
-    
-    #endif
-    
-                    int16x8_t t0_16s = vreinterpretq_s16_u16(vmovl_u8(t0));
-                int16x8_t t1_16s = vreinterpretq_s16_u16(vmovl_u8(t1));
-                int16x8_t t2_16s = vreinterpretq_s16_u16(vmovl_u8(t2));
-    
-    template <typename T>
-inline void inRangeCheck(const Size2D &_size,
-                         const T * srcBase, ptrdiff_t srcStride,
-                         const T * rng1Base, ptrdiff_t rng1Stride,
-                         const T * rng2Base, ptrdiff_t rng2Stride,
-                         u8 * dstBase, ptrdiff_t dstStride)
-{
-    typedef typename internal::VecTraits<T>::vec128 vec128;
-    typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
-    }
-    
-    inline float32x2_t vsqrt_f32(float32x2_t val)
-{
-    return vrecp_f32(vrsqrt_f32(val));
-}
-    
-    #ifndef STORAGE_LEVELDB_DB_DB_ITER_H_
-#define STORAGE_LEVELDB_DB_DB_ITER_H_
-    
-    LookupKey::LookupKey(const Slice& user_key, SequenceNumber s) {
-  size_t usize = user_key.size();
-  size_t needed = usize + 13;  // A conservative estimate
-  char* dst;
-  if (needed <= sizeof(space_)) {
-    dst = space_;
-  } else {
-    dst = new char[needed];
-  }
-  start_ = dst;
-  dst = EncodeVarint32(dst, usize + 8);
-  kstart_ = dst;
-  memcpy(dst, user_key.data(), usize);
-  dst += usize;
-  EncodeFixed64(dst, PackSequenceAndType(s, kValueTypeForSeek));
-  dst += 8;
-  end_ = dst;
-}
-    
-      ReadOptions ro;
-  ro.fill_cache = false;
-  Iterator* iter = table->NewIterator(ro);
-  std::string r;
-  for (iter->SeekToFirst(); iter->Valid(); iter->Next()) {
-    r.clear();
-    ParsedInternalKey key;
-    if (!ParseInternalKey(iter->key(), &key)) {
-      r = 'badkey '';
-      AppendEscapedStringTo(&r, iter->key());
-      r += '' => '';
-      AppendEscapedStringTo(&r, iter->value());
-      r += ''\n';
-      dst->Append(r);
-    } else {
-      r = ''';
-      AppendEscapedStringTo(&r, key.user_key);
-      r += '' @ ';
-      AppendNumberTo(&r, key.sequence);
-      r += ' : ';
-      if (key.type == kTypeDeletion) {
-        r += 'del';
-      } else if (key.type == kTypeValue) {
-        r += 'val';
-      } else {
-        AppendNumberTo(&r, key.type);
-      }
-      r += ' => '';
-      AppendEscapedStringTo(&r, iter->value());
-      r += ''\n';
-      dst->Append(r);
-    }
-  }
-  s = iter->status();
-  if (!s.ok()) {
-    dst->Append('iterator error: ' + s.ToString() + '\n');
-  }
-    
-    TEST(FileNameTest, Construction) {
-  uint64_t number;
-  FileType type;
-  std::string fname;
-    }
-    
-    class Reader {
+    class MemTableIterator: public Iterator {
  public:
-  // Interface for reporting errors.
-  class Reporter {
-   public:
-    virtual ~Reporter();
-    }
+  explicit MemTableIterator(MemTable::Table* table) : iter_(table) { }
     }
     
-      Status AddRecord(const Slice& slice);
     
-        // Create builder.
-    std::string copy = TableFileName(dbname_, next_file_number_++);
-    WritableFile* file;
-    Status s = env_->NewWritableFile(copy, &file);
-    if (!s.ok()) {
-      return;
+    {}  // namespace leveldb
+
+    
+    // Tag numbers for serialized VersionEdit.  These numbers are written to
+// disk and should not be changed.
+enum Tag {
+  kComparator           = 1,
+  kLogNumber            = 2,
+  kNextFileNumber       = 3,
+  kLastSequence         = 4,
+  kCompactPointer       = 5,
+  kDeletedFile          = 6,
+  kNewFile              = 7,
+  // 8 was used for large value refs
+  kPrevLogNumber        = 9
+};
+    
+      void EncodeTo(std::string* dst) const;
+  Status DecodeFrom(const Slice& src);
+    
+    // WriteBatchInternal provides static methods for manipulating a
+// WriteBatch that we don't want in the public WriteBatch interface.
+class WriteBatchInternal {
+ public:
+  // Return the number of entries in the batch.
+  static int Count(const WriteBatch* batch);
     }
-    TableBuilder* builder = new TableBuilder(options_, file);
     
-      // If a seek to internal key 'k' in specified file finds an entry,
-  // call (*handle_result)(arg, found_key, found_value).
-  Status Get(const ReadOptions& options,
-             uint64_t file_number,
-             uint64_t file_size,
-             const Slice& k,
-             void* arg,
-             void (*handle_result)(void*, const Slice&, const Slice&));
+    namespace mxnet {
+namespace io {
+/*! \return the parameter of default augmenter */
+std::vector<dmlc::ParamFieldInfo> ListDefaultAugParams();
+std::vector<dmlc::ParamFieldInfo> ListDefaultDetAugParams();
+}  // namespace io
+}  // namespace mxnet
+#endif  // MXNET_IO_IMAGE_AUGMENTER_H_
+
     
-    TEST(VersionEditTest, EncodeDecode) {
-  static const uint64_t kBig = 1ull << 50;
-    }
     
-            // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-    }
-    
-        // Select graphics queue family
     {
-        uint32_t count;
-        vkGetPhysicalDeviceQueueFamilyProperties(g_PhysicalDevice, &count, NULL);
-        VkQueueFamilyProperties* queues = (VkQueueFamilyProperties*)malloc(sizeof(VkQueueFamilyProperties) * count);
-        vkGetPhysicalDeviceQueueFamilyProperties(g_PhysicalDevice, &count, queues);
-        for (uint32_t i = 0; i < count; i++)
-            if (queues[i].queueFlags & VK_QUEUE_GRAPHICS_BIT)
-            {
-                g_QueueFamily = i;
-                break;
-            }
-        free(queues);
-        IM_ASSERT(g_QueueFamily != (uint32_t)-1);
+    {
+    { private:
+  /*! \brief output data */
+  DataBatch *out_;
+  /*! \brief queue to be recycled */
+  std::queue<DataBatch*> recycle_queue_;
+};
+}  // namespace io
+}  // namespace mxnet
+#endif  // MXNET_IO_ITER_PREFETCHER_H_
+
+    
+    template<typename xpu>
+void Quantize2BitKernelLaunch(mshadow::Stream<xpu> *s, const std::vector<mxnet::TBlob> &inputs,
+                              const float threshold) {
+  mxnet::op::mxnet_op::Kernel<quantize_2bit, xpu>
+    ::Launch(s,
+            inputs[2].Size(),         // compressed array size
+            inputs[0].Size(),         // original size
+            inputs[2].dptr<float>(),  // compressed array
+            inputs[0].dptr<float>(),  // original array
+            inputs[1].dptr<float>(),  // residual array
+            -1 *threshold,            // negative threshold
+            threshold);               // positive threshold
+}
+    
+    #ifndef MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
+#define MXNET_KVSTORE_GRADIENT_COMPRESSION_H_
+#include <dmlc/parameter.h>
+#include <string>
+#include <utility>
+#include <vector>
+#include 'mxnet/ndarray.h'
+    
+    template<>
+void EvalRandom<DEVICE, GammaDistribution>(
+    const real_t &alpha,
+    const real_t &beta,
+    const Resource &resource,
+    TBlob *ret,
+    RunContext ctx) {
+  typedef cpu xpu;  // No support for gpu for this distribution.
+  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
+  switch (ret->type_flag_) {
+  case mshadow::kFloat32:
+    {
+      mshadow::Random<xpu, float> *prnd = resource.get_random<xpu, float>(s);
+      mshadow::Tensor<xpu, 2, float> tmp = ret->FlatTo2D<xpu, float>(s);
+      prnd->SampleGamma(&tmp, float(alpha), float(beta));  // NOLINT(*)
+      break;
+    }
+  case mshadow::kFloat64:
+    {
+      mshadow::Random<xpu, double> *prnd = resource.get_random<xpu, double>(s);
+      mshadow::Tensor<xpu, 2, double> tmp = ret->FlatTo2D<xpu, double>(s);
+      prnd->SampleGamma(&tmp, double(alpha), double(beta));  // NOLINT(*)
+      break;
+    }
+  default:
+    LOG(FATAL) << 'Random only support float32 and float64';
+  }
+}
+    
+    
+    {
+    {}  // namespace op
+}  // namespace mxnet
+
+    
+    
+    {  CHECK(param_.pinfo->forward(ptrs.size(), ptrs.data(), tags.data(), param_.pinfo->p_forward));
+  Engine::Get()->PushAsync(
+      [ndcpy, ctx](RunContext rctx, Engine::CallbackOnComplete on_complete) {
+        ctx.async_on_complete();
+        on_complete();
+      }, ndctx, ndvar, {}, FnProperty::kNormal, 0, 'NDArrayOpForward');
+}
+    
+                callback(m_samples.second, m_updates.second, m_totalSummaries, m_loss.second, m_metric.second, durationMs);
+    
+    #pragma once
+    
+        // Acquires the mutex. If 'wait' is true and mutex is acquired by someone else then
+    // function waits until mutex is released
+    // Returns false if !wait and lock cannot be acquired, or in case of a system error that prevents us from acquiring the lock.
+    bool Acquire(bool wait)
+    {
+        assert(m_handle == NULL);
+        m_handle = ::CreateMutexA(NULL /*security attr*/, FALSE /*bInitialOwner*/, m_name.c_str());
+        if (m_handle == NULL)
+        {
+            if (!wait)
+                return false;   // can't lock due to access permissions: lock already exists, consider not available
+            else
+                RuntimeError('Acquire: Failed to create named mutex %s: %d.', m_name.c_str(), GetLastError());
+        }
     }
     
-        // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+        string precision = config(L'precision', 'float');
     
-                ImGui::SliderFloat('float', &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3('clear color', (float*)&clear_color); // Edit 3 floats representing a color
+    using namespace std;
     
-                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
-            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox('Another Window', &show_another_window);
-    
-    #ifdef _WIN32
-    SDL_SysWMinfo wmInfo;
-    SDL_VERSION(&wmInfo.version);
-    SDL_GetWindowWMInfo(window, &wmInfo);
-    io.ImeWindowHandle = wmInfo.info.win.window;
-#else
-    (void)window;
-#endif
+    // -----------------------------------------------------------------------
+// EpochAccumulatorNode calculates mean values of all samples used in forward pass.
+// -----------------------------------------------------------------------
