51dffdd7d119a5524adfcc6d33e0a670f5dc4c5e
SUNsung <weatherius@gmail.com>
2019-04-13 08:51:26 +0300

    Rewrite [40PZ3wERdoxJM409.SMH08PAfw40I8nceK5LVm6406kV.cpp] File in: 2019-04-13 08:51:25

--- cccccc/40PZ3wERdoxJM409.SMH08PAfw40I8nceK5LVm6406kV.cpp
+++ cccccc/40PZ3wERdoxJM409.SMH08PAfw40I8nceK5LVm6406kV.cpp
@@ -1,124 +1,230 @@
 
         
-        #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
-#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
+        #include <condition_variable>
+#include <mutex>
+#include <string>
+#include <system_error>
+#include <vector>
+    
+    REGISTER_GRADIENT(Sub, GetSubGradient);
+    
+    OPERATOR_SCHEMA(GivenTensorIntFill)
+    .NumInputs(0, 1)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .Arg(
+        'values',
+        'The value for the elements of the output tensor.',
+        true /* required */)
+    .Arg(
+        'shape',
+        'The shape of the output tensor.'
+        'Cannot set the shape argument and pass in an input at the same time.')
+    .Arg(
+        'extra_shape',
+        'The additional dimensions appended at the end of the shape indicated'
+        'by the input blob.'
+        'Cannot set the extra_shape argument when there is no input blob.')
+    .Arg(
+        'input_as_shape',
+        '1D tensor containing the desired output shape. First input must be in CPU context.')
+    .TensorInferenceFunction(FillerTensorInference<TensorProto_DataType_INT32>);
     
     
-    {  if (op->send_message() != nullptr) {
-    ++sent_message_count_;
+    {          return out;
+        })
+    .Input(0, 'X', '4-tensor in NCHW or NHWC.')
+    .Output(
+        0,
+        'Y',
+        '4-tensor. For NCHW: N x (C x kH x kW) x outH x outW.'
+        'For NHWC: N x outH x outW x (kH x kW x C');
+    
+    // Converts a streamable value to an std::string.  A NULL pointer is
+// converted to '(null)'.  When the input value is a ::string,
+// ::std::string, ::wstring, or ::std::wstring object, each NUL
+// character in it is replaced with '\\0'.
+template <typename T>
+std::string StreamableToString(const T& streamable) {
+  return (Message() << streamable).GetString();
+}
+    
+    // GetTypeId<T>() returns the ID of type T.  Different values will be
+// returned for different types.  Calling the function twice with the
+// same type argument is guaranteed to return the same ID.
+template <typename T>
+TypeId GetTypeId() {
+  // The compiler is required to allocate a different
+  // TypeIdHelper<T>::dummy_ variable for each T used to instantiate
+  // the template.  Therefore, the address of dummy_ is guaranteed to
+  // be unique.
+  return &(TypeIdHelper<T>::dummy_);
+}
+    
+      // Many linked_ptr operations may change p.link_ for some linked_ptr
+  // variable p in the same circle as this object.  Therefore we need
+  // to prevent two such operations from occurring concurrently.
+  //
+  // Note that different types of linked_ptr objects can coexist in a
+  // circle (e.g. linked_ptr<Base>, linked_ptr<Derived1>, and
+  // linked_ptr<Derived2>).  Therefore we must use a single mutex to
+  // protect all linked_ptr objects.  This can create serious
+  // contention in production code, but is acceptable in a testing
+  // framework.
+    
+      // Clones a 0-terminated C string, allocating memory using new.  The
+  // caller is responsible for deleting the return value using
+  // delete[].  Returns the cloned string, or NULL if the input is
+  // NULL.
+  //
+  // This is different from strdup() in string.h, which allocates
+  // memory using malloc().
+  static const char* CloneCString(const char* c_str);
+    
+    #include <limits.h>
+#include 'sample1.h'
+#include 'gtest/gtest.h'
+    
+    
+    {  return clone;
+}
+    
+    
+    {  // Can we set the MyString to NULL?
+  s.Set(NULL);
+  EXPECT_STREQ(NULL, s.c_string());
+}
+
+    
+    
+    {}  // namespace grpc
+
+    
+    AuthPropertyIterator::AuthPropertyIterator(
+    const grpc_auth_property* property, const grpc_auth_property_iterator* iter)
+    : property_(property),
+      ctx_(iter->ctx),
+      index_(iter->index),
+      name_(iter->name) {}
+    
+    #include <grpcpp/impl/channel_argument_option.h>
+    
+    struct DataStreamWrapper final : Stream::Wrapper {
+  DataStreamWrapper() {
+    m_isLocal = true;
   }
-  if (op->recv_message() != nullptr) {
-    recv_message_ = op->op()->payload->recv_message.recv_message;
-    initial_on_done_recv_message_ =
-        op->op()->payload->recv_message.recv_message_ready;
-    op->op()->payload->recv_message.recv_message_ready = &on_done_recv_message_;
-  }
-  if (op->recv_trailing_metadata() != nullptr) {
-    recv_trailing_metadata_ = op->recv_trailing_metadata()->batch();
-    initial_on_done_recv_trailing_metadata_ =
-        op->op()->payload->recv_trailing_metadata.recv_trailing_metadata_ready;
-    op->op()->payload->recv_trailing_metadata.recv_trailing_metadata_ready =
-        &on_done_recv_trailing_metadata_;
-  }
-  // Call next op.
-  grpc_call_next_op(elem, op->op());
-}
-    
-    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
-constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
-constexpr size_t TraceContextEncoding::kVersionIdSize;
-constexpr size_t TraceContextEncoding::kFieldIdSize;
-constexpr size_t TraceContextEncoding::kVersionIdOffset;
-constexpr size_t TraceContextEncoding::kVersionId;
-    
-    const ViewDescriptor& ServerSentMessagesPerRpcHour() {
-  const static ViewDescriptor descriptor =
-      HourDescriptor()
-          .set_name('grpc.io/server/sent_messages_per_rpc/hour')
-          .set_measure(kRpcServerSentMessagesPerRpcMeasureName)
-          .set_aggregation(CountDistributionAggregation())
-          .add_column(ServerMethodTagKey());
-  return descriptor;
-}
-    
-    DynamicThreadPool::DynamicThread::DynamicThread(DynamicThreadPool* pool)
-    : pool_(pool),
-      thd_('grpcpp_dynamic_pool',
-           [](void* th) {
-             static_cast<DynamicThreadPool::DynamicThread*>(th)->ThreadFunc();
-           },
-           this) {
-  thd_.Start();
-}
-DynamicThreadPool::DynamicThread::~DynamicThread() { thd_.Join(); }
-    
-    #include 'src/cpp/server/load_reporter/get_cpu_stats.h'
-    
-    // This utility works with Box2d version 2.0 (or higher), and not with 1.4.3
-    
-    		void Init(Block4x4 *a_pblockParent,
-					ColorFloatRGBA *a_pafrgbaSource,
-    
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-    
-    /** 16x32 multiply, followed by a 16-bit shift right and 32-bit add.
-    Result fits in 32 bits. */
-#undef MAC16_32_Q16
-static OPUS_INLINE opus_val32 MAC16_32_Q16_armv5e(opus_val32 c, opus_val16 a,
- opus_val32 b)
-{
-  int res;
-  __asm__(
-      '#MAC16_32_Q16\n\t'
-      'smlawb %0, %1, %2, %3;\n'
-      : '=r'(res)
-      : 'r'(b), 'r'(a), 'r'(c)
-  );
-  return res;
-}
-#define MAC16_32_Q16(c, a, b) (MAC16_32_Q16_armv5e(c, a, b))
-    
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-    
-    /* silk_min() versions with typecast in the function call */
-static OPUS_INLINE opus_int silk_min_int(opus_int a, opus_int b)
-{
-    return (((a) < (b)) ? (a) : (b));
-}
-static OPUS_INLINE opus_int16 silk_min_16(opus_int16 a, opus_int16 b)
-{
-    return (((a) < (b)) ? (a) : (b));
-}
-static OPUS_INLINE opus_int32 silk_min_32(opus_int32 a, opus_int32 b)
-{
-    return (((a) < (b)) ? (a) : (b));
-}
-static OPUS_INLINE opus_int64 silk_min_64(opus_int64 a, opus_int64 b)
-{
-    return (((a) < (b)) ? (a) : (b));
-}
-    
-    using apollo::common::ErrorCode;
-    
-    TEST(MessageManagerTest, GetMutableProtocolDataById) {
-  uint8_t mock_data = 1;
-  MockMessageManager manager;
-  manager.Parse(MockProtocolData::ID, &mock_data, 8);
-  manager.ResetSendMessages();
-  EXPECT_TRUE(manager.GetMutableProtocolDataById(MockProtocolData::ID) !=
-              nullptr);
     }
     
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-    }
+    #endif // HPHP_GLOB_STREAM_WRAPPER_H
+
+    
+    constexpr int64_t kDefaultPerfWarningRate = 100;
+    
+        Rational Sin(Rational const& rat, ANGLE_TYPE angletype);
+    Rational Cos(Rational const& rat, ANGLE_TYPE angletype);
+    Rational Tan(Rational const& rat, ANGLE_TYPE angletype);
+    Rational ASin(Rational const& rat, ANGLE_TYPE angletype);
+    Rational ACos(Rational const& rat, ANGLE_TYPE angletype);
+    Rational ATan(Rational const& rat, ANGLE_TYPE angletype);
+    
+            static NarratorAnnouncement^ GetAnnouncement(Windows::UI::Xaml::DependencyObject^ element)
+        {
+            return safe_cast<NarratorAnnouncement^>(element->GetValue(s_announcementProperty));
+        }
+    
+    using namespace  ::osquery::extensions;
+    
+    namespace osquery {
+namespace ebpf {
     }
     }
     
-      x <<= 4;
-  x |= t;
+    // NOTE: the original code used the same class for the `reverse()` method
+EASERATE_TEMPLATE_IMPL(EaseIn, tweenfunc::easeIn);
+EASERATE_TEMPLATE_IMPL(EaseOut, tweenfunc::easeOut);
+EASERATE_TEMPLATE_IMPL(EaseInOut, tweenfunc::easeInOut);
+    
+    AccelDeccelAmplitude* AccelDeccelAmplitude::clone() const
+{
+    // no copy constructor
+    if (_other)
+        return AccelDeccelAmplitude::create(_other->clone(), _rate);
+    
+    return nullptr;
+}
+    
+    // implementation of Shaky3D
+    
+    ActionInstant* Show::reverse() const
+{
+    return Hide::create();
+}
+    
+        //
+    // Overrides
+    //
+    virtual RepeatForever* clone() const override;
+    virtual RepeatForever* reverse(void) const override;
+    virtual void startWithTarget(Node* target) override;
+    /**
+     * @param dt In seconds.
+     */
+    virtual void step(float dt) override;
+    virtual bool isDone(void) const override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    RepeatForever()
+    : _innerAction(nullptr)
+    {}
+    virtual ~RepeatForever();
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the 'Software'), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+    
+        // Overrides
+    virtual SplitRows* clone() const override;
+    virtual void update(float time) override;
+    virtual void startWithTarget(Node *target) override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    SplitRows() {}
+    virtual ~SplitRows() {}
+    
+        /* Creates an animation with an array of SpriteFrame and a delay between frames in seconds.
+     * The frames will be added with one 'delay unit'.
+     * @since v0.99.5
+     * @param arrayOfSpriteFrameNames An array of SpriteFrame.
+     * @param delay A delay between frames in seconds.
+     * @param loops The times the animation is going to loop.
+     */
+    static Animation* createWithSpriteFrames(const Vector<SpriteFrame*>& arrayOfSpriteFrameNames, float delay = 0.0f, unsigned int loops = 1);
+    
+        /** Deletes a Animation from the cache.
+     *
+     * @param name The name of animation.
+     */
+    void removeAnimation(const std::string& name);
+    /** @deprecated. Use removeAnimation() instead
+     * @js NA
+     * @lua NA
+     */
+    CC_DEPRECATED_ATTRIBUTE void removeAnimationByName(const std::string& name){ removeAnimation(name);}
+    
+        /**
+     * get vertex count
+     * @return number of vertices
+     */
+    unsigned int getVertCount() const;
+    
+    /**
+     * get triangles count
+     * @return number of triangles
+     */
+    unsigned int getTrianglesCount() const;
