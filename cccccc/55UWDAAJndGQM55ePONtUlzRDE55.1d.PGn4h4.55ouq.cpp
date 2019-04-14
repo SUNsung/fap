42471be5bdc8611d39461a280c37fb1a441f0bcb
SUNsung <weatherius@gmail.com>
2019-04-14 12:40:41 +0300

    Rewrite [55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp] File in: 2019-04-14 12:40:40

--- cccccc/55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp
+++ cccccc/55UWDAAJndGQM55ePONtUlzRDE55.1d.PGn4h4.55ouq.cpp
@@ -1,306 +1,153 @@
 
         
-        // Finally, you are free to instantiate the pattern with the types you
-// want.  If you put the above code in a header file, you can #include
-// it in multiple C++ source files and instantiate it multiple times.
-//
-// To distinguish different instances of the pattern, the first
-// argument to the INSTANTIATE_* macro is a prefix that will be added
-// to the actual test case name.  Remember to pick unique prefixes for
-// different instances.
-typedef testing::Types<char, int, unsigned int> MyTypes;
-INSTANTIATE_TYPED_TEST_CASE_P(My, FooTest, MyTypes);
+          scoped_ptr<base::Value> value_option(
+      converter->FromV8Value(options, isolate->GetCurrentContext()));
+  if (!value_option.get() ||
+      !value_option->IsType(base::Value::TYPE_DICTIONARY))
+    return isolate->ThrowException(v8::Exception::Error(v8::String::NewFromUtf8(isolate,
+        'Unable to convert 'option' passed to AllocateObject')));
     
+       void Call(const std::string& method,
+                    const base::ListValue& arguments) override;
+   void CallSync(const std::string& method,
+                        const base::ListValue& arguments,
+                        base::ListValue* result) override;
     
-    {  // Disables compiler warning 'assignment operator could not be generated.'
-  void operator=(const PreCalculatedPrimeTable& rhs);
-};
-    
-    
-// Step 2. Use the TEST macro to define your tests.
-//
-// TEST has two parameters: the test case name and the test name.
-// After using the macro, you should define your test logic between a
-// pair of braces.  You can use a bunch of macros to indicate the
-// success or failure of a test.  EXPECT_TRUE and EXPECT_EQ are
-// examples of such macros.  For a complete list, see gtest.h.
-//
-// <TechnicalDetails>
-//
-// In Google Test, tests are grouped into test cases.  This is how we
-// keep test code organized.  You should put logically related tests
-// into the same test case.
-//
-// The test case name and the test name should both be valid C++
-// identifiers.  And you should not use underscore (_) in the names.
-//
-// Google Test guarantees that each test you define is run exactly
-// once, but it makes no guarantee on the order the tests are
-// executed.  Therefore, you should write your tests in such a way
-// that their results don't depend on their order.
-//
-// </TechnicalDetails>
-    
-    #include <grpcpp/support/channel_arguments.h>
-    
-    // TODO: This may not be needed. Check to see if opencensus requires
-// a trailing server response.
-// RpcServerStatsEncoding encapsulates the logic for encoding and decoding of
-// rpc server stats messages. Rpc server stats consists of a uint64_t time
-// value (server latency in nanoseconds).
-class RpcServerStatsEncoding {
- public:
-  // Size of encoded RPC server stats.
-  static constexpr size_t kRpcServerStatsSize = 10;
-  // Error value.
-  static constexpr size_t kEncodeDecodeFailure = 0;
+    bool MenuDelegate::IsCommandIdChecked(int command_id) const {
+  if (command_id < 0)
+    return false;
     }
     
-    void DynamicThreadPool::DynamicThread::ThreadFunc() {
-  pool_->ThreadFunc();
-  // Now that we have killed ourselves, we should reduce the thread count
-  std::unique_lock<std::mutex> lock(pool_->mu_);
-  pool_->nthreads_--;
-  // Move ourselves to dead list
-  pool_->dead_threads_.push_back(this);
+    MenuItem::MenuItem(int id,
+                   const base::WeakPtr<ObjectManager>& object_manager,
+                   const base::DictionaryValue& option,
+                   const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id) {
+  Create(option);
+}
+    
+    void MenuItem::Create(const base::DictionaryValue& option) {
+  std::string type;
+  option.GetString('type', &type);
+  submenu_ = NULL;
+  gtk_accel_group = NULL;
     }
     
-      void RunBenchmark(int n, Slice name,
-                    void (Benchmark::*method)(ThreadState*)) {
-    SharedState shared(n);
-    }
     
-    // Return a new iterator that converts internal keys (yielded by
-// '*internal_iter') that were live at the specified 'sequence' number
-// into appropriate user keys.
-Iterator* NewDBIterator(DBImpl* db,
-                        const Comparator* user_key_comparator,
-                        Iterator* internal_iter,
-                        SequenceNumber sequence,
-                        uint32_t seed);
-    
-    Status DumpTable(Env* env, const std::string& fname, WritableFile* dst) {
-  uint64_t file_size;
-  RandomAccessFile* file = nullptr;
-  Table* table = nullptr;
-  Status s = env->GetFileSize(fname, &file_size);
-  if (s.ok()) {
-    s = env->NewRandomAccessFile(fname, &file);
-  }
-  if (s.ok()) {
-    // We use the default comparator, which may or may not match the
-    // comparator used in this database. However this should not cause
-    // problems since we only use Table operations that do not require
-    // any comparisons.  In particular, we do not call Seek or Prev.
-    s = Table::Open(Options(), file, file_size, &table);
-  }
-  if (!s.ok()) {
-    delete table;
-    delete file;
-    return s;
-  }
-    }
-    
-     private:
-  SequentialFile* const file_;
-  Reporter* const reporter_;
-  bool const checksum_;
-  char* const backing_store_;
-  Slice buffer_;
-  bool eof_;   // Last Read() indicated EOF by returning < kBlockSize
-    
-    #include 'db/builder.h'
-#include 'db/db_impl.h'
-#include 'db/dbformat.h'
-#include 'db/filename.h'
-#include 'db/log_reader.h'
-#include 'db/log_writer.h'
-#include 'db/memtable.h'
-#include 'db/table_cache.h'
-#include 'db/version_edit.h'
-#include 'db/write_batch_internal.h'
-#include 'leveldb/comparator.h'
-#include 'leveldb/db.h'
-#include 'leveldb/env.h'
-    
-      // Current state of the test
-  State current_;
-    
-    void TableCache::Evict(uint64_t file_number) {
-  char buf[sizeof(file_number)];
-  EncodeFixed64(buf, file_number);
-  cache_->Erase(Slice(buf, sizeof(buf)));
-}
-    
-      Extension_ping_result result;
-  try {
-    iface_->ping(result.success);
-    result.__isset.success = true;
-  } catch (const std::exception& e) {
-    if (this->eventHandler_.get() != NULL) {
-      this->eventHandler_->handlerError(ctx, 'Extension.ping');
-    }
-    }
-    
-      virtual ~Extension_call_args() throw();
-  std::string registry;
-  std::string item;
-  ExtensionPluginRequest request;
-    
-    
-    {  sentry.commit();
-  return cseqid;
-}
-    
-    #include 'ExtensionManager.h'
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
-#include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TBufferTransports.h>
-    
-    typedef struct _InternalOptionInfo__isset {
-  _InternalOptionInfo__isset() : value(false), default_value(false), type(false) {}
-  bool value :1;
-  bool default_value :1;
-  bool type :1;
-} _InternalOptionInfo__isset;
-    
-    
-    {
-    {    std::ostringstream oss;
-    std::copy(line.begin() + options_index,
-              line.end(),
-              std::ostream_iterator<std::string>(oss, ' '));
-    r['options'] = oss.str();
-    results.push_back(r);
-  }
-}
-    
-    
-    {
-    {
-    {  return results;
-}
-}
-}
+    {} // namespace extensions
+#endif
 
     
-    static char zHelp[] =
-    'Welcome to the osquery shell. Please explore your OS!\n'
-    'You are connected to a transient 'in-memory' virtual database.\n'
-    '\n'
-    '.all [TABLE]     Select all from a table\n'
-    '.bail ON|OFF     Stop after hitting an error\n'
-    '.echo ON|OFF     Turn command echo on or off\n'
-    '.exit            Exit this program\n'
-    '.features        List osquery's features and their statuses\n'
-    '.headers ON|OFF  Turn display of headers on or off\n'
-    '.help            Show this message\n'
-    '.mode MODE       Set output mode where MODE is one of:\n'
-    '                   csv      Comma-separated values\n'
-    '                   column   Left-aligned columns see .width\n'
-    '                   line     One value per line\n'
-    '                   list     Values delimited by .separator string\n'
-    '                   pretty   Pretty printed SQL results (default)\n'
-    '.nullvalue STR   Use STRING in place of NULL values\n'
-    '.print STR...    Print literal STRING\n'
-    '.quit            Exit this program\n'
-    '.schema [TABLE]  Show the CREATE statements\n'
-    '.separator STR   Change separator used by output mode\n'
-    '.socket          Show the osquery extensions socket path\n'
-    '.show            Show the current values for various settings\n'
-    '.summary         Alias for the show meta command\n'
-    '.tables [TABLE]  List names of tables\n'
-    '.types [SQL]     Show result of getQueryColumns for the given query\n'
-    '.width [NUM1]+   Set column widths for \'column\' mode\n';
     
-    namespace osquery {
+       case URX_CARET:                    //  ^, test for start of line
+            if (fp->fInputIdx != fAnchorStart) {
+                fp = (REStackFrame *)fStack->popFrame(fFrameSize);
+            }
+            break;
+    
+    
+ScriptSet &ScriptSet::set(UScriptCode script, UErrorCode &status) {
+    if (U_FAILURE(status)) {
+        return *this;
     }
-    
-    namespace {
+    if (script < 0 || script >= (int32_t)sizeof(bits) * 8) {
+        status = U_ILLEGAL_ARGUMENT_ERROR;
+        return *this;
     }
-    
-    
-    {  // This value is used by EnterExitJoiner, final return value of the syscall
-  // is placed here as a result of join().
-  // Also this member is used by EnterExitJoiner to preserve the age of the
-  // event.
-  __s32 return_value;
-};
-    
-        for (i = 1; i < 1024; i++)
-    {
-        uint32_t key = ((rand() % 19999) + 1) * 37;
-        int ret = (int) (long) swRbtree_find(tree, key);
-        ASSERT_GT(ret, 0);
-        lists.insert(key);
-    }
-    
-    
-    {    //1
-    ret = p.read(&p, buf, sizeof(buf));
-    if (ret < 0)
-    {
-        swSysError('read() failed.');
-    }
-    ASSERT_GT(ret, 0);
-    ASSERT_EQ(strcmp('hello world1', buf), 0);
-    //2
-    ret = p.read(&p, buf, sizeof(buf));
-    ASSERT_GT(ret, 0);
-    ASSERT_EQ(strcmp('hello world2', buf), 0);
-    //3
-    ret = p.read(&p, buf, sizeof(buf));
-    ASSERT_GT(ret, 0);
-    ASSERT_EQ(strcmp('hello world3', buf), 0);
+    uint32_t index = script / 32;
+    uint32_t bit   = 1 << (script & 31);
+    bits[index] |= bit;
+    return *this;
 }
     
-    void Channel::yield(enum opcode type)
-{
-    Coroutine *co = Coroutine::get_current();
-    if (unlikely(!co))
+    
     {
-        swError('Channel::yield() must be called in the coroutine.');
+    {        if (matchlength > 0) {
+            // if matchlength is 0 we are at the start of the iteration
+            if (m_search_->isOverlap) {
+                offset ++;
+            }
+            else {
+                offset += matchlength;
+            }
+        }
+        return handleNext(offset, status);
     }
-    if (type == PRODUCER)
-    {
-        producer_queue.push_back(co);
-        swTraceLog(SW_TRACE_CHANNEL, 'producer cid=%ld', co->get_cid());
-    }
-    else
-    {
-        consumer_queue.push_back(co);
-        swTraceLog(SW_TRACE_CHANNEL, 'consumer cid=%ld', co->get_cid());
-    }
-    co->yield();
+    return USEARCH_DONE;
 }
     
-        http2_stream(int _fd, uint32_t _stream_id)
-    {
-        ctx = swoole_http_context_new(_fd);
-        ctx->stream = (void *) this;
-        stream_id = _stream_id;
-        send_window = SW_HTTP2_DEFAULT_WINDOW_SIZE;
-        recv_window = SW_HTTP2_DEFAULT_WINDOW_SIZE;
+    #include 'unicode/format.h'
+#include 'unicode/locid.h'
+#include 'unicode/parseerr.h'
+#include 'unicode/utypes.h'
+#include 'uvector.h'
+#include 'hash.h'
+    
+    #include 'unicode/utypes.h'
+#include 'sharedobject.h'
+    
+    void StoreBuffer::MoveEntriesToRememberedSet(int index) {
+  if (!lazy_top_[index]) return;
+  DCHECK_GE(index, 0);
+  DCHECK_LT(index, kStoreBuffers);
+  Address last_inserted_addr = kNullAddress;
+  MemoryChunk* chunk = nullptr;
     }
     
-        sleep(1);
-    srand((unsigned int) time(NULL));
-    thread_write();
     
-    TEST(coroutine_base, yield_resume)
-{
-    long _cid;
-    long cid = Coroutine::create([](void *arg)
     {
-        long cid = Coroutine::get_current_cid();
-        Coroutine *co = Coroutine::get_by_cid(cid);
-        co->yield();
-        *(long *) arg = Coroutine::get_current_cid();
-    }, &_cid);
-    }
+    {}  // namespace internal
+}  // namespace v8
+
     
-    void swoole_throw_error(enum swErrorCode code)
-{
-    throw swoole::Exception(code);
-}
+      void FlushToGlobal(int task_id) {
+    PublishPushSegmentToGlobal(task_id);
+    PublishPopSegmentToGlobal(task_id);
+  }
+    
+     private:
+  // Set the ModRM byte without an encoded 'reg' register. The
+  // register is encoded later as part of the emit_operand operation.
+  inline void set_modrm(int mod, Register rm) {
+    DCHECK_EQ(mod & -4, 0);
+    buf_[0] = mod << 6 | rm.code();
+    len_ = 1;
+  }
+    
+      // Replace the current (input) frame with the output frames.
+  Label outer_push_loop, inner_push_loop,
+      outer_loop_header, inner_loop_header;
+  // Outer loop state: eax = current FrameDescription**, edx = one
+  // past the last FrameDescription**.
+  __ mov(edx, Operand(eax, Deoptimizer::output_count_offset()));
+  __ mov(eax, Operand(eax, Deoptimizer::output_offset()));
+  __ lea(edx, Operand(eax, edx, times_system_pointer_size, 0));
+  __ jmp(&outer_loop_header);
+  __ bind(&outer_push_loop);
+  // Inner loop state: esi = current FrameDescription*, ecx = loop
+  // index.
+  __ mov(esi, Operand(eax, 0));
+  __ mov(ecx, Operand(esi, FrameDescription::frame_size_offset()));
+  __ jmp(&inner_loop_header);
+  __ bind(&inner_push_loop);
+  __ sub(ecx, Immediate(sizeof(uint32_t)));
+  __ push(Operand(esi, ecx, times_1, FrameDescription::frame_content_offset()));
+  __ bind(&inner_loop_header);
+  __ test(ecx, ecx);
+  __ j(not_zero, &inner_push_loop);
+  __ add(eax, Immediate(kSystemPointerSize));
+  __ bind(&outer_loop_header);
+  __ cmp(eax, edx);
+  __ j(below, &outer_push_loop);
+    
+      // Rest of parameters passed to JSEntry and JSConstructEntry.
+  static constexpr int kNewTargetArgOffset = +3 * kSystemPointerSize;
+  static constexpr int kFunctionArgOffset = +4 * kSystemPointerSize;
+  static constexpr int kReceiverArgOffset = +5 * kSystemPointerSize;
+  static constexpr int kArgcOffset = +6 * kSystemPointerSize;
+  static constexpr int kArgvOffset = +7 * kSystemPointerSize;
+    
+      // ---------------------------------------------------------------------------
+  // Runtime calls
+    
+      // StoreIC implementation.
