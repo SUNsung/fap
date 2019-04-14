9d693c9c82233fb8896a0f8892da485dca19a287
SUNsung <weatherius@gmail.com>
2019-04-14 23:22:29 +0300

    Rewrite [52Gxn4tsIkuaE52Po3fJ2SeCg52Z99gfg.VeLo52CGP.cpp] File in: 2019-04-14 23:22:28

--- cccccc/52Gxn4tsIkuaE52Po3fJ2SeCg52Z99gfg.VeLo52CGP.cpp
+++ cccccc/52Gxn4tsIkuaE52Po3fJ2SeCg52Z99gfg.VeLo52CGP.cpp
@@ -1,404 +1,65 @@
 
         
-          // Finally, add the generic parameters from the requirement.
-  for (auto genericParam : reqSig->getGenericParams().slice(1)) {
-    // The only depth that makes sense is depth == 1, the generic parameters
-    // of the requirement itself. Anything else is from invalid code.
-    if (genericParam->getDepth() != 1) {
-      return;
-    }
-    }
-    
-    #ifndef NDEBUG
-/// Verify that the types of fields are valid within a given generic signature.
-static void verifyFields(CanGenericSignature Sig, ArrayRef<SILField> Fields) {
-  for (auto &field : Fields) {
-    auto ty = field.getLoweredType();
-    // Layouts should never refer to archetypes, since they represent an
-    // abstract generic type layout.
-    assert(!ty->hasArchetype()
-           && 'SILLayout field cannot have an archetype type');
-    assert(!ty->hasTypeVariable()
-           && 'SILLayout cannot contain constraint system type variables');
-    if (!ty->hasTypeParameter())
-      continue;
-    field.getLoweredType().findIf([Sig](Type t) -> bool {
-      if (auto gpt = t->getAs<GenericTypeParamType>()) {
-        // Check that the generic param exists in the generic signature.
-        assert(Sig && 'generic param in nongeneric layout?');
-        assert(std::find(Sig.getGenericParams().begin(),
-                         Sig.getGenericParams().end(),
-                         gpt->getCanonicalType()) != Sig.getGenericParams().end()
-               && 'generic param not declared in generic signature?!');
-      }
-      return false;
-    });
+        
+    {  std::vector<OperatorDef> GetGradientDefs() override {
+    return SingleGradientDef(
+        'SubGradient',
+        '',
+        std::vector<std::string>{GO(0), I(0), I(1)},
+        std::vector<std::string>{GI(0), GI(1)});
   }
-}
-#endif
-    
-    void
-swift::trimLeadingWhitespaceFromLines(StringRef RawText,
-                                      unsigned WhitespaceToTrim,
-                                      SmallVectorImpl<StringRef> &OutLines) {
-  SmallVector<StringRef, 8> Lines;
-    }
-    
-    // Import As Member -- attempt to import C global functions and variables as
-// members on types or instances.
-    
-    
-    {    if (waiting_for_decision_) {
-      EXPECT_EQ(expect_allow_, allow);
-      if (message_loop_runner_.get())
-        message_loop_runner_->Quit();
-      callback.Run(allow);
-      return;
-    }
-    last_download_allowed_ = allow;
-    callback.Run(allow);
-  }
-    
-    void KickNextTick() {
-  v8::MicrotasksScope microtasks(v8::Isolate::GetCurrent(), v8::MicrotasksScope::kDoNotRunMicrotasks);
-  void* env = g_get_node_env_fn();
-  if (env)
-    g_call_tick_callback_fn(env);
-}
-    
-    void Base::CallSync(const std::string& method,
-                    const base::ListValue& arguments,
-                    base::ListValue* result) {
-  NOTREACHED() << 'Uncatched callAsync in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
-}
-    
-      static int getUID() {
-    static int id = 0;
-    return ++id;
-  }
-    
-       bool HasIcon(int command_id) override;
-    
-      if (menu_item->submenu_)
-    menu_model_->InsertSubMenuAt(pos, menu_item->id(), menu_item->label_,
-                                 menu_item->submenu_->menu_model_.get());
-  else if (menu_item->type_ == 'normal')
-    menu_model_->InsertItemAt(pos, menu_item->id(), menu_item->label_);
-  else if (menu_item->type_ == 'checkbox')
-    menu_model_->InsertCheckItemAt(pos, menu_item->id(), menu_item->label_);
-  else if (menu_item->type_ == 'separator')
-    menu_model_->InsertSeparatorAt(pos, ui::NORMAL_SEPARATOR);
-    
-    
-    {      }
-      keyval = gdk_keyval_from_name(key.c_str());
-    
-    NwObjCallObjectMethodAsyncFunction::~NwObjCallObjectMethodAsyncFunction() {
-}
-    
-    
-    {        return handlePrev(offset, status);
-    }
-    
-    #define LOW_A             ((UChar)0x0061)
-#define LOW_B             ((UChar)0x0062)
-#define LOW_C             ((UChar)0x0063)
-#define LOW_D             ((UChar)0x0064)
-#define LOW_E             ((UChar)0x0065)
-#define LOW_F             ((UChar)0x0066)
-#define LOW_G             ((UChar)0x0067)
-#define LOW_H             ((UChar)0x0068)
-#define LOW_I             ((UChar)0x0069)
-#define LOW_J             ((UChar)0x006a)
-#define LOW_K             ((UChar)0x006B)
-#define LOW_L             ((UChar)0x006C)
-#define LOW_M             ((UChar)0x006D)
-#define LOW_N             ((UChar)0x006E)
-#define LOW_O             ((UChar)0x006F)
-#define LOW_P             ((UChar)0x0070)
-#define LOW_Q             ((UChar)0x0071)
-#define LOW_R             ((UChar)0x0072)
-#define LOW_S             ((UChar)0x0073)
-#define LOW_T             ((UChar)0x0074)
-#define LOW_U             ((UChar)0x0075)
-#define LOW_V             ((UChar)0x0076)
-#define LOW_W             ((UChar)0x0077)
-#define LOW_X             ((UChar)0x0078)
-#define LOW_Y             ((UChar)0x0079)
-#define LOW_Z             ((UChar)0x007A)
-    
-    #ifndef __SHARED_CALENDAR_H__
-#define __SHARED_CALENDAR_H__
-    
-    
-class U_I18N_API SharedDateFormatSymbols : public SharedObject {
-public:
-    SharedDateFormatSymbols(
-            const Locale &loc, const char *type, UErrorCode &status)
-            : dfs(loc, type, status) { }
-    virtual ~SharedDateFormatSymbols();
-    const DateFormatSymbols &get() const { return dfs; }
-private:
-    DateFormatSymbols dfs;
-    SharedDateFormatSymbols(const SharedDateFormatSymbols &);
-    SharedDateFormatSymbols &operator=(const SharedDateFormatSymbols &);
 };
     
-    
-    {private:
-    int32_t fMax;
-    int32_t fMin;
-};
-    
-        case UDAT_TIMEZONE_FIELD: // 'z'
-        {
-            UTimeZoneFormatStyle style = (count < 4) ? UTZFMT_STYLE_SPECIFIC_SHORT : UTZFMT_STYLE_SPECIFIC_LONG;
-            TimeZone *tz  = tzFormat()->parse(style, text, pos, tzTimeType);
-            if (tz != NULL) {
-                cal.adoptTimeZone(tz);
-                return pos.getIndex();
-            }
-        }
-        break;
-    case UDAT_TIMEZONE_RFC_FIELD: // 'Z'
-        {
-            UTimeZoneFormatStyle style = (count < 4) ?
-                UTZFMT_STYLE_ISO_BASIC_LOCAL_FULL : ((count == 5) ? UTZFMT_STYLE_ISO_EXTENDED_FULL: UTZFMT_STYLE_LOCALIZED_GMT);
-            TimeZone *tz  = tzFormat()->parse(style, text, pos, tzTimeType);
-            if (tz != NULL) {
-                cal.adoptTimeZone(tz);
-                return pos.getIndex();
-            }
-            return -start;
-        }
-    case UDAT_TIMEZONE_GENERIC_FIELD: // 'v'
-        {
-            UTimeZoneFormatStyle style = (count < 4) ? UTZFMT_STYLE_GENERIC_SHORT : UTZFMT_STYLE_GENERIC_LONG;
-            TimeZone *tz  = tzFormat()->parse(style, text, pos, tzTimeType);
-            if (tz != NULL) {
-                cal.adoptTimeZone(tz);
-                return pos.getIndex();
-            }
-            return -start;
-        }
-    case UDAT_TIMEZONE_SPECIAL_FIELD: // 'V'
-        {
-            UTimeZoneFormatStyle style;
-            switch (count) {
-            case 1:
-                style = UTZFMT_STYLE_ZONE_ID_SHORT;
-                break;
-            case 2:
-                style = UTZFMT_STYLE_ZONE_ID;
-                break;
-            case 3:
-                style = UTZFMT_STYLE_EXEMPLAR_LOCATION;
-                break;
-            default:
-                style = UTZFMT_STYLE_GENERIC_LOCATION;
-                break;
-            }
-            TimeZone *tz  = tzFormat()->parse(style, text, pos, tzTimeType);
-            if (tz != NULL) {
-                cal.adoptTimeZone(tz);
-                return pos.getIndex();
-            }
-            return -start;
-        }
-    case UDAT_TIMEZONE_LOCALIZED_GMT_OFFSET_FIELD: // 'O'
-        {
-            UTimeZoneFormatStyle style = (count < 4) ? UTZFMT_STYLE_LOCALIZED_GMT_SHORT : UTZFMT_STYLE_LOCALIZED_GMT;
-            TimeZone *tz  = tzFormat()->parse(style, text, pos, tzTimeType);
-            if (tz != NULL) {
-                cal.adoptTimeZone(tz);
-                return pos.getIndex();
-            }
-            return -start;
-        }
-    case UDAT_TIMEZONE_ISO_FIELD: // 'X'
-        {
-            UTimeZoneFormatStyle style;
-            switch (count) {
-            case 1:
-                style = UTZFMT_STYLE_ISO_BASIC_SHORT;
-                break;
-            case 2:
-                style = UTZFMT_STYLE_ISO_BASIC_FIXED;
-                break;
-            case 3:
-                style = UTZFMT_STYLE_ISO_EXTENDED_FIXED;
-                break;
-            case 4:
-                style = UTZFMT_STYLE_ISO_BASIC_FULL;
-                break;
-            default:
-                style = UTZFMT_STYLE_ISO_EXTENDED_FULL;
-                break;
-            }
-            TimeZone *tz  = tzFormat()->parse(style, text, pos, tzTimeType);
-            if (tz != NULL) {
-                cal.adoptTimeZone(tz);
-                return pos.getIndex();
-            }
-            return -start;
-        }
-    case UDAT_TIMEZONE_ISO_LOCAL_FIELD: // 'x'
-        {
-            UTimeZoneFormatStyle style;
-            switch (count) {
-            case 1:
-                style = UTZFMT_STYLE_ISO_BASIC_LOCAL_SHORT;
-                break;
-            case 2:
-                style = UTZFMT_STYLE_ISO_BASIC_LOCAL_FIXED;
-                break;
-            case 3:
-                style = UTZFMT_STYLE_ISO_EXTENDED_LOCAL_FIXED;
-                break;
-            case 4:
-                style = UTZFMT_STYLE_ISO_BASIC_LOCAL_FULL;
-                break;
-            default:
-                style = UTZFMT_STYLE_ISO_EXTENDED_LOCAL_FULL;
-                break;
-            }
-            TimeZone *tz  = tzFormat()->parse(style, text, pos, tzTimeType);
-            if (tz != NULL) {
-                cal.adoptTimeZone(tz);
-                return pos.getIndex();
-            }
-            return -start;
-        }
-    // currently no pattern character is defined for UDAT_TIME_SEPARATOR_FIELD
-    // so we should not get here. Leave support in for future definition.
-    case UDAT_TIME_SEPARATOR_FIELD:
-        {
-            static const UChar def_sep = DateFormatSymbols::DEFAULT_TIME_SEPARATOR;
-            static const UChar alt_sep = DateFormatSymbols::ALTERNATE_TIME_SEPARATOR;
-    }
-    
-    ExitConstrDeleteAll: // Remove all sets and return error
-    delete fDateIgnorables;  fDateIgnorables = NULL;
-    delete fTimeIgnorables;  fTimeIgnorables = NULL;
-    delete fOtherIgnorables; fOtherIgnorables = NULL;
-    
-    CollationKey::~CollationKey()
-{
-    if(fFlagAndLength < 0) { uprv_free(fUnion.fFields.fBytes); }
+    template <>
+template <typename T>
+bool EnforceFiniteOp<CPUContext>::DoRunWithType() {
+  EnforceOnCPU<T>(Input(0));
+  return true;
 }
     
-    class TransliterationRuleData;
-    
-            // The temporary buffer starts at tempStart, and extends
-        // to destLimit.  The start of the buffer has a single
-        // character from before the key.  This provides style
-        // data when addition characters are filled into the
-        // temporary buffer.  If there is nothing to the left, use
-        // the non-character U+FFFF, which Replaceable subclasses
-        // should treat specially as a 'no-style character.'
-        // destStart points to the point after the style context
-        // character, so it is tempStart+1 or tempStart+2.
-        int32_t tempStart = text.length(); // start of temp buffer
-        int32_t destStart = tempStart; // copy new text to here
-        if (start > 0) {
-            int32_t len = U16_LENGTH(text.char32At(start-1));
-            text.copy(start-len, start, tempStart);
-            destStart += len;
-        } else {
-            UnicodeString str((UChar) 0xFFFF);
-            text.handleReplaceBetween(tempStart, tempStart, str);
-            destStart++;
-        }
-        int32_t destLimit = destStart;
-    
-      bool operator < (const Extension_call_args & ) const;
-    
-    int main(int argc, char **argv) {
-  int port = 9090;
-  ::apache::thrift::stdcxx::shared_ptr<ExtensionManagerHandler> handler(new ExtensionManagerHandler());
-  ::apache::thrift::stdcxx::shared_ptr<TProcessor> processor(new ExtensionManagerProcessor(handler));
-  ::apache::thrift::stdcxx::shared_ptr<TServerTransport> serverTransport(new TServerSocket(port));
-  ::apache::thrift::stdcxx::shared_ptr<TTransportFactory> transportFactory(new TBufferedTransportFactory());
-  ::apache::thrift::stdcxx::shared_ptr<TProtocolFactory> protocolFactory(new TBinaryProtocolFactory());
+        for (int inputIdx = 0; inputIdx < def_.input_size() / kNumTensorsPerInput;
+         ++inputIdx) {
+      input_blob_names.push_back(I(inputIdx * kNumTensorsPerInput));
+      output_blob_names.push_back(GI(inputIdx * kNumTensorsPerInput + 2));
     }
+    input_blob_names.push_back(GO(2));
     
-      xfer += oprot->writeFieldBegin('name', ::apache::thrift::protocol::T_STRING, 1);
-  xfer += oprot->writeString(this->name);
-  xfer += oprot->writeFieldEnd();
+    Example 2 (with KEY):
+DATA  = [1, 2, 3, 4, 5, 6, 7, 8]
+KEY   = [0, 1, 3, 2, 1, 0, 1, 0]
+RANGES = [
+  [
+    [2, 4],
+    [0, 2],
+  ],
+  [
+    [0, 0],
+    [6, 2],
+  ]
+]
+lengths = [4, 2]
+OUTPUT[0] = [[6, 5, 4, 3], [0, 0, 0, 0]]
+OUTPUT[1] = [[1, 2], [8, 7]]
     
-    typedef struct _InternalExtensionInfo__isset {
-  _InternalExtensionInfo__isset() : name(false), version(false), sdk_version(false), min_sdk_version(false) {}
-  bool name :1;
-  bool version :1;
-  bool sdk_version :1;
-  bool min_sdk_version :1;
-} _InternalExtensionInfo__isset;
+    workspace.RunOperatorOnce(op)
+print('Out:\n', workspace.FetchBlob('out'))
     
-    /**
- * @brief Represents a hardware driver type that SMART api can you use to query
- * device information.
- *
- * @param driver name of SMART controller driver
- * @param maxID max ID number of which disks on the controller is monitored
- */
-struct hardwareDriver {
-  std::string driver;
-  size_t maxID;
-};
+    REGISTER_CUDA_OPERATOR(LC2D, LocallyConnectedOp<float, CUDAContext>);
+REGISTER_CUDA_OPERATOR(
+    LC2DGradient,
+    LocallyConnectedGradientOp<float, CUDAContext>);
     
-    #include <osquery/core.h>
-#include <osquery/database.h>
-#include <osquery/filesystem/filesystem.h>
-#include <osquery/logger.h>
-#include <osquery/tables.h>
-    
-    Status logProcessUptime(IDebugSystemObjects2* system, Row& r) {
-  unsigned long uptime = 0;
-  if (system->GetCurrentProcessUpTime(&uptime) == S_OK) {
-    r['process_uptime'] = BIGINT(uptime);
-    return Status();
-  }
-  return Status(1);
-}
-    
-      // Join results..
-  for (auto& entry : hw_info) {
-    bool matched = false;
-    for (auto& row : results) {
-      auto serial = row.find('serial_number');
-      if (serial == row.end()) {
-        continue;
-      }
-    }
-    }
-    
-    
+        // out_of_range.403
+    try
     {
-    {struct mei_version {
-  uint32_t important_details[7];
-  uint16_t major;
-  uint16_t minor;
-  uint16_t hotfix;
-  uint16_t build;
-  uint16_t r_major;
-  uint16_t r_minor;
-  uint16_t r_hotfix;
-  uint16_t r_build;
-  uint16_t codes[6];
-};
-}
-}
-
-    
-    
+        // try to use a JSON pointer to an nonexistent object key
+        json::const_reference ref = j.at('/foo'_json_pointer);
+    }
+    catch (json::out_of_range& e)
     {
-    {} // namespace perf_event_open
-} // namespace osquery
-
+        std::cout << e.what() << '\n';
+    }
     
-    
-    {
-    {} // namespace perf_event_open
-} // namespace osquery
+        // print values
+    std::cout << object << '\n';
+    std::cout << *res1.first << ' ' << std::boolalpha << res1.second << '\n';
