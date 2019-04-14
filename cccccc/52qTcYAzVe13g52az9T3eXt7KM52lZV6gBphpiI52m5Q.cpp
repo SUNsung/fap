16d263690373fadecec20a764835741a0640f609
SUNsung <weatherius@gmail.com>
2019-04-14 17:58:17 +0300

    Rewrite [52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp] File in: 2019-04-14 17:58:16

--- cccccc/52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp
+++ cccccc/52qTcYAzVe13g52az9T3eXt7KM52lZV6gBphpiI52m5Q.cpp
@@ -1,129 +1,356 @@
 
         
-        std::string tmppath() {
-  // TMPFILE is for manual test execution during which the user will specify
-  // the full temp file path using the environmental variable TMPFILE
-  const char* tmpfile = getenv('TMPFILE');
-  if (tmpfile) {
-    return std::string(tmpfile);
+        #include 'tensorflow/core/framework/op.h'
+    
+      for (const auto& node : item_.MainOpsFanin()) {
+    PrintNodeInfo(node, properties, debug, os);
   }
-    }
+  for (const auto& node : item_.EnqueueOpsFanin()) {
+    PrintNodeInfo(node, properties, debug, os);
+  }
     
-        for (int inputIdx = 0; inputIdx < def_.input_size() / kNumTensorsPerInput;
-         ++inputIdx) {
-      input_blob_names.push_back(I(inputIdx * kNumTensorsPerInput));
-      input_blob_names.push_back(I(inputIdx * kNumTensorsPerInput + 2));
-      output_blob_names.push_back(GI(inputIdx * kNumTensorsPerInput + 4));
-    }
-    input_blob_names.push_back(GO(4));
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
+#define TENSORFLOW_PYTHON_LIB_CORE_PY_EXCEPTION_REGISTRY_H_
     
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-          typename T6, typename T7, typename T8, typename T9, typename T10>
-void PrintTo(
-    const ::std::tr1::tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>& t,
-    ::std::ostream* os) {
-  PrintTupleTo(t, os);
-}
-#endif  // GTEST_HAS_TR1_TUPLE
-    
-    // Helper function for implementing {EXPECT|ASSERT}_PRED2.  Don't use
-// this in your code.
-template <typename Pred,
-          typename T1,
-          typename T2>
-AssertionResult AssertPred2Helper(const char* pred_text,
-                                  const char* e1,
-                                  const char* e2,
-                                  Pred pred,
-                                  const T1& v1,
-                                  const T2& v2) {
-  if (pred(v1, v2)) return AssertionSuccess();
-    }
-    
-     private:
-  // Replaces multiple consecutive separators with a single separator.
-  // For example, 'bar///foo' becomes 'bar/foo'. Does not eliminate other
-  // redundancies that might be in a pathname involving '.' or '..'.
-  //
-  // A pathname with multiple consecutive separators may occur either through
-  // user error or as a result of some scripts or APIs that generate a pathname
-  // with a trailing separator. On other platforms the same API or script
-  // may NOT generate a pathname with a trailing '/'. Then elsewhere that
-  // pathname may have another '/' and pathname components added to it,
-  // without checking for the separator already being there.
-  // The script language and operating system may allow paths like 'foo//bar'
-  // but some of the functions in FilePath will not handle that correctly. In
-  // particular, RemoveTrailingPathSeparator() only removes one separator, and
-  // it is called in CreateDirectoriesRecursively() assuming that it will change
-  // a pathname from directory syntax (trailing separator) to filename syntax.
-  //
-  // On Windows this method also replaces the alternate path separator '/' with
-  // the primary path separator '\\', so that for example 'bar\\/\\foo' becomes
-  // 'bar\\foo'.
-    
-    // A simple Linear Congruential Generator for generating random
-// numbers with a uniform distribution.  Unlike rand() and srand(), it
-// doesn't use global state (and therefore can't interfere with user
-// code).  Unlike rand_r(), it's portable.  An LCG isn't very random,
-// but it's good enough for our purposes.
-class GTEST_API_ Random {
+    // A wrapper around io::RecordReader that is more easily SWIG wrapped for
+// Python.  An instance of this class is not safe for concurrent access
+// by multiple threads.
+class PyRecordReader {
  public:
-  static const UInt32 kMaxRange = 1u << 31;
+  // TODO(vrv): make this take a shared proto to configure
+  // the compression options.
+  static PyRecordReader* New(const string& filename, uint64 start_offset,
+                             const string& compression_type_string,
+                             TF_Status* out_status);
+    }
+    
+      const tensorflow::OpRegistrationData* op_reg_data;
+  auto status =
+      tensorflow::OpRegistry::Global()->LookUp(node_def.op(), &op_reg_data);
+  if (!status.ok()) {
+    LOG(WARNING) << 'Op ' << node_def.op() << ' not found: ' << status;
+    return '';
+  }
+  AddDefaultsToNodeDef(op_reg_data->op_def, &node_def);
+    
+    bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv) {
+  const base::CommandLine::StringType dashdash(2, '-');
+  bool block_args = false;
+  for (int i = 0; i < argc; ++i) {
+    if (argv[i] == dashdash)
+      break;
+    if (block_args) {
+      return false;
+    } else if (IsUrlArg(argv[i])) {
+      block_args = true;
+    }
+  }
+  return true;
+}
+    
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
+    
+      bool IsDestroyed() {
+    v8::Local<v8::Object> wrapper = Wrappable<T>::GetWrapper();
+    return wrapper->InternalFieldCount() == 0 ||
+           wrapper->GetAlignedPointerFromInternalField(0) == nullptr;
+  }
+    
+    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
+ public:
+  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
+  ~AtomJavaScriptDialogManager() override;
+    }
+    
+    #include <map>
+#include <string>
+    
+    #if defined(OS_LINUX)
+typedef PowerObserverLinux PowerObserver;
+#else
+typedef base::PowerObserver PowerObserver;
+#endif  // defined(OS_LINUX)
+    
+    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
+constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
+constexpr size_t TraceContextEncoding::kVersionIdSize;
+constexpr size_t TraceContextEncoding::kFieldIdSize;
+constexpr size_t TraceContextEncoding::kVersionIdOffset;
+constexpr size_t TraceContextEncoding::kVersionId;
+    
+    
+    {    size_t pos = kVersionIdSize;
+    while (pos < buf.size()) {
+      size_t bytes_read =
+          ParseField(absl::string_view(&buf[pos], buf.size() - pos), tc);
+      if (bytes_read == 0) {
+        break;
+      } else {
+        pos += bytes_read;
+      }
+    }
+    return pos;
+  }
+    
+      Status GetFileByName(ServerContext* context, const grpc::string& file_name,
+                       reflection::v1alpha::ServerReflectionResponse* response);
+    
+    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
+    const grpc::string& name, const grpc::string& value) {
+  class StringOption final : public ServerBuilderOption {
+   public:
+    StringOption(const grpc::string& name, const grpc::string& value)
+        : name_(name), value_(value) {}
+    }
     }
     
     
-    { private:
-  String();  // Not meant to be instantiated.
-};  // class String
+    {}  // namespace grpc
+
     
-      tuple(const tuple& t) : f0_(t.f0_), f1_(t.f1_), f2_(t.f2_), f3_(t.f3_),
-      f4_(t.f4_), f5_(t.f5_), f6_(t.f6_), f7_(t.f7_), f8_(t.f8_), f9_(t.f9_) {}
+    namespace leveldb {
+    }
     
-      // Copy c'tor
-  MyString(const MyString& string) : c_string_(NULL) {
-    Set(string.c_string_);
+      std::string fname = TableFileName(dbname, meta->number);
+  if (iter->Valid()) {
+    WritableFile* file;
+    s = env->NewWritableFile(fname, &file);
+    if (!s.ok()) {
+      return s;
+    }
+    }
+    
+    #endif  // STORAGE_LEVELDB_DB_BUILDER_H_
+
+    
+    #include <ctype.h>
+#include <stdio.h>
+#include 'db/filename.h'
+#include 'db/dbformat.h'
+#include 'leveldb/env.h'
+#include 'util/logging.h'
+    
+    
+    {
+    {}  // namespace
+}  // namespace leveldb
+    
+    Reader::Reader(SequentialFile* file, Reporter* reporter, bool checksum,
+               uint64_t initial_offset)
+    : file_(file),
+      reporter_(reporter),
+      checksum_(checksum),
+      backing_store_(new char[kBlockSize]),
+      buffer_(),
+      eof_(false),
+      last_record_offset_(0),
+      end_of_buffer_offset_(0),
+      initial_offset_(initial_offset),
+      resyncing_(initial_offset > 0) {
+}
+    
+    #ifndef STORAGE_LEVELDB_DB_LOG_READER_H_
+#define STORAGE_LEVELDB_DB_LOG_READER_H_
+    
+      ~LogTest() {
+    delete writer_;
+    delete reader_;
   }
     
-      // Load the options file.
-  DBOptions loaded_db_opt;
-  std::vector<ColumnFamilyDescriptor> loaded_cf_descs;
-  s = LoadLatestOptions(kDBPath, Env::Default(), &loaded_db_opt,
-                        &loaded_cf_descs);
-  assert(s.ok());
-  assert(loaded_db_opt.create_if_missing == db_opt.create_if_missing);
+      void EncodeTo(std::string* dst) const;
+  Status DecodeFrom(const Slice& src);
     
-    Status GetOptionsFromString(const Options& base_options,
-                            const std::string& opts_str, Options* new_options);
-    
-    #pragma once
-#ifndef ROCKSDB_LITE
-    
-      // Insert to page cache
-  //
-  // page_key   Identifier to identify a page uniquely across restarts
-  // data       Page data
-  // size       Size of the page
-  virtual Status Insert(const Slice& key, const char* data,
-                        const size_t size) = 0;
+    // WriteBatchInternal provides static methods for manipulating a
+// WriteBatch that we don't want in the public WriteBatch interface.
+class WriteBatchInternal {
+ public:
+  // Return the number of entries in the batch.
+  static int Count(const WriteBatch* batch);
+    }
     
     
-    { private:
-  DB* db_;
-  const Snapshot* snapshot_;
+    {    return;
+}
+    
+    public:
+    vector<int> inorderTraversal(TreeNode* root) {
+    }
+    
+    
+    {    return 0;
+}
+
+    
+    
+    {                if(node->left){
+                    q.push(node->left);
+                    new_level_num ++;
+                }
+                if(node->right){
+                    q.push(node->right);
+                    new_level_num ++;
+                }
+            }
+    
+    
+    {
+    {
+    {            if(command.s == 'print')
+                res.push_back(command.node->val);
+            else{
+                assert(command.s == 'go');
+                if(command.node->right)
+                    stack.push(Command('go',command.node->right));
+                if(command.node->left)
+                    stack.push(Command('go',command.node->left));
+                stack.push(Command('print', command.node));
+            }
+        }
+        return res;
+    }
 };
     
-      // Builds an openable snapshot of RocksDB on the same disk, which
-  // accepts an output directory on the same disk, and under the directory
-  // (1) hard-linked SST files pointing to existing live SST files
-  // SST files will be copied if output directory is on a different filesystem
-  // (2) a copied manifest files and other files
-  // The directory should not already exist and will be created by this API.
-  // The directory will be an absolute path
-  // log_size_for_flush: if the total log file size is equal or larger than
-  // this value, then a flush is triggered for all the column families. The
-  // default value is 0, which means flush is always triggered. If you move
-  // away from the default, the checkpoint may not contain up-to-date data
-  // if WAL writing is not always enabled.
-  // Flush will always trigger if it is 2PC.
-  virtual Status CreateCheckpoint(const std::string& checkpoint_dir,
-                                  uint64_t log_size_for_flush = 0);
+    public:
+    vector<int> preorderTraversal(TreeNode* root) {
+    }
+    
+            vector<int> res;
+        if(root == NULL)
+            return res;
+    
+    
+    {        vector<int> res;
+        __postorderTraversal(root, res);
+        return res;
+    }
+    
+    
+    {
+    {
+    {            if(command.s == 'print')
+                res.push_back(command.node->val);
+            else{
+                assert(command.s == 'go');
+                stack.push(Command('print', command.node));
+                if(command.node->right)
+                    stack.push(Command('go',command.node->right));
+                if(command.node->left)
+                    stack.push(Command('go',command.node->left));
+            }
+        }
+        return res;
+    }
+};
+    
+    struct APCCollection {
+  static APCHandle::Pair Make(const ObjectData*,
+                              APCHandleLevel level,
+                              bool unserializeObj);
+  static void Delete(APCHandle*);
+    }
+    
+    namespace HPHP {
+///////////////////////////////////////////////////////////////////////////////
+struct Variant;
+struct IniSettingMap;
+typedef std::map<std::string, std::string> ConfigMap;
+typedef std::set<std::string> ConfigSet;
+// with comparer
+typedef std::set<std::string, stdltistr> ConfigSetC;
+typedef std::map<std::string, std::string, stdltistr> ConfigMapC;
+typedef boost::container::flat_set<std::string> ConfigFlatSet;
+typedef hphp_string_imap<std::string> ConfigIMap;
+    }
+    
+    #include <memory>
+    
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
+    
+        if (row_indices_.empty()) {  // edge case: empty instance set
+      // assign arbitrary address here, to bypass nullptr check
+      // (nullptr usually indicates a nonexistent rowset, but we want to
+      //  indicate a valid rowset that happens to have zero length and occupies
+      //  the whole instance set)
+      // this is okay, as BuildHist will compute (end-begin) as the set size
+      const size_t* begin = reinterpret_cast<size_t*>(20);
+      const size_t* end = begin;
+      elem_of_each_node_.emplace_back(Elem(begin, end, 0));
+      return;
+    }
+    
+    namespace xgboost {
+/*!
+ * \brief interface of gradient boosting model.
+ */
+class GradientBooster {
+ public:
+  /*! \brief virtual destructor */
+  virtual ~GradientBooster() = default;
+  /*!
+   * \brief set configuration from pair iterators.
+   * \param begin The beginning iterator.
+   * \param end The end iterator.
+   * \tparam PairIter iterator<std::pair<std::string, std::string> >
+   */
+  template<typename PairIter>
+  inline void Configure(PairIter begin, PairIter end);
+  /*!
+   * \brief Set the configuration of gradient boosting.
+   *  User must call configure once before InitModel and Training.
+   *
+   * \param cfg configurations on both training and model parameters.
+   */
+  virtual void Configure(const std::vector<std::pair<std::string, std::string> >& cfg) = 0;
+  /*!
+   * \brief load model from stream
+   * \param fi input stream.
+   */
+  virtual void Load(dmlc::Stream* fi) = 0;
+  /*!
+   * \brief save model to stream.
+   * \param fo output stream
+   */
+  virtual void Save(dmlc::Stream* fo) const = 0;
+  /*!
+   * \brief whether the model allow lazy checkpoint
+   * return true if model is only updated in DoBoost
+   * after all Allreduce calls
+   */
+  virtual bool AllowLazyCheckPoint() const {
+    return false;
+  }
+  /*!
+   * \brief perform update to the model(boosting)
+   * \param p_fmat feature matrix that provide access to features
+   * \param in_gpair address of the gradient pair statistics of the data
+   * \param obj The objective function, optional, can be nullptr when use customized version
+   * the booster may change content of gpair
+   */
+  virtual void DoBoost(DMatrix* p_fmat,
+                       HostDeviceVector<GradientPair>* in_gpair,
+                       ObjFunction* obj = nullptr) = 0;
+    }
+    }
+    
+    XGBOOST_REGISTER_SPARSE_PAGE_FORMAT(lz4hc)
+.describe('Apply LZ4 binary data compression(high compression ratio) for ext memory.')
+.set_body([]() {
+    return new SparsePageLZ4Format<bst_uint>(true);
+  });
+    
+    void SimpleCSRSource::CopyFrom(DMatrix* src) {
+  this->Clear();
+  this->info = src->Info();
+  for (const auto &batch : src->GetRowBatches()) {
+    page_.Push(batch);
+  }
+}
