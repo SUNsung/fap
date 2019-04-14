278a41d8ac11792811f8552f29ce9a16a6ec3b34
SUNsung <weatherius@gmail.com>
2019-04-14 09:45:59 +0300

    Rewrite [35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py] File in: 2019-04-14 09:45:58

--- pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
+++ pyton/35ingT3M8Z.35wnoMbF08yc35tqPXAfYQPqI35Ikg.py
@@ -1,97 +1,65 @@
 
         
-        def main():
-    # First, we load the current README into memory as an array of lines
-    with open('README.md', 'r') as read_me_file:
-        read_me = read_me_file.readlines()
+        def generateLargePrime(keysize = 1024):
+    while True:
+        num = random.randrange(2 ** (keysize - 1), 2 ** (keysize))
+        if isPrime(num):
+            return num
     
-        global pteredor_is_running
-    pteredor_is_running = True
-    server_list = prober.eval_servers()
-    pteredor_is_running = False
+            while self.values[new_key] is not None and self.values[new_key] != key:
+            new_key = self.__hash_double_function(key, data, i) if \
+                self.balanced_factor() >= self.lim_charge else None
+            if new_key is None: break 
+            else: i += 1
     
-        Because operations never actually alter the buffer, you may always get
-    the original token stream back without undoing anything.  Since
-    the instructions are queued up, you can easily simulate transactions and
-    roll back any changes if there is an error just by removing instructions.
-    For example,
+    def kmeans(data, k, initial_centroids, maxiter=500, record_heterogeneity=None, verbose=False):
+    '''This function runs k-means on given data and initial set of centroids.
+       maxiter: maximum number of iterations to run.(default=500)
+       record_heterogeneity: (optional) a list, to store the history of heterogeneity as function of iterations
+                             if None, do not store the history.
+       verbose: if True, print how many data points changed their cluster labels in each iteration'''
+    centroids = initial_centroids[:]
+    prev_cluster_assignment = None
     
-            Lines are numbered 1..n
+    for itr in range(maxiter):        
+        if verbose:
+            print(itr, end='')
         
-        Using setter/getter methods is deprecated. Use o.line instead.'''
+        # 1. Make cluster assignments using nearest centroids
+        cluster_assignment = assign_clusters(data,centroids)
+            
+        # 2. Compute a new centroid for each of the k clusters, averaging all data points assigned to that cluster.
+        centroids = revise_centroids(data,k, cluster_assignment)
+            
+        # Check for convergence: if none of the assignments changed, stop
+        if prev_cluster_assignment is not None and \
+          (prev_cluster_assignment==cluster_assignment).all():
+            break
+        
+        # Print number of new assignments 
+        if prev_cluster_assignment is not None:
+            num_changed = np.sum(prev_cluster_assignment!=cluster_assignment)
+            if verbose:
+                print('    {0:5d} elements changed their cluster assignment.'.format(num_changed))   
+        
+        # Record heterogeneity convergence metric
+        if record_heterogeneity is not None:
+            # YOUR CODE HERE
+            score = compute_heterogeneity(data,k,centroids,cluster_assignment)
+            record_heterogeneity.append(score)
+        
+        prev_cluster_assignment = cluster_assignment[:]
+        
+    return centroids, cluster_assignment
     
     
-ALL_SSL_OPTIONS_HASHES = [
-    '0f81093a1465e3d4eaa8b0c14e77b2a2e93568b0fc1351c2b87893a95f0de87c',
-    '9a7b32c49001fed4cff8ad24353329472a50e86ade1ef9b2b9e43566a619612e',
-    'a6d9f1c7d6b36749b52ba061fff1421f9a0a3d2cfdafbd63c05d06f65b990937',
-    '7f95624dd95cf5afc708b9f967ee83a24b8025dc7c8d9df2b556bbc64256b3ff',
-    '394732f2bbe3e5e637c3fb5c6e980a1f1b90b01e2e8d6b7cff41dde16e2a756d',
-    '4b16fec2bcbcd8a2f3296d886f17f9953ffdcc0af54582452ca1e52f5f776f16',
-]
-'''SHA256 hashes of the contents of all versions of MOD_SSL_CONF_SRC'''
+def b_expo_mod(a, b, c):
+    res = 0
+    while b > 0:
+        if b&1:
+            res = ((res%c) + (a%c)) % c
     
-    # Language to be used for generating the HTML full-text search index.
-# Sphinx supports the following languages:
-#   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
-#   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr'
-#html_search_language = 'en'
-    
-    REWRITE_HTTPS_ARGS = [
-    '^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[L,NE,R=permanent]']
-'''Apache version<2.3.9 rewrite rule arguments used for redirections to
-https vhost'''
-    
-            self.assertEqual(None, self._call(self.vhosts))
-    
-        def test_nonexistent_generic(self):
-        with mock.patch('certbot.util.get_os_info') as mock_info:
-            mock_info.return_value = ('nonexistent', 'irrelevant')
-            with mock.patch('certbot.util.get_systemd_os_like') as mock_like:
-                mock_like.return_value = ['unknonwn']
-                self.assertEqual(entrypoint.get_configurator(),
-                                 configurator.ApacheConfigurator)
-    
-    import mock
-    
-        def __init__(self, parent):
-        ttk.Frame.__init__(self, parent)
-        self.pack(side=tk.BOTTOM, padx=10, pady=2, fill=tk.X, expand=False)
-    
-        def convert_tabs(self):
-        ''' Build the convert tabs
-            Currently identical to Extract, so just call that '''
-        logger.debug('Build convert tabs')
-        self.extract_tabs()
-        logger.debug('Built convert tabs')
-    
-    
-class SessionGraph(GraphBase):  # pylint: disable=too-many-ancestors
-    ''' Session Graph for session pop-up '''
-    def __init__(self, parent, data, ylabel, scale):
-        GraphBase.__init__(self, parent, data, ylabel)
-        self.scale = scale
-    
-                mouse_x, mouse_y = widget.winfo_pointerxy()
-    
-        Parameters:
-        initializer: initializer used for sub kernels (orthogonal, glorot uniform, etc.)
-        scale: scale factor of sub pixel convolution (upsampling from 8x8 to 16x16 is scale 2)
-    Return:
-        The modified kernel weights
-    Example:
-        x = conv2d(... weights_initializer=ICNR(initializer=he_uniform(), scale=2))
-    '''
-    
-            input_shape = K.int_shape(inputs)
-        if len(input_shape) != 4:
-            raise ValueError('Inputs should have rank ' +
-                             str(4) +
-                             '; Received input shape:', str(input_shape))
-    
-        def debug_monitor(self, update_secs=2):
-        ''' Debug tool for monitoring queues '''
-        thread = threading.Thread(target=self.debug_queue_sizes,
-                                  args=(update_secs, ))
-        thread.daemon = True
-        thread.start()
+        with tf.variable_scope(name, reuse=reuse):
+        alpha = get_w(alpha_shape, w_initializer=alpha_init, name='alpha')
+        # o = relu(x) + 0.5 * tf.multiply(alpha, x - tf.abs(x))  # TFLearn
+        o = leaky_relu(x, alpha)  # TensorLayer / <Deep Learning>
