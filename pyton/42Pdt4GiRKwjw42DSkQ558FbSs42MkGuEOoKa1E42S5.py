c570fb470983e65dff2c9622c21ea9f4f58d94f8
SUNsung <weatherius@gmail.com>
2019-04-14 14:14:29 +0300

    Rewrite [42Pdt4GiRKwjw42DSkQ558FbSs42MkGuEOoKa1E42S5.py] File in: 2019-04-14 14:14:29

--- pyton/42Pdt4GiRKwjw42DSkQ558FbSs42MkGuEOoKa1E42S5.py
+++ pyton/42Pdt4GiRKwjw42DSkQ558FbSs42MkGuEOoKa1E42S5.py
@@ -1,145 +1,108 @@
 
         
-            def describe(self):
-        '''Returns the existing details of the rule in AWS'''
+        from .compat import is_py2, builtin_str, str
+    
+        def _close_server_sock_ignore_errors(self):
         try:
-            rule_info = self.client.describe_rule(Name=self.name)
-        except botocore.exceptions.ClientError as e:
-            error_code = e.response.get('Error', {}).get('Code')
-            if error_code == 'ResourceNotFoundException':
-                return {}
-            self.module.fail_json_aws(e, msg='Could not describe rule %s' % self.name)
-        except botocore.exceptions.BotoCoreError as e:
-            self.module.fail_json_aws(e, msg='Could not describe rule %s' % self.name)
-        return self._snakify(rule_info)
+            self.server_sock.close()
+        except IOError:
+            pass
     
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.ec2 import boto3_conn, get_aws_connection_info, ec2_argument_spec, camel_dict_to_snake_dict
-    
-        :returns True on success, raises ValueError on type error.
-    :rtype ``bool``
     '''
-    errmsg = ''
-    if not isinstance(instance.node_count, int):
-        errmsg = 'node_count must be an integer %s (%s)' % (
-            instance.node_count, type(instance.node_count))
-    if instance.display_name and not isinstance(instance.display_name,
-                                                string_types):
-        errmsg = 'instance_display_name must be an string %s (%s)' % (
-            instance.display_name, type(instance.display_name))
-    if errmsg:
-        raise ValueError(errmsg)
+Requests HTTP Library
+~~~~~~~~~~~~~~~~~~~~~
     
-        def dnszone_find(self, zone_name):
-        return self._post_json(
-            method='dnszone_find',
-            name=zone_name,
-            item={'idnsname': zone_name}
-        )
+        builtin_str = str
+    str = str
+    bytes = bytes
+    basestring = (str, bytes)
+    numeric_types = (int, float)
+    integer_types = (int,)
+
     
-    # Ensure rule is absent
-- ipa_hbacrule:
-    name: rule_to_be_deleted
-    state: absent
-    ipa_host: ipa.example.com
-    ipa_user: admin
-    ipa_pass: topsecret
+    :copyright: (c) 2012 by Kenneth Reitz.
+:license: Apache2, see LICENSE for more details.
 '''
     
-        module = AnsibleModule(
-        argument_spec=dict(
-            token=dict(required=True, no_log=True),
-            environment=dict(required=True),
-            user=dict(required=False),
-            repo=dict(required=False),
-            revision=dict(required=False),
-            url=dict(required=False, default='https://api.airbrake.io/deploys.txt'),
-            validate_certs=dict(default='yes', type='bool'),
-        ),
-        supports_check_mode=True
-    )
-    
-                if re.search('already enabled', out) is None:
-                change_applied = True
-        else:
-            if rc == 0:
-                change_applied = True
-            # RC is not 0 for this already disabled feature, handle it as no change applied
-            elif re.search('Cannot disable feature '%s'. Target file .* does not exist' % self.feature_name, out):
-                change_applied = False
-            else:
-                self.module.fail_json(msg='Failed to disable feature. Command returns %s' % out)
-    
-        def get_ip(self):
+        def reset_appid(self):
+        # called by web_control
         with self.lock:
-            while True:
-                line = self.in_fd.readline()
-                if not line:
-                    raise Exception()
+            self.working_appid_list = list()
+            for appid in self.config.GAE_APPIDS:
+                if not appid:
+                    self.config.GAE_APPIDS.remove(appid)
+                    continue
+                self.working_appid_list.append(appid)
+            self.not_exist_appids = []
+            self.out_of_quota_appids = []
+        self.last_reset_time = time.time()
     
-        def get_stat(self):
-        return self.network_stat
+            return ret == 1
     
-            raise NotImplementedError
+    A tree.RewriteCardinalityException is raised, when the parsers hits a
+cardinality mismatch during AST construction. Although this is basically a
+bug in your grammar, it can only be detected at runtime.
     
+    MIN_TOKEN_TYPE = UP+1
+	
+INVALID_TOKEN_TYPE = 0
     
-    def getLine(self):
-        '''@brief Get the line number on which this token was matched
+        def test_default_decoder_raises_deserialization_error(self):
+        from acme.fields import RFC3339Field
+        self.assertRaises(
+            jose.DeserializationError, RFC3339Field.default_decoder, '')
     
-            if not version.is_prerelease or version.release[2] != 0:
-            to_change['release'] = _bump_release(version.release, 'minor')
+        # TODO: decoder/encoder should accept cls? Otherwise, subclassing
+    # JSONObjectWithFields is tricky...
+    header_cls = Header
+    header = jose.Field(
+        'header', omitempty=True, default=header_cls(),
+        decoder=header_cls.from_json)
     
+            self.assertRaises(
+            jose.DeserializationError, nonce_field.decode, self.wrong_nonce)
+        self.assertEqual(b'foo', nonce_field.decode(self.good_nonce))
     
-@asyncio.coroutine
-def test_if_fires_on_hass_start(hass):
-    '''Test the firing when HASS starts.'''
-    calls = async_mock_service(hass, 'test', 'automation')
-    hass.state = CoreState.not_running
-    config = {
-        automation.DOMAIN: {
-            'alias': 'hello',
-            'trigger': {
-                'platform': 'homeassistant',
-                'event': 'start',
-            },
-            'action': {
-                'service': 'test.automation',
-            }
-        }
-    }
+    # The name for this set of Sphinx documents.  If None, it defaults to
+# '<project> v<release> documentation'.
+#html_title = None
     
-        rm1.state = 'Idle'
-    rm2.state = 'Running'
+    OLD_REWRITE_HTTPS_ARGS = [
+    ['^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[L,QSA,R=permanent]'],
+    ['^', 'https://%{SERVER_NAME}%{REQUEST_URI}', '[END,QSA,R=permanent]']]
     
-    ### OUTPUT ###
-# Name: Jhon    Occupation: Coder
-# Before we access `relatives`:
-# {'call_count2': 0, 'name': 'Jhon', 'occupation': 'Coder'}
-# Jhon's relatives: Many relatives.
-# After we've accessed `relatives`:
-# {'relatives': 'Many relatives.', 'call_count2': 0, 'name': 'Jhon', 'occupation': 'Coder'}
-# Father and mother
-# {'_lazy__parents': 'Father and mother', 'relatives': 'Many relatives.', 'call_count2': 1, 'name': 'Jhon', 'occupation': 'Coder'}  # noqa flake8
-# Father and mother
-# 1
+        The roulette wheel has 37 spots. If the bet is 0 and a 0 comes up,
+    you win a reward of 35. If the parity of your bet matches the parity
+    of the spin, you win 1. Otherwise you receive a reward of -1.
+    
+            def __del__(self):
+            self.close()
+    ```
+    
+    human_agent_action = 0
+human_wants_restart = False
+human_sets_pause = False
+    
+        env = gym.make(args.env_id)
+    
+        for c in cs:
+        yield globals()[c]
 
     
+        def visit(n):
+        if n['name'] in temporary_marked:
+            if n['name'] in get_service_names(n.get('links', [])):
+                raise DependencyError('A service can not link to itself: %s' % n['name'])
+            if n['name'] in n.get('volumes_from', []):
+                raise DependencyError('A service can not mount itself as volume: %s' % n['name'])
+            if n['name'] in n.get('depends_on', []):
+                raise DependencyError('A service can not depend on itself: %s' % n['name'])
+            raise DependencyError('Circular dependency between %s' % ' and '.join(temporary_marked))
     
-class SalesManager:
-    def talk(self):
-        print('Sales Manager ready to talk')
-    
-        def test_data_name_shall_be_changeable(cls):
-        cls.sub.name = 'New Data Name'
-        cls.assertEqual(cls.sub.name, 'New Data Name')
-
-    
-        def test_pool_behavior_with_single_object_inside(self):
-        sample_queue = queue.Queue()
-        sample_queue.put('yam')
-        with ObjectPool(sample_queue) as obj:
-            # print('Inside with: {}'.format(obj))
-            self.assertEqual(obj, 'yam')
-        self.assertFalse(sample_queue.empty())
-        self.assertTrue(sample_queue.get() == 'yam')
-        self.assertTrue(sample_queue.empty())
+        for name, service in data.items():
+        warn_for_links(name, service)
+        warn_for_external_links(name, service)
+        rewrite_net(service, service_names)
+        rewrite_build(service)
+        rewrite_logging(service)
+        rewrite_volumes_from(service, service_names)
