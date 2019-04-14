412956a20c6aaaf752655bae1fd632aaaa72e0fa
SUNsung <weatherius@gmail.com>
2019-04-14 13:13:56 +0300

    Rewrite [59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb] File in: 2019-04-14 13:13:56

--- ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
+++ ruby/59B7HLznbiIrQ59sNWrlHHECCs5990Cbv8BtQGc59NPk.rb
@@ -1,70 +1,118 @@
 
         
-            def class_ref_for_action(named: nil)
-      class_ref = Actions.action_class_ref(named)
-      unless class_ref
-        if Fastlane::Actions.formerly_bundled_actions.include?(action)
-          # This was a formerly bundled action which is now a plugin.
-          UI.verbose(caller.join('\n'))
-          UI.user_error!('The action '#{action}' is no longer bundled with fastlane. You can install it using `fastlane add_plugin #{action}`')
-        else
-          Fastlane::ActionsList.print_suggestions(action)
-          UI.user_error!('Action '#{action}' not available, run `fastlane actions` to get a full list')
-        end
-      end
-    
-          inspector = GhInspector::Inspector.new('fastlane', 'fastlane', verbose: FastlaneCore::Globals.verbose?)
-      delegate = Fastlane::InspectorReporter.new
-      if message_or_error.kind_of?(String)
-        inspector.search_query(message_or_error, delegate)
-      else
-        inspector.search_exception(message_or_error, delegate)
-      end
-    rescue => ex
-      FastlaneCore::UI.error('Error finding relevant GitHub issues: #{ex}') if FastlaneCore::Globals.verbose?
-    end
-  end
-end
-
-    
-            result = Fastlane::FastFile.new.parse('lane :test do
-          add_git_tag ({
-            grouping: '#{grouping}',
-            build_number: #{specified_build_number},
-          })
-        end').runner.execute(:test)
-    
-          it 'handles the exclude_dirs parameter with multiple  elements correctly' do
+              it 'adds docs_section_title param to command' do
         result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: ['.bundle', 'Packages/'])
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            docs_section_title: 'Section title'
+          )
         end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}' --exclude-dir .bundle --exclude-dir Packages/')
+    
+          @key = key
+      @env_name = env_name
+      @description = description
+      @short_option = short_option
+      @default_value = default_value
+      @default_value_dynamic = default_value_dynamic
+      @verify_block = verify_block
+      @is_string = is_string
+      @data_type = type
+      @data_type = String if type == :shell_string
+      @optional = optional
+      @conflicting_options = conflicting_options
+      @conflict_block = conflict_block
+      @deprecated = deprecated
+      @sensitive = sensitive
+      @code_gen_sensitive = code_gen_sensitive || sensitive
+      @allow_shell_conversion = (type == :shell_string)
+      @display_in_shell = display_in_shell
+      @skip_type_validation = skip_type_validation # sometimes we allow multiple types which causes type validation failures, e.g.: export_options in gym
+    
+            FastlaneCore::CertChecker.installed_identies
       end
+    end
+    
+      def initialize(repo: 'twbs/bootstrap', branch: 'master', save_to: {}, cache_path: 'tmp/converter-cache-bootstrap')
+    @logger     = Logger.new
+    @repo       = repo
+    @branch     = branch || 'master'
+    @branch_sha = get_branch_sha
+    @cache_path = cache_path
+    @repo_url   = 'https://github.com/#@repo'
+    @save_to    = {
+        js:    'assets/javascripts/bootstrap',
+        scss:  'assets/stylesheets/bootstrap',
+        fonts: 'assets/fonts/bootstrap'}.merge(save_to)
+  end
+    
+      # Disable request forgery protection in test environment.
+  config.action_controller.allow_forgery_protection = false
+    
+          def header
+        if @header.nil?
+          if page = @page.header
+            @header = page.text_data
+          else
+            @header = false
+          end
+        end
+        @header
+      end
+    
+    def testpath(path)
+  File.join(TEST_DIR, path)
+end
+    
+      test 'extract destination file name in case of path renaming' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
+    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
+  end
+    
+        @view = Precious::Views::Page.new
+    @view.instance_variable_set :@page, page
+    @view.instance_variable_set :@content, page.formatted_data
+    @view.instance_variable_set :@h1_title, false
+    
+      s.authors  = ['Tom Preston-Werner', 'Rick Olson']
+  s.email    = 'tom@github.com'
+  s.homepage = 'http://github.com/gollum/gollum'
+    
+          click_link 'Completed At'
+    
+          within_row(2) { click_icon :split }
+      targetted_select2 'LA(#{order.reload.shipments.last.number})', from: '#s2id_item_stock_location'
+      click_icon :save
+      wait_for_ajax
+      expect(page.find('#shipment_#{order.reload.shipments.last.id}')).to be_present
     end
   end
 end
 
     
-            expect(result).to include(' -rule #{rule.shellescape} ')
-        expect(result).to include(' -disable-rule #{rule.shellescape} ')
-      end
+        for_each_gem do |gem_path|
+      sh 'gem push '#{gem_path}''
+    end
+  end
+end
     
-          it 'returns the return value of the block if present' do
-        expect_command('ls', '-la')
-        return_value = Fastlane::Actions.sh('ls', '-la') do |status, result|
-          42
+              if Spree::Dependencies.cart_update_service.constantize.call(order: @order, params: line_items_attributes).success?
+            @line_item.reload
+            respond_with(@line_item, default_template: :show)
+          else
+            invalid_resource!(@line_item)
+          end
         end
-        expect(return_value).to eq(42)
+    
+          def variant_attributes
+        if @current_user_roles&.include?('admin')
+          @@variant_attributes + [:cost_price]
+        else
+          @@variant_attributes
+        end
       end
     end
   end
-    
-        # An empty argument will be skipped, so return empty quotes.
-    # https://github.com/ruby/ruby/blob/a6413848153e6c37f6b0fea64e3e871460732e34/lib/shellwords.rb#L142-L143
-    return ''''.dup if str.empty?
-    
-        # From asking people, it seems MacPorts does not have a `prefix` command, like
-    # Homebrew does, so make an educated guess:
-    if port_prefix = prefix_from_bin('port')
-      prefixes << port_prefix
-    end
+end
