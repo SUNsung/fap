cb9495c61869783d6831eb3c5c17c67c880c44b1
SUNsung <weatherius@gmail.com>
2019-04-15 20:36:38 +0300

    Rewrite [35QvFVI5T0WoY35Qtylh9ddWAg35NB4MVqbDOlg35aZ.py] File in: 2019-04-15 20:36:37

--- pyton/35QvFVI5T0WoY35Qtylh9ddWAg35NB4MVqbDOlg35aZ.py
+++ pyton/35QvFVI5T0WoY35Qtylh9ddWAg35NB4MVqbDOlg35aZ.py
@@ -1,66 +1,19 @@
 
         
-            # login request set the user_id in the session
-    # check that the user is loaded from the session
-    with client:
-        client.get('/')
-        assert session['user_id'] == 1
-        assert g.user['username'] == 'test'
+        def generateKey(keySize):
+    print('Generating prime p...')
+    p = rabinMiller.generateLargePrime(keySize)
+    print('Generating prime q...')
+    q = rabinMiller.generateLargePrime(keySize)
+    n = p * q
     
-        exec('def reraise(tp, value, tb=None):\n raise tp, value, tb')
+            else:
+            colision_resolution = self._colision_resolution(key, data)
+            if colision_resolution is not None:
+                self._set_value(colision_resolution, data)
+            else:
+                self.rehashing()
+                self.insert_data(data)
     
-        In both cases (loading from any Python file or loading from modules),
-    only uppercase keys are added to the config.  This makes it possible to use
-    lowercase values in the config file for temporary values that are not added
-    to the config or to define the config keys in the same file that implements
-    the application.
-    
-            buf.append('  Make sure to directly send your %s-request to this URL '
-                   'since we can\'t make browsers or HTTP clients redirect '
-                   'with form data reliably or without user interaction.' %
-                   request.method)
-        buf.append('\n\nNote: this exception is only raised in debug mode')
-        AssertionError.__init__(self, ''.join(buf).encode('utf-8'))
-    
-        By default it will try to guess the mimetype for you, but you can
-    also explicitly provide one.  For extra security you probably want
-    to send certain files as attachment (HTML for instance).  The mimetype
-    guessing requires a `filename` or an `attachment_filename` to be
-    provided.
-    
-    
-class Environment(BaseEnvironment):
-    '''Works like a regular Jinja2 environment but has some additional
-    knowledge of how Flask's blueprint works so that it can prepend the
-    name of the blueprint to referenced templates if necessary.
-    '''
-    
-    
-@pytest.mark.functional
-def test_select_command_with_arrows(proc, TIMEOUT):
-    select_command_with_arrows(proc, TIMEOUT)
-    history_changed(proc, TIMEOUT, u'git help')
-    
-    
-misspelled_subcommand_with_multiple_options = '''\
-usage: aws [options] <command> <subcommand> [<subcommand> ...] [parameters]
-To see help text, you can run:
-    
-    
-if __name__ == '__main__':
-    unittest.main()  # pragma: no cover
-
-    
-    import josepy as jose
-    
-    # If true, show URL addresses after external links.
-#latex_show_urls = False
-    
-        return file_path, '/'.join(reversed(internal_path))
-    
-        def test_conflicts(self):
-        # Note: Defined IP is more important than defined port in match
-        self.assertTrue(self.addr.conflicts(self.addr1))
-        self.assertTrue(self.addr.conflicts(self.addr2))
-        self.assertTrue(self.addr.conflicts(self.addr_defined))
-        self.assertFalse(self.addr.conflicts(self.addr_default))
+            a *= a
+        b >>= 1
