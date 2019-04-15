165e55bb43e76bd091f2348d3ef407f25e65d690
SUNsung <weatherius@gmail.com>
2019-04-15 08:28:01 +0300

    Rewrite [30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb] File in: 2019-04-15 08:28:00

--- ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
+++ ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
@@ -1,163 +1,35 @@
 
         
-          context 'every `.sh` file' do
-    it 'has valid bash syntax' do
-      Pathname.glob('#{HOMEBREW_LIBRARY_PATH}/**/*.sh').each do |path|
-        relative_path = path.relative_path_from(HOMEBREW_LIBRARY_PATH)
-        next if relative_path.to_s.start_with?('shims/', 'test/', 'vendor/')
-    
-                components.each do |c|
-              encoded << [c.length].pack('N')
-              encoded << c
-            end
-    
-              # Encodes the authenticator field
-          #
-          # @return [String]
-          def encode_authenticator
-            authenticator.encode
-          end
-        end
-      end
-    end
-  end
-end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos Checksum definition.
-        class Checksum < Element
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a principal, an asset (e.g., a
-        # workstation user or a network server) on a network.
-        class Element
-    
-              # Decodes the start_time field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_start_time(input)
-            input.value[0].value
-          end
-    
-              # Makes a checksum from the Rex::Proto::Kerberos::Model::KdcRequestBody
-          #
-          # @param etype [Integer] the crypto schema to checksum
-          # @return [String] the checksum
-          # @raise [NotImplementedError] if the encryption schema isn't supported
-          def checksum(etype)
-            data = self.encode
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of request time
-        class LastRequest < Element
-    
-      # Provides configurability to Paperclip. The options available are:
-  # * whiny: Will raise an error if Paperclip cannot process thumbnails of
-  #   an uploaded image. Defaults to true.
-  # * log: Logs progress to the Rails log. Uses ActiveRecord's logger, so honors
-  #   log levels, etc. Defaults to true.
-  # * command_path: Defines the path at which to find the command line
-  #   programs if they are not visible to Rails the system's search path. Defaults to
-  #   nil, which uses the first executable found in the user's search path.
-  # * use_exif_orientation: Whether to inspect EXIF data to determine an
-  #   image's orientation. Defaults to true.
-  def self.options
-    @options ||= {
-      command_path: nil,
-      content_type_mappings: {},
-      log: true,
-      log_command: true,
-      read_timeout: nil,
-      swallow_stderr: true,
-      use_exif_orientation: true,
-      whiny: true,
-      is_windows: Gem.win_platform?
-    }
-  end
-    
-        def add_paperclip_callbacks
-      @klass.send(
-        :define_paperclip_callbacks,
-        :post_process, :'#{@name}_post_process')
-    end
-    
-        # Returns the pluralized form of the attachment name. e.g.
-    # 'avatars' for an attachment of :avatar
-    def attachment attachment, style_name
-      plural_cache.pluralize_symbol(attachment.name)
-    end
-    
-      def organization
-    @organization ||= current_user.organizations_scope.find_by_permalink!(params[:org_permalink])
-  end
-    
-      include WithinOrganization
-    
-      def update
-    if @ip_pool.update(safe_params)
-      redirect_to_with_json [:edit, @ip_pool], :notice => 'IP Pool has been updated.'
+            # If the package output (-p flag) is a directory, write to the default file name
+    # but inside that directory.
+    if ! package.nil? && File.directory?(package)
+      package_file = File.join(package, output.to_s)
     else
-      render_form_errors 'edit', @ip_pool
+      package_file = output.to_s(package)
     end
-  end
     
-          if @query = (params[:query] || session['msg_query_#{@server.id}_#{scope}']).presence
-        session['msg_query_#{@server.id}_#{scope}'] = @query
-        qs = Postal::QueryString.new(@query)
-        if qs.empty?
-          flash.now[:alert] = 'It doesn't appear you entered anything to filter on. Please double check your query.'
-        else
-          @queried = true
-          if qs[:order] == 'oldest-first'
-            options[:direction] = 'asc'
-          end
+      # scripts_path and write_scripts cribbed from deb.rb
+  def scripts_path(path=nil)
+    @scripts_path ||= build_path('Scripts')
+    FileUtils.mkdir(@scripts_path) if !File.directory?(@scripts_path)
     
-      def update
-    extra_params = [:spam_threshold, :spam_failure_threshold, :postmaster_address]
-    extra_params += [:send_limit, :allow_sender, :log_smtp_data, :outbound_spam_threshold, :message_retention_days, :raw_message_retention_days, :raw_message_retention_size] if current_user.admin?
-    if @server.update(safe_params(*extra_params))
-      redirect_to_with_json organization_server_path(organization, @server), :notice => 'Server settings have been updated'
-    else
-      render_form_errors 'edit', @server
+        # Evaluate dependencies.
+    if !attributes[:no_auto_depends?]
+	    pkgdepend_gen = safesystemout('pkgdepend', 'generate',  '-md', '#{staging_path}',  manifest_fn)
+      File.write(build_path('#{name}.p5m.3'), pkgdepend_gen)
+    
+        if File.exists?(params[:output])
+      # TODO(sissel): Allow folks to choose output?
+      logger.error('Puppet module directory '#{params[:output]}' already ' \
+                    'exists. Delete it or choose another output (-p flag)')
     end
-  end
     
-      def update
-    if @track_domain.update(params.require(:track_domain).permit(:track_loads, :track_clicks, :excluded_click_domains, :ssl_enabled))
-      redirect_to_with_json [organization, @server, :track_domains]
-    else
-      render_form_errors 'edit', @track_domain
+      # Output a zipfile.
+  def output(output_path)
+    output_check(output_path)
+    realpath = Pathname.new(output_path).realdirpath.to_s
+    ::Dir.chdir(staging_path) do
+      safesystem('zip', '-9r', realpath, '.')
     end
-  end
-    
-      context 'called with four widths' do
-    it 'applies different widths to all sides' do
-      rule = 'border-width: 7px 8px 9px 10px'
-    
-      context 'expands focus buttons' do
-    it 'finds selectors' do
-      list = @buttons_list.map { |input| '#{input}:focus' }
-      list = list.join(', ')
-      ruleset = 'content: #{list};'
-    
-    describe 'margin' do
-  before(:all) do
-    ParserSupport.parse_file('library/margin')
-  end
-    
-      context 'called with null values' do
-    it 'writes rules for other three' do
-      ruleset = 'padding-top: 11px; ' +
-                'padding-right: 12px; ' +
-                'padding-left: 13px;'
-      bad_rule = 'padding-bottom: null;'
+  end # def output
+end # class FPM::Package::Tar
