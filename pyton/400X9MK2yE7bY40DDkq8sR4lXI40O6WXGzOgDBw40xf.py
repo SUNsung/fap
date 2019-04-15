90dd4a65213d119e0c4f382ea1c326bdc7e0a107
SUNsung <weatherius@gmail.com>
2019-04-16 01:30:43 +0300

    Rewrite [400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py] File in: 2019-04-16 01:30:42

--- pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
+++ pyton/400X9MK2yE7bY40DDkq8sR4lXI40O6WXGzOgDBw40xf.py
@@ -1,80 +1,64 @@
 
         
-                # Retrieve list of modified files
-        # Note: Noop saves can cause the file to be listed twice, I used a
-        # set to remove this possibility. This is a known augeas 0.10 error.
-        save_paths = self.aug.match('/augeas/events/saved')
+            plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
+    plt.plot(sample_sizes, one_core, label='one core')
+    plt.plot(sample_sizes, multi_core, label='multi core')
+    plt.xlabel('n_samples')
+    plt.ylabel('Time (s)')
+    plt.title('Parallel %s' % func.__name__)
+    plt.legend()
     
-        def test_select_no_input(self):
-        self.assertFalse(select_vhost_multiple([]))
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
     
-            self.addr_defined = Addr.fromstring('127.0.0.1:443')
-        self.addr_default = Addr.fromstring('_default_:443')
+            plt.subplot(m, 2, i + 1)
+        plt.plot(list_n_samples, np.sqrt(elnet_results[:, j, 1]),
+                 label='ElasticNet')
+        plt.plot(list_n_samples, np.sqrt(sgd_results[:, j, 1]),
+                 label='SGDRegressor')
+        plt.plot(list_n_samples, np.sqrt(asgd_results[:, j, 1]),
+                 label='A-SGDRegressor')
+        plt.plot(list_n_samples, np.sqrt(ridge_results[:, j, 1]),
+                 label='Ridge')
+        plt.legend(prop={'size': 10})
+        plt.xlabel('n_train')
+        plt.ylabel('Time [sec]')
+        plt.title('Training time - %d features' % list_n_features[j])
+        i += 1
     
-        return o
-
+    import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
     
+    from sklearn.datasets import make_checkerboard
+from sklearn.datasets import samples_generator as sg
+from sklearn.cluster.bicluster import SpectralBiclustering
+from sklearn.metrics import consensus_score
     
-# TODO(huay): char_cnn_embedding
-def char_cnn_embedding(x, c_embed_size=8, share_cnn_weights=True, name='char_cnn_embedding', reuse=None):
+    The example is engineered to show the effect of the choice of different
+metrics. It is applied to waveforms, which can be seen as
+high-dimensional vector. Indeed, the difference between metrics is
+usually more pronounced in high dimension (in particular for euclidean
+and cityblock).
+    
     '''
-    In:  [N, max_n_word, max_n_char]
-    Out: [N, max_n_word, c_embed_size]
+=========================================================
+K-means Clustering
+=========================================================
     
-        References:
-        K.repeat()
-        tf.tile()
-    '''
-    assert x.get_shape().ndims == 2
-    x = tf.expand_dims(x, axis=1)  # -> [batch_size, 1, n_input]
-    return tf.tile(x, [1, n, 1])  # -> [batch_size, n, n_input]
-    
-    
-  def Start( self ):
-    self.PostDataToHandler( {},
-                            'shutdown',
-                            TIMEOUT_SECONDS,
-                            display_message = False )
-    
-    
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
-    
-          f = _base.Future()
-      w = _WorkItem( f, fn, args, kwargs )
-    
-            # Make the call queue slightly larger than the number of processes to
-        # prevent the worker processes from idling. But don't make it too big
-        # because futures in the call queue cannot be cancelled.
-        self._call_queue = multiprocessing.Queue(self._max_workers +
-                                                 EXTRA_QUEUED_CALLS)
-        self._result_queue = multiprocessing.Queue()
-        self._work_ids = queue.Queue()
-        self._queue_management_thread = None
-        self._processes = set()
-    
-    def _worker(executor_reference, work_queue):
-    try:
-        while True:
-            work_item = work_queue.get(block=True)
-            if work_item is not None:
-                work_item.run()
-                continue
-            executor = executor_reference()
-            # Exit if:
-            #   - The interpreter is shutting down OR
-            #   - The executor that owns the worker has been collected OR
-            #   - The executor that owns the worker has been shutdown.
-            if _shutdown or executor is None or executor._shutdown:
-                # Notice other workers
-                work_queue.put(None)
-                return
-            del executor
-    except BaseException:
-        _base.LOGGER.critical('Exception in worker', exc_info=True)
+    digits = datasets.load_digits()
+images = digits.images
+X = np.reshape(images, (len(images), -1))
+connectivity = grid_to_graph(*images[0].shape)
