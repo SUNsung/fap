4851fd66bff0932e0e624903ab42253fcf95b138
SUNsung <weatherius@gmail.com>
2019-04-17 00:42:47 +0300

    Rewrite [17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py] File in: 2019-04-17 00:42:47

--- pyton/17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py
+++ pyton/17aajJwH8ovn617NAmZfX1OPa.17CHt5HAS.4n217EF8.py
@@ -1,153 +1,33 @@
 
         
-                # Keyword arguments > stream.encoding > default utf8
-        if self.stdin_encoding is None:
-            self.stdin_encoding = getattr(
-                self.stdin, 'encoding', None) or 'utf8'
-        if self.stdout_encoding is None:
-            actual_stdout = self.stdout
-            if is_windows:
-                # noinspection PyUnresolvedReferences
-                from colorama import AnsiToWin32
-                if isinstance(self.stdout, AnsiToWin32):
-                    actual_stdout = self.stdout.wrapped
-            self.stdout_encoding = getattr(
-                actual_stdout, 'encoding', None) or 'utf8'
+        
+@pytest.mark.parametrize('command', [
+    Command('apt-cache search foo', ''),
+    Command('aptitude search foo', ''),
+    Command('apt search foo', ''),
+    Command('apt-get install foo', ''),
+    Command('apt-get source foo', ''),
+    Command('apt-get clean', ''),
+    Command('apt-get remove', ''),
+    Command('apt-get update', ''),
+    Command('sudo apt update', no_match_output)
+])
+def test_not_match(command):
+    assert not match(command)
     
-            '''
-        available_plugins = plugin_manager.get_formatters_grouped()
-        self.enabled_plugins = []
-        for group in groups:
-            for cls in available_plugins[group]:
-                p = cls(env=env, **kwargs)
-                if p.enabled:
-                    self.enabled_plugins.append(p)
-    
-    
-def test_basic_auth(httpbin_both):
-    r = http('--auth=user:password',
-             'GET', httpbin_both + '/basic-auth/user/password')
-    assert HTTP_OK in r
-    assert r.json == {'authenticated': True, 'user': 'user'}
-    
-    '''
-from httpie.client import JSON_ACCEPT
-from utils import MockEnvironment, http, HTTP_OK
-from fixtures import FILE_PATH
-    
-        def test_request_body_from_file_by_path_no_field_name_allowed(
-            self, httpbin):
-        env = MockEnvironment(stdin_isatty=True)
-        r = http('POST', httpbin.url + '/post', 'field-name@' + FILE_PATH_ARG,
-                 env=env, error_exit_ok=True)
-        assert 'perhaps you meant --form?' in r.stderr
-    
-        @property
-    def path(self):
-        '''Return the config file path creating basedir, if needed.'''
-        path = self._get_path()
-        try:
-            os.makedirs(os.path.dirname(path), mode=0o700)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        return path
-    
-        try:
-        response = requests_session.request(**kwargs)
-    except Exception:
-        raise
-    else:
-        # Existing sessions with `read_only=True` don't get updated.
-        if session.is_new() or not read_only:
-            session.cookies = requests_session.cookies
-            session.save()
-        return response
-    
-            try:
-            speed = actually_downloaded / time_taken
-        except ZeroDivisionError:
-            # Either time is 0 (not all systems provide `time.time`
-            # with a better precision than 1 second), and/or nothing
-            # has been downloaded.
-            speed = actually_downloaded
-    
-    
-class CloudWatchEventRuleManager(object):
-    RULE_FIELDS = ['name', 'event_pattern', 'schedule_expression', 'description', 'role_arn']
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-    ## List all Subscriptions for a Topic in a project
-- gcpubsub_facts:
-    view: subscriptions
-    topic: my-topic
-    state: list
+    \tDid you mean `build`?
 '''
     
-        if purge_rules is True and len(rules) > 0:
-        result['rules_purged'] = len(rules)
-        deletions = result['rules_purged']
-        rules = list()
-        if not module.check_mode:
-            gateway.del_all_nat_rules()
-            task = gateway.save_services_configuration()
-            vca.block_until_completed(task)
-            rules = gateway.get_nat_rules()
-        result['changed'] = True
+                with tarfile.TarFile(path, 'r') as archive:
+                archive.extractall()
     
-    RETURN = '''# '''
+      # Implements linear decay of the learning rate.
+  learning_rate = tf.train.polynomial_decay(
+      learning_rate,
+      global_step,
+      num_train_steps,
+      end_learning_rate=0.0,
+      power=1.0,
+      cycle=False)
     
-    
-ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-    
-        params = {}
-    params['message'] = msg
-    if annotated_by:
-        params['annotated_by'] = annotated_by
-    if level:
-        params['level'] = level
-    if instance_id:
-        params['instance_id'] = instance_id
-    if event_epoch:
-        params['event_epoch'] = event_epoch
-    
-    for face_location in face_locations:
-    
-            # Now let's list all the faces we found in all 128 frames
-        for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
-            number_of_faces_in_frame = len(face_locations)
-    
-    
-def print_result(filename, name, distance, show_distance=False):
-    if show_distance:
-        print('{},{},{}'.format(filename, name, distance))
-    else:
-        print('{},{}'.format(filename, name))
-    
-        def test_raw_face_landmarks(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
-        face_landmarks = api._raw_face_landmarks(img)
-        example_landmark = face_landmarks[0].parts()[10]
-    
-    
-@app.route('/', methods=['GET', 'POST'])
-def upload_image():
-    # 检测图片是否上传成功
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-    
-        :param file: image file name or file object to load
-    :param mode: format to convert the image to. Only 'RGB' (8-bit RGB, 3 channels) and 'L' (black and white) are supported.
-    :return: image contents as numpy array
-    '''
-    im = PIL.Image.open(file)
-    if mode:
-        im = im.convert(mode)
-    return np.array(im)
+      tokenizer = create_tokenizer_from_hub_module(FLAGS.bert_hub_module_handle)
