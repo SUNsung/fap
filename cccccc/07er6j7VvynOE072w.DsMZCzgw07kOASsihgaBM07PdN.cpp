89f95bcd7bc3a573d3076c062c1269b1f0d78915
SUNsung <weatherius@gmail.com>
2019-04-17 01:46:31 +0300

    Rewrite [07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp] File in: 2019-04-17 01:46:30

--- cccccc/07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp
+++ cccccc/07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp
@@ -1,228 +1,102 @@
 
         
-        #include 'tensorflow/python/grappler/model_analyzer.h'
-    
-      // Support dtype(bfloat16)
-  if (PyDict_SetItemString(PyBfloat16_Type.tp_dict, 'dtype',
-                           reinterpret_cast<PyObject*>(&NPyBfloat16_Descr)) <
-      0) {
-    return false;
-  }
-    
-    #ifndef TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_PLATFORM_ID_H_
-#define TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_PLATFORM_ID_H_
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    ExecutorCache::Entry::~Entry() {
-  mutex_lock lock{configurations_mutex};
-  configurations.clear();
+        MeasureDouble RpcServerServerLatency() {
+  static const auto measure = MeasureDouble::Register(
+      kRpcServerServerLatencyMeasureName,
+      'Time between first byte of request received to last byte of response '
+      'sent, or terminal error',
+      kUnitMilliseconds);
+  return measure;
 }
     
-    #include 'tensorflow/stream_executor/lib/status.h'
-#include 'tensorflow/stream_executor/lib/statusor.h'
-#include 'tensorflow/stream_executor/platform/mutex.h'
-#include 'tensorflow/stream_executor/stream_executor_pimpl.h'
+    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
+::opencensus::stats::MeasureDouble RpcClientServerLatency();
+::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
     
     
-    {    internal::vtransform(size,
-                     srcBase, srcStride,
-                     dstBase, dstStride,
-                     dstBase, dstStride,
-                     AccumulateWeighted(alpha));
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)dstBase;
-    (void)dstStride;
-    (void)alpha;
-#endif
-}
-    
-        void operator() (const typename VecTraits<T>::vec64 & v_src0,
-                     const typename VecTraits<T>::vec64 & v_src1,
-                     typename VecTraits<T>::vec64 & v_dst) const
     {
-        typename VecTraits<wtype>::vec128 vr;
-        wideAdd(vmovl(v_src0), vmovl(v_src1), vr);
-    }
+    {}  // namespace load_reporter
+}  // namespace grpc
     
-    template <bool L2gradient>
-inline void prepareThresh(f64 low_thresh, f64 high_thresh,
-                          s32 &low, s32 &high)
+    #endif  // !defined(GPR_LINUX) && !defined(GPR_WINDOWS) && !defined(GPR_APPLE)
+
+    
+        std::shared_ptr<DHTNode> localNode;
+    
+    void DHTReplaceNodeTask::startup() { sendMessage(); }
+    
+    DHTResponseMessage::~DHTResponseMessage() = default;
+    
+    #include <cstring>
+#include <cassert>
+#include <cstdio>
+#include <utility>
+    
+    #include <vector>
+#include <string>
+#include <memory>
+    
+      virtual void startup() = 0;
+    
+    #include <algorithm>
+    
+    #include 'common.h'
+    
+    std::shared_ptr<DHTTask>
+DHTTaskFactoryImpl::createPeerAnnounceTask(const unsigned char* infoHash)
 {
-    if (low_thresh > high_thresh)
-        std::swap(low_thresh, high_thresh);
-#if defined __GNUC__
-    low = (s32)low_thresh;
-    high = (s32)high_thresh;
-    low -= (low > low_thresh);
-    high -= (high > high_thresh);
-#else
-    low = internal::round(low_thresh);
-    high = internal::round(high_thresh);
-    f32 ldiff = (f32)(low_thresh - low);
-    f32 hdiff = (f32)(high_thresh - high);
-    low -= (ldiff < 0);
-    high -= (hdiff < 0);
-#endif
-}
-template <>
-inline void prepareThresh<true>(f64 low_thresh, f64 high_thresh,
-                                s32 &low, s32 &high)
-{
-    if (low_thresh > high_thresh)
-        std::swap(low_thresh, high_thresh);
-    if (low_thresh > 0) low_thresh *= low_thresh;
-    if (high_thresh > 0) high_thresh *= high_thresh;
-#if defined __GNUC__
-    low = (s32)low_thresh;
-    high = (s32)high_thresh;
-    low -= (low > low_thresh);
-    high -= (high > high_thresh);
-#else
-    low = internal::round(low_thresh);
-    high = internal::round(high_thresh);
-    f32 ldiff = (f32)(low_thresh - low);
-    f32 hdiff = (f32)(high_thresh - high);
-    low -= (ldiff < 0);
-    high -= (hdiff < 0);
-#endif
-}
-    
-    #if defined WITH_NEON && (defined __ARM_NEON__ || defined __ARM_NEON)
-#define CAROTENE_NEON
-#endif
-    
-             int32x4_t vline1_s32 = vcvtq_s32_f32(vline1_f32);
-         int32x4_t vline2_s32 = vcvtq_s32_f32(vline2_f32);
-         int16x4_t vline1_s16 = vqmovn_s32(vline1_s32);
-         int16x4_t vline2_s16 = vqmovn_s32(vline2_s32);
-    
-                int32x2_t vnz1 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx1), vget_high_u32(vlx1)));
-            int32x2_t vnz2 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx2), vget_high_u32(vlx2)));
-            int32x2_t vnz3 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx3), vget_high_u32(vlx3)));
-            int32x2_t vnz4 = vreinterpret_s32_u32(vpmax_u32(vget_low_u32(vlx4), vget_high_u32(vlx4)));
-    
-                    d &= tab[ptr[pixel[1]]] | tab[ptr[pixel[9]]];
-                d &= tab[ptr[pixel[3]]] | tab[ptr[pixel[11]]];
-                d &= tab[ptr[pixel[5]]] | tab[ptr[pixel[13]]];
-                d &= tab[ptr[pixel[7]]] | tab[ptr[pixel[15]]];
-    
-    
-    {            vec128 v_src = vld1q(src + js);
-            vec128 v_dst = vrev64q(v_src);
-            v_dst = vcombine(vget_high(v_dst), vget_low(v_dst));
-            vst1q(dst + jd - step_base, v_dst);
-        }
-        for (; js < roiw_tail; js += step_tail, jd -= step_tail)
-        {
-            vec64 v_src = vld1(src + js);
-            vst1(dst + jd - step_tail, vrev64(v_src));
-        }
-    
-        /**
-     * Get maximum significant digits. 0 means no minimum.
-     */
-    int32_t getMin() const {
-        return fMin;
-    }
-    
-    SimpleTimeZone::SimpleTimeZone(int32_t rawOffsetGMT, const UnicodeString& ID,
-    int8_t savingsStartMonth, int8_t savingsStartDay,
-    int8_t savingsStartDayOfWeek, int32_t savingsStartTime,
-    int8_t savingsEndMonth, int8_t savingsEndDay,
-    int8_t savingsEndDayOfWeek, int32_t savingsEndTime,
-    int32_t savingsDST, UErrorCode& status)
-:   BasicTimeZone(ID)
-{
-    clearTransitionRules();
-    construct(rawOffsetGMT,
-              savingsStartMonth, savingsStartDay, savingsStartDayOfWeek,
-              savingsStartTime, WALL_TIME,
-              savingsEndMonth, savingsEndDay, savingsEndDayOfWeek,
-              savingsEndTime, WALL_TIME,
-              savingsDST, status);
+  // TODO
+  return nullptr;
 }
     
     
-    {    return ch < UPRV_LENGTHOF(mapCharToLevel) ? mapCharToLevel[ch] : -1;
-}
-    
-    U_CDECL_BEGIN
-static UBool U_CALLCONV
-smpdtfmt_cleanup(void)
-{
-    return SimpleDateFormatStaticSets::cleanup();
-}
-    
-    
-    {    /**
-     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
-     *
-     * @param keyword for example 'few' or 'other'
-     * @return the index of the plural form corresponding to the keyword
-     */
-    static int32_t indexFromString(const UnicodeString &keyword, UErrorCode &errorCode);
+    {  virtual void addImmediateTask(const std::shared_ptr<DHTTask>& task) = 0;
 };
     
-    // Declaration of the NarratorAnnouncementHostFactory class.
-// This class exists to hide the construction of a concrete INarratorAnnouncementHost.
-// Depending on the version of the OS the app is running on, the factory will return
-// an announcement host appropriate for that version.
-    
-    void NarratorNotifier::OnAnnouncementChanged(_In_ DependencyObject^ dependencyObject, _In_ DependencyPropertyChangedEventArgs^ e)
+    void DHTTaskQueueImpl::addPeriodicTask1(const std::shared_ptr<DHTTask>& task)
 {
-    auto instance = safe_cast<NarratorNotifier^>(dependencyObject);
-    if (instance != nullptr)
-    {
-        instance->Announce(safe_cast<NarratorAnnouncement^>(e->NewValue));
+  periodicTaskQueue1_.addTask(task);
+}
+    
+    #include <cstring>
+#include <cstdlib>
+    
+    DNSCache::AddrEntry& DNSCache::AddrEntry::operator=(const AddrEntry& c)
+{
+  if (this != &c) {
+    addr_ = c.addr_;
+    good_ = c.good_;
+  }
+  return *this;
+}
+    
+    
+
+    
+    namespace BulkOperations {
     }
+    
+    template <class T>
+TabViewModel::ModelFactory getTabModelFactory() {
+  return TabViewModel::ModelFactory(
+      [](QSharedPointer<RedisClient::Connection> c, int dbIndex) {
+        return QSharedPointer<TabModel>(new T(c, dbIndex),
+                                        &QObject::deleteLater);
+      });
 }
 
     
-    namespace CalculatorApp::Common::Automation
-{
-    public ref class NarratorNotifier sealed : public Windows::UI::Xaml::DependencyObject
-    {
-    public:
-        NarratorNotifier();
-    }
-    }
+        public:
+        enum Roles {
+            name = Qt::UserRole + 1,
+            arguments,
+            summary,
+            since
+        };
     
-            if (id.compare(L'sGrouping') == 0)
-        {
-            // The following groupings are the onces that CalcEngine supports.
-            //   0;0             0x000          - no grouping
-            //   3;0             0x003          - group every 3 digits
-            //   3;2;0           0x023          - group 1st 3 and then every 2 digits
-            //   4;0             0x004          - group every 4 digits
-            //   5;3;2;0         0x235          - group 5, then 3, then every 2
-            wstring numberGroupingString = localizationSettings.GetNumberGroupingStr();
-            return numberGroupingString;
-        }
-    
-    #pragma once
-    
-    
-    {        Windows::Globalization::Fonts::LanguageFontGroup^ m_fontGroup;
-        Platform::String^ m_language;
-        Windows::UI::Xaml::FlowDirection m_flowDirection;
-        bool m_overrideFontApiValues;
-        Platform::String^ m_fontFamilyOverride;
-        Windows::UI::Text::FontWeight m_fontWeightOverride;
-        double m_uiTextFontScaleFactorOverride;
-        double m_uiCaptionFontScaleFactorOverride;
-    };
-    
-        // out_of_range.109
-    try
-    {
-        // try to use an array index that is not a number
-        json::reference ref = j.at('/array/one'_json_pointer);
-    }
-    catch (json::parse_error& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+     public slots:
+  void executeCommand(const QString &);
