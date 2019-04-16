4416a04a9d21c37c6711a3d1033283dd665056e8
SUNsung <weatherius@gmail.com>
2019-04-16 20:31:46 +0300

    Rewrite [58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py] File in: 2019-04-16 20:31:46

--- pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
+++ pyton/58fL0zazqehQg58txi7X4cdw8Q58F2F4BTDIHtU58p0.py
@@ -1,152 +1,121 @@
 
         
-            theplatform_download_by_pid(pid, title, output_dir=output_dir, merge=merge, info_only=info_only)
+                if any([s_line.startswith(s) for s in ['* [', '- [']]):
+            if indent == last_indent:
+                blocks[-1].append(line)
+            else:
+                blocks.append([line])
+            last_indent = indent
+        else:
+            blocks.append([line])
+            last_indent = None
     
-    __all__ = ['ckplayer_download']
+                # >leading or trailing LWS MAY be removed without
+            # >changing the semantics of the field value'
+            # -https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
+            # Also, requests raises `InvalidHeader` for leading spaces.
+            value = value.strip()
     
-        def prepare(self, **kwargs):
-        self.api_data = json.loads(get_content(self.__class__.ep.format(self.vid)))
-        self.title = self.api_data['title']
-        for s in self.api_data['video']:
-            for st in self.__class__.stream_types:
-                if st['map_to'] == s:
-                    urls = self.api_data['video'][s]
-                    src = [u['url'] for u in urls]
-                    stream_data = dict(src=src, size=0, container='mp4', video_profile=st['video_profile'])
-                    self.streams[st['id']] = stream_data
     
-    site_info = 'Dailymotion.com'
-download = dailymotion_download
-download_playlist = playlist_not_supported('dailymotion')
+class ConverterPlugin(object):
+    
+    from httpie.plugins.base import AuthPlugin
+    
+        def get_formatters_grouped(self):
+        groups = {}
+        for group_name, group in groupby(
+                self.get_formatters(),
+                key=lambda p: getattr(p, 'group_name', 'format')):
+            groups[group_name] = list(group)
+        return groups
+    
+    
+def test_basic_auth(httpbin_both):
+    r = http('--auth=user:password',
+             'GET', httpbin_both + '/basic-auth/user/password')
+    assert HTTP_OK in r
+    assert r.json == {'authenticated': True, 'user': 'user'}
+    
+        def test_POST_with_data_auto_JSON_headers(self, httpbin):
+        r = http('POST', httpbin.url + '/post', 'a=b')
+        assert HTTP_OK in r
+        assert r.json['headers']['Accept'] == JSON_ACCEPT
+        assert r.json['headers']['Content-Type'] == 'application/json'
+    
+    from utils import TESTS_ROOT
+    
+        # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
 
     
-        vids = matchall(content, yinyuetai_embed_patterns)
-    for vid in vids:
-        found = True
-        yinyuetai_download_by_id(vid, title=title, output_dir=output_dir, merge=merge, info_only=info_only)
-    
-    #----------------------------------------------------------------------
-def fc2video_download_by_upid(upid, output_dir = '.', merge = True, info_only = False, **kwargs):
-    ''''''
-    fake_headers = {
-        'DNT': '1',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Accept-Language': 'en-CA,en;q=0.8,en-US;q=0.6,zh-CN;q=0.4,zh;q=0.2',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.58 Safari/537.36',
-        'Accept': '*/*',
-        'X-Requested-With': 'ShockwaveFlash/19.0.0.245',
-        'Connection': 'keep-alive',
-    }
-    api_base = 'http://video.fc2.com/ginfo.php?upid={upid}&mimi={mimi}'.format(upid = upid, mimi = makeMimi(upid))
-    html = get_content(api_base, headers=fake_headers)
+    # NOTE: This example requires flask to be installed! You can install it with pip:
+# $ pip3 install flask
     
     
-class KBaseMapping:
-    def __init__(self, base=62):
-        self.base = base
-        mapping_table = [str(num) for num in range(10)]
-        for i in range(26):
-            mapping_table.append(chr(i + ord('a')))
-        for i in range(26):
-            mapping_table.append(chr(i + ord('A')))
+def image_files_in_folder(folder):
+    return [os.path.join(folder, f) for f in os.listdir(folder) if re.match(r'.*\.(jpg|jpeg|png)', f, flags=re.I)]
     
-    site_info = 'Giphy.com'
-download = giphy_download
-download_playlist = playlist_not_supported('giphy')
-
+    print('I found {} face(s) in this photograph.'.format(len(face_landmarks_list)))
     
-    For more details about this component, please refer to the documentation at
-https://home-assistant.io/components/alexa/
-'''
-import copy
-from datetime import datetime
-import logging
-import uuid
+    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+# See http://pillow.readthedocs.io/ for more about PIL/Pillow
+pil_image = Image.fromarray(unknown_image)
+# Create a Pillow ImageDraw Draw instance to draw with
+draw = ImageDraw.Draw(pil_image)
     
-            devices = {}
-        for lease in leases_result:
-            match = _LEASES_REGEX.search(lease.decode('utf-8'))
-            if match is not None:
-                devices[match.group('ip')] = {
-                    'ip': match.group('ip'),
-                    'mac': match.group('mac').upper(),
-                    'timevalid': int(match.group('timevalid'))
-                    }
-        return devices
-
+        # Create a subprocess to capture frames
+    p.append(Process(target=capture, args=(read_frame_list,)))
+    p[0].start()
     
+        def step(self, action):
+        assert self.action_space.contains(action)
+        if self.np_random.rand() < self.slip:
+            action = not action  # agent slipped, reverse action taken
+        if action:  # 'backwards': go back to the beginning, get small reward
+            reward = self.small
+            self.state = 0
+        elif self.state < self.n - 1:  # 'forwards': go up along the chain
+            reward = 0
+            self.state += 1
+        else:  # 'forwards': stay at the end of the chain, collect large reward
+            reward = self.large
+        done = False
+        return self.state, reward, done, {}
     
-def setup_scanner(hass, config, see, discovery_info=None):
-    '''Set up the demo tracker.'''
-    def offset():
-        '''Return random offset.'''
-        return (random.randrange(500, 2000)) / 2e5 * random.choice((-1, 1))
+            Returns:
+            int: The registration ID of this object. It is the caller's responsibility to save this ID if early closing is desired.
+        '''
+        assert hasattr(closeable, 'close'), 'No close method for {}'.format(closeable)
     
-            return [device.mac for device in self.last_results]
+        Args:
+        a (Optional[int, str]): None seeds from an operating system specific randomness source.
+        max_bytes: Maximum number of bytes to use in the seed.
+    '''
+    # Adapted from https://svn.python.org/projects/python/tags/r32/Lib/random.py
+    if a is None:
+        a = _bigint_from_bytes(os.urandom(max_bytes))
+    elif isinstance(a, str):
+        a = a.encode('utf8')
+        a += hashlib.sha512(a).digest()
+        a = _bigint_from_bytes(a[:max_bytes])
+    elif isinstance(a, integer_types):
+        a = a % 2**(8 * max_bytes)
+    else:
+        raise error.Error('Invalid type for seed: {} ({})'.format(type(a), a))
     
+        logger.info('Generating rollout for {}'.format(spec.id))
     
-def get_scanner(hass, config):
-    '''Validate the configuration and return a Linksys AP scanner.'''
-    try:
-        return LinksysSmartWifiDeviceScanner(config[DOMAIN])
-    except ConnectionError:
-        return None
-    
-            _LOGGER.info('nmap scan successful')
-        return True
-
-    
-        return scanner if scanner.success_init else None
-    
-        def _retrieve_list_with_retry(self):
-        '''Retrieve the device list with a retry if token is invalid.
-    
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_API_KEY): cv.string,
-    vol.Required(CONF_RECIPIENT): cv.string,
-})
-    
-        @property
-    def targets(self):
-        '''Return a dictionary of registered targets.'''
-        return {'test target name': 'test target id'}
-    
-    CONF_PAGE_ACCESS_TOKEN = 'page_access_token'
-BASE_URL = 'https://graph.facebook.com/v2.6/me/messages'
-CREATE_BROADCAST_URL = 'https://graph.facebook.com/v2.11/me/message_creatives'
-SEND_BROADCAST_URL = 'https://graph.facebook.com/v2.11/me/broadcast_messages'
-    
-        complete_apps = ['sentry']
-
-    
-            # Adding model 'DSymApp'
-        db.create_table(
-            'sentry_dsymapp', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.Project']
-                    )
-                ), ('app_id', self.gf('django.db.models.fields.CharField')(max_length=64)),
-                ('sync_id', self.gf('django.db.models.fields.CharField')(max_length=64, null=True)),
-                ('data', self.gf('jsonfield.fields.JSONField')(default={})), (
-                    'platform',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(
-                        default=0
-                    )
-                ), (
-                    'last_synced',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['DSymApp'])
-    
-            for user in RangeQuerySetWrapperWithProgressBar(users):
-            orm.UserEmail.objects.get_or_create(user=user, email=user.email)
+        def _create_particle(self, point1, point2, grass):
+        class Particle:
+            pass
+        p = Particle()
+        p.color = WHEEL_COLOR if not grass else MUD_COLOR
+        p.ttl = 1
+        p.poly = [(point1[0],point1[1]), (point2[0],point2[1])]
+        p.grass = grass
+        self.particles.append(p)
+        while len(self.particles) > 30:
+            self.particles.pop(0)
+        return p
