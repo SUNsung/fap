3463f726b74bf8fa2de6e564c91996679b27bdb5
SUNsung <weatherius@gmail.com>
2019-04-16 06:08:12 +0300

    Rewrite [00P5sLc2QnsGQ00frxQyWJtow00OVoPUnC7CG6001Ph.py] File in: 2019-04-16 06:08:12

--- pyton/00P5sLc2QnsGQ00frxQyWJtow00OVoPUnC7CG6001Ph.py
+++ pyton/00P5sLc2QnsGQ00frxQyWJtow00OVoPUnC7CG6001Ph.py
@@ -1,153 +1,92 @@
 
         
-                infos = []
-        if not self.wasSuccessful():
-            write('FAILED')
-            failed, errored = map(len, (self.failures, self.errors))
-            if failed:
-                infos.append('failures=%d' % failed)
-            if errored:
-                infos.append('errors=%d' % errored)
-        else:
-            write('OK')
+        signature = hexlify(rsa.pkcs1.sign(json.dumps(versions_info, sort_keys=True).encode('utf-8'), privkey, 'SHA-256')).decode()
+print('signature: ' + signature)
     
-        def process_options(self, args, opts):
-        ScrapyCommand.process_options(self, args, opts)
-        try:
-            opts.spargs = arglist_to_dict(opts.spargs)
-        except ValueError:
-            raise UsageError('Invalid -a value, use -a NAME=VALUE', print_help=False)
-        if opts.output:
-            if opts.output == '-':
-                self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
-            else:
-                self.settings.set('FEED_URI', opts.output, priority='cmdline')
-            feed_exporters = without_none_values(
-                self.settings.getwithbase('FEED_EXPORTERS'))
-            valid_output_formats = feed_exporters.keys()
-            if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace('.', '')
-            if opts.output_format not in valid_output_formats:
-                raise UsageError('Unrecognized output format '%s', set one'
-                                 ' using the '-t' switch or as a file extension'
-                                 ' from the supported list %s' % (opts.output_format,
-                                                                  tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
-    
-        def add_options(self, parser):
-        ScrapyCommand.add_options(self, parser)
-        parser.add_option('-l', '--list', dest='list', action='store_true',
-            help='List available templates')
-        parser.add_option('-e', '--edit', dest='edit', action='store_true',
-            help='Edit spider after creating it')
-        parser.add_option('-d', '--dump', dest='dump', metavar='TEMPLATE',
-            help='Dump template to standard output')
-        parser.add_option('-t', '--template', dest='template', default='basic',
-            help='Uses a custom template.')
-        parser.add_option('--force', dest='force', action='store_true',
-            help='If the spider already exists, overwrite it with the template')
-    
-            for x in iterate_spider_output(cb(response)):
-            if isinstance(x, (BaseItem, dict)):
-                items.append(x)
-            elif isinstance(x, Request):
-                requests.append(x)
-        return items, requests
-    
-    openssl_methods = {
-    METHOD_TLS:    SSL.SSLv23_METHOD,                   # protocol negotiation (recommended)
-    METHOD_SSLv3:  SSL.SSLv3_METHOD,                    # SSL 3 (NOT recommended)
-    METHOD_TLSv10: SSL.TLSv1_METHOD,                    # TLS 1.0 only
-    METHOD_TLSv11: getattr(SSL, 'TLSv1_1_METHOD', 5),   # TLS 1.1 only
-    METHOD_TLSv12: getattr(SSL, 'TLSv1_2_METHOD', 6),   # TLS 1.2 only
-}
+    # the encrypted data can be generate with 'devscripts/generate_aes_testdata.py'
     
     
-class AjaxCrawlMiddleware(object):
-    '''
-    Handle 'AJAX crawlable' pages marked as crawlable via meta tag.
-    For more info see https://developers.google.com/webmasters/ajax-crawling/docs/getting-started.
-    '''
+class TestCache(unittest.TestCase):
+    def setUp(self):
+        TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+        TESTDATA_DIR = os.path.join(TEST_DIR, 'testdata')
+        _mkdir(TESTDATA_DIR)
+        self.test_dir = os.path.join(TESTDATA_DIR, 'cache_test')
+        self.tearDown()
     
-        @staticmethod
-    def create_ca():
-        key = OpenSSL.crypto.PKey()
-        key.generate_key(OpenSSL.crypto.TYPE_RSA, 2048)
-        ca = OpenSSL.crypto.X509()
-        ca.set_version(2)
-        ca.set_serial_number(0)
-        subj = ca.get_subject()
-        subj.countryName = 'CN'
-        subj.stateOrProvinceName = 'Internet'
-        subj.localityName = 'Cernet'
-        subj.organizationName = CertUtil.ca_vendor
-        # Log generated time.
-        subj.organizationalUnitName = '%s Root - %d' % (CertUtil.ca_vendor, int(time.time()))
-        subj.commonName = '%s XX-Net' % CertUtil.ca_vendor
-        ca.gmtime_adj_notBefore(- 3600 * 24)
-        ca.gmtime_adj_notAfter(CertUtil.ca_validity - 3600 * 24)
-        ca.set_issuer(subj)
-        ca.set_subject(subj)
-        ca.set_pubkey(key)
-        ca.add_extensions([
-            OpenSSL.crypto.X509Extension(
-                'basicConstraints', False, 'CA:TRUE', subject=ca, issuer=ca)
-            ])
-        ca.sign(key, CertUtil.ca_digest)
-        #xlog.debug('CA key:%s', key)
-        xlog.info('create CA')
-        return key, ca
+    from test.helper import FakeYDL
+from youtube_dl.extractor import IqiyiIE
     
-        def checker(self):
-        while True:
-            try:
-                ip = self.get_ip()
-            except Exception as e:
-                xlog.info('no ip left')
-                return
+        def test_youtube_translated_subtitles(self):
+        # This video has a subtitles track, which can be translated
+        self.url = 'Ky9eprVWzlI'
+        self.DL.params['writeautomaticsub'] = True
+        self.DL.params['subtitleslangs'] = ['it']
+        subtitles = self.getSubtitles()
+        self.assertTrue(subtitles['it'] is not None)
     
     
-__licence__ = 'BSD (3 clause)'
+@pytest.mark.functional
+def test_without_confirmation(proc, TIMEOUT):
+    without_confirmation(proc, TIMEOUT)
+    history_changed(proc, TIMEOUT, u'echo test')
     
     
-def get_service(hass, config, discovery_info=None):
-    '''Get the Clickatell notification service.'''
-    return ClickatellNotificationService(config)
-    
-        def __init__(self, hass, filename, add_timestamp):
-        '''Initialize the service.'''
-        self.filepath = os.path.join(hass.config.config_dir, filename)
-        self.add_timestamp = add_timestamp
-    
-                if tname not in self.pbtargets[ttype] and not refreshed:
-                self.refresh()
-                refreshed = True
-    
-            message_doc = {
-            'body': {
-                'version': 1,
-                'type': 'doc',
-                'content':
-                [
-                    panel_text,
-                ]
-                }
-            }
+@pytest.fixture(params=[(python_3, False),
+                        (python_3, True),
+                        (python_2, False)])
+def proc(request, spawnu, TIMEOUT):
+    container, instant_mode = request.param
+    proc = spawnu(*container)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(init_zshrc.format(
+        u'--enable-experimental-instant-mode' if instant_mode else ''))
+    proc.sendline(u'zsh')
+    if instant_mode:
+        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
+    return proc
     
     
+@pytest.mark.parametrize('script, output', [
+    ('apt', invalid_operation('saerch')),
+    ('apt-get', invalid_operation('isntall')),
+    ('apt-cache', invalid_operation('rumove'))])
+def test_match(script, output):
+    assert match(Command(script, output))
     
+    You can download from:
+  https://osxfuse.github.io/
+Error: An unsatisfied requirement failed this build.'''
     
-def conv2d(x, kernel_size, out_channels,
-           act_fn=relu,
-           strides=1,
-           padding='SAME',
-           name=None,
-           reuse=None):
-    '''2-D 卷积层
-    Input shape:  [batch_size, in_h, in_w, in_channels]
-    Output shape: [batch_size, out_h, out_w, out_channels]
+        @classmethod
+    def get_session_store_class(cls):
+        raise NotImplementedError
     
+        The Django sessions framework is entirely cookie-based. It does
+    not fall back to putting session IDs in URLs. This is an intentional
+    design decision. Not only does that behavior make URLs ugly, it makes
+    your site vulnerable to session-ID theft via the 'Referer' header.
     
-'''预定义好的正则化器
-'''
-l1_regularizer = L1L2Regularizer(l1=0.01)
+            assert (mocap_id != -1)
+        sim.data.mocap_pos[mocap_id][:] = sim.data.body_xpos[body_idx]
+        sim.data.mocap_quat[mocap_id][:] = sim.data.body_xquat[body_idx]
+
+    
+            if atexit_register:
+            atexit.register(self.close)
+    
+        def close(self):
+        self.flush()
+        self.closed = True
+    
+        rollout = {}
+    rollout['observations'] = observations_hash
+    rollout['actions'] = actions_hash
+    rollout['rewards'] = rewards_hash
+    rollout['dones'] = dones_hash
+    
+    from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import itchat
