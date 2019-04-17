6a2ee67a94a92c581c87d604f324dfa15384b775
SUNsung <weatherius@gmail.com>
2019-04-17 22:56:24 +0300

    Rewrite [54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py] File in: 2019-04-17 22:56:23

--- pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
+++ pyton/54BOP07aQzvc254Yhqoq4sfyA.54bIaRsCB4QL2548b.py
@@ -1,19 +1,236 @@
 
         
-        def get_orig_video_source(api_key, pid, secret):
-    parsed = json.loads(get_content_headered(tmpl_api_call_video_info % (api_key, pid, secret)))
-    for stream in parsed['streams']['stream']:
-        if stream['type'] == 'orig':
-            return stream['_content'].replace('\\', '')
-    return None
+            bug_text = re.search(
+        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
+    dev_text = re.search(
+        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
+        readme).group(1)
     
-    site_info = 'Giphy.com'
-download = giphy_download
-download_playlist = playlist_not_supported('giphy')
+    make_valid_template = '''
+    @classmethod
+    def _make_valid_url(cls):
+        return {valid_url!r}
+'''
+    
+    
+def _mkdir(d):
+    if not os.path.exists(d):
+        os.mkdir(d)
+    
+    
+class TestIqiyiSDKInterpreter(unittest.TestCase):
+    def test_iqiyi_sdk_interpreter(self):
+        '''
+        Test the functionality of IqiyiSDKInterpreter by trying to log in
+    
+    Latin5BulgarianModel = {
+  'char_to_order_map': Latin5_BulgarianCharToOrderMap,
+  'precedence_matrix': BulgarianLangModel,
+  'typical_positive_ratio': 0.969392,
+  'keep_english_letter': False,
+  'charset_name': 'ISO-8859-5',
+  'language': 'Bulgairan',
+}
+    
+            total = sum(self._freq_counter)
+        if total < 0.01:
+            confidence = 0.0
+        else:
+            confidence = ((self._freq_counter[3] - self._freq_counter[1] * 20.0)
+                          / total)
+        if confidence < 0.0:
+            confidence = 0.0
+        # lower the confidence of latin1 so that other more accurate
+        # detector can take priority.
+        confidence = confidence * 0.73
+        return confidence
 
     
-        return re.search(patt, page).group(1)
+        @property
+    def language(self):
+        raise NotImplementedError
     
-    from ..common import *
-from ..extractor import VideoExtractor
-from .universal import *
+    from .enums import MachineState
+    
+        def __init__(self, model, reversed=False, name_prober=None):
+        super(SingleByteCharSetProber, self).__init__()
+        self._model = model
+        # TRUE if we need to reverse every pair in the model lookup
+        self._reversed = reversed
+        # Optional auxiliary prober for name decision
+        self._name_prober = name_prober
+        self._last_order = None
+        self._seq_counters = None
+        self._total_seqs = None
+        self._total_char = None
+        self._freq_char = None
+        self.reset()
+    
+        def reset(self):
+        super(SingleByteCharSetProber, self).reset()
+        # char order of last character
+        self._last_order = 255
+        self._seq_counters = [0] * SequenceLikelihood.get_num_categories()
+        self._total_seqs = 0
+        self._total_char = 0
+        # characters that fall in our sampling range
+        self._freq_char = 0
+    
+    
+import codecs
+import logging
+import re
+    
+    
+def init():
+    global distances, press_times
+    distances = []
+    press_times = []
+    
+        def adb_path(self):
+        return self.adb_path
+
+    
+        return parser.parse_args()
+    
+    if sys.version_info.major != 3:
+    print('请使用Python3')
+    exit(1)
+try:
+    from common import debug, config, screenshot, UnicodeStreamFilter
+    from common.auto_adb import auto_adb
+except Exception as ex:
+    print(ex)
+    print('请将脚本放在项目根目录中运行')
+    print('请检查项目根目录中的 common 文件夹是否存在')
+    exit(1)
+adb = auto_adb()
+VERSION = '1.1.4'
+    
+            for j in range(int(board_x_start), int(board_x_end)):
+            pixel = im_pixel[j, i]
+            # 修掉脑袋比下一个小格子还高的情况的 bug
+            if abs(j - piece_x) < piece_body_width:
+                continue
+    
+    
+def set_button_position(im, gameover=0):  # 重设点击位置 再来一局位置
+    w, h = im.size
+    if h // 16 > w // 9 + 2:  # 长窄屏 2px容差 获取ui描绘的高度
+        uih = int(w / 9 * 16)
+    else:
+        uih = h
+    # uiw = int(uih / 16 * 9)
+    
+        # Deal with forward many-to-many relationships.
+    for field in [field for field in opts.many_to_many if field.serialize]:
+        forward_relations[field.name] = RelationInfo(
+            model_field=field,
+            related_model=field.remote_field.model,
+            to_many=True,
+            # manytomany do not have to_fields
+            to_field=None,
+            has_through_model=(
+                not field.remote_field.through._meta.auto_created
+            ),
+            reverse=False
+        )
+    
+    import pytest
+from django.test import TestCase
+from django.utils.timezone import utc
+    
+        def handle(self, *args, **options):
+        username = options['username']
+        reset_token = options['reset_token']
+    
+        return get_schema_view(
+        title=title,
+        url=schema_url,
+        description=description,
+        renderer_classes=renderer_classes,
+        public=public,
+        patterns=patterns,
+        generator_class=generator_class,
+        authentication_classes=authentication_classes,
+        permission_classes=permission_classes,
+    )
+    
+    DEFAULT_HPARAMS = tf_utils.merge_hparams(
+    audio_transform.DEFAULT_AUDIO_TRANSFORM_HPARAMS,
+    tf.contrib.training.HParams(
+        eval_batch_size=1,
+        predict_batch_size=1,
+        sample_rate=16000,
+        spec_type='mel',
+        spec_mel_htk=True,
+        spec_log_amplitude=True,
+        spec_hop_length=512,
+        spec_n_bins=229,
+        spec_fmin=30.0,  # A0
+        cqt_bins_per_octave=36,
+        truncated_length_secs=0,
+        max_expected_train_example_len=0,
+        onset_length=32,
+        offset_length=32,
+        onset_mode='length_ms',
+        onset_delay=0,
+        min_frame_occupancy_for_label=0.0,
+        jitter_amount_ms=0,
+        min_duration_ms=0,
+        backward_shift_amount_ms=0))
+    
+      frame_metrics = infer_util.frame_metrics(
+      frame_labels=frame_labels, frame_predictions=processed_frame_predictions)
+    
+      Raises:
+    ValueError: When an unknown onset_mode is supplied.
+    
+        expected_sequence = copy.copy(self.note_sequence)
+    testing_lib.add_track_to_sequence(
+        expected_sequence, 0, [(10, 100, 0.02, 20.0), (11, 55, 0.44, 1.0),
+                               (38, 45, 5., 7.), (53, 120, 8.0, 8.02),
+                               (50, 99, 9.5, 10.0)])
+    expected_sequence.tempos[0].qpm = 30.
+    
+              # Infer beat-aligned chords (only for relative timing).
+          try:
+            chord_inference.infer_chords_for_sequence(
+                performance_sequence,
+                chord_change_prob=0.25,
+                chord_note_concentration=50.0,
+                add_key_signatures=True)
+          except chord_inference.ChordInferenceError:
+            Metrics.counter('extract_examples', 'chord_inference_failed').inc()
+            continue
+    
+        # The generator should emit all zeros, as only a single sequence is ever
+    # considered so the counter doesn't reach one.
+    self.assertEqual(sequence, [0, 0, 0, 0, 0])
+    self.assertEqual(state, 32)
+    self.assertEqual(score, 0)
+    
+        remaining_time = time.time() - wake_time
+    # Enter critical section for updating the offset.
+    with self._lock:
+      # Only update if the current offset value is what was used in this call.
+      if self.offset == offset_:
+        offset_delta = (offset_ - Sleeper._MIN_OFFSET) / 2
+        if remaining_time > 0:
+          self.offset -= offset_delta
+        elif remaining_time < -Sleeper._MIN_OFFSET:
+          self.offset += offset_delta
+    
+      def testSleeper_SleepUntil(self):
+    # Burn in.
+    for _ in range(10):
+      concurrency.Sleeper().sleep(.01)
+    
+        # Broadcast if needed.
+    if b_enc.shape[0] == 1 != batch_size:
+      b_enc = tf.tile(b_enc, [batch_size, 1])
+    if b_dec.shape[0] == 1 != batch_size:
+      b_dec = tf.tile(b_dec, [batch_size, 1])
+    
+    
+class StateUtilTest(tf.test.TestCase):
