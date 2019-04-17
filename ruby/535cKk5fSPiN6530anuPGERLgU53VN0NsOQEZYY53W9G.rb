cddecc1921f7b6cc5f718b4ec047cc3c6c341d06
SUNsung <weatherius@gmail.com>
2019-04-17 19:21:37 +0300

    Rewrite [535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb] File in: 2019-04-17 19:21:36

--- ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
+++ ruby/535cKk5fSPiN6530anuPGERLgU53VN0NsOQEZYY53W9G.rb
@@ -1,36 +1,25 @@
 
         
-          if config.log_to.include? 'file'
-    # Configure an appender that will write log events to a file.
-    if AppConfig.environment.logging.logrotate.enable?
-      # The file will be rolled on a daily basis, and the rolled files will be kept
-      # the configured number of days. Older files will be deleted. The default pattern
-      # layout is used when formatting log events into strings.
-      Logging.appenders.rolling_file('file',
-                                     filename:      config.paths['log'].first,
-                                     keep:          AppConfig.environment.logging.logrotate.days.to_i,
-                                     age:           'daily',
-                                     truncate:      false,
-                                     auto_flushing: true,
-                                     layout:        layout
-                                    )
-    else
-      # No file rolling, use logrotate to roll the logfile.
-      Logging.appenders.file('file',
-                             filename:      config.paths['log'].first,
-                             truncate:      false,
-                             auto_flushing: true,
-                             layout:        layout
-                            )
-    end
-  end
+              context 'rvm option is present' do
+        before do
+          allow(project).to receive_messages(rbenv?: false)
+          allow(project).to \
+            receive_message_chain(:yaml, :[]).and_return('ruby-2.0.0-p247')
+        end
     
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
-    end
+          def template
+        asset_path 'template.erb'
+      end
+    
+        context '$EDITOR is not set' do
+      before do
+        allow(ENV).to receive(:[]).with('EDITOR') { nil }
+      end
+    
+          it 'returns three panes' do
+        expect(window.panes).to all be_a_pane.with(
+          project: project, tab: window
+        )
+    
+        package_name 'tmuxinator' \
+      unless Gem::Version.create(Thor::VERSION) < Gem::Version.create('0.18')
