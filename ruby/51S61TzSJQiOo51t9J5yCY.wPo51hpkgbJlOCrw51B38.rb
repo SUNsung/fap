a167ec8ae1eb9ff042c720ec12a71888107e0f71
SUNsung <weatherius@gmail.com>
2019-04-18 00:02:07 +0300

    Rewrite [51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb] File in: 2019-04-18 00:02:06

--- ruby/51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb
+++ ruby/51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb
@@ -1,51 +1,49 @@
 
         
-        # Just a slash
-Benchmark.ips do |x|
-  path = '/'
-  x.report('pre_pr:#{path}')    { pre_pr(path) }
-  x.report('pr:#{path}')        { pr(path) }
-  x.report('envygeeks:#{path}') { pr(path) }
-  x.compare!
-end
-    
-    def local_require
-  require 'json'
-  JSON.pretty_generate(DATA)
-end
-    
-    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
-COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
-    
-    # No trailing slash
-Benchmark.ips do |x|
-  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
-  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
-  x.compare!
-end
-
-    
-      p.action do |args, _|
-    if args.empty?
-      Jekyll.logger.error 'A subcommand is required.'
-      puts p
-      abort
-    else
-      subcommand = args.first
-      unless p.has_command? subcommand
-        Jekyll.logger.abort_with 'fatal: 'jekyll #{args.first}' could not' \
-          ' be found. You may need to install the jekyll-#{args.first} gem' \
-          ' or a related gem to be able to use this subcommand.'
-      end
-    end
-  end
-end
-
-    
-        # Loops through the list of category pages and processes each one.
-    def write_category_indexes
-      if self.layouts.key? 'category_index'
-        dir = self.config['category_dir'] || 'categories'
-        self.categories.keys.each do |category|
-          self.write_category_index(File.join(dir, category.to_url), category)
+              context 'when f.lib or f.include is a directory' do
+        it 'gives command to be run when f.lib is a directory' do
+          Pathname.new(f.lib).mkpath
+          expect(caveats).to include('-L#{f.opt_lib}')
         end
+    
+        def byte_to_str_pos(pos)
+      @s.string.byteslice(0, pos).length
+    end
+    
+          I18N_PLURAL_MANY_COUNT = 2.1
+      def plural_resource_name(resource_class)
+        resource_class.model_name.human(count: I18N_PLURAL_MANY_COUNT)
+      end
+    
+          # the per_page_dropdown is used on index pages like orders, products, promotions etc.
+      # this method generates the select_tag
+      def per_page_dropdown
+        # there is a config setting for admin_products_per_page, only for the orders page
+        if @products && per_page_default = Spree::Config.admin_products_per_page
+          per_page_options = []
+          5.times do |amount|
+            per_page_options << (amount + 1) * Spree::Config.admin_products_per_page
+          end
+        else
+          per_page_default = Spree::Config.admin_orders_per_page
+          per_page_options = %w{15 30 45 60}
+        end
+    
+          # Insure checkbox still checked
+      expect(find('#q_considered_risky_eq')).to be_checked
+      # Insure we have the risky order, R100
+      within_row(1) do
+        expect(page).to have_content('R100')
+      end
+      # Insure the non risky order is not present
+      expect(page).not_to have_content('R200')
+    end
+    
+      context 'with only read permissions' do
+    before do
+      allow_any_instance_of(Spree::Admin::BaseController).to receive(:spree_current_user).and_return(nil)
+    end
+    
+              def serialize_order(order)
+            resource_serializer.new(order.reload, include: resource_includes, fields: sparse_fields).serializable_hash
+          end
