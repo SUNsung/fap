ef1611866ad53e585eeda6062c0098d6e848ae1d
SUNsung <weatherius@gmail.com>
2019-04-17 05:34:56 +0300

    Rewrite [00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp] File in: 2019-04-17 05:34:56

--- cccccc/00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp
+++ cccccc/00YashchWMu6I003AYMbi4wBd600hOaXlBZzHgY00cd4.cpp
@@ -1,115 +1,96 @@
 
         
-        // Convert an AttrValue with type `type` to the Python representation for
-// that value.
-string AttrValueToPython(const string& type, const AttrValue& value,
-                         const string& dtype_module = 'tf.');
+        namespace atom {
+    }
     
-    REGISTER_OP('Invalid')
-    .Attr('invalid attr: int32')  // invalid since the name has a space.
-    .Doc(R'doc(
-An op to test that invalid ops do not successfully generate invalid python code.
-)doc');
+      // Delegate implementations.
+  void OnError(const std::string& error) override;
+  void OnError(const std::string& message,
+               const int code,
+               const std::string& domain) override;
+  void OnCheckingForUpdate() override;
+  void OnUpdateAvailable() override;
+  void OnUpdateNotAvailable() override;
+  void OnUpdateDownloaded(const std::string& release_notes,
+                          const std::string& release_name,
+                          const base::Time& release_date,
+                          const std::string& update_url) override;
     
-        http://www.apache.org/licenses/LICENSE-2.0
+     private:
+  atom::RenderProcessPreferences preferences_;
     
-    // Creates a numpy array with shapes specified by dim_size and dims and content
-// in data. The array does not own the memory, and destructor will be called to
-// release it. If the status is not ok the caller is responsible for releasing
-// the memory.
-Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
-                       std::function<void()> destructor, PyObject** result);
+      // display::DisplayObserver:
+  void OnDisplayAdded(const display::Display& new_display) override;
+  void OnDisplayRemoved(const display::Display& old_display) override;
+  void OnDisplayMetricsChanged(const display::Display& display,
+                               uint32_t changed_metrics) override;
     
-    #endif  // TENSORFLOW_PYTHON_LIB_CORE_PY_FUNC_H_
+    #include 'atom/browser/net/about_protocol_handler.h'
+    
+    #endif  // ATOM_BROWSER_NET_URL_REQUEST_ABOUT_JOB_H_
 
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-      bool operator==(const DeviceOptions& other) const {
-    return flags_ == other.flags_;
-  }
-    
-    namespace swift {
-    }
-    
-    #if defined(__APPLE__)
-#include 'Darwin/Cache-Mac.cpp'
-#else
-    
-    void DiverseStackBase::pushNewStorageSlow(std::size_t needed) {
-  bool wasInline = isAllocatedInline();
-    }
-    
-    
-    {    // Must be 'const' or nothing.
-    clang::Qualifiers quals = pointee.getQualifiers();
-    bool isConst = quals.hasConst();
-    quals.removeConst();
-    if (quals.empty()) {
-      if (auto record = pointee->getAs<clang::RecordType>()) {
-        auto recordDecl = record->getDecl();
-        if (recordDecl->hasAttr<clang::ObjCBridgeAttr>() ||
-            recordDecl->hasAttr<clang::ObjCBridgeMutableAttr>() ||
-            recordDecl->hasAttr<clang::ObjCBridgeRelatedAttr>() ||
-            isKnownCFTypeName(typedefDecl->getName())) {
-          return forRecord(isConst, record->getDecl());
-        }
-      } else if (pointee->isVoidType()) {
-        if (typedefDecl->hasAttr<clang::ObjCBridgeAttr>() ||
-            isKnownCFTypeName(typedefDecl->getName())) {
-          return isConst ? forConstVoid() : forVoid();
-        }
-      }
-    }
-  }
-    
-    
-bool Mangle::isNonAscii(StringRef str) {
-  for (unsigned char c : str) {
-    if (c >= 0x80)
-      return true;
-  }
-  return false;
+    const gfx::Rect& OffscreenViewProxy::GetBounds() {
+  return view_bounds_;
 }
     
-    string Status::ToString() const {
-  if (error_code_ == error::OK) {
-    return 'OK';
-  } else {
-    if (error_message_.empty()) {
-      return error::CodeEnumToString(error_code_);
-    } else {
-      return error::CodeEnumToString(error_code_) + ':' +
-          error_message_;
+    int LaunchProgram(const StringVector& relauncher_args,
+                  const StringVector& argv);
+    
+    StringType AddQuoteForArg(const StringType& arg) {
+  // We follow the quoting rules of CommandLineToArgvW.
+  // http://msdn.microsoft.com/en-us/library/17w5ykft.aspx
+  std::wstring quotable_chars(L' \\\'');
+  if (arg.find_first_of(quotable_chars) == std::wstring::npos) {
+    // No quoting necessary.
+    return arg;
+  }
     }
-  }
-}
     
-    // integral_constant, defined in tr1, is a wrapper for an integer
-// value. We don't really need this generality; we could get away
-// with hardcoding the integer type to bool. We use the fully
-// general integer_constant for compatibility with tr1.
+    #endif  // ATOM_BROWSER_UI_DRAG_UTIL_H_
+
     
-    using google::protobuf::io::FileInputStream;
-using google::protobuf::io::GzipInputStream;
     
-    #ifdef _WIN32
-#ifndef STDIN_FILENO
-#define STDIN_FILENO 0
-#endif
-#ifndef STDOUT_FILENO
-#define STDOUT_FILENO 1
-#endif
-#endif
+    {
+    {
+    {}  // namespace python
+}  // namespace protobuf
+}  // namespace google
+#endif  // GOOGLE_PROTOBUF_PYTHON_CPP_SCOPED_PYOBJECT_PTR_H__
+
     
-    class GogoDataStripper : public DataStripper {
- private:
-  virtual bool ShouldBeClear(const FieldDescriptor *field) {
-    return field->type() == FieldDescriptor::TYPE_GROUP;
-  }
-};
+      protobuf_unittest::TestAllTypes message2;
+    
+    
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace strings
+}  // namespace protobuf
+}  // namespace google
+
+    
+    
+    {
+    {
+    {}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
+    
+    
+    {};
+    
+    #include <string>
+#include <set>
+#include <vector>
+#include <google/protobuf/descriptor.h>
+#include <google/protobuf/io/printer.h>
+    
+    #pragma once
+#include 'INarratorAnnouncementHost.h'
+    
+                static void OnVirtualKeyControlShiftChordPropertyChanged(
+                Windows::UI::Xaml::DependencyObject^ target,
+                MyVirtualKey oldValue,
+                MyVirtualKey newValue);
