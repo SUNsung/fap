dc4072ee1cf5fc350ec76f250d67db78b6351588
SUNsung <weatherius@gmail.com>
2019-04-18 01:45:26 +0300

    Rewrite [14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb] File in: 2019-04-18 01:45:25

--- ruby/14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb
+++ ruby/14uAwoLZpzO3o14nJ1rNxsRVHo14wsBqzd1t1OA14Ysk.rb
@@ -1,147 +1,172 @@
 
         
-            def push(*names)
-      @filters.push *filter_const(names)
+              if options[:skip_helpers] == true
+        @used_helpers = @used_routes
+      elsif skip = options[:skip_helpers]
+        @used_helpers = self.routes - Array(skip).map(&singularizer)
+      else
+        @used_helpers = self.routes
+      end
     end
+  end
+end
+
     
-    module Docs
-  class PageDb
-    attr_reader :pages
-    
-        def mime_type
-      headers['Content-Type'] || 'text/plain'
+          if failed_attributes.any?
+        fail Devise::Models::MissingAttribute.new(failed_attributes)
+      end
     end
-    
-            css('.row-margin', '.ng-cloak').each do |node|
-          node.before(node.children).remove
-        end
-    
-            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
-          node.before(node.children).remove
-        end
-    
-      private
     
     module Devise
-  module Controllers
-    # Provide sign in and sign out functionality.
-    # Included by default in all controllers.
-    module SignInOut
-      # Return true if the given scope is signed in session. If no scope given, return
-      # true if any scope is signed in. This will run authentication hooks, which may
-      # cause exceptions to be thrown from this method; if you simply want to check
-      # if a scope has already previously been authenticated without running
-      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
-      def signed_in?(scope=nil)
-        [scope || Devise.mappings.keys].flatten.any? do |_scope|
-          warden.authenticate?(scope: _scope)
-        end
+  module Models
+    # Timeoutable takes care of verifying whether a user session has already
+    # expired or not. When a session expires after the configured time, the user
+    # will be asked for credentials again, it means, they will be redirected
+    # to the sign in page.
+    #
+    # == Options
+    #
+    # Timeoutable adds the following options to devise_for:
+    #
+    #   * +timeout_in+: the interval to timeout the user session without activity.
+    #
+    # == Examples
+    #
+    #   user.timedout?(30.minutes.ago)
+    #
+    module Timeoutable
+      extend ActiveSupport::Concern
+    
+    blk = block_get{11}
+test_ok(blk.class == Proc)
+test_ok(blk.to_proc.class == Proc)
+test_ok(blk.clone.call == 11)
+test_ok(block_get(&blk).class == Proc)
+    
+          if t > 0.0 and t < isect.t then
+        isect.t = t
+        isect.hit = true
+        isect.pl = Vec.new(ray.org.x + ray.dir.x * t,
+                          ray.org.y + ray.dir.y * t,
+                          ray.org.z + ray.dir.z * t)
+        n = isect.pl.vsub(@center)
+        isect.n = n.vnormalize
+      else
+        0.0
       end
-    
-          # Stores the provided location to redirect the user after signing in.
-      # Useful in combination with the `stored_location_for` helper.
-      #
-      # Example:
-      #
-      #   store_location_for(:user, dashboard_path)
-      #   redirect_to user_facebook_omniauth_authorize_path
-      #
-      def store_location_for(resource_or_scope, location)
-        session_key = stored_location_key_for(resource_or_scope)
-        
-        path = extract_path_from_location(location)
-        session[session_key] = path if path
-      end
-    
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-        def default_path_names(options)
-      @path_names = Hash.new { |h,k| h[k] = k.to_s }
-      @path_names[:registration] = ''
-      @path_names.merge!(options[:path_names]) if options[:path_names]
     end
+    nil
+  end
+end
     
-            # Generate a token checking if one does not already exist in the database.
-        def remember_token #:nodoc:
-          loop do
-            token = Devise.friendly_token
-            break token unless to_adapter.find_first({ remember_token: token })
-          end
+    def piece(n, a, nb)
+  nb.each{|x|
+    a[n] = x
+    if n == NP-1
+      $p << [a.sort]
+    else
+      nbc=nb.dup
+      [-ROW, -1, 1, ROW].each{|d|
+        if x+d > 0 and not a.include?(x+d) and not nbc.include?(x+d)
+          nbc << x+d
         end
+      }
+      nbc.delete x
+      piece(n+1,a[0..n],nbc)
+    end
+  }
+end
     
-          def timeout_in
-        self.class.timeout_in
-      end
-    
-            if Utils::HttpClient.remote_file_exist?(uri)
-          PluginManager.ui.debug('Found package at: #{uri}')
-          return LogStash::PluginManager::PackInstaller::Remote.new(uri)
-        else
-          PluginManager.ui.debug('Package not found at: #{uri}')
-          return nil
-        end
-      rescue SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
-        # This probably means there is a firewall in place of the proxy is not correctly configured.
-        # So lets skip this strategy but log a meaningful errors.
-        PluginManager.ui.debug('Network error, skipping Elastic pack, exception: #{e}')
-    
-          # Install the gems to make them available locally when bundler does his local resolution
-      post_install_messages = []
-      pack.gems.each do |packed_gem|
-        PluginManager.ui.debug('Installing, #{packed_gem.name}, version: #{packed_gem.version} file: #{packed_gem.file}')
-        post_install_messages << LogStash::PluginManager::GemInstaller::install(packed_gem.file, packed_gem.plugin?)
-      end
-    
-        puts('Unpacking #{package_file}')
-    
-          def footer
-        if @footer.nil?
-          if page = @page.footer
-            @footer = page.text_data
-          else
-            @footer = false
-          end
-        end
-        @footer
-      end
-    
-    MiniTest::Reporters.use!
-    
-      test 'extract destination file name in case of path renaming' do
-    view = Precious::Views::LatestChanges.new
-    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
-    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
+        system_command '#{staged_path}/AdobePatchInstaller.app/Contents/MacOS/AdobePatchInstaller',
+                   args: [
+                           '--mode=silent',
+                         ],
+                   sudo: true
   end
     
-    if options[:irb]
-  require 'irb'
-  # http://jameskilton.com/2009/04/02/embedding-irb-into-your-ruby-application/
-  module IRB # :nodoc:
-    def self.start_session(binding)
-      unless @__initialized
-        args = ARGV
-        ARGV.replace(ARGV.dup)
-        IRB.setup(nil)
-        ARGV.replace(args)
-        @__initialized = true
+      gem.required_ruby_version = '>= 2.0'
+  gem.add_dependency 'airbrussh', '>= 1.0.0'
+  gem.add_dependency 'i18n'
+  gem.add_dependency 'rake', '>= 10.0.0'
+  gem.add_dependency 'sshkit', '>= 1.9.0'
+    
+    Given(/^(\d+) valid existing releases$/) do |num|
+  a_day = 86_400 # in seconds
+  offset = -(a_day * num.to_i)
+  num.to_i.times do
+    run_vagrant_command('mkdir -p #{TestApp.release_path(TestApp.timestamp(offset))}')
+    offset += a_day
+  end
+end
+    
+      def exists?(type, path)
+    %Q{[ -#{type} '#{path}' ]}
+  end
+    
+          def set(key, value=nil, &block)
+        @trusted_keys << key if trusted? && !@trusted_keys.include?(key)
+        remember_location(key)
+        values[key] = block || value
+        trace_set(key)
+        values[key]
       end
     
-        module PsychAutoload
-      def resolve_class(klass_name)
-        return nil if !klass_name || klass_name.empty?
-        # constantize
-        names = klass_name.split('::')
-        names.shift if names.empty? || names.first.empty?
+    @@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset='utf-8' />
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+  </head>
+  <body><%= yield %></body>
+</html>
     
-        UnitOfWork = Struct.new(:queue, :job) do
-      def acknowledge
-        # nothing to do
+    class Rack::Builder
+  include Sinatra::Delegator
+end
+
+    
+            elsif masked_token?(token)
+          token = unmask_token(token)
+    
+        class << self
+      def jobs # :nodoc:
+        Queues.jobs_by_queue.values.flatten
       end
     
-          def constantize(str)
-        names = str.split('::')
-        names.shift if names.empty? || names.first.empty?
+    # Example for adding special attributes
+package.attributes[:deb_group] = 'super-useful'
+package.attributes[:rpm_group] = 'super-useful'
+    
+        return [osname, osversion, arch, wordsize].join(':')
+  end
+    
+        platforms.each do |platform|
+      logger.info('Generating service manifest.', :platform => platform.class.name)
+      platform.program = command.first
+      platform.name = attributes[:pleaserun_name]
+      platform.args = command[1..-1]
+      platform.description = if attributes[:description_given?]
+        attributes[:description]
+      else
+        platform.name
+      end
+      pleaserun_attributes.each do |attribute_name|
+        attribute = 'pleaserun_#{attribute_name}'.to_sym
+        if attributes.has_key?(attribute) and not attributes[attribute].nil?
+          platform.send('#{attribute_name}=', attributes[attribute])
+        end
+      end
+    
+      option '--downcase-name', :flag, 'Should the target package name be in ' \
+    'lowercase?', :default => true
+  option '--downcase-dependencies', :flag, 'Should the package dependencies ' \
+    'be in lowercase?', :default => true
+    
+      def architecture
+    case @architecture
+    when nil, 'native'
+      @architecture = %x{uname -p}.chomp
+    end
+    # 'all' is a valid arch according to
+    # http://www.bolthole.com/solaris/makeapackage.html
