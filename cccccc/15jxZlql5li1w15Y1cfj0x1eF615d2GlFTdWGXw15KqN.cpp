e25c4f85b67dedddc19f1b6444f946f5ddca4506
SUNsung <weatherius@gmail.com>
2019-04-17 09:43:59 +0300

    Rewrite [15jxZlql5li1w15Y1cfj0x1eF615d2GlFTdWGXw15KqN.cpp] File in: 2019-04-17 09:43:58

--- cccccc/15jxZlql5li1w15Y1cfj0x1eF615d2GlFTdWGXw15KqN.cpp
+++ cccccc/15jxZlql5li1w15Y1cfj0x1eF615d2GlFTdWGXw15KqN.cpp
@@ -1,162 +1,202 @@
 
         
-        #include 'tensorflow/python/lib/core/numpy.h'
+        
+    {}  // namespace atom
+
+    
+    class AutoUpdater : public mate::EventEmitter<AutoUpdater>,
+                    public auto_updater::Delegate,
+                    public WindowListObserver {
+ public:
+  static mate::Handle<AutoUpdater> Create(v8::Isolate* isolate);
+    }
     
     
-    {  VLOG(2) << 'building executor';
-  port::StatusOr<std::unique_ptr<StreamExecutor>> result = factory();
-  if (!result.ok()) {
-    VLOG(2) << 'failed to get build executor: ' << result.status();
-    // If construction failed, leave the cache Entry around, but with a null
-    // executor.
-    return result.status();
+    {  DISALLOW_COPY_AND_ASSIGN(RenderProcessPreferences);
+};
+    
+    
+    {}  // namespace auto_updater
+
+    
+    void OffScreenOutputDevice::OnPaint(const gfx::Rect& damage_rect) {
+  gfx::Rect rect = damage_rect;
+  if (!pending_damage_rect_.IsEmpty()) {
+    rect.Union(pending_damage_rect_);
+    pending_damage_rect_.SetRect(0, 0, 0, 0);
   }
-  entry->configurations.emplace_back(config, std::move(result.ValueOrDie()));
-  return entry->configurations.back().second.get();
-}
-    
-      // Creates a 2d FFT plan with scratch allocator.
-  virtual std::unique_ptr<Plan> Create2dPlanWithScratchAllocator(
-      Stream *stream, uint64 num_x, uint64 num_y, Type type, bool in_place_fft,
-      ScratchAllocator *scratch_allocator) = 0;
-    
-    
-    
-    void Base::CallSync(const std::string& method,
-                    const base::ListValue& arguments,
-                    base::ListValue* result) {
-  NOTREACHED() << 'Uncatched callAsync in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
-}
-    
-    #ifndef CONTENT_NW_SRC_API_BASE_BASE_H_
-#define CONTENT_NW_SRC_API_BASE_BASE_H_
-    
-    void Clipboard::SetText(std::string& text) {
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  ui::Clipboard::ObjectMap map;
-  map[ui::Clipboard::CBF_TEXT].push_back(
-      std::vector<char>(text.begin(), text.end()));
-  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
-}
-    
-    
-    {}  // namespace nwapi
-    
-    class BaseEvent {
-  friend class EventListener;
-  DISALLOW_COPY_AND_ASSIGN(BaseEvent);
-    }
-    
-        bool ReadRTF(ClipboardData& data) {
-      DCHECK(data.type == TYPE_RTF);
-      std::string text;
-      clipboard_->ReadRTF(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
-      data.data.reset(new std::string(text));
-      return true;
     }
     
     
-    {    struct KeypointStore {
-        virtual void push(f32 kpX, f32 kpY, f32 kpSize, f32 kpAngle=-1, f32 kpResponse=0, s32 kpOctave=0, s32 kpClass_id=-1) = 0;
-        virtual ~KeypointStore() {};
-    };
-}
+    {}  // namespace atom
+
     
-    void absDiff(const Size2D &size,
-             const f32 * src0Base, ptrdiff_t src0Stride,
-             const f32 * src1Base, ptrdiff_t src1Stride,
-             f32 * dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, AbsDiff<f32>());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
-}
+      // Notify the parent process that it can quit now.
+  StringType name = internal::GetWaitEventName(process.Pid());
+  base::win::ScopedHandle wait_event(
+      ::CreateEventW(NULL, TRUE, FALSE, name.c_str()));
+  ::SetEvent(wait_event.Get());
     
-    #else
+    REGISTER_CPU_OPERATOR(
+    MergeMultiListFeatureTensors,
+    MergeMultiListFeatureTensorsOp<CPUContext>);
+REGISTER_CPU_OPERATOR(
+    MergeMultiListFeatureTensorsGradient,
+    MergeMultiListOrMapFeatureTensorsGradientOp<CPUContext>);
+OPERATOR_SCHEMA(MergeMultiListFeatureTensors)
+    .SetDoc(
+        'Merge given multi-feature tensors with list features into one.' + doc)
+    .NumInputs([](int n) { return n >= 4 && n % 4 == 0; })
+    .NumOutputs(4)
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_keys', '.keys')
+    .Input(2, 'in1_values_lengths', '.values.lengths')
+    .Input(3, 'in1_values_values', '.values.values')
+    .Output(0, 'out_lengths', '.lengths')
+    .Output(1, 'out_keys', '.keys')
+    .Output(2, 'out_values_lengths', '.values.lengths')
+    .Output(3, 'out_values_values', '.values.values');
+OPERATOR_SCHEMA(MergeMultiListFeatureTensorsGradient)
+    .SetDoc(
+        'Explode given multi-feature tensors with list features '
+        'into many.' +
+        doc)
+    .NumInputs([](int n) { return n >= 3 && n % 2 == 1; })
+    .NumOutputs([](int n) { return n >= 1; })
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_values_lengths', '.values.lengths')
+    .Input(2, 'out_values_values_grad', '.values.values_grad')
+    .Output(0, 'in1_values_values_grad', '.values.values_grad');
     
-             int16x8_t vline1_s16 = vmovl_s8(vget_low_s8(vline_s8));
-         int16x8_t vline2_s16 = vmovl_s8(vget_high_s8(vline_s8));
+    Example 1:
+  DATA  = [1, 2, 3, 4, 5, 6, 7, 8]
+  RANGES = [
+    [
+      [2, 4],
+      [0, 2],
+    ],
+    [
+      [0, 0],
+      [6, 2],
+    ]
+  ]
+  lengths = [4, 2]
+  OUTPUT[0] = [[3, 4, 5, 6], [0, 0, 0, 0]]
+  OUTPUT[1] = [[1, 2], [7, 8]]
     
-            if (cpolicy == CONVERT_POLICY_SATURATE)
-        {
-            for (; j < roiw16; j += 16)
-            {
-                internal::prefetch(src + j);
-                int16x8_t v_src0 = vshrq_n_s16(vld1q_s16(src + j), shift),
-                          v_src1 = vshrq_n_s16(vld1q_s16(src + j + 8), shift);
-                uint8x16_t v_dst = vcombine_u8(vqmovun_s16(v_src0),
-                                               vqmovun_s16(v_src1));
-                vst1q_u8(dst + j, v_dst);
-            }
-            for (; j < roiw8; j += 8)
-            {
-                int16x8_t v_src = vshrq_n_s16(vld1q_s16(src + j), shift);
-                vst1_u8(dst + j, vqmovun_s16(v_src));
-            }
-    }
+    OPERATOR_SCHEMA(GivenTensorFill)
+    .NumInputs(0, 1)
+    .NumOutputs(1)
+    .AllowInplace({{0, 0}})
+    .SetDoc(R'DOC(
+This op fills an output tensor with the data specified by the *value* and *dtype* arguments.  The output tensor shape is specified by the *shape* argument. Beware, when using this argument *value* should have a value for every element of the *output*, as missing values will not be initialized automatically. If *input_as_shape* is set to *true*, then the *input* should be a 1D tensor containing the desired output shape (the dimensions specified in *extra_shape* will also be appended). In this case, the *shape* argument should **not** be set.
     
-                uint8x8_t vsrc = vld1_u8(src + j);
+              const int dkernel_h = dilation_h * (kernel_h - 1) + 1;
+          const int dkernel_w = dilation_w * (kernel_w - 1) + 1;
+          CAFFE_ENFORCE(H >= dkernel_h);
+          CAFFE_ENFORCE(W >= dkernel_w);
+          const int out_h = (H + 2 * pad - dkernel_h) / stride_h + 1;
+          const int out_w = (W + 2 * pad - dkernel_w) / stride_w + 1;
     
-    #ifndef B2_TRIANGLE_H
-#define B2_TRIANGLE_H
     
-    		void InitFromEtcEncodingBits(Image::Format a_imageformat,
-										unsigned int a_uiSourceH,
-										unsigned int a_uiSourceV,
-										unsigned char *a_paucEncodingBits,
-										Image *a_pimageSource,
-										ErrorMetric a_errormetric);
-    
-    			ColorFloatRGBA frgbaSumLL = afrgbaAlphaWeightedSource[2] +
-										afrgbaAlphaWeightedSource[3] +
-										afrgbaAlphaWeightedSource[6] +
-										afrgbaAlphaWeightedSource[7];
-    
-      FT_LOCAL( void )
-  af_latin_metrics_init_widths( AF_LatinMetrics  metrics,
-                                FT_Face          face );
+    {} // namespace caffe2
     
     
     {
-    {        //test for duplicate points and collinear edges ...
-        if ((pp->Pt == pp->Next->Pt) || (pp->Pt == pp->Prev->Pt) ||
-            (SlopesEqual(pp->Prev->Pt, pp->Pt, pp->Next->Pt, m_UseFullRange) &&
-            (!preserveCol || !Pt2IsBetweenPt1AndPt3(pp->Prev->Pt, pp->Pt, pp->Next->Pt))))
-        {
-            lastOK = 0;
-            OutPt *tmp = pp;
-            pp->Prev->Next = pp->Next;
-            pp->Next->Prev = pp->Prev;
-            pp = pp->Prev;
-            delete tmp;
-        }
-        else if (pp == lastOK) break;
-        else
-        {
-            if (!lastOK) lastOK = pp;
-            pp = pp->Next;
-        }
+    {        throw invalid_argument('the input has no solution');
     }
-    outrec.Pts = pp;
+};
+    
+    
+    {            record[nums[i]] = i;
+        }
+    
+            ListNode* p = dummyHead;
+        while(p->next && p->next->next){
+            ListNode* node1 = p->next;
+            ListNode* node2 = node1->next;
+            ListNode* next = node2->next;
+            node2->next = node1;
+            node1->next = next;
+            p->next = node2;
+            p = node1;
+        }
+    
+    
+void printArr(const vector<int>& vec){
+    for(int e: vec)
+        cout << e << ' ';
+    cout << endl;
 }
-//------------------------------------------------------------------------------
     
-    #endif /* HAVE_ARM_NE10 */
+    /// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
     
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
+    // Another Classic Non-Recursive algorithm for preorder traversal
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+    }
+    
+    // Recursive
+// Time Complexity: O(n), n is the node number in the tree
+// Space Complexity: O(h), h is the height of the tree
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+    }
+    }
+    
+    
+    {
+    {}}
+    
+    #include 'hphp/ppc64-asm/asm-ppc64.h'
+#include 'hphp/ppc64-asm/decoded-instr-ppc64.h'
+#include 'hphp/ppc64-asm/decoder-ppc64.h'
+#include 'hphp/runtime/base/runtime-option.h'
+#include 'hphp/util/trace.h'
+#include <folly/MicroSpinLock.h>
+    
+          MDS_form_t mds_formater {{
+                               rc,
+                               xop,
+                               mb,
+                               static_cast<uint32_t>(rb),
+                               static_cast<uint32_t>(ra),
+                               static_cast<uint32_t>(rs),
+                               op
+                              }};
+    
+    private:
+  APCCollection();
+  ~APCCollection();
+  static APCHandle::Pair WrapArray(APCHandle::Pair, CollectionType);
+    
+    struct DataStreamWrapper final : Stream::Wrapper {
+  DataStreamWrapper() {
+    m_isLocal = true;
+  }
+    }
+    
+    #define ERROR_RAISE_WARNING(exp)        \
+  int ret = (exp);                      \
+  if (ret != 0) {                       \
+    raise_warning(                      \
+      '%s(): %s',                       \
+      __FUNCTION__,                     \
+      folly::errnoStr(errno).c_str()    \
+    );                                  \
+  }                                     \
+    
+    #ifndef incl_HPHP_PIPE_H_
+#define incl_HPHP_PIPE_H_
+    
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
