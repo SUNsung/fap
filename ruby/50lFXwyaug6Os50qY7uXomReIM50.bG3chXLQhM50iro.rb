04dc045b93b7a29de038076c8ad549c51059279d
SUNsung <weatherius@gmail.com>
2019-04-18 09:04:27 +0300

    Rewrite [50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb] File in: 2019-04-18 09:04:26

--- ruby/50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb
+++ ruby/50lFXwyaug6Os50qY7uXomReIM50.bG3chXLQhM50iro.rb
@@ -1,36 +1,72 @@
 
         
-        describe 'Kernel.sleep' do
-  it 'needs to be reviewed for spec completeness'
-end
+            def as_json
+      @pages
+    end
+    
+                case platform
+            when 'iOS' then self.platform :ios, '10.0'
+            when 'macOS' then self.platform :macos, '10.10'
+            end
+    
+            def run
+          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
+          if @pod_name.nil? # Print all
+            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
+              print_pod_cache_infos(pod_name, cache_descriptors)
+            end
+          else # Print only for the requested pod
+            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
+            if cache_descriptors.nil?
+              UI.notice('No cache for pod named #{@pod_name} found')
+            else
+              print_pod_cache_infos(@pod_name, cache_descriptors)
+            end
+          end
+        end
+    
+            def listen
+          while repl_command = STDIN.gets
+            execute_repl_command(repl_command)
+          end
+        end
+    
+    # This stuff needs to be run after Paperclip is defined.
+require 'paperclip/io_adapters/registry'
+require 'paperclip/io_adapters/abstract_adapter'
+require 'paperclip/io_adapters/empty_string_adapter'
+require 'paperclip/io_adapters/identity_adapter'
+require 'paperclip/io_adapters/file_adapter'
+require 'paperclip/io_adapters/stringio_adapter'
+require 'paperclip/io_adapters/data_uri_adapter'
+require 'paperclip/io_adapters/nil_adapter'
+require 'paperclip/io_adapters/attachment_adapter'
+require 'paperclip/io_adapters/uploaded_file_adapter'
+require 'paperclip/io_adapters/uri_adapter'
+require 'paperclip/io_adapters/http_url_proxy_adapter'
 
     
-      it 'raises a TypeError when passed nil' do
-    lambda { srand(nil) }.should raise_error(TypeError)
-  end
+        def initialize
+      clear
+    end
     
-          def title
-        'Latest Changes (Globally)'
+        # Returns a String describing the file's content type
+    def detect
+      if blank_name?
+        SENSIBLE_DEFAULT
+      elsif empty_file?
+        EMPTY_TYPE
+      elsif calculated_type_matches.any?
+        calculated_type_matches.first
+      else
+        type_from_file_contents || SENSIBLE_DEFAULT
+      end.to_s
+    end
+    
+        # Swaps the height and width if necessary
+    def auto_orient
+      if EXIF_ROTATED_ORIENTATION_VALUES.include?(@orientation)
+        @height, @width = @width, @height
+        @orientation -= 4
       end
-    
-          def custom_path
-        '#{@base_url}#{@page_dir.empty? ? '' : '/'}#{@page_dir}'
-      end
-    
-      def app
-    Precious::App
-  end
-end
-    
-      test 'create sets the correct path for a relative path subdirectory' do
-    dir  = 'foodir'
-    name = '#{dir}/bar'
-    get '/create/#{name}'
-    assert_match(/\/#{dir}/, last_response.body)
-    assert_no_match(/[^\/]#{dir}/, last_response.body)
-  end
-    
-      class DuplicatePageError < Error
-    attr_accessor :dir
-    attr_accessor :existing_path
-    attr_accessor :attempted_path
+    end
