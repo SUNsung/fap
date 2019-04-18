572417b0c184af84ebf70393e73f91c0218599af
SUNsung <weatherius@gmail.com>
2019-04-18 15:55:39 +0300

    Rewrite [12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp] File in: 2019-04-18 15:55:38

--- cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
+++ cccccc/12F.9VYDFxRQs123ptEoS0uAY12qSkujQeeJss1240g.cpp
@@ -1,251 +1,144 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    
-    {}  // namespace tensorflow
-
-    
-    void ImportNumpy() {
-  import_array1();
-}
-    
-    #endif  // TENSORFLOW_PYTHON_LIB_CORE_SAFE_PTR_H_
-
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #endif  // TENSORFLOW_STREAM_EXECUTOR_DEVICE_OPTIONS_H_
-
-    
-    namespace {
+        grpc::string ChannelArguments::GetSslTargetNameOverride() const {
+  for (unsigned int i = 0; i < args_.size(); i++) {
+    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
+      return args_[i].value.string;
     }
-    
-    void BrowserWindow::UpdateDraggableRegions(
-    content::RenderFrameHost* rfh,
-    const std::vector<DraggableRegion>& regions) {
-  if (window_->has_frame())
-    return;
-  static_cast<NativeWindowViews*>(window_.get())
-      ->UpdateDraggableRegions(DraggableRegionsToSkRegion(regions));
-}
-    
-    
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(Net);
-};
-    
-    
-    {}  // namespace auto_updater
-    
-    #include 'atom/browser/net/about_protocol_handler.h'
-    
-      // net::URLRequestJobFactory::ProtocolHandler:
-  net::URLRequestJob* MaybeCreateJob(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const override;
-  bool IsSafeRedirectTarget(const GURL& location) const override;
-    
-    void URLRequestAboutJob::Start() {
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(&URLRequestAboutJob::StartAsync,
-                                weak_ptr_factory_.GetWeakPtr()));
-}
-    
-      // PID 1 identifies init. launchd, that is. launchd never starts the
-  // relauncher process directly, having this parent_pid means that the parent
-  // already exited and launchd 'inherited' the relauncher as its child.
-  // There's no reason to synchronize with launchd.
-  if (parent_pid == 1) {
-    LOG(ERROR) << 'unexpected parent_pid';
-    return;
   }
-    
-      int id() const { return id_; }
-  std::string extension_id_;
-  ObjectManager* object_manager() const { return object_manager_.get(); }
-    
-    // Tell browser to delete a object.
-// function DeallocateObject(id);
-v8::Handle<v8::Value> DeallocateObject(int routing_id,
-                                       int object_id);
-    
-    
-#include 'content/nw/src/api/event/event.h'
-#include 'base/values.h'
-#include 'content/nw/src/api/dispatcher_host.h'
-#include 'ui/gfx/screen.h'
-    
-    bool MenuDelegate::HasIcon(int command_id) {
-  if (command_id < 0)
-    return false;
-    }
-    
-     private:
-  ObjectManager* object_manager_;
-    
-    void Menu::Insert(MenuItem* menu_item, int pos) {
-  std::vector<MenuItem*>::iterator begin = menu_items.begin();
-  menu_items.insert(begin+pos,menu_item);
-  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
-    menu_item->UpdateKeys(gtk_accel_group);
-  }
-  gtk_menu_shell_insert(GTK_MENU_SHELL(menu_), menu_item->menu_item_, pos);
+  return '';
 }
     
-    NwObjCallObjectMethodSyncFunction::NwObjCallObjectMethodSyncFunction() {
+    void CensusClientCallData::Destroy(grpc_call_element* elem,
+                                   const grpc_call_final_info* final_info,
+                                   grpc_closure* then_call_closure) {
+  const uint64_t request_size = GetOutgoingDataSize(final_info);
+  const uint64_t response_size = GetIncomingDataSize(final_info);
+  double latency_ms = absl::ToDoubleMilliseconds(absl::Now() - start_time_);
+  ::opencensus::stats::Record(
+      {{RpcClientSentBytesPerRpc(), static_cast<double>(request_size)},
+       {RpcClientReceivedBytesPerRpc(), static_cast<double>(response_size)},
+       {RpcClientRoundtripLatency(), latency_ms},
+       {RpcClientServerLatency(),
+        ToDoubleMilliseconds(absl::Nanoseconds(elapsed_time_))},
+       {RpcClientSentMessagesPerRpc(), sent_message_count_},
+       {RpcClientReceivedMessagesPerRpc(), recv_message_count_}},
+      {{ClientMethodTagKey(), method_},
+       {ClientStatusTagKey(), StatusCodeToString(final_info->final_status)}});
+  grpc_slice_unref_internal(path_);
+  context_.EndSpan();
 }
-    
-    
-    {
-    {
-    {}  // namespace io
-}  // namespace protobuf
-}  // namespace google
-
-    
-    // integral_constant, defined in tr1, is a wrapper for an integer
-// value. We don't really need this generality; we could get away
-// with hardcoding the integer type to bool. We use the fully
-// general integer_constant for compatibility with tr1.
-    
-    #include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-    
-        std::unique_ptr<google::protobuf::io::ZeroCopyOutputStream> output(
-        context->Open(basename + '.proto'));
-    string content = GetPool()->BuildFile(new_file)->DebugString();
-    Printer printer(output.get(), '$');
-    printer.WriteRaw(content.c_str(), content.size());
-    
-    void ReversePolygon(float32* x, float32* y, int n);
-    
-    	void EncodeMipmaps(float *a_pafSourceRGBA,
-		unsigned int a_uiSourceWidth,
-		unsigned int a_uiSourceHeight,
-		Image::Format a_format,
-		ErrorMetric a_eErrMetric,
-		float a_fEffort,
-		unsigned int a_uiJobs,
-		unsigned int a_uiMaxJobs,
-		unsigned int a_uiMaxMipmaps,
-		unsigned int a_uiMipFilterFlags,
-		RawImage* a_pMipmapImages,
-		int *a_piEncodingTime_ms, 
-		bool a_bVerboseOutput)
-	{
-		auto mipWidth = a_uiSourceWidth;
-		auto mipHeight = a_uiSourceHeight;
-		int totalEncodingTime = 0;
-		for(unsigned int mip = 0; mip < a_uiMaxMipmaps && mipWidth >= 1 && mipHeight >= 1; mip++)
-		{
-			float* pImageData = nullptr;
-			float* pMipImage = nullptr;
-    }
-    }
-    
-    namespace Etc
-{
-	class Block4x4EncodingBits;
-    }
-    
-    namespace Etc
-{
-	class Block4x4;
-    }
-    
-    		static const unsigned int BYTES_PER_BLOCK = 16;
-		static const unsigned int SELECTOR_BYTES = 12;
-    
-    
-#endif /* AFBLUE_H_ */
-    
-    void MinkowskiSum(const Path& pattern, const Path& path, Paths& solution, bool pathIsClosed);
-void MinkowskiSum(const Path& pattern, const Paths& paths, Paths& solution, bool pathIsClosed);
-void MinkowskiDiff(const Path& poly1, const Path& poly2, Paths& solution);
-    
-    /** Multiply a 16-bit signed value by a 16-bit unsigned value. The result is a 32-bit signed value */
-#define MULT16_16SU(a,b) ((opus_val32)(opus_val16)(a)*(opus_val32)(opus_uint16)(b))
-    
-       - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in the
-   documentation and/or other materials provided with the distribution.
-    
-    
-    {}  // namespace grpc
-
     
     #include <grpc/support/port_platform.h>
     
-      grpc_error* Init(grpc_call_element* elem,
-                   const grpc_call_element_args* args) override;
+    MeasureInt64 RpcClientSentMessagesPerRpc() {
+  static const auto measure =
+      MeasureInt64::Register(kRpcClientSentMessagesPerRpcMeasureName,
+                             'Number of messages sent per RPC', kCount);
+  return measure;
+}
     
-    Status ProtoServerReflection::GetFileContainingExtension(
-    ServerContext* context, const ExtensionRequest* request,
+    Status ProtoServerReflection::GetFileContainingSymbol(
+    ServerContext* context, const grpc::string& symbol,
     ServerReflectionResponse* response) {
   if (descriptor_pool_ == nullptr) {
     return Status::CANCELLED;
   }
     }
     
-    namespace grpc {
+    std::unique_ptr<ServerBuilderOption> MakeChannelArgumentOption(
+    const grpc::string& name, const grpc::string& value) {
+  class StringOption final : public ServerBuilderOption {
+   public:
+    StringOption(const grpc::string& name, const grpc::string& value)
+        : name_(name), value_(value) {}
+    }
     }
     
-    void PerHostStore::ReportStreamClosed(const grpc::string& lb_id) {
-  auto it_store_for_gone_lb = per_balancer_stores_.find(lb_id);
-  GPR_ASSERT(it_store_for_gone_lb != per_balancer_stores_.end());
-  // Remove this closed stream from our records.
-  GPR_ASSERT(UnorderedMapOfSetEraseKeyValue(
-      load_key_to_receiving_lb_ids_, it_store_for_gone_lb->second->load_key(),
-      lb_id));
-  std::set<PerBalancerStore*> orphaned_stores =
-      UnorderedMapOfSetExtract(assigned_stores_, lb_id);
-  // The stores that were assigned to this balancer are orphaned now. They
-  // should be re-assigned to other balancers which are still receiving reports.
-  for (PerBalancerStore* orphaned_store : orphaned_stores) {
-    const grpc::string* new_receiver = nullptr;
-    auto it = load_key_to_receiving_lb_ids_.find(orphaned_store->load_key());
-    if (it != load_key_to_receiving_lb_ids_.end()) {
-      // First, try to pick from the active balancers with the same load key.
-      new_receiver = RandomElement(it->second);
-    } else if (!assigned_stores_.empty()) {
-      // If failed, pick from all the remaining active balancers.
-      new_receiver = &(RandomElement(assigned_stores_)->first);
+    #endif  // !GRPC_CUSTOM_DEFAULT_THREAD_POOL
+
+    
+    
+    {}  // namespace
+    
+    ThreadPoolInterface* CreateDefaultThreadPool();
+    
+    bool __CCCallFuncO::initWithTarget(Ref* selectorTarget, SEL_CallFuncO selector, Ref* object)
+{
+    if (CallFunc::initWithTarget(selectorTarget))
+    {
+        _object = object;
+        CC_SAFE_RETAIN(_object);
+        
+        _callFuncO = selector;
+        return true;
     }
-    if (new_receiver != nullptr) {
-      AssignOrphanedStore(orphaned_store, *new_receiver);
-    } else {
-      // Load data for an LB ID that can't be assigned to any stream should
-      // be dropped.
-      orphaned_store->Suspend();
-    }
-  }
+    
+    return false;
 }
     
-    #include <grpc/support/log.h>
+        
+    /** Gets the duration in seconds of the whole animation. It is the result of totalDelayUnits * delayPerUnit.
+     *
+     * @return Result of totalDelayUnits * delayPerUnit.
+     */
+    float getDuration() const;
     
-        // output element with JSON pointer '/number'
-    std::cout << j.at('/number'_json_pointer) << '\n';
-    // output element with JSON pointer '/string'
-    std::cout << j.at('/string'_json_pointer) << '\n';
-    // output element with JSON pointer '/array'
-    std::cout << j.at('/array'_json_pointer) << '\n';
-    // output element with JSON pointer '/array/1'
-    std::cout << j.at('/array/1'_json_pointer) << '\n';
+    /** Gets the array of AnimationFrames.
+     * 
+     * @return The array of AnimationFrames.
+     */
+    const Vector<AnimationFrame*>& getFrames() const { return _frames; };
     
+    /** Sets the array of AnimationFrames. 
+     *
+     * @param frames The array of AnimationFrames.
+     */
+    void setFrames(const Vector<AnimationFrame*>& frames)
+    {
+        _frames = frames;
+    }
     
-    {    std::cout << null << '\n';
-    std::cout << *res2.first << ' ' << std::boolalpha << res2.second << '\n';
-}
+    /** Checks whether to restore the original frame when animation finishes. 
+     *
+     * @return Restore the original frame when animation finishes.
+     */
+    bool getRestoreOriginalFrame() const { return _restoreOriginalFrame; };
+    
+    /** Sets whether to restore the original frame when animation finishes. 
+     *
+     * @param restoreOriginalFrame Whether to restore the original frame when animation finishes.
+     */
+    void setRestoreOriginalFrame(bool restoreOriginalFrame) { _restoreOriginalFrame = restoreOriginalFrame; };
+    
+    /** Gets the times the animation is going to loop. 0 means animation is not animated. 1, animation is executed one time, ... 
+     *
+     * @return The times the animation is going to loop.
+     */
+    unsigned int getLoops() const { return _loops; };
+    
+    /** Sets the times the animation is going to loop. 0 means animation is not animated. 1, animation is executed one time, ... 
+     *
+     * @param loops The times the animation is going to loop.
+     */
+    void setLoops(unsigned int loops) { _loops = loops; };
+    
+    // overrides
+    virtual Animation *clone() const override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    Animation();
+    virtual ~Animation(void);
+    
+    /** Initializes a Animation. */
+    bool init();
+    
+    /** Initializes a Animation with frames and a delay between frames.
+     * @since v0.99.5
+     */
+    bool initWithSpriteFrames(const Vector<SpriteFrame*>& arrayOfSpriteFrameNames, float delay = 0.0f, unsigned int loops = 1);
+    
+    /** Initializes a Animation with AnimationFrame.
+     * @since v2.0
+     */
+    bool initWithAnimationFrames(const Vector<AnimationFrame*>& arrayOfAnimationFrameNames, float delayPerUnit, unsigned int loops);
