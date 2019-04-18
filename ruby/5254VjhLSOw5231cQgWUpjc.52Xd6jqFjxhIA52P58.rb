a9c715847f539df398b59006e9862d79576d8aae
SUNsung <weatherius@gmail.com>
2019-04-19 01:18:21 +0300

    Rewrite [5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb] File in: 2019-04-19 01:18:20

--- ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
+++ ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
@@ -1,29 +1,193 @@
 
         
-              context 'when file paths contain spaces' do
-        it 'escapes spaces in output and config file paths' do
-          output_file = 'output path with spaces.txt'
-          config_file = '.config file with spaces.yml'
-          result = Fastlane::FastFile.new.parse('lane :test do
-            swiftlint(
-              output_file: '#{output_file}',
-              config_file: '#{config_file}'
-            )
-          end').runner.execute(:test)
+        # Just a slash
+Benchmark.ips do |x|
+  path = '/'
+  x.report('pre_pr:#{path}')    { pre_pr(path) }
+  x.report('pr:#{path}')        { pr(path) }
+  x.report('envygeeks:#{path}') { pr(path) }
+  x.compare!
+end
     
-      def unpin
-    path.unlink if pinned?
-    HOMEBREW_PINNED_KEGS.rmdir_if_possible
+    def graceful_require
+  Jekyll::External.require_with_graceful_fail('json')
+  JSON.pretty_generate(DATA)
+end
+    
+      </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+    
+    Mercenary.program(:jekyll) do |p|
+  p.version Jekyll::VERSION
+  p.description 'Jekyll is a blog-aware, static site generator in Ruby'
+  p.syntax 'jekyll <subcommand> [options]'
+    
+            redirect_to admin_report_path(@report), notice: I18n.t('admin.report_notes.created_msg')
+      else
+        @report_notes = @report.notes.latest
+        @report_history = @report.history
+        @form = Form::StatusBatch.new
+    
+      def status_finder
+    StatusFinder.new(params[:url])
   end
     
-        it 'returns an array of flags' do
-      expect(subject.flags_only).to eq ['--foo']
+      def local_domain?
+    TagManager.instance.web_domain?(hub_topic_domain)
+  end
+    
+      def update
+    if verify_payload?
+      process_salmon
+      head 202
+    elsif payload.present?
+      render plain: signature_verification_failure_reason, status: 401
+    else
+      head 400
     end
   end
     
-      it 'returns the pipeline id' do
-    expect(subject.pipeline_id).to eq(pipeline_id)
+      def show
+    if subscription.valid?(params['hub.topic'])
+      @account.update(subscription_expires_at: future_expires)
+      render plain: encoded_challenge, status: 200
+    else
+      head 404
+    end
   end
     
-    module ActiveAdmin
-  module BatchActions
+    class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  skip_before_action :verify_authenticity_token
+    
+      def load_export
+    @export = Export.new(current_account)
+  end
+    
+          it 'warns about brew failing under tmux' do
+        f = formula do
+          url 'foo-1.0'
+          def plist
+            'plist_test.plist'
+          end
+        end
+        allow(ENV).to receive(:[]).with('TMUX').and_return(true)
+        allow(Homebrew).to receive(:_system).with('/usr/bin/pbpaste').and_return(false)
+        caveats = described_class.new(f).caveats
+    
+      #
+  # Move these into an IPMI stack or mixin at some point
+  #
+    
+                checksum = OpenSSL::HMAC.digest('MD5', k1, data_encrypt)
+    
+              # Rex::Proto::Kerberos::Model::ApReq decoding isn't supported
+          #
+          # @raise [NotImplementedError]
+          def decode(input)
+            raise ::NotImplementedError, 'AP-REQ decoding not supported'
+          end
+    
+              # Decodes the Rex::Proto::Kerberos::Model::Element from the input. This
+          # method has been designed to be overridden by subclasses.
+          #
+          # @raise [NoMethodError]
+          def decode(input)
+            raise ::NoMethodError, 'Method designed to be overridden'
+          end
+    
+              # Encodes the sname
+          #
+          # @return [String]
+          def encode_sname
+            sname.encode
+          end
+    
+              # Decodes a Rex::Proto::Kerberos::Model::LastReque from an String
+          #
+          # @param input [String] the input to decode from
+          def decode_string(input)
+            asn1 = OpenSSL::ASN1.decode(input)
+    
+        def role_properties_for(names, &block)
+      servers.role_properties_for(names, &block)
+    end
+    
+          def value_or_default
+        if response.empty?
+          default
+        else
+          response
+        end
+      end
+    
+            def to_h
+          @properties
+        end
+    
+          def remember_location(key)
+        location = caller.find do |line|
+          IGNORED_LOCATIONS.none? { |i| line.include?(i) }
+        end
+        (locations[key] ||= []) << location
+      end
+    
+      test 'extract destination file name in case of path renaming' do
+    view = Precious::Views::LatestChanges.new
+    assert_equal 'newname.md', view.extract_renamed_path_destination('oldname.md => newname.md')
+    assert_equal 'newDirectoryName/fileName.md', view.extract_renamed_path_destination('{oldDirectoryName => newDirectoryName}/fileName.md')
+  end
+    
+      File.write('lib/#{name}.rb', old_file)
+    
+      begin
+    require 'gollum-lib'
+    wiki = Gollum::Wiki.new(gollum_path, wiki_options)
+    if !wiki.exist? then
+      raise Gollum::InvalidGitRepositoryError
+    end
+    if wiki_options[:plantuml_url]
+      Gollum::Filter::PlantUML.configure do |config|
+        puts 'Using #{wiki_options[:plantuml_url]} as PlantUML endpoint'
+        config.url = wiki_options[:plantuml_url]
+      end
+    end
+    puts
+    puts 'Loaded Gollum wiki at:'
+    puts '#{File.expand_path(gollum_path).inspect}'
+    puts
+    puts 'Example API calls:'
+    puts %(    page = wiki.page('page-name'))
+    puts %(    # => <Gollum::Page>)
+    puts
+    puts %(    page.raw_data)
+    puts %(    # => '# My wiki page')
+    puts
+    puts %(    page.formatted_data)
+    puts %(    # => '<h1>My wiki page</h1>')
+    puts
+    puts 'Full API documentation at:'
+    puts 'https://github.com/gollum/gollum-lib'
+    puts
+    IRB.start_session(binding)
+  rescue Gollum::InvalidGitRepositoryError, Gollum::NoSuchPathError
+    puts 'Invalid Gollum wiki at #{File.expand_path(gollum_path).inspect}'
+    exit 0
+  end
+else
+  require 'gollum/app'
+  Precious::App.set(:gollum_path, gollum_path)
+  Precious::App.set(:wiki_options, wiki_options)
+  Precious::App.settings.mustache[:templates] = wiki_options[:template_dir] if wiki_options[:template_dir]
