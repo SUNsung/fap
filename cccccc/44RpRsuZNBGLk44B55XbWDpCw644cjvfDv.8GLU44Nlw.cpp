cb4716e37cf9cf01699c49b711a314fac87fd5dd
SUNsung <weatherius@gmail.com>
2019-04-19 12:41:00 +0300

    Rewrite [44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp] File in: 2019-04-19 12:40:59

--- cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
+++ cccccc/44RpRsuZNBGLk44B55XbWDpCw644cjvfDv.8GLU44Nlw.cpp
@@ -1,179 +1,131 @@
 
         
-        #define TEGRA_XOR(src1, sz1, src2, sz2, dst, sz, w, h) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-    CAROTENE_NS::bitwiseXor(CAROTENE_NS::Size2D(w, h), \
-                     src1, sz1, \
-                     src2, sz2, \
-                     dst, sz), \
-    CV_HAL_ERROR_OK \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
+        #ifndef GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
+#define GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CHANNEL_FILTER_H
     
-        void thresholdToZero(const Size2D &size,
-                         const s32 *srcBase, ptrdiff_t srcStride,
-                         s32 *dstBase, ptrdiff_t dstStride,
-                         s32 threshold);
+      static void OnDoneRecvTrailingMetadataCb(void* user_data, grpc_error* error);
     
-    void add(const Size2D &size,
-         const u8 * src0Base, ptrdiff_t src0Stride,
-         const s16 * src1Base, ptrdiff_t src1Stride,
-         s16 *dstBase, ptrdiff_t dstStride,
-         CONVERT_POLICY policy)
+    #include <grpc/support/port_platform.h>
+    
+        virtual void UpdateArguments(ChannelArguments* args) override {
+      args->SetString(name_, value_);
+    }
+    virtual void UpdatePlugins(
+        std::vector<std::unique_ptr<ServerBuilderPlugin>>* plugins) override {}
+    
+    #ifndef GRPC_INTERNAL_CPP_UTIL_CORE_STATS_H
+#define GRPC_INTERNAL_CPP_UTIL_CORE_STATS_H
+    
+    void Assembler::li64TOC(const Reg64& rt, int64_t imm64, ImmType /*immt*/,
+                        bool immMayChange) {
+  int64_t TOCoffset;
+  TOCoffset = VMTOC::getInstance().pushElem(imm64, immMayChange);
+    }
+    
+    APCHandle::Pair APCCollection::WrapArray(APCHandle::Pair inner,
+                                         CollectionType colType) {
+  auto const col = new APCCollection;
+  col->m_arrayHandle = inner.handle;
+  col->m_colType = colType;
+  return { &col->m_handle, inner.size + sizeof(APCCollection) };
+}
+    
+      Object createObject() const;
+    
+    
+    {}
+    
+    
+    { private:
+  int mkdir_recursive(const String& path, int mode);
+};
+    
+    
+    {///////////////////////////////////////////////////////////////////////////////
+}
+    
+    
+    {}
+    
+    
+    {        // Apply multiply operator
+        if (cfg.RasterizerMultiply != 1.0f)
+        {
+            unsigned char multiply_table[256];
+            ImFontAtlasBuildMultiplyCalcLookupTable(multiply_table, cfg.RasterizerMultiply);
+            stbrp_rect* r = &src_tmp.Rects[0];
+            for (int glyph_i = 0; glyph_i < src_tmp.GlyphsCount; glyph_i++, r++)
+                if (r->was_packed)
+                    ImFontAtlasBuildMultiplyRectAlpha8(multiply_table, atlas->TexPixelsAlpha8, r->x, r->y, r->w, r->h, atlas->TexWidth * 1);
+        }
+        src_tmp.Rects = NULL;
+    }
+    
+    IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window, bool install_callbacks);
+IMGUI_IMPL_API bool     ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window, bool install_callbacks);
+IMGUI_IMPL_API void     ImGui_ImplGlfw_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplGlfw_NewFrame();
+    
+        // Cleanup
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    
+    // About GLSL version:
+//  The 'glsl_version' initialization parameter should be NULL (default) or a '#version XXX' string.
+//  On computer platform the GLSL version default to '#version 130'. On OpenGL ES 3 platform it defaults to '#version 300 es'
+//  Only override if your GL version doesn't handle this GLSL version. See GLSL version table at the top of imgui_impl_opengl3.cpp.
+    
+    Action::Action()
+:_originalTarget(nullptr)
+,_target(nullptr)
+,_tag(Action::INVALID_TAG)
+,_flags(0)
 {
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    size_t roiw16 = size.width >= 15 ? size.width - 15 : 0;
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
-    
-                    tprev = tcurr;
-                tcurr = tnext;
-                tnext = vaddw_u8(vaddl_u8(x0, x1), x2);
-    
-    namespace CAROTENE_NS {
-    }
-    
-    namespace CAROTENE_NS {
-    }
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 7 && !defined(__clang__)
-CVTS_FUNC(s32, u16, 8,
-    register float32x4_t vscale asm ('q0') = vdupq_n_f32((f32)alpha);
-    register float32x4_t vshift asm ('q1') = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        __asm__ (
-            'vld1.32 {d4-d5}, [%[src1]]                             \n\t'
-            'vld1.32 {d6-d7}, [%[src2]]                             \n\t'
-            'vcvt.f32.s32 q4, q2                                    \n\t'
-            'vcvt.f32.s32 q5, q3                                    \n\t'
-            'vmul.f32 q6, q4, q0                                    \n\t'
-            'vmul.f32 q7, q5, q0                                    \n\t'
-            'vadd.f32 q8, q6, q1                                    \n\t'
-            'vadd.f32 q9, q7, q1                                    \n\t'
-            'vcvt.s32.f32 q10, q8                                   \n\t'
-            'vcvt.s32.f32 q11, q9                                   \n\t'
-            'vqmovun.s32 d24, q10                                   \n\t'
-            'vqmovun.s32 d25, q11                                   \n\t'
-            'vst1.16 {d24-d25}, [%[dst]]                            \n\t'
-            : /*no output*/
-            : [src1] 'r' (_src + i + 0),
-              [src2] 'r' (_src + i + 4),
-              [dst] 'r' (_dst + i),
-              'w'  (vscale), 'w' (vshift)
-            : 'd4','d5','d6','d7','d8','d9','d10','d11','d12','d13','d14','d15','d16','d17','d18','d19','d20','d21','d22','d23','d24','d25'
-        );
-    }
-})
-#else
-CVTS_FUNC(s32, u16, 8,
-    float32x4_t vscale = vdupq_n_f32((f32)alpha);
-    float32x4_t vshift = vdupq_n_f32((f32)beta + 0.5f);,
-{
-    for (size_t i = 0; i < w; i += 8)
-    {
-        internal::prefetch(_src + i);
-        int32x4_t vline1_s32 = vld1q_s32(_src + i + 0);
-        int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
-        float32x4_t vline1_f32 = vcvtq_f32_s32(vline1_s32);
-        float32x4_t vline2_f32 = vcvtq_f32_s32(vline2_s32);
-        vline1_f32 = vmulq_f32(vline1_f32, vscale);
-        vline2_f32 = vmulq_f32(vline2_f32, vscale);
-        vline1_f32 = vaddq_f32(vline1_f32, vshift);
-        vline2_f32 = vaddq_f32(vline2_f32, vshift);
-        vline1_s32 = vcvtq_s32_f32(vline1_f32);
-        vline2_s32 = vcvtq_s32_f32(vline2_f32);
-        uint16x4_t vRes1 = vqmovun_s32(vline1_s32);
-        uint16x4_t vRes2 = vqmovun_s32(vline2_s32);
-        vst1q_u16(_dst + i, vcombine_u16(vRes1, vRes2));
-    }
-})
+#if CC_ENABLE_SCRIPT_BINDING
+    ScriptEngineProtocol* engine = ScriptEngineManager::getInstance()->getScriptEngine();
+    _scriptType = engine != nullptr ? engine->getScriptType() : kScriptTypeNone;
 #endif
+}
+    
+        /** 
+     * Creates the action with X Y Z factor.
+     * @param duration Duration time, in seconds.
+     * @param sx Scale factor of x.
+     * @param sy Scale factor of y.
+     * @param sz Scale factor of z.
+     * @return An autoreleased ScaleBy object.
+     */
+    static ScaleBy* create(float duration, float sx, float sy, float sz);
     
     
-    {} // namespace CAROTENE_NS
-
+    {    setTile(pos, coords);
+}
     
-                t0 = vextq_s16(tprev, tcurr, 7);
-            t2 = vextq_s16(tcurr, tnext, 1);
     
-    using namespace CalculatorApp::Common::Automation;
-using namespace Platform;
-using namespace Windows::UI::Xaml;
-using namespace Windows::UI::Xaml::Automation;
-using namespace Windows::UI::Xaml::Automation::Peers;
+    {    //turn the result into simply polygon (AKA, fix overlap)
     
-            static NarratorAnnouncement^ GetAnnouncement(Windows::UI::Xaml::DependencyObject^ element)
-        {
-            return safe_cast<NarratorAnnouncement^>(element->GetValue(s_announcementProperty));
-        }
+    //clamp into the specified rect
+    ClipperLib::Clipper cl;
+    cl.StrictlySimple(true);
+    cl.AddPath(p->Contour, ClipperLib::ptSubject, true);
+    //create the clipping rect
+    ClipperLib::Path clamp;
+    clamp.push_back(ClipperLib::IntPoint(0, 0));
+    clamp.push_back(ClipperLib::IntPoint(rect.size.width/_scaleFactor * PRECISION, 0));
+    clamp.push_back(ClipperLib::IntPoint(rect.size.width/_scaleFactor * PRECISION, rect.size.height/_scaleFactor * PRECISION));
+    clamp.push_back(ClipperLib::IntPoint(0, rect.size.height/_scaleFactor * PRECISION));
+    cl.AddPath(clamp, ClipperLib::ptClip, true);
+    cl.Execute(ClipperLib::ctIntersection, out);
     
-            if (id.compare(L'sGrouping') == 0)
-        {
-            // The following groupings are the onces that CalcEngine supports.
-            //   0;0             0x000          - no grouping
-            //   3;0             0x003          - group every 3 digits
-            //   3;2;0           0x023          - group 1st 3 and then every 2 digits
-            //   4;0             0x004          - group every 4 digits
-            //   5;3;2;0         0x235          - group 5, then 3, then every 2
-            wstring numberGroupingString = localizationSettings.GetNumberGroupingStr();
-            return numberGroupingString;
-        }
-    
-                static void OnVirtualKeyControlInverseChordPropertyChanged(
-                Windows::UI::Xaml::DependencyObject^ target,
-                MyVirtualKey oldValue,
-                MyVirtualKey newValue);
-    
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-#include 'modules/common/proto/error_code.pb.h'
-#include 'modules/drivers/canbus/can_client/fake/fake_can_client.h'
-#include 'modules/drivers/canbus/can_comm/protocol_data.h'
-    
-      Byte t1(bytes + 5);
-  int32_t t = t1.get_byte(5, 3);
-    
-      constraint.AddBoundary(index_list, lower_bound, upper_bound);
-  const auto mat = constraint.inequality_constraint_matrix();
-  const auto bd = constraint.inequality_constraint_boundary();
-    
-      MatrixXd mat_golden = MatrixXd::Zero(10, 10);
-  // clang-format off
-  mat_golden <<
-     10,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0, 10,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0, 10,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0, 10,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-      0,  0,  0,  0,  0,  0,  0,  0,  0,  0;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-    // config detail: {'name': 'torque_output', 'offset': 0.0, 'precision': 0.001,
-// 'len': 32, 'is_signed_var': True, 'physical_range':
-// '[-2147483.648|2147483.647]', 'bit': 7, 'type': 'double', 'order':
-// 'motorola', 'physical_unit': 'N-m'}
-double Brakemotorrpt372::torque_output(const std::uint8_t* bytes,
-                                       int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 8);
+    std::vector<Vec2> outPoints;
+    ClipperLib::PolyNode* p2 = out.GetFirst();
+    while(p2->IsHole()){
+        p2 = p2->GetNext();
     }
-    
-    #include 'glog/logging.h'
-    
-    #include 'modules/canbus/vehicle/gem/protocol/brake_rpt_6c.h'
-    
-    namespace apollo {
-namespace canbus {
-namespace gem {
+    for(const auto& pt : p2->Contour)
+    {
+        outPoints.push_back(Vec2(pt.X/PRECISION, pt.Y/PRECISION));
     }
-    }
-    }
+    return outPoints;
+}
