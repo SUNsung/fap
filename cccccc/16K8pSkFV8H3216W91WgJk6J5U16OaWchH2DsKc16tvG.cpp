235aa634b7b46a86db11cda7e25154e5a5009ce5
SUNsung <weatherius@gmail.com>
2019-04-19 03:50:40 +0300

    Rewrite [16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp] File in: 2019-04-19 03:50:39

--- cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
+++ cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
@@ -1,173 +1,104 @@
 
         
-        #if GTEST_HAS_DEATH_TEST
+        Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
-    #define EXPECT_NONFATAL_FAILURE_ON_ALL_THREADS(statement, substr) \
-  do {\
-    ::testing::TestPartResultArray gtest_failures;\
-    ::testing::internal::SingleFailureChecker gtest_checker(\
-        &gtest_failures, ::testing::TestPartResult::kNonFatalFailure, \
-        (substr));\
-    {\
-      ::testing::ScopedFakeTestPartResultReporter gtest_reporter(\
-          ::testing::ScopedFakeTestPartResultReporter::INTERCEPT_ALL_THREADS, \
-          &gtest_failures);\
-      if (::testing::internal::AlwaysTrue()) { statement; }\
-    }\
-  } while (::testing::internal::AlwaysFalse())
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Creates a numpy array with shapes specified by dim_size and dims and content
+// in data. The array does not own the memory, and destructor will be called to
+// release it. If the status is not ok the caller is responsible for releasing
+// the memory.
+Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
+                       std::function<void()> destructor, PyObject** result);
+    
+        http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    
+            virtual const std::unordered_set<StreamInformation>& StreamInfos() override { return m_streamInfos; }
+    
+    
+    {            // reshape Input(2)
+            Input(2)->SetDims(TensorShape(dimsA), false);
+            fprintf(stderr, '\n%ls %ls operation: For legacy compatibility, the sample layout of third input (%ls %ls operation) was patched to [%s] (from [%s])\n',
+                NodeName().c_str(), OperationName().c_str(), Input(2)->NodeName().c_str(), Input(2)->OperationName().c_str(), string(Input(2)->GetSampleLayout()).c_str(), dimsCstring.c_str());
+        }
+    
+    
+    {                wrongSampleNum += insMatrix(firstSize, secondSize) + delMatrix(firstSize, secondSize) + subMatrix(firstSize, secondSize);
+            }
+    
+        // called from ComputationNode::ValidateInferInputDimsFrom()
+    // In case of an error, this function just backs out without updating.
+    // The caller must verify the dimensions.
+    // This is a bit weird since it is called after this node has been Validated once.
+    // BUGBUG: This will clear out any random initialization to 0. So currently this is not usable for most cases.
+    void InferInputDimsFrom(const TensorShape& otherShape);
+    
+    //////////////////////////////////////////////////////////////////////
+    
+    
+    {}
+    
+      path_str += strlen(prefix);
+  path_len -= strlen(prefix);
+    
+    namespace HPHP {
+    }
+    
+    #include 'hphp/runtime/base/plain-file.h'
+    
+    
+    {  double ret = x * OBJECT_WIDTH_RES;
+  return ret;
+}
+    
+      Byte t1(bytes + 4);
+  int32_t t = t1.get_byte(7, 1);
     
     
     {
-    {}  // namespace internal
-}  // namespace testing
-    
-    #include 'sample1.h'
-    
-    int Water::allocated_ = 0;
-    
-    
-#endif  // GTEST_SAMPLES_SAMPLE2_H_
+    {}  // namespace planning
+}  // namespace apollo
 
     
-    #include <dmlc/registry.h>
-#include <vector>
-#include <utility>
-#include <string>
-#include <functional>
-#include <memory>
-#include './base.h'
-#include './data.h'
-#include './objective.h'
-#include './feature_map.h'
-#include '../../src/common/host_device_vector.h'
+      kernel.AddThirdOrderDerivativeMatrix(init_derivative, init_second_derivative,
+                                       1.0);
     
-    
-    {/*!
- * \brief Macro to register objective function.
- *
- * \code
- * // example of registering a objective
- * XGBOOST_REGISTER_OBJECTIVE(LinearRegression, 'reg:linear')
- * .describe('Linear regression objective')
- * .set_body([]() {
- *     return new RegLossObj(LossType::kLinearSquare);
- *   });
- * \endcode
- */
-#define XGBOOST_REGISTER_OBJECTIVE(UniqueId, Name)                      \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::ObjFunctionReg &              \
-  __make_ ## ObjFunctionReg ## _ ## UniqueId ## __ =                    \
-      ::dmlc::Registry< ::xgboost::ObjFunctionReg>::Get()->__REGISTER__(Name)
-}  // namespace xgboost
-#endif  // XGBOOST_OBJECTIVE_H_
-
-    
-    template<typename StorageIndex>
-class SparsePageLZ4Format : public SparsePageFormat {
+    class Accelrpt68Test : public ::testing::Test {
  public:
-  explicit SparsePageLZ4Format(bool use_lz4_hc)
-      : use_lz4_hc_(use_lz4_hc) {
-    raw_bytes_ = raw_bytes_value_ = raw_bytes_index_ = 0;
-    encoded_bytes_value_ = encoded_bytes_index_ = 0;
-    nthread_ = dmlc::GetEnv('XGBOOST_LZ4_DECODE_NTHREAD', 4);
-    nthread_write_ = dmlc::GetEnv('XGBOOST_LZ4_COMPRESS_NTHREAD', 12);
-  }
-  virtual ~SparsePageLZ4Format() {
-    size_t encoded_bytes = raw_bytes_ +  encoded_bytes_value_ + encoded_bytes_index_;
-    raw_bytes_ += raw_bytes_value_ + raw_bytes_index_;
-    if (raw_bytes_ != 0) {
-      LOG(CONSOLE) << 'raw_bytes=' << raw_bytes_
-                   << ', encoded_bytes=' << encoded_bytes
-                   << ', ratio=' << double(encoded_bytes) / raw_bytes_
-                   << ', ratio-index=' << double(encoded_bytes_index_) /raw_bytes_index_
-                   << ', ratio-value=' << double(encoded_bytes_value_) /raw_bytes_value_;
-    }
-  }
-    }
-    
-    class LambdaRankObjMAP : public LambdaRankObj {
- protected:
-  struct MAPStats {
-    /*! \brief the accumulated precision */
-    float ap_acc;
-    /*!
-     * \brief the accumulated precision,
-     *   assuming a positive instance is missing
-     */
-    float ap_acc_miss;
-    /*!
-     * \brief the accumulated precision,
-     * assuming that one more positive instance is inserted ahead
-     */
-    float ap_acc_add;
-    /* \brief the accumulated positive instance count */
-    float hits;
-    MAPStats() = default;
-    MAPStats(float ap_acc, float ap_acc_miss, float ap_acc_add, float hits)
-        : ap_acc(ap_acc), ap_acc_miss(ap_acc_miss), ap_acc_add(ap_acc_add), hits(hits) {}
-  };
-  /*!
-   * \brief Obtain the delta MAP if trying to switch the positions of instances in index1 or index2
-   *        in sorted triples
-   * \param sorted_list the list containing entry information
-   * \param index1,index2 the instances switched
-   * \param map_stats a vector containing the accumulated precisions for each position in a list
-   */
-  inline bst_float GetLambdaMAP(const std::vector<ListEntry> &sorted_list,
-                                int index1, int index2,
-                                std::vector<MAPStats> *p_map_stats) {
-    std::vector<MAPStats> &map_stats = *p_map_stats;
-    if (index1 == index2 || map_stats[map_stats.size() - 1].hits == 0) {
-      return 0.0f;
-    }
-    if (index1 > index2) std::swap(index1, index2);
-    bst_float original = map_stats[index2].ap_acc;
-    if (index1 != 0) original -= map_stats[index1 - 1].ap_acc;
-    bst_float changed = 0;
-    bst_float label1 = sorted_list[index1].label > 0.0f ? 1.0f : 0.0f;
-    bst_float label2 = sorted_list[index2].label > 0.0f ? 1.0f : 0.0f;
-    if (label1 == label2) {
-      return 0.0;
-    } else if (label1 < label2) {
-      changed += map_stats[index2 - 1].ap_acc_add - map_stats[index1].ap_acc_add;
-      changed += (map_stats[index1].hits + 1.0f) / (index1 + 1);
-    } else {
-      changed += map_stats[index2 - 1].ap_acc_miss - map_stats[index1].ap_acc_miss;
-      changed += map_stats[index2].hits / (index2 + 1);
-    }
-    bst_float ans = (changed - original) / (map_stats[map_stats.size() - 1].hits);
-    if (ans < 0) ans = -ans;
-    return ans;
-  }
-  /*
-   * \brief obtain preprocessing results for calculating delta MAP
-   * \param sorted_list the list containing entry information
-   * \param map_stats a vector containing the accumulated precisions for each position in a list
-   */
-  inline void GetMAPStats(const std::vector<ListEntry> &sorted_list,
-                          std::vector<MAPStats> *p_map_acc) {
-    std::vector<MAPStats> &map_acc = *p_map_acc;
-    map_acc.resize(sorted_list.size());
-    bst_float hit = 0, acc1 = 0, acc2 = 0, acc3 = 0;
-    for (size_t i = 1; i <= sorted_list.size(); ++i) {
-      if (sorted_list[i - 1].label > 0.0f) {
-        hit++;
-        acc1 += hit / i;
-        acc2 += (hit - 1) / i;
-        acc3 += (hit + 1) / i;
-      }
-      map_acc[i - 1] = MAPStats(acc1, acc2, acc3, hit);
-    }
-  }
-  void GetLambdaWeight(const std::vector<ListEntry> &sorted_list,
-                       std::vector<LambdaPair> *io_pairs) override {
-    std::vector<LambdaPair> &pairs = *io_pairs;
-    std::vector<MAPStats> map_stats;
-    GetMAPStats(sorted_list, &map_stats);
-    for (auto & pair : pairs) {
-      pair.weight *=
-          GetLambdaMAP(sorted_list, pair.pos_index,
-                       pair.neg_index, &map_stats);
-    }
-  }
+  virtual void SetUp() {}
 };
+    
+    #include 'glog/logging.h'
+    
+    
+    {  auto &brakerpt = chassis_detail.gem().brake_rpt_6c();
+  EXPECT_DOUBLE_EQ(brakerpt.manual_input(), 0.258);
+  EXPECT_DOUBLE_EQ(brakerpt.commanded_value(), 0.772);
+  EXPECT_DOUBLE_EQ(brakerpt.output_value(), 4.37);
+  EXPECT_EQ(brakerpt.brake_on_off(), Brake_rpt_6c::BRAKE_ON_OFF_ON);
+}
