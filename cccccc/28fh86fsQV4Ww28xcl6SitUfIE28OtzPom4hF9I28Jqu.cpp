339a2a7c03d368ef9eb8d0a9e78c7bc27d9121d3
SUNsung <weatherius@gmail.com>
2019-04-19 09:54:08 +0300

    Rewrite [28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp] File in: 2019-04-19 09:54:07

--- cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
+++ cccccc/28fh86fsQV4Ww28xcl6SitUfIE28OtzPom4hF9I28Jqu.cpp
@@ -1,192 +1,311 @@
 
         
-        
-    {  if (!wasInline) delete[] oldAllocation;
-}  
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+#ifndef TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+#define TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
     
-        if (node->Further || node->Right) {
-      IndentScope ms(this, (childKind == ChildKind::Right ||
-                            childKind == ChildKind::Further ||
-                            childKind == ChildKind::Root) ? '  ' : '| ');
-    }
-    
-      // If the result type matches the context, remove the context type from the
-  // prefix of the name.
-  bool resultTypeMatchesContext = returnsSelf || (resultType == contextType);
-  if (resultTypeMatchesContext) {
-    StringRef newBaseName = omitNeedlessWordsFromPrefix(baseName, contextType,
-                                                        scratch);
-    if (newBaseName != baseName) {
-      baseName = newBaseName;
-      anyChanges = true;
-    }
-  }
+    REGISTER_OP('Ackermann')
+    .Output('ackermann: string')
+    .Doc(R'doc(
+Output a fact about the ackermann function.
+)doc');
     
     
-    {  return GraphemeClusterBreakProperty::Other;
-}
-    
-    bool Mangle::needsPunycodeEncoding(StringRef str) {
-  for (unsigned char c : str) {
-    if (!isValidSymbolChar(c))
-      return true;
-  }
-  return false;
-}
-    
-    
-    {
-    {    switch (Bytes) {
-    case 1:
-      OutUTF8.push_back(S);
-      break;
-    case 2: {
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xC0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      break;
-    }
-    case 3: {
-      uint8_t Byte3 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xE0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      OutUTF8.push_back(Byte3);
-      break;
-    }
-    case 4: {
-      uint8_t Byte4 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte3 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte2 = (S | 0x80) & 0xBF;
-      S >>= 6;
-      uint8_t Byte1 = S | 0xF0;
-      OutUTF8.push_back(Byte1);
-      OutUTF8.push_back(Byte2);
-      OutUTF8.push_back(Byte3);
-      OutUTF8.push_back(Byte4);
-      break;
-    }
-    }
-  }
-  return true;
-}
-    
-    void MergeModuleJobAction::anchor() {}
-    
-    
-    {
-b2Polygon ConvexHull(b2Vec2* v, int nVert);
-b2Polygon ConvexHull(float32* cloudX, float32* cloudY, int32 nVert);
-}
-#endif
+    {}  // namespace tensorflow
 
     
-    #include 'b2Glue.h'
+        http://www.apache.org/licenses/LICENSE-2.0
     
+    // Destructor passed to TF_NewTensor when it reuses a numpy buffer. Stores a
+// pointer to the pyobj in a buffer to be dereferenced later when we're actually
+// holding the GIL. Data and len are ignored.
+void DelayedNumpyDecref(void* data, size_t len, void* obj);
     
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
-    Block4x4Encoding is the abstract base class for the different encoders.  Each encoder targets a 
-particular file format (e.g. ETC1, RGB8, RGBA8, R11)
+      // Computes complex-to-complex FFT in the transform direction as specified
+  // by direction parameter.
+  virtual bool DoFft(Stream *stream, Plan *plan,
+                     const DeviceMemory<std::complex<float>> &input,
+                     DeviceMemory<std::complex<float>> *output) = 0;
+  virtual bool DoFft(Stream *stream, Plan *plan,
+                     const DeviceMemory<std::complex<double>> &input,
+                     DeviceMemory<std::complex<double>> *output) = 0;
     
-    		unsigned int	m_uiEncodingIterations;
-		bool			m_boolDone;						// all iterations have been done
-		ErrorMetric		m_errormetric;
+    #ifndef TENSORFLOW_STREAM_EXECUTOR_HOST_HOST_PLATFORM_ID_H_
+#define TENSORFLOW_STREAM_EXECUTOR_HOST_HOST_PLATFORM_ID_H_
     
+      reqToSyntheticEnvMap = SubstitutionMap::get(reqSig,
+    [selfType, substConcreteType, depth, covariantSelf, &ctx]
+    (SubstitutableType *type) -> Type {
+      // If the conforming type is a class, the protocol 'Self' maps to
+      // the class-constrained 'Self'. Otherwise, it maps to the concrete
+      // type.
+      if (type->isEqual(selfType)) {
+        if (covariantSelf)
+          return GenericTypeParamType::get(/*depth=*/0, /*index=*/0, ctx);
+        return substConcreteType;
+      }
+      // Other requirement generic parameters map 1:1 with their depth
+      // increased appropriately.
+      auto *genericParam = cast<GenericTypeParamType>(type);
+      // In a protocol requirement, the only generic parameter at depth 0
+      // should be 'Self', and all others at depth 1. Anything else is
+      // invalid code.
+      if (genericParam->getDepth() != 1)
+        return Type();
+      auto substGenericParam =
+        GenericTypeParamType::get(depth, genericParam->getIndex(), ctx);
+      return substGenericParam;
+    },
+    [selfType, substConcreteType, conformance, conformanceDC, &ctx](
+        CanType type, Type replacement, ProtocolDecl *proto)
+          -> Optional<ProtocolConformanceRef> {
+      // The protocol 'Self' conforms concretely to the conforming type.
+      if (type->isEqual(selfType)) {
+        ProtocolConformance *specialized = conformance;
+        if (conformance && conformance->getGenericSignature()) {
+          auto concreteSubs =
+            substConcreteType->getContextSubstitutionMap(
+              conformanceDC->getParentModule(), conformanceDC);
+          specialized =
+            ctx.getSpecializedConformance(substConcreteType, conformance,
+                                          concreteSubs);
+        }
+    }
+    }
     
-    {	};
+    using namespace swift;
     
-    				iDRed = iRed2 - iRed1;
-				iDGreen = iGreen2 - iGreen1;
-				iDBlue = iBlue2 - iBlue1;
-    
-      /* The next level is to group blue strings into style-specific sets. */
-    
-        FT_Fixed         org_scale;
-    FT_Pos           org_delta;
-    
-    #define FASTLZ_VERSION 0x000100
-    
-    #if defined(OPUS_ARM_INLINE_EDSP)
-#include 'arm/kiss_fft_armv5e.h'
-#endif
-#if defined(MIPSr1_ASM)
-#include 'mips/kiss_fft_mipsr1.h'
-#endif
-    
-    void opus_fft_neon(const kiss_fft_state *st,
-                   const kiss_fft_cpx *fin,
-                   kiss_fft_cpx *fout);
-    
-    void DHTReplaceNodeTask::sendMessage()
-{
-  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
-  if (!questionableNode) {
-    setFinished(true);
+      DefaultCacheKey CKey(Key, &DCache.CBs);
+  auto Entry = DCache.Entries.find(CKey);
+  if (Entry != DCache.Entries.end()) {
+    if (Entry->second == Value)
+      return;
+    DCache.CBs.keyDestroyCB(Entry->first.Key, nullptr);
+    DCache.CBs.valueReleaseCB(Entry->second, nullptr);
+    DCache.Entries.erase(Entry);
   }
-  else {
-    getMessageDispatcher()->addMessageToQueue(
-        getMessageFactory()->createPingMessage(questionableNode), timeout_,
-        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
+    
+    void PrefixMapKeyPrinter<unsigned char>::print(raw_ostream &out,
+                                               ArrayRef<unsigned char> key) {
+  out << '\'';
+  for (auto byte : key) {
+    if (byte < 16) out << '0';
+    out.write_hex(byte);
+  }
+  out << '\'';
+}
+
+    
+    
+    {  OutLines.append(Lines.begin(), Lines.end());
+}
+
+    
+      bool isImportAsMember() const { return bool(effectiveDC); }
+    
+    std::string Mangle::translateOperator(StringRef Op) {
+  std::string Encoded;
+  for (char ch : Op) {
+    Encoded.push_back(translateOperatorChar(ch));
+  }
+  return Encoded;
+}
+    
+    static bool encodeToUTF8(const std::vector<uint32_t> &Scalars,
+                         std::string &OutUTF8) {
+  for (auto S : Scalars) {
+    if (!isValidUnicodeScalar(S)) {
+      OutUTF8.clear();
+      return false;
+    }
+    if (S >= 0xD800 && S < 0xD880)
+      S -= 0xD800;
+    }
+    }
+    
+    grpc_error* CensusClientCallData::Init(grpc_call_element* elem,
+                                       const grpc_call_element_args* args) {
+  path_ = grpc_slice_ref_internal(args->path);
+  start_time_ = absl::Now();
+  method_ = GetMethod(&path_);
+  qualified_method_ = absl::StrCat('Sent.', method_);
+  GRPC_CLOSURE_INIT(&on_done_recv_message_, OnDoneRecvMessageCb, elem,
+                    grpc_schedule_on_exec_ctx);
+  GRPC_CLOSURE_INIT(&on_done_recv_trailing_metadata_,
+                    OnDoneRecvTrailingMetadataCb, elem,
+                    grpc_schedule_on_exec_ctx);
+  return GRPC_ERROR_NONE;
+}
+    
+    
+    {  RpcServerStatsEncoding() = delete;
+  RpcServerStatsEncoding(const RpcServerStatsEncoding&) = delete;
+  RpcServerStatsEncoding(RpcServerStatsEncoding&&) = delete;
+  RpcServerStatsEncoding operator=(const RpcServerStatsEncoding&) = delete;
+  RpcServerStatsEncoding operator=(RpcServerStatsEncoding&&) = delete;
+};
+    
+    #ifndef GRPC_CUSTOM_DEFAULT_THREAD_POOL
+    
+    #ifndef GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+#define GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+    
+    #ifdef GPR_APPLE
+    
+    
+    {
+    {}  // namespace load_reporter
+}  // namespace grpc
+    
+    void ProtoToCoreStats(const grpc::core::Stats& proto, grpc_stats_data* core) {
+  memset(core, 0, sizeof(*core));
+  for (const auto& m : proto.metrics()) {
+    switch (m.value_case()) {
+      case Metric::VALUE_NOT_SET:
+        break;
+      case Metric::kCount:
+        for (int i = 0; i < GRPC_STATS_COUNTER_COUNT; i++) {
+          if (m.name() == grpc_stats_counter_name[i]) {
+            core->counters[i] = m.count();
+            break;
+          }
+        }
+        break;
+      case Metric::kHistogram:
+        for (int i = 0; i < GRPC_STATS_HISTOGRAM_COUNT; i++) {
+          if (m.name() == grpc_stats_histogram_name[i]) {
+            const auto& h = m.histogram();
+            bool valid = true;
+            if (grpc_stats_histo_buckets[i] != h.buckets_size()) valid = false;
+            for (int j = 0; valid && j < h.buckets_size(); j++) {
+              if (grpc_stats_histo_bucket_boundaries[i][j] !=
+                  h.buckets(j).start()) {
+                valid = false;
+              }
+            }
+            if (!valid) {
+              gpr_log(GPR_ERROR,
+                      'Found histogram %s but shape is different from proto',
+                      m.name().c_str());
+            }
+            for (int j = 0; valid && j < h.buckets_size(); j++) {
+              core->histograms[grpc_stats_histo_start[i] + j] =
+                  h.buckets(j).count();
+            }
+          }
+        }
+        break;
+    }
   }
 }
     
-      virtual void accept(DHTMessageCallback* callback) = 0;
+    // Print contents of a log file. (*func)() is called on every record.
+Status PrintLogContents(Env* env, const std::string& fname,
+                        void (*func)(uint64_t, Slice, WritableFile*),
+                        WritableFile* dst) {
+  SequentialFile* file;
+  Status s = env->NewSequentialFile(fname, &file);
+  if (!s.ok()) {
+    return s;
+  }
+  CorruptionReporter reporter;
+  reporter.dst_ = dst;
+  log::Reader reader(file, &reporter, true, 0);
+  Slice record;
+  std::string scratch;
+  while (reader.ReadRecord(&record, &scratch)) {
+    (*func)(reader.LastRecordOffset(), record, dst);
+  }
+  delete file;
+  return Status::OK();
+}
     
-      // number of nodes
-  READ_CHECK(fp, &temp32, sizeof(temp32));
-  uint32_t numNodes = ntohl(temp32);
-  // 4bytes reserved
-  readBytes(fp, buf, buf.size(), 4);
     
-        peerAnnounceStorage->setTaskQueue(taskQueue.get());
-    peerAnnounceStorage->setTaskFactory(taskFactory.get());
+    {    // Some corruption was detected.  'size' is the approximate number
+    // of bytes dropped due to the corruption.
+    virtual void Corruption(size_t bytes, const Status& status) = 0;
+  };
     
-      virtual std::shared_ptr<DHTTask>
-  createPingTask(const std::shared_ptr<DHTNode>& remoteNode,
-                 int numRetry = 0) = 0;
+      // crc32c values for all supported record types.  These are
+  // pre-computed to reduce the overhead of computing the crc of the
+  // record type stored in the header.
+  uint32_t type_crc_[kMaxRecordType + 1];
     
-    void DHTUnknownMessage::doReceivedAction() {}
-    
-      // Input statistics
-  // TODO(noetzli): The stats are incomplete. They are lacking everything
-  // consumed by MergeHelper.
-  uint64_t num_input_records = 0;
-  uint64_t num_input_deletion_records = 0;
-  uint64_t num_input_corrupt_records = 0;
-  uint64_t total_input_raw_key_bytes = 0;
-  uint64_t total_input_raw_value_bytes = 0;
-    
-    Status WriteBatchBase::Put(const SliceParts& key, const SliceParts& value) {
-  std::string key_buf, value_buf;
-  Slice key_slice(key, &key_buf);
-  Slice value_slice(value, &value_buf);
+    namespace leveldb {
     }
     
-      virtual Status Write(uint64_t offset, const Slice& data) override;
-    
-      bool FilterMergeOperand(int level, const rocksdb::Slice& key,
-                          const rocksdb::Slice& existing_value) const override {
-    fprintf(stderr, 'FilterMerge(%s)\n', key.ToString().c_str());
-    ++merge_count_;
-    return existing_value == 'bad';
+    class Repairer {
+ public:
+  Repairer(const std::string& dbname, const Options& options)
+      : dbname_(dbname),
+        env_(options.env),
+        icmp_(options.comparator),
+        ipolicy_(options.filter_policy),
+        options_(SanitizeOptions(dbname, &icmp_, &ipolicy_, options)),
+        owns_info_log_(options_.info_log != options.info_log),
+        owns_cache_(options_.block_cache != options.block_cache),
+        next_file_number_(1) {
+    // TableCache can be small since we expect each table to be opened once.
+    table_cache_ = new TableCache(dbname_, options_, 10);
   }
+    }
     
-      // Do a write outside of the transaction to key 'y'
-  s = txn_db->Put(write_options, 'y', 'y');
+    namespace leveldb {
+    }
     
-    Status GetStringFromColumnFamilyOptions(std::string* opts_str,
-                                        const ColumnFamilyOptions& cf_options,
-                                        const std::string& delimiter = ';  ');
+    class TableCache {
+ public:
+  TableCache(const std::string& dbname, const Options& options, int entries);
+  ~TableCache();
+    }
     
-    #include <string>
-#include <vector>
+    //-----------------------------------------------------------------------------
+// [SECTION] ImFontConfig
+//-----------------------------------------------------------------------------
     
+    // CHANGELOG
+// (minor and older changes stripped away, please see git history for details)
+//  2019-02-11: OpenGL: Projecting clipping rectangles correctly using draw_data->FramebufferScale to allow multi-viewports for retina display.
+//  2018-11-30: Misc: Setting up io.BackendRendererName so it can be displayed in the About Window.
+//  2018-08-03: OpenGL: Disabling/restoring GL_LIGHTING and GL_COLOR_MATERIAL to increase compatibility with legacy OpenGL applications.
+//  2018-06-08: Misc: Extracted imgui_impl_opengl2.cpp/.h away from the old combined GLFW/SDL+OpenGL2 examples.
+//  2018-06-08: OpenGL: Use draw_data->DisplayPos and draw_data->DisplaySize to setup projection matrix and clipping rectangle.
+//  2018-02-16: Misc: Obsoleted the io.RenderDrawListsFn callback and exposed ImGui_ImplGlfwGL2_RenderDrawData() in the .h file so you can call it yourself.
+//  2017-09-01: OpenGL: Save and restore current polygon mode.
+//  2016-09-10: OpenGL: Uploading font texture as RGBA32 to increase compatibility with users shaders (not ideal).
+//  2016-09-05: OpenGL: Fixed save and restore of current scissor rectangle.
     
-    {}
+    //---- Define assertion handler. Defaults to calling assert().
+//#define IM_ASSERT(_EXPR)  MyAssert(_EXPR)
+//#define IM_ASSERT(_EXPR)  ((void)(_EXPR))     // Disable asserts
+    
+        // Cleanup
+    ImGui_ImplAllegro5_Shutdown();
+    ImGui::DestroyContext();
+    al_destroy_event_queue(queue);
+    al_destroy_display(display);
+    
+        // Main loop
+    while (true)
+    {
+        if (s3eDeviceCheckQuitRequest())
+            break;
+    }
+    
+        outliterals(input+length - literals, literals);
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
