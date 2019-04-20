4e25a18957eb97423939c20bafc8455ed137c70c
SUNsung <weatherius@gmail.com>
2019-04-20 09:43:54 +0300

    Rewrite [20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp] File in: 2019-04-20 09:43:53

--- cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
+++ cccccc/20g2ttVPtZV9k20iAR8KR88Tbc20wAcYRl6MR7w20dgF.cpp
@@ -1,182 +1,166 @@
 
         
-                for (;;)
-        {
-            pid2 = swoole_coroutine_wait(&status);
-            if (pid2 == pid)
-            {
-                break;
-            }
-        }
+        #ifndef BITCOIN_QT_NETWORKSTYLE_H
+#define BITCOIN_QT_NETWORKSTYLE_H
     
-    const char* swoole_strerror(enum swErrorCode code)
-{
-    /* swstrerror {{{*/
-    switch(code)
-    {
-    case SW_ERROR_MALLOC_FAIL:
-        return 'Malloc fail';
-    case SW_ERROR_SYSTEM_CALL_FAIL:
-        return 'System call fail';
-    case SW_ERROR_PHP_FATAL_ERROR:
-        return 'PHP fatal error';
-    case SW_ERROR_NAME_TOO_LONG:
-        return 'Name too long';
-    case SW_ERROR_INVALID_PARAMS:
-        return 'Invalid params';
-    case SW_ERROR_QUEUE_FULL:
-        return 'Queue full';
-    case SW_ERROR_FILE_NOT_EXIST:
-        return 'File not exist';
-    case SW_ERROR_FILE_TOO_LARGE:
-        return 'File too large';
-    case SW_ERROR_FILE_EMPTY:
-        return 'File empty';
-    case SW_ERROR_DNSLOOKUP_DUPLICATE_REQUEST:
-        return 'DNS Lookup duplicate request';
-    case SW_ERROR_DNSLOOKUP_RESOLVE_FAILED:
-        return 'DNS Lookup resolve failed';
-    case SW_ERROR_DNSLOOKUP_RESOLVE_TIMEOUT:
-        return 'DNS Lookup resolve timeout';
-    case SW_ERROR_BAD_IPV6_ADDRESS:
-        return 'Bad ipv6 address';
-    case SW_ERROR_UNREGISTERED_SIGNAL:
-        return 'Unregistered signal';
-    case SW_ERROR_SESSION_CLOSED_BY_SERVER:
-        return 'Session closed by server';
-    case SW_ERROR_SESSION_CLOSED_BY_CLIENT:
-        return 'Session closed by client';
-    case SW_ERROR_SESSION_CLOSING:
-        return 'Session closing';
-    case SW_ERROR_SESSION_CLOSED:
-        return 'Session closed';
-    case SW_ERROR_SESSION_NOT_EXIST:
-        return 'Session not exist';
-    case SW_ERROR_SESSION_INVALID_ID:
-        return 'Session invalid id';
-    case SW_ERROR_SESSION_DISCARD_TIMEOUT_DATA:
-        return 'Session discard timeout data';
-    case SW_ERROR_OUTPUT_BUFFER_OVERFLOW:
-        return 'Output buffer overflow';
-    case SW_ERROR_SSL_NOT_READY:
-        return 'SSL not ready';
-    case SW_ERROR_SSL_CANNOT_USE_SENFILE:
-        return 'SSL cannot use senfile';
-    case SW_ERROR_SSL_EMPTY_PEER_CERTIFICATE:
-        return 'SSL empty peer certificate';
-    case SW_ERROR_SSL_VEFIRY_FAILED:
-        return 'SSL vefiry failed';
-    case SW_ERROR_SSL_BAD_CLIENT:
-        return 'SSL bad client';
-    case SW_ERROR_SSL_BAD_PROTOCOL:
-        return 'SSL bad protocol';
-    case SW_ERROR_PACKAGE_LENGTH_TOO_LARGE:
-        return 'Package length too large';
-    case SW_ERROR_DATA_LENGTH_TOO_LARGE:
-        return 'Data length too large';
-    case SW_ERROR_TASK_PACKAGE_TOO_BIG:
-        return 'Task package too big';
-    case SW_ERROR_TASK_DISPATCH_FAIL:
-        return 'Task dispatch fail';
-    case SW_ERROR_HTTP2_STREAM_ID_TOO_BIG:
-        return 'Http2 stream id too big';
-    case SW_ERROR_HTTP2_STREAM_NO_HEADER:
-        return 'Http2 stream no header';
-    case SW_ERROR_HTTP2_STREAM_NOT_FOUND:
-        return 'Http2 stream not found';
-    case SW_ERROR_AIO_BAD_REQUEST:
-        return 'Aio bad request';
-    case SW_ERROR_AIO_CANCELED:
-        return 'Aio canceled';
-    case SW_ERROR_CLIENT_NO_CONNECTION:
-        return 'Client no connection';
-    case SW_ERROR_SOCKET_CLOSED:
-        return 'Socket closed';
-    case SW_ERROR_SOCKS5_UNSUPPORT_VERSION:
-        return 'Socks5 unsupport version';
-    case SW_ERROR_SOCKS5_UNSUPPORT_METHOD:
-        return 'Socks5 unsupport method';
-    case SW_ERROR_SOCKS5_AUTH_FAILED:
-        return 'Socks5 auth failed';
-    case SW_ERROR_SOCKS5_SERVER_ERROR:
-        return 'Socks5 server error';
-    case SW_ERROR_HTTP_PROXY_HANDSHAKE_ERROR:
-        return 'Http proxy handshake error';
-    case SW_ERROR_HTTP_INVALID_PROTOCOL:
-        return 'Http invalid protocol';
-    case SW_ERROR_WEBSOCKET_BAD_CLIENT:
-        return 'Websocket bad client';
-    case SW_ERROR_WEBSOCKET_BAD_OPCODE:
-        return 'Websocket bad opcode';
-    case SW_ERROR_WEBSOCKET_UNCONNECTED:
-        return 'Websocket unconnected';
-    case SW_ERROR_WEBSOCKET_HANDSHAKE_FAILED:
-        return 'Websocket handshake failed';
-    case SW_ERROR_SERVER_MUST_CREATED_BEFORE_CLIENT:
-        return 'Server must created before client';
-    case SW_ERROR_SERVER_TOO_MANY_SOCKET:
-        return 'Server too many socket';
-    case SW_ERROR_SERVER_WORKER_TERMINATED:
-        return 'Server worker terminated';
-    case SW_ERROR_SERVER_INVALID_LISTEN_PORT:
-        return 'Server invalid listen port';
-    case SW_ERROR_SERVER_TOO_MANY_LISTEN_PORT:
-        return 'Server too many listen port';
-    case SW_ERROR_SERVER_PIPE_BUFFER_FULL:
-        return 'Server pipe buffer full';
-    case SW_ERROR_SERVER_NO_IDLE_WORKER:
-        return 'Server no idle worker';
-    case SW_ERROR_SERVER_ONLY_START_ONE:
-        return 'Server only start one';
-    case SW_ERROR_SERVER_SEND_IN_MASTER:
-        return 'Server send in master';
-    case SW_ERROR_SERVER_INVALID_REQUEST:
-        return 'Server invalid request';
-    case SW_ERROR_SERVER_WORKER_EXIT_TIMEOUT:
-        return 'Server worker exit timeout';
-    case SW_ERROR_CO_OUT_OF_COROUTINE:
-        return 'Coroutine out of coroutine';
-    case SW_ERROR_CO_HAS_BEEN_BOUND:
-        return 'Coroutine has been bound';
-    case SW_ERROR_CO_MUTEX_DOUBLE_UNLOCK:
-        return 'Coroutine mutex double unlock';
-    case SW_ERROR_CO_BLOCK_OBJECT_LOCKED:
-        return 'Coroutine block object locked';
-    case SW_ERROR_CO_BLOCK_OBJECT_WAITING:
-        return 'Coroutine block object waiting';
-    case SW_ERROR_CO_YIELD_FAILED:
-        return 'Coroutine yield failed';
-    case SW_ERROR_CO_GETCONTEXT_FAILED:
-        return 'Coroutine getcontext failed';
-    case SW_ERROR_CO_SWAPCONTEXT_FAILED:
-        return 'Coroutine swapcontext failed';
-    case SW_ERROR_CO_MAKECONTEXT_FAILED:
-        return 'Coroutine makecontext failed';
-    case SW_ERROR_CO_IOCPINIT_FAILED:
-        return 'Coroutine iocpinit failed';
-    case SW_ERROR_CO_PROTECT_STACK_FAILED:
-        return 'Coroutine protect stack failed';
-    case SW_ERROR_CO_STD_THREAD_LINK_ERROR:
-        return 'Coroutine std thread link error';
-    case SW_ERROR_CO_DISABLED_MULTI_THREAD:
-        return 'Coroutine disabled multi thread';
-    default:
-        static char buffer[32];
-#ifndef __MACH__
-        snprintf(buffer, sizeof(buffer), 'Unknown error %d', code);
-#else
-        snprintf(buffer, sizeof(buffer), 'Unknown error: %d', code);
-#endif
-        return buffer;
-    }
-/*}}}*/
+    static SECP256K1_INLINE void secp256k1_gej_double_nonzero(secp256k1_gej *r, const secp256k1_gej *a, secp256k1_fe *rzr) {
+    VERIFY_CHECK(!secp256k1_gej_is_infinity(a));
+    secp256k1_gej_double_var(r, a, rzr);
 }
     
-    void check_reactor(void)
+    /// Format list of arguments to the stream according to given format string.
+template<typename... Args>
+void format(std::ostream& out, const char* fmt, const Args&... args)
 {
-    swoole_init();
-    if (SwooleWG.reactor_init)
+    vformat(out, fmt, makeFormatList(args...));
+}
+    
+    /** A hasher class for RIPEMD-160. */
+class CRIPEMD160
+{
+private:
+    uint32_t s[5];
+    unsigned char buf[64];
+    uint64_t bytes;
+    }
+    
+        movdqa	XFER, [TBL + 1*16]
+    paddd	XFER, X0
+    movdqa	[rsp + _XFER], XFER
+    FOUR_ROUNDS_AND_SCHED
+    
+    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
+  result->append(key.user_key.data(), key.user_key.size());
+  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
+}
+    
+            friend Rational operator&(Rational lhs, Rational const& rhs);
+        friend Rational operator|(Rational lhs, Rational const& rhs);
+        friend Rational operator^(Rational lhs, Rational const& rhs);
+    
+            virtual void Announce(NarratorAnnouncement^ announcement);
+    
+    namespace CalculatorApp
+{
+    EngineResourceProvider::EngineResourceProvider()
     {
-        return;
+        m_resLoader = ResourceLoader::GetForViewIndependentUse('CEngineStrings');
     }
     }
+    
+    #include 'pch.h'
+#include 'ExpressionCommandDeserializer.h'
+    
+      static void clear(Data& data);
+    
+      virtual const std::string& getType() const CXX11_OVERRIDE;
+    
+    #include 'DHTNode.h'
+#include 'DHTBucket.h'
+#include 'DHTBucketTree.h'
+#include 'DHTTaskQueue.h'
+#include 'DHTTaskFactory.h'
+#include 'DHTTask.h'
+#include 'util.h'
+#include 'LogFactory.h'
+#include 'Logger.h'
+#include 'fmt.h'
+    
+    #include 'TimeA2.h'
+    
+    #define WRITE_CHECK(fp, ptr, count)                                            \
+  if (fp.write((ptr), (count)) != (count)) {                                   \
+    throw DL_ABORT_EX(                                                         \
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));     \
+  }
+    
+    
+    {} // namespace aria2
+    
+      ~DHTTaskExecutor();
+    
+      virtual std::shared_ptr<DHTTask>
+  createReplaceNodeTask(const std::shared_ptr<DHTBucket>& bucket,
+                        const std::shared_ptr<DHTNode>& newNode) CXX11_OVERRIDE;
+    
+    #include <memory>
+    
+    
+    {  virtual void
+  addImmediateTask(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+};
+    
+    bool DNSCache::CacheEntry::add(const std::string& addr)
+{
+  for (std::vector<AddrEntry>::const_iterator i = addrEntries_.begin(),
+                                              eoi = addrEntries_.end();
+       i != eoi; ++i) {
+    if ((*i).addr_ == addr) {
+      return false;
+    }
+  }
+  addrEntries_.push_back(AddrEntry(addr));
+  return true;
+}
+    
+    #include <string>
+#include <set>
+#include <algorithm>
+#include <vector>
+    
+            void addWrite() {
+            if (m_write) return;
+            m_write = new QSocketNotifier(m_ctx->c.fd, QSocketNotifier::Write, 0);
+            connect(m_write, SIGNAL(activated(int)), this, SLOT(write()));
+        }
+    
+        cache.set('test1', val1);
+    ASSERT_EQ(cache.get('test1').get(), val1.get());
+    val1.reset();
+    ASSERT_EQ(dtor_num, 0);
+    
+            for (int i = 0; i < 100; ++i)
+        {
+            std::string addr3 = Coroutine::gethostbyname('ipv6.sjtu.edu.cn', AF_INET);
+            std::string addr4 = Coroutine::gethostbyname('ipv6.sjtu.edu.cn', AF_INET6);
+    }
+    
+    TEST(ringbuffer, thread)
+{
+    int i;
+    pool = swRingBuffer_new(1024 * 1024 * 4, 1);
+    ASSERT_NE(pool, nullptr);
+    }
+    
+    TEST(coroutine_base, yield_resume)
+{
+    long _cid;
+    long cid = Coroutine::create([](void *arg)
+    {
+        long cid = Coroutine::get_current_cid();
+        Coroutine *co = Coroutine::get_by_cid(cid);
+        co->yield();
+        *(long *) arg = Coroutine::get_current_cid();
+    }, &_cid);
+    }
+    
+    
+    {            ret = chan->push(&i);
+            ASSERT_TRUE(ret);
+            ret = chan->push(&i);
+            ASSERT_TRUE(ret);
+        }, &chan),
+    
+    static swThreadPool pool;
+static int _pipe;
+const static int N = 10000;
+    
+        int length;
+    char address[256];
+    int port = 0;
+    int ret;
+    
+    void MyServer::onTask(int task_id, int src_worker_id, const DataBuffer &data)
+{
+    printf('PID=%d\tTaskID=%d\n', getpid(), task_id);
+}
