53ea4308acb1a65f847fe1bf17073a2831ef8faa
SUNsung <weatherius@gmail.com>
2019-04-20 16:29:20 +0300

    Rewrite [58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb] File in: 2019-04-20 16:29:20

--- ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
+++ ruby/58V9h27QF0DB.58YJhepFqlts2580660ayrqaww58Ary.rb
@@ -1,178 +1,123 @@
 
         
-          #
-  # Initializes an HTTP packet header class that inherits from a Hash base
-  # class.
-  #
-  def initialize
-    self.dcase_hash = {}
+                  def label(label_html_options = {}, &block)
+            html_options = @input_html_options.slice(:index, :namespace).merge(label_html_options)
+            html_options[:for] ||= @input_html_options[:id] if @input_html_options[:id]
     
-        self.method    = method
-    self.raw_uri   = uri
-    self.uri_parts = {}
-    self.proto     = proto || DefaultProtocol
-    self.chunk_min_size = 1
-    self.chunk_max_size = 10
-    self.uri_encode_mode = 'hex-normal'
-    
-      #
-  # Dispatches the supplied request for a given connection.
-  #
-  def dispatch_request(cli, request)
-    # Is the client requesting keep-alive?
-    if ((request['Connection']) and
-       (request['Connection'].downcase == 'Keep-Alive'.downcase))
-      cli.keepalive = true
-    end
-    
-      #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
-    
-            # UDP isn't supported
-        #
-        # @raise [NotImplementedError]
-        def recv_response_udp
-          raise ::NotImplementedError, 'Kerberos Client: UDP unsupported'
-        end
-    
-                cipher = OpenSSL::Cipher.new('rc4')
-            cipher.decrypt
-            cipher.key = k3
-            decrypted = cipher.update(data) + cipher.final
-    
-              # Decodes the sname field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Rex::Proto::Kerberos::Type::PrincipalName]
-          def decode_sname(input)
-            Rex::Proto::Kerberos::Model::PrincipalName.decode(input.value[0])
+              def render_component(builder)
+            builder.radio_button + builder.label
           end
-        end
-      end
-    end
-  end
-end
-    
-              # Decodes the Rex::Proto::Kerberos::Model::KrbError from an input
-          #
-          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [self] if decoding succeeds
-          # @raise [RuntimeError] if decoding doesn't succeed
-          def decode(input)
-            case input
-            when String
-              decode_string(input)
-            when OpenSSL::ASN1::ASN1Data
-              decode_asn1(input)
-            else
-              raise ::RuntimeError, 'Failed to decode KrbError, invalid input'
-            end
-    
-        file package(gem, '.tar.gz') => ['pkg/'] do |f|
-      sh <<-SH
-        git archive \
-          --prefix=#{gem}-#{source_version}/ \
-          --format=tar \
-          HEAD -- #{directory} | gzip > #{f.name}
-      SH
-    end
-  end
-    
-          def html?(headers)
-        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
-        options[:html_types].include? header.last[/^\w+\/\w+/]
       end
     end
   end
 end
 
     
-    module Rack
-  module Protection
-    ##
-    # Prevented attack::   XSS and others
-    # Supported browsers:: Firefox 23+, Safari 7+, Chrome 25+, Opera 15+
-    #
-    # Description:: Content Security Policy, a mechanism web applications
-    #               can use to mitigate a broad class of content injection
-    #               vulnerabilities, such as cross-site scripting (XSS).
-    #               Content Security Policy is a declarative policy that lets
-    #               the authors (or server administrators) of a web application
-    #               inform the client about the sources from which the
-    #               application expects to load resources.
-    #
-    # More info::   W3C CSP Level 1 : https://www.w3.org/TR/CSP1/ (deprecated)
-    #               W3C CSP Level 2 : https://www.w3.org/TR/CSP2/ (current)
-    #               W3C CSP Level 3 : https://www.w3.org/TR/CSP3/ (draft)
-    #               https://developer.mozilla.org/en-US/docs/Web/Security/CSP
-    #               http://caniuse.com/#search=ContentSecurityPolicy
-    #               http://content-security-policy.com/
-    #               https://securityheaders.io
-    #               https://scotthelme.co.uk/csp-cheat-sheet/
-    #               http://www.html5rocks.com/en/tutorials/security/content-security-policy/
-    #
-    # Sets the 'Content-Security-Policy[-Report-Only]' header.
-    #
-    # Options: ContentSecurityPolicy configuration is a complex topic with
-    #          several levels of support that has evolved over time.
-    #          See the W3C documentation and the links in the more info
-    #          section for CSP usage examples and best practices. The
-    #          CSP3 directives in the 'NO_ARG_DIRECTIVES' constant need to be
-    #          presented in the options hash with a boolean 'true' in order
-    #          to be used in a policy.
-    #
-    class ContentSecurityPolicy < Base
-      default_options default_src: :none, script_src: ''self'',
-                      img_src: ''self'', style_src: ''self'',
-                      connect_src: ''self'', report_only: false
+            class << self
+          def select_type
+            @select_type ||= name.split('::').last.sub('Select', '').downcase
+          end
+        end
     
-      it 'accepts post form requests with correct authenticity_token field' do
-    post('/', {'authenticity_token' => token}, 'rack.session' => session)
-    expect(last_response).to be_ok
+        it 'does not send previously configured receivers when the current agent does not support them' do
+      select_agent_type('Website Agent scrapes')
+      sleep 0.5
+      select2('ZKCD', from: 'Receivers')
+      select_agent_type('Email Agent')
+      fill_in(:agent_name, with: 'No receivers')
+      click_on 'Save'
+      expect(page).to have_content('No receivers')
+      agent = Agent.find_by(name: 'No receivers')
+      expect(agent.receivers).to eq([])
+    end
   end
+end
+
     
-      def validate_target_file
-    if File.exist?(target_file)
-      if  delete_target_file?
-        File.delete(target_file)
-      else
-        signal_error('Package creation cancelled, a previously generated package exist at location: #{target_file}, move this file to safe place and run the command again')
-      end
+    describe 'Dry running an Agent', js: true do
+  let(:agent)   { agents(:bob_website_agent) }
+  let(:formatting_agent) { agents(:bob_formatting_agent) }
+  let(:user)    { users(:bob) }
+  let(:emitter) { agents(:bob_weather_agent) }
+    
+        it 'returns a correct icon tag for GitHub' do
+      icon = omniauth_provider_icon(:github)
+      expect(icon).to be_html_safe
+      elem = Nokogiri(icon).at('i.fa.fa-github')
+      expect(elem).to be_a Nokogiri::XML::Element
+    end
+    
+        it 'can be turned off' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { 'false' }
+      DefaultScenarioImporter.import(user)
+    end
+    
+            it 'adds errors when updated agents are invalid' do
+          scenario_import.merges = {
+            '0' => {
+              'name' => '',
+              'schedule' => 'foo',
+              'keep_events_for' => 2.days.to_i.to_s,
+              'options' => weather_agent_options.merge('api_key' => '').to_json
+            }
+          }
+    
+      describe 'path request must exist' do
+    it 'should check that validation added if path does not exist' do
+      opts = @opts.tap { |o| o.delete('path') }
+      @checker = Agents::AftershipAgent.new(:name => 'tectonic', :options => opts)
+      @checker.user = users(:bob)
+      expect(@checker.save).to eq false
+      expect(@checker.errors.full_messages.first).to eq('You need to specify a path request')
     end
   end
     
-      namespace :vm do
+        def types_as_json
+      @types.values.sort! { |a, b| sort_fn(a.name, b.name) }.map(&:as_json)
+    end
     
-            names.inject(Object) do |constant, name|
-          constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+        def initialize(filters = nil)
+      @filters = filters ? filters.dup : []
+    end
+    
+        def initialize
+      @pages = {}
+    end
+    
+        def effective_url
+      @effective_url ||= URL.parse super
+    end
+    
+            def store_pages(store)
+          instrument 'info.doc', msg: 'Fetching redirections...'
+          with_redirections do
+            instrument 'info.doc', msg: 'Continuing...'
+            super
+          end
         end
-      rescue NameError
-        super
+    
+            css('ul.methods', 'ul.properties', 'ul.events').add_class('defs').each do |node|
+          node.css('> li > h3').each do |h3|
+            next if h3.content.present?
+            h3.content = h3.next_element.content
+            h3.next_element.remove
+          end
+        end
       end
     end
   end
 end
+
     
-          def insert_before(oldklass, newklass, *args)
-        i = entries.index { |entry| entry.klass == newklass }
-        new_entry = i.nil? ? Entry.new(newklass, *args) : entries.delete_at(i)
-        i = entries.index { |entry| entry.klass == oldklass } || 0
-        entries.insert(i, new_entry)
-      end
+    require_relative 'converter/fonts_conversion'
+require_relative 'converter/less_conversion'
+require_relative 'converter/js_conversion'
+require_relative 'converter/logger'
+require_relative 'converter/network'
     
-        def erb(content, options = {})
-      if content.kind_of? Symbol
-        unless respond_to?(:'_erb_#{content}')
-          src = ERB.new(File.read('#{Web.settings.views}/#{content}.erb')).src
-          WebAction.class_eval('def _erb_#{content}\n#{src}\n end')
-        end
-      end
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
