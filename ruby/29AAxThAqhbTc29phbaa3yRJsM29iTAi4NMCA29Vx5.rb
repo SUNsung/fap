887a2350ab5762023a5713d5a5c4af6e9004876a
SUNsung <weatherius@gmail.com>
2019-04-20 12:52:57 +0300

    Rewrite [29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb] File in: 2019-04-20 12:52:57

--- ruby/29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb
+++ ruby/29AAxThAqhbTc29phbaa3yRJsM29iTAi4NMCA29Vx5.rb
@@ -1,72 +1,28 @@
 
         
-            private
-    
-        weeks
-  end
-    
-      def available_locales
-    I18n.available_locales.reverse
+              # Custom destructuring method. This is used to normalize the branches
+      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
+      #
+      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
+      def node_parts
+        [self, self]
+      end
+    end
   end
 end
 
     
-      private
+          def title
+        'Home'
+      end
     
-      def session_needs_update?
-    !current_session.nil? && current_session.updated_at < UPDATE_SIGN_IN_HOURS.hours.ago
+        assert_no_match /Edit Page/,             last_response.body, ''Edit Page' link not blocked in compare template'
+    assert_no_match /Revert Changes/,        last_response.body, ''Revert Changes' link not blocked in compare template'
   end
+    
+      # piece file back together and write
+  manifest = '  s.files = %w[\n#{files}\n  ]\n'
+  spec = [head, manifest, tail].join('  # = MANIFEST =\n')
+  File.open(gemspec_file, 'w') { |io| io.write(spec) }
+  puts 'Updated #{gemspec_file}'
 end
-
-    
-      def process_bootstrap
-    log_status 'Convert Bootstrap LESS to Sass'
-    puts ' repo   : #@repo_url'
-    puts ' branch : #@branch_sha #@repo_url/tree/#@branch'
-    puts ' save to: #{@save_to.to_json}'
-    puts ' twbs cache: #{@cache_path}'
-    puts '-' * 60
-    
-      # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-    
-      def test_font_helper_with_suffix_sharp
-    assert_match %r(url\(['']?/assets/.*svg#.+['']?\)), @css
-  end
-    
-          # The body of the method definition.
-      #
-      # @note this can be either a `begin` node, if the method body contains
-      #       multiple expressions, or any other node, if it contains a single
-      #       expression.
-      #
-      # @return [Node] the body of the method definition
-      def body
-        node_parts[0]
-      end
-    
-          # Calls the given block for each `key` node in the `hash` literal.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_key
-        return pairs.map(&:key).to_enum unless block_given?
-    
-            def keyword_splat?
-          [first, second].any?(&:kwsplat_type?)
-        end
-      end
-    
-    module RuboCop
-  module AST
-    # Common functionality for nodes that are parameterized:
-    # `send`, `super`, `zsuper`, `def`, `defs`
-    module ParameterizedNode
-      # Checks whether this node's arguments are wrapped in parentheses.
-      #
-      # @return [Boolean] whether this node's arguments are
-      #                   wrapped in parentheses
-      def parenthesized?
-        loc.end && loc.end.is?(')')
-      end
