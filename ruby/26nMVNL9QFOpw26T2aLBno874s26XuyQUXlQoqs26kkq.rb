10484d2269216df0e90f4449f324cae8f6a2ae91
SUNsung <weatherius@gmail.com>
2019-04-21 00:49:28 +0300

    Rewrite [26nMVNL9QFOpw26T2aLBno874s26XuyQUXlQoqs26kkq.rb] File in: 2019-04-21 00:49:28

--- ruby/26nMVNL9QFOpw26T2aLBno874s26XuyQUXlQoqs26kkq.rb
+++ ruby/26nMVNL9QFOpw26T2aLBno874s26XuyQUXlQoqs26kkq.rb
@@ -1,83 +1,45 @@
-      class MergeRequest < ActiveRecord::Base
-        self.table_name = 'merge_requests'
+
+        
+                # attributes - A Hash containing the user details. The keys of this
+        #              Hash (and any nested hashes) must be symbols.
+        def initialize(attributes)
+          @attributes = attributes
+        end
+      end
+    end
+  end
+end
+
     
-    module Gitlab
-  module GithubImport
-    # IssuableFinder can be used for caching and retrieving database IDs for
-    # issuable objects such as issues and pull requests. By caching these IDs we
-    # remove the need for running a lot of database queries when importing
-    # GitHub projects.
-    class IssuableFinder
-      attr_reader :project, :object
+        # we assume that the first file that requires 'sinatra' is the
+    # app_file. all other path related options are calculated based
+    # on this path by default.
+    set :app_file, caller_files.first || $0
     
-          # The class to use for importing objects when importing them sequentially.
-      def importer_class
-        raise NotImplementedError
+          def call(env)
+        status, headers, body = @app.call(env)
+        header = options[:report_only] ? 'Content-Security-Policy-Report-Only' : 'Content-Security-Policy'
+        headers[header] ||= csp_policy if html? headers
+        [status, headers, body]
+      end
+    end
+  end
+end
+
+    
+          def escape_hash(hash)
+        hash = hash.dup
+        hash.each { |k,v| hash[k] = escape(v) }
+        hash
       end
     
-              user = Representation::User.from_api_response(note.user) if note.user
-          hash = {
-            noteable_type: 'MergeRequest',
-            noteable_id: matches[:iid].to_i,
-            file_path: note.path,
-            commit_id: note.commit_id,
-            diff_hunk: note.diff_hunk,
-            author: user,
-            note: note.body,
-            created_at: note.created_at,
-            updated_at: note.updated_at,
-            github_id: note.id
-          }
+      it_behaves_like 'any rack application'
     
-        get '/'
-    assert last_response.ok?
+    Given /^I run a paperclip generator to add a paperclip '([^']*)' to the '([^']*)' model$/ do |attachment_name, model_name|
+  step %[I successfully run `rails generate paperclip #{model_name} #{attachment_name}`]
+end
+    
+    module RailsCommandHelpers
+  def framework_version?(version_string)
+    framework_version =~ /^#{version_string}/
   end
-    
-        if last_request_at.is_a? Integer
-      last_request_at = Time.at(last_request_at).utc
-    elsif last_request_at.is_a? String
-      last_request_at = Time.parse(last_request_at)
-    end
-    
-          attr_reader :scope_name, :resource
-    
-      def default_locale
-    if ENV['DEFAULT_LOCALE'].present?
-      I18n.default_locale
-    else
-      request_locale || I18n.default_locale
-    end
-  end
-    
-        # From asking people, it seems MacPorts does not have a `prefix` command, like
-    # Homebrew does, so make an educated guess:
-    if port_prefix = prefix_from_bin('port')
-      prefixes << port_prefix
-    end
-    
-          private
-    
-        self.name = [attributes[:npm_package_name_prefix], name].join('-')
-    self.version = info.fetch('version', '0.0.0')
-    
-      # Helper for user lookup
-  def uid2user(uid)
-    begin
-      pwent = Etc.getpwuid(uid)
-      return pwent.name
-    rescue ArgumentError => e
-      # Invalid user id? No user? Return the uid.
-      logger.warn('Failed to find username for uid #{uid}')
-      return uid.to_s
-    end
-  end # def uid2user
-    
-        args = [ tar_cmd,
-             '-C',
-             staging_path,
-             '-cf',
-             payload_tar,
-             '--owner=0',
-             '--group=0',
-             '--numeric-owner',
-             '.' ]
