07ddd987c4f5f52f897d610ffa5ec1d6dc7e895c
SUNsung <weatherius@gmail.com>
2019-04-21 13:19:55 +0300

    Create [54ZMEAvxR2rlc54BOUAsRMyA254SoDpiG6PrE54PGj.rb] File in: 2019-04-21 13:19:54

---
+++ ruby/54ZMEAvxR2rlc54BOUAsRMyA254SoDpiG6PrE54PGj.rb
@@ -0,0 +1,113 @@
+      it 'creates a new user' do
+        visit new_admin_user_path
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Username', with: 'usertest'
+        fill_in 'Password', with: '12345678'
+        fill_in 'Password confirmation', with: '12345678'
+        click_on 'Create User'
+        expect(page).to have_text('User 'usertest' was successfully created.')
+        expect(page).to have_text('test@test.com')
+      end
+    
+          it 'generates a DOT script' do
+        expect(agents_dot(@agents)).to match(%r{
+          \A
+          digraph \x20 'Agent \x20 Event \x20 Flow' \{
+            node \[ [^\]]+ \];
+            edge \[ [^\]]+ \];
+            (?<foo>\w+) \[label=foo\];
+            \k<foo> -> (?<bar1>\w+) \[style=dashed\];
+            \k<foo> -> (?<bar2>\w+) \[color='\#999999'\];
+            \k<bar1> \[label=bar1\];
+            \k<bar2> \[label=bar2,style='rounded,dashed',color='\#999999',fontcolor='\#999999'\];
+            \k<bar2> -> (?<bar3>\w+) \[style=dashed,color='\#999999'\];
+            \k<bar3> \[label=bar3\];
+          \}
+          \z
+        }x)
+      end
+    
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
+    end
+    
+          describe '#generate_diff' do
+        it 'returns AgentDiff objects for the incoming Agents' do
+          expect(scenario_import).to be_valid
+    
+          expect(@scheduler.scheduler_agent_jobs.map(&:scheduler_agent)).to eq([@agent2])
+    end
+    
+      describe 'path request must exist' do
+    it 'should check that validation added if path does not exist' do
+      opts = @opts.tap { |o| o.delete('path') }
+      @checker = Agents::AftershipAgent.new(:name => 'tectonic', :options => opts)
+      @checker.user = users(:bob)
+      expect(@checker.save).to eq false
+      expect(@checker.errors.full_messages.first).to eq('You need to specify a path request')
+    end
+  end
+    
+      describe '#working?' do
+    it 'it is working when at least one event was emitted' do
+      expect(@checker).not_to be_working
+      @checker.memory[:last_event] = '2014-04-17T10:25:31.000+02:00'
+      @checker.check
+      expect(@checker.reload).to be_working
+    end
+  end
+end
+
+    
+        it 'should raise error when invalid response arrives' do
+      stub(HTTParty).post { {'blah' => 'blah'} }
+      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Invalid response from Boxcar:/)
+    end
+    
+        private
+    
+      private
+    
+      def compatible_locale
+    http_accept_language.compatible_language_from(available_locales)
+  end
+    
+      config.vm.define 'debian7' do |debian7|
+    debian7.vm.box = 'puppetlabs/centos-7.0-64-puppet'
+  end
+    
+      option '--name', 'SERVICE_NAME', 'The name of the service you are creating'
+  option '--chdir', 'CHDIR', 'The working directory used by the service'
+    
+        safesystem('tar', *args)
+  end # def output
+    
+      # Output a zipfile.
+  def output(output_path)
+    output_check(output_path)
+    realpath = Pathname.new(output_path).realdirpath.to_s
+    ::Dir.chdir(staging_path) do
+      safesystem('zip', '-9r', realpath, '.')
+    end
+  end # def output
+end # class FPM::Package::Tar
+
+    
+    module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+    
+    class PaperclipGenerator < ActiveRecord::Generators::Base
+  desc 'Create a migration to add paperclip-specific fields to your model. ' +
+       'The NAME argument is the name of your model, and the following ' +
+       'arguments are the name of the attachments'
+    
+        def self.clear
+      instance.clear
+    end
