a711d6507b9241906ab6db0264997cf4a38138a4
SUNsung <weatherius@gmail.com>
2019-04-21 07:34:20 +0300

    Rewrite [27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py] File in: 2019-04-21 07:34:19

--- pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
+++ pyton/27ycjxKXlpUUo27cLDu6IWTHA27anUsPPjnw270SL.py
@@ -1,147 +1,77 @@
 
         
-        with open('update/LATEST_VERSION', 'w') as f:
-    f.write(version)
+        index_title = 0
+index_desc = 1
+index_auth = 2
+index_https = 3
+index_cors = 4
+index_link = 5
+num_segments = 5
+    
+    # One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    ('index', 'face_recognition',
+     u'Face Recognition Documentation',
+     [u'Adam Geitgey'], 1)
+]
     
     
-# Import youtube_dl
-ROOT_DIR = os.path.join(os.path.dirname(__file__), '..')
-sys.path.insert(0, ROOT_DIR)
-import youtube_dl
+def run_test(setup, test, iterations_per_test=5, tests_to_run=10):
+    fastest_execution = min(timeit.Timer(test, setup=setup).repeat(tests_to_run, iterations_per_test))
+    execution_time = fastest_execution / iterations_per_test
+    fps = 1.0 / execution_time
+    return execution_time, fps
     
-        def test_cbc_encrypt(self):
-        data = bytes_to_intlist(self.secret_msg)
-        encrypted = intlist_to_bytes(aes_cbc_encrypt(data, self.key, self.iv))
-        self.assertEqual(
-            encrypted,
-            b'\x97\x92+\xe5\x0b\xc3\x18\x91ky9m&\xb3\xb5@\xe6'\xc2\x96.\xc8u\x88\xab9-[\x9e|\xf1\xcd')
-    
-    
-if __name__ == '__main__':
-    unittest.main()
-
-    
-            jsi = JSInterpreter('''
-        function f() {
-            var x = '/*';
-            var y = 1 /* comment */ + 2;
-            return y;
-        }
-        ''')
-        self.assertEqual(jsi.call_function('f'), 3)
-    
-    n_samples, n_features = 5000, 300
-X = np.random.randn(n_samples, n_features)
-inds = np.arange(n_samples)
-np.random.shuffle(inds)
-X[inds[int(n_features / 1.2):]] = 0  # sparsify input
-print('input data sparsity: %f' % sparsity_ratio(X))
-coef = 3 * np.random.randn(n_features)
-inds = np.arange(n_features)
-np.random.shuffle(inds)
-coef[inds[n_features // 2:]] = 0  # sparsify coef
-print('true coef sparsity: %f' % sparsity_ratio(coef))
-y = np.dot(X, coef)
-    
-    
-def issue_role(name, rawtext, text, lineno,
-               inliner, options=None, content=None):
-    '''Sphinx role for linking to an issue. Must have
-    `issues_uri` or `issues_github_path` configured in ``conf.py``.
-    
-    X = np.array(X)
-y = np.array(y)
-    
-    import numpy as np
-import matplotlib.pyplot as plt
-    
-    The first plot shows the best inertia reached for each combination
-of the model (``KMeans`` or ``MiniBatchKMeans``) and the init method
-(``init='random'`` or ``init='kmeans++'``) for increasing values of the
-``n_init`` parameter that controls the number of initializations.
-    
-    # Anisotropicly distributed data
-random_state = 170
-X, y = datasets.make_blobs(n_samples=n_samples, random_state=random_state)
-transformation = [[0.6, -0.6], [-0.4, 0.8]]
-X_aniso = np.dot(X, transformation)
-aniso = (X_aniso, y)
-    
-    
-class BadNonceTest(unittest.TestCase):
-    '''Tests for acme.errors.BadNonce.'''
-    
-            # Placeholder for augeas
-        self.aug = None
-    
-        # NOTE: Only run one test per function otherwise you will have
-    # inf recursion
-    def test_include(self):
-        self.verify_fnmatch('test_fnmatch.?onf')
-    
-            api_url = self.API_ENDPOINT + \
-            'servlet/playinfo?vid={vid}&m=0'.format(vid = vid)  #return XML
-    
-        if re.match(r'https?://movie', url):
-        title = match1(html, 'name='description' content='([^']+)')
-        tid = match1(url, 'trailer/(\d+)')
-        real_url = 'https://movie.douban.com/trailer/video_url?tid=%s' % tid
-        type, ext, size = url_info(real_url)
-    
-    def get_photo_id(url, page):
-    return match1(url, pattern_url_single_photo)
-    
-        type, ext, size = url_info(url[0], True)
-    size = urls_size(url)
-    
-        stream_types = [
-        {'id': 'video'},
-        {'id': 'audio'},
-        {'id': 'slides'}
-    ]
-    
-    import face_recognition
-    
-        # Print the location of each face in this image
-    top, right, bottom, left = face_location
-    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
-    
-        if os.path.isdir(image_to_check):
-        if cpus == 1:
-            [test_image(image_file, model) for image_file in image_files_in_folder(image_to_check)]
-        else:
-            process_images_in_process_pool(image_files_in_folder(image_to_check), cpus, model)
-    else:
-        test_image(image_to_check, model)
-    
-        # Label the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        if not name:
-            continue
-    
-    frames = []
-frame_count = 0
-    
-    # Create arrays of known face encodings and their names
-known_face_encodings = [
+    known_encodings = [
     obama_face_encoding,
     biden_face_encoding
 ]
-known_face_names = [
-    'Barack Obama',
-    'Joe Biden'
-]
     
-            for detected_faces in batched_detected_faces:
-            self.assertEqual(len(detected_faces), 1)
-            self.assertEqual(detected_faces[0].rect.top(), 154)
-            self.assertEqual(detected_faces[0].rect.bottom(), 390)
+        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
+    context = multiprocessing
+    if 'forkserver' in multiprocessing.get_all_start_methods():
+        context = multiprocessing.get_context('forkserver')
     
-    # Find all facial features in all the faces in the image
-face_landmarks_list = face_recognition.face_landmarks(image)
+    while True:
+    print('Capturing image.')
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format='rgb')
     
-            if match[0]:
-            name = 'Barack Obama'
+    import face_recognition
+import picamera
+import numpy as np
     
-            #Conversion of md to HTML
-        for md in allmd:
+    
+    # Display the results
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+    
+    # It false, will not define \strong, \code, 	itleref, \crossref ... but only
+# \sphinxstrong, ..., \sphinxtitleref, ... To help avoid clash with user added
+# packages.
+#
+# latex_keep_old_macro_names = True
+    
+    from homeassistant.core import HomeAssistant
+from homeassistant.util.temperature import convert as convert_temperature
+from homeassistant.const import PRECISION_HALVES, PRECISION_TENTHS
+    
+    
+def __feet_to_meters(feet: float) -> float:
+    '''Convert feet to meters.'''
+    return feet * 0.3048
+    
+    from homeassistant import const
+    
+        changed = True  # 标记样本类别是否改变
+    n_iter = 0  # 记录迭代次数
+    while changed and n_iter < max_iter:
+        changed = False
+        n_iter += 1
+    
+            return self._call(x)
