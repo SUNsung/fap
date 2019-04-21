b0fd94272f20ec611436c78c2a075362038e7129
SUNsung <weatherius@gmail.com>
2019-04-21 08:03:59 +0300

    Rewrite [570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb] File in: 2019-04-21 08:03:58

--- ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
+++ ruby/570SXNh0YL6mE577ZQMttwua7Q57zY8wo6SCLLo57JE2.rb
@@ -1,94 +1,37 @@
 
         
-                expect(result).to eq('appledoc --project-name \'Project Name\' --project-company \'Company\' --docset-fallback-url \'http://docset-fallback-url.com\' --exit-threshold \'2\' input/dir')
-      end
+        require 'vagrant/util/safe_puts'
     
-              expect(result).to eq('SCRIPT_INPUT_FILE_COUNT=3 SCRIPT_INPUT_FILE_0=AppDelegate.swift SCRIPT_INPUT_FILE_1=path/to/project/src/Model.swift SCRIPT_INPUT_FILE_2=path/to/project/test/Test.swift swiftlint lint --use-script-input-files')
-        end
-      end
-    
-        # Returns an updated value type (if necessary)
-    def auto_convert_value(value)
-      return nil if value.nil?
-    
-          it 'should shell escape keychain names when checking for installation' do
-        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
-        expect(FastlaneCore::Helper).to receive(:backticks).with(name_regex, anything).and_return('')
-    
-            def sidekiq_worker_class
-          ImportNoteWorker
+            # Initializes the system. Any subclasses MUST make sure this
+        # method is called on the parent. Therefore, if a subclass overrides
+        # `initialize`, then you must call `super`.
+        def initialize(vm)
+          @vm = vm
         end
     
-          def call(env)
-        transaction, retval = Transaction.run do
-          @app.call(env)
+            # Returns the internal data associated with this plugin. This
+        # should NOT be called by the general public.
+        #
+        # @return [Hash]
+        def self.data
+          @data ||= {}
         end
     
-            a_length = a_split.length
-        b_length = b_split.length
+              # Make sure we're only working with one VM if single target
+          if options[:single_target] && machines.length != 1
+            @logger.debug('Using primary machine since single target')
+            primary_name = @env.primary_machine_name
+            raise Errors::MultiVMTargetRequired if !primary_name
+            machines = [get_machine.call(primary_name)]
+          end
     
-        def inheritable_copy
-      self.class.new @filters
-    end
+            # This contains all the hosts and their parents.
+        #
+        # @return [Registry<Symbol, Array<Class, Symbol>>]
+        attr_reader :hosts
     
-      def pin_at(version)
-    HOMEBREW_PINNED_KEGS.mkpath
-    version_path = @f.rack/version
-    path.make_relative_symlink(version_path) unless pinned? || !version_path.exist?
-  end
+            # The logger for this class.
+        LOGGER = Log4r::Logger.new('vagrant::plugin::v2::plugin')
     
-          # Checks whether this case statement has an `else` branch.
-      #
-      # @return [Boolean] whether the `case` statement has an `else` branch
-      def else?
-        loc.else
-      end
-    end
-  end
-end
-
-    
-            def delimiter_delta
-          return 0 if first.same_line?(second)
-          return 0 if first.delimiter != second.delimiter
-    
-          dir = if File.directory?(file_or_dir)
-              file_or_dir
-            else
-              File.dirname(file_or_dir)
-            end
-      @path_cache[dir] ||= ConfigLoader.configuration_file_for(dir)
-      path = @path_cache[dir]
-      @object_cache[path] ||= begin
-                                print 'For #{dir}: ' if ConfigLoader.debug?
-                                ConfigLoader.configuration_from_file(path)
-                              end
-    end
-  end
-end
-
-    
-    get '/' do
-  stats = Sidekiq::Stats.new
-  @failed = stats.failed
-  @processed = stats.processed
-  @messages = $redis.lrange('sinkiq-example-messages', 0, -1)
-  erb :index
-end
-    
-          # Pop out a single job and perform it
-      def perform_one
-        raise(EmptyQueueError, 'perform_one called with empty job queue') if jobs.empty?
-        next_job = jobs.first
-        Queues.delete_for(next_job['jid'], queue, self.to_s)
-        process_job(next_job)
-      end
-    
-            run WebApplication.new(klass)
-      end
-    end
-  end
-    
-        def redirect(location)
-      throw :halt, [302, { 'Location' => '#{request.base_url}#{location}' }, []]
+          its(:env) { is_expected.to use_userpaths }
     end
