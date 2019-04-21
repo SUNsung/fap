cc06475906c4bcc8fa5e205eb05809d29967d08c
SUNsung <weatherius@gmail.com>
2019-04-21 14:46:58 +0300

    Rewrite [39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp] File in: 2019-04-21 14:46:58

--- cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
+++ cccccc/39UChgc.keD5c39I7q888hxspQ39hOgycteb6yE396I1.cpp
@@ -1,321 +1,114 @@
 
         
-          /**
-   * @brief Applies the same transformation defined in the data layer's
-   * transform_param block to all the num images in a input_blob.
-   *
-   * @param input_blob
-   *    A Blob containing the data to be transformed. It applies the same
-   *    transformation to all the num images in the blob.
-   * @param transformed_blob
-   *    This is destination blob, it will contain as many images as the
-   *    input blob. It can be part of top blob's data.
-   */
-  void Transform(Blob<Dtype>* input_blob, Blob<Dtype>* transformed_blob);
-    
-      /// @brief The spatial dimensions of the input.
-  inline int input_shape(int i) {
-    return (*bottom_shape_)[channel_axis_ + i];
-  }
-  // reverse_dimensions should return true iff we are implementing deconv, so
-  // that conv helpers know which dimensions are which.
-  virtual bool reverse_dimensions() = 0;
-  // Compute height_out_ and width_out_ from other parameters.
-  virtual void compute_output_shape() = 0;
-    
-    #include <vector>
-    
-    /**
- * @brief Computes a sum of two input Blobs, with the shape of the latter Blob
- *        'broadcast' to match the shape of the former. Equivalent to tiling
- *        the latter Blob, then computing the elementwise sum.
- *
- * The second input may be omitted, in which case it's learned as a parameter
- * of the layer. Note: in case bias and scaling are desired, both operations can
- * be handled by `ScaleLayer` configured with `bias_term: true`.
- */
-template <typename Dtype>
-class BiasLayer : public Layer<Dtype> {
- public:
-  explicit BiasLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+        namespace {
     }
     
-    #include 'caffe/layers/neuron_layer.hpp'
     
-    /**
- * @brief Takes a Blob and crop it, to the shape specified by the second input
- *  Blob, across all dimensions after the specified axis.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
+    {}  // namespace atom
     
-    #include 'caffe/layers/lrn_layer.hpp'
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    grpc::string ChannelArguments::GetSslTargetNameOverride() const {
-  for (unsigned int i = 0; i < args_.size(); i++) {
-    if (grpc::string(GRPC_SSL_TARGET_NAME_OVERRIDE_ARG) == args_[i].key) {
-      return args_[i].value.string;
-    }
-  }
-  return '';
-}
-    
-    class CensusChannelData : public ChannelData {
+    class AutoUpdater : public mate::EventEmitter<AutoUpdater>,
+                    public auto_updater::Delegate,
+                    public WindowListObserver {
  public:
-  grpc_error* Init(grpc_channel_element* elem,
-                   grpc_channel_element_args* args) override;
-};
+  static mate::Handle<AutoUpdater> Create(v8::Isolate* isolate);
+    }
     
-    #endif /* GRPC_INTERNAL_CPP_EXT_FILTERS_CENSUS_CLIENT_FILTER_H */
+    #endif  // ATOM_BROWSER_API_ATOM_API_NET_H_
 
     
-    void SetCreateThreadPool(CreateThreadPoolFunc func) { g_ctp_impl = func; }
+    #include 'content/public/browser/javascript_dialog_manager.h'
     
-    #include <utility>
-    
-    /*!
-+ * \brief The result holder of dispatch mode of each Node in the graph.
-+ * \note Stored under graph.attrs['dispatch_mode'], provided by Pass 'InferStorageType'
-+ *
-+ * \code
-+ *  Graph g = ApplyPass(src_graph, 'InferStorageType');
-+ *  const DispatchModeVector& dispatch_modes = g.GetAttr<DispatchModeVector>('dispatch_mode');
-+ *  // get dispatch mode by entry node id
-+ *  int node_type = dispatch_modes[nid];
-+ * \endcode
-+ *
-+ * \sa FInferStorageType
-+ */
-using DispatchModeVector = std::vector<DispatchMode>;
-    
-    // Initialization funciton called by caffeOp & caffeLoss
-template<typename Dtype>
-void InitCaffeBlobs(std::vector< ::caffe::Blob<Dtype>*>* v, int n_num) {
-  for (index_t i=0; i < n_num; ++i)
-    v->push_back(new ::caffe::Blob<Dtype>());
+    void AtomQuotaPermissionContext::RequestQuotaPermission(
+    const content::StorageQuotaParams& params,
+    int render_process_id,
+    const PermissionCallback& callback) {
+  callback.Run(response::QUOTA_PERMISSION_RESPONSE_ALLOW);
 }
     
-    Graph DetectInplaceAddTo(Graph g) {
-  nnvm::StorageVector storage_id =
-      g.MoveCopyAttr<nnvm::StorageVector>('storage_id');
-  std::vector<int> storage_inplace_index =
-      g.MoveCopyAttr<std::vector<int> >('storage_inplace_index');
-  static const Op* ewise_plus_op = Op::Get('_grad_add');
-  auto& idx = g.indexed_graph();
-  // reference cont.
-  std::vector<int> ref_count(idx.num_node_entries(), 0);
-  std::vector<int> addto_entry(idx.num_node_entries(), 0);
-  std::vector<int> skip_plus_node(idx.num_nodes(), 0);
-    }
+    #ifndef ATOM_BROWSER_LIB_POWER_OBSERVER_H_
+#define ATOM_BROWSER_LIB_POWER_OBSERVER_H_
     
-      /*! \brief internal next function, inlined for fater processing. */
-  inline bool Next_(void) {
-    if (!base_->Next()) return false;
-    const DataInst &src = base_->Value();
-    this->SetOutImg(src);
-    out_.data.resize(2);
-    out_.data[0] = outimg_;
-    out_.data[1] = src.data[1];
-    out_.index = src.index;
-    out_.extra_data = src.extra_data;
-    return true;
-  }
-  /*!
-   * \brief Set the output image, after augmentation and normalization.
-   * \param src The source image.
-   */
-  inline void SetOutImg(const DataInst &src) {
-    using namespace mshadow::expr;  // NOLINT(*)
-    mshadow::Tensor<cpu, 3> data = src.data[0].get<cpu, 3, real_t>();
-    }
+    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
+                                       net::NetworkDelegate* network_delegate)
+    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
     
-    template<>
-void EvalRandom<DEVICE, GaussianDistribution>(
-    const real_t &mu,
-    const real_t &sigma,
-    const Resource &resource,
-    TBlob *ret,
-    RunContext ctx) {
-  typedef DEVICE xpu;
-  mshadow::Stream<xpu> *s = ctx.get_stream<xpu>();
-  switch (ret->type_flag_) {
-  case mshadow::kFloat32:
-    {
-      mshadow::Random<xpu, float> *prnd = resource.get_random<xpu, float>(s);
-      mshadow::Tensor<xpu, 2, float> tmp = ret->FlatTo2D<xpu, float>(s);
-      prnd->SampleGaussian(&tmp, float(mu), float(sigma));  // NOLINT(*)
-      break;
-    }
-  case mshadow::kFloat64:
-    {
-      mshadow::Random<xpu, double> *prnd = resource.get_random<xpu, double>(s);
-      mshadow::Tensor<xpu, 2, double> tmp = ret->FlatTo2D<xpu, double>(s);
-      prnd->SampleGaussian(&tmp, double(mu), double(sigma));  // NOLINT(*)
-      break;
-    }
-  default:
-    LOG(FATAL) << 'Random only support float32 and float64';
+    OffscreenViewProxy::~OffscreenViewProxy() {
+  if (observer_) {
+    observer_->ProxyViewDestroyed(this);
   }
 }
     
-    namespace mxnet {
-namespace op {
-    }
-    }
+    #include 'atom/browser/relauncher.h'
     
-    Crop the 2nd and 3rd dim of input data, with the corresponding size of h_w or
-with width and height of the second input symbol, i.e., with one input, we need h_w to
-specify the crop height and width, otherwise the second input symbol's size will be used
-)code' ADD_FILELINE)
-    
-      CustomOpPropCreator Find(const std::string &op_type) {
-    std::lock_guard<std::mutex> lock(mutex_);
-    auto it = registry_.find(op_type);
-    if (it != registry_.end()) return it->second;
-    return nullptr;
-  }
-    
-    MXNET_REGISTER_OP_PROPERTY(_Native, NativeOpProp)
-.describe('Stub for implementing an operator implemented in native frontend language.')
-.add_argument('data', 'NDArray-or-Symbol[]', 'Input data for the custom operator.')
-.add_arguments(NativeOpParam::__FIELDS__());
+    #define  FARG_LINE(type, n) , const type * src##n##Base, ptrdiff_t src##n##Stride
     
     /*!
- * \brief Macro to register linear updater.
+ *  Aligns pointer by the certain number of bytes
+ *
+ *  This small inline function aligns the pointer by the certain number of bytes by shifting
+ *  it forward by 0 or a positive offset.
  */
-#define XGBOOST_REGISTER_LINEAR_UPDATER(UniqueId, Name)                        \
-  static DMLC_ATTRIBUTE_UNUSED ::xgboost::LinearUpdaterReg&                    \
-      __make_##LinearUpdaterReg##_##UniqueId##__ =                             \
-          ::dmlc::Registry< ::xgboost::LinearUpdaterReg>::Get()->__REGISTER__( \
-              Name)
+template<typename T> inline T* alignPtr(T* ptr, size_t n=sizeof(T))
+{
+    return (T*)(((size_t)ptr + n-1) & -n);
+}
     
-    template<typename IndexType>
-class DensifyParser : public dmlc::Parser<IndexType> {
- public:
-  DensifyParser(dmlc::Parser<IndexType>* parser, uint32_t num_col)
-      : parser_(parser), num_col_(num_col) {
-  }
+    
+    {} // namespace
+    
+            cornerpos[-1] = ncorners;
+    
+        // the others
+    for (size_t i = 1; i < size.height ; ++i)
+    {
+        src = internal::getRowPtr(srcBase, srcStride, i);
+        u32 * prevSum = internal::getRowPtr(sumBase, sumStride, i - 1);
+        sum = internal::getRowPtr(sumBase, sumStride, i);
     }
     
-    #include <dmlc/registry.h>
-#include <vector>
-#include <utility>
-#include <string>
-#include <functional>
-#include './data.h'
-#include './base.h'
-#include '../../src/common/host_device_vector.h'
-    
-    /// Brute Force
-/// Time Complexity: O(n^2)
-/// Space Complexity: O(1)
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-    }
-    }
-    
-        cout << endl;
-    
-            stack<char> stack;
-        for( int i = 0 ; i < s.size() ; i ++ )
-            if( s[i] == '(' || s[i] == '{' || s[i] == '[')
-                stack.push(s[i]);
-            else{
-    }
-    }
-    
-    int main() {
-    }
-    
-    /// Definition for a binary tree node.
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+    #include 'modules/drivers/radar/conti_radar/protocol/cluster_general_info_701.h'
     
     
     {
     {
-    {            cur = stack.top();
-            stack.pop();
-            cur = cur->right;
-        }
-        return res;
+    {}  // namespace conti_radar
+}  // namespace drivers
+}  // namespace apollo
+
+    
+    namespace apollo {
+namespace drivers {
+namespace conti_radar {
     }
-};
-    
-    
-    
-            stack.push(root);
-        while(!stack.empty()){
+    }
     }
     
-    #include 'gtest/gtest.h'
     
-    #include 'modules/drivers/canbus/can_comm/message_manager.h'
-    
-    
-    {  Byte t1(bytes + 3);
-  uint32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-  double ret = x * CLUSTER_DIST_RES + CLUSTER_DIST_LAT_MIN;
-  return ret;
+    {  // Report Messages
+  AddRecvProtocolData<Accelrpt68, true>();
+  AddRecvProtocolData<Brakemotorrpt170, true>();
+  AddRecvProtocolData<Brakemotorrpt271, true>();
+  AddRecvProtocolData<Brakemotorrpt372, true>();
+  AddRecvProtocolData<Brakerpt6c, true>();
+  AddRecvProtocolData<Datetimerpt83, true>();
+  AddRecvProtocolData<Globalrpt6a, true>();
+  AddRecvProtocolData<Headlightrpt77, true>();
+  AddRecvProtocolData<Hornrpt79, true>();
+  AddRecvProtocolData<Latlonheadingrpt82, true>();
+  AddRecvProtocolData<Parkingbrakestatusrpt80, true>();
+  AddRecvProtocolData<Shiftrpt66, true>();
+  AddRecvProtocolData<Steeringmotorrpt173, true>();
+  AddRecvProtocolData<Steeringmotorrpt274, true>();
+  AddRecvProtocolData<Steeringmotorrpt375, true>();
+  AddRecvProtocolData<Steeringrpt16e, true>();
+  AddRecvProtocolData<Turnrpt64, true>();
+  AddRecvProtocolData<Vehiclespeedrpt6f, true>();
+  AddRecvProtocolData<Wheelspeedrpt7a, true>();
+  AddRecvProtocolData<Wiperrpt91, true>();
+  AddRecvProtocolData<Yawraterpt81, true>();
 }
     
-    
-    {  int ret = x;
-  return ret;
-}
-    
-      Byte t1(bytes + 2);
-  uint32_t t = t1.get_byte(6, 2);
-  x <<= 2;
-  x |= t;
-    
-    TEST_F(SpeedLimitTest, GetSpeedLimitByS) {
-  EXPECT_EQ(speed_limit_.speed_limit_points().size(), 100);
-  double s = 0.0;
-  const double ds = 0.01;
-  while (s < 99.0) {
-    double v_limit = speed_limit_.GetSpeedLimitByS(s);
-    }
-    }
-    
-    #include 'modules/prediction/container/obstacles/obstacle_clusters.h'
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    // config detail: {'name': 'torque_output', 'offset': 0.0, 'precision': 0.001,
-// 'len': 32, 'is_signed_var': True, 'physical_range':
-// '[-2147483.648|2147483.647]', 'bit': 7, 'type': 'double', 'order':
-// 'motorola', 'physical_unit': 'N-m'}
-double Brakemotorrpt372::torque_output(const std::uint8_t* bytes,
-                                       int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 8);
-    }
-    
-      Byte t1(bytes + 1);
-  int32_t t = t1.get_byte(0, 8);
+      Byte t3(bytes + 7);
+  t = t3.get_byte(0, 8);
   x <<= 8;
   x |= t;
