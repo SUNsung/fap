2825f6363e6025973c884b93943f0c17c7b4470b
SUNsung <weatherius@gmail.com>
2019-04-21 20:34:31 +0300

    Rewrite [59os555kFUxhw59ySEdjMDoAn259EQ5JjyZWBI.59kXx.py] File in: 2019-04-21 20:34:31

--- pyton/59os555kFUxhw59ySEdjMDoAn259EQ5JjyZWBI.59kXx.py
+++ pyton/59os555kFUxhw59ySEdjMDoAn259EQ5JjyZWBI.59kXx.py
@@ -1,66 +1,68 @@
 
         
-            def save(self, session_key, session_dict, expire_date):
-        s = self.model(session_key, self.encode(session_dict), expire_date)
-        if session_dict:
-            s.save()
-        else:
-            s.delete()  # Clear sessions with no data.
-        return s
-    
-    
-if __name__ == '__main__':
-    main()
+        
+def init_app(app):
+    '''Register database functions with the Flask app. This is called by
+    the application factory.
+    '''
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
     
     
-if __name__ == '__main__':
-    main()
-
+@pytest.mark.parametrize('path', (
+    '/create',
+    '/1/update',
+    '/1/delete',
+))
+def test_login_required(client, path):
+    response = client.post(path)
+    assert response.headers['Location'] == 'http://localhost/auth/login'
     
-    - oneandone_moitoring_policy:
-    auth_token: oneandone_private_api_key
-    monitoring_policy: ansible monitoring policy updated
-    update_ports:
-     -
-       id: existing_port_id
-       protocol: TCP
-       port: 34
-       alert_if: RESPONDING
-       email_notification: false
-     -
-       id: existing_port_id
-       protocol: TCP
-       port: 23
-       alert_if: RESPONDING
-       email_notification: false
-    wait: true
-    state: update
-    
-    
-DOCUMENTATION = '''
----
-module: ipa_dnszone
-author: Fran Fitzpatrick (@fxfitz)
-short_description: Manage FreeIPA DNS Zones
-description:
-- Add and delete an IPA DNS Zones using IPA API
-options:
-  zone_name:
-    description:
-    - The DNS zone name to which needs to be managed.
-    required: true
-  state:
-    description: State to ensure
-    required: false
-    default: present
-    choices: ['present', 'absent']
-extends_documentation_fragment: ipa.documentation
-version_added: '2.5'
+    :copyright: © 2010 by the Pallets team.
+:license: BSD, see LICENSE for more details.
 '''
     
-    ANSIBLE_METADATA = {'metadata_version': '1.1',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
     
-        module.exit_json(changed=False, msg='logs(s) already unfollowed')
+class Environment(BaseEnvironment):
+    '''Works like a regular Jinja2 environment but has some additional
+    knowledge of how Flask's blueprint works so that it can prepend the
+    name of the blueprint to referenced templates if necessary.
+    '''
+    
+    
+class View(object):
+    '''Alternative way to use view functions.  A subclass has to implement
+    :meth:`dispatch_request` which is called with the view arguments from
+    the URL routing system.  If :attr:`methods` is provided the methods
+    do not have to be passed to the :meth:`~flask.Flask.add_url_rule`
+    method explicitly::
+    
+    import pytest
+    
+        def __init__(self):
+        self.name = 'citext'
+    
+        def __init__(self, get_response=None):
+        if not apps.is_installed('django.contrib.sites'):
+            raise ImproperlyConfigured(
+                'You cannot use RedirectFallbackMiddleware when '
+                'django.contrib.sites is not installed.'
+            )
+        super().__init__(get_response)
+    
+            if data is None:
+            s = self._get_session_from_db()
+            if s:
+                data = self.decode(s.session_data)
+                self._cache.set(self.cache_key, data, self.get_expiry_age(expiry=s.expire_date))
+            else:
+                data = {}
+        return data
+    
+    import simple_http_client
+from xlog import getLogger
+xlog = getLogger('gae_proxy')
+    
+        def __init__(self, stream, first, last):
+        ReplaceOp.__init__(self, stream, first, last, None)
