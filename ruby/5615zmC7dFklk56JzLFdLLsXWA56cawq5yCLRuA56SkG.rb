cee9f780843b036f86dc13797d667ab6f995bcea
SUNsung <weatherius@gmail.com>
2019-04-21 06:05:47 +0300

    Rewrite [5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb] File in: 2019-04-21 06:05:47

--- ruby/5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb
+++ ruby/5615zmC7dFklk56JzLFdLLsXWA56cawq5yCLRuA56SkG.rb
@@ -1,246 +1,83 @@
 
         
-                  def checked?(value)
-            case value
-            when TrueClass, FalseClass
-              value == !!@checked_value
-            when NilClass
-              false
-            when String
-              value == @checked_value
-            else
-              if value.respond_to?(:include?)
-                value.include?(@checked_value)
-              else
-                value.to_i == @checked_value.to_i
-              end
-            end
-          end
+              dt = Date.today
+      freeze_time dt
     
-            class RadioButtonBuilder < Builder # :nodoc:
-          def radio_button(extra_html_options = {})
-            html_options = extra_html_options.merge(@input_html_options)
-            html_options[:skip_default_ids] = false
-            @template_object.radio_button(@object_name, @method_name, @value, html_options)
-          end
-        end
+        false
+  end
     
-                options = options.dup
-            options[:field_name]           = @method_name
-            options[:include_position]     = true
-            options[:prefix]             ||= @object_name
-            options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
+    class FormulaPin
+  def initialize(f)
+    @f = f
+  end
     
-    desc 'Open an irb session preloaded with this library'
-task :console do
-  sh 'irb -rubygems -r ./lib/#{name}.rb'
-end
-
+          # Returns the operator for the `kwsplat` as a string.
+      #
+      # @return [String] the double splat operator
+      def operator
+        DOUBLE_SPLAT
+      end
     
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
+          # A shorthand for getting the last argument of the node.
+      # Equivalent to `arguments.last`.
+      #
+      # @return [Node, nil] the last argument of the node,
+      #                     or `nil` if there are no arguments
+      def last_argument
+        arguments[-1]
+      end
     
-                # stream_file_data would free us from keeping livereload.js in memory
-            # but JRuby blocks on that call and never returns
-            send_data(reload_body)
-            close_connection_after_writing
+          def self.for(class_name)
+        new(*class_name.split('::').last(2))
+      end
+    
+          def header
+        if @header.nil?
+          if page = @page.header
+            @header = page.text_data
           else
-            body = 'This port only serves livereload.js over HTTP.\n'
-            headers = [
-              'HTTP/1.1 400 Bad Request',
-              'Content-Type: text/plain',
-              'Content-Length: #{body.bytesize}',
-              '',
-              '',
-            ].join('\r\n')
-            send_data(headers)
-            send_data(body)
-            close_connection_after_writing
+            @header = false
           end
         end
-        # rubocop:enable Metrics/MethodLength
+        @header
       end
+    
+          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
+      def left_shift(int, shift)
+        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
+        # 1>>31, 2**32
+        (r & 2147483648) == 0 ? r : r - 4294967296
+      end
+    
+          def editable
+        @editable
+      end
+    
+      test 'extracting paths from URLs' do
+    assert_nil extract_path('Eye-Of-Sauron')
+    assert_equal 'Mordor', extract_path('Mordor/Sauron')
+    assert_equal 'Mordor/Sauron', extract_path('Mordor/Sauron/Evil')
+  end
+    
+      s.add_development_dependency 'rack-test', '~> 0.6.2'
+  s.add_development_dependency 'shoulda', '~> 3.5.0'
+  s.add_development_dependency 'minitest-reporters', '~> 0.14.16'
+  s.add_development_dependency 'twitter_cldr', '~> 3.2.0'
+  s.add_development_dependency 'mocha', '~> 1.1.0'
+  s.add_development_dependency 'test-unit', '~> 3.1.0'
+  s.add_development_dependency 'webrick', '~> 1.3.1'
+    
+          within_row(2) { click_icon :split }
+      targetted_select2 'LA(#{order.reload.shipments.last.number})', from: '#s2id_item_stock_location'
+      click_icon :save
+      wait_for_ajax
+      expect(page.find('#shipment_#{order.reload.shipments.last.id}')).to be_present
     end
   end
 end
 
     
-                @config['syntax_highlighter_opts'] = begin
-              strip_coderay_prefix(
-                @config['syntax_highlighter_opts'] \
-                  .merge(CODERAY_DEFAULTS) \
-                  .merge(@config['coderay'])
-              )
-            end
-          end
-        end
-      end
-    end
-  end
-end
-
-    
-        def no_subcommand(args)
-      unless args.empty? ||
-          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
-        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
-                          Run `jekyll help` to find out more.'
-        abort
-      end
-    end
-    
-        # The path used after unlocking the resource
-    def after_unlock_path_for(resource)
-      new_session_path(resource)  if is_navigational_format?
-    end
-    
-      def app
-    Rails.application
-  end
-end
-
-    
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
-        end
-      end
-    
-          def mailer_sender(mapping, sender = :from)
-        default_sender = default_params[sender]
-        if default_sender.present?
-          default_sender.respond_to?(:to_proc) ? instance_eval(&default_sender) : default_sender
-        elsif Devise.mailer_sender.is_a?(Proc)
-          Devise.mailer_sender.call(mapping.name)
-        else
-          Devise.mailer_sender
-        end
-      end
-    
-    require 'devise/hooks/timeoutable'
-    
-        def index
-      authorize :email_domain_block, :index?
-      @email_domain_blocks = EmailDomainBlock.page(params[:page])
-    end
-    
-    class Api::SubscriptionsController < Api::BaseController
-  before_action :set_account
-  respond_to :txt
-    
-        render json: @web_subscription, serializer: REST::WebPushSubscriptionSerializer
-  end
-    
-        render_empty
-  end
-    
-      def export_data
-    raise 'Override in controller'
-  end
-    
-      def request_locale
-    preferred_locale || compatible_locale
-  end
-    
-      def rate_limit_remaining
-    (api_throttle_data[:limit] - api_throttle_data[:count]).to_s
-  end
-    
-        change.down do
-      Notification.where(type: 'Notifications::MentionedInPost').update_all(type: 'Notifications::Mentioned')
-      Mention.where(mentions_container_type: 'Comment').destroy_all
-      Notification.where(type: 'Notifications::MentionedInComment').destroy_all
-    end
-  end
-end
-
-    
-      failure_message_for_should do |actual|
-    'expected #{actual.inspect} to have path in #{expected.inspect} but was #{actual.current_path.inspect}'
-  end
-  failure_message_for_should_not do |actual|
-    'expected #{actual.inspect} to not have path in #{expected.inspect} but it had'
-  end
-end
-    
-      def post_path_by_content(text)
-    p = Post.find_by_text(text)
-    post_path(p)
-  end
-    
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
-    end
-    
-        desc 'Release all gems as packages'
-    task :all => [:test, :commit_version] + GEMS_AND_ROOT_DIRECTORIES.keys
-  end
-end
-
-    
-    get '/stream', :provides => 'text/event-stream' do
-  stream :keep_open do |out|
-    connections << out
-    out.callback { connections.delete(out) }
-  end
-end
-    
-      trap('INT') {
-    [jekyllPid, compassPid].each { |pid| Process.kill(9, pid) rescue Errno::ESRCH }
-    exit 0
-  }
-    
-    # The project root directory
-$root = ::File.dirname(__FILE__)
-    
-        def paragraphize(input)
-      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
-    end
-  end
-end
-    
-          if File.symlink?(code_path)
-        return 'Code directory '#{code_path}' cannot be a symlink'
-      end
-    
-    Liquid::Template.register_tag('render_partial', Jekyll::RenderPartialTag)
-
-    
-        def initialize(options)
-      @strictly_ordered_queues = !!options[:strict]
-      @queues = options[:queues].map { |q| 'queue:#{q}' }
-      if @strictly_ordered_queues
-        @queues = @queues.uniq
-        @queues << TIMEOUT
-      end
-    end
-    
-          # Provide a call() method that returns the formatted message.
-      def call(severity, time, program_name, message)
-        '#{time.utc.iso8601(3)} #{::Process.pid} TID-#{Sidekiq::Logging.tid}#{context} #{severity}: #{message}\n'
-      end
-    
-          def enabled?
-        self.__test_mode != :disable
-      end
-    
-          erb(content, options)
-    end
+            def create
+          authorize! :create, Product
+          params[:product][:available_on] ||= Time.current
+          set_up_shipping_category
