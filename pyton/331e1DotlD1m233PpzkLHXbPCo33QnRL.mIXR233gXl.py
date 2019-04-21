ac85deb2616b5a6bcef83b00382097d52c2fa84a
SUNsung <weatherius@gmail.com>
2019-04-21 14:07:30 +0300

    Rewrite [331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py] File in: 2019-04-21 14:07:29

--- pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
+++ pyton/331e1DotlD1m233PpzkLHXbPCo33QnRL.mIXR233gXl.py
@@ -1,118 +1,113 @@
 
         
-        try:
-    # https://urllib3.readthedocs.io/en/latest/security.html
-    # noinspection PyPackageRequirements
-    import urllib3
-    urllib3.disable_warnings()
-except (ImportError, AttributeError):
-    # In some rare cases, the user may have an old version of the requests
-    # or urllib3, and there is no method called 'disable_warnings.' In these
-    # cases, we don't need to call the method.
-    # They may get some noisy output but execution shouldn't die. Move on.
-    pass
+          labels = tf.squeeze(curr_features['rel_id'], [-1])
+  return labels
     
-        '''
-    req_h = OUT_REQ_HEAD in output_options
-    req_b = OUT_REQ_BODY in output_options
-    resp_h = OUT_RESP_HEAD in output_options
-    resp_b = OUT_RESP_BODY in output_options
-    req = req_h or req_b
-    resp = resp_h or resp_b
+        for index, instance in enumerate(inputs):
+    
+    # Sometimes the task can be sufficiently hard to learn that the
+# optimizer takes the 'easy route', and simply minimizes the KL
+# divergence, setting it to near zero, and the optimization gets
+# stuck.  These two parameters will help avoid that by by getting the
+# optimization to 'latch' on to the main optimization, and only
+# turning in the regularizers later.
+flags.DEFINE_integer('kl_start_step', KL_START_STEP,
+                     'Start increasing weight after this many steps.')
+# training passes, not epochs, increase by 0.5 every kl_increase_steps
+flags.DEFINE_integer('kl_increase_steps', KL_INCREASE_STEPS,
+                     'Increase weight of kl cost to avoid local minimum.')
+# Same story for l2 regularizer.  One wants a simple generator, for scientific
+# reasons, but not at the expense of hosing the optimization.
+flags.DEFINE_integer('l2_start_step', L2_START_STEP,
+                     'Start increasing l2 weight after this many steps.')
+flags.DEFINE_integer('l2_increase_steps', L2_INCREASE_STEPS,
+                     'Increase weight of l2 cost to avoid local minimum.')
+    
+    # Getting hard vs. easy data can be a little stochastic, so we set the seed.
+    
+    x0s = []
+condition_labels = []
+condition_number = 0
+for c in range(C):
+  x0 = FLAGS.x0_std * rng.randn(N, 1)
+  x0s.append(np.tile(x0, nreplications))
+  for ns in range(nreplications):
+    condition_labels.append(condition_number)
+  condition_number += 1
+x0s = np.concatenate(x0s, axis=1)
+    
+      Raises:
+    ValueError: if batch_size or num_steps are too high.
+  '''
+  del epoch_size_override
+  data_len = len(raw_data)
+  num_batches = data_len // batch_size - 1
+    
+      elif FLAGS.baseline_method is None:
+    num_missing = tf.reduce_sum(missing)
+    final_gen_objective += tf.reduce_sum(rewards) / (num_missing + eps)
+    baselines = tf.zeros_like(rewards)
+    critic_loss = None
+    maintain_averages_op = None
+    advantages = cumulative_rewards
+    
+      Args:
+    name: to label variables.
+    num_units: hidden state dimension.
+    attention_option: how to compute attention, either 'luong' or 'bahdanau'.
+      'bahdanau': additive (Bahdanau et al., ICLR'2015)
+      'luong': multiplicative (Luong et al., EMNLP'2015)
+    reuse: whether to reuse variable scope.
+    dtype: (default: `tf.float32`) data type to use.
+    
+        if not FLAGS.dis_share_embedding:
+      embedding = tf.get_variable('embedding',
+                                  [FLAGS.vocab_size, hparams.dis_rnn_size])
+    
+            :param mime: E.g., 'application/atom+xml'.
+        :param content: The body content as text
+    
+        # noinspection PyMethodOverriding
+    def get_auth(self, username, password):
+        return HTTPBasicAuth(username, password)
     
     
-def test_current_version():
-    version = Environment().config['__meta__']['httpie']
-    assert version == __version__
-
+def repr_dict_nice(d):
+    def prepare_dict(d):
+        for k, v in d.items():
+            if isinstance(v, dict):
+                v = dict(prepare_dict(v))
+            elif isinstance(v, bytes):
+                v = v.decode('utf8')
+            elif not isinstance(v, (int, str)):
+                v = repr(v)
+            yield k, v
+    return json.dumps(
+        dict(prepare_dict(d)),
+        indent=4, sort_keys=True,
+    )
     
-        def test_print_overridable_when_stdout_redirected(self, httpbin):
-        env = MockEnvironment(stdin_isatty=True, stdout_isatty=False)
-        r = http('--print=h', 'GET', httpbin.url + '/get', env=env)
-        assert HTTP_OK in r
-
     
-    error_msg = None
+FIXTURES_ROOT = path.join(path.abspath(path.dirname(__file__)))
+FILE_PATH = path.join(FIXTURES_ROOT, 'test.txt')
+JSON_FILE_PATH = path.join(FIXTURES_ROOT, 'test.json')
+BIN_FILE_PATH = path.join(FIXTURES_ROOT, 'test.bin')
     
     
-def test_follow_all_redirects_shown(httpbin):
-    r = http('--follow', '--all', httpbin.url + '/redirect/2')
-    assert r.count('HTTP/1.1') == 3
-    assert r.count('HTTP/1.1 302 FOUND', 2)
+def test_unicode_raw_json_item(httpbin):
+    r = http('--json', 'POST', httpbin.url + '/post',
+             u'test:={ '%s' : [ '%s' ] }' % (UNICODE, UNICODE))
     assert HTTP_OK in r
-    
-        try:
-        response = requests_session.request(**kwargs)
-    except Exception:
-        raise
-    else:
-        # Existing sessions with `read_only=True` don't get updated.
-        if session.is_new() or not read_only:
-            session.cookies = requests_session.cookies
-            session.save()
-        return response
+    assert r.json['json'] == {'test': {UNICODE: [UNICODE]}}
     
         '''
-    if content_range is None:
-        raise ContentRangeError('Missing Content-Range')
-    
-            r = None
-        try:
-            r = Redirect.objects.get(site=current_site, old_path=full_path)
-        except Redirect.DoesNotExist:
-            pass
-        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
-            try:
-                r = Redirect.objects.get(
-                    site=current_site,
-                    old_path=request.get_full_path(force_append_slash=True),
-                )
-            except Redirect.DoesNotExist:
-                pass
-        if r is not None:
-            if r.new_path == '':
-                return self.response_gone_class()
-            return self.response_redirect_class(r.new_path)
-    
-        def delete(self, session_key=None):
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        self._cache.delete(self.cache_key_prefix + session_key)
-    
-        For complete documentation on using Sessions in your code, consult
-    the sessions documentation that is shipped with Django (also available
-    on the Django Web site).
-    '''
-    objects = SessionManager()
-    
-                tested_methods = conman.tested_methods_from_spidercls(spidercls)
-            if opts.list:
-                for method in tested_methods:
-                    contract_reqs[spidercls.name].append(method)
-            elif tested_methods:
-                self.crawler_process.crawl(spidercls)
-    
-        def short_desc(self):
-        return 'Edit spider'
-    
-        class _v19_S3Connection(S3Connection):
-        '''A dummy S3Connection wrapper that doesn't do any synchronous download'''
-        def _mexe(self, method, bucket, key, headers, *args, **kwargs):
-            return headers
-    
-    openssl_methods = {
-    METHOD_TLS:    SSL.SSLv23_METHOD,                   # protocol negotiation (recommended)
-    METHOD_SSLv3:  SSL.SSLv3_METHOD,                    # SSL 3 (NOT recommended)
-    METHOD_TLSv10: SSL.TLSv1_METHOD,                    # TLS 1.0 only
-    METHOD_TLSv11: getattr(SSL, 'TLSv1_1_METHOD', 5),   # TLS 1.1 only
-    METHOD_TLSv12: getattr(SSL, 'TLSv1_2_METHOD', 6),   # TLS 1.2 only
-}
     
     
-def leaky_relu(x, alpha=0.1):
-    '''渗透 ReLU
-    `o = max(alpha * x, x)`
-    '''
-    return tf.nn.leaky_relu(x, alpha)
+def __kilometers_to_meters(kilometers: float) -> float:
+    '''Convert kilometers to meters.'''
+    return kilometers * 1000
+    
+        # with patch('homeassistant.config.async_hass_config_yaml',
+    #            Mock(return_value=mock_coro(config))):
+    #     yield from hass.services.async_call(
+    #         automation.DOMAIN, automation.SERVICE_RELOAD, blocking=True)
