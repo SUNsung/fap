dafdc75b177d55800f6e5f60197d703c57e9d052
SUNsung <weatherius@gmail.com>
2019-04-22 13:52:01 +0300

    Rewrite [30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb] File in: 2019-04-22 13:52:01

--- ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
+++ ruby/30Ll.RqfXta8E30YS1oMHia6yE30DcG6KlFWCDQ30HyN.rb
@@ -1,35 +1,11 @@
 
         
-            # If the package output (-p flag) is a directory, write to the default file name
-    # but inside that directory.
-    if ! package.nil? && File.directory?(package)
-      package_file = File.join(package, output.to_s)
-    else
-      package_file = output.to_s(package)
-    end
+                if parallel?
+          raise RateLimitError
+        else
+          sleep(rate_limit_resets_in)
+        end
+      end
     
-      # scripts_path and write_scripts cribbed from deb.rb
-  def scripts_path(path=nil)
-    @scripts_path ||= build_path('Scripts')
-    FileUtils.mkdir(@scripts_path) if !File.directory?(@scripts_path)
-    
-        # Evaluate dependencies.
-    if !attributes[:no_auto_depends?]
-	    pkgdepend_gen = safesystemout('pkgdepend', 'generate',  '-md', '#{staging_path}',  manifest_fn)
-      File.write(build_path('#{name}.p5m.3'), pkgdepend_gen)
-    
-        if File.exists?(params[:output])
-      # TODO(sissel): Allow folks to choose output?
-      logger.error('Puppet module directory '#{params[:output]}' already ' \
-                    'exists. Delete it or choose another output (-p flag)')
-    end
-    
-      # Output a zipfile.
-  def output(output_path)
-    output_check(output_path)
-    realpath = Pathname.new(output_path).realdirpath.to_s
-    ::Dir.chdir(staging_path) do
-      safesystem('zip', '-9r', realpath, '.')
-    end
-  end # def output
-end # class FPM::Package::Tar
+              new(hash)
+        end
