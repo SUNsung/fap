1b7d7a4ef1fda9d5153cf9712d642142b3a5bb1a
SUNsung <weatherius@gmail.com>
2019-04-22 17:22:37 +0300

    Rewrite [01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py] File in: 2019-04-22 17:22:36

--- pyton/01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py
+++ pyton/01MvdAqlSHeE201GbhSugxz9gQ01gy3YYb2RoT.014xq.py
@@ -1,44 +1,106 @@
 
         
+            proc.sendline(u'fuck')
+    assert proc.expect([TIMEOUT, u'git show'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git push'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git help'])
+    proc.send('\033[A')
+    assert proc.expect([TIMEOUT, u'git push'])
+    proc.send('\033[B')
+    assert proc.expect([TIMEOUT, u'git help'])
+    proc.send('\n')
+    
+    
+@pytest.fixture(params=containers)
+def proc(request, spawnu, TIMEOUT):
+    proc = spawnu(*request.param)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(u'tcsh')
+    proc.sendline(u'setenv PYTHONIOENCODING utf8')
+    proc.sendline(u'eval `thefuck --alias`')
+    return proc
+    
+    
+@pytest.mark.skipif(_is_not_okay_to_test(),
+                    reason='No need to run if there\'s no formula')
+def test_get_new_command(brew_no_available_formula):
+    assert get_new_command(Command('brew install elsticsearch',
+                                   brew_no_available_formula))\
+        == 'brew install elasticsearch'
+    
+    In the second benchmark, we increase the number of dimensions of the
+training set, classify a sample and plot the time taken as a function
+of the number of dimensions.
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import gc
+from datetime import datetime
+    
+    
+def benchmark_influence(conf):
+    '''
+    Benchmark influence of :changing_param: on both MSE and latency.
+    '''
+    prediction_times = []
+    prediction_powers = []
+    complexities = []
+    for param_value in conf['changing_param_values']:
+        conf['tuned_params'][conf['changing_param']] = param_value
+        estimator = conf['estimator'](**conf['tuned_params'])
+        print('Benchmarking %s' % estimator)
+        estimator.fit(conf['data']['X_train'], conf['data']['y_train'])
+        conf['postfit_hook'](estimator)
+        complexity = conf['complexity_computer'](estimator)
+        complexities.append(complexity)
+        start_time = time.time()
+        for _ in range(conf['n_samples']):
+            y_pred = estimator.predict(conf['data']['X_test'])
+        elapsed_time = (time.time() - start_time) / float(conf['n_samples'])
+        prediction_times.append(elapsed_time)
+        pred_score = conf['prediction_performance_computer'](
+            conf['data']['y_test'], y_pred)
+        prediction_powers.append(pred_score)
+        print('Complexity: %d | %s: %.4f | Pred. Time: %fs\n' % (
+            complexity, conf['prediction_performance_label'], pred_score,
+            elapsed_time))
+    return prediction_powers, prediction_times, complexities
+    
+    n_clusters = 3
+    
+            elif self.values[key] == data:
+            pass
+    
+    def get_initial_centroids(data, k, seed=None):
+    '''Randomly choose k data points as initial centroids'''
+    if seed is not None: # useful for obtaining consistent results
+        np.random.seed(seed)
+    n = data.shape[0] # number of data points
         
-def __feet_to_meters(feet: float) -> float:
-    '''Convert feet to meters.'''
-    return feet * 0.3048
+    # Pick K indices from range [0, N).
+    rand_indices = np.random.randint(0, n, k)
     
-        elif bump_type == 'dev':
-        # Convert 0.67.3 to 0.67.4.dev0
-        # Convert 0.67.3.b5 to 0.67.4.dev0
-        # Convert 0.67.3.dev0 to 0.67.3.dev1
-        if version.is_devrelease:
-            to_change['dev'] = ('dev', version.dev + 1)
-        else:
-            to_change['pre'] = ('dev', 0)
-            to_change['release'] = _bump_release(version.release, 'minor')
+    # Keep centroids as dense format, as many entries will be nonzero due to averaging.
+    # As long as at least one document in a cluster contains a word,
+    # it will carry a nonzero weight in the TF-IDF vector of the centroid.
+    centroids = data[rand_indices,:]
     
-    def get_extension(link):
-    extension = os.path.splitext(link)[1][1:]
-    if extension in ['pdf', 'html']:
-        return extension
-    if 'pdf' in extension:
-        return 'pdf'    
-    return 'pdf'    
+    return centroids
     
-        def __call__(self, text, **kargs):
-        words = jieba.tokenize(text, mode='search')
-        token = Token()
-        for (w, start_pos, stop_pos) in words:
-            if not accepted_chars.match(w) and len(w) <= 1:
-                continue
-            token.original = token.text = w
-            token.pos = start_pos
-            token.startchar = start_pos
-            token.endchar = stop_pos
-            yield token
+        difference = predict - actual
+    square_diff = np.square(difference)
+    mean_square_diff = square_diff.mean()
+    score = np.sqrt(mean_square_diff)
+    return score
     
-            for y in obs_states:
-            prob, state = max((V[t - 1][y0] + trans_p[y0].get(y, MIN_INF) +
-                               emit_p[y].get(obs[t], MIN_FLOAT), y0) for y0 in prev_states)
-            V[t][y] = prob
-            mem_path[t][y] = state
     
-    file_name = args[0]
+def random_letters(ctbi, i):
+  pass  # Put your code here...
+    
+      line = clean_lines.elided[linenum]
+  if (line[pos] not in '({[<') or Match(r'<[<=]', line[pos:]):
+    return (line, clean_lines.NumLines(), -1)
+    }
