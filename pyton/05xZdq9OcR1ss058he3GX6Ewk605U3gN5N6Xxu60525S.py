811ab14ee26f467dc4e8141fc762d67578743780
SUNsung <weatherius@gmail.com>
2019-04-23 02:40:59 +0300

    Rewrite [05xZdq9OcR1ss058he3GX6Ewk605U3gN5N6Xxu60525S.py] File in: 2019-04-23 02:40:58

--- pyton/05xZdq9OcR1ss058he3GX6Ewk605U3gN5N6Xxu60525S.py
+++ pyton/05xZdq9OcR1ss058he3GX6Ewk605U3gN5N6Xxu60525S.py
@@ -1,210 +1,88 @@
 
         
-        from mrjob.job import MRJob
+            elif not RESULT and ('info_dict' in test and 'age_limit' in test['info_dict'] and
+                         test['info_dict']['age_limit'] == 18):
+        print('\nPotential false negative: {0}'.format(test['name']))
+    
+        with open(ZSH_COMPLETION_FILE, 'w') as f:
+        f.write(template)
+    
+        def test_cmdline_umlauts(self):
+        p = subprocess.Popen(
+            [sys.executable, 'youtube_dl/__main__.py', encodeArgument('ä'), '--version'],
+            cwd=rootDir, stdout=_DEV_NULL, stderr=subprocess.PIPE)
+        _, stderr = p.communicate()
+        self.assertFalse(stderr)
+    
+            jsi = JSInterpreter('function f () { x =  2  ; return x; }')
+        self.assertEqual(jsi.call_function('f'), 2)
     
     
-class PersonServer(object):
-    
-            if error is None:
-            # the name is available, store it in the database and go to
-            # the login page
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
-            return redirect(url_for('auth.login'))
-    
-        return g.db
-    
-        :copyright: © 2010 by the Pallets team.
-    :license: BSD, see LICENSE for more details.
-'''
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    '''Clear existing data and create new tables.'''
+    init_db()
+    click.echo('Initialized the database.')
     
     
-@pytest.fixture
-def site_packages(modules_tmpdir, monkeypatch):
-    '''Create a fake site-packages.'''
-    rv = modules_tmpdir \
-        .mkdir('lib') \
-        .mkdir('python{x[0]}.{x[1]}'.format(x=sys.version_info)) \
-        .mkdir('site-packages')
-    monkeypatch.syspath_prepend(str(rv))
-    return rv
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('a', 'test', b'Incorrect username.'),
+    ('test', 'a', b'Incorrect password.'),
+))
+def test_login_validate_input(auth, username, password, message):
+    response = auth.login(username, password)
+    assert message in response.data
     
-        try:
-        oids, array_oids = get_hstore_oids(connection.alias)
-        register_hstore(connection.connection, globally=True, oid=oids, array_oid=array_oids)
-    except ProgrammingError:
-        # Hstore is not available on the database.
-        #
-        # If someone tries to create an hstore field it will error there.
-        # This is necessary as someone may be using PSQL without extensions
-        # installed but be using other features of contrib.postgres.
-        #
-        # This is also needed in order to create the connection in order to
-        # install the hstore extension.
-        pass
+        If there is no handler for the logger's effective level, add a
+    :class:`~logging.StreamHandler` for
+    :func:`~flask.logging.wsgi_errors_stream` with a basic format.
+    '''
+    logger = logging.getLogger('flask.app')
     
-        def _reset_stats(self):
-        self.tail.clear()
-        self.start = self.lastmark = self.lasttime = time()
-    
-        def run(self, args, opts):
-        if len(args) < 1:
-            raise UsageError()
-        elif len(args) > 1:
-            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
-        spname = args[0]
-    
-        def short_desc(self):
-        return 'Print Scrapy version'
-    
-            try:
-            self.min_bound = int(self.args[1])
-        except IndexError:
-            self.min_bound = 1
+        @property
+    def blueprint(self):
+        '''The name of the current blueprint'''
+        if self.url_rule and '.' in self.url_rule.endpoint:
+            return self.url_rule.endpoint.rsplit('.', 1)[0]
     
     
-def get_im_dir(name):
-    '''Retrieve the image directory for the dataset.'''
-    return _DATASETS[name][_IM_DIR]
-    
-        # Histogram of ground-truth objects
-    gt_hist = np.zeros((len(classes)), dtype=np.int)
-    for entry in roidb:
-        gt_inds = np.where(
-            (entry['gt_classes'] > 0) & (entry['is_crowd'] == 0))[0]
-        gt_classes = entry['gt_classes'][gt_inds]
-        gt_hist += np.histogram(gt_classes, bins=hist_bins)[0]
-    logger.debug('Ground-truth class histogram:')
-    for i, v in enumerate(gt_hist):
-        logger.debug(
-            '{:d}{:s}: {:d}'.format(
-                i, classes[i].rjust(char_len), v))
-    logger.debug('-' * char_len)
-    logger.debug(
-        '{:s}: {:d}'.format(
-            'total'.rjust(char_len), np.sum(gt_hist)))
-
-    
-    from detectron.core.config import cfg
-from detectron.datasets.dataset_catalog import get_devkit_dir
-from detectron.datasets.voc_eval import voc_eval
-from detectron.utils.io import save_object
-    
-    
-def add_roi_Xconv1fc_head(model, blob_in, dim_in, spatial_scale):
-    '''Add a X conv + 1fc head, as a reference if not using GroupNorm'''
-    hidden_dim = cfg.FAST_RCNN.CONV_HEAD_DIM
-    roi_size = cfg.FAST_RCNN.ROI_XFORM_RESOLUTION
-    roi_feat = model.RoIFeatureTransform(
-        blob_in,
-        'roi_feat',
-        blob_rois='rois',
-        method=cfg.FAST_RCNN.ROI_XFORM_METHOD,
-        resolution=roi_size,
-        sampling_ratio=cfg.FAST_RCNN.ROI_XFORM_SAMPLING_RATIO,
-        spatial_scale=spatial_scale
-    )
-    
-    
-_RENAME = {
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up4convs':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up4convs',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v1up':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v1up',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0upshare':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v0upshare',
-    # Removed 'ResNet_' from the name because it wasn't relevent
-    'mask_rcnn_heads.ResNet_mask_rcnn_fcn_head_v0up':
-        'mask_rcnn_heads.mask_rcnn_fcn_head_v0up',
-    # Removed head_builder module in favor of the more specific fast_rcnn name
-    'head_builder.add_roi_2mlp_head':
-        'fast_rcnn_heads.add_roi_2mlp_head',
-}
-    
-        if cfg.MODEL.FASTER_RCNN:
-        if model.train:
-            # Add op that generates training labels for in-network RPN proposals
-            model.GenerateProposalLabels(['rpn_rois', 'roidb', 'im_info'])
-        else:
-            # Alias rois to rpn_rois for inference
-            model.net.Alias('rpn_rois', 'rois')
-    
-        ind_kp = gt_inds[roidb['box_to_gt_ind_map']]
-    within_box = _within_box(gt_keypoints[ind_kp, :, :], roidb['boxes'])
-    vis_kp = gt_keypoints[ind_kp, 2, :] > 0
-    is_visible = np.sum(np.logical_and(vis_kp, within_box), axis=1) > 0
-    kp_fg_inds = np.where(
-        np.logical_and(max_overlaps >= cfg.TRAIN.FG_THRESH, is_visible)
-    )[0]
-    
-    
-if __name__ == '__main__':
-    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
-    logger = setup_logging(__name__)
-    logger.setLevel(logging.DEBUG)
-    logging.getLogger('detectron.roi_data.loader').setLevel(logging.INFO)
-    np.random.seed(cfg.RNG_SEED)
-    args = parse_args()
-    logger.info('Called with args:')
-    logger.info(args)
-    if args.cfg_file is not None:
-        merge_cfg_from_file(args.cfg_file)
-    if args.opts is not None:
-        merge_cfg_from_list(args.opts)
-    assert_and_infer_cfg()
-    logger.info('Running with config:')
-    logger.info(pprint.pformat(cfg))
-    main(args)
-
+  def Start( self ):
+    request_data = BuildRequestData()
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    self._response = self.PostDataToHandler( request_data,
+                                             'debug_info',
+                                             display_message = False )
     
     _logger = logging.getLogger( __name__ )
     
-      post_vim_message.assert_has_exact_calls( [
-    call( 'On the first day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'A test file in a Command-T', warning=False, truncate=True ),
-    call( 'On the second day of Christmas, my VimScript gave to me',
-          warning=False,
-          truncate=True ),
-    call( 'Two popup menus, and a test file in a Command-T',
-          warning=False,
-          truncate=True ),
-  ] )
-
     
-      opts = _JavaFilter( { 'regex' : '.*taco.*',
-                        'level' : 'warning' } )
-  f = _CreateFilterForTypes( opts, [ 'java' ] )
+class ShutdownRequest( BaseRequest ):
+  def __init__( self ):
+    super( ShutdownRequest, self ).__init__()
     
-        This worker is run in a separate process.
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
     
-    if __name__ == '__main__':
-    main()
-
     
-        route = [None] * len(obs)
-    i = len(obs) - 1
-    while i >= 0:
-        route[i] = state
-        state = mem_path[i][state]
-        i -= 1
-    return (prob, route)
-
+  def raise_for_status( self ):
+    if self._exception:
+      raise self._exception
     
-    content = open(file_name, 'rb').read()
+        def test_done_callback_with_cancel(self):
+        was_cancelled = [None]
+        def fn(callback_future):
+            was_cancelled[0] = callback_future.cancelled()
     
-    parser = OptionParser(USAGE)
-parser.add_option('-k', dest='topK')
-opt, args = parser.parse_args()
+    
+def add_force_split(word):
+    global Force_Split_Words
+    Force_Split_Words.add(word)
     
     if len(sys.argv)>1:
     default_encoding = sys.argv[1]
-    
-            return True
