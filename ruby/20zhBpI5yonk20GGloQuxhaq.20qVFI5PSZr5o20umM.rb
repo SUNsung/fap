6cbda71676a3ff8a5fea7982565f4e1f7612d8f4
SUNsung <weatherius@gmail.com>
2019-04-22 08:58:03 +0300

    Rewrite [20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb] File in: 2019-04-22 08:58:03

--- ruby/20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb
+++ ruby/20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb
@@ -1,124 +1,81 @@
 
         
-                  def render_collection_for(builder_class, &block)
-            options = @options.stringify_keys
-            rendered_collection = render_collection do |item, value, text, default_html_options|
-              builder = instantiate_builder(builder_class, item, value, text, default_html_options)
+        # Use an OS X pkg built with pkgbuild.
+#
+# Supports input and output. Requires pkgbuild and (for input) pkgutil, part of a
+# standard OS X install in 10.7 and higher.
+class FPM::Package::OSXpkg < FPM::Package
     
-          def self.symbols
-        type_klass::SET.symbols
+        # do channel-discover if required
+    if !attributes[:pear_channel].nil?
+      logger.info('Custom channel specified', :channel => attributes[:pear_channel])
+      channel_list = safesystemout('pear', '-c', config, 'list-channels')
+      if channel_list !~ /#{Regexp.quote(attributes[:pear_channel])}/
+        logger.info('Discovering new channel', :channel => attributes[:pear_channel])
+        safesystem('pear', '-c', config, 'channel-discover', attributes[:pear_channel])
       end
+      input_package = '#{attributes[:pear_channel]}/#{input_package}'
+      logger.info('Prefixing package name with channel', :package => input_package)
     end
-  end
-end
-
     
-          private
-        def self.layout(formats)
-          find_template(name.underscore, { formats: formats }, { _prefixes: ['layouts'] })
-        rescue ActionView::MissingTemplate
-          begin
-            find_template('application', { formats: formats }, { _prefixes: ['layouts'] })
-          rescue ActionView::MissingTemplate
-          end
+        platforms.each do |platform|
+      logger.info('Generating service manifest.', :platform => platform.class.name)
+      platform.program = command.first
+      platform.name = attributes[:pleaserun_name]
+      platform.args = command[1..-1]
+      platform.description = if attributes[:description_given?]
+        attributes[:description]
+      else
+        platform.name
+      end
+      pleaserun_attributes.each do |attribute_name|
+        attribute = 'pleaserun_#{attribute_name}'.to_sym
+        if attributes.has_key?(attribute) and not attributes[attribute].nil?
+          platform.send('#{attribute_name}=', attributes[attribute])
         end
+      end
     
-          helper :abc
+      def build!(params)
+    # TODO(sissel): Support these somehow, perhaps with execs and files.
+    self.scripts.each do |name, path|
+      case name
+        when 'pre-install'
+        when 'post-install'
+        when 'pre-uninstall'
+        when 'post-uninstall'
+      end # case name
+    end # self.scripts.each
     
-        def length
-      @entries.length
-    end
+      def initialize(package_name, opts = {}, &block)
+    @options = OpenStruct.new(:name => package_name.to_s)
+    @source, @target = opts.values_at(:source, :target).map(&:to_s)
+    @directory = File.expand_path(opts[:directory].to_s)
     
-        def links
-      context[:links]
-    end
-    
-        def insert_after(index, *names)
-      insert assert_index(index) + 1, *names
-    end
-    
-        def to_json
-      JSON.generate(as_json)
-    end
+      def is_pane
+    @actual.is_a? Tmuxinator::Pane
   end
 end
 
     
-        def parse_as_document
-      document = Nokogiri::HTML.parse @content, nil, 'UTF-8'
-      @title = document.at_css('title').try(:content)
-      document
-    end
-    
-        def blank?
-      body.blank?
-    end
-    
-          def mod
-        return @mod if defined?(@mod)
-        @mod = slug[/api\/([\w\-\.]+)\//, 1]
-        @mod.remove! 'angular2.' if @mod
-        @mod
-      end
-    end
-  end
-end
-
-    
-            css('pre > code').each do |node|
-          node['class'] ||= ''
-          lang = if node['class'].include?('lang-html') || node.content =~ /\A</
-            'html'
-          elsif node['class'].include?('lang-css')
-            'css'
-          elsif node['class'].include?('lang-js') || node['class'].include?('lang-javascript')
-            'javascript'
-          end
-          node.parent['data-language'] = lang if lang
-    
-      def exists?(type, path)
-    %Q{[ -#{type} '#{path}' ]}
-  end
-    
-        (stdout + stderr).each_line { |line| puts '[vagrant] #{line}' }
-    
-          def scm_name
-        fetch(:scm)
+          # Pathname of given project searching only global directories
+      def global_project(name)
+        project_in(environment, name) ||
+          project_in(xdg, name) ||
+          project_in(home, name)
       end
     
-        before do
-      allow_any_instance_of(Spree::Api::V2::Storefront::CartController).to receive(:spree_current_order).and_return(order)
-      patch '/api/v2/storefront/cart/empty', headers: headers_bearer
-    end
+      describe '#synchronize_before?' do
+    subject { window.synchronize_before? }
     
-        context 'with params 'include=default_billing_address'' do
-      before { get '/api/v2/storefront/account?include=default_billing_address', headers: headers }
-    
-          it 'does not return country second time if it appears in multiple zones' do
-        zone.countries << country
-        shipping_method.zones << zone
-        get shippable_url
-        expect(json_response['data'].size).to eq(to_return.size)
+            Kernel.exec(project.render)
       end
     
-              private
-    
-              scope = scope.not_deleted unless params[:show_deleted]
-          scope = scope.not_discontinued unless params[:show_discontinued]
-        else
-          scope = Product.accessible_by(current_ability, :read).active.includes(*product_includes)
-        end
-    
-      opts.separator ''
-  opts.separator 'Options:'
-    
-      def async_create_archive(branch)
-    Resque.enqueue(Archive, self.id, branch)
-  end
-    
-          # The string name of the queue from which the failed job was pulled
-      attr_accessor :queue
-    
-          def self.requeue_all
-        queues.each { |queue| requeue_queue(Resque::Failure.job_queue_name(queue)) }
-      end
+    Gem::Specification.new do |s|
+  s.name          = 'tmuxinator'
+  s.version       = Tmuxinator::VERSION
+  s.authors       = ['Allen Bargi', 'Christopher Chow']
+  s.email         = ['allen.bargi@gmail.com', 'chris@chowie.net']
+  s.description   = %q{Create and manage complex tmux sessions easily.}
+  s.summary       = %q{Create and manage complex tmux sessions easily.}
+  s.homepage      = 'https://github.com/tmuxinator/tmuxinator'
+  s.license       = 'MIT'
