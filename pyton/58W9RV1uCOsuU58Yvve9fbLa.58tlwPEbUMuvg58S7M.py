5cca08d21df16a4084dab31e9330691f46e757a2
SUNsung <weatherius@gmail.com>
2019-04-22 04:44:52 +0300

    Rewrite [58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py] File in: 2019-04-22 04:44:51

--- pyton/58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py
+++ pyton/58W9RV1uCOsuU58Yvve9fbLa.58tlwPEbUMuvg58S7M.py
@@ -1,186 +1,274 @@
 
         
-        
-class FlaskyStyle(Style):
-    background_color = '#f8f8f8'
-    default_style = ''
+            def __init__(self, get_response=None):
+        if not apps.is_installed('django.contrib.sites'):
+            raise ImproperlyConfigured(
+                'You cannot use RedirectFallbackMiddleware when '
+                'django.contrib.sites is not installed.'
+            )
+        super().__init__(get_response)
     
-            # Verify we receive an Authorization header in response, then redirect.
-        request_content = consume_socket_content(sock, timeout=0.5)
-        assert expected_digest in request_content
-        sock.send(text_302)
+        class Meta:
+        abstract = True
+        verbose_name = _('session')
+        verbose_name_plural = _('sessions')
     
-            # In general, we want to try IDNA encoding the hostname if the string contains
-        # non-ASCII characters. This allows users to automatically get the correct IDNA
-        # behaviour. For strings containing only ASCII characters, we need to also verify
-        # it doesn't start with a wildcard (*), before allowing the unencoded hostname.
-        if not unicode_is_ascii(host):
-            try:
-                host = self._get_idna_encoded_host(host)
-            except UnicodeError:
-                raise InvalidURL('URL has an invalid label.')
-        elif host.startswith(u'*'):
-            raise InvalidURL('URL has an invalid label.')
+        return TemplateResponse(request, template_name, {'sitemaps': sites},
+                            content_type=content_type)
     
-            :param url: URL for the new :class:`Request` object.
-        :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-            object to send in the body of the :class:`Request`.
-        :param json: (optional) json to send in the body of the :class:`Request`.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
+    
+class HTTPieHTTPAdapter(HTTPAdapter):
+    
+    from httpie.plugins import plugin_manager
+from httpie.context import Environment
+    
+    import requests.auth
+    
+        Assumes `from __future__ import division`.
+    
+    
+def test_follow_all_redirects_shown(httpbin):
+    r = http('--follow', '--all', httpbin.url + '/redirect/2')
+    assert r.count('HTTP/1.1') == 3
+    assert r.count('HTTP/1.1 302 FOUND', 2)
+    assert HTTP_OK in r
+    
+        def test_request_body_from_file_by_path_no_data_items_allowed(
+            self, httpbin):
+        env = MockEnvironment(stdin_isatty=False)
+        r = http('POST', httpbin.url + '/post', '@' + FILE_PATH_ARG, 'foo=bar',
+                 env=env, error_exit_ok=True)
+        assert 'cannot be mixed' in r.stderr
+
+    
+    
+def filename_from_content_disposition(content_disposition):
+    '''
+    Extract and validate filename from a Content-Disposition header.
+    
+    import keras
+from keras import losses
+from keras import backend as K
+from keras.utils.generic_utils import custom_object_scope
+    
+    # input image dimensions
+img_rows, img_cols = 28, 28
+# number of convolutional filters to use
+filters = 32
+# size of pooling area for max pooling
+pool_size = 2
+# convolution kernel size
+kernel_size = 3
+    
+        :param str u_string: unicode string to check. Must be unicode
+        and not Python 2 `str`.
+    :rtype: bool
+    '''
+    assert isinstance(u_string, str)
+    try:
+        u_string.encode('ascii')
+        return True
+    except UnicodeEncodeError:
+        return False
+
+    
+            :param url: The URL to connect to.
+        :param proxies: (optional) A Requests-style dictionary of proxies used on this request.
+        :rtype: urllib3.ConnectionPool
         '''
+        proxy = select_proxy(url, proxies)
     
     
-def patch(url, data=None, **kwargs):
-    r'''Sends a PATCH request.
+@pytest.mark.parametrize(
+    'value, expected', (
+        (
+            'application/xml',
+            ('application/xml', {})
+        ),
+        (
+            'application/json ; charset=utf-8',
+            ('application/json', {'charset': 'utf-8'})
+        ),
+        (
+            'application/json ; Charset=utf-8',
+            ('application/json', {'charset': 'utf-8'})
+        ),
+        (
+            'text/plain',
+            ('text/plain', {})
+        ),
+        (
+            'multipart/form-data; boundary = something ; boundary2=\'something_else\' ; no_equals ',
+            ('multipart/form-data', {'boundary': 'something', 'boundary2': 'something_else', 'no_equals': True})
+        ),
+        (
+                'multipart/form-data; boundary = something ; boundary2='something_else' ; no_equals ',
+                ('multipart/form-data', {'boundary': 'something', 'boundary2': 'something_else', 'no_equals': True})
+        ),
+        (
+            'multipart/form-data; boundary = something ; \'boundary2=something_else\' ; no_equals ',
+            ('multipart/form-data', {'boundary': 'something', 'boundary2': 'something_else', 'no_equals': True})
+        ),
+        (
+            'multipart/form-data; boundary = something ; 'boundary2=something_else' ; no_equals ',
+            ('multipart/form-data', {'boundary': 'something', 'boundary2': 'something_else', 'no_equals': True})
+        ),
+        (
+            'application/json ; ; ',
+            ('application/json', {})
+        )
+    ))
+def test__parse_content_type_header(value, expected):
+    assert _parse_content_type_header(value) == expected
     
-        if sys.platform == 'win32':
-        win32_lib = os.path.abspath( os.path.join(python_path, 'lib', 'win32'))
-        sys.path.append(win32_lib)
-    elif sys.platform == 'linux' or sys.platform == 'linux2':
-        linux_lib = os.path.abspath( os.path.join(python_path, 'lib', 'linux'))
-        sys.path.append(linux_lib)
-    elif sys.platform == 'darwin':
-        darwin_lib = os.path.abspath( os.path.join(python_path, 'lib', 'darwin'))
-        sys.path.append(darwin_lib)
+    _codes = {
+    }
     
-        ANTLR generates code that throws exceptions upon recognition error and
-    also generates code to catch these exceptions in each rule.  If you
-    want to quit upon first error, you can turn off the automatic error
-    handling mechanism using rulecatch action, but you still need to
-    override methods mismatch and recoverFromMismatchSet.
-    
-    In general, the recognition exceptions can track where in a grammar a
-    problem occurred and/or what was the expected input.  While the parser
-    knows its state (such as current input symbol and line info) that
-    state can change before the exception is reported so current token index
-    is computed and stored at exception time.  From this info, you can
-    perhaps print an entire line of input not just a single token, for example.
-    Better to just say the recognizer had a problem and then let the parser
-    figure out a fancy report.
-    
+        :param url: URL for the new :class:`Request` object.
+    :param params: (optional) Dictionary, list of tuples or bytes to send
+        in the query string for the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
     '''
     
-        # Get IoU overlap between each ex ROI and gt ROI
-    ex_gt_overlaps = box_utils.bbox_overlaps(
-        rois[ex_inds, :].astype(dtype=np.float32, copy=False),
-        rois[gt_inds, :].astype(dtype=np.float32, copy=False))
+        def test_duplicate_function_equality(self):
+        # Body of `duplicate' is the exact same as self.b
+        def duplicate():
+            'my docstring'
+            return 3
+        self.assertNotEqual(self.b, duplicate)
+    
+        def _Slice(self, t):
+        if t.lower:
+            self.dispatch(t.lower)
+        self.write(':')
+        if t.upper:
+            self.dispatch(t.upper)
+        if t.step:
+            self.write(':')
+            self.dispatch(t.step)
+    
+        def test_false_encoding(self):
+        # Issue 18873: 'Encoding' detected in non-comment lines
+        readline = self.get_readline((b'print('#coding=fake')',))
+        encoding, consumed_lines = detect_encoding(readline)
+        self.assertEqual(encoding, 'utf-8')
+        self.assertEqual(consumed_lines, [b'print('#coding=fake')'])
     
     
-def _write_voc_results_files(json_dataset, all_boxes, salt):
-    filenames = []
-    image_set_path = voc_info(json_dataset)['image_set_path']
-    assert os.path.exists(image_set_path), \
-        'Image set path does not exist: {}'.format(image_set_path)
-    with open(image_set_path, 'r') as f:
-        image_index = [x.strip() for x in f.readlines()]
-    # Sanity check that order of images in json dataset matches order in the
-    # image set
-    roidb = json_dataset.get_roidb()
-    for i, entry in enumerate(roidb):
-        index = os.path.splitext(os.path.split(entry['image'])[1])[0]
-        assert index == image_index[i]
-    for cls_ind, cls in enumerate(json_dataset.classes):
-        if cls == '__background__':
-            continue
-        logger.info('Writing VOC results for: {}'.format(cls))
-        filename = _get_voc_results_file_template(json_dataset,
-                                                  salt).format(cls)
-        filenames.append(filename)
-        assert len(all_boxes[cls_ind]) == len(image_index)
-        with open(filename, 'wt') as f:
-            for im_ind, index in enumerate(image_index):
-                dets = all_boxes[cls_ind][im_ind]
-                if type(dets) == list:
-                    assert len(dets) == 0, \
-                        'dets should be numpy.ndarray or empty list'
-                    continue
-                # the VOCdevkit expects 1-based indices
-                for k in range(dets.shape[0]):
-                    f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
-                            format(index, dets[k, -1],
-                                   dets[k, 0] + 1, dets[k, 1] + 1,
-                                   dets[k, 2] + 1, dets[k, 3] + 1))
-    return filenames
+def read_text(package: Package,
+              resource: Resource,
+              encoding: str = 'utf-8',
+              errors: str = 'strict') -> str:
+    '''Return the decoded string of the resource.
     
-    from detectron.core.config import cfg
-from detectron.modeling.generate_anchors import generate_anchors
-from detectron.utils.c2 import const_fill
-from detectron.utils.c2 import gauss_fill
-from detectron.utils.net import get_group_gn
-import detectron.modeling.ResNet as ResNet
-import detectron.utils.blob as blob_utils
-import detectron.utils.boxes as box_utils
+    # Add the html version.  This converts the message into a multipart/alternative
+# container, with the original text message as the first part and the new html
+# message as the second part.
+asparagus_cid = make_msgid()
+msg.add_alternative('''\
+<html>
+  <head></head>
+  <body>
+    <p>Salut!</p>
+    <p>Cela ressemble à un excellent
+        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
+            recipie
+        </a> déjeuner.
+    </p>
+    <img src='cid:{asparagus_cid}' />
+  </body>
+</html>
+'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
+# note that we needed to peel the <> off the msgid for use in the html.
     
-    # array([[ -83.,  -39.,  100.,   56.],
-#        [-175.,  -87.,  192.,  104.],
-#        [-359., -183.,  376.,  200.],
-#        [ -55.,  -55.,   72.,   72.],
-#        [-119., -119.,  136.,  136.],
-#        [-247., -247.,  264.,  264.],
-#        [ -35.,  -79.,   52.,   96.],
-#        [ -79., -167.,   96.,  184.],
-#        [-167., -343.,  184.,  360.]])
     
-            if cfg.FPN.FPN_ON:
-            # After adding the RPN head, restrict FPN blobs and scales to
-            # those used in the RoI heads
-            blob_conv, spatial_scale_conv = _narrow_to_fpn_roi_levels(
-                blob_conv, spatial_scale_conv
-            )
+def main():
+    parser = ArgumentParser(description='''\
+Send the contents of a directory as a MIME message.
+Unless the -o option is given, the email is sent by forwarding to your local
+SMTP server, which then does the normal delivery process.  Your local machine
+must be running an SMTP server.
+''')
+    parser.add_argument('-d', '--directory',
+                        help='''Mail the contents of the specified directory,
+                        otherwise use the current directory.  Only the regular
+                        files in the directory are sent, and we don't recurse to
+                        subdirectories.''')
+    parser.add_argument('-o', '--output',
+                        metavar='FILE',
+                        help='''Print the composed message to FILE instead of
+                        sending the message to the SMTP server.''')
+    parser.add_argument('-s', '--sender', required=True,
+                        help='The value of the From: header (required)')
+    parser.add_argument('-r', '--recipient', required=True,
+                        action='append', metavar='RECIPIENT',
+                        default=[], dest='recipients',
+                        help='A To: header value (at least one required)')
+    args = parser.parse_args()
+    directory = args.directory
+    if not directory:
+        directory = '.'
+    # Create the message
+    msg = EmailMessage()
+    msg['Subject'] = 'Contents of directory %s' % os.path.abspath(directory)
+    msg['To'] = ', '.join(args.recipients)
+    msg['From'] = args.sender
+    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
     
-    from detectron.core.config import cfg
-from detectron.datasets import json_dataset
-from detectron.datasets import roidb as roidb_utils
-import detectron.modeling.FPN as fpn
-import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
-import detectron.utils.blob as blob_utils
+    def handleSlides(slides):
+    for slide in slides:
+        handleSlide(slide)
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
+            print('Testing ApplyResult.get() with timeout:', end=' ')
+        res = pool.apply_async(calculate, TASKS[0])
+        while 1:
+            sys.stdout.flush()
+            try:
+                sys.stdout.write('\n\t%s' % res.get(0.02))
+                break
+            except multiprocessing.TimeoutError:
+                sys.stdout.write('.')
+        print()
+        print()
     
-      # See ':h group-name' for details on how the initial group hierarchy is built.
-  group_name_to_group = {
-    'Boolean': SyntaxGroup( 'Boolean' ),
-    'Identifier': identifier_group,
-    'Statement': statement_group,
-    'PreProc': preproc_group,
-    'Type': type_group
-  }
     
-      with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
-    with MockCompletionRequest( ServerResponse ):
-      ycm.SendCompletionRequest()
-      ok_( ycm.CompletionRequestReady() )
-      response = ycm.GetCompletionResponse()
-      post_vim_message.assert_has_exact_calls( [
-        call( 'Exception: message', truncate = True )
-      ] )
-      assert_that(
-        response,
-        has_entries( {
-          'completions': contains( has_entries( {
-            'word': 'insertion_text',
-            'abbr': 'menu_text',
-            'menu': 'extra_menu_info',
-            'info': 'detailed_info\ndoc_string',
-            'kind': 'k',
-            'dup': 1,
-            'empty': 1
-          } ) ),
-          'completion_start_column': 3
-        } )
-      )
+# -- Options for Texinfo output ----------------------------------------
     
-    # The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-version = '2.1.3'
-# The full version, including alpha/beta/rc tags.
-release = '2.1.3'
+        # You can access the actual face itself like this:
+    face_image = image[top:bottom, left:right]
+    pil_image = Image.fromarray(face_image)
+    pil_image.show()
+
+    
+    # Get a reference to the Raspberry Pi camera.
+# If this fails, make sure you have a camera connected to the RPi and that you
+# enabled your camera in raspi-config and rebooted first.
+camera = picamera.PiCamera()
+camera.resolution = (320, 240)
+output = np.empty((240, 320, 3), dtype=np.uint8)
+    
+    # Create a PIL imagedraw object so we can draw on the picture
+pil_image = Image.fromarray(image)
+d = ImageDraw.Draw(pil_image)
+    
+    # 载入样本图片（奥巴马和拜登）
+print('Loading known face image(s)')
+obama_image = face_recognition.load_image_file('obama_small.jpg')
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+    
+    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
+# See http://pillow.readthedocs.io/ for more about PIL/Pillow
+pil_image = Image.fromarray(unknown_image)
+# Create a Pillow ImageDraw Draw instance to draw with
+draw = ImageDraw.Draw(pil_image)
+    
+    def shorten_title(title):
+    m1 = re.search('[[0-9]*]', title)
+    m2 = re.search(''.*'', title)
+    if m1:
+        title = m1.group(0)
+    if m2:
+        title = ' '.join((title, m2.group(0)))   
+    return title[:50] + ' [...]'    
