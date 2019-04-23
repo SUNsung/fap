787695a5987643fb5d866583b63993414cddbbc0
SUNsung <weatherius@gmail.com>
2019-04-23 04:18:38 +0300

    Rewrite [50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp] File in: 2019-04-23 04:18:37

--- cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
+++ cccccc/50KvNgMnpFZww50mBU3eTVj.50Wi1M79IV1B650xcm.cpp
@@ -1,38 +1,11 @@
 
         
-        Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#ifndef TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
-#define TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+        #endif  // TENSORFLOW_PYTHON_FRAMEWORK_PYTHON_OP_GEN_H_
+
     
-     protected:
-  // Print: def Function(parameters):
-  void AddDefLine(const string& function_name, const string& parameters);
-  void AddDefLine(const string& parameters);
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    
-    {  return Status::OK();
-}
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-        http://www.apache.org/licenses/LICENSE-2.0
+    Licensed under the Apache License, Version 2.0 (the 'License');
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
     
     Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an 'AS IS' BASIS,
@@ -44,19 +17,6 @@ limitations under the License.
     namespace tensorflow {
     }
     
-    // Safe containers for an owned TF_Tensor. On destruction, the tensor will be
-// deleted by TF_DeleteTensor.
-using Safe_TF_TensorPtr = std::unique_ptr<TF_Tensor, detail::TFTensorDeleter>;
-Safe_TF_TensorPtr make_safe(TF_Tensor* tensor);
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-#include 'tensorflow/python/util/kernel_registry.h'
-    
     Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an 'AS IS' BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -64,143 +24,235 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
     
-      // Acquire the per-Entry mutex without holding the map mutex. Initializing
-  // an Executor may be expensive, so we want to allow concurrent
-  // initialization of different entries.
-  mutex_lock lock{entry->configurations_mutex};
-  for (const auto& iter : entry->configurations) {
-    if (iter.first.plugin_config == config.plugin_config &&
-        iter.first.device_options == config.device_options) {
-      VLOG(2) << 'hit in cache';
-      return iter.second.get();
-    }
-  }
     
-    // FFT support interface -- this can be derived from a GPU executor when the
-// underlying platform has an FFT library implementation available. See
-// StreamExecutor::AsFft().
-//
-// This support interface is not generally thread-safe; it is only thread-safe
-// for the CUDA platform (cuFFT) usage; host side FFT support is known
-// thread-compatible, but not thread-safe.
-class FftSupport {
+    {
+    {}  // namespace cuda
+}  // namespace stream_executor
+    
+    #include 'tensorflow/stream_executor/executor_cache.h'
+#include 'tensorflow/stream_executor/lib/statusor.h'
+#include 'tensorflow/stream_executor/multi_platform_manager.h'
+#include 'tensorflow/stream_executor/platform.h'
+#include 'tensorflow/stream_executor/platform/mutex.h'
+#include 'tensorflow/stream_executor/platform/port.h'
+#include 'tensorflow/stream_executor/platform/thread_annotations.h'
+#include 'tensorflow/stream_executor/stream_executor_pimpl.h'
+#include 'tensorflow/stream_executor/trace_listener.h'
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Opaque and unique identifier for the host platform.
+// This is needed so that plugins can refer to/identify this platform without
+// instantiating a HostPlatform object.
+// This is broken out here to avoid a circular dependency between HostPlatform
+// and HostStreamExecutor.
+extern const Platform::Id kHostPlatformId;
+    
+      std::map<uv_timer_t*, base::OnceClosure> tasks_;
+    
+    #ifndef ATOM_BROWSER_API_ATOM_API_AUTO_UPDATER_H_
+#define ATOM_BROWSER_API_ATOM_API_AUTO_UPDATER_H_
+    
+    #endif  // ATOM_BROWSER_API_ATOM_API_NET_H_
+
+    
+      static void BuildPrototype(v8::Isolate* isolate,
+                             v8::Local<v8::FunctionTemplate> prototype);
+    
+    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
  public:
-  virtual ~FftSupport() {}
+  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
+  ~AtomJavaScriptDialogManager() override;
     }
     
-        // out_of_range.109
-    try
-    {
-        // try to use an array index that is not a number
-        json::const_reference ref = j.at('/array/one'_json_pointer);
-    }
-    catch (json::parse_error& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+    #include 'content/public/browser/quota_permission_context.h'
+#include 'content/public/common/storage_quota_params.h'
     
-    void ObjectExtendedInfo60D::Parse(const std::uint8_t* bytes, int32_t length,
-                                  ContiRadar* conti_radar) const {
-  int obj_id = object_id(bytes, length);
-  for (int i = 0; i < conti_radar->contiobs_size(); ++i) {
-    if (conti_radar->contiobs(i).obstacle_id() == obj_id) {
-      auto obs = conti_radar->mutable_contiobs(i);
-      obs->set_longitude_accel(longitude_accel(bytes, length));
-      obs->set_lateral_accel(lateral_accel(bytes, length));
-      obs->set_oritation_angle(oritation_angle(bytes, length));
-      obs->set_length(object_length(bytes, length));
-      obs->set_width(object_width(bytes, length));
-      obs->set_obstacle_class(obstacle_class(bytes, length));
-      break;
-    }
-  }
-  // auto conti_obs = conti_radar->mutable_contiobs(object_id(bytes, length));
+    
+    {}  // namespace atom
+
+    
+    const gfx::Rect& OffscreenViewProxy::GetBounds() {
+  return view_bounds_;
 }
     
-    TEST(TestPiecewiseLinearKernel, add_second_order_derivative_matrix) {
-  PiecewiseLinearKernel kernel(10, 0.1);
-  const double init_derivative = 5.0;
+    namespace atom {
     }
     
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+    #include <vector>
     
-    namespace apollo {
-namespace canbus {
-namespace gem {
-    }
-    }
+    
+    {  gfx::Point location = display::Screen::GetScreen()->GetCursorScreenPoint();
+  // TODO(varunjain): Properly determine and send DRAG_EVENT_SOURCE below.
+  aura::client::GetDragDropClient(root_window)
+      ->StartDragAndDrop(
+          data, root_window, view, location,
+          ui::DragDropTypes::DRAG_COPY | ui::DragDropTypes::DRAG_LINK,
+          ui::DragDropTypes::DRAG_EVENT_SOURCE_MOUSE);
+}
+    
+    // Generate param traits write methods.
+#include 'ipc/param_traits_write_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+    
+    IPC_SYNC_MESSAGE_ROUTED4_1(ShellViewHostMsg_Call_Object_Method_Sync,
+                           int /* object id */,
+                           std::string /* type name */,
+                           std::string /* method name */,
+                           base::ListValue /* arguments */,
+                           base::ListValue /* result */)
+    
+      static void ClearCache(content::RenderProcessHost* render_view_host);
+  static void SetProxyConfig(content::RenderProcessHost* render_process_host,
+                             const std::string& proxy_config);
+    
+    Base::~Base() {
+}
+    
+    // Tell browser to delete a object.
+// function DeallocateObject(id);
+v8::Handle<v8::Value> DeallocateObject(int routing_id,
+                                       int object_id);
+    
+    bool MenuDelegate::IsCommandIdEnabled(int command_id) const {
+  if (command_id < 0)
+    return false;
     }
     
-    // config detail: {'name': 'usr_can_read_errors', 'offset': 0.0,
-// 'precision': 1.0, 'len': 16, 'is_signed_var': False, 'physical_range':
-// '[0|65535]', 'bit': 55, 'type': 'int', 'order': 'motorola', 'physical_unit':
-// ''}
-int Globalrpt6a::usr_can_read_errors(const std::uint8_t* bytes,
-                                     int32_t length) const {
-  Byte t0(bytes + 6);
-  int32_t x = t0.get_byte(0, 8);
+    
+    {}  // namespace
+    
+    static KeyMap keymap = {
+  {'`'    , 'Backquote'},
+  {'\\'   , 'Backslash'},
+  {'['    , 'BracketLeft'},
+  {']'    , 'BracketRight'},
+  {','    , 'Comma'},
+  {'='    , 'Equal'},
+  {'-'    , 'Minus'},
+  {'.'    , 'Period'},
+  {'''    , 'Quote'},
+  {';'    , 'Semicolon'},
+  {'/'    , 'Slash'},
+  {'\n'   , 'Enter'},
+  {'\t'   , 'Tab'},
+  {'UP'   , 'ArrowUp'},
+  {'DOWN' , 'ArrowDown'},
+  {'LEFT' , 'ArrowLeft'},
+  {'RIGHT', 'ArrowRight'},
+  {'ESC'  , 'Escape'},
+  {'MEDIANEXTTRACK', 'MediaTrackNext'},
+  {'MEDIAPREVTRACK', 'MediaTrackPrevious'}
+};
+    
+    NwObjCallObjectMethodFunction::NwObjCallObjectMethodFunction() {
+}
+    
+    
+    {  private:
+    DISALLOW_COPY_AND_ASSIGN(NwScreenStartMonitorFunction);
+  };
+    
+    class b2Triangle{
+public:
+	float* x;
+    float* y;
+	b2Triangle();
+	b2Triangle(float32 x1, float32 y1, float32 x2, float32 y2, float32 x3, float32 y3);
+	~b2Triangle();
+	bool IsInside(float32 _x, float32 _y);
+	void Set(const b2Triangle& toMe);
     }
     
-    Context::~Context()
+    					ErrorMetric a_errormetric);
+    
+    #ifdef FLOAT_APPROX
+/* This code should reliably detect NaN/inf even when -ffast-math is used.
+   Assumes IEEE 754 format. */
+static OPUS_INLINE int celt_isnan(float x)
 {
-    if (stack_)
+   union {float f; opus_uint32 i;} in;
+   in.f = x;
+   return ((in.i>>23)&0xFF)==0xFF && (in.i&0x007FFFFF)!=0;
+}
+#else
+#ifdef __FAST_MATH__
+#error Cannot build libopus with -ffast-math unless FLOAT_APPROX is defined. This could result in crashes on extreme (e.g. NaN) input
+#endif
+#define celt_isnan(x) ((x)!=(x))
+#endif
+    
+    #endif /* HAVE_ARM_NE10 */
+    
+       THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+    
+    // TODO: Rename to GrpcTraceContextV0.
+struct GrpcTraceContext {
+  GrpcTraceContext() {}
+    }
+    
+    #ifndef GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+#define GRPC_SRC_CPP_SERVER_LOAD_REPORTER_GET_CPU_STATS_H
+    
+    
     {
-        swTraceLog(SW_TRACE_COROUTINE, 'free stack: ptr=%p', stack_);
-        if (protect_page_)
+    {}  // namespace load_reporter
+}  // namespace grpc
+    
+    #include <grpc/support/log.h>
+    
+    #include 'src/cpp/server/load_reporter/get_cpu_stats.h'
+    
+    namespace grpc {
+    }
+    
+            friend bool operator==(Rational const& lhs, Rational const& rhs);
+        friend bool operator!=(Rational const& lhs, Rational const& rhs);
+        friend bool operator<(Rational const& lhs, Rational const& rhs);
+        friend bool operator>(Rational const& lhs, Rational const& rhs);
+        friend bool operator<=(Rational const& lhs, Rational const& rhs);
+        friend bool operator>=(Rational const& lhs, Rational const& rhs);
+    
+    INarratorAnnouncementHost^ NarratorAnnouncementHostFactory::GetHostProducer()
+{
+    for (INarratorAnnouncementHost^ host : NarratorAnnouncementHostFactory::s_hosts)
+    {
+        if (host->IsHostAvailable())
         {
-            unprotect_stack(stack_, protect_page_);
+            return host;
         }
     }
     }
     
-        signals:
-        void finished();
-    
-            //保存长度值
-        memcpy(ptr, &size, sizeof(size));
-        //在指针末尾保存一个串号
-        memcpy((char*) ptr + size - 4, &(send_pkg.serial_num), sizeof(send_pkg.serial_num));
-    
-    TEST(coroutine_base, yield_resume)
-{
-    long _cid;
-    long cid = Coroutine::create([](void *arg)
-    {
-        long cid = Coroutine::get_current_cid();
-        Coroutine *co = Coroutine::get_by_cid(cid);
-        co->yield();
-        *(long *) arg = Coroutine::get_current_cid();
-    }, &_cid);
-    }
-    
-        coro_test({
-        make_pair([](void *arg)
+            static void SetAnnouncement(Windows::UI::Xaml::DependencyObject^ element, NarratorAnnouncement^ value)
         {
-            auto chan = (Channel *) arg;
-    }
-    }
+            element->SetValue(s_announcementProperty, value);
+        }
     
-        virtual void onStart() = 0;
-    virtual void onShutdown() = 0;
-    virtual void onWorkerStart(int worker_id) = 0;
-    virtual void onWorkerStop(int worker_id) = 0;
-    virtual void onReceive(int fd, const DataBuffer &data) = 0;
-    virtual void onConnect(int fd) = 0;
-    virtual void onClose(int fd) = 0;
-    virtual void onPacket(const DataBuffer &, ClientInfo &) = 0;
-    virtual void onPipeMessage(int src_worker_id, const DataBuffer &) = 0;
-    virtual void onTask(int, int, const DataBuffer &) = 0;
-    virtual void onFinish(int, const DataBuffer &) = 0;
+    using namespace CalculatorApp::Common;
+using namespace Windows::Storage::Streams;
     
-        long buf;
-    read(_pipe, (void*) &buf, sizeof(buf));
-    close(_pipe);
+                static void RegisterNewAppViewId();
+            static void OnWindowClosed(int viewId);
     
-        swSignalfd_init();
-    swSignal_add(SIGUSR1,sig_usr1);
-    swSignalfd_setup(SwooleG.main_reactor);
+                // store history in app data functions
+            Platform::String^ SerializeHistoryItem(_In_ std::shared_ptr<CalculationManager::HISTORYITEM> const &item);
+            void SaveHistory();
