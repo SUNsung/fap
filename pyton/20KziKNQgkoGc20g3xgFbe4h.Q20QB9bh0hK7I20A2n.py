0adfda34121b1bc7482eb743eae984398e60d165
SUNsung <weatherius@gmail.com>
2019-04-24 00:09:17 +0300

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2019-04-24 00:09:16

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,185 +1,217 @@
 
         
-        # Allow direct execution
-import os
-import sys
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+            with open('README.md', 'w+') as sorted_file:
+        sorted_file.write(final_README)
+    
+        def load(self):
+        try:
+            session_data = self._cache.get(self.cache_key)
+        except Exception:
+            # Some backends (e.g. memcache) raise an exception on invalid
+            # cache keys. If this happens, reset the session. See #17810.
+            session_data = None
+        if session_data is not None:
+            return session_data
+        self._session_key = None
+        return {}
+    
+        @classmethod
+    def get_model_class(cls):
+        # Avoids a circular import and allows importing SessionStore when
+        # django.contrib.sessions is not in INSTALLED_APPS.
+        from django.contrib.sessions.models import Session
+        return Session
+    
+        def load(self):
+        '''
+        Load the data from the key itself instead of fetching from some
+        external data store. Opposite of _get_session_key(), raise BadSignature
+        if signature fails.
+        '''
+        try:
+            return signing.loads(
+                self.session_key,
+                serializer=self.serializer,
+                # This doesn't handle non-default expiry dates, see #19201
+                max_age=settings.SESSION_COOKIE_AGE,
+                salt='django.contrib.sessions.backends.signed_cookies',
+            )
+        except Exception:
+            # BadSignature, ValueError, or unpickling exceptions. If any of
+            # these happen, reset the session.
+            self.create()
+        return {}
     
     
-    {    # Options that need a file parameter
-    'download-archive': ['--require-parameter'],
-    'cookies': ['--require-parameter'],
-    'load-info': ['--require-parameter'],
-    'batch-file': ['--require-parameter'],
-}
+class BaseSessionManager(models.Manager):
+    def encode(self, session_dict):
+        '''
+        Return the given session dictionary serialized and encoded as a string.
+        '''
+        session_store_class = self.model.get_session_store_class()
+        return session_store_class().encode(session_dict)
     
-        def test_cmdline_umlauts(self):
-        p = subprocess.Popen(
-            [sys.executable, 'youtube_dl/__main__.py', encodeArgument('ä'), '--version'],
-            cwd=rootDir, stdout=_DEV_NULL, stderr=subprocess.PIPE)
-        _, stderr = p.communicate()
-        self.assertFalse(stderr)
+        for node in doctree.traverse(settingslist_node):
+        settings_list = nodes.bullet_list()
+        settings_list.extend([make_setting_element(d, app, fromdocname)
+                              for d in sorted(env.scrapy_all_settings,
+                                              key=itemgetter('setting_name'))
+                              if fromdocname != d['docname']])
+        node.replace_self(settings_list)
     
-        author = proj_info['author'],
-    author_email = proj_info['author_email'],
-    url = proj_info['url'],
-    license = proj_info['license'],
-    
-                elif opt in ('-V', '--version'):
-                # Display version.
-                log.println('you-get:', log.BOLD)
-                log.println('    version:  {}'.format(__version__))
-                if head is not None:
-                    log.println('    branch:   {}\n    commit:   {}'.format(*head))
-                else:
-                    log.println('    branch:   {}\n    commit:   {}'.format('(stable)', '(tag v{})'.format(__version__)))
-    
-    __all__ = ['baomihua_download', 'baomihua_download_by_id']
-    
-    class BokeCC(VideoExtractor):
-    name = 'BokeCC'
-    
-    import re
-    
-        title = match1(html, r'<title>([^<]{1,9999})</title>')
-    
-    
-def icourses_playlist_new(url, page=None):
-    # 2 helpers using same interface in the js code
-    def to_chap(course_id, chap_id, mod):
-        ep = 'http://www.icourses.cn/jpk/viewCharacterDetail2.action?courseId={}&characId={}&mod={}'
-        req = post_content(ep.format(course_id, chap_id, mod), post_data={})
-        return req
-    
-    for face_location in face_locations:
-    
-            if file.filename == '':
-            return redirect(request.url)
-    
-        # macOS will crash due to a bug in libdispatch if you don't use 'forkserver'
-    context = multiprocessing
-    if 'forkserver' in multiprocessing.get_all_start_methods():
-        context = multiprocessing.get_context('forkserver')
-    
-            # Now let's list all the faces we found in all 128 frames
-        for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
-            number_of_faces_in_frame = len(face_locations)
-    
-        def test_fd_command_line_interface_cnn_model(self):
-        target_string = 'obama.jpg'
-        runner = CliRunner()
-        image_file = os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg')
-    
-    
-def caesar_decode(n=0, text=''):
-  return caesar_encode(-n, text)
-    
-    '''As a simple example of Python Fire, a Widget serves no clear purpose.'''
-    
-    '''Tests for the widget module.'''
-    
-      Args:
-    fn: The function to be used for parsing arguments.
-    *arguments: The arguments for which to use the parse fn. If none are listed,
-      then this will set the default parse function.
-  Returns:
-    The decorated function, which now has metadata telling Fire how to perform.
-  '''
-  def _Decorator(func):
-    parse_fns = GetParseFns(func)
-    if not arguments:
-      parse_fns['default'] = fn
-    else:
-      for argument in arguments:
-        parse_fns['named'][argument] = fn
-    _SetMetadata(func, FIRE_PARSE_FNS, parse_fns)
-    return func
-    
-    import mock
-    
-      def test_one_line_simple_whitespace(self):
-    docstring = '''
-      A simple one line docstring.
     '''
-    docstring_info = docstrings.parse(docstring)
-    expected_docstring_info = DocstringInfo(
-        summary='A simple one line docstring.',
-    )
-    self.assertEqual(docstring_info, expected_docstring_info)
     
-      def testTabCompletionWithDict(self):
-    actions = {'multiply': lambda a, b: a * b}
-    completion_script = fire.Fire(
-        actions, command=['--', '--completion'], name='actCLI')
-    self.assertIn('actCLI', completion_script)
-    self.assertIn('multiply', completion_script)
+        def run(self, args, opts):
+        # load contracts
+        contracts = build_component_list(self.settings.getwithbase('SPIDER_CONTRACTS'))
+        conman = ContractsManager(load_object(c) for c in contracts)
+        runner = TextTestRunner(verbosity=2 if opts.verbose else 1)
+        result = TextTestResult(runner.stream, runner.descriptions, runner.verbosity)
     
-      This method generates `branch_factor` branches for each sequence in the beam,
-  where each branch extends the event sequence by `num_steps` steps (via calls
-  to `generate_step_fn`). The resulting beam is returned.
+            editor = self.settings['EDITOR']
+        try:
+            spidercls = self.crawler_process.spider_loader.load(args[0])
+        except KeyError:
+            return self._err('Spider not found: %s' % args[0])
     
-      def testSleeper_SleepUntil(self):
-    # Burn in.
-    for _ in range(10):
-      concurrency.Sleeper().sleep(.01)
+    from scrapy import twisted_version
     
-    
-def extract_state(batched_states, i):
-  '''Extracts a single state from a batch of states.
-    
-      def test_Unbatch(self):
-    unbatched_states = state_util.unbatch(self._batched_states, batch_size=2)
-    
-      Returns:
-    A merged tf.HParams object with the hyperparameters from both `hparams_1`
-    and `hparams_2`.
-  '''
-  hparams_map = hparams_1.values()
-  hparams_map.update(hparams_2.values())
-  return tf.contrib.training.HParams(**hparams_map)
-    
-        for text in TEXTS:
-        doc = nlp(text)
-        relations = extract_currency_relations(doc)
-        for r1, r2 in relations:
-            print('{:<10}\t{}\t{}'.format(r1.text, r2.ent_type_, r2.text))
-    
-    import plac
-from spacy.lang.en import English
-from spacy.tokens import Doc, Span
-from spacy import displacy
-from pathlib import Path
-    
-        doc = nlp(text)
-    print('Pipeline', nlp.pipe_names)  # pipeline contains component name
-    print('Tokens', [t.text for t in doc])  # company names from the list are merged
-    print('Doc has_tech_org', doc._.has_tech_org)  # Doc contains tech orgs
-    print('Token 0 is_tech_org', doc[0]._.is_tech_org)  # 'Alphabet Inc.' is a tech org
-    print('Token 1 is_tech_org', doc[1]._.is_tech_org)  # 'is' is not
-    print('Entities', [(e.text, e.label_) for e in doc.ents])  # all orgs are entities
-    
-        print('Building Tensorboard Projector metadata for ({}) vectors: {}'.format(vector_count, out_meta_file))
+        def _debug_set_cookie(self, response, spider):
+        if self.debug:
+            cl = [to_native_str(c, errors='replace')
+                  for c in response.headers.getlist('Set-Cookie')]
+            if cl:
+                cookies = '\n'.join('Set-Cookie: {}\n'.format(c) for c in cl)
+                msg = 'Received cookies from: {}\n{}'.format(response, cookies)
+                logger.debug(msg, extra={'spider': spider})
     
     
-def test():
-    with virtualenv(VENV_DIR):
-        with lcd(path.dirname(__file__)):
-            local('py.test -x spacy/tests')
+# This class can be used to keep the ycmd server alive for the duration of the
+# life of the client. By default, ycmd shuts down if it doesn't see a request in
+# a while.
+class YcmdKeepalive( object ):
+  def __init__( self, ping_interval_seconds = 60 * 10 ):
+    self._keepalive_thread = Thread( target = self._ThreadMain )
+    self._keepalive_thread.daemon = True
+    self._ping_interval_seconds = ping_interval_seconds
+    
+    
+def _CreateInitialGroupMap():
+  def AddToGroupMap( name, parent ):
+    new_group = SyntaxGroup( name )
+    group_name_to_group[ name ] = new_group
+    parent.children.append( new_group )
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+def KeywordsFromSyntaxListOutput_MultipleStatementGroups_test():
+  assert_that( syntax_parse._KeywordsFromSyntaxListOutput( '''
+foogroup xxx foo bar
+             links to Statement
+bargroup xxx zoo goo
+             links to Statement''' ),
+               contains_inanyorder( 'foo', 'bar', 'zoo', 'goo' ) )
+    
+    class ProcessPoolExecutor(_base.Executor):
+    def __init__(self, max_workers=None):
+        '''Initializes a new ProcessPoolExecutor instance.
+    
+            print('One element')
+        assert_equal(merge_sort.sort([5]), [5])
+    
+        # TODO: It would be better if we had unit tests for each
+    # method in addition to the following end-to-end test
+    def test_end_to_end(self):
+        print('Test: Empty stack')
+        stack = Stack()
+        assert_equal(stack.peek(), None)
+        assert_equal(stack.pop(), None)
+    
+            # Scenario: call is successful
+        return_value = get_customer_profile(self.customer_id)
+        GetRequest.assert_called_once_with(customerProfileId=self.customer_id)
+        self.assertTrue(_request.make_request.called)
+        self.assertEqual(return_value, profile_mock)
+    
+        # IE<8 allows backslash escapes in place of pretty much any char
+    def test_escape_function_obfuscation(self):
+        testcase = u'*{color: expression\\28 alert\\28 1 \\29 \\29 }'
+        self.assertInvalid(testcase)
+    
+        def test_nested_modules_include_all_sources(self):
+        test_files_a = ['foo.js', 'bar.js']
+        test_module_a = TestModule('test_module_a', *test_files_a)
+        test_files_b = ['baz.js', 'qux.js']
+        test_module_b = TestModule('test_module_b', *test_files_b)
+        test_module = TestModule('test_mobule', test_module_a, test_module_b)
+        self.assertEqual(test_module.build(), concat_sources(test_files_a + test_files_b))
+    
+            self.assertEqual(srnames, set(nsfw_collection_srnames))
+    
+        def test_no_resize(self):
+        image = dict(url='http://s3.amazonaws.com/a.jpg', width=200,
+                      height=800)
+        url = self.provider.resize_image(image)
+        self.assertEqual(url, 'https://unsplash.it/200/400')
+    
+        def test_crashers(self):
+        # Chrome crashes on weirdly encoded nulls.
+        self.assertFragmentRaises('<a href='http://example.com/%%30%30'>foo</a>',
+                                  SoupDetectedCrasherError)
+        self.assertFragmentRaises('<a href='http://example.com/%0%30'>foo</a>',
+                                  SoupDetectedCrasherError)
+        self.assertFragmentRaises('<a href='http://example.com/%%300'>foo</a>',
+                                  SoupDetectedCrasherError)
+        # Chrome crashes on extremely long hostnames
+        self.assertFragmentRaises('<a href='http://%s.com'>foo</a>' % ('x' * 300),
+                                  SoupDetectedCrasherError)
 
     
     
-def get_model_links(compat):
-    links = {}
-    data_path = get_data_path()
-    if data_path:
-        models = [p for p in data_path.iterdir() if is_model_path(p)]
-        for model in models:
-            meta_path = Path(model) / 'meta.json'
-            if not meta_path.exists():
-                continue
-            meta = read_json(meta_path)
-            link = model.parts[-1]
-            name = meta['lang'] + '_' + meta['name']
-            links[link] = {'name': name, 'version': meta['version'],
-                           'compat': is_compat(compat, name, meta['version'])}
-    return links
+if __name__ == '__main__':
+    rs = RandomizedSet()
+    print('insert 1: ', rs.insert(1))
+    print('insert 2: ', rs.insert(2))
+    print('insert 3: ', rs.insert(3))
+    print('insert 4: ', rs.insert(4))
+    print('remove 3: ', rs.remove(3))
+    print('remove 3: ', rs.remove(3))
+    print('remove 1: ', rs.remove(1))
+    print('random: ', rs.get_random())
+    print('random: ', rs.get_random())
+    print('random: ', rs.get_random())
+    print('random: ', rs.get_random())
+
+    
+    Condensed mathematical description:
+    
+    
+# Python solution with only one table for B (~196ms):
+def multiply(self, a, b):
+    '''
+    :type A: List[List[int]]
+    :type B: List[List[int]]
+    :rtype: List[List[int]]
+    '''
+    if a is None or b is None: return None
+    m, n, l = len(a), len(a[0]), len(b[0])
+    if len(b) != n:
+        raise Exception('A's column number must be equal to B's row number.')
+    c = [[0 for _ in range(l)] for _ in range(m)]
+    table_b = {}
+    for k, row in enumerate(b):
+        table_b[k] = {}
+        for j, eleB in enumerate(row):
+            if eleB: table_b[k][j] = eleB
+    for i, row in enumerate(a):
+        for k, eleA in enumerate(row):
+            if eleA:
+                for j, eleB in table_b[k].iteritems():
+                    c[i][j] += eleA * eleB
+    return c
