fc844b339b5a7072f46038a1aacf03a4832d4614
SUNsung <weatherius@gmail.com>
2019-04-24 23:40:02 +0300

    Rewrite [22kHDzsFx68c22tq4HDqSuHyc22lU3ei9vaKs622LFn.py] File in: 2019-04-24 23:40:01

--- pyton/22kHDzsFx68c22tq4HDqSuHyc22lU3ei9vaKs622LFn.py
+++ pyton/22kHDzsFx68c22tq4HDqSuHyc22lU3ei9vaKs622LFn.py
@@ -1,145 +1,49 @@
 
         
-        
-class CiscoSparkNotificationService(BaseNotificationService):
-    '''The Cisco Spark Notification Service.'''
+                inputs[i, cur_pos:next_pos] = cur_stream[i][0][:how_many]
+        char_inputs[i, cur_pos:next_pos] = cur_stream[i][1][:how_many]
+        global_word_ids[i, cur_pos:next_pos] = cur_stream[i][2][:how_many]
+        targets[i, cur_pos:next_pos] = cur_stream[i][0][1:how_many+1]
+        weights[i, cur_pos:next_pos] = 1.0
     
-        def __init__(self, config):
-        '''Initialize the service.'''
-        self.api_key = config.get(CONF_API_KEY)
-        self.recipient = config.get(CONF_RECIPIENT)
+    import os
+import pickle as pkl
+import numpy as np
+import tensorflow as tf
+import utils
     
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_COMMAND): cv.string,
-    vol.Optional(CONF_NAME): cv.string,
-})
+      Returns:
+    mask1: mask for first sentence
+    mask2: mask for second sentence
+  '''
+  mask1_start, mask2_start = [], []
+  while sent1[0] == sent2[0]:
+    sent1 = sent1[1:]
+    sent2 = sent2[1:]
+    mask1_start.append(0.)
+    mask2_start.append(0.)
     
+      Returns:
+    loss: Scalar tf.float32 total loss.
+  '''
+  cross_entropy_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+      labels=gen_labels, logits=gen_logits)
+  # Maximize the dis_values (minimize the negative)
+  gan_loss = -dis_values
+  loss_matrix = tf.where(is_real_input, cross_entropy_loss, gan_loss)
+  loss = tf.reduce_mean(loss_matrix)
+  return loss
     
-class FacebookNotificationService(BaseNotificationService):
-    '''Implementation of a notification service for the Facebook service.'''
+      The `attention_decoder_fn_train` is called with a set of the user arguments
+  and returns the `decoder_fn`, which can be passed to the
+  `dynamic_rnn_decoder`, such that
     
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_FILENAME): cv.string,
-    vol.Optional(CONF_TIMESTAMP, default=False): cv.boolean,
-})
+        Examples:
+        以下示例使用了 TensorLayer 库来快速构建模型；本来想用 Keras，但是无法完整获取中间输出的 shape
     
-        def __init__(self, mycroft_ip):
-        '''Initialize the service.'''
-        self.mycroft_ip = mycroft_ip
-    
-            if not targets:
-            # Backward compatibility, notify all devices in own account.
-            self._push_data(message, title, data, self.pushbullet)
-            _LOGGER.info('Sent notification to self')
-            return
-    
-        def __init__(self, api_key, channel_name, send_test_msg):
-        '''Initialize the service.'''
-        from pushetta import Pushetta
-        self._api_key = api_key
-        self._channel_name = channel_name
-        self.is_valid = True
-        self.pushetta = Pushetta(api_key)
-    
-    import voluptuous as vol
-    
-    _LOGGER = logging.getLogger(__name__)
-    
-    from sentry.utils.query import RangeQuerySetWrapperWithProgressBar
-    
-            # User chose to not deal with backwards NULL issues for 'ReleaseEnvironment.project_id'
-        raise RuntimeError(
-            'Cannot reverse this migration. 'ReleaseEnvironment.project_id' and its values cannot be restored.'
-        )
-    
-            db.start_transaction()
-    
-    
-def identity(x):
-    ''''''
-    return tf.identity(x)
-    
-    
-def leaky_relu(x, alpha=0.1):
-    '''渗透 ReLU
-    `o = max(alpha * x, x)`
-    '''
-    return tf.nn.leaky_relu(x, alpha)
-    
-    
-def conv2d(x, kernel_size, out_channels,
-           act_fn=relu,
-           strides=1,
-           padding='SAME',
-           name=None,
-           reuse=None):
-    '''2-D 卷积层
-    Input shape:  [batch_size, in_h, in_w, in_channels]
-    Output shape: [batch_size, out_h, out_w, out_channels]
-    
-        Args:
-        h: context encoding     shape: [N, T, d]
-        u: question encoding    shape: [N, J, d]
-        T(int): context length
-        J(int): question length
-        d(int): features size
-        name(str):
-        reuse(bool):
-    
-    
-def repeat(x, n):
-    '''
-    Examples:
-        x.shape == [batch_size, n_input]
-        x = repeat(x, n_step)
-        x.shape == [batch_size, n_step, n_input]
-    
-        def __init__(self, animal_factory=None):
-        '''pet_factory is our abstract factory.  We can set it at will.'''
-    
-    
-class ComplexHouse(ComplexBuilding):
-    def build_floor(self):
-        self.floor = 'One'
-    
-    
-if __name__ == '__main__':
-    main()
-    
-        def find_all_path(self, start, end, path=None):
-        path = path or []
-        path.append(start)
-        if start == end:
-            return [path]
-        paths = []
-        for node in self.graph.get(start, []):
-            if node not in path:
-                newpaths = self.find_all_path(node, end, path[:])
-                paths.extend(newpaths)
-        return paths
-    
-    
-if __name__ == '__main__':
-    main()
-    
-        for shape in shapes:
-        shape.scale(2.5)
-        shape.draw()
-    
-    *What does this example do?
-This example shows a way to add formatting options (boldface and
-italic) to a text by appending the corresponding tags (<b> and
-<i>). Also, we can see that decorators can be applied one after the other,
-since the original text is passed to the bold wrapper, which in turn
-is passed to the italic wrapper.
-    
-        mobile_type = 'mobile'
-    tablet_type = 'tablet'
-    
-        def test_am_station_overflow_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
-    
-            self.assertEqual(noise, expected_noise)
+    print(word_vec)
+print(model.wv['aam'])
+'''
+[ 0.02210762 -0.10488641  0.05512805  0.09150169  0.00725085]
+[ 0.02210762 -0.10488641  0.05512805  0.09150169  0.00725085]
+'''
