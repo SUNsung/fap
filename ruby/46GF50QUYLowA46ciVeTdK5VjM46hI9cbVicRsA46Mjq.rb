65ad802ae5f63f93309dbcd098c2d930653bee4b
SUNsung <weatherius@gmail.com>
2019-04-24 11:38:35 +0300

    Rewrite [46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb] File in: 2019-04-24 11:38:34

--- ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
+++ ruby/46GF50QUYLowA46ciVeTdK5VjM46hI9cbVicRsA46Mjq.rb
@@ -1,145 +1,97 @@
 
         
-            # The path used after resending confirmation instructions.
-    def after_resending_confirmation_instructions_path_for(resource_name)
-      is_navigational_format? ? new_session_path(resource_name) : '/'
-    end
-    
-      private
-    
-    class TestApp < Rails::Application
-  config.root = File.dirname(__FILE__)
-  config.session_store :cookie_store, key: 'cookie_store_key'
-  secrets.secret_token    = 'secret_token'
-  secrets.secret_key_base = 'secret_key_base'
-  config.eager_load = false
-    
-                  define_method method do |resource_or_scope, *args|
-                scope = Devise::Mapping.find_scope!(resource_or_scope)
-                router_name = Devise.mappings[scope].router_name
-                context = router_name ? send(router_name) : _devise_route_context
-                context.send('#{action}#{scope}_#{module_name}_#{path_or_url}', *args)
-              end
-            end
-          end
-        end
+              it 'creates a new user' do
+        visit new_admin_user_path
+        fill_in 'Email', with: 'test@test.com'
+        fill_in 'Username', with: 'usertest'
+        fill_in 'Password', with: '12345678'
+        fill_in 'Password confirmation', with: '12345678'
+        click_on 'Create User'
+        expect(page).to have_text('User 'usertest' was successfully created.')
+        expect(page).to have_text('test@test.com')
       end
     
-        def default_path_names(options)
-      @path_names = Hash.new { |h,k| h[k] = k.to_s }
-      @path_names[:registration] = ''
-      @path_names.merge!(options[:path_names]) if options[:path_names]
+        it 'returns a Glyphicon icon element with an addidional class' do
+      icon = icon_tag('glyphicon-help', class: 'text-info')
+      expect(icon).to be_html_safe
+      expect(Nokogiri(icon).at('span.glyphicon.glyphicon-help.text-info')).to be_a Nokogiri::XML::Element
     end
     
-                if class_mod.respond_to?(:available_configs)
-              available_configs = class_mod.available_configs
-              available_configs.each do |config|
-                next unless options.key?(config)
-                send(:'#{config}=', options.delete(config))
-              end
-            end
-          end
-    
-        def resource_params
-      params.require(:user).permit(
-        :unconfirmed_email
-      )
+        it 'is turned off for existing instances of Huginn' do
+      stub(DefaultScenarioImporter).seed { fail 'seed should not have been called'}
+      stub.proxy(ENV).[](anything)
+      stub(ENV).[]('IMPORT_DEFAULT_SCENARIO_FOR_ALL_USERS') { nil }
+      DefaultScenarioImporter.import(user)
     end
+    
+        stub.any_instance_of(Agents::SchedulerAgent).second_precision_enabled { true }
+    
+          a = '        Events will have the fields you specified.  Your options look like:\n\n            {\n      \'url\': {\n        \'css\': \'#comic img\',\n        \'value\': \'@src\'\n      },\n      \'title\': {\n        \'css\': \'#comic img\',\n        \'value\': \'@title\'\n      }\n    }\'\n'
+      expect(Utils.unindent(a)).to eq('Events will have the fields you specified.  Your options look like:\n\n    {\n      \'url\': {\n\'css\': \'#comic img\',\n\'value\': \'@src\'\n      },\n      \'title\': {\n\'css\': \'#comic img\',\n\'value\': \'@title\'\n      }\n    }\'')
+    end
+  end
+    
+        it 'should generate the correct projects url' do
+      expect(@checker.send(:projects_url)).to eq('https://basecamp.com/12345/api/v1/projects.json')
+    end
+    
+        ntheta    = NAO_SAMPLES
+    nphi      = NAO_SAMPLES
+    eps       = 0.0001
+    occlusion = 0.0
+    
+    Then /^I should see an image in the publisher$/ do
+  photo_in_publisher.should be_present
+end
+    
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
+  end
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
   end
 end
-
     
-      private
-    
-      before_action :set_account
-  respond_to :txt
-    
-        render_empty
-  end
-    
-    class Auth::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token
-    
-      # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-    
-      # Setup a color scheme called 'bright' than can be used to add color codes
-  # to the pattern layout. Color schemes should only be used with appenders
-  # that write to STDOUT or STDERR; inserting terminal color codes into a file
-  # is generally considered bad form.
-  Logging.color_scheme('bright',
-                       levels:  {
-                         info:  :green,
-                         warn:  :yellow,
-                         error: :red,
-                         fatal: %i(white on_red)
-                       },
-                       date:    :blue,
-                       logger:  :cyan,
-                       message: :magenta
-                      )
-    
-    class PolymorphicMentions < ActiveRecord::Migration[4.2]
-  def change
-    remove_index :mentions, column: %i(post_id)
-    remove_index :mentions, column: %i(person_id post_id), unique: true
-    rename_column :mentions, :post_id, :mentions_container_id
-    add_column :mentions, :mentions_container_type, :string
-    add_index :mentions,
-              %i(mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_mc_id_and_mc_type',
-              length: {mentions_container_type: 191}
-    add_index :mentions,
-              %i(person_id mentions_container_id mentions_container_type),
-              name:   'index_mentions_on_person_and_mc_id_and_mc_type',
-              length: {mentions_container_type: 191},
-              unique: true
-    
-    RSpec::Matchers.define :have_path_in do |expected|
-  match do |actual|
-    await_condition { expected.include? actual.current_path }
-  end
-    
-    describe ContactsController, :type => :controller do
-  describe '#index' do
-    before do
-      AppConfig.chat.enabled = true
-      @aspect = bob.aspects.create(:name => 'another aspect')
-      bob.share_with alice.person, @aspect
-      bob.share_with eve.person, @aspect
-      sign_in bob, scope: :user
-    end
-    
-    describe StatusMessagesController, :type => :controller do
-  describe '#bookmarklet' do
-    before do
-      sign_in bob, scope: :user
-    end
-    
-      let(:valid_params) {
-    {
-      user: {
-        username:              'jdoe',
-        email:                 'jdoe@example.com',
-        password:              'password',
-        password_confirmation: 'password'
-      }
-    }
-  }
-    
-          def html?(headers)
-        return false unless header = headers.detect { |k,v| k.downcase == 'content-type' }
-        options[:html_types].include? header.last[/^\w+\/\w+/]
+          # Returns the iteration variable of the `for` loop.
+      #
+      # @return [Node] The iteration variable of the `for` loop
+      def variable
+        node_parts[0]
       end
-    end
-  end
-end
-
     
-          def csp_policy
-        directives = []
+          # A shorthand for getting the first argument of the node.
+      # Equivalent to `arguments.first`.
+      #
+      # @return [Node, nil] the first argument of the node,
+      #                     or `nil` if there are no arguments
+      def first_argument
+        arguments[0]
+      end
     
-      it 'should allow setting CSP3 no arg directives' do
-    mock_app do
-      use Rack::Protection::ContentSecurityPolicy, :block_all_mixed_content => true, :disown_opener => true, :upgrade_insecure_requests => true
+          # Calls the given block for each condition node in the `when` branch.
+      # If no block is given, an `Enumerator` is returned.
+      #
+      # @return [self] if a block is given
+      # @return [Enumerator] if no block is given
+      def each_condition
+        return conditions.to_enum(__method__) unless block_given?
+    
+          each_mentioned_cop do |cop_name, disabled, line, single_line|
+        analyses[cop_name] =
+          analyze_cop(analyses[cop_name], disabled, line, single_line)
+      end
+    
+    module RuboCop
+  module Cop
+    # Identifier of all cops containing a department and cop name.
+    #
+    # All cops are identified by their badge. For example, the badge
+    # for `RuboCop::Cop::Layout::Tab` is `Layout/Tab`. Badges can be
+    # parsed as either `Department/CopName` or just `CopName` to allow
+    # for badge references in source files that omit the department
+    # for RuboCop to infer.
+    class Badge
+      # Error raised when a badge parse fails.
+      class InvalidBadge < Error
+        MSG = 'Invalid badge %<badge>p. ' \
+              'Expected `Department/CopName` or `CopName`.'.freeze
