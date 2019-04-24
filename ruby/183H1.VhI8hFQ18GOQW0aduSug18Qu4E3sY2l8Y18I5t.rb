3037664fb284068510ee9b1dfca180c5fddb5b44
SUNsung <weatherius@gmail.com>
2019-04-24 08:17:22 +0300

    Rewrite [183H1.VhI8hFQ18GOQW0aduSug18Qu4E3sY2l8Y18I5t.rb] File in: 2019-04-24 08:17:22

--- ruby/183H1.VhI8hFQ18GOQW0aduSug18Qu4E3sY2l8Y18I5t.rb
+++ ruby/183H1.VhI8hFQ18GOQW0aduSug18Qu4E3sY2l8Y18I5t.rb
@@ -1,96 +1,121 @@
 
         
-              * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-      * Redistributions in binary form must reproduce the above
-        copyright notice, this list of conditions and the following
-        disclaimer in the documentation and/or other materials provided
-        with the distribution.
-      * Neither the name of Google Inc. nor the names of its
-        contributors may be used to endorse or promote products derived
-        from this software without specific prior written permission.
+                  # First determine the proper array of VMs.
+          vms = []
+          if names.length > 0
+            names.each do |name|
+              if pattern = name[/^\/(.+?)\/$/, 1]
+                # This is a regular expression name, so we convert to a regular
+                # expression and allow that sort of matching.
+                regex = Regexp.new(pattern)
     
-          def format
-        @format = (@page.format || false) if @format.nil? && @page
-        @format.to_s.downcase
+            # This returns all the registered host capabilities.
+        #
+        # @return [Hash]
+        def host_capabilities
+          results = Hash.new { |h, k| h[k] = Registry.new }
+    
+        remove_duplicates
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
+    
+    module Workers
+  class PublishToHub < Base
+    def perform(*_args)
+      # don't publish to pubsubhubbub in cucumber
+    end
+  end
+    
+        it 'generates the aspects_manage fixture', :fixture => true do
+      get :index, params: {a_id: @aspect.id}
+      save_fixture(html_for('body'), 'aspects_manage')
+    end
+    
+        it 'does redirect if there is an invalid invite token' do
+      get :new, params: {invite: {token: 'fssdfsd'}}
+      expect(response).to redirect_to new_user_session_path
+    end
+    
+      # fetch data
+  fields = {
+    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
+    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
+    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
+  }
+    
+          def instrument(env)
+        return unless i = options[:instrumenter]
+        env['rack.protection.attack'] = self.class.name.split('::').last.downcase
+        i.instrument('rack.protection', env)
       end
     
-          # http://stackoverflow.com/questions/9445760/bit-shifting-in-ruby
-      def left_shift int, shift
-        r = ((int & 0xFF) << (shift & 0x1F)) & 0xFFFFFFFF
-        # 1>>31, 2**32
-        (r & 2147483648) == 0 ? r : r - 4294967296
+          def call(env)
+        status, headers, body = super
+        response = Rack::Response.new(body, status, headers)
+        request = Rack::Request.new(env)
+        remove_bad_cookies(request, response)
+        response.finish
       end
     
-          # Finds header node inside Nokogiri::HTML document.
-      #
-      def find_header_node(doc)
-        case @page.format
-          when :asciidoc
-            doc.css('div#gollum-root > h1:first-child')
-          when :org
-            doc.css('div#gollum-root > p.title:first-child')
-          when :pod
-            doc.css('div#gollum-root > a.dummyTopAnchor:first-child + h1')
-          when :rest
-            doc.css('div#gollum-root > div > div > h1:first-child')
-          else
-            doc.css('div#gollum-root > h1:first-child')
+          def escape(object)
+        case object
+        when Hash   then escape_hash(object)
+        when Array  then object.map { |o| escape(o) }
+        when String then escape_string(object)
+        when Tempfile then object
+        else nil
         end
       end
     
-    # Silence locale validation warning
-require 'i18n'
-I18n.enforce_available_locales = false
-    
-    # Read command line options into `options` hash
-begin
-  opts.parse!
-rescue OptionParser::InvalidOption
-  puts 'gollum: #{$!.message}'
-  puts 'gollum: try 'gollum --help' for more information'
-  exit
+        it 'Returns nil when Referer header is invalid' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => 'http://bar.com/bad|uri'}
+      expect(subject.referrer(env)).to be_nil
+    end
+  end
 end
+
     
-        # Query details about our now-installed package.
-    # We do this by using 'npm ls' with json + long enabled to query details
-    # about the installed package.
-    npm_ls_out = safesystemout(attributes[:npm_bin], 'ls', '--json', '--long', *npm_flags)
-    npm_ls = JSON.parse(npm_ls_out)
-    name, info = npm_ls['dependencies'].first
-    
-        ::Dir.mkdir(File.join(builddir, 'manifests'))
-    manifests.each do |manifest|
-      dir = File.join(builddir, 'manifests', File.dirname(manifest))
-      logger.info('manifests targeting: #{dir}')
-      ::Dir.mkdir(dir) if !File.directory?(dir)
-    
-      def create_scripts
-    if script?(:after_install)
-      File.write(File.join(fpm_meta_path, 'after_install'), script(:after_install))
-    end
+      def send_sinatra_file(path, &missing_file_block)
+    file_path = File.join(File.dirname(__FILE__), 'public',  path)
+    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
+    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
   end
     
-        File.utime(source_stat.atime, source_stat.mtime, destination)
-    mode = source_stat.mode
+    module Jekyll
+    
+    Liquid::Template.register_tag('video', Jekyll::VideoTag)
+    
+        # Reference
+    # http://www.debian.org/doc/manuals/maint-guide/first.en.html
+    # http://wiki.debian.org/DeveloperConfiguration
+    # https://github.com/jordansissel/fpm/issues/37
+    if ENV.include?('DEBEMAIL') and ENV.include?('DEBFULLNAME')
+      # Use DEBEMAIL and DEBFULLNAME as the default maintainer if available.
+      @maintainer = '#{ENV['DEBFULLNAME']} <#{ENV['DEBEMAIL']}>'
+    else
+      # TODO(sissel): Maybe support using 'git config' for a default as well?
+      # git config --get user.name, etc can be useful.
+      #
+      # Otherwise default to user@currenthost
+      @maintainer = '<#{ENV['USER']}@#{Socket.gethostname}>'
+    end
+    
+      def to_s_extension; 'txz'; end
+    
+      # Helper for group lookup
+  def gid2group(gid)
     begin
-      File.lchown(source_stat.uid, source_stat.gid, destination)
-    rescue Errno::EPERM
-      # clear setuid/setgid
-      mode &= 01777
+      grent = Etc.getgrgid(gid)
+      return grent.name
+    rescue ArgumentError => e
+      # Invalid user id? No user? Return the uid.
+      logger.warn('Failed to find group for gid #{gid}')
+      return gid.to_s
     end
+  end # def uid2user
+end # class FPM::Target::Puppet
     
-        desc 'version', 'Show Bourbon version'
-    def version
-      say 'Bourbon #{Bourbon::VERSION}'
-    end
-    
-      context 'called with arguments (1, $ratio: $golden-ratio)' do
-    it 'output the first value from the golden ratio scale' do
-      expect('.one-golden-ratio').to have_rule('font-size: 1.618em')
-    end
-  end
-    
-      context 'called with one size' do
-    it 'applies same width to both height and width' do
-      rule = 'height: 10px; width: 10px;'
+      # Returns the path to the tar file containing the packed up staging directory
+  def payload
+    payload_tar = build_path('payload.tar')
+    logger.info('Creating payload tar ', :path => payload_tar)
