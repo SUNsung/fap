eeec3e1beb457432f565f2e160ad9a503b54484f
SUNsung <weatherius@gmail.com>
2019-04-24 10:41:21 +0300

    Rewrite [20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb] File in: 2019-04-24 10:41:20

--- ruby/20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb
+++ ruby/20prHYUcKLOmw20aKy6Jnqkcxg20PHiyDt2VOYE20O.D.rb
@@ -1,126 +1,115 @@
 
         
-        DATA = {'foo'=>'bar', 'alpha'=>{'beta'=>'gamma'}, 'lipsum'=>['lorem', 'ipsum', 'dolor']}
-    
-                c.action do |args, opts|
-              Jekyll::Commands::NewTheme.process(args, opts)
-            end
-          end
-        end
-    
-              # WebSockets requests will have a Connection: Upgrade header
-          if parser.http_method != 'GET' || parser.upgrade?
-            super
-          elsif parser.request_url =~ %r!^\/livereload.js!
-            headers = [
-              'HTTP/1.1 200 OK',
-              'Content-Type: application/javascript',
-              'Content-Length: #{reload_size}',
-              '',
-              '',
-            ].join('\r\n')
-            send_data(headers)
-    
-    module Gitlab
-  module GithubImport
-    module Importer
-      class DiffNotesImporter
-        include ParallelScheduling
-    
-                rows << {
-              label_id: label_id,
-              target_id: target_id,
-              target_type: issue.issuable_type,
-              created_at: time,
-              updated_at: time
-            }
-          end
-    
-            def importer_class
-          LfsObjectImporter
-        end
-    
-            # Builds a user from a GitHub API response.
-        #
-        # user - An instance of `Sawyer::Resource` containing the user details.
-        def self.from_api_response(user)
-          new(id: user.id, login: user.login)
-        end
-    
-    module RuboCop
-  module AST
-    # A node extension for `hash` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `hash` nodes within RuboCop.
-    class HashNode < Node
-      # Returns an array of all the key value pairs in the `hash` literal.
-      #
-      # @return [Array<PairNode>] an array of `pair` nodes
-      def pairs
-        each_pair.to_a
-      end
-    
-    module RuboCop
-  module AST
-    # A node extension for `when` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `when` nodes within RuboCop.
-    class WhenNode < Node
-      # Returns an array of all the conditions in the `when` branch.
-      #
-      # @return [Array<Node>] an array of condition nodes
-      def conditions
-        node_parts[0...-1]
-      end
-    
-          # finds class for a given symbol / string
-      #
-      # Example :
-      # :products returns Spree::Product
-      # :my_products returns MyProduct if MyProduct is defined
-      # :my_products returns My::Product if My::Product is defined
-      # if cannot constantize it returns nil
-      # This will allow us to use cancan abilities on tab
-      def klass_for(name)
-        model_name = name.to_s
-    
-        def checkout_progress(numbers: false)
-      states = @order.checkout_steps
-      items = states.each_with_index.map do |state, i|
-        text = Spree.t('order_state.#{state}').titleize
-        text.prepend('#{i.succ}. ') if numbers
-    
-            def update
-          authorize! :update, @order, order_token
-          @address = find_address
-    
-                if @order.completed? || @order.next
-              state_callback(:after)
-              respond_with(@order, default_template: 'spree/api/v1/orders/show')
-            else
-              respond_with(@order, default_template: 'spree/api/v1/orders/could_not_transition', status: 422)
-            end
-          else
-            invalid_resource!(@order)
-          end
-        end
-    
-            def destroy
-          @option_value = scope.accessible_by(current_ability, :destroy).find(params[:id])
-          @option_value.destroy
-          render plain: nil, status: 204
-        end
-    
-        execute 'restart-sidekiq' do 
-      command %Q{ 
-        echo 'sleep 20 && monit -g #{app}_sidekiq restart all' | at now 
-      }
+          # Helper for use after calling send_*_instructions methods on a resource.
+  # If we are in paranoid mode, we always act as if the resource was valid
+  # and instructions were sent.
+  def successfully_sent?(resource)
+    notice = if Devise.paranoid
+      resource.errors.clear
+      :send_paranoid_instructions
+    elsif resource.errors.empty?
+      :send_instructions
     end
-  end 
-end
-
     
           private
     
-      Sidekiq::WebAction.class_eval 'def _render\n#{ERB.new(File.read(Web::LAYOUT)).src}\nend'
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
+    end
+    
+    When /^I fill out change password section with my password and '([^']*)' and '([^']*)'$/ do |new_pass, confirm_pass|
+  fill_change_password_section(@me.password, new_pass, confirm_pass)
 end
+    
+    def await_condition &condition
+  start_time = Time.zone.now
+  until condition.call
+    return false if (Time.zone.now - start_time) > Capybara.default_max_wait_time
+    sleep 0.05
+  end
+  true
+end
+
+    
+      def confirm_on_page(page_name)
+    if page_name == 'my profile page'
+      expect(page).to have_path_in([person_path(@me.person), user_profile_path(@me.username)])
+    else
+      expect(page).to have_path(path_to(page_name))
+    end
+  end
+end
+    
+    describe ConversationsController, :type => :controller do
+  describe '#index' do
+    before do
+      @person = alice.contacts.first.person
+      hash = {
+        :author => @person,
+        :participant_ids => [alice.person.id, @person.id],
+        :subject => 'not spam',
+        :messages_attributes => [ {:author => @person, :text => 'cool stuff'} ]
+      }
+      @conv1 = Conversation.create(hash)
+      Message.create(:author => @person, :created_at => Time.now + 100, :text => 'message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+      Message.create(:author => @person, :created_at => Time.now + 200, :text => 'another message', :conversation_id => @conv1.id)
+             .increase_unread(alice)
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
+    end
+  end
+end
+
+    
+      # fetch data
+  fields = {
+    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
+    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
+    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
+  }
+    
+          class << self
+        alias escape_url escape
+        public :escape_html
+      end
+    
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   CSRF
+    # Supported browsers:: all
+    # More infos::         http://flask.pocoo.org/docs/0.10/security/#json-security
+    #                      http://haacked.com/archive/2008/11/20/anatomy-of-a-subtle-json-vulnerability.aspx
+    #
+    # JSON GET APIs are vulnerable to being embedded as JavaScript when the
+    # Array prototype has been patched to track data. Checks the referrer
+    # even on GET requests if the content type is JSON.
+    #
+    # If request includes Origin HTTP header, defers to HttpOrigin to determine
+    # if the request is safe. Please refer to the documentation for more info.
+    #
+    # The `:allow_if` option can be set to a proc to use custom allow/deny logic.
+    class JsonCsrf < Base
+      default_options :allow_if => nil
+    
+    # The project root directory
+$root = ::File.dirname(__FILE__)
+    
+      class ImageTag < Liquid::Tag
+    @img = nil
+    
+        def type_from_mime_magic
+      @type_from_mime_magic ||= File.open(@filepath) do |file|
+        MimeMagic.by_magic(file).try(:type)
+      end
+    end
