0ef466cddb843fe5aded97070f7de932f2256d30
SUNsung <weatherius@gmail.com>
2019-04-25 05:24:19 +0300

    Rewrite [179zOb0EJ4tjU17x.nC93ltSGI17Ez85h46WLng17B1q.cpp] File in: 2019-04-25 05:24:19

--- cccccc/179zOb0EJ4tjU17x.nC93ltSGI17Ez85h46WLng17B1q.cpp
+++ cccccc/179zOb0EJ4tjU17x.nC93ltSGI17Ez85h46WLng17B1q.cpp
@@ -1,79 +1,292 @@
 
         
-        AuthPropertyIterator AuthPropertyIterator::operator++(int) {
-  AuthPropertyIterator tmp(*this);
-  operator++();
-  return tmp;
+        bool CacheImpl::remove(const void *Key) {
+  int Ret = cache_remove(static_cast<cache_t*>(Impl), const_cast<void*>(Key));
+  return Ret == 0;
 }
     
-    constexpr size_t TraceContextEncoding::kGrpcTraceContextSize;
-constexpr size_t TraceContextEncoding::kEncodeDecodeFailure;
-constexpr size_t TraceContextEncoding::kVersionIdSize;
-constexpr size_t TraceContextEncoding::kFieldIdSize;
-constexpr size_t TraceContextEncoding::kVersionIdOffset;
-constexpr size_t TraceContextEncoding::kVersionId;
+      std::size_t capacity = EndOfAllocation - Begin;
+  std::size_t requiredCapacity = capacity + needed;
+  do {
+    capacity = 2 * capacity + 16;
+  } while (capacity < requiredCapacity);
     
-    #include <grpc/support/port_platform.h>
-    
-    #include <grpcpp/grpcpp.h>
-#include 'src/proto/grpc/reflection/v1alpha/reflection.grpc.pb.h'
-    
-    ThreadPoolInterface* CreateDefaultThreadPool() { return g_ctp_impl(); }
-    
-    std::pair<uint64_t, uint64_t> GetCpuStatsImpl() {
-  uint64_t busy = 0, total = 0;
-  host_cpu_load_info_data_t cpuinfo;
-  mach_msg_type_number_t count = HOST_CPU_LOAD_INFO_COUNT;
-  if (host_statistics(mach_host_self(), HOST_CPU_LOAD_INFO,
-                      (host_info_t)&cpuinfo, &count) == KERN_SUCCESS) {
-    for (int i = 0; i < CPU_STATE_MAX; i++) total += cpuinfo.cpu_ticks[i];
-    busy = total - cpuinfo.cpu_ticks[CPU_STATE_IDLE];
-  }
-  return std::make_pair(busy, total);
-}
-    
-    	wchar_t message[1024];
-	swprintf_s(message, L'%s\nFunction: %s\nLine: %d', buffer, func, line);
-	LocalFree(buffer);
-    
-    // System gflags
-DEFINE_string(sensor_node_name, '', 'Sensor node name.');
-
-    
-    #include 'glog/logging.h'
-    
-    unsigned int BaseMapMatrix::CreateBinary(unsigned char* buf,
-                                         unsigned int buf_size) const {
-  return 0;
-}
-    
-      const double init_derivative = 0.1;
-  constraint.AddSecondDerivativeBoundary(init_derivative, index_list,
-                                         lower_bound, upper_bound);
-  const auto mat = constraint.inequality_constraint_matrix();
-  const auto bd = constraint.inequality_constraint_boundary();
-    
-    TEST(TestPiecewiseLinearKernel, add_third_order_derivative_matrix) {
-  PiecewiseLinearKernel kernel(10, 0.1);
-  const double init_derivative = 5.0;
-  const double init_second_derivative = 2.0;
+      while (!RawText.empty()) {
+    size_t Pos = RawText.find_first_of('\n\r');
+    if (Pos == StringRef::npos)
+      Pos = RawText.size();
     }
     
-    TEST_F(GemMessageManagerTest, GetSendProtocols) {
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Accelcmd67::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Brakecmd6b::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Globalcmd69::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Headlightcmd76::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Horncmd78::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Shiftcmd65::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Steeringcmd6d::ID) !=
-              nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Turncmd63::ID) != nullptr);
-  EXPECT_TRUE(manager_.GetMutableProtocolDataById(Wipercmd90::ID) != nullptr);
+    /// Determine whether this typedef is a CF type.
+bool isCFTypeDecl(const clang::TypedefNameDecl *Decl);
+    
+    char Mangle::getStandardTypeSubst(StringRef TypeName) {
+#define STANDARD_TYPE(KIND, MANGLING, TYPENAME)      \
+  if (TypeName == #TYPENAME) {                       \
+    return #MANGLING[0];                             \
+  }
+    }
+    
+    using namespace swift;
+using namespace Demangle;
+    
+      if (!Files.empty() && !hasAnyPrimaryFiles) {
+    Optional<std::vector<std::string>> userSuppliedNamesOrErr =
+        OutputFilesComputer::getOutputFilenamesFromCommandLineOrFilelist(Args,
+                                                                         Diags);
+    if (userSuppliedNamesOrErr && userSuppliedNamesOrErr->size() == 1)
+      result.setIsSingleThreadedWMO(true);
+  }
+    
+      /// Returns the newly set-up FrontendInputsAndOutputs, as well as a set of
+  /// any unused primary files (those that do not correspond to an input).
+  std::pair<FrontendInputsAndOutputs, std::set<StringRef>>
+  createInputFilesConsumingPrimaries(std::set<StringRef> primaryFiles);
+    
+    
+    {    ICOORD pt;
+    int halfwidth;
+  };
+  // Type holds the distance of each point from the fitted line and the point
+  // itself. Use of double allows integer distances from ICOORDs to be stored
+  // exactly, and also the floating point results from ConstrainedFit.
+  using DistPointPair = KDPairInc<double, ICOORD>;
+    
+      double m() const;  // get gradient
+  double c(double m) const;            // get constant
+  double rms(double m, double c) const;            // get error
+  double pearson() const;  // get correlation coefficient.
+    
+    #include <cstdio>
+    
+    // Computes the histogram for the given image rectangle, and the given
+// single channel. Each channel is always one byte per pixel.
+// Histogram is always a kHistogramSize(256) element array to count
+// occurrences of each pixel value.
+void HistogramRect(Pix* src_pix, int channel,
+                   int left, int top, int width, int height,
+                   int* histogram);
+    
+        int16_t reject_count() {  //How many rejects?
+      return len - accept_count ();
+    }
+    
+    // A smart pointer class that implements a double-ended pointer. Each end
+// points to the other end. The copy constructor and operator= have MOVE
+// semantics, meaning that the relationship with the other end moves to the
+// destination of the copy, leaving the source unattached.
+// For this reason both the copy constructor and the operator= take a non-const
+// reference argument, and the const reference versions cannot be used.
+// DoublePtr is useful to incorporate into structures that are part of a
+// collection such as GenericVector or STL containers, where reallocs can
+// relocate the members. DoublePtr is also useful in a GenericHeap, where it
+// can correctly maintain the pointer to an element of the heap despite it
+// getting moved around on the heap.
+class DoublePtr {
+ public:
+  DoublePtr() : other_end_(nullptr) {}
+  // Copy constructor steals the partner off src and is therefore a non
+  // const reference arg.
+  // Copying a const DoublePtr generates a compiler error.
+  DoublePtr(DoublePtr& src) {
+    other_end_ = src.other_end_;
+    if (other_end_ != nullptr) {
+      other_end_->other_end_ = this;
+      src.other_end_ = nullptr;
+    }
+  }
+  // Operator= steals the partner off src, and therefore needs src to be a non-
+  // const reference.
+  // Assigning from a const DoublePtr generates a compiler error.
+  void operator=(DoublePtr& src) {
+    Disconnect();
+    other_end_ = src.other_end_;
+    if (other_end_ != nullptr) {
+      other_end_->other_end_ = this;
+      src.other_end_ = nullptr;
+    }
+  }
+    }
+    
+    struct Options;
+struct FileMetaData;
+    
+    #include <stdint.h>
+#include 'leveldb/db.h'
+#include 'db/dbformat.h'
+    
+    bool HandleDumpCommand(Env* env, char** files, int num) {
+  StdoutPrinter printer;
+  bool ok = true;
+  for (int i = 0; i < num; i++) {
+    Status s = DumpFile(env, files[i], &printer);
+    if (!s.ok()) {
+      fprintf(stderr, '%s\n', s.ToString().c_str());
+      ok = false;
+    }
+  }
+  return ok;
 }
+    
+        // Parse the header
+    const char* header = buffer_.data();
+    const uint32_t a = static_cast<uint32_t>(header[4]) & 0xff;
+    const uint32_t b = static_cast<uint32_t>(header[5]) & 0xff;
+    const unsigned int type = header[6];
+    const uint32_t length = a | (b << 8);
+    if (kHeaderSize + length > buffer_.size()) {
+      size_t drop_size = buffer_.size();
+      buffer_.clear();
+      if (!eof_) {
+        ReportCorruption(drop_size, 'bad record length');
+        return kBadRecord;
+      }
+      // If the end of the file has been reached without reading |length| bytes
+      // of payload, assume the writer died in the middle of writing the record.
+      // Don't report a corruption.
+      return kEof;
+    }
+    
+    namespace leveldb {
+    }
+    
+        SnapshotImpl* snapshot = new SnapshotImpl(sequence_number);
+    
+        // Preparing sqlite3 statements
+    status = sqlite3_prepare_v2(db_, replace_str.c_str(), -1,
+                                &replace_stmt, nullptr);
+    ErrorCheck(status);
+    status = sqlite3_prepare_v2(db_, begin_trans_str.c_str(), -1,
+                                &begin_trans_stmt, nullptr);
+    ErrorCheck(status);
+    status = sqlite3_prepare_v2(db_, end_trans_str.c_str(), -1,
+                                &end_trans_stmt, nullptr);
+    ErrorCheck(status);
+    
+    namespace ImGuiFreeType
+{
+    // Hinting greatly impacts visuals (and glyph sizes).
+    // When disabled, FreeType generates blurrier glyphs, more or less matches the stb's output.
+    // The Default hinting mode usually looks good, but may distort glyphs in an unusual way.
+    // The Light hinting mode generates fuzzier glyphs but better matches Microsoft's rasterizer.
+    }
+    
+    // Data
+static ID3D11Device*            g_pd3dDevice = NULL;
+static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
+static IDXGISwapChain*          g_pSwapChain = NULL;
+static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
+    
+    
+    {    InputTextCallback_UserData cb_user_data;
+    cb_user_data.Str = str;
+    cb_user_data.ChainCallback = callback;
+    cb_user_data.ChainCallbackUserData = user_data;
+    return InputTextMultiline(label, (char*)str->c_str(), str->capacity() + 1, size, flags, InputTextCallback, &cb_user_data);
+}
+    
+    // !!! GLUT/FreeGLUT IS OBSOLETE SOFTWARE. Using GLUT is not recommended unless you really miss the 90's. !!!
+// !!! If someone or something is teaching you GLUT in 2019, you are being abused. Please show some resistance. !!!
+    
+    #include 'modules/drivers/canbus/proto/can_card_parameter.pb.h'
+    
+    int ObjectExtendedInfo60D::object_id(const std::uint8_t* bytes,
+                                     int32_t length) const {
+  Byte t0(bytes);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    namespace apollo {
+namespace planning {
+    }
+    }
+    
+    Eigen::MatrixXd SplineSegKernel::ThirdOrderDerivativeKernel(
+    const uint32_t num_params, const double accumulated_x) {
+  if (num_params > reserved_order_ + 1) {
+    CalculateThirdOrderDerivative(num_params);
+  }
+  Eigen::MatrixXd term_matrix;
+  IntegratedTermMatrix(num_params, accumulated_x, 'third_order', &term_matrix);
+  return (kernel_third_order_derivative_.block(0, 0, num_params, num_params))
+      .cwiseProduct(term_matrix);
+}
+    
+    // config detail: {'name': 'manual_input', 'offset': 0.0, 'precision': 0.001,
+// 'len': 16, 'is_signed_var': False, 'physical_range': '[0|1]', 'bit': 7,
+// 'type': 'double', 'order': 'motorola', 'physical_unit': '%'}
+double Brakerpt6c::manual_input(const std::uint8_t* bytes,
+                                int32_t length) const {
+  Byte t0(bytes + 0);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    #include 'modules/canbus/vehicle/gem/protocol/headlight_rpt_77.h'
+    
+        QObject::connect(&example, SIGNAL(finished()), &app, SLOT(quit()));
+    QTimer::singleShot(0, &example, SLOT(run()));
+    
+    #endif /* !__HIREDIS_EXAMPLE_QT_H */
+
+    
+            size_t size = cache_list.size();
+        if (size == cache_capacity && size > 0)
+        {
+            auto del = cache_list.back();
+            cache_map.erase(del.first);
+            cache_list.pop_back();
+        }
+    
+    int main(int argc, char **argv)
+{
+    swoole_init();
+    }
+    
+    using namespace swoole;
+    
+    #include <atomic>
+    
+    static inline void coro_test_create(coroutine_func_t fn, void *arg, int *complete_num)
+{
+    auto test = new coro_test(fn, arg, complete_num);
+    long cid = swoole::Coroutine::create(coro_test_fn, test);
+    ASSERT_GT(cid, 0);
+}
+    
+        coro_test({
+        make_pair([](void *arg)
+        {
+            auto chan = (Channel *) arg;
+            int i = 1;
+            bool ret;
+    }
+    }
     
     
     {
-    {}  // namespace canbus
-}  // namespace apollo
+    {        usleep(100000);
+        int status = -1;
+        pid_t pid2 = swoole_coroutine_waitpid(pid, &status, 0);
+        ASSERT_EQ(status, 0);
+        ASSERT_EQ(pid, pid2);
+    });
+}
+    
+    namespace swoole_test
+{
+static int my_onPacket(swServer *serv, swEventData *req);
+static int my_onReceive(swServer *serv, swEventData *req);
+static void my_onStart(swServer *serv);
+static void my_onShutdown(swServer *serv);
+static void my_onConnect(swServer *serv, swDataHead *info);
+static void my_onClose(swServer *serv, swDataHead *info);
+static void my_onWorkerStart(swServer *serv, int worker_id);
+static void my_onWorkerStop(swServer *serv, int worker_id);
+    }
+    
+      void ExecuteAction();
+    
+    namespace WeexCore {
+    }
+    
+      void ExecuteAction();
