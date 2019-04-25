5416e42127de0d9302f3dd59c448e09a247a4331
SUNsung <weatherius@gmail.com>
2019-04-25 07:32:01 +0300

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2019-04-25 07:32:00

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,307 +1,147 @@
 
         
-        namespace swig {
-namespace {
-    }
-    }
-    
-        http://www.apache.org/licenses/LICENSE-2.0
-    
-    Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    
-    #include 'tensorflow/python/grappler/model_analyzer.h'
-    
-    // Creates a numpy array in 'ret' which either aliases the content of 't' or has
-// a copy.
-Status TensorToNdarray(const Tensor& t, PyObject** ret);
-    
-    #ifndef PyObject_HEAD
-struct _object;
-typedef _object PyObject;
-#endif
-    
-    #ifndef TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_PLATFORM_ID_H_
-#define TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_PLATFORM_ID_H_
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    
-    {  return Begin + oldSize;
-}  
-
-    
-    namespace {
-  class ClangDiagRenderer final : public clang::DiagnosticNoteRenderer {
-    const llvm::function_ref<void(clang::FullSourceLoc,
-                                  clang::DiagnosticsEngine::Level,
-                                  StringRef)> callback;
-    }
-    }
-    
-    
-    {  llvm_unreachable('invalid class');
-}
-    
-    class LLVM_LIBRARY_VISIBILITY GenericUnix : public ToolChain {
-protected:
-  InvocationInfo constructInvocation(const InterpretJobAction &job,
-                                     const JobContext &context) const override;
-  InvocationInfo constructInvocation(const AutolinkExtractJobAction &job,
-                                     const JobContext &context) const override;
-    }
-    
-    #endif // BITCOIN_QT_NETWORKSTYLE_H
-
-    
-            friend void vformat(std::ostream& out, const char* fmt,
-                            const FormatList& list);
-    
-    #define COMBINE64(sgn,n) void combine##n(const Size2D &_size                                                \
-                                               FILL_LINES##n(FARG, sgn##64),                                \
-                                               sgn##64 * dstBase, ptrdiff_t dstStride)                      \
-{                                                                                                           \
-    internal::assertSupportedConfiguration();                                                               \
-    Size2D size(_size);                                                                                     \
-    if (CONTSRC##n                                                                                          \
-        dstStride == (ptrdiff_t)(size.width))                                                               \
-    {                                                                                                       \
-        size.width *= size.height;                                                                          \
-        size.height = 1;                                                                                    \
-    }                                                                                                       \
-    typedef internal::VecTraits<sgn##64, n>::vec64 vec64;                                                   \
-                                                                                                            \
-    for (size_t i = 0u; i < size.height; ++i)                                                               \
-    {                                                                                                       \
-        FILL_LINES##n(VROW, sgn##64)                                                                        \
-        sgn##64 * dst = internal::getRowPtr(dstBase, dstStride, i);                                         \
-        size_t sj = 0u, dj = 0u;                                                                            \
-                                                                                                            \
-        for (; sj < size.width; ++sj, dj += n)                                                              \
-        {                                                                                                   \
-            vec64 v_dst;                                                                                    \
-            FILL_LINES##n(VLD1, sgn##64)                                                                    \
-            vst##n##_##sgn##64(dst + dj, v_dst);                                                            \
-            /*FILL_LINES##n(SLD, sgn##64)*/                                                                 \
-        }                                                                                                   \
-    }                                                                                                       \
-}
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s32, f32, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src]]                              \n\t'
-             'vcvt.f32.s32 q1, q0                                    \n\t'
-             'vst1.32 {d2-d3}, [%[dst]]                              \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i),
-               [dst] 'r' (_dst + i)
-             : 'd0','d1','d2','d3'//,'d4','d5'
-         );
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src]]                              \n\t'
-             'vcvt.f32.s32 q1, q0                                    \n\t'
-             'vst1.32 {d2-d3}, [%[dst]]                              \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i + 4)
-             : 'd0','d1','d2','d3'//,'d4','d5'
-         );
-     }
-})
-#else
-CVT_FUNC(s32, f32, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         int32x4_t vline_s32 = vld1q_s32(_src + i);
-         float32x4_t vline_f32 = vcvtq_f32_s32(vline_s32);
-         vst1q_f32(_dst + i, vline_f32);
-    }
-    }
-    
-            for ( ; x < width; ++x)
-        {
-            // make extrapolation for the last elements
-            if (x + 1 >= width)
-            {
-                if (border == BORDER_MODE_CONSTANT)
-                {
-                    nextx[0] = borderValue;
-                    nextx[1] = borderValue;
-                    nextx[2] = borderValue;
-                }
-                else if (border == BORDER_MODE_REPLICATE)
-                {
-                    nextx[0] = srow0[x];
-                    nextx[1] = srow1[x];
-                    nextx[2] = srow2[x];
-                }
-            }
-            else
-            {
-                nextx[0] = srow0 ? srow0[x + 1] : borderValue;
-                nextx[1] =         srow1[x + 1]              ;
-                nextx[2] = srow2 ? srow2[x + 1] : borderValue;
-            }
-    }
-    
-                uint32x4_t vs4 = vpaddlq_u16(vpaddlq_u8(vs));
-            uint32x2_t vs2 = vadd_u32(vget_low_u32(vs4), vget_high_u32(vs4));
-    
-        v0k8 = vextq_s16(d8_15, d16_23, 7);
-    ak8 = vminq_s16(ak8, v0k8);
-    bk8 = vmaxq_s16(bk8, v0k8);
-    
-    void OneofGenerator::GenerateClearFunctionDeclaration(io::Printer* printer) {
-  printer->Print(
-      variables_,
-      '/**\n'
-      ' * Clears whatever value was set for the oneof '$name$'.\n'
-      ' **/\n'
-      'void $owning_message_class$_Clear$capitalized_name$OneOfCase($owning_message_class$ *message);\n');
-}
-    
-    void MapLiteTestUtil::ModifyMapFields(unittest::TestMapLite* message) {
-  MapTestUtilImpl::ModifyMapFields<unittest::MapEnumLite,
-                                   unittest::MAP_ENUM_FOO_LITE>(message);
-}
-    
-    TEST(StatusOr, TestPointerAssignmentStatusOk) {
-  const int kI = 0;
-  StatusOr<const int*> source(&kI);
-  StatusOr<const int*> target;
-  target = source;
-  EXPECT_EQ(source.status(), target.status());
-  EXPECT_EQ(source.ValueOrDie(), target.ValueOrDie());
-}
-    
-    TEST(TemplateUtilTest, TestTemplateTypeEquals) {
-  // Check that the TemplateTypeEquals works correctly.
-  bool value = false;
-    }
-    
-    int main(int argc, char** argv) {
-  if (argc != 4) {
-    printf('This script converts the MNIST dataset to the leveldb format used\n'
-           'by caffe to train a siamese network.\n'
-           'Usage:\n'
-           '    convert_mnist_data input_image_file input_label_file '
-           'output_db_file\n'
-           'The MNIST dataset could be downloaded at\n'
-           '    http://yann.lecun.com/exdb/mnist/\n'
-           'You should gunzip them after downloading.\n');
-  } else {
-    google::InitGoogleLogging(argv[0]);
-    convert_dataset(argv[1], argv[2], argv[3]);
+          // Copy constructor.
+  Message(const Message& msg) : ss_(new ::std::stringstream) {  // NOLINT
+    *ss_ << msg.GetString();
   }
-  return 0;
+    
+    TEST_P(FooTest, HasBlahBlah) {
+  ...
 }
-#else
-int main(int argc, char** argv) {
-  LOG(FATAL) << 'This example requires LevelDB; compile with USE_LEVELDB.';
-}
-#endif  // USE_LEVELDB
-
     
-      /** Will not return until the internal thread has exited. */
-  void StopInternalThread();
+      static void Print(const T& value, ::std::ostream* os) {
+    // Prints the address of the value.  We use reinterpret_cast here
+    // as static_cast doesn't compile when T is a function type.
+    *os << '@' << reinterpret_cast<const void*>(&value) << ' ';
+    }
     
-    #include <vector>
+    // INTERNAL IMPLEMENTATION - DO NOT USE IN USER CODE DIRECTLY.
+//
+// Expands to the name of the variable used to remember the names of
+// the registered tests in the given test case.
+# define GTEST_REGISTERED_TEST_NAMES_(TestCaseName) \
+  gtest_registered_test_names_##TestCaseName##_
     
-     protected:
-  // Helper functions that abstract away the column buffer and gemm arguments.
-  // The last argument in forward_cpu_gemm is so that we can skip the im2col if
-  // we just called weight_cpu_gemm with the same input.
-  void forward_cpu_gemm(const Dtype* input, const Dtype* weights,
-      Dtype* output, bool skip_im2col = false);
-  void forward_cpu_bias(Dtype* output, const Dtype* bias);
-  void backward_cpu_gemm(const Dtype* input, const Dtype* weights,
-      Dtype* output);
-  void weight_cpu_gemm(const Dtype* input, const Dtype* output, Dtype*
-      weights);
-  void backward_cpu_bias(Dtype* bias, const Dtype* input);
-    
-    
-    {  // extra temporarary variables is used to carry out sums/broadcasting
-  // using BLAS
-  Blob<Dtype> batch_sum_multiplier_;
-  Blob<Dtype> num_by_chans_;
-  Blob<Dtype> spatial_sum_multiplier_;
+    template <typename T1, typename T2, typename T3, typename T4, typename T5,
+    typename T6, typename T7, typename T8, typename T9, typename T10,
+    typename T11, typename T12, typename T13, typename T14, typename T15,
+    typename T16, typename T17, typename T18, typename T19, typename T20,
+    typename T21, typename T22>
+struct Types22 {
+  typedef T1 Head;
+  typedef Types21<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
+      T16, T17, T18, T19, T20, T21, T22> Tail;
 };
     
-    #include 'caffe/layers/loss_layer.hpp'
+    // This provides interface PrimeTable that determines whether a number is a
+// prime and determines a next prime number. This interface is used
+// in Google Test samples demonstrating use of parameterized tests.
+    
+      // Now, we have that n is odd and n >= 3.
+    
+      static int allocated() { return allocated_; }
+    
+    #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Utils.h'
     
     
-    {  bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnTensorDescriptor_t bottom_desc_;
-  cudnnTensorDescriptor_t top_desc_;
-  cudnnActivationDescriptor_t activ_desc_;
-};
-#endif
-    
-    #endif  // CAFFE_CUDNN_SOFTMAX_LAYER_HPP_
-
-    
-    #include 'gtest/gtest.h'
-    
-    #include 'modules/drivers/canbus/sensor_gflags.h'
-    
-    
-    {  double ret = x * OBJECT_WIDTH_RES;
-  return ret;
+template <class ConfigRecordType>
+void DataReader::InitFromConfig(const ConfigRecordType& /*config*/)
+{
+    RuntimeError('Init shouldn't be called, use constructor');
+    // not implemented, calls the underlying class instead
 }
     
-    unsigned int BaseMapMatrix::LoadBinary(unsigned char* buf) { return 0; }
-    
-    class SpeedLimitTest : public ::testing::Test {
- public:
-  virtual void SetUp() {
-    speed_limit_.Clear();
-    for (int i = 0; i < 100; ++i) {
-      std::pair<double, double> sp;
-      sp.first = i * 1.0;
-      sp.second = (i % 2 == 0) ? 5.0 : 10.0;
-      speed_limit_.AppendSpeedLimit(sp.first, sp.second);
+    template <typename TVector>
+inline void RandomShuffleMT(TVector& v, std::mt19937_64& rng)
+{
+    foreach_index(currentLocation, v)
+    {
+        // Pick a random location and swap with current one
+        const size_t randomLocation = RandMT(0, v.size(), rng);
+        std::swap(v[currentLocation], v[randomLocation]);
     }
-  }
+}
+    
+    #endif
+
+    
+         // GP Register cannot be greater than 31
+     assert(static_cast<uint32_t>(rt) < 32);
+     assert(static_cast<uint32_t>(ra) < 32);
+     assert(static_cast<uint32_t>(rb) < 32);
+     assert(static_cast<uint32_t>(bc) < 32);
+    
+    #include <folly/ScopeGuard.h>
+    
+    #include 'hphp/runtime/base/file.h'
+#include 'hphp/runtime/base/stream-wrapper.h'
+#include 'hphp/runtime/base/stream-wrapper-registry.h'
+    
+    /*
+ * Attempt to log an entry to the perf warning service.
+ *
+ * If StructuredLog::enabled() returns false or this event is discarded by the
+ * effective sample rate, nothing will be logged. If both of those checks pass,
+ * fillCols will be passed a StructuredLogEntry& to populate, which will then
+ * be logged. The column names 'event_name' and 'priority' are reserved and
+ * will be overwritten is fillCols() sets them.
+ *
+ * The effective sample rate is determined by Eval.PerfWarningSampleRate * rate
+ * (or kDefaultPerfWarningRate for the overloads that don't take a rate). If
+ * the effective sample rate is 0, all events will be discarded.
+ */
+template<typename F>
+void logPerfWarning(folly::StringPiece event, F fillCols);
+template<typename F>
+void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols);
+    
+        // Create Window Surface
+    VkSurfaceKHR surface;
+    VkResult err = glfwCreateWindowSurface(g_Instance, window, g_Allocator, &surface);
+    check_vk_result(err);
+    
+    namespace ImGuiFreeType
+{
+    // Hinting greatly impacts visuals (and glyph sizes).
+    // When disabled, FreeType generates blurrier glyphs, more or less matches the stb's output.
+    // The Default hinting mode usually looks good, but may distort glyphs in an unusual way.
+    // The Light hinting mode generates fuzzier glyphs but better matches Microsoft's rasterizer.
     }
     
-    TEST(TestPiecewiseLinearConstraint, add_second_derivative_boundary) {
-  PiecewiseLinearConstraint constraint(10, 0.1);
-  std::vector<uint32_t> index_list;
-  std::vector<double> lower_bound;
-  std::vector<double> upper_bound;
-  for (uint32_t i = 0; i < 10; ++i) {
-    index_list.push_back(i);
-    lower_bound.push_back(-4.0);
-    upper_bound.push_back(2.0);
-  }
+    //---- Don't implement some functions to reduce linkage requirements.
+//#define IMGUI_DISABLE_WIN32_DEFAULT_CLIPBOARD_FUNCTIONS   // [Win32] Don't implement default clipboard handler. Won't use and link with OpenClipboard/GetClipboardData/CloseClipboard etc.
+//#define IMGUI_DISABLE_WIN32_DEFAULT_IME_FUNCTIONS         // [Win32] Don't implement default IME handler. Won't use and link with ImmGetContext/ImmSetCompositionWindow.
+//#define IMGUI_DISABLE_WIN32_FUNCTIONS                     // [Win32] Won't use and link with any Win32 function.
+//#define IMGUI_DISABLE_FORMAT_STRING_FUNCTIONS             // Don't implement ImFormatString/ImFormatStringV so you can implement them yourself if you don't want to link with vsnprintf.
+//#define IMGUI_DISABLE_MATH_FUNCTIONS                      // Don't implement ImFabs/ImSqrt/ImPow/ImFmod/ImCos/ImSin/ImAcos/ImAtan2 wrapper so you can implement them yourself. Declare your prototypes in imconfig.h.
+//#define IMGUI_DISABLE_DEFAULT_ALLOCATORS                  // Don't implement default allocators calling malloc()/free() to avoid linking with them. You will need to call ImGui::SetAllocatorFunctions().
+    
+    IMGUI_IMPL_API bool     ImGui_ImplDX9_Init(IDirect3DDevice9* device);
+IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
+    
+    struct GLFWwindow;
+    
+    // Main code
+int main(int, char**)
+{
+    // Create application window
+    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T('ImGui Example'), NULL };
+    ::RegisterClassEx(&wc);
+    HWND hwnd = ::CreateWindow(wc.lpszClassName, _T('Dear ImGui DirectX12 Example'), WS_OVERLAPPEDWINDOW, 100, 100, 1280, 800, NULL, NULL, wc.hInstance, NULL);
     }
     
-    TEST_F(ObstacleClustersTest, ObstacleClusters) {
-  auto lane = PredictionMap::LaneById('l9');
-  double start_s = 99.0;
-  double length = 100.0;
+    
+    {    // 3. Show another simple window.
+    if (show_another_window)
+    {
+        ImGui::Begin('Another Window', &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+        ImGui::Text('Hello from another window!');
+        if (ImGui::Button('Close Me'))
+            show_another_window = false;
+        ImGui::End();
     }
-    
-    #include 'modules/canbus/vehicle/gem/gem_message_manager.h'
-    
-    
-    {  double ret = x * 0.001000;
-  return ret;
 }
