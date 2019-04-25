79a13aa58aca03b867a12fdfdff38eda114c5b61
SUNsung <weatherius@gmail.com>
2019-04-25 09:44:13 +0300

    Rewrite [14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py] File in: 2019-04-25 09:44:13

--- pyton/14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py
+++ pyton/14gtqrLR6WMNU1444ceFhhzF0614rzgWOjTaJLU14lg0.py
@@ -1,127 +1,147 @@
 
         
-          # Four initially random vectors for the special tokens: <PAD>, <UNK>, <X>, <Y>
-  special_embeddings = np.random.normal(0, 0.1, (4, dim))
-  embeddings = np.vstack((special_embeddings, embeddings))
-  embeddings = embeddings.astype(np.float32)
+            def _park_starting_at_spot(self, spot, vehicle):
+        '''Occupy starting at spot.spot_number to vehicle.spot_size.'''
+        pass
     
+        def mapper_sort(self, key, value):
+        '''Construct key to ensure proper sorting.
     
-# Note that with N small, (as it is 25 above), the finite size effects
-# will have pretty dramatic effects on the dynamics of the random RNN.
-# If you want more complex dynamics, you'll have to run the script a
-# lot, or increase N (or g).
-    
-    
-data = { 'train_truth': data_train_truth,
-         'valid_truth': data_valid_truth,
-         'train_data' : data_train_spiking,
-         'valid_data' : data_valid_spiking,
-         'train_percentage' : train_percentage,
-         'nreplications' : nreplications,
-         'dt' : FLAGS.dt,
-         'u_std' : FLAGS.u_std,
-         'max_firing_rate': FLAGS.max_firing_rate,
-         'train_inputs_u': train_inputs_u,
-         'valid_inputs_u': valid_inputs_u,
-         'train_outputs_u': train_outputs_u,
-         'valid_outputs_u': valid_outputs_u,
-         'conversion_factor' : FLAGS.max_firing_rate/(1.0/FLAGS.dt) }
-    
-      if FLAGS.mode == 'eval':
-    dataset = data_utils.LM1BDataset(FLAGS.input_data, vocab)
-    _EvalModel(dataset)
-  elif FLAGS.mode == 'sample':
-    _SampleModel(FLAGS.prefix, vocab)
-  elif FLAGS.mode == 'dump_emb':
-    _DumpEmb(vocab)
-  elif FLAGS.mode == 'dump_lstm_emb':
-    _DumpSentenceEmbedding(FLAGS.sentence, vocab)
-  else:
-    raise Exception('Mode not supported.')
-    
-      def assign_probs(self, sentences, test_data_name='wsc273'):
-    '''Return prediction accuracy using this LM for a test.'''
-    
-        prediction_correctness.append(
-        (scoring2['joint_prob'] > scoring1['joint_prob']) ==
-         scoring2['correctness'])
+        def get_people(self, ids):
+        results = []
+        for id in ids:
+            if id in self.people:
+                results.append(self.people[id])
+        return results
     
       Args:
-    hparams:  Hyperparameters for the MaskGAN.
-    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
-    is_training:  Whether the model is training.
-    reuse (Optional):  Whether to reuse the model.
+    model: The currently trained path-based model.
+    session: The current TensorFlow session.
+    instances: The current set instances.
+    labels: The current set labels.
+    set_name: The current set name (train/validation/test).
+    classes: The class label names.
+    
+      def call(self, h_tm1_bxn, u_bx1):
+    act_t_bxn = tf.matmul(h_tm1_bxn, self.Wh_nxn) + self.b_1xn + u_bx1 * self.Bu_1xn
+    h_t_bxn = tf.nn.tanh(act_t_bxn)
+    z_t = tf.nn.xw_plus_b(h_t_bxn, self.Wro_nxo, self.bro_o)
+    return z_t, h_t_bxn
+    
+      W,b = init_linear(in_size, out_size, do_bias=do_bias, alpha=alpha,
+                    normalized=normalized, name=name, collections=collections)
+    
+      def evaluate(self):
+    '''Evaluate the current ensemble.'''
+    # Attach word probabilities and correctness label to each substitution
+    ensembled_probs = sum(self.all_probs) / len(self.all_probs)
+    scorings = []
+    for i, sentence in enumerate(self.sentences):
+      correctness = self.labels[i]
+      word_probs = ensembled_probs[i, :len(sentence)]
+      joint_prob = np.prod(word_probs, dtype=np.float64)
+    
+    import tensorflow as tf
+    
+      Args:
+    hparams:  MaskGAN hyperparameters.
+    learning_rate:  tf.Variable scalar learning rate.
+    final_gen_objective:  Scalar final REINFORCE objective for the sequence.
+    averages_op:  ExponentialMovingAverage apply average op to
+      maintain the baseline.
+    global_step:  global_step tf.Variable.
+    
+            # init attention
+        attention = _init_attention(encoder_state)
+      else:
+        # construct attention
+        attention = attention_construct_fn(cell_output, attention_keys,
+                                           attention_values)
+        cell_output = attention
     
     
-def create_critic_train_op(hparams, critic_loss, global_step):
-  '''Create Discriminator train op.'''
-  with tf.name_scope('train_critic'):
-    critic_optimizer = tf.train.AdamOptimizer(hparams.critic_learning_rate)
-    output_vars = [
-        v for v in tf.trainable_variables() if v.op.name.startswith('critic')
-    ]
+@pytest.fixture(params=[(python_3, False),
+                        (python_3, True),
+                        (python_2, False)])
+def proc(request, spawnu, TIMEOUT):
+    container, instant_mode = request.param
+    proc = spawnu(*container)
+    proc.sendline(u'pip install /src')
+    assert proc.expect([TIMEOUT, u'Successfully installed'])
+    proc.sendline(init_bashrc.format(
+        u'--enable-experimental-instant-mode' if instant_mode else ''))
+    proc.sendline(u'bash')
+    if instant_mode:
+        assert proc.expect([TIMEOUT, u'instant mode ready: True'])
+    return proc
     
     
-def gen_encoder_seq2seq_nas(hparams):
-  '''Returns the NAS Variable name to MaskGAN Variable
-  dictionary mapping.  This is a highly restrictive function just for testing.
-  This is for the *unidirecitional* seq2seq_nas encoder.
+@pytest.mark.functional
+def test_with_confirmation(proc, TIMEOUT):
+    with_confirmation(proc, TIMEOUT)
     
-      with tf.variable_scope('dis', reuse=reuse):
-    cell_fwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
-        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
-    cell_bwd = tf.contrib.rnn.LayerNormBasicLSTMCell(
-        hparams.dis_rnn_size, forget_bias=1.0, reuse=reuse)
-    if FLAGS.zoneout_drop_prob > 0.0:
-      cell_fwd = zoneout.ZoneoutWrapper(
-          cell_fwd,
-          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
-          is_training=is_training)
-      cell_bwd = zoneout.ZoneoutWrapper(
-          cell_bwd,
-          zoneout_drop_prob=FLAGS.zoneout_drop_prob,
-          is_training=is_training)
-    
-    from .cifar import load_batch
-from ..utils.data_utils import get_file
-from .. import backend as K
-import numpy as np
-import os
-    
-        with custom_object_scope({'MSE_MAE_loss': MSE_MAE_loss}):
-        loss = MSE_MAE_loss(0.3)
-        inputs = keras.layers.Input((2,))
-        outputs = keras.layers.Dense(1, name='model_output')(inputs)
-        model = keras.models.Model(inputs, outputs)
-        model.compile(optimizer='sgd', loss={'model_output': loss})
-        model.fit(np.random.rand(256, 2), np.random.rand(256, 1))
-        model.save(model_filename)
-    
-    batch_size = 128
-num_classes = 10
-epochs = 20
-    
-        # Returns
-        Tensor with one scalar loss entry per sample.
-    '''
-    def _logcosh(x):
-        return x + K.softplus(-2. * x) - K.log(2.)
-    return K.mean(_logcosh(y_pred - y_true), axis=-1)
-    
-    *TL;DR80
-Delays the eval of an expr until its value is needed and avoids repeated evals.
-'''
-    
-    ### OUTPUT ###
-# [{'objectb': 'b-value'}, {'default': 'default'}, {'objecta': 'a-value'}]
+        new_command = get_new_command(Command('apt update', match_output))
+    assert new_command == 'apt list --upgradable'
 
     
-        def _send_diagnostics_request(self):
-        return 'send diagnostic request'
+    match_output = '''
+Listing... Done
+heroku/stable 6.15.2-1 amd64 [upgradable from: 6.14.43-1]
+resolvconf/zesty-updates,zesty-updates 1.79ubuntu4.1 all [upgradable from: 1.79ubuntu4]
+squashfs-tools/zesty-updates 1:4.3-3ubuntu2.17.04.1 amd64 [upgradable from: 1:4.3-3ubuntu2]
+unattended-upgrades/zesty-updates,zesty-updates 0.93.1ubuntu2.4 all [upgradable from: 0.93.1ubuntu2.3]
+'''
     
-    '''
-*What is this pattern about?
-The Decorator pattern is used to dynamically add a new feature to an
-object without changing its implementation. It differs from
-inheritance because the new feature is added only to that particular
-object, not to the entire subclass.
+    dynamodb                                 | dynamodbstreams
+ec2                                      | ecr
+'''
+    
+    
+@pytest.mark.parametrize('script, output', [
+    ('brew link sshfs', output),
+    ('cat output', output),
+    ('brew install sshfs', '')])
+def test_not_match(script, output):
+    command = Command(script, output)
+    assert not match(command)
+    
+            raise NotImplementedError
+    
+    
+    
+# I have written my code naively same as definition of primitive root
+# however every time I run this program, memory exceeded...
+# so I used 4.80 Algorithm in Handbook of Applied Cryptography(CRC Press, ISBN : 0-8493-8523-7, October 1996)
+# and it seems to run nicely!
+def primitiveRoot(p_val):
+    print('Generating primitive root of p')
+    while True:
+        g = random.randrange(3,p_val)
+        if pow(g, 2, p_val) == 1:
+            continue
+        if pow(g, p_val, p_val) == 1:
+            continue
+        return g
+    
+        return clean
+    
+            while self.values[new_key] is not None and self.values[new_key] != key:
+            new_key = self.__hash_double_function(key, data, i) if \
+                self.balanced_factor() >= self.lim_charge else None
+            if new_key is None: break 
+            else: i += 1
+    
+    The problem is :
+Given two strings A and B. Find the minimum number of operations to string B such that A = B. The permitted operations are removal,  insertion, and substitution.
+'''
+from __future__ import print_function
+    
+    * @author chinmoy159
+* @version 1.0 dated 10/08/2017
+'''
+    
+    min_length = 8
+max_length = 16
+password = ''.join(random.choice(chars) for x in range(random.randint(min_length, max_length)))
+print('Password: ' + password)
+print('[ If you are thinking of using this passsword, You better save it. ]')
