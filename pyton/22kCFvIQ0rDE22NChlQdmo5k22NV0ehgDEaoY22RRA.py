cc7c5e9362cc6ebcf6022709d1c51733c862af39
SUNsung <weatherius@gmail.com>
2019-04-25 22:41:52 +0300

    Rewrite [22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py] File in: 2019-04-25 22:41:52

--- pyton/22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py
+++ pyton/22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py
@@ -1,55 +1,202 @@
 
         
-        errors = []
-title_links = []
-previous_links = []
-anchor_re = re.compile(anchor + '\s(.+)')
-section_title_re = re.compile('\*\s\[(.*)\]')
-link_re = re.compile('\[(.+)\]\((http.*)\)')
+        
+def main():
+    if len(sys.argv) < 2:
+        print('No file passed (file should contain Markdown table syntax)')
+        sys.exit(1)
+    check_format(sys.argv[1])
+    if len(errors) > 0:
+        for err in errors:
+            print(err)
+        sys.exit(1)
     
-                    # Update the previous file
-                if _filename:
-                    with open(_filename, 'w') as _file:
-                        _file.write(_contents)
+    DEPLABEL_TO_ID = {label: lid for lid, label in enumerate(DEPLABELS)}
     
-    # Scrapy version
-import pkgutil
-__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
-version_info = tuple(int(v) if v.isdigit() else v
-                     for v in __version__.split('.'))
-del pkgutil
+      Returns:
+    In the equation, y = x W + b, returns the tensorflow op that yields y.
+  '''
+  in_size = int(x.get_shape()[1]) # from Dimension(10) -> 10
+  stddev = alpha/np.sqrt(float(in_size))
+  mat_init = tf.random_normal_initializer(0.0, stddev)
+  wname = (name + '/W') if name else '/W'
     
-        def short_desc(self):
-        return 'Run quick benchmark test'
+    FLAGS = tf.flags.FLAGS
     
-        def _needs_backout(self, spider):
-        slot = self.slot
-        return not self.running \
-            or slot.closing \
-            or self.downloader.needs_backout() \
-            or self.scraper.slot.needs_backout()
+    EOS_INDEX = 88892
     
-            print('One element')
-        assert_equal(insertion_sort.sort([5]), [5])
+    '''PTB data loader and helpers.'''
+    
+      Returns:
+    Tuple of the (sequence, logits, log_probs) of the Generator.   Sequence
+      and logits have shape [batch_size, sequence_length, vocab_size].  The
+      log_probs will have shape [batch_size, sequence_length].  Log_probs
+      corresponds to the log probability of selecting the words.
+  '''
+  if FLAGS.generator_model == 'rnn':
+    (sequence, logits, log_probs, initial_state, final_state) = rnn.generator(
+        hparams,
+        inputs,
+        targets,
+        present,
+        is_training=is_training,
+        is_validating=is_validating,
+        reuse=reuse)
+  elif FLAGS.generator_model == 'rnn_zaremba':
+    (sequence, logits, log_probs, initial_state,
+     final_state) = rnn_zaremba.generator(
+         hparams,
+         inputs,
+         targets,
+         present,
+         is_training=is_training,
+         is_validating=is_validating,
+         reuse=reuse)
+  elif FLAGS.generator_model == 'seq2seq':
+    (sequence, logits, log_probs, initial_state,
+     final_state) = seq2seq.generator(
+         hparams,
+         inputs,
+         targets,
+         present,
+         is_training=is_training,
+         is_validating=is_validating,
+         reuse=reuse)
+  elif FLAGS.generator_model == 'seq2seq_zaremba':
+    (sequence, logits, log_probs, initial_state,
+     final_state) = seq2seq_zaremba.generator(
+         hparams,
+         inputs,
+         targets,
+         present,
+         is_training=is_training,
+         is_validating=is_validating,
+         reuse=reuse)
+  elif FLAGS.generator_model == 'rnn_nas':
+    (sequence, logits, log_probs, initial_state,
+     final_state) = rnn_nas.generator(
+         hparams,
+         inputs,
+         targets,
+         present,
+         is_training=is_training,
+         is_validating=is_validating,
+         reuse=reuse)
+  elif FLAGS.generator_model == 'seq2seq_nas':
+    (sequence, logits, log_probs, initial_state,
+     final_state) = seq2seq_nas.generator(
+         hparams,
+         inputs,
+         targets,
+         present,
+         is_training=is_training,
+         is_validating=is_validating,
+         reuse=reuse)
+  elif FLAGS.generator_model == 'seq2seq_vd':
+    (sequence, logits, log_probs, initial_state, final_state,
+     encoder_states) = seq2seq_vd.generator(
+         hparams,
+         inputs,
+         targets,
+         present,
+         is_training=is_training,
+         is_validating=is_validating,
+         reuse=reuse)
+  else:
+    raise NotImplementedError
+  return (sequence, logits, log_probs, initial_state, final_state,
+          encoder_states)
+    
+      if FLAGS.dis_share_embedding:
+    assert hparams.dis_rnn_size == hparams.gen_rnn_size, (
+        'If you wish to share Discriminator/Generator embeddings, they must be'
+        ' same dimension.')
+    with tf.variable_scope('gen/rnn', reuse=True):
+      embedding = tf.get_variable('embedding',
+                                  [FLAGS.vocab_size, hparams.gen_rnn_size])
+    
+            # Identify targets that need to be added or updated on AWS
+        targets_to_put = self._targets_to_put()
+        if targets_to_put:
+            self.rule.put_targets(targets_to_put)
+    
+        try:
+        client.login(username=module.params['ipa_user'],
+                     password=module.params['ipa_pass'])
+        changed, hbacrule = ensure(module, client)
+        module.exit_json(changed=changed, hbacrule=hbacrule)
+    except Exception as e:
+        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
+    
+        # Build the common request body
+    body = dict()
+    for k in ('component', 'version', 'hosts'):
+        v = module.params[k]
+        if v is not None:
+            body[k] = v
+    
+    EXAMPLES = '''
+- name: Enable ido-pgsql feature
+  icinga2_feature:
+    name: ido-pgsql
+    state: present
     
     
-class PriorityQueue(object):
+def get_github_url(app, view, path):
+    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
+    return (
+        github_fmt.format(app.config.edit_on_github_project, view,
+                          app.config.edit_on_github_branch,
+                          app.config.edit_on_github_src_path, path))
     
-        >>> timeparse('1m24s')
-    84
-    >>> timeparse('1.2 minutes')
-    72
-    >>> timeparse('1.2 seconds')
-    1.2
-    '''
-    match = re.match(r'\s*' + TIMEFORMAT + r'\s*$', sval, re.I)
-    if not match or not match.group(0).strip():
-        return
+        extract_tags = textrank
+
     
-        def test_start_failure(self):
-        with mock.patch('compose.service.Service.start_container', crash):
-            with pytest.raises(Crash):
-                self.project.up(strategy=ConvergenceStrategy.always)
+    MIN_FLOAT = -3.14e100
+    
+    seg_list = jieba.cut('我来到北京清华大学', cut_all=True)
+print('Full Mode: ' + '/ '.join(seg_list))  # 全模式
+    
+    import jieba
+import jieba.analyse
+from optparse import OptionParser
     
     
-class VerboseProxyTestCase(unittest.TestCase):
+    
+    
+# copied from fix_tuple_params.py
+def is_docstring(stmt):
+    return isinstance(stmt, pytree.Node) and stmt.children[0].type == token.STRING
+    
+    
+@gen.engine
+def run_tests():
+    url = options.url + '/getCaseCount'
+    control_ws = yield websocket_connect(url, None)
+    num_tests = int((yield control_ws.read_message()))
+    logging.info('running %d cases', num_tests)
+    msg = yield control_ws.read_message()
+    assert msg is None
+    
+    
+class LeakTest(unittest.TestCase):
+    def setUp(self):
+        # Trigger a cleanup of the mapping so we start with a clean slate.
+        AsyncIOLoop().close()
+        # If we don't clean up after ourselves other tests may fail on
+        # py34.
+        self.orig_policy = asyncio.get_event_loop_policy()
+        asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
+    
+        @gen.coroutine
+    def get(self):
+        if self.get_argument('openid.mode', None):
+            user = yield self.get_authenticated_user(
+                http_client=self.settings['http_client']
+            )
+            if user is None:
+                raise Exception('user is None')
+            self.finish(user)
+            return
+        res = self.authenticate_redirect()
+        assert res is None
