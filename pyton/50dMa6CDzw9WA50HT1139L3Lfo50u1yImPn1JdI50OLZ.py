8015e5ad7916b8db1d09fd84dbfc86adbb5ed199
SUNsung <weatherius@gmail.com>
2019-04-26 22:11:38 +0300

    Rewrite [50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py] File in: 2019-04-26 22:11:37

--- pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
+++ pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
@@ -1,318 +1,137 @@
 
         
         
-def login_required(view):
-    '''View decorator that redirects anonymous users to the login page.'''
-    @functools.wraps(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirect(url_for('auth.login'))
+class CallCenter(object):
     
+        def approve_friend_request(self, friend_id):
+        pass
     
-@pytest.mark.parametrize('path', (
-    '/create',
-    '/1/update',
-    '/1/delete',
-))
-def test_login_required(client, path):
-    response = client.post(path)
-    assert response.headers['Location'] == 'http://localhost/auth/login'
+        proc.sendline(u'fuck')
+    assert proc.expect([TIMEOUT, u'echo test'])
+    assert proc.expect([TIMEOUT, u'test'])
     
-        def make_setup_state(self, app, options, first_registration=False):
-        '''Creates an instance of :meth:`~flask.blueprints.BlueprintSetupState`
-        object that is later passed to the register callback functions.
-        Subclasses can override this to return a subclass of the setup state.
-        '''
-        return BlueprintSetupState(self, app, options, first_registration)
-    
-    
-def explain_ignored_app_run():
-    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
-        warn(Warning('Silently ignoring app.run() because the '
-                     'application is run from the flask command line '
-                     'executable.  Consider putting app.run() behind an '
-                     'if __name__ == '__main__' guard to silence this '
-                     'warning.'), stacklevel=3)
-
-    
-    
-def _lookup_req_object(name):
-    top = _request_ctx_stack.top
-    if top is None:
-        raise RuntimeError(_request_ctx_err_msg)
-    return getattr(top, name)
-    
-    signals_available = False
-try:
-    from blinker import Namespace
-    signals_available = True
-except ImportError:
-    class Namespace(object):
-        def signal(self, name, doc=None):
-            return _FakeSignal(name, doc)
-    
-        n_samples = 2000
-    list_n_features = np.linspace(500, 3000, 5).astype(np.int)
-    lasso_results, lars_lasso_results = compute_bench(alpha, [n_samples],
-                                           list_n_features, precompute=False)
-    plt.subplot(212)
-    plt.plot(list_n_features, lasso_results, 'b-', label='Lasso')
-    plt.plot(list_n_features, lars_lasso_results, 'r-', label='LassoLars')
-    plt.title('%d samples, alpha=%s' % (n_samples, alpha))
-    plt.legend(loc='upper left')
-    plt.xlabel('number of features')
-    plt.ylabel('Time (s)')
-    plt.axis('tight')
-    plt.show()
-
-    
-    fixed_batch_size_comparison(X)
-variable_batch_size_comparison(X)
-plt.show()
-
-    
-    '''
-# Author: Olivier Grisel <olivier.grisel@ensta.org>
-# License: Simplified BSD
-    
-    fit_data = data[np.argsort(model.row_labels_)]
-fit_data = fit_data[:, np.argsort(model.column_labels_)]
-    
-    '''
-print(__doc__)
-    
-    We generate data from three groups of waveforms. Two of the waveforms
-(waveform 1 and waveform 2) are proportional one to the other. The cosine
-distance is invariant to a scaling of the data, as a result, it cannot
-distinguish these two waveforms. Thus even with no noise, clustering
-using this distance will not separate out waveform 1 and 2.
-    
-    agglo.fit(X)
-X_reduced = agglo.transform(X)
-    
-        plt.figure(figsize=(6, 4))
-    for i in range(X_red.shape[0]):
-        plt.text(X_red[i, 0], X_red[i, 1], str(y[i]),
-                 color=plt.cm.nipy_spectral(labels[i] / 10.),
-                 fontdict={'weight': 'bold', 'size': 9})
-    
-    plt.figure(figsize=(12, 12))
-    
-    # Number of run (with randomly generated dataset) for each strategy so as
-# to be able to compute an estimate of the standard deviation
-n_runs = 5
-    
-    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-ms.fit(X)
-labels = ms.labels_
-cluster_centers = ms.cluster_centers_
-    
-        stream_types = [  #this is just a sample. Will make it in prepare()
-        # {'id': '1080'},
-        # {'id': '720'},
-        # {'id': '360'},
-        # {'id': '288'},
-        # {'id': '190'},
-        # {'id': '180'},
-        
-    ]
-    
-    #----------------------------------------------------------------------
-#helper
-#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
-def dictify(r,root=True):
-    if root:
-        return {r.tag : dictify(r, False)}
-    d=copy(r.attrib)
-    if r.text:
-        d['_text']=r.text
-    for x in r.findall('./*'):
-        if x.tag not in d:
-            d[x.tag]=[]
-        d[x.tag].append(dictify(x,False))
-    return d
-    
-            for p in js_path:
-            if 'mtool' in p or 'mcore' in p:
-                js_text = get_content(p)
-                hit = re.search(r'\(\'(.+?)\',(\d+),(\d+),\'(.+?)\'\.split\(\'\|\'\),\d+,\{\}\)', js_text)
-    
-        def get_sign(self, media_url):
-        media_host = parse.urlparse(media_url).netloc
-        ran = random.randint(0, 9999999)
-        ssl_callback = get_content('http://{}/ssl/ssl.shtml?r={}'.format(media_host, ran)).split(',')
-        ssl_ts = int(datetime.datetime.strptime(ssl_callback[1], '%b %d %H:%M:%S %Y').timestamp() + int(ssl_callback[0]))
-        sign_this = self.__class__.ENCRYPT_SALT + parse.urlparse(media_url).path + str(ssl_ts)
-        arg_h = base64.b64encode(hashlib.md5(bytes(sign_this, 'utf-8')).digest(), altchars=b'-_')
-        return ssl_ts, arg_h.decode('utf-8').strip('=')
-    
-    def shorten_title(title):
-    m1 = re.search('[[0-9]*]', title)
-    m2 = re.search(''.*'', title)
-    if m1:
-        title = m1.group(0)
-    if m2:
-        title = ' '.join((title, m2.group(0)))   
-    return title[:50] + ' [...]'    
-    
-            self._hass.bus.fire(EVENT_NAME, {
-            EVENT_DATA_NAME: self.name,
-            EVENT_DATA_ADDRESS: self.address,
-            EVENT_DATA_QUEUED_TIME: time_diff,
-            EVENT_DATA_TYPE: hass_click_type
-        })
-    
-    
-def get_scanner(hass, config):
-    '''Validate the configuration and return an Actiontec scanner.'''
-    scanner = ActiontecDeviceScanner(config[DOMAIN])
-    return scanner if scanner.success_init else None
-    
-            self.last_results = {}
-        response = self._make_request()
-        if response.status_code != 200:
-            _LOGGER.error(
-                'Got HTTP status code %d when getting device list',
-                response.status_code)
-            return False
-        try:
-            data = response.json()
-            result = data['responses'][0]
-            devices = result['output']['devices']
-            for device in devices:
-                macs = device['knownMACAddresses']
-                if not macs:
-                    _LOGGER.warning(
-                        'Skipping device without known MAC address')
-                    continue
-                mac = macs[-1]
-                connections = device['connections']
-                if not connections:
-                    _LOGGER.debug('Device %s is not connected', mac)
-                    continue
-    
-        def __init__(self, config):
-        '''Initialize the scanner.'''
-        self.host = config[CONF_HOST]
-        self.username = config[CONF_USERNAME]
-        self.password = config[CONF_PASSWORD]
-        self.last_results = {}
-    
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_COMMAND): cv.string,
-    vol.Optional(CONF_NAME): cv.string,
-})
-    
-        _LOGGER.error(
-        'Error %s : %s (Code %s)', response.status_code, error_message,
-        error_code)
-
-    
-    PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_FILENAME): cv.string,
-    vol.Optional(CONF_TIMESTAMP, default=False): cv.boolean,
-})
-    
-    For more details about this platform, please refer to the documentation at
-https://home-assistant.io/components/notify.free_mobile/
-'''
-import logging
-    
-        def send_message(self, message='', **kwargs):
-        '''Send a message to Lannouncer.'''
-        data = kwargs.get(ATTR_DATA)
-        if data is not None and ATTR_METHOD in data:
-            method = data.get(ATTR_METHOD)
-        else:
-            method = ATTR_METHOD_DEFAULT
-    
-        def send_message(self, message='', **kwargs):
-        '''Send a message to a user.'''
-        _LOGGER.debug('Sending to: %s, %s', self._recipient, str(self._device))
-        data = {
-            'secret': self._secret,
-            'to': self._recipient,
-            'device': self._device,
-            'payload': message,
-        }
-    
-    
-def im_detect_keypoints_hflip(model, im, target_scale, target_max_size, boxes):
-    '''Computes keypoint predictions on the horizontally flipped image.
-    Function signature is the same as for im_detect_keypoints_aug.
-    '''
-    # Compute keypoints for the flipped image
-    im_hf = im[:, ::-1, :]
-    boxes_hf = box_utils.flip_boxes(boxes, im.shape[1])
-    
-    # mapping coco categories to cityscapes (our converted json) id
-# cityscapes
-# INFO roidb.py: 220: 1       bicycle: 7286
-# INFO roidb.py: 220: 2           car: 53684
-# INFO roidb.py: 220: 3        person: 35704
-# INFO roidb.py: 220: 4         train: 336
-# INFO roidb.py: 220: 5         truck: 964
-# INFO roidb.py: 220: 6    motorcycle: 1468
-# INFO roidb.py: 220: 7           bus: 758
-# INFO roidb.py: 220: 8         rider: 3504
-    
-    
-def add_ResNet50_conv5_body(model):
-    return add_ResNet_convX_body(model, (3, 4, 6, 3))
-    
-    The Fast R-CNN head produces a feature representation of the RoI for the purpose
-of bounding box classification and regression. The box output module converts
-the feature representation into classification and regression predictions.
+    no_match_output = '''
+Listing... Done
 '''
     
-    '''Various network 'heads' for predicting keypoints in Mask R-CNN.
+    
+no_such_subcommand_old = '''No such subcommand
     
     
-def get_retinanet_bias_init(model):
-    '''Initialize the biases for the conv ops that predict class probabilities.
-    Initialization is performed such that at the start of training, all
-    locations are predicted to be background with high probability
-    (e.g., ~0.99 = 1 - cfg.RETINANET.PRIOR_PROB). See the Focal Loss paper for
-    details.
+@parametrize_extensions
+@parametrize_filename
+@parametrize_script
+def test_get_new_command(ext, tar_error, filename, unquoted, quoted, script, fixed):
+    tar_error(unquoted.format(ext))
+    assert (get_new_command(Command(script.format(filename.format(ext)), ''))
+            == fixed.format(dir=quoted.format(''), filename=filename.format(ext)))
+
+    
+    
+def test_objective_shapes_2d():
+    y_a = K.variable(np.random.random((6, 7)))
+    y_b = K.variable(np.random.random((6, 7)))
+    for obj in allobj:
+        objective_output = obj(y_a, y_b)
+        assert K.eval(objective_output).shape == (6,)
+    
+    print('Evaluate IRNN...')
+model = Sequential()
+model.add(SimpleRNN(hidden_units,
+                    kernel_initializer=initializers.RandomNormal(stddev=0.001),
+                    recurrent_initializer=initializers.Identity(gain=1.0),
+                    activation='relu',
+                    input_shape=x_train.shape[1:]))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+rmsprop = RMSprop(lr=learning_rate)
+model.compile(loss='categorical_crossentropy',
+              optimizer=rmsprop,
+              metrics=['accuracy'])
+    
+    print('Building model...')
+model = Sequential()
+model.add(Dense(512, input_shape=(max_words,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+    
+        # Raises
+        ValueError if unknown identifier.
     '''
-    prior_prob = cfg.RETINANET.PRIOR_PROB
-    scales_per_octave = cfg.RETINANET.SCALES_PER_OCTAVE
-    aspect_ratios = len(cfg.RETINANET.ASPECT_RATIOS)
-    if cfg.RETINANET.SOFTMAX:
-        # Multiclass softmax case
-        bias = np.zeros((model.num_classes, 1), dtype=np.float32)
-        bias[0] = np.log(
-            (model.num_classes - 1) * (1 - prior_prob) / (prior_prob)
-        )
-        bias = np.vstack(
-            [bias for _ in range(scales_per_octave * aspect_ratios)]
-        )
-        bias_init = (
-            'GivenTensorFill', {
-                'values': bias.astype(dtype=np.float32)
-            }
-        )
+    if identifier is None:
+        return None
+    if isinstance(identifier, six.string_types):
+        identifier = str(identifier)
+        return deserialize(identifier)
+    if isinstance(identifier, dict):
+        return deserialize(identifier)
+    elif callable(identifier):
+        return identifier
     else:
-        # Per-class sigmoid (binary classification) case
-        bias_init = (
-            'ConstantFill', {
-                'value': -np.log((1 - prior_prob) / prior_prob)
-            }
-        )
-    return bias_init
+        raise ValueError('Could not interpret '
+                         'loss function identifier:', identifier)
+
     
-    from detectron.core.config import cfg
-import detectron.modeling.FPN as fpn
-import detectron.roi_data.keypoint_rcnn as keypoint_rcnn_roi_data
-import detectron.roi_data.mask_rcnn as mask_rcnn_roi_data
-import detectron.utils.blob as blob_utils
-import detectron.utils.boxes as box_utils
+            # 更新聚类中心
+        log.info(centers)
+        for i in range(k):
+            data_i = data[ret[:, 0] == i]  # 标签为 i 的样本
+            centers[i, :] = np.mean(data_i, axis=0)  # 按类别过滤样本
     
-    # Example usage:
-# data_loader_benchmark.par \
-#   NUM_GPUS 2 \
-#   TRAIN.DATASETS '('voc_2007_trainval',)' \
-#   TRAIN.PROPOSAL_FILES /path/to/voc_2007_trainval/proposals.pkl \
-#   DATA_LOADER.NUM_THREADS 4 \
-#   DATA_LOADER.MINIBATCH_QUEUE_SIZE 64 \
-#   DATA_LOADER.BLOBS_QUEUE_CAPACITY 8
+    
+# TODO(huay): char_cnn_embedding
+def char_cnn_embedding(x, c_embed_size=8, share_cnn_weights=True, name='char_cnn_embedding', reuse=None):
+    '''
+    In:  [N, max_n_word, max_n_char]
+    Out: [N, max_n_word, c_embed_size]
+    
+            H = act_fn(tf.nn.conv2d(x, W, strides=strides, padding=padding) + b)
+        T = sigmoid(tf.nn.conv2d(x, W_T, strides=strides, padding=padding) + b_T)
+        o = tf.multiply(H, T) + tf.multiply(x, (1. - T))
+    return o
+    
+        Args:
+        h: context encoding     shape: [N, T, d]
+        u: question encoding    shape: [N, J, d]
+        T(int): context length
+        J(int): question length
+        d(int): features size
+        name(str):
+        reuse(bool):
+    
+    def clean_pdf_link(link):
+    if 'arxiv' in link:
+        link = link.replace('abs', 'pdf')   
+        if not(link.endswith('.pdf')):
+            link = '.'.join((link, 'pdf'))
+    
+            return -1, char_len
+    
+    
+class Latin1Prober(CharSetProber):
+    def __init__(self):
+        super(Latin1Prober, self).__init__()
+        self._last_char_class = None
+        self._freq_counter = None
+        self.reset()
+    
+        def __init__(self, lang_filter=None):
+        super(MultiByteCharSetProber, self).__init__(lang_filter=lang_filter)
+        self.distribution_analyzer = None
+        self.coding_sm = None
+        self._last_char = [0, 0]
+    
+        @property
+    def charset_name(self):
+        if self._name_prober:
+            return self._name_prober.charset_name
+        else:
+            return self._model['charset_name']
+    
+            # Default to ASCII if it is all we've seen so far
+        elif self._input_state == InputState.PURE_ASCII:
+            self.result = {'encoding': 'ascii',
+                           'confidence': 1.0,
+                           'language': ''}
