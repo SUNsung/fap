1f7a99fbe9aea088fddc9b588bffc7263d3377a8
SUNsung <weatherius@gmail.com>
2019-04-27 02:13:04 +0300

    Rewrite [55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py] File in: 2019-04-27 02:13:04

--- pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
+++ pyton/55m.9i.FurH4o55bSeC03sbEvU55.ADtsVjUXIM55Ofr.py
@@ -1,167 +1,52 @@
 
         
-        
+        filenames = {
+    'bin': 'youtube-dl',
+    'exe': 'youtube-dl.exe',
+    'tar': 'youtube-dl-%s.tar.gz' % version}
+build_dir = os.path.join('..', '..', 'build', version)
+for key, filename in filenames.items():
+    url = 'https://yt-dl.org/downloads/%s/%s' % (version, filename)
+    fn = os.path.join(build_dir, filename)
+    with open(fn, 'rb') as f:
+        data = f.read()
+    if not data:
+        raise ValueError('File %s is empty!' % fn)
+    sha256sum = hashlib.sha256(data).hexdigest()
+    new_version[key] = (url, sha256sum)
+    
+    
 def main():
-    if len(sys.argv) < 2:
-        print('No file passed (file should contain Markdown table syntax)')
-        sys.exit(1)
-    check_format(sys.argv[1])
-    if len(errors) > 0:
-        for err in errors:
-            print(err)
-        sys.exit(1)
+    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
+        template = tmplf.read()
     
-          # self.path_embeddings is of shape
-      # [batch_size, max_path_per_instance, output_dim]
-      # We need to multiply it by path counts
-      # ([batch_size, max_path_per_instance]).
-      # Start by duplicating path_counts along the output_dim axis.
-      self.path_freq = tf.tile(tf.expand_dims(self.path_counts, -1),
-                               [1, 1, self.path_dim])
+    sys.path.insert(0, dirn(dirn((os.path.abspath(__file__)))))
+import youtube_dl
     
-        self.sequence_lengths = tf.reshape(self.seq_lengths, [-1])
+    # Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named 'default.css' will overwrite the builtin 'default.css'.
+html_static_path = ['_static']
     
-        plt.subplot(313)
-    plt.imshow(np.dot(all_data_zm_chxtc.T, W_chxp).T - all_data_pca_pxtc)
-    plt.colorbar()
+    from youtube_dl.jsinterp import JSInterpreter
     
     
-def init_linear(in_size, out_size, do_bias=True, mat_init_value=None,
-                bias_init_value=None, alpha=1.0, identity_if_possible=False,
-                normalized=False, name=None, collections=None, trainable=True):
-  '''Linear (affine) transformation, y = x W + b, for a variety of
-  configurations.
-    
-      def _load_random_shard(self):
-    '''Randomly select a file and read it.'''
-    return self._load_shard(random.choice(self._all_shards))
-    
-          if (vocab.id_to_word(samples[0]) == '</S>' or
-          len(sent) > FLAGS.max_sample_words):
-        break
-    
-      Args:
-    sentences: a list of sentences, each sentence is a list of str token.
-    batch_size: batch size
-    num_timesteps: number of backprop step
-    
-      # The unique ngrams in the training set.
-  unique_ngrams_in_train = 0.
-    
-        for group in opt_parser.option_groups:
-        for option in group.option_list:
-            long_option = option.get_opt_string().strip('-')
-            complete_cmd = ['complete', '--command', 'youtube-dl', '--long-option', long_option]
-            if option._short_opts:
-                complete_cmd += ['--short-option', option._short_opts[0].strip('-')]
-            if option.help != optparse.SUPPRESS_HELP:
-                complete_cmd += ['--description', option.help]
-            complete_cmd.extend(EXTRA_ARGS.get(long_option, []))
-            commands.append(shell_quote(complete_cmd))
-    
-    print('Enter the PKCS1 private key, followed by a blank line:')
-privkey = b''
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if line == '':
-        break
-    privkey += line.encode('ascii') + b'\n'
-privkey = rsa.PrivateKey.load_pkcs1(privkey)
-    
-        flags = [opt.get_opt_string() for opt in opts]
-    
-    from test.helper import try_rm
-    
-        def test_call(self):
-        jsi = JSInterpreter('''
-        function x() { return 2; }
-        function y(a) { return x() + a; }
-        function z() { return y(3); }
-        ''')
-        self.assertEqual(jsi.call_function('z'), 5)
-    
-        def test_allsubtitles(self):
-        self.DL.expect_warning('Automatic Captions not supported by this server')
-        self.DL.params['writesubtitles'] = True
-        self.DL.params['allsubtitles'] = True
-        subtitles = self.getSubtitles()
-        self.assertEqual(set(subtitles.keys()), set(['heb']))
-        self.assertEqual(md5(subtitles['heb']), 'e758c5d7cb982f6bef14f377ec7a3920')
-    
-    try:
-    from io import BytesIO
-except ImportError:
-    from cStringIO import StringIO as BytesIO
-try:
-    from google.appengine.api import urlfetch
-    from google.appengine.runtime import apiproxy_errors
-except ImportError:
-    urlfetch = None
-try:
-    import sae
-except ImportError:
-    sae = None
-try:
-    import bae.core.wsgi
-except ImportError:
-    bae = None
-try:
-    import socket
-    import select
-except ImportError:
-    socket = None
-try:
-    import OpenSSL
-except ImportError:
-    OpenSSL = None
-    
-    
-    
-    
-    def reportError(self, e):
-        '''Report a recognition problem.
-            
-        This method sets errorRecovery to indicate the parser is recovering
-        not parsing.  Once in recovery mode, no errors are generated.
-        To get out of recovery mode, the parser must successfully match
-        a token (after a resync).  So it will go:
-    
-    # begin[licence]
-#
-# [The 'BSD licence']
-# Copyright (c) 2005-2008 Terence Parr
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# end[licence]
-    
-    
-def display_temp(hass: HomeAssistant, temperature: float, unit: str,
-                 precision: float) -> float:
-    '''Convert temperature into preferred units/precision for display.'''
-    temperature_unit = unit
-    ha_unit = hass.config.units.temperature_unit
+from youtube_dl.extractor import (
+    YoutubeIE,
+    DailymotionIE,
+    TEDIE,
+    VimeoIE,
+    WallaIE,
+    CeskaTelevizeIE,
+    LyndaIE,
+    NPOIE,
+    ComedyCentralIE,
+    NRKTVIE,
+    RaiPlayIE,
+    VikiIE,
+    ThePlatformIE,
+    ThePlatformFeedIE,
+    RTVEALaCartaIE,
+    FunnyOrDieIE,
+    DemocracynowIE,
+)
