346112dee66fcc234863c5873435319fe725fe27
SUNsung <weatherius@gmail.com>
2019-04-27 00:40:31 +0300

    Rewrite [5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb] File in: 2019-04-27 00:40:31

--- ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
+++ ruby/5254VjhLSOw5231cQgWUpjc.52Xd6jqFjxhIA52P58.rb
@@ -1,40 +1,32 @@
 
         
-              it 'allows you to force the tag creation' do
-        tag = '2.0.0'
-        message = 'message'
-    
-          it 'works with new resolver' do
-        expect do
-          Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              use_ssh: true,
-              use_submodules: true,
-              use_binaries: false,
-              cache_builds: true,
-              platform: 'iOS',
-              new_resolver: true
-            )
-          end').runner.execute(:test)
-        end.not_to(raise_error)
-      end
-    
-        it 'adds an environment Hash at the beginning' do
-      message = 'A message'
-      command = command_from_args({ 'PATH' => '/usr/local/bin' }, 'git', 'commit', '-m', message)
-      expect(command).to eq('PATH=/usr/local/bin git commit -m #{message.shellescape}')
+            def release
+      context[:release]
     end
     
-    # Contributors should always provide a changelog when submitting a PR
-if github.pr_body.length < 5
-  warn('Please provide a changelog summary in the Pull Request description @#{github.pr_author}')
-end
-    
-          private
-    
-          require 'sidekiq/extensions/class_methods'
-      Module.__send__(:include, Sidekiq::Extensions::Klass)
+        def insert(index, *names)
+      @filters.insert assert_index(index), *filter_const(names)
     end
     
-            options = { secret: secret }
-        options = options.merge(s.to_hash) if s.respond_to? :to_hash
+    module Docs
+  class Entry
+    class Invalid < StandardError; end
+    
+      # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+    
+      def iteration
+    return @iteration ? @iteration : 1
+  end
+    
+        args = [ tar_cmd,
+             '-C',
+             staging_path,
+             '-cf',
+             payload_tar,
+             '--owner=0',
+             '--group=0',
+             '--numeric-owner',
+             '.' ]
