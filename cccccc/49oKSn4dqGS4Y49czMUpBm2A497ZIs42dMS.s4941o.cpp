4bc522b8718b7d0070d699eff246c5606de89a76
SUNsung <weatherius@gmail.com>
2019-04-26 13:18:05 +0300

    Rewrite [49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp] File in: 2019-04-26 13:18:04

--- cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
+++ cccccc/49oKSn4dqGS4Y49czMUpBm2A497ZIs42dMS.s4941o.cpp
@@ -1,310 +1,257 @@
 
         
         
-    {  std::move(self->tasks_[timer]).Run();
-  self->tasks_.erase(timer);
-  uv_timer_stop(timer);
-  uv_close(reinterpret_cast<uv_handle_t*>(timer), UvTaskRunner::OnClose);
+/*
+ * Checks if polygon is valid for use in Box2d engine.
+ * Last ditch effort to ensure no invalid polygons are
+ * added to world geometry.
+ *
+ * Performs a full check, for simplicity, convexity,
+ * orientation, minimum angle, and volume.  This won't
+ * be very efficient, and a lot of it is redundant when
+ * other tools in this section are used.
+ */
+bool b2Polygon::IsUsable(bool printErrors){
+	int32 error = -1;
+	bool noError = true;
+	if (nVertices < 3 || nVertices > b2_maxPolygonVertices) {noError = false; error = 0;}
+	if (!IsConvex()) {noError = false; error = 1;}
+	if (!IsSimple()) {noError = false; error = 2;}
+	if (GetArea() < CMP_EPSILON) {noError = false; error = 3;}
+    }
+    
+    
+    {	b2PolyNode(b2Vec2& pos);
+	b2PolyNode();
+	void AddConnection(b2PolyNode& toMe);
+	void RemoveConnection(b2PolyNode& fromMe);
+	void RemoveConnectionByIndex(int32 index);
+	bool IsConnectedTo(b2PolyNode& me);
+	b2PolyNode* GetRightestConnection(b2PolyNode* incoming);
+	b2PolyNode* GetRightestConnection(b2Vec2& incomingDir);
+};
+    
+    
+    {	}
+    
+    		m_mode = MODE_ETC1;
+		m_boolDiff = m_pencodingbitsRGB8->individual.diff;
+		m_boolFlip = m_pencodingbitsRGB8->individual.flip;
+		if (m_boolDiff)
+		{
+			int iR2 = (int)(m_pencodingbitsRGB8->differential.red1 + m_pencodingbitsRGB8->differential.dred2);
+			if (iR2 < 0)
+			{
+				iR2 = 0;
+			}
+			else if (iR2 > 31)
+			{
+				iR2 = 31;
+			}
+    }
+    
+    typedef opus_int16 opus_val16;
+typedef opus_int32 opus_val32;
+    
+    void opus_ifft_neon(const kiss_fft_state *st,
+                    const kiss_fft_cpx *fin,
+                    kiss_fft_cpx *fout);
+    
+    
+/** 16x32 multiplication, followed by a 15-bit shift right. Results fits in 32 bits */
+#undef MULT16_32_Q15
+static OPUS_INLINE opus_val32 MULT16_32_Q15_armv4(opus_val16 a, opus_val32 b)
+{
+  unsigned rd_lo;
+  int rd_hi;
+  __asm__(
+      '#MULT16_32_Q15\n\t'
+      'smull %0, %1, %2, %3\n\t'
+      : '=&r'(rd_lo), '=&r'(rd_hi)
+      : '%r'(b), 'r'(a<<16)
+  );
+  /*We intentionally don't OR in the high bit of rd_lo for speed.*/
+  return rd_hi<<1;
 }
+#define MULT16_32_Q15(a, b) (MULT16_32_Q15_armv4(a, b))
     
-      // WindowListObserver:
-  void OnWindowAllClosed() override;
-    
-      // display::DisplayObserver:
-  void OnDisplayAdded(const display::Display& new_display) override;
-  void OnDisplayRemoved(const display::Display& old_display) override;
-  void OnDisplayMetricsChanged(const display::Display& display,
-                               uint32_t changed_metrics) override;
-    
-    #ifndef ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
-#define ATOM_BROWSER_ATOM_QUOTA_PERMISSION_CONTEXT_H_
-    
-    #endif  // ATOM_BROWSER_LIB_POWER_OBSERVER_H_
-
-    
-    
-    {}  // namespace atom
-
-    
-    #include 'net/url_request/url_request_job_factory.h'
-    
-    void OffscreenViewProxy::SetObserver(OffscreenViewProxyObserver* observer) {
-  if (observer_) {
-    observer_->ProxyViewDestroyed(this);
-  }
-  observer_ = observer;
-}
-    
-    private:
-    NetworkStyle(const QString &appName, const int iconColorHueShift, const int iconColorSaturationReduction, const char *titleAddText);
-    
-    private:
-    reverse_lock(reverse_lock const&);
-    reverse_lock& operator=(reverse_lock const&);
-    
-    static void secp256k1_rfc6979_hmac_sha256_finalize(secp256k1_rfc6979_hmac_sha256_t *rng) {
-    memset(rng->k, 0, 32);
-    memset(rng->v, 0, 32);
-    rng->retry = 0;
-}
-    
-        /* Check bad contexts and NULLs for recovery */
-    ecount = 0;
-    CHECK(secp256k1_ecdsa_recover(none, &recpubkey, &recsig, message) == 0);
-    CHECK(ecount == 1);
-    CHECK(secp256k1_ecdsa_recover(sign, &recpubkey, &recsig, message) == 0);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(vrfy, &recpubkey, &recsig, message) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, message) == 1);
-    CHECK(ecount == 2);
-    CHECK(secp256k1_ecdsa_recover(both, NULL, &recsig, message) == 0);
-    CHECK(ecount == 3);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, NULL, message) == 0);
-    CHECK(ecount == 4);
-    CHECK(secp256k1_ecdsa_recover(both, &recpubkey, &recsig, NULL) == 0);
-    CHECK(ecount == 5);
-    
-    #include <bech32.h>
-#include <test/test_bitcoin.h>
-    
-    ; rotate_Xs
-; Rotate values of symbols X0...X3
-%macro rotate_Xs 0
-%xdefine X_ X0
-%xdefine X0 X1
-%xdefine X1 X2
-%xdefine X2 X3
-%xdefine X3 X_
-%endm
-    
-    // Owns a python object and decrements the reference count on destruction.
-// This class is not threadsafe.
-template <typename PyObjectStruct>
-class ScopedPythonPtr {
- public:
-  // Takes the ownership of the specified object to ScopedPythonPtr.
-  // The reference count of the specified py_object is not incremented.
-  explicit ScopedPythonPtr(PyObjectStruct* py_object = NULL)
-      : ptr_(py_object) {}
-    }
-    
-    namespace google {
-namespace protobuf {
-namespace compiler {
-namespace csharp {
-namespace {
-    }
-    }
-    }
-    }
-    }
-    
-    ExtensionGenerator* ImmutableGeneratorFactory::NewExtensionGenerator(
-    const FieldDescriptor* descriptor) const {
-  if (HasDescriptorMethods(descriptor->file(), context_->EnforceLite())) {
-    return new ImmutableExtensionGenerator(descriptor, context_);
-  } else {
-    return new ImmutableExtensionLiteGenerator(descriptor, context_);
-  }
-}
-    
-      // File descriptor only needed if there are messages to use it.
-  if (message_generators_.size() > 0) {
-    std::map<string, string> vars;
-    vars['root_class_name'] = root_class_name_;
-    vars['package'] = file_->package();
-    vars['objc_prefix'] = FileClassPrefix(file_);
-    switch (file_->syntax()) {
-      case FileDescriptor::SYNTAX_UNKNOWN:
-        vars['syntax'] = 'GPBFileSyntaxUnknown';
-        break;
-      case FileDescriptor::SYNTAX_PROTO2:
-        vars['syntax'] = 'GPBFileSyntaxProto2';
-        break;
-      case FileDescriptor::SYNTAX_PROTO3:
-        vars['syntax'] = 'GPBFileSyntaxProto3';
-        break;
-    }
-    printer->Print(vars,
-        '#pragma mark - $root_class_name$_FileDescriptor\n'
-        '\n'
-        'static GPBFileDescriptor *$root_class_name$_FileDescriptor(void) {\n'
-        '  // This is called by +initialize so there is no need to worry\n'
-        '  // about thread safety of the singleton.\n'
-        '  static GPBFileDescriptor *descriptor = NULL;\n'
-        '  if (!descriptor) {\n'
-        '    GPB_DEBUG_CHECK_RUNTIME_VERSIONS();\n');
-    if (vars['objc_prefix'].size() > 0) {
-      printer->Print(
-          vars,
-          '    descriptor = [[GPBFileDescriptor alloc] initWithPackage:@\'$package$\'\n'
-          '                                                 objcPrefix:@\'$objc_prefix$\'\n'
-          '                                                     syntax:$syntax$];\n');
-    } else {
-      printer->Print(
-          vars,
-          '    descriptor = [[GPBFileDescriptor alloc] initWithPackage:@\'$package$\'\n'
-          '                                                     syntax:$syntax$];\n');
-    }
-    printer->Print(
-        '  }\n'
-        '  return descriptor;\n'
-        '}\n'
-        '\n');
-  }
-    
-    bool GzipOutputStream::Flush() {
-  zerror_ = Deflate(Z_FULL_FLUSH);
-  // Return true if the flush succeeded or if it was a no-op.
-  return  (zerror_ == Z_OK) ||
-      (zerror_ == Z_BUF_ERROR && zcontext_.avail_in == 0 &&
-       zcontext_.avail_out != 0);
-}
-    
-    
-bool ZeroCopyOutputStream::WriteAliasedRaw(const void* /* data */,
-                                           int /* size */) {
-  GOOGLE_LOG(FATAL) << 'This ZeroCopyOutputStream doesn't support aliasing. '
-                'Reaching here usually means a ZeroCopyOutputStream '
-                'implementation bug.';
-  return false;
-}
-    
-    Status::Status(const Status& other)
-    : error_code_(other.error_code_), error_message_(other.error_message_) {
-}
-    
-    
-// Abbreviations: true_type and false_type are structs that represent boolean
-// true and false values. Also define the boost::mpl versions of those names,
-// true_ and false_.
-typedef integral_constant<bool, true>  true_type;
-typedef integral_constant<bool, false> false_type;
-typedef true_type  true_;
-typedef false_type false_;
-    
-    // Force shutdown at process exit so that we can test for memory leaks.  To
-// actually check for leaks, I suggest using the heap checker included with
-// google-perftools.  Set it to 'draconian' mode to ensure that every last
-// call to malloc() has a corresponding free().
-struct ForceShutdown {
-  ~ForceShutdown() {
-    ShutdownProtobufLibrary();
-    // Test to shutdown the library twice, which should succeed.
-    ShutdownProtobufLibrary();
-  }
-} force_shutdown;
-    
-      while (true) {
-    void* outptr;
-    int outlen;
-    bool ok;
-    do {
-      ok = out.Next(&outptr, &outlen);
-      if (!ok) {
-        break;
-      }
-    } while (outlen <= 0);
-    readlen = read(STDIN_FILENO, outptr, outlen);
-    if (readlen <= 0) {
-      out.BackUp(outlen);
-      break;
-    }
-    if (readlen < outlen) {
-      out.BackUp(outlen - readlen);
-    }
-  }
-    
-    
-    {    msgPattern.parseSelectStyle(newPattern, NULL, status);
-    if (U_FAILURE(status)) {
-        msgPattern.clear();
-    }
-}
-    
-    U_NAMESPACE_END
-    
-    U_NAMESPACE_BEGIN
-    
-    // A hash code of kInvalidHashCode indicates that the hash code needs
-// to be computed. A hash code of kEmptyHashCode is used for empty keys
-// and for any key whose computed hash code is kInvalidHashCode.
-static const int32_t kInvalidHashCode = 0;
-static const int32_t kEmptyHashCode = 1;
-// The 'bogus hash code' replaces a separate fBogus flag.
-static const int32_t kBogusHashCode = 2;
-    
-        /**
-     * Remove any match data.  This must be called before performing a
-     * set of matches with this segment.
-     */
-    void resetMatch();
-    
-    int main() {
-    }
-    
-    int main() {
-    }
-    
-    
-void printBool(bool res){
-    cout << (res ? 'True' : 'False') << endl;
-}
-    
-    // Recursive
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-public:
-    vector<int> inorderTraversal(TreeNode* root) {
-    }
-    }
+    /** 16x16 multiply-add where the result fits in 32 bits */
+#define MAC16_16(c,a,b) (ADD32((c),MULT16_16((a),(b))))
+/** 16x32 multiply, followed by a 15-bit shift right and 32-bit add.
+    b must fit in 31 bits.
+    Result fits in 32 bits. */
+#define MAC16_32_Q15(c,a,b) ADD32((c),ADD32(MULT16_16((a),SHR((b),15)), SHR(MULT16_16((a),((b)&0x00007fff)),15)))
     
     
     {    return 0;
 }
 
     
+            vector<int> res;
+        if( root == NULL )
+            return res;
     
-// Classic Non-Recursive algorithm for inorder traversal
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
+    int main() {
     }
     
-    public:
-    vector<int> preorderTraversal(TreeNode* root) {
+    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
+// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
+// https://github.com/ocornut/imgui
+    
+    int main(int, char**)
+{
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+        return 1;
+    }
+    
+    // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, 'legacy_stdio_definitions')
+#endif
+    
+        // Setup Platform/Renderer bindings
+    ImGui_ImplWin32_Init(hwnd);
+    ImGui_ImplDX10_Init(g_pd3dDevice);
+    
+    // Helper functions
+    
+    void glut_display_func()
+{
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL2_NewFrame();
+    ImGui_ImplGLUT_NewFrame();
+    }
+    
+        for (int n = 0; n < 20; n++)
+    {
+        printf('NewFrame() %d\n', n);
+        io.DisplaySize = ImVec2(1920, 1080);
+        io.DeltaTime = 1.0f / 60.0f;
+        ImGui::NewFrame();
+    }
+    
+    void Action::stop()
+{
+    _target = nullptr;
+}
+    
+        //
+    // Overrides
+    //
+    /**
+     * @param time In seconds.
+     */
+    virtual void update(float time) override;
+    virtual DelayTime* reverse() const override;
+    virtual DelayTime* clone() const override;
+    
+        tHashElement *element = nullptr;
+    // we should convert it to Ref*, because we save it as Ref*
+    Ref *tmp = target;
+    HASH_FIND_PTR(_targets, &tmp, element);
+    if (! element)
+    {
+        element = (tHashElement*)calloc(sizeof(*element), 1);
+        element->paused = paused;
+        target->retain();
+        element->target = target;
+        HASH_ADD_PTR(_targets, target, element);
     }
     
     /**
- * @brief A single typed row from a database query
- *
- * RowTyped is a simple map where individual column names are keys, which map to
- * the Row's respective type-variant value
+ @brief This action simulates a page turn from the bottom right hand corner of the screen.
+ 
+ @details It's not much use by itself but is used by the PageTurnTransition.
+         Based on an original paper by L Hong et al.
+         http://www.parc.com/publication/1638/turning-pages-of-3d-electronic-books.html
+  
+ @since v0.8.2
  */
-using RowTyped = std::map<std::string, RowDataTyped>;
-    
-      // Start a server.
-  TLSServerRunner::start();
-  TLSServerRunner::setClientConfig();
-  clearNodeKey();
-    
-    std::pair<JSON, RowTyped> getSerializedRow(bool unordered_and_repeated) {
-  auto cns = getSerializedRowColumnNames(unordered_and_repeated);
+class CC_DLL PageTurn3D : public Grid3DAction
+{
+public:
+    /**
+     * @js NA 
+     */
+    virtual GridBase* getGrid() override;
     }
     
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
     
-    {  std::string operator()(const std::string& str) const {
-    return str;
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __CCACTIONTWEEN_H__
+#define __CCACTIONTWEEN_H__
+    
+    void AnimationCache::parseVersion1(const ValueMap& animations)
+{
+    SpriteFrameCache *frameCache = SpriteFrameCache::getInstance();
+    }
+    
+      // We keep the lowest and highest addresses allocated as a quick way
+  // of determining that pointers are outside the heap. The estimate is
+  // conservative, i.e. not all addresses in 'allocated' space are allocated
+  // to our heap. The range is [lowest, highest[, inclusive on the low end
+  // and exclusive on the high end.
+  std::atomic<Address> lowest_ever_allocated_;
+  std::atomic<Address> highest_ever_allocated_;
+    
+      if (FLAG_trace_stress_scavenge) {
+    heap_.isolate()->PrintWithTimestamp(
+        '[Scavenge] %.2lf%% of the new space capacity reached\n',
+        current_percent);
+    heap_.isolate()->PrintWithTimestamp('[Scavenge] %d%% is the new limit\n',
+                                        limit_percentage_);
   }
-};
     
-    QueryData execute_query(std::string query) {
-  auto instance = SQLiteDBManager::get();
-  QueryData rows;
-  Status status = queryInternal(query, rows, instance);
-  EXPECT_TRUE(status.ok()) << 'Query execution failed with error: '
-                           << boost::io::quoted(status.what());
-  return rows;
+    
+    {  ForAllSweepingSpaces([this](AllocationSpace space) {
+    CHECK(sweeping_list_[GetSweepSpaceIndex(space)].empty());
+  });
+  sweeping_in_progress_ = false;
 }
+    
+      void andps(XMMRegister dst, Operand src);
+  void andps(XMMRegister dst, XMMRegister src) { andps(dst, Operand(src)); }
+  void xorps(XMMRegister dst, Operand src);
+  void xorps(XMMRegister dst, XMMRegister src) { xorps(dst, Operand(src)); }
+  void orps(XMMRegister dst, Operand src);
+  void orps(XMMRegister dst, XMMRegister src) { orps(dst, Operand(src)); }
+    
+    
+class InstructionTable {
+ public:
+  InstructionTable();
+  const InstructionDesc& Get(byte x) const { return instructions_[x]; }
+  static InstructionTable* get_instance() {
+    static InstructionTable table;
+    return &table;
+  }
+    }
+    
+    void AccessorAssembler::HandleStoreICHandlerCase(
+    const StoreICParameters* p, TNode<MaybeObject> handler, Label* miss,
+    ICMode ic_mode, ElementSupport support_elements) {
+  Label if_smi_handler(this), if_nonsmi_handler(this);
+  Label if_proto_handler(this), if_element_handler(this), call_handler(this),
+      store_transition_or_global(this);
+    }
+    
+    #include 'src/code-stub-assembler.h'
