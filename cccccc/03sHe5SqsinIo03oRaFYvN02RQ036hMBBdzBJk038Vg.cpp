4096d39c90264a7b6bd8309578ba9d863ae5226e
SUNsung <weatherius@gmail.com>
2019-04-27 22:43:59 +0300

    Rewrite [03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp] File in: 2019-04-27 22:43:58

--- cccccc/03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp
+++ cccccc/03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp
@@ -1,195 +1,103 @@
 
         
         
-    {    void operator() (const u8 * src0, const u8 * src1, u8 * dst) const
-    {
-        dst[0] = src0[0] & src1[0];
-    }
-};
-    
-    
-    {            v_src = vld4q_u8(src + sj + 64);
-            vst1q_u8(dst + dj + 16, v_src.val[coi]);
-        }
-#endif
-    
-    
-    {            v_y = vld2q_u8(srcy + syj + 32);
-            v_dst.val[0] = vld1q_u8(srcu + sj + 16);
-            v_dst.val[1] = v_y.val[0];
-            v_dst.val[2] = vld1q_u8(srcv + sj + 16);
-            v_dst.val[3] = v_y.val[1];
-            vst4q_u8(dst + dj + 64, v_dst);
-        }
-#endif
-    
-             int32x4_t vline_s32_lo = vmovl_s16(vget_low_s16(vline_s16));
-         int32x4_t vline_s32_hi = vmovl_s16(vget_high_s16(vline_s16));
-    
-            if(i + 2 <= size.width)
-        {
-            float32x2_t vres = vmul_f32(vld1_f32(src0 + i), vld1_f32(src1 + i));
-            result += vget_lane_f32(vres, 0) + vget_lane_f32(vres, 1);
-            i += 2;
-        }
-    
-                    uint8x16_t v0 = vld1q_u8(ptr);
-                int8x16_t v1 = vreinterpretq_s8_u8(veorq_u8(vqsubq_u8(v0, t), delta));
-                int8x16_t v2 = vreinterpretq_s8_u8(veorq_u8(vqaddq_u8(v0, t), delta));
-    
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const T * src = getRowPtr((const T *)srcBase, srcStride, i);
-        T * dst = getRowPtr((T *)dstBase, dstStride, (flipMode & FLIP_VERTICAL_MODE) != 0 ? size.height - i - 1 : i);
-        size_t j = 0, js = 0, jd = size.width * 3;
-    }
-    
-    #include 'common.hpp'
-    
-    #include 'common.hpp'
-    
-    namespace CAROTENE_NS { namespace internal {
-    }
-    }
-    
-    // Helper method to convert an orientation index to its value in degrees.
-// The value represents the amount of clockwise rotation in degrees that must be
-// applied for the text to be upright (readable).
-TESS_API int OrientationIdToValue(const int& id);
-    
-    #include 'allheaders.h'
-    
-      // Copying a DENORM is allowed.
-  DENORM(const DENORM &);
-  DENORM& operator=(const DENORM&);
-  ~DENORM();
-    
-      // Gets the text streamed to this object so far as an std::string.
-  // Each '\0' character in the buffer is replaced with '\\0'.
-  //
-  // INTERNAL IMPLEMENTATION - DO NOT USE IN A USER PROGRAM.
-  std::string GetString() const;
-    
-    ]]
-    
-      // Same as above, but you can choose the interception scope of this object.
-  ScopedFakeTestPartResultReporter(InterceptMode intercept_mode,
-                                   TestPartResultArray* result);
-    
-    // The empty template list.
-struct Templates0 {};
-    
-    using ::testing::EmptyTestEventListener;
-using ::testing::InitGoogleTest;
-using ::testing::Test;
-using ::testing::TestCase;
-using ::testing::TestEventListeners;
-using ::testing::TestInfo;
-using ::testing::TestPartResult;
-using ::testing::UnitTest;
-    
-      const size_t len = strlen(a_c_string);
-  char* const clone = new char[ len + 1 ];
-  memcpy(clone, a_c_string, len + 1);
-    
-    // A sample program demonstrating using Google C++ testing framework.
-//
-// Author: wan@google.com (Zhanyong Wan)
-    
-    // Tests the c'tor that accepts a C string.
-TEST(MyString, ConstructorFromCString) {
-  const MyString s(kHelloString);
-  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
-  EXPECT_EQ(sizeof(kHelloString)/sizeof(kHelloString[0]) - 1,
-            s.Length());
+    {  // Verify that the size of the key space not touched by the reads
+  // is pretty much unchanged.
+  const int64_t final_other_size = Size(Key(n), Key(kCount));
+  ASSERT_LE(final_other_size, initial_other_size + 1048576);
+  ASSERT_GE(final_other_size, initial_other_size/5 - 1048576);
 }
-    
-    void DBImpl::DeleteObsoleteFiles() {
-  mutex_.AssertHeld();
-    }
     
     #ifndef STORAGE_LEVELDB_DB_LOG_FORMAT_H_
 #define STORAGE_LEVELDB_DB_LOG_FORMAT_H_
     
-    namespace leveldb {
-namespace log {
-    }
-    }
-    
-    
-    {}  // namespace leveldb
-    
-      // If a seek to internal key 'k' in specified file finds an entry,
-  // call (*handle_result)(arg, found_key, found_value).
-  Status Get(const ReadOptions& options,
-             uint64_t file_number,
-             uint64_t file_size,
-             const Slice& k,
-             void* arg,
-             void (*handle_result)(void*, const Slice&, const Slice&));
-    
-    
-    {  TSimpleServer server(processor, serverTransport, transportFactory, protocolFactory);
-  server.serve();
-  return 0;
-}
-    
-    // Saved resource information for the beginning of an operation
-#ifdef WIN32
-struct rusage {
-  FILETIME ru_utime;
-  FILETIME ru_stime;
-};
-#endif
-    
-      // Join results..
-  for (auto& entry : hw_info) {
-    bool matched = false;
-    for (auto& row : results) {
-      auto serial = row.find('serial_number');
-      if (serial == row.end()) {
-        continue;
-      }
-    }
-    }
-    
-    TEST_F(PerfOutputTests,
-       impl_consumeWrappedMessagesFromCircularBuffer_simply_wrapped) {
-  using WrappedMessage = ebpf::PerfOutput<TestMessage>::WrappedMessage;
-  auto const test_size = std::size_t{9};
-  auto buf =
-      std::vector<ebpf::impl::ByteType>(sizeof(WrappedMessage) * test_size, 0);
-  auto buf_ptr = &buf[0];
-  for (std::size_t i = 0; i < test_size; ++i) {
-    auto wrapped = WrappedMessage{};
-    wrapped.msg.a_ = i + 1;
-    wrapped.msg.b_ = i * 2 + 2;
-    wrapped.msg.c_ = 'y';
-    wrapped.msg.d_ = 'x';
-    wrapped.size = sizeof(TestMessage);
-    wrapped.header.type = PERF_RECORD_SAMPLE;
-    wrapped.header.size = sizeof(WrappedMessage);
-    auto const wrapped_ptr =
-        reinterpret_cast<ebpf::impl::ByteType const*>(&wrapped);
-    std::copy(wrapped_ptr, wrapped_ptr + sizeof(WrappedMessage), buf_ptr);
-    buf_ptr += sizeof(WrappedMessage);
+      void SetComparatorName(const Slice& name) {
+    has_comparator_ = true;
+    comparator_ = name.ToString();
   }
-  auto dst = std::vector<TestMessage>{};
-  auto status =
-      ebpf::impl::consumeWrappedMessagesFromCircularBuffer<WrappedMessage>(
-          &buf[0],
-          sizeof(WrappedMessage),
-          buf.size() + sizeof(WrappedMessage),
-          buf.size(),
-          dst);
-  ASSERT_FALSE(status.isError()) << status.getError().getMessage();
-  ASSERT_EQ(dst.size(), test_size);
-  for (std::size_t i = 0; i < test_size; ++i) {
-    EXPECT_EQ(dst[i].c_, 'y') << i;
-    EXPECT_EQ(dst[i].d_, 'x');
+  void SetLogNumber(uint64_t num) {
+    has_log_number_ = true;
+    log_number_ = num;
   }
-  EXPECT_EQ(dst.back().a_, 1);
-  EXPECT_EQ(dst.back().b_, 2);
-  EXPECT_EQ(dst[0].a_, 2);
-  EXPECT_EQ(dst[0].b_, 4);
+  void SetPrevLogNumber(uint64_t num) {
+    has_prev_log_number_ = true;
+    prev_log_number_ = num;
+  }
+  void SetNextFile(uint64_t num) {
+    has_next_file_number_ = true;
+    next_file_number_ = num;
+  }
+  void SetLastSequence(SequenceNumber seq) {
+    has_last_sequence_ = true;
+    last_sequence_ = seq;
+  }
+  void SetCompactPointer(int level, const InternalKey& key) {
+    compact_pointers_.push_back(std::make_pair(level, key));
+  }
+    
+    namespace php {
+struct Program;
 }
+struct Index;
+    
+    Object APCCollection::createObject() const {
+  if (m_arrayHandle->isTypedValue()) {
+    Variant local(m_arrayHandle->toLocal());
+    assertx(local.isArray());
+    return Object::attach(
+      collections::alloc(m_colType, local.getArrayData())
+    );
+  }
+    }
+    
+    struct FileStreamWrapper final : Stream::Wrapper {
+  static req::ptr<MemFile> openFromCache(const String& filename,
+                                         const String& mode);
+  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+  int access(const String& path, int mode) override {
+    return ::access(File::TranslatePath(path).data(), mode);
+  }
+  int stat(const String& path, struct stat* buf) override {
+    return ::stat(File::TranslatePath(path).data(), buf);
+  }
+  int lstat(const String& path, struct stat* buf) override {
+    return ::lstat(File::TranslatePath(path).data(), buf);
+  }
+  int unlink(const String& path) override;
+  int rename(const String& oldname, const String& newname) override;
+  int mkdir(const String& path, int mode, int options) override;
+  int rmdir(const String& path, int /*options*/) override {
+    ERROR_RAISE_WARNING(::rmdir(File::TranslatePath(path).data()));
+    return ret;
+  }
+  bool isNormalFileStream() const override { return true; }
+    }
+    
+      // implementing File
+  bool open(const String& filename, const String& mode) override;
+  bool close() override;
+  int64_t readImpl(char *buffer, int64_t length) override;
+  int getc() override;
+  int64_t writeImpl(const char *buffer, int64_t length) override;
+  bool seek(int64_t offset, int whence = SEEK_SET) override;
+  int64_t tell() override;
+  bool eof() override;
+  bool rewind() override;
+  bool flush() override;
+  bool truncate(int64_t size) override;
+    
+    
+    {}
+
+    
+        // out_of_range.403
+    try
+    {
+        // try to use a JSON pointer to an nonexistent object key
+        json::const_reference ref = j.at('/foo'_json_pointer);
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
+    }
