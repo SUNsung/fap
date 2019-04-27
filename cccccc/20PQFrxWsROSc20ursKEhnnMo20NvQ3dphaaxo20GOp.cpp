337b2175635ca5984618e60080a00caa08c60cd1
SUNsung <weatherius@gmail.com>
2019-04-27 09:07:01 +0300

    Rewrite [20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp] File in: 2019-04-27 09:07:00

--- cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
+++ cccccc/20PQFrxWsROSc20ursKEhnnMo20NvQ3dphaaxo20GOp.cpp
@@ -1,284 +1,116 @@
 
         
-        void absDiff(const Size2D &size,
-             const f32 * src0Base, ptrdiff_t src0Stride,
-             const f32 * src1Base, ptrdiff_t src1Stride,
-             f32 * dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, AbsDiff<f32>());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
-}
+            int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(int);
     
-    namespace {
-    }
-    
-                rowx[k]   = srow4[px*cn+k] + srow3[px*cn+k] + srow2[px*cn+k] + srow1[px*cn+k] + srow0[px*cn+k];
-    
-    
-    {
-    {        for( k = 0; k < ncorners; k++ )
-        {
-            j = cornerpos[k];
-            s32 score = prev[j];
-            if( !nonmax_suppression ||
-                    (score > prev[j+1] && score > prev[j-1] &&
-                     score > pprev[j-1] && score > pprev[j] && score > pprev[j+1] &&
-                     score > curr[j-1] && score > curr[j] && score > curr[j+1]) )
-            {
-                keypoints->push((f32)j, (f32)(i-1), 7.f, -1, (f32)score);
-            }
-        }
-    }
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)keypoints;
-    (void)threshold;
-    (void)nonmax_suppression;
-#endif
-}
-    
-        if (borderType == BORDER_MODE_CONSTANT)
-        for (s32 k = 0; k < cn; ++k)
-        {
-            lane[-cn+k] = borderValue;
-            lane[-cn-cn+k] = borderValue;
-            lane[colsn+k] = borderValue;
-            lane[colsn+cn+k] = borderValue;
+            ListNode* pre = dummyHead;
+        for(int i = 0; i < m - 1; i ++){
+            pre = pre->next
         }
     
-    template <typename T, int elsize> struct vtail
-{
-    static inline void inRange(const T *, const T *, const T *,
-                               u8 *, size_t &, size_t)
-    {
-        //do nothing since there couldn't be enough data
-    }
+    
+/// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-template <typename T> struct vtail<T, 2>
-{
-    static inline void inRange(const T * src, const T * rng1, const T * rng2,
-                               u8 * dst, size_t &x, size_t width)
-    {
-        typedef typename internal::VecTraits<T>::vec128 vec128;
-        typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
-        //There no more than 15 elements in the tail, so we could handle 8 element vector only once
-        if( x + 8 < width)
-        {
-             vec128  vs = internal::vld1q( src + x);
-             vec128 vr1 = internal::vld1q(rng1 + x);
-             vec128 vr2 = internal::vld1q(rng2 + x);
-            uvec128  vd = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
-            internal::vst1(dst + x, internal::vmovn(vd));
-            x+=8;
-        }
-    }
+    
+    using namespace std;
+    
+            vector<int> res;
+        if( root == NULL )
+            return res;
+    
+    /// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-template <typename T> struct vtail<T, 1>
-{
-    static inline void inRange(const T * src, const T * rng1, const T * rng2,
-                               u8 * dst, size_t &x, size_t width)
+    
+    
     {
-        typedef typename internal::VecTraits<T>::vec128 vec128;
-        typedef typename internal::VecTraits<T>::unsign::vec128 uvec128;
-        typedef typename internal::VecTraits<T>::vec64 vec64;
-        typedef typename internal::VecTraits<T>::unsign::vec64 uvec64;
-        //There no more than 31 elements in the tail, so we could handle once 16+8 or 16 or 8 elements
-        if( x + 16 < width)
-        {
-             vec128  vs = internal::vld1q( src + x);
-             vec128 vr1 = internal::vld1q(rng1 + x);
-             vec128 vr2 = internal::vld1q(rng2 + x);
-            uvec128  vd = internal::vandq(internal::vcgeq(vs, vr1), internal::vcgeq(vr2, vs));
-            internal::vst1q(dst + x, vd);
-            x+=16;
-        }
-        if( x + 8 < width)
-        {
-             vec64  vs = internal::vld1( src + x);
-             vec64 vr1 = internal::vld1(rng1 + x);
-             vec64 vr2 = internal::vld1(rng2 + x);
-            uvec64  vd = internal::vand(internal::vcge(vs, vr1), internal::vcge(vr2, vs));
-            internal::vst1(dst + x, vd);
-            x+=8;
+    {        if( node ){
+            __postorderTraversal(node->left, res);
+            __postorderTraversal(node->right, res);
+            res.push_back(node->val);
         }
     }
 };
     
-    void Laplacian3x3(const Size2D &size,
-                  const u8 * srcBase, ptrdiff_t srcStride,
-                  u8 * dstBase, ptrdiff_t dstStride,
-                  BORDER_MODE border, u8 borderValue)
-{
-    internal::assertSupportedConfiguration(isLaplacian3x3Supported(size, border));
-#ifdef CAROTENE_NEON
-    const uint16x8_t v_border_x3 = vdupq_n_u16(borderValue * 3);
-    const uint16x8_t v_zero = vdupq_n_u16(0);
-    const uint8x8_t v_border = vdup_n_u8(borderValue);
-    }
     
-    enum GARBAGE_LEVEL
-{
-  G_NEVER_CRUNCH,
-  G_OK,
-  G_DODGY,
-  G_TERRIBLE
+    {
+    {        return res;
+    }
 };
     
-    
-/**********************************************************************
- * recog_word_recursive
- *
- * Convert the word to tess form and pass it to the tess segmenter.
- * Convert the output back to editor form.
- **********************************************************************/
-void Tesseract::recog_word_recursive(WERD_RES *word) {
-  int word_length = word->chopped_word->NumBlobs();  // no of blobs
-  if (word_length > MAX_UNDIVIDED_LENGTH) {
-    return split_and_recog_word(word);
-  }
-  cc_recog(word);
-  word_length = word->rebuild_word->NumBlobs();  // No of blobs in output.
+    template<typename Dtype>
+class CaffeDataIter : public IIterator<TBlobBatch> {
+ public:
+  explicit CaffeDataIter(int type_flag) : batch_size_(0), channels_(1), width_(1), height_(1)
+                               , type_flag_(type_flag), loc_(0)
+  {}
+  virtual ~CaffeDataIter(void) {}
     }
     
-      // Adds a new point. Takes a copy - the pt doesn't need to stay in scope.
-  // Add must be called on points in sequence along the line.
-  void Add(const ICOORD& pt);
-  // Associates a half-width with the given point if a point overlaps the
-  // previous point by more than half the width, and its distance is further
-  // than the previous point, then the more distant point is ignored in the
-  // distance calculation. Useful for ignoring i dots and other diacritics.
-  void Add(const ICOORD& pt, int halfwidth);
+      /**
+   * /brief Customize set method for LayerParameter
+   * /tparam value string of caffe's layer configuration
+   * */
+  virtual void Set(void *head, const std::string &value) const {
+    caffe::NetParameter net_param;
+    if (!ReadProtoFromTextContent(value, &net_param))
+      CHECK(false)<< 'Caffe Net Prototxt: ' << value << 'Initialized Failed';
+    }
     
-    // Solve the dynamic programming problem for the given array of points, with
-// the given size and cost function.
-// Steps backwards are limited to being between min_step and max_step
-// inclusive.
-// The return value is the tail of the best path.
-DPPoint* DPPoint::Solve(int min_step, int max_step, bool debug,
-                        CostFunc cost_func, int size, DPPoint* points) {
-  if (size <= 0 || max_step < min_step || min_step >= size)
-    return nullptr;  // Degenerate, but not necessarily an error.
-  ASSERT_HOST(min_step > 0);  // Infinite loop possible if this is not true.
-  if (debug)
-    tprintf('min = %d, max=%d\n',
-            min_step, max_step);
-  // Evaluate the total cost at each point.
-  for (int i = 0; i < size; ++i) {
-    for (int offset = min_step; offset <= max_step; ++offset) {
-      DPPoint* prev = offset <= i ? points + i - offset : nullptr;
-      int64_t new_cost = (points[i].*cost_func)(prev);
-      if (points[i].best_prev_ != nullptr && offset > min_step * 2 &&
-          new_cost > points[i].total_cost_)
-        break;  // Find only the first minimum if going over twice the min.
+    namespace mxnet {
+namespace io {
+// iterator on image recordio
+class PrefetcherIter : public IIterator<DataBatch> {
+ public:
+  explicit PrefetcherIter(IIterator<TBlobBatch>* base)
+      : loader_(base), out_(nullptr) {}
     }
-    points[i].total_cost_ += points[i].local_cost_;
-    if (debug) {
-      tprintf('At point %d, local cost=%d, total_cost=%d, steps=%d\n',
-              i, points[i].local_cost_, points[i].total_cost_,
-              points[i].total_steps_);
     }
-  }
-  // Now find the end of the best path and return it.
-  int best_cost = points[size - 1].total_cost_;
-  int best_end = size - 1;
-  for (int end = best_end - 1; end >= size - min_step; --end) {
-    int cost = points[end].total_cost_;
-    if (cost < best_cost) {
-      best_cost = cost;
-      best_end = end;
     }
-  }
-  return points + best_end;
+    
+    
+    {
+    {inline void Dequantize2BitImpl(mshadow::Stream<mshadow::cpu> *s,
+                               const std::vector<mxnet::TBlob> &inputs,
+                               const float threshold) {
+  Dequantize2BitKernelLaunch(s, inputs, threshold);
 }
+}  // namespace kvstore
+}  // namespace mxnet
     
-     private:
-  // Returns a page by serial number, selecting them in a round-robin fashion
-  // from all the documents. Highly disk-intensive, but doesn't need samples
-  // to be shuffled between files to begin with.
-  const ImageData* GetPageRoundRobin(int serial);
-  // Returns a page by serial number, selecting them in sequence from each file.
-  // Requires the samples to be shuffled between the files to give a random or
-  // uniform distribution of data. Less disk-intensive than GetPageRoundRobin.
-  const ImageData* GetPageSequential(int serial);
-    
-    class DLLSYM DIR128
-{
-  public:
-    DIR128() = default;
+        // Compute the current row-wise Kronecker product
+    *result = 0;
+    for (int i = 0; i < nrows; ++i) {
+      // BLAS signature
+      //
+      // dger(
+      //   m : ts.size(1), length of each row of current matrix
+      //   n : ncols, length of each row of previous result
+      //   alpha : 1, scaling to the outer product of x and y
+      //   x : ts[i].dptr_, current row of current matrix
+      //   incx : 1, as each element in the row is contiguous
+      //   y : (*given)[i].dptr_, current row of the given variable
+      //   incy : 1, as each element in the row is contiguous
+      //   a : (*result)[i].dptr_, current row of the result variable
+      //   lda : ts.size(1), as the outer product is stored as one row
+      //         which occupies contiguous memory, and as BLASEngine::ger()
+      //         assumes column-major matrix, lda has to be precisely
+      //         the length of x, i.e. ts[i].size(1)
+      // )
+      expr::BLASEngine<cpu, DType>::ger
+        (result->stream_,
+        ts.size(1), ncols, 1,
+        ts[i].dptr_, 1,
+        (*given)[i].dptr_, 1,
+        (*result)[i].dptr_, ts.size(1));
     }
-    
-    
-    {    return SW_OK;
-}
-    
-    bool Channel::push(void *data, double timeout)
-{
-    if (closed)
-    {
-        return false;
-    }
-    if (is_full() || !producer_queue.empty())
-    {
-        timer_msg_t msg;
-        msg.error = false;
-        msg.timer = NULL;
-        if (timeout > 0)
-        {
-            long msec = (long) (timeout * 1000);
-            msg.chan = this;
-            msg.type = PRODUCER;
-            msg.co = Coroutine::get_current();
-            msg.timer = swTimer_add(&SwooleG.timer, msec, 0, &msg, timer_callback);
-        }
-    }
-    }
-    
-        swListenPort *port = swServer_add_port(&serv, SW_SOCK_TCP, '127.0.0.1', 9501);
-    port->open_eof_check = 0;
-    //config
-    port->backlog = 128;
-    memcpy(port->protocol.package_eof, SW_STRL('\r\n\r\n'));  //开启eof检测，启用buffer区
-    
-        for (int i = 0; i < 1000; ++i)
-    {
-        auto ret = swAio_dispatch2(&event);
-        ASSERT_EQ(ret->object, event.object);
-        ASSERT_NE(ret->task_id, event.task_id);
-    }
-    
-    #include 'swoole.h'
-#include 'async.h'
-    
-    
-    {
-    {        ASSERT_LT(swTimer_get_absolute_msec() - start, 5);
-    });
-}
-    
-        for (long i = 0; i < N; i++)
-    {
-        ASSERT_EQ(swThreadPool_dispatch(&pool, (void*) &result, sizeof(result)), SW_OK);
-    }
-    
-    class coro_test
-{
-public:
-    coro_test(coroutine_func_t _fn, void *_arg, int *_complete_num) : fn(_fn), arg(_arg), complete_num(_complete_num)
-    {
-    }
-    }
+    ncols *= ts.size(1);
