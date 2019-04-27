f9707b263630b3b7fa6c6df50a430ce04da0ed59
SUNsung <weatherius@gmail.com>
2019-04-27 05:14:29 +0300

    Rewrite [41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb] File in: 2019-04-27 05:14:28

--- ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
+++ ruby/41zKSOHgKYjxQ41IE7sXNkPWnI41RygJVbm7mY.41cfW.rb
@@ -1,97 +1,118 @@
 
         
-            def initialize
-      @entries = []
-      @index = Set.new
-      @types = Hash.new { |hash, key| hash[key] = Type.new key }
-    end
+              if @verify_block
+        begin
+          @verify_block.call(value)
+        rescue => ex
+          UI.error('Error setting value '#{value}' for option '#{@key}'')
+          raise Interface::FastlaneError.new, ex.to_s
+        end
+      end
     
-        def insert_after(index, *names)
-      insert assert_index(index) + 1, *names
-    end
+        def run
+      program :name, 'cert'
+      program :version, Fastlane::VERSION
+      program :description, 'CLI for \'cert\' - Create new iOS code signing certificates'
+      program :help, 'Author', 'Felix Krause <cert@krausefx.com>'
+      program :help, 'Website', 'https://fastlane.tools'
+      program :help, 'Documentation', 'https://docs.fastlane.tools/actions/cert/'
+      program :help_formatter, :compact
     
-          response.success? && response.html? && process_url?(response.effective_url)
-    end
-    
-            # Allows setting options from a hash. By default this simply calls
-        # the `#{key}=` method on the config class with the value, which is
-        # the expected behavior most of the time.
-        #
-        # This is expected to mutate itself.
-        #
-        # @param [Hash] options A hash of options to set on this configuration
-        #   key.
-        def set_options(options)
-          options.each do |key, value|
-            send('#{key}=', value)
-          end
+            def preload_commit_authors
+          # This also preloads the author of every commit. We're using 'lazy_author'
+          # here since 'author' immediately loads the data on the first call.
+          @pipeline.commit.try(:lazy_author)
         end
     
-            # Defines additional communicators to be available. Communicators
-        # should be returned by a block passed to this method. This is done
-        # to ensure that the class is lazy loaded, so if your class inherits
-        # from or uses any Vagrant internals specific to Vagrant 1.0, then
-        # the plugin can still be defined without breaking anything in future
-        # versions of Vagrant.
-        #
-        # @param [String] name Communicator name.
-        def self.communicator(name=UNSET_VALUE, &block)
-          data[:communicator] ||= Registry.new
+            def id_for_already_imported_cache(note)
+          note.id
+        end
+      end
+    end
+  end
+end
+
     
-            # This returns all the registered configuration classes.
-        #
-        # @return [Hash]
-        def config
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.configs[:top])
-            end
-          end
+            def collection_method
+          :issues_comments
         end
     
-        p0 = Vec.new(isect.pl.x + eps * isect.n.x,
-                isect.pl.y + eps * isect.n.y,
-                isect.pl.z + eps * isect.n.z)
-    nphi.times do |j|
-      ntheta.times do |i|
-        r = rand
-        phi = 2.0 * 3.14159265 * rand
-        x = Math.cos(phi) * Math.sqrt(1.0 - r)
-        y = Math.sin(phi) * Math.sqrt(1.0 - r)
-        z = Math.sqrt(r)
+            # Builds a note from a GitHub API response.
+        #
+        # note - An instance of `Sawyer::Resource` containing the note details.
+        def self.from_api_response(note)
+          matches = note.html_url.match(NOTEABLE_TYPE_REGEX)
     
-    def kikaku(a)
-  a.collect {|x| x - a[0]}
-end
-def ud(a)
-  kikaku(a.collect {|x| ((x+NP)%ROW)-ROW*((x+NP)/ROW) }.sort)
-end
-def rl(a)
-  kikaku(a.collect {|x| ROW*((x+NP)/ROW)+ROW-((x+NP)%ROW)}.sort)
-end
-def xy(a)
-  kikaku(a.collect {|x| ROW*((x+NP)%ROW) + (x+NP)/ROW }.sort)
-end
-    
-      # staged_path not available in Installer/Uninstall Stanza, workaround by nesting with preflight/postflight
-  # see https://github.com/Homebrew/homebrew-cask/pull/8887
-  # and https://github.com/Homebrew/homebrew-cask-versions/pull/296
-    
-          expect(page).not_to have_content(order2.number)
+      describe '#highlighted?' do
+    it 'understands hl=6-8' do
+      stub(params).[](:hl) { '6-8' }
+      expect((1..10).select { |i| highlighted?(i) }).to eq [6, 7, 8]
     end
     
-          within_row(1) { click_icon :split }
-      wait_for_ajax
-      targetted_select2 'LA', from: '#s2id_item_stock_location'
-      click_icon :'save-split'
-      wait_for_ajax
-      expect(page.find('#shipment_#{order.shipments.first.id}')).to be_present
+          it 'runs until stop is called' do
+        mock.instance_of(Rufus::Scheduler).join
+        Thread.new { while @agent_runner.instance_variable_get(:@running) != false do sleep 0.1; @agent_runner.stop end }
+        @agent_runner.run
+      end
     
-              def ensure_order
-            raise ActiveRecord::RecordNotFound if spree_current_order.nil?
-          end
+      let :new_template do
+    {
+      'description' => '{{ hovertext }}',
+      'comment' => '{{ comment }}'
+    }
+  end
     
-          @@taxon_attributes = [
-        :id, :name, :pretty_name, :permalink, :parent_id,
-        :taxonomy_id, :meta_title, :meta_description
-      ]
+      describe '#log_for_agent' do
+    it 'creates AgentLogs' do
+      log = AgentLog.log_for_agent(agents(:jane_website_agent), 'some message', :level => 4, :outbound_event => events(:jane_website_agent_event))
+      expect(log).not_to be_new_record
+      expect(log.agent).to eq(agents(:jane_website_agent))
+      expect(log.outbound_event).to eq(events(:jane_website_agent_event))
+      expect(log.message).to eq('some message')
+      expect(log.level).to eq(4)
+    end
+    
+        it 'should raise error when response says unauthorized' do
+      stub(HTTParty).post { {'Response' => 'Not authorized'} }
+      expect { @checker.send_notification({}) }.to raise_error(StandardError, /Not authorized/)
+    end
+    
+    def to_integer(proc)
+  proc[-> n { n + 1 }][0]
+end
+    
+    Given /^I run a paperclip generator to add a paperclip '([^']*)' to the '([^']*)' model$/ do |attachment_name, model_name|
+  step %[I successfully run `rails generate paperclip #{model_name} #{attachment_name}`]
+end
+    
+    # This stuff needs to be run after Paperclip is defined.
+require 'paperclip/io_adapters/registry'
+require 'paperclip/io_adapters/abstract_adapter'
+require 'paperclip/io_adapters/empty_string_adapter'
+require 'paperclip/io_adapters/identity_adapter'
+require 'paperclip/io_adapters/file_adapter'
+require 'paperclip/io_adapters/stringio_adapter'
+require 'paperclip/io_adapters/data_uri_adapter'
+require 'paperclip/io_adapters/nil_adapter'
+require 'paperclip/io_adapters/attachment_adapter'
+require 'paperclip/io_adapters/uploaded_file_adapter'
+require 'paperclip/io_adapters/uri_adapter'
+require 'paperclip/io_adapters/http_url_proxy_adapter'
+
+    
+        def type_from_file_command
+      @type_from_file_command ||=
+        FileCommandContentTypeDetector.new(@filepath).detect
+    end
+  end
+end
+
+    
+        # Returns the width and height in a format suitable to be passed to Geometry.parse
+    def to_s
+      s = ''
+      s << width.to_i.to_s if width > 0
+      s << 'x#{height.to_i}' if height > 0
+      s << modifier.to_s
+      s
+    end
