c923fe3f4c8adcaeb6906e6d6d4c9998f5bcf76c
SUNsung <weatherius@gmail.com>
2019-04-27 09:37:05 +0300

    Rewrite [21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py] File in: 2019-04-27 09:37:04

--- pyton/21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py
+++ pyton/21NUTFkmOKF.215Okfg0X71Js21JAvEXS8uvsw213w..py
@@ -1,156 +1,37 @@
 
         
-            if RESULT and ('info_dict' not in test or 'age_limit' not in test['info_dict'] or
-                   test['info_dict']['age_limit'] != 18):
-        print('\nPotential missing age_limit check: {0}'.format(test['name']))
+            def load(self):
+        try:
+            session_data = self._cache.get(self.cache_key)
+        except Exception:
+            # Some backends (e.g. memcache) raise an exception on invalid
+            # cache keys. If this happens, reset the session. See #17810.
+            session_data = None
+        if session_data is not None:
+            return session_data
+        self._session_key = None
+        return {}
     
-    with open('update/versions.json', 'w') as jsonf:
-    json.dump(versions_info, jsonf, indent=4, sort_keys=True)
-
     
+class BaseSessionManager(models.Manager):
+    def encode(self, session_dict):
+        '''
+        Return the given session dictionary serialized and encoded as a string.
+        '''
+        session_store_class = self.model.get_session_store_class()
+        return session_store_class().encode(session_dict)
     
-def get_base_name(base):
-    if base is InfoExtractor:
-        return 'LazyLoadExtractor'
-    elif base is SearchInfoExtractor:
-        return 'LazyLoadSearchExtractor'
+        if section is not None:
+        if section not in sitemaps:
+            raise Http404('No sitemap available for section: %r' % section)
+        maps = [sitemaps[section]]
     else:
-        return base.__name__
-    
-    header = oldreadme[:oldreadme.index('# OPTIONS')]
-footer = oldreadme[oldreadme.index('# CONFIGURATION'):]
-    
-    import io
-import optparse
-import os
-import sys
+        maps = sitemaps.values()
+    page = request.GET.get('p', 1)
     
     
-from test.helper import FakeYDL
-from youtube_dl.cache import Cache
-    
-    
-if __name__ == '__main__':
-    unittest.main()
-
-    
-    
-def test_cce_one_hot():
-    y_a = K.variable(np.random.randint(0, 7, (5, 6)))
-    y_b = K.variable(np.random.random((5, 6, 7)))
-    objective_output = losses.sparse_categorical_crossentropy(y_a, y_b)
-    assert K.eval(objective_output).shape == (5, 6)
-    
-    
-def normalize(x, axis=-1, order=2):
-    '''Normalizes a Numpy array.
-    
-    # create two datasets one with digits below 5 and one with 5 and above
-x_train_lt5 = x_train[y_train < 5]
-y_train_lt5 = y_train[y_train < 5]
-x_test_lt5 = x_test[y_test < 5]
-y_test_lt5 = y_test[y_test < 5]
-    
-    Compares the performance of a simple MLP using two
-different activation functions: RELU and SELU
-on the Reuters newswire topic classification task.
-    
-    
-def mean_absolute_percentage_error(y_true, y_pred):
-    diff = K.abs((y_true - y_pred) / K.clip(K.abs(y_true),
-                                            K.epsilon(),
-                                            None))
-    return 100. * K.mean(diff, axis=-1)
-    
-        def testReadInto(self):
-        self.createTempFile()
-        with BZ2File(self.filename) as bz2f:
-            n = 128
-            b = bytearray(n)
-            self.assertEqual(bz2f.readinto(b), n)
-            self.assertEqual(b, self.TEXT[:n])
-            n = len(self.TEXT) - n
-            b = bytearray(len(self.TEXT))
-            self.assertEqual(bz2f.readinto(b), n)
-            self.assertEqual(b[:n], self.TEXT[-n:])
-    
-    
-if __name__ == '__main__':
-    main()
-
-    
-        counter = 1
-    for part in msg.walk():
-        # multipart/* are just containers
-        if part.get_content_maintype() == 'multipart':
-            continue
-        # Applications should really sanitize the given filename so that an
-        # email message can't be used to overwrite important files
-        filename = part.get_filename()
-        if not filename:
-            ext = mimetypes.guess_extension(part.get_content_type())
-            if not ext:
-                # Use a generic bag-of-bits extension
-                ext = '.bin'
-            filename = 'part-%03d%s' % (counter, ext)
-        counter += 1
-        with open(os.path.join(args.directory, filename), 'wb') as fp:
-            fp.write(part.get_payload(decode=True))
-    
-        def __init__(self):
-        self.trans = dict(dog='σκύλος', cat='γάτα')
-    
-    
-def main():
-    Jhon = Person('Jhon', 'Coder')
-    print(u'Name: {0}    Occupation: {1}'.format(Jhon.name, Jhon.occupation))
-    print(u'Before we access `relatives`:')
-    print(Jhon.__dict__)
-    print(u'Jhon's relatives: {0}'.format(Jhon.relatives))
-    print(u'After we've accessed `relatives`:')
-    print(Jhon.__dict__)
-    print(Jhon.parents)
-    print(Jhon.__dict__)
-    print(Jhon.parents)
-    print(Jhon.call_count2)
-    
-        value = 'default'
-    
-        def now(self):
-        current_time_is_always_midnight = '24:01'
-        return current_time_is_always_midnight
-
-    
-        def render(self):
-        return '<b>{}</b>'.format(self._wrapped.render())
-    
-    delim = text_type(args.delimiter)
-cutall = args.cutall
-hmm = args.hmm
-fp = open(args.filename, 'r') if args.filename else sys.stdin
-    
-    s = '此外，公司拟对全资子公司吉林欧亚置业有限公司增资4.3亿元，增资后，吉林欧亚置业注册资本由7000万元增加到5亿元。吉林欧亚置业主要经营范围为房地产开发及百货零售等业务。目前在建吉林欧亚城市商业综合体项目。2013年，实现营业收入0万元，实现净利润-139.13万元。'
-for x, w in jieba.analyse.extract_tags(s, withWeight=True):
-    print('%s %s' % (x, w))
-    
-    tags = jieba.analyse.extract_tags(content, topK=topK)
-    
-    
-    
-    # os.execv is broken on Windows and can't properly parse command line
-# arguments and executable name if they contain whitespaces. subprocess
-# fixes that behavior.
-_has_execv = sys.platform != 'win32'
-    
-        See `OptionParser.add_parse_callback`
-    '''
-    options.add_parse_callback(callback)
-    
-    For each function or class described in `tornado.platform.interface`,
-the appropriate platform-specific implementation exists in this module.
-Most code that needs access to this functionality should do e.g.::
-    
-    Template expressions are surrounded by double curly braces: ``{{ ... }}``.
-The contents may be any python expression, which will be escaped according
-to the current autoescape setting and inserted into the output.  Other
-template directives use ``{% %}``.
+def display_temp(hass: HomeAssistant, temperature: float, unit: str,
+                 precision: float) -> float:
+    '''Convert temperature into preferred units/precision for display.'''
+    temperature_unit = unit
+    ha_unit = hass.config.units.temperature_unit
