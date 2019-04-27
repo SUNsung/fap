71a211f0e602ce9cd20e48dad7024d8d2dd70cc9
SUNsung <weatherius@gmail.com>
2019-04-27 15:34:14 +0300

    Rewrite [53pejT8MI7aaI53LaG91vuGzMM5322SX1q46X9A53ulu.py] File in: 2019-04-27 15:34:13

--- pyton/53pejT8MI7aaI53LaG91vuGzMM5322SX1q46X9A53ulu.py
+++ pyton/53pejT8MI7aaI53LaG91vuGzMM5322SX1q46X9A53ulu.py
@@ -1,26 +1,268 @@
 
         
-            def process_appid_not_exist(self, appid, ip):
-        ret = self.check_api(ip, 'xxnet-1')
-        if ret and ret.ok:
-            self.set_appid_not_exist(appid)
+            def __init__(self):
+        self.head = None
+        self.tail = None
+    
+        def __init__(self, license_plate):
+        super(Car, self).__init__(VehicleSize.COMPACT, license_plate, spot_size=1)
+    
+    
+if __name__ == '__main__':
+    SpendingByCategory.run()
+
+    
+    
+no_such_subcommand_old = '''No such subcommand
+    
+    
+class ArrayMinLengthValidator(MinLengthValidator):
+    message = ngettext_lazy(
+        'List contains %(show_value)d item, it should contain no fewer than %(limit_value)d.',
+        'List contains %(show_value)d items, it should contain no fewer than %(limit_value)d.',
+        'limit_value')
+    
+            r = None
+        try:
+            r = Redirect.objects.get(site=current_site, old_path=full_path)
+        except Redirect.DoesNotExist:
+            pass
+        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
+            try:
+                r = Redirect.objects.get(
+                    site=current_site,
+                    old_path=request.get_full_path(force_append_slash=True),
+                )
+            except Redirect.DoesNotExist:
+                pass
+        if r is not None:
+            if r.new_path == '':
+                return self.response_gone_class()
+            return self.response_redirect_class(r.new_path)
+    
+        def exists(self, session_key):
+        return bool(session_key) and (self.cache_key_prefix + session_key) in self._cache
+    
+        def flush(self):
+        '''
+        Remove the current session data from the database and regenerate the
+        key.
+        '''
+        self.clear()
+        self.delete(self.session_key)
+        self._session_key = None
+
+    
+        def create_model_instance(self, data):
+        '''
+        Return a new instance of the session model object, which represents the
+        current session state. Intended to be used for saving the session data
+        to the database.
+        '''
+        return self.model(
+            session_key=self._get_or_create_session_key(),
+            session_data=self.encode(data),
+            expire_date=self.get_expiry_date(),
+        )
+    
+    
+def x_robots_tag(func):
+    @wraps(func)
+    def inner(request, *args, **kwargs):
+        response = func(request, *args, **kwargs)
+        response['X-Robots-Tag'] = 'noindex, noodp, noarchive'
+        return response
+    return inner
+    
+        fpath = os.path.join(path, 'test_batch')
+    x_test, y_test = load_batch(fpath)
+    
+        paths = []
+    for fname in files:
+        paths.append(get_file(fname,
+                              origin=base + fname,
+                              cache_subdir=dirname))
+    
+    
+def test_imdb():
+    # only run data download tests 20% of the time
+    # to speed up frequent testing
+    random.seed(time.time())
+    if random.random() > 0.8:
+        (x_train, y_train), (x_test, y_test) = imdb.load_data()
+        (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=40)
+        assert len(x_train) == len(y_train)
+        assert len(x_test) == len(y_test)
+        word_index = imdb.get_word_index()
+        assert isinstance(word_index, dict)
+    
+    
+class ConvRNN2D(RNN):
+    '''Base class for convolutional-recurrent layers.
+    
+    x = Conv2DTranspose(filters=1,
+                    kernel_size=kernel_size,
+                    padding='same')(x)
+    
+    
+def hinge(y_true, y_pred):
+    return K.mean(K.maximum(1. - y_true * y_pred, 0.), axis=-1)
+    
+        try:
+        changed = False
+        exists = False
+    
+        root = ET.fromstring(result)
+    
+    def main():
+    
+        for trials in range(5):
+        a = random.randrange(2, num - 1)
+        v = pow(a, s, num)
+        if v != 1:
+            i = 0
+            while v != (num - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i = i + 1
+                    v = (v ** 2) % num
+    return True
+    
+        def __solveDP(self, x, y):
+        if (x==-1):
+            return y+1
+        elif (y==-1):
+            return x+1
+        elif (self.dp[x][y]>-1):
+            return self.dp[x][y]
         else:
-            self.logger.warn('process_appid_not_exist, remove ip:%s', ip)
-    
-            if response.status == 503:
-            # out of quota
-            if 'gws' not in server_type and 'Google Frontend' not in server_type and 'GFE' not in server_type:
-                xlog.warn('503 but server type:%s', server_type)
-                return False
+            if (self.A[x]==self.B[y]):
+                self.dp[x][y] = self.__solveDP(x-1,y-1)
             else:
-                return True
+                self.dp[x][y] = 1+min(self.__solveDP(x,y-1), self.__solveDP(x-1,y), self.__solveDP(x-1,y-1))
     
-    Here is a little overview over the most commonly used classes provided by
-this runtime:
+        def train(self, X, y):
+        '''
+        train:
+        @param X: a one dimensional numpy array
+        @param y: a one dimensional numpy array. 
+        The contents of y are the labels for the corresponding X values
     
-    EOF_TOKEN = CommonToken(type=EOF)
-	
-INVALID_TOKEN = CommonToken(type=INVALID_TOKEN_TYPE)
+            a += a
+        b >>= 1
     
-                with open('htmlout.html', 'w') as out:
-                out.write(header)
+    
+# ALTERNATIVE METHODS  
+# ctbi= characters that must be in password
+# i= how many letters or characters the password length will be 
+def password_generator(ctbi, i):
+  # Password generator = full boot with random_number, random_letters, and random_character FUNCTIONS
+  pass  # Put your code here...
+    
+    __unittest = True
+    
+        def _generate_symbols(self, grammar_file, target_symbol_py_file):
+        proc = subprocess.Popen([sys.executable,
+                                 GEN_SYMBOL_FILE,
+                                 grammar_file,
+                                 target_symbol_py_file], stderr=subprocess.PIPE)
+        stderr = proc.communicate()[1]
+        return proc.returncode, stderr
+    
+        f2 = manager.Foo2()
+    f2.g()
+    f2._h()
+    assert not hasattr(f2, 'f')
+    assert sorted(f2._exposed_) == sorted(['g', '_h'])
+    
+    rcParams['mathtext.fontset'] = 'cm'
+    
+        result = s[lambda x: [True, False, True, True]]
+    tm.assert_series_equal(result, s.iloc[[0, 2, 3]])
+    
+    
+@pytest.fixture
+def three_group():
+    return DataFrame({'A': ['foo', 'foo', 'foo',
+                            'foo', 'bar', 'bar',
+                            'bar', 'bar',
+                            'foo', 'foo', 'foo'],
+                      'B': ['one', 'one', 'one',
+                            'two', 'one', 'one', 'one', 'two',
+                            'two', 'two', 'one'],
+                      'C': ['dull', 'dull', 'shiny',
+                            'dull', 'dull', 'shiny', 'shiny',
+                            'dull', 'shiny', 'shiny', 'shiny'],
+                      'D': np.random.randn(11),
+                      'E': np.random.randn(11),
+                      'F': np.random.randn(11)})
+
+    
+    
+@pytest.mark.parametrize('grps', [
+    ['qux'], ['qux', 'quux']])
+@pytest.mark.parametrize('vals', [
+    [2, 2, 8, 2, 6],
+    [pd.Timestamp('2018-01-02'), pd.Timestamp('2018-01-02'),
+     pd.Timestamp('2018-01-08'), pd.Timestamp('2018-01-02'),
+     pd.Timestamp('2018-01-06')]])
+@pytest.mark.parametrize('ties_method,ascending,pct,exp', [
+    ('average', True, False, [2., 2., 5., 2., 4.]),
+    ('average', True, True, [0.4, 0.4, 1.0, 0.4, 0.8]),
+    ('average', False, False, [4., 4., 1., 4., 2.]),
+    ('average', False, True, [.8, .8, .2, .8, .4]),
+    ('min', True, False, [1., 1., 5., 1., 4.]),
+    ('min', True, True, [0.2, 0.2, 1.0, 0.2, 0.8]),
+    ('min', False, False, [3., 3., 1., 3., 2.]),
+    ('min', False, True, [.6, .6, .2, .6, .4]),
+    ('max', True, False, [3., 3., 5., 3., 4.]),
+    ('max', True, True, [0.6, 0.6, 1.0, 0.6, 0.8]),
+    ('max', False, False, [5., 5., 1., 5., 2.]),
+    ('max', False, True, [1., 1., .2, 1., .4]),
+    ('first', True, False, [1., 2., 5., 3., 4.]),
+    ('first', True, True, [0.2, 0.4, 1.0, 0.6, 0.8]),
+    ('first', False, False, [3., 4., 1., 5., 2.]),
+    ('first', False, True, [.6, .8, .2, 1., .4]),
+    ('dense', True, False, [1., 1., 3., 1., 2.]),
+    ('dense', True, True, [1. / 3., 1. / 3., 3. / 3., 1. / 3., 2. / 3.]),
+    ('dense', False, False, [3., 3., 1., 3., 2.]),
+    ('dense', False, True, [3. / 3., 3. / 3., 1. / 3., 3. / 3., 2. / 3.]),
+])
+def test_rank_args(grps, vals, ties_method, ascending, pct, exp):
+    key = np.repeat(grps, len(vals))
+    vals = vals * len(grps)
+    df = DataFrame({'key': key, 'val': vals})
+    result = df.groupby('key').rank(method=ties_method,
+                                    ascending=ascending, pct=pct)
+    
+    import numpy as np
+    
+            print('Two or more elements')
+        data = [5, 1, 7, 2, 6, -3, 5, 7, -1]
+        assert_equal(merge_sort.sort(data), sorted(data))
+    
+        def get_sorted_stack(self, stack, numbers):
+        for x in numbers:
+            stack.push(x)
+        sorted_stack = stack.sort()
+        return sorted_stack
+    
+            print('Test: More than one element')
+        stack = Stack()
+        stack.push(1)
+        stack.push(2)
+        stack.push(3)
+        assert_equal(stack.pop(), 3)
+        assert_equal(stack.peek(), 2)
+        assert_equal(stack.pop(), 2)
+        assert_equal(stack.peek(), 1)
+        assert_equal(stack.is_empty(), False)
+        assert_equal(stack.pop(), 1)
+        assert_equal(stack.peek(), None)
+        assert_equal(stack.is_empty(), True)
+    
+        # TODO: It would be better if we had unit tests for each
+    # method in addition to the following end-to-end test
+    def test_end_to_end(self):
+        hash_table = HashTable(10)
