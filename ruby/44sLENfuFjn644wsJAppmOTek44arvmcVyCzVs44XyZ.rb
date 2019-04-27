bad9f657f412b2abf34ddca812df8c23a5fded40
SUNsung <weatherius@gmail.com>
2019-04-27 16:18:21 +0300

    Rewrite [44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb] File in: 2019-04-27 16:18:21

--- ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
+++ ruby/44sLENfuFjn644wsJAppmOTek44arvmcVyCzVs44XyZ.rb
@@ -1,65 +1,147 @@
 
         
-        require 'action_view/helpers/tags/collection_helpers'
+        FORWARD_SLASH = '/'.freeze
     
-                  Time.utc(
-                default[:year], default[:month], default[:day],
-                default[:hour], default[:min], default[:sec]
-              )
+    # No trailing slash
+Benchmark.ips do |x|
+  x.report('with body include?') { CONTENT_CONTAINING.include?('<body') }
+  x.report('with body regexp')   { CONTENT_CONTAINING =~ /<\s*body/ }
+  x.compare!
+end
+
+    
+    module Jekyll
+  module Commands
+    class NewTheme < Jekyll::Command
+      class << self
+        def init_with_program(prog)
+          prog.command(:'new-theme') do |c|
+            c.syntax 'new-theme NAME'
+            c.description 'Creates a new Jekyll theme scaffold'
+            c.option 'code_of_conduct', \
+                     '-c', '--code-of-conduct', \
+                     'Include a Code of Conduct. (defaults to false)'
+    
+          def site
+        @site_drop ||= SiteDrop.new(@obj)
+      end
+    
+        class Agents::DotBar < Agent
+      cannot_be_scheduled!
+    
+        it 'in the future' do
+      expect(relative_distance_of_time_in_words(Time.now+5.minutes)).to eq('in 5m')
+    end
+  end
+end
+
+    
+                scenario_import.merges = {
+              '0' => {
+                'name' => 'a new name',
+                'schedule' => '6pm',
+                'keep_events_for' => 2.days.to_i.to_s,
+                'disabled' => 'true',
+                'options' => weather_agent_options.merge('api_key' => 'foo').to_json
+              }
+            }
+    
+        it 'work with set FAILED_JOBS_TO_KEEP env variable' do
+      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(-1)
+      expect { @scheduler.send(:cleanup_failed_jobs!) }.to change(Delayed::Job, :count).by(0)
+      expect(@keep.id).to eq(Delayed::Job.order(:failed_at)[0].id)
+    end
+    
+          expect(Utils.unindent(<<-MD)).to eq('Hello World\nThis is\nnot indented')
+        Hello World
+        This is
+        not indented
+      MD
+    
+            def initialize(env, config)
+          @env    = env
+          @config = config
+        end
+    
+              # Set all of our instance variables on the new class
+          [self, other].each do |obj|
+            obj.instance_variables.each do |key|
+              # Ignore keys that start with a double underscore. This allows
+              # configuration classes to still hold around internal state
+              # that isn't propagated.
+              if !key.to_s.start_with?('@__')
+                # Don't set the value if it is the unset value, either.
+                value = obj.instance_variable_get(key)
+                result.instance_variable_set(key, value) if value != UNSET_VALUE
+              end
             end
           end
-      end
+    
+      if ENV['HOMEBREW_INTEGRATION_TEST']
+    command_name '#{ENV['HOMEBREW_INTEGRATION_TEST']} (#{$PROCESS_ID})'
+    
+      def intersect(ray, isect)
+    d = -@p.vdot(@n)
+    v = ray.dir.vdot(@n)
+    v0 = v
+    if v < 0.0 then
+      v0 = -v
+    end
+    if v0 < 1.0e-17 then
+      return
+    end
+    
+    $p = []
+$b = []
+$no = 0
+    
+    desc 'Clean out caches: .pygments-cache, .gist-cache, .sass-cache'
+task :clean do
+  rm_rf [Dir.glob('.pygments-cache/**'), Dir.glob('.gist-cache/**'), Dir.glob('.sass-cache/**'), 'source/stylesheets/screen.css']
+end
+    
+        def paragraphize(input)
+      '<p>#{input.lstrip.rstrip.gsub(/\n\n/, '</p><p>').gsub(/\n/, '<br/>')}</p>'
     end
   end
 end
-
     
-            private
-    
-    DOC_PATH = File.join(File.expand_path(__dir__), '_puppies', 'rover.md')
-COL_PATH = File.join(File.expand_path(__dir__), '_puppies')
-    
-    CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
-    
-      it 'imports a scenario which requires a service' do
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
-    click_on 'Start Import'
-    check('I confirm that I want to import these Agents.')
-    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
-    expect(page).to have_text('Import successful!')
-  end
-end
-
-    
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+          # Create an Atom-feed for each index.
+      feed = CategoryFeed.new(self, self.source, category_dir, category)
+      feed.render(self.layouts, site_payload)
+      feed.write(self.dest)
+      # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
+      self.pages << feed
     end
     
-      describe 'down' do
-    let :valid_options do
-      super().merge('extract' => new_extract,
-                    'template' => new_template)
+        def get_cached_gist(gist, file)
+      return nil if @cache_disabled
+      cache_file = get_cache_file_for gist, file
+      File.read cache_file if File.exist? cache_file
     end
     
-      describe '#receive' do
-    it 'sends a message' do
-      stub(HTTParty).post { {'id' => 1, 'message' => 'blah', 'title' => 'blah','source_name' => 'Custom Notification'} }
-      @checker.receive([@event])
-    end
-    
-          def stack
-        UI::ErrorReport.stack
+      class IncludeArrayTag < Liquid::Tag
+    Syntax = /(#{Liquid::QuotedFragment}+)/
+    def initialize(tag_name, markup, tokens)
+      if markup =~ Syntax
+        @array_name = $1
+      else
+        raise SyntaxError.new('Error in tag 'include_array' - Valid syntax: include_array [array from _config.yml]')
       end
+    
+    # Add our files (should be in the current directory):
+package.input('my-executable=/usr/bin/')
+package.input('my-library.so=/usr/lib/')
+    
+        # Create the .txz package archive from the files in staging_path.
+    File.open(output_path, 'wb') do |file|
+      XZ::StreamWriter.new(file) do |xz|
+        FPM::Util::TarWriter.new(xz) do |tar|
+          # The manifests must come first for pkg.
+          add_path(tar, '+COMPACT_MANIFEST',
+                   File.join(staging_path, '+COMPACT_MANIFEST'))
+          add_path(tar, '+MANIFEST',
+                   File.join(staging_path, '+MANIFEST'))
+    
+        args = [ '-B', build_path('build-info'), '-c', build_path('comment'), '-d', build_path('description'), '-f', build_path('packlist'), '-I', '/opt/local', '-p', staging_path,  '-U', '#{cwd}/#{name}-#{self.version}-#{iteration}.tgz' ]
+    safesystem('pkg_create', *args)
