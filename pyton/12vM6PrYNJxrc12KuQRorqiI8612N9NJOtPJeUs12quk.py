b002741e1725cbdb71d22a76396a3dc7e44a10f9
SUNsung <weatherius@gmail.com>
2019-04-27 08:22:23 +0300

    Rewrite [12vM6PrYNJxrc12KuQRorqiI8612N9NJOtPJeUs12quk.py] File in: 2019-04-27 08:22:23

--- pyton/12vM6PrYNJxrc12KuQRorqiI8612N9NJOtPJeUs12quk.py
+++ pyton/12vM6PrYNJxrc12KuQRorqiI8612N9NJOtPJeUs12quk.py
@@ -1,109 +1,42 @@
 
         
-        
-@pytest.fixture(params=containers)
-def proc(request, spawnu, TIMEOUT):
-    proc = spawnu(*request.param)
-    proc.sendline(u'pip install /src')
-    assert proc.expect([TIMEOUT, u'Successfully installed'])
-    proc.sendline(u'tcsh')
-    proc.sendline(u'setenv PYTHONIOENCODING utf8')
-    proc.sendline(u'eval `thefuck --alias`')
-    return proc
+            def __init__(self):
+        self.name = 'citext'
+    
+        def _get_session_from_db(self):
+        try:
+            return self.model.objects.get(
+                session_key=self.session_key,
+                expire_date__gt=timezone.now()
+            )
+        except (self.model.DoesNotExist, SuspiciousOperation) as e:
+            if isinstance(e, SuspiciousOperation):
+                logger = logging.getLogger('django.security.%s' % e.__class__.__name__)
+                logger.warning(str(e))
+            self._session_key = None
+    
+        # Output processing
+    def get_formatters(self):
+        return [plugin for plugin in self
+                if issubclass(plugin, FormatterPlugin)]
+    
+            :param chunk: A chunk of response body data that has just
+                      been downloaded and written to the output.
+        :type chunk: bytes
     
     
-@pytest.mark.functional
-def test_how_to_configure_alias(proc, TIMEOUT):
-    proc.sendline(u'unfunction fuck')
-    how_to_configure(proc, TIMEOUT)
+def softsign(x):
+    '''
+    o = x / (1 + abs(x))
+    '''
+    return tf.nn.softsign(x)
+    
+        return o
 
     
-    You can download from:
-  https://osxfuse.github.io/
-Error: An unsatisfied requirement failed this build.'''
+            self._built = False
     
-    no_such_subcommand = '''error: no such subcommand
+    import tensorflow as tf
+import keras.backend as K
     
     
-class TestLookupDict:
-    
-            Keyword:                   'bold #004461',   # class: 'k'
-        Keyword.Constant:          'bold #004461',   # class: 'kc'
-        Keyword.Declaration:       'bold #004461',   # class: 'kd'
-        Keyword.Namespace:         'bold #004461',   # class: 'kn'
-        Keyword.Pseudo:            'bold #004461',   # class: 'kp'
-        Keyword.Reserved:          'bold #004461',   # class: 'kr'
-        Keyword.Type:              'bold #004461',   # class: 'kt'
-    
-    logging.getLogger(__name__).addHandler(NullHandler())
-    
-        def test_jwk_serialize(self):
-        from acme.jws import JWS
-        jws = JWS.sign(payload=b'foo', key=self.privkey,
-                       alg=jose.RS256, nonce=self.nonce,
-                       url=self.url)
-        self.assertEqual(jws.signature.combined.kid, None)
-        self.assertEqual(jws.signature.combined.jwk, self.pubkey)
-    
-    # -- Options for LaTeX output ---------------------------------------------
-    
-            self.vhost1 = VirtualHost(
-            'filep', 'vh_path', set([self.addr1]), False, False, 'localhost')
-    
-    # http://docs.readthedocs.org/en/latest/theme.html#how-do-i-use-this-locally-and-on-read-the-docs
-# on_rtd is whether we are on readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-# otherwise, readthedocs.org uses their theme by default, so no need to specify it
-    
-            for project in RangeQuerySetWrapperWithProgressBar(orm.Project.objects.all()):
-            orm.Environment.objects.filter(
-                project_id=project.id, organization_id__isnull=True
-            ).update(organization_id=project.organization_id)
-    
-            # Adding unique constraint on 'Distribution', fields ['release', 'name']
-        db.create_unique('sentry_distribution', ['release_id', 'name'])
-    
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-        match = SYNTAX_GROUP_REGEX.search( line )
-    if match:
-      if looking_for_group:
-        looking_for_group = False
-      else:
-        group_name_to_group[ current_group.name ] = current_group
-    
-          # We set up a fake response.
-      with patch( 'ycm.client.base_request._JsonFromFuture',
-                  side_effect = response_method ):
-        yield
-    
-        def _adjust_thread_count(self):
-        # When the executor gets lost, the weakref callback will wake up
-        # the worker threads.
-        def weakref_cb(_, q=self._work_queue):
-            q.put(None)
-        # TODO(bquinlan): Should avoid creating new threads if there are more
-        # idle threads than items in the work queue.
-        if len(self._threads) < self._max_workers:
-            t = threading.Thread(target=_worker,
-                                 args=(weakref.ref(self, weakref_cb),
-                                       self._work_queue))
-            t.daemon = True
-            t.start()
-            self._threads.add(t)
-            _threads_queues[t] = self._work_queue
-    
-            self.assertEqual(set([CANCELLED_AND_NOTIFIED_FUTURE,
-                              EXCEPTION_FUTURE,
-                              SUCCESSFUL_FUTURE,
-                              future1]), finished)
-        self.assertEqual(set([future2]), pending)
