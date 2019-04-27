b8c5733c96ef702e0fb581f436acf059f2c4dac4
SUNsung <weatherius@gmail.com>
2019-04-27 20:20:30 +0300

    Rewrite [05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb] File in: 2019-04-27 20:20:29

--- ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
+++ ruby/05X2vWEctN5Wg05EZS5pA.gJuI05PPQUPUWpkNM05QBJ.rb
@@ -1,92 +1,59 @@
 
         
-            def empty?
-      @entries.empty?
+                  if options['multiple']
+            add_default_name_and_id_for_value(@checked_value, options)
+            options.delete('multiple')
+          else
+            add_default_name_and_id(options)
+          end
+    
+              def hidden_field
+            hidden_name = @html_options[:name] || hidden_field_name
+            @template_object.hidden_field_tag(hidden_name, '', id: nil)
+          end
+    
+        def set_email_domain_block
+      @email_domain_block = EmailDomainBlock.find(params[:id])
     end
     
-            css('tr[style]').each do |node|
-          node.remove_attribute 'style'
-        end
-    
-            subtitle = at_css('.hero-subtitle').try(:content)
-        breadcrumbs = css('.breadcrumbs li').map(&:content)[2..-2]
-    
-            # Upload a file to the remote machine.
-        #
-        # @param [String] from Path of the file locally to upload.
-        # @param [String] to Path of where to save the file on the remote
-        #   machine.
-        def upload(from, to)
-        end
-    
-              @commands = Registry.new
-          @configs = Hash.new { |h, k| h[k] = Registry.new }
-          @guests  = Registry.new
-          @guest_capabilities = Hash.new { |h, k| h[k] = Registry.new }
-          @hosts   = Registry.new
-          @host_capabilities = Hash.new { |h, k| h[k] = Registry.new }
-          @providers = Registry.new
-          @provider_capabilities = Hash.new { |h, k| h[k] = Registry.new }
-          @pushes = Registry.new
-          @synced_folders = Registry.new
-        end
+            render template: 'admin/reports/show'
       end
     end
+    
+      def process_push_request
+    case hub_mode
+    when 'subscribe'
+      Pubsubhubbub::SubscribeService.new.call(account_from_topic, hub_callback, hub_secret, hub_lease_seconds, verified_domain)
+    when 'unsubscribe'
+      Pubsubhubbub::UnsubscribeService.new.call(account_from_topic, hub_callback)
+    else
+      ['Unknown mode: #{hub_mode}', 422]
+    end
   end
-end
+    
+      def create
+    active_session = current_session
+    
+      def update
+    setting.data = params[:data]
+    setting.save!
+    
+      private
+    
+      private
+    
+    ENV['COCOAPODS_DISABLE_STATS'] = 'true'
 
     
-            # Allows setting options from a hash. By default this simply calls
-        # the `#{key}=` method on the config class with the value, which is
-        # the expected behavior most of the time.
-        #
-        # This is expected to mutate itself.
-        #
-        # @param [Hash] options A hash of options to set on this configuration
-        #   key.
-        def set_options(options)
-          options.each do |key, value|
-            send('#{key}=', value)
-          end
-        end
-    
-            # Initialize the provider to represent the given machine.
-        #
-        # @param [Vagrant::Machine] machine The machine that this provider
-        #   is responsible for.
-        def initialize(machine)
-        end
-    
-      def symlinked?(symlink_path, target_path)
-    '[ #{symlink_path} -ef #{target_path} ]'
+    class FPM::Package::NPM < FPM::Package
+  class << self
+    include FPM::Util
   end
+  # Flags '--foo' will be accessable  as attributes[:npm_foo]
+  option '--bin', 'NPM_EXECUTABLE',
+    'The path to the npm executable you wish to run.', :default => 'npm'
     
-        # allows the `cap install` task to load without a capfile
-    def find_rakefile_location
-      if (location = super).nil?
-        [capfile, Dir.pwd]
-      else
-        location
-      end
-    end
-    
-    http://capistranorb.com/documentation/advanced-features/custom-scm
-    
-          def self.[](host)
-        host.is_a?(Server) ? host : new(host)
-      end
-    
-          # rubocop:disable Security/MarshalLoad
-      def add_role(role, hosts, options={})
-        options_deepcopy = Marshal.dump(options.merge(roles: role))
-        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
-      end
-      # rubocop:enable Security/MarshalLoad
-    
-          # Calls the given block for each `pair` node in the `hash` literal.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_pair
-        return each_child_node(:pair).to_enum unless block_given?
+      def initialize(package_name, opts = {}, &block)
+    @options = OpenStruct.new(:name => package_name.to_s)
+    @source, @target = opts.values_at(:source, :target).map(&:to_s)
+    @directory = File.expand_path(opts[:directory].to_s)
