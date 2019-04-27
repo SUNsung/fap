993baae80e66c01c348d200428e5c769c5f78d77
SUNsung <weatherius@gmail.com>
2019-04-27 13:17:06 +0300

    Rewrite [29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py] File in: 2019-04-27 13:17:05

--- pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
+++ pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
@@ -1,154 +1,206 @@
 
         
-            # apply the blueprints to the app
-    from flaskr import auth, blog
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+        import codecs
+import subprocess
     
-            if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
+    README_FILE = 'README.md'
+helptext = sys.stdin.read()
     
-            #: a reference to the blueprint that created this setup state.
-        self.blueprint = blueprint
+        params = {
+        'age_limit': age,
+        'skip_download': True,
+        'writeinfojson': True,
+        'outtmpl': '%(id)s.%(ext)s',
+    }
+    ydl = YoutubeDL(params)
+    ydl.add_default_info_extractors()
+    json_filename = os.path.splitext(filename)[0] + '.info.json'
+    try_rm(json_filename)
+    ydl.download([url])
+    res = os.path.exists(json_filename)
+    try_rm(json_filename)
+    return res
     
-        info(
-        'Releasing %s (codename %s, release date %s)',
-        version, codename, release_date.strftime('%d/%m/%Y')
+        def test_allsubtitles(self):
+        self.DL.params['writesubtitles'] = True
+        self.DL.params['allsubtitles'] = True
+        subtitles = self.getSubtitles()
+        self.assertEqual(set(subtitles.keys()), set(['en']))
+        self.assertEqual(md5(subtitles['en']), '97e7670cbae3c4d26ae8bcc7fdd78d4b')
+    
+    # Snapshot of volume with tagging
+- ec2_snapshot:
+    instance_id: i-12345678
+    device_name: /dev/sdb1
+    snapshot_tags:
+        frequency: hourly
+        source: /data
+    
+    EXAMPLES = '''
+# Add or change a subnet group
+- elasticache_subnet_group:
+    state: present
+    name: norwegian-blue
+    description: My Fancy Ex Parrot Subnet Group
+    subnets:
+      - subnet-aaaaaaaa
+      - subnet-bbbbbbbb
+    
+        elif state == 'present':
+        for param in ('name', 'rules'):
+            if not module.params.get(param):
+                module.fail_json(
+                    msg='%s parameter is required for new firewall policies.' % param)
+        try:
+            (changed, firewall_policy) = create_firewall_policy(module, oneandone_conn)
+        except Exception as e:
+            module.fail_json(msg=str(e))
+    
+    # Make coding more python3-ish
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+    
+    
+DOCUMENTATION = '''
+---
+module: ipa_role
+author: Thomas Krahn (@Nosmoht)
+short_description: Manage FreeIPA role
+description:
+- Add, modify and delete a role within FreeIPA server using FreeIPA API
+options:
+  cn:
+    description:
+    - Role name.
+    - Can not be changed as it is the unique identifier.
+    required: true
+    aliases: ['name']
+  description:
+    description:
+    - A description of this role-group.
+  group:
+    description:
+    - List of group names assign to this role.
+    - If an empty list is passed all assigned groups will be unassigned from the role.
+    - If option is omitted groups will not be checked or changed.
+    - If option is passed all assigned groups that are not passed will be unassigned from the role.
+  host:
+    description:
+    - List of host names to assign.
+    - If an empty list is passed all assigned hosts will be unassigned from the role.
+    - If option is omitted hosts will not be checked or changed.
+    - If option is passed all assigned hosts that are not passed will be unassigned from the role.
+  hostgroup:
+    description:
+    - List of host group names to assign.
+    - If an empty list is passed all assigned host groups will be removed from the role.
+    - If option is omitted host groups will not be checked or changed.
+    - If option is passed all assigned hostgroups that are not passed will be unassigned from the role.
+  privilege:
+    description:
+    - List of privileges granted to the role.
+    - If an empty list is passed all assigned privileges will be removed.
+    - If option is omitted privileges will not be checked or changed.
+    - If option is passed all assigned privileges that are not passed will be removed.
+    version_added: '2.4'
+  service:
+    description:
+    - List of service names to assign.
+    - If an empty list is passed all assigned services will be removed from the role.
+    - If option is omitted services will not be checked or changed.
+    - If option is passed all assigned services that are not passed will be removed from the role.
+  state:
+    description: State to ensure
+    default: 'present'
+    choices: ['present', 'absent']
+  user:
+    description:
+    - List of user names to assign.
+    - If an empty list is passed all assigned users will be removed from the role.
+    - If option is omitted users will not be checked or changed.
+extends_documentation_fragment: ipa.documentation
+version_added: '2.3'
+'''
+    
+    
+def main():
+    arg_spec = dict(
+        name=dict(required=True),
+        timeout=dict(default=300, type='int'),
+        state=dict(required=True, choices=['present', 'started', 'restarted', 'stopped', 'monitored', 'unmonitored', 'reloaded'])
     )
-    tags = get_git_tags()
     
-            X_test = X[-n_test_samples:]
-        Y_test = Y[-n_test_samples:]
-        X = X[:(i * step)]
-        Y = Y[:(i * step)]
     
-    ###############################################################################
-clf = SGDRegressor(penalty='l1', alpha=.2, fit_intercept=True, max_iter=2000,
-                   tol=None)
-clf.fit(X_train, y_train)
-print('model sparsity: %f' % sparsity_ratio(clf.coef_))
+def do_send_request(module, url, params, key):
+    data = json.dumps(params)
+    headers = {
+        'Content-Type': 'application/json',
+        'x-stackdriver-apikey': key
+    }
+    response, info = fetch_url(module, url, headers=headers, data=data, method='POST')
+    if info['status'] != 200:
+        module.fail_json(msg='Unable to send msg: %s' % info['msg'])
     
-        url_fmt is along the lines of ('https://github.com/USER/PROJECT/'
-                                   'blob/{revision}/{package}/'
-                                   '{path}#L{lineno}')
-    '''
-    revision = _get_git_revision()
-    return partial(_linkcode_resolve, revision=revision, package=package,
-                   url_fmt=url_fmt)
+        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
+    plt.plot(sample_sizes, one_core, label='one core')
+    plt.plot(sample_sizes, multi_core, label='multi core')
+    plt.xlabel('n_samples')
+    plt.ylabel('Time (s)')
+    plt.title('Parallel %s' % func.__name__)
+    plt.legend()
+    
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
 
     
-    The dataset is the Boston Housing dataset (resp. 20 Newsgroups) for
-regression (resp. classification).
+        # import matplotlib.pyplot as plt
+    # plt.matshow(cm)
+    # plt.show()
+
     
-    '''
+    # original face
+plt.figure(1, figsize=(3, 2.2))
+plt.imshow(face, cmap=plt.cm.gray, vmin=vmin, vmax=256)
+    
+    We will cluster a set of data, first with KMeans and then with
+MiniBatchKMeans, and plot the results.
+We will also plot the points that are labelled differently between the two
+algorithms.
+'''
 print(__doc__)
     
-    from sklearn.cluster import KMeans
-from sklearn import datasets
-    
-            for i in self.tree.iterfind('video/quality'):
-            quality = i.attrib ['value']
-            url = i[0].attrib['playurl']
-            self.stream_types.append({'id': quality,
-                                      'video_profile': i.attrib ['desp']})
-            self.streams[quality] = {'url': url,
-                                     'video_profile': i.attrib ['desp']}
-            self.streams_sorted = [dict([('id', stream_type['id'])] + list(self.streams[stream_type['id']].items())) for stream_type in self.__class__.stream_types if stream_type['id'] in self.streams]
+    from ._base import Detector, dlib, logger
     
     
-def cntv_download_by_id(rid, **kwargs):
-    CNTV().download_by_vid(rid, **kwargs)
+class Model(OriginalModel):
+    ''' Villain Faceswap Model '''
+    def __init__(self, *args, **kwargs):
+        logger.debug('Initializing %s: (args: %s, kwargs: %s',
+                     self.__class__.__name__, args, kwargs)
     
+            else:
+            if keep_original:
+                def process_file(src, dst, changes):  # pylint: disable=unused-argument
+                    ''' Process file method if not logging changes
+                        and keeping original '''
+                    copyfile(src, dst)
     
-def download_url(url, merge, output_dir, title, info_only):
-    mime, ext, size = url_info(url)
-    print_info(site_info, title, mime, size)
-    if not info_only:
-        download_urls([url], title, ext, size, output_dir, merge=merge)
-    
-        if re.match(re_str, url):
-        html = get_content(url)
-        title = match1(html, r'<title>(.+)丨(.+)</title>')  #title
-        
-        # player loaded via internal iframe
-        frame_url = re.search(r'<iframe src=\'(.+?)\'', html).group(1)
-        logging.debug('dilidili_download: %s' % frame_url)
-        
-        #https://player.005.tv:60000/?vid=a8760f03fd:a04808d307&v=yun&sign=a68f8110cacd892bc5b094c8e5348432
-        html = get_content(frame_url, headers=headers, decoded=False).decode('utf-8')
-        
-        match = re.search(r'(.+?)var video =(.+?);', html)
-        vid = match1(html, r'var vid='(.+)'')
-        hd2 = match1(html, r'var hd2='(.+)'')
-        typ = match1(html, r'var typ='(.+)'')
-        sign = match1(html, r'var sign='(.+)'')
-        tmsign = match1(html, r'tmsign=([A-Za-z0-9]+)')
-        ulk =  match1(html, r'var ulk='(.+)'')
-    
-    
-def fantasy_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    if 'fantasy.tv' not in url:
-        raise Exception('Wrong place!')
-    
-            while self.values[new_key] is not None \
-                and self.values[new_key] != key:
-            i += 1
-            new_key = self.hash_function(key + i*i) if not \
-                self.balanced_factor() >= self.lim_charge else None
-    
-            print()
-        print('The minimum Edit Distance is: %d' % (solver.solve(S1, S2)))
-        print()
-        print('*************** End of Testing Edit Distance DP Algorithm ***************')
-
-    
-    def longestSub(ARRAY): 			#This function is recursive
-	
-	ARRAY_LENGTH = len(ARRAY)
-	if(ARRAY_LENGTH <= 1):  	#If the array contains only one element, we return it (it's the stop condition of recursion)
-		return ARRAY
-								#Else
-	PIVOT=ARRAY[0]
-	isFound=False
-	i=1
-	LONGEST_SUB=[]
-	while(not isFound and i<ARRAY_LENGTH):
-		if (ARRAY[i] < PIVOT):
-			isFound=True
-			TEMPORARY_ARRAY = [ element for element in ARRAY[i:] if element >= ARRAY[i] ]
-			TEMPORARY_ARRAY = longestSub(TEMPORARY_ARRAY)
-			if ( len(TEMPORARY_ARRAY) > len(LONGEST_SUB) ):
-				LONGEST_SUB = TEMPORARY_ARRAY
-		else:
-			i+=1
-    
-    The problem is  :
-Given an array, to find the longest and continuous sub array and get the max sum of the sub array in the given array.
-'''
-from __future__ import print_function
-    
-        for i in range(1, n+1):
-        for j in range(1, s+1):
-            dp[i][j]= dp[i][j-1]
-    
-    def compute_heterogeneity(data, k, centroids, cluster_assignment):
-    
-    heterogeneity = 0.0
-    for i in range(k):
-        
-        # Select all data points that belong to cluster i. Fill in the blank (RHS only)
-        member_data_points = data[cluster_assignment==i, :]
-        
-        if member_data_points.shape[0] > 0: # check if i-th cluster is non-empty
-            # Compute distances from centroid to data points (RHS only)
-            distances = pairwise_distances(member_data_points, [centroids[i]], metric='euclidean')
-            squared_distances = distances**2
-            heterogeneity += np.sum(squared_distances)
-        
-    return heterogeneity
-    
-        for uncommonLetter in ETAOIN[-6:]:
-        if uncommonLetter in freqOrder[-6:]:
-            matchScore += 1
+        def graph_build(self, frame):
+        ''' Build the graph in the top right paned window '''
+        logger.debug('Building Graph')
+        self.graph = SessionGraph(frame,
+                                  self.display_data,
+                                  self.vars['display'].get(),
+                                  self.vars['scale'].get())
+        self.graph.pack(expand=True, fill=tk.BOTH)
+        self.graph.build()
+        self.graph_initialised = True
+        logger.debug('Built Graph')
