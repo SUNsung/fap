7a909d526fc1c7c2f385b0a8c78bbf3de4c97d64
SUNsung <weatherius@gmail.com>
2019-04-28 01:26:24 +0300

    Rewrite [51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py] File in: 2019-04-28 01:26:23

--- pyton/51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py
+++ pyton/51pPwVy8s0y.651VpFYHAHdgTQ51PQgLy5fhB251rAe.py
@@ -1,103 +1,134 @@
 
         
-        
-def main():
-    module = AnsibleModule(argument_spec=dict(
-        view=dict(choices=['topics', 'subscriptions'], default='topics'),
-        topic=dict(required=False),
-        state=dict(choices=['list'], default='list'),
-        service_account_email=dict(),
-        credentials_file=dict(),
-        project_id=dict(), ),)
+          wname = (name + '/W') if name else '/W'
     
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.gcp import check_min_pkg_version, get_google_cloud_credentials
-from ansible.module_utils.six import string_types
-    
-            if add_rules:
-            firewall_policy = _add_firewall_rules(module,
-                                                  oneandone_conn,
-                                                  firewall_policy['id'],
-                                                  add_rules)
-            _check_mode(module, firewall_policy)
-            changed = True
+        fname = os.path.join(FLAGS.save_dir, 'lstm_emb_step_%d.npy' % i)
+    with tf.gfile.Open(fname, mode='w') as f:
+      np.save(f, lstm_emb)
+    sys.stderr.write('LSTM embedding step %d file saved\n' % i)
     
     
-# ===========================================
-# Module execution.
-#
+def parse_commonsense_reasoning_test(test_data_name):
+  '''Read JSON test data.'''
+  with tf.gfile.Open(os.path.join(
+      FLAGS.data_dir, 'commonsense_test',
+      '{}.json'.format(test_data_name)), 'r') as f:
+    data = json.load(f)
     
-        # Using a for loop in case of error, we can report the package that failed
-    for log in logs:
-        # Query the log first, to see if we even need to remove.
-        if not query_log_status(module, le_path, log):
-            continue
+      for n in xrange(num_batches):
+    indices_batch = indices[n]
+    new_arr_batch = new_arr[n]
+    
+            # init attention
+        attention = _init_attention(encoder_state)
+      else:
+        # construct attention
+        attention = attention_construct_fn(cell_output, attention_keys,
+                                           attention_values)
+        cell_output = attention
+    
+    with open('update/LATEST_VERSION', 'w') as f:
+    f.write(version)
+    
+        def gen_ies_md(ies):
+        for ie in ies:
+            ie_md = '**{0}**'.format(ie.IE_NAME)
+            ie_desc = getattr(ie, 'IE_DESC', None)
+            if ie_desc is False:
+                continue
+            if ie_desc is not None:
+                ie_md += ': {0}'.format(ie.IE_DESC)
+            if not ie.working():
+                ie_md += ' (Currently broken)'
+            yield ie_md
+    
+    ZSH_COMPLETION_FILE = 'youtube-dl.zsh'
+ZSH_COMPLETION_TEMPLATE = 'devscripts/zsh-completion.in'
+    
+        def test_encrypt(self):
+        msg = b'message'
+        key = list(range(16))
+        encrypted = aes_encrypt(bytes_to_intlist(msg), key)
+        decrypted = intlist_to_bytes(aes_decrypt(encrypted, key))
+        self.assertEqual(decrypted, msg)
     
     
-def main():
-    global module
-    module = AnsibleModule(
-        argument_spec=dict(
-            device=dict(required=True, aliases=['host', 'name']),
-            landscape=dict(required=True),
-            state=dict(choices=['present', 'absent'], default='present'),
-            community=dict(required=True, no_log=True),
-            agentport=dict(type='int', default=161),
-            url=dict(required=True, aliases=['oneclick_url']),
-            url_username=dict(required=True, aliases=['oneclick_user']),
-            url_password=dict(required=True, no_log=True, aliases=['oneclick_password']),
-            use_proxy=dict(type='bool', default='yes'),
-            validate_certs=dict(type='bool', default='yes'),
-        ),
-        required_if=[('state', 'present', ['community'])],
-        supports_check_mode=True
-    )
-    
-        def __enter__(self):
-        from scrapy.utils.test import get_testenv
-        pargs = [sys.executable, '-u', '-m', 'scrapy.utils.benchserver']
-        self.proc = subprocess.Popen(pargs, stdout=subprocess.PIPE,
-                                     env=get_testenv())
-        self.proc.stdout.readline()
-    
-    logger = logging.getLogger(__name__)
-    
-            if self.crawler_process.bootstrap_failed:
-            self.exitcode = 1
+if __name__ == '__main__':
+    unittest.main()
 
     
+        def test_proxy_http(self):
+        params = self._check_params(['primary_proxy', 'primary_server_ip'])
+        if params is None:
+            return
+        ydl = FakeYDL({
+            'proxy': params['primary_proxy']
+        })
+        self.assertEqual(
+            ydl.urlopen('http://yt-dl.org/ip').read().decode('utf-8'),
+            params['primary_server_ip'])
     
-def run_test(setup, test, iterations_per_test=5, tests_to_run=10):
-    fastest_execution = min(timeit.Timer(test, setup=setup).repeat(tests_to_run, iterations_per_test))
-    execution_time = fastest_execution / iterations_per_test
-    fps = 1.0 / execution_time
-    return execution_time, fps
+            for deferred in self.deferred_functions:
+            deferred(state)
     
-        # Gloss the lips
-    d.polygon(face_landmarks['top_lip'], fill=(150, 0, 0, 128))
-    d.polygon(face_landmarks['bottom_lip'], fill=(150, 0, 0, 128))
-    d.line(face_landmarks['top_lip'], fill=(150, 0, 0, 64), width=8)
-    d.line(face_landmarks['bottom_lip'], fill=(150, 0, 0, 64), width=8)
+    for i, n in enumerate(n_samples):
+    for j, p in enumerate(n_features):
+        X = np.random.normal(size=(n, p))
+        t0 = time.time()
+        ward.fit(X)
+        scikits_time[j, i] = time.time() - t0
+        t0 = time.time()
+        hierarchy.ward(X)
+        scipy_time[j, i] = time.time() - t0
     
-        function_parameters = zip(
-        images_to_check,
-        itertools.repeat(model),
-    )
+        package is the name of the root module of the package
     
-        # Loop over each face found in the frame to see if it's someone we know.
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
-        name = '<Unknown Person>'
+    import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
     
-        def test_cnn_face_locations(self):
-        img = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
-        detected_faces = api.face_locations(img, model='cnn')
+        # TASK: Build a grid search to find out whether unigrams or bigrams are
+    # more useful.
+    # Fit the pipeline on the training set using grid search for the parameters
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2)],
+    }
+    grid_search = GridSearchCV(pipeline, parameters, n_jobs=-1)
+    grid_search.fit(docs_train, y_train)
     
-    requirements = [
-    'face_recognition_models>=0.3.0',
-    'Click>=6.0',
-    'dlib>=19.7',
-    'numpy',
-    'Pillow'
-]
+    The outer product of the row and column label vectors shows a
+representation of the checkerboard structure.
+    
+    n_clusters = 3
+    
+    for center_1, center_2 in zip(regular_values[:-1], regular_values[1:]):
+    plt.axvline(.5 * (center_1 + center_2), color='b', linestyle='--')
+    
+        changed = True  # 标记样本类别是否改变
+    n_iter = 0  # 记录迭代次数
+    while changed and n_iter < max_iter:
+        changed = False
+        n_iter += 1
+    
+    
+def hard_sigmoid(x):
+    '''
+    x = 0.                  x < -2.5
+      = 1.                  x > 2.5
+      = 0.2 * x + 0.5       otherwise
+    '''
+    x = (0.2 * x) + 0.5
+    x = tf.clip_by_value(x, 0., 1.)
+    return x
+    
+        return x
+    
+        #Required strings to create intermediate HTML files
+    header = '<html><head><link rel=stylesheet type=text/css href=' + colorscheme + '.css></head><body>\n'
+    footer = '</body></html>'
+    title_content = '<h1 class=titlemain>tldr pages</h1><h4 class=titlesub>Simplified and community driven man pages</h4></body></html>'
