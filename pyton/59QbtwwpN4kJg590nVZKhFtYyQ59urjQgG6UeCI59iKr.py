3c4352827b54691b58ddcf147846e6658930cb21
SUNsung <weatherius@gmail.com>
2019-04-28 00:31:35 +0300

    Rewrite [59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py] File in: 2019-04-28 00:31:34

--- pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
+++ pyton/59QbtwwpN4kJg590nVZKhFtYyQ59urjQgG6UeCI59iKr.py
@@ -1,445 +1,74 @@
 
         
-          Returns:
-    The word pairs
-  '''
-  curr_features = tf.parse_example(records, {
-      'pair': tf.FixedLenFeature([1], dtype=tf.string)
-  })
+        # If true, SmartyPants will be used to convert quotes and dashes to
+# typographically correct entities.
+#html_use_smartypants = True
     
-        paths_filename = os.path.join(save_dir, '%s.paths' % relation)
-    with open(paths_filename, 'w') as f_out:
-      for index, score in relation_paths:
-        print('\t'.join([index_to_path[index], str(score)]), file=f_out)
+            # this line for build stable iteration
+        sorted_keys = sorted(self.graph.keys())
+        for x in xrange(10):  # 10 iters
+            for n in sorted_keys:
+                s = 0
+                for e in self.graph[n]:
+                    s += e[2] / outSum[e[1]] * ws[e[1]]
+                ws[n] = (1 - self.d) + self.d * s
+    
+    
+def viterbi(obs, states, start_p, trans_p, emit_p):
+    V = [{}]  # tabular
+    path = {}
+    for y in states:  # init
+        V[0][y] = start_p[y] + emit_p[y].get(obs[0], MIN_FLOAT)
+        path[y] = [y]
+    for t in xrange(1, len(obs)):
+        V.append({})
+        newpath = {}
+        for y in states:
+            em_p = emit_p[y].get(obs[t], MIN_FLOAT)
+            (prob, state) = max(
+                [(V[t - 1][y0] + trans_p[y0].get(y, MIN_FLOAT) + em_p, y0) for y0 in PrevStatus[y]])
+            V[t][y] = prob
+            newpath[y] = path[state] + [y]
+        path = newpath
+    
+            x = 0
+        buf = ''
+        N = len(sentence)
+        while x < N:
+            y = route[x][1] + 1
+            l_word = sentence[x:y]
+            if y - x == 1:
+                buf += l_word
+            else:
+                if buf:
+                    if len(buf) == 1:
+                        yield pair(buf, self.word_tag_tab.get(buf, 'x'))
+                    elif not self.tokenizer.FREQ.get(buf):
+                        recognized = self.__cut_detail(buf)
+                        for t in recognized:
+                            yield t
+                    else:
+                        for elem in buf:
+                            yield pair(elem, self.word_tag_tab.get(elem, 'x'))
+                    buf = ''
+                yield pair(l_word, self.word_tag_tab.get(l_word, 'x'))
+            x = y
+    
+        route = [None] * len(obs)
+    i = len(obs) - 1
+    while i >= 0:
+        route[i] = state
+        state = mem_path[i][state]
+        i -= 1
+    return (prob, route)
 
     
-    from synthetic_data_utils import generate_data, generate_rnn
-from synthetic_data_utils import get_train_n_valid_inds
-from synthetic_data_utils import nparray_and_transpose
-from synthetic_data_utils import spikify_data, split_list_by_inds
-import tensorflow as tf
-from utils import write_datasets
+    if opt.topK is None:
+    topK = 10
+else:
+    topK = int(opt.topK)
     
-      Args:
-    data_fname: The filename of the file from which to read the data.
-  Returns:
-    A dictionary whose keys will vary depending on dataset (but should
-    always contain the keys 'train_data' and 'valid_data') and whose
-    values are numpy arrays.
-  '''
+    jieba.analyse.set_idf_path('../extra_dict/idf.txt.big');
     
-      def __init__(self, test_data_name='wsc273'):
-    vocab_file = os.path.join(FLAGS.data_dir, 'vocab.txt')
-    self.vocab = utils.CharsVocabulary(vocab_file, 50)
-    assert test_data_name in ['pdp60', 'wsc273'], (
-        'Test data must be pdp60 or wsc273, got {}'.format(test_data_name))
-    self.test_data_name = test_data_name
-    
-        # Split special chars at the end of word
-    special_end_tokens = []
-    will_split = True
-    while will_split:
-      will_split = False
-      for char in _SPECIAL_CHARS:
-        if word.endswith(char):
-          special_end_tokens = [char] + special_end_tokens
-          word = word[:-len(char)]
-          will_split = True
-    
-      Args:
-    hparams:  Hyperparameters for the MaskGAN.
-    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
-    is_training:  Whether the model is training.
-    reuse (Optional):  Whether to reuse the model.
-    
-    FLAGS = tf.app.flags.FLAGS
-    
-    from six.moves import xrange
-    
-    
-def gen_encoder_seq2seq_nas(hparams):
-  '''Returns the NAS Variable name to MaskGAN Variable
-  dictionary mapping.  This is a highly restrictive function just for testing.
-  This is for the *unidirecitional* seq2seq_nas encoder.
-    
-    
-def test_imdb():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = imdb.load_data()
-        (x_train, y_train), (x_test, y_test) = imdb.load_data(maxlen=40)
-        assert len(x_train) == len(y_train)
-        assert len(x_test) == len(y_test)
-        word_index = imdb.get_word_index()
-        assert isinstance(word_index, dict)
-    
-    
-def test_categorical_hinge():
-    y_pred = K.variable(np.array([[0.3, 0.2, 0.1],
-                                  [0.1, 0.2, 0.7]]))
-    y_true = K.variable(np.array([[0, 1, 0],
-                                  [1, 0, 0]]))
-    expected_loss = ((0.3 - 0.2 + 1) + (0.7 - 0.1 + 1)) / 2.0
-    loss = K.eval(losses.categorical_hinge(y_true, y_pred))
-    assert np.isclose(expected_loss, np.mean(loss))
-    
-    # Create the dataset and its associated one-shot iterator.
-dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-dataset = dataset.repeat()
-dataset = dataset.shuffle(buffer_size)
-dataset = dataset.batch(batch_size)
-iterator = dataset.make_one_shot_iterator()
-    
-    batch_size = 32
-num_classes = 10
-epochs = 200
-hidden_units = 100
-    
-    history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-    
-            # Raises
-            AttributeError: if the layer is connected to
-            more than one incoming layers.
-        '''
-        if not self._inbound_nodes:
-            raise AttributeError('The layer has never been called '
-                                 'and thus has no defined input shape.')
-        all_input_shapes = set(
-            [str(node.input_shapes) for node in self._inbound_nodes])
-        if len(all_input_shapes) == 1:
-            input_shapes = self._inbound_nodes[0].input_shapes
-            return unpack_singleton(input_shapes)
-        else:
-            raise AttributeError('The layer '' + str(self.name) +
-                                 ' has multiple inbound nodes, '
-                                 'with different input shapes. Hence '
-                                 'the notion of 'input shape' is '
-                                 'ill-defined for the layer. '
-                                 'Use `get_input_shape_at(node_index)` '
-                                 'instead.')
-    
-    
-def plot_feature_errors(all_errors, batch_size, all_components, data):
-    plt.figure()
-    plot_results(all_components, all_errors['pca'], label='PCA')
-    plot_results(all_components, all_errors['ipca'],
-                 label='IncrementalPCA, bsize=%i' % batch_size)
-    plt.legend(loc='lower left')
-    plt.suptitle('Algorithm error vs. n_components\n'
-                 'LFW, size %i x %i' % data.shape)
-    plt.xlabel('Number of components (out of max %i)' % data.shape[1])
-    plt.ylabel('Mean absolute error')
-    
-    This example demonstrates how to generate a checkerboard dataset and
-bicluster it using the Spectral Biclustering algorithm.
-    
-    
-plt.show()
-
-    
-    digits = datasets.load_digits()
-images = digits.images
-X = np.reshape(images, (len(images), -1))
-connectivity = grid_to_graph(*images[0].shape)
-    
-    from sklearn import cluster
-    
-    plt.subplot(223)
-plt.scatter(X_varied[:, 0], X_varied[:, 1], c=y_pred)
-plt.title('Unequal Variance')
-    
-        def bulk_insert(self, values):
-        i = 1
-        self.__aux_list = values
-        for value in values:
-            self.insert_data(value)
-            self._step_by_step(i)
-            i += 1
-    
-        return diff
-
-    
-            self.title = match1(html, r'<meta property='og:title' content='([^']*)'')
-    
-    	xml = get_html('http://www.ehow.com/services/video/series.xml?demand_ehow_videoid=%s' % vid)
-    
-	from xml.dom.minidom import parseString
-	doc = parseString(xml)
-	tab = doc.getElementsByTagName('related')[0].firstChild
-    
-    #A Python script to generate a single PDF document with all the tldr pages. It works by generating 
-#intermediate HTML files from existing md files using Python-markdown, applying desired formatting 
-#through CSS, and finally rendering them as PDF. There is no LaTeX dependency for generating the PDF.
-    
-        print('rm1 id: {0}'.format(id(rm1)))
-    print('rm2 id: {0}'.format(id(rm2)))
-    
-        def find_path(self, start, end, path=None):
-        path = path or []
-    
-    
-if __name__ == '__main__':
-    main()
-    
-    *References:
-https://sourcemaking.com/design_patterns/facade
-https://fkromer.github.io/python-pattern-references/design/#facade
-http://python-3-patterns-idioms-test.readthedocs.io/en/latest/ChangeInterface.html#facade
-    
-        def __init__(self):
-        self.dispatcher = Dispatcher()
-    
-    
-### OUTPUT ###
-# PRODUCT LIST:
-# cheese
-# eggs
-# milk
-#
-# PRODUCT INFORMATION:
-# Name: Cheese, Price: 2.00, Quantity: 10
-#
-# PRODUCT INFORMATION:
-# Name: Eggs, Price: 0.20, Quantity: 100
-#
-# PRODUCT INFORMATION:
-# Name: Milk, Price: 1.50, Quantity: 10
-#
-# That product 'arepas' does not exist in the records
-
-    
-    
-class TestData(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.dec_obs = DecimalViewer()
-        cls.hex_obs = HexViewer()
-        cls.sub = Data('Data')
-        # inherited behavior already tested with TestSubject
-        cls.sub.attach(cls.dec_obs)
-        cls.sub.attach(cls.hex_obs)
-    
-        def test_subscriber_shall_be_attachable_to_subscriptions(cls):
-        subscription = 'sub msg'
-        pro = Provider()
-        cls.assertEqual(len(pro.subscribers), 0)
-        sub = Subscriber('sub name', pro)
-        sub.subscribe(subscription)
-        cls.assertEqual(len(pro.subscribers[subscription]), 1)
-    
-        def test_am_station_overflow_after_scan(self):
-        self.radio.scan()
-        station = self.radio.state.stations[self.radio.state.pos]
-        expected_station = '1250'
-        self.assertEqual(station, expected_station)
-    
-        os.environ['CITYSCAPES_DATASET'] = get_raw_dir(json_dataset.name)
-    os.environ['CITYSCAPES_RESULTS'] = output_dir
-    
-    # Available datasets
-_DATASETS = {
-    'cityscapes_fine_instanceonly_seg_train': {
-        _IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
-        _ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_gtFine_train.json',
-        _RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
-    },
-    'cityscapes_fine_instanceonly_seg_val': {
-        _IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
-        # use filtered validation as there is an issue converting contours
-        _ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_filtered_gtFine_val.json',
-        _RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
-    },
-    'cityscapes_fine_instanceonly_seg_test': {
-        _IM_DIR:
-            _DATA_DIR + '/cityscapes/images',
-        _ANN_FN:
-            _DATA_DIR + '/cityscapes/annotations/instancesonly_gtFine_test.json',
-        _RAW_DIR:
-            _DATA_DIR + '/cityscapes/raw'
-    },
-    'coco_2014_train': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_train2014.json'
-    },
-    'coco_2014_val': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_val2014.json'
-    },
-    'coco_2014_minival': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_minival2014.json'
-    },
-    'coco_2014_valminusminival': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/instances_valminusminival2014.json'
-    },
-    'coco_2015_test': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2015.json'
-    },
-    'coco_2015_test-dev': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2015.json'
-    },
-    'coco_2017_test': {  # 2017 test uses 2015 test images
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2017.json',
-        _IM_PREFIX:
-            'COCO_test2015_'
-    },
-    'coco_2017_test-dev': {  # 2017 test-dev uses 2015 test images
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2017.json',
-        _IM_PREFIX:
-            'COCO_test2015_'
-    },
-    'coco_stuff_train': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/coco_stuff_train.json'
-    },
-    'coco_stuff_val': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/coco_stuff_val.json'
-    },
-    'keypoints_coco_2014_train': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_train2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_train2014.json'
-    },
-    'keypoints_coco_2014_val': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_val2014.json'
-    },
-    'keypoints_coco_2014_minival': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_minival2014.json'
-    },
-    'keypoints_coco_2014_valminusminival': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_val2014',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/person_keypoints_valminusminival2014.json'
-    },
-    'keypoints_coco_2015_test': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test2015.json'
-    },
-    'keypoints_coco_2015_test-dev': {
-        _IM_DIR:
-            _DATA_DIR + '/coco/coco_test2015',
-        _ANN_FN:
-            _DATA_DIR + '/coco/annotations/image_info_test-dev2015.json'
-    },
-    'voc_2007_train': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2007/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2007/annotations/voc_2007_train.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2007/VOCdevkit2007'
-    },
-    'voc_2007_val': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2007/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2007/annotations/voc_2007_val.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2007/VOCdevkit2007'
-    },
-    'voc_2007_test': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2007/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2007/annotations/voc_2007_test.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2007/VOCdevkit2007'
-    },
-    'voc_2012_train': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2012/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2012/annotations/voc_2012_train.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2012/VOCdevkit2012'
-    },
-    'voc_2012_val': {
-        _IM_DIR:
-            _DATA_DIR + '/VOC2012/JPEGImages',
-        _ANN_FN:
-            _DATA_DIR + '/VOC2012/annotations/voc_2012_val.json',
-        _DEVKIT_DIR:
-            _DATA_DIR + '/VOC2012/VOCdevkit2012'
-    }
-}
-    
-    
-def _do_matlab_eval(json_dataset, salt, output_dir='output'):
-    import subprocess
-    logger.info('-----------------------------------------------------')
-    logger.info('Computing results with the official MATLAB eval code.')
-    logger.info('-----------------------------------------------------')
-    info = voc_info(json_dataset)
-    path = os.path.join(
-        cfg.ROOT_DIR, 'detectron', 'datasets', 'VOCdevkit-matlab-wrapper')
-    cmd = 'cd {} && '.format(path)
-    cmd += '{:s} -nodisplay -nodesktop '.format(cfg.MATLAB)
-    cmd += '-r 'dbstop if error; '
-    cmd += 'voc_eval(\'{:s}\',\'{:s}\',\'{:s}\',\'{:s}\'); quit;'' \
-       .format(info['devkit_path'], 'comp4' + salt, info['image_set'],
-               output_dir)
-    logger.info('Running:\n{}'.format(cmd))
-    subprocess.call(cmd, shell=True)
-    
-        # Anchors at a single feature cell
-    cell_anchors = generate_anchors(
-        stride=stride, sizes=anchor_sizes, aspect_ratios=anchor_aspect_ratios
-    )
-    num_cell_anchors = cell_anchors.shape[0]
-    
-        if fg_inds.shape[0] > 0:
-        # Class labels for the foreground rois
-        mask_class_labels = blobs['labels_int32'][fg_inds]
-        masks = blob_utils.zeros((fg_inds.shape[0], M**2), int32=True)
+    nmf = decomposition.NMF(n_components=n_topic).fit(tfidf)
+print('done in %0.3fs.' % (time.time() - t0))
