44bd3a58b6ad519115e259aee0f317cfe832ed8f
SUNsung <weatherius@gmail.com>
2019-04-29 02:19:33 +0300

    Rewrite [19DlhCzR5DPQ219o37Gg4Yep8s19XrAG3fqBR9s197c5.cpp] File in: 2019-04-29 02:19:32

--- cccccc/19DlhCzR5DPQ219o37Gg4Yep8s19XrAG3fqBR9s197c5.cpp
+++ cccccc/19DlhCzR5DPQ219o37Gg4Yep8s19XrAG3fqBR9s197c5.cpp
@@ -1,181 +1,143 @@
 
         
-        /// @brief Fills a Blob with constant or randomly-generated data.
-template <typename Dtype>
-class Filler {
- public:
-  explicit Filler(const FillerParameter& param) : filler_param_(param) {}
-  virtual ~Filler() {}
-  virtual void Fill(Blob<Dtype>* blob) = 0;
- protected:
-  FillerParameter filler_param_;
-};  // class Filler
+          // Sets the destination filename and enables images to be written to a PDF
+  // on destruction.
+  void WritePDF(const char* filename) {
+    if (pixaGetCount(pixa_) > 0) {
+      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
+      pixaClear(pixa_);
+    }
+  }
     
-    template <typename Dtype>
-class Layer;
+      double m() const;  // get gradient
+  double c(double m) const;            // get constant
+  double rms(double m, double c) const;            // get error
+  double pearson() const;  // get correlation coefficient.
     
-    #endif  // CAFFE_CONV_LAYER_HPP_
+    
+    {    if (s.ok()) {
+      // Verify that the table is usable
+      Iterator* it = table_cache->NewIterator(ReadOptions(),
+                                              meta->number,
+                                              meta->file_size);
+      s = it->status();
+      delete it;
+    }
+  }
+    
+    struct Options;
+struct FileMetaData;
+    
+    std::string LogFileName(const std::string& dbname, uint64_t number) {
+  assert(number > 0);
+  return MakeFileName(dbname, number, 'log');
+}
+    
+    // Make the CURRENT file point to the descriptor file with the
+// specified number.
+Status SetCurrentFile(Env* env, const std::string& dbname,
+                      uint64_t descriptor_number);
+    
+    int main(int argc, char** argv) {
+  leveldb::Env* env = leveldb::Env::Default();
+  bool ok = true;
+  if (argc < 2) {
+    Usage();
+    ok = false;
+  } else {
+    std::string command = argv[1];
+    if (command == 'dump') {
+      ok = leveldb::HandleDumpCommand(env, argv+2, argc-2);
+    } else {
+      Usage();
+      ok = false;
+    }
+  }
+  return (ok ? 0 : 1);
+}
 
     
+      kFullType = 1,
     
-    {  size_t *workspace_fwd_sizes_;
-  size_t *workspace_bwd_data_sizes_;
-  size_t *workspace_bwd_filter_sizes_;
-  size_t workspaceSizeInBytes;  // size of underlying storage
-  void *workspaceData;  // underlying storage
-  void **workspace;  // aliases into workspaceData
+     public:
+  LogTest() : reading_(false),
+              writer_(new Writer(&dest_)),
+              reader_(new Reader(&source_, &report_, true/*checksum*/,
+                      0/*initial_offset*/)) {
+  }
+    
+      KeyComparator comparator_;
+  int refs_;
+  Arena arena_;
+  Table table_;
+    
+    
+    {
+    {  void ArchiveFile(const std::string& fname) {
+    // Move into another directory.  E.g., for
+    //    dir/foo
+    // rename to
+    //    dir/lost/foo
+    const char* slash = strrchr(fname.c_str(), '/');
+    std::string new_dir;
+    if (slash != nullptr) {
+      new_dir.assign(fname.data(), slash - fname.data());
+    }
+    new_dir.append('/lost');
+    env_->CreateDir(new_dir);  // Ignore error
+    std::string new_file = new_dir;
+    new_file.append('/');
+    new_file.append((slash == nullptr) ? fname.c_str() : slash + 1);
+    Status s = env_->RenameFile(fname, new_file);
+    Log(options_.info_log, 'Archiving %s: %s\n',
+        fname.c_str(), s.ToString().c_str());
+  }
 };
-#endif
-    
-    b2Triangle::~b2Triangle(){
-	delete[] x;
-	delete[] y;
-}
-    
-    			if (!pImageData)
-			{
-				break;
-			}
+}  // namespace
     
     
-    {	}
-    
-    #if defined (__cplusplus)
-extern 'C' {
-#endif
-    }
-    
-    #ifdef FLOAT_APPROX
-/* This code should reliably detect NaN/inf even when -ffast-math is used.
-   Assumes IEEE 754 format. */
-static OPUS_INLINE int celt_isnan(float x)
-{
-   union {float f; opus_uint32 i;} in;
-   in.f = x;
-   return ((in.i>>23)&0xFF)==0xFF && (in.i&0x007FFFFF)!=0;
-}
-#else
-#ifdef __FAST_MATH__
-#error Cannot build libopus with -ffast-math unless FLOAT_APPROX is defined. This could result in crashes on extreme (e.g. NaN) input
-#endif
-#define celt_isnan(x) ((x)!=(x))
-#endif
-    
-    void opus_fft_neon(const kiss_fft_state *st,
-                   const kiss_fft_cpx *fin,
-                   kiss_fft_cpx *fout);
+    { private:
+  // Dummy head of doubly-linked list of snapshots
+  SnapshotImpl head_;
+};
     
     
-/** 16x32 multiplication, followed by a 15-bit shift right. Results fits in 32 bits */
-#undef MULT16_32_Q15
-static OPUS_INLINE opus_val32 MULT16_32_Q15_armv4(opus_val16 a, opus_val32 b)
-{
-  unsigned rd_lo;
-  int rd_hi;
-  __asm__(
-      '#MULT16_32_Q15\n\t'
-      'smull %0, %1, %2, %3\n\t'
-      : '=&r'(rd_lo), '=&r'(rd_hi)
-      : '%r'(b), 'r'(a<<16)
-  );
-  /*We intentionally don't OR in the high bit of rd_lo for speed.*/
-  return rd_hi<<1;
-}
-#define MULT16_32_Q15(a, b) (MULT16_32_Q15_armv4(a, b))
+    {  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+};
     
-    #include <grpc/status.h>
-#include 'absl/memory/memory.h'
-#include 'absl/strings/string_view.h'
-#include 'absl/strings/strip.h'
-#include 'opencensus/trace/span.h'
-#include 'opencensus/trace/span_context.h'
-#include 'opencensus/trace/trace_params.h'
-#include 'src/core/lib/slice/slice_internal.h'
-#include 'src/cpp/common/channel_filter.h'
-#include 'src/cpp/ext/filters/census/rpc_encoding.h'
-    
-    // TODO: This may not be needed. Check to see if opencensus requires
-// a trailing server response.
-// RpcServerStatsEncoding encapsulates the logic for encoding and decoding of
-// rpc server stats messages. Rpc server stats consists of a uint64_t time
-// value (server latency in nanoseconds).
-class RpcServerStatsEncoding {
- public:
-  // Size of encoded RPC server stats.
-  static constexpr size_t kRpcServerStatsSize = 10;
-  // Error value.
-  static constexpr size_t kEncodeDecodeFailure = 0;
-    }
-    
-    
-    {}  // namespace grpc
-    
-    #include <utility>
-    
-        dword(xfx_formater.instruction);
+    struct FileStreamWrapper final : Stream::Wrapper {
+  static req::ptr<MemFile> openFromCache(const String& filename,
+                                         const String& mode);
+  req::ptr<File> open(const String& filename, const String& mode, int options,
+                      const req::ptr<StreamContext>& context) override;
+  int access(const String& path, int mode) override {
+    return ::access(File::TranslatePath(path).data(), mode);
   }
-  void EmitXFXForm(const uint8_t op,
-                   const RegNumber rs,
-                   const uint16_t mask,
-                   const uint16_t xo,
-                   const uint8_t rsv = 0) {
-    
-    #include 'hphp/runtime/base/apc-object.h'
-#include 'hphp/runtime/base/apc-array.h'
-#include 'hphp/runtime/base/apc-stats.h'
-#include 'hphp/runtime/base/object-data.h'
-#include 'hphp/runtime/base/type-object.h'
-#include 'hphp/runtime/ext/apc/ext_apc.h'
-#include 'hphp/runtime/base/collections.h'
-#include 'hphp/runtime/ext/collections/ext_collections-map.h'
-#include 'hphp/runtime/ext/collections/ext_collections-set.h'
-#include 'hphp/runtime/ext/collections/ext_collections-vector.h'
-#include 'hphp/runtime/base/data-walker.h'
-    
-    
-    {///////////////////////////////////////////////////////////////////////////////
-}
-
-    
-    template<typename AHM>
-void checkAHMSubMaps(const AHM& map, folly::StringPiece mapName,
-                     std::atomic<bool>& done) {
-  if (LIKELY(map.numSubMaps() == 1) ||
-      done.load(std::memory_order_relaxed) ||
-      done.exchange(true, std::memory_order_relaxed)) {
-    return;
+  int stat(const String& path, struct stat* buf) override {
+    return ::stat(File::TranslatePath(path).data(), buf);
   }
+  int lstat(const String& path, struct stat* buf) override {
+    return ::lstat(File::TranslatePath(path).data(), buf);
+  }
+  int unlink(const String& path) override;
+  int rename(const String& oldname, const String& newname) override;
+  int mkdir(const String& path, int mode, int options) override;
+  int rmdir(const String& path, int /*options*/) override {
+    ERROR_RAISE_WARNING(::rmdir(File::TranslatePath(path).data()));
+    return ret;
+  }
+  bool isNormalFileStream() const override { return true; }
     }
     
+    namespace HPHP {
+    }
     
-    {    // exception out_of_range.401
-    try
-    {
-        // try to write at a nonexisting key
-        object.at('the fast') = 'il rapido';
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+    template<typename F>
+void logPerfWarning(folly::StringPiece event, F fillCols) {
+  logPerfWarningImpl(event, 1, kDefaultPerfWarningRate, fillCols);
 }
-
-    
-        // output changed array
-    std::cout << array << '\n';
-    
-        // out_of_range.109
-    try
-    {
-        // try to use an array index that is not a number
-        json::reference ref = j.at('/array/one'_json_pointer);
-    }
-    catch (json::parse_error& e)
-    {
-        std::cout << e.what() << '\n';
-    }
-    
-    
-    // =============
-    // boolean types
-    // =============
+template<typename F>
+void logPerfWarning(folly::StringPiece event, int64_t rate, F fillCols) {
+  logPerfWarningImpl(event, 1, rate, fillCols);
+}
