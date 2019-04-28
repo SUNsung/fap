e76a4f010904a8d07e2c73fc0ef7b35a97f48d8a
SUNsung <weatherius@gmail.com>
2019-04-28 10:54:21 +0300

    Rewrite [06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp] File in: 2019-04-28 10:54:20

--- cccccc/06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp
+++ cccccc/06iF7PG22WEgI06PtzgkRBXZto06wsQxptz23fs06qeD.cpp
@@ -1,314 +1,192 @@
 
         
-        namespace leveldb {
-    }
-    
-    // Print contents of a log file. (*func)() is called on every record.
-Status PrintLogContents(Env* env, const std::string& fname,
-                        void (*func)(uint64_t, Slice, WritableFile*),
-                        WritableFile* dst) {
-  SequentialFile* file;
-  Status s = env->NewSequentialFile(fname, &file);
-  if (!s.ok()) {
-    return s;
-  }
-  CorruptionReporter reporter;
-  reporter.dst_ = dst;
-  log::Reader reader(file, &reporter, true, 0);
-  Slice record;
-  std::string scratch;
-  while (reader.ReadRecord(&record, &scratch)) {
-    (*func)(reader.LastRecordOffset(), record, dst);
-  }
-  delete file;
-  return Status::OK();
-}
-    
-    #include <ctype.h>
-#include <stdio.h>
-#include 'db/filename.h'
-#include 'db/dbformat.h'
-#include 'leveldb/env.h'
-#include 'util/logging.h'
-    
-    // Return the name of a temporary file owned by the db named 'dbname'.
-// The result will be prefixed with 'dbname'.
-std::string TempFileName(const std::string& dbname, uint64_t number);
-    
-    static const int kBlockSize = 32768;
-    
-    TEST(LogTest, PartialLastIsIgnored) {
-  Write(BigString('bar', kBlockSize));
-  // Cause a bad record length in the LAST block.
-  ShrinkSize(1);
-  ASSERT_EQ('EOF', Read());
-  ASSERT_EQ('', ReportMessage());
-  ASSERT_EQ(0, DroppedBytes());
-}
-    
-      // Return true if key is greater than the data stored in 'n'
-  bool KeyIsAfterNode(const Key& key, Node* n) const;
-    
-      static uint64_t HashNumbers(uint64_t k, uint64_t g) {
-    uint64_t data[2] = { k, g };
-    return Hash(reinterpret_cast<char*>(data), sizeof(data), 0);
-  }
-    
-    
-    {#if !defined(NDEBUG)
-    snapshot->list_ = this;
-#endif  // !defined(NDEBUG)
-    snapshot->next_ = &head_;
-    snapshot->prev_ = head_.prev_;
-    snapshot->prev_->next_ = snapshot;
-    snapshot->next_->prev_ = snapshot;
-    return snapshot;
-  }
-    
-    /**
- * \class Predictor
- *
- * \brief Performs prediction on individual training instances or batches of
- * instances for GBTree. The predictor also manages a prediction cache
- * associated with input matrices. If possible, it will use previously
- * calculated predictions instead of calculating new predictions.
- *        Prediction functions all take a GBTreeModel and a DMatrix as input and
- * output a vector of predictions. The predictor does not modify any state of
- * the model itself.
- */
-    
-      bool Next() override {
-    if (!parser_->Next()) return false;
-    const RowBlock<IndexType>& batch = parser_->Value();
-    LOG(INFO) << batch.size;
-    dense_index_.resize(num_col_ * batch.size);
-    dense_value_.resize(num_col_ * batch.size);
-    std::fill(dense_value_.begin(), dense_value_.end(), 0.0);
-    offset_.resize(batch.size + 1);
-    offset_[0] = 0;
-    }
-    
-        size_t* it = begin;
-    for (bst_omp_uint tid = 0; tid < nthread; ++tid) {
-      std::copy(row_split_tloc[tid].left.begin(), row_split_tloc[tid].left.end(), it);
-      it += row_split_tloc[tid].left.size();
-    }
-    size_t* split_pt = it;
-    for (bst_omp_uint tid = 0; tid < nthread; ++tid) {
-      std::copy(row_split_tloc[tid].right.begin(), row_split_tloc[tid].right.end(), it);
-      it += row_split_tloc[tid].right.size();
-    }
-    
-    /*!
- * \brief Macro to register sparse page format.
- *
- * \code
- * // example of registering a objective
- * XGBOOST_REGISTER_SPARSE_PAGE_FORMAT(raw)
- * .describe('Raw binary data format.')
- * .set_body([]() {
- *     return new RawFormat();
- *   });
- * \endcode
- */
-#define XGBOOST_REGISTER_SPARSE_PAGE_FORMAT(Name)                       \
-  DMLC_REGISTRY_REGISTER(::xgboost::data::SparsePageFormatReg, SparsePageFormat, Name)
-    
-      void Load(dmlc::Stream* fi) {
-    CHECK_EQ(fi->Read(&param, sizeof(param)), sizeof(param))
-        << 'GBTree: invalid model file';
-    trees.clear();
-    trees_to_update.clear();
-    for (int i = 0; i < param.num_trees; ++i) {
-      std::unique_ptr<RegTree> ptr(new RegTree());
-      ptr->Load(fi);
-      trees.push_back(std::move(ptr));
-    }
-    tree_info.resize(param.num_trees);
-    if (param.num_trees != 0) {
-      CHECK_EQ(
-          fi->Read(dmlc::BeginPtr(tree_info), sizeof(int) * param.num_trees),
-          sizeof(int) * param.num_trees);
-    }
-  }
-    
-    
-using namespace dmlc;
-    
-    TEST(Span, RBeginREnd) {
-  int status = 1;
-  TestRBeginREnd{&status}();
-  ASSERT_EQ(status, 1);
-}
-    
-    CardinalSplineTo::~CardinalSplineTo()
-{
-    CC_SAFE_RELEASE_NULL(_points);
-}
-    
-    void ActionEase::startWithTarget(Node *target)
-{
-    if (target && _inner)
+            void operator() (const typename VecTraits<T>::vec128 & v_src0,
+                     const typename VecTraits<T>::vec128 & v_src1,
+                     typename VecTraits<T>::vec128 & v_dst) const
     {
-        ActionInterval::startWithTarget(target);
-        _inner->startWithTarget(_target);
+        typename VecTraits<wtype>::vec128 vrl, vrh;
+        wideAdd(vmovl( vget_low(v_src0)), vmovl( vget_low(v_src1)), vrl);
+        wideAdd(vmovl(vget_high(v_src0)), vmovl(vget_high(v_src1)), vrh);
     }
-    else
-    {
-        log('ActionEase::startWithTarget error: target or _inner is nullptr!');
-    }
+    
+        func(size,
+         srcBase, srcStride,
+         dstBase, dstStride,
+         flipMode);
+    
+    TEST(StructurallyValidTest, InvalidUTF8String) {
+  const string invalid_str('abcd\xA0\xB0\xA0\xB0\xA0\xB0 - xyz789');
+  EXPECT_FALSE(IsStructurallyValidUTF8(invalid_str.data(),
+                                       invalid_str.size()));
+  // Additional check for pointer alignment
+  for (int i = 1; i < 8; ++i) {
+    EXPECT_FALSE(IsStructurallyValidUTF8(invalid_str.data() + i,
+                                         invalid_str.size() - i));
+  }
 }
     
-        /** Set the selector target.
-     *
-     * @param sel The selector target.
-     */
-    void setTargetCallback(Ref* sel)
+    
+    {  typedef if_<false, true_type, false_type>::type if_false;
+  EXPECT_FALSE(if_false::value);
+}
+    
+      // Returns the x,y means as an FCOORD.
+  FCOORD mean_point() const;
+    
+    #ifndef TESSERACT_CCMAIN_OTSUTHR_H_
+#define TESSERACT_CCMAIN_OTSUTHR_H_
+    
+    // A collection of utility functions for arrays of UNICHAR_IDs that are
+// terminated by INVALID_UNICHAR_ID.
+class UnicharIdArrayUtils {
+ public:
+  // Compares two arrays of unichar ids. Returns -1 if the length of array1 is
+  // less than length of array2, if any array1[i] is less than array2[i].
+  // Returns 0 if the arrays are equal, 1 otherwise.
+  // The function assumes that the arrays are terminated by INVALID_UNICHAR_ID.
+  static inline int compare(const UNICHAR_ID *ptr1, const UNICHAR_ID *ptr2) {
+    for (;;) {
+      const UNICHAR_ID val1 = *ptr1++;
+      const UNICHAR_ID val2 = *ptr2++;
+      if (val1 != val2) {
+        if (val1 == INVALID_UNICHAR_ID) return -1;
+        if (val2 == INVALID_UNICHAR_ID) return 1;
+        if (val1 < val2) return -1;
+        return 1;
+      }
+      if (val1 == INVALID_UNICHAR_ID) return 0;
+    }
+  }
+    }
+    
+     private:
+  struct ReferenceCount {
+    STRING id;  // A unique ID to identify the object (think path on disk)
+    T *object;  // A copy of the object in memory.  Can be delete'd.
+    int count;  // A count of the number of active users of this object.
+  };
+    
+    
+    {  // Verify that the size of the key space not touched by the reads
+  // is pretty much unchanged.
+  const int64_t final_other_size = Size(Key(n), Key(kCount));
+  ASSERT_LE(final_other_size, initial_other_size + 1048576);
+  ASSERT_GE(final_other_size, initial_other_size/5 - 1048576);
+}
+    
+    // Return the name of the old info log file for 'dbname'.
+std::string OldInfoLogFileName(const std::string& dbname);
+    
+        // Open the log file
+    std::string logname = LogFileName(dbname_, log);
+    SequentialFile* lfile;
+    Status status = env_->NewSequentialFile(logname, &lfile);
+    if (!status.ok()) {
+      return status;
+    }
+    
+    IMGUI_IMPL_API bool     ImGui_Marmalade_Init(bool install_callbacks);
+IMGUI_IMPL_API void     ImGui_Marmalade_Shutdown();
+IMGUI_IMPL_API void     ImGui_Marmalade_NewFrame();
+IMGUI_IMPL_API void     ImGui_Marmalade_RenderDrawData(ImDrawData* draw_data);
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+            // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!), 
+        // you may need to backup/reset/restore current shader using the commented lines below.
+        //GLint last_program; 
+        //glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
+        //glUseProgram(0);
+        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+        //glUseProgram(last_program);
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+            g_pd3dCommandList->Reset(frameCtxt->CommandAllocator, NULL);
+        g_pd3dCommandList->ResourceBarrier(1, &barrier);
+        g_pd3dCommandList->ClearRenderTargetView(g_mainRenderTargetDescriptor[backBufferIdx], (float*)&clear_color, 0, NULL);
+        g_pd3dCommandList->OMSetRenderTargets(1, &g_mainRenderTargetDescriptor[backBufferIdx], FALSE, NULL);
+        g_pd3dCommandList->SetDescriptorHeaps(1, &g_pd3dSrvDescHeap);
+        ImGui::Render();
+        ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), g_pd3dCommandList);
+        barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+        barrier.Transition.StateAfter  = D3D12_RESOURCE_STATE_PRESENT;
+        g_pd3dCommandList->ResourceBarrier(1, &barrier);
+        g_pd3dCommandList->Close();
+    
+        // Main loop
+    MSG msg;
+    ZeroMemory(&msg, sizeof(msg));
+    while (msg.message != WM_QUIT)
     {
-        if (sel != _selectorTarget)
+        // Poll and handle messages (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        if (::PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
         {
-            CC_SAFE_RETAIN(sel);
-            CC_SAFE_RELEASE(_selectorTarget);
-            _selectorTarget = sel;
+            ::TranslateMessage(&msg);
+            ::DispatchMessage(&msg);
+            continue;
         }
     }
-    //
-    // Overrides
-    //
-    /**
-     * @param time In seconds.
-     */
-    virtual void update(float time) override;
-    virtual CallFunc* reverse() const override;
-    virtual CallFunc* clone() const override;
     
-CC_CONSTRUCTOR_ACCESS:
-    CallFunc()
-    : _selectorTarget(nullptr)
-    , _callFunc(nullptr)
-    , _function(nullptr)
-    {
-    }
-    virtual ~CallFunc();
     
-    void ActionManager::removeActionsByFlags(unsigned int flags, Node *target)
-{
-    if (flags == 0)
-    {
-        return;
-    }
-    CCASSERT(target != nullptr, 'target can't be nullptr!');
-    if (target == nullptr)
-    {
-        return;
-    }
-    }
-    
-        void removeActionAtIndex(ssize_t index, struct _hashElement *element);
-    void deleteHashElement(struct _hashElement *element);
-    void actionAllocWithHashElement(struct _hashElement *element);
-    
-    /** @brief AtlasNode is a subclass of Node that implements the RGBAProtocol and TextureProtocol protocol.
- * It knows how to render a TextureAtlas object.
- * If you are going to render a TextureAtlas consider subclassing AtlasNode (or a subclass of AtlasNode).
- * All features from Node are valid, plus the following features:
- * - opacity and RGB colors.
- */
-class CC_DLL AtlasNode : public Node, public TextureProtocol
-{    
-public:
-	/** creates a AtlasNode  with an Atlas file the width and height of each item and the quantity of items to render.
-     *
-     * @param filename The path of Atlas file.
-     * @param tileWidth The width of the item.
-     * @param tileHeight The height of the item.
-     * @param itemsToRender The quantity of items to render.
-     */
-	static AtlasNode * create(const std::string& filename, int tileWidth, int tileHeight, int itemsToRender);
-    }
-    
-        /**
-     * get vertex count
-     * @return number of vertices
-     */
-    unsigned int getVertCount() const;
-    
-    /**
-     * get triangles count
-     * @return number of triangles
-     */
-    unsigned int getTrianglesCount() const;
-    
-      {
-    auto ka = getKeepAliveToken(exec);
-    EXPECT_TRUE(ka);
-    EXPECT_EQ(&exec, ka.get());
-    EXPECT_EQ(1, exec.refCount);
-    }
-    
-    BENCHMARK_RELATIVE(sformat_short_string_unsafe, iters) {
-  BenchmarkSuspender suspender;
-  auto const& shortString = getShortString();
-  while (iters--) {
-    std::string out;
-    suspender.dismissing([&] { out = sformat(shortString); });
-  }
+    {    InputTextCallback_UserData cb_user_data;
+    cb_user_data.Str = str;
+    cb_user_data.ChainCallback = callback;
+    cb_user_data.ChainCallbackUserData = user_data;
+    return InputText(label, (char*)str->c_str(), str->capacity() + 1, flags, InputTextCallback, &cb_user_data);
 }
     
     
-    {  /**
-   * Returns a double in [min, max), if min == max, returns 0.
-   */
-  template <class RNG = ThreadLocalPRNG, class /* EnableIf */ = ValidRNG<RNG>>
-  static double randDouble(double min, double max, RNG&& rng) {
-    if (std::fabs(max - min) < std::numeric_limits<double>::epsilon()) {
-      return 0;
-    }
-    return std::uniform_real_distribution<double>(min, max)(rng);
+    {} // namespace aria2
+    
+      ~DHTRoutingTableDeserializer();
+    
+    
+    {} // namespace aria2
+    
+    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createReplaceNodeTask(
+    const std::shared_ptr<DHTBucket>& bucket,
+    const std::shared_ptr<DHTNode>& newNode)
+{
+  auto task = std::make_shared<DHTReplaceNodeTask>(bucket, newNode);
+  task->setTimeout(timeout_);
+  setCommonProperty(task);
+  return task;
+}
+    
+    
+    {} // namespace aria2
+    
+    void DHTTokenUpdateCommand::preProcess()
+{
+  if (getDownloadEngine()->getRequestGroupMan()->downloadFinished() ||
+      getDownloadEngine()->isHaltRequested()) {
+    enableExit();
   }
-};
+}
     
-    template <
-    class Iterator = const char*,
-    class Base = folly::Range<boost::u8_to_u32_iterator<Iterator>>>
-class UTF8Range : public Base {
- public:
-  /* implicit */ UTF8Range(const folly::Range<Iterator> baseRange)
-      : Base(
-            boost::u8_to_u32_iterator<Iterator>(
-                baseRange.begin(),
-                baseRange.begin(),
-                baseRange.end()),
-            boost::u8_to_u32_iterator<Iterator>(
-                baseRange.end(),
-                baseRange.begin(),
-                baseRange.end())) {}
-  /* implicit */ UTF8Range(const std::string& baseString)
-      : Base(folly::Range<Iterator>(baseString)) {}
-};
+    #include 'TimeBasedCommand.h'
     
-      explicit VirtualExecutor(Executor* executor)
-      : VirtualExecutor(getKeepAliveToken(executor)) {}
+    public:
+  // _remoteNode is always null
+  DHTUnknownMessage(const std::shared_ptr<DHTNode>& localNode,
+                    const unsigned char* data, size_t length,
+                    const std::string& ipaddr, uint16_t port);
     
-      tv = to<struct timeval>(milliseconds(987654321012LL));
-  EXPECT_EQ(987654321, tv.tv_sec);
-  EXPECT_EQ(12000, tv.tv_usec);
-    
-    
-    {
-    {
-    {} // namespace zlib
-} // namespace io
-} // namespace folly
-    
-      std::shared_ptr<T> get() const {
-    folly::hazptr_local<1> hazptr;
-    auto slots = hazptr[0].get_protected(slots_);
-    if (!slots) {
-      return nullptr;
-    }
-    return (slots->slots_)[AccessSpreader<>::current(kNumSlots)];
+    DNSCache::CacheEntry& DNSCache::CacheEntry::operator=(const CacheEntry& c)
+{
+  if (this != &c) {
+    hostname_ = c.hostname_;
+    port_ = c.port_;
+    addrEntries_ = c.addrEntries_;
   }
-    
-    
-    {} // namespace
+  return *this;
+}
