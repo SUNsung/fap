a8524d19652ac661f78c1c915a538be10f26eaca
SUNsung <weatherius@gmail.com>
2019-04-29 08:37:31 +0300

    Rewrite [42qUlG.4MUW1Y42ciJgNlfX4QM42B5YfiDS6QPw42BkW.py] File in: 2019-04-29 08:37:30

--- pyton/42qUlG.4MUW1Y42ciJgNlfX4QM42B5YfiDS6QPw42BkW.py
+++ pyton/42qUlG.4MUW1Y42ciJgNlfX4QM42B5YfiDS6QPw42BkW.py
@@ -1,37 +1,149 @@
 
         
-            # Display the results
-    for top, right, bottom, left in face_locations:
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-    
-        # You can access the actual face itself like this:
-    face_image = image[top:bottom, left:right]
-    pil_image = Image.fromarray(face_image)
-    pil_image.show()
+                When updating an entry, updates its position to the front of the LRU list.
+        If the entry is new and the cache is at capacity, removes the oldest entry
+        before the new entry is added.
+        '''
+        node = self.lookup.get(query)
+        if node is not None:
+            # Key exists in cache, update the value
+            node.results = results
+            self.linked_list.move_to_front(node)
+        else:
+            # Key does not exist in cache
+            if self.size == self.MAX_SIZE:
+                # Remove the oldest entry from the linked list and lookup
+                self.lookup.pop(self.linked_list.tail.query, None)
+                self.linked_list.remove_from_tail()
+            else:
+                self.size += 1
+            # Add the new key and value
+            new_node = Node(results)
+            self.linked_list.append_to_front(new_node)
+            self.lookup[query] = new_node
 
     
-                for face_location in face_locations:
-                # Print the location of each face in this frame
-                top, right, bottom, left = face_location
-                print(' - A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
-    # Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
-# See http://pillow.readthedocs.io/ for more about PIL/Pillow
-pil_image = Image.fromarray(unknown_image)
-# Create a Pillow ImageDraw Draw instance to draw with
-draw = ImageDraw.Draw(pil_image)
+class User(object):
     
-        # 图片上传失败，输出以下html代码
-    return '''
-    <!doctype html>
-    <title>Is this a picture of Obama?</title>
-    <h1>Upload a picture and see if it's a picture of Obama!</h1>
-    <form method='POST' enctype='multipart/form-data'>
-      <input type='file' name='file'>
-      <input type='submit' value='Upload'>
-    </form>
-    '''
+        def __init__(self, template_categories_to_budget_map):
+        self.categories_to_budget_map = template_categories_to_budget_map
+    
+        def get(self, key):
+        hash_index = self._hash_function(key)
+        for item in self.table[hash_index]:
+            if item.key == key:
+                return item.value
+        raise KeyError('Key not found')
+    
+            is_not_importable = False
+        is_namespace = False
+        tests = []
+        if os.path.isdir(os.path.abspath(start_dir)):
+            start_dir = os.path.abspath(start_dir)
+            if start_dir != top_level_dir:
+                is_not_importable = not os.path.isfile(os.path.join(start_dir, '__init__.py'))
+        else:
+            # support for discovery from dotted module names
+            try:
+                __import__(start_dir)
+            except ImportError:
+                is_not_importable = True
+            else:
+                the_module = sys.modules[start_dir]
+                top_part = start_dir.split('.')[0]
+                try:
+                    start_dir = os.path.abspath(
+                       os.path.dirname((the_module.__file__)))
+                except AttributeError:
+                    # look for namespace packages
+                    try:
+                        spec = the_module.__spec__
+                    except AttributeError:
+                        spec = None
+    
+            class Foo(unittest.TestCase):
+            def test_1(self): pass
+            def test_2(self): pass
+    
+        def testPickle(self):
+        for proto in range(pickle.HIGHEST_PROTOCOL + 1):
+            with self.assertRaises(TypeError):
+                pickle.dumps(BZ2Compressor(), proto)
+    
+    # Add the html version.  This converts the message into a multipart/alternative
+# container, with the original text message as the first part and the new html
+# message as the second part.
+asparagus_cid = make_msgid()
+msg.add_alternative('''\
+<html>
+  <head></head>
+  <body>
+    <p>Salut!</p>
+    <p>Cela ressemble à un excellent
+        <a href='http://www.yummly.com/recipe/Roasted-Asparagus-Epicurious-203718'>
+            recipie
+        </a> déjeuner.
+    </p>
+    <img src='cid:{asparagus_cid}' />
+  </body>
+</html>
+'''.format(asparagus_cid=asparagus_cid[1:-1]), subtype='html')
+# note that we needed to peel the <> off the msgid for use in the html.
+    
+        print('-' * 20)
+    
+        # Create queues
+    task_queue = Queue()
+    done_queue = Queue()
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+# Not installing aliases from python-future; it's unreliable and slow.
+from builtins import *  # noqa
+    
+    
+def MockAsyncServerResponseException( exception ):
+  '''Return a fake future object that is complete, but raises an exception.
+  Suitable for mocking a response future within a client request. For example:
+    
+    d = {'name': None, 'description': None, 'short_description': None}
+    
+        filepath = os.path.join(_dir, f)
+    with open(filepath) as file:
+        try:
+            _ = json.loads(file.read())
+            return True
+        except:
+            sys.stderr.write(traceback.format_exc())            
+            assert False, u'校验(%s)失败' % f
+    
+    ICONS_PACKAGE = 'A File Icon'
+PKGCTRL_SETTINGS = 'Package Control.sublime-settings'
+    
+    
+def get_ui_theme():
+  return sublime.load_settings(PREFERENCES).get('theme', '')
+    
+    .mt-config-changes ul li, .mt-config-changes p {
+  {{'.foreground'|css}}
+}
+    
+        def display_list(self, themes):
+        self.themes = themes
+        self.initial_theme = get_theme(PLAIN_NOTES)
+    
+            try:
+            import pygments
+            info['pygments_version'] = format_version(pygments, '__version__')
+        except Exception:
+            info['pygments_version'] = 'Version could not be acquired!'
