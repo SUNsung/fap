48c4c3b43f525b499b74171d2c84aa1fe8cce74d
SUNsung <weatherius@gmail.com>
2019-04-29 19:17:48 +0300

    Rewrite [11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb] File in: 2019-04-29 19:17:47

--- ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
+++ ruby/11RNXSPAgESD61181vAkOOCBLI11EwVtlCqbvgE11UMJ.rb
@@ -1,22 +1,108 @@
 
         
-            def action_launched(launch_context: nil)
-      unless did_show_message?
-        show_message
-      end
+          context 'with a user' do
+    let(:user) { Fabricate(:user) }
     
-          it 'doesn't add a no-skip-current flag to command if no_skip_current is set to false' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-            carthage(
-              command: 'build',
-              no_skip_current: false
-            )
-          end').runner.execute(:test)
+        # See {CapabilityHost#capability}
+    def capability(*args)
+      super
+    rescue Errors::CapabilityNotFound => e
+      raise Errors::GuestCapabilityNotFound,
+        cap: e.extra_data[:cap],
+        guest: name
+    rescue Errors::CapabilityInvalid => e
+      raise Errors::GuestCapabilityInvalid,
+        cap: e.extra_data[:cap],
+        guest: name
+    end
     
-            it 'executes the correct git command' do
-          allow(Fastlane::Actions).to receive(:sh).with('git add *.h *.m', anything).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: #{path}, shell_escape: false)
-          end').runner.execute(:test)
+            # Set the root class up to be ourself, so that we can reference this
+        # from within methods which are probably in subclasses.
+        ROOT_CLASS = self
+    
+            # This contains all the command plugins by name, and returns
+        # the command class and options. The command class is wrapped
+        # in a Proc so that it can be lazy loaded.
+        #
+        # @return [Registry<Symbol, Array<Proc, Hash>>]
+        attr_reader :commands
+    
+      #
+  # The raw command string associated with the header which will vary between
+  # requests and responses.
+  #
+  attr_accessor :junk_headers
+  attr_accessor :cmd_string
+  attr_accessor :fold
+    
+      # add junk end of URI
+  attr_accessor :junk_end_of_uri
+    
+              sent = 0
+          case protocol
+          when 'tcp'
+            sent = send_request_tcp(req)
+          when 'udp'
+            sent = send_request_udp(req)
+          else
+            raise ::RuntimeError, 'Kerberos Client: unknown transport protocol'
+          end
+    
+                checksum = OpenSSL::HMAC.digest('MD5', k1, data_encrypt)
+    
+              # Encodes the authenticator field
+          #
+          # @return [String]
+          def encode_authenticator
+            authenticator.encode
+          end
         end
       end
+    end
+  end
+end
+    
+              # Encodes the type
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_type(type)
+            bn = OpenSSL::BN.new(type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+                seq = OpenSSL::ASN1::Sequence.new(elems)
+    
+          # Returns the else branch of the `case` statement, if any.
+      #
+      # @return [Node] the else branch node of the `case` statement
+      # @return [nil] if the case statement does not have an else branch.
+      def else_branch
+        node_parts[-1]
+      end
+    
+          # Checks whether the `for` node has a `do` keyword.
+      #
+      # @return [Boolean] whether the `for` node has a `do` keyword
+      def do?
+        loc.begin && loc.begin.is?('do')
+      end
+    
+          # Checks whether any of the key value pairs in the `hash` literal are on
+      # the same line.
+      #
+      # @note A multiline `pair` is considered to be on the same line if it
+      #       shares any of its lines with another `pair`
+      #
+      # @return [Boolean] whether any `pair` nodes are on the same line
+      def pairs_on_same_line?
+        pairs.each_cons(2).any? { |first, second| first.same_line?(second) }
+      end
+    
+          DOUBLE_SPLAT = '**'.freeze
+    
+      <form method='post' action='/msg'>
+    <input type='text' name='msg'>
+    <input type='submit' value='Add Message'>
+  </form>
+    
+      # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
