583d05fbf30774f59b15a90ffafb51ecb6e4b691
SUNsung <weatherius@gmail.com>
2019-04-29 10:53:00 +0300

    Rewrite [25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb] File in: 2019-04-29 10:52:59

--- ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
+++ ruby/25a3.kxPhUsE25a6lVJEmo3BQ25kZhSMSFcSbs25IS.rb
@@ -1,180 +1,70 @@
 
         
-          end
+            # Delimit on each variable
+    str.split(/[;&]/).each { |vv|
+      var = vv
+      val = ''
+    }
     
-        def initialize
-      Fastlane.load_actions
-      @runner = Runner.new
-      @actions_requiring_special_handling = ['sh'].to_set
-    end
+                if OpenSSL::HMAC.digest('MD5', k1, decrypted) != checksum
+              raise ::RuntimeError, 'RC4-HMAC decryption failed, incorrect checksum verification'
+            end
     
-          it 'handles the exclude_dirs parameter with no elements correctly' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          ensure_no_debug_code(text: 'pry', path: '.', exclude_dirs: [])
-        end').runner.execute(:test)
-        expect(result).to eq('grep -RE 'pry' '#{File.absolute_path('./')}'')
-      end
-    
-          it 'logs the command if verbose' do
-        with_verbose(true) do
-          allow(Fastlane::Actions).to receive(:sh).with(anything, { log: true }).and_return('')
-          result = Fastlane::FastFile.new.parse('lane :test do
-            git_add(path: 'foo.bar')
-          end').runner.execute(:test)
-        end
-      end
-    
-              expect(result).to eq('swiftlint lint > #{output_file}')
-        end
-      end
-    
-        context 'external commands are failed' do
-      context 'with error_callback' do
-        it 'doesn't raise shell_error' do
-          allow(FastlaneCore::UI).to receive(:error)
-          called = false
-          expect_command('exit 1', exitstatus: 1)
-          Fastlane::Actions.sh('exit 1', error_callback: ->(_) { called = true })
-    
-    # Here be helper
-    
-      def prev_page
-    account_outbox_url(@account, page: true, min_id: @statuses.first.id) unless @statuses.empty?
-  end
-    
-      private
-    
-      def lease_seconds_or_default
-    (params['hub.lease_seconds'] || 1.day).to_i.seconds
-  end
-    
-      respond_to :json
-    
-      # Returns a request packet
-  def to_s
-    str = ''
-    if self.junk_pipeline
-      host = ''
-      if self.headers['Host']
-        host = 'Host: #{self.headers['Host']}\r\n'
-      end
-      str << 'GET / HTTP/1.1\r\n#{host}Connection: Keep-Alive\r\n\r\n' * self.junk_pipeline
-      self.headers['Connection'] = 'Closed'
-    end
-    str + super
-  end
-    
-      #
-  # Returns the hardcore alias for the HTTP service
-  #
-  def self.hardcore_alias(*args)
-    '#{(args[0] || '')}#{(args[1] || '')}'
-  end
-    
-    # Supported
-IAX_SUPPORTED_CODECS  = IAX_CODEC_G711_MULAW | IAX_CODEC_G711_ALAW | IAX_CODEC_LINEAR_PCM
-    
-      #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
-    
-              # Encodes the Rex::Proto::Kerberos::CredentialCache::Principal into an String
-          #
-          # @return [String] encoded principal
-          def encode
-            encoded = ''
-            encoded << encode_name_type
-            encoded << [components.length].pack('N')
-            encoded << encode_realm
-            encoded << encode_components
-    
-              # Encodes the end_time field
-          #
-          # @return [String]
-          def encode_end_time
-            [end_time].pack('N')
-          end
-    
-              # Decodes the etype from an OpenSSL::ASN1::ASN1Data
+              # Decodes the flags field
           #
           # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
           # @return [Integer]
-          def decode_etype(input)
+          def decode_flags(input)
             input.value[0].value.to_i
           end
     
-              # Decodes the till field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_till(input)
-            input.value[0].value
+                res
           end
     
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a Kerberos KDC-REQ (response) data
-        # definition
-        class KdcResponse < Element
-          # @!attribute pvno
-          #   @return [Integer] The protocol version number
-          attr_accessor :pvno
-          # @!attribute msg_type
-          #   @return [Integer] The type of a protocol message
-          attr_accessor :msg_type
-          # @!attribute crealm
-          #   @return [String] The realm part of the client's principal identifier
-          attr_accessor :crealm
-          # @!attribute cname
-          #   @return [Rex::Proto::Kerberos::Model::PrincipalName] The name part of the client's principal identifier
-          attr_accessor :cname
-          # @!attribute ticket
-          #   @return [Rex::Proto::Kerberos::Model::Ticket] The issued ticket
-          attr_accessor :ticket
-          # @!attribute enc_part
-          #   @return [Rex::Proto::Kerberos::Model::EncryptedData] The encrypted part of the response
-          attr_accessor :enc_part
-    
-      desc 'update main and version in bower.json'
-  task :generate do
-    require 'bootstrap-sass'
-    Dir.chdir Bootstrap.gem_path do
-      spec       = JSON.parse(File.read 'bower.json')
-    
-    World(RemoteCommandHelpers)
-
-    
-      def run_vagrant_command(command)
-    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
-    return [stdout, stderr] if status.success?
-    raise VagrantSSHCommandError, status
+      if config.log_to.include? 'file'
+    # Configure an appender that will write log events to a file.
+    if AppConfig.environment.logging.logrotate.enable?
+      # The file will be rolled on a daily basis, and the rolled files will be kept
+      # the configured number of days. Older files will be deleted. The default pattern
+      # layout is used when formatting log events into strings.
+      Logging.appenders.rolling_file('file',
+                                     filename:      config.paths['log'].first,
+                                     keep:          AppConfig.environment.logging.logrotate.days.to_i,
+                                     age:           'daily',
+                                     truncate:      false,
+                                     auto_flushing: true,
+                                     layout:        layout
+                                    )
+    else
+      # No file rolling, use logrotate to roll the logfile.
+      Logging.appenders.file('file',
+                             filename:      config.paths['log'].first,
+                             truncate:      false,
+                             auto_flushing: true,
+                             layout:        layout
+                            )
+    end
   end
+    
+        remove_duplicates
+    remove_index :share_visibilities, name: :shareable_and_user_id
+    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
+    
+    Then /^I should have (\d+) nsfw posts$/ do |num_posts|
+  page.should have_css('.nsfw-shield', count: num_posts.to_i)
 end
     
-          def add_role(role)
-        roles.add role.to_sym
-        self
-      end
+        it 'only lets you read your own notifications' do
+      user2 = bob
     
-            expect(cop.offenses.size).to eq(1)
-        expect(cop.highlights).to eq([close])
-        expect(cop.messages).to eq([described_class::NEW_LINE_MESSAGE])
-      end
+        it 'does redirect if there is an invalid invite token' do
+      get :new, params: {invite: {token: 'fssdfsd'}}
+      expect(response).to redirect_to new_user_session_path
+    end
     
-        def erb(content, options = {})
-      if content.kind_of? Symbol
-        unless respond_to?(:'_erb_#{content}')
-          src = ERB.new(File.read('#{Web.settings.views}/#{content}.erb')).src
-          WebAction.class_eval('def _erb_#{content}\n#{src}\n end')
-        end
-      end
+        def initialize(processed_source)
+      @processed_source = processed_source
+    end
+    
+        def for(file_or_dir)
+      return @options_config if @options_config
