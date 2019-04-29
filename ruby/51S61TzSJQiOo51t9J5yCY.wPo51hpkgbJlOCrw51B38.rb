686ad80504882280c4fc1b1e340da35f3a9d5e82
SUNsung <weatherius@gmail.com>
2019-04-29 13:51:47 +0300

    Rewrite [51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb] File in: 2019-04-29 13:51:46

--- ruby/51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb
+++ ruby/51S61TzSJQiOo51t9J5yCY.wPo51hpkgbJlOCrw51B38.rb
@@ -1,205 +1,108 @@
 
         
-          it 'requires a URL or file uplaod' do
-    visit new_scenario_imports_path
-    click_on 'Start Import'
-    expect(page).to have_text('Please provide either a Scenario JSON File or a Public Scenario URL.')
-  end
+                  def hidden_field
+            hidden_name = @html_options[:name] || hidden_field_name
+            @template_object.hidden_field_tag(hidden_name, '', id: nil)
+          end
     
-        it 'works for queued jobs' do
-      expect(status(job)).to eq('<span class='label label-warning'>queued</span>')
-    end
-  end
-    
-      describe '.seed' do
-    it 'imports a set of agents to get the user going when they are first created' do
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(7)
+        def action_completed(completion_context: nil)
     end
     
-              existing_scenario.reload
-          expect(existing_scenario.guid).to eq(guid)
-          expect(existing_scenario.tag_fg_color).to eq(tag_fg_color)
-          expect(existing_scenario.tag_bg_color).to eq(tag_bg_color)
-          expect(existing_scenario.icon).to eq(icon)
-          expect(existing_scenario.description).to eq(description)
-          expect(existing_scenario.name).to eq(name)
-          expect(existing_scenario.source_url).to eq(source_url)
-          expect(existing_scenario.public).to be_falsey
-        end
+    module Commander
+  # This class override the run method with our custom stack trace handling
+  # In particular we want to distinguish between user_error! and crash! (one with, one without stack trace)
+  class Runner
+    # Code taken from https://github.com/commander-rb/commander/blob/master/lib/commander/runner.rb#L50
     
-          it 'loads only the workers specified in the :only option' do
-        agent_runner = AgentRunner.new(only: HuginnScheduler)
-        workers = agent_runner.send(:load_workers)
-        expect(workers.keys).to eq(['HuginnScheduler'])
-        agent_runner.stop
-      end
-    
-        it 'does not output links to other agents outside of the incoming set' do
-      Link.create!(:source_id => agents(:jane_weather_agent).id, :receiver_id => agents(:jane_website_agent).id)
-      Link.create!(:source_id => agents(:jane_website_agent).id, :receiver_id => agents(:jane_rain_notifier_agent).id)
-    
-        # The path used after resending confirmation instructions.
-    def after_resending_confirmation_instructions_path_for(resource_name)
-      is_navigational_format? ? new_session_path(resource_name) : '/'
-    end
-    
-      def passthru
-    render status: 404, plain: 'Not found. Authentication passthru.'
-  end
-    
-                bypass_sign_in(user)
-          DEPRECATION
-          warden.session_serializer.store(resource, scope)
-        elsif warden.user(scope) == resource && !options.delete(:force)
-          # Do nothing. User already signed in and we are not forcing it.
-          true
-        else
-          warden.set_user(resource, options.merge!(scope: scope))
+            it 'executes the correct git command' do
+          allow(Fastlane::Actions).to receive(:sh).with('git add #{path[0].shellescape} #{path[1].shellescape}', anything).and_return('')
+          result = Fastlane::FastFile.new.parse('lane :test do
+            git_add(path: #{path})
+          end').runner.execute(:test)
         end
       end
     
-    module Devise
-  module Models
-    # Rememberable manages generating and clearing token for remembering the user
-    # from a saved cookie. Rememberable also has utility methods for dealing
-    # with serializing the user into the cookie and back from the cookie, trying
-    # to lookup the record based on the saved information.
-    # You probably wouldn't use rememberable methods directly, they are used
-    # mostly internally for handling the remember token.
-    #
-    # == Options
-    #
-    # Rememberable adds the following options in devise_for:
-    #
-    #   * +remember_for+: the time you want the user will be remembered without
-    #     asking for credentials. After this time the user will be blocked and
-    #     will have to enter their credentials again. This configuration is also
-    #     used to calculate the expires time for the cookie created to remember
-    #     the user. By default remember_for is 2.weeks.
-    #
-    #   * +extend_remember_period+: if true, extends the user's remember period
-    #     when remembered via cookie. False by default.
-    #
-    #   * +rememberable_options+: configuration options passed to the created cookie.
-    #
-    # == Examples
-    #
-    #   User.find(1).remember_me!  # regenerating the token
-    #   User.find(1).forget_me!    # clearing the token
-    #
-    #   # generating info to put into cookies
-    #   User.serialize_into_cookie(user)
-    #
-    #   # lookup the user based on the incoming cookie information
-    #   User.serialize_from_cookie(cookie_string)
-    module Rememberable
-      extend ActiveSupport::Concern
-    
-          # Checks whether the user session has expired based on configured time.
-      def timedout?(last_access)
-        !timeout_in.nil? && last_access && last_access <= timeout_in.ago
+              expect(result).to eq('swiftlint lint')
+        end
       end
     
-            # Returns the internal data associated with this plugin. This
-        # should NOT be called by the general public.
-        #
-        # @return [Hash]
-        def self.data
-          @data ||= {}
-        end
+            FastlaneCore::CommanderGenerator.new.generate(Cert::Options.available_options, command: c)
     
-            # This returns all the config classes for the various providers.
-        #
-        # @return [Hash]
-        def provider_configs
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.configs[:provider])
+    module Gitlab
+  module Ci
+    module Pipeline
+      # Class for preloading data associated with pipelines such as commit
+      # authors.
+      class Preloader
+        def self.preload!(pipelines)
+          ##
+          # This preloads all commits at once, because `Ci::Pipeline#commit` is
+          # using a lazy batch loading, what results in only one batched Gitaly
+          # call.
+          #
+          pipelines.each(&:commit)
+    
+          # Sets multiple keys to a given value.
+      #
+      # mapping - A Hash mapping the cache keys to their values.
+      # timeout - The time after which the cache key should expire.
+      def self.write_multiple(mapping, timeout: TIMEOUT)
+        Redis::Cache.with do |redis|
+          redis.multi do |multi|
+            mapping.each do |raw_key, value|
+              multi.set(cache_key_for(raw_key), value, ex: timeout)
             end
           end
         end
+      end
     
-        puts('Packaging plugins for offline usage')
-    
-      # We compare the before the update and after the update
-  def display_updated_plugins(previous_gem_specs_map)
-    update_count = 0
-    find_latest_gem_specs.values.each do |spec|
-      name = spec.name.downcase
-      if previous_gem_specs_map.has_key?(name)
-        if spec.version != previous_gem_specs_map[name].version
-          puts('Updated #{spec.name} #{previous_gem_specs_map[name].version.to_s} to #{spec.version.to_s}')
-          update_count += 1
+            def collection_options
+          { state: 'all', sort: 'created', direction: 'asc' }
         end
-      else
-        puts('Installed #{spec.name} #{spec.version.to_s}')
-        update_count += 1
       end
     end
-    
-    desc 'Move sass to sass.old, install sass theme updates, replace sass/custom with sass.old/custom'
-task :update_style, :theme do |t, args|
-  theme = args.theme || 'classic'
-  if File.directory?('sass.old')
-    puts 'removed existing sass.old directory'
-    rm_r 'sass.old', :secure=>true
   end
-  mv 'sass', 'sass.old'
-  puts '## Moved styles into sass.old/'
-  cp_r '#{themes_dir}/'+theme+'/sass/', 'sass', :remove_destination=>true
-  cp_r 'sass.old/custom/.', 'sass/custom/', :remove_destination=>true
-  puts '## Updated Sass ##'
 end
-    
-        def handle_gist_redirecting(data)
-      redirected_url = data.header['Location']
-      if redirected_url.nil? || redirected_url.empty?
-        raise ArgumentError, 'GitHub replied with a 302 but didn't provide a location in the response headers.'
-      end
-    
-    Liquid::Template.register_tag('img', Jekyll::ImageTag)
 
     
-      # Clean up any temporary storage used by this class.
-  def cleanup
-    cleanup_staging
-    cleanup_build
-  end # def cleanup
-    
-        File.write(build_path('description'), self.description + '\n')
-    
-        # Make one file. The installscript can unpack itself.
-    `cat #{install_script} #{payload} > #{output_path}`
-    FileUtils.chmod('+x', output_path)
-  end
-    
-        cleanup_staging
-    # Tell 'dir' to input '.' and chdir/prefix will help it figure out the
-    # rest.
-    dir.input('.')
-    @staging_path = dir.staging_path
-    dir.cleanup_build
-  end # def input
-    
-    Given 'I empty the application.js file' do
-  cd('.') do
-    transform_file('app/assets/javascripts/application.js') do |content|
-      ''
+            # attributes - A Hash containing the user details. The keys of this
+        #              Hash (and any nested hashes) must be symbols.
+        def initialize(attributes)
+          @attributes = attributes
+        end
+      end
     end
   end
 end
+
     
-      def migration_class_name
-    migration_name.camelize
-  end
-    
-        EMPTY_TYPE = 'inode/x-empty'
-    SENSIBLE_DEFAULT = 'application/octet-stream'
-    
-        # scale to the requested geometry and preserve the aspect ratio
-    def scale_to(new_geometry)
-      scale = [new_geometry.width.to_f / self.width.to_f , new_geometry.height.to_f / self.height.to_f].min
-      Paperclip::Geometry.new((self.width * scale).round, (self.height * scale).round)
+          if @email_domain_block.save
+        log_action :create, @email_domain_block
+        redirect_to admin_email_domain_blocks_path, notice: I18n.t('admin.email_domain_blocks.created_msg')
+      else
+        render :new
+      end
     end
-  end
-end
+    
+      def update
+    raise ActiveRecord::RecordNotFound if @web_subscription.nil?
+    
+        web_subscription = ::Web::PushSubscription.create!(
+      endpoint: subscription_params[:endpoint],
+      key_p256dh: subscription_params[:keys][:p256dh],
+      key_auth: subscription_params[:keys][:auth],
+      data: data,
+      user_id: active_session.user_id,
+      access_token_id: active_session.access_token_id
+    )
+    
+          # Returns the else branch of the `case` statement, if any.
+      #
+      # @return [Node] the else branch node of the `case` statement
+      # @return [nil] if the case statement does not have an else branch.
+      def else_branch
+        node_parts[-1]
+      end
+    
+          CopAnalysis.new(analysis.line_ranges, line)
+    end
