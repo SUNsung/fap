ed082d9fd3c21bc6f8cf4df693d9a64120fda1a9
SUNsung <weatherius@gmail.com>
2019-04-29 07:19:03 +0300

    Rewrite [26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb] File in: 2019-04-29 07:19:02

--- ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
+++ ruby/26tBD5S7P8yGY26Y63rljUUVGQ26JVzcr5cbf7M26gMy.rb
@@ -1,29 +1,89 @@
 
         
-                  def datetime_selector(options, html_options)
-            datetime = options.fetch(:selected) { value || default_datetime(options) }
-            @auto_index ||= nil
+            def add_entry(entry)
+      if @index.add?(entry.as_json.to_s)
+        @entries << entry.dup
+        @types[entry.type].count += 1 if entry.type
+      end
+    end
     
-    require 'action_view/helpers/tags/placeholderable'
+        private
     
-    module ActionView
-  class Template #:nodoc:
-    class Types
-      class Type
-        SET = Struct.new(:symbols).new([ :html, :text, :js, :css, :xml, :json ])
-    
-            def importer_class
-          LfsObjectImporter
+            css('.multi-line-signature').each do |node|
+          node.name = 'pre'
+          node.content = node.content.strip
         end
     
-            # Builds a diff note from a GitHub API response.
-        #
-        # note - An instance of `Sawyer::Resource` containing the note details.
-        def self.from_api_response(note)
-          matches = note.html_url.match(NOTEABLE_ID_REGEX)
+            if mod
+          if name == 'Index'
+            return slug.split('/')[1..-2].join('/')
+          elsif name == 'Angular'
+            return slug.split('/').last.split('-').first
+          end
+        end
     
-          private
+          def root
+        css('.nav-index-group').each do |node|
+          if heading = node.at_css('.nav-index-group-heading')
+            heading.name = 'h2'
+          end
+          node.parent.before(node.children)
+        end
     
-          def upload_dest
-        @upload_dest
+            css('p > code:first-child:last-child', 'td > code:first-child:last-child').each do |node|
+          next if node.previous.try(:content).present? || node.next.try(:content).present?
+          node.inner_html = node.inner_html.squish.gsub(/<br(\ \/)?>\s*/, '\n')
+          node.content = node.content.strip
+          node.name = 'pre' if node.content =~ /\s/
+          node.parent.before(node.parent.children).remove if node.parent.name == 'p'
+        end
+    
+      failure_message_for_should do |actual|
+    'expected #{actual.inspect} to have value #{expected.inspect} but was #{actual.value.inspect}'
+  end
+  failure_message_for_should_not do |actual|
+    'expected #{actual.inspect} to not have value #{expected.inspect} but it had'
+  end
+end
+    
+      class FetchWebfinger < Base
+    def perform(*_args)
+      # don't do real discovery in cucumber
+    end
+  end
+end
+
+    
+    describe ContactsController, :type => :controller do
+  describe '#index' do
+    before do
+      AppConfig.chat.enabled = true
+      @aspect = bob.aspects.create(:name => 'another aspect')
+      bob.share_with alice.person, @aspect
+      bob.share_with eve.person, @aspect
+      sign_in bob, scope: :user
+    end
+    
+          get :index, params: {conversation_id: @conv1.id}
+      save_fixture(html_for('body'), 'conversations_read')
+    end
+  end
+    
+        it 'generates a jasmine fixture', :fixture => true do
+      contact = alice.contact_for(bob.person)
+      aspect = alice.aspects.create(:name => 'people')
+      contact.aspects << aspect
+      contact.save
+      get :new, params: {person_id: bob.person.id}
+      save_fixture(html_for('body'), 'status_message_new')
+    end
+  end
+end
+
+    
+      describe '#index' do
+    context 'with a private post' do
+      before do
+        @alices_aspect = alice.aspects.where(name: 'generic').first
+        @post = alice.post(:status_message, text: 'hey', to: @alices_aspect.id)
       end
