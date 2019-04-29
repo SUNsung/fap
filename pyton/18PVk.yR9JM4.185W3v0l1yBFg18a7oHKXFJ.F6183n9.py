a90c04245403e5c93baa18d56114809d54d91e64
SUNsung <weatherius@gmail.com>
2019-04-29 09:13:44 +0300

    Rewrite [18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py] File in: 2019-04-29 09:13:44

--- pyton/18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py
+++ pyton/18PVk.yR9JM4.185W3v0l1yBFg18a7oHKXFJ.F6183n9.py
@@ -1,3 +1,76 @@
 
         
         
+class DefaultCategories(Enum):
+    
+        def within_past_week(self, timestamp):
+        '''Return True if timestamp is within past week, False otherwise.'''
+        ...
+    
+    
+class UserGraphService(object):
+    
+    from scrapy.commands import ScrapyCommand
+from scrapy.http import Request
+from scrapy.exceptions import UsageError
+from scrapy.utils.datatypes import SequenceExclude
+from scrapy.utils.spider import spidercls_for_request, DefaultSpider
+    
+        def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option('--verbose', '-v', dest='verbose', action='store_true',
+            help='also display twisted/python/platform info (useful for bug reports)')
+    
+        @classmethod
+    def default_decoder(cls, value):
+        try:
+            return pyrfc3339.parse(value)
+        except ValueError as error:
+            raise jose.DeserializationError(error)
+    
+    The JWS implementation in josepy only implements the base JOSE standard. In
+order to support the new header fields defined in ACME, this module defines some
+ACME-specific classes that layer on top of josepy.
+'''
+import josepy as jose
+    
+    AUTOHSTS_FREQ = 172800
+'''Minimum time since last increase to perform a new one: 48h'''
+    
+    
+here = os.path.abspath(os.path.dirname(__file__))
+    
+    
+  def Start( self ):
+    request_data = BuildRequestData()
+    request_data.update( { 'filetypes': self.filetypes } )
+    self._response = self.PostDataToHandler( request_data,
+                                             'semantic_completion_available' )
+    
+    
+  def Start( self ):
+    request_data = BuildRequestData( self._buffer_number )
+    if self._extra_data:
+      request_data.update( self._extra_data )
+    request_data[ 'event_name' ] = self._event_name
+    
+      with CurrentWorkingDirectory( unicode_dir ):
+    with MockVimBuffers( [ current_buffer ], [ current_buffer ] ):
+      with MockCompletionRequest( ServerResponse ):
+        ycm.SendCompletionRequest()
+        ok_( ycm.CompletionRequestReady() )
+        assert_that(
+          ycm.GetCompletionResponse(),
+          has_entries( {
+            'completions': empty(),
+            'completion_start_column': 1
+          } )
+        )
+    
+        server_message = {
+      'message': 'this message came from the server'
+    }
+    
+    def is_prime(n):
+    if n % 2 == 0:
+        return False
