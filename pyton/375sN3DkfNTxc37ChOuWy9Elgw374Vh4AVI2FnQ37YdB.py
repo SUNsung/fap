644da8ab95b2b9f9e58bbebe39d00554d1444ca1
SUNsung <weatherius@gmail.com>
2019-04-29 15:16:34 +0300

    Rewrite [375sN3DkfNTxc37ChOuWy9Elgw374Vh4AVI2FnQ37YdB.py] File in: 2019-04-29 15:16:34

--- pyton/375sN3DkfNTxc37ChOuWy9Elgw374Vh4AVI2FnQ37YdB.py
+++ pyton/375sN3DkfNTxc37ChOuWy9Elgw374Vh4AVI2FnQ37YdB.py
@@ -1,325 +1,164 @@
 
         
-        
-def main():
-    with open('supportedsites.html.in', 'r', encoding='utf-8') as tmplf:
-        template = tmplf.read()
+            return wrapped_view
     
-        bug_text = re.search(
-        r'(?s)#\s*BUGS\s*[^\n]*\s*(.*?)#\s*COPYRIGHT', readme).group(1)
-    dev_text = re.search(
-        r'(?s)(#\s*DEVELOPER INSTRUCTIONS.*?)#\s*EMBEDDING YOUTUBE-DL',
-        readme).group(1)
-    
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
-    
-                if 'playlist_mincount' in test_case:
-                assertGreaterEqual(
-                    self,
-                    len(res_dict['entries']),
-                    test_case['playlist_mincount'],
-                    'Expected at least %d in playlist %s, but got only %d' % (
-                        test_case['playlist_mincount'], test_case['url'],
-                        len(res_dict['entries'])))
-            if 'playlist_count' in test_case:
-                self.assertEqual(
-                    len(res_dict['entries']),
-                    test_case['playlist_count'],
-                    'Expected %d entries in playlist %s, but got %d.' % (
-                        test_case['playlist_count'],
-                        test_case['url'],
-                        len(res_dict['entries']),
-                    ))
-            if 'playlist_duration_sum' in test_case:
-                got_duration = sum(e['duration'] for e in res_dict['entries'])
-                self.assertEqual(
-                    test_case['playlist_duration_sum'], got_duration)
-    
-        def test_module_exec(self):
-        if sys.version_info >= (2, 7):  # Python 2.6 doesn't support package execution
-            subprocess.check_call([sys.executable, '-m', 'youtube_dl', '--version'], cwd=rootDir, stdout=_DEV_NULL)
+    from flaskr.auth import login_required
+from flaskr.db import get_db
     
     
-from youtube_dl.extractor import (
-    YoutubeIE,
-    DailymotionIE,
-    TEDIE,
-    VimeoIE,
-    WallaIE,
-    CeskaTelevizeIE,
-    LyndaIE,
-    NPOIE,
-    ComedyCentralIE,
-    NRKTVIE,
-    RaiPlayIE,
-    VikiIE,
-    ThePlatformIE,
-    ThePlatformFeedIE,
-    RTVEALaCartaIE,
-    FunnyOrDieIE,
-    DemocracynowIE,
-)
-    
-    
-def init_app(app):
-    '''Register database functions with the Flask app. This is called by
-    the application factory.
+def close_db(e=None):
+    '''If this request connected to the database, close the
+    connection.
     '''
-    app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+    db = g.pop('db', None)
+    
+    
+@pytest.mark.parametrize(('username', 'password', 'message'), (
+    ('', '', b'Username is required.'),
+    ('a', '', b'Password is required.'),
+    ('test', 'test', b'already registered'),
+))
+def test_register_validate_input(client, username, password, message):
+    response = client.post(
+        '/auth/register',
+        data={'username': username, 'password': password}
+    )
+    assert message in response.data
+    
+        def register_error_handler(self, code_or_exception, f):
+        '''Non-decorator version of the :meth:`errorhandler` error attach
+        function, akin to the :meth:`~flask.Flask.register_error_handler`
+        application-wide function of the :class:`~flask.Flask` object but
+        for error handlers limited to this blueprint.
+    
+        The following extra types are supported:
+    
+        class _FakeSignal(object):
+        '''If blinker is unavailable, create a fake class with the same
+        interface that allows sending of signals but will fail with an
+        error on anything else.  Instead of doing anything on send, it
+        will just ignore the arguments and do nothing instead.
+        '''
+    
+        :param source: the source code of the template to be
+                   rendered
+    :param context: the variables that should be available in the
+                    context of the template.
+    '''
+    ctx = _app_ctx_stack.top
+    ctx.app.update_template_context(context)
+    return _render(ctx.app.jinja_env.from_string(source),
+                   context, ctx.app)
 
     
+        @property
+    def max_content_length(self):
+        '''Read-only view of the ``MAX_CONTENT_LENGTH`` config key.'''
+        if current_app:
+            return current_app.config['MAX_CONTENT_LENGTH']
     
-@pytest.fixture
-def app():
-    '''Create and configure a new app instance for each test.'''
-    # create a temporary file to isolate the database for each test
-    db_fd, db_path = tempfile.mkstemp()
-    # create the app with common test config
-    app = create_app({
-        'TESTING': True,
-        'DATABASE': db_path,
-    })
+        with app.app_context():
+        rv = flask.url_for('index')
+        assert rv == 'https://localhost/'
     
-            .. versionadded:: 0.10
+        strings = []
+    with open(filepath, 'r') as f:
     
-            return Client.open(
-            self, environ,
-            as_tuple=as_tuple,
-            buffered=buffered,
-            follow_redirects=follow_redirects
-        )
+    def baomihua_download_by_id(id, title=None, output_dir='.', merge=True, info_only=False, **kwargs):
+    html = get_html('http://play.baomihua.com/getvideourl.aspx?flvid=%s&devicetype=phone_app' % id)
+    host = r1(r'host=([^&]*)', html)
+    assert host
+    type = r1(r'videofiletype=([^&]*)', html)
+    assert type
+    vid = r1(r'&stream_name=([^&]*)', html)
+    assert vid
+    dir_str = r1(r'&dir=([^&]*)', html).strip()
+    url = 'http://%s/%s/%s.%s' % (host, dir_str, vid, type)
+    _, ext, size = url_info(url)
+    print_info(site_info, title, type, size)
+    if not info_only:
+        download_urls([url], title, ext, size, output_dir, merge = merge)
     
+    #----------------------------------------------------------------------
+#helper
+#https://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
+def dictify(r,root=True):
+    if root:
+        return {r.tag : dictify(r, False)}
+    d=copy(r.attrib)
+    if r.text:
+        d['_text']=r.text
+    for x in r.findall('./*'):
+        if x.tag not in d:
+            d[x.tag]=[]
+        d[x.tag].append(dictify(x,False))
+    return d
     
-def git_is_clean():
-    return Popen(['git', 'diff', '--quiet']).wait() == 0
+        @classmethod
+    def checksum(cls, sha1_str):
+        if len(sha1_str) != 41:
+            return False
+        if not re.match(r'[0-9A-Za-z]{41}', sha1_str):
+            return False
+        sha1 = sha1_str[:-1]
+        if (15 & sum([int(char, 16) for char in sha1])) == int(sha1_str[-1], 16):
+            return True
+        return False
     
-        @app.route('/')
-    def index():
-        raise Exception('dummy')
+        type, ext, size = url_info(url[0], True)
+    size = urls_size(url)
     
-        lambda_facts = dict()
-    
-    #An example for a source nat
-    
-    
-DOCUMENTATION = '''
----
-module: ipa_dnszone
-author: Fran Fitzpatrick (@fxfitz)
-short_description: Manage FreeIPA DNS Zones
-description:
-- Add and delete an IPA DNS Zones using IPA API
-options:
-  zone_name:
-    description:
-    - The DNS zone name to which needs to be managed.
-    required: true
-  state:
-    description: State to ensure
-    required: false
-    default: present
-    choices: ['present', 'absent']
-extends_documentation_fragment: ipa.documentation
-version_added: '2.5'
-'''
-    
-    # ===========================================
-# Module execution.
-#
-import json
-import socket
-import traceback
-    
-        if module.params['user']:
-        params['deploy[local_username]'] = module.params['user']
-    
-            cmd = [le_path, 'follow', log]
-        if name:
-            cmd.extend(['--name', name])
-        if logtype:
-            cmd.extend(['--type', logtype])
-        rc, out, err = module.run_command(' '.join(cmd))
-    
-        SUMMARY_COMMAND = ('summary', 'summary -B')[is_version_higher_than_5_18()]
-    
-                # determine real callback
-            cb = response.meta['_callback']
-            if not cb:
-                if opts.callback:
-                    cb = opts.callback
-                elif opts.rules and self.first_response == response:
-                    cb = self.get_callback_from_rules(spider, response)
-    
-    from scrapy.utils.spider import iter_spider_classes
-from scrapy.commands import ScrapyCommand
-from scrapy.exceptions import UsageError
-from scrapy.utils.conf import arglist_to_dict
-from scrapy.utils.python import without_none_values
-    
-        def run(self, args, opts):
-        if opts.verbose:
-            versions = scrapy_components_versions()
-            width = max(len(n) for (n, _) in versions)
-            patt = '%-{}s : %s'.format(width)
-            for name, version in versions:
-                print(patt % (name, version))
-        else:
-            print('Scrapy %s' % scrapy.__version__)
-    
-        def _build_response(self, body, request):
-        request.meta['download_latency'] = self.headers_time-self.start_time
-        status = int(self.status)
-        headers = Headers(self.response_headers)
-        respcls = responsetypes.from_args(headers=headers, url=self._url)
-        return respcls(url=self._url, status=status, headers=headers, body=body)
-    
-            for i in html_json['sources']:
-            if 'src' in i:  #to avoid KeyError
-                if i['src'].startswith('https'):
-                    link_list.append((str(i['height']), i['src']))
-    
-    class BokeCC(VideoExtractor):
-    name = 'BokeCC'
-    
-    __all__ = ['cbs_download']
-    
-    __all__ = ['dailymotion_download']
-    
-    def get_api_key(page):
-    match = match1(page, pattern_inline_api_key)
-    # this happens only when the url points to a gallery page
-    # that contains no inline api_key(and never makes xhr api calls)
-    # in fact this might be a better approch for getting a temporary api key
-    # since there's no place for a user to add custom infomation that may
-    # misguide the regex in the homepage
-    if not match:
-        return match1(get_html('https://flickr.com'), pattern_inline_api_key)
-    return match
-    
-        html = get_content(url)
-    uuid_pattern = r''([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})''
-    id = r1(r'var vid='([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})'', html)
-    if id is None:
-        video_pattern = r''vid'\s*:\s*' + uuid_pattern
-        id = match1(html, video_pattern)
-    assert id, 'can't find video info'
-    return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
-    
-            else:
-            # gallery image
-            content = get_content(self.url)
-            image = json.loads(match1(content, r'image\s*:\s*({.*}),'))
-            ext = image['ext']
-            self.streams = {
-                'original': {
-                    'src': ['http://i.imgur.com/%s%s' % (image['hash'], ext)],
-                    'size': image['size'],
-                    'container': ext[1:]
-                },
-                'thumbnail': {
-                    'src': ['http://i.imgur.com/%ss%s' % (image['hash'], '.jpg')],
-                    'container': 'jpg'
-                }
-            }
-            self.title = image['title'] or image['hash']
-    
-    Loosely based on https://github.com/astropy/astropy/pull/347
-'''
+        ''' 倒排索引字典 '''
+    index = dict()
+    ''' 频率字典：统计每个词在多少篇文档中出现 '''
+    word_freq = dict()
     
     
-def display_temp(hass: HomeAssistant, temperature: float, unit: str,
-                 precision: float) -> float:
-    '''Convert temperature into preferred units/precision for display.'''
-    temperature_unit = unit
-    ha_unit = hass.config.units.temperature_unit
+def softmax(x, axis=-1):
+    '''
+    Examples:
+        n_dim = x.get_shape().ndims
+        assert n_dim >= 2
     
-    ax = fig.add_axes((0.63 + 0.18, 0.1, 0.16, 0.8))
-y = np.row_stack((fnx(), fnx(), fnx()))
-x = np.arange(10)
-y1, y2, y3 = fnx(), fnx(), fnx()
-ax.stackplot(x, y1, y2, y3)
-ax.set_xticks([])
-ax.set_yticks([])
+        References:
+        tflearn.prelu
+    '''
+    if channel_shared:
+        alpha_shape = get_shape(x)[-1:]
+    else:
+        alpha_shape = [1]
     
-    from pandas.io.msgpack.exceptions import *  # noqa
-from pandas.io.msgpack._version import version  # noqa
+        with tf.variable_scope(name or 'attention_for_dense', reuse=reuse):
+        W, b = get_wb([n_input, n_input])
     
-            # tz mismatch
-        exp = pd.Index([pd.Timestamp('2011-01-01 09:00'),
-                        pd.Timestamp('2011-01-01 10:00', tz=tz),
-                        pd.Timestamp('2011-01-01 11:00')], dtype=object)
-        tm.assert_index_equal(
-            idx.fillna(pd.Timestamp('2011-01-01 10:00', tz=tz)), exp)
-    
-        result = s[lambda x: [True, False, True, True]]
-    tm.assert_series_equal(result, s.iloc[[0, 2, 3]])
-    
-    define('num', default=100, help='number of iterations')
-define('dump', default=False, help='print template generated code and exit')
-    
-    define('url', default='ws://localhost:9001')
-define('name', default='Tornado')
-    
-    Reloading loses any Python interpreter command-line arguments (e.g. ``-u``)
-because it re-executes Python using ``sys.executable`` and ``sys.argv``.
-Additionally, modifying these variables will cause reloading to behave
-incorrectly.
-    
-    All defined options are available as attributes on this object.
-'''
-    
-        @gen.coroutine
-    def resolve(
-        self, host: str, port: int, family: int = 0
-    ) -> 'Generator[Any, Any, List[Tuple[int, Any]]]':
-        if is_valid_ip(host):
-            addresses = [host]
-        else:
-            # gethostbyname doesn't take callback as a kwarg
-            fut = Future()  # type: Future[Tuple[Any, Any]]
-            self.channel.gethostbyname(
-                host, family, lambda result, error: fut.set_result((result, error))
-            )
-            result, error = yield fut
-            if error:
-                raise IOError(
-                    'C-Ares returned error %s: %s while resolving %s'
-                    % (error, pycares.errno.strerror(error), host)
-                )
-            addresses = result.addresses
-        addrinfo = []
-        for address in addresses:
-            if '.' in address:
-                address_family = socket.AF_INET
-            elif ':' in address:
-                address_family = socket.AF_INET6
-            else:
-                address_family = socket.AF_UNSPEC
-            if family != socket.AF_UNSPEC and family != address_family:
-                raise IOError(
-                    'Requested socket family %d but got %d' % (family, address_family)
-                )
-            addrinfo.append((typing.cast(int, address_family), (address, port)))
-        return addrinfo
+            return self._call(x)
 
     
-        def test_asyncio_adapter(self):
-        # This test demonstrates that when using the asyncio coroutine
-        # runner (i.e. run_until_complete), the to_asyncio_future
-        # adapter is needed. No adapter is needed in the other direction,
-        # as demonstrated by other tests in the package.
-        @gen.coroutine
-        def tornado_coroutine():
-            yield gen.moment
-            raise gen.Return(42)
+        Examples:
+        l2_regularizer = l2(0.01)
+        tf.get_variable(..., regularizer=l2_regularizer, ...)
+    '''
+    
+    
+def get_wb(shape,
+           w_initializer=truncated_normal,
+           b_initializer=zeros,
+           w_regularizer=l2_regularizer,
+           b_regularizer=None,  # 一般不对偏置做权重惩罚，可能会导致欠拟合
+           name=None):
+    ''''''
+    name = '' if name is None else name + '_'
+    W = tf.get_variable(name + 'W', shape=shape,
+                        dtype=tf_float, initializer=w_initializer, regularizer=w_regularizer)
+    b = tf.get_variable(name + 'b', shape=shape[-1:],
+                        dtype=tf_float, initializer=b_initializer, regularizer=b_regularizer)
+    return W, b
+    
+    # 可以通过相同的方式获取每个单词以及任一个 n-gram 的向量
+print(model.wv['hello'])
+print(model.wv['<h'])
+'''
+[-0.03481839  0.00606661  0.02581969  0.00188777  0.0325358 ]
+[ 0.04481247 -0.1784363  -0.03192253  0.07162753  0.16744071]
+'''
+print()
