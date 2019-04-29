210d32bdca92e3ae5bd4f7369f1e8f9775cb64d3
SUNsung <weatherius@gmail.com>
2019-04-29 16:20:37 +0300

    Rewrite [42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb] File in: 2019-04-29 16:20:36

--- ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
+++ ruby/42hTAjaoOXwaY42mKe7EMSYF7M42r3G58CJKBho42tq2.rb
@@ -1,186 +1,196 @@
 
         
-                  if options['multiple']
-            add_default_name_and_id_for_value(@checked_value, options)
-            options.delete('multiple')
-          else
-            add_default_name_and_id(options)
-          end
-    
-                content ||= Translator
-              .new(object, @object_name, method_and_value, scope: 'helpers.label')
-              .translate
-            content ||= @method_name.humanize
-    
-            class << self
-          def field_type
-            @field_type ||= name.split('::').last.sub('Field', '').downcase
+                # Returns the instance variables as a hash of key-value pairs.
+        def instance_variables_hash
+          instance_variables.inject({}) do |acc, iv|
+            acc[iv.to_s[1..-1]] = instance_variable_get(iv)
+            acc
           end
         end
     
-        # Controls whether an action should be rendered using a layout.
-    # If you want to disable any <tt>layout</tt> settings for the
-    # current action so that it is rendered without a layout then
-    # either override this method in your controller to return false
-    # for that action or set the <tt>action_has_layout</tt> attribute
-    # to false before rendering.
-    def action_has_layout?
-      @_action_has_layout
-    end
-    
-        def find_file(path, prefixes = [], *args)
-      _find_all(path, prefixes, args, true).first || raise(MissingTemplate.new(self, path, prefixes, *args))
-    end
-    
-          # Override to prevent #cause resetting during re-raise.
-      attr_reader :cause
-    
-        begin
-      json = JSON.parse(self.body)
-    rescue JSON::ParserError => e
-      elog('#{e.class} #{e.message}\n#{e.backtrace * '\n'}')
-    end
-    
-    end
-end
-end
-    
-    =begin
- +------+----------------+-------------------------------------------+
-   | HEX  | NAME           | DESCRIPTION                               |
-   +------+----------------+-------------------------------------------+
-   | HEX  | NAME           | DESCRIPTION                               |
-   | 0x01 | CALLED NUMBER  | Number/extension being called             |
-   | 0x02 | CALLING NUMBER | Calling number                            |
-   | 0x03 | CALLING ANI    | Calling number ANI for billing            |
-   | 0x04 | CALLING NAME   | Name of caller                            |
-   | 0x05 | CALLED CONTEXT | Context for number                        |
-   | 0x06 | USERNAME       | Username (peer or user) for               |
-   |      |                | authentication                            |
-   | 0x07 | PASSWORD       | Password for authentication               |
-   | 0x08 | CAPABILITY     | Actual CODEC capability                   |
-   | 0x09 | FORMAT         | Desired CODEC format                      |
-   | 0x0a | LANGUAGE       | Desired language                          |
-   | 0x0b | VERSION        | Protocol version                          |
-   | 0x0c | ADSICPE        | CPE ADSI capability                       |
-   | 0x0d | DNID           | Originally dialed DNID                    |
-   | 0x0e | AUTHMETHODS    | Authentication method(s)                  |
-   | 0x0f | CHALLENGE      | Challenge data for MD5/RSA                |
-   | 0x10 | MD5 RESULT     | MD5 challenge result                      |
-   | 0x11 | RSA RESULT     | RSA challenge result                      |
-   | 0x12 | APPARENT ADDR  | Apparent address of peer                  |
-   | 0x13 | REFRESH        | When to refresh registration              |
-   | 0x14 | DPSTATUS       | Dialplan status                           |
-   | 0x15 | CALLNO         | Call number of peer                       |
-   | 0x16 | CAUSE          | Cause                                     |
-   | 0x17 | IAX UNKNOWN    | Unknown IAX command                       |
-   | 0x18 | MSGCOUNT       | How many messages waiting                 |
-   | 0x19 | AUTOANSWER     | Request auto-answering                    |
-   | 0x1a | MUSICONHOLD    | Request musiconhold with QUELCH           |
-   | 0x1b | TRANSFERID     | Transfer Request Identifier               |
-   | 0x1c | RDNIS          | Referring DNIS                            |
-   | 0x1d | Reserved       | Reserved for future use                   |
-   | 0x1e | Reserved       | Reserved for future use                   |
-   | 0x1f | DATETIME       | Date/Time                                 |
-   | 0x20 | Reserved       | Reserved for future use                   |
-   | 0x21 | Reserved       | Reserved for future use                   |
-   | 0x22 | Reserved       | Reserved for future use                   |
-   | 0x23 | Reserved       | Reserved for future use                   |
-   | 0x24 | Reserved       | Reserved for future use                   |
-   | 0x25 | Reserved       | Reserved for future use                   |
-   | 0x26 | CALLINGPRES    | Calling presentation                      |
-   | 0x27 | CALLINGTON     | Calling type of number                    |
-   | 0x28 | CALLINGTNS     | Calling transit network select            |
-   | 0x29 | SAMPLINGRATE   | Supported sampling rates                  |
-   | 0x2a | CAUSECODE      | Hangup cause                              |
-   | 0x2b | ENCRYPTION     | Encryption format                         |
-   | 0x2c | ENCKEY         | Reserved for future Use                   |
-   | 0x2d | CODEC PREFS    | CODEC Negotiation                         |
-   | 0x2e | RR JITTER      | Received jitter, as in RFC 3550           |
-   | 0x2f | RR LOSS        | Received loss, as in RFC 3550             |
-   | 0x30 | RR PKTS        | Received frames                           |
-   | 0x31 | RR DELAY       | Max playout delay for received frames in  |
-   |      |                | ms                                        |
-   | 0x32 | RR DROPPED     | Dropped frames (presumably by jitter      |
-   |      |                | buffer)                                   |
-   | 0x33 | RR OOO         | Frames received Out of Order              |
-   | 0x34 | OSPTOKEN       | OSP Token Block                           |
-   +------+----------------+-------------------------------------------+
-=end
-    
-    
-end
-end
-end
-
-    
-              # Decodes the Rex::Proto::Kerberos::Model::Element from the input. This
-          # method has been designed to be overridden by subclasses.
-          #
-          # @raise [NoMethodError]
-          def decode(input)
-            raise ::NoMethodError, 'Method designed to be overridden'
-          end
-    
-              # Decodes the key_expiration field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Time]
-          def decode_key_expiration(input)
-            input.value[0].value
-          end
-    
-                elems << OpenSSL::ASN1::ASN1Data.new([encode_options], 0, :CONTEXT_SPECIFIC) if options
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_cname], 1, :CONTEXT_SPECIFIC) if cname
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_realm], 2, :CONTEXT_SPECIFIC) if realm
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_sname], 3, :CONTEXT_SPECIFIC) if sname
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_from], 4, :CONTEXT_SPECIFIC) if from
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_till], 5, :CONTEXT_SPECIFIC) if till
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_rtime], 6, :CONTEXT_SPECIFIC) if rtime
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_nonce], 7, :CONTEXT_SPECIFIC) if nonce
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_etype], 8, :CONTEXT_SPECIFIC) if etype
-            elems << OpenSSL::ASN1::ASN1Data.new([encode_enc_auth_data], 10, :CONTEXT_SPECIFIC) if enc_auth_data
-    
-      def up
-    Photo.joins('INNER JOIN posts ON posts.guid = photos.status_message_guid')
-         .where(posts: {type: 'StatusMessage', public: true}).update_all(public: true)
-    
-    When /^I toggle all nsfw posts$/ do
-  all('a.toggle_nsfw_state').each &:click
-end
-    
-      class PostToService < Base
-    def perform(*_args)
-      # don't post to services in cucumber
-    end
-  end
-    
-        it 'generates a jasmine fixture', fixture: true do
-      session[:mobile_view] = true
-      get :new, format: :mobile
-      save_fixture(html_for('body'), 'conversations_new_mobile')
-    end
-  end
-end
-
-    
-          get :new, params: {invite: {token: code.token}}
-      expect(response).not_to be_redirect
-    end
-  end
-    
-        def sort_options(options)
-      not_applicable_to_capistrano = %w(quiet silent verbose)
-      options.reject! do |(switch, *)|
-        switch =~ /--#{Regexp.union(not_applicable_to_capistrano)}/
-      end
-    
-            @response = (gets || '').chomp
-      end
-    
-            if Rake::Task.task_defined?('deploy:check')
-          before 'deploy:check', '#{scm_name}:check'
+            # This is the method called to provision the system. This method
+        # is expected to do whatever necessary to provision the system (create files,
+        # SSH, etc.)
+        def provision!
         end
     
-    set_if_empty :format, :airbrussh
-set_if_empty :log_level, :debug
+            # This contains all the registered provider capabilities.
+        #
+        # @return [Hash<Symbol, Registry>]
+        attr_reader :provider_capabilities
+    
+            # Sentinel value denoting that a value has not been set.
+        UNSET_VALUE = Object.new
+    
+      #
+  # Split the URI into the resource being requested and its query string.
+  #
+  def update_uri_parts
+    # If it has a query string, get the parts.
+    if ((self.raw_uri) and (md = self.raw_uri.match(/(.+?)\?(.*)$/)))
+      self.uri_parts['QueryString'] = parse_cgi_qstring(md[2])
+      self.uri_parts['Resource']    = md[1]
+    # Otherwise, just assume that the URI is equal to the resource being
+    # requested.
+    else
+      self.uri_parts['QueryString'] = {}
+      self.uri_parts['Resource']    = self.raw_uri
+    end
+    
+      #
+  # Payload types were identified from xCAT-server source code (IPMI.pm)
+  #
+  PAYLOAD_IPMI = 0
+  PAYLOAD_SOL  = 1
+  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
+  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
+  PAYLOAD_RAKP1 = 0x12
+  PAYLOAD_RAKP2 = 0x13
+  PAYLOAD_RAKP3 = 0x14
+  PAYLOAD_RAKP4 = 0x15
+    
+              # Encodes the msg_type field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_msg_type
+            bn = OpenSSL::BN.new(msg_type.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+              # Encodes the pvno field
+          #
+          # @return [OpenSSL::ASN1::Integer]
+          def encode_pvno
+            bn = OpenSSL::BN.new(pvno.to_s)
+            int = OpenSSL::ASN1::Integer.new(bn)
+    
+      class IncludeCodeTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      @title = nil
+      @file = nil
+      if markup.strip =~ /\s*lang:(\S+)/i
+        @filetype = $1
+        markup = markup.strip.sub(/lang:\S+/i,'')
+      end
+      if markup.strip =~ /(.*)?(\s+|^)(\/*\S+)/i
+        @title = $1 || nil
+        @file = $3
+      end
+      super
+    end
+    
+    
+module OctopressLiquidFilters
+    
+          unless file.file?
+        return 'File #{file} could not be found'
+      end
+    
+        def render(context)
+      output = super
+      types = {
+        '.mp4' => 'type='video/mp4; codecs=\'avc1.42E01E, mp4a.40.2\''',
+        '.ogv' => 'type='video/ogg; codecs=theora, vorbis'',
+        '.webm' => 'type='video/webm; codecs=vp8, vorbis''
+      }
+      if @videos.size > 0
+        video =  '<video #{sizes} preload='metadata' controls #{poster}>'
+        @videos.each do |v|
+          video << '<source src='#{v}' #{types[File.extname(v)]}>'
+        end
+        video += '</video>'
+      else
+        'Error processing input, expected syntax: {% video url/to/video [url/to/video] [url/to/video] [width height] [url/to/poster] %}'
+      end
+    end
+    
+        if input_type == 'pleaserun'
+      # Special case for pleaserun that all parameters are considered the 'command'
+      # to run through pleaserun.
+      input.input(args)
+    else
+      # Each remaining command line parameter is used as an 'input' argument.
+      # For directories, this means paths. For things like gem and python, this
+      # means package name or paths to the packages (rails, foo-1.0.gem, django,
+      # bar/setup.py, etc)
+      args.each do |arg|
+        input.input(arg)
+      end
+    end
+    
+          if param == :flag
+        # Automatically make 'flag' (boolean) options tunable with '--[no-]...'
+        flag = flag.collect { |f| '--[no-]#{type}-#{f.gsub(/^--/, '')}' }
+      else
+        flag = flag.collect { |f| '--#{type}-#{f.gsub(/^--/, '')}' }
+      end
+    
+      option '--registry', 'NPM_REGISTRY',
+    'The npm registry to use instead of the default.'
+    
+      option '--identifier-prefix', 'IDENTIFIER_PREFIX',
+    'Reverse domain prefix prepended to package identifier, ' \
+    'ie. 'org.great.my'. If this is omitted, the identifer ' \
+    'will be the package name.'
+  option '--payload-free', :flag, 'Define no payload, assumes use of script options.',
+    :default => false
+  option '--ownership', 'OWNERSHIP',
+    '--ownership option passed to pkgbuild. Defaults to 'recommended'. ' \
+    'See pkgbuild(1).', :default => 'recommended' do |value|
+    if !OWNERSHIP_OPTIONS.include?(value)
+      raise ArgumentError, 'osxpkg-ownership value of '#{value}' is invalid. ' \
+        'Must be one of #{OWNERSHIP_OPTIONS.join(', ')}'
+    end
+    value
+  end
+    
+        # This value is used later in the template for PKGINFO
+    size = safesystemout('du', '-sk', build_path).split(/\s+/)[0].to_i * 1024
+    builddate = Time.new.to_i
+    
+      # Output a zipfile.
+  def output(output_path)
+    output_check(output_path)
+    realpath = Pathname.new(output_path).realdirpath.to_s
+    ::Dir.chdir(staging_path) do
+      safesystem('zip', '-9r', realpath, '.')
+    end
+  end # def output
+end # class FPM::Package::Tar
+
+    
+        return process.exit_code
+  end # def execmd
+    
+    desc 'Clean up files.'
+task :clean do |t|
+  FileUtils.rm_rf 'doc'
+  FileUtils.rm_rf 'tmp'
+  FileUtils.rm_rf 'pkg'
+  FileUtils.rm_rf 'public'
+  FileUtils.rm 'test/debug.log' rescue nil
+  FileUtils.rm 'test/paperclip.db' rescue nil
+  Dir.glob('paperclip-*.gem').each{|f| FileUtils.rm f }
+end
+
+    
+    Given 'I empty the application.js file' do
+  cd('.') do
+    transform_file('app/assets/javascripts/application.js') do |content|
+      ''
+    end
+  end
+end
+    
+        def type_from_file_contents
+      type_from_mime_magic || type_from_file_command
+    rescue Errno::ENOENT => e
+      Paperclip.log('Error while determining content type: #{e}')
+      SENSIBLE_DEFAULT
+    end
+    
+        def define_setter
+      name = @name
+      @klass.send :define_method, '#{@name}=' do |file|
+        send(name).assign(file)
+      end
+    end
