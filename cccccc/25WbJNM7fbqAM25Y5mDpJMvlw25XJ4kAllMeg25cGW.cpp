a3c56cbd8bb80388d52b39efd3dd96e50a5d5f95
SUNsung <weatherius@gmail.com>
2019-04-30 22:01:50 +0300

    Rewrite [25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp] File in: 2019-04-30 22:01:49

--- cccccc/25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp
+++ cccccc/25WbJNM7fbqAM25Y5mDpJMvlw25XJ4kAllMeg25cGW.cpp
@@ -1,298 +1,97 @@
 
         
-        #include 'ExtensionManager.h'
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
-#include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TBufferTransports.h>
-    
-    typedef struct _ExtensionException__isset {
-  _ExtensionException__isset() : code(false), message(false), uuid(false) {}
-  bool code :1;
-  bool message :1;
-  bool uuid :1;
-} _ExtensionException__isset;
-    
-    /**
- * @brief Represents a hardware driver type that SMART api can you use to query
- * device information.
- *
- * @param driver name of SMART controller driver
- * @param maxID max ID number of which disks on the controller is monitored
- */
-struct hardwareDriver {
-  std::string driver;
-  size_t maxID;
-};
-    
-      // Get the hash value for the kernel's .text memory segment
-  auto f2 = osquery::readFile(kKernelTextHashPath, content);
-  if (f2.ok()) {
-    boost::trim(content);
-    text_segment_hash = content;
-  } else {
-    VLOG(1) << 'Cannot read file: ' << kKernelTextHashPath;
-    return results;
-  }
-    
-    // If the following flag is set, then command execution stops
-// at an error if we are not interactive.
-static int bail_on_error = 0;
-    
-    
-    {      if (entry.first == serial->second) {
-        matched = true;
-        row['disk_id'] = entry.second['disk_id'];
-        row['driver_type'] = entry.second['driver_type'];
-      }
-    }
-    
-    namespace osquery {
-namespace perf_event_open {
-    }
-    }
-    
-      DHTRegistry();
-    
-    void DHTReplaceNodeTask::sendMessage()
-{
-  std::shared_ptr<DHTNode> questionableNode = bucket_->getLRUQuestionableNode();
-  if (!questionableNode) {
-    setFinished(true);
-  }
-  else {
-    getMessageDispatcher()->addMessageToQueue(
-        getMessageFactory()->createPingMessage(questionableNode), timeout_,
-        make_unique<DHTPingReplyMessageCallback<DHTReplaceNodeTask>>(this));
-  }
-}
-    
-      void onReceived(const DHTPingReplyMessage* message);
-    
-    DHTResponseMessage::DHTResponseMessage(
-    const std::shared_ptr<DHTNode>& localNode,
-    const std::shared_ptr<DHTNode>& remoteNode,
-    const std::string& transactionID)
-    : DHTAbstractMessage(localNode, remoteNode, transactionID)
-{
-}
-    
-    std::shared_ptr<DHTNode> DHTRoutingTable::getNode(const unsigned char* nodeID,
-                                                  const std::string& ipaddr,
-                                                  uint16_t port) const
-{
-  std::shared_ptr<DHTBucket> bucket = getBucketFor(nodeID);
-  return bucket->getNode(nodeID, ipaddr, port);
-}
-    
-    #include <cstring>
-#include <cassert>
-#include <cstdio>
-#include <utility>
-    
-    #endif // D_DHT_TASK_H
+        
+    {} // namespace caffe2
 
     
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
     
-    {} // namespace aria2
+    // Called by Init/NewFrame/Shutdown
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateFontsTexture();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyFontsTexture();
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyDeviceObjects();
 
     
-    public:
-  DHTTokenTracker();
+        // Setup render state
+    const float blend_factor[4] = { 0.f, 0.f, 0.f, 0.f };
+    ctx->OMSetBlendState(g_pBlendState, blend_factor, 0xffffffff);
+    ctx->OMSetDepthStencilState(g_pDepthStencilState, 0);
+    ctx->RSSetState(g_pRasterizerState);
     
-    DHTTokenUpdateCommand::DHTTokenUpdateCommand(cuid_t cuid, DownloadEngine* e,
-                                             std::chrono::seconds interval)
-    : TimeBasedCommand{cuid, e, std::move(interval)}, tokenTracker_{nullptr}
-{
+    
+    {    return 0;
 }
     
-    const std::string& DNSCache::find(const std::string& hostname,
-                                  uint16_t port) const
-{
-  auto target = std::make_shared<CacheEntry>(hostname, port);
-  auto i = entries_.find(target);
-  if (i == entries_.end()) {
-    return A2STR::NIL;
-  }
-  else {
-    return (*i)->getGoodAddr();
-  }
-}
     
-        bool add(const std::string& addr);
-    
-      uint64_t single_refill_amount =
-      delayed_write_rate_ * kRefillInterval / kMicrosPerSecond;
-  if (bytes_left_ + single_refill_amount >= num_bytes) {
-    // Wait until a refill interval
-    // Never trigger expire for less than one refill interval to avoid to get
-    // time.
-    bytes_left_ = bytes_left_ + single_refill_amount - num_bytes;
-    last_refill_time_ = time_now + kRefillInterval;
-    return kRefillInterval + sleep_debt;
-  }
-    
-    #pragma once
-    
-    #include 'rocksdb/db.h'
-#include 'rocksdb/slice.h'
-#include 'rocksdb/options.h'
-    
-    // Example structure that describes a compaction task.
-struct CompactionTask {
-  CompactionTask(
-      DB* _db, Compactor* _compactor,
-      const std::string& _column_family_name,
-      const std::vector<std::string>& _input_file_names,
-      const int _output_level,
-      const CompactionOptions& _compact_options,
-      bool _retry_on_fail)
-          : db(_db),
-            compactor(_compactor),
-            column_family_name(_column_family_name),
-            input_file_names(_input_file_names),
-            output_level(_output_level),
-            compact_options(_compact_options),
-            retry_on_fail(_retry_on_fail) {}
-  DB* db;
-  Compactor* compactor;
-  const std::string& column_family_name;
-  std::vector<std::string> input_file_names;
-  int output_level;
-  CompactionOptions compact_options;
-  bool retry_on_fail;
-};
-    
-      // Commit.  Since the snapshot was advanced, the write done outside of the
-  // transaction does not prevent this transaction from Committing.
-  s = txn->Commit();
-  assert(s.ok());
-  delete txn;
-  // Clear snapshot from read options since it is no longer valid
-  read_options.snapshot = nullptr;
-    
-    #include 'rocksdb/db.h'
-    
-      // A list of properties that describe some details about the current
-  // operation.  Same field in op_properties[] might have different
-  // meanings for different operations.
-  uint64_t op_properties[kNumOperationProperties];
-    
-      // Get the number of jobs scheduled in the ThreadPool queue.
-  virtual unsigned int GetQueueLen() const = 0;
-    
-    namespace rocksdb {
-    }
-    
-    TEST(ByteTest, SetBit) {
-  unsigned char byte_value = 0xFF;
-  Byte value(&byte_value);
-  value.set_bit_0(1);
-  EXPECT_EQ(0xFD, value.get_byte());
-  value.set_bit_0(7);
-  EXPECT_EQ(0x7D, value.get_byte());
-  value.set_bit_1(7);
-  EXPECT_EQ(0xFD, value.get_byte());
-  value.set_value(0x77);
-  value.set_bit_1(0);
-  EXPECT_EQ(0x77, value.get_byte());
-    }
-    
-    ObjectExtendedInfo60D::ObjectExtendedInfo60D() {}
-const uint32_t ObjectExtendedInfo60D::ID = 0x60D;
-    
-        auto it_lower = std::lower_bound(
-        speed_limit_.speed_limit_points().begin(),
-        speed_limit_.speed_limit_points().end(), s,
-        [](const std::pair<double, double>& point, const double curr_s) {
-          return point.first < curr_s;
-        });
-    
-      } else if (type == 'second_order') {
-    for (uint32_t r = 2; r < num_params; ++r) {
-      for (uint32_t c = 2; c < num_params; ++c) {
-        (*term_matrix)(r, c) = x_pow[r + c - 3];
-      }
-    }
-    (*term_matrix).block(0, 0, num_params, 2) =
-        Eigen::MatrixXd::Zero(num_params, 2);
-    (*term_matrix).block(0, 0, 2, num_params) =
-        Eigen::MatrixXd::Zero(2, num_params);
-    
-    #include 'modules/canbus/vehicle/gem/gem_vehicle_factory.h'
-    
-      Byte t1(bytes + 3);
-  int32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-        for (i = 1; i < 20000; i++)
-    {
-        uint32_t key = i * 37;
-        swRbtree_insert(tree, key, (void *) (long) (i * 8));
-    }
-    
-    #endif
-
-    
-            void addWrite() {
-            if (m_write) return;
-            m_write = new QSocketNotifier(m_ctx->c.fd, QSocketNotifier::Write, 0);
-            connect(m_write, SIGNAL(activated(int)), this, SLOT(write()));
+    {        template <int i>
+        static inline void f(uint32_t *a, uint32_t *b) {
+            switch (state) {
+            case 1:
+                a[i % 5] += ((a[(3 + i) % 5] & (a[(2 + i) % 5] ^ a[(1 + i) % 5])) ^ a[(1 + i) % 5]) + b[i] + 0x5a827999 + rol(a[(4 + i) % 5], 5);
+                a[(3 + i) % 5] = rol(a[(3 + i) % 5], 30);
+                break;
+            case 2:
+                b[i] = blk(b, i);
+                a[(1 + i) % 5] += ((a[(4 + i) % 5] & (a[(3 + i) % 5] ^ a[(2 + i) % 5])) ^ a[(2 + i) % 5]) + b[i] + 0x5a827999 + rol(a[(5 + i) % 5], 5);
+                a[(4 + i) % 5] = rol(a[(4 + i) % 5], 30);
+                break;
+            case 3:
+                b[(i + 4) % 16] = blk(b, (i + 4) % 16);
+                a[i % 5] += (a[(3 + i) % 5] ^ a[(2 + i) % 5] ^ a[(1 + i) % 5]) + b[(i + 4) % 16] + 0x6ed9eba1 + rol(a[(4 + i) % 5], 5);
+                a[(3 + i) % 5] = rol(a[(3 + i) % 5], 30);
+                break;
+            case 4:
+                b[(i + 8) % 16] = blk(b, (i + 8) % 16);
+                a[i % 5] += (((a[(3 + i) % 5] | a[(2 + i) % 5]) & a[(1 + i) % 5]) | (a[(3 + i) % 5] & a[(2 + i) % 5])) + b[(i + 8) % 16] + 0x8f1bbcdc + rol(a[(4 + i) % 5], 5);
+                a[(3 + i) % 5] = rol(a[(3 + i) % 5], 30);
+                break;
+            case 5:
+                b[(i + 12) % 16] = blk(b, (i + 12) % 16);
+                a[i % 5] += (a[(3 + i) % 5] ^ a[(2 + i) % 5] ^ a[(1 + i) % 5]) + b[(i + 12) % 16] + 0xca62c1d6 + rol(a[(4 + i) % 5], 5);
+                a[(3 + i) % 5] = rol(a[(3 + i) % 5], 30);
+                break;
+            case 6:
+                b[i] += a[4 - i];
+            }
         }
-    
-        Q_OBJECT
-    
-    namespace swoole
-{
-class Channel
-{
-public:
-    enum opcode
-    {
-        PRODUCER = 1,
-        CONSUMER = 2,
     };
+    
+        struct optparse_long longopts[] = {
+        {'port', 'p', OPTPARSE_REQUIRED},
+        {'help', 'h', OPTPARSE_NONE},
+        {'passphrase', 'a', OPTPARSE_REQUIRED},
+        {'key', 'k', OPTPARSE_REQUIRED},
+        {'cert', 'c', OPTPARSE_REQUIRED},
+        {'dh_params', 'd', OPTPARSE_REQUIRED},
+        {0}
+    };
+    
+    #include 'WebSocketProtocol.h'
+#include 'AsyncSocketData.h'
+#include 'PerMessageDeflate.h'
+    
+    #ifndef LOOPDATA_H
+#define LOOPDATA_H
+    
+            if (zlibContext->dynamicDeflationBuffer.length()) {
+            zlibContext->dynamicDeflationBuffer.append(zlibContext->deflationBuffer, DEFLATE_OUTPUT_CHUNK - deflationStream.avail_out);
     }
+    
+        /* Subscribe to a topic according to MQTT rules and syntax */
+    void subscribe(std::string_view topic) {
+        WebSocketContextData<SSL> *webSocketContextData = (WebSocketContextData<SSL> *) us_new_socket_context_ext(SSL,
+            (us_new_socket_context_t *) us_new_socket_context(SSL, (us_new_socket_t *) this)
+        );
     }
     
-    typedef struct
-{
-    int fd;
-    int key;
-} swFdInfo;
-    
-    
-    {
-    {        if (i % 100 == 0)
-        {
-            usleep(10);
-        }
-    }
-}
-    
-    #include <gtest/gtest.h>
-#include <initializer_list>
-#include <utility>
-    
-    
-    {    void *alloc(size_t _size)
-    {
-        if (_size >= _callback_buffer->size)
-        {
-            size_t new_size = _callback_buffer->size * 2;
-            while (new_size < _size + 1)
-            {
-                new_size *= 2;
+            if (opCode == uWS::TEXT) {
+            if (!uWS::protocol::isValidUtf8((unsigned char *)data, length)) {
+                /* Return break */
+                return true;
             }
-            if (swString_extend(_callback_buffer, new_size) < 0)
-            {
-                abort();
-            }
+        } else if (opCode == uWS::CLOSE) {
+            uWS::protocol::parseClosePayload((char *)data, length);
         }
-        length = _size;
-        buffer = _callback_buffer->str;
-        ((char *) buffer)[_size] = '\0';
-        return buffer;
-    }
-};
