0854e8c92698a2dcbe8e2f6868fdb919ba8c0a6c
SUNsung <weatherius@gmail.com>
2019-04-30 08:36:08 +0300

    Rewrite [06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py] File in: 2019-04-30 08:36:08

--- pyton/06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py
+++ pyton/06u9NC.qJbvJs06PpzJsimffRg06nrtqiU0Ky6066S8.py
@@ -1,218 +1,23 @@
 
         
-            def notify_call_escalated(self, call):
-        pass
-    
-        def __init__(self, from_user_id, to_user_id, request_status, timestamp):
-        self.from_user_id = from_user_id
-        self.to_user_id = to_user_id
-        self.request_status = request_status
-        self.timestamp = timestamp
-    
-        def categorize(self, transaction):
-        if transaction.seller in self.seller_category_map:
-            return self.seller_category_map[transaction.seller]
-        if transaction.seller in self.seller_category_overrides_map:
-            seller_category_map[transaction.seller] = \
-                self.manual_overrides[transaction.seller].peek_min()
-            return self.seller_category_map[transaction.seller]
-        return None
-    
-        def mapper(self, _, line):
-        '''Parse each log line, extract and transform relevant lines.
-    
-    # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
-    
-def deserialize(config, custom_objects=None):
-    return deserialize_keras_object(config,
-                                    module_objects=globals(),
-                                    custom_objects=custom_objects,
-                                    printable_module_name='regularizer')
-    
-    from ..layers.merge import concatenate
-from .. import backend as K
-from ..layers.core import Lambda
-from ..engine.training import Model
-from ..models import clone_model
-from ..utils.generic_utils import to_list
-    
-            # initialize state if None
-        if self.states[0] is None:
-            if hasattr(self.cell.state_size, '__len__'):
-                self.states = [K.zeros(get_tuple_shape(dim))
-                               for dim in self.cell.state_size]
-            else:
-                self.states = [K.zeros(get_tuple_shape(self.cell.state_size))]
-        elif states is None:
-            if hasattr(self.cell.state_size, '__len__'):
-                for state, dim in zip(self.states, self.cell.state_size):
-                    K.set_value(state, np.zeros(get_tuple_shape(dim)))
-            else:
-                K.set_value(self.states[0],
-                            np.zeros(get_tuple_shape(self.cell.state_size)))
-        else:
-            states = to_list(states, allow_tuple=True)
-            if len(states) != len(self.states):
-                raise ValueError('Layer ' + self.name + ' expects ' +
-                                 str(len(self.states)) + ' states, '
-                                 'but it received ' + str(len(states)) +
-                                 ' state values. Input received: ' +
-                                 str(states))
-            for index, (value, state) in enumerate(zip(states, self.states)):
-                if hasattr(self.cell.state_size, '__len__'):
-                    dim = self.cell.state_size[index]
-                else:
-                    dim = self.cell.state_size
-                if value.shape != get_tuple_shape(dim):
-                    raise ValueError('State ' + str(index) +
-                                     ' is incompatible with layer ' +
-                                     self.name + ': expected shape=' +
-                                     str(get_tuple_shape(dim)) +
-                                     ', found shape=' + str(value.shape))
-                # TODO: consider batch calls to `set_value`.
-                K.set_value(state, value)
-    
-    test_model.load_weights(weight_path)
-test_model.compile(optimizer='rmsprop',
-                   loss='sparse_categorical_crossentropy',
-                   metrics=['accuracy'])
-test_model.summary()
-    
-    batch_size = 32
-num_classes = 10
-epochs = 200
-hidden_units = 100
-    
-    # input image dimensions
-img_rows, img_cols = 28, 28
-# number of convolutional filters to use
-filters = 32
-# size of pooling area for max pooling
-pool_size = 2
-# convolution kernel size
-kernel_size = 3
-    
-        def __init__(self):
-        self.__dict__ = self.__shared_state
-        self.state = 'Init'
-    
-        def product_list(self):
-        return self.data['products'].keys()
-    
-        graphic = CompositeGraphic()
-    
-    
-class Proxy:
-    def __init__(self):
-        self.busy = 'No'
-        self.sales = None
-    
-    
-class TestSubject(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.s = Subject()
-        cls.dec_obs = DecimalViewer()
-        cls.hex_obs = HexViewer()
-    
-        def test_relatives_after_access(self):
-        print(u'John's relatives: {0}'.format(self.John.relatives))
-        self.assertIn('relatives', self.John.__dict__)
-    
-    # mapping coco categories to cityscapes (our converted json) id
-# cityscapes
-# INFO roidb.py: 220: 1       bicycle: 7286
-# INFO roidb.py: 220: 2           car: 53684
-# INFO roidb.py: 220: 3        person: 35704
-# INFO roidb.py: 220: 4         train: 336
-# INFO roidb.py: 220: 5         truck: 964
-# INFO roidb.py: 220: 6    motorcycle: 1468
-# INFO roidb.py: 220: 7           bus: 758
-# INFO roidb.py: 220: 8         rider: 3504
-    
-        dim = 64
-    p = model.Conv(data, 'conv1', 3, dim, 7, pad=3, stride=2, no_bias=1)
-    p = model.AffineChannel(p, 'res_conv1_bn', dim=dim, inplace=True)
-    p = model.Relu(p, p)
-    p = model.MaxPool(p, 'pool1', kernel=3, pad=1, stride=2)
-    return p, dim
-    
-        for i in range(cfg.KRCNN.NUM_STACKED_CONVS):
-        current = model.Conv(
-            current,
-            'conv_fcn' + str(i + 1),
-            dim_in,
-            hidden_dim,
-            kernel_size,
-            stride=1,
-            pad=pad_size,
-            weight_init=(cfg.KRCNN.CONV_INIT, {'std': 0.01}),
-            bias_init=('ConstantFill', {'value': 0.})
-        )
-        current = model.Relu(current, current)
-        dim_in = hidden_dim
-    
-    
-def add_single_scale_rpn_losses(model):
-    '''Add losses for a single scale RPN model (i.e., no FPN).'''
-    # Spatially narrow the full-sized RPN label arrays to match the feature map
-    # shape
-    model.net.SpatialNarrowAs(
-        ['rpn_labels_int32_wide', 'rpn_cls_logits'], 'rpn_labels_int32'
-    )
-    for key in ('targets', 'inside_weights', 'outside_weights'):
-        model.net.SpatialNarrowAs(
-            ['rpn_bbox_' + key + '_wide', 'rpn_bbox_pred'], 'rpn_bbox_' + key
-        )
-    loss_rpn_cls = model.net.SigmoidCrossEntropyLoss(
-        ['rpn_cls_logits', 'rpn_labels_int32'],
-        'loss_rpn_cls',
-        scale=model.GetLossScale()
-    )
-    loss_rpn_bbox = model.net.SmoothL1Loss(
-        [
-            'rpn_bbox_pred', 'rpn_bbox_targets', 'rpn_bbox_inside_weights',
-            'rpn_bbox_outside_weights'
-        ],
-        'loss_rpn_bbox',
-        beta=1. / 9.,
-        scale=model.GetLossScale()
-    )
-    loss_gradients = blob_utils.get_loss_gradients(
-        model, [loss_rpn_cls, loss_rpn_bbox]
-    )
-    model.AddLosses(['loss_rpn_cls', 'loss_rpn_bbox'])
-    return loss_gradients
-
-    
-    from detectron.datasets import json_dataset
-from detectron.datasets import roidb as roidb_utils
-from detectron.utils import blob as blob_utils
-import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
-    
-        def _distribute_rois_over_fpn_levels(rois_blob_name):
-        '''Distribute rois over the different FPN levels.'''
-        # Get target level for each roi
-        # Recall blob rois are in (batch_idx, x1, y1, x2, y2) format, hence take
-        # the box coordinates from columns 1:5
-        target_lvls = fpn.map_rois_to_fpn_levels(
-            blobs[rois_blob_name][:, 1:5], lvl_min, lvl_max
-        )
-        # Add per FPN level roi blobs named like: <rois_blob_name>_fpn<lvl>
-        fpn.add_multilevel_roi_blobs(
-            blobs, rois_blob_name, blobs[rois_blob_name], target_lvls, lvl_min,
-            lvl_max
-        )
-    
-        sampled_fg_rois *= im_scale
-    repeated_batch_idx = batch_idx * blob_utils.ones(
-        (sampled_fg_rois.shape[0], 1)
-    )
-    sampled_fg_rois = np.hstack((repeated_batch_idx, sampled_fg_rois))
-    
-    from detectron.core.config import cfg
-import detectron.utils.blob as blob_utils
-import detectron.utils.boxes as box_utils
-import detectron.utils.segms as segm_utils
+          assert start_position > 0, (
+      'start_pattern must ends with an opening punctuation.')
+  assert text[start_position - 1] in matching_punctuation, (
+      'start_pattern must ends with an opening punctuation.')
+  # Stack of closing punctuations we expect to have in text after position.
+  punctuation_stack = [matching_punctuation[text[start_position - 1]]]
+  position = start_position
+  while punctuation_stack and position < len(text):
+    if text[position] == punctuation_stack[-1]:
+      punctuation_stack.pop()
+    elif text[position] in closing_punctuation:
+      # A closing punctuation without matching opening punctuations.
+      return None
+    elif text[position] in matching_punctuation:
+      punctuation_stack.append(matching_punctuation[text[position]])
+    position += 1
+  if punctuation_stack:
+    # Opening punctuations left without matching close-punctuations.
+    return None
+  # punctuations match.
+  return text[start_position:position - 1]
