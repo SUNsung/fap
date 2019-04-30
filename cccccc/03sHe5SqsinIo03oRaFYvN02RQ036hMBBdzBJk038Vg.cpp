5bb3bb2623c68aeaf16b5e5d947af42351a5b07a
SUNsung <weatherius@gmail.com>
2019-04-30 10:03:43 +0300

    Rewrite [03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp] File in: 2019-04-30 10:03:42

--- cccccc/03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp
+++ cccccc/03sHe5SqsinIo03oRaFYvN02RQ036hMBBdzBJk038Vg.cpp
@@ -1,103 +1,329 @@
 
         
-        
-    {  // Verify that the size of the key space not touched by the reads
-  // is pretty much unchanged.
-  const int64_t final_other_size = Size(Key(n), Key(kCount));
-  ASSERT_LE(final_other_size, initial_other_size + 1048576);
-  ASSERT_GE(final_other_size, initial_other_size/5 - 1048576);
-}
+         protected:
+  explicit AutoUpdater(v8::Isolate* isolate);
+  ~AutoUpdater() override;
     
-    #ifndef STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-#define STORAGE_LEVELDB_DB_LOG_FORMAT_H_
-    
-      void SetComparatorName(const Slice& name) {
-    has_comparator_ = true;
-    comparator_ = name.ToString();
-  }
-  void SetLogNumber(uint64_t num) {
-    has_log_number_ = true;
-    log_number_ = num;
-  }
-  void SetPrevLogNumber(uint64_t num) {
-    has_prev_log_number_ = true;
-    prev_log_number_ = num;
-  }
-  void SetNextFile(uint64_t num) {
-    has_next_file_number_ = true;
-    next_file_number_ = num;
-  }
-  void SetLastSequence(SequenceNumber seq) {
-    has_last_sequence_ = true;
-    last_sequence_ = seq;
-  }
-  void SetCompactPointer(int level, const InternalKey& key) {
-    compact_pointers_.push_back(std::make_pair(level, key));
-  }
-    
-    namespace php {
-struct Program;
-}
-struct Index;
-    
-    Object APCCollection::createObject() const {
-  if (m_arrayHandle->isTypedValue()) {
-    Variant local(m_arrayHandle->toLocal());
-    assertx(local.isArray());
-    return Object::attach(
-      collections::alloc(m_colType, local.getArrayData())
-    );
-  }
-    }
-    
-    struct FileStreamWrapper final : Stream::Wrapper {
-  static req::ptr<MemFile> openFromCache(const String& filename,
-                                         const String& mode);
-  req::ptr<File> open(const String& filename, const String& mode, int options,
-                      const req::ptr<StreamContext>& context) override;
-  int access(const String& path, int mode) override {
-    return ::access(File::TranslatePath(path).data(), mode);
-  }
-  int stat(const String& path, struct stat* buf) override {
-    return ::stat(File::TranslatePath(path).data(), buf);
-  }
-  int lstat(const String& path, struct stat* buf) override {
-    return ::lstat(File::TranslatePath(path).data(), buf);
-  }
-  int unlink(const String& path) override;
-  int rename(const String& oldname, const String& newname) override;
-  int mkdir(const String& path, int mode, int options) override;
-  int rmdir(const String& path, int /*options*/) override {
-    ERROR_RAISE_WARNING(::rmdir(File::TranslatePath(path).data()));
-    return ret;
-  }
-  bool isNormalFileStream() const override { return true; }
-    }
-    
-      // implementing File
-  bool open(const String& filename, const String& mode) override;
-  bool close() override;
-  int64_t readImpl(char *buffer, int64_t length) override;
-  int getc() override;
-  int64_t writeImpl(const char *buffer, int64_t length) override;
-  bool seek(int64_t offset, int whence = SEEK_SET) override;
-  int64_t tell() override;
-  bool eof() override;
-  bool rewind() override;
-  bool flush() override;
-  bool truncate(int64_t size) override;
-    
-    
-    {}
+    #endif  // ATOM_BROWSER_NET_ABOUT_PROTOCOL_HANDLER_H_
 
     
-        // out_of_range.403
-    try
-    {
-        // try to use a JSON pointer to an nonexistent object key
-        json::const_reference ref = j.at('/foo'_json_pointer);
+    bool URLRequestAboutJob::GetMimeType(std::string* mime_type) const {
+  *mime_type = 'text/html';
+  return true;
+}
+    
+    void OffscreenViewProxy::SetBitmap(const SkBitmap& bitmap) {
+  if (view_bounds_.width() == bitmap.width() &&
+      view_bounds_.height() == bitmap.height() && observer_) {
+    view_bitmap_.reset(new SkBitmap(bitmap));
+    observer_->OnProxyViewPaint(view_bounds_);
+  }
+}
+    
+      struct kevent change = {0};
+  EV_SET(&change, parent_pid, EVFILT_PROC, EV_ADD, NOTE_EXIT, 0, NULL);
+  if (kevent(kq.get(), &change, 1, nullptr, 0, nullptr) == -1) {
+    PLOG(ERROR) << 'kevent (add)';
+    return;
+  }
+    
+    #ifndef ATOM_BROWSER_UI_COCOA_ROOT_VIEW_MAC_H_
+#define ATOM_BROWSER_UI_COCOA_ROOT_VIEW_MAC_H_
+    
+     protected:
+#ifndef CPU_ONLY
+  cublasHandle_t cublas_handle_;
+  curandGenerator_t curand_generator_;
+#endif
+  shared_ptr<RNG> random_generator_;
+    
+    
+    { protected:
+  /**
+   * @param bottom input Blob vector (length 1)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$
+   * @param top output Blob vector (length 1)
+   *   -# @f$ (N \times 1 \times K) @f$ or, if out_max_val
+   *      @f$ (N \times 2 \times K) @f$ unless axis set than e.g.
+   *      @f$ (N \times K \times H \times W) @f$ if axis == 1
+   *      the computed outputs @f$
+   *       y_n = \arg\max\limits_i x_{ni}
+   *      @f$ (for @f$ K = 1 @f$).
+   */
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  /// @brief Not implemented (non-differentiable function)
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+    NOT_IMPLEMENTED;
+  }
+  bool out_max_val_;
+  size_t top_k_;
+  bool has_axis_;
+  int axis_;
+};
+    
+    
+    {  /**
+   * @brief Computes the error gradient w.r.t. the BNLL inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x}
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+    
+    #endif  // CAFFE_CUDNN_CONV_LAYER_HPP_
+
+    
+    #include 'caffe/layers/lrn_layer.hpp'
+#include 'caffe/layers/power_layer.hpp'
+    
+    #include 'caffe/blob.hpp'
+#include 'caffe/layer.hpp'
+#include 'caffe/proto/caffe.pb.h'
+    
+    // Max number of scripts in ICU + 'NULL' + Japanese and Korean + Fraktur
+const int kMaxNumberOfScripts = 116 + 1 + 2 + 1;
+    
+    namespace tesseract {
     }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
+    
+    // Parses the given box file string into a page_number, utf8_str, and
+// bounding_box. Returns true on a successful parse.
+bool ParseBoxFileStr(const char* boxfile_str, int* page_number,
+                     STRING* utf8_str, TBOX* bounding_box);
+    
+    bool ParagraphModel::ValidBodyLine(int lmargin, int lindent,
+                                   int rindent, int rmargin) const {
+  switch (justification_) {
+    case JUSTIFICATION_LEFT:
+      return NearlyEqual(lmargin + lindent, margin_ + body_indent_,
+                         tolerance_);
+    case JUSTIFICATION_RIGHT:
+      return NearlyEqual(rmargin + rindent, margin_ + body_indent_,
+                         tolerance_);
+    case JUSTIFICATION_CENTER:
+      return NearlyEqual(lindent, rindent, tolerance_ * 2);
+    default:
+      // shouldn't happen
+      return false;
+  }
+}
+    
+    class UnicharAmbigs {
+ public:
+  UnicharAmbigs() = default;
+  ~UnicharAmbigs() {
+    replace_ambigs_.delete_data_pointers();
+    dang_ambigs_.delete_data_pointers();
+    one_to_one_definite_ambigs_.delete_data_pointers();
+  }
     }
+    
+    /*!
+ * \file graph_attr_types.h
+ * \brief Data structures that can appear in graph attributes.
+ */
+#ifndef MXNET_GRAPH_ATTR_TYPES_H_
+#define MXNET_GRAPH_ATTR_TYPES_H_
+    
+    // specialize define for Layer Parameter
+template<>
+class FieldEntry<caffe::LayerParameter>
+    : public FieldEntryBase<FieldEntry<caffe::LayerParameter>, caffe::LayerParameter> {
+ public:
+  // parent class
+  typedef FieldEntryBase<FieldEntry<caffe::LayerParameter>, caffe::LayerParameter> Parent;
+    }
+    
+    /*!
+ * Copyright (c) 2016 by Contributors
+ * \file inplace_addto_detect_pass.cc
+ * \brief Detect whether inplace addto operation is possible for certain op.
+ */
+#include <mxnet/base.h>
+#include <mxnet/operator.h>
+#include <mxnet/op_attr_types.h>
+#include <nnvm/graph_attr_types.h>
+    
+     protected:
+  /*! \brief prefetcher parameters */
+  PrefetcherParam param_;
+  /*! \brief backend thread */
+  dmlc::ThreadedIter<DataBatch> iter;
+  /*! \brief internal batch loader */
+  std::unique_ptr<IIterator<TBlobBatch> > loader_;
+    
+    // these gpu functions are defined in gradient_compression.cu
+void Quantize2BitImpl(mshadow::Stream<mshadow::gpu> *s, const std::vector<mxnet::TBlob> &inputs,
+                      const float threshold);
+void Dequantize2BitImpl(mshadow::Stream<mshadow::gpu> *s, const std::vector<mxnet::TBlob> &inputs,
+                        const float threshold);
+    
+      /*!
+   * \brief sets parameters for gradient compression
+   * \param kwargs a vector of pair of strings. A pair represents key and value
+   * of the parameter. Will be parsed by GradientCompressionParam
+   */
+  void SetParams(const std::vector<std::pair<std::string, std::string> >& kwargs);
+    
+    
+    {    tmp = given;
+    given = result;
+    result = tmp;
+  }
+    
+     private:
+  inline void Init(mshadow::Stream<gpu> *s,
+                   const std::vector<TBlob> &in_data,
+                   const std::vector<TBlob> &out_data) {
+    using namespace mshadow;
+    #if CUDNN_MAJOR >= 5
+    format_ = CUDNN_TENSOR_NCHW;
+    #endif
+    CHECK_EQ(in_data.size(), 2U);
+    CHECK_EQ(out_data.size(), 2U);
+    if (!init_cudnn_) {
+      init_cudnn_ = true;
+      Tensor<gpu, 4, DType> data = in_data[bs::kData].get<gpu, 4, DType>(s);
+      Tensor<gpu, 4, DType> out = out_data[bs::kOut].get<gpu, 4, DType>(s);
+      CUDNN_CALL(cudnnCreateSpatialTransformerDescriptor(&st_desc_));
+      CUDNN_CALL(cudnnCreateTensorDescriptor(&in_desc_));
+      CUDNN_CALL(cudnnCreateTensorDescriptor(&out_desc_));
+      CUDNN_CALL(cudnnSetTensor4dDescriptor(in_desc_,
+                                            format_,
+                                            dtype_,
+                                            data.size(0),
+                                            data.size(1),
+                                            data.size(2),
+                                            data.size(3)));
+      CUDNN_CALL(cudnnSetTensor4dDescriptor(out_desc_,
+                                            format_,
+                                            dtype_,
+                                            out.size(0),
+                                            out.size(1),
+                                            out.size(2),
+                                            out.size(3)));
+      int dim[] = {static_cast<int>(out.size(0)), static_cast<int>(out.size(1)),
+                   static_cast<int>(out.size(2)), static_cast<int>(out.size(3))};
+      CUDNN_CALL(cudnnSetSpatialTransformerNdDescriptor(st_desc_,
+                                                        sampler_,
+                                                        dtype_,
+                                                        4,
+                                                        dim));
+    }
+  }
+    
+      ~CuDNNLocalResponseNormOp() {
+    if (init_cudnn_) {
+      CUDNN_CALL(cudnnDestroyLRNDescriptor(lrn_desc_));
+      CUDNN_CALL(cudnnDestroyTensorDescriptor(shape_desc_));
+    }
+  }
+    
+    
+    {} // namespace aria2
+
+    
+    class DHTTask {
+public:
+  virtual ~DHTTask() = default;
+    }
+    
+    void DHTTaskExecutor::update()
+{
+  execTasks_.erase(std::remove_if(execTasks_.begin(), execTasks_.end(),
+                                  std::mem_fn(&DHTTask::finished)),
+                   execTasks_.end());
+  int r;
+  if (static_cast<size_t>(numConcurrent_) > execTasks_.size()) {
+    r = numConcurrent_ - execTasks_.size();
+  }
+  else {
+    r = 0;
+  }
+  while (r && !queue_.empty()) {
+    std::shared_ptr<DHTTask> task = queue_.front();
+    queue_.pop_front();
+    task->startup();
+    if (!task->finished()) {
+      execTasks_.push_back(task);
+      --r;
+    }
+  }
+  A2_LOG_DEBUG(fmt('Executing %u Task(s). Queue has %u task(s).',
+                   static_cast<unsigned int>(getExecutingTaskSize()),
+                   static_cast<unsigned int>(getQueueSize())));
+}
+    
+      ~DHTTaskExecutor();
+    
+    std::shared_ptr<DHTTask> DHTTaskFactoryImpl::createBucketRefreshTask()
+{
+  auto task = std::make_shared<DHTBucketRefreshTask>();
+  setCommonProperty(task);
+  return task;
+}
+    
+      virtual void
+  addPeriodicTask2(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+    
+      DHTTokenTracker(const unsigned char* initialSecret);
+    
+    DHTTokenUpdateCommand::~DHTTokenUpdateCommand() = default;
+    
+    #include 'Extension.h'
+#include <thrift/protocol/TBinaryProtocol.h>
+#include <thrift/server/TSimpleServer.h>
+#include <thrift/transport/TServerSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+    
+    typedef struct _InternalOptionInfo__isset {
+  _InternalOptionInfo__isset() : value(false), default_value(false), type(false) {}
+  bool value :1;
+  bool default_value :1;
+  bool type :1;
+} _InternalOptionInfo__isset;
+    
+    
+    {
+    {
+    {      shell_callback(pArg, 2, &row[0], COLUMN_NAMES, nullptr);
+    }
+    pretty_print_if_needed(pArg);
+  } else {
+    fprintf(
+        stdout, 'Error %d: %s\n', status.getCode(), status.toString().c_str());
+  }
+}
+    
+    #include <vector>
+    
+    namespace osquery {
+namespace table_tests {
+    }
+    }
+    
+    // Sanity check integration test for kernel_info
+// Spec file: specs/kernel_info.table
+    
+    // Sanity check integration test for kernel_integrity
+// Spec file: specs/linux/kernel_integrity.table
+    
+    // Sanity check integration test for kernel_modules
+// Spec file: specs/linux/kernel_modules.table
