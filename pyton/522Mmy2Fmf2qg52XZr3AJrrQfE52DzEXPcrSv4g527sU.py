356301a23bfe5e5e6a4120958cd0f9a08b65409c
SUNsung <weatherius@gmail.com>
2019-04-30 13:04:57 +0300

    Rewrite [522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py] File in: 2019-04-30 13:04:56

--- pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
+++ pyton/522Mmy2Fmf2qg52XZr3AJrrQfE52DzEXPcrSv4g527sU.py
@@ -1,125 +1,69 @@
 
         
-          def load_labels(self, session, instances):
-    '''Loads the labels for these instances.
-    
-    
-# Initial condition generation, and condition label generation.  This
-# happens outside of the dataset loop, so that all datasets have the
-# same conditions, which is similar to a neurophys setup.
-condition_number = 0
-x0s = []
-condition_labels = []
-for c in range(C):
-  x0 = FLAGS.x0_std * rng.randn(N, 1)
-  x0s.append(np.tile(x0, nreplications)) # replicate x0 nreplications times
-  # replicate the condition label nreplications times
-  for ns in range(nreplications):
-    condition_labels.append(condition_number)
-  condition_number += 1
-x0s = np.concatenate(x0s, axis=1)
-    
-      def encode(self, sentence):
-    '''Convert a sentence to a list of ids, with special tokens added.'''
-    word_ids = [self.word_to_id(cur_word) for cur_word in sentence.split()]
-    return np.array([self.bos] + word_ids + [self.eos], dtype=np.int32)
-    
-    import os
-import pickle as pkl
-import numpy as np
-import tensorflow as tf
-import utils
-    
-        ## Calculate the Advantages, A(s,a) = Q(s,a) - \hat{V}(s).
-    advantages = []
-    for t in xrange(FLAGS.sequence_length):
-      log_probability = log_probs_list[t]
-      cum_advantage = tf.zeros(shape=[FLAGS.batch_size / 2])
-    
-            r = None
-        try:
-            r = Redirect.objects.get(site=current_site, old_path=full_path)
-        except Redirect.DoesNotExist:
-            pass
-        if r is None and settings.APPEND_SLASH and not request.path.endswith('/'):
-            try:
-                r = Redirect.objects.get(
-                    site=current_site,
-                    old_path=request.get_full_path(force_append_slash=True),
-                )
-            except Redirect.DoesNotExist:
-                pass
-        if r is not None:
-            if r.new_path == '':
-                return self.response_gone_class()
-            return self.response_redirect_class(r.new_path)
-    
-        def __init__(self, session_key=None):
-        self._cache = caches[settings.SESSION_CACHE_ALIAS]
-        super().__init__(session_key)
-    
-    
-class AbstractBaseSession(models.Model):
-    session_key = models.CharField(_('session key'), max_length=40, primary_key=True)
-    session_data = models.TextField(_('session data'))
-    expire_date = models.DateTimeField(_('expire date'), db_index=True)
-    
-    In both cases, only 10% of the features are informative.
-'''
-import numpy as np
-import gc
-from time import time
-from sklearn.datasets.samples_generator import make_regression
-    
-    
-def euclidean_distances(X, n_jobs):
-    return pairwise_distances(X, metric='euclidean', n_jobs=n_jobs)
-    
-    plt.figure('scikit-learn Ward's method benchmark results')
-plt.imshow(np.log(ratio), aspect='auto', origin='lower')
-plt.colorbar()
-plt.contour(ratio, levels=[1, ], colors='k')
-plt.yticks(range(len(n_features)), n_features.astype(np.int))
-plt.ylabel('N features')
-plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
-plt.xlabel('N samples')
-plt.title('Scikit's time, in units of scipy time (log)')
-plt.show()
+        # You can now launch tensorboard with `tensorboard --logdir=./logs` on your
+# command line and then go to http://localhost:6006/#projector to view the
+# embeddings
 
     
-        n = 10
-    step = 10000
-    n_samples = 10000
-    dim = 10
-    n_classes = 10
-    for i in range(n):
-        print('============================================')
-        print('Entering iteration %s of %s' % (i, n))
-        print('============================================')
-        n_samples += step
-        X = np.random.randn(n_samples, dim)
-        Y = np.random.randint(0, n_classes, (n_samples,))
-        bench_scikit_tree_classifier(X, Y)
-        Y = np.random.randn(n_samples)
-        bench_scikit_tree_regressor(X, Y)
-    
-    solutions = os.listdir(exercise_dir)
-    
-    data, row_idx, col_idx = sg._shuffle(data, random_state=0)
-plt.matshow(data, cmap=plt.cm.Blues)
-plt.title('Shuffled dataset')
-    
-    This example shows the effect of imposing a connectivity graph to capture
-local structure in the data. The graph is simply the graph of 20 nearest
-neighbors.
-    
-    # Code source: Gaël Varoquaux
-# Modified for documentation by Jaques Grobler
-# License: BSD 3 clause
+        # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
     
     
-#----------------------------------------------------------------------
-# Visualize the clustering
-def plot_clustering(X_red, labels, title=None):
-    x_min, x_max = np.min(X_red, axis=0), np.max(X_red, axis=0)
-    X_red = (X_red - x_min) / (x_max - x_min)
+def l1(l=0.01):
+    return L1L2(l1=l)
+    
+    
+def create_multi_input_model_from(layer1, layer2):
+    input_1 = Input(shape=(data_dim,))
+    input_2 = Input(shape=(data_dim,))
+    out1 = layer1(input_1)
+    out2 = layer2(input_2)
+    out = Average()([out1, out2])
+    model = Model([input_1, input_2], out)
+    model.add_loss(K.mean(out2))
+    model.add_loss(1)
+    model.add_loss(1)
+    return model
+    
+    - Klambauer, G., Unterthiner, T., Mayr, A., & Hochreiter, S. (2017).
+  Self-Normalizing Neural Networks. arXiv preprint arXiv:1706.02515.
+  https://arxiv.org/abs/1706.02515
+'''
+from __future__ import print_function
+    
+        with pytest.raises(ValueError):
+        conv_utils.normalize_tuple(None, 2, 'kernel_size')
+    with pytest.raises(ValueError):
+        conv_utils.normalize_tuple([2, 3, 4], 2, 'kernel_size')
+    with pytest.raises(ValueError):
+        conv_utils.normalize_tuple(['str', 'impossible'], 2, 'kernel_size')
+    
+    
+if __name__ == '__main__':
+    ''''''
+    # _test()
+    
+    
+def leaky_relu(x, alpha=0.1):
+    '''渗透 ReLU
+    `o = max(alpha * x, x)`
+    '''
+    return tf.nn.leaky_relu(x, alpha)
+    
+                # Use attention after lstm
+            x = RNNLayer(x, tf.nn.rnn_cell.LSTMCell, n_hidden=32)
+            x = attention_for_rnn(x.outputs)
+    
+    
+def create_training_instances(input_files, tokenizer, max_seq_length,
+                              dupe_factor, short_seq_prob, masked_lm_prob,
+                              max_predictions_per_seq, rng):
+  '''Create `TrainingInstance`s from raw text.'''
+  all_documents = [[]]
+    
+    import modeling
+import six
+import tensorflow as tf
+    
+        if not HAS_BOTO3:
+        module.fail_json(msg='Python module 'boto3' is missing, please install it')
