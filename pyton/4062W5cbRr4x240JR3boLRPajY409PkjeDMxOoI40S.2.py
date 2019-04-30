2eda81751317f78501777156db82e7f60f96469d
SUNsung <weatherius@gmail.com>
2019-04-30 04:32:01 +0300

    Rewrite [4062W5cbRr4x240JR3boLRPajY409PkjeDMxOoI40S.2.py] File in: 2019-04-30 04:32:00

--- pyton/4062W5cbRr4x240JR3boLRPajY409PkjeDMxOoI40S.2.py
+++ pyton/4062W5cbRr4x240JR3boLRPajY409PkjeDMxOoI40S.2.py
@@ -1,184 +1,135 @@
 
         
-            # Convert to full tensors, not lists of tensors in time dim.
-    gen_states = list_t_bxn_to_tensor_bxtxn(gen_states)
-    factors = list_t_bxn_to_tensor_bxtxn(factors)
-    out_dist_params = list_t_bxn_to_tensor_bxtxn(out_dist_params)
-    if self.hps.ic_dim > 0:
-      # select first time point
-      prior_g0_mean = prior_g0_mean[0]
-      prior_g0_logvar = prior_g0_logvar[0]
-      post_g0_mean = post_g0_mean[0]
-      post_g0_logvar = post_g0_logvar[0]
-    if self.hps.co_dim > 0:
-      controller_outputs = list_t_bxn_to_tensor_bxtxn(controller_outputs)
-    
-      if input_magnitude > 0.0:
-    # time of 'hits' randomly chosen between [1/4 and 3/4] of total time
-    input_times = rng.choice(int(ntime_steps/2), size=[E]) + int(ntime_steps/4)
-  else:
-    input_times = None
-    
-    model = IntegrationToBoundModel(N)
-inputs_ph_t = [tf.placeholder(tf.float32,
-                              shape=[None, 1]) for _ in range(ntimesteps)]
-state = tf.zeros([batch_size, N])
-saver = tf.train.Saver()
-    
-      # Pad to shape [height, width]
-  # where height is a multiple of batch_size
-  # and width is a multiple of num_timesteps
-  nrow = int(np.ceil(len(preprocessed) * 1.0 / batch_size))
-  ncol = int(np.ceil(max_len * 1.0 / num_timesteps))
-  height, width = nrow * batch_size, ncol * num_timesteps + 1
-  preprocessed = [sent + [PAD] * (width - len(sent)) for sent in preprocessed]
-  preprocessed += [[PAD] * width] * (height - len(preprocessed))
-    
-          x[i] = final_x
-      y[i] = final_y
-    
-      Args:
-    hparams:  Hyperparameters for the MaskGAN.
-    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
-    is_training:  Whether the model is training.
-    reuse (Optional):  Whether to reuse the model.
-    
-      decoder_embedding = [
-      v for v in tf.trainable_variables()
-      if v.op.name == 'gen/decoder/rnn/embedding'
-  ][0]
-  decoder_lstm_w_0 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      'gen/decoder/rnn/GenericMultiRNNCell/Cell0/Alien/rnn_builder/big_h_mat'
-  ][0]
-  decoder_lstm_b_0 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      'gen/decoder/rnn/GenericMultiRNNCell/Cell0/Alien/rnn_builder/big_inputs_mat'
-  ][0]
-  decoder_lstm_w_1 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      'gen/decoder/rnn/GenericMultiRNNCell/Cell1/Alien/rnn_builder/big_h_mat'
-  ][0]
-  decoder_lstm_b_1 = [
-      v for v in tf.trainable_variables()
-      if v.op.name ==
-      'gen/decoder/rnn/GenericMultiRNNCell/Cell1/Alien/rnn_builder/big_inputs_mat'
-  ][0]
+        def main():
+    print('Making key files...')
+    makeKeyFiles('rsa', 1024)
+    print('Key files generation successful.')
     
     
-def openssl_encode(algo, key, iv):
-    cmd = ['openssl', 'enc', '-e', '-' + algo, '-K', hex_str(key), '-iv', hex_str(iv)]
-    prog = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    out, _ = prog.communicate(secret_msg)
-    return out
-    
-    new_version = {}
-    
-    print('Enter the PKCS1 private key, followed by a blank line:')
-privkey = b''
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-    if line == '':
-        break
-    privkey += line.encode('ascii') + b'\n'
-privkey = rsa.PrivateKey.load_pkcs1(privkey)
-    
-    # We must be able to import youtube_dl
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-    
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
-    
-        with open(ZSH_COMPLETION_FILE, 'w') as f:
-        f.write(template)
-    
-            if 'skip' in test_case:
-            print_skipping(test_case['skip'])
-            return
-        for other_ie in other_ies:
-            if not other_ie.working():
-                print_skipping('test depends on %sIE, marked as not WORKING' % other_ie.ie_key())
-                return
-    
-        def test_module_exec(self):
-        if sys.version_info >= (2, 7):  # Python 2.6 doesn't support package execution
-            subprocess.check_call([sys.executable, '-m', 'youtube_dl', '--version'], cwd=rootDir, stdout=_DEV_NULL)
-    
-        with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-    
-    
-def _lookup_app_object(name):
-    top = _app_ctx_stack.top
-    if top is None:
-        raise RuntimeError(_app_ctx_err_msg)
-    return getattr(top, name)
-    
-        def dumps(self, value):
-        '''Tag the value and dump it to a compact JSON string.'''
-        return dumps(self.tag(value), separators=(',', ':'))
-    
-        If you configure your own :class:`logging.StreamHandler`, you may want to
-    use this for the stream. If you are using file or dict configuration and
-    can't import this directly, you can refer to it as
-    ``ext://flask.logging.wsgi_errors_stream``.
+class EditDistance:
     '''
-    return request.environ['wsgi.errors'] if request else sys.stderr
+    Use :
+    solver              = EditDistance()
+    editDistanceResult  = solver.solve(firstString, secondString)
+    '''
     
-        info(
-        'Releasing %s (codename %s, release date %s)',
-        version, codename, release_date.strftime('%d/%m/%Y')
-    )
-    tags = get_git_tags()
+    #Some examples
+    
+        return diff
+
+    
+    '''
+* Wondering how this method works !
+* It's pretty simple.
+* Let's say you need to calculate a ^ b
+* RULE 1 : a ^ b = (a*a) ^ (b/2) ---- example : 4 ^ 4 = (4*4) ^ (4/2) = 16 ^ 2
+* RULE 2 : IF b is ODD, then ---- a ^ b = a * (a ^ (b - 1)) :: where (b - 1) is even.
+* Once b is even, repeat the process to get a ^ b
+* Repeat the process till b = 1 OR b = 0, because a^1 = a AND a^0 = 1
+*
+* As far as the modulo is concerned,
+* the fact : (a*b) % c = ((a%c) * (b%c)) % c
+* Now apply RULE 1 OR 2 whichever is required.
+'''
+
+    
+    site = Bigthink()
+download = site.download_by_url
+
+    
+    from ..common import *
+    
+                    moz_ec_name = search_dict(sym_to_name, 'mozEcName')
+                push = search_dict(sym_to_name, 'push')
+                patt = '{}\.{}\('(.+?)'\)'.format(moz_ec_name, push)
+                ec_list = re.findall(patt, code)
+                [magic_list.append(sym_to_name[ec]) for ec in ec_list]
+        return magic_list
+    
+        url = list(set([
+        unicodize(str.replace(i, '\\/', '/'))
+        for i in re.findall(r'<meta property='og:video:secure_url' content='(.*?)'>', html)
+    ]))
+    
+        def update_url(self, received):
+        args = self.common_args.copy()
+        play_type = 'seek' if received else 'play'
+        received = received if received else -1
+        args['ls'] = play_type
+        args['start'] = received + 1
+        args['lt'] = self.get_date_str()
+        if self.enc_mode:
+            ssl_ts, sign = self.get_sign(self.media_url)
+            extra_args = dict(h=sign, r=ssl_ts, p=self.__class__.ENCRYPT_MOD_VER)
+            args.update(extra_args)
+        return '{}?{}'.format(self.media_url, parse.urlencode(args))
+    
+    def ifeng_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+# old pattern /uuid.shtml
+# now it could be #uuid
+    id = r1(r'([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', url)
+    if id:
+        return ifeng_download_by_id(id, None, output_dir = output_dir, merge = merge, info_only = info_only)
+    
+    site = Infoq()
+download = site.download_by_url
+download_playlist = site.download_by_url
+
+    
+            self._built = False
     
     
-IPv4 = CheckNetwork('IPv4')
-IPv4.urls = [
-            'https://www.microsoft.com',
-            'https://www.apple.com',
-            'https://code.jquery.com',
-            'https://cdn.bootcss.com',
-            'https://cdnjs.cloudflare.com']
-IPv4.triger_check_network()
+def highway_conv2d(x, kernel_size,
+                   act_fn=relu,
+                   strides=1,
+                   padding='SAME',
+                   carry_bias=-1.0,
+                   name=None):
+    '''用于 conv2d 的 highway
+    Input shape:  [batch_size, in_h, in_w, in_channels]
+    Output shape: [batch_size, in_h, in_w, in_channels]
     
-        fetchmethod = getattr(urlfetch, method, None)
-    if not fetchmethod:
-        start_response('405 Method Not Allowed', [('Content-Type', 'text/html')])
-        yield message_html('405 Method Not Allowed', 'Method Not Allowed: %r' % method, detail='Method Not Allowed URL=%r' % url)
-        raise StopIteration
+        with tf.variable_scope(name or 'attention_flow', reuse=reuse):
+        h_expand = tf.tile(tf.expand_dims(h, axis=2), [1, 1, J, 1])  # [N, T, J, d]
+        u_expand = tf.tile(tf.expand_dims(u, axis=1), [1, T, 1, 1])  # [N, T, J, d]
+        hu = tf.multiply(h_expand, u_expand)  # [N, T, J, d]
+        h_u_hu = tf.concat([h_expand, u_expand, hu], axis=-1)  # [N, T, J, 3d]
+        W_s = get_w([3 * d, 1])  # [3d, 1]
     
-        pass
+    word_unk = 'aam'
+ngrams = compute_ngrams(word_unk, min_ngrams, max_ngrams)  # min_ngrams, max_ngrams = 2, 4
+word_vec = np.zeros(model.vector_size, dtype=np.float32)
+ngrams_found = 0
+for ngram in ngrams:
+    ngram_hash = ft_hash(ngram) % model.bucket
+    if ngram_hash in model.wv.hash2index:
+        word_vec += model.wv.vectors_ngrams[model.wv.hash2index[ngram_hash]]
+        ngrams_found += 1
     
-        def getTokenIndex(self):
-        '''@brief Get the index in the input stream.
     
+@gen.engine
+def run_tests():
+    url = options.url + '/getCaseCount'
+    control_ws = yield websocket_connect(url, None)
+    num_tests = int((yield control_ws.read_message()))
+    logging.info('running %d cases', num_tests)
+    msg = yield control_ws.read_message()
+    assert msg is None
     
-class MissingNonceTest(unittest.TestCase):
-    '''Tests for acme.errors.MissingNonce.'''
+        def _curl_debug(self, debug_type: int, debug_msg: str) -> None:
+        debug_types = ('I', '<', '>', '<', '>')
+        if debug_type == 0:
+            debug_msg = native_str(debug_msg)
+            curl_log.debug('%s', debug_msg.strip())
+        elif debug_type in (1, 2):
+            debug_msg = native_str(debug_msg)
+            for line in debug_msg.splitlines():
+                curl_log.debug('%s %s', debug_types[debug_type], line)
+        elif debug_type == 4:
+            curl_log.debug('%s %r', debug_types[debug_type], debug_msg)
     
-    
-class FixedTest(unittest.TestCase):
-    '''Tests for acme.fields.Fixed.'''
-    
-        def get_names(self):
-        '''Return a set of all names.'''
-        all_names = set()  # type: Set[str]
-        all_names.update(self.aliases)
-        # Strip out any scheme:// and <port> field from servername
-        if self.name is not None:
-            all_names.add(VirtualHost.strip_name.findall(self.name)[0])
+    ``{% whitespace *mode* %}``
+    Sets the whitespace mode for the remainder of the current file
+    (or until the next ``{% whitespace %}`` directive). See
+    `filter_whitespace` for available options. New in Tornado 4.3.
+'''
