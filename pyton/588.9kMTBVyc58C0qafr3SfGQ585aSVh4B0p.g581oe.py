b5d85dfcfde1814b2dcf112ce54eda74abbedf7f
SUNsung <weatherius@gmail.com>
2019-05-01 08:31:11 +0300

    Rewrite [588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py] File in: 2019-05-01 08:31:10

--- pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
+++ pyton/588.9kMTBVyc58C0qafr3SfGQ585aSVh4B0p.g581oe.py
@@ -1,112 +1,38 @@
 
         
-            def fake_init_db():
-        Recorder.called = True
+        from scrapy.spiders import Spider
+from scrapy.http import Request
     
-        #: The name of the package or module that this app belongs to. Do not
-    #: change this once it is set by the constructor.
-    import_name = None
+    # Apply monkey patches to fix issues in external libraries
+from . import _monkeypatches
+del _monkeypatches
     
+            for spidername in args or spider_loader.list():
+            spidercls = spider_loader.load(spidername)
+            spidercls.start_requests = lambda s: conman.from_spider(s, result)
     
-def explain_ignored_app_run():
-    if os.environ.get('WERKZEUG_RUN_MAIN') != 'true':
-        warn(Warning('Silently ignoring app.run() because the '
-                     'application is run from the flask command line '
-                     'executable.  Consider putting app.run() behind an '
-                     'if __name__ == '__main__' guard to silence this '
-                     'warning.'), stacklevel=3)
-
+        def run(self, args, opts):
+        if len(args) < 1:
+            raise UsageError()
+        elif len(args) > 1:
+            raise UsageError('running 'scrapy crawl' with more than one spider is no longer supported')
+        spname = args[0]
     
-    from jinja2 import Markup
-from werkzeug.http import http_date, parse_date
+        def long_desc(self):
+        return ('Edit a spider using the editor defined in the EDITOR environment'
+                ' variable or else the EDITOR setting')
     
-                    if change_info:
-                    break
-    
-        for i in range(1, n + 1):
-        print('==================')
-        print('Iteration %02d of %02d' % (i, n))
-        print('==================')
-        n_features = i * step
-        n_informative = n_features / 10
-    
-        if dataset_name == 'SA':
-        lb = LabelBinarizer()
-        x1 = lb.fit_transform(X[:, 1].astype(str))
-        x2 = lb.fit_transform(X[:, 2].astype(str))
-        x3 = lb.fit_transform(X[:, 3].astype(str))
-        X = np.c_[X[:, :1], x1, x2, x3, X[:, 4:]]
-        y = (y != b'normal.').astype(int)
+        def _list_templates(self):
+        print('Available templates:')
+        for filename in sorted(os.listdir(self.templates_dir)):
+            if filename.endswith('.tmpl'):
+                print('  %s' % splitext(filename)[0])
     
     
-# Initialize random generator
-np.random.seed(0)
+logger = logging.getLogger(__name__)
     
-    from sklearn import cluster
-    
-    # #############################################################################
-# Plot result
-import matplotlib.pyplot as plt
-from itertools import cycle
-    
-        for trials in range(5):
-        a = random.randrange(2, num - 1)
-        v = pow(a, s, num)
-        if v != 1:
-            i = 0
-            while v != (num - 1):
-                if i == t - 1:
-                    return False
-                else:
-                    i = i + 1
-                    v = (v ** 2) % num
-    return True
-    
-        def _colision_resolution(self, key, data=None):
-        i = 1
-        new_key = self.hash_function(key + i*i)
-    
-    def longestSub(ARRAY): 			#This function is recursive
-	
-	ARRAY_LENGTH = len(ARRAY)
-	if(ARRAY_LENGTH <= 1):  	#If the array contains only one element, we return it (it's the stop condition of recursion)
-		return ARRAY
-								#Else
-	PIVOT=ARRAY[0]
-	isFound=False
-	i=1
-	LONGEST_SUB=[]
-	while(not isFound and i<ARRAY_LENGTH):
-		if (ARRAY[i] < PIVOT):
-			isFound=True
-			TEMPORARY_ARRAY = [ element for element in ARRAY[i:] if element >= ARRAY[i] ]
-			TEMPORARY_ARRAY = longestSub(TEMPORARY_ARRAY)
-			if ( len(TEMPORARY_ARRAY) > len(LONGEST_SUB) ):
-				LONGEST_SUB = TEMPORARY_ARRAY
-		else:
-			i+=1
-    
-    
-if __name__ == '__main__':
-    whole_array = input('please input some numbers:')
-    array = SubArray(whole_array)
-    re = array.solve_sub_array()
-    print(('the results is:', re))
-    
-        return res
-    
-        # A shop that sells random animals
-    shop = PetShop(random_animal)
-    for i in range(3):
-        shop.show_pet()
-        print('=' * 20)
-    
-    '''
-Lazily-evaluated property pattern in Python.
-    
-        def test_cloning_propperty_innate_values(self):
-        sample_object_1 = self.prototype.clone()
-        sample_object_2 = self.prototype.clone()
-        self.assertEqual(sample_object_1.value, sample_object_2.value)
-    
-            self.assertEqual(noise, expected_noise)
+        # Perform any final work and validity checks after the collating blobs for
+    # all minibatch images
+    valid = True
+    if cfg.MODEL.KEYPOINTS_ON:
+        valid = keypoint_rcnn_roi_data.finalize_keypoint_minibatch(blobs, valid)
