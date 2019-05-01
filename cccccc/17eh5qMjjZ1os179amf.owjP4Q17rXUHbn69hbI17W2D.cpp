0263790c05090167d2fd84ca763c1847eeecb7ab
SUNsung <weatherius@gmail.com>
2019-05-02 02:23:43 +0300

    Rewrite [17eh5qMjjZ1os179amf.owjP4Q17rXUHbn69hbI17W2D.cpp] File in: 2019-05-02 02:23:42

--- cccccc/17eh5qMjjZ1os179amf.owjP4Q17rXUHbn69hbI17W2D.cpp
+++ cccccc/17eh5qMjjZ1os179amf.owjP4Q17rXUHbn69hbI17W2D.cpp
@@ -1,184 +1,66 @@
 
         
-        
-    {
-    {        for (; j < size.width; j++)
-        {
-            dst[j] = ~src[j];
-        }
+        namespace tensorflow {
     }
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
+    
+    // Creates a numpy array with shapes specified by dim_size and dims and content
+// in data. The array does not own the memory, and destructor will be called to
+// release it. If the status is not ok the caller is responsible for releasing
+// the memory.
+Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
+                       std::function<void()> destructor, PyObject** result);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    PyObject* PyExceptionRegistry::Lookup(TF_Code code) {
+  DCHECK(singleton_ != nullptr) << 'Must call PyExceptionRegistry::Init() '
+                                   'before PyExceptionRegistry::Lookup()';
+  DCHECK_NE(code, TF_OK);
+  DCHECK(singleton_->exc_types_.find(code) != singleton_->exc_types_.end())
+      << 'Unknown error code passed to PyExceptionRegistry::Lookup: ' << code;
+  return singleton_->exc_types_[code];
 }
     
-        size_t maxsize = std::max<size_t>( 1u << 10, size.width * size.height / 10 );
-    std::vector<u8*> stack( maxsize );
-    u8 **stack_top = &stack[0];
-    u8 **stack_bottom = &stack[0];
+        http://www.apache.org/licenses/LICENSE-2.0
     
-    ptrdiff_t borderInterpolate(ptrdiff_t _p, size_t _len, BORDER_MODE borderType, size_t startMargin = 0, size_t endMargin = 0);
+    #include 'tensorflow/core/framework/node_def.pb.h'
+#include 'tensorflow/core/framework/node_def_util.h'
+#include 'tensorflow/core/framework/op.h'
+#include 'tensorflow/core/framework/op_kernel.h'
+#include 'tensorflow/core/framework/types.h'
+#include 'tensorflow/core/lib/core/status.h'
+#include 'tensorflow/core/util/device_name_utils.h'
     
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const s16 * src = internal::getRowPtr(srcBase, srcStride, i);
-        u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
-        size_t j = 0;
-    }
+      // The following options refer to synchronization options when
+  // using SynchronizeStream or SynchronizeContext.
     
-                    uint8x16_t m0 =   vandq_u8(vcgtq_s8(x0, v2), vcgtq_s8(x1, v2));
-                uint8x16_t m1 =   vandq_u8(vcgtq_s8(v1, x0), vcgtq_s8(v1, x1));
-                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x1, v2), vcgtq_s8(x2, v2)));
-                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x1), vcgtq_s8(v1, x2)));
-                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x2, v2), vcgtq_s8(x3, v2)));
-                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x2), vcgtq_s8(v1, x3)));
-                m0 = vorrq_u8(m0, vandq_u8(vcgtq_s8(x3, v2), vcgtq_s8(x0, v2)));
-                m1 = vorrq_u8(m1, vandq_u8(vcgtq_s8(v1, x3), vcgtq_s8(v1, x0)));
-                m0 = vorrq_u8(m0, m1);
+    #endif  // TENSORFLOW_STREAM_EXECUTOR_KERNEL_CACHE_CONFIG_H_
+
     
-    
-    {            if (mask[0])
-                process(src, j, j + 8, i,
-                        minVal, minLocPtr, minLocCount, minLocCapacity,
-                        maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
-        }
-    
-        for (size_t i = 0; i < size.height; ++i)
-    {
-        const T * src = getRowPtr((const T *)srcBase, srcStride, i);
-        T * dst = getRowPtr((T *)dstBase, dstStride, (flipMode & FLIP_VERTICAL_MODE) != 0 ? size.height - i - 1 : i);
-        size_t js = 0, jd = size.width;
-    }
-    
-            const u8* ln0 = idx_rm2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm2) : tmp;
-        const u8* ln1 = idx_rm1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rm1) : tmp;
-        const u8* ln2 = internal::getRowPtr(srcBase, srcStride, i);
-        const u8* ln3 = idx_rp1 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp1) : tmp;
-        const u8* ln4 = idx_rp2 >= -(ptrdiff_t)borderMargin.top ? internal::getRowPtr(srcBase, srcStride, idx_rp2) : tmp;
-    
-    #include <arm_neon.h>
-    
-            int16x8_t tnext, tc, t0;
-        int16x8_t tnext2, tnext3;
-        int16x8_t tnext1Old, tnext2Old, tnext3Old;
-        int16x8_t tnext4OldOldOld, tnext5OldOldOld;
-    
-    int main() {
-    }
+    private:
+    struct Command{
+        string s;   // go, print
+        TreeNode* node;
+        Command(string s, TreeNode* node): s(s), node(node){}
+    };
     
     
     {
-    {        int index = 0;
-        for(int i = 0 ; i < count[0] ; i ++)
-            nums[index++] = 0;
-        for(int i = 0 ; i < count[1] ; i ++)
-            nums[index++] = 1;
-        for(int i = 0 ; i < count[2] ; i ++)
-            nums[index++] = 2;
-    }
-};
-    
-    
-    {        ListNode* ret = dummyHead->next;
-        delete dummyHead;
-        return ret;
-    }
-    
-    #include <iostream>
-#include <vector>
-    
-            vector<int> res;
-        if( root == NULL )
-            return res;
-    
-    // Classic Non-Recursive algorithm for preorder traversal
-// Time Complexity: O(n), n is the node number in the tree
-// Space Complexity: O(h), h is the height of the tree
-class Solution {
-    }
-    
-    int main() {
-    }
-    
-    
-    {
-    {
-    {            TagNode tagNode = stack.top();
-            stack.pop();
-            cur = tagNode.node;
-            if(tagNode.isFirst == false){
-                tagNode.isFirst = true;
-                stack.push(tagNode);
-                cur = cur->right;
+    {                if(prev->right == NULL){
+                    res.push_back(cur->val);
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else{
+                    prev->right = NULL;
+                    cur = cur->right;
+                }
             }
-            else{
-                res.push_back(cur->val);
-                cur = NULL;
-            };
         }
-        return res;
-    }
-};
     
-            std::shared_ptr<const Matrix<V1ElemType>> matrix = GetMatrix<V1ElemType>();
-        auto matrixDims = GetMatrixDimensions(Shape());
-        if (matrix->GetNumRows() != matrixDims.first)
-            LogicError('The number of rows of the underlying matrix does not match the shape.');
-        if (matrix->GetNumCols() != matrixDims.second)
-            LogicError('The number of columns of the underlying matrix does not match the shape.');
-    
-        static void setupTimeout(int seconds)
-    {
-        struct sigaction action = {};
-        action.sa_handler = &CrossProcessMutex::noOpAlarmHandler;
-        sigaction(SIGALRM, &action, NULL);
-        alarm(seconds);
-    }
-    
-        typedef unsigned int INDEXTYPE; // don't use size_t, as this saves HUGE amounts of RAM
-    std::vector<INDEXTYPE> map;     // [t] -> t' indices in randomized order
-    size_t currentseed;             // seed for current sequence
-    size_t randomizationrange;      // t - randomizationrange/2 <= t' < t + randomizationrange/2 (we support this to enable swapping)
-                                    // special values (randomizeDisable)
-    void Invalidate()
-    {
-        currentseed = (size_t) -1;
-    }
-    
-    
-    {private:
-    long long m_start;
-    long long m_end;
-};
-    
-    
-    {        for (const auto& pair : rootLookupMap)
-        {
-            if (groupMap.find(pair.second) == groupMap.end())
-                InvalidArgument('lookup root should be group key');
-        }
-        m_aliasLookup = rootLookupMap;
-    }
-    
-            static Windows::UI::Xaml::DependencyProperty^ s_announcementProperty;
-    
-    CUnaryCommand CommandDeserializer::DeserializeUnary()
-{
-    auto cmdSize = m_dataReader->ReadUInt32();
-    std::shared_ptr<CalculatorVector<int>> cmdVector = std::make_shared<CalculatorVector<int>>();
-    }
-    
-                static void OnVirtualKeyAltChordPropertyChanged(
-                Windows::UI::Xaml::DependencyObject^ target,
-                MyVirtualKey oldValue,
-                MyVirtualKey newValue);
-    
-    IAsyncOperationWithProgress<String^, HttpProgress>^ CurrencyHttpClient::GetCurrencyMetadata()
-{
-    wstring uri = wstring{ sc_MetadataUriLocalizeFor } + m_responseLanguage->Data();
-    auto metadataUri = ref new Uri(StringReference(uri.c_str()));
-    }
+    using namespace std;
