af82ace3137d8c13b6c98dc923c21943db1c2713
SUNsung <weatherius@gmail.com>
2019-05-02 00:52:07 +0300

    Rewrite [34gPNBNGAg.fw34IIQxPQzSXa.34k2xfGtFXOiw34eai.cpp] File in: 2019-05-02 00:52:06

--- cccccc/34gPNBNGAg.fw34IIQxPQzSXa.34k2xfGtFXOiw34eai.cpp
+++ cccccc/34gPNBNGAg.fw34IIQxPQzSXa.34k2xfGtFXOiw34eai.cpp
@@ -1,352 +1,81 @@
 
         
-        #define TEGRA_UNARYOPSCALE(type, op, src1, sz1, dst, sz, w, h, scales) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-    parallel_for_(Range(0, h), \
-    TegraGenOp_##op##_Invoker<const type, type>(src1, sz1, dst, sz, w, h, scales), \
-    (w * h) / static_cast<double>(1<<16)), \
-    CV_HAL_ERROR_OK \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
-    
-    void absDiff(const Size2D &size,
-             const s8 *src0Base, ptrdiff_t src0Stride,
-             const s8 *src1Base, ptrdiff_t src1Stride,
-             s8 *dstBase, ptrdiff_t dstStride)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, AbsDiffSigned<s8>());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
+        void CacheImpl::releaseValue(void *Value) {
+  cache_release_value(static_cast<cache_t*>(Impl), Value);
 }
     
-            for (; j < roiw32; j += 32)
-        {
-            internal::prefetch(src + j);
-            uint8x16_t v_src0 = vld1q_u8(src + j), v_src1 = vld1q_u8(src + j + 16);
-            uint8x16_t v_dst0 = vmvnq_u8(v_src0), v_dst1 = vmvnq_u8(v_src1);
-            vst1q_u8(dst + j, v_dst0);
-            vst1q_u8(dst + j + 16, v_dst1);
-        }
-        for (; j < roiw8; j += 8)
-        {
-            uint8x8_t v_src = vld1_u8(src + j);
-            uint8x8_t v_dst = vmvn_u8(v_src);
-            vst1_u8(dst + j, v_dst);
-        }
-    
-                    t0 = vqaddq_u16(t0, vqaddq_u16(t1, t2));
-    
-    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
-inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
-inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
-    
-    #include <carotene/functions.hpp>
-#include 'saturate_cast.hpp'
-    
-    #define CVTS_FUNC1(T1, SIMD_SIZE, CVTSINIT, CVTSROW)                             \
-    void convertScale(const Size2D &,                                            \
-                      const T1 *, ptrdiff_t,                                     \
-                      T1 *, ptrdiff_t,                                           \
-                      f64, f64)                                                  \
-    {                                                                            \
-        internal::assertSupportedConfiguration();                                \
+        if (node->Further || node->Right) {
+      IndentScope ms(this, (childKind == ChildKind::Right ||
+                            childKind == ChildKind::Further ||
+                            childKind == ChildKind::Root) ? '  ' : '| ');
     }
     
-                tnext[0] = x0;
-            tnext[1] = x1;
-            tnext[2] = x2;
-    
-                for( ; i <= lim; i += 4 )
-            {
-                internal::prefetch(src0 + i);
-                internal::prefetch(src1 + i);
-                v_sum = vmlaq_f32(v_sum, vld1q_f32(src0 + i), vld1q_f32(src1 + i));
-            }
-    
-            for ( ; j + 7 < size.width; j += 8)
-        {
-            internal::prefetch(sqsum + j);
-            internal::prefetch(src + j);
-    }
-    
-                uint8x8_t x0 = vld1_u8(v0 + x);
-            uint8x8_t x1 = vld1_u8(v1 + x);
-            uint8x8_t x2 = vld1_u8(v2 + x);
-            uint8x8_t x3 = vld1_u8(v3 + x);
-            uint8x8_t x4 = vld1_u8(v4 + x);
-            if(x) {
-                tcurr1 = tnext1;
-            }
-    
-    
-    { private:
-  struct pair_sort_first {
-    bool operator()(const std::pair<int, int> &left,
-                    const std::pair<int, int> &right) {
-      return left.first < right.first;
-    }
-  };
-  void check_batch_reindex(int initial_num, int final_num,
-                           const Dtype* ridx_data);
-};
-    
-    #include <vector>
-    
-     protected:
-  /**
-   * @param bottom input Blob vector (length 2+)
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_1 @f$
-   *   -# @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_2 @f$
-   *   -# ...
-   *   - K @f$ (N \times C \times H \times W) @f$
-   *      the inputs @f$ x_K @f$
-   * @param top output Blob vector (length 1)
-   *   -# @f$ (KN \times C \times H \times W) @f$ if axis == 0, or
-   *      @f$ (N \times KC \times H \times W) @f$ if axis == 1:
-   *      the concatenated output @f$
-   *        y = [\begin{array}{cccc} x_1 & x_2 & ... & x_K \end{array}]
-   *      @f$
-   */
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    
-    
-    {
-    {} // namespace test
-} // namespace c10d
-
-    
-    
-    {} // namespace caffe2
-
-    
-    template <>
-void GluOp<float, CPUContext>::ComputeGlu(
-    const int M,
-    const int split_dim,
-    const int N,
-    const float* Xdata,
-    float* Ydata) {
-  const int xStride = 2 * split_dim * N;
-  const int yStride = split_dim * N;
-  for (int i = 0; i < M; ++i) {
-    const int idx = i * xStride;
-    const int idy = i * yStride;
-    for (int j = 0; j < split_dim; ++j) {
-      const int jN = j * N;
-      const int jdx1 = idx + jN;
-      const int jdx2 = idx + (j + split_dim) * N;
-      const int jdy = idy + jN;
-      for (int k = 0; k < N; ++k) {
-        const float x1 = Xdata[jdx1 + k];
-        const float x2 = Xdata[jdx2 + k];
-        Ydata[jdy + k] = x1 * sigmoid(x2);
+        void emitDiagnosticMessage(clang::FullSourceLoc Loc,
+                               clang::PresumedLoc PLoc,
+                               clang::DiagnosticsEngine::Level Level,
+                               StringRef Message,
+                               ArrayRef<clang::CharSourceRange> Ranges,
+                               clang::DiagOrStoredDiag Info) override {
+      if (isInSwiftBuffers(Loc)) {
+        // FIXME: Ideally, we'd report non-suppressed diagnostics on synthetic
+        // buffers, printing their names (eg. <swift-imported-modules>:...) but
+        // this risks printing _excerpts_ of those buffers to stderr too; at
+        // present the synthetic buffers are 'large blocks of null bytes' which
+        // we definitely don't want to print out. So until we have some clever
+        // way to print the name but suppress printing excerpts, we just replace
+        // the Loc with an invalid one here, which suppresses both.
+        Loc = clang::FullSourceLoc();
+        if (shouldSuppressDiagInSwiftBuffers(Info))
+          return;
       }
+      callback(Loc, Level, Message);
     }
+    
+    const char *Demangle::getNodeKindString(swift::Demangle::Node::Kind k) {
+  switch (k) {
+#define NODE(ID)                                                               \
+  case Node::Kind::ID:                                                         \
+    return #ID;
+#include 'swift/Demangling/DemangleNodes.def'
   }
+  return 'Demangle::Node::Kind::???';
 }
     
-    template <typename T, class Context>
-class BernoulliJSDOp final : public Operator<Context> {
- public:
-  USE_SIMPLE_CTOR_DTOR(BernoulliJSDOp);
-  USE_OPERATOR_CONTEXT_FUNCTIONS;
-  bool RunOnDevice() override;
+    class LLVM_LIBRARY_VISIBILITY Windows : public ToolChain {
+protected:
+  InvocationInfo constructInvocation(const LinkJobAction &job,
+                                     const JobContext &context) const override;
+    }
+    
+      /// Emits an error for each file in \p unusedPrimaryFiles.
+  ///
+  /// \returns true if \p unusedPrimaryFiles is non-empty.
+  bool diagnoseUnusedPrimaryFiles(std::set<StringRef> unusedPrimaryFiles);
+    
+            unordered_map<int,int> record;
+        for(int i = 0 ; i < nums.size() ; i ++)
+            record[nums[i]] = i;
+    
+        ListNode* head = createLinkedList(arr, n);
+    printLinkedList(head);
+    
+    
+    {    return 0;
+}
+    
+        head = Solution().swapPairs(head);
+    printLinkedList(head);
+    
+    
+    {
+    {        delete dummyHead1;
+        delete dummyHead2;
+        return ret;
+    }
 };
     
-    // Main entry point for Paragraph Detection Algorithm.
-//
-// Given a set of equally spaced textlines (described by row_infos),
-// Split them into paragraphs.  See http://goto/paragraphstalk
-//
-// Output:
-//   row_owners - one pointer for each row, to the paragraph it belongs to.
-//   paragraphs - this is the actual list of PARA objects.
-//   models - the list of paragraph models referenced by the PARA objects.
-//            caller is responsible for deleting the models.
-void DetectParagraphs(int debug_level,
-                      GenericVector<RowInfo> *row_infos,
-                      GenericVector<PARA *> *row_owners,
-                      PARA_LIST *paragraphs,
-                      GenericVector<ParagraphModel *> *models);
+    #include <iostream>
+#include <vector>
+#include <stack>
     
-    #endif  // TESSERACT_CCUTIL_BOXREAD_H_
-
-    
-    // Update the other members if the cost is lower.
-void DPPoint::UpdateIfBetter(int64_t cost, int32_t steps, const DPPoint* prev,
-                             int32_t n, int32_t sig_x, int64_t sig_xsq) {
-  if (cost < total_cost_) {
-    total_cost_ = cost;
-    total_steps_ = steps;
-    best_prev_ = prev;
-    n_ = n;
-    sig_x_ = sig_x;
-    sig_xsq_ = sig_xsq;
-  }
-}
-    
-    bool ParagraphModel::ValidBodyLine(int lmargin, int lindent,
-                                   int rindent, int rmargin) const {
-  switch (justification_) {
-    case JUSTIFICATION_LEFT:
-      return NearlyEqual(lmargin + lindent, margin_ + body_indent_,
-                         tolerance_);
-    case JUSTIFICATION_RIGHT:
-      return NearlyEqual(rmargin + rindent, margin_ + body_indent_,
-                         tolerance_);
-    case JUSTIFICATION_CENTER:
-      return NearlyEqual(lindent, rindent, tolerance_ * 2);
-    default:
-      // shouldn't happen
-      return false;
-  }
-}
-    
-        bool accepted() {  //Is char accepted?
-      return !rejected ();
-    }
-    
-      // Accessors.
-  const Key& key() const {
-    return key_;
-  }
-  void set_key(const Key& new_key) {
-    key_ = new_key;
-  }
-  const Data* data() const {
-    return data_;
-  }
-  // Sets the data pointer, taking ownership of the data.
-  void set_data(Data* new_data) {
-    delete data_;
-    data_ = new_data;
-  }
-  // Relinquishes ownership of the data pointer (setting it to nullptr).
-  Data* extract_data() {
-    Data* result = data_;
-    data_ = nullptr;
-    return result;
-  }
-    
-            static Windows::UI::Xaml::DependencyProperty^ s_announcementProperty;
-    
-    namespace CalculatorApp
-{
-    EngineResourceProvider::EngineResourceProvider()
-    {
-        m_resLoader = ResourceLoader::GetForViewIndependentUse('CEngineStrings');
-    }
-    }
-    
-    
-    {
-    {    private:
-        std::wstring m_activityName;
-        winrt::Windows::Foundation::Diagnostics::LoggingChannel m_channel;
-        winrt::Windows::Foundation::Diagnostics::LoggingFields m_fields;
-        winrt::Windows::Foundation::Diagnostics::LoggingActivity m_activity;
-    };
-}
-
-    
-    TEST(ProtocolDataTest, CheckSum) {
-  const uint8_t INPUT[] = {0x00, 0x12, 0x00, 0x13, 0x00, 0xF3, 0x00, 0x00};
-  const uint8_t result =
-      ProtocolData<apollo::canbus::ChassisDetail>::CalculateCheckSum(INPUT, 8);
-  EXPECT_EQ(0xE7, result);
-}
-    
-    TEST(ByteTest, SetGetHighLowBit) {
-  unsigned char byte_value = 0x37;
-  Byte value(&byte_value);
-  value.set_value_high_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x07, value.get_byte_low_4_bits());
-  value.set_value_low_4_bits(0x0B);
-  EXPECT_EQ(0x0B, value.get_byte_high_4_bits());
-  EXPECT_EQ(0x0B, value.get_byte_low_4_bits());
-}
-    
-    
-    {  int ret = x;
-  return ret;
-}
-    
-    TEST(TestPiecewiseLinearConstraint, add_monotone_inequality_constraint) {
-  PiecewiseLinearConstraint constraint(10, 0.1);
-  constraint.AddMonotoneInequalityConstraint();
-  const auto mat = constraint.inequality_constraint_matrix();
-  const auto bd = constraint.inequality_constraint_boundary();
-    }
-    
-      MatrixXd mat_golden(10, 10);
-  // clang-format off
-  mat_golden <<
-     20, -15,   6,  -1,   0,   0,   0,   0,   0,   0,
-    -15,  20, -15,   6,  -1,   0,   0,   0,   0,   0,
-      6, -15,  20, -15,   6,  -1,   0,   0,   0,   0,
-     -1,   6, -15,  20, -15,   6,  -1,   0,   0,   0,
-      0,  -1,   6, -15,  20, -15,   6,  -1,   0,   0,
-      0,   0,  -1,   6, -15,  20, -15,   6,  -1,   0,
-      0,   0,   0,  -1,   6, -15,  20, -15,   6,  -1,
-      0,   0,   0,   0,  -1,   6, -15,  19, -12,   3,
-      0,   0,   0,   0,   0,  -1,   6, -12,  10,  -3,
-      0,   0,   0,   0,   0,   0,  -1,   3,  -3,   1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-    void SplineSegKernel::CalculateSecondOrderDerivative(
-    const uint32_t num_params) {
-  kernel_second_order_derivative_ =
-      Eigen::MatrixXd::Zero(num_params, num_params);
-  for (int r = 2; r < kernel_second_order_derivative_.rows(); ++r) {
-    for (int c = 2; c < kernel_second_order_derivative_.cols(); ++c) {
-      kernel_second_order_derivative_(r, c) =
-          (r * r - r) * (c * c - c) / (r + c - 3.0);
-    }
-  }
-}
-    
-    #include 'modules/canbus/vehicle/gem/protocol/accel_cmd_67.h'
-#include 'modules/canbus/vehicle/gem/protocol/brake_cmd_6b.h'
-#include 'modules/canbus/vehicle/gem/protocol/global_cmd_69.h'
-#include 'modules/canbus/vehicle/gem/protocol/headlight_cmd_76.h'
-#include 'modules/canbus/vehicle/gem/protocol/horn_cmd_78.h'
-#include 'modules/canbus/vehicle/gem/protocol/shift_cmd_65.h'
-#include 'modules/canbus/vehicle/gem/protocol/steering_cmd_6d.h'
-#include 'modules/canbus/vehicle/gem/protocol/turn_cmd_63.h'
-#include 'modules/canbus/vehicle/gem/protocol/wiper_cmd_90.h'
-    
-      Byte t1(bytes + 3);
-  int32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-    // config detail: {'name': 'pacmod_status', 'enum': {0:
-// 'PACMOD_STATUS_CONTROL_DISABLED', 1: 'PACMOD_STATUS_CONTROL_ENABLED'},
-// 'precision': 1.0, 'len': 1, 'is_signed_var': False, 'offset': 0.0,
-// 'physical_range': '[0|1]', 'bit': 0, 'type': 'enum', 'order': 'motorola',
-// 'physical_unit': ''}
-Global_rpt_6a::Pacmod_statusType Globalrpt6a::pacmod_status(
-    const std::uint8_t* bytes, int32_t length) const {
-  Byte t0(bytes + 0);
-  int32_t x = t0.get_byte(0, 1);
-    }
+            stack<TreeNode*> stack, output;
