87c33188c28f0fb4ceab2907add96aa8e22723f0
SUNsung <weatherius@gmail.com>
2019-05-01 12:53:07 +0300

    Rewrite [07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp] File in: 2019-05-01 12:53:06

--- cccccc/07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp
+++ cccccc/07er6j7VvynOE072w.DsMZCzgw07kOASsihgaBM07PdN.cpp
@@ -1,102 +1,162 @@
 
         
-        MeasureDouble RpcServerServerLatency() {
-  static const auto measure = MeasureDouble::Register(
-      kRpcServerServerLatencyMeasureName,
-      'Time between first byte of request received to last byte of response '
-      'sent, or terminal error',
-      kUnitMilliseconds);
-  return measure;
+            QColor TextColor() const { return textColor; }
+    QColor SingleColor() const { return singleColor; }
+    
+    #ifndef BITCOIN_BECH32_H
+#define BITCOIN_BECH32_H
+    
+    constexpr static inline uint32_t rotl32(uint32_t v, int c) { return (v << c) | (v >> (32 - c)); }
+    
+    void InternalKeyComparator::FindShortSuccessor(std::string* key) const {
+  Slice user_key = ExtractUserKey(*key);
+  std::string tmp(user_key.data(), user_key.size());
+  user_comparator_->FindShortSuccessor(&tmp);
+  if (tmp.size() < user_key.size() &&
+      user_comparator_->Compare(user_key, tmp) < 0) {
+    // User key has become shorter physically, but larger logically.
+    // Tack on the earliest possible number to the shortened user key.
+    PutFixed64(&tmp, PackSequenceAndType(kMaxSequenceNumber,kValueTypeForSeek));
+    assert(this->Compare(*key, tmp) < 0);
+    key->swap(tmp);
+  }
 }
     
-    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
-::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
-::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
-::opencensus::stats::MeasureDouble RpcClientServerLatency();
-::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
+    
+    { private:
+  App();
+  DISALLOW_COPY_AND_ASSIGN(App);
+};
+    
+    #include 'base/logging.h'
+#include 'base/values.h'
+    
+    #include 'content/nw/src/api/menu/menu_delegate.h'
+    
+       bool IsItemForCommandIdDynamic(int command_id) const override;
+   base::string16 GetLabelForCommandId(int command_id) const override;
+   bool GetIconForCommandId(int command_id,
+                                   gfx::Image* icon) const override;
+    
+    namespace extensions {
+    }
+    
+    class OrientationDetector {
+ public:
+  OrientationDetector(const GenericVector<int>* allowed_scripts,
+                      OSResults* results);
+  bool detect_blob(BLOB_CHOICE_LIST* scores);
+  int get_orientation();
+ private:
+  OSResults* osr_;
+  const GenericVector<int>* allowed_scripts_;
+};
+    
+    
+    {}  // namespace tesseract.
+    
+    
+    { private:
+  // A name for this document.
+  STRING document_name_;
+  // A group of pages that corresponds in some loose way to a document.
+  PointerVector<ImageData> pages_;
+  // Page number of the first index in pages_.
+  int pages_offset_;
+  // Total number of pages in document (may exceed size of pages_.)
+  int total_pages_;
+  // Total of all pix sizes in the document.
+  int64_t memory_used_;
+  // Max memory to use at any time.
+  int64_t max_memory_;
+  // Saved reader from LoadDocument to allow re-caching.
+  FileReader reader_;
+  // Mutex that protects pages_ and pages_offset_ against multiple parallel
+  // loads, and provides a wait for page.
+  SVMutex pages_mutex_;
+  // Mutex that protects other data members that callers want to access without
+  // waiting for a load operation.
+  mutable SVMutex general_mutex_;
+};
+    
+    
+#endif  // TESSERACT_CCSTRUCT_LINLSQ_H_
+
+    
+      // Copying a DENORM is allowed.
+  DENORM(const DENORM &);
+  DENORM& operator=(const DENORM&);
+  ~DENORM();
+    
+      // Functions to navigate the tree. Unlike the original implementation, we
+  // store the root at index 0.
+  int ParentNode(int index) const {
+    return (index + 1) / 2 - 1;
+  }
+  int LeftChild(int index) const {
+    return index * 2 + 1;
+  }
+    
+      /**
+   * /brief Customize set method for LayerParameter
+   * /tparam value string of caffe's layer configuration
+   * */
+  virtual void Set(void *head, const std::string &value) const {
+    caffe::NetParameter net_param;
+    if (!ReadProtoFromTextContent(value, &net_param))
+      CHECK(false)<< 'Caffe Net Prototxt: ' << value << 'Initialized Failed';
+    }
+    
+    namespace mxnet {
+namespace io {
+// iterator on image recordio
+class PrefetcherIter : public IIterator<DataBatch> {
+ public:
+  explicit PrefetcherIter(IIterator<TBlobBatch>* base)
+      : loader_(base), out_(nullptr) {}
+    }
+    }
+    }
+    
+    struct GradientCompressionParam : public dmlc::Parameter<GradientCompressionParam> {
+  std::string type;
+  float threshold;
+  DMLC_DECLARE_PARAMETER(GradientCompressionParam) {
+    DMLC_DECLARE_FIELD(type)
+      .describe('Type of gradient compression to use, like `2bit` for example');
+    DMLC_DECLARE_FIELD(threshold).set_default(0.5)
+      .describe('Threshold to use for 2bit gradient compression');
+  }
+};
     
     
     {
-    {}  // namespace load_reporter
-}  // namespace grpc
-    
-    #endif  // !defined(GPR_LINUX) && !defined(GPR_WINDOWS) && !defined(GPR_APPLE)
-
-    
-        std::shared_ptr<DHTNode> localNode;
-    
-    void DHTReplaceNodeTask::startup() { sendMessage(); }
-    
-    DHTResponseMessage::~DHTResponseMessage() = default;
-    
-    #include <cstring>
-#include <cassert>
-#include <cstdio>
-#include <utility>
-    
-    #include <vector>
-#include <string>
-#include <memory>
-    
-      virtual void startup() = 0;
-    
-    #include <algorithm>
-    
-    #include 'common.h'
-    
-    std::shared_ptr<DHTTask>
-DHTTaskFactoryImpl::createPeerAnnounceTask(const unsigned char* infoHash)
-{
-  // TODO
-  return nullptr;
-}
-    
-    
-    {  virtual void addImmediateTask(const std::shared_ptr<DHTTask>& task) = 0;
+    {
+    {  bool init_cudnn_;
+  cudnnDataType_t dtype_;
+  cudnnSpatialTransformerDescriptor_t st_desc_;
+  cudnnTensorDescriptor_t in_desc_;
+  cudnnTensorDescriptor_t out_desc_;
+  cudnnSamplerType_t sampler_;
+  #if CUDNN_MAJOR >= 5
+  cudnnTensorFormat_t format_;
+  #endif
+  SpatialTransformerParam param_;
 };
+#endif  // __CUDACC__ && CUDNN
+}  // namespace op
+}  // namespace mxnet
     
-    void DHTTaskQueueImpl::addPeriodicTask1(const std::shared_ptr<DHTTask>& task)
-{
-  periodicTaskQueue1_.addTask(task);
-}
-    
-    #include <cstring>
-#include <cstdlib>
-    
-    DNSCache::AddrEntry& DNSCache::AddrEntry::operator=(const AddrEntry& c)
-{
-  if (this != &c) {
-    addr_ = c.addr_;
-    good_ = c.good_;
-  }
-  return *this;
-}
-    
-    
-
-    
-    namespace BulkOperations {
+    template<typename xpu>
+void NDArrayOp<xpu>::Forward(const OpContext &ctx,
+                   const std::vector<TBlob> &in_data,
+                   const std::vector<OpReqType> &req,
+                   const std::vector<TBlob> &out_data,
+                   const std::vector<TBlob> &aux_args) {
+  using namespace mshadow;
+  Context ndctx = get_ctx();
+  std::vector<void*> ptrs;
+  std::vector<Engine::VarHandle> ndvar;
+  std::vector<int> tags;
+  for (auto& i : req) CHECK_NE(i, kAddTo);
     }
-    
-    template <class T>
-TabViewModel::ModelFactory getTabModelFactory() {
-  return TabViewModel::ModelFactory(
-      [](QSharedPointer<RedisClient::Connection> c, int dbIndex) {
-        return QSharedPointer<TabModel>(new T(c, dbIndex),
-                                        &QObject::deleteLater);
-      });
-}
-
-    
-        public:
-        enum Roles {
-            name = Qt::UserRole + 1,
-            arguments,
-            summary,
-            since
-        };
-    
-     public slots:
-  void executeCommand(const QString &);
