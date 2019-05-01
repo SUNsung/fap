b1eab52a79d05527fb2ffdbab3250e768ba2a7da
SUNsung <weatherius@gmail.com>
2019-05-01 08:51:55 +0300

    Rewrite [16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb] File in: 2019-05-01 08:51:54

--- ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
+++ ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
@@ -1,81 +1,190 @@
 
         
-                message = '#{tag} (fastlane)'
-        expect(result).to eq('git tag -am #{message.shellescape} #{tag.shellescape}')
+                expect(result).to eq('git tag -am #{message.shellescape} --force #{tag.shellescape}')
       end
     
-            expect(Open3).to receive(:capture3).with(cmd).and_return('')
-        expect(FastlaneCore::CertChecker).to receive(:wwdr_keychain).and_return(keychain_name)
+          it 'adds clean_output param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            clean_output: true
+          )
+        end').runner.execute(:test)
     
-        def run
-      program :name, 'cert'
-      program :version, Fastlane::VERSION
-      program :description, 'CLI for \'cert\' - Create new iOS code signing certificates'
-      program :help, 'Author', 'Felix Krause <cert@krausefx.com>'
-      program :help, 'Website', 'https://fastlane.tools'
-      program :help, 'Documentation', 'https://docs.fastlane.tools/actions/cert/'
-      program :help_formatter, :compact
-    
-    # SPECS ===============================================================
-    
-          def escape(object)
-        case object
-        when Hash   then escape_hash(object)
-        when Array  then object.map { |o| escape(o) }
-        when String then escape_string(object)
-        when Tempfile then object
-        else nil
+          it 'yields command output' do
+        expect_command('ls', '-la', exitstatus: 1, output: 'Heeeelp! Something went wrong.')
+        Fastlane::Actions.sh('ls', '-la') do |status, result|
+          expect(status.exitstatus).to eq(1)
+          expect(result).to eq('Heeeelp! Something went wrong.')
         end
       end
     
-        it 'Reads referrer from Host header when Referer header is missing' do
-      env = {'HTTP_HOST' => 'foo.com'}
-      expect(subject.referrer(env)).to eq('foo.com')
-    end
+            keychain = 'keychain with spaces.keychain'
+        cmd = %r{curl -f -o (([A-Z]\:)?\/.+) https://developer\.apple\.com/certificationauthority/AppleWWDRCA.cer && security import \1 -k #{Regexp.escape(keychain.shellescape)}}
+        require 'open3'
     
-            def set_up_shipping_category
-          if shipping_category = params[:product].delete(:shipping_category)
-            id = ShippingCategory.find_or_create_by(name: shipping_category).id
-            params[:product][:shipping_category_id] = id
+              @registered.each do |plugin|
+            configs = plugin.data[:config_upgrade_safe]
+            if configs
+              configs.each do |key|
+                result[key] = plugin.config.get(key)
+              end
+            end
           end
-        end
-      end
+    
+      def show
+    @status = status_finder.status
+    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
+  end
+    
+      def account_from_topic
+    if hub_topic.present? && local_domain? && account_feed_path?
+      Account.find_local(hub_topic_params[:username])
     end
+  end
+    
+      def process_salmon
+    SalmonWorker.perform_async(@account.id, payload.force_encoding('UTF-8'))
   end
 end
 
     
-          # Pathname of given project searching only global directories
-      def global_project(name)
-        project_in(environment, name) ||
-          project_in(xdg, name) ||
-          project_in(home, name)
-      end
+          weeks << {
+        week: week.to_time.to_i.to_s,
+        statuses: Redis.current.get('activity:statuses:local:#{week_id}') || '0',
+        logins: Redis.current.pfcount('activity:logins:#{week_id}').to_s,
+        registrations: Redis.current.get('activity:accounts:local:#{week_id}') || '0',
+      }
+    end
     
-        context 'when no project name is provided' do
-      it 'should raise if the local project file doesn't exist' do
-        expect(File).to receive(:exist?).with(default) { false }
-        expect do
-          described_class.validate
-        end.to raise_error RuntimeError, %r{Project.+doesn't.exist}
-      end
+    module RateLimitHeaders
+  extend ActiveSupport::Concern
     
-          it 'returns false' do
-        expect(described_class.installed?).to be_falsey
+      def ambient_occlusion(isect)
+    basis = Array.new
+    otherBasis(basis, isect.n)
+    
+        def puts(*args)
+      STDERR.puts *args unless @silence
+    end
+    
+      def setup
+    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
+    success = Dir.chdir DUMMY_PATH do
+      silence_stdout_if !ENV['VERBOSE'] do
+        system 'node', 'manifest.js', tmp_dir
       end
     end
+    assert success, 'Node.js Mincer compilation failed'
+    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
+    css_name = manifest['assets']['application.css']
+    @css = File.read('#{tmp_dir}/#{css_name}')
+  end
+end
+
+    
+    Then(/^the releases path is created$/) do
+  run_vagrant_command(test_dir_exists(TestApp.releases_path))
+end
+    
+    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
+  file_shared_path = TestApp.shared_path.join(file)
+  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
+  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
+end
+    
+          def roles_for(names)
+        options = extract_options(names)
+        s = Filter.new(:role, names).filter(servers_by_key.values)
+        s.select { |server| server.select?(options) }
+      end
+    
+          # Register a validation rule for the given key.
+      def validate(key, &validator)
+        vs = (validators[key] || [])
+        vs << validator
+        validators[key] = vs
+      end
+    
+    set_if_empty :pty, false
+    
+    
+# The main fpm command entry point.
+class FPM::Command < Clamp::Command
+  include FPM::Util
+    
+        File.open(staging_path('+MANIFEST'), 'w+') do |file|
+      file.write(pkgdata.to_json + '\n')
+    end
+    
+      def self.default_prefix
+    npm_prefix = safesystemout('npm', 'prefix', '-g').chomp
+    if npm_prefix.count('\n') > 0
+      raise FPM::InvalidPackageConfiguration, '`npm prefix -g` returned unexpected output.'
+    elsif !File.directory?(npm_prefix)
+      raise FPM::InvalidPackageConfiguration, '`npm prefix -g` returned a non-existent directory'
+    end
+    logger.info('Setting default npm install prefix', :prefix => npm_prefix)
+    npm_prefix
   end
     
-            expect(window.panes).to match(
-          [
-            a_pane.with(index: 0).and_commands('vim'),
-            a_pane.with(index: 1).and_commands('ls'),
-            a_pane.with(index: 2).and_commands('top')
-          ]
-        )
-      end
-    end
+      # Returns path of a processed template PackageInfo given to 'pkgbuild --info'
+  # note: '--info' is undocumented:
+  # http://managingosx.wordpress.com/2012/07/05/stupid-tricks-with-pkgbuild
+  def pkginfo_template_path
+    pkginfo_template = Tempfile.open('fpm-PackageInfo')
+    pkginfo_data = template('osxpkg.erb').result(binding)
+    pkginfo_template.write(pkginfo_data)
+    pkginfo_template.close
+    pkginfo_template.path
+  end # def write_pkginfo_template
     
-    lib = File.expand_path('../lib', __FILE__)
-$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require 'tmuxinator/version'
+        # Create a temporary config file
+    logger.debug('Creating pear config file')
+    config = File.expand_path(build_path('pear.config'))
+    installroot = attributes[:prefix] || '/usr/share'
+    safesystem('pear', 'config-create', staging_path(installroot), config)
+    
+        args = [ '-B', build_path('build-info'), '-c', build_path('comment'), '-d', build_path('description'), '-f', build_path('packlist'), '-I', '/opt/local', '-p', staging_path,  '-U', '#{cwd}/#{name}-#{self.version}-#{iteration}.tgz' ]
+    safesystem('pkg_create', *args)
+    
+      private
+  def input(command)
+    platforms = [
+      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
+      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
+      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
+      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
+      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
+    ]
+    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
+                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
+                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
+                             'log_directory', 'log_file_stderr', 'log_file_stdout']
+    
+    
+    
+      def initialize(package_name, opts = {}, &block)
+    @options = OpenStruct.new(:name => package_name.to_s)
+    @source, @target = opts.values_at(:source, :target).map(&:to_s)
+    @directory = File.expand_path(opts[:directory].to_s)
+    
+      # Raised if a safesystem program exits nonzero
+  class ProcessFailed < StandardError; end
+    
+    Given /^I update my user view to include the attachment$/ do
+  steps %{
+    Given I overwrite 'app/views/users/show.html.erb' with:
+      '''
+      <p>Name: <%= @user.name %></p>
+      <p>Attachment: <%= image_tag @user.attachment.url %></p>
+      '''
+  }
+end
+    
+    module RailsCommandHelpers
+  def framework_version?(version_string)
+    framework_version =~ /^#{version_string}/
+  end
