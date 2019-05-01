c925021b3729392b25ed2dad1c07d29ad22b0b1b
SUNsung <weatherius@gmail.com>
2019-05-01 18:16:53 +0300

    Rewrite [007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py] File in: 2019-05-01 18:16:53

--- pyton/007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py
+++ pyton/007.FiUbGeC600rmwku1wqr0o00XnzJu9WgZQM00i8e.py
@@ -1,68 +1,188 @@
 
         
-                self.assertEqual(self.vhosts[5], self._call(self.vhosts))
+          if FLAGS.noise_type == 'poisson':
+    noisy_data = spikify_data(rates, rng, rnn['dt'], rnn['max_firing_rate'])
+  elif FLAGS.noise_type == 'gaussian':
+    noisy_data = gaussify_data(rates, rng, rnn['dt'], rnn['max_firing_rate'])
+  else:
+    raise ValueError('Only noise types supported are poisson or gaussian')
     
-            self.assertTrue(self.addr_default.conflicts(self.addr))
-        self.assertTrue(self.addr_default.conflicts(self.addr1))
-        self.assertTrue(self.addr_default.conflicts(self.addr_defined))
+    rng = np.random.RandomState(seed=FLAGS.synth_data_seed)
+rnn_rngs = [np.random.RandomState(seed=FLAGS.synth_data_seed+1),
+            np.random.RandomState(seed=FLAGS.synth_data_seed+2)]
+T = FLAGS.T
+C = FLAGS.C
+N = FLAGS.N
+nreplications = FLAGS.nreplications
+E = nreplications * C
+train_percentage = FLAGS.train_percentage
+ntimesteps = int(T / FLAGS.dt)
+    
+      Returns:
+    loss_matrix:  Loss matrix of shape [batch_size, sequence_length].
+  '''
+  cross_entropy_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+      labels=gen_labels, logits=gen_logits)
+  return cross_entropy_loss
+    
+      Args:
+    hparams:  Hyperparameters for the MaskGAN.
+    sequence:  tf.int32 Tensor sequence of shape [batch_size, sequence_length]
+    is_training:  Whether the model is training.
+    reuse (Optional):  Whether to reuse the model.
+    
+      The `attention_decoder_fn_inference` is called with user arguments
+  and returns the `decoder_fn`, which can be passed to the
+  `dynamic_rnn_decoder`, such that
+    
+        state = module.params.get('state')
+    group_name = module.params.get('name').lower()
+    group_description = module.params.get('description')
+    group_subnets = module.params.get('subnets') or {}
+    
+        module = AnsibleModule(argument_spec=argument_spec,)
+    
+        if module.params.get('max_items'):
+        params['MaxItems'] = module.params.get('max_items')
     
     
-def voc_info(json_dataset):
-    year = json_dataset.name[4:8]
-    image_set = json_dataset.name[9:]
-    devkit_path = get_devkit_dir(json_dataset.name)
-    assert os.path.exists(devkit_path), \
-        'Devkit directory {} not found'.format(devkit_path)
-    anno_path = os.path.join(
-        devkit_path, 'VOC' + year, 'Annotations', '{:s}.xml')
-    image_set_path = os.path.join(
-        devkit_path, 'VOC' + year, 'ImageSets', 'Main', image_set + '.txt')
-    return dict(
-        year=year,
-        image_set=image_set,
-        devkit_path=devkit_path,
-        anno_path=anno_path,
-        image_set_path=image_set_path)
-
+DOCUMENTATION = '''
+---
+module: gcpubsub_facts
+version_added: '2.3'
+short_description: List Topics/Subscriptions and Messages from Google PubSub.
+description:
+    - List Topics/Subscriptions from Google PubSub.  Use the gcpubsub module for
+      topic/subscription management.
+      See U(https://cloud.google.com/pubsub/docs) for an overview.
+requirements:
+  - 'python >= 2.6'
+  - 'google-auth >= 0.5.0'
+  - 'google-cloud-pubsub >= 0.22.0'
+notes:
+  - list state enables user to list topics or subscriptions in the project.  See examples for details.
+author:
+  - 'Tom Melendez (@supertom) <tom@supertom.com>'
+options:
+  topic:
+    description:
+       - GCP pubsub topic name.  Only the name, not the full path, is required.
+    required: False
+  view:
+    description:
+       - Choices are 'topics' or 'subscriptions'
+    required: True
+  state:
+    description:
+       - list is the only valid option.
+    required: False
+'''
     
-    from detectron.core.config import cfg
-from detectron.utils.c2 import const_fill
-from detectron.utils.c2 import gauss_fill
-from detectron.utils.net import get_group_gn
-import detectron.utils.blob as blob_utils
+            if add_rules:
+            firewall_policy = _add_firewall_rules(module,
+                                                  oneandone_conn,
+                                                  firewall_policy['id'],
+                                                  add_rules)
+            _check_mode(module, firewall_policy)
+            changed = True
     
-    from detectron.core.config import cfg
-from detectron.datasets import json_dataset
-from detectron.datasets import roidb as roidb_utils
-import detectron.modeling.FPN as fpn
-import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
-import detectron.utils.blob as blob_utils
+    # Copyright: (c) 2015, VMware, Inc. All Rights Reserved.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
     
-        def forward(self, inputs, outputs):
-        '''See modeling.detector.GenerateProposalLabels for inputs/outputs
-        documentation.
-        '''
-        # During training we reuse the data loader code. We populate roidb
-        # entries on the fly using the rois generated by RPN.
-        # im_info: [[im_height, im_width, im_scale], ...]
-        rois = inputs[0].data
-        roidb = blob_utils.deserialize(inputs[1].data)
-        im_info = inputs[2].data
-        im_scales = im_info[:, 2]
-        output_blob_names = fast_rcnn_roi_data.get_fast_rcnn_blob_names()
-        # For historical consistency with the original Faster R-CNN
-        # implementation we are *not* filtering crowd proposals.
-        # This choice should be investigated in the future (it likely does
-        # not matter).
-        json_dataset.add_proposals(roidb, rois, im_scales, crowd_thresh=0)
-        roidb_utils.add_bbox_regression_targets(roidb)
-        blobs = {k: [] for k in output_blob_names}
-        fast_rcnn_roi_data.add_fast_rcnn_blobs(blobs, im_scales, roidb)
-        for i, k in enumerate(output_blob_names):
-            blob_utils.py_op_copy_blob(blobs[k], outputs[i])
-
+        for rule in desired_rules:
+        if rule not in current_rules:
+            additions += 1
+            if not module.check_mode:
+                gateway.add_nat_rule(**rule)
+            result['changed'] = True
+    result['rules_added'] = additions
     
-        if fg_inds.shape[0] > 0:
-        # Class labels for the foreground rois
-        mask_class_labels = blobs['labels_int32'][fg_inds]
-        masks = blob_utils.zeros((fg_inds.shape[0], M**2), int32=True)
+        module_hbacrule = get_hbacrule_dict(description=module.params['description'],
+                                        hostcategory=hostcategory,
+                                        ipaenabledflag=ipaenabledflag,
+                                        servicecategory=servicecategory,
+                                        sourcehostcategory=sourcehostcategory,
+                                        usercategory=usercategory)
+    ipa_hbacrule = client.hbacrule_find(name=name)
+    
+    
+DOCUMENTATION = '''
+---
+module: spectrum_device
+short_description: Creates/deletes devices in CA Spectrum.
+description:
+   - This module allows you to create and delete devices in CA Spectrum U(https://www.ca.com/us/products/ca-spectrum.html).
+   - Tested on CA Spectrum 9.4.2, 10.1.1 and 10.2.1
+version_added: '2.6'
+author: 'Renato Orgito (@orgito)'
+options:
+    device:
+        aliases: [ host, name ]
+        required: true
+        description:
+            - IP address of the device.
+            - If a hostname is given, it will be resolved to the IP address.
+    community:
+        description:
+            - SNMP community used for device discovery.
+            - Required when C(state=present).
+    landscape:
+        required: true
+        description:
+            - Landscape handle of the SpectroServer to which add or remove the device.
+    state:
+        required: false
+        description:
+            - On C(present) creates the device when it does not exist.
+            - On C(absent) removes the device when it exists.
+        choices: ['present', 'absent']
+        default: 'present'
+    url:
+        aliases: [ oneclick_url ]
+        required: true
+        description:
+            - HTTP, HTTPS URL of the Oneclick server in the form (http|https)://host.domain[:port]
+    url_username:
+        aliases: [ oneclick_user ]
+        required: true
+        description:
+            - Oneclick user name.
+    url_password:
+        aliases: [ oneclick_password ]
+        required: true
+        description:
+            - Oneclick user password.
+    use_proxy:
+        required: false
+        description:
+            - if C(no), it will not use a proxy, even if one is defined in an environment
+                variable on the target hosts.
+        default: 'yes'
+        type: bool
+    validate_certs:
+        required: false
+        description:
+            - If C(no), SSL certificates will not be validated. This should only be used
+                on personally controlled sites using self-signed certificates.
+        default: 'yes'
+        type: bool
+    agentport:
+        required: false
+        description:
+            - UDP port used for SNMP discovery.
+        default: 161
+notes:
+   -  The devices will be created inside the I(Universe) container of the specified landscape.
+   -  All the operations will be performed only on the specified landscape.
+'''
+    
+        from sklearn.tree import DecisionTreeClassifier
+    
+    The data is generated with the ``make_checkerboard`` function, then
+shuffled and passed to the Spectral Biclustering algorithm. The rows
+and columns of the shuffled matrix are rearranged to show the
+biclusters found by the algorithm.
+    
+    plt.legend(loc='best')
+    
+    # Part 1: Quantitative evaluation of various init methods
