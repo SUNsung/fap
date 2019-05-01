87e7143be41cdccfe53b41f311a2084abfcc6a0c
SUNsung <weatherius@gmail.com>
2019-05-01 09:54:37 +0300

    Rewrite [21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb] File in: 2019-05-01 09:54:36

--- ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
+++ ruby/21W2opHb8j9nc21BW1kdzP3EsU21DPttMJECOBk21iml.rb
@@ -1,102 +1,39 @@
 
         
-            def initialize_copy(other)
-      @paths = other.paths.dup
-      self
-    end
+                cmd << ['-am #{message.shellescape}']
+        cmd << '--force' if options[:force]
+        cmd << '-s' if options[:sign]
+        cmd << tag.shellescape
+        cmd << options[:commit].to_s if options[:commit]
     
-          def test_controller_raise_error_about_missing_helper
-        e = assert_raise(AbstractController::Helpers::MissingHelperError) { AbstractInvalidHelpers.helper(:missing) }
-        assert_equal 'Missing helper file helpers/missing_helper.rb', e.message
-      end
+          context 'as string' do
+        let(:path) { 'myfile.txt' }
     
-        # This returns whether the guest is ready to work. If this returns
-    # `false`, then {#detect!} should be called in order to detect the
-    # guest OS.
-    #
-    # @return [Boolean]
-    def ready?
-      !!capability_host_chain
-    end
+          it 'works given a path to the directory containing compile_commands.json' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+            oclint(
+              compile_commands: './fastlane/spec/fixtures/oclint'
+            )
+          end').runner.execute(:test)
+    
+          context 'when specify config_file options' do
+        it 'adds config option' do
+          result = Fastlane::FastFile.new.parse('lane :test do
+            swiftlint(
+              config_file: '#{config_file}'
+            )
+          end').runner.execute(:test)
+    
+    # Windows implementation
+module WindowsShellwords
+  def shellescape(str)
+    str = str.to_s
+    
+      # https://stackoverflow.com/a/18623297/252627, last variant
+  require 'open3'
+  Open3.popen3(compare_command) do |stdin, stdout, stderr, thread|
+    error = stderr.read.chomp
+    # expect(error).to eq(expected_compare_error)
+    expect(error).to eq(expected_compare_error) # match(/#{expected_compare_error}/)
   end
 end
-
-    
-            # Execute a command on the remote machine. The exact semantics
-        # of this method are up to the implementor, but in general the
-        # users of this class will expect this to be a shell.
-        #
-        # This method gives you no way to write data back to the remote
-        # machine, so only execute commands that don't expect input.
-        #
-        # @param [String] command Command to execute.
-        # @yield [type, data] Realtime output of the command being executed.
-        # @yieldparam [String] type Type of the output. This can be
-        #   `:stdout`, `:stderr`, etc. The exact types are up to the
-        #   implementor.
-        # @yieldparam [String] data Data for the given output.
-        # @return [Integer] Exit code of the command.
-        def execute(command, opts=nil)
-        end
-    
-            # Helper method that will set a value if a value is given, or otherwise
-        # return the already set value.
-        #
-        # @param [Symbol] key Key for the data
-        # @param [Object] value Value to store.
-        # @return [Object] Stored value.
-        def self.get_or_set(key, value=UNSET_VALUE)
-          # If no value is to be set, then return the value we have already set
-          return data[key] if value.eql?(UNSET_VALUE)
-    
-            # This method is expected to return a class that is used for
-        # configuring the provisioner. This return value is expected to be
-        # a subclass of {Config}.
-        #
-        # @return [Config]
-        def self.config_class
-        end
-    
-              # Make sure we're only working with one VM if single target
-          if options[:single_target] && machines.length != 1
-            @logger.debug('Using primary machine since single target')
-            primary_name = @env.primary_machine_name
-            raise Errors::MultiVMTargetRequired if !primary_name
-            machines = [get_machine.call(primary_name)]
-          end
-    
-            format('%{foo}', foo: obj).should == '42'
-      end
-    end
-  end
-    
-        $?.should be_an_instance_of Process::Status
-    $?.pid.should be_kind_of(Integer)
-    $?.exitstatus.should == 127
-  end
-    
-      it 'has no effect on immediate values' do
-    [nil, true, false].each do |v|
-      v.taint
-      v.tainted?.should == false
-    end
-  end
-    
-        def dry_run
-      ['--dry-run', '-n',
-       'Do a dry run without executing actions',
-       lambda do |_value|
-         Configuration.env.set(:sshkit_backend, SSHKit::Backend::Printer)
-       end]
-    end
-    
-          def untrusted_keys
-        keys - @trusted_keys
-      end
-    
-    # We use a special :_default_git value so that SCMResolver can tell whether the
-# default has been replaced by the user via `set`.
-set_if_empty :scm, Capistrano::Configuration::SCMResolver::DEFAULT_GIT
-set_if_empty :branch, 'master'
-set_if_empty :deploy_to, -> { '/var/www/#{fetch(:application)}' }
-set_if_empty :tmp_dir, '/tmp'
