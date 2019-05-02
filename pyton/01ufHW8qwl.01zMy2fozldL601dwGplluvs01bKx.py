335a7d3a0cf5a13489818e51260b0cb9e127149d
SUNsung <weatherius@gmail.com>
2019-05-03 02:47:32 +0300

    Rewrite [01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py] File in: 2019-05-03 02:47:32

--- pyton/01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py
+++ pyton/01ufHW8qwl.01zMy2fozldL601dwGplluvs01bKx.py
@@ -1,57 +1,159 @@
 
         
-        password = key
-new_key = aes_encrypt(password, key_expansion(password))
-r = openssl_encode('aes-128-ctr', new_key, iv)
-print('aes_decrypt_text 16')
-print(repr(r))
+        # pick which RNN is used on each trial
+rnn_to_use = rng.randint(2, size=E)
+ext_input = np.repeat(np.expand_dims(rnn_to_use, axis=1), ntimesteps, axis=1)
+ext_input = np.expand_dims(ext_input, axis=2)  # these are 'a's' in the paper
     
-    with io.open(README_FILE, 'w', encoding='utf-8') as f:
-    f.write(header)
-    f.write(options)
-    f.write(footer)
+    import os
+import h5py
+import json
+    
+        with tf.gfile.Open(filename) as f:
+      idx = 0
+      for line in f:
+        word_name = line.strip()
+        if word_name == '<S>':
+          self._bos = idx
+        elif word_name == '</S>':
+          self._eos = idx
+        elif word_name == '<UNK>':
+          self._unk = idx
+        if word_name == '!!!MAXTERMID':
+          continue
+    
+    
+def build_vocab(filename):
+  data = _read_words(filename)
+    
+    
+def generate_logs(sess, model, log, id_to_word, feed):
+  '''Impute Sequences using the model for a particular feed and send it to
+  logs.
+  '''
+  # Impute Sequences.
+  [p, inputs_eval, sequence_eval] = sess.run(
+      [model.present, model.inputs, model.fake_sequence], feed_dict=feed)
+    
+      def construct_fn(attention_query, attention_keys, attention_values):
+    with tf.variable_scope(name, reuse=reuse) as scope:
+      context = attention_score_fn(attention_query, attention_keys,
+                                   attention_values)
+      concat_input = tf.concat([attention_query, context], 1)
+      attention = tf.contrib.layers.linear(
+          concat_input, num_units, biases_initializer=None, scope=scope)
+      return attention
+    
+    try:
+    import simplejson as json
+except ImportError:
+    import json
+    
+            # This MUST go after prepare_auth. Authenticators could add a hook
+        self.prepare_hooks(hooks)
+    
+    score(y_test, clf.predict(X_test), 'dense model')
+benchmark_dense_predict()
+clf.sparsify()
+score(y_test, clf.predict(X_test), 'sparse model')
+benchmark_sparse_predict()
 
     
-        def gen_ies_md(ies):
-        for ie in ies:
-            ie_md = '**{0}**'.format(ie.IE_NAME)
-            ie_desc = getattr(ie, 'IE_DESC', None)
-            if ie_desc is False:
-                continue
-            if ie_desc is not None:
-                ie_md += ': {0}'.format(ie.IE_DESC)
-            if not ie.working():
-                ie_md += ' (Currently broken)'
-            yield ie_md
+    # to store the results
+scikit_classifier_results = []
+scikit_regressor_results = []
+    
+    data, row_idx, col_idx = sg._shuffle(data, random_state=0)
+plt.matshow(data, cmap=plt.cm.Blues)
+plt.title('Shuffled dataset')
+    
+    The dataset is generated using the ``make_biclusters`` function, which
+creates a matrix of small values and implants bicluster with large
+values. The rows and columns are then shuffled and passed to the
+Spectral Co-Clustering algorithm. Rearranging the shuffled matrix to
+make biclusters contiguous shows how accurately the algorithm found
+the biclusters.
+    
+    from sklearn.cluster import AgglomerativeClustering
+from sklearn.neighbors import kneighbors_graph
     
     
-def _mkdir(d):
-    if not os.path.exists(d):
-        os.mkdir(d)
+# Plot clustering results
+for index, metric in enumerate(['cosine', 'euclidean', 'cityblock']):
+    model = AgglomerativeClustering(n_clusters=n_clusters,
+                                    linkage='average', affinity=metric)
+    model.fit(X)
+    plt.figure()
+    plt.axes([0, 0, 1, 1])
+    for l, c in zip(np.arange(model.n_clusters), 'rgbk'):
+        plt.plot(X[model.labels_ == l].T, c=c, alpha=.5)
+    plt.axis('tight')
+    plt.axis('off')
+    plt.suptitle('AgglomerativeClustering(affinity=%s)' % metric, size=20)
     
-    from youtube_dl.jsinterp import JSInterpreter
+            plt.xlim(-2.5, 2.5)
+        plt.ylim(-2.5, 2.5)
+        plt.xticks(())
+        plt.yticks(())
+        plt.text(.99, .01, ('%.2fs' % (t1 - t0)).lstrip('0'),
+                 transform=plt.gca().transAxes, size=15,
+                 horizontalalignment='right')
+        plot_num += 1
     
-        def test_proxy_https(self):
-        params = self._check_params(['primary_proxy', 'primary_server_ip'])
-        if params is None:
-            return
-        ydl = FakeYDL({
-            'proxy': params['primary_proxy']
-        })
-        self.assertEqual(
-            ydl.urlopen('https://yt-dl.org/ip').read().decode('utf-8'),
-            params['primary_server_ip'])
+    # There are two options for replacing |today|: either, you set today to some
+# non-false value, then it is used:
+#today = ''
+# Else, today_fmt is used as the format for a strftime call.
+#today_fmt = '%B %d, %Y'
     
+    .. caution::
+   You should protect these API credentials as you would the password to your
+   Cloudflare account. Users who can read this file can use these credentials
+   to issue arbitrary API calls on your behalf. Users who can cause Certbot to
+   run using these credentials can complete a ``dns-01`` challenge to acquire
+   new certificates or revoke existing certificates for associated domains,
+   even if those domains aren't being managed by this server.
     
-class StreamConsumedError(RequestException, TypeError):
-    '''The content for this response was already consumed'''
+    ==========================================  ===================================
+``--dns-digitalocean-credentials``          DigitalOcean credentials_ INI file.
+                                            (Required)
+``--dns-digitalocean-propagation-seconds``  The number of seconds to wait for
+                                            DNS to propagate before asking the
+                                            ACME server to verify the DNS
+                                            record.
+                                            (Default: 10)
+==========================================  ===================================
     
-    # Attempt to enable urllib3's SNI support, if possible
-try:
-    from urllib3.contrib import pyopenssl
-    pyopenssl.inject_into_urllib3()
+        with tf.variable_scope(name, reuse=reuse):
+        alpha = get_w(alpha_shape, w_initializer=alpha_init, name='alpha')
+        # o = relu(x) + 0.5 * tf.multiply(alpha, x - tf.abs(x))  # TFLearn
+        o = leaky_relu(x, alpha)  # TensorLayer / <Deep Learning>
     
-            assert len(server.handler_results) == 0
+    from ..utils import get_wb, permute
+from ..activations import softmax
     
-    A tree.TreeAdaptor is used by the parser to create tree.Tree objects for the
-input Token objects.
+        Args:
+        x(tf.Tensor):
+        n_unit(int): 
+        act_fn:
+        name(str):
+        reuse(bool):
+    '''
+    # n_input = tf.shape(x)[-1]  # err: need int but tensor
+    n_input = int(x.get_shape()[-1])
+    with tf.variable_scope(name or 'dense', reuse=reuse):
+        W, b = get_wb([n_input, n_unit])
+        o = act_fn(tf.matmul(x, W) + b)
+    return o
+    
+    References:
+    keras.regularizers
+'''
+import tensorflow as tf
+import numpy as np
+    
+            #Creating directory title page for current directory
+        with open('dir_title.html', 'w') as os_html:
+            os_html.write(header + dir_title)
+        
+        group.append(HTML('dir_title.html').render())
