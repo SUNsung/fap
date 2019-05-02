64e928210d509b87c4526769be00600a980ab95a
SUNsung <weatherius@gmail.com>
2019-05-02 14:46:37 +0300

    Rewrite [50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py] File in: 2019-05-02 14:46:37

--- pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
+++ pyton/50HvlJAG6ZdTw50zFeC3vUY4zY50kjFOVh4G.ts50EeY.py
@@ -1,177 +1,53 @@
 
         
-        data_train_truth = nparray_and_transpose(data_train_truth)
-data_valid_truth = nparray_and_transpose(data_valid_truth)
-data_train_spiking = nparray_and_transpose(data_train_spiking)
-data_valid_spiking = nparray_and_transpose(data_valid_spiking)
+                if opts.nolog:
+            self.settings.set('LOG_ENABLED', False, priority='cmdline')
     
-    # not the best way to do this but E is small enough
-rates = []
-spikes = []
-for trial in xrange(E):
-  if rnn_to_use[trial] == 0:
-    rates.append(rates_a[trial])
-    spikes.append(spikes_a[trial])
-  else:
-    rates.append(rates_b[trial])
-    spikes.append(spikes_b[trial])
+        def long_desc(self):
+        return ('Edit a spider using the editor defined in the EDITOR environment'
+                ' variable or else the EDITOR setting')
     
-        # Group maintain averages op.
-    if averages_op:
-      gen_train_op = tf.group(maximize_op, averages_op)
-    else:
-      gen_train_op = maximize_op
-    
-        def __repr__(self):
-        return str(dict(self.items()))
-    
-        @get_item_parameters
-    def test_getitem(self, key, value):
-        assert self.lookup_dict[key] == value
-    
-        styles = {
-        # No corresponding class for the following:
-        #Text:                     '', # class:  ''
-        Whitespace:                'underline #f8f8f8',      # class: 'w'
-        Error:                     '#a40000 border:#ef2929', # class: 'err'
-        Other:                     '#000000',                # class 'x'
-    }
-    
-    from base64 import b64encode
-    
-    import chardet
-    
-        if hasattr(jar, 'copy'):
-        # We're dealing with an instance of RequestsCookieJar
-        return jar.copy()
-    # We're dealing with a generic CookieJar instance
-    new_jar = copy.copy(jar)
-    new_jar.clear()
-    for cookie in jar:
-        new_jar.set_cookie(copy.copy(cookie))
-    return new_jar
-    
-        @pytest.mark.parametrize(
-        'url', (
-            'http://192.168.1.1:5000/',
-            'http://192.168.1.1/',
-            'http://www.requests.com/',
-        ))
-    def test_not_bypass(self, url):
-        assert get_environ_proxies(url, no_proxy=None) != {}
-    
-    
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'GroupCommitResolution'
-        db.create_table(
-            'sentry_groupcommitresolution', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'group_id',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
-                ), (
-                    'commit_id',
-                    self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')()
-                ), (
-                    'datetime', self.gf('django.db.models.fields.DateTimeField')(
-                        db_index=True
-                    )
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['GroupCommitResolution'])
-    
-    
-class Migration(SchemaMigration):
-    def forwards(self, orm):
-        # Adding model 'VersionDSymFile'
-        db.create_table(
-            'sentry_versiondsymfile', (
-                (
-                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
-                        primary_key=True
-                    )
-                ), (
-                    'dsym_file', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.ProjectDSymFile'], null=True
-                    )
-                ), (
-                    'dsym_app', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
-                        to=orm['sentry.DSymApp']
-                    )
-                ), ('version', self.gf('django.db.models.fields.CharField')(max_length=32)),
-                ('build', self.gf('django.db.models.fields.CharField')(max_length=32, null=True)), (
-                    'date_added',
-                    self.gf('django.db.models.fields.DateTimeField')()
-                ),
-            )
-        )
-        db.send_create_signal('sentry', ['VersionDSymFile'])
-    
-    
-class Migration(DataMigration):
-    def forwards(self, orm):
-        from sentry.utils.query import RangeQuerySetWrapperWithProgressBar
-    
-            users = orm.User.objects.exclude(
-            email__in=orm.UserEmail.objects.all().values_list('email', flat=True)
-        )
-    
-            # Deleting field 'CommitAuthor.external_id'
-        db.delete_column('sentry_commitauthor', 'external_id')
-    
-        def __init__(self, idf_path=None):
-        self.tokenizer = jieba.dt
-        self.postokenizer = jieba.posseg.dt
-        self.stop_words = self.STOP_WORDS.copy()
-        self.idf_loader = IDFLoader(idf_path or DEFAULT_IDF)
-        self.idf_freq, self.median_idf = self.idf_loader.get_idf()
-    
-        (prob, state) = max((V[len(obs) - 1][y], y) for y in 'ES')
-    
-        def _lcut_internal(self, sentence):
-        return list(self.__cut_internal(sentence))
-    
-    tags = jieba.analyse.extract_tags(content, topK=topK)
-    
-    jieba.analyse.set_idf_path('../extra_dict/idf.txt.big');
-    
-        The roulette wheel has 37 spots. If the bet is 0 and a 0 comes up,
-    you win a reward of 35. If the parity of your bet matches the parity
-    of the spin, you win 1. Otherwise you receive a reward of -1.
-    
-    def update_rollout_dict(spec, rollout_dict):
-    '''
-    Takes as input the environment spec for which the rollout is to be generated,
-    and the existing dictionary of rollouts. Returns True iff the dictionary was
-    modified.
-    '''
-    # Skip platform-dependent
-    if should_skip_env_spec_for_tests(spec):
-        logger.info('Skipping tests for {}'.format(spec.id))
-        return False
-    
-    def key_press(key, mod):
-    global human_agent_action, human_wants_restart, human_sets_pause
-    if key==0xff0d: human_wants_restart = True
-    if key==32: human_sets_pause = not human_sets_pause
-    a = int( key - ord('0') )
-    if a <= 0 or a >= ACTIONS: return
-    human_agent_action = a
-    
-                # Skid trace
-            if abs(force) > 2.0*friction_limit:
-                if w.skid_particle and w.skid_particle.grass==grass and len(w.skid_particle.poly) < 30:
-                    w.skid_particle.poly.append( (w.position[0], w.position[1]) )
-                elif w.skid_start is None:
-                    w.skid_start = w.position
-                else:
-                    w.skid_particle = self._create_particle( w.skid_start, w.position, grass )
-                    w.skid_start = None
+            if not assertion:
+            if self.min_bound == self.max_bound:
+                expected = self.min_bound
             else:
-                w.skid_start = None
-                w.skid_particle = None
+                expected = '%s..%s' % (self.min_bound, self.max_bound)
+    
+    from scrapy import twisted_version
+    
+            # XXX: Google parses at least first 100k bytes; scrapy's redirect
+        # middleware parses first 4k. 4k turns out to be insufficient
+        # for this middleware, and parsing 100k could be slow.
+        # We use something in between (32K) by default.
+        self.lookup_bytes = settings.getint('AJAXCRAWL_MAXSIZE', 32768)
+    
+        Extension is everything from the last dot to the end, ignoring
+    leading dots.  Returns '(root, ext)'; ext may be empty.'''
+    # NOTE: This code must work for text and bytes strings.
+    
+    
+if __name__ == '__main__':
+    main()
+
+    
+    # If we want to print a preview of the message content, we can extract whatever
+# the least formatted payload is and print the first three lines.  Of course,
+# if the message has no plain text part printing the first three lines of html
+# is probably useless, but this is just a conceptual example.
+simplest = msg.get_body(preferencelist=('plain', 'html'))
+print()
+print(''.join(simplest.get_content().splitlines(keepends=True)[:3]))
+    
+            try:
+            print(pool.map(f, list(range(10))))
+        except ZeroDivisionError:
+            print('\tGot ZeroDivisionError as expected from pool.map()')
+        else:
+            raise AssertionError('expected ZeroDivisionError')
+    
+            # h_tilde(h~): question to context attended query vectors
+        b = tf.reduce_max(S, axis=2)  # [N, T]
+        b = softmax(b, axis=-1)  # [N, T]
+        b = tf.expand_dims(b, axis=1)  # [N, 1, T]
+        h_tilde = tf.matmul(b, h)  # [N, 1, d]
+        h_tilde = tf.tile(h_tilde, [1, T, 1])  # [N, T, d]
