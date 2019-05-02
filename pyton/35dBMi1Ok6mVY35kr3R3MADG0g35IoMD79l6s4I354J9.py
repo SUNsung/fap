b90d6e97b422eea35c48a936849323cb34cd3cf8
SUNsung <weatherius@gmail.com>
2019-05-02 05:25:02 +0300

    Rewrite [35dBMi1Ok6mVY35kr3R3MADG0g35IoMD79l6s4I354J9.py] File in: 2019-05-02 05:25:02

--- pyton/35dBMi1Ok6mVY35kr3R3MADG0g35IoMD79l6s4I354J9.py
+++ pyton/35dBMi1Ok6mVY35kr3R3MADG0g35IoMD79l6s4I354J9.py
@@ -1,152 +1,89 @@
 
         
-                if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
+        
+DOCUMENTATION = '''
+---
+module: iam_server_certificate_facts
+short_description: Retrieve the facts of a server certificate
+description:
+  - Retrieve the attributes of a server certificate
+version_added: '2.2'
+author: 'Allen Sanabria (@linuxdynasty)'
+requirements: [boto3, botocore]
+options:
+  name:
+    description:
+      - The name of the server certificate you are retrieving attributes for.
+    required: true
+extends_documentation_fragment:
+    - aws
+    - ec2
+'''
+    
+            for rule in rules:
+            firewall_rule = oneandone.client.FirewallPolicyRule(
+                protocol=rule['protocol'],
+                port_from=rule['port_from'],
+                port_to=rule['port_to'],
+                source=rule['source'])
+            firewall_rules.append(firewall_rule)
     
     
-@pytest.fixture
-def app():
-    '''Create and configure a new app instance for each test.'''
-    # create a temporary file to isolate the database for each test
-    db_fd, db_path = tempfile.mkstemp()
-    # create the app with common test config
-    app = create_app({
-        'TESTING': True,
-        'DATABASE': db_path,
-    })
+DOCUMENTATION = '''
+---
+module: honeybadger_deployment
+author: 'Benjamin Curtis (@stympy)'
+version_added: '2.2'
+short_description: Notify Honeybadger.io about app deployments
+description:
+   - Notify Honeybadger.io about app deployments (see http://docs.honeybadger.io/article/188-deployment-tracking)
+options:
+  token:
+    description:
+      - API token.
+    required: true
+  environment:
+    description:
+      - The environment name, typically 'production', 'staging', etc.
+    required: true
+  user:
+    description:
+      - The username of the person doing the deployment
+  repo:
+    description:
+      - URL of the project repository
+  revision:
+    description:
+      - A hash, number, tag, or other identifier showing what revision was deployed
+  url:
+    description:
+      - Optional URL to submit the notification to.
+    default: 'https://api.honeybadger.io/v1/deploys'
+  validate_certs:
+    description:
+      - If C(no), SSL certificates for the target url will not be validated. This should only be used
+        on personally controlled sites using self-signed certificates.
+    type: bool
+    default: 'yes'
     
-        with app.app_context():
-        db = get_db()
-        count = db.execute('SELECT COUNT(id) FROM post').fetchone()[0]
-        assert count == 2
+        return device_ip
+    
+            o = tf.nn.conv2d(x, W, strides=strides, padding=padding) + b
+        o = act_fn(o)
     
     
-def explain_template_loading_attempts(app, template, attempts):
-    '''This should help developers understand what failed'''
-    info = ['Locating template '%s':' % template]
-    total_found = 0
-    blueprint = None
-    reqctx = _request_ctx_stack.top
-    if reqctx is not None and reqctx.request.blueprint is not None:
-        blueprint = reqctx.request.blueprint
+    
+        return o
     
     
-def has_level_handler(logger):
-    '''Check if there is a handler in the logging chain that will handle the
-    given logger's :meth:`effective level <~logging.Logger.getEffectiveLevel>`.
+def get_shape(x):
     '''
-    level = logger.getEffectiveLevel()
-    current = logger
-    
-    
-# Core signals.  For usage examples grep the source code or consult
-# the API documentation in docs/api.rst as well as docs/signals.rst
-template_rendered = _signals.signal('template-rendered')
-before_render_template = _signals.signal('before-render-template')
-request_started = _signals.signal('request-started')
-request_finished = _signals.signal('request-finished')
-request_tearing_down = _signals.signal('request-tearing-down')
-got_request_exception = _signals.signal('got-request-exception')
-appcontext_tearing_down = _signals.signal('appcontext-tearing-down')
-appcontext_pushed = _signals.signal('appcontext-pushed')
-appcontext_popped = _signals.signal('appcontext-popped')
-message_flashed = _signals.signal('message-flashed')
-
-    
-        def cycle_key(self):
-        '''
-        Keep the same data but with a new key. Call save() and it will
-        automatically save a cookie with a new key at the end of the request.
-        '''
-        self.save()
-    
-        get_item_parameters = pytest.mark.parametrize(
-        'key, value', (
-            ('bad_gateway', 502),
-            ('not_a_key', None)
-        )
-    )
-    
-        def run(self):
-        try:
-            self.server_sock = self._create_socket_and_bind()
-            # in case self.port = 0
-            self.port = self.server_sock.getsockname()[1]
-            self.ready_event.set()
-            self._handle_requests()
-    
-        if cryptography_version < [1, 3, 4]:
-        warning = 'Old version of cryptography ({}) may cause slowdown.'.format(cryptography_version)
-        warnings.warn(warning, RequestsDependencyWarning)
-    
-        pyopenssl_info = {
-        'version': None,
-        'openssl_version': '',
-    }
-    if OpenSSL:
-        pyopenssl_info = {
-            'version': OpenSSL.__version__,
-            'openssl_version': '%x' % OpenSSL.SSL.OPENSSL_VERSION_NUMBER,
-        }
-    cryptography_info = {
-        'version': getattr(cryptography, '__version__', ''),
-    }
-    idna_info = {
-        'version': getattr(idna, '__version__', ''),
-    }
-    
-        if no_proxy:
-        # We need to check whether we match here. We need to see if we match
-        # the end of the hostname, both with and without the port.
-        no_proxy = (
-            host for host in no_proxy.replace(' ', '').split(',') if host
-        )
-    
-    
-def _remove_firewall_rule(module, oneandone_conn, firewall_id, rule_id):
+    References:
+        tflearn.utils.get_incoming_shape
     '''
-    Removes a rule from a firewall policy.
-    '''
-    try:
-        if module.check_mode:
-            rule = oneandone_conn.get_firewall_policy_rule(
-                firewall_id=firewall_id,
-                rule_id=rule_id)
-            if rule:
-                return True
-            return False
-    
-            monitoring_policy_process = oneandone.client.Process(
-            process=process['process'],
-            alert_if=process['alert_if'],
-            email_notification=process['email_notification']
-        )
-    
-            for k in rule.keys():
-            if k not in VALID_RULE_KEYS:
-                raise VcaError('%s is not a valid key in nat rules, please '
-                               'check above..' % k, valid_keys=VALID_RULE_KEYS)
-    
-        if state in ['present', 'enabled']:
-        ipaenabledflag = 'TRUE'
+    if isinstance(x, (tf.Tensor, tf.SparseTensor)):
+        return x.get_shape().as_list()
+    elif type(x) in [np.array, np.ndarray, list, tuple]:
+        return list(np.shape(x))
     else:
-        ipaenabledflag = 'FALSE'
-    
-        if state == 'present':
-        rc, out, err = module.run_command('%s followed %s' % (le_path, path))
-        if rc == 0:
-            return True
-    
-        resource = 'model?ipaddress=' + device_ip + '&commstring=' + module.params.get('community')
-    resource += '&landscapeid=' + module.params.get('landscape')
-    
-        return strings
+        raise Exception('Invalid `x`.')
