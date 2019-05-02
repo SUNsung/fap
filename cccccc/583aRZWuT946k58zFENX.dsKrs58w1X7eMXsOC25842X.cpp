6eba40f5702b13418203077682a992ce36d6b55f
SUNsung <weatherius@gmail.com>
2019-05-02 06:42:20 +0300

    Rewrite [583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp] File in: 2019-05-02 06:42:20

--- cccccc/583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp
+++ cccccc/583aRZWuT946k58zFENX.dsKrs58w1X7eMXsOC25842X.cpp
@@ -1,12 +1,9 @@
 
         
-        class GetMergeSingleListFeatureTensorsGradient : public GradientMakerBase {
-  using GradientMakerBase::GradientMakerBase;
-  vector<OperatorDef> GetGradientDefs() override {
-    vector<string> input_blob_names{};
-    vector<string> output_blob_names{};
+        namespace caffe2 {
     }
-    }
+    
+    workspace.ResetWorkspace()
     
     OPERATOR_SCHEMA(Glu)
     .NumInputs(1)
@@ -19,3 +16,359 @@ X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
 )DOC')
     .Input(0, 'X', '1D input tensor')
     .Output(0, 'Y', '1D output tensor');
+    
+    template <typename T, class Context>
+class BernoulliJSDGradientOp final : public Operator<Context> {
+ public:
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDGradientOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
+};
+    
+    // Functions producing parameter generators.
+//
+// Google Test uses these generators to produce parameters for value-
+// parameterized tests. When a parameterized test case is instantiated
+// with a particular generator, Google Test creates and runs tests
+// for each element in the sequence produced by the generator.
+//
+// In the following sample, tests from test case FooTest are instantiated
+// each three times with parameter values 3, 5, and 8:
+//
+// class FooTest : public TestWithParam<int> { ... };
+//
+// TEST_P(FooTest, TestThis) {
+// }
+// TEST_P(FooTest, TestThat) {
+// }
+// INSTANTIATE_TEST_CASE_P(TestSequence, FooTest, Values(3, 5, 8));
+//
+    
+    namespace internal {
+    }
+    
+    GTEST_DECLARE_string_(internal_run_death_test);
+    
+    // This exception is thrown by (and only by) a failed Google Test
+// assertion when GTEST_FLAG(throw_on_failure) is true (if exceptions
+// are enabled).  We derive it from std::runtime_error, which is for
+// errors presumably detectable only at run time.  Since
+// std::runtime_error inherits from std::exception, many testing
+// frameworks know how to extract and print the message inside it.
+class GTEST_API_ GoogleTestFailureException : public ::std::runtime_error {
+ public:
+  explicit GoogleTestFailureException(const TestPartResult& failure);
+};
+    
+      // Leave whatever circle we're part of.  Returns true if we were the
+  // last member of the circle.  Once this is done, you can join() another.
+  bool depart()
+      GTEST_LOCK_EXCLUDED_(g_linked_ptr_mutex) {
+    MutexLock lock(&g_linked_ptr_mutex);
+    }
+    
+    
+    {    // Now, we have i <= n/i < n.
+    // If n is divisible by i, n is not prime.
+    if (n % i == 0) return false;
+  }
+    
+      // Copy c'tor
+  MyString(const MyString& string) : c_string_(NULL) {
+    Set(string.c_string_);
+  }
+    
+    
+// This sample shows how to write a more complex unit test for a class
+// that has multiple member functions.
+//
+// Usually, it's a good idea to have one test for each method in your
+// class.  You don't have to do that exactly, but it helps to keep
+// your tests organized.  You may also throw in additional tests as
+// needed.
+    
+    	//Collapse duplicate points
+	bool foundDupe = true;
+	int nActive = nNodes;
+	while (foundDupe){
+		foundDupe = false;
+		for (int32 i=0; i < nNodes; ++i){
+			if (nodes[i].nConnected == 0) continue;
+			for (int32 j=i+1; j < nNodes; ++j){
+				if (nodes[j].nConnected == 0) continue;
+				b2Vec2 diff = nodes[i].position - nodes[j].position;
+				if (diff.LengthSquared() <= COLLAPSE_DIST_SQR){
+					if (nActive <= 3) {
+						// -- GODOT start --
+						delete[] nodes;
+						// -- GODOT end --
+						return b2Polygon();
+					}	
+					//printf('Found dupe, %d left\n',nActive);
+					--nActive;
+					foundDupe = true;
+					b2PolyNode* inode = &nodes[i];
+					b2PolyNode* jnode = &nodes[j];
+					//Move all of j's connections to i, and orphan j
+					int32 njConn = jnode->nConnected;
+					for (int32 k=0; k < njConn; ++k){
+						b2PolyNode* knode = jnode->connected[k];
+						b2Assert(knode != jnode);
+						if (knode != inode) {
+							inode->AddConnection(*knode);
+							knode->AddConnection(*inode);
+						}
+						knode->RemoveConnection(*jnode);
+						//printf('knode %d on node %d now has %d connections\n',k,j,knode->nConnected);
+						//printf('Found duplicate point.\n');
+					}
+					/*
+					printf('Orphaning node at address %d\n',(int)jnode);
+					for (int32 k=0; k<njConn; ++k) {
+						if (jnode->connected[k]->IsConnectedTo(*jnode)) printf('Problem!!!\n');
+					}
+					for (int32 k=0; k < njConn; ++k){
+						jnode->RemoveConnectionByIndex(k);
+					}
+					*/
+					jnode->nConnected = 0;
+				}
+			}
+		}
+	}
+	
+	/*
+	// Debugging: check for connection consistency
+	for (int32 i=0; i<nNodes; ++i) {
+		int32 nConn = nodes[i].nConnected;
+		printf('Node %d has %d connections\n',i,nConn);
+		for (int32 j=0; j<nConn; ++j) {
+			if (nodes[i].connected[j]->nConnected == 0) {
+				printf('Problem with node %d connection at address %d\n',i,(int)(nodes[i].connected[j]));
+				b2Assert(false);
+			}
+			b2PolyNode* connect = nodes[i].connected[j];
+			bool found = false;
+			for (int32 k=0; k<connect->nConnected; ++k) {
+				if (connect->connected[k] == &nodes[i]) found = true;
+			}
+			if (!found) printf('Connection %d (of %d) on node %d (of %d) did not have reciprocal connection.\n',j,nConn,i,nNodes);
+			b2Assert(found);
+		}
+	}//*/
+    
+    	void print(){
+		printFormatted();
+		/*
+		for (int32 i=0; i<nVertices; ++i){
+			printf('i: %d, x:%f, y:%f\n',i,x[i],y[i]);
+		}
+		*/
+	}
+    
+    	struct RawImage
+	{
+		int uiExtendedWidth;
+		int uiExtendedHeight;
+		unsigned int uiEncodingBitsBytes;
+		std::shared_ptr<unsigned char> paucEncodingBits;
+	};
+    
+    				case SourceAlphaMix::TRANSPARENT:
+					m_pencoding = new Block4x4Encoding_RGBA8_Transparent;
+					break;
+    
+    		inline bool GetFlip(void)
+		{
+			return m_pencoding->GetFlip();
+		}
+    
+    		ColorFloatRGBA	m_afrgbaDecodedColors[PIXELS];	// decoded RGB components, ignore Alpha
+		float			m_afDecodedAlphas[PIXELS];		// decoded alpha component
+		float			m_fError;						// error for RGBA relative to m_pafrgbaSource
+    
+    struct DoublePoint
+{
+  double X;
+  double Y;
+  DoublePoint(double x = 0, double y = 0) : X(x), Y(y) {}
+  DoublePoint(IntPoint ip) : X((double)ip.X), Y((double)ip.Y) {}
+};
+//------------------------------------------------------------------------------
+    
+    int fastlz_compress(const void* input, int length, void* output);
+    
+    #ifndef SILK_MACROS_ARMv4_H
+#define SILK_MACROS_ARMv4_H
+    
+    #endif
+
+    
+      /**
+   * Use the Iterate method for iterating over options that are stored as
+   * objects in runtime options (e.g. FilesMatch). This function iterates over
+   * the settings passed as ini/hdf, calls back to, generally, the constructor
+   * of the object in question.
+   *
+   * Note: For now, we are not `ini_get()` enabling these type of options as
+   * it is not trivial to come up with a non-hacky and workable way to store
+   * the data correctly. Also, as usual, Hdf takes priority.
+   */
+  static void Iterate(std::function<void (const IniSettingMap&,
+                                          const Hdf&,
+                                          const std::string&)> cb,
+                      const IniSettingMap &ini, const Hdf& config,
+                      const std::string &name, const bool prepend_hhvm = true);
+    
+    
+    {  if (base64) {
+    decoded = string_base64_decode(data, data_len, true);
+    if (decoded.isNull()) {
+      raise_warning('unable to decode base64 data');
+      return nullptr;
+    }
+  } else {
+    decoded = url_decode(data, data_len);
+  }
+  return req::make<MemFile>(decoded.data(), decoded.size());
+}
+    
+    #ifndef HPHP_DATA_STREAM_WRAPPER_H
+#define HPHP_DATA_STREAM_WRAPPER_H
+    
+    CachedDirectory::CachedDirectory(const String& path) {
+  assertx(File::IsVirtualDirectory(path));
+  m_files = StaticContentCache::TheFileCache->readDirectory(path.c_str());
+}
+    
+    
+    {  // if the function was called via FCallBuiltin, we'll get a bogus name as
+  // the stack frame will be wrong
+  ActRec* ar = g_context->getStackFrame();
+  const char* fn = (ar != nullptr)
+    ? ar->func()->name()->data()
+    : 'OPTIMIZED_BUILTIN';
+  raise_warning('%s(%s): failed to open stream: '
+                'wrapper does not support stream open',
+                fn, filename.data());
+  return nullptr;
+}
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+      // overriding ResourceData
+  const String& o_getClassNameHook() const override { return classnameof(); }
+    
+      virtual void startup() = 0;
+    
+    #endif // D_DHT_TASK_QUEUE_H
+
+    
+      DHTTaskExecutor immediateTaskQueue_;
+    
+    bool DHTTokenTracker::validateToken(const std::string& token,
+                                    const unsigned char* infoHash,
+                                    const std::string& ipaddr,
+                                    uint16_t port) const
+{
+  for (auto& elem : secret_) {
+    if (generateToken(infoHash, ipaddr, port, elem) == token) {
+      return true;
+    }
+  }
+  return false;
+}
+    
+    namespace aria2 {
+    }
+    
+    
+    {};
+    
+    
+  while (true)
+  {
+    xfer += iprot->readFieldBegin(fname, ftype, fid);
+    if (ftype == ::apache::thrift::protocol::T_STOP) {
+      break;
+    }
+    switch (fid)
+    {
+      case 1:
+        if (ftype == ::apache::thrift::protocol::T_I32) {
+          xfer += iprot->readI32(this->code);
+          this->__isset.code = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      case 2:
+        if (ftype == ::apache::thrift::protocol::T_STRING) {
+          xfer += iprot->readString(this->message);
+          this->__isset.message = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      case 3:
+        if (ftype == ::apache::thrift::protocol::T_I64) {
+          xfer += iprot->readI64(this->uuid);
+          this->__isset.uuid = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      default:
+        xfer += iprot->skip(ftype);
+        break;
+    }
+    xfer += iprot->readFieldEnd();
+  }
+    
+    typedef std::map<std::string, class InternalOptionInfo>  InternalOptionList;
+    
+    namespace osquery {
+namespace tables {
+    }
+    }
+    
+    void genNFSShare(const std::string& share_line, QueryData& results) {
+  auto line = osquery::split(share_line);
+  if (line.size() == 0 || boost::starts_with(line[0], '#')) {
+    return;
+  }
+    }
+    
+      // Get an integral value, 0 or 1, for whether a syscall table pointer is modified.
+  auto f1 = osquery::readFile(kKernelSyscallAddrModifiedPath, content);
+  if (f1.ok()) {
+    boost::trim(content);
+    syscall_addr_modified = content;
+  } else {
+    VLOG(1) << 'Cannot read file: ' << kKernelSyscallAddrModifiedPath;
+    return results;
+  }
+    
+    static inline std::ostream& operator<<(
+    std::ostream& stream, const std::vector<hardwareDriver>& devs) {
+  for (const auto& dev : devs) {
+    stream << ' ' << dev.driver;
+  }
+    }
+    
+    struct TestMessage {
+  int a_;
+  int b_;
+  char c_;
+  char d_;
+};
+    
+    #include <osquery/tests/integration/tables/helper.h>
+    
+    // Sanity check integration test for kernel_extensions
+// Spec file: specs/darwin/kernel_extensions.table
+    
+    // Sanity check integration test for kernel_integrity
+// Spec file: specs/linux/kernel_integrity.table
+    
+    #include <osquery/tests/integration/tables/helper.h>
