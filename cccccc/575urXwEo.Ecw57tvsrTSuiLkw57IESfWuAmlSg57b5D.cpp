2a57db817b4e29f6b06681e05fa64c60021a3cd7
SUNsung <weatherius@gmail.com>
2019-05-03 09:07:06 +0300

    Rewrite [575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp] File in: 2019-05-03 09:07:05

--- cccccc/575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp
+++ cccccc/575urXwEo.Ecw57tvsrTSuiLkw57IESfWuAmlSg57b5D.cpp
@@ -1,422 +1,109 @@
 
         
-          assert(capacity % 16 == 0 && 'not allocating multiple of alignment');
+        	const float32 inv3 = 1.0f / 3.0f;
+	b2Vec2 pRef(0.0f, 0.0f);
+	for (int32 i = 0; i < count; ++i)
+	{
+		// Triangle vertices.
+		b2Vec2 p1 = pRef;
+		b2Vec2 p2 = vs[i];
+		b2Vec2 p3 = i + 1 < count ? vs[i+1] : vs[0];
+    }
     
-    namespace clang {
-class Sema;
-class NamedDecl;
-class TypeDecl;
-class FunctionDecl;
+    			return fDRed*fDRed + fDGreen*fDGreen + fDBlue*fDBlue + fDAlpha*fDAlpha;
+		}
+		else if (m_errormetric == ErrorMetric::RGBX)
+		{
+			assert(a_fDecodedAlpha >= 0.0f);
+    
+    /** 16x32 multiplication, followed by a 16-bit shift right. Results fits in 32 bits */
+#undef MULT16_32_Q16
+static OPUS_INLINE opus_val32 MULT16_32_Q16_armv4(opus_val16 a, opus_val32 b)
+{
+  unsigned rd_lo;
+  int rd_hi;
+  __asm__(
+      '#MULT16_32_Q16\n\t'
+      'smull %0, %1, %2, %3\n\t'
+      : '=&r'(rd_lo), '=&r'(rd_hi)
+      : '%r'(b),'r'(a<<16)
+  );
+  return rd_hi;
+}
+#define MULT16_32_Q16(a, b) (MULT16_32_Q16_armv4(a, b))
+    
+    #undef USUB32
+#define USUB32(a, b) USUB32_(a, b, __FILE__, __LINE__)
+static OPUS_INLINE unsigned int USUB32_(opus_uint64 a, opus_uint64 b, char *file, int line)
+{
+   opus_uint64 res;
+   if (!VERIFY_UINT(a) || !VERIFY_UINT(b))
+   {
+      fprintf (stderr, 'USUB32: inputs are not uint32: %llu %llu in %s: line %d\n', a, b, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   if (a<b)
+   {
+      fprintf (stderr, 'USUB32: inputs underflow: %llu < %llu in %s: line %d\n', a, b, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = a-b;
+   if (!VERIFY_UINT(res))
+   {
+      fprintf (stderr, 'USUB32: output is not uint32: %llu - %llu = %llu in %s: line %d\n', a, b, res, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips+=2;
+   return res;
 }
     
-    static void printNode(DemanglerPrinter &Out, const Node *node, unsigned depth) {
-  // Indent two spaces per depth.
-  for (unsigned i = 0; i < depth * 2; ++i) {
-    Out << ' ';
-  }
-  if (!node) {
-    Out << '<<NULL>>';
+    opus_int32 silk_inner_prod_aligned(
+    const opus_int16 *const     inVec1,             /*    I input vector 1                                              */
+    const opus_int16 *const     inVec2,             /*    I input vector 2                                              */
+    const opus_int              len,                /*    I vector lengths                                              */
+    int                         arch                /*    I Run-time architecture                                       */
+);
+    
+    constexpr size_t RpcServerStatsEncoding::kRpcServerStatsSize;
+constexpr size_t RpcServerStatsEncoding::kEncodeDecodeFailure;
+constexpr size_t RpcServerStatsEncoding::kVersionIdSize;
+constexpr size_t RpcServerStatsEncoding::kFieldIdSize;
+constexpr size_t RpcServerStatsEncoding::kVersionIdOffset;
+constexpr size_t RpcServerStatsEncoding::kVersionId;
+    
+      static void OnDoneRecvMessageCb(void* user_data, grpc_error* error);
+    
+      protobuf::FileDescriptorProto file_desc_proto;
+  grpc::string data;
+  file_desc->CopyTo(&file_desc_proto);
+  file_desc_proto.SerializeToString(&data);
+  response->mutable_file_descriptor_response()->add_file_descriptor_proto(data);
+    
+    
+    {
+    {}  // namespace load_reporter
+}  // namespace grpc
+    
+    #include <mach/mach.h>
+    
+    void TimepointHR2Timespec(const high_resolution_clock::time_point& from,
+                          gpr_timespec* to) {
+  high_resolution_clock::duration deadline = from.time_since_epoch();
+  seconds secs = duration_cast<seconds>(deadline);
+  if (from == high_resolution_clock::time_point::max() ||
+      secs.count() >= gpr_inf_future(GPR_CLOCK_REALTIME).tv_sec ||
+      secs.count() < 0) {
+    *to = gpr_inf_future(GPR_CLOCK_REALTIME);
     return;
   }
-  Out << 'kind=' << getNodeKindString(node->getKind());
-  if (node->hasText()) {
-    Out << ', text=\'' << node->getText() << '\'';
-  }
-  if (node->hasIndex()) {
-    Out << ', index=' << node->getIndex();
-  }
-  Out << '\n';
-  for (auto &child : *node) {
-    printNode(Out, child, depth + 1);
-  }
+  nanoseconds nsecs = duration_cast<nanoseconds>(deadline - secs);
+  to->tv_sec = static_cast<int64_t>(secs.count());
+  to->tv_nsec = static_cast<int32_t>(nsecs.count());
+  to->clock_type = GPR_CLOCK_REALTIME;
 }
-    
-    #ifndef BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
-#define BITCOIN_QT_SIGNVERIFYMESSAGEDIALOG_H
-    
-    static int secp256k1_gej_eq_x_var(const secp256k1_fe *x, const secp256k1_gej *a) {
-    secp256k1_fe r, r2;
-    VERIFY_CHECK(!a->infinity);
-    secp256k1_fe_sqr(&r, &a->z); secp256k1_fe_mul(&r, &r, x);
-    r2 = a->x; secp256k1_fe_normalize_weak(&r2);
-    return secp256k1_fe_equal_var(&r, &r2);
-}
-    
-    
-    {    secp256k1_ecdsa_recoverable_signature_load(ctx, &r, &s, recid, sig);
-    secp256k1_scalar_get_b32(&output64[0], &r);
-    secp256k1_scalar_get_b32(&output64[32], &s);
-    return 1;
-}
-    
-    
-    {    // Inputs that are effectively invalid under the target grammar.
-    // PROTOCOLINFO accepts an OtherLine that is just an OptArguments, which
-    // would make these inputs valid. However,
-    // - This parser is never used in that situation, because the
-    //   SplitTorReplyLine parser enables OtherLine to be skipped.
-    // - Even if these were valid, an OptArguments contains no semantic data,
-    //   so there is no point in parsing it.
-    CheckParseTorReplyMapping('ARGS', {});
-    CheckParseTorReplyMapping('MORE ARGS', {});
-    CheckParseTorReplyMapping('MORE  ARGS', {});
-    CheckParseTorReplyMapping('EVEN more=ARGS', {});
-    CheckParseTorReplyMapping('EVEN+more ARGS', {});
-}
-    
-    %define f r9d
-%define g r10d
-%define h r11d
-    
-    SHOULD_NOT_DO_GRADIENT(EnforceFinite);
-    
-    REGISTER_CPU_OPERATOR(Floor, FloorOp<float, CPUContext>);
-    
-    OPERATOR_SCHEMA(GatherRangesToDense)
-    .NumInputs(2, 3)
-    .NumOutputs(1, INT_MAX)
-    .SetDoc(R'DOC(
-Given DATA tensor of rank 1, and RANGES tensor of rank 3, gather values
-corresponding to each range into a separate output tensor. If the optional input
-KEY tensor is also given, the output will be sorted by KEY for each example.
-    
-            static Windows::UI::Xaml::Automation::Peers::AutomationNotificationProcessing GetWindowsNotificationProcessing(
-            CalculatorApp::Common::Automation::AutomationNotificationProcessing customProcessingType);
-    
-            static std::unordered_map<std::wstring, std::wstring> GetTokenToReadableNameMap();
-    
-      virtual void fillMessage(Dict* msgDict) CXX11_OVERRIDE;
-    
-    std::pair<std::vector<std::unique_ptr<Command>>,
-          std::vector<std::unique_ptr<Command>>>
-DHTSetup::setup(DownloadEngine* e, int family)
-{
-  std::vector<std::unique_ptr<Command>> tempCommands;
-  std::vector<std::unique_ptr<Command>> tempRoutineCommands;
-  if ((family != AF_INET && family != AF_INET6) ||
-      (family == AF_INET && DHTRegistry::isInitialized()) ||
-      (family == AF_INET6 && DHTRegistry::isInitialized6())) {
-    return {};
-  }
-  try {
-    // load routing table and localnode id here
-    std::shared_ptr<DHTNode> localNode;
-    }
-    }
-    
-    namespace aria2 {
-    }
-    
-      virtual std::shared_ptr<DHTTask>
-  createPeerAnnounceTask(const unsigned char* infoHash) = 0;
-    
-    void DHTTaskFactoryImpl::setLocalNode(const std::shared_ptr<DHTNode>& localNode)
-{
-  localNode_ = localNode;
-}
-    
-      virtual void
-  addPeriodicTask2(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
-    
-    void DHTTokenUpdateCommand::preProcess()
-{
-  if (getDownloadEngine()->getRequestGroupMan()->downloadFinished() ||
-      getDownloadEngine()->isHaltRequested()) {
-    enableExit();
-  }
-}
-    
-    DNSCache::DNSCache() {}
-    
-    
-    {    bool operator==(const CacheEntry& e) const;
-  };
-    
-      RateLimiter* low_pri_rate_limiter() { return low_pri_rate_limiter_.get(); }
-    
-      // Set a snapshot at start of transaction by setting set_snapshot=true
-  txn_options.set_snapshot = true;
-  txn = txn_db->BeginTransaction(write_options, txn_options);
-    
-    #include <memory>
-    
-            if(_rightBoundary < _leftBoundary)
-        {
-            // screen width is larger than world's boundary width
-            //set both in the middle of the world
-            _rightBoundary = _leftBoundary = (_leftBoundary + _rightBoundary) / 2;
-        }
-        if(_topBoundary < _bottomBoundary)
-        {
-            // screen width is larger than world's boundary width
-            //set both in the middle of the world
-            _topBoundary = _bottomBoundary = (_topBoundary + _bottomBoundary) / 2;
-        }
-    
-    void FlipY3D::update(float time)
-{
-    float angle = (float)M_PI * time; // 180 degrees
-    float mz = sinf( angle );
-    angle = angle / 2.0f;     // x calculates degrees from 0 to 90
-    float my = cosf(angle);
-    
-    Vec3    v0, v1, v, diff;
-    
-    v0 = getOriginalVertex(Vec2(1.0f, 1.0f));
-    v1 = getOriginalVertex(Vec2());
-    
-    float    y0 = v0.y;
-    float    y1 = v1.y;
-    float y;
-    Vec2    a, b, c, d;
-    
-    if (y0 > y1)
-    {
-        // Normal Grid
-        a.setZero();
-        b.set(0.0f, 1.0f);
-        c.set(1.0f, 0.0f);
-        d.set(1.0f, 1.0f);
-        y = y0;
-    }
-    else
-    {
-        // Reversed Grid
-        b.setZero();
-        a.set(0.0f, 1.0f);
-        d.set(1.0f, 0.0f);
-        c.set(1.0f, 1.0f);
-        y = y1;
-    }
-    
-    diff.y = y - y * my;
-    diff.z = fabsf(floorf((y * mz) / 4.0f));
-    
-    // bottom-left
-    v = getOriginalVertex(a);
-    v.y = diff.y;
-    v.z += diff.z;
-    setVertex(a, v);
-    
-    // upper-left
-    v = getOriginalVertex(b);
-    v.y -= diff.y;
-    v.z -= diff.z;
-    setVertex(b, v);
-    
-    // bottom-right
-    v = getOriginalVertex(c);
-    v.y = diff.y;
-    v.z += diff.z;
-    setVertex(c, v);
-    
-    // upper-right
-    v = getOriginalVertex(d);
-    v.y -= diff.y;
-    v.z -= diff.z;
-    setVertex(d, v);
-}
-    
-        //
-    // Overrides
-    //
-    virtual Repeat* clone() const override;
-    virtual Repeat* reverse() const override;
-    virtual void startWithTarget(Node *target) override;
-    virtual void stop(void) override;
-    /**
-     * @param dt In seconds.
-     */
-    virtual void update(float dt) override;
-    virtual bool isDone(void) const override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    Repeat() {}
-    virtual ~Repeat();
-    
-    #endif // __ACTION_CCACTION_MANAGER_H__
-
-    
-    
-    {
-    {
-    {            p.z = (r * ( 1 - cosBeta ) * cosTheta);// '100' didn't work for
-            p.x = p.z * sinf(rotateByYAxis) + p.x * cosf(rotateByYAxis);
-            p.z = p.z * cosf(rotateByYAxis) - p.x * sinf(rotateByYAxis);
-            p.z/=7;
-            //    Stop z coord from dropping beneath underlying page in a transition
-            // issue #751
-            if( p.z < 0.5f )
-            {
-                p.z = 0.5f;
-            }
-            
-            // Set new coords
-            p.x += getGridRect().origin.x;
-            setVertex(Vec2(i, j), p);
-            
-        }
-    }
-}
-    
-    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-****************************************************************************/
-#ifndef __ACTION_CCPAGETURN3D_ACTION_H__
-#define __ACTION_CCPAGETURN3D_ACTION_H__
-    
-        /**
-     * @brief Initializes the action with a duration, a 'from' percentage and a 'to' percentage.
-     * @param duration Specify the duration of the ProgressFromTo action. It's a value in seconds.
-     * @param fromPercentage Specify the source percentage.
-     * @param toPercentage Specify the destination percentage.
-     * @return If the creation success, return true; otherwise, return false.
-     */
-    bool initWithDuration(float duration, float fromPercentage, float toPercentage);
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-    
-    Animation* Animation::create(const Vector<AnimationFrame*>& arrayOfAnimationFrameNames, float delayPerUnit, unsigned int loops /* = 1 */)
-{
-    Animation *animation = new (std::nothrow) Animation();
-    animation->initWithAnimationFrames(arrayOfAnimationFrameNames, delayPerUnit, loops);
-    animation->autorelease();
-    return animation;
-}
-    
-    
-/**
- * AutoPolygon is a helper Object
- * AutoPolygon's purpose is to process an image into 2d polygon mesh in runtime
- * It has functions for each step in the process, from tracing all the points, to triangulation
- * the result can be then passed to Sprite::create() to create a Polygon Sprite
- */
-class CC_DLL AutoPolygon
-{
-public:
-    /**
-     * create an AutoPolygon and initialize it with an image file
-     * the image must be a 32bit PNG for current version 3.7
-     * @param   filename    a path to image file, e.g., 'scene1/monster.png'.
-     * @return  an AutoPolygon object;
-     */
-    AutoPolygon(const std::string &filename);
-    
-    /**
-     * Destructor of AutoPolygon.
-     */
-    ~AutoPolygon();
-    
-    /**
-     * trace all the points along the outline of the image, 
-     * @warning must create AutoPolygon with filename to use this function
-     * @param   rect    a texture rect for specify an area of the image
-     * @param   threshold   the value when alpha is greater than this value will be counted as opaque, default to 0.0
-     * @return  a vector of vec2 of all the points found in clockwise order
-     * @code
-     * auto ap = AutoPolygon('grossini.png');
-     * auto rect = Rect(100, 100, 200, 200);
-     * std::vector<Vec2> points = ap.trace(rect);//default threshold is 0.0
-     * @endcode
-     */
-     std::vector<Vec2> trace(const cocos2d::Rect& rect, float threshold = 0.0f);
-    
-    /**
-     * reduce the amount of points so its faster for GPU to process and draw
-     * based on Ramer-Douglas-Peucker algorithm
-     * @param   points  a vector of Vec2 points as input
-     * @param   rect    a texture rect for specify an area of the image to avoid over reduction
-     * @param   epsilon the perpendicular distance where points smaller than this value will be discarded
-     * @return  a vector of Vec2 of the remaining points in clockwise order
-     * @code
-     * auto ap = AutoPolygon();
-     * std::vector<Vec2> reduced = ap.reduce(inputPoints, rect);//default epsilon is 2
-     * @endcode
-     */
-    std::vector<Vec2> reduce(const std::vector<Vec2>& points, const Rect& rect, float epsilon = 2.0f);
-    
-    /**
-     * expand the points along their edge, useful after you reduce the points that cuts into the sprite
-     * using ClipperLib
-     * @param   points  a vector of Vec2 points as input
-     * @param   rect    a texture rect for specify an area of the image, the expanded points will be clamped in this rect, ultimately resulting in a quad if the expansion is too great
-     * @param   epsilon the distance which the edges will expand
-     * @return  a vector of Vec2 as the result of the expansion
-     * @code
-     * auto ap = AutoPolygon();
-     * std::vector<Vec2> expanded = ap.expand(inputPoints, rect, 2.0);
-     * @endcode
-     */
-    std::vector<Vec2> expand(const std::vector<Vec2>& points, const Rect& rect, float epsilon);
-    
-    /**
-     * Triangulate the input points into triangles for rendering
-     * using poly2tri
-     * @warning points must be closed loop, cannot have 2 points sharing the same position and cannot intersect itself
-     * @param   points  a vector of vec2 points as input
-     * @return  a Triangles object with points and indices
-     * @code
-     * auto ap = AutoPolygon();
-     * TrianglesCommand::Triangles myPolygons = ap.triangulate(myPoints);
-     * @endcode
-     */
-    TrianglesCommand::Triangles triangulate(const std::vector<Vec2>& points);
-    
-    /**
-     * calculate the UV coordinates for each points based on a texture rect
-     * @warning This method requires the AutoPolygon object to know the texture file dimension
-     * @param   rect    a texture rect to specify where to map the UV
-     * @param   verts   a pointer to the verts array, served both as input and output verts
-     * @param   count   the count for the verts array
-     * @code
-     * auto ap = AutoPolygon('grossini.png');
-     * TrianglesCommand::Triangles myPolygons = ap.triangulate(myPoints);
-     * ap.calculateUV(rect, myPolygons.verts, 20);
-     * @endcode
-     */
-    void calculateUV(const Rect& rect, V3F_C4B_T2F* verts, ssize_t count);
-    
-    /**
-     * a helper function, packing trace, reduce, expand, triangulate and calculate uv in one function
-     * @param   rect    texture rect, use Rect::ZERO for the size of the texture, default is Rect::ZERO
-     * @param   epsilon the value used to reduce and expand, default to 2.0
-     * @param   threshold   the value where bigger than the threshold will be counted as opaque, used in trace
-     * @return  a PolygonInfo, to use with sprite
-     * @code
-     * auto ap = AutoPolygon('grossini.png');
-     * PolygonInfo myInfo = ap.generateTriangles();//use all default values
-     * auto sp1 = Sprite::create(myInfo);
-     * polygonInfo myInfo2 = ap.generateTriangles(Rect::ZERO, 5.0, 0.1);//ap can be reused to generate another set of PolygonInfo with different settings
-     * auto sp2 = Sprite::create(myInfo2);
-     * @endcode
-     */
-    PolygonInfo generateTriangles(const Rect& rect = Rect::ZERO, float epsilon = 2.0f, float threshold = 0.05f);
-    
-    /**
-     * a helper function, packing autoPolygon creation, trace, reduce, expand, triangulate and calculate uv in one function
-     * @warning if you want to repetitively generate polygons, consider create an AutoPolygon object, and use generateTriangles function, as it only reads the file once
-     * @param   filename     A path to image file, e.g., 'scene1/monster.png'.
-     * @param   rect    texture rect, use Rect::ZERO for the size of the texture, default is Rect::ZERO
-     * @param   epsilon the value used to reduce and expand, default to 2.0
-     * @param   threshold   the value where bigger than the threshold will be counted as opaque, used in trace
-     * @return  a PolygonInfo, to use with sprite
-     * @code
-     * auto sp = Sprite::create(AutoPolygon::generatePolygon('grossini.png'));
-     * @endcode
-     */
-    static PolygonInfo generatePolygon(const std::string& filename, const Rect& rect = Rect::ZERO, float epsilon = 2.0f, float threshold = 0.05f);
-protected:
-    Vec2 findFirstNoneTransparentPixel(const Rect& rect, float threshold);
-    std::vector<cocos2d::Vec2> marchSquare(const Rect& rect, const Vec2& first, float threshold);
-    unsigned int getSquareValue(unsigned int x, unsigned int y, const Rect& rect, float threshold);
-    }
