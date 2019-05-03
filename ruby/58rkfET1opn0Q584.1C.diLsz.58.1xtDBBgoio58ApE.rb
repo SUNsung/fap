0c47883497d6aad397bec0b6c9757efc3f88f812
SUNsung <weatherius@gmail.com>
2019-05-03 12:17:17 +0300

    Rewrite [58rkfET1opn0Q584.1C.diLsz.58.1xtDBBgoio58ApE.rb] File in: 2019-05-03 12:17:17

--- ruby/58rkfET1opn0Q584.1C.diLsz.58.1xtDBBgoio58ApE.rb
+++ ruby/58rkfET1opn0Q584.1C.diLsz.58.1xtDBBgoio58ApE.rb
@@ -1,39 +1,110 @@
 
         
-            def value_for(user_id, date)
-      GivenDailyLike.find_for(user_id, date).pluck(:likes_given)[0] || 0
-    end
+        module ActionView
+  module Helpers
+    module Tags # :nodoc:
+      class CollectionRadioButtons < Base # :nodoc:
+        include CollectionHelpers
     
-        def data_url_string?(str)
-      str.start_with?(DATA_URL)
-    end
+                case options[:default]
+            when nil
+              Time.current
+            when Date, Time
+              options[:default]
+            else
+              default = options[:default].dup
     
-        def insert(index, *names)
-      @filters.insert assert_index(index), *filter_const(names)
-    end
-    
-        def add(path, content)
-      @pages[path] = content
-    end
-    
-            css('h1:not(:first-child)').each do |node|
-          node.name = 'h2'
-        end unless at_css('h2')
-    
-            subtitle = at_css('.hero-subtitle').try(:content)
-        breadcrumbs = css('.breadcrumbs li').map(&:content)[2..-2]
-    
-    Given /^I run a migration$/ do
-  step %[I successfully run `rake db:migrate --trace`]
+      gem.add_development_dependency 'danger'
+  gem.add_development_dependency 'mocha'
+  gem.add_development_dependency 'rspec'
+  gem.add_development_dependency 'rubocop', '0.48.1'
 end
+
     
-        def define
-      define_flush_errors
-      define_getters
-      define_setter
-      define_query
-      register_new_attachment
-      add_active_record_callbacks
-      add_paperclip_callbacks
-      add_required_validations
+            filter = Object.new
+        def filter.filter(servers)
+          block.call(servers)
+        end
+      elsif !filter.respond_to? :filter
+        raise TypeError, 'Provided custom filter <#{filter.inspect}> does ' \
+                         'not have a public 'filter' method'
+      end
+      @custom_filters ||= []
+      @custom_filters << filter
     end
+    
+          def using_default_scm?
+        return @using_default_scm if defined? @using_default_scm
+        @using_default_scm = (fetch(:scm) == DEFAULT_GIT)
+      end
+    
+          ServerKey = Struct.new(:hostname, :port)
+    
+    class Rack::Builder
+  include Sinatra::Delegator
+end
+
+    
+      # fetch data
+  fields = {
+    :authors => `git shortlog -sn`.force_encoding('utf-8').scan(/[^\d\s].*/),
+    :email   => ['mail@zzak.io', 'konstantin.haase@gmail.com'],
+    :files   => %w(License README.md Rakefile Gemfile rack-protection.gemspec) + Dir['lib/**/*']
+  }
+    
+          def encrypt(value)
+        options[:encryptor].hexdigest value.to_s
+      end
+    
+          def call(env)
+        status, headers, body = super
+        response = Rack::Response.new(body, status, headers)
+        request = Rack::Request.new(env)
+        remove_bad_cookies(request, response)
+        response.finish
+      end
+    
+        it 'Reads referrer from Host header when Referer header is relative' do
+      env = {'HTTP_HOST' => 'foo.com', 'HTTP_REFERER' => '/valid'}
+      expect(subject.referrer(env)).to eq('foo.com')
+    end
+    
+    # Set some attributes
+package.name = 'my-service'
+package.version = '1.0'
+    
+        pkgdata = {
+      'arch' => architecture,
+      'name' => name,
+      'version' => pkg_version,
+      'comment' => description,
+      'desc' => description,
+      'origin' => pkg_origin,
+      'maintainer' => maintainer,
+      'www' => url,
+      # prefix is required, but it doesn't seem to matter
+      'prefix' => '/',
+    }
+    
+      option '--validate', :flag, 'Validate with pkg install',
+    :default => true
+    
+        # This value is used later in the template for PKGINFO
+    size = safesystemout('du', '-sk', build_path).split(/\s+/)[0].to_i * 1024
+    builddate = Time.new.to_i
+    
+      # Where we keep metadata and post install scripts and such
+  def fpm_meta_path
+    @fpm_meta_path ||= begin
+                         path = File.join(staging_path, '.fpm')
+                         FileUtils.mkdir_p(path)
+                         path
+                       end
+  end
+end
+
+    
+      # Input a zipfile.
+  def input(input_path)
+    # use part of the filename as the package name
+    self.name = File.extname(input_path)[1..-1]
