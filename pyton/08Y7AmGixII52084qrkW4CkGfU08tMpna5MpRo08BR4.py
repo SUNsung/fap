99b98d894d63ad718a71bc41f51297c3d7358277
SUNsung <weatherius@gmail.com>
2019-05-03 03:47:14 +0300

    Rewrite [08Y7AmGixII52084qrkW4CkGfU08tMpna5MpRo08BR4.py] File in: 2019-05-03 03:47:14

--- pyton/08Y7AmGixII52084qrkW4CkGfU08tMpna5MpRo08BR4.py
+++ pyton/08Y7AmGixII52084qrkW4CkGfU08tMpna5MpRo08BR4.py
@@ -1,59 +1,169 @@
 
         
-            def exists(self, session_key=None):
-        '''
-        This method makes sense when you're talking to a shared resource, but
-        it doesn't matter when you're storing the information in the client's
-        cookie.
-        '''
-        return False
-    
-    from django.contrib.sites.shortcuts import get_current_site
-from django.core.paginator import EmptyPage, PageNotAnInteger
-from django.http import Http404
-from django.template.response import TemplateResponse
-from django.urls import reverse
-from django.utils.http import http_date
-    
-    model.compile(loss=keras.losses.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
-    
-        # Test single image
-    x = np.random.uniform(0, 255, (10, 10, 3))
-    inputs = Input(shape=x.shape)
-    outputs = Lambda(utils.preprocess_input, output_shape=x.shape)(inputs)
-    model = Model(inputs, outputs)
-    assert model.predict(x[np.newaxis])[0].shape == x.shape
+            try:
+        region, ec2_url, aws_connect_kwargs = get_aws_connection_info(module, boto3=True)
+        iam = boto3_conn(module, conn_type='client', resource='iam', region=region, endpoint=ec2_url, **aws_connect_kwargs)
+    except botocore.exceptions.ClientError as e:
+        module.fail_json(msg='Boto3 Client Error - ' + str(e.msg))
     
     
-def test_cce_one_hot():
-    y_a = K.variable(np.random.randint(0, 7, (5, 6)))
-    y_b = K.variable(np.random.random((5, 6, 7)))
-    objective_output = losses.sparse_categorical_crossentropy(y_a, y_b)
-    assert K.eval(objective_output).shape == (5, 6)
+def ensure(module, client):
+    zone_name = module.params['zone_name']
+    state = module.params['state']
     
-        # Arguments
-        y: class vector to be converted into a matrix
-            (integers from 0 to num_classes).
-        num_classes: total number of classes.
-        dtype: The data type expected by the input, as a string
-            (`float32`, `float64`, `int32`...)
+        try:
+        data = urlencode(params)
+        response, info = fetch_url(module, url, data=data)
+    except Exception as e:
+        module.fail_json(msg='Unable to notify Honeybadger: %s' % to_native(e), exception=traceback.format_exc())
+    else:
+        if info['status'] == 201:
+            module.exit_json(changed=True)
+        else:
+            module.fail_json(msg='HTTP result code: %d connecting to %s' % (info['status'], url))
     
-        @property
-    def activation(self):
-        return self.cell.activation
     
-    # Train the autoencoder
-autoencoder.fit(x_train_noisy,
-                x_train,
-                validation_data=(x_test_noisy, x_test),
-                epochs=30,
-                batch_size=batch_size)
+class Icinga2FeatureHelper:
+    def __init__(self, module):
+        self.module = module
+        self._icinga2 = module.get_bin_path('icinga2', True)
+        self.feature_name = self.module.params['name']
+        self.state = self.module.params['state']
     
-    learning_rate = 1e-6
-clip_norm = 1.0
+        if event == 'deploy':
+        if not revision_id:
+            module.fail_json(msg='revision_id required for deploy events')
+        try:
+            send_deploy_event(module, key, revision_id, deployed_by, deployed_to, repository)
+        except Exception as e:
+            module.fail_json(msg='unable to sent deploy event: %s' % to_native(e),
+                             exception=traceback.format_exc())
     
-    batch_size = 128
-num_classes = 10
-epochs = 20
+        def test_empty_cell(self):
+        def f(): print(a)
+        try:
+            f.__closure__[0].cell_contents
+        except ValueError:
+            pass
+        else:
+            self.fail('shouldn't be able to read an empty cell')
+        a = 12
+    
+            try:
+            loader.loadTestsFromName('')
+        except ValueError as e:
+            self.assertEqual(str(e), 'Empty module name')
+        else:
+            self.fail('TestLoader.loadTestsFromName failed to raise ValueError')
+    
+    # Same, but for 3.x to 2.x
+REVERSE_IMPORT_MAPPING = dict((v, k) for (k, v) in IMPORT_MAPPING.items())
+assert len(REVERSE_IMPORT_MAPPING) == len(IMPORT_MAPPING)
+REVERSE_NAME_MAPPING = dict((v, k) for (k, v) in NAME_MAPPING.items())
+assert len(REVERSE_NAME_MAPPING) == len(NAME_MAPPING)
+    
+    from argparse import ArgumentParser
+    
+    # In a real program you'd get the filename from the arguments.
+with open('outgoing.msg', 'rb') as fp:
+    msg = BytesParser(policy=policy.default).parse(fp)
+    
+    class MyManager(BaseManager):
+    pass
+    
+        if cfg.TEST.BBOX_REG:
+        # Apply bounding-box regression deltas
+        box_deltas = workspace.FetchBlob(core.ScopedName('bbox_pred')).squeeze()
+        # In case there is 1 proposal
+        box_deltas = box_deltas.reshape([-1, box_deltas.shape[-1]])
+        if cfg.MODEL.CLS_AGNOSTIC_BBOX_REG:
+            # Remove predictions for bg class (compat with MSRA code)
+            box_deltas = box_deltas[:, -4:]
+        pred_boxes = box_utils.bbox_transform(
+            boxes, box_deltas, cfg.MODEL.BBOX_REG_WEIGHTS
+        )
+        pred_boxes = box_utils.clip_tiled_boxes(pred_boxes, im.shape)
+        if cfg.MODEL.CLS_AGNOSTIC_BBOX_REG:
+            pred_boxes = np.tile(pred_boxes, (1, scores.shape[1]))
+    else:
+        # Simply repeat the boxes, once for each class
+        pred_boxes = np.tile(boxes, (1, scores.shape[1]))
+    
+    # coco (val5k)
+# INFO roidb.py: 220: 1        person: 21296
+# INFO roidb.py: 220: 2       bicycle: 628
+# INFO roidb.py: 220: 3           car: 3818
+# INFO roidb.py: 220: 4    motorcycle: 732
+# INFO roidb.py: 220: 5      airplane: 286 <------ irrelevant
+# INFO roidb.py: 220: 6           bus: 564
+# INFO roidb.py: 220: 7         train: 380
+# INFO roidb.py: 220: 8         truck: 828
+    
+        # sum -> ReLU
+    # shortcut function: by default using bn; support gn
+    add_shortcut = globals()[cfg.RESNETS.SHORTCUT_FUNC]
+    sc = add_shortcut(model, prefix, blob_in, dim_in, dim_out, stride)
+    if inplace_sum:
+        s = model.net.Sum([tr, sc], tr)
+    else:
+        s = model.net.Sum([tr, sc], prefix + '_sum')
+    
+    
+def _whctrs(anchor):
+    '''Return width, height, x center, and y center for an anchor (window).'''
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
+    
+    def add_ResNet_roi_conv5_head_for_keypoints(
+    model, blob_in, dim_in, spatial_scale
+):
+    '''Add a ResNet 'conv5' / 'stage5' head for Mask R-CNN keypoint prediction.
+    '''
+    model.RoIFeatureTransform(
+        blob_in,
+        '_[pose]_pool5',
+        blob_rois='keypoint_rois',
+        method=cfg.KRCNN.ROI_XFORM_METHOD,
+        resolution=cfg.KRCNN.ROI_XFORM_RESOLUTION,
+        sampling_ratio=cfg.KRCNN.ROI_XFORM_SAMPLING_RATIO,
+        spatial_scale=spatial_scale
+    )
+    # Using the prefix '_[pose]_' to 'res5' enables initializing the head's
+    # parameters using pretrained 'res5' parameters if given (see
+    # utils.net.initialize_from_weights_file)
+    s, dim_in = ResNet.add_stage(
+        model,
+        '_[pose]_res5',
+        '_[pose]_pool5',
+        3,
+        dim_in,
+        2048,
+        512,
+        cfg.KRCNN.DILATION,
+        stride_init=int(cfg.KRCNN.ROI_XFORM_RESOLUTION / 7)
+    )
+    return s, 2048
+    
+        if len(data.shape) == 1:
+        ret = np.empty((count, ), dtype=data.dtype)
+        ret.fill(fill)
+        ret[inds] = data
+    else:
+        ret = np.empty((count, ) + data.shape[1:], dtype=data.dtype)
+        ret.fill(fill)
+        ret[inds, :] = data
+    return ret
+    
+        # Perform any final work and validity checks after the collating blobs for
+    # all minibatch images
+    valid = True
+    if cfg.MODEL.KEYPOINTS_ON:
+        valid = keypoint_rcnn_roi_data.finalize_keypoint_minibatch(blobs, valid)
+    
+    
+@pytest.fixture
+def seriesd():
+    return tm.getSeriesData()
