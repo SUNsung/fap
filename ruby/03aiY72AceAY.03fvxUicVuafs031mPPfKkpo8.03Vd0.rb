6ff69897cedaa4014cfbf8fa4e6ef59ee94a519b
SUNsung <weatherius@gmail.com>
2019-05-04 00:08:10 +0300

    Rewrite [03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb] File in: 2019-05-04 00:08:09

--- ruby/03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb
+++ ruby/03aiY72AceAY.03fvxUicVuafs031mPPfKkpo8.03Vd0.rb
@@ -1,87 +1,117 @@
 
         
-            def did_show_message?
-      file_name = '.did_show_opt_info'
-    
-        def run!
-      require_program(:version, :description)
-      trap('INT') { abort(program(:int_message)) } if program(:int_message)
-      trap('INT') { program(:int_block).call } if program(:int_block)
-      global_option('-h', '--help', 'Display help documentation') do
-        args = @args - %w(-h --help)
-        command(:help).run(*args)
-        return
-      end
-      global_option('-v', '--version', 'Display version information') do
-        say(version)
-        return
-      end
-      parse_global_options
-      remove_global_options(options, @args)
-    
-          it 'adds html_anchors param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            html_anchors: 'some anchors'
-          )
-        end').runner.execute(:test)
-    
-        it 'adds an environment Hash at the beginning' do
-      message = 'A message'
-      command = command_from_args({ 'PATH' => '/usr/local/bin' }, 'git', 'commit', '-m', message)
-      expect(command).to eq('PATH=/usr/local/bin git commit -m #{message.shellescape}')
-    end
-    
-    # Windows implementation
-module WindowsShellwords
-  def shellescape(str)
-    str = str.to_s
-    
-    module Jekyll
-    
-          if markup =~ /(?<class>\S.*\s+)?(?<src>(?:https?:\/\/|\/|\S+\/)\S+)(?:\s+(?<width>\d+))?(?:\s+(?<height>\d+))?(?<title>\s+.+)?/i
-        @img = attributes.reduce({}) { |img, attr| img[attr] = $~[attr].strip if $~[attr]; img }
-        if /(?:'|')(?<title>[^'']+)?(?:'|')\s+(?:'|')(?<alt>[^'']+)?(?:'|')/ =~ @img['title']
-          @img['title']  = title
-          @img['alt']    = alt
+            # Extract each header value pair
+    header.split(/\r\n/mn).each { |str|
+      if (md = str.match(/^(.+?)\s*:\s*(.+?)\s*$/))
+        if (self[md[1]])
+          self[md[1]] << ', ' + md[2]
         else
-          @img['alt']    = @img['title'].gsub!(/'/, '&#34;') if @img['title']
+          self[md[1]] = md[2]
         end
-        @img['class'].gsub!(/'/, '') if @img['class']
       end
-      super
+    }
+  end
+    
+      # Returns a parsed json document.
+  # Instead of using regexes to parse the JSON body, you should use this.
+  #
+  # @return [Hash]
+  def get_json_document
+    json = {}
+    
+          # If the handler class requires a relative resource...
+      if (handler.relative_resource_required?)
+        # Substituted the mount point root in the request to make things
+        # relative to the mount point.
+        request.relative_resource = request.resource.gsub(/^#{root}/, '')
+        request.relative_resource = '/' + request.relative_resource if (request.relative_resource !~ /^\//)
+      end
+    
+              res
+        end
+      end
     end
+  end
+end
+
     
-      class PageFilters < Octopress::Hooks::Page
-    def pre_render(page)
-      OctopressFilters::pre_filter(page)
+                components.each do |c|
+              encoded << [c.length].pack('N')
+              encoded << c
+            end
+    
+                res = ''
+            case etype
+            when RC4_HMAC
+              res = encrypt_rc4_hmac(data, key, 5)
+            else
+              raise ::NotImplementedError, 'EncryptedData schema is not supported'
+            end
+    
+              # Decodes the Rex::Proto::Kerberos::Model::EncKdcResponse from an input
+          #
+          # @param input [String, OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [self] if decoding succeeds
+          # @raise [RuntimeError] if decoding doesn't succeed
+          def decode(input)
+            case input
+            when String
+              decode_string(input)
+            when OpenSSL::ASN1::ASN1Data
+              decode_asn1(input)
+            else
+              raise ::RuntimeError, 'Failed to decode EncKdcResponse, invalid input'
+            end
+    
+              # Decodes the enc_part
+          #
+          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
+          # @return [Rex::Proto::Kerberos::Model::EncryptedData]
+          def decode_enc_part(input)
+            Rex::Proto::Kerberos::Model::EncryptedData.decode(input.value[0])
+          end
+        end
+      end
     end
+  end
+end
     
-        # Evaluate dependencies.
-    if !attributes[:no_auto_depends?]
-	    pkgdepend_gen = safesystemout('pkgdepend', 'generate',  '-md', '#{staging_path}',  manifest_fn)
-      File.write(build_path('#{name}.p5m.3'), pkgdepend_gen)
+      gem.required_ruby_version = '>= 2.0'
+  gem.add_dependency 'airbrussh', '>= 1.0.0'
+  gem.add_dependency 'i18n'
+  gem.add_dependency 'rake', '>= 10.0.0'
+  gem.add_dependency 'sshkit', '>= 1.9.0'
     
-      # Helper for group lookup
-  def gid2group(gid)
-    begin
-      grent = Etc.getgrgid(gid)
-      return grent.name
-    rescue ArgumentError => e
-      # Invalid user id? No user? Return the uid.
-      logger.warn('Failed to find group for gid #{gid}')
-      return gid.to_s
+    Given(/^the configuration is in a custom location$/) do
+  TestApp.move_configuration_to_custom_location('app')
+end
+    
+        def add_filter(filter=nil, &block)
+      if block
+        raise ArgumentError, 'Both a block and an object were given' if filter
+    
+            if echo?
+          $stdin.gets
+        else
+          $stdin.noecho(&:gets).tap { $stdout.print '\n' }
+        end
+      rescue Errno::EIO
+        # when stdio gets closed
+        return
+      end
+    
+          def third_party_scm_name?
+        !built_in_scm_name?
+      end
+    
+            def lvalue(key)
+          key.to_s.chomp('=').to_sym
+        end
+      end
     end
-  end # def uid2user
-end # class FPM::Target::Puppet
+  end
+end
+
     
-        cleanup_staging
-    # Tell 'dir' to input '.' and chdir/prefix will help it figure out the
-    # rest.
-    dir.input('.')
-    @staging_path = dir.staging_path
-    dir.cleanup_build
-  end # def input
+      # Install JavaScript dependencies if using Yarn
+  # system('bin/yarn')
