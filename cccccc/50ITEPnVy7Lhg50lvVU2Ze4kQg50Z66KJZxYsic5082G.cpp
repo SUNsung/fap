7bdb8e6bc6cb8c57b69f2b05d7aa7a6acec9d3fe
SUNsung <weatherius@gmail.com>
2019-05-03 14:36:05 +0300

    Rewrite [50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp] File in: 2019-05-03 14:36:04

--- cccccc/50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp
+++ cccccc/50ITEPnVy7Lhg50lvVU2Ze4kQg50Z66KJZxYsic5082G.cpp
@@ -1,380 +1,164 @@
 
         
-        void Base::CallSync(const std::string& method,
-                    const base::ListValue& arguments,
-                    base::ListValue* result) {
-  NOTREACHED() << 'Uncatched callAsync in Base'
-               << ' method:' << method
-               << ' arguments:' << arguments;
+        Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+    
+    // Creates a numpy array in 'ret' and copies the content of tensor 't'
+// into 'ret'.
+Status ConvertTensorToNdarray(const Tensor& t, PyObject** ret);
+    
+    Safe_TFE_TensorHandlePtr make_safe(TFE_TensorHandle* handle) {
+  return Safe_TFE_TensorHandlePtr(handle);
 }
     
-    namespace nwapi {
-    }
+      const tensorflow::OpRegistrationData* op_reg_data;
+  auto status =
+      tensorflow::OpRegistry::Global()->LookUp(node_def.op(), &op_reg_data);
+  if (!status.ok()) {
+    LOG(WARNING) << 'Op ' << node_def.op() << ' not found: ' << status;
+    return '';
+  }
+  AddDefaultsToNodeDef(op_reg_data->op_def, &node_def);
+    
+    Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an 'AS IS' BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
     
     
-    {
-    {    if (zoom_controller) {
-      double zoom_factor = content::ZoomLevelToZoomFactor(zoom_controller->GetZoomLevel());
-      if (zoom_factor > content::kMaximumZoomFactor) {
-        zoom_factor = content::kMaximumZoomFactor;
-      }
-      if (zoom_factor < content::kMinimumZoomFactor) {
-        zoom_factor = content::kMinimumZoomFactor;
-      }
-      x *= zoom_factor;
-      y *= zoom_factor;
-    }
+    { private:
+  union {
+    ElemT value_;
+    DeviceMemory<ElemT> pointer_;
+  };
+  bool is_pointer_;
+};
     
-    Popup(x, y, rvh);
-  } else if (method == 'EnableShowEvent') {
-    arguments.GetBoolean(0, &enable_show_event_);
+      InternalExtensionInfo(const InternalExtensionInfo&);
+  InternalExtensionInfo& operator=(const InternalExtensionInfo&);
+  InternalExtensionInfo() : name(), version(), sdk_version(), min_sdk_version() {
+  }
+    
+      // Get an integral value, 0 or 1, for whether a syscall table pointer is modified.
+  auto f1 = osquery::readFile(kKernelSyscallAddrModifiedPath, content);
+  if (f1.ok()) {
+    boost::trim(content);
+    syscall_addr_modified = content;
   } else {
-    NOTREACHED() << 'Invalid call to Menu method:' << method
-                 << ' arguments:' << arguments;
-  }
-}
-    
-    base::string16 MenuDelegate::GetLabelForCommandId(int command_id) const {
-  MenuItem* item = object_manager_->GetApiObject<MenuItem>(command_id);
-  return item->label_;
-}
-    
-      keyval = GetKeycodeFromText(key);
-  if (keyval == ui::VKEY_UNKNOWN){
-    enable_shortcut_ = false;
-  } else {
-    enable_shortcut_ = true;
-    //only code for ctrl, shift, alt, super and meta modifiers
-    int modifiers_value = ui::EF_NONE;
-    modifiers = base::ToLowerASCII(modifiers);
-    if (modifiers.find('ctrl')!=std::string::npos){
-      modifiers_value |= ui::EF_CONTROL_DOWN;
-    }
-    if (modifiers.find('shift')!=std::string::npos){
-      modifiers_value |= ui::EF_SHIFT_DOWN ;
-    }
-    if (modifiers.find('alt')!=std::string::npos){
-      modifiers_value |= ui::EF_ALT_DOWN;
-    }
-    if (modifiers.find('super')!=std::string::npos
-     || modifiers.find('cmd')!=std::string::npos
-     || modifiers.find('command')!=std::string::npos){
-      modifiers_value |= ui::EF_COMMAND_DOWN;
-    }
-    if (modifiers.find('meta')!=std::string::npos){
-      meta_down_flag_ = true;
-    }
-    accelerator_ = ui::Accelerator(keyval,modifiers_value);
+    VLOG(1) << 'Cannot read file: ' << kKernelSyscallAddrModifiedPath;
+    return results;
   }
     
-     protected:
-  ~NwClipboardReadAvailableTypesFunction() override;
-    
-    #include <vector>
-    
-    int GzipInputStream::Inflate(int flush) {
-  if ((zerror_ == Z_OK) && (zcontext_.avail_out == 0)) {
-    // previous inflate filled output buffer. don't change input params yet.
-  } else if (zcontext_.avail_in == 0) {
-    const void* in;
-    int in_size;
-    bool first = zcontext_.next_in == NULL;
-    bool ok = sub_stream_->Next(&in, &in_size);
-    if (!ok) {
-      zcontext_.next_out = NULL;
-      zcontext_.avail_out = 0;
-      return Z_STREAM_END;
+    namespace osquery {
+namespace table_tests {
     }
-    zcontext_.next_in = static_cast<Bytef*>(const_cast<void*>(in));
-    zcontext_.avail_in = in_size;
-    if (first) {
-      int error = internalInflateInit2(&zcontext_, format_);
-      if (error != Z_OK) {
-        return error;
-      }
     }
-  }
-  zcontext_.next_out = static_cast<Bytef*>(output_buffer_);
-  zcontext_.avail_out = output_buffer_length_;
-  output_position_ = output_buffer_;
-  int error = inflate(&zcontext_, flush);
-  return error;
-}
-    
-    // Identity metafunction.
-template <class T>
-struct identity_ {
-  typedef T type;
-};
-    
-                    char c = stack.top();
-                stack.pop();
-    
-    /// LinkedList Test Helper Functions
-ListNode* createLinkedList(int arr[], int n){
-    }
-    
-    using namespace std;
     
     
     {
-    {        if( node ){
-            __inorderTraversal(node->left, res);
-            res.push_back( node->val );
-            __inorderTraversal(node->right, res);
-        }
-    }
-};
-    
-    
-    {
-    {
-    {        }
-        return res;
-    }
-};
-    
-    int main() {
-    }
-    
-    
-    {    return 0;
-}
+    {} // namespace table_tests
+} // namespace osquery
 
     
-    public:
-    vector<int> preorderTraversal(TreeNode* root) {
+    namespace osquery {
+namespace table_tests {
+    }
     }
     
-    #include 'remtrans.h'
-#include 'unicode/unifilt.h'
+    // A simple compaction algorithm that always compacts everything
+// to the highest level whenever possible.
+class FullCompactor : public Compactor {
+ public:
+  explicit FullCompactor(const Options options) : options_(options) {
+    compact_options_.compression = options_.compression;
+    compact_options_.output_file_size_limit =
+        options_.target_file_size_base;
+  }
+    }
     
-    UnicodeString &ScientificNumberFormatter::SuperscriptStyle::format(
-        const UnicodeString &original,
-        FieldPositionIterator &fpi,
-        const UnicodeString &preExponent,
-        const DecimalFormatStaticSets &staticSets,
-        UnicodeString &appendTo,
-        UErrorCode &status) const {
-    if (U_FAILURE(status)) {
-        return appendTo;
+      // Write a key OUTSIDE of transaction
+  db->Put(write_options, 'abc', 'xyz');
+    
+    namespace {
+// A dummy compaction filter
+class DummyCompactionFilter : public CompactionFilter {
+ public:
+  virtual ~DummyCompactionFilter() {}
+  virtual bool Filter(int level, const Slice& key, const Slice& existing_value,
+                      std::string* new_value, bool* value_changed) const {
+    return false;
+  }
+  virtual const char* Name() const { return 'DummyCompactionFilter'; }
+};
     }
-    FieldPosition fp;
-    int32_t copyFromOffset = 0;
-    while (fpi.next(fp)) {
-        switch (fp.getField()) {
-        case UNUM_EXPONENT_SYMBOL_FIELD:
-            appendTo.append(
-                    original,
-                    copyFromOffset,
-                    fp.getBeginIndex() - copyFromOffset);
-            copyFromOffset = fp.getEndIndex();
-            appendTo.append(preExponent);
-            break;
-        case UNUM_EXPONENT_SIGN_FIELD:
-            {
-                int32_t beginIndex = fp.getBeginIndex();
-                int32_t endIndex = fp.getEndIndex();
-                UChar32 aChar = original.char32At(beginIndex);
-                if (staticSets.fMinusSigns->contains(aChar)) {
-                    appendTo.append(
-                            original,
-                            copyFromOffset,
-                            beginIndex - copyFromOffset);
-                    appendTo.append(kSuperscriptMinusSign);
-                } else if (staticSets.fPlusSigns->contains(aChar)) {
-                    appendTo.append(
-                           original,
-                           copyFromOffset,
-                           beginIndex - copyFromOffset);
-                    appendTo.append(kSuperscriptPlusSign);
-                } else {
-                    status = U_INVALID_CHAR_FOUND;
-                    return appendTo;
-                }
-                copyFromOffset = endIndex;
-            }
-            break;
-        case UNUM_EXPONENT_FIELD:
-            appendTo.append(
-                    original,
-                    copyFromOffset,
-                    fp.getBeginIndex() - copyFromOffset);
-            if (!copyAsSuperscript(
-                    original,
-                    fp.getBeginIndex(),
-                    fp.getEndIndex(),
-                    appendTo,
-                    status)) {
-              return appendTo;
-            }
-            copyFromOffset = fp.getEndIndex();
-            break;
-        default:
-            break;
-        }
+    
+    using namespace rocksdb;
+    
+    namespace rocksdb {
     }
-    appendTo.append(
-            original, copyFromOffset, original.length() - copyFromOffset);
-    return appendTo;
+    
+      // Compress blocks using the specified compression algorithm.  This
+  // parameter can be changed dynamically.
+  //
+  // Default: kSnappyCompression, which gives lightweight but fast
+  // compression.
+  //
+  // Typical speeds of kSnappyCompression on an Intel(R) Core(TM)2 2.4GHz:
+  //    ~200-500MB/s compression
+  //    ~400-800MB/s decompression
+  // Note that these speeds are significantly faster than most
+  // persistent storage speeds, and therefore it is typically never
+  // worth switching to kNoCompression.  Even if the input data is
+  // incompressible, the kSnappyCompression implementation will
+  // efficiently detect that and will switch to uncompressed mode.
+  CompressionType compression;
+    
+    #endif  // ROCKSDB_LITE
+
+    
+    Speed* Speed::create(ActionInterval* action, float speed)
+{
+    Speed *ret = new (std::nothrow) Speed();
+    if (ret && ret->initWithAction(action, speed))
+    {
+        ret->autorelease();
+        return ret;
+    }
+    CC_SAFE_DELETE(ret);
+    return nullptr;
 }
     
-    U_NAMESPACE_BEGIN
-    
-    #ifndef __SHARED_DATEFORMATSYMBOLS_H__
-#define __SHARED_DATEFORMATSYMBOLS_H__
-    
-    #include 'unicode/utypes.h'
-#include 'sharedobject.h'
-    
-    UnicodeString &
-SmallIntFormatter::format(
-        int32_t smallPositiveValue,
-        const IntDigitCountRange &range,
-        UnicodeString &appendTo) {
-    int32_t digits = range.pin(gDigitCount[smallPositiveValue]);
+    void OrbitCamera::startWithTarget(Node *target)
+{
+    ActionCamera::startWithTarget(target);
     }
     
+    
+    {    return action;
+}
+    
+        //
+    // Overrides
+    //
     /**
- * A representation an acceptable range of digit counts for integers.
- */
-class U_I18N_API IntDigitCountRange : public UMemory {
-public:
-    /**
-     * No constraints: 0 up to INT32_MAX
+     * @param time In seconds.
      */
-    IntDigitCountRange() : fMin(0), fMax(INT32_MAX) { }
-    IntDigitCountRange(int32_t min, int32_t max);
-    int32_t pin(int32_t digitCount) const;
-    int32_t getMax() const { return fMax; }
-    int32_t getMin() const { return fMin; }
-private:
-    int32_t fMin;
-    int32_t fMax;
-};
+    virtual void update(float time) override;
+    virtual FlipX* reverse() const override;
+    virtual FlipX* clone() const override;
     
-            if (txtLoc > parseStart) {
-            value = number.getLong();
-            gotNumber = TRUE;
-    }
+CC_CONSTRUCTOR_ACCESS:
+    FlipX() :_flipX(false) {}
+    virtual ~FlipX() {}
     
-    #if !UCONFIG_NO_FORMATTING
-    
-    
-uint32_t ExtensionManager_registerExtension_args::read(::apache::thrift::protocol::TProtocol* iprot) {
-    }
-    
-    #include 'ExtensionManager.h'
-#include <thrift/protocol/TBinaryProtocol.h>
-#include <thrift/server/TSimpleServer.h>
-#include <thrift/transport/TServerSocket.h>
-#include <thrift/transport/TBufferTransports.h>
-    
-    const std::string kKernelSyscallAddrModifiedPath = '/sys/kernel/camb/syscall_addr_modified';
-const std::string kKernelTextHashPath = '/sys/kernel/camb/text_segment_hash';
-    
-    /*
-** Output the given string as a quoted according to C or TCL quoting rules.
-*/
-static void output_c_string(FILE* out, const char* z) {
-  unsigned int c;
-  fputc(''', out);
-  while ((c = *(z++)) != 0) {
-    if (c == '\\') {
-      fputc(c, out);
-      fputc(c, out);
-    } else if (c == ''') {
-      fputc('\\', out);
-      fputc(''', out);
-    } else if (c == '\t') {
-      fputc('\\', out);
-      fputc('t', out);
-    } else if (c == '\n') {
-      fputc('\\', out);
-      fputc('n', out);
-    } else if (c == '\r') {
-      fputc('\\', out);
-      fputc('r', out);
-    } else if (isprint(c & 0xff) == 0) {
-      fprintf(out, R'(\%03o)', c & 0xff);
-    } else {
-      fputc(c, out);
-    }
-  }
-  fputc(''', out);
-}
-    
-    struct mei_response {
-  uint32_t maxlen;
-  uint8_t version;
-};
-    
-    TEST_F(PerfOutputTests, move_constructor) {
-  auto buf = std::array<char, 4>{};
-  auto from_obj = ebpf::PerfOutput<TestMessage>{};
-  from_obj.size_ = buf.size();
-  from_obj.fd_ = 42;
-  from_obj.data_ptr_ = static_cast<void*>(buf.data());
-    }
-    
-    TEST_F(iokitDevicetree, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from iokit_devicetree');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'name', NormalType}
-  //      {'class', NormalType}
-  //      {'id', IntType}
-  //      {'parent', IntType}
-  //      {'device_path', NormalType}
-  //      {'service', IntType}
-  //      {'busy_state', IntType}
-  //      {'retain_count', IntType}
-  //      {'depth', IntType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
-}
-    
-    class iokitRegistry : public testing::Test {
- protected:
-  void SetUp() override {
-    setUpEnvironment();
-  }
-};
-    
-    TEST_F(kernelExtensions, test_sanity) {
-  // 1. Query data
-  auto const data = execute_query('select * from kernel_extensions');
-  // 2. Check size before validation
-  // ASSERT_GE(data.size(), 0ul);
-  // ASSERT_EQ(data.size(), 1ul);
-  // ASSERT_EQ(data.size(), 0ul);
-  // 3. Build validation map
-  // See helper.h for avaialbe flags
-  // Or use custom DataCheck object
-  // ValidatatioMap row_map = {
-  //      {'idx', IntType}
-  //      {'refs', IntType}
-  //      {'size', IntType}
-  //      {'name', NormalType}
-  //      {'version', NormalType}
-  //      {'linked_against', NormalType}
-  //      {'path', NormalType}
-  //}
-  // 4. Perform validation
-  // validate_rows(data, row_map);
-}
-    
-    class keychainAcls : public testing::Test {
- protected:
-  void SetUp() override {
-    setUpEnvironment();
-  }
-};
+        /**
+     * @js NA
+     * @lua NA
+     */
+    virtual ~ActionManager();
