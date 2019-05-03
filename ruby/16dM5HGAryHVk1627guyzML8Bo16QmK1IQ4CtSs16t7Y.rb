3b0a4008c69df819a0d561cb4cef3ad0fb5bc57a
SUNsung <weatherius@gmail.com>
2019-05-03 13:45:52 +0300

    Rewrite [16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb] File in: 2019-05-03 13:45:52

--- ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
+++ ruby/16dM5HGAryHVk1627guyzML8Bo16QmK1IQ4CtSs16t7Y.rb
@@ -1,190 +1,194 @@
 
         
-                expect(result).to eq('git tag -am #{message.shellescape} --force #{tag.shellescape}')
-      end
+                  add_default_name_and_id_for_value(tag_value, name_and_id)
+          options.delete('index')
+          options.delete('namespace')
+          options['for'] = name_and_id['id'] unless options.key?('for')
     
-          it 'adds clean_output param to command' do
-        result = Fastlane::FastFile.new.parse('lane :test do
-          appledoc(
-            project_name: 'Project Name',
-            project_company: 'Company',
-            input: 'input/dir',
-            clean_output: true
-          )
-        end').runner.execute(:test)
-    
-          it 'yields command output' do
-        expect_command('ls', '-la', exitstatus: 1, output: 'Heeeelp! Something went wrong.')
-        Fastlane::Actions.sh('ls', '-la') do |status, result|
-          expect(status.exitstatus).to eq(1)
-          expect(result).to eq('Heeeelp! Something went wrong.')
-        end
-      end
-    
-            keychain = 'keychain with spaces.keychain'
-        cmd = %r{curl -f -o (([A-Z]\:)?\/.+) https://developer\.apple\.com/certificationauthority/AppleWWDRCA.cer && security import \1 -k #{Regexp.escape(keychain.shellescape)}}
-        require 'open3'
-    
-              @registered.each do |plugin|
-            configs = plugin.data[:config_upgrade_safe]
-            if configs
-              configs.each do |key|
-                result[key] = plugin.config.get(key)
-              end
-            end
-          end
-    
-      def show
-    @status = status_finder.status
-    render json: @status, serializer: OEmbedSerializer, width: maxwidth_or_default, height: maxheight_or_default
-  end
-    
-      def account_from_topic
-    if hub_topic.present? && local_domain? && account_feed_path?
-      Account.find_local(hub_topic_params[:username])
+        def insert_after(index, *names)
+      insert assert_index(index) + 1, *names
     end
+    
+          unless root?
+        raise Invalid, 'missing name' if !name || name.empty?
+        raise Invalid, 'missing path' if !path || path.empty?
+        raise Invalid, 'missing type' if !type || type.empty?
+      end
+    end
+    
+            css('h1:not(:first-child)').each do |node|
+          node.name = 'h2'
+        end unless at_css('h2')
+    
+            name.prepend '#{breadcrumbs.join('.')}#' if breadcrumbs.present? && breadcrumbs[0] != name
+        name << '()' if %w(Function Method Constructor).include?(subtitle)
+        name
+      end
+    
+    $x = [1, 2, 3, 4]
+$y = []
+    
+            raydir = Vec.new(rx, ry, rz)
+        ray = Ray.new(p0, raydir)
+    
+        reversible(&method(:up_down))
   end
     
-      def process_salmon
-    SalmonWorker.perform_async(@account.id, payload.force_encoding('UTF-8'))
+        it 'generates the contacts_json fixture', :fixture => true do
+      json = bob.contacts.map { |c|
+               ContactPresenter.new(c, bob).full_hash_with_person
+             }.to_json
+      save_fixture(json, 'contacts_json')
+    end
   end
 end
 
     
-          weeks << {
-        week: week.to_time.to_i.to_s,
-        statuses: Redis.current.get('activity:statuses:local:#{week_id}') || '0',
-        logins: Redis.current.pfcount('activity:logins:#{week_id}').to_s,
-        registrations: Redis.current.get('activity:accounts:local:#{week_id}') || '0',
-      }
+      describe '#destroy' do
+    before do
+      sign_in @user, scope: :user
+    end
+    it 'redirects to / for a non-mobile user' do
+      delete :destroy
+      expect(response).to redirect_to new_user_session_path
     end
     
-    module RateLimitHeaders
-  extend ActiveSupport::Concern
+    Then(/^directories referenced in :linked_files are created in shared$/) do
+  dirs = TestApp.linked_files.map { |path| TestApp.shared_path.join(path).dirname }
+  dirs.each do |dir|
+    run_vagrant_command(test_dir_exists(dir))
+  end
+end
     
-      def ambient_occlusion(isect)
-    basis = Array.new
-    otherBasis(basis, isect.n)
+      def test_file_exists(path)
+    exists?('f', path)
+  end
     
-        def puts(*args)
-      STDERR.puts *args unless @silence
-    end
+        def load_imports
+      if options.show_tasks && Rake::Task.task_defined?('load:defaults')
+        invoke 'load:defaults'
+        set(:stage, '')
+        Dir[deploy_config_path].each { |f| add_import f }
+      end
     
-      def setup
-    tmp_dir = File.join GEM_PATH, 'tmp/node-mincer'
-    success = Dir.chdir DUMMY_PATH do
-      silence_stdout_if !ENV['VERBOSE'] do
-        system 'node', 'manifest.js', tmp_dir
+          # rubocop:disable Security/MarshalLoad
+      def add_role(role, hosts, options={})
+        options_deepcopy = Marshal.dump(options.merge(roles: role))
+        Array(hosts).each { |host| add_host(host, Marshal.load(options_deepcopy)) }
+      end
+      # rubocop:enable Security/MarshalLoad
+    
+    ::Bundler.with_friendly_errors do
+  ::Bundler::CLI.start(ARGV, :debug => true)
+end
+
+    
+        def self.find_by_name_with_wildcards(pattern)
+      re = transform_pattern_into_re(pattern)
+      ::Gem::Specification.find_all.select do |specification|
+        specification.name =~ re
       end
     end
-    assert success, 'Node.js Mincer compilation failed'
-    manifest = JSON.parse(File.read('#{tmp_dir}/manifest.json'))
-    css_name = manifest['assets']['application.css']
-    @css = File.read('#{tmp_dir}/#{css_name}')
+    
+        puts('Generated at #{target_file}')
+  end
+    
+            PluginManager.ui.debug('Looking if package named: #{plugin_name} exists at #{uri}')
+    
+        private
+    def uncompress(source)
+      temporary_directory = Stud::Temporary.pathname
+      LogStash::Util::Zip.extract(source, temporary_directory, LOGSTASH_PATTERN_RE)
+      temporary_directory
+    rescue Zip::Error => e
+      # OK Zip's handling of file is bit weird, if the file exist but is not a valid zip, it will raise
+      # a `Zip::Error` exception with a file not found message...
+      raise InvalidPackError, 'Cannot uncompress the zip: #{source}'
+    end
+    
+      # create list of plugins to update
+  def plugins_to_update(previous_gem_specs_map)
+    if update_all?
+      previous_gem_specs_map.values.map{|spec| spec.name}
+    else
+      # If the plugins isn't available in the gemspec or in 
+      # the gemfile defined with a local path, we assume the plugins is not
+      # installed.
+      not_installed = plugins_arg.select{|plugin| !previous_gem_specs_map.has_key?(plugin.downcase) && !gemfile.find(plugin) }
+      signal_error('Plugin #{not_installed.join(', ')} is not installed so it cannot be updated, aborting') unless not_installed.empty?
+      plugins_arg
+    end
+  end
+    
+          it 'list the plugin with his version' do
+        result = logstash.run_command_in_path('bin/logstash-plugin list --verbose #{plugin_name}')
+        expect(result).to run_successfully_and_output(/^#{plugin_name} \(\d+\.\d+.\d+\)/)
+      end
+    end
   end
 end
 
     
-    Then(/^the releases path is created$/) do
-  run_vagrant_command(test_dir_exists(TestApp.releases_path))
-end
-    
-    Given(/^file '(.*?)' does not exist in shared path$/) do |file|
-  file_shared_path = TestApp.shared_path.join(file)
-  run_vagrant_command('mkdir -p #{TestApp.shared_path}')
-  run_vagrant_command('touch #{file_shared_path} && rm #{file_shared_path}')
-end
-    
-          def roles_for(names)
-        options = extract_options(names)
-        s = Filter.new(:role, names).filter(servers_by_key.values)
-        s.select { |server| server.select?(options) }
+        context 'update all the plugins' do
+      it 'has executed successfully' do
+        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
+        expect(logstash).to have_installed?(plugin_name, '0.1.1')
       end
+    end
+  end
+end
+
     
-          # Register a validation rule for the given key.
-      def validate(key, &validator)
-        vs = (validators[key] || [])
-        vs << validator
-        validators[key] = vs
+          def default_markup
+        Precious::App.settings.default_markup
       end
-    
-    set_if_empty :pty, false
-    
-    
-# The main fpm command entry point.
-class FPM::Command < Clamp::Command
-  include FPM::Util
-    
-        File.open(staging_path('+MANIFEST'), 'w+') do |file|
-      file.write(pkgdata.to_json + '\n')
     end
+  end
+end
+
     
-      def self.default_prefix
-    npm_prefix = safesystemout('npm', 'prefix', '-g').chomp
-    if npm_prefix.count('\n') > 0
-      raise FPM::InvalidPackageConfiguration, '`npm prefix -g` returned unexpected output.'
-    elsif !File.directory?(npm_prefix)
-      raise FPM::InvalidPackageConfiguration, '`npm prefix -g` returned a non-existent directory'
-    end
-    logger.info('Setting default npm install prefix', :prefix => npm_prefix)
-    npm_prefix
+          attr_reader :page, :content
+    
+      # Here's the dir structure of lotr.git
+  #
+  # .
+  # ├── Bilbo-Baggins.md
+  # ├── Data.csv
+  # |-- Data-Two.csv -> Data.csv
+  # ├── Gondor
+  # │   ├── Boromir.md
+  # │   ├── _Footer.md
+  # │   ├── _Header.md
+  # │   └── _Sidebar.md
+  # |-- Hobbit.md -> Bilbo-Baggins.md
+  # ├── Home.textile
+  # ├── Mordor
+  # │   ├── Eye-Of-Sauron.md
+  # │   ├── _Footer.md
+  # │   ├── _Header.md
+  # │   ├── _Sidebar.md
+  # │   ├── eye.jpg
+  # │   └── todo.txt
+  # ├── My-Precious.md
+  # ├── Samwise\ Gamgee.mediawiki
+  # ├── _Footer.md
+  # ├── _Header.md
+  # └── _Sidebar.md
+  #
+    
+    context 'Precious::Views::Page' do
+  setup do
+    examples = testpath 'examples'
+    @path    = File.join(examples, 'test.git')
+    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
+    @wiki = Gollum::Wiki.new(@path)
   end
     
-      # Returns path of a processed template PackageInfo given to 'pkgbuild --info'
-  # note: '--info' is undocumented:
-  # http://managingosx.wordpress.com/2012/07/05/stupid-tricks-with-pkgbuild
-  def pkginfo_template_path
-    pkginfo_template = Tempfile.open('fpm-PackageInfo')
-    pkginfo_data = template('osxpkg.erb').result(binding)
-    pkginfo_template.write(pkginfo_data)
-    pkginfo_template.close
-    pkginfo_template.path
-  end # def write_pkginfo_template
-    
-        # Create a temporary config file
-    logger.debug('Creating pear config file')
-    config = File.expand_path(build_path('pear.config'))
-    installroot = attributes[:prefix] || '/usr/share'
-    safesystem('pear', 'config-create', staging_path(installroot), config)
-    
-        args = [ '-B', build_path('build-info'), '-c', build_path('comment'), '-d', build_path('description'), '-f', build_path('packlist'), '-I', '/opt/local', '-p', staging_path,  '-U', '#{cwd}/#{name}-#{self.version}-#{iteration}.tgz' ]
-    safesystem('pkg_create', *args)
-    
-      private
-  def input(command)
-    platforms = [
-      ::PleaseRun::Platform::Systemd.new('default'), # RHEL 7, Fedora 19+, Debian 8, Ubuntu 16.04
-      ::PleaseRun::Platform::Upstart.new('1.5'), # Recent Ubuntus
-      ::PleaseRun::Platform::Upstart.new('0.6.5'), # CentOS 6
-      ::PleaseRun::Platform::Launchd.new('10.9'), # OS X
-      ::PleaseRun::Platform::SYSV.new('lsb-3.1') # Ancient stuff
-    ]
-    pleaserun_attributes = [ 'chdir', 'user', 'group', 'umask', 'chroot', 'nice', 'limit_coredump',
-                             'limit_cputime', 'limit_data', 'limit_file_size', 'limit_locked_memory',
-                             'limit_open_files', 'limit_user_processes', 'limit_physical_memory', 'limit_stack_size',
-                             'log_directory', 'log_file_stderr', 'log_file_stdout']
-    
-    
-    
-      def initialize(package_name, opts = {}, &block)
-    @options = OpenStruct.new(:name => package_name.to_s)
-    @source, @target = opts.values_at(:source, :target).map(&:to_s)
-    @directory = File.expand_path(opts[:directory].to_s)
-    
-      # Raised if a safesystem program exits nonzero
-  class ProcessFailed < StandardError; end
-    
-    Given /^I update my user view to include the attachment$/ do
-  steps %{
-    Given I overwrite 'app/views/users/show.html.erb' with:
-      '''
-      <p>Name: <%= @user.name %></p>
-      <p>Attachment: <%= image_tag @user.attachment.url %></p>
-      '''
-  }
+    def utf8(str)
+  str.respond_to?(:force_encoding) ? str.force_encoding('utf-8') : str
 end
     
-    module RailsCommandHelpers
-  def framework_version?(version_string)
-    framework_version =~ /^#{version_string}/
-  end
+          # File.basename is too eager to please and will return the last
+      # component of the path even if it ends with a directory separator.
+      ::File.basename(file_path)
+    end
