92b3ae7068bd5aae0347ab171e800c566f600cd9
SUNsung <weatherius@gmail.com>
2019-05-03 10:24:35 +0300

    Rewrite [20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb] File in: 2019-05-03 10:24:34

--- ruby/20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb
+++ ruby/20zhBpI5yonk20GGloQuxhaq.20qVFI5PSZr5o20umM.rb
@@ -1,81 +1,109 @@
 
         
-        # Use an OS X pkg built with pkgbuild.
-#
-# Supports input and output. Requires pkgbuild and (for input) pkgutil, part of a
-# standard OS X install in 10.7 and higher.
-class FPM::Package::OSXpkg < FPM::Package
+                class RadioButtonBuilder < Builder # :nodoc:
+          def radio_button(extra_html_options = {})
+            html_options = extra_html_options.merge(@input_html_options)
+            html_options[:skip_default_ids] = false
+            @template_object.radio_button(@object_name, @method_name, @value, html_options)
+          end
+        end
     
-        # do channel-discover if required
-    if !attributes[:pear_channel].nil?
-      logger.info('Custom channel specified', :channel => attributes[:pear_channel])
-      channel_list = safesystemout('pear', '-c', config, 'list-channels')
-      if channel_list !~ /#{Regexp.quote(attributes[:pear_channel])}/
-        logger.info('Discovering new channel', :channel => attributes[:pear_channel])
-        safesystem('pear', '-c', config, 'channel-discover', attributes[:pear_channel])
+          cattr_accessor :type_klass
+    
+          def test_helpers_with_block
+        @controller.process(:with_block)
+        assert_equal 'Hello World', @controller.response_body
       end
-      input_package = '#{attributes[:pear_channel]}/#{input_package}'
-      logger.info('Prefixing package name with channel', :package => input_package)
+    
+          # A single page of data and the corresponding page number.
+      Page = Struct.new(:objects, :number)
+    
+                rows << {
+              label_id: label_id,
+              target_id: target_id,
+              target_type: issue.issuable_type,
+              created_at: time,
+              updated_at: time
+            }
+          end
+    
+          # Returns the database ID for the object.
+      #
+      # This method will return `nil` if no ID could be found.
+      def database_id
+        val = Caching.read(cache_key)
+    
+            # attributes - A Hash containing the raw note details. The keys of this
+        #              Hash must be Symbols.
+        def initialize(attributes)
+          @attributes = attributes
+        end
+    
+            def labels?
+          label_names && label_names.any?
+        end
+    
+            # Builds a user from a GitHub API response.
+        #
+        # user - An instance of `Sawyer::Resource` containing the user details.
+        def self.from_api_response(user)
+          new(id: user.id, login: user.login)
+        end
+    
+        def to_json
+      JSON.generate(as_json)
     end
     
-        platforms.each do |platform|
-      logger.info('Generating service manifest.', :platform => platform.class.name)
-      platform.program = command.first
-      platform.name = attributes[:pleaserun_name]
-      platform.args = command[1..-1]
-      platform.description = if attributes[:description_given?]
-        attributes[:description]
-      else
-        platform.name
-      end
-      pleaserun_attributes.each do |attribute_name|
-        attribute = 'pleaserun_#{attribute_name}'.to_sym
-        if attributes.has_key?(attribute) and not attributes[attribute].nil?
-          platform.send('#{attribute_name}=', attributes[attribute])
+        def at_css(*args)
+      doc.at_css(*args)
+    end
+    
+        def parse_as_document
+      document = Nokogiri::HTML.parse @content, nil, 'UTF-8'
+      @title = document.at_css('title').try(:content)
+      document
+    end
+    
+            css('ul.methods', 'ul.properties', 'ul.events').add_class('defs').each do |node|
+          node.css('> li > h3').each do |h3|
+            next if h3.content.present?
+            h3.content = h3.next_element.content
+            h3.next_element.remove
+          end
         end
       end
-    
-      def build!(params)
-    # TODO(sissel): Support these somehow, perhaps with execs and files.
-    self.scripts.each do |name, path|
-      case name
-        when 'pre-install'
-        when 'post-install'
-        when 'pre-uninstall'
-        when 'post-uninstall'
-      end # case name
-    end # self.scripts.each
-    
-      def initialize(package_name, opts = {}, &block)
-    @options = OpenStruct.new(:name => package_name.to_s)
-    @source, @target = opts.values_at(:source, :target).map(&:to_s)
-    @directory = File.expand_path(opts[:directory].to_s)
-    
-      def is_pane
-    @actual.is_a? Tmuxinator::Pane
+    end
   end
 end
 
     
-          # Pathname of given project searching only global directories
-      def global_project(name)
-        project_in(environment, name) ||
-          project_in(xdg, name) ||
-          project_in(home, name)
+        def dependencies
+      @dependencies ||= [Cask::CaskLoader.load('adobe-air')]
+    end
+    
+        def role(name, hosts, options={})
+      if name == :all
+        raise ArgumentError, '#{name} reserved name for role. Please choose another name'
       end
     
-      describe '#synchronize_before?' do
-    subject { window.synchronize_before? }
-    
-            Kernel.exec(project.render)
+          locations = Array.new
+      while (data.code.to_i == 301 || data.code.to_i == 302)
+        data = handle_gist_redirecting(data)
+        break if locations.include? data.header['Location']
+        locations << data.header['Location']
       end
     
-    Gem::Specification.new do |s|
-  s.name          = 'tmuxinator'
-  s.version       = Tmuxinator::VERSION
-  s.authors       = ['Allen Bargi', 'Christopher Chow']
-  s.email         = ['allen.bargi@gmail.com', 'chris@chowie.net']
-  s.description   = %q{Create and manage complex tmux sessions easily.}
-  s.summary       = %q{Create and manage complex tmux sessions easily.}
-  s.homepage      = 'https://github.com/tmuxinator/tmuxinator'
-  s.license       = 'MIT'
+    # Now, add our init-scripts, systemd services, and so on:
+pleaserun = package.convert(FPM::Package::PleaseRun)
+pleaserun.input ['/usr/bin/my-executable', '--foo-from', 'bar']
+    
+    class FPM::Package::Puppet < FPM::Package
+  def architecture
+    case @architecture
+    when nil, 'native'
+      @architecture = %x{uname -m}.chomp
+    end
+    return @architecture
+  end # def architecture
+    
+        logger.debug('Running command', :args => args2)
