360633ebd3674971f46761838dbdf61b1555e9a2
SUNsung <weatherius@gmail.com>
2019-05-04 11:26:28 +0300

    Rewrite [50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py] File in: 2019-05-04 11:26:28

--- pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
+++ pyton/50dMa6CDzw9WA50HT1139L3Lfo50u1yImPn1JdI50OLZ.py
@@ -1,137 +1,146 @@
 
         
-        
-class CallCenter(object):
-    
-        def approve_friend_request(self, friend_id):
-        pass
-    
-        proc.sendline(u'fuck')
+            proc.sendline(u'fuck')
     assert proc.expect([TIMEOUT, u'echo test'])
-    assert proc.expect([TIMEOUT, u'test'])
-    
-    no_match_output = '''
-Listing... Done
-'''
+    assert proc.expect([TIMEOUT, u'enter'])
+    assert proc.expect_exact([TIMEOUT, u'ctrl+c'])
+    proc.send('\003')
     
     
-no_such_subcommand_old = '''No such subcommand
+@pytest.mark.parametrize('command, new_command, packages', [
+    (Command('vim', ''), 'sudo apt-get install vim && vim',
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('convert', ''), 'sudo apt-get install imagemagick && convert',
+     [('imagemagick', 'main'),
+      ('graphicsmagick-imagemagick-compat', 'universe')]),
+    (Command('sudo vim', ''), 'sudo apt-get install vim && sudo vim',
+     [('vim', 'main'), ('vim-tiny', 'main')]),
+    (Command('sudo convert', ''), 'sudo apt-get install imagemagick && sudo convert',
+     [('imagemagick', 'main'),
+      ('graphicsmagick-imagemagick-compat', 'universe')])])
+def test_get_new_command(mocker, command, new_command, packages):
+    mocker.patch('thefuck.rules.apt_get._get_packages',
+                 create=True, return_value=packages)
     
     
-@parametrize_extensions
-@parametrize_filename
-@parametrize_script
-def test_get_new_command(ext, tar_error, filename, unquoted, quoted, script, fixed):
-    tar_error(unquoted.format(ext))
-    assert (get_new_command(Command(script.format(filename.format(ext)), ''))
-            == fixed.format(dir=quoted.format(''), filename=filename.format(ext)))
+@pytest.mark.parametrize('command, result', [
+    (Command('aws dynamdb scan', misspelled_command),
+     ['aws dynamodb scan']),
+    (Command('aws dynamodb scn', misspelled_subcommand),
+     ['aws dynamodb scan']),
+    (Command('aws dynamodb t-item',
+             misspelled_subcommand_with_multiple_options),
+     ['aws dynamodb put-item', 'aws dynamodb get-item'])])
+def test_get_new_command(command, result):
+    assert get_new_command(command) == result
 
     
     
-def test_objective_shapes_2d():
-    y_a = K.variable(np.random.random((6, 7)))
-    y_b = K.variable(np.random.random((6, 7)))
-    for obj in allobj:
-        objective_output = obj(y_a, y_b)
-        assert K.eval(objective_output).shape == (6,)
-    
-    print('Evaluate IRNN...')
-model = Sequential()
-model.add(SimpleRNN(hidden_units,
-                    kernel_initializer=initializers.RandomNormal(stddev=0.001),
-                    recurrent_initializer=initializers.Identity(gain=1.0),
-                    activation='relu',
-                    input_shape=x_train.shape[1:]))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
-rmsprop = RMSprop(lr=learning_rate)
-model.compile(loss='categorical_crossentropy',
-              optimizer=rmsprop,
-              metrics=['accuracy'])
-    
-    print('Building model...')
-model = Sequential()
-model.add(Dense(512, input_shape=(max_words,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes))
-model.add(Activation('softmax'))
-    
-        # Raises
-        ValueError if unknown identifier.
-    '''
-    if identifier is None:
-        return None
-    if isinstance(identifier, six.string_types):
-        identifier = str(identifier)
-        return deserialize(identifier)
-    if isinstance(identifier, dict):
-        return deserialize(identifier)
-    elif callable(identifier):
-        return identifier
-    else:
-        raise ValueError('Could not interpret '
-                         'loss function identifier:', identifier)
+@pytest.mark.parametrize('before, after', [
+    ('brew install sshfs',
+     'brew cask install osxfuse && brew install sshfs')])
+def test_get_new_command(before, after):
+    command = Command(before, output)
+    assert get_new_command(command) == after
 
     
-            # 更新聚类中心
-        log.info(centers)
-        for i in range(k):
-            data_i = data[ret[:, 0] == i]  # 标签为 i 的样本
-            centers[i, :] = np.mean(data_i, axis=0)  # 按类别过滤样本
+    
+@pytest.mark.skipif(_is_not_okay_to_test(),
+                    reason='No need to run if there\'s no formula')
+def test_get_new_command(brew_no_available_formula):
+    assert get_new_command(Command('brew install elsticsearch',
+                                   brew_no_available_formula))\
+        == 'brew install elasticsearch'
+    
+            full_path = request.get_full_path()
+        current_site = get_current_site(request)
+    
+        n_features = 10
+    list_n_samples = np.linspace(100, 1000000, 5).astype(np.int)
+    lasso_results, lars_lasso_results = compute_bench(alpha, list_n_samples,
+                                            [n_features], precompute=True)
+    
+    plt.figure('scikit-learn Ward's method benchmark results')
+plt.imshow(np.log(ratio), aspect='auto', origin='lower')
+plt.colorbar()
+plt.contour(ratio, levels=[1, ], colors='k')
+plt.yticks(range(len(n_features)), n_features.astype(np.int))
+plt.ylabel('N features')
+plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
+plt.xlabel('N samples')
+plt.title('Scikit's time, in units of scipy time (log)')
+plt.show()
+
+    
+    $ kernprof.py -l sparsity_benchmark.py
+$ python -m line_profiler sparsity_benchmark.py.lprof
+    
+        print('============================================')
+    print('Warning: this is going to take a looong time')
+    print('============================================')
+    
+    import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import GridSearchCV
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+    
+    # The digits dataset
+digits = datasets.load_digits()
+    
+    plt.subplot(3, 4, 10)
+plt.imshow(np.reshape(agglo.labels_, images[0].shape),
+           interpolation='nearest', cmap=plt.cm.nipy_spectral)
+plt.xticks(())
+plt.yticks(())
+plt.title('Labels')
+plt.show()
+
+    
+        # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <= distance_threshold for i in range(len(X_face_locations))]
+    
+    # Find all the faces in the image using a pre-trained convolutional neural network.
+# This method is more accurate than the default HOG model, but it's slower
+# unless you have an nvidia GPU and dlib compiled with CUDA extensions. But if you do,
+# this will use GPU acceleration and perform well.
+# See also: find_faces_in_picture.py
+face_locations = face_recognition.face_locations(image, number_of_times_to_upsample=0, model='cnn')
+    
+            if file.filename == '':
+            return redirect(request.url)
+    
+        # 将每一个人脸与已知样本图片比对
+    for face_encoding in face_encodings:
+        # 看是否属于奥巴马或者拜登
+        match = face_recognition.compare_faces([obama_face_encoding], face_encoding)
+        name = '<Unknown Person>'
     
     
-# TODO(huay): char_cnn_embedding
-def char_cnn_embedding(x, c_embed_size=8, share_cnn_weights=True, name='char_cnn_embedding', reuse=None):
-    '''
-    In:  [N, max_n_word, max_n_char]
-    Out: [N, max_n_word, c_embed_size]
-    
-            H = act_fn(tf.nn.conv2d(x, W, strides=strides, padding=padding) + b)
-        T = sigmoid(tf.nn.conv2d(x, W_T, strides=strides, padding=padding) + b_T)
-        o = tf.multiply(H, T) + tf.multiply(x, (1. - T))
-    return o
-    
-        Args:
-        h: context encoding     shape: [N, T, d]
-        u: question encoding    shape: [N, J, d]
-        T(int): context length
-        J(int): question length
-        d(int): features size
-        name(str):
-        reuse(bool):
-    
-    def clean_pdf_link(link):
-    if 'arxiv' in link:
-        link = link.replace('abs', 'pdf')   
-        if not(link.endswith('.pdf')):
-            link = '.'.join((link, 'pdf'))
-    
-            return -1, char_len
+# ---------------
+# HELPER METHODS
+# ---------------
     
     
-class Latin1Prober(CharSetProber):
-    def __init__(self):
-        super(Latin1Prober, self).__init__()
-        self._last_char_class = None
-        self._freq_counter = None
-        self.reset()
+def setUp():
+    document = {
+        'first_name': 'Jane',
+        'last_name': 'Smith',
+        'age': 32,
+        'about': 'I like to collect rock albums',
+        'interests': ['music']
+    }
+    resp = add_document(TEST_DOC_ID, document)
+    assert_equal(201, resp.status_code,
+        msg='Request failed({}): {}'.format(resp.status_code, resp.text))
     
-        def __init__(self, lang_filter=None):
-        super(MultiByteCharSetProber, self).__init__(lang_filter=lang_filter)
-        self.distribution_analyzer = None
-        self.coding_sm = None
-        self._last_char = [0, 0]
+        def test_path_matches(self):
+        path, details = apigateway_listener.get_resource_for_path('/foo/bar', {'/foo/{param1}': {}})
+        self.assertEqual(path, '/foo/{param1}')
     
-        @property
-    def charset_name(self):
-        if self._name_prober:
-            return self._name_prober.charset_name
-        else:
-            return self._model['charset_name']
-    
-            # Default to ASCII if it is all we've seen so far
-        elif self._input_state == InputState.PURE_ASCII:
-            self.result = {'encoding': 'ascii',
-                           'confidence': 1.0,
-                           'language': ''}
+    import re
+from setuptools import find_packages, setup
