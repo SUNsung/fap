c5fbe01a4444cb841ff068503c4ba0b1c079e632
SUNsung <weatherius@gmail.com>
2019-05-04 17:31:01 +0300

    Rewrite [37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py] File in: 2019-05-04 17:31:00

--- pyton/37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py
+++ pyton/37sMcpLMBHFY37jeqNofnTuys372SHlnh9gRFU371oN.py
@@ -1,191 +1,47 @@
 
         
-        import keras
-from keras.callbacks import TensorBoard
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+        # -- Options for LaTeX output ---------------------------------------------
+    
+        def test_eq(self):
+        self.assertTrue(self.vhost1b == self.vhost1)
+        self.assertFalse(self.vhost1 == self.vhost2)
+        self.assertEqual(str(self.vhost1b), str(self.vhost1))
+        self.assertFalse(self.vhost1b == 1234)
+    
+    ==========================================  ===================================
+``--dns-digitalocean-credentials``          DigitalOcean credentials_ INI file.
+                                            (Required)
+``--dns-digitalocean-propagation-seconds``  The number of seconds to wait for
+                                            DNS to propagate before asking the
+                                            ACME server to verify the DNS
+                                            record.
+                                            (Default: 10)
+==========================================  ===================================
     
     
-# Aliases.
+if __name__ == '__main__':
+    ''''''
+    # _test()
+    
+        def __init__(self, n_unit, act_fn=relu, name=None):
+        ''''''
+        self.n_unit = n_unit
+        self.act_fn = act_fn
+        self.name = name
     
     
-def test_cifar():
-    # only run data download tests 20% of the time
-    # to speed up frequent testing
-    random.seed(time.time())
-    if random.random() > 0.8:
-        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
-        (x_train, y_train), (x_test, y_test) = cifar100.load_data('fine')
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
-        (x_train, y_train), (x_test, y_test) = cifar100.load_data('coarse')
-        assert len(x_train) == len(y_train) == 50000
-        assert len(x_test) == len(y_test) == 10000
     
-            if self.data_format == 'channels_first':
-            channel_axis = 1
-        else:
-            channel_axis = -1
-        if input_shape[channel_axis] is None:
-            raise ValueError('The channel dimension of the inputs '
-                             'should be defined. Found `None`.')
-        input_dim = input_shape[channel_axis]
-        kernel_shape = self.kernel_size + (input_dim, self.filters * 4)
-        self.kernel_shape = kernel_shape
-        recurrent_kernel_shape = self.kernel_size + (self.filters, self.filters * 4)
-    
-    history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
-                    epochs=epochs,
-                    verbose=1,
-                    validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-    
-    plt.plot(range(epochs),
-         history_model1.history['val_loss'],
-         'g-',
-         label='Network 1 Val Loss')
-plt.plot(range(epochs),
-         history_model2.history['val_loss'],
-         'r-',
-         label='Network 2 Val Loss')
-plt.plot(range(epochs),
-         history_model1.history['loss'],
-         'g--',
-         label='Network 1 Loss')
-plt.plot(range(epochs),
-         history_model2.history['loss'],
-         'r--',
-         label='Network 2 Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.savefig('comparison_of_networks.png')
-
-    
-    
-def makeKeyFiles(name, keySize):
-    if os.path.exists('%s_pubkey.txt' % name) or os.path.exists('%s_privkey.txt' % name):
-        print('\nWARNING:')
-        print(''%s_pubkey.txt' or '%s_privkey.txt' already exists. \n'
-              'Use a different name or delete these files and re-run this program.' %
-              (name, name))
-        sys.exit()
-    
-    from .hash_table import HashTable
-from number_theory.prime_numbers import next_prime, check_prime
-    
-        def __solveDP(self, x, y):
-        if (x==-1):
-            return y+1
-        elif (y==-1):
-            return x+1
-        elif (self.dp[x][y]>-1):
-            return self.dp[x][y]
-        else:
-            if (self.A[x]==self.B[y]):
-                self.dp[x][y] = self.__solveDP(x-1,y-1)
-            else:
-                self.dp[x][y] = 1+min(self.__solveDP(x,y-1), self.__solveDP(x-1,y), self.__solveDP(x-1,y-1))
-    
+        References:
+        K.repeat()
+        tf.tile()
     '''
-The number of partitions of a number n into at least k parts equals the number of partitions into exactly k parts
-plus the number of partitions into at least k-1 parts. Subtracting 1 from each part of a partition of n into k parts
-gives a partition of n-k into k parts. These two facts together are used for this algorithm.
+    assert x.get_shape().ndims == 2
+    x = tf.expand_dims(x, axis=1)  # -> [batch_size, 1, n_input]
+    return tf.tile(x, [1, n, 1])  # -> [batch_size, n, n_input]
+    
+    print(word_vec)
+print(model.wv['aam'])
 '''
-def partition(m):
-	memo = [[0 for _ in xrange(m)] for _ in xrange(m+1)]
-	for i in xrange(m+1):
-		memo[i][0] = 1
-    
-        for i in range(1, s+1):
-        dp[0][i] = False
-    
-    	startLength = len(bitString)
-	bitString += '1'
-	while len(bitString) % 512 != 448:
-		bitString += '0'
-	lastPart = format(startLength,'064b')
-	bitString += rearrange(lastPart[32:]) + rearrange(lastPart[:32]) 
-	return bitString
-    
-        def mean_squared_error(self, labels, prediction):
-        '''
-        mean_squared_error:
-        @param labels: a one dimensional numpy array 
-        @param prediction: a floating point value
-        return value: mean_squared_error calculates the error if prediction is used to estimate the labels
-        '''
-        if labels.ndim != 1:
-            print('Error: Input labels must be one dimensional')
-    
-    
-  def Start( self ):
-    request_data = BuildRequestData()
-    request_data.update( { 'filetypes': self.filetypes } )
-    self._response = self.PostDataToHandler( request_data,
-                                             'semantic_completion_available' )
-    
-    
-class FakeResponse( object ):
-  '''A fake version of a requests response object, just about suitable for
-  mocking a server response. Not usually used directly. See
-  MockServerResponse* methods'''
-  def __init__( self, response, exception ):
-    self._json = response
-    self._exception = exception
-    self.status_code = requests.codes.ok
-    self.text = not exception
-    
-        def submit(self, fn, *args, **kwargs):
-        with self._shutdown_lock:
-            if self._shutdown:
-                raise RuntimeError('cannot schedule new futures after shutdown')
-    
-    PRIMES = [
-    112272535095293,
-    112582705942171,
-    112272535095293,
-    115280095190773,
-    115797848077099,
-    117450548693743,
-    993960000099397]
-    
-    try:
-    import yaml
-except ImportError:
-    yaml = None
-    
-            if self.config.get('mask_type', None):
-            var_y = input_
-            var_y = self.blocks.upscale(var_y, 512)
-            var_y = self.blocks.upscale(var_y, 256)
-            var_y = self.blocks.upscale(var_y, self.input_shape[0])
-            var_y = Conv2D(1, kernel_size=5, padding='same', activation='sigmoid')(var_y)
-            outputs.append(var_y)
-        return KerasModel(input_, outputs=outputs)
-
-    
-    class ResetNeeded(Exception):
-    '''When the monitor is active, raised when the user tries to step an
-    environment that's already done.
-    '''
-    pass
-    
-        rollout = {}
-    rollout['observations'] = observations_hash
-    rollout['actions'] = actions_hash
-    rollout['rewards'] = rewards_hash
-    rollout['dones'] = dones_hash
-    
-    human_agent_action = 0
-human_wants_restart = False
-human_sets_pause = False
+[ 0.02210762 -0.10488641  0.05512805  0.09150169  0.00725085]
+[ 0.02210762 -0.10488641  0.05512805  0.09150169  0.00725085]
+'''
