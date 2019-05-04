afd87c1b47e72fbc0e252ea7973976b2471b75c4
SUNsung <weatherius@gmail.com>
2019-05-04 16:40:41 +0300

    Rewrite [55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp] File in: 2019-05-04 16:40:40

--- cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
+++ cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
@@ -1,30 +1,415 @@
 
         
-        namespace atom {
+        void DiverseStackBase::pushNewStorageSlow(std::size_t needed) {
+  bool wasInline = isAllocatedInline();
     }
     
-    void Event::SetSenderAndMessage(content::RenderFrameHost* sender,
-                                IPC::Message* message) {
-  DCHECK(!sender_);
-  DCHECK(!message_);
-  sender_ = sender;
-  message_ = message;
+        {
+      Out << Indent;
+      if (childKind == ChildKind::Root) {
+        Out << '+- ';
+      } else if (childKind == ChildKind::Left) {
+        Out << '/- ';
+      } else if (childKind == ChildKind::Right) {
+        Out << '\\- ';
+      } else if (childKind == ChildKind::Further) {
+        Out << '\\-> ';
+      }
+      PrintNodeData(Out, node);
+      Out << '\n';
     }
     
-      // content::WebContentsObserver implementations:
-  void RenderFrameDeleted(content::RenderFrameHost* rfh) override;
-  void RenderFrameHostChanged(content::RenderFrameHost* old_rfh,
-                              content::RenderFrameHost* new_rfh) override;
-  void FrameDeleted(content::RenderFrameHost* rfh) override;
     
-    #if defined(OS_LINUX)
-typedef PowerObserverLinux PowerObserver;
-#else
-typedef base::PowerObserver PowerObserver;
-#endif  // defined(OS_LINUX)
-    
-    OffscreenViewProxy::~OffscreenViewProxy() {
-  if (observer_) {
-    observer_->ProxyViewDestroyed(this);
+    {  bool isTypedef() const {
+    assert(isValid());
+    return !Decl.isNull() && Decl.is<const clang::TypedefNameDecl *>();
   }
+  const clang::TypedefNameDecl *getTypedef() const {
+    assert(isTypedef());
+    return Decl.get<const clang::TypedefNameDecl *>();
+  }
+};
+    
+    using namespace swift;
+using namespace Mangle;
+    
+    const char *Action::getClassName(Kind AC) {
+  switch (AC) {
+  case Kind::Input:  return 'input';
+  case Kind::CompileJob:  return 'compile';
+  case Kind::InterpretJob:  return 'interpret';
+  case Kind::BackendJob:  return 'backend';
+  case Kind::MergeModuleJob:  return 'merge-module';
+  case Kind::ModuleWrapJob:  return 'modulewrap';
+  case Kind::AutolinkExtractJob:  return 'swift-autolink-extract';
+  case Kind::REPLJob:  return 'repl';
+  case Kind::LinkJob:  return 'link';
+  case Kind::GenerateDSYMJob:  return 'generate-dSYM';
+  case Kind::VerifyDebugInfoJob:  return 'verify-debug-info';
+  case Kind::GeneratePCHJob:  return 'generate-pch';
+  }
+    }
+    
+    static bool B2_POLYGON_REPORT_ERRORS = false;
+    
+    namespace b2ConvexDecomp {
+    }
+    
+    			if(mip == 0)
+			{
+				pImageData = a_pafSourceRGBA;
+			}
+			else
+			{
+				pMipImage = new float[mipWidth*mipHeight*4];
+				if(FilterTwoPass(a_pafSourceRGBA, a_uiSourceWidth, a_uiSourceHeight, pMipImage, mipWidth, mipHeight, a_uiMipFilterFlags, Etc::FilterLanczos3) )
+				{
+					pImageData = pMipImage;
+				}
+			}
+    
+    		ColorFloatRGBA	m_afrgbaDecodedColors[PIXELS];	// decoded RGB components, ignore Alpha
+		float			m_afDecodedAlphas[PIXELS];		// decoded alpha component
+		float			m_fError;						// error for RGBA relative to m_pafrgbaSource
+    
+    #ifndef VPX_DSP_TXFM_COMMON_H_
+#define VPX_DSP_TXFM_COMMON_H_
+    
+      All rights reserved.
+    
+    /** 16x32 multiplication, followed by a 15-bit shift right. Results fits in 32 bits */
+#if OPUS_FAST_INT64
+#define MULT16_32_Q15(a,b) ((opus_val32)SHR((opus_int64)((opus_val16)(a))*(b),15))
+#else
+#define MULT16_32_Q15(a,b) ADD32(SHL(MULT16_16((a),SHR((b),16)),1), SHR(MULT16_16SU((a),((b)&0x0000ffff)),15))
+#endif
+    
+    /* For input in Q24 domain */
+opus_int32 silk_LPC_inverse_pred_gain_Q24(          /* O    Returns inverse prediction gain in energy domain, Q30       */
+    const opus_int32            *A_Q24,             /* I    Prediction coefficients [order]                             */
+    const opus_int              order               /* I    Prediction order                                            */
+);
+    
+    ::opencensus::stats::MeasureInt64 RpcClientSentMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientSentBytesPerRpc();
+::opencensus::stats::MeasureInt64 RpcClientReceivedMessagesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientReceivedBytesPerRpc();
+::opencensus::stats::MeasureDouble RpcClientRoundtripLatency();
+::opencensus::stats::MeasureDouble RpcClientServerLatency();
+::opencensus::stats::MeasureInt64 RpcClientCompletedRpcs();
+    
+      grpc_error* Init(grpc_call_element* elem,
+                   const grpc_call_element_args* args) override;
+    
+    #include <utility>
+    
+    const std::set<PerBalancerStore*>* PerHostStore::GetAssignedStores(
+    const grpc::string& lb_id) const {
+  auto it = assigned_stores_.find(lb_id);
+  if (it == assigned_stores_.end()) return nullptr;
+  return &(it->second);
 }
+    
+    namespace grpc {
+    }
+    
+            friend Rational operator<<(Rational lhs, Rational const& rhs);
+        friend Rational operator>>(Rational lhs, Rational const& rhs);
+    
+    private:
+    bool m_fPrecedence;
+    bool m_fIntegerMode; /* This is true if engine is explicitly called to be in integer mode. All bases are restricted to be in integers only */
+    ICalcDisplay* m_pCalcDisplay;
+    CalculationManager::IResourceProvider* const m_resourceProvider;
+    int m_nOpCode;     /* ID value of operation.                       */
+    int m_nPrevOpCode; // opcode which computed the number in m_currentVal. 0 if it is already bracketed or plain number or
+    // if it hasn't yet been computed
+    bool m_bChangeOp;              /* Flag for changing operation.       */
+    bool m_bRecord;                // Global mode: recording or displaying
+    bool m_bSetCalcState;          // Flag for setting the engine result state
+    CalcEngine::CalcInput m_input; // Global calc input object for decimal strings
+    eNUMOBJ_FMT m_nFE;             /* Scientific notation conversion flag.       */
+    CalcEngine::Rational m_maxTrigonometricNum;
+    std::unique_ptr<CalcEngine::Rational> m_memoryValue; // Current memory value.
+    
+    namespace HPHP { namespace HHBBC {
+    }
+    }
+    
+    /* Floating point operations */
+void Assembler::fadd(const RegXMM& frt, const RegXMM& fra, const RegXMM& frb,
+                     bool rc) {
+  EmitAForm(63, rn(frt), rn(fra), rn(frb), rn(0), 21, rc);
+}
+    
+      Object createObject() const;
+    
+      static void ParseIniFile(const std::string &filename,
+                           const bool is_system = true);
+  static void ParseIniFile(const std::string &filename, IniSettingMap &ini,
+                           const bool constants_only = false,
+                           const bool is_system = true);
+    
+    #endif // HPHP_GLOB_STREAM_WRAPPER_H
+
+    
+      explicit OutputFile(const String& filename);
+  virtual ~OutputFile();
+    
+    ///////////////////////////////////////////////////////////////////////////////
+    
+    
+        case URX_BACKSLASH_V:         // Any single code point line ending.
+            {
+                if (fp->fInputIdx >= fActiveLimit) {
+                    fHitEnd = TRUE;
+                    fp = (REStackFrame *)fStack->popFrame(fFrameSize);
+                    break;
+                }
+                UChar32 c;
+                U16_NEXT(inputBuf, fp->fInputIdx, fActiveLimit, c);
+                UBool success = isLineTerminator(c);
+                success ^= (UBool)(opValue != 0);        // flip sense for \V
+                if (!success) {
+                    fp = (REStackFrame *)fStack->popFrame(fFrameSize);
+                }
+            }
+            break;
+    
+    UnicodeString &ScientificNumberFormatter::MarkupStyle::format(
+        const UnicodeString &original,
+        FieldPositionIterator &fpi,
+        const UnicodeString &preExponent,
+        const DecimalFormatStaticSets & /*unusedDecimalFormatSets*/,
+        UnicodeString &appendTo,
+        UErrorCode &status) const {
+    if (U_FAILURE(status)) {
+        return appendTo;
+    }
+    FieldPosition fp;
+    int32_t copyFromOffset = 0;
+    while (fpi.next(fp)) {
+        switch (fp.getField()) {
+        case UNUM_EXPONENT_SYMBOL_FIELD:
+            appendTo.append(
+                    original,
+                    copyFromOffset,
+                    fp.getBeginIndex() - copyFromOffset);
+            copyFromOffset = fp.getEndIndex();
+            appendTo.append(preExponent);
+            appendTo.append(fBeginMarkup);
+            break;
+        case UNUM_EXPONENT_FIELD:
+            appendTo.append(
+                    original,
+                    copyFromOffset,
+                    fp.getEndIndex() - copyFromOffset);
+            copyFromOffset = fp.getEndIndex();
+            appendTo.append(fEndMarkup);
+            break;
+        default:
+            break;
+        }
+    }
+    appendTo.append(
+            original, copyFromOffset, original.length() - copyFromOffset);
+    return appendTo;
+}
+    
+    ScriptSet::~ScriptSet() {
+}
+    
+        UBool operator == (const ScriptSet &other) const;
+    ScriptSet & operator = (const ScriptSet &other);
+    
+    U_NAMESPACE_BEGIN
+    
+    class UnicodeSet;
+    
+    
+    {    BreakIterator *get() const { return ptr; }
+    BreakIterator *operator->() const { return ptr; }
+    BreakIterator &operator*() const { return *ptr; }
+private:
+    BreakIterator *ptr;
+    SharedBreakIterator(const SharedBreakIterator &);
+    SharedBreakIterator &operator=(const SharedBreakIterator &);
+};
+    
+    #include 'unicode/utypes.h'
+#include 'sharedobject.h'
+    
+    #if !UCONFIG_NO_FORMATTING
+    
+    class  UnicodeSet;
+    
+    
+  while (true)
+  {
+    xfer += iprot->readFieldBegin(fname, ftype, fid);
+    if (ftype == ::apache::thrift::protocol::T_STOP) {
+      break;
+    }
+    switch (fid)
+    {
+      case 1:
+        if (ftype == ::apache::thrift::protocol::T_I32) {
+          xfer += iprot->readI32(this->code);
+          this->__isset.code = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      case 2:
+        if (ftype == ::apache::thrift::protocol::T_STRING) {
+          xfer += iprot->readString(this->message);
+          this->__isset.message = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      case 3:
+        if (ftype == ::apache::thrift::protocol::T_I64) {
+          xfer += iprot->readI64(this->uuid);
+          this->__isset.uuid = true;
+        } else {
+          xfer += iprot->skip(ftype);
+        }
+        break;
+      default:
+        xfer += iprot->skip(ftype);
+        break;
+    }
+    xfer += iprot->readFieldEnd();
+  }
+    
+    #include <vector>
+    
+    #include <osquery/tests/integration/tables/helper.h>
+    
+    TEST_F(kernelIntegrity, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from kernel_integrity');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'sycall_addr_modified', IntType}
+  //      {'text_segment_hash', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
+    
+    TEST_F(KernelModules, test_sanity) {
+  QueryData data = execute_query('select * from kernel_modules');
+  ASSERT_GT(data.size(), 0ul);
+  ValidatatioMap row_map = {
+      {'name', NonEmptyString},
+      {'size', NonNegativeInt},
+      {'used_by', NonEmptyString},
+      {'status', NonEmptyString},
+      {'address', NonNegativeInt},
+  };
+  validate_rows(data, row_map);
+}
+    
+    TEST_F(keychainAcls, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from keychain_acls');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'keychain_path', NormalType}
+  //      {'authorizations', NormalType}
+  //      {'path', NormalType}
+  //      {'description', NormalType}
+  //      {'label', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
+}
+    
+        /** 
+     * Initializes the action with a duration and an array of points.
+     *
+     * @param duration In seconds.
+     * @param points An PointArray.
+     * @param tension Goodness of fit.
+     */
+    bool initWithDuration(float duration, PointArray* points, float tension);
+    /** It will update the target position and change the _previousPosition to newPos
+     *
+     * @param newPos The new position.
+     */
+    virtual void updatePosition(const Vec2 &newPos);
+    /** Return a PointArray.
+     *
+     * @return A PointArray.
+     */
+    PointArray* getPoints() { return _points; }
+    /**
+     * @js NA
+     * @lua NA
+     */
+    void setPoints(PointArray* points)
+    {
+        CC_SAFE_RETAIN(points);
+        CC_SAFE_RELEASE(_points);
+        _points = points;
+    }
+    
+    protected:
+    unsigned int _waves;
+    float _amplitude;
+    float _amplitudeRate;
+    
+    FlipX *FlipX::create(bool x)
+{
+    FlipX *ret = new (std::nothrow) FlipX();
+    }
+    
+    
+    {private:
+    CC_DISALLOW_COPY_AND_ASSIGN(Place);
+};
+    
+    
+    {private:
+    CC_DISALLOW_COPY_AND_ASSIGN(ProgressFromTo);
+};
+    
+    // implementation of ShuffleTiles
+    
+     Another example: ScaleTo action could be rewritten using PropertyAction:
+    
+    AtlasNode::AtlasNode()
+: _itemsPerRow(0)
+, _itemsPerColumn(0)
+, _itemWidth(0)
+, _itemHeight(0)
+, _textureAtlas(nullptr)
+, _isOpacityModifyRGB(false)
+, _quadsToDraw(0)
+, _uniformColor(0)
+, _ignoreContentScaleFactor(false)
+{
+}
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
