0595269d06230066f26be0391a290d12cd07f07c
SUNsung <weatherius@gmail.com>
2019-05-04 23:08:10 +0300

    Rewrite [36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py] File in: 2019-05-04 23:08:09

--- pyton/36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py
+++ pyton/36k8wUQo12Ih.36Eev1ZDXFexs36zgHR6KbaF7Y36XJb.py
@@ -1,78 +1,130 @@
 
         
-            entry_points = {'console_scripts': proj_info['console_scripts']}
-)
+        import sys
+import os
+import textwrap
+    
+        with io.open(outfile, 'w', encoding='utf-8') as outf:
+        outf.write(out)
+    
+    lazy_extractors_filename = sys.argv[1]
+if os.path.exists(lazy_extractors_filename):
+    os.remove(lazy_extractors_filename)
+    
+    import os
+from os.path import dirname as dirn
+import sys
+    
+        def test_module_exec(self):
+        if sys.version_info >= (2, 7):  # Python 2.6 doesn't support package execution
+            subprocess.check_call([sys.executable, '-m', 'youtube_dl', '--version'], cwd=rootDir, stdout=_DEV_NULL)
+    
+            If `sign` is incorrect, /validate call throws an HTTP 556 error
+        '''
+        logger = WarningLogger()
+        ie = IqiyiIEWithCredentials(FakeYDL({'logger': logger}))
+        ie._login()
+        self.assertTrue('unable to log in:' in logger.messages[0])
+    
+    
+@pytest.mark.functional
+def test_refuse_with_confirmation(proc, TIMEOUT):
+    refuse_with_confirmation(proc, TIMEOUT)
+    history_not_changed(proc, TIMEOUT)
+    
+    
+def test_get_new_command():
+    new_command = get_new_command(Command('apt list --upgradable', match_output))
+    assert new_command == 'apt upgrade'
+    
+        assert get_new_command(Command('brew install aa',
+                                   brew_no_available_formula))\
+        != 'brew install aha'
 
     
-    #----------------------------------------------------------------------
-def fc2video_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    '''wrapper'''
-    #'http://video.fc2.com/en/content/20151021bTVKnbEw'
-    #'http://xiaojiadianvideo.asia/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/ja/content/20151021bTVKnbEw'
-    #'http://video.fc2.com/tw/content/20151021bTVKnbEw'
-    hostname = urlparse(url).hostname
-    if not ('fc2.com' in hostname or 'xiaojiadianvideo.asia' in hostname):
-        return False
-    upid = match1(url, r'.+/content/(\w+)')
-    
-                print_info(site_info, title_i, ext, size)
-            if not info_only:
-                download_urls([real_url], title_i, ext, size, output_dir, merge = merge)
-    
-        m3u8_url = get_m3u8_url(stream_id)
-    
-        def assert_failure(self, res, code=None):
-        self.assertEqual(res.status, 200)
-        body = res.body
-        body = json.loads(body)
-        self.assertTrue('json' in body)
-        errors = body['json'].get('errors')
-        self.assertTrue(code in [x[0] for x in errors])
-        data = body['json'].get('data')
-        self.assertFalse(bool(data))
-    
-        def test_subdomain_not_in(self):
-        cfg = {'subdomains': []}
-        feature_state = self.world._make_state(cfg)
-        self.assertFalse(feature_state.is_enabled(subdomain='beta'))
-        self.assertFalse(feature_state.is_enabled(subdomain=''))
-    
-        def test_no_upgrade_oauth(self):
-        # When g.domain == g.oauth_domain we might send a cookie even though
-        # we're not using it for auth. Don't echo it back in responses.
-        c.oauth_user = self._account
-        self._setSessionCookie(days_old=60)
-        upgrade_cookie_security()
-        self.assertFalse(c.cookies[g.login_cookie].dirty)
-    
-        def test_submit_event(self):
-        self.patch_liveconfig('events_collector_submit_sample_rate', 1.0)
-        new_link = MagicMock(name='new_link')
-        context = MagicMock(name='context')
-        request = MagicMock(name='request')
-        request.ip = '1.2.3.4'
-        g.events.submit_event(new_link, context=context, request=request)
-    
-        def test_resize(self):
-        image = dict(url='http://s3.amazonaws.com/a.jpg', width=1200,
-                      height=800)
+      # Detect variable-length arrays.
+  match = Match(r'\s*(.+::)?(\w+) [a-z]\w*\[(.+)];', line)
+  if (match and match.group(2) != 'return' and match.group(2) != 'delete' and
+      match.group(3).find(']') == -1):
+    # Split the size using space and arithmetic operators as delimiters.
+    # If any of the resulting tokens are not compile time constants then
+    # report the error.
+    tokens = re.split(r'\s|\+|\-|\*|\/|<<|>>]', match.group(3))
+    is_const = True
+    skip_next = False
+    for tok in tokens:
+      if skip_next:
+        skip_next = False
+        continue
     
     
-from r2.lib.utils.reddit_agent_parser import (
-    AlienBlueDetector,
-    BaconReaderDetector,
-    detect,
-    McRedditDetector,
-    NarwhalForRedditDetector,
-    ReaditDetector,
-    RedditAndroidDetector,
-    RedditIsFunDetector,
-    RedditIOSDetector,
-    RedditSyncDetector,
-    RelayForRedditDetector)
-from r2.tests import RedditTestCase
+class IDFLoader(object):
     
-        def test_scb(self):
-        scb = stats.StringCountBuffer()
-        self.assertEquals([], list(scb.flush()))
+    
+def viterbi(obs, states, start_p, trans_p, emit_p):
+    V = [{}]  # tabular
+    mem_path = [{}]
+    all_states = trans_p.keys()
+    for y in states.get(obs[0], all_states):  # init
+        V[0][y] = start_p[y] + emit_p[y].get(obs[0], MIN_FLOAT)
+        mem_path[0][y] = ''
+    for t in xrange(1, len(obs)):
+        V.append({})
+        mem_path.append({})
+        #prev_states = get_top_states(V[t-1])
+        prev_states = [
+            x for x in mem_path[t - 1].keys() if len(trans_p[x]) > 0]
+    
+        def test_multiple_strong_etag_not_match(self):
+        computed_etag = ''xyzzy''
+        etags = ''xyzzy1', 'xyzzy2''
+        self.check_url(
+            '/cache/' + computed_etag, method='GET',
+            headers=[('If-None-Match', etags)],
+            expected_status=200)
+    
+                `tornado.options` is primarily a command-line library.
+            Config file support is provided for applications that wish
+            to use it, but applications that prefer config files may
+            wish to look at other libraries instead.
+    
+            super(BaseAsyncIOLoop, self).initialize(**kwargs)
+    
+        @gen.coroutine
+    def resolve(
+        self, host: str, port: int, family: int = 0
+    ) -> 'Generator[Any, Any, List[Tuple[int, Any]]]':
+        if is_valid_ip(host):
+            addresses = [host]
+        else:
+            # gethostbyname doesn't take callback as a kwarg
+            fut = Future()  # type: Future[Tuple[Any, Any]]
+            self.channel.gethostbyname(
+                host, family, lambda result, error: fut.set_result((result, error))
+            )
+            result, error = yield fut
+            if error:
+                raise IOError(
+                    'C-Ares returned error %s: %s while resolving %s'
+                    % (error, pycares.errno.strerror(error), host)
+                )
+            addresses = result.addresses
+        addrinfo = []
+        for address in addresses:
+            if '.' in address:
+                address_family = socket.AF_INET
+            elif ':' in address:
+                address_family = socket.AF_INET6
+            else:
+                address_family = socket.AF_UNSPEC
+            if family != socket.AF_UNSPEC and family != address_family:
+                raise IOError(
+                    'Requested socket family %d but got %d' % (family, address_family)
+                )
+            addrinfo.append((typing.cast(int, address_family), (address, port)))
+        return addrinfo
+
+    
+    
+class Matcher(object):
+    '''Represents a matcher for request features.'''
