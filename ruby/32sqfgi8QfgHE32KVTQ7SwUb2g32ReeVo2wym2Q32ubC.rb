8eb857ae5904a7e8838f1ddbcb8fde4ce3020334
SUNsung <weatherius@gmail.com>
2019-05-04 14:04:37 +0300

    Rewrite [32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb] File in: 2019-05-04 14:04:37

--- ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
+++ ruby/32sqfgi8QfgHE32KVTQ7SwUb2g32ReeVo2wym2Q32ubC.rb
@@ -1,14 +1,18 @@
 
         
-            SCHEME_RGX = /\A[^:\/?#]+:/
-    
-        DOCUMENT_RGX = /\A(?:\s|(?:<!--.*?-->))*<(?:\!doctype|html)/i
-    
-            css('.nav-index-section').each do |node|
-          node.content = node.content
+                # This is what is called on the class to actually execute it. Any
+        # subclasses should implement this method and do any option parsing
+        # and validation here.
+        def execute
         end
     
-            self.description = <<-DESC
-          Shows the content of the pods cache as a YAML tree output, organized by pod.
-          If `NAME` is given, only the caches for that pod will be included in the output.
-        DESC
+            # Halt the machine. This method should gracefully shut down the
+        # operating system. This method will cause `vagrant halt` and associated
+        # commands to _block_, meaning that if the machine doesn't halt
+        # in a reasonable amount of time, this method should just return.
+        #
+        # If when this method returns, the machine's state isn't 'powered_off,'
+        # Vagrant will proceed to forcefully shut the machine down.
+        def halt
+          raise BaseError, _key: :unsupported_halt
+        end
