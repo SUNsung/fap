cc7d7b4cbdf832aa2d03e1fea753eccb11675b59
SUNsung <weatherius@gmail.com>
2019-05-05 11:59:57 +0300

    Rewrite [22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py] File in: 2019-05-05 11:59:57

--- pyton/22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py
+++ pyton/22kCFvIQ0rDE22NChlQdmo5k22NV0ehgDEaoY22RRA.py
@@ -1,202 +1,80 @@
 
         
-        
-def main():
-    if len(sys.argv) < 2:
-        print('No file passed (file should contain Markdown table syntax)')
-        sys.exit(1)
-    check_format(sys.argv[1])
-    if len(errors) > 0:
-        for err in errors:
-            print(err)
-        sys.exit(1)
+            # Replacing the non-sorted libraries by the sorted ones and gathering all at the final_README file
+    blocks[0] = inner_blocks
+    final_README = table_of_contents + '- - -' + ''.join(blocks)
     
-    DEPLABEL_TO_ID = {label: lid for lid, label in enumerate(DEPLABELS)}
-    
-      Returns:
-    In the equation, y = x W + b, returns the tensorflow op that yields y.
-  '''
-  in_size = int(x.get_shape()[1]) # from Dimension(10) -> 10
-  stddev = alpha/np.sqrt(float(in_size))
-  mat_init = tf.random_normal_initializer(0.0, stddev)
-  wname = (name + '/W') if name else '/W'
-    
-    FLAGS = tf.flags.FLAGS
-    
-    EOS_INDEX = 88892
-    
-    '''PTB data loader and helpers.'''
-    
-      Returns:
-    Tuple of the (sequence, logits, log_probs) of the Generator.   Sequence
-      and logits have shape [batch_size, sequence_length, vocab_size].  The
-      log_probs will have shape [batch_size, sequence_length].  Log_probs
-      corresponds to the log probability of selecting the words.
-  '''
-  if FLAGS.generator_model == 'rnn':
-    (sequence, logits, log_probs, initial_state, final_state) = rnn.generator(
-        hparams,
-        inputs,
-        targets,
-        present,
-        is_training=is_training,
-        is_validating=is_validating,
-        reuse=reuse)
-  elif FLAGS.generator_model == 'rnn_zaremba':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = rnn_zaremba.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_zaremba':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq_zaremba.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'rnn_nas':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = rnn_nas.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_nas':
-    (sequence, logits, log_probs, initial_state,
-     final_state) = seq2seq_nas.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  elif FLAGS.generator_model == 'seq2seq_vd':
-    (sequence, logits, log_probs, initial_state, final_state,
-     encoder_states) = seq2seq_vd.generator(
-         hparams,
-         inputs,
-         targets,
-         present,
-         is_training=is_training,
-         is_validating=is_validating,
-         reuse=reuse)
-  else:
-    raise NotImplementedError
-  return (sequence, logits, log_probs, initial_state, final_state,
-          encoder_states)
-    
-      if FLAGS.dis_share_embedding:
-    assert hparams.dis_rnn_size == hparams.gen_rnn_size, (
-        'If you wish to share Discriminator/Generator embeddings, they must be'
-        ' same dimension.')
-    with tf.variable_scope('gen/rnn', reuse=True):
-      embedding = tf.get_variable('embedding',
-                                  [FLAGS.vocab_size, hparams.gen_rnn_size])
-    
-            # Identify targets that need to be added or updated on AWS
-        targets_to_put = self._targets_to_put()
-        if targets_to_put:
-            self.rule.put_targets(targets_to_put)
-    
-        try:
-        client.login(username=module.params['ipa_user'],
-                     password=module.params['ipa_pass'])
-        changed, hbacrule = ensure(module, client)
-        module.exit_json(changed=changed, hbacrule=hbacrule)
-    except Exception as e:
-        module.fail_json(msg=to_native(e), exception=traceback.format_exc())
-    
-        # Build the common request body
-    body = dict()
-    for k in ('component', 'version', 'hosts'):
-        v = module.params[k]
-        if v is not None:
-            body[k] = v
-    
-    EXAMPLES = '''
-- name: Enable ido-pgsql feature
-  icinga2_feature:
-    name: ido-pgsql
-    state: present
-    
-    
-def get_github_url(app, view, path):
-    github_fmt = 'https://github.com/{}/{}/{}/{}{}'
-    return (
-        github_fmt.format(app.config.edit_on_github_project, view,
-                          app.config.edit_on_github_branch,
-                          app.config.edit_on_github_src_path, path))
-    
-        extract_tags = textrank
-
-    
-    MIN_FLOAT = -3.14e100
-    
-    seg_list = jieba.cut('我来到北京清华大学', cut_all=True)
-print('Full Mode: ' + '/ '.join(seg_list))  # 全模式
-    
-    import jieba
-import jieba.analyse
-from optparse import OptionParser
-    
-    
-    
-    
-# copied from fix_tuple_params.py
-def is_docstring(stmt):
-    return isinstance(stmt, pytree.Node) and stmt.children[0].type == token.STRING
-    
-    
-@gen.engine
-def run_tests():
-    url = options.url + '/getCaseCount'
-    control_ws = yield websocket_connect(url, None)
-    num_tests = int((yield control_ws.read_message()))
-    logging.info('running %d cases', num_tests)
-    msg = yield control_ws.read_message()
-    assert msg is None
-    
-    
-class LeakTest(unittest.TestCase):
-    def setUp(self):
-        # Trigger a cleanup of the mapping so we start with a clean slate.
-        AsyncIOLoop().close()
-        # If we don't clean up after ourselves other tests may fail on
-        # py34.
-        self.orig_policy = asyncio.get_event_loop_policy()
-        asyncio.set_event_loop_policy(asyncio.DefaultEventLoopPolicy())
-    
-        @gen.coroutine
-    def get(self):
-        if self.get_argument('openid.mode', None):
-            user = yield self.get_authenticated_user(
-                http_client=self.settings['http_client']
+        def __init__(self, get_response=None):
+        if not apps.is_installed('django.contrib.sites'):
+            raise ImproperlyConfigured(
+                'You cannot use RedirectFallbackMiddleware when '
+                'django.contrib.sites is not installed.'
             )
-            if user is None:
-                raise Exception('user is None')
-            self.finish(user)
-            return
-        res = self.authenticate_redirect()
-        assert res is None
+        super().__init__(get_response)
+    
+        plugin_manager.register(Plugin)
+    try:
+        r = http(
+            httpbin + BASIC_AUTH_URL,
+            '--auth-type',
+            Plugin.auth_type,
+            '--auth',
+            USERNAME + SEP_CREDENTIALS + PASSWORD,
+        )
+        assert HTTP_OK in r
+        assert r.json == AUTH_OK
+    finally:
+        plugin_manager.unregister(Plugin)
+    
+    
+class TestAutoContentTypeAndAcceptHeaders:
+    '''
+    Test that Accept and Content-Type correctly defaults to JSON,
+    but can still be overridden. The same with Content-Type when --form
+    -f is used.
+    
+    import gzip
+import os
+    
+        def __call__(self, x):
+        return 0.
+    
+    from ..layers.merge import concatenate
+from .. import backend as K
+from ..layers.core import Lambda
+from ..engine.training import Model
+from ..models import clone_model
+from ..utils.generic_utils import to_list
+    
+        model.compile(loss='categorical_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['accuracy'])
+    
+    import numpy as np
+import keras
+from keras.datasets import reuters
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.preprocessing.text import Tokenizer
+    
+        with pytest.raises(AssertionError):
+        conv_utils.conv_output_length(32, 5, 'diagonal', 2)
+    
+        # with patch('homeassistant.config.async_hass_config_yaml',
+    #            Mock(return_value=mock_coro(config))):
+    #     yield from hass.services.async_call(
+    #         automation.DOMAIN, automation.SERVICE_RELOAD, blocking=True)
+    
+    
+if __name__ == '__main__':
+    dir_path = './data'
+    files = ['a.txt', 'b.txt', 'c.txt']
+    files = [os.path.join(dir_path, file) for file in files]
+    
+        def _call(self, x):
+        ''''''
+        o = self.act_fn(tf.matmul(x, self.W) + self.b)
+        return o
+    
+        '''
+    max_sentence_len, max_word_len, char_vocab_size = get_shape(x)[1:]
