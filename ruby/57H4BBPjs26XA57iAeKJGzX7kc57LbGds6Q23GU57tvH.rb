cecb3939fc8912e8e8bf6b889526c020f5e5354e
SUNsung <weatherius@gmail.com>
2019-05-05 23:01:43 +0300

    Rewrite [57H4BBPjs26XA57iAeKJGzX7kc57LbGds6Q23GU57tvH.rb] File in: 2019-05-05 23:01:42

--- ruby/57H4BBPjs26XA57iAeKJGzX7kc57LbGds6Q23GU57tvH.rb
+++ ruby/57H4BBPjs26XA57iAeKJGzX7kc57LbGds6Q23GU57tvH.rb
@@ -1,57 +1,73 @@
 
         
-            def resource_params
-      params.require(:user).permit(
-        :unconfirmed_email
-      )
-    end
-  end
-end
-
+                  if options['multiple']
+            add_default_name_and_id_for_value(@checked_value, options)
+            options.delete('multiple')
+          else
+            add_default_name_and_id(options)
+          end
     
-            if params[:create_and_unresolve]
-          @report.unresolve!
-          log_action :reopen, @report
+                options = options.dup
+            options[:field_name]           = @method_name
+            options[:include_position]     = true
+            options[:prefix]             ||= @object_name
+            options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
+    
+                case platform
+            when 'iOS' then self.platform :ios, '10.0'
+            when 'macOS' then self.platform :macos, '10.10'
+            end
+    
+            private
+    
+          def preference_field_options(options)
+        field_options = case options[:type]
+                        when :integer
+                          {
+                            size: 10,
+                            class: 'input_integer form-control'
+                          }
+                        when :boolean
+                          {}
+                        when :string
+                          {
+                            size: 10,
+                            class: 'input_string form-control'
+                          }
+                        when :password
+                          {
+                            size: 10,
+                            class: 'password_string form-control'
+                          }
+                        when :text
+                          {
+                            rows: 15,
+                            cols: 85,
+                            class: 'form-control'
+                          }
+                        else
+                          {
+                            size: 10,
+                            class: 'input_string form-control'
+                          }
+                        end
+    
+              context 'A shipment has shipped' do
+            it 'does not show or let me back to the cart page, nor show the shipment edit buttons', js: false do
+              order = create(:order, state: 'payment')
+              order.shipments.create!(stock_location_id: stock_location.id, state: 'shipped')
+    
+      context 'moving variants between shipments', js: true do
+    before do
+      create(:stock_location, name: 'LA')
+      visit spree.admin_orders_path
+      within_row(1) do
+        click_link 'R100'
+      end
+    end
+    
+            def find_product
+          super(params[:id])
         end
     
-      private
-    
-        # Mobile devices do not support regular notifications, so we enable push notifications by default
-    alerts_enabled = active_session.detection.device.mobile? || active_session.detection.device.tablet?
-    
-    # Include LoggerSilence from ActiveSupport. This is needed to silent assets
-# requests with `config.assets.quiet`, because the default silence method of
-# the logging gem is no-op. See: https://github.com/TwP/logging/issues/11
-Logging::Logger.send :alias_method, :local_level, :level
-Logging::Logger.send :alias_method, :local_level=, :level=
-Logging::Logger.send :include, LoggerSilence
-
-    
-          get :index, params: {a_id: @aspect.id, page: '1'}, format: :json
-      save_fixture(response.body, 'aspects_manage_contacts_json')
-    end
-    
-        it 'does redirect if there are no invites available with this code' do
-      code = InvitationCode.create(user: bob)
-      code.update_attributes(count: 0)
-    
-      gem 'danger'
-end
-    
-    #{stack}
-#{executable_path}
-### Plugins
-    
-              <% end %>
-    
-      task :index do
-    doc = File.read('README.md')
-    file = 'doc/rack-protection-readme.md'
-    Dir.mkdir 'doc' unless File.directory? 'doc'
-    puts 'writing #{file}'
-    File.open(file, 'w') { |f| f << doc }
-  end
-    
-          def empty_cookie(host, path)
-        {:value => '', :domain => host, :path => path, :expires => Time.at(0)}
-      end
+          @@property_attributes = [:id, :name, :presentation]
