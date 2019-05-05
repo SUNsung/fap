eeb539d4e79e25d63ebdf34539acf88e21dd0029
SUNsung <weatherius@gmail.com>
2019-05-05 16:58:33 +0300

    Rewrite [576e7EFuAbQks57ELmnfLtuA7s57fvme2ct7Lv657aBY.cpp] File in: 2019-05-05 16:58:33

--- cccccc/576e7EFuAbQks57ELmnfLtuA7s57fvme2ct7Lv657aBY.cpp
+++ cccccc/576e7EFuAbQks57ELmnfLtuA7s57fvme2ct7Lv657aBY.cpp
@@ -1,342 +1,106 @@
 
         
-          // Open leveldb
-  leveldb::DB* db;
-  leveldb::Options options;
-  options.create_if_missing = true;
-  options.error_if_exists = true;
-  leveldb::Status status = leveldb::DB::Open(
-      options, db_filename, &db);
-  CHECK(status.ok()) << 'Failed to open leveldb ' << db_filename
-      << '. Is it already existing?';
+          Storage(GenericSignature *genericSig,
+          ArrayRef<Type> replacementTypes,
+          ArrayRef<ProtocolConformanceRef> conformances);
     
-      /**
-   * @brief Given the bottom blobs, compute the top blobs and the loss.
-   *
-   * @param bottom
-   *     the input blobs, whose data fields store the input data for this layer
-   * @param top
-   *     the preshaped output blobs, whose data fields will store this layers'
-   *     outputs
-   * \return The total loss from the layer.
-   *
-   * The Forward wrapper calls the relevant device wrapper function
-   * (Forward_cpu or Forward_gpu) to compute the top blob values given the
-   * bottom blobs.  If the layer has any non-zero loss_weights, the wrapper
-   * then computes and returns the loss.
-   *
-   * Your layer should implement Forward_cpu and (optionally) Forward_gpu.
-   */
-  inline Dtype Forward(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+      bool isRecord() const {
+    assert(isValid());
+    return !Decl.isNull() && Decl.is<const clang::RecordDecl *>();
+  }
+  const clang::RecordDecl *getRecord() const {
+    assert(isRecord());
+    return Decl.get<const clang::RecordDecl *>();
+  }
     
-    /**
- * @brief Computes the classification accuracy for a one-of-many
- *        classification task.
- */
-template <typename Dtype>
-class AccuracyLayer : public Layer<Dtype> {
+        void emitDiagnosticMessage(clang::FullSourceLoc Loc,
+                               clang::PresumedLoc PLoc,
+                               clang::DiagnosticsEngine::Level Level,
+                               StringRef Message,
+                               ArrayRef<clang::CharSourceRange> Ranges,
+                               clang::DiagOrStoredDiag Info) override {
+      if (isInSwiftBuffers(Loc)) {
+        // FIXME: Ideally, we'd report non-suppressed diagnostics on synthetic
+        // buffers, printing their names (eg. <swift-imported-modules>:...) but
+        // this risks printing _excerpts_ of those buffers to stderr too; at
+        // present the synthetic buffers are 'large blocks of null bytes' which
+        // we definitely don't want to print out. So until we have some clever
+        // way to print the name but suppress printing excerpts, we just replace
+        // the Loc with an invalid one here, which suppresses both.
+        Loc = clang::FullSourceLoc();
+        if (shouldSuppressDiagInSwiftBuffers(Info))
+          return;
+      }
+      callback(Loc, Level, Message);
+    }
+    
+    
+    {
+    {} // namespace test
+} // namespace c10d
+
+    
+    namespace caffe2 {
+    }
+    
+    REGISTER_CPU_OPERATOR(
+    MergeSingleMapFeatureTensorsGradient,
+    MergeSingleListOrMapFeatureTensorsGradientOp<CPUContext>);
+OPERATOR_SCHEMA(MergeSingleMapFeatureTensorsGradient)
+    .SetDoc(
+        'Explode given multi-feature tensors with map features into '
+        'multiple single-feature tensor.' +
+        doc)
+    .NumInputs([](int n) { return n >= 3 && n % 2 == 1; })
+    .NumOutputs([](int n) { return n >= 1; })
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_presence', '.presence')
+    .Input(2, 'out_values_values_grad', '.values.values_grad')
+    .Output(0, 'in1_values_grad', '.values_grad');
+REGISTER_GRADIENT(
+    MergeSingleMapFeatureTensors,
+    GetMergeSingleMapFeatureTensorsGradient);
+    
+    template <typename T, class Context>
+class BernoulliJSDGradientOp final : public Operator<Context> {
  public:
-  /**
-   * @param param provides AccuracyParameter accuracy_param,
-   *     with AccuracyLayer options:
-   *   - top_k (\b optional, default 1).
-   *     Sets the maximum rank @f$ k @f$ at which a prediction is considered
-   *     correct.  For example, if @f$ k = 5 @f$, a prediction is counted
-   *     correct if the correct label is among the top 5 predicted labels.
-   */
-  explicit AccuracyLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
+  USE_SIMPLE_CTOR_DTOR(BernoulliJSDGradientOp);
+  USE_OPERATOR_CONTEXT_FUNCTIONS;
+  bool RunOnDevice() override;
+};
     
-     protected:
-  /// @copydoc BNLLLayer
-  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
+    REGISTER_CUDA_OPERATOR(LC1D, LocallyConnectedOp<float, CUDAContext>);
+REGISTER_CUDA_OPERATOR(
+    LC1DGradient,
+    LocallyConnectedGradientOp<float, CUDAContext>);
     
-      /**
-   * @brief Computes the error gradient w.r.t. the concatenate inputs.
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *        respect to the outputs
-   *   -# @f$ (KN \times C \times H \times W) @f$ if axis == 0, or
-   *      @f$ (N \times KC \times H \times W) @f$ if axis == 1:
-   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
-   *      with respect to concatenated outputs @f$ y @f$
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length K), into which the top gradient
-   *        @f$ \frac{\partial E}{\partial y} @f$ is deconcatenated back to the
-   *        inputs @f$
-   *        \left[ \begin{array}{cccc}
-   *          \frac{\partial E}{\partial x_1} &
-   *          \frac{\partial E}{\partial x_2} &
-   *          ... &
-   *          \frac{\partial E}{\partial x_K}
-   *        \end{array} \right] =
-   *        \frac{\partial E}{\partial y}
-   *        @f$
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    void Assembler::stdu(const Reg64& rt, MemoryRef m) {
-  assertx(Reg64(-1) == m.r.index);  // doesn't support base+index
-  EmitDSForm(62, rn(rt), rn(m.r.base), m.r.disp, 1);
-}
+    				cmderOptions.cmderCfgRoot = cmderCfgRoot;
     
     
-    {  if (base64) {
-    decoded = string_base64_decode(data, data_len, true);
-    if (decoded.isNull()) {
-      raise_warning('unable to decode base64 data');
-      return nullptr;
-    }
-  } else {
-    decoded = url_decode(data, data_len);
-  }
-  return req::make<MemFile>(decoded.data(), decoded.size());
-}
+    { private:
+  RowBlock<IndexType> out_;
+  std::unique_ptr<Parser<IndexType> > parser_;
+  uint32_t num_col_;
+  std::vector<size_t> offset_;
+  std::vector<IndexType> dense_index_;
+  std::vector<xgboost::bst_float> dense_value_;
+};
     
-    
-    {  // if the function was called via FCallBuiltin, we'll get a bogus name as
-  // the stack frame will be wrong
-  ActRec* ar = g_context->getStackFrame();
-  const char* fn = (ar != nullptr)
-    ? ar->func()->name()->data()
-    : 'OPTIMIZED_BUILTIN';
-  raise_warning('%s(%s): failed to open stream: '
-                'wrapper does not support stream open',
-                fn, filename.data());
-  return nullptr;
-}
-    
-    #include 'hphp/runtime/base/stream-wrapper.h'
-#include 'hphp/runtime/base/runtime-error.h'
-    
-    #ifndef incl_HPHP_PERF_EVENT_H_
-#define incl_HPHP_PERF_EVENT_H_
-    
-    template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, F fillCols) {
-  logPerfWarningImpl(event, 0, kDefaultPerfWarningRate, fillCols);
-}
-template<typename F>
-void logLowPriPerfWarning(folly::StringPiece event, int64_t rate, F fillCols) {
-  logPerfWarningImpl(event, 0, rate, fillCols);
-}
-    
-    
-  while (true)
-  {
-    xfer += iprot->readFieldBegin(fname, ftype, fid);
-    if (ftype == ::apache::thrift::protocol::T_STOP) {
-      break;
-    }
-    switch (fid)
-    {
-      case 0:
-        if (ftype == ::apache::thrift::protocol::T_STRUCT) {
-          xfer += this->success.read(iprot);
-          this->__isset.success = true;
-        } else {
-          xfer += iprot->skip(ftype);
-        }
-        break;
-      default:
-        xfer += iprot->skip(ftype);
-        break;
-    }
-    xfer += iprot->readFieldEnd();
-  }
-    
-    
-    {};
+    /*!
+ * \brief Registry entry for tree updater.
+ */
+struct GradientBoosterReg
+    : public dmlc::FunctionRegEntryBase<
+  GradientBoosterReg,
+  std::function<GradientBooster* (const std::vector<std::shared_ptr<DMatrix> > &cached_mats,
+                                  bst_float base_margin)> > {
+};
     
     
     {
-    {    std::ostringstream oss;
-    std::copy(line.begin() + options_index,
-              line.end(),
-              std::ostream_iterator<std::string>(oss, ' '));
-    r['options'] = oss.str();
-    results.push_back(r);
+    {    ++right;
+    SPAN_ASSERT_TRUE(right > left, status_);
+    SPAN_ASSERT_TRUE(left < right, status_);
+    SPAN_ASSERT_TRUE(left <= right, status_);
   }
-}
-    
-    
-    {
-    {    for (const auto& query : pack.getSchedule()) {
-      rc = runQuery(data, query.second.query.c_str());
-      if (rc != 0) {
-        fprintf(stderr,
-                'Could not execute query %s: %s\n',
-                query.first.c_str(),
-                query.second.query.c_str());
-        return;
-      }
-    }
-  });
-  return rc;
-}
-    
-    Expected<int, PosixError> syscall(struct perf_event_attr* attr,
-                                  pid_t pid,
-                                  int cpu,
-                                  int group_fd,
-                                  unsigned long const flags);
-    
-        flatbuffers::grpc::Message<HelloReply> response_msg;
-    
-    
-    {}  // namespace grpc
-    
-    int main(int /*argc*/, const char * /*argv*/ []) {
-  // Build up a serialized buffer algorithmically:
-  flatbuffers::FlatBufferBuilder builder;
-    }
-    
-    struct Parameters {
-  //Defines the custom parameter types for methods
-  //eg: flatbuffers uses flatbuffers.Builder as input for the client and output for the server
-  grpc::string custom_method_io_type;
-    }
-    
-    
-    {  std::string schema;
-  schema += '// Generated from ' + file_name + '.proto\n\n';
-  if (parser.opts.include_dependence_headers) {
-    // clang-format off
-    #ifdef FBS_GEN_INCLUDES  // TODO: currently all in one file.
-    int num_includes = 0;
-    for (auto it = parser.included_files_.begin();
-         it != parser.included_files_.end(); ++it) {
-      if (it->second.empty())
-        continue;
-      auto basename = flatbuffers::StripPath(
-                        flatbuffers::StripExtension(it->second));
-      schema += 'include \'' + basename + '.fbs\';\n';
-      num_includes++;
-    }
-    if (num_includes) schema += '\n';
-    #endif
-    // clang-format on
-  }
-  // Generate code for all the enum declarations.
-  const Namespace *last_namespace = nullptr;
-  for (auto enum_def_it = parser.enums_.vec.begin();
-       enum_def_it != parser.enums_.vec.end(); ++enum_def_it) {
-    EnumDef &enum_def = **enum_def_it;
-    GenNameSpace(*enum_def.defined_namespace, &schema, &last_namespace);
-    GenComment(enum_def.doc_comment, &schema, nullptr);
-    if (enum_def.is_union)
-      schema += 'union ' + enum_def.name;
-    else
-      schema += 'enum ' + enum_def.name + ' : ';
-    schema += GenType(enum_def.underlying_type, true) + ' {\n';
-    for (auto it = enum_def.Vals().begin(); it != enum_def.Vals().end(); ++it) {
-      auto &ev = **it;
-      GenComment(ev.doc_comment, &schema, nullptr, '  ');
-      if (enum_def.is_union)
-        schema += '  ' + GenType(ev.union_type) + ',\n';
-      else
-        schema += '  ' + ev.name + ' = ' + NumToString(ev.value) + ',\n';
-    }
-    schema += '}\n\n';
-  }
-  // Generate code for all structs/tables.
-  for (auto it = parser.structs_.vec.begin(); it != parser.structs_.vec.end();
-       ++it) {
-    StructDef &struct_def = **it;
-    GenNameSpace(*struct_def.defined_namespace, &schema, &last_namespace);
-    GenComment(struct_def.doc_comment, &schema, nullptr);
-    schema += 'table ' + struct_def.name + ' {\n';
-    for (auto field_it = struct_def.fields.vec.begin();
-         field_it != struct_def.fields.vec.end(); ++field_it) {
-      auto &field = **field_it;
-      if (field.value.type.base_type != BASE_TYPE_UTYPE) {
-        GenComment(field.doc_comment, &schema, nullptr, '  ');
-        schema += '  ' + field.name + ':' + GenType(field.value.type);
-        if (field.value.constant != '0') schema += ' = ' + field.value.constant;
-        if (field.required) schema += ' (required)';
-        schema += ';\n';
-      }
-    }
-    schema += '}\n\n';
-  }
-  return schema;
-}
-    
-      bool generate() {
-    std::string code;
-    code += std::string('// ') + FlatBuffersGeneratedWarning() +
-            '\n\ninclude \'flatbuffers.lobster\'\n\n';
-    for (auto it = parser_.enums_.vec.begin(); it != parser_.enums_.vec.end();
-         ++it) {
-      auto &enum_def = **it;
-      GenEnum(enum_def, &code);
-    }
-    for (auto it = parser_.structs_.vec.begin();
-         it != parser_.structs_.vec.end(); ++it) {
-      auto &struct_def = **it;
-      GenStructPreDecl(struct_def, &code);
-    }
-    for (auto it = parser_.structs_.vec.begin();
-         it != parser_.structs_.vec.end(); ++it) {
-      auto &struct_def = **it;
-      GenStruct(struct_def, &code);
-    }
-    return SaveFile((path_ + file_name_ + '_generated.lobster').c_str(),
-                    code, false);
-  }
-    
-    
-    {    // Finish a buffer with a given root object:
-    code_.SetValue('OFFSET_TYPELABEL', Name(struct_def) + 'Offset');
-    code_ += '#[inline]';
-    code_ += 'pub fn finish_{{STRUCT_NAME_SNAKECASE}}_buffer<'a, 'b>(';
-    code_ += '    fbb: &'b mut flatbuffers::FlatBufferBuilder<'a>,';
-    code_ += '    root: flatbuffers::WIPOffset<{{STRUCT_NAME}}<'a>>) {';
-    if (parser_.file_identifier_.length()) {
-      code_ += '  fbb.finish(root, Some({{STRUCT_NAME_CAPS}}_IDENTIFIER));';
-    } else {
-      code_ += '  fbb.finish(root, None);';
-    }
-    code_ += '}';
-    code_ += '';
-    code_ += '#[inline]';
-    code_ += 'pub fn finish_size_prefixed_{{STRUCT_NAME_SNAKECASE}}_buffer'
-             '<'a, 'b>('
-             'fbb: &'b mut flatbuffers::FlatBufferBuilder<'a>, '
-             'root: flatbuffers::WIPOffset<{{STRUCT_NAME}}<'a>>) {';
-    if (parser_.file_identifier_.length()) {
-      code_ += '  fbb.finish_size_prefixed(root, '
-               'Some({{STRUCT_NAME_CAPS}}_IDENTIFIER));';
-    } else {
-      code_ += '  fbb.finish_size_prefixed(root, None);';
-    }
-    code_ += '}';
-  }
-    
-      // Releases the ownership of the buffer pointer.
-  // Returns the size, offset, and the original grpc_slice that
-  // allocated the buffer. Also see grpc_slice_unref().
-  uint8_t *ReleaseRaw(size_t &size, size_t &offset, grpc_slice &slice) {
-    uint8_t *buf = FlatBufferBuilder::ReleaseRaw(size, offset);
-    slice = slice_allocator_.slice_;
-    slice_allocator_.slice_ = grpc_empty_slice();
-    return buf;
-  }
+};
