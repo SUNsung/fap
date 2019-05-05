867079feb044532aa27ec3e3eece55f03e09bbee
SUNsung <weatherius@gmail.com>
2019-05-06 00:08:38 +0300

    Rewrite [27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp] File in: 2019-05-06 00:08:38

--- cccccc/27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp
+++ cccccc/27T1x4Hu6qSk27L0Rj1ebQ71U27aN9wy6jAR7627tM6.cpp
@@ -1,164 +1,144 @@
 
         
-        Licensed under the Apache License, Version 2.0 (the 'License');
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+        DIRECTIONAL_PREPOSITION(above)
+DIRECTIONAL_PREPOSITION(after)
+DIRECTIONAL_PREPOSITION(along)
+DIRECTIONAL_PREPOSITION(alongside)
+DIRECTIONAL_PREPOSITION(as)
+DIRECTIONAL_PREPOSITION(at)
+DIRECTIONAL_PREPOSITION(before)
+DIRECTIONAL_PREPOSITION(below)
+DIRECTIONAL_PREPOSITION(by)
+DIRECTIONAL_PREPOSITION(following)
+DIRECTIONAL_PREPOSITION(for)
+DIRECTIONAL_PREPOSITION(from)
+DIRECTIONAL_PREPOSITION(given)
+DIRECTIONAL_PREPOSITION(in)
+DIRECTIONAL_PREPOSITION(including)
+DIRECTIONAL_PREPOSITION(inside)
+DIRECTIONAL_PREPOSITION(into)
+DIRECTIONAL_PREPOSITION(matching)
+DIRECTIONAL_PREPOSITION(of)
+DIRECTIONAL_PREPOSITION(on)
+DIRECTIONAL_PREPOSITION(passing)
+DIRECTIONAL_PREPOSITION(preceding)
+DIRECTIONAL_PREPOSITION(since)
+DIRECTIONAL_PREPOSITION(to)
+DIRECTIONAL_PREPOSITION(until)
+DIRECTIONAL_PREPOSITION(using)
+DIRECTIONAL_PREPOSITION(via)
+DIRECTIONAL_PREPOSITION(when)
+PREPOSITION(with)
+DIRECTIONAL_PREPOSITION(within)
     
-    Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
-                       std::function<void()> destructor, PyObject** result) {
-  int size = 1;
-  for (int i = 0; i < dim_size; ++i) {
-    size *= dims[i];
-  }
-  if (dtype == DT_STRING || dtype == DT_RESOURCE || size == 0) {
-    return errors::FailedPrecondition(
-        'Cannot convert strings, resources, or empty Tensors.');
-  }
-    }
+    void AutolinkExtractJobAction::anchor() {}
     
-    // Creates a numpy array with shapes specified by dim_size and dims and content
-// in data. The array does not own the memory, and destructor will be called to
-// release it. If the status is not ok the caller is responsible for releasing
-// the memory.
-Status ArrayFromMemory(int dim_size, npy_intp* dims, void* data, DataType dtype,
-                       std::function<void()> destructor, PyObject** result);
     
-    void ImportNumpy() {
-  import_array1();
+    {public:
+  Windows(const Driver &D, const llvm::Triple &Triple) : ToolChain(D, Triple) {}
+  ~Windows() = default;
+  std::string sanitizerRuntimeLibName(StringRef Sanitizer,
+                                      bool shared = true) const override;
+};
+    
+    
+    {  // Check if the optional_nested_message was actually moved (and not just
+  // copied).
+  EXPECT_EQ(nested, &message2.optional_nested_message());
+  EXPECT_NE(nested, &message1.optional_nested_message());
+}
+    
+    TEST(JavaDocCommentTest, Escaping) {
+  EXPECT_EQ('foo /&#42; bar *&#47; baz', EscapeJavadoc('foo /* bar */ baz'));
+  EXPECT_EQ('foo /&#42;&#47; baz', EscapeJavadoc('foo /*/ baz'));
+  EXPECT_EQ('{&#64;foo}', EscapeJavadoc('{@foo}'));
+  EXPECT_EQ('&lt;i&gt;&amp;&lt;/i&gt;', EscapeJavadoc('<i>&</i>'));
+  EXPECT_EQ('foo&#92;u1234bar', EscapeJavadoc('foo\\u1234bar'));
+  EXPECT_EQ('&#64;deprecated', EscapeJavadoc('@deprecated'));
 }
     
     
-    {  tensorflow::DeviceNameUtils::ParsedName parsed_name;
-  if (!tensorflow::DeviceNameUtils::ParseFullName(node_def.device(),
-                                                  &parsed_name)) {
-    LOG(WARNING) << 'Failed to parse device from node_def: '
-                 << node_def.ShortDebugString();
-    return '';
-  }
-  string class_name = '';
-  tensorflow::FindKernelDef(tensorflow::DeviceType(parsed_name.type.c_str()),
-                            node_def, nullptr /* kernel_def */, &class_name)
-      .IgnoreError();
-  return class_name;
-}
-    
-    PLATFORM_DEFINE_ID(kCudaPlatformId);
-    
-    #endif  // TENSORFLOW_STREAM_EXECUTOR_CUDA_CUDA_PLATFORM_ID_H_
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace strings
+}  // namespace protobuf
+}  // namespace google
 
     
-    namespace atom {
+    #include <ostream>
+#include <stdio.h>
+#include <string>
+#include <utility>
+    
+    TEST(StatusOr, TestPointerDefaultCtor) {
+  StatusOr<int*> thing;
+  EXPECT_FALSE(thing.ok());
+  EXPECT_EQ(Status::UNKNOWN, thing.status());
+}
+    
+    #include <google/protobuf/io/gzip_stream.h>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+    
+    class DataStripper {
+ public:
+  void StripMessage(Message *message) {
+    std::vector<const FieldDescriptor*> set_fields;
+    const Reflection* reflection = message->GetReflection();
+    reflection->ListFields(*message, &set_fields);
+    }
     }
     
-    AtomQuotaPermissionContext::~AtomQuotaPermissionContext() {}
     
-    AboutProtocolHandler::~AboutProtocolHandler() {}
+    {
+    {
+    {}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
     
-    namespace atom {
-    }
     
-    void CacheImpl::removeAll() {
-  cache_remove_all(static_cast<cache_t*>(Impl));
+    {}  // namespace leveldb
+    
+    #include 'db/filename.h'
+#include 'leveldb/env.h'
+#include 'leveldb/table.h'
+#include 'util/coding.h'
+    
+      static size_t ByteSize(const WriteBatch* batch) { return batch->rep_.size(); }
+    
+    #include 'modules/drivers/canbus/can_client/socket/socket_can_client_raw.h'
+    
+    
+    {  Byte t1(bytes + 2);
+  uint32_t t = t1.get_byte(3, 5);
+  x <<= 5;
+  x |= t;
+  double ret = x * CLUSTER_DIST_RES + CLUSTER_DIST_LONG_MIN;
+  return ret;
 }
     
-      assert(capacity % 16 == 0 && 'not allocating multiple of alignment');
+      x <<= 4;
+  x |= t;
     
-      // Satisfy the default implementation (bookkeeping).
-  if (DumpToStderr)
-    TextDiagnosticPrinter::HandleDiagnostic(clangDiagLevel, clangDiag);
-  else
-    DiagnosticConsumer::HandleDiagnostic(clangDiagLevel, clangDiag);
-    
-    #include 'swift/Demangling/StandardTypesMangling.def'
-    
-    void Demangler::dump() {
-  for (unsigned Idx = 0; Idx < NodeStack.size(); ++Idx) {
-    fprintf(stderr, 'NodeStack[%u]:\n', Idx);
-    NodeStack[Idx]->dump();
-    fprintf(stderr, '\n');
-  }
-  fprintf(stderr, 'Position = %zd:\n%.*s\n%*s\n', Pos,
-          (int)Text.size(), Text.data(), (int)Pos + 1, '^');
-}
+    unsigned int BaseMapMatrix::LoadBinary(unsigned char* buf) { return 0; }
     
     
-    {  if (failed) throw std::runtime_error('parallel::for_each failed');
-}
-    
-    namespace php {
-struct Program;
-}
-struct Index;
-    
-      /*
-   * Create an uncounted array if we can.
-   *
-   * If this collection is an OuterHandle, then we need to do a full check on
-   * this array for things like circularity.  If we're an InnerHandle, someone
-   * already checked that, but we want to check for whether it's uncounted to
-   * use a better representation.  For the OuterHandle case, we just delegate
-   * to APCArray below (which will do the full DataWalker pass).
-   */
-  if (level == APCHandleLevel::Inner && apcExtension::UseUncounted &&
-      !array->empty()) {
-    DataWalker walker(DataWalker::LookupFeature::HasObjectOrResource);
-    auto const features = walker.traverseData(const_cast<ArrayData*>(array));
-    assertx(!features.isCircular);
-    if (!features.hasObjectOrResource) {
-      auto const makeUncounted = [&] () {
-        if (isVectorCollection(obj->collectionType())) {
-          return APCArray::MakeUncountedVec(const_cast<ArrayData*>(array));
-        }
-        return APCArray::MakeUncountedDict(const_cast<ArrayData*>(array));
-      };
-      return WrapArray(
-        { makeUncounted(), getMemSize(array) + sizeof(APCTypedValue) },
-        obj->collectionType()
-      );
-    }
-  }
-    
-    void Config::ParseConfigFile(const std::string &filename, IniSettingMap &ini,
-                             Hdf &hdf, const bool is_system /* = true */) {
-  // We don't allow a filename of just '.ini'
-  if (boost::ends_with(filename, '.ini') && filename.length() > 4) {
-    Config::ParseIniFile(filename, ini, false, is_system);
-  } else {
-    // For now, assume anything else is an hdf file
-    // TODO(#5151773): Have a non-invasive warning if HDF file does not end
-    // .hdf
-    Config::ParseHdfFile(filename, hdf);
-  }
-}
-    
-      /**
-   * Use the Iterate method for iterating over options that are stored as
-   * objects in runtime options (e.g. FilesMatch). This function iterates over
-   * the settings passed as ini/hdf, calls back to, generally, the constructor
-   * of the object in question.
-   *
-   * Note: For now, we are not `ini_get()` enabling these type of options as
-   * it is not trivial to come up with a non-hacky and workable way to store
-   * the data correctly. Also, as usual, Hdf takes priority.
-   */
-  static void Iterate(std::function<void (const IniSettingMap&,
-                                          const Hdf&,
-                                          const std::string&)> cb,
-                      const IniSettingMap &ini, const Hdf& config,
-                      const std::string &name, const bool prepend_hhvm = true);
-    
-    #endif // HPHP_FILE_STREAM_WRAPPER_H
+    {
+    {}  // namespace planning
+}  // namespace apollo
 
     
+      Byte t1(bytes + 3);
+  int32_t t = t1.get_byte(0, 8);
+  x <<= 8;
+  x |= t;
     
-    {  // if the function was called via FCallBuiltin, we'll get a bogus name as
-  // the stack frame will be wrong
-  ActRec* ar = g_context->getStackFrame();
-  const char* fn = (ar != nullptr)
-    ? ar->func()->name()->data()
-    : 'OPTIMIZED_BUILTIN';
-  raise_warning('%s(%s): failed to open stream: '
-                'wrapper does not support stream open',
-                fn, filename.data());
-  return nullptr;
-}
+    // config detail: {'name': 'latitude_seconds', 'offset': 0.0, 'precision': 1.0,
+// 'len': 8, 'is_signed_var': True, 'physical_range': '[-128|127]', 'bit': 23,
+// 'type': 'int', 'order': 'motorola', 'physical_unit': 'sec'}
+int Latlonheadingrpt82::latitude_seconds(const std::uint8_t* bytes,
+                                         int32_t length) const {
+  Byte t0(bytes + 2);
+  int32_t x = t0.get_byte(0, 8);
+    }
