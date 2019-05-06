d12e67bc97f98f8dc99501af39ca3e43cd9015ad
SUNsung <weatherius@gmail.com>
2019-05-07 00:22:45 +0300

    Rewrite [56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp] File in: 2019-05-07 00:22:44

--- cccccc/56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp
+++ cccccc/56vRS5DVhsJLc56RRfEHeaEmk56A8swzzqKs56KKQ.cpp
@@ -1,201 +1,271 @@
 
         
-        #ifndef BITCOIN_REVERSELOCK_H
-#define BITCOIN_REVERSELOCK_H
+        
+    {}  // namespace caffe
     
     
-    {    /* cleanup */
-    secp256k1_context_destroy(none);
-    secp256k1_context_destroy(sign);
-    secp256k1_context_destroy(vrfy);
-    secp256k1_context_destroy(both);
+    {  /**
+   * @brief Computes the error gradient w.r.t. the BNLL inputs.
+   *
+   * @param top output Blob vector (length 1), providing the error gradient with
+   *      respect to the outputs
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      containing error gradients @f$ \frac{\partial E}{\partial y} @f$
+   *      with respect to computed outputs @f$ y @f$
+   * @param propagate_down see Layer::Backward.
+   * @param bottom input Blob vector (length 2)
+   *   -# @f$ (N \times C \times H \times W) @f$
+   *      the inputs @f$ x @f$; Backward fills their diff with
+   *      gradients @f$
+   *        \frac{\partial E}{\partial x}
+   *      @f$ if propagate_down[0]
+   */
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+    
+      virtual inline int ExactNumBottomBlobs() const { return 3; }
+  virtual inline const char* type() const { return 'ContrastiveLoss'; }
+  /**
+   * Unlike most loss layers, in the ContrastiveLossLayer we can backpropagate
+   * to the first two inputs.
+   */
+  virtual inline bool AllowForceBackward(const int bottom_index) const {
+    return bottom_index != 2;
+  }
+    
+      // algorithms for forward and backwards convolutions
+  cudnnConvolutionFwdAlgo_t *fwd_algo_;
+  cudnnConvolutionBwdFilterAlgo_t *bwd_filter_algo_;
+  cudnnConvolutionBwdDataAlgo_t *bwd_data_algo_;
+    
+    int32 remainder(int32 x, int32 modulus);
+int32 TriangulatePolygon(float32* xv, float32* yv, int32 vNum, b2Triangle* results);
+bool IsEar(int32 i, float32* xv, float32* yv, int32 xvLength); //Not for external use
+int32 PolygonizeTriangles(b2Triangle* triangulated, int32 triangulatedLength, b2Polygon* polys, int32 polysLength);
+int32 DecomposeConvex(b2Polygon* p, b2Polygon* results, int32 maxPolys);
+//void DecomposeConvexAndAddTo(b2Polygon* p, b2Body* bd, b2FixtureDef* prototype);
+    
+    //Constructor automatically fixes orientation to ccw
+b2Triangle::b2Triangle(float32 x1, float32 y1, float32 x2, float32 y2, float32 x3, float32 y3){
+	x = new float32[3];
+	y = new float32[3];
+	float32 dx1 = x2-x1;
+	float32 dx2 = x3-x1;
+	float32 dy1 = y2-y1;
+	float32 dy2 = y3-y1;
+	float32 cross = dx1*dy2-dx2*dy1;
+	bool ccw = (cross>0);
+	if (ccw){
+		x[0] = x1; x[1] = x2; x[2] = x3;
+		y[0] = y1; y[1] = y2; y[2] = y3;
+	} else{
+		x[0] = x1; x[1] = x3; x[2] = x2;
+		y[0] = y1; y[1] = y3; y[2] = y2;
+	}
 }
     
-        BOOST_CHECK(arr.push_backV(vec));
+b2Triangle::b2Triangle(){
+	x = new float32[3];
+	y = new float32[3];
+}
     
-    // Bech32 is a string encoding format used in newer address types.
-// The output consists of a human-readable part (alphanumeric), a
-// separator character (1), and a base32 data section, the last
-// 6 characters of which are a checksum.
-//
-// For more information, see BIP 173.
     
-    void absDiff(const Size2D &size,
-             const f32 * src0Base, ptrdiff_t src0Stride,
-             const f32 * src1Base, ptrdiff_t src1Stride,
-             f32 * dstBase, ptrdiff_t dstStride)
+    
+    		inline Block4x4Encoding * GetEncoding(void)
+		{
+			return m_pencoding;
+		}
+    
+    	// ----------------------------------------------------------------------------------------------------
+	//
+    
+    	// ################################################################################
+	// Block4x4EncodingBits_A8
+	// Encoding bits for the A portion of RGBA8
+	// ################################################################################
+    
+    #endif
+
+    
+    #define SHR16(a, shift) SHR16_(a, shift, __FILE__, __LINE__)
+static OPUS_INLINE short SHR16_(int a, int shift, char *file, int line)
 {
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    internal::vtransform(size,
-                         src0Base, src0Stride,
-                         src1Base, src1Stride,
-                         dstBase, dstStride, AbsDiff<f32>());
-#else
-    (void)size;
-    (void)src0Base;
-    (void)src0Stride;
-    (void)src1Base;
-    (void)src1Stride;
-    (void)dstBase;
-    (void)dstStride;
+   int res;
+   if (!VERIFY_SHORT(a) || !VERIFY_SHORT(shift))
+   {
+      fprintf (stderr, 'SHR16: inputs are not short: %d >> %d in %s: line %d\n', a, shift, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
 #endif
+   }
+   res = a>>shift;
+   if (!VERIFY_SHORT(res))
+   {
+      fprintf (stderr, 'SHR16: output is not short: %d in %s: line %d\n', res, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips++;
+   return res;
+}
+#define SHL16(a, shift) SHL16_(a, shift, __FILE__, __LINE__)
+static OPUS_INLINE short SHL16_(int a, int shift, char *file, int line)
+{
+   int res;
+   if (!VERIFY_SHORT(a) || !VERIFY_SHORT(shift))
+   {
+      fprintf (stderr, 'SHL16: inputs are not short: %d %d in %s: line %d\n', a, shift, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   res = a<<shift;
+   if (!VERIFY_SHORT(res))
+   {
+      fprintf (stderr, 'SHL16: output is not short: %d in %s: line %d\n', res, file, line);
+#ifdef FIXED_DEBUG_ASSERT
+      celt_assert(0);
+#endif
+   }
+   celt_mips++;
+   return res;
 }
     
-        void operator() (const typename internal::VecTraits<T>::vec64 & v_src0,
-                     const typename internal::VecTraits<T>::vec64 & v_src1,
-                     typename internal::VecTraits<T>::vec64 & v_dst) const
-    {
-        v_dst = internal::vqadd(v_src0, v_src1);
+       - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+    
+    GC_T:
+            if (fp->fInputIdx >= fActiveLimit)         goto GC_Done;
+            U16_NEXT(inputBuf, fp->fInputIdx, fActiveLimit, c);
+            if (sets[URX_GC_T]->contains(c))       goto GC_T;
+            U16_PREV(inputBuf, 0, fp->fInputIdx, c);
+            goto GC_Extend;
+    
+    static UBool copyAsSuperscript(
+        const UnicodeString &s,
+        int32_t beginIndex,
+        int32_t endIndex,
+        UnicodeString &result,
+        UErrorCode &status) {
+    if (U_FAILURE(status)) {
+        return FALSE;
     }
-    
-    inline float32x2_t vrecp_f32(float32x2_t val)
-{
-    float32x2_t reciprocal = vrecpe_f32(val);
-    reciprocal = vmul_f32(vrecps_f32(val, reciprocal), reciprocal);
-    reciprocal = vmul_f32(vrecps_f32(val, reciprocal), reciprocal);
-    return reciprocal;
-}
-    
-            virtual Dictionary GetCheckpointState() const override;
-        virtual void RestoreFromCheckpoint(const Dictionary& checkpoint) override;
-    
-    namespace CNTK
-{
-    const std::wstring versionKey = L'version';
-    const std::wstring typeKey = L'type';
-    const std::wstring uidKey = L'uid';
-    const std::wstring kindKey = L'kind';
-    const std::wstring dataTypeKey = L'data_type';
-    const std::wstring dynamicAxisKey = L'dynamic_axis';
-    const std::wstring isSparseKey = L'is_sparse';
-    const std::wstring nameKey = L'name';
-    const std::wstring needsGradientKey = L'needs_gradient';
-    const std::wstring shapeKey = L'shape';
-    const std::wstring valueKey = L'value';
-    const std::wstring opKey = L'op';
-    const std::wstring attributesKey = L'attributes';
-    const std::wstring inputsKey = L'inputs';
-    const std::wstring rootKey = L'root';
-    const std::wstring functionsKey = L'primitive_functions';
-    const std::wstring sampleCountKey = L'sample_count';
-    const std::wstring minibatchCountKey = L'minibatchCount'; // TODO: Python-style spelling
-    const std::wstring sweepCountKey = L'sweepCount';
-    const std::wstring unitKey = L'unit';
-    const std::wstring refMBSizeKey = L'ref_mb_size';
-    const std::wstring epochSizeKey = L'epoch_size';
-    const std::wstring scheduleKey = L'schedule';
-    const std::wstring learningRateScheduleKey = L'learnig_rate_schedule';
-    const std::wstring smoothedGradientsKey = L'smoothed_gradients';
-    const std::wstring noiseInjectionSeedKey = L'noise_injection_seed';
-    const std::wstring masterParameterUpdatedKey = L'master_parameter_updated';
-    const std::wstring smoothedCountKey = L'smoothed_count';
-    const std::wstring stateKey = L'state';
-    const std::wstring rngSeedKey = L'rng_seed';
-    const std::wstring rngOffsetKey = L'rng_offset';
-    const std::wstring blockFunctionCompositeKey = L'block_function_composite';
-    const std::wstring blockFunctionOpNameKey = L'block_function_op_name';
-    const std::wstring blockFunctionCompositeArgumentsMapKeysKey = L'block_function_composite_arguments_map_keys';
-    const std::wstring blockFunctionCompositeArgumentsMapValuesKey = L'block_function_composite_arguments_map_values';
-    const std::wstring internalWorkerStateKey = L'internal_worker_state';
-    const std::wstring externalWorkerStateKey = L'external_worker_state';
-    const std::wstring userDefinedStateKey = L'user_defined_state';
-    const std::wstring udfModuleNameKey = L'module';
-    const std::wstring udfFactoryMethodNameKey = L'deserialize_method';
-    const std::wstring nativeUDFKey = L'native';
-    }
-    
-            m_combinedTrainingFunction = Combine(combinedFunctionArgs);
-        SetCombinedEvalFunction(m_combinedTrainingFunction);
-    
-    namespace CNTK
-{
-    class UDFUtils
-    {
-    public:
-    }
-    }
-    
-    
-    {            if ((m_varKind == VariableKind::Parameter) || (m_varKind == VariableKind::Constant))
-            {
-                if (m_shape.HasFreeDimension())
-                    InvalidArgument('Parameter/Constant '%S' has invalid shape '%S'; it is illegal for a Parameter/Constant to have a FreeDimension.', AsString().c_str(), m_shape.AsString().c_str());
-            }
+    for (int32_t i = beginIndex; i < endIndex;) {
+        UChar32 c = s.char32At(i);
+        int32_t digit = u_charDigitValue(c);
+        if (digit < 0) {
+            status = U_INVALID_CHAR_FOUND;
+            return FALSE;
         }
-    
-            aliasInfo.releaseCount++;
-    
-            case URX_CTR_LOOP_NG:
-            {
-                // Non-greedy {min, max} loops
-                U_ASSERT(opValue>0 && opValue < fp->fPatIdx-2);
-                int32_t initOp = (int32_t)pat[opValue];
-                U_ASSERT(URX_TYPE(initOp) == URX_CTR_INIT_NG);
-                int64_t *pCounter = &fp->fExtra[URX_VAL(initOp)];
-                int32_t minCount  = (int32_t)pat[opValue+2];
-                int32_t maxCount  = (int32_t)pat[opValue+3];
+        result.append(kSuperscriptDigits[digit]);
+        i += U16_LENGTH(c);
     }
+    return TRUE;
+}
     
-        pat     = RegexPattern::compile(regex, 0, pe, status);
-    matcher = pat->matcher(status);
-    if (U_SUCCESS(status)) {
-        matcher->reset(input);
-        retVal  = matcher->matches(status);
+    SearchIterator::SearchIterator(const SearchIterator &other)
+    : UObject(other)
+{   
+    m_breakiterator_            = other.m_breakiterator_;
+    m_text_                     = other.m_text_;
+    m_search_                   = (USearch *)uprv_malloc(sizeof(USearch));   
+    m_search_->breakIter        = other.m_search_->breakIter;
+    m_search_->isCanonicalMatch = other.m_search_->isCanonicalMatch;
+    m_search_->isOverlap        = other.m_search_->isOverlap;
+    m_search_->elementComparisonType = other.m_search_->elementComparisonType;
+    m_search_->matchedIndex     = other.m_search_->matchedIndex;
+    m_search_->matchedLength    = other.m_search_->matchedLength;
+    m_search_->text             = other.m_search_->text;
+    m_search_->textLength       = other.m_search_->textLength;
+}
+    
+    UBool
+SelectFormat::operator==(const Format& other) const {
+    if (this == &other) {
+        return TRUE;
     }
+    if (!Format::operator==(other)) {
+        return FALSE;
+    }
+    const SelectFormat& o = (const SelectFormat&)other;
+    return msgPattern == o.msgPattern;
+}
     
-    #include 'unicode/utypes.h'
-#include 'sharedobject.h'
-#include 'unicode/dtfmtsym.h'
+    U_NAMESPACE_BEGIN
     
-    // When calendar uses hebr numbering (i.e. he@calendar=hebrew),
-// offset the years within the current millenium down to 1-999
-static const int32_t HEBREW_CAL_CUR_MILLENIUM_START_YEAR = 5000;
-static const int32_t HEBREW_CAL_CUR_MILLENIUM_END_YEAR = 6000;
+    #endif
     
-        // Handle a cursor after the output.  Use > rather than >= because
-    // if cursor == output.length() it is at the end of the output,
-    // which is the default position, so we need not emit it.
-    if (hasCursor && cursor > output.length()) {
-        cursor -= output.length();
-        while (cursor-- > 0) {
-            ICU_Utility::appendToRule(rule, (UChar)0x0040 /*@*/, TRUE, escapeUnprintable, quoteBuf);
+    U_NAMESPACE_BEGIN
+    
+    U_NAMESPACE_END
+    
+    /**
+ * A representation an acceptable range of digit counts for integers.
+ */
+class U_I18N_API IntDigitCountRange : public UMemory {
+public:
+    /**
+     * No constraints: 0 up to INT32_MAX
+     */
+    IntDigitCountRange() : fMin(0), fMax(INT32_MAX) { }
+    IntDigitCountRange(int32_t min, int32_t max);
+    int32_t pin(int32_t digitCount) const;
+    int32_t getMax() const { return fMax; }
+    int32_t getMin() const { return fMin; }
+private:
+    int32_t fMin;
+    int32_t fMax;
+};
+    
+    #if !UCONFIG_NO_FORMATTING
+    
+    #endif   // #if !UCONFIG_NO_FORMATTING
+#endif   // SMPDTFST_H
+
+    
+    int32_t StandardPlural::indexOrNegativeFromString(const char *keyword) {
+    switch (*keyword++) {
+    case 'f':
+        if (uprv_strcmp(keyword, 'ew') == 0) {
+            return FEW;
         }
-        ICU_Utility::appendToRule(rule, (UChar)0x007C /*|*/, TRUE, escapeUnprintable, quoteBuf);
+        break;
+    case 'm':
+        if (uprv_strcmp(keyword, 'any') == 0) {
+            return MANY;
+        }
+        break;
+    case 'o':
+        if (uprv_strcmp(keyword, 'ther') == 0) {
+            return OTHER;
+        } else if (uprv_strcmp(keyword, 'ne') == 0) {
+            return ONE;
+        }
+        break;
+    case 't':
+        if (uprv_strcmp(keyword, 'wo') == 0) {
+            return TWO;
+        }
+        break;
+    case 'z':
+        if (uprv_strcmp(keyword, 'ero') == 0) {
+            return ZERO;
+        }
+        break;
+    default:
+        break;
     }
-    // Flush quoteBuf out to result
-    ICU_Utility::appendToRule(rule, -1,
-                              TRUE, escapeUnprintable, quoteBuf);
-    
-    #define WRITE_CHECK(fp, ptr, count)                                            \
-  if (fp.write((ptr), (count)) != (count)) {                                   \
-    throw DL_ABORT_EX(                                                         \
-        fmt('Failed to save DHT routing table to %s.', filename.c_str()));     \
-  }
-    
-    #include 'common.h'
-    
-    void DHTTaskQueueImpl::addPeriodicTask1(const std::shared_ptr<DHTTask>& task)
-{
-  periodicTaskQueue1_.addTask(task);
+    return -1;
 }
     
-    std::string DHTTokenTracker::generateToken(const unsigned char* infoHash,
-                                           const std::string& ipaddr,
-                                           uint16_t port,
-                                           const unsigned char* secret) const
-{
-  unsigned char src[DHT_ID_LENGTH + COMPACT_LEN_IPV6 + SECRET_SIZE];
-  memset(src, 0, sizeof(src));
-  int compactlen = bittorrent::packcompact(src + DHT_ID_LENGTH, ipaddr, port);
-  if (compactlen == 0) {
-    throw DL_ABORT_EX(fmt('Token generation failed: ipaddr=%s, port=%u',
-                          ipaddr.c_str(), port));
-  }
-  memcpy(src, infoHash, DHT_ID_LENGTH);
-  memcpy(src + DHT_ID_LENGTH + COMPACT_LEN_IPV6, secret, SECRET_SIZE);
-  unsigned char md[20];
-  message_digest::digest(md, sizeof(md), MessageDigest::sha1().get(), src,
-                         sizeof(src));
-  return std::string(&md[0], &md[sizeof(md)]);
-}
+        for (int32_t i=0; i<output.length(); ++i) {
+        if (hasCursor && i == cursor) {
+            ICU_Utility::appendToRule(rule, (UChar)0x007C /*|*/, TRUE, escapeUnprintable, quoteBuf);
+        }
+        UChar c = output.charAt(i); // Ok to use 16-bits here
+    }
