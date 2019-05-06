ffb5f965560e689038943239932585f37c5dedd2
SUNsung <weatherius@gmail.com>
2019-05-06 21:09:44 +0300

    Rewrite [49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb] File in: 2019-05-06 21:09:43

--- ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
+++ ruby/49cAh2izTnts49Zq8cRnzZfM249HheCcJzWWn249JMh.rb
@@ -1,165 +1,60 @@
 
         
-          def translation_scope
-    'devise.sessions'
-  end
+            # we assume that the first file that requires 'sinatra' is the
+    # app_file. all other path related options are calculated based
+    # on this path by default.
+    set :app_file, caller_files.first || $0
     
-              path
-        end
+      table td.code       {width:750px}
+  table td.code div   {width:750px;overflow:hidden}
+</style>
+</head>
+<body>
+  <div id='wrap'>
+    <div id='header'>
+      <img src='<%= env['SCRIPT_NAME'] %>/__sinatra__/500.png' alt='application error' height='161' width='313' />
+      <div id='summary'>
+        <h1><strong><%=h exception.class %></strong> at <strong><%=h path %>
+          </strong></h1>
+        <h2><%=h exception.message %></h2>
+        <ul>
+          <li class='first'><strong>file:</strong> <code>
+            <%=h frames.first.filename.split('/').last %></code></li>
+          <li><strong>location:</strong> <code><%=h frames.first.function %>
+            </code></li>
+          <li class='last'><strong>line:
+            </strong> <%=h frames.first.lineno %></li>
+        </ul>
+      </div>
+      <div class='clear'></div>
+    </div>
+    
+          def react(env)
+        result = send(options[:reaction], env)
+        result if Array === result and result.size == 3
       end
     
-          def self.generate_helpers!(routes=nil)
-        routes ||= begin
-          mappings = Devise.mappings.values.map(&:used_helpers).flatten.uniq
-          Devise::URL_HELPERS.slice(*mappings)
-        end
-    
-      if record && record.respond_to?(:timedout?) && warden.authenticated?(scope) &&
-     options[:store] != false && !env['devise.skip_timeoutable']
-    last_request_at = warden.session(scope)['last_request_at']
-    
-          def headers_for(action, opts)
-        headers = {
-          subject: subject_for(action),
-          to: resource.email,
-          from: mailer_sender(devise_mapping),
-          reply_to: mailer_reply_to(devise_mapping),
-          template_path: template_paths,
-          template_name: action
-        }.merge(opts)
-    
-          private
-    
-        # Tests if the index has the given UUID.
-    #
-    # @param [String] uuid
-    # @return [Boolean]
-    def include?(uuid)
-      @lock.synchronize do
-        with_index_lock do
-          unlocked_reload
-          return !!find_by_prefix(uuid)
-        end
-      end
-    end
-    
-            # Mounts a shared folder.
-        #
-        # This method should create, mount, and properly set permissions
-        # on the shared folder. This method should also properly
-        # adhere to any configuration values such as `shared_folder_uid`
-        # on `config.vm`.
-        #
-        # @param [String] name The name of the shared folder.
-        # @param [String] guestpath The path on the machine which the user
-        #   wants the folder mounted.
-        # @param [Hash] options Additional options for the shared folder
-        #   which can be honored.
-        def mount_shared_folder(name, guestpath, options)
-          raise BaseError, _key: :unsupported_shared_folder
-        end
-    
-              # Return the registry
-          data[:config]
-        end
-    
-            # This returns all the config classes for the various providers.
-        #
-        # @return [Hash]
-        def provider_configs
-          Registry.new.tap do |result|
-            @registered.each do |plugin|
-              result.merge!(plugin.components.configs[:provider])
-            end
+          def accepts?(env)
+        cookie_header = env['HTTP_COOKIE']
+        cookies = Rack::Utils.parse_query(cookie_header, ';,') { |s| s }
+        cookies.each do |k, v|
+          if k == session_key && Array(v).size > 1
+            bad_cookies << k
+          elsif k != session_key && Rack::Utils.unescape(k) == session_key
+            bad_cookies << k
           end
         end
-    
-            # Defines additional communicators to be available. Communicators
-        # should be returned by a block passed to this method. This is done
-        # to ensure that the class is lazy loaded, so if your class inherits
-        # from or uses any Vagrant internals specific to Vagrant 1.0, then
-        # the plugin can still be defined without breaking anything in future
-        # versions of Vagrant.
-        #
-        # @param [String] name Communicator name.
-        def self.communicator(name=UNSET_VALUE, &block)
-          data[:communicator] ||= Registry.new
-    
-      def pod_prefix
-    File.expand_path('../..', pod_bin)
-  end
-    
-        pod 'ObjCPod', path: 'ObjCPod'
-    pod 'SwiftPod', path: 'SwiftPod'
-    pod 'MixedPod', path: 'MixedPod'
-    pod 'CustomModuleMapPod', path: 'CustomModuleMapPod'
-    
-            def run
-          UI.puts('$CACHE_ROOT: #{@cache.root}') if @short_output
-          if @pod_name.nil? # Print all
-            @cache.cache_descriptors_per_pod.each do |pod_name, cache_descriptors|
-              print_pod_cache_infos(pod_name, cache_descriptors)
-            end
-          else # Print only for the requested pod
-            cache_descriptors = @cache.cache_descriptors_per_pod[@pod_name]
-            if cache_descriptors.nil?
-              UI.notice('No cache for pod named #{@pod_name} found')
-            else
-              print_pod_cache_infos(@pod_name, cache_descriptors)
-            end
-          end
-        end
-    
-          def executable_path
-        <<-EOS
-### Installation Source
-    
-      gem.files         = `git ls-files -z`.split('\x0').reject { |f| f =~ /^docs/ }
-  gem.executables   = %w(cap capify)
-  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
-  gem.require_paths = ['lib']
-    
-        private
-    
-          def select?(options)
-        options.each do |k, v|
-          callable = v.respond_to?(:call) ? v : ->(server) { server.fetch(v) }
-          result = \
-            case k
-            when :filter, :select
-              callable.call(self)
-            when :exclude
-              !callable.call(self)
-            else
-              fetch(k) == v
-            end
-          return false unless result
-        end
-    
-          # Keys that have been set, but which have never been fetched.
-      def unused_keys
-        keys - fetched_keys
+        bad_cookies.empty?
       end
     
-        BUNDLE_DIR = ::File.join(LOGSTASH_HOME, 'vendor', 'bundle')
-    GEMFILE_PATH = ::File.join(LOGSTASH_HOME, 'Gemfile')
-    LOCAL_GEM_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'local_gems')
-    CACHE_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'cache')
-    LOCKFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile.lock'))
-    GEMFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile'))
-    
-      def validate_target_file
-    if File.exist?(target_file)
-      if  delete_target_file?
-        File.delete(target_file)
-      else
-        signal_error('Package creation cancelled, a previously generated package exist at location: #{target_file}, move this file to safe place and run the command again')
-      end
-    end
+      it 'accepts post requests with masked X-CSRF-Token header' do
+    post('/', {}, 'rack.session' => session, 'HTTP_X_CSRF_TOKEN' => masked_token)
+    expect(last_response).to be_ok
   end
     
-      namespace :acceptance do
-    desc 'Run all acceptance'
-    task :all do
-      exit(RSpec::Core::Runner.run([Rake::FileList['acceptance/spec/lib/**/*_spec.rb']]))
+      it 'should allow changing report only' do
+    # I have no clue what other modes are available
+    mock_app do
+      use Rack::Protection::ContentSecurityPolicy, :report_uri => '/my_amazing_csp_report_parser', :report_only => true
+      run DummyApp
     end
