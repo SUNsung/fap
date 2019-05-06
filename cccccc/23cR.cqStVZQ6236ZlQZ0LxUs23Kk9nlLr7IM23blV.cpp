e1095723467bda36105ba2a2cff7c9ad932c94e4
SUNsung <weatherius@gmail.com>
2019-05-06 21:58:22 +0300

    Rewrite [23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp] File in: 2019-05-06 21:58:20

--- cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
+++ cccccc/23cR.cqStVZQ6236ZlQZ0LxUs23Kk9nlLr7IM23blV.cpp
@@ -1,263 +1,136 @@
 
         
-        // TaskRunner implementation that posts tasks into libuv's default loop.
-class UvTaskRunner : public base::SingleThreadTaskRunner {
- public:
-  explicit UvTaskRunner(uv_loop_t* loop);
-    }
-    
-    #include 'atom/browser/api/atom_api_browser_window.h'
-    
-      static void BuildPrototype(v8::Isolate* isolate,
-                             v8::Local<v8::FunctionTemplate> prototype);
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(RenderProcessPreferences);
-};
-    
-    void AutoUpdater::SetDelegate(Delegate* delegate) {
-  delegate_ = delegate;
-}
-    
-    
-    {}  // namespace atom
-    
-    net::URLRequestJob* AboutProtocolHandler::MaybeCreateJob(
-    net::URLRequest* request,
-    net::NetworkDelegate* network_delegate) const {
-  return new URLRequestAboutJob(request, network_delegate);
-}
-    
-      // net::URLRequestJobFactory::ProtocolHandler:
-  net::URLRequestJob* MaybeCreateJob(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const override;
-  bool IsSafeRedirectTarget(const GURL& location) const override;
-    
-    
-bool ZeroCopyOutputStream::WriteAliasedRaw(const void* /* data */,
-                                           int /* size */) {
-  GOOGLE_LOG(FATAL) << 'This ZeroCopyOutputStream doesn't support aliasing. '
-                'Reaching here usually means a ZeroCopyOutputStream '
-                'implementation bug.';
-  return false;
-}
-    
-    void MapLiteTestUtil::ExpectClear(const unittest::TestMapLite& message) {
-  MapTestUtilImpl::ExpectClear(message);
-}
-    
-    TEST(ByteSourceTest, CopyToStringByteSink) {
-  StringPiece data('Hello world!');
-  MockByteSource source(data, 3);
-  string str;
-  StringByteSink sink(&str);
-  source.CopyTo(&sink, data.size());
-  EXPECT_EQ(data, str);
-}
-    
-      while (true) {
-    const void* inptr;
-    int inlen;
-    bool ok;
-    ok = in.Next(&inptr, &inlen);
-    if (!ok) {
-      break;
-    }
-    if (inlen > 0) {
-      int err = write(STDOUT_FILENO, inptr, inlen);
-      if (err != inlen) {
-        fprintf(stderr, 'write unexpectedly returned %d.\n', err);
-        return 1;
-      }
-    }
-  }
-    
-      while (true) {
-    void* outptr;
-    int outlen;
-    bool ok;
-    do {
-      ok = out.Next(&outptr, &outlen);
-      if (!ok) {
-        break;
-      }
-    } while (outlen <= 0);
-    readlen = read(STDIN_FILENO, outptr, outlen);
-    if (readlen <= 0) {
-      out.BackUp(outlen);
-      break;
-    }
-    if (readlen < outlen) {
-      out.BackUp(outlen - readlen);
-    }
-  }
-    
-      EXPECT_EQ(
-      1, TimeUtil::TimestampToNanoseconds(TimeUtil::NanosecondsToTimestamp(1)));
-  EXPECT_EQ(-1, TimeUtil::TimestampToNanoseconds(
-                    TimeUtil::NanosecondsToTimestamp(-1)));
-  EXPECT_EQ(1, TimeUtil::TimestampToMicroseconds(
-                   TimeUtil::MicrosecondsToTimestamp(1)));
-  EXPECT_EQ(-1, TimeUtil::TimestampToMicroseconds(
-                    TimeUtil::MicrosecondsToTimestamp(-1)));
-  EXPECT_EQ(1, TimeUtil::TimestampToMilliseconds(
-                   TimeUtil::MillisecondsToTimestamp(1)));
-  EXPECT_EQ(-1, TimeUtil::TimestampToMilliseconds(
-                    TimeUtil::MillisecondsToTimestamp(-1)));
-  EXPECT_EQ(1, TimeUtil::TimestampToSeconds(TimeUtil::SecondsToTimestamp(1)));
-  EXPECT_EQ(-1, TimeUtil::TimestampToSeconds(TimeUtil::SecondsToTimestamp(-1)));
-    
-    template <typename T1, typename T2, typename T3, typename T4>
-internal::ValueArray4<T1, T2, T3, T4> Values(T1 v1, T2 v2, T3 v3, T4 v4) {
-  return internal::ValueArray4<T1, T2, T3, T4>(v1, v2, v3, v4);
-}
-    
-    
-    {  GTEST_DISALLOW_COPY_AND_ASSIGN_(HasNewFatalFailureHelper);
-};
-    
-      // These fields are immutable properties of the test.
-  const std::string test_case_name_;     // Test case name
-  const std::string name_;               // Test name
-  // Name of the parameter type, or NULL if this is not a typed or a
-  // type-parameterized test.
-  const internal::scoped_ptr<const ::std::string> type_param_;
-  // Text representation of the value parameter, or NULL if this is not a
-  // value-parameterized test.
-  const internal::scoped_ptr<const ::std::string> value_param_;
-  const internal::TypeId fixture_class_id_;   // ID of the test fixture class
-  bool should_run_;                 // True iff this test should run
-  bool is_disabled_;                // True iff this test is disabled
-  bool matches_filter_;             // True if this test matches the
-                                    // user-specified filter.
-  internal::TestFactoryBase* const factory_;  // The factory that creates
-                                              // the test object
-    
-    template <typename T>
-class linked_ptr {
- public:
-  typedef T element_type;
-    }
-    
-    
-    {	LRESULT lr = SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)'Environment', SMTO_ABORTIFHUNG | SMTO_NOTIMEOUTIFNOTHUNG, 5000, NULL);
-	lr = SendMessageTimeout(HWND_BROADCAST, WM_SETTINGCHANGE, 0, (LPARAM)L'Environment', SMTO_ABORTIFHUNG | SMTO_NOTIMEOUTIFNOTHUNG, 5000, NULL); // For Windows >= 8
-}
-    
-            // Inner state of the underlying reader.
-        // Is set in the RestoreFromCheckpoint call and used in the next GetNextMinibatch
-        // when the reader state is restored after the first StartEpoch call.
-        Internal::Optional<Dictionary> m_state;
-    
-        static void* AllocateTensorView(CNTK::DataType dataType,
-                                    const NDShape& viewShape,
-                                    const DeviceDescriptor& device,
-                                    void* dataBuffer,
-                                    size_t bufferSizeInBytes)
-    {
-        switch (dataType)
+        
+    {        auto maskMatrix = GetMatrix();
+        size_t rowOffset = offset[0];
+        size_t colOffset = offset[1];
+        size_t sliceRowLength = (shape[0] != NDShape::InferredDimension) ? shape[0] : (maskMatrix->GetNumRows() - rowOffset);
+        size_t sliceColLength = (shape[1] != NDShape::InferredDimension) ? shape[1] : (maskMatrix->GetNumCols() - colOffset);
+        if ((rowOffset == 0) && (sliceRowLength == maskMatrix->GetNumRows()))
+            maskMatrix->ColumnSlice(colOffset, sliceColLength).SetValue((char)maskKind);
+        else
         {
-        case DataType::Float:
-            return AllocateTensorView<float>(viewShape, device, dataBuffer, bufferSizeInBytes);
-        case DataType::Double:
-            return AllocateTensorView<double>(viewShape, device, dataBuffer, bufferSizeInBytes);
-        case DataType::Float16:
-            return AllocateTensorView<half>(viewShape, device, dataBuffer, bufferSizeInBytes);
-        case DataType::Int8:
-            return AllocateTensorView<char>(viewShape, device, dataBuffer, bufferSizeInBytes);
-        case DataType::Int16:
-            return AllocateTensorView<short>(viewShape, device, dataBuffer, bufferSizeInBytes);
-        default:
-            LogicError('Unsupported DataType %s', DataTypeName(dataType));
-            break;
-        }
-    }
-    
-        void ProgressWriter::WriteTestSummary(const ValuePtr& accumulatedMetric)
-    {
-        m_test->WriteSummary(
-            nullptr, accumulatedMetric,
-            [this](size_t samples, size_t updates, size_t summaries, double /*aggregateLoss*/, double aggregateMetric,
-                uint64_t elapsedMs)
+            // Since Matrix does not support strides in the row dimension, we will need to create separate slices for each column
+            for (size_t i = colOffset; i < (colOffset + sliceColLength); ++i)
             {
-                OnWriteTestSummary(samples, updates, summaries, aggregateMetric, elapsedMs);
-            });
-    }
-    
-            ValuePtr DeepClone(bool readOnly) const override
-        {
-            if (m_isPacked)
-            {
-                std::shared_ptr<Microsoft::MSR::CNTK::MBLayout> packedLayoutCopy;
-                if (m_packedDataLayout)
-                {
-                    packedLayoutCopy = std::make_shared<Microsoft::MSR::CNTK::MBLayout>();
-                    packedLayoutCopy->CopyFrom(m_packedDataLayout);
-                }
-                return MakeSharedObject<PackedValue>(m_sampleShape, m_sampleDynamicAxes, m_packedData->DeepClone(readOnly), packedLayoutCopy, readOnly);
+                auto column = maskMatrix->ColumnSlice(i, 1);
+                column.Reshape(1, maskMatrix->GetNumRows());
+                column.ColumnSlice(rowOffset, sliceRowLength).SetValue((char)maskKind);
             }
-            else
-                return Value::DeepClone(readOnly);
+        }
+    }
+    
+            Dictionary aggregatedState;
+        for (const auto& w : communicator->Workers())
+        {
+            aggregatedState[std::to_wstring(w.m_globalRank)] = *remoteState[w.m_globalRank];
         }
     
-                if ((oldOutputRank != SentinelValueForInferParamInitRank) && (oldOutputRank != outputRank))
-                InvalidArgument('Output rank of a non-uniform random initialier cannot be overridden if it has been already specified!');
+    #include 'stdafx.h'
+#include 'CNTKLibrary.h'
+#include 'Sequences.h'
+#include 'TensorView.h'
+#include 'Utils.h'
     
-    bool DataReaderBase::GetMinibatch(StreamMinibatchInputs& minibatch)
+                if (m_varKind == VariableKind::Input)
+            {
+                for (auto dim : m_shape.Dimensions())
+                {
+                    if (dim == 0)
+                        InvalidArgument('Variable '%S' has invalid shape '%S'.', AsString().c_str(), m_shape.AsString().c_str());
+                }
+            }
+    
+            if (::WaitForSingleObject(m_handle, wait ? INFINITE : 0) != WAIT_OBJECT_0)
+        {
+            // failed to acquire
+            int rc = ::CloseHandle(m_handle);
+            if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
+            {
+                RuntimeError('Acquire: Handler close failure with error code %d', ::GetLastError());
+            }
+            m_handle = NULL;
+            return false;
+        }
+    
+    template <class ElemType>
+TensorView<ElemType> EpochAccumulatorNode<ElemType>::EnsureAccumlator()
 {
-    if (TryGetMinibatch(minibatch))
+    if (m_accumulator->HasNoElements())
     {
-        SetMinibatchLayout(minibatch);
-        return true;
+        // Accumulator has not been resized yet, allocate with necessary size.
+        const size_t sampleSize = GetSampleLayout().GetNumElements();
+        m_accumulator->Resize(sampleSize, 1);
+        Reset();
     }
-    }
-    
-    #pragma once
-    
-            // create a vector with the correct number of timesteps(shapeXT[2]) containing the sequence count (shapeXT[1])
-        numSequencesForFrame = vector<size_t>(shapeXT[2], shapeXT[1]);
-        m_transposedOutput->RNNForward(*m_transposedInput, paramW, shapeXT[0], shapeYT[0], numSequencesForFrame, m_rnnAttributes, *m_reserve, *m_workspace);
-    
-    // Canbus gflags
-DEFINE_double(sensor_freq, 100,
-              'Sensor feedback timer frequency -- 0 means event trigger.');
-    
-      MatrixXd mat_golden(20, 10);
-  // clang-format off
-  mat_golden <<
-    -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-     1,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-     1, -1,  0,  0,  0,  0,  0,  0,  0,  0,
-    -1,  1,  0,  0,  0,  0,  0,  0,  0,  0,
-     0,  1, -1,  0,  0,  0,  0,  0,  0,  0,
-     0, -1,  1,  0,  0,  0,  0,  0,  0,  0,
-     0,  0,  1, -1,  0,  0,  0,  0,  0,  0,
-     0,  0, -1,  1,  0,  0,  0,  0,  0,  0,
-     0,  0,  0,  1, -1,  0,  0,  0,  0,  0,
-     0,  0,  0, -1,  1,  0,  0,  0,  0,  0,
-     0,  0,  0,  0,  1, -1,  0,  0,  0,  0,
-     0,  0,  0,  0, -1,  1,  0,  0,  0,  0,
-     0,  0,  0,  0,  0,  1, -1,  0,  0,  0,
-     0,  0,  0,  0,  0, -1,  1,  0,  0,  0,
-     0,  0,  0,  0,  0,  0,  1, -1,  0,  0,
-     0,  0,  0,  0,  0,  0, -1,  1,  0,  0,
-     0,  0,  0,  0,  0,  0,  0,  1, -1,  0,
-     0,  0,  0,  0,  0,  0,  0, -1,  1,  0,
-     0,  0,  0,  0,  0,  0,  0,  0,  1, -1,
-     0,  0,  0,  0,  0,  0,  0,  0, -1,  1;
-  // clang-format on
-  EXPECT_EQ(mat, mat_golden);
-    
-    
-    {  double length_2 = 50.0;
-  const LaneGraph &lane_graph_2 =
-      ObstacleClusters::GetLaneGraph(start_s, length_2, lane);
-  EXPECT_EQ(1, lane_graph_2.lane_sequence_size());
-  EXPECT_EQ(3, lane_graph_2.lane_sequence(0).lane_segment_size());
-  EXPECT_EQ('l9', lane_graph_2.lane_sequence(0).lane_segment(0).lane_id());
-  EXPECT_EQ('l18', lane_graph_2.lane_sequence(0).lane_segment(1).lane_id());
-  EXPECT_EQ('l21', lane_graph_2.lane_sequence(0).lane_segment(2).lane_id());
+    size_t rank = DetermineElementwiseTensorRank();
+    return DataTensorFor(m_accumulator, rank, FrameRange());
 }
     
-    #include 'gtest/gtest.h'
+    /**
+@brief FlipX3D action.
+@details This action is used for flipping the target node on the x axis.
+*/
+class CC_DLL FlipX3D : public Grid3DAction
+{
+public:
+    /**
+    @brief Create the action with duration.
+    @param duration Specify the duration of the FilpX3D action. It's a value in seconds.
+    @return If the creation success, return a pointer of FilpX3D action; otherwise, return nil.
+    */
+    static FlipX3D* create(float duration);
+    }
+    
+    /** @class ActionManager
+ @brief ActionManager is a singleton that manages all the actions.
+ Normally you won't need to use this singleton directly. 99% of the cases you will use the Node interface,
+ which uses this singleton.
+ But there are some cases where you might need to use this singleton.
+ Examples:
+    - When you want to run an action where the target is different from a Node. 
+    - When you want to pause / resume the actions.
+ 
+ @since v0.8
+ */
+class CC_DLL ActionManager : public Ref
+{
+public:
+    /**
+     * @js ctor
+     */
+    ActionManager();
+    }
+    
+    GridBase* PageTurn3D::getGrid()
+{
+    auto result = Grid3D::create(_gridSize, _gridNodeTarget->getGridRect());
+    if (result)
+    {
+        result->setNeedDepthTestForBlit(true);
+    }
+    
+    return result;
+}
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __CCACTIONTWEEN_H__
+#define __CCACTIONTWEEN_H__
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+#ifndef __CC_ANIMATION_CACHE_H__
+#define __CC_ANIMATION_CACHE_H__
