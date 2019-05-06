f65537a2229e55cbaf0191b8dbe4c4d160689312
SUNsung <weatherius@gmail.com>
2019-05-06 10:27:13 +0300

    Rewrite [352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp] File in: 2019-05-06 10:27:12

--- cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
+++ cccccc/352dk9XKbEZs359EYYB1utTt.35hqzcg4u.eU35MnU.cpp
@@ -1,209 +1,264 @@
 
         
-          // base::SingleThreadTaskRunner:
-  bool PostDelayedTask(const base::Location& from_here,
-                       base::OnceClosure task,
-                       base::TimeDelta delay) override;
-  bool RunsTasksInCurrentSequence() const override;
-  bool PostNonNestableDelayedTask(const base::Location& from_here,
-                                  base::OnceClosure task,
-                                  base::TimeDelta delay) override;
-    
-    
-    {}  // namespace api
+        bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv);
     
     namespace atom {
     }
     
-    #include 'atom/browser/api/trackable_object.h'
-#include 'atom/browser/net/atom_network_delegate.h'
-#include 'native_mate/arguments.h'
-#include 'native_mate/handle.h'
+    class AtomJavaScriptDialogManager : public content::JavaScriptDialogManager {
+ public:
+  explicit AtomJavaScriptDialogManager(api::WebContents* api_web_contents);
+  ~AtomJavaScriptDialogManager() override;
+    }
     
-      int32_t weak_map_id_ = 0;
+    Delegate* AutoUpdater::GetDelegate() {
+  return delegate_;
+}
     
-    bool AboutProtocolHandler::IsSafeRedirectTarget(const GURL& location) const {
-  return false;
+    OffscreenViewProxy::OffscreenViewProxy(views::View* view) : view_(view) {
+  view_bitmap_.reset(new SkBitmap);
+}
+    
+    #include 'atom/browser/relauncher.h'
+    
+    void DragFileItems(const std::vector<base::FilePath>& files,
+                   const gfx::Image& icon,
+                   gfx::NativeView view) {
+  // Set up our OLE machinery
+  ui::OSExchangeData data;
+    }
+    
+    class PlatformStyle;
+class WalletModel;
+    
+    #endif // BITCOIN_REVERSELOCK_H
+
+    
+        mov	SRND, 2
+loop2:
+    paddd	X0, [TBL + 0*16]
+    movdqa	[rsp + _XFER], X0
+    DO_ROUND	0
+    DO_ROUND	1
+    DO_ROUND	2
+    DO_ROUND	3
+    paddd	X1, [TBL + 1*16]
+    movdqa	[rsp + _XFER], X1
+    add	TBL, 2*16
+    DO_ROUND	0
+    DO_ROUND	1
+    DO_ROUND	2
+    DO_ROUND	3
+    
+      iter->Seek('b');
+  ASSERT_EQ(IterStatus(iter), '(invalid)');
+    
+    // Generate param traits write methods.
+#include 'ipc/param_traits_write_macros.h'
+namespace IPC {
+#include 'content/nw/src/common/common_message_generator.h'
+}  // namespace IPC
+    
+    // Create a Shell and returns its routing id.
+IPC_SYNC_MESSAGE_ROUTED2_1(ShellViewHostMsg_CreateShell,
+                           std::string /* url */,
+                           base::DictionaryValue /* manifest */,
+                           int /* result */)
+    
+    #include 'base/basictypes.h'
+#include '../dispatcher_host.h'
+    
+      WebView* view = frame->view();
+  if (!view)
+    return NULL;  // can happen during closing.
+    
+    void Clipboard::SetText(std::string& text) {
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  ui::Clipboard::ObjectMap map;
+  map[ui::Clipboard::CBF_TEXT].push_back(
+      std::vector<char>(text.begin(), text.end()));
+  clipboard->WriteObjects(ui::CLIPBOARD_TYPE_COPY_PASTE, map);
+}
+    
+      if (!item->enable_shortcut_)
+    return false;
+    
+    void Menu::Create(const base::DictionaryValue& option) {
+  gtk_accel_group = NULL;
+  std::string type;
+  if (option.GetString('type', &type) && type == 'menubar')
+    menu_ = gtk_menu_bar_new();
+  else
+    menu_ = gtk_menu_new();
+    }
+    
+    #include 'content/nw/src/api/menuitem/menuitem.h'
+    
+    bool NwAppClearCacheFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  content::BrowsingDataRemover* remover = content::BrowserContext::GetBrowsingDataRemover(
+      Profile::FromBrowserContext(context_));
+    }
+    
+    NwObjDestroyFunction::~NwObjDestroyFunction() {
 }
     
     
-    {}  // namespace atom
-
+    {    private:
+      DISALLOW_COPY_AND_ASSIGN(NwScreenGetScreensFunction);      
+  };
     
-    
-    {}  // namespace atom
-    
-      aura::Window* root_window = view->GetRootWindow();
-  if (!root_window || !aura::client::GetDragDropClient(root_window))
-    return;
-    
-    namespace tesseract {
+    TEST(MovableMessageTest, MoveSameArena) {
+  Arena arena;
     }
     
-      // Sets the destination filename and enables images to be written to a PDF
-  // on destruction.
-  void WritePDF(const char* filename) {
-    if (pixaGetCount(pixa_) > 0) {
-      pixaConvertToPdf(pixa_, 300, 1.0f, 0, 0, 'AllDebugImages', filename);
-      pixaClear(pixa_);
+    // Author: kenton@google.com (Kenton Varda)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+    
+    void MapLiteTestUtil::SetMapFieldsInitialized(unittest::TestMapLite* message) {
+  MapTestUtilImpl::SetMapFieldsInitialized(message);
+}
+    
+    TEST(StatusOr, TestCopyCtorStatusNotOkConverting) {
+  StatusOr<int>    original(Status::CANCELLED);
+  StatusOr<double> copy(original);
+  EXPECT_EQ(original.status(), copy.status());
+}
+    
+    namespace google {
+namespace protobuf {
+namespace internal {
+namespace {
     }
-  }
-    
-    
-    { private:
-  double total_weight;         // no of elements or sum of weights.
-  double sigx;                 // sum of x
-  double sigy;                 // sum of y
-  double sigxx;                // sum x squared
-  double sigxy;                // sum of xy
-  double sigyy;                // sum y squared
-};
-    
-      // Sets up the DENORM to execute a non-linear transformation based on
-  // preserving an even distribution of stroke edges. The transformation
-  // operates only within the given box, scaling input coords within the box
-  // non-linearly to a box of target_width by target_height, with all other
-  // coords being clipped to the box edge. As with SetupNormalization above,
-  // final_xshift and final_yshift are applied after scaling, and the bottom-
-  // left of box is used as a pre-scaling origin.
-  // x_coords is a collection of the x-coords of vertical edges for each
-  // y-coord starting at box.bottom().
-  // y_coords is a collection of the y-coords of horizontal edges for each
-  // x-coord starting at box.left().
-  // Eg x_coords[0] is a collection of the x-coords of edges at y=bottom.
-  // Eg x_coords[1] is a collection of the x-coords of edges at y=bottom + 1.
-  // The second-level vectors must all be sorted in ascending order.
-  void SetupNonLinear(const DENORM* predecessor, const TBOX& box,
-                      float target_width, float target_height,
-                      float final_xshift, float final_yshift,
-                      const GenericVector<GenericVector<int> >& x_coords,
-                      const GenericVector<GenericVector<int> >& y_coords);
-    
-      tesseract::ParagraphJustification justification() const {
-    return justification_;
-  }
-  int margin() const { return margin_; }
-  int first_indent() const { return first_indent_; }
-  int body_indent() const { return body_indent_; }
-  int tolerance() const { return tolerance_; }
-  bool is_flush() const {
-    return (justification_ == tesseract::JUSTIFICATION_LEFT ||
-            justification_ == tesseract::JUSTIFICATION_RIGHT) &&
-        abs(first_indent_ - body_indent_) <= tolerance_;
-  }
-    
-      /* Reject modes generated after NN_ACCEPT but before MM_ACCEPT */
-  R_HYPHEN,        // TEMP Post NN dodgy hyphen or full stop
-  R_DUBIOUS,       // TEMP Post NN dodgy chars
-  R_NO_ALPHANUMS,  // TEMP No alphanumerics in word after NN
-  R_MOSTLY_REJ,    // TEMP Most of word rejected so rej the rest
-  R_XHT_FIXUP,     // TEMP Xht tests unsure
-    
-      bool Read(SparsePage* page,
-            dmlc::SeekStream* fi,
-            const std::vector<bst_uint>& sorted_index_set) override {
-    if (!fi->Read(&disk_offset_)) return false;
-    auto& offset_vec = page->offset.HostVector();
-    auto& data_vec = page->data.HostVector();
-    // setup the offset
-    offset_vec.clear();
-    offset_vec.push_back(0);
-    for (unsigned int fid : sorted_index_set) {
-      CHECK_LT(fid + 1, disk_offset_.size());
-      size_t size = disk_offset_[fid + 1] - disk_offset_[fid];
-      offset_vec.push_back(offset_vec.back() + size);
     }
-    data_vec.resize(offset_vec.back());
-    // read in the data
-    size_t begin = fi->Tell();
-    size_t curr_offset = 0;
-    for (size_t i = 0; i < sorted_index_set.size();) {
-      bst_uint fid = sorted_index_set[i];
-      if (disk_offset_[fid] != curr_offset) {
-        CHECK_GT(disk_offset_[fid], curr_offset);
-        fi->Seek(begin + disk_offset_[fid] * sizeof(Entry));
-        curr_offset = disk_offset_[fid];
+    }
+    }
+    
+    // Author: brianolson@google.com (Brian Olson)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+//
+// Test program to verify that GzipInputStream is compatible with command line
+// gunzip or java.util.zip.GzipInputStream
+//
+// Reads gzip stream on standard input and writes decompressed data to standard
+// output.
+    
+        for (size_t i = 0; i < set_fields.size(); i++) {
+      const FieldDescriptor* field = set_fields[i];
+      if (ShouldBeClear(field)) {
+        reflection->ClearField(message, field);
+        continue;
       }
-      size_t j, size_to_read = 0;
-      for (j = i; j < sorted_index_set.size(); ++j) {
-        if (disk_offset_[sorted_index_set[j]] == disk_offset_[fid] + size_to_read) {
-          size_to_read += offset_vec[j + 1] - offset_vec[j];
+      if (field->type() == FieldDescriptor::TYPE_MESSAGE) {
+        if (field->is_repeated()) {
+          for (int j = 0; j < reflection->FieldSize(*message, field); j++) {
+            StripMessage(reflection->MutableRepeatedMessage(message, field, j));
+          }
         } else {
-          break;
+          StripMessage(reflection->MutableMessage(message, field));
         }
       }
     }
-    }
     
-    struct LambdaRankParam : public dmlc::Parameter<LambdaRankParam> {
-  int num_pairsample;
-  float fix_list_weight;
-  // declare parameters
-  DMLC_DECLARE_PARAMETER(LambdaRankParam) {
-    DMLC_DECLARE_FIELD(num_pairsample).set_lower_bound(1).set_default(1)
-        .describe('Number of pair generated for each instance.');
-    DMLC_DECLARE_FIELD(fix_list_weight).set_lower_bound(0.0f).set_default(0.0f)
-        .describe('Normalize the weight of each list by this value,'
-                  ' if equals 0, no effect will happen');
+    std::string ReadFile(const std::string& name) {
+  std::ifstream file(name.c_str());
+  GOOGLE_CHECK(file.is_open()) << 'Couldn't find file ''
+      << name
+      << '', please make sure you are running this command from the benchmarks'
+      << ' directory.\n';
+  return std::string((std::istreambuf_iterator<char>(file)),
+                     std::istreambuf_iterator<char>());
+}
+    
+        std::vector<std::pair<string,string>> option_pairs;
+    ParseGeneratorParameter(parameter, &option_pairs);
+    
+    
+    {
+    {    cout << 'Is this a mobile, home, or work phone? ';
+    string type;
+    getline(cin, type);
+    if (type == 'mobile') {
+      phone_number->set_type(tutorial::Person::MOBILE);
+    } else if (type == 'home') {
+      phone_number->set_type(tutorial::Person::HOME);
+    } else if (type == 'work') {
+      phone_number->set_type(tutorial::Person::WORK);
+    } else {
+      cout << 'Unknown phone type.  Using default.' << endl;
+    }
   }
-};
-    
-    TEST(c_api, XGDMatrixCreateFromMat_omp) {
-  std::vector<int> num_rows = {100, 11374, 15000};
-  for (auto row : num_rows) {
-    int num_cols = 50;
-    int num_missing = 5;
-    DMatrixHandle handle;
-    std::vector<float> data(num_cols * row, 1.5);
-    for (int i = 0; i < num_missing; i++) {
-      data[i] = std::numeric_limits<float>::quiet_NaN();
-    }
-    }
-    }
-    
-        inline void producer_remove(Coroutine *co)
-    {
-        producer_queue.remove(co);
-    }
-    
-        cache.set('test2', val2);
-    ASSERT_EQ(cache.get('test2').get(), val2.get());
-    val2.reset();
-    ASSERT_EQ(dtor_num, 1);
-    ASSERT_EQ(cache.get('test'), nullptr);
-    
-    
-    {
-    {    inline void clear()
-    {
-        cache_list.clear();
-        cache_map.clear();
-    }
-};
+  *person->mutable_last_updated() = TimeUtil::SecondsToTimestamp(time(NULL));
 }
     
-    TEST(coroutine_gethostbyname, resolve_without_cache)
-{
-    coro_test([](void *arg)
+    
     {
-        set_dns_cache_capacity(0);
-    }
-    }
-    
-    TEST(coroutine_channel, pop_yield)
-{
-    Channel chan(1);
-    }
-    
-    using namespace swoole;
-    
-    static void timer1(swTimer *timer, swTimer_node *tnode)
-{
-    static int count = 0;
-    printf('#%d\thello world\n', count);
-    if (count > 9)
     {
-        swoole_timer_clear(tnode->id);
+    {      switch (phone_number.type()) {
+        case tutorial::Person::MOBILE:
+          cout << '  Mobile phone #: ';
+          break;
+        case tutorial::Person::HOME:
+          cout << '  Home phone #: ';
+          break;
+        case tutorial::Person::WORK:
+          cout << '  Work phone #: ';
+          break;
+        default:
+          cout << '  Unknown phone #: ';
+          break;
+      }
+      cout << phone_number.number() << endl;
     }
-    count++;
+    if (person.has_last_updated()) {
+      cout << '  Updated: ' << TimeUtil::ToString(person.last_updated()) << endl;
+    }
+  }
 }
+    
+      // An enumeration of the three reasons that a test might be aborted.
+  enum AbortReason {
+    TEST_ENCOUNTERED_RETURN_STATEMENT,
+    TEST_THREW_EXCEPTION,
+    TEST_DID_NOT_DIE
+  };
+    
+      // RemoveFileName returns the directory path with the filename removed.
+  // Example: FilePath('path/to/file').RemoveFileName() returns 'path/to/'.
+  // If the FilePath is 'a_file' or '/a_file', RemoveFileName returns
+  // FilePath('./') or, on Windows, FilePath('.\\'). If the filepath does
+  // not have a file, like 'just/a/dir/', it returns the FilePath unmodified.
+  // On Windows platform, '\' is the path separator, otherwise it is '/'.
+  FilePath RemoveFileName() const;
+    
+    #include <algorithm>
+    
+      // Trivial case 2: even numbers
+  if (n % 2 == 0) return n == 2;
+    
+        bool Trainer::TrainMinibatch(const std::unordered_map<Variable, ValuePtr>& arguments, bool isSweepEndInArguments, std::unordered_map<Variable, ValuePtr>& outputsToFetch, const DeviceDescriptor& computeDevice /*= DeviceDescriptor::UseDefaultDevice()*/)
+    {
+#ifndef  CNTK_UWP
+        auto profMinibatch = Microsoft::MSR::CNTK::ScopeProfile(Microsoft::MSR::CNTK::profilerEvtMainMinibatch);
+#endif
+    }
+    
+                if ((oldOutputRank != SentinelValueForInferParamInitRank) && (oldOutputRank != outputRank))
+                InvalidArgument('Output rank of a non-uniform random initialier cannot be overridden if it has been already specified!');
+    
+            std::wstring AsString() const;
+        std::shared_ptr<VariableFields> Clone() const;
+        FunctionPtr Owner() const;
+    
+    namespace Microsoft { namespace MSR { namespace CNTK {
+    }
+    }
+    }
+    
+    class Clock
+{
+public:
+    static long long GetTimeStamp();
+    static long long GetTicksPerSecond();
+};
