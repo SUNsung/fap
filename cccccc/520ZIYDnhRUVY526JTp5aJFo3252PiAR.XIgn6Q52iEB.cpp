f3751ef510fefa6d72c636b6fc0a9c3712d91fba
SUNsung <weatherius@gmail.com>
2019-05-07 03:48:36 +0300

    Rewrite [520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp] File in: 2019-05-07 03:48:36

--- cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
+++ cccccc/520ZIYDnhRUVY526JTp5aJFo3252PiAR.XIgn6Q52iEB.cpp
@@ -1,212 +1,45 @@
 
         
-        // Given a MutableIterator to the start of a block, run DetectParagraphs on
-// that block and commit the results to the underlying ROW and BLOCK structs,
-// saving the ParagraphModels in models.  Caller owns the models.
-// We use unicharset during the function to answer questions such as 'is the
-// first letter of this word upper case?'
-void DetectParagraphs(int debug_level,
-                      bool after_text_recognition,
-                      const MutableIterator *block_start,
-                      GenericVector<ParagraphModel *> *models);
+        // Create a Shell and returns its routing id.
+IPC_SYNC_MESSAGE_ROUTED2_1(ShellViewHostMsg_CreateShell,
+                           std::string /* url */,
+                           base::DictionaryValue /* manifest */,
+                           int /* result */)
     
-    // Possible normalization methods. Use NEGATIVE values as these also
-// double up as markers for the last sub-classifier.
-enum NormalizationMode {
-  NM_BASELINE = -3,         // The original BL normalization mode.
-  NM_CHAR_ISOTROPIC = -2,   // Character normalization but isotropic.
-  NM_CHAR_ANISOTROPIC = -1  // The original CN normalization mode.
+    
+    {  DISALLOW_COPY_AND_ASSIGN(Base);
 };
     
-    Basically, there is a flag for each sort of rejection or acceptance. This
-provides a history of what has happened to EACH character.
+    Menu::Menu(int id,
+           const base::WeakPtr<ObjectManager>& object_manager,
+           const base::DictionaryValue& option,
+           const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id), enable_show_event_(false)  {
+  Create(option);
+}
     
     
-    {} // namespace aria2
+    {}  // namespace nwapi
 
     
-      bool addGoodNode(const std::shared_ptr<DHTNode>& node);
-    
-    
-    {} // namespace aria2
-
-    
-    
-    {} // namespace aria2
-    
-    DHTTaskQueueImpl::DHTTaskQueueImpl()
-    : periodicTaskQueue1_(NUM_CONCURRENT_TASK),
-      periodicTaskQueue2_(NUM_CONCURRENT_TASK),
-      immediateTaskQueue_(NUM_CONCURRENT_TASK)
-{
+    void MenuItem::SetLabel(const std::string& label) {
+  label_ = label;
+  gtk_menu_item_set_label(GTK_MENU_ITEM(menu_item_), label.c_str());
 }
     
-      void put(const std::string& hostname, const std::string& ipaddr,
-           uint16_t port);
+      content::RenderProcessHost* render_process_host = GetSenderWebContents()->GetMainFrame()->GetProcess();
+  net::URLRequestContextGetter* context_getter =
+    render_process_host->GetStoragePartition()->GetURLRequestContext();
     
-      while(true) {
-    if(!this->sync_.getPending(fname, mtype, rseqid)) {
-      iprot_->readMessageBegin(fname, mtype, rseqid);
+    
+    {  DECLARE_EXTENSION_FUNCTION('nw.App.clearAppCache', UNKNOWN)
+ private:
+  DISALLOW_COPY_AND_ASSIGN(NwAppClearAppCacheFunction);
+};
+    
+      // implement nw.Screen.initEventListeners()
+  class NwScreenInitEventListenersFunction: public NWSyncExtensionFunction {
+    public:
+      NwScreenInitEventListenersFunction();
+      bool RunNWSync(base::ListValue* response, std::string* error) override;
     }
-    if(seqid == rseqid) {
-      if (mtype == ::apache::thrift::protocol::T_EXCEPTION) {
-        ::apache::thrift::TApplicationException x;
-        x.read(iprot_);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-        sentry.commit();
-        throw x;
-      }
-      if (mtype != ::apache::thrift::protocol::T_REPLY) {
-        iprot_->skip(::apache::thrift::protocol::T_STRUCT);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-      }
-      if (fname.compare('registerExtension') != 0) {
-        iprot_->skip(::apache::thrift::protocol::T_STRUCT);
-        iprot_->readMessageEnd();
-        iprot_->getTransport()->readEnd();
-    }
-    }
-    }
-    
-    
-    {};
-    
-    extern const osqueryConstants g_osquery_constants;
-    
-    /*
-** Read input from *in and process it.  If *in==0 then input
-** is interactive - the user is typing it it.  Otherwise, input
-** is coming from a file or device.  A prompt is issued and history
-** is saved only if input is interactive.  An interrupt signal will
-** cause this routine to exit immediately, unless input is interactive.
-**
-** Return the number of errors.
-*/
-static int process_input(struct callback_data* p, FILE* in) {
-  /* A single input line */
-  char* zLine = nullptr;
-    }
-    
-    
-    {
-    {} // namespace table_tests
-} // namespace osquery
-
-    
-    namespace osquery {
-namespace table_tests {
-    }
-    }
-    
-    TEST_F(KernelModules, test_sanity) {
-  QueryData data = execute_query('select * from kernel_modules');
-  ASSERT_GT(data.size(), 0ul);
-  ValidatatioMap row_map = {
-      {'name', NonEmptyString},
-      {'size', NonNegativeInt},
-      {'used_by', NonEmptyString},
-      {'status', NonEmptyString},
-      {'address', NonNegativeInt},
-  };
-  validate_rows(data, row_map);
-}
-    
-    /** @class Speed
- * @brief Changes the speed of an action, making it take longer (speed>1)
- * or shorter (speed<1) time.
- * Useful to simulate 'slow motion' or 'fast forward' effect.
- * @warning This action can't be Sequenceable because it is not an IntervalAction.
- */
-class CC_DLL Speed : public Action
-{
-public:
-    /** Create the action and set the speed.
-     *
-     * @param action An action.
-     * @param speed The action speed.
-     */
-    static Speed* create(ActionInterval* action, float speed);
-    /** Return the speed.
-     *
-     * @return The action speed.
-     */
-    float getSpeed() const { return _speed; }
-    /** Alter the speed of the inner function in runtime. 
-     *
-     * @param speed Alter the speed of the inner function in runtime.
-     */
-    void setSpeed(float speed) { _speed = speed; }
-    }
-    
-    void PointArray::addControlPoint(const Vec2& controlPoint)
-{    
-    _controlPoints.push_back(controlPoint);
-}
-    
-    /** @class Blink
- * @brief Blinks a Node object by modifying it's visible attribute.
-*/
-class CC_DLL Blink : public ActionInterval
-{
-public:
-    /** 
-     * Creates the action.
-     * @param duration Duration time, in seconds.
-     * @param blinks Blink times.
-     * @return An autoreleased Blink object.
-     */
-    static Blink* create(float duration, int blinks);
-    }
-    
-    
-    {
-    {
-    {            if (action->getTag() == (int)tag && action->getOriginalTarget() == target)
-            {
-                removeActionAtIndex(i, element);
-                --limit;
-            }
-            else
-            {
-                ++i;
-            }
-        }
-    }
-}
-    
-    /**
- @brief This action simulates a page turn from the bottom right hand corner of the screen.
- 
- @details It's not much use by itself but is used by the PageTurnTransition.
-         Based on an original paper by L Hong et al.
-         http://www.parc.com/publication/1638/turning-pages-of-3d-electronic-books.html
-  
- @since v0.8.2
- */
-class CC_DLL PageTurn3D : public Grid3DAction
-{
-public:
-    /**
-     * @js NA 
-     */
-    virtual GridBase* getGrid() override;
-    }
-    
-        // Overrides
-	virtual TurnOffTiles* clone() const override;
-    virtual void startWithTarget(Node *target) override;
-    virtual void update(float time) override;
-    
-CC_CONSTRUCTOR_ACCESS:
-    TurnOffTiles() {}
-    virtual ~TurnOffTiles();
-    
-     Another example: ScaleTo action could be rewritten using PropertyAction:
-    
-            if ( frameNames.empty() )
-        {
-            CCLOG('cocos2d: AnimationCache: Animation '%s' found in dictionary without any frames - cannot add to animation cache.', anim.first.c_str());
-            continue;
-        }
