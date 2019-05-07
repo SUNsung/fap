82759910b8de7424d57d62c7dc7714c6809e1dfc
SUNsung <weatherius@gmail.com>
2019-05-07 22:32:46 +0300

    Rewrite [00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp] File in: 2019-05-07 22:32:45

--- cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
+++ cccccc/00iGJQ.B9rTbk00xaeb8Nyp.020034J8mDYXON.00y2v.cpp
@@ -1,85 +1,234 @@
 
         
-        U_NAMESPACE_BEGIN
+            if (pStartupInfo)
+    {
+        size_t attrListSize{};
+    }
     
-    class U_I18N_API SharedNumberFormat : public SharedObject {
-public:
-    SharedNumberFormat(NumberFormat *nfToAdopt) : ptr(nfToAdopt) { }
-    virtual ~SharedNumberFormat();
-    const NumberFormat *get() const { return ptr; }
-    const NumberFormat *operator->() const { return ptr; }
-    const NumberFormat &operator*() const { return *ptr; }
-private:
-    NumberFormat *ptr;
-    SharedNumberFormat(const SharedNumberFormat &);
-    SharedNumberFormat &operator=(const SharedNumberFormat &);
-};
+      void PopulateCache() {
+    Random rnd(1);
+    for (int64_t i = 0; i < FLAGS_cache_size; i++) {
+      uint64_t rand_key = rnd.Next() % FLAGS_max_key;
+      // Cast uint64* to be char*, data would be copied to cache
+      Slice key(reinterpret_cast<char*>(&rand_key), 8);
+      // do insert
+      cache_->Insert(key, new char[10], 1, &deleter);
+    }
+  }
     
-    /**
- * A representation an acceptable range of digit counts for integers.
+    #pragma once
+    
+     private:
+  uint64_t NowMicrosMonotonic(Env* env);
+    
+      // Create column family, and rocksdb will persist the options.
+  ColumnFamilyHandle* cf;
+  s = db->CreateColumnFamily(ColumnFamilyOptions(), 'new_cf', &cf);
+  assert(s.ok());
+    
+      {
+    std::string string_val;
+    // If it cannot pin the value, it copies the value to its internal buffer.
+    // The intenral buffer could be set during construction.
+    PinnableSlice pinnable_val(&string_val);
+    db->Get(ReadOptions(), db->DefaultColumnFamily(), 'key2', &pinnable_val);
+    assert(pinnable_val == 'value');
+    // If the value is not pinned, the internal buffer must have the value.
+    assert(pinnable_val.IsPinned() || string_val == 'value');
+  }
+    
+    /*
+ * Class:     org_rocksdb_BackupableDBOptions
+ * Method:    setSync
+ * Signature: (JZ)V
  */
-class U_I18N_API IntDigitCountRange : public UMemory {
-public:
-    /**
-     * No constraints: 0 up to INT32_MAX
-     */
-    IntDigitCountRange() : fMin(0), fMax(INT32_MAX) { }
-    IntDigitCountRange(int32_t min, int32_t max);
-    int32_t pin(int32_t digitCount) const;
-    int32_t getMax() const { return fMax; }
-    int32_t getMin() const { return fMin; }
-private:
-    int32_t fMin;
-    int32_t fMax;
+void Java_org_rocksdb_BackupableDBOptions_setSync(JNIEnv* /*env*/,
+                                                  jobject /*jobj*/,
+                                                  jlong jhandle,
+                                                  jboolean flag) {
+  auto* bopt = reinterpret_cast<rocksdb::BackupableDBOptions*>(jhandle);
+  bopt->sync = flag;
+}
+    
+    
+    { protected:
+  SpeedLimit speed_limit_;
 };
     
-            // Fallback schedule:
-        // Midnight/Noon -> General Periods -> AM/PM.
+    const PolynomialXd& Spline1dSeg::ThirdOrderDerivative() const {
+  return third_order_derivative_;
+}
     
-    U_NAMESPACE_END
-    
-        /**
-     * Sets U_ILLEGAL_ARGUMENT_ERROR if the keyword is not a plural form.
-     *
-     * @param keyword for example 'few' or 'other'
-     * @return the plural form corresponding to the keyword
-     */
-    static Form fromString(const UnicodeString &keyword, UErrorCode &errorCode) {
-        return static_cast<Form>(indexFromString(keyword, errorCode));
+    void SplineSegKernel::CalculateThirdOrderDerivative(const uint32_t num_params) {
+  kernel_third_order_derivative_ =
+      Eigen::MatrixXd::Zero(num_params, num_params);
+  for (int r = 3; r < kernel_third_order_derivative_.rows(); ++r) {
+    for (int c = 3; c < kernel_third_order_derivative_.cols(); ++c) {
+      kernel_third_order_derivative_(r, c) =
+          (r * r - r) * (r - 2) * (c * c - c) * (c - 2) / (r + c - 5.0);
     }
-    
-        /**
-     * UnicodeFunctor API.  Cast 'this' to a UnicodeMatcher* pointer
-     * and return the pointer.
-     * @return the UnicodeMatcher point.
-     */
-    virtual UnicodeMatcher* toMatcher() const;
-    
-    #include 'modules/canbus/proto/chassis_detail.pb.h'
-#include 'modules/drivers/canbus/can_comm/protocol_data.h'
-    
-    double ClusterGeneralInfo701::longitude_dist(const std::uint8_t* bytes,
-                                             int32_t length) const {
-  Byte t0(bytes + 1);
-  uint32_t x = t0.get_byte(0, 8);
-    }
-    
-      MatrixXd offset_golden(10, 1);
-  offset_golden << 0, 0, -80, 0, -160, 0, -240, 0, 0, 0;
+  }
+}
     
     namespace apollo {
-namespace prediction {
+namespace canbus {
+namespace gem {
+    }
     }
     }
     
-    void Brakerpt6c::Parse(const std::uint8_t* bytes, int32_t length,
-                       ChassisDetail* chassis) const {
-  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_manual_input(
-      manual_input(bytes, length));
-  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_commanded_value(
-      commanded_value(bytes, length));
-  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_output_value(
+    // config detail: {'name': 'shaft_position', 'offset': 0.0, 'precision': 0.001,
+// 'len': 32, 'is_signed_var': True, 'physical_range':
+// '[-2147483.648|2147483.647]', 'bit': 39, 'type': 'double', 'order':
+// 'motorola', 'physical_unit': 'radians'}
+double Brakemotorrpt170::shaft_position(const std::uint8_t* bytes,
+                                        int32_t length) const {
+  Byte t0(bytes + 4);
+  int32_t x = t0.get_byte(0, 8);
+    }
+    
+    #include 'modules/canbus/vehicle/gem/protocol/global_rpt_6a.h'
+    
+    void Headlightrpt77::Parse(const std::uint8_t* bytes, int32_t length,
+                           ChassisDetail* chassis) const {
+  chassis->mutable_gem()->mutable_headlight_rpt_77()->set_output_value(
       output_value(bytes, length));
-  chassis->mutable_gem()->mutable_brake_rpt_6c()->set_brake_on_off(
-      brake_on_off(bytes, length));
+  chassis->mutable_gem()->mutable_headlight_rpt_77()->set_manual_input(
+      manual_input(bytes, length));
+  chassis->mutable_gem()->mutable_headlight_rpt_77()->set_commanded_value(
+      commanded_value(bytes, length));
 }
+    
+        for (i = 1; i < 1024; i++)
+    {
+        uint32_t key = (rand() % (20000 * 37));
+        if (key % 37 == 0)
+        {
+            continue;
+        }
+        int ret = (int) (long) swRbtree_find(tree, key);
+        ASSERT_EQ(ret, 0);
+    }
+    
+            void cleanup() {
+            delRead();
+            delWrite();
+        }
+    
+        public:
+        ExampleQt(const char * value, QObject * parent = 0)
+            : QObject(parent), m_value(value) {}
+    
+    using namespace swoole;
+using namespace std;
+    
+        SwooleG.main_reactor = (swReactor *) sw_malloc(sizeof(swReactor));
+    swReactor_create(SwooleG.main_reactor, SW_REACTOR_MAXEVENTS);
+    
+    TEST(coroutine_base, get_current)
+{
+    long _cid;
+    long cid = Coroutine::create([](void *arg)
+    {
+        auto co = Coroutine::get_current();
+        *(long *) arg = co->get_cid();
+    }, &_cid);
+    }
+    
+    TEST(os_wait, waitpid_after_child_exit)
+{
+    coro_test([](void *arg)
+    {
+        swoole_coroutine_signal_init();
+    }
+    }
+    
+        unlink(sock1_path);
+    unlink(sock2_path);
+    
+    static swThreadPool pool;
+static int _pipe;
+const static int N = 10000;
+    
+        swListenPort *port = swServer_add_port(&serv, SW_SOCK_TCP, '127.0.0.1', 9501);
+    port->open_eof_check = 0;
+    //config
+    port->backlog = 128;
+    memcpy(port->protocol.package_eof, SW_STRL('\r\n\r\n'));  //开启eof检测，启用buffer区
+    
+    THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+    
+        // Overrides
+    OrbitCamera *clone() const override;
+    virtual void startWithTarget(Node *target) override;
+    virtual void update(float time) override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    /**
+     * @js ctor
+     */
+    OrbitCamera();
+    /**
+     * @js NA
+     * @lua NA
+     */
+    virtual ~OrbitCamera();
+    
+    /** Initializes a OrbitCamera action with radius, delta-radius,  z, deltaZ, x, deltaX. */
+    bool initWithDuration(float t, float radius, float deltaRadius, float angleZ, float deltaAngleZ, float angleX, float deltaAngleX);
+    
+        //
+    // Overrides
+    //
+    virtual TintBy* clone() const override;
+    virtual TintBy* reverse() const override;
+    virtual void startWithTarget(Node *target) override;
+    /**
+     * @param time In seconds.
+     */
+    virtual void update(float time) override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    TintBy() {}
+    virtual ~TintBy() {}
+    
+    // FIXME: Passing 'const O *' instead of 'const O&' because HASH_FIND_IT requires the address of a pointer
+// and, it is not possible to get the address of a reference
+size_t ActionManager::getNumberOfRunningActionsInTargetByTag(const Node *target,
+                                                             int tag)
+{
+    CCASSERT(tag != Action::INVALID_TAG, 'Invalid tag value!');
+    }
+    
+        /* Creates an animation with an array of AnimationFrame, the delay per units in seconds and how many times it should be executed.
+     * @since v2.0
+     * @param arrayOfAnimationFrameNames An animation with an array of AnimationFrame.
+     * @param delayPerUnit The delay per units in seconds and how many times it should be executed.
+     * @param loops The times the animation is going to loop.
+     */
+    static Animation* create(const Vector<AnimationFrame*>& arrayOfAnimationFrameNames, float delayPerUnit, unsigned int loops = 1);
+    
+    #include <string>
+    
+                    
+            case 8:
+            case 10:
+            case 11:
+                /* going DOWN with these cases:
+                 8          10          11
+                 +---+---+  +---+---+   +---+---+
+                 |   |   |  |   | 2 |   | 1 | 2 |
+                 +---+---+  +---+---+   +---+---+
+                 |   | 8 |  |   | 8 |   |   | 8 |
+                 +---+---+  +---+---+  	+---+---+
+                 */
+                stepx = 0;
+                stepy = 1;
+                break;
