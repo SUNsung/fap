b0496e7bdb6ffe5c45e0ff77c8a45b2ac78b7e28
SUNsung <weatherius@gmail.com>
2019-05-08 01:47:09 +0300

    Rewrite [24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb] File in: 2019-05-08 01:47:08

--- ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
+++ ruby/24KvFr6avlgQ6249KhGcDKz4c24FvSvjbWBORo24J7x.rb
@@ -1,137 +1,36 @@
 
         
-            it 'shows the dry run pop up without previous events and selects the events tab when a event was created' do
-      open_dry_run_modal(agent)
-      click_on('Dry Run')
-      expect(page).to have_text('Biologists play reverse')
-      expect(page).to have_selector(:css, 'li[role='presentation'].active a[href='#tabEvents']')
-    end
-    
-            it 'adds errors when updated agents are invalid' do
-          scenario_import.merges = {
-            '0' => {
-              'name' => '',
-              'schedule' => 'foo',
-              'keep_events_for' => 2.days.to_i.to_s,
-              'options' => weather_agent_options.merge('api_key' => '').to_json
-            }
-          }
-    
-      describe '#recursively_interpolate_jsonpaths' do
-    it 'interpolates all string values in a structure' do
-      struct = {
-        :int => 5,
-        :string => 'this <escape $.works>',
-        :array => ['<works>', 'now', '<$.there.world>'],
-        :deep => {
-          :string => 'hello <there.world>',
-          :hello => :world
-        }
-      }
-      data = { :there => { :world => 'WORLD' }, :works => 'should work' }
-      expect(Utils.recursively_interpolate_jsonpaths(struct, data)).to eq({
-        :int => 5,
-        :string => 'this should+work',
-        :array => ['should work', 'now', 'WORLD'],
-        :deep => {
-          :string => 'hello WORLD',
-          :hello => :world
-        }
-      })
-    end
-  end
-    
-    def mkpieces
-  piece(0,[],[0])
-  $p.each do |a|
-    a0 = a[0]
-    a[1] = ud(a0)
-    a[2] = rl(a0)
-    a[3] = ud(rl(a0))
-    a[4] = xy(a0)
-    a[5] = ud(xy(a0))
-    a[6] = rl(xy(a0))
-    a[7] = ud(rl(xy(a0)))
-    a.sort!
-    a.uniq!
-  end
-  $p.uniq!.sort! {|x,y| x[0] <=> y[0] }
+        def version
+  Jekyll::VERSION
 end
     
-        class Main < Clamp::Command
-      subcommand 'list', 'List all installed Logstash plugins', LogStash::PluginManager::List
-      subcommand 'install', 'Install a Logstash plugin', LogStash::PluginManager::Install
-      subcommand 'remove', 'Remove a Logstash plugin', LogStash::PluginManager::Remove
-      subcommand 'update', 'Update a plugin', LogStash::PluginManager::Update
-      subcommand 'pack', 'Package currently installed plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Pack
-      subcommand 'unpack', 'Unpack packaged plugins, Deprecated: Please use prepare-offline-pack instead', LogStash::PluginManager::Unpack
-      subcommand 'generate', 'Create the foundation for a new plugin', LogStash::PluginManager::Generate
-      subcommand 'uninstall', 'Uninstall a plugin. Deprecated: Please use remove instead', LogStash::PluginManager::Remove
-      subcommand 'prepare-offline-pack', 'Create an archive of specified plugins to use for offline installation', LogStash::PluginManager::PrepareOfflinePack
-    end
+            UI.message('Adding git tag '#{tag}' 🎯.')
+        Actions.sh(cmd.join(' '))
+      end
+    
+    # Here be monkey patches
+    
+    # remove all double quotes completely
+def simulate_normal_shell_unwrapping(string)
+  string.gsub!(''', '')
+  regex = /^(')(\S*)(')$/
+  unless string.to_s.match(regex).nil?
+    string = string.to_s.match(regex)[2] # get only part in quotes
   end
+  return string
 end
     
-        class << self
-      def elastic_pack_base_uri
-        env_url = ENV['LOGSTASH_PACK_URL']
-        (env_url.nil? || env_url.empty?) ? DEFAULT_PACK_URL : env_url
-      end
+      zap trash: [
+               '~/Library/Application Support/Adobe/Lightroom',
+               '~/Library/Preferences/com.adobe.Lightroom#{version.major}.plist',
+             ]
     
-    class LogStash::PluginManager::Unpack < LogStash::PluginManager::PackCommand
-  option '--tgz', :flag, 'unpack a packaged tar.gz file', :default => !LogStash::Environment.windows?
-  option '--zip', :flag, 'unpack a packaged  zip file', :default => LogStash::Environment.windows?
-    
-      it 'returns the source' do
-    expect(subject.source).to eq(source)
+      def commands_match
+    @actual.commands == commands
   end
     
-        context 'update all the plugins' do
-      it 'has executed successfully' do
-        logstash.run_command_in_path('bin/logstash-plugin update --no-verify')
-        expect(logstash).to have_installed?(plugin_name, '0.1.1')
-      end
-    end
-  end
-end
-
+          private
     
-          def field_container(model, method, options = {}, &block)
-        css_classes = options[:class].to_a
-        css_classes << 'field'
-        css_classes << 'withError' if error_message_on(model, method).present?
-        content_tag(
-          :div, capture(&block),
-          options.merge(class: css_classes.join(' '), id: '#{model}_#{method}_field')
-        )
-      end
-    
-        context 'Shipment edit page' do
-      let!(:stock_location2) { create(:stock_location_with_items, name: 'Clarksville') }
-    
-          @@variant_attributes = [
-        :id, :name, :sku, :price, :weight, :height, :width, :depth, :is_master,
-        :slug, :description, :track_inventory
-      ]
-    
-    ENV['RAILS_ENV'] = 'test'
-    
-        def cropping dst, ratio, scale
-      if ratio.horizontal? || ratio.square?
-        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
-      else
-        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
-      end
-    end
-    
-            if attachment.nil?
-          attachment = Attachment.new(name, self, options)
-          instance_variable_set(ivar, attachment)
-        end
-    
-        # Hash assignment of interpolations. Included only for compatibility,
-    # and is not intended for normal use.
-    def self.[]= name, block
-      define_method(name, &block)
-      @interpolators_cache = nil
+          expect(described_class.configs).
+        to eq ['both', 'both', 'dup/local-dup', 'home', 'local-dup', 'xdg']
     end
