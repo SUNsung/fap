d8fe8495330e9b3af56e0f322ae6691d2f5c60cf
SUNsung <weatherius@gmail.com>
2019-05-08 17:54:47 +0300

    Rewrite [08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp] File in: 2019-05-08 17:54:47

--- cccccc/08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp
+++ cccccc/08lI11vQuCkRc08szfp3suib9I0851yhYXVPuXs08Ni..cpp
@@ -1,209 +1,23 @@
 
         
-          reqToSyntheticEnvMap = SubstitutionMap::get(reqSig,
-    [selfType, substConcreteType, depth, covariantSelf, &ctx]
-    (SubstitutableType *type) -> Type {
-      // If the conforming type is a class, the protocol 'Self' maps to
-      // the class-constrained 'Self'. Otherwise, it maps to the concrete
-      // type.
-      if (type->isEqual(selfType)) {
-        if (covariantSelf)
-          return GenericTypeParamType::get(/*depth=*/0, /*index=*/0, ctx);
-        return substConcreteType;
-      }
-      // Other requirement generic parameters map 1:1 with their depth
-      // increased appropriately.
-      auto *genericParam = cast<GenericTypeParamType>(type);
-      // In a protocol requirement, the only generic parameter at depth 0
-      // should be 'Self', and all others at depth 1. Anything else is
-      // invalid code.
-      if (genericParam->getDepth() != 1)
-        return Type();
-      auto substGenericParam =
-        GenericTypeParamType::get(depth, genericParam->getIndex(), ctx);
-      return substGenericParam;
-    },
-    [selfType, substConcreteType, conformance, conformanceDC, &ctx](
-        CanType type, Type replacement, ProtocolDecl *proto)
-          -> Optional<ProtocolConformanceRef> {
-      // The protocol 'Self' conforms concretely to the conforming type.
-      if (type->isEqual(selfType)) {
-        ProtocolConformance *specialized = conformance;
-        if (conformance && conformance->getGenericSignature()) {
-          auto concreteSubs =
-            substConcreteType->getContextSubstitutionMap(
-              conformanceDC->getParentModule(), conformanceDC);
-          specialized =
-            ctx.getSpecializedConformance(substConcreteType, conformance,
-                                          concreteSubs);
+        
+    {            return handlePrev(matchindex, status); 
         }
+    
+    U_NAMESPACE_END
+    
+    SharedBreakIterator::SharedBreakIterator(
+        BreakIterator *biToAdopt) : ptr(biToAdopt) { }
+    
+    #include 'smallintformatter.h'
+    
+    
+U_NAMESPACE_END
+    
+        /**
+     * @param keyword for example 'few' or 'other'
+     * @return the plural form corresponding to the keyword, or OTHER
+     */
+    static Form orOtherFromString(const UnicodeString &keyword) {
+        return static_cast<Form>(indexOrOtherIndexFromString(keyword));
     }
-    }
-    
-    
-    {
-    {  explicit DefaultCache(CacheImpl::CallBacks CBs) : CBs(std::move(CBs)) { }
-};
-} // end anonymous namespace
-    
-    void DiverseStackBase::pushNewStorageSlow(std::size_t needed) {
-  bool wasInline = isAllocatedInline();
-    }
-    
-        if (node->Further || node->Right) {
-      IndentScope ms(this, (childKind == ChildKind::Right ||
-                            childKind == ChildKind::Further ||
-                            childKind == ChildKind::Root) ? '  ' : '| ');
-    }
-    
-    #include 'swift/Demangling/Demangle.h'
-#include 'swift/Demangling/Demangler.h'
-#include <cstdio>
-    
-    void AutolinkExtractJobAction::anchor() {}
-    
-      // Must be set before iterating over inputs needing outputs.
-  result.setBypassBatchModeChecks(
-      Args.hasArg(options::OPT_bypass_batch_mode_checks));
-    
-    #endif  // CAFFE_ABSVAL_LAYER_HPP_
-
-    
-    /**
- * @brief Compute the index of the @f$ K @f$ max values for each datum across
- *        all dimensions @f$ (C \times H \times W) @f$.
- *
- * Intended for use after a classification layer to produce a prediction.
- * If parameter out_max_val is set to true, output is a vector of pairs
- * (max_ind, max_val) for each image. The axis parameter specifies an axis
- * along which to maximise.
- *
- * NOTE: does not implement Backwards operation.
- */
-template <typename Dtype>
-class ArgMaxLayer : public Layer<Dtype> {
- public:
-  /**
-   * @param param provides ArgMaxParameter argmax_param,
-   *     with ArgMaxLayer options:
-   *   - top_k (\b optional uint, default 1).
-   *     the number @f$ K @f$ of maximal items to output.
-   *   - out_max_val (\b optional bool, default false).
-   *     if set, output a vector of pairs (max_ind, max_val) unless axis is set then
-   *     output max_val along the specified axis.
-   *   - axis (\b optional int).
-   *     if set, maximise along the specified axis else maximise the flattened
-   *     trailing dimensions for each index of the first / num dimension.
-   */
-  explicit ArgMaxLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-      Blob<Dtype> mean_, variance_, temp_, x_norm_;
-  bool use_global_stats_;
-  Dtype moving_average_fraction_;
-  int channels_;
-  Dtype eps_;
-    
-    /**
- * @brief Index into the input blob along its first axis.
- *
- * This layer can be used to select, reorder, and even replicate examples in a
- * batch.  The second blob is cast to int and treated as an index into the
- * first axis of the first blob.
- */
-template <typename Dtype>
-class BatchReindexLayer : public Layer<Dtype> {
- public:
-  explicit BatchReindexLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    namespace caffe {
-    }
-    
-    
-    {  bool handles_setup_;
-  cudnnHandle_t             handle_;
-  cudnnTensorDescriptor_t bottom_desc_, top_desc_;
-  cudnnPoolingDescriptor_t  pooling_desc_;
-  cudnnPoolingMode_t        mode_;
-};
-#endif
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    
-    {			char      *lPr = (char *)malloc(MAX_PATH);
-			char      *pR = (char *)malloc(MAX_PATH);
-			size_t i;
-			wcstombs_s(&i, lPr, (size_t)MAX_PATH,
-				legacyUserProfilePath, (size_t)MAX_PATH);
-			wcstombs_s(&i, pR, (size_t)MAX_PATH,
-				userProfilePath, (size_t)MAX_PATH);
-			rename(lPr, pR);
-		}
-    
-    DHTResponseMessage::DHTResponseMessage(
-    const std::shared_ptr<DHTNode>& localNode,
-    const std::shared_ptr<DHTNode>& remoteNode,
-    const std::string& transactionID)
-    : DHTAbstractMessage(localNode, remoteNode, transactionID)
-{
-}
-    
-    #include <vector>
-#include <string>
-#include <memory>
-    
-    void DHTTokenUpdateCommand::process()
-{
-  try {
-    tokenTracker_->updateTokenSecret();
-  }
-  catch (RecoverableException& e) {
-    A2_LOG_ERROR_EX(EX_EXCEPTION_CAUGHT, e);
-  }
-}
-    
-    
-    {} // namespace aria2
-    
-        public slots:
-        void run();
-    
-        int ret3 = (int) (long) swHashMap_find(hm, (char *) SW_STRL('notfound'));
-    ASSERT_EQ(ret3, 0);
-    
-            //保存长度值
-        memcpy(ptr, &size, sizeof(size));
-        //在指针末尾保存一个串号
-        memcpy((char*) ptr + size - 4, &(send_pkg.serial_num), sizeof(send_pkg.serial_num));
-    
-    TEST(coroutine_channel, pop_yield)
-{
-    Channel chan(1);
-    }
-    
-    
-    {
-    {        int status = -1;
-        pid_t pid2 = swoole_coroutine_waitpid(pid, &status, 0);
-        ASSERT_EQ(status, 0);
-        ASSERT_EQ(pid, pid2);
-    });
-}
-    
-        fd1 = socket(AF_UNIX,SOCK_DGRAM,0);
-    strncpy(un1.sun_path, sock1_path, sizeof(un1.sun_path) - 1); 
-    bind(fd1,(struct sockaddr *)&un1,sizeof(un1));
