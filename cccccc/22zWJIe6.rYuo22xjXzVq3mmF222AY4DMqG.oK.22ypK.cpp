cc04d473c73a18aac6f94ad05d259e6686cd0533
SUNsung <weatherius@gmail.com>
2019-05-08 10:58:39 +0300

    Rewrite [22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp] File in: 2019-05-08 10:58:38

--- cccccc/22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp
+++ cccccc/22zWJIe6.rYuo22xjXzVq3mmF222AY4DMqG.oK.22ypK.cpp
@@ -1,3 +1,169 @@
 
         
-        
+          auto* message2_on_arena =
+      Arena::CreateMessage<protobuf_unittest::TestAllTypes>(&arena);
+    
+    // Author: kenton@google.com (Kenton Varda)
+    
+      desired_output_for_decode = 'abcdefghIJ';
+  expected = string('\x0A\x0', 2);
+  result = TextFormatDecodeData::DecodeDataForString(input_for_decode,
+                                                     desired_output_for_decode);
+  EXPECT_EQ(expected, result);
+    
+    TEST(ByteSourceTest, CopyTo) {
+  StringPiece data('Hello world!');
+  MockByteSource source(data, 3);
+  string str;
+  StringByteSink sink(&str);
+    }
+    
+    // Author: brianolson@google.com (Brian Olson)
+//  Based on original Protocol Buffers design by
+//  Sanjay Ghemawat, Jeff Dean, and others.
+//
+// Test program to verify that GzipInputStream is compatible with command line
+// gunzip or java.util.zip.GzipInputStream
+//
+// Reads gzip stream on standard input and writes decompressed data to standard
+// output.
+    
+    string StripProto(string filename) {
+  return filename.substr(0, filename.rfind('.proto'));
+}
+    
+      if (argc != 2) {
+    cerr << 'Usage:  ' << argv[0] << ' ADDRESS_BOOK_FILE' << endl;
+    return -1;
+  }
+    
+      // Optional:  Delete all global objects allocated by libprotobuf.
+  google::protobuf::ShutdownProtobufLibrary();
+    
+        // 7. Allocate texture
+    atlas->TexHeight = (atlas->Flags & ImFontAtlasFlags_NoPowerOfTwoHeight) ? (atlas->TexHeight + 1) : ImUpperPowerOfTwo(atlas->TexHeight);
+    atlas->TexUvScale = ImVec2(1.0f / atlas->TexWidth, 1.0f / atlas->TexHeight);
+    atlas->TexPixelsAlpha8 = (unsigned char*)IM_ALLOC(atlas->TexWidth * atlas->TexHeight);
+    memset(atlas->TexPixelsAlpha8, 0, atlas->TexWidth * atlas->TexHeight);
+    
+    namespace ImGuiFreeType
+{
+    // Hinting greatly impacts visuals (and glyph sizes).
+    // When disabled, FreeType generates blurrier glyphs, more or less matches the stb's output.
+    // The Default hinting mode usually looks good, but may distort glyphs in an unusual way.
+    // The Light hinting mode generates fuzzier glyphs but better matches Microsoft's rasterizer.
+    }
+    
+    struct ID3D10Device;
+    
+    IMGUI_IMPL_API bool     ImGui_ImplDX9_Init(IDirect3DDevice9* device);
+IMGUI_IMPL_API void     ImGui_ImplDX9_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplDX9_NewFrame();
+IMGUI_IMPL_API void     ImGui_ImplDX9_RenderDrawData(ImDrawData* draw_data);
+    
+    // Use if you want to reset your rendering device without losing ImGui state.
+IMGUI_IMPL_API void     ImGui_Marmalade_InvalidateDeviceObjects();
+IMGUI_IMPL_API bool     ImGui_Marmalade_CreateDeviceObjects();
+    
+                ImGui::Text('This is some useful text.');               // Display some text (you can use a format strings too)
+            ImGui::Checkbox('Demo Window', &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox('Another Window', &show_another_window);
+    
+    // Called by Init/NewFrame/Shutdown
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateFontsTexture();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyFontsTexture();
+IMGUI_IMPL_API bool     ImGui_ImplOpenGL2_CreateDeviceObjects();
+IMGUI_IMPL_API void     ImGui_ImplOpenGL2_DestroyDeviceObjects();
+
+    
+            // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+    
+    
+    {        ImGui::Render();
+    }
+    
+    void Speed::startWithTarget(Node* target)
+{
+    if (target && _innerAction)
+    {
+        Action::startWithTarget(target);
+        _innerAction->startWithTarget(target);
+    }
+    else
+        log('Speed::startWithTarget error: target(%p) or _innerAction(%p) is nullptr!', target, _innerAction);
+}
+    
+    /**
+ * @addtogroup actions
+ * @{
+ */
+    }
+    
+    void StopGrid::cacheTargetAsGridNode()
+{
+    _gridNodeTarget = dynamic_cast<NodeGrid*> (_target);
+    CCASSERT(_gridNodeTarget, 'GridActions can only used on NodeGrid');
+}
+    
+    /** @class CallFuncN
+* @brief Calls a 'callback' with the node as the first argument. N means Node.
+* @js NA
+*/
+class CC_DLL CallFuncN : public CallFunc
+{
+public:
+    /** Creates the action with the callback of type std::function<void()>.
+     This is the preferred way to create the callback.
+     *
+     * @param func  A callback function need to be executed.
+     * @return  An autoreleased CallFuncN object.
+     */
+    static CallFuncN * create(const std::function<void(Node*)>& func);
+    }
+    
+    class Texture2D;
+/** @class Animate
+ * @brief Animates a sprite given the name of an Animation.
+ */
+class CC_DLL Animate : public ActionInterval
+{
+public:
+    /** Creates the action with an Animation and will restore the original frame when the animation is over.
+     *
+     * @param animation A certain animation.
+     * @return An autoreleased Animate object.
+     */
+    static Animate* create(Animation *animation);
+    }
+    
+        /** Resumes the target. All queued actions will be resumed.
+     *
+     * @param target    A certain target.
+     */
+    virtual void resumeTarget(Node *target);
+    
+    /** Pauses all running actions, returning a list of targets whose actions were paused.
+     *
+     * @return  A list of targets whose actions were paused.
+     */
+    virtual Vector<Node*> pauseAllRunningActions();
+    
+    /** Resume a set of targets (convenience function to reverse a pauseAllRunningActions call).
+     *
+     * @param targetsToResume   A set of targets need to be resumed.
+     */
+    virtual void resumeTargets(const Vector<Node*>& targetsToResume);
+    
+    /** Main loop of ActionManager.
+     * @param dt    In seconds.
+     */
+    virtual void update(float dt);
+    
+protected:
+    // declared in ActionManager.m
+    
+    
+    {    return animation;
+}
