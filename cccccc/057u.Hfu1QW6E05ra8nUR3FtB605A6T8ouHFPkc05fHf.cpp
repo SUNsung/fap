c2cfc9b3944f971720103146d9810059d4258ca2
SUNsung <weatherius@gmail.com>
2019-05-08 12:09:42 +0300

    Rewrite [057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp] File in: 2019-05-08 12:09:39

--- cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
+++ cccccc/057u.Hfu1QW6E05ra8nUR3FtB605A6T8ouHFPkc05fHf.cpp
@@ -1,187 +1,32 @@
 
         
-        REGISTER_OP('Ackermann')
-    .Output('ackermann: string')
-    .Doc(R'doc(
-Output a fact about the ackermann function.
-)doc');
+        #include 'base/compiler_specific.h'
+#include 'content/nw/src/api/base/base.h'
     
-    REGISTER_OP('Invalid')
-    .Attr('invalid attr: int32')  // invalid since the name has a space.
-    .Doc(R'doc(
-An op to test that invalid ops do not successfully generate invalid python code.
-)doc');
     
-    // NumPy casts
-    
-    void ImportNumpy() {
-  import_array1();
+    {  *accelerator = item->accelerator_;
+  return true;
 }
     
+       bool IsItemForCommandIdDynamic(int command_id) const override;
+   base::string16 GetLabelForCommandId(int command_id) const override;
+   bool GetIconForCommandId(int command_id,
+                                   gfx::Image* icon) const override;
     
-    {  DCHECK(PyDict_Check(code_to_exc_type_map));
-  PyObject* key;
-  PyObject* value;
-  Py_ssize_t pos = 0;
-  while (PyDict_Next(code_to_exc_type_map, &pos, &key, &value)) {
-    TF_Code code = static_cast<TF_Code>(PyLong_AsLong(key));
-    singleton_->exc_types_[code] = value;
-    // The exception classes should also have the lifetime of the process, but
-    // incref just in case.
-    Py_INCREF(value);
-  }
-}
-    
-    
-    {}  // namespace tensorflow
-
+      gfx::Point* point = reinterpret_cast<gfx::Point*>(userdata);
+  *x = point->x();
+  *y = point->y();
     
     
     {
-    {}  // namespace swig
-}  // namespace tensorflow
-
-    
-    Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-    
-    #include 'tensorflow/stream_executor/platform.h'
-    
-      // Computes complex-to-real FFT in inverse direction.
-  virtual bool DoFft(Stream *stream, Plan *plan,
-                     const DeviceMemory<std::complex<float>> &input,
-                     DeviceMemory<float> *output) = 0;
-  virtual bool DoFft(Stream *stream, Plan *plan,
-                     const DeviceMemory<std::complex<double>> &input,
-                     DeviceMemory<double> *output) = 0;
-    
-    
-    {  DISALLOW_COPY_AND_ASSIGN(UvTaskRunner);
-};
-    
-    void Event::PreventDefault(v8::Isolate* isolate) {
-  GetWrapper()->Set(StringToV8(isolate, 'defaultPrevented'), v8::True(isolate));
+    {    // build keyboard code
+    ui::DomCode domCode = ui::KeycodeConverter::CodeStringToDomCode(keyName.c_str());
+    retval = ui::DomCodeToUsLayoutKeyboardCode(domCode);
+  }
+  return retval;
 }
     
-    void AutoUpdater::QuitAndInstall() {}
-#endif
-    
-      // There is a new update which has been downloaded.
-  virtual void OnUpdateDownloaded(const std::string& release_notes,
-                                  const std::string& release_name,
-                                  const base::Time& release_date,
-                                  const std::string& update_url) {}
-    
-    class AsarProtocolHandler : public net::URLRequestJobFactory::ProtocolHandler {
+    class NwAppCloseAllWindowsFunction : public UIThreadExtensionFunction {
  public:
-  explicit AsarProtocolHandler(
-      const scoped_refptr<base::TaskRunner>& file_task_runner);
-  ~AsarProtocolHandler() override;
-    }
-    
-    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
-                                       net::NetworkDelegate* network_delegate)
-    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
-    
-     private:
-  ~URLRequestAboutJob() override;
-  void StartAsync();
-    
-    
-    {}  // namespace
-    
-    /**
- * Virtual class encapsulate boost::thread for use in base class
- * The child class will acquire the ability to run a single thread,
- * by reimplementing the virtual function InternalThreadEntry.
- */
-class InternalThread {
- public:
-  InternalThread() : thread_() {}
-  virtual ~InternalThread();
-    }
-    
-      /**
-   * Called by the parent Layer's SetUp to check that the number of bottom
-   * and top Blobs provided as input match the expected numbers specified by
-   * the {ExactNum,Min,Max}{Bottom,Top}Blobs() functions.
-   */
-  virtual void CheckBlobCounts(const vector<Blob<Dtype>*>& bottom,
-                               const vector<Blob<Dtype>*>& top) {
-    if (ExactNumBottomBlobs() >= 0) {
-      CHECK_EQ(ExactNumBottomBlobs(), bottom.size())
-          << type() << ' Layer takes ' << ExactNumBottomBlobs()
-          << ' bottom blob(s) as input.';
-    }
-    if (MinBottomBlobs() >= 0) {
-      CHECK_LE(MinBottomBlobs(), bottom.size())
-          << type() << ' Layer takes at least ' << MinBottomBlobs()
-          << ' bottom blob(s) as input.';
-    }
-    if (MaxBottomBlobs() >= 0) {
-      CHECK_GE(MaxBottomBlobs(), bottom.size())
-          << type() << ' Layer takes at most ' << MaxBottomBlobs()
-          << ' bottom blob(s) as input.';
-    }
-    if (ExactNumTopBlobs() >= 0) {
-      CHECK_EQ(ExactNumTopBlobs(), top.size())
-          << type() << ' Layer produces ' << ExactNumTopBlobs()
-          << ' top blob(s) as output.';
-    }
-    if (MinTopBlobs() >= 0) {
-      CHECK_LE(MinTopBlobs(), top.size())
-          << type() << ' Layer produces at least ' << MinTopBlobs()
-          << ' top blob(s) as output.';
-    }
-    if (MaxTopBlobs() >= 0) {
-      CHECK_GE(MaxTopBlobs(), top.size())
-          << type() << ' Layer produces at most ' << MaxTopBlobs()
-          << ' top blob(s) as output.';
-    }
-    if (EqualNumBottomTopBlobs()) {
-      CHECK_EQ(bottom.size(), top.size())
-          << type() << ' Layer produces one top blob as output for each '
-          << 'bottom blob input.';
-    }
-  }
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    #include <vector>
-    
-    /**
- * @brief Computes a sum of two input Blobs, with the shape of the latter Blob
- *        'broadcast' to match the shape of the former. Equivalent to tiling
- *        the latter Blob, then computing the elementwise sum.
- *
- * The second input may be omitted, in which case it's learned as a parameter
- * of the layer. Note: in case bias and scaling are desired, both operations can
- * be handled by `ScaleLayer` configured with `bias_term: true`.
- */
-template <typename Dtype>
-class BiasLayer : public Layer<Dtype> {
- public:
-  explicit BiasLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    
-    {}  // namespace caffe
-    
-    #endif  // CAFFE_CONTRASTIVE_LOSS_LAYER_HPP_
-
-    
-    #include <vector>
-    
-    namespace caffe {
+  NwAppCloseAllWindowsFunction() {}
     }
