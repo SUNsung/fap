c2a59b74894932d94196ac9b9c43c1b97063dbdc
SUNsung <weatherius@gmail.com>
2019-05-08 04:12:40 +0300

    Rewrite [32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb] File in: 2019-05-08 04:12:40

--- ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
+++ ruby/32cLmvQyulfeY32tS4E1im3RvY32vPed4xfmXeg32rUJ.rb
@@ -1,4 +1,20 @@
 
         
-        blogs.sort_by { |b| b.name.capitalize }
-unavailable.sort_by { |b| b.name.capitalize }
+          Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
+    cmd = g.split('-').last
+    p.command(cmd.to_sym) do |c|
+      c.syntax cmd
+      c.action do
+        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
+          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
+      end
+    end
+  end
+    
+        def defaults_deprecate_type(old, current)
+      Jekyll.logger.warn 'Defaults:', 'The '#{old}' type has become '#{current}'.'
+      Jekyll.logger.warn 'Defaults:', 'Please update your front-matter defaults to use \
+                        'type: #{current}'.'
+    end
+  end
+end
