13e283aad9e4fe03cb16622353ee85f28fc4265a
SUNsung <weatherius@gmail.com>
2019-05-08 09:29:09 +0300

    Rewrite [56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb] File in: 2019-05-08 09:29:08

--- ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
+++ ruby/56.TkAHW1n1g56eRN4hURhejs562.bR3Jj5Tp.56sFH.rb
@@ -1,92 +1,115 @@
 
         
-        # Include LoggerSilence from ActiveSupport. This is needed to silent assets
-# requests with `config.assets.quiet`, because the default silence method of
-# the logging gem is no-op. See: https://github.com/TwP/logging/issues/11
-Logging::Logger.send :alias_method, :local_level, :level
-Logging::Logger.send :alias_method, :local_level=, :level=
-Logging::Logger.send :include, LoggerSilence
+              class MergeRequest < ActiveRecord::Base
+        self.table_name = 'merge_requests'
+    
+            def collection_options
+          { state: 'all', sort: 'created', direction: 'asc' }
+        end
+      end
+    end
+  end
+end
 
     
-        remove_duplicates
-    remove_index :share_visibilities, name: :shareable_and_user_id
-    add_index :share_visibilities, %i(shareable_id shareable_type user_id), name: :shareable_and_user_id, unique: true
-    
-      context 'heredoc' do
-    let(:cop_config) { { 'EnforcedStyle' => 'same_line' } }
-    
-    module RuboCop
-  module AST
-    # A node extension for `for` nodes. This will be used in place of a plain
-    # node when the builder constructs the AST, making its methods available
-    # to all `for` nodes within RuboCop.
-    class ForNode < Node
-      # Returns the keyword of the `for` statement as a string.
-      #
-      # @return [String] the keyword of the `until` statement
-      def keyword
-        'for'
-      end
-    
-            pairs.map(&:key).each do |key|
-          yield key
+            def sidekiq_worker_class
+          ImportNoteWorker
         end
     
-          # A shorthand for getting the last argument of the node.
-      # Equivalent to `arguments.last`.
-      #
-      # @return [Node, nil] the last argument of the node,
-      #                     or `nil` if there are no arguments
-      def last_argument
-        arguments[-1]
-      end
+    lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'capistrano/version'
     
-          # Returns the index of the `when` branch within the `case` statement.
-      #
-      # @return [Integer] the index of the `when` branch
-      def branch_index
-        parent.when_branches.index(self)
-      end
+    Then(/^the failure task will not run$/) do
+  failed = TestApp.shared_path.join('failed')
+  expect { run_vagrant_command(test_file_exists(failed)) }
+    .to raise_error(VagrantHelpers::VagrantSSHCommandError)
+end
     
-          def initialize(department, cop_name)
-        @department = department.to_sym if department
-        @cop_name   = cop_name
-      end
+      def symlinked?(symlink_path, target_path)
+    '[ #{symlink_path} -ef #{target_path} ]'
+  end
     
-        def initialize(tag_name, markup, tokens)
-      @by = nil
-      @source = nil
-      @title = nil
-      if markup =~ FullCiteWithTitle
-        @by = $1
-        @source = $2 + $3
-        @title = $4.titlecase.strip
-      elsif markup =~ FullCite
-        @by = $1
-        @source = $2 + $3
-      elsif markup =~ AuthorTitle
-        @by = $1
-        @title = $2.titlecase.strip
-      elsif markup =~ Author
-        @by = $1
-      end
-      super
-    end
-    
-      def render(context)
-    config_tag(context.registers[:site].config, @key, @tag, @classname)
+      def run_vagrant_command(command)
+    stdout, stderr, status = vagrant_cli_command('ssh -c #{command.inspect}')
+    return [stdout, stderr] if status.success?
+    raise VagrantSSHCommandError, status
   end
 end
     
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
+        def any?(key)
+      value = fetch(key)
+      if value && value.respond_to?(:any?)
+        begin
+          return value.any?
+        rescue ArgumentError # rubocop:disable Lint/HandleExceptions
+          # Gracefully ignore values whose `any?` method doesn't accept 0 args
         end
+      end
     
-        def initialize(tag_name, markup, tokens)
-      @videos = markup.scan(/((https?:\/\/|\/)\S+\.(webm|ogv|mp4)\S*)/i).map(&:first).compact
-      @poster = markup.scan(/((https?:\/\/|\/)\S+\.(png|gif|jpe?g)\S*)/i).map(&:first).compact.first
-      @sizes  = markup.scan(/\s(\d\S+)/i).map(&:first).compact
-      super
+          def extract_options(array)
+        array.last.is_a?(::Hash) ? array.pop : {}
+      end
     end
+  end
+end
+
+    
+    module Capistrano
+  class Configuration
+    # Holds the variables assigned at Capistrano runtime via `set` and retrieved
+    # with `fetch`. Does internal bookkeeping to help identify user mistakes
+    # like spelling errors or unused variables that may lead to unexpected
+    # behavior.
+    class Variables
+      CAPISTRANO_LOCATION = File.expand_path('../..', __FILE__).freeze
+      IGNORED_LOCATIONS = [
+        '#{CAPISTRANO_LOCATION}/configuration/variables.rb:',
+        '#{CAPISTRANO_LOCATION}/configuration.rb:',
+        '#{CAPISTRANO_LOCATION}/dsl/env.rb:',
+        '/dsl.rb:',
+        '/forwardable.rb:'
+      ].freeze
+      private_constant :CAPISTRANO_LOCATION, :IGNORED_LOCATIONS
+    
+      def safe_params
+    params.require(:address_endpoint).permit(:address)
+  end
+    
+      included do
+    helper_method :organization
+    before_action :add_organization_to_page_title
+  end
+    
+    end
+
+    
+      def check
+    if @domain.check_dns(:manual)
+      redirect_to_with_json [organization, @server, :domains], :notice => 'Your DNS records for #{@domain.name} look good!'
+    else
+      redirect_to_with_json [:setup, organization, @server, @domain], :alert => 'There seems to be something wrong with your DNS records. Check below for information.'
+    end
+  end
+    
+      def get_messages(scope)
+    if scope == 'held'
+      options = {:where => {:held => 1}}
+    else
+      options = {:where => {:scope => scope, :spam => false}, :order => :timestamp, :direction => 'desc'}
+    
+      def destroy
+    if @user == current_user
+      redirect_to_with_json [organization, :users], :alert => 'You cannot revoke your own access.'
+      return
+    end
+    
+          organization_domains = server.organization.domains.verified.order(:name)
+      unless organization_domains.empty?
+        s << '<optgroup label='Organization Domains'>'
+        for domain in organization_domains
+          selected = domain == selected_domain ? 'selected='selected'' : ''
+          s << '<option value='#{domain.id}' #{selected}>#{domain.name}</option>'
+        end
+        s << '</optgroup>'
+      end
