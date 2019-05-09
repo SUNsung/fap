e62829b7bc17f8c3703ea64b4f1954a8bb60194c
SUNsung <weatherius@gmail.com>
2019-05-09 23:40:56 +0300

    Rewrite [56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py] File in: 2019-05-09 23:40:55

--- pyton/56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py
+++ pyton/56mOEBmaE6br256IHGaM4Kh5Us56xp0cFRW.uvw56Sc.py
@@ -1,95 +1,86 @@
 
         
-            def delete(self, session_key=None):
-        if session_key is None:
-            if self.session_key is None:
-                return
-            session_key = self.session_key
-        try:
-            self.model.objects.get(session_key=session_key).delete()
-        except self.model.DoesNotExist:
-            pass
+        def _make_failed_load_tests(name, exception, suiteClass):
+    message = 'Failed to call load_tests:\n%s' % (traceback.format_exc(),)
+    return _make_failed_test(
+        name, exception, suiteClass, message)
     
-        results_dir = os.path.join(output_dir, 'results')
-    if not os.path.exists(results_dir):
-        os.mkdir(results_dir)
-    
-    # Required dataset entry keys
-_IM_DIR = 'image_directory'
-_ANN_FN = 'annotation_file'
+    # Same, but for 3.x to 2.x
+REVERSE_IMPORT_MAPPING = dict((v, k) for (k, v) in IMPORT_MAPPING.items())
+assert len(REVERSE_IMPORT_MAPPING) == len(IMPORT_MAPPING)
+REVERSE_NAME_MAPPING = dict((v, k) for (k, v) in NAME_MAPPING.items())
+assert len(REVERSE_NAME_MAPPING) == len(NAME_MAPPING)
     
     
-def _write_voc_results_files(json_dataset, all_boxes, salt):
-    filenames = []
-    image_set_path = voc_info(json_dataset)['image_set_path']
-    assert os.path.exists(image_set_path), \
-        'Image set path does not exist: {}'.format(image_set_path)
-    with open(image_set_path, 'r') as f:
-        image_index = [x.strip() for x in f.readlines()]
-    # Sanity check that order of images in json dataset matches order in the
-    # image set
-    roidb = json_dataset.get_roidb()
-    for i, entry in enumerate(roidb):
-        index = os.path.splitext(os.path.split(entry['image'])[1])[0]
-        assert index == image_index[i]
-    for cls_ind, cls in enumerate(json_dataset.classes):
-        if cls == '__background__':
-            continue
-        logger.info('Writing VOC results for: {}'.format(cls))
-        filename = _get_voc_results_file_template(json_dataset,
-                                                  salt).format(cls)
-        filenames.append(filename)
-        assert len(all_boxes[cls_ind]) == len(image_index)
-        with open(filename, 'wt') as f:
-            for im_ind, index in enumerate(image_index):
-                dets = all_boxes[cls_ind][im_ind]
-                if type(dets) == list:
-                    assert len(dets) == 0, \
-                        'dets should be numpy.ndarray or empty list'
-                    continue
-                # the VOCdevkit expects 1-based indices
-                for k in range(dets.shape[0]):
-                    f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
-                            format(index, dets[k, -1],
-                                   dets[k, 0] + 1, dets[k, 1] + 1,
-                                   dets[k, 2] + 1, dets[k, 3] + 1))
-    return filenames
-    
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+if __name__ == '__main__':
+    main()
+
     
     
-# ---------------------------------------------------------------------------- #
-# RPN and Faster R-CNN outputs and losses
-# ---------------------------------------------------------------------------- #
+def main():
+    parser = ArgumentParser(description='''\
+Unpack a MIME message into a directory of files.
+''')
+    parser.add_argument('-d', '--directory', required=True,
+                        help='''Unpack the MIME message into the named
+                        directory, which will be created if it doesn't already
+                        exist.''')
+    parser.add_argument('msgfile')
+    args = parser.parse_args()
     
-    from detectron.datasets import json_dataset
-from detectron.datasets import roidb as roidb_utils
-from detectron.utils import blob as blob_utils
-import detectron.roi_data.fast_rcnn as fast_rcnn_roi_data
+        print('-' * 20)
     
-        if (sim.model.eq_type is None or
-        sim.model.eq_obj1id is None or
-        sim.model.eq_obj2id is None):
-        return
-    for eq_type, obj1_id, obj2_id in zip(sim.model.eq_type,
-                                         sim.model.eq_obj1id,
-                                         sim.model.eq_obj2id):
-        if eq_type != mujoco_py.const.EQ_WELD:
-            continue
+    References:
+    tensorlayer.layers.Conv2dLayer
+'''
+import tensorflow as tf
     
-    def key_press(key, mod):
-    global human_agent_action, human_wants_restart, human_sets_pause
-    if key==0xff0d: human_wants_restart = True
-    if key==32: human_sets_pause = not human_sets_pause
-    a = int( key - ord('0') )
-    if a <= 0 or a >= ACTIONS: return
-    human_agent_action = a
+        def _build(self, n_input):
+        ''''''
+        with tf.variable_scope(self.name or 'dense'):
+            self.W, self.b = get_wb([n_input, self.n_unit])
     
-                # WHEEL_MOMENT_OF_INERTIA*np.square(w.omega)/2 = E -- energy
-            # WHEEL_MOMENT_OF_INERTIA*w.omega * domega/dt = dE/dt = W -- power
-            # domega = dt*W/WHEEL_MOMENT_OF_INERTIA/w.omega
-            w.omega += dt*ENGINE_POWER*w.gas/WHEEL_MOMENT_OF_INERTIA/(abs(w.omega)+5.0)  # small coef not to divide by zero
-            self.fuel_spent += dt*ENGINE_POWER*w.gas
+      {} requires <code>A File Icon</code> package for enhanced<br>support of
+  the file-specific icons.
+  <br><br>Would you like to install it?<br>
+  <br><a href='install'>Install</a> <a href='cancel'>Cancel</a>
+</div>
+'''.format(THEME_NAME)
+    
+    
+def revert_ui_theme(path):
+  set_ui_theme(path)
+    
+        :param repo_dir: Local directory of newly cloned repo.
+    :returns project_template: Relative path to project template.
+    '''
+    logger.debug('Searching {} for the project template.'.format(repo_dir))
+    
+    # If false, no index is generated.
+#epub_use_index = True
+    
+            # Carefully delete the created ~/.cookiecutter_replay dir only in
+        # certain conditions.
+        restore_backup_dir(
+            cookiecutter_replay_dir,
+            cookiecutter_replay_dir_backup,
+            cookiecutter_replay_dir_found
+        )
+    
+    
+@pytest.fixture
+def replay_file(replay_test_dir, template_name):
+    '''Fixture to return a actual file name of the dump.'''
+    file_name = '{}.json'.format(template_name)
+    return os.path.join(replay_test_dir, file_name)
+    
+        assert str(err.value) == (
+        'A valid repository for '{}' could not be found in the following '
+        'locations:\n{}'.format(
+            template_path,
+            '\n'.join([
+                template_path,
+                str(tmpdir / 'tests/unknown-repo')
+            ]),
+        )
+    )
