fd95f523bbf857c56f1fc762e35af020db9d56ee
SUNsung <weatherius@gmail.com>
2019-05-09 19:03:17 +0300

    Rewrite [12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb] File in: 2019-05-09 19:03:16

--- ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
+++ ruby/12X62DRlq6dNk12nY6Nfm5Eoc12mysCiZ.EaP612t3Q.rb
@@ -1,14 +1,91 @@
 
         
-                def render(&block)
-          render_collection_for(RadioButtonBuilder, &block)
+              it 'adds logformat param to command' do
+        result = Fastlane::FastFile.new.parse('lane :test do
+          appledoc(
+            project_name: 'Project Name',
+            project_company: 'Company',
+            input: 'input/dir',
+            logformat: '1'
+          )
+        end').runner.execute(:test)
+    
+            context 'with given path to oclint' do
+          let(:result) do
+            Fastlane::FastFile.new.parse('lane :test do
+              oclint(
+                compile_commands: './fastlane/spec/fixtures/oclint/compile_commands.json',
+                oclint_path: 'test/bin/oclint'
+              )
+            end').runner.execute(:test)
+          end
+          let(:command) { 'cd #{File.expand_path('.').shellescape} && test/bin/oclint -report-type=html -o=oclint_report.html' }
+    
+        it 'shelljoins multiple args' do
+      message = 'A message'
+      command = command_from_args('git', 'commit', '-m', message)
+      expect(command).to eq('git commit -m #{message.shellescape}')
+    end
+    
+            it 'sets up screenshots folder in current folder' do
+          expect(options[:screenshots_path]).to eq('./screenshots')
         end
     
-        class WithNilLayout < Base
-      layout nil
+        attr_accessor :name, :type, :path
     
-        puts('Generated at #{target_file}')
+        def html?
+      mime_type.include? 'html'
+    end
+    
+          def fetch_redirections
+        result = {}
+        with_filters 'apply_base_url', 'container', 'normalize_urls', 'internal_urls' do
+          build_pages do |page|
+            next if page[:response_effective_path] == page[:response_path]
+            result[page[:response_path].downcase] = page[:response_effective_path]
+          end
+        end
+        result
+      end
+    
+            css('> .section', '#preamble', 'a[href*='dict.html']', 'code var', 'code strong').each do |node|
+          node.before(node.children).remove
+        end
+    
+            # Initializes the system. Any subclasses MUST make sure this
+        # method is called on the parent. Therefore, if a subclass overrides
+        # `initialize`, then you must call `super`.
+        def initialize(vm)
+          @vm = vm
+        end
+    
+      at_exit { Application.run! if $!.nil? && Application.run? }
+end
+    
+      task :index do
+    doc = File.read('README.md')
+    file = 'doc/rack-protection-readme.md'
+    Dir.mkdir 'doc' unless File.directory? 'doc'
+    puts 'writing #{file}'
+    File.open(file, 'w') { |f| f << doc }
   end
+    
+          def decode_token(token)
+        Base64.strict_decode64(token)
+      end
+    
+        BUNDLE_DIR = ::File.join(LOGSTASH_HOME, 'vendor', 'bundle')
+    GEMFILE_PATH = ::File.join(LOGSTASH_HOME, 'Gemfile')
+    LOCAL_GEM_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'local_gems')
+    CACHE_PATH = ::File.join(LOGSTASH_HOME, 'vendor', 'cache')
+    LOCKFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile.lock'))
+    GEMFILE = Pathname.new(::File.join(LOGSTASH_HOME, 'Gemfile'))
+    
+          prepare_package(explicit_plugins_specs, temp_path)
+      LogStash::Util::Zip.compress(temp_path, @target)
+    ensure
+      FileUtils.rm_rf(temp_path)
+    end
     
       def validate_cache_location
     cache_location = LogStash::Environment::CACHE_PATH
@@ -24,159 +101,3 @@
     end
   end
 end
-
-    
-        let(:plugin_name) { 'logstash-filter-qatest' }
-    let(:previous_version) { '0.1.0' }
-    
-          def format
-        @format = (@page.format || false) if @format.nil?
-        @format.to_s.downcase
-      end
-    end
-  end
-end
-
-    
-          def escaped_name
-        CGI.escape(@name)
-      end
-    
-          # Wraps page formatted data to Nokogiri::HTML document.
-      #
-      def build_document(content)
-        Nokogiri::HTML::fragment(%{<div id='gollum-root'>} + content.to_s + %{</div>}, 'UTF-8')
-      end
-    
-    def commit_details
-  { :message => 'Did something at #{Time.now}',
-    :name    => 'Tom Preston-Werner',
-    :email   => 'tom@github.com' }
-end
-    
-        assert_match /Delete this Page/, last_response.body, ''Delete this Page' link is blocked in page template'
-    assert_match /New/,              last_response.body, ''New' button is blocked in page template'
-    assert_match /Upload/,           last_response.body, ''Upload' link is blocked in page template'
-    assert_match /Rename/,           last_response.body, ''Rename' link is blocked in page template'
-    assert_match /Edit/,             last_response.body, ''Edit' link is blocked in page template'
-    
-    context 'Precious::Views::Page' do
-  setup do
-    examples = testpath 'examples'
-    @path    = File.join(examples, 'test.git')
-    FileUtils.cp_r File.join(examples, 'empty.git'), @path, :remove_destination => true
-    @wiki = Gollum::Wiki.new(@path)
-  end
-    
-            it 'deletes the old shipment if enough are split off' do
-          expect(order.shipments.count).to eq(2)
-    
-          separator = raw(separator)
-      crumbs = [content_tag(:li, content_tag(:span, link_to(content_tag(:span, Spree.t(:home), itemprop: 'name'), spree.root_path, itemprop: 'url') + separator, itemprop: 'item'), itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement', class: 'breadcrumb-item')]
-      if taxon
-        crumbs << content_tag(:li, content_tag(:span, link_to(content_tag(:span, Spree.t(:products), itemprop: 'name'), spree.products_path, itemprop: 'url') + separator, itemprop: 'item'), itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement', class: 'breadcrumb-item')
-        crumbs << taxon.ancestors.collect { |ancestor| content_tag(:li, content_tag(:span, link_to(content_tag(:span, ancestor.name, itemprop: 'name'), seo_url(ancestor), itemprop: 'url') + separator, itemprop: 'item'), itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement', class: 'breadcrumb-item') } unless taxon.ancestors.empty?
-        crumbs << content_tag(:li, content_tag(:span, link_to(content_tag(:span, taxon.name, itemprop: 'name'), seo_url(taxon), itemprop: 'url'), itemprop: 'item'), class: 'active breadcrumb-item', itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement')
-      else
-        crumbs << content_tag(:li, content_tag(:span, Spree.t(:products), itemprop: 'item'), class: 'active', itemscope: 'itemscope', itemtype: 'https://schema.org/ListItem', itemprop: 'itemListElement')
-      end
-      crumb_list = content_tag(:ol, raw(crumbs.flatten.map(&:mb_chars).join), class: 'breadcrumb', itemscope: 'itemscope', itemtype: 'https://schema.org/BreadcrumbList')
-      content_tag(:nav, crumb_list, id: 'breadcrumbs', class: 'col-12 mt-4', aria: { label: 'breadcrumb' })
-    end
-    
-              def order_token
-            request.headers['X-Spree-Order-Token'] || params[:order_token]
-          end
-    
-                return if after_update_attributes
-    
-            def show
-          expires_in 15.minutes, public: true
-          headers['Surrogate-Control'] = 'max-age=#{15.minutes}'
-          headers['Surrogate-Key'] = 'product_id=1'
-          respond_with(@product)
-        end
-    
-              def advance
-            spree_authorize! :update, spree_current_order, order_token
-    
-    @@ index
-  <h1>Sinatra + Sidekiq Example</h1>
-  <h2>Failed: <%= @failed %></h2>
-  <h2>Processed: <%= @processed %></h2>
-    
-          def create_worker_spec
-        template_file = File.join(
-            'spec/workers',
-            class_path,
-            '#{file_name}_worker_spec.rb'
-        )
-        template 'worker_spec.rb.erb', template_file
-      end
-    
-    module Sidekiq
-  module Extensions
-    ##
-    # Adds 'delay', 'delay_for' and `delay_until` methods to ActionMailer to offload arbitrary email
-    # delivery to Sidekiq.  Example:
-    #
-    #    UserMailer.delay.send_welcome_email(new_user)
-    #    UserMailer.delay_for(5.days).send_welcome_email(new_user)
-    #    UserMailer.delay_until(5.days.from_now).send_welcome_email(new_user)
-    class DelayedMailer
-      include Sidekiq::Worker
-    
-          # Jobs queued for this worker
-      def jobs
-        Queues.jobs_by_worker[self.to_s]
-      end
-    
-              return WebAction.new(env, route.block)
-        end
-      end
-    
-        # Find all leaf-like paths (files, symlink, empty directories, etc)
-    # Also trim the leading path such that '#{staging_path}/' is removed from
-    # the path before returning.
-    #
-    # Wrapping Find.find in an Enumerator is required for sane operation in ruby 1.8.7,
-    # but requires the 'backports' gem (which is used in other places in fpm)
-    return Enumerator.new { |y| Find.find(staging_path) { |path| y << path } } \
-      .select { |path| path != staging_path } \
-      .select { |path| is_leaf.call(path) } \
-      .collect { |path| path[staging_path.length + 1.. -1] }
-  end # def files
-    
-        # Publish the package.
-    repo_path = build_path('#{name}_repo')
-    safesystem('pkgrepo', 'create', repo_path)
-    safesystem('pkgrepo', 'set', '-s', repo_path, 'publisher/prefix=#{attributes[:p5p_publisher]}')
-    safesystem('pkgsend', '-s', repo_path,
-      'publish', '-d', '#{staging_path}', '#{build_path}/#{name}.p5m')
-    safesystem('pkgrecv', '-s', repo_path, '-a',
-      '-d', build_path('#{name}.p5p'), name)
-    
-      # Input a PEAR package.
-  #
-  # The parameter is a PHP PEAR package name.
-  #
-  # Attributes that affect behavior here:
-  # * :prefix - changes the install root, default is /usr/share
-  # * :pear_package_name_prefix - changes the
-  def input(input_package)
-    if !program_in_path?('pear')
-      raise ExecutableNotFound.new('pear')
-    end
-    
-        args = [ '-B', build_path('build-info'), '-c', build_path('comment'), '-d', build_path('description'), '-f', build_path('packlist'), '-I', '/opt/local', '-p', staging_path,  '-U', '#{cwd}/#{name}-#{self.version}-#{iteration}.tgz' ]
-    safesystem('pkg_create', *args)
-    
-      # Output a zipfile.
-  def output(output_path)
-    output_check(output_path)
-    realpath = Pathname.new(output_path).realdirpath.to_s
-    ::Dir.chdir(staging_path) do
-      safesystem('zip', '-9r', realpath, '.')
-    end
-  end # def output
-end # class FPM::Package::Tar
