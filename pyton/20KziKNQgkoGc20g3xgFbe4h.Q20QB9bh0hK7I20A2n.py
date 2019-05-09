702ca652ca116367dc5b6052e669d98e5858f678
SUNsung <weatherius@gmail.com>
2019-05-09 21:11:52 +0300

    Rewrite [20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py] File in: 2019-05-09 21:11:51

--- pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
+++ pyton/20KziKNQgkoGc20g3xgFbe4h.Q20QB9bh0hK7I20A2n.py
@@ -1,217 +1,153 @@
 
         
-            with open('README.md', 'w+') as sorted_file:
-        sorted_file.write(final_README)
+        
+def main():
+    '''Pretty-print the bug information as JSON.'''
+    print(json.dumps(info(), sort_keys=True, indent=2))
     
-        def load(self):
-        try:
-            session_data = self._cache.get(self.cache_key)
-        except Exception:
-            # Some backends (e.g. memcache) raise an exception on invalid
-            # cache keys. If this happens, reset the session. See #17810.
-            session_data = None
-        if session_data is not None:
-            return session_data
-        self._session_key = None
-        return {}
+        with server as (host, port):
+        url = 'http://{}:{}/'.format(host, port)
+        r = requests.get(url, auth=auth)
+        # Verify server didn't authenticate us.
+        assert r.status_code == 401
+        assert r.history[0].status_code == 401
+        close_server.set()
     
-        @classmethod
-    def get_model_class(cls):
-        # Avoids a circular import and allows importing SessionStore when
-        # django.contrib.sessions is not in INSTALLED_APPS.
-        from django.contrib.sessions.models import Session
-        return Session
-    
-        def load(self):
-        '''
-        Load the data from the key itself instead of fetching from some
-        external data store. Opposite of _get_session_key(), raise BadSignature
-        if signature fails.
-        '''
-        try:
-            return signing.loads(
-                self.session_key,
-                serializer=self.serializer,
-                # This doesn't handle non-default expiry dates, see #19201
-                max_age=settings.SESSION_COOKIE_AGE,
-                salt='django.contrib.sessions.backends.signed_cookies',
-            )
-        except Exception:
-            # BadSignature, ValueError, or unpickling exceptions. If any of
-            # these happen, reset the session.
-            self.create()
-        return {}
+                if point.name == 'p':
+                link = point.find('a')
+                if link is not None:
+                    link = clean_pdf_link(link.attrs['href'])
+                    ext = get_extension(link)
+                    print(ext)
+                    if not ext in forbidden_extensions:
+                        print(shorten_title(point.text) + ' (' + link + ')')
+                        try:
+                            name = clean_text(point.text.split('[' + ext + ']')[0])
+                            fullname = '.'.join((name, ext))
+                            if not os.path.exists('/'.join((current_directory, fullname)) ):
+                                download_pdf(link, current_directory, '.'.join((name, ext)))
+                        except KeyboardInterrupt:
+                            try:
+                                print('Press Ctrl-C in 1 second to quit')
+                                time.sleep(1)
+                            except KeyboardInterrupt:
+                                print('Cancelling..')
+                                break
+                        except:
+                            failures.append(point.text)
+                        
+        point = point.next_sibling          
     
     
-class BaseSessionManager(models.Manager):
-    def encode(self, session_dict):
-        '''
-        Return the given session dictionary serialized and encoded as a string.
-        '''
-        session_store_class = self.model.get_session_store_class()
-        return session_store_class().encode(session_dict)
-    
-        for node in doctree.traverse(settingslist_node):
-        settings_list = nodes.bullet_list()
-        settings_list.extend([make_setting_element(d, app, fromdocname)
-                              for d in sorted(env.scrapy_all_settings,
-                                              key=itemgetter('setting_name'))
-                              if fromdocname != d['docname']])
-        node.replace_self(settings_list)
-    
-    '''
-    
-        def run(self, args, opts):
-        # load contracts
-        contracts = build_component_list(self.settings.getwithbase('SPIDER_CONTRACTS'))
-        conman = ContractsManager(load_object(c) for c in contracts)
-        runner = TextTestRunner(verbosity=2 if opts.verbose else 1)
-        result = TextTestResult(runner.stream, runner.descriptions, runner.verbosity)
-    
-            editor = self.settings['EDITOR']
-        try:
-            spidercls = self.crawler_process.spider_loader.load(args[0])
-        except KeyError:
-            return self._err('Spider not found: %s' % args[0])
-    
-    from scrapy import twisted_version
-    
-        def _debug_set_cookie(self, response, spider):
-        if self.debug:
-            cl = [to_native_str(c, errors='replace')
-                  for c in response.headers.getlist('Set-Cookie')]
-            if cl:
-                cookies = '\n'.join('Set-Cookie: {}\n'.format(c) for c in cl)
-                msg = 'Received cookies from: {}\n{}'.format(response, cookies)
-                logger.debug(msg, extra={'spider': spider})
+  def Start( self ):
+    request_data = BuildRequestData()
+    request_data.update( { 'filetypes': self.filetypes } )
+    self._response = self.PostDataToHandler( request_data,
+                                             'semantic_completion_available' )
     
     
-# This class can be used to keep the ycmd server alive for the duration of the
-# life of the client. By default, ycmd shuts down if it doesn't see a request in
-# a while.
-class YcmdKeepalive( object ):
-  def __init__( self, ping_interval_seconds = 60 * 10 ):
-    self._keepalive_thread = Thread( target = self._ThreadMain )
-    self._keepalive_thread.daemon = True
-    self._ping_interval_seconds = ping_interval_seconds
+class ShutdownRequest( BaseRequest ):
+  def __init__( self ):
+    super( ShutdownRequest, self ).__init__()
     
+      _assert_accepts( f, { 'text' : 'This is an IMPORTANT taco',
+                        'kind' : 'WARNING' } )
+  _assert_rejects( f, { 'text' : 'This taco will NOT be shown',
+                        'kind' : 'ERROR' } )
     
-def _CreateInitialGroupMap():
-  def AddToGroupMap( name, parent ):
-    new_group = SyntaxGroup( name )
-    group_name_to_group[ name ] = new_group
-    parent.children.append( new_group )
+    # Output file base name for HTML help builder.
+htmlhelp_basename = 'futuresdoc'
     
-    from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-# Not installing aliases from python-future; it's unreliable and slow.
-from builtins import *  # noqa
-    
-    
-def KeywordsFromSyntaxListOutput_MultipleStatementGroups_test():
-  assert_that( syntax_parse._KeywordsFromSyntaxListOutput( '''
-foogroup xxx foo bar
-             links to Statement
-bargroup xxx zoo goo
-             links to Statement''' ),
-               contains_inanyorder( 'foo', 'bar', 'zoo', 'goo' ) )
-    
-    class ProcessPoolExecutor(_base.Executor):
-    def __init__(self, max_workers=None):
-        '''Initializes a new ProcessPoolExecutor instance.
-    
-            print('One element')
-        assert_equal(merge_sort.sort([5]), [5])
-    
-        # TODO: It would be better if we had unit tests for each
-    # method in addition to the following end-to-end test
-    def test_end_to_end(self):
-        print('Test: Empty stack')
-        stack = Stack()
-        assert_equal(stack.peek(), None)
-        assert_equal(stack.pop(), None)
-    
-            # Scenario: call is successful
-        return_value = get_customer_profile(self.customer_id)
-        GetRequest.assert_called_once_with(customerProfileId=self.customer_id)
-        self.assertTrue(_request.make_request.called)
-        self.assertEqual(return_value, profile_mock)
-    
-        # IE<8 allows backslash escapes in place of pretty much any char
-    def test_escape_function_obfuscation(self):
-        testcase = u'*{color: expression\\28 alert\\28 1 \\29 \\29 }'
-        self.assertInvalid(testcase)
-    
-        def test_nested_modules_include_all_sources(self):
-        test_files_a = ['foo.js', 'bar.js']
-        test_module_a = TestModule('test_module_a', *test_files_a)
-        test_files_b = ['baz.js', 'qux.js']
-        test_module_b = TestModule('test_module_b', *test_files_b)
-        test_module = TestModule('test_mobule', test_module_a, test_module_b)
-        self.assertEqual(test_module.build(), concat_sources(test_files_a + test_files_b))
-    
-            self.assertEqual(srnames, set(nsfw_collection_srnames))
-    
-        def test_no_resize(self):
-        image = dict(url='http://s3.amazonaws.com/a.jpg', width=200,
-                      height=800)
-        url = self.provider.resize_image(image)
-        self.assertEqual(url, 'https://unsplash.it/200/400')
-    
-        def test_crashers(self):
-        # Chrome crashes on weirdly encoded nulls.
-        self.assertFragmentRaises('<a href='http://example.com/%%30%30'>foo</a>',
-                                  SoupDetectedCrasherError)
-        self.assertFragmentRaises('<a href='http://example.com/%0%30'>foo</a>',
-                                  SoupDetectedCrasherError)
-        self.assertFragmentRaises('<a href='http://example.com/%%300'>foo</a>',
-                                  SoupDetectedCrasherError)
-        # Chrome crashes on extremely long hostnames
-        self.assertFragmentRaises('<a href='http://%s.com'>foo</a>' % ('x' * 300),
-                                  SoupDetectedCrasherError)
+    if __name__ == '__main__':
+    main()
 
     
+        def test_exception_with_timeout(self):
+        self.assertRaises(futures.TimeoutError,
+                          PENDING_FUTURE.exception, timeout=0)
+        self.assertRaises(futures.TimeoutError,
+                          RUNNING_FUTURE.exception, timeout=0)
+        self.assertRaises(futures.CancelledError,
+                          CANCELLED_FUTURE.exception, timeout=0)
+        self.assertRaises(futures.CancelledError,
+                          CANCELLED_AND_NOTIFIED_FUTURE.exception, timeout=0)
+        self.assertTrue(isinstance(EXCEPTION_FUTURE.exception(timeout=0),
+                                   IOError))
+        self.assertEqual(SUCCESSFUL_FUTURE.exception(timeout=0), None)
     
-if __name__ == '__main__':
-    rs = RandomizedSet()
-    print('insert 1: ', rs.insert(1))
-    print('insert 2: ', rs.insert(2))
-    print('insert 3: ', rs.insert(3))
-    print('insert 4: ', rs.insert(4))
-    print('remove 3: ', rs.remove(3))
-    print('remove 3: ', rs.remove(3))
-    print('remove 1: ', rs.remove(1))
-    print('random: ', rs.get_random())
-    print('random: ', rs.get_random())
-    print('random: ', rs.get_random())
-    print('random: ', rs.get_random())
+    parser = OptionParser(USAGE)
+parser.add_option('-k', dest='topK')
+opt, args = parser.parse_args()
+    
+    content = open(file_name, 'rb').read()
+    
+    for topic_idx, topic in enumerate(nmf.components_):
+    print('Topic #%d:' % topic_idx)
+    print(' '.join([feature_names[i]
+                    for i in topic.argsort()[:-n_top_words - 1:-1]]))
+    print('')
 
     
-    Condensed mathematical description:
+    Latin5_BulgarianCharToOrderMap = (
+255,255,255,255,255,255,255,255,255,255,254,255,255,254,255,255,  # 00
+255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,  # 10
+253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,253,  # 20
+252,252,252,252,252,252,252,252,252,252,253,253,253,253,253,253,  # 30
+253, 77, 90, 99,100, 72,109,107,101, 79,185, 81,102, 76, 94, 82,  # 40
+110,186,108, 91, 74,119, 84, 96,111,187,115,253,253,253,253,253,  # 50
+253, 65, 69, 70, 66, 63, 68,112,103, 92,194,104, 95, 86, 87, 71,  # 60
+116,195, 85, 93, 97,113,196,197,198,199,200,253,253,253,253,253,  # 70
+194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,  # 80
+210,211,212,213,214,215,216,217,218,219,220,221,222,223,224,225,  # 90
+ 81,226,227,228,229,230,105,231,232,233,234,235,236, 45,237,238,  # a0
+ 31, 32, 35, 43, 37, 44, 55, 47, 40, 59, 33, 46, 38, 36, 41, 30,  # b0
+ 39, 28, 34, 51, 48, 49, 53, 50, 54, 57, 61,239, 67,240, 60, 56,  # c0
+  1, 18,  9, 20, 11,  3, 23, 15,  2, 26, 12, 10, 14,  6,  4, 13,  # d0
+  7,  8,  5, 19, 29, 25, 22, 21, 27, 24, 17, 75, 52,241, 42, 16,  # e0
+ 62,242,243,244, 58,245, 98,246,247,248,249,250,251, 91,252,253,  # f0
+)
     
+            if self.state == ProbingState.DETECTING:
+            if (self.distribution_analyzer.got_enough_data() and
+                    (self.get_confidence() > self.SHORTCUT_THRESHOLD)):
+                self._state = ProbingState.FOUND_IT
     
-# Python solution with only one table for B (~196ms):
-def multiply(self, a, b):
-    '''
-    :type A: List[List[int]]
-    :type B: List[List[int]]
-    :rtype: List[List[int]]
-    '''
-    if a is None or b is None: return None
-    m, n, l = len(a), len(a[0]), len(b[0])
-    if len(b) != n:
-        raise Exception('A's column number must be equal to B's row number.')
-    c = [[0 for _ in range(l)] for _ in range(m)]
-    table_b = {}
-    for k, row in enumerate(b):
-        table_b[k] = {}
-        for j, eleB in enumerate(row):
-            if eleB: table_b[k][j] = eleB
-    for i, row in enumerate(a):
-        for k, eleA in enumerate(row):
-            if eleA:
-                for j, eleB in table_b[k].iteritems():
-                    c[i][j] += eleA * eleB
-    return c
+        def __init__(self, lang_filter=None):
+        super(MultiByteCharSetProber, self).__init__(lang_filter=lang_filter)
+        self.distribution_analyzer = None
+        self.coding_sm = None
+        self._last_char = [0, 0]
+    
+        def reset(self):
+        super(SingleByteCharSetProber, self).reset()
+        # char order of last character
+        self._last_order = 255
+        self._seq_counters = [0] * SequenceLikelihood.get_num_categories()
+        self._total_seqs = 0
+        self._total_char = 0
+        # characters that fall in our sampling range
+        self._freq_char = 0
+    
+            # Log all prober confidences if none met MINIMUM_THRESHOLD
+        if self.logger.getEffectiveLevel() == logging.DEBUG:
+            if self.result['encoding'] is None:
+                self.logger.debug('no probers hit minimum threshold')
+                for group_prober in self._charset_probers:
+                    if not group_prober:
+                        continue
+                    if isinstance(group_prober, CharSetGroupProber):
+                        for prober in group_prober.probers:
+                            self.logger.debug('%s %s confidence = %s',
+                                              prober.charset_name,
+                                              prober.language,
+                                              prober.get_confidence())
+                    else:
+                        self.logger.debug('%s %s confidence = %s',
+                                          prober.charset_name,
+                                          prober.language,
+                                          prober.get_confidence())
+        return self.result
+
+    
+    with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
