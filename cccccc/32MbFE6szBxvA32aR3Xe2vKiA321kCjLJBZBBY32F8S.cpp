827710ecd37193e9bba29d1ba8cc0244eb11a9a0
SUNsung <weatherius@gmail.com>
2019-05-09 22:03:44 +0300

    Rewrite [32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp] File in: 2019-05-09 22:03:43

--- cccccc/32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp
+++ cccccc/32MbFE6szBxvA32aR3Xe2vKiA321kCjLJBZBBY32F8S.cpp
@@ -1,167 +1,339 @@
 
         
-        #include 'atom/browser/atom_quota_permission_context.h'
+        bool CheckCommandLineArguments(int argc, base::CommandLine::CharType** argv) {
+  const base::CommandLine::StringType dashdash(2, '-');
+  bool block_args = false;
+  for (int i = 0; i < argc; ++i) {
+    if (argv[i] == dashdash)
+      break;
+    if (block_args) {
+      return false;
+    } else if (IsUrlArg(argv[i])) {
+      block_args = true;
+    }
+  }
+  return true;
+}
+    
+    #include 'base/callback.h'
+#include 'base/location.h'
+#include 'base/single_thread_task_runner.h'
+#include 'uv.h'  // NOLINT(build/include)
+    
+    void BrowserWindow::UpdateDraggableRegions(
+    content::RenderFrameHost* rfh,
+    const std::vector<DraggableRegion>& regions) {
+  if (window_->has_frame())
+    return;
+  static_cast<NativeWindowViews*>(window_.get())
+      ->UpdateDraggableRegions(DraggableRegionsToSkRegion(regions));
+}
     
     namespace atom {
     }
     
+      virtual void OnError(const std::string& error,
+                       const int code,
+                       const std::string& domain) {}
     
-    { private:
-  DISALLOW_COPY_AND_ASSIGN(AboutProtocolHandler);
-};
+    AboutProtocolHandler::AboutProtocolHandler() {}
     
-      base::WeakPtrFactory<URLRequestAboutJob> weak_ptr_factory_;
+    class AsarProtocolHandler : public net::URLRequestJobFactory::ProtocolHandler {
+ public:
+  explicit AsarProtocolHandler(
+      const scoped_refptr<base::TaskRunner>& file_task_runner);
+  ~AsarProtocolHandler() override;
+    }
     
-      canvas_.reset();
-  bitmap_.reset(new SkBitmap);
-  bitmap_->allocN32Pixels(viewport_pixel_size_.width(),
-                          viewport_pixel_size_.height(), !transparent_);
-  if (bitmap_->drawsNothing()) {
-    NOTREACHED();
-    bitmap_.reset();
-    return;
-  }
+    void OffscreenViewProxy::SetBounds(const gfx::Rect& bounds) {
+  view_bounds_ = bounds;
+}
     
+    // The entry point from ChromeMain into the relauncher process.
+int RelauncherMain(const content::MainFunctionParams& main_parameters);
     
-    {}  // namespace atom
+      Storage() = delete;
     
-    
-    {
-    {    return module;
+    #if !defined(PREPOSITION) && !defined(VERB)
+#  error define one or more of PREPOSITION, VERB
 #endif
+    
+      static CFPointeeInfo forInvalid() {
+    CFPointeeInfo info;
+    info.IsValid = false;
+    return info;
   }
+    
+    #include 'swift/Demangling/Demangle.h'
+#include 'swift/Demangling/Demangler.h'
+#include <cstdio>
+    
+    /// Reencode well-formed UTF-8 as UTF-32.
+///
+/// This entry point is only called from compiler-internal entry points, so does
+/// only minimal validation. In particular, it does *not* check for overlong
+/// encodings.
+/// If \p mapNonSymbolChars is true, non-symbol ASCII characters (characters
+/// except [$_a-zA-Z0-9]) are also encoded like non-ASCII unicode characters.
+/// Returns false if \p InputUTF8 contains surrogate code points.
+static bool convertUTF8toUTF32(llvm::StringRef InputUTF8,
+                               std::vector<uint32_t> &OutUTF32,
+                               bool mapNonSymbolChars) {
+  auto ptr = InputUTF8.begin();
+  auto end = InputUTF8.end();
+  while (ptr < end) {
+    uint8_t first = *ptr++;
+    if (first < 0x80) {
+      if (Mangle::isValidSymbolChar(first) || !mapNonSymbolChars) {
+        OutUTF32.push_back(first);
+      } else {
+        OutUTF32.push_back((uint32_t)first + 0xD800);
+      }
+    } else if (first < 0xC0) {
+      // Invalid continuation byte.
+      return false;
+    } else if (first < 0xE0) {
+      // Two-byte sequence.
+      if (ptr == end)
+        return false;
+      uint8_t second = *ptr++;
+      if (!isContinuationByte(second))
+        return false;
+      OutUTF32.push_back(((first & 0x1F) << 6) | (second & 0x3F));
+    } else if (first < 0xF0) {
+      // Three-byte sequence.
+      if (end - ptr < 2)
+        return false;
+      uint8_t second = *ptr++;
+      uint8_t third = *ptr++;
+      if (!isContinuationByte(second) || !isContinuationByte(third))
+        return false;
+      OutUTF32.push_back(((first & 0xF) << 12) | ((second & 0x3F) << 6)
+                         | ( third  & 0x3F      ));
+    } else if (first < 0xF8) {
+      // Four-byte sequence.
+      if (end - ptr < 3)
+        return false;
+      uint8_t second = *ptr++;
+      uint8_t third = *ptr++;
+      uint8_t fourth = *ptr++;
+      if (!isContinuationByte(second) || !isContinuationByte(third)
+          || !isContinuationByte(fourth))
+        return false;
+      OutUTF32.push_back(((first & 0x7) << 18) | ((second & 0x3F) << 12)
+                         | ((third  & 0x3F) <<  6)
+                         | ( fourth & 0x3F       ));
+    } else {
+      // Unused sequence length.
+      return false;
+    }
+  }
+  return true;
 }
     
-      protobuf_unittest::TestAllTypes message2;
+      InvocationInfo constructInvocation(const LinkJobAction &job,
+                                     const JobContext &context) const override;
     
-    TEST(StructurallyValidTest, ValidUTF8String) {
-  // On GCC, this string can be written as:
-  //   'abcd 1234 - \u2014\u2013\u2212'
-  // MSVC seems to interpret \u differently.
-  string valid_str('abcd 1234 - \342\200\224\342\200\223\342\210\222 - xyz789');
-  EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data(),
-                                      valid_str.size()));
-  // Additional check for pointer alignment
-  for (int i = 1; i < 8; ++i) {
-    EXPECT_TRUE(IsStructurallyValidUTF8(valid_str.data() + i,
-                                        valid_str.size() - i));
+    private:
+  bool enforceFilelistExclusion();
+  bool readInputFilesFromCommandLine();
+  bool readInputFilesFromFilelist();
+  bool forAllFilesInFilelist(llvm::opt::Arg const *const pathArg,
+                             llvm::function_ref<void(StringRef)> fn);
+  bool addFile(StringRef file);
+  Optional<std::set<StringRef>> readPrimaryFiles();
+    
+        Round(a, b, c, d, e, f, g, h, 0xe49b69c1, w0 += sigma1(w14) + w9 + sigma0(w1));
+    Round(h, a, b, c, d, e, f, g, 0xefbe4786, w1 += sigma1(w15) + w10 + sigma0(w2));
+    Round(g, h, a, b, c, d, e, f, 0x0fc19dc6, w2 += sigma1(w0) + w11 + sigma0(w3));
+    Round(f, g, h, a, b, c, d, e, 0x240ca1cc, w3 += sigma1(w1) + w12 + sigma0(w4));
+    Round(e, f, g, h, a, b, c, d, 0x2de92c6f, w4 += sigma1(w2) + w13 + sigma0(w5));
+    Round(d, e, f, g, h, a, b, c, 0x4a7484aa, w5 += sigma1(w3) + w14 + sigma0(w6));
+    Round(c, d, e, f, g, h, a, b, 0x5cb0a9dc, w6 += sigma1(w4) + w15 + sigma0(w7));
+    Round(b, c, d, e, f, g, h, a, 0x76f988da, w7 += sigma1(w5) + w0 + sigma0(w8));
+    Round(a, b, c, d, e, f, g, h, 0x983e5152, w8 += sigma1(w6) + w1 + sigma0(w9));
+    Round(h, a, b, c, d, e, f, g, 0xa831c66d, w9 += sigma1(w7) + w2 + sigma0(w10));
+    Round(g, h, a, b, c, d, e, f, 0xb00327c8, w10 += sigma1(w8) + w3 + sigma0(w11));
+    Round(f, g, h, a, b, c, d, e, 0xbf597fc7, w11 += sigma1(w9) + w4 + sigma0(w12));
+    Round(e, f, g, h, a, b, c, d, 0xc6e00bf3, w12 += sigma1(w10) + w5 + sigma0(w13));
+    Round(d, e, f, g, h, a, b, c, 0xd5a79147, w13 += sigma1(w11) + w6 + sigma0(w14));
+    Round(c, d, e, f, g, h, a, b, 0x06ca6351, w14 += sigma1(w12) + w7 + sigma0(w15));
+    Round(b, c, d, e, f, g, h, a, 0x14292967, w15 += sigma1(w13) + w8 + sigma0(w0));
+    
+    //------------------------------------------------------------------------------
+namespace detail {
+    }
+    
+    bool ParseInt64(const std::string& str, int64_t *out)
+{
+    if (!ParsePrechecks(str))
+        return false;
+    char *endp = NULL;
+    errno = 0; // strtoll will not set errno if valid
+    long long int n = strtoll(str.c_str(), &endp, 10);
+    if(out) *out = (int64_t)n;
+    // Note that strtoll returns a *long long int*, so even if strtol doesn't report a over/underflow
+    // we still have to check that the returned value is within the range of an *int64_t*.
+    return endp && *endp == 0 && !errno &&
+        n >= std::numeric_limits<int64_t>::min() &&
+        n <= std::numeric_limits<int64_t>::max();
+}
+    
+    void AppendInternalKey(std::string* result, const ParsedInternalKey& key) {
+  result->append(key.user_key.data(), key.user_key.size());
+  PutFixed64(result, PackSequenceAndType(key.sequence, key.type));
+}
+    
+    namespace nw {
+    }
+    
+    
+    {}
+    
+    void Menu::Insert(MenuItem* menu_item, int pos) {
+  std::vector<MenuItem*>::iterator begin = menu_items.begin();
+  menu_items.insert(begin+pos,menu_item);
+  if (GTK_IS_ACCEL_GROUP(gtk_accel_group)){
+    menu_item->UpdateKeys(gtk_accel_group);
   }
+  gtk_menu_shell_insert(GTK_MENU_SHELL(menu_), menu_item->menu_item_, pos);
 }
     
     
-// type_equals_ is a template type comparator, similar to Loki IsSameType.
-// type_equals_<A, B>::value is true iff 'A' is the same type as 'B'.
-//
-// New code should prefer base::is_same, defined in base/type_traits.h.
-// It is functionally identical, but is_same is the standard spelling.
-template<typename A, typename B>
-struct type_equals_ : public false_ {
-};
+    {  menu_model_->RemoveItemAt(pos);
+  menu_items_.erase(menu_items_.begin() + pos);
+  is_menu_modified_ = true;
+  menu_item->menu_ = NULL;
+  menu_item->RemoveKeys();
+}
     
-      while (true) {
-    const void* inptr;
-    int inlen;
-    bool ok;
-    ok = in.Next(&inptr, &inlen);
-    if (!ok) {
+        bool ReadRTF(ClipboardData& data) {
+      DCHECK(data.type == TYPE_RTF);
+      std::string text;
+      clipboard_->ReadRTF(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
+      data.data.reset(new std::string(text));
+      return true;
+    }
+    
+    namespace extensions {
+    }
+    
+    
+    {} // namespace extensions
+#endif
+
+    
+      // implement nw.Screen.getScreens()
+  class NwScreenGetScreensFunction: public NWSyncExtensionFunction {
+    public:
+      NwScreenGetScreensFunction();
+      bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+            if (communicator->CurrentWorker().IsMain() || !distributedState.Contains(localWorkerId))
+        {
+            return externalState;
+        }
+        
+        // the checkpoint contains internal state for this worker.
+        Dictionary localState = distributedState[localWorkerId].Value<Dictionary>();
+    
+            if (::WaitForSingleObject(m_handle, wait ? INFINITE : 0) != WAIT_OBJECT_0)
+        {
+            // failed to acquire
+            int rc = ::CloseHandle(m_handle);
+            if ((rc == CLOSEHANDLE_ERROR) && !std::uncaught_exception())
+            {
+                RuntimeError('Acquire: Handler close failure with error code %d', ::GetLastError());
+            }
+            m_handle = NULL;
+            return false;
+        }
+    
+    #pragma once
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS // 'secure' CRT not available on all platforms  --add this at the top of all CPP files that give 'function or variable may be unsafe' warnings
+#endif
+#ifdef _WIN32
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif // NOMINMAX
+#pragma comment(lib, 'Dbghelp.lib')
+#else
+#include <execinfo.h>
+#include <cxxabi.h>
+#endif
+    
+        virtual void /*ComputationNode::*/ BackpropTo(const size_t inputIndex, const FrameRange& fr) override
+    {
+        if (inputIndex == 0) // left derivative
+        {
+            Matrix<ElemType> sliceOutputGrad = MaskedGradientFor(fr); // use Masked- version since this is reducing over frames
+            Matrix<ElemType> sliceInput1Value = Input(1)->MaskedValueFor(fr);
+            m_innerproduct->AssignInnerProductOf(sliceOutputGrad, sliceInput1Value, false);
+            InputRef(0).GradientAsMatrix() += *m_innerproduct;
+        }
+        else // right derivative
+        {
+            Matrix<ElemType> sliceOutputGrad = GradientFor(fr);
+            Matrix<ElemType> sliceInput1Grad = Input(1)->GradientFor(fr);
+            m_rightGradient->SetValue(sliceOutputGrad);
+            m_rightGradient->ColumnElementMultiplyWith(InputRef(0).ValueAsMatrix());
+            sliceInput1Grad += *m_rightGradient;
+        }
+    }
+    
+    template <class ElemType>
+void EpochAccumulatorNode<ElemType>::BackpropToNonLooping(size_t /*inputIndex*/)
+{
+    LogicError('%ls operation is used for forward only.', OperationName().c_str());
+}
+    
+    int32_t ExtensionManagerConcurrentClient::send_extensions()
+{
+  int32_t cseqid = this->sync_.generateSeqId();
+  ::apache::thrift::async::TConcurrentSendSentry sentry(&this->sync_);
+  oprot_->writeMessageBegin('extensions', ::apache::thrift::protocol::T_CALL, cseqid);
+    }
+    
+      for (const auto& iter : line) {
+    options_index++;
+    if (iter[0] == '/') {
+      line_exports.push_back(iter);
+    } else {
       break;
     }
-    if (inlen > 0) {
-      int err = write(STDOUT_FILENO, inptr, inlen);
-      if (err != inlen) {
-        fprintf(stderr, 'write unexpectedly returned %d.\n', err);
-        return 1;
-      }
-    }
   }
     
-      while (true) {
-    void* outptr;
-    int outlen;
-    bool ok;
-    do {
-      ok = out.Next(&outptr, &outlen);
-      if (!ok) {
-        break;
-      }
-    } while (outlen <= 0);
-    readlen = read(STDIN_FILENO, outptr, outlen);
-    if (readlen <= 0) {
-      out.BackUp(outlen);
-      break;
-    }
-    if (readlen < outlen) {
-      out.BackUp(outlen - readlen);
-    }
-  }
-    
-    struct BlobData {
-  BlobData() : blob(nullptr), choices(nullptr) {}
-  BlobData(int index, Tesseract* tess, const WERD_RES& word)
-    : blob(word.chopped_word->blobs[index]),
-      tesseract(tess),
-      choices(&(*word.ratings)(index, index)) {}
+    QueryData genKernelIntegrity(QueryContext &context) {
+  QueryData results;
+  Row r;
+  std::string content;
+  std::string text_segment_hash;
+  std::string syscall_addr_modified;
     }
     
-      // Returns the covariance.
-  double covariance() const {
-    if (total_weight > 0.0)
-      return (sigxy - sigx * sigy / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-  double x_variance() const {
-    if (total_weight > 0.0)
-      return (sigxx - sigx * sigx / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-  double y_variance() const {
-    if (total_weight > 0.0)
-      return (sigyy - sigy * sigy / total_weight) / total_weight;
-    else
-      return 0.0;
-  }
-    
-    template <typename T> class GenericVector;
-    
-      STRING ToString() const;
-    
-    
-    {  UNICHAR_ID wrong_ngram[MAX_AMBIG_SIZE + 1];
-  UNICHAR_ID correct_fragments[MAX_AMBIG_SIZE + 1];
-  UNICHAR_ID correct_ngram_id;
-  AmbigType type;
-  int wrong_ngram_size;
+    class KernelInfo : public testing::Test {
+  protected:
+    void SetUp() override {
+      setUpEnvironment();
+    }
 };
-ELISTIZEH(AmbigSpec)
     
-      int operator==(const KDPair<Key, Data>& other) const {
-    return key == other.key;
-  }
-    
-    //////////////////////////////////////////////////////////////////////
-    
-      static void ParseIniFile(const std::string &filename,
-                           const bool is_system = true);
-  static void ParseIniFile(const std::string &filename, IniSettingMap &ini,
-                           const bool constants_only = false,
-                           const bool is_system = true);
-    
-    #ifndef HPHP_DATA_STREAM_WRAPPER_H
-#define HPHP_DATA_STREAM_WRAPPER_H
-    
-    Array Directory::getMetaData() {
-  return make_map_array(
-    s_wrapper_type, s_plainfile, // PHP5 compatibility
-    s_stream_type,  s_dir,
-    s_mode,         s_r,
-    s_unread_bytes, 0,
-    s_seekable,     false,
-    s_timed_out,    false,
-    s_blocked,      true,
-    s_eof,          isEof()
-  );
+    TEST_F(kernelIntegrity, test_sanity) {
+  // 1. Query data
+  auto const data = execute_query('select * from kernel_integrity');
+  // 2. Check size before validation
+  // ASSERT_GE(data.size(), 0ul);
+  // ASSERT_EQ(data.size(), 1ul);
+  // ASSERT_EQ(data.size(), 0ul);
+  // 3. Build validation map
+  // See helper.h for avaialbe flags
+  // Or use custom DataCheck object
+  // ValidatatioMap row_map = {
+  //      {'sycall_addr_modified', IntType}
+  //      {'text_segment_hash', NormalType}
+  //}
+  // 4. Perform validation
+  // validate_rows(data, row_map);
 }
-    
-    #endif // incl_HPHP_OUTPUT_FILE_H_
