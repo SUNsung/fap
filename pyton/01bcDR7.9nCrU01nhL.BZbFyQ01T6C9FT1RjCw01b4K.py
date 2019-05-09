0cf8b575db54433d76125fd2471ba10da8e3b6d0
SUNsung <weatherius@gmail.com>
2019-05-10 01:24:09 +0300

    Rewrite [01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py] File in: 2019-05-10 01:24:08

--- pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
+++ pyton/01bcDR7.9nCrU01nhL.BZbFyQ01T6C9FT1RjCw01b4K.py
@@ -1,170 +1,228 @@
 
         
-        
-class Cache(object):
+            with open('README.md', 'w+') as sorted_file:
+        # Then all of the blocks are sorted individually
+        blocks = [''.join(sorted(block, key=lambda s: s.lower())) for block in blocks]
+        # And the result is written back to README.md
+        sorted_file.write(''.join(blocks))
     
-      model.train_model(datasets)
+        @property
+    def config(self):
+        if not hasattr(self, '_config'):
+            self._config = Config(directory=self.config_dir)
+            if self._config.is_new():
+                self._config.save()
+            else:
+                self._config.load()
+        return self._config
     
-        # Loop through the 2D matrix of word_patches and score each.
-    for i, row in enumerate(word_patches):
-      print('Reset RNN states.')
-      self.reset()  # reset states before processing each row.
-      row_probs = np.zeros([batch_size, 0])
-      for j, word_patch in enumerate(row):
-        print('Processing patch '
-              '({}, {}) / ({}, {})'.format(i+1, j+1, nrow, ncol))
-        patch_probs = (self._score(word_patch) if word_patch else
-                       np.zeros([batch_size, num_timesteps]))
-        row_probs = np.concatenate([row_probs, patch_probs], 1)
-      probs = np.concatenate([probs, row_probs], 0)
-    return probs
+            See https://github.com/jakubroztocil/httpie/issues/212
     
-      train_data = _read_words(train_path)
-  valid_data = _read_words(valid_path)
-  return train_data, valid_data
     
-    from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+def humanize_bytes(n, precision=2):
+    # Author: Doug Latornell
+    # Licence: MIT
+    # URL: http://code.activestate.com/recipes/577081/
+    '''Return a humanized string representation of a number of bytes.
     
-      # The unique ngrams in the training set.
-  unique_ngrams_in_train = 0.
     
-      # Dictionary mapping.
-  if model == 'gen':
-    variable_mapping = {
-        'Model/embedding': embedding,
-        'Model/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel': lstm_w_0,
-        'Model/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias': lstm_b_0,
-        'Model/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel': lstm_w_1,
-        'Model/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias': lstm_b_1,
-        'Model/softmax_w': softmax_w,
-        'Model/softmax_b': softmax_b
-    }
-  else:
-    if FLAGS.dis_share_embedding:
-      variable_mapping = {
-          'Model/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel': lstm_w_0,
-          'Model/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias': lstm_b_0,
-          'Model/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel': lstm_w_1,
-          'Model/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias': lstm_b_1
-      }
-    else:
-      variable_mapping = {
-          'Model/embedding': embedding,
-          'Model/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/kernel': lstm_w_0,
-          'Model/RNN/multi_rnn_cell/cell_0/basic_lstm_cell/bias': lstm_b_0,
-          'Model/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/kernel': lstm_w_1,
-          'Model/RNN/multi_rnn_cell/cell_1/basic_lstm_cell/bias': lstm_b_1
-      }
+@mock.patch('httpie.input.AuthCredentials._getpass',
+            new=lambda self, prompt: 'UNEXPECTED_PROMPT_RESPONSE')
+def test_auth_plugin_prompt_password_false(httpbin):
     
-            # The rule matches AWS only if all rule data fields are equal
-        # to their corresponding local value defined in the task
-        return all([
-            getattr(self.rule, field) == aws_rule_data.get(field, None)
-            for field in self.RULE_FIELDS
-        ])
+    import pytest
     
-    # Create a snapshot only if the most recent one is older than 1 hour
-- local_action:
-    module: ec2_snapshot
-    volume_id: vol-abcdef12
-    last_snapshot_min_age: 60
+        def load(self):
+        super(Config, self).load()
+        self._migrate_implicit_content_type()
+    
+        Uses threading to periodically update the status (speed, ETA, etc.).
+    
+    
+def generateKey(keySize):
+    print('Generating prime p...')
+    p = rabinMiller.generateLargePrime(keySize)  # select large prime number.
+    e_1 = primitiveRoot(p)  # one primitive root on modulo p.
+    d = random.randrange(3, p)  # private_key -> have to be greater than 2 for safety.
+    e_2 = cryptoMath.findModInverse(pow(e_1, d, p), p)
+    
+    try:
+	xrange		#Python 2
+except NameError:
+	xrange = range	#Python 3
+    
+    * @author chinmoy159
+* @version 1.0 dated 10/08/2017
 '''
     
-    - heroku_collaborator:
-    api_key: YOUR_API_KEY
-    user: '{{ item.user }}'
-    apps: '{{ item.apps | default(apps) }}'
-    suppress_invitation: '{{ item.suppress_invitation | default(suppress_invitation) }}'
-    state: '{{ item.state | default('present') }}'
-  with_items:
-    - { user: 'a.b@example.com' }
-    - { state: 'absent', user: 'b.c@example.com', suppress_invitation: false }
-    - { user: 'x.y@example.com', apps: ['heroku-example-app'] }
-'''
+    site_info = 'ehow.com'
+download = ehow_download
+download_playlist = playlist_not_supported('ehow')
+
     
-        def role_remove_group(self, name, item):
-        return self.role_remove_member(name=name, item={'group': item})
+    site_info = 'ifeng.com'
+download = ifeng_download
+download_playlist = playlist_not_supported('ifeng')
+
     
-        if module.params['user']:
-        params['deploy[local_username]'] = module.params['user']
+        def extract(self, **kwargs):
+        if 'stream_id' in kwargs and kwargs['stream_id']:
+            i = kwargs['stream_id']
+            if 'size' not in self.streams[i]:
+                self.streams[i]['size'] = urls_size(self.streams[i]['src'])
     
-    from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils._text import to_native
-from ansible.module_utils.urls import fetch_url
+        # Print the location of each face in this image
+    top, right, bottom, left = face_location
+    print('A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}'.format(top, left, bottom, right))
     
-        # Deploy params
-    revision_id = module.params['revision_id']
-    deployed_by = module.params['deployed_by']
-    deployed_to = module.params['deployed_to']
-    repository = module.params['repository']
+        if len(unknown_face_encodings) > 0:
+        face_found = True
+        # See if the first face in the uploaded image matches the known face of Obama
+        match_results = face_recognition.compare_faces([known_face_encoding], unknown_face_encodings[0])
+        if match_results[0]:
+            is_obama = True
     
-    # Used for remembering the file (and its contents)
-# so we don't have to open the same file again.
-_filename = None
-_contents = None
+        pool.starmap(test_image, function_parameters)
     
-        def syntax(self):
-        return '[options] <spider>'
+        function_parameters = zip(
+        images_to_check,
+        itertools.repeat(known_names),
+        itertools.repeat(known_face_encodings),
+        itertools.repeat(tolerance),
+        itertools.repeat(show_distance)
+    )
     
-        def _print_response(self, response, opts):
-        if opts.headers:
-            self._print_headers(response.request.headers, b'>')
-            print('>')
-            self._print_headers(response.headers, b'<')
-        else:
-            self._print_bytes(response.body)
+        # 载入用户上传的图片
+    img = face_recognition.load_image_file(file_stream)
+    # 为用户上传的图片中的人脸编码
+    unknown_face_encodings = face_recognition.face_encodings(img)
     
-        def _find_template(self, template):
-        template_file = join(self.templates_dir, '%s.tmpl' % template)
-        if exists(template_file):
-            return template_file
-        print('Unable to find template: %s\n' % template)
-        print('Use 'scrapy genspider --list' to see all available templates.')
+        # Apply some eyeliner
+    d.line(face_landmarks['left_eye'] + [face_landmarks['left_eye'][0]], fill=(0, 0, 0, 110), width=6)
+    d.line(face_landmarks['right_eye'] + [face_landmarks['right_eye'][0]], fill=(0, 0, 0, 110), width=6)
     
-    class Command(ScrapyCommand):
+        # Loop through each face in this frame of video
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
     
-        def short_desc(self):
-        return 'Print Scrapy version'
+    # Release handle to the webcam
+video_capture.release()
+cv2.destroyAllWindows()
+
     
-            def __init__(self, method=SSL.SSLv23_METHOD, *args, **kwargs):
-            super(ScrapyClientContextFactory, self).__init__(*args, **kwargs)
-            self._ssl_method = method
+            # 更新聚类中心
+        log.info(centers)
+        for i in range(k):
+            data_i = data[ret[:, 0] == i]  # 标签为 i 的样本
+            centers[i, :] = np.mean(data_i, axis=0)  # 按类别过滤样本
     
-        if twisted_version < (17, 0, 0):
-        from twisted.internet._sslverify import _maybeSetHostNameIndication
-        set_tlsext_host_name = _maybeSetHostNameIndication
-    else:
-        def set_tlsext_host_name(connection, hostNameBytes):
-            connection.set_tlsext_host_name(hostNameBytes)
-    
-    
-def _parsed_url_args(parsed):
-    # Assume parsed is urlparse-d from Request.url,
-    # which was passed via safe_url_string and is ascii-only.
-    b = lambda s: to_bytes(s, encoding='ascii')
-    path = urlunparse(('', '', parsed.path or '/', parsed.params, parsed.query, ''))
-    path = b(path)
-    host = b(parsed.hostname)
-    port = parsed.port
-    scheme = b(parsed.scheme)
-    netloc = b(parsed.netloc)
-    if port is None:
-        port = 443 if scheme == b'https' else 80
-    return scheme, netloc, host, port, path
-    
-        def _debug_set_cookie(self, response, spider):
-        if self.debug:
-            cl = [to_native_str(c, errors='replace')
-                  for c in response.headers.getlist('Set-Cookie')]
-            if cl:
-                cookies = '\n'.join('Set-Cookie: {}\n'.format(c) for c in cl)
-                msg = 'Received cookies from: {}\n{}'.format(response, cookies)
-                logger.debug(msg, extra={'spider': spider})
+        with tf.variable_scope(name or 'attention_for_rnn', reuse=reuse):
+        a = permute(x, [0, 2, 1])  # [batch_size, n_input, n_step]
+        a = tf.reshape(a, [-1, n_step])  # [batch_size*n_input, n_step]
     
     
-def highway_dense(x, act_fn=relu, carry_bias=-1.0, name=None):
-    '''用于全连接层的 highway
+def linear_dense(x, n_unit, name=None, reuse=None):
+    '''线性全连接层
     Input shape:  [batch_size, n_input]
-    Output shape: [batch_size, n_input]
+    Output shape: [batch_size, n_unit]
+    
+    
+def update_version(release, sentry_models):
+    old_version = release.version
+    try:
+        project_slug = release.projects.values_list('slug', flat=True)[0]
+    except IndexError:
+        # delete releases if they have no projects
+        release.delete()
+        return
+    new_version = ('%s-%s' % (project_slug, old_version))[:64]
+    sentry_models.Release.objects.filter(id=release.id).update(version=new_version)
+    sentry_models.TagValue.objects.filter(
+        project__in=release.projects.all(), key='sentry:release', value=old_version
+    ).update(value=new_version)
+    
+        complete_apps = ['sentry']
+
+    
+    
+class Migration(SchemaMigration):
+    def forwards(self, orm):
+        # Adding model 'ProcessingIssue'
+        db.create_table(
+            'sentry_processingissue', (
+                (
+                    'id', self.gf('sentry.db.models.fields.bounded.BoundedBigAutoField')(
+                        primary_key=True
+                    )
+                ), (
+                    'project', self.gf('sentry.db.models.fields.foreignkey.FlexibleForeignKey')(
+                        to=orm['sentry.Project']
+                    )
+                ), (
+                    'checksum',
+                    self.gf('django.db.models.fields.CharField')(max_length=40, db_index=True)
+                ), ('type', self.gf('django.db.models.fields.CharField')(max_length=30)),
+                ('data', self.gf('sentry.db.models.fields.gzippeddict.GzippedDictField')()), (
+                    'datetime',
+                    self.gf('django.db.models.fields.DateTimeField')()
+                ),
+            )
+        )
+        db.send_create_signal('sentry', ['ProcessingIssue'])
+    
+            # Changing field 'ReleaseEnvironment.project_id'
+        if is_postgres():
+            db.execute(
+                'ALTER TABLE sentry_environmentrelease ALTER COLUMN project_id DROP NOT NULL'
+            )
+        else:
+            db.alter_column(
+                'sentry_environmentrelease',
+                'project_id',
+                self.gf('sentry.db.models.fields.bounded.BoundedPositiveIntegerField')(null=True)
+            )
+    
+    
+class Migration(DataMigration):
+    def forwards(self, orm):
+        db.commit_transaction()
+        try:
+            self._forwards(orm)
+        except Exception:
+            # Explicitly resume the transaction because
+            # South is going to try and roll it back, but when
+            # it can't find one, it'll error itself, masking
+            # the actual exception being raised
+            #
+            # See https://github.com/getsentry/sentry/issues/5035
+            db.start_transaction()
+            raise
+        db.start_transaction()
+    
+            # Adding field 'ApiKey.scope_list'
+        db.add_column(
+            'sentry_apikey',
+            'scope_list',
+            self.gf('sentry.db.models.fields.array.ArrayField')(
+                of=('django.db.models.fields.TextField', [], {})
+            ),
+            keep_default=False
+        )
+    
+            for model in (
+            orm['sentry.ApiAuthorization'], orm['sentry.ApiGrant'], orm['sentry.ApiKey'],
+            orm['sentry.ApiToken'],
+        ):
+            queryset = model.objects.all()
+            for obj in RangeQuerySetWrapperWithProgressBar(queryset):
+                if not obj.scope_list:
+                    handler = make_handler(obj.scopes)
+                    model.objects.filter(
+                        id=obj.id,
+                    ).update(
+                        scope_list=[k for k, v in six.iteritems(handler) if v],
+                    )
+    
+            # Adding unique constraint on 'CommitAuthor', fields ['organization_id', 'external_id']
+        db.create_unique('sentry_commitauthor', ['organization_id', 'external_id'])
