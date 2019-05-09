9be82b29a53b2a53afaa5131e5edbf02b886050d
SUNsung <weatherius@gmail.com>
2019-05-10 00:55:10 +0300

    Rewrite [12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py] File in: 2019-05-10 00:55:10

--- pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
+++ pyton/12uel7VeGalu.12rKZ9KtCHwXo12qH.RuaplRtg12I2w.py
@@ -1,28 +1,79 @@
 
         
-            return publicKey, privateKey
+            @property
+    def body(self):
+        body = self._orig.body
+        if isinstance(body, str):
+            # Happens with JSON/form request data parsed from the command line.
+            body = body.encode('utf8')
+        return body or b''
+
     
-    def rabinMiller(num):
-    s = num - 1
-    t = 0
     
+def test_max_redirects(httpbin):
+    r = http('--max-redirects=1', '--follow', httpbin.url + '/redirect/3',
+             error_exit_ok=True)
+    assert r.exit_status == ExitStatus.ERROR_TOO_MANY_REDIRECTS
+
     
-class HashTable:
+        def _get_path(self):
+        return os.path.join(self.directory, self.name + '.json')
+    
+            if ext:
+            fn += ext
+    
     '''
-        Basic Hash Table example with open addressing and linear probing
-    '''
+requests._internal_utils
+~~~~~~~~~~~~~~
     
-        tree = Decision_Tree(depth = 10, min_leaf_size = 10)
-    tree.train(X,y)
+            Punctuation:               'bold #000000',   # class: 'p'
     
-        difference = predict - actual
-    square_diff = np.square(difference)
-    mean_square_diff = square_diff.mean()
-    score = np.sqrt(mean_square_diff)
-    return score
+        # Check chardet for compatibility.
+    major, minor, patch = chardet_version.split('.')[:3]
+    major, minor, patch = int(major), int(minor), int(patch)
+    # chardet >= 3.0.2, < 3.1.0
+    assert major == 3
+    assert minor < 1
+    assert patch >= 2
     
-        return res
+        expires = None
+    if morsel['max-age']:
+        try:
+            expires = int(time.time() + int(morsel['max-age']))
+        except ValueError:
+            raise TypeError('max-age: %s must be integer' % morsel['max-age'])
+    elif morsel['expires']:
+        time_template = '%a, %d-%b-%Y %H:%M:%S GMT'
+        expires = calendar.timegm(
+            time.strptime(morsel['expires'], time_template)
+        )
+    return create_cookie(
+        comment=morsel['comment'],
+        comment_url=bool(morsel['comment']),
+        discard=False,
+        domain=morsel['domain'],
+        expires=expires,
+        name=morsel.key,
+        path=morsel['path'],
+        port=None,
+        rest={'HttpOnly': morsel['httponly']},
+        rfc2109=False,
+        secure=bool(morsel['secure']),
+        value=morsel.value,
+        version=morsel['version'] or 0,
+    )
     
+            with server as (host, port):
+            server_url = 'http://{}:{}'.format(host, port)
+            for _ in range(requests_to_handle):
+                r = requests.get(server_url)
+                assert r.status_code == 200
     
-def random_letters(ctbi, i):
-  pass  # Put your code here...
+            for field, val in fields:
+            if isinstance(val, basestring) or not hasattr(val, '__iter__'):
+                val = [val]
+            for v in val:
+                if v is not None:
+                    # Don't call str() on bytestrings: in Py3 it all goes wrong.
+                    if not isinstance(v, bytes):
+                        v = str(v)
