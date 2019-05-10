318d20ae2f4a35347c91a1a37cd78768bb069c62
SUNsung <weatherius@gmail.com>
2019-05-10 22:24:17 +0300

    Rewrite [28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp] File in: 2019-05-10 22:24:17

--- cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
+++ cccccc/28lhOdaSfkQ2M28EinVXva6ImI28224UUxLh7A.28X7f.cpp
@@ -1,207 +1,346 @@
 
         
-        #define TEGRA_ADDF(src1, sz1, src2, sz2, dst, sz, w, h) \
-( \
-    CAROTENE_NS::isSupportedConfiguration() ? \
-    CAROTENE_NS::add(CAROTENE_NS::Size2D(w, h), \
-                     src1, sz1, \
-                     src2, sz2, \
-                     dst, sz), \
-    CV_HAL_ERROR_OK \
-    : CV_HAL_ERROR_NOT_IMPLEMENTED \
-)
+         protected:
+  explicit AutoUpdater(v8::Isolate* isolate);
+  ~AutoUpdater() override;
     
-        void convertScale(const Size2D &_size,
-                      const u8 * srcBase, ptrdiff_t srcStride,
-                      f32 * dstBase, ptrdiff_t dstStride,
-                      f64 alpha, f64 beta);
-    
-    void accumulateWeighted(const Size2D &size,
-                        const u8 *srcBase, ptrdiff_t srcStride,
-                        u8 *dstBase, ptrdiff_t dstStride,
-                        f32 alpha)
-{
-    if (alpha == 0.0f)
-        return;
-    if (alpha == 1.0f)
-    {
-        for (size_t i = 0; i < size.height; ++i)
-        {
-            const u8 * src = internal::getRowPtr(srcBase, srcStride, i);
-            u8 * dst = internal::getRowPtr(dstBase, dstStride, i);
-            std::memcpy(dst, src, sizeof(u8) * size.width);
-        }
-        return;
-    }
+    class Net : public mate::EventEmitter<Net> {
+ public:
+  static v8::Local<v8::Value> Create(v8::Isolate* isolate);
     }
     
-    
-    {
-    {         vst1q_f32(_dst + i, vline1_f32);
-         vst1q_f32(_dst + i + 4, vline2_f32);
-         vst1q_f32(_dst + i + 8, vline3_f32);
-         vst1q_f32(_dst + i + 12, vline4_f32);
-     }
-})
-#endif
-    
-    #include 'common.hpp'
-    
-    void fillMinMaxLocs(const Size2D & size,
-                    const u32 * srcBase, ptrdiff_t srcStride,
-                    u32 minVal, size_t * minLocPtr, s32 & minLocCount, s32 minLocCapacity,
-                    u32 maxVal, size_t * maxLocPtr, s32 & maxLocCount, s32 maxLocCapacity)
-{
-    internal::assertSupportedConfiguration();
-#ifdef CAROTENE_NEON
-    size_t roiw8 = size.width >= 7 ? size.width - 7 : 0;
-    }
-    
-    #ifndef GOOGLE_PROTOBUF_PYTHON_CPP_SCOPED_PYOBJECT_PTR_H__
-#define GOOGLE_PROTOBUF_PYTHON_CPP_SCOPED_PYOBJECT_PTR_H__
-    
-      auto* message1_on_arena =
-      Arena::CreateMessage<protobuf_unittest::TestAllTypes>(&arena1);
-  TestUtil::SetAllFields(message1_on_arena);
-  const auto* nested = &message1_on_arena->optional_nested_message();
-    
-    // Author: liujisi@google.com (Pherl Liu)
-    
-    // Author: kenton@google.com (Kenton Varda)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-    
-    void MapLiteTestUtil::ExpectMapFieldsModified(
-    const unittest::TestMapLite& message) {
-  MapTestUtilImpl::ExpectMapFieldsModified<unittest::MapEnumLite,
-                                           unittest::MAP_ENUM_BAR_LITE,
-                                           unittest::MAP_ENUM_FOO_LITE>(
-      message);
+    void AtomQuotaPermissionContext::RequestQuotaPermission(
+    const content::StorageQuotaParams& params,
+    int render_process_id,
+    const PermissionCallback& callback) {
+  callback.Run(response::QUOTA_PERMISSION_RESPONSE_ALLOW);
 }
     
-    // Copyright 2008 Google Inc. All Rights Reserved.
-// Author: xpeng@google.com (Peter Peng)
+    #include 'base/macros.h'
+#include 'build/build_config.h'
+#include 'native_mate/arguments.h'
     
-    #endif  // GOOGLE_PROTOBUF_TEMPLATE_UTIL_H_
-
+    #include 'atom/browser/relauncher.h'
     
-    // Author: brianolson@google.com (Brian Olson)
-//  Based on original Protocol Buffers design by
-//  Sanjay Ghemawat, Jeff Dean, and others.
-//
-// Test program to verify that GzipInputStream is compatible with command line
-// gunzip or java.util.zip.GzipInputStream
-//
-// Reads gzip stream on standard input and writes decompressed data to standard
-// output.
     
-      // Parsing accepts an fractional digits as long as they fit into nano
-  // precision.
-  EXPECT_TRUE(TimeUtil::FromString('1970-01-01T00:00:00.1Z', &time));
-  EXPECT_EQ(100000000, TimeUtil::TimestampToNanoseconds(time));
-  EXPECT_TRUE(TimeUtil::FromString('1970-01-01T00:00:00.0001Z', &time));
-  EXPECT_EQ(100000, TimeUtil::TimestampToNanoseconds(time));
-  EXPECT_TRUE(TimeUtil::FromString('1970-01-01T00:00:00.0000001Z', &time));
-  EXPECT_EQ(100, TimeUtil::TimestampToNanoseconds(time));
+    {  DISALLOW_COPY_AND_ASSIGN(RootViewMac);
+};
     
-        ListNode* curNode = head;
-    while(curNode != NULL){
-        ListNode* delNode = curNode;
-        curNode = curNode->next;
-        delete delNode;
+    namespace atom {
     }
     
-    int main() {
+    REGISTER_CPU_OPERATOR(
+    MergeSingleListFeatureTensors,
+    MergeSingleListFeatureTensorsOp<CPUContext>);
+OPERATOR_SCHEMA(MergeSingleListFeatureTensors)
+    .SetDoc(
+        'Merge given single-feature tensors with list features into one '
+        'multi-feature tensor.' +
+        doc)
+    .NumInputs([](int n) { return n >= 3 && n % 3 == 0; })
+    .NumOutputs(4)
+    .Input(0, 'in1_lengths', '.lengths')
+    .Input(1, 'in1_values', '.values')
+    .Input(2, 'in1_presence', '.presence')
+    .Output(0, 'out_lengths', '.lengths')
+    .Output(1, 'out_keys', '.keys')
+    .Output(2, 'out_values_lengths', '.values.lengths')
+    .Output(3, 'out_values_values', '.values.values')
+    .Arg('feature_ids', 'feature ids');
+    
+    X before running op:
+[[ 3.813361   -1.319647    5.2089314  -4.931328    0.6218652 ]
+ [ 7.2757645   5.5552588   5.785643   -2.4790506  -0.41400087]
+ [ 1.1541046  -6.933266    3.3754056   1.6569928  -1.7670316 ]
+ [-3.4932013   4.891472    1.5530115  -3.2443287  -4.605099  ]
+ [-4.574543   -7.360948    5.91305    -8.196495   -5.357458  ]]
+X after running op:
+[[ 3. -2.  5. -5.  0.]
+ [ 7.  5.  5. -3. -1.]
+ [ 1. -7.  3.  1. -2.]
+ [-4.  4.  1. -4. -5.]
+ [-5. -8.  5. -9. -6.]]
+    
+    void UnregisterShellMenu(std::wstring opt, wchar_t* keyBaseName)
+{
+	HKEY root = GetRootKey(opt);
+	HKEY cmderKey;
+	FAIL_ON_ERROR(RegCreateKeyEx(root, keyBaseName, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, NULL, &cmderKey, NULL));
+	FAIL_ON_ERROR(RegDeleteTree(cmderKey, NULL));
+	RegDeleteKeyEx(root, keyBaseName, KEY_ALL_ACCESS, NULL);
+	RegCloseKey(cmderKey);
+	RegCloseKey(root);
+}
+    
+      virtual void Update(HostDeviceVector<GradientPair>* in_gpair, DMatrix* data,
+                      gbm::GBLinearModel* model,
+                      double sum_instance_weight) = 0;
+    
+    
+    { private:
+  /*! \brief the underlying stream */
+  dmlc::Stream *stream_;
+  /*! \brief buffer to hold data */
+  std::string buffer_;
+  /*! \brief length of valid data in buffer */
+  size_t read_len_;
+  /*! \brief pointer in the buffer */
+  size_t read_ptr_;
+};
+    
+    SparsePageWriter::~SparsePageWriter() {
+  for (auto& queue : qworkers_) {
+    // use nullptr to signal termination.
+    std::shared_ptr<SparsePage> sig(nullptr);
+    queue.Push(std::move(sig));
+  }
+  for (auto& thread : workers_) {
+    thread->join();
+  }
+}
+    
+          ASSERT_TRUE(input == output);
+    
+    namespace xgboost {
+namespace common {
+/*!
+ * \brief experimental wsummary
+ * \tparam DType type of data content
+ * \tparam RType type of rank
+ */
+template<typename DType, typename RType>
+struct WQSummary {
+  /*! \brief an entry in the sketch summary */
+  struct Entry {
+    /*! \brief minimum rank */
+    RType rmin;
+    /*! \brief maximum rank */
+    RType rmax;
+    /*! \brief maximum weight */
+    RType wmin;
+    /*! \brief the value of data */
+    DType value;
+    // constructor
+    XGBOOST_DEVICE Entry() {}  // NOLINT
+    // constructor
+    XGBOOST_DEVICE Entry(RType rmin, RType rmax, RType wmin, DType value)
+        : rmin(rmin), rmax(rmax), wmin(wmin), value(value) {}
+    /*!
+     * \brief debug function,  check Valid
+     * \param eps the tolerate level for violating the relation
+     */
+    inline void CheckValid(RType eps = 0) const {
+      CHECK(rmin >= 0 && rmax >= 0 && wmin >= 0) << 'nonneg constraint';
+      CHECK(rmax- rmin - wmin > -eps) <<  'relation constraint: min/max';
+    }
+    /*! \return rmin estimation for v strictly bigger than value */
+    XGBOOST_DEVICE inline RType RMinNext() const {
+      return rmin + wmin;
+    }
+    /*! \return rmax estimation for v strictly smaller than value */
+    XGBOOST_DEVICE inline RType RMaxPrev() const {
+      return rmax - wmin;
+    }
+  };
+  /*! \brief input data queue before entering the summary */
+  struct Queue {
+    // entry in the queue
+    struct QEntry {
+      // value of the instance
+      DType value;
+      // weight of instance
+      RType weight;
+      // default constructor
+      QEntry() = default;
+      // constructor
+      QEntry(DType value, RType weight)
+          : value(value), weight(weight) {}
+      // comparator on value
+      inline bool operator<(const QEntry &b) const {
+        return value < b.value;
+      }
+    };
+    // the input queue
+    std::vector<QEntry> queue;
+    // end of the queue
+    size_t qtail;
+    // push data to the queue
+    inline void Push(DType x, RType w) {
+      if (qtail == 0 || queue[qtail - 1].value != x) {
+        queue[qtail++] = QEntry(x, w);
+      } else {
+        queue[qtail - 1].weight += w;
+      }
+    }
+    inline void MakeSummary(WQSummary *out) {
+      std::sort(queue.begin(), queue.begin() + qtail);
+      out->size = 0;
+      // start update sketch
+      RType wsum = 0;
+      // construct data with unique weights
+      for (size_t i = 0; i < qtail;) {
+        size_t j = i + 1;
+        RType w = queue[i].weight;
+        while (j < qtail && queue[j].value == queue[i].value) {
+          w += queue[j].weight; ++j;
+        }
+        out->data[out->size++] = Entry(wsum, wsum + w, w, queue[i].value);
+        wsum += w; i = j;
+      }
+    }
+  };
+  /*! \brief data field */
+  Entry *data;
+  /*! \brief number of elements in the summary */
+  size_t size;
+  // constructor
+  WQSummary(Entry *data, size_t size)
+      : data(data), size(size) {}
+  /*!
+   * \return the maximum error of the Summary
+   */
+  inline RType MaxError() const {
+    RType res = data[0].rmax - data[0].rmin - data[0].wmin;
+    for (size_t i = 1; i < size; ++i) {
+      res = std::max(data[i].RMaxPrev() - data[i - 1].RMinNext(), res);
+      res = std::max(data[i].rmax - data[i].rmin - data[i].wmin, res);
+    }
+    return res;
+  }
+  /*!
+   * \brief query qvalue, start from istart
+   * \param qvalue the value we query for
+   * \param istart starting position
+   */
+  inline Entry Query(DType qvalue, size_t &istart) const { // NOLINT(*)
+    while (istart < size && qvalue > data[istart].value) {
+      ++istart;
+    }
+    if (istart == size) {
+      RType rmax = data[size - 1].rmax;
+      return Entry(rmax, rmax, 0.0f, qvalue);
+    }
+    if (qvalue == data[istart].value) {
+      return data[istart];
+    } else {
+      if (istart == 0) {
+        return Entry(0.0f, 0.0f, 0.0f, qvalue);
+      } else {
+        return Entry(data[istart - 1].RMinNext(),
+                     data[istart].RMaxPrev(),
+                     0.0f, qvalue);
+      }
+    }
+  }
+  /*! \return maximum rank in the summary */
+  inline RType MaxRank() const {
+    return data[size - 1].rmax;
+  }
+  /*!
+   * \brief copy content from src
+   * \param src source sketch
+   */
+  inline void CopyFrom(const WQSummary &src) {
+    size = src.size;
+    std::memcpy(data, src.data, sizeof(Entry) * size);
+  }
+  inline void MakeFromSorted(const Entry* entries, size_t n) {
+    size = 0;
+    for (size_t i = 0; i < n;) {
+      size_t j = i + 1;
+      // ignore repeated values
+      for (; j < n && entries[j].value == entries[i].value; ++j) {}
+      data[size++] = Entry(entries[i].rmin, entries[i].rmax, entries[i].wmin,
+                           entries[i].value);
+      i = j;
+    }
+  }
+  /*!
+   * \brief debug function, validate whether the summary
+   *  run consistency check to check if it is a valid summary
+   * \param eps the tolerate error level, used when RType is floating point and
+   *        some inconsistency could occur due to rounding error
+   */
+  inline void CheckValid(RType eps) const {
+    for (size_t i = 0; i < size; ++i) {
+      data[i].CheckValid(eps);
+      if (i != 0) {
+        CHECK(data[i].rmin >= data[i - 1].rmin + data[i - 1].wmin) << 'rmin range constraint';
+        CHECK(data[i].rmax >= data[i - 1].rmax + data[i].wmin) << 'rmax range constraint';
+      }
+    }
+  }
+  /*!
+   * \brief set current summary to be pruned summary of src
+   *        assume data field is already allocated to be at least maxsize
+   * \param src source summary
+   * \param maxsize size we can afford in the pruned sketch
+   */
+    }
+    }
     }
     
-                    char match;
-                if( s[i] == ')' )
-                    match = '(';
-                else if( s[i] == ']' )
-                    match = '[';
-                else{
-                    assert( s[i] == '}' );
-                    match = '{';
-                }
-    
-        deleteLinkedList(head);
-    
-    
-int main() {
+      void Write(const SparsePage& page, dmlc::Stream* fo) override {
+    const auto& offset_vec = page.offset.HostVector();
+    const auto& data_vec = page.data.HostVector();
+    CHECK(page.offset.Size() != 0 && offset_vec[0] == 0);
+    CHECK_EQ(offset_vec.back(), page.data.Size());
+    fo->Write(offset_vec);
+    if (page.data.Size() != 0) {
+      fo->Write(dmlc::BeginPtr(data_vec), page.data.Size() * sizeof(Entry));
     }
+  }
+    
+            yield_count = 0;
+        do
+        {
+            ptr = pool->alloc(pool, size);
+            if (ptr)
+            {
+                break;
+            }
+            else
+            {
+                yield_count++;
+                yield_total_count++;
+                usleep(10);
+            }
+        } while (yield_count < 100);
+    
+        time_t start = time(nullptr);
+    while (i != 1000)
+    {
+        usleep(100);
+        
+        if ((time(nullptr) - start) > 3)
+        {
+            ASSERT_TRUE(false);
+        }
+    }
+    
+    TEST(coroutine_base, get_current)
+{
+    long _cid;
+    long cid = Coroutine::create([](void *arg)
+    {
+        auto co = Coroutine::get_current();
+        *(long *) arg = co->get_cid();
+    }, &_cid);
+    }
+    
+    enum
+{
+    EVENT_onStart = 1u << 1,
+    EVENT_onShutdown = 1u << 2,
+    EVENT_onWorkerStart = 1u << 3,
+    EVENT_onWorkerStop = 1u << 4,
+    EVENT_onConnect = 1u << 5,
+    EVENT_onReceive = 1u << 6,
+    EVENT_onPacket = 1u << 7,
+    EVENT_onClose = 1u << 8,
+    EVENT_onTask = 1u << 9,
+    EVENT_onFinish = 1u << 10,
+    EVENT_onPipeMessage = 1u << 11,
+};
     
     using namespace std;
-    
-    /// Definition for singly-linked list.
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-    
-    
-    {
-    {
-    {            if(command.s == 'print')
-                res.push_back(command.node->val);
-            else{
-                assert(command.s == 'go');
-                if(command.node->right)
-                    stack.push(Command('go',command.node->right));
-                stack.push(Command('print', command.node));
-                if(command.node->left)
-                    stack.push(Command('go',command.node->left));
-            }
-        }
-        return res;
-    }
-};
-    
-    
-    {
-    {        return res;
-    }
-};
-    
-    public:
-    vector<int> postorderTraversal(TreeNode* root) {
-    }
-    
-    int main() {
-    }
-    
-      ~SharedState() {}
-    
-    
-    {}  // namespace rocksdb
-
-    
-    
-    {  std::unique_ptr<RateLimiter> low_pri_rate_limiter_;
-};
-    
-      // Always pick a compaction which includes all files whenever possible.
-  CompactionTask* PickCompaction(
-      DB* db, const std::string& cf_name) override {
-    ColumnFamilyMetaData cf_meta;
-    db->GetColumnFamilyMetaData(&cf_meta);
-    }
-    
-      const char* Name() const override { return 'MyFilter'; }
-    
-    
-    {  // close DB
-  for (auto* handle : handles) {
-    delete handle;
-  }
-  delete db;
-}
-
-    
-      // Set a new snapshot in the transaction
-  txn->SetSnapshot();
-  txn->SetSavePoint();
-  read_options.snapshot = txn_db->GetSnapshot();
-    
-    #pragma once
-#ifndef ROCKSDB_LITE
