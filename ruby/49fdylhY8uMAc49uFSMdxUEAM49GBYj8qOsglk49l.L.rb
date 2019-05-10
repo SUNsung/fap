f9318ae2f23a6b60d758e43ee493c2f86f7fada1
SUNsung <weatherius@gmail.com>
2019-05-10 20:25:37 +0300

    Rewrite [49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb] File in: 2019-05-10 20:25:36

--- ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
+++ ruby/49fdylhY8uMAc49uFSMdxUEAM49GBYj8qOsglk49l.L.rb
@@ -1,123 +1,55 @@
 
         
-        def pboard
-  return # skip print
-  print 'No. #$no\n'
-  (0...COL).each{|i|
-    print '|'
-    (0...ROW-NP).each{|j|
-      x = $b[i*ROW+j]
-      if x < 0
-        print '..|'
-      else
-        printf '%2d|',x+1
-      end
-    }
-    print '\n'
-  }
-  print '\n'
-end
+        lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'capistrano/version'
     
-    (allow file-read-metadata)
-(allow file-read*
-  ; This is currenly only added because using `xcodebuild` to build a resource
-  ; bundle target starts a FSEvents stream on `/`. No idea why that would be
-  ; needed, but for now it doesn’t seem like a real problem.
-  (literal '/')
-  (regex
-    ; TODO see if we can restrict this more, but it's going to be hard
-    #'^/Users/[^.]+/*'
-    ;#'^/Users/[^.]+/.netrc'
-    ;#'^/Users/[^.]+/.gemrc'
-    ;#'^/Users/[^.]+/.gem/*'
-    ;#'^/Users/[^.]+/Library/.*'
-    #'^/Library/*'
-    #'^/System/Library/*'
-    #'^/usr/lib/*'
-    #'^/usr/share/*'
-    #'^/private/*'
-    #'^/dev/*'
-    #'^<%= ruby_prefix %>'
-    #'^<%= pod_prefix %>'
-    #'^<%= xcode_app_path %>'
-    #'^<%= Pod::Config.instance.repos_dir %>'
-<% prefixes.each do |prefix| %>
-    #'^<%= prefix %>/*'
-<% end %>
-  )
-)
+          # Decorate Variables#set to add validation behavior.
+      def set(key, value=nil, &block)
+        assert_value_or_block_not_both(value, block)
     
-    abstract_target 'Abstract Target' do
-    use_modular_headers!
+    module Capistrano
+  class Configuration
+    # Holds the variables assigned at Capistrano runtime via `set` and retrieved
+    # with `fetch`. Does internal bookkeeping to help identify user mistakes
+    # like spelling errors or unused variables that may lead to unexpected
+    # behavior.
+    class Variables
+      CAPISTRANO_LOCATION = File.expand_path('../..', __FILE__).freeze
+      IGNORED_LOCATIONS = [
+        '#{CAPISTRANO_LOCATION}/configuration/variables.rb:',
+        '#{CAPISTRANO_LOCATION}/configuration.rb:',
+        '#{CAPISTRANO_LOCATION}/dsl/env.rb:',
+        '/dsl.rb:',
+        '/forwardable.rb:'
+      ].freeze
+      private_constant :CAPISTRANO_LOCATION, :IGNORED_LOCATIONS
     
-          def report
-        <<-EOS
+    # We use a special :_default_git value so that SCMResolver can tell whether the
+# default has been replaced by the user via `set`.
+set_if_empty :scm, Capistrano::Configuration::SCMResolver::DEFAULT_GIT
+set_if_empty :branch, 'master'
+set_if_empty :deploy_to, -> { '/var/www/#{fetch(:application)}' }
+set_if_empty :tmp_dir, '/tmp'
     
-          # Custom destructuring method. This can be used to normalize
-      # destructuring for different variations of the node.
-      #
-      # In this case, the `def` node destructures into:
-      #
-      #   `method_name, arguments, body`
-      #
-      # while the `defs` node destructures into:
-      #
-      #   `receiver, method_name, arguments, body`
-      #
-      # so we reverse the destructured array to get the optional receiver
-      # at the end, where it can be discarded.
-      #
-      # @return [Array] the different parts of the `def` or `defs` node
-      def node_parts
-        to_a.reverse
-      end
+          # A helper class for comparing the positions of different parts of a
+      # `pair` node.
+      class HashElementDelta
+        def initialize(first, second)
+          @first = first
+          @second = second
+    
+          dir = if File.directory?(file_or_dir)
+              file_or_dir
+            else
+              File.dirname(file_or_dir)
+            end
+      @path_cache[dir] ||= ConfigLoader.configuration_file_for(dir)
+      path = @path_cache[dir]
+      @object_cache[path] ||= begin
+                                print 'For #{dir}: ' if ConfigLoader.debug?
+                                ConfigLoader.configuration_from_file(path)
+                              end
     end
   end
 end
-
-    
-    module RuboCop
-  module AST
-    # A node extension for `kwsplat` nodes. This will be used in place of a
-    # plain  node when the builder constructs the AST, making its methods
-    # available to all `kwsplat` nodes within RuboCop.
-    class KeywordSplatNode < Node
-      include HashElementNode
-    
-          # Returns the index of the `when` branch within the `case` statement.
-      #
-      # @return [Integer] the index of the `when` branch
-      def branch_index
-        parent.when_branches.index(self)
-      end
-    
-        # Evaluate dependencies.
-    if !attributes[:no_auto_depends?]
-	    pkgdepend_gen = safesystemout('pkgdepend', 'generate',  '-md', '#{staging_path}',  manifest_fn)
-      File.write(build_path('#{name}.p5m.3'), pkgdepend_gen)
-    
-        if attributes[:pear_data_dir]
-      logger.info('Setting data_dir', :data_dir => attributes[:pear_data_dir])
-      safesystem('pear', '-c', config, 'config-set', 'data_dir', '#{staging_path(installroot)}/#{attributes[:pear_data_dir]}')
-    end
-    
-        File.write(build_path('comment'),  self.description + '\n')
-    
-        if File.exists?(params[:output])
-      # TODO(sissel): Allow folks to choose output?
-      logger.error('Puppet module directory '#{params[:output]}' already ' \
-                    'exists. Delete it or choose another output (-p flag)')
-    end
-    
-        begin
-      json_test_code = [
-        'try:',
-        '  import json',
-        'except ImportError:',
-        '  import simplejson as json'
-      ].join('\n')
-      safesystem('#{attributes[:python_bin]} -c '#{json_test_code}'')
-    rescue FPM::Util::ProcessFailed => e
-      logger.error('Your python environment is missing json support (either json or simplejson python module). I cannot continue without this.', :python => attributes[:python_bin], :error => e)
-      raise FPM::Util::ProcessFailed, 'Python (#{attributes[:python_bin]}) is missing simplejson or json modules.'
-    end
