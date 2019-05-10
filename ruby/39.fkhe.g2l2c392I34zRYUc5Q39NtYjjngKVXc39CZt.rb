6274a665e664212e758ee835a89389e05f79ef24
SUNsung <weatherius@gmail.com>
2019-05-11 02:13:33 +0300

    Rewrite [39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb] File in: 2019-05-11 02:13:32

--- ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
+++ ruby/39.fkhe.g2l2c392I34zRYUc5Q39NtYjjngKVXc39CZt.rb
@@ -1,200 +1,44 @@
 
         
-        # Sample contents
-WITHOUT_LIQUID = <<-TEXT.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. Etiam bibendum magna et metus fermentum, eu cursus lorem
-mattis. Curabitur vel dui et lacus rutrum suscipit et eget neque.
-    
-    # -------------------------------------------------------------------
-# Benchmarking changes in https://github.com/jekyll/jekyll/pull/6767
-# -------------------------------------------------------------------
-    
-      Jekyll::External.require_if_present(Jekyll::External.blessed_gems) do |g, ver_constraint|
-    cmd = g.split('-').last
-    p.command(cmd.to_sym) do |c|
-      c.syntax cmd
-      c.action do
-        Jekyll.logger.abort_with 'You must install the '#{g}' gem' \
-          ' version #{ver_constraint} to use the 'jekyll #{cmd}' command.'
-      end
-    end
-  end
-    
-            # For a description of the protocol see
-        # http://feedback.livereload.com/knowledgebase/articles/86174-livereload-protocol
-        def reload(pages)
-          pages.each do |p|
-            json_message = JSON.dump(
-              :command => 'reload',
-              :path    => p.url,
-              :liveCSS => true
-            )
-    
-          def fallback_data
-        @fallback_data ||= {}
-      end
-    end
-  end
+        test_check 'alias'
+class Alias0
+  def foo; 'foo' end
 end
+class Alias1 < Alias0
+  alias bar foo
+  def foo; 'foo+' + super end
+end
+class Alias2 < Alias1
+  alias baz foo
+  undef foo
+end
+    
+      context 'moving variants between shipments', js: true do
+    before do
+      create(:stock_location, name: 'LA')
+      visit spree.admin_orders_path
+      within_row(1) do
+        click_link 'R100'
+      end
+    end
+    
+              if Spree::Dependencies.cart_update_service.constantize.call(order: @order, params: line_items_attributes).success?
+            @line_item.reload
+            respond_with(@line_item, default_template: :show)
+          else
+            invalid_resource!(@line_item)
+          end
+        end
+    
+            def product_params
+          params.require(:product).permit(permitted_product_attributes)
+        end
+    
+                render_serialized_payload { serialize_shipping_rates(result.value) }
+          end
+    
+    run SinatraStaticServer
 
     
-          # Reads an integer from the cache, or returns nil if no value was found.
-      #
-      # See Caching.read for more information.
-      def self.read_integer(raw_key, timeout: TIMEOUT)
-        value = read(raw_key, timeout: timeout)
-    
-            def collection_options
-          { state: 'all', sort: 'created', direction: 'asc' }
-        end
-      end
     end
-  end
-end
-
-    
-              issue.label_names.each do |label_name|
-            # Although unlikely it's technically possible for an issue to be
-            # given a label that was created and assigned after we imported all
-            # the project's labels.
-            next unless (label_id = label_finder.id_for(label_name))
-    
-            def representation_class
-          Representation::Note
-        end
-    
-            # attributes - A Hash containing the raw note details. The keys of this
-        #              Hash must be Symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
-    
-          private
-    
-      it 'asks to accept conflicts when the scenario was modified' do
-    DefaultScenarioImporter.seed(user)
-    agent = user.agents.where(name: 'Rain Notifier').first
-    agent.options['expected_receive_period_in_days'] = 9001
-    agent.save!
-    visit new_scenario_imports_path
-    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'data/default_scenario.json'))
-    click_on 'Start Import'
-    expect(page).to have_text('This Scenario already exists in your system.')
-    expect(page).to have_text('9001')
-    check('I confirm that I want to import these Agents.')
-    click_on 'Finish Import'
-    expect(page).to have_text('Import successful!')
-  end
-    
-        it 'returns a Glyphicon icon element with an addidional class' do
-      icon = icon_tag('glyphicon-help', class: 'text-info')
-      expect(icon).to be_html_safe
-      expect(Nokogiri(icon).at('span.glyphicon.glyphicon-help.text-info')).to be_a Nokogiri::XML::Element
-    end
-    
-        it 'outputs a structure containing name, description, the date, all agents & their links' do
-      data = exporter.as_json
-      expect(data[:name]).to eq(name)
-      expect(data[:description]).to eq(description)
-      expect(data[:source_url]).to eq(source_url)
-      expect(data[:guid]).to eq(guid)
-      expect(data[:schema_version]).to eq(1)
-      expect(data[:tag_fg_color]).to eq(tag_fg_color)
-      expect(data[:tag_bg_color]).to eq(tag_bg_color)
-      expect(data[:icon]).to eq(icon)
-      expect(Time.parse(data[:exported_at])).to be_within(2).of(Time.now.utc)
-      expect(data[:links]).to eq([{ :source => guid_order(agent_list, :jane_weather_agent), :receiver => guid_order(agent_list, :jane_rain_notifier_agent)}])
-      expect(data[:control_links]).to eq([])
-      expect(data[:agents]).to eq(agent_list.sort_by{|a| a.guid}.map { |agent| exporter.agent_as_json(agent) })
-      expect(data[:agents].all? { |agent_json| agent_json[:guid].present? && agent_json[:type].present? && agent_json[:name].present? }).to be_truthy
-    
-      describe '#recursively_interpolate_jsonpaths' do
-    it 'interpolates all string values in a structure' do
-      struct = {
-        :int => 5,
-        :string => 'this <escape $.works>',
-        :array => ['<works>', 'now', '<$.there.world>'],
-        :deep => {
-          :string => 'hello <there.world>',
-          :hello => :world
-        }
-      }
-      data = { :there => { :world => 'WORLD' }, :works => 'should work' }
-      expect(Utils.recursively_interpolate_jsonpaths(struct, data)).to eq({
-        :int => 5,
-        :string => 'this should+work',
-        :array => ['should work', 'now', 'WORLD'],
-        :deep => {
-          :string => 'hello WORLD',
-          :hello => :world
-        }
-      })
-    end
-  end
-    
-      let :new_extract do
-    {
-      'url' => { 'css' => '#comic img', 'value' => '@src' },
-      'title' => { 'css' => '#comic img', 'value' => '@alt' },
-      'hovertext' => { 'css' => '#comic img', 'value' => '@title', 'hidden' => true }
-    }
-  end
-    
-      describe '#working?' do
-    it 'should not be working until the first event was received' do
-      expect(@checker).not_to be_working
-      @checker.last_receive_at = Time.now
-      expect(@checker).to be_working
-    end
-  end
-    
-            # This is called to upgrade this V1 config to V2. The parameter given
-        # is the full V2 configuration object, so you can do anything to it
-        # that you want.
-        #
-        # No return value is expected, modifications should be made directly
-        # to the new V2 object.
-        #
-        # @param [V2::Root] new
-        def upgrade(new)
-        end
-    
-            # This returns all the registered configuration classes that were
-        # marked as 'upgrade safe.'
-        #
-        # @return [Hash]
-        def config_upgrade_safe
-          result = {}
-    
-            # This contains all the synced folder implementations by name.
-        #
-        # @return [Registry<Symbol, Array<Class, Integer>>]
-        attr_reader :synced_folders
-    
-            # This clears out all the registered plugins. This is only used by
-        # unit tests and should not be called directly.
-        def reset!
-          @registered.clear
-        end
-    
-            # This should return an action callable for the given name.
-        #
-        # @param [Symbol] name Name of the action.
-        # @return [Object] A callable action sequence object, whether it
-        #   is a proc, object, etc.
-        def action(name)
-          nil
-        end
-    
-      def process_bootstrap
-    log_status 'Convert Bootstrap LESS to Sass'
-    puts ' repo   : #@repo_url'
-    puts ' branch : #@branch_sha #@repo_url/tree/#@branch'
-    puts ' save to: #{@save_to.to_json}'
-    puts ' twbs cache: #{@cache_path}'
-    puts '-' * 60
-    
-        def pos
-      byte_to_str_pos @s.pos
-    end
+Liquid::Template.register_filter OctopressLiquidFilters
