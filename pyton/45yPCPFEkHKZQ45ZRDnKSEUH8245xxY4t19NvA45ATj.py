5c591b3474eae761788eb3fe46c68186177c6663
SUNsung <weatherius@gmail.com>
2019-05-10 21:23:34 +0300

    Rewrite [45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py] File in: 2019-05-10 21:23:33

--- pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
+++ pyton/45yPCPFEkHKZQ45ZRDnKSEUH8245xxY4t19NvA45ATj.py
@@ -1,126 +1,153 @@
 
         
-        
-class RequestStatus(Enum):
+        Uses the linkcheck's output file to fix links in docs.
     
+    import scrapy
+from scrapy.commands import ScrapyCommand
+from scrapy.linkextractors import LinkExtractor
     
-class Bus(Vehicle):
+    class Command(ScrapyCommand):
     
-            status_line = 'HTTP/{version} {status} {reason}'.format(
-            version=version,
-            status=original.status,
-            reason=original.reason
-        )
-        headers = [status_line]
-        try:
-            # `original.msg` is a `http.client.HTTPMessage` on Python 3
-            # `_headers` is a 2-tuple
-            headers.extend(
-                '%s: %s' % header for header in original.msg._headers)
-        except AttributeError:
-            # and a `httplib.HTTPMessage` on Python 2.x
-            # `headers` is a list of `name: val<CRLF>`.
-            headers.extend(h.strip() for h in original.msg.headers)
-    
-    
-def is_valid_mime(mime):
-    return mime and MIME_RE.match(mime)
-    
-    
-with codecs.open(FILE_PATH, encoding='utf8') as f:
-    # Strip because we don't want new lines in the data so that we can
-    # easily count occurrences also when embedded in JSON (where the new
-    # line would be escaped).
-    FILE_CONTENT = f.read().strip()
-    
-    
-def test_default_options(httpbin):
-    env = MockEnvironment()
-    env.config['default_options'] = ['--form']
-    env.config.save()
-    r = http(httpbin.url + '/post', 'foo=bar', env=env)
-    assert r.json['form'] == {'foo': 'bar'}
-    
-        exc = ConnectionError('Connection aborted')
-    exc.request = Request(method='GET', url='http://www.google.com')
-    get_response.side_effect = exc
-    ret = main(['--ignore-stdin', 'www.google.com'], custom_log_error=error)
-    assert ret == ExitStatus.ERROR
-    assert error_msg == (
-        'ConnectionError: '
-        'Connection aborted while doing GET request to URL: '
-        'http://www.google.com')
-    
-        def _migrate_implicit_content_type(self):
-        '''Migrate the removed implicit_content_type config option'''
-        try:
-            implicit_content_type = self.pop('implicit_content_type')
-        except KeyError:
-            self.save()
+        def run(self, args, opts):
+        if opts.verbose:
+            versions = scrapy_components_versions()
+            width = max(len(n) for (n, _) in versions)
+            patt = '%-{}s : %s'.format(width)
+            for name, version in versions:
+                print(patt % (name, version))
         else:
-            if implicit_content_type == 'form':
-                self['default_options'].insert(0, '--form')
-            self.save()
-            self.load()
-
+            print('Scrapy %s' % scrapy.__version__)
     
-            _LOGGER.info('Request successful')
-        return True
-
+        class ScrapyClientContextFactory(ClientContextFactory):
+        'A SSL context factory which is more permissive against SSL bugs.'
+        # see https://github.com/scrapy/scrapy/issues/82
+        # and https://github.com/scrapy/scrapy/issues/26
+        # and https://github.com/scrapy/scrapy/issues/981
     
+    openssl_methods = {
+    METHOD_TLS:    SSL.SSLv23_METHOD,                   # protocol negotiation (recommended)
+    METHOD_SSLv3:  SSL.SSLv3_METHOD,                    # SSL 3 (NOT recommended)
+    METHOD_TLSv10: SSL.TLSv1_METHOD,                    # TLS 1.0 only
+    METHOD_TLSv11: getattr(SSL, 'TLSv1_1_METHOD', 5),   # TLS 1.1 only
+    METHOD_TLSv12: getattr(SSL, 'TLSv1_2_METHOD', 6),   # TLS 1.2 only
+}
     
-def log_error(response):
-    '''Log error message.'''
-    obj = response.json()
-    error_message = obj['error']['message']
-    error_code = obj['error']['code']
-    
-    
-def get_service(hass, config, discovery_info=None):
-    '''Get the Free Mobile SMS notification service.'''
-    return FreeSMSNotificationService(
-        config[CONF_USERNAME], config[CONF_ACCESS_TOKEN])
-    
-    import voluptuous as vol
-    
-    _LOGGER = logging.getLogger(__name__)
-    
-        Returns:
+            if use_mean_attention:
+            a = tf.reduce_mean(a, axis=1)  # [batch_size, n_step]
+            a = tf.expand_dims(a, axis=1)  # [batch_size, 1, n_step]
+            a = tf.tile(a, [1, n_input, 1])  # [batch_size, n_input, n_step]
     
     
-def dense(x, n_unit, act_fn=relu, name=None, reuse=None):
-    '''全连接层
-    Input shape:  [batch_size, n_input]
-    Output shape: [batch_size, n_unit]
-    
-        name = name or 'highway_conv2d'
-    for i, kz in enumerate(kernel_size):
-        x = highway_conv2d(x, kz, act_fn, strides, padding, carry_bias, name='{}-{}'.format(name, i))
-    
-        with tf.variable_scope(name or 'attention_flow', reuse=reuse):
-        h_expand = tf.tile(tf.expand_dims(h, axis=2), [1, 1, J, 1])  # [N, T, J, d]
-        u_expand = tf.tile(tf.expand_dims(u, axis=1), [1, T, 1, 1])  # [N, T, J, d]
-        hu = tf.multiply(h_expand, u_expand)  # [N, T, J, d]
-        h_u_hu = tf.concat([h_expand, u_expand, hu], axis=-1)  # [N, T, J, 3d]
-        W_s = get_w([3 * d, 1])  # [3d, 1]
-    
-    sum_ngrams = 0
-for s in sentences:
-    for w in s:
-        w = w.lower()
-        # from gensim.models._utils_any2vec import compute_ngrams
-        ret = compute_ngrams(w, min_ngrams, max_ngrams)
-        print(ret)
-        sum_ngrams += len(ret)
+'''预定义好的正则化器
 '''
-['<h', 'he', 'el', 'll', 'lo', 'o>', '<he', 'hel', 'ell', 'llo', 'lo>', '<hel', 'hell', 'ello', 'llo>']
-['<w', 'wo', 'or', 'rl', 'ld', 'd>', '<wo', 'wor', 'orl', 'rld', 'ld>', '<wor', 'worl', 'orld', 'rld>']
-['<!', '!>', '<!>']
-['<i', 'i>', '<i>']
-['<a', 'am', 'm>', '<am', 'am>', '<am>']
-['<h', 'hu', 'ua', 'ay', 'y>', '<hu', 'hua', 'uay', 'ay>', '<hua', 'huay', 'uay>']
-['<.', '.>', '<.>']
+l1_regularizer = L1L2Regularizer(l1=0.01)
+    
+            self._colors = {
+            'A': '#00cc00',
+            'B': '#00cc00',
+            'C': '#00aacc',
+            'D': '#888888',
+            'E': '#cccc00',
+            'F': '#ff0000',
+            'H': '#22aa22',
+            'I': '#cc0000',
+            'J': '#000000',
+        }
+    
+        >>> rewrite_editor_section_name('js')
+    'js'
+    >>> rewrite_editor_section_name('vscode:js')
+    'js'
+    '''
+    if ':' not in section_name:
+        return section_name
+    
+        prefix = 'erlang'
+    _filename = 'erlang.html.markdown'
+    
+        # cut code blocks
+    block_number = 0
+    while True:
+        section_text, replacements = re.subn(
+            '^```.*?^```',
+            'MULTILINE_BLOCK_%s' % block_number,
+            section_text,
+            1,
+            flags=re.S | re.MULTILINE)
+        block_number += 1
+        if not replacements:
+            break
+    
+        find_answer_by_keyword()
+    
+    if os.environ.get('REDIS_HOST', '').lower() != 'none':
+    _REDIS = redis.StrictRedis(host=REDISHOST, port=6379, db=0)
+else:
+    _REDIS = None
+    
+    def _colorize_ansi_answer(topic, answer, color_style,       # pylint: disable=too-many-arguments
+                          highlight_all=True, highlight_code=False,
+                          unindent_code=False):
+    
+    import signal
+    
+    
+def format_return(result, max_lines):
+    if isinstance(result, (list, tuple, set)):
+        return '({0} with {1} items)'.format(type(result).__name__, len(result))
+    
+    It has been modified to mimic the behaviour of
+https://golang.org/pkg/time/#ParseDuration
 '''
-assert sum_ngrams == len(model.wv.vectors_ngrams)
-print(sum_ngrams)  # 57
-print()
+# MIT LICENSE
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the 'Software'), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+from __future__ import absolute_import
+from __future__ import unicode_literals
+    
+        def remove(self):
+        if self.external:
+            log.info('Volume %s is external, skipping', self.true_name)
+            return
+        log.info('Removing volume %s', self.true_name)
+        return self.client.remove_volume(self.true_name)
+    
+        opts = parse_opts(args)
+    
+        return ''.join(lines[i + 2:j - 1])
+    
+    
+def create_custom_host_file(client, filename, content):
+    dirname = os.path.dirname(filename)
+    container = client.create_container(
+        'busybox:latest',
+        ['sh', '-c', 'echo -n '{}' > {}'.format(content, filename)],
+        volumes={dirname: {}},
+        host_config=client.create_host_config(
+            binds={dirname: {'bind': dirname, 'ro': False}},
+            network_mode='none',
+        ),
+    )
+    try:
+        client.start(container)
+        exitcode = client.wait(container)['StatusCode']
+    
+    from compose.utils import unquote_path
