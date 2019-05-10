148b8b6e409b22d3fc4f24210653776ab2f26205
SUNsung <weatherius@gmail.com>
2019-05-10 19:24:41 +0300

    Rewrite [52c7TUBp2DX3c52TB6nlzmlbXQ52Zj61CsL3oAs52ry.rb] File in: 2019-05-10 19:24:40

--- ruby/52c7TUBp2DX3c52TB6nlzmlbXQ52Zj61CsL3oAs52ry.rb
+++ ruby/52c7TUBp2DX3c52TB6nlzmlbXQ52Zj61CsL3oAs52ry.rb
@@ -1,155 +1,62 @@
 
         
-        module Docs
-  class Filter < ::HTML::Pipeline::Filter
-    def css(*args)
-      doc.css(*args)
+        module ActionView
+  module Helpers
+    module Tags # :nodoc:
+      class DateSelect < Base # :nodoc:
+        def initialize(object_name, method_name, template_object, options, html_options)
+          @html_options = html_options
+    
+              label_tag(name_and_id['id'], content, options)
+        end
+    
+          test 'when the layout is specified as a symbol and the method returns something besides a string/false/nil, raise an exception' do
+        assert_raises(ArgumentError) { WithSymbolReturningObj.new.process(:index) }
+      end
+    
+          alias :expire_data_after_sign_out! :expire_data_after_sign_in!
+    end
+  end
+end
+
+    
+      # We manage the result cache ourselves and the default of 10 minutes can be
+  # too low (particularly on Travis CI), causing results from some integration
+  # tests to be dropped. This causes random fluctuations in test coverage.
+  merge_timeout 86400
+    
+      # Configure public file server for tests with Cache-Control for performance.
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=#{1.hour.to_i}'
+  }
+    
+    def ok_failed(condition)
+  if (condition)
+    puts 'OK'
+  else
+    puts 'FAILED'
+  end
+end
+    
+    # The project root directory
+$root = ::File.dirname(__FILE__)
+    
+        def generate(site)
+      site.write_category_indexes
     end
     
-            true
+        def handle_gist_redirecting(data)
+      redirected_url = data.header['Location']
+      if redirected_url.nil? || redirected_url.empty?
+        raise ArgumentError, 'GitHub replied with a 302 but didn't provide a location in the response headers.'
       end
     
-          INDEX = Set.new
-    
-            # Configures the given list of networks on the virtual machine.
-        #
-        # The networks parameter will be an array of hashes where the hashes
-        # represent the configuration of a network interface. The structure
-        # of the hash will be roughly the following:
-        #
-        # {
-        #   type:      :static,
-        #   ip:        '192.168.33.10',
-        #   netmask:   '255.255.255.0',
-        #   interface: 1
-        # }
-        #
-        def configure_networks(networks)
-          raise BaseError, _key: :unsupported_configure_networks
-        end
-    
-            # Defines additional configuration keys to be available in the
-        # Vagrantfile. The configuration class should be returned by a
-        # block passed to this method. This is done to ensure that the class
-        # is lazy loaded, so if your class inherits from any classes that
-        # are specific to Vagrant 1.0, then the plugin can still be defined
-        # without breaking anything in future versions of Vagrant.
-        #
-        # @param [String] name Configuration key.
-        # @param [Boolean] upgrade_safe If this is true, then this configuration
-        #   key is safe to load during an upgrade, meaning that it depends
-        #   on NO Vagrant internal classes. Do _not_ set this to true unless
-        #   you really know what you're doing, since you can cause Vagrant
-        #   to crash (although Vagrant will output a user-friendly error
-        #   message if this were to happen).
-        def self.config(name=UNSET_VALUE, upgrade_safe=false, &block)
-          data[:config] ||= Registry.new
-    
-                    if provider_to_use && provider_to_use != active_provider
-                  # We found an active machine with a provider that doesn't
-                  # match the requested provider. Show an error.
-                  raise Errors::ActiveMachineWithDifferentProvider,
-                    name: active_name.to_s,
-                    active_provider: active_provider.to_s,
-                    requested_provider: provider_to_use.to_s
-                else
-                  # Use this provider and exit out of the loop. One of the
-                  # invariants [for now] is that there shouldn't be machines
-                  # with multiple providers.
-                  @logger.info('Active machine found with name #{active_name}. ' +
-                               'Using provider: #{active_provider}')
-                  provider_to_use = active_provider
-                  break
-                end
-              end
-            end
-    
-            # This returns all the registered host capabilities.
-        #
-        # @return [Hash]
-        def host_capabilities
-          results = Hash.new { |h, k| h[k] = Registry.new }
-    
-      #
-  # Payload types were identified from xCAT-server source code (IPMI.pm)
-  #
-  PAYLOAD_IPMI = 0
-  PAYLOAD_SOL  = 1
-  PAYLOAD_RMCPPLUSOPEN_REQ = 0x10
-  PAYLOAD_RMCPPLUSOPEN_REP = 0x11
-  PAYLOAD_RAKP1 = 0x12
-  PAYLOAD_RAKP2 = 0x13
-  PAYLOAD_RAKP3 = 0x14
-  PAYLOAD_RAKP4 = 0x15
-    
-        data =
-      [0x00, 0x00, 0x00, 0x00].pack('C*') +
-      bmc_session_id +
-      console_random_id +
-      [
-        0x14, 0x00, 0x00,
-        username.length
-      ].pack('C*') +
-      username
-    
-            # Creates a TCP connection using Rex::Socket::Tcp
-        #
-        # @return [Rex::Socket::Tcp]
-        def create_tcp_connection
-          self.connection = Rex::Socket::Tcp.create(
-            'PeerHost'   => host,
-            'PeerPort'   => port.to_i,
-            'Context'    => context,
-            'Timeout'    => timeout
-          )
-        end
-    
-      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-=end
-    
-          def sidebar
-        if @sidebar.nil?
-          if page = @page.sidebar
-            @sidebar = page.text_data
-          else
-            @sidebar = false
-          end
-        end
-        @sidebar
+          unless file.file?
+        return 'File #{file} could not be found'
       end
     
-        follow_redirect!
-    assert_equal '/I/C', last_request.fullpath
-    assert last_response.ok?
-    
-      test 'h1 title can be disabled' do
-    title = 'H1'
-    @wiki.write_page(title, :markdown, '# 1 & 2 <script>alert('js')</script>' + '\n # 3', commit_details)
-    page = @wiki.page(title)
-    
-    exec         = {}
-options      = {
-  :port => 4567,
-  :bind => '0.0.0.0',
-}
-wiki_options = {
-  :live_preview  => false,
-  :allow_uploads => false,
-  :allow_editing => true,
-}
-    
-      s.name              = 'gollum'
-  s.version           = '4.1.4'
-  s.date              = '2018-10-01'
-  s.rubyforge_project = 'gollum'
-  s.license           = 'MIT'
+      def user_is_author?
+    record.user_id == user.id
+  end
+end
