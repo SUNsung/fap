02be588dde2d771b6cc7b856167bfa5e2bf74a94
SUNsung <weatherius@gmail.com>
2019-05-11 07:33:10 +0300

    Rewrite [51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb] File in: 2019-05-11 07:33:09

--- ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
+++ ruby/51UImNtHosRDA51re7e.p2Gcgc51KLiSLkIloEs519NB.rb
@@ -1,39 +1,96 @@
 
         
-          describe '#hook_on_project_start' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_start' }
+                # Executes a command on the remote machine with administrative
+        # privileges. See {#execute} for documentation, as the API is the
+        # same.
+        #
+        # @see #execute
+        def sudo(command, opts=nil)
+        end
+    
+            # This is called to upgrade this V1 config to V2. The parameter given
+        # is the full V2 configuration object, so you can do anything to it
+        # that you want.
+        #
+        # No return value is expected, modifications should be made directly
+        # to the new V2 object.
+        #
+        # @param [V2::Root] new
+        def upgrade(new)
+        end
+    
+              # Return the registry
+          data[:providers]
+        end
+    
+      private
+    
+      def show
+    if subscription.valid?(params['hub.topic'])
+      @account.update(subscription_expires_at: future_expires)
+      render plain: encoded_challenge, status: 200
+    else
+      head 404
     end
   end
-  describe '#hook_on_project_first_start' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_first_start' }
-    end
+    
+      def subscription_params
+    params.require(:subscription).permit(:endpoint, keys: [:auth, :p256dh])
   end
-  describe '#hook_on_project_restart' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_restart' }
-    end
-  end
-  describe '#hook_on_project_exit' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_exit' }
-    end
-  end
-  describe '#hook_on_project_stop' do
-    it_should_behave_like 'a project hook' do
-      let(:hook_name) { 'on_project_stop' }
+    
+      private
+    
+      # Setup the root logger with the Rails log level and the desired set of
+  # appenders. The list of appenders to use should be set in the environment
+  # specific configuration file.
+  #
+  # For example, in a production application you would not want to log to
+  # STDOUT, but you would want to send an email for 'error' and 'fatal'
+  # messages:
+  #
+  # => config/environments/production.rb
+  #
+  #     config.log_to = %w[file email]
+  #
+  # In development you would want to log to STDOUT and possibly to a file:
+  #
+  # => config/environments/development.rb
+  #
+  #     config.log_to = %w[stdout file]
+  #
+  Logging.logger.root.appenders = config.log_to unless config.log_to.empty?
+    
+        it 'generates the contacts_json fixture', :fixture => true do
+      json = bob.contacts.map { |c|
+               ContactPresenter.new(c, bob).full_hash_with_person
+             }.to_json
+      save_fixture(json, 'contacts_json')
     end
   end
 end
 
     
-      describe '#get_pane_base_index' do
-    it 'extracts pane-base-index from the global tmux window options' do
-      allow_any_instance_of(Kernel).to receive(:`).
-        with(Regexp.new('tmux.+ show-window-option -g pane-base-index')).
-        and_return('pane-base-index 3\n')
-    
-        def send_pane_command(cmd, window_index, _pane_index)
-      send_keys(cmd, window_index)
+    describe StatusMessagesController, :type => :controller do
+  describe '#bookmarklet' do
+    before do
+      sign_in bob, scope: :user
     end
+    
+    SPREE_GEMS = %w(core api cmd backend frontend sample).freeze
+    
+              def serialize_order(order)
+            resource_serializer.new(order.reload, include: resource_includes, fields: sparse_fields).serializable_hash
+          end
+    
+            def new; end
+    
+          def variant_attributes
+        if @current_user_roles&.include?('admin')
+          @@variant_attributes + [:cost_price]
+        else
+          @@variant_attributes
+        end
+      end
+    end
+  end
+end
