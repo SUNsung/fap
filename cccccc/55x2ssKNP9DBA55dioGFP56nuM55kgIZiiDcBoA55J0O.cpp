5170219c19ce1fa687d3f275b97cac8937456194
SUNsung <weatherius@gmail.com>
2019-05-11 22:32:09 +0300

    Rewrite [55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp] File in: 2019-05-11 22:32:08

--- cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
+++ cccccc/55x2ssKNP9DBA55dioGFP56nuM55kgIZiiDcBoA55J0O.cpp
@@ -1,202 +1,180 @@
 
         
-        #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    
-    { private:
-  struct pair_sort_first {
-    bool operator()(const std::pair<int, int> &left,
-                    const std::pair<int, int> &right) {
-      return left.first < right.first;
-    }
-  };
-  void check_batch_reindex(int initial_num, int final_num,
-                           const Dtype* ridx_data);
-};
-    
-      /**
-   * @brief Computes the Contrastive error gradient w.r.t. the inputs.
-   *
-   * Computes the gradients with respect to the two input vectors (bottom[0] and
-   * bottom[1]), but not the similarity label (bottom[2]).
-   *
-   * @param top output Blob vector (length 1), providing the error gradient with
-   *      respect to the outputs
-   *   -# @f$ (1 \times 1 \times 1 \times 1) @f$
-   *      This Blob's diff will simply contain the loss_weight* @f$ \lambda @f$,
-   *      as @f$ \lambda @f$ is the coefficient of this layer's output
-   *      @f$\ell_i@f$ in the overall Net loss
-   *      @f$ E = \lambda_i \ell_i + \mbox{other loss terms}@f$; hence
-   *      @f$ \frac{\partial E}{\partial \ell_i} = \lambda_i @f$.
-   *      (*Assuming that this top Blob is not used as a bottom (input) by any
-   *      other layer of the Net.)
-   * @param propagate_down see Layer::Backward.
-   * @param bottom input Blob vector (length 2)
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$a@f$; Backward fills their diff with
-   *      gradients if propagate_down[0]
-   *   -# @f$ (N \times C \times 1 \times 1) @f$
-   *      the features @f$b@f$; Backward fills their diff with gradients if
-   *      propagate_down[1]
-   */
-  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    
-    {}  // namespace caffe
-    
-    #include 'allheaders.h'
-    
-    
-    { private:
-  tesseract::ParagraphJustification justification_;
-  int margin_;
-  int first_indent_;
-  int body_indent_;
-  int tolerance_;
-};
-    
-    Determining whether a character is CURRENTLY rejected depends on implicit
-understanding of the SEQUENCE of possible calls. The flags are defined and
-grouped in the REJ_FLAGS enum. These groupings are used in determining a
-characters CURRENT rejection status. Basically, a character is ACCEPTED if
-    
-    // A smart pointer class that implements a double-ended pointer. Each end
-// points to the other end. The copy constructor and operator= have MOVE
-// semantics, meaning that the relationship with the other end moves to the
-// destination of the copy, leaving the source unattached.
-// For this reason both the copy constructor and the operator= take a non-const
-// reference argument, and the const reference versions cannot be used.
-// DoublePtr is useful to incorporate into structures that are part of a
-// collection such as GenericVector or STL containers, where reallocs can
-// relocate the members. DoublePtr is also useful in a GenericHeap, where it
-// can correctly maintain the pointer to an element of the heap despite it
-// getting moved around on the heap.
-class DoublePtr {
- public:
-  DoublePtr() : other_end_(nullptr) {}
-  // Copy constructor steals the partner off src and is therefore a non
-  // const reference arg.
-  // Copying a const DoublePtr generates a compiler error.
-  DoublePtr(DoublePtr& src) {
-    other_end_ = src.other_end_;
-    if (other_end_ != nullptr) {
-      other_end_->other_end_ = this;
-      src.other_end_ = nullptr;
-    }
-  }
-  // Operator= steals the partner off src, and therefore needs src to be a non-
-  // const reference.
-  // Assigning from a const DoublePtr generates a compiler error.
-  void operator=(DoublePtr& src) {
-    Disconnect();
-    other_end_ = src.other_end_;
-    if (other_end_ != nullptr) {
-      other_end_->other_end_ = this;
-      src.other_end_ = nullptr;
-    }
-  }
-    }
-    
-        Rational Sin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Cos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational Tan(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ASin(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ACos(Rational const& rat, ANGLE_TYPE angletype);
-    Rational ATan(Rational const& rat, ANGLE_TYPE angletype);
-    
-        try
-    {
-        lograt(&prat, RATIONAL_PRECISION);
-    }
-    catch (uint32_t error)
-    {
-        destroyrat(prat);
-        throw(error);
-    }
-    
-            int32_t const& Sign() const;
-        int32_t const& Exp() const;
-        std::vector<uint32_t> const& Mantissa() const;
-    
-                m_HistoryCollector.AddCloseBraceToHistory();
-    
-    void CalculatorHistory::ClearHistory()
-{
-    m_historyItems.clear();
+        void App::ClearCache(content::RenderProcessHost* render_process_host) {
+  render_process_host->Send(new ShellViewMsg_ClearCache());
+  nw::RemoveHttpDiskCache(render_process_host->GetBrowserContext(),
+                          render_process_host->GetID());
 }
+    
+    #endif  // CONTENT_NW_SRC_API_BINDINGS_COMMON_H_
 
     
     
-    {                Windows::UI::Xaml::Interop::IBindableObservableVector ^ observable =
-                    dynamic_cast<Windows::UI::Xaml::Interop::IBindableObservableVector ^>(source);
-                if (observable)
-                {
-                    observable->VectorChanged += ref new Windows::UI::Xaml::Interop::BindableVectorChangedEventHandler(
-                        this, &AlwaysSelectedCollectionView::OnSourceBindableVectorChanged);
-                }
+namespace nwapi {
+    }
+    
+    class NwAppSetProxyConfigFunction : public NWSyncExtensionFunction {
+ public:
+  NwAppSetProxyConfigFunction();
+  bool RunNWSync(base::ListValue* response, std::string* error) override;
+    }
+    
+    class NwObjCallObjectMethodAsyncFunction : public UIThreadExtensionFunction {
+ public:
+  NwObjCallObjectMethodAsyncFunction();
+    }
+    
+    void NwDesktopCaptureMonitor::OnSourceNameChanged(DesktopMediaList* list, int index) {
+    DesktopMediaList::Source src = list->GetSource(index);
+    std::unique_ptr<base::ListValue> args = nwapi::nw__screen::OnSourceNameChanged::Create(
+      src.id.ToString(),
+      base::UTF16ToUTF8(src.name));
+    DispatchEvent(
+      events::HistogramValue::UNKNOWN, 
+      nwapi::nw__screen::OnSourceNameChanged::kEventName,
+      std::move(args));    
+  }
+    
+        /*
+        Extract a single plane from 3 channel image
+    */
+    void extract3(const Size2D &size,
+                  const u8 * srcBase, ptrdiff_t srcStride,
+                  u8 * dstBase, ptrdiff_t dstStride,
+                  u32 coi);
+    
+    #define IMPL_ADDWEIGHTED(type)                                \
+void addWeighted(const Size2D &,                              \
+                 const type *, ptrdiff_t,                     \
+                 const type *, ptrdiff_t,                     \
+                 type *, ptrdiff_t,                           \
+                 f32, f32, f32)                               \
+{                                                             \
+    internal::assertSupportedConfiguration();                 \
+}
+    
+    
+    {
+    {
+    {            x -= 8;
+            if(x == colsn){
+                x -= cn;
             }
-    
-    NarratorAnnouncement ^ CalculatorAnnouncement::GetHistoryClearedAnnouncement(String ^ announcement)
-{
-    return ref new NarratorAnnouncement(
-        announcement, CalculatorActivityIds::HistoryCleared, AutomationNotificationKind::ItemRemoved, AutomationNotificationProcessing::MostRecent);
+            s16 prevx[4], rowx[4], nextx[4];
+            for( s32 k = 0; k < cn; k++ )
+            {
+                prevx[(k + x%cn)%cn] = srow2[x+k-cn] + srow1[x+k-cn] + srow0[x+k-cn];
+                rowx[(k + x%cn)%cn] = srow2[x+k] + srow1[x+k] + srow0[x+k];
+            }
+            for( ; x < colsn; x++ )
+            {
+                size_t xx = x%cn;
+                if(x+cn >= colsn) {
+                    // make border
+                    if (borderType == BORDER_MODE_CONSTANT)
+                    {
+                        nextx[xx] = borderValue;
+                    } else if (borderType == BORDER_MODE_REFLECT101)
+                    {
+                        nextx[xx] = srow2[x-cn] + srow1[x-cn] + srow0[x-cn];
+                    } else {
+                        nextx[xx] = srow2[x] + srow1[x] + srow0[x];
+                    }
+                } else {
+                    nextx[xx] = srow2[x+cn] + srow1[x+cn] + srow0[x+cn];
+                }
+                *(drow+x) = internal::saturate_cast<u8>((prevx[xx] + rowx[xx] + nextx[xx])*(1/9.));
+                prevx[xx] = rowx[xx];
+                rowx[xx] = nextx[xx];
+            }
+        }
+    }
+#else
+    (void)srcBase;
+    (void)srcStride;
+    (void)dstBase;
+    (void)dstStride;
+    (void)borderValue;
+#endif
 }
     
-    namespace apollo {
-namespace drivers {
-namespace canbus {
-    }
+    void assertSupportedConfiguration(bool parametersSupported)
+{
+    if (!isSupportedConfiguration()) {
+        std::cerr << 'internal error: attempted to use an unavailable function' << std::endl;
+        std::abort();
     }
     }
     
-    #include 'modules/common/time/time.h'
-#include 'modules/drivers/canbus/common/byte.h'
-#include 'modules/drivers/canbus/common/canbus_consts.h'
-#include 'modules/drivers/radar/conti_radar/protocol/const_vars.h'
+            //left&right borders
+        if (borderType != BORDER_MODE_CONSTANT)
+            for (s32 k = 0; k < cn; ++k)
+            {
+                lane[-cn+k] = lane[idx_l1 + k];
+                lane[-cn-cn+k] = lane[idx_l2 + k];
+    }
+    
+    bool IsUserOnly(std::wstring opt)
+{
+	bool userOnly;
+    }
+    
+    namespace aria2 {
+    }
+    
+    namespace aria2 {
+    }
+    
+      char zero[18];
+  memset(zero, 0, sizeof(zero));
     
     
-    {  double ret = x * OBJECT_WIDTH_RES;
-  return ret;
+    {} // namespace aria2
+    
+        taskFactory->setLocalNode(localNode);
+    taskFactory->setRoutingTable(routingTable.get());
+    taskFactory->setMessageDispatcher(dispatcher.get());
+    taskFactory->setMessageFactory(factory.get());
+    taskFactory->setTaskQueue(taskQueue.get());
+    taskFactory->setTimeout(std::chrono::seconds(messageTimeout));
+    
+    public:
+  DHTTaskExecutor(int numConcurrent);
+    
+      virtual void executeTask() = 0;
+    
+    
+    {  virtual void
+  addImmediateTask(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+};
+    
+    namespace aria2 {
+    }
+    
+    bool DNSCache::CacheEntry::add(const std::string& addr)
+{
+  for (std::vector<AddrEntry>::const_iterator i = addrEntries_.begin(),
+                                              eoi = addrEntries_.end();
+       i != eoi; ++i) {
+    if ((*i).addr_ == addr) {
+      return false;
+    }
+  }
+  addrEntries_.push_back(AddrEntry(addr));
+  return true;
 }
     
-      Byte t1(bytes + 1);
-  int32_t t = t1.get_byte(0, 8);
-  x <<= 8;
-  x |= t;
-    
-        signals:
-        void finished();
-    
-    TEST(lru_cache, lru_kick)
-{
-    dtor_num = 0;
-    shared_ptr<lru_cache_test_class> val = make_shared<lru_cache_test_class>();
-    shared_ptr<lru_cache_test_class> val1 = make_shared<lru_cache_test_class>();
-    shared_ptr<lru_cache_test_class> val2 = make_shared<lru_cache_test_class>();
-    shared_ptr<lru_cache_test_class> val3 = make_shared<lru_cache_test_class>();
+        // out_of_range.403
+    try
+    {
+        // try to use a JSON pointer to an nonexistent object key
+        json::const_reference ref = j.at('/foo'_json_pointer);
+    }
+    catch (json::out_of_range& e)
+    {
+        std::cout << e.what() << '\n';
     }
     
-        unlink(sock1_path);
-    unlink(sock2_path);
-    
-    
-    {    return SW_OK;
-}
-    
-    void MyServer::onReceive(int fd, const DataBuffer &data)
-{
-    swConnection *conn = swWorker_get_connection(&this->serv, fd);
-    printf('onReceive: fd=%d, ip=%s|port=%d Data=%s|Len=%ld\n', fd, swConnection_get_ip(conn),
-           swConnection_get_port(conn), (char *) data.buffer, data.length);
-    }
-    
-    bool signal_init = false;
+        // print values
+    std::cout << object << '\n';
+    std::cout << *res1.first << ' ' << std::boolalpha << res1.second << '\n';
