48b2e87a3394c227c2897d9f18b66bbd125e757b
SUNsung <weatherius@gmail.com>
2019-05-11 09:13:07 +0300

    Rewrite [43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp] File in: 2019-05-11 09:13:07

--- cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
+++ cccccc/43EM8hmCRHiAI436MdQ.eQlo3U43e6fhNdk2BI43GuK.cpp
@@ -1,274 +1,38 @@
 
         
-        #include 'atom/browser/native_window_views.h'
+        OPERATOR_SCHEMA(Glu)
+    .NumInputs(1)
+    .NumOutputs(1)
+    .SetDoc(R'DOC(
+Applies gated linear unit to the input Tensor X. The output Y is half the size
+of the input X, so if the shape of X is [d1, d2, ..., N] shape of Y will be
+[d1, d2, ..., dn/2] and Y(:dn-1, i) = GLU(X(:dn-1, i), X(:dn-1, i+N/2)) =
+X(dn-1, i) * sigmoid(X(dn-1, i+N/2))
+)DOC')
+    .Input(0, 'X', '1D input tensor')
+    .Output(0, 'Y', '1D output tensor');
     
-    class Net : public mate::EventEmitter<Net> {
- public:
-  static v8::Local<v8::Value> Create(v8::Isolate* isolate);
+    #if DMLC_ENABLE_STD_THREAD
+namespace xgboost {
+namespace data {
+    }
     }
     
-      // C++ can not distinguish overloaded member function.
-  template <AtomNetworkDelegate::SimpleEvent type>
-  void SetSimpleListener(mate::Arguments* args);
-  template <AtomNetworkDelegate::ResponseEvent type>
-  void SetResponseListener(mate::Arguments* args);
-  template <typename Listener, typename Method, typename Event>
-  void SetListener(Method method, Event type, mate::Arguments* args);
-    
-    void AutoUpdater::CheckForUpdates() {}
-    
-    URLRequestAboutJob::URLRequestAboutJob(net::URLRequest* request,
-                                       net::NetworkDelegate* network_delegate)
-    : net::URLRequestJob(request, network_delegate), weak_ptr_factory_(this) {}
-    
-    namespace atom {
+    namespace xgboost {
+namespace obj {
+    }
     }
     
-    const gfx::Rect& OffscreenViewProxy::GetBounds() {
-  return view_bounds_;
-}
     
-    #include 'base/process/launch.h'
-#include 'base/strings/stringprintf.h'
-#include 'base/win/scoped_handle.h'
-#include 'sandbox/win/src/nt_internals.h'
-#include 'sandbox/win/src/win_utils.h'
-#include 'ui/base/win/shell.h'
-    
-    #include 'ui/gfx/image/image.h'
-    
-    /* Coin network-specific GUI style information */
-class NetworkStyle
-{
-public:
-    /** Get style associated with provided BIP70 network id, or 0 if not known */
-    static const NetworkStyle *instantiate(const QString &networkId);
-    }
-    
-    #endif // BITCOIN_QT_OPENURIDIALOG_H
+    {
+    {}  // namespace obj
+}  // namespace xgboost
 
     
-    SECP256K1_INLINE static void secp256k1_fe_mul_inner(uint64_t *r, const uint64_t *a, const uint64_t * SECP256K1_RESTRICT b) {
-/**
- * Registers: rdx:rax = multiplication accumulator
- *            r9:r8   = c
- *            r15:rcx = d
- *            r10-r14 = a0-a4
- *            rbx     = b
- *            rdi     = r
- *            rsi     = a / t?
- */
-  uint64_t tmp1, tmp2, tmp3;
-__asm__ __volatile__(
-    'movq 0(%%rsi),%%r10\n'
-    'movq 8(%%rsi),%%r11\n'
-    'movq 16(%%rsi),%%r12\n'
-    'movq 24(%%rsi),%%r13\n'
-    'movq 32(%%rsi),%%r14\n'
-    }
-    
-    
-    {        secp256k1_sha256_initialize(&sha);
-        secp256k1_sha256_write(&sha, y, sizeof(y));
-        secp256k1_sha256_write(&sha, x, sizeof(x));
-        secp256k1_sha256_finalize(&sha, result);
-        ret = 1;
-    }
-    
-    /// Format list of arguments to the stream according to given format string.
-template<typename... Args>
-void format(std::ostream& out, const char* fmt, const Args&... args)
-{
-    vformat(out, fmt, makeFormatList(args...));
-}
-    
-    void ChaCha20::Seek(uint64_t pos)
-{
-    input[12] = pos;
-    input[13] = pos >> 32;
-}
-    
-        mov	INP, [rsp + _INP]
-    add	INP, 64
-    cmp	INP, [rsp + _INP_END]
-    jne	loop0
-    
-        std::string big = RandomString(&rnd, 50000);
-    Put('foo', big);
-    Put('pastfoo', 'v');
-    const Snapshot* snapshot = db_->GetSnapshot();
-    Put('foo', 'tiny');
-    Put('pastfoo2', 'v2');        // Advance sequence number one more
-    
-    int InternalKeyComparator::Compare(const Slice& akey, const Slice& bkey) const {
-  // Order by:
-  //    increasing user key (according to user-supplied comparator)
-  //    decreasing sequence number
-  //    decreasing type (though sequence# should be enough to disambiguate)
-  int r = user_comparator_->Compare(ExtractUserKey(akey), ExtractUserKey(bkey));
-  if (r == 0) {
-    const uint64_t anum = DecodeFixed64(akey.data() + akey.size() - 8);
-    const uint64_t bnum = DecodeFixed64(bkey.data() + bkey.size() - 8);
-    if (anum > bnum) {
-      r = -1;
-    } else if (anum < bnum) {
-      r = +1;
-    }
-  }
-  return r;
-}
-    
-    #include <string>
-    
-    #ifndef CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
-#define CONTENT_NW_SRC_API_CLIPBOARD_CLIPBOARD_H_
-    
-    
-    {protected:
-  BaseEvent(){}
-  virtual ~BaseEvent(){}
-};
-    
-    void MenuDelegate::ExecuteCommand(int command_id, int event_flags) {
-  if (command_id < 0)
-    return;
-    }
-    
-    
-    {  *y = CalculateMenuYPosition(&screen_rect, &menu_req, NULL, *y);
-}
-    
-    #include 'content/nw/src/api/menuitem/menuitem.h'
-    
-     protected:
-  ~NwObjCallObjectMethodSyncFunction() override;
-    
-        std::string type;
-    switch(src.id.type) {
-    case content::DesktopMediaID::TYPE_WINDOW:
-      type = 'window';
-      break;
-    case content::DesktopMediaID::TYPE_SCREEN:
-      type = 'screen';
-      break;
-    case content::DesktopMediaID::TYPE_NONE:
-      type = 'none';
-      break;
-    default:
-      type = 'unknown';
-      break;
-    }
-    
-    void convert_dataset(const char* image_filename, const char* label_filename,
-        const char* db_filename) {
-  // Open files
-  std::ifstream image_file(image_filename, std::ios::in | std::ios::binary);
-  std::ifstream label_file(label_filename, std::ios::in | std::ios::binary);
-  CHECK(image_file) << 'Unable to open file ' << image_filename;
-  CHECK(label_file) << 'Unable to open file ' << label_filename;
-  // Read the magic and the meta data
-  uint32_t magic;
-  uint32_t num_items;
-  uint32_t num_labels;
-  uint32_t rows;
-  uint32_t cols;
-    }
-    
-      inline int offset(const int n, const int c = 0, const int h = 0,
-      const int w = 0) const {
-    CHECK_GE(n, 0);
-    CHECK_LE(n, num());
-    CHECK_GE(channels(), 0);
-    CHECK_LE(c, channels());
-    CHECK_GE(height(), 0);
-    CHECK_LE(h, height());
-    CHECK_GE(width(), 0);
-    CHECK_LE(w, width());
-    return ((n * channels() + c) * height() + h) * width() + w;
-  }
-    
-    /**
- * @brief Applies common transformations to the input data, such as
- * scaling, mirroring, substracting the image mean...
- */
-template <typename Dtype>
-class DataTransformer {
- public:
-  explicit DataTransformer(const TransformationParameter& param, Phase phase);
-  virtual ~DataTransformer() {}
-    }
-    
-      /**
-   * @brief Does layer-specific setup: your layer should implement this function
-   *        as well as Reshape.
-   *
-   * @param bottom
-   *     the preshaped input blobs, whose data fields store the input data for
-   *     this layer
-   * @param top
-   *     the allocated but unshaped output blobs
-   *
-   * This method should do one-time layer specific setup. This includes reading
-   * and processing relevent parameters from the <code>layer_param_</code>.
-   * Setting up the shapes of top blobs and internal buffers should be done in
-   * <code>Reshape</code>, which will be called before the forward pass to
-   * adjust the top blob sizes.
-   */
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top) {}
-    
-      // If there are two top blobs, then the second blob will contain
-  // accuracies per class.
-  virtual inline int MinTopBlobs() const { return 1; }
-  virtual inline int MaxTopBlobs() const { return 2; }
-    
-    
-    { private:
-  struct pair_sort_first {
-    bool operator()(const std::pair<int, int> &left,
-                    const std::pair<int, int> &right) {
-      return left.first < right.first;
-    }
-  };
-  void check_batch_reindex(int initial_num, int final_num,
-                           const Dtype* ridx_data);
-};
-    
-    /**
- * @brief Takes a Blob and crop it, to the shape specified by the second input
- *  Blob, across all dimensions after the specified axis.
- *
- * TODO(dox): thorough documentation for Forward, Backward, and proto params.
- */
-    
-    #endif  // CAFFE_CUDNN_DECONV_LAYER_HPP_
-
-    
-     protected:
-  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
-      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
-    
-    // You can copy and use unmodified imgui_impl_* files in your project. See main.cpp for an example of using this.
-// If you are new to dear imgui, read examples/README.txt and read the documentation at the top of imgui.cpp.
-// https://github.com/ocornut/imgui
-    
-        ImGui_ImplDX10_Shutdown();
-    ImGui_ImplWin32_Shutdown();
-    ImGui::DestroyContext();
-    
-    void CleanupRenderTarget()
-{
-    if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
-}
-    
-        if (g_pd3dDevice->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, g_frameContext[0].CommandAllocator, NULL, IID_PPV_ARGS(&g_pd3dCommandList)) != S_OK ||
-        g_pd3dCommandList->Close() != S_OK)
-        return false;
-    
-    void CleanupDeviceD3D()
-{
-    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
-    if (g_pD3D) { g_pD3D->Release(); g_pD3D = NULL; }
+    void SimpleCSRSource::SaveBinary(dmlc::Stream* fo) const {
+  int tmagic = kMagic;
+  fo->Write(&tmagic, sizeof(tmagic));
+  info.SaveBinary(fo);
+  fo->Write(page_.offset.HostVector());
+  fo->Write(page_.data.HostVector());
 }
