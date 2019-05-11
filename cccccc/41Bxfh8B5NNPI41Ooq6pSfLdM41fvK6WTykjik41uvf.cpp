7108f4785ae867de2739f3b17b1b07f0f6d41aea
SUNsung <weatherius@gmail.com>
2019-05-11 17:22:35 +0300

    Rewrite [41Bxfh8B5NNPI41Ooq6pSfLdM41fvK6WTykjik41uvf.cpp] File in: 2019-05-11 17:22:35

--- cccccc/41Bxfh8B5NNPI41Ooq6pSfLdM41fvK6WTykjik41uvf.cpp
+++ cccccc/41Bxfh8B5NNPI41Ooq6pSfLdM41fvK6WTykjik41uvf.cpp
@@ -1,259 +1,137 @@
 
         
-        static void secp256k1_ecdsa_recoverable_signature_save(secp256k1_ecdsa_recoverable_signature* sig, const secp256k1_scalar* r, const secp256k1_scalar* s, int recid) {
-    if (sizeof(secp256k1_scalar) == 32) {
-        memcpy(&sig->data[0], r, 32);
-        memcpy(&sig->data[32], s, 32);
-    } else {
-        secp256k1_scalar_get_b32(&sig->data[0], r);
-        secp256k1_scalar_get_b32(&sig->data[32], s);
-    }
-    sig->data[64] = recid;
+        
+    {  int result = 0;
+  RenderThread::Get()->Send(new ShellViewHostMsg_AllocateId(
+      routing_id,
+      &result));
+  return scope.Escape(v8::Integer::New(isolate, result));
 }
     
-    static std::string RandomString(Random* rnd, int len) {
-  std::string r;
-  test::RandomString(rnd, len, &r);
-  return r;
+    
+namespace nwapi {
+    }
+    
+    
+#include 'base/basictypes.h'
+    
+     private:
+  ObjectManager* object_manager_;
+    
+    MenuItem::MenuItem(int id,
+                   const base::WeakPtr<ObjectManager>& object_manager,
+                   const base::DictionaryValue& option,
+                   const std::string& extension_id)
+  : Base(id, object_manager, option, extension_id) {
+  Create(option);
 }
     
-      /// @brief Deprecated; use <code>Reshape(const vector<int>& shape)</code>.
-  void Reshape(const int num, const int channels, const int height,
-      const int width);
-  /**
-   * @brief Change the dimensions of the blob, allocating new memory if
-   *        necessary.
-   *
-   * This function can be called both to create an initial allocation
-   * of memory, and to adjust the dimensions of a top blob during Layer::Reshape
-   * or Layer::Forward. When changing the size of blob, memory will only be
-   * reallocated if sufficient memory does not already exist, and excess memory
-   * will never be freed.
-   *
-   * Note that reshaping an input blob and immediately calling Net::Backward is
-   * an error; either Net::Forward or Net::Reshape need to be called to
-   * propagate the new input shape to higher layers.
-   */
-  void Reshape(const vector<int>& shape);
-  void Reshape(const BlobShape& shape);
-  void ReshapeLike(const Blob& other);
-  inline string shape_string() const {
-    ostringstream stream;
-    for (int i = 0; i < shape_.size(); ++i) {
-      stream << shape_[i] << ' ';
-    }
-    stream << '(' << count_ << ')';
-    return stream.str();
+    NwObjCallObjectMethodFunction::~NwObjCallObjectMethodFunction() {
+}
+    
+    
+    {  private:
+    DISALLOW_COPY_AND_ASSIGN(NwScreenIsMonitorStartedFunction);
+  };
+    
+      Pix* pix() const {
+    return pix_;
   }
-  inline const vector<int>& shape() const { return shape_; }
-  /**
-   * @brief Returns the dimension of the index-th axis (or the negative index-th
-   *        axis from the end, if index is negative).
-   *
-   * @param index the axis index, which may be negative as it will be
-   *        'canonicalized' using CanonicalAxisIndex.
-   *        Dies on out of range index.
-   */
-  inline int shape(int index) const {
-    return shape_[CanonicalAxisIndex(index)];
+  void set_pix(Pix* pix) {
+    pix_ = pix;
   }
-  inline int num_axes() const { return shape_.size(); }
-  inline int count() const { return count_; }
+  bool inverse() const {
+    return inverse_;
+  }
+  void set_inverse(bool value) {
+    inverse_ = value;
+  }
+  const DENORM* RootDenorm() const {
+    if (predecessor_ != nullptr)
+      return predecessor_->RootDenorm();
+    return this;
+  }
+  const DENORM* predecessor() const {
+    return predecessor_;
+  }
+  // Accessors - perhaps should not be needed.
+  float x_scale() const {
+    return x_scale_;
+  }
+  float y_scale() const {
+    return y_scale_;
+  }
+  const BLOCK* block() const {
+    return block_;
+  }
+  void set_block(const BLOCK* block) {
+    block_ = block;
+  }
     
-      /**
-   * Caffe's thread local state will be initialized using the current
-   * thread values, e.g. device id, solver index etc. The random seed
-   * is initialized using caffe_rng_rand.
-   */
-  void StartInternalThread();
-    
-    
-#define REGISTER_LAYER_CREATOR(type, creator)                                  \
-  static LayerRegisterer<float> g_creator_f_##type(#type, creator<float>);     \
-  static LayerRegisterer<double> g_creator_d_##type(#type, creator<double>)    \
-    
-    /**
- * @brief Computes a sum of two input Blobs, with the shape of the latter Blob
- *        'broadcast' to match the shape of the former. Equivalent to tiling
- *        the latter Blob, then computing the elementwise sum.
- *
- * The second input may be omitted, in which case it's learned as a parameter
- * of the layer. Note: in case bias and scaling are desired, both operations can
- * be handled by `ScaleLayer` configured with `bias_term: true`.
- */
-template <typename Dtype>
-class BiasLayer : public Layer<Dtype> {
- public:
-  explicit BiasLayer(const LayerParameter& param)
-      : Layer<Dtype>(param) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-    }
-    
-    namespace caffe {
-    }
-    
-    #endif  // CAFFE_CONV_LAYER_HPP_
+    STRING ParagraphModel::ToString() const {
+  char buffer[200];
+  const STRING &alignment = ParagraphJustificationToString(justification_);
+  snprintf(buffer, sizeof(buffer),
+           'margin: %d, first_indent: %d, body_indent: %d, alignment: %s',
+           margin_, first_indent_, body_indent_, alignment.string());
+  return STRING(buffer);
+}
 
     
-    #ifdef USE_CUDNN
-template <typename Dtype>
-class CuDNNLCNLayer : public LRNLayer<Dtype> {
- public:
-  explicit CuDNNLCNLayer(const LayerParameter& param)
-      : LRNLayer<Dtype>(param), handles_setup_(false), tempDataSize(0),
-        tempData1(NULL), tempData2(NULL) {}
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
-      const vector<Blob<Dtype>*>& top);
-  virtual ~CuDNNLCNLayer();
+      // Copies UNICHAR_IDs from dst to src. Returns the number of ids copied.
+  // The function assumes that the arrays are terminated by INVALID_UNICHAR_ID
+  // and that dst has enough space for all the elements from src.
+  static inline int copy(const UNICHAR_ID src[], UNICHAR_ID dst[]) {
+    int i = 0;
+    do {
+      dst[i] = src[i];
+    } while (dst[i++] != INVALID_UNICHAR_ID);
+    return i - 1;
+  }
+    
+      // Connects this and other, discarding any existing connections.
+  void Connect(DoublePtr* other) {
+    other->Disconnect();
+    Disconnect();
+    other->other_end_ = this;
+    other_end_ = other;
+  }
+  // Disconnects this and other, making OtherEnd() return nullptr for both.
+  void Disconnect() {
+    if (other_end_ != nullptr) {
+      other_end_->other_end_ = nullptr;
+      other_end_ = nullptr;
+    }
+  }
+  // Returns the pointer to the other end of the double pointer.
+  DoublePtr* OtherEnd() const {
+    return other_end_;
+  }
+    
+    
+    {        return newMask;
     }
     
-    #include 'caffe/blob.hpp'
-#include 'caffe/layer.hpp'
-#include 'caffe/proto/caffe.pb.h'
-    
-    namespace leveldb {
-namespace log {
-    }
-    }
-    
-    using leveldb::Cache;
-using leveldb::Comparator;
-using leveldb::CompressionType;
-using leveldb::DB;
-using leveldb::Env;
-using leveldb::FileLock;
-using leveldb::FilterPolicy;
-using leveldb::Iterator;
-using leveldb::kMajorVersion;
-using leveldb::kMinorVersion;
-using leveldb::Logger;
-using leveldb::NewBloomFilterPolicy;
-using leveldb::NewLRUCache;
-using leveldb::Options;
-using leveldb::RandomAccessFile;
-using leveldb::Range;
-using leveldb::ReadOptions;
-using leveldb::SequentialFile;
-using leveldb::Slice;
-using leveldb::Snapshot;
-using leveldb::Status;
-using leveldb::WritableFile;
-using leveldb::WriteBatch;
-using leveldb::WriteOptions;
-    
-      // Constant after construction
-  Env* const env_;
-  const InternalKeyComparator internal_comparator_;
-  const InternalFilterPolicy internal_filter_policy_;
-  const Options options_;  // options_.comparator == &internal_comparator_
-  const bool owns_info_log_;
-  const bool owns_cache_;
-  const std::string dbname_;
-    
-     public:
-  explicit InternalKeyComparator(const Comparator* c) : user_comparator_(c) {}
-  virtual const char* Name() const;
-  virtual int Compare(const Slice& a, const Slice& b) const;
-  virtual void FindShortestSeparator(std::string* start,
-                                     const Slice& limit) const;
-  virtual void FindShortSuccessor(std::string* key) const;
-    
-      // Return type, or one of the preceding special values
-  unsigned int ReadPhysicalRecord(Slice* result);
-    
-    TEST(LogTest, MarginalTrailer2) {
-  // Make a trailer that is exactly the same length as an empty record.
-  const int n = kBlockSize - 2 * kHeaderSize;
-  Write(BigString('foo', n));
-  ASSERT_EQ(kBlockSize - kHeaderSize, WrittenBytes());
-  Write('bar');
-  ASSERT_EQ(BigString('foo', n), Read());
-  ASSERT_EQ('bar', Read());
-  ASSERT_EQ('EOF', Read());
-  ASSERT_EQ(0, DroppedBytes());
-  ASSERT_EQ('', ReportMessage());
-}
-    
-      // Returns an estimate of the number of bytes of data in use by this
-  // data structure. It is safe to call when MemTable is being modified.
-  size_t ApproximateMemoryUsage();
-    
-      Status FindFiles() {
-    std::vector<std::string> filenames;
-    Status status = env_->GetChildren(dbname_, &filenames);
-    if (!status.ok()) {
-      return status;
-    }
-    if (filenames.empty()) {
-      return Status::IOError(dbname_, 'repair found no files');
-    }
-    }
-    
-    template <typename Key, class Comparator>
-inline SkipList<Key, Comparator>::Iterator::Iterator(const SkipList* list) {
-  list_ = list;
-  node_ = nullptr;
-}
-    
-      // Given a key, return an approximate byte offset in the file where
-  // the data for that key begins (or would begin if the key were
-  // present in the file).  The returned value is in terms of file
-  // bytes, and so includes effects like compression of the underlying data.
-  // E.g., the approximate offset of the last key in the table will
-  // be close to the file length.
-  uint64_t ApproximateOffsetOf(const Slice& key) const;
-    
-    class LEVELDB_EXPORT TableBuilder {
- public:
-  // Create a builder that will store the contents of the table it is
-  // building in *file.  Does not close the file.  It is up to the
-  // caller to close the file after calling Finish().
-  TableBuilder(const Options& options, WritableFile* file);
-    }
-    
-    TEST(heap, random)
-{
-    swHeap *pq;
-    node_t *ns;
-    node_t *n;
-    }
-    
-    int dtor_num = 0;
-class lru_cache_test_class
-{
-public:
-    lru_cache_test_class()
+        void ProgressWriter::UpdateTraining(size_t samples, const ValuePtr& accumulatedLoss,
+                                        const ValuePtr& accumulatedMetric)
     {
+        m_training->Update(samples, accumulatedLoss, accumulatedMetric,
+            [this](const std::pair<size_t, size_t> samples, std::pair<size_t, size_t> updates,
+                   const std::pair<double, double> aggregateLoss, std::pair<double, double> aggregateMetric)
+            {
+                OnWriteTrainingUpdate(samples, updates, aggregateLoss, aggregateMetric);
+            });
+        OnTrainingUpdateEnd();
     }
-    }
     
-        sleep(1);
-    srand((unsigned int) time(NULL));
-    thread_write();
+                if ((oldOutputRank != SentinelValueForInferParamInitRank) && (oldOutputRank != outputRank))
+                InvalidArgument('Output rank of a non-uniform random initialier cannot be overridden if it has been already specified!');
+    
+    using namespace std;
     
     
-    {
-    {
-    {            ret = chan->push(&i);
-            ASSERT_TRUE(ret);
-            ret = chan->push(&i);
-            ASSERT_TRUE(ret);
-        }, &chan)
-    });
-}
-    
-            pid_t pid = fork();
-        ASSERT_NE(pid, -1);
-    
-    void swReactor_defer_task_destroy(swReactor *reactor)
-{
-    list<defer_task *> *tasks = (list<defer_task *> *) reactor->defer_tasks;
-    delete tasks;
-}
-    
-        SwooleG.use_signalfd = 1;
+    {            // reshape Input(2)
+            Input(2)->SetDims(TensorShape(dimsA), false);
+            fprintf(stderr, '\n%ls %ls operation: For legacy compatibility, the sample layout of third input (%ls %ls operation) was patched to [%s] (from [%s])\n',
+                NodeName().c_str(), OperationName().c_str(), Input(2)->NodeName().c_str(), Input(2)->OperationName().c_str(), string(Input(2)->GetSampleLayout()).c_str(), dimsCstring.c_str());
+        }
