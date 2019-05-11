022edea757dc19a73d591b8575eea5daf5c184b4
SUNsung <weatherius@gmail.com>
2019-05-11 23:01:38 +0300

    Rewrite [25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py] File in: 2019-05-11 23:01:37

--- pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
+++ pyton/25PuTKr3dUjSw25qlDBvvo5xhI25.XYkPjPtqhM259la.py
@@ -1,76 +1,79 @@
 
         
-        
-def create_app(test_config=None):
-    '''Create and configure an instance of the Flask application.'''
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY='dev',
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+            proc.sendline(u'ehco test')
     
-        def logout(self):
-        return self._client.get('/auth/logout')
+    containers = (('thefuck/python3-tcsh',
+               u'''FROM python:3
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'),
+              ('thefuck/python2-tcsh',
+               u'''FROM python:2
+                   RUN apt-get update
+                   RUN apt-get install -yy tcsh''',
+               u'tcsh'))
     
     
-class Environment(BaseEnvironment):
-    '''Works like a regular Jinja2 environment but has some additional
-    knowledge of how Flask's blueprint works so that it can prepend the
-    name of the blueprint to referenced templates if necessary.
-    '''
-    
-            app.add_url_rule('/hello/<name>', view_func=MyView.as_view('myview'))
-    
-                version = match.group(1).strip()
-    
-        def __hash_double_function(self, key, data, increment):
-        return (increment * self.__hash_function_2(key, data)) % self.size_table
-    
-        def _colision_resolution(self, key, data=None):
-        i = 1
-        new_key = self.hash_function(key + i*i)
-    
-            if isinstance(B,bytes):
-            B = B.decode('ascii')
-    
-    # Mock test below
-if False: # change to true to run this test case.
-    import sklearn.datasets as ds
-    dataset = ds.load_iris()
-    k = 3
-    heterogeneity = []
-    initial_centroids = get_initial_centroids(dataset['data'], k, seed=0)
-    centroids, cluster_assignment = kmeans(dataset['data'], k, initial_centroids, maxiter=400,
-                                        record_heterogeneity=heterogeneity, verbose=True)
-    plot_heterogeneity(heterogeneity, k)
+@pytest.mark.parametrize('command, new_command', [
+    (Command('cargo buid', no_such_subcommand_old), 'cargo build'),
+    (Command('cargo buils', no_such_subcommand), 'cargo build')])
+def test_get_new_command(command, new_command):
+    assert get_new_command(command) == new_command
 
     
-        return res
     
-        s = score_euclidean(data[0], data[1])
-    print(s)
+@parametrize_extensions
+@parametrize_filename
+@parametrize_script
+def test_get_new_command(ext, tar_error, filename, unquoted, quoted, script, fixed):
+    tar_error(unquoted.format(ext))
+    assert (get_new_command(Command(script.format(filename.format(ext)), ''))
+            == fixed.format(dir=quoted.format(''), filename=filename.format(ext)))
+
     
-    
-def leaky_relu(x, alpha=0.1):
-    '''渗透 ReLU
-    `o = max(alpha * x, x)`
-    '''
-    return tf.nn.leaky_relu(x, alpha)
-    
-        with tf.variable_scope(name or 'attention_flow', reuse=reuse):
-        h_expand = tf.tile(tf.expand_dims(h, axis=2), [1, 1, J, 1])  # [N, T, J, d]
-        u_expand = tf.tile(tf.expand_dims(u, axis=1), [1, T, 1, 1])  # [N, T, J, d]
-        hu = tf.multiply(h_expand, u_expand)  # [N, T, J, d]
-        h_u_hu = tf.concat([h_expand, u_expand, hu], axis=-1)  # [N, T, J, 3d]
-        W_s = get_w([3 * d, 1])  # [3d, 1]
-    
-    # 可以通过相同的方式获取每个单词以及任一个 n-gram 的向量
-print(model.wv['hello'])
-print(model.wv['<h'])
+    - heroku_collaborator:
+    api_key: YOUR_API_KEY
+    user: '{{ item.user }}'
+    apps: '{{ item.apps | default(apps) }}'
+    suppress_invitation: '{{ item.suppress_invitation | default(suppress_invitation) }}'
+    state: '{{ item.state | default('present') }}'
+  with_items:
+    - { user: 'a.b@example.com' }
+    - { state: 'absent', user: 'b.c@example.com', suppress_invitation: false }
+    - { user: 'x.y@example.com', apps: ['heroku-example-app'] }
 '''
-[-0.03481839  0.00606661  0.02581969  0.00188777  0.0325358 ]
-[ 0.04481247 -0.1784363  -0.03192253  0.07162753  0.16744071]
+    
+    EXAMPLES = '''
+- name: Create a new host in OpenNebula
+  one_host:
+    name: host1
+    cluster_id: 1
+    api_url: http://127.0.0.1:2633/RPC2
+    
+        def role_remove_member(self, name, item):
+        return self._post_json(method='role_remove_member', name=name, item=item)
+    
+        if module.params['environment']:
+        params['deploy[environment]'] = module.params['environment']
+    
+    EXAMPLES = '''
+- name: Add device to CA Spectrum
+  local_action:
+    module: spectrum_device
+    device: '{{ ansible_host }}'
+    community: secret
+    landscape: '0x100000'
+    oneclick_url: http://oneclick.example.com:8080
+    oneclick_user: username
+    oneclick_password: password
+    state: present
+    
+    Loosely based on https://github.com/astropy/astropy/pull/347
 '''
-print()
+    
+    # The language for content autogenerated by Sphinx. Refer to documentation
+# for a list of supported languages.
+#
+# This is also used if you do content translation via gettext catalogs.
+# Usually you set 'language' from the command line for these cases.
+language = None
