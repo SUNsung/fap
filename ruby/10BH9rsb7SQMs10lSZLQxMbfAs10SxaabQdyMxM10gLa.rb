d3e9f5cf764c8c085beb9bd8b81e12ba8c7a0116
SUNsung <weatherius@gmail.com>
2019-05-11 12:42:03 +0300

    Rewrite [10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb] File in: 2019-05-11 12:42:02

--- ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
+++ ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
@@ -1,43 +1,113 @@
 
         
-                redirect_to admin_report_path(@report), notice: I18n.t('admin.report_notes.created_msg')
-      else
-        @report_notes = @report.notes.latest
-        @report_history = @report.history
-        @form = Form::StatusBatch.new
+        require_relative 'converter/fonts_conversion'
+require_relative 'converter/less_conversion'
+require_relative 'converter/js_conversion'
+require_relative 'converter/logger'
+require_relative 'converter/network'
     
-      def send_export_file
-    respond_to do |format|
-      format.csv { send_data export_data, filename: export_filename }
-    end
+      def test_font_helper_without_suffix
+    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
   end
     
-    Given(/^a test app without any configuration$/) do
-  TestApp.create_test_app
-end
-    
-            if obj && obj.errors[method].present?
-          errors = safe_join(obj.errors[method], '<br />'.html_safe)
-          content_tag(:span, errors, class: 'formError')
-        else
-          ''
-        end
+          def title
+        'Comparison of #{@page.title}'
       end
     
-        for_each_gem do |gem_path|
-      sh 'gem push '#{gem_path}''
-    end
-  end
+          def remove_page_extentions(page_path)
+        Gollum::Markup.formats.values.each do |format|
+          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
+        end
+        return page_path
+      end
+    
+    def testpath(path)
+  File.join(TEST_DIR, path)
 end
     
-            def order_params
-          if params[:order]
-            normalize_params
-            params.require(:order).permit(permitted_order_attributes)
-          else
-            {}
-          end
-        end
+        @view = Precious::Views::Page.new
+    @view.instance_variable_set :@page, page
+    @view.instance_variable_set :@content, page.formatted_data
+    @view.instance_variable_set :@h1_title, false
     
-              options = { variants_attrs: variants_params, options_attrs: option_types_params }
-          @product = Core::Importer::Product.new(nil, product_params, options).create
+      test 'creates korean page which contains korean content' do
+    post '/create', :content => '한글 text', :page => 'k',
+         :format             => 'markdown', :message => 'def'
+    follow_redirect!
+    assert last_response.ok?
+    
+    desc 'Validate lib files and version file'
+task :validate do
+  libfiles = Dir['lib/*'] - ['lib/#{name}.rb', 'lib/#{name}']
+  unless libfiles.empty?
+    puts 'Directory `lib` should only contain a `#{name}.rb` file and `#{name}` dir.'
+    exit!
+  end
+  unless Dir['VERSION*'].empty?
+    puts 'A `VERSION` file at root level violates Gem best practices.'
+    exit!
+  end
+end
+
+    
+      context 'moving variants between shipments', js: true do
+    before do
+      create(:stock_location, name: 'LA')
+      visit spree.admin_orders_path
+      within_row(1) do
+        click_link 'R100'
+      end
+    end
+    
+    desc 'Creates a sandbox application for simulating the Spree code in a deployed Rails app'
+task :sandbox do
+  Bundler.with_clean_env do
+    exec('lib/sandbox.sh')
+  end
+end
+
+    
+            def update
+          authorize! :update, @order, order_token
+    
+          @@product_property_attributes = [
+        :id, :product_id, :property_id, :value, :property_name
+      ]
+    
+    RSpec.describe JSFiddleTag, type: :liquid_template do
+  describe '#link' do
+    let(:jsfiddle_link) { 'http://jsfiddle.net/link2twenty/v2kx9jcd' }
+    let(:jsfiddle_link_with_custom_tabs) { 'http://jsfiddle.net/link2twenty/v2kx9jcd result,html,css' }
+    
+    When /^(?:|I )follow '([^']*)'$/ do |link|
+  click_link(link)
+end
+    
+      def migration_file_name
+    '#{migration_name}.rb'
+  end
+    
+        def clear
+      @attachments = Hash.new { |h,k| h[k] = {} }
+    end
+    
+        def cropping dst, ratio, scale
+      if ratio.horizontal? || ratio.square?
+        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
+      else
+        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
+      end
+    end
+    
+        def add_active_record_callbacks
+      name = @name
+      @klass.send(:after_save) { send(name).send(:save) }
+      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
+      if @klass.respond_to?(:after_commit)
+        @klass.send(:after_commit, on: :destroy) do
+          send(name).send(:flush_deletes)
+        end
+      else
+        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
+      end
+    end
