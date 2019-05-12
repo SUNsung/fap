9b5a5b66931f978d0f1257fbe8f5a8e1c1ae96a7
SUNsung <weatherius@gmail.com>
2019-05-13 02:32:35 +0300

    Rewrite [14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb] File in: 2019-05-13 02:32:34

--- ruby/14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb
+++ ruby/14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb
@@ -1,27 +1,65 @@
 
         
-              # Checks whether this node body is a void context.
-      #
-      # @return [Boolean] whether the `def` node body is a void context
-      def void_context?
-        method?(:initialize) || assignment_method?
-      end
+                  # Reload so we have the latest data, then delete and save
+          unlocked_reload
+          @machines.delete(entry.id)
+          unlocked_save
     
-          # Returns the delta between this pair's key and the argument pair's.
-      #
-      # @note Keys on the same line always return a delta of 0
-      # @note Keyword splats always return a delta of 0 for right alignment
-      #
-      # @param [Symbol] alignment whether to check the left or right side
-      # @return [Integer] the delta between the two keys
-      def key_delta(other, alignment = :left)
-        HashElementDelta.new(self, other).key_delta(alignment)
-      end
+              # Make sure we're only working with one VM if single target
+          if options[:single_target] && vms.length != 1
+            vm = @env.primary_vm
+            raise Errors::MultiVMTargetRequired if !vm
+            vms = [vm]
+          end
     
-    module RuboCop
-  module Cop
-    # This auto-corrects gem dependency order
-    class OrderedGemCorrector
-      extend OrderedGemNode
+            # Sets a human-friendly description of the plugin.
+        #
+        # @param [String] value Description of the plugin.
+        # @return [String] Description of the plugin.
+        def self.description(value=UNSET_VALUE)
+          get_or_set(:description, value)
+        end
     
-            private
+            # This method is expected to return a class that is used for
+        # configuring the provisioner. This return value is expected to be
+        # a subclass of {Config}.
+        #
+        # @return [Config]
+        def self.config_class
+        end
+    
+            # This contains all the synced folder implementations by name.
+        #
+        # @return [Registry<Symbol, Array<Class, Integer>>]
+        attr_reader :synced_folders
+    
+    module Vagrant
+  module Plugin
+    module V2
+      # This class maintains a list of all the registered plugins as well
+      # as provides methods that allow querying all registered components of
+      # those plugins as a single unit.
+      class Manager
+        attr_reader :registered
+    
+    module Vagrant
+  module Plugin
+    module V2
+      # This is the superclass for all V2 plugins.
+      class Plugin
+        # Special marker that can be used for action hooks that matches
+        # all action sequences.
+        ALL_ACTIONS = :__all_actions__
+    
+      attr_accessor :t, :hit, :pl, :n
+end
+    
+      def pod_prefix
+    File.expand_path('../..', pod_bin)
+  end
+    
+            def run
+          print_version
+          signal_end_of_output
+          listen
+        end
