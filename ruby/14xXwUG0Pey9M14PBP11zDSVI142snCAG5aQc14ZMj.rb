627239ee8af663435ca22fd8f7f4688a6bd0d64d
SUNsung <weatherius@gmail.com>
2019-05-12 11:21:32 +0300

    Rewrite [14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb] File in: 2019-05-12 11:21:32

--- ruby/14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb
+++ ruby/14xXwUG0Pey9M14PBP11zDSVI142snCAG5aQc14ZMj.rb
@@ -1,197 +1,27 @@
-module Vagrant
-  module Plugin
-    module V2
-      # This is the base class for a provider for the V2 API. A provider
-      # is responsible for creating compute resources to match the needs
-      # of a Vagrant-configured system.
-      class Provider
-        include CapabilityHost
-    
-        def resource_params
-      params.require(:email_domain_block).permit(:domain)
-    end
-  end
-end
 
-    
-      def account_feed_path?
-    hub_topic_params[:controller] == 'accounts' && hub_topic_params[:action] == 'show' && hub_topic_params[:format] == 'atom'
-  end
-end
-
-    
-      def update
-    if subscription.verify(body, request.headers['HTTP_X_HUB_SIGNATURE'])
-      ProcessingWorker.perform_async(@account.id, body.force_encoding('UTF-8'))
-    end
-    
-      def update
-    raise ActiveRecord::RecordNotFound if @web_subscription.nil?
-    
-        data = {
-      alerts: {
-        follow: alerts_enabled,
-        favourite: alerts_enabled,
-        reblog: alerts_enabled,
-        mention: alerts_enabled,
-      },
-    }
-    
-        class << self
-      def elastic_pack_base_uri
-        env_url = ENV['LOGSTASH_PACK_URL']
-        (env_url.nil? || env_url.empty?) ? DEFAULT_PACK_URL : env_url
+        
+              # Checks whether this node body is a void context.
+      #
+      # @return [Boolean] whether the `def` node body is a void context
+      def void_context?
+        method?(:initialize) || assignment_method?
       end
     
-          # Install the gems to make them available locally when bundler does his local resolution
-      post_install_messages = []
-      pack.gems.each do |packed_gem|
-        PluginManager.ui.debug('Installing, #{packed_gem.name}, version: #{packed_gem.version} file: #{packed_gem.file}')
-        post_install_messages << LogStash::PluginManager::GemInstaller::install(packed_gem.file, packed_gem.plugin?)
+          # Returns the delta between this pair's key and the argument pair's.
+      #
+      # @note Keys on the same line always return a delta of 0
+      # @note Keyword splats always return a delta of 0 for right alignment
+      #
+      # @param [Symbol] alignment whether to check the left or right side
+      # @return [Integer] the delta between the two keys
+      def key_delta(other, alignment = :left)
+        HashElementDelta.new(self, other).key_delta(alignment)
       end
     
-              it 'successfully install the plugin when verification is disabled' do
-            command = logstash.run_command_in_path('bin/logstash-plugin install --no-verify logstash-filter-qatest')
-            expect(command).to install_successfully
-            expect(logstash).to have_installed?('logstash-filter-qatest')
-          end
+    module RuboCop
+  module Cop
+    # This auto-corrects gem dependency order
+    class OrderedGemCorrector
+      extend OrderedGemNode
     
-        context 'with a specific plugin' do
-      let(:plugin_name) { 'logstash-input-stdin' }
-      it 'list the plugin and display the plugin name' do
-        result = logstash.run_command_in_path('bin/logstash-plugin list #{plugin_name}')
-        expect(result).to run_successfully_and_output(/^#{plugin_name}$/)
-      end
-    
-          def is_edit_page
-        false
-      end
-    
-          def next_link
-        label = 'Next &raquo;'
-        if @versions.size == Gollum::Page.per_page
-          link = '/history/#{@page.name}?page=#{@page_num+1}'
-          %(<a href='#{link}' hotkey='l'>#{label}</a>)
-        else
-          %(<span class='disabled'>#{label}</span>)
-        end
-      end
-    end
-  end
-end
-
-    
-        def self.teardown(&block)
-      define_method(:teardown, &block)
-    end
-  end
-  (
-  class << klass;
-    self
-  end).send(:define_method, :name) { name.gsub(/\W/, '_') }
-  $contexts << klass
-  klass.class_eval &block
-end
-    
-        get page1
-    assert_match /Last edited by <b>user1/, last_response.body
-    
-        # Extract the 'page' name from the file_path
-    def extract_name(file_path)
-      if file_path[-1, 1] == '/'
-        return nil
-      end
-    
-            ActiveSupport.on_load(:active_record) do
-          include Sidekiq::Extensions::ActiveRecord
-        end
-        ActiveSupport.on_load(:action_mailer) do
-          extend Sidekiq::Extensions::ActionMailer
-        end
-      end
-    
-            begin
-          File.open(fp.path, 'a') { |tmpfp| fp.reopen(tmpfp) }
-          fp.sync = true
-          nr += 1
-        rescue IOError, Errno::EBADF
-          # not much we can do...
-        end
-      end
-      nr
-    rescue RuntimeError => ex
-      # RuntimeError: ObjectSpace is disabled; each_object will only work with Class, pass -X+O to enable
-      puts 'Unable to reopen logs: #{ex.message}'
-    end
-    
-          def __set_test_mode(mode)
-        if block_given?
-          current_mode = self.__test_mode
-          begin
-            self.__test_mode = mode
-            yield
-          ensure
-            self.__test_mode = current_mode
-          end
-        else
-          self.__test_mode = mode
-        end
-      end
-    
-        def disable(*opts)
-      opts.each {|key| set(key, false) }
-    end
-    
-      def template(path)
-    template_path = File.join(template_dir, path)
-    template_code = File.read(template_path)
-    logger.info('Reading template', :path => template_path)
-    erb = ERB.new(template_code, nil, '-')
-    erb.filename = template_path
-    return erb
-  end # def template
-    
-            # Clear ownership fields
-        header = replace_ownership_headers(header, false)
-    
-      def to_s(format=nil)
-    return super(format.nil? ? 'NAME-VERSION.EXTENSION' : format)
-  end # def to_s
-    
-      option '--data-dir', 'DATA_DIR',
-    'Specify php dir relative to prefix if differs from pear default (pear/data)'
-    
-        File.write(build_path('comment'),  self.description + '\n')
-    
-        platforms.each do |platform|
-      logger.info('Generating service manifest.', :platform => platform.class.name)
-      platform.program = command.first
-      platform.name = attributes[:pleaserun_name]
-      platform.args = command[1..-1]
-      platform.description = if attributes[:description_given?]
-        attributes[:description]
-      else
-        platform.name
-      end
-      pleaserun_attributes.each do |attribute_name|
-        attribute = 'pleaserun_#{attribute_name}'.to_sym
-        if attributes.has_key?(attribute) and not attributes[attribute].nil?
-          platform.send('#{attribute_name}=', attributes[attribute])
-        end
-      end
-    
-        ::Dir.mkdir(File.join(builddir, 'manifests'))
-    manifests.each do |manifest|
-      dir = File.join(builddir, 'manifests', File.dirname(manifest))
-      logger.info('manifests targeting: #{dir}')
-      ::Dir.mkdir(dir) if !File.directory?(dir)
-    
-      # Output a zipfile.
-  def output(output_path)
-    output_check(output_path)
-    realpath = Pathname.new(output_path).realdirpath.to_s
-    ::Dir.chdir(staging_path) do
-      safesystem('zip', '-9r', realpath, '.')
-    end
-  end # def output
-end # class FPM::Package::Tar
+            private
