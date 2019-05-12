ba3f867832a0b4d1059f87eb0174b2c1a5377239
SUNsung <weatherius@gmail.com>
2019-05-12 16:28:33 +0300

    Rewrite [10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb] File in: 2019-05-12 16:28:32

--- ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
+++ ruby/10BH9rsb7SQMs10lSZLQxMbfAs10SxaabQdyMxM10gLa.rb
@@ -1,113 +1,73 @@
 
         
-        require_relative 'converter/fonts_conversion'
-require_relative 'converter/less_conversion'
-require_relative 'converter/js_conversion'
-require_relative 'converter/logger'
-require_relative 'converter/network'
-    
-      def test_font_helper_without_suffix
-    assert_match %r(url\(['']?/assets/.*eot['']?\)), @css
-  end
-    
-          def title
-        'Comparison of #{@page.title}'
-      end
-    
-          def remove_page_extentions(page_path)
-        Gollum::Markup.formats.values.each do |format|
-          page_path = page_path.gsub(/\.#{format[:regexp]}$/, '')
-        end
-        return page_path
-      end
-    
-    def testpath(path)
-  File.join(TEST_DIR, path)
-end
-    
-        @view = Precious::Views::Page.new
-    @view.instance_variable_set :@page, page
-    @view.instance_variable_set :@content, page.formatted_data
-    @view.instance_variable_set :@h1_title, false
-    
-      test 'creates korean page which contains korean content' do
-    post '/create', :content => '한글 text', :page => 'k',
-         :format             => 'markdown', :message => 'def'
-    follow_redirect!
-    assert last_response.ok?
-    
-    desc 'Validate lib files and version file'
-task :validate do
-  libfiles = Dir['lib/*'] - ['lib/#{name}.rb', 'lib/#{name}']
-  unless libfiles.empty?
-    puts 'Directory `lib` should only contain a `#{name}.rb` file and `#{name}` dir.'
-    exit!
-  end
-  unless Dir['VERSION*'].empty?
-    puts 'A `VERSION` file at root level violates Gem best practices.'
-    exit!
+          it 'imports a scenario which requires a service' do
+    visit new_scenario_imports_path
+    attach_file('Option 2: Upload a Scenario JSON File', File.join(Rails.root, 'spec/data_fixtures/twitter_scenario.json'))
+    click_on 'Start Import'
+    check('I confirm that I want to import these Agents.')
+    expect { click_on 'Finish Import' }.to change(Scenario, :count).by(1)
+    expect(page).to have_text('Import successful!')
   end
 end
 
     
-      context 'moving variants between shipments', js: true do
-    before do
-      create(:stock_location, name: 'LA')
-      visit spree.admin_orders_path
-      within_row(1) do
-        click_link 'R100'
+      describe 'DotHelper::DotDrawer' do
+    describe '#id' do
+      it 'properly escapes double quotaion and backslash' do
+        expect(DotHelper::DotDrawer.draw(foo: '') {
+          id('hello\\'')
+        }).to eq(''hello\\\\\\''')
       end
     end
-    
-    desc 'Creates a sandbox application for simulating the Spree code in a deployed Rails app'
-task :sandbox do
-  Bundler.with_clean_env do
-    exec('lib/sandbox.sh')
   end
 end
 
     
-            def update
-          authorize! :update, @order, order_token
+      describe '#scenario_label' do
+    it 'creates a scenario label with the scenario name' do
+      expect(scenario_label(scenario)).to eq(
+        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Scene</span>'
+      )
+    end
     
-          @@product_property_attributes = [
-        :id, :product_id, :property_id, :value, :property_name
+          expect(exporter.as_json[:control_links]).to eq([{ :controller => guid_order(agent_list, :jane_rain_notifier_agent), :control_target => guid_order(agent_list, :jane_weather_agent) }])
+    end
+  end
+    
+        it 'always succeeds in sorting even if it finds pairs of incomparable objects' do
+      time = Time.now
+      tuples = [
+        [2,   'a', time - 1],  # 0
+        [1,   'b', nil],       # 1
+        [1,   'b', time],      # 2
+        ['2', nil, time],      # 3
+        [1,   nil, time],      # 4
+        [nil, 'a', time + 1],  # 5
+        [2,   'a', time],      # 6
       ]
+      orders = [true, false, true]
+      expected = tuples.values_at(3, 6, 0, 4, 2, 1, 5)
     
-    RSpec.describe JSFiddleTag, type: :liquid_template do
-  describe '#link' do
-    let(:jsfiddle_link) { 'http://jsfiddle.net/link2twenty/v2kx9jcd' }
-    let(:jsfiddle_link_with_custom_tabs) { 'http://jsfiddle.net/link2twenty/v2kx9jcd result,html,css' }
+        def show
+      authorize @user, :change_email?
+    end
     
-    When /^(?:|I )follow '([^']*)'$/ do |link|
-  click_link(link)
-end
-    
-      def migration_file_name
-    '#{migration_name}.rb'
+      def update
+    if verify_payload?
+      process_salmon
+      head 202
+    elsif payload.present?
+      render plain: signature_verification_failure_reason, status: 401
+    else
+      head 400
+    end
   end
     
-        def clear
-      @attachments = Hash.new { |h,k| h[k] = {} }
+      def after_sign_in_path_for(resource)
+    if resource.email_verified?
+      root_path
+    else
+      finish_signup_path
     end
-    
-        def cropping dst, ratio, scale
-      if ratio.horizontal? || ratio.square?
-        '%dx%d+%d+%d' % [ dst.width, dst.height, 0, (self.height * scale - dst.height) / 2 ]
-      else
-        '%dx%d+%d+%d' % [ dst.width, dst.height, (self.width * scale - dst.width) / 2, 0 ]
-      end
-    end
-    
-        def add_active_record_callbacks
-      name = @name
-      @klass.send(:after_save) { send(name).send(:save) }
-      @klass.send(:before_destroy) { send(name).send(:queue_all_for_delete) }
-      if @klass.respond_to?(:after_commit)
-        @klass.send(:after_commit, on: :destroy) do
-          send(name).send(:flush_deletes)
-        end
-      else
-        @klass.send(:after_destroy) { send(name).send(:flush_deletes) }
-      end
-    end
+  end
+end
