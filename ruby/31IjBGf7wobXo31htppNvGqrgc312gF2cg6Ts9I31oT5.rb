f6b052b10a8727340aa809b4d578193b680368b6
SUNsung <weatherius@gmail.com>
2019-05-12 22:22:36 +0300

    Rewrite [31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb] File in: 2019-05-12 22:22:36

--- ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
+++ ruby/31IjBGf7wobXo31htppNvGqrgc312gF2cg6Ts9I31oT5.rb
@@ -1,82 +1,34 @@
 
         
-        WITH_JUST_LIQUID_VAR = <<-LIQUID.freeze
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce auctor libero at
-pharetra tempus. et metus fermentum, eu cursus lorem, ac dictum enim.
-mattis. Curabitur vel dui et lacus rutrum suscipit et {{ title }} neque.
-    
-      p.option 'source', '-s', '--source [DIR]', 'Source directory (defaults to ./)'
-  p.option 'destination', '-d', '--destination [DIR]',
-    'Destination directory (defaults to ./_site)'
-  p.option 'safe', '--safe', 'Safe mode (defaults to false)'
-  p.option 'plugins_dir', '-p', '--plugins PLUGINS_DIR1[,PLUGINS_DIR2[,...]]', Array,
-    'Plugins directory (defaults to ./_plugins)'
-  p.option 'layouts_dir', '--layouts DIR', String,
-    'Layouts directory (defaults to ./_layouts)'
-  p.option 'profile', '--profile', 'Generate a Liquid rendering profile'
-    
-          def feature_element_timing_key(feature_element)
-        '\'#{feature_element.name}\' (#{feature_element.location})'
-      end
-    
-    def jekyll_run_status
-  if Paths.status_file.file?
-    then return Paths.status_file.read
-  end
-end
-    
-        def no_subcommand(args)
-      unless args.empty? ||
-          args.first !~ %r(!/^--/!) || %w(--help --version).include?(args.first)
-        deprecation_message 'Jekyll now uses subcommands instead of just switches. \
-                          Run `jekyll help` to find out more.'
-        abort
-      end
-    end
-    
-          # Reads a cache key.
-      #
-      # If the key exists and has a non-empty value its TTL is refreshed
-      # automatically.
-      #
-      # raw_key - The cache key to read.
-      # timeout - The new timeout of the key if the key is to be refreshed.
-      def self.read(raw_key, timeout: TIMEOUT)
-        key = cache_key_for(raw_key)
-        value = Redis::Cache.with { |redis| redis.get(key) }
-    
-              hash[:author] &&= Representation::User.from_json_hash(hash[:author])
-    
-    $stderr.puts <<DEPRECATION
-WARNING: Ruby Sass's Git repository is moving, and the old repository will be
-deled on 26 March 2019! Please update your Git URLs to point to the new
-repository at https://github.com/sass/ruby-sass.
-    
-      # An environment that can write to in-scope global variables, but doesn't
-  # create new variables in the global scope. Useful for top-level control
-  # directives.
-  class SemiGlobalEnvironment < Environment
-    def try_set_var(name, value)
-      @vars ||= {}
-      if @vars.include?(name)
-        @vars[name] = value
-        true
-      elsif @parent
-        @parent.try_set_var(name, value)
-      else
-        false
+                # Prints the list of specs & pod cache dirs for a single pod name.
+        #
+        # This output is valid YAML so it can be parsed with 3rd party tools
+        #
+        # @param [Array<Hash>] cache_descriptors
+        #        The various infos about a pod cache. Keys are
+        #        :spec_file, :version, :release and :slug
+        #
+        def print_pod_cache_infos(pod_name, cache_descriptors)
+          UI.puts '#{pod_name}:'
+          cache_descriptors.each do |desc|
+            if @short_output
+              [:spec_file, :slug].each { |k| desc[k] = desc[k].relative_path_from(@cache.root) }
+            end
+            UI.puts('  - Version: #{desc[:version]}')
+            UI.puts('    Type:    #{pod_type(desc)}')
+            UI.puts('    Spec:    #{desc[:spec_file]}')
+            UI.puts('    Pod:     #{desc[:slug]}')
+          end
+        end
       end
     end
   end
 end
 
     
-    module Jekyll
-    
-        def sizes
-      attrs = 'width='#{@sizes[0]}'' if @sizes[0]
-      attrs += ' height='#{@sizes[1]}'' if @sizes[1]
-      attrs
+        it 'accepts jsfiddle link with a / at the end' do
+      jsfiddle_link = 'http://jsfiddle.net/link2twenty/v2kx9jcd/'
+      expect do
+        generate_new_liquid(jsfiddle_link)
+      end.not_to raise_error
     end
-  end
-end
