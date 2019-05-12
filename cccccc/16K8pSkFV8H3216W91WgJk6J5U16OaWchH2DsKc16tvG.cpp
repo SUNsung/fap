8125e7aeb17ef414f1b48bea61d22d0f70b83675
SUNsung <weatherius@gmail.com>
2019-05-12 04:08:08 +0300

    Rewrite [16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp] File in: 2019-05-12 04:08:07

--- cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
+++ cccccc/16K8pSkFV8H3216W91WgJk6J5U16OaWchH2DsKc16tvG.cpp
@@ -1,267 +1,128 @@
 
         
-        
-    {
-    {        for (; j < size.width; j++)
-        {
-            dst[j] = ~src[j];
-        }
-    }
-#else
-    (void)size;
-    (void)srcBase;
-    (void)srcStride;
-    (void)dstBase;
-    (void)dstStride;
-#endif
+        void CacheImpl::removeAll() {
+  cache_remove_all(static_cast<cache_t*>(Impl));
 }
     
-        inline void operator()(const u8* src, s16* dstx, s16* dsty, ptrdiff_t width)
-    {
-        uint8x8_t l = vtbl1_u8(vld1_u8(src - lookLeft), vfmask);
-        ptrdiff_t i = 0;
-        for (; i < width - 8 + lookRight; i += 8)
-        {
-            internal::prefetch(src + i);
-            uint8x8_t l18u = vld1_u8(src + i + 1);
-    }
-    }
-    
-    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
-CVT_FUNC(s32, f32, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src]]                              \n\t'
-             'vcvt.f32.s32 q1, q0                                    \n\t'
-             'vst1.32 {d2-d3}, [%[dst]]                              \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i),
-               [dst] 'r' (_dst + i)
-             : 'd0','d1','d2','d3'//,'d4','d5'
-         );
-         __asm__ (
-             'vld1.32 {d0-d1}, [%[src]]                              \n\t'
-             'vcvt.f32.s32 q1, q0                                    \n\t'
-             'vst1.32 {d2-d3}, [%[dst]]                              \n\t'
-             : /*no output*/
-             : [src] 'r' (_src + i + 4),
-               [dst] 'r' (_dst + i + 4)
-             : 'd0','d1','d2','d3'//,'d4','d5'
-         );
-     }
-})
-#else
-CVT_FUNC(s32, f32, 8,
-,
-{
-     for (size_t i = 0; i < w; i += 8)
-     {
-         internal::prefetch(_src + i);
-         int32x4_t vline_s32 = vld1q_s32(_src + i);
-         float32x4_t vline_f32 = vcvtq_f32_s32(vline_s32);
-         vst1q_f32(_dst + i, vline_f32);
-    }
-    }
-    
-                    tprev[1] = tcurr[1];
-                tcurr[1] = tnext[1];
-    
-            float32x4_t vc0 = vmovq_n_f32(0);
-        int32x4_t vs = vmovq_n_s32(0);
-    
-    
-    {        process(src, j, size.width, i,
-                minVal, minLocPtr, minLocCount, minLocCapacity,
-                maxVal, maxLocPtr, maxLocCount, maxLocCapacity);
-    }
-    
-    template <typename T>
-void flip(const Size2D & size,
-          const void * srcBase, ptrdiff_t srcStride,
-          void * dstBase, ptrdiff_t dstStride,
-          FLIP_MODE flipMode)
-{
-    using namespace internal;
-    }
-    
-                s32 val = (prevx + currx + nextx) - 9 * srow1[x];
-            drow[x] = internal::saturate_cast<u8>((s32)val);
-    
-    template <typename T1, typename T2, typename T3, typename T4, typename T5,
-          typename T6, typename T7, typename T8, typename T9>
-void PrintTo(const ::std::tr1::tuple<T1, T2, T3, T4, T5, T6, T7, T8, T9>& t,
-             ::std::ostream* os) {
-  PrintTupleTo(t, os);
-}
-    
-      // Returns true iff the test part fatally failed.
-  bool fatally_failed() const { return type_ == kFatalFailure; }
-    
-    // The 'Types' template argument below must have spaces around it
-// since some compilers may choke on '>>' when passing a template
-// instance (e.g. Types<int>)
-# define TYPED_TEST_CASE(CaseName, Types) \
-  typedef ::testing::internal::TypeList< Types >::type \
-      GTEST_TYPE_PARAMS_(CaseName)
-    
-    // The result of a single Test.  This includes a list of
-// TestPartResults, a list of TestProperties, a count of how many
-// death tests there are in the Test, and how much time it took to run
-// the Test.
-//
-// TestResult is not copyable.
-class GTEST_API_ TestResult {
- public:
-  // Creates an empty TestResult.
-  TestResult();
-    }
-    
-    #define GTEST_ASSERT_(expression, on_failure) \
-  GTEST_AMBIGUOUS_ELSE_BLOCKER_ \
-  if (const ::testing::AssertionResult gtest_ar = (expression)) \
-    ; \
-  else \
-    on_failure(gtest_ar.failure_message())
-    
-    // FilePath - a class for file and directory pathname manipulation which
-// handles platform-specific conventions (like the pathname separator).
-// Used for helper functions for naming files in a directory for xml output.
-// Except for Set methods, all methods are const or static, which provides an
-// 'immutable value object' -- useful for peace of mind.
-// A FilePath with a value ending in a path separator ('like/this/') represents
-// a directory, otherwise it is assumed to represent a file. In either case,
-// it may or may not represent an actual file or directory in the file system.
-// Names are NOT checked for syntax correctness -- no checking for illegal
-// characters, malformed paths, etc.
-    
-    #include 'gtest/internal/gtest-port.h'
-    
-    
-    {  return result;
-}
-    
-    // In this example, we test the MyString class (a simple string).
-    
-        E* element = new E(old_head->element());
-    delete old_head;
-    
-    	STARTUPINFO si = { 0 };
-    
-    /*!
- * \brief Iterator that normalize a image.
- *  It also applies a few augmention before normalization.
- */
-class ImageDetNormalizeIter : public IIterator<DataInst> {
- public:
-  explicit ImageDetNormalizeIter(IIterator<DataInst> *base)
-      : base_(base), meanfile_ready_(false) {
-  }
-    }
-    
-    template <>
-void EvalBroadcast<DEVICE>(TBlob const& src, TBlob* ret, int size, RunContext ctx) {
-  typedef DEVICE xpu;
-  mshadow::Stream<xpu>* s = ctx.get_stream<xpu>();
-  mshadow::Tensor<xpu, 3> out = ret->get<xpu, 3, real_t>(s);
-  mshadow::Tensor<xpu, 2> in = src.get<xpu, 2, real_t>(s);
-  out = mshadow::expr::broadcast_with_axis(in, 0, size);
+    namespace clang {
+  class RecordDecl;
+  class TypedefNameDecl;
 }
     
     
     {
-    {.add_argument('data', 'Symbol or Symbol[]', 'Tensor or List of Tensors, the second input '
-'will be used as crop_like shape reference')
-.add_arguments(CropParam::__FIELDS__())
-.set_key_var_num_args('num_args');
-}  // namespace op
-}  // namespace mxnet
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace csharp
+}  // namespace compiler
+}  // namespace protobuf
+}  // namespace google
 
     
-    namespace mxnet {
-namespace op {
-template<>
-Operator *CreateOp<cpu>(NativeOpParam param) {
-  return new NativeOp<cpu>(param);
+    
+    {
+    {
+    {
+    {}  // namespace
+}  // namespace strings
+}  // namespace protobuf
+}  // namespace google
+
+    
+        for (int i = 0; i < dataset.payload_size(); i++) {
+      message->ParseFromString(dataset.payload(i));
+      GogoDataStripper stripper;
+      stripper.StripMessage(message);
+      dataset.set_payload(i, message->SerializeAsString());
+    }
+    
+    namespace {
+    }
+    
+    // Prints the fields of a tuple tersely to a string vector, one
+// element for each field.  See the comment before
+// UniversalTersePrint() for how we define 'tersely'.
+template <typename Tuple>
+Strings UniversalTersePrintTupleFieldsToStrings(const Tuple& value) {
+  Strings result;
+  TuplePrefixPrinter< ::std::tr1::tuple_size<Tuple>::value>::
+      TersePrintPrefixToStrings(value, &result);
+  return result;
 }
+#endif  // GTEST_HAS_TR1_TUPLE
+    
+      // Returns a pathname for a file that does not currently exist. The pathname
+  // will be directory/base_name.extension or
+  // directory/base_name_<number>.extension if directory/base_name.extension
+  // already exists. The number will be incremented until a pathname is found
+  // that does not already exist.
+  // Examples: 'dir/foo_test.xml' or 'dir/foo_test_1.xml'.
+  // There could be a race condition if two or more processes are calling this
+  // function at the same time -- they could both pick the same filename.
+  static FilePath GenerateUniqueFileName(const FilePath& directory,
+                                         const FilePath& base_name,
+                                         const char* extension);
+    
+    template <GTEST_TEMPLATE_ T1, GTEST_TEMPLATE_ T2, GTEST_TEMPLATE_ T3,
+    GTEST_TEMPLATE_ T4, GTEST_TEMPLATE_ T5, GTEST_TEMPLATE_ T6,
+    GTEST_TEMPLATE_ T7, GTEST_TEMPLATE_ T8, GTEST_TEMPLATE_ T9,
+    GTEST_TEMPLATE_ T10, GTEST_TEMPLATE_ T11, GTEST_TEMPLATE_ T12,
+    GTEST_TEMPLATE_ T13, GTEST_TEMPLATE_ T14, GTEST_TEMPLATE_ T15,
+    GTEST_TEMPLATE_ T16>
+struct Templates16 {
+  typedef TemplateSel<T1> Head;
+  typedef Templates15<T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+      T15, T16> Tail;
+};
+    
+    
+// Tests IsPrime()
+    
+    // A sample program demonstrating using Google C++ testing framework.
+//
+// Author: wan@google.com (Zhanyong Wan)
+    
+      s.Set(kHelloString);
+  EXPECT_EQ(0, strcmp(s.c_string(), kHelloString));
+    
+    namespace leveldb {
+namespace log {
     }
     }
     
-    /*!
- * Copyright (c) 2015 by Contributors
- * \file ndarray_op.cc
- * \brief
- * \author Junyuan Xie
-*/
-#include './ndarray_op-inl.h'
-#include <mxnet/base.h>
-#include <mxnet/ndarray.h>
+    // Store the snappy compression of 'input[0,input_length-1]' in *output.
+// Returns false if snappy is not supported by this port.
+bool Snappy_Compress(const char* input, size_t input_length,
+                     std::string* output);
     
-            Rational operator-() const;
-        Rational& operator+=(Rational const& rhs);
-        Rational& operator-=(Rational const& rhs);
-        Rational& operator*=(Rational const& rhs);
-        Rational& operator/=(Rational const& rhs);
-        Rational& operator%=(Rational const& rhs);
-    
-            static property Windows::UI::Xaml::DependencyProperty^ AnnouncementProperty
-        {
-            Windows::UI::Xaml::DependencyProperty^ get()
-            {
-                return s_announcementProperty;
-            }
-        }
-    
-            TraceActivity(
-            winrt::Windows::Foundation::Diagnostics::LoggingChannel channel,
-            std::wstring_view activityName,
-            winrt::Windows::Foundation::Diagnostics::LoggingFields fields) :
-            m_channel(channel),
-            m_activityName(activityName),
-            m_fields(fields),
-            m_activity(nullptr)
-        {
-            // Write the activity's START event. Note that you must not specify keyword
-            // or level for START and STOP events because they always use the activity's
-            // keyword and level.
-            m_activity = m_channel.StartActivity(
-                m_activityName,
-                m_fields,
-                winrt::Windows::Foundation::Diagnostics::LoggingLevel::Verbose,
-                winrt::Windows::Foundation::Diagnostics::LoggingOptions(WINEVENT_KEYWORD_RESPONSE_TIME)
-                );
-        }
-    
-        // change element at index 1 (second element) to 'second'
-    array.at(1) = 'second';
-    
-    
-    // out_of_range.106
-    try
-    {
-        // try to use an array index with leading '0'
-        json::reference ref = j.at('/array/01'_json_pointer);
+      std::string ManifestFileName() {
+    std::string current;
+    ASSERT_OK(ReadFileToString(env_, CurrentFileName(dbname_), &current));
+    size_t len = current.size();
+    if (len > 0 && current[len - 1] == '\n') {
+      current.resize(len - 1);
     }
-    catch (json::parse_error& e)
-    {
-        std::cout << e.what() << '\n';
+    return dbname_ + '/' + current;
+  }
+    
+        Key pos = RandomTarget(rnd);
+    SkipList<Key, Comparator>::Iterator iter(&list_);
+    iter.Seek(pos);
+    while (true) {
+      Key current;
+      if (!iter.Valid()) {
+        current = MakeKey(K, 0);
+      } else {
+        current = iter.key();
+        ASSERT_TRUE(IsValidKey(current)) << current;
+      }
+      ASSERT_LE(pos, current) << 'should not go backwards';
     }
     
-        // out_of_range.401
-    try
-    {
-        // try to use a an invalid array index
-        json::const_reference ref = j.at('/array/4'_json_pointer);
-    }
-    catch (json::out_of_range& e)
-    {
-        std::cout << e.what() << '\n';
-    }
+    int main(int argc, char** argv) { return leveldb::test::RunAllTests(); }
+
     
     
-    {    std::cout << null << '\n';
-    std::cout << *res2.first << ' ' << std::boolalpha << res2.second << '\n';
-}
+    {}  // namespace leveldb
