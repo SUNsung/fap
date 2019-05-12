dd21559b0900342212d515aab75c0d0f2ed604b6
SUNsung <weatherius@gmail.com>
2019-05-12 10:58:32 +0300

    Rewrite [57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb] File in: 2019-05-12 10:58:31

--- ruby/57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb
+++ ruby/57QNI.r0YnPtA574Nq7nlA4Ng57C5M7UEq3LE57tVP.rb
@@ -1,44 +1,51 @@
 
         
-              # Sets the expiration time of a key.
-      #
-      # raw_key - The key for which to change the timeout.
-      # timeout - The new timeout.
-      def self.expire(raw_key, timeout)
-        key = cache_key_for(raw_key)
+            ShareVisibility.joins('INNER JOIN photos ON photos.id = share_visibilities.shareable_id')
+                   .where(shareable_type: 'Photo', photos: {public: true}).delete_all
     
-            def sidekiq_worker_class
-          ImportDiffNoteWorker
-        end
+    class PolymorphicMentions < ActiveRecord::Migration[4.2]
+  def change
+    remove_index :mentions, column: %i(post_id)
+    remove_index :mentions, column: %i(person_id post_id), unique: true
+    rename_column :mentions, :post_id, :mentions_container_id
+    add_column :mentions, :mentions_container_type, :string
+    add_index :mentions,
+              %i(mentions_container_id mentions_container_type),
+              name:   'index_mentions_on_mc_id_and_mc_type',
+              length: {mentions_container_type: 191}
+    add_index :mentions,
+              %i(person_id mentions_container_id mentions_container_type),
+              name:   'index_mentions_on_person_and_mc_id_and_mc_type',
+              length: {mentions_container_type: 191},
+              unique: true
     
-            # attributes - A hash containing the raw issue details. The keys of this
-        #              Hash (and any nested hashes) must be symbols.
-        def initialize(attributes)
-          @attributes = attributes
-        end
+        it 'generates the aspects_manage_contacts_json fixture', fixture: true do
+      # adds one not mutual contact
+      bob.share_with(FactoryGirl.create(:person), @aspect)
     
-            # Builds a user from a GitHub API response.
-        #
-        # user - An instance of `Sawyer::Resource` containing the user details.
-        def self.from_api_response(user)
-          new(id: user.id, login: user.login)
-        end
-    
-      def update
-    if subscription.verify(body, request.headers['HTTP_X_HUB_SIGNATURE'])
-      ProcessingWorker.perform_async(@account.id, body.force_encoding('UTF-8'))
+          sign_in alice, scope: :user
     end
     
-      def update
-    raise ActiveRecord::RecordNotFound if @web_subscription.nil?
+        before do
+      @post = FactoryGirl.create(:status_message, :public => true)
+      @post_guid = @post.guid
+    end
     
-      def set_locale
-    I18n.locale = default_locale
-    I18n.locale = current_user.locale if user_signed_in?
-  rescue I18n::InvalidLocale
-    I18n.locale = default_locale
+      before do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    @user = alice
+    @user.password = 'evankorth'
+    @user.password_confirmation = 'evankorth'
+    @user.save
   end
     
-      def rate_limit_limit
-    api_throttle_data[:limit].to_s
-  end
+          it 'raises an error' do
+        expect {
+          put :update, params: {id: 42, post_id: @status.id}, format: :js
+        }.to raise_error ActiveRecord::RecordNotFound
+      end
+    
+      context 'with a logged in user' do
+    before do
+      sign_in alice
+    end
