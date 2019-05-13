57ba7bd97741efa8e1ba07e001a0bb05f512fd34
SUNsung <weatherius@gmail.com>
2019-05-13 18:00:13 +0300

    Rewrite [141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp] File in: 2019-05-13 18:00:12

--- cccccc/141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp
+++ cccccc/141ExHxAUVCQ14iZ3SOn0PREM14DCIfQPq7NBo14JM.cpp
@@ -1,228 +1,239 @@
 
         
-        // Generate param traits read methods.
-#include 'ipc/param_traits_read_macros.h'
-namespace IPC {
-#include 'content/nw/src/common/common_message_generator.h'
-}  // namespace IPC
+            accumulateSquareConstFunc funcs[16] =
+    {
+        accumulateSquareConst<0>,
+        accumulateSquareConst<1>,
+        accumulateSquareConst<2>,
+        accumulateSquareConst<3>,
+        accumulateSquareConst<4>,
+        accumulateSquareConst<5>,
+        accumulateSquareConst<6>,
+        accumulateSquareConst<7>,
+        accumulateSquareConst<8>,
+        accumulateSquareConst<9>,
+        accumulateSquareConst<10>,
+        accumulateSquareConst<11>,
+        accumulateSquareConst<12>,
+        accumulateSquareConst<13>,
+        accumulateSquareConst<14>,
+        accumulateSquareConst<15>
+    }, func = funcs[shift];
     
-    namespace base {
-class ListValue;
+    
+    {            v_y = vld2q_u8(srcy + syj + 32);
+            v_dst.val[0] = v_y.val[0];
+            v_dst.val[1] = vld1q_u8(srcu + sj + 16);
+            v_dst.val[2] = v_y.val[1];
+            v_dst.val[3] = vld1q_u8(srcv + sj + 16);
+            vst4q_u8(dst + dj + 64, v_dst);
+        }
+#endif
+    
+    inline void vnst(u8* dst, uint8x16_t v1, uint8x16_t v2) { vst1q_u8(dst, v1); vst1q_u8(dst+16, v2); }
+inline void vnst(u8* dst, uint16x8_t v1, uint16x8_t v2) { vst1q_u8(dst, vcombine_u8(vmovn_u16(v1), vmovn_u16(v2))); }
+inline void vnst(u8* dst, uint32x4_t v1, uint32x4_t v2) { vst1_u8(dst, vmovn_u16(vcombine_u16(vmovn_u32(v1), vmovn_u32(v2)))); }
+    
+    #if !defined(__aarch64__) && defined(__GNUC__) && __GNUC__ == 4 &&  __GNUC_MINOR__ < 6 && !defined(__clang__)
+CVT_FUNC(s32, u16, 8,
+,
+{
+     for (size_t i = 0; i < w; i += 8)
+     {
+         internal::prefetch(_src + i);
+         __asm__ (
+             'vld1.32 {d0-d1}, [%[src1]]                              \n\t'
+             'vld1.32 {d2-d3}, [%[src2]]                              \n\t'
+             'vqmovun.s32 d4, q0                                      \n\t'
+             'vqmovun.s32 d5, q1                                      \n\t'
+             'vst1.16 {d4-d5}, [%[dst]]                               \n\t'
+             : /*no output*/
+             : [src1] 'r' (_src + i + 0),
+               [src2] 'r' (_src + i + 4),
+               [dst] 'r' (_dst + i)
+             : 'd0','d1','d2','d3','d4','d5'
+         );
+     }
+})
+#else
+CVT_FUNC(s32, u16, 8,
+,
+{
+     for (size_t i = 0; i < w; i += 8)
+     {
+         internal::prefetch(_src + i);
+         int32x4_t vline1_s32 = vld1q_s32(_src + i);
+         int32x4_t vline2_s32 = vld1q_s32(_src + i + 4);
+    }
+    }
+    
+    
+    {    return (u8)(vget_lane_s32(q8, 0) - 1);
 }
     
-    namespace nw {
-    }
+        uint16x8_t v_maxval8 = vdupq_n_u16(maxVal),
+               v_minval8 = vdupq_n_u16(minVal);
+    u64 mask[2] = { 0ul };
     
-    #include 'base/logging.h'
-#include 'base/values.h'
-#include 'content/nw/src/api/api_messages.h'
-#include 'content/public/renderer/render_view.h'
-#include 'content/public/renderer/render_thread.h'
-#include 'content/public/renderer/v8_value_converter.h'
-#include 'third_party/WebKit/public/web/WebView.h'
-#include 'third_party/WebKit/public/web/WebLocalFrame.h'
-#include 'ui/base/resource/resource_bundle.h'
     
-    #endif  // CONTENT_NW_SRC_API_BINDINGS_COMMON_H_
+    {}  // namespace xgboost
 
     
-    #include 'base/logging.h'
-#include 'base/strings/string16.h'
-#include 'content/nw/src/api/object_manager.h'
-#include 'content/nw/src/api/menuitem/menuitem.h'
     
-    
-    {  DISALLOW_COPY_AND_ASSIGN(MenuDelegate);
-};
-    
-    namespace ui {
-    }
-    
-        if (!found) {
-      KeyMap::iterator it = keymap.find(upperText);
-      if (it != keymap.end()) {
-        keyName = it->second;
-        found = true;
+    {  /*! \brief push up temp */
+  inline void PushTemp() {
+    temp.Reserve(limit_size * 2);
+    for (size_t l = 1; true; ++l) {
+      this->InitLevel(l + 1);
+      // check if level l is empty
+      if (level[l].size == 0) {
+        level[l].SetPrune(temp, limit_size);
+        break;
+      } else {
+        // level 0 is actually temp space
+        level[0].SetPrune(temp, limit_size);
+        temp.SetCombine(level[0], level[l]);
+        if (temp.size > limit_size) {
+          // try next level
+          level[l].size = 0;
+        } else {
+          // if merged record is still smaller, no need to send to next level
+          level[l].CopyFrom(temp); break;
+        }
       }
     }
-    
-     protected:
-  ~NwClipboardClearSyncFunction() override;
-    
-     protected:
-  ~NwObjCallObjectMethodSyncFunction() override;
-    
-    /*!
- * \brief Registry entry for linear updater.
- */
-struct LinearUpdaterReg
-    : public dmlc::FunctionRegEntryBase<LinearUpdaterReg,
-                                        std::function<LinearUpdater*()> > {};
-    
-      void Write(const SparsePage& page, dmlc::Stream* fo) override {
-    const auto& offset_vec = page.offset.HostVector();
-    const auto& data_vec = page.data.HostVector();
-    CHECK(page.offset.Size() != 0 && offset_vec[0] == 0);
-    CHECK_EQ(offset_vec.back(), page.data.Size());
-    fo->Write(offset_vec);
-    if (page.data.Size() != 0) {
-      fo->Write(dmlc::BeginPtr(data_vec), page.data.Size() * sizeof(Entry));
+  }
+  /*! \brief get the summary after finalize */
+  inline void GetSummary(SummaryContainer *out) {
+    if (level.size() != 0) {
+      out->Reserve(limit_size * 2);
+    } else {
+      out->Reserve(inqueue.queue.size());
+    }
+    inqueue.MakeSummary(out);
+    if (level.size() != 0) {
+      level[0].SetPrune(*out, limit_size);
+      for (size_t l = 1; l < level.size(); ++l) {
+        if (level[l].size == 0) continue;
+        if (level[0].size == 0) {
+          level[0].CopyFrom(level[l]);
+        } else {
+          out->SetCombine(level[0], level[l]);
+          level[0].SetPrune(*out, limit_size);
+        }
+      }
+      out->CopyFrom(level[0]);
+    } else {
+      if (out->size > limit_size) {
+        temp.Reserve(limit_size);
+        temp.SetPrune(*out, limit_size);
+        out->CopyFrom(temp);
+      }
     }
   }
-    
-    TEST(c_api, XGDMatrixCreateFromMat_omp) {
-  std::vector<int> num_rows = {100, 11374, 15000};
-  for (auto row : num_rows) {
-    int num_cols = 50;
-    int num_missing = 5;
-    DMatrixHandle handle;
-    std::vector<float> data(num_cols * row, 1.5);
-    for (int i = 0; i < num_missing; i++) {
-      data[i] = std::numeric_limits<float>::quiet_NaN();
+  // used for debug, check if the sketch is valid
+  inline void CheckValid(RType eps) const {
+    for (size_t l = 1; l < level.size(); ++l) {
+      level[l].CheckValid(eps);
     }
-    }
-    }
-    
-        // add values
-    auto res1 = object.emplace('three', 3);
-    null.emplace('A', 'a');
-    null.emplace('B', 'b');
-    
-      bool writable() const { return !is_marked_read_only_; }
-    
-    class StressMarkingObserver : public AllocationObserver {
- public:
-  explicit StressMarkingObserver(Heap& heap);
-    }
-    
-      if (FLAG_fuzzer_gc_analysis) {
-    max_new_space_size_reached_ =
-        std::max(max_new_space_size_reached_, current_percent);
-    return;
   }
-    
-      void SweepOrWaitUntilSweepingCompleted(Page* page);
-    
-    void Assembler::vsd(byte op, XMMRegister dst, XMMRegister src1, Operand src2) {
-  vinstr(op, dst, src1, src2, kF2, k0F, kWIG);
-}
-    
-    namespace v8 {
-namespace internal {
+  // initialize level space to at least nlevel
+  inline void InitLevel(size_t nlevel) {
+    if (level.size() >= nlevel) return;
+    data.resize(limit_size * nlevel);
+    level.resize(nlevel, Summary(nullptr, 0));
+    for (size_t l = 0; l < level.size(); ++l) {
+      level[l].data = dmlc::BeginPtr(data) + l * limit_size;
     }
-    }
-    
-      const int kDoubleRegsSize = kDoubleSize * XMMRegister::kNumRegisters;
-  __ sub(esp, Immediate(kDoubleRegsSize));
-  const RegisterConfiguration* config = RegisterConfiguration::Default();
-  for (int i = 0; i < config->num_allocatable_double_registers(); ++i) {
-    int code = config->GetAllocatableDoubleCode(i);
-    XMMRegister xmm_reg = XMMRegister::from_code(code);
-    int offset = code * kDoubleSize;
-    __ movsd(Operand(esp, offset), xmm_reg);
   }
-    
-    
-    {  // FP-relative.
-  static constexpr int kWasmInstanceOffset = TYPED_FRAME_PUSHED_VALUE_OFFSET(0);
-  static constexpr int kFixedFrameSizeFromFp =
-      TypedFrameConstants::kFixedFrameSizeFromFp +
-      kNumberOfSavedGpParamRegs * kSystemPointerSize +
-      kNumberOfSavedFpParamRegs * kSimd128Size;
+  // input data queue
+  typename Summary::Queue inqueue;
+  // number of levels
+  size_t nlevel;
+  // size of summary in each level
+  size_t limit_size;
+  // the level of each summaries
+  std::vector<Summary> level;
+  // content of the summary
+  std::vector<Entry> data;
+  // temporal summary, used for temp-merge
+  SummaryContainer temp;
 };
     
-    // The ExtraArg1Register not part of the real JS calling convention and is
-// mostly there to simplify consistent interface descriptor definitions across
-// platforms. Note that on ia32 it aliases kJavaScriptCallCodeStartRegister.
-constexpr Register kJavaScriptCallExtraArg1Register = ecx;
+        // Remove the assertion on batch.index, which can be null in the case that the data in this
+    // batch is entirely sparse. Although it's true that this indicates a likely issue with the
+    // user's data workflows, passing XGBoost entirely sparse data should not cause it to fail.
+    // See https://github.com/dmlc/xgboost/issues/1827 for complete detail.
+    // CHECK(batch.index != nullptr);
     
-    void AccessorAssembler::HandleLoadICSmiHandlerLoadNamedCase(
-    const LoadICParameters* p, Node* holder, TNode<IntPtrT> handler_kind,
-    TNode<WordT> handler_word, Label* rebox_double, Variable* var_double_value,
-    SloppyTNode<Object> handler, Label* miss, ExitPoint* exit_point,
-    OnNonExistent on_nonexistent, ElementSupport support_elements) {
-  Label constant(this), field(this), normal(this, Label::kDeferred),
-      interceptor(this, Label::kDeferred), nonexistent(this),
-      accessor(this, Label::kDeferred), global(this, Label::kDeferred),
-      module_export(this, Label::kDeferred), proxy(this, Label::kDeferred),
-      native_data_property(this), api_getter(this);
-  GotoIf(WordEqual(handler_kind, IntPtrConstant(LoadHandler::kField)), &field);
+     public:
+  void PredictBatch(DMatrix* dmat, HostDeviceVector<bst_float>* out_preds,
+                    const gbm::GBTreeModel& model, int tree_begin,
+                    unsigned ntree_limit = 0) override {
+    if (this->PredictFromCache(dmat, out_preds, model, ntree_limit)) {
+      return;
+    }
     }
     
-    #include 'src/ic/binary-op-assembler.h'
+    // Implemented features:
+//  [X] Renderer: User texture binding. Use 'ID3D10ShaderResourceView*' as ImTextureID. Read the FAQ about ImTextureID in imgui.cpp.
     
-    	/** Return complete error message with line/column number info (if present)
-	 * @note for your own exceptions override this one. Call getMessage from
-	 * here to get the 'clean' error message stored in the text attribute.
-	 */
-	virtual ANTLR_USE_NAMESPACE(std)string toString() const
-	{
-		return text;
-	}
+    // Build with, e.g:
+//   # cl.exe binary_to_compressed_c.cpp
+//   # gcc binary_to_compressed_c.cpp
+// You can also find a precompiled Windows binary in the binary/demo package available from https://github.com/ocornut/imgui
     
-    #endif //INC_ASTPair_hpp__
-
     
-    template<class T>
-	class ANTLR_API ASTRefCount
+    {    ImGuiIO& io = ImGui::GetIO();
+    if (g_pVertexShaderBlob) { g_pVertexShaderBlob->Release(); g_pVertexShaderBlob = NULL; }
+    if (g_pPixelShaderBlob) { g_pPixelShaderBlob->Release(); g_pPixelShaderBlob = NULL; }
+    if (g_pRootSignature) { g_pRootSignature->Release(); g_pRootSignature = NULL; }
+    if (g_pPipelineState) { g_pPipelineState->Release(); g_pPipelineState = NULL; }
+    if (g_pFontTextureResource) { g_pFontTextureResource->Release(); g_pFontTextureResource = NULL; io.Fonts->TexID = NULL; } // We copied g_pFontTextureView to io.Fonts->TexID so let's clear that as well.
+    for (UINT i = 0; i < g_numFramesInFlight; i++)
+    {
+        FrameResources* fr = &g_pFrameResources[i];
+        if (fr->IndexBuffer)  { fr->IndexBuffer->Release();  fr->IndexBuffer = NULL; }
+        if (fr->VertexBuffer) { fr->VertexBuffer->Release(); fr->VertexBuffer = NULL; }
+    }
+}
+    
+    // **DO NOT USE THIS CODE IF YOUR CODE/ENGINE IS USING MODERN OPENGL (SHADERS, VBO, VAO, etc.)**
+// **Prefer using the code in imgui_impl_opengl3.cpp**
+// This code is mostly provided as a reference to learn how ImGui integration works, because it is shorter to read.
+// If your code is using GL3+ context or any semi modern OpenGL calls, using this is likely to make everything more
+// complicated, will require your code to reset every single OpenGL attributes to their initial state, and might
+// confuse your GPU driver.
+// The GL2 code is unable to reset attributes or even call e.g. 'glUseProgram(0)' because they don't exist in that API.
+    
+    #define WRITE_CHECK(fp, ptr, count)                                            \
+  if (fp.write((ptr), (count)) != (count)) {                                   \
+    throw DL_ABORT_EX(                                                         \
+        fmt('Failed to save DHT routing table to %s.', filename.c_str()));     \
+  }
+    
+    
+    {  virtual void
+  addImmediateTask(const std::shared_ptr<DHTTask>& task) CXX11_OVERRIDE;
+};
+    
+    void DHTTokenUpdateCommand::process()
 {
-private:
-	ASTRef* ref;
-    }
+  try {
+    tokenTracker_->updateTokenSecret();
+  }
+  catch (RecoverableException& e) {
+    A2_LOG_ERROR_EX(EX_EXCEPTION_CAUGHT, e);
+  }
+}
     
-    	/// Return the class name
-	virtual const char* typeName( void ) const = 0;
+      static const std::string E;
     
-    #ifdef ANTLR_CXX_SUPPORTS_NAMESPACE
-namespace antlr {
-#endif
-    }
+      void put(const std::string& hostname, const std::string& ipaddr,
+           uint16_t port);
     
-    /** CharInputBuffer.hpp provides an InputBuffer for plain character arrays (buffers).
- */
-class CharInputBuffer : public InputBuffer
-{
-public:
-	/** Construct a CharInputBuffer.hpp object with a char* buffer of 'size'
-	 * if 'owner' is true, then the buffer will be delete[]-ed on destruction.
-	 * @note it is assumed the buffer was allocated with new[]!
-	 */
-	CharInputBuffer( unsigned char* buf, size_t size, bool owner = false )
-	: buffer(buf)
-	, ptr(buf)
-	, end(buf + size)
-	, delete_buffer(owner)
-	{
-	}
-    }
+    using json = nlohmann::json;
     
-    
-    {		if (m_offset >= OFFSET_MAX_RESIZE)
-		{
-			storage.erase( storage.begin(), storage.begin() + m_offset + nb );
-			m_offset = 0;
-		}
-		else
-			m_offset += nb;
-	}
-	inline void append(const T& t)
-	{
-		storage.push_back(t);
-	}
-	inline size_t entries() const
-	{
-		return storage.size() - m_offset;
-	}
-    
-    #ifdef ANTLR_CXX_SUPPORTS_NAMESPACE
-namespace antlr {
-#endif
-    }
-    
-    public:
-	/// The token that was encountered
-	const RefToken token;
-	/// The offending AST node if tree walking
-	const RefAST node;
-	/// taken from node or token object
-	ANTLR_USE_NAMESPACE(std)string tokenText;
+        // print values
+    std::cout << object << '\n';
+    std::cout << null << '\n';
