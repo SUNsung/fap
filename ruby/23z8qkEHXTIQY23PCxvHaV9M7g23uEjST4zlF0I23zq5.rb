80bbba0115d0831827ea3c24d382ef806949a6e2
SUNsung <weatherius@gmail.com>
2019-05-13 15:55:30 +0300

    Rewrite [23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb] File in: 2019-05-13 15:55:29

--- ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
+++ ruby/23z8qkEHXTIQY23PCxvHaV9M7g23uEjST4zlF0I23zq5.rb
@@ -1,108 +1,174 @@
 
         
-        CONTENT_CONTAINING = <<-HTML.freeze
-<!DOCTYPE HTML>
-<html lang='en-US'>
-  <head>
-<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
-    <meta charset='UTF-8'>
-    <title>Jemoji</title>
-    <meta name='viewport' content='width=device-width,initial-scale=1'>
-    <link rel='stylesheet' href='/css/screen.css'>
-  </head>
-  <body class='wrap'>
-    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
+            headers
+  end
+end
     
-            def log_error(error)
-          Jekyll.logger.error 'LiveReload experienced an error. ' \
-            'Run with --trace for more information.'
-          raise error
+      def self.headings
+    @headings ||= [:likes_received,
+                   :likes_given,
+                   :topics_entered,
+                   :topic_count,
+                   :post_count,
+                   :posts_read,
+                   :days_visited]
+  end
+    
+    class Draft < ActiveRecord::Base
+  NEW_TOPIC ||= 'new_topic'
+  NEW_PRIVATE_MESSAGE ||= 'new_private_message'
+  EXISTING_TOPIC ||= 'topic_'
+    
+      def build(theme_ids)
+    builder = Builder.new
+    
+        def site_setting_extension
+      { script_src: SiteSetting.content_security_policy_script_src.split('|') }
+    end
+    
+          response
+    end
+    
+        plugin.enabled = false
+    expect(parse(policy)['script-src']).to_not include('https://from-plugin.com')
+    
+              [serialized_poll, options]
         end
       end
-    end
-  end
-end
-
     
-          def grouped_array(groups)
-        groups.each_with_object([]) do |item, array|
-          array << {
-            'name'  => item.first,
-            'items' => item.last,
-            'size'  => item.last.size,
-          }
+    describe ApplicationHelper do
+  describe '#icon_tag' do
+    it 'returns a Glyphicon icon element' do
+      icon = icon_tag('glyphicon-help')
+      expect(icon).to be_html_safe
+      expect(Nokogiri(icon).at('span.glyphicon.glyphicon-help')).to be_a Nokogiri::XML::Element
+    end
+    
+          it 'generates a DOT script' do
+        expect(agents_dot(@agents)).to match(%r{
+          \A
+          digraph \x20 'Agent \x20 Event \x20 Flow' \{
+            node \[ [^\]]+ \];
+            edge \[ [^\]]+ \];
+            (?<foo>\w+) \[label=foo\];
+            \k<foo> -> (?<bar1>\w+) \[style=dashed\];
+            \k<foo> -> (?<bar2>\w+) \[color='\#999999'\];
+            \k<bar1> \[label=bar1\];
+            \k<bar2> \[label=bar2,style='rounded,dashed',color='\#999999',fontcolor='\#999999'\];
+            \k<bar2> -> (?<bar3>\w+) \[style=dashed,color='\#999999'\];
+            \k<bar3> \[label=bar3\];
+          \}
+          \z
+        }x)
+      end
+    
+              expect(weather_agent.name).to eq('a weather agent')
+          expect(weather_agent.schedule).to eq('5pm')
+          expect(weather_agent.keep_events_for).to eq(14.days)
+          expect(weather_agent.propagate_immediately).to be_falsey
+          expect(weather_agent).to be_disabled
+          expect(weather_agent.memory).to be_empty
+          expect(weather_agent.options).to eq(weather_agent_options)
+    
+          @log.level = -1
+      expect(@log).not_to be_valid
+      expect(@log).to have(1).error_on(:level)
+    
+      describe '#receive' do
+    it 'sends a message' do
+      stub(HTTParty).post { {'id' => 1, 'message' => 'blah', 'title' => 'blah','source_name' => 'Custom Notification'} }
+      @checker.receive([@event])
+    end
+    
+            # This returns all the registered configuration classes that were
+        # marked as 'upgrade safe.'
+        #
+        # @return [Hash]
+        def config_upgrade_safe
+          result = {}
+    
+            # This method is expected to return a class that is used for
+        # configuring the provisioner. This return value is expected to be
+        # a subclass of {Config}.
+        #
+        # @return [Config]
+        def self.config_class
         end
-      end
-    end
-  end
+    
+              # Add the help option, which must be on every command.
+          opts.on_tail('-h', '--help', 'Print this help') do
+            safe_puts(opts.help)
+            return nil
+          end
+    
+            # This returns all the config classes for the various providers.
+        #
+        # @return [Hash]
+        def provider_configs
+          Registry.new.tap do |result|
+            @registered.each do |plugin|
+              result.merge!(plugin.components.configs[:provider])
+            end
+          end
+        end
+    
+            # This returns the manager for all V2 plugins.
+        #
+        # @return [V2::Manager]
+        def self.manager
+          @manager ||= Manager.new
+        end
+    
+    answer = to_array(solution).map do |p|
+  to_string(p)
+end
+    
+      # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = ::Logger::Formatter.new
 end
 
     
-      private
+    class NodeMincerTest < Minitest::Test
+  DUMMY_PATH = 'test/dummy_node_mincer'
     
-      included do
-    before_action :set_locale
-  end
+        desc 'Commits the version to github repository'
+    task :commit_version do
+      sh <<-SH
+        sed -i 's/.*VERSION.*/  VERSION = '#{source_version}'/' lib/sinatra/version.rb
+        sed -i 's/.*VERSION.*/    VERSION = '#{source_version}'/' sinatra-contrib/lib/sinatra/contrib/version.rb
+        sed -i 's/.*VERSION.*/    VERSION = '#{source_version}'/' rack-protection/lib/rack/protection/version.rb
+      SH
     
-      gem.add_development_dependency 'danger'
-  gem.add_development_dependency 'mocha'
-  gem.add_development_dependency 'rspec'
-  gem.add_development_dependency 'rubocop', '0.48.1'
-end
-
-    
-    module Capistrano
-  class Configuration
-    # Decorates a Variables object to additionally perform an optional set of
-    # user-supplied validation rules. Each rule for a given key is invoked
-    # immediately whenever `set` is called with a value for that key.
-    #
-    # If `set` is called with a callable value or a block, validation is not
-    # performed immediately. Instead, the validation rules are invoked the first
-    # time `fetch` is used to access the value.
-    #
-    # A rule is simply a block that accepts two arguments: key and value. It is
-    # up to the rule to raise an exception when it deems the value is invalid
-    # (or just print a warning).
-    #
-    # Rules can be registered using the DSL like this:
-    #
-    #   validate(:my_key) do |key, value|
-    #     # rule goes here
-    #   end
-    #
-    class ValidatedVariables < SimpleDelegator
-      include Capistrano::ProcHelpers
-    
-          def trusted?
-        @trusted
+          # Creates a masked version of the authenticity token that varies
+      # on each request. The masking is used to mitigate SSL attacks
+      # like BREACH.
+      def mask_token(token)
+        token = decode_token(token)
+        one_time_pad = SecureRandom.random_bytes(token.length)
+        encrypted_token = xor_byte_strings(one_time_pad, token)
+        masked_token = one_time_pad + encrypted_token
+        encode_token(masked_token)
       end
     
-            def update
-          authorize! :update, @product
+    module Rack
+  module Protection
+    ##
+    # Prevented attack::   XSS
+    # Supported browsers:: all
+    # More infos::         http://en.wikipedia.org/wiki/Cross-site_scripting
+    #
+    # Automatically escapes Rack::Request#params so they can be embedded in HTML
+    # or JavaScript without any further issues. Calls +html_safe+ on the escaped
+    # strings if defined, to avoid double-escaping in Rails.
+    #
+    # Options:
+    # escape:: What escaping modes to use, should be Symbol or Array of Symbols.
+    #          Available: :html (default), :javascript, :url
+    class EscapedParams < Base
+      extend Rack::Utils
     
-          @@inventory_unit_attributes = [
-        :id, :lock_version, :state, :variant_id, :shipment_id,
-        :return_authorization_id
-      ]
-    
-      # Array of configuration files
-  attr_accessor :config_files
-    
-        # Execute the transmogrification on the manifest
-    pkg_mogrify = safesystemout('pkgmogrify', manifest_fn)
-    File.write(build_path('#{name}.p5m.2'), pkg_mogrify)
-    safesystem('cp', build_path('#{name}.p5m.2'), manifest_fn)
-    
-        File.write(build_path('packlist'), files.sort.join('\n'))
-    
-      def default_output
-    v = version
-    v = '#{epoch}:#{v}' if epoch
-    if iteration
-      '#{name}_#{v}-#{iteration}_#{architecture}.#{type}'
-    else
-      '#{name}_#{v}_#{architecture}.#{type}'
+      it 'allows for a custom authenticity token param' do
+    mock_app do
+      use Rack::Protection::AuthenticityToken, :authenticity_param => 'csrf_param'
+      run proc { |e| [200, {'Content-Type' => 'text/plain'}, ['hi']] }
     end
-  end # def default_output
-end # class FPM::Deb
