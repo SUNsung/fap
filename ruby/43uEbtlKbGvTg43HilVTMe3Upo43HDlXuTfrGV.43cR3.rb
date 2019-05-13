1986eaedd6f1d0acd67fb0155350090033591a3f
SUNsung <weatherius@gmail.com>
2019-05-13 22:56:10 +0300

    Rewrite [43uEbtlKbGvTg43HilVTMe3Upo43HDlXuTfrGV.43cR3.rb] File in: 2019-05-13 22:56:10

--- ruby/43uEbtlKbGvTg43HilVTMe3Upo43HDlXuTfrGV.43cR3.rb
+++ ruby/43uEbtlKbGvTg43HilVTMe3Upo43HDlXuTfrGV.43cR3.rb
@@ -1,49 +1,92 @@
-              yield item, value, text, default_html_options.merge(additional_html_options)
-            end.join.html_safe
-          end
+
+        
+              expect(builder.build).to eq(previous)
+    end
+  end
     
-    require 'action_view/helpers/tags/placeholderable'
+    class ContentSecurityPolicy
+  class Builder
+    EXTENDABLE_DIRECTIVES = %i[
+      base_uri
+      object_src
+      script_src
+      worker_src
+    ].freeze
     
-              redirect_to admin_reports_path, notice: I18n.t('admin.reports.resolved_msg')
-          return
-        end
+          theme_ids = env[:resolved_theme_ids]
+      headers['Content-Security-Policy'] = policy(theme_ids) if SiteSetting.content_security_policy
+      headers['Content-Security-Policy-Report-Only'] = policy(theme_ids) if SiteSetting.content_security_policy_report_only
     
-    module ExportControllerConcern
-  extend ActiveSupport::Concern
-    
-        context 'single argument' do
-      before do
-        subject.instance_eval do
-          env :userpaths
+            if is_navigational_format?
+          session.delete(session_key)
+        else
+          session[session_key]
         end
       end
     
-      def send_sinatra_file(path, &missing_file_block)
-    file_path = File.join(File.dirname(__FILE__), 'public',  path)
-    file_path = File.join(file_path, 'index.html') unless file_path =~ /\.[a-z]+$/i
-    File.exist?(file_path) ? send_file(file_path) : missing_file_block.call
+        if record.timedout?(last_request_at) &&
+        !env['devise.skip_timeout'] &&
+        !proxy.remember_me_is_active?(record)
+      Devise.sign_out_all_scopes ? proxy.sign_out : proxy.sign_out(scope)
+      throw :warden, scope: scope, message: :timeout
+    end
+    
+            # The token is only valid if:
+        # 1. we have a date
+        # 2. the current time does not pass the expiry period
+        # 3. the record has a remember_created_at date
+        # 4. the token date is bigger than the remember_created_at
+        # 5. the token matches
+        generated_at.is_a?(Time) &&
+         (self.class.remember_for.ago < generated_at) &&
+         (generated_at > (remember_created_at || Time.now).utc) &&
+         Devise.secure_compare(rememberable_value, token)
+      end
+    
+      GEMS_AND_ROOT_DIRECTORIES.each do |gem, directory|
+    file package(gem, '.gem') => ['pkg/', '#{directory + '/' + gem}.gemspec'] do |f|
+      sh 'cd #{directory} && gem build #{gem}.gemspec'
+      mv directory + '/' + File.basename(f.name), f.name
+    end
+    
+    @@ layout
+<html>
+  <head>
+    <title>Super Simple Chat with Sinatra</title>
+    <meta charset='utf-8' />
+    <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
+  </head>
+  <body><%= yield %></body>
+</html>
+    
+          def masked_token?(token)
+        token.length == TOKEN_LENGTH * 2
+      end
+    
+        headers = get('/', {}, 'wants' => 'text/html').headers
+    expect(headers['Content-Security-Policy']).to eq('block-all_mixed_content; connect-src 'self'; default-src none; disown-opener; img-src 'self'; script-src 'self'; style-src 'self'; upgrade-insecure_requests')
   end
     
-          rtn = ''
-      (context.environments.first['site'][@array_name] || []).each do |file|
-        if file !~ /^[a-zA-Z0-9_\/\.-]+$/ || file =~ /\.\// || file =~ /\/\./
-          rtn = rtn + 'Include file '#{file}' contains invalid characters or sequences'
-        end
+    if (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  puts '## Set the codepage to 65001 for Windows machines'
+  `chcp 65001`
+end
     
-    module Jekyll
+      def render(context)
+    config_tag(context.registers[:site].config, @key, @tag, @classname)
+  end
+end
     
-    # The base module that gets included in ActiveRecord::Base. See the
-# documentation for Paperclip::ClassMethods for more useful information.
-module Paperclip
-  extend Helpers
-  extend Logger
-  extend ProcessorHelpers
+        def get_gist_from_web(gist, file)
+      gist_url = get_gist_url_for(gist, file)
+      data     = get_web_content(gist_url)
     
-        # Returns the interpolated URL. Will raise an error if the url itself
-    # contains ':url' to prevent infinite recursion. This interpolation
-    # is used in the default :path to ease default specifications.
-    RIGHT_HERE = '#{__FILE__.gsub(%r{\A\./}, '')}:#{__LINE__ + 3}'
-    def url attachment, style_name
-      raise Errors::InfiniteInterpolationError if caller.any?{|b| b.index(RIGHT_HERE) }
-      attachment.url(style_name, :timestamp => false, :escape => false)
+        def render(context)
+      if @img
+        '<img #{@img.collect {|k,v| '#{k}=\'#{v}\'' if v}.join(' ')}>'
+      else
+        'Error processing input, expected syntax: {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | \'title text\' [\'alt text\']] %}'
+      end
     end
+  end
+end
