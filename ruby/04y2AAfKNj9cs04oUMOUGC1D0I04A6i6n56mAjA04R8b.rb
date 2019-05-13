98aec1672827eb54cad6993f075f9f1b7320032c
SUNsung <weatherius@gmail.com>
2019-05-13 23:16:53 +0300

    Rewrite [04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb] File in: 2019-05-13 23:16:53

--- ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
+++ ruby/04y2AAfKNj9cs04oUMOUGC1D0I04A6i6n56mAjA04R8b.rb
@@ -1,113 +1,95 @@
 
         
-        describe 'Creating a new agent', js: true do
-  before(:each) do
-    login_as(users(:bob))
-  end
+                # This method will split the argv given into three parts: the
+        # flags to this command, the subcommand, and the flags to the
+        # subcommand. For example:
+        #
+        #     -v status -h -v
+        #
+        # The above would yield 3 parts:
+        #
+        #     ['-v']
+        #     'status'
+        #     ['-h', '-v']
+        #
+        # These parts are useful because the first is a list of arguments
+        # given to the current command, the second is a subcommand, and the
+        # third are the commands given to the subcommand.
+        #
+        # @return [Array] The three parts.
+        def split_main_and_subcommand(argv)
+          # Initialize return variables
+          main_args   = nil
+          sub_command = nil
+          sub_args    = []
     
-        it 'respects an environment variable that specifies a path or URL to a different scenario' do
-      stub.proxy(ENV).[](anything)
-      stub(ENV).[]('DEFAULT_SCENARIO_FILE') { File.join(Rails.root, 'spec', 'fixtures', 'test_default_scenario.json') }
-      expect { DefaultScenarioImporter.seed(user) }.to change(user.agents, :count).by(3)
-    end
-    
-      before(:each) do
-    stub_request(:get, /events.json$/).to_return(
-      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    stub_request(:get, /projects.json$/).to_return(
-      :body => JSON.dump([{name: 'test', id: 1234},{name: 'test1', id: 1235}]),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    stub_request(:get, /02:00$/).to_return(
-      :body => File.read(Rails.root.join('spec/data_fixtures/basecamp.json')),
-      :status => 200,
-      :headers => {'Content-Type' => 'text/json'}
-    )
-    @valid_params = { :project_id => 6789 }
-    
-    module Admin
-  class ChangeEmailsController < BaseController
-    before_action :set_account
-    before_action :require_local_account!
-    
-      before_action :set_account
-  respond_to :txt
-    
-    
-end
-end
-end
-
-    
-        head + [data.length].pack('v') + data
-  end
-    
-    module Rex
-  module Proto
-    module Kerberos
-      module Model
-        # This class provides a representation of a principal, an asset (e.g., a
-        # workstation user or a network server) on a network.
-        class Element
-    
-              # Decodes the crealm field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [String]
-          def decode_crealm(input)
-            input.value[0].value
-          end
-    
-              # Decodes the cusec field
-          #
-          # @param input [OpenSSL::ASN1::ASN1Data] the input to decode from
-          # @return [Integer]
-          def decode_cusec(input)
-            input.value[0].value
-          end
-    
-      until to_boolean(IS_EMPTY[proc])
-    array.push(FIRST[proc])
-    proc = REST[proc]
-  end
-    
-    $pnum=[]
-def setpiece(a,pos)
-  if a.length == $p.length then
-    $no += 1
-    pboard
-    return
-  end
-  while $b[pos] != -1
-    pos += 1
-  end
-  ($pnum - a).each do |i|
-    $p[i].each do |x|
-      f = 0
-      x.each{|s|
-        if $b[pos+s] != -1
-          f=1
-          break
+            def initialize(argv, env)
+          @argv = argv
+          @env  = env
+          @logger = Log4r::Logger.new('vagrant::command::#{self.class.to_s.downcase}')
         end
-      }
-      if f == 0 then
-        x.each{|s|
-          $b[pos+s] = i
-        }
-        a << i
-        setpiece(a.dup, pos)
-        a.pop
-        x.each{|s|
-          $b[pos+s] = -1
-        }
-      end
+    
+            # Capture all bad configuration calls and save them for an error
+        # message later during validation.
+        def method_missing(name, *args, &block)
+          return super if @__finalized
+    
+        def URIAddEncodedOctetToBuffer(octet, result, index)
+      result[index] = 37; # Char code of '%'.
+      index         += 1
+      result[index] = @@hexCharCodeArray[octet >> 4];
+      index         += 1
+      result[index] = @@hexCharCodeArray[octet & 0x0F];
+      index += 1
+      return index;
     end
-  end
+    
+    def normal(text)
+  text.gsub!(' ', '')
+  text.gsub!('\n', '')
+  text
 end
     
-      # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
+        # Test page_header_from_content(@content)
+    actual = @view.title
+    assert_equal '1 & 2', actual
+  end
+    
+        def not_found(msg = nil)
+      @message = msg || 'The requested page does not exist.'
+      status 404
+      return mustache :error
+    end
+    
+          # renders hidden field and link to remove record using nested_attributes
+      def link_to_icon_remove_fields(form)
+        url = form.object.persisted? ? [:admin, form.object] : '#'
+        link_to_with_icon('delete', '', url,
+                          class: 'spree_remove_fields btn btn-sm btn-danger',
+                          data: {
+                            action: 'remove'
+                          },
+                          title: Spree.t(:remove),
+                          no_text: true
+                         ) + form.hidden_field(:_destroy)
+      end
+    
+          before do
+        product.master.stock_items.first.update_column(:backorderable, true)
+        product.master.stock_items.first.update_column(:count_on_hand, 100)
+        product.master.stock_items.last.update_column(:count_on_hand, 100)
+      end
+    
+              def render_order(result)
+            if result.success?
+              render_serialized_payload { serialized_current_order }
+            else
+              render_error_payload(result.error)
+            end
+          end
+    
+            def find_line_item
+          id = params[:id].to_i
+          order.line_items.detect { |line_item| line_item.id == id } or
+            raise ActiveRecord::RecordNotFound
+        end
