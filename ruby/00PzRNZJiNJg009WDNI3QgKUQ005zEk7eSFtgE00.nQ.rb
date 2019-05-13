00fef7dedd7f8f01efb1c760374c244ec6fb6afb
SUNsung <weatherius@gmail.com>
2019-05-13 16:47:01 +0300

    Rewrite [00PzRNZJiNJg009WDNI3QgKUQ005zEk7eSFtgE00.nQ.rb] File in: 2019-05-13 16:47:00

--- ruby/00PzRNZJiNJg009WDNI3QgKUQ005zEk7eSFtgE00.nQ.rb
+++ ruby/00PzRNZJiNJg009WDNI3QgKUQ005zEk7eSFtgE00.nQ.rb
@@ -1,31 +1,61 @@
-    it 'returns a label 'Yes' if a given agent is working' do
-      stub(@agent).working? { true }
-      label = working(@agent)
-      expect(label).to be_html_safe
-      expect(Nokogiri(label).text).to eq 'Yes'
-    end
+
+        
+          </body>
+</html>
+HTML
+CONTENT_NOT_CONTAINING = <<-HTML.freeze
+<!DOCTYPE HTML>
+<html lang='en-US'>
+  <head>
+<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+    <meta charset='UTF-8'>
+    <title>Jemoji</title>
+    <meta name='viewport' content='width=device-width,initial-scale=1'>
+    <link rel='stylesheet' href='/css/screen.css'>
+  </head>
+  <body class='wrap'>
+    <p><img class='emoji' title=':+1:' alt=':+1:' src='https://assets.github.com/images/icons/emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle'></p>
     
-      describe '#scenario_label' do
-    it 'creates a scenario label with the scenario name' do
-      expect(scenario_label(scenario)).to eq(
-        '<span class='label scenario' style='color:#AAAAAA;background-color:#000000'>Scene</span>'
-      )
-    end
+              new_theme_name = args.join('_')
+          theme = Jekyll::ThemeBuilder.new(new_theme_name, opts)
+          Jekyll.logger.abort_with 'Conflict:', '#{theme.path} already exists.' if theme.path.exist?
     
-      describe 'migrating the 'make_message' format' do
-    it 'should work' do
-      expect(LiquidMigrator.convert_make_message('<message>')).to eq('{{message}}')
-      expect(LiquidMigrator.convert_make_message('<new.message>')).to eq('{{new.message}}')
-      expect(LiquidMigrator.convert_make_message('Hello <world>. How is <nested.life>')).to eq('Hello {{world}}. How is {{nested.life}}')
+            def handle_websockets_event(websocket)
+          websocket.onopen { |handshake| connect(websocket, handshake) }
+          websocket.onclose { disconnect(websocket) }
+          websocket.onmessage { |msg| print_message(msg) }
+          websocket.onerror { |error| log_error(error) }
+        end
+    
+          private
+    
+          it 'should not be catched when it is unexpected' do
+        @target = alice.post :status_message, text: 'AWESOME', to: @alices_aspect.id
+        allow(alice).to receive(:like!).and_raise('something')
+        allow(@controller).to receive(:current_user).and_return(alice)
+        expect { post :create, params: like_hash, format: :json }.to raise_error('something')
+      end
     end
   end
     
-        it 'should generate the correct events url' do
-      expect(@checker.send(:event_url)).to eq('https://api.aftership.com/v4/trackings')
-    end
+        context 'on my own post' do
+      before do
+        aspect_to_post = alice.aspects.where(:name => 'generic').first
+        @post = alice.post :status_message, :text => 'something', :to => aspect_to_post
+      end
     
-    require 'bundler/cli'
-require 'bundler/friendly_errors'
+      describe '#index' do
+    context 'with a private post' do
+      before do
+        @alices_aspect = alice.aspects.where(name: 'generic').first
+        @post = alice.post(:status_message, text: 'hey', to: @alices_aspect.id)
+      end
     
-      def execute
-    signal_deprecation_warning_for_pack
+    Then(/^the current symlink points to that specific release$/) do
+  specific_release_path = TestApp.releases_path.join(@rollback_release)
+    
+          def roles_for(names)
+        options = extract_options(names)
+        s = Filter.new(:role, names).filter(servers_by_key.values)
+        s.select { |server| server.select?(options) }
+      end
