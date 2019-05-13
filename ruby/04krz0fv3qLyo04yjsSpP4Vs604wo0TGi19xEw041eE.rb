829bee931d2c9d010cdadcb72e51f4deab90c375
SUNsung <weatherius@gmail.com>
2019-05-13 17:11:06 +0300

    Rewrite [04krz0fv3qLyo04yjsSpP4Vs604wo0TGi19xEw041eE.rb] File in: 2019-05-13 17:11:05

--- ruby/04krz0fv3qLyo04yjsSpP4Vs604wo0TGi19xEw041eE.rb
+++ ruby/04krz0fv3qLyo04yjsSpP4Vs604wo0TGi19xEw041eE.rb
@@ -1,73 +1,73 @@
-  # GET /resource/unlock/new
-  def new
-    self.resource = resource_class.new
-  end
+
+        
+          def create
+    raise Discourse::NotFound unless report_collection_enabled?
     
-    module Devise
-  module Controllers
-    # A module that may be optionally included in a controller in order
-    # to provide remember me behavior. Useful when signing in is done
-    # through a callback, like in OmniAuth.
-    module Rememberable
-      # Return default cookie values retrieved from session options.
-      def self.cookie_values
-        Rails.configuration.session_options.slice(:path, :domain, :secure)
-      end
-    
-    module Devise
-  module Controllers
-    # Provide sign in and sign out functionality.
-    # Included by default in all controllers.
-    module SignInOut
-      # Return true if the given scope is signed in session. If no scope given, return
-      # true if any scope is signed in. This will run authentication hooks, which may
-      # cause exceptions to be thrown from this method; if you simply want to check
-      # if a scope has already previously been authenticated without running
-      # authentication hooks, you can directly call `warden.authenticated?(scope: scope)`
-      def signed_in?(scope=nil)
-        [scope || Devise.mappings.keys].flatten.any? do |_scope|
-          warden.authenticate?(scope: _scope)
-        end
-      end
-    
-        def fullpath
-      '/#{@path_prefix}/#{@path}'.squeeze('/')
+          DB.exec(<<~SQL, id: d.id, sequence: sequence, data: data)
+        UPDATE drafts
+           SET sequence = :sequence
+             , data = :data
+             , revisions = revisions + 1
+         WHERE id = :id
+      SQL
+    else
+      Draft.create!(user_id: user.id, draft_key: key, data: data, sequence: sequence)
     end
     
-          included do
-        before_update :clear_reset_password_token, if: :clear_reset_password_token?
-      end
-    
-          def self.required_fields(klass)
-        [:remember_created_at]
-      end
-    
-        def URIEncodeSingle(cc, result, index)
-      x = (cc >> 12) & 0xF;
-      y = (cc >> 6) & 63;
-      z = cc & 63;
-      octets = Array.new(3);
-      if (cc <= 0x007F)
-        octets[0] = cc;
-      elsif (cc <= 0x07FF)
-        octets[0] = y + 192;
-        octets[1] = z + 128;
-      else
-        octets[0] = x + 224;
-        octets[1] = y + 128;
-        octets[2] = z + 128;
-      end
-      return URIEncodeOctets(octets, result, index);
+        def extendable?(directive)
+      EXTENDABLE_DIRECTIVES.include?(directive)
     end
     
-          def title
-        @page.title
-      end
+        def cache
+      @cache ||= DistributedCache.new('csp_extensions')
+    end
     
-          def bar_side
-        @bar_side.to_s
-      end
+        def theme_policy
+      policy([theme.id])
+    end
     
-      test 'clean path with leading slash' do
-    assert_equal '/Mordor', clean_path('/Mordor')
+    require_relative 'bundler'
+require_relative 'rubygems'
+require 'pathname'
+    
+      let(:unordered_config_parts) { ordered_config_parts.shuffle }
+  let(:settings) { LogStash::SETTINGS }
+    
+    platforms = PlatformConfig.new
+    
+    shared_examples 'logstash update' do |logstash|
+  describe 'logstash-plugin update on #{logstash.hostname}' do
+    before :each do
+      logstash.install({:version => LOGSTASH_VERSION})
+    end
+    
+          Array(panes_yml).map.with_index do |pane_yml, index|
+        commands =  case pane_yml
+                    when Hash
+                      pane_yml.values.first
+                    when Array
+                      pane_yml
+                    else
+                      pane_yml
+                    end
+    
+      it 'creates an instance' do
+    expect(subject).to be_a(Tmuxinator::Pane)
   end
+    
+      def is_pane
+    @actual.is_a? Tmuxinator::Pane
+  end
+end
+
+    
+          new_exists = Tmuxinator::Config.exists?(name: new)
+      question = '#{new} already exists, would you like to overwrite it?'
+      if !new_exists || yes?(question, :red)
+        say 'Overwriting #{new}' if Tmuxinator::Config.exists?(name: new)
+        FileUtils.copy_file(existing_config_path, new_config_path)
+      end
+    
+      describe '#render' do
+    it 'renders the template' do
+      expect(File).to receive(:read).at_least(:once) { 'wemux ls 2>/dev/null' }
