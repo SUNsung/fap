756a0cbe7bfee17b5aedb01f2d01a7bf0eac7f63
SUNsung <weatherius@gmail.com>
2019-05-13 20:58:39 +0300

    Rewrite [54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb] File in: 2019-05-13 20:58:39

--- ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
+++ ruby/54XVkSR74MtXU547C0sWa6JO3w54RqW3SlrQ7ck5465p.rb
@@ -1,47 +1,39 @@
 
         
-            # [Boolean] Set if the variable is to be converted to a shell-escaped String when provided as a Hash or Array
-    # Allows items expected to be strings used in shell arguments to be alternatively provided as a Hash or Array for better readability and auto-escaped for us.
-    attr_accessor :allow_shell_conversion
+              delegate_to Type
     
-              it 'asks if no other option' do
-            allow(FastlaneCore::Helper).to receive(:test?).and_return(false)
-            allow(FastlaneCore::UI).to receive(:interactive?).and_return(true)
-            allow(FastlaneCore::Helper).to receive(:ci?).and_return(false)
-    
-            self
+          test 'when layout is specified as false, do not use a layout' do
+        controller = WithFalseLayout.new
+        controller.process(:index)
+        assert_equal 'Hello false!', controller.response_body
       end
     
-          # Checks whether this node body is a void context.
-      # Always `true` for `for`.
-      #
-      # @return [true] whether the `for` node body is a void context
-      def void_context?
-        true
-      end
+          def part_of_ignored_node?(node)
+        ignored_nodes.map(&:loc).any? do |ignored_loc|
+          if ignored_loc.expression.begin_pos > node.source_range.begin_pos
+            next false
+          end
     
-          # Custom destructuring method. This is used to normalize the branches
-      # for `pair` and `kwsplat` nodes, to add duck typing to `hash` elements.
-      #
-      # @return [Array<KeywordSplatNode>] the different parts of the `kwsplat`
-      def node_parts
-        [self, self]
-      end
+            def message_for_normal_block(variable, all_arguments)
+          if all_arguments.none?(&:referenced?) &&
+             !define_method_call?(variable)
+            if all_arguments.count > 1
+              'You can omit all the arguments if you don't care about them.'
+            else
+              'You can omit the argument if you don't care about it.'
+            end
+          else
+            message_for_underscore_prefix(variable)
+          end
+        end
+    
+          attr_reader :page, :content
+    
+        def self.setup(&block)
+      define_method(:setup, &block)
     end
-  end
-end
-
     
-          # Calls the given block for each condition node in the `when` branch.
-      # If no block is given, an `Enumerator` is returned.
-      #
-      # @return [self] if a block is given
-      # @return [Enumerator] if no block is given
-      def each_condition
-        return conditions.to_enum(__method__) unless block_given?
-    
-        def options_config=(options_config)
-      loaded_config = ConfigLoader.load_file(options_config)
-      @options_config = ConfigLoader.merge_with_default(loaded_config,
-                                                        options_config)
-    end
+        @view = Precious::Views::Page.new
+    @view.instance_variable_set :@page, page
+    @view.instance_variable_set :@content, page.formatted_data
+    @view.instance_variable_set :@h1_title, false
