f1b2380986a881f9c0646f03f0a4d882f9b03f25
SUNsung <weatherius@gmail.com>
2019-05-14 11:28:41 +0300

    Rewrite [29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py] File in: 2019-05-14 11:28:40

--- pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
+++ pyton/29hvjJGY8r3ew29cg0GwdeEgCk29FnOijCWpSFY29oay.py
@@ -1,206 +1,71 @@
 
         
-        import codecs
-import subprocess
-    
-    README_FILE = 'README.md'
-helptext = sys.stdin.read()
-    
-        params = {
-        'age_limit': age,
-        'skip_download': True,
-        'writeinfojson': True,
-        'outtmpl': '%(id)s.%(ext)s',
-    }
-    ydl = YoutubeDL(params)
-    ydl.add_default_info_extractors()
-    json_filename = os.path.splitext(filename)[0] + '.info.json'
-    try_rm(json_filename)
-    ydl.download([url])
-    res = os.path.exists(json_filename)
-    try_rm(json_filename)
-    return res
-    
-        def test_allsubtitles(self):
-        self.DL.params['writesubtitles'] = True
-        self.DL.params['allsubtitles'] = True
-        subtitles = self.getSubtitles()
-        self.assertEqual(set(subtitles.keys()), set(['en']))
-        self.assertEqual(md5(subtitles['en']), '97e7670cbae3c4d26ae8bcc7fdd78d4b')
-    
-    # Snapshot of volume with tagging
-- ec2_snapshot:
-    instance_id: i-12345678
-    device_name: /dev/sdb1
-    snapshot_tags:
-        frequency: hourly
-        source: /data
-    
-    EXAMPLES = '''
-# Add or change a subnet group
-- elasticache_subnet_group:
-    state: present
-    name: norwegian-blue
-    description: My Fancy Ex Parrot Subnet Group
-    subnets:
-      - subnet-aaaaaaaa
-      - subnet-bbbbbbbb
-    
-        elif state == 'present':
-        for param in ('name', 'rules'):
-            if not module.params.get(param):
-                module.fail_json(
-                    msg='%s parameter is required for new firewall policies.' % param)
+            def process_options(self, args, opts):
         try:
-            (changed, firewall_policy) = create_firewall_policy(module, oneandone_conn)
-        except Exception as e:
-            module.fail_json(msg=str(e))
+            self.settings.setdict(arglist_to_dict(opts.set),
+                                  priority='cmdline')
+        except ValueError:
+            raise UsageError('Invalid -s value, use -s NAME=VALUE', print_help=False)
     
-    # Make coding more python3-ish
-from __future__ import (absolute_import, division, print_function)
-__metaclass__ = type
+        def start_requests(self):
+        qargs = {'total': self.total, 'show': self.show}
+        url = '{}?{}'.format(self.baseurl, urlencode(qargs, doseq=1))
+        return [scrapy.Request(url, dont_filter=True)]
     
+            # start checks
+        if opts.list:
+            for spider, methods in sorted(contract_reqs.items()):
+                if not methods and not opts.verbose:
+                    continue
+                print(spider)
+                for method in sorted(methods):
+                    print('  * %s' % method)
+        else:
+            start = time.time()
+            self.crawler_process.start()
+            stop = time.time()
     
-DOCUMENTATION = '''
----
-module: ipa_role
-author: Thomas Krahn (@Nosmoht)
-short_description: Manage FreeIPA role
-description:
-- Add, modify and delete a role within FreeIPA server using FreeIPA API
-options:
-  cn:
-    description:
-    - Role name.
-    - Can not be changed as it is the unique identifier.
-    required: true
-    aliases: ['name']
-  description:
-    description:
-    - A description of this role-group.
-  group:
-    description:
-    - List of group names assign to this role.
-    - If an empty list is passed all assigned groups will be unassigned from the role.
-    - If option is omitted groups will not be checked or changed.
-    - If option is passed all assigned groups that are not passed will be unassigned from the role.
-  host:
-    description:
-    - List of host names to assign.
-    - If an empty list is passed all assigned hosts will be unassigned from the role.
-    - If option is omitted hosts will not be checked or changed.
-    - If option is passed all assigned hosts that are not passed will be unassigned from the role.
-  hostgroup:
-    description:
-    - List of host group names to assign.
-    - If an empty list is passed all assigned host groups will be removed from the role.
-    - If option is omitted host groups will not be checked or changed.
-    - If option is passed all assigned hostgroups that are not passed will be unassigned from the role.
-  privilege:
-    description:
-    - List of privileges granted to the role.
-    - If an empty list is passed all assigned privileges will be removed.
-    - If option is omitted privileges will not be checked or changed.
-    - If option is passed all assigned privileges that are not passed will be removed.
-    version_added: '2.4'
-  service:
-    description:
-    - List of service names to assign.
-    - If an empty list is passed all assigned services will be removed from the role.
-    - If option is omitted services will not be checked or changed.
-    - If option is passed all assigned services that are not passed will be removed from the role.
-  state:
-    description: State to ensure
-    default: 'present'
-    choices: ['present', 'absent']
-  user:
-    description:
-    - List of user names to assign.
-    - If an empty list is passed all assigned users will be removed from the role.
-    - If option is omitted users will not be checked or changed.
-extends_documentation_fragment: ipa.documentation
-version_added: '2.3'
-'''
+    # Documents to append as an appendix to all manuals.
+#latex_appendices = []
+    
+            '''
+        # Check for the root of save problems
+        new_errs = self.aug.match('/augeas//error')
+        # logger.error('During Save - %s', mod_conf)
+        logger.error('Unable to save files: %s. Attempted Save Notes: %s',
+                     ', '.join(err[13:len(err) - 6] for err in new_errs
+                               # Only new errors caused by recent save
+                               if err not in ex_errs), self.save_notes)
     
     
-def main():
-    arg_spec = dict(
-        name=dict(required=True),
-        timeout=dict(default=300, type='int'),
-        state=dict(required=True, choices=['present', 'started', 'restarted', 'stopped', 'monitored', 'unmonitored', 'reloaded'])
-    )
+# -- Options for HTML output ----------------------------------------------
+    
+        def test_duplicate_function_equality(self):
+        # Body of `duplicate' is the exact same as self.b
+        def duplicate():
+            'my docstring'
+            return 3
+        self.assertNotEqual(self.b, duplicate)
+    
+        def _testFile(self, filename):
+        path = os.path.join(os.path.dirname(__file__), filename)
+        TestRoundtrip.check_roundtrip(self, open(path, 'rb'))
     
     
-def do_send_request(module, url, params, key):
-    data = json.dumps(params)
-    headers = {
-        'Content-Type': 'application/json',
-        'x-stackdriver-apikey': key
-    }
-    response, info = fetch_url(module, url, headers=headers, data=data, method='POST')
-    if info['status'] != 200:
-        module.fail_json(msg='Unable to send msg: %s' % info['msg'])
-    
-        plt.figure('scikit-learn parallel %s benchmark results' % func.__name__)
-    plt.plot(sample_sizes, one_core, label='one core')
-    plt.plot(sample_sizes, multi_core, label='multi core')
-    plt.xlabel('n_samples')
-    plt.ylabel('Time (s)')
-    plt.title('Parallel %s' % func.__name__)
-    plt.legend()
-    
-    plt.figure('scikit-learn Ward's method benchmark results')
-plt.imshow(np.log(ratio), aspect='auto', origin='lower')
-plt.colorbar()
-plt.contour(ratio, levels=[1, ], colors='k')
-plt.yticks(range(len(n_features)), n_features.astype(np.int))
-plt.ylabel('N features')
-plt.xticks(range(len(n_samples)), n_samples.astype(np.int))
-plt.xlabel('N samples')
-plt.title('Scikit's time, in units of scipy time (log)')
-plt.show()
-
-    
-        # import matplotlib.pyplot as plt
-    # plt.matshow(cm)
-    # plt.show()
-
-    
-    # original face
-plt.figure(1, figsize=(3, 2.2))
-plt.imshow(face, cmap=plt.cm.gray, vmin=vmin, vmax=256)
-    
-    We will cluster a set of data, first with KMeans and then with
-MiniBatchKMeans, and plot the results.
-We will also plot the points that are labelled differently between the two
-algorithms.
-'''
-print(__doc__)
-    
-    from ._base import Detector, dlib, logger
+# Is a path a directory?
+# This follows symbolic links, so both islink() and isdir()
+# can be true for the same path on systems that support symlinks
+def isdir(s):
+    '''Return true if the pathname refers to an existing directory.'''
+    try:
+        st = os.stat(s)
+    except (OSError, ValueError):
+        return False
+    return stat.S_ISDIR(st.st_mode)
     
     
-class Model(OriginalModel):
-    ''' Villain Faceswap Model '''
-    def __init__(self, *args, **kwargs):
-        logger.debug('Initializing %s: (args: %s, kwargs: %s',
-                     self.__class__.__name__, args, kwargs)
-    
-            else:
-            if keep_original:
-                def process_file(src, dst, changes):  # pylint: disable=unused-argument
-                    ''' Process file method if not logging changes
-                        and keeping original '''
-                    copyfile(src, dst)
-    
-        def graph_build(self, frame):
-        ''' Build the graph in the top right paned window '''
-        logger.debug('Building Graph')
-        self.graph = SessionGraph(frame,
-                                  self.display_data,
-                                  self.vars['display'].get(),
-                                  self.vars['scale'].get())
-        self.graph.pack(expand=True, fill=tk.BOTH)
-        self.graph.build()
-        self.graph_initialised = True
-        logger.debug('Built Graph')
+def read_text(package: Package,
+              resource: Resource,
+              encoding: str = 'utf-8',
+              errors: str = 'strict') -> str:
+    '''Return the decoded string of the resource.
